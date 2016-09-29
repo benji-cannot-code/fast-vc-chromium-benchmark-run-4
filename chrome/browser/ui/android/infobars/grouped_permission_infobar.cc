49182b2a139a78b74962931d65a19c0b5387dde1
lshang <lshang@chromium.org>
2016-09-28 22:40:05 -0700

    Add InfoBarIdentifier to GroupedPermissionInfoBarDelegate
    This will also allow GroupedPermissionInfoBarDelegate to be instantiated to manage
    other permission types.
    
    BUG=606138
    
    Review-Url: https://codereview.chromium.org/2319443002
    Cr-Commit-Position: refs/heads/master@{#421758}

--- chrome/browser/ui/android/infobars/grouped_permission_infobar.cc
+++ chrome/browser/ui/android/infobars/grouped_permission_infobar.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_array.h"
 #include "base/android/jni_string.h"
 #include "chrome/browser/android/resource_mapper.h"
-#include "chrome/browser/permissions/grouped_permission_infobar_delegate.h"
+#include "chrome/browser/permissions/grouped_permission_infobar_delegate_android.h"
 #include "jni/GroupedPermissionInfoBar_jni.h"
 
 GroupedPermissionInfoBar::GroupedPermissionInfoBar(
@@ -27,11 +27,10 @@ void GroupedPermissionInfoBar::SetPermissionState(
     JNIEnv* env,
     const base::android::JavaParamRef<jobject>& obj,
     const base::android::JavaParamRef<jbooleanArray>& permissions) {
-
-  for (int i = 0; i < GetDelegate()->GetPermissionCount(); i++) {
-      jboolean value;
-      env->GetBooleanArrayRegion(permissions.obj(), i, 1, &value);
-      GetDelegate()->ToggleAccept(i, value);
+  for (size_t i = 0; i < GetDelegate()->permission_count(); i++) {
+    jboolean value;
+    env->GetBooleanArrayRegion(permissions.obj(), i, 1, &value);
+    GetDelegate()->ToggleAccept(i, value);
   }
 }
 
@@ -65,7 +64,7 @@ GroupedPermissionInfoBar::CreateRenderInfoBar(JNIEnv* env) {
   std::vector<int> permission_icons;
   std::vector<int> content_settings_types;
 
-  for (int i = 0; i < delegate->GetPermissionCount(); i++) {
+  for (size_t i = 0; i < delegate->permission_count(); i++) {
     permission_strings.push_back(delegate->GetMessageTextFragment(i));
     permission_icons.push_back(
         ResourceMapper::MapFromChromiumId(delegate->GetIconIdForPermission(i)));
@@ -91,12 +90,4 @@ void GroupedPermissionInfoBar::SetJavaInfoBar(
 
 GroupedPermissionInfoBarDelegate* GroupedPermissionInfoBar::GetDelegate() {
   return static_cast<GroupedPermissionInfoBarDelegate*>(delegate());
-}
-
-std::unique_ptr<infobars::InfoBar>
-GroupedPermissionInfoBarDelegate::CreateInfoBar(
-    infobars::InfoBarManager* infobar_manager,
-    std::unique_ptr<GroupedPermissionInfoBarDelegate> delegate) {
-  return std::unique_ptr<infobars::InfoBar>(
-      new GroupedPermissionInfoBar(std::move(delegate)));
 }
