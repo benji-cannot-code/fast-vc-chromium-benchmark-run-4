e55320b03aa7ff3d3e68234476a6493a249bf702
sammc <sammc@chromium.org>
2016-09-28 18:49:14 -0700

    Move registration of Java mojo interfaces to the new InterfaceRegistrar.
    Previously, Java-implemented mojo interfaces were registered on the C++
    InterfaceRegistries via JNI. This CL migrates those registrations to
    dedicated Java InterfaceRegistries that C++ code can access via
    corresponding InterfaceProviders.
    
    BUG=634568, 637174
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Committed: https://crrev.com/58dcb20da65389d841cd02d9197fb43efb6e8ee2
    Review-Url: https://codereview.chromium.org/2214383002
    Cr-Original-Commit-Position: refs/heads/master@{#421403}
    Cr-Commit-Position: refs/heads/master@{#421711}

--- chrome/browser/android/mojo/chrome_interface_registrar_android.h
+++ chrome/browser/android/mojo/chrome_interface_registrar_android.h
@@ -6,25 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ANDROID_MOJO_CHROME_INTERFACE_REGISTRAR_ANDROID_H_
 #define CHROME_BROWSER_ANDROID_MOJO_CHROME_INTERFACE_REGISTRAR_ANDROID_H_
 
-#include <jni.h>
-
-namespace content {
-class RenderFrameHost;
-}
-
-namespace shell {
-class InterfaceRegistry;
-}
-
-class ChromeInterfaceRegistrarAndroid {
- public:
-  static void ExposeInterfacesToFrame(
-      shell::InterfaceRegistry* registry,
-      content::RenderFrameHost* render_frame_host);
-
- private:
-  ChromeInterfaceRegistrarAndroid() {}
-  ~ChromeInterfaceRegistrarAndroid() {}
-};
+void RegisterChromeJavaMojoInterfaces();
 
 #endif  // CHROME_BROWSER_ANDROID_MOJO_CHROME_INTERFACE_REGISTRAR_ANDROID_H_
--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -99,6 +99,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_ANDROID)
 #include "content/browser/mojo/interface_registrar_android.h"
+#include "content/public/browser/android/java_interfaces.h"
 #if defined(ENABLE_MOJO_CDM)
 #include "content/browser/media/android/provision_fetcher_impl.h"
 #endif
@@ -2145,7 +2146,11 @@ void RenderFrameHostImpl::RegisterMojoInterfaces() {
   GetInterfaceRegistry()->AddInterface(base::Bind(
       &PresentationServiceImpl::CreateMojoService, base::Unretained(this)));
 
-#if !defined(OS_ANDROID)
+#if defined(OS_ANDROID)
+  GetInterfaceRegistry()->AddInterface(
+      GetGlobalJavaInterfaces()
+          ->CreateInterfaceFactory<device::VibrationManager>());
+#else
   GetInterfaceRegistry()->AddInterface(
       base::Bind(&device::VibrationManagerImpl::Create));
 #endif
