5458566495168d0bed5b1e039fc4cd6a974da3b4
nzolghadr <nzolghadr@chromium.org>
2016-09-29 11:04:41 -0700

    Update automation scripts for wpt tests
    Update automation scripts for web platform pointer
    event tests to accomodate for reduce hit-testing.
    
    BUG=630671
    
    Review-Url: https://codereview.chromium.org/2380943003
    Cr-Commit-Position: refs/heads/master@{#421869}

--- third_party/WebKit/LayoutTests/imported/wpt_automation/pointerevents/pointerevent_lostpointercapture_is_first-manual-automation.js
+++ third_party/WebKit/LayoutTests/imported/wpt_automation/pointerevents/pointerevent_lostpointercapture_is_first-manual-automation.js
@@ -2,9 +2,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 importAutomationScript('/pointerevents/pointerevent_common_input.js');
 
 function inject_input() {
-  return mouseClickInTarget('#btnCapture').then(function() {
-    // To handle delayed capturing.
-    return mouseMoveIntoTarget('#btnCapture');
-  });
+  return mouseDragInTarget('#btnCapture');
 }
 
