e2c57a9784134d5028037458a935cfaec9381206
dgn <dgn@chromium.org>
2016-09-29 05:47:33 -0700

    [NTP Client] Stop refreshing the whole NTP for every adapter change
    Replace NTPAdapter#updateGroups() with more granular notifications.
    
    Note: We are using SuggestionSections#getItems() quite often,
    and that creates new lists every time, so it could still
    be improved.
    
    Preview: https://goo.gl/photos/UHWg4pfbkWWPHdGi8
    BUG=647304,643106,647671
    
    Review-Url: https://codereview.chromium.org/2360813004
    Cr-Commit-Position: refs/heads/master@{#421806}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/snippets/FakeSuggestionsSource.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/snippets/FakeSuggestionsSource.java
@@ -111,7 +111,7 @@ public class FakeSuggestionsSource implements SuggestionsSource {
     @Override
     public void getSuggestionVisited(
             SnippetArticle suggestion, Callback<Boolean> callback) {
-        throw new UnsupportedOperationException();
+        callback.onResult(false);
     }
 
     @Override
