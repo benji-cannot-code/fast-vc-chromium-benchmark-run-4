6e28dae55f9464a7545b5c39831ce90fd9a96652
qinmin <qinmin@chromium.org>
2016-09-29 15:30:10 -0700

    Use system download completion notification if app notification is disabled.
    Because all download now goes through Chrome, user will not see
    notification if app notification is disabled.
    This change allows us to use system DownloadManager to display the
    completion notification,
    in case app notification is disabled.
    
    BUG=648221
    
    Review-Url: https://codereview.chromium.org/2387453002
    Cr-Commit-Position: refs/heads/master@{#421959}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadManagerDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadManagerDelegate.java
@@ -11,6 +11,7 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
+import android.support.v4.app.NotificationManagerCompat;
 
 import org.chromium.base.Log;
 
@@ -36,6 +37,8 @@ public class DownloadManagerDelegate {
             String path, long length, String originalUrl, String referer) {
         DownloadManager manager =
                 (DownloadManager) mContext.getSystemService(Context.DOWNLOAD_SERVICE);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(mContext);
+        boolean useSystemNotification = !notificationManager.areNotificationsEnabled();
         String newMimeType =
                 ChromeDownloadDelegate.remapGenericMimeType(mimeType, originalUrl, fileName);
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
@@ -47,7 +50,7 @@ public class DownloadManagerDelegate {
                 Uri originalUri = Uri.parse(originalUrl);
                 Uri refererUri = referer == null ? Uri.EMPTY : Uri.parse(referer);
                 return (Long) method.invoke(manager, fileName, description, true, newMimeType, path,
-                        length, false, originalUri, refererUri);
+                        length, useSystemNotification, originalUri, refererUri);
             } catch (SecurityException e) {
                 Log.e(TAG, "Cannot access the needed method.");
             } catch (NoSuchMethodException e) {
@@ -59,7 +62,7 @@ public class DownloadManagerDelegate {
             }
         }
         return manager.addCompletedDownload(fileName, description, true, newMimeType, path, length,
-                false);
+                useSystemNotification);
     }
 
     /**
