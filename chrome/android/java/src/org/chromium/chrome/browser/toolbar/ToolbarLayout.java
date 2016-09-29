a73df9128c8d7447979223bfcd60de5fe5fe1b5b
tedchoc <tedchoc@chromium.org>
2016-09-29 10:42:34 -0700

    Remove non-elderberry herb flavors.
    BUG=650769
    
    Review-Url: https://codereview.chromium.org/2371003003
    Cr-Commit-Position: refs/heads/master@{#421865}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarLayout.java
@@ -785,8 +785,4 @@ abstract class ToolbarLayout extends FrameLayout implements Toolbar {
                     R.string.accessibility_toolbar_btn_menu));
         }
     }
-
-    @Override
-    public void setReturnButtonListener(View.OnClickListener listener) {
-    }
 }
