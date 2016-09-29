cbcbf02bbad80aa65d54ec22b4d933b91536183f
sashab <sashab@chromium.org>
2016-09-28 22:17:07 -0700

    Added support for non-inherited flags in ComputedStyleBase
    Added support for non-inherited flags to be generated in
    ComputedStyleBase. Also moved Float and its enum EFloat to be generated.
    
    BUG=628043
    
    Review-Url: https://codereview.chromium.org/2351993002
    Cr-Commit-Position: refs/heads/master@{#421754}

--- third_party/WebKit/Source/core/style/ComputedStyleConstants.h
+++ third_party/WebKit/Source/core/style/ComputedStyleConstants.h
@@ -125,10 +125,6 @@ enum EPosition {
     FixedPosition = 6
 };
 
-enum class EFloat : unsigned {
-    None, Left, Right
-};
-
 enum EMarginCollapse { MarginCollapseCollapse, MarginCollapseSeparate, MarginCollapseDiscard };
 
 // Box decoration attributes. Not inherited.
