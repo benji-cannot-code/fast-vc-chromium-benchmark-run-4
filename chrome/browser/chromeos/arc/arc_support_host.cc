b52b6dfadf5a4d380ef3056dd1a48799b16a96ba
hidehiko <hidehiko@chromium.org>
2016-09-30 13:03:45 -0700

    Fix resource leak on extension window closing.
    The ARC support extension should be unloaded on window
    closed. However, it is not, because the port connected to
    Chrome is kept. In such a case, background page is kept alive
    so that the extension will not be unloaded.
    
    This CL closes the port so that extension is unloaded.
    Also, the "close" related messaging across ArcAuthService,
    ArcSupportHost and extension's background.js is slightly
    cleaned up to remove the unnecessary/unused code.
    It could be as a preparation of fixing dependencies, too.
    
    BUG=b/31079732, 651838
    TEST=Ran on device. Ran unittests.
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2380683008
    Cr-Commit-Position: refs/heads/master@{#422196}

--- chrome/browser/chromeos/arc/arc_support_host.cc
+++ chrome/browser/chromeos/arc/arc_support_host.cc
@@ -48,14 +48,17 @@ const char kActionBackupAndRestoreMode[] = "setBackupAndRestoreMode";
 const char kActionLocationServiceMode[] = "setLocationServiceMode";
 const char kActionSetWindowBounds[] = "setWindowBounds";
 const char kActionStartLso[] = "startLso";
-const char kActionCancelAuthCode[] = "cancelAuthCode";
 const char kActionSetAuthCode[] = "setAuthCode";
 const char kActionEnableMetrics[] = "enableMetrics";
 const char kActionSendFeedback[] = "sendFeedback";
 const char kActionSetBackupRestore[] = "setBackupRestore";
 const char kActionSetLocationService[] = "setLocationService";
-const char kActionCloseUI[] = "closeUI";
+const char kActionCloseWindow[] = "closeWindow";
 const char kActionShowPage[] = "showPage";
+
+// Fired when the extension window is closed.
+const char kActionOnWindowClosed[] = "onWindowClosed";
+
 }  // namespace
 
 // static
@@ -110,12 +113,24 @@ ArcSupportHost::~ArcSupportHost() {
     arc_auth_service->RemoveObserver(this);
 }
 
+void ArcSupportHost::Close() {
+  if (!client_)
+    return;
+
+  close_requested_ = true;
+  base::DictionaryValue response;
+  response.SetString(kAction, kActionCloseWindow);
+  std::string response_string;
+  base::JSONWriter::Write(response, &response_string);
+  client_->PostMessageFromNativeHost(response_string);
+}
+
 void ArcSupportHost::Start(Client* client) {
   DCHECK(!client_);
   client_ = client;
 
   if (!Initialize()) {
-    OnOptInUIClose();
+    Close();
     return;
   }
 
@@ -286,14 +301,7 @@ void ArcSupportHost::SendLocationServicesMode() {
 }
 
 void ArcSupportHost::OnOptInUIClose() {
-  if (!client_)
-    return;
-
-  base::DictionaryValue response;
-  response.SetString(kAction, kActionCloseUI);
-  std::string response_string;
-  base::JSONWriter::Write(response, &response_string);
-  client_->PostMessageFromNativeHost(response_string);
+  Close();
 }
 
 void ArcSupportHost::OnOptInUIShowPage(arc::ArcAuthService::UIPage page,
@@ -355,8 +363,9 @@ void ArcSupportHost::OnMessage(const std::string& request_string) {
       return;
     }
     arc_auth_service->SetAuthCodeAndStartArc(code);
-  } else if (action == kActionCancelAuthCode) {
-    arc_auth_service->CancelAuthCode();
+  } else if (action == kActionOnWindowClosed) {
+    if (!close_requested_)
+      arc_auth_service->CancelAuthCode();
   } else if (action == kActionEnableMetrics) {
     bool is_enabled;
     if (!request->GetBoolean(kEnabled, &is_enabled)) {
--- chrome/browser/resources/chromeos/arc_support/background.js
+++ chrome/browser/resources/chromeos/arc_support/background.js
@@ -42,19 +42,6 @@ var termsView = null;
 var port = null;
 
 /**
- * Indicates that window was closed internally and it is not required to send
- * closure notification.
- * @type {boolean}
- */
-var windowClosedInternally = false;
-
-/**
- * Timer for terms reload.
- * @type {Object}
- */
-var termsReloadTimeout = null;
-
-/**
  * Stores current device id.
  * @type {string}
  */
@@ -90,16 +77,6 @@ var INNER_WIDTH = 960;
  */
 var INNER_HEIGHT = 688;
 
-
-/**
- * Closes current window in response to request from native code. This does not
- * issue 'cancelAuthCode' message to native code.
- */
-function closeWindowInternally() {
-  windowClosedInternally = true;
-  appWindow.close();
-  appWindow = null;
-}
 
 /**
  * Sends a native message to ArcSupportHost.
@@ -302,8 +279,10 @@ function onNativeMessage(message) {
     setBackupRestoreMode(message.enabled, message.managed);
   } else if (message.action == 'setLocationServiceMode') {
     setLocationServiceMode(message.enabled, message.managed);
-  } else if (message.action == 'closeUI') {
-    closeWindowInternally();
+  } else if (message.action == 'closeWindow') {
+    if (appWindow) {
+      appWindow.close();
+    }
   } else if (message.action == 'showPage') {
     showPageWithStatus(message.page, message.status);
   } else if (message.action == 'setWindowBounds') {
@@ -321,17 +300,12 @@ function connectPort() {
 }
 
 /**
- * Shows requested page and hide others. Show appWindow if it was hidden before
- * for non 'none' pages. For 'none' page this closes host window.
+ * Shows requested page and hide others. Show appWindow if it was hidden before.
+ * 'none' hides all views.
  * @param {string} pageDivId id of divider of the page to show.
  */
 function showPage(pageDivId) {
   if (!appWindow) {
-    return;
-  }
-
-  if (pageDivId == 'none') {
-    closeWindowInternally();
     return;
   }
 
@@ -578,9 +552,7 @@ chrome.app.runtime.onLaunched.addListener(function() {
 
     var onCancel = function() {
       if (appWindow) {
-        windowClosedInternally = false;
         appWindow.close();
-        appWindow = null;
       }
     };
 
@@ -608,24 +580,23 @@ chrome.app.runtime.onLaunched.addListener(function() {
   var onWindowCreated = function(createdWindow) {
     appWindow = createdWindow;
     appWindow.contentWindow.onload = onAppContentLoad;
-    createdWindow.onClosed.addListener(onWindowClosed);
+    appWindow.onClosed.addListener(onWindowClosed);
 
     setWindowBounds();
   };
 
   var onWindowClosed = function() {
-    if (termsReloadTimeout) {
-      clearTimeout(termsReloadTimeout);
-      termsReloadTimeout = null;
-    }
+    appWindow = null;
 
-    if (windowClosedInternally) {
-      return;
-    }
-    sendNativeMessage('cancelAuthCode');
+    // Notify to Chrome.
+    sendNativeMessage('onWindowClosed');
+
+    // On window closed, then dispose the extension. So, close the port
+    // otherwise the background page would be kept alive so that the extension
+    // would not be unloaded.
+    port.disconnect();
+    port = null;
   };
-
-  windowClosedInternally = false;
 
   var options = {
     'id': 'play_store_wnd',
