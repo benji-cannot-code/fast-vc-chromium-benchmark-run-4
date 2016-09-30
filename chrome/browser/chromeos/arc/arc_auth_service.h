bfb849e0426d55a80c60184155451b0fdb59f9d8
peletskyi <peletskyi@chromium.org>
2016-09-30 07:35:15 -0700

    Added parameter that specifies which type of account the auth code belongs to. For now only user account is supported. But in future other types of account can be added (e.g. robot account).
    BUG=634497
    
    Review-Url: https://codereview.chromium.org/2368233002
    Cr-Commit-Position: refs/heads/master@{#422107}

--- chrome/browser/chromeos/arc/arc_auth_service.h
+++ chrome/browser/chromeos/arc/arc_auth_service.h
@@ -145,6 +145,8 @@ class ArcAuthService : public ArcService,
   void GetAuthCodeDeprecated(
       const GetAuthCodeDeprecatedCallback& callback) override;
   void GetAuthCode(const GetAuthCodeCallback& callback) override;
+  void GetAuthCodeAndAccountType(
+      const GetAuthCodeAndAccountTypeCallback& callback) override;
   void OnSignInComplete() override;
   void OnSignInFailed(arc::mojom::ArcSignInFailureReason reason) override;
   // Callback is called with a bool that indicates the management status of the
@@ -235,6 +237,7 @@ class ArcAuthService : public ArcService,
   std::unique_ptr<ArcAppLauncher> playstore_launcher_;
   std::string auth_code_;
   GetAuthCodeCallback auth_callback_;
+  GetAuthCodeAndAccountTypeCallback auth_account_callback_;
   bool initial_opt_in_ = false;
   bool disable_arc_from_ui_ = false;
   UIPage ui_page_ = UIPage::NO_PAGE;
