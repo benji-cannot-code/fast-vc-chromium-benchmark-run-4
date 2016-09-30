217d65a94916d7a896328e476e6bfef83d24e9c6
beaufort.francois <beaufort.francois@gmail.com>
2016-09-30 00:50:24 -0700

    bluetooth: Add chrome://bluetooth-internals to chrome://chrome-urls
    BUG=651282
    
    Review-Url: https://codereview.chromium.org/2379883003
    Cr-Commit-Position: refs/heads/master@{#422060}

--- chrome/common/url_constants.cc
+++ chrome/common/url_constants.cc
@@ -609,13 +609,14 @@ const char kNotificationWelcomeLearnMoreURL[] =
 // Add hosts here to be included in chrome://chrome-urls (about:about).
 // These hosts will also be suggested by BuiltinProvider.
 const char* const kChromeHostURLs[] = {
+  kChromeUIBluetoothInternalsHost,
   kChromeUICacheHost,
   kChromeUIChromeURLsHost,
   kChromeUIComponentsHost,
   kChromeUICrashesHost,
   kChromeUICreditsHost,
-  kChromeUIDeviceLogHost,
   kChromeUIDNSHost,
+  kChromeUIDeviceLogHost,
   kChromeUIFlagsHost,
   kChromeUIGCMInternalsHost,
   kChromeUIHistoryHost,
