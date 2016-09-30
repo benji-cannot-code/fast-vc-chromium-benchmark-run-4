428f3327bfd67721807c31ff4c073a906d79bbde
meade <meade@chromium.org>
2016-09-30 01:11:07 -0700

    Update generated CSSOM type checking to allow checking just the type
    This is in addition to being able to check a whole CSSStyleValue.
    
    BUG=545318
    
    Review-Url: https://codereview.chromium.org/2380933002
    Cr-Commit-Position: refs/heads/master@{#422062}

--- third_party/WebKit/Source/core/css/cssom/CSSOMTypes.h
+++ third_party/WebKit/Source/core/css/cssom/CSSOMTypes.h
@@ -7,17 +7,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CSSOMTypes_h
 
 #include "core/CSSPropertyNames.h"
+#include "core/css/cssom/CSSStyleValue.h"
 #include "wtf/Allocator.h"
 
 namespace blink {
-
-class CSSStyleValue;
 
 class CSSOMTypes {
     STATIC_ONLY(CSSOMTypes);
 
 public:
     static bool propertyCanTake(CSSPropertyID, const CSSStyleValue&);
+    static bool propertyCanTakeType(CSSPropertyID, CSSStyleValue::StyleValueType);
 };
 
 } // namespace blink
