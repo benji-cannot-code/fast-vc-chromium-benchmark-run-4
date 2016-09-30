a90d4f59b54559d3b7d8815d9be87f9e4877844d
lushnikov <lushnikov@chromium.org>
2016-09-30 13:51:48 -0700

    DevTools: [CMTE] hide autocomplete when cursor jumps over smart brace
    If the cursor is located right before closing brace and user types in
    ")", then cursor just jumps over the brace. This is a "smart brace"
    functionality.
    
    In this case, it is important to close autocomplete. Today this doesn't
    happen because the onChange event is not sent in this case.
    
    BUG=none
    R=einbinder
    
    Review-Url: https://codereview.chromium.org/2382323002
    Cr-Commit-Position: refs/heads/master@{#422214}

--- third_party/WebKit/Source/devtools/front_end/text_editor/TextEditorAutocompleteController.js
+++ third_party/WebKit/Source/devtools/front_end/text_editor/TextEditorAutocompleteController.js
@@ -298,6 +298,8 @@ WebInspector.TextEditorAutocompleteController.prototype = {
      */
     _onSuggestionsShownForTest: function(suggestions) { },
 
+    _onSuggestionsHiddenForTest: function() { },
+
     clearAutocomplete: function()
     {
         if (!this._suggestBox)
@@ -307,6 +309,7 @@ WebInspector.TextEditorAutocompleteController.prototype = {
         this._prefixRange = null;
         this._anchorBox = null;
         this._clearHintMarker();
+        this._onSuggestionsHiddenForTest();
     },
 
     /**
@@ -401,7 +404,15 @@ WebInspector.TextEditorAutocompleteController.prototype = {
         if (!this._suggestBox)
             return;
         var cursor = this._codeMirror.getCursor();
-        if (cursor.line !== this._prefixRange.startLine || cursor.ch > this._prefixRange.endColumn + 1 || cursor.ch <= this._prefixRange.startColumn)
+        // Hide autocomplete for smart braces.
+        if (cursor.line === this._prefixRange.startLine && cursor.ch === this._prefixRange.endColumn + 1) {
+            var line = this._codeMirror.getLine(cursor.line);
+            var isWordChar = this._config.isWordChar(line.charAt(cursor.ch - 1));
+            if (!isWordChar)
+                this.clearAutocomplete();
+            return;
+        }
+        if (cursor.line !== this._prefixRange.startLine || cursor.ch > this._prefixRange.endColumn || cursor.ch <= this._prefixRange.startColumn)
             this.clearAutocomplete();
     },
 
