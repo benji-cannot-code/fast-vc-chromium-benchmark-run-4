2c2dde751abe97b914ea2884e476e460065edf17
thestig <thestig@chromium.org>
2016-09-30 15:42:16 -0700

    Remove PrintingContext::InitWithSettings().
    It is unused on many platforms. Keep it in the implementations on
    platforms that do use it.
    
    Review-Url: https://codereview.chromium.org/2383473004
    Cr-Commit-Position: refs/heads/master@{#422250}

--- printing/printing_context_android.cc
+++ printing/printing_context_android.cc
@@ -25,19 +25,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using base::android::JavaParamRef;
 using base::android::ScopedJavaLocalRef;
 
+namespace printing {
+
 namespace {
 
 // 1 inch in mils.
 const int kInchToMil = 1000;
 
-inline int Round(double x) {
+int Round(double x) {
   return static_cast<int>(x + 0.5);
 }
 
 // Sets the page sizes for a |PrintSettings| object.  |width| and |height|
 // arguments should be in device units.
-void SetSizes(
-    printing::PrintSettings* settings, int dpi, int width, int height) {
+void SetSizes(PrintSettings* settings, int dpi, int width, int height) {
   gfx::Size physical_size_device_units(width, height);
   // Assume full page is printable for now.
   gfx::Rect printable_area_device_units(0, 0, width, height);
@@ -48,24 +49,18 @@ void SetSizes(
                                     false);
 }
 
-void GetPageRanges(JNIEnv* env,
-                   jintArray int_arr,
-                   printing::PageRanges& range_vector) {
+void GetPageRanges(JNIEnv* env, jintArray int_arr, PageRanges* range_vector) {
   std::vector<int> pages;
   base::android::JavaIntArrayToIntVector(env, int_arr, &pages);
-  for (std::vector<int>::const_iterator it = pages.begin();
-      it != pages.end();
-      ++it) {
-    printing::PageRange range;
-    range.from = *it;
-    range.to = *it;
-    range_vector.push_back(range);
+  for (int page : pages) {
+    PageRange range;
+    range.from = page;
+    range.to = page;
+    range_vector->push_back(range);
   }
 }
 
 }  // namespace
-
-namespace printing {
 
 // static
 std::unique_ptr<PrintingContext> PrintingContext::Create(Delegate* delegate) {
@@ -127,9 +122,9 @@ void PrintingContextAndroid::AskUserForSettingsReply(
 
   ScopedJavaLocalRef<jintArray> intArr =
       Java_PrintingContext_getPages(env, j_printing_context_);
-  if (intArr.obj() != NULL) {
+  if (intArr.obj()) {
     PageRanges range_vector;
-    GetPageRanges(env, intArr.obj(), range_vector);
+    GetPageRanges(env, intArr.obj(), &range_vector);
     settings_.set_ranges(range_vector);
   }
 
@@ -199,15 +194,6 @@ PrintingContext::Result PrintingContextAndroid::UpdatePrinterSettings(
   return OK;
 }
 
-PrintingContext::Result PrintingContextAndroid::InitWithSettings(
-    const PrintSettings& settings) {
-  DCHECK(!in_print_job_);
-
-  settings_ = settings;
-
-  return OK;
-}
-
 PrintingContext::Result PrintingContextAndroid::NewDocument(
     const base::string16& document_name) {
   DCHECK(!in_print_job_);
@@ -256,12 +242,12 @@ void PrintingContextAndroid::ReleaseContext() {
 
 gfx::NativeDrawingContext PrintingContextAndroid::context() const {
   // Intentional No-op.
-  return NULL;
+  return nullptr;
 }
 
 // static
 bool PrintingContextAndroid::RegisterPrintingContext(JNIEnv* env) {
-   return RegisterNativesImpl(env);
+  return RegisterNativesImpl(env);
 }
 
 }  // namespace printing
