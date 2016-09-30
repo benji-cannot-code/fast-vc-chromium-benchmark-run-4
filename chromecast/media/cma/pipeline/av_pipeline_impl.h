122e171fc82cc1bd7de2c24ac118e83d5b0954af
alokp <alokp@chromium.org>
2016-09-30 11:13:52 -0700

    Delete MediaPipelineImpl::Stop and AvPipelineImpl::Stop.
    MediaPipelineImpl::Stop was dead code. The logic in AvPipelineImpl::Stop
    has been folded into AvPipelineImpl::Flush.
    
    BUG=571155
    
    Review-Url: https://codereview.chromium.org/2364783002
    Cr-Commit-Position: refs/heads/master@{#422154}

--- chromecast/media/cma/pipeline/av_pipeline_impl.h
+++ chromecast/media/cma/pipeline/av_pipeline_impl.h
@@ -51,9 +51,6 @@ class AvPipelineImpl : MediaPipelineBackend::Decoder::Delegate {
                         const scoped_refptr<BufferingState>& buffering_state);
   void Flush(const base::Closure& flush_cb);
 
-  // Stop feeding the backend.
-  void Stop();
-
   virtual void UpdateStatistics() = 0;
 
   int bytes_decoded_since_last_update() const {
@@ -67,7 +64,6 @@ class AvPipelineImpl : MediaPipelineBackend::Decoder::Delegate {
     kPlaying,
     kFlushing,
     kFlushed,
-    kStopped,
     kError,
   };
 
