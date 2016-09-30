dce7864b2be51c8a5489f194c0a89ebc79319bf4
vitaliii <vitaliii@chromium.org>
2016-09-30 01:12:13 -0700

    [NTPSnippets] Show all downloads on the NTP (2/3): RecentTabs provider.
    This CL is the second of the three CLs.
    The overall aim is to show all types of downloads on the NTP in
    downloads section.
    
    This CL removes OfflinePageSuggestionsProvider and instead adds
    RecentTabSuggestionsProvider, which does not provide downloads and
    listens to OfflinePageProxy.
    
    If the first CL of the three is reverted, this CL must be reverted too.
    
    BUG=639233
    
    Review-Url: https://codereview.chromium.org/2359073002
    Cr-Commit-Position: refs/heads/master@{#422063}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -159,7 +159,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_ANDROID)
 #include "chrome/browser/notifications/notification_platform_bridge_android.h"
-#include "components/ntp_snippets/offline_pages/offline_page_suggestions_provider.h"
+#include "components/ntp_snippets/offline_pages/recent_tab_suggestions_provider.h"
 #else
 #include "chrome/browser/services/gcm/gcm_product_util.h"
 #include "chrome/browser/signin/signin_promo.h"
@@ -577,7 +577,7 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
 
 #if defined(OS_ANDROID)
   NotificationPlatformBridgeAndroid::RegisterProfilePrefs(registry);
-  ntp_snippets::OfflinePageSuggestionsProvider::RegisterProfilePrefs(registry);
+  ntp_snippets::RecentTabSuggestionsProvider::RegisterProfilePrefs(registry);
 #else
   browser_sync::ForeignSessionHandler::RegisterProfilePrefs(registry);
   gcm::GCMChannelStatusSyncer::RegisterProfilePrefs(registry);
