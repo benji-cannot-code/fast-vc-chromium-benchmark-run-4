f4c7c8c7310d55d2c7af63286c6765d9b96ff35f
cjgrant <cjgrant@chromium.org>
2016-09-30 11:25:58 -0700

    VrShell: Fix button animations on hover.
    BUG=641413
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2384743003
    Cr-Commit-Position: refs/heads/master@{#422157}

--- chrome/browser/resources/vr_shell/vr_shell_ui.js
+++ chrome/browser/resources/vr_shell/vr_shell_ui.js
@@ -129,7 +129,6 @@ var vrShellUi = (function() {
     constructor(meshId, durationMs) {
       this.meshId = meshId;
       this.easing = {};
-      this.from = {};
       this.to = {};
       this.easing.type = Easing.LINEAR;
 
@@ -211,13 +210,13 @@ var vrShellUi = (function() {
         var resize = new Animation(buttonId, 250);
         resize.id = idIndex++;
         resize.setResizeTo(width, height);
-        chrome.send('addAnimations', [resize]);
+        addAnimations([resize]);
       }.bind(undefined, id, buttonWidth * 1.5, buttonHeight * 1.5));
       b.addEventListener('mouseleave', function(buttonId, width, height) {
         var resize = new Animation(buttonId, 250);
         resize.id = idIndex++;
         resize.setResizeTo(width, height);
-        chrome.send('addAnimations', [resize]);
+        addAnimations([resize]);
       }.bind(undefined, id, buttonWidth, buttonHeight));
     }
   }
