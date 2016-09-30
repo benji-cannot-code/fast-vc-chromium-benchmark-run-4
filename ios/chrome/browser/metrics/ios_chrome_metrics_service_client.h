e33f1a08f3275d7b4d3bf1d55a5a3826c57487c9
asvitkine <asvitkine@chromium.org>
2016-09-30 10:00:04 -0700

    Make logging of user actions consistent with histograms for OTR.
    Previously, we would not record user actions in UMA when an OTR
    (Incognito) window was active. However, since user actions don't
    contain any PII it was just causing a confusing discrepancy in
    counts when compared to histograms. This CL makes them consistent
    by making user actions be logged regardless of the OTR state.
    
    BUG=604001
    TBR=sgurun@chromium.org, wez@chromium.org, rohitrao@chromium.org
    
    Review-Url: https://codereview.chromium.org/2383533002
    Cr-Commit-Position: refs/heads/master@{#422133}

--- ios/chrome/browser/metrics/ios_chrome_metrics_service_client.h
+++ ios/chrome/browser/metrics/ios_chrome_metrics_service_client.h
@@ -54,7 +54,6 @@ class IOSChromeMetricsServiceClient
   // metrics::MetricsServiceClient:
   metrics::MetricsService* GetMetricsService() override;
   void SetMetricsClientId(const std::string& client_id) override;
-  bool IsOffTheRecordSessionActive() override;
   int32_t GetProduct() override;
   std::string GetApplicationLocale() override;
   bool GetBrand(std::string* brand_code) override;
