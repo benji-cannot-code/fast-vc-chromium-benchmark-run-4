b548c3b6b14fe19290f3cee5947f40122f0b879e
oshima <oshima@chromium.org>
2016-09-30 10:39:24 -0700

    Revert auto-hide-invisible mode for ARC++
    This was added in https://codereview.chromium.org/2248773002
    to implement MD's new auto hide behavior for ARC++ windows. This is now default, so reverting that part.
    
    BUG=637883
    
    Review-Url: https://codereview.chromium.org/2368343002
    Cr-Commit-Position: refs/heads/master@{#422147}

--- components/exo/shell_surface.cc
+++ components/exo/shell_surface.cc
@@ -6,11 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/shell_surface.h"
 
 #include "ash/aura/wm_window_aura.h"
+#include "ash/common/shelf/wm_shelf.h"
 #include "ash/common/shell_window_ids.h"
 #include "ash/common/wm/window_resizer.h"
 #include "ash/common/wm/window_state.h"
 #include "ash/common/wm/window_state_delegate.h"
-#include "ash/shell.h"
+#include "ash/common/wm_shell.h"
 #include "ash/wm/window_state_aura.h"
 #include "ash/wm/window_util.h"
 #include "base/logging.h"
@@ -54,9 +55,9 @@ const struct Accelerator {
 void UpdateShelfStateForFullscreenChange(views::Widget* widget) {
   ash::wm::WindowState* window_state =
       ash::wm::GetWindowState(widget->GetNativeWindow());
-  window_state->set_shelf_mode_in_fullscreen(
-      ash::wm::WindowState::SHELF_AUTO_HIDE_INVISIBLE);
-  ash::Shell::GetInstance()->UpdateShelfVisibility();
+  window_state->set_hide_shelf_when_fullscreen(false);
+  for (ash::WmWindow* root_window : ash::WmShell::Get()->GetAllRootWindows())
+    ash::WmShelf::ForWindow(root_window)->UpdateVisibilityState();
 }
 
 class CustomFrameView : public views::NonClientFrameView {
@@ -402,7 +403,6 @@ void ShellSurface::SetFullscreen(bool fullscreen) {
   // state doesn't change.
   ScopedConfigure scoped_configure(this, true);
   widget_->SetFullscreen(fullscreen);
-  UpdateShelfStateForFullscreenChange(widget_);
 }
 
 void ShellSurface::SetPinned(bool pinned, bool trusted) {
