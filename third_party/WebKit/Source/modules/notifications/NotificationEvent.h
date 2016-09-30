5e1b057c169c593d59738bf550abd211e2f50db0
awdf <awdf@google.com>
2016-09-30 08:20:18 -0700

    Added reply field to blink NotificationEvent (not yet piped through)
    Added a blink test for notificationevent.reply
    
    BUG=599859
    
    Review-Url: https://codereview.chromium.org/2380273002
    Cr-Commit-Position: refs/heads/master@{#422116}

--- third_party/WebKit/Source/modules/notifications/NotificationEvent.h
+++ third_party/WebKit/Source/modules/notifications/NotificationEvent.h
@@ -32,6 +32,7 @@ public:
 
     Notification* getNotification() const { return m_notification.get(); }
     String action() const { return m_action; }
+    String reply() const { return m_reply; }
 
     const AtomicString& interfaceName() const override;
 
@@ -43,6 +44,7 @@ private:
 
     Member<Notification> m_notification;
     String m_action;
+    String m_reply;
 };
 
 } // namespace blink
