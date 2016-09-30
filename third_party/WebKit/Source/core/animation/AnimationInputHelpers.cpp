ff0803032bfe4d10fdcc7d3836841b2a18a6acee
alancutter <alancutter@chromium.org>
2016-09-29 23:15:55 -0700

    Store custom properties specified in element.animate()
    This change alters calls to element.animate() that use custom properties
    such that the custom property values are stored internally in animation
    keyframes.
    
    This patch has no behavioural changes as custom property animations
    are currently not applied (see bug). This patch is a step towards
    enabling custom property animations.
    
    BUG=644148
    
    Review-Url: https://codereview.chromium.org/2324963003
    Cr-Commit-Position: refs/heads/master@{#422041}

--- third_party/WebKit/Source/core/animation/AnimationInputHelpers.cpp
+++ third_party/WebKit/Source/core/animation/AnimationInputHelpers.cpp
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/SVGNames.h"
 #include "core/css/CSSValueList.h"
 #include "core/css/parser/CSSParser.h"
+#include "core/css/parser/CSSVariableParser.h"
 #include "core/css/resolver/CSSToStyleMap.h"
 #include "core/frame/Deprecation.h"
 #include "core/svg/SVGElement.h"
@@ -33,10 +34,13 @@ static String removeSVGPrefix(const String& property)
 
 CSSPropertyID AnimationInputHelpers::keyframeAttributeToCSSProperty(const String& property, const Document& document)
 {
-    // TODO(crbug.com/644148): Allow custom properties that begin with "--".
+    if (CSSVariableParser::isValidVariableName(property))
+        return CSSPropertyVariable;
 
     // Disallow prefixed properties.
-    if (property[0] == '-' || isASCIIUpper(property[0]))
+    if (property[0] == '-')
+        return CSSPropertyInvalid;
+    if (isASCIIUpper(property[0]))
         return CSSPropertyInvalid;
     if (property == "cssFloat")
         return CSSPropertyFloat;
