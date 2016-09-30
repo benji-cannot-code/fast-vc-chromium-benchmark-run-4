d69829e7736f66d6e3d2a53916bebc43c8cb6cec
pdr <pdr@chromium.org>
2016-09-30 10:37:27 -0700

    Hit test floats in visual painting order
    BlockFlowPainter paints inlines and then the floats. Hit testing
    should work in reverse: hit test floats, then inlines. This patch fixes
    a bug where overlapping floats were hit test in the wrong order.
    
    This patch is partially based on http://wkrev.com/192854 by Jiewen Tan.
    
    Review-Url: https://codereview.chromium.org/2386553002
    Cr-Commit-Position: refs/heads/master@{#422146}

--- third_party/WebKit/Source/core/layout/LayoutBlockFlow.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBlockFlow.cpp
@@ -3271,6 +3271,10 @@ Node* LayoutBlockFlow::nodeForHitTest() const
 bool LayoutBlockFlow::hitTestChildren(HitTestResult& result, const HitTestLocation& locationInContainer, const LayoutPoint& accumulatedOffset, HitTestAction hitTestAction)
 {
     LayoutPoint scrolledOffset(hasOverflowClip() ? accumulatedOffset - scrolledContentOffset() : accumulatedOffset);
+
+    if (hitTestAction == HitTestFloat && hitTestFloats(result, locationInContainer, scrolledOffset))
+        return true;
+
     if (childrenInline()) {
         if (m_lineBoxes.hitTest(LineLayoutBoxModel(this), result, locationInContainer, scrolledOffset, hitTestAction)) {
             updateHitTestResult(result, flipForWritingMode(toLayoutPoint(locationInContainer.point() - accumulatedOffset)));
@@ -3279,9 +3283,6 @@ bool LayoutBlockFlow::hitTestChildren(HitTestResult& result, const HitTestLocati
     } else if (LayoutBlock::hitTestChildren(result, locationInContainer, accumulatedOffset, hitTestAction)) {
         return true;
     }
-
-    if (hitTestAction == HitTestFloat && hitTestFloats(result, locationInContainer, scrolledOffset))
-        return true;
 
     return false;
 }
