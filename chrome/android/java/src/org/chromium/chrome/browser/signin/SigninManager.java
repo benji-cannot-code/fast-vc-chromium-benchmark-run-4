6526d5cf2a5c83694b08a3ce7da97f1902fa1b26
twellington <twellington@chromium.org>
2016-09-29 17:17:31 -0700

    Return false for #isSignInAllowed() if #isDemoUser()
    SigninManager#isSignInAllowed() returns false if
    ApiCompatibilityUtils#isDemoUser() returns true because
    sign is not available in demo mode. This will remove ensure
    the sign in activity isn't launched and remove sign in
    promos from places like the bookmark manager
    
    BUG=633289
    
    Review-Url: https://codereview.chromium.org/2379973003
    Cr-Commit-Position: refs/heads/master@{#421990}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninManager.java
@@ -11,6 +11,7 @@ import android.content.Context;
 import android.os.Handler;
 
 import org.chromium.base.ActivityState;
+import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.ApplicationStatus;
 import org.chromium.base.Callback;
 import org.chromium.base.Log;
@@ -235,7 +236,8 @@ public class SigninManager implements AccountTrackerService.OnSystemAccountsSeed
      */
     public boolean isSignInAllowed() {
         return mSigninAllowedByPolicy && !mFirstRunCheckIsPending && mSignInState == null
-                && ChromeSigninController.get(mContext).getSignedInUser() == null;
+                && ChromeSigninController.get(mContext).getSignedInUser() == null
+                && !ApiCompatibilityUtils.isDemoUser(mContext);
     }
 
     /**
