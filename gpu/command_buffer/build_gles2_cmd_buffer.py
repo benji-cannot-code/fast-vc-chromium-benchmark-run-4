b7725088c72555faa4a64b6298d89c9047e4ba8a
zmo <zmo@chromium.org>
2016-09-30 12:57:56 -0700

    Strengthing CopyBufferSubData in GPU command buffer.
    1) Makes sure shadow data is updated accordingly.
    2) Add all validations, including WebGL2 specific limitation.
    
    This CL also check if buffer is mapped in BufferSubData.
    
    BUG=429053
    TEST=webgl2_conformance,gpu_unittests
    R=kbr@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2385583002
    Cr-Commit-Position: refs/heads/master@{#422194}

--- gpu/command_buffer/build_gles2_cmd_buffer.py
+++ gpu/command_buffer/build_gles2_cmd_buffer.py
@@ -2565,6 +2565,8 @@ _FUNCTION_INFO = {
     'trace_level': 2,
   },
   'CopyBufferSubData': {
+    'decoder_func': 'DoCopyBufferSubData',
+    'unit_test': False,
     'unsafe': True,
   },
   'CoverageModulationCHROMIUM': {
