7a02d55f21a22ab6c1e35a5766139433f558027a
mlamouri <mlamouri@chromium.org>
2016-10-01 07:15:22 -0700

    Don't use cast overlay when using default controls.
    It's now in the overflow menu.
    
    BUG=651428
    
    Review-Url: https://codereview.chromium.org/2378933005
    Cr-Commit-Position: refs/heads/master@{#422315}

--- third_party/WebKit/Source/core/html/shadow/MediaControls.cpp
+++ third_party/WebKit/Source/core/html/shadow/MediaControls.cpp
@@ -775,7 +775,6 @@ void MediaControls::computeWhichControlsFit() {
   std::list<MediaControlElement*> overflowElements;
   MediaControlElement* firstDisplacedElement = nullptr;
   // For each control that fits, enable it in order of decreasing priority.
-  bool droppedCastButton = false;
   for (MediaControlElement* element : elements) {
     if (!element)
       continue;
@@ -789,10 +788,6 @@ void MediaControls::computeWhichControlsFit() {
         usedWidth += width;
       } else {
         element->setDoesFit(false);
-        // TODO(mlamouri): we should probably no longer mark the cast
-        // button as dropped, it should appear in the overflow menu.
-        if (element == m_castButton.get())
-          droppedCastButton = true;
         element->shouldShowButtonInOverflowMenu(true);
         if (element->hasOverflowButton())
           overflowElements.push_front(element);
@@ -822,15 +817,6 @@ void MediaControls::computeWhichControlsFit() {
   } else if (overflowElements.size() == 1) {
     m_overflowMenu->setIsWanted(false);
     overflowElements.front()->setDoesFit(true);
-  }
-
-  // Special case for cast: if we want a cast button but dropped it, then
-  // show the overlay cast button instead.
-  if (m_castButton->isWanted()) {
-    if (droppedCastButton)
-      m_overlayCastButton->tryShowOverlay();
-    else
-      m_overlayCastButton->setIsWanted(false);
   }
 }
 
