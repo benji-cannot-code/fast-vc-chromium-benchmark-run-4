731d360a36eb98baeecddc71ec4d2c2738d4aff4
rch <rch@chromium.org>
2016-09-30 19:18:27 -0700

    Continue to shrink the exposed QuicClient interface in favor of QuicClientBase
    The next CL will introduce a Helper interface that will encapsulate the
    existing QuicClient methods.
    
    Merge internal change: 134620371
    
    Review-Url: https://codereview.chromium.org/2387593003
    Cr-Commit-Position: refs/heads/master@{#422290}

--- net/tools/quic/test_tools/quic_client_peer.cc
+++ net/tools/quic/test_tools/quic_client_peer.cc
@@ -17,7 +17,9 @@ QuicCryptoClientConfig* QuicClientPeer::GetCryptoConfig(QuicClient* client) {
 
 // static
 bool QuicClientPeer::CreateUDPSocketAndBind(QuicClient* client) {
-  return client->CreateUDPSocketAndBind();
+  return client->CreateUDPSocketAndBind(client->server_address(),
+                                        client->bind_to_address(),
+                                        client->local_port());
 }
 
 // static
