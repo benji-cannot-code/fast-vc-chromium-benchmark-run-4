6abc3bcc4508871befe709eb46db2a0297b4a6c4
petewil <petewil@chromium.org>
2016-09-30 22:13:03 -0700

    [Offline Pages] Prevents background loading if 3rd part cookies not allowed This is minimal patch for merging to M54. It provides preference check and also counts attempts to start so that requests will not forever remain in the request queue. It lacks some notification support for Downloads though so that might be judged if needed for M54 separately (since Downloads not expected to launch to Stable 54 after all)
    Also check to see if the user has disabled prerendering via preferences.
    
    This is taking over an existing change, http://crrev.com/2371203003.
    
    BUG=650409
    
    patch from issue 2371203003 at patchset 60001 (http://crrev.com/2371203003#ps60001)
    
    Review-Url: https://codereview.chromium.org/2385953002
    Cr-Commit-Position: refs/heads/master@{#422300}

--- chrome/browser/android/offline_pages/prerendering_offliner.cc
+++ chrome/browser/android/offline_pages/prerendering_offliner.cc
@@ -8,10 +8,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/sys_info.h"
 #include "chrome/browser/android/offline_pages/offline_page_mhtml_archiver.h"
+#include "chrome/browser/net/prediction_options.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/common/pref_names.h"
+#include "components/content_settings/core/common/pref_names.h"
 #include "components/offline_pages/background/save_page_request.h"
+#include "components/offline_pages/client_namespace_constants.h"
 #include "components/offline_pages/offline_page_model.h"
+#include "components/prefs/pref_service.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/web_contents.h"
+
+namespace {
+
+bool AreThirdPartyCookiesBlocked(content::BrowserContext* browser_context) {
+  return Profile::FromBrowserContext(browser_context)
+      ->GetPrefs()
+      ->GetBoolean(prefs::kBlockThirdPartyCookies);
+}
+
+bool IsNetworkPredictionDisabled(content::BrowserContext* browser_context) {
+  return Profile::FromBrowserContext(browser_context)
+             ->GetPrefs()
+             ->GetInteger(prefs::kNetworkPredictionOptions) ==
+         chrome_browser_net::NETWORK_PREDICTION_NEVER;
+}
+
+}  // namespace
 
 namespace offline_pages {
 
@@ -117,6 +140,17 @@ bool PrerenderingOffliner::LoadAndSave(const SavePageRequest& request,
 
   if (!GetOrCreateLoader()->CanPrerender()) {
     DVLOG(1) << "Prerendering not allowed/configured";
+    return false;
+  }
+
+  // Do not allow loading for custom tabs clients if 3rd party cookies blocked.
+  // TODO(dewittj): Revise api to specify policy rather than hard code to
+  // name_space.
+  if (request.client_id().name_space == kCCTNamespace &&
+      (AreThirdPartyCookiesBlocked(browser_context_) ||
+       IsNetworkPredictionDisabled(browser_context_))) {
+    DVLOG(1) << "WARNING: Unable to load when 3rd party cookies blocked or "
+             << "prediction disabled";
     return false;
   }
 
