8a33ef80f94986dbd5a8422e8ea684abfa157413
alph <alph@chromium.org>
2016-10-01 10:17:40 -0700

    Add TRACE_EVENT_SAMPLE_WITH_ID1 macro.
    It is going to be used to record V8 CpuProfile events stream.
    
    BUG=406277
    
    Review-Url: https://codereview.chromium.org/2385973002
    Cr-Commit-Position: refs/heads/master@{#422323}

--- base/trace_event/common/trace_event_common.h
+++ base/trace_event/common/trace_event_common.h
@@ -550,6 +550,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       TRACE_EVENT_PHASE_SAMPLE, category_group, name, 0, thread_id, timestamp, \
       TRACE_EVENT_FLAG_NONE, arg1_name, arg1_val, arg2_name, arg2_val)
 
+#define TRACE_EVENT_SAMPLE_WITH_ID1(category_group, name, id, arg1_name,       \
+                                    arg1_val)                                  \
+  INTERNAL_TRACE_EVENT_ADD_WITH_ID(TRACE_EVENT_PHASE_SAMPLE, category_group,   \
+                                   name, id, TRACE_EVENT_FLAG_NONE, arg1_name, \
+                                   arg1_val)
+
 // ASYNC_STEP_* APIs should be only used by legacy code. New code should
 // consider using NESTABLE_ASYNC_* APIs to describe substeps within an async
 // event.
