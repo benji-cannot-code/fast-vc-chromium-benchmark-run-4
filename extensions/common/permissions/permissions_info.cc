f8f862a3bd513cd0a0d9a141121a647e859dbab7
rdevlin.cronin <rdevlin.cronin@chromium.org>
2016-09-30 18:55:15 -0700

    [Extensions] Make PermissionsInfo leaky
    BUG=650842
    
    Review-Url: https://codereview.chromium.org/2387703002
    Cr-Commit-Position: refs/heads/master@{#422267}

--- extensions/common/permissions/permissions_info.cc
+++ extensions/common/permissions/permissions_info.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace extensions {
 
-static base::LazyInstance<PermissionsInfo> g_permissions_info =
+static base::LazyInstance<PermissionsInfo>::Leaky g_permissions_info =
     LAZY_INSTANCE_INITIALIZER;
 
 // static
