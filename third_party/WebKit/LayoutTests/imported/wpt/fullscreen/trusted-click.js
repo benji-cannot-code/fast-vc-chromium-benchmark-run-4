38dc11666a32b71be2b5e46b670e06fcc3aacdd9
foolip <foolip@chromium.org>
2016-10-01 01:47:44 -0700

    Import wpt@09907a9c4bcee14986431d53e4381384c7c69107
    Using update-w3c-deps in Chromium 2bce9cb8f98aaed076e6129966aaf38a9f001fd1.
    
    BUG=651660
    
    Review-Url: https://codereview.chromium.org/2376103007
    Cr-Commit-Position: refs/heads/master@{#422309}

---
+++ third_party/WebKit/LayoutTests/imported/wpt/fullscreen/trusted-click.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Invokes callback from a trusted click event, to satisfy
+// https://html.spec.whatwg.org/#triggered-by-user-activation
+function trusted_click(callback, container)
+{
+    var document = container.ownerDocument;
+    var button = document.createElement("button");
+    button.textContent = "click to continue test";
+    button.style.display = "block";
+    button.style.fontSize = "20px";
+    button.style.padding = "10px";
+    button.onclick = function()
+    {
+        callback();
+        container.removeChild(button);
+    };
+    container.appendChild(button);
+}
+
+// Invokes element.requestFullscreen() from a trusted click.
+function trusted_request(element, container)
+{
+    var request = element.requestFullscreen.bind(element);
+    trusted_click(request, container || element.parentNode);
+}
