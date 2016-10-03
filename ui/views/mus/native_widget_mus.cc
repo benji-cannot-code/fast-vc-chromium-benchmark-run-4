0f9231811b930c08c01f5bd153a43fe10e84af28
erg <erg@chromium.org>
2016-10-03 16:38:09 -0700

    Revert of mus: Use TooltipManagerAura in NativeWidgetMus. (patchset #9 id:160001 of https://codereview.chromium.org/2379073003/ )
    Reason for revert:
    sky@ reports that TooltipControllerCaptureTest.Capture is failing on local chromeos builds.
    
    Original issue's description:
    > mus: Use TooltipManagerAura in NativeWidgetMus.
    >
    > mash now shows tooltips on hover over items.
    >
    > BUG=599558
    >
    > Committed: https://crrev.com/1b3d5f1878974c14c21a29a13a93ef630c74d1f6
    > Cr-Commit-Position: refs/heads/master@{#422562}
    
    TBR=sky@chromium.org,jonross@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=599558
    
    Review-Url: https://codereview.chromium.org/2384083005
    Cr-Commit-Position: refs/heads/master@{#422599}

--- ui/views/mus/native_widget_mus.cc
+++ ui/views/mus/native_widget_mus.cc
@@ -36,9 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/path.h"
 #include "ui/native_theme/native_theme_aura.h"
 #include "ui/platform_window/platform_window_delegate.h"
-#include "ui/views/corewm/tooltip.h"
-#include "ui/views/corewm/tooltip_aura.h"
-#include "ui/views/corewm/tooltip_controller.h"
 #include "ui/views/drag_utils.h"
 #include "ui/views/mus/drag_drop_client_mus.h"
 #include "ui/views/mus/drop_target_mus.h"
@@ -48,7 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/mus/window_tree_host_mus.h"
 #include "ui/views/widget/drop_helper.h"
 #include "ui/views/widget/native_widget_aura.h"
-#include "ui/views/widget/tooltip_manager_aura.h"
 #include "ui/views/widget/widget_delegate.h"
 #include "ui/views/window/custom_frame_view.h"
 #include "ui/wm/core/base_focus_rules.h"
@@ -594,14 +590,6 @@ aura::Window* NativeWidgetMus::GetRootWindow() {
 void NativeWidgetMus::OnPlatformWindowClosed() {
   native_widget_delegate_->OnNativeWidgetDestroying();
 
-  tooltip_manager_.reset();
-  if (tooltip_controller_.get()) {
-    window_tree_host_->window()->RemovePreTargetHandler(
-        tooltip_controller_.get());
-    aura::client::SetTooltipClient(window_tree_host_->window(), NULL);
-    tooltip_controller_.reset();
-  }
-
   window_tree_client_.reset();  // Uses |content_|.
   capture_client_.reset();      // Uses |content_|.
 
@@ -732,15 +720,6 @@ void NativeWidgetMus::InitNativeWidget(const Widget::InitParams& params) {
   drop_helper_ = base::MakeUnique<DropHelper>(GetWidget()->GetRootView());
   aura::client::SetDragDropDelegate(content_, this);
 
-  if (params.type != Widget::InitParams::TYPE_TOOLTIP) {
-    tooltip_manager_ = base::MakeUnique<TooltipManagerAura>(GetWidget());
-    tooltip_controller_ = base::MakeUnique<corewm::TooltipController>(
-        base::MakeUnique<corewm::TooltipAura>());
-    aura::client::SetTooltipClient(window_tree_host_->window(),
-                                   tooltip_controller_.get());
-    window_tree_host_->window()->AddPreTargetHandler(tooltip_controller_.get());
-  }
-
   // TODO(erg): Remove this check when ash/mus/move_event_handler.cc's
   // direct usage of ui::Window::SetPredefinedCursor() is switched to a
   // private method on WindowManagerClient.
@@ -864,7 +843,8 @@ void* NativeWidgetMus::GetNativeWindowProperty(const char* name) const {
 }
 
 TooltipManager* NativeWidgetMus::GetTooltipManager() const {
-  return tooltip_manager_.get();
+  // NOTIMPLEMENTED();
+  return nullptr;
 }
 
 void NativeWidgetMus::SetCapture() {
@@ -1430,12 +1410,8 @@ void NativeWidgetMus::OnKeyEvent(ui::KeyEvent* event) {
 }
 
 void NativeWidgetMus::OnMouseEvent(ui::MouseEvent* event) {
+  // TODO(sky): forward to tooltipmanager. See NativeWidgetDesktopAura.
   DCHECK(content_->IsVisible());
-
-  if (tooltip_manager_.get())
-    tooltip_manager_->UpdateTooltip();
-  TooltipManagerAura::UpdateTooltipManagerForCapture(GetWidget());
-
   native_widget_delegate_->OnMouseEvent(event);
   // WARNING: we may have been deleted.
 }
--- ui/views/widget/tooltip_manager_aura.h
+++ ui/views/widget/tooltip_manager_aura.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/strings/string16.h"
 #include "ui/gfx/geometry/point.h"
-#include "ui/views/views_export.h"
 #include "ui/views/widget/tooltip_manager.h"
 
 namespace aura {
@@ -26,7 +25,7 @@ namespace views {
 class Widget;
 
 // TooltipManager implementation for Aura.
-class VIEWS_EXPORT TooltipManagerAura : public TooltipManager {
+class TooltipManagerAura : public TooltipManager {
  public:
   explicit TooltipManagerAura(Widget* widget);
   ~TooltipManagerAura() override;
