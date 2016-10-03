3684eaf8d4235d97a0d529940bcb6426a2d08ca2
fdoray <fdoray@chromium.org>
2016-10-03 10:22:41 -0700

    Use FileDescriptorWatcher in ClipboardX11.
    This allows ClipboardX11 to be used from any thread that
    instantiates a FileDescriptorWatcher (not just threads that run
    a MessageLoopForIO). This will facilitate the migration of
    BrowserThreads to base/task_scheduler.
    
    BUG=645114
    
    Review-Url: https://codereview.chromium.org/2382723007
    Cr-Commit-Position: refs/heads/master@{#422448}

--- remoting/host/clipboard_x11.cc
+++ remoting/host/clipboard_x11.cc
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #undef Status  // Xlib.h #defines this, which breaks protobuf headers.
 
 #include "base/bind.h"
+#include "base/files/file_descriptor_watcher_posix.h"
 #include "base/logging.h"
 #include "base/macros.h"
-#include "base/message_loop/message_loop.h"
 #include "remoting/host/linux/x_server_clipboard.h"
 #include "remoting/proto/event.pb.h"
 #include "remoting/protocol/clipboard_stub.h"
@@ -21,8 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace remoting {
 
 // This code is expected to be called on the desktop thread only.
-class ClipboardX11 : public Clipboard,
-                     public base::MessageLoopForIO::Watcher {
+class ClipboardX11 : public Clipboard {
  public:
   ClipboardX11();
   ~ClipboardX11() override;
@@ -31,10 +30,6 @@ class ClipboardX11 : public Clipboard,
   void Start(
       std::unique_ptr<protocol::ClipboardStub> client_clipboard) override;
   void InjectClipboardEvent(const protocol::ClipboardEvent& event) override;
-
-  // MessageLoopForIO::Watcher interface.
-  void OnFileCanReadWithoutBlocking(int fd) override;
-  void OnFileCanWriteWithoutBlocking(int fd) override;
 
  private:
   void OnClipboardChanged(const std::string& mime_type,
@@ -51,7 +46,8 @@ class ClipboardX11 : public Clipboard,
   Display* display_;
 
   // Watcher used to handle X11 events from |display_|.
-  base::MessageLoopForIO::FileDescriptorWatcher x_connection_watcher_;
+  std::unique_ptr<base::FileDescriptorWatcher::Controller>
+      x_connection_watch_controller_;
 
   DISALLOW_COPY_AND_ASSIGN(ClipboardX11);
 };
@@ -68,6 +64,7 @@ ClipboardX11::~ClipboardX11() {
 void ClipboardX11::Start(
     std::unique_ptr<protocol::ClipboardStub> client_clipboard) {
   // TODO(lambroslambrou): Share the X connection with InputInjector.
+  DCHECK(!display_);
   display_ = XOpenDisplay(nullptr);
   if (!display_) {
     LOG(ERROR) << "Couldn't open X display";
@@ -79,25 +76,15 @@ void ClipboardX11::Start(
                            base::Bind(&ClipboardX11::OnClipboardChanged,
                                       base::Unretained(this)));
 
-  base::MessageLoopForIO::current()->WatchFileDescriptor(
+  x_connection_watch_controller_ = base::FileDescriptorWatcher::WatchReadable(
       ConnectionNumber(display_),
-      true,
-      base::MessageLoopForIO::WATCH_READ,
-      &x_connection_watcher_,
-      this);
+      base::Bind(&ClipboardX11::PumpXEvents, base::Unretained(this)));
   PumpXEvents();
 }
 
 void ClipboardX11::InjectClipboardEvent(
     const protocol::ClipboardEvent& event) {
   x_server_clipboard_.SetClipboard(event.mime_type(), event.data());
-}
-
-void ClipboardX11::OnFileCanReadWithoutBlocking(int fd) {
-  PumpXEvents();
-}
-
-void ClipboardX11::OnFileCanWriteWithoutBlocking(int fd) {
 }
 
 void ClipboardX11::OnClipboardChanged(const std::string& mime_type,
