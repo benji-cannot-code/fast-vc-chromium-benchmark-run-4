a6546bdc35756a773d46cd9d57c46bd594fa54eb
thakis <thakis@chromium.org>
2016-10-03 15:24:13 -0700

    reflow comments in platform/{animation,blob,clipboard,exported}
    BUG=563793
    
    Review-Url: https://codereview.chromium.org/2390833002
    Cr-Commit-Position: refs/heads/master@{#422563}

--- third_party/WebKit/Source/platform/exported/WebURLResponse.cpp
+++ third_party/WebKit/Source/platform/exported/WebURLResponse.cpp
@@ -96,8 +96,8 @@ WebURLResponse::WebURLResponse(const WebURL& url) : WebURLResponse() {
 }
 
 WebURLResponse& WebURLResponse::operator=(const WebURLResponse& r) {
-  // Copying subclasses that have different m_resourceResponse ownership semantics
-  // via this operator is just not supported.
+  // Copying subclasses that have different m_resourceResponse ownership
+  // semantics via this operator is just not supported.
   DCHECK(m_ownedResourceResponse);
   DCHECK(m_resourceResponse);
   if (&r != this)
