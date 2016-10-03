01ce7d2853dce5e5552a02dcbb7a922fa1bf99b1
csharrison <csharrison@chromium.org>
2016-10-03 07:09:31 -0700

    Reformat comments in core/html/parser
    BUG=563793
    
    Review-Url: https://codereview.chromium.org/2386893002
    Cr-Commit-Position: refs/heads/master@{#422410}

--- third_party/WebKit/Source/core/html/parser/BackgroundHTMLParser.cpp
+++ third_party/WebKit/Source/core/html/parser/BackgroundHTMLParser.cpp
@@ -43,23 +43,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-// On a network with high latency and high bandwidth, using a device
-// with a fast CPU, we could end up speculatively tokenizing
-// the whole document, well ahead of when the main-thread actually needs it.
-// This is a waste of memory (and potentially time if the speculation fails).
-// So we limit our outstanding tokens arbitrarily to 10,000.
-// Our maximal memory spent speculating will be approximately:
+// On a network with high latency and high bandwidth, using a device with a fast
+// CPU, we could end up speculatively tokenizing the whole document, well ahead
+// of when the main-thread actually needs it. This is a waste of memory (and
+// potentially time if the speculation fails). So we limit our outstanding
+// tokens arbitrarily to 10,000. Our maximal memory spent speculating will be
+// approximately:
 // (defaultOutstandingTokenLimit + defaultPendingTokenLimit) *
-//     sizeof(CompactToken)
-// We use a separate low and high water mark to avoid constantly topping
-// off the main thread's token buffer.
-// At time of writing, this is (10000 + 1000) * 28 bytes = ~308kb of memory.
-// These numbers have not been tuned.
+// sizeof(CompactToken)
+//
+// We use a separate low and high water mark to avoid
+// constantly topping off the main thread's token buffer. At time of writing,
+// this is (10000 + 1000) * 28 bytes = ~308kb of memory. These numbers have not
+// been tuned.
 static const size_t defaultOutstandingTokenLimit = 10000;
 
-// We limit our chucks to 1000 tokens, to make sure the main
-// thread is never waiting on the parser thread for tokens.
-// This was tuned in https://bugs.webkit.org/show_bug.cgi?id=110408.
+// We limit our chucks to 1000 tokens, to make sure the main thread is never
+// waiting on the parser thread for tokens. This was tuned in
+// https://bugs.webkit.org/show_bug.cgi?id=110408.
 static const size_t defaultPendingTokenLimit = 1000;
 
 using namespace HTMLNames;
@@ -196,8 +197,8 @@ void BackgroundHTMLParser::resumeFrom(std::unique_ptr<Checkpoint> checkpoint) {
 
 void BackgroundHTMLParser::startedChunkWithCheckpoint(
     HTMLInputCheckpoint inputCheckpoint) {
-  // Note, we should not have to worry about the index being invalid
-  // as messages from the main thread will be processed in FIFO order.
+  // Note, we should not have to worry about the index being invalid as messages
+  // from the main thread will be processed in FIFO order.
   m_input.invalidateCheckpointsBefore(inputCheckpoint);
   pumpTokenizer();
 }
@@ -212,9 +213,10 @@ void BackgroundHTMLParser::stop() {
 }
 
 void BackgroundHTMLParser::forcePlaintextForTextDocument() {
-  // This is only used by the TextDocumentParser (a subclass of HTMLDocumentParser)
-  // to force us into the PLAINTEXT state w/o using a <plaintext> tag.
-  // The TextDocumentParser uses a <pre> tag for historical/compatibility reasons.
+  // This is only used by the TextDocumentParser (a subclass of
+  // HTMLDocumentParser) to force us into the PLAINTEXT state w/o using a
+  // <plaintext> tag. The TextDocumentParser uses a <pre> tag for historical /
+  // compatibility reasons.
   m_tokenizer->setState(HTMLTokenizer::PLAINTEXTState);
 }
 
@@ -269,8 +271,9 @@ void BackgroundHTMLParser::pumpTokenizer() {
       simulatedToken =
           m_treeBuilderSimulator.simulate(token, m_tokenizer.get());
 
-      // Break chunks before a script tag is inserted and flag the chunk as starting a script
-      // so the main parser can decide if it should yield before processing the chunk.
+      // Break chunks before a script tag is inserted and flag the chunk as
+      // starting a script so the main parser can decide if it should yield
+      // before processing the chunk.
       if (simulatedToken == HTMLTreeBuilderSimulator::ScriptStart) {
         shouldNotifyMainThread |= queueChunkForMainThread();
         m_startingScript = true;
@@ -290,7 +293,8 @@ void BackgroundHTMLParser::pumpTokenizer() {
     if (simulatedToken == HTMLTreeBuilderSimulator::ScriptEnd ||
         m_pendingTokens->size() >= m_pendingTokenLimit) {
       shouldNotifyMainThread |= queueChunkForMainThread();
-      // If we're far ahead of the main thread, yield for a bit to avoid consuming too much memory.
+      // If we're far ahead of the main thread, yield for a bit to avoid
+      // consuming too much memory.
       if (m_input.totalCheckpointTokenCount() > m_outstandingTokenLimit)
         break;
     }
@@ -301,10 +305,10 @@ void BackgroundHTMLParser::pumpTokenizer() {
       shouldNotifyMainThread = false;
     }
   }
-  // Wait to notify the main thread about the chunks until we're at the
-  // limit. This lets the background parser generate lots of valuable
-  // preloads before anything expensive (extensions, scripts) take up time
-  // on the main thread. A busy main thread can cause preload delays.
+  // Wait to notify the main thread about the chunks until we're at the limit.
+  // This lets the background parser generate lots of valuable preloads before
+  // anything expensive (extensions, scripts) take up time on the main thread. A
+  // busy main thread can cause preload delays.
   if (shouldNotifyMainThread) {
     runOnMainThread(&HTMLDocumentParser::notifyPendingTokenizedChunks,
                     m_parser);
