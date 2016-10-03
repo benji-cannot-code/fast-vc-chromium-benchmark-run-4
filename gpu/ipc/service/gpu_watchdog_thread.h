ebcc38bb1506745f0b79e69b6a20027ea68f956a
ericrk <ericrk@chromium.org>
2016-10-03 14:52:13 -0700

    Throttle GpuWatchdogThread task posting
    The GpuWatchdogThread currently allows multiple tasks to disarm the
    watchdog to be posted in a row, only stopping posting when the watchdog
    is actually disarmed. This is fairly wasteful, as all but the first
    task will just early out. This change adjusts the logic so that we only
    post one disarm task, at a time, blocking future disarm tasks until
    we have successfully disarmed / rearmed the watchdog.
    
    This change also removes the current "volatile bool", and replaces this with a
    base::subtle::Atomic32, which makes the expectations regarding synchronization
    more explicit.
    
    BUG=612219
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2389633003
    Cr-Commit-Position: refs/heads/master@{#422549}

--- gpu/ipc/service/gpu_watchdog_thread.h
+++ gpu/ipc/service/gpu_watchdog_thread.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef GPU_IPC_SERVICE_GPU_WATCHDOG_THREAD_H_
 #define GPU_IPC_SERVICE_GPU_WATCHDOG_THREAD_H_
 
+#include "base/atomicops.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
@@ -38,10 +39,7 @@ class GPU_EXPORT GpuWatchdogThread
  public:
   static scoped_refptr<GpuWatchdogThread> Create();
 
-  // Accessible on watched thread but only modified by watchdog thread.
-  bool armed() const { return armed_; }
   void PostAcknowledge();
-
   void CheckArmed();
 
   // Must be called after a PowerMonitor has been created. Can be called from
@@ -98,8 +96,13 @@ class GPU_EXPORT GpuWatchdogThread
 
   base::MessageLoop* watched_message_loop_;
   base::TimeDelta timeout_;
-  volatile bool armed_;
+  bool armed_;
   GpuWatchdogTaskObserver task_observer_;
+
+  // |awaiting_acknowledge_| is only ever read on the watched thread, but may
+  // be modified on either the watched or watchdog thread. Reads/writes should
+  // be careful to ensure that appropriate synchronization is used.
+  base::subtle::Atomic32 awaiting_acknowledge_;
 
   // True if the watchdog should wait for a certain amount of CPU to be used
   // before killing the process.
