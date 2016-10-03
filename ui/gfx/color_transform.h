bef7e35b480e2a6293a270a88b617ebf390500a1
gurrrik <gurrrik@yandex-team.ru>
2016-10-03 08:46:55 -0700

    Add virtual destructor for ColorTransform
    This CL fixes a leak in ColorTransform::NewColorTransform.
    Without virtual destructor in ColorTransform, `qcms_profile_release`
    won't be called for `from_profile` and `to_profile`.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2387023002
    Cr-Commit-Position: refs/heads/master@{#422430}

--- ui/gfx/color_transform.h
+++ ui/gfx/color_transform.h
@@ -25,6 +25,8 @@ class GFX_EXPORT ColorTransform {
   // Channel order is XYZ, RGB or YUV.
   typedef Point3F TriStim;
 
+  virtual ~ColorTransform() {}
+
   // Perform transformation of colors, |colors| is both input and output.
   virtual void transform(TriStim* colors, size_t num) = 0;
 
