03f6dd43d3a688ea19c94106a0ff68cc6a7a18da
erg <erg@chromium.org>
2016-10-03 15:23:08 -0700

    mus: Use TooltipManagerAura in NativeWidgetMus.
    mash now shows tooltips on hover over items.
    
    BUG=599558
    
    Review-Url: https://codereview.chromium.org/2379073003
    Cr-Commit-Position: refs/heads/master@{#422562}

--- ui/views/widget/tooltip_manager_aura.h
+++ ui/views/widget/tooltip_manager_aura.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/strings/string16.h"
 #include "ui/gfx/geometry/point.h"
+#include "ui/views/views_export.h"
 #include "ui/views/widget/tooltip_manager.h"
 
 namespace aura {
@@ -25,7 +26,7 @@ namespace views {
 class Widget;
 
 // TooltipManager implementation for Aura.
-class TooltipManagerAura : public TooltipManager {
+class VIEWS_EXPORT TooltipManagerAura : public TooltipManager {
  public:
   explicit TooltipManagerAura(Widget* widget);
   ~TooltipManagerAura() override;
