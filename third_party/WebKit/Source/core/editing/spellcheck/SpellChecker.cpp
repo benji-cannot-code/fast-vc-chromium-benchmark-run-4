a98b95805a95a6ee1832ae39ea5498b3c0cacc43
xiaochengh <xiaochengh@chromium.org>
2016-10-03 02:04:08 -0700

    Reland of Prune CreateVisibleSelectionDeprecated from SpellChecker (patchset #1 id:1 of https://codereview.chromium.org/2383733002/ )
    Reason for revert:
    The DCHECK is removed, so this patch can be relanded.
    
    Original issue's description:
    > Revert of Prune CreateVisibleSelectionDeprecated from SpellChecker (patchset #1 id:1 of https://codereview.chromium.org/2381233003/ )
    >
    > Reason for revert:
    > Causing a lot of editing layout tests to fail by hitting the DCHECK in createVisibleSelection.
    >
    > Original issue's description:
    > > Prune CreateVisibleSelectionDeprecated from SpellChecker
    > >
    > > BUG=651373
    > >
    > > Committed: https://crrev.com/7771b00740741a22c321c23340c40900f47f26d1
    > > Cr-Commit-Position: refs/heads/master@{#422055}
    >
    > TBR=yosin@chromium.org
    > # Skipping CQ checks because original CL landed less than 1 days ago.
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=651373
    >
    > Committed: https://crrev.com/008a854391318e289c35c43eeb9b78e1896c30f7
    > Cr-Commit-Position: refs/heads/master@{#422079}
    
    TBR=yosin@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=651373
    
    Review-Url: https://codereview.chromium.org/2390573002
    Cr-Commit-Position: refs/heads/master@{#422394}

--- third_party/WebKit/Source/core/editing/spellcheck/SpellChecker.cpp
+++ third_party/WebKit/Source/core/editing/spellcheck/SpellChecker.cpp
@@ -324,8 +324,7 @@ void SpellChecker::advanceToNextMisspelling(bool startBeforeSelection) {
     const EphemeralRange misspellingRange = calculateCharacterSubrange(
         EphemeralRange(spellingSearchStart, spellingSearchEnd),
         misspellingOffset, misspelledWord.length());
-    frame().selection().setSelection(
-        createVisibleSelectionDeprecated(misspellingRange));
+    frame().selection().setSelection(createVisibleSelection(misspellingRange));
     frame().selection().revealSelection();
     spellCheckerClient().updateSpellingUIWithMisspelledWord(misspelledWord);
     frame().document()->markers().addMarker(misspellingRange.startPosition(),
@@ -427,8 +426,8 @@ void SpellChecker::markMisspellingsAfterTypingCommand(
   if (cmd.commandTypeOfOpenCommand() ==
       TypingCommand::InsertParagraphSeparator) {
     VisiblePosition nextWord = nextWordPosition(start);
-    VisibleSelection words = createVisibleSelectionDeprecated(
-        wordStartOfPrevious, endOfWord(nextWord));
+    VisibleSelection words =
+        createVisibleSelection(wordStartOfPrevious, endOfWord(nextWord));
     markMisspellingsAfterLineBreak(words);
     return;
   }
@@ -452,9 +451,9 @@ void SpellChecker::markMisspellingsAfterTypingToWord(
     const VisiblePosition& wordStart) {
   TRACE_EVENT0("blink", "SpellChecker::markMisspellingsAfterTypingToWord");
 
-  VisibleSelection adjacentWords = createVisibleSelectionDeprecated(
-      startOfWord(wordStart, LeftWordIfOnBoundary),
-      endOfWord(wordStart, RightWordIfOnBoundary));
+  VisibleSelection adjacentWords =
+      createVisibleSelection(startOfWord(wordStart, LeftWordIfOnBoundary),
+                             endOfWord(wordStart, RightWordIfOnBoundary));
   markMisspellingsAndBadGrammar(adjacentWords);
 }
 
@@ -798,8 +797,8 @@ void SpellChecker::replaceMisspelledRange(const String& text) {
                               markers[0]->endOffset()));
   if (markerRange.isNull())
     return;
-  frame().selection().setSelection(
-      createVisibleSelectionDeprecated(markerRange), CharacterGranularity);
+  frame().selection().setSelection(createVisibleSelection(markerRange),
+                                   CharacterGranularity);
 
   // TODO(xiaochengh): The use of updateStyleAndLayoutIgnorePendingStylesheets
   // needs to be audited.  See http://crbug.com/590369 for more details.
@@ -867,9 +866,9 @@ void SpellChecker::respondToChangedSelection(
         frame().settings() && frame().settings()->caretBrowsingEnabled();
     if (newSelection.isContentEditable() || caretBrowsing) {
       const VisiblePosition newStart(newSelection.visibleStart());
-      newAdjacentWords = createVisibleSelectionDeprecated(
-          startOfWord(newStart, LeftWordIfOnBoundary),
-          endOfWord(newStart, RightWordIfOnBoundary));
+      newAdjacentWords =
+          createVisibleSelection(startOfWord(newStart, LeftWordIfOnBoundary),
+                                 endOfWord(newStart, RightWordIfOnBoundary));
     }
   }
 
@@ -926,9 +925,9 @@ void SpellChecker::spellCheckOldSelection(
   TRACE_EVENT0("blink", "SpellChecker::spellCheckOldSelection");
 
   VisiblePosition oldStart(oldSelection.visibleStart());
-  VisibleSelection oldAdjacentWords = createVisibleSelectionDeprecated(
-      startOfWord(oldStart, LeftWordIfOnBoundary),
-      endOfWord(oldStart, RightWordIfOnBoundary));
+  VisibleSelection oldAdjacentWords =
+      createVisibleSelection(startOfWord(oldStart, LeftWordIfOnBoundary),
+                             endOfWord(oldStart, RightWordIfOnBoundary));
   if (oldAdjacentWords == newAdjacentWords)
     return;
   markMisspellingsAndBadGrammar(oldAdjacentWords);
