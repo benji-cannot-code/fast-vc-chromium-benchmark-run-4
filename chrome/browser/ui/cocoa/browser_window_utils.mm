ba607deb2b47ba806f70b87d5cce29c79c70242d
rsesek <rsesek@chromium.org>
2016-10-04 15:41:40 -0700

    [Mac] Remove usage of deprecated Carbon SetFrontProcessWithOptions().
    Replace with -[NSRunningApplication activateWithOptions:].
    
    BUG=650845
    R=avi@chromium.org
    
    Review-Url: https://codereview.chromium.org/2386343004
    Cr-Commit-Position: refs/heads/master@{#422973}

--- chrome/browser/ui/cocoa/browser_window_utils.mm
+++ chrome/browser/ui/cocoa/browser_window_utils.mm
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "chrome/browser/ui/cocoa/browser_window_utils.h"
 
-#include <Carbon/Carbon.h>
-
 #include "base/logging.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/global_keyboard_shortcuts_mac.h"
@@ -150,9 +148,7 @@ const CGFloat kPatternHorizontalOffset = -5;
   // Per http://crbug.com/73779 and http://crbug.com/75223, we need this to
   // properly activate windows if Chrome is not the active application.
   [[controller window] makeKeyAndOrderFront:controller];
-  ProcessSerialNumber psn;
-  GetCurrentProcess(&psn);
-  SetFrontProcessWithOptions(&psn, kSetFrontProcessFrontWindowOnly);
+  [[NSRunningApplication currentApplication] activateWithOptions:0];
 }
 
 @end
