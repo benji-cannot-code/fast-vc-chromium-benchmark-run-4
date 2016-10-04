59b279676b4490640a3950e7fab03a0fa941dc44
rob.buis <rob.buis@samsung.com>
2016-10-03 19:06:38 -0700

    Make cols/rows/span not accept zero when set from idl
    Make cols/rows/span not accept zero when set from idl, but do
    similar to what happens when setting the attribute, i.e. set
    the default values instead [1].
    
    BUG=490511
    
    [1] https://html.spec.whatwg.org/multipage/forms.html#attr-textarea-cols
        https://html.spec.whatwg.org/multipage/forms.html#attr-textarea-rows
        https://html.spec.whatwg.org/multipage/tables.html#dom-colgroup-span
    
    Review-Url: https://codereview.chromium.org/2387033003
    Cr-Commit-Position: refs/heads/master@{#422669}

--- third_party/WebKit/Source/core/html/HTMLTableColElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLTableColElement.cpp
@@ -98,7 +98,7 @@ HTMLTableColElement::additionalPresentationAttributeStyle() {
 }
 
 void HTMLTableColElement::setSpan(unsigned n) {
-  setUnsignedIntegralAttribute(spanAttr, n);
+  setUnsignedIntegralAttribute(spanAttr, n ? n : 1);
 }
 
 const AtomicString& HTMLTableColElement::width() const {
