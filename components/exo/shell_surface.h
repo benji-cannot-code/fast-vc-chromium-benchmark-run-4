5d98aef0e9d3d910cd1cd4aef15ff49e7849ac06
erosky <erosky@chromium.org>
2016-10-03 18:10:16 -0700

    Draw underlay behind android apps using talkback
    When spoken-feedback is enabled, shell_surface's shadow_underlay will
    fill the screen for active android windows. When in this mode, the
    underlay will consume all events and play an earcon to indicate that
    the event was located outside the android window.
    
    BUG=649524
    TEST=unit-test and tested on device
    
    Review-Url: https://codereview.chromium.org/2361993003
    Cr-Commit-Position: refs/heads/master@{#422651}

--- components/exo/shell_surface.h
+++ components/exo/shell_surface.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "ash/common/system/accessibility_observer.h"
 #include "ash/common/wm/window_state_observer.h"
 #include "base/macros.h"
 #include "base/strings/string16.h"
@@ -43,6 +44,7 @@ class ShellSurface : public SurfaceDelegate,
                      public SurfaceObserver,
                      public views::WidgetDelegate,
                      public views::View,
+                     public ash::AccessibilityObserver,
                      public ash::wm::WindowStateObserver,
                      public aura::WindowObserver,
                      public WMHelper::ActivationObserver {
@@ -195,6 +197,10 @@ class ShellSurface : public SurfaceDelegate,
   // Overridden from views::View:
   gfx::Size GetPreferredSize() const override;
 
+  // Overridden from ash::AccessibilityObserver:
+  void OnAccessibilityModeChanged(
+      ash::AccessibilityNotificationVisibility notify) override;
+
   // Overridden from ash::wm::WindowStateObserver:
   void OnPreWindowStateTypeChange(ash::wm::WindowState* window_state,
                                   ash::wm::WindowStateType old_type) override;
@@ -219,9 +225,7 @@ class ShellSurface : public SurfaceDelegate,
   // Overridden from ui::AcceleratorTarget:
   bool AcceleratorPressed(const ui::Accelerator& accelerator) override;
 
-  const aura::Window* shadow_underlay_for_test() const {
-    return shadow_underlay_;
-  }
+  aura::Window* shadow_underlay() { return shadow_underlay_; }
 
  private:
   class ScopedConfigure;
@@ -292,6 +296,7 @@ class ShellSurface : public SurfaceDelegate,
   int pending_resize_component_ = HTCAPTION;
   aura::Window* shadow_overlay_ = nullptr;
   aura::Window* shadow_underlay_ = nullptr;
+  std::unique_ptr<ui::EventHandler> shadow_underlay_event_handler_;
   gfx::Rect shadow_content_bounds_;
   std::deque<Config> pending_configs_;
   std::unique_ptr<ash::WindowResizer> resizer_;
