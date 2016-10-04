66b50892a856743f02ed6e787b65641b1e5fd47f
vasilvv <vasilvv@chromium.org>
2016-10-04 09:15:51 -0700

    Add an interface to access individual queues on the switch
    Merge internal change: 134721332
    
    R=rch@chromium.org
    BUG=
    
    Review-Url: https://codereview.chromium.org/2391723003
    Cr-Commit-Position: refs/heads/master@{#422823}

--- net/quic/test_tools/simulator/switch.h
+++ net/quic/test_tools/simulator/switch.h
@@ -32,6 +32,10 @@ class Switch {
     return &ports_[port_number - 1];
   }
 
+  inline const Queue* port_queue(SwitchPortNumber port_number) {
+    return ports_[port_number - 1].queue();
+  }
+
  private:
   class Port : public Endpoint, public UnconstrainedPortInterface {
    public:
@@ -53,7 +57,8 @@ class Switch {
 
     void Act() override;
 
-    inline bool connected() { return connected_; }
+    inline bool connected() const { return connected_; }
+    inline const Queue* queue() const { return &queue_; }
 
    private:
     Switch* parent_;
