0f66864fc0835e10b3454111dfe5680ba2349bac
hiroshige <hiroshige@chromium.org>
2016-10-03 23:55:25 -0700

    Do not revalidate when ResourceLoaderOptions.synchronousPolicy is different
    This CL adds a check of synchronousPolicy to
    ResourceLoaderOptions::canReuseRequest() to avoid Resources to be reused
    between sync and async requests.
    This CL also removes a condition from
    ResourceFetcher::determineRevalidationPolicy() that is covered by the newly
    added check.
    
    CachingCorrectnessTest is updated so that it doesn't use synchronous
    RawResource requests.
    
    BUG=652172
    
    Review-Url: https://codereview.chromium.org/2391523002
    Cr-Commit-Position: refs/heads/master@{#422725}

--- third_party/WebKit/LayoutTests/http/tests/cache/resources/etag-200.php
+++ third_party/WebKit/LayoutTests/http/tests/cache/resources/etag-200.php
@@ -4,5 +4,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // revalidating requests to test failed revalidation.
 header('ETag: foo');
 header('Cache-control: max-age=0');
+echo rand();
 ?>
-foo
