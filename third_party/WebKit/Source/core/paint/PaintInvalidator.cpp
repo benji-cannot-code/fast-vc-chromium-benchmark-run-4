1e4fe3b3f0782d41c0e9eecaa8e075ba9061947d
wangxianzhu <wangxianzhu@chromium.org>
2016-10-03 21:58:55 -0700

    Use enclosingLayoutRect instead of enclosingIntRect in SVGLayoutSupport::transformPaintInvalidationRect()
    https://bugs.chromium.org/p/chromium/issues/detail?id=651521#c2:
    The purpose of the original enclosingIntRect() might be to avoid
    unwanted inward-snapping due to FloatRect -> LayoutRect conversion.
    
    Change it to enclosingLayoutRect. Also changed PaintInvalidator to
    use enclosingLayoutRect when converting from FloatRect to
    LayoutRect.
    
    BUG=651521
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2380403002
    Cr-Commit-Position: refs/heads/master@{#422710}

--- third_party/WebKit/Source/core/paint/PaintInvalidator.cpp
+++ third_party/WebKit/Source/core/paint/PaintInvalidator.cpp
@@ -21,6 +21,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+// TODO(wangxianzhu): Avoid using function when possible. For example, we can
+// avoid it by avoiding unnecessary conversions between LayoutRects and
+// FloatRects.
+static LayoutRect enclosingLayoutRectIfNotEmpty(const FloatRect& floatRect) {
+  return floatRect.isEmpty() ? LayoutRect(floatRect)
+                             : enclosingLayoutRect(floatRect);
+}
+
 static LayoutRect slowMapToVisualRectInAncestorSpace(
     const LayoutObject& object,
     const LayoutBoxModelObject& ancestor,
@@ -32,7 +40,7 @@ static LayoutRect slowMapToVisualRectInAncestorSpace(
     return result;
   }
 
-  LayoutRect result(rect);
+  LayoutRect result = enclosingLayoutRectIfNotEmpty(rect);
   if (object.isLayoutView())
     toLayoutView(object).mapToVisualRectInAncestorSpace(
         &ancestor, result, InputIsInFrameCoordinates, DefaultVisualRectFlags);
@@ -41,7 +49,9 @@ static LayoutRect slowMapToVisualRectInAncestorSpace(
   return result;
 }
 
-// TODO(wangxianzhu): Combine this into PaintInvalidator::mapLocalRectToPaintInvalidationBacking() when removing PaintInvalidationState.
+// TODO(wangxianzhu): Combine this into
+// PaintInvalidator::mapLocalRectToPaintInvalidationBacking() when removing
+// PaintInvalidationState.
 static LayoutRect mapLocalRectToPaintInvalidationBacking(
     GeometryMapper& geometryMapper,
     const LayoutObject& object,
@@ -71,7 +81,7 @@ static LayoutRect mapLocalRectToPaintInvalidationBacking(
   if (RuntimeEnabledFeatures::slimmingPaintV2Enabled()) {
     // In SPv2, visual rects are in the space of their local transform node.
     rect.moveBy(FloatPoint(context.treeBuilderContext.current.paintOffset));
-    return LayoutRect(rect);
+    return enclosingLayoutRectIfNotEmpty(rect);
   }
 
   LayoutRect result;
@@ -80,7 +90,7 @@ static LayoutRect mapLocalRectToPaintInvalidationBacking(
     result = slowMapToVisualRectInAncestorSpace(
         object, *context.paintInvalidationContainer, rect);
   } else if (object == context.paintInvalidationContainer) {
-    result = LayoutRect(rect);
+    result = enclosingLayoutRectIfNotEmpty(rect);
   } else {
     rect.moveBy(FloatPoint(context.treeBuilderContext.current.paintOffset));
 
@@ -95,9 +105,10 @@ static LayoutRect mapLocalRectToPaintInvalidationBacking(
         containerPaintProperties->contentsProperties();
 
     bool success = false;
-    result = LayoutRect(geometryMapper.mapToVisualRectInDestinationSpace(
-        rect, currentTreeState, containerContentsProperties.propertyTreeState,
-        success));
+    result = enclosingLayoutRectIfNotEmpty(
+        geometryMapper.mapToVisualRectInDestinationSpace(
+            rect, currentTreeState,
+            containerContentsProperties.propertyTreeState, success));
     DCHECK(success);
 
     // Convert the result to the container's contents space.
