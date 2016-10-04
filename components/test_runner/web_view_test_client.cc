5528645ea2a374c29dbd45d595d386733a0fcada
lukasza <lukasza@chromium.org>
2016-10-04 13:20:51 -0700

    Do not store TestRunner* in WebViewTestClient and/or WebFrameTestClient.
    Instead of storing borrowed pointer to TestRunner, WebFrameTestClient
    and WebViewTestClient can both obtain the same pointer via another (also
    borrowed) pointer they already have - one to WebViewTestProxyBase.
    
    Less fields to worry about (e.g. wrt lifetime management) is good.
    In the long-term we also should remove |delegate_| field from
    WebFrameTestClient (responsible for UaF in https://crbug.com/606594).
    
    BUG=595089
    
    Review-Url: https://codereview.chromium.org/2386683002
    Cr-Commit-Position: refs/heads/master@{#422901}

--- components/test_runner/web_view_test_client.cc
+++ components/test_runner/web_view_test_client.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/test_runner/event_sender.h"
 #include "components/test_runner/mock_web_speech_recognizer.h"
 #include "components/test_runner/test_common.h"
+#include "components/test_runner/test_interfaces.h"
 #include "components/test_runner/test_runner.h"
 #include "components/test_runner/test_runner_for_specific_view.h"
 #include "components/test_runner/web_test_delegate.h"
@@ -29,11 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace test_runner {
 
 WebViewTestClient::WebViewTestClient(
-    TestRunner* test_runner,
     WebViewTestProxyBase* web_view_test_proxy_base)
-    : test_runner_(test_runner),
-      web_view_test_proxy_base_(web_view_test_proxy_base) {
-  DCHECK(test_runner);
+    : web_view_test_proxy_base_(web_view_test_proxy_base) {
   DCHECK(web_view_test_proxy_base);
 }
 
@@ -44,7 +42,7 @@ void WebViewTestClient::startDragging(blink::WebLocalFrame* frame,
                                       blink::WebDragOperationsMask mask,
                                       const blink::WebImage& image,
                                       const blink::WebPoint& point) {
-  test_runner_->setDragImage(image);
+  test_runner()->setDragImage(image);
 
   // When running a test, we need to fake a drag drop operation otherwise
   // Windows waits for real mouse events to know when the drag is over.
@@ -55,7 +53,7 @@ void WebViewTestClient::startDragging(blink::WebLocalFrame* frame,
 // expected by the layout tests. See EditingDelegate.m in DumpRenderTree.
 
 void WebViewTestClient::didChangeContents() {
-  if (test_runner_->shouldDumpEditingCallbacks())
+  if (test_runner()->shouldDumpEditingCallbacks())
     delegate()->PrintMessage(
         "EDITING DELEGATE: webViewDidChange:WebViewDidChangeNotification\n");
 }
@@ -67,15 +65,15 @@ blink::WebView* WebViewTestClient::createView(
     const blink::WebString& frame_name,
     blink::WebNavigationPolicy policy,
     bool suppress_opener) {
-  if (test_runner_->shouldDumpNavigationPolicy()) {
+  if (test_runner()->shouldDumpNavigationPolicy()) {
     delegate()->PrintMessage("Default policy for createView for '" +
                              URLDescription(request.url()) + "' is '" +
                              WebNavigationPolicyToString(policy) + "'\n");
   }
 
-  if (!test_runner_->canOpenWindows())
+  if (!test_runner()->canOpenWindows())
     return nullptr;
-  if (test_runner_->shouldDumpCreateView())
+  if (test_runner()->shouldDumpCreateView())
     delegate()->PrintMessage(std::string("createView(") +
                              URLDescription(request.url()) + ")\n");
 
@@ -87,7 +85,7 @@ blink::WebView* WebViewTestClient::createView(
 }
 
 void WebViewTestClient::setStatusText(const blink::WebString& text) {
-  if (!test_runner_->shouldDumpStatusCallbacks())
+  if (!test_runner()->shouldDumpStatusCallbacks())
     return;
   delegate()->PrintMessage(
       std::string("UI DELEGATE STATUS CALLBACK: setStatusText:") +
@@ -110,7 +108,7 @@ void WebViewTestClient::showValidationMessage(
     blink::WebTextDirection main_message_hint,
     const blink::WebString& sub_message,
     blink::WebTextDirection sub_message_hint) {
-  if (test_runner_->is_web_platform_tests_mode())
+  if (test_runner()->is_web_platform_tests_mode())
     return;
 
   base::string16 wrapped_main_text = main_message;
@@ -139,11 +137,11 @@ void WebViewTestClient::showValidationMessage(
 }
 
 blink::WebSpeechRecognizer* WebViewTestClient::speechRecognizer() {
-  return test_runner_->getMockWebSpeechRecognizer();
+  return test_runner()->getMockWebSpeechRecognizer();
 }
 
 blink::WebString WebViewTestClient::acceptLanguages() {
-  return blink::WebString::fromUTF8(test_runner_->GetAcceptLanguages());
+  return blink::WebString::fromUTF8(test_runner()->GetAcceptLanguages());
 }
 
 WebTestDelegate* WebViewTestClient::delegate() {
@@ -151,7 +149,11 @@ WebTestDelegate* WebViewTestClient::delegate() {
 }
 
 void WebViewTestClient::didFocus() {
-  test_runner_->SetFocus(web_view_test_proxy_base_->web_view(), true);
+  test_runner()->SetFocus(web_view_test_proxy_base_->web_view(), true);
+}
+
+TestRunner* WebViewTestClient::test_runner() {
+  return web_view_test_proxy_base_->test_interfaces()->GetTestRunner();
 }
 
 }  // namespace test_runner
