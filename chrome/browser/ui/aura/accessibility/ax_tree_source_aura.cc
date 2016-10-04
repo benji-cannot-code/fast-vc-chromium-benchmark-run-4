039bf0988f3238c45a0652d104365f479e605b08
slan <slan@chromium.org>
2016-10-04 11:21:16 -0700

    [Chromecast] Correct some Cast dependencies in //chrome/browser/ui.
    A recent CL introduced some new GN dependencies that broke internal Cast
    variants:
      https://codereview.chromium.org/2354633002
    
    Correct the dependencies here. Also put stronger asserts in x11 targets.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2371583002
    Cr-Commit-Position: refs/heads/master@{#422860}

--- chrome/browser/ui/aura/accessibility/ax_tree_source_aura.cc
+++ chrome/browser/ui/aura/accessibility/ax_tree_source_aura.cc
@@ -16,7 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/accessibility/ax_aura_obj_cache.h"
 #include "ui/views/accessibility/ax_aura_obj_wrapper.h"
 #include "ui/views/accessibility/ax_view_obj_wrapper.h"
-#include "ui/views/controls/webview/webview.h"
+
+#if defined(TOOLKIT_VIEWS)
+#include "ui/views/controls/webview/webview.h"  // nogncheck
+#endif
 
 using views::AXAuraObjCache;
 using views::AXAuraObjWrapper;
--- chrome/browser/ui/browser_commands.cc
+++ chrome/browser/ui/browser_commands.cc
@@ -112,7 +112,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(ENABLE_MEDIA_ROUTER)
-#include "chrome/browser/media/router/media_router_dialog_controller.h"
+#include "chrome/browser/media/router/media_router_dialog_controller.h"  // nogncheck
 #endif
 
 namespace {
