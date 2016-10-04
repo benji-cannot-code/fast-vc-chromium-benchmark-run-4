2944ba43ccb7f26a032ddd9c5684bfe1588af33e
yuweih <yuweih@chromium.org>
2016-10-03 18:21:44 -0700

    [Remoting Android] Fix Deprecated API Warnings
    This CL fixes some compiler warnings about using deprecated APIs.
    
    Review-Url: https://codereview.chromium.org/2391633003
    Cr-Commit-Position: refs/heads/master@{#422654}

--- remoting/android/java/src/org/chromium/chromoting/base/OAuthTokenFetcher.java
+++ remoting/android/java/src/org/chromium/chromoting/base/OAuthTokenFetcher.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chromoting.base;
 
+import android.accounts.Account;
 import android.app.Activity;
 import android.content.Context;
 import android.os.AsyncTask;
@@ -101,10 +102,8 @@ public class OAuthTokenFetcher {
                         GoogleAuthUtil.clearToken(mContext, expiredToken);
                     }
 
-                    // This method is deprecated but its replacement is not yet available.
-                    // TODO(lambroslambrou): Fix this by replacing |mAccountName| with an instance
-                    // of android.accounts.Account.
-                    String token = GoogleAuthUtil.getToken(mContext, mAccountName, mTokenScope);
+                    Account account = new Account(mAccountName, GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE);
+                    String token = GoogleAuthUtil.getToken(mContext, account, mTokenScope);
                     handleTokenReceived(token);
                 } catch (IOException ioException) {
                     handleError(Error.NETWORK);
