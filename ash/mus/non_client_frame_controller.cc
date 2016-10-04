b5d259c280aa17afd13a97d24c35f9c4c74a2410
kylechar <kylechar@chromium.org>
2016-10-04 09:51:17 -0700

    Two small ash fixes.
    Remove an unused include and fix a return value. Small things I noticed
    during other cleanup work.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2391023002
    Cr-Commit-Position: refs/heads/master@{#422828}

--- ash/mus/non_client_frame_controller.cc
+++ ash/mus/non_client_frame_controller.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/ui/public/cpp/window_property.h"
 #include "services/ui/public/cpp/window_tree_client.h"
 #include "services/ui/public/interfaces/window_manager.mojom.h"
-#include "services/ui/public/interfaces/window_tree_host.mojom.h"
 #include "ui/aura/layout_manager.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_tree_host.h"
--- ash/mus/root_window_controller.cc
+++ ash/mus/root_window_controller.cc
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/ui/public/cpp/window.h"
 #include "services/ui/public/cpp/window_property.h"
 #include "services/ui/public/cpp/window_tree_client.h"
-#include "services/ui/public/cpp/window_tree_host_factory.h"
 #include "ui/display/display_list.h"
 #include "ui/display/screen_base.h"
 
