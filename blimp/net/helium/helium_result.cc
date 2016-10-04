37285e2c2a3721ee3d638c8179e72a7e54e7aed4
kmarshall <kmarshall@chromium.org>
2016-10-03 19:55:35 -0700

    Define canonical enum for "HeliumResult" status values.
    Add HeliumResult-to-string mapping for easier logging.
    Add unit tests for string conversion functions.
    
    R=lethalantidote@chromium.org,gcasto@chromium.org
    BUG=
    
    Review-Url: https://codereview.chromium.org/2385913002
    Cr-Commit-Position: refs/heads/master@{#422683}

---
+++ blimp/net/helium/helium_result.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "blimp/net/helium/helium_result.h"
+
+#include "base/logging.h"
+
+namespace blimp {
+
+const char* HeliumResultToString(HeliumResult result) {
+  switch (result) {
+    case HeliumResult::SUCCESS:
+      return "SUCCESS";
+      break;
+#define HELIUM_ERROR(label, value) \
+  case HeliumResult::ERR_##label:  \
+    return "ERR_" #label;
+      break;
+#include "blimp/net/helium/helium_errors.h"
+#undef HELIUM_ERROR
+  }
+
+  NOTREACHED();
+  return "";
+}
+
+}  // namespace blimp
