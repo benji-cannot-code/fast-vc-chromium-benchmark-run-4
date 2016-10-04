6e3c9fab82c574330efb25622083fe3de7141ea7
ellyjones <ellyjones@chromium.org>
2016-10-04 11:40:26 -0700

    cocoa browser: remove non-MD bookmark bar support
    This change removes all uses of IsModeMaterial() in c/b/ui/c/bookmarks and
    refactors the code a bunch as a result.
    
    BUG=649368
    
    Review-Url: https://codereview.chromium.org/2394523002
    Cr-Commit-Position: refs/heads/master@{#422867}

--- chrome/browser/ui/cocoa/bookmarks/bookmark_bar_folder_controller.h
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_bar_folder_controller.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Cocoa/Cocoa.h>
 
 #include "base/mac/scoped_nsobject.h"
+#import "chrome/browser/ui/cocoa/bookmarks/bookmark_bar_controller.h"
 #import "chrome/browser/ui/cocoa/bookmarks/bookmark_button.h"
 #import "ui/base/cocoa/tracking_area.h"
 
@@ -24,10 +25,10 @@ class Profile;
 namespace bookmarks {
 
 // The padding between the top of the folder menu and the topmost button.
-CGFloat BookmarkTopVerticalPadding();
+const CGFloat kBookmarkTopVerticalPadding = bookmarks::kBookmarkVerticalPadding;
 
 // The padding between the bottom of the folder menu and the bottommost button.
-CGFloat BookmarkBottomVerticalPadding();
+const CGFloat kBookmarkBottomVerticalPadding = 0.0;
 
 }  // bookmarks
 
