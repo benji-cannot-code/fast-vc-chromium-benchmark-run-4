175e284fd15d8169bc09beab31adf2a9a541476f
pkasting <pkasting@chromium.org>
2016-10-03 22:51:47 -0700

    Rewrap comments to 80 columns in Source/platform/graphics/filters/.
    This also makes some minor modifications (add trailing periods, eliminate
    unneeded comment, etc.).
    
    BUG=563793
    TEST=none
    
    Review-Url: https://codereview.chromium.org/2389703004
    Cr-Commit-Position: refs/heads/master@{#422718}

--- third_party/WebKit/Source/platform/graphics/filters/FEColorMatrix.h
+++ third_party/WebKit/Source/platform/graphics/filters/FEColorMatrix.h
@@ -63,8 +63,8 @@ class PLATFORM_EXPORT FEColorMatrix final : public FilterEffect {
 
   ColorMatrixType m_type;
 
-  // The m_values vector may not contain the right number of values. Always check
-  // before accessing contents.
+  // The m_values vector may not contain the right number of values. Always
+  // check before accessing contents.
   Vector<float> m_values;
 };
 
