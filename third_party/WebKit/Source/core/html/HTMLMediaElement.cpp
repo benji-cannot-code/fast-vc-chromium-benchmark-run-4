947e5af2c0bdc9fc29c025d6dd44bdeb188bd5c0
avayvod <avayvod@chromium.org>
2016-10-04 08:44:14 -0700

    [Blink, RemotePlayback] Reject the prompt() with OperationError if there's a pending promise for this element.
    BUG=647441
    TEST=avayvod.github.io/remote-playback/test.html + layout test
    
    Review-Url: https://codereview.chromium.org/2347763002
    Cr-Commit-Position: refs/heads/master@{#422810}

--- third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
@@ -2195,13 +2195,13 @@ void HTMLMediaElement::pauseInternal() {
 }
 
 void HTMLMediaElement::requestRemotePlayback() {
-  DCHECK(m_remoteRoutesAvailable);
-  webMediaPlayer()->requestRemotePlayback();
+  if (webMediaPlayer())
+    webMediaPlayer()->requestRemotePlayback();
 }
 
 void HTMLMediaElement::requestRemotePlaybackControl() {
-  DCHECK(m_remoteRoutesAvailable);
-  webMediaPlayer()->requestRemotePlaybackControl();
+  if (webMediaPlayer())
+    webMediaPlayer()->requestRemotePlaybackControl();
 }
 
 void HTMLMediaElement::closeMediaSource() {
