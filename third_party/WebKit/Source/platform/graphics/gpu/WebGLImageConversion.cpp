18c5062eb96fedbec37fc27e73a97af75e503285
Prashant.Patil <Prashant.Patil@imgtec.com>
2016-10-05 00:21:21 -0700

    Add MSA (MIPS SIMD Arch) optimized color conversion functions
    We add following MSA optimized WebGL color conversion functions -
    - packOneRowOfRGBA8ToUnsignedShort4444MSA (Gain 8x)
    - packOneRowOfRGBA8LittleToR8MSA (Gain 3.5x)
    - packOneRowOfRGBA8LittleToRA8MSA (Gain 4x)
    
    R=kbr@chromium.org
    BUG=642653
    
    Review-Url: https://codereview.chromium.org/2392443003
    Cr-Commit-Position: refs/heads/master@{#423098}

--- third_party/WebKit/Source/platform/graphics/gpu/WebGLImageConversion.cpp
+++ third_party/WebKit/Source/platform/graphics/gpu/WebGLImageConversion.cpp
@@ -715,6 +715,9 @@ void pack<WebGLImageConversion::DataFormatR8,
 #if CPU(X86) || CPU(X86_64)
   SIMD::packOneRowOfRGBA8LittleToR8(source, destination, pixelsPerRow);
 #endif
+#if HAVE(MIPS_MSA_INTRINSICS)
+  SIMD::packOneRowOfRGBA8LittleToR8MSA(source, destination, pixelsPerRow);
+#endif
   for (unsigned i = 0; i < pixelsPerRow; ++i) {
     float scaleFactor = source[3] ? 255.0f / source[3] : 1.0f;
     uint8_t sourceR =
@@ -768,6 +771,9 @@ void pack<WebGLImageConversion::DataFormatRA8,
                    unsigned pixelsPerRow) {
 #if CPU(X86) || CPU(X86_64)
   SIMD::packOneRowOfRGBA8LittleToRA8(source, destination, pixelsPerRow);
+#endif
+#if HAVE(MIPS_MSA_INTRINSICS)
+  SIMD::packOneRowOfRGBA8LittleToRA8MSA(source, destination, pixelsPerRow);
 #endif
   for (unsigned i = 0; i < pixelsPerRow; ++i) {
     float scaleFactor = source[3] ? 255.0f / source[3] : 1.0f;
@@ -907,6 +913,10 @@ void pack<WebGLImageConversion::DataFormatRGBA4444,
                     unsigned pixelsPerRow) {
 #if HAVE(ARM_NEON_INTRINSICS)
   SIMD::packOneRowOfRGBA8ToUnsignedShort4444(source, destination, pixelsPerRow);
+#endif
+#if HAVE(MIPS_MSA_INTRINSICS)
+  SIMD::packOneRowOfRGBA8ToUnsignedShort4444MSA(source, destination,
+                                                pixelsPerRow);
 #endif
   for (unsigned i = 0; i < pixelsPerRow; ++i) {
     *destination = (((source[0] & 0xF0) << 8) | ((source[1] & 0xF0) << 4) |
