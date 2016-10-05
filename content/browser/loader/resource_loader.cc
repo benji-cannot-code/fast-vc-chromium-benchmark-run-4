79d69ef4228fa21efddc7cc261e02f332d2360ff
estark <estark@chromium.org>
2016-10-05 12:06:06 -0700

    Collapse SSLPolicy/SSLPolicyBackend into SSLManager
    SSLManager, SSLPolicy, and SSLPolicyBackend all pretty much ferry events
    from one to the other, without any clear division of responsibility
    among them. This CL collapses all the functionality into SSLManager for
    clearer code.
    
    BUG=558491
    
    Review-Url: https://codereview.chromium.org/2395663002
    Cr-Commit-Position: refs/heads/master@{#423240}

--- content/browser/loader/resource_loader.cc
+++ content/browser/loader/resource_loader.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/service_worker/service_worker_response_info.h"
 #include "content/browser/ssl/ssl_client_auth_handler.h"
 #include "content/browser/ssl/ssl_manager.h"
-#include "content/browser/ssl/ssl_policy.h"
 #include "content/public/browser/resource_dispatcher_host_login_delegate.h"
 #include "content/public/common/content_client.h"
 #include "content/public/common/content_switches.h"
