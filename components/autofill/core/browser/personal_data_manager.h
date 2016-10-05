bd3df56ade9be809f14b3fd81dd2fb9508e6484d
sebsg <sebsg@chromium.org>
2016-10-05 08:04:14 -0700

    Normalizes the state and city of the selected address before sending the information to the merchant.
    BUG=641051
    
    Review-Url: https://codereview.chromium.org/2338283003
    Cr-Commit-Position: refs/heads/master@{#423162}

--- components/autofill/core/browser/personal_data_manager.h
+++ components/autofill/core/browser/personal_data_manager.h
@@ -28,6 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/prefs/pref_member.h"
 #include "components/webdata/common/web_data_service_consumer.h"
+#if defined(OS_ANDROID)
+#include "net/url_request/url_request_context_getter.h"
+#endif
 
 class AccountTrackerService;
 class Browser;
@@ -264,6 +267,20 @@ class PersonalDataManager : public KeyedService,
     NotifyPersonalDataChanged();
   }
 
+#if defined(OS_ANDROID)
+  // Sets the URL request context getter to be used when normalizing addresses
+  // with libaddressinput's address validator.
+  void SetURLRequestContextGetter(
+      net::URLRequestContextGetter* context_getter) {
+    context_getter_ = context_getter;
+  }
+
+  // Returns the class used to fetch the address validation rules.
+  net::URLRequestContextGetter* GetURLRequestContextGetter() const {
+    return context_getter_.get();
+  }
+#endif
+
  protected:
   // Only PersonalDataManagerFactory and certain tests can create instances of
   // PersonalDataManager.
@@ -496,6 +513,12 @@ class PersonalDataManager : public KeyedService,
   // Set to true if autofill profile deduplication is enabled and needs to be
   // performed on the next data refresh.
   bool is_autofill_profile_dedupe_pending_ = false;
+
+#if defined(OS_ANDROID)
+  // The context for the request to be used to fetch libaddressinput's address
+  // validation rules.
+  scoped_refptr<net::URLRequestContextGetter> context_getter_;
+#endif
 
   DISALLOW_COPY_AND_ASSIGN(PersonalDataManager);
 };
