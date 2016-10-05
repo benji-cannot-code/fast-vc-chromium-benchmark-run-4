6a7151598fc76476e91e439af6efca4be6589b99
kundaji <kundaji@chromium.org>
2016-10-05 15:42:41 -0700

    Core classes to compute and store data use from high level entities.
    Each instance of DataUseRecorder tracks data use from a single
    high level entity, such as a page load. DataUseAscriber maintains
    a one:many mapping from DataUseRecorder to URLRequest and
    propagates network data use events to the appropriate DataUseRecorder.
    The actual data used is stored in instances of DataUse.
    
    BUG=645188
    
    Review-Url: https://codereview.chromium.org/2392123003
    Cr-Commit-Position: refs/heads/master@{#423321}

--- chrome/browser/data_use_measurement/chrome_data_use_ascriber.cc
+++ chrome/browser/data_use_measurement/chrome_data_use_ascriber.cc
@@ -20,7 +20,7 @@ ChromeDataUseAscriber::~ChromeDataUseAscriber() {
 }
 
 DataUseRecorder* ChromeDataUseAscriber::GetDataUseRecorder(
-    const net::URLRequest* request) {
+    net::URLRequest* request) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
   return nullptr;
 }
