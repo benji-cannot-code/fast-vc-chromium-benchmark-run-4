f9226d828460754aed22355e539edce14af2afae
hlopko <hlopko@chromium.org>
2016-10-05 01:03:04 -0700

    Re-Add tests for trace wrappers
    This cl adds very basic tests for general trace wrappers features. It does not enable trace wrappers feature for tests.
    
    LOG=no
    BUG=468240
    
    Review-Url: https://codereview.chromium.org/2392963002
    Cr-Commit-Position: refs/heads/master@{#423105}

--- third_party/WebKit/Source/platform/heap/ThreadState.cpp
+++ third_party/WebKit/Source/platform/heap/ThreadState.cpp
@@ -1057,7 +1057,8 @@ void ThreadState::makeConsistentForMutator() {
 }
 
 void ThreadState::preGC() {
-  if (RuntimeEnabledFeatures::traceWrappablesEnabled() && m_performCleanup)
+  if (RuntimeEnabledFeatures::traceWrappablesEnabled() && m_isolate &&
+      m_performCleanup)
     m_performCleanup(m_isolate);
 
   ASSERT(!isInGC());
