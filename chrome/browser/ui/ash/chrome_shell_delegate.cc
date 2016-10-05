9b16208b3f84de0fa29d199ba7199152cc99b5f3
erosky <erosky@chromium.org>
2016-10-05 15:24:09 -0700

    Stop chromevox audio and clear focus highlight on switch to arc app
    BUG=653201
    TEST=manual
    
    Review-Url: https://codereview.chromium.org/2396913002
    Cr-Commit-Position: refs/heads/master@{#423315}

--- chrome/browser/ui/ash/chrome_shell_delegate.cc
+++ chrome/browser/ui/ash/chrome_shell_delegate.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/policy/display_rotation_default_handler.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/chromeos/system/input_device_settings.h"
+#include "chrome/browser/chromeos/ui/accessibility_focus_ring_controller.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
@@ -258,6 +259,12 @@ class AccessibilityDelegateImpl : public ash::AccessibilityDelegate {
 
   void SilenceSpokenFeedback() const override {
     TtsController::GetInstance()->Stop();
+  }
+
+  void ClearFocusHighlight() const override {
+    chromeos::AccessibilityFocusRingController::GetInstance()->SetFocusRing(
+        std::vector<gfx::Rect>(),
+        chromeos::AccessibilityFocusRingController::PERSIST_FOCUS_RING);
   }
 
   void SaveScreenMagnifierScale(double scale) override {
