90c54f59522847ec1cbc55996dac0c3db06cb563
khmel <khmel@chromium.org>
2016-10-05 09:20:01 -0700

    arc: Don't show default Arc apps for managed users with Arc-disabled.
    TEST=Manually on device. @chromium account has no PlayStore icon
         visible.
    TEST=extended browser_tests
    BUG=b/31577285
    BUG=648796
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2357763002
    Cr-Commit-Position: refs/heads/master@{#423178}

--- chrome/browser/ui/app_list/arc/arc_default_app_list.h
+++ chrome/browser/ui/app_list/arc/arc_default_app_list.h
@@ -70,6 +70,11 @@ class ArcDefaultAppList {
 
   const AppInfoMap& app_map() const { return apps_; }
 
+  // Marks default apps as hidden for user, for example in case Arc is managed
+  // and disabled.
+  void set_hidden(bool hidden) { hidden_ = hidden; }
+  bool is_hidden() const { return hidden_; }
+
  private:
   // Defines mapping package name to uninstalled state.
   using PacakageMap = std::map<std::string, bool>;
@@ -80,6 +85,7 @@ class ArcDefaultAppList {
   // Unowned pointer.
   Delegate* const delegate_;
   content::BrowserContext* const context_;
+  bool hidden_ = true;
 
   AppInfoMap apps_;
   PacakageMap packages_;
