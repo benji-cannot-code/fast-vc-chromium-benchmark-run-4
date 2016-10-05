d73e3669310d4f3624db0c3942af616b72a0bc4d
alexilin <alexilin@chromium.org>
2016-10-05 08:50:15 -0700

    predictors: Refactor resource_prefetch_predictor_tables.
    Replace PrefetchData struct by proto message. It changes sqlite tables
    structure from 'single resource - single row' to 'single url/host -
    single row'. Also get rid of {Host,Url}MetadataTable because we don't need it
    anymore.
    It allows to get more uniform database structure and reuse the same functions
    for manipulating data.
    
    + 2 little bug fixes:
    - Sort redirect data while reading from the database.
    - Check the length of the key in redirect table before adding an entry.
    
    BUG=631966
    
    Review-Url: https://codereview.chromium.org/2388783002
    Cr-Commit-Position: refs/heads/master@{#423168}

--- chrome/browser/predictors/resource_prefetch_predictor.h
+++ chrome/browser/predictors/resource_prefetch_predictor.h
@@ -205,7 +205,6 @@ class ResourcePrefetchPredictor
     DISALLOW_COPY_AND_ASSIGN(GetUrlVisitCountTask);
   };
 
-  typedef ResourcePrefetchPredictorTables::PrefetchData PrefetchData;
   typedef ResourcePrefetchPredictorTables::PrefetchDataMap PrefetchDataMap;
   typedef ResourcePrefetchPredictorTables::RedirectDataMap RedirectDataMap;
 
