78de72d96a3f4c7016fea6b0a1d668e53d3ccd80
dtapuska <dtapuska@chromium.org>
2016-10-05 13:50:37 -0700

    Add ability to send crash keys from the browser to the renderer process.
    We will use this ability to add some additional crash keys to
    identify the causes of hung renderers.
    
    BUG=615090
    
    Review-Url: https://codereview.chromium.org/2307993002
    Cr-Commit-Position: refs/heads/master@{#423284}

--- chrome/browser/hang_monitor/hang_crash_dump_win.h
+++ chrome/browser/hang_monitor/hang_crash_dump_win.h
@@ -8,9 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <windows.h>
 
+#include "base/strings/string_split.h"
+
 // Causes the given child process to generate a crash dump and terminates the
-// process.
-void CrashDumpAndTerminateHungChildProcess(HANDLE hprocess);
+// process. |additional_serialized_crash_keys| are additional key/value string
+// pairs that will be logged in the child crash report. The crash keys provided
+// must be preregistered before calling this method.
+void CrashDumpAndTerminateHungChildProcess(
+    HANDLE hprocess,
+    const base::StringPairs& additional_crash_keys);
 
 // TODO(yzshen): Remove when enough information is collected and the hang rate
 // of pepper/renderer processes is reduced.
