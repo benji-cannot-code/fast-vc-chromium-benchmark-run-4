2e64e856405f8c01f2b9a790574065bf9f877087
soorya.r <soorya.r@samsung.com>
2016-10-04 23:11:46 -0700

    Fix compilation error in Vulkan Renderer header file
    The commit https://crrev.com/db49547c7bbadfaa155607339856944a7eb5fa92 deletes the
    header file 'cc/output/renderer.h'.
    
    Removed the header file 'cc/output/renderer.h' present in source file.
    
    BUG=582564
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2374143006
    Cr-Commit-Position: refs/heads/master@{#423086}

--- cc/output/vulkan_renderer.h
+++ cc/output/vulkan_renderer.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "cc/base/cc_export.h"
 #include "cc/output/direct_renderer.h"
-#include "cc/output/renderer.h"
 
 namespace cc {
 
