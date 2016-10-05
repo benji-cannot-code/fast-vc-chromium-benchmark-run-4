7f3bbc05019ea66c8235f66362dd369eb6f32190
thakis <thakis@chromium.org>
2016-10-05 07:08:28 -0700

    reflow comments in modules/[fetch,indexeddb]
    BUG=563793
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2388423003
    Cr-Commit-Position: refs/heads/master@{#423151}

--- third_party/WebKit/Source/modules/fetch/FetchManager.cpp
+++ third_party/WebKit/Source/modules/fetch/FetchManager.cpp
@@ -700,7 +700,8 @@ void FetchManager::Loader::performHTTPFetch(bool corsFlag,
       SchemeRegistry::shouldTreatURLSchemeAsSupportingFetchAPI(
           m_request->url().protocol()) ||
       (m_request->url().protocolIs("blob") && !corsFlag && !corsPreflightFlag));
-  // CORS preflight fetch procedure is implemented inside DocumentThreadableLoader.
+  // CORS preflight fetch procedure is implemented inside
+  // DocumentThreadableLoader.
 
   // "1. Let |HTTPRequest| be a copy of |request|, except that |HTTPRequest|'s
   //  body is a tee of |request|'s body."
@@ -833,7 +834,8 @@ void FetchManager::Loader::performDataFetch() {
   request.setUseStreamOnResponse(true);
   request.setHTTPMethod(m_request->method());
   request.setFetchRedirectMode(WebURLRequest::FetchRedirectModeError);
-  // We intentionally skip 'setExternalRequestStateFromRequestorAddressSpace', as 'data:' can never be external.
+  // We intentionally skip 'setExternalRequestStateFromRequestorAddressSpace',
+  // as 'data:' can never be external.
 
   ResourceLoaderOptions resourceLoaderOptions;
   resourceLoaderOptions.dataBufferingPolicy = DoNotBufferData;
--- third_party/WebKit/Source/modules/filesystem/DirectoryReader.cpp
+++ third_party/WebKit/Source/modules/filesystem/DirectoryReader.cpp
@@ -53,7 +53,8 @@ class DirectoryReader::EntriesCallbackHelper final : public EntriesCallback {
   }
 
  private:
-  // FIXME: This Member keeps the reader alive until all of the readDirectory results are received. crbug.com/350285
+  // FIXME: This Member keeps the reader alive until all of the readDirectory
+  // results are received. crbug.com/350285
   Member<DirectoryReader> m_reader;
 };
 
@@ -94,7 +95,8 @@ void DirectoryReader::readEntries(EntriesCallback* entriesCallback,
   }
 
   if (m_entriesCallback) {
-    // Non-null m_entriesCallback means multiple readEntries() calls are made concurrently. We don't allow doing it.
+    // Non-null m_entriesCallback means multiple readEntries() calls are made
+    // concurrently. We don't allow doing it.
     filesystem()->reportError(ScriptErrorCallback::wrap(errorCallback),
                               FileError::kInvalidStateErr);
     return;
