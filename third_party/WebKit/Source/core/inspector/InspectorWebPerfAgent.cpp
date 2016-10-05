00c9503e714575cf9d6130e0974a8ae25cf4394b
panicker <panicker@chromium.org>
2016-10-04 22:44:45 -0700

    Add use counter for Long Task Observer
    BUG=635596
    
    Review-Url: https://codereview.chromium.org/2387963002
    Cr-Commit-Position: refs/heads/master@{#423082}

--- third_party/WebKit/Source/core/inspector/InspectorWebPerfAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorWebPerfAgent.cpp
@@ -8,9 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/InstrumentingAgents.h"
 #include "core/dom/Document.h"
 #include "core/dom/ExecutionContext.h"
+#include "core/frame/DOMWindow.h"
 #include "core/frame/Frame.h"
 #include "core/frame/LocalFrame.h"
 #include "core/frame/Location.h"
+#include "core/frame/UseCounter.h"
 #include "core/inspector/InspectedFrames.h"
 #include "core/timing/DOMWindowPerformance.h"
 #include "core/timing/Performance.h"
@@ -41,6 +43,8 @@ InspectorWebPerfAgent::~InspectorWebPerfAgent() {
 }
 
 void InspectorWebPerfAgent::enable() {
+  // Update usage count.
+  UseCounter::count(m_localFrame, UseCounter::LongTaskObserver);
   Platform::current()->currentThread()->addTaskTimeObserver(this);
   Platform::current()->currentThread()->addTaskObserver(this);
   m_localFrame->instrumentingAgents()->addInspectorWebPerfAgent(this);
