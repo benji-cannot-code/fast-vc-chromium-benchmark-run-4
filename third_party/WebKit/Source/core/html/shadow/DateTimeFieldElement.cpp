ac35d53b196c4ff5618db9ce5fee771d0831a013
thakis <thakis@chromium.org>
2016-10-05 16:26:45 -0700

    reflow comments in core/html/shadow
    BUG=563793
    
    Review-Url: https://codereview.chromium.org/2393133004
    Cr-Commit-Position: refs/heads/master@{#423335}

--- third_party/WebKit/Source/core/html/shadow/DateTimeFieldElement.cpp
+++ third_party/WebKit/Source/core/html/shadow/DateTimeFieldElement.cpp
@@ -90,8 +90,8 @@ void DateTimeFieldElement::defaultKeyboardEventHandler(
   if (key == "ArrowLeft") {
     if (!m_fieldOwner)
       return;
-    // FIXME: We'd like to use FocusController::advanceFocus(FocusDirectionLeft, ...)
-    // but it doesn't work for shadow nodes. webkit.org/b/104650
+    // FIXME: We'd like to use FocusController::advanceFocus(FocusDirectionLeft,
+    // ...) but it doesn't work for shadow nodes. webkit.org/b/104650
     if (!localeForOwner().isRTL() && m_fieldOwner->focusOnPreviousField(*this))
       keyboardEvent->setDefaultHandled();
     return;
@@ -100,7 +100,8 @@ void DateTimeFieldElement::defaultKeyboardEventHandler(
   if (key == "ArrowRight") {
     if (!m_fieldOwner)
       return;
-    // FIXME: We'd like to use FocusController::advanceFocus(FocusDirectionRight, ...)
+    // FIXME: We'd like to use
+    // FocusController::advanceFocus(FocusDirectionRight, ...)
     // but it doesn't work for shadow nodes. webkit.org/b/104650
     if (!localeForOwner().isRTL() && m_fieldOwner->focusOnNextField(*this))
       keyboardEvent->setDefaultHandled();
