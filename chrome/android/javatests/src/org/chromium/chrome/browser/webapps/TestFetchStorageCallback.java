8ce9361b79bac226b5c2dc9ebfa0e9cedbe0385e
dominickn <dominickn@chromium.org>
2016-10-04 21:52:31 -0700

    [Reland] Refactor WebappRegistry into a singleton instance.
    This CL refactors WebappRegistry and WebappDataStorage to make most of
    the methods synchronous. WebappRegistry is now a singleton instance that
    is instantiated at browser startup. This allows all SharedPreferences files to
    be pre-warmed before the class is used; new web apps open new
    SharedPreferences on a background thread when registered, after which the
    preferences are cached automatically.
    
    Most static methods on WebappRegistry and WebappDataStorage have been
    converted to instance methods or removed. This makes the code much
    cleaner and more efficient; each static method had to independently open
    their SharedPreferences, which minimally performs a stat() on the
    underlying XML file to see if it has changed. Now the singleton
    WebappRegistry caches all WebappDataStorage objects on startup and
    whenever new ones are added. This reduces disk IO overhead.
    
    This CL allows all calls to SharedPreferences.Editor.apply() in
    WebappRegistry and WebappDataStorage to occur on the main thread,
    mostly removing the need for unwieldy callback interfaces and bare
    pointer passing across the JNI.
    
    BUG=633791
    
    Committed: https://crrev.com/78cf6596040662c0a73c0e4038bc84fb2b3c470a
    Review-Url: https://codereview.chromium.org/2351113005
    Cr-Original-Commit-Position: refs/heads/master@{#422703}
    Cr-Commit-Position: refs/heads/master@{#423077}

---
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webapps/TestFetchStorageCallback.java
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.webapps;
+
+import org.chromium.content.browser.test.util.CallbackHelper;
+
+/**
+ * CallbackHelper subclass which implements WebappRegistry.FetchWebappDataStorageCallback for tests.
+ */
+public class TestFetchStorageCallback
+        extends CallbackHelper implements WebappRegistry.FetchWebappDataStorageCallback {
+    protected WebappDataStorage mStorage;
+
+    @Override
+    public void onWebappDataStorageRetrieved(WebappDataStorage storage) {
+        mStorage = storage;
+        notifyCalled();
+    }
+
+    public WebappDataStorage getStorage() {
+        return mStorage;
+    }
+}
