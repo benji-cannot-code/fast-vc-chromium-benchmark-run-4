3613fcb6e69346e241cf1e669f162d667ef30624
rob.buis <rob.buis@samsung.com>
2016-10-05 09:16:41 -0700

    Fix setting properties on CSSFontFaceRule
    Fix setting properties on CSSFontFaceRule, before this patch
    we used the regular css style property parsing routines which
    are different from the @font-face specific descriptor parsing.
    So reintroduce a mode to the CSSParserMode enumeration for
    font-face rule and use it in order to properly parse
    @font-face descriptors.
    
    BUG=652362
    
    Review-Url: https://codereview.chromium.org/2390373002
    Cr-Commit-Position: refs/heads/master@{#423177}

--- third_party/WebKit/Source/core/css/parser/CSSParserImpl.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSParserImpl.cpp
@@ -50,6 +50,8 @@ bool CSSParserImpl::parseValue(MutableStylePropertySet* declaration,
   StyleRule::RuleType ruleType = StyleRule::Style;
   if (declaration->cssParserMode() == CSSViewportRuleMode)
     ruleType = StyleRule::Viewport;
+  else if (declaration->cssParserMode() == CSSFontFaceRuleMode)
+    ruleType = StyleRule::FontFace;
   CSSTokenizer::Scope scope(string);
   parser.consumeDeclarationValue(scope.tokenRange(), unresolvedProperty,
                                  important, ruleType);
@@ -634,7 +636,7 @@ StyleRuleFontFace* CSSParserImpl::consumeFontFaceRule(
 
   consumeDeclarationList(block, StyleRule::FontFace);
   return StyleRuleFontFace::create(
-      createStylePropertySet(m_parsedProperties, m_context.mode()));
+      createStylePropertySet(m_parsedProperties, CSSFontFaceRuleMode));
 }
 
 StyleRuleKeyframes* CSSParserImpl::consumeKeyframesRule(
