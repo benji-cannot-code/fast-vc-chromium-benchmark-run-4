bb1013436a025c708e76905132fafc671dfe8f4b
hush <hush@chromium.org>
2016-10-05 12:26:41 -0700

    Convert drag event position to root frame.
    The drag event positions are in viewport's coordinates when they are passed in
    blink::WebView. We then need to convert these coordinates into the root frame.
    In this way, the page scale factor is properly factored into the coordination
    conversion.
    
    BUG=651626, 652789
    
    Review-Url: https://codereview.chromium.org/2396693002
    Cr-Commit-Position: refs/heads/master@{#423250}

--- third_party/WebKit/public/web/WebView.h
+++ third_party/WebKit/public/web/WebView.h
@@ -371,7 +371,7 @@ class WebView : protected WebWidget {
                                                const WebSize& tapArea) = 0;
 
   // Notifies the WebView that a drag has terminated.
-  virtual void dragSourceEndedAt(const WebPoint& clientPoint,
+  virtual void dragSourceEndedAt(const WebPoint& pointInViewport,
                                  const WebPoint& screenPoint,
                                  WebDragOperation operation) = 0;
 
@@ -382,18 +382,18 @@ class WebView : protected WebWidget {
   // something on the WebView.
   virtual WebDragOperation dragTargetDragEnter(
       const WebDragData&,
-      const WebPoint& clientPoint,
+      const WebPoint& pointInViewport,
       const WebPoint& screenPoint,
       WebDragOperationsMask operationsAllowed,
       int modifiers) = 0;
   virtual WebDragOperation dragTargetDragOver(
-      const WebPoint& clientPoint,
+      const WebPoint& pointInViewport,
       const WebPoint& screenPoint,
       WebDragOperationsMask operationsAllowed,
       int modifiers) = 0;
   virtual void dragTargetDragLeave() = 0;
   virtual void dragTargetDrop(const WebDragData&,
-                              const WebPoint& clientPoint,
+                              const WebPoint& pointInViewport,
                               const WebPoint& screenPoint,
                               int modifiers) = 0;
 
