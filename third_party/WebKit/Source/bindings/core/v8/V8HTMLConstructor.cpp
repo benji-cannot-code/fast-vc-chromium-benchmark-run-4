39f20484dc53382224119054a6b45eeb0bd99c36
yurak <yurak@google.com>
2016-10-04 20:46:58 -0700

    Implemented HTMLConstructor extended attribute.
    Specs: https://html.spec.whatwg.org/#html-element-constructors
    
    Replaced V8HTMLElementCustom with V8HTMLConstructor
    
    Add new extended attribute [HTMLConstructor] to bindings and IDLExtendedAttributes.txt
      Added description to IDLExtendedAttributes.md and re-organized descriptions
      Added checks for conflicting extended attributes with [HTMLConstructor] in v8_interface.py
      Edited jinja templates for [HTMLConstructor] interface
    
    Replaced [CustomConstructor] with [HTMLConstructor] in HTMLElement.idl
    
    BUG=648828
    
    Review-Url: https://codereview.chromium.org/2385073002
    Cr-Commit-Position: refs/heads/master@{#423068}

--- third_party/WebKit/Source/bindings/core/v8/V8HTMLConstructor.cpp
+++ third_party/WebKit/Source/bindings/core/v8/V8HTMLConstructor.cpp
@@ -3,14 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "bindings/core/v8/V8HTMLElement.h"
-
 #include "bindings/core/v8/DOMWrapperWorld.h"
 #include "bindings/core/v8/ExceptionState.h"
 #include "bindings/core/v8/ScriptCustomElementDefinition.h"
 #include "bindings/core/v8/V8Binding.h"
 #include "bindings/core/v8/V8BindingMacros.h"
 #include "bindings/core/v8/V8DOMWrapper.h"
+#include "bindings/core/v8/V8HTMLElement.h"
 #include "bindings/core/v8/V8ThrowException.h"
 #include "core/dom/Document.h"
 #include "core/dom/Element.h"
@@ -21,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-void V8HTMLElement::constructorCustom(
+void V8HTMLElement::HTMLConstructor(
     const v8::FunctionCallbackInfo<v8::Value>& info) {
   DCHECK(info.IsConstructCall());
 
