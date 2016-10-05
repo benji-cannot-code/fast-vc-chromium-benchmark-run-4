db78c4d7153e39f26529cf69d9ace87db0cfd358
estade <estade@chromium.org>
2016-10-05 11:19:27 -0700

    Restore correct size for omnibox search icon.
    Even though most OMNIBOX_FOO icons have a 1x version which explicitly
    sets a canvas size, this one only has the generic-DSF version so the
    icon size must be specified.
    
    BUG=652800
    
    Review-Url: https://codereview.chromium.org/2389073004
    Cr-Commit-Position: refs/heads/master@{#423222}

--- chrome/browser/ui/views/location_bar/bubble_icon_view.cc
+++ chrome/browser/ui/views/location_bar/bubble_icon_view.cc
@@ -193,7 +193,7 @@ void BubbleIconView::UpdateIcon() {
               ui::NativeTheme::kColorId_ProminentButtonColor)
           : GetInkDropBaseColor();
   image_->SetImage(gfx::CreateVectorIcon(
-      GetVectorIcon(), LocationBarView::kLocationBarIconWidth, icon_color));
+      GetVectorIcon(), LocationBarView::kIconWidth, icon_color));
 }
 
 void BubbleIconView::SetActiveInternal(bool active) {
