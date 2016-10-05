e25e513aa782992bd66a23633f0c66305667a681
thakis <thakis@chromium.org>
2016-10-05 15:59:30 -0700

    reflow comments in core/imagebitmap,core/html/track
    BUG=563793
    
    Review-Url: https://codereview.chromium.org/2393633005
    Cr-Commit-Position: refs/heads/master@{#423327}

--- third_party/WebKit/Source/core/html/track/CueTimeline.cpp
+++ third_party/WebKit/Source/core/html/track/CueTimeline.cpp
@@ -137,7 +137,8 @@ void CueTimeline::updateActiveCues(double movieTime) {
   CueList currentCues;
 
   // The user agent must synchronously unset [the text track cue active] flag
-  // whenever ... the media element's readyState is changed back to kHaveNothing.
+  // whenever ... the media element's readyState is changed back to
+  // kHaveNothing.
   if (mediaElement.getReadyState() != HTMLMediaElement::kHaveNothing &&
       mediaElement.webMediaPlayer())
     currentCues =
@@ -316,7 +317,8 @@ void CueTimeline::updateActiveCues(double movieTime) {
                  trackIndexCompare);
 
   // 15 - For each text track in affected tracks, in the list order, queue a
-  // task to fire a simple event named cuechange at the TextTrack object, and, ...
+  // task to fire a simple event named cuechange at the TextTrack object, and,
+  // ...
   for (size_t i = 0; i < affectedTracks.size(); ++i) {
     mediaElement.scheduleEvent(createEventWithTarget(EventTypeNames::cuechange,
                                                      affectedTracks[i].get()));
--- third_party/WebKit/Source/core/html/track/vtt/VTTRegion.cpp
+++ third_party/WebKit/Source/core/html/track/vtt/VTTRegion.cpp
@@ -47,7 +47,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-// The following values default values are defined within the WebVTT Regions Spec.
+// The following values default values are defined within the WebVTT Regions
+// Spec.
 // https://dvcs.w3.org/hg/text-tracks/raw-file/default/608toVTT/region.html
 
 // The region occupies by default 100% of the width of the video viewport.
@@ -353,9 +354,9 @@ void VTTRegion::displayLastVTTCueBox() {
   DVLOG(VTT_LOG_LEVEL) << "displayLastVTTCueBox";
   DCHECK(m_cueContainer);
 
-  // FIXME: This should not be causing recalc styles in a loop to set the "top" css
-  // property to move elements. We should just scroll the text track cues on the
-  // compositor with an animation.
+  // FIXME: This should not be causing recalc styles in a loop to set the "top"
+  // css property to move elements. We should just scroll the text track cues on
+  // the compositor with an animation.
 
   if (m_scrollTimer.isActive())
     return;
