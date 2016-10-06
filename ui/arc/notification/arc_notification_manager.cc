402dff96f9908629cb9ab2ed069a3ca132a56d21
xiyuan <xiyuan@chromium.org>
2016-10-06 09:58:52 -0700

    arc: OnClosedFromAndroid removes notifications as not by_user
    Otherwise, a pinned notification remains in message center while
    the underlying ArcNotificationItem is destroyed. This would cause
    a crash later when message center attempts to create a view for
    the notification.
    
    BUG=647149,649379
    BUG=b/31905643
    TEST=Install any app from play store. After install finishes, bring
        up the message center and observe no crash.
    
    Review-Url: https://codereview.chromium.org/2394483004
    Cr-Commit-Position: refs/heads/master@{#423565}

--- ui/arc/notification/arc_notification_manager.cc
+++ ui/arc/notification/arc_notification_manager.cc
@@ -63,7 +63,7 @@ void ArcNotificationManager::OnInstanceClosed() {
     auto it = items_.begin();
     std::unique_ptr<ArcNotificationItem> item = std::move(it->second);
     items_.erase(it);
-    item->OnClosedFromAndroid(false /* by_user */);
+    item->OnClosedFromAndroid();
   }
   ready_ = false;
 }
@@ -102,7 +102,7 @@ void ArcNotificationManager::OnNotificationRemoved(const mojo::String& key) {
 
   std::unique_ptr<ArcNotificationItem> item = std::move(it->second);
   items_.erase(it);
-  item->OnClosedFromAndroid(true /* by_user */);
+  item->OnClosedFromAndroid();
 }
 
 void ArcNotificationManager::SendNotificationRemovedFromChrome(
