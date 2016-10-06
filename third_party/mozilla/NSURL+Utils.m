8923b3d563ffab61f52cb6383c85a4775e1b9e05
eugenebut <eugenebut@chromium.org>
2016-10-05 21:10:32 -0700

    [Mac Fix-It] Removed usage of deprecated resource fork API.
    There is no modern API to open resource forks on macOS. Currently used
    API is deprecated and its usage should be removed as a part of
    "Chrome Mac Deployment Target Fix-It". |URLFromInetloc| allows opening
    of webloc files created by Safari, which works fine just by reading data
    from webloc plist. New implementation will not open resource forks, but
    it's very unlikely that they would exist on modern macOS.
    
    BUG=650805
    
    Review-Url: https://codereview.chromium.org/2391653003
    Cr-Commit-Position: refs/heads/master@{#423427}

--- third_party/mozilla/NSURL+Utils.m
+++ third_party/mozilla/NSURL+Utils.m
@@ -65,44 +65,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 +(NSURL*)URLFromInetloc:(NSString*)inFile
 {
-  FSRef ref;
-  NSURL *ret = nil;
-  
-  if (inFile && FSPathMakeRef((UInt8 *)[inFile fileSystemRepresentation], &ref, NULL) == noErr) {
-    short resRef;
-    
-    resRef = FSOpenResFile(&ref, fsRdPerm);
-    
-    if (resRef != -1) { // Has resouce fork.
-      Handle urlResHandle;
-      
-      if ((urlResHandle = Get1Resource('url ', 256))) { // Has 'url ' resource with ID 256.
-        long size;
-        
-        size = GetMaxResourceSize(urlResHandle);
-// Begin Google Modified
-//        ret = [NSURL URLWithString:[NSString stringWithCString:(char *)*urlResHandle length:size]];
-        NSString *urlString = [[[NSString alloc] initWithBytes:(void *)*urlResHandle
-                                                        length:size
-                                                      encoding:NSMacOSRomanStringEncoding]  // best guess here
-                               autorelease];
-        ret = [NSURL URLWithString:urlString];
-// End Google Modified
-      }
-      
-      CloseResFile(resRef);
-    }
-
-    if (!ret) { // Look for valid plist data.
-      NSDictionary *plist;
-      if ((plist = [[NSDictionary alloc] initWithContentsOfFile:inFile])) {
-        ret = [NSURL URLWithString:[plist objectForKey:@"URL"]];
-        [plist release];
-      }
-    }
-  }
-  
-  return ret;
+  //// Begin Google Modified
+  NSDictionary *plist = [NSDictionary dictionaryWithContentsOfFile:inFile];
+  return [NSURL URLWithString:[plist objectForKey:@"URL"]];
+  //// End Google Modified
 }
 
 //
