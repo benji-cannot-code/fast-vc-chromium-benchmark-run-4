1610357a6c9302162ff6a7963bb29623bff68135
xunjieli <xunjieli@chromium.org>
2016-10-05 17:45:42 -0700

    Remove OnDataAvailable() notification when trailers are to be delivered
    This CL used the change that is suggested by mpw@ in
    https://bugs.chromium.org/p/chromium/issues/detail?id=652935#c1.
    
    Specifically, this CL removes
    QuicChromiumClientStream::Delegate::OnDataAvailable()
    notification if there is no byte can be read and trailers are to be delivered.
    
    This CL adjusted unit tests to test the correct ordering of events.
    
    BUG=652935
    
    Review-Url: https://codereview.chromium.org/2393233002
    Cr-Commit-Position: refs/heads/master@{#423357}

--- net/quic/core/quic_spdy_stream.h
+++ net/quic/core/quic_spdy_stream.h
@@ -190,6 +190,9 @@ class NET_EXPORT_PRIVATE QuicSpdyStream : public ReliableQuicStream {
   // Returns true if headers have been fully read and consumed.
   bool FinishedReadingHeaders() const;
 
+  // Returns true if trailers have been fully read and consumed.
+  bool FinishedReadingTrailers() const;
+
   virtual SpdyPriority priority() const;
 
   // Sets priority_ to priority.  This should only be called before bytes are
@@ -230,9 +233,6 @@ class NET_EXPORT_PRIVATE QuicSpdyStream : public ReliableQuicStream {
   friend class test::QuicSpdyStreamPeer;
   friend class test::ReliableQuicStreamPeer;
   friend class QuicStreamUtils;
-
-  // Returns true if trailers have been fully read and consumed.
-  bool FinishedReadingTrailers() const;
 
   QuicSpdySession* spdy_session_;
 
