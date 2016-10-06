b488742d52aa93301b45a02de6fe828283b8db92
erikchen <erikchen@chromium.org>
2016-10-06 13:58:56 -0700

    Add a variable use_system_xcode to GN.
    Start plumbing it through to actions that require binaries from the Xcode
    toolchain.
    
    BUG=651267
    TBR=sdefresne@chromium.org
    
    Review-Url: https://codereview.chromium.org/2388063003
    Cr-Commit-Position: refs/heads/master@{#423673}

--- build/config/mac/compile_xib.py
+++ build/config/mac/compile_xib.py
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 
 import argparse
+import logging
 import os
 import re
 import subprocess
@@ -19,7 +20,12 @@ def main():
                       help='Path to output bundle.')
   parser.add_argument('-i', '--input', required=True,
                       help='Path to input xib or storyboard.')
+  parser.add_argument('--developer_dir', required=False,
+                      help='Path to Xcode.')
   args, unknown_args = parser.parse_known_args()
+
+  if (args.developer_dir):
+    os.environ['DEVELOPER_DIR'] = args.developer_dir
 
   ibtool_args = [
       'xcrun', 'ibtool',
--- third_party/WebKit/Source/build/scripts/rule_bison.py
+++ third_party/WebKit/Source/build/scripts/rule_bison.py
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# usage: rule_bison.py INPUT_FILE OUTPUT_DIR [BISON_EXE]
+# usage: rule_bison.py INPUT_FILE OUTPUT_DIR BISON_EXE [DEVELOPER_DIR]
 # INPUT_FILE is a path to either XPathGrammar.y.
 # OUTPUT_DIR is where the bison-generated .cpp and .h files should be placed.
 
@@ -43,13 +43,13 @@ import os.path
 import subprocess
 import sys
 
-assert len(sys.argv) == 3 or len(sys.argv) == 4
+assert len(sys.argv) == 4 or len(sys.argv) == 5
 
 inputFile = sys.argv[1]
 outputDir = sys.argv[2]
-bisonExe = 'bison'
-if len(sys.argv) > 3:
-    bisonExe = sys.argv[3]
+bisonExe = sys.argv[3]
+if len(sys.argv) > 4:
+    os.environ['DEVELOPER_DIR'] = sys.argv[4]
 
 pathToBison = os.path.split(bisonExe)[0]
 if pathToBison:
