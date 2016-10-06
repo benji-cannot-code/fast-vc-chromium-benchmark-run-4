c6b60e496536582977b4355b24122af9ef2a4434
jdufault <jdufault@chromium.org>
2016-10-06 13:10:18 -0700

    cros: Small tweaks to palette behavior.
    Update laser icon color and automatically close the palette when enabling the magnifier or laser.
    
    BUG=645312
    
    Review-Url: https://codereview.chromium.org/2368713002
    Cr-Commit-Position: refs/heads/master@{#423642}

--- ash/common/system/chromeos/palette/tools/laser_pointer_mode.cc
+++ ash/common/system/chromeos/palette/tools/laser_pointer_mode.cc
@@ -32,6 +32,7 @@ void LaserPointerMode::OnEnable() {
   CommonPaletteTool::OnEnable();
 
   WmShell::Get()->SetLaserPointerEnabled(true);
+  delegate()->HidePalette();
 }
 
 void LaserPointerMode::OnDisable() {
