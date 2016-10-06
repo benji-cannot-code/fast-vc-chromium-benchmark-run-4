2edd358c5d1e7c225fcbc125e80bfdf7292edb34
tim <tim@rnc-ag.de>
2016-10-06 14:45:42 -0700

    GN: Initialize Tool::defined_from_ in the constructor
    set_defined_from() isn't called in some tests (e.g. those using TestWithScope),
    thus calling defined_from() will only yield garbage, likely leading to a crash.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2394323002
    Cr-Commit-Position: refs/heads/master@{#423698}

--- tools/gn/tool.cc
+++ tools/gn/tool.cc
@@ -6,7 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "tools/gn/tool.h"
 
 Tool::Tool()
-    : depsformat_(DEPS_GCC),
+    : defined_from_(nullptr),
+      depsformat_(DEPS_GCC),
       precompiled_header_type_(PCH_NONE),
       restat_(false),
       complete_(false) {
