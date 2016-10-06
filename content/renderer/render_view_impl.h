eff87ea3a24f913b1179c50fb33bd0314ed01d06
altimin <altimin@chromium.org>
2016-10-05 19:40:20 -0700

    [scheduler] Teach scheduler about audio state
    This patch wires information about audio state to renderer scheduler
    and stops all throttling (background and offscreen) while
    audio is playing and for a short period after audio is stopped.
    
    BUG=642321,616519
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    
    Review-Url: https://codereview.chromium.org/2383473002
    Cr-Commit-Position: refs/heads/master@{#423405}

--- content/renderer/render_view_impl.h
+++ content/renderer/render_view_impl.h
@@ -618,6 +618,7 @@ class CONTENT_EXPORT RenderViewImpl
   void OnZoom(PageZoom zoom);
   void OnForceRedraw(const ui::LatencyInfo& latency_info);
   void OnSelectWordAroundCaret();
+  void OnAudioStateChanged(bool is_audio_playing);
 #if defined(OS_ANDROID)
   void OnUndoScrollFocusedEditableNodeIntoRect();
   void OnUpdateTopControlsState(bool enable_hiding,
--- third_party/WebKit/Source/web/WebViewImpl.cpp
+++ third_party/WebKit/Source/web/WebViewImpl.cpp
@@ -3769,6 +3769,10 @@ void WebViewImpl::performPluginAction(const WebPluginAction& action,
   }
 }
 
+void WebViewImpl::audioStateChanged(bool isAudioPlaying) {
+  m_scheduler->audioStateChanged(isAudioPlaying);
+}
+
 WebHitTestResult WebViewImpl::hitTestResultAt(const WebPoint& point) {
   return coreHitTestResultAt(point);
 }
