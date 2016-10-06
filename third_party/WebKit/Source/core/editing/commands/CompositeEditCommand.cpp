e43caaca25831252a7e1aadf977bca4c7b54f565
chongz <chongz@chromium.org>
2016-10-05 18:29:38 -0700

    [LayoutTest] Fix flakey test "inputevent-drag-drop.html"
    On debug mode the WPT will start before image/document is loaded.
    
    This CL changed the tests to use styled text (instead of image) as
    drag data.
    
    Also fixed a DCHECK, which would trigger when you do Drag&Drop and
    then refresh the page. (introduced in crrev.com/2374743002)
    
    BUG=652979
    
    Review-Url: https://codereview.chromium.org/2393903003
    Cr-Commit-Position: refs/heads/master@{#423379}

--- third_party/WebKit/Source/core/editing/commands/CompositeEditCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/CompositeEditCommand.cpp
@@ -301,6 +301,7 @@ void CompositeEditCommand::applyCommandToComposite(
 void CompositeEditCommand::appendCommandToComposite(
     CompositeEditCommand* command) {
   ensureComposition()->append(command->ensureComposition());
+  command->m_composition = nullptr;
   command->setParent(this);
   m_commands.append(command);
 }
