7ba8da33711c6b7a290f5c556f3c93c1b932682d
twellington <twellington@chromium.org>
2016-10-05 17:20:03 -0700

    Add @RetryOnFailure to MediaAccessPermissionRequestTest#*
    BUG=653307
    TBR=michaelbai@chromium.org
    
    Review-Url: https://codereview.chromium.org/2397923002
    Cr-Commit-Position: refs/heads/master@{#423351}

--- android_webview/javatests/src/org/chromium/android_webview/test/MediaAccessPermissionRequestTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/MediaAccessPermissionRequestTest.java
@@ -77,6 +77,7 @@ public class MediaAccessPermissionRequestTest extends AwTestBase {
     @Feature({"AndroidWebView"})
     @SmallTest
     @DisableIf.Build(sdk_is_greater_than = 22, message = "crbug.com/623921")
+    @RetryOnFailure
     public void testGrantAccess() throws Throwable {
         final OnPermissionRequestHelper helper = new OnPermissionRequestHelper();
         TestAwContentsClient contentsClient =
@@ -100,6 +101,7 @@ public class MediaAccessPermissionRequestTest extends AwTestBase {
     @Feature({"AndroidWebView"})
     @SmallTest
     @DisableIf.Build(sdk_is_greater_than = 22, message = "crbug.com/614347")
+    @RetryOnFailure
     public void testDenyAccess() throws Throwable {
         final OnPermissionRequestHelper helper = new OnPermissionRequestHelper();
         TestAwContentsClient contentsClient =
@@ -170,6 +172,7 @@ public class MediaAccessPermissionRequestTest extends AwTestBase {
     @Feature({"AndroidWebView"})
     @SmallTest
     @DisableIf.Build(sdk_is_greater_than = 22, message = "crbug.com/614347")
+    @RetryOnFailure
     public void testCancelPermission() throws Throwable {
         final OnPermissionRequestHelper helper = new OnPermissionRequestHelper();
         TestAwContentsClient contentsClient =
