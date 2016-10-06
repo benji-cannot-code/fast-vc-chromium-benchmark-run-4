34321da1e6612ffb275e05f335326c1af6742def
dcheng <dcheng@chromium.org>
2016-10-05 23:55:05 -0700

    Reflow comments in //third_party/WebKit/Source/core/editing/commands
    BUG=563793
    
    Review-Url: https://codereview.chromium.org/2397963002
    Cr-Commit-Position: refs/heads/master@{#423458}

--- third_party/WebKit/Source/core/editing/commands/EditorCommandNames.h
+++ third_party/WebKit/Source/core/editing/commands/EditorCommandNames.h
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-// Must be ordered in a case-folding manner for binary search.
-// Covered by unit tests in EditingCommandTest.cpp (not able to use static_assert)
+// Must be ordered in a case-folding manner for binary search. Covered by unit
+// tests in EditingCommandTest.cpp (not able to use static_assert)
 #define FOR_EACH_BLINK_EDITING_COMMAND_NAME(V)    \
   V(AlignCenter)                                  \
   V(AlignJustified)                               \
--- third_party/WebKit/Source/core/editing/commands/FormatBlockCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/FormatBlockCommand.cpp
@@ -102,8 +102,9 @@ void FormatBlockCommand::formatRange(const Position& start,
   }
 
   if (!blockElement) {
-    // Create a new blockquote and insert it as a child of the root editable element. We accomplish
-    // this by splitting all parents of the current paragraph up to that point.
+    // Create a new blockquote and insert it as a child of the root editable
+    // element. We accomplish this by splitting all parents of the current
+    // paragraph up to that point.
     blockElement = createBlockElement();
     insertNodeBefore(blockElement, nodeAfterInsertionPosition, editingState);
     if (editingState->isAborted())
@@ -123,7 +124,8 @@ void FormatBlockCommand::formatRange(const Position& start,
   if (editingState->isAborted())
     return;
 
-  // Copy the inline style of the original block element to the newly created block-style element.
+  // Copy the inline style of the original block element to the newly created
+  // block-style element.
   if (outerBlock != nodeAfterInsertionPosition &&
       toHTMLElement(nodeAfterInsertionPosition)->hasAttribute(styleAttr))
     blockElement->setAttribute(
