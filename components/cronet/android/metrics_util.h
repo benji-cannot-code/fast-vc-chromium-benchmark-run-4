6a1c547b648198b412cba068703c5d1c90d6c68f
xunjieli <xunjieli@chromium.org>
2016-10-06 11:43:26 -0700

    [Cronet] Pass metrics information from C++ BidirectionalStream to Java
    This CL adds plumbing to pass metrics information from C++
    BidirectionalStream to its Java counterpart.
    
    BUG=648346
    
    Review-Url: https://codereview.chromium.org/2360813003
    Cr-Commit-Position: refs/heads/master@{#423603}

---
+++ components/cronet/android/metrics_util.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_CRONET_ANDROID_CRONET_METRICS_UTIL_H_
+#define COMPONENTS_CRONET_ANDROID_CRONET_METRICS_UTIL_H_
+
+#include <stdint.h>
+
+#include "base/time/time.h"
+
+namespace cronet {
+
+namespace metrics_util {
+
+// Converts timing metrics stored as TimeTicks into the format expected by the
+// Java layer: ms since Unix epoch, or -1 for null
+int64_t ConvertTime(const base::TimeTicks& ticks,
+                    const base::TimeTicks& start_ticks,
+                    const base::Time& start_time);
+}  // namespace metrics_util
+
+}  // namespace cronet
+
+#endif  // COMPONENTS_CRONET_ANDROID_CRONET_METRICS_UTIL_H_
