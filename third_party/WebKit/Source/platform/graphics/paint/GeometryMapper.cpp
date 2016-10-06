cfac65df3770b9164d19f8327aac69d196819f61
wangxianzhu <wangxianzhu@chromium.org>
2016-10-06 11:21:24 -0700

    [SPInvalidation] Ignore clip failure in GeometryMapper::localToVisualRectInAncestorSpace()
    On SPv1 GeometryMapper may fail to apply clip when the paint
    invalidation container creates an overflow clip (in ancestorState)
    which is not in localState of an out-of-flow positioned descendant.
    
    See crbug.com/513108 and layout test
    compositing/overflow/handle-non-ancestor-clip-parent.html (run with
    --enable-prefer-compositing-to-lcd-text) and crbug.com/ for details.
    
    Ignore clip failure for SPv1 for now.
    
    BUG=646176
    TEST=virtual/prefer_compositing_to_lcd_text/compositing/overflow/handle-non-ancestor-clip-parent.html
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2394483003
    Cr-Commit-Position: refs/heads/master@{#423592}

--- third_party/WebKit/Source/platform/graphics/paint/GeometryMapper.cpp
+++ third_party/WebKit/Source/platform/graphics/paint/GeometryMapper.cpp
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "platform/graphics/paint/GeometryMapper.h"
 
+#include "platform/RuntimeEnabledFeatures.h"
 #include "platform/geometry/LayoutRect.h"
 
 namespace blink {
@@ -108,9 +109,21 @@ FloatRect GeometryMapper::localToVisualRectInAncestorSpace(
 
   const auto clipRect =
       localToAncestorClipRect(localState, ancestorState, success);
-  DCHECK(success);
 
-  mappedRect.intersect(clipRect);
+  if (success) {
+    mappedRect.intersect(clipRect);
+  } else if (!RuntimeEnabledFeatures::slimmingPaintV2Enabled()) {
+    // On SPv1 we may fail when the paint invalidation container creates an
+    // overflow clip (in ancestorState) which is not in localState of an
+    // out-of-flow positioned descendant. See crbug.com/513108 and layout test
+    // compositing/overflow/handle-non-ancestor-clip-parent.html (run with
+    // --enable-prefer-compositing-to-lcd-text) for details.
+    // Ignore it for SPv1 for now.
+    success = true;
+  } else {
+    DCHECK(success);
+  }
+
   return mappedRect;
 }
 
