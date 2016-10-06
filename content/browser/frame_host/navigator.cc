b3c4a989e4a8f44b46415fcfe21212a8324550c3
creis <creis@chromium.org>
2016-10-05 19:19:34 -0700

    Record UMA stats for subframes history navigations.
    This CL tracks how often subframe history navigations go to a different
    URL than the default frame src URL, and how long the unique names are
    in these cases when they include frame paths.  The latter is useful for
    knowing whether a plan to truncate unique names would affect session
    history behavior.
    
    This should be reverted after sufficient data is collected.
    
    BUG=626202
    TEST=UMA stats reported when going back after navigating an unnamed
         child frame, particularly unnamed grandchildren of subframes
         with long names.
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2398753002
    Cr-Commit-Position: refs/heads/master@{#423397}

--- content/browser/frame_host/navigator.cc
+++ content/browser/frame_host/navigator.cc
@@ -26,7 +26,8 @@ bool Navigator::NavigateToPendingEntry(FrameTreeNode* frame_tree_node,
   return false;
 }
 
-bool Navigator::NavigateNewChildFrame(RenderFrameHostImpl* render_frame_host) {
+bool Navigator::NavigateNewChildFrame(RenderFrameHostImpl* render_frame_host,
+                                      const GURL& default_url) {
   return false;
 }
 
