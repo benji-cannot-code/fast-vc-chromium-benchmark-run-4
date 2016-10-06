19df0591dc312adaa519a4dcd7eb6f53766c4a50
alexclarke <alexclarke@chromium.org>
2016-10-06 12:15:09 -0700

    Add a DOM.getLayoutTreeNodes devtools command
    This command will help Headless API users understand the layout
    of the page.
    
    BUG=546953
    
    Review-Url: https://codereview.chromium.org/2346853002
    Cr-Commit-Position: refs/heads/master@{#423623}

--- third_party/WebKit/Source/core/inspector/InspectorDOMAgent.h
+++ third_party/WebKit/Source/core/inspector/InspectorDOMAgent.h
@@ -123,6 +123,10 @@ class CORE_EXPORT InspectorDOMAgent final
   void disable(ErrorString*) override;
   void getDocument(ErrorString*,
                    std::unique_ptr<protocol::DOM::Node>* root) override;
+  void getLayoutTreeNodes(
+      ErrorString*,
+      std::unique_ptr<protocol::Array<protocol::DOM::LayoutTreeNode>>*
+          layoutTreeNodes) override;
   void collectClassNamesFromSubtree(
       ErrorString*,
       int nodeId,
@@ -351,6 +355,9 @@ class CORE_EXPORT InspectorDOMAgent final
   bool pushDocumentUponHandlelessOperation(ErrorString*);
 
   Member<InspectorRevalidateDOMTask> revalidateTask();
+
+  void visitLayoutTreeNodes(Node*,
+                            protocol::Array<protocol::DOM::LayoutTreeNode>&);
 
   v8::Isolate* m_isolate;
   Member<InspectedFrames> m_inspectedFrames;
