c66c3380f76f70a4e66e7fc0398b96f4340761cf
fdoray <fdoray@chromium.org>
2016-10-06 11:42:19 -0700

    Use FileDescriptorWatcher in BrlapiConnectionImpl.
    This allows BrlapiConnectionImpl to be used from any thread that
    instantiates a FileDescriptorWatcher (not just threads that run
    a MessageLoopForIO). This will facilitate the migration of
    BrowserThreads to base/task_scheduler.
    
    BUG=645114
    
    Review-Url: https://codereview.chromium.org/2388913002
    Cr-Commit-Position: refs/heads/master@{#423602}

--- chrome/browser/extensions/api/braille_display_private/brlapi_connection.cc
+++ chrome/browser/extensions/api/braille_display_private/brlapi_connection.cc
@@ -7,14 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <errno.h>
 
+#include "base/files/file_descriptor_watcher_posix.h"
 #include "base/macros.h"
 #include "base/memory/free_deleter.h"
-#include "base/message_loop/message_loop.h"
 #include "base/sys_info.h"
 #include "build/build_config.h"
 
 namespace extensions {
-using base::MessageLoopForIO;
 namespace api {
 namespace braille_display_private {
 
@@ -31,8 +30,7 @@ static const int kDefaultTtyChromeOS = 1;
 #endif
 }  // namespace
 
-class BrlapiConnectionImpl : public BrlapiConnection,
-                             MessageLoopForIO::Watcher {
+class BrlapiConnectionImpl : public BrlapiConnection {
  public:
   explicit BrlapiConnectionImpl(LibBrlapiLoader* loader) :
       libbrlapi_loader_(loader) {}
@@ -48,19 +46,13 @@ class BrlapiConnectionImpl : public BrlapiConnection,
   bool WriteDots(const unsigned char* cells) override;
   int ReadKey(brlapi_keyCode_t* keyCode) override;
 
-  // MessageLoopForIO::Watcher
-  void OnFileCanReadWithoutBlocking(int fd) override { on_data_ready_.Run(); }
-
-  void OnFileCanWriteWithoutBlocking(int fd) override {}
-
  private:
   bool CheckConnected();
   ConnectResult ConnectResultForError();
 
   LibBrlapiLoader* libbrlapi_loader_;
   std::unique_ptr<brlapi_handle_t, base::FreeDeleter> handle_;
-  MessageLoopForIO::FileDescriptorWatcher fd_controller_;
-  OnDataReadyCallback on_data_ready_;
+  std::unique_ptr<base::FileDescriptorWatcher::Controller> fd_controller_;
 
   DISALLOW_COPY_AND_ASSIGN(BrlapiConnectionImpl);
 };
@@ -133,14 +125,8 @@ BrlapiConnection::ConnectResult BrlapiConnectionImpl::Connect(
     return CONNECT_ERROR_RETRY;
   }
 
-  if (!MessageLoopForIO::current()->WatchFileDescriptor(
-          fd, true, MessageLoopForIO::WATCH_READ, &fd_controller_, this)) {
-    LOG(ERROR) << "Couldn't watch file descriptor " << fd;
-    Disconnect();
-    return CONNECT_ERROR_RETRY;
-  }
-
-  on_data_ready_ = on_data_ready;
+  fd_controller_ =
+      base::FileDescriptorWatcher::WatchReadable(fd, on_data_ready);
 
   return CONNECT_SUCCESS;
 }
@@ -149,7 +135,7 @@ void BrlapiConnectionImpl::Disconnect() {
   if (!handle_) {
     return;
   }
-  fd_controller_.StopWatchingFileDescriptor();
+  fd_controller_.reset();
   libbrlapi_loader_->brlapi__closeConnection(
       handle_.get());
   handle_.reset();
