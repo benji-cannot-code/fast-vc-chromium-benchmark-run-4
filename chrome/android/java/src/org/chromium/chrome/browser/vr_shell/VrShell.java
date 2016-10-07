af99bc2fff6170ec7234bf44f91d31ce48cc2600
mthiesse <mthiesse@chromium.org>
2016-10-07 08:05:27 -0700

    Fix Cardboard trigger (broken by GVR update)
    BUG=
    
    Review-Url: https://codereview.chromium.org/2392943005
    Cr-Commit-Position: refs/heads/master@{#423861}

--- chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShell.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShell.java
@@ -94,7 +94,6 @@ public class VrShell extends GvrLayout implements GLSurfaceView.Renderer, VrShel
 
         @Override
         public boolean dispatchTouchEvent(MotionEvent event) {
-            VrShell.this.onTouchEvent(event);
             return true;
         }
     }
@@ -259,6 +258,15 @@ public class VrShell extends GvrLayout implements GLSurfaceView.Renderer, VrShel
     }
 
     @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
+            nativeOnTriggerEvent(mNativeVrShell);
+        }
+        // Don't mark this as handled so that Daydream screen alignment still functions.
+        return false;
+    }
+
+    @Override
     public void onResume() {
         super.onResume();
         if (mNativeVrShell != 0) {
@@ -319,15 +327,6 @@ public class VrShell extends GvrLayout implements GLSurfaceView.Renderer, VrShel
     @Override
     public void setWebVrModeEnabled(boolean enabled) {
         nativeSetWebVrMode(mNativeVrShell, enabled);
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
-            nativeOnTriggerEvent(mNativeVrShell);
-        }
-
-        return true;
     }
 
     @Override
--- chrome/browser/android/vr_shell/vr_shell.cc
+++ chrome/browser/android/vr_shell/vr_shell.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/referrer.h"
 #include "content/public/common/screen_info.h"
-
 #include "jni/VrShell_jni.h"
 #include "third_party/WebKit/public/web/WebInputEvent.h"
 #include "ui/android/view_android.h"
