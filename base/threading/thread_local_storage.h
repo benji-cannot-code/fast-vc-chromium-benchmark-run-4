fa11fc225b714b1f5cca5bd8ab01e4b532d11d23
robliao <robliao@chromium.org>
2016-10-07 13:22:12 -0700

    Ensure Freed TLS Slots Contain nullptr on Reallocation
    Code generally depends on TLS slots initialized to zero. This means
    that code that gets a reused slot also depends on the reused slot
    containing zero.
    
    This CL introduces a versioning system to allow us to quickly determine
    if a slot was previously freed.
    
    Also added overview comments as a bonus.
    
    BUG=590907
    
    Review-Url: https://codereview.chromium.org/2383833004
    Cr-Commit-Position: refs/heads/master@{#423952}

--- base/threading/thread_local_storage.h
+++ base/threading/thread_local_storage.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_THREADING_THREAD_LOCAL_STORAGE_H_
 #define BASE_THREADING_THREAD_LOCAL_STORAGE_H_
 
+#include <stdint.h>
+
 #include "base/atomicops.h"
 #include "base/base_export.h"
 #include "base/macros.h"
@@ -124,6 +126,7 @@ class BASE_EXPORT ThreadLocalStorage {
     // The internals of this struct should be considered private.
     base::subtle::Atomic32 initialized_;
     int slot_;
+    uint32_t version_;
   };
 
   // A convenience wrapper around StaticSlot with a constructor. Can be used
