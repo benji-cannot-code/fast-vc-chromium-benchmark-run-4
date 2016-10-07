37dc6ef9b321f34c7c2b36172275041386243338
sky <sky@chromium.org>
2016-10-07 09:35:21 -0700

    Fixes crash in IconLabelBubbleView::GetSizeForLabelWidth
    Widget::GetCompositor() may return null in certain cases, in
    particular on chromeos if the widget's native view doesn't have a
    parent than GetCompositor() returns null.
    
    BUG=643661
    TEST=covered by test
    R=pkasting@chromium.org
    
    Review-Url: https://codereview.chromium.org/2387423002
    Cr-Commit-Position: refs/heads/master@{#423881}

--- chrome/browser/ui/views/location_bar/icon_label_bubble_view_unittest.cc
+++ chrome/browser/ui/views/location_bar/icon_label_bubble_view_unittest.cc
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/image_view.h"
 #include "ui/views/test/views_test_base.h"
 
+#if defined(USE_ASH)
+#include "ui/aura/window.h"
+#endif
+
 namespace {
 
 const int kStayOpenTimeMS = 100;
@@ -218,3 +222,30 @@ TEST_F(IconLabelBubbleViewTest, AnimateLayout) {
   VerifyWithAnimationStep(10);
   VerifyWithAnimationStep(25);
 }
+
+#if defined(USE_ASH)
+// Verifies IconLabelBubbleView::GetPreferredSize() doesn't crash when there is
+// a widget but no compositor.
+using IconLabelBubbleViewCrashTest = views::ViewsTestBase;
+
+TEST_F(IconLabelBubbleViewCrashTest,
+       GetPreferredSizeDoesntCrashWhenNoCompositor) {
+  gfx::FontList font_list;
+  views::Widget::InitParams params =
+      CreateParams(views::Widget::InitParams::TYPE_WINDOW);
+  params.ownership = views::Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET;
+  views::Widget widget;
+  widget.Init(params);
+  IconLabelBubbleView* icon_label_bubble_view =
+      new TestIconLabelBubbleView(font_list);
+  icon_label_bubble_view->SetLabel(base::ASCIIToUTF16("x"));
+  widget.GetContentsView()->AddChildView(icon_label_bubble_view);
+  aura::Window* widget_native_view = widget.GetNativeView();
+  // Remove the window from its parent. This means GetWidget() in
+  // IconLabelBubbleView will return non-null, but GetWidget()->GetCompositor()
+  // will return null.
+  ASSERT_TRUE(widget_native_view->parent());
+  widget_native_view->parent()->RemoveChild(widget_native_view);
+  static_cast<views::View*>(icon_label_bubble_view)->GetPreferredSize();
+}
+#endif
