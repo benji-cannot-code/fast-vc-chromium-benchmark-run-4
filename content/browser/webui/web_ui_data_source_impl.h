a0b5f1e04ec9f8363272585de6791081ba37ac98
michaelpg <michaelpg@chromium.org>
2016-10-07 00:58:24 -0700

    WebUIDataSource::AddInteger (like AddBoolean/AddString)
    Convenience function to add integers to a WebUIDataSource
    without using AddLocalizedStrings with a base::DictionaryValue.
    
    Limits width to 32 bits to prevent integers larger than can
    be represented in ES6 (2^53 - 1). (As before, larger numbers
    could still be set via AddLocalizedStrings.)
    
    BUG=none
    R=tommycli@chromium.org,creis@chromium.org
    TEST=content_unittests:WebUIDataSourceTest
    
    Review-Url: https://codereview.chromium.org/2391073005
    Cr-Commit-Position: refs/heads/master@{#423816}

--- content/browser/webui/web_ui_data_source_impl.h
+++ content/browser/webui/web_ui_data_source_impl.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_WEBUI_WEB_UI_DATA_SOURCE_IMPL_H_
 #define CONTENT_BROWSER_WEBUI_WEB_UI_DATA_SOURCE_IMPL_H_
 
+#include <stdint.h>
+
 #include <map>
 #include <string>
 
@@ -35,6 +37,7 @@ class CONTENT_EXPORT WebUIDataSourceImpl
   void AddLocalizedStrings(
       const base::DictionaryValue& localized_strings) override;
   void AddBoolean(const std::string& name, bool value) override;
+  void AddInteger(const std::string& name, int32_t value) override;
   void SetJsonPath(const std::string& path) override;
   void AddResourcePath(const std::string& path, int resource_id) override;
   void SetDefaultResource(int resource_id) override;
