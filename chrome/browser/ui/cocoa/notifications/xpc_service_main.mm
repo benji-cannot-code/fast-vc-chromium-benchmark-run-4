22dbf4ec74b32dc552934b4170d9530d08f8fb01
miguelg <miguelg@chromium.org>
2016-10-07 07:16:08 -0700

    Add missing include in the alert xpc service
    BUG=571056
    
    Review-Url: https://codereview.chromium.org/2396363002
    Cr-Commit-Position: refs/heads/master@{#423851}

--- chrome/browser/ui/cocoa/notifications/xpc_service_main.mm
+++ chrome/browser/ui/cocoa/notifications/xpc_service_main.mm
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #import <Foundation/Foundation.h>
+#include <xpc/xpc.h>
 
 #include "base/mac/scoped_nsobject.h"
 #import "chrome/browser/ui/cocoa/notifications/notification_service_delegate.h"
