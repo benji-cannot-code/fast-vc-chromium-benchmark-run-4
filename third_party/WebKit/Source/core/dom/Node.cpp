3e885bd1db6b99c1af240ced09f196d39b1aaecf
mlippautz <mlippautz@chromium.org>
2016-10-07 01:48:36 -0700

    [wrapper-tracing] MutationObserver: Add write barriers
    Overview: https://docs.google.com/spreadsheets/d/1RhLiHF9Pnw7Zx8EijuR0LruPkeiVJIXXi0eRkRDFwBc/edit#gid=0&vpid=A3
    
    BUG=chromium:468240
    R=haraken@chromium.org
    
    Review-Url: https://codereview.chromium.org/2396693004
    Cr-Commit-Position: refs/heads/master@{#423820}

--- third_party/WebKit/Source/core/dom/Node.cpp
+++ third_party/WebKit/Source/core/dom/Node.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/DOMDataStore.h"
 #include "bindings/core/v8/ExceptionState.h"
 #include "bindings/core/v8/Microtask.h"
+#include "bindings/core/v8/ScriptWrappableVisitor.h"
 #include "bindings/core/v8/V8DOMWrapper.h"
 #include "core/HTMLNames.h"
 #include "core/MathMLNames.h"
@@ -295,8 +296,8 @@ NodeRareData& Node::ensureRareData() {
     m_data.m_rareData = NodeRareData::create(m_data.m_layoutObject);
 
   DCHECK(m_data.m_rareData);
-
   setFlag(HasRareDataFlag);
+  ScriptWrappableVisitor::writeBarrier(this, rareData());
   return *rareData();
 }
 
@@ -1974,6 +1975,7 @@ void Node::registerMutationObserver(
     registry.append(MutationObserverRegistration::create(
         observer, this, options, attributeFilter));
     registration = registry.last().get();
+    ScriptWrappableVisitor::writeBarrier(this, registration);
   }
 
   document().addMutationObserverTypes(registration->mutationTypes());
