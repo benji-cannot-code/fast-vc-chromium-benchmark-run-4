2c8a94811701e5dd4f1029d8e56be1c71114748d
bzanotti <bzanotti@chromium.org>
2016-10-07 06:24:13 -0700

    Add LogOut method to GaiaCookieManagerService::Observer.
    This allows GMCS observers to be notified whenever a logout operation
    finished.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2397263002
    Cr-Commit-Position: refs/heads/master@{#423845}

--- components/signin/core/browser/gaia_cookie_manager_service.cc
+++ components/signin/core/browser/gaia_cookie_manager_service.cc
@@ -667,8 +667,10 @@ void GaiaCookieManagerService::OnLogOutSuccess() {
   VLOG(1) << "GaiaCookieManagerService::OnLogOutSuccess";
 
   list_accounts_stale_ = true;
-
   fetcher_backoff_.InformOfRequest(true);
+  FOR_EACH_OBSERVER(Observer, observer_list_,
+                    OnLogOutAccountsFromCookieCompleted(
+                        GoogleServiceAuthError(GoogleServiceAuthError::NONE)));
   HandleNextRequest();
 }
 
@@ -688,6 +690,8 @@ void GaiaCookieManagerService::OnLogOutFailure(
     return;
   }
 
+  FOR_EACH_OBSERVER(Observer, observer_list_,
+                    OnLogOutAccountsFromCookieCompleted(error));
   HandleNextRequest();
 }
 
