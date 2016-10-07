74c5127446e03df009ec0054a0bf20f8dd51c1d9
rtoy <rtoy@chromium.org>
2016-10-07 11:33:11 -0700

    Remove WebAudio doppler effect API surface (already no-ops)
    Intent to Deprecate:
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/-1SI1GoHYO8/6XUjRs-fdv4J
    Intent to Remove: https://groups.google.com/a/chromium.org/d/msg/blink-dev/59cVGPqud1M/gR8yby0_BgAJ
    Feature: https://www.chromestatus.com/features/5753252045914112
    
    BUG=439644
    TEST=
    
    patch from issue 2356223002 at patchset 20001 (http://crrev.com/2356223002#ps20001)
    
    Review-Url: https://codereview.chromium.org/2380183002
    Cr-Commit-Position: refs/heads/master@{#423915}

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -477,9 +477,6 @@ class CORE_EXPORT UseCounter {
     MixedContentPlugin = 616,
     MixedContentPrefetch = 617,
     MixedContentVideo = 618,
-    AudioListenerDopplerFactor = 620,
-    AudioListenerSpeedOfSound = 621,
-    AudioListenerSetVelocity = 622,
     CSSSelectorPseudoFullScreenAncestor = 628,
     CSSSelectorPseudoFullScreen = 629,
     WebKitCSSMatrix = 630,
@@ -991,7 +988,6 @@ class CORE_EXPORT UseCounter {
 
     CSSAtRuleApply = 1249,
     CSSSelectorPseudoAny = 1250,
-    PannerNodeSetVelocity = 1251,
     DocumentAllItemNoArguments = 1252,
     DocumentAllItemNamed = 1253,
     DocumentAllItemIndexed = 1254,
