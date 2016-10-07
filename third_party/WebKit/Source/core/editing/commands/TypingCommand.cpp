f43073a0dc8b82006a2db0b6ca8697c84bbb46c7
xiaochengh <xiaochengh@chromium.org>
2016-10-06 22:36:48 -0700

    Prune deprecated functions from TypingCommand
    BUG=648949,651373
    
    Review-Url: https://codereview.chromium.org/2393353004
    Cr-Commit-Position: refs/heads/master@{#423801}

--- third_party/WebKit/Source/core/editing/commands/TypingCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/TypingCommand.cpp
@@ -513,9 +513,10 @@ bool TypingCommand::makeEditableRootEmpty(EditingState* editingState) {
   addBlockPlaceholderIfNeeded(root, editingState);
   if (editingState->isAborted())
     return false;
-  setEndingSelection(createVisibleSelectionDeprecated(
-      Position::firstPositionInNode(root), TextAffinity::Downstream,
-      endingSelection().isDirectional()));
+  document().updateStyleAndLayoutIgnorePendingStylesheets();
+  setEndingSelection(createVisibleSelection(Position::firstPositionInNode(root),
+                                            TextAffinity::Downstream,
+                                            endingSelection().isDirectional()));
 
   return true;
 }
@@ -558,7 +559,7 @@ void TypingCommand::deleteKeyPressed(TextGranularity granularity,
         selectionModifier.modify(FrameSelection::AlterationExtend,
                                  DirectionBackward, CharacterGranularity);
 
-      VisiblePosition visibleStart(endingSelection().visibleStartDeprecated());
+      VisiblePosition visibleStart(endingSelection().visibleStart());
       if (previousPositionOf(visibleStart, CannotCrossEditingBoundary)
               .isNull()) {
         // When the caret is at the start of the editable area in an empty list
@@ -594,7 +595,7 @@ void TypingCommand::deleteKeyPressed(TextGranularity granularity,
 
       // If the caret is at the start of a paragraph after a table, move content
       // into the last table cell.
-      if (isStartOfParagraphDeprecated(visibleStart) &&
+      if (isStartOfParagraph(visibleStart) &&
           tableElementJustBefore(
               previousPositionOf(visibleStart, CannotCrossEditingBoundary))) {
         // Unless the caret is just before a table.  We don't want to move a
@@ -608,7 +609,7 @@ void TypingCommand::deleteKeyPressed(TextGranularity granularity,
         // If the caret is just after a table, select the table and don't delete
         // anything.
       } else if (Element* table = tableElementJustBefore(visibleStart)) {
-        setEndingSelection(createVisibleSelectionDeprecated(
+        setEndingSelection(createVisibleSelection(
             Position::beforeNode(table), endingSelection().start(),
             TextAffinity::Downstream, endingSelection().isDirectional()));
         typingAddedToOpenCommand(DeleteKey);
@@ -708,7 +709,7 @@ void TypingCommand::forwardDeleteKeyPressed(TextGranularity granularity,
 
       Position downstreamEnd =
           mostForwardCaretPosition(endingSelection().end());
-      VisiblePosition visibleEnd = endingSelection().visibleEndDeprecated();
+      VisiblePosition visibleEnd = endingSelection().visibleEnd();
       Node* enclosingTableCell =
           enclosingNodeOfType(visibleEnd.deepEquivalent(), &isTableCell);
       if (enclosingTableCell &&
@@ -717,7 +718,7 @@ void TypingCommand::forwardDeleteKeyPressed(TextGranularity granularity,
                   .deepEquivalent())
         return;
       if (visibleEnd.deepEquivalent() ==
-          endOfParagraphDeprecated(visibleEnd).deepEquivalent())
+          endOfParagraph(visibleEnd).deepEquivalent())
         downstreamEnd = mostForwardCaretPosition(
             nextPositionOf(visibleEnd, CannotCrossEditingBoundary)
                 .deepEquivalent());
@@ -725,7 +726,7 @@ void TypingCommand::forwardDeleteKeyPressed(TextGranularity granularity,
       if (isDisplayInsideTable(downstreamEnd.computeContainerNode()) &&
           downstreamEnd.computeOffsetInContainerNode() <=
               caretMinOffset(downstreamEnd.computeContainerNode())) {
-        setEndingSelection(createVisibleSelectionDeprecated(
+        setEndingSelection(createVisibleSelection(
             endingSelection().end(),
             Position::afterNode(downstreamEnd.computeContainerNode()),
             TextAffinity::Downstream, endingSelection().isDirectional()));
@@ -737,8 +738,7 @@ void TypingCommand::forwardDeleteKeyPressed(TextGranularity granularity,
       // the next paragraph (if any)
       if (granularity == ParagraphBoundary &&
           selectionModifier.selection().isCaret() &&
-          isEndOfParagraphDeprecated(
-              selectionModifier.selection().visibleEndDeprecated()))
+          isEndOfParagraph(selectionModifier.selection().visibleEnd()))
         selectionModifier.modify(FrameSelection::AlterationExtend,
                                  DirectionForward, CharacterGranularity);
 
