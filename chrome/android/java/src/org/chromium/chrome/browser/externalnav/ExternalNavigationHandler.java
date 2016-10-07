e95f3737586429ac8626ab8286a4547552cebaf4
mariakhomenko <mariakhomenko@chromium.org>
2016-10-07 16:15:28 -0700

    Add an authenticated proxy activity.
    This is an invisible activity that allows us to dispatch intent:// URLs
    from SERP using startActivityForResult, so that the call can be
    authenticated by Supervisor.
    
    In the cases where SERP triggered the intent, we will start the activity
    in a new task and launch the instant apps intent from there.
    
    This is necessary, so that the speed bump can be bypassed from SERP.
    
    BUG=644772
    
    Review-Url: https://codereview.chromium.org/2394393002
    Cr-Commit-Position: refs/heads/master@{#424005}

--- chrome/android/java/src/org/chromium/chrome/browser/externalnav/ExternalNavigationHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/externalnav/ExternalNavigationHandler.java
@@ -48,6 +48,9 @@ public class ExternalNavigationHandler {
     @VisibleForTesting
     static final String EXTRA_BROWSER_FALLBACK_URL = "browser_fallback_url";
 
+    // Supervisor package name
+    private static final Object SUPERVISOR_PKG = "com.google.android.instantapps.supervisor";
+
     // An extra that may be specified on an intent:// URL that contains an encoded value for the
     // referrer field passed to the market:// URL in the case where the app is not present.
     @VisibleForTesting
@@ -259,7 +262,7 @@ public class ExternalNavigationHandler {
             // number=string(phone-number)
             mDelegate.startActivity(new Intent(Intent.ACTION_VIEW,
                     Uri.parse(WebView.SCHEME_TEL
-                            + params.getUrl().substring(SCHEME_WTAI_MC.length()))));
+                            + params.getUrl().substring(SCHEME_WTAI_MC.length()))), false);
             return OverrideUrlLoadingResult.OVERRIDE_WITH_EXTERNAL_INTENT;
         }
 
@@ -336,7 +339,7 @@ public class ExternalNavigationHandler {
                     if (params.getReferrerUrl() != null) {
                         intent.putExtra(Intent.EXTRA_REFERRER, Uri.parse(params.getReferrerUrl()));
                     }
-                    mDelegate.startActivity(intent);
+                    mDelegate.startActivity(intent, false);
                     return OverrideUrlLoadingResult.OVERRIDE_WITH_EXTERNAL_INTENT;
                 } catch (ActivityNotFoundException ex) {
                     // ignore the error on devices that does not have
@@ -389,7 +392,7 @@ public class ExternalNavigationHandler {
             if (!mDelegate.isSpecializedHandlerAvailable(resolvingInfos)) {
                 if (params.webApkPackageName() != null) {
                     intent.setPackage(mDelegate.getPackageName());
-                    mDelegate.startActivity(intent);
+                    mDelegate.startActivity(intent, false);
                     return OverrideUrlLoadingResult.OVERRIDE_WITH_EXTERNAL_INTENT;
                 }
 
@@ -438,13 +441,18 @@ public class ExternalNavigationHandler {
             }
         }
 
+        boolean shouldProxyForInstantApps = isExternalProtocol
+                && SUPERVISOR_PKG.equals(intent.getPackage())
+                && mDelegate.isSerpReferrer(params.getReferrerUrl(), params.getTab());
+
         try {
             if (params.isIncognito() && !mDelegate.willChromeHandleIntent(intent)) {
                 // This intent may leave Chrome.  Warn the user that incognito does not carry over
                 // to apps out side of Chrome.
                 mDelegate.startIncognitoIntent(intent, params.getReferrerUrl(),
                         hasBrowserFallbackUrl ? browserFallbackUrl : null, params.getTab(),
-                        params.shouldCloseContentsOnOverrideUrlLoadingAndLaunchIntent());
+                        params.shouldCloseContentsOnOverrideUrlLoadingAndLaunchIntent(),
+                        shouldProxyForInstantApps);
                 return OverrideUrlLoadingResult.OVERRIDE_WITH_ASYNC_ACTION;
             }
 
@@ -487,7 +495,7 @@ public class ExternalNavigationHandler {
                 }
             }
 
-            if (mDelegate.startActivityIfNeeded(intent)) {
+            if (mDelegate.startActivityIfNeeded(intent, shouldProxyForInstantApps)) {
                 return OverrideUrlLoadingResult.OVERRIDE_WITH_EXTERNAL_INTENT;
             }
 
