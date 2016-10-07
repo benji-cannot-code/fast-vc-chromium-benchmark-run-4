36b606494e4a0064c347c4d737fa6f860c651f42
xiaochengh <xiaochengh@chromium.org>
2016-10-07 06:05:38 -0700

    Remove MoveSelectionCommand
    MoveSelectionCommand is no longer in use after r422915. Hence, this
    patch removes it from the code base.
    
    BUG=n/a
    
    Review-Url: https://codereview.chromium.org/2403443002
    Cr-Commit-Position: refs/heads/master@{#423843}

--- third_party/WebKit/Source/core/editing/Editor.cpp
+++ third_party/WebKit/Source/core/editing/Editor.cpp
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/commands/DeleteSelectionCommand.h"
 #include "core/editing/commands/IndentOutdentCommand.h"
 #include "core/editing/commands/InsertListCommand.h"
-#include "core/editing/commands/MoveSelectionCommand.h"
 #include "core/editing/commands/RemoveFormatCommand.h"
 #include "core/editing/commands/ReplaceSelectionCommand.h"
 #include "core/editing/commands/SimplifyMarkupCommand.h"
