a0f2d1bc5e80db9b17090efdd8803305e5ea0679
valih <valih@google.com>
2016-10-06 19:34:30 -0700

    DevTools: add test for for page reload with screenshots not stopping recording
    BUG=569557
    
    Review-Url: https://codereview.chromium.org/2396883005
    Cr-Commit-Position: refs/heads/master@{#423784}

--- third_party/WebKit/Source/devtools/front_end/network/NetworkPanel.js
+++ third_party/WebKit/Source/devtools/front_end/network/NetworkPanel.js
@@ -106,6 +106,8 @@ WebInspector.NetworkPanel = function()
     WebInspector.DataSaverInfobar.maybeShowInPanel(this);
 }
 
+WebInspector.NetworkPanel.displayScreenshotDelay = 1000;
+
 WebInspector.NetworkPanel.prototype = {
     /**
      * @param {!WebInspector.Event} event
@@ -274,7 +276,7 @@ WebInspector.NetworkPanel.prototype = {
     _load: function(event)
     {
         if (this._filmStripRecorder && this._filmStripRecorder.isRecording())
-            this._pendingStopTimer = setTimeout(this._stopFilmStripRecording.bind(this), 1000);
+            this._pendingStopTimer = setTimeout(this._stopFilmStripRecording.bind(this), WebInspector.NetworkPanel.displayScreenshotDelay);
     },
 
     _stopFilmStripRecording: function()
