95b9b878ccf7809d4f7479c37ab59dbe1cb929d3
stanisc <stanisc@chromium.org>
2016-10-07 18:57:10 -0700

    Remove code that checks if MessagePumpForGpu was signaled.
    This reverts https://codereview.chromium.org/2077613002/.
    
    The check was supposed to tell us whether GPU process main
    thread's message pump was signaled at the time of the hang.
    In practice 100% of crash dumps had the negative result.
    After some additional research I realized that that was
    a false negative and that this check doesn't work as
    expected with auto-reset events. I confirmed that an
    auto-reset event gets promptly reset back to non-signaled
    when it gets signaled as long as there is at least one
    thread already waiting on it. That is the case even when
    when the target thread is never scheduled to run. The
    check would work with a manual-reset event but apparently
    it is useless in the case of an auto-reset event.
    
    BUG=620904
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2393333002
    Cr-Commit-Position: refs/heads/master@{#424040}

--- base/message_loop/message_pump.cc
+++ base/message_loop/message_pump.cc
@@ -16,11 +16,4 @@ MessagePump::~MessagePump() {
 void MessagePump::SetTimerSlack(TimerSlack) {
 }
 
-#if defined(OS_WIN)
-bool MessagePump::WasSignaled() {
-  NOTREACHED();
-  return false;
-}
-#endif
-
 }  // namespace base
