b837bc97d4e1de804e07215086c2f420db4bfd87
thomasanderson <thomasanderson@google.com>
2016-10-07 18:14:38 -0700

    Aura: Remove call to GetCursorScreenPoint during DND
    CL https://codereview.chromium.org/2398343002/ tries to avoid round-tripping to
    the X server to get the the pointer location when we're already responding to a
    pointer event.  This is one of 2 cases currently that is slipping through the
    cracks even with that CL applied.
    
    R=sky@chromium.org
    
    Review-Url: https://codereview.chromium.org/2401183002
    Cr-Commit-Position: refs/heads/master@{#424034}

--- content/browser/web_contents/web_contents_view_aura.cc
+++ content/browser/web_contents/web_contents_view_aura.cc
@@ -1150,7 +1150,7 @@ int WebContentsViewAura::OnDragUpdated(const ui::DropTargetEvent& event) {
     return ui::DragDropTypes::DRAG_NONE;
 
   blink::WebDragOperationsMask op = ConvertToWeb(event.source_operations());
-  gfx::Point screen_pt = display::Screen::GetScreen()->GetCursorScreenPoint();
+  gfx::Point screen_pt = event.root_location();
   web_contents_->GetRenderViewHost()->DragTargetDragOver(
       event.location(), screen_pt, op,
       ConvertAuraEventFlagsToWebInputEventModifiers(event.flags()));
