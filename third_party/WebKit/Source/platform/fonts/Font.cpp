0c54949e4af7e7d6f05175db674c74e4aefd39aa
debugwang <debugwang@tencent.com>
2016-10-08 21:22:24 -0700

    remove the useless head files
    HarfBuzz is not used in Font.cpp, can remove it
    
    BUG=none
    R=thakis@chromium.org
    
    Review-Url: https://codereview.chromium.org/2403633003
    Cr-Commit-Position: refs/heads/master@{#424097}

--- third_party/WebKit/Source/platform/fonts/Font.cpp
+++ third_party/WebKit/Source/platform/fonts/Font.cpp
@@ -36,8 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/fonts/GlyphPageTreeNode.h"
 #include "platform/fonts/SimpleFontData.h"
 #include "platform/fonts/shaping/CachingWordShaper.h"
-#include "platform/fonts/shaping/HarfBuzzFace.h"
-#include "platform/fonts/shaping/HarfBuzzShaper.h"
 #include "platform/fonts/shaping/SimpleShaper.h"
 #include "platform/geometry/FloatRect.h"
 #include "platform/text/BidiResolver.h"
