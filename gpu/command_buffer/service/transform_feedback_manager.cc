34bc29fd64d18445b6dddc2956af781a08cda4f9
zmo <zmo@chromium.org>
2016-10-10 16:22:51 -0700

    Fix an issue that caused lifetime.html to crash
    If we run lifetime.html on Linux (with virtual context) and then try to
    close chrome, chrome will crash with an assertion on an unexpected GL
    error. This CL fixed that.
    
    BUG=429053
    TEST=manual
    R=kbr@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2404023002
    Cr-Commit-Position: refs/heads/master@{#424280}

--- gpu/command_buffer/service/transform_feedback_manager.cc
+++ gpu/command_buffer/service/transform_feedback_manager.cc
@@ -27,6 +27,8 @@ TransformFeedback::TransformFeedback(TransformFeedbackManager* manager,
 
 TransformFeedback::~TransformFeedback() {
   if (!manager_->lost_context()) {
+    if (active_)
+      glEndTransformFeedback();
     glDeleteTransformFeedbacks(1, &service_id_);
   }
 }
