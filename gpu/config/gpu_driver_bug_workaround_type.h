ecfa881593de6a5b73bb5d23bba904bfbe351897
treib <treib@chromium.org>
2016-10-10 01:43:34 -0700

    Revert of Add isnan() workaround for Intel Mac (patchset #4 id:60001 of https://codereview.chromium.org/2376033004/ )
    Reason for revert:
    Seems to have broken NavigatingExtensionPopupBrowserTest.DownloadViaPost on Mac 10.10:
    https://build.chromium.org/p/chromium.mac/builders/Mac10.10%20Tests/builds/8270
    
    Original issue's description:
    > Add isnan() workaround for Intel Mac
    >
    > On some Intel drivers (skylake Windows and Mac), calling
    > function isnan() on highp float will get wrong answer.
    > This patch work arounds this bug by using an expression
    > to emulate this GLSL function. Skylake device ids are
    > referenced from:
    > https://cgit.freedesktop.org/vaapi/intel-driver/tree/src/i965_pciids.h
    >
    > BUG=650547
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    >
    > Committed: https://crrev.com/9358866a937bc8cb9facb7feb9d087fab355e459
    > Cr-Commit-Position: refs/heads/master@{#424101}
    
    TBR=qiankun.miao@intel.com,kbr@chromium.org,zmo@chromium.org,jiawei.shao@intel.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=650547
    
    Review-Url: https://codereview.chromium.org/2405773002
    Cr-Commit-Position: refs/heads/master@{#424121}

--- gpu/config/gpu_driver_bug_workaround_type.h
+++ gpu/config/gpu_driver_bug_workaround_type.h
@@ -86,8 +86,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
          etc1_power_of_two_only)                             \
   GPU_OP(EMULATE_ABS_INT_FUNCTION,                           \
          emulate_abs_int_function)                           \
-  GPU_OP(EMULATE_ISNAN_ON_FLOAT,                             \
-         emulate_isnan_on_float)                             \
   GPU_OP(EXIT_ON_CONTEXT_LOST,                               \
          exit_on_context_lost)                               \
   GPU_OP(FORCE_CUBE_COMPLETE,                                \
