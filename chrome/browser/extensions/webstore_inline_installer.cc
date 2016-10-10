dfb2bba91c63afbcdba205b28b1f06ed61e8feab
ackermanb <ackermanb@chromium.org>
2016-10-10 14:45:47 -0700

    Prevent inline install on fullscreen windows.
    BUG=488143
    
    Review-Url: https://codereview.chromium.org/2391353003
    Cr-Commit-Position: refs/heads/master@{#424254}

--- chrome/browser/extensions/webstore_inline_installer.cc
+++ chrome/browser/extensions/webstore_inline_installer.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_finder.h"
+#include "chrome/browser/ui/exclusive_access/fullscreen_controller.h"
 #include "content/public/browser/navigation_details.h"
 #include "content/public/browser/web_contents.h"
 
@@ -28,6 +29,8 @@ const char kInlineInstallSupportedError[] =
     "redirected to the Chrome Web Store.";
 const char kInitiatedFromPopupError[] =
     "Inline installs can not be initiated from pop-up windows.";
+const char kInitiatedFromFullscreenError[] =
+    "Inline installs can not be initiated from fullscreen.";
 
 WebstoreInlineInstaller::WebstoreInlineInstaller(
     content::WebContents* web_contents,
@@ -137,6 +140,12 @@ bool WebstoreInlineInstaller::CheckInlineInstallPermitted(
   DCHECK(browser);
   if (browser->is_type_popup()) {
     *error = kInitiatedFromPopupError;
+    return false;
+  }
+  FullscreenController* controller =
+      browser->exclusive_access_manager()->fullscreen_controller();
+  if (controller->IsFullscreenForBrowser() || controller->IsTabFullscreen()) {
+    *error = kInitiatedFromFullscreenError;
     return false;
   }
   // The store may not support inline installs for this item, in which case
