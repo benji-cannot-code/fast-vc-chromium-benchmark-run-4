010d8ed020e92414e11c5cf71093218ac4984964
alph <alph@chromium.org>
2016-10-10 13:34:17 -0700

    Enable v8::TracingCpuProfiler in Blink.
    BUG=406277
    
    Review-Url: https://codereview.chromium.org/2397643002
    Cr-Commit-Position: refs/heads/master@{#424225}

--- third_party/WebKit/Source/core/inspector/ThreadDebugger.h
+++ third_party/WebKit/Source/core/inspector/ThreadDebugger.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/Vector.h"
 #include <memory>
 #include <v8-inspector.h>
+#include <v8-profiler.h>
 #include <v8.h>
 
 namespace blink {
@@ -104,6 +105,7 @@ class CORE_EXPORT ThreadDebugger : public v8_inspector::V8InspectorClient {
       const v8::FunctionCallbackInfo<v8::Value>&);
 
   std::unique_ptr<v8_inspector::V8Inspector> m_v8Inspector;
+  std::unique_ptr<v8::TracingCpuProfiler> m_v8TracingCpuProfiler;
   Vector<std::unique_ptr<Timer<ThreadDebugger>>> m_timers;
   Vector<v8_inspector::V8InspectorClient::TimerCallback> m_timerCallbacks;
   Vector<void*> m_timerData;
