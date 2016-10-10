3846bbfedefcfc2a030900ed8e5d6ebb7ac69a01
treib <treib@chromium.org>
2016-10-10 02:24:46 -0700

    [NTP Snippets] Persist remote categories in prefs
    This is the first part of persisting non-article remote suggestions.
    The next part will be persisting the suggestions themselves.
    
    BUG=653476
    
    Review-Url: https://codereview.chromium.org/2395273003
    Cr-Commit-Position: refs/heads/master@{#424122}

--- components/ntp_snippets/pref_names.h
+++ components/ntp_snippets/pref_names.h
@@ -9,9 +9,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ntp_snippets {
 namespace prefs {
 
+// If set to false, remote suggestions are completely disabled. This is set by
+// an enterprise policy.
 extern const char kEnableSnippets[];
 
+// TODO(treib): Remove this after M56.
 extern const char kSnippetHosts[];
+
+// The pref name under which remote suggestion categories (including their ID
+// and title) are stored.
+extern const char kRemoteSuggestionCategories[];
 
 // The pref name for the currently-scheduled background fetching interval when
 // there is WiFi connectivity.
