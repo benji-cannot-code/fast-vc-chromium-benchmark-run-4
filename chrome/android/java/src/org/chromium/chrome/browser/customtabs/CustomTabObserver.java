ac2be07597dbfa18f79745b8bfc2d07f6d97d022
yusufo <yusufo@chromium.org>
2016-10-10 13:57:57 -0700

    Make sure one navigation info per tab is captured
    Rather than only checking on pageLoadFinished for capturing navigation
    info, this also adds a check for onHidden, to make sure that one
    navigation info per tab is captured. In cases where we abort the load.
    we send the url and title only.
    
    BUG=647342
    
    Review-Url: https://codereview.chromium.org/2395933002
    Cr-Commit-Position: refs/heads/master@{#424234}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabObserver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabObserver.java
@@ -36,6 +36,8 @@ class CustomTabObserver extends EmptyTabObserver {
     private long mIntentReceivedTimestamp;
     private long mPageLoadStartedTimestamp;
 
+    private boolean mScreenshotTakenForCurrentNavigation;
+
     private static final int STATE_RESET = 0;
     private static final int STATE_WAITING_LOAD_START = 1;
     private static final int STATE_WAITING_LOAD_FINISH = 2;
@@ -86,12 +88,15 @@ class CustomTabObserver extends EmptyTabObserver {
             if (mCustomTabsConnection != null) {
                 mCustomTabsConnection.notifyNavigationEvent(
                         mSession, CustomTabsCallback.NAVIGATION_ABORTED);
+                mCustomTabsConnection.sendNavigationInfo(
+                        mSession, tab.getUrl(), tab.getTitle(), null);
             }
             mPageLoadStartedTimestamp = SystemClock.elapsedRealtime();
         }
         if (mCustomTabsConnection != null) {
             mCustomTabsConnection.notifyNavigationEvent(
                     mSession, CustomTabsCallback.NAVIGATION_STARTED);
+            mScreenshotTakenForCurrentNavigation = false;
         }
     }
 
@@ -101,6 +106,11 @@ class CustomTabObserver extends EmptyTabObserver {
             mCustomTabsConnection.notifyNavigationEvent(
                     mSession, CustomTabsCallback.TAB_SHOWN);
         }
+    }
+
+    @Override
+    public void onHidden(Tab tab) {
+        if (!mScreenshotTakenForCurrentNavigation) captureNavigationInfo(tab);
     }
 
     @Override
@@ -169,5 +179,6 @@ class CustomTabObserver extends EmptyTabObserver {
         };
         tab.getWebContents().getContentBitmapAsync(
                 Bitmap.Config.ARGB_8888, mScaleForNavigationInfo, new Rect(), callback);
+        mScreenshotTakenForCurrentNavigation = true;
     }
 }
