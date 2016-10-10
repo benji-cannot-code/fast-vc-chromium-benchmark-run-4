57d0ae708ce52c7fc826b0f4af1b254124cd91bf
bradnelson <bradnelson@chromium.org>
2016-10-10 11:31:14 -0700

    [asm.js] [wasm] Move asm->wasm + WebAssembly switches to features.
    Moving asm->wasm and webassembly to a feature will make them
    ready to control from a Finch trial with kill-switch.
    
    NOTE: This will mean --enable-wasm will be replaced with:
    --enable-features=WebAssembly
    and --enable-asm-wasm will be replaced with:
    --enable-features=AsmJsToWebAssembly
    
    BUG= https://bugs.chromium.org/p/v8/issues/detail?id=4203
    R=asvitkine@chromium.org,jam@chromium.org,titzer@chromium.org,mtrofin@chromium.org
    
    Review-Url: https://codereview.chromium.org/2400823002
    Cr-Commit-Position: refs/heads/master@{#424193}

--- content/public/common/content_features.h
+++ content/public/common/content_features.h
@@ -17,6 +17,7 @@ namespace features {
 
 // All features in alphabetical order. The features should be documented
 // alongside the definition of their values in the .cc file.
+CONTENT_EXPORT extern const base::Feature kAsmJsToWebAssembly;
 CONTENT_EXPORT extern const base::Feature kBrotliEncoding;
 CONTENT_EXPORT extern const base::Feature kCanvas2DImageChromium;
 CONTENT_EXPORT extern const base::Feature kCompositeOpaqueScrollers;
@@ -50,6 +51,7 @@ CONTENT_EXPORT extern const base::Feature kStaleWhileRevalidate;
 CONTENT_EXPORT extern const base::Feature kTimerThrottlingForHiddenFrames;
 CONTENT_EXPORT extern const base::Feature kTokenBinding;
 CONTENT_EXPORT extern const base::Feature kTouchpadAndWheelScrollLatching;
+CONTENT_EXPORT extern const base::Feature kWebAssembly;
 CONTENT_EXPORT extern const base::Feature kWebGLImageChromium;
 CONTENT_EXPORT extern const base::Feature kWebRtcEcdsaDefault;
 CONTENT_EXPORT extern const base::Feature kWebRtcUseGpuMemoryBufferVideoFrames;
--- third_party/WebKit/LayoutTests/virtual/enable_wasm/README.txt
+++ third_party/WebKit/LayoutTests/virtual/enable_wasm/README.txt
@@ -1,2 +1,2 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-Tests that depend on --enable-wasm
+Tests that depend on --enable-features=WebAssembly
