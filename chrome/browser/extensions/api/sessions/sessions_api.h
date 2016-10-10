594fee96a5129d40c963b7964002ec9d31047a83
rdevlin.cronin <rdevlin.cronin@chromium.org>
2016-10-10 13:20:21 -0700

    [Extensions] Convert some ChromeSyncExtensionFunctions
    SyncExtensionFunctions, AsyncExtensionFunctions,
    ChromeSyncExtensionFunctions, and ChromeAsyncExtensionFunctions are
    deprecated.
    
    Remove ChromeSyncExtensionFunctions from:
    - preference
    - sessions
    
    BUG=634140
    
    Review-Url: https://codereview.chromium.org/2408763002
    Cr-Commit-Position: refs/heads/master@{#424219}

--- chrome/browser/extensions/api/sessions/sessions_api.h
+++ chrome/browser/extensions/api/sessions/sessions_api.h
@@ -28,10 +28,10 @@ namespace extensions {
 
 class SessionId;
 
-class SessionsGetRecentlyClosedFunction : public ChromeSyncExtensionFunction {
+class SessionsGetRecentlyClosedFunction : public UIThreadExtensionFunction {
  protected:
   ~SessionsGetRecentlyClosedFunction() override {}
-  bool RunSync() override;
+  ResponseAction Run() override;
   DECLARE_EXTENSION_FUNCTION("sessions.getRecentlyClosed",
                              SESSIONS_GETRECENTLYCLOSED)
 
@@ -44,10 +44,10 @@ class SessionsGetRecentlyClosedFunction : public ChromeSyncExtensionFunction {
       const sessions::TabRestoreService::Entry& entry);
 };
 
-class SessionsGetDevicesFunction : public ChromeSyncExtensionFunction {
+class SessionsGetDevicesFunction : public UIThreadExtensionFunction {
  protected:
   ~SessionsGetDevicesFunction() override {}
-  bool RunSync() override;
+  ResponseAction Run() override;
   DECLARE_EXTENSION_FUNCTION("sessions.getDevices", SESSIONS_GETDEVICES)
 
  private:
@@ -65,20 +65,20 @@ class SessionsGetDevicesFunction : public ChromeSyncExtensionFunction {
       const sync_sessions::SyncedSession* session);
 };
 
-class SessionsRestoreFunction : public ChromeSyncExtensionFunction {
+class SessionsRestoreFunction : public UIThreadExtensionFunction {
  protected:
   ~SessionsRestoreFunction() override {}
-  bool RunSync() override;
+  ResponseAction Run() override;
   DECLARE_EXTENSION_FUNCTION("sessions.restore", SESSIONS_RESTORE)
 
  private:
-  void SetInvalidIdError(const std::string& invalid_id);
-  void SetResultRestoredTab(content::WebContents* contents);
-  bool SetResultRestoredWindow(int window_id);
-  bool RestoreMostRecentlyClosed(Browser* browser);
-  bool RestoreLocalSession(const SessionId& session_id, Browser* browser);
-  bool RestoreForeignSession(const SessionId& session_id,
-                             Browser* browser);
+  ResponseValue GetRestoredTabResult(content::WebContents* contents);
+  ResponseValue GetRestoredWindowResult(int window_id);
+  ResponseValue RestoreMostRecentlyClosed(Browser* browser);
+  ResponseValue RestoreLocalSession(const SessionId& session_id,
+                                    Browser* browser);
+  ResponseValue RestoreForeignSession(const SessionId& session_id,
+                                      Browser* browser);
 };
 
 class SessionsEventRouter : public sessions::TabRestoreServiceObserver {
