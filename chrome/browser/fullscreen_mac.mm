6903e7a07c3e1e025b22629fa82a8eab169ddf87
tapted <tapted@chromium.org>
2016-10-10 15:56:14 -0700

    Mac: Remove deprecated call to CGDisplayIsCaptured IsFullScreenMode().
    The call was added in r72539 to ensure notifications are not shown when
    another app has fullscreen (e.g. games or the screensaver).
    
    r178470 added additional calls using Cocoa APIs which made it redundant,
    so just delete the call to CGDisplayIsCaptured().
    
    Verified that attempts to show notifications when running an old
    fullscreen game or with the screensaver active are correctly suppressed.
    Traced to ensure CGDisplayIsCaptured() would have been responsible for
    doing the same in these cases.
    
    BUG=650849
    
    Review-Url: https://codereview.chromium.org/2406753003
    Cr-Commit-Position: refs/heads/master@{#424272}

--- chrome/browser/fullscreen_mac.mm
+++ chrome/browser/fullscreen_mac.mm
@@ -10,10 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 
 bool IsFullScreenMode() {
-  // Check if the main display has been captured (by games in particular).
-  if (CGDisplayIsCaptured(CGMainDisplayID()))
-    return true;
-
   NSApplicationPresentationOptions options =
       [NSApp currentSystemPresentationOptions];
 
