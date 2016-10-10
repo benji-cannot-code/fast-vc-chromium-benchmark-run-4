b4f723986df71723b755ff31aacb8b1f6ba9d9f2
ellyjones <ellyjones@chromium.org>
2016-10-10 08:40:26 -0700

    cocoa browser: add accessible tooltip for security state
    The accessible tooltip is: "Label text. View site information, button", where
    the label text is either "Secure" for regular SSL sites, or the origin's EV
    name for EV sites.
    
    BUG=447191
    
    Review-Url: https://codereview.chromium.org/2407853002
    Cr-Commit-Position: refs/heads/master@{#424155}

--- chrome/browser/ui/cocoa/location_bar/security_state_bubble_decoration.mm
+++ chrome/browser/ui/cocoa/location_bar/security_state_bubble_decoration.mm
@@ -12,9 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/location_bar/location_bar_view_mac.h"
 #import "chrome/browser/ui/cocoa/location_bar/location_icon_decoration.h"
 #import "chrome/browser/ui/cocoa/themed_window.h"
+#include "chrome/grit/generated_resources.h"
 #include "chrome/grit/theme_resources.h"
 #include "skia/ext/skia_utils_mac.h"
 #import "ui/base/cocoa/nsview_additions.h"
+#include "ui/base/l10n/l10n_util_mac.h"
 #include "ui/base/material_design/material_design_controller.h"
 #include "ui/gfx/animation/tween.h"
 #include "ui/gfx/color_palette.h"
@@ -289,6 +291,11 @@ NSPoint SecurityStateBubbleDecoration::GetBubblePointInFrame(NSRect frame) {
   NSRect image_rect = GetImageRectInFrame(frame);
   return NSMakePoint(NSMidX(image_rect),
                      NSMaxY(image_rect) - kPageInfoBubblePointYOffset);
+}
+
+NSString* SecurityStateBubbleDecoration::GetToolTip() {
+  return [NSString stringWithFormat:@"%@. %@", full_label_.get(),
+                   l10n_util::GetNSStringWithFixup(IDS_TOOLTIP_LOCATION_ICON)];
 }
 
 //////////////////////////////////////////////////////////////////
