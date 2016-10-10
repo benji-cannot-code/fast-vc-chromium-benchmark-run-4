5dd92f29f4f8a741854de9c3285b9330ebd71964
pkotwicz <pkotwicz@chromium.org>
2016-10-10 07:47:08 -0700

    Add log with result of WebAPK update check to make testing easier
    The intent of this CL is to make it easier to test the update logic
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2407693002
    Cr-Commit-Position: refs/heads/master@{#424150}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkUpdateManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkUpdateManager.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 
 import org.chromium.base.CommandLine;
 import org.chromium.base.ContextUtils;
+import org.chromium.base.Log;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.tab.Tab;
@@ -28,6 +29,8 @@ import java.util.concurrent.TimeUnit;
  * an update request to the WebAPK Server when an update is needed.
  */
 public class WebApkUpdateManager implements ManifestUpgradeDetector.Callback {
+    private static final String TAG = "WebApkUpdateManager";
+
     /** Number of milliseconds between checks for whether the WebAPK's Web Manifest has changed. */
     private static final long FULL_CHECK_UPDATE_INTERVAL = TimeUnit.DAYS.toMillis(3L);
 
@@ -101,7 +104,9 @@ public class WebApkUpdateManager implements ManifestUpgradeDetector.Callback {
     @Override
     public void onUpgradeNeededCheckFinished(boolean needsUpgrade,
             ManifestUpgradeDetector.FetchedManifestData data) {
-        if (needsUpgrade || mForceUpgrade) {
+        needsUpgrade = (needsUpgrade || mForceUpgrade);
+        Log.v(TAG, "WebAPK upgrade needed: " + needsUpgrade);
+        if (needsUpgrade) {
             updateAsync(data);
         }
         if (mUpgradeDetector != null) {
