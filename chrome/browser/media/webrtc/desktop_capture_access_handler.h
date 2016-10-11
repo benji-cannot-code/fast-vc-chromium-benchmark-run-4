5759a0fe7e64bb44cbfa69751ac59df78ab780e9
mfoltz <mfoltz@chromium.org>
2016-10-11 10:42:42 -0700

    Updates desktop_capture_access_handler to bypass prompts for external components (including the Media Router) when the capture id is obtained through the capture picker.
    Simplify the logic for determining which desktop capture requests are
    default approved.
    
    Update desktop capture resource strings to remove references to
    the Google Cast extension.
    
    BUG=567443
    
    Review-Url: https://codereview.chromium.org/2400803003
    Cr-Commit-Position: refs/heads/master@{#424476}

--- chrome/browser/media/webrtc/desktop_capture_access_handler.h
+++ chrome/browser/media/webrtc/desktop_capture_access_handler.h
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/media/capture_access_handler_base.h"
 #include "chrome/browser/media/media_access_handler.h"
 
+namespace extensions {
+class Extension;
+}
+
 // MediaAccessHandler for DesktopCapture API.
 class DesktopCaptureAccessHandler : public CaptureAccessHandlerBase {
  public:
@@ -37,6 +41,10 @@ class DesktopCaptureAccessHandler : public CaptureAccessHandlerBase {
       const content::MediaResponseCallback& callback,
       const extensions::Extension* extension);
 
+  // Returns whether desktop capture is always approved for |extension|.
+  // Currently component extensions and some whitelisted extensions are default
+  // approved.
+  static bool IsDefaultApproved(const extensions::Extension* extension);
 };
 
 #endif  // CHROME_BROWSER_MEDIA_WEBRTC_DESKTOP_CAPTURE_ACCESS_HANDLER_H_
