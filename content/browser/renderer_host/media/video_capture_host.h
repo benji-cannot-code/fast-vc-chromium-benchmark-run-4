d1d73089edc089948a7b3bb7029271938a226115
mcasas <mcasas@chromium.org>
2016-10-11 13:00:46 -0700

    VideoCapture: more migration IPC-->mojo, part 5
    This CL migrates the video capture messages
    VideoCaptureMsg_BufferReady and FreeBuffer
    to mojom.
    
    This step of the migration was quite straightforward,
    most of the +- lines come from unittest adjustments,
    and also I threw away some unused code in
    video_capture_host_unittest.cc.
    
    BUG=651897
    
    Review-Url: https://codereview.chromium.org/2409893003
    Cr-Commit-Position: refs/heads/master@{#424520}

--- content/browser/renderer_host/media/video_capture_host.h
+++ content/browser/renderer_host/media/video_capture_host.h
@@ -20,27 +20,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //      | < VideoCaptureMsg_NewBuffer(2)         |
 //      | < VideoCaptureMsg_NewBuffer(3)         |
 //      |                                        |
-//      | < VideoCaptureMsg_BufferReady(1)       |
-//      | < VideoCaptureMsg_BufferReady(2)       |
+//      | <-------- OnBufferReady(1) ---------   |
+//      | <-------- OnBufferReady(2) ---------   |
 //      | -------- ReleaseBuffer(1) --------->   |
-//      | < VideoCaptureMsg_BufferReady(3)       |
+//      | <-------- OnBufferReady(3) ---------   |
 //      | -------- ReleaseBuffer(2) --------->   |
-//      | < VideoCaptureMsg_BufferReady(1)       |
+//      | <-------- OnBufferReady(1) ---------   |
 //      | -------- ReleaseBuffer(3) --------->   |
-//      | < VideoCaptureMsg_BufferReady(2)       |
+//      | <-------- OnBufferReady(2) ---------   |
 //      | -------- ReleaseBuffer(1) --------->   |
 //      |             ...                        |
-//      | < VideoCaptureMsg_BufferReady(3)       |
-//      |                                        |
+//      | <-------- OnBufferReady(3) ---------   |
+//      =                                        =
 //      |             ... (resolution change)    |
-//      | < VideoCaptureMsg_FreeBuffer(1)        |  Buffers are re-allocated
+//      | <------ OnBufferDestroyed(3) -------   |  Buffers are re-allocated
 //      | < VideoCaptureMsg_NewBuffer(4)         |  with a larger size, as
-//      | < VideoCaptureMsg_BufferReady(4)       |  needed.
+//      | <-------- OnBufferReady(4) ---------   |  needed.
 //      | -------- ReleaseBuffer(2) --------->   |
-//      | < VideoCaptureMsg_FreeBuffer(2)        |
+//      | <------ OnBufferDestroyed(2) -------   |
 //      | < VideoCaptureMsg_NewBuffer(5)         |
-//      | < VideoCaptureMsg_BufferReady(5)       |
-//      |             ...                        |
+//      | <-------- OnBufferReady(5) ---------   |
+//      =             ...                        =
 //      |                                        |
 //      | < VideoCaptureMsg_BufferReady          |
 //      |  --------- StopCapture --------->      |
