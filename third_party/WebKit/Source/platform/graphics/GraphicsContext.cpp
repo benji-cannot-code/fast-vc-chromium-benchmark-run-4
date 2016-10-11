918acbae4d8d798b162d037603ab1650977f0b4e
bsep <bsep@chromium.org>
2016-10-11 16:35:46 -0700

    Scale focus outline thickness with zoom level.
    Focus rings were only being scaled by the dsf, which after the launch of
    use-zoom-for-dsf meant that they weren't being scaled at all. It seems
    like we should keep the ring in proportion with the rest of the page, so
    I changed it to scale with the total zoom level. In order to keep the
    ring from vanishing when zoomed out, I floored its thickness at 1px.
    
    Focus rings draw the same at 1x device scale. A subtle effect of this
    patch is that elements will have their focus rings scaled by the css
    zoom property.
    
    BUG=651510
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2386033002
    Cr-Commit-Position: refs/heads/master@{#424596}

--- third_party/WebKit/Source/platform/graphics/GraphicsContext.cpp
+++ third_party/WebKit/Source/platform/graphics/GraphicsContext.cpp
@@ -330,18 +330,18 @@ void GraphicsContext::compositePicture(sk_sp<SkPicture> picture,
 
 void GraphicsContext::drawFocusRingPath(const SkPath& path,
                                         const Color& color,
-                                        int width) {
+                                        float width) {
   drawPlatformFocusRing(path, m_canvas, color.rgb(), width);
 }
 
 void GraphicsContext::drawFocusRingRect(const SkRect& rect,
                                         const Color& color,
-                                        int width) {
+                                        float width) {
   drawPlatformFocusRing(rect, m_canvas, color.rgb(), width);
 }
 
 void GraphicsContext::drawFocusRing(const Path& focusRingPath,
-                                    int width,
+                                    float width,
                                     int offset,
                                     const Color& color) {
   // FIXME: Implement support for offset.
@@ -352,7 +352,7 @@ void GraphicsContext::drawFocusRing(const Path& focusRingPath,
 }
 
 void GraphicsContext::drawFocusRing(const Vector<IntRect>& rects,
-                                    int width,
+                                    float width,
                                     int offset,
                                     const Color& color) {
   if (contextDisabled())
