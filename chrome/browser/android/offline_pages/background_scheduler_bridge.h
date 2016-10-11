22fa986850797d6af03bbe301498b747c0362808
petewil <petewil@chromium.org>
2016-10-10 17:57:25 -0700

    Set up a backup schedule request if we have deferred requests.
    BUG=652889
    
    Review-Url: https://codereview.chromium.org/2400393002
    Cr-Commit-Position: refs/heads/master@{#424315}

--- chrome/browser/android/offline_pages/background_scheduler_bridge.h
+++ chrome/browser/android/offline_pages/background_scheduler_bridge.h
@@ -23,6 +23,8 @@ class BackgroundSchedulerBridge : public Scheduler {
  public:
   // Scheduler implementation.
   void Schedule(const TriggerConditions& trigger_conditions) override;
+  void BackupSchedule(const TriggerConditions& trigger_conditions,
+                      long delay_in_seconds) override;
   void Unschedule() override;
 
  private:
@@ -39,4 +41,3 @@ bool RegisterBackgroundSchedulerBridge(JNIEnv* env);
 }  // namespace offline_pages
 
 #endif  // CHROME_BROWSER_ANDROID_OFFLINE_PAGES_BACKGROUND_SCHEDULER_BRIDGE_H_
-
