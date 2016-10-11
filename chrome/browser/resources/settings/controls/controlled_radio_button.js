3614680a5c35b12057a9376dca806b989fc15215
hcarmona <hcarmona@chromium.org>
2016-10-11 15:38:58 -0700

    Move tab index and focus to radio button.
    BUG=646284
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2389203003
    Cr-Commit-Position: refs/heads/master@{#424575}

--- chrome/browser/resources/settings/controls/controlled_radio_button.js
+++ chrome/browser/resources/settings/controls/controlled_radio_button.js
@@ -22,6 +22,10 @@ Polymer({
     },
   },
 
+  listeners: {
+    'focus': 'onFocus_',
+  },
+
   /**
    * @return {boolean} Whether the button is disabled.
    * @private
@@ -49,5 +53,10 @@ Polymer({
     // Disallow <controlled-radio-button on-tap="..."> when controlled.
     e.preventDefault();
     e.stopPropagation();
+  },
+
+  /** Focuses the internal radio button when the row is selected. */
+  onFocus_: function() {
+    this.$.radioButton.focus();
   },
 });
