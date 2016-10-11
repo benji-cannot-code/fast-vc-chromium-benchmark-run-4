b9009c33e164fe6677ce6e2d7589c71f6f7f9803
petewil <petewil@chromium.org>
2016-10-10 19:37:42 -0700

    Implement MarkRequestCompleted.
    When the foreground tab finishes a request, it will let us know, and we
    need to send out the observer notification.  This code catches the
    foreground request completing, and calls the observer with SUCCESS
    after removing the request, and starting the next available request.
    
    BUG=652889
    
    Review-Url: https://codereview.chromium.org/2409823002
    Cr-Commit-Position: refs/heads/master@{#424346}

--- components/offline_pages/background/request_coordinator.h
+++ components/offline_pages/background/request_coordinator.h
@@ -231,6 +231,8 @@ class RequestCoordinator : public KeyedService,
   void UpdateMultipleRequestsCallback(
       std::unique_ptr<UpdateRequestsResult> result);
 
+  void CompletedRequestCallback(const MultipleItemStatuses& status);
+
   void HandleRemovedRequestsAndCallback(
       const RemoveRequestsCallback& callback,
       BackgroundSavePageResult status,
