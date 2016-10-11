9cafa8c392c543d513e83786a106d256cdae5c47
thakis <thakis@chromium.org>
2016-10-11 16:53:40 -0700

    Reformat a file
    https://codereview.chromium.org/2401523002 landed before
    clang-formatting was enforced in core/.  Since it's the
    only change that happens when I re-reformat all of blink,
    let's check this in. No behavior change.
    
    BUG=563793
    
    Review-Url: https://codereview.chromium.org/2405873003
    Cr-Commit-Position: refs/heads/master@{#424602}

--- third_party/WebKit/Source/core/dom/SpaceSplitString.h
+++ third_party/WebKit/Source/core/dom/SpaceSplitString.h
@@ -57,9 +57,7 @@ class SpaceSplitString {
 
   size_t size() const { return m_data ? m_data->size() : 0; }
   bool isNull() const { return !m_data; }
-  const AtomicString& operator[](size_t i) const {
-    return (*m_data)[i];
-  }
+  const AtomicString& operator[](size_t i) const { return (*m_data)[i]; }
 
  private:
   class Data : public RefCounted<Data> {
@@ -85,9 +83,7 @@ class SpaceSplitString {
 
     bool isUnique() const { return m_keyString.isNull(); }
     size_t size() const { return m_vector.size(); }
-    const AtomicString& operator[](size_t i) {
-      return m_vector[i];
-    }
+    const AtomicString& operator[](size_t i) { return m_vector[i]; }
 
    private:
     explicit Data(const AtomicString&);
