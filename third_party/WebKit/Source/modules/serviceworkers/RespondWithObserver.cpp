b73eabf755b832f02bc27d081f6ee4ebf0cf234c
horo <horo@chromium.org>
2016-10-11 01:15:42 -0700

    Remove WaitUntilObserver::decrementPendingActivity() from RespondWithObserver::contextDestroyed()
    We don't need to (and should not) call this while the ServiceWorker context is being destroyed.
    
    And also this caused crashes in ServiceWorkerContextClient after mojofying FetchEvent.
    (crbug.com/653403) This crash only happens when LifecycleNotifier::notifyContextDestroyed() calls
    RespondWithObserver::contextDestroyed() before WaitUntilObserver::contextDestroyed().
    
    BUG=653403,649558,629701
    
    Review-Url: https://codereview.chromium.org/2396293002
    Cr-Commit-Position: refs/heads/master@{#424390}

--- third_party/WebKit/Source/modules/serviceworkers/RespondWithObserver.cpp
+++ third_party/WebKit/Source/modules/serviceworkers/RespondWithObserver.cpp
@@ -189,7 +189,6 @@ void RespondWithObserver::contextDestroyed() {
   ContextLifecycleObserver::contextDestroyed();
   if (m_observer) {
     DCHECK_EQ(Pending, m_state);
-    m_observer->decrementPendingActivity();
     m_observer.clear();
   }
   m_state = Done;
