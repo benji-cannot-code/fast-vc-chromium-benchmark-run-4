2b38f3651b882837100059af2dd3a478cba88c2b
eae <eae@chromium.org>
2016-10-11 11:57:19 -0700

    Reduce code duplication in LayoutInline::generateCulledLineBoxRects
    Reduce code duplication by moving common logic out into a shared helper
    function, computeItemTopHeight, that computes the logical top & height.
    
    R=pdr@chromium.org
    
    Review-Url: https://codereview.chromium.org/2409233002
    Cr-Commit-Position: refs/heads/master@{#424498}

--- third_party/WebKit/Source/core/layout/LayoutInline.cpp
+++ third_party/WebKit/Source/core/layout/LayoutInline.cpp
@@ -627,6 +627,17 @@ void LayoutInline::generateLineBoxRects(GeneratorContext& yield) const {
   }
 }
 
+static inline void computeItemTopHeight(const LayoutInline* container,
+                                        const RootInlineBox& rootBox,
+                                        LayoutUnit* top,
+                                        LayoutUnit* height) {
+  bool firstLine = rootBox.isFirstLineStyle();
+  auto metrics = rootBox.getLineLayoutItem().style(firstLine)->getFontMetrics();
+  auto containerMetrics = container->style(firstLine)->getFontMetrics();
+  *top = rootBox.logicalTop() + (metrics.ascent() - containerMetrics.ascent());
+  *height = LayoutUnit(containerMetrics.height());
+}
+
 template <typename GeneratorContext>
 void LayoutInline::generateCulledLineBoxRects(
     GeneratorContext& yield,
@@ -636,6 +647,7 @@ void LayoutInline::generateCulledLineBoxRects(
 
   bool isHorizontal = style()->isHorizontalWritingMode();
 
+  LayoutUnit logicalTop, logicalHeight;
   for (LayoutObject* curr = firstChild(); curr; curr = curr->nextSibling()) {
     if (curr->isFloatingOrOutOfFlowPositioned())
       continue;
@@ -647,32 +659,16 @@ void LayoutInline::generateCulledLineBoxRects(
       LayoutBox* currBox = toLayoutBox(curr);
       if (currBox->inlineBoxWrapper()) {
         RootInlineBox& rootBox = currBox->inlineBoxWrapper()->root();
-        LayoutUnit logicalTop =
-            rootBox.logicalTop() + (rootBox.getLineLayoutItem()
-                                        .style(rootBox.isFirstLineStyle())
-                                        ->font()
-                                        .getFontMetrics()
-                                        .ascent() -
-                                    container->style(rootBox.isFirstLineStyle())
-                                        ->font()
-                                        .getFontMetrics()
-                                        .ascent());
-        LayoutUnit logicalHeight(container->style(rootBox.isFirstLineStyle())
-                                     ->font()
-                                     .getFontMetrics()
-                                     .height());
+        computeItemTopHeight(container, rootBox, &logicalTop, &logicalHeight);
         if (isHorizontal) {
           yield(LayoutRect(
-              LayoutUnit(currBox->inlineBoxWrapper()->x() -
-                         currBox->marginLeft()),
-              LayoutUnit(logicalTop),
-              LayoutUnit(currBox->size().width() + currBox->marginWidth()),
-              LayoutUnit(logicalHeight)));
+              currBox->inlineBoxWrapper()->x() - currBox->marginLeft(),
+              logicalTop, currBox->size().width() + currBox->marginWidth(),
+              logicalHeight));
         } else {
-          yield(LayoutRect(LayoutUnit(logicalTop),
-                           LayoutUnit(currBox->inlineBoxWrapper()->y() -
-                                      currBox->marginTop()),
-                           LayoutUnit(logicalHeight),
+          yield(LayoutRect(logicalTop, currBox->inlineBoxWrapper()->y() -
+                                           currBox->marginTop(),
+                           logicalHeight,
                            currBox->size().height() + currBox->marginHeight()));
         }
       }
@@ -685,34 +681,18 @@ void LayoutInline::generateCulledLineBoxRects(
         for (InlineFlowBox* childLine = currInline->firstLineBox(); childLine;
              childLine = childLine->nextLineBox()) {
           RootInlineBox& rootBox = childLine->root();
-          LayoutUnit logicalTop = rootBox.logicalTop() +
-                                  (rootBox.getLineLayoutItem()
-                                       .style(rootBox.isFirstLineStyle())
-                                       ->font()
-                                       .getFontMetrics()
-                                       .ascent() -
-                                   container->style(rootBox.isFirstLineStyle())
-                                       ->font()
-                                       .getFontMetrics()
-                                       .ascent());
-          LayoutUnit logicalHeight(container->style(rootBox.isFirstLineStyle())
-                                       ->font()
-                                       .getFontMetrics()
-                                       .height());
+          computeItemTopHeight(container, rootBox, &logicalTop, &logicalHeight);
+          LayoutUnit logicalWidth =
+              childLine->logicalWidth() + childLine->marginLogicalWidth();
           if (isHorizontal) {
             yield(LayoutRect(
                 LayoutUnit(childLine->x() - childLine->marginLogicalLeft()),
-                logicalTop, LayoutUnit(childLine->logicalWidth() +
-                                       childLine->marginLogicalLeft() +
-                                       childLine->marginLogicalRight()),
-                logicalHeight));
+                logicalTop, logicalWidth, logicalHeight));
           } else {
             yield(LayoutRect(
                 logicalTop,
                 LayoutUnit(childLine->y() - childLine->marginLogicalLeft()),
-                logicalHeight, LayoutUnit(childLine->logicalWidth() +
-                                          childLine->marginLogicalLeft() +
-                                          childLine->marginLogicalRight())));
+                logicalHeight, logicalWidth));
           }
         }
       }
@@ -721,20 +701,7 @@ void LayoutInline::generateCulledLineBoxRects(
       for (InlineTextBox* childText = currText->firstTextBox(); childText;
            childText = childText->nextTextBox()) {
         RootInlineBox& rootBox = childText->root();
-        LayoutUnit logicalTop = LayoutUnit(
-            rootBox.logicalTop() + (rootBox.getLineLayoutItem()
-                                        .style(rootBox.isFirstLineStyle())
-                                        ->font()
-                                        .getFontMetrics()
-                                        .ascent() -
-                                    container->style(rootBox.isFirstLineStyle())
-                                        ->font()
-                                        .getFontMetrics()
-                                        .ascent()));
-        LayoutUnit logicalHeight(container->style(rootBox.isFirstLineStyle())
-                                     ->font()
-                                     .getFontMetrics()
-                                     .height());
+        computeItemTopHeight(container, rootBox, &logicalTop, &logicalHeight);
         if (isHorizontal)
           yield(LayoutRect(childText->x(), logicalTop,
                            childText->logicalWidth(), logicalHeight));
