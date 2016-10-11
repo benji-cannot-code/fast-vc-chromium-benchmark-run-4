bc6ed2ed7d632fae7be4e781854430284ba91275
cbiesinger <cbiesinger@chromium.org>
2016-10-11 15:46:59 -0700

    [css-flexbox] Remove unnecessary call to adjustAlignmentForChild
    adjustAlignmentForChild does nothing if child.isOutOfFlowPositioned(),
    so no need to call it and confuse readers of this function.
    
    R=eae@chromium.org,dgrogan@chromium.org
    
    Review-Url: https://codereview.chromium.org/2411073002
    Cr-Commit-Position: refs/heads/master@{#424580}

--- third_party/WebKit/Source/core/layout/LayoutFlexibleBox.cpp
+++ third_party/WebKit/Source/core/layout/LayoutFlexibleBox.cpp
@@ -2158,8 +2158,6 @@ void LayoutFlexibleBox::alignChildren(const Vector<LineContext>& lineContexts,
       if (child == childToExclude)
         continue;
       if (child->isOutOfFlowPositioned()) {
-        if (style()->flexWrap() == FlexWrapReverse)
-          adjustAlignmentForChild(*child, lineCrossAxisExtent);
         continue;
       }
 
