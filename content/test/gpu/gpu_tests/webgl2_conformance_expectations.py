299d474772d11dee0416f2d083266fa33e2fe8ab
yunchao.he <yunchao.he@intel.com>
2016-10-10 18:20:34 -0700

    Roll WebGL 79ef088..a5f3d8b
    https://chromium.googlesource.com/external/khronosgroup/webgl.git/+log/79ef088..a5f3d8b
    
    BUG=654201
    
    TEST=bots
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.win:win_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2396403004
    Cr-Commit-Position: refs/heads/master@{#424325}

--- content/test/gpu/gpu_tests/webgl2_conformance_expectations.py
+++ content/test/gpu/gpu_tests/webgl2_conformance_expectations.py
@@ -54,6 +54,8 @@ class WebGL2ConformanceExpectations(WebGLConformanceExpectations):
 
     # Windows only.
     # We are awesome!
+    self.Fail('conformance2/buffers/getBufferSubData.html',
+        ['win'], bug=654201)
 
     # Win / NVidia
     self.Flaky('deqp/functional/gles3/fbomultisample*',
@@ -503,6 +505,8 @@ class WebGL2ConformanceExpectations(WebGLConformanceExpectations):
         ['mac', 'intel'], bug=483282)
 
     # Linux only.
+    self.Fail('conformance2/buffers/getBufferSubData.html',
+        ['linux'], bug=654201)
 
     self.Flaky('conformance/textures/video/' +
                'tex-2d-rgba-rgba-unsigned_byte.html',
