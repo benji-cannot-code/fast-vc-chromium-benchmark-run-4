a18e29f8365f32d22723ca1d1ef35e2e35076e22
msramek <msramek@chromium.org>
2016-10-12 06:28:01 -0700

    Disable two cocoa FramedBrowserWindowTest tests.
    The following tests:
    
    FramedBrowserWindowTest.WindowWidgetLocation
    FramedBrowserWindowTest.DoesHideTitle
    
    were broken by https://codereview.chromium.org/2416513002/.
    
    TBR=eugenebut@chromium.org,tapted@chromium.org
    BUG=655112
    
    Review-Url: https://codereview.chromium.org/2414673002
    Cr-Commit-Position: refs/heads/master@{#424725}

--- chrome/browser/ui/cocoa/framed_browser_window_unittest.mm
+++ chrome/browser/ui/cocoa/framed_browser_window_unittest.mm
@@ -72,7 +72,8 @@ TEST_F(FramedBrowserWindowTest, ShowAndClose) {
 }
 
 // Test that undocumented title-hiding API we're using does the job.
-TEST_F(FramedBrowserWindowTest, DoesHideTitle) {
+// TODO(crbug.com/655112): Fails on Mac 10.11 Tests.
+TEST_F(FramedBrowserWindowTest, DISABLED_DoesHideTitle) {
   // The -display calls are not strictly necessary, but they do
   // make it easier to see what's happening when debugging (without
   // them the changes are never flushed to the screen).
@@ -103,7 +104,8 @@ TEST_F(FramedBrowserWindowTest, DoesHideTitle) {
 }
 
 // Test to make sure that our window widgets are in the right place.
-TEST_F(FramedBrowserWindowTest, WindowWidgetLocation) {
+// TODO(crbug.com/655112): Fails on Mac 10.11 Tests.
+TEST_F(FramedBrowserWindowTest, DISABLED_WindowWidgetLocation) {
   BOOL yes = YES;
   BOOL no = NO;
 
