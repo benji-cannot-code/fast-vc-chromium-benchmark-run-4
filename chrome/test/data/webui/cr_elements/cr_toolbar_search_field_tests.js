03abd701104499427472182f3b60805e05d0a555
tsergeant <tsergeant@chromium.org>
2016-10-12 16:42:10 -0700

    MD WebUI: Change <cr-toolbar> clear button to not close the search field
    Clicking the clear seach button will now empty the search text and
    refocus the input field. This changes the behavior of the button to be
    in line with <cr-search-field>.
    
    BUG=633967
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2372503002
    Cr-Commit-Position: refs/heads/master@{#424906}

--- chrome/test/data/webui/cr_elements/cr_toolbar_search_field_tests.js
+++ chrome/test/data/webui/cr_elements/cr_toolbar_search_field_tests.js
@@ -53,6 +53,19 @@ cr.define('cr_toolbar_search_field', function() {
         assertNotEquals(field.$.searchInput, field.root.activeElement);
       });
 
+      test('clear search button clears and refocuses input', function() {
+        MockInteractions.tap(field);
+        simulateSearch('query1');
+        Polymer.dom.flush();
+
+        var clearSearch = field.$$('#clearSearch');
+        clearSearch.focus();
+        MockInteractions.tap(clearSearch);
+        assertTrue(field.showingSearch);
+        assertEquals('', field.getValue());
+        assertEquals(field.$.searchInput, field.root.activeElement);
+      });
+
       test('notifies on new searches', function() {
         MockInteractions.tap(field);
         simulateSearch('query1');
@@ -60,7 +73,7 @@ cr.define('cr_toolbar_search_field', function() {
         assertEquals('query1', field.getValue());
 
         MockInteractions.tap(field.$$('#clearSearch'));
-        assertFalse(field.showingSearch);
+        assertTrue(field.showingSearch);
         assertEquals('', field.getValue());
 
         simulateSearch('query2');
@@ -99,9 +112,6 @@ cr.define('cr_toolbar_search_field', function() {
         var clearSearch = field.$$('#clearSearch');
         assertFalse(clearSearch.hidden);
         assertTrue(field.showingSearch);
-
-        MockInteractions.tap(clearSearch);
-        assertFalse(field.showingSearch);
       });
     });
   }
