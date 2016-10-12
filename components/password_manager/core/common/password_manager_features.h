1c6add9efeb7237239e24f6858220cca3d972aa8
peary2 <peary2@gmail.com>
2016-10-12 03:45:16 -0700

    Remove kEnablePasswordChangeSupport and clean-up form_autofill_util.cc
    password_manager::features::kEnablePasswordChangeSupport is defined
    but not used in Chromium any more. Change password support is now
    always on also on Android, including the "update password" prompt.
    Therefore also Android-specific handling inside
    autofill::form_util::IsFormVisible is outdated, Android should do
    what other platforms do.
    
    This CL removes the unused feature, and also makes IsFormVisible copy
    desktop behaviour on Android.
    
    Review-Url: https://codereview.chromium.org/2409123002
    Cr-Commit-Position: refs/heads/master@{#424708}

--- components/password_manager/core/common/password_manager_features.h
+++ components/password_manager/core/common/password_manager_features.h
@@ -22,7 +22,6 @@ extern const base::Feature kAffiliationBasedMatching;
 extern const base::Feature kDropSyncCredential;
 extern const base::Feature kEnableAutomaticPasswordSaving;
 extern const base::Feature kEnableManualPasswordGeneration;
-extern const base::Feature kEnablePasswordChangeSupport;
 extern const base::Feature kEnablePasswordForceSaving;
 extern const base::Feature kProtectSyncCredential;
 extern const base::Feature kProtectSyncCredentialOnReauth;
