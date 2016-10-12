9219e344db8250b7e4f62d7c681013ce5eb54aff
dtseng <dtseng@chromium.org>
2016-10-11 17:20:23 -0700

    Recovery: Implement focus recovery across root AutomationNodes
    This cl maintains a WeakMap of AutomationRootNode objects to the last known node that had ChromeVox range.
    
    This map gets updated manually when marked or implicitly when focus crosses top level root nodes.
    
    TEST=manual; open context menu, open panel, activate menus that manipulate focus, press escape; ctrl+tab, alt+tab. Verify focus lands as expected.
    BUG=631918,628912,624586,605377,524673,652143
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2412433004
    Cr-Commit-Position: refs/heads/master@{#424612}

--- chrome/browser/resources/chromeos/chromevox/cvox2/background/desktop_automation_handler.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/background/desktop_automation_handler.js
@@ -253,6 +253,28 @@ DesktopAutomationHandler.prototype = {
     // views.
     if (evt.eventFrom == '')
       Output.forceModeForNextSpeechUtterance(cvox.QueueMode.CATEGORY_FLUSH);
+    if (!node.root)
+      return;
+
+    var root = AutomationUtil.getTopLevelRoot(node.root);
+
+    // If we're crossing out of a root, save it in case it needs recovering.
+    var prevRange = ChromeVoxState.instance.currentRange;
+    var prevNode = prevRange ? prevRange.start.node : null;
+    if (prevNode) {
+    var prevRoot = AutomationUtil.getTopLevelRoot(prevNode);
+      if (prevRoot && prevRoot !== root)
+      ChromeVoxState.instance.focusRecoveryMap.set(prevRoot, prevRange);
+    }
+
+    // If a previous node was saved for this focus, restore it.
+    var savedRange = ChromeVoxState.instance.focusRecoveryMap.get(root);
+    ChromeVoxState.instance.focusRecoveryMap.delete(root);
+    if (savedRange) {
+      ChromeVoxState.instance.navigateToRange(savedRange, false);
+      return;
+    }
+
     this.onEventDefault(new chrome.automation.AutomationEvent(
         EventType.focus, node, evt.eventFrom));
   },
@@ -430,7 +452,7 @@ DesktopAutomationHandler.prototype = {
    * @param {!AutomationEvent} evt
    */
   onMenuStart: function(evt) {
-    ChromeVoxState.instance.startExcursion();
+    ChromeVoxState.instance.markCurrentRange();
     this.onEventDefault(evt);
   },
 
@@ -440,7 +462,16 @@ DesktopAutomationHandler.prototype = {
    */
   onMenuEnd: function(evt) {
     this.onEventDefault(evt);
-    ChromeVoxState.instance.endExcursion();
+
+    // This is a work around for Chrome context menus not firing a focus event
+    // after you close them.
+    chrome.automation.getFocus(function(focus) {
+      if (focus) {
+        this.onFocus(
+            new chrome.automation.AutomationEvent(
+                EventType.focus, focus, 'page'));
+      }
+    }.bind(this));
   },
 
   /**
