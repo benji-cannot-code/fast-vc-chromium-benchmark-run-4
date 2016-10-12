d1c942240711298fb761c0ebe22cbabc68ab1056
tapted <tapted@chromium.org>
2016-10-12 00:11:27 -0700

    Mac: Fix SDK 10.11 build error from r424609.
    BUG=605219
    TBR=eugenebut@chromium.org
    
    Review-Url: https://codereview.chromium.org/2416513002
    Cr-Commit-Position: refs/heads/master@{#424685}

--- chrome/browser/ui/cocoa/framed_browser_window.mm
+++ chrome/browser/ui/cocoa/framed_browser_window.mm
@@ -34,6 +34,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // - close/min in the background don't bring the window forward
 // - rollover effects work correctly
 
+// The NSLayoutConstraint class hierarchy only exists in the 10.11 SDK. When
+// targeting something lower, constraintEqualToAnchor:constant: needs to be
+// invoked using duck typing.
+#if !defined(MAC_OS_X_VERSION_10_11) || \
+    MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_10_11
+@interface NSObject (NSLayoutConstraint)
+- (NSLayoutConstraint*)constraintEqualToAnchor:(id)anchor constant:(CGFloat)c;
+@end
+#endif
+
 namespace {
 
 // Size of the gradient. Empirically determined so that the gradient looks
@@ -266,8 +276,16 @@ const CGFloat kWindowGradientHeight = 24.0;
     leadingTargetAnchor = [[button superview] rightAnchor];
     leadingOffset = -leadingOffset;
   }
-  [[leadingSourceAnchor constraintEqualToAnchor:leadingTargetAnchor
-                                       constant:leadingOffset] setActive:YES];
+
+#if !defined(MAC_OS_X_VERSION_10_11) || \
+    MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_10_11
+  id leadingSourceAnchorDuck = leadingSourceAnchor;
+#else
+  NSLayoutXAxisAnchor* leadingSourceAnchorDuck = leadingSourceAnchor;
+#endif
+  [[leadingSourceAnchorDuck constraintEqualToAnchor:leadingTargetAnchor
+                                           constant:leadingOffset]
+      setActive:YES];
 }
 
 - (void)adjustCloseButton:(NSNotification*)notification {
