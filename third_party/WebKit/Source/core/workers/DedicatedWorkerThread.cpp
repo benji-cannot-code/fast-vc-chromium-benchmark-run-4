932797c729b4f384766343be14837ac6f169d7e7
keishi <keishi@chromium.org>
2016-10-12 00:26:35 -0700

    Remove CrossThreadPersistent::release
    Calling CrossThreadPersistent::release was causing WorkerClients to be destroyed prematurely.
    The stack for per thread heap enabled threads are invisible from other threads running GC. Using CrossThreadPersistent::release runs the risk of premature collection so we should delete it.
    
    BUG=652001,653272,652458,653657,652556,653124,651790
    
    Review-Url: https://codereview.chromium.org/2406333002
    Cr-Commit-Position: refs/heads/master@{#424688}

--- third_party/WebKit/Source/core/workers/DedicatedWorkerThread.cpp
+++ third_party/WebKit/Source/core/workers/DedicatedWorkerThread.cpp
@@ -56,7 +56,7 @@ DedicatedWorkerThread::DedicatedWorkerThread(
     : WorkerThread(std::move(workerLoaderProxy), workerObjectProxy),
       m_workerBackingThread(
           WorkerBackingThread::create("DedicatedWorker Thread",
-                                      BlinkGC::MainThreadHeapMode)),
+                                      BlinkGC::PerThreadHeapMode)),
       m_workerObjectProxy(workerObjectProxy),
       m_timeOrigin(timeOrigin) {}
 
