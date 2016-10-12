9d4ba9ad799f47933442953230d7a2c6a230c36a
pwnall <pwnall@chromium.org>
2016-10-11 19:13:53 -0700

    Align IDBCursor#continuePrimaryKey exception order with spec.
    BUG=435727
    
    Review-Url: https://codereview.chromium.org/2410303004
    Cr-Commit-Position: refs/heads/master@{#424647}

--- third_party/WebKit/Source/modules/indexeddb/IDBCursor.cpp
+++ third_party/WebKit/Source/modules/indexeddb/IDBCursor.cpp
@@ -234,11 +234,7 @@ void IDBCursor::continuePrimaryKey(ScriptState* scriptState,
         TransactionInactiveError, IDBDatabase::transactionInactiveErrorMessage);
     return;
   }
-  if (!m_gotValue) {
-    exceptionState.throwDOMException(InvalidStateError,
-                                     IDBDatabase::noValueErrorMessage);
-    return;
-  }
+
   if (isDeleted()) {
     exceptionState.throwDOMException(InvalidStateError,
                                      IDBDatabase::sourceDeletedErrorMessage);
@@ -250,10 +246,17 @@ void IDBCursor::continuePrimaryKey(ScriptState* scriptState,
                                      "The cursor's source is not an index.");
     return;
   }
+
   if (m_direction != WebIDBCursorDirectionNext &&
       m_direction != WebIDBCursorDirectionPrev) {
     exceptionState.throwDOMException(
         InvalidAccessError, "The cursor's direction is not 'next' or 'prev'.");
+    return;
+  }
+
+  if (!m_gotValue) {
+    exceptionState.throwDOMException(InvalidStateError,
+                                     IDBDatabase::noValueErrorMessage);
     return;
   }
 
