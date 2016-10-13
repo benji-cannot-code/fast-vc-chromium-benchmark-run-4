323c236e1e1396ca11739768fe05dd709093c97b
davidben <davidben@chromium.org>
2016-10-13 13:56:07 -0700

    Roll src/third_party/boringssl/src 0d81373f9..1991af690
    https://boringssl.googlesource.com/boringssl/+log/0d81373f9169637ff935449227be924c08bf1ec5..1991af69006a8fe4faf9b80b2340688846e62514
    
    This requires a change in WebCrypto. BoringSSL's
    https://boringssl.googlesource.com/boringssl/+/97227dc52d9e6620c8b83eb1dd4d45edf51d9d7f
    switches to aligning with upstream's APIs for AES key wrap.
    
    This also teaches DevTools how to handle the new draft TLS 1.3 ciphers
    and adds them to histograms.xml. See
    https://tools.ietf.org/html/draft-ietf-tls-tls13-16#appendix-A.4
    
    Finally, it adds the CECPQ1 experimental ciphers which we forgot to add
    there earlier.
    
    BUG=639495
    
    Review-Url: https://codereview.chromium.org/2395323002
    Cr-Commit-Position: refs/heads/master@{#425147}

--- third_party/WebKit/public/platform/WebURLResponse.h
+++ third_party/WebKit/public/platform/WebURLResponse.h
@@ -115,6 +115,8 @@ class WebURLResponse {
           sctList(sctList) {}
     // All strings are human-readable values.
     WebString protocol;
+    // keyExchange is the empty string if not applicable for the connection's
+    // protocol.
     WebString keyExchange;
     // keyExchangeGroup is the empty string if not applicable for the
     // connection's key exchange.
