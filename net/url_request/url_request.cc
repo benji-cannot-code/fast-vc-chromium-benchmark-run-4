e0d4b0de5b9109f8ae3dba8a90083d577bb46bc2
fdoray <fdoray@chromium.org>
2016-10-13 14:03:55 -0700

    Remove MessageLoop::current() from url_request.cc.
    Whenever possible, use ThreadTaskRunnerHandle::Get() instead of
    MessageLoop::current(). ThreadTaskRunnerHandle::Get() works within
    TaskScheduler while MessageLoop::current() doesn't.
    
    Good reasons to use MessageLoop::current():
    - Add destruction, nesting or task observers.
    - Run nested loops.
    
    Bad reasons to use MessageLoop::current():
    - Post tasks. Use ThreadTaskRunnerHandle::Get() instead.
    - Watch a file descriptor. Use FileDescriptorWatcher instead.
    - Verify that it is possible to post tasks to the current thread.
      Use ThreadTaskRunnerHandle::IsSet() instead.
    - Verify that code runs on a specific thread. Use
      SingleThreadTaskRunner::BelongsToCurrentThread() instead.
    
    BUG=650723
    
    Review-Url: https://codereview.chromium.org/2404313002
    Cr-Commit-Position: refs/heads/master@{#425151}

--- net/url_request/url_request.cc
+++ net/url_request/url_request.cc
@@ -13,12 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/lazy_instance.h"
 #include "base/memory/singleton.h"
-#include "base/message_loop/message_loop.h"
 #include "base/profiler/scoped_tracker.h"
 #include "base/rand_util.h"
 #include "base/stl_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/synchronization/lock.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "base/values.h"
 #include "net/base/auth.h"
 #include "net/base/host_port_pair.h"
@@ -579,8 +579,7 @@ URLRequest::URLRequest(const GURL& url,
       received_response_content_length_(0),
       creation_time_(base::TimeTicks::Now()) {
   // Sanity check out environment.
-  DCHECK(base::MessageLoop::current())
-      << "The current base::MessageLoop must exist";
+  DCHECK(base::ThreadTaskRunnerHandle::IsSet());
 
   context->url_requests()->insert(this);
   net_log_.BeginEvent(NetLogEventType::REQUEST_ALIVE);
