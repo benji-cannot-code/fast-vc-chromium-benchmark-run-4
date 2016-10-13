0ee2a6a8b820f0c1447fb15021d9bbd2c8c44959
dbeam <dbeam@chromium.org>
2016-10-13 15:20:31 -0700

    Remove unnecessary, left over Polymer.dom.flush() from appearance tests
    R=dpapad@chromium.org
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2421653002
    Cr-Commit-Position: refs/heads/master@{#425188}

--- chrome/test/data/webui/settings/appearance_page_test.js
+++ chrome/test/data/webui/settings/appearance_page_test.js
@@ -144,8 +144,6 @@ cr.define('settings_appearance', function() {
 
       if (cr.isLinux && !cr.isChromeOS) {
         test('useSystemTheme', function() {
-          Polymer.dom.flush();
-
           var button = appearancePage.$$('#useSystem');
           assertTrue(!!button);
           MockInteractions.tap(button);
