ea1410c1da2be6bf938d9d9a9847fb3e3e2c1384
luoe <luoe@chromium.org>
2016-10-12 18:29:24 -0700

    DevTools: update styles for debugger's CallStackSidebarPane
    - async links now format the same as non-async links
    - updated gray color
    - async dividers aligned to the left
    - line separators only appear for async dividers
    
    BUG=652461
    
    Review-Url: https://codereview.chromium.org/2389333002
    Cr-Commit-Position: refs/heads/master@{#424929}

---
+++ third_party/WebKit/LayoutTests/inspector/sources/debugger/resources/unformatted-async.js
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function f2()
+{
+    var a=0;var b=1;var c=3;var d=4;var e=5;
+    var f=0;setTimeout(f3, 0);
+}
+
+function f3()
+{
+    debugger;
+}
