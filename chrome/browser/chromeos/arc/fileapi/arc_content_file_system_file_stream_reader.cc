15747dcd666547c625af204ae9de4e4bb4eedf4f
hashimoto <hashimoto@chromium.org>
2016-10-13 08:12:24 -0700

    arc: Empty implementation of ARC content file system
    Actual implementation will be added later.
    
    BUG=654684
    
    Review-Url: https://codereview.chromium.org/2411323002
    Cr-Commit-Position: refs/heads/master@{#425034}

---
+++ chrome/browser/chromeos/arc/fileapi/arc_content_file_system_file_stream_reader.cc
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/arc/fileapi/arc_content_file_system_file_stream_reader.h"
+
+#include "net/base/net_errors.h"
+
+namespace arc {
+
+ArcContentFileSystemFileStreamReader::ArcContentFileSystemFileStreamReader(
+    const storage::FileSystemURL& url,
+    int64_t offset,
+    int64_t max_bytes_to_read) {}
+
+ArcContentFileSystemFileStreamReader::~ArcContentFileSystemFileStreamReader() =
+    default;
+
+int ArcContentFileSystemFileStreamReader::Read(
+    net::IOBuffer* buffer,
+    int buffer_length,
+    const net::CompletionCallback& callback) {
+  NOTIMPLEMENTED();
+  return net::ERR_NOT_IMPLEMENTED;
+}
+
+int64_t ArcContentFileSystemFileStreamReader::GetLength(
+    const net::Int64CompletionCallback& callback) {
+  NOTIMPLEMENTED();
+  return net::ERR_NOT_IMPLEMENTED;
+}
+
+}  // namespace arc
---
+++ chrome/browser/chromeos/arc/fileapi/arc_content_file_system_file_stream_reader.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_ARC_FILEAPI_ARC_CONTENT_FILE_SYSTEM_FILE_STREAM_READER_H_
+#define CHROME_BROWSER_CHROMEOS_ARC_FILEAPI_ARC_CONTENT_FILE_SYSTEM_FILE_STREAM_READER_H_
+
+#include "storage/browser/fileapi/file_stream_reader.h"
+
+namespace arc {
+
+class ArcContentFileSystemFileStreamReader : public storage::FileStreamReader {
+ public:
+  ArcContentFileSystemFileStreamReader(const storage::FileSystemURL& url,
+                                       int64_t offset,
+                                       int64_t max_bytes_to_read);
+  ~ArcContentFileSystemFileStreamReader() override;
+
+  // storage::FileStreamReader override:
+  int Read(net::IOBuffer* buffer,
+           int buffer_length,
+           const net::CompletionCallback& callback) override;
+  int64_t GetLength(const net::Int64CompletionCallback& callback) override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(ArcContentFileSystemFileStreamReader);
+};
+
+}  // namespace arc
+
+#endif  // CHROME_BROWSER_CHROMEOS_ARC_FILEAPI_ARC_CONTENT_FILE_SYSTEM_FILE_STREAM_READER_H_
