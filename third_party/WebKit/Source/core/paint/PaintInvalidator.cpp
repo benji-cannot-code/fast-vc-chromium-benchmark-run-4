f249d04b4a514ddd0c5123efe978e3662ba0e3fd
wangxianzhu <wangxianzhu@chromium.org>
2016-10-13 11:36:29 -0700

    Revert of Use enclosingLayoutRect instead of enclosingIntRect in SVGLayoutSupport::transformPaintInvalidation… (patchset #9 id:160001 of https://codereview.chromium.org/2380403002/ )
    Reason for revert:
    The CL might be incorrect. See https://bugs.chromium.org/p/chromium/issues/detail?id=651521#c7 for details.
    
    Original issue's description:
    > Use enclosingLayoutRect instead of enclosingIntRect in SVGLayoutSupport::transformPaintInvalidationRect()
    >
    > https://bugs.chromium.org/p/chromium/issues/detail?id=651521#c2:
    > The purpose of the original enclosingIntRect() might be to avoid
    > unwanted inward-snapping due to FloatRect -> LayoutRect conversion.
    >
    > Change it to enclosingLayoutRect. Also changed PaintInvalidator to
    > use enclosingLayoutRect when converting from FloatRect to
    > LayoutRect.
    >
    > BUG=651521
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    >
    > Committed: https://crrev.com/97024f931ff54c93b9b0633c3406b4f9ff9779b7
    > Cr-Commit-Position: refs/heads/master@{#422710}
    
    TBR=fs@opera.com,pdr@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=651521
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2413783002
    Cr-Commit-Position: refs/heads/master@{#425101}

--- third_party/WebKit/Source/core/paint/PaintInvalidator.cpp
+++ third_party/WebKit/Source/core/paint/PaintInvalidator.cpp
@@ -21,14 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-// TODO(wangxianzhu): Avoid using function when possible. For example, we can
-// avoid it by avoiding unnecessary conversions between LayoutRects and
-// FloatRects.
-static LayoutRect enclosingLayoutRectIfNotEmpty(const FloatRect& floatRect) {
-  return floatRect.isEmpty() ? LayoutRect(floatRect)
-                             : enclosingLayoutRect(floatRect);
-}
-
 static LayoutRect slowMapToVisualRectInAncestorSpace(
     const LayoutObject& object,
     const LayoutBoxModelObject& ancestor,
@@ -40,7 +32,7 @@ static LayoutRect slowMapToVisualRectInAncestorSpace(
     return result;
   }
 
-  LayoutRect result = enclosingLayoutRectIfNotEmpty(rect);
+  LayoutRect result(rect);
   if (object.isLayoutView())
     toLayoutView(object).mapToVisualRectInAncestorSpace(
         &ancestor, result, InputIsInFrameCoordinates, DefaultVisualRectFlags);
@@ -81,7 +73,7 @@ static LayoutRect mapLocalRectToPaintInvalidationBacking(
   if (RuntimeEnabledFeatures::slimmingPaintV2Enabled()) {
     // In SPv2, visual rects are in the space of their local transform node.
     rect.moveBy(FloatPoint(context.treeBuilderContext.current.paintOffset));
-    return enclosingLayoutRectIfNotEmpty(rect);
+    return LayoutRect(rect);
   }
 
   LayoutRect result;
@@ -90,7 +82,7 @@ static LayoutRect mapLocalRectToPaintInvalidationBacking(
     result = slowMapToVisualRectInAncestorSpace(
         object, *context.paintInvalidationContainer, rect);
   } else if (object == context.paintInvalidationContainer) {
-    result = enclosingLayoutRectIfNotEmpty(rect);
+    result = LayoutRect(rect);
   } else {
     rect.moveBy(FloatPoint(context.treeBuilderContext.current.paintOffset));
 
@@ -105,10 +97,9 @@ static LayoutRect mapLocalRectToPaintInvalidationBacking(
         containerPaintProperties->contentsProperties();
 
     bool success = false;
-    result = enclosingLayoutRectIfNotEmpty(
-        geometryMapper.mapToVisualRectInDestinationSpace(
-            rect, currentTreeState,
-            containerContentsProperties.propertyTreeState, success));
+    result = LayoutRect(geometryMapper.mapToVisualRectInDestinationSpace(
+        rect, currentTreeState, containerContentsProperties.propertyTreeState,
+        success));
     DCHECK(success);
 
     // Convert the result to the container's contents space.
