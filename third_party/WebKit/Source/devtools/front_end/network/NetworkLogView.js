9170f39f139eed1f096a27201407a70fb47d267c
allada <allada@chromium.org>
2016-10-12 21:03:25 -0700

    [Devtools] Bug fix of event dividers not showing in network panel
    Fixed bug were event dividers from film strip were not showing in
    network panel timeline.
    
    This was introduced when NetworkLogViewColumns was split out from NetworkLogView.
    
    Offending patch: https://codereview.chromium.org/2118663002/
    
    R=lushnikov,dgozman
    BUG=655368
    
    Review-Url: https://codereview.chromium.org/2417723002
    Cr-Commit-Position: refs/heads/master@{#424952}

--- third_party/WebKit/Source/devtools/front_end/network/NetworkLogView.js
+++ third_party/WebKit/Source/devtools/front_end/network/NetworkLogView.js
@@ -508,14 +508,12 @@ WebInspector.NetworkLogView.prototype = {
      */
     selectFilmStripFrame: function(time)
     {
-        for (var divider of this._eventDividers)
-            divider.element.classList.toggle("network-frame-divider-selected", divider.time === time);
+        this._columns.selectFilmStripFrame(time);
     },
 
     clearFilmStripFrame: function()
     {
-        for (var divider of this._eventDividers)
-            divider.element.classList.toggle("network-frame-divider-selected", false);
+        this._columns.clearFilmStripFrame();
     },
 
     _refreshIfNeeded: function()
@@ -700,7 +698,6 @@ WebInspector.NetworkLogView.prototype = {
 
         this._mainRequestLoadTime = -1;
         this._mainRequestDOMContentLoadedTime = -1;
-        this._eventDividers = [];
 
         if (this._dataGrid) {
             this._dataGrid.rootNode().removeChildren();
