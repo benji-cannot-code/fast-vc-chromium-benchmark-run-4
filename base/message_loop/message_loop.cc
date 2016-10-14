28f952c3ae75167d193ed7c7f34dcd9aa61d7943
fdoray <fdoray@chromium.org>
2016-10-14 12:12:38 -0700

    Disallow task observers on some BrowserThreads.
    This CL disallows task observers on the DB, FILE, FILE_USER_BLOCKING,
    PROCESS_LAUNCHER and CACHE threads. Calling Add/RemoveTaskObserver
    on these threads will result in a crash.
    
    This is important because these threads will be migrated to
    TaskScheduler which doesn't support task observers.
    
    To our knowledge, no code uses task observers on these threads. This
    CL will help us confirm that in the wild.
    
    BUG=653916
    
    Review-Url: https://codereview.chromium.org/2419803002
    Cr-Commit-Position: refs/heads/master@{#425418}

--- base/message_loop/message_loop.cc
+++ base/message_loop/message_loop.cc
@@ -279,11 +279,13 @@ bool MessageLoop::IsNested() {
 
 void MessageLoop::AddTaskObserver(TaskObserver* task_observer) {
   DCHECK_EQ(this, current());
+  CHECK(allow_task_observers_);
   task_observers_.AddObserver(task_observer);
 }
 
 void MessageLoop::RemoveTaskObserver(TaskObserver* task_observer) {
   DCHECK_EQ(this, current());
+  CHECK(allow_task_observers_);
   task_observers_.RemoveObserver(task_observer);
 }
 
