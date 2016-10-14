05855acbe28aeefacddea9b10a63c8df3955b2c3
msw <msw@chromium.org>
2016-10-14 10:53:34 -0700

    Fix BindingSet::FlushForTesting.
    bindings_ is a std::map<BindingId, std::unique_ptr<Entry>> bindings_;
    So, the second element should be used here, not the first.
    
    BUG=NONE
    TEST=NONE
    R=rockot@chromium.org
    
    Review-Url: https://codereview.chromium.org/2418813004
    Cr-Commit-Position: refs/heads/master@{#425383}

--- mojo/public/cpp/bindings/binding_set.h
+++ mojo/public/cpp/bindings/binding_set.h
@@ -138,9 +138,8 @@ class BindingSet {
   }
 
   void FlushForTesting() {
-    for (auto& binding : bindings_) {
-      binding.first->FlushForTesting();
-    }
+    for (auto& binding : bindings_)
+      binding.second->FlushForTesting();
   }
 
  private:
