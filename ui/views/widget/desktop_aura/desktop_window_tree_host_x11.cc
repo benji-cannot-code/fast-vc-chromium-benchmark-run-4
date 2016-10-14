d0a2373104588352e9371a65a6b9e585e4832e8f
tfarina <tfarina@chromium.org>
2016-10-14 09:49:01 -0700

    ui/views: convert usage of FOR_EACH_OBSERVER macro
    ObserverList now supports range-based for loops and thus this can be
    used to iterate through a list of observers as the FOR_EACH_OBSERVER
    macro was deprecated.
    
    This patch was generated by the following command line:
    $ cd ui/views
    $ perl -0777 -pi -e
    's/FOR_EACH_OBSERVER\(\s*([\w:]+?)\s*,\s*([\w:()]+?)\s*,\s*([^;]+?)\);/for
    (\1& observer : \2) observer.\3;/mg' $(git gs FOR_EACH_OBSERVER | cut -d: -f1 | uniq)
    
    And then manually inspected for line indentation fixes.
    
    BUG=655021
    TEST=views_unittests
    R=sadrul@chromium.org
    
    Review-Url: https://codereview.chromium.org/2418983002
    Cr-Commit-Position: refs/heads/master@{#425359}

--- ui/views/widget/desktop_aura/desktop_window_tree_host_x11.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_x11.cc
@@ -2171,9 +2171,8 @@ uint32_t DesktopWindowTreeHostX11::DispatchEvent(
     case MapNotify: {
       window_mapped_ = true;
 
-      FOR_EACH_OBSERVER(DesktopWindowTreeHostObserverX11,
-                        observer_list_,
-                        OnWindowMapped(xwindow_));
+      for (DesktopWindowTreeHostObserverX11& observer : observer_list_)
+        observer.OnWindowMapped(xwindow_);
 
       UpdateMinAndMaxSize();
 
@@ -2193,9 +2192,8 @@ uint32_t DesktopWindowTreeHostX11::DispatchEvent(
       has_pointer_grab_ = false;
       has_pointer_focus_ = false;
       has_window_focus_ = false;
-      FOR_EACH_OBSERVER(DesktopWindowTreeHostObserverX11,
-                        observer_list_,
-                        OnWindowUnmapped(xwindow_));
+      for (DesktopWindowTreeHostObserverX11& observer : observer_list_)
+        observer.OnWindowUnmapped(xwindow_);
       break;
     }
     case ClientMessage: {
