1069d8b956eaa9a3bf1264dd89edbb4df32a2490
alito <alito@chromium.org>
2016-10-14 12:09:06 -0700

    Add Chrome's executable path when launching the CCT.
    BUG=
    
    Review-Url: https://codereview.chromium.org/2421683002
    Cr-Commit-Position: refs/heads/master@{#425416}

--- chrome/browser/safe_browsing/srt_global_error_win.cc
+++ chrome/browser/safe_browsing/srt_global_error_win.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/safe_browsing/srt_global_error_win.h"
 
+#include "base/base_paths.h"
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/callback.h"
@@ -53,6 +54,7 @@ const base::FilePath::CharType kExecutableExtension[] = L"exe";
 
 // Switches to add to the command line when executing the SRT.
 const char kChromePromptSwitch[] = "chrome-prompt";
+const char kChromeExePathSwitch[] = "chrome-exe-path";
 const char kChromeSystemInstallSwitch[] = "chrome-system-install";
 const char kUmaUserSwitch[] = "uma-user";
 
@@ -89,6 +91,7 @@ void MaybeExecuteSRTFromBlockingPool(
 
       base::FilePath chrome_exe_path;
       PathService::Get(base::FILE_EXE, &chrome_exe_path);
+      srt_command_line.AppendSwitchPath(kChromeExePathSwitch, chrome_exe_path);
       if (!InstallUtil::IsPerUserInstall(chrome_exe_path))
         srt_command_line.AppendSwitch(kChromeSystemInstallSwitch);
 
