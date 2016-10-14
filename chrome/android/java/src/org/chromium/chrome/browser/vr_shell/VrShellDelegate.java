86a711ca6f463d8719599330ebe1b3e93ecc6a57
bshe <bshe@chromium.org>
2016-10-14 11:29:31 -0700

    Remove Interface from file names and add Impl to actual implementation
    A follow up CL of https://codereview.chromium.org/2398103002/
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2417743003
    Cr-Commit-Position: refs/heads/master@{#425393}

--- chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellDelegate.java
@@ -35,10 +35,10 @@ public class VrShellDelegate {
 
     private boolean mVrEnabled;
 
-    private Class<? extends VrShellInterface> mVrShellClass;
-    private Class<? extends NonPresentingGvrContextInterface> mNonPresentingGvrContextClass;
-    private VrShellInterface mVrShell;
-    private NonPresentingGvrContextInterface mNonPresentingGvrContext;
+    private Class<? extends VrShell> mVrShellClass;
+    private Class<? extends NonPresentingGvrContext> mNonPresentingGvrContextClass;
+    private VrShell mVrShell;
+    private NonPresentingGvrContext mNonPresentingGvrContext;
     private boolean mInVr;
     private int mRestoreSystemUiVisibilityFlag = -1;
     private String mVrExtra;
@@ -71,11 +71,11 @@ public class VrShellDelegate {
     @SuppressWarnings("unchecked")
     private boolean maybeFindVrClasses() {
         try {
-            mVrShellClass = (Class<? extends VrShellInterface>) Class.forName(
-                    "org.chromium.chrome.browser.vr_shell.VrShell");
+            mVrShellClass = (Class<? extends VrShell>) Class.forName(
+                    "org.chromium.chrome.browser.vr_shell.VrShellImpl");
             mNonPresentingGvrContextClass =
-                    (Class<? extends NonPresentingGvrContextInterface>) Class.forName(
-                            "org.chromium.chrome.browser.vr_shell.NonPresentingGvrContext");
+                    (Class<? extends NonPresentingGvrContext>) Class.forName(
+                            "org.chromium.chrome.browser.vr_shell.NonPresentingGvrContextImpl");
             return true;
         } catch (ClassNotFoundException e) {
             mVrShellClass = null;
@@ -196,7 +196,7 @@ public class VrShellDelegate {
             Constructor<?> nonPresentingGvrContextConstructor =
                     mNonPresentingGvrContextClass.getConstructor(Activity.class);
             mNonPresentingGvrContext =
-                    (NonPresentingGvrContextInterface)
+                    (NonPresentingGvrContext)
                             nonPresentingGvrContextConstructor.newInstance(mActivity);
         } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
                 | InvocationTargetException | NoSuchMethodException e) {
@@ -233,7 +233,7 @@ public class VrShellDelegate {
         StrictMode.allowThreadDiskWrites();
         try {
             Constructor<?> vrShellConstructor = mVrShellClass.getConstructor(Activity.class);
-            mVrShell = (VrShellInterface) vrShellConstructor.newInstance(mActivity);
+            mVrShell = (VrShell) vrShellConstructor.newInstance(mActivity);
         } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
                 | InvocationTargetException | NoSuchMethodException e) {
             Log.e(TAG, "Unable to instantiate VrShell", e);
