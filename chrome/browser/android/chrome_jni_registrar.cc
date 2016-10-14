c4ba5eac7ceaf2ac884ee013532c51c162bc4f98
romax <romax@chromium.org>
2016-10-14 12:17:08 -0700

    [Offline Pages] Added OfflinePageEvaluationBridge for testing.
    Build another bridge for evaluation tests use only. This is part of a larger
    change tracked in the bug. The bridge exposes some testing only interface and
    implements another observer.
    The bridge would not be compiled for official build, and currently there seems
    no way to have testing-only JNI code without marking with build flags.
    
    BUG=646947
    
    Review-Url: https://codereview.chromium.org/2378803003
    Cr-Commit-Position: refs/heads/master@{#425419}

--- chrome/browser/android/chrome_jni_registrar.cc
+++ chrome/browser/android/chrome_jni_registrar.cc
@@ -182,6 +182,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/vr_shell/vr_shell_delegate.h"
 #endif
 
+#if !defined(OFFICIAL_BUILD)
+#include "chrome/browser/android/offline_pages/evaluation/offline_page_evaluation_bridge.h"
+#endif
+
 namespace android {
 
 static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
@@ -313,6 +317,10 @@ static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
     {"OfflinePageBridge", offline_pages::android::RegisterOfflinePageBridge},
     {"OfflinePageDownloadBridge",
      offline_pages::android::OfflinePageDownloadBridge::Register},
+#if !defined(OFFICIAL_BUILD)
+    {"OfflinePageEvaluationBridge",
+     offline_pages::android::OfflinePageEvaluationBridge::Register},
+#endif
     {"OmniboxPrerender", RegisterOmniboxPrerender},
     {"OmniboxUrlEmphasizer",
      OmniboxUrlEmphasizer::RegisterOmniboxUrlEmphasizer},
