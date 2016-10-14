7ffed382e365e92060a56c145514110a78b68c9d
estade <estade@chromium.org>
2016-10-14 09:03:02 -0700

    Update appearance of invalid textfields in Harmony.
    BUG=650875
    
    Review-Url: https://codereview.chromium.org/2406363003
    Cr-Commit-Position: refs/heads/master@{#425341}

--- chrome/browser/ui/views/autofill/card_unmask_prompt_views.cc
+++ chrome/browser/ui/views/autofill/card_unmask_prompt_views.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/ui/autofill/create_card_unmask_prompt_view.h"
-#include "chrome/browser/ui/views/autofill/decorated_textfield.h"
 #include "chrome/browser/ui/views/autofill/tooltip_icon.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/theme_resources.h"
@@ -35,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/controls/link.h"
+#include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/controls/throbber.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/widget/widget.h"
@@ -433,9 +433,10 @@ void CardUnmaskPromptViews::InitIfNecessary() {
       input_row_->child_at(i)->SetVisible(false);
   }
 
-  cvc_input_ = new DecoratedTextfield(
-      base::string16(),
-      l10n_util::GetStringUTF16(IDS_AUTOFILL_DIALOG_PLACEHOLDER_CVC), this);
+  cvc_input_ = new views::Textfield();
+  cvc_input_->set_placeholder_text(
+      l10n_util::GetStringUTF16(IDS_AUTOFILL_DIALOG_PLACEHOLDER_CVC));
+  cvc_input_->set_controller(this);
   cvc_input_->set_default_width_in_chars(8);
   input_row_->AddChildView(cvc_input_);
 
--- ui/views/examples/textfield_example.h
+++ ui/views/examples/textfield_example.h
@@ -47,6 +47,7 @@ class VIEWS_EXAMPLES_EXPORT TextfieldExample : public ExampleBase,
   Textfield* password_;
   Textfield* disabled_;
   Textfield* read_only_;
+  Textfield* invalid_;
 
   // Various buttons to control textfield.
   LabelButton* show_password_;
