c3c94595c6e2b24722073cc4d6513dddcf25b7eb
lizeb <lizeb@chromium.org>
2016-10-14 06:46:00 -0700

    tools: Extend the CustomTabs benchmark scripts to speculative_prefetch.
    This extends the Custom Tabs benchmark scripts to also be useful for
    speculative prefetch evaluation.
    
    Also a bunch of small cleanups (including making the linter happy).
    
    BUG=655980
    
    Review-Url: https://codereview.chromium.org/2414363002
    Cr-Commit-Position: refs/heads/master@{#425312}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabsConnection.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabsConnection.java
@@ -78,6 +78,7 @@ public class CustomTabsConnection {
     @VisibleForTesting
     static final String DEBUG_OVERRIDE_KEY =
             "android.support.customtabs.maylaunchurl.DEBUG_OVERRIDE";
+    private static final int NO_OVERRIDE = 0;
     @VisibleForTesting
     static final int NO_PRERENDERING = 1;
     @VisibleForTesting
@@ -277,7 +278,8 @@ public class CustomTabsConnection {
         Profile profile = Profile.getLastUsedProfile();
 
         url = DataReductionProxySettings.getInstance().maybeRewriteWebliteUrl(url);
-        int debugOverrideValue = extras != null ? extras.getInt(DEBUG_OVERRIDE_KEY, -1) : -1;
+        int debugOverrideValue = NO_OVERRIDE;
+        if (extras != null) debugOverrideValue = extras.getInt(DEBUG_OVERRIDE_KEY, NO_OVERRIDE);
 
         boolean didStartPrerender = false, didStartPrefetch = false;
         boolean mayPrerender = mayPrerender(session);
