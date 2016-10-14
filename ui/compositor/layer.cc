7d82a069b579941ab610f26376cc6671f2095b7a
loyso <loyso@chromium.org>
2016-10-13 18:16:36 -0700

    Remove usage of FOR_EACH_OBSERVER macro in ui/compositor/.
    Observer lists now support range-based for loops.
    
    BUG=655021
    
    Review-Url: https://codereview.chromium.org/2417613003
    Cr-Commit-Position: refs/heads/master@{#425228}

--- ui/compositor/layer.cc
+++ ui/compositor/layer.cc
@@ -97,7 +97,8 @@ Layer::Layer(LayerType type)
 }
 
 Layer::~Layer() {
-  FOR_EACH_OBSERVER(LayerObserver, observer_list_, LayerDestroyed(this));
+  for (auto& observer : observer_list_)
+    observer.LayerDestroyed(this);
 
   // Destroying the animator may cause observers to use the layer (and
   // indirectly the WebLayer). Destroy the animator first so that the WebLayer
@@ -593,7 +594,8 @@ void Layer::SetShowSurface(
   frame_size_in_dip_ = frame_size_in_dip;
   RecomputeDrawsContentAndUVRect();
 
-  FOR_EACH_OBSERVER(LayerObserver, observer_list_, SurfaceChanged(this));
+  for (auto& observer : observer_list_)
+    observer.SurfaceChanged(this);
 }
 
 void Layer::SetShowSolidColorContent() {
@@ -796,8 +798,8 @@ scoped_refptr<cc::DisplayItemList> Layer::PaintContentsToDisplayList(
         PaintContext(display_list.get(), device_scale_factor_, invalidation));
   }
   display_list->Finalize();
-  FOR_EACH_OBSERVER(LayerObserver, observer_list_,
-                    DidPaintLayer(this, invalidation));
+  for (auto& observer : observer_list_)
+    observer.DidPaintLayer(this, invalidation);
   return display_list;
 }
 
--- ui/compositor/test/in_process_context_factory.cc
+++ ui/compositor/test/in_process_context_factory.cc
@@ -132,7 +132,8 @@ InProcessContextFactory::~InProcessContextFactory() {
 }
 
 void InProcessContextFactory::SendOnLostResources() {
-  FOR_EACH_OBSERVER(ContextFactoryObserver, observer_list_, OnLostResources());
+  for (auto& observer : observer_list_)
+    observer.OnLostResources();
 }
 
 void InProcessContextFactory::CreateCompositorFrameSink(
