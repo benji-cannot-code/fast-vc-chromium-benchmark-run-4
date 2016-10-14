5ece869a53a4aa7812ba7299f5abe26eca16bbae
agrieve <agrieve@chromium.org>
2016-10-14 11:49:13 -0700

    Reland #2 of Move language pak files to assets
    Reverted in:
    https://codereview.chromium.org/2406093002/
    and https://codereview.chromium.org/2404043002
    
    Reason for reland:
    Now auto-detects whether locale paks are compressed or stored
    (for chrome, but not webview)
    
    TBR=michaelbai
    BUG=653771,644377, 529604
    
    Review-Url: https://codereview.chromium.org/2404253003
    Cr-Commit-Position: refs/heads/master@{#425402}

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumFactoryProvider.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumFactoryProvider.java
@@ -46,7 +46,6 @@ import org.chromium.android_webview.AwQuotaManagerBridge;
 import org.chromium.android_webview.AwResource;
 import org.chromium.android_webview.AwSettings;
 import org.chromium.android_webview.HttpAuthDatabase;
-import org.chromium.android_webview.R;
 import org.chromium.android_webview.ResourcesContextWrapperFactory;
 import org.chromium.base.BuildConfig;
 import org.chromium.base.CommandLine;
@@ -63,7 +62,6 @@ import org.chromium.base.library_loader.LibraryProcessType;
 import org.chromium.base.library_loader.ProcessInitException;
 import org.chromium.content.browser.ContentViewStatics;
 import org.chromium.net.NetworkChangeNotifier;
-import org.chromium.ui.base.ResourceBundle;
 
 import java.io.File;
 import java.util.Queue;
@@ -393,7 +391,6 @@ public class WebViewChromiumFactoryProvider implements WebViewFactoryProvider {
         final String webViewPackageName = WebViewFactory.getLoadedPackageInfo().packageName;
         Context context = ContextUtils.getApplicationContext();
         setUpResources(webViewPackageName, context);
-        ResourceBundle.initializeLocalePaks(context, R.array.locale_paks);
         initPlatSupportLibrary();
         initNetworkChangeNotifier(context);
         final int extraBindFlags = Context.BIND_EXTERNAL_SERVICE;
