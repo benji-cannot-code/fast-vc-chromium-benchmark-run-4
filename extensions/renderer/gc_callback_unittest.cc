c153fccee2616d263fb797e23ef17104ac9f9e30
rdevlin.cronin <rdevlin.cronin@chromium.org>
2016-10-14 10:27:16 -0700

    [Blink] Pass in and check the execution context for script executors
    Pass in and check the context in SuspendableScriptExecutor before
    calling into V8ScriptRunner to execute the script.
    
    BUG=654344
    
    Review-Url: https://codereview.chromium.org/2410863005
    Cr-Commit-Position: refs/heads/master@{#425370}

--- extensions/renderer/gc_callback_unittest.cc
+++ extensions/renderer/gc_callback_unittest.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/renderer/script_context.h"
 #include "extensions/renderer/script_context_set.h"
 #include "gin/function_template.h"
-#include "gin/public/context_holder.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/WebKit/public/web/WebFrame.h"
 #include "v8/include/v8.h"
@@ -60,15 +59,15 @@ class GCCallbackTest : public testing::Test {
   void SetUp() override {
     v8::Isolate* isolate = v8::Isolate::GetCurrent();
     v8::HandleScope handle_scope(isolate);
-    v8::Local<v8::Context> local_v8_context = v8::Context::New(isolate);
+    // We need a context that has been initialized by blink; grab the main world
+    // context from the web frame.
+    v8::Local<v8::Context> local_v8_context =
+        web_frame_.frame()->mainWorldScriptContext();
+    DCHECK(!local_v8_context.IsEmpty());
     v8_context_.Reset(isolate, local_v8_context);
-    // ScriptContexts rely on gin.
-    gin_context_holder_.reset(new gin::ContextHolder(isolate));
-    gin_context_holder_->SetContext(local_v8_context);
   }
 
   void TearDown() override {
-    gin_context_holder_.reset();
     v8_context_.Reset();
     RequestGarbageCollection();
   }
@@ -78,7 +77,6 @@ class GCCallbackTest : public testing::Test {
   ExtensionIdSet active_extensions_;
   ScriptContextSet script_context_set_;
   v8::Global<v8::Context> v8_context_;
-  std::unique_ptr<gin::ContextHolder> gin_context_holder_;
 
   DISALLOW_COPY_AND_ASSIGN(GCCallbackTest);
 };
