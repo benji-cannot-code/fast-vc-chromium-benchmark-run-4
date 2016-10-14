8e136cb66e657c195ef5c167889f1daaec8fef88
yosin <yosin@chromium.org>
2016-10-14 03:49:11 -0700

    Get rid of flat tree version of createVisibleSelection() taking position with affinity
    This patch gets rid of flat tree version of |createVisibleSelection()| taking
    position with affinity by replacing with |SelectionInFlatTree| version to
    reduce number of overloads for improving code health.
    
    Since |SelectionTemplate::Builder::collapse()| doesn't accept null
    position, this patch changes |hahandleMousePressEventSingleClick()| to avoid
    passing null position to |collapse()|.
    
    BUG=n/a
    TEST=n/a; no behavior changes
    
    Review-Url: https://codereview.chromium.org/2420823002
    Cr-Commit-Position: refs/heads/master@{#425290}

--- third_party/WebKit/Source/core/editing/VisibleSelection.cpp
+++ third_party/WebKit/Source/core/editing/VisibleSelection.cpp
@@ -163,12 +163,6 @@ VisibleSelectionInFlatTree createVisibleSelection(
 }
 
 VisibleSelectionInFlatTree createVisibleSelection(
-    const PositionInFlatTreeWithAffinity& pos,
-    bool isDirectional) {
-  return createVisibleSelection(pos.position(), pos.affinity(), isDirectional);
-}
-
-VisibleSelectionInFlatTree createVisibleSelection(
     const VisiblePositionInFlatTree& pos,
     bool isDirectional) {
   DCHECK(pos.isValid());
