3e6a32d3b147e97c8e6349acd89eefc43b68568d
hongchan <hongchan@chromium.org>
2016-10-14 02:30:49 -0700

    [worklets] Add AudioWorkletGlobalScope and AudioWorkletThread
    This CL is to add AudioWorkletGlobalScope and AudioWorkletThread. This
    change is largely based on the https://codereview.chromium.org/2254593002,
    but some adaptations have been made to accommodate the AudioWorklet
    design.
    
    - AudioWorkletGlobalScope extends ThreadedWorkletGlobalScope
    - AudioWorklerThread uses WebThread(SupportingGC)
    
    BUG=469639
    TEST=AudioWorkletThreadTest.cpp
    
    Review-Url: https://codereview.chromium.org/2372303002
    Cr-Commit-Position: refs/heads/master@{#425278}

---
+++ third_party/WebKit/Source/modules/webaudio/AudioWorkletGlobalScope.cpp
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "modules/webaudio/AudioWorkletGlobalScope.h"
+
+#include "platform/weborigin/SecurityOrigin.h"
+
+namespace blink {
+
+AudioWorkletGlobalScope* AudioWorkletGlobalScope::create(
+    const KURL& url,
+    const String& userAgent,
+    PassRefPtr<SecurityOrigin> securityOrigin,
+    v8::Isolate* isolate,
+    WorkerThread* thread) {
+  return new AudioWorkletGlobalScope(url, userAgent, std::move(securityOrigin),
+                                     isolate, thread);
+}
+
+AudioWorkletGlobalScope::AudioWorkletGlobalScope(
+    const KURL& url,
+    const String& userAgent,
+    PassRefPtr<SecurityOrigin> securityOrigin,
+    v8::Isolate* isolate,
+    WorkerThread* thread)
+    : ThreadedWorkletGlobalScope(url,
+                                 userAgent,
+                                 std::move(securityOrigin),
+                                 isolate,
+                                 thread) {}
+
+AudioWorkletGlobalScope::~AudioWorkletGlobalScope() {}
+
+}  // namespace blink
---
+++ third_party/WebKit/Source/modules/webaudio/AudioWorkletGlobalScope.h
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef AudioWorkletGlobalScope_h
+#define AudioWorkletGlobalScope_h
+
+#include "core/workers/ThreadedWorkletGlobalScope.h"
+
+namespace blink {
+
+class AudioWorkletGlobalScope final : public ThreadedWorkletGlobalScope {
+  DEFINE_WRAPPERTYPEINFO();
+
+ public:
+  static AudioWorkletGlobalScope* create(const KURL&,
+                                         const String& userAgent,
+                                         PassRefPtr<SecurityOrigin>,
+                                         v8::Isolate*,
+                                         WorkerThread*);
+  ~AudioWorkletGlobalScope() override;
+
+ private:
+  AudioWorkletGlobalScope(const KURL&,
+                          const String& userAgent,
+                          PassRefPtr<SecurityOrigin>,
+                          v8::Isolate*,
+                          WorkerThread*);
+};
+
+}  // namespace blink
+
+#endif  // AudioWorkletGlobalScope_h
