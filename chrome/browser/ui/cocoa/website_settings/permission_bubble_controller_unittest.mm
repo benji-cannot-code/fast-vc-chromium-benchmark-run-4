bda191632e4e0d7821f1e3af67af0e0cf0c05d95
avi <avi@chromium.org>
2016-10-14 13:40:09 -0700

    Remove stl_util's deletion functions from website_settings.
    BUG=555865
    
    Review-Url: https://codereview.chromium.org/2405763002
    Cr-Commit-Position: refs/heads/master@{#425456}

--- chrome/browser/ui/cocoa/website_settings/permission_bubble_controller_unittest.mm
+++ chrome/browser/ui/cocoa/website_settings/permission_bubble_controller_unittest.mm
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/foundation_util.h"
 #import "base/mac/scoped_objc_class_swizzler.h"
 #include "base/mac/sdk_forward_declarations.h"
-#include "base/stl_util.h"
+#include "base/memory/ptr_util.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/permissions/mock_permission_request.h"
@@ -90,16 +90,17 @@ class PermissionBubbleControllerTest : public CocoaProfileTest,
   void TearDown() override {
     [controller_ close];
     chrome::testing::NSRunLoopRunAllPending();
-    base::STLDeleteElements(&requests_);
+    owned_requests_.clear();
     CocoaProfileTest::TearDown();
   }
 
   void AddRequest(const std::string& title) {
-    MockPermissionRequest* request = new MockPermissionRequest(
-        title,
-        l10n_util::GetStringUTF8(IDS_PERMISSION_ALLOW),
-        l10n_util::GetStringUTF8(IDS_PERMISSION_DENY));
-    requests_.push_back(request);
+    std::unique_ptr<MockPermissionRequest> request =
+        base::MakeUnique<MockPermissionRequest>(
+            title, l10n_util::GetStringUTF8(IDS_PERMISSION_ALLOW),
+            l10n_util::GetStringUTF8(IDS_PERMISSION_DENY));
+    requests_.push_back(request.get());
+    owned_requests_.push_back(std::move(request));
   }
 
   NSButton* FindButtonWithTitle(const std::string& title) {
@@ -161,6 +162,7 @@ class PermissionBubbleControllerTest : public CocoaProfileTest,
   PermissionBubbleController* controller_;  // Weak;  it deletes itself.
   std::unique_ptr<PermissionBubbleCocoa> bridge_;
   std::vector<PermissionRequest*> requests_;
+  std::vector<std::unique_ptr<PermissionRequest>> owned_requests_;
   std::vector<bool> accept_states_;
 };
 
--- chrome/browser/ui/website_settings/website_settings.cc
+++ chrome/browser/ui/website_settings/website_settings.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/i18n/time_formatting.h"
 #include "base/macros.h"
+#include "base/memory/ptr_util.h"
 #include "base/metrics/field_trial.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/string_number_conversions.h"
@@ -774,11 +775,13 @@ void WebsiteSettings::PresentSitePermissions() {
     auto chosen_objects = context->GetGrantedObjects(origin, origin);
     for (std::unique_ptr<base::DictionaryValue>& object : chosen_objects) {
       chosen_object_info_list.push_back(
-          new WebsiteSettingsUI::ChosenObjectInfo(ui_info, std::move(object)));
+          base::MakeUnique<WebsiteSettingsUI::ChosenObjectInfo>(
+              ui_info, std::move(object)));
     }
   }
 
-  ui_->SetPermissionInfo(permission_info_list, chosen_object_info_list);
+  ui_->SetPermissionInfo(permission_info_list,
+                         std::move(chosen_object_info_list));
 }
 
 void WebsiteSettings::PresentSiteData() {
