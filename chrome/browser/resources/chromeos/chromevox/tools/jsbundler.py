6e887276ad92f06ef3cdf21be188dd7269f4b8c0
kinaba <kinaba@chromium.org>
2016-10-14 00:33:28 -0700

    chromevox: Fix a racy failure in directory creation in jsbundler.py.
    Previous implementation can fail if other concurrent task created the
    directory between path.exists and makedirs.
    
    BUG=655750
    TEST=Manually build select_to_speak with ninja -j10 many times.
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2421613003
    Cr-Commit-Position: refs/heads/master@{#425268}

--- chrome/browser/resources/chromeos/chromevox/tools/jsbundler.py
+++ chrome/browser/resources/chromeos/chromevox/tools/jsbundler.py
@@ -31,6 +31,7 @@ ways:
 '''
 
 
+import errno
 import optparse
 import os
 import re
@@ -234,8 +235,11 @@ def LinkOrCopyFiles(sources, dest_dir):
   '''Copies a list of sources to a destination directory.'''
 
   def LinkOrCopyOneFile(src, dst):
-    if not os.path.exists(os.path.dirname(dst)):
+    try:
       os.makedirs(os.path.dirname(dst))
+    except OSError as err:
+      if err.errno != errno.EEXIST:
+        raise
     if os.path.exists(dst):
       os.unlink(dst)
     try:
