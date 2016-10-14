695115dcf4fbae4c6b092952bf5bbcb4ca4bf5ae
bshe <bshe@chromium.org>
2016-10-14 15:35:10 -0700

    Fix a compile error
    This introduced by https://codereview.chromium.org/2417743003/
    Not sure why bots didn't catch it
    
    BUG=NONE
    
    Review-Url: https://codereview.chromium.org/2421973002
    Cr-Commit-Position: refs/heads/master@{#425495}

--- chrome/browser/android/vr_shell/vr_shell.cc
+++ chrome/browser/android/vr_shell/vr_shell.cc
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/referrer.h"
 #include "content/public/common/screen_info.h"
-#include "jni/VrShell_jni.h"
+#include "jni/VrShellImpl_jni.h"
 #include "third_party/WebKit/public/web/WebInputEvent.h"
 #include "ui/android/view_android.h"
 #include "ui/android/window_android.h"
