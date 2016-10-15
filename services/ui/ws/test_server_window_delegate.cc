85fbefec00a7c1b730b1fc70136deaf4a26506dd
fsamuel <fsamuel@chromium.org>
2016-10-15 00:28:01 -0700

    Mus+Ash: propagate Surface ID to parents
    Currently, in Mus+Ash, the window server is responsible for positioning
    windows and generating SurfaceDrawQuads for them. This CL propagates surface
    IDs for child windows to their parents so that parents can embed them directly
    via CompositorFrames. This will happen in a subsequent patch.
    
    A SurfaceSequence is allocated for the surface ID that is propagated up the
    window hierarchy. As long as the parent holds on to the SurfaceSequence,
    then the surface ID will be preserved. Once the parent is informed of a
    new surface ID then it will return the SurfaceSequence back to the window
    server for reclamation (mus-gpu in the future). If a window is destroyed then
    it will also return its SurfaceSequence.
    
    BUG=647852
    
    Review-Url: https://codereview.chromium.org/2414683003
    Cr-Commit-Position: refs/heads/master@{#425556}

--- services/ui/ws/test_server_window_delegate.cc
+++ services/ui/ws/test_server_window_delegate.cc
@@ -12,7 +12,8 @@ namespace ui {
 namespace ws {
 
 TestServerWindowDelegate::TestServerWindowDelegate()
-    : root_window_(nullptr), display_compositor_(new DisplayCompositor()) {}
+    : root_window_(nullptr),
+      display_compositor_(new DisplayCompositor(nullptr)) {}
 
 TestServerWindowDelegate::~TestServerWindowDelegate() {}
 
--- services/ui/ws/window_tree.cc
+++ services/ui/ws/window_tree.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/ui/ws/platform_display.h"
 #include "services/ui/ws/server_window.h"
 #include "services/ui/ws/server_window_observer.h"
+#include "services/ui/ws/server_window_surface_manager.h"
 #include "services/ui/ws/user_display_manager.h"
 #include "services/ui/ws/window_manager_display_root.h"
 #include "services/ui/ws/window_manager_state.h"
@@ -757,6 +758,26 @@ void WindowTree::ProcessTransientWindowRemoved(
                                      transient_client_window_id.id);
 }
 
+void WindowTree::ProcessWindowSurfaceChanged(ServerWindow* window,
+                                             const cc::SurfaceId& surface_id,
+                                             const gfx::Size& frame_size,
+                                             float device_scale_factor) {
+  ServerWindow* parent_window = window->parent();
+  ClientWindowId client_window_id, parent_client_window_id;
+  if (!IsWindowKnown(window, &client_window_id) ||
+      !IsWindowKnown(parent_window, &parent_client_window_id) ||
+      !created_window_map_.count(parent_window->id())) {
+    return;
+  }
+
+  ServerWindowSurfaceManager* surface_manager =
+      window->GetOrCreateSurfaceManager();
+  ServerWindowSurface* surface = surface_manager->GetDefaultSurface();
+  cc::SurfaceSequence sequence = surface->CreateSurfaceSequence();
+  client()->OnWindowSurfaceChanged(client_window_id.id, surface_id, sequence,
+                                   frame_size, device_scale_factor);
+}
+
 void WindowTree::SendToPointerWatcher(const ui::Event& event,
                                       ServerWindow* target_window) {
   if (!EventMatchesPointerWatcher(event))
@@ -1338,6 +1359,15 @@ void WindowTree::AttachSurface(Id transport_window_id,
     return;
   }
   window->CreateSurface(type, std::move(surface), std::move(client));
+}
+
+void WindowTree::OnWindowSurfaceDetached(Id transport_window_id,
+                                         const cc::SurfaceSequence& sequence) {
+  ServerWindow* window =
+      GetWindowByClientId(ClientWindowId(transport_window_id));
+  if (!window)
+    return;
+  window_server_->GetDisplayCompositor()->ReturnSurfaceReference(sequence);
 }
 
 void WindowTree::SetWindowTextInputState(Id transport_window_id,
