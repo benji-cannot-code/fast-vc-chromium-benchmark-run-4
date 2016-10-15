484440600ed6ab34bdc367cd46c8166e0f44ed19
einbinder <einbinder@chromium.org>
2016-10-14 20:30:28 -0700

    DevTools: Preserve hintMarker for as long as possible in TextEditorAutocompleteController Removing and creating a hint marker is expensive.
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2352193002
    Cr-Commit-Position: refs/heads/master@{#425546}

--- third_party/WebKit/Source/devtools/front_end/externs.js
+++ third_party/WebKit/Source/devtools/front_end/externs.js
@@ -593,7 +593,8 @@ CodeMirror.StringStream.prototype = {
 CodeMirror.TextMarker = function(doc, type) { }
 CodeMirror.TextMarker.prototype = {
     clear: function() { },
-    find: function() { }
+    find: function() { },
+    changed: function() { }
 }
 
 /** @constructor */
