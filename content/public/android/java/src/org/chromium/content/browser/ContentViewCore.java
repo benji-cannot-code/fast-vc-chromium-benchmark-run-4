eb6f3a30f3b1164ac8ab4d63b59862f925c1ce88
timav <timav@chromium.org>
2016-10-14 19:35:41 -0700

    Moved FORCE_DEVICE_SCALE_FACTOR to DisplayAndroid
    On Java side the commend line switch --force-device_scale-factor
    used to be processed by RenderCoordinates (a part of ContentViewCore),
    move it from content/ fo ui/ layer.
    
    This is a prerequisite to https://codereview.chromium.org/2300463002
    
    BUG=620929
    
    Review-Url: https://codereview.chromium.org/2414173002
    Cr-Commit-Position: refs/heads/master@{#425539}

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -628,15 +628,6 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Displa
     }
 
     /**
-     * Returns the context used to obtain display density (ratio of physical pixels to DIP).
-     */
-    private Context getDisplayContext(WindowAndroid window) {
-        Context displayContext = null;
-        if (window != null) displayContext = window.getContext().get();
-        return displayContext != null ? displayContext : mContext;
-    }
-
-    /**
      *
      * @param viewDelegate Delegate to add/remove anchor views.
      * @param internalDispatcher Handles dispatching all hidden or super methods to the
@@ -669,7 +660,7 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Displa
         setContainerViewInternals(internalDispatcher);
 
         mRenderCoordinates.reset();
-        mRenderCoordinates.setDeviceScaleFactor(getDisplayContext(windowAndroid));
+        mRenderCoordinates.updateDeviceScaleFactorFromWindow(windowAndroid);
 
         initPopupZoomer(mContext);
         mImeAdapter = createImeAdapter();
@@ -693,7 +684,6 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Displa
         mSelectPopup = null;
         mPastePopupMenu = null;
 
-        mRenderCoordinates.setDeviceScaleFactor(getDisplayContext(windowAndroid));
         addDisplayAndroidObserverIfNeeded();
 
         for (WindowAndroidChangedObserver observer : mWindowAndroidChangedObservers) {
@@ -705,6 +695,7 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Displa
         if (!mAttachedToWindow) return;
         WindowAndroid windowAndroid = getWindowAndroid();
         if (windowAndroid != null) {
+            mRenderCoordinates.updateDeviceScaleFactorFromWindow(windowAndroid);
             windowAndroid.getDisplay().addObserver(this);
             onRotationChanged(windowAndroid.getDisplay().getRotation());
         }
---
+++ ui/android/java/src/org/chromium/ui/display/DisplaySwitches.java
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.ui.display;
+
+/**
+ * Contains all of the command line switches that are specific to the display.
+ */
+public abstract class DisplaySwitches {
+    // Native switch - display_switches::kForceDeviceScaleFactor
+    public static final String FORCE_DEVICE_SCALE_FACTOR = "force-device-scale-factor";
+
+    // Prevent instantiation.
+    private DisplaySwitches() {}
+}
