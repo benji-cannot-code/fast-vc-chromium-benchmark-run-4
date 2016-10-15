76136363b97bf60c923475f31609017307e6871a
ananta <ananta@chromium.org>
2016-10-14 19:09:27 -0700

    PlzNavigate: Fix the failing ContinueWhereILeftOffTest.PostWithPassword test.
    This test exercises the session restore code path by posting passwords via a top level FORM post and
    then exits the browser and restarts it. Assumption being that after restart we won't have the post data
    available as it should have been stripped here https://cs.chromium.org/chromium/src/components/sessions/content/content_serialized_navigation_driver.cc?dr&q=GetSanitizedPageSta&sq=package:chromium&l=101
    in the sesson restore code path during exit.
    
    This fails in PlzNavigate because the FrameHostMsg_BeginNavigation IPC only sends the post data via the CommonNavigationParams structure.
    Hence when the request eventually completes in the renderer via the FrameMsg_CommitNavigation IPC, we lose the fact that the post contains passwords.
    This eventually makes it back to the browser via the PageState which also loses the flag.
    
    Fix is to pass the flag which indicates the post sensitive data like passwords in the ResourceRequestBodyImpl structure.
    
    BUG=655422
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2406053002
    Cr-Commit-Position: refs/heads/master@{#425533}

--- content/child/web_url_request_util.cc
+++ content/child/web_url_request_util.cc
@@ -234,6 +234,7 @@ WebHTTPBody GetWebHTTPBodyForRequestBody(
   WebHTTPBody http_body;
   http_body.initialize();
   http_body.setIdentifier(input->identifier());
+  http_body.setContainsPasswordData(input->contains_sensitive_info());
   for (const auto& element : *input->elements()) {
     switch (element.type()) {
       case ResourceRequestBodyImpl::Element::TYPE_BYTES:
@@ -329,6 +330,7 @@ scoped_refptr<ResourceRequestBodyImpl> GetRequestBodyForWebHTTPBody(
     }
   }
   request_body->set_identifier(httpBody.identifier());
+  request_body->set_contains_sensitive_info(httpBody.containsPasswordData());
   return request_body;
 }
 
