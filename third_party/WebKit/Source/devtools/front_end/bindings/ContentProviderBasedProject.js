50fcae29ccf09fa57cc7e290a40ab5d4272682cd
lushnikov <lushnikov@chromium.org>
2016-10-14 19:48:53 -0700

    DevTools: teach network UISourceCodes to return metadata
    The patch teaches network-based UISourceCodes to return non-null
    metadata.
    
    For the resource-based UISourceCodes, the metadata simply comes
    from the underlying resource.
    
    For the sourcemap-originating UISourceCodes which content was
    embedded in the sourcemap, the contentSize metadata is available.
    
    Drive-By: improve complexity of WI.ResourceTreeModel.resourceForURL
    from O(|resources|) to O(|frames|).
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2408203007
    Cr-Commit-Position: refs/heads/master@{#425540}

--- third_party/WebKit/Source/devtools/front_end/bindings/ContentProviderBasedProject.js
+++ third_party/WebKit/Source/devtools/front_end/bindings/ContentProviderBasedProject.js
@@ -46,6 +46,8 @@ WebInspector.ContentProviderBasedProject = function(workspace, id, type, display
     workspace.addProject(this);
 }
 
+WebInspector.ContentProviderBasedProject._metadata = Symbol("ContentProviderBasedProject.Metadata");
+
 WebInspector.ContentProviderBasedProject.prototype = {
     /**
      * @override
@@ -65,7 +67,7 @@ WebInspector.ContentProviderBasedProject.prototype = {
      */
     requestMetadata: function(uiSourceCode)
     {
-        return Promise.resolve(/** @type {?WebInspector.UISourceCodeMetadata} */(null));
+        return Promise.resolve(uiSourceCode[WebInspector.ContentProviderBasedProject._metadata]);
     },
 
     /**
@@ -276,10 +278,12 @@ WebInspector.ContentProviderBasedProject.prototype = {
     /**
      * @param {!WebInspector.UISourceCode} uiSourceCode
      * @param {!WebInspector.ContentProvider} contentProvider
+     * @param {?WebInspector.UISourceCodeMetadata} metadata
      */
-    addUISourceCodeWithProvider: function(uiSourceCode, contentProvider)
+    addUISourceCodeWithProvider: function(uiSourceCode, contentProvider, metadata)
     {
         this._contentProviders[uiSourceCode.url()] = contentProvider;
+        uiSourceCode[WebInspector.ContentProviderBasedProject._metadata] = metadata;
         this.addUISourceCode(uiSourceCode, true);
     },
 
@@ -291,7 +295,7 @@ WebInspector.ContentProviderBasedProject.prototype = {
     addContentProvider: function(url, contentProvider)
     {
         var uiSourceCode = this.createUISourceCode(url, contentProvider.contentType());
-        this.addUISourceCodeWithProvider(uiSourceCode, contentProvider);
+        this.addUISourceCodeWithProvider(uiSourceCode, contentProvider, null);
         return uiSourceCode;
     },
 
