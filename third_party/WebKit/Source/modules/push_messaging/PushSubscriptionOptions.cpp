2baafa83426ed6ad0811fea3dea75d8b331d9a6c
harkness <harkness@chromium.org>
2016-10-17 03:34:23 -0700

    Check the format of an applicationServerKey when used to register a push subscription.
    The Web Push specificatoion requires applicationServerKeys to be valid VAPID keys,
    but we have not previous enforced that, which has led to issues where web developers
    attempt to subscribe with invalid keys. The error eventually returned is not
    informative.
    
    This CL adds code in Blink to do some basic sanity checking that the provided value is
    syntactically valid. Although the spec only allows p256 keys to be valid, this code
    also allows the web developer to subscribe with a numeric sender ID as an argument to
    subscribe() instead of specifying it in the manifest file, but checks that the ID provided
    is a number.
    
    This CL also adds testing for the new syntax checking.
    
    BUG=636022
    
    Review-Url: https://codereview.chromium.org/2411733002
    Cr-Commit-Position: refs/heads/master@{#425649}

--- third_party/WebKit/Source/modules/push_messaging/PushSubscriptionOptions.cpp
+++ third_party/WebKit/Source/modules/push_messaging/PushSubscriptionOptions.cpp
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/push_messaging/PushSubscriptionOptionsInit.h"
 #include "public/platform/WebString.h"
 #include "public/platform/modules/push_messaging/WebPushSubscriptionOptions.h"
+#include "third_party/WebKit/Source/wtf/ASCIICType.h"
 #include "wtf/Assertions.h"
 #include "wtf/text/WTFString.h"
 
@@ -22,10 +23,9 @@ const int kMaxApplicationServerKeyLength = 255;
 String bufferSourceToString(
     const ArrayBufferOrArrayBufferView& applicationServerKey,
     ExceptionState& exceptionState) {
-  // Check the validity of the sender info. It must be a 65-byte uncompressed
-  // key, which has the byte 0x04 as the first byte as a marker.
   unsigned char* input;
   int length;
+  // Convert the input array into a string of bytes.
   if (applicationServerKey.isArrayBuffer()) {
     input = static_cast<unsigned char*>(
         applicationServerKey.getAsArrayBuffer()->data());
@@ -40,9 +40,16 @@ String bufferSourceToString(
     return String();
   }
 
-  // If the key is valid, just treat it as a string of bytes and pass it to
-  // the push service.
-  if (length <= kMaxApplicationServerKeyLength)
+  // Check the validity of the sender info. It must either be a 65-byte
+  // uncompressed VAPID key, which has the byte 0x04 as the first byte or a
+  // numeric sender ID.
+  const bool isVapid = length == 65 && *input == 0x04;
+  const bool isSenderId =
+      length > 0 && length < kMaxApplicationServerKeyLength &&
+      (std::find_if_not(input, input + length,
+                        &WTF::isASCIIDigit<unsigned char>) == input + length);
+
+  if (isVapid || isSenderId)
     return WebString::fromLatin1(input, length);
 
   exceptionState.throwDOMException(
