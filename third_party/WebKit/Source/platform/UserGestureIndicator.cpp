712d9a923d8a246cf0d8716aaea6a203541a14b4
jyasskin <jyasskin@chromium.org>
2016-10-17 16:14:54 -0700

    Count how many times user gesture tokens are merged.
    In the "Blink.Gesture.Merged" histogram. This will help determine
    if we can change the merging behavior when standardizing user
    gestures.
    
    BUG=404161
    
    Review-Url: https://codereview.chromium.org/2417153003
    Cr-Commit-Position: refs/heads/master@{#425807}

--- third_party/WebKit/Source/platform/UserGestureIndicator.cpp
+++ third_party/WebKit/Source/platform/UserGestureIndicator.cpp
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "platform/UserGestureIndicator.h"
 
+#include "platform/Histogram.h"
 #include "wtf/Assertions.h"
 #include "wtf/CurrentTime.h"
 
@@ -46,7 +47,7 @@ UserGestureToken::UserGestureToken(Status status)
     m_consumableGestures++;
 }
 
-bool UserGestureToken::hasGestures() {
+bool UserGestureToken::hasGestures() const {
   return m_consumableGestures && !hasTimedOut();
 }
 
@@ -92,6 +93,27 @@ void UserGestureToken::userGestureUtilized() {
   }
 }
 
+// This enum is used in a histogram, so its values shouldn't change.
+enum GestureMergeState {
+  OldTokenHasGesture = 1 << 0,
+  NewTokenHasGesture = 1 << 1,
+  GestureMergeStateEnd = 1 << 2,
+};
+
+// Remove this when user gesture propagation is standardized. See
+// https://crbug.com/404161.
+static void RecordUserGestureMerge(const UserGestureToken& oldToken,
+                                   const UserGestureToken& newToken) {
+  DEFINE_STATIC_LOCAL(EnumerationHistogram, gestureMergeHistogram,
+                      ("Blink.Gesture.Merged", GestureMergeStateEnd));
+  int sample = 0;
+  if (oldToken.hasGestures())
+    sample |= OldTokenHasGesture;
+  if (newToken.hasGestures())
+    sample |= NewTokenHasGesture;
+  gestureMergeHistogram.count(sample);
+}
+
 UserGestureToken* UserGestureIndicator::s_rootToken = nullptr;
 bool UserGestureIndicator::s_processedUserGestureSinceLoad = false;
 
@@ -101,10 +123,12 @@ UserGestureIndicator::UserGestureIndicator(PassRefPtr<UserGestureToken> token)
   if (!isMainThread() || !m_token)
     return;
 
-  if (!s_rootToken)
+  if (!s_rootToken) {
     s_rootToken = m_token.get();
-  else
+  } else {
+    RecordUserGestureMerge(*s_rootToken, *m_token);
     m_token->transferGestureTo(s_rootToken);
+  }
   s_processedUserGestureSinceLoad = true;
 }
 
