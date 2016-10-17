b629f942b0d21ba064bfad2b698a0caca3ef96b4
ellyjones <ellyjones@chromium.org>
2016-10-17 05:21:42 -0700

    views: add focus ring to TreeView
    When TreeView is focused, there should be a focus ring around the control,
    at least on Mac. This is mildly complicated because TreeViews are often
    hosted in a ScrollView, and in that case the focus ring should go around
    the ScrollView instead of the TreeView so the focus ring does not scroll.
    
    BUG=605589,647515
    
    Review-Url: https://codereview.chromium.org/2411693002
    Cr-Commit-Position: refs/heads/master@{#425662}

--- ui/views/controls/scroll_view.cc
+++ ui/views/controls/scroll_view.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/native_theme/native_theme.h"
 #include "ui/views/background.h"
 #include "ui/views/border.h"
+#include "ui/views/controls/focus_ring.h"
 #include "ui/views/style/platform_style.h"
 #include "ui/views/widget/widget.h"
 
@@ -272,6 +273,18 @@ void ScrollView::SetVerticalScrollBar(ScrollBar* vert_sb) {
   vert_sb_ = vert_sb;
 }
 
+void ScrollView::SetHasFocusRing(bool has_focus_ring) {
+  if (has_focus_ring == (focus_ring_ != nullptr))
+    return;
+  if (has_focus_ring) {
+    focus_ring_ = FocusRing::Install(this);
+  } else {
+    FocusRing::Uninstall(this);
+    focus_ring_ = nullptr;
+  }
+  SchedulePaint();
+}
+
 gfx::Size ScrollView::GetPreferredSize() const {
   if (!is_bounded())
     return View::GetPreferredSize();
@@ -295,6 +308,9 @@ int ScrollView::GetHeightForWidth(int width) const {
 }
 
 void ScrollView::Layout() {
+  if (focus_ring_)
+    focus_ring_->Layout();
+
   gfx::Rect available_rect = GetContentsBounds();
   if (is_bounded()) {
     int content_width = available_rect.width();
--- ui/views/controls/tree/tree_view.cc
+++ ui/views/controls/tree/tree_view.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "ui/accessibility/ax_view_state.h"
 #include "ui/base/ime/input_method.h"
+#include "ui/base/material_design/material_design_controller.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/events/event.h"
 #include "ui/events/keycodes/keyboard_codes.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/skia_util.h"
 #include "ui/native_theme/native_theme.h"
 #include "ui/resources/grit/ui_resources.h"
+#include "ui/views/controls/focus_ring.h"
 #include "ui/views/controls/prefix_selector.h"
 #include "ui/views/controls/scroll_view.h"
 #include "ui/views/controls/textfield/textfield.h"
@@ -632,6 +634,8 @@ void TreeView::OnFocus() {
   // Notify the InputMethod so that it knows to query the TextInputClient.
   if (GetInputMethod())
     GetInputMethod()->OnCaretBoundsChanged(GetPrefixSelector());
+
+  SetHasFocusRing(true);
 }
 
 void TreeView::OnBlur() {
@@ -640,6 +644,7 @@ void TreeView::OnBlur() {
   SchedulePaintForNode(selected_node_);
   if (selector_)
     selector_->OnViewBlur();
+  SetHasFocusRing(false);
 }
 
 bool TreeView::OnClickOrTap(const ui::LocatedEvent& event) {
@@ -1056,6 +1061,22 @@ bool TreeView::IsPointInExpandControl(InternalNode* node,
   if (base::i18n::IsRTL())
     arrow_bounds.set_x(bounds().width() - arrow_dx - kArrowRegionSize);
   return arrow_bounds.Contains(point);
+}
+
+void TreeView::SetHasFocusRing(bool shows) {
+  if (!ui::MaterialDesignController::IsSecondaryUiMaterial() ||
+      !PlatformStyle::kTreeViewHasFocusRing) {
+    return;
+  }
+  // If this View is the grandchild of a ScrollView, use the grandparent
+  // ScrollView for the focus ring instead of this View so that the focus ring
+  // won't be scrolled. Since ScrollViews have a single content view, which they
+  // wrap in a ScrollView::Viewport, being the grandchild of a ScrollView
+  // implies being the sole grandchild, which means it's fine to wrap the focus
+  // ring around the grandparent here.
+  View* grandparent = parent() ? parent()->parent() : nullptr;
+  if (grandparent && grandparent->GetClassName() == ScrollView::kViewClassName)
+    static_cast<ScrollView*>(grandparent)->SetHasFocusRing(shows);
 }
 
 // InternalNode ----------------------------------------------------------------
