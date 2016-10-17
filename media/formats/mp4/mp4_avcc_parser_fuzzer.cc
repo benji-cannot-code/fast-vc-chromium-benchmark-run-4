8cb446a8ebb286ec6dea7b3ccb97c25421cf270e
watk <watk@chromium.org>
2016-10-17 14:16:28 -0700

    Add a fuzzer target for MP4 AVCC record parsing
    Review-Url: https://codereview.chromium.org/2401963003
    Cr-Commit-Position: refs/heads/master@{#425779}

---
+++ media/formats/mp4/mp4_avcc_parser_fuzzer.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "base/logging.h"
+#include "media/formats/mp4/box_definitions.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  media::mp4::AVCDecoderConfigurationRecord().Parse(data, size);
+  return 0;
+}
+
+// For disabling noisy logging.
+struct Environment {
+  Environment() { logging::SetMinLogLevel(logging::LOG_FATAL); }
+};
+
+Environment* env = new Environment();
