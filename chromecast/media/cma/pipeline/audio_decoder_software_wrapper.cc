7a0616ba6980c91199e82fbec795916a3445d58f
kmackay <kmackay@chromium.org>
2016-10-17 16:27:27 -0700

    [Chromecast] Log whether or not software decoding is used
    BUG= none
    
    Review-Url: https://codereview.chromium.org/2425063002
    Cr-Commit-Position: refs/heads/master@{#425810}

--- chromecast/media/cma/pipeline/audio_decoder_software_wrapper.cc
+++ chromecast/media/cma/pipeline/audio_decoder_software_wrapper.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromecast/media/cma/pipeline/audio_decoder_software_wrapper.h"
 
+#include <ostream>
+
 #include "base/bind.h"
 #include "base/logging.h"
 #include "base/threading/thread_task_runner_handle.h"
@@ -12,6 +14,39 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromecast {
 namespace media {
+
+namespace {
+
+std::ostream& operator<<(std::ostream& stream, AudioCodec codec) {
+  switch (codec) {
+    case kAudioCodecUnknown:
+      return stream << "unknown codec";
+    case kCodecAAC:
+      return stream << "AAC";
+    case kCodecMP3:
+      return stream << "MP3";
+    case kCodecPCM:
+      return stream << "PCM";
+    case kCodecPCM_S16BE:
+      return stream << "PCM_S16BE";
+    case kCodecVorbis:
+      return stream << "Vorbis";
+    case kCodecOpus:
+      return stream << "Opus";
+    case kCodecEAC3:
+      return stream << "EAC3";
+    case kCodecAC3:
+      return stream << "AC3";
+    case kCodecDTS:
+      return stream << "DTS";
+    case kCodecFLAC:
+      return stream << "FLAC";
+  }
+  NOTREACHED();
+  return stream << "UNKNOWN";
+}
+
+}  // namespace
 
 AudioDecoderSoftwareWrapper::AudioDecoderSoftwareWrapper(
     MediaPipelineBackend::AudioDecoder* backend_decoder)
@@ -55,13 +90,18 @@ bool AudioDecoderSoftwareWrapper::SetConfig(const AudioConfig& config) {
   DCHECK(IsValidConfig(config));
 
   if (backend_decoder_->SetConfig(config)) {
+    LOG(INFO) << "Using backend decoder for " << config.codec;
     software_decoder_.reset();
     output_config_ = config;
     return true;
   }
 
-  if (!CreateSoftwareDecoder(config))
+  if (!CreateSoftwareDecoder(config)) {
+    LOG(INFO) << "Failed to create software decoder";
     return false;
+  }
+
+  LOG(INFO) << "Using software decoder for " << config.codec;
 
   output_config_.codec = media::kCodecPCM;
   output_config_.sample_format = media::kSampleFormatS16;
