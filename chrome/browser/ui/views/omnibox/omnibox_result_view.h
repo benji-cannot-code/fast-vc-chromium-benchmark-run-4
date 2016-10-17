ab914480e83e1982c42972b18ea3f8d5a40b61b4
sdefresne <sdefresne@chromium.org>
2016-10-17 14:37:07 -0700

    Reland of Delete pre-MD code from OmniboxResultView
    Reland of https://codereview.chromium.org/2365263006/ after the
    downstream iOS code was fixed to not uses the deleted resources
    or methods.
    
    BUG=648281
    
    Review-Url: https://codereview.chromium.org/2422193002
    Cr-Commit-Position: refs/heads/master@{#425783}

--- chrome/browser/ui/views/omnibox/omnibox_result_view.h
+++ chrome/browser/ui/views/omnibox/omnibox_result_view.h
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/image_view.h"
 #include "ui/views/view.h"
 
-class LocationBarView;
 class OmniboxPopupContentsView;
 
 namespace gfx {
@@ -51,7 +50,6 @@ class OmniboxResultView : public views::View,
 
   OmniboxResultView(OmniboxPopupContentsView* model,
                     int model_index,
-                    LocationBarView* location_bar_view,
                     const gfx::FontList& font_list);
   ~OmniboxResultView() override;
 
@@ -134,8 +132,6 @@ class OmniboxResultView : public views::View,
 
   gfx::ImageSkia GetIcon() const;
 
-  gfx::ImageSkia GetKeywordIcon() const;
-
   // Utility function for creating vector icons.
   gfx::ImageSkia GetVectorIcon(gfx::VectorIconId icon_id) const;
 
@@ -185,13 +181,9 @@ class OmniboxResultView : public views::View,
                               int text_type,
                               bool is_bold) const;
 
-  static int default_icon_size_;
-
   // This row's model and model index.
   OmniboxPopupContentsView* model_;
   size_t model_index_;
-
-  LocationBarView* location_bar_view_;
 
   const gfx::FontList font_list_;
   int font_height_;
