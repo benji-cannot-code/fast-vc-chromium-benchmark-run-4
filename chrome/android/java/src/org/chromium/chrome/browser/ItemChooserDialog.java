67cf95a9086e7154f3412cfae54f0c7f67ba20ba
twellington <twellington@chromium.org>
2016-10-17 16:29:42 -0700

    Revert "Inherit default text color from parent themes."
    This reverts commit e4f0a51143b3db3643b7bac31d0be4d08642fe4e.
    
    This CL broke the text color for disabled text fields and dialog
    buttons.
    
    BUG=655930, 654062
    TBR=dfalcantara@chromium.org
    
    Review-Url: https://codereview.chromium.org/2427853002
    Cr-Commit-Position: refs/heads/master@{#425812}

--- chrome/android/java/src/org/chromium/chrome/browser/ItemChooserDialog.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ItemChooserDialog.java
@@ -11,8 +11,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.support.annotation.ColorInt;
-import android.support.annotation.ColorRes;
 import android.text.SpannableString;
 import android.text.method.LinkMovementMethod;
 import android.view.Gravity;
@@ -138,6 +136,9 @@ public class ItemChooserDialog {
         // The background color of the highlighted item.
         private final int mBackgroundHighlightColor;
 
+        // The color of the non-highlighted text.
+        private final int mDefaultTextColor;
+
         // The zero-based index of the item currently selected in the dialog,
         // or -1 (INVALID_POSITION) if nothing is selected.
         private int mSelectedItem = ListView.INVALID_POSITION;
@@ -158,6 +159,8 @@ public class ItemChooserDialog {
 
             mBackgroundHighlightColor = ApiCompatibilityUtils.getColor(getContext().getResources(),
                     R.color.light_active_color);
+            mDefaultTextColor = ApiCompatibilityUtils.getColor(getContext().getResources(),
+                    R.color.default_text_color);
         }
 
         @Override
@@ -298,22 +301,18 @@ public class ItemChooserDialog {
                         R.layout.item_chooser_dialog_row, parent, false);
             }
 
-
             // Set highlighting for currently selected item.
             if (position == mSelectedItem) {
                 view.setBackgroundColor(mBackgroundHighlightColor);
                 view.setTextColor(Color.WHITE);
             } else {
                 view.setBackground(null);
-
-                @ColorRes final int textColorId = isEnabled(position)
-                        ? R.color.default_text_color
-                        : R.color.primary_text_disabled_material_light;
-
-                @ColorInt final int textColor = ApiCompatibilityUtils.getColor(
-                        getContext().getResources(), textColorId);
-
-                view.setTextColor(textColor);
+                if (!isEnabled(position)) {
+                    view.setTextColor(ApiCompatibilityUtils.getColor(getContext().getResources(),
+                            R.color.primary_text_disabled_material_light));
+                } else {
+                    view.setTextColor(mDefaultTextColor);
+                }
             }
 
             view.setText(getDisplayText(position));
--- chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ContextMenuTitleView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ContextMenuTitleView.java
@@ -10,6 +10,9 @@ import android.text.TextUtils;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import org.chromium.base.ApiCompatibilityUtils;
+import org.chromium.chrome.R;
+
 /**
  * Context menu title text view that is restricted height and scrollable.
  */
@@ -39,6 +42,8 @@ public class ContextMenuTitleView extends ScrollView {
             title = sb.toString();
         }
         titleView.setText(title);
+        titleView.setTextColor(ApiCompatibilityUtils.getColor(getResources(),
+                R.color.default_text_color));
         titleView.setPadding(0, 0, 0, padding);
         addView(titleView);
     }
