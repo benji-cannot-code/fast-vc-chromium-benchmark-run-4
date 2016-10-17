b0da287fe95ccd9e96a6abfab49511582637698e
nasko <nasko@chromium.org>
2016-10-17 13:52:25 -0700

    Enable ExtensionApiTest.TabEvents test.
    This test has been disabled on Linux/Windows due to a CL that was
    reverted long time ago. Let's try to re-enable the test.
    
    BUG=145639
    
    Review-Url: https://codereview.chromium.org/2428663002
    Cr-Commit-Position: refs/heads/master@{#425773}

--- chrome/browser/extensions/extension_tabs_apitest.cc
+++ chrome/browser/extensions/extension_tabs_apitest.cc
@@ -30,13 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MAYBE_UpdateWindowShowState UpdateWindowShowState
 #endif  // defined(USE_AURA) || defined(OS_MACOSX)
 
-// http://crbug.com/145639
-#if defined(OS_LINUX) || defined(OS_WIN)
-#define MAYBE_TabEvents DISABLED_TabEvents
-#else
-#define MAYBE_TabEvents TabEvents
-#endif
-
 class ExtensionApiNewTabTest : public ExtensionApiTest {
  public:
   ExtensionApiNewTabTest() {}
@@ -112,7 +105,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionApiTest, MAYBE_TabMove) {
   ASSERT_TRUE(RunExtensionSubtest("tabs/basics", "move.html")) << message_;
 }
 
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, MAYBE_TabEvents) {
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, TabEvents) {
   ASSERT_TRUE(RunExtensionSubtest("tabs/basics", "events.html")) << message_;
 }
 
