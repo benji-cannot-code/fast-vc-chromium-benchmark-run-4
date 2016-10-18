ab878a4c916cf06468807e74d22abc472b902b7e
chenwilliam <chenwilliam@chromium.org>
2016-10-18 12:36:23 -0700

    DevTools: add "npm test" to run tests by fetching content shells
    Running "npm test" from /devtools fetches an uploaded content shell
    (which is cached) and runs devtools layout tests.
    
    This allows developers, especially external contributors, to run our tests
    without compiling.
    
    API:
    * "npm test" - builds devtools and run all inspector layout tests (primary command)
    * "npm test -- -f --child-processes=16" - pass in additional flags to the test harness
    * "npm test -- inspector/sources inspector/console" - run specific tests
    * "npm test -- inspector/cookie-resource-match.html --debug-devtools" - debug a specific test (non-bundled & minified)
    * "npm run test:build" - only builds devtools (in debug mode)
    * "npm run test:run" - only runs devtools tests
    
    This currently works w/ a full-checkout, but I plan on working
    to enable running devtools layout tests w/ only a frontend checkout.
    
    BUG=654486
    
    Review-Url: https://codereview.chromium.org/2413563002
    Cr-Commit-Position: refs/heads/master@{#426024}

--- third_party/WebKit/Source/devtools/scripts/utils.js
+++ third_party/WebKit/Source/devtools/scripts/utils.js
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var fs = require("fs");
 var http = require("http");
 var https = require("https");
+var path = require("path");
 var parseURL = require("url").parse;
 var Stream = require("stream").Transform;
 
@@ -46,18 +47,88 @@ function atob(str)
     return new Buffer(str, "base64").toString("binary");
 }
 
-
 function isFile(path)
 {
     try {
         return fs.statSync(path).isFile();
-    } catch (e) {
+    } catch (error) {
         return false;
     }
+}
+
+function isDir(path)
+{
+    try {
+        return fs.statSync(path).isDirectory();
+    } catch (error) {
+        return false;
+    }
+}
+
+function copy(src, dest)
+{
+    try {
+        var targetFilePath = path.resolve(dest, path.basename(src));
+        fs.writeFileSync(targetFilePath, fs.readFileSync(src));
+    } catch (error) {
+        throw new Error(`Received an error: [${error}] while trying to copy: ${src} -> ${dest}`);
+    }
+}
+
+function copyRecursive(src, dest)
+{
+    try {
+        var targetDirPath = path.resolve(dest, path.basename(src));
+        if (!fs.existsSync(targetDirPath))
+            fs.mkdirSync(targetDirPath);
+        if (isDir(src)) {
+            var files = fs.readdirSync(src);
+            for (var i = 0; i < files.length; i++) {
+                var childPath = path.resolve(src, files[i]);
+                if (isDir(childPath)) {
+                    copyRecursive(childPath, targetDirPath);
+                } else {
+                    var targetFilePath =  path.resolve(targetDirPath, path.basename(childPath));
+                    fs.writeFileSync(targetFilePath, fs.readFileSync(childPath));
+                }
+            }
+        }
+    } catch (error) {
+        throw new Error(`Received an error: [${error}] while trying to copy: ${src} -> ${dest}`);
+    }
+}
+
+function removeRecursive(filePath)
+{
+    try {
+        if (fs.existsSync(filePath)) {
+            var files = fs.readdirSync(filePath);
+            for (var i = 0; i < files.length; i++) {
+                var childPath = path.resolve(filePath, files[i]);
+                if (isDir(childPath))
+                    removeRecursive(childPath);
+                else
+                    fs.unlinkSync(childPath);
+            }
+            fs.rmdirSync(filePath);
+        }
+    } catch (error) {
+        throw new Error(`Received an error: [${error}] while trying to remove: ${filePath}`);
+    }
+}
+
+function includes(sequence, target)
+{
+    return sequence.indexOf(target) > -1;
 }
 
 module.exports = {
     fetch,
     atob,
     isFile,
+    isDir,
+    copy,
+    copyRecursive,
+    removeRecursive,
+    includes,
 };
