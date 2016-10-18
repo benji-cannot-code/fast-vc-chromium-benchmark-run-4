50297801a0664143affc6235af739ad4750ef62f
mohsen <mohsen@chromium.org>
2016-10-17 17:18:03 -0700

    Vectorize audio icons in Ash system menu
    This includes:
     - Volume level icons (low, medium, and high);
     - Mute icon;
     - USB icon;
     - HDMI icon.
    
    BUG=646537
    TEST=TEST=manually run Ash with --ash-md=experimental and check icons
    
    Review-Url: https://codereview.chromium.org/2413313003
    Cr-Commit-Position: refs/heads/master@{#425829}

--- ash/common/system/audio/tray_audio_delegate.h
+++ ash/common/system/audio/tray_audio_delegate.h
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_COMMON_SYSTEM_AUDIO_TRAY_AUDIO_DELEGATE_H_
 #define ASH_COMMON_SYSTEM_AUDIO_TRAY_AUDIO_DELEGATE_H_
 
+namespace gfx {
+struct VectorIcon;
+}
+
 namespace ash {
 namespace system {
 
@@ -27,8 +31,13 @@ class ASH_EXPORT TrayAudioDelegate {
   // should be muted.
   virtual int GetOutputDefaultVolumeMuteLevel() = 0;
 
-  // Gets the icon to use for the active output device.
+  // Gets the non-MD icon to use for the active output device.
+  // TODO(mohsen): Remove this once material design is enabled by default. See
+  // https://crbug.com/614453.
   virtual int GetActiveOutputDeviceIconId() = 0;
+
+  // Gets the MD icon to use for the active output device.
+  virtual const gfx::VectorIcon& GetActiveOutputDeviceVectorIcon() = 0;
 
   // Returns the volume level of the output device in the range 0%-100%.
   virtual int GetOutputVolumeLevel() = 0;
--- ash/common/system/chromeos/audio/tray_audio_delegate_chromeos.cc
+++ ash/common/system/chromeos/audio/tray_audio_delegate_chromeos.cc
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/common/system/chromeos/audio/tray_audio_delegate_chromeos.h"
 
+#include "ash/resources/vector_icons/vector_icons.h"
 #include "chromeos/audio/cras_audio_handler.h"
 #include "grit/ash_resources.h"
+#include "ui/gfx/paint_vector_icon.h"
 
 using chromeos::CrasAudioHandler;
 
@@ -40,6 +42,22 @@ int TrayAudioDelegateChromeOs::GetActiveOutputDeviceIconId() {
     return IDR_AURA_UBER_TRAY_AUDIO_HDMI;
   else
     return kNoAudioDeviceIcon;
+}
+
+const gfx::VectorIcon&
+TrayAudioDelegateChromeOs::GetActiveOutputDeviceVectorIcon() {
+  chromeos::AudioDevice device;
+  if (CrasAudioHandler::Get()->GetPrimaryActiveOutputDevice(&device)) {
+    if (device.type == chromeos::AUDIO_TYPE_HEADPHONE)
+      return kSystemMenuHeadsetIcon;
+    if (device.type == chromeos::AUDIO_TYPE_USB)
+      return kSystemMenuUsbIcon;
+    if (device.type == chromeos::AUDIO_TYPE_BLUETOOTH)
+      return kSystemMenuBluetoothIcon;
+    if (device.type == chromeos::AUDIO_TYPE_HDMI)
+      return kSystemMenuHdmiIcon;
+  }
+  return gfx::kNoneIcon;
 }
 
 bool TrayAudioDelegateChromeOs::HasAlternativeSources() {
