9e61e9a2ff4daecf67d02545e4ffe1a002c97489
dschuyler <dschuyler@chromium.org>
2016-10-18 11:54:41 -0700

    [MD settings] name Content settings separately from site settings
    This CL puts the 'site settings' name behind the enable-site-settings
    flag. When site settings is disabled, the name 'content settings' is
    used.
    
    BUG=654940
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://chromiumcodereview.appspot.com/2422233004
    Cr-Commit-Position: refs/heads/master@{#426013}

--- chrome/browser/ui/webui/settings/md_settings_localized_strings_provider.cc
+++ chrome/browser/ui/webui/settings/md_settings_localized_strings_provider.cc
@@ -1226,6 +1226,7 @@ void AddPrivacyStrings(content::WebUIDataSource* html_source) {
       {"manageCertificates", IDS_SETTINGS_MANAGE_CERTIFICATES},
       {"manageCertificatesDescription",
        IDS_SETTINGS_MANAGE_CERTIFICATES_DESCRIPTION},
+      {"contentSettings", IDS_SETTINGS_CONTENT_SETTINGS},
       {"siteSettings", IDS_SETTINGS_SITE_SETTINGS},
       {"siteSettingsDescription", IDS_SETTINGS_SITE_SETTINGS_DESCRIPTION},
       {"clearBrowsingData", IDS_SETTINGS_CLEAR_DATA},
