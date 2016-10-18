755d43024adfacf44766cecbf99c619e64663eee
shimazu <shimazu@chromium.org>
2016-10-18 09:00:08 -0700

    ServiceWorker: Exchange InterfaceProviders when starting worker thread
    Browser process sends the InterfaceProvider bound to InterfaceRegistry on
    EmbeddedWorkerInstance in this patch. This enables SWContextClient to send IPCs
    via mojo when its context is starting.
    
    BUG=629701
    
    Review-Url: https://codereview.chromium.org/2313653002
    Cr-Commit-Position: refs/heads/master@{#425978}

--- content/renderer/service_worker/embedded_worker_instance_client_impl.cc
+++ content/renderer/service_worker/embedded_worker_instance_client_impl.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_client.h"
 #include "content/renderer/service_worker/embedded_worker_devtools_agent.h"
 #include "content/renderer/service_worker/service_worker_context_client.h"
+#include "services/service_manager/public/cpp/interface_registry.h"
 #include "third_party/WebKit/public/web/WebEmbeddedWorker.h"
 #include "third_party/WebKit/public/web/WebEmbeddedWorkerStartData.h"
 
@@ -24,6 +25,12 @@ void EmbeddedWorkerInstanceClientImpl::Create(
     mojo::InterfaceRequest<mojom::EmbeddedWorkerInstanceClient> request) {
   // This won't be leaked because the lifetime will be managed internally.
   new EmbeddedWorkerInstanceClientImpl(dispatcher, std::move(request));
+}
+
+void EmbeddedWorkerInstanceClientImpl::ExposeInterfacesToBrowser(
+    service_manager::InterfaceRegistry* interface_registry) {
+  DCHECK(renderer_request_.is_pending());
+  interface_registry->Bind(std::move(renderer_request_));
 }
 
 void EmbeddedWorkerInstanceClientImpl::StopWorkerCompleted() {
@@ -38,10 +45,15 @@ void EmbeddedWorkerInstanceClientImpl::StopWorkerCompleted() {
 }
 
 void EmbeddedWorkerInstanceClientImpl::StartWorker(
-    const EmbeddedWorkerStartParams& params) {
+    const EmbeddedWorkerStartParams& params,
+    service_manager::mojom::InterfaceProviderPtr browser_interfaces,
+    service_manager::mojom::InterfaceProviderRequest renderer_request) {
+  DCHECK(ChildThreadImpl::current());
   TRACE_EVENT0("ServiceWorker",
                "EmbeddedWorkerInstanceClientImpl::StartWorker");
   embedded_worker_id_ = params.embedded_worker_id;
+  remote_interfaces_.Bind(std::move(browser_interfaces));
+  renderer_request_ = std::move(renderer_request);
 
   std::unique_ptr<EmbeddedWorkerDispatcher::WorkerWrapper> wrapper =
       dispatcher_->StartWorkerContext(
@@ -56,6 +68,7 @@ void EmbeddedWorkerInstanceClientImpl::StartWorker(
 
 void EmbeddedWorkerInstanceClientImpl::StopWorker(
     const StopWorkerCallback& callback) {
+  DCHECK(ChildThreadImpl::current());
   DCHECK(embedded_worker_id_);
   DCHECK(!stop_callback_);
   TRACE_EVENT0("ServiceWorker", "EmbeddedWorkerInstanceClientImpl::StopWorker");
