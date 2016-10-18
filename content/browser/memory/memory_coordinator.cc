af1b10974d1bd554e5cbe96f04eef331dc3fd064
hajimehoshi <hajimehoshi@chromium.org>
2016-10-18 00:10:47 -0700

    WIP: Add MemoryCoordinator::GetCurrentMemoryState
    This depends on crrev/2374343002
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2412413002
    Cr-Commit-Position: refs/heads/master@{#425916}

--- content/browser/memory/memory_coordinator.cc
+++ content/browser/memory/memory_coordinator.cc
@@ -121,6 +121,10 @@ mojom::MemoryState MemoryCoordinator::GetMemoryState(
   return iter->second.memory_state;
 }
 
+base::MemoryState MemoryCoordinator::GetCurrentMemoryState() const {
+  return base::MemoryState::UNKNOWN;
+}
+
 void MemoryCoordinator::AddChildForTesting(
     int dummy_render_process_id, mojom::ChildMemoryCoordinatorPtr child) {
   mojom::MemoryCoordinatorHandlePtr mch;
