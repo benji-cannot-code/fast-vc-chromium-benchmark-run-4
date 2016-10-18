b0824abdb1c588759004ecd5f8ea55002f33a37d
domlaskowski <domlaskowski@chromium.org>
2016-10-18 16:43:58 -0700

    ash: Filter root window children when resizing
    When a root window is resized, the bounds of its children and grand-
    children are updated to match, which assumes that all windows in the
    second and third levels of the tree are containers. This CL skips
    children that are descendants of a top-level window.
    
    In Exosphere, the pointer is a 0x0 widget whose parent is the mouse
    cursor container. After a display resolution change, its bounds would
    be reset to the display bounds. When positioning a widget using screen
    coordinates, the target display is the one that most closely intersects
    its bounds. Due to its size, the pointer would sometimes be positioned
    in the off-screen area of another display, in which case the cursor was
    invisible.
    
    Also, this CL partially reverts crrev.com/216184, which relied on the
    above behavior but has been obsoleted by CursorWindowController.
    
    BUG=642894
    TEST=ash_unittests
    TEST=Enable paint rect debugging and observe that the 0x0 size of the
         Exo pointer is preserved after changing the display resolution.
    TEST=Exo cursor is always visible after display resolution change.
    TEST=No regression with software cursor on login screen in extended
         and unified desktop mode.
    
    Review-Url: https://codereview.chromium.org/2333923006
    Cr-Commit-Position: refs/heads/master@{#426086}

--- ash/aura/wm_root_window_controller_aura.h
+++ ash/aura/wm_root_window_controller_aura.h
@@ -43,7 +43,6 @@ class ASH_EXPORT WmRootWindowControllerAura : public WmRootWindowController {
   gfx::Point GetLastMouseLocationInRoot() override;
   void OnInitialWallpaperAnimationStarted() override;
   void OnWallpaperAnimationFinished(views::Widget* widget) override;
-  void UpdateAfterLoginStatusChange(LoginStatus status) override;
 
  protected:
   // WmRootWindowController:
