47370cb0b1e1ce582d3f3bf9e5547c929322430a
alexclarke <alexclarke@chromium.org>
2016-10-18 09:57:15 -0700

    Revert of [Reland] Optimize blink scheduler with an intrusive heap (patchset #12 id:240001 of https://codereview.chromium.org/2419793002/ )
    Reason for revert:
    Looks like an assert is firing:
    https://build.chromium.org/p/tryserver.chromium.linux/builders/linux_chromium_rel_ng/builds/318138
    
    Original issue's description:
    > [Reland] Optimize blink scheduler with an intrusive heap.
    >
    > This shows a ~10% improvement on the TaskQueueManagerPerfTest
    > micro benchmark.
    >
    > Alternatives considered: A flat map/set
    > https://codereview.chromium.org/2396533004/
    >
    > BUG=
    >
    > Committed: https://crrev.com/36d98e3b544f310943986dcaa98beabacdbccc96
    > Committed: https://crrev.com/1e13bd23dd2aac48f6bbcce65a1c56b14d5254ef
    > Cr-Original-Commit-Position: refs/heads/master@{#425647}
    > Cr-Commit-Position: refs/heads/master@{#425930}
    
    TBR=altimin@chromium.org,haraken@chromium.org,skyostil@chromium.org,yutak@chromium.org,ojan@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=
    
    Review-Url: https://codereview.chromium.org/2428073002
    Cr-Commit-Position: refs/heads/master@{#425992}

--- third_party/WebKit/Source/platform/scheduler/base/work_queue_sets.h
+++ third_party/WebKit/Source/platform/scheduler/base/work_queue_sets.h
@@ -14,9 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/trace_event/trace_event_argument.h"
-#include "platform/scheduler/base/intrusive_heap.h"
 #include "platform/scheduler/base/task_queue_impl.h"
-#include "platform/scheduler/base/work_queue.h"
 #include "public/platform/WebCommon.h"
 
 namespace blink {
@@ -58,7 +56,7 @@ class BLINK_PLATFORM_EXPORT WorkQueueSets {
   bool IsSetEmpty(size_t set_index) const;
 
 #if DCHECK_IS_ON() || !defined(NDEBUG)
-  // Note this iterates over everything in |work_queue_heaps_|.
+  // Note this iterates over everything in |enqueue_order_to_work_queue_maps_|.
   // It's intended for use with DCHECKS and for testing
   bool ContainsWorkQueueForTest(const WorkQueue* queue) const;
 #endif
@@ -66,20 +64,8 @@ class BLINK_PLATFORM_EXPORT WorkQueueSets {
   const char* name() const { return name_; }
 
  private:
-  struct OldestTaskEnqueueOrder {
-    EnqueueOrder key;
-    WorkQueue* value;
-
-    bool operator<=(const OldestTaskEnqueueOrder& other) const {
-      return key <= other.key;
-    }
-
-    void SetHeapHandle(HeapHandle handle) { value->set_heap_handle(handle); }
-  };
-
-  // For each set |work_queue_heaps_| has a queue of WorkQueue ordered by the
-  // oldest task in each WorkQueue.
-  std::vector<IntrusiveHeap<OldestTaskEnqueueOrder>> work_queue_heaps_;
+  typedef std::map<EnqueueOrder, WorkQueue*> EnqueueOrderToWorkQueueMap;
+  std::vector<EnqueueOrderToWorkQueueMap> enqueue_order_to_work_queue_maps_;
   const char* name_;
 
   DISALLOW_COPY_AND_ASSIGN(WorkQueueSets);
