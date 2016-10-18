96d96926e81f3f92eec89c94dcab1ec3edd1a216
pkl <pkl@chromium.org>
2016-10-18 09:15:22 -0700

    Removed unused include file in credential_manager.
    This should have been part of https://codereview.chromium.org/2425543002/
    
    BUG=633626
    
    Review-Url: https://codereview.chromium.org/2425933003
    Cr-Commit-Position: refs/heads/master@{#425983}

--- ios/chrome/browser/passwords/credential_manager.mm
+++ ios/chrome/browser/passwords/credential_manager.mm
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "base/ios/ios_util.h"
 #import "base/ios/weak_nsobject.h"
 #include "base/mac/bind_objc_block.h"
 #include "base/memory/scoped_vector.h"
