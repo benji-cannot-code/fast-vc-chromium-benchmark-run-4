7854d8f2f1f87255039caa2767bc263eef2b4f37
Guido Urdaneta <guidou@chromium.org>
2016-10-19 14:13:56 +0200

    Disable flaky tests on Linux and ChromeOS.
    The disabled tests are:
    * BudgetManagerBrowserTest.BudgetInWorker
    * PushMessagingBrowserTest.SubscribeWorker
    * PushMessagingBrowserTest.SubscribeWorkerUsingManifest
    
    BUG=657202
    TBR=peter@chromium.org
    
    Review URL: https://codereview.chromium.org/2429263003 .
    
    Cr-Commit-Position: refs/heads/master@{#426156}

--- chrome/browser/budget_service/budget_manager_browsertest.cc
+++ chrome/browser/budget_service/budget_manager_browsertest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "base/command_line.h"
 #include "base/run_loop.h"
+#include "build/build_config.h"
 #include "chrome/browser/budget_service/budget_manager.h"
 #include "chrome/browser/budget_service/budget_manager_factory.h"
 #include "chrome/browser/profiles/profile.h"
@@ -118,7 +119,13 @@ IN_PROC_BROWSER_TEST_F(BudgetManagerBrowserTest, BudgetInDocument) {
   ASSERT_FALSE(success());
 }
 
-IN_PROC_BROWSER_TEST_F(BudgetManagerBrowserTest, BudgetInWorker) {
+// Flaky on ChromeOS and Linux. See http://crbug.com/657202.
+#if defined(OS_CHROMEOS) || defined(OS_LINUX)
+#define MAYBE_BudgetInWorker DISABLED_BudgetInWorker
+#else
+#define MAYBE_BudgetInWorker BudgetInWorker
+#endif
+IN_PROC_BROWSER_TEST_F(BudgetManagerBrowserTest, MAYBE_BudgetInWorker) {
   std::string script_result;
 
   ASSERT_TRUE(RunScript("registerServiceWorker()", &script_result));
