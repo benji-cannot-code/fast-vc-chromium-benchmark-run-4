7ef20a044f91403f181520f99171cd9aa7ffc15e
ericwilligers <ericwilligers@chromium.org>
2016-10-19 05:29:58 -0700

    Remove FOR_EACH_OBSERVER macro usage in chrome/browser/android
    Observer lists now support range-based for loops.
    
    BUG=655021
    
    Review-Url: https://chromiumcodereview.appspot.com/2423883005
    Cr-Commit-Position: refs/heads/master@{#426158}

--- chrome/browser/android/bookmarks/partner_bookmarks_shim.cc
+++ chrome/browser/android/bookmarks/partner_bookmarks_shim.cc
@@ -124,8 +124,8 @@ void PartnerBookmarksShim::RenameBookmark(const BookmarkNode* node,
   const NodeRenamingMapKey key(node->url(), node->GetTitle());
   node_rename_remove_map_[key] = title;
   SaveNodeMapping();
-  FOR_EACH_OBSERVER(PartnerBookmarksShim::Observer, observers_,
-                    PartnerShimChanged(this));
+  for (PartnerBookmarksShim::Observer& observer : observers_)
+    observer.PartnerShimChanged(this);
 }
 
 void PartnerBookmarksShim::AddObserver(
@@ -181,8 +181,8 @@ void PartnerBookmarksShim::SetPartnerBookmarksRoot(
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   g_partner_model_keeper.Get().partner_bookmarks_root = std::move(root_node);
   g_partner_model_keeper.Get().loaded = true;
-  FOR_EACH_OBSERVER(PartnerBookmarksShim::Observer, observers_,
-                    PartnerShimLoaded(this));
+  for (PartnerBookmarksShim::Observer& observer : observers_)
+    observer.PartnerShimLoaded(this);
 }
 
 PartnerBookmarksShim::NodeRenamingMapKey::NodeRenamingMapKey(
@@ -222,8 +222,8 @@ PartnerBookmarksShim::PartnerBookmarksShim(PrefService* prefs)
 }
 
 PartnerBookmarksShim::~PartnerBookmarksShim() {
-  FOR_EACH_OBSERVER(PartnerBookmarksShim::Observer, observers_,
-                    ShimBeingDeleted(this));
+  for (PartnerBookmarksShim::Observer& observer : observers_)
+    observer.ShimBeingDeleted(this);
 }
 
 const BookmarkNode* PartnerBookmarksShim::GetNodeByID(
