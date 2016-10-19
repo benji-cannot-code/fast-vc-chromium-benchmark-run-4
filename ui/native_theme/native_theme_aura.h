f3f55a18e1333a4800a1114fc2faa68a3a3e9eec
bokan <bokan@chromium.org>
2016-10-19 08:07:58 -0700

    Change Aura overlay scrollbars from solid color to painted scrollbars.
    With this change, the Aura overlay scrollbars are now painted by the native
    theme. If the scroller is composited, these scrollbars will now be uploaded to
    the compositor as PaintedScrollbarLayers rather than SolidColorScrollbarLayers.
    
    Additionally, many of the MD scrollbar constants and properties are scattered
    around the code. This patch moves them into a single location in
    ui/native_theme. I've also updated the values to match the latest spec.
    Follow-up patches will make use of these values from the compositor.
    
    BUG=592098, 652520
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    
    Review-Url: https://chromiumcodereview.appspot.com/2421913002
    Cr-Commit-Position: refs/heads/master@{#426191}

--- ui/native_theme/native_theme_aura.h
+++ ui/native_theme/native_theme_aura.h
@@ -54,6 +54,9 @@ class NATIVE_THEME_EXPORT NativeThemeAura : public NativeThemeBase {
                                           State endState,
                                           double progress,
                                           const gfx::Rect& rect) const override;
+  gfx::Size GetPartSize(Part part,
+                        State state,
+                        const ExtraParams& extra) const override;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(NativeThemeAura);
