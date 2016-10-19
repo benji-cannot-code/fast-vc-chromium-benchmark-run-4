021920f3ce0c8c4d847c8c89f5f3b3ec8bda8e7b
fwang <fwang@igalia.com>
2016-10-19 09:00:20 -0700

    Revert of Use FileDescriptorWatcher in wl::FakeServer. (patchset #1 id:1 of https://codereview.chromium.org/2397803002)
    Reason for revert: There is no bots that build/run ozone/wayland
    unit tests at the moment, although it is being worked on in
    https://codereview.chromium.org/2423833002/.
    
    This CL caused a build failure in Wayland unit tests, see
    https://codereview.chromium.org/2434723002/. It also made them fail.
    
    One can run them with GN args
    
      dcheck_always_on = true
        is_debug = false
          symbol_level = 1
    
      target_os = "chromeos"
        use_ozone = true
          ozone_platform_wayland = true
    
    $ ninja time ninja -C <out> wayland_unittests  ozone_unittests
    $ <out>/ozone_unittests
    
    Failure:
    
    [1019/104135:ERROR:kill_posix.cc(84)] Unable to terminate process group 19687:
    No such process
    [ RUN      ] WaylandConnectionTest.UseUnstableVersion
    [19687:19687:1019/104135:94999256539:FATAL:file_descriptor_watcher_posix.cc(29)]
    Check failed: sequence_checker_.CalledOnValidSequence().
    
    Review-Url: https://chromiumcodereview.appspot.com/2431123003
    Cr-Commit-Position: refs/heads/master@{#426209}

--- ui/ozone/platform/wayland/fake_server.h
+++ ui/ozone/platform/wayland/fake_server.h
@@ -8,10 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <wayland-server-core.h>
 
-#include <memory>
-
 #include "base/bind.h"
-#include "base/files/file_descriptor_watcher_posix.h"
+#include "base/message_loop/message_pump_libevent.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/threading/thread.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -185,7 +183,7 @@ struct DisplayDeleter {
   void operator()(wl_display* display);
 };
 
-class FakeServer : public base::Thread {
+class FakeServer : public base::Thread, base::MessagePumpLibevent::Watcher {
  public:
   FakeServer();
   ~FakeServer() override;
@@ -216,9 +214,11 @@ class FakeServer : public base::Thread {
  private:
   void DoPause();
 
-  void StartWatchingFileDescriptor();
+  std::unique_ptr<base::MessagePump> CreateMessagePump();
 
-  void OnFileCanReadWithoutBlocking();
+  // base::MessagePumpLibevent::Watcher
+  void OnFileCanReadWithoutBlocking(int fd) override;
+  void OnFileCanWriteWithoutBlocking(int fd) override;
 
   std::unique_ptr<wl_display, DisplayDeleter> display_;
   wl_client* client_ = nullptr;
@@ -233,7 +233,7 @@ class FakeServer : public base::Thread {
   MockSeat seat_;
   MockXdgShell xdg_shell_;
 
-  std::unique_ptr<base::FileDescriptorWatcher::Controller> controller_;
+  base::MessagePumpLibevent::FileDescriptorWatcher controller_;
 
   DISALLOW_COPY_AND_ASSIGN(FakeServer);
 };
