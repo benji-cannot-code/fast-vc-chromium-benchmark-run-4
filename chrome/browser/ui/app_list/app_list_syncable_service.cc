bbc4316603df6793d577fb2a119b9a7310cb57cb
khmel <khmel@chromium.org>
2016-10-19 14:10:32 -0700

    Discard unused AppListPrefs code.
    This CL discard unused code that monitors changes in app list model
    but does not expose any functionality.
    
    BUG=641535
    Test=git cl try
    
    Review-Url: https://chromiumcodereview.appspot.com/2430753002
    Cr-Commit-Position: refs/heads/master@{#426283}

--- chrome/browser/ui/app_list/app_list_syncable_service.cc
+++ chrome/browser/ui/app_list/app_list_syncable_service.cc
@@ -16,11 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/drive/drive_app_provider.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/app_list/app_list_prefs.h"
 #include "chrome/browser/ui/app_list/app_list_service.h"
 #include "chrome/browser/ui/app_list/extension_app_item.h"
 #include "chrome/browser/ui/app_list/extension_app_model_builder.h"
-#include "chrome/browser/ui/app_list/model_pref_updater.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "chrome/grit/generated_resources.h"
@@ -277,7 +275,6 @@ AppListSyncableService::AppListSyncableService(
 AppListSyncableService::~AppListSyncableService() {
   // Remove observers.
   model_observer_.reset();
-  model_pref_updater_.reset();
 }
 
 void AppListSyncableService::BuildModel() {
@@ -310,9 +307,6 @@ void AppListSyncableService::BuildModel() {
       arc_apps_builder_->InitializeWithProfile(profile_, model_.get());
 #endif
   }
-
-  model_pref_updater_.reset(
-      new ModelPrefUpdater(AppListPrefs::Get(profile_), model_.get()));
 
   if (app_list::switches::IsDriveAppsInAppListEnabled())
     drive_app_provider_.reset(new DriveAppProvider(profile_, this));
