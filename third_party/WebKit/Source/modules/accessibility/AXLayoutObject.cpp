e233079a904cb7ed16f2884b0c2ebd091e97f446
dmazzoni <dmazzoni@chromium.org>
2016-10-19 02:58:04 -0700

    Accessibility: Ignore all anonymous blocks
    Anonymous blocks are an implementation detail of the
    Blink layout tree. They're surprising and not useful in
    the accessibility tree. Get rid of them by marking them
    all as ignored.
    
    While it may seem counterintuitive, this is actually
    part of a longer-term plan to get rid of the idea of
    ignoring nodes for accessibility, this will bring us
    closer to just exposing the DOM tree plus generated
    content, like FIrefox and IE do.
    
    The code change here is tiny, most of the change is
    rebasing or updating dozens and dozens of affected
    tests. I deleted a couple of tests that specifically
    tested corner cases involving anonymous blocks;
    the whole point of this change is to not make this a
    special case anymore.
    
    Also updates this script, designed to automatically
    pull test output from try servers to a local change:
    
    tools/accessibility/rebase_dump_accessibility_tree_test.py
    
    BUG=651614
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://chromiumcodereview.appspot.com/2387533003
    Cr-Commit-Position: refs/heads/master@{#426138}

--- third_party/WebKit/Source/modules/accessibility/AXLayoutObject.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXLayoutObject.cpp
@@ -501,6 +501,9 @@ bool AXLayoutObject::computeAccessibilityIsIgnored(
   if (decision == IgnoreObject)
     return true;
 
+  if (m_layoutObject->isAnonymousBlock())
+    return true;
+
   // If this element is within a parent that cannot have children, it should not
   // be exposed.
   if (isDescendantOfLeafNode()) {
