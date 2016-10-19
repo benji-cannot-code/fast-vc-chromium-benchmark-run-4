3d8e6191256aac9bacde877b91e049f32e4c0a21
jfernandez <jfernandez@igalia.com>
2016-10-19 06:00:12 -0700

    Simplifiying the firstInFlowChildBox function
    Since we use nextInFlowChildBox we don't need to iterate over the
    children, looking for the first in-flow child.
    
    Review-Url: https://chromiumcodereview.appspot.com/2439483002
    Cr-Commit-Position: refs/heads/master@{#426165}

--- third_party/WebKit/Source/core/layout/LayoutBox.h
+++ third_party/WebKit/Source/core/layout/LayoutBox.h
@@ -1579,9 +1579,9 @@ inline LayoutBox* LayoutBox::parentBox() const {
 
 inline LayoutBox* LayoutBox::firstInFlowChildBox() const {
   LayoutBox* first = firstChildBox();
-  while (first && first->isOutOfFlowPositioned())
-    first = first->nextInFlowSiblingBox();
-  return first;
+  return (first && first->isOutOfFlowPositioned())
+             ? first->nextInFlowSiblingBox()
+             : first;
 }
 
 inline LayoutBox* LayoutBox::firstChildBox() const {
