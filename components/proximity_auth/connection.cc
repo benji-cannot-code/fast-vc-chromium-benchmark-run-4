d3001cc110914f88dce8aa68a7f5a999d9c5b562
ericwilligers <ericwilligers@chromium.org>
2016-10-18 18:45:51 -0700

    Reduce usage of FOR_EACH_OBSERVER macro in components/
    Observer lists now support range-based for loops.
    
    BUG=655021
    
    Review-Url: https://chromiumcodereview.appspot.com/2423353002
    Cr-Commit-Position: refs/heads/master@{#426114}

--- components/proximity_auth/connection.cc
+++ components/proximity_auth/connection.cc
@@ -61,8 +61,8 @@ void Connection::SetStatus(Status status) {
 
   Status old_status = status_;
   status_ = status;
-  FOR_EACH_OBSERVER(ConnectionObserver, observers_,
-                    OnConnectionStatusChanged(this, old_status, status_));
+  for (auto& observer : observers_)
+    observer.OnConnectionStatusChanged(this, old_status, status_);
 }
 
 void Connection::OnDidSendMessage(const WireMessage& message, bool success) {
@@ -72,8 +72,8 @@ void Connection::OnDidSendMessage(const WireMessage& message, bool success) {
   }
 
   is_sending_message_ = false;
-  FOR_EACH_OBSERVER(
-      ConnectionObserver, observers_, OnSendCompleted(*this, message, success));
+  for (auto& observer : observers_)
+    observer.OnSendCompleted(*this, message, success);
 }
 
 void Connection::OnBytesReceived(const std::string& bytes) {
@@ -91,8 +91,8 @@ void Connection::OnBytesReceived(const std::string& bytes) {
     return;
 
   if (message) {
-    FOR_EACH_OBSERVER(
-        ConnectionObserver, observers_, OnMessageReceived(*this, *message));
+    for (auto& observer : observers_)
+      observer.OnMessageReceived(*this, *message);
   }
 
   // Whether the message was parsed successfully or not, clear the
--- components/proximity_auth/cryptauth/cryptauth_device_manager.cc
+++ components/proximity_auth/cryptauth/cryptauth_device_manager.cc
@@ -216,11 +216,12 @@ void CryptAuthDeviceManager::OnGetMyDevicesSuccess(
   sync_request_->OnDidComplete(true);
   cryptauth_client_.reset();
   sync_request_.reset();
-  FOR_EACH_OBSERVER(
-      Observer, observers_,
-      OnSyncFinished(SyncResult::SUCCESS, unlock_keys_changed
-                                              ? DeviceChangeResult::CHANGED
-                                              : DeviceChangeResult::UNCHANGED));
+  for (auto& observer : observers_) {
+    observer.OnSyncFinished(SyncResult::SUCCESS,
+                            unlock_keys_changed
+                                ? DeviceChangeResult::CHANGED
+                                : DeviceChangeResult::UNCHANGED);
+  }
 }
 
 void CryptAuthDeviceManager::OnGetMyDevicesFailure(const std::string& error) {
@@ -230,9 +231,8 @@ void CryptAuthDeviceManager::OnGetMyDevicesFailure(const std::string& error) {
   sync_request_->OnDidComplete(false);
   cryptauth_client_.reset();
   sync_request_.reset();
-  FOR_EACH_OBSERVER(
-      Observer, observers_,
-      OnSyncFinished(SyncResult::FAILURE, DeviceChangeResult::UNCHANGED));
+  for (auto& observer : observers_)
+    observer.OnSyncFinished(SyncResult::FAILURE, DeviceChangeResult::UNCHANGED);
 }
 
 std::unique_ptr<SyncScheduler> CryptAuthDeviceManager::CreateSyncScheduler() {
@@ -269,7 +269,8 @@ void CryptAuthDeviceManager::UpdateUnlockKeysFromPrefs() {
 
 void CryptAuthDeviceManager::OnSyncRequested(
     std::unique_ptr<SyncScheduler::SyncRequest> sync_request) {
-  FOR_EACH_OBSERVER(Observer, observers_, OnSyncStarted());
+  for (auto& observer : observers_)
+    observer.OnSyncStarted();
 
   sync_request_ = std::move(sync_request);
   cryptauth_client_ = client_factory_->CreateInstance();
