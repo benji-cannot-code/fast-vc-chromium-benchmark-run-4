47ae089834a9f3d16f9b0d37f5911bcc0368765d
tkent <tkent@chromium.org>
2016-10-19 06:06:20 -0700

    Input element: changing the type to "file" should discard a dirty value of the previous type.
    A dirty value was incorrectly kept after changing type to "file".
    The new behavior matches to the standard and Firefox.
    
    BUG=532403
    
    Review-Url: https://chromiumcodereview.appspot.com/2432023003
    Cr-Commit-Position: refs/heads/master@{#426168}

--- third_party/WebKit/Source/core/html/HTMLInputElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLInputElement.cpp
@@ -411,8 +411,7 @@ void HTMLInputElement::updateType() {
   InputType* newType = InputType::create(*this, newTypeName);
   removeFromRadioButtonGroup();
 
-  bool didStoreValue = m_inputType->valueMode() == ValueMode::kValue ||
-                       m_inputType->valueMode() == ValueMode::kFilename;
+  ValueMode oldValueMode = m_inputType->valueMode();
   bool didRespectHeightAndWidth =
       m_inputType->shouldRespectHeightAndWidthAttributes();
   bool couldBeSuccessfulSubmitButton = canBeSuccessfulSubmitButton();
@@ -426,8 +425,7 @@ void HTMLInputElement::updateType() {
 
   setNeedsWillValidateCheck();
 
-  bool willStoreValue = m_inputType->valueMode() == ValueMode::kValue ||
-                        m_inputType->valueMode() == ValueMode::kFilename;
+  ValueMode newValueMode = m_inputType->valueMode();
 
   // https://html.spec.whatwg.org/multipage/forms.html#input-type-change
   //
@@ -436,7 +434,10 @@ void HTMLInputElement::updateType() {
   // string, and the new state of the element's type attribute puts the value
   // IDL attribute in either the default mode or the default/on mode, then set
   // the element's value content attribute to the element's value.
-  if (didStoreValue && !willStoreValue && hasDirtyValue()) {
+  if (oldValueMode == ValueMode::kValue &&
+      (newValueMode == ValueMode::kDefault ||
+       newValueMode == ValueMode::kDefaultOn) &&
+      hasDirtyValue()) {
     setAttribute(valueAttr, AtomicString(m_valueIfDirty));
     m_valueIfDirty = String();
     m_hasDirtyValue = false;
@@ -447,11 +448,22 @@ void HTMLInputElement::updateType() {
   // value mode, then set the value of the element to the value of the value
   // content attribute, if there is one, or the empty string otherwise, and then
   // set the control's dirty value flag to false.
-  if (!didStoreValue && willStoreValue) {
+  else if (oldValueMode != ValueMode::kValue &&
+           newValueMode == ValueMode::kValue) {
     AtomicString valueString = fastGetAttribute(valueAttr);
     m_inputType->warnIfValueIsInvalid(valueString);
     m_valueIfDirty = String();
     m_hasDirtyValue = false;
+  }
+  // 3. Otherwise, if the previous state of the element's type attribute put the
+  // value IDL attribute in any mode other than the filename mode, and the new
+  // state of the element's type attribute puts the value IDL attribute in the
+  // filename mode, then set the value of the element to the empty string.
+  else if (oldValueMode != ValueMode::kFilename &&
+           newValueMode == ValueMode::kFilename) {
+    m_valueIfDirty = String();
+    m_hasDirtyValue = false;
+
   } else {
     if (!hasDirtyValue())
       m_inputType->warnIfValueIsInvalid(
