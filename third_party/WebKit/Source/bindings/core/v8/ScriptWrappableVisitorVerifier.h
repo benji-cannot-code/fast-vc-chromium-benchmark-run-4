5341a62515a3d09cb80061a820459afb3396807d
mlippautz <mlippautz@chromium.org>
2016-10-19 06:58:12 -0700

    [wrapper-tracing] Add check for missing dispatchTraceWrappers
    Check flushed out missing handling for ElementShadowV0.
    
    BUG=chromium:468240
    R=haraken@chromium.org,hlopko@chromium.org
    
    Review-Url: https://chromiumcodereview.appspot.com/2436733002
    Cr-Commit-Position: refs/heads/master@{#426175}

--- third_party/WebKit/Source/bindings/core/v8/ScriptWrappableVisitorVerifier.h
+++ third_party/WebKit/Source/bindings/core/v8/ScriptWrappableVisitorVerifier.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/NodeRareData.h"
 #include "core/dom/StyleEngine.h"
 #include "core/dom/shadow/ElementShadow.h"
+#include "core/dom/shadow/ElementShadowV0.h"
 #include "core/html/imports/HTMLImportsController.h"
 
 namespace blink {
