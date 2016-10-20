023b5a70900b116506a42abbc0944626931fa6f2
dgozman <dgozman@chromium.org>
2016-10-19 20:21:48 -0700

    [DevTools] Destroy attached clients when DevToolsHTTPHandler terminates.
    BUG=651080
    
    Review-Url: https://chromiumcodereview.appspot.com/2437723003
    Cr-Commit-Position: refs/heads/master@{#426379}

--- content/browser/devtools/devtools_http_handler.cc
+++ content/browser/devtools/devtools_http_handler.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/single_thread_task_runner.h"
-#include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
@@ -654,8 +653,9 @@ void DevToolsHttpHandler::OnWebSocketRequest(
             thread_->task_runner(),
             base::Bind(&DevToolsSocketFactory::CreateForTethering,
                        base::Unretained(socket_factory_)));
-    connection_to_client_[connection_id] = new DevToolsAgentHostClientImpl(
-        thread_->message_loop(), server_wrapper_, connection_id, browser_agent);
+    connection_to_client_[connection_id].reset(new DevToolsAgentHostClientImpl(
+        thread_->message_loop(), server_wrapper_, connection_id,
+        browser_agent));
     AcceptWebSocket(connection_id, request);
     return;
   }
@@ -679,9 +679,8 @@ void DevToolsHttpHandler::OnWebSocketRequest(
     return;
   }
 
-  DevToolsAgentHostClientImpl* client_host = new DevToolsAgentHostClientImpl(
-      thread_->message_loop(), server_wrapper_, connection_id, agent);
-  connection_to_client_[connection_id] = client_host;
+  connection_to_client_[connection_id].reset(new DevToolsAgentHostClientImpl(
+      thread_->message_loop(), server_wrapper_, connection_id, agent));
 
   AcceptWebSocket(connection_id, request);
 }
@@ -696,12 +695,7 @@ void DevToolsHttpHandler::OnWebSocketMessage(
 }
 
 void DevToolsHttpHandler::OnClose(int connection_id) {
-  ConnectionToClientMap::iterator it =
-      connection_to_client_.find(connection_id);
-  if (it != connection_to_client_.end()) {
-    delete it->second;
-    connection_to_client_.erase(connection_id);
-  }
+  connection_to_client_.erase(connection_id);
 }
 
 DevToolsHttpHandler::DevToolsHttpHandler(
