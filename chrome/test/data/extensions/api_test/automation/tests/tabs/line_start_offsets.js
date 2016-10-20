9c6043f3eb756209ce8ac1ba049719a982ff6d43
nektar <nektar@chromium.org>
2016-10-20 16:07:59 -0700

    Fixed line start offsets.
    Now they indeed refer to the offset at the start of each line. This means that if e.g. the first text node starts a new line, the first offset returned should be 0. Further, the offset equivalent to the text's length should never be returned.
    I chose to stick with line start offsets instead of line breaks because what exactly is a line break? The beginning of the line, the end of the previous line or the line break character in between two lines when it exists? And what happens if there is no line break character, i.e. a soft line break?
    Line start offsets are less ambiguous.
    BUG=652059
    R=dmazzoni@chromium.org, dtseng@chromium.org
    TESTED=automation browsertests, manually with ChromeVox on ChromeOS
    
    Review-Url: https://chromiumcodereview.appspot.com/2437473003
    Cr-Commit-Position: refs/heads/master@{#426636}

--- chrome/test/data/extensions/api_test/automation/tests/tabs/line_start_offsets.js
+++ chrome/test/data/extensions/api_test/automation/tests/tabs/line_start_offsets.js
@@ -11,8 +11,7 @@ var allTests = [
     assertTrue(!!input);
     assertTrue('lineStartOffsets' in input);
     var lineStarts = input.lineStartOffsets;
-    assertEq(1, lineStarts.length);
-    assertEq(9, lineStarts[0]);
+    assertEq(0, lineStarts.length);
     chrome.test.succeed();
   },
 
@@ -23,10 +22,9 @@ var allTests = [
     assertTrue(!!textarea);
     assertTrue('lineStartOffsets' in textarea);
     var lineStarts = textarea.lineStartOffsets;
-    assertEq(3, lineStarts.length);
+    assertEq(2, lineStarts.length);
     assertEq(10, lineStarts[0]);
     assertEq(20, lineStarts[1]);
-    assertEq(31, lineStarts[2]);
     chrome.test.succeed();
   }
 ];
