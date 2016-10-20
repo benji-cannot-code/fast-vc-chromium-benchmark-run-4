5fc3e6da27ae2fda2d6c8f1b7b9d91cf1fff8af5
yolandyan <yolandyan@chromium.org>
2016-10-19 20:33:28 -0700

    Create TestRule and WebView wrapper to load page/js sync in UiTests
    BUG=627910
    
    Review-Url: https://chromiumcodereview.appspot.com/2402183002
    Cr-Commit-Position: refs/heads/master@{#426387}

---
+++ android_webview/tools/automated_ui_tests/javatests/src/org/chromium/webview_ui_test/test/util/UseLayout.java
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.webview_ui_test.test.util;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * UseLayout annotation is used to inform WebViewUiTestRule to launch activity with specific layout
+ */
+// TODO(yolandyan): enable Parametrized test with layout, write one test, run in multiple layouts
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UseLayout {
+    String value();
+}
