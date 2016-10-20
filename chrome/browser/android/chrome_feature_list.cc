a156622cf4f9e44194dd2a3a0148d4550d5cc400
dgn <dgn@chromium.org>
2016-10-20 05:39:45 -0700

    [NTP Client] Put dismissing sections behind a flag
    Dismissing status cards (and sections) now requires the
    NTPSuggestionsSectionDismissal feature to be enabled.
    
    Dismissing the Sign In promo is still possible.
    
    BUG=657494
    
    Review-Url: https://chromiumcodereview.appspot.com/2440673002
    Cr-Commit-Position: refs/heads/master@{#426457}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -43,6 +43,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kImportantSitesInCBD,
     &ntp_snippets::kContentSuggestionsFeature,
     &ntp_snippets::kSaveToOfflineFeature,
+    &ntp_snippets::kSectionDismissalFeature,
     &kNTPFakeOmniboxTextFeature,
     &kNTPOfflinePagesFeature,
     &kPhysicalWebFeature,
