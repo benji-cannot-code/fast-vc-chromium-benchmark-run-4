b9bef047b4a5167683fe5082216d2633fe8a1840
pkl <pkl@chromium.org>
2016-10-20 10:12:26 -0700

    Removed some URL Utility functions & improved unit tests
    The removed utility functions are no longer used.
    The improved unit tests give better coverage to the
    functions that remain.
    
    Review-Url: https://chromiumcodereview.appspot.com/2364373002
    Cr-Commit-Position: refs/heads/master@{#426511}

--- ios/chrome/browser/chrome_url_util.mm
+++ ios/chrome/browser/chrome_url_util.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <UIKit/UIKit.h>
 
 #include "base/logging.h"
+#include "base/mac/foundation_util.h"
 #include "base/mac/scoped_block.h"
 #include "base/mac/scoped_nsobject.h"
 #include "base/strings/string_util.h"
@@ -21,19 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 bool UrlIsExternalFileReference(const GURL& url) {
   return url.SchemeIs(kChromeUIScheme) &&
          base::LowerCaseEqualsASCII(url.host(), kChromeUIExternalFileHost);
-}
-
-NSURL* UrlToLaunchChrome() {
-  // Determines the target URL scheme that will launch Chrome.
-  NSString* scheme = [[ChromeAppConstants sharedInstance] getBundleURLScheme];
-  return [NSURL URLWithString:[NSString stringWithFormat:@"%@://", scheme]];
-}
-
-NSURL* UrlOfChromeAppIcon(int width, int height) {
-  NSString* url =
-      [[ChromeAppConstants sharedInstance] getChromeAppIconURLOfWidth:width
-                                                               height:height];
-  return [NSURL URLWithString:url];
 }
 
 bool UrlHasChromeScheme(const GURL& url) {
@@ -57,7 +45,6 @@ bool IsHandledProtocol(const std::string& scheme) {
 
 @implementation ChromeAppConstants {
   base::scoped_nsobject<NSString> _callbackScheme;
-  base::mac::ScopedBlock<AppIconURLProvider> _appIconURLProvider;
 }
 
 + (ChromeAppConstants*)sharedInstance {
@@ -68,15 +55,14 @@ bool IsHandledProtocol(const std::string& scheme) {
 - (NSString*)getBundleURLScheme {
   if (!_callbackScheme) {
     NSSet* allowableSchemes =
-        [NSSet setWithObjects:@"googlechrome", @"chromium", nil];
+        [NSSet setWithObjects:@"googlechrome", @"chromium",
+                              @"ios-chrome-unittests.http", nil];
     NSDictionary* info = [[NSBundle mainBundle] infoDictionary];
     NSArray* urlTypes = [info objectForKey:@"CFBundleURLTypes"];
     for (NSDictionary* urlType in urlTypes) {
       DCHECK([urlType isKindOfClass:[NSDictionary class]]);
-      NSArray* schemes = [urlType objectForKey:@"CFBundleURLSchemes"];
-      if (!schemes)
-        continue;
-      DCHECK([schemes isKindOfClass:[NSArray class]]);
+      NSArray* schemes =
+          base::mac::ObjCCastStrict<NSArray>(urlType[@"CFBundleURLSchemes"]);
       for (NSString* scheme in schemes) {
         if ([allowableSchemes containsObject:scheme])
           _callbackScheme.reset([scheme copy]);
@@ -87,27 +73,8 @@ bool IsHandledProtocol(const std::string& scheme) {
   return _callbackScheme;
 }
 
-- (NSString*)getChromeAppIconURLOfWidth:(int)width height:(int)height {
-  if (!_appIconURLProvider) {
-    // TODO(ios): iOS code should default to "Chromium" branding.
-    // http://crbug.com/489270
-    _appIconURLProvider.reset(^(int width, int height) {
-      return [NSString
-          stringWithFormat:@"https://%@/Icon-%dx%d-%dx.png",
-                           @"ssl.gstatic.com/ios-apps/com.google.chrome.ios",
-                           width, height,
-                           [[UIScreen mainScreen] scale] == 1.0 ? 1 : 2];
-    }, base::scoped_policy::RETAIN);
-  }
-  return _appIconURLProvider.get()(width, height);
-}
-
 - (void)setCallbackSchemeForTesting:(NSString*)scheme {
   _callbackScheme.reset([scheme copy]);
-}
-
-- (void)setAppIconURLProviderForTesting:(AppIconURLProvider)block {
-  _appIconURLProvider.reset(block, base::scoped_policy::RETAIN);
 }
 
 @end
