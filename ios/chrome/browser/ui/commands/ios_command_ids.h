4b913667f4dab8d0b56b0921ad03e030a2534ac8
pkl <pkl@chromium.org>
2016-10-20 09:16:03 -0700

    Remove IDC_BACK_TO_CALLING_APP which is now obsolete
    This depends on https://chromereviews.googleplex.com/523537014
    which deleted the code for Back to Calling App button.
    
    BUG=633626
    
    Review-Url: https://chromiumcodereview.appspot.com/2427883002
    Cr-Commit-Position: refs/heads/master@{#426490}

--- ios/chrome/browser/ui/commands/ios_command_ids.h
+++ ios/chrome/browser/ui/commands/ios_command_ids.h
@@ -55,7 +55,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IDC_REQUEST_DESKTOP_SITE                       40921
 #define IDC_CLEAR_BROWSING_DATA_IOS                    40924
 #define IDC_SHOW_MAIL_COMPOSER                         40926
-#define IDC_BACK_TO_CALLING_APP                        40927
 #define IDC_RESET_ALL_WEBVIEWS                         40928
 #define IDC_SHARE_PAGE                                 40929
 #define IDC_BACK_FORWARD_IN_TAB_HISTORY                40930
