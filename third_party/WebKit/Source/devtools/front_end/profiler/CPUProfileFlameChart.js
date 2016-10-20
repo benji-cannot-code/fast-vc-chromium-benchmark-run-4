f13dd1c059c810fd37b2ab593cdb75e29f46c8ee
alph <alph@chromium.org>
2016-10-19 20:34:40 -0700

    DevTools: Remove unused dividerOffsets from FlameChart data provider.
    Review-Url: https://chromiumcodereview.appspot.com/2436593003
    Cr-Commit-Position: refs/heads/master@{#426388}

--- third_party/WebKit/Source/devtools/front_end/profiler/CPUProfileFlameChart.js
+++ third_party/WebKit/Source/devtools/front_end/profiler/CPUProfileFlameChart.js
@@ -72,17 +72,6 @@ WebInspector.ProfileFlameChartDataProvider.prototype = {
 
     /**
      * @override
-     * @param {number} startTime
-     * @param {number} endTime
-     * @return {?Array.<number>}
-     */
-    dividerOffsets: function(startTime, endTime)
-    {
-        return null;
-    },
-
-    /**
-     * @override
      * @return {number}
      */
     minimumBoundary: function()
--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineFlameChart.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineFlameChart.js
@@ -197,17 +197,6 @@ WebInspector.TimelineFlameChartDataProviderBase.prototype = {
 
     /**
      * @override
-     * @param {number} startTime
-     * @param {number} endTime
-     * @return {?Array.<number>}
-     */
-    dividerOffsets: function(startTime, endTime)
-    {
-        return null;
-    },
-
-    /**
-     * @override
      * @return {number}
      */
     paddingLeft: function()
