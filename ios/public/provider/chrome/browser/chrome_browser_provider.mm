2845e203adaa95e2105cde97dd7dcee0136cf71c
sdefresne <sdefresne@chromium.org>
2016-10-20 08:48:49 -0700

    Access SigninErrorProvider through ChromeBrowserProvider.
    BUG=475439
    
    Review-Url: https://chromiumcodereview.appspot.com/2437003003
    Cr-Commit-Position: refs/heads/master@{#426485}

--- ios/public/provider/chrome/browser/chrome_browser_provider.mm
+++ ios/public/provider/chrome/browser/chrome_browser_provider.mm
@@ -47,6 +47,10 @@ InfoBarViewPlaceholder ChromeBrowserProvider::CreateInfoBarView(
   return nullptr;
 }
 
+SigninErrorProvider* ChromeBrowserProvider::GetSigninErrorProvider() {
+  return nullptr;
+}
+
 SigninResourcesProvider* ChromeBrowserProvider::GetSigninResourcesProvider() {
   return nullptr;
 }
