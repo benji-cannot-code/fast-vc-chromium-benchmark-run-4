67bf4f5fc608bcd5e19fafb268e01cfe9a9188ec
tibell <tibell@chromium.org>
2016-10-19 21:46:08 -0700

    Limit Mojo messages recursion depth in Java
    BUG=640298
    
    Review-Url: https://chromiumcodereview.appspot.com/2429203003
    Cr-Commit-Position: refs/heads/master@{#426406}

--- mojo/public/java/bindings/src/org/chromium/mojo/bindings/Decoder.java
+++ mojo/public/java/bindings/src/org/chromium/mojo/bindings/Decoder.java
@@ -35,6 +35,10 @@ public class Decoder {
          * Minimal value of the start of the next memory to claim.
          */
         private long mMinNextMemory = 0;
+        /**
+         * The current nesting level when decoding.
+         */
+        private long mStackDepth;
 
         /**
          * The maximal memory accessible.
@@ -47,11 +51,17 @@ public class Decoder {
         private final long mNumberOfHandles;
 
         /**
+         * The maximum nesting level when decoding.
+         */
+        private static final int MAX_RECURSION_DEPTH = 100;
+
+        /**
          * Constructor.
          */
         Validator(long maxMemory, int numberOfHandles) {
             mMaxMemory = maxMemory;
             mNumberOfHandles = numberOfHandles;
+            mStackDepth = 0;
         }
 
         public void claimHandle(int handle) {
@@ -79,6 +89,17 @@ public class Decoder {
                 throw new DeserializationException("Trying to access out of range memory.");
             }
             mMinNextMemory = BindingsHelper.align(end);
+        }
+
+        public void increaseStackDepth() {
+            ++mStackDepth;
+            if (mStackDepth >= MAX_RECURSION_DEPTH) {
+                throw new DeserializationException("Recursion depth limit exceeded.");
+            }
+        }
+
+        public void decreaseStackDepth() {
+            --mStackDepth;
         }
     }
 
@@ -744,5 +765,13 @@ public class Decoder {
         if (mMessage.getData().limit() < offset + size) {
             throw new DeserializationException("Buffer is smaller than expected.");
         }
+    }
+
+    public void increaseStackDepth() {
+        mValidator.increaseStackDepth();
+    }
+
+    public void decreaseStackDepth() {
+        mValidator.decreaseStackDepth();
     }
 }
