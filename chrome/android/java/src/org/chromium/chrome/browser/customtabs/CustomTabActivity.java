f0cd3541ff7ebbd6d4b2c06ec058513d811bbb3f
lizeb <lizeb@chromium.org>
2016-10-20 12:01:02 -0700

    customtabs: Re-Create a spare renderer on Tab close.
    In a lot of cases, several Custom Tabs are created in a sequence,
    without the embedding application calling warmup() in between
    launches. This means that spare renderer is only available the first
    time.
    
    This changes re-creates a spare renderer once the Custom Tab is closed.
    
    BUG=617214
    
    Review-Url: https://chromiumcodereview.appspot.com/2431833002
    Cr-Commit-Position: refs/heads/master@{#426552}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
@@ -158,7 +158,7 @@ public class CustomTabActivity extends ChromeActivity {
         public void didCloseTab(int tabId, boolean incognito) {
             PageLoadMetrics.removeObserver(mMetricsObserver);
             // Finish the activity after we intent out.
-            if (getTabModelSelector().getCurrentModel().getCount() == 0) finish();
+            if (getTabModelSelector().getCurrentModel().getCount() == 0) finishAndClose(false);
         }
     };
 
@@ -364,7 +364,7 @@ public class CustomTabActivity extends ChromeActivity {
                     @Override
                     public void onClick(View v) {
                         RecordUserAction.record("CustomTabs.CloseButtonClicked");
-                        finishAndClose();
+                        finishAndClose(false);
                     }
                 });
 
@@ -645,9 +645,25 @@ public class CustomTabActivity extends ChromeActivity {
 
     /**
      * Finishes the activity and removes the reference from the Android recents.
+     *
+     * @param reparenting true iff the activity finishes due to tab reparenting.
      */
-    public final void finishAndClose() {
+    public final void finishAndClose(boolean reparenting) {
         mIsClosing = true;
+        if (!reparenting) {
+            // Closing the activity destroys the renderer as well. Re-create a spare renderer some
+            // time after, so that we have one ready for the next tab open. This does not increase
+            // memory consumption, as the current renderer goes away. We create a renderer as a lot
+            // of users open several Custom Tabs in a row. The delay is there to avoid jank in the
+            // transition animation when closing the tab.
+            ThreadUtils.postOnUiThreadDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    WarmupManager.getInstance().createSpareWebContents();
+                }
+            }, 500);
+        }
+
         handleFinishAndClose();
     }
 
@@ -672,7 +688,7 @@ public class CustomTabActivity extends ChromeActivity {
             if (getCurrentTabModel().getCount() > 1) {
                 getCurrentTabModel().closeTab(getActivityTab(), false, false, false);
             } else {
-                finishAndClose();
+                finishAndClose(false);
             }
         }
         return true;
@@ -824,7 +840,7 @@ public class CustomTabActivity extends ChromeActivity {
             Runnable finalizeCallback = new Runnable() {
                 @Override
                 public void run() {
-                    finishAndClose();
+                    finishAndClose(true);
                 }
             };
 
