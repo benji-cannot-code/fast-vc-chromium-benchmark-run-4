a3ecb0724311985a65f7b9cb590804afd177f8b0
flackr <flackr@chromium.org>
2016-10-19 20:29:51 -0700

    Only automatically promote scrollers which are untransformed and opaque.
    To be sure that we actually get LCD text, we only promote scrollers which have
    not been transformed themselves or by any ancestor, and also exclude those
    which are translucent due to an opacity on themselves or an ancestor.
    
    BUG=644833
    TEST=PaintLayerScrollableAreaTest.OnlyNonTransformedOpaqueLayersPromoted, PaintLayerScrollableAreaTest.OnlyOpaqueLayersPromoted
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://chromiumcodereview.appspot.com/2434443005
    Cr-Commit-Position: refs/heads/master@{#426385}

--- third_party/WebKit/Source/platform/testing/RuntimeEnabledFeaturesTestHelpers.h
+++ third_party/WebKit/Source/platform/testing/RuntimeEnabledFeaturesTestHelpers.h
@@ -34,6 +34,10 @@ typedef ScopedRuntimeEnabledFeatureForTest<
     RuntimeEnabledFeatures::setCompositeOpaqueFixedPositionEnabled>
     ScopedCompositeFixedPositionForTest;
 typedef ScopedRuntimeEnabledFeatureForTest<
+    RuntimeEnabledFeatures::compositeOpaqueScrollersEnabled,
+    RuntimeEnabledFeatures::setCompositeOpaqueScrollersEnabled>
+    ScopedCompositeOpaqueScrollersForTest;
+typedef ScopedRuntimeEnabledFeatureForTest<
     RuntimeEnabledFeatures::compositorWorkerEnabled,
     RuntimeEnabledFeatures::setCompositorWorkerEnabled>
     ScopedCompositorWorkerForTest;
