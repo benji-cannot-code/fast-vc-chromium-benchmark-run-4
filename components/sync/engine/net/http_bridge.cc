23e4c8b4923e8fddf49b4fb491de429cf0013926
gangwu <gangwu@chromium.org>
2016-10-19 17:00:46 -0700

    [Sync] Enable Compression from Client to Server by experiment
    Add padding to sync protobuff for mitigating CRIME attacks.
    
    BUG=653629
    
    Review-Url: https://chromiumcodereview.appspot.com/2380143002
    Cr-Commit-Position: refs/heads/master@{#426343}

--- components/sync/engine/net/http_bridge.cc
+++ components/sync/engine/net/http_bridge.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_fetcher.h"
 #include "net/url_request/url_request_job_factory_impl.h"
 #include "net/url_request/url_request_status.h"
+#include "third_party/zlib/google/compression_utils.h"
 
 namespace syncer {
 
@@ -62,6 +63,10 @@ void RecordSyncResponseContentLengthHistograms(
 }
 
 }  // namespace
+
+// Enables compression of messages from client to server.
+const base::Feature kSyncClientToServerCompression{
+    "EnableSyncClientToServerCompression", base::FEATURE_DISABLED_BY_DEFAULT};
 
 HttpBridgeFactory::HttpBridgeFactory(
     const scoped_refptr<net::URLRequestContextGetter>& request_context_getter,
@@ -241,8 +246,15 @@ void HttpBridge::MakeAsynchronousPost() {
   fetch_state_.url_poster->SetRequestContext(request_context_getter_.get());
   fetch_state_.url_poster->SetExtraRequestHeaders(extra_headers_);
 
-  fetch_state_.url_poster->SetUploadData(content_type_, request_content_);
-  RecordSyncRequestContentLengthHistograms(request_content_.size(),
+  std::string request_to_send;
+  if (base::FeatureList::IsEnabled(kSyncClientToServerCompression)) {
+    compression::GzipCompress(request_content_, &request_to_send);
+    fetch_state_.url_poster->AddExtraRequestHeader("Content-Encoding: gzip");
+  } else {
+    request_to_send = request_content_;
+  }
+  fetch_state_.url_poster->SetUploadData(content_type_, request_to_send);
+  RecordSyncRequestContentLengthHistograms(request_to_send.size(),
                                            request_content_.size());
 
   fetch_state_.url_poster->AddExtraRequestHeader(base::StringPrintf(
