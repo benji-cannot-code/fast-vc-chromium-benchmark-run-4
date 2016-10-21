1ab25fdb9bcd2b86488cf196125930ffae2b49cc
hajimehoshi <hajimehoshi@chromium.org>
2016-10-21 02:01:57 -0700

    Bug fix: Use the last hidden time for purge + suspend
    |TabStats::last_active| is updated when the tab is made active, and
    using this as a base time point for purge + suspend is wrong. Instead,
    the time point should be the time when the tab is made hidden.
    
    This CL fixes this by replacing this usage with last_hidden, which is
    newly introduced.
    
    BUG=551995, 607077
    
    Review-Url: https://chromiumcodereview.appspot.com/2426733004
    Cr-Commit-Position: refs/heads/master@{#426755}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -1325,6 +1325,10 @@ void WebContentsImpl::SetLastActiveTime(base::TimeTicks last_active_time) {
   last_active_time_ = last_active_time;
 }
 
+base::TimeTicks WebContentsImpl::GetLastHiddenTime() const {
+  return last_hidden_time_;
+}
+
 void WebContentsImpl::WasShown() {
   controller_.SetActive(true);
 
@@ -1364,6 +1368,8 @@ void WebContentsImpl::WasHidden() {
 
     SendPageMessage(new PageMsg_WasHidden(MSG_ROUTING_NONE));
   }
+
+  last_hidden_time_ = base::TimeTicks::Now();
 
   for (auto& observer : observers_)
     observer.WasHidden();
