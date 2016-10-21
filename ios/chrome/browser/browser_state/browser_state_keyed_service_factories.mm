a90eecd2cb2cb5155be0926fec7cdc1708d6a4b7
rohitrao <rohitrao@chromium.org>
2016-10-21 08:11:57 -0700

    [ios] Adds GoogleLogoService to help with fetching doodles.
    BUG=656617
    
    Review-Url: https://chromiumcodereview.appspot.com/2434143003
    Cr-Commit-Position: refs/heads/master@{#426795}

--- ios/chrome/browser/browser_state/browser_state_keyed_service_factories.mm
+++ ios/chrome/browser/browser_state/browser_state_keyed_service_factories.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/favicon/ios_chrome_favicon_loader_factory.h"
 #include "ios/chrome/browser/favicon/ios_chrome_large_icon_cache_factory.h"
 #include "ios/chrome/browser/favicon/ios_chrome_large_icon_service_factory.h"
+#include "ios/chrome/browser/google/google_logo_service_factory.h"
 #include "ios/chrome/browser/google/google_url_tracker_factory.h"
 #include "ios/chrome/browser/history/history_service_factory.h"
 #include "ios/chrome/browser/history/top_sites_factory.h"
@@ -87,6 +88,7 @@ void EnsureBrowserStateKeyedServiceFactoriesBuilt() {
   IOSChromePasswordManagerSettingMigratorServiceFactory::GetInstance();
   IOSChromePasswordStoreFactory::GetInstance();
   IOSChromeProfileInvalidationProviderFactory::GetInstance();
+  GoogleLogoServiceFactory::GetInstance();
   OAuth2TokenServiceFactory::GetInstance();
   SigninClientFactory::GetInstance();
   suggestions::SuggestionsServiceFactory::GetInstance();
