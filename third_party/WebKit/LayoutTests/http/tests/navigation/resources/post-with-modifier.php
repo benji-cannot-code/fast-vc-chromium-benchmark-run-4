ec28a65955f126f0931f8ef6637cef17387e457b
lukasza <lukasza@chromium.org>
2016-10-21 13:26:32 -0700

    Form submissions opening in a new window don't need special treatment anymore.
    This CL removes one of workaround used by FrameLoader::load method when
    the http request is associated with a form.  The workaround is no longer
    needed after r400442 fixed how HTTP method and request body are
    correctly plumbed through the OpenURL path.
    
    To make sure that the layout test exercising this functionality
    (shift-clicking on the form's submit button) still passes, some
    follow-up changes had to be made:
    
    - The CL needed to add support for NEW_WINDOW disposition to
      content::Shell::OpenURLFromTab
    
    - The CL needed to tweak the test to account for the fact that
      window.opener is null after opening web contents via shift-click (I've
      verified that this behavior is present for non-form-related, GET
      requests in 55.0.2883.18 so it seems to be WAI).
    
    BUG=344348
    
    Review-Url: https://chromiumcodereview.appspot.com/2417983002
    Cr-Commit-Position: refs/heads/master@{#426876}

--- third_party/WebKit/LayoutTests/http/tests/navigation/resources/post-with-modifier.php
+++ third_party/WebKit/LayoutTests/http/tests/navigation/resources/post-with-modifier.php
@@ -1,13 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <script>
-if (window.opener) {
-    var data = {
-        'method': '<?=$_SERVER['REQUEST_METHOD'] ?>',
-        'formValue': '<?= $_POST['a'] ?>',
-    };
-    window.opener.postMessage(data, '*');
-} else {
-    document.writeln('FAIL');
-    testRunner.notifyDone();
-}
+mainTestWindow = window.open("", "mainTestWindow")
+var data = {
+    'method': '<?=$_SERVER['REQUEST_METHOD'] ?>',
+    'formValue': '<?= $_POST['a'] ?>',
+};
+mainTestWindow.postMessage(data, '*');
 </script>
