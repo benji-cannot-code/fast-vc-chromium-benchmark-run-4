359a8d47bad739a15d9767186d374ef0d049460f
jonross <jonross@chromium.org>
2016-10-21 09:48:23 -0700

    We would like to share the event handling logic of RenderWidgetHostViewAura with RenderWidgetHostViewMus. As a first step this change refactors the EventHandler code out of RenderWidgetHostViewAura into its own class
    TEST=manually run browser, dry-run of CQ to verify all existing tests.
    BUG=631270
    
    Review-Url: https://chromiumcodereview.appspot.com/2317333002
    Cr-Commit-Position: refs/heads/master@{#426815}

--- content/browser/renderer_host/input/touch_selection_controller_client_aura_browsertest.cc
+++ content/browser/renderer_host/input/touch_selection_controller_client_aura_browsertest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/run_loop.h"
 #include "content/browser/renderer_host/render_widget_host_view_aura.h"
+#include "content/browser/renderer_host/render_widget_host_view_event_handler.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/content_browser_test.h"
@@ -169,6 +170,7 @@ class TouchSelectionControllerClientAuraTest : public ContentBrowserTest {
         new TestTouchSelectionControllerClientAura(rwhva);
     rwhva->SetSelectionControllerClientForTest(
         base::WrapUnique(selection_controller_client_));
+    rwhva->event_handler()->disable_input_event_router_for_testing();
   }
 
  private:
