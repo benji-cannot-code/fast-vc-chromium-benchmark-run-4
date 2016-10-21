e3d3d0f4a893ea7b7420eb0a5486f3c854487718
wnwen <wnwen@chromium.org>
2016-10-21 10:16:29 -0700

    Move WebappRegistry warmup to async task.
    On Android N the constructor results in a StrictMode violation due to
    WebappRegistry#openSharedPreferences. Use initialization on demand
    holder idiom to avoid synchronization issues.
    
    BUG=656035
    
    Review-Url: https://chromiumcodereview.appspot.com/2421833003
    Cr-Commit-Position: refs/heads/master@{#426823}

--- chrome/android/java/src/org/chromium/chrome/browser/DeferredStartupHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/DeferredStartupHandler.java
@@ -175,10 +175,6 @@ public class DeferredStartupHandler {
         mDeferredTasks.add(new Runnable() {
             @Override
             public void run() {
-                // Initialize the WebappRegistry if it's not already initialized. Must be done on
-                // the main thread.
-                WebappRegistry.getInstance();
-
                 // Punt all tasks that may block on disk off onto a background thread.
                 initAsyncDiskTask();
 
@@ -244,6 +240,11 @@ public class DeferredStartupHandler {
                 try {
                     TraceEvent.begin("ChromeBrowserInitializer.onDeferredStartup.doInBackground");
                     long asyncTaskStartTime = SystemClock.uptimeMillis();
+
+                    // Initialize the WebappRegistry if it's not already initialized. Must be in
+                    // async task due to shared preferences disk access on N.
+                    WebappRegistry.getInstance();
+
                     boolean crashDumpDisabled = CommandLine.getInstance().hasSwitch(
                             ChromeSwitches.DISABLE_CRASH_DUMP_UPLOAD);
                     if (!crashDumpDisabled) {
