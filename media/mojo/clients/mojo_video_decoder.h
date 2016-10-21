e8a53e6b7313d3bb0030daaf988e65b813fc6948
sandersd <sandersd@chromium.org>
2016-10-20 19:45:44 -0700

    MojoVideoDecoder: Plumb metadata methods.
    This CL plumbs NeedsBitstreamConversion() and GetMaxDecodeRequests(),
    which will be available after Initialize() completes, and
    CanReadWithoutStalling(), which is updated each time a Decode()
    completes.
    
    BUG=522298
    
    Review-Url: https://chromiumcodereview.appspot.com/2429723006
    Cr-Commit-Position: refs/heads/master@{#426705}

--- media/mojo/clients/mojo_video_decoder.h
+++ media/mojo/clients/mojo_video_decoder.h
@@ -51,12 +51,16 @@ class MojoVideoDecoder final : public VideoDecoder,
   void OnVideoFrameDecoded(mojom::VideoFramePtr frame) final;
 
  private:
-  void OnInitializeDone(bool status);
-  void OnDecodeDone(DecodeStatus status);
+  void OnInitializeDone(bool status,
+                        bool needs_bitstream_conversion,
+                        int32_t max_decode_requests);
+  void OnDecodeDone(uint64_t decode_id, DecodeStatus status);
   void OnResetDone();
 
   void BindRemoteDecoder();
-  void OnConnectionError();
+
+  // Cleans up callbacks and blocks future calls.
+  void Stop();
 
   scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
   GpuVideoAcceleratorFactories* gpu_factories_;
@@ -67,7 +71,8 @@ class MojoVideoDecoder final : public VideoDecoder,
 
   InitCB init_cb_;
   OutputCB output_cb_;
-  DecodeCB decode_cb_;
+  uint64_t decode_counter_ = 0;
+  std::map<uint64_t, DecodeCB> pending_decodes_;
   base::Closure reset_cb_;
 
   mojom::VideoDecoderPtr remote_decoder_;
@@ -75,6 +80,10 @@ class MojoVideoDecoder final : public VideoDecoder,
   bool remote_decoder_bound_ = false;
   bool has_connection_error_ = false;
   mojo::AssociatedBinding<VideoDecoderClient> client_binding_;
+
+  bool initialized_ = false;
+  bool needs_bitstream_conversion_ = false;
+  int32_t max_decode_requests_ = 1;
 
   DISALLOW_COPY_AND_ASSIGN(MojoVideoDecoder);
 };
