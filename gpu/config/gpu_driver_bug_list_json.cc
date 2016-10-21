3f214ac6fc80e8beb73795dcc1d783252b72f26a
boliu <boliu@chromium.org>
2016-10-20 19:46:43 -0700

    android: Expand qualcomm EGL refcounting workaround
    There is evidence from google-internal testing that the bug still
    affects newer driver versions, and newer hardware (5xx).
    
    BUG=585250
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://chromiumcodereview.appspot.com/2436993003
    Cr-Commit-Position: refs/heads/master@{#426706}

--- gpu/config/gpu_driver_bug_list_json.cc
+++ gpu/config/gpu_driver_bug_list_json.cc
@@ -20,7 +20,7 @@ const char kGpuDriverBugListJson[] = LONG_STRING_CONST(
 {
   "name": "gpu driver bug list",
   // Please update the version number whenever you change this file.
-  "version": "9.11",
+  "version": "9.12",
   "entries": [
     {
       "id": 1,
@@ -1458,11 +1458,7 @@ LONG_STRING_CONST(
         "type": "android"
       },
       "gl_vendor": "Qualcomm.*",
-      "gl_renderer": "Adreno \\(TM\\) 4.*",
-      "driver_version": {
-        "op": "<",
-        "value": "141.0"
-      },
+      "gl_renderer": "Adreno \\(TM\\) [45].*",
       "features": [
         "broken_egl_image_ref_counting"
       ]
