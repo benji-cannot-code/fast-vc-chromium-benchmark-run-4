a3c1fd275c6ef8333278f3a67b042d42d5e99a79
jrummell <jrummell@chromium.org>
2016-10-21 13:29:59 -0700

    Update MediaKeySession to keep related code blocks together
    BUG=646902
    TEST=EME tests pass
    
    Review-Url: https://chromiumcodereview.appspot.com/2440893002
    Cr-Commit-Position: refs/heads/master@{#426877}

--- third_party/WebKit/Source/modules/encryptedmedia/MediaKeySession.h
+++ third_party/WebKit/Source/modules/encryptedmedia/MediaKeySession.h
@@ -89,7 +89,6 @@ class MediaKeySession final
                                 const String& initDataType,
                                 const DOMArrayPiece& initData);
   ScriptPromise load(ScriptState*, const String& sessionId);
-
   ScriptPromise update(ScriptState*, const DOMArrayPiece& response);
   ScriptPromise close(ScriptState*);
   ScriptPromise remove(ScriptState*);
@@ -116,6 +115,18 @@ class MediaKeySession final
 
   void actionTimerFired(TimerBase*);
 
+  // The following perform the asynchronous part of the command referenced.
+  void generateRequestTask(ContentDecryptionModuleResult*,
+                           WebEncryptedMediaInitDataType,
+                           DOMArrayBuffer* initDataBuffer);
+  void finishGenerateRequest();
+  void loadTask(ContentDecryptionModuleResult*, const String& sessionId);
+  void finishLoad();
+  void updateTask(ContentDecryptionModuleResult*,
+                  DOMArrayBuffer* sanitizedResponse);
+  void closeTask(ContentDecryptionModuleResult*);
+  void removeTask(ContentDecryptionModuleResult*);
+
   // WebContentDecryptionModuleSession::Client
   void message(MessageType,
                const unsigned char* message,
@@ -124,12 +135,6 @@ class MediaKeySession final
   void expirationChanged(double updatedExpiryTimeInMS) override;
   void keysStatusesChange(const WebVector<WebEncryptedMediaKeyInformation>&,
                           bool hasAdditionalUsableKey) override;
-
-  // Called by NewSessionResult when the new session has been created.
-  void finishGenerateRequest();
-
-  // Called by LoadSessionResult when the session has been loaded.
-  void finishLoad();
 
   Member<GenericEventQueue> m_asyncEventQueue;
   std::unique_ptr<WebContentDecryptionModuleSession> m_session;
