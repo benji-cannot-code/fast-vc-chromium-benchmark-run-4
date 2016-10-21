769a250a10f5f6793d66c324146b4f04ad71ae5a
hashimoto <hashimoto@chromium.org>
2016-10-20 20:53:47 -0700

    arc: Add URL converter functions
    When receiving a content: URL or a file: URL from ARC,
    these functions will be used to convert the URL to an externalfile: URL
    to display the contents on a Chrome tab.
    
    BUG=654684
    
    Review-Url: https://chromiumcodereview.appspot.com/2437693002
    Cr-Commit-Position: refs/heads/master@{#426724}

---
+++ chrome/browser/chromeos/arc/fileapi/arc_content_file_system_url_util.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_ARC_FILEAPI_ARC_CONTENT_FILE_SYSTEM_URL_UTIL_H_
+#define CHROME_BROWSER_CHROMEOS_ARC_FILEAPI_ARC_CONTENT_FILE_SYSTEM_URL_UTIL_H_
+
+#include "url/gurl.h"
+
+namespace arc {
+
+// Converts a URL which can be used within the ARC container to an externalfile:
+// URL which can be used by Chrome.
+GURL ArcUrlToExternalFileUrl(const GURL& arc_url);
+
+// Converts an externalfile: URL to a URL which can be used within the ARC
+// container. If the given URL cannot be converted to an ARC URL, returns an
+// empty GURL.
+GURL ExternalFileUrlToArcUrl(const GURL& external_file_url);
+
+}  // namespace arc
+
+#endif  // CHROME_BROWSER_CHROMEOS_ARC_FILEAPI_ARC_CONTENT_FILE_SYSTEM_URL_UTIL_H_
--- chrome/browser/chromeos/fileapi/external_file_url_util_unittest.cc
+++ chrome/browser/chromeos/fileapi/external_file_url_util_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile_manager.h"
 #include "components/drive/file_system_core_util.h"
+#include "content/public/common/url_constants.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "storage/browser/fileapi/file_system_url.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -77,6 +78,14 @@ TEST_F(ExternalFileURLUtilTest, FilePathToExternalFileURL) {
   EXPECT_EQ(url.virtual_path().AsUTF8Unsafe(),
             ExternalFileURLToVirtualPath(FileSystemURLToExternalFileURL(url))
                 .AsUTF8Unsafe());
+}
+
+TEST_F(ExternalFileURLUtilTest, VirtualPathToExternalFileURL) {
+  base::FilePath virtual_path(FILE_PATH_LITERAL("foo/bar012.txt"));
+  GURL result = VirtualPathToExternalFileURL(virtual_path);
+  EXPECT_TRUE(result.is_valid());
+  EXPECT_EQ(content::kExternalFileScheme, result.scheme());
+  EXPECT_EQ(virtual_path.value(), ExternalFileURLToVirtualPath(result).value());
 }
 
 }  // namespace chromeos
