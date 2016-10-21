62550e7b27a4b25d3926a6f4f99e4718dcbfddaf
dmurph <dmurph@chromium.org>
2016-10-21 14:51:20 -0700

    [BlobStorage] BlobMemoryController & tests
    This is a split off of https://codereview.chromium.org/2055053003/.
    
    This adds the BlobMemoryController & tests, as well as any other small
    required changes. I avoided doing too much refactoring (like the move
    to BlobStatus everywhere, and removing BlobStorageRegistry::Entry for
    InternalBlobData) to just focus on the BlobMemoryController API.
    
    BUG=375297
    R=michaeln@chromium.org,kinuko@chromium.org,mek@chromium.org
    
    Review-Url: https://chromiumcodereview.appspot.com/2339933004
    Cr-Commit-Position: refs/heads/master@{#426901}

--- content/browser/blob_storage/blob_async_transport_request_builder_unittest.cc
+++ content/browser/blob_storage/blob_async_transport_request_builder_unittest.cc
@@ -16,8 +16,6 @@ namespace storage {
 namespace {
 
 const char kNewUUID[] = "newUUID";
-const base::FilePath kFuturePopulatingFilePath = base::FilePath::FromUTF8Unsafe(
-    std::string(BlobDataBuilder::kAppendFutureFileTemporaryFileName));
 const char kFakeBlobUUID[] = "fakeBlob";
 
 void AddMemoryItem(size_t length, std::vector<DataElement>* out) {
@@ -88,8 +86,7 @@ TEST(BlobAsyncTransportRequestBuilderTest, TestLargeBlockToFile) {
       memory_item_request.message);
 
   BlobDataBuilder expected_builder(kNewUUID);
-  expected_builder.AppendFile(kFuturePopulatingFilePath, 0, 305,
-                              base::Time::FromDoubleT(0));
+  expected_builder.AppendFutureFile(0, 305, 0);
   EXPECT_EQ(expected_builder, builder);
 }
 
@@ -132,12 +129,9 @@ TEST(BlobAsyncTransportRequestBuilderTest, TestLargeBlockToFiles) {
       memory_item_request.message);
 
   BlobDataBuilder expected_builder(kNewUUID);
-  expected_builder.AppendFile(kFuturePopulatingFilePath, 0, 400,
-                              base::Time::FromDoubleT(0));
-  expected_builder.AppendFile(kFuturePopulatingFilePath, 0, 400,
-                              base::Time::FromDoubleT(0));
-  expected_builder.AppendFile(kFuturePopulatingFilePath, 0, 200,
-                              base::Time::FromDoubleT(0));
+  expected_builder.AppendFutureFile(0, 400, 0);
+  expected_builder.AppendFutureFile(0, 400, 1);
+  expected_builder.AppendFutureFile(0, 200, 2);
   EXPECT_EQ(expected_builder, builder);
 }
 
