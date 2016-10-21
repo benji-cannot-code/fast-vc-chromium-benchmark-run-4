8ab7a63f81333767c3b9ba87894c254e8615096b
rdsmith <rdsmith@chromium.org>
2016-10-21 10:36:08 -0700

    Add a new priority level, THROTTLED, below IDLE.
    THROTTLED is used when the consumer is expecting that low priority
    requests will soon be followed by higher priority ones.  It directs the
    network stack to reserve resources to handle the coming requests.  In
    practice this will mean that only a very small number of THROTTLED requests
    may be outstanding at any given time.
    
    BUG=600839
    
    Review-Url: https://chromiumcodereview.appspot.com/1866483002
    Cr-Commit-Position: refs/heads/master@{#426831}

--- net/base/request_priority.cc
+++ net/base/request_priority.cc
@@ -11,6 +11,8 @@ namespace net {
 
 const char* RequestPriorityToString(RequestPriority priority) {
   switch (priority) {
+    case THROTTLED:
+      return "THROTTLED";
     case IDLE:
       return "IDLE";
     case LOWEST:
