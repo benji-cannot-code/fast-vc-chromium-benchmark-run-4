e374d414329e21d27b26a6fa06795d1d7d71a92b
luoe <luoe@chromium.org>
2016-10-21 21:09:54 -0700

    DevTools: remove unused css in objectValue.css
    Review-Url: https://chromiumcodereview.appspot.com/2435153003
    Cr-Commit-Position: refs/heads/master@{#426967}

--- third_party/WebKit/Source/devtools/front_end/components/RemoteObjectPreviewFormatter.js
+++ third_party/WebKit/Source/devtools/front_end/components/RemoteObjectPreviewFormatter.js
@@ -155,7 +155,6 @@ WebInspector.RemoteObjectPreviewFormatter.prototype = {
         }
 
         if (type === "object" && subtype === "node" && description) {
-            span.classList.add("object-value-preview-node");
             WebInspector.DOMPresentationUtils.createSpansForNodeTitle(span, description);
             return span;
         }
