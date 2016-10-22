f01ab730a2ccaa25202985737a748ae1c44d73d8
jamescook <jamescook@chromium.org>
2016-10-21 19:22:04 -0700

    mash: Introduce ShowWebDialogWithContainer for webui dialogs
    Several webui dialogs spawned by the ash system tray can be shown either as
    children of the general settings webui window or by themselves at the login
    screen. In the latter case we need to place them in the right ash window
    container. Under mash, chrome is not aware of the ash window hierarchy, so
    it needs to use a container ID rather than a native window parent.
    
    Wire up the "set time" dialog as an example.
    
    BUG=657021
    TEST=manual, spawn set time dialog from settings and from lock screen
    TBR=stevenjb@chromium.org for function rename touching c/b/ui/webui/chromeos
    
    Review-Url: https://chromiumcodereview.appspot.com/2441133002
    Cr-Commit-Position: refs/heads/master@{#426953}

--- chrome/browser/ui/ash/system_tray_delegate_chromeos.cc
+++ chrome/browser/ui/ash/system_tray_delegate_chromeos.cc
@@ -350,8 +350,9 @@ bool SystemTrayDelegateChromeOS::ShouldShowSettings() {
 }
 
 void SystemTrayDelegateChromeOS::ShowSetTimeDialog() {
-  // TODO(mash): Refactor out GetNativeWindow and move to SystemTrayClient.
-  SetTimeDialog::ShowDialog(GetNativeWindow());
+  // TODO(mash): Move to SystemTrayClient.
+  SetTimeDialog::ShowDialogInContainer(
+      SystemTrayClient::GetDialogParentContainerId());
 }
 
 void SystemTrayDelegateChromeOS::ShowEnterpriseInfo() {
