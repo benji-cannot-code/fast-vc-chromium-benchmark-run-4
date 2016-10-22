6681b00236e51015d00a2b2e8308c0c19e9be23d
dschuyler <dschuyler@chromium.org>
2016-10-21 18:15:33 -0700

    [MD settings] help button in content settings site lists
    This CL removes the Learn more row button in the Content settings and
    puts that functionality in a new help button in the upper right of the
    subpage.
    
    BUG=658060
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://chromiumcodereview.appspot.com/2432383005
    Cr-Commit-Position: refs/heads/master@{#426941}

--- chrome/browser/resources/settings/privacy_page/privacy_page.js
+++ chrome/browser/resources/settings/privacy_page/privacy_page.js
@@ -96,6 +96,12 @@ Polymer({
     settings.navigateToPreviousRoute();
   },
 
+  /** @private */
+  onHelpTap_: function() {
+    window.open(
+        'https://support.google.com/chrome/?p=settings_manage_exceptions');
+  },
+
 <if expr="_google_chrome and not chromeos">
   /** @private */
   onMetricsReportingCheckboxTap_: function() {
--- chrome/browser/resources/settings/site_settings/site_settings_category.js
+++ chrome/browser/resources/settings/site_settings/site_settings_category.js
@@ -195,12 +195,6 @@ Polymer({
   },
 
   /** @private */
-  onLearnMoreClicked_: function() {
-    window.open(
-        'https://support.google.com/chrome/?p=settings_manage_exceptions');
-  },
-
-  /** @private */
   onAdobeFlashStorageClicked_: function() {
     window.open('https://www.macromedia.com/support/' +
         'documentation/en/flashplayer/help/settings_manager07.html');
