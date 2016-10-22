3ef8e8a2ac0b64fa226e694a7a1f2334a68b0229
drbasic <drbasic@yandex-team.ru>
2016-10-21 22:09:14 -0700

    Setting "SubSystem" option on "Link" page for Visual Studio project.
    When the debugger session in Visual Studio finished, the console window closed
    by default. For console applications, such as unit tests, it is possible not to
    close the console window immediately. This is controlled by the property
    "SubSystem" on "Link" page in project settings. This patch set property
    "SubSystem" in generated *.vcxproj file based on the "ldflags" field.
    
    R=brettw@chromium.org
    BUG=
    
    Review-Url: https://chromiumcodereview.appspot.com/2428013002
    Cr-Commit-Position: refs/heads/master@{#426977}

--- tools/gn/visual_studio_utils.cc
+++ tools/gn/visual_studio_utils.cc
@@ -5,12 +5,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "tools/gn/visual_studio_utils.h"
 
+#include <vector>
+
 #include "base/md5.h"
+#include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 
 CompilerOptions::CompilerOptions() = default;
 
 CompilerOptions::~CompilerOptions() = default;
+
+LinkerOptions::LinkerOptions() = default;
+
+LinkerOptions::~LinkerOptions() = default;
 
 std::string MakeGuid(const std::string& entry_path, const std::string& seed) {
   std::string str = base::ToUpperASCII(base::MD5String(seed + entry_path));
@@ -115,4 +122,19 @@ void ParseCompilerOption(const std::string& cflag, CompilerOptions* options) {
 
   // Put everything else into additional_options.
   options->additional_options += cflag + ' ';
+}
+
+// Parses |ldflags| value and stores it in |options|.
+void ParseLinkerOption(const std::string& ldflag, LinkerOptions* options) {
+  const char kSubsytemPrefix[] ="/SUBSYSTEM:";
+  if (base::StartsWith(ldflag, kSubsytemPrefix,
+                       base::CompareCase::SENSITIVE)) {
+    const std::string subsystem(
+        ldflag.begin() + std::string(kSubsytemPrefix).length(),
+        ldflag.end());
+    const std::vector<std::string> tokens = base::SplitString(
+        subsystem, ",", base::TRIM_WHITESPACE, base::SPLIT_WANT_NONEMPTY);
+    if (!tokens.empty())
+      options->subsystem = tokens[0];
+  }
 }
