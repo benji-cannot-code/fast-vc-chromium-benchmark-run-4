ec73136ee4bdb640424dc73167b43df631527df3
mbrunson <mbrunson@chromium.org>
2016-10-24 12:46:15 -0700

    bluetooth: Add DeviceChanged logging in Device service.
    Adds DeviceChanged callback for BluetoothAdapter::Observer in Device.
    Logs received DeviceChanged events in console.
    
    BUG=651282
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2404623002
    Cr-Commit-Position: refs/heads/master@{#427124}

--- device/bluetooth/adapter.h
+++ device/bluetooth/adapter.h
@@ -38,6 +38,8 @@ class Adapter : public mojom::Adapter,
                    device::BluetoothDevice* device) override;
   void DeviceRemoved(device::BluetoothAdapter* adapter,
                      device::BluetoothDevice* device) override;
+  void DeviceChanged(device::BluetoothAdapter* adapter,
+                     device::BluetoothDevice* device) override;
 
  private:
   // The current Bluetooth adapter.
