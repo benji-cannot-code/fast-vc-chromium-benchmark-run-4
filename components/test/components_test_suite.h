87ad9772a7dd5630a5eaecf8193608f68d02a14f
dyaroshev <dyaroshev@yandex-team.ru>
2016-10-24 04:10:58 -0700

    Preparing components_perftests for adding more benchmarks.
    This patch creates a complete main for components perftests from
    components unittests main and switches components perftests to using
    testing/perf instead of base/perf.
    
    BUG=648992
    
    Review-Url: https://codereview.chromium.org/2358063002
    Cr-Commit-Position: refs/heads/master@{#427043}

---
+++ components/test/components_test_suite.h
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_TEST_COMPONENTS_TEST_SUITE_H_
+#define COMPONENTS_TEST_COMPONENTS_TEST_SUITE_H_
+
+#include "base/test/launcher/unit_test_launcher.h"
+
+base::RunTestSuiteCallback GetLaunchCallback(int argc, char** argv);
+
+#endif  // COMPONENTS_TEST_COMPONENTS_TEST_SUITE_H_
