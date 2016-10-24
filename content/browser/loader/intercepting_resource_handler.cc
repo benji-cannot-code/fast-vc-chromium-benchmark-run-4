10322a998bea830ce6267efac6dd29ed6f541816
mmenke <mmenke@chromium.org>
2016-10-24 16:59:27 -0700

    When InterceptingResourceHandler swaps in a handler, call OnWillStart.
    The new ResourceHandler's OnWillStart was never being called, which
    violates the ResourceHandler's API contract, and has caused at least one
    crash previously (Though that fixed by another CL).
    
    BUG=650717
    
    Review-Url: https://codereview.chromium.org/2436163002
    Cr-Commit-Position: refs/heads/master@{#427183}

--- content/browser/loader/intercepting_resource_handler.cc
+++ content/browser/loader/intercepting_resource_handler.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "content/public/common/resource_response.h"
 #include "net/base/io_buffer.h"
+#include "net/url_request/url_request.h"
 
 namespace content {
 
@@ -176,7 +177,10 @@ bool InterceptingResourceHandler::DoLoop(bool* defer) {
       case State::PASS_THROUGH:
         NOTREACHED();
         break;
-      case State::NOTIFYING_ON_RESPONSE_STARTED_TO_NEW_HANDLER:
+      case State::SENDING_ON_WILL_START_TO_NEW_HANDLER:
+        result = SendOnResponseStartedToNewHandler(defer);
+        break;
+      case State::SENDING_ON_RESPONSE_STARTED_TO_NEW_HANDLER:
         if (first_read_buffer_double_) {
           // OnWillRead has been called, so copying the data from
           // |first_read_buffer_double_| to |first_read_buffer_| will be needed
@@ -240,7 +244,13 @@ bool InterceptingResourceHandler::SendPayloadToOldHandler(bool* defer) {
   DCHECK(!*defer);
 
   next_handler_ = std::move(new_handler_);
-  state_ = State::NOTIFYING_ON_RESPONSE_STARTED_TO_NEW_HANDLER;
+  state_ = State::SENDING_ON_WILL_START_TO_NEW_HANDLER;
+  return next_handler_->OnWillStart(request()->url(), defer);
+}
+
+bool InterceptingResourceHandler::SendOnResponseStartedToNewHandler(
+    bool* defer) {
+  state_ = State::SENDING_ON_RESPONSE_STARTED_TO_NEW_HANDLER;
   return next_handler_->OnResponseStarted(response_.get(), defer);
 }
 
