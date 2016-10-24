38efbeb64bf40725f77742cc83bb019a800ea0f4
sunyunjia <sunyunjia@chromium.org>
2016-10-24 14:00:37 -0700

    Prevent AutoscrollForSelection when selecting text in a fixed-position element.
    AutoscrollForSelection is triggered when selecting text and dragging mouse out
    of the container. However, we don't want selecting text in a fixed-position
    element triggers autoscroll otherwhere. Inside findAutoscrollable, when we find
    the object is inside a fixed-position element, we should prevent
    AutoscrollForSelection from happening.
    
    BUG=655489
    
    Review-Url: https://codereview.chromium.org/2441683002
    Cr-Commit-Position: refs/heads/master@{#427146}

--- third_party/WebKit/Source/core/layout/LayoutBox.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBox.cpp
@@ -1009,6 +1009,12 @@ LayoutBox* LayoutBox::findAutoscrollable(LayoutObject* layoutObject) {
   while (
       layoutObject &&
       !(layoutObject->isBox() && toLayoutBox(layoutObject)->canAutoscroll())) {
+    // Do not start autoscroll when the node is inside a fixed-position element.
+    if (layoutObject->isBox() && toLayoutBox(layoutObject)->hasLayer() &&
+        toLayoutBox(layoutObject)->layer()->scrollsWithViewport()) {
+      return nullptr;
+    }
+
     if (!layoutObject->parent() &&
         layoutObject->node() == layoutObject->document() &&
         layoutObject->document().localOwner())
@@ -1017,7 +1023,8 @@ LayoutBox* LayoutBox::findAutoscrollable(LayoutObject* layoutObject) {
       layoutObject = layoutObject->parent();
   }
 
-  return layoutObject && layoutObject->isBox() ? toLayoutBox(layoutObject) : 0;
+  return layoutObject && layoutObject->isBox() ? toLayoutBox(layoutObject)
+                                               : nullptr;
 }
 
 static inline int adjustedScrollDelta(int beginningDelta) {
