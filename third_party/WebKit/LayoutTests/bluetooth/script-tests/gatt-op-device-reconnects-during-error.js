171cc32d925b1321ae1a329596cc2268faf346bb
ortuno <ortuno@chromium.org>
2016-10-23 19:02:41 -0700

    bluetooth: Add tests for when frame reconnects during readValue
    Follow up patch that adds a new fake adapter that finishes a gatt operation
    after a connect is called and adds tests that uses it.
    
    Previous patch: http://crrev.com/2443473003
    
    BUG=608538
    
    Review-Url: https://codereview.chromium.org/2438963002
    Cr-Commit-Position: refs/heads/master@{#427009}

---
+++ third_party/WebKit/LayoutTests/bluetooth/script-tests/gatt-op-device-reconnects-during-error.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+promise_test(() => {
+  return setBluetoothFakeAdapter('DisconnectingDuringFailureGATTOperationAdapter')
+    .then(() => requestDeviceWithKeyDown({
+      filters: [{services: ['health_thermometer']}]}))
+    .then(device => device.gatt.connect())
+    .then(gatt => gatt.getPrimaryService('health_thermometer'))
+    .then(service => service.getCharacteristic('measurement_interval'))
+    .then(characteristic => {
+      let disconnected = eventPromise(characteristic.service.device, 'gattserverdisconnected');
+      let promise = assert_promise_rejects_with_message(
+        characteristic.CALLS([readValue()]),
+        new DOMException('GATT Server disconnected while performing a GATT operation.',
+                         'NetworkError'));
+      return disconnected.then(() => characteristic.service.device.gatt.connect())
+        .then(() => promise);
+    });
+}, 'Device reconnects during a FUNCTION_NAME call that fails. Reject ' +
+   'with NetworkError.');
---
+++ third_party/WebKit/LayoutTests/bluetooth/script-tests/gatt-op-reconnect-during-error.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+promise_test(() => {
+  return setBluetoothFakeAdapter('GATTOperationFailsAfterReconnectionAdapter')
+    .then(() => requestDeviceWithKeyDown({
+      filters: [{services: ['health_thermometer']}]}))
+    .then(device => device.gatt.connect())
+    .then(gatt => gatt.getPrimaryService('health_thermometer'))
+    .then(service => service.getCharacteristic('measurement_interval'))
+    .then(characteristic => {
+      let promise = assert_promise_rejects_with_message(
+        characteristic.CALLS([readValue()]),
+        new DOMException('GATT Server disconnected while performing a GATT operation.',
+                         'NetworkError'));
+      let gatt = characteristic.service.device.gatt;
+      gatt.disconnect();
+      return gatt.connect().then(() => promise);
+    });
+}, 'disconnect() and connect() called during a FUNCTION_NAME call that ' +
+   'fails. Reject with NetworkError.');
