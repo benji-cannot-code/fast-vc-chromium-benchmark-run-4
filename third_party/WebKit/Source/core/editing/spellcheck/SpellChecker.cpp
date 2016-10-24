fdb85d86913a5693bb3b3b815ac58498540b1270
yosin <yosin@chromium.org>
2016-10-23 21:58:15 -0700

    Make editingIgnoresContents() as global function
    This patch makes |editingIgnoresContents()| as global function instead of
    static member function of template class |EditingStratgegy| since it doesn't
    use template parameter to simplify code for improving code health.
    
    BUG=n/a
    TEST=n/a; no behavior changes
    
    Review-Url: https://codereview.chromium.org/2441693004
    Cr-Commit-Position: refs/heads/master@{#427016}

--- third_party/WebKit/Source/core/editing/spellcheck/SpellChecker.cpp
+++ third_party/WebKit/Source/core/editing/spellcheck/SpellChecker.cpp
@@ -214,7 +214,7 @@ void SpellChecker::didBeginEditing(Element* element) {
   }
 
   if (isTextField || !parent->isAlreadySpellChecked()) {
-    if (EditingStrategy::editingIgnoresContent(element))
+    if (editingIgnoresContent(element))
       return;
     // We always recheck textfields because markers are removed from them on
     // blur.
