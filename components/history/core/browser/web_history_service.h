01b913244ea3731a9049e75c680f63e6ca54d5b2
avi <avi@chromium.org>
2016-10-24 12:44:49 -0700

    Remove stl_util's deletion function use from components/history/.
    BUG=555865
    
    Review-Url: https://codereview.chromium.org/2441223002
    Cr-Commit-Position: refs/heads/master@{#427123}

--- components/history/core/browser/web_history_service.h
+++ components/history/core/browser/web_history_service.h
@@ -8,7 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
-#include <set>
+#include <map>
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -213,18 +214,20 @@ class WebHistoryService : public KeyedService {
 
   // Pending expiration requests to be canceled if not complete by profile
   // shutdown.
-  std::set<Request*> pending_expire_requests_;
+  std::map<Request*, std::unique_ptr<Request>> pending_expire_requests_;
 
   // Pending requests to be canceled if not complete by profile shutdown.
-  std::set<Request*> pending_audio_history_requests_;
+  std::map<Request*, std::unique_ptr<Request>> pending_audio_history_requests_;
 
   // Pending web and app activity queries to be canceled if not complete by
   // profile shutdown.
-  std::set<Request*> pending_web_and_app_activity_requests_;
+  std::map<Request*, std::unique_ptr<Request>>
+      pending_web_and_app_activity_requests_;
 
   // Pending queries for other forms of browsing history to be canceled if not
   // complete by profile shutdown.
-  std::set<Request*> pending_other_forms_of_browsing_history_requests_;
+  std::map<Request*, std::unique_ptr<Request>>
+      pending_other_forms_of_browsing_history_requests_;
 
   // Observers.
   base::ObserverList<WebHistoryServiceObserver, true> observer_list_;
