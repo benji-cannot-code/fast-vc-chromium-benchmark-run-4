acc3ddd6c9d8c860f540b14f46c5df2c03a3cac6
bauerb <bauerb@chromium.org>
2016-10-24 06:46:46 -0700

    [Android NTP] Move suggestions in SuggestionSection into a separate TreeNode.
    This makes SnippetArticle a pure model class that doesn't know about the adapter.
    
    Goals of the overall refactoring:
    1) Decoupling the various pieces of logic, by moving them from the adapter into
    the tree structure. For example, dismissal is something that could be handled by
    a TreeNode instead of doing the big switch statement we have right now in the
    Adapter.
    2) Stabilizing the structure of the tree. Right now we tend to throw away
    everything and rebuild, which means we have to manually figure out what changes
    exactly to notify about (cf. removeSuggestion). If we keep the tree structure
    stable and just adjust the number of children below each node as necessary (like
    SigninPromo already does), we get correct notifications for free, and it will
    allow for some optimizations as well (we could cache the number of children for
    each node and update it incrementally when a subtree changes. Right now we'd
    have to contort ourselves a bit to make that work with things like
    resetChildren()).
    
    BUG=616090
    
    Review-Url: https://codereview.chromium.org/2439683003
    Cr-Commit-Position: refs/heads/master@{#427060}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/SuggestionsSection.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/SuggestionsSection.java
@@ -10,6 +10,7 @@ import org.chromium.chrome.browser.ntp.snippets.CategoryInt;
 import org.chromium.chrome.browser.ntp.snippets.CategoryStatus.CategoryStatusEnum;
 import org.chromium.chrome.browser.ntp.snippets.SectionHeader;
 import org.chromium.chrome.browser.ntp.snippets.SnippetArticle;
+import org.chromium.chrome.browser.ntp.snippets.SnippetArticleViewHolder;
 import org.chromium.chrome.browser.ntp.snippets.SnippetsBridge;
 
 import java.util.ArrayList;
@@ -22,6 +23,7 @@ public class SuggestionsSection extends InnerNode {
     private final List<TreeNode> mChildren = new ArrayList<>();
     private final List<SnippetArticle> mSuggestions = new ArrayList<>();
     private final SectionHeader mHeader;
+    private final TreeNode mSuggestionsList = new SuggestionsList(this);
     private final StatusItem mStatus;
     private final ProgressItem mProgressIndicator = new ProgressItem();
     private final ActionItem mMoreButton;
@@ -36,6 +38,39 @@ public class SuggestionsSection extends InnerNode {
         resetChildren();
     }
 
+    private class SuggestionsList extends ChildNode {
+        public SuggestionsList(NodeParent parent) {
+            super(parent);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mSuggestions.size();
+        }
+
+        @Override
+        @ItemViewType
+        public int getItemViewType(int position) {
+            return ItemViewType.SNIPPET;
+        }
+
+        @Override
+        public void onBindViewHolder(NewTabPageViewHolder holder, int position) {
+            assert holder instanceof SnippetArticleViewHolder;
+            ((SnippetArticleViewHolder) holder).onBindViewHolder(getSuggestionAt(position));
+        }
+
+        @Override
+        public SnippetArticle getSuggestionAt(int position) {
+            return mSuggestions.get(position);
+        }
+
+        @Override
+        public int getDismissSiblingPosDelta(int position) {
+            return 0;
+        }
+    }
+
     @Override
     protected List<TreeNode> getChildren() {
         return mChildren;
@@ -44,7 +79,7 @@ public class SuggestionsSection extends InnerNode {
     private void resetChildren() {
         mChildren.clear();
         mChildren.add(mHeader);
-        mChildren.addAll(mSuggestions);
+        mChildren.add(mSuggestionsList);
 
         if (mSuggestions.isEmpty()) mChildren.add(mStatus);
         if (mCategoryInfo.hasMoreButton() || mSuggestions.isEmpty()) mChildren.add(mMoreButton);
