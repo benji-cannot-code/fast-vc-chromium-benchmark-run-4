13d41b6c0ec69e96c04d8cfffacf96a4f7e13e17
sdefresne <sdefresne@chromium.org>
2016-10-25 07:24:19 -0700

    Remove unused method GetSecurityLevelForSecurityStyle() on iOS.
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2446203002
    Cr-Commit-Position: refs/heads/master@{#427344}

--- ios/chrome/browser/ssl/ios_chrome_security_state_model_client.mm
+++ ios/chrome/browser/ssl/ios_chrome_security_state_model_client.mm
@@ -18,35 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 DEFINE_WEB_STATE_USER_DATA_KEY(IOSChromeSecurityStateModelClient);
 
-namespace {
-
-// Converts a web::SecurityStyle (an indicator of a request's
-// overall security level computed by //ios/web) into a
-// SecurityStateModel::SecurityLevel (a finer-grained SecurityStateModel
-// concept that can express all of SecurityStateModel's policies that
-// //ios/web doesn't necessarily know about).
-security_state::SecurityStateModel::SecurityLevel
-GetSecurityLevelForSecurityStyle(web::SecurityStyle style) {
-  switch (style) {
-    case web::SECURITY_STYLE_UNKNOWN:
-      NOTREACHED();
-      return security_state::SecurityStateModel::NONE;
-    case web::SECURITY_STYLE_UNAUTHENTICATED:
-      return security_state::SecurityStateModel::NONE;
-    case web::SECURITY_STYLE_AUTHENTICATION_BROKEN:
-      return security_state::SecurityStateModel::DANGEROUS;
-    case web::SECURITY_STYLE_WARNING:
-      // //ios/web currently doesn't use this style.
-      NOTREACHED();
-      return security_state::SecurityStateModel::SECURITY_WARNING;
-    case web::SECURITY_STYLE_AUTHENTICATED:
-      return security_state::SecurityStateModel::SECURE;
-  }
-  return security_state::SecurityStateModel::NONE;
-}
-
-}  // namespace
-
 IOSChromeSecurityStateModelClient::IOSChromeSecurityStateModelClient(
     web::WebState* web_state)
     : web_state_(web_state),
