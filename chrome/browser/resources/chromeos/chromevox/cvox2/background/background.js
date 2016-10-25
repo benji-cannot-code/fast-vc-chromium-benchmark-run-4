d942c881649b65a2ecebf538e800a91440baed17
dtseng <dtseng@chromium.org>
2016-10-25 11:34:57 -0700

    Fix next compat to convey state for classic.
    ChromeVox's content script requests a boolean isClasicEnabled before document load (sometimes). Ensure that boolean accounts for the next compat reg exp.
    BUG=657098,657100,657092
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2443273002
    Cr-Commit-Position: refs/heads/master@{#427420}

--- chrome/browser/resources/chromeos/chromevox/cvox2/background/background.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/background/background.js
@@ -75,7 +75,7 @@ Background = function() {
    * @type {RegExp}
    * @private
    */
-  this.NextCompatRegExp_ = Background.globsToRegExp_([
+  this.nextCompatRegExp_ = Background.globsToRegExp_([
     '*docs.google.com*'
   ]);
 
@@ -251,7 +251,7 @@ Background.prototype = {
           ChromeVoxMode.CLASSIC_COMPAT;
 
     var nextSite = this.isWhitelistedForNext_(topLevelRoot.docUrl);
-    var nextCompat = this.NextCompatRegExp_.test(topLevelRoot.docUrl);
+    var nextCompat = this.nextCompatRegExp_.test(topLevelRoot.docUrl);
     var classicCompat =
         this.isWhitelistedForClassicCompat_(topLevelRoot.docUrl);
     if (nextCompat && useNext)
@@ -561,9 +561,10 @@ Background.prototype = {
    * @private
    */
   shouldEnableClassicForUrl_: function(url) {
-    return this.mode != ChromeVoxMode.FORCE_NEXT &&
-        !this.isBlacklistedForClassic_(url) &&
-        !this.isWhitelistedForNext_(url);
+    return this.nextCompatRegExp_.test(url) ||
+        (this.mode != ChromeVoxMode.FORCE_NEXT &&
+         !this.isBlacklistedForClassic_(url) &&
+         !this.isWhitelistedForNext_(url));
   },
 
   /**
@@ -618,7 +619,7 @@ Background.prototype = {
     };
 
     if (params.forNextCompat) {
-      var reStr = this.NextCompatRegExp_.toString();
+      var reStr = this.nextCompatRegExp_.toString();
       disableChromeVoxCommand['excludeUrlRegExp'] =
           reStr.substring(1, reStr.length - 1);
     }
