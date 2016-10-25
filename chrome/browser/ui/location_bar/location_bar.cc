befd15a637834b61e4923391eba8db3552c24794
spqchan <spqchan@chromium.org>
2016-10-25 16:08:28 -0700

    Add a feature flag for the security verbose
    BUG=658379
    
    Review-Url: https://codereview.chromium.org/2430923010
    Cr-Commit-Position: refs/heads/master@{#427526}

--- chrome/browser/ui/location_bar/location_bar.cc
+++ chrome/browser/ui/location_bar/location_bar.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/feature_switch.h"
 #include "extensions/common/permissions/permissions_data.h"
 
+const char LocationBar::kSecurityChipFeatureVisibilityParam[] = "visibility";
+const char LocationBar::kSecurityChipFeatureAnimationParam[] = "animation";
 
 LocationBar::LocationBar(Profile* profile) : profile_(profile) {
 }
