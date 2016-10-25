ba85a946c85f7ec60deeb95bcb50214b186af622
qinmin <qinmin@chromium.org>
2016-10-24 18:09:48 -0700

    Fix an issue that opening a download with remapped MIME type will launch an intent
    When a download's MIME type is remapped, Chrome should use the new MIME
    type.
    However, when opening a download from download home, GUID is used to
    find the download.
    The download history stores the original MIME type, which is not
    remapped.
    This causes Chrome to incorrectly launch an intent even if the type is
    supported.
    
    BUG=657594
    
    Review-Url: https://codereview.chromium.org/2437243002
    Cr-Commit-Position: refs/heads/master@{#427205}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/ui/StubbedProvider.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/ui/StubbedProvider.java
@@ -82,7 +82,8 @@ public class StubbedProvider implements BackendProvider {
         }
 
         @Override
-        public boolean isDownloadOpenableInBrowser(String guid, boolean isOffTheRecord) {
+        public boolean isDownloadOpenableInBrowser(
+                String guid, boolean isOffTheRecord, String mimeType) {
             return false;
         }
     }
