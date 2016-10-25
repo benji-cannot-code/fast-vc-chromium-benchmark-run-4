37affa425a50a9ba93d1ca86362778b16eb7e394
amaralp <amaralp@chromium.org>
2016-10-24 18:17:28 -0700

    Start action mode even if clipboard is empty
    Previously clank would not call View#startActionMode for a FloatingPastePopup menu
    if the copy/paste clipboard was empty (because there would be nothing in the menu).
    This is a problem because apps using WebView may want to add items to the
    FloatingPastePopup menu (using startActionMode) even if the clipboard is empty.
    
    This patch makes clank call View#startActionMode and make an empty menu when there
    is nothing to paste.
    
    BUG=652916
    
    Review-Url: https://codereview.chromium.org/2443963003
    Cr-Commit-Position: refs/heads/master@{#427210}

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -2526,7 +2526,7 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Displa
             return;
         }
 
-        if (!mHasInsertion || !canPaste()) return;
+        if (!mHasInsertion || (!supportsFloatingActionMode() && !canPaste())) return;
 
         PastePopupMenu pastePopupMenu = getPastePopup();
         if (pastePopupMenu == null) return;
