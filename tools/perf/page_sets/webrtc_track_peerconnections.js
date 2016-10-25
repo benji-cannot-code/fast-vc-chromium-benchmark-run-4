6e7145b5ae34dc517fdafb3f1d8a0a21706f3eeb
foolip <foolip@chromium.org>
2016-10-25 09:05:47 -0700

    Expose unprefixed RTCPeerConnection
    Intent to Ship:
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/7dTqNBRIUrg/GfaDq9-YBAAJ
    
    BUG=658423
    
    Review-Url: https://codereview.chromium.org/2450803002
    Cr-Commit-Position: refs/heads/master@{#427372}

--- tools/perf/page_sets/webrtc_track_peerconnections.js
+++ tools/perf/page_sets/webrtc_track_peerconnections.js
@@ -3,16 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// This file overwrites the webkitRTCPeerConnection constructor with a
-// new constructor which tracks all created connections. It does this by
-// periodically gathering statistics on all connections, using the WebRTC
-// statistics API. All reports are gathered into window.peerConnectionReports,
-// which contains one list per connection. In each list there is a number of
-// report batches, which in turn contains metric names mapped to values.
+// This file overwrites the RTCPeerConnection constructor with a new constructor
+// which tracks all created connections. It does this by periodically gathering
+// statistics on all connections, using the WebRTC statistics API. All reports
+// are gathered into window.peerConnectionReports, which contains one list per
+// connection. In each list there is a number of report batches, which in turn
+// contains metric names mapped to values.
 
 window.peerConnectionReports = [];
 
-webkitRTCPeerConnection = (function() {
+RTCPeerConnection = webkitRTCPeerConnection = (function() {
   function getReportsAsDicts(getStatsResult) {
     var result = [];
     getStatsResult.forEach(function(report) {
@@ -38,7 +38,7 @@ webkitRTCPeerConnection = (function() {
     }, pollIntervalMs);
   }
 
-  var originalConstructor = webkitRTCPeerConnection;
+  var originalConstructor = RTCPeerConnection;
   return function() {
     // Bind the incoming arguments to the original constructor.
     var args = [null].concat(Array.prototype.slice.call(arguments));
