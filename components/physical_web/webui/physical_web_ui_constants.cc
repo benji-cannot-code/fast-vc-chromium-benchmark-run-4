dea7eb25dc7e15433da54e74ae4f8b3f2a466fa3
mattreynolds <mattreynolds@chromium.org>
2016-10-25 11:10:00 -0700

    Add metrics for Physical Web WebUI selections
    Count how many times the user has selected a Physical Web URL at each index
    in the Physical Web WebUI list view.  Also count the total number of list
    view selections.
    
    BUG=630769
    
    Review-Url: https://codereview.chromium.org/2439613002
    Cr-Commit-Position: refs/heads/master@{#427414}

--- components/physical_web/webui/physical_web_ui_constants.cc
+++ components/physical_web/webui/physical_web_ui_constants.cc
@@ -13,6 +13,7 @@ const char kPhysicalWebCSS[] = "physical_web.css";
 
 // Message handlers.
 const char kRequestNearbyUrls[] = "requestNearbyURLs";
+const char kPhysicalWebItemClicked[] = "physicalWebItemClicked";
 
 // Strings.
 const char kTitle[] = "title";
@@ -22,6 +23,7 @@ const char kPageInfoIcon[] = "pageInfoIcon";
 const char kPageInfoTitle[] = "pageInfoTitle";
 const char kResolvedUrl[] = "resolvedUrl";
 const char kScannedUrl[] = "scannedUrl";
+const char kIndex[] = "index";
 const char kReturnNearbyUrls[] = "returnNearbyURLs";
 const char kMetadata[] = "metadata";
 
--- ios/chrome/browser/ui/webui/physical_web_ui.cc
+++ ios/chrome/browser/ui/webui/physical_web_ui.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/macros.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/metrics/user_metrics.h"
 #include "base/values.h"
 #include "components/grit/components_resources.h"
@@ -21,6 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/web/public/webui/web_ui_ios_message_handler.h"
 
 namespace {
+
+// Maximum value for the "selected position" histogram.
+const int kSelectedPositionMaxValue = 50;
 
 web::WebUIIOSDataSource* CreatePhysicalWebUIDataSource() {
   web::WebUIIOSDataSource* html_source =
@@ -54,6 +58,7 @@ class PhysicalWebDOMHandler : public web::WebUIIOSMessageHandler {
   void RegisterMessages() override;
 
   void HandleRequestNearbyURLs(const base::ListValue* args);
+  void HandlePhysicalWebItemClicked(const base::ListValue* args);
 
  private:
   DISALLOW_COPY_AND_ASSIGN(PhysicalWebDOMHandler);
@@ -64,6 +69,10 @@ void PhysicalWebDOMHandler::RegisterMessages() {
       physical_web_ui::kRequestNearbyUrls,
       base::Bind(&PhysicalWebDOMHandler::HandleRequestNearbyURLs,
                  base::Unretained(this)));
+  web_ui()->RegisterMessageCallback(
+      physical_web_ui::kPhysicalWebItemClicked,
+      base::Bind(&PhysicalWebDOMHandler::HandlePhysicalWebItemClicked,
+                 base::Unretained(this)));
 }
 
 void PhysicalWebDOMHandler::HandleRequestNearbyURLs(
@@ -73,9 +82,40 @@ void PhysicalWebDOMHandler::HandleRequestNearbyURLs(
   std::unique_ptr<base::ListValue> metadata =
       GetApplicationContext()->GetPhysicalWebDataSource()->GetMetadata();
 
+  // Add item indices. When an item is selected, the index of the selected item
+  // is recorded in a UMA histogram.
+  for (size_t i = 0; i < metadata->GetSize(); i++) {
+    base::DictionaryValue* metadata_item = NULL;
+    metadata->GetDictionary(i, &metadata_item);
+    metadata_item->SetInteger(physical_web_ui::kIndex, i);
+  }
+
   results.Set(physical_web_ui::kMetadata, metadata.release());
 
+  // Pass the list of Physical Web URL metadata to the WebUI. A jstemplate will
+  // create a list view with an item for each URL.
   web_ui()->CallJavascriptFunction(physical_web_ui::kReturnNearbyUrls, results);
+}
+
+void PhysicalWebDOMHandler::HandlePhysicalWebItemClicked(
+    const base::ListValue* args) {
+  int index;
+  if (!args->GetInteger(0, &index)) {
+    DLOG(ERROR) << "Invalid selection index";
+    return;
+  }
+
+  // Record the index of the selected item. The index must be strictly less than
+  // the maximum enumeration value.
+  if (index > kSelectedPositionMaxValue) {
+    index = kSelectedPositionMaxValue;
+  }
+  UMA_HISTOGRAM_EXACT_LINEAR("PhysicalWeb.WebUI.ListViewUrlPosition", index,
+                             kSelectedPositionMaxValue);
+
+  // Count the number of selections.
+  base::RecordAction(
+      base::UserMetricsAction("PhysicalWeb.WebUI.ListViewUrlSelected"));
 }
 
 }  // namespace
