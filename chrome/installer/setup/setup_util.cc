f47b9d0ed8aad0826dcbe710190544515adf074e
zmin <zmin@chromium.org>
2016-10-25 12:26:10 -0700

    Fix typo of Lzma metric suffix
    BUG=630181
    
    Review-Url: https://codereview.chromium.org/2450013002
    Cr-Commit-Position: refs/heads/master@{#427438}

--- chrome/installer/setup/setup_util.cc
+++ chrome/installer/setup/setup_util.cc
@@ -633,7 +633,7 @@ void RecordUnPackMetrics(UnPackStatus unpack_status, UnPackConsumer consumer) {
 
   switch (consumer) {
     case UnPackConsumer::CHROME_ARCHIVE_PATCH:
-      consumer_name = "ChromeArchivePath";
+      consumer_name = "ChromeArchivePatch";
       break;
     case UnPackConsumer::COMPRESSED_CHROME_ARCHIVE:
       consumer_name = "CompressedChromeArchive";
