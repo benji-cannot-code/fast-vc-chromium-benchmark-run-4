51e349c3b4683e7faeaeeca89c96588a4cf97460
rohitrao <rohitrao@chromium.org>
2016-10-25 11:41:56 -0700

    [ios] Adds support for parsing Text-to-Speech search results.
    BUG=656617
    
    Review-Url: https://codereview.chromium.org/2449593002
    Cr-Commit-Position: refs/heads/master@{#427423}

---
+++ ios/chrome/browser/ui/voice/voice_search_notification_names.mm
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/voice/voice_search_notification_names.h"
+
+NSString* const kVoiceSearchWillShowNotification =
+    @"kVoiceSearchWillShowNotification";
+NSString* const kVoiceSearchWillHideNotification =
+    @"kVoiceSearchWillHideNotification";
+
+NSString* const kTTSAudioReadyForPlaybackNotification =
+    @"kTTSAudioReadyForPlaybackNotification";
+NSString* const kTTSWillStartPlayingNotification =
+    @"kTTSWillStartPlayingNotification";
+NSString* const kTTSDidStopPlayingNotification =
+    @"kTTSDidStopPlayingNotification";
+
+NSString* const kVoiceSearchBarViewButtonSelectedNotification =
+    @"kVoiceSearchBarViewButtonSelectedNotification";
+NSString* const kVoiceSearchBarViewButtonDeselectedNotification =
+    @"kVoiceSearchBarViewButtonDeselectedNotification";
