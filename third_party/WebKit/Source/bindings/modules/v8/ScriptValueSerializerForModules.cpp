60ec930b4edbc7f59e6a7ff86ddf714bdd275601
jbroman <jbroman@chromium.org>
2016-10-24 18:55:12 -0700

    Handle RTCCertificate::fromPEM returning nullptr, with a unit test.
    BUG=148757,webrtc:6488
    
    Review-Url: https://codereview.chromium.org/2432493002
    Cr-Commit-Position: refs/heads/master@{#427235}

--- third_party/WebKit/Source/bindings/modules/v8/ScriptValueSerializerForModules.cpp
+++ third_party/WebKit/Source/bindings/modules/v8/ScriptValueSerializerForModules.cpp
@@ -413,6 +413,8 @@ bool SerializedScriptValueReaderForModules::readRTCCertificate(
 
   std::unique_ptr<WebRTCCertificate> certificate(
       certificateGenerator->fromPEM(pemPrivateKey, pemCertificate));
+  if (!certificate)
+    return false;
   RTCCertificate* jsCertificate = new RTCCertificate(std::move(certificate));
 
   *value =
