c9fabb669b2ce2896ea100fc693386df84d6cab5
mattreynolds <mattreynolds@chromium.org>
2016-10-25 11:28:25 -0700

    Expose Physical Web metadata to native clients over JNI
    BUG=636490
    
    Review-Url: https://codereview.chromium.org/2403423005
    Cr-Commit-Position: refs/heads/master@{#427418}

--- ios/chrome/common/physical_web/physical_web_scanner.mm
+++ ios/chrome/common/physical_web/physical_web_scanner.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/values.h"
+#include "components/physical_web/data_source/physical_web_data_source.h"
 #include "device/bluetooth/uribeacon/uri_encoder.h"
 #import "ios/chrome/common/physical_web/physical_web_device.h"
 #import "ios/chrome/common/physical_web/physical_web_request.h"
@@ -190,11 +191,11 @@ enum BeaconType {
     std::string description = base::SysNSStringToUTF8([device description]);
 
     auto metadataItem = base::MakeUnique<base::DictionaryValue>();
-    metadataItem->SetString("scannedUrl", scannedUrl);
-    metadataItem->SetString("resolvedUrl", resolvedUrl);
-    metadataItem->SetString("icon", icon);
-    metadataItem->SetString("title", title);
-    metadataItem->SetString("description", description);
+    metadataItem->SetString(kPhysicalWebScannedUrlKey, scannedUrl);
+    metadataItem->SetString(kPhysicalWebResolvedUrlKey, resolvedUrl);
+    metadataItem->SetString(kPhysicalWebIconUrlKey, icon);
+    metadataItem->SetString(kPhysicalWebTitleKey, title);
+    metadataItem->SetString(kPhysicalWebDescriptionKey, description);
     metadataList->Append(std::move(metadataItem));
   }
 
