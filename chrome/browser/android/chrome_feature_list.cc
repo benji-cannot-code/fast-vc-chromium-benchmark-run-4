09cdaa832049c8907994c54099f231e43ab056c3
pkotwicz <pkotwicz@chromium.org>
2016-10-25 14:52:31 -0700

    Use feature architecture as kill switch for WebAPKs instead of field trial
    This CL converts the existing WebAPK finch field trial to use the feature
    architecture. This is in preparation to adding "configurable WebAPK feature
    parameters" in https://codereview.chromium.org/2441203002/
    
    BUG=651561
    
    Review-Url: https://codereview.chromium.org/2441243003
    Cr-Commit-Position: refs/heads/master@{#427494}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -51,6 +51,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kSpecialLocaleFeature,
     &kSpecialLocaleWrapper,
     &kTabReparenting,
+    &kWebApks,
     &ntp_snippets::kContentSuggestionsFeature,
     &ntp_snippets::kSaveToOfflineFeature,
     &ntp_snippets::kSectionDismissalFeature,
@@ -107,6 +108,10 @@ const base::Feature kTabReparenting{"TabReparenting",
 
 const base::Feature kUserMediaScreenCapturing{
     "UserMediaScreenCapturing", base::FEATURE_DISABLED_BY_DEFAULT};
+
+// Makes "Add to Home screen" in the app menu generate an APK for the shortcut
+// URL which opens Chrome in fullscreen.
+const base::Feature kWebApks{"WebApks", base::FEATURE_DISABLED_BY_DEFAULT};
 
 static jboolean IsEnabled(JNIEnv* env,
                           const JavaParamRef<jclass>& clazz,
