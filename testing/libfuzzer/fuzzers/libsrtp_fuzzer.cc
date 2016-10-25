d9c80782bcfae869174e6610886e891e8aa6242d
mattdr <mattdr@chromium.org>
2016-10-24 17:28:46 -0700

    Remove unused include
    Added by mistake in https://chromiumcodereview.appspot.com/2436913003
    
    BUG=webrtc:6376
    
    Review-Url: https://codereview.chromium.org/2450553002
    Cr-Commit-Position: refs/heads/master@{#427187}

--- testing/libfuzzer/fuzzers/libsrtp_fuzzer.cc
+++ testing/libfuzzer/fuzzers/libsrtp_fuzzer.cc
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include <assert.h>
-#include <netinet/in.h>
 #include <stddef.h>
 #include <stdint.h>
 
