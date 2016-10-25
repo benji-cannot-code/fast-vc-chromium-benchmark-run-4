c2a2ac358696804bfbbc75199592a86be2cd8483
kraynov <kraynov@chromium.org>
2016-10-25 09:45:06 -0700

    Tracing: Enable tracing_perftests on bots.
    There is an intention to track TRACE_EVENT macros performance
    without browser and it's designed exactly like cc_perftests.
    See bug entry for details.
    
    BUG=656029
    
    Review-Url: https://codereview.chromium.org/2418983003
    Cr-Commit-Position: refs/heads/master@{#427379}

--- tools/perf/generate_perf_json.py
+++ tools/perf/generate_perf_json.py
@@ -114,6 +114,20 @@ SCRIPT_TESTS = [
   },
   {
     'args': [
+      'tracing_perftests',
+      '--test-launcher-print-test-stdio=always'
+    ],
+    'name': 'tracing_perftests',
+    'script': 'gtest_perf_test.py',
+    'testers': [
+      {
+        'name': 'Linux Perf',
+        'shards': [3]
+      },
+    ]
+  },
+  {
+    'args': [
       'load_library_perf_tests',
       '--test-launcher-print-test-stdio=always'
     ],
