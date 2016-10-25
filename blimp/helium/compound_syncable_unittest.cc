19544112e34178a6003c0488d9f6c5c0b1868ba3
scf <scf@chromium.org>
2016-10-25 10:16:00 -0700

    Helium: Fix flaky test
    Unit test fixture was not using HeliumTest test fixture and thus not properly cleaning state.
    
    BUG=658798
    
    Review-Url: https://codereview.chromium.org/2444193002
    Cr-Commit-Position: refs/heads/master@{#427387}

--- blimp/helium/compound_syncable_unittest.cc
+++ blimp/helium/compound_syncable_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/macros.h"
+#include "blimp/helium/helium_test.h"
 #include "blimp/helium/lww_register.h"
 #include "blimp/helium/revision_generator.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -39,7 +40,7 @@ class SampleCompoundSyncable : public CompoundSyncable {
   DISALLOW_COPY_AND_ASSIGN(SampleCompoundSyncable);
 };
 
-class CompoundSyncableTest : public testing::Test {
+class CompoundSyncableTest : public HeliumTest {
  public:
   CompoundSyncableTest()
       : last_sync_engine_(0),
