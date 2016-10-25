e18cc59ef6d303da99857d2a0c91f2a4ce5a2927
watk <watk@chromium.org>
2016-10-24 19:02:54 -0700

    Use clearer names for the PictureBuffer texture id members
    s/texture_ids/client_texture_ids and s/internal_texture_ids/service_texture_ids/ because
    these names are clearer and more consistent with the rest of the gpu code. This also
    cleans up GpuVideoDecodeAccelerator and the VDAs, which used internal_texure_ids to refer
    to the client ids, and texture_ids to refer to the service ids.
    
    Review-Url: https://codereview.chromium.org/2382113002
    Cr-Commit-Position: refs/heads/master@{#427237}

--- media/gpu/dxva_picture_buffer_win.cc
+++ media/gpu/dxva_picture_buffer_win.cc
@@ -275,7 +275,7 @@ bool PbufferPictureBuffer::CopySurfaceComplete(
   GLint current_texture = 0;
   glGetIntegerv(GL_TEXTURE_BINDING_2D, &current_texture);
 
-  glBindTexture(GL_TEXTURE_2D, picture_buffer_.texture_ids()[0]);
+  glBindTexture(GL_TEXTURE_2D, picture_buffer_.service_texture_ids()[0]);
 
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
@@ -361,11 +361,11 @@ bool EGLStreamPictureBuffer::Initialize() {
   stream_ = eglCreateStreamKHR(egl_display, stream_attributes);
   RETURN_ON_FAILURE(!!stream_, "Could not create stream", false);
   gl::ScopedActiveTexture texture0(GL_TEXTURE0);
-  gl::ScopedTextureBinder texture0_binder(GL_TEXTURE_EXTERNAL_OES,
-                                          picture_buffer_.texture_ids()[0]);
+  gl::ScopedTextureBinder texture0_binder(
+      GL_TEXTURE_EXTERNAL_OES, picture_buffer_.service_texture_ids()[0]);
   gl::ScopedActiveTexture texture1(GL_TEXTURE1);
-  gl::ScopedTextureBinder texture1_binder(GL_TEXTURE_EXTERNAL_OES,
-                                          picture_buffer_.texture_ids()[1]);
+  gl::ScopedTextureBinder texture1_binder(
+      GL_TEXTURE_EXTERNAL_OES, picture_buffer_.service_texture_ids()[1]);
 
   EGLAttrib consumer_attributes[] = {
       EGL_COLOR_BUFFER_TYPE,
@@ -468,11 +468,11 @@ bool EGLStreamCopyPictureBuffer::Initialize(
   stream_ = eglCreateStreamKHR(egl_display, stream_attributes);
   RETURN_ON_FAILURE(!!stream_, "Could not create stream", false);
   gl::ScopedActiveTexture texture0(GL_TEXTURE0);
-  gl::ScopedTextureBinder texture0_binder(GL_TEXTURE_EXTERNAL_OES,
-                                          picture_buffer_.texture_ids()[0]);
+  gl::ScopedTextureBinder texture0_binder(
+      GL_TEXTURE_EXTERNAL_OES, picture_buffer_.service_texture_ids()[0]);
   gl::ScopedActiveTexture texture1(GL_TEXTURE1);
-  gl::ScopedTextureBinder texture1_binder(GL_TEXTURE_EXTERNAL_OES,
-                                          picture_buffer_.texture_ids()[1]);
+  gl::ScopedTextureBinder texture1_binder(
+      GL_TEXTURE_EXTERNAL_OES, picture_buffer_.service_texture_ids()[1]);
 
   EGLAttrib consumer_attributes[] = {
       EGL_COLOR_BUFFER_TYPE,
