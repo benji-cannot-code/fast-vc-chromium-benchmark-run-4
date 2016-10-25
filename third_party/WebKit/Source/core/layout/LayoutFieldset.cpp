20ab7888bee6b33d4abf1c057becbe6ce3281ce2
glebl <glebl@chromium.org>
2016-10-25 08:47:05 -0700

    Revert "Allow to set display "inline" on legend element"
    This reverts commit 2383abb8b227d12ae53f2b163b57e02ff63dbf0b.
    
    Reason: it violates the spec that says that fieldset's legend should be
    displayed as a block box (overriding any explicit 'display' value).
    Also it introduces an interop issue with Firefox
    https://bugzilla.mozilla.org/show_bug.cgi?id=653870.
    
    BUG=481983
    
    Review-Url: https://codereview.chromium.org/2447843002
    Cr-Commit-Position: refs/heads/master@{#427364}

--- third_party/WebKit/Source/core/layout/LayoutFieldset.cpp
+++ third_party/WebKit/Source/core/layout/LayoutFieldset.cpp
@@ -222,10 +222,8 @@ LayoutBox* LayoutFieldset::findInFlowLegend() const {
     if (legend->isFloatingOrOutOfFlowPositioned())
       continue;
 
-    if (isHTMLLegendElement(legend->node())) {
-      if (legend->isBox())
-        return toLayoutBox(legend);
-    }
+    if (isHTMLLegendElement(legend->node()))
+      return toLayoutBox(legend);
   }
   return nullptr;
 }
