0987fb7ac6352e1a3e2b6827dadefc4e4d85c1e9
mohsen <mohsen@chromium.org>
2016-10-26 10:24:11 -0700

    Revert of [ObjC ARC] Converts ios/chrome/app to ARC. (patchset #1 id:1 of https://codereview.chromium.org/2449063002/ )
    Reason for revert:
    Compile failure on ios-device builder: https://uberchromegw.corp.google.com/i/chromium.mac/builders/ios-device/builds/26773
    
    Original issue's description:
    > [ObjC ARC] Converts ios/chrome/app to ARC.
    >
    > Notable changes:none.
    >
    > BUG=659150
    > TEST=None.
    >
    > Committed: https://crrev.com/7a1f218b33faf5d0dd637f6a6854566e6cd80a3d
    > Cr-Commit-Position: refs/heads/master@{#427722}
    
    TBR=eugenebut@chromium.org,thestig@chromium.org,stkhapugin@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=659150
    
    Review-Url: https://codereview.chromium.org/2453033002
    Cr-Commit-Position: refs/heads/master@{#427729}

--- ios/chrome/app/application_delegate/memory_warning_helper.mm
+++ ios/chrome/app/application_delegate/memory_warning_helper.mm
@@ -10,10 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/crash_report/breakpad_helper.h"
 #import "ios/chrome/browser/metrics/previous_session_info.h"
 
-#if !defined(__has_feature) || !__has_feature(objc_arc)
-#error "This file requires ARC support."
-#endif
-
 namespace {
 // The number of seconds to wait after a memory warning to clear the flag used
 // to detect Out Of Memory crashes.
--- ios/chrome/app/safe_mode_crashing_modules_config.mm
+++ ios/chrome/app/safe_mode_crashing_modules_config.mm
@@ -9,10 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/foundation_util.h"
 #import "base/mac/scoped_nsobject.h"
 
-#if !defined(__has_feature) || !__has_feature(objc_arc)
-#error "This file requires ARC support."
-#endif
-
 namespace {
 
 NSString* const kStartupCrashModulesKey = @"StartupCrashModules";
