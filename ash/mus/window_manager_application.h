ced6d59c10c6ebf00daf7fecdd0cb0907911b1da
erg <erg@chromium.org>
2016-10-26 16:24:52 -0700

    mash: remove AcceleratorRegistrar.
    As requested after the remove app_driver patch, we can now remove the
    entire AcceleratorRegistrar concept now that accelerators are hooked up
    properly in ash.
    
    BUG=631836
    
    Review-Url: https://codereview.chromium.org/2452783002
    Cr-Commit-Position: refs/heads/master@{#427872}

--- ash/mus/window_manager_application.h
+++ ash/mus/window_manager_application.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/service_manager/public/cpp/service.h"
 #include "services/tracing/public/cpp/provider.h"
 #include "services/ui/common/types.h"
-#include "services/ui/public/interfaces/accelerator_registrar.mojom.h"
 
 namespace base {
 class SequencedWorkerPool;
@@ -45,7 +44,6 @@ class WindowTreeClient;
 namespace ash {
 namespace mus {
 
-class AcceleratorRegistrarImpl;
 class NativeWidgetFactoryMus;
 class NetworkConnectDelegateMus;
 class WindowManager;
@@ -53,7 +51,6 @@ class WindowManager;
 // Hosts the window manager and the ash system user interface for mash.
 class WindowManagerApplication
     : public service_manager::Service,
-      public service_manager::InterfaceFactory<ui::mojom::AcceleratorRegistrar>,
       public mash::session::mojom::ScreenlockStateListener {
  public:
   WindowManagerApplication();
@@ -67,8 +64,6 @@ class WindowManagerApplication
   friend class WmTestBase;
   friend class WmTestHelper;
 
-  void OnAcceleratorRegistrarDestroyed(AcceleratorRegistrarImpl* registrar);
-
   void InitWindowManager(
       std::unique_ptr<ui::WindowTreeClient> window_tree_client,
       const scoped_refptr<base::SequencedWorkerPool>& blocking_pool);
@@ -81,10 +76,6 @@ class WindowManagerApplication
   void OnStart(const service_manager::ServiceInfo& info) override;
   bool OnConnect(const service_manager::ServiceInfo& remote_info,
                  service_manager::InterfaceRegistry* registry) override;
-
-  // service_manager::InterfaceFactory<ui::mojom::AcceleratorRegistrar>:
-  void Create(const service_manager::Identity& remote_identity,
-              ui::mojom::AcceleratorRegistrarRequest request) override;
 
   // session::mojom::ScreenlockStateListener:
   void ScreenlockStateChanged(bool locked) override;
@@ -100,8 +91,6 @@ class WindowManagerApplication
 
   // A blocking pool used by the WindowManager's shell; not used in tests.
   scoped_refptr<base::SequencedWorkerPool> blocking_pool_;
-
-  std::set<AcceleratorRegistrarImpl*> accelerator_registrars_;
 
   mash::session::mojom::SessionPtr session_;
 
