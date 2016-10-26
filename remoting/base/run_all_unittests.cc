908d00ceb36c6896d1185d954a46b87be7c81aaa
sammc <sammc@chromium.org>
2016-10-26 15:09:44 -0700

    Use ChannelMojo between the remoting daemon and network processes.
    This refactors LaunchProcessWithToken to take an explicit list of
    handles for the child to inherit (copied from the equivalent process
    launch code in //base).
    
    BUG=604282
    
    Committed: https://crrev.com/41c99b466b9bbba9dcb002e8cb00f1d7da22dff4
    Review-Url: https://codereview.chromium.org/2424353002
    Cr-Original-Commit-Position: refs/heads/master@{#427238}
    Cr-Commit-Position: refs/heads/master@{#427843}

--- remoting/base/run_all_unittests.cc
+++ remoting/base/run_all_unittests.cc
@@ -5,9 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/launcher/unit_test_launcher.h"
 #include "base/test/test_suite.h"
+#include "mojo/edk/embedder/embedder.h"
 
 int main(int argc, char** argv) {
   base::TestSuite test_suite(argc, argv);
+
+  mojo::edk::Init();
 
   return base::LaunchUnitTests(
       argc, argv, base::Bind(&base::TestSuite::Run,
--- remoting/host/host_main.cc
+++ remoting/host/host_main.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
+#include "mojo/edk/embedder/embedder.h"
 #include "remoting/base/breakpad.h"
 #include "remoting/host/host_exit_codes.h"
 #include "remoting/host/logging.h"
@@ -211,6 +212,10 @@ int HostMain(int argc, char** argv) {
   base::i18n::InitializeICU();
 
   remoting::LoadResources("");
+
+#if defined(REMOTING_MULTI_PROCESS)
+  mojo::edk::Init();
+#endif
 
   // Invoke the entry point.
   int exit_code = main_routine();
