253313932808889e7caac35deab3e039cf736763
ryansturm <ryansturm@chromium.org>
2016-10-26 14:14:44 -0700

    Add UMA to log the size of the Previews opt out DB
    This logs the number of rows in the Previews opt out DB at profile
    startup.
    
    BUG=656739
    
    Review-Url: https://codereview.chromium.org/2448213002
    Cr-Commit-Position: refs/heads/master@{#427793}

--- components/previews/core/previews_opt_out_store_sql.cc
+++ components/previews/core/previews_opt_out_store_sql.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/sequenced_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "components/previews/core/previews_black_list.h"
@@ -178,7 +179,8 @@ void LoadBlackListFromDataBase(
         base::Time::FromInternalValue(statement.ColumnInt64(1)));
   }
 
-  // TODO(ryansturm): Add UMA to log |count|. crbug.com/656739
+  UMA_HISTOGRAM_COUNTS_10000("Previews.OptOut.DBRowCount", count);
+
   if (count > kMaxRowsInDB) {
     // Delete the oldest entries if there are more than |kMaxEntriesInDB|.
     // DELETE ... LIMIT -1 OFFSET x means delete all but the first x entries.
