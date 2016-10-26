0648a6e484b5563bb22c4f4d9e60c00443ad07be
thestig <thestig@chromium.org>
2016-10-26 13:52:33 -0700

    Fix the build after r427784.
    r427784 originally landed as r427752, but failed with an error.
    Earlier, r427683 landed, adding the same #include. Rather than
    conflicting, the CQ somehow ignored this. Thus when r427752 got reverted
    as r427772, the #include got deleted, and the build failed.
    
    TBR=hongchan@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2453103002
    Cr-Commit-Position: refs/heads/master@{#427785}

--- pdf/pdfium/pdfium_engine.cc
+++ pdf/pdfium/pdfium_engine.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "base/macros.h"
+#include "base/memory/ptr_util.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
