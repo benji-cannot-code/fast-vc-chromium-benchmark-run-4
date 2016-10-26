dd2241467fbf3187738dbbe0bc8c69527a8538a8
tkent <tkent@chromium.org>
2016-10-26 06:28:37 -0700

    INPUT element: Updating value to the previous defaultValue should dispatch "change" event.
    We have set defaultValue to m_textAsOfLastFormControlChangeEvent only when input
    type was determined, and updating defalutValue after it didn't update
    m_textAsOfLastFormControlChangeEvent. So, 'change' event was not dispatched in
    some cases.
    
    BUG=659499
    
    Review-Url: https://codereview.chromium.org/2452903002
    Cr-Commit-Position: refs/heads/master@{#427675}

--- third_party/WebKit/Source/core/html/HTMLInputElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLInputElement.cpp
@@ -719,6 +719,8 @@ void HTMLInputElement::parseAttribute(const QualifiedName& name,
       setNeedsStyleRecalc(
           SubtreeStyleChange,
           StyleChangeReasonForTracing::fromAttribute(valueAttr));
+      if (m_inputType->valueMode() == ValueMode::kValue)
+        setTextAsOfLastFormControlChangeEvent(sanitizeValue(value));
     }
     m_needsToUpdateViewValue = true;
     setNeedsValidityCheck();
