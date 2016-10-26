020a2618a8e9d3d6a7574e5f6bcf7bb9b146a245
hayato <hayato@chromium.org>
2016-10-26 02:40:58 -0700

    Do not use outdated m_distributedNodes of slots in non-shadow trees
    HTMLSlotElement::m_distributedNodes might be outdated for slots in non-shadow trees.
    
    There are other places where outdated m_distributedNodes might be used, which could be addressed in another CL.
    
    BUG=642303
    
    Review-Url: https://codereview.chromium.org/2452473003
    Cr-Commit-Position: refs/heads/master@{#427650}

--- third_party/WebKit/Source/core/html/HTMLSlotElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLSlotElement.cpp
@@ -98,6 +98,8 @@ void HTMLSlotElement::updateDistributedNodesManually() {
 
 const HeapVector<Member<Node>>& HTMLSlotElement::getDistributedNodes() {
   DCHECK(!needsDistributionRecalc());
+  // m_distributedNodes of slots in non-shadow trees are not updated in recalc
+  // distribution flow.
   if (!supportsDistribution())
     updateDistributedNodesManually();
   return m_distributedNodes;
@@ -179,7 +181,7 @@ AtomicString HTMLSlotElement::name() const {
 }
 
 void HTMLSlotElement::attachLayoutTree(const AttachContext& context) {
-  for (auto& node : m_distributedNodes) {
+  for (auto& node : getDistributedNodes()) {
     if (node->needsAttach())
       node->attachLayoutTree(context);
   }
@@ -318,6 +320,8 @@ void HTMLSlotElement::enqueueSlotChangeEvent() {
   }
 
   ShadowRoot* root = containingShadowRoot();
+  // TODO(hayato): Relax this check if slots in non-shadow trees are well
+  // supported.
   DCHECK(root);
   DCHECK(root->isV1());
   root->owner()->setNeedsDistributionRecalc();
