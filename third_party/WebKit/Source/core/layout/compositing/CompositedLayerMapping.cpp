1696a00682031bf4e161e2782a2138751e4daca3
schenney <schenney@chromium.org>
2016-10-26 16:48:21 -0700

    Fix the check for RootScrollerDescendant with needsAncestorClip
    The existing code checks for the presence of a
    RootScrollerAsDescendant outside of
    owningLayerClippedByLayerNotAboveCompositedAncestor.
    This is a problem because only one of the call sites to
    owningLayerClippedByLayerNotAboveCompositedAncestor
    makes the additional check.
    
    This patch fixes the prob;lem by moving the RootScrollerAsDescendant
    check inside owningLayerClippedByLayerNotAboveCompositedAncestor.
    
    R=flackr@chromium.org, bokan@chromium.org
    BUG=505516
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2451273004
    Cr-Commit-Position: refs/heads/master@{#427882}

--- third_party/WebKit/Source/core/layout/compositing/CompositedLayerMapping.cpp
+++ third_party/WebKit/Source/core/layout/compositing/CompositedLayerMapping.cpp
@@ -473,6 +473,9 @@ bool CompositedLayerMapping::
   if (clippingContainer->enclosingLayer() == scrollParent)
     return false;
 
+  if (clippingContainer->enclosingLayer()->hasRootScrollerAsDescendant())
+    return false;
+
   if (compositingAncestor->layoutObject()->isDescendantOf(clippingContainer))
     return false;
 
@@ -546,10 +549,7 @@ bool CompositedLayerMapping::updateGraphicsLayerConfiguration() {
   // that sibling need not be composited at all. In such scenarios, an ancestor
   // clipping layer is necessary to apply the composited clip for this layer.
   bool needsAncestorClip =
-      owningLayerClippedByLayerNotAboveCompositedAncestor(scrollParent) &&
-      !m_owningLayer.clippingContainer()
-           ->enclosingLayer()
-           ->hasRootScrollerAsDescendant();
+      owningLayerClippedByLayerNotAboveCompositedAncestor(scrollParent);
 
   if (updateClippingLayers(needsAncestorClip, needsDescendantsClippingLayer))
     layerConfigChanged = true;
