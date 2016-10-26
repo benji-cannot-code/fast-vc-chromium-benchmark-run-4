b3e6c743091c42f0a40b2201ded12177a4a7a059
mlamouri <mlamouri@chromium.org>
2016-10-26 14:09:40 -0700

    Remove all "new" suffixes from media player assets.
    Now that the "old" assets are out, we should leave the "new" namespace
    for things that might actually be new-er :)
    
    BUG=658520
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2443903002
    Cr-Commit-Position: refs/heads/master@{#427788}

--- third_party/WebKit/Source/core/layout/LayoutThemeMobile.cpp
+++ third_party/WebKit/Source/core/layout/LayoutThemeMobile.cpp
@@ -47,7 +47,7 @@ String LayoutThemeMobile::extraDefaultStyleSheet() {
 }
 
 String LayoutThemeMobile::extraMediaControlsStyleSheet() {
-  return loadResourceAsASCIIString("mediaControlsAndroidNew.css");
+  return loadResourceAsASCIIString("mediaControlsAndroid.css");
 }
 
 String LayoutThemeMobile::extraFullscreenStyleSheet() {
