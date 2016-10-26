51697a1d5e54c03e4733eb60e8a22d932a22a572
mlippautz <mlippautz@chromium.org>
2016-10-26 14:21:40 -0700

    [wrapper-tracing] More write barriers for ContainerNode
    Consistently use the inline setter that also does the write barrier.
    
    BUG=chromium:468240
    R=haraken@chromium.org,hlopko@chromium.org
    
    Review-Url: https://codereview.chromium.org/2452103002
    Cr-Commit-Position: refs/heads/master@{#427800}

--- third_party/WebKit/Source/core/dom/ContainerNode.cpp
+++ third_party/WebKit/Source/core/dom/ContainerNode.cpp
@@ -320,7 +320,7 @@ void ContainerNode::insertBeforeCommon(Node& nextChild, Node& newChild) {
     prev->setNextSibling(&newChild);
   } else {
     DCHECK(firstChild() == nextChild);
-    m_firstChild = &newChild;
+    setFirstChild(&newChild);
   }
   newChild.setParentOrShadowHostNode(this);
   newChild.setPreviousSibling(prev);
@@ -558,9 +558,9 @@ void ContainerNode::removeBetween(Node* previousChild,
   if (previousChild)
     previousChild->setNextSibling(nextChild);
   if (m_firstChild == &oldChild)
-    m_firstChild = nextChild;
+    setFirstChild(nextChild);
   if (m_lastChild == &oldChild)
-    m_lastChild = previousChild;
+    setLastChild(previousChild);
 
   oldChild.setPreviousSibling(nullptr);
   oldChild.setNextSibling(nullptr);
