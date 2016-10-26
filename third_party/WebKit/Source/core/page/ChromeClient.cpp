e748451a94b771cf15e879eb6caff72fa9936bd3
mkwst <mkwst@chromium.org>
2016-10-26 03:47:55 -0700

    Sandbox should prevent 'execCommand("print", ...)'.
    Unless 'allow-modals' is present, 'sandbox' should prevent modal dialogs,
    including the print dialog popped up from 'execCommand("print", ...)'.
    The existing code doesn't handle this case; this patch moves the sandbox
    check up to 'ChromeClient::print()', which handles both 'print()' and
    'execCommand("print", ...'.
    
    BUG=658985
    R=jochen@chromium.org
    
    Review-Url: https://codereview.chromium.org/2448833003
    Cr-Commit-Position: refs/heads/master@{#427664}

--- third_party/WebKit/Source/core/page/ChromeClient.cpp
+++ third_party/WebKit/Source/core/page/ChromeClient.cpp
@@ -24,7 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/dom/Document.h"
 #include "core/dom/Element.h"
+#include "core/frame/FrameConsole.h"
 #include "core/frame/LocalFrame.h"
+#include "core/inspector/ConsoleMessage.h"
 #include "core/inspector/InspectorInstrumentation.h"
 #include "core/layout/HitTestResult.h"
 #include "core/page/FrameTree.h"
@@ -195,13 +197,23 @@ void ChromeClient::clearToolTip(LocalFrame& frame) {
   setToolTip(frame, String(), LTR);
 }
 
-void ChromeClient::print(LocalFrame* frame) {
+bool ChromeClient::print(LocalFrame* frame) {
+  if (frame->document()->isSandboxed(SandboxModals)) {
+    UseCounter::count(frame, UseCounter::DialogInSandboxedContext);
+    frame->console().addMessage(ConsoleMessage::create(
+        SecurityMessageSource, ErrorMessageLevel,
+        "Ignored call to 'print()'. The document is sandboxed, and the "
+        "'allow-modals' keyword is not set."));
+    return false;
+  }
+
   // Defer loads in case the client method runs a new event loop that would
   // otherwise cause the load to continue while we're in the middle of
   // executing JavaScript.
   ScopedPageLoadDeferrer deferrer;
 
   printDelegate(frame);
+  return true;
 }
 
 }  // namespace blink
