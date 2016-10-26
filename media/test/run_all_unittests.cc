b246d5a9a05f09c56e75922ae8c4f4208ebca641
xjz <xjz@chromium.org>
2016-10-25 18:22:11 -0700

    Add media remoting unittests to media_unittests. Remove media_remoting_unittests from the FYI bots. Instead, add them to media_unittests as a subset of the tests.
    BUG=643964
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2435423002
    Cr-Commit-Position: refs/heads/master@{#427562}

--- media/test/run_all_unittests.cc
+++ media/test/run_all_unittests.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/fake_media_resources.h"
 #include "media/base/media.h"
 #include "media/base/media_switches.h"
+#include "mojo/edk/embedder/embedder.h"
 
 #if defined(OS_ANDROID)
 #include "base/android/jni_android.h"
@@ -60,6 +61,7 @@ void TestSuiteNoAtExit::Initialize() {
 }
 
 int main(int argc, char** argv) {
+  mojo::edk::Init();
   TestSuiteNoAtExit test_suite(argc, argv);
 
   return base::LaunchUnitTests(
