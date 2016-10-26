db2afd5289dd99357c86e709c3c06bb577d03de5
rouslan <rouslan@chromium.org>
2016-10-26 10:04:43 -0700

    Add pending line item support for web payments.
    The API allows merchants to indicate when a line item is not yet final.
     - https://github.com/w3c/browser-payment-api/pull/270
     - https://w3c.github.io/browser-payment-api/#paymentitem-dictionary
    
    BUG=648050
    
    Review-Url: https://codereview.chromium.org/2449943002
    Cr-Commit-Position: refs/heads/master@{#427723}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/ui/PaymentRequestSection.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ui/PaymentRequestSection.java
@@ -604,8 +604,9 @@ public abstract class PaymentRequestSection extends LinearLayout implements View
                 LineItem item = cart.getContents().get(i);
 
                 TextView description = new TextView(context);
-                ApiCompatibilityUtils.setTextAppearance(
-                        description, R.style.PaymentsUiSectionDescriptiveTextEndAligned);
+                ApiCompatibilityUtils.setTextAppearance(description, item.getIsPending()
+                                ? R.style.PaymentsUiSectionPendingTextEndAligned
+                                : R.style.PaymentsUiSectionDescriptiveTextEndAligned);
                 description.setText(item.getLabel());
                 description.setEllipsize(TruncateAt.END);
                 description.setMaxLines(2);
@@ -614,8 +615,9 @@ public abstract class PaymentRequestSection extends LinearLayout implements View
                 }
 
                 TextView amount = new TextView(context);
-                ApiCompatibilityUtils.setTextAppearance(
-                        amount, R.style.PaymentsUiSectionDescriptiveTextEndAligned);
+                ApiCompatibilityUtils.setTextAppearance(amount, item.getIsPending()
+                                ? R.style.PaymentsUiSectionPendingTextEndAligned
+                                : R.style.PaymentsUiSectionDescriptiveTextEndAligned);
                 amount.setText(createValueString(item.getCurrency(), item.getPrice(), false));
 
                 // Each item is represented by a row in the GridLayout.
