05f8f48ac0776bd06a7759cf61c8f13fe9132ac2
sdefresne <sdefresne@chromium.org>
2016-10-26 08:25:46 -0700

    Remove global getter/setter for SigninErrorProvider.
    BUG=475439
    
    Review-Url: https://codereview.chromium.org/2438603004
    Cr-Commit-Position: refs/heads/master@{#427696}

--- ios/public/provider/chrome/browser/signin/signin_error_provider.mm
+++ ios/public/provider/chrome/browser/signin/signin_error_provider.mm
@@ -7,18 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ios {
 
-namespace {
-SigninErrorProvider* g_signin_error_provider = nullptr;
-}
-
-void SetSigninErrorProvider(SigninErrorProvider* provider) {
-  g_signin_error_provider = provider;
-}
-
-SigninErrorProvider* GetSigninErrorProvider() {
-  return g_signin_error_provider;
-}
-
 SigninErrorProvider::SigninErrorProvider() {}
 
 SigninErrorProvider::~SigninErrorProvider() {}
