276bfa4e188d0c9335ede02063f4d29c4228e82a
michaelpg <michaelpg@chromium.org>
2016-10-26 14:13:44 -0700

    settings-dropdown-menu must set <select>#value after its options change
    The |value| property of an HTMLSelectElement is a getter/setter, not a POD
    variable. Setting it performs an algorithm which is something like:
    
      1. Let O be the first <option> in my children whose "property" attribute
         equals |value|.
      2. If O exists, add the "selected" attribute to O.
    
    As a result, a vanilla Polymer binding is not safe if the contents are
    dynamically generated:
    
      <select value="[[myValue]]">
        <template is="dom-repeat" items="[[myItems]]">
          <option value="[[item.value]]">[[item.name]]</option>
        </template>
      </select>
    
    because if |myValue| already has a particular value, and then |myItems| is
    set, |myValue| has the correct value but the <select>'s |value| "setter"
    is not called. Thus the option is never actually selected.
    
    Fix this by setting |value| directly instead of in a data binding.
    
    Also, ensure "Custom" can't be selected when hidden via type-ahead.
    
    BUG=658075
    R=dpapad@chromium.org
    TEST=CrSettingsDropdownMenu browser test
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2446003002
    Cr-Commit-Position: refs/heads/master@{#427792}

--- chrome/test/data/webui/settings/dropdown_menu_tests.js
+++ chrome/test/data/webui/settings/dropdown_menu_tests.js
@@ -16,6 +16,12 @@ cr.define('settings_dropdown_menu', function() {
        */
       var selectElement;
 
+      /**
+       * The "Custom" option in the <select> menu.
+       * @type {HTMLOptionElement}
+       */
+      var customOption;
+
       function waitUntilDropdownUpdated() {
         return new Promise(function(resolve) { dropdown.async(resolve); });
       }
@@ -29,7 +35,9 @@ cr.define('settings_dropdown_menu', function() {
       setup(function() {
         PolymerTest.clearBody();
         dropdown = document.createElement('settings-dropdown-menu');
-        selectElement = dropdown.$$('select');
+        selectElement = assert(dropdown.$$('select'));
+        var options = selectElement.options;
+        customOption = assert(options[options.length - 1]);
         document.body.appendChild(dropdown);
       });
 
@@ -85,11 +93,13 @@ cr.define('settings_dropdown_menu', function() {
         }).then(function() {
           assertEquals('a', dropdown.pref.value);
 
-          // Updating the pref selects an item.
-          dropdown.set('pref.value', 'b');
+          // Item remains selected after updating menu items.
+          var newMenuOptions = dropdown.menuOptions.slice().reverse();
+          dropdown.menuOptions = newMenuOptions;
           return waitUntilDropdownUpdated();
         }).then(function() {
-          assertEquals('b', selectElement.value);
+          assertEquals('AAA',
+              selectElement.selectedOptions[0].textContent.trim());
         });
       });
 
@@ -108,6 +118,9 @@ cr.define('settings_dropdown_menu', function() {
         return waitUntilDropdownUpdated().then(function() {
           // "Custom" initially selected.
           assertEquals(dropdown.notFoundValue_, selectElement.value);
+          assertEquals('block', getComputedStyle(customOption).display);
+          assertFalse(customOption.disabled);
+
           // Pref should not have changed.
           assertEquals('f', dropdown.pref.value);
         });
@@ -124,11 +137,10 @@ cr.define('settings_dropdown_menu', function() {
           value: 200,
         };
 
-        console.log('running test');
         return waitForTimeout(100).then(function() {
           return waitUntilDropdownUpdated();
         }).then(function() {
-          assertTrue(dropdown.$.dropdownMenu.disabled);
+          assertTrue(selectElement.disabled);
           assertEquals('SETTINGS_DROPDOWN_NOT_FOUND_ITEM', selectElement.value);
 
           dropdown.menuOptions = [{value: 100, name: 'Option 100'},
@@ -139,8 +151,13 @@ cr.define('settings_dropdown_menu', function() {
         }).then(function() {
           // Dropdown menu enables itself and selects the new menu option
           // correpsonding to the pref value.
-          assertFalse(dropdown.$.dropdownMenu.disabled);
+          assertFalse(selectElement.disabled);
           assertEquals('200', selectElement.value);
+
+          // The "Custom" option should not show up in the dropdown list or be
+          // reachable via type-ahead.
+          assertEquals('none', getComputedStyle(customOption).display);
+          assertTrue(customOption.disabled);
         });
       });
     });
