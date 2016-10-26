a8aa21f52ba59a703bf82676aafbce1083e8c15f
jdoerrie <jdoerrie@chromium.org>
2016-10-26 03:02:35 -0700

    Fix Password Manager Performance Issues
    This change further addresses the performance issue of password manager when
    many passwords are saved. Prior to this change a long password list (~1000
    entries) can freeze the password manager for multiple seconds.
    
    Inspired by the edit dictionary settings (chrome://settings/editDictionary),
    this change removes the |autoExpands| attributes from the saved passwords and
    password exceptions lists and adds a CSS |max-height| attribute. This results in
    rendering the list almost instantaneously and keeps it responsive with regard to
    user interactions.
    
    BUG=651049
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2443143002
    Cr-Commit-Position: refs/heads/master@{#427655}

--- chrome/browser/resources/options/password_manager.js
+++ chrome/browser/resources/options/password_manager.js
@@ -110,7 +110,6 @@ cr.define('options', function() {
       options.passwordManager.PasswordsList.decorate(savedPasswordsList);
       this.savedPasswordsList_ = assertInstanceof(savedPasswordsList,
           options.DeletableItemList);
-      this.savedPasswordsList_.autoExpands = true;
     },
 
     /**
@@ -123,7 +122,6 @@ cr.define('options', function() {
           passwordExceptionsList);
       this.passwordExceptionsList_ = assertInstanceof(passwordExceptionsList,
           options.DeletableItemList);
-      this.passwordExceptionsList_.autoExpands = true;
     },
 
     /**
