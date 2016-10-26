1aafbb451df34eb7af43e5f7f64dace70f7f22d3
mkwst <mkwst@chromium.org>
2016-10-26 01:23:02 -0700

    CSP: 'violated-directive' should match 'effective-directive'.
    In [1], mikispag@ noted that we're currently returning the entire
    text of the violated directive, including its source list, etc. This
    information is replicated in the report via the 'original-policy'
    property, and CSP3 defines both 'violated-directive' and
    'effective-directive' to contain the same data.
    
    This patch changes our implementation in line with the spec.
    
    [1]: https://github.com/w3c/web-platform-tests/pull/4020
    
    R=jochen@chromium.org
    
    Review-Url: https://codereview.chromium.org/2447003002
    Cr-Commit-Position: refs/heads/master@{#427634}

--- third_party/WebKit/Source/core/frame/csp/ContentSecurityPolicy.cpp
+++ third_party/WebKit/Source/core/frame/csp/ContentSecurityPolicy.cpp
@@ -1087,7 +1087,7 @@ static void gatherSecurityPolicyViolationEventData(
     }
   }
   init.setReferrer(document->referrer());
-  init.setViolatedDirective(directiveText);
+  init.setViolatedDirective(effectiveDirective);
   init.setEffectiveDirective(effectiveDirective);
   init.setOriginalPolicy(header);
   init.setDisposition(headerType == ContentSecurityPolicyHeaderTypeEnforce
