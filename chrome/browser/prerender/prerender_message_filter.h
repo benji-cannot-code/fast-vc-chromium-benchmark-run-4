6e49d32a34068007e62711d508191b8f306cc56d
pasko <pasko@chromium.org>
2016-10-26 06:47:46 -0700

    [NoStatePrefetch] Kill renderer after preload scanning
    The design doc is linked in the first bug. Main goal: remove Prerender.
    
    Currently NoState Prefetch uses the same logic to manage renderers as the
    PrerenderManager. Prefetch code is still living in PrerenderManager, separating
    the code from there is planned after we finalize on how much of the
    functionality of PrerenderManager is needed.
    
    This is the first tweak in the renderer lifetime: the renderer asks to be killed
    as soon as the main resource is fully preload-scanned and all possible
    subresources are requested, special Prerendering FinalStatus is recorded to allow:
    * testing
    * provide a hint that a new prefetch can be started soon
    
    Browsertest changes:
    
    * TestPrerender now keeps FinalStatus for longer, to be able to verify that it
      is correct even after the TestPrerenderContents is destroyed.
    
    * Prefetch browsertests wait only for creation of PrerenderContents, Prerender
      tests have more waiting for page loads on top of that
    
    * PrerenderTestUrlImpl is removed to eliminate one hop through protected method
      in the parent class, which also simplified the prefetch side of the tests: no
      need to mention how many page loads to wait for
    
    * Disabled two tests that load a non-HTML document, correctly killing the
      renderer will be done in later changes.
    
    BUG=632361,649632
    
    Review-Url: https://codereview.chromium.org/2411863002
    Cr-Commit-Position: refs/heads/master@{#427678}

--- chrome/browser/prerender/prerender_message_filter.h
+++ chrome/browser/prerender/prerender_message_filter.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_PRERENDER_PRERENDER_MESSAGE_FILTER_H_
 #define CHROME_BROWSER_PRERENDER_PRERENDER_MESSAGE_FILTER_H_
 
+#include <memory>
+
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "components/keyed_service/core/keyed_service_shutdown_notifier.h"
@@ -30,6 +32,7 @@ class Message;
 namespace prerender {
 
 class PrerenderLinkManager;
+class PrerenderManager;
 
 class PrerenderMessageFilter : public content::BrowserMessageFilter {
  public:
@@ -58,10 +61,13 @@ class PrerenderMessageFilter : public content::BrowserMessageFilter {
                       int render_view_route_id);
   void OnCancelPrerender(int prerender_id);
   void OnAbandonPrerender(int prerender_id);
+  void OnPrefetchFinished();
 
   void ShutdownOnUIThread();
 
   void OnChannelClosingInUIThread();
+
+  PrerenderManager* prerender_manager_;
 
   const int render_process_id_;
 
