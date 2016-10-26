cda6a5d1a56d1173f90a6e4c076f2234f1af7ab9
zhongyi <zhongyi@chromium.org>
2016-10-26 15:36:23 -0700

    Add checks in QuicSession::GetNumOpenOutgoingStreams() to prevent overflow.
    BUG=
    
    Review-Url: https://codereview.chromium.org/2452163002
    Cr-Commit-Position: refs/heads/master@{#427854}

--- net/quic/core/quic_session.cc
+++ net/quic/core/quic_session.cc
@@ -738,8 +738,12 @@ size_t QuicSession::GetNumOpenIncomingStreams() const {
 }
 
 size_t QuicSession::GetNumOpenOutgoingStreams() const {
-  return GetNumDynamicOutgoingStreams() - GetNumDrainingOutgoingStreams() +
-         GetNumLocallyClosedOutgoingStreamsHighestOffset();
+  CHECK_GE(GetNumDynamicOutgoingStreams() +
+               GetNumLocallyClosedOutgoingStreamsHighestOffset(),
+           GetNumDrainingOutgoingStreams());
+  return GetNumDynamicOutgoingStreams() +
+         GetNumLocallyClosedOutgoingStreamsHighestOffset() -
+         GetNumDrainingOutgoingStreams();
 }
 
 size_t QuicSession::GetNumActiveStreams() const {
