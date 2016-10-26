e1582c424d0de0a3930cfddf06a05dacad07b1df
dtseng <dtseng@chromium.org>
2016-10-25 17:51:31 -0700

    Recovery: Implement tab recovery
    This cl provides uninvasive tab recovery.
    
    When setting focus, first, check to see if either endpoint of the range is already focused. If not, attempt to focus each in turn. Next, attempt to find the previous focusable node from the start of the range and give that node focus.
    
    BUG=
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2405263002
    Cr-Commit-Position: refs/heads/master@{#427554}

--- chrome/browser/resources/chromeos/chromevox/cvox2/background/desktop_automation_handler.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/background/desktop_automation_handler.js
@@ -110,6 +110,10 @@ DesktopAutomationHandler.prototype = {
     if (!node)
       return;
 
+    // Decide whether to announce and sync this event.
+    if (!DesktopAutomationHandler.announceActions && evt.eventFrom == 'action')
+      return;
+
     var prevRange = ChromeVoxState.instance.currentRange;
 
     ChromeVoxState.instance.setCurrentRange(cursors.Range.fromNode(node));
@@ -121,10 +125,6 @@ DesktopAutomationHandler.prototype = {
     if (prevRange &&
         evt.type == 'focus' &&
         ChromeVoxState.instance.currentRange.equals(prevRange))
-      return;
-
-    // Decide whether to announce this event.
-    if (!DesktopAutomationHandler.announceActions && evt.eventFrom == 'action')
       return;
 
     var output = new Output();
