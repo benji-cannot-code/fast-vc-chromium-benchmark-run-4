dfc85bfc66704e67564582ada66c0f5c9c0af7ce
luoe <luoe@chromium.org>
2016-10-25 17:50:26 -0700

    DevTools: hoist debugger paused reason to top
    This interface change moves the paused reason (e.g. paused on breakpoint, paused
    on subtree modification, etc) message to the top of the sources panel.  See bug
    for screenshots.
    
    BUG=572068
    
    Review-Url: https://codereview.chromium.org/2389883003
    Cr-Commit-Position: refs/heads/master@{#427553}

--- third_party/WebKit/Source/devtools/front_end/sources/SourcesPanel.js
+++ third_party/WebKit/Source/devtools/front_end/sources/SourcesPanel.js
@@ -51,6 +51,7 @@ WebInspector.SourcesPanel = function()
 
     this._debugToolbar = this._createDebugToolbar();
     this._debugToolbarDrawer = this._createDebugToolbarDrawer();
+    this._debuggerPausedMessage = new WebInspector.DebuggerPausedMessage();
 
     const initialDebugSidebarWidth = 225;
     this._splitWidget = new WebInspector.SplitWidget(true, true, "sourcesPanelSplitViewState", initialDebugSidebarWidth);
@@ -94,7 +95,7 @@ WebInspector.SourcesPanel = function()
     WebInspector.moduleSetting("sidebarPosition").addChangeListener(this._updateSidebarPosition.bind(this));
     this._updateSidebarPosition();
 
-    this._updateDebuggerButtons();
+    this._updateDebuggerButtonsAndStatus();
     this._pauseOnExceptionEnabledChanged();
     WebInspector.moduleSetting("pauseOnExceptionEnabled").addChangeListener(this._pauseOnExceptionEnabledChanged, this);
 
@@ -263,7 +264,7 @@ WebInspector.SourcesPanel.prototype = {
     _showDebuggerPausedDetails: function(details)
     {
         this._paused = true;
-        this._updateDebuggerButtons();
+        this._updateDebuggerButtonsAndStatus();
         WebInspector.context.setFlavor(WebInspector.DebuggerPausedDetails, details);
         this._toggleDebuggerSidebarButton.setEnabled(false);
         window.focus();
@@ -294,7 +295,7 @@ WebInspector.SourcesPanel.prototype = {
         if (WebInspector.context.flavor(WebInspector.Target) !== target)
             return;
 
-        this._updateDebuggerButtons();
+        this._updateDebuggerButtonsAndStatus();
     },
 
     /**
@@ -447,7 +448,7 @@ WebInspector.SourcesPanel.prototype = {
         this._debugToolbarDrawer.classList.toggle("expanded", enabled);
     },
 
-    _updateDebuggerButtons: function()
+    _updateDebuggerButtonsAndStatus: function()
     {
         var currentTarget = WebInspector.context.flavor(WebInspector.Target);
         var currentDebuggerModel = WebInspector.DebuggerModel.fromTarget(currentTarget);
@@ -469,12 +470,15 @@ WebInspector.SourcesPanel.prototype = {
             this._stepIntoAction.setEnabled(false);
             this._stepOutAction.setEnabled(false);
         }
+
+        var details = currentDebuggerModel ? currentDebuggerModel.debuggerPausedDetails() : null;
+        this._debuggerPausedMessage.render(details, WebInspector.debuggerWorkspaceBinding, WebInspector.breakpointManager);
     },
 
     _clearInterface: function()
     {
         this._sourcesView.clearCurrentExecutionLine();
-        this._updateDebuggerButtons();
+        this._updateDebuggerButtonsAndStatus();
         WebInspector.context.setFlavor(WebInspector.DebuggerPausedDetails, null);
 
         if (this._switchToPausedTargetTimeout)
@@ -1059,6 +1063,7 @@ WebInspector.SourcesPanel.prototype = {
         this._sidebarPaneStack = WebInspector.viewManager.createStackLocation(this._revealDebuggerSidebar.bind(this));
         this._sidebarPaneStack.widget().element.classList.add("overflow-auto");
         this._sidebarPaneStack.widget().show(vbox.element);
+        this._sidebarPaneStack.widget().element.appendChild(this._debuggerPausedMessage.element());
         vbox.element.appendChild(this._debugToolbar.element);
 
         if (this._threadsSidebarPane)
