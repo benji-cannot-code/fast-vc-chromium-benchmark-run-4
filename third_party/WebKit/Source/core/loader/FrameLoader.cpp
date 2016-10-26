7afd0dcd397dae16b08d1eb1ee9902d8d700bd33
csharrison <csharrison@chromium.org>
2016-10-26 06:27:20 -0700

    Defer SecurityOrigin creation for requests that don't need an Origin header
    In many cases, we create a SecurityOrigin from a string, only to
    immediately discard the results because most requests don't need http
    origin headers (i.e. GET requests).
    
    This ends up being rather expensive per-request and also per frame load.
    This CL splits the method into two to allow for origin computation from
    a string only if the request actually needs the header.
    
    The computation in addAdditionalRequestHeaders accounts for ~7% of
    per-request CPU time in cr.kungfoo.net/many_resources/link_preloads.html
    even though we won't generally need the header for subresource requests.
    
    BUG=348655
    
    Review-Url: https://codereview.chromium.org/2439403002
    Cr-Commit-Position: refs/heads/master@{#427674}

--- third_party/WebKit/Source/core/loader/FrameLoader.cpp
+++ third_party/WebKit/Source/core/loader/FrameLoader.cpp
@@ -141,9 +141,7 @@ ResourceRequest FrameLoader::resourceRequestFromHistoryItem(
     request.setHTTPMethod(HTTPNames::POST);
     request.setHTTPBody(formData);
     request.setHTTPContentType(item->formContentType());
-    RefPtr<SecurityOrigin> securityOrigin =
-        SecurityOrigin::createFromString(item->referrer().referrer);
-    request.addHTTPOriginIfNeeded(securityOrigin.get());
+    request.addHTTPOriginIfNeeded(item->referrer().referrer);
   }
   return request;
 }
@@ -890,9 +888,7 @@ void FrameLoader::setReferrerForFrameRequest(FrameLoadRequest& frameRequest) {
       originDocument->outgoingReferrer());
 
   request.setHTTPReferrer(referrer);
-  RefPtr<SecurityOrigin> referrerOrigin =
-      SecurityOrigin::createFromString(referrer.referrer);
-  request.addHTTPOriginIfNeeded(referrerOrigin.get());
+  request.addHTTPOriginIfNeeded(referrer.referrer);
 }
 
 FrameLoadType FrameLoader::determineFrameLoadType(
