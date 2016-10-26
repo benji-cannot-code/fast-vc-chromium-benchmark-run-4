c189af5185a4a6b895b6ef25d1e12873538e58fd
gab <gab@chromium.org>
2016-10-26 11:18:16 -0700

    [TaskScheduler] Temporarily co-init all TaskScheduler threads to match SequencedWorkerPool's behaviour.
    BUG=622400
    
    Review-Url: https://codereview.chromium.org/2435013006
    Cr-Commit-Position: refs/heads/master@{#427748}

--- base/task_scheduler/scheduler_worker.cc
+++ base/task_scheduler/scheduler_worker.cc
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_MACOSX)
 #include "base/mac/scoped_nsautorelease_pool.h"
+#elif defined(OS_WIN)
+#include "base/win/scoped_com_initializer.h"
 #endif
 
 namespace base {
@@ -41,6 +43,14 @@ class SchedulerWorker::Thread : public PlatformThread::Delegate {
 
     // A SchedulerWorker starts out waiting for work.
     WaitForWork();
+
+#if defined(OS_WIN)
+    // This is required as SequencedWorkerPool previously blindly CoInitialized
+    // all of its threads.
+    // TODO: Get rid of this broad COM scope and force tasks that care about a
+    // CoInitialized environment to request one (via an upcoming ExecutionMode).
+    win::ScopedCOMInitializer com_initializer;
+#endif
 
     while (!outer_->task_tracker_->IsShutdownComplete() &&
            !outer_->should_exit_for_testing_.IsSet()) {
