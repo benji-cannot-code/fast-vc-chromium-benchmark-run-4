d4442e2f88959f6d2b4dd4729f99c5fd25214cd9
trchen <trchen@chromium.org>
2016-10-26 18:11:01 -0700

    [SPv2] Remove PaintArtifactToSkCanvas
    This is a legacy of the past. Now PaintArtifactCompositor implements its own
    cc::ContentLayerClient directly and converts paint chunks to cc display list
    in there.
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2447823003
    Cr-Commit-Position: refs/heads/master@{#427900}

--- third_party/WebKit/Source/platform/graphics/ContentLayerDelegate.cpp
+++ third_party/WebKit/Source/platform/graphics/ContentLayerDelegate.cpp
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/geometry/IntRect.h"
 #include "platform/graphics/GraphicsContext.h"
 #include "platform/graphics/GraphicsLayer.h"
-#include "platform/graphics/paint/PaintArtifactToSkCanvas.h"
 #include "platform/graphics/paint/PaintController.h"
 #include "platform/tracing/TraceEvent.h"
 #include "platform/tracing/TracedValue.h"
@@ -45,24 +44,6 @@ ContentLayerDelegate::ContentLayerDelegate(GraphicsLayer* graphicsLayer)
     : m_graphicsLayer(graphicsLayer) {}
 
 ContentLayerDelegate::~ContentLayerDelegate() {}
-
-static void paintArtifactToWebDisplayItemList(WebDisplayItemList* list,
-                                              const PaintArtifact& artifact,
-                                              const gfx::Rect& bounds) {
-  if (RuntimeEnabledFeatures::slimmingPaintV2Enabled()) {
-    // This is a temporary path to paint the artifact using the paint chunk
-    // properties. Ultimately, we should instead split the artifact into
-    // separate layers and send those to the compositor, instead of sending
-    // one big flat SkPicture.
-    SkRect skBounds = SkRect::MakeXYWH(bounds.x(), bounds.y(), bounds.width(),
-                                       bounds.height());
-    list->appendDrawingItem(
-        WebRect(bounds.x(), bounds.y(), bounds.width(), bounds.height()),
-        paintArtifactToSkPicture(artifact, skBounds));
-    return;
-  }
-  artifact.appendToWebDisplayItemList(list);
-}
 
 gfx::Rect ContentLayerDelegate::paintableRegion() {
   IntRect interestRect = m_graphicsLayer->interestRect();
@@ -105,8 +86,8 @@ void ContentLayerDelegate::paintContents(
   if (paintingControl != PaintDefaultBehavior)
     m_graphicsLayer->paint(nullptr, disabledMode);
 
-  paintArtifactToWebDisplayItemList(
-      webDisplayItemList, paintController.paintArtifact(), paintableRegion());
+  paintController.paintArtifact().appendToWebDisplayItemList(
+      webDisplayItemList);
 
   paintController.setDisplayItemConstructionIsDisabled(false);
   paintController.setSubsequenceCachingIsDisabled(false);
