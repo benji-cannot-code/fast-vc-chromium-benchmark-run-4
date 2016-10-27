6103128cfb75e8803ba4690a918ed40a2192e023
rohitrao <rohitrao@chromium.org>
2016-10-27 06:38:38 -0700

    [ios] Adds protocols for voice search.
    BUG=656617
    
    Review-Url: https://codereview.chromium.org/2459533002
    Cr-Commit-Position: refs/heads/master@{#428012}

---
+++ ios/public/provider/chrome/browser/voice/voice_search_bar_owner.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_PUBLIC_PROVIDER_CHROME_BROWSER_VOICE_VOICE_SEARCH_BAR_OWNER_H_
+#define IOS_PUBLIC_PROVIDER_CHROME_BROWSER_VOICE_VOICE_SEARCH_BAR_OWNER_H_
+
+#import <Foundation/Foundation.h>
+
+@protocol VoiceSearchBar;
+
+// Protocol implemented by UIViewControllers that own a VoiceSearchBar.
+@protocol VoiceSearchBarOwner<NSObject>
+
+// The VoiceSearchBar.
+@property(nonatomic, readonly) id<VoiceSearchBar> voiceSearchBar;
+
+@end
+
+#endif  // IOS_PUBLIC_PROVIDER_CHROME_BROWSER_VOICE_VOICE_SEARCH_BAR_OWNER_H_
