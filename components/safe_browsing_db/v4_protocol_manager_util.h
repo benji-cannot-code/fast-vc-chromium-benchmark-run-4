2b1e3b52c63bdb1b7a6a9ca9c345a0d70c29594f
vakh <vakh@chromium.org>
2016-10-26 18:24:44 -0700

    Small: Implement CheckDownloadUrls -- not called yet.
    The structure is very similar to CheckBrowseUrl so it should be an easy
    review.
    
    BUG=543161
    
    Review-Url: https://codereview.chromium.org/2450633003
    Cr-Commit-Position: refs/heads/master@{#427905}

--- components/safe_browsing_db/v4_protocol_manager_util.h
+++ components/safe_browsing_db/v4_protocol_manager_util.h
@@ -133,6 +133,7 @@ std::ostream& operator<<(std::ostream& os, const ListIdentifier& id);
 PlatformType GetCurrentPlatformType();
 const ListIdentifier GetChromeUrlApiId();
 const ListIdentifier GetUrlMalwareId();
+const ListIdentifier GetUrlMalBinId();
 const ListIdentifier GetUrlSocEngId();
 const ListIdentifier GetUrlUwsId();
 
