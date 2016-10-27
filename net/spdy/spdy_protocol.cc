8e0a74f7fcfd23019515b716b7016da58c3f97af
bnc <bnc@chromium.org>
2016-10-26 21:45:42 -0700

    Add HTTP/2 error code NO_ERROR.
    Add HTTP/2 error code NO_ERROR defined by RFC7540 Section 7.
    
    This CL lands server change 137070429 by bnc.
    
    BUG=606990, 488484
    
    Review-Url: https://codereview.chromium.org/2445113002
    Cr-Commit-Position: refs/heads/master@{#427942}

--- net/spdy/spdy_protocol.cc
+++ net/spdy/spdy_protocol.cc
@@ -391,7 +391,7 @@ bool SpdyConstants::IsValidRstStreamStatus(SpdyMajorVersion version,
     case HTTP2:
       // NO_ERROR is the first valid status code.
       if (rst_stream_status_field <
-          SerializeRstStreamStatus(version, RST_STREAM_PROTOCOL_ERROR)) {
+          SerializeRstStreamStatus(version, RST_STREAM_NO_ERROR)) {
         return false;
       }
 
@@ -448,6 +448,8 @@ SpdyRstStreamStatus SpdyConstants::ParseRstStreamStatus(
       break;
     case HTTP2:
       switch (rst_stream_status_field) {
+        case 0:
+          return RST_STREAM_NO_ERROR;
         case 1:
           return RST_STREAM_PROTOCOL_ERROR;
         case 2:
@@ -510,6 +512,8 @@ int SpdyConstants::SerializeRstStreamStatus(
       }
     case HTTP2:
       switch (rst_stream_status) {
+        case RST_STREAM_NO_ERROR:
+          return 0;
         case RST_STREAM_PROTOCOL_ERROR:
           return 1;
         case RST_STREAM_INTERNAL_ERROR:
