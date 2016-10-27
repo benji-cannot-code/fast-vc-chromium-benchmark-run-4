0b65e2a29d935321ac5e49f6a2c8c27afc1302a6
siva.gunturi <siva.gunturi@samsung.com>
2016-10-27 10:13:38 -0700

    RemoveSurfaceTextureAndProxy is not used.
    RemoveSurfaceTextureAndProxy is not used anywhere.
    It can be removed.
    
    BUG=None.
    
    Review-Url: https://codereview.chromium.org/2460483002
    Cr-Commit-Position: refs/heads/master@{#428065}

--- content/renderer/media/android/webmediaplayer_android.h
+++ content/renderer/media/android/webmediaplayer_android.h
@@ -246,7 +246,6 @@ class WebMediaPlayerAndroid
   void DrawRemotePlaybackText(const std::string& remote_playback_message);
   void ReallocateVideoFrame();
   void SetCurrentFrameInternal(scoped_refptr<media::VideoFrame>& frame);
-  void RemoveSurfaceTextureAndProxy();
   void DidLoadMediaInfo(MediaInfoLoader::Status status,
                         const GURL& redirected_url,
                         const GURL& first_party_for_cookies,
