467c49fa5c759fe55f98b1cb49c0eeb0b77a0d01
avi <avi@chromium.org>
2016-10-27 15:20:11 -0700

    Remove stl_util's deletion function use from google_apis/.
    BUG=555865
    
    Review-Url: https://codereview.chromium.org/2447333002
    Cr-Commit-Position: refs/heads/master@{#428165}

--- google_apis/gaia/account_tracker.cc
+++ google_apis/gaia/account_tracker.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/gaia/account_tracker.h"
 
 #include "base/logging.h"
+#include "base/memory/ptr_util.h"
 #include "base/profiler/scoped_tracker.h"
 #include "base/stl_util.h"
 #include "base/trace_event/trace_event.h"
@@ -29,7 +30,7 @@ AccountTracker::~AccountTracker() {
 
 void AccountTracker::Shutdown() {
   shutdown_called_ = true;
-  base::STLDeleteValues(&user_info_requests_);
+  user_info_requests_.clear();
   identity_provider_->GetTokenService()->RemoveObserver(this);
   identity_provider_->RemoveObserver(this);
 }
@@ -227,7 +228,7 @@ void AccountTracker::StopTrackingAccount(const std::string account_key) {
   }
 
   if (base::ContainsKey(user_info_requests_, account_key))
-    DeleteFetcher(user_info_requests_[account_key]);
+    DeleteFetcher(user_info_requests_[account_key].get());
 }
 
 void AccountTracker::StopTrackingAllAccounts() {
@@ -249,7 +250,7 @@ void AccountTracker::StartFetchingUserInfo(const std::string& account_key) {
         FROM_HERE_WITH_EXPLICIT_FUNCTION(
             "422460 AccountTracker::StartFetchingUserInfo 1"));
 
-    DeleteFetcher(user_info_requests_[account_key]);
+    DeleteFetcher(user_info_requests_[account_key].get());
   }
 
   DVLOG(1) << "StartFetching " << account_key;
@@ -264,7 +265,7 @@ void AccountTracker::StartFetchingUserInfo(const std::string& account_key) {
                            request_context_getter_.get(),
                            this,
                            account_key);
-  user_info_requests_[account_key] = fetcher;
+  user_info_requests_[account_key] = base::WrapUnique(fetcher);
 
   // TODO(robliao): Remove ScopedTracker below once https://crbug.com/422460 is
   // fixed.
@@ -301,9 +302,8 @@ void AccountTracker::DeleteFetcher(AccountIdFetcher* fetcher) {
   DVLOG(1) << "DeleteFetcher " << fetcher->account_key();
   const std::string& account_key = fetcher->account_key();
   DCHECK(base::ContainsKey(user_info_requests_, account_key));
-  DCHECK_EQ(fetcher, user_info_requests_[account_key]);
+  DCHECK_EQ(fetcher, user_info_requests_[account_key].get());
   user_info_requests_.erase(account_key);
-  delete fetcher;
 }
 
 AccountIdFetcher::AccountIdFetcher(
