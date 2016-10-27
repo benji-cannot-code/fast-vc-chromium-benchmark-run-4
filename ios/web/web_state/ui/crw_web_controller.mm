7c52743c252b32fdeaaa9a13f11706dd32b1611a
eugenebut <eugenebut@chromium.org>
2016-10-26 17:18:57 -0700

    [ios] Do not trigger context menu while scrolling.
    Fail _contextMenuRecognizer if webview's scroll view
    panGestureRecognizer was recognized.
    
    BUG=642375
    
    Review-Url: https://codereview.chromium.org/2452973003
    Cr-Commit-Position: refs/heads/master@{#427888}

--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -4595,6 +4595,8 @@ const NSTimeInterval kSnapshotOverlayTransition = 0.5;
     _contextMenuRecognizer.reset([[UILongPressGestureRecognizer alloc]
         initWithTarget:self
                 action:@selector(showContextMenu:)]);
+    [_contextMenuRecognizer
+        requireGestureRecognizerToFail:self.webScrollView.panGestureRecognizer];
     [_contextMenuRecognizer setMinimumPressDuration:kLongPressDurationSeconds];
     [_contextMenuRecognizer setAllowableMovement:kLongPressMoveDeltaPixels];
     [_contextMenuRecognizer setDelegate:self];
