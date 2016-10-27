6a581704223a7de1bc709dc1ebd7c922b7e048a3
dbeam <dbeam@chromium.org>
2016-10-26 21:46:53 -0700

    MD Downloads: show "No results found" even with no items
    R=dpapad@chromium.org
    BUG=none
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2449963006
    Cr-Commit-Position: refs/heads/master@{#427943}

--- chrome/browser/resources/md_downloads/manager.js
+++ chrome/browser/resources/md_downloads/manager.js
@@ -8,22 +8,32 @@ cr.define('downloads', function() {
     is: 'downloads-manager',
 
     properties: {
+      /** @private */
       hasDownloads_: {
         observer: 'hasDownloadsChanged_',
         type: Boolean,
       },
 
+      /** @private */
       hasShadow_: {
         type: Boolean,
         value: false,
         reflectToAttribute: true,
       },
 
+      /** @private */
+      inSearchMode_: {
+        type: Boolean,
+        value: false,
+      },
+
+      /** @private {!Array<!downloads.Data>} */
       items_: {
         type: Array,
         value: function() { return []; },
       },
 
+      /** @private */
       spinnerActive_: {
         type: Boolean,
         notify: true,
@@ -36,6 +46,7 @@ cr.define('downloads', function() {
 
     listeners: {
       'downloads-list.scroll': 'onListScroll_',
+      'toolbar.search-changed': 'onSearchChanged_',
     },
 
     observers: [
@@ -52,14 +63,8 @@ cr.define('downloads', function() {
       if (loadTimeData.getBoolean('allowDeletingHistory'))
         this.$.toolbar.downloadsShowing = this.hasDownloads_;
 
-      if (this.hasDownloads_) {
+      if (this.hasDownloads_)
         this.$['downloads-list'].fire('iron-resize');
-      } else {
-        var isSearching = downloads.ActionService.getInstance().isSearching();
-        var messageToShow = isSearching ? 'noSearchResults' : 'noDownloads';
-        this.$['no-downloads'].querySelector('span').textContent =
-            loadTimeData.getString(messageToShow);
-      }
     },
 
     /**
@@ -77,6 +82,15 @@ cr.define('downloads', function() {
     /** @private */
     itemsChanged_: function() {
       this.hasDownloads_ = this.items_.length > 0;
+    },
+
+    /**
+     * @return {string} The text to show when no download items are showing.
+     * @private
+     */
+    noDownloadsText_: function() {
+      return loadTimeData.getString(
+          this.inSearchMode_ ? 'noSearchResults' : 'noDownloads');
     },
 
     /**
@@ -128,6 +142,11 @@ cr.define('downloads', function() {
       document.addEventListener('command', this.onCommand_.bind(this));
 
       downloads.ActionService.getInstance().loadMore();
+    },
+
+    /** @private */
+    onSearchChanged_: function() {
+      this.inSearchMode_ = downloads.ActionService.getInstance().isSearching();
     },
 
     /**
