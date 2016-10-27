af7717fb2ebecccdd79755041e2ae85c4a53a271
bruthig <bruthig@chromium.org>
2016-10-27 08:25:09 -0700

    Reland (attempt 3) of "Added common layout framework for system menu rows."
    Added common layout framework for system menu rows.
    
    Currently most of the system menu rows define their own layouts even
    though many rows should have the same layout.  Additionally we have to
    support two separate layout schemes while material design is being
    implemented.  Thus this CL lays the ground work to make it easier to
    support multiple layout schemes and a centralized layout source.
    
    Follow-up TODO:
    - wire in the new layouts to the system menu rows
    
    TBR=sky@chromium.org, tdanderson@chromium.org
    
    BUG=657669
    Previously landed here: https://codereview.chromium.org/2439093002
    Reverted here: https://codereview.chromium.org/2460443002
    
    Review-Url: https://codereview.chromium.org/2455983003
    Cr-Commit-Position: refs/heads/master@{#428033}

---
+++ ui/views/test/test_layout_manager.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/views/test/test_layout_manager.h"
+
+namespace views {
+namespace test {
+
+TestLayoutManager::TestLayoutManager() {}
+
+TestLayoutManager::~TestLayoutManager() {}
+
+void TestLayoutManager::Layout(View* host) {}
+
+gfx::Size TestLayoutManager::GetPreferredSize(const View* host) const {
+  return preferred_size_;
+}
+
+int TestLayoutManager::GetPreferredHeightForWidth(const View* host,
+                                                  int width) const {
+  return preferred_height_for_width_;
+}
+
+}  // namespace test
+}  // namespace views
