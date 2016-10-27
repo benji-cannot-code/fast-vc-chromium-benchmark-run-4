c10776fb69c9234103cc0f19a27b0d121b6c6a9d
dmurph <dmurph@chromium.org>
2016-10-27 11:43:36 -0700

    [IndexedDB] Add IDB Observer Tests
    Adding pipelined IDB Observer tests. The pipeline is:
    1. Init databases,
    2. Add observers & start observer tests,
    3. Perform database actions (in two stages).
    
    These are split up so they can be executed in workers
    or in the same frame. This does not include all cases
    where we want to throw an exception, that will be in
    a follow up patch.
    
    Current Implementation and Future Scope: https://goo.gl/Y2dobn
    
    Reference: https://github.com/WICG/indexed-db-observers/blob/gh-pages/EXPLAINER.md
    
    BUG=609934
    
    Review-Url: https://codereview.chromium.org/2449563002
    Cr-Commit-Position: refs/heads/master@{#428091}

--- third_party/WebKit/LayoutTests/storage/indexeddb/resources/observer.js
+++ third_party/WebKit/LayoutTests/storage/indexeddb/resources/observer.js
@@ -11,7 +11,7 @@ async_test(function(t) {
   var obs = new IDBObserver(t.step_func(function() { callback_count++; }), {operationTypes: ['put']});
 
   openRequest.onupgradeneeded = t.step_func(function() {
-      createDatabase(openRequest.result, ['store']);
+      openRequest.result.createObjectStore('store');
   });
   openRequest.onsuccess = t.step_func(function() {
     var db = openRequest.result;
@@ -37,7 +37,7 @@ async_test(function(t) {
   var callback_count = 0;
   var obs;
   openRequest.onupgradeneeded = t.step_func(function() {
-      createDatabase(openRequest.result, ['store']);
+      openRequest.result.createObjectStore('store');
       obs = new IDBObserver(t.step_func(function(changes) { callback_count++; }), { operationTypes: ['put'] });
   });
   openRequest.onsuccess = t.step_func(function() {
@@ -87,7 +87,7 @@ async_test(function(t) {
   var callback_count = 0;
   var obs = new IDBObserver(t.step_func(function() { callback_count++; }), { operationTypes: ['put'] });
   openRequest.onupgradeneeded = t.step_func(function() {
-    createDatabase(openRequest.result, ['store']);
+    openRequest.result.createObjectStore('store');
   });
   openRequest.onsuccess = t.step_func(function() {
     var db = openRequest.result;
@@ -116,7 +116,7 @@ async_test(function(t) {
   var callback_count = 0;
   var obs = new IDBObserver(t.step_func(function() { callback_count++; }), { operationTypes: ['put'] });
   openRequest.onupgradeneeded = t.step_func(function() {
-    createDatabase(openRequest.result, ['store']);
+    openRequest.result.createObjectStore('store');
   });
   openRequest.onsuccess = t.step_func(function() {
     var db = openRequest.result;
