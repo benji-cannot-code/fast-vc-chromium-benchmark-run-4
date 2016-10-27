09f2db4c89d3bdee624b7dccd3e6119ded29d920
wfh <wfh@chromium.org>
2016-10-27 15:50:44 -0700

    Control DefaultPluginsSetting policy by variations.
    BUG=658812
    TEST=launch chrome on machine with a plugins enterprise policy with --enable-features=IgnoreDefaultPluginsSetting and verify HBD code gets used.
    
    Review-Url: https://codereview.chromium.org/2442423002
    Cr-Commit-Position: refs/heads/master@{#428176}

--- components/content_settings/core/browser/content_settings_policy_provider.cc
+++ components/content_settings/core/browser/content_settings_policy_provider.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/bind.h"
+#include "base/feature_list.h"
 #include "base/json/json_reader.h"
 #include "base/macros.h"
 #include "base/values.h"
@@ -355,8 +356,14 @@ void PolicyProvider::UpdateManagedDefaultSetting(
   DCHECK(!prefs_->HasPrefPath(entry.pref_name) ||
          prefs_->IsManagedPreference(entry.pref_name));
   base::AutoLock auto_lock(lock_);
-
   int setting = prefs_->GetInteger(entry.pref_name);
+  // TODO(wfh): Remove once HDB is enabled by default.
+  if (entry.pref_name == prefs::kManagedDefaultPluginsSetting) {
+    static constexpr base::Feature kIgnoreDefaultPluginsSetting = {
+        "IgnoreDefaultPluginsSetting", base::FEATURE_DISABLED_BY_DEFAULT};
+    if (base::FeatureList::IsEnabled(kIgnoreDefaultPluginsSetting))
+      setting = CONTENT_SETTING_DEFAULT;
+  }
   if (setting == CONTENT_SETTING_DEFAULT) {
     value_map_.DeleteValue(ContentSettingsPattern::Wildcard(),
                            ContentSettingsPattern::Wildcard(),
