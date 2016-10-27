80f1a58a6430a0fe58f5f84e1639b5160358e9ae
pasko <pasko@chromium.org>
2016-10-27 08:01:15 -0700

    Reland: [NoStatePrefetch] Kill renderer after preload scanning
    Remove the inherently flaky test
    NoStatePrefetchBrowserTest.OpenTaskManagerBeforePrefetch and revert the
    following revert:
    
    > Reason for revert:
    > "NoStatePrefetchBrowserTest.OpenTaskManagerBeforePrefetch" became flaky
    > shortly after this landed.
    >
    > BUG=659697
    > Original issue's description:
    > > [NoStatePrefetch] Kill renderer after preload scanning
    > >
    > > The design doc is linked in the first bug. Main goal: remove Prerender.
    > >
    > > Currently NoState Prefetch uses the same logic to manage renderers as the
    > > PrerenderManager. Prefetch code is still living in PrerenderManager, separating
    > > the code from there is planned after we finalize on how much of the
    > > functionality of PrerenderManager is needed.
    > >
    > > This is the first tweak in the renderer lifetime: the renderer asks to be killed
    > > as soon as the main resource is fully preload-scanned and all possible
    > > subresources are requested, special Prerendering FinalStatus is recorded to allow:
    > > * testing
    > > * provide a hint that a new prefetch can be started soon
    > >
    > > Browsertest changes:
    > >
    > > * TestPrerender now keeps FinalStatus for longer, to be able to verify that it
    > >   is correct even after the TestPrerenderContents is destroyed.
    > >
    > > * Prefetch browsertests wait only for creation of PrerenderContents, Prerender
    > >   tests have more waiting for page loads on top of that
    > >
    > > * PrerenderTestUrlImpl is removed to eliminate one hop through protected method
    > >   in the parent class, which also simplified the prefetch side of the tests: no
    > >   need to mention how many page loads to wait for
    > >
    > > * Disabled two tests that load a non-HTML document, correctly killing the
    > >   renderer will be done in later changes.
    > >
    > > BUG=632361, 649632
    > >
    > > Committed: https://crrev.com/66351bd0a0fc8740749663fe765a301e1a175538
    > > Cr-Commit-Position: refs/heads/master@{#427678}
    > Committed: https://crrev.com/f69bf24f0861768f03628206668ddb84f55dc8d4
    > Cr-Commit-Position: refs/heads/master@{#427974}
    
    TBR=jochen@chromium.org,csharrison@chromium.org,mattcary@chromium.org,mkwst@chromium.org,pasko@chromium.org
    BUG=632361, 649632
    
    Review-Url: https://codereview.chromium.org/2455653005
    Cr-Commit-Position: refs/heads/master@{#428027}

--- third_party/WebKit/Source/web/tests/PrerenderingTest.cpp
+++ third_party/WebKit/Source/web/tests/PrerenderingTest.cpp
@@ -150,6 +150,8 @@ class TestPrerenderingSupport : public WebPrerenderingSupport {
     m_abandonedPrerenders.append(prerender);
   }
 
+  void prefetchFinished() override {}
+
   Vector<WebPrerender> m_addedPrerenders;
   Vector<WebPrerender> m_canceledPrerenders;
   Vector<WebPrerender> m_abandonedPrerenders;
