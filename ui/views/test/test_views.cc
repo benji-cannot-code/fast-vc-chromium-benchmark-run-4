d5ec1cfbf253930fdfbf562c210687e0db00a938
alancutter <alancutter@chromium.org>
2016-10-26 21:21:58 -0700

    Revert of "Added common layout framework for system menu rows." (patchset #4 id:60001 of https://codereview.chromium.org/2439093002/ )
    Reason for revert:
    TriViewTest.ViewsRemovedOnRemoveAllChildren is leaking on Linux Chromium OS ASan LSan Tests (1)
    https://build.chromium.org/p/chromium.memory/builders/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29/builds/17234
    
    Original issue's description:
    > Reland of "Added common layout framework for system menu rows."
    >
    > Added common layout framework for system menu rows.
    >
    > Currently most of the system menu rows define their own layouts even
    > though many rows should have the same layout.  Additionally we have to
    > support two separate layout schemes while material design is being
    > implemented.  Thus this CL lays the ground work to make it easier to
    > support multiple layout schemes and a centralized layout source.
    >
    > Follow-up TODO:
    > - wire in the new layouts to the system menu rows
    >
    > BUG=657669
    >
    > Originally landed here: https://chromiumcodereview.appspot.com/2414103003/
    > Reverted here: https://codereview.chromium.org/2441923004/
    >
    > Committed: https://crrev.com/44fcf74b6ff76829c51a504693629c6278558bab
    > Cr-Commit-Position: refs/heads/master@{#427851}
    
    TBR=sky@chromium.org,tdanderson@chromium.org,bruthig@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=657669
    
    Review-Url: https://codereview.chromium.org/2460443002
    Cr-Commit-Position: refs/heads/master@{#427936}

--- ui/views/test/test_views.cc
+++ ui/views/test/test_views.cc
@@ -11,16 +11,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace views {
 
-StaticSizedView::StaticSizedView(const gfx::Size& preferred_size)
+StaticSizedView::StaticSizedView(const gfx::Size& size)
     // Default GetMinimumSize() is GetPreferredSize(). Default GetMaximumSize()
     // is 0x0.
-    : preferred_size_(preferred_size),
-      minimum_size_(preferred_size) {}
+    : size_(size),
+      minimum_size_(size) {
+}
 
 StaticSizedView::~StaticSizedView() {}
 
 gfx::Size StaticSizedView::GetPreferredSize() const {
-  return preferred_size_;
+  return size_;
 }
 
 gfx::Size StaticSizedView::GetMinimumSize() const {
