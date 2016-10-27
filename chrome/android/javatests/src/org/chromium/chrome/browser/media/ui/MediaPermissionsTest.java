b51285c54bb1cd28cc4fc88a561afab0ccc01442
foolip <foolip@chromium.org>
2016-10-27 05:30:19 -0700

    Disable flaky MediaPermissionsTest#testMicrophonePersistenceOff
    BUG=659969
    TBR=dominickn@chromium.org
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2451353003
    Cr-Commit-Position: refs/heads/master@{#428004}

--- chrome/android/javatests/src/org/chromium/chrome/browser/media/ui/MediaPermissionsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/media/ui/MediaPermissionsTest.java
@@ -13,6 +13,7 @@ import android.test.suitebuilder.annotation.Smoke;
 
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.base.test.util.RetryOnFailure;
@@ -144,9 +145,10 @@ public class MediaPermissionsTest extends ChromeActivityTestCaseBase<ChromeActiv
      * switch appears and that permission is granted with it toggled off.
      * @throws Exception
      */
-    @MediumTest
-    @CommandLineFlags.Add("enable-features=DisplayPersistenceToggleInPermissionPrompts")
-    @Feature({"MediaPermissions"})
+    //@MediumTest
+    //@CommandLineFlags.Add("enable-features=DisplayPersistenceToggleInPermissionPrompts")
+    //@Feature({"MediaPermissions"})
+    @DisabledTest
     public void testMicrophonePersistenceOff() throws Exception {
         testMediaPermissionsPlumbing("Mic count:", "initiate_getMicrophone()", 1, true, true);
     }
