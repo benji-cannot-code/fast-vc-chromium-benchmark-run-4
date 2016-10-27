b02bb19ea1e1d50e0aa1c44bfae7c5332a991ded
estevenson <estevenson@chromium.org>
2016-10-27 15:55:40 -0700

    Add @MainDex to DeviceDisplayInfo.
    DeviceDisplayInfo is used in the render process and should always be
    included in the main dex file.
    
    BUG=657024
    
    Review-Url: https://codereview.chromium.org/2445783003
    Cr-Commit-Position: refs/heads/master@{#428181}

--- ui/android/java/src/org/chromium/ui/gfx/DeviceDisplayInfo.java
+++ ui/android/java/src/org/chromium/ui/gfx/DeviceDisplayInfo.java
@@ -17,6 +17,7 @@ import android.view.WindowManager;
 
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
+import org.chromium.base.annotations.MainDex;
 
 /**
  * This class facilitates access to android information typically only
@@ -28,6 +29,7 @@ import org.chromium.base.annotations.JNINamespace;
  * This class is being Deprecated. Use DisplayAndroid instead where possible.
  */
 @JNINamespace("gfx")
+@MainDex
 public class DeviceDisplayInfo {
 
     private final Context mAppContext;
