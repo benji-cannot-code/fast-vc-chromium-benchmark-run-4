9c82028c2c682acdb5e13faa8f8c35b04e4aa5a9
mikhail.pozdnyakov <mikhail.pozdnyakov@intel.com>
2016-10-27 04:02:35 -0700

    Make more media APIs aware of |delay| and |delay_timestamp|
    This patch is a part of 'AudioContext.getOutputTimestamp' method implementation [1] and it continues the changes introduced at [2].
    
    The AudioRendererSink::RenderCallback::Render method has same semantics as AudioSourceCallback::OnMoreData.
    That caused other changes in the following interfaces:
    - AudioSyncReader::UpdatePendingBytes(uint32_t bytes, uint32_t frames_skipped) -> AudioSyncReader::PrepareNextData(base::TimeDelta delay, base::TimeTicks delay_timestamp, uint32_t frames_skipped)
    - AudioDeviceThread::Callback::Process(uint32_t pending_data) -> AudioDeviceThread::Callback::Process(int64_t pending_data, base::TimeTicks data_timestamp)
    - AudioDeviceThread is receiving a "packet" of two int64_t values
    
    BUG=619533
    
    [1] https://codereview.chromium.org/2060833002/
    [2] https://codereview.chromium.org/2101303004/
    
    Review-Url: https://codereview.chromium.org/2437863004
    Cr-Commit-Position: refs/heads/master@{#427996}

--- content/browser/renderer_host/media/audio_input_sync_writer.cc
+++ content/browser/renderer_host/media/audio_input_sync_writer.cc
@@ -13,10 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "content/browser/renderer_host/media/media_stream_manager.h"
 #include "content/public/browser/browser_thread.h"
+#include "media/audio/audio_device_thread.h"
 
 using media::AudioBus;
 using media::AudioInputBuffer;
 using media::AudioInputBufferParameters;
+using Packet = media::AudioDeviceThread::Packet;
 
 namespace content {
 
@@ -326,8 +328,10 @@ void AudioInputSyncWriter::WriteParametersToCurrentSegment(
 }
 
 bool AudioInputSyncWriter::SignalDataWrittenAndUpdateCounters() {
-  if (socket_->Send(&current_segment_id_, sizeof(current_segment_id_)) !=
-      sizeof(current_segment_id_)) {
+  Packet packet = {
+      current_segment_id_,
+      (base::TimeTicks::Now() - base::TimeTicks()).InMicroseconds()};
+  if (socket_->Send(&packet, sizeof(Packet)) != sizeof(Packet)) {
     const std::string error_message = "AISW: No room in socket buffer.";
     LOG(WARNING) << error_message;
     AddToNativeLog(error_message);
--- media/base/audio_renderer_mixer.cc
+++ media/base/audio_renderer_mixer.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/trace_event/trace_event.h"
+#include "media/base/audio_timestamp_helper.h"
 
 namespace media {
 
@@ -163,9 +164,10 @@ bool AudioRendererMixer::CurrentThreadIsRenderingThread() {
   return audio_sink_->CurrentThreadIsRenderingThread();
 }
 
-int AudioRendererMixer::Render(AudioBus* audio_bus,
-                               uint32_t frames_delayed,
-                               uint32_t frames_skipped) {
+int AudioRendererMixer::Render(base::TimeDelta delay,
+                               base::TimeTicks delay_timestamp,
+                               int prior_frames_skipped,
+                               AudioBus* audio_bus) {
   TRACE_EVENT0("audio", "AudioRendererMixer::Render");
   base::AutoLock auto_lock(lock_);
 
@@ -180,6 +182,8 @@ int AudioRendererMixer::Render(AudioBus* audio_bus,
     playing_ = false;
   }
 
+  uint32_t frames_delayed =
+      AudioTimestampHelper::TimeToFrames(delay, output_params_.sample_rate());
   master_converter_.ConvertWithDelay(frames_delayed, audio_bus);
   return audio_bus->frames();
 }
