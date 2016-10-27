b2fc17807d33b032f1c456eb1e3026e5425e2341
mstensho <mstensho@opera.com>
2016-10-27 09:27:12 -0700

    Allow pagination struts to push objects below the exact top of the next column.
    The top margin of a float may push its border box below the top of the next
    column.
    
    Similarly, a repeated table header may do the same to the first row in the next
    column, to make room for itself above it.
    
    The column balancer had assertions to boom at such situations, but it's pretty
    clear now that it's an unreasonable requirements that sturts take us to the
    exact top of the next column.
    
    Added visual tests that crashed before (in debug). No behavioral changes here,
    apart from the fact that the tests no longer crash.
    
    Review-Url: https://codereview.chromium.org/2456003002
    Cr-Commit-Position: refs/heads/master@{#428047}

--- third_party/WebKit/Source/core/layout/ColumnBalancer.cpp
+++ third_party/WebKit/Source/core/layout/ColumnBalancer.cpp
@@ -140,7 +140,6 @@ void InitialColumnHeightFinder::examineBoxAfterEntering(
     if (box.needsForcedBreakBefore(previousBreakAfterValue)) {
       addContentRun(flowThreadOffset());
     } else {
-      ASSERT(isFirstAfterBreak(flowThreadOffset()) || !box.paginationStrut());
       if (isFirstAfterBreak(flowThreadOffset())) {
         // This box is first after a soft break.
         recordStrutBeforeOffset(flowThreadOffset(), box.paginationStrut());
@@ -307,7 +306,6 @@ void MinimumSpaceShortageFinder::examineBoxAfterEntering(
     if (box.needsForcedBreakBefore(previousBreakAfterValue)) {
       m_forcedBreaksCount++;
     } else {
-      ASSERT(isFirstAfterBreak(flowThreadOffset()) || !box.paginationStrut());
       if (isFirstAfterBreak(flowThreadOffset())) {
         // This box is first after a soft break.
         LayoutUnit strut = box.paginationStrut();
