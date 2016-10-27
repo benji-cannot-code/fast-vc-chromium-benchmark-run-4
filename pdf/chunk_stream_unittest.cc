4615ffe03758698c4ae7fa3c0562bd3f0433ab35
art-snake <art-snake@yandex-team.ru>
2016-10-27 10:21:45 -0700

    Add pdf_unittests target.
    Needs for https://codereview.chromium.org/2349753003/
    
    Review-Url: https://codereview.chromium.org/2453323002
    Cr-Commit-Position: refs/heads/master@{#428066}

---
+++ pdf/chunk_stream_unittest.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "pdf/chunk_stream.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace chrome_pdf {
+
+TEST(ChunkStreamTest, Simple) {
+  ChunkStream stream;
+  stream.Preallocate(1000);
+  EXPECT_FALSE(stream.IsRangeAvailable(100, 500));
+}
+
+}  // namespace chrome_pdf
