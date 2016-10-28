2f04e5534ac94de58731c3dfc85905c8af07338b
pwnall <pwnall@chromium.org>
2016-10-28 06:19:17 -0700

    Fix compilation error on OSX 10.12.
    chrome/browser/ui/cocoa/fullscreen/fullscreen_toolbar_mouse_tracker.mm
    does not include the header for a class that it uses,
    BrowserWindowController. This CL adds the missing include and fixes the
    OSX 10.12 build error documented in the associated bug.
    
    BUG=660292
    
    Review-Url: https://codereview.chromium.org/2462433003
    Cr-Commit-Position: refs/heads/master@{#428351}

--- chrome/browser/ui/cocoa/fullscreen/fullscreen_toolbar_mouse_tracker.mm
+++ chrome/browser/ui/cocoa/fullscreen/fullscreen_toolbar_mouse_tracker.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "chrome/browser/ui/cocoa/fullscreen/fullscreen_toolbar_mouse_tracker.h"
 
+#include "chrome/browser/ui/cocoa/browser_window_controller.h"
 #include "chrome/browser/ui/cocoa/fullscreen/fullscreen_toolbar_animation_controller.h"
 #import "chrome/browser/ui/cocoa/fullscreen_toolbar_controller.h"
 #include "ui/base/cocoa/appkit_utils.h"
