ca99cbf284abfc23826d999ed1fa0d1c1ec6d365
hans <hans@chromium.org>
2016-10-27 21:07:06 -0700

    Clarify that BlockSizeForFileType can't be called with EXTERNAL FileType
    PVS-Studio analysis pointed out that if it were called with such a file
    type, that would lead to division by zero in RequiredBlocks.
    
    Also, remove the default: case since the enum is now covered. Clang warns
    if the switch doesn't cover all enum values.
    
    BUG=660198
    
    Review-Url: https://codereview.chromium.org/2458773003
    Cr-Commit-Position: refs/heads/master@{#428279}

--- net/disk_cache/blockfile/addr.h
+++ net/disk_cache/blockfile/addr.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include "base/logging.h"
 #include "net/base/net_export.h"
 #include "net/disk_cache/blockfile/disk_format_base.h"
 
@@ -139,9 +140,11 @@ class NET_EXPORT_PRIVATE Addr {
         return 104;
       case BLOCK_EVICTED:
         return 48;
-      default:
+      case EXTERNAL:
+        NOTREACHED();
         return 0;
     }
+    return 0;
   }
 
   static FileType RequiredFileType(int size) {
