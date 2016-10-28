9fc43fa39e1909d641d3ddbd2b0d3f5a96930e14
krasin <krasin@chromium.org>
2016-10-27 19:35:14 -0700

    Fix LayoutTableSectionTest test cases which called a method on nil.
    In particular, section var was nil for the tests, which had
    document().body()->firstChild()->firstChild()->layoutObject());
    
    These are switched to getLayoutObjectByElementId("section"), and
    also checks for section != NULL were added.
    
    The bug was found with running the test with UBSan.
    
    Also adding net_unittests to UBSanVptr ToT, as it has already been runnin on the regular UBSanVptr bot.
    
    BUG=607996
    
    Review-Url: https://codereview.chromium.org/2455223002
    Cr-Commit-Position: refs/heads/master@{#428259}

--- third_party/WebKit/Source/core/layout/LayoutTableSectionTest.cpp
+++ third_party/WebKit/Source/core/layout/LayoutTableSectionTest.cpp
@@ -17,13 +17,15 @@ TEST_F(LayoutTableSectionTest,
        BackgroundIsKnownToBeOpaqueWithLayerAndCollapsedBorder) {
   setBodyInnerHTML(
       "<table style='border-collapse: collapse'>"
-      "  <thead style='will-change: transform; background-color: blue'>"
+      "  <thead id='section' style='will-change: transform; background-color: "
+      "blue'>"
       "    <tr><td>Cell</td></tr>"
       "  </thead>"
       "</table>");
 
-  LayoutTableSection* section = toLayoutTableSection(
-      document().body()->firstChild()->firstChild()->layoutObject());
+  LayoutTableSection* section =
+      toLayoutTableSection(getLayoutObjectByElementId("section"));
+  EXPECT_TRUE(section);
   EXPECT_FALSE(
       section->backgroundIsKnownToBeOpaqueInRect(LayoutRect(0, 0, 1, 1)));
 }
@@ -31,13 +33,14 @@ TEST_F(LayoutTableSectionTest,
 TEST_F(LayoutTableSectionTest, BackgroundIsKnownToBeOpaqueWithBorderSpacing) {
   setBodyInnerHTML(
       "<table style='border-spacing: 10px'>"
-      "  <thead style='background-color: blue'>"
+      "  <thead id='section' style='background-color: blue'>"
       "    <tr><td>Cell</td></tr>"
       "  </thead>"
       "</table>");
 
-  LayoutTableSection* section = toLayoutTableSection(
-      document().body()->firstChild()->firstChild()->layoutObject());
+  LayoutTableSection* section =
+      toLayoutTableSection(getLayoutObjectByElementId("section"));
+  EXPECT_TRUE(section);
   EXPECT_FALSE(
       section->backgroundIsKnownToBeOpaqueInRect(LayoutRect(0, 0, 1, 1)));
 }
@@ -45,14 +48,15 @@ TEST_F(LayoutTableSectionTest, BackgroundIsKnownToBeOpaqueWithBorderSpacing) {
 TEST_F(LayoutTableSectionTest, BackgroundIsKnownToBeOpaqueWithEmptyCell) {
   setBodyInnerHTML(
       "<table style='border-spacing: 10px'>"
-      "  <thead style='background-color: blue'>"
+      "  <thead id='section' style='background-color: blue'>"
       "    <tr><td>Cell</td></tr>"
       "    <tr><td>Cell</td><td>Cell</td></tr>"
       "  </thead>"
       "</table>");
 
-  LayoutTableSection* section = toLayoutTableSection(
-      document().body()->firstChild()->firstChild()->layoutObject());
+  LayoutTableSection* section =
+      toLayoutTableSection(getLayoutObjectByElementId("section"));
+  EXPECT_TRUE(section);
   EXPECT_FALSE(
       section->backgroundIsKnownToBeOpaqueInRect(LayoutRect(0, 0, 1, 1)));
 }
@@ -65,6 +69,7 @@ TEST_F(LayoutTableSectionTest, EmptySectionDirtiedRows) {
 
   LayoutTableSection* section =
       toLayoutTableSection(getLayoutObjectByElementId("section"));
+  EXPECT_TRUE(section);
   CellSpan cellSpan = section->dirtiedRows(LayoutRect(50, 50, 100, 100));
   EXPECT_EQ(0u, cellSpan.start());
   EXPECT_EQ(0u, cellSpan.end());
@@ -78,6 +83,7 @@ TEST_F(LayoutTableSectionTest, EmptySectionDirtiedEffectiveColumns) {
 
   LayoutTableSection* section =
       toLayoutTableSection(getLayoutObjectByElementId("section"));
+  EXPECT_TRUE(section);
   CellSpan cellSpan =
       section->dirtiedEffectiveColumns(LayoutRect(50, 50, 100, 100));
   // The table has at least 1 column even if there is no cell.
