13f2079da0e25c71ecd29a811433d85c14a17c8d
justincohen <justincohen@chromium.org>
2016-10-28 08:06:33 -0700

    Correct iOS GN hermetic support.
    BUG=
    
    Review-Url: https://codereview.chromium.org/2452593004
    Cr-Commit-Position: refs/heads/master@{#428373}

--- build/config/ios/codesign.py
+++ build/config/ios/codesign.py
@@ -415,6 +415,8 @@ class GenerateEntitlementsAction(Action):
 
 def Main():
   parser = argparse.ArgumentParser('codesign iOS bundles')
+  parser.add_argument('--developer_dir', required=False,
+                      help='Path to Xcode.')
   subparsers = parser.add_subparsers()
 
   actions = [
@@ -427,6 +429,8 @@ def Main():
     action.Register(subparsers)
 
   args = parser.parse_args()
+  if args.developer_dir:
+    os.environ['DEVELOPER_DIR'] = args.developer_dir
   args.func(args)
 
 
