1e36ddb769f92db3296c064659c52b21c235ae8c
chenwilliam <chenwilliam@chromium.org>
2016-10-28 11:18:40 -0700

    DevTools: clean up scripts folder
    - Use consistent snake_case naming for folders and files
    - Move Python build scripts to scripts/build
    - Add description in readme.md
    - Rename js_build -> gulp
    
    BUG=none
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2441163002
    Cr-Commit-Position: refs/heads/master@{#428430}

--- third_party/WebKit/Source/devtools/scripts/convert_svg_images_to_png.py
+++ third_party/WebKit/Source/devtools/scripts/convert_svg_images_to_png.py
@@ -28,13 +28,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import devtools_file_hashes
-import hashlib
-import os
 import os.path
 import re
 import subprocess
 import sys
+
+from build import devtools_file_hashes
 
 try:
     import json
