f301d7b25af773d044790a304ddcee616b56b048
xidachen <xidachen@chromium.org>
2016-10-28 13:38:38 -0700

    Revert of DevTools: clean up scripts folder (patchset #7 id:120001 of https://codereview.chromium.org/2441163002/ )
    Reason for revert:
    Causing compile error here:
    https://build.chromium.org/p/chromium.chromiumos/builders/ChromiumOS%20x86-generic%20Compile/builds/27586
    
    Original issue's description:
    > DevTools: clean up scripts folder
    >
    > - Use consistent snake_case naming for folders and files
    > - Move Python build scripts to scripts/build
    > - Add description in readme.md
    > - Rename js_build -> gulp
    >
    > BUG=none
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    >
    > Committed: https://crrev.com/c5c20f9f1b639a556c3aa87b1ebcd4ddc396649e
    > Cr-Commit-Position: refs/heads/master@{#428430}
    
    TBR=dgozman@chromium.org,dmazzoni@chromium.org,chenwilliam@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2464463002
    Cr-Commit-Position: refs/heads/master@{#428480}

---
+++ third_party/WebKit/Source/devtools/scripts/compiler-runner/src/org/chromium/devtools/compiler/DevToolsCodingConvention.java
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+package org.chromium.devtools.compiler;
+
+import com.google.javascript.jscomp.CodingConventions;
+
+public class DevToolsCodingConvention extends CodingConventions.Proxy {
+
+    private static final long serialVersionUID = 1L;
+
+    public DevToolsCodingConvention() {
+        super(CodingConventions.getDefault());
+    }
+
+    @Override
+    public boolean isPrivate(String name) {
+        return name.length() > 1 && name.charAt(0) == '_' && name.charAt(1) != '_';
+    }
+
+    @Override
+    public boolean isValidEnumKey(String key) {
+        return !key.isEmpty() && Character.isUpperCase(key.charAt(0));
+    }
+}
