140eee000fe8bb6638bc4a15658f80c5563517c1
shaochuan <shaochuan@chromium.org>
2016-10-28 07:05:59 -0700

    Add isCacheMiss field to WebURLError/ResourceError
    Add a field to WebURLError/ResourceError to make blink resource loading code
    aware of disk cache miss in network stack (net::ERR_CACHE_MISS).
    
    This is a preparation CL for cache-aware Resource loading, which specifies
    loading only from disk cache in the first request and retries with original
    request if disk cache misses. See discussion in
    https://codereview.chromium.org/2390583002/ for details.
    
    BUG=570205
    
    Review-Url: https://codereview.chromium.org/2452023004
    Cr-Commit-Position: refs/heads/master@{#428361}

--- third_party/WebKit/Source/platform/exported/WebURLError.cpp
+++ third_party/WebKit/Source/platform/exported/WebURLError.cpp
@@ -51,6 +51,7 @@ WebURLError& WebURLError::operator=(const ResourceError& error) {
     staleCopyInCache = error.staleCopyInCache();
     localizedDescription = error.localizedDescription();
     wasIgnoredByHandler = error.wasIgnoredByHandler();
+    isCacheMiss = error.isCacheMiss();
   }
   return *this;
 }
@@ -63,6 +64,7 @@ WebURLError::operator ResourceError() const {
   resourceError.setIsCancellation(isCancellation);
   resourceError.setStaleCopyInCache(staleCopyInCache);
   resourceError.setWasIgnoredByHandler(wasIgnoredByHandler);
+  resourceError.setIsCacheMiss(isCacheMiss);
   return resourceError;
 }
 
