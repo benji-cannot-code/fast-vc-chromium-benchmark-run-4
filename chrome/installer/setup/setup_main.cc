8848a7e70bb56a5553bc1a9573efc22bff2fda90
grt <grt@chromium.org>
2016-10-27 23:58:08 -0700

    Remove the installation validator.
    The tests in chrome/test/mini_installer are the path forward for
    ensuring that the installer does the right thing.
    
    BUG=none
    R=robertshield@chromium.org
    
    Review-Url: https://codereview.chromium.org/2456863002
    Cr-Commit-Position: refs/heads/master@{#428302}

--- chrome/installer/setup/setup_main.cc
+++ chrome/installer/setup/setup_main.cc
@@ -67,7 +67,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/installer/util/html_dialog.h"
 #include "chrome/installer/util/install_util.h"
 #include "chrome/installer/util/installation_state.h"
-#include "chrome/installer/util/installation_validator.h"
 #include "chrome/installer/util/installer_state.h"
 #include "chrome/installer/util/installer_util_strings.h"
 #include "chrome/installer/util/l10n_string_util.h"
@@ -89,7 +88,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using installer::InstallerState;
 using installer::InstallationState;
-using installer::InstallationValidator;
 using installer::MasterPreferences;
 using installer::Product;
 using installer::ProductState;
@@ -1967,15 +1965,6 @@ int WINAPI wWinMain(HINSTANCE instance, HINSTANCE prev_instance,
         InstallProducts(original_state, setup_exe, cmd_line, prefs,
                         &installer_state, &installer_directory);
   }
-
-  // Validate that the machine is now in a good state following the operation.
-  // TODO(grt): change this to log at DFATAL once we're convinced that the
-  // validator handles all cases properly.
-  InstallationValidator::InstallationType installation_type =
-      InstallationValidator::NO_PRODUCTS;
-  LOG_IF(ERROR,
-         !InstallationValidator::ValidateInstallationType(system_install,
-                                                          &installation_type));
 
   UMA_HISTOGRAM_ENUMERATION("Setup.Install.Result", install_status,
                             installer::MAX_INSTALL_STATUS);
