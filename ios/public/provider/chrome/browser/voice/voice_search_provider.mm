f94a9cede8d195a4ec86c2303b1e890d27e7b00d
rohitrao <rohitrao@chromium.org>
2016-10-28 09:54:16 -0700

    [ios] Adds VoiceSearchProvider::IsVoiceSearchEnabled().
    The other provider methods must not be called if voice search is disabled.
    
    BUG=656617
    
    Review-Url: https://codereview.chromium.org/2457093002
    Cr-Commit-Position: refs/heads/master@{#428393}

--- ios/public/provider/chrome/browser/voice/voice_search_provider.mm
+++ ios/public/provider/chrome/browser/voice/voice_search_provider.mm
@@ -7,6 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/public/provider/chrome/browser/voice/voice_search_controller.h"
 
+bool VoiceSearchProvider::IsVoiceSearchEnabled() const {
+  return false;
+}
+
 NSArray* VoiceSearchProvider::GetAvailableLanguages() const {
   return @[];
 }
