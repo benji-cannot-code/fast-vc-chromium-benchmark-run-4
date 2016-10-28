735a08b9959e1719c9e8e4b3499f7006c4319e46
tyoshino <tyoshino@chromium.org>
2016-10-28 00:41:54 -0700

    Disable flaky Android WebView test ClientOnPageFinishedTest.testCalledAfterError()
    TBR=michaelbai@chromium.org
    BUG=652577
    
    Review-Url: https://codereview.chromium.org/2455943004
    Cr-Commit-Position: refs/heads/master@{#428311}

--- android_webview/javatests/src/org/chromium/android_webview/test/ClientOnPageFinishedTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/ClientOnPageFinishedTest.java
@@ -11,7 +11,7 @@ import org.chromium.android_webview.AwContents;
 import org.chromium.android_webview.test.util.CommonResources;
 import org.chromium.android_webview.test.util.JSUtils;
 import org.chromium.base.test.util.Feature;
-import org.chromium.base.test.util.RetryOnFailure;
+import org.chromium.base.test.util.FlakyTest;
 import org.chromium.content.browser.test.util.TestCallbackHelperContainer;
 import org.chromium.net.test.util.TestWebServer;
 
@@ -54,7 +54,7 @@ public class ClientOnPageFinishedTest extends AwTestBase {
 
     @MediumTest
     @Feature({"AndroidWebView"})
-    @RetryOnFailure
+    @FlakyTest(message = "crbug.com/652577")
     public void testCalledAfterError() throws Throwable {
         class LocalTestClient extends TestAwContentsClient {
             private boolean mIsOnReceivedErrorCalled = false;
