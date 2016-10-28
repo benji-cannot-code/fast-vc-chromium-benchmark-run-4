4b78ff2e8ad29d5590a6909df1d1c339b08f0b11
msarda <msarda@chromium.org>
2016-10-28 04:42:32 -0700

    Indicate that sync was disabled by administrator.
    This CL does the folllowing changes to the sign-in confirmation dialog:
    * hides the  "Personalized Google Services" section and the "sync settings" link when sync is disabled.
    * changes tge button titles to  [ CANCEL SIGN IN ] and [ SIGN IN ANYWAY ]
    * changes the Chrome sync title to "Chrome Sync is disabled by your administrator"
    * changes the body of the "Chrome Sync" section to "Your administrator has disabled syncing of your bookmarks, history, passwords, and other settings."
    
    Screenshot with the new UI can be found here (internal like only):
    https://screenshot.googleplex.com/XwOrvUgYDvS
    
    BUG=629602
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2450843002
    Cr-Commit-Position: refs/heads/master@{#428337}

--- chrome/browser/ui/cocoa/profiles/signin_view_controller_delegate_mac.mm
+++ chrome/browser/ui/cocoa/profiles/signin_view_controller_delegate_mac.mm
@@ -42,6 +42,13 @@ const int kSyncConfirmationDialogHeight = 351;
 // Initial height of the signin error modal dialog.
 const int kSigninErrorDialogHeight = 164;
 
+CGFloat GetSyncConfirmationDialogPreferredHeight(Profile* profile) {
+  // If sync is disabled, then the sync confirmation dialog looks like an error
+  // dialog and thus it has the same preferred size.
+  return profile->IsSyncAllowed() ? kSyncConfirmationDialogHeight
+                                  : kSigninErrorDialogHeight;
+}
+
 }  // namespace
 
 SigninViewControllerDelegateMac::SigninViewControllerDelegateMac(
@@ -113,8 +120,9 @@ SigninViewControllerDelegateMac::CreateSyncConfirmationWebContents(
       ui::PAGE_TRANSITION_AUTO_TOPLEVEL, std::string());
 
   NSView* webview = web_contents->GetNativeView();
-  [webview setFrameSize:NSMakeSize(kModalDialogWidth,
-                                   kSyncConfirmationDialogHeight)];
+  [webview setFrameSize:NSMakeSize(
+                            kModalDialogWidth,
+                            GetSyncConfirmationDialogPreferredHeight(profile))];
 
   return web_contents;
 }
@@ -204,7 +212,7 @@ SigninViewControllerDelegate::CreateSyncConfirmationDelegate(
           browser->profile()),
       browser->tab_strip_model()->GetActiveWebContents(),
       NSMakeRect(0, 0, kModalDialogWidth,
-                 kSyncConfirmationDialogHeight),
+                 GetSyncConfirmationDialogPreferredHeight(browser->profile())),
       true);
 }
 
