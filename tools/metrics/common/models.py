d9b157883fcca655c20f6e66cab5cc36c6d3c936
holte <holte@chromium.org>
2016-10-28 15:36:49 -0700

    Fix line wrapping in rappor.xml to handle indentation correctly.
    BUG=
    
    Review-Url: https://codereview.chromium.org/2460473004
    Cr-Commit-Position: refs/heads/master@{#428526}

--- tools/metrics/common/models.py
+++ tools/metrics/common/models.py
@@ -131,7 +131,10 @@ class TextNodeType(NodeType):
     Returns:
       The string content of the node.
     """
-    return node.firstChild.nodeValue.strip()
+    if not node.firstChild:
+      return ''
+    text = node.firstChild.nodeValue
+    return '\n\n'.join(pretty_print_xml.SplitParagraphs(text))
 
   def Marshall(self, doc, obj):
     """Converts an object into an XML node of this type.
@@ -144,7 +147,8 @@ class TextNodeType(NodeType):
       An XML node encoding the object.
     """
     node = doc.createElement(self.tag)
-    node.appendChild(doc.createTextNode(obj))
+    if obj:
+      node.appendChild(doc.createTextNode(obj))
     return node
 
 
