e245149d62568d84ec4140d1075419dec3dadf66
ianwen <ianwen@chromium.org>
2016-10-28 12:01:21 -0700

    Fix some remaining issues with Special Locale
    This CL fixes issues regarding to recent_tabs' visibility in menu,
    special engine's existence in special locale, and some dialog bugs.
    
    BUG=656512,658942,655776
    
    Review-Url: https://codereview.chromium.org/2450373002
    Cr-Commit-Position: refs/heads/master@{#428448}

--- chrome/android/java/src/org/chromium/chrome/browser/locale/SearchEnginePromoDialog.java
+++ chrome/android/java/src/org/chromium/chrome/browser/locale/SearchEnginePromoDialog.java
@@ -55,6 +55,7 @@ public class SearchEnginePromoDialog extends Dialog
             Intent intent = PreferencesLauncher.createIntentForSettingsPage(getContext(),
                     SearchEnginePreference.class.getName());
             getContext().startActivity(intent);
+            dismiss();
         }
     };
 
