1af2868916341f6d03221083bcad338261f960b6
eyaich <eyaich@chromium.org>
2016-10-28 05:43:30 -0700

    Adding blacklist for benchmarks never run on a waterfall.
    BUG=chromium:633253
    
    Review-Url: https://codereview.chromium.org/2453913005
    Cr-Commit-Position: refs/heads/master@{#428345}

--- tools/perf/generate_perf_json.py
+++ tools/perf/generate_perf_json.py
@@ -119,12 +119,14 @@ SCRIPT_TESTS = [
     ],
     'name': 'tracing_perftests',
     'script': 'gtest_perf_test.py',
-    'testers': [
-      {
-        'name': 'Linux Perf',
-        'shards': [3]
-      },
-    ]
+    'testers': {
+      'chromium.perf': [
+        {
+          'name': 'Linux Perf',
+          'shards': [3]
+        },
+      ]
+    }
   },
   {
     'args': [
@@ -470,6 +472,17 @@ BENCHMARK_NAME_WHITELIST = set([
     u'dromaeo.cssqueryjquery',
 ])
 
+# List of benchmarks that are to never be run on a waterfall.
+BENCHMARK_NAME_BLACKLIST = [
+    'multipage_skpicture_printer',
+    'multipage_skpicture_printer_ct',
+    'rasterize_and_record_micro_ct',
+    'repaint_ct',
+    'multipage_skpicture_printer',
+    'multipage_skpicture_printer_ct',
+    'skpicture_printer',
+    'skpicture_printer_ct',
+]
 
 def current_benchmarks(use_whitelist):
   current_dir = os.path.dirname(__file__)
@@ -479,6 +492,13 @@ def current_benchmarks(use_whitelist):
   all_benchmarks = discover.DiscoverClasses(
       benchmarks_dir, top_level_dir, benchmark_module.Benchmark,
       index_by_class_name=True).values()
+  # Remove all blacklisted benchmarks
+  for blacklisted in BENCHMARK_NAME_BLACKLIST:
+    for benchmark in all_benchmarks:
+      if benchmark.Name() == blacklisted:
+        all_benchmarks.remove(benchmark)
+        break
+
   if use_whitelist:
     all_benchmarks = (
         bench for bench in all_benchmarks
