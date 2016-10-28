fefb1510ec4416a5191ab7f5f455a173c29b996a
glevin <glevin@chromium.org>
2016-10-27 18:33:24 -0700

    Fix login navigation button enabling
    BUG=658014,660011
    TEST=Back, Close, and Refresh buttons should always be disabled when a
    modal dialog is present, and enabled otherwise.  See bugs for specific
    repro instructions.
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2452333003
    Cr-Commit-Position: refs/heads/master@{#428231}

--- chrome/browser/resources/chromeos/login/oobe_screen_oauth_enrollment.js
+++ chrome/browser/resources/chromeos/login/oobe_screen_oauth_enrollment.js
@@ -136,6 +136,16 @@ login.createScreen('OAuthEnrollmentScreen', 'oauth-enrollment', function() {
             this.updateControlsState();
           }).bind(this));
 
+      this.authenticator_.addEventListener('dialogShown',
+        (function(e) {
+          this.navigation_.disabled = true;
+        }).bind(this));
+
+      this.authenticator_.addEventListener('dialogHidden',
+        (function(e) {
+          this.navigation_.disabled = false;
+        }).bind(this));
+
       this.authenticator_.insecureContentBlockedCallback =
           (function(url) {
             this.showError(
@@ -235,6 +245,7 @@ login.createScreen('OAuthEnrollmentScreen', 'oauth-enrollment', function() {
       }
       this.isManualEnrollment_ = data.enrollment_mode === 'manual';
       this.isCancelDisabled = true;
+      this.navigation_.disabled = false;
 
       this.showStep(data.attestationBased ? STEP_WORKING : STEP_SIGNIN);
     },
