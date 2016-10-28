706986a2b09b885c63ebc255ba99f0fa0768ca9a
jsbell <jsbell@chromium.org>
2016-10-28 10:26:41 -0700

    MessagePort: Remove unused RefPtr<ScriptState> member
    Dead code following r393589 and other simplifications.
    
    R=haraken@chromium.org
    
    Review-Url: https://codereview.chromium.org/2461583002
    Cr-Commit-Position: refs/heads/master@{#428408}

--- third_party/WebKit/Source/core/dom/MessagePort.cpp
+++ third_party/WebKit/Source/core/dom/MessagePort.cpp
@@ -59,8 +59,6 @@ MessagePort::MessagePort(ExecutionContext& executionContext)
 
 MessagePort::~MessagePort() {
   DCHECK(!m_started || !isEntangled());
-  if (m_scriptStateForConversion)
-    m_scriptStateForConversion->disposePerContextData();
 }
 
 void MessagePort::postMessage(ExecutionContext* context,
