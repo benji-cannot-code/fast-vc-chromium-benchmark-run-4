d8f08aea0cfeeb40debe42585befdc7f31fe6f42
hunyadym <hunyadym@chromium.org>
2016-10-28 05:40:55 -0700

    Add "critical" parameter to DMServer requests. Set value to true if the request is the first policy fetch.
    BUG=660009
    
    Review-Url: https://codereview.chromium.org/2458653003
    Cr-Commit-Position: refs/heads/master@{#428344}

--- components/policy/core/common/cloud/cloud_policy_constants.cc
+++ components/policy/core/common/cloud/cloud_policy_constants.cc
@@ -23,6 +23,7 @@ namespace dm_protocol {
 // Name constants for URL query parameters.
 const char kParamAgent[] = "agent";
 const char kParamAppType[] = "apptype";
+const char kParamCritical[] = "critical";
 const char kParamDeviceID[] = "deviceid";
 const char kParamDeviceType[] = "devicetype";
 const char kParamLastError[] = "lasterror";
