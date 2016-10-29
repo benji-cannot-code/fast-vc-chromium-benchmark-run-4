c0d0dcf35fca677ab61c9fb036d60778832228b4
shaochuan <shaochuan@chromium.org>
2016-10-29 00:26:00 -0700

    Revert "Enable MidiManagerWinrt by default on Windows 10"
    Make MidiManagerWinrt back behind a Finch flag disabled by default.
    
    Reverting CL https://codereview.chromium.org/2392263002/ while preserving
    strings and histograms for the "disabled" flag. Revert this CL to enable it by
    default again.
    
    BUG=645403,659458
    
    Review-Url: https://codereview.chromium.org/2450033003
    Cr-Commit-Position: refs/heads/master@{#428608}

--- media/midi/midi_manager_win.cc
+++ media/midi/midi_manager_win.cc
@@ -28,8 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/bind.h"
-#include "base/command_line.h"
 #include "base/containers/hash_tables.h"
+#include "base/feature_list.h"
 #include "base/macros.h"
 #include "base/message_loop/message_loop.h"
 #include "base/single_thread_task_runner.h"
@@ -1195,8 +1195,7 @@ void MidiManagerWin::OnReceiveMidiData(uint32_t port_index,
 }
 
 MidiManager* MidiManager::Create() {
-  if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kDisableWinrtMidiApi) &&
+  if (base::FeatureList::IsEnabled(features::kMidiManagerWinrt) &&
       base::win::GetVersion() >= base::win::VERSION_WIN10)
     return new MidiManagerWinrt();
   return new MidiManagerWin();
