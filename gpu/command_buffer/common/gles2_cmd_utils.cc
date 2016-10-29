08bf3ae8c5bf4cf116e5e5bb869e94671cf511ab
qiankun.miao <qiankun.miao@intel.com>
2016-10-28 19:02:53 -0700

    Add missing integer formats
    BUG=612542
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2450663002
    Cr-Commit-Position: refs/heads/master@{#428589}

--- gpu/command_buffer/common/gles2_cmd_utils.cc
+++ gpu/command_buffer/common/gles2_cmd_utils.cc
@@ -1632,6 +1632,9 @@ bool GLES2Util::IsUnsignedIntegerFormat(uint32_t internal_format) {
     case GL_RG8UI:
     case GL_RG16UI:
     case GL_RG32UI:
+    case GL_RGB8UI:
+    case GL_RGB16UI:
+    case GL_RGB32UI:
     case GL_RGBA8UI:
     case GL_RGB10_A2UI:
     case GL_RGBA16UI:
@@ -1651,6 +1654,9 @@ bool GLES2Util::IsSignedIntegerFormat(uint32_t internal_format) {
     case GL_RG8I:
     case GL_RG16I:
     case GL_RG32I:
+    case GL_RGB8I:
+    case GL_RGB16I:
+    case GL_RGB32I:
     case GL_RGBA8I:
     case GL_RGBA16I:
     case GL_RGBA32I:
