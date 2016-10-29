e7bcd8f008d454a6dd4d59a721ea3afc83ec1de5
estade <estade@chromium.org>
2016-10-28 22:45:07 -0700

    Some more pre-material cleanups
    BUG=648281
    
    Review-Url: https://codereview.chromium.org/2444923003
    Cr-Commit-Position: refs/heads/master@{#428603}

--- chrome/browser/ui/cocoa/infobars/infobar_controller.mm
+++ chrome/browser/ui/cocoa/infobars/infobar_controller.mm
@@ -76,8 +76,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   // Infobars are drawn a little taller, so have to move its controls to keep
   // them centered.
-  CGFloat heightDelta = InfoBarContainerDelegate::kDefaultBarTargetHeightMd -
-                        InfoBarContainerDelegate::kDefaultBarTargetHeight;
+  // TODO(ellyjones): Remove this constant.
+  CGFloat heightDelta = 2;
   for (NSView* nextSubview in [infoBarView_ subviews]) {
     NSRect frame = [nextSubview frame];
     frame.origin.y += heightDelta / 2;
