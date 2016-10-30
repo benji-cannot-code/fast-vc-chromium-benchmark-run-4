c92e2a29490cbbbe0ef877ed83fb55d431cf5ae4
ilevy <ilevy@chromium.org>
2016-10-30 13:46:57 -0700

    Add popcnt support for pnacl
    Detect popcount from cpuid and add feature flag in
    pnacl.
    
    BUG=660673
    
    Review-Url: https://codereview.chromium.org/2458333002
    Cr-Commit-Position: refs/heads/master@{#428632}

--- components/nacl/renderer/platform_info.cc
+++ components/nacl/renderer/platform_info.cc
@@ -76,7 +76,9 @@ std::string GetCpuFeatures() {
   // TODO: SSE 3
   else if (cpu.has_sse2()) features.push_back("+sse2");
 
-  // TODO: AES, POPCNT, LZCNT, ...
+  if (cpu.has_popcnt()) features.push_back("+popcnt");
+
+  // TODO: AES, LZCNT, ...
   return base::JoinString(features, ",");
 }
 
