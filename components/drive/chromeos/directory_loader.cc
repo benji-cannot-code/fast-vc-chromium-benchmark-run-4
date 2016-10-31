4a9a2cbcffa39fcf841e91595f43c86a997c765a
avi <avi@chromium.org>
2016-10-30 21:33:58 -0700

    Remove stl_util's deletion function use from components/drive/.
    BUG=555865
    
    Review-Url: https://codereview.chromium.org/2459213002
    Cr-Commit-Position: refs/heads/master@{#428653}

--- components/drive/chromeos/directory_loader.cc
+++ components/drive/chromeos/directory_loader.cc
@@ -6,11 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/drive/chromeos/directory_loader.h"
 
 #include <stddef.h>
+
+#include <algorithm>
 #include <utility>
 
 #include "base/callback.h"
 #include "base/callback_helpers.h"
 #include "base/macros.h"
+#include "base/memory/ptr_util.h"
 #include "base/metrics/histogram.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/time/time.h"
@@ -210,7 +213,6 @@ DirectoryLoader::DirectoryLoader(
 }
 
 DirectoryLoader::~DirectoryLoader() {
-  base::STLDeleteElements(&fast_fetch_feed_fetcher_set_);
 }
 
 void DirectoryLoader::AddObserver(ChangeListLoaderObserver* observer) {
@@ -503,7 +505,7 @@ void DirectoryLoader::LoadDirectoryFromServer(
   FeedFetcher* fetcher = new FeedFetcher(this,
                                          directory_fetch_info,
                                          about_resource->root_folder_id());
-  fast_fetch_feed_fetcher_set_.insert(fetcher);
+  fast_fetch_feed_fetcher_set_.insert(base::WrapUnique(fetcher));
   fetcher->Run(
       base::Bind(&DirectoryLoader::LoadDirectoryFromServerAfterLoad,
                  weak_ptr_factory_.GetWeakPtr(),
@@ -519,8 +521,12 @@ void DirectoryLoader::LoadDirectoryFromServerAfterLoad(
   DCHECK(!directory_fetch_info.empty());
 
   // Delete the fetcher.
-  fast_fetch_feed_fetcher_set_.erase(fetcher);
-  delete fetcher;
+  auto it = std::find_if(fast_fetch_feed_fetcher_set_.begin(),
+                         fast_fetch_feed_fetcher_set_.end(),
+                         [fetcher](const std::unique_ptr<FeedFetcher>& ptr) {
+                           return ptr.get() == fetcher;
+                         });
+  fast_fetch_feed_fetcher_set_.erase(it);
 
   logger_->Log(logging::LOG_INFO,
                "Fast-fetch complete: %s => %s",
