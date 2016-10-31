22a31fa1732ab69253ec60d84755a6fec552cb87
fs <fs@opera.com>
2016-10-31 03:34:39 -0700

    Make sure to always reset the cached filter in ReferenceFilterOperation
    BUG=658305
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2453033004
    Cr-Commit-Position: refs/heads/master@{#428678}

--- third_party/WebKit/Source/core/paint/FilterEffectBuilder.cpp
+++ third_party/WebKit/Source/core/paint/FilterEffectBuilder.cpp
@@ -149,16 +149,17 @@ FilterEffect* FilterEffectBuilder::buildFilterEffect(
       case FilterOperation::REFERENCE: {
         ReferenceFilterOperation& referenceOperation =
             toReferenceFilterOperation(*filterOperation);
-        if (Filter* referenceFilter =
-                buildReferenceFilter(referenceOperation, previousEffect)) {
+        Filter* referenceFilter =
+            buildReferenceFilter(referenceOperation, previousEffect);
+        if (referenceFilter) {
+          effect = referenceFilter->lastEffect();
           // TODO(fs): This is essentially only needed for the
           // side-effects (mapRect). The filter differs from the one
           // computed just above in what the SourceGraphic is, and how
           // it's connected to the filter-chain.
-          referenceOperation.setFilter(
-              buildReferenceFilter(referenceOperation, nullptr));
-          effect = referenceFilter->lastEffect();
+          referenceFilter = buildReferenceFilter(referenceOperation, nullptr);
         }
+        referenceOperation.setFilter(referenceFilter);
         break;
       }
       case FilterOperation::GRAYSCALE: {
@@ -309,7 +310,6 @@ CompositorFilterOperations FilterEffectBuilder::buildFilterOperations(
         Filter* referenceFilter =
             buildReferenceFilter(referenceOperation, nullptr);
         if (referenceFilter && referenceFilter->lastEffect()) {
-          referenceOperation.setFilter(referenceFilter);
           SkiaImageFilterBuilder::populateSourceGraphicImageFilters(
               referenceFilter->getSourceGraphic(), nullptr, currentColorSpace);
 
@@ -318,6 +318,7 @@ CompositorFilterOperations FilterEffectBuilder::buildFilterOperations(
           filters.appendReferenceFilter(
               SkiaImageFilterBuilder::build(filterEffect, currentColorSpace));
         }
+        referenceOperation.setFilter(referenceFilter);
         break;
       }
       case FilterOperation::GRAYSCALE:
