066e41897b33148b35b9641e357083a4add7c995
sky <sky@chromium.org>
2016-10-31 15:14:32 -0700

    Runs most aura tests with both backends
    I'm not updating the gesture detection ones as we may end up doing
    gestures slightly differently for mus.
    
    BUG=659155
    TEST=none
    R=msw@chromium.org
    
    Review-Url: https://codereview.chromium.org/2463823003
    Cr-Commit-Position: refs/heads/master@{#428836}

--- ui/aura/test/aura_test_base.h
+++ ui/aura/test/aura_test_base.h
@@ -28,6 +28,8 @@ class WindowTreeClientDelegate;
 
 namespace test {
 
+enum class BackendType { CLASSIC, MUS };
+
 // A base class for aura unit tests.
 // TODO(beng): Instances of this test will create and own a RootWindow.
 class AuraTestBase : public testing::Test,
@@ -58,6 +60,10 @@ class AuraTestBase : public testing::Test,
 
   // Turns on mus. Must be called before SetUp().
   void EnableMus();
+
+  // Used to configure the backend. This is exposed to make parameterized tests
+  // easy to write. This *must* be called from SetUp().
+  void ConfigureBackend(BackendType type);
 
   void RunAllPendingInMessageLoop();
 
@@ -127,6 +133,22 @@ class AuraTestBase : public testing::Test,
   std::unique_ptr<AuraTestHelper> helper_;
 
   DISALLOW_COPY_AND_ASSIGN(AuraTestBase);
+};
+
+// Use as a base class for tests that want to target both backends.
+class AuraTestBaseWithType : public AuraTestBase,
+                             public ::testing::WithParamInterface<BackendType> {
+ public:
+  AuraTestBaseWithType();
+  ~AuraTestBaseWithType() override;
+
+  // AuraTestBase:
+  void SetUp() override;
+
+ private:
+  bool setup_called_ = false;
+
+  DISALLOW_COPY_AND_ASSIGN(AuraTestBaseWithType);
 };
 
 }  // namespace test
