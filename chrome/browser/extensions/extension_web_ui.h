981bf222e1494f89723c6d2798e3bceba3c971c0
xidachen <xidachen@chromium.org>
2016-10-31 12:26:53 -0700

    Revert of [Extensions] Limit Extension WebUI (patchset #6 id:140001 of https://codereview.chromium.org/2452773002/ )
    Reason for revert:
    Suspect causing failure:
    https://build.chromium.org/p/chromium.win/builders/Win7%20Tests%20%28dbg%29%281%29/builds/54207
    
    Original issue's description:
    > [Extensions] Limit ExtensionWebUI
    >
    > The bookmark manager needs a WebUI controller in order to change the
    > transition type for links. However, right now, we're creating WebUI for
    > every single extension.
    >
    > Instead, *only* create WebUI for the bookmark manager.
    >
    > Note that the ExtensionWebUI class is currently used for both this WebUI
    > and for chrome url overrides, which are very separate concepts and
    > should be split up, but I'll save that for a later CL.
    >
    > BUG=659798
    >
    > Committed: https://crrev.com/f9bc386d7f73366b45cb85848d27b4343facf45d
    > Cr-Commit-Position: refs/heads/master@{#428747}
    
    TBR=dbeam@chromium.org,sky@chromium.org,rdevlin.cronin@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=659798
    
    Review-Url: https://codereview.chromium.org/2463153003
    Cr-Commit-Position: refs/heads/master@{#428766}

--- chrome/browser/extensions/extension_web_ui.h
+++ chrome/browser/extensions/extension_web_ui.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
-#include "base/macros.h"
 #include "chrome/common/extensions/chrome_manifest_url_handlers.h"
 #include "components/favicon_base/favicon_callback.h"
 #include "content/public/browser/web_ui_controller.h"
@@ -33,8 +32,6 @@ class PrefRegistrySyncable;
 // the main tab contents area. For example, each extension can specify an
 // "options_page", and that page is displayed in the tab contents area and is
 // hosted by this class.
-// TODO(devlin): The above description has nothing to do with this class as far
-// as I can tell.
 class ExtensionWebUI : public content::WebUIController {
  public:
   static const char kExtensionURLOverrides[];
@@ -43,15 +40,9 @@ class ExtensionWebUI : public content::WebUIController {
 
   ~ExtensionWebUI() override;
 
-  // Returns true if the given url requires WebUI bindings.
-  static bool NeedsExtensionWebUI(content::BrowserContext* browser_context,
-                                  const GURL& url);
+  virtual extensions::BookmarkManagerPrivateDragEventRouter*
+      bookmark_manager_private_drag_event_router();
 
-  // TODO(devlin): The rest of this class is static methods dealing with
-  // chrome url overrides (e.g. changing chrome://newtab to go to an extension-
-  // provided new tab page). This should be in a separate class from the WebUI
-  // controller for the bookmark manager, and the WebUI controller should be
-  // renamed.
   // BrowserURLHandler
   static bool HandleChromeURLOverride(GURL* url,
                                       content::BrowserContext* browser_context);
@@ -94,11 +85,6 @@ class ExtensionWebUI : public content::WebUIController {
       const GURL& page_url,
       const favicon_base::FaviconResultsCallback& callback);
 
-  extensions::BookmarkManagerPrivateDragEventRouter*
-      bookmark_manager_private_drag_event_router() {
-    return bookmark_manager_private_drag_event_router_.get();
-  }
-
  private:
   // Unregister the specified override, and if it's the currently active one,
   // ensure that something takes its place.
@@ -112,7 +98,8 @@ class ExtensionWebUI : public content::WebUIController {
   std::unique_ptr<extensions::BookmarkManagerPrivateDragEventRouter>
       bookmark_manager_private_drag_event_router_;
 
-  DISALLOW_COPY_AND_ASSIGN(ExtensionWebUI);
+  // The URL this WebUI was created for.
+  GURL url_;
 };
 
 #endif  // CHROME_BROWSER_EXTENSIONS_EXTENSION_WEB_UI_H_
