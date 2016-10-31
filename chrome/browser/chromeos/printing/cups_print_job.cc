c6193d8e867524977a3f2a47a768bd0ab4125716
xdai <xdai@chromium.org>
2016-10-31 13:47:58 -0700

    [CUPS] Implement the Print Job notification.
    - One print job is associated with one notification id. The notification gets updated when the print job status changes.
    - There are four kinds of notifications for the print job: Waiting for printing -> Printing in progress -> Printing Done / Failed.
    - If the print job is cancelled, the notification is removed from the message center.
    - If the notification is closed by the user when showing the printing progress, prevent showing the following printing progress.
    
    BUG=625376, 626752
    
    Review-Url: https://codereview.chromium.org/2435303003
    Cr-Commit-Position: refs/heads/master@{#428798}

---
+++ chrome/browser/chromeos/printing/cups_print_job.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/printing/cups_print_job.h"
+
+#include "base/strings/string_number_conversions.h"
+
+namespace chromeos {
+
+CupsPrintJob::CupsPrintJob(const Printer& printer,
+                           int job_id,
+                           const std::string& document_title,
+                           int total_page_number)
+    : printer_(printer),
+      job_id_(job_id),
+      document_title_(document_title),
+      total_page_number_(total_page_number) {}
+
+CupsPrintJob::~CupsPrintJob() {}
+
+std::string CupsPrintJob::GetUniqueId() const {
+  return printer_.id() + base::IntToString(job_id_);
+}
+
+}  // namespace chromeos
