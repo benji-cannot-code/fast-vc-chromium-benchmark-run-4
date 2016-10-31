f8b02043e0ed98b84261bb01f930c0b54e618fa1
ortuno <ortuno@chromium.org>
2016-10-30 20:45:53 -0700

    bluetooth: Require frame to be connected for startNotifications to succeed.
    If a frame is not connected or disconnects during a startNotifications
    request we reject the startNotifications promise with NetworkError.
    
    BUG=608538
    
    Review-Url: https://codereview.chromium.org/2447593002
    Cr-Commit-Position: refs/heads/master@{#428651}

--- third_party/WebKit/Source/modules/bluetooth/BluetoothRemoteGATTCharacteristic.h
+++ third_party/WebKit/Source/modules/bluetooth/BluetoothRemoteGATTCharacteristic.h
@@ -99,6 +99,7 @@ class BluetoothRemoteGATTCharacteristic final
  private:
   friend class ReadValueCallback;
   friend class WriteValueCallback;
+  friend class NotificationsCallback;
 
   BluetoothRemoteGATTServer* gatt() { return m_service->device()->gatt(); }
 
