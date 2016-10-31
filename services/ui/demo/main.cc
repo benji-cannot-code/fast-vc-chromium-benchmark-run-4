f38ebb99e7e5b34e4b924fdcd466286b8d4ec573
jcivelli <jcivelli@chromium.org>
2016-10-31 13:51:02 -0700

    Unsplit the MUS demo code back into just one service, as it turns out the Android MUS demo can connect to the service instead of calling the demo directly.
    BUG=660623
    
    Review-Url: https://codereview.chromium.org/2459123003
    Cr-Commit-Position: refs/heads/master@{#428801}

--- services/ui/demo/main.cc
+++ services/ui/demo/main.cc
@@ -5,9 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/service_manager/public/c/main.h"
 #include "services/service_manager/public/cpp/service_runner.h"
-#include "services/ui/demo/mus_demo_service.h"
+#include "services/ui/demo/mus_demo.h"
 
 MojoResult ServiceMain(MojoHandle service_request_handle) {
-  service_manager::ServiceRunner runner(new ui::demo::MusDemoService);
+  service_manager::ServiceRunner runner(new ui::demo::MusDemo);
   return runner.Run(service_request_handle);
 }
