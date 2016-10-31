a2a21909f9b0ce050aa1a83497b75849955f770f
gogerald <gogerald@chromium.org>
2016-10-31 11:48:07 -0700

    Remove credit scan scanner experimental flag
    BUG=640430
    
    Review-Url: https://codereview.chromium.org/2452243002
    Cr-Commit-Position: refs/heads/master@{#428761}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -48,7 +48,6 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kNTPOfflinePagesFeature,
     &kPhysicalWebFeature,
     &kPhysicalWebIgnoreOtherClientsFeature,
-    &kScanCardsInWebPayments,
     &kSpecialLocaleFeature,
     &kSpecialLocaleWrapper,
     &kTabReparenting,
@@ -99,9 +98,6 @@ const base::Feature kPhysicalWebFeature{"PhysicalWeb",
 
 const base::Feature kPhysicalWebIgnoreOtherClientsFeature{
     "PhysicalWebIgnoreOtherClients", base::FEATURE_DISABLED_BY_DEFAULT};
-
-const base::Feature kScanCardsInWebPayments{"ScanCardsInWebPayments",
-                                            base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kSpecialLocaleFeature{"SpecialLocale",
                                           base::FEATURE_DISABLED_BY_DEFAULT};
