79824eba14432eadb382d95383043497bcf8714b
xunjieli <xunjieli@chromium.org>
2016-11-01 08:22:00 -0700

    Add a BrotliSourceStream fuzzer
    This CL adds a fuzzer for BrotliSourceStream.
    
    BUG=602424
    
    Review-Url: https://codereview.chromium.org/2463233002
    Cr-Commit-Position: refs/heads/master@{#429004}

--- net/filter/brotli_source_stream_fuzzer.cc
+++ net/filter/brotli_source_stream_fuzzer.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "net/filter/gzip_source_stream.h"
+#include "net/filter/brotli_source_stream.h"
 
 #include "base/logging.h"
 #include "base/memory/ref_counted.h"
@@ -11,8 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/io_buffer.h"
 #include "net/base/test_completion_callback.h"
 #include "net/filter/fuzzed_source_stream.h"
+#include "net/filter/source_stream.h"
 
-// Fuzzer for GzipSourceStream.
+// Fuzzer for BrotliSourceStream.
 //
 // |data| is used to create a FuzzedSourceStream.
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
@@ -20,19 +21,13 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   base::FuzzedDataProvider data_provider(data, size);
   std::unique_ptr<net::FuzzedSourceStream> fuzzed_source_stream(
       new net::FuzzedSourceStream(&data_provider));
-
-  const net::SourceStream::SourceType kGzipTypes[] = {
-      net::SourceStream::TYPE_GZIP, net::SourceStream::TYPE_DEFLATE,
-      net::SourceStream::TYPE_GZIP_FALLBACK};
-  net::SourceStream::SourceType type =
-      data_provider.PickValueInArray(kGzipTypes);
-  std::unique_ptr<net::GzipSourceStream> gzip_stream =
-      net::GzipSourceStream::Create(std::move(fuzzed_source_stream), type);
+  std::unique_ptr<net::SourceStream> brotli_stream =
+      net::CreateBrotliSourceStream(std::move(fuzzed_source_stream));
   while (true) {
     scoped_refptr<net::IOBufferWithSize> io_buffer(
         new net::IOBufferWithSize(64));
-    int result = gzip_stream->Read(io_buffer.get(), io_buffer->size(),
-                                   callback.callback());
+    int result = brotli_stream->Read(io_buffer.get(), io_buffer->size(),
+                                     callback.callback());
     // Releasing the pointer to IOBuffer immediately is more likely to lead to a
     // use-after-free.
     io_buffer = nullptr;
