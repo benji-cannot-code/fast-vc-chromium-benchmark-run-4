1fd94b1e276a1f2bb02ff8fb588ca5b5a8d8bd8f
avi <avi@chromium.org>
2016-11-01 16:29:43 -0700

    Remove stl_util's STLDeleteContainerPointers.
    BUG=555865
    
    Review-Url: https://codereview.chromium.org/2457343002
    Cr-Commit-Position: refs/heads/master@{#429152}

--- base/stl_util.h
+++ base/stl_util.h
@@ -30,24 +30,6 @@ void STLClearObject(T* obj) {
   obj->reserve(0);
 }
 
-// For a range within a container of pointers, calls delete (non-array version)
-// on these pointers.
-// NOTE: for these three functions, we could just implement a DeleteObject
-// functor and then call for_each() on the range and functor, but this
-// requires us to pull in all of algorithm.h, which seems expensive.
-// For hash_[multi]set, it is important that this deletes behind the iterator
-// because the hash_set may call the hash function on the iterator when it is
-// advanced, which could result in the hash function trying to deference a
-// stale pointer.
-template <class ForwardIterator>
-void STLDeleteContainerPointers(ForwardIterator begin, ForwardIterator end) {
-  while (begin != end) {
-    ForwardIterator temp = begin;
-    ++begin;
-    delete *temp;
-  }
-}
-
 // Counts the number of instances of val in a container.
 template <typename Container, typename T>
 typename std::iterator_traits<
@@ -86,7 +68,13 @@ template <class T>
 void STLDeleteElements(T* container) {
   if (!container)
     return;
-  STLDeleteContainerPointers(container->begin(), container->end());
+
+  for (auto it = container->begin(); it != container->end();) {
+    auto temp = it;
+    ++it;
+    delete *temp;
+  }
+
   container->clear();
 }
 
@@ -98,8 +86,7 @@ void STLDeleteValues(T* container) {
   if (!container)
     return;
 
-  auto it = container->begin();
-  while (it != container->end()) {
+  for (auto it = container->begin(); it != container->end();) {
     auto temp = it;
     ++it;
     delete temp->second;
