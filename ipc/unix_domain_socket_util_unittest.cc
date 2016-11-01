5f8b30ffead5ba978fee303f4bd7a20038375d01
krasin <krasin@chromium.org>
2016-11-01 13:15:22 -0700

    Fix undefined behavior in UnixDomainSocketUtil.Connect test.
    After SocketAcceptor is destructed, the thread might still have been
    attached to it, and an invalid cast on the already destructed object
    was performed.
    
    This bug was found under UBSan.
    
    BUG=660174
    
    Review-Url: https://codereview.chromium.org/2468593002
    Cr-Commit-Position: refs/heads/master@{#429085}

--- ipc/unix_domain_socket_util_unittest.cc
+++ ipc/unix_domain_socket_util_unittest.cc
@@ -32,6 +32,9 @@ class SocketAcceptor : public base::MessageLoopForIO::Watcher {
         started_watching_event_(
             base::WaitableEvent::ResetPolicy::AUTOMATIC,
             base::WaitableEvent::InitialState::NOT_SIGNALED),
+        stopped_watching_event_(
+            base::WaitableEvent::ResetPolicy::AUTOMATIC,
+            base::WaitableEvent::InitialState::NOT_SIGNALED),
         accepted_event_(base::WaitableEvent::ResetPolicy::AUTOMATIC,
                         base::WaitableEvent::InitialState::NOT_SIGNALED) {
     target_thread->PostTask(FROM_HERE,
@@ -56,7 +59,8 @@ class SocketAcceptor : public base::MessageLoopForIO::Watcher {
     if (watcher_.get()) {
       target_thread_->PostTask(FROM_HERE,
           base::Bind(&SocketAcceptor::StopWatching, base::Unretained(this),
-              watcher_.release()));
+                     watcher_.release()));
+      stopped_watching_event_.Wait();
     }
   }
 
@@ -70,6 +74,7 @@ class SocketAcceptor : public base::MessageLoopForIO::Watcher {
   void StopWatching(base::MessageLoopForIO::FileDescriptorWatcher* watcher) {
     watcher->StopWatchingFileDescriptor();
     delete watcher;
+    stopped_watching_event_.Signal();
   }
   void OnFileCanReadWithoutBlocking(int fd) override {
     ASSERT_EQ(-1, server_fd_);
@@ -83,6 +88,7 @@ class SocketAcceptor : public base::MessageLoopForIO::Watcher {
   base::SingleThreadTaskRunner* target_thread_;
   std::unique_ptr<base::MessageLoopForIO::FileDescriptorWatcher> watcher_;
   base::WaitableEvent started_watching_event_;
+  base::WaitableEvent stopped_watching_event_;
   base::WaitableEvent accepted_event_;
 
   DISALLOW_COPY_AND_ASSIGN(SocketAcceptor);
