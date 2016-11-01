93ad851753c50f5b1f73a5227b7083d57d6805e1
limasdf <limasdf@gmail.com>
2016-10-31 20:33:27 -0700

    Remove deprecated extension ready notification from background content service. Use ExtensionSystem::Get(browser_context)->ready().Post() instead.
    BUG=411566
    
    Review-Url: https://codereview.chromium.org/2050163002
    Cr-Commit-Position: refs/heads/master@{#428927}

--- chrome/browser/background/background_contents_service_factory.cc
+++ chrome/browser/background/background_contents_service_factory.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "chrome/browser/background/background_contents_service.h"
+#include "chrome/browser/extensions/extension_system_factory.h"
 #include "chrome/browser/profiles/incognito_helpers.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/pref_names.h"
@@ -33,6 +34,7 @@ BackgroundContentsServiceFactory::BackgroundContentsServiceFactory()
         "BackgroundContentsService",
         BrowserContextDependencyManager::GetInstance()) {
   DependsOn(extensions::ExtensionRegistryFactory::GetInstance());
+  DependsOn(extensions::ExtensionSystemFactory::GetInstance());
 }
 
 BackgroundContentsServiceFactory::~BackgroundContentsServiceFactory() {
