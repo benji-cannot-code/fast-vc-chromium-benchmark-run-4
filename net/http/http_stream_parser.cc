c986492d7136da1675f6243a15b1cb1de842c76b
hans <hans@chromium.org>
2016-10-31 17:28:19 -0700

    Fix a redundant expression
    Found by static analysis with PVS-Studio!
    
    BUG=660198
    
    Review-Url: https://codereview.chromium.org/2448193007
    Cr-Commit-Position: refs/heads/master@{#428882}

--- net/http/http_stream_parser.cc
+++ net/http/http_stream_parser.cc
@@ -1232,7 +1232,7 @@ void HttpStreamParser::ValidateStatusLine(const std::string& status_line) {
 
 bool HttpStreamParser::SendRequestBuffersEmpty() {
   return request_headers_ == nullptr && request_body_send_buf_ == nullptr &&
-         request_body_send_buf_ == nullptr;
+         request_body_read_buf_ == nullptr;
 }
 
 }  // namespace net
