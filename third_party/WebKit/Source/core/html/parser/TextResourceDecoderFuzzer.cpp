a1f64588a50cadab0ff6900b70bc463e31e47722
kouhei <kouhei@chromium.org>
2016-11-01 05:22:56 -0700

    TextResourceDecoderFuzzer should use blink::InitializeBlinkFuzzTest
    Before this CL, TextResourceDecoder was run on uninitialized Blink platform.
    This CL ensures Blink is initialized by using blink::InitializeBlinkFuzzTest.
    
    BUG=659280
    
    Review-Url: https://codereview.chromium.org/2465213002
    Cr-Commit-Position: refs/heads/master@{#428986}

--- third_party/WebKit/Source/core/html/parser/TextResourceDecoderFuzzer.cpp
+++ third_party/WebKit/Source/core/html/parser/TextResourceDecoderFuzzer.cpp
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/html/parser/TextResourceDecoderForFuzzing.h"
 
+#include "platform/testing/BlinkFuzzerTestSupport.h"
 #include "platform/testing/FuzzedDataProvider.h"
-#include "platform/testing/TestingPlatformSupport.h"
 #include <algorithm>
 
 namespace blink {
@@ -27,11 +27,6 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 }
 
 extern "C" int LLVMFuzzerInitialize(int* argc, char*** argv) {
-  // Intentional leak - no need to do cleanup as explained in
-  // "Initialization/Cleanup" section of testing/libfuzzer/efficient_fuzzer.md
-  DEFINE_STATIC_LOCAL(blink::ScopedUnittestsEnvironmentSetup, testSetup,
-                      (*argc, *argv));
-  ALLOW_UNUSED_LOCAL(testSetup);
-
+  blink::InitializeBlinkFuzzTest(argc, argv);
   return 0;
 }
