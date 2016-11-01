e4c4f7fa9f79a0145f06823889431aba7a77890e
sebsg <sebsg@chromium.org>
2016-11-01 09:48:26 -0700

    [Payments] Don't show select when clicking in billing address drop.
    BUG=660207
    
    Review-Url: https://codereview.chromium.org/2462473002
    Cr-Commit-Position: refs/heads/master@{#429023}

--- chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestNoShippingTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestNoShippingTest.java
@@ -26,7 +26,7 @@ import java.util.concurrent.TimeoutException;
 public class PaymentRequestNoShippingTest extends PaymentRequestTestBase {
     private static final int DECEMBER = 11;
     private static final int NEXT_YEAR = 1;
-    private static final int FIRST_BILLING_ADDRESS = 1;
+    private static final int FIRST_BILLING_ADDRESS = 0;
 
     public PaymentRequestNoShippingTest() {
         super("payment_request_no_shipping_test.html");
@@ -187,7 +187,7 @@ public class PaymentRequestNoShippingTest extends PaymentRequestTestBase {
         // dropdown.
         int december = 11;
         int nextYear = 1;
-        int addBillingAddress = 2;
+        int addBillingAddress = 1;
         setSpinnerSelectionsInCardEditorAndWait(new int[] {december, nextYear, addBillingAddress},
                 mReadyToEdit);
 
