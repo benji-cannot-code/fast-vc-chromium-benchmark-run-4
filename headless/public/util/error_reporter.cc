875ff7d445329b89b0614b1ab68757148f28098e
skyostil <skyostil@chromium.org>
2016-11-01 09:33:48 -0700

    headless: Only report protocol parsing errors when DCHECK is enabled
    Only report verbose protocol parsing errors when DCHECK is enabled. This
    reduces the release binary size on Linux by about 77k.
    
    BUG=612904
    
    Review-Url: https://codereview.chromium.org/2466923002
    Cr-Commit-Position: refs/heads/master@{#429017}

--- headless/public/util/error_reporter.cc
+++ headless/public/util/error_reporter.cc
@@ -15,6 +15,7 @@ ErrorReporter::ErrorReporter() {}
 
 ErrorReporter::~ErrorReporter() {}
 
+#if DCHECK_IS_ON()
 void ErrorReporter::Push() {
   path_.push_back(nullptr);
 }
@@ -48,5 +49,6 @@ void ErrorReporter::AddError(base::StringPiece description) {
 bool ErrorReporter::HasErrors() const {
   return !errors_.empty();
 }
+#endif  // DCHECK_IS_ON()
 
 }  // namespace headless
--- headless/public/util/error_reporter.h
+++ headless/public/util/error_reporter.h
@@ -14,12 +14,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace headless {
 
-// Tracks errors which are encountered while parsing client API types.
+// Tracks errors which are encountered while parsing client API types. Note that
+// errors are only reported in debug builds (i.e., when DCHECK is enabled).
 class HEADLESS_EXPORT ErrorReporter {
  public:
   ErrorReporter();
   ~ErrorReporter();
 
+#if DCHECK_IS_ON()
   // Enter a new nested parsing context. It will initially have a null name.
   void Push();
 
@@ -38,6 +40,13 @@ class HEADLESS_EXPORT ErrorReporter {
 
   // Returns a list of reported errors.
   const std::vector<std::string>& errors() const { return errors_; }
+#else  // DCHECK_IS_ON()
+  inline void Push() {}
+  inline void Pop() {}
+  inline void SetName(const char* name) {}
+  inline void AddError(base::StringPiece description) {}
+  inline bool HasErrors() const { return false; }
+#endif  // DCHECK_IS_ON()
 
  private:
   std::vector<const char*> path_;
