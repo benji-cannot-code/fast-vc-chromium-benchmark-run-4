11a75e6ed0ba7b99966b266c5499d59ae6c79357
pfeldman <pfeldman@chromium.org>
2016-11-01 16:56:25 -0700

    DevTools: clang format all the source + clang config landed (2)
    BUG=659389
    NOPRESUBMIT=true
    NOTRY=true
    TBR=chenwilliam
    
    Review-Url: https://codereview.chromium.org/2464283002
    Cr-Commit-Position: refs/heads/master@{#429157}

--- third_party/WebKit/Source/devtools/.eslintrc.js
+++ third_party/WebKit/Source/devtools/.eslintrc.js
@@ -23,7 +23,6 @@ module.exports = {
 
 
         // syntax preferences
-        // "indent": [2, 2, { "SwitchCase": 1, "CallExpression": {"arguments": 2}, "MemberExpression": 2 }],
         "quotes": [2, "single", {
             "avoidEscape": true,
             "allowTemplateLiterals": true
@@ -54,7 +53,6 @@ module.exports = {
         "no-shadow-restricted-names": 2,
 
         // es2015 features
-        // "no-useless-constructor": 2,
         "require-yield": 2,
         "template-curly-spacing": [2, "never"],
 
@@ -89,6 +87,8 @@ module.exports = {
         /**
          * Disabled, aspirational rules
          */
+
+        "indent": [0, 2, { "SwitchCase": 1, "CallExpression": {"arguments": 2}, "MemberExpression": 2 }],
 
         // brace-style is disabled, as eslint cannot enforce 1tbs as default, but allman for functions
         "brace-style": [0, "allman", { "allowSingleLine": true }],
