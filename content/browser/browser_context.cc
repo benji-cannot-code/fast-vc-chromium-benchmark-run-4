186ce81bfa8d1fca7c9988e3a2cfa8885fad3a4d
blundell <blundell@chromium.org>
2016-10-31 18:34:30 -0700

    Introduce Device Service
    This CL introduces the Device Service into //services/device along
    with the infrastructure required in //content for the renderer to
    connect to it and the browser to host it as an embedded service.
    
    The service is currently empty. A followup CL will move the first
    device-related service to be host in the Device Service instead of in
    //content/browser.
    
    BUG=612328
    
    Review-Url: https://codereview.chromium.org/2451413003
    Cr-Commit-Position: refs/heads/master@{#428904}

--- content/browser/browser_context.cc
+++ content/browser/browser_context.cc
@@ -40,6 +40,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/ssl/channel_id_store.h"
 #include "net/url_request/url_request_context.h"
 #include "net/url_request/url_request_context_getter.h"
+#include "services/device/device_service.h"
+#include "services/device/public/cpp/constants.h"
 #include "services/file/file_service.h"
 #include "services/file/public/cpp/constants.h"
 #include "services/file/user_id_map.h"
@@ -458,6 +460,13 @@ void BrowserContext::Initialize(
     connection->Start();
 
     // New embedded service factories should be added to |connection| here.
+    // TODO(blundell): Does this belong as a global service rather than per
+    // BrowserContext?
+    ServiceInfo info;
+    info.factory =
+        base::Bind(&device::CreateDeviceService,
+                   BrowserThread::GetTaskRunnerForThread(BrowserThread::FILE));
+    connection->AddEmbeddedService(device::kDeviceServiceName, info);
 
     if (base::CommandLine::ForCurrentProcess()->HasSwitch(
             switches::kMojoLocalStorage)) {
