88505859f2d49608385980a0c2f39a977f0518d1
robhogan <robhogan@gmail.com>
2016-11-01 14:07:40 -0700

    Percent height content should respect the height of its containing cell
    Another go at the collection of CLs reverted in https://codereview.chromium.org/2309893002.
    
    https://drafts.csswg.org/css-tables-3/#row-layout now says:
    
    "For the purpose of calculating this height, descendants of table cells whose
     height depends on percentages of their parent cell' height are considered to
    have an auto height if they have overflow set to *visible or hidden* or if
    they are replaced elements, and a 0px height if they have not."
    
    So introduce that behaviour. We also need to account for intrinsic padding and
    collapsed border values stored on cells when collecting their border and padding
    values.
    
    BUG=637811,627305,623026,465096,468699
    
    Review-Url: https://codereview.chromium.org/2441373002
    Cr-Commit-Position: refs/heads/master@{#429109}

--- third_party/WebKit/Source/core/layout/LayoutBox.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBox.cpp
@@ -1365,7 +1365,7 @@ void LayoutBox::clearExtraInlineAndBlockOffests() {
 
 LayoutUnit LayoutBox::adjustBorderBoxLogicalWidthForBoxSizing(
     float width) const {
-  LayoutUnit bordersPlusPadding = borderAndPaddingLogicalWidth();
+  LayoutUnit bordersPlusPadding = collapsedBorderAndCSSPaddingLogicalWidth();
   LayoutUnit result(width);
   if (style()->boxSizing() == BoxSizingContentBox)
     return result + bordersPlusPadding;
@@ -1374,7 +1374,7 @@ LayoutUnit LayoutBox::adjustBorderBoxLogicalWidthForBoxSizing(
 
 LayoutUnit LayoutBox::adjustBorderBoxLogicalHeightForBoxSizing(
     float height) const {
-  LayoutUnit bordersPlusPadding = borderAndPaddingLogicalHeight();
+  LayoutUnit bordersPlusPadding = collapsedBorderAndCSSPaddingLogicalHeight();
   LayoutUnit result(height);
   if (style()->boxSizing() == BoxSizingContentBox)
     return result + bordersPlusPadding;
@@ -1385,7 +1385,7 @@ LayoutUnit LayoutBox::adjustContentBoxLogicalWidthForBoxSizing(
     float width) const {
   LayoutUnit result(width);
   if (style()->boxSizing() == BoxSizingBorderBox)
-    result -= borderAndPaddingLogicalWidth();
+    result -= collapsedBorderAndCSSPaddingLogicalWidth();
   return std::max(LayoutUnit(), result);
 }
 
@@ -1393,7 +1393,7 @@ LayoutUnit LayoutBox::adjustContentBoxLogicalHeightForBoxSizing(
     float height) const {
   LayoutUnit result(height);
   if (style()->boxSizing() == BoxSizingBorderBox)
-    result -= borderAndPaddingLogicalHeight();
+    result -= collapsedBorderAndCSSPaddingLogicalHeight();
   return std::max(LayoutUnit(), result);
 }
 
@@ -3203,7 +3203,8 @@ LayoutUnit LayoutBox::computePercentageLogicalHeight(
         // cases, but it is preferable to the alternative (sizing intrinsically
         // and making the row end up too big).
         LayoutTableCell* cell = toLayoutTableCell(cb);
-        if (scrollsOverflowY() &&
+        if (style()->overflowY() != OverflowVisible &&
+            style()->overflowY() != OverflowHidden &&
             (!cell->style()->logicalHeight().isAuto() ||
              !cell->table()->style()->logicalHeight().isAuto()))
           return LayoutUnit();
