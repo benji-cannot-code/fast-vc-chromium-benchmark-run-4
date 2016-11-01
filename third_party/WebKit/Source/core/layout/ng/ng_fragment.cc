04136e14882d2fa3ace9fe115995001a81fcc6d9
glebl <glebl@chromium.org>
2016-10-31 18:38:25 -0700

    Use NGLogicalRect instead of NGExclusion to work with exclusions.
    This patch does the following:
    - Delete NGExclusion
    - Change the code to use NGLogicalRect to store exclusions
    - Make NGPhysicalConstraintSpace to own the list of exclusions through Vector<std::unique_ptr>
    - Introduce WRITING_MODE_IGNORED macros that can be used to annotate cases where the writing mode is inored
    
    BUG=635619
    
    Review-Url: https://codereview.chromium.org/2457013004
    Cr-Commit-Position: refs/heads/master@{#428907}

--- third_party/WebKit/Source/core/layout/ng/ng_fragment.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_fragment.cc
@@ -5,14 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/layout/ng/ng_fragment.h"
 
+#include "core/layout/ng/ng_macros.h"
 #include "core/layout/ng/ng_physical_fragment.h"
 
 namespace blink {
 
 NGMarginStrut NGFragment::MarginStrut() const {
-  // NOTE: Accessing the margin strut ignoring the writing mode here is fine.
-  // Changing the writing mode establishes a new formatting context, for which
-  // a margin strut is never set for a fragment.
+  WRITING_MODE_IGNORED(
+      "Accessing the margin strut ignoring the writing mode here is fine."
+      "A margin strut is never set for a fragment");
   return toNGPhysicalFragment(physical_fragment_)->MarginStrut();
 }
 
