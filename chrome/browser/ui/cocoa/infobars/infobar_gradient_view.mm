e2ff3fa7512d5794363343a1cba1e5fec4dad8c9
kjellander <kjellander@chromium.org>
2016-11-01 06:31:09 -0700

    Reland of Some more pre-material cleanups (patchset #1 id:1 of https://codereview.chromium.org/2464063002/ )
    Reason for revert:
    Relanding as newer builds experienced the same flakiness: https://build.chromium.org/p/chromium.mac/builders/Mac10.10%20Tests/builds/8533
    
    Original issue's description:
    > Revert of Some more pre-material cleanups (patchset #4 id:60001 of https://codereview.chromium.org/2444923003/ )
    >
    > Reason for revert:
    > Speculative revert since being the only commit in the first of a long series of flaky failures in interactive_ui_tests on Mac10.10 Tests: https://build.chromium.org/p/chromium.mac/builders/Mac10.10%20Tests/builds/8428
    >
    > Original issue's description:
    > > Some more pre-material cleanups
    > >
    > > BUG=648281
    > >
    > > Committed: https://crrev.com/3d5fe31add4e90541a5164216f9a16b6cf0c7086
    > > Cr-Commit-Position: refs/heads/master@{#428603}
    >
    > TBR=groby@chromium.org,pkasting@chromium.org,sadrul@chromium.org,estade@chromium.org
    > # Not skipping CQ checks because original CL landed more than 1 days ago.
    > BUG=648281
    >
    > Committed: https://crrev.com/b828ed6e000cbe757ca8f1aa2da2bfa3c42b960a
    > Cr-Commit-Position: refs/heads/master@{#428969}
    
    TBR=groby@chromium.org,pkasting@chromium.org,sadrul@chromium.org,estade@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=648281
    
    Review-Url: https://codereview.chromium.org/2466343002
    Cr-Commit-Position: refs/heads/master@{#428990}

--- chrome/browser/ui/cocoa/infobars/infobar_gradient_view.mm
+++ chrome/browser/ui/cocoa/infobars/infobar_gradient_view.mm
@@ -61,7 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)drawRect:(NSRect)rect {
   NSRect bounds = [self bounds];
-  bounds.size.height = InfoBarContainerDelegate::kDefaultBarTargetHeightMd;
+  bounds.size.height = InfoBarContainerDelegate::kDefaultBarTargetHeight;
   CGFloat tipXOffset = arrowX_ - arrowHalfWidth_;
 
   // Around the bounds of the infobar, continue drawing the path into which the
