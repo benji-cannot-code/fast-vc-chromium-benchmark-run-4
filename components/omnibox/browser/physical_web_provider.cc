a77fde95c686480df7a35ab15c83109ed30686f7
mattreynolds <mattreynolds@chromium.org>
2016-11-01 11:56:17 -0700

    Record UMA when a Physical Web suggestion is included in omnibox results
    BUG=630769
    
    Review-Url: https://codereview.chromium.org/2435853002
    Cr-Commit-Position: refs/heads/master@{#429063}

--- components/omnibox/browser/physical_web_provider.cc
+++ components/omnibox/browser/physical_web_provider.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/memory/ptr_util.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
 #include "components/omnibox/browser/autocomplete_provider_client.h"
@@ -124,7 +125,7 @@ void PhysicalWebProvider::ConstructMatches(base::ListValue* metadata_list) {
     const size_t remaining_metadata = metadata_count - i;
     if ((remaining_slots == 1) && (remaining_metadata > remaining_slots)) {
       AppendOverflowItem(remaining_metadata, relevance, title);
-      return;
+      break;
     }
 
     GURL url(url_string);
@@ -152,6 +153,9 @@ void PhysicalWebProvider::ConstructMatches(base::ListValue* metadata_list) {
     matches_.push_back(match);
     ++used_slots;
   }
+
+  UMA_HISTOGRAM_EXACT_LINEAR(
+      "Omnibox.PhysicalWebProviderMatches", matches_.size(), kMaxMatches);
 }
 
 void PhysicalWebProvider::AppendOverflowItem(int additional_url_count,
