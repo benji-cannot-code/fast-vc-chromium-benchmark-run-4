7b3674d44c3c6d7b167241b141ab0625e6407c2d
cfroussios <cfroussios@chromium.org>
2016-11-02 13:01:19 -0700

    Fix inconsistency between included headers and calls.
    The call to OSCryptMockerLinux was guarded by a different precompiler
    condition than the inclusion of its header.
    
    BUG=661693
    
    Review-Url: https://codereview.chromium.org/2475503002
    Cr-Commit-Position: refs/heads/master@{#429377}

--- components/os_crypt/os_crypt_mocker.cc
+++ components/os_crypt/os_crypt_mocker.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/os_crypt/os_crypt.h"
 
-#if defined(USE_LIBSECRET)
+#if defined(USE_LIBSECRET) || defined(USE_KEYRING) || defined(USE_KWALLET)
 #include "components/os_crypt/os_crypt_mocker_linux.h"
 #endif
 
