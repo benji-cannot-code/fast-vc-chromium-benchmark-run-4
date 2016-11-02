e5ddf93607fea8fc2428d87b75eaeb46a0aa3ca6
bruthig <bruthig@chromium.org>
2016-11-01 18:08:48 -0700

    Applied Material Design layout to TrayItemMore system menu rows.
    This change applies the new common system menu layout to all rows that
    inherit from the TrayItemMore class.  This includes the following rows:
    
    - SMS default row
    - Bluetooth default row
    - Cast default row
    - Accessibility default row
    - IME default row
    - Network default row
    - Display rotation lock default row
    - VPN default row
    
    TBR=jamescook@chromium.org
    BUG=657669
    
    Review-Url: https://codereview.chromium.org/2468533002
    Cr-Commit-Position: refs/heads/master@{#429171}

--- ash/common/system/tray/tray_item_more.h
+++ ash/common/system/tray/tray_item_more.h
@@ -55,7 +55,6 @@ class TrayItemMore : public ActionableView {
   bool PerformAction(const ui::Event& event) override;
 
   // Overridden from views::View.
-  void Layout() override;
   void GetAccessibleState(ui::AXViewState* state) override;
   void OnNativeThemeChanged(const ui::NativeTheme* theme) override;
 
