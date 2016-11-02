2225f6f7b552ae763df4ca4dff24e159d697e8b3
meacer <meacer@chromium.org>
2016-11-01 18:47:38 -0700

    Open ChromeOS clock settings on the UI thread instead of File.
    ChromeOS clock settings code accesses BrowserProcessImpl::profile_manager()
    which should be accessed on the UI thread. Since this code is simply opening
    a new window, there is no need to run it on a different thread.
    
    BUG=657704
    TEST=Manual, change system clock, go to https://expired.badssl.com and launch clock settings.
    
    Review-Url: https://codereview.chromium.org/2459193003
    Cr-Commit-Position: refs/heads/master@{#429182}

--- chrome/browser/interstitials/chrome_controller_client.cc
+++ chrome/browser/interstitials/chrome_controller_client.cc
@@ -42,21 +42,18 @@ using content::Referrer;
 
 namespace {
 
-void LaunchDateAndTimeSettingsOnFile() {
+#if !defined(OS_CHROMEOS)
+void LaunchDateAndTimeSettingsOnFileThread() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::FILE);
 // The code for each OS is completely separate, in order to avoid bugs like
-// https://crbug.com/430877 .
+// https://crbug.com/430877 . ChromeOS is handled on the UI thread.
 #if defined(OS_ANDROID)
   chrome::android::OpenDateAndTimeSettings();
 
-#elif defined(OS_CHROMEOS)
-  chrome::ShowSettingsSubPageForProfile(ProfileManager::GetActiveUserProfile(),
-                                        chrome::kDateTimeSubPage);
-
 #elif defined(OS_LINUX)
   struct ClockCommand {
-    const char* pathname;
-    const char* argument;
+    const char* const pathname;
+    const char* const argument;
   };
   static const ClockCommand kClockCommands[] = {
       // Unity
@@ -120,6 +117,7 @@ void LaunchDateAndTimeSettingsOnFile() {
 #endif
   // Don't add code here! (See the comment at the beginning of the function.)
 }
+#endif
 
 }  // namespace
 
@@ -147,9 +145,16 @@ bool ChromeControllerClient::CanLaunchDateAndTimeSettings() {
 }
 
 void ChromeControllerClient::LaunchDateAndTimeSettings() {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+
+#if defined(OS_CHROMEOS)
+  chrome::ShowSettingsSubPageForProfile(ProfileManager::GetActiveUserProfile(),
+                                        chrome::kDateTimeSubPage);
+#else
   content::BrowserThread::PostTask(
       content::BrowserThread::FILE, FROM_HERE,
-      base::Bind(&LaunchDateAndTimeSettingsOnFile));
+      base::Bind(&LaunchDateAndTimeSettingsOnFileThread));
+#endif
 }
 
 void ChromeControllerClient::GoBack() {
