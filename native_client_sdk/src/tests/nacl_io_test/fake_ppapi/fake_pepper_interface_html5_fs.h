1457c3f45e19f75646e60922284f70e3b62f7a92
chanpatorikku <chanpatorikku@gmail.com>
2016-11-01 19:30:18 -0700

    [NaCl SDK] Refactor FakeFileSystemInterface.
    Move FakeFileSystemInterface from fake_pepper_interface_html5_fs.{cc,h} to
    fake_file_system_interface.{cc,h} to share the code with the future code.
    depot_tools/clang-format was run so the formatting of code is accepted
    in the code review by policy. It was run in all the {.cc,.h} files,
    and the code formats of some files were changed.
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_nacl_sdk;master.tryserver.chromium.mac:mac_nacl_sdk;master.tryserver.chromium.win:win_nacl_sdk
    
    Review-Url: https://codereview.chromium.org/2455973003
    Cr-Commit-Position: refs/heads/master@{#429186}

--- native_client_sdk/src/tests/nacl_io_test/fake_ppapi/fake_pepper_interface_html5_fs.h
+++ native_client_sdk/src/tests/nacl_io_test/fake_ppapi/fake_pepper_interface_html5_fs.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "fake_ppapi/fake_core_interface.h"
 #include "fake_ppapi/fake_file_io_interface.h"
 #include "fake_ppapi/fake_file_ref_interface.h"
+#include "fake_ppapi/fake_file_system_interface.h"
 #include "fake_ppapi/fake_var_interface.h"
 #include "fake_ppapi/fake_var_manager.h"
 #include "nacl_io/pepper_interface_dummy.h"
@@ -96,22 +97,6 @@ class FakeHtml5FsFilesystem {
   typedef std::map<Path, FakeHtml5FsNode> NodeMap;
   NodeMap node_map_;
   PP_FileSystemType filesystem_type_;
-};
-
-class FakeFileSystemInterface : public nacl_io::FileSystemInterface {
- public:
-  FakeFileSystemInterface(FakeCoreInterface* core_interface);
-
-  virtual PP_Bool IsFileSystem(PP_Resource resource);
-  virtual PP_Resource Create(PP_Instance instance, PP_FileSystemType type);
-  virtual int32_t Open(PP_Resource file_system,
-                       int64_t expected_size,
-                       PP_CompletionCallback callback);
-
- private:
-  FakeCoreInterface* core_interface_;  // Weak reference.
-
-  DISALLOW_COPY_AND_ASSIGN(FakeFileSystemInterface);
 };
 
 class FakePepperInterfaceHtml5Fs : public nacl_io::PepperInterfaceDummy {
