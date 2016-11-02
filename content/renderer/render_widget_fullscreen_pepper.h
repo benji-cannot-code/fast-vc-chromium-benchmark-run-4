e7c34e8e91d0f27682a91337c885ff30f58e27d0
bmcquade <bmcquade@chromium.org>
2016-11-02 10:50:51 -0700

    Remove dead paint flush tracking code.
    In https://codereview.chromium.org/2437863005, page_load_histograms,
    which was the only consumer of this code, was removed. Now that the
    page_load_histograms code has been removed, this code can be removed as
    well.
    
    BUG=657993
    
    Review-Url: https://codereview.chromium.org/2448543002
    Cr-Commit-Position: refs/heads/master@{#429329}

--- content/renderer/render_widget_fullscreen_pepper.h
+++ content/renderer/render_widget_fullscreen_pepper.h
@@ -65,7 +65,6 @@ class RenderWidgetFullscreenPepper : public RenderWidgetFullscreen,
 
   // RenderWidget API.
   void DidInitiatePaint() override;
-  void DidFlushPaint() override;
   void Close() override;
   void OnResize(const ResizeParams& params) override;
 
