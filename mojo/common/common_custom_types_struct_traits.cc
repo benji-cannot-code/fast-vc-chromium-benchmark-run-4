55f6217df0a41cac5928363947639964a64c7c2e
yzshen <yzshen@chromium.org>
2016-11-02 13:54:04 -0700

    Mojo C++ bindings: some improvements for String16 struct traits.
    - map nullable String16 to WTF::String (instead of WTF::Optional<WTF::String>);
    - avoid unnecessary copies and null checks;
    - add one more test case.
    
    BUG=653209
    
    Review-Url: https://codereview.chromium.org/2464793004
    Cr-Commit-Position: refs/heads/master@{#429397}

--- mojo/common/common_custom_types_struct_traits.cc
+++ mojo/common/common_custom_types_struct_traits.cc
@@ -10,35 +10,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace mojo {
 
 // static
-mojo::ConstCArray<uint16_t>
-StructTraits<mojo::common::mojom::String16DataView, base::string16>::data(
-    const base::string16& str) {
-  return mojo::ConstCArray<uint16_t>(
-      str.size(), reinterpret_cast<const uint16_t*>(str.data()));
-}
-
-// static
-bool StructTraits<mojo::common::mojom::String16DataView, base::string16>::Read(
-    mojo::common::mojom::String16DataView data,
+bool StructTraits<common::mojom::String16DataView, base::string16>::Read(
+    common::mojom::String16DataView data,
     base::string16* out) {
-  mojo::ArrayDataView<uint16_t> view;
+  ArrayDataView<uint16_t> view;
   data.GetDataDataView(&view);
-  if (view.is_null())
-    return false;
   out->assign(reinterpret_cast<const base::char16*>(view.data()), view.size());
   return true;
 }
 
 // static
 const std::vector<uint32_t>&
-StructTraits<mojo::common::mojom::VersionDataView, base::Version>::components(
+StructTraits<common::mojom::VersionDataView, base::Version>::components(
     const base::Version& version) {
   return version.components();
 }
 
 // static
-bool StructTraits<mojo::common::mojom::VersionDataView, base::Version>::Read(
-    mojo::common::mojom::VersionDataView data,
+bool StructTraits<common::mojom::VersionDataView, base::Version>::Read(
+    common::mojom::VersionDataView data,
     base::Version* out) {
   std::vector<uint32_t> components;
   if (!data.ReadComponents(&components))
@@ -49,10 +39,10 @@ bool StructTraits<mojo::common::mojom::VersionDataView, base::Version>::Read(
 }
 
 // static
-bool StructTraits<mojo::common::mojom::UnguessableTokenDataView,
-                  base::UnguessableToken>::
-    Read(mojo::common::mojom::UnguessableTokenDataView data,
-         base::UnguessableToken* out) {
+bool StructTraits<
+    common::mojom::UnguessableTokenDataView,
+    base::UnguessableToken>::Read(common::mojom::UnguessableTokenDataView data,
+                                  base::UnguessableToken* out) {
   uint64_t high = data.high();
   uint64_t low = data.low();
 
