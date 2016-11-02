7c5a70c135287a576fe4b43acf9d6da47b5bcab5
ekaramad <ekaramad@chromium.org>
2016-11-02 12:42:44 -0700

    Fix an invalid cast from RenderWidgetHostViewGuest to RenderWidgetHostViewMac
    The method GetRenderWidgetHostViewToUse() picks the guest WebContents for the
    fullscreen page to be used for start/stopSpeaking commands in Mac. The guest
    WebContents's RenderWidgetHostView is however is incorrectly assumed to be
    RenderWidgetHostViewMac. It is actually a RenderWidgetHostViewGuest.
    
    Moreover, since the method SpeakSelection() is implemented for
    RenderWidgetHostView there is indeed no need to further cast the view to
    RenderWidgetHostViewMac/Guest at all.
    
    BUG=661329
    
    Review-Url: https://codereview.chromium.org/2473443003
    Cr-Commit-Position: refs/heads/master@{#429368}

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -98,6 +98,7 @@ using content::RenderFrameHost;
 using content::RenderViewHost;
 using content::RenderViewHostImpl;
 using content::RenderWidgetHostImpl;
+using content::RenderWidgetHostView;
 using content::RenderWidgetHostViewMac;
 using content::RenderWidgetHostViewMacEditCommandHelper;
 using content::TextInputClientMac;
@@ -119,7 +120,7 @@ BOOL EventIsReservedBySystem(NSEvent* event) {
   return helper->map()->IsEventReserved(event);
 }
 
-RenderWidgetHostViewMac* GetRenderWidgetHostViewToUse(
+RenderWidgetHostView* GetRenderWidgetHostViewToUse(
     RenderWidgetHostViewMac* render_widget_host_view) {
   WebContents* web_contents = render_widget_host_view->GetWebContents();
   if (!web_contents)
@@ -132,8 +133,7 @@ RenderWidgetHostViewMac* GetRenderWidgetHostViewToUse(
       guest_manager->GetFullPageGuest(web_contents);
   if (!guest)
     return render_widget_host_view;
-  return static_cast<RenderWidgetHostViewMac*>(
-      guest->GetRenderWidgetHostView());
+  return guest->GetRenderWidgetHostView();
 }
 
 }  // namespace
