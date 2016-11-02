15c3a4b29f396425738c5d1e7ece844c22ffcb0f
pdr <pdr@chromium.org>
2016-11-01 22:24:29 -0700

    Refactor LayoutView paint offset updates out of FrameView update code
    Root layer scrolls lets us treat LayoutView like other LayoutObjects
    and lets us move property updates into the regular LayoutObject update
    code. This patch removes a special-case for LayoutView in the FrameView
    property update. Comments have been added describing the LayoutView
    context updates for fixed and absolute cases.
    
    Additionally, LayoutView tests have been unskipped to ensure this
    does not regress.
    
    BUG=645667
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2464823003
    Cr-Commit-Position: refs/heads/master@{#429213}

--- third_party/WebKit/Source/core/paint/PrePaintTreeWalk.cpp
+++ third_party/WebKit/Source/core/paint/PrePaintTreeWalk.cpp
@@ -42,7 +42,7 @@ void PrePaintTreeWalk::walk(FrameView& frameView,
     return;
 
   PrePaintTreeWalkContext localContext(context);
-  m_propertyTreeBuilder.updatePropertiesAndContext(
+  m_propertyTreeBuilder.updateFramePropertiesAndContext(
       frameView, localContext.treeBuilderContext);
 
   if (RuntimeEnabledFeatures::slimmingPaintInvalidationEnabled()) {
