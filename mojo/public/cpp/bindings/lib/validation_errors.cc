885413dfcf975df7c717753663bc89f6ee114af3
rockot <rockot@chromium.org>
2016-11-02 13:45:54 -0700

    Mojo: Suppress validation error logging in some tests
    These errors are useful for developers when developers make mistakes,
    but they make a mess of test logs. Disable them in tests which are
    particularly noisy.
    
    BUG=659195
    
    Review-Url: https://codereview.chromium.org/2448203002
    Cr-Commit-Position: refs/heads/master@{#429393}

--- mojo/public/cpp/bindings/lib/validation_errors.cc
+++ mojo/public/cpp/bindings/lib/validation_errors.cc
@@ -15,6 +15,7 @@ namespace {
 ValidationErrorObserverForTesting* g_validation_error_observer = nullptr;
 SerializationWarningObserverForTesting* g_serialization_warning_observer =
     nullptr;
+bool g_suppress_logging = false;
 
 }  // namespace
 
@@ -72,8 +73,10 @@ void ReportValidationError(ValidationContext* context,
   }
 
   if (description) {
-    LOG(ERROR) << "Invalid message: " << ValidationErrorToString(error) << " ("
-               << description << ")";
+    if (!g_suppress_logging) {
+      LOG(ERROR) << "Invalid message: " << ValidationErrorToString(error)
+                 << " (" << description << ")";
+    }
     if (context->message()) {
       context->message()->NotifyBadMessage(
           base::StringPrintf("Validation failed for %s [%s (%s)]",
@@ -81,7 +84,8 @@ void ReportValidationError(ValidationContext* context,
                              ValidationErrorToString(error), description));
     }
   } else {
-    LOG(ERROR) << "Invalid message: " << ValidationErrorToString(error);
+    if (!g_suppress_logging)
+      LOG(ERROR) << "Invalid message: " << ValidationErrorToString(error);
     if (context->message()) {
       context->message()->NotifyBadMessage(
           base::StringPrintf("Validation failed for %s [%s]",
@@ -100,6 +104,17 @@ void ReportValidationErrorForMessage(
       message->handles()->size(), message,
       description);
   ReportValidationError(&validation_context, error);
+}
+
+ScopedSuppressValidationErrorLoggingForTests
+    ::ScopedSuppressValidationErrorLoggingForTests()
+    : was_suppressed_(g_suppress_logging) {
+  g_suppress_logging = true;
+}
+
+ScopedSuppressValidationErrorLoggingForTests
+    ::~ScopedSuppressValidationErrorLoggingForTests() {
+  g_suppress_logging = was_suppressed_;
 }
 
 ValidationErrorObserverForTesting::ValidationErrorObserverForTesting(
