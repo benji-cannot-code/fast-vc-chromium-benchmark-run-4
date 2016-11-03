873424223f6aaa5b96a718fdb75d61b49a1a8aa9
skyostil <skyostil@chromium.org>
2016-11-03 07:13:30 -0700

    headless: Librarify Headless Shell
    Turn the Headless Shell application into a library so it can be embedded
    by other targets (e.g., Chrome). This moves the shell into headless
    namespace and adds exports new function for the main shell entrypoint,
    which is intented to be called from main() when the Headless Shell
    should be started.
    
    BUG=612904
    
    Review-Url: https://codereview.chromium.org/2470233003
    Cr-Commit-Position: refs/heads/master@{#429572}

---
+++ headless/app/headless_shell_main.cc
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "headless/public/headless_shell.h"
+
+int main(int argc, const char** argv) {
+  return headless::HeadlessShellMain(argc, argv);
+}
