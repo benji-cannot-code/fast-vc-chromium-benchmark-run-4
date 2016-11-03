c56b58ae9b6b63d6c279a446c1135ec052364026
michaelbai <michaelbai@chromium.org>
2016-11-03 10:55:00 -0700

    Upstream Context.BIND_EXTERNAL_SERVICE
    BUG=592722
    
    Review-Url: https://codereview.chromium.org/2464143004
    Cr-Commit-Position: refs/heads/master@{#429642}

--- content/public/android/javatests/src/org/chromium/content/browser/ChildProcessLauncherTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/ChildProcessLauncherTest.java
@@ -337,8 +337,7 @@ public class ChildProcessLauncherTest extends InstrumentationTestCase {
     }
 
     private ChildProcessCreationParams getDefaultChildProcessCreationParams(String packageName) {
-        return new ChildProcessCreationParams(packageName, 0,
-                LibraryProcessType.PROCESS_CHILD);
+        return new ChildProcessCreationParams(packageName, LibraryProcessType.PROCESS_CHILD);
     }
 
     private void triggerConnectionSetup(ChildProcessConnectionImpl connection) {
