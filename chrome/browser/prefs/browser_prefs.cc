cd57a8a10a0b12a7189d4bb80106058faf31bf9a
khmel <khmel@chromium.org>
2016-11-03 09:54:45 -0700

    Implement local storage for App List in case app sync is off.
    This CL keeps local copy of current app list service and in
    case app sync is off, date from local storage is used to
    init app list model. Once sync is resumed, local copy is
    overriden and information form sync is used.
    
    BUG=641535
    TEST=Unit and sync tests added
    TEST=Manually on device.
    
    TBR=reviewer@chromium.org
    Reland: http://crrev.com/2416133002
    (cherry picked from commit f06c029695e6d3a62ea950c1ba0f60cf01c353a8)
    
    Review-Url: https://codereview.chromium.org/2416133002
    Review-Url: https://codereview.chromium.org/2468413005
    Cr-Original-Commit-Position: refs/heads/master@{#427390}
    Cr-Commit-Position: refs/heads/master@{#429621}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -111,6 +111,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(ENABLE_APP_LIST)
 #include "chrome/browser/apps/drive/drive_app_mapping.h"
+#include "chrome/browser/ui/app_list/app_list_syncable_service.h"
 #endif
 
 #if BUILDFLAG(ENABLE_BACKGROUND)
@@ -567,6 +568,7 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   DevToolsWindow::RegisterProfilePrefs(registry);
 #if BUILDFLAG(ENABLE_APP_LIST)
   DriveAppMapping::RegisterProfilePrefs(registry);
+  app_list::AppListSyncableService::RegisterProfilePrefs(registry);
 #endif
   extensions::CommandService::RegisterProfilePrefs(registry);
   extensions::ExtensionSettingsHandler::RegisterProfilePrefs(registry);
