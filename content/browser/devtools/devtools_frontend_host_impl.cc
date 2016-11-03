e4cad364f5df17be3312208981603fcb6b17b23e
lushnikov <lushnikov@chromium.org>
2016-11-03 10:36:30 -0700

    DevTools: rename devtools.js into devtools_compatibility.js
    File "devtools.js" gets injected in the front-end by the embedder with
    the actual name "devtools_compatibility.js". The latter is a much
    better name for the file.
    
    This patch also sets a sourceURL for the injected script so that
    the devtools_compatibility.js file actually gets mapped by
    automapping.
    
    BUG=none
    R=dgozman
    
    Review-Url: https://codereview.chromium.org/2471363002
    Cr-Commit-Position: refs/heads/master@{#429634}

--- content/browser/devtools/devtools_frontend_host_impl.cc
+++ content/browser/devtools/devtools_frontend_host_impl.cc
@@ -18,7 +18,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 namespace {
-const char kCompatibilityScript[] = "devtools.js";
+const char kCompatibilityScript[] = "devtools_compatibility.js";
+const char kCompatibilityScriptSourceURL[] =
+    "\n//# "
+    "sourceURL=chrome-devtools://devtools/bundled/devtools_compatibility.js";
 }
 
 // static
@@ -49,8 +52,9 @@ DevToolsFrontendHostImpl::DevToolsFrontendHostImpl(
       handle_message_callback_(handle_message_callback) {
   frontend_main_frame->Send(new DevToolsMsg_SetupDevToolsClient(
       frontend_main_frame->GetRoutingID(),
-      DevToolsFrontendHost::GetFrontendResource(
-          kCompatibilityScript).as_string()));
+      DevToolsFrontendHost::GetFrontendResource(kCompatibilityScript)
+              .as_string() +
+          kCompatibilityScriptSourceURL));
 }
 
 DevToolsFrontendHostImpl::~DevToolsFrontendHostImpl() {
