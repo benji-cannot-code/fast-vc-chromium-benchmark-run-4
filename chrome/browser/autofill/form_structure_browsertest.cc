b7bfb4de94f6e53aad9512a56f83933b1346a6e8
mathp <mathp@chromium.org>
2016-11-03 07:48:40 -0700

    [Autofill] Disable output logs for FormStructureBrowserTest
    We can't realistically control the chattiness of arbitrary websites without
    impacting the material under test.
    
    The new output is something like
    
    Note: Google Test filter = AllForms/FormStructureBrowserTest.DataDrivenHeuristics/7
    [==========] Running 1 test from 1 test case.
    [----------] Global test environment set-up.
    [----------] 1 test from AllForms/FormStructureBrowserTest
    [ RUN      ] AllForms/FormStructureBrowserTest.DataDrivenHeuristics/7
    [100261:100292:1103/100357:ERROR:entry.cc(168)] Entry::Deserialize: dictionary has no interface_provider_specs key
    [100261:100292:1103/100357:ERROR:entry.cc(168)] Entry::Deserialize: dictionary has no interface_provider_specs key
    [1:2:1103/100357:WARNING:persistent_histogram_allocator.cc(502)] Creating the results-histogram inside persistent memory can cause future allocations to crash if that memory is ever released (for testing).
    [       OK ] AllForms/FormStructureBrowserTest.DataDrivenHeuristics/7 (4501 ms)
    [----------] 1 test from AllForms/FormStructureBrowserTest (4502 ms total)
    
    [----------] Global test environment tear-down
    [==========] 1 test from 1 test case ran. (4504 ms total)
    [  PASSED  ] 1 test.
    
    which is reasonable.
    
    BUG=634418
    TEST=FormStructureBrowserTest
    
    Review-Url: https://codereview.chromium.org/2474933002
    Cr-Commit-Position: refs/heads/master@{#429582}

--- chrome/browser/autofill/form_structure_browsertest.cc
+++ chrome/browser/autofill/form_structure_browsertest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <vector>
 
+#include "base/command_line.h"
 #include "base/files/file_enumerator.h"
 #include "base/files/file_path.h"
 #include "base/macros.h"
@@ -23,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/autofill_manager.h"
 #include "components/autofill/core/browser/data_driven_test.h"
 #include "components/autofill/core/browser/form_structure.h"
+#include "content/public/common/content_switches.h"
 #include "url/gurl.h"
 
 #if defined(OS_MACOSX)
@@ -81,6 +83,13 @@ class FormStructureBrowserTest
  protected:
   FormStructureBrowserTest();
   ~FormStructureBrowserTest() override;
+
+  // InProcessBrowserTest
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    // Suppress most output logs because we can't really control the output for
+    // arbitrary test sites.
+    command_line->AppendSwitch(switches::kDisableLogging);
+  }
 
   // DataDrivenTest:
   void GenerateResults(const std::string& input, std::string* output) override;
