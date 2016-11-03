441e74c7687d322c09c266a328ed16de02762cb5
kozyatinskiy <kozyatinskiy@chromium.org>
2016-11-03 06:53:56 -0700

    [DevTools] migrate DOMDebugger, Input, DeviceOrientation, Tracing to new style
    BUG=none
    R=dgozman@chromium.org
    TBR=timvolodine@chromium.org
    
    Review-Url: https://codereview.chromium.org/2474743002
    Cr-Commit-Position: refs/heads/master@{#429565}

--- third_party/WebKit/Source/modules/device_orientation/DeviceOrientationInspectorAgent.cpp
+++ third_party/WebKit/Source/modules/device_orientation/DeviceOrientationInspectorAgent.cpp
@@ -42,8 +42,7 @@ DeviceOrientationController& DeviceOrientationInspectorAgent::controller() {
       *m_page->deprecatedLocalMainFrame()->document());
 }
 
-void DeviceOrientationInspectorAgent::setDeviceOrientationOverride(
-    ErrorString* error,
+Response DeviceOrientationInspectorAgent::setDeviceOrientationOverride(
     double alpha,
     double beta,
     double gamma) {
@@ -54,19 +53,21 @@ void DeviceOrientationInspectorAgent::setDeviceOrientationOverride(
   m_state->setDouble(DeviceOrientationInspectorAgentState::gamma, gamma);
   controller().setOverride(
       DeviceOrientationData::create(alpha, beta, gamma, false));
+  return Response::OK();
 }
 
-void DeviceOrientationInspectorAgent::clearDeviceOrientationOverride(
-    ErrorString* error) {
+Response DeviceOrientationInspectorAgent::clearDeviceOrientationOverride() {
   m_state->setBoolean(DeviceOrientationInspectorAgentState::overrideEnabled,
                       false);
   controller().clearOverride();
+  return Response::OK();
 }
 
-void DeviceOrientationInspectorAgent::disable(ErrorString*) {
+Response DeviceOrientationInspectorAgent::disable() {
   m_state->setBoolean(DeviceOrientationInspectorAgentState::overrideEnabled,
                       false);
   controller().clearOverride();
+  return Response::OK();
 }
 
 void DeviceOrientationInspectorAgent::restore() {
