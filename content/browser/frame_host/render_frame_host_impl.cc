a3800f0abf4b9168d0c488bb83fe3bc763496fc8
mathp <mathp@chromium.org>
2016-11-03 09:02:54 -0700

    Revert of [sensors] Ambient light sensor implementation for ChromeOS and Linux. (patchset #26 id:1050001 of https://codereview.chromium.org/2370343002/ )
    Reason for revert:
    Causing a failure on Linux bot:
    
    https://build.chromium.org/p/chromium/builders/Linux%20x64/builds/28030
    
    Original issue's description:
    > [sensors] Ambient light sensor implementation for ChromeOS and Linux.
    >
    > The patch introduces sensors implementation for
    > ChromeOS/Linux platforms using ambient light sensor. Both
    > platforms share the same code and require polling threads to
    > be used.
    >
    > SensorDataIio structure is used to initialize a generic
    > SensorReader, which is created only when sensor read files
    > are found, and to create a concrete sensor, which takes
    > an ownership of the SensorReader.
    >
    > A SensorReader must always be created on a polling thread,
    > which is further used by a sensor to poll data. Each new
    > sensor will have its own thread in order to avoid blocking of
    > each other.
    >
    > As a temp solution to manage polling threads, which are not
    > passed to new sensors, a manager thread is used. It kills a
    > polling thread if a sensor cannot be created. In the future,
    > the manager thread will evolve to a manager class that will
    > manage finding new sensors attached to a system and notify a
    > provider about that. The provider will have its own cache in
    > order to avoid trying to find sensors each time after it
    > fails to find a requested sensor. Once the manager notifies
    > the provider about a new sensor, the provider updates its
    > cache and starts to process requests for that type of sensor.
    >
    > Intent to Implement:
    > https://groups.google.com/a/chromium.org/forum/#!msg/blink-dev/TkfdVqYAYiE/xLGN2b1-AAAJ
    >
    > BUG=606766
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    >
    > Committed: https://crrev.com/de2d07084492be68633a47bae161cf9014ac88e2
    > Cr-Commit-Position: refs/heads/master@{#429584}
    
    TBR=reillyg@chromium.org,alexander.shalamov@intel.com,alexis.menard@intel.com,alexmos@chromium.org,mikhail.pozdnyakov@intel.com,rockot@chromium.org,timvolodine@chromium.org,maksim.sisov@intel.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=606766
    
    Review-Url: https://codereview.chromium.org/2468283003
    Cr-Commit-Position: refs/heads/master@{#429604}

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -2213,8 +2213,7 @@ void RenderFrameHostImpl::RegisterMojoInterfaces() {
 #endif
   if (base::FeatureList::IsEnabled(features::kGenericSensor)) {
     GetInterfaceRegistry()->AddInterface(
-        base::Bind(&device::SensorProviderImpl::Create,
-                   BrowserThread::GetTaskRunnerForThread(BrowserThread::FILE)),
+        base::Bind(&device::SensorProviderImpl::Create),
         BrowserThread::GetTaskRunnerForThread(BrowserThread::IO));
   }
 
