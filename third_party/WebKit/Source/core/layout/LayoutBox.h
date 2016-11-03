aaa1a6b187d38932bc63c5c6faaa1a50cd3b48e0
mstensho <mstensho@opera.com>
2016-11-03 09:20:45 -0700

    logicalHeightWithVisibleOverflow() needs to include overhanging floats.
    Otherwise we might end up skipping layout of blocks that contain floats
    that really need to be relaid out.
    
    We get some marking for layout for free in layoutBlockFlow(), if
    pageLogicalHeightChanged, but that only goes one level deep.
    
    Review-Url: https://codereview.chromium.org/2474883002
    Cr-Commit-Position: refs/heads/master@{#429611}

--- third_party/WebKit/Source/core/layout/LayoutBox.h
+++ third_party/WebKit/Source/core/layout/LayoutBox.h
@@ -255,7 +255,7 @@ class CORE_EXPORT LayoutBox : public LayoutBoxModelObject {
 
   // Logical height of the object, including content overflowing the
   // border-after edge.
-  LayoutUnit logicalHeightWithVisibleOverflow() const;
+  virtual LayoutUnit logicalHeightWithVisibleOverflow() const;
 
   LayoutUnit constrainLogicalWidthByMinMax(LayoutUnit,
                                            LayoutUnit,
