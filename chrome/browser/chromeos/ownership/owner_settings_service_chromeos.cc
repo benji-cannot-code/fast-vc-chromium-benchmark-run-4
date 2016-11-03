7071d5793a0bde5f22c7ab341037a65a5b597961
erg <erg@chromium.org>
2016-11-03 12:46:33 -0700

    chromeos: remove obsolete includes.
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2475703002
    Cr-Commit-Position: refs/heads/master@{#429675}

--- chrome/browser/chromeos/ownership/owner_settings_service_chromeos.cc
+++ chrome/browser/chromeos/ownership/owner_settings_service_chromeos.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chrome/browser/chromeos/settings/device_settings_provider.h"
-#include "chrome/browser/chromeos/settings/session_manager_operation.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chromeos/chromeos_switches.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
