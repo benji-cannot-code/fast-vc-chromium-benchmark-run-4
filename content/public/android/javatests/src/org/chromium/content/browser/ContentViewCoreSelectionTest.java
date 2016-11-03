61211c8f80ea7dc65b50ee4b4295489dcd4da17a
amaralp <amaralp@chromium.org>
2016-11-03 10:46:07 -0700

    Re-enabling tests that seem to not be flaky
    These tests seem to not be flaky after crrev.com/2468043002 landed. I've
    run each of these tests 400+ times locally on my Nexus 6P (Android M) and
    ~40 times on my Nexus 4 (Android K) and had no failures.
    
    BUG=592428
    
    Review-Url: https://codereview.chromium.org/2471283003
    Cr-Commit-Position: refs/heads/master@{#429640}

--- content/public/android/javatests/src/org/chromium/content/browser/ContentViewCoreSelectionTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/ContentViewCoreSelectionTest.java
@@ -146,11 +146,8 @@ public class ContentViewCoreSelectionTest extends ContentShellTestBase {
         waitForPastePopupStatus(false);
     }
 
-    /*
     @SmallTest
     @Feature({"TextInput"})
-    */
-    @DisabledTest(message = "https://crbug.com/592428")
     public void testPastePopupClearedOnTappingEmptyInput() throws Throwable {
         copyStringToClipboard("SampleTextToCopy");
         DOMUtils.longPressNode(this, mContentViewCore, "empty_input_text");
@@ -159,11 +156,8 @@ public class ContentViewCoreSelectionTest extends ContentShellTestBase {
         waitForPastePopupStatus(false);
     }
 
-    /*
     @SmallTest
     @Feature({"TextInput"})
-    */
-    @DisabledTest(message = "https://crbug.com/592428")
     public void testPastePopupClearedOnTappingNonEmptyInput() throws Throwable {
         copyStringToClipboard("SampleTextToCopy");
         DOMUtils.longPressNode(this, mContentViewCore, "empty_input_text");
@@ -172,11 +166,8 @@ public class ContentViewCoreSelectionTest extends ContentShellTestBase {
         waitForPastePopupStatus(false);
     }
 
-    /*
     @SmallTest
     @Feature({"TextInput"})
-    */
-    @DisabledTest(message = "https://crbug.com/592428")
     public void testPastePopupClearedOnTappingOutsideInput() throws Throwable {
         copyStringToClipboard("SampleTextToCopy");
         DOMUtils.longPressNode(this, mContentViewCore, "empty_input_text");
@@ -185,11 +176,8 @@ public class ContentViewCoreSelectionTest extends ContentShellTestBase {
         waitForPastePopupStatus(false);
     }
 
-    /*
     @SmallTest
     @Feature({"TextInput"})
-    */
-    @DisabledTest(message = "https://crbug.com/592428")
     public void testPastePopupClearedOnLongPressingOutsideInput() throws Throwable {
         copyStringToClipboard("SampleTextToCopy");
         DOMUtils.longPressNode(this, mContentViewCore, "empty_input_text");
@@ -198,19 +186,16 @@ public class ContentViewCoreSelectionTest extends ContentShellTestBase {
         waitForPastePopupStatus(false);
     }
 
-    /*
     @SmallTest
     @Feature({"TextInput"})
-    */
-    @DisabledTest(message = "https://crbug.com/592428")
     public void testPastePopupNotShownOnLongPressingDisabledInput() throws Throwable {
         copyStringToClipboard("SampleTextToCopy");
         DOMUtils.longPressNode(this, mContentViewCore, "empty_input_text");
         waitForPastePopupStatus(true);
-        assertTrue(mContentViewCore.hasInsertion());
+        waitForInsertion(true);
         DOMUtils.longPressNode(this, mContentViewCore, "disabled_text");
         waitForPastePopupStatus(false);
-        assertFalse(mContentViewCore.hasInsertion());
+        waitForInsertion(false);
     }
 
     /*
@@ -738,6 +723,15 @@ public class ContentViewCoreSelectionTest extends ContentShellTestBase {
             @Override
             public Boolean call() {
                 return mContentViewCore.isPastePopupShowing();
+            }
+        }));
+    }
+
+    private void waitForInsertion(final boolean show) throws InterruptedException {
+        CriteriaHelper.pollUiThread(Criteria.equals(show, new Callable<Boolean>() {
+            @Override
+            public Boolean call() {
+                return mContentViewCore.hasInsertion();
             }
         }));
     }
