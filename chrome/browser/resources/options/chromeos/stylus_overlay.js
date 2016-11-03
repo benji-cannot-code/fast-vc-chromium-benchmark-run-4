ae3026f74a7f4bce4141e273590cfafc4028913f
jdufault <jdufault@chromium.org>
2016-11-03 11:07:54 -0700

    Disable auto-show palette pref UI when show stylus tools pref is false.
    BUG=658986
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2464123002
    Cr-Commit-Position: refs/heads/master@{#429649}

--- chrome/browser/resources/options/chromeos/stylus_overlay.js
+++ chrome/browser/resources/options/chromeos/stylus_overlay.js
@@ -11,7 +11,7 @@ cr.define('options', function() {
    */
   function StylusOverlay() {
     options.SettingsDialog.call(this, 'stylus-overlay',
-         loadTimeData.getString('stylusOverlayTabTitle'),
+        loadTimeData.getString('stylusOverlayTabTitle'),
         'stylus-overlay',
         assertInstanceof($('stylus-confirm'), HTMLButtonElement),
         assertInstanceof($('stylus-cancel'), HTMLButtonElement));
@@ -25,6 +25,12 @@ cr.define('options', function() {
     /** @override */
     initializePage: function() {
       options.SettingsDialog.prototype.initializePage.call(this);
+
+      // Disable auto-launch pref when enable stylus tools pref is false.
+      Preferences.getInstance().addEventListener('settings.enable_stylus_tools',
+          function(e) {
+            $('launch-palette-on-eject-input').disabled = !e.value.value;
+          });
     },
   };
 
