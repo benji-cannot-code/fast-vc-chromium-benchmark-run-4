f1da2fe0b190f38c72177f66726d2bc1c0226429
lukasza <lukasza@chromium.org>
2016-11-02 17:21:43 -0700

    Initial browser tests for OOPIF support for drag-n-drop.
    This CL adds a single test - dragging and dropping a piece of text from
    outside of the browser into a cross-site frame.  The test expectedly
    fails for now in --site-per-process mode (the CL adds an exception
    in site-per-process.content_browsertests.filter).
    
    DOMMessageQueue
    ===============
    
    The new test verifies whether appropriate javascript events have fired.
    This is done by DOMMessageQueue, which has been tweaked slightly to
    target only a specified WebContents and detect renderer crashes.
    
    The modified DOMMessageQueue makes the old DOMOperationObserver
    obsolete, so the CL deletes the old class and switches its users
    to use DOMMessageQueue instead.
    
    DomAutomationController::SendMsg was refusing to serialize and send
    javascript objects.  This restriction has been lifted (any object
    type can be sent, as long as it can be serialized to JSON).
    
    Simulation of drag-and-drop
    ===========================
    
    The CL introduces a DragAndDropSimulator that helps with simulating
    drag-and-drop events for WebContents.  The simulator supports only
    the minimum functionality required to enable the first OOPIF test:
    
    - Currently the simulator only supports drag-enter (dragging something
      from outside the browser into WebContenst) and drop events.
    
    - Currently the simulator only works when WebContents is built on top
      of aura UI abstraction/platform.
    
    Test pages
    ==========
    
    The CL provides a main test page (content/test/data/drag_and_drop/page.html)
    which enables:
    
    - Easy locating of subframes (which are positioned at fixed coordinates).
    
    - Easy replacing of subframe contents, which are specified by URL query.
    
      - This helps with testing various drag-and-drop scenarios (e.g. using
        different subframes for testing drop with drop_target.html OR for
        testing drag-start with image_source.html).
    
      - This also helps exercise various OOPIF scenarios (e.g. using
        subframes that are same-site or cross-site).
    
    BUG=647249
    
    Review-Url: https://codereview.chromium.org/2453693003
    Cr-Commit-Position: refs/heads/master@{#429472}

---
+++ content/test/data/drag_and_drop/event_monitoring.js
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/* Reports an event to DragAndDropBrowserTest and  DOMDragEventWaiter */
+window.reportDragAndDropEvent = function(ev) {
+  function safe(f) {
+    try {
+      return f();
+    } catch(err) {
+      return "exception: " + err.message;
+    }
+  }
+
+  console.log("got event: " + ev.type);
+
+  if (window.domAutomationController) {
+    window.domAutomationController.setAutomationId(0);
+    window.domAutomationController.send({
+      drop_effect: safe(function() { return ev.dataTransfer.dropEffect; }),
+      effect_allowed: safe(function() {
+        return ev.dataTransfer.effectAllowed;
+      }),
+      event_type: ev.type,
+      file_names: safe(function() {
+        return Array
+            .from(ev.dataTransfer.files)
+            .map(function(file) { return file.name; })
+            .sort().join();
+      }),
+      mime_types: safe(function() {
+        return Array.from(ev.dataTransfer.types).sort().join();
+      }),
+      window_name: window.name
+    });
+  }
+}
