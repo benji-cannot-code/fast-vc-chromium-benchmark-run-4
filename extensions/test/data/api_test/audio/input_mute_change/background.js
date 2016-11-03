1c46192ff6eb0fa563fd558320ca09753357e570
tbarzic <tbarzic@chromium.org>
2016-11-03 12:59:41 -0700

    Refactor audio API tests
    Instead of using custom messages to send test reuslts from test
    app to test runner for tests for API events, use standard
    extensions::ResultCatcher to get results - it workss better with
    chrome.test.assert methods and produces better failure messages.
    
    Use chrome.test.listenOnce to register event listeners in tests.
    Note that there is no need for chrome.test.succeed for test cases
    using listenOnce - it will get called once listener is run (unless
    listener function asserts, in which case test will fail).
    
    Refactors audio/test.js - added helper methods for testing
    relevant properties of devices returned by chrome.audio.getInfo to
    make expectations a bit easier to specify/understand.
    The refactored test are less dependent on exact order in which
    audio devices are reported in device list (as the order is of no
    relevance).
    
    Review-Url: https://codereview.chromium.org/2462833002
    Cr-Commit-Position: refs/heads/master@{#429678}

--- extensions/test/data/api_test/audio/input_mute_change/background.js
+++ extensions/test/data/api_test/audio/input_mute_change/background.js
@@ -3,13 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-chrome.audio.OnMuteChanged.addListener(function (is_input, is_muted) {
-  if (is_input === true && is_muted === true) {
-    chrome.test.sendMessage("success");
-  } else {
-    console.error("Got unexpected OnMuteChanged event is_input:" + is_input +
-                  " is_muted:" + is_muted);
-    chrome.test.sendMessage("failure");
+chrome.test.runTests([
+  function waitForMuteChangedEventTests() {
+    chrome.test.listenOnce(
+        chrome.audio.OnMuteChanged,
+        function(isInput, isMuted) {
+          chrome.test.assertTrue(isInput);
+          chrome.test.assertTrue(isMuted);
+        });
   }
-});
-chrome.test.sendMessage("loaded");
+]);
+
+chrome.test.sendMessage('loaded');
--- extensions/test/data/api_test/audio/output_mute_change/background.js
+++ extensions/test/data/api_test/audio/output_mute_change/background.js
@@ -3,13 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-chrome.audio.OnMuteChanged.addListener(function (is_input, is_muted) {
-  if (is_input === false && is_muted === false) {
-    chrome.test.sendMessage("success");
-  } else {
-    console.error("Got unexpected OnMuteChanged event is_input:" + is_input +
-                  " is_muted:" + is_muted);
-    chrome.test.sendMessage("failure");
+chrome.test.runTests([
+  function waitForMuteChangedEventTests() {
+    chrome.test.listenOnce(
+        chrome.audio.OnMuteChanged,
+        function(isInput, isMuted) {
+          chrome.test.assertFalse(isInput);
+          chrome.test.assertFalse(isMuted);
+        });
   }
-});
-chrome.test.sendMessage("loaded");
+]);
+
+chrome.test.sendMessage('loaded');
