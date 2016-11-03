a6dccb7f2b74154eb506e75b27739329c3823668
alph <alph@chromium.org>
2016-11-02 17:42:46 -0700

    DevTools: Refactoring: remove unrestricted from certain timeline classes.
    Review-Url: https://codereview.chromium.org/2469023003
    Cr-Commit-Position: refs/heads/master@{#429484}

--- third_party/WebKit/Source/devtools/front_end/timeline_model/TimelineModel.js
+++ third_party/WebKit/Source/devtools/front_end/timeline_model/TimelineModel.js
@@ -1371,9 +1371,6 @@ WebInspector.TimelineModel.NetworkRequest = class {
   }
 };
 
-/**
- * @unrestricted
- */
 WebInspector.TimelineModel.Filter = class {
   /**
    * @param {!WebInspector.TracingModel.Event} event
@@ -1384,9 +1381,6 @@ WebInspector.TimelineModel.Filter = class {
   }
 };
 
-/**
- * @unrestricted
- */
 WebInspector.TimelineVisibleEventsFilter = class extends WebInspector.TimelineModel.Filter {
   /**
    * @param {!Array.<string>} visibleTypes
@@ -1406,9 +1400,6 @@ WebInspector.TimelineVisibleEventsFilter = class extends WebInspector.TimelineMo
   }
 };
 
-/**
- * @unrestricted
- */
 WebInspector.ExclusiveNameFilter = class extends WebInspector.TimelineModel.Filter {
   /**
    * @param {!Array<string>} excludeNames
@@ -1428,9 +1419,6 @@ WebInspector.ExclusiveNameFilter = class extends WebInspector.TimelineModel.Filt
   }
 };
 
-/**
- * @unrestricted
- */
 WebInspector.ExcludeTopLevelFilter = class extends WebInspector.TimelineModel.Filter {
   constructor() {
     super();
@@ -1502,11 +1490,13 @@ WebInspector.InvalidationTrackingEvent = class {
 /** @typedef {{reason: string, stackTrace: ?Array<!Protocol.Runtime.CallFrame>}} */
 WebInspector.InvalidationCause;
 
-/**
- * @unrestricted
- */
 WebInspector.InvalidationTracker = class {
   constructor() {
+    /** @type {?WebInspector.TracingModel.Event} */
+    this._lastRecalcStyle = null;
+    /** @type {?WebInspector.TracingModel.Event} */
+    this._lastPaintWithLayer = null;
+    this._didPaint = false;
     this._initializePerFrameState();
   }
 
@@ -1650,7 +1640,6 @@ WebInspector.InvalidationTracker = class {
   _addSyntheticStyleRecalcInvalidation(baseEvent, styleInvalidatorInvalidation) {
     var invalidation = new WebInspector.InvalidationTrackingEvent(baseEvent);
     invalidation.type = WebInspector.TimelineModel.RecordType.StyleRecalcInvalidationTracking;
-    invalidation.synthetic = true;
     if (styleInvalidatorInvalidation.cause.reason)
       invalidation.cause.reason = styleInvalidatorInvalidation.cause.reason;
     if (styleInvalidatorInvalidation.selectorPart)
@@ -1750,8 +1739,8 @@ WebInspector.InvalidationTracker = class {
     /** @type {!Object.<number, !Array.<!WebInspector.InvalidationTrackingEvent>>} */
     this._invalidationsByNodeId = {};
 
-    this._lastRecalcStyle = undefined;
-    this._lastPaintWithLayer = undefined;
+    this._lastRecalcStyle = null;
+    this._lastPaintWithLayer = null;
     this._didPaint = false;
   }
 };
