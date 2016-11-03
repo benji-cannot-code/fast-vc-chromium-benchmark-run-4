31b6d523379125b258e6759e9373410d653695b5
Yoonjae.Cho92 <Yoonjae.Cho92@gmail.com>
2016-11-03 10:22:52 -0700

    Replace for each loop
    BUG=655950
    
    Review-Url: https://codereview.chromium.org/2470273003
    Cr-Commit-Position: refs/heads/master@{#429627}

--- net/log/net_log_util.cc
+++ net/log/net_log_util.cc
@@ -159,8 +159,8 @@ std::unique_ptr<base::DictionaryValue> GetNetConstants() {
   {
     std::unique_ptr<base::DictionaryValue> dict(new base::DictionaryValue());
 
-    for (size_t i = 0; i < arraysize(kCertStatusFlags); i++)
-      dict->SetInteger(kCertStatusFlags[i].name, kCertStatusFlags[i].constant);
+    for (const auto& flag : kCertStatusFlags)
+      dict->SetInteger(flag.name, flag.constant);
 
     constants_dict->Set("certStatusFlag", std::move(dict));
   }
@@ -170,8 +170,8 @@ std::unique_ptr<base::DictionaryValue> GetNetConstants() {
   {
     std::unique_ptr<base::DictionaryValue> dict(new base::DictionaryValue());
 
-    for (size_t i = 0; i < arraysize(kLoadFlags); i++)
-      dict->SetInteger(kLoadFlags[i].name, kLoadFlags[i].constant);
+    for (const auto& flag : kLoadFlags)
+      dict->SetInteger(flag.name, flag.constant);
 
     constants_dict->Set("loadFlag", std::move(dict));
   }
@@ -181,8 +181,8 @@ std::unique_ptr<base::DictionaryValue> GetNetConstants() {
   {
     std::unique_ptr<base::DictionaryValue> dict(new base::DictionaryValue());
 
-    for (size_t i = 0; i < arraysize(kLoadStateTable); i++)
-      dict->SetInteger(kLoadStateTable[i].name, kLoadStateTable[i].constant);
+    for (const auto& state : kLoadStateTable)
+      dict->SetInteger(state.name, state.constant);
 
     constants_dict->Set("loadState", std::move(dict));
   }
@@ -201,8 +201,8 @@ std::unique_ptr<base::DictionaryValue> GetNetConstants() {
   {
     std::unique_ptr<base::DictionaryValue> dict(new base::DictionaryValue());
 
-    for (size_t i = 0; i < arraysize(kNetErrors); i++)
-      dict->SetInteger(ErrorToShortString(kNetErrors[i]), kNetErrors[i]);
+    for (const auto& error : kNetErrors)
+      dict->SetInteger(ErrorToShortString(error), error);
 
     constants_dict->Set("netError", std::move(dict));
   }
@@ -241,8 +241,8 @@ std::unique_ptr<base::DictionaryValue> GetNetConstants() {
   {
     std::unique_ptr<base::DictionaryValue> dict(new base::DictionaryValue());
 
-    for (size_t i = 0; i < arraysize(kSdchProblems); i++)
-      dict->SetInteger(kSdchProblems[i].name, kSdchProblems[i].constant);
+    for (const auto& problem : kSdchProblems)
+      dict->SetInteger(problem.name, problem.constant);
 
     constants_dict->Set("sdchProblemCode", std::move(dict));
   }
