e600c3aa4ab44fc420d6a6fcc9c63095a3f07b30
emaxx <emaxx@chromium.org>
2016-11-03 10:34:37 -0700

    Fix behavior of cloud policy refreshes scheduled for soon
    The CloudPolicyRefreshScheduler class was implemented in such a way
    that refreshes scheduled for soon using the RefreshSoon method could
    be actually overridden by subsequent changes resulting in re-scheduling
    of the refresh.
    
    As an example of case which would result in ignoring a soon refresh is the OnStoreLoaded observer called when there's a non-empty response map in the policy client. This observer in this case would update the last_refresh_ field when it's empty, breaking some assumptions in the existing code.
    
    This CL introduces a different implementation for the RefreshSoon method which ensures that the scheduled soon refresh cannot be overridden by subsequent refresh updates.
    
    BUG=644304
    TEST=unit test
    
    Review-Url: https://codereview.chromium.org/2472593003
    Cr-Commit-Position: refs/heads/master@{#429632}

--- components/policy/core/common/cloud/cloud_policy_refresh_scheduler.cc
+++ components/policy/core/common/cloud/cloud_policy_refresh_scheduler.cc
@@ -101,7 +101,12 @@ int64_t CloudPolicyRefreshScheduler::GetActualRefreshDelay() const {
 }
 
 void CloudPolicyRefreshScheduler::RefreshSoon() {
-  RefreshNow();
+  // If the client isn't registered, there is nothing to do.
+  if (!client_->is_registered())
+    return;
+
+  is_scheduled_for_soon_ = true;
+  RefreshAfter(0);
 }
 
 void CloudPolicyRefreshScheduler::SetInvalidationServiceAvailability(
@@ -137,7 +142,7 @@ void CloudPolicyRefreshScheduler::OnRegistrationStateChanged(
   error_retry_delay_ms_ = kInitialErrorRetryDelayMs;
 
   // The client might have registered, so trigger an immediate refresh.
-  RefreshNow();
+  RefreshSoon();
 }
 
 void CloudPolicyRefreshScheduler::OnClientError(CloudPolicyClient* client) {
@@ -230,15 +235,16 @@ void CloudPolicyRefreshScheduler::UpdateLastRefreshFromPolicy() {
 #endif
 }
 
-void CloudPolicyRefreshScheduler::RefreshNow() {
-  last_refresh_ = base::Time();
-  ScheduleRefresh();
-}
-
 void CloudPolicyRefreshScheduler::ScheduleRefresh() {
   // If the client isn't registered, there is nothing to do.
   if (!client_->is_registered()) {
-    refresh_callback_.Cancel();
+    CancelRefresh();
+    return;
+  }
+
+  // Ignore the refresh request if there's a request scheduled for soon.
+  if (is_scheduled_for_soon_) {
+    DCHECK(!refresh_callback_.IsCancelled());
     return;
   }
 
@@ -274,7 +280,7 @@ void CloudPolicyRefreshScheduler::ScheduleRefresh() {
     case DM_STATUS_SERVICE_DEPROVISIONED:
     case DM_STATUS_SERVICE_DOMAIN_MISMATCH:
       // Need a re-registration, no use in retrying.
-      refresh_callback_.Cancel();
+      CancelRefresh();
       return;
   }
 
@@ -283,6 +289,8 @@ void CloudPolicyRefreshScheduler::ScheduleRefresh() {
 }
 
 void CloudPolicyRefreshScheduler::PerformRefresh() {
+  CancelRefresh();
+
   if (client_->is_registered()) {
     // Update |last_refresh_| so another fetch isn't triggered inadvertently.
     last_refresh_ = base::Time::NowFromSystemTime();
@@ -300,7 +308,6 @@ void CloudPolicyRefreshScheduler::PerformRefresh() {
 
 void CloudPolicyRefreshScheduler::RefreshAfter(int delta_ms) {
   base::TimeDelta delta(base::TimeDelta::FromMilliseconds(delta_ms));
-  refresh_callback_.Cancel();
 
   // Schedule the callback.
   base::TimeDelta delay =
@@ -310,6 +317,11 @@ void CloudPolicyRefreshScheduler::RefreshAfter(int delta_ms) {
       base::Bind(&CloudPolicyRefreshScheduler::PerformRefresh,
                  base::Unretained(this)));
   task_runner_->PostDelayedTask(FROM_HERE, refresh_callback_.callback(), delay);
+}
+
+void CloudPolicyRefreshScheduler::CancelRefresh() {
+  refresh_callback_.Cancel();
+  is_scheduled_for_soon_ = false;
 }
 
 }  // namespace policy
