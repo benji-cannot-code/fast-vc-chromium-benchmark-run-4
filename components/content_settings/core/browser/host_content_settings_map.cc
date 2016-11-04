2a84580cd87784fb499d8e790b7c69e9362e74fc
dullweber <dullweber@chromium.org>
2016-11-04 05:22:24 -0700

    Change mentions of 'off-the-record' to 'incognito' in components/content_settings
    TBR=dbeam@chromium.org
    BUG=3333
    
    Review-Url: https://codereview.chromium.org/2478803002
    Cr-Commit-Position: refs/heads/master@{#429865}

--- components/content_settings/core/browser/host_content_settings_map.cc
+++ components/content_settings/core/browser/host_content_settings_map.cc
@@ -164,7 +164,7 @@ HostContentSettingsMap::HostContentSettingsMap(PrefService* prefs,
       used_from_thread_id_(base::PlatformThread::CurrentId()),
 #endif
       prefs_(prefs),
-      is_off_the_record_(is_incognito_profile || is_guest_profile),
+      is_incognito_(is_incognito_profile || is_guest_profile),
       weak_ptr_factory_(this) {
   DCHECK(!(is_incognito_profile && is_guest_profile));
 
@@ -175,7 +175,7 @@ HostContentSettingsMap::HostContentSettingsMap(PrefService* prefs,
   policy_provider->AddObserver(this);
 
   pref_provider_ =
-      new content_settings::PrefProvider(prefs_, is_off_the_record_);
+      new content_settings::PrefProvider(prefs_, is_incognito_);
   content_settings_providers_[PREF_PROVIDER] = base::WrapUnique(pref_provider_);
   pref_provider_->AddObserver(this);
 
@@ -186,7 +186,7 @@ HostContentSettingsMap::HostContentSettingsMap(PrefService* prefs,
     pref_provider_->ClearPrefs();
 
   auto default_provider = base::MakeUnique<content_settings::DefaultProvider>(
-      prefs_, is_off_the_record_);
+      prefs_, is_incognito_);
   default_provider->AddObserver(this);
   content_settings_providers_[DEFAULT_PROVIDER] = std::move(default_provider);
 
@@ -261,7 +261,7 @@ ContentSetting HostContentSettingsMap::GetDefaultContentSettingInternal(
       continue;
     ContentSetting default_setting = GetDefaultContentSettingFromProvider(
         content_type, provider_pair.second.get());
-    if (is_off_the_record_) {
+    if (is_incognito_) {
       default_setting = content_settings::ValueToContentSetting(
           ProcessIncognitoInheritanceBehavior(
               content_type,
@@ -313,7 +313,7 @@ void HostContentSettingsMap::GetSettingsForOneType(
   for (const auto& provider_pair : content_settings_providers_) {
     // For each provider, iterate first the incognito-specific rules, then the
     // normal rules.
-    if (is_off_the_record_) {
+    if (is_incognito_) {
       AddSettingsForOneType(provider_pair.second.get(), provider_pair.first,
                             content_type, resource_identifier, settings, true);
     }
@@ -920,7 +920,7 @@ std::unique_ptr<base::Value> HostContentSettingsMap::GetWebsiteSettingInternal(
   for (const auto& provider_pair : content_settings_providers_) {
     std::unique_ptr<base::Value> value = GetContentSettingValueAndPatterns(
         provider_pair.second.get(), primary_url, secondary_url, content_type,
-        resource_identifier, is_off_the_record_, primary_pattern,
+        resource_identifier, is_incognito_, primary_pattern,
         secondary_pattern);
     if (value) {
       if (info)
