53a4c2169af7f41619c5db749ac677a21c7f7bf3
scheib <scheib@chromium.org>
2016-11-04 15:38:54 -0700

    Enable chrome://device-emulator in non-official NDEBUG builds.
    This page is useful to chromium developers working with
    ChromeOS builds on Linux -- even if they are running Release
    development builds.
    
    Also, add to chrome://about page which lists all internal pages.
    
    Review-Url: https://codereview.chromium.org/2435033004
    Cr-Commit-Position: refs/heads/master@{#430040}

--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -153,7 +153,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/proximity_auth/webui/url_constants.h"
 #endif
 
-#if defined(OS_CHROMEOS) && !defined(NDEBUG)
+#if defined(OS_CHROMEOS) && !defined(OFFICIAL_BUILD)
 #include "chrome/browser/ui/webui/chromeos/emulator/device_emulator_ui.h"
 #endif
 
@@ -512,12 +512,12 @@ WebUIFactoryFunction GetWebUIFactoryFunction(WebUI* web_ui,
     return &NewWebUI<chromeos::SlowTraceController>;
   if (url.host() == chrome::kChromeUIVoiceSearchHost)
     return &NewWebUI<VoiceSearchUI>;
-#if !defined(NDEBUG)
+#if !defined(OFFICIAL_BUILD)
   if (!base::SysInfo::IsRunningOnChromeOS()) {
     if (url.host() == chrome::kChromeUIDeviceEmulatorHost)
       return &NewWebUI<DeviceEmulatorUI>;
   }
-#endif  // !defined(NDEBUG)
+#endif  // !defined(OFFICIAL_BUILD)
 #endif  // defined(OS_CHROMEOS)
 #if defined(OS_ANDROID)
   if (url.host() == chrome::kChromeUIOfflineInternalsHost)
