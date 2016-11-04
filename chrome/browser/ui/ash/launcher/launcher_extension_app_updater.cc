957d1bf6e3ffbcda42ea34c712a780cdec6ddea7
hidehiko <hidehiko@chromium.org>
2016-11-04 09:35:48 -0700

    Remove OnOptInChanged callback.
    The callback is used in two different purposes.
    - Callback in browsertest.
      This is unused, so just removed.
    - Trigger to update the badge.
      The actual trigger timing is opt-in preference change.
      Replace it by OnOptInEnabled().
    
    BUG=657687
    BUG=b/31079732
    TEST=Ran on test device.
         Enable ARC. Install conflict app. Verify the badge. Disable
         ARC. Verify the badge is disappeared.
    
    Review-Url: https://codereview.chromium.org/2456803002
    Cr-Commit-Position: refs/heads/master@{#429911}

--- chrome/browser/ui/ash/launcher/launcher_extension_app_updater.cc
+++ chrome/browser/ui/ash/launcher/launcher_extension_app_updater.cc
@@ -17,11 +17,6 @@ LauncherExtensionAppUpdater::LauncherExtensionAppUpdater(
     : LauncherAppUpdater(delegate, browser_context) {
   StartObservingExtensionRegistry();
 
-  arc::ArcAuthService* arc_auth_service = arc::ArcAuthService::Get();
-  // ArcAuthService may not be available for some unit tests.
-  if (arc_auth_service)
-    arc_auth_service->AddObserver(this);
-
   ArcAppListPrefs* prefs = ArcAppListPrefs::Get(browser_context);
   if (prefs)
     prefs->AddObserver(this);
@@ -29,10 +24,6 @@ LauncherExtensionAppUpdater::LauncherExtensionAppUpdater(
 
 LauncherExtensionAppUpdater::~LauncherExtensionAppUpdater() {
   StopObservingExtensionRegistry();
-
-  arc::ArcAuthService* arc_auth_service = arc::ArcAuthService::Get();
-  if (arc_auth_service)
-    arc_auth_service->RemoveObserver(this);
 
   ArcAppListPrefs* prefs = ArcAppListPrefs::Get(browser_context());
   if (prefs)
@@ -66,15 +57,6 @@ void LauncherExtensionAppUpdater::OnShutdown(
     extensions::ExtensionRegistry* registry) {
   DCHECK_EQ(extension_registry_, registry);
   StopObservingExtensionRegistry();
-}
-
-void LauncherExtensionAppUpdater::OnOptInChanged(
-    arc::ArcAuthService::State state) {
-  if (!chromeos::ProfileHelper::IsPrimaryProfile(
-          Profile::FromBrowserContext(browser_context()))) {
-    return;
-  }
-  UpdateHostedApps();
 }
 
 void LauncherExtensionAppUpdater::OnPackageInstalled(
--- chrome/browser/ui/ash/launcher/launcher_extension_app_updater.h
+++ chrome/browser/ui/ash/launcher/launcher_extension_app_updater.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_ASH_LAUNCHER_LAUNCHER_EXTENSION_APP_UPDATER_H_
 
 #include "base/macros.h"
-#include "chrome/browser/chromeos/arc/arc_auth_service.h"
 #include "chrome/browser/ui/app_list/arc/arc_app_list_prefs.h"
 #include "chrome/browser/ui/ash/launcher/launcher_app_updater.h"
 #include "extensions/browser/extension_registry_observer.h"
@@ -19,7 +18,6 @@ class ExtensionSet;
 class LauncherExtensionAppUpdater
     : public LauncherAppUpdater,
       public extensions::ExtensionRegistryObserver,
-      public arc::ArcAuthService::Observer,
       public ArcAppListPrefs::Observer {
  public:
   LauncherExtensionAppUpdater(Delegate* delegate,
@@ -37,9 +35,6 @@ class LauncherExtensionAppUpdater
                               const extensions::Extension* extension,
                               extensions::UninstallReason reason) override;
   void OnShutdown(extensions::ExtensionRegistry* registry) override;
-
-  // arc::ArcAuthService::Observer:
-  void OnOptInChanged(arc::ArcAuthService::State state) override;
 
   // ArcAppListPrefs::Observer
   void OnPackageInstalled(
