83ebc7bea9c896f0d8f1d8c767328154e803b352
aleksandar.stojiljkovic <aleksandar.stojiljkovic@intel.com>
2016-11-03 23:20:32 -0700

    Workaround for undeclared identifier V4L2_PIX_FMT_INVZ.
    Realsense SR300 driver installation patches the kernel currently.
    Until the format is officially introduced to kernel, enable using it and
    resolve build error on kernel > 4.6.
    
    BUG=661877
    
    Review-Url: https://codereview.chromium.org/2475633002
    Cr-Commit-Position: refs/heads/master@{#429810}

--- media/capture/video/linux/v4l2_capture_delegate.cc
+++ media/capture/video/linux/v4l2_capture_delegate.cc
@@ -24,6 +24,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 6, 0)
 // 16 bit depth, Realsense F200.
 #define V4L2_PIX_FMT_Z16 v4l2_fourcc('Z', '1', '6', ' ')
+#endif
+
+// TODO(aleksandar.stojiljkovic): Wrap this with kernel version check once the
+// format is introduced to kernel.
+// See https://crbug.com/661877
+#ifndef V4L2_PIX_FMT_INVZ
 // 16 bit depth, Realsense SR300.
 #define V4L2_PIX_FMT_INVZ v4l2_fourcc('I', 'N', 'V', 'Z')
 #endif
