ca342b59dfbc2256e1aa1d5958c4e3a6170275cd
tschumann <tschumann@chromium.org>
2016-11-04 09:08:44 -0700

    Separate fetchmore from regular fetch paths. Fix bug in fetch-more.
    The previous logic tried merging both paths (fetching for more and regular
    fetches).
    This included one bug in that for fetch-more, newly fetched suggestions got
    merged after existing ones before getting sent to the caller.
    Given many other side effects, it seems cleaner to leave the two paths
    separated and don't update any internal state in case of fetch more requests.
    Especially the whole logic about updating category states is a brittle and
    easy to get wrong. Given that category states should not change with FetchMore
    requests this seems another win.
    
    We can consider caching new suggestions from FetchMore requests -- but that's
    a separate issue.
    
    BUG=634892
    
    Review-Url: https://codereview.chromium.org/2464163006
    Cr-Commit-Position: refs/heads/master@{#429904}

--- components/ntp_snippets/remote/ntp_snippets_service_unittest.cc
+++ components/ntp_snippets/remote/ntp_snippets_service_unittest.cc
@@ -69,6 +69,10 @@ MATCHER_P(IdEq, value, "") {
   return arg->id() == value;
 }
 
+MATCHER_P(IdWithinCategoryEq, expected_id, "") {
+  return arg.id().id_within_category() == expected_id;
+}
+
 MATCHER_P(IsCategory, id, "") {
   return arg.id() == static_cast<int>(id);
 }
@@ -389,8 +393,7 @@ class NTPSnippetsServiceTest : public ::testing::Test {
         test_url_(kTestContentSuggestionsServerWithAPIKey),
         user_classifier_(/*pref_service=*/nullptr),
         image_fetcher_(nullptr),
-        image_decoder_(nullptr),
-        caller_() {
+        image_decoder_(nullptr) {
     NTPSnippetsService::RegisterProfilePrefs(utils_.pref_service()->registry());
     RequestThrottler::RegisterProfilePrefs(utils_.pref_service()->registry());
 
@@ -516,13 +519,6 @@ class NTPSnippetsServiceTest : public ::testing::Test {
   }
 
  private:
-  class MockCaller {
-   public:
-    void EmptyCallback(std::vector<ContentSuggestion>) {}
-    void CheckCallback(std::vector<ContentSuggestion> v) { MustCallback(); }
-    MOCK_METHOD0(MustCallback, void());
-  };
-
   variations::testing::VariationParamsManager params_manager_;
   test::NTPSnippetsTestUtils utils_;
   base::MessageLoop message_loop_;
@@ -539,7 +535,6 @@ class NTPSnippetsServiceTest : public ::testing::Test {
   FakeImageDecoder* image_decoder_;
 
   base::ScopedTempDir database_dir_;
-  MockCaller caller_;
 
   DISALLOW_COPY_AND_ASSIGN(NTPSnippetsServiceTest);
 };
@@ -855,21 +850,35 @@ TEST_F(NTPSnippetsServiceTest, ReplaceSnippets) {
 TEST_F(NTPSnippetsServiceTest, LoadsAdditionalSnippets) {
   auto service = MakeSnippetsService();
 
-  std::string first("http://first");
-  LoadFromJSONString(service.get(), GetTestJson({GetSnippetWithUrl(first)}));
+  LoadFromJSONString(service.get(),
+                     GetTestJson({GetSnippetWithUrl("http://first")}));
   EXPECT_THAT(service->GetSnippetsForTesting(articles_category()),
-              ElementsAre(IdEq(first)));
+              ElementsAre(IdEq("http://first")));
 
-  std::string second("http://second");
-  LoadMoreFromJSONString(service.get(),
-                         articles_category(),
-                         GetTestJson({GetSnippetWithUrl(second)}),
-                         std::set<std::string>(),
-                         base::Bind([](std::vector<ContentSuggestion>) {}));
-  // The snippets loaded last are added to the previously loaded.
-  EXPECT_THAT(service->GetSnippetsForTesting(articles_category()),
-              ElementsAre(IdEq(first), IdEq(second)));
+  auto expect_only_second_suggestion_received =
+      base::Bind([](std::vector<ContentSuggestion> suggestions) {
+        EXPECT_THAT(suggestions, SizeIs(1));
+        EXPECT_THAT(suggestions[0].id().id_within_category(),
+                    Eq("http://second"));
+      });
+  LoadMoreFromJSONString(service.get(), articles_category(),
+                         GetTestJson({GetSnippetWithUrl("http://second")}),
+                         /*known_ids=*/std::set<std::string>(),
+                         expect_only_second_suggestion_received);
+
+  // Verify that the observer did not get updated about the additional snippets.
+  // Rationale: It's not clear that snippets fetched in one context (i.e. one
+  // potentially old NTP) should be shown in other NTPs as well.
+  // We can revisit this decision, but for now it's easiest to keep them
+  // independent.
+  EXPECT_THAT(observer().SuggestionsForCategory(articles_category()),
+              ElementsAre(IdWithinCategoryEq("http://first")));
 }
+
+// TODO(tschumann): We don't have test making sure the NTPSnippetsFetcher
+// actually gets the proper parameters. Add tests with an injected
+// NTPSnippetsFetcher to verify the parameters, including proper handling of
+// dismissed and known_ids.
 
 namespace {
 
