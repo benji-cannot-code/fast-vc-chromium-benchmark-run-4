18dda7b5fa3e01a6502f8fb7740214a91d82ec53
vabr <vabr@chromium.org>
2016-11-04 08:08:37 -0700

    Clean up #includes in content_password_manager_driver.cc
    This is a tiny clean-up, removing headers added but ultimately unused by
    https://codereview.chromium.org/2467773002/.
    
    BUG=658764
    
    Review-Url: https://codereview.chromium.org/2473353002
    Cr-Commit-Position: refs/heads/master@{#429886}

--- components/password_manager/content/browser/content_password_manager_driver.cc
+++ components/password_manager/content/browser/content_password_manager_driver.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/password_manager/content/browser/content_password_manager_driver.h"
 
-#include <set>
-
 #include "components/autofill/content/browser/content_autofill_driver.h"
 #include "components/autofill/core/common/form_data.h"
 #include "components/autofill/core/common/password_form.h"
@@ -20,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/child_process_security_policy.h"
 #include "content/public/browser/navigation_details.h"
 #include "content/public/browser/navigation_entry.h"
-#include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_view_host.h"
