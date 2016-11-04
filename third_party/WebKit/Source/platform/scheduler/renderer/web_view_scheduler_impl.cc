e37d8e05e1a84ef16534ff0100f892255871e417
altimin <altimin@chromium.org>
2016-11-04 04:44:54 -0700

    [scheduler] Teach scheduler about audio state
    This patch wires information about audio state to renderer scheduler
    and stops all throttling (background and offscreen) while
    audio is playing and for a short period after audio is stopped.
    
    BUG=642321,616519, 656019
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    
    Committed: https://crrev.com/a7a651d546b76499821b4ba47b8f017d4b8becac
    Review-Url: https://codereview.chromium.org/2383473002
    Cr-Original-Commit-Position: refs/heads/master@{#423405}
    Cr-Commit-Position: refs/heads/master@{#429861}

--- third_party/WebKit/Source/platform/scheduler/renderer/web_view_scheduler_impl.cc
+++ third_party/WebKit/Source/platform/scheduler/renderer/web_view_scheduler_impl.cc
@@ -177,6 +177,11 @@ void WebViewSchedulerImpl::setVirtualTimePolicy(VirtualTimePolicy policy) {
   }
 }
 
+void WebViewSchedulerImpl::audioStateChanged(bool is_audio_playing) {
+  is_audio_playing_ = is_audio_playing;
+  renderer_scheduler_->OnAudioStateChanged();
+}
+
 void WebViewSchedulerImpl::ApplyVirtualTimePolicy() {
   if (virtual_time_policy_ != VirtualTimePolicy::DETERMINISTIC_LOADING) {
     return;
@@ -188,6 +193,10 @@ void WebViewSchedulerImpl::ApplyVirtualTimePolicy() {
   setAllowVirtualTimeToAdvance(pending_loads_.size() == 0 &&
                                background_parser_count_ == 0 &&
                                have_seen_loading_task_);
+}
+
+bool WebViewSchedulerImpl::IsAudioPlaying() const {
+  return is_audio_playing_;
 }
 
 }  // namespace scheduler
--- third_party/WebKit/Source/web/WebViewImpl.cpp
+++ third_party/WebKit/Source/web/WebViewImpl.cpp
@@ -3473,6 +3473,10 @@ void WebViewImpl::performPluginAction(const WebPluginAction& action,
   }
 }
 
+void WebViewImpl::audioStateChanged(bool isAudioPlaying) {
+  m_scheduler->audioStateChanged(isAudioPlaying);
+}
+
 WebHitTestResult WebViewImpl::hitTestResultAt(const WebPoint& point) {
   return coreHitTestResultAt(point);
 }
