7a40a42542b16aab79cf23a9f38b480f5602e052
keishi <keishi@chromium.org>
2016-11-04 02:50:09 -0700

    Lock CrossThreadPersistentRegion while poisoning HeapPages
    When CrossThreadPersistentRegion::prepareForThreadStateTermination or CrossThreadPersistentRegion::tracePersistentNodes is called, we iterate over all CrossThreadPersistents.
    While that is happening in one thread, another thread was calling ThreadState::poisonAllHeaps(), causing poisoning to happen simultaneously.
    
    BUG=660760
    
    Review-Url: https://codereview.chromium.org/2471023004
    Cr-Commit-Position: refs/heads/master@{#429842}

--- third_party/WebKit/Source/platform/heap/ThreadState.cpp
+++ third_party/WebKit/Source/platform/heap/ThreadState.cpp
@@ -1138,6 +1138,8 @@ void ThreadState::preSweep() {
 
 #if defined(ADDRESS_SANITIZER)
 void ThreadState::poisonAllHeaps() {
+  CrossThreadPersistentRegion::LockScope persistentLock(
+      ProcessHeap::crossThreadPersistentRegion());
   // Poisoning all unmarked objects in the other arenas.
   for (int i = 1; i < BlinkGC::NumberOfArenas; i++)
     m_arenas[i]->poisonArena();
@@ -1148,6 +1150,8 @@ void ThreadState::poisonAllHeaps() {
 }
 
 void ThreadState::poisonEagerArena() {
+  CrossThreadPersistentRegion::LockScope persistentLock(
+      ProcessHeap::crossThreadPersistentRegion());
   m_arenas[BlinkGC::EagerSweepArenaIndex]->poisonArena();
   // CrossThreadPersistents in unmarked objects may be accessed from other
   // threads (e.g. in CrossThreadPersistentRegion::shouldTracePersistent) and
