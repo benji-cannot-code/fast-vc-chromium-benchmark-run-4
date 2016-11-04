41c88404c2f6e1a14985464cc437fc44a0df2606
twellington <twellington@chromium.org>
2016-11-04 12:53:32 -0700

    [Contextual Search] Add command line flag for contextual cards version override
    BUG=657063
    
    Review-Url: https://codereview.chromium.org/2474273005
    Cr-Commit-Position: refs/heads/master@{#429985}

--- chrome/browser/android/contextualsearch/contextual_search_delegate.cc
+++ chrome/browser/android/contextualsearch/contextual_search_delegate.cc
@@ -73,6 +73,8 @@ const int kContextualCardsNoIntegration = 0;
 const int kContextualCardsBarIntegration = 1;
 const int kContextualCardsSingleAction = 2;
 
+const char kContextualCardsVersionOverride[] = "contextual_cards_version";
+
 }  // namespace
 
 // URLFetcher ID, only used for tests: we only have one kind of fetcher.
@@ -234,6 +236,13 @@ std::string ContextualSearchDelegate::BuildRequestUrl(std::string selection) {
   if (base::FeatureList::IsEnabled(
           chrome::android::kContextualSearchSingleActions)) {
     contextual_cards_version = kContextualCardsSingleAction;
+  }
+
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+      kContextualCardsVersionOverride)){
+    contextual_cards_version =
+        std::stoi(base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
+            kContextualCardsVersionOverride), nullptr);
   }
 
   TemplateURLRef::SearchTermsArgs::ContextualSearchParams params(
