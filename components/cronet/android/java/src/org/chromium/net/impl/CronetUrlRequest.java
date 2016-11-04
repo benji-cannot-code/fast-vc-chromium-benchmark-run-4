70f6589c6b2918ff91ee0f8793b49425e36bf4f7
mgersh <mgersh@chromium.org>
2016-11-04 14:07:40 -0700

    Fix null annotations, and move checks into RequestFinishedInfo
    RequestFinishedInfo.getAnnotations() was returning null instead of an
    empty collection when used with BidirectionalStream. It now returns an
    empty collection instead, and the code to do this is moved out of the
    builders into RequestFinishedInfo.
    
    BUG=662409
    
    Review-Url: https://codereview.chromium.org/2476983002
    Cr-Commit-Position: refs/heads/master@{#430013}

--- components/cronet/android/java/src/org/chromium/net/impl/CronetUrlRequest.java
+++ components/cronet/android/java/src/org/chromium/net/impl/CronetUrlRequest.java
@@ -139,9 +139,6 @@ public final class CronetUrlRequest extends UrlRequestBase {
         if (executor == null) {
             throw new NullPointerException("Executor is required");
         }
-        if (requestAnnotations == null) {
-            throw new NullPointerException("requestAnnotations is required");
-        }
 
         mAllowDirectExecutor = allowDirectExecutor;
         mRequestContext = requestContext;
