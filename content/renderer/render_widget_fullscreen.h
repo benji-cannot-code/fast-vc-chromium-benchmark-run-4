18151b706b994fca789f452f6600ca83423a1b14
jcivelli <jcivelli@chromium.org>
2016-11-04 10:12:14 -0700

    Converts the ViewHostMsg_CreateFullscreenWidget message to mojom.
    BUG=612500
    TEST=The TestFlashFullscreen PPAPI test should pass.
    
    Review-Url: https://codereview.chromium.org/2472733002
    Cr-Commit-Position: refs/heads/master@{#429923}

--- content/renderer/render_widget_fullscreen.h
+++ content/renderer/render_widget_fullscreen.h
@@ -30,7 +30,7 @@ class RenderWidgetFullscreen : public RenderWidget {
   bool Init(int32_t opener_id);
 
  private:
-  bool SendIPC(int32_t opener_id, int32_t* routing_id);
+  bool CreateFullscreenWidget(int32_t opener_id, int32_t* routing_id);
 };
 
 }  // namespace content
