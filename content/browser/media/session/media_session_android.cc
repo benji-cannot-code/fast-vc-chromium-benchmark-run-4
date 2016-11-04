fdaacf36e4fc3491479056f8435676507be95617
zqzhang <zqzhang@chromium.org>
2016-11-03 18:38:43 -0700

    Another fix for MediaSessionAndroid DCHECK on destruction
    This CL removes the unnecessary DCHECK for the Java object
    in MediaSessionAndroid destruction. This should fix an flaky test
    caused by hitting this DCHECK() in some situations.
    
    BUG=661444
    
    Review-Url: https://codereview.chromium.org/2479703002
    Cr-Commit-Position: refs/heads/master@{#429757}

--- content/browser/media/session/media_session_android.cc
+++ content/browser/media/session/media_session_android.cc
@@ -37,10 +37,7 @@ MediaSessionAndroid::MediaSessionAndroid(MediaSessionImpl* session)
     contents_android->SetMediaSession(j_media_session);
 }
 
-MediaSessionAndroid::~MediaSessionAndroid() {
-  // MediaSessionDestroyed() should have already been called.
-  DCHECK(j_media_session_.is_empty());
-}
+MediaSessionAndroid::~MediaSessionAndroid() = default;
 
 // static
 bool MediaSessionAndroid::Register(JNIEnv* env) {
@@ -64,12 +61,8 @@ ScopedJavaLocalRef<jobject> GetMediaSessionFromWebContents(
 
 void MediaSessionAndroid::MediaSessionDestroyed() {
   ScopedJavaLocalRef<jobject> j_local_session = GetJavaObject();
-  if (j_local_session.is_null()) {
-    // If the Java MediaSession is already garbage collected, unset the weak
-    // Java reference.
-    j_media_session_.reset();
+  if (j_local_session.is_null())
     return;
-  }
 
   JNIEnv* env = base::android::AttachCurrentThread();
   // The Java object will tear down after this call.
