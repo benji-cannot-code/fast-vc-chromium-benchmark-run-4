8fd4403e8d308a316fcc30441eca3225e95f9887
tommycli <tommycli@chromium.org>
2016-11-03 23:10:26 -0700

    MD Settings: Add an Import Data BrowserProxy.
    This patch:
     a) Adds a BrowserProxy for the ImportDataHandler.
     b) Updates ImportDataHandler to use promises and WebUI events.
    
    BUG=659259
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2473053004
    Cr-Commit-Position: refs/heads/master@{#429809}

--- chrome/browser/ui/webui/settings/settings_import_data_handler.h
+++ chrome/browser/ui/webui/settings/settings_import_data_handler.h
@@ -42,7 +42,7 @@ class ImportDataHandler : public SettingsPageUIHandler,
   void ImportData(const base::ListValue* args);
 
   void InitializeDialog(const base::ListValue* args);
-  void SendBrowserProfileData();
+  void SendBrowserProfileData(const std::string& callback_id);
 
   // importer::ImporterProgressObserver:
   void ImportStarted() override;
