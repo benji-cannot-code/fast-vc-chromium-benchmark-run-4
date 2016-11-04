64151372bb7e6d12630e63e603888ea700037b30
thakis <thakis@chromium.org>
2016-11-04 14:15:16 -0700

    win: Don't define TOOLKIT_VIEWS in build_config.h
    ui/views/BUILD.gn adds this as a define in gn, so rely on that on Windows
    just like we do on other platforms that use views.  No intended behavior
    change.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2479913002
    Cr-Commit-Position: refs/heads/master@{#430017}

--- build/build_config.h
+++ build/build_config.h
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #elif defined(_WIN32)
 #define OS_WIN 1
-#define TOOLKIT_VIEWS 1
 #elif defined(__FreeBSD__)
 #define OS_FREEBSD 1
 #elif defined(__NetBSD__)
