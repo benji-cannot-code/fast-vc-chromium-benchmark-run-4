628e9140e7bd546b90a07404c5d523be51deaab4
guidou <guidou@chromium.org>
2016-11-03 17:56:38 -0700

    Remove MediaStreamTrack.getSources().
    Intent-to-remove thread:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/do3t86PtHCY
    
    BUG=649710,388194
    
    Review-Url: https://codereview.chromium.org/2471803002
    Cr-Commit-Position: refs/heads/master@{#429745}

--- third_party/WebKit/Source/platform/mediastream/MediaStreamCenter.cpp
+++ third_party/WebKit/Source/platform/mediastream/MediaStreamCenter.cpp
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/mediastream/MediaStreamCenter.h"
 
 #include "platform/mediastream/MediaStreamDescriptor.h"
-#include "platform/mediastream/MediaStreamTrackSourcesRequest.h"
 #include "platform/mediastream/MediaStreamWebAudioSource.h"
 #include "public/platform/Platform.h"
 #include "public/platform/WebAudioSourceProvider.h"
