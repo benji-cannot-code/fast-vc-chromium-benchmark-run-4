c298c518631c2ca40718fc2082936c05dea5c205
nigeltao <nigeltao@chromium.org>
2016-11-03 20:51:10 -0700

    Add chrome namespace to chrome/common/network_diagnostics.mojom
    BUG=
    
    Review-Url: https://codereview.chromium.org/2465623002
    Cr-Commit-Position: refs/heads/master@{#429777}

--- chrome/browser/net/net_error_tab_helper_unittest.cc
+++ chrome/browser/net/net_error_tab_helper_unittest.cc
@@ -52,7 +52,9 @@ class TestNetErrorTabHelper : public NetErrorTabHelper {
         render_frame_host);
   }
 
-  mojom::NetworkDiagnostics* network_diagnostics_interface() { return this; }
+  chrome::mojom::NetworkDiagnostics* network_diagnostics_interface() {
+    return this;
+  }
 
  private:
   // NetErrorTabHelper implementation:
