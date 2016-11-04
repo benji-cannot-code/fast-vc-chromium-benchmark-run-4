bbd28dce1c8a85d2425365ade17c5142cb02373e
einbinder <einbinder@chromium.org>
2016-11-03 23:55:37 -0700

    DevTools: Substring autocomplete in Console and StylesSideBar.
    Case insensitive matching as well as matching user entered text
    in the middle of a suggestion.
    
    BUG=660941
    
    Review-Url: https://codereview.chromium.org/2478563002
    Cr-Commit-Position: refs/heads/master@{#429819}

--- third_party/WebKit/Source/devtools/front_end/components/JavaScriptAutocomplete.js
+++ third_party/WebKit/Source/devtools/front_end/components/JavaScriptAutocomplete.js
@@ -57,11 +57,11 @@ WebInspector.JavaScriptAutocomplete.completionsForTextInCurrentContext = functio
 
   /**
    * @param {string} expressionString
-   * @param {string} prefix
+   * @param {string} query
    * @param {boolean=} force
    * @return {!Promise<!Array<string>>}
    */
-WebInspector.JavaScriptAutocomplete.completionsForExpression = function(expressionString, prefix, force) {
+WebInspector.JavaScriptAutocomplete.completionsForExpression = function(expressionString, query, force) {
   var executionContext = WebInspector.context.flavor(WebInspector.ExecutionContext);
   if (!executionContext)
     return Promise.resolve([]);
@@ -78,7 +78,7 @@ WebInspector.JavaScriptAutocomplete.completionsForExpression = function(expressi
   if (expressionString && !isNaN(expressionString))
     return Promise.resolve([]);
 
-  if (!prefix && !expressionString && !force)
+  if (!query && !expressionString && !force)
     return Promise.resolve([]);
 
   var fufill;
@@ -224,8 +224,8 @@ WebInspector.JavaScriptAutocomplete.completionsForExpression = function(expressi
       for (var i = 0; i < commandLineAPI.length; ++i)
         propertyNames[commandLineAPI[i]] = true;
     }
-    fufill(WebInspector.JavaScriptAutocomplete._completionsForPrefix(
-      dotNotation, bracketNotation, expressionString, prefix, Object.keys(propertyNames)));
+    fufill(WebInspector.JavaScriptAutocomplete._completionsForQuery(
+      dotNotation, bracketNotation, expressionString, query, Object.keys(propertyNames)));
   }
 };
 
@@ -233,19 +233,17 @@ WebInspector.JavaScriptAutocomplete.completionsForExpression = function(expressi
    * @param {boolean} dotNotation
    * @param {boolean} bracketNotation
    * @param {string} expressionString
-   * @param {string} prefix
+   * @param {string} query
    * @param {!Array.<string>} properties
    * @return {!Array<string>}
    */
-WebInspector.JavaScriptAutocomplete._completionsForPrefix = function(dotNotation, bracketNotation, expressionString, prefix, properties) {
+WebInspector.JavaScriptAutocomplete._completionsForQuery = function(dotNotation, bracketNotation, expressionString, query, properties) {
   if (bracketNotation) {
-    if (prefix.length && prefix[0] === '\'')
+    if (query.length && query[0] === '\'')
       var quoteUsed = '\'';
     else
       var quoteUsed = '"';
   }
-
-  var results = [];
 
   if (!expressionString) {
     const keywords = [
@@ -258,6 +256,10 @@ WebInspector.JavaScriptAutocomplete._completionsForPrefix = function(dotNotation
 
   properties.sort();
 
+  var caseSensitivePrefix = [];
+  var caseInsensitivePrefix = [];
+  var caseSensitiveAnywhere = [];
+  var caseInsensitiveAnywhere = [];
   for (var i = 0; i < properties.length; ++i) {
     var property = properties[i];
 
@@ -271,13 +273,21 @@ WebInspector.JavaScriptAutocomplete._completionsForPrefix = function(dotNotation
       property += ']';
     }
 
-    if (property.length < prefix.length)
+    if (property.length < query.length)
       continue;
-    if (prefix.length && !property.startsWith(prefix))
+    if (query.length && property.toLowerCase().indexOf(query.toLowerCase()) === -1)
       continue;
-
     // Substitute actual newlines with newline characters. @see crbug.com/498421
-    results.push(property.split('\n').join('\\n'));
+    var prop = property.split('\n').join('\\n');
+
+    if (property.startsWith(query))
+      caseSensitivePrefix.push(prop);
+    else if (property.toLowerCase().startsWith(query.toLowerCase()))
+      caseInsensitivePrefix.push(prop);
+    else if (property.indexOf(query) !== -1)
+      caseSensitiveAnywhere.push(prop);
+    else
+      caseInsensitiveAnywhere.push(prop);
   }
-  return results;
+  return caseSensitivePrefix.concat(caseInsensitivePrefix).concat(caseSensitiveAnywhere).concat(caseInsensitiveAnywhere);
 };
