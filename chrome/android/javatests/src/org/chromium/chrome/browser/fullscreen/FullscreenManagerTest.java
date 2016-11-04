22e2df02440c2a81d6121d9399495913054790df
tedchoc <tedchoc@chromium.org>
2016-11-04 13:35:53 -0700

    Pull fullscreen tab loading logic out of Tab.
    This reduces the bloat of Tab and allows a bit more extraction
    of the fullscreen logic.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2467133002
    Cr-Commit-Position: refs/heads/master@{#430002}

--- chrome/android/javatests/src/org/chromium/chrome/browser/fullscreen/FullscreenManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/fullscreen/FullscreenManagerTest.java
@@ -28,6 +28,7 @@ import org.chromium.base.test.util.UrlUtils;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.fullscreen.ChromeFullscreenManager.FullscreenListener;
 import org.chromium.chrome.browser.omnibox.UrlBar;
+import org.chromium.chrome.browser.tab.BrowserControlsVisibilityDelegate;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabWebContentsDelegateAndroid;
 import org.chromium.chrome.test.ChromeTabbedActivityTestBase;
@@ -84,6 +85,17 @@ public class FullscreenManagerTest extends ChromeTabbedActivityTestBase {
             + "<body style='height:10000px;' onclick='toggleFullScreen();'>"
             + "</body>"
             + "</html>");
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
+            @Override
+            public void run() {
+                BrowserControlsVisibilityDelegate.disablePageLoadDelayForTests();
+            }
+        });
+    }
 
     @MediumTest
     @Feature({"Fullscreen"})
@@ -312,13 +324,6 @@ public class FullscreenManagerTest extends ChromeTabbedActivityTestBase {
             OmniboxTestUtils.toggleUrlBarFocus(urlBar, false);
             OmniboxTestUtils.waitForFocusAndKeyboardActive(urlBar, false);
 
-            ThreadUtils.runOnUiThreadBlocking(new Runnable() {
-                @Override
-                public void run() {
-                    tab.processEnableFullscreenRunnableForTest();
-                }
-            });
-
             waitForBrowserControlsToBeMoveable(tab);
         } finally {
             testServer.stopAndDestroyServer();
@@ -485,18 +490,6 @@ public class FullscreenManagerTest extends ChromeTabbedActivityTestBase {
             @Override
             public void run() {
                 getActivity().getFullscreenManager().disableBrowserOverrideForTest();
-            }
-        });
-    }
-
-    @Override
-    protected void startMainActivityWithURL(String url) throws InterruptedException {
-        super.startMainActivityWithURL(url);
-        final Tab tab = getActivity().getActivityTab();
-        ThreadUtils.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                tab.processEnableFullscreenRunnableForTest();
             }
         });
     }
