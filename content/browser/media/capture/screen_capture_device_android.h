9011972240f801a81c5b3da658c91ade151a7f40
braveyao <braveyao@chromium.org>
2016-11-04 18:12:06 -0700

    Android ScreenCapture: add support to device rotation.
    When device is rotated during screen capturing with MediaProjection API
    on Android, the rotated screen will be captured and fit into the frame requested
    with padding. So when we detect a device rotation, we need to re-create capture
    with the new orientation and update the oracle.
    
    Also refactored(mainly reordered some lines) the java file a bit for a newly
    added unittest file.
    
    BUG=487935
    
    Review-Url: https://codereview.chromium.org/2468383002
    Cr-Commit-Position: refs/heads/master@{#430111}

--- content/browser/media/capture/screen_capture_device_android.h
+++ content/browser/media/capture/screen_capture_device_android.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "content/common/content_export.h"
 #include "media/capture/content/screen_capture_device_core.h"
 #include "media/capture/video/video_capture_device.h"
 
@@ -16,7 +17,8 @@ namespace content {
 // ScreenCaptureDeviceAndroid is a forwarder to media::ScreenCaptureDeviceCore
 // while keeping the Power Saving from kicking in between AllocateAndStart() and
 // StopAndDeAllocate().
-class ScreenCaptureDeviceAndroid : public media::VideoCaptureDevice {
+class CONTENT_EXPORT ScreenCaptureDeviceAndroid
+    : public media::VideoCaptureDevice {
  public:
   ScreenCaptureDeviceAndroid();
   ~ScreenCaptureDeviceAndroid() override;
