2799571df3a1730adc77de1df0673db0c045f05d
elijahtaylor <elijahtaylor@chromium.org>
2016-11-04 17:07:18 -0700

    Update Arc.State regularly
    This is required so data is filtered correctly on the server,
    it must be present in every record, with one value.
    
    BUG=b/27346460
    
    Review-Url: https://codereview.chromium.org/2468973007
    Cr-Commit-Position: refs/heads/master@{#430069}

--- chrome/browser/metrics/chromeos_metrics_provider.cc
+++ chrome/browser/metrics/chromeos_metrics_provider.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/chromeos/arc/arc_auth_service.h"
 #include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/pref_names.h"
@@ -248,6 +249,7 @@ void ChromeOSMetricsProvider::ProvideGeneralMetrics(
   }
 
   RecordEnrollmentStatus();
+  RecordArcState();
 }
 
 void ChromeOSMetricsProvider::WriteBluetoothProto(
@@ -332,4 +334,10 @@ void ChromeOSMetricsProvider::SetBluetoothAdapter(
 void ChromeOSMetricsProvider::RecordEnrollmentStatus() {
   UMA_HISTOGRAM_ENUMERATION(
       "UMA.EnrollmentStatus", GetEnrollmentStatus(), ENROLLMENT_STATUS_MAX);
+}
+
+void ChromeOSMetricsProvider::RecordArcState() {
+  arc::ArcAuthService* auth_service = arc::ArcAuthService::Get();
+  if (auth_service)
+    auth_service->RecordArcState();
 }
