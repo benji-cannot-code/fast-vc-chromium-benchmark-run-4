fe20960a1e86f830e3576961e86cc04a2b1668cc
zmo <zmo@chromium.org>
2016-11-04 23:21:29 -0700

    Change WebGL API arg nullable behavior.
    This is due to a spec change.
    
    BUG=662520
    TEST=webgl_conformance,webgl2_conformance
    R=kbr@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2485443002
    Cr-Commit-Position: refs/heads/master@{#430154}

--- third_party/WebKit/Source/modules/webgl/WebGL2RenderingContextBase.cpp
+++ third_party/WebKit/Source/modules/webgl/WebGL2RenderingContextBase.cpp
@@ -2816,12 +2816,7 @@ GLboolean WebGL2RenderingContextBase::isQuery(WebGLQuery* query) {
 
 void WebGL2RenderingContextBase::beginQuery(GLenum target, WebGLQuery* query) {
   bool deleted;
-  if (!query) {
-    synthesizeGLError(GL_INVALID_OPERATION, "beginQuery",
-                      "query object is null");
-    return;
-  }
-
+  DCHECK(query);
   if (!checkObjectToBeBound("beginQuery", query, deleted))
     return;
   if (deleted) {
@@ -2981,12 +2976,8 @@ ScriptValue WebGL2RenderingContextBase::getQueryParameter(
     ScriptState* scriptState,
     WebGLQuery* query,
     GLenum pname) {
+  DCHECK(query);
   bool deleted;
-  if (!query) {
-    synthesizeGLError(GL_INVALID_OPERATION, "getQueryParameter",
-                      "query object is null");
-    return ScriptValue::createNull(scriptState);
-  }
   if (!checkObjectToBeBound("getQueryParameter", query, deleted))
     return ScriptValue::createNull(scriptState);
   if (deleted) {
