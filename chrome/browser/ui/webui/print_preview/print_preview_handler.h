114ec20f0c58655081a9e5f023b38af14515cd08
skau <skau@chromium.org>
2016-11-04 17:12:09 -0700

    Present the printer list from preferences for Chrome OS.
    Chrome OS keeps the canonical printer listing in preferences rather than
    in the printing backend.  Add a new codepath on this platform.
    
    CL 1 of 3 to enable printer selection on Chrome OS
    
    BUG=658461
    TEST=Verified that printers from preferences show up in printer selection.
    
    Review-Url: https://codereview.chromium.org/2463473002
    Cr-Commit-Position: refs/heads/master@{#430072}

--- chrome/browser/ui/webui/print_preview/print_preview_handler.h
+++ chrome/browser/ui/webui/print_preview/print_preview_handler.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/features.h"
 #include "components/signin/core/browser/gaia_cookie_manager_service.h"
 #include "content/public/browser/web_ui_message_handler.h"
+#include "printing/backend/print_backend.h"
 #include "printing/features/features.h"
 #include "ui/shell_dialogs/select_file_dialog.h"
 
@@ -215,7 +216,7 @@ class PrintPreviewHandler
       std::unique_ptr<base::DictionaryValue> settings_info);
 
   // Send the list of printers to the Web UI.
-  void SetupPrinterList(const base::ListValue* printers);
+  void SetupPrinterList(const printing::PrinterList& printer_list);
 
   // Send whether cloud print integration should be enabled.
   void SendCloudPrintEnabled();
