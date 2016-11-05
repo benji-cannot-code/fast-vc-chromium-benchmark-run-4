b1155a55e878e3ec0262ca5546ce18d845974835
thomasanderson <thomasanderson@google.com>
2016-11-04 17:47:19 -0700

    Ozone Linux: Fix build errors with chromeos==0 ozone_platform_drm=true
    BUG=295089
    R=rjkroege@chromium.org
    
    Review-Url: https://codereview.chromium.org/2472213002
    Cr-Commit-Position: refs/heads/master@{#430100}

--- ui/ozone/platform/drm/common/client_native_pixmap_dmabuf.cc
+++ ui/ozone/platform/drm/common/client_native_pixmap_dmabuf.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/memory.h"
 #include "base/trace_event/trace_event.h"
 
-#if defined(OS_CHROMEOS)
+#if defined(USE_OZONE)
 // TODO(vignatti): replace the local definitions below with #include
 // <linux/dma-buf.h> once kernel version 4.6 becomes widely used.
 #include <linux/types.h>
