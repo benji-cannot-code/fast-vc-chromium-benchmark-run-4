5250d505a16b8ede86523a95806166f998d4cd4d
vakh <vakh@chromium.org>
2016-11-04 18:32:49 -0700

    Update histogram names to follow the usual PVer4 format: SafeBrowsing.V4*
    The new names also include the module that owns them, which makes it easier to
    understand the histogram.
    
    BUG=651911
    
    Review-Url: https://codereview.chromium.org/2467673002
    Cr-Commit-Position: refs/heads/master@{#430118}

--- components/safe_browsing_db/v4_get_hash_protocol_manager.cc
+++ components/safe_browsing_db/v4_get_hash_protocol_manager.cc
@@ -28,7 +28,7 @@ namespace {
 // Record a GetHash result.
 void RecordGetHashResult(safe_browsing::V4OperationResult result) {
   UMA_HISTOGRAM_ENUMERATION(
-      "SafeBrowsing.GetV4HashResult", result,
+      "SafeBrowsing.V4GetHash.Result", result,
       safe_browsing::V4OperationResult::OPERATION_RESULT_MAX);
 }
 
@@ -68,7 +68,7 @@ enum ParseResultType {
 
 // Record parsing errors of a GetHash result.
 void RecordParseGetHashResult(ParseResultType result_type) {
-  UMA_HISTOGRAM_ENUMERATION("SafeBrowsing.ParseV4HashResult", result_type,
+  UMA_HISTOGRAM_ENUMERATION("SafeBrowsing.V4GetHash.Parse.Result", result_type,
                             PARSE_RESULT_TYPE_MAX);
 }
 
@@ -91,8 +91,8 @@ enum V4FullHashCacheResultType {
 
 // Record a full hash cache hit result.
 void RecordV4FullHashCacheResult(V4FullHashCacheResultType result_type) {
-  UMA_HISTOGRAM_ENUMERATION("SafeBrowsing.V4FullHashCacheResult", result_type,
-                            FULL_HASH_CACHE_RESULT_MAX);
+  UMA_HISTOGRAM_ENUMERATION("SafeBrowsing.V4GetHash.CacheHit.Result",
+                            result_type, FULL_HASH_CACHE_RESULT_MAX);
 }
 
 // Enumerate GetHash hits/misses for histogramming purposes. DO NOT CHANGE THE
@@ -114,7 +114,7 @@ enum V4GetHashCheckResultType {
 
 // Record a GetHash hit result.
 void RecordV4GetHashCheckResult(V4GetHashCheckResultType result_type) {
-  UMA_HISTOGRAM_ENUMERATION("SafeBrowsing.V4GetHashCheckResult", result_type,
+  UMA_HISTOGRAM_ENUMERATION("SafeBrowsing.V4GetHash.Check.Result", result_type,
                             GET_HASH_CHECK_RESULT_MAX);
 }
 
@@ -131,9 +131,6 @@ const char kPhishing[] = "PHISHING";
 }  // namespace
 
 namespace safe_browsing {
-
-const char kUmaV4HashResponseMetricName[] =
-    "SafeBrowsing.GetV4HashHttpResponseOrErrorCode";
 
 // The default V4GetHashProtocolManagerFactory.
 class V4GetHashProtocolManagerFactoryImpl
@@ -697,7 +694,7 @@ void V4GetHashProtocolManager::OnURLFetchComplete(
   int response_code = source->GetResponseCode();
   net::URLRequestStatus status = source->GetStatus();
   V4ProtocolManagerUtil::RecordHttpResponseOrErrorCode(
-      kUmaV4HashResponseMetricName, status, response_code);
+      "SafeBrowsing.V4GetHash.Network.Result", status, response_code);
 
   std::vector<FullHashInfo> full_hash_infos;
   Time negative_cache_expire;
@@ -725,7 +722,7 @@ void V4GetHashProtocolManager::OnURLFetchComplete(
   }
 
   const std::unique_ptr<FullHashCallbackInfo>& fhci = it->second;
-  UMA_HISTOGRAM_LONG_TIMES("SafeBrowsing.V4GetHashNetwork.Time",
+  UMA_HISTOGRAM_LONG_TIMES("SafeBrowsing.V4GetHash.Network.Time",
                            clock_->Now() - fhci->network_start_time);
   UpdateCache(fhci->prefixes_requested, full_hash_infos, negative_cache_expire);
   MergeResults(fhci->full_hash_to_store_and_hash_prefixes, full_hash_infos,
