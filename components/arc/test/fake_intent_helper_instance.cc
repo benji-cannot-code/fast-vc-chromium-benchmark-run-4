c02757e851845fc52acb594b2c921aa8ac9e0f0b
hashimoto <hashimoto@chromium.org>
2016-11-05 21:57:17 -0700

    arc: Add methods to IntentHelperInstance to read ARC contents
    BUG=654684
    
    Review-Url: https://codereview.chromium.org/2469963003
    Cr-Commit-Position: refs/heads/master@{#430185}

--- components/arc/test/fake_intent_helper_instance.cc
+++ components/arc/test/fake_intent_helper_instance.cc
@@ -28,6 +28,10 @@ FakeIntentHelperInstance::~FakeIntentHelperInstance() {}
 void FakeIntentHelperInstance::AddPreferredPackage(
     const mojo::String& package_name) {}
 
+void FakeIntentHelperInstance::GetFileSize(
+    const mojo::String& url,
+    const GetFileSizeCallback& callback) {}
+
 void FakeIntentHelperInstance::HandleIntent(mojom::IntentInfoPtr intent,
                                             mojom::ActivityNamePtr activity) {}
 
@@ -40,6 +44,10 @@ void FakeIntentHelperInstance::HandleUrlList(
     mojom::ActionType action) {}
 
 void FakeIntentHelperInstance::Init(mojom::IntentHelperHostPtr host_ptr) {}
+
+void FakeIntentHelperInstance::OpenFileToRead(
+    const mojo::String& url,
+    const OpenFileToReadCallback& callback) {}
 
 void FakeIntentHelperInstance::RequestActivityIcons(
     mojo::Array<mojom::ActivityNamePtr> activities,
