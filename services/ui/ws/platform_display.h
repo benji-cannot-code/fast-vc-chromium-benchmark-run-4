957083919891e288ea0e81d49d24d236bb131826
kylechar <kylechar@chromium.org>
2016-11-07 14:16:08 -0800

    Remove two functions from FrameGeneratorDelegate.
    Remove GetRootWindow() and GetViewportMetrics(). The root window is
    passed into FrameGenerator already, so there is no need to get the root
    window from a delegate. The root window also has the correct size, so
    there is no need to get viewport metrics from the delegate.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2482793003
    Cr-Commit-Position: refs/heads/master@{#430397}

--- services/ui/ws/platform_display.h
+++ services/ui/ws/platform_display.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/strings/string16.h"
 #include "build/build_config.h"
+#include "services/ui/display/viewport_metrics.h"
 #include "services/ui/public/interfaces/cursor.mojom.h"
 #include "services/ui/public/interfaces/window_manager.mojom.h"
 #include "services/ui/public/interfaces/window_manager_constants.mojom.h"
@@ -163,7 +164,6 @@ class DefaultPlatformDisplay : public PlatformDisplay,
   void OnActivationChanged(bool active) override;
 
   // FrameGeneratorDelegate:
-  ServerWindow* GetRootWindow() override;
   bool IsInHighContrastMode() override;
 
   int64_t id_;
