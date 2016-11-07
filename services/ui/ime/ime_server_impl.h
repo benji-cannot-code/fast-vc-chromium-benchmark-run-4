98585aceaa6b9c0f08e0fe89b2432343b96ce3e6
moshayedi <moshayedi@chromium.org>
2016-11-07 13:57:54 -0800

    IME for Mus: Use the catalog service to decide IME driver's name.
    Previously IME driver's name was hard-coded in IMEServerImpl. This change
    queries the catalog service to decide which driver to connect to.
    
    BUG=641041
    
    Review-Url: https://codereview.chromium.org/2473483008
    Cr-Commit-Position: refs/heads/master@{#430390}

--- services/ui/ime/ime_server_impl.h
+++ services/ui/ime/ime_server_impl.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "mojo/public/cpp/bindings/binding_set.h"
+#include "services/catalog/public/interfaces/catalog.mojom.h"
 #include "services/ui/public/interfaces/ime.mojom.h"
 
 namespace service_manager {
@@ -31,6 +32,10 @@ class IMEServerImpl : public mojom::IMEServer {
   void StartSession(mojom::TextInputClientPtr client,
                     mojom::InputMethodRequest input_method) override;
 
+  void OnGotCatalogEntries(std::vector<catalog::mojom::EntryPtr> entries);
+
+  service_manager::Connector* connector_;
+  catalog::mojom::CatalogPtr catalog_;
   mojo::BindingSet<mojom::IMEServer> bindings_;
   mojom::IMEDriverPtr driver_;
   int current_id_;
