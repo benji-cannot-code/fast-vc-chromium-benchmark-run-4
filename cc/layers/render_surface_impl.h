cb636e2a34dc6dd2a25b251c01e3647b12423ca5
ajuma <ajuma@chromium.org>
2016-11-07 15:36:20 -0800

    Revert of cc: Remove more references to owning_layer_ from RenderSurfaceImpl (patchset #2 id:20001 of https://codereview.chromium.org/2460413003/ )
    Reason for revert:
    Possibly caused a regression in draw properties computation time (see http://crbug.com/663068).
    
    Original issue's description:
    > cc: Remove more references to owning_layer_ from RenderSurfaceImpl
    >
    > This removes more dependencies of RenderSurfaceImpl on its owning layer.
    > Specifically, this removes the use of owning_layer_ for accessing
    > property trees and debug colors, and for gettng the surface's
    > effect tree index. This also renames the OwningLayerId() method to id().
    >
    > BUG=611883
    > CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    >
    > Committed: https://crrev.com/740a49caf190bab13f8fdfd0979b29e09752da6b
    > Cr-Commit-Position: refs/heads/master@{#428991}
    
    TBR=jaydasika@chromium.org,weiliangc@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=611883,663068
    
    Review-Url: https://codereview.chromium.org/2483033002
    Cr-Commit-Position: refs/heads/master@{#430419}

--- cc/layers/render_surface_impl.h
+++ cc/layers/render_surface_impl.h
@@ -32,7 +32,6 @@ class RenderPassId;
 class RenderPassSink;
 class LayerImpl;
 class LayerIterator;
-class LayerTreeImpl;
 
 struct AppendQuadsData;
 
@@ -126,7 +125,7 @@ class CC_EXPORT RenderSurfaceImpl {
   LayerImplList& layer_list() { return layer_list_; }
   void ClearLayerLists();
 
-  int id() const { return stable_effect_id_; }
+  int OwningLayerId() const;
 
   LayerImpl* MaskLayer();
   bool HasMask() const;
@@ -169,9 +168,6 @@ class CC_EXPORT RenderSurfaceImpl {
   const EffectNode* OwningEffectNode() const;
 
   LayerImpl* owning_layer_;
-
-  LayerTreeImpl* layer_tree_impl_;
-  int stable_effect_id_;
 
   // Container for properties that render surfaces need to compute before they
   // can be drawn.
