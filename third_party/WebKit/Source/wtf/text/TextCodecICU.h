4003fe9188d8be16948a05ae1138b3f9d4cd5c0e
jsbell <jsbell@chromium.org>
2016-11-07 12:17:32 -0800

    Don't initialize ICU replacement character for UTF-32 encodings
    The TextCodecICU wrapper erroneously called ucnv_setSubstChars() with
    only a single byte replacement for non-byte-based encodings -
    basically the UTF-32 family. The code path is only exercised when
    computing names for files in form uploads. This would hit an ASSERT in
    debug builds, and result in an empty filename in release builds.
    
    BUG=662146,417850
    R=tkent@chromium.org
    
    Review-Url: https://codereview.chromium.org/2478653002
    Cr-Commit-Position: refs/heads/master@{#430356}

--- third_party/WebKit/Source/wtf/text/TextCodecICU.h
+++ third_party/WebKit/Source/wtf/text/TextCodecICU.h
@@ -88,6 +88,8 @@ class TextCodecICU final : public TextCodec {
 #endif
 
   FRIEND_TEST_ALL_PREFIXES(TextCodecICUTest, IgnorableCodePoint);
+  FRIEND_TEST_ALL_PREFIXES(TextCodecICUTest, UTF32AndQuestionMarks);
+  FRIEND_TEST_ALL_PREFIXES(TextCodecICUTest, UTF32Aliases);
 };
 
 struct ICUConverterWrapper {
