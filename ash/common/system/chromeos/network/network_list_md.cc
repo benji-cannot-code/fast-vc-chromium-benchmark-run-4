2c89fd727bfe11e067b31de30f1b7e910151104f
varkha <varkha@chromium.org>
2016-11-06 19:57:24 -0800

    [ash-md] Makes Wi-Fi header row sticky when network list is scrolled
    Adds support for sticky header rows to tray icon scroll views. When
    marked as sticky the headers will not scroll above the top of the
    visible viewport unless there is another sticky header row just below.
    Multiple rows can be indicated as header rows by setting View::id_.
    
    BUG=631831
    
    Review-Url: https://codereview.chromium.org/2453133002
    Cr-Commit-Position: refs/heads/master@{#430214}

--- ash/common/system/chromeos/network/network_list_md.cc
+++ ash/common/system/chromeos/network/network_list_md.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/common/system/chromeos/network/network_icon_animation.h"
 #include "ash/common/system/chromeos/network/network_info.h"
 #include "ash/common/system/chromeos/network/network_list_delegate.h"
+#include "ash/common/system/tray/tray_constants.h"
 #include "base/memory/ptr_util.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/power_manager/power_supply_properties.pb.h"
@@ -28,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/font.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/gfx/vector_icons_public.h"
+#include "ui/views/background.h"
 #include "ui/views/border.h"
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/button/toggle_button.h"
@@ -47,15 +49,13 @@ namespace ash {
 
 namespace {
 
+// TODO(varkha): Merge some of those those constants in tray_constants.h
 const int kWiFiButtonSize = 48;
 const int kWifiRowVerticalInset = 4;
 const int kWifiRowLeftInset = 18;
 const int kWifiRowRightInset = 14;
-const int kWifiRowSeparatorThickness = 1;
 const int kWifiRowChildSpacing = 14;
 const int kFocusBorderInset = 1;
-
-const SkColor kWifiRowSeparatorColor = SkColorSetA(SK_ColorBLACK, 0x1F);
 
 bool IsProhibitedByPolicy(const chromeos::NetworkState* network) {
   if (!NetworkTypePattern::WiFi().MatchesType(network->type()))
@@ -117,11 +117,13 @@ class NetworkListViewMd::WifiHeaderRowView : public views::View {
     return GetPreferredSize().height();
   }
 
+  const char* GetClassName() const override { return "WifiHeaderRowView"; }
+
  private:
   void Init() {
+    set_id(kHeaderRowId);
+    set_background(views::Background::CreateSolidBackground(kBackgroundColor));
     // TODO(tdanderson): Need to unify this with the generic menu row class.
-    SetBorder(views::Border::CreateSolidSidedBorder(
-        kWifiRowSeparatorThickness, 0, 0, 0, kWifiRowSeparatorColor));
     views::View* container = new views::View;
     container->SetBorder(views::Border::CreateEmptyBorder(
         0, kWifiRowLeftInset, 0, kWifiRowRightInset));
--- ash/common/system/tray/tray_constants.cc
+++ ash/common/system/tray/tray_constants.cc
@@ -78,6 +78,8 @@ const int kTrayNotificationContentsWidth =
     kTrayPopupMinWidth - (kNotificationIconWidth + kNotificationButtonWidth +
                           (kTrayPopupPaddingHorizontal / 2) * 3);
 
+const int kHeaderRowId = -1;
+
 const int kTrayIconSize = 16;
 const SkColor kTrayIconColor = SK_ColorWHITE;
 const int kMenuIconSize = 20;
