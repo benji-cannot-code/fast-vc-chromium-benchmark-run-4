10018188445ca1023cbfa11430c47df88b16fd6b
rtoy <rtoy@chromium.org>
2016-11-07 15:04:14 -0800

    Remove ZeroPole class and files
    The ZeroPole class is not used anywhere and therefore not tested.
    Remove it.
    
    BUG=657033
    TEST=compile still works
    
    Review-Url: https://codereview.chromium.org/2432473002
    Cr-Commit-Position: refs/heads/master@{#430406}

--- third_party/WebKit/Source/platform/audio/DynamicsCompressor.h
+++ third_party/WebKit/Source/platform/audio/DynamicsCompressor.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "platform/audio/AudioArray.h"
 #include "platform/audio/DynamicsCompressorKernel.h"
-#include "platform/audio/ZeroPole.h"
 #include "wtf/Allocator.h"
 #include "wtf/Noncopyable.h"
 #include <memory>
