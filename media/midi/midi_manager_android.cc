1a867be0561c8a33bbf0c260e52cbcec669ba17d
toyoshim <toyoshim@chromium.org>
2016-11-07 03:45:44 -0800

    Web MIDI: convert Android native API switch to a field trial
    To ship new MIDI backend for Android in a safe way, let me
    change a existing flag to a feature list flag.
    
    BUG=650913
    
    Review-Url: https://codereview.chromium.org/2445723002
    Cr-Commit-Position: refs/heads/master@{#430252}

--- media/midi/midi_manager_android.cc
+++ media/midi/midi_manager_android.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/build_info.h"
 #include "base/android/context_utils.h"
-#include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/stringprintf.h"
 #include "jni/MidiManagerAndroid_jni.h"
@@ -26,8 +26,7 @@ namespace midi {
 MidiManager* MidiManager::Create() {
   auto sdk_version = base::android::BuildInfo::GetInstance()->sdk_int();
   if (sdk_version <= base::android::SDK_VERSION_LOLLIPOP_MR1 ||
-      !base::CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kUseAndroidMidiApi)) {
+      !base::FeatureList::IsEnabled(features::kMidiManagerAndroid)) {
     return new MidiManagerUsb(std::unique_ptr<UsbMidiDevice::Factory>(
         new UsbMidiDeviceFactoryAndroid));
   }
--- media/midi/midi_switches.h
+++ media/midi/midi_switches.h
@@ -12,16 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "media/midi/midi_export.h"
 
-namespace switches {
-
-#if defined(OS_ANDROID)
-MIDI_EXPORT extern const char kUseAndroidMidiApi[];
-#endif
-
-}  // namespace switches
-
 namespace midi {
 namespace features {
+
+#if defined(OS_ANDROID)
+MIDI_EXPORT extern const base::Feature kMidiManagerAndroid;
+#endif
 
 #if defined(OS_WIN)
 MIDI_EXPORT extern const base::Feature kMidiManagerWinrt;
