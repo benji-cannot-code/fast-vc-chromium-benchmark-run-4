33a03136f3501bcc9ab79e53625bf2539175eaf9
xunjieli <xunjieli@chromium.org>
2016-11-07 15:26:11 -0800

    Fix GzipSourceStream handling of uncompressed output
    When there is not enough output buffer space,
    GzipSourceStream::FilterData() should break early.
    This CL modifies GzipSourceStreamTest#PassThroughAfterEOF to catch this
    situation and additionally cleans up other tests to test the case of
    small output buffer size.
    
    BUG=662950
    
    Review-Url: https://codereview.chromium.org/2485693002
    Cr-Commit-Position: refs/heads/master@{#430413}

--- net/filter/gzip_source_stream.cc
+++ net/filter/gzip_source_stream.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/filter/gzip_source_stream.h"
 
+#include <algorithm>
+#include <utility>
+
 #include "base/bind.h"
 #include "base/bit_cast.h"
 #include "base/logging.h"
@@ -85,7 +88,7 @@ int GzipSourceStream::FilterData(IOBuffer* output_buffer,
   int input_data_size = input_buffer_size;
   int bytes_out = 0;
   bool state_compressed_entered = false;
-  while (input_data_size > 0) {
+  while (input_data_size > 0 && bytes_out < output_buffer_size) {
     InputState state = input_state_;
     switch (state) {
       case STATE_START: {
@@ -160,16 +163,12 @@ int GzipSourceStream::FilterData(IOBuffer* output_buffer,
         bytes_out = output_buffer_size - zlib_stream_.get()->avail_out;
         input_data_size -= bytes_used;
         input_data += bytes_used;
-        if (ret == Z_STREAM_END) {
+        if (ret == Z_STREAM_END)
           input_state_ = STATE_GZIP_FOOTER;
-          break;
-        }
-        // Return early here since zlib has written as much data to
-        // |output_buffer| as it could. There might still be some unconsumed
-        // data in |input_buffer| if there is no space in |output_buffer|.
-        DCHECK_EQ(Z_OK, ret);
-        *consumed_bytes = input_buffer_size - input_data_size;
-        return bytes_out;
+        // zlib has written as much data to |output_buffer| as it could.
+        // There might still be some unconsumed data in |input_buffer| if there
+        // is no space in |output_buffer|.
+        break;
       }
       case STATE_GZIP_FOOTER: {
         size_t to_read = std::min(gzip_footer_bytes_left_,
--- net/filter/gzip_source_stream.h
+++ net/filter/gzip_source_stream.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define NET_FILTER_GZIP_SOURCE_STREAM_H_
 
 #include <memory>
+#include <string>
 
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
