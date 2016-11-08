549ebbe5dc3700e514efbe599cad6397a0f75c4a
dpapad <dpapad@chromium.org>
2016-11-07 17:08:52 -0800

    MD Settings: Close edit startup URL dialog, if underlying URL list changes.
    BUG=661867
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2477693005
    Cr-Commit-Position: refs/heads/master@{#430455}

--- ui/webui/resources/cr_elements/cr_scrollable_behavior.js
+++ ui/webui/resources/cr_elements/cr_scrollable_behavior.js
@@ -37,6 +37,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /** @polymerBehavior */
 var CrScrollableBehavior = {
+  properties: {
+    /** @private */
+    intervalId_: Number,
+  },
+
   ready: function() {
     var scrollableElements = this.root.querySelectorAll('[scrollable]');
 
@@ -51,6 +56,11 @@ var CrScrollableBehavior = {
       scrollable.addEventListener('scroll', this.updateScrollEvent_.bind(this));
   },
 
+  detached: function() {
+    if (this.intervalId_)
+      clearInterval(this.intervalId_);
+  },
+
   /**
    * Called any time the contents of a scrollable container may have changed.
    * This ensures that the <iron-list> contents of dynamically sized
@@ -59,7 +69,7 @@ var CrScrollableBehavior = {
   updateScrollableContents() {
     let nodeList = this.root.querySelectorAll('[scrollable] iron-list');
     // Use setTimeout to avoid initial render / sizing issues.
-    let intervalId = setInterval(function() {
+    this.intervalId_ = setInterval(function() {
       let unreadyNodes = [];
       for (let node of nodeList) {
         if (node.parentNode.scrollHeight == 0) {
@@ -70,10 +80,10 @@ var CrScrollableBehavior = {
         ironList.notifyResize();
       }
       if (unreadyNodes.length == 0)
-        clearInterval(intervalId);
+        clearInterval(this.intervalId_);
       else
         nodeList = unreadyNodes;
-    }, 10);
+    }.bind(this), 10);
   },
 
   /**
