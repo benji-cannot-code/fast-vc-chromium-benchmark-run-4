ef5efc206bb265d2c1301f8562e99953686dd8d4
allada <allada@chromium.org>
2016-11-08 15:48:25 -0800

    [Devtools] Renamed Network's Timeline to Waterfall
    This patch renames all references of network's timeline to waterfall.
    This should help with reducing the name conflict of Timeline vs
    Network's timeline.
    
    R=dgozman,lushnikov
    BUG=653738
    
    Review-Url: https://codereview.chromium.org/2483853004
    Cr-Commit-Position: refs/heads/master@{#430769}

--- third_party/WebKit/Source/devtools/front_end/network/NetworkWaterfallColumn.js
+++ third_party/WebKit/Source/devtools/front_end/network/NetworkWaterfallColumn.js
@@ -5,15 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * @unrestricted
  */
-WebInspector.NetworkTimelineColumn = class extends WebInspector.VBox {
+WebInspector.NetworkWaterfallColumn = class extends WebInspector.VBox {
   /**
    * @param {number} rowHeight
    * @param {!WebInspector.NetworkTimeCalculator} calculator
    */
   constructor(rowHeight, calculator) {
-    // TODO(allada) Make this a shadowDOM when the NetworkTimelineColumn gets moved into NetworkLogViewColumns.
+    // TODO(allada) Make this a shadowDOM when the NetworkWaterfallColumn gets moved into NetworkLogViewColumns.
     super(false);
-    this.registerRequiredCSS('network/networkTimelineColumn.css');
+    this.registerRequiredCSS('network/networkWaterfallColumn.css');
 
     this._canvas = this.contentElement.createChild('canvas');
     this._canvas.tabIndex = 1;
@@ -192,7 +192,7 @@ WebInspector.NetworkTimelineColumn = class extends WebInspector.VBox {
   /**
    * @param {number=} scrollTop
    * @param {!Map<string, !Array<number>>=} eventDividers
-   * @param {!WebInspector.NetworkTimelineColumn.RequestData=} requestData
+   * @param {!WebInspector.NetworkWaterfallColumn.RequestData=} requestData
    */
   update(scrollTop, eventDividers, requestData) {
     if (scrollTop !== undefined)
@@ -331,7 +331,7 @@ WebInspector.NetworkTimelineColumn = class extends WebInspector.VBox {
   /**
    * @return {number}
    */
-  _timelineDuration() {
+  _waterfallDuration() {
     return this._calculator.maximumBoundary() - this._calculator.minimumBoundary();
   }
 
@@ -363,7 +363,7 @@ WebInspector.NetworkTimelineColumn = class extends WebInspector.VBox {
     var resourceType = request.resourceType();
     if (this._borderColorsForResourceTypeCache.has(resourceType))
       return this._borderColorsForResourceTypeCache.get(resourceType);
-    var colorsForResourceType = WebInspector.NetworkTimelineColumn._colorsForResourceType;
+    var colorsForResourceType = WebInspector.NetworkWaterfallColumn._colorsForResourceType;
     var color = colorsForResourceType[resourceType] || colorsForResourceType.Other;
     var parsedColor = WebInspector.Color.parse(color);
     var hsla = parsedColor.hsla();
@@ -380,7 +380,7 @@ WebInspector.NetworkTimelineColumn = class extends WebInspector.VBox {
    * @return {string|!CanvasGradient}
    */
   _colorForResourceType(context, request) {
-    var colorsForResourceType = WebInspector.NetworkTimelineColumn._colorsForResourceType;
+    var colorsForResourceType = WebInspector.NetworkWaterfallColumn._colorsForResourceType;
     var resourceType = request.resourceType();
     var color = colorsForResourceType[resourceType] || colorsForResourceType.Other;
     if (request.cached())
@@ -591,7 +591,7 @@ WebInspector.NetworkTimelineColumn = class extends WebInspector.VBox {
 
     /**
      * @return {string}
-     * @this {WebInspector.NetworkTimelineColumn}
+     * @this {WebInspector.NetworkWaterfallColumn}
      */
     function getRowColor() {
       if (this._hoveredRequest === request)
@@ -614,9 +614,9 @@ WebInspector.NetworkTimelineColumn = class extends WebInspector.VBox {
 /**
  * @typedef {{requests: !Array<!WebInspector.NetworkRequest>, navigationRequest: ?WebInspector.NetworkRequest}}
  */
-WebInspector.NetworkTimelineColumn.RequestData;
+WebInspector.NetworkWaterfallColumn.RequestData;
 
-WebInspector.NetworkTimelineColumn._colorsForResourceType = {
+WebInspector.NetworkWaterfallColumn._colorsForResourceType = {
   document: 'hsl(215, 100%, 80%)',
   font: 'hsl(8, 100%, 80%)',
   media: 'hsl(272, 64%, 80%)',
