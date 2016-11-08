9f3ff069485169379520cb0770ab0f0cfea66acb
yurak <yurak@google.com>
2016-11-07 19:14:34 -0800

    https://html.spec.whatwg.org/multipage/scripting.html#look-up-a-custom-element-definition
    Steps 1-3 already implemented in:
        /core/html/parser/HTMLConstructionSite.cpp
    
    Steps 4-6 made a little faster in CustomElementRegistry.cpp
    
    Unit tests added for custom element definition look up.
    
    Replaced uses of definitionForName with definitionFor.
        CustomElementRegistry.cpp
        CustomElement.cpp
    There are other calls to definitionForName, but those are fine according to spec.
    
    I'll do another patch to use the 'is' attribute in definitionFor. Right now it's using the localName twice.
    
    BUG=648828
    
    Review-Url: https://codereview.chromium.org/2446903008
    Cr-Commit-Position: refs/heads/master@{#430498}

--- third_party/WebKit/Source/core/dom/custom/CustomElement.cpp
+++ third_party/WebKit/Source/core/dom/custom/CustomElement.cpp
@@ -70,10 +70,11 @@ bool CustomElement::shouldCreateCustomElement(const QualifiedName& tagName) {
          tagName.namespaceURI() == HTMLNames::xhtmlNamespaceURI;
 }
 
-static CustomElementDefinition* definitionForName(const Document& document,
-                                                  const QualifiedName& name) {
+static CustomElementDefinition* definitionFor(
+    const Document& document,
+    const CustomElementDescriptor desc) {
   if (CustomElementRegistry* registry = CustomElement::registry(document))
-    return registry->definitionForName(name.localName());
+    return registry->definitionFor(desc);
   return nullptr;
 }
 
@@ -89,8 +90,9 @@ HTMLElement* CustomElement::createCustomElementSync(
     Document& document,
     const QualifiedName& tagName) {
   DCHECK(shouldCreateCustomElement(tagName));
-  if (CustomElementDefinition* definition =
-          definitionForName(document, tagName))
+  if (CustomElementDefinition* definition = definitionFor(
+          document,
+          CustomElementDescriptor(tagName.localName(), tagName.localName())))
     return definition->createElementSync(document, tagName);
   return createUndefinedElement(document, tagName);
 }
@@ -104,8 +106,9 @@ HTMLElement* CustomElement::createCustomElementAsync(
   // https://dom.spec.whatwg.org/#concept-create-element
   // 6. If definition is non-null, then:
   // 6.2. If the synchronous custom elements flag is not set:
-  if (CustomElementDefinition* definition =
-          definitionForName(document, tagName))
+  if (CustomElementDefinition* definition = definitionFor(
+          document,
+          CustomElementDescriptor(tagName.localName(), tagName.localName())))
     return definition->createElementAsync(document, tagName);
 
   return createUndefinedElement(document, tagName);
@@ -210,8 +213,8 @@ void CustomElement::tryToUpgrade(Element* element) {
   CustomElementRegistry* registry = CustomElement::registry(*element);
   if (!registry)
     return;
-  if (CustomElementDefinition* definition =
-          registry->definitionForName(element->localName()))
+  if (CustomElementDefinition* definition = registry->definitionFor(
+          CustomElementDescriptor(element->localName(), element->localName())))
     definition->enqueueUpgradeReaction(element);
   else
     registry->addCandidate(element);
