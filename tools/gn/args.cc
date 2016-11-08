8fc48579dd2f6ab8bd7121bdd76e97f986927249
brettw <brettw@chromium.org>
2016-11-08 11:10:03 -0800

    Convert gn docstrings to C++11 raw strings.
    This makes them more readable and easier to modify.
    
    The documentation was un-indented and re-wrapped to 80 cols which makes it fill out the column on the screen better.
    
    A few tests were updated to use raw strings as well, but converting all of them seemed not worth the effort.
    
    There is a documentation fix in the set_defaults (added [] around the configs in the example), and an obsolete line in the group documentation was removed ("exactly like depending on its deps).
    
    Review-Url: https://codereview.chromium.org/2481423002
    Cr-Commit-Position: refs/heads/master@{#430679}

--- tools/gn/args.cc
+++ tools/gn/args.cc
@@ -12,13 +12,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 const char kBuildArgs_Help[] =
     R"(Build Arguments Overview
 
-  Build arguments are variables passed in from outside of the build
-  that build files can query to determine how the build works.
+  Build arguments are variables passed in from outside of the build that build
+  files can query to determine how the build works.
 
 How build arguments are set
 
-  First, system default arguments are set based on the current system.
-  The built-in arguments are:
+  First, system default arguments are set based on the current system. The
+  built-in arguments are:
    - host_cpu
    - host_os
    - current_cpu
@@ -26,20 +26,18 @@ How build arguments are set
    - target_cpu
    - target_os
 
-  If specified, arguments from the --args command line flag are used. If
-  that flag is not specified, args from previous builds in the build
-  directory will be used (this is in the file args.gn in the build
-  directory).
+  If specified, arguments from the --args command line flag are used. If that
+  flag is not specified, args from previous builds in the build directory will
+  be used (this is in the file args.gn in the build directory).
 
-  Last, for targets being compiled with a non-default toolchain, the
-  toolchain overrides are applied. These are specified in the
-  toolchain_args section of a toolchain definition. The use-case for
-  this is that a toolchain may be building code for a different
-  platform, and that it may want to always specify Posix, for example.
-  See "gn help toolchain" for more.
+  Last, for targets being compiled with a non-default toolchain, the toolchain
+  overrides are applied. These are specified in the toolchain_args section of a
+  toolchain definition. The use-case for this is that a toolchain may be
+  building code for a different platform, and that it may want to always
+  specify Posix, for example. See "gn help toolchain" for more.
 
-  If you specify an override for a build argument that never appears in
-  a "declare_args" call, a nonfatal error will be displayed.
+  If you specify an override for a build argument that never appears in a
+  "declare_args" call, a nonfatal error will be displayed.
 
 Examples
 
@@ -50,22 +48,21 @@ Examples
           os="android"
 
   gn gen out/FooBar --args="enable_doom_melon=true os=\"android\""
-      This will overwrite the build directory with the given arguments.
-      (Note that the quotes inside the args command will usually need to
-      be escaped for your shell to pass through strings values.)
+      This will overwrite the build directory with the given arguments. (Note
+      that the quotes inside the args command will usually need to be escaped
+      for your shell to pass through strings values.)
 
 How build arguments are used
 
-  If you want to use an argument, you use declare_args() and specify
-  default values. These default values will apply if none of the steps
-  listed in the "How build arguments are set" section above apply to
-  the given argument, but the defaults will not override any of these.
+  If you want to use an argument, you use declare_args() and specify default
+  values. These default values will apply if none of the steps listed in the
+  "How build arguments are set" section above apply to the given argument, but
+  the defaults will not override any of these.
 
-  Often, the root build config file will declare global arguments that
-  will be passed to all buildfiles. Individual build files can also
-  specify arguments that apply only to those files. It is also useful
-  to specify build args in an "import"-ed file if you want such
-  arguments to apply to multiple buildfiles.
+  Often, the root build config file will declare global arguments that will be
+  passed to all buildfiles. Individual build files can also specify arguments
+  that apply only to those files. It is also useful to specify build args in an
+  "import"-ed file if you want such arguments to apply to multiple buildfiles.
 )";
 
 namespace {
