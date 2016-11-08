acf36aff10126e562d7acc5d7d5eec65e90a2be0
piman <piman@chromium.org>
2016-11-08 10:35:35 -0800

    Make SurfaceFactory lifetime match frame_sink_id_ registration in DelegatedFrameHostAndroid
    This will allow further simplification/refactoring.
    
    BUG=657959
    
    Review-Url: https://codereview.chromium.org/2476303004
    Cr-Commit-Position: refs/heads/master@{#430666}

--- ui/android/delegated_frame_host_android.cc
+++ ui/android/delegated_frame_host_android.cc
@@ -86,6 +86,8 @@ DelegatedFrameHostAndroid::DelegatedFrameHostAndroid(
       ui::ContextProviderFactory::GetInstance()->GetSurfaceManager();
   surface_id_allocator_.reset(new cc::SurfaceIdAllocator());
   surface_manager_->RegisterFrameSinkId(frame_sink_id_);
+  surface_factory_ = base::WrapUnique(
+      new cc::SurfaceFactory(frame_sink_id_, surface_manager_, this));
 
   background_layer_->SetBackgroundColor(background_color);
   view_->GetLayer()->AddChild(background_layer_);
@@ -107,11 +109,6 @@ DelegatedFrameHostAndroid::FrameData::~FrameData() = default;
 void DelegatedFrameHostAndroid::SubmitCompositorFrame(
     cc::CompositorFrame frame,
     cc::SurfaceFactory::DrawCallback draw_callback) {
-  if (!surface_factory_) {
-    surface_factory_ = base::WrapUnique(
-        new cc::SurfaceFactory(frame_sink_id_, surface_manager_, this));
-  }
-
   cc::RenderPass* root_pass = frame.render_pass_list.back().get();
   gfx::Size surface_size = root_pass->output_rect.size();
 
@@ -192,7 +189,6 @@ void DelegatedFrameHostAndroid::DestroyDelegatedContent() {
   if (!current_frame_)
     return;
 
-  DCHECK(surface_factory_.get());
   DCHECK(content_layer_);
 
   content_layer_->RemoveFromParent();
@@ -209,7 +205,7 @@ bool DelegatedFrameHostAndroid::HasDelegatedContent() const {
 
 void DelegatedFrameHostAndroid::CompositorFrameSinkChanged() {
   DestroyDelegatedContent();
-  surface_factory_.reset();
+  surface_factory_->Reset();
 }
 
 void DelegatedFrameHostAndroid::UpdateBackgroundColor(SkColor color) {
