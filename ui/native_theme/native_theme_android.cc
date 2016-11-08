244b1dd8593f4eff9832d9dd065f8eb085ac0b9c
scheib <scheib@chromium.org>
2016-11-07 17:26:34 -0800

    Revert of Clean up some NativeTheme code. (patchset #2 id:20001 of https://codereview.chromium.org/2480993002/ )
    Reason for revert:
    Scroll bars are appearing as errors in layout tests: e.g.
    fast/forms/select/listbox-appearance-basic.html
    
    https://test-results.appspot.com/dashboards/flakiness_dashboard.html#tests=fast%2Fforms%2Fselect%2Flistbox-appearance-basic.html&testType=webkit_tests
    
    This rollback is speculative, but there are few possibilities in the blame list
    https://chromium.googlesource.com/chromium/src/+log/40639df0c3a5e05761a846b2fe116961d87c9c62%5E..99453a7509f05ca729e66d774f5ae2817c3988c3?pretty=fuller
    
    Original issue's description:
    > Clean up some NativeTheme code.
    >
    > - improve instantiation
    > - allow to specify separate instances for web and native
    > - two NativeThemeAura instances coexist for w/wo overlay scrollbars
    >
    > BUG=307091
    >
    > Committed: https://crrev.com/99453a7509f05ca729e66d774f5ae2817c3988c3
    > Cr-Commit-Position: refs/heads/master@{#430381}
    
    TBR=sky@chromium.org,bokan@chromium.org,estade@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=307091
    
    Review-Url: https://codereview.chromium.org/2484043003
    Cr-Commit-Position: refs/heads/master@{#430463}

--- ui/native_theme/native_theme_android.cc
+++ ui/native_theme/native_theme_android.cc
@@ -21,11 +21,6 @@ const int kCheckboxAndRadioHeight = 16;
 NativeTheme* NativeTheme::GetInstanceForWeb() {
   return NativeThemeAndroid::instance();
 }
-
-NativeTheme* NativeTheme::GetInstanceForNativeUi() {
-  NOTREACHED();
-  return nullptr;
-}
 #endif
 
 // static
--- ui/native_theme/native_theme_aura.h
+++ ui/native_theme/native_theme_aura.h
@@ -15,10 +15,9 @@ namespace ui {
 class NATIVE_THEME_EXPORT NativeThemeAura : public NativeThemeBase {
  public:
   static NativeThemeAura* instance();
-  static NativeThemeAura* web_instance();
 
  protected:
-  explicit NativeThemeAura(bool use_overlay_scrollbars);
+  NativeThemeAura();
   ~NativeThemeAura() override;
 
   // Overridden from NativeThemeBase:
@@ -54,8 +53,6 @@ class NATIVE_THEME_EXPORT NativeThemeAura : public NativeThemeBase {
                         const ExtraParams& extra) const override;
 
  private:
-  bool use_overlay_scrollbars_;
-
   DISALLOW_COPY_AND_ASSIGN(NativeThemeAura);
 };
 
