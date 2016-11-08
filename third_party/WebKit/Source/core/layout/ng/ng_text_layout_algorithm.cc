fd4f5f8d960fb1388858a4c301215874ac986f9a
ikilpatrick <ikilpatrick@chromium.org>
2016-11-07 17:17:00 -0800

    [LayoutNG] Initial scaffolding for inline-level layout.
    BUG=636993
    
    Review-Url: https://codereview.chromium.org/2481743002
    Cr-Commit-Position: refs/heads/master@{#430459}

---
+++ third_party/WebKit/Source/core/layout/ng/ng_text_layout_algorithm.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/layout/ng/ng_text_layout_algorithm.h"
+
+#include "core/layout/ng/ng_constraint_space.h"
+
+namespace blink {
+
+NGTextLayoutAlgorithm::NGTextLayoutAlgorithm(
+    NGInlineBox* inline_box,
+    NGConstraintSpace* constraint_space)
+    : inline_box_(inline_box), constraint_space_(constraint_space) {
+  DCHECK(inline_box_);
+}
+
+bool NGTextLayoutAlgorithm::Layout(NGPhysicalFragment** out) {
+  // TODO(layout-dev): implement.
+  *out = nullptr;
+  return true;
+}
+
+DEFINE_TRACE(NGTextLayoutAlgorithm) {
+  NGLayoutAlgorithm::trace(visitor);
+  visitor->trace(inline_box_);
+  visitor->trace(constraint_space_);
+}
+
+}  // namespace blink
