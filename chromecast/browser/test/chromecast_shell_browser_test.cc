870109021ed66bb9302713818fd8a66a3ffee007
alokp <alokp@chromium.org>
2016-11-08 13:40:36 -0800

    [chromecast] Merge ChromecastBrowserTestHelper into ChromecastBrowserTest.
    BUG=663067
    TEST=cast_shell_browsertests
    
    Review-Url: https://codereview.chromium.org/2485843002
    Cr-Commit-Position: refs/heads/master@{#430724}

--- chromecast/browser/test/chromecast_shell_browser_test.cc
+++ chromecast/browser/test/chromecast_shell_browser_test.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_split.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chromecast/browser/test/chromecast_browser_test.h"
-#include "chromecast/browser/test/chromecast_browser_test_helper.h"
 #include "chromecast/chromecast_features.h"
 #include "content/public/test/browser_test_utils.h"
 #include "media/base/test_data_util.h"
@@ -28,7 +27,7 @@ class ChromecastShellBrowserTest : public ChromecastBrowserTest {
 
   void LoadAboutBlank() {
     content::WebContents* web_contents =
-        helper_->NavigateToURL(GURL(url::kAboutBlankURL));
+        NavigateToURL(GURL(url::kAboutBlankURL));
     content::TitleWatcher title_watcher(
         web_contents, base::ASCIIToUTF16(url::kAboutBlankURL));
     base::string16 result = title_watcher.WaitAndGetTitle();
@@ -63,7 +62,7 @@ class ChromecastShellBrowserTest : public ChromecastBrowserTest {
 
   std::string RunTest(const GURL& gurl,
                       const std::string& expected_title) {
-    content::WebContents* web_contents = helper_->NavigateToURL(gurl);
+    content::WebContents* web_contents = NavigateToURL(gurl);
     content::TitleWatcher title_watcher(web_contents,
                                         base::ASCIIToUTF16(expected_title));
     title_watcher.AlsoWaitForTitle(base::ASCIIToUTF16(kEnded));
