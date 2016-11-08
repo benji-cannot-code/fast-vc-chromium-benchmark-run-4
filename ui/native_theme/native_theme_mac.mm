6994af644f5234065c7333fb714eb22ea82a0ad9
xlai <xlai@chromium.org>
2016-11-08 06:49:54 -0800

    Revert of Clean up some NativeTheme code. (patchset #1 id:1 of https://codereview.chromium.org/2485013002/ )
    Reason for revert:
    Apparently CQ landed this CL twice. Tried reverting it once to see what happens.
    
    Original issue's description:
    > Reland of Clean up some NativeTheme code.
    >  ( https://codereview.chromium.org/2480993002/ )
    >
    > Now with appropriate use of break in switch statement.
    >
    > Original issue's description:
    > > Clean up some NativeTheme code.
    > >
    > > - improve instantiation
    > > - allow to specify separate instances for web and native
    > > - two NativeThemeAura instances coexist for w/wo overlay scrollbars
    > >
    > > BUG=307091
    >
    > Committed: https://crrev.com/93ed99a0238f25fe391874d3f6dfa02c01acb781
    > Cr-Commit-Position: refs/heads/master@{#430608}
    
    TBR=bokan@chromium.org,sky@chromium.org,estade@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2484973003
    Cr-Commit-Position: refs/heads/master@{#430610}

--- ui/native_theme/native_theme_mac.mm
+++ ui/native_theme/native_theme_mac.mm
@@ -119,11 +119,6 @@ NativeTheme* NativeTheme::GetInstanceForNativeUi() {
 }
 
 // static
-NativeTheme* NativeTheme::GetInstanceForNativeUi() {
-  return NativeThemeMac::instance();
-}
-
-// static
 NativeThemeMac* NativeThemeMac::instance() {
   CR_DEFINE_STATIC_LOCAL(NativeThemeMac, s_native_theme, ());
   return &s_native_theme;
--- ui/native_theme/native_theme_win.cc
+++ ui/native_theme/native_theme_win.cc
@@ -151,10 +151,6 @@ class ScopedCreateDCWithBitmap {
 
 namespace ui {
 
-NativeTheme* NativeTheme::GetInstanceForNativeUi() {
-  return NativeThemeWin::instance();
-}
-
 bool NativeThemeWin::IsThemingActive() const {
   return is_theme_active_ && is_theme_active_();
 }
