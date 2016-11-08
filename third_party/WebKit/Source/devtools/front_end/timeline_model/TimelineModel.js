1d0f2334e2d936eef5e1b2fa80ab21a39f6a8436
caseq <caseq@chromium.org>
2016-11-08 13:31:05 -0800

    DevTools: add per-frame grouping into aggregated timeline details
    - add frame list to TracingStartedInPage
    - add frame details (URL, name) to CommitLoad
    - refactor event grouping in the top-down tree to never prematurely
        merge events that would end up in different groups adter grouping.
    - move GroupBy enum from WI.TimelineAggregator to AggregatedTimelineTreeView;
    
    BUG=662990
    
    Review-Url: https://codereview.chromium.org/2473283003
    Cr-Commit-Position: refs/heads/master@{#430719}

--- third_party/WebKit/Source/devtools/front_end/timeline_model/TimelineModel.js
+++ third_party/WebKit/Source/devtools/front_end/timeline_model/TimelineModel.js
@@ -119,6 +119,19 @@ WebInspector.TimelineModel = class {
   }
 
   /**
+   * @param {!WebInspector.TracingModel.Event} event
+   * @return {string}
+   */
+  static eventFrameId(event) {
+    var data = event.args['data'] || event.args['beginData'];
+    var frame = data && data['frame'];
+    if (!frame)
+        return '';
+    var processId = event.thread.process().id();
+    return `${processId}.${frame}`;
+  }
+
+  /**
    * @deprecated Test use only!
    * @param {?function(!WebInspector.TimelineModel.Record)|?function(!WebInspector.TimelineModel.Record,number)} preOrderCallback
    * @param {function(!WebInspector.TimelineModel.Record)|function(!WebInspector.TimelineModel.Record,number)=} postOrderCallback
@@ -265,6 +278,18 @@ WebInspector.TimelineModel = class {
     for (var event of metadataEvents) {
       if (event.name === WebInspector.TimelineModel.DevToolsMetadataEvent.TracingStartedInPage) {
         pageDevToolsMetadataEvents.push(event);
+        var frames = (event.args['data'] && event.args['data']['frames']) || [];
+        for (var frame of frames) {
+          var processId = event.thread.process().id();
+          var frameId = `${processId}.${frame.frame}`;
+          var frameData = {
+            url: frame['url'] || '',
+            name: frame['name'] || '',
+            processId: processId,
+            frameId: frame['frame']
+          };
+          this._pageFrames.set(frameId, frameData);
+        }
       } else if (event.name === WebInspector.TimelineModel.DevToolsMetadataEvent.TracingSessionIdForWorker) {
         workersDevToolsMetadataEvents.push(event);
       } else if (event.name === WebInspector.TimelineModel.DevToolsMetadataEvent.TracingStartedInBrowser) {
@@ -780,6 +805,15 @@ WebInspector.TimelineModel = class {
         break;
 
       case recordTypes.CommitLoad:
+        var frameId = WebInspector.TimelineModel.eventFrameId(event);
+        var pageFrame = this._pageFrames.get(frameId);
+        if (pageFrame) {
+          pageFrame.url = eventData.url || '';
+          pageFrame.name = eventData.name || '';
+        } else {
+          var processId = event.thread.process().id();
+          this._pageFrames.set(frameId, {url: eventData.url || '', processId: processId, frameId: eventData.frame, name: eventData.name || ''});
+        }
         var page = eventData['page'];
         if (page && page !== this._currentPage)
           return false;
@@ -918,6 +952,9 @@ WebInspector.TimelineModel = class {
     this._cpuProfiles = [];
     /** @type {!WeakMap<!WebInspector.TracingModel.Thread, string>} */
     this._workerIdByThread = new WeakMap();
+    /** @type {!Map<string, !WebInspector.TimelineModel.PageFrame>} */
+    this._pageFrames = new Map();
+
     this._minimumRecordTime = 0;
     this._maximumRecordTime = 0;
   }
@@ -997,6 +1034,14 @@ WebInspector.TimelineModel = class {
    */
   eventDividerRecords() {
     return this._eventDividerRecords;
+  }
+
+  /**
+   * @param {string} frameId
+   * @return {?WebInspector.TimelineModel.PageFrame}
+   */
+  pageFrameById(frameId) {
+    return frameId ? this._pageFrames.get(frameId) || null : null;
   }
 
   /**
@@ -1331,6 +1376,9 @@ WebInspector.TimelineModel.Record = class {
 /** @typedef {!{page: !Array<!WebInspector.TracingModel.Event>, workers: !Array<!WebInspector.TracingModel.Event>}} */
 WebInspector.TimelineModel.MetadataEvents;
 
+
+/** @typedef {!{url: string, processId: number, frameId: string, name: string}} */
+WebInspector.TimelineModel.PageFrame;
 
 /**
  * @unrestricted
