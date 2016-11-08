710870c2008ab6eb79340019ea485883af60e96c
kennetht <kennetht@chromium.org>
2016-11-08 12:13:47 -0800

    Replaces gender specific terms in comments and strings.
    This replaces 'he', 'she', 'his', 'her' and 'guys' with 'they', 'their' and
    'user' as appropriate.
    
    BUG=542537
    
    Review-Url: https://codereview.chromium.org/2477253002
    Cr-Commit-Position: refs/heads/master@{#430696}

--- chrome/browser/ui/ash/multi_user/user_switch_util.h
+++ chrome/browser/ui/ash/multi_user/user_switch_util.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 
 // Tries to switch to a new user by first checking if desktop casting / sharing
-// is going on, and let the user decide if he wants to terminate it or not.
+// is going on, and letting the user decide if they want to terminate it or not.
 // After terminating any desktop sharing operations, the |switch_user| function
 // will be called.
 void TrySwitchingActiveUser(const base::Callback<void()> switch_user);
