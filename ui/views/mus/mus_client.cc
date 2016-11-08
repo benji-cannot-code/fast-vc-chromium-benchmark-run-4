9915e771c167a282a8029d8df90cc1e4fbb49d33
mfomitchev <mfomitchev@chromium.org>
2016-11-08 15:13:12 -0800

    Aura Mus: Creating a MusContextFactory in ui/aura/mus.
    SurfaceContextFactory currently lives in views/ because it needs NativeWidgetMus
    to determine the surface type. This is no longer needed with Aura Mus, and so we
    can move SurfaceContextFactory to ui/aura/mus, which is a much better place for
    it. Also renaming it to MusContextFactory, since we are trying to get rid of the
    term "surface".
    
    Once Aura Mus transition is complete, we can get rid of the original
    SurfaceContextFactory in views.
    
    This also undoes changes made to views::SurfaceContextFactory in CL
    https://codereview.chromium.org/2464123003 - it doesn't need to support Aura Mus
    anymore, since we have a new specialized one.
    
    R=sky,sadrul
    
    BUG=NONE
    
    Review-Url: https://codereview.chromium.org/2483533002
    Cr-Commit-Position: refs/heads/master@{#430748}

--- ui/views/mus/mus_client.cc
+++ ui/views/mus/mus_client.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/ui/public/interfaces/event_matcher.mojom.h"
 #include "ui/aura/env.h"
 #include "ui/aura/mus/gpu_service.h"
+#include "ui/aura/mus/mus_context_factory.h"
 #include "ui/aura/mus/os_exchange_data_provider_mus.h"
 #include "ui/aura/mus/property_converter.h"
 #include "ui/aura/mus/window_tree_client.h"
@@ -22,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/mus/clipboard_mus.h"
 #include "ui/views/mus/desktop_window_tree_host_mus.h"
 #include "ui/views/mus/screen_mus.h"
-#include "ui/views/mus/surface_context_factory.h"
 #include "ui/views/views_delegate.h"
 #include "ui/views/widget/desktop_aura/desktop_native_widget_aura.h"
 #include "ui/wm/core/capture_controller.h"
@@ -111,7 +111,7 @@ MusClient::MusClient(service_manager::Connector* connector,
 
   gpu_service_ = aura::GpuService::Create(connector, std::move(io_task_runner));
   compositor_context_factory_ =
-      base::MakeUnique<views::SurfaceContextFactory>(gpu_service_.get());
+      base::MakeUnique<aura::MusContextFactory>(gpu_service_.get());
   aura::Env::GetInstance()->set_context_factory(
       compositor_context_factory_.get());
   window_tree_client_ =
