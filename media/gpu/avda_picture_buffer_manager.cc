9a5c906ecea956650378e9ec8fe97cc8fed0ebc4
dalecurtis <dalecurtis@chromium.org>
2016-11-09 14:44:09 -0800

    Enforce no uncleared OES external textures. Clear in AVDA, not GVD.
    A few of us on the team have been bitten by the fact that GVD is
    the one clearing textures instead of AVDA when it comes to OES
    external textures. Lets save a few hours of debugging for someone
    in the future.
    
    OES external textures are now always pre-cleared by AVDA (the only
    VDA using these) and checked by GpuVideoDecoder. Instead of an opaque
    GL_INVALID_ENUM error in GLES2DecoderImpl::PrepareTexturesForRender()
    a DCHECK will now fire in ClearLevel() indicating the exact problem.
    
    BUG=662251
    TEST=implement fancy new feature, notice there are no holes in foot.
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2481943003
    Cr-Commit-Position: refs/heads/master@{#431064}

--- media/gpu/avda_picture_buffer_manager.cc
+++ media/gpu/avda_picture_buffer_manager.cc
@@ -176,6 +176,7 @@ void AVDAPictureBufferManager::SetImageForPicture(
   texture_manager->SetLevelStreamTextureImage(texture_ref, GetTextureTarget(),
                                               0, image, image_state,
                                               stream_texture_service_id);
+  texture_manager->SetLevelCleared(texture_ref, GetTextureTarget(), 0, true);
 }
 
 AVDACodecImage* AVDAPictureBufferManager::GetImageForPicture(
