99b433dcf7ca585dcca5984bce69956896049633
dpapad <dpapad@chromium.org>
2016-11-08 17:47:10 -0800

    MD Settings: Fix clipping of search bubbles by card boundary.
    This patch also seems to address the case where a settings card is briefly
    animated to a 0px height after exiting an animation.
    
    BUG=647487,649174
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2365453002
    Cr-Commit-Position: refs/heads/master@{#430816}

--- chrome/browser/resources/settings/settings_main/settings_main.js
+++ chrome/browser/resources/settings/settings_main/settings_main.js
@@ -91,8 +91,14 @@ Polymer({
 
   /** @override */
   attached: function() {
+    this.listen(this, 'freeze-scroll', 'onFreezeScroll_');
     var currentRoute = settings.getCurrentRoute();
     this.hasExpandedSection_ = currentRoute && currentRoute.isSubpage();
+  },
+
+  /** @override */
+  detached: function() {
+    this.unlisten(this, 'freeze-scroll', 'onFreezeScroll_');
   },
 
   /** @private */
@@ -134,12 +140,11 @@ Polymer({
    * Enables or disables user scrolling, via overscroll: hidden. Room for the
    * hidden scrollbar is added to prevent the page width from changing back and
    * forth. Also freezes the overscroll height.
-   * @param {!Event} e
-   * @param {boolean} detail True to freeze, false to unfreeze.
+   * @param {!Event} e |e.detail| is true to freeze, false to unfreeze.
    * @private
    */
-  onFreezeScroll_: function(e, detail) {
-    if (detail) {
+  onFreezeScroll_: function(e) {
+    if (e.detail) {
       // Update the overscroll and ignore scroll events.
       this.setOverscroll_(this.overscrollHeight_());
       this.ignoreScroll_ = true;
