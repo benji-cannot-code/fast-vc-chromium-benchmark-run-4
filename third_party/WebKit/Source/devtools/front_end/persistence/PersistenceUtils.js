5fa710062785d66b35612e4220c64a5b9e44a310
lushnikov <lushnikov@chromium.org>
2016-11-09 14:09:46 -0800

    DevTools: introduce beautiful checkmarks in navigator and editor tabs
    These checkmarks should designate that the file syncs with a network
    resource.
    
    BUG=649837
    R=dgozman
    
    Review-Url: https://codereview.chromium.org/2482073002
    Cr-Commit-Position: refs/heads/master@{#431048}

---
+++ third_party/WebKit/Source/devtools/front_end/persistence/PersistenceUtils.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+WebInspector.PersistenceUtils = class {
+  /**
+   * @param {!WebInspector.UISourceCode} uiSourceCode
+   * @return {string}
+   */
+  static tooltipForUISourceCode(uiSourceCode) {
+    var binding = WebInspector.persistence.binding(uiSourceCode);
+    if (!binding)
+      return '';
+    if (uiSourceCode === binding.network)
+      return WebInspector.UIString('Persisted to file system: %s', binding.fileSystem.url().trimMiddle(150));
+    if (binding.network.contentType().isFromSourceMap())
+      return WebInspector.UIString('Linked to source map: %s', binding.network.url().trimMiddle(150));
+    return WebInspector.UIString('Linked to %s', binding.network.url().trimMiddle(150));
+  }
+};
