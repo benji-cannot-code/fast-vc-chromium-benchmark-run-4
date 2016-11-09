166d07c447ce1f6187a177e1f38c4e2257c4dbf7
qiangchen <qiangchen@chromium.org>
2016-11-09 09:24:12 -0800

    Use Finch To Track DirectX Screen Capturer Launch
    This CL removes the command line flag controlling DirectX Capturer, but
    instead uses Finch framework, so that easier to experiment on the new
    feature and track its launch.
    
    BUG=314516
    
    Review-Url: https://codereview.chromium.org/2489453002
    Cr-Commit-Position: refs/heads/master@{#430959}

--- content/browser/media/capture/desktop_capture_device.cc
+++ content/browser/media/capture/desktop_capture_device.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
-#include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/macros.h"
@@ -63,6 +63,11 @@ bool IsFrameUnpackedOrInverted(webrtc::DesktopFrame* frame) {
 }
 
 }  // namespace
+
+#if defined(OS_WIN)
+const base::Feature kDirectXCapturer{"DirectXCapturer",
+                                     base::FEATURE_DISABLED_BY_DEFAULT};
+#endif
 
 class DesktopCaptureDevice::Core : public webrtc::DesktopCapturer::Callback {
  public:
@@ -368,8 +373,7 @@ std::unique_ptr<media::VideoCaptureDevice> DesktopCaptureDevice::Create(
   options.set_disable_effects(false);
 
 #if defined(OS_WIN)
-  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kDisableDirectXScreenCapturer)) {
+  if (!base::FeatureList::IsEnabled(kDirectXCapturer)) {
     options.set_allow_use_magnification_api(true);
   } else {
     options.set_allow_directx_capturer(true);
--- content/public/common/content_switches.h
+++ content/public/common/content_switches.h
@@ -316,7 +316,6 @@ CONTENT_EXPORT extern const char kMemoryPressureThresholdsMb[];
 CONTENT_EXPORT extern const char kPpapiAntialiasedTextEnabled[];
 CONTENT_EXPORT extern const char kPpapiSubpixelRenderingSetting[];
 CONTENT_EXPORT extern const char kTraceExportEventsToETW[];
-CONTENT_EXPORT extern const char kDisableDirectXScreenCapturer[];
 #endif
 
 #if defined(ENABLE_IPC_FUZZER)
