6c20c65eb8fb0d1e158654e09fb8c624ecaecf52
estade <estade@chromium.org>
2016-11-09 08:53:10 -0800

    Change color of horizontal separator in cros system menu detailed views.
    Also simplify progress bar/separator layout because why not.
    
    BUG=663042,632128
    
    Review-Url: https://codereview.chromium.org/2486083002
    Cr-Commit-Position: refs/heads/master@{#430951}

--- ash/common/system/chromeos/bluetooth/tray_bluetooth.cc
+++ ash/common/system/chromeos/bluetooth/tray_bluetooth.cc
@@ -40,6 +40,10 @@ namespace ash {
 namespace tray {
 namespace {
 
+bool UseMd() {
+  return MaterialDesignController::IsSystemTrayMenuMaterial();
+}
+
 // Updates bluetooth device |device| in the |list|. If it is new, append to the
 // end of the |list|; otherwise, keep it at the same place, but update the data
 // with new device info provided by |device|.
@@ -110,7 +114,7 @@ class BluetoothDefaultView : public TrayItemMore {
  public:
   BluetoothDefaultView(SystemTrayItem* owner, bool show_more)
       : TrayItemMore(owner, show_more) {
-    if (!MaterialDesignController::IsSystemTrayMenuMaterial()) {
+    if (!UseMd()) {
       // The icon doesn't change in non-md.
       ui::ResourceBundle& bundle = ui::ResourceBundle::GetSharedInstance();
       SetImage(
@@ -155,7 +159,7 @@ class BluetoothDefaultView : public TrayItemMore {
   void UpdateStyle() override {
     TrayItemMore::UpdateStyle();
 
-    if (!MaterialDesignController::IsSystemTrayMenuMaterial())
+    if (!UseMd())
       return;
 
     SystemTrayDelegate* delegate = WmShell::Get()->system_tray_delegate();
@@ -205,8 +209,6 @@ class BluetoothDetailedView : public TrayDetailsView {
     CreateScrollableList();
     AppendSettingsEntries();
     CreateTitleRow(IDS_ASH_STATUS_TRAY_BLUETOOTH);
-    if (MaterialDesignController::IsSystemTrayMenuMaterial())
-      CreateProgressBar();
   }
 
   void BluetoothStartDiscovering() {
@@ -318,7 +320,7 @@ class BluetoothDetailedView : public TrayDetailsView {
                                          bool enabled) {
     for (size_t i = 0; i < list.size(); ++i) {
       HoverHighlightView* container = nullptr;
-      if (MaterialDesignController::IsSystemTrayMenuMaterial()) {
+      if (UseMd()) {
         gfx::ImageSkia icon_image = CreateVectorIcon(
             GetBluetoothDeviceIcon(list[i].device_type), kMenuIconColor);
         container = AddScrollListItemWithIcon(list[i].display_name, highlight,
@@ -370,7 +372,7 @@ class BluetoothDetailedView : public TrayDetailsView {
   void AppendSettingsEntries() {
     // Do not append the bottom button row in material design; this is replaced
     // by the settings button in the header row.
-    if (MaterialDesignController::IsSystemTrayMenuMaterial())
+    if (UseMd())
       return;
 
     if (!WmShell::Get()->system_tray_delegate()->ShouldShowSettings())
@@ -459,7 +461,7 @@ class BluetoothDetailedView : public TrayDetailsView {
 
   void HandleButtonPressed(views::Button* sender,
                            const ui::Event& event) override {
-    if (MaterialDesignController::IsSystemTrayMenuMaterial()) {
+    if (UseMd()) {
       if (sender == toggle_)
         WmShell::Get()->system_tray_delegate()->ToggleBluetooth();
       else if (sender == settings_)
@@ -480,7 +482,7 @@ class BluetoothDetailedView : public TrayDetailsView {
     if (login_ == LoginStatus::LOCKED)
       return;
 
-    if (MaterialDesignController::IsSystemTrayMenuMaterial()) {
+    if (UseMd()) {
       toggle_ = title_row()->AddToggleButton(this);
       settings_ = title_row()->AddSettingsButton(this, login_);
       return;
@@ -518,20 +520,17 @@ class BluetoothDetailedView : public TrayDetailsView {
   void ShowLoadingIndicator() {
     if (throbber_) {
       throbber_->Start();
-    } else if (progress_bar()) {
+    } else if (UseMd()) {
       // Setting a value of -1 gives progress_bar an infinite-loading behavior.
-      progress_bar()->SetValue(-1);
-      progress_bar()->SetVisible(true);
+      ShowProgress(-1, true);
     }
   }
 
   void HideLoadingIndicator() {
-    if (throbber_) {
+    if (throbber_)
       throbber_->Stop();
-    } else if (progress_bar()) {
-      progress_bar()->SetValue(0);
-      progress_bar()->SetVisible(false);
-    }
+    else if (UseMd())
+      ShowProgress(0, false);
   }
 
   LoginStatus login_;
