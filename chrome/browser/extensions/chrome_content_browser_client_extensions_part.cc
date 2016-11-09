fbe075ddb03f3107723923c5c5b424853979ea33
alexmos <alexmos@chromium.org>
2016-11-09 15:19:45 -0800

    Allow navigations to non-web-accessible resources from chrome schemes.
    Recent fixes for web accessible resource checks in ShouldAllowOpenURL
    (https://codereview.chromium.org/2454563003) removed the free pass
    that non-HTTP(S) and non-extensions schemes used to have to load
    non-web-accessible resources.  However, data gathered in that CL
    showed that this is breaking navigations from NTP pages (e.g., if an
    extensions page appears on the recent tiles) and chrome://history when
    clicking on an extensions page.  To fix these, this CL relaxes the
    rules in ShouldAllowOpenURL to allow chrome:// and chrome-search://
    pages to navigate to non-WAR extension pages.
    
    BUG=662602
    
    Review-Url: https://codereview.chromium.org/2486843003
    Cr-Commit-Position: refs/heads/master@{#431074}

--- chrome/browser/extensions/chrome_content_browser_client_extensions_part.cc
+++ chrome/browser/extensions/chrome_content_browser_client_extensions_part.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "chrome/common/extensions/extension_process_policy.h"
+#include "chrome/common/url_constants.h"
 #include "components/guest_view/browser/guest_view_message_filter.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/browser_url_handler.h"
@@ -39,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/vpn_service_proxy.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_switches.h"
+#include "content/public/common/url_constants.h"
 #include "extensions/browser/api/web_request/web_request_api.h"
 #include "extensions/browser/api/web_request/web_request_api_helpers.h"
 #include "extensions/browser/bad_message.h"
@@ -603,6 +605,19 @@ bool ChromeContentBrowserClientExtensionsPart::ShouldAllowOpenURL(
     base::debug::DumpWithoutCrashing();
 
     *result = false;
+    return true;
+  }
+
+  // Navigations from chrome:// or chrome-search:// pages need to be allowed,
+  // even if |to_url| is not web-accessible.  See https://crbug.com/662602.
+  //
+  // Note that this is intentionally done after the check for blob: and
+  // filesystem: URLs above, for consistency with the renderer-side checks
+  // which already disallow navigations from chrome URLs to blob/filesystem
+  // URLs.
+  if (site_url.SchemeIs(content::kChromeUIScheme) ||
+      site_url.SchemeIs(chrome::kChromeSearchScheme)) {
+    *result = true;
     return true;
   }
 
