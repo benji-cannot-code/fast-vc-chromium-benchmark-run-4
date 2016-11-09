15de6a53cb902debd58d1ceba0dd013ba9e69329
ryansturm <ryansturm@chromium.org>
2016-11-09 12:17:11 -0800

    Adding UMA to track previews opt outs and blacklist eligibility
    This CL adds UMA to track the reason that a user was not shown a preview
    (or if it was allowed) and whether a user opts out when shown a preview.
    
    BUG=647717
    
    Review-Url: https://codereview.chromium.org/2477073002
    Cr-Commit-Position: refs/heads/master@{#431020}

--- components/previews/core/previews_black_list.cc
+++ components/previews/core/previews_black_list.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/memory/ptr_util.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/optional.h"
 #include "base/time/clock.h"
 #include "components/previews/core/previews_black_list_item.h"
@@ -74,6 +75,13 @@ void PreviewsBlackList::AddPreviewNavigation(const GURL& url,
                                              PreviewsType type) {
   DCHECK(thread_checker_.CalledOnValidThread());
   DCHECK(url.has_host());
+  switch (type) {
+    case PreviewsType::OFFLINE:
+      UMA_HISTOGRAM_BOOLEAN("Previews.OptOut.UserOptedOut.Offline", opt_out);
+      break;
+    default:
+      NOTREACHED();
+  }
   if (opt_out) {
     last_opt_out_time_ = clock_->Now();
   }
@@ -109,23 +117,26 @@ void PreviewsBlackList::AddPreviewNavigationSync(const GURL& url,
   opt_out_store_->AddPreviewNavigation(opt_out, host_name, type, now);
 }
 
-bool PreviewsBlackList::IsLoadedAndAllowed(const GURL& url,
-                                           PreviewsType type) const {
+PreviewsEligibilityReason PreviewsBlackList::IsLoadedAndAllowed(
+    const GURL& url,
+    PreviewsType type) const {
   DCHECK(thread_checker_.CalledOnValidThread());
   DCHECK(url.has_host());
   if (!loaded_)
-    return false;
+    return PreviewsEligibilityReason::BLACKLIST_DATA_NOT_LOADED;
   DCHECK(black_list_item_map_);
   if (last_opt_out_time_ &&
       clock_->Now() <
           last_opt_out_time_.value() + params::SingleOptOutDuration()) {
-    return false;
+    return PreviewsEligibilityReason::USER_RECENTLY_OPTED_OUT;
   }
   if (host_indifferent_black_list_item_->IsBlackListed(clock_->Now()))
-    return false;
+    return PreviewsEligibilityReason::USER_BLACKLISTED;
   PreviewsBlackListItem* black_list_item =
       GetBlackListItemFromMap(*black_list_item_map_, url.host());
-  return !black_list_item || !black_list_item->IsBlackListed(clock_->Now());
+  if (black_list_item && black_list_item->IsBlackListed(clock_->Now()))
+    return PreviewsEligibilityReason::HOST_BLACKLISTED;
+  return PreviewsEligibilityReason::ALLOWED;
 }
 
 void PreviewsBlackList::ClearBlackList(base::Time begin_time,
