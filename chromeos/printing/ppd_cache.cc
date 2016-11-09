1d7aac05458640167e642f860e5b5473e49234e3
xunjieli <xunjieli@chromium.org>
2016-11-08 18:27:38 -0800

    Remove an unused include in ppd_cache.cc
    net::Filter will be removed soon. This CL removes the unused #include
    from ppd_cache.cc in preparation of removing net::Filter.
    
    BUG=474859
    
    Review-Url: https://codereview.chromium.org/2481333002
    Cr-Commit-Position: refs/heads/master@{#430831}

--- chromeos/printing/ppd_cache.cc
+++ chromeos/printing/ppd_cache.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "crypto/sha2.h"
 #include "net/base/io_buffer.h"
-#include "net/filter/filter.h"
 #include "net/filter/gzip_header.h"
 
 namespace chromeos {
