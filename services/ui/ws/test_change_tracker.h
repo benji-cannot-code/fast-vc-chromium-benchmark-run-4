690267ae3e04ff17f5190d8bcf9c06fa831d98ef
kylechar <kylechar@chromium.org>
2016-11-08 19:17:17 -0800

    Remove SurfaceSequence from mus client code.
    This is a first step in replacing all SurfaceSequences in mus. We were
    passing a sequence from WT to WTC and the client was storing it. The
    client would later return the sequence. However, we never registered the
    sequence as a dependency so it was one big no-op.
    
    Remove all of the no-op code. Drop OnWindowSurfacedDetached() since it's
    not needed now. Something along these lines will need to be added in the
    future, but it will need to be synchronized with SubmitCompositorFrame()
    so would need to be in a different interface anyways.
    
    BUG=659227
    
    Review-Url: https://codereview.chromium.org/2478383002
    Cr-Commit-Position: refs/heads/master@{#430841}

--- services/ui/ws/test_change_tracker.h
+++ services/ui/ws/test_change_tracker.h
@@ -90,7 +90,6 @@ struct Change {
   int32_t cursor_id;
   uint32_t change_id;
   cc::SurfaceId surface_id;
-  cc::SurfaceSequence surface_sequence;
   gfx::Size frame_size;
   float device_scale_factor;
 };
@@ -177,7 +176,6 @@ class TestChangeTracker {
                          bool drawn);
   void OnWindowSurfaceChanged(Id window_id,
                               const cc::SurfaceId& surface_id,
-                              const cc::SurfaceSequence& surface_sequence,
                               const gfx::Size& frame_size,
                               float device_scale_factor);
 
