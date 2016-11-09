b15e10e893a0d66a31948eebf85973673adab6c9
halcanary <halcanary@google.com>
2016-11-09 09:16:36 -0800

    Stop using Skia's SkAutoMalloc in Blink.
    BUG=
    
    Review-Url: https://codereview.chromium.org/2487223002
    Cr-Commit-Position: refs/heads/master@{#430956}

--- third_party/WebKit/Source/platform/graphics/skia/ImagePixelLocker.h
+++ third_party/WebKit/Source/platform/graphics/skia/ImagePixelLocker.h
@@ -30,7 +30,7 @@ class ImagePixelLocker final {
  private:
   const sk_sp<const SkImage> m_image;
   const void* m_pixels;
-  SkAutoMalloc m_pixelStorage;
+  Vector<char> m_pixelStorage;
 };
 
 }  // namespace blink
