c7db1c8a81b2089011a5aceda5eaab902d969950
alexander.shalamov <alexander.shalamov@intel.com>
2016-11-09 04:42:49 -0800

    [sensors] Reject test promise when callback throws an error
    This CL improves test reports for ambient light sensor by wrapping
    Sensor.on* callbacks that might throw an error. When error is thrown,
    promise for async test is rejected and executed test would be marked
    as 'FAIL'. Before this change, failing tests could timeout and block
    consecutive test execution.
    
    BUG=606766
    
    Review-Url: https://codereview.chromium.org/2481013004
    Cr-Commit-Position: refs/heads/master@{#430917}

--- third_party/WebKit/LayoutTests/sensor/resources/sensor-helpers.js
+++ third_party/WebKit/LayoutTests/sensor/resources/sensor-helpers.js
@@ -1,6 +1,23 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 'use strict';
 
+// Wraps callback and calls reject_func if callback throws an error.
+class CallbackWrapper {
+  constructor(callback, reject_func) {
+    this.wrapper_func_ = (args) => {
+      try {
+        callback(args);
+      } catch(e) {
+        reject_func();
+      }
+    }
+  }
+
+  get callback() {
+    return this.wrapper_func_;
+  }
+}
+
 function sensor_mocks(mojo) {
   return define('Generic Sensor API mocks', [
     'mojo/public/js/core',
@@ -54,7 +71,8 @@ function sensor_mocks(mojo) {
         if (!this.start_should_fail_ && this.update_reading_function_ != null) {
           let timeout = (1 / configuration.frequency) * 1000;
           this.sensor_reading_timer_id_ = window.setTimeout(() => {
-            this.update_reading_function_(this.buffer_);
+            if (this.update_reading_function_)
+              this.update_reading_function_(this.buffer_);
             if (this.reporting_mode_ === sensor.ReportingMode.ON_CHANGE) {
               this.client_.sensorReadingChanged();
             }
