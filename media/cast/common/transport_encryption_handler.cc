a51f128dfa9b2fd1366531d18fdecb186492394c
wez <wez@chromium.org>
2016-11-09 13:56:27 -0800

    Fix direct dependencies in //media/cast/BUILD.gn.
    - Add direct dependencies (e.g. on //base) where missing.
    - Move Codec and RtpPayloadType from
      //media/cast/net/cast_transport_config.h to //media/cast/cast_config.h
    - Remove an unused header include.
    
    Note that dependencies on //media/* sub-components, in the build targets for executables use //media to reach the sub-components, to avoid duplicate symbols in component builds, due to use of source_set for //media/* subcomponents.
    
    BUG=609355, 530839
    
    Review-Url: https://codereview.chromium.org/2462863002
    Cr-Commit-Position: refs/heads/master@{#431043}

--- media/cast/common/transport_encryption_handler.cc
+++ media/cast/common/transport_encryption_handler.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "crypto/encryptor.h"
 #include "crypto/symmetric_key.h"
-#include "media/cast/net/cast_transport_defines.h"
 
 namespace media {
 namespace cast {
