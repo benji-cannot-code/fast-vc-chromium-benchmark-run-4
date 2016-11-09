d9f0c2b43cfc2bf457c713fbb9eb358818b478f6
xidachen <xidachen@chromium.org>
2016-11-09 09:01:12 -0800

    Consolidate implementation of ImageBitmapRenderingContext
    To prepare for the shipping of ImageBitmapRenderingContext, we need more
    layout test coverage. When we add more tests, we found that there are
    some problem in our implementation.
    
    1. transferFromImageBitmap API should be capable of throwing DOMException.
    2. After calling transferFromImageBitmap(bitmap), the bitmap should be
       detached.
    3. When calling transferFromImageBitmap(bitmap), if bitmap has already
       detached, this call should throw an InvalidStateError DOMException.
    
    The corresponding layout test cases are added.
    
    BUG=659156
    
    Committed: https://crrev.com/300be765de46941accbb0d12fee1dba27fd06bb0
    Review-Url: https://codereview.chromium.org/2459233002
    Cr-Original-Commit-Position: refs/heads/master@{#428988}
    Cr-Commit-Position: refs/heads/master@{#430954}

--- third_party/WebKit/Source/modules/imagebitmap/ImageBitmapRenderingContext.h
+++ third_party/WebKit/Source/modules/imagebitmap/ImageBitmapRenderingContext.h
@@ -36,7 +36,7 @@ class MODULES_EXPORT ImageBitmapRenderingContext final
   };
 
   // Script API
-  void transferFromImageBitmap(ImageBitmap*);
+  void transferFromImageBitmap(ImageBitmap*, ExceptionState&);
 
   // CanvasRenderingContext implementation
   ContextType getContextType() const override {
