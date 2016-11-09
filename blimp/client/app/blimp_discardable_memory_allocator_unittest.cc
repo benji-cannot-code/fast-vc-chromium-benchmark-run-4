2bc13681691e7f8d2ca3cf0d5d73bb820e61d931
wez <wez@chromium.org>
2016-11-08 16:25:37 -0800

    Get a clean crash stack if DiscardableMemory outlives allocator.
    We have unlocked DiscardableMemory instances somehow outliving the
    BlimpDiscardableMemory allocator in our X11 Client build. This CL adds
    DCHECK_IS_ON() nulling of the |allocator_| reference in all unlocked
    DiscardableMemory instances, and a DCHECK in their destructor so that
    we can get clean crash stacks to determine where they are being owned.
    
    BUG=662439
    
    Review-Url: https://codereview.chromium.org/2484183003
    Cr-Commit-Position: refs/heads/master@{#430787}

--- blimp/client/app/blimp_discardable_memory_allocator_unittest.cc
+++ blimp/client/app/blimp_discardable_memory_allocator_unittest.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include "base/memory/discardable_memory.h"
+#include "base/memory/ptr_util.h"
+#include "base/test/gtest_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace blimp {
@@ -87,6 +89,28 @@ TEST(BlimpDiscardableMemoryAllocator, DontDiscardLiveChunks) {
   chunk_two->Unlock();
   chunk_three->Unlock();
 }
+
+#if DCHECK_IS_ON()
+TEST(BlimpDiscardableMemoryAllocator, DiscardWhileUnlockedLive) {
+  std::unique_ptr<BlimpDiscardableMemoryAllocator> allocator =
+      base::MakeUnique<BlimpDiscardableMemoryAllocator>();
+
+  std::unique_ptr<base::DiscardableMemory> chunk_one =
+      allocator->AllocateLockedDiscardableMemory(kAlmostOneMegabyte);
+
+  // These accesses will fail if the underlying weak ptr has been deallocated.
+  EXPECT_NE(nullptr, chunk_one->data());
+
+  chunk_one->Unlock();
+
+  allocator.reset();
+
+  EXPECT_DCHECK_DEATH(chunk_one.reset());
+
+  // Leak the DiscardableMemory instance, to avoid triggering the DCHECK.
+  chunk_one.release();
+}
+#endif  // DCHECK_IS_ON()
 
 }  // namespace
 }  // namespace client
