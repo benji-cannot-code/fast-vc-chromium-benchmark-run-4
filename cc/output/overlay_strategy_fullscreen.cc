5f785cdd9878810f4910e3fd5f6938e84225b136
dcastagna <dcastagna@chromium.org>
2016-11-09 20:47:06 -0800

    Disable fullscren fb optimization with sw mirroring.
    Display software mirroring copies the current fb into a texture
    and draws with that.
    
    This CL disables fullscreen fb optimization and always falls back
    to GL compositing when SW mirroring is enabled.
    
    BUG=662621
    TEST=ran on kevin with sw mirroring and fs app.
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    
    Review-Url: https://codereview.chromium.org/2476103002
    Cr-Commit-Position: refs/heads/master@{#431184}

--- cc/output/overlay_strategy_fullscreen.cc
+++ cc/output/overlay_strategy_fullscreen.cc
@@ -14,7 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc {
 
-OverlayStrategyFullscreen::OverlayStrategyFullscreen() {}
+OverlayStrategyFullscreen::OverlayStrategyFullscreen(
+    OverlayCandidateValidator* capability_checker)
+    : capability_checker_(capability_checker) {
+  DCHECK(capability_checker);
+}
 
 OverlayStrategyFullscreen::~OverlayStrategyFullscreen() {}
 
@@ -53,7 +57,12 @@ bool OverlayStrategyFullscreen::Attempt(ResourceProvider* resource_provider,
   candidate.overlay_handled = true;
   OverlayCandidateList new_candidate_list;
   new_candidate_list.push_back(candidate);
+  capability_checker_->CheckOverlaySupport(&new_candidate_list);
+  if (!new_candidate_list.front().overlay_handled)
+    return false;
+
   candidate_list->swap(new_candidate_list);
+
   render_pass->quad_list = QuadList();  // Remove all the quads
   return true;
 }
