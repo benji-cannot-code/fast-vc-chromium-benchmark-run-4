642dce9c1f647589b512ba17a83ec5d98efbcd31
shans <shans@chromium.org>
2016-11-10 01:05:36 -0800

    Modify Layout declarations to take extra parameters and return an enum.
    Review-Url: https://codereview.chromium.org/2489243002
    Cr-Commit-Position: refs/heads/master@{#431215}

--- third_party/WebKit/Source/core/layout/ng/ng_box.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_box.cc
@@ -58,7 +58,7 @@ bool NGBox::Layout(const NGConstraintSpace* constraint_space,
     }
 
     NGPhysicalFragmentBase* fragment = nullptr;
-    if (!layout_algorithm_->Layout(&fragment))
+    if (!layout_algorithm_->Layout(nullptr, &fragment, nullptr))
       return false;
     fragment_ = toNGPhysicalFragment(fragment);
 
@@ -126,7 +126,7 @@ bool NGBox::ComputeMinAndMaxContentSizes(MinAndMaxContentSizes* sizes) {
 
   // Have to synthesize this value.
   NGPhysicalFragmentBase* physical_fragment;
-  while (!minmax_algorithm_->Layout(&physical_fragment))
+  while (!minmax_algorithm_->Layout(nullptr, &physical_fragment, nullptr))
     continue;
   NGFragment* fragment = new NGFragment(
       FromPlatformWritingMode(Style()->getWritingMode()), Style()->direction(),
@@ -146,7 +146,7 @@ bool NGBox::ComputeMinAndMaxContentSizes(MinAndMaxContentSizes* sizes) {
 
   minmax_algorithm_ =
       new NGBlockLayoutAlgorithm(Style(), FirstChild(), constraint_space);
-  while (!minmax_algorithm_->Layout(&physical_fragment))
+  while (!minmax_algorithm_->Layout(nullptr, &physical_fragment, nullptr))
     continue;
 
   fragment = new NGFragment(FromPlatformWritingMode(Style()->getWritingMode()),
