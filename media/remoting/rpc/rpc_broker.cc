fe8f9c46305dfcc6af7e52ce41ea8a8135add19a
erickung <erickung@chromium.org>
2016-11-09 16:55:36 -0800

    Media Remoting: Add media dependency for rpc
    - Rpc should have depndecy on media. This doesn't get caught due
      to the fact that gn check under media is disable.
    - Also to remove unnecessary inclde header to avoid build
    
    Review-Url: https://codereview.chromium.org/2487343002
    Cr-Commit-Position: refs/heads/master@{#431116}

--- media/remoting/rpc/rpc_broker.cc
+++ media/remoting/rpc/rpc_broker.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/atomic_sequence_num.h"
 #include "base/logging.h"
 #include "media/base/bind_to_current_loop.h"
-#include "media/remoting/rpc/proto_utils.h"
 
 namespace media {
 namespace remoting {
