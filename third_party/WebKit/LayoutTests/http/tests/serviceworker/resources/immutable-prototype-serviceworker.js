0e9faeb73065598af792a8181218d2633960aadc
littledan <littledan@chromium.org>
2016-11-09 23:46:55 -0800

    Freeze global prototype chain per WebIDL
    There are three places where WebIDL freezes the global object prototype chain:
    - Named properties objects have an immutable prototype
    - Prototypes of interfaces declared with [Global] or [PrimaryGlobal], and
      classes which are inherited from by those. For the latter, this patch
      introduces an [ImmutablePrototype] synthetic extended attribute.
    - Instances of [Global] or [PrimaryGlobal]
    
    This patch causes all three types of objects to be immutable prototype exotic
    objects.
    
    A number of tests previously checked what behavior occurs when the prototype
    chain of the global object is mutated. This patch changes the tests to just
    check that the prototype chain of the global object is immutable; tests which
    check what happens after mutating are deleted as the path is unreachable.
    
    BUG=v8:5149
    
    Review-Url: https://codereview.chromium.org/2452073002
    Cr-Commit-Position: refs/heads/master@{#431204}

---
+++ third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/immutable-prototype-serviceworker.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function prototypeChain(global) {
+  let result = [];
+  while (global !== null) {
+    let thrown = false;
+    let next = global.__proto__;
+    try {
+      global.__proto__ = {};
+      result.push('mutable');
+    } catch (e) {
+      result.push('immutable');
+    }
+    global = next;
+  }
+  return result;
+}
+
+self.onmessage = function(e) {
+  e.data.postMessage(prototypeChain(self));
+};
---
+++ third_party/WebKit/LayoutTests/security/immutable-prototype.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function prototypeChain(global) {
+  let result = [];
+  while (global !== null) {
+    let thrown = false;
+    let next = global.__proto__;
+    try {
+      global.__proto__ = {};
+      result.push('mutable');
+    } catch (e) {
+      result.push('immutable');
+    }
+    global = next;
+  }
+  return result;
+}
+
