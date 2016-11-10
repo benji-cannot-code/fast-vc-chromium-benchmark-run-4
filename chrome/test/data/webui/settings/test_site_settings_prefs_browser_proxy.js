7f3ef2814661705a28953ce60937904116c8e314
scottchen <scottchen@chromium.org>
2016-11-10 13:37:56 -0800

    change site-settings -> usb-device to use cr-action-menu instead of paper-menu
    This CL is part of the effort to move off of paper-item which is known to have performance issues.
    Note for future improvement: dpapad@ and I noticed that there's only a remove button in the dropdown menu, so we can probably replace it with just an "X" button.
    
    BUG=603976
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2480843003
    Cr-Commit-Position: refs/heads/master@{#431362}

--- chrome/test/data/webui/settings/test_site_settings_prefs_browser_proxy.js
+++ chrome/test/data/webui/settings/test_site_settings_prefs_browser_proxy.js
@@ -51,9 +51,11 @@ var prefsEmpty = {
  */
 var TestSiteSettingsPrefsBrowserProxy = function() {
   settings.TestBrowserProxy.call(this, [
+    'fetchUsbDevices',
     'fetchZoomLevels',
     'getDefaultValueForContentType',
     'getExceptionList',
+    'removeUsbDevice',
     'removeZoomLevel',
     'resetCategoryPermissionForOrigin',
     'setCategoryPermissionForOrigin',
@@ -65,6 +67,9 @@ var TestSiteSettingsPrefsBrowserProxy = function() {
 
   /** @private {!Array<ZoomLevelEntry>} */
   this.zoomList_ = [];
+
+  /** @private {!Array<UsbDeviceEntry>} */
+  this.usbDevices_ = [];
 };
 
 TestSiteSettingsPrefsBrowserProxy.prototype = {
@@ -88,10 +93,19 @@ TestSiteSettingsPrefsBrowserProxy.prototype = {
 
   /**
    * Sets the prefs to use when testing.
-   * @param !Array<ZoomLevelEntry> list The zoom list to set.
+   * @param {!Array<ZoomLevelEntry>} list The zoom list to set.
    */
   setZoomList: function(list) {
     this.zoomList_ = list;
+  },
+
+  /**
+   * Sets the prefs to use when testing.
+   * @param {!Array<UsbDeviceEntry>} list The usb device entry list to set.
+   */
+  setUsbDevices: function(list) {
+    // Shallow copy of the passed-in array so mutation won't impact the source
+    this.usbDevices_ = list.slice();
   },
 
   /** @override */
@@ -208,4 +222,15 @@ TestSiteSettingsPrefsBrowserProxy.prototype = {
   removeZoomLevel: function(host) {
     this.methodCalled('removeZoomLevel', [host]);
   },
+
+  /** @override */
+  fetchUsbDevices: function() {
+    this.methodCalled('fetchUsbDevices');
+    return Promise.resolve(this.usbDevices_);
+  },
+
+  /** @override */
+  removeUsbDevice: function() {
+    this.methodCalled('removeUsbDevice', arguments);
+  }
 };
