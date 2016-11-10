38d7259072c3adf4049c553148ea48b184074e8d
krb <krb@chromium.org>
2016-11-10 11:45:29 -0800

    [Web Payments] Mojom namespace blink -> payments
    When we migrated payment_request.mojom from Blink to components/
    we left the namespace in place to minimize changes and facilitate
    the move. This CL simply changes the namespace that the payment
    request stuff resides in. Lots of files changed, little depth.
    
    BUG=659644
    
    Committed: https://crrev.com/3a05ff5fc9fce479f12e3fbcdaaa9df88a7b7aa2
    Review-Url: https://codereview.chromium.org/2477883002
    Cr-Original-Commit-Position: refs/heads/master@{#430990}
    Cr-Commit-Position: refs/heads/master@{#431320}

--- components/payments/payment_details_validation.h
+++ components/payments/payment_details_validation.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace payments {
 
-bool validatePaymentDetails(const blink::mojom::PaymentDetailsPtr& details,
+bool validatePaymentDetails(const mojom::PaymentDetailsPtr& details,
                             std::string* error_message);
 
 }  // namespace payments
