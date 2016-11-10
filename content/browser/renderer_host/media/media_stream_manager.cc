b9096f565cea6f4d3808f3dcbd46f1341045b9e1
guidou <guidou@chromium.org>
2016-11-09 16:08:54 -0800

    Do not kill renderer when enumerateDevices is called from unique origins (e.g., about:blank).
    This CL restores the behavior of enumerateDevices with unique origins before the Mojo migration.
    
    BUG=660204
    
    Review-Url: https://codereview.chromium.org/2468853002
    Cr-Commit-Position: refs/heads/master@{#431090}

--- content/browser/renderer_host/media/media_stream_manager.cc
+++ content/browser/renderer_host/media/media_stream_manager.cc
@@ -1975,7 +1975,8 @@ bool MediaStreamManager::IsOriginAllowed(int render_process_id,
                                          const url::Origin& origin) {
   if (!ChildProcessSecurityPolicyImpl::GetInstance()->CanRequestURL(
           render_process_id, origin.GetURL())) {
-    LOG(ERROR) << "MSM: Renderer requested a URL it's not allowed to use.";
+    LOG(ERROR) << "MSM: Renderer requested a URL it's not allowed to use: "
+               << origin.Serialize();
     return false;
   }
 
