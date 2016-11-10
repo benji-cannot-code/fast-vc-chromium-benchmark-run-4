ab225b23f3a6a463dd0139c48a1105e47040c0d1
yamaguchi <yamaguchi@chromium.org>
2016-11-09 23:59:07 -0800

    Remount all removable storage devices when received update of ExternalStorageReadOnly policy.
    The end-to-end test can be done manually in this way:
    1. Enter public session mode.
       See https://support.google.com/chrome/a/answer/3017014?hl=en
    2. Plug a removable storage / MTP device.
    3. Change the policy in Google admin and make sure it is reloaded at chrome://policy.
    4. Check if the device is mounted RO/RW accordingly by right-clicking it in Files app. When the device is read-only, it the "format drive" menu will be grayed out.
    
    This change depends on
    https://chromium-review.googlesource.com/#/c/390410/
    in chromeos. (which is already merged to trunk)
    
    BUG=642247
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    TEST=manual test as described above
    
    Review-Url: https://codereview.chromium.org/2401963002
    Cr-Commit-Position: refs/heads/master@{#431206}

--- chrome/browser/chromeos/file_manager/volume_manager.cc
+++ chrome/browser/chromeos/file_manager/volume_manager.cc
@@ -372,6 +372,10 @@ void VolumeManager::Initialize() {
       prefs::kExternalStorageDisabled,
       base::Bind(&VolumeManager::OnExternalStorageDisabledChanged,
                  weak_ptr_factory_.GetWeakPtr()));
+  pref_change_registrar_.Add(
+      prefs::kExternalStorageReadOnly,
+      base::Bind(&VolumeManager::OnExternalStorageReadOnlyChanged,
+                 weak_ptr_factory_.GetWeakPtr()));
 
   // Subscribe to storage monitor for MTP notifications.
   if (storage_monitor::StorageMonitor::GetInstance()) {
@@ -717,6 +721,11 @@ void VolumeManager::OnExternalStorageDisabledChanged() {
             &VolumeManager::OnExternalStorageDisabledChangedUnmountCallback,
             weak_ptr_factory_.GetWeakPtr()));
   }
+}
+
+void VolumeManager::OnExternalStorageReadOnlyChanged() {
+  disk_mount_manager_->RemountAllRemovableDrives(
+      GetExternalStorageAccessMode(profile_));
 }
 
 void VolumeManager::OnRemovableStorageAttached(
