2a1b72de5779c715ab093414dc83126e6f4193df
yirui <yirui@google.com>
2016-11-09 17:13:33 -0800

    Use LocaleList in Android/Webview locale representation
    From Android N, uses are able to select multiple languages in system
    Settings. This CL provides supports for multi-locales features for
    Chrome and System WebView. This does not breaking behaviours on
    Andriod M and before.
    
    Since toLanguageTags in LocaleList does not convert Android language
     "tl"(Tagalog) to Chromium language "fil"(Filipino), an update for
    toLanguageTags is added.
    
    BUG=593515
    
    Review-Url: https://codereview.chromium.org/2481293004
    Cr-Commit-Position: refs/heads/master@{#431128}

--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -361,7 +361,7 @@ public class AwContents implements SmartClipProvider, PostMessageSender.PostMess
     // Do not use directly, call isDestroyed() instead.
     private boolean mIsDestroyed = false;
 
-    private static String sCurrentLocale = "";
+    private static String sCurrentLocales = "";
 
     private static final class AwContentsDestroyRunnable implements Runnable {
         private final long mNativeAwContents;
@@ -695,7 +695,11 @@ public class AwContents implements SmartClipProvider, PostMessageSender.PostMess
 
         @Override
         public void onConfigurationChanged(Configuration configuration) {
-            setLocale(LocaleUtils.toLanguageTag(configuration.locale));
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                setLocale(LocaleUtils.toLanguageTags(configuration.getLocales()));
+            } else {
+                setLocale(LocaleUtils.toLanguageTag(configuration.locale));
+            }
             mSettings.updateAcceptLanguages();
         }
     };
@@ -1020,11 +1024,12 @@ public class AwContents implements SmartClipProvider, PostMessageSender.PostMess
         return wrapper;
     }
 
+    // Deal with a string that represents either a single locale or a locale list.
     @VisibleForTesting
-    public static void setLocale(String locale) {
-        if (!sCurrentLocale.equals(locale)) {
-            sCurrentLocale = locale;
-            nativeSetLocale(sCurrentLocale);
+    public static void setLocale(String locales) {
+        if (!sCurrentLocales.equals(locales)) {
+            sCurrentLocales = locales;
+            nativeSetLocale(sCurrentLocales);
         }
     }
 
