625cdc6fb94944a477a8eb7615be7e97e5e825c1
sunjian <sunjian@chromium.org>
2016-11-09 19:22:27 -0800

    Updated error message in PerformanceObserver.observe
    BUG=662210
    
    Review-Url: https://codereview.chromium.org/2468063006
    Cr-Commit-Position: refs/heads/master@{#431165}

--- third_party/WebKit/Source/core/timing/PerformanceObserver.cpp
+++ third_party/WebKit/Source/core/timing/PerformanceObserver.cpp
@@ -50,7 +50,8 @@ void PerformanceObserver::observe(const PerformanceObserverInit& observerInit,
   }
   if (entryTypes == PerformanceEntry::Invalid) {
     exceptionState.throwTypeError(
-        "A Performance Observer MUST have a non-empty entryTypes attribute.");
+        "A Performance Observer MUST have at least one valid entryType in its "
+        "entryTypes attribute.");
     return;
   }
   m_filterOptions = entryTypes;
