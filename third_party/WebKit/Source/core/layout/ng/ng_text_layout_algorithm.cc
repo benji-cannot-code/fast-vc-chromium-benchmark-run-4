f0f27ee979b4d2dfcd9019ade3d46ce1ad2a097e
vabr <vabr@chromium.org>
2016-11-10 01:28:58 -0800

    Revert of Modify Layout declarations to take extra parameters and return an enum. (patchset #2 id:20001 of https://codereview.chromium.org/2489243002/ )
    Reason for revert:
    This broke the build on many bots (see the findint entry; will try to add more info on the bug).
    
    Original issue's description:
    > Modify Layout declarations to take extra parameters and return an enum.
    >
    > Committed: https://crrev.com/e9f4dc8eca56e715cdfa62cf31e9ef5d58009c3a
    > Cr-Commit-Position: refs/heads/master@{#431215}
    
    TBR=ikilpatrick@chromium.org,shans@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2488733004
    Cr-Commit-Position: refs/heads/master@{#431220}

--- third_party/WebKit/Source/core/layout/ng/ng_text_layout_algorithm.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_text_layout_algorithm.cc
@@ -20,13 +20,10 @@ NGTextLayoutAlgorithm::NGTextLayoutAlgorithm(
   DCHECK(inline_box_);
 }
 
-NGLayoutStatus NGTextLayoutAlgorithm::Layout(
-    NGFragmentBase*,
-    NGPhysicalFragmentBase** fragment_out,
-    NGBox**) {
+bool NGTextLayoutAlgorithm::Layout(NGPhysicalFragmentBase** out) {
   // TODO(layout-dev): implement.
-  *fragment_out = nullptr;
-  return NewFragment;
+  *out = nullptr;
+  return true;
 }
 
 DEFINE_TRACE(NGTextLayoutAlgorithm) {
