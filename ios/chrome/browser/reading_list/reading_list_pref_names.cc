248b23b6c1bbaaa6203bfb7280c1f43dda8b4c2d
olivierrobin <olivierrobin@chromium.org>
2016-11-10 01:31:03 -0800

    Remove ReadingListModel::RemoveEntryByUrl
    Cleaning after cl/2486823002
    Replaced by RemoveEntryByURL.
    Move comment on pref name to implementation file for consistency with
    other prefs.
    
    BUG=661668
    
    Review-Url: https://codereview.chromium.org/2485043005
    Cr-Commit-Position: refs/heads/master@{#431222}

--- ios/chrome/browser/reading_list/reading_list_pref_names.cc
+++ ios/chrome/browser/reading_list/reading_list_pref_names.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace reading_list {
 namespace prefs {
 
+// Boolean to track if some reading list entries have never been seen on this
+// device. Not synced.
 const char kReadingListHasUnseenEntries[] = "reading_list.has_unseen_entries";
 
 }  // namespace prefs
--- ios/chrome/browser/reading_list/reading_list_pref_names.h
+++ ios/chrome/browser/reading_list/reading_list_pref_names.h
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace reading_list {
 namespace prefs {
 
-// Boolean to track if some reading list entries have never been seen on this
-// device. Not synced.
 extern const char kReadingListHasUnseenEntries[];
 
 }  // namespace prefs
