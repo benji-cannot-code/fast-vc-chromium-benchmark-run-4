8fe7a8ff2e655a3738ab08b45055c48e92af11ba
cfredric <cfredric@chromium.org>
2016-11-10 09:01:57 -0800

    Stop preloading scripts that have invalid type/language attributes
    This patch removes prefetching of scripts that have invalid type/language
    attributes, and removes the deprecation warning. This also adds/edits tests
    accordingly.
    
    BUG=626321
    
    Review-Url: https://codereview.chromium.org/2479703003
    Cr-Commit-Position: refs/heads/master@{#431277}

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -1265,7 +1265,6 @@ class CORE_EXPORT UseCounter {
     DragAndDropScrollStart = 1554,
     PresentationConnectionListConnectionAvailableEventListener = 1555,
     WebAudioAutoplayCrossOriginIframe = 1556,
-    ScriptInvalidTypeOrLanguage = 1557,
     VRGetDisplays = 1558,
     VRPresent = 1559,
     VRDeprecatedGetPose = 1560,
