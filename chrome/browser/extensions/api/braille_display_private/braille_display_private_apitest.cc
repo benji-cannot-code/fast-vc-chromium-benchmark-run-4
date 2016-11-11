a8797a483b55059bd537ad8d65c2040411b8db4b
brettw <brettw@chromium.org>
2016-11-11 14:14:23 -0800

    Move use_brlapi build flag to chrome.
    It used to be in the global build faetures file but is only used by Chrome.
    
    The define is removed. It was only used in one place to make sure the compile
    matched the build flags, which in this case is uselessly circular.
    
    Review-Url: https://codereview.chromium.org/2497653002
    Cr-Commit-Position: refs/heads/master@{#431665}

--- chrome/browser/extensions/api/braille_display_private/braille_display_private_apitest.cc
+++ chrome/browser/extensions/api/braille_display_private/braille_display_private_apitest.cc
@@ -3,10 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef USE_BRLAPI
-#error This test requires brlapi.
-#endif
-
 #include <stddef.h>
 
 #include <deque>
