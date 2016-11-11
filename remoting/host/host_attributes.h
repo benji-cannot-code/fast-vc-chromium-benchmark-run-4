d91e535910d5ba2351b3a5d31a650dec9bb7c2f6
zijiehe <zijiehe@chromium.org>
2016-11-10 17:50:58 -0800

    [Chromoting] GetHostAttributes with tests
    This change adds GetHostAttributes function, which returns a comma-separated
    string to represent all attributes of the host, into host. Host sends the result
    of this function to client to indicate the capability of host experiments.
    
    This is part of Chromoting Host Experiment feature.
    
    BUG=650926
    
    Review-Url: https://codereview.chromium.org/2464293002
    Cr-Commit-Position: refs/heads/master@{#431451}

---
+++ remoting/host/host_attributes.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_HOST_HOST_ATTRIBUTES_H_
+#define REMOTING_HOST_HOST_ATTRIBUTES_H_
+
+#include <string>
+
+namespace remoting {
+
+// Returns a comma-separated string to represent host attributes. The result may
+// vary if any system configurations change. So consumers should not cache the
+// result.
+// This function is thread-safe.
+std::string GetHostAttributes();
+
+}  // namespace remoting
+
+#endif  // REMOTING_HOST_HOST_ATTRIBUTES_H_
