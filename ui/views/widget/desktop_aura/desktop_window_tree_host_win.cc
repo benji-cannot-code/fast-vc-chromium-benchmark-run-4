b7813eaa77255a6dbb55b4f0081eb61416f4ee3e
jbauman <jbauman@chromium.org>
2016-11-11 11:45:44 -0800

    Make Chrome window opaque when custom drawing system titlebar.
    This will save power by avoiding the need to blend with the contents
    behind the window.
    
    BUG=543904
    
    Review-Url: https://codereview.chromium.org/2487893002
    Cr-Commit-Position: refs/heads/master@{#431619}

--- ui/views/widget/desktop_aura/desktop_window_tree_host_win.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_win.cc
@@ -425,7 +425,7 @@ bool DesktopWindowTreeHostWin::ShouldWindowContentsBeTransparent() const {
   // is therefore transparent. Note: This is not equivalent to calling
   // IsAeroGlassEnabled, because ShouldUseNativeFrame is overridden in a
   // subclass.
-  return ShouldUseNativeFrame();
+  return ShouldUseNativeFrame() && !IsFullscreen();
 }
 
 void DesktopWindowTreeHostWin::FrameTypeChanged() {
@@ -963,7 +963,7 @@ HWND DesktopWindowTreeHostWin::GetHWND() const {
 }
 
 void DesktopWindowTreeHostWin::SetWindowTransparency() {
-  bool transparent = ShouldUseNativeFrame() && !IsFullscreen();
+  bool transparent = ShouldWindowContentsBeTransparent();
   compositor()->SetHostHasTransparentBackground(transparent);
   window()->SetTransparent(transparent);
   content_window_->SetTransparent(transparent);
