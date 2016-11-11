d70ff459c0e5ef68b2009adbc8d68a47d3a9232e
mkwst <mkwst@chromium.org>
2016-11-11 03:14:53 -0800

    Block 'javascript:' navigation in the correct document.
    While working through [1], I noticed that Blink blocks 'javascript:' URL
    navigation in the _target_ document as opposed to in the _navigating_
    document. This is particularly incorrect for `<iframe>`.
    
    This patch corrects the targeting. As a drive-by, this patch also ensures
    that we fire a `SecurityPolicyViolationEvent` for all CSP events. Previously,
    we fired only _unique_ CSP events. This optimization is meant to reduce
    traffic to a server (as completely duplicate reports are ~useless), but
    inadvertently blocked DOM events as well. The change to
    `ContentSecurityPolicy::reportViolation` fixes that.
    
    [1]: https://github.com/w3c/webappsec-csp/issues/127
    
    R=jochen@chromium.org
    
    Review-Url: https://codereview.chromium.org/2490943002
    Cr-Commit-Position: refs/heads/master@{#431539}

--- third_party/WebKit/Source/core/html/HTMLFrameElementBase.cpp
+++ third_party/WebKit/Source/core/html/HTMLFrameElementBase.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/LocalFrame.h"
 #include "core/frame/RemoteFrame.h"
 #include "core/frame/RemoteFrameView.h"
+#include "core/frame/csp/ContentSecurityPolicy.h"
 #include "core/html/parser/HTMLParserIdioms.h"
 #include "core/loader/FrameLoader.h"
 #include "core/loader/FrameLoaderClient.h"
@@ -85,7 +86,21 @@ void HTMLFrameElementBase::openURL(bool replaceCurrentItem) {
   KURL scriptURL;
   KURL url = document().completeURL(m_URL);
   if (protocolIsJavaScript(m_URL)) {
-    scriptURL = url;
+    // We'll set/execute |scriptURL| iff CSP allows us to execute inline
+    // JavaScript. If CSP blocks inline JavaScript, then exit early if
+    // we're trying to execute script in an existing document. If we're
+    // executing JavaScript to create a new document (e.g.
+    // '<iframe src="javascript:...">' then continue loading 'about:blank'
+    // so that the frame is populated with something reasonable.
+    if (ContentSecurityPolicy::shouldBypassMainWorld(&document()) ||
+        document().contentSecurityPolicy()->allowJavaScriptURLs(
+            this, document().url(), OrdinalNumber::first())) {
+      scriptURL = url;
+    } else {
+      if (contentFrame())
+        return;
+    }
+
     url = blankURL();
   }
 
