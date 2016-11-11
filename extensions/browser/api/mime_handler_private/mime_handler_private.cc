8704b27e5554ead0d77247f7bc3b5fdb006acd12
yzshen <yzshen@chromium.org>
2016-11-10 17:33:14 -0800

    Mojo C++ bindings: switch mime_handler mojom target to use STL types.
    BUG=624136
    
    Review-Url: https://codereview.chromium.org/2494873002
    Cr-Commit-Position: refs/heads/master@{#431445}

--- extensions/browser/api/mime_handler_private/mime_handler_private.cc
+++ extensions/browser/api/mime_handler_private/mime_handler_private.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "extensions/browser/api/mime_handler_private/mime_handler_private.h"
 
+#include <unordered_map>
 #include <utility>
 
 #include "base/strings/string_util.h"
@@ -13,18 +14,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_constants.h"
 #include "extensions/browser/guest_view/mime_handler_view/mime_handler_view_guest.h"
 #include "extensions/common/constants.h"
-#include "mojo/public/cpp/bindings/map.h"
 #include "mojo/public/cpp/bindings/strong_binding.h"
 #include "net/http/http_response_headers.h"
 
 namespace extensions {
 namespace {
 
-mojo::Map<mojo::String, mojo::String> CreateResponseHeadersMap(
+std::unordered_map<std::string, std::string> CreateResponseHeadersMap(
     const net::HttpResponseHeaders* headers) {
-  std::map<std::string, std::string> result;
+  std::unordered_map<std::string, std::string> result;
   if (!headers)
-    return mojo::Map<mojo::String, mojo::String>::From(result);
+    return result;
 
   size_t iter = 0;
   std::string header_name;
@@ -44,7 +44,7 @@ mojo::Map<mojo::String, mojo::String> CreateResponseHeadersMap(
       current_value += ", ";
     current_value += header_value;
   }
-  return mojo::Map<mojo::String, mojo::String>::From(result);
+  return result;
 }
 
 }  // namespace
