46e4ab3d41b9ffa927d66b204e7979c77aedcf42
martijn <martijn@martijnc.be>
2016-11-11 02:59:27 -0800

    Correct net::EmbeddedTestServer usage in frame_host tests.
    Adding request handlers to the EmbeddedTestServer after it has started
    isn't thread safe.
    
    This CL updates some tests to only initialize the server before adding
    the CrossSiteRedirector handler so that it can be done in a thread
    safe manner. The server's IO thread is started once the handler is
    added.
    
    BUG=546060
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2491073002
    Cr-Commit-Position: refs/heads/master@{#431538}

--- content/browser/frame_host/frame_tree_browsertest.cc
+++ content/browser/frame_host/frame_tree_browsertest.cc
@@ -51,8 +51,8 @@ class FrameTreeBrowserTest : public ContentBrowserTest {
 
   void SetUpOnMainThread() override {
     host_resolver()->AddRule("*", "127.0.0.1");
-    ASSERT_TRUE(embedded_test_server()->Start());
     SetupCrossSiteRedirector(embedded_test_server());
+    ASSERT_TRUE(embedded_test_server()->Start());
   }
 
  private:
@@ -596,8 +596,8 @@ class CrossProcessFrameTreeBrowserTest : public ContentBrowserTest {
 
   void SetUpOnMainThread() override {
     host_resolver()->AddRule("*", "127.0.0.1");
-    ASSERT_TRUE(embedded_test_server()->Start());
     SetupCrossSiteRedirector(embedded_test_server());
+    ASSERT_TRUE(embedded_test_server()->Start());
   }
 
  private:
@@ -715,8 +715,8 @@ class IsolateIcelandFrameTreeBrowserTest : public ContentBrowserTest {
 
   void SetUpOnMainThread() override {
     host_resolver()->AddRule("*", "127.0.0.1");
-    ASSERT_TRUE(embedded_test_server()->Start());
     SetupCrossSiteRedirector(embedded_test_server());
+    ASSERT_TRUE(embedded_test_server()->Start());
   }
 
  private:
--- content/browser/frame_host/navigation_handle_impl_browsertest.cc
+++ content/browser/frame_host/navigation_handle_impl_browsertest.cc
@@ -319,8 +319,8 @@ class NavigationHandleImplBrowserTest : public ContentBrowserTest {
  protected:
   void SetUpOnMainThread() override {
     host_resolver()->AddRule("*", "127.0.0.1");
-    ASSERT_TRUE(embedded_test_server()->Start());
     SetupCrossSiteRedirector(embedded_test_server());
+    ASSERT_TRUE(embedded_test_server()->Start());
   }
 };
 
