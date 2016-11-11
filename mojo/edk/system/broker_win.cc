fc1f864385ea52958c6affe02aa712ac9a93d9db
rockot <rockot@chromium.org>
2016-11-11 11:09:04 -0800

    Mojo EDK: Add some debugging to Windows Broker
    We're seeing CHECK failures in Canary, indicating that
    the initial message sent from browser to renderer cannot
    be read from the pipe.
    
    This adds some early CHECKs and aliasing to try to
    determine exactly how the read is failing.
    
    BUG=664491
    TBR=sammc@chromium.org
    
    Review-Url: https://codereview.chromium.org/2495023002
    Cr-Commit-Position: refs/heads/master@{#431610}

--- mojo/edk/system/broker_win.cc
+++ mojo/edk/system/broker_win.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <limits>
 #include <utility>
 
+#include "base/debug/alias.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/strings/string_piece.h"
 #include "mojo/edk/embedder/named_platform_handle.h"
@@ -54,6 +55,11 @@ Channel::MessagePtr WaitForBrokerMessage(PlatformHandle platform_handle,
                            kMaxBrokerMessageSize, &bytes_read, nullptr);
   if (!result) {
     PLOG(ERROR) << "Error reading broker pipe";
+
+    DWORD error = GetLastError();
+    base::debug::Alias(&platform_handle);
+    base::debug::Alias(&error);
+    CHECK(false);
     return nullptr;
   }
 
@@ -61,6 +67,11 @@ Channel::MessagePtr WaitForBrokerMessage(PlatformHandle platform_handle,
       Channel::Message::Deserialize(buffer, static_cast<size_t>(bytes_read));
   if (!message || message->payload_size() < sizeof(BrokerMessageHeader)) {
     LOG(ERROR) << "Invalid broker message";
+
+    base::debug::Alias(&buffer[0]);
+    base::debug::Alias(&bytes_read);
+    base::debug::Alias(message.get());
+    CHECK(false);
     return nullptr;
   }
 
@@ -68,6 +79,11 @@ Channel::MessagePtr WaitForBrokerMessage(PlatformHandle platform_handle,
       reinterpret_cast<const BrokerMessageHeader*>(message->payload());
   if (header->type != expected_type) {
     LOG(ERROR) << "Unexpected broker message type";
+
+    base::debug::Alias(&buffer[0]);
+    base::debug::Alias(&bytes_read);
+    base::debug::Alias(message.get());
+    CHECK(false);
     return nullptr;
   }
 
