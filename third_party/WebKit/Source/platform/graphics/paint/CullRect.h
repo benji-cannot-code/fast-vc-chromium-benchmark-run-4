e77721722eac5dfacbf7169c98f32c306a96c344
wangxianzhu <wangxianzhu@chromium.org>
2016-11-11 14:03:52 -0800

    Add default constructor and operator==/!= for CullRect
    This is split from https://codereview.chromium.org/2430313004/
    
    BUG=663208
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2496983002
    Cr-Commit-Position: refs/heads/master@{#431661}

--- third_party/WebKit/Source/platform/graphics/paint/CullRect.h
+++ third_party/WebKit/Source/platform/graphics/paint/CullRect.h
@@ -25,6 +25,7 @@ class PLATFORM_EXPORT CullRect {
   DISALLOW_NEW();
 
  public:
+  CullRect() {}
   explicit CullRect(const IntRect& rect) : m_rect(rect) {}
   CullRect(const CullRect&, const IntPoint& offset);
   CullRect(const CullRect&, const IntSize& offset);
@@ -42,6 +43,8 @@ class PLATFORM_EXPORT CullRect {
  private:
   IntRect m_rect;
 
+  friend bool operator==(const CullRect&, const CullRect&);
+
   // TODO(chrishtr): temporary while we implement CullRect everywhere.
   friend class FramePainter;
   friend class GridPainter;
@@ -53,6 +56,13 @@ class PLATFORM_EXPORT CullRect {
   friend class ThemePainterMac;
   friend class WebPluginContainerImpl;
 };
+
+inline bool operator==(const CullRect& a, const CullRect& b) {
+  return a.m_rect == b.m_rect;
+}
+inline bool operator!=(const CullRect& a, const CullRect& b) {
+  return !(a == b);
+}
 
 }  // namespace blink
 
