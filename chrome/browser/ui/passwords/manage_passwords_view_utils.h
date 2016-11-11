adb5931b31e9cbe29d1316f0f611a1043d0d9883
vabr <vabr@chromium.org>
2016-11-11 02:08:44 -0800

    Correct a typo: PasswordTittleType -> PasswordTitleType
    (Interestingly, a "tittle" also has a meaning: a tiny amount or part of
    something. Don't ask what UrbanDictionary suggests as "something" here.)
    
    BUG=658902
    
    Review-Url: https://codereview.chromium.org/2493883004
    Cr-Commit-Position: refs/heads/master@{#431534}

--- chrome/browser/ui/passwords/manage_passwords_view_utils.h
+++ chrome/browser/ui/passwords/manage_passwords_view_utils.h
@@ -19,7 +19,7 @@ class Range;
 
 class GURL;
 
-enum class PasswordTittleType {
+enum class PasswordTitleType {
   SAVE_PASSWORD,    // plain password
   SAVE_ACCOUNT,     // login via IDP
   UPDATE_PASSWORD,  // update plain password
@@ -52,7 +52,7 @@ void GetSavePasswordDialogTitleTextAndLinkRange(
     const GURL& user_visible_url,
     const GURL& form_origin_url,
     bool is_smartlock_branding_enabled,
-    PasswordTittleType dialog_type,
+    PasswordTitleType dialog_type,
     base::string16* title,
     gfx::Range* title_link_range);
 
