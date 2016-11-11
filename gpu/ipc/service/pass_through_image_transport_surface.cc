8a6802889fb2c9430c351dd6a7f99fa87ef38b7e
fsamuel <fsamuel@chromium.org>
2016-11-11 13:35:15 -0800

    gpu: Fix Mac fullscreen video
    https://codereview.chromium.org/2492583002/ introduced a bug where
    one of the SwapBufeersCompleteParams were dropped before sending
    an IPC to the gpu host. This CL plumbs the fullscreen_low_power_ca_context_id
    parameter and deletes a stale comment.
    
    BUG=664489
    TBR=piman@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2499703002
    Cr-Commit-Position: refs/heads/master@{#431652}

--- gpu/ipc/service/pass_through_image_transport_surface.cc
+++ gpu/ipc/service/pass_through_image_transport_surface.cc
@@ -143,8 +143,6 @@ void PassThroughImageTransportSurface::SetLatencyInfo(
 void PassThroughImageTransportSurface::SendVSyncUpdateIfAvailable() {
   gfx::VSyncProvider* vsync_provider = GetVSyncProvider();
   if (vsync_provider) {
-    // PassThroughImageTransportSurface owns the VSyncProvider and will
-    // outlive it. Thus, base::Unretained is safe here.
     vsync_provider->GetVSyncParameters(base::Bind(
         &ImageTransportSurfaceDelegate::UpdateVSyncParameters, delegate_));
   }
