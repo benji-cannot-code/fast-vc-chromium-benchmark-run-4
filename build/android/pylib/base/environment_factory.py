4c980f3594c81d904a1a184ac8c1358840fbd3bf
mikecase <mikecase@chromium.org>
2016-11-11 16:38:10 -0800

    Add support for Junit tests in platform mode.
    Adding support for Junit tests in platform mode. Added new test env
    for running tests on local machine.
    
    Review-Url: https://codereview.chromium.org/2498553004
    Cr-Commit-Position: refs/heads/master@{#431711}

--- build/android/pylib/base/environment_factory.py
+++ build/android/pylib/base/environment_factory.py
@@ -5,12 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 from pylib import constants
 from pylib.local.device import local_device_environment
+from pylib.local.machine import local_machine_environment
 
 def CreateEnvironment(args, error_func):
 
   if args.environment == 'local':
     if args.command not in constants.LOCAL_MACHINE_TESTS:
       return local_device_environment.LocalDeviceEnvironment(args, error_func)
-    # TODO(jbudorick) Add local machine environment.
+    else:
+      return local_machine_environment.LocalMachineEnvironment(args, error_func)
 
   error_func('Unable to create %s environment.' % args.environment)
--- build/android/pylib/base/test_run_factory.py
+++ build/android/pylib/base/test_run_factory.py
@@ -5,10 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 from pylib.gtest import gtest_test_instance
 from pylib.instrumentation import instrumentation_test_instance
+from pylib.junit import junit_test_instance
 from pylib.local.device import local_device_environment
 from pylib.local.device import local_device_gtest_run
 from pylib.local.device import local_device_instrumentation_test_run
 from pylib.local.device import local_device_perf_test_run
+from pylib.local.machine import local_machine_environment
+from pylib.local.machine import local_machine_junit_test_run
 from pylib.perf import perf_test_instance
 
 
@@ -34,6 +37,11 @@ def CreateTestRun(args, env, test_instance, error_func):
     if isinstance(test_instance,
                   perf_test_instance.PerfTestInstance):
       return _CreatePerfTestRun(args, env, test_instance)
+
+  if isinstance(env, local_machine_environment.LocalMachineEnvironment):
+    if isinstance(test_instance, junit_test_instance.JunitTestInstance):
+      return (local_machine_junit_test_run
+              .LocalMachineJunitTestRun(env, test_instance))
 
   error_func('Unable to create test run for %s tests in %s environment'
              % (str(test_instance), str(env)))
