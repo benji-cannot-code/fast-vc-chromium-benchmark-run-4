f449727101a8ae38eaffa7b98fe32fe6877a5bcb
zijiehe <zijiehe@chromium.org>
2016-11-11 19:40:12 -0800

    [Chromoting] Build break after change 2494013003
    One reference to the deprecated screen_capturer_mock_objects.h has been missed
    from change https://codereview.chromium.org/2494013003/.
    
    BUG=webrtc:6513
    
    TBR=sergeyu@chromium.org
    
    Review-Url: https://codereview.chromium.org/2491773007
    Cr-Commit-Position: refs/heads/master@{#431758}

--- remoting/protocol/video_frame_pump_unittest.cc
+++ remoting/protocol/video_frame_pump_unittest.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/webrtc/modules/desktop_capture/desktop_frame.h"
-#include "third_party/webrtc/modules/desktop_capture/screen_capturer_mock_objects.h"
 
 using ::testing::_;
 using ::testing::AtLeast;
