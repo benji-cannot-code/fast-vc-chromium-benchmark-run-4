75185a0d44669329b31195c4b288d3c9cf6f56f1
jochen <jochen@chromium.org>
2016-11-12 14:23:44 -0800

    Wait for history backend in popup blocker test
    BUG=663333
    R=avi@chromium.org
    
    Review-Url: https://codereview.chromium.org/2495053003
    Cr-Commit-Position: refs/heads/master@{#431794}

---
+++ chrome/browser/history/history_test_utils.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_HISTORY_HISTORY_TEST_UTILS_H_
+#define CHROME_BROWSER_HISTORY_HISTORY_TEST_UTILS_H_
+
+class Profile;
+
+// Spins the current message loop until all pending messages on the history DB
+// thread complete.
+void WaitForHistoryBackendToRun(Profile* profile);
+
+#endif  // CHROME_BROWSER_HISTORY_HISTORY_TEST_UTILS_H_
