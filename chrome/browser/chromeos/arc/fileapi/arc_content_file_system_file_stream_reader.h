f2042b43c579ad9fd1d95f1719ce3f809aa49ba7
hashimoto <hashimoto@chromium.org>
2016-11-14 15:55:32 -0800

    arc: Implement ArcContentFileSystemFileStreamReader::Read/GetLength
    Non-zero offset handling will be implemented later.
    
    BUG=654684
    TEST=unit_tests
    
    Review-Url: https://codereview.chromium.org/2470843004
    Cr-Commit-Position: refs/heads/master@{#431992}

--- chrome/browser/chromeos/arc/fileapi/arc_content_file_system_file_stream_reader.h
+++ chrome/browser/chromeos/arc/fileapi/arc_content_file_system_file_stream_reader.h
@@ -6,15 +6,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_ARC_FILEAPI_ARC_CONTENT_FILE_SYSTEM_FILE_STREAM_READER_H_
 #define CHROME_BROWSER_CHROMEOS_ARC_FILEAPI_ARC_CONTENT_FILE_SYSTEM_FILE_STREAM_READER_H_
 
+#include <memory>
+
+#include "base/memory/weak_ptr.h"
+#include "mojo/public/cpp/system/handle.h"
+#include "net/base/completion_callback.h"
 #include "storage/browser/fileapi/file_stream_reader.h"
+#include "url/gurl.h"
+
+namespace net {
+class FileStream;
+class IOBuffer;
+}
 
 namespace arc {
 
+// FileStreamReader implementation for ARC content file system.
 class ArcContentFileSystemFileStreamReader : public storage::FileStreamReader {
  public:
-  ArcContentFileSystemFileStreamReader(const storage::FileSystemURL& url,
-                                       int64_t offset,
-                                       int64_t max_bytes_to_read);
+  ArcContentFileSystemFileStreamReader(const GURL& arc_url, int64_t offset);
   ~ArcContentFileSystemFileStreamReader() override;
 
   // storage::FileStreamReader override:
@@ -24,6 +34,18 @@ class ArcContentFileSystemFileStreamReader : public storage::FileStreamReader {
   int64_t GetLength(const net::Int64CompletionCallback& callback) override;
 
  private:
+  void OnOpenFile(scoped_refptr<net::IOBuffer> buf,
+                  int buffer_length,
+                  const net::CompletionCallback& callback,
+                  mojo::ScopedHandle handle);
+
+  GURL arc_url_;
+  int64_t offset_;
+
+  std::unique_ptr<net::FileStream> file_stream_;
+
+  base::WeakPtrFactory<ArcContentFileSystemFileStreamReader> weak_ptr_factory_;
+
   DISALLOW_COPY_AND_ASSIGN(ArcContentFileSystemFileStreamReader);
 };
 
