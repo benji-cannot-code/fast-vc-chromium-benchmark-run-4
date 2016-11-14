5f727f8adb7e097881525e31d8578b89d9c988a2
jbudorick <jbudorick@chromium.org>
2016-11-14 15:18:40 -0800

    [Android] Fix instrumentation test filtering for parameterized tests.
    BUG=664631
    
    Review-Url: https://codereview.chromium.org/2491373005
    Cr-Commit-Position: refs/heads/master@{#431958}

--- build/android/pylib/local/device/local_device_instrumentation_test_run.py
+++ build/android/pylib/local/device/local_device_instrumentation_test_run.py
@@ -14,6 +14,7 @@ from devil.android import flag_changer
 from devil.utils import reraiser_thread
 from pylib import valgrind_tools
 from pylib.base import base_test_result
+from pylib.instrumentation import instrumentation_test_instance
 from pylib.local.device import local_device_environment
 from pylib.local.device import local_device_test_run
 import tombstones
@@ -183,21 +184,9 @@ class LocalDeviceInstrumentationTestRun(
   def _GetTests(self):
     return self._test_instance.GetTests()
 
-  def _GetTestName(self, test):
-    # pylint: disable=no-self-use
-    return '%s#%s' % (test['class'], test['method'])
-
   #override
   def _GetUniqueTestName(self, test):
-    display_name = self._GetTestName(test)
-    if 'flags' in test:
-      flags = test['flags']
-      if flags.add:
-        display_name = '%s with {%s}' % (display_name, ' '.join(flags.add))
-      if flags.remove:
-        display_name = '%s without {%s}' % (
-            display_name, ' '.join(flags.remove))
-    return display_name
+    return instrumentation_test_instance.GetUniqueTestName(test)
 
   #override
   def _RunTest(self, device, test):
@@ -221,7 +210,7 @@ class LocalDeviceInstrumentationTestRun(
                         'Please build it and try again.')
 
       def name_and_timeout(t):
-        n = self._GetTestName(t)
+        n = instrumentation_test_instance.GetTestName(t)
         i = self._GetTimeoutFromAnnotations(t['annotations'], n)
         return (n, i)
 
@@ -237,7 +226,7 @@ class LocalDeviceInstrumentationTestRun(
               test_list=test_names))
       timeout = sum(timeouts)
     else:
-      test_name = self._GetTestName(test)
+      test_name = instrumentation_test_instance.GetTestName(test)
       test_display_name = self._GetUniqueTestName(test)
       target = '%s/%s' % (
           self._test_instance.test_package, self._test_instance.test_runner)
