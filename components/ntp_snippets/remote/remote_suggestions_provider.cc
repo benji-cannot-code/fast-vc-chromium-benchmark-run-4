9a00cb76b51471ae00f884956d8f6e2017cfeed3
treib <treib@chromium.org>
2016-11-15 02:05:45 -0800

    [NTP Snippets] Record the state of the RemoteSuggestionsProvider in UMA
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2497883002
    Cr-Commit-Position: refs/heads/master@{#432157}

--- components/ntp_snippets/remote/remote_suggestions_provider.cc
+++ components/ntp_snippets/remote/remote_suggestions_provider.cc
@@ -1092,6 +1092,10 @@ void RemoteSuggestionsProvider::EnterState(State state) {
   if (state == state_)
     return;
 
+  UMA_HISTOGRAM_ENUMERATION("NewTabPage.Snippets.EnteredState",
+                            static_cast<int>(state),
+                            static_cast<int>(State::COUNT));
+
   switch (state) {
     case State::NOT_INITED:
       // Initial state, it should not be possible to get back there.
@@ -1118,6 +1122,10 @@ void RemoteSuggestionsProvider::EnterState(State state) {
       DVLOG(1) << "Entering state: ERROR_OCCURRED";
       state_ = State::ERROR_OCCURRED;
       EnterStateError();
+      break;
+
+    case State::COUNT:
+      NOTREACHED();
       break;
   }
 
--- components/ntp_snippets/remote/remote_suggestions_provider.h
+++ components/ntp_snippets/remote/remote_suggestions_provider.h
@@ -192,7 +192,9 @@ class RemoteSuggestionsProvider final
 
     // The service or one of its dependencies encountered an unrecoverable error
     // and the service can't be used anymore.
-    ERROR_OCCURRED
+    ERROR_OCCURRED,
+
+    COUNT
   };
 
   struct CategoryContent {
