28d3640c023bdef8acf0013f1ea31331f1fcbd57
nick <nick@chromium.org>
2016-11-15 12:42:45 -0800

    RenderWidget: make routing_id be a parameter of the ctor
    This fixes a longstanding issue where, in layout-test-specific code, a
    RenderViewObserver was instantiated before the RenderView was fully
    initialized with a routing ID. RenderViewObserver internally caches
    the routing id, so that's dangerous.
    
    BUG=627852
    
    Review-Url: https://codereview.chromium.org/2493293002
    Cr-Commit-Position: refs/heads/master@{#432246}

--- content/renderer/render_widget_fullscreen.cc
+++ content/renderer/render_widget_fullscreen.cc
@@ -26,9 +26,11 @@ void RenderWidgetFullscreen::show(blink::WebNavigationPolicy) {
 }
 
 RenderWidgetFullscreen::RenderWidgetFullscreen(
+    int32_t widget_routing_id,
     CompositorDependencies* compositor_deps,
     const ScreenInfo& screen_info)
-    : RenderWidget(compositor_deps,
+    : RenderWidget(widget_routing_id,
+                   compositor_deps,
                    blink::WebPopupTypeNone,
                    screen_info,
                    false,
@@ -45,14 +47,6 @@ WebWidget* RenderWidgetFullscreen::CreateWebWidget() {
 bool RenderWidgetFullscreen::Init(int32_t opener_id) {
   DCHECK(!GetWebWidget());
 
-  // Synchronous IPC to obtain a routing id for ourselves.
-  int32_t routing_id = MSG_ROUTING_NONE;
-  if (!RenderThreadImpl::current_render_message_filter()
-           ->CreateFullscreenWidget(opener_id, &routing_id)) {
-    return false;
-  }
-
-  RenderWidget::InitRoutingID(routing_id);
   RenderWidget::Init(opener_id, CreateWebWidget());
   return true;
 }
--- content/renderer/render_widget_fullscreen_pepper.h
+++ content/renderer/render_widget_fullscreen_pepper.h
@@ -31,6 +31,7 @@ class RenderWidgetFullscreenPepper : public RenderWidgetFullscreen,
                                      public FullscreenContainer {
  public:
   static RenderWidgetFullscreenPepper* Create(
+      int32_t routing_id,
       int32_t opener_id,
       CompositorDependencies* compositor_deps,
       PepperPluginInstanceImpl* plugin,
@@ -57,7 +58,8 @@ class RenderWidgetFullscreenPepper : public RenderWidgetFullscreen,
   }
 
  protected:
-  RenderWidgetFullscreenPepper(CompositorDependencies* compositor_deps,
+  RenderWidgetFullscreenPepper(int32_t routing_id,
+                               CompositorDependencies* compositor_deps,
                                PepperPluginInstanceImpl* plugin,
                                const GURL& active_url,
                                const ScreenInfo& screen_info);
