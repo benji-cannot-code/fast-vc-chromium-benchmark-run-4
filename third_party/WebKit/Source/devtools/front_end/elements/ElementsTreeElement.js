66fbba1ab93fe8bc4a1a9c66e33a2ee798ea4dea
dgozman <dgozman@chromium.org>
2016-11-15 09:05:10 -0800

    [DevTools] Remove preventFollowOnDoubleClick.
    It doesn't bring any value - double click still doesn't work on urls in attributes.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2500293002
    Cr-Commit-Position: refs/heads/master@{#432201}

--- third_party/WebKit/Source/devtools/front_end/elements/ElementsTreeElement.js
+++ third_party/WebKit/Source/devtools/front_end/elements/ElementsTreeElement.js
@@ -264,18 +264,7 @@ Elements.ElementsTreeElement = class extends TreeElement {
     }
 
     this.updateTitle();
-    this._preventFollowingLinksOnDoubleClick();
     this.listItemElement.draggable = true;
-  }
-
-  _preventFollowingLinksOnDoubleClick() {
-    var links = this.listItemElement.querySelectorAll(
-        'li .webkit-html-tag > .webkit-html-attribute > .webkit-html-external-link, li .webkit-html-tag > .webkit-html-attribute > .webkit-html-resource-link');
-    if (!links)
-      return;
-
-    for (var i = 0; i < links.length; ++i)
-      links[i].preventFollowOnDoubleClick = true;
   }
 
   /**
@@ -1023,7 +1012,6 @@ Elements.ElementsTreeElement = class extends TreeElement {
     delete this.selectionElement;
     if (this.selected)
       this._createSelection();
-    this._preventFollowingLinksOnDoubleClick();
     this._highlightSearchResults();
   }
 
