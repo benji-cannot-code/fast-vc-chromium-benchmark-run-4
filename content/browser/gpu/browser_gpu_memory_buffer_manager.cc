f20113fe4a54acb3ee813e824ba991ba8446ac55
sadrul <sadrul@chromium.org>
2016-11-14 16:40:02 -0800

    gpu: Reuse existing code for shared memory allocation.
    Introduce base::SharedMemory::TakeHandle() to take ownership of the handle
    to shared memory. This is in order to avoid unnecessary DuplicateHandle(),
    or GiveToProcess(), which is already taken care of by mojo, since these
    shm handles go over a mojo message-pipe.
    
    BUG=643746
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2496783002
    Cr-Commit-Position: refs/heads/master@{#432022}

--- content/browser/gpu/browser_gpu_memory_buffer_manager.cc
+++ content/browser/gpu/browser_gpu_memory_buffer_manager.cc
@@ -289,7 +289,7 @@ void BrowserGpuMemoryBufferManager::AllocateGpuMemoryBufferForChildProcess(
   }
 
   callback.Run(gpu::GpuMemoryBufferImplSharedMemory::AllocateForChildProcess(
-      id, size, format, child_process_handle));
+      id, size, format));
 }
 
 void BrowserGpuMemoryBufferManager::SetDestructionSyncToken(
