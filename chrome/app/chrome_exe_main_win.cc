931553375da829e0462917e67e4129fb87faa841
grt <grt@chromium.org>
2016-11-15 14:26:30 -0800

    Windows install_static refactor.
    This change introduces install_static::InstallDetails, a module-global
    holder of details relating to the current install. It also more clearly
    defines the concept of a primary install mode and one or more secondary
    install modes (e.g., "Chrome SxS" for Google Chrome's canary channel).
    Furthermore, it creates a clear boundary for brand-specific constants so
    that Chromium and Google Chrome don't bleed into one another. See
    chrome/install_static/README.md for more information.
    
    BUG=373987
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    
    Committed: https://crrev.com/92bc27b8b101d2b300a7b3afd79265b3cfba1152
    Review-Url: https://codereview.chromium.org/2422643002
    Cr-Original-Commit-Position: refs/heads/master@{#430728}
    Cr-Commit-Position: refs/heads/master@{#432270}

--- chrome/app/chrome_exe_main_win.cc
+++ chrome/app/chrome_exe_main_win.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/at_exit.h"
 #include "base/command_line.h"
 #include "base/files/file_path.h"
-#include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "base/path_service.h"
 #include "base/strings/string16.h"
@@ -27,9 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/app/main_dll_loader_win.h"
 #include "chrome/browser/policy/policy_path_parser.h"
 #include "chrome/browser/win/chrome_process_finder.h"
+#include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_paths_internal.h"
 #include "chrome/common/chrome_switches.h"
-#include "chrome/installer/util/browser_distribution.h"
+#include "chrome/install_static/install_details.h"
 #include "chrome_elf/chrome_elf_main.h"
 #include "components/crash/content/app/crash_switches.h"
 #include "components/crash/content/app/crashpad.h"
@@ -205,6 +205,8 @@ int APIENTRY wWinMain(HINSTANCE instance, HINSTANCE prev, wchar_t*, int) {
 int main() {
   HINSTANCE instance = GetModuleHandle(nullptr);
 #endif
+  install_static::InstallDetails::InitializeFromPrimaryModule(
+      chrome::kChromeElfDllName);
   SignalInitializeCrashReporting();
 
   // Initialize the CommandLine singleton from the environment.
