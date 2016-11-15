d8d660d1bf78d8fa3ca77b8ea0855434b6967734
pasko <pasko@chromium.org>
2016-11-14 16:07:13 -0800

    Prerender: remove tiny leftovers from refactoring
    Refactoring was at: https://codereview.chromium.org/707173004
    A few forward-declarations are now unused -> removing.
    
    BUG=425756, 400186
    
    Review-Url: https://codereview.chromium.org/2496243002
    Cr-Commit-Position: refs/heads/master@{#432001}

--- chrome/browser/prerender/prerender_tab_helper.h
+++ chrome/browser/prerender/prerender_tab_helper.h
@@ -17,14 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents_user_data.h"
 #include "url/gurl.h"
 
-namespace autofill {
-struct PasswordForm;
-}
-
-namespace password_manager {
-class PasswordManager;
-}
-
 namespace prerender {
 
 class PrerenderManager;
