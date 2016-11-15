aa3e8c79c2e035c41fe8b02a8433f123be9cd8b7
vakh <vakh@chromium.org>
2016-11-14 19:28:45 -0800

    Implement support for checking bad IPs aka MatchMalwareIP
    It works as follows:
    1. Get an IP address as string.
    2. Convert it to IPV6, if needed.
    3. Convert the IPV6 address to packed string.
    4. Generate SHA1 hash of the packed string.
    5. Append 128 at the end because the server does so.
    6. Compare it to the hashes sent by the server.
    7. Do not send a full hash request. A local match is sufficient to determine
       reputation.
    
    BUG=543161
    
    Review-Url: https://codereview.chromium.org/2495783003
    Cr-Commit-Position: refs/heads/master@{#432067}

--- components/safe_browsing_db/v4_local_database_manager.cc
+++ components/safe_browsing_db/v4_local_database_manager.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/metrics/histogram_macros.h"
 #include "components/safe_browsing_db/v4_feature_list.h"
+#include "components/safe_browsing_db/v4_protocol_manager_util.h"
 #include "content/public/browser/browser_thread.h"
 
 using content::BrowserThread;
@@ -237,9 +238,23 @@ bool V4LocalDatabaseManager::MatchDownloadWhitelistUrl(const GURL& url) {
 }
 
 bool V4LocalDatabaseManager::MatchMalwareIP(const std::string& ip_address) {
-  // TODO(vakh): Implement this skeleton.
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
-  return false;
+  if (!enabled_) {
+    return false;
+  }
+  FullHash hashed_encoded_ip;
+  if (!V4ProtocolManagerUtil::IPAddressToEncodedIPV6Hash(ip_address,
+                                                         &hashed_encoded_ip)) {
+    return false;
+  }
+
+  std::set<FullHash> hashed_encoded_ips{hashed_encoded_ip};
+  std::unique_ptr<PendingCheck> check = base::MakeUnique<PendingCheck>(
+      nullptr, ClientCallbackType::CHECK_MALWARE_IP,
+      StoresToCheck({GetAnyIpMalwareId()}), hashed_encoded_ips);
+
+  // HandleCheckSynchronously() tells us whether the resource is safe.
+  return !HandleCheckSynchronously(std::move(check));
 }
 
 bool V4LocalDatabaseManager::MatchModuleWhitelistString(
@@ -372,7 +387,8 @@ bool V4LocalDatabaseManager::GetPrefixMatches(
   const base::TimeTicks before = TimeTicks::Now();
   if (check->client_callback_type == ClientCallbackType::CHECK_BROWSE_URL ||
       check->client_callback_type == ClientCallbackType::CHECK_DOWNLOAD_URLS ||
-      check->client_callback_type == ClientCallbackType::CHECK_EXTENSION_IDS) {
+      check->client_callback_type == ClientCallbackType::CHECK_EXTENSION_IDS ||
+      check->client_callback_type == ClientCallbackType::CHECK_MALWARE_IP) {
     DCHECK(!check->full_hashes.empty());
 
     full_hash_to_store_and_hash_prefixes->clear();
@@ -456,7 +472,18 @@ bool V4LocalDatabaseManager::HandleCheck(std::unique_ptr<PendingCheck> check) {
       base::Bind(&V4LocalDatabaseManager::PerformFullHashCheck, this,
                  base::Passed(std::move(check)),
                  full_hash_to_store_and_hash_prefixes));
+
   return false;
+}
+
+bool V4LocalDatabaseManager::HandleCheckSynchronously(
+    std::unique_ptr<PendingCheck> check) {
+  if (!v4_database_) {
+    return true;
+  }
+
+  FullHashToStoreAndHashPrefixesMap full_hash_to_store_and_hash_prefixes;
+  return !GetPrefixMatches(check, &full_hash_to_store_and_hash_prefixes);
 }
 
 void V4LocalDatabaseManager::OnFullHashResponse(
