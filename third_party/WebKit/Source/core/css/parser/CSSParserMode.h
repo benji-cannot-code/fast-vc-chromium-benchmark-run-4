74e205775e13a2ffd14b42ffdca1ef6538462dfd
kochi <kochi@chromium.org>
2016-11-14 22:47:07 -0800

    Introduce CSS parser mode for distinguishing static/dynamic profile
    Static/dynamic profile is proposed at CSS Selectors Level 4 draft[1]
    to selectively enable or disable CSS selectors depending on the
    performance characteristics that selectors require.
    
    '>>>' (aka /deep/) is planned to be enabled under the experimental
    flag in static profile [2].  In the future :has() pseudo class will
    also be enabled in static profile.
    
    Implementation for '>>>' combinator is done in later CLs.
    
    [1] https://drafts.csswg.org/selectors/#profiles
    [2] https://github.com/w3c/webcomponents/issues/78
    
    BUG=633007
    
    Review-Url: https://codereview.chromium.org/2493003003
    Cr-Commit-Position: refs/heads/master@{#432133}

--- third_party/WebKit/Source/core/css/parser/CSSParserMode.h
+++ third_party/WebKit/Source/core/css/parser/CSSParserMode.h
@@ -86,6 +86,9 @@ class CORE_EXPORT CSSParserContext {
   USING_FAST_MALLOC(CSSParserContext);
 
  public:
+  // https://drafts.csswg.org/selectors/#profiles
+  enum SelectorProfile { DynamicProfile, StaticProfile };
+
   CSSParserContext(CSSParserMode, UseCounter*);
   // FIXME: We shouldn't need the UseCounter argument as we could infer it from
   // the Document but some callers want to disable use counting (e.g. the
@@ -93,7 +96,8 @@ class CORE_EXPORT CSSParserContext {
   CSSParserContext(const Document&,
                    UseCounter*,
                    const KURL& baseURL = KURL(),
-                   const String& charset = emptyString());
+                   const String& charset = emptyString(),
+                   SelectorProfile = DynamicProfile);
   // FIXME: This constructor shouldn't exist if we properly piped the UseCounter
   // through the CSS subsystem. Currently the UseCounter life time is too crazy
   // and we need a way to override it.
@@ -110,6 +114,8 @@ class CORE_EXPORT CSSParserContext {
   const String& charset() const { return m_charset; }
   const Referrer& referrer() const { return m_referrer; }
   bool isHTMLDocument() const { return m_isHTMLDocument; }
+  bool isDynamicProfile() const { return m_profile == DynamicProfile; }
+  bool isStaticProfile() const { return m_profile == StaticProfile; }
 
   // This quirk is to maintain compatibility with Android apps built on
   // the Android SDK prior to and including version 18. Presumably, this
@@ -141,6 +147,7 @@ class CORE_EXPORT CSSParserContext {
   String m_charset;
   CSSParserMode m_mode;
   CSSParserMode m_matchMode;
+  SelectorProfile m_profile = DynamicProfile;
   Referrer m_referrer;
   bool m_isHTMLDocument;
   bool m_useLegacyBackgroundSizeShorthandBehavior;
