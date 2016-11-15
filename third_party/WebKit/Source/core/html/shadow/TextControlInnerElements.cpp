d96cda2f75ad9a6a6ea53f4be26693ec5df352b8
tkent <tkent@chromium.org>
2016-11-14 21:08:43 -0800

    Rename blink::LayoutTextControlInnerBlock to blink::LayoutTextControlInnerEditor.
    For consistency with TextControlInnerEditorElement.
    This CL has no behavior changes.
    
    Review-Url: https://codereview.chromium.org/2499233002
    Cr-Commit-Position: refs/heads/master@{#432109}

--- third_party/WebKit/Source/core/html/shadow/TextControlInnerElements.cpp
+++ third_party/WebKit/Source/core/html/shadow/TextControlInnerElements.cpp
@@ -134,7 +134,7 @@ void TextControlInnerEditorElement::defaultEventHandler(Event* event) {
 
 LayoutObject* TextControlInnerEditorElement::createLayoutObject(
     const ComputedStyle&) {
-  return new LayoutTextControlInnerBlock(this);
+  return new LayoutTextControlInnerEditor(this);
 }
 
 PassRefPtr<ComputedStyle>
