b404e9944eef61493e7c141cc8cd6e06208df8c9
rdevlin.cronin <rdevlin.cronin@chromium.org>
2016-11-15 14:34:22 -0800

    [Extensions] Add missing include
    Fix failure from crrev.com/2097de33a1f3d04e78d93e9e2f16aad4b97e47d7
    
    BUG=665517
    BUG=660498
    NOTRY=true (cast_shell_linux flaking out of control)
    
    Review-Url: https://codereview.chromium.org/2502063002
    Cr-Commit-Position: refs/heads/master@{#432274}

--- chrome/browser/extensions/api/tabs/tabs_api_unittest.cc
+++ chrome/browser/extensions/api/tabs/tabs_api_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_side_navigation_test_utils.h"
 #include "content/public/test/web_contents_tester.h"
 #include "extensions/browser/api_test_utils.h"
+#include "extensions/common/constants.h"
 #include "extensions/common/extension_builder.h"
 #include "extensions/common/test_util.h"
 
