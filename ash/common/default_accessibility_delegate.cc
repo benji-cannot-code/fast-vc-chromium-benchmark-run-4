f4dc843b5c4fa7813f151c21c94f262f3be43a5e
dmazzoni <dmazzoni@chromium.org>
2016-11-14 22:20:43 -0800

    Toggle spoken feedback if two fingers are held down.
    Blind users need a way to toggle spoken feedback on devices that only
    have a touch screen, and devices that are temporarily in a mode where
    the keyboard is disabled.
    
    Add a class that watches for two fingers held down, plays a tick sound
    to let the user know something might happen, then toggles spoken feedback
    if two fingers remain down without moving for enough time.
    
    BUG=662501
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2476843003
    Cr-Commit-Position: refs/heads/master@{#432129}

--- ash/common/default_accessibility_delegate.cc
+++ ash/common/default_accessibility_delegate.cc
@@ -147,6 +147,9 @@ AccessibilityAlert DefaultAccessibilityDelegate::GetLastAccessibilityAlert() {
   return accessibility_alert_;
 }
 
+void DefaultAccessibilityDelegate::PlaySpokenFeedbackToggleCountdown(
+    int tick_count) {}
+
 void DefaultAccessibilityDelegate::PlayEarcon(int sound_key) {}
 
 base::TimeDelta DefaultAccessibilityDelegate::PlayShutdownSound() const {
--- ui/chromeos/touch_exploration_controller.cc
+++ ui/chromeos/touch_exploration_controller.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window.h"
 #include "ui/aura/window_event_dispatcher.h"
 #include "ui/aura/window_tree_host.h"
+#include "ui/chromeos/touch_accessibility_enabler.h"
 #include "ui/events/event.h"
 #include "ui/events/event_processor.h"
 #include "ui/events/event_utils.h"
@@ -41,14 +42,16 @@ void SetTouchAccessibilityFlag(ui::Event* event) {
 
 TouchExplorationController::TouchExplorationController(
     aura::Window* root_window,
-    TouchExplorationControllerDelegate* delegate)
+    TouchExplorationControllerDelegate* delegate,
+    TouchAccessibilityEnabler* touch_accessibility_enabler)
     : root_window_(root_window),
       delegate_(delegate),
       state_(NO_FINGERS_DOWN),
       anchor_point_state_(ANCHOR_POINT_NONE),
       gesture_provider_(new GestureProviderAura(this, this)),
       prev_state_(NO_FINGERS_DOWN),
-      VLOG_on_(true) {
+      VLOG_on_(true),
+      touch_accessibility_enabler_(touch_accessibility_enabler) {
   DCHECK(root_window);
   root_window->GetHost()->GetEventSource()->AddEventRewriter(this);
 }
@@ -84,6 +87,10 @@ ui::EventRewriteStatus TouchExplorationController::RewriteEvent(
     return ui::EVENT_REWRITE_CONTINUE;
   }
   const ui::TouchEvent& touch_event = static_cast<const ui::TouchEvent&>(event);
+
+  // Let TouchAccessibilityEnabler process the unrewritten event.
+  if (touch_accessibility_enabler_)
+    touch_accessibility_enabler_->HandleTouchEvent(touch_event);
 
   if (!exclude_bounds_.IsEmpty()) {
     gfx::Point location = touch_event.location();
