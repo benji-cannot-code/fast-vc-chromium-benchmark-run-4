0ec1aa88786f87d8ed54bade494681d148f874ad
bajones <bajones@chromium.org>
2016-11-15 08:59:30 -0800

    Refactoring how WebVR eye bounds are handled
    BUG=389343
    
    Review-Url: https://codereview.chromium.org/2499793002
    Cr-Commit-Position: refs/heads/master@{#432198}

--- third_party/WebKit/Source/modules/vr/VRDisplay.cpp
+++ third_party/WebKit/Source/modules/vr/VRDisplay.cpp
@@ -283,8 +283,6 @@ ScriptPromise VRDisplay::requestPresent(ScriptState* scriptState,
     return promise;
   }
 
-  m_isPresenting = false;
-
   // A valid number of layers must be provided in order to present.
   if (layers.size() == 0 || layers.size() > m_capabilities->maxLayers()) {
     forceExitPresent();
@@ -350,6 +348,7 @@ ScriptPromise VRDisplay::requestPresent(ScriptState* scriptState,
   if (firstPresent) {
     bool secureContext = scriptState->getExecutionContext()->isSecureContext();
     if (!m_display) {
+      forceExitPresent();
       DOMException* exception = DOMException::create(
           InvalidStateError, "The service is no longer active.");
       resolver->reject(exception);
