58a86756aea32bbc5f1819d0484ec149381ef1f6
zmin <zmin@chromium.org>
2016-11-15 12:51:18 -0800

    Make sure that the browser will always restore settings page instead of sign out page after user sign out and sign in again with force sign in enabled.
    BUG=642059
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2493573002
    Cr-Commit-Position: refs/heads/master@{#432251}

--- chrome/browser/sessions/session_common_utils.h
+++ chrome/browser/sessions/session_common_utils.h
@@ -6,11 +6,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_SESSIONS_SESSION_COMMON_UTILS_H_
 #define CHROME_BROWSER_SESSIONS_SESSION_COMMON_UTILS_H_
 
+namespace sessions {
+struct SessionTab;
+}  // namespace sessions
+
 class GURL;
 
 // Returns whether sessions code should track a URL for restoring in the context
 // of //chrome. In particular, blacklists chrome://quit and chrome://restart to
 // avoid quit or restart loops.
 bool ShouldTrackURLForRestore(const GURL& url);
+
+// Returns the current navigation index from the tab. If current navigation's
+// url is the sign out page and the url of previous one is the setting page,
+// returns the index of previous one.
+int GetNavigationIndexToSelect(const sessions::SessionTab& tab);
 
 #endif  // CHROME_BROWSER_SESSIONS_SESSION_COMMON_UTILS_H_
