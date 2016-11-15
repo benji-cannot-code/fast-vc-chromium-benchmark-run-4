affeed7edec875c3717411f4ea6b1d690ebc4e3d
jbauman <jbauman@chromium.org>
2016-11-14 20:45:19 -0800

    Reenable idle suspension of media elements on Windows.
    Use GLImages to hold references from the DXVA picture buffer to the
    EGLStream or EGLSurface. This allows their contents to outlive the video
    decoder.
    
    BUG=663983
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2495753002
    Cr-Commit-Position: refs/heads/master@{#432101}

--- media/gpu/dxva_picture_buffer_win.h
+++ media/gpu/dxva_picture_buffer_win.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/angle/include/EGL/egl.h"
 #include "third_party/angle/include/EGL/eglext.h"
 #include "ui/gl/gl_fence.h"
+#include "ui/gl/gl_image.h"
 
 interface IMFSample;
 
@@ -54,6 +55,8 @@ class DXVAPictureBuffer {
   gfx::Size size() const { return picture_buffer_.size(); }
   void set_bound();
 
+  scoped_refptr<gl::GLImage> gl_image() { return gl_image_; }
+
   const gfx::ColorSpace& color_space() const { return color_space_; }
   void set_color_space(const gfx::ColorSpace& color_space) {
     color_space_ = color_space;
@@ -74,6 +77,7 @@ class DXVAPictureBuffer {
   State state_ = UNUSED;
   PictureBuffer picture_buffer_;
   gfx::ColorSpace color_space_;
+  scoped_refptr<gl::GLImage> gl_image_;
 
   DISALLOW_COPY_AND_ASSIGN(DXVAPictureBuffer);
 };
