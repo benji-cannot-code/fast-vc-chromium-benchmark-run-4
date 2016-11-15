2ca60d86a399bf0e22c8f4bac2f21ae0986186bf
sigbjornf <sigbjornf@opera.com>
2016-11-15 00:58:33 -0800

    XMLHttpRequest: implement "send() flag" tracking and updating per spec.
    The implementation has until now tracked/approximated the spec's
    "send() flag"[1] by checking if the XMLHttpRequest object had an active
    loader. That object does not have lifetime equal to what the spec
    requires for the "send() flag", nor is the loader set for sync XHR
    send()s.
    
    There's no good reason to hold out on tracking this flag per spec,
    so introduce it here.
    
    [1] - https://xhr.spec.whatwg.org/#send-flag
    
    R=yhirano
    BUG=649516
    
    Review-Url: https://codereview.chromium.org/2496933002
    Cr-Commit-Position: refs/heads/master@{#432148}

--- third_party/WebKit/Source/core/xmlhttprequest/XMLHttpRequest.cpp
+++ third_party/WebKit/Source/core/xmlhttprequest/XMLHttpRequest.cpp
@@ -247,7 +247,8 @@ XMLHttpRequest::XMLHttpRequest(
       m_uploadComplete(false),
       m_sameOriginRequest(true),
       m_downloadingToFile(false),
-      m_responseTextOverflow(false) {}
+      m_responseTextOverflow(false),
+      m_sendFlag(false) {}
 
 XMLHttpRequest::~XMLHttpRequest() {}
 
@@ -553,7 +554,7 @@ void XMLHttpRequest::dispatchReadyStateChangeEvent() {
 
 void XMLHttpRequest::setWithCredentials(bool value,
                                         ExceptionState& exceptionState) {
-  if (m_state > kOpened || m_loader) {
+  if (m_state > kOpened || m_sendFlag) {
     exceptionState.throwDOMException(
         InvalidStateError,
         "The value may only be set if the object's state is UNSENT or OPENED.");
@@ -669,6 +670,7 @@ void XMLHttpRequest::open(const AtomicString& method,
   m_async = async;
 
   DCHECK(!m_loader);
+  m_sendFlag = false;
 
   // Check previous state to avoid dispatching readyState event
   // when calling open several times in a row.
@@ -686,7 +688,7 @@ bool XMLHttpRequest::initSend(ExceptionState& exceptionState) {
     return false;
   }
 
-  if (m_state != kOpened || m_loader) {
+  if (m_state != kOpened || m_sendFlag) {
     exceptionState.throwDOMException(InvalidStateError,
                                      "The object's state must be OPENED.");
     return false;
@@ -930,6 +932,7 @@ void XMLHttpRequest::createRequest(PassRefPtr<EncodedFormData> httpBody,
   DCHECK(getExecutionContext());
   ExecutionContext& executionContext = *getExecutionContext();
 
+  m_sendFlag = true;
   // The presence of upload event listeners forces us to use preflighting
   // because POSTing to an URL that does not permit cross origin requests should
   // look exactly like POSTing to an URL that does not respond at all.
@@ -1033,6 +1036,7 @@ void XMLHttpRequest::createRequest(PassRefPtr<EncodedFormData> httpBody,
       request.setReportUploadProgress(true);
 
     DCHECK(!m_loader);
+    DCHECK(m_sendFlag);
     m_loader = ThreadableLoader::create(executionContext, this, options,
                                         resourceLoaderOptions);
     m_loader->start(request);
@@ -1050,14 +1054,6 @@ void XMLHttpRequest::createRequest(PassRefPtr<EncodedFormData> httpBody,
 
 void XMLHttpRequest::abort() {
   NETWORK_DVLOG(1) << this << " abort()";
-
-  // internalAbort() clears |m_loader|. Compute |sendFlag| now.
-  //
-  // |sendFlag| corresponds to "the send() flag" defined in the XHR spec.
-  //
-  // |sendFlag| is only set when we have an active, asynchronous loader.
-  // Don't use it as "the send() flag" when the XHR is in sync mode.
-  bool sendFlag = m_loader.get();
 
   // internalAbort() clears the response. Save the data needed for
   // dispatching ProgressEvents.
@@ -1077,7 +1073,7 @@ void XMLHttpRequest::abort() {
   // becomes true by that. We should implement more reliable treatment for
   // nested method invocations at some point.
   if (m_async) {
-    if ((m_state == kOpened && sendFlag) || m_state == kHeadersReceived ||
+    if ((m_state == kOpened && m_sendFlag) || m_state == kHeadersReceived ||
         m_state == kLoading) {
       DCHECK(!m_loader);
       handleRequestError(0, EventTypeNames::abort, receivedLength,
@@ -1226,6 +1222,7 @@ void XMLHttpRequest::handleRequestError(ExceptionCode exceptionCode,
   InspectorInstrumentation::didFailXHRLoading(getExecutionContext(), this, this,
                                               m_method, m_url);
 
+  m_sendFlag = false;
   if (!m_async) {
     DCHECK(exceptionCode);
     m_state = kDone;
@@ -1272,7 +1269,7 @@ void XMLHttpRequest::overrideMimeType(const AtomicString& mimeType,
 void XMLHttpRequest::setRequestHeader(const AtomicString& name,
                                       const AtomicString& value,
                                       ExceptionState& exceptionState) {
-  if (m_state != kOpened || m_loader) {
+  if (m_state != kOpened || m_sendFlag) {
     exceptionState.throwDOMException(InvalidStateError,
                                      "The object's state must be OPENED.");
     return;
@@ -1610,6 +1607,7 @@ void XMLHttpRequest::endLoading() {
     m_loader = nullptr;
   }
 
+  m_sendFlag = false;
   changeState(kDone);
 
   if (!getExecutionContext() || !getExecutionContext()->isDocument() ||
