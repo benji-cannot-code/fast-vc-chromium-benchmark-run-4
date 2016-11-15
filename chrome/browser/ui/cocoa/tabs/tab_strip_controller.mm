84f2d94cafc2efc0d308b726833433d5ed659f51
lgrey <lgrey@chromium.org>
2016-11-14 16:42:45 -0800

    [Mac] Fix autoresizing mask for full-screen windows controls
    BUG=664807
    
    Review-Url: https://codereview.chromium.org/2503533002
    Cr-Commit-Position: refs/heads/master@{#432025}

--- chrome/browser/ui/cocoa/tabs/tab_strip_controller.mm
+++ chrome/browser/ui/cocoa/tabs/tab_strip_controller.mm
@@ -2262,8 +2262,8 @@ NSRect FlipRectInView(NSView* view, NSRect rect) {
     customWindowControls_.reset(
         [[CustomWindowControlsView alloc] initWithFrame:frame]);
     [customWindowControls_
-        setAutoresizingMask:isRTL ? NSViewMaxXMargin | NSViewHeightSizable
-                                  : NSViewMinXMargin | NSViewHeightSizable];
+        setAutoresizingMask:isRTL ? NSViewMinXMargin | NSViewHeightSizable
+                                  : NSViewMaxXMargin | NSViewHeightSizable];
 
     // Add the traffic light buttons. The horizontal layout was determined by
     // manual inspection on Yosemite.
