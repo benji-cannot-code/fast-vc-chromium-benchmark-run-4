b63177482c59754f4b9a3d83ea73747cb83c3675
skym <skym@chromium.org>
2016-11-15 10:57:05 -0800

    [Sync] Return nullptr for OpenTabsUIDelegate when PROXY_TABS is disabled.
    Previously each UI component that relied on this would have its separate checks to see if PROXY_TABS was enabled or not, and only display the tabs data from the OpenTabsUIDelegate if it was. As more UI was created that relied on foreign tabs data, we saw the mistake of not making this check occur multiple times.
    
    This change will result in a null OpenTabsUIDelegate being provided when proxy tabs is disabled, and it will be impossible to make the mentioned mistake.
    
    Going forward, there are a handful of places with logic that checks and/or passes around if proxy tabs is enabled or not. As a follow up to this CL, we should go through and remove/simplify all that logic as foreign tabs data will no longer be accessible when proxy tabs becomes disabled.
    
    BUG=662601
    
    Review-Url: https://codereview.chromium.org/2504433002
    Cr-Commit-Position: refs/heads/master@{#432227}

--- components/browser_sync/profile_sync_service_unittest.cc
+++ components/browser_sync/profile_sync_service_unittest.cc
@@ -40,7 +40,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/l10n/l10n_util.h"
 
+using syncer::DataTypeController;
 using syncer::SyncBackendHostMock;
+using syncer::SyncMergeResult;
 using testing::Return;
 
 namespace browser_sync {
@@ -199,6 +201,10 @@ ACTION_P(ReturnNewMockHostCaptureClearServerData, captured_callback) {
   return new SyncBackendHostCaptureClearServerData(base::Bind(
       &OnClearServerDataCalled, base::Unretained(captured_callback)));
 }
+
+void DoNothing(DataTypeController::ConfigureResult ignored1,
+               const SyncMergeResult& ignored2,
+               const SyncMergeResult& ignored3) {}
 
 // A test harness that uses a real ProfileSyncService and in most cases a
 // MockSyncBackendHost.
@@ -951,6 +957,22 @@ TEST_F(ProfileSyncServiceTest, ValidPointersInDTCMap) {
   CreateService(ProfileSyncService::AUTO_START);
   service()->OnSessionRestoreComplete();
   service()->OnSyncCycleCompleted();
+}
+
+// The OpenTabsUIDelegate should only be accessable when PROXY_TABS is enabled.
+TEST_F(ProfileSyncServiceTest, GetOpenTabsUIDelegate) {
+  CreateService(ProfileSyncService::AUTO_START);
+  InitializeForNthSync();
+  EXPECT_EQ(nullptr, service()->GetOpenTabsUIDelegate());
+
+  auto controller =
+      base::MakeUnique<syncer::FakeDataTypeController>(syncer::PROXY_TABS);
+  // Progress the controller to RUNNING first, which is how the service
+  // determines whether a type is enabled.
+  controller->StartAssociating(base::Bind(&DoNothing));
+  controller->FinishStart(DataTypeController::OK_FIRST_RUN);
+  service()->RegisterDataTypeController(std::move(controller));
+  EXPECT_NE(nullptr, service()->GetOpenTabsUIDelegate());
 }
 
 }  // namespace
