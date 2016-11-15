b16b0af0a3a8a482d9a47a22fd280aebad4d08ad
tsergeant <tsergeant@chromium.org>
2016-11-15 14:58:52 -0800

    MD WebUI: Remove dependency on paper-input/iron-input from cr-toolbar
    The paper-input and iron-input elements are quite heavy, providing a lot
    of functionality to do with input validation. We make use of very few
    of these features in cr-toolbar.
    
    This CL removes paper-input and iron-input from cr-toolbar, replacing
    them with a plain <input> and some extra CSS/JS. This saves a small
    amount of page load time (15-20ms on my Z620) for
    History/Downloads/Settings, and removes 15K of code from the
    History/Downloads vulcanized bundles.
    
    BUG=589355
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2479733002
    Cr-Commit-Position: refs/heads/master@{#432278}

--- ui/webui/resources/cr_elements/cr_toolbar/cr_toolbar_search_field.js
+++ ui/webui/resources/cr_elements/cr_toolbar/cr_toolbar_search_field.js
@@ -29,17 +29,26 @@ Polymer({
     },
 
     /** @private */
-    hasSearchText_: Boolean,
+    hasSearchText_: {
+      type: Boolean,
+      reflectToAttribute: true
+    },
 
+    /** @private */
     isSpinnerShown_: {
       type: Boolean,
       computed: 'computeIsSpinnerShown_(spinnerActive, showingSearch)'
-    }
+    },
+
+    /** @private */
+    searchFocused_: {
+      type: Boolean,
+      value: false
+    },
   },
 
   listeners: {
     'tap': 'showSearch_',
-    'searchInput.bind-value-changed': 'onBindValueChanged_',
   },
 
   /** @return {!HTMLInputElement} */
@@ -47,9 +56,20 @@ Polymer({
     return this.$.searchInput;
   },
 
+  /**
+   * Sets the value of the search field. Overridden from CrSearchFieldBehavior.
+   * @param {string} value
+   * @param {boolean=} opt_noEvent Whether to prevent a 'search-changed' event
+   *     firing for this change.
+   */
+  setValue: function(value, opt_noEvent) {
+    CrSearchFieldBehavior.setValue.call(this, value, opt_noEvent);
+    this.onSearchInput_();
+  },
+
   /** @return {boolean} */
   isSearchFocused: function() {
-    return this.$.searchTerm.focused;
+    return this.searchFocused_;
   },
 
   /**
@@ -70,7 +90,13 @@ Polymer({
   },
 
   /** @private */
+  onInputFocus_: function() {
+    this.searchFocused_ = true;
+  },
+
+  /** @private */
   onInputBlur_: function() {
+    this.searchFocused_ = false;
     if (!this.hasSearchText_)
       this.showingSearch = false;
   },
@@ -81,8 +107,8 @@ Polymer({
    * after any change, whether the result of user input or JS modification.
    * @private
    */
-  onBindValueChanged_: function() {
-    var newValue = this.$.searchInput.bindValue;
+  onSearchInput_: function() {
+    var newValue = this.$.searchInput.value;
     this.hasSearchText_ = newValue != '';
     if (newValue != '')
       this.showingSearch = true;
