253c42f8e64b491d50e3d53ef45ccbfa25cd7455
jbauman <jbauman@chromium.org>
2016-11-14 19:39:12 -0800

    Add histogram for whether Pepper Graphics3D is using a share group.
    We want to see if this feature is being used by anyone. Contexts created
    by Flash are ignored, as they never use it and would swamp other
    possible users.
    
    Review-Url: https://codereview.chromium.org/2492413002
    Cr-Commit-Position: refs/heads/master@{#432076}

--- content/renderer/pepper/ppb_graphics_3d_impl.cc
+++ content/renderer/pepper/ppb_graphics_3d_impl.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/feature_list.h"
 #include "base/location.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/thread_task_runner_handle.h"
@@ -253,6 +254,8 @@ bool PPB_Graphics3D_Impl::InitRaw(
   attrib_helper.context_type = gpu::gles2::CONTEXT_TYPE_OPENGLES2;
 
   gpu::CommandBufferProxyImpl* share_buffer = NULL;
+  if (!plugin_instance->is_flash_plugin())
+    UMA_HISTOGRAM_BOOLEAN("Pepper.Graphics3DHasShareGroup", !!share_context);
   if (share_context) {
     PPB_Graphics3D_Impl* share_graphics =
         static_cast<PPB_Graphics3D_Impl*>(share_context);
