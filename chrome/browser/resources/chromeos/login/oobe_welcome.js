351369e410aba7e589c97e5ceb521d90505a32e1
alemate <alemate@chromium.org>
2016-11-15 22:21:17 -0800

    ChromeOS OOBE: Implement network selection screen "Back" button.
    BUG=642400
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2495153002
    Cr-Commit-Position: refs/heads/master@{#432385}

--- chrome/browser/resources/chromeos/login/oobe_welcome.js
+++ chrome/browser/resources/chromeos/login/oobe_welcome.js
@@ -124,21 +124,21 @@ Polymer({
     return [
       {
         customItemName: 'proxySettingsMenuName',
-        polymerIcon: 'oobe-welcome:add',
+        polymerIcon: 'oobe-welcome-20:add-proxy',
         customData: {
           onTap: function() { self.OpenProxySettingsDialog_(); },
         },
       },
       {
         customItemName: 'addWiFiNetworkMenuName',
-        polymerIcon: 'oobe-welcome:add',
+        polymerIcon: 'oobe-welcome-20:add-wifi',
         customData: {
           onTap: function() { self.OpenAddWiFiNetworkDialog_(); },
         },
       },
       {
         customItemName: 'addMobileNetworkMenuName',
-        polymerIcon: 'oobe-welcome:add',
+        polymerIcon: 'oobe-welcome-20:add-cellular',
         customData: {
           onTap: function() { self.OpenAddWiFiNetworkDialog_(); },
         },
@@ -275,6 +275,17 @@ Polymer({
   onNetworkListCustomItemSelected_: function(e) {
     var itemState = e.detail;
     itemState.customData.onTap();
+  },
+
+  /**
+   * Handle "<- Back" button on network selection screen.
+   *
+   * @private
+   */
+  onNetworkSelectionBackButtonPressed_: function() {
+    this.networkLastSelectedGuid_ = '';
+    this.hideAllScreens_();
+    this.welcomeScreenShown = true;
   },
 
   /**
