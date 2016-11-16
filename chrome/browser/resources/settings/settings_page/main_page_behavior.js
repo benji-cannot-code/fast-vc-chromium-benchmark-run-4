20148286fabb7da25d0c36374a0eced4ef166e78
tommycli <tommycli@chromium.org>
2016-11-16 09:59:17 -0800

    MD Settings: Fix Back navigation from section route to BASIC route.
    Currently, there is this problem:
    
    1. Open MD Settings.
    2. Open left menu and click on a section route.
    3. Observe that the window has scrolled to the section.
    4. Click the browser Back button.
    5. Observe that the window remains scrolled to the section.
    
    This CL fixes that bug and adds a test.
    
    BUG=664070
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2506793002
    Cr-Commit-Position: refs/heads/master@{#432549}

--- chrome/browser/resources/settings/settings_page/main_page_behavior.js
+++ chrome/browser/resources/settings/settings_page/main_page_behavior.js
@@ -31,6 +31,12 @@ var MainPageBehaviorImpl = {
     var oldRouteWasSection =
         !!oldRoute && !!oldRoute.parent && !!oldRoute.section &&
         oldRoute.parent.section != oldRoute.section;
+
+    if (oldRouteWasSection && newRoute == settings.Route.BASIC) {
+      this.scroller.scrollTop = 0;
+      return;
+    }
+
     var scrollToSection =
         !settings.lastRouteChangeWasPopstate() || oldRouteWasSection;
 
