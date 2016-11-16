9a6c4a2881f4d48e0c766b7b7bab8785a6c6d333
matteo.franchin <matteo.franchin@arm.com>
2016-11-16 13:11:04 -0800

    tcmalloc: Use futex syscall in SpinLockDelay() for ARM.
    SpinLockDelay() was avoiding using futex() when compiled for ARM.
    The alternative implementation was sleeping for 2 ms. This caused
    performance issues in Telemetry's SvgCubics benchmark. The score for
    this benchmark more than doubles when this patch is applied (scores were
    measured on veyron_jerry, veyron_minnie, daisy). Other benchmarks
    (blink_perf.bindings/post-message, blink_perf.canvas/draw-static-webgl-
    to-hw-accelerated-canvas-2d) benefit from this change to a lesser
    extent. This patch also removes a reference to futex1 in a comment in
    linux-syscall_support.h, as this is not a valid syscall name.
    
    Note that a similar fix was pushed in the upstream version of tcmalloc
    (issue-693, https://github.com/gperftools/gperftools/commit/7df7f14).
    
    BUG=
    
    Change-Id: I390ac51ed5e1b0ad021ac63eaf3bce81cdca8599
    Review-Url: https://codereview.chromium.org/2457473003
    Cr-Commit-Position: refs/heads/master@{#432628}

--- third_party/tcmalloc/chromium/src/base/linux_syscall_support.h
+++ third_party/tcmalloc/chromium/src/base/linux_syscall_support.h
@@ -83,7 +83,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *      sys_fcntl(
  *      sys_fstat(
  *      sys_futex(
- *      sys_futex1(
  *      sys_getcpu(
  *      sys_getdents(
  *      sys_getppid(
