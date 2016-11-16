263c7e7872da6022cea4651bb8481fb48a195f9a
maxbogue <maxbogue@chromium.org>
2016-11-15 22:36:24 -0800

    [Sync] Remove SyncClient dep from SyncBackendRegistrar.
    Instead, just pass in a factory function to create ModelSafeWorkers.
    
    This change also includes various cleanups of the test file:
    
    - No more need for FakeSyncClient.
    - Don't take SBR as a param to functions that don't need it.
    - Actually have a private section so it's clear what the tests use.
    - Move a couple functions to better places.
    
    BUG=663125
    
    Review-Url: https://codereview.chromium.org/2501743005
    Cr-Commit-Position: refs/heads/master@{#432399}

--- components/sync/driver/glue/sync_backend_registrar.h
+++ components/sync/driver/glue/sync_backend_registrar.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/callback.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/synchronization/lock.h"
@@ -24,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace syncer {
 
 class ChangeProcessor;
-class SyncClient;
 struct UserShare;
 
 // A class that keep track of the workers, change processors, and
@@ -32,8 +32,12 @@ struct UserShare;
 // events to the right processors.
 class SyncBackendRegistrar : public SyncManager::ChangeDelegate {
  public:
+  using ModelSafeWorkerFactory =
+      base::Callback<scoped_refptr<ModelSafeWorker>(ModelSafeGroup)>;
+
   // |name| is used for debugging. Must be created on the UI thread.
-  SyncBackendRegistrar(const std::string& name, SyncClient* sync_client);
+  SyncBackendRegistrar(const std::string& name,
+                       ModelSafeWorkerFactory worker_factory);
 
   // A SyncBackendRegistrar is owned by a SyncBackendHostImpl. It is destroyed
   // by SyncBackendHostImpl::Shutdown() which performs the following operations
@@ -116,8 +120,10 @@ class SyncBackendRegistrar : public SyncManager::ChangeDelegate {
   typedef std::map<ModelSafeGroup, scoped_refptr<ModelSafeWorker>> WorkerMap;
   typedef std::map<ModelType, ChangeProcessor*> ProcessorMap;
 
-  // Add a worker for |group| to the worker map if one can be created.
-  void MaybeAddWorker(ModelSafeGroup group);
+  // Add a worker for |group| to the worker map if one is successfully created
+  // by |worker_factory|.
+  void MaybeAddWorker(ModelSafeWorkerFactory worker_factory,
+                      ModelSafeGroup group);
 
   // Returns the change processor for the given model, or null if none
   // exists.  Must be called from |group|'s native thread.
@@ -139,9 +145,6 @@ class SyncBackendRegistrar : public SyncManager::ChangeDelegate {
 
   // Name used for debugging.
   const std::string name_;
-
-  // A pointer to the sync client.
-  SyncClient* const sync_client_;
 
   // Checker for the UI thread (where this object is constructed).
   base::ThreadChecker ui_thread_checker_;
