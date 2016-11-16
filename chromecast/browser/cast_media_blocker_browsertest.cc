6df0af949db3fc006b376beb1d1fb35b485a487a
alokp <alokp@chromium.org>
2016-11-16 07:57:10 -0800

    Reland: [chromecast] Make testnames consistent.
    Note that cast_shell_tests were not being run on the bots. This patch
    moves it into a top-level cast_shell_unittests target.
    
    BUG=665489
    
    Review-Url: https://codereview.chromium.org/2510473002
    Cr-Commit-Position: refs/heads/master@{#432501}

--- chromecast/browser/cast_media_blocker_browsertest.cc
+++ chromecast/browser/cast_media_blocker_browsertest.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/platform_thread.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chromecast/browser/cast_media_blocker.h"
-#include "chromecast/browser/test/chromecast_browser_test.h"
+#include "chromecast/browser/test/cast_browser_test.h"
 #include "chromecast/chromecast_features.h"
 #include "content/public/browser/media_session.h"
 #include "content/public/browser/web_contents.h"
@@ -20,16 +20,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromecast {
 namespace shell {
 
-class ChromecastShellMediaBlockingBrowserTest : public ChromecastBrowserTest {
+class CastMediaBlockerBrowserTest : public CastBrowserTest {
  public:
-  ChromecastShellMediaBlockingBrowserTest() {}
+  CastMediaBlockerBrowserTest() {}
 
  protected:
-  // ChromecastBrowserTest implementation.
+  // CastBrowserTest implementation.
   void TearDownOnMainThread() override {
     blocker_.reset();
 
-    ChromecastBrowserTest::TearDownOnMainThread();
+    CastBrowserTest::TearDownOnMainThread();
   }
 
   void PlayMedia(const std::string& tag, const std::string& media_file) {
@@ -84,11 +84,10 @@ class ChromecastShellMediaBlockingBrowserTest : public ChromecastBrowserTest {
   content::WebContents* web_contents_;
   std::unique_ptr<CastMediaBlocker> blocker_;
 
-  DISALLOW_COPY_AND_ASSIGN(ChromecastShellMediaBlockingBrowserTest);
+  DISALLOW_COPY_AND_ASSIGN(CastMediaBlockerBrowserTest);
 };
 
-IN_PROC_BROWSER_TEST_F(ChromecastShellMediaBlockingBrowserTest,
-                       Audio_BlockUnblock) {
+IN_PROC_BROWSER_TEST_F(CastMediaBlockerBrowserTest, Audio_BlockUnblock) {
   PlayMedia("audio", "bear-audio-10s-CBR-has-TOC.mp3");
 
   BlockAndTestPlayerState("audio", true);
@@ -96,8 +95,7 @@ IN_PROC_BROWSER_TEST_F(ChromecastShellMediaBlockingBrowserTest,
 }
 
 #if !BUILDFLAG(IS_CAST_AUDIO_ONLY)
-IN_PROC_BROWSER_TEST_F(ChromecastShellMediaBlockingBrowserTest,
-                       Video_BlockUnblock) {
+IN_PROC_BROWSER_TEST_F(CastMediaBlockerBrowserTest, Video_BlockUnblock) {
   PlayMedia("video", "tulip2.webm");
 
   BlockAndTestPlayerState("video", true);
--- chromecast/browser/test/cast_navigation_browsertest.cc
+++ chromecast/browser/test/cast_navigation_browsertest.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/strings/string_split.h"
 #include "base/strings/utf_string_conversions.h"
-#include "chromecast/browser/test/chromecast_browser_test.h"
+#include "chromecast/browser/test/cast_browser_test.h"
 #include "chromecast/chromecast_features.h"
 #include "content/public/test/browser_test_utils.h"
 #include "media/base/test_data_util.h"
@@ -21,9 +21,9 @@ const char kError[] = "ERROR";
 const char kFailed[] = "FAILED";
 }
 
-class ChromecastShellBrowserTest : public ChromecastBrowserTest {
+class CastNavigationBrowserTest : public CastBrowserTest {
  public:
-  ChromecastShellBrowserTest() {}
+  CastNavigationBrowserTest() {}
 
   void LoadAboutBlank() {
     content::WebContents* web_contents =
@@ -41,8 +41,7 @@ class ChromecastShellBrowserTest : public ChromecastBrowserTest {
   }
 
  private:
-  void PlayMedia(const std::string& tag,
-                 const std::string& media_file) {
+  void PlayMedia(const std::string& tag, const std::string& media_file) {
     base::StringPairs query_params;
     query_params.push_back(std::make_pair(tag, media_file));
     RunMediaTestPage("player.html", query_params, kEnded);
@@ -53,15 +52,13 @@ class ChromecastShellBrowserTest : public ChromecastBrowserTest {
                         const std::string& expected_title) {
     std::string query = media::GetURLQueryString(query_params);
     GURL gurl = content::GetFileUrlWithQuery(
-        media::GetTestDataFilePath(html_page),
-        query);
+        media::GetTestDataFilePath(html_page), query);
 
     std::string final_title = RunTest(gurl, expected_title);
     EXPECT_EQ(expected_title, final_title);
   }
 
-  std::string RunTest(const GURL& gurl,
-                      const std::string& expected_title) {
+  std::string RunTest(const GURL& gurl, const std::string& expected_title) {
     content::WebContents* web_contents = NavigateToURL(gurl);
     content::TitleWatcher title_watcher(web_contents,
                                         base::ASCIIToUTF16(expected_title));
@@ -72,20 +69,20 @@ class ChromecastShellBrowserTest : public ChromecastBrowserTest {
     return base::UTF16ToASCII(result);
   }
 
-  DISALLOW_COPY_AND_ASSIGN(ChromecastShellBrowserTest);
+  DISALLOW_COPY_AND_ASSIGN(CastNavigationBrowserTest);
 };
 
-IN_PROC_BROWSER_TEST_F(ChromecastShellBrowserTest, EmptyTest) {
+IN_PROC_BROWSER_TEST_F(CastNavigationBrowserTest, EmptyTest) {
   // Run an entire browser lifecycle to ensure nothing breaks.
   LoadAboutBlank();
 }
 
-IN_PROC_BROWSER_TEST_F(ChromecastShellBrowserTest, AudioPlaybackWavPcm) {
+IN_PROC_BROWSER_TEST_F(CastNavigationBrowserTest, AudioPlaybackWavPcm) {
   PlayAudio("bear_pcm.wav");
 }
 
 #if !BUILDFLAG(IS_CAST_AUDIO_ONLY)
-IN_PROC_BROWSER_TEST_F(ChromecastShellBrowserTest, VideoPlaybackMp4) {
+IN_PROC_BROWSER_TEST_F(CastNavigationBrowserTest, VideoPlaybackMp4) {
   PlayVideo("bear.mp4");
 }
 #endif
