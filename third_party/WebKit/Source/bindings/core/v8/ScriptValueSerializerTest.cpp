f9d486278f6117df5f8913ee14c19344b7ccfbc8
jbroman <jbroman@chromium.org>
2016-11-15 22:34:33 -0800

    Remove Blink.ScriptValueSerializer.*Count histograms.
    It was used to validate an assumption, but is unlikely to be useful to track
    going forward. It isn't implemented in the replacement implementation, so
    removing this prevents the unit test from failing when we ship it.
    
    BUG=148757
    
    Review-Url: https://codereview.chromium.org/2509433003
    Cr-Commit-Position: refs/heads/master@{#432397}

--- third_party/WebKit/Source/bindings/core/v8/ScriptValueSerializerTest.cpp
+++ third_party/WebKit/Source/bindings/core/v8/ScriptValueSerializerTest.cpp
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "bindings/core/v8/ScriptController.h"
 #include "core/layout/LayoutTestHelper.h"
-#include "platform/testing/HistogramTester.h"
 #include "wtf/Alignment.h"
 
 namespace blink {
@@ -23,24 +22,6 @@ class ScriptValueSerializerTest : public RenderingTest {
     settings.setScriptEnabled(true);
   }
 };
-
-TEST_F(ScriptValueSerializerTest, ValueCountHistograms) {
-  HistogramTester histogramTester;
-  document().frame()->script().executeScriptInMainWorld(
-      "postMessage({ foo: 42, bar: [new Blob]}, '*')");
-
-  // Three primitives, "foo", 42 and "bar".
-  histogramTester.expectUniqueSample(
-      "Blink.ScriptValueSerializer.PrimitiveCount", 3, 1);
-
-  // Two JS objects: the outer object and the array.
-  histogramTester.expectUniqueSample(
-      "Blink.ScriptValueSerializer.JSObjectCount", 2, 1);
-
-  // One DOM wrapper: the blob.
-  histogramTester.expectUniqueSample(
-      "Blink.ScriptValueSerializer.DOMWrapperCount", 1, 1);
-}
 
 TEST_F(ScriptValueSerializerTest, Uint64Decode) {
   // Even large 64-bit integers should decode properly.
