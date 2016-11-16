63f63d5f76c17e373f5081e2b47fca63473243fb
kapishnikov <kapishnikov@chromium.org>
2016-11-16 12:57:54 -0800

    cronet: Use ApplicationContext in CronetEngine builder
    Instead of storing Android context that is passed to the CronetEngine
    builder, it is better to retrieve the app context and store it instead.
    The passed context may have lifespan shorter than the builder and
    therefore become invalid. In addition, it may hold some resources that
    will not be garbage collected.
    
    Review-Url: https://codereview.chromium.org/2494863002
    Cr-Commit-Position: refs/heads/master@{#432624}

--- components/cronet/android/test/javatests/src/org/chromium/net/CronetUrlRequestContextTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/CronetUrlRequestContextTest.java
@@ -1431,7 +1431,7 @@ public class CronetUrlRequestContextTest extends CronetTestBase {
         try {
             // ensureInitialized() calls native code to check the version right after library load
             // and will error with the message below if library loading was skipped
-            CronetLibraryLoader.ensureInitialized(getContext(), builder);
+            CronetLibraryLoader.ensureInitialized(getContext().getApplicationContext(), builder);
             fail("Native library should not be loaded");
         } catch (UnsatisfiedLinkError e) {
             assertTrue(loader.wasCalled());
