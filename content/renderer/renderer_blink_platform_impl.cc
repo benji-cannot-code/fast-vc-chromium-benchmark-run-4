ee31ad84b00a72b3014c7b7dde06eecc9d9cc36c
thomasanderson <thomasanderson@google.com>
2016-11-16 10:15:44 -0800

    Disable scrollbar snapback on desktop Linux (Reland)
    This CL disables scrollbar snapback to be more consistent with typical
    desktop Linux UI.  Additionally, rename
    webscrollbarbehavior_impl_gtkoraura* to
    webscrollbarbehavior_impl_aura* since there's no longer a GTK backend
    for Linux Chromium.
    
    BUG=567968
    
    Committed: https://crrev.com/abe8ce9085986cb01cb9bc397886fb51c307032f
    Review-Url: https://codereview.chromium.org/2483113002
    Cr-Original-Commit-Position: refs/heads/master@{#430994}
    Cr-Commit-Position: refs/heads/master@{#432556}

--- content/renderer/renderer_blink_platform_impl.cc
+++ content/renderer/renderer_blink_platform_impl.cc
@@ -129,7 +129,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(USE_AURA)
-#include "content/renderer/webscrollbarbehavior_impl_gtkoraura.h"
+#include "content/renderer/webscrollbarbehavior_impl_aura.h"
 #elif !defined(OS_MACOSX)
 #include "third_party/WebKit/public/platform/WebScrollbarBehavior.h"
 #define WebScrollbarBehaviorImpl blink::WebScrollbarBehavior
