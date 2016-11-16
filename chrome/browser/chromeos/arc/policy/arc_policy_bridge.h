eee937c1d1004d14390eb03526732c64130c774d
poromov <poromov@chromium.org>
2016-11-16 14:37:43 -0800

    arc: Parse policy compliance report and set corresponding pref.
    ChromeOS side parses the report received from Android side and if there is
    no non-compliance, sets "kArcPolicyCompliant" pref. Otherwise, unset it.
    
    BUG=657405
    
    Review-Url: https://codereview.chromium.org/2491733003
    Cr-Commit-Position: refs/heads/master@{#432649}

--- chrome/browser/chromeos/arc/policy/arc_policy_bridge.h
+++ chrome/browser/chromeos/arc/policy/arc_policy_bridge.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/common/policy.mojom.h"
 #include "components/arc/instance_holder.h"
 #include "components/policy/core/common/policy_service.h"
+#include "components/pref_registry/pref_registry_syncable.h"
 #include "mojo/public/cpp/bindings/binding.h"
 
 namespace policy {
@@ -41,6 +42,8 @@ class ArcPolicyBridge : public ArcService,
   ArcPolicyBridge(ArcBridgeService* bridge_service,
                   policy::PolicyService* policy_service);
   ~ArcPolicyBridge() override;
+
+  static void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry);
 
   void OverrideIsManagedForTesting(bool is_managed);
 
