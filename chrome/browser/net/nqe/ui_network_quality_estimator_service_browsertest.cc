c17280adf2f451093811eaf42bfbcb97392f0865
tbansal <tbansal@chromium.org>
2016-11-16 13:58:56 -0800

    NQE: Notify observer as soon as it is added
    When an EffectiveConnectionTypeObserver or an
    RTTAndThroughputEstimatesObserver is added to NetworkQualityEstimator
    (NQE), it is notified of the current effective connection type or
    current RTT/throughput estimates.
    
    Doing this makes the API simpler (the caller does not have to
    call Get*() functions when registering as an observer). It also
    reduces the need of exposing both Get*() and Add*() APIs to different
    layers of Chromium.
    
    BUG=638304
    
    Review-Url: https://codereview.chromium.org/2491703003
    Cr-Commit-Position: refs/heads/master@{#432637}

--- chrome/browser/net/nqe/ui_network_quality_estimator_service_browsertest.cc
+++ chrome/browser/net/nqe/ui_network_quality_estimator_service_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/bind.h"
+#include "base/run_loop.h"
 #include "base/test/histogram_tester.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/io_thread.h"
@@ -32,7 +33,8 @@ namespace {
 class TestEffectiveConnectionTypeObserver
     : public net::NetworkQualityEstimator::EffectiveConnectionTypeObserver {
  public:
-  TestEffectiveConnectionTypeObserver() {}
+  TestEffectiveConnectionTypeObserver()
+      : effective_connection_type_(net::EFFECTIVE_CONNECTION_TYPE_UNKNOWN) {}
   ~TestEffectiveConnectionTypeObserver() override {}
 
   // net::NetworkQualityEstimator::EffectiveConnectionTypeObserver
@@ -187,6 +189,26 @@ IN_PROC_BROWSER_TEST_F(UINetworkQualityEstimatorServiceBrowserTest,
             nqe_service->GetEffectiveConnectionType());
   EXPECT_EQ(net::EFFECTIVE_CONNECTION_TYPE_SLOW_2G,
             nqe_observer.effective_connection_type());
+
+  // Observer should be notified on addition.
+  TestEffectiveConnectionTypeObserver nqe_observer_2;
+  nqe_service->AddEffectiveConnectionTypeObserver(&nqe_observer_2);
+  EXPECT_EQ(net::EFFECTIVE_CONNECTION_TYPE_UNKNOWN,
+            nqe_observer_2.effective_connection_type());
+  base::RunLoop().RunUntilIdle();
+  EXPECT_EQ(net::EFFECTIVE_CONNECTION_TYPE_OFFLINE,
+            nqe_observer_2.effective_connection_type());
+
+  // |nqe_observer_3| should be not notified since it unregisters before the
+  // message loop is run.
+  TestEffectiveConnectionTypeObserver nqe_observer_3;
+  nqe_service->AddEffectiveConnectionTypeObserver(&nqe_observer_3);
+  EXPECT_EQ(net::EFFECTIVE_CONNECTION_TYPE_UNKNOWN,
+            nqe_observer_3.effective_connection_type());
+  nqe_service->RemoveEffectiveConnectionTypeObserver(&nqe_observer_3);
+  base::RunLoop().RunUntilIdle();
+  EXPECT_EQ(net::EFFECTIVE_CONNECTION_TYPE_UNKNOWN,
+            nqe_observer_3.effective_connection_type());
 }
 
 // Verify that prefs are not writen when writing of the prefs is not enabled
