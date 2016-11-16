586fca02ca1e6946701f3b055f42af6bc2490a6d
raphael.kubo.da.costa <raphael.kubo.da.costa@intel.com>
2016-11-16 03:29:56 -0800

    TemplateURLService: Properly enclose calls within an if block
    https://codereview.chromium.org/1543043007 introduced two calls which,
    from the looks of it, were supposed to be part of an if check. The if
    block, however, did not use braces so only the first call was
    conditional ("goto fail").
    
    R=pkasting@chromium.org,stevet@chromium.org
    
    Review-Url: https://codereview.chromium.org/2499253003
    Cr-Commit-Position: refs/heads/master@{#432460}

--- components/search_engines/template_url_service.cc
+++ components/search_engines/template_url_service.cc
@@ -1829,10 +1829,11 @@ void TemplateURLService::GoogleBaseURLChanged() {
   if (!loaded_) {
     if (initial_default_search_provider_.get() &&
         initial_default_search_provider_->HasGoogleBaseURLs(
-            search_terms_data()))
+            search_terms_data())) {
       initial_default_search_provider_->InvalidateCachedValues();
       initial_default_search_provider_->ResetKeywordIfNecessary(
           search_terms_data(), false);
+    }
     return;
   }
 
