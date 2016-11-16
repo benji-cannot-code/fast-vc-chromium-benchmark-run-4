6e0bffbaaf1036cecd2c62c785e2d4a8ae953219
yusukes <yusukes@chromium.org>
2016-11-15 16:26:02 -0800

    arc: enable use_new_wrapper_types for notifications.mojom
    BUG=662510
    BUG=624136
    TEST=try
    
    Review-Url: https://codereview.chromium.org/2496423004
    Cr-Commit-Position: refs/heads/master@{#432313}

--- components/arc/test/fake_notifications_instance.h
+++ components/arc/test/fake_notifications_instance.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_ARC_TEST_FAKE_NOTIFICATIONS_INSTANCE_H_
 #define COMPONENTS_ARC_TEST_FAKE_NOTIFICATIONS_INSTANCE_H_
 
+#include <string>
 #include <utility>
 #include <vector>
 
@@ -21,16 +22,16 @@ class FakeNotificationsInstance : public mojom::NotificationsInstance {
   void Init(mojom::NotificationsHostPtr host_ptr) override;
 
   void SendNotificationEventToAndroid(
-      const mojo::String& key,
+      const std::string& key,
       mojom::ArcNotificationEvent event) override;
-  void CreateNotificationWindow(const mojo::String& key) override;
-  void CloseNotificationWindow(const mojo::String& key) override;
+  void CreateNotificationWindow(const std::string& key) override;
+  void CloseNotificationWindow(const std::string& key) override;
 
-  const std::vector<std::pair<mojo::String, mojom::ArcNotificationEvent>>&
+  const std::vector<std::pair<std::string, mojom::ArcNotificationEvent>>&
   events() const;
 
  private:
-  std::vector<std::pair<mojo::String, mojom::ArcNotificationEvent>> events_;
+  std::vector<std::pair<std::string, mojom::ArcNotificationEvent>> events_;
 
   DISALLOW_COPY_AND_ASSIGN(FakeNotificationsInstance);
 };
--- ui/arc/notification/arc_custom_notification_item.cc
+++ ui/arc/notification/arc_custom_notification_item.cc
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/arc/notification/arc_custom_notification_item.h"
 
+#include <memory>
+#include <string>
+#include <utility>
+
 #include "base/memory/ptr_util.h"
 #include "base/strings/string16.h"
 #include "base/strings/utf_string_conversions.h"
@@ -69,19 +73,17 @@ void ArcCustomNotificationItem::UpdateWithArcNotificationData(
   rich_data.priority = ConvertAndroidPriority(data->priority);
   if (data->small_icon)
     rich_data.small_image = gfx::Image::CreateFrom1xBitmap(*data->small_icon);
-  if (!data->accessible_name.is_null())
-    rich_data.accessible_name = base::UTF8ToUTF16(data->accessible_name.get());
+  if (data->accessible_name.has_value())
+    rich_data.accessible_name = base::UTF8ToUTF16(*data->accessible_name);
 
   message_center::NotifierId notifier_id(
       message_center::NotifierId::SYSTEM_COMPONENT, kNotifierId);
   notifier_id.profile_id = profile_id().GetUserEmail();
 
-  DCHECK(!data->title.is_null());
-  DCHECK(!data->message.is_null());
   SetNotification(base::MakeUnique<message_center::Notification>(
       message_center::NOTIFICATION_TYPE_CUSTOM, notification_id(),
-      base::UTF8ToUTF16(data->title.get()),
-      base::UTF8ToUTF16(data->message.get()), gfx::Image(),
+      base::UTF8ToUTF16(data->title), base::UTF8ToUTF16(data->message),
+      gfx::Image(),
       base::UTF8ToUTF16("arc"),  // display source
       GURL(),                    // empty origin url, for system component
       notifier_id, rich_data, new ArcNotificationDelegate(this)));
