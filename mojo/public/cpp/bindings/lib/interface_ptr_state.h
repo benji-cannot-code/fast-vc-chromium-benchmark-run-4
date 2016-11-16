b19c2a73d8ba7b279e89cc2a71e5d64972bb8494
jcivelli <jcivelli@chromium.org>
2016-11-15 22:23:28 -0800

    Creating a thread safe interface pointer that is tied to an actual InterfacePtr and forwards message from interface calls to the InterfacePtr thread and forwards back response messages to the calling thread.
    BUG=664628
    TEST=InterfacePtrTest.TestThreadSafeInterfacePointer
    
    Review-Url: https://codereview.chromium.org/2498743002
    Cr-Commit-Position: refs/heads/master@{#432387}

--- mojo/public/cpp/bindings/lib/interface_ptr_state.h
+++ mojo/public/cpp/bindings/lib/interface_ptr_state.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/ref_counted.h"
+#include "base/memory/weak_ptr.h"
 #include "base/single_thread_task_runner.h"
 #include "mojo/public/cpp/bindings/associated_group.h"
 #include "mojo/public/cpp/bindings/connection_error_callback.h"
@@ -210,7 +211,7 @@ class InterfacePtrState<Interface, false> {
 template <typename Interface>
 class InterfacePtrState<Interface, true> {
  public:
-  InterfacePtrState() : version_(0u) {}
+  InterfacePtrState() : version_(0u), weak_ptr_factory_(this) {}
 
   ~InterfacePtrState() {
     endpoint_client_.reset();
@@ -332,6 +333,17 @@ class InterfacePtrState<Interface, true> {
     router_->EnableTestingMode();
   }
 
+  base::Callback<void(Message)> GetThreadSafePtrAcceptCallback() {
+    return base::Bind(&InterfacePtrState::ForwardMessage,
+                      weak_ptr_factory_.GetWeakPtr());
+  }
+
+  base::Callback<void(Message, std::unique_ptr<MessageReceiver>)>
+  GetThreadSafePtrAcceptWithResponderCallback() {
+    return base::Bind(&InterfacePtrState::ForwardMessageWithResponder,
+                      weak_ptr_factory_.GetWeakPtr());
+  }
+
  private:
   using Proxy = typename Interface::Proxy_;
 
@@ -362,16 +374,25 @@ class InterfacePtrState<Interface, true> {
         // will not be used.
         0u));
     proxy_.reset(new Proxy(endpoint_client_.get()));
-    if (Interface::PassesAssociatedKinds_) {
-      proxy_->serialization_context()->group_controller =
-          endpoint_client_->group_controller();
-    }
+    if (Interface::PassesAssociatedKinds_)
+      proxy_->set_group_controller(endpoint_client_->group_controller());
   }
 
   void OnQueryVersion(const base::Callback<void(uint32_t)>& callback,
                       uint32_t version) {
     version_ = version;
     callback.Run(version);
+  }
+
+  void ForwardMessage(Message message) {
+    ConfigureProxyIfNecessary();
+    endpoint_client_->Accept(&message);
+  }
+
+  void ForwardMessageWithResponder(Message message,
+                                   std::unique_ptr<MessageReceiver> responder) {
+    ConfigureProxyIfNecessary();
+    endpoint_client_->AcceptWithResponder(&message, responder.release());
   }
 
   scoped_refptr<MultiplexRouter> router_;
@@ -386,6 +407,8 @@ class InterfacePtrState<Interface, true> {
   scoped_refptr<base::SingleThreadTaskRunner> runner_;
 
   uint32_t version_;
+
+  base::WeakPtrFactory<InterfacePtrState> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(InterfacePtrState);
 };
