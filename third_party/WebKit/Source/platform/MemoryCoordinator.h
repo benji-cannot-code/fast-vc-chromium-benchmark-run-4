8fd27a859b1496f51b3827ddecd435d82bdfcbdd
avayvod <avayvod@chromium.org>
2016-11-16 00:16:50 -0800

    Move ProcessHeap::isLowEndDevice to MemoryCoordinator
    Per discussion at https://codereview.chromium.org/2475293003
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2503433003
    Cr-Commit-Position: refs/heads/master@{#432427}

--- third_party/WebKit/Source/platform/MemoryCoordinator.h
+++ third_party/WebKit/Source/platform/MemoryCoordinator.h
@@ -43,6 +43,8 @@ class PLATFORM_EXPORT MemoryCoordinator final
 
   void onMemoryStateChange(MemoryState);
 
+  bool isLowEndDevice() const { return m_isLowEndDevice; }
+
   DECLARE_TRACE();
 
  private:
@@ -51,6 +53,8 @@ class PLATFORM_EXPORT MemoryCoordinator final
   void clearMemory();
 
   HeapHashSet<WeakMember<MemoryCoordinatorClient>> m_clients;
+
+  bool m_isLowEndDevice;
 };
 
 }  // namespace blink
