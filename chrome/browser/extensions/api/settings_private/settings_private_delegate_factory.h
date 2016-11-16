e70811131c8d3ac65d6ddbe1f9a2c68af9a9edcb
hcarmona <hcarmona@chromium.org>
2016-11-16 09:43:52 -0800

    Fix Guest Mode errors in MD Settings by using the incognito context.
    BUG=654588
    
    Review-Url: https://codereview.chromium.org/2499093002
    Cr-Commit-Position: refs/heads/master@{#432544}

--- chrome/browser/extensions/api/settings_private/settings_private_delegate_factory.h
+++ chrome/browser/extensions/api/settings_private/settings_private_delegate_factory.h
@@ -35,6 +35,8 @@ class SettingsPrivateDelegateFactory
   // BrowserContextKeyedBaseFactory implementation.
   KeyedService* BuildServiceInstanceFor(
       content::BrowserContext* profile) const override;
+  content::BrowserContext* GetBrowserContextToUse(
+    content::BrowserContext* context) const override;
 
   DISALLOW_COPY_AND_ASSIGN(SettingsPrivateDelegateFactory);
 };
