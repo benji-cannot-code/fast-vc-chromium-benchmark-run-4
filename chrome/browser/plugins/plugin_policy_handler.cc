5afc9f13ffb50bea088b6a20de82580e9f6e23c0
pastarmovj <pastarmovj@chromium.org>
2016-11-16 06:18:13 -0800

    Migrate enabled state of plugins into specialized Flash and PDF prefs.
    For PDF and Flash the enabled state is defined by the
    ManagedDefaultPluginsSetting and PluginsAlwaysOpenPdfExternally prefs.
    All other plugins should never be disabled in the first place.
    
    BUG=662002
    TEST=manual
    
    Review-Url: https://codereview.chromium.org/2474713003
    Cr-Commit-Position: refs/heads/master@{#432477}

--- chrome/browser/plugins/plugin_policy_handler.cc
+++ chrome/browser/plugins/plugin_policy_handler.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/pattern.h"
 #include "base/strings/string_util.h"
 #include "base/values.h"
+#include "chrome/browser/plugins/plugin_metadata.h"
 #include "chrome/browser/plugins/plugin_prefs.h"
 #include "chrome/common/chrome_content_client.h"
 #include "chrome/common/pref_names.h"
@@ -20,8 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_constants.h"
 
 namespace {
-
-const char kAdobeFlashPlayerName[] = "Adobe Flash Player";
 
 // Retrieves a list typed policy or nullptr if not present or not a list.
 const base::ListValue* GetListPolicy(const policy::PolicyMap& policies,
@@ -61,7 +60,8 @@ void PluginPolicyHandler::ProcessPolicy(const policy::PolicyMap& policies,
           prefs::kPluginsAlwaysOpenPdfExternally,
           base::MakeUnique<base::FundamentalValue>(disable_pdf_plugin));
     }
-    if ((base::MatchPattern(kAdobeFlashPlayerName, plugin) ||
+    if ((base::MatchPattern(
+             PluginMetadata::kAdobeFlashPlayerGroupName, plugin) ||
          base::MatchPattern(content::kFlashPluginName, plugin)) &&
         !policies.GetValue(policy::key::kDefaultPluginsSetting)) {
       prefs->SetValue(
@@ -111,7 +111,8 @@ void PluginPolicyHandler::ApplyPolicySettings(const policy::PolicyMap& policies,
         !policies.GetValue(policy::key::kAlwaysOpenPdfExternally)) {
       prefs->RemoveValue(prefs::kPluginsAlwaysOpenPdfExternally);
     }
-    if ((base::MatchPattern(kAdobeFlashPlayerName, plugin) ||
+    if ((base::MatchPattern(
+            PluginMetadata::kAdobeFlashPlayerGroupName, plugin) ||
          base::MatchPattern(content::kFlashPluginName, plugin)) &&
         !policies.GetValue(policy::key::kDefaultPluginsSetting)) {
       prefs->RemoveValue(prefs::kManagedDefaultPluginsSetting);
--- chrome/browser/plugins/plugin_prefs.cc
+++ chrome/browser/plugins/plugin_prefs.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/location.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/path_service.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/pattern.h"
@@ -24,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_notification_types.h"
+#include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/plugins/plugin_installer.h"
 #include "chrome/browser/plugins/plugin_metadata.h"
 #include "chrome/browser/plugins/plugin_prefs_factory.h"
@@ -33,11 +35,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
+#include "components/content_settings/core/browser/host_content_settings_map.h"
+#include "components/content_settings/core/common/content_settings.h"
+#include "components/content_settings/core/common/pref_names.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/prefs/scoped_user_pref_update.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/plugin_service.h"
+#include "content/public/common/content_constants.h"
 #include "content/public/common/webplugininfo.h"
 
 #if !defined(DISABLE_NACL)
@@ -64,6 +70,12 @@ bool IsComponentUpdatedPepperFlash(const base::FilePath& plugin) {
 
 bool IsPDFViewerPlugin(const base::string16& plugin_name) {
   return plugin_name == base::ASCIIToUTF16(ChromeContentClient::kPDFPluginName);
+}
+
+bool IsAdobeFlashPlayerPlugin(const base::string16& plugin_name) {
+  return (plugin_name == base::ASCIIToUTF16(content::kFlashPluginName) ||
+          plugin_name == base::ASCIIToUTF16(
+              PluginMetadata::kAdobeFlashPlayerGroupName));
 }
 
 }  // namespace
@@ -340,6 +352,7 @@ void PluginPrefs::ListValueToStringSet(const base::ListValue* src,
 void PluginPrefs::SetPrefs(PrefService* prefs) {
   prefs_ = prefs;
   bool update_internal_dir = false;
+  bool plugins_migrated = false;
   base::FilePath last_internal_dir =
       prefs_->GetFilePath(prefs::kPluginsLastInternalDirectory);
   base::FilePath cur_internal_dir;
@@ -361,10 +374,27 @@ void PluginPrefs::SetPrefs(PrefService* prefs) {
           continue;  // Oops, don't know what to do with this item.
         }
 
-        base::string16 group_name;
         bool enabled;
         if (!plugin->GetBoolean("enabled", &enabled))
           enabled = true;
+
+        // Migrate disabled plugins and re-enable them all internally.
+        // TODO(http://crbug.com/662006): Remove migration eventually.
+        if (!enabled) {
+          base::string16 name;
+          plugin->GetString("name", &name);
+          if (IsPDFViewerPlugin(name))
+            prefs->SetBoolean(prefs::kPluginsAlwaysOpenPdfExternally, true);
+          if (IsAdobeFlashPlayerPlugin(name)) {
+            HostContentSettingsMapFactory::GetForProfile(profile_)
+                ->SetDefaultContentSetting(CONTENT_SETTINGS_TYPE_PLUGINS,
+                                           CONTENT_SETTING_BLOCK);
+          }
+          // TODO(http://crbug.com/662002): Remove the enabled flag completely.
+          enabled = true;
+          plugin->SetBoolean("enabled", true);
+          plugins_migrated = true;
+        }
 
         base::FilePath::StringType path;
         // The plugin list constains all the plugin files in addition to the
@@ -413,9 +443,6 @@ void PluginPrefs::SetPrefs(PrefService* prefs) {
           }
 
           plugin_state_.Set(plugin_path, enabled);
-        } else if (!enabled && plugin->GetString("name", &group_name)) {
-          // Otherwise this is a list of groups.
-          plugin_group_state_[group_name] = false;
         }
       }
     } else {
@@ -430,8 +457,11 @@ void PluginPrefs::SetPrefs(PrefService* prefs) {
     }
   }  // Scoped update of prefs::kPluginsPluginsList.
 
+  UMA_HISTOGRAM_BOOLEAN("Plugin.EnabledStatusMigrationDone", plugins_migrated);
+
   // Build the set of policy enabled/disabled plugin patterns once and cache it.
   // Don't do this in the constructor, there's no profile available there.
+  // TODO(http://crbug.com/662002): : Remove the prefs completely.
   ListValueToStringSet(prefs_->GetList(prefs::kPluginsDisabledPlugins),
                        &policy_disabled_plugin_patterns_);
   ListValueToStringSet(
