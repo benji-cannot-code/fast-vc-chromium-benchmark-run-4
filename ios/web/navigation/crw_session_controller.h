e01a450fc02f57bc0cd2c506170fdcbc53e8f869
eugenebut <eugenebut@chromium.org>
2016-11-16 11:37:04 -0800

    [ios] Removed goBack/goForward from CRWSessionController.
    These methods are unused and CRWSessionController should be folded into
    NavigationManager.
    
    BUG=661316
    
    Review-Url: https://codereview.chromium.org/2498483002
    Cr-Commit-Position: refs/heads/master@{#432593}

--- ios/web/navigation/crw_session_controller.h
+++ ios/web/navigation/crw_session_controller.h
@@ -123,10 +123,6 @@ struct SSLStatus;
 - (BOOL)canGoBack;
 - (BOOL)canGoForward;
 - (BOOL)canGoDelta:(int)delta;
-// Adjusts the current entry to reflect the navigation in the corresponding
-// direction in history.
-- (void)goBack;
-- (void)goForward;
 // Calls goBack or goForward the appropriate number of times to adjust
 // currentNavigationIndex_ by delta.
 - (void)goDelta:(int)delta;
