66ef8dfc653bc755f9acc075e6e9c313f3a464b4
lgcheng <lgcheng@google.com>
2016-11-15 16:38:23 -0800

    Fix memory leak for extension uninstall dialog
    test=Add Test coverage. Pass browser test.
    
    BUG=661373
    
    Review-Url: https://codereview.chromium.org/2474783002
    Cr-Commit-Position: refs/heads/master@{#432320}

--- components/constrained_window/constrained_window_views.cc
+++ components/constrained_window/constrained_window_views.cc
@@ -61,7 +61,7 @@ class WidgetModalDialogHostObserverViews
   }
 
   // WidgetObserver overrides
-  void OnWidgetClosing(views::Widget* widget) override { delete this; }
+  void OnWidgetDestroying(views::Widget* widget) override { delete this; }
 
   // WebContentsModalDialogHostObserver overrides
   void OnPositionRequiresUpdate() override {
