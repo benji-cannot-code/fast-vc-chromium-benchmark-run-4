cbae8a64358aeeb089c440675a854891e28370c5
martijn <martijn@martijnc.be>
2016-11-16 09:25:04 -0800

    Correct net::EmbeddedTestServer usage in content/browser tests.
    Adding request handlers to the EmbeddedTestServer after it has started
    isn't thread safe.
    
    This CL updates some tests in content/browser to make net::EmbeddedTestServer
    usage thread safe. Either by adding all handlers before calling
    EmbeddedTestServer::Start() or by splitting the call in two when the handler
    needs the server to be initialized to build URLs.
    
    BUG=546060
    
    Review-Url: https://codereview.chromium.org/2499133002
    Cr-Commit-Position: refs/heads/master@{#432535}

--- content/browser/blob_storage/blob_url_browsertest.cc
+++ content/browser/blob_storage/blob_url_browsertest.cc
@@ -27,8 +27,8 @@ class BlobUrlBrowserTest : public ContentBrowserTest {
 
   void SetUpOnMainThread() override {
     host_resolver()->AddRule("*", "127.0.0.1");
-    ASSERT_TRUE(embedded_test_server()->Start());
     SetupCrossSiteRedirector(embedded_test_server());
+    ASSERT_TRUE(embedded_test_server()->Start());
   }
 
  private:
