c574cc13849568797cb044df73b6601bef2de0d6
mvanouwerkerk <mvanouwerkerk@chromium.org>
2016-11-16 08:57:32 -0800

    Add NewTabPageRecyclerViewTest.
    * Adds test for clicking a suggestion, dismissing all, and dismissing with context menu.
    * Bumps coverage of org.chromium.chrome.browser.ntp* from 53% to 57%.
    
    BUG=659631
    
    Review-Url: https://codereview.chromium.org/2504113003
    Cr-Commit-Position: refs/heads/master@{#432523}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPage.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPage.java
@@ -113,6 +113,7 @@ public class NewTabPage
             "https://www.googleapis.com/auth/chrome-content-suggestions";
 
     private static MostVisitedSites sMostVisitedSitesForTests;
+    private static SuggestionsSource sSuggestionsSourceForTests;
 
     private final Tab mTab;
     private final TabModelSelector mTabModelSelector;
@@ -218,8 +219,13 @@ public class NewTabPage
     }
 
     @VisibleForTesting
-    static void setMostVisitedSitesForTests(MostVisitedSites mostVisitedSitesForTests) {
+    public static void setMostVisitedSitesForTests(MostVisitedSites mostVisitedSitesForTests) {
         sMostVisitedSitesForTests = mostVisitedSitesForTests;
+    }
+
+    @VisibleForTesting
+    public static void setSuggestionsSourceForTests(SuggestionsSource suggestionsSource) {
+        sSuggestionsSourceForTests = suggestionsSource;
     }
 
     private final NewTabPageManager mNewTabPageManager = new NewTabPageManager() {
@@ -654,6 +660,7 @@ public class NewTabPage
 
         @Override
         public SuggestionsSource getSuggestionsSource() {
+            if (sSuggestionsSourceForTests != null) return sSuggestionsSourceForTests;
             return mSnippetsBridge;
         }
 
@@ -778,7 +785,7 @@ public class NewTabPage
 
     /** @return The view container for the new tab page. */
     @VisibleForTesting
-    NewTabPageView getNewTabPageView() {
+    public NewTabPageView getNewTabPageView() {
         return mNewTabPageView;
     }
 
