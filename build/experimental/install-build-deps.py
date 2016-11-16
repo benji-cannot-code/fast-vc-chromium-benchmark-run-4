f72064581cdadc579957d9afcfb59ec0ad97fbcf
jack <jack@nottheoilrig.com>
2016-11-15 16:14:51 -0800

    Replace kochi-*.ttf with IPA Fonts.
    kochi-gothic.ttf and kochi-mincho.ttf were deprecated [1]
    and dropped from Debian and Ubuntu [2].
    They were replaced with ipag.ttf and ipam.ttf (fonts-ipafont package).
    
    [1] https://packages.debian.org/wheezy/ttf-kochi-gothic
    [2] https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=726382#33
    
    BUG=665515
    
    Review-Url: https://codereview.chromium.org/2500063003
    Cr-Commit-Position: refs/heads/master@{#432306}

--- build/experimental/install-build-deps.py
+++ build/experimental/install-build-deps.py
@@ -40,6 +40,7 @@ _packages_dev = (
   'elfutils',
   'fakeroot',
   'flex',
+  'fonts-ipafont',
   'fonts-thai-tlwg',
   'g++',
   'git-core',
@@ -100,8 +101,6 @@ _packages_dev = (
   'subversion',
   'ttf-dejavu-core',
   'ttf-indic-fonts',
-  'ttf-kochi-gothic',
-  'ttf-kochi-mincho',
   'wdiff',
   'zip',
 )
