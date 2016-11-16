d27aea71e81a5776e3dd5a99b25fa83383cb32f0
mlamouri <mlamouri@chromium.org>
2016-11-15 22:30:35 -0800

    Add tests for PresentationConnectionAvailable constructor.
    This is introducing a mock mojo presentation service in LayoutTests
    which could be used for future tests.
    
    BUG=648922
    R=mfoltz@chromium.org
    
    Review-Url: https://codereview.chromium.org/2491693003
    Cr-Commit-Position: refs/heads/master@{#432394}

---
+++ third_party/WebKit/LayoutTests/presentation/resources/presentation-service-mock.js
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/*
+ * Mock implementation of mojo PresentationService.
+ */
+
+"use strict";
+
+let presentationServiceMock = loadMojoModules(
+    'presentationServiceMock',
+    [
+      'third_party/WebKit/public/platform/modules/presentation/presentation.mojom',
+      'mojo/public/js/router',
+    ]).then(mojo => {
+      let [ presentationService, router ] = mojo.modules;
+
+      class PresentationServiceMock {
+        constructor(interfaceProvider) {
+          interfaceProvider.addInterfaceOverrideForTesting(
+              presentationService.PresentationService.name,
+              handle => this.connectPresentationService_(handle));
+          this.interfaceProvider_ = interfaceProvider;
+          this.pendingResponse_ = null;
+        }
+
+        connectPresentationService_(handle) {
+          this.presentationServiceStub_ = new presentationService.PresentationService.stubClass(this);
+          this.presentationServiceRouter_ = new router.Router(handle);
+          this.presentationServiceRouter_.setIncomingReceiver(this.presentationServiceStub_);
+        }
+
+        startSession(urls) {
+          return Promise.resolve({
+              sessionInfo: { url: urls[0], id: 'fakesession' },
+              error: null,
+          });
+        }
+      }
+
+      return new PresentationServiceMock(mojo.frameInterfaces);
+    });
