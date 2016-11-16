2bc536c0c83366c15cd3efddb1a9c42210d34231
yosin <yosin@chromium.org>
2016-11-15 16:11:10 -0800

    Use DEFINE_TRACE in SelectionEdtior instead of DEFINE_VIRTUAL_TRACE
    This patch changes |SelectionEdtior| class to use |DEFINE_TRACE()| instead of
    |DEFINE_VIRTUAL_TRACE()| since |SelectionEdtior| is final.
    
    |DEFINE_VIRTUAL_TRACE()| was introduced by [1] mistakenly.
    
    [1] https://chromium.googlesource.com/chromium/src/+/812ed4cbe27194ca3978c8dd4bab8149a567ba59
    
    BUG=n/a
    TEST=n/a; no behavior changes.
    
    Review-Url: https://codereview.chromium.org/2500353002
    Cr-Commit-Position: refs/heads/master@{#432303}

--- third_party/WebKit/Source/core/editing/SelectionEditor.h
+++ third_party/WebKit/Source/core/editing/SelectionEditor.h
@@ -83,7 +83,7 @@ class SelectionEditor final
   // layout if needed.
   void updateIfNeeded();
 
-  DECLARE_VIRTUAL_TRACE();
+  DECLARE_TRACE();
 
  private:
   explicit SelectionEditor(FrameSelection&);
