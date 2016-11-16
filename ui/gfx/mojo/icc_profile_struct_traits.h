46c8cbdc206d5c1a715eb11207b519d0031cc3f1
ccameron <ccameron@chromium.org>
2016-11-16 00:35:03 -0800

    Add more robust gfx::ColorSpace<->gfx::ICCProfile conversion
    When a gfx::ICCProfile is created from a gfx::ColorSpace, store the
    original gfx::ColorSpace that it was created from, so that when
    gfx::ICCProfile::GetColorSpace is called, we can return the exact same
    original object.
    
    BUG=657946
    
    Review-Url: https://codereview.chromium.org/2502703002
    Cr-Commit-Position: refs/heads/master@{#432430}

--- ui/gfx/mojo/icc_profile_struct_traits.h
+++ ui/gfx/mojo/icc_profile_struct_traits.h
@@ -12,13 +12,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_piece.h"
 #include "mojo/public/cpp/bindings/struct_traits.h"
 #include "ui/gfx/icc_profile.h"
+#include "ui/gfx/ipc/color/gfx_param_traits.h"
 #include "ui/gfx/mojo/icc_profile.mojom.h"
 
 namespace mojo {
 
 template <>
 struct StructTraits<gfx::mojom::ICCProfileDataView, gfx::ICCProfile> {
-  static bool valid(const gfx::ICCProfile& profile) { return profile.valid_; }
+  static gfx::ICCProfile::Type type(const gfx::ICCProfile& profile) {
+    return profile.type_;
+  }
+  static gfx::ColorSpace color_space(const gfx::ICCProfile& profile) {
+    return profile.color_space_;
+  }
   static base::StringPiece data(const gfx::ICCProfile& profile) {
     return base::StringPiece(profile.data_.data(), profile.data_.size());
   }
