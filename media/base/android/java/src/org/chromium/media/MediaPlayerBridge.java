f4ca0d5766a50ba7999d52002e68080a07a3e4b9
watk <watk@chromium.org>
2016-11-16 16:21:26 -0800

    Use StreamUtil#closeQuietly to close a stream in MediaPlayerBridge
    This is cleaner and will suppress a findbugs warning we had.
    
    BUG=665788
    
    Review-Url: https://codereview.chromium.org/2509113002
    Cr-Commit-Position: refs/heads/master@{#432674}

--- media/base/android/java/src/org/chromium/media/MediaPlayerBridge.java
+++ media/base/android/java/src/org/chromium/media/MediaPlayerBridge.java
@@ -18,6 +18,7 @@ import android.util.Base64InputStream;
 import android.view.Surface;
 
 import org.chromium.base.Log;
+import org.chromium.base.StreamUtil;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
 
@@ -261,11 +262,7 @@ public class MediaPlayerBridge {
             } catch (IOException e) {
                 return false;
             } finally {
-                try {
-                    if (fos != null) fos.close();
-                } catch (IOException e) {
-                    // Can't do anything.
-                }
+                StreamUtil.closeQuietly(fos);
             }
         }
 
