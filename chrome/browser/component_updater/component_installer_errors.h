f52d71a07ed53834872ec578d01dcae3cfde414c
waffles <waffles@chromium.org>
2016-11-16 20:37:54 -0800

    Add custom error codes to DefaultComponentInstaller and Flash.
    BUG=615669
    
    Review-Url: https://codereview.chromium.org/2502393003
    Cr-Commit-Position: refs/heads/master@{#432743}

---
+++ chrome/browser/component_updater/component_installer_errors.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_COMPONENT_UPDATER_COMPONENT_INSTALLER_ERRORS_H_
+#define CHROME_BROWSER_COMPONENT_UPDATER_COMPONENT_INSTALLER_ERRORS_H_
+
+#include "components/update_client/update_client.h"
+
+namespace component_updater {
+
+// Errors specific to the Flash installer.
+enum class FlashError {
+  MISSING_VERSION_IN_MANIFEST = 1,
+  HINT_FILE_RECORD_ERROR = 2,
+};
+
+update_client::CrxInstaller::Result ToInstallerResult(const FlashError& err) {
+  return update_client::CrxInstaller::Result(
+      static_cast<int>(update_client::InstallError::CUSTOM_ERROR_BASE) +
+      static_cast<int>(err));
+}
+
+}  // namespace component_updater
+
+#endif  // CHROME_BROWSER_COMPONENT_UPDATER_COMPONENT_INSTALLER_ERRORS_H_
