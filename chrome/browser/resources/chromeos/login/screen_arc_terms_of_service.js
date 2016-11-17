ae18d07c3ff781b46c9545fc42fabba38b78aa13
khmel <khmel@chromium.org>
2016-11-17 00:28:38 -0800

    arc: Fix ToS selection in Arc OptIn OOBE.
    This makes it possible to correctly select ToS based on lang
    and time zone. It also handle timezone change event which is
    very likely in OOBE by reloading ToS if needed.
    
    BUG=651144
    TEST=Manually on device for different languages.
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2502363003
    Cr-Commit-Position: refs/heads/master@{#432812}

--- chrome/browser/resources/chromeos/login/screen_arc_terms_of_service.js
+++ chrome/browser/resources/chromeos/login/screen_arc_terms_of_service.js
@@ -12,7 +12,8 @@ login.createScreen('ArcTermsOfServiceScreen', 'arc-tos',
     EXTERNAL_API: [
       'setMetricsMode',
       'setBackupAndRestoreMode',
-      'setLocationServicesMode'
+      'setLocationServicesMode',
+      'setCountryCode'
     ],
 
     /** @override */
@@ -119,6 +120,27 @@ login.createScreen('ArcTermsOfServiceScreen', 'arc-tos',
       this.setPreference('arc-enable-location-service',
                          'arc-text-location-service',
                          enabled, managed);
+    },
+
+    /**
+     * Sets current country code for ToS.
+     * @param {string} countryCode Country code based on current timezone.
+     */
+    setCountryCode: function(countryCode) {
+      var scriptSetCountryCode = 'document.countryCode = \'' +
+          countryCode.toLowerCase() + '\';';
+      var termsView = $('arc-tos-view');
+      termsView.removeContentScripts(['preProcess']);
+      termsView.addContentScripts([
+          { name: 'preProcess',
+            matches: ['https://play.google.com/*'],
+            js: { code: scriptSetCountryCode },
+            run_at: 'document_start'
+          }]);
+
+      if (!$('arc-tos').hidden) {
+        this.reloadPlayStore();
+      }
     },
 
     /**
@@ -285,17 +307,6 @@ login.createScreen('ArcTermsOfServiceScreen', 'arc-tos',
       $('learn-more-link-location-service').onclick = function() {
         self.showLearnMoreOverlay(leanMoreLocationServiceText);
       };
-
-      var scriptSetCountryCode = 'document.countryCode = \'' +
-          loadTimeData.getString('arcCountryCode') + '\';';
-      var termsView = $('arc-tos-view');
-      termsView.removeContentScripts(['preProcess']);
-      termsView.addContentScripts([
-          { name: 'preProcess',
-            matches: ['https://play.google.com/*'],
-            js: { code: scriptSetCountryCode },
-            run_at: 'document_start'
-          }]);
     }
   };
 });
--- chrome/browser/ui/webui/chromeos/login/arc_terms_of_service_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/arc_terms_of_service_screen_handler.cc
@@ -29,6 +29,8 @@ ArcTermsOfServiceScreenHandler::ArcTermsOfServiceScreenHandler()
 }
 
 ArcTermsOfServiceScreenHandler::~ArcTermsOfServiceScreenHandler() {
+  system::TimezoneSettings::GetInstance()->RemoveObserver(this);
+
   if (screen_)
     screen_->OnActorDestroyed(this);
 }
@@ -38,6 +40,19 @@ void ArcTermsOfServiceScreenHandler::RegisterMessages() {
               &ArcTermsOfServiceScreenHandler::HandleSkip);
   AddCallback("arcTermsOfServiceAccept",
               &ArcTermsOfServiceScreenHandler::HandleAccept);
+}
+
+void ArcTermsOfServiceScreenHandler::UpdateTimeZone() {
+  const std::string country_code = base::CountryCodeForCurrentTimezone();
+  if (country_code == last_applied_contry_code_)
+    return;
+  last_applied_contry_code_ = country_code;
+  CallJS("setCountryCode", country_code);
+}
+
+void ArcTermsOfServiceScreenHandler::TimezoneChanged(
+    const icu::TimeZone& timezone) {
+  UpdateTimeZone();
 }
 
 void ArcTermsOfServiceScreenHandler::DeclareLocalizedValues(
@@ -60,7 +75,6 @@ void ArcTermsOfServiceScreenHandler::DeclareLocalizedValues(
   builder->Add("arcLearnMoreBackupAndRestore",
       IDS_ARC_OPT_IN_LEARN_MORE_BACKUP_AND_RESTORE);
   builder->Add("arcOverlayClose", IDS_ARC_OOBE_TERMS_POPUP_HELP_CLOSE_BUTTON);
-  builder->Add("arcCountryCode", base::CountryCodeForCurrentTimezone());
 }
 
 void ArcTermsOfServiceScreenHandler::OnMetricsModeChanged(bool enabled,
@@ -114,6 +128,7 @@ void ArcTermsOfServiceScreenHandler::Show() {
 }
 
 void ArcTermsOfServiceScreenHandler::Hide() {
+  system::TimezoneSettings::GetInstance()->RemoveObserver(this);
   pref_handler_.reset();
 }
 
@@ -131,6 +146,10 @@ void ArcTermsOfServiceScreenHandler::DoShow() {
   pref_handler_.reset(new arc::ArcOptInPreferenceHandler(
       this, profile->GetPrefs()));
   pref_handler_->Start();
+
+  UpdateTimeZone();
+  system::TimezoneSettings::GetInstance()->AddObserver(this);
+
   ShowScreen(OobeScreen::SCREEN_ARC_TERMS_OF_SERVICE);
 }
 
