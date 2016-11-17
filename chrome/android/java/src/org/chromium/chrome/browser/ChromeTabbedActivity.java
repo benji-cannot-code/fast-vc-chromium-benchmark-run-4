1ef0bec9369b255aea81943ade894f3212e98305
mthiesse <mthiesse@chromium.org>
2016-11-16 19:39:08 -0800

    Fix broken VR transitions due to ChromeTabbedActivity not being exported.
    Recently, VR transitions broke due to us properly registering as a VR capable app. For some reason this lead VrCore to handle us differently and try launching ChromeTabbedActivity, which it couldn't because it's not exported.
    
    This CL updates our transition code to use the new daydream API calls, giving us enter and exit VR transitions. I also introduce an activity-alias for VR purposes so that VrCore can call back into us after the VR DON flow completes.
    
    BUG=665006
    
    Review-Url: https://codereview.chromium.org/2500153003
    Cr-Commit-Position: refs/heads/master@{#432727}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -451,9 +451,6 @@ public class ChromeTabbedActivity extends ChromeActivity implements OverviewMode
 
         if (getActivityTab() != null) getActivityTab().setIsAllowedToReturnToExternalApp(false);
 
-        if (mVrShellDelegate.isVrInitialized()) {
-            mVrShellDelegate.close();
-        }
         mTabModelSelectorImpl.saveState();
         StartupMetrics.getInstance().recordHistogram(true);
         mActivityStopMetrics.onStopWithNative(this);
@@ -483,9 +480,7 @@ public class ChromeTabbedActivity extends ChromeActivity implements OverviewMode
             if (CommandLine.getInstance().hasSwitch(ContentSwitches.ENABLE_TEST_INTENTS)) {
                 handleDebugIntent(intent);
             }
-            if (!mVrShellDelegate.isInVR() && mVrShellDelegate.isVrIntent(intent)) {
-                mVrShellDelegate.enterVRIfNecessary(false);
-            }
+            if (mVrShellDelegate.isVrIntent(intent)) mVrShellDelegate.enterVRFromIntent(intent);
         } finally {
             TraceEvent.end("ChromeTabbedActivity.onNewIntentWithNative");
         }
@@ -728,6 +723,9 @@ public class ChromeTabbedActivity extends ChromeActivity implements OverviewMode
                     launchFirstRunExperience();
                 }
             }
+            return true;
+        } else if (requestCode == VrShellDelegate.EXIT_VR_RESULT) {
+            mVrShellDelegate.onExitVRResult(resultCode);
             return true;
         }
         return false;
@@ -1241,7 +1239,7 @@ public class ChromeTabbedActivity extends ChromeActivity implements OverviewMode
         if (!mUIInitialized) return false;
         final Tab currentTab = getActivityTab();
 
-        if (mVrShellDelegate.exitVRIfNecessary()) return true;
+        if (mVrShellDelegate.exitVRIfNecessary(true)) return true;
 
         if (currentTab == null) {
             recordBackPressedUma("currentTab is null", BACK_PRESSED_TAB_IS_NULL);
