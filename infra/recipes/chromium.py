d0846429b758fe55b5d4c2761ef2dfc31cdeeca8
Dan Jacques <dnj@chromium.org>
2016-11-17 15:23:43 -0800

    Manually roll recipes (again).
    Last time, the roll succeeded but was incomplete due to bad recipe
    coverage in the Chromuium repository. Do another roll and fill in the
    missing dependencies for the other repositories.
    
    BUG=chromium:666342
    TEST=None
    R=iannucci@chromium.org
    
    Review URL: https://codereview.chromium.org/2511293002 .
    
    Cr-Commit-Position: refs/heads/master@{#433016}

--- infra/recipes/chromium.py
+++ infra/recipes/chromium.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 DEPS = [
   'build/adb',
   'build/bisect_tester',
+  'build/build',
   'build/chromium',
   'build/chromium_android',
   'build/chromium_swarming',
