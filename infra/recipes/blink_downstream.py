b2511b8c0171f3ba1b75dbec5f940e47bd983b21
dnj <dnj@chromium.org>
2016-11-17 12:17:55 -0800

    Manually roll recipes
    TBR=martiniss@chromium.org,phajdan.jr@chromium.org
    BUG=None
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2513473004
    Cr-Commit-Position: refs/heads/master@{#432945}

--- infra/recipes/blink_downstream.py
+++ infra/recipes/blink_downstream.py
@@ -29,6 +29,7 @@ in a forced build with no revision provided).
 from recipe_engine.types import freeze
 
 DEPS = [
+  'build/build',
   'build/chromium',
   'build/chromium_checkout',
   'build/chromium_tests',
