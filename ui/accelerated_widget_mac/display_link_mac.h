7352a40d81500282571c75f11a3bf0eedbc32834
gab <gab@chromium.org>
2016-11-17 10:16:13 -0800

    IWYU: Add missing fwd-decl in display_link_mac.h
    As a result of removing SingleThreadTaskRunner fwd-decl from timer.h
    in https://codereview.chromium.org/2491613004/#
    
    BUG=552633
    TBR=ccameron@chromium.org
    
    Review-Url: https://codereview.chromium.org/2507043003
    Cr-Commit-Position: refs/heads/master@{#432915}

--- ui/accelerated_widget_mac/display_link_mac.h
+++ ui/accelerated_widget_mac/display_link_mac.h
@@ -17,6 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/timer.h"
 #include "ui/accelerated_widget_mac/accelerated_widget_mac_export.h"
 
+namespace base {
+class SingleThreadTaskRunner;
+}
+
 namespace ui {
 
 class ACCELERATED_WIDGET_MAC_EXPORT DisplayLinkMac :
