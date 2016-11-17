47ca086707c8762cee929d1fbf34fec2114f4223
carlosk <carlosk@chromium.org>
2016-11-17 13:37:18 -0800

    Small fix to passive mixed-content reporting.
    There was a change in passive mixed-content reporting behavior when
    https://crrev.com/2278303002 landed: whereas before this reporting would
    only occurr if |stricMode| was not true, with that change it would
    always happen. This change simply reverts to the previous behavior.
    
    BUG=631525
    
    Review-Url: https://codereview.chromium.org/2497893006
    Cr-Commit-Position: refs/heads/master@{#432966}

--- third_party/WebKit/Source/core/loader/MixedContentChecker.cpp
+++ third_party/WebKit/Source/core/loader/MixedContentChecker.cpp
@@ -264,10 +264,11 @@ bool MixedContentChecker::shouldBlockFetch(
 
   switch (contextType) {
     case WebMixedContent::ContextType::OptionallyBlockable:
-      client->passiveInsecureContentFound(url);
       allowed = !strictMode;
-      if (allowed)
+      if (allowed) {
+        client->passiveInsecureContentFound(url);
         client->didDisplayInsecureContent();
+      }
       break;
 
     case WebMixedContent::ContextType::Blockable: {
