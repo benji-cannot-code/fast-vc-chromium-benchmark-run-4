9852bfd764bcb2fa35a66ca8b2383d7266f503aa
jdufault <jdufault@chromium.org>
2016-11-17 13:47:56 -0800

    Hide dismiss button on toast when doing partial screenshot using stylus tools.
    Users click the toast dismiss button using the stylus, which takes a screenshot
    instead of actually dismissing the toast. Since the toast is relatively short
    (2.5s), hiding the dismiss button should be fine.
    
    BUG=665630
    
    Review-Url: https://codereview.chromium.org/2506583005
    Cr-Commit-Position: refs/heads/master@{#432977}

--- ash/common/system/toast/toast_data.h
+++ ash/common/system/toast/toast_data.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "ash/ash_export.h"
+#include "base/optional.h"
 #include "base/strings/string16.h"
 
 namespace ash {
@@ -21,13 +22,14 @@ struct ASH_EXPORT ToastData {
   ToastData(std::string id,
             const base::string16& text,
             int32_t duration_ms,
-            const base::string16& dismiss_text);
+            const base::Optional<base::string16>& dismiss_text);
   ToastData(const ToastData& other);
+  ~ToastData();
 
   std::string id;
   base::string16 text;
   int32_t duration_ms;
-  base::string16 dismiss_text;
+  base::Optional<base::string16> dismiss_text;
 };
 
 }  // namespace ash
--- ash/common/system/toast/toast_overlay.h
+++ ash/common/system/toast/toast_overlay.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/ash_export.h"
+#include "base/optional.h"
 #include "base/strings/string16.h"
 #include "ui/compositor/layer_animation_observer.h"
 #include "ui/events/event.h"
@@ -39,10 +40,12 @@ class ASH_EXPORT ToastOverlay : public ui::ImplicitAnimationObserver {
 
   // Creates the Toast overlay UI. |text| is the message to be shown, and
   // |dismiss_text| is the message for the button to dismiss the toast message.
-  // |dismiss_text| is optional. If empty, the default text is used.
+  // If |dismiss_text| is null, no dismiss button will be shown. If
+  // |dismiss_text| has a value but the string is empty, the default text is
+  // used.
   ToastOverlay(Delegate* delegate,
                const base::string16& text,
-               const base::string16& dismiss_text);
+               base::Optional<base::string16> dismiss_text);
   ~ToastOverlay() override;
 
   // Shows or hides the overlay.
@@ -59,11 +62,12 @@ class ASH_EXPORT ToastOverlay : public ui::ImplicitAnimationObserver {
   void OnImplicitAnimationsCompleted() override;
 
   views::Widget* widget_for_testing();
+  ToastOverlayButton* dismiss_button_for_testing();
   void ClickDismissButtonForTesting(const ui::Event& event);
 
   Delegate* const delegate_;
   const base::string16 text_;
-  const base::string16 dismiss_text_;
+  const base::Optional<base::string16> dismiss_text_;
   std::unique_ptr<views::Widget> overlay_widget_;
   std::unique_ptr<ToastOverlayView> overlay_view_;
   gfx::Size widget_size_;
