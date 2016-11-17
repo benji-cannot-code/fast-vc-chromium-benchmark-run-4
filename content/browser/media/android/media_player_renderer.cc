af670fdc92d29756d4ab6191e27948f953efacdd
tguilbert <tguilbert@chromium.org>
2016-11-16 18:39:34 -0800

    Add MediaServiceThrottler
    WMPA used the MediaThrottler to display a notification bar to the user
    if an unusual number of media server crashes were detected. This was
    done in order to prevent a webpage from loading the same url over and
    over in order to crash the Media Server.
    
    This change introduces the MediaServerCrashListener which reuses the
    same technique that the MediaThrottler used to monitor for Media Server
    crashes.
    
    It also introduces the MediaServiceThrottler which uses the
    MediaServerCrashListener, and exponentially delays the creation of new
    clients (MediaPlayerBridges for now) based on the number of Media
    Server crashes.
    
    The MediaServiceThrottler resets its internal clock state after a
    minute of not having schedule new client creations, and reset its
    crash counter after a minute of not having detected any crashes. It
    also support "burst scheduling", allowing for a fixed number of clients
    to be scheduled immediately within some sliding window, in order to
    minimize impact from multiple requests coming in from new page loads.
    
    BUG=636615
    TEST=Added UTs
    
    Review-Url: https://codereview.chromium.org/2471903002
    Cr-Commit-Position: refs/heads/master@{#432712}

--- content/browser/media/android/media_player_renderer.cc
+++ content/browser/media/android/media_player_renderer.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/callback_helpers.h"
 #include "content/browser/android/scoped_surface_request_manager.h"
 #include "content/browser/media/android/media_resource_getter_impl.h"
 #include "content/public/browser/browser_context.h"
@@ -15,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/storage_partition.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_client.h"
+#include "media/base/android/media_service_throttler.h"
 
 // TODO(tguilbert): Remove this ID once MediaPlayerManager has been deleted
 // and MediaPlayerBridge updated. See comment in header file.
@@ -42,6 +44,11 @@ void MediaPlayerRenderer::Initialize(
     media::RendererClient* client,
     const media::PipelineStatusCB& init_cb) {
   DVLOG(1) << __func__;
+
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+
+  renderer_client_ = client;
+
   if (demuxer_stream_provider->GetType() !=
       media::DemuxerStreamProvider::Type::URL) {
     DLOG(ERROR) << "DemuxerStreamProvider is not of Type URL";
@@ -49,9 +56,26 @@ void MediaPlayerRenderer::Initialize(
     return;
   }
 
-  media::MediaUrlParams url_params =
-      demuxer_stream_provider->GetMediaUrlParams();
-  renderer_client_ = client;
+  base::TimeDelta creation_delay =
+      media::MediaServiceThrottler::GetInstance()->GetDelayForClientCreation();
+
+  if (creation_delay.is_zero()) {
+    CreateMediaPlayer(demuxer_stream_provider->GetMediaUrlParams(), init_cb);
+    return;
+  }
+
+  BrowserThread::PostDelayedTask(
+      BrowserThread::UI, FROM_HERE,
+      base::Bind(&MediaPlayerRenderer::CreateMediaPlayer,
+                 weak_factory_.GetWeakPtr(),
+                 demuxer_stream_provider->GetMediaUrlParams(), init_cb),
+      creation_delay);
+}
+
+void MediaPlayerRenderer::CreateMediaPlayer(
+    const media::MediaUrlParams& url_params,
+    const media::PipelineStatusCB& init_cb) {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
   const std::string user_agent = GetContentClient()->GetUserAgent();
 
