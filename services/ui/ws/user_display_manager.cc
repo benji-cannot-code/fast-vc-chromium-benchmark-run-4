f26a78d1121f7cb74875a87d436dbab8258a46a2
yzshen <yzshen@chromium.org>
2016-11-17 00:37:50 -0800

    Mojo C++ bindings: switch part of the services/ui/public/interfaces mojom target to use STL types.
    BUG=662515
    
    Review-Url: https://codereview.chromium.org/2505133003
    Cr-Commit-Position: refs/heads/master@{#432815}

--- services/ui/ws/user_display_manager.cc
+++ services/ui/ws/user_display_manager.cc
@@ -40,10 +40,10 @@ void UserDisplayManager::OnFrameDecorationValuesChanged() {
   mojo::Array<mojom::WsDisplayPtr> displays = GetAllDisplays();
   display_manager_observers_.ForAllPtrs(
       [this, &displays](mojom::DisplayManagerObserver* observer) {
-        observer->OnDisplaysChanged(displays.Clone());
+        observer->OnDisplaysChanged(displays.Clone().PassStorage());
       });
   if (test_observer_)
-    test_observer_->OnDisplaysChanged(displays.Clone());
+    test_observer_->OnDisplaysChanged(displays.Clone().PassStorage());
 }
 
 void UserDisplayManager::AddDisplayManagerBinding(
@@ -60,10 +60,10 @@ void UserDisplayManager::OnDisplayUpdate(Display* display) {
 
   display_manager_observers_.ForAllPtrs(
       [&displays](mojom::DisplayManagerObserver* observer) {
-        observer->OnDisplaysChanged(displays.Clone());
+        observer->OnDisplaysChanged(displays.Clone().PassStorage());
       });
   if (test_observer_)
-    test_observer_->OnDisplaysChanged(displays.Clone());
+    test_observer_->OnDisplaysChanged(displays.Clone().PassStorage());
 }
 
 void UserDisplayManager::OnWillDestroyDisplay(Display* display) {
@@ -160,7 +160,7 @@ void UserDisplayManager::CallOnDisplays(
     mojom::DisplayManagerObserver* observer) {
   // TODO(kylechar): Pass internal display id to clients here.
   observer->OnDisplays(
-      GetAllDisplays(),
+      GetAllDisplays().PassStorage(),
       display::PlatformScreen::GetInstance()->GetPrimaryDisplayId(),
       display::Display::kInvalidDisplayID);
 }
