f1b1d463676ccb98c9ccbf2a6671a9f2c2c97f40
klausw <klausw@chromium.org>
2016-11-17 00:27:52 -0800

    WebVR: Use content CVC size for compositor rendering
    We can't currently resize the content window used by the
    compositor rendering path, so we must use its dimensions
    for the WebVR API reported render sizes.
    
    It includes a rather nasty hack to override canvas size to 100% if
    it was manually set to something different. This works around
    three.js examples not rendering right, apparently a fullscreened
    canvas just gets centered if it explicitly sets a size smaller
    than the fullscreen dimensions.
    
    BUG=389343,655722
    
    Review-Url: https://codereview.chromium.org/2508703002
    Cr-Commit-Position: refs/heads/master@{#432811}

--- device/vr/android/gvr/gvr_delegate.h
+++ device/vr/android/gvr/gvr_delegate.h
@@ -17,6 +17,8 @@ class GvrApi;
 
 namespace device {
 
+constexpr gvr::Sizei kFallbackRenderTargetSize = {2048, 1024};
+
 class DEVICE_VR_EXPORT GvrDelegate {
  public:
   virtual void SetWebVRSecureOrigin(bool secure_origin) = 0;
@@ -26,6 +28,8 @@ class DEVICE_VR_EXPORT GvrDelegate {
 
   virtual void SetGvrPoseForWebVr(const gvr::Mat4f& pose,
                                   uint32_t pose_index) = 0;
+  virtual gvr::Sizei GetWebVRCompositorSurfaceSize() = 0;
+  virtual void SetWebVRRenderSurfaceSize(int width, int height) = 0;
   virtual gvr::GvrApi* gvr_api() = 0;
 };
 
