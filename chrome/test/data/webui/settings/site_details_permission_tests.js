171dff79e1382f1fb59cab642887b9c7e2d4229a
dschuyler <dschuyler@chromium.org>
2016-11-16 18:37:43 -0800

    [MD settings] remove switch() blocks; put values in html
    This CL changes code internals without changing the UI. The code was
    using several large switch() statement functions to choose HTML values
    which should be in the HTML instead. Some examples are choosing which
    icon to show, or which i18n text string to show. This CL removes the
    functions with the switch()-es and inlines the values.
    
    BUG=543635
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2510853002
    Cr-Commit-Position: refs/heads/master@{#432710}

--- chrome/test/data/webui/settings/site_details_permission_tests.js
+++ chrome/test/data/webui/settings/site_details_permission_tests.js
@@ -101,9 +101,10 @@ cr.define('site_details_permission', function() {
       });
 
       test('camera category', function() {
-        var origin = "https://www.example.com";
+        var origin = 'https://www.example.com';
         browserProxy.setPrefs(prefs);
         testElement.category = settings.ContentSettingsTypes.CAMERA;
+        testElement.label = 'Camera';
         testElement.site = {
           origin: origin,
           embeddingOrigin: '',
@@ -130,9 +131,10 @@ cr.define('site_details_permission', function() {
       });
 
       test('cookies category', function() {
-        var origin = "https://www.example.com";
+        var origin = 'https://www.example.com';
         browserProxy.setPrefs(prefsCookies);
         testElement.category = settings.ContentSettingsTypes.COOKIES;
+        testElement.label = 'Cookies';
         testElement.site = {
           origin: origin,
           embeddingOrigin: '',
