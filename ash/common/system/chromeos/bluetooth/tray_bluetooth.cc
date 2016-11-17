d838d43d27b2e5225ac84b1c1c5bde0ea9b8351f
estade <estade@chromium.org>
2016-11-16 20:58:40 -0800

    Don't reset scroll position of contents on every ScrollView layout.
    Also do a little cleanup in ash scrollview related code.
    
    BUG=664930
    
    Review-Url: https://codereview.chromium.org/2509783002
    Cr-Commit-Position: refs/heads/master@{#432749}

--- ash/common/system/chromeos/bluetooth/tray_bluetooth.cc
+++ ash/common/system/chromeos/bluetooth/tray_bluetooth.cc
@@ -331,7 +331,7 @@ class BluetoothDetailedView : public TrayDetailsView {
       }
     }
 
-    scroll_content()->SizeToPreferredSize();
+    scroll_content()->InvalidateLayout();
   }
 
   void AppendSameTypeDevicesToScrollList(const BluetoothDeviceList& list,
