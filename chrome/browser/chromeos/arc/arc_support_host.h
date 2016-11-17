12f6762bab69fe03403b8f453e49ff677f3e63e8
hidehiko <hidehiko@chromium.org>
2016-11-17 02:32:51 -0800

    Refactor ArcSupportHost and ArcAuthService part2.
    This CL moves more UI related implementation from ArcAuthService
    to ArcSupportHost, and remove the dependency from ArcSupportHost
    to ArcAuthService.
    
    BUG=657687
    BUG=b/31079732
    TEST=Ran on test device. Ran bots.
    
    Review-Url: https://codereview.chromium.org/2501013002
    Cr-Commit-Position: refs/heads/master@{#432833}

--- chrome/browser/chromeos/arc/arc_support_host.h
+++ chrome/browser/chromeos/arc/arc_support_host.h
@@ -13,22 +13,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/api/messaging/native_message_host.h"
 #include "ui/display/display_observer.h"
 
+class Profile;
+
 // Native interface to control ARC support chrome App.
-// TODO(hidehiko): Move more implementation for the UI control from
-// ArcAuthService to this class.
 // TODO(hidehiko,lhchavez): Move this into extensions/ directory, and put it
 // into "arc" namespace. Add unittests at the time.
 class ArcSupportHost : public arc::ArcSupportMessageHost::Observer,
                        public display::DisplayObserver {
  public:
   enum class UIPage {
-    NO_PAGE,              // Hide everything.
-    TERMS,                // Terms content page.
-    LSO_PROGRESS,         // LSO loading progress page.
-    LSO,                  // LSO page to enter user's credentials.
-    START_PROGRESS,       // Arc starting progress page.
-    ERROR,                // Arc start error page.
-    ERROR_WITH_FEEDBACK,  // Arc start error page, plus feedback button.
+    NO_PAGE,      // Hide everything.
+    TERMS,        // Terms content page.
+    LSO,          // LSO page to enter user's credentials.
+    ARC_LOADING,  // ARC loading progress page.
+    ERROR,        // ARC start error page.
+  };
+
+  // Error types whose corresponding message ARC support has.
+  enum class Error {
+    SIGN_IN_NETWORK_ERROR,
+    SIGN_IN_SERVICE_UNAVAILABLE_ERROR,
+    SIGN_IN_BAD_AUTHENTICATION_ERROR,
+    SIGN_IN_GMS_NOT_AVAILABLE_ERROR,
+    SIGN_IN_CLOUD_PROVISION_FLOW_FAIL_ERROR,
+    SIGN_IN_UNKNOWN_ERROR,
+    SERVER_COMMUNICATION_ERROR,
+    ANDROID_MANAGEMENT_REQUIRED_ERROR,
   };
 
   // Observer to notify UI event.
@@ -59,7 +69,7 @@ class ArcSupportHost : public arc::ArcSupportMessageHost::Observer,
   static const char kHostAppId[];
   static const char kStorageId[];
 
-  ArcSupportHost();
+  explicit ArcSupportHost(Profile* profile);
   ~ArcSupportHost() override;
 
   // Currently only one observer can be added.
@@ -76,9 +86,24 @@ class ArcSupportHost : public arc::ArcSupportMessageHost::Observer,
   // this is no op.
   void UnsetMessageHost(arc::ArcSupportMessageHost* message_host);
 
+  // Sets the ARC managed state. This must be called before ARC support app
+  // is started.
+  void SetArcManaged(bool is_arc_managed);
+
   // Requests to close the extension window.
   void Close();
-  void ShowPage(UIPage page, const base::string16& status);
+
+  // Requests to show the "Terms Of Service" page.
+  void ShowTermsOfService();
+
+  // Requests to show the LSO page.
+  void ShowLso();
+
+  // Requests to show the "ARC is loading" page.
+  void ShowArcLoading();
+
+  // Requests to show the error page
+  void ShowError(Error error, bool should_show_send_feedback);
 
   void SetMetricsPreferenceCheckbox(bool is_enabled, bool is_managed);
   void SetBackupAndRestorePreferenceCheckbox(bool is_enabled, bool is_managed);
@@ -93,6 +118,11 @@ class ArcSupportHost : public arc::ArcSupportMessageHost::Observer,
   void OnDisplayMetricsChanged(const display::Display& display,
                                uint32_t changed_metrics) override;
 
+  // Returns current page that has to be shown in OptIn UI.
+  // Note that this can be inconsistent from the actually shown page.
+  // TODO(hidehiko): Remove this exposure.
+  UIPage ui_page() const { return ui_page_; }
+
  private:
   struct PreferenceCheckboxData {
     PreferenceCheckboxData() : PreferenceCheckboxData(false, false) {}
@@ -103,7 +133,13 @@ class ArcSupportHost : public arc::ArcSupportMessageHost::Observer,
     bool is_managed;
   };
 
+  // Requests to start the ARC support Chrome app.
+  void RequestAppStart();
+
   bool Initialize();
+
+  // Requests to ARC support Chrome app to show the specified page.
+  void ShowPage(UIPage ui_page);
 
   // Sends a preference update to the extension.
   // The message will be
@@ -117,16 +153,34 @@ class ArcSupportHost : public arc::ArcSupportMessageHost::Observer,
 
   void DisconnectMessageHost();
 
+  Profile* const profile_;
+
   // Currently Observer is only ArcAuthService, so it is unique.
   // Use ObserverList when more classes start to observe it.
   Observer* observer_ = nullptr;
 
-  PreferenceCheckboxData metrics_checkbox_;
-  PreferenceCheckboxData backup_and_restore_checkbox_;
-  PreferenceCheckboxData location_services_checkbox_;
+  // True, if ARC support app is requested to start, but the connection is not
+  // yet established. Reset to false, when the app is started and the
+  // connection to the app is established.
+  bool app_start_pending_ = false;
 
   // The instance is created and managed by Chrome.
   arc::ArcSupportMessageHost* message_host_ = nullptr;
+
+  // The lifetime of the message_host_ is out of control from ARC.
+  // Fields below are UI parameter cache in case the value is set before
+  // connection to the ARC support Chrome app is established.
+  UIPage ui_page_ = UIPage::NO_PAGE;
+
+  // These have valid values iff ui_page_ == ERROR.
+  Error error_;
+  bool should_show_send_feedback_;
+
+  bool is_arc_managed_ = false;
+
+  PreferenceCheckboxData metrics_checkbox_;
+  PreferenceCheckboxData backup_and_restore_checkbox_;
+  PreferenceCheckboxData location_services_checkbox_;
 
   DISALLOW_COPY_AND_ASSIGN(ArcSupportHost);
 };
