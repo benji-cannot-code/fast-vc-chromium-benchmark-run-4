36db8afb147c2f3c560953489f3d857f824a2790
cmumford <cmumford@chromium.org>
2016-11-17 12:27:49 -0800

    Moved CacheStorage classes into content.proto package.
    Places the generated Protobuf classes in the content::proto namespace.
    
    Review-Url: https://codereview.chromium.org/2509363003
    Cr-Commit-Position: refs/heads/master@{#432955}

--- content/browser/cache_storage/cache_storage_manager.cc
+++ content/browser/cache_storage/cache_storage_manager.cc
@@ -65,7 +65,7 @@ void ListOriginsAndLastModifiedOnTaskRunner(
     std::string protobuf;
     base::ReadFileToString(path.AppendASCII(CacheStorage::kIndexFileName),
                            &protobuf);
-    CacheStorageIndex index;
+    proto::CacheStorageIndex index;
     if (index.ParseFromString(protobuf)) {
       if (index.has_origin()) {
         base::File::Info file_info;
