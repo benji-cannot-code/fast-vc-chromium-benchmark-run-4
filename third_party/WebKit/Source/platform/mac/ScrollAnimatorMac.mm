39831892ba5c7d95419cd10fce87782e8cf4bfa8
bokan <bokan@chromium.org>
2016-11-16 16:35:37 -0800

    Fix typo in ScrollAnimatorMac
    BUG=
    
    Review-Url: https://codereview.chromium.org/2506133002
    Cr-Commit-Position: refs/heads/master@{#432678}

--- third_party/WebKit/Source/platform/mac/ScrollAnimatorMac.mm
+++ third_party/WebKit/Source/platform/mac/ScrollAnimatorMac.mm
@@ -970,7 +970,7 @@ bool ScrollAnimatorMac::setScrollbarsVisibleForTesting(bool show) {
     [m_scrollbarPainterController.get() hideOverlayScrollers];
 
   [m_verticalScrollbarPainterDelegate.get() updateVisibilityImmediately:show];
-  [m_verticalScrollbarPainterDelegate.get() updateVisibilityImmediately:show];
+  [m_horizontalScrollbarPainterDelegate.get() updateVisibilityImmediately:show];
   return true;
 }
 
