2a68e50bcda417e8510c700e3d85a994fba3c337
sashab <sashab@chromium.org>
2016-11-16 19:29:19 -0800

    Changed ECursor to an enum class and renamed its members to keywords
    Changed ECursor to an enum class and gave it an unsigned underlying
    type. Also renamed its members to match its keywords from
    CSSValueKeywords.in.
    
    Changing it to an enum class enforces better namespacing and code
    practices. Adding the unsigned underlying type is pre-work for when the
    class is eventually stored as an enum bitfield (it would be done in this
    patch, except a presubmit warning already exists that prevents that. The
    presubmit warning needs to be updated before that change can occur.)
    
    This is also pre-work to move ECursor to be generated in
    ComputedStyleBase.
    
    BUG=628043
    
    Review-Url: https://codereview.chromium.org/2505853002
    Cr-Commit-Position: refs/heads/master@{#432724}

--- third_party/WebKit/Source/core/style/ComputedStyle.h
+++ third_party/WebKit/Source/core/style/ComputedStyle.h
@@ -371,7 +371,7 @@ class CORE_EXPORT ComputedStyle : public ComputedStyleBase,
     m_inheritedData.m_textAlign = initialTextAlign();
     m_inheritedData.m_textTransform = initialTextTransform();
     m_inheritedData.m_textUnderline = false;
-    m_inheritedData.m_cursorStyle = initialCursor();
+    m_inheritedData.m_cursorStyle = static_cast<unsigned>(initialCursor());
     m_inheritedData.m_direction = initialDirection();
     m_inheritedData.m_whiteSpace = initialWhiteSpace();
     m_inheritedData.m_borderCollapse = initialBorderCollapse();
@@ -2088,11 +2088,13 @@ class CORE_EXPORT ComputedStyle : public ComputedStyleBase,
   }
 
   // cursor
-  static ECursor initialCursor() { return CURSOR_AUTO; }
+  static ECursor initialCursor() { return ECursor::Auto; }
   ECursor cursor() const {
     return static_cast<ECursor>(m_inheritedData.m_cursorStyle);
   }
-  void setCursor(ECursor c) { m_inheritedData.m_cursorStyle = c; }
+  void setCursor(ECursor c) {
+    m_inheritedData.m_cursorStyle = static_cast<unsigned>(c);
+  }
 
   // direction
   static TextDirection initialDirection() { return LTR; }
