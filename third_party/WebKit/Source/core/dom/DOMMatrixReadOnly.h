f8b9b977ce87701b399d68b5a6140c3a391df3b0
hs1217.lee <hs1217.lee@samsung.com>
2016-11-16 21:25:49 -0800

    [GeometryInterface] add transformPoint(point) function.
    Let point object be the result of invoking create
    a DOMPoint from the dictionary point.
    Point object is post-multiplied to the current matrix
    and returns the resulting point.
    The passed argument does not get modified.
    
    spec list:
    https://drafts.fxtf.org/geometry/#dom-dommatrixreadonly-transformpoint
    
    BUG=388780
    
    Review-Url: https://codereview.chromium.org/2423753002
    Cr-Commit-Position: refs/heads/master@{#432762}

--- third_party/WebKit/Source/core/dom/DOMMatrixReadOnly.h
+++ third_party/WebKit/Source/core/dom/DOMMatrixReadOnly.h
@@ -17,6 +17,8 @@ namespace blink {
 
 class DOMMatrix;
 class DOMMatrixInit;
+class DOMPoint;
+class DOMPointInit;
 
 class CORE_EXPORT DOMMatrixReadOnly
     : public GarbageCollectedFinalized<DOMMatrixReadOnly>,
@@ -83,6 +85,8 @@ class CORE_EXPORT DOMMatrixReadOnly
   DOMMatrix* flipX();
   DOMMatrix* flipY();
   DOMMatrix* inverse();
+
+  DOMPoint* transformPoint(const DOMPointInit&);
 
   DOMFloat32Array* toFloat32Array() const;
   DOMFloat64Array* toFloat64Array() const;
