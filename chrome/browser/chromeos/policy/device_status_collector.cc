d978cf00ad891752854454be039a52ff3a2b321d
yusukes <yusukes@chromium.org>
2016-11-16 23:43:51 -0800

    arc: enable use_new_wrapper_types for enterprise_reporting.mojom
    BUG=662510
    BUG=624136
    TEST=try
    
    Review-Url: https://codereview.chromium.org/2504013003
    Cr-Commit-Position: refs/heads/master@{#432804}

--- chrome/browser/chromeos/policy/device_status_collector.cc
+++ chrome/browser/chromeos/policy/device_status_collector.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <cstdio>
 #include <limits>
-#include <memory>
 #include <sstream>
 
 #include "base/bind.h"
@@ -390,8 +389,8 @@ class GetStatusState : public base::RefCountedThreadSafe<GetStatusState> {
       *device_status_->add_cpu_temp_info() = info;
   }
 
-  void OnAndroidInfoReceived(mojo::String status,
-                             mojo::String droid_guard_info) {
+  void OnAndroidInfoReceived(const std::string& status,
+                             const std::string& droid_guard_info) {
     em::AndroidStatus* const android_status =
         session_status_->mutable_android_status();
     android_status->set_status_payload(status);
