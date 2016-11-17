e0cc596320e2d06f544dd580730c4979279e1a9b
vitaliii <vitaliii@chromium.org>
2016-11-17 01:32:47 -0800

    [NTP] Propagate OfflineId from C++ for Downloads.
    This CL propagates OfflineId from C++ for Downloads in order to match
    exactly the corresponding offline page (this is important when there
    are multiple ones for the same URL). While we are here, RecentTabs are
    changed to store OfflineId in a general (non recent tabs) variable.
    
    BUG=664903
    
    Review-Url: https://codereview.chromium.org/2507793002
    Cr-Commit-Position: refs/heads/master@{#432830}

--- components/ntp_snippets/content_suggestion.cc
+++ components/ntp_snippets/content_suggestion.cc
@@ -9,6 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ntp_snippets {
 
+DownloadSuggestionExtra::DownloadSuggestionExtra() = default;
+
+DownloadSuggestionExtra::~DownloadSuggestionExtra() = default;
+
 bool ContentSuggestion::ID::operator==(const ID& rhs) const {
   return category_ == rhs.category_ &&
          id_within_category_ == rhs.id_within_category_;
