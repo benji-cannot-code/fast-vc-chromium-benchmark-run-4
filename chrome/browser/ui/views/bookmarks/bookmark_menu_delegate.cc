3901ebb61d093207bb6ba109893a19b9960fa63b
oshima <oshima@chromium.org>
2016-11-17 06:53:20 -0800

    Update the bookmark tooltip text only when necessary
    BUG=612682
    TEST=manual. see bug for repro step.
    
    Review-Url: https://codereview.chromium.org/2507743002
    Cr-Commit-Position: refs/heads/master@{#432874}

--- chrome/browser/ui/views/bookmarks/bookmark_menu_delegate.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_menu_delegate.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/button/menu_button.h"
 #include "ui/views/controls/menu/menu_item_view.h"
 #include "ui/views/controls/menu/submenu_view.h"
+#include "ui/views/widget/tooltip_manager.h"
 #include "ui/views/widget/widget.h"
 
 using base::UserMetricsAction;
@@ -146,8 +147,10 @@ base::string16 BookmarkMenuDelegate::GetTooltipText(
 
   const BookmarkNode* node = i->second;
   if (node->is_url()) {
+    const views::TooltipManager* tooltip_manager = parent_->GetTooltipManager();
     return BookmarkBarView::CreateToolTipForURLAndTitle(
-        parent_, screen_loc, node->url(), node->GetTitle());
+        tooltip_manager->GetMaxWidth(screen_loc),
+        tooltip_manager->GetFontList(), node->url(), node->GetTitle());
   }
   return base::string16();
 }
