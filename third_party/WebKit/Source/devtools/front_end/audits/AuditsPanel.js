0249f5fff50483d0aa972981a5d47fbc19f06f8d
dgozman <dgozman@chromium.org>
2016-11-17 19:19:49 -0800

    [DevTools] Make UI.linkifyURLAsNode handle only external links.
    This will make it possible to handle clicks on them in a single place.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2503823002
    Cr-Commit-Position: refs/heads/master@{#433053}

--- third_party/WebKit/Source/devtools/front_end/audits/AuditsPanel.js
+++ third_party/WebKit/Source/devtools/front_end/audits/AuditsPanel.js
@@ -378,7 +378,7 @@ Audits.AuditRuleResult = class {
    * @return {!Element}
    */
   static linkifyDisplayName(url) {
-    return UI.linkifyURLAsNode(url, Bindings.displayNameForURL(url));
+    return Components.Linkifier.linkifyURLAsNode(url, Bindings.displayNameForURL(url));
   }
 
   /**
