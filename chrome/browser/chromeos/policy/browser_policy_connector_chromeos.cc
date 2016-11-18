6781fbfc2be49b0b37a851019b53b636ac9302e4
tnagel <tnagel@chromium.org>
2016-11-18 07:14:07 -0800

    Fix enrollment crash
    Fix a crash when hitting the "Done" button after enrollment that was
    introduced in 8d2791ecec5187ab2188b123ce50710d14f9b9e4.
    
    BUG=656558
    
    Review-Url: https://codereview.chromium.org/2515673002
    Cr-Commit-Position: refs/heads/master@{#433199}

--- chrome/browser/chromeos/policy/browser_policy_connector_chromeos.cc
+++ chrome/browser/chromeos/policy/browser_policy_connector_chromeos.cc
@@ -157,7 +157,7 @@ void BrowserPolicyConnectorChromeOS::Init(
   }
 
   DCHECK(install_attributes_);
-  if (install_attributes_->IsCloudManaged()) {
+  if (!install_attributes_->IsActiveDirectoryManaged()) {
     device_local_account_policy_service_ =
         base::MakeUnique<DeviceLocalAccountPolicyService>(
             chromeos::DBusThreadManager::Get()->GetSessionManagerClient(),
