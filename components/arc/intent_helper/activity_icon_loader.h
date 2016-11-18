5b77f998f556fdf74fa27a9d51ba065dde622f0d
kinaba <kinaba@chromium.org>
2016-11-17 19:44:10 -0800

    ARC: Fix crash on activity icon loading.
    gfx::Image has pointer-like semantics and is not thread-safe.
    The previous code did PNG encoding on blocking pool on a gfx::Image
    object that might be shared with other places, and lead to the crash.
    
    The new code does the encode only once on the initialization time,
    to avoid the racy state.
    
    BUG=661197
    TEST=manually verified file manager and browser context menu icons.
    
    Review-Url: https://codereview.chromium.org/2507403003
    Cr-Commit-Position: refs/heads/master@{#433070}

--- components/arc/intent_helper/activity_icon_loader.h
+++ components/arc/intent_helper/activity_icon_loader.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/intent_helper/arc_intent_helper_bridge.h"
 #include "ui/base/layout.h"
 #include "ui/gfx/image/image.h"
+#include "url/gurl.h"
 
 namespace arc {
 
@@ -27,9 +28,14 @@ namespace arc {
 class ActivityIconLoader : public base::RefCounted<ActivityIconLoader> {
  public:
   struct Icons {
-    Icons(const gfx::Image& icon16, const gfx::Image& icon20);
+    Icons(const gfx::Image& icon16, const gfx::Image& icon20,
+          const scoped_refptr<base::RefCountedData<GURL>>& icon16_dataurl);
+    Icons(const Icons& other);
+    ~Icons();
+
     const gfx::Image icon16;  // 16 dip
     const gfx::Image icon20;  // 20 dip
+    const scoped_refptr<base::RefCountedData<GURL>> icon16_dataurl;  // as URL
   };
 
   struct ActivityName {
@@ -78,8 +84,7 @@ class ActivityIconLoader : public base::RefCounted<ActivityIconLoader> {
 
   void OnIconsResizedForTesting(const OnIconsReadyCallback& cb,
                                 std::unique_ptr<ActivityToIconsMap> result);
-  void AddIconToCacheForTesting(const ActivityName& activity,
-                                const gfx::Image& image);
+  void AddCacheEntryForTesting(const ActivityName& activity);
 
   // Returns true if |result| indicates that the |cb| object passed to
   // GetActivityIcons() has already called.
@@ -96,8 +101,8 @@ class ActivityIconLoader : public base::RefCounted<ActivityIconLoader> {
                     const OnIconsReadyCallback& cb,
                     std::vector<mojom::ActivityIconPtr> icons);
 
-  // Resize |icons| and returns the results as ActivityToIconsMap.
-  std::unique_ptr<ActivityToIconsMap> ResizeIcons(
+  // Resizes and encodes |icons| and returns the results as ActivityToIconsMap.
+  std::unique_ptr<ActivityToIconsMap> ResizeAndEncodeIcons(
       std::vector<mojom::ActivityIconPtr> icons);
 
   // A function called when ResizeIcons finishes. Append items in |result| to
