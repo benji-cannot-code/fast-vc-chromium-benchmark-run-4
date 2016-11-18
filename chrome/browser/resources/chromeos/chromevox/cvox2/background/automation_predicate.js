0eb8670cfa19d3b939243511b1d6f6a27f062787
dtseng <dtseng@chromium.org>
2016-11-18 14:44:28 -0800

    Ensure fake alert window attached to AXAura tree and refine alert dialog output
    - in the AXAura tree, we use a fake (undrawn) window to support raw text string alerts from callers. Previously, we were not attaching the window properly as a child of desktop. This resulted in some inconsistencies in the client (extension side) tree. Fix it by always adding this window as a child of desktop
    
    - output for the permissions bubble is strange at the moment because the entire subtree is strange:
    - a window contains descendants dialog, alert dialog. The dialog itself contains duplicate text with the dialog's name. The alert dialog contains a name identical to the dialog, but contains the actual content and the allow, deny buttons.
    
    This cl doesn't change the structure, but does allow the user to descend properly into the alert dialog with linear navigation. We do this by making an alert dialog a structural container (i.e. something we descend into when navigating next).
    
    The bubble looks like:
    node id=364 role=window state={} parentID=6 childIds=[359]
     node id=359 role=dialog state={} parentID=364 childIds=[363]
      node id=363 role=dialog state={"focused":true} parentID=359 childIds=[362]
       node id=362 role=client state={} parentID=363 childIds=[367,361]
        node id=367 role=client state={} parentID=362 childIds=[368,369,358]
         node id=368 role=image state={} parentID=367 childIds=[]
         node id=369 role=staticText state={"readOnly":true} parentID=367 childIds=[]
         node id=358 role=button state={} parentID=367 childIds=[370,371]
          node id=370 role=image state={} parentID=358 childIds=[]
          node id=371 role=staticText state={"readOnly":true} parentID=358 childIds=[]
        node id=361 role=client state={} parentID=362 childIds=[360,365,366]
         node id=360 role=alertDialog state={} parentID=361 childIds=[372]
          node id=372 role=unknown state={} parentID=360 childIds=[373,374]
           node id=373 role=unknown state={} parentID=372 childIds=[375,376]
            node id=375 role=image state={} parentID=373 childIds=[]
            node id=376 role=staticText state={"readOnly":true} parentID=373 childIds=[]
           node id=374 role=unknown state={} parentID=372 childIds=[]
         node id=365 role=button state={"focusable":true} parentID=361 childIds=[377,378]
          node id=377 role=image state={} parentID=365 childIds=[]
          node id=378 role=staticText state={"readOnly":true} parentID=365 childIds=[]
         node id=366 role=button state={"focusable":true} parentID=361 childIds=[379,380]
          node id=379 role=image state={} parentID=366 childIds=[]
          node id=380 role=staticText state={"readOnly":true} parentID=366 childIds=[]
    
    TEST=visit site containing notification; ensure we get focused on that window and search+right visits all window elements.
    Use ChromeVox for a while and cause an alert (e.g. press caps lock). Ensure we don't get any errors concerning invalid nodes on deserialization and that ChromeVox only alerts once.
    
    BUG=619266
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2514583002
    Cr-Commit-Position: refs/heads/master@{#433321}

--- chrome/browser/resources/chromeos/chromevox/cvox2/background/automation_predicate.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/background/automation_predicate.js
@@ -301,8 +301,11 @@ AutomationPredicate.container = function(node) {
  * @return {boolean}
  */
 AutomationPredicate.structuralContainer = AutomationPredicate.roles([
+    Role.alertDialog,
+    Role.dialog,
     Role.rootWebArea,
     Role.webView,
+    Role.window,
     Role.embeddedObject,
     Role.iframe,
     Role.iframePresentational]);
@@ -360,7 +363,9 @@ AutomationPredicate.shouldIgnoreNode = function(node) {
                                   Role.image,
                                   Role.staticText,
                                   Role.svgRoot,
-                                  Role.tableHeaderContainer])(node));
+                                  Role.tableHeaderContainer,
+                                  Role.unknown
+                                 ])(node));
 };
 
 /**
