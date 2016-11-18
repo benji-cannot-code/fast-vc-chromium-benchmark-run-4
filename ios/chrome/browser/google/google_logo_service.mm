efffd7455ddf50f1bf8a0efeb515296a21031a7e
stkhapugin <stkhapugin@chromium.org>
2016-11-18 10:48:49 -0800

    [ObjC ARC] Converts ios/chrome/browser/google:google_logo to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2511043004
    Cr-Commit-Position: refs/heads/master@{#433257}

--- ios/chrome/browser/google/google_logo_service.mm
+++ ios/chrome/browser/google/google_logo_service.mm
@@ -19,6 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_context_getter.h"
 #include "ui/gfx/image/image.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 using search_provider_logos::Logo;
 using search_provider_logos::LogoDelegate;
 using search_provider_logos::LogoTracker;
--- ios/chrome/browser/google/google_logo_service_factory.mm
+++ ios/chrome/browser/google/google_logo_service_factory.mm
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/google/google_logo_service.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 // static
 GoogleLogoService* GoogleLogoServiceFactory::GetForBrowserState(
     ios::ChromeBrowserState* browser_state) {
