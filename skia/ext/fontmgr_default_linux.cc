51331fc2c7f88f315d70f69b58634f094dcbd6b8
steimel <steimel@chromium.org>
2016-11-18 12:56:43 -0800

    Add skia extension to allow setting default fontmgr on linux. Use it to allow the linux blimp client to use android fonts
    BUG=617821
    
    Committed: https://crrev.com/f6c1cd2bb6a2253e9547c091671c98ed28300685
    Review-Url: https://codereview.chromium.org/2500643002
    Cr-Original-Commit-Position: refs/heads/master@{#432023}
    Cr-Commit-Position: refs/heads/master@{#433292}

---
+++ skia/ext/fontmgr_default_linux.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "skia/ext/fontmgr_default_linux.h"
+
+#include "third_party/skia/include/ports/SkFontConfigInterface.h"
+#include "third_party/skia/include/ports/SkFontMgr.h"
+#include "third_party/skia/include/ports/SkFontMgr_FontConfigInterface.h"
+
+namespace {
+// This is a purposefully leaky pointer that has ownership of the FontMgr.
+SkFontMgr* g_default_fontmgr;
+}  // namespace
+
+void SetDefaultSkiaFactory(SkFontMgr* fontmgr) {
+  g_default_fontmgr = fontmgr;
+}
+
+SK_API SkFontMgr* SkFontMgr::Factory() {
+  if (g_default_fontmgr) {
+    return SkRef(g_default_fontmgr);
+  }
+  sk_sp<SkFontConfigInterface> fci(SkFontConfigInterface::RefGlobal());
+  return fci ? SkFontMgr_New_FCI(std::move(fci)) : nullptr;
+}
