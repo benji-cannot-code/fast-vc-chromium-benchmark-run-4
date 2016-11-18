26117cabaea5ef03f328f9ad7c8158402f8c3e2c
jbauman <jbauman@chromium.org>
2016-11-17 21:35:29 -0800

    Re-enable VP9 hardware decoding on new Intel drivers.
    Intel GPU drivers 21.20.16.4542 and above have a fix for the hang bug.
    
    BUG=654111,666512
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2508063002
    Cr-Commit-Position: refs/heads/master@{#433121}

--- gpu/config/software_rendering_list_json.cc
+++ gpu/config/software_rendering_list_json.cc
@@ -19,7 +19,7 @@ const char kSoftwareRenderingListJson[] = LONG_STRING_CONST(
 {
   "name": "software rendering list",
   // Please update the version number whenever you change this file.
-  "version": "12.02",
+  "version": "12.03",
   "entries": [
     {
       "id": 1,
@@ -1356,6 +1356,10 @@ LONG_STRING_CONST(
         "type": "win"
       },
       "vendor_id": "0x8086",
+      "driver_version": {
+        "op": "<",
+        "value": "21.20.16.4542"
+      },
       "features": [
         "accelerated_vpx_decode"
       ]
