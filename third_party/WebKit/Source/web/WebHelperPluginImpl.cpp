f8bf85f35bd4018b038d3434ead915d6da88c552
jrummell <jrummell@chromium.org>
2016-11-17 20:20:45 -0800

    Don't call dispose() if the WebPluginContainer wasn't created
    If the plugin fails to load, then there is nothing to dispose.
    
    BUG=654556
    
    Review-Url: https://codereview.chromium.org/2512023003
    Cr-Commit-Position: refs/heads/master@{#433096}

--- third_party/WebKit/Source/web/WebHelperPluginImpl.cpp
+++ third_party/WebKit/Source/web/WebHelperPluginImpl.cpp
@@ -80,7 +80,8 @@ bool WebHelperPluginImpl::initialize(const String& pluginType,
 }
 
 void WebHelperPluginImpl::reallyDestroy(TimerBase*) {
-  m_pluginContainer->dispose();
+  if (m_pluginContainer)
+    m_pluginContainer->dispose();
   delete this;
 }
 
