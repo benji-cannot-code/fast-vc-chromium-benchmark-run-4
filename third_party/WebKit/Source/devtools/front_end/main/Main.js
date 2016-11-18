f01ebcd16697a796cc0d0ede1a9421df833fce44
dgozman <dgozman@chromium.org>
2016-11-18 15:37:08 -0800

    [DevTools] Handle external link clicks and context menu separately.
    BUG=665661
    
    Review-Url: https://codereview.chromium.org/2512013002
    Cr-Commit-Position: refs/heads/master@{#433331}

--- third_party/WebKit/Source/devtools/front_end/main/Main.js
+++ third_party/WebKit/Source/devtools/front_end/main/Main.js
@@ -392,10 +392,7 @@ Main.Main = class {
       InspectorFrontendHost.openInNewTab(anchor.href);
     }
 
-    if (!anchor.classList.contains('webkit-html-external-link'))
-      followLink();
-    else
-      InspectorFrontendHost.openInNewTab(anchor.href);
+    followLink();
   }
 
   _registerShortcuts() {
