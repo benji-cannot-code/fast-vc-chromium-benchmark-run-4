ef9aa31500d9af3fcffa6a98d23000a7d729b7d8
jbudorick <jbudorick@chromium.org>
2016-11-17 18:12:19 -0800

    Revert of [android] Stop using isolate.py for data dependency management. (patchset #9 id:160001 of https://codereview.chromium.org/2492123002/ )
    Reason for revert:
    Breaks the blink bot, e.g. https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Android%20%28Nexus4%29/builds/57103
    
    Original issue's description:
    > [android] Stop using isolate.py for data dependency management.
    >
    > BUG=663110
    >
    > Committed: https://crrev.com/634c87cf4af7ac11f2dbe7687a6d7904c2217ee3
    > Cr-Commit-Position: refs/heads/master@{#432940}
    
    TBR=agrieve@chromium.org,dpranke@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=663110
    
    Review-Url: https://codereview.chromium.org/2514453003
    Cr-Commit-Position: refs/heads/master@{#433020}

--- build/android/pylib/gtest/gtest_test_instance.py
+++ build/android/pylib/gtest/gtest_test_instance.py
@@ -15,6 +15,7 @@ from pylib import constants
 from pylib.constants import host_paths
 from pylib.base import base_test_result
 from pylib.base import test_instance
+from pylib.utils import isolator
 
 with host_paths.SysPath(host_paths.BUILD_COMMON_PATH):
   import unittest_util # pylint: disable=import-error
@@ -230,7 +231,7 @@ def ConvertTestFilterFileIntoGTestFilterArgument(input_lines):
 
 class GtestTestInstance(test_instance.TestInstance):
 
-  def __init__(self, args, data_deps_delegate, error_func):
+  def __init__(self, args, isolate_delegate, error_func):
     super(GtestTestInstance, self).__init__()
     # TODO(jbudorick): Support multiple test suites.
     if len(args.suite_name) > 1:
@@ -287,10 +288,16 @@ class GtestTestInstance(test_instance.TestInstance):
     else:
       self._gtest_filter = None
 
-    self._data_deps_delegate = data_deps_delegate
-    self._runtime_deps_path = args.runtime_deps_path
-    if not self._runtime_deps_path:
-      logging.warning('No data dependencies will be pushed.')
+    if (args.isolate_file_path and
+        not isolator.IsIsolateEmpty(args.isolate_file_path)):
+      self._isolate_abs_path = os.path.abspath(args.isolate_file_path)
+      self._isolate_delegate = isolate_delegate
+      self._isolated_abs_path = os.path.join(
+          constants.GetOutDirectory(), '%s.isolated' % self._suite)
+    else:
+      logging.warning('%s isolate file provided. No data deps will be pushed.',
+                      'Empty' if args.isolate_file_path else 'No')
+      self._isolate_delegate = None
 
     if args.app_data_files:
       self._app_data_files = args.app_data_files
@@ -387,8 +394,15 @@ class GtestTestInstance(test_instance.TestInstance):
   #override
   def SetUp(self):
     """Map data dependencies via isolate."""
-    self._data_deps.extend(
-        self._data_deps_delegate(self._runtime_deps_path))
+    if self._isolate_delegate:
+      self._isolate_delegate.Remap(
+          self._isolate_abs_path, self._isolated_abs_path)
+      self._isolate_delegate.PurgeExcluded(_DEPS_EXCLUSION_LIST)
+      self._isolate_delegate.MoveOutputDeps()
+      dest_dir = None
+      self._data_deps.extend([
+          (self._isolate_delegate.isolate_deps_dir, dest_dir)])
+
 
   def GetDataDependencies(self):
     """Returns the test suite's data dependencies.
@@ -442,6 +456,7 @@ class GtestTestInstance(test_instance.TestInstance):
 
   #override
   def TearDown(self):
-    """Do nothing."""
-    pass
+    """Clear the mappings created by SetUp."""
+    if self._isolate_delegate:
+      self._isolate_delegate.Clear()
 
