ecfb745453de66a404b1178113c141f1167f7abd
treib <treib@chromium.org>
2016-11-18 00:56:48 -0800

    ExtensionInstallUIDefault cleanup: Remove check for InstantExtended API
    InstantExtended has been enabled for years (on desktop), and anyway checking it here doesn't make any sense
    
    BUG=627747
    
    Review-Url: https://codereview.chromium.org/2511143003
    Cr-Commit-Position: refs/heads/master@{#433153}

--- chrome/browser/ui/extensions/extension_install_ui_default.cc
+++ chrome/browser/ui/extensions/extension_install_ui_default.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "components/infobars/core/confirm_infobar_delegate.h"
 #include "components/infobars/core/infobar.h"
-#include "components/search/search.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/web_contents.h"
@@ -230,11 +229,8 @@ void ExtensionInstallUIDefault::OpenAppInstalledUI(const std::string& app_id) {
   Profile* current_profile = profile_->GetOriginalProfile();
   Browser* browser = FindOrCreateVisibleBrowser(current_profile);
   if (browser) {
-    GURL url(search::IsInstantExtendedAPIEnabled()
-                 ? chrome::kChromeUIAppsURL
-                 : chrome::kChromeUINewTabURL);
-    chrome::NavigateParams params(
-        chrome::GetSingletonTabNavigateParams(browser, url));
+    chrome::NavigateParams params(chrome::GetSingletonTabNavigateParams(
+        browser, GURL(chrome::kChromeUIAppsURL)));
     chrome::Navigate(&params);
 
     content::NotificationService::current()->Notify(
