66198e9edcba94e19fb3c95a4c4eaa1903e18a72
msarda <msarda@chromium.org>
2016-11-18 03:27:41 -0800

    Update the sign-in email confirmation dialog.
    This CL uses WebUI to diaply the new sign-in email confirmation dialog.
    
    Screenshot:
    https://screenshot.googleplex.com/QDLbyYtsn8a
    
    BUG=661307
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2485613002
    Cr-Commit-Position: refs/heads/master@{#433177}

--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -166,6 +166,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/sync/sync_promo_ui.h"
 #include "chrome/browser/ui/webui/signin/inline_login_ui.h"
 #include "chrome/browser/ui/webui/signin/md_user_manager_ui.h"
+#include "chrome/browser/ui/webui/signin/signin_email_confirmation_ui.h"
 #include "chrome/browser/ui/webui/signin/signin_error_ui.h"
 #include "chrome/browser/ui/webui/signin/sync_confirmation_ui.h"
 #include "chrome/browser/ui/webui/welcome_ui.h"
@@ -536,6 +537,8 @@ WebUIFactoryFunction GetWebUIFactoryFunction(WebUI* web_ui,
     return &NewWebUI<SigninErrorUI>;
   if (url.host_piece() == chrome::kChromeUISyncConfirmationHost)
     return &NewWebUI<SyncConfirmationUI>;
+  if (url.host_piece() == chrome::kChromeUISigninEmailConfirmationHost)
+    return &NewWebUI<SigninEmailConfirmationUI>;
   if (url.host_piece() == chrome::kChromeUIProfileSigninConfirmationHost)
     return &NewWebUI<ProfileSigninConfirmationUI>;
   if (url.host_piece() == chrome::kChromeUIWelcomeHost)
