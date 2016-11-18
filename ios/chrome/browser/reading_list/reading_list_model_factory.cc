6c3fc40ba9cce014269956245bc9ef09f9a2c7c7
mgiuca <mgiuca@chromium.org>
2016-11-17 18:39:29 -0800

    Revert of Fix nullptr exception on ios_internal (patchset #1 id:1 of https://codereview.chromium.org/2512653003/ )
    Reason for revert:
    This CL depends upon r432845 which has also been reverted.
    
    Please re-land this after re-landing r432845.
    
    Original issue's description:
    > Fix nullptr exception on ios_internal
    >
    > BUG=661668
    >
    > Committed: https://crrev.com/2a006f7420305db8a069bcc747d4bc343e47f702
    > Cr-Commit-Position: refs/heads/master@{#432860}
    
    TBR=jif@chromium.org,jif@google.com,olivierrobin@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=661668
    
    Review-Url: https://codereview.chromium.org/2506183004
    Cr-Commit-Position: refs/heads/master@{#433021}

--- ios/chrome/browser/reading_list/reading_list_model_factory.cc
+++ ios/chrome/browser/reading_list/reading_list_model_factory.cc
@@ -18,11 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/reading_list/reading_list_pref_names.h"
 
 // static
-bool ReadingListModelFactory::IsReadingListEnabled() {
-  return experimental_flags::IsReadingListEnabled();
-}
-
-// static
 ReadingListModel* ReadingListModelFactory::GetForBrowserState(
     ios::ChromeBrowserState* browser_state) {
   return static_cast<ReadingListModelImpl*>(
