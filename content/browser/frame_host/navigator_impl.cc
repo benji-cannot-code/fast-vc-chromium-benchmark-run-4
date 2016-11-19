e1a9777c094f3cbbe05eeee3401601b99fcbdd0b
csharrison <csharrison@chromium.org>
2016-11-19 15:57:12 -0800

    Kill vestigial transfer requests
    When requests are marked as transferring, we mark them as protected in
    the ResourceDispatcherHostImpl, so when their RFH goes away we won't
    lose the request.
    
    However, there are cases where the transfer can fail, and the navigation
    abandoned, while the request on the IO thread is still marked as
    protected.
    
    This patch catches that case by checking for transferring navigation
    handles in ~RFH, and cancelling them there.
    
    Browser test written by lukasza@chromium.org
    
    BUG=657195
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2508973006
    Cr-Commit-Position: refs/heads/master@{#433430}

--- content/browser/frame_host/navigator_impl.cc
+++ content/browser/frame_host/navigator_impl.cc
@@ -436,6 +436,8 @@ bool NavigatorImpl::NavigateToEntry(
               controller_->GetIndexOfEntry(&entry),
               controller_->GetLastCommittedEntryIndex(),
               controller_->GetEntryCount()));
+    } else {
+      dest_render_frame_host->navigation_handle()->set_is_transferring(false);
     }
   }
 
