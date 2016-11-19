5841e5f7272023fa089c355807c7771f63ff10ed
kmarshall <kmarshall@chromium.org>
2016-11-18 17:35:26 -0800

    Throw IPC errors on invalid Mojo BlobChannel messages. This code instructs the browser to kill a renderer process if it receives semantically invalid/bad data over the Mojo pipe.
    BUG=629096
    R=mlliu@chromium.org
    CC=wez@chromium.org
    
    Review-Url: https://codereview.chromium.org/2517733002
    Cr-Commit-Position: refs/heads/master@{#433371}

--- blimp/engine/mojo/blob_channel_service.cc
+++ blimp/engine/mojo/blob_channel_service.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task_runner_util.h"
 #include "base/threading/thread.h"
 #include "blimp/net/blob_channel/blob_channel_sender.h"
+#include "mojo/public/cpp/bindings/message.h"
 #include "mojo/public/cpp/system/buffer.h"
 
 namespace blimp {
@@ -74,12 +75,13 @@ void BlobChannelService::PutBlob(const std::string& id,
 
   // Map |data| into the address space and copy out its contents.
   if (!data.is_valid()) {
-    LOG(ERROR) << "Invalid data handle received from renderer process.";
+    mojo::ReportBadMessage(
+        "Invalid data handle received from renderer process.");
     return;
   }
 
   if (size > kMaxBlobSizeBytes) {
-    LOG(ERROR) << "Blob size too big: " << size;
+    mojo::ReportBadMessage("Blob size too large.");
     return;
   }
 
