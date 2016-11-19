ce22f2db2d1495bd3734824169b2f356800cbf43
asvitkine <asvitkine@chromium.org>
2016-11-18 16:25:13 -0800

    Fix TSAN errors with reload_cache_control_browsertest.cc.
    Initializing the feature list in the ctor makes sure its
    initialized before threads get started that query its
    value.
    
    BUG=663850
    
    Review-Url: https://codereview.chromium.org/2516783002
    Cr-Commit-Position: refs/heads/master@{#433347}

--- content/browser/loader/reload_cache_control_browsertest.cc
+++ content/browser/loader/reload_cache_control_browsertest.cc
@@ -40,15 +40,15 @@ struct RequestLog {
 
 class ReloadCacheControlBrowserTest : public ContentBrowserTest {
  protected:
-  ReloadCacheControlBrowserTest() = default;
-  ~ReloadCacheControlBrowserTest() override = default;
-
-  void SetUpOnMainThread() override {
+  ReloadCacheControlBrowserTest() {
     // TODO(toyoshim): Tests in this file depend on current reload behavior,
     // and should be modified when we enable the new reload behavior.
     scoped_feature_list_.InitAndDisableFeature(
         features::kNonValidatingReloadOnNormalReload);
+  }
+  ~ReloadCacheControlBrowserTest() override = default;
 
+  void SetUpOnMainThread() override {
     SetUpTestServerOnMainThread();
   }
 
@@ -86,13 +86,13 @@ class ReloadCacheControlBrowserTest : public ContentBrowserTest {
 class ReloadCacheControlWithAnExperimentBrowserTest
     : public ReloadCacheControlBrowserTest {
  protected:
-  ReloadCacheControlWithAnExperimentBrowserTest() = default;
+  ReloadCacheControlWithAnExperimentBrowserTest() {
+    scoped_feature_list_.InitAndEnableFeature(
+        features::kNonValidatingReloadOnNormalReload);
+  }
   ~ReloadCacheControlWithAnExperimentBrowserTest() override = default;
 
   void SetUpOnMainThread() override {
-    scoped_feature_list_.InitAndEnableFeature(
-        features::kNonValidatingReloadOnNormalReload);
-
     SetUpTestServerOnMainThread();
   }
 
