6a560bf0cc35eb355e46f1bff0073b40ce2ba0b5
ianwen <ianwen@chromium.org>
2016-11-18 16:57:55 -0800

    [Android] Add GTEST for special locale handler
    Special locale handler manipulates the template url service. This CL
    adds unit tests for it.
    
    BUG=638062
    
    Review-Url: https://codereview.chromium.org/2510723003
    Cr-Commit-Position: refs/heads/master@{#433360}

--- chrome/browser/android/locale/special_locale_handler.h
+++ chrome/browser/android/locale/special_locale_handler.h
@@ -15,10 +15,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using base::android::JavaParamRef;
 
 class TemplateURLService;
+class Profile;
 
 class SpecialLocaleHandler {
  public:
-  explicit SpecialLocaleHandler(const std::string& locale);
+  explicit SpecialLocaleHandler(Profile* profile,
+                                const std::string& locale,
+                                TemplateURLService* service);
   void Destroy(JNIEnv* env, const JavaParamRef<jobject>& obj);
   jboolean LoadTemplateUrls(JNIEnv* env, const JavaParamRef<jobject>& obj);
   void RemoveTemplateUrls(JNIEnv* env, const JavaParamRef<jobject>& obj);
@@ -26,8 +29,15 @@ class SpecialLocaleHandler {
                                      const JavaParamRef<jobject>& obj);
   void SetGoogleAsDefaultSearch(JNIEnv* env, const JavaParamRef<jobject>& obj);
 
- private:
   virtual ~SpecialLocaleHandler();
+
+ protected:
+  virtual std::vector<std::unique_ptr<TemplateURLData>>
+    GetLocalPrepopulatedEngines(Profile* profile);
+  virtual int GetDesignatedSearchEngine();
+
+ private:
+  Profile* profile_;
 
   std::string locale_;
 
