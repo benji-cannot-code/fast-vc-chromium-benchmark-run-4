164b45d836c52b25166691842e4c5cbf1ab5650b
mbrunson <mbrunson@chromium.org>
2016-11-18 17:08:29 -0800

    bluetooth: Basic browser tests for chrome://bluetooth-internals.
    UI tests of device management chrome://bluetooth-internals page
      including device creation, device changes, and device removal.
    
    BUG=651282
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2428773005
    Cr-Commit-Position: refs/heads/master@{#433365}

--- chrome/browser/resources/bluetooth_internals/bluetooth_internals.js
+++ chrome/browser/resources/bluetooth_internals/bluetooth_internals.js
@@ -8,13 +8,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *     chrome://bluetooth-internals/.
  */
 
+// Expose for testing.
+var adapterBroker = null;
+var devices = null;
+
 cr.define('bluetooth_internals', function() {
 
   /** @type {!Map<string, !interfaces.BluetoothDevice.Device.proxyClass>} */
   var deviceAddressToProxy = new Map();
 
   function initializeViews() {
-    var adapterBroker = null;
     adapter_broker.getAdapterBroker()
       .then(function(broker) { adapterBroker = broker; })
       .then(function() { return adapterBroker.getInfo(); })
@@ -22,7 +25,7 @@ cr.define('bluetooth_internals', function() {
       .then(function() { return adapterBroker.getDevices(); })
       .then(function(response) {
         // Hook up device collection events.
-        var devices = new device_collection.DeviceCollection([]);
+        devices = new device_collection.DeviceCollection([]);
         adapterBroker.addEventListener('deviceadded', function(event) {
           devices.addOrUpdate(event.detail.deviceInfo);
         });
@@ -81,6 +84,8 @@ cr.define('bluetooth_internals', function() {
         });
 
         deviceTable.setDevices(devices);
+        deviceTable.id = 'device-table';
+
         document.body.appendChild(deviceTable);
       })
       .catch(function(error) { console.error(error); });
@@ -89,7 +94,6 @@ cr.define('bluetooth_internals', function() {
   return {
     initializeViews: initializeViews
   };
-
 });
 
 document.addEventListener(
