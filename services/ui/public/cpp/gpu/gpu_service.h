f53ac22905d2d89d4cc4ec8828eaa6cc4a5d06a3
sadrul <sadrul@chromium.org>
2016-11-19 21:35:33 -0800

    mus-gpu: Remove some unnecessary headers.
    BUG=none
    TBR=sky@ since trivial changes.
    
    Review-Url: https://codereview.chromium.org/2516843002
    Cr-Commit-Position: refs/heads/master@{#433447}

--- services/ui/public/cpp/gpu/gpu_service.h
+++ services/ui/public/cpp/gpu/gpu_service.h
@@ -12,11 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/single_thread_task_runner.h"
-#include "base/synchronization/lock.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/threading/thread.h"
 #include "gpu/ipc/client/gpu_channel_host.h"
-#include "gpu/ipc/common/gpu_info.mojom.h"
 #include "services/ui/public/cpp/gpu/mojo_gpu_memory_buffer_manager.h"
 #include "services/ui/public/interfaces/gpu_service.mojom.h"
 
