46d407c55d61313c856950f573f9f6ee5c9a4daa
stkhapugin <stkhapugin@chromium.org>
2016-11-21 08:01:03 -0800

    [ObjC ARC] Converts ios/chrome/browser/ui/context_menu:context_menu to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2512003003
    Cr-Commit-Position: refs/heads/master@{#433553}

--- ios/chrome/browser/ui/context_menu/context_menu_coordinator.mm
+++ ios/chrome/browser/ui/context_menu/context_menu_coordinator.mm
@@ -13,6 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/strings/grit/ui_strings.h"
 #import "ios/web/public/web_state/context_menu_params.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 @interface ContextMenuCoordinator () {
   // Coordinator handling the alert.
   base::scoped_nsobject<ActionSheetCoordinator> _alertCoordinator;
