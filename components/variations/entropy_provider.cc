62f01b040e5aa11dd54ccd9007ca35a0a10d8c8a
jwd <jwd@chromium.org>
2016-11-21 08:36:54 -0800

    Making the SHA1 entropy provider use the randomization_seed.
    BUG=665603
    
    Review-Url: https://codereview.chromium.org/2503683005
    Cr-Commit-Position: refs/heads/master@{#433561}

--- components/variations/entropy_provider.cc
+++ components/variations/entropy_provider.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/rand_util.h"
 #include "base/sha1.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/sys_byteorder.h"
 #include "components/variations/metrics_util.h"
 
@@ -88,7 +89,10 @@ double SHA1EntropyProvider::GetEntropyForTrial(
   // it has been observed that this method does not result in a uniform
   // distribution given the same |trial_name|. When using such a low entropy
   // source, PermutedEntropyProvider should be used instead.
-  std::string input(entropy_source_ + trial_name);
+  std::string input(entropy_source_);
+  input.append(randomization_seed == 0 ? trial_name : base::UintToString(
+                                                          randomization_seed));
+
   unsigned char sha1_hash[base::kSHA1Length];
   base::SHA1HashBytes(reinterpret_cast<const unsigned char*>(input.c_str()),
                       input.size(),
