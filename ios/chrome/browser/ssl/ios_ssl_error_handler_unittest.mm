dd388d9ae4ba383f8228091b4bea7c76d7fe5c46
stkhapugin <stkhapugin@chromium.org>
2016-11-21 05:55:54 -0800

    [ObjC ARC] Converts ios/chrome/browser/ssl:unit_tests to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2514833002
    Cr-Commit-Position: refs/heads/master@{#433527}

--- ios/chrome/browser/ssl/ios_ssl_error_handler_unittest.mm
+++ ios/chrome/browser/ssl/ios_ssl_error_handler_unittest.mm
@@ -14,6 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/test/cert_test_util.h"
 #include "net/test/test_data_directory.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace {
 const char kTestCertFileName[] = "ok_cert.pem";
 const char kTestHostName[] = "https://chromium.test/";
