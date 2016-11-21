d57309ca7fc4f7686ca0cb3cb2c49a773361a2cf
stkhapugin <stkhapugin@chromium.org>
2016-11-21 06:26:31 -0800

    [ObjC ARC] Converts ios/chrome/browser/ui/keyboard:unit_tests to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2514863002
    Cr-Commit-Position: refs/heads/master@{#433535}

--- ios/chrome/browser/ui/keyboard/UIKeyCommand+ChromeTest.mm
+++ ios/chrome/browser/ui/keyboard/UIKeyCommand+ChromeTest.mm
@@ -8,6 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/scoped_nsobject.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace {
 
 // Tests that UIApplication correctly calls the keyboard command action block
