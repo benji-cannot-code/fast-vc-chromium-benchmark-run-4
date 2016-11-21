02d74ed066275c49758f794cfe25c2ad32c4bcfc
fdoray <fdoray@chromium.org>
2016-11-21 12:46:04 -0800

    Disallow posting tasks to SequencedWorkerPools by default.
    We have observed crash reports in which SequencedWorkerPool workers
    were active while redirection to TaskScheduler was enabled. This can
    happen if a task is posted to a SequencedWorkerPool before redirection
    to TaskScheduler is enabled. With this CL, DumpWithoutCrashing() is
    called if a task is posted to a SequencedWorkerPool too early. This
    will help us find offending call sites.
    
    A commented DCHECK is also added to find offending call sites. It
    will be uncommented in a separate CL to avoid a revert of this CL
    in case it fails on the waterfall.
    
    BUG=622400
    
    Committed: https://crrev.com/4606ada249a240e115a08a7c66409e8fdd457ea1
    Committed: https://crrev.com/8fd22217395d50b6704109c7f1d4deff59f9a109
    Review-Url: https://codereview.chromium.org/2445763002
    Cr-Original-Original-Commit-Position: refs/heads/master@{#432913}
    Cr-Original-Commit-Position: refs/heads/master@{#433237}
    Cr-Commit-Position: refs/heads/master@{#433639}

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_split.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/threading/sequenced_worker_pool.h"
 #include "base/threading/simple_thread.h"
 #include "base/threading/thread_local.h"
 #include "base/threading/thread_restrictions.h"
@@ -904,6 +905,13 @@ void RenderThreadImpl::Init(
   is_renderer_suspended_ = false;
 
   base::MemoryCoordinatorClientRegistry::GetInstance()->Register(this);
+
+  // If this renderer doesn't run inside the browser process, enable
+  // SequencedWorkerPool. Otherwise, it should already have been enabled.
+  // TODO(fdoray): Remove this once the SequencedWorkerPool to TaskScheduler
+  // redirection experiment concludes https://crbug.com/622400.
+  if (!command_line.HasSwitch(switches::kSingleProcess))
+    base::SequencedWorkerPool::EnableForProcess();
 }
 
 RenderThreadImpl::~RenderThreadImpl() {
