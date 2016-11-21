062290004ece828123810e84c2506e0c8ade854b
vitaliii <vitaliii@chromium.org>
2016-11-21 02:50:33 -0800

    [NTP] Cleanup: add brackets in components/ntp_snippets. [1/2]
    This CL adds curly brackets to |if|, |for| and |while| in
    components/ntp_snippets. The search was done using
    "((if)|(for)|(while)) \([^{]*?\n" regexp. Note that it matches
    multiline statements and this was intended, since there were
    not many such cases and I wanted to confirm that we did not
    miss brackets there.
    
    BUG=667202
    
    Review-Url: https://codereview.chromium.org/2520843002
    Cr-Commit-Position: refs/heads/master@{#433505}

--- components/ntp_snippets/bookmarks/bookmark_suggestions_provider.cc
+++ components/ntp_snippets/bookmarks/bookmark_suggestions_provider.cc
@@ -218,8 +218,9 @@ void BookmarkSuggestionsProvider::GetDismissedSuggestionsForDebugging(
 void BookmarkSuggestionsProvider::ClearDismissedSuggestionsForDebugging(
     Category category) {
   DCHECK_EQ(category, provided_category_);
-  if (!bookmark_model_->loaded())
+  if (!bookmark_model_->loaded()) {
     return;
+  }
   MarkAllBookmarksUndismissed(bookmark_model_);
 }
 
@@ -349,16 +350,18 @@ void BookmarkSuggestionsProvider::FetchBookmarksInternal() {
 }
 
 void BookmarkSuggestionsProvider::FetchBookmarks() {
-  if (bookmark_model_->loaded())
+  if (bookmark_model_->loaded()) {
     FetchBookmarksInternal();
-  else
+  } else {
     fetch_requested_ = true;
+  }
 }
 
 void BookmarkSuggestionsProvider::NotifyStatusChanged(
     CategoryStatus new_status) {
-  if (category_status_ == new_status)
+  if (category_status_ == new_status) {
     return;
+  }
   category_status_ = new_status;
   observer()->OnCategoryStatusChanged(this, provided_category_, new_status);
 }
