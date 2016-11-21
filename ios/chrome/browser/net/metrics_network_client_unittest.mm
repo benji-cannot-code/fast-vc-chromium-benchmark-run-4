bcd780a5a92a2461354afbd1728f673f769b74b2
stkhapugin <stkhapugin@chromium.org>
2016-11-21 05:18:32 -0800

    [ObjC ARC] Converts ios/chrome/browser/net:unit_tests to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2517633002
    Cr-Commit-Position: refs/heads/master@{#433519}

--- ios/chrome/browser/net/metrics_network_client_unittest.mm
+++ ios/chrome/browser/net/metrics_network_client_unittest.mm
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_errors.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 // Dummy client to be registered as underlying client for the
 // MetricsNetworkClient.
 @interface MetricsMockClient : CRNForwardingNetworkClient
