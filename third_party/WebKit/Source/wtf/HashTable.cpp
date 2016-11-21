04e579028f359995243b2e53157c410cb8767d79
sigbjornf <sigbjornf@opera.com>
2016-11-21 00:07:31 -0800

    HashTable: bring per-table stats tracking back to life.
    Recording per-hash table stats (DUMP_HASHTABLE_STATS_PER_TABLE)
    broke with the introduction of Oilpan, as the feature depended
    on finalizable HashTable<>s, something Oilpan heap hash tables
    are not.
    
    If the hash table resides on the Oilpan heap, arrange for the
    stats object to also reside there.
    
    While here, also unify the handling of global HashTable stats
    recording and the per-table representation.
    
    R=
    BUG=
    
    Review-Url: https://codereview.chromium.org/2511983003
    Cr-Commit-Position: refs/heads/master@{#433494}

--- third_party/WebKit/Source/wtf/HashTable.cpp
+++ third_party/WebKit/Source/wtf/HashTable.cpp
@@ -27,29 +27,47 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WTF {
 
-int HashTableStats::numAccesses;
-int HashTableStats::numCollisions;
-int HashTableStats::collisionGraph[4096];
-int HashTableStats::maxCollisions;
-int HashTableStats::numRehashes;
-int HashTableStats::numRemoves;
-int HashTableStats::numReinserts;
-
 static Mutex& hashTableStatsMutex() {
   DEFINE_THREAD_SAFE_STATIC_LOCAL(Mutex, mutex, new Mutex);
   return mutex;
 }
 
+HashTableStats& HashTableStats::instance() {
+  DEFINE_THREAD_SAFE_STATIC_LOCAL(HashTableStats, stats, new HashTableStats);
+  return stats;
+}
+
+void HashTableStats::copy(const HashTableStats* other) {
+  numAccesses = other->numAccesses;
+  numRehashes = other->numRehashes;
+  numRemoves = other->numRemoves;
+  numReinserts = other->numReinserts;
+
+  maxCollisions = other->maxCollisions;
+  numCollisions = other->numCollisions;
+  memcpy(collisionGraph, other->collisionGraph, sizeof(collisionGraph));
+}
+
 void HashTableStats::recordCollisionAtCount(int count) {
-  MutexLocker lock(hashTableStatsMutex());
+  // The global hash table singleton needs to be atomically updated.
+  bool isGlobalSingleton = this == &instance();
+  if (isGlobalSingleton)
+    hashTableStatsMutex().lock();
+
   if (count > maxCollisions)
     maxCollisions = count;
   numCollisions++;
   collisionGraph[count]++;
+
+  if (isGlobalSingleton)
+    hashTableStatsMutex().unlock();
 }
 
 void HashTableStats::dumpStats() {
-  MutexLocker lock(hashTableStatsMutex());
+  // Lock the global hash table singleton while dumping.
+  bool isGlobalSingleton = this == &instance();
+  if (isGlobalSingleton)
+    hashTableStatsMutex().lock();
 
   dataLogF("\nWTF::HashTable statistics\n\n");
   dataLogF("%d accesses\n", numAccesses);
@@ -66,6 +84,9 @@ void HashTableStats::dumpStats() {
   }
   dataLogF("%d rehashes\n", numRehashes);
   dataLogF("%d reinserts\n", numReinserts);
+
+  if (isGlobalSingleton)
+    hashTableStatsMutex().unlock();
 }
 
 }  // namespace WTF
