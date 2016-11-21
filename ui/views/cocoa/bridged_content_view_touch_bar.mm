106b6b06bab2f359485b613b5e02647e992c55a2
sdy <sdy@chromium.org>
2016-11-21 08:33:06 -0800

    [Mac] Switch from NSObject categories to forward declarations for Touch Bar support.
    This fixes a crash when -[BridgedContentView touchBar:] calls
    -[NSGroupTouchBarItem groupItemWithIdentifier:items:], because
    NSGroupTouchBarItem was typedef'd to NSObject.
    
    BUG=666893
    
    Review-Url: https://codereview.chromium.org/2519563002
    Cr-Commit-Position: refs/heads/master@{#433558}

--- ui/views/cocoa/bridged_content_view_touch_bar.mm
+++ ui/views/cocoa/bridged_content_view_touch_bar.mm
@@ -49,7 +49,8 @@ NSString* const kTouchBarCancelId = @"com.google.chrome-CANCEL";
 // NSTouchBarDelegate protocol implementation.
 
 - (NSTouchBarItem*)touchBar:(NSTouchBar*)touchBar
-      makeItemForIdentifier:(NSTouchBarItemIdentifier)identifier {
+      makeItemForIdentifier:(NSTouchBarItemIdentifier)identifier
+    NS_AVAILABLE_MAC(10_12_1) {
   if (!hostedView_)
     return nil;
 
@@ -62,7 +63,9 @@ NSString* const kTouchBarCancelId = @"com.google.chrome-CANCEL";
     }
     if ([items count] == 0)
       return nil;
-    return [NSGroupTouchBarItem groupItemWithIdentifier:identifier items:items];
+    return [NSClassFromString(@"NSGroupTouchBarItem")
+        groupItemWithIdentifier:identifier
+                          items:items];
   }
 
   ui::DialogButton type = ui::DIALOG_BUTTON_NONE;
@@ -102,7 +105,7 @@ NSString* const kTouchBarCancelId = @"com.google.chrome-CANCEL";
 
 // NSTouchBarProvider protocol implementation (via NSResponder category).
 
-- (NSTouchBar*)makeTouchBar {
+- (NSTouchBar*)makeTouchBar NS_AVAILABLE_MAC(10_12_1) {
   if (!hostedView_)
     return nil;
 
