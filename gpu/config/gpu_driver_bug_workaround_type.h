6ca50ac2b733d575bed1e858005740173934fece
kainino <kainino@chromium.org>
2016-11-21 15:55:28 -0800

    Revert workaround due to perf regression
    Revert "Work around unknown Mac Intel driver bug causing non-deterministic behavior in GPU readback from 2D canvas" ( https://crrev.com/2503283007 )
    
    Revert "clarify description for force_software_readback_from_2d_canvas" ( https://crrev.com/2505363004 )
    
    BUG=667352,665656
    TBR=kbr@chromium.org,esprehn@chromium.org,piman@chromium.org,zmo@chromium.org,tsepez@chromium.org,ben@chromium.org
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2515413002
    Cr-Commit-Position: refs/heads/master@{#433704}

--- gpu/config/gpu_driver_bug_workaround_type.h
+++ gpu/config/gpu_driver_bug_workaround_type.h
@@ -104,8 +104,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
          force_discrete_gpu)                                 \
   GPU_OP(FORCE_INTEGRATED_GPU,                               \
          force_integrated_gpu)                               \
-  GPU_OP(FORCE_SOFTWARE_READBACK_FROM_2D_CANVAS,             \
-         force_software_readback_from_2d_canvas)             \
   GPU_OP(GET_FRAG_DATA_INFO_BUG,                             \
          get_frag_data_info_bug)                             \
   GPU_OP(GL_CLEAR_BROKEN,                                    \
--- third_party/WebKit/Source/web/WebSettingsImpl.h
+++ third_party/WebKit/Source/web/WebSettingsImpl.h
@@ -66,7 +66,6 @@ class WEB_EXPORT WebSettingsImpl final
   void setAlwaysShowContextMenuOnTouch(bool) override;
   void setAntialiased2dCanvasEnabled(bool) override;
   void setAntialiasedClips2dCanvasEnabled(bool) override;
-  void setForceSoftwareReadbackFrom2DCanvas(bool) override;
   void setAutoplayExperimentMode(const WebString&) override;
   void setAutoZoomFocusedNodeToLegibleScale(bool) override;
   void setBrowserSideNavigationEnabled(bool) override;
