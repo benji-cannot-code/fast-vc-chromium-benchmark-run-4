eba0f01dd3d6bd249ba7a7846f33dac94e5a3db9
stkhapugin <stkhapugin@chromium.org>
2016-11-21 06:42:27 -0800

    [ObjC ARC] Converts ios/chrome/browser/sessions:sessions to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2512993002
    Cr-Commit-Position: refs/heads/master@{#433541}

--- ios/chrome/browser/sessions/ios_chrome_session_tab_helper.mm
+++ ios/chrome/browser/sessions/ios_chrome_session_tab_helper.mm
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ios/chrome/browser/sessions/ios_chrome_session_tab_helper.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 DEFINE_WEB_STATE_USER_DATA_KEY(IOSChromeSessionTabHelper);
 
 IOSChromeSessionTabHelper::IOSChromeSessionTabHelper(web::WebState* web_state) {
--- ios/chrome/browser/sessions/ios_chrome_tab_restore_service_client.mm
+++ ios/chrome/browser/sessions/ios_chrome_tab_restore_service_client.mm
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/web/public/web_thread.h"
 #include "url/gurl.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 IOSChromeTabRestoreServiceClient::IOSChromeTabRestoreServiceClient(
     ios::ChromeBrowserState* browser_state)
     : browser_state_(browser_state) {}
