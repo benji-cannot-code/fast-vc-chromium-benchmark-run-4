d6ce37ee952a0dcdd20134b94812e53936d69762
thomasanderson <thomasanderson@google.com>
2016-11-21 11:27:56 -0800

    Add check_build_deps step to gclient runhooks
    This CL adds a build hook to ensure that any necessary build
    dependencies are installed before proceeding with the
    build.  A failure would indicate install_build_deps.sh
    needs to be rerun.
    
    BUG=666937
    R=dpranke@chromium.org
    
    Review-Url: https://codereview.chromium.org/2505413003
    Cr-Commit-Position: refs/heads/master@{#433603}

---
+++ build/check-build-deps.py
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Checks that the host environment has the necessary packages installed to
+build chrome.
+"""
+
+import subprocess
+import sys
+
+if sys.platform.startswith('linux'):
+  if subprocess.call(['src/build/install-build-deps.sh', '--quick-check']):
+    print ("Missing build dependencies. "
+           "Try running 'build/install-build-deps.sh'")
+    exit(1)
