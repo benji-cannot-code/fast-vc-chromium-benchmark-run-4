ad7fb0e3da640b1c52db12896985cf3000039aa2
stkhapugin <stkhapugin@chromium.org>
2016-11-21 04:59:28 -0800

    [ObjC ARC] Converts ios/chrome/browser/share_extension:share_extension to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2519433002
    Cr-Commit-Position: refs/heads/master@{#433516}

--- ios/chrome/browser/share_extension/share_extension_item_receiver.mm
+++ ios/chrome/browser/share_extension/share_extension_item_receiver.mm
@@ -24,6 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "net/base/mac/url_conversions.h"
 #include "url/gurl.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace {
 // Enum used to send metrics on item reception.
 // If you change this enum, update histograms.xml.
@@ -133,7 +137,6 @@ void LogHistogramReceivedItem(ShareExtensionItemReceived type) {
 
 - (void)dealloc {
   NOTREACHED();
-  [super dealloc];
 }
 
 - (void)createReadingListFolder {
--- ios/chrome/browser/share_extension/share_extension_service.mm
+++ ios/chrome/browser/share_extension/share_extension_service.mm
@@ -9,6 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/share_extension/share_extension_item_receiver.h"
 #include "ios/chrome/browser/share_extension/share_extension_service.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 ShareExtensionService::ShareExtensionService(
     bookmarks::BookmarkModel* bookmark_model,
     ReadingListModel* reading_list_model)
