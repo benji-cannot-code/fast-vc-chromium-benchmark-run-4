3e581c8accd7bef19c7e7e3286b3bae0bd49894c
stkhapugin <stkhapugin@chromium.org>
2016-11-21 05:33:13 -0800

    [ObjC ARC] Converts ios/chrome/browser/autocomplete to ARC.
    Notable changes: none.
    
    BUG=663723
    
    Review-Url: https://codereview.chromium.org/2500963002
    Cr-Commit-Position: refs/heads/master@{#433522}

--- ios/chrome/browser/autocomplete/autocomplete_scheme_classifier_impl.mm
+++ ios/chrome/browser/autocomplete/autocomplete_scheme_classifier_impl.mm
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/chrome_url_util.h"
 #include "url/url_util.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 AutocompleteSchemeClassifierImpl::AutocompleteSchemeClassifierImpl() {}
 
 AutocompleteSchemeClassifierImpl::~AutocompleteSchemeClassifierImpl() {}
