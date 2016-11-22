fd922b5f8586197b5ebc52673ee75806449ad1c0
tnagel <tnagel@chromium.org>
2016-11-22 02:58:31 -0800

    Minor PolicyStatusProvider fixes
    Follow-up to https://codereview.chromium.org/2486813002/.
    
    BUG=656558
    
    Review-Url: https://codereview.chromium.org/2519023002
    Cr-Commit-Position: refs/heads/master@{#433833}

--- chrome/browser/ui/webui/policy_ui_handler.cc
+++ chrome/browser/ui/webui/policy_ui_handler.cc
@@ -321,6 +321,8 @@ class DeviceActiveDirectoryPolicyStatusProvider : public PolicyStatusProvider {
 
  private:
   const policy::CloudPolicyStore* store_;
+
+  DISALLOW_COPY_AND_ASSIGN(DeviceActiveDirectoryPolicyStatusProvider);
 };
 #endif
 
