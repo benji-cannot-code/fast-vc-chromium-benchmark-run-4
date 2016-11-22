3bb8af22c4aa57bb08967a5875b90843acb12724
mhashmi <mhashmi@chromium.org>
2016-11-22 15:53:16 -0800

    Add ViewObserver to View for view updates
    - OnViewParentChanged for whenever a view is added/removed
    - OnViewVisibilityChanged, OnViewEnabledChanged, OnViewBoundsChanged
    are all for attribute changes
    - OnViewChildReordered for whenever a view is reordered within its parent
    
    Future CLs will allow devtools for listen to view changes and push them
    to the inspector.
    
    BUG=648701
    
    Review-Url: https://codereview.chromium.org/2500623002
    Cr-Commit-Position: refs/heads/master@{#434026}

--- ui/views/view.cc
+++ ui/views/view.cc
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/drag_controller.h"
 #include "ui/views/focus/view_storage.h"
 #include "ui/views/layout/layout_manager.h"
+#include "ui/views/view_observer.h"
 #include "ui/views/views_delegate.h"
 #include "ui/views/widget/native_widget_private.h"
 #include "ui/views/widget/root_view.h"
@@ -221,6 +222,9 @@ void View::AddChildViewAt(View* view, int index) {
 
   if (layout_manager_.get())
     layout_manager_->ViewAdded(this, view);
+
+  for (ViewObserver& observer : observers_)
+    observer.OnChildViewAdded(view);
 }
 
 void View::ReorderChildView(View* view, int index) {
@@ -247,6 +251,9 @@ void View::ReorderChildView(View* view, int index) {
   // Add it in the specified index now.
   InitFocusSiblings(view, index);
   children_.insert(children_.begin() + index, view);
+
+  for (ViewObserver& observer : observers_)
+    observer.OnChildViewReordered(view);
 
   ReorderLayers();
 }
@@ -299,6 +306,9 @@ void View::SetBoundsRect(const gfx::Rect& bounds) {
   gfx::Rect prev = bounds_;
   bounds_ = bounds;
   BoundsChanged(prev);
+
+  for (ViewObserver& observer : observers_)
+    observer.OnViewBoundsChanged(this);
 }
 
 void View::SetSize(const gfx::Size& size) {
@@ -419,6 +429,9 @@ void View::SetVisible(bool visible) {
       parent_->NotifyAccessibilityEvent(ui::AX_EVENT_CHILDREN_CHANGED, false);
     }
 
+    for (ViewObserver& observer : observers_)
+      observer.OnViewVisibilityChanged(this);
+
     // This notifies all sub-views recursively.
     PropagateVisibilityNotifications(this, visible_);
     UpdateLayerVisibility();
@@ -437,7 +450,11 @@ void View::SetEnabled(bool enabled) {
   if (enabled != enabled_) {
     enabled_ = enabled;
     AdvanceFocusIfNecessary();
+
     OnEnabledChanged();
+
+    for (ViewObserver& observer : observers_)
+      observer.OnViewEnabledChanged(this);
   }
 }
 
@@ -1365,6 +1382,19 @@ int View::GetLineScrollIncrement(ScrollView* scroll_view,
   return 0;
 }
 
+void View::AddObserver(ViewObserver* observer) {
+  CHECK(observer);
+  observers_.AddObserver(observer);
+}
+
+void View::RemoveObserver(ViewObserver* observer) {
+  observers_.RemoveObserver(observer);
+}
+
+bool View::HasObserver(const ViewObserver* observer) const {
+  return observers_.HasObserver(observer);
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // View, protected:
 
@@ -1840,6 +1870,9 @@ void View::DoRemoveChildView(View* view,
 
   if (layout_manager_)
     layout_manager_->ViewRemoved(this, view);
+
+  for (ViewObserver& observer : observers_)
+    observer.OnChildViewRemoved(view, this);
 }
 
 void View::PropagateRemoveNotifications(View* old_parent, View* new_parent) {
--- ui/views/view.h
+++ ui/views/view.h
@@ -75,6 +75,7 @@ class FocusTraversable;
 class LayoutManager;
 class NativeViewAccessibility;
 class ScrollView;
+class ViewObserver;
 class Widget;
 class WordLookupClient;
 
@@ -1001,6 +1002,10 @@ class VIEWS_EXPORT View : public ui::LayerDelegate,
   virtual int GetLineScrollIncrement(ScrollView* scroll_view,
                                      bool is_horizontal, bool is_positive);
 
+  void AddObserver(ViewObserver* observer);
+  void RemoveObserver(ViewObserver* observer);
+  bool HasObserver(const ViewObserver* observer) const;
+
  protected:
   // Used to track a drag. RootView passes this into
   // ProcessMousePressed/Dragged.
@@ -1575,6 +1580,10 @@ class VIEWS_EXPORT View : public ui::LayerDelegate,
   // Belongs to this view, but it's reference-counted on some platforms
   // so we can't use a scoped_ptr. It's dereferenced in the destructor.
   NativeViewAccessibility* native_view_accessibility_;
+
+  // Observers -------------------------------------------------------------
+
+  base::ObserverList<ViewObserver> observers_;
 
   DISALLOW_COPY_AND_ASSIGN(View);
 };
