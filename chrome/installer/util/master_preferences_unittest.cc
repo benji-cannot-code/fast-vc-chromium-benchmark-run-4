9b196fb15502587d4bfd266d9f7640d3f29ca2c0
grt <grt@chromium.org>
2016-11-22 06:12:07 -0800

    Really check that GoogleUpdateIsMachine=1 for system-level installs.
    Previously things like "1likerockandroll" would suffice.
    
    BUG=none
    R=fdoray@chromium.org
    
    Review-Url: https://codereview.chromium.org/2522483002
    Cr-Commit-Position: refs/heads/master@{#433854}

--- chrome/installer/util/master_preferences_unittest.cc
+++ chrome/installer/util/master_preferences_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/environment.h"
 #include "base/files/file_util.h"
 #include "base/macros.h"
 #include "base/path_service.h"
@@ -22,6 +23,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
+
+// A helper class to set the "GoogleUpdateIsMachine" environment variable.
+class ScopedGoogleUpdateIsMachine {
+ public:
+  explicit ScopedGoogleUpdateIsMachine(const char* value)
+      : env_(base::Environment::Create()) {
+    env_->SetVar("GoogleUpdateIsMachine", value);
+  }
+
+  ~ScopedGoogleUpdateIsMachine() {
+    env_->UnSetVar("GoogleUpdateIsMachine");
+  }
+
+ private:
+  std::unique_ptr<base::Environment> env_;
+};
+
 class MasterPreferencesTest : public testing::Test {
  protected:
   void SetUp() override {
@@ -407,4 +425,39 @@ TEST_F(MasterPreferencesTest, DontEnforceLegacyCreateAllShortcutsNotSpecified) {
     EXPECT_FALSE(do_not_create_desktop_shortcut);
     EXPECT_FALSE(do_not_create_quick_launch_shortcut);
     EXPECT_FALSE(do_not_create_taskbar_shortcut);
+}
+
+TEST_F(MasterPreferencesTest, GoogleUpdateIsMachine) {
+  {
+    ScopedGoogleUpdateIsMachine env_setter("0");
+    installer::MasterPreferences prefs(
+        base::CommandLine(base::FilePath(FILE_PATH_LITERAL("setup.exe"))));
+    bool value = false;
+    prefs.GetBool(installer::master_preferences::kSystemLevel, &value);
+    EXPECT_FALSE(value);
+  }
+  {
+    ScopedGoogleUpdateIsMachine env_setter("1");
+    installer::MasterPreferences prefs(
+        base::CommandLine(base::FilePath(FILE_PATH_LITERAL("setup.exe"))));
+    bool value = false;
+    prefs.GetBool(installer::master_preferences::kSystemLevel, &value);
+    EXPECT_TRUE(value);
+  }
+  {
+    ScopedGoogleUpdateIsMachine env_setter("1bridgetoofar");
+    installer::MasterPreferences prefs(
+        base::CommandLine(base::FilePath(FILE_PATH_LITERAL("setup.exe"))));
+    bool value = false;
+    prefs.GetBool(installer::master_preferences::kSystemLevel, &value);
+    EXPECT_FALSE(value);
+  }
+  {
+    ScopedGoogleUpdateIsMachine env_setter("2");
+    installer::MasterPreferences prefs(
+        base::CommandLine(base::FilePath(FILE_PATH_LITERAL("setup.exe"))));
+    bool value = false;
+    prefs.GetBool(installer::master_preferences::kSystemLevel, &value);
+    EXPECT_FALSE(value);
+  }
 }
