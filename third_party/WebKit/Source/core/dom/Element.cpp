77dc3940e6314879fecd8498cac847e4dcd65a5a
hayato <hayato@chromium.org>
2016-11-21 22:03:54 -0800

    Remove ShadowDOMV1 runtime flag (status=stable)
    BUG=402536
    
    Review-Url: https://codereview.chromium.org/2522793002
    Cr-Commit-Position: refs/heads/master@{#433799}

--- third_party/WebKit/Source/core/dom/Element.cpp
+++ third_party/WebKit/Source/core/dom/Element.cpp
@@ -2163,8 +2163,6 @@ ShadowRoot* Element::createShadowRoot(const ScriptState* scriptState,
 ShadowRoot* Element::attachShadow(const ScriptState* scriptState,
                                   const ShadowRootInit& shadowRootInitDict,
                                   ExceptionState& exceptionState) {
-  DCHECK(RuntimeEnabledFeatures::shadowDOMV1Enabled());
-
   HostsUsingFeatures::countMainWorldOnly(
       scriptState, document(),
       HostsUsingFeatures::Feature::ElementAttachShadow);
