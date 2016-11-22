7f34a73b6c652d2a1cde9005456d1ab34bd98fb8
bpastene <bpastene@chromium.org>
2016-11-22 13:45:02 -0800

    Add .jar files to timestamp zapping in remove_build_metadata.py
    A jar file was added to isolated android tests
    (crrev.com/302e4da2da2463a360e26beb8cb88766c48ab6db), which killed the
    dedupe rate since the jar files have timestamps.
    
    BUG=664292
    
    Review-Url: https://codereview.chromium.org/2521773002
    Cr-Commit-Position: refs/heads/master@{#433993}

--- build/android/pylib/device/commands/java/src/org/chromium/android/commands/unzip/Unzip.java
+++ build/android/pylib/device/commands/java/src/org/chromium/android/commands/unzip/Unzip.java
@@ -87,7 +87,7 @@ public class Unzip {
     public void run(String[] args) {
         if (args.length != 1) {
             showUsage(System.err);
-            throw new RuntimeException("Incorrect usage.");
+            throw new RuntimeException("Incorrect usage!");
         }
 
         unzip(args);
