c94592a65ae96745b1b700a70af613f2815bd07a
iclelland <iclelland@chromium.org>
2016-11-21 16:50:28 -0800

    Add a configurable maximum parse depth for the blink JSON parser.
    BUG=662920
    
    Review-Url: https://codereview.chromium.org/2517183002
    Cr-Commit-Position: refs/heads/master@{#433716}

--- third_party/WebKit/Source/platform/json/JSONParser.h
+++ third_party/WebKit/Source/platform/json/JSONParser.h
@@ -17,6 +17,9 @@ class JSONValue;
 
 PLATFORM_EXPORT std::unique_ptr<JSONValue> parseJSON(const String& json);
 
+PLATFORM_EXPORT std::unique_ptr<JSONValue> parseJSON(const String& json,
+                                                     int maxDepth);
+
 }  // namespace blink
 
 #endif  // JSONParser_h
