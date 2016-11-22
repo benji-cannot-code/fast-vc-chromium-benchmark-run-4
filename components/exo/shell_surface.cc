122945fc40eda966b5df31a06d1f68f548d5cc65
mtomasz <mtomasz@chromium.org>
2016-11-22 01:25:50 -0800

    Allow to create ARC++ windows minimized on startup.
    TEST=Tested with OneNote badge.
    BUG=667608
    
    Review-Url: https://codereview.chromium.org/2517833003
    Cr-Commit-Position: refs/heads/master@{#433824}

--- components/exo/shell_surface.cc
+++ components/exo/shell_surface.cc
@@ -448,7 +448,7 @@ void ShellSurface::Minimize() {
   TRACE_EVENT0("exo", "ShellSurface::Minimize");
 
   if (!widget_)
-    return;
+    CreateShellSurfaceWidget(ui::SHOW_STATE_MINIMIZED);
 
   // Note: This will ask client to configure its surface even if already
   // minimized.
