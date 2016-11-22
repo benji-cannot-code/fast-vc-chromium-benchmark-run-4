06cc81c8204f95fc937b9062afa117dfeb3dea56
svillar <svillar@igalia.com>
2016-11-22 07:47:06 -0800

    [css-grid] Isolate internal grid size from the actual grid size
    LayoutGrid has an internal representation of a grid used to place grid
    items, compute grid positions, run the track sizing algorithm etc. That data
    structure normally has exactly the same size as the actual grid specified
    using the grid-template-xxx properties (or any other shorthand). But in some
    cases, like for example when the grid is empty, the internal data structure
    does not really match the actual grid. In the particular case of empty grids
    no memory allocations are done to create a grid representation as it is
    not needed.
    
    This is the first required step of the process of isolating the data used by
    the grid track sizing algorithm from the actual internal state of the
    LayoutGrid object.
    
    BUG=627812
    
    Review-Url: https://codereview.chromium.org/2521553002
    Cr-Commit-Position: refs/heads/master@{#433870}

--- third_party/WebKit/Source/core/layout/LayoutGrid.cpp
+++ third_party/WebKit/Source/core/layout/LayoutGrid.cpp
@@ -401,15 +401,7 @@ bool LayoutGrid::namedGridLinesDefinitionDidChange(
 
 size_t LayoutGrid::gridColumnCount() const {
   DCHECK(!m_gridIsDirty);
-  // Due to limitations in our internal representation, we cannot know the
-  // number of columns from m_grid *if* there is no row (because m_grid would be
-  // empty). That's why in that case we need to get it from the style. Note that
-  // we know for sure that there are't any implicit tracks, because not having
-  // rows implies that there are no "normal" children (out-of-flow children are
-  // not stored in m_grid).
-  return m_grid.size() ? m_grid[0].size()
-                       : GridPositionsResolver::explicitGridColumnCount(
-                             styleRef(), m_autoRepeatColumns);
+  return m_grid.size() ? m_grid[0].size() : 0;
 }
 
 size_t LayoutGrid::gridRowCount() const {
@@ -520,7 +512,7 @@ void LayoutGrid::layoutBlock(bool relayoutChildren) {
       dirtyGrid();
     placeItemsOnGrid(TrackSizing);
 
-    GridSizingData sizingData(gridColumnCount(), gridRowCount());
+    GridSizingData sizingData(numTracks(ForColumns), numTracks(ForRows));
 
     // 1- First, the track sizing algorithm is used to resolve the sizes of the
     // grid columns.
@@ -696,7 +688,7 @@ void LayoutGrid::computeIntrinsicLogicalWidths(
     LayoutUnit& maxLogicalWidth) const {
   const_cast<LayoutGrid*>(this)->placeItemsOnGrid(IntrinsicSizeComputation);
 
-  GridSizingData sizingData(gridColumnCount(), gridRowCount());
+  GridSizingData sizingData(numTracks(ForColumns), numTracks(ForRows));
   computeTrackSizesForIndefiniteSize(ForColumns, sizingData, minLogicalWidth,
                                      maxLogicalWidth);
 
@@ -2026,10 +2018,14 @@ void LayoutGrid::placeItemsOnGrid(SizingOperation sizingOperation) {
     insertItemIntoGrid(*child, area);
   }
 
-  DCHECK_GE(gridRowCount(), GridPositionsResolver::explicitGridRowCount(
-                                *style(), m_autoRepeatRows));
-  DCHECK_GE(gridColumnCount(), GridPositionsResolver::explicitGridColumnCount(
-                                   *style(), m_autoRepeatColumns));
+#if ENABLE(ASSERT)
+  if (!m_gridItemArea.isEmpty()) {
+    DCHECK_GE(gridRowCount(), GridPositionsResolver::explicitGridRowCount(
+                                  *style(), m_autoRepeatRows));
+    DCHECK_GE(gridColumnCount(), GridPositionsResolver::explicitGridColumnCount(
+                                     *style(), m_autoRepeatColumns));
+  }
+#endif
 
   placeSpecifiedMajorAxisItemsOnGrid(specifiedMajorAxisAutoGridItems);
   placeAutoMajorAxisItemsOnGrid(autoMajorAxisAutoGridItems);
@@ -2543,7 +2539,7 @@ void LayoutGrid::offsetAndBreadthForPositionedChild(
                                             : child.style()->gridRowStart();
   GridPosition endPosition = isForColumns ? child.style()->gridColumnEnd()
                                           : child.style()->gridRowEnd();
-  int lastLine = isForColumns ? gridColumnCount() : gridRowCount();
+  int lastLine = numTracks(direction);
 
   bool startIsAuto =
       startPosition.isAuto() ||
@@ -3497,6 +3493,21 @@ void LayoutGrid::paintChildren(const PaintInfo& paintInfo,
 bool LayoutGrid::cachedHasDefiniteLogicalHeight() const {
   SECURITY_DCHECK(m_hasDefiniteLogicalHeight);
   return m_hasDefiniteLogicalHeight.value();
+}
+
+size_t LayoutGrid::numTracks(GridTrackSizingDirection direction) const {
+  // Due to limitations in our internal representation, we cannot know the
+  // number of columns from m_grid *if* there is no row (because m_grid would be
+  // empty). That's why in that case we need to get it from the style. Note that
+  // we know for sure that there are't any implicit tracks, because not having
+  // rows implies that there are no "normal" children (out-of-flow children are
+  // not stored in m_grid).
+  if (direction == ForRows)
+    return m_grid.size();
+
+  return m_grid.size() ? m_grid[0].size()
+                       : GridPositionsResolver::explicitGridColumnCount(
+                             styleRef(), m_autoRepeatColumns);
 }
 
 }  // namespace blink
