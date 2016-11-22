d9b27f06bab2c5c5eceedda4bc0337652c2a4579
vitaliii <vitaliii@chromium.org>
2016-11-22 03:00:10 -0800

    [NTP] Make max number of recent tab suggestion a variational parameter.
    Previously the maximal number of recent tab suggestions was hardcoded as
    5. In this CL the value is taken from a variational parameter (same
    approach as in BookmarkSuggestionsProvider and
    DownloadSuggestionsProvider).
    
    BUG=659250
    
    Review-Url: https://codereview.chromium.org/2521913002
    Cr-Commit-Position: refs/heads/master@{#433834}

--- components/ntp_snippets/offline_pages/recent_tab_suggestions_provider.cc
+++ components/ntp_snippets/offline_pages/recent_tab_suggestions_provider.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "components/ntp_snippets/features.h"
 #include "components/ntp_snippets/pref_names.h"
 #include "components/ntp_snippets/pref_util.h"
 #include "components/offline_pages/client_policy_controller.h"
@@ -33,7 +34,15 @@ namespace ntp_snippets {
 
 namespace {
 
-const int kMaxSuggestionsCount = 5;
+const int kDefaultMaxSuggestionsCount = 5;
+
+const char* kMaxSuggestionsCountParamName = "recent_tabs_max_count";
+
+int GetMaxSuggestionsCount() {
+  return GetParamAsInt(kRecentOfflineTabSuggestionsFeature,
+                       kMaxSuggestionsCountParamName,
+                       kDefaultMaxSuggestionsCount);
+}
 
 struct OrderOfflinePagesByMostRecentlyVisitedFirst {
   bool operator()(const OfflinePageItem* left,
@@ -280,7 +289,7 @@ RecentTabSuggestionsProvider::GetMostRecentlyVisited(
   std::vector<ContentSuggestion> suggestions;
   for (const OfflinePageItem* offline_page_item : offline_page_items) {
     suggestions.push_back(ConvertOfflinePage(*offline_page_item));
-    if (suggestions.size() == kMaxSuggestionsCount) {
+    if (static_cast<int>(suggestions.size()) == GetMaxSuggestionsCount()) {
       break;
     }
   }
