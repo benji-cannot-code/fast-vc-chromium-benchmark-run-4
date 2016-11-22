b7b93f71bf412dc0f9dfffa58a7eaa44596acdd3
tnagel <tnagel@chromium.org>
2016-11-21 23:07:52 -0800

    Remove use of GetProfileByUserUnsafe() in SharedOptionsTest.
    BUG=361528
    
    Review-Url: https://codereview.chromium.org/2504233002
    Cr-Commit-Position: refs/heads/master@{#433805}

--- chrome/browser/ui/webui/options/chromeos/shared_options_browsertest.cc
+++ chrome/browser/ui/webui/options/chromeos/shared_options_browsertest.cc
@@ -146,7 +146,7 @@ class SharedOptionsTest : public LoginManagerTest {
       bool disabled = !is_owner && kPrefTests[i].owner_only;
       if (strcmp(kPrefTests[i].pref_name, kSystemTimezone) == 0) {
         disabled = ProfileHelper::Get()
-                       ->GetProfileByUserUnsafe(user)
+                       ->GetProfileByUser(user)
                        ->GetPrefs()
                        ->GetBoolean(prefs::kResolveTimezoneByGeolocation);
       }
@@ -162,7 +162,7 @@ class SharedOptionsTest : public LoginManagerTest {
 
   // Creates a browser and navigates to the Settings page.
   Browser* CreateBrowserForUser(const user_manager::User* user) {
-    Profile* profile = ProfileHelper::Get()->GetProfileByUserUnsafe(user);
+    Profile* profile = ProfileHelper::Get()->GetProfileByUser(user);
     SigninManagerFactory::GetForProfile(profile)->SetAuthenticatedAccountInfo(
         GetGaiaIDForUserID(user->GetAccountId().GetUserEmail()),
         user->GetAccountId().GetUserEmail());
@@ -345,9 +345,9 @@ IN_PROC_BROWSER_TEST_F(SharedOptionsTest, ScreenLockPreferencePrimary) {
       manager->FindUser(test_non_owner_account_id_);
 
   PrefService* prefs1 =
-      ProfileHelper::Get()->GetProfileByUserUnsafe(user1)->GetPrefs();
+      ProfileHelper::Get()->GetProfileByUser(user1)->GetPrefs();
   PrefService* prefs2 =
-      ProfileHelper::Get()->GetProfileByUserUnsafe(user2)->GetPrefs();
+      ProfileHelper::Get()->GetProfileByUser(user2)->GetPrefs();
 
   // Set both users' preference to false, then change the secondary user's to
   // true. We'll do the opposite in the next test. Doesn't provide 100% coverage
@@ -417,9 +417,9 @@ IN_PROC_BROWSER_TEST_F(SharedOptionsTest, ScreenLockPreferenceSecondary) {
       manager->FindUser(test_non_owner_account_id_);
 
   PrefService* prefs1 =
-      ProfileHelper::Get()->GetProfileByUserUnsafe(user1)->GetPrefs();
+      ProfileHelper::Get()->GetProfileByUser(user1)->GetPrefs();
   PrefService* prefs2 =
-      ProfileHelper::Get()->GetProfileByUserUnsafe(user2)->GetPrefs();
+      ProfileHelper::Get()->GetProfileByUser(user2)->GetPrefs();
 
   // Set both users' preference to true, then change the secondary user's to
   // false.
