61e120458b2ddd93be8469d86fbb22730dc73486
tkent <tkent@chromium.org>
2016-11-22 01:22:20 -0800

    Remove WTF_NON_EXPORTED_BASE.
    We can use NON_EXPORTED_BASE defined in base/compiler_specific.h by including
    wtf/Compiler.h.
    
    Review-Url: https://codereview.chromium.org/2521723004
    Cr-Commit-Position: refs/heads/master@{#433822}

--- third_party/WebKit/Source/platform/graphics/OffscreenCanvasFrameDispatcherImpl.h
+++ third_party/WebKit/Source/platform/graphics/OffscreenCanvasFrameDispatcherImpl.h
@@ -13,13 +13,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/binding.h"
 #include "platform/graphics/OffscreenCanvasFrameDispatcher.h"
 #include "platform/graphics/StaticBitmapImage.h"
+#include "wtf/Compiler.h"
 #include <memory>
 
 namespace blink {
 
 class PLATFORM_EXPORT OffscreenCanvasFrameDispatcherImpl final
     : public OffscreenCanvasFrameDispatcher,
-      WTF_NON_EXPORTED_BASE(
+      NON_EXPORTED_BASE(
           public cc::mojom::blink::MojoCompositorFrameSinkClient) {
  public:
   OffscreenCanvasFrameDispatcherImpl(uint32_t clientId,
