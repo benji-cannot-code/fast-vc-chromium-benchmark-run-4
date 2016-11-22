b0dc455727c058978c7e44e47047fb372893331c
vitaliii <vitaliii@chromium.org>
2016-11-21 23:55:22 -0800

    [NTP] Cleanup: specific category related data and bridge.
    This CL:
    - replaces |(.*)ForLastSuggestion| functions with |\1ForSuggestions|;
    - renames specific category related data variables to be more consistent
      with C++ side;
    - removes mFile variable;
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2514343003
    Cr-Commit-Position: refs/heads/master@{#433811}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/snippets/SnippetArticleViewHolder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/snippets/SnippetArticleViewHolder.java
@@ -252,7 +252,7 @@ public class SnippetArticleViewHolder
             Runnable offlineChecker = new Runnable() {
                 @Override
                 public void run() {
-                    if (mArticle.getOfflinePageOfflineId() != null || mArticle.mIsDownloadedAsset) {
+                    if (mArticle.getOfflinePageOfflineId() != null || mArticle.mIsAssetDownload) {
                         mOfflineBadge.setVisibility(View.VISIBLE);
                     }
                 }
