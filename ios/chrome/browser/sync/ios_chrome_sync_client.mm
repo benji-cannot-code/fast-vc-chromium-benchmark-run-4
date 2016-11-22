6f0cac683115d05de9a7cc6d2bcbc87e914f0ea7
stkhapugin <stkhapugin@chromium.org>
2016-11-22 07:07:47 -0800

    [ObjC ARC] Converts ios/chrome/browser/sync:sync to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2518563002
    Cr-Commit-Position: refs/heads/master@{#433859}

--- ios/chrome/browser/sync/ios_chrome_sync_client.mm
+++ ios/chrome/browser/sync/ios_chrome_sync_client.mm
@@ -66,6 +66,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/web/public/web_thread.h"
 #include "ui/base/device_form_factor.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace {
 
 // iOS implementation of SyncSessionsClient. Needs to be in a separate class
