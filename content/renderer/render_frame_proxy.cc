f1282cdd72fea248ba99671b6d91ecae71c20c16
nick <nick@chromium.org>
2016-11-22 15:07:11 -0800

    Simplify RenderFrameImpl::ResolveOpener() by removing the |opener_view_routing_id| optional param.
    In the prior patch set, we removed the last user of that param.
    
    BUG=627852
    
    Review-Url: https://codereview.chromium.org/2499053003
    Cr-Commit-Position: refs/heads/master@{#434015}

--- content/renderer/render_frame_proxy.cc
+++ content/renderer/render_frame_proxy.cc
@@ -83,7 +83,7 @@ RenderFrameProxy* RenderFrameProxy::CreateProxyToReplaceFrame(
 RenderFrameProxy* RenderFrameProxy::CreateFrameProxy(
     int routing_id,
     int render_view_routing_id,
-    int opener_routing_id,
+    blink::WebFrame* opener,
     int parent_routing_id,
     const FrameReplicationState& replicated_state) {
   RenderFrameProxy* parent = nullptr;
@@ -95,9 +95,6 @@ RenderFrameProxy* RenderFrameProxy::CreateFrameProxy(
     if (!parent)
       return nullptr;
   }
-
-  blink::WebFrame* opener =
-      RenderFrameImpl::ResolveOpener(opener_routing_id, nullptr);
 
   std::unique_ptr<RenderFrameProxy> proxy(
       new RenderFrameProxy(routing_id, MSG_ROUTING_NONE));
@@ -328,8 +325,7 @@ void RenderFrameProxy::OnSetChildFrameSurface(
 }
 
 void RenderFrameProxy::OnUpdateOpener(int opener_routing_id) {
-  blink::WebFrame* opener =
-      RenderFrameImpl::ResolveOpener(opener_routing_id, nullptr);
+  blink::WebFrame* opener = RenderFrameImpl::ResolveOpener(opener_routing_id);
   web_frame_->setOpener(opener);
 }
 
