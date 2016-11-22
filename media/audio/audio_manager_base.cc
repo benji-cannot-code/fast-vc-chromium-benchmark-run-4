be24f638c996fcb56c23b1801833c20e79278ddf
alokp <alokp@chromium.org>
2016-11-21 19:05:06 -0800

    Adds speculative CHECKs to diagnose AudioManager crashes.
    Adds CHECKs to verify that input/output streams are created/released on
    the audio thread.
    
    BUG=608049
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2521583003
    Cr-Commit-Position: refs/heads/master@{#433762}

--- media/audio/audio_manager_base.cc
+++ media/audio/audio_manager_base.cc
@@ -107,7 +107,7 @@ AudioOutputStream* AudioManagerBase::MakeAudioOutputStream(
     const AudioParameters& params,
     const std::string& device_id,
     const LogCallback& log_callback) {
-  DCHECK(GetTaskRunner()->BelongsToCurrentThread());
+  CHECK(GetTaskRunner()->BelongsToCurrentThread());
 
   if (!params.IsValid()) {
     DLOG(ERROR) << "Audio parameters are invalid";
@@ -161,7 +161,7 @@ AudioInputStream* AudioManagerBase::MakeAudioInputStream(
     const AudioParameters& params,
     const std::string& device_id,
     const LogCallback& log_callback) {
-  DCHECK(GetTaskRunner()->BelongsToCurrentThread());
+  CHECK(GetTaskRunner()->BelongsToCurrentThread());
 
   if (!params.IsValid() || (params.channels() > kMaxInputChannels) ||
       device_id.empty()) {
@@ -205,7 +205,7 @@ AudioInputStream* AudioManagerBase::MakeAudioInputStream(
 AudioOutputStream* AudioManagerBase::MakeAudioOutputStreamProxy(
     const AudioParameters& params,
     const std::string& device_id) {
-  DCHECK(GetTaskRunner()->BelongsToCurrentThread());
+  CHECK(GetTaskRunner()->BelongsToCurrentThread());
 
   // If the caller supplied an empty device id to select the default device,
   // we fetch the actual device id of the default device so that the lookup
@@ -287,6 +287,7 @@ void AudioManagerBase::GetAudioOutputDeviceNames(
 }
 
 void AudioManagerBase::ReleaseOutputStream(AudioOutputStream* stream) {
+  CHECK(GetTaskRunner()->BelongsToCurrentThread());
   DCHECK(stream);
   CHECK_GT(num_output_streams_, 0);
   // TODO(xians) : Have a clearer destruction path for the AudioOutputStream.
@@ -297,6 +298,7 @@ void AudioManagerBase::ReleaseOutputStream(AudioOutputStream* stream) {
 }
 
 void AudioManagerBase::ReleaseInputStream(AudioInputStream* stream) {
+  CHECK(GetTaskRunner()->BelongsToCurrentThread());
   DCHECK(stream);
   // TODO(xians) : Have a clearer destruction path for the AudioInputStream.
   CHECK_EQ(1u, input_streams_.erase(stream));
