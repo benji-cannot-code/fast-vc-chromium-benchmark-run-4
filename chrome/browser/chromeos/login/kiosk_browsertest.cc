cfdd5e4f72cfca5769dd4437be1d360cdcf47959
asargent <asargent@chromium.org>
2016-11-22 14:58:02 -0800

    Continue attempts to reinstall corrupt policy extensions across restarts
    If a policy force-installed extension gets corrupted, we'll disable it
    and start attempting to reinstall it. However, if chrome shuts down
    before this is complete, we won't start trying again on any subsequent
    run of the browser.
    
    This also switches from using the ManagementPolicy :: Provider ::
    UserMayModifySettings function to determine "policy force installed-ess"
    to using ManagementPolicy :: Provider :: MustRemainEnabled instead, which
    is more correct for the case of force installs (eg
    "ExtensionInstallForcelist" in the policy settings).
    
    BUG=664690
    
    Review-Url: https://codereview.chromium.org/2495123003
    Cr-Commit-Position: refs/heads/master@{#434012}

--- chrome/browser/chromeos/login/kiosk_browsertest.cc
+++ chrome/browser/chromeos/login/kiosk_browsertest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/common/wm_shell.h"
 #include "base/bind.h"
 #include "base/bind_helpers.h"
-#include "base/files/file_util.h"
 #include "base/location.h"
 #include "base/macros.h"
 #include "base/path_service.h"
@@ -43,8 +42,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/settings/device_oauth2_token_service.h"
 #include "chrome/browser/chromeos/settings/device_oauth2_token_service_factory.h"
 #include "chrome/browser/chromeos/settings/scoped_cros_settings_test_helper.h"
+#include "chrome/browser/extensions/browsertest_util.h"
 #include "chrome/browser/extensions/extension_service.h"
-#include "chrome/browser/extensions/updater/local_extension_cache.h"
 #include "chrome/browser/profiles/profile_impl.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/profiles/profiles_state.h"
@@ -53,7 +52,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/pref_names.h"
-#include "chromeos/chromeos_paths.h"
 #include "chromeos/chromeos_switches.h"
 #include "chromeos/dbus/cryptohome_client.h"
 #include "chromeos/disks/disk_mount_manager.h"
@@ -527,7 +525,7 @@ class KioskTest : public OobeBaseTest {
         ProfileManager::GetPrimaryUserProfile());
 
     // Set up local cache for app update check.
-    CreateAndInitializeLocalCache();
+    extensions::browsertest_util::CreateAndInitializeLocalCache();
   }
 
   void TearDownOnMainThread() override {
@@ -540,19 +538,6 @@ class KioskTest : public OobeBaseTest {
     // Clean up while main thread still runs.
     // See http://crbug.com/176659.
     KioskAppManager::Get()->CleanUp();
-  }
-
-  // The local cache is supposed to be initialized on chromeos device, and a
-  // ready flag file will be pre-created to mark the ready state, before chrome
-  // starts. In order for the tests to run without being on real chromeos
-  // device, we need to manually create this file.
-  void CreateAndInitializeLocalCache() {
-    base::FilePath extension_cache_dir;
-    CHECK(PathService::Get(chromeos::DIR_DEVICE_EXTENSION_LOCAL_CACHE,
-                           &extension_cache_dir));
-    base::FilePath cache_init_file = extension_cache_dir.Append(
-        extensions::LocalExtensionCache::kCacheReadyFlagFileName);
-    EXPECT_EQ(base::WriteFile(cache_init_file, "", 0), 0);
   }
 
   void SetUpCommandLine(base::CommandLine* command_line) override {
@@ -2375,6 +2360,7 @@ class KioskHiddenWebUITest : public KioskTest,
   bool wallpaper_loaded_;
   scoped_refptr<content::MessageLoopRunner> runner_;
 
+ private:
   DISALLOW_COPY_AND_ASSIGN(KioskHiddenWebUITest);
 };
 
--- chrome/browser/extensions/browsertest_util.cc
+++ chrome/browser/extensions/browsertest_util.cc
@@ -5,11 +5,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/browsertest_util.h"
 
+#include "base/files/file_util.h"
+#include "base/path_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "content/public/test/browser_test_utils.h"
 #include "extensions/browser/extension_host.h"
 #include "extensions/browser/process_manager.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+#if defined(OS_CHROMEOS)
+#include "chrome/browser/extensions/updater/local_extension_cache.h"
+#include "chromeos/chromeos_paths.h"
+#endif
 
 namespace extensions {
 namespace browsertest_util {
@@ -46,6 +53,17 @@ bool ExecuteScriptInBackgroundPageNoWait(Profile* profile,
     return false;
   }
   return content::ExecuteScript(host->host_contents(), script);
+}
+
+void CreateAndInitializeLocalCache() {
+#if defined(OS_CHROMEOS)
+  base::FilePath extension_cache_dir;
+  CHECK(PathService::Get(chromeos::DIR_DEVICE_EXTENSION_LOCAL_CACHE,
+                         &extension_cache_dir));
+  base::FilePath cache_init_file = extension_cache_dir.Append(
+      extensions::LocalExtensionCache::kCacheReadyFlagFileName);
+  EXPECT_EQ(base::WriteFile(cache_init_file, "", 0), 0);
+#endif
 }
 
 }  // namespace browsertest_util
