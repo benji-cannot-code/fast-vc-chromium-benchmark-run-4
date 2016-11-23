5c888eef244fa2e71cc44d3b784e785ff0177144
fdoray <fdoray@chromium.org>
2016-11-23 04:29:59 -0800

    Remove destruction observer from file_path_watcher_win.cc
    Now that the FilePathWatcher::~FilePathWatcher() is called on the same
    sequence as FilePathWatcher::Watch() (see
    https://codereview.chromium.org/2438913003/), the work done in
    FilePathWatcherImpl::WillDestroyCurrentMessageLoop() can safely be
    moved to FilePathWatcherImpl::Cancel().
    
    BUG=650318
    
    Review-Url: https://codereview.chromium.org/2523573002
    Cr-Commit-Position: refs/heads/master@{#434150}

--- base/files/file_path_watcher_win.cc
+++ base/files/file_path_watcher_win.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
-#include "base/message_loop/message_loop.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "base/win/object_watcher.h"
@@ -22,25 +21,19 @@ namespace base {
 namespace {
 
 class FilePathWatcherImpl : public FilePathWatcher::PlatformDelegate,
-                            public base::win::ObjectWatcher::Delegate,
-                            public MessageLoop::DestructionObserver {
+                            public base::win::ObjectWatcher::Delegate {
  public:
   FilePathWatcherImpl()
       : handle_(INVALID_HANDLE_VALUE),
         recursive_watch_(false) {}
 
-  // FilePathWatcher::PlatformDelegate overrides.
+  // FilePathWatcher::PlatformDelegate:
   bool Watch(const FilePath& path,
              bool recursive,
              const FilePathWatcher::Callback& callback) override;
   void Cancel() override;
 
-  // Deletion of the FilePathWatcher will call Cancel() to dispose of this
-  // object in the right thread. This also observes destruction of the required
-  // cleanup thread, in case it quits before Cancel() is called.
-  void WillDestroyCurrentMessageLoop() override;
-
-  // Callback from MessageLoopForIO.
+  // base::win::ObjectWatcher::Delegate:
   void OnObjectSignaled(HANDLE object) override;
 
  private:
@@ -59,9 +52,6 @@ class FilePathWatcherImpl : public FilePathWatcher::PlatformDelegate,
 
   // Destroy the watch handle.
   void DestroyWatch();
-
-  // Cleans up and stops observing the |task_runner_| thread.
-  void CancelOnMessageLoopThread();
 
   // Callback to notify upon changes.
   FilePathWatcher::Callback callback_;
@@ -98,7 +88,6 @@ bool FilePathWatcherImpl::Watch(const FilePath& path,
   callback_ = callback;
   target_ = path;
   recursive_watch_ = recursive;
-  MessageLoop::current()->AddDestructionObserver(this);
 
   File::Info file_info;
   if (GetFileInfo(target_, &file_info)) {
@@ -121,30 +110,13 @@ void FilePathWatcherImpl::Cancel() {
     return;
   }
 
-  // Switch to the file thread if necessary so we can stop |watcher_|.
-  if (!task_runner()->BelongsToCurrentThread()) {
-    task_runner()->PostTask(
-        FROM_HERE, Bind(&FilePathWatcherImpl::CancelOnMessageLoopThread, this));
-  } else {
-    CancelOnMessageLoopThread();
-  }
-}
-
-void FilePathWatcherImpl::CancelOnMessageLoopThread() {
   DCHECK(task_runner()->BelongsToCurrentThread());
   set_cancelled();
 
   if (handle_ != INVALID_HANDLE_VALUE)
     DestroyWatch();
 
-  if (!callback_.is_null()) {
-    MessageLoop::current()->RemoveDestructionObserver(this);
-    callback_.Reset();
-  }
-}
-
-void FilePathWatcherImpl::WillDestroyCurrentMessageLoop() {
-  CancelOnMessageLoopThread();
+  callback_.Reset();
 }
 
 void FilePathWatcherImpl::OnObjectSignaled(HANDLE object) {
