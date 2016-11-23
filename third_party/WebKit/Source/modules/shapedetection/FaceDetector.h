5127d109ebe715c8f46d889b69169be42d74a0a3
xianglu <xianglu@chromium.org>
2016-11-23 09:24:11 -0800

    This CL moves initialization of m_options from FaceDetector to its superclass ShapeDetector.
    BUG=659139, 646083
    
    Review-Url: https://codereview.chromium.org/2527503003
    Cr-Commit-Position: refs/heads/master@{#434196}

--- third_party/WebKit/Source/modules/shapedetection/FaceDetector.h
+++ third_party/WebKit/Source/modules/shapedetection/FaceDetector.h
@@ -29,7 +29,7 @@ class MODULES_EXPORT FaceDetector final : public ShapeDetector,
   DECLARE_VIRTUAL_TRACE();
 
  private:
-  explicit FaceDetector(LocalFrame&, const FaceDetectorOptions&);
+  FaceDetector(LocalFrame&, const FaceDetectorOptions&);
   ~FaceDetector() override = default;
 };
 
