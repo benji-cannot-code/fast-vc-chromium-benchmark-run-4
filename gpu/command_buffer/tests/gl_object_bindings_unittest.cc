de92c42a648b575185f4d9e6b6ab0cd156d624e1
geofflang <geofflang@chromium.org>
2016-11-23 07:26:22 -0800

    Check IsApplicable in ObjectBindingsTest.
    TBR=piman@chromium.org
    
    BUG=602688
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2527653002
    Cr-Commit-Position: refs/heads/master@{#434169}

--- gpu/command_buffer/tests/gl_object_bindings_unittest.cc
+++ gpu/command_buffer/tests/gl_object_bindings_unittest.cc
@@ -31,6 +31,10 @@ class ObjectBindingsTest : public testing::Test {
 };
 
 TEST_F(ObjectBindingsTest, Buffers) {
+  if (!IsApplicable()) {
+    return;
+  }
+
   const std::pair<GLenum, GLenum> buffer_bindings[] = {
       {GL_ARRAY_BUFFER, GL_ARRAY_BUFFER_BINDING},
       {GL_ELEMENT_ARRAY_BUFFER, GL_ELEMENT_ARRAY_BUFFER_BINDING},
@@ -67,6 +71,10 @@ TEST_F(ObjectBindingsTest, Buffers) {
 }
 
 TEST_F(ObjectBindingsTest, FramebufferAttachments) {
+  if (!IsApplicable()) {
+    return;
+  }
+
   GLuint texture = 0;
   glGenTextures(1, &texture);
   glBindTexture(GL_TEXTURE_2D, texture);
