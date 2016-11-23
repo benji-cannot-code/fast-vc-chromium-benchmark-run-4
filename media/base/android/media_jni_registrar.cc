d74264be7e16459ee7f1621ca910dd777bfa47a3
tguilbert <tguilbert@chromium.org>
2016-11-22 16:52:16 -0800

    Fix MediaServerCrashListener JNI registration
    Commit f524383bbd03acfba4ffe18b7e77a66c46228dcd introduced the
    MediaServerCrashListener, but did not properly call the method to
    register the native methods with JNI.
    
    This change properly registers the native methods.
    
    BUG=667149
    
    Review-Url: https://codereview.chromium.org/2527553002
    Cr-Commit-Position: refs/heads/master@{#434044}

--- media/base/android/media_jni_registrar.cc
+++ media/base/android/media_jni_registrar.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/android/media_drm_bridge.h"
 #include "media/base/android/media_player_bridge.h"
 #include "media/base/android/media_player_listener.h"
+#include "media/base/android/media_server_crash_listener.h"
 
 namespace media {
 
@@ -23,6 +24,8 @@ static base::android::RegistrationMethod kMediaRegisteredMethods[] = {
     {"MediaDrmBridge", MediaDrmBridge::RegisterMediaDrmBridge},
     {"MediaPlayerBridge", MediaPlayerBridge::RegisterMediaPlayerBridge},
     {"MediaPlayerListener", MediaPlayerListener::RegisterMediaPlayerListener},
+    {"MediaServerCrashListener",
+     MediaServerCrashListener::RegisterMediaServerCrashListener},
 };
 
 bool RegisterJni(JNIEnv* env) {
