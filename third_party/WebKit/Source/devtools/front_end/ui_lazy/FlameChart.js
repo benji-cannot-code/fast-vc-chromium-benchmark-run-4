85053d61fa0d577b7e1012280d1984c467ee6261
allada <allada@chromium.org>
2016-11-22 18:38:41 -0800

    [Devtools] Fix hover issue with darktheme in network
    This patch fixes dark theme hover issues with dark theme due to
    TimelineGrid.js having fixed height for the header draw area.
    
    See: http://imgur.com/a/mWpZu
    
    R=dgozman,lushnikov
    BUG=667170
    
    Review-Url: https://codereview.chromium.org/2520003006
    Cr-Commit-Position: refs/heads/master@{#434092}

--- third_party/WebKit/Source/devtools/front_end/ui_lazy/FlameChart.js
+++ third_party/WebKit/Source/devtools/front_end/ui_lazy/FlameChart.js
@@ -661,7 +661,8 @@ UI.FlameChart = class extends UI.ChartViewport {
 
     context.restore();
 
-    UI.TimelineGrid.drawCanvasGrid(context, this._calculator, 3);
+    const headerHeight = 15;
+    UI.TimelineGrid.drawCanvasGrid(context, this._calculator, 3, headerHeight);
     this._drawMarkers();
     this._drawGroupHeaders(width, height);
 
