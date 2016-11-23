2fec816dbfb98f86421ec1f047576e680a6567f3
fdoray <fdoray@chromium.org>
2016-11-23 10:29:29 -0800

    Stop checking that FilePathWatcher is used from a MessageLoopForIO.
    No FilePathWatcher implementation requires to be used from a
    MessageLoopForIO.
    
    BUG=650723
    
    Review-Url: https://codereview.chromium.org/2516353003
    Cr-Commit-Position: refs/heads/master@{#434205}

--- base/files/file_path_watcher_linux.cc
+++ base/files/file_path_watcher_linux.cc
@@ -336,7 +336,7 @@ void FilePathWatcherImpl::OnFilePathChanged(InotifyReader::Watch fired_watch,
     return;
   }
 
-  DCHECK(MessageLoopForIO::current());
+  DCHECK(task_runner()->BelongsToCurrentThread());
   DCHECK(HasValidWatchVector());
 
   // Used below to avoid multiple recursive updates.
@@ -421,7 +421,6 @@ bool FilePathWatcherImpl::Watch(const FilePath& path,
                                 bool recursive,
                                 const FilePathWatcher::Callback& callback) {
   DCHECK(target_.empty());
-  DCHECK(MessageLoopForIO::current());
 
   set_task_runner(ThreadTaskRunnerHandle::Get());
   callback_ = callback;
