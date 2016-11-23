7b897f61f06a474c53c746bca4da18454f75ee65
fsamuel <fsamuel@chromium.org>
2016-11-23 15:50:24 -0800

    Mus: Fix Capabilities and Install SwapBuffersCompleted/VSync callbacks
    DirectOutputSurface(Ozone) should be informed when a swap buffers is
    completed or when vsync parameters are updated so that it can update the
    compositor accordingly. We need to make sure that the callback is called on the Mus
    compositor thread. InProcessCommandBuffer calls
    these callbacks on the "origin" thread in this patch.
    
    Furthermore, InProcessContextProvider was faking Capabilities instead of grabbing
    them from InProcessContextProvider. This doesn't work on
    ozone platforms.
    
    BUG=668136
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2529703002
    Cr-Commit-Position: refs/heads/master@{#434285}

--- gpu/ipc/gl_in_process_context.cc
+++ gpu/ipc/gl_in_process_context.cc
@@ -64,6 +64,7 @@ class GLInProcessContextImpl
                   scoped_refptr<base::SingleThreadTaskRunner> task_runner);
 
   // GLInProcessContext implementation:
+  gpu::Capabilities GetCapabilities() override;
   gles2::GLES2Implementation* GetImplementation() override;
   void SetSwapBuffersCompletionCallback(
       const gpu::InProcessCommandBuffer::SwapBuffersCompletionCallback&
@@ -89,6 +90,10 @@ GLInProcessContextImpl::GLInProcessContextImpl() = default;
 
 GLInProcessContextImpl::~GLInProcessContextImpl() {
   Destroy();
+}
+
+Capabilities GLInProcessContextImpl::GetCapabilities() {
+  return command_buffer_->GetCapabilities();
 }
 
 gles2::GLES2Implementation* GLInProcessContextImpl::GetImplementation() {
