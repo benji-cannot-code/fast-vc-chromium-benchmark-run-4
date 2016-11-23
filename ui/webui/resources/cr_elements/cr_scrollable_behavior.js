d2f5ceeeff21d0924b90234ec2f89ba717389080
stevenjb <stevenjb@chromium.org>
2016-11-23 12:20:20 -0800

    MD Settings: Fix CrScrollableBehavior
    Currently if updateScrollableContents gets called more than once
    before the container height resolves, setInterval will be called
    multiplie times but only cleared once. This is very bad and is
    responsible for strange behavior and (likely) performance issues.
    
    BUG=665631
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2522783003
    Cr-Commit-Position: refs/heads/master@{#434235}

--- ui/webui/resources/cr_elements/cr_scrollable_behavior.js
+++ ui/webui/resources/cr_elements/cr_scrollable_behavior.js
@@ -38,8 +38,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /** @polymerBehavior */
 var CrScrollableBehavior = {
   properties: {
-    /** @private */
-    intervalId_: Number,
+    /** @private {number|null} */
+    intervalId_: {
+      type: Number,
+      value: null
+    }
   },
 
   ready: function() {
@@ -57,7 +60,7 @@ var CrScrollableBehavior = {
   },
 
   detached: function() {
-    if (this.intervalId_)
+    if (this.intervalId_ !== null)
       clearInterval(this.intervalId_);
   },
 
@@ -66,10 +69,13 @@ var CrScrollableBehavior = {
    * This ensures that the <iron-list> contents of dynamically sized
    * containers are resized correctly.
    */
-  updateScrollableContents() {
+  updateScrollableContents: function() {
+    if (this.intervalId_ !== null)
+      return;  // notifyResize is arelady in progress.
+
     let nodeList = this.root.querySelectorAll('[scrollable] iron-list');
-    // Use setTimeout to avoid initial render / sizing issues.
-    this.intervalId_ = setInterval(function() {
+    // Use setInterval to avoid initial render / sizing issues.
+    this.intervalId_ = window.setInterval(function() {
       let unreadyNodes = [];
       for (let node of nodeList) {
         if (node.parentNode.scrollHeight == 0) {
@@ -79,10 +85,12 @@ var CrScrollableBehavior = {
         let ironList = /** @type {!IronListElement} */ (node);
         ironList.notifyResize();
       }
-      if (unreadyNodes.length == 0)
-        clearInterval(this.intervalId_);
-      else
+      if (unreadyNodes.length == 0) {
+        window.clearInterval(this.intervalId_);
+        this.intervalId_ = null;
+      } else {
         nodeList = unreadyNodes;
+      }
     }.bind(this), 10);
   },
 
@@ -91,7 +99,7 @@ var CrScrollableBehavior = {
    * @param {!Event} event
    * @private
    */
-  updateScrollEvent_(event) {
+  updateScrollEvent_: function(event) {
     let scrollable = /** @type {!HTMLElement} */ (event.target);
     this.updateScroll_(scrollable);
   },
@@ -101,7 +109,7 @@ var CrScrollableBehavior = {
    * @param {!HTMLElement} scrollable
    * @private
    */
-  updateScroll_(scrollable) {
+  updateScroll_: function(scrollable) {
     scrollable.classList.toggle(
         'can-scroll', scrollable.clientHeight < scrollable.scrollHeight);
     scrollable.classList.toggle('is-scrolled', scrollable.scrollTop > 0);
