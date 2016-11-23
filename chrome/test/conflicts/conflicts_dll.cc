4c549c236513304f70daf5ea4c031087dad45c3c
chrisha <chrisha@chromium.org>
2016-11-23 13:24:24 -0800

    [Win] Create ModuleWatcher.
    This is a utility class that will be instantiated in each process type. The resulting events will be funneled back to the browser process for use in third-party metrics gathering and the chrome://conflicts UI.
    
    BUG=662084
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    
    Review-Url: https://codereview.chromium.org/2473783005
    Cr-Commit-Position: refs/heads/master@{#434251}

---
+++ chrome/test/conflicts/conflicts_dll.cc
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// An empty DLL for use when testing the chrome://conflicts implementation.
+
+#include <windows.h>
+
+BOOL WINAPI DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved) {
+  return TRUE;
+}
