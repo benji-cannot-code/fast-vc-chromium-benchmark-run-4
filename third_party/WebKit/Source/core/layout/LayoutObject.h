e8efededdea387cb601934b25b68ac102580c145
pdr <pdr@chromium.org>
2016-11-22 17:01:21 -0800

    Devirtualize LayoutObject::clearPaintInvalidationFlags
    Trivial patch to remove an unneeded virtual. Noticed this when working
    on the similar paint property flag clearing in
    https://codereview.chromium.org/2521613003.
    
    Review-Url: https://codereview.chromium.org/2519293002
    Cr-Commit-Position: refs/heads/master@{#434053}

--- third_party/WebKit/Source/core/layout/LayoutObject.h
+++ third_party/WebKit/Source/core/layout/LayoutObject.h
@@ -1602,7 +1602,7 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
     m_bitfields.setFullPaintInvalidationReason(PaintInvalidationNone);
   }
 
-  virtual void clearPaintInvalidationFlags();
+  void clearPaintInvalidationFlags();
 
   bool mayNeedPaintInvalidation() const {
     return m_bitfields.mayNeedPaintInvalidation();
