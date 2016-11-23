89db45a13395c31b8b2e5d9c71599a382a057fe9
zijiehe <zijiehe@chromium.org>
2016-11-22 16:57:31 -0800

    [Chromoting] Change ChromotingHost::SetEnableCurtaining() to DesktopEnvironmentOptions
    After this change, both ChromotingHost and HostProcess own a
    DesktopEnvironmentOptions. In HostProcess, the instance is used to receive
    policy updates, and send to ChromotingHost. In ChromotingHost, the instance is
    used as the default DesktopEnvironmentOptions for each connection.
    
    In a coming change, ChromotingHost builds a HostSessionOptions from
    protocol::Session, applies it to the DesktopEnvironmentOptions, and sends to
    ClientSession.
    
    This is part of host experiment feature.
    
    BUG=650926
    
    Review-Url: https://codereview.chromium.org/2510083007
    Cr-Commit-Position: refs/heads/master@{#434049}

--- remoting/host/chromoting_host.h
+++ remoting/host/chromoting_host.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread.h"
 #include "net/base/backoff_entry.h"
 #include "remoting/host/client_session.h"
+#include "remoting/host/desktop_environment_options.h"
 #include "remoting/host/host_extension.h"
 #include "remoting/host/host_status_monitor.h"
 #include "remoting/host/host_status_observer.h"
@@ -75,7 +76,8 @@ class ChromotingHost : public base::NonThreadSafe,
       std::unique_ptr<protocol::SessionManager> session_manager,
       scoped_refptr<protocol::TransportContext> transport_context,
       scoped_refptr<base::SingleThreadTaskRunner> audio_task_runner,
-      scoped_refptr<base::SingleThreadTaskRunner> video_encode_task_runner);
+      scoped_refptr<base::SingleThreadTaskRunner> video_encode_task_runner,
+      const DesktopEnvironmentOptions& options);
   ~ChromotingHost() override;
 
   // Asynchronously starts the host.
@@ -101,12 +103,6 @@ class ChromotingHost : public base::NonThreadSafe,
   // factory before all authenticators it created are deleted.
   void SetAuthenticatorFactory(
       std::unique_ptr<protocol::AuthenticatorFactory> authenticator_factory);
-
-  // Enables/disables curtaining when one or more clients are connected.
-  // Takes immediate effect if clients are already connected.
-  // TODO(zijiehe): Replace this function with
-  // SetDesktopEnvironmentOptions(const DesktopEnvironmentOptions&).
-  void SetEnableCurtaining(bool enable);
 
   // Sets the maximum duration of any session. By default, a session has no
   // maximum duration.
@@ -149,11 +145,6 @@ class ChromotingHost : public base::NonThreadSafe,
 
   typedef ScopedVector<HostExtension> HostExtensionList;
 
-  // Immediately disconnects all active clients. Host-internal components may
-  // shutdown asynchronously, but the caller is guaranteed not to receive
-  // callbacks for disconnected clients after this call returns.
-  void DisconnectAllClients();
-
   // Unless specified otherwise all members of this class must be
   // used on the network thread only.
 
@@ -176,8 +167,8 @@ class ChromotingHost : public base::NonThreadSafe,
   // Login backoff state.
   net::BackoffEntry login_backoff_;
 
-  // True if the curtain mode is enabled.
-  bool enable_curtaining_;
+  // Options to initialize a DesktopEnvironment.
+  const DesktopEnvironmentOptions desktop_environment_options_;
 
   // The maximum duration of any session.
   base::TimeDelta max_session_duration_;
