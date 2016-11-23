00c35dbfc8545d7873ea83a27db6671129123489
treib <treib@chromium.org>
2016-11-23 08:12:46 -0800

    ntp_tiles::metrics cleanup: Merge RecordPageImpression and RecordImpressionTileTypes
    BUG=514752
    
    Review-Url: https://codereview.chromium.org/2523223002
    Cr-Commit-Position: refs/heads/master@{#434176}

--- components/ntp_tiles/metrics.h
+++ components/ntp_tiles/metrics.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_NTP_TILES_METRICS_H_
 #define COMPONENTS_NTP_TILES_METRICS_H_
 
+#include <utility>
 #include <vector>
 
 #include "components/ntp_tiles/ntp_tile.h"
@@ -39,14 +40,10 @@ enum MostVisitedTileType {
 // Records an impression of an individual tile.
 void RecordTileImpression(int index, NTPTileSource source);
 
-// Records an impression of the page itself, after all tiles have loaded.
-void RecordPageImpression(int number_of_tiles);
-
-// Records the visual types (see above) of all visible tiles.
-// TODO(treib): Merge this with RecordPageImpression.
-void RecordImpressionTileTypes(
-    const std::vector<MostVisitedTileType>& tile_types,
-    const std::vector<NTPTileSource>& sources);
+// Records an impression of the page itself, after all tiles have loaded,
+// including the visual types (see above) of all visible tiles.
+void RecordPageImpression(
+    const std::vector<std::pair<NTPTileSource, MostVisitedTileType>>& tiles);
 
 // Records a click on a tile.
 void RecordTileClick(int index,
