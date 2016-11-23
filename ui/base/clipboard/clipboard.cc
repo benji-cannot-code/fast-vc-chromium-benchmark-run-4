155b25b23ec55feeacc8b91e39875c1afe5406e6
fdoray <fdoray@chromium.org>
2016-11-23 08:21:05 -0800

    Disable ui::Clipboard on the FILE thread.
    We found no evidence that ui::Clipboard is used on the
    FILE thread. This CL disallows usage of ui::Clipboard on
    the FILE thread and adds a DumpWithoutCrashing() call to
    detect when it's used on unauthorized threads.
    
    Disallowing usage of ui::Clipboard on the FILE thread
    will facilitate its migration to TaskScheduler.
    
    BUG=662055
    
    Review-Url: https://codereview.chromium.org/2518363002
    Cr-Commit-Position: refs/heads/master@{#434178}

--- ui/base/clipboard/clipboard.cc
+++ ui/base/clipboard/clipboard.cc
@@ -5,10 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/base/clipboard/clipboard.h"
 
+#include <algorithm>
 #include <iterator>
 #include <limits>
 #include <memory>
 
+#include "base/debug/dump_without_crashing.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "third_party/skia/include/core/SkBitmap.h"
@@ -133,22 +135,19 @@ void Clipboard::DispatchObject(ObjectType type, const ObjectMapParams& params) {
 }
 
 base::PlatformThreadId Clipboard::GetAndValidateThreadID() {
-  base::PlatformThreadId id = base::PlatformThread::CurrentId();
-#ifndef NDEBUG
-  AllowedThreadsVector* allowed_threads = allowed_threads_.Pointer();
-  if (!allowed_threads->empty()) {
-    bool found = false;
-    for (AllowedThreadsVector::const_iterator it = allowed_threads->begin();
-         it != allowed_threads->end(); ++it) {
-      if (*it == id) {
-        found = true;
-        break;
-      }
-    }
+  clipboard_map_lock_.Get().AssertAcquired();
 
-    DCHECK(found);
+  const base::PlatformThreadId id = base::PlatformThread::CurrentId();
+
+  // TODO(fdoray): Surround this block with #if DCHECK_IS_ON() and remove the
+  // DumpWithoutCrashing() call once https://crbug.com/662055 is resolved.
+  AllowedThreadsVector* allowed_threads = allowed_threads_.Pointer();
+  if (!allowed_threads->empty() &&
+      std::find(allowed_threads->begin(), allowed_threads->end(), id) ==
+          allowed_threads->end()) {
+    NOTREACHED();
+    base::debug::DumpWithoutCrashing();
   }
-#endif
 
   return id;
 }
