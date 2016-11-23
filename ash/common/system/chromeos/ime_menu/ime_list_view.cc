08263bc21e67aa80c5a40caf34e425f2414f731a
azurewei <azurewei@chromium.org>
2016-11-22 19:20:35 -0800

    Set the right ChromeVox spoken feedback for IME row.
    As described in the bug, fixed the broken ChromeVox spoken feedback for IME row:
    Output "<IME name>", followed by "check box", followed by either "checked" or "not checked" depending on the checked state of the row.
    
    BUG=666526
    TEST=Verified on local build.
    
    Review-Url: https://codereview.chromium.org/2518863002
    Cr-Commit-Position: refs/heads/master@{#434102}

--- ash/common/system/chromeos/ime_menu/ime_list_view.cc
+++ ash/common/system/chromeos/ime_menu/ime_list_view.cc
@@ -85,7 +85,8 @@ class ImeListItemView : public ActionableView {
                   bool selected,
                   const SkColor button_color)
       : ActionableView(owner, TrayPopupInkDropStyle::FILL_BOUNDS),
-        ime_list_view_(list_view) {
+        ime_list_view_(list_view),
+        selected_(selected) {
     if (MaterialDesignController::IsSystemTrayMenuMaterial())
       SetInkDropMode(InkDropHostView::InkDropMode::ON);
 
@@ -133,6 +134,7 @@ class ImeListItemView : public ActionableView {
           gfx::VectorIconId::CHECK_CIRCLE, kMenuIconSize, button_color));
       tri_view->AddView(TriView::Container::END, checked_image);
     }
+    SetAccessibleName(label_->text());
   }
 
   ~ImeListItemView() override {}
@@ -148,6 +150,13 @@ class ImeListItemView : public ActionableView {
     UpdateStyle();
   }
 
+  void GetAccessibleNodeData(ui::AXNodeData* node_data) override {
+    ActionableView::GetAccessibleNodeData(node_data);
+    node_data->role = ui::AX_ROLE_CHECK_BOX;
+    node_data->AddStateFlag(selected_ ? ui::AX_STATE_CHECKED
+                                      : ui::AX_STATE_NONE);
+  }
+
  private:
   // Updates the style of |label_| based on the current native theme.
   void UpdateStyle() {
@@ -158,8 +167,9 @@ class ImeListItemView : public ActionableView {
 
   // The label shows the IME name.
   views::Label* label_;
-
   ImeListView* ime_list_view_;
+  bool selected_;
+
   DISALLOW_COPY_AND_ASSIGN(ImeListItemView);
 };
 
