3958a0e3972a4c4e78b0544b933032c7e861270d
jschuh <jschuh@chromium.org>
2016-11-22 17:08:14 -0800

    Remove remaining base dependencies from base/numerics
    All that was left was base/logging.h. And since this gets pulled in for
    a lot of other projects, it's best to make it dependency clean.
    It also necessitated several IWYU fixes for the missing header.
    
    TBR=jam@chromium.org,
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2523663002
    Cr-Commit-Position: refs/heads/master@{#434057}

--- ui/gfx/geometry/rect.h
+++ ui/gfx/geometry/rect.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <iosfwd>
 #include <string>
 
+#include "base/logging.h"
 #include "build/build_config.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/safe_integer_conversions.h"
