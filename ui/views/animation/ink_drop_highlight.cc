a448f106552aff15bc4019d140a9568b460105d5
bruthig <bruthig@chromium.org>
2016-11-23 20:47:12 -0800

    Disabled ink drop animations based on gfx::Animation::ShouldRenderRichAnimation().
    Forces an animation duration of 0 for all ink drop animations if
    ShouldRenderRichAnimations() returns false.
    
    BUG=658384
    
    Review-Url: https://codereview.chromium.org/2520353004
    Cr-Commit-Position: refs/heads/master@{#434301}

--- ui/views/animation/ink_drop_highlight.cc
+++ ui/views/animation/ink_drop_highlight.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/layer.h"
 #include "ui/compositor/layer_animation_sequence.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
+#include "ui/gfx/animation/animation.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/views/animation/ink_drop_highlight_observer.h"
 #include "ui/views/animation/ink_drop_painted_layer_delegates.h"
@@ -95,6 +96,11 @@ void InkDropHighlight::AnimateFade(AnimationType animation_type,
                                    const base::TimeDelta& duration,
                                    const gfx::Size& initial_size,
                                    const gfx::Size& target_size) {
+  // Ink drop animations are controlled by the system animation settings for
+  // accessibility reasons."See https://crbug.com/658384.
+  const base::TimeDelta effective_duration =
+      gfx::Animation::ShouldRenderRichAnimation() ? duration
+                                                  : base::TimeDelta();
   last_animation_initiated_was_fade_in_ = animation_type == FADE_IN;
 
   layer_->SetTransform(CalculateTransform(initial_size));
@@ -117,7 +123,7 @@ void InkDropHighlight::AnimateFade(AnimationType animation_type,
   ui::LayerAnimationElement* opacity_element =
       ui::LayerAnimationElement::CreateOpacityElement(
           animation_type == FADE_IN ? visible_opacity_ : kHiddenOpacity,
-          duration);
+          effective_duration);
   ui::LayerAnimationSequence* opacity_sequence =
       new ui::LayerAnimationSequence(opacity_element);
   opacity_sequence->AddObserver(animation_observer);
@@ -126,7 +132,7 @@ void InkDropHighlight::AnimateFade(AnimationType animation_type,
   if (initial_size != target_size) {
     ui::LayerAnimationElement* transform_element =
         ui::LayerAnimationElement::CreateTransformElement(
-            CalculateTransform(target_size), duration);
+            CalculateTransform(target_size), effective_duration);
     ui::LayerAnimationSequence* transform_sequence =
         new ui::LayerAnimationSequence(transform_element);
     transform_sequence->AddObserver(animation_observer);
