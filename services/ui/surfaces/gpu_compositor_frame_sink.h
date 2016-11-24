84eb155752d89925b9a9f6fdc72ca475b7e79203
fsamuel <fsamuel@chromium.org>
2016-11-24 08:38:09 -0800

    Mus: GpuCompositorFrameSink uses WeakPtrFactory
    A Surface can outlive the GpuCompositorFrameSink that created it.
    Submitting a CompositorFrame to a surface means that the draw callback
    could get called after GpuCompositorFrameSink is destroyed. By binding
    the draw callback to a weak ptr we ensure we don't call it if
    GpuCompositorFrameSink has been destroyed.
    
    BUG=668420
    TBR=yzshen@chromium.org, sadrul@chromium.org
    
    Review-Url: https://codereview.chromium.org/2527203002
    Cr-Commit-Position: refs/heads/master@{#434347}

--- services/ui/surfaces/gpu_compositor_frame_sink.h
+++ services/ui/surfaces/gpu_compositor_frame_sink.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
+#include "base/memory/weak_ptr.h"
 #include "cc/ipc/compositor_frame.mojom.h"
 #include "cc/ipc/mojo_compositor_frame_sink.mojom.h"
 #include "cc/output/context_provider.h"
@@ -129,6 +130,8 @@ class GpuCompositorFrameSink : public cc::mojom::MojoCompositorFrameSink,
   cc::mojom::MojoCompositorFrameSinkClientPtr client_;
   mojo::Binding<cc::mojom::MojoCompositorFrameSink> binding_;
   mojo::Binding<cc::mojom::MojoCompositorFrameSinkPrivate> private_binding_;
+
+  base::WeakPtrFactory<GpuCompositorFrameSink> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(GpuCompositorFrameSink);
 };
