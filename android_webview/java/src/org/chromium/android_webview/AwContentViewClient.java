aa6f3deefc0c472074a2ccc19fd446e4d9d77c42
torne <torne@chromium.org>
2016-11-24 07:19:56 -0800

    WebView: Add scheme whitelist for content intents.
    Use the content intent scheme whitelist introduced in
    https://codereview.chromium.org/2448363003 in WebView, which overrides
    the default implementation.
    
    BUG=659477
    
    Review-Url: https://codereview.chromium.org/2524843003
    Cr-Commit-Position: refs/heads/master@{#434325}

--- android_webview/java/src/org/chromium/android_webview/AwContentViewClient.java
+++ android_webview/java/src/org/chromium/android_webview/AwContentViewClient.java
@@ -6,14 +6,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.android_webview;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.KeyEvent;
 
+import org.chromium.base.Log;
 import org.chromium.content.browser.ContentViewClient;
+
+import java.net.URISyntaxException;
 
 /**
  * ContentViewClient implementation for WebView
  */
 public class AwContentViewClient extends ContentViewClient {
+    private static final String TAG = "AwCVC";
+
     private final AwContentsClient mAwContentsClient;
     private final AwSettings mAwSettings;
     private final AwContents mAwContents;
@@ -34,6 +40,20 @@ public class AwContentViewClient extends ContentViewClient {
 
     @Override
     public void onStartContentIntent(Context context, String contentUrl, boolean isMainFrame) {
+        // Make sure that this URL is a valid scheme for this callback if interpreted as an intent,
+        // even though we don't dispatch it as an intent here, because many WebView apps will once
+        // it reaches them.
+        String scheme = null;
+        try {
+            Intent intent = Intent.parseUri(contentUrl, Intent.URI_INTENT_SCHEME);
+            scheme = intent.getScheme();
+        } catch (URISyntaxException e) {
+            // Just don't set the scheme, it will be rejected.
+        }
+        if (!isAcceptableContentIntentScheme(scheme)) {
+            Log.w(TAG, "Invalid scheme for URI %s", contentUrl);
+            return;
+        }
         // Comes from WebViewImpl::detectContentOnTouch in Blink, so must be user-initiated, and
         // isn't a redirect.
         mAwContentsClient.shouldIgnoreNavigation(context, contentUrl, isMainFrame, true, false);
