017c5291a4c3680be8ea1241bbd33d2bfced8960
mohsen <mohsen@chromium.org>
2016-11-23 21:15:09 -0800

    Add round rect mask to overflow button's ripple
    BUG=667889
    TEST=manual
    
    Review-Url: https://codereview.chromium.org/2524183002
    Cr-Commit-Position: refs/heads/master@{#434305}

--- ash/common/shelf/overflow_button.cc
+++ ash/common/shelf/overflow_button.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/vector_icons_public.h"
 #include "ui/views/animation/flood_fill_ink_drop_ripple.h"
 #include "ui/views/animation/ink_drop_impl.h"
+#include "ui/views/animation/ink_drop_mask.h"
 
 namespace ash {
 
@@ -109,6 +110,12 @@ bool OverflowButton::ShouldEnterPushedState(const ui::Event& event) {
 void OverflowButton::NotifyClick(const ui::Event& event) {
   CustomButton::NotifyClick(event);
   shelf_view_->ButtonPressed(this, event, GetInkDrop());
+}
+
+std::unique_ptr<views::InkDropMask> OverflowButton::CreateInkDropMask() const {
+  gfx::Insets insets = GetLocalBounds().InsetsFrom(CalculateButtonBounds());
+  return base::MakeUnique<views::RoundRectInkDropMask>(
+      size(), insets, kOverflowButtonCornerRadius);
 }
 
 void OverflowButton::PaintBackground(gfx::Canvas* canvas,
