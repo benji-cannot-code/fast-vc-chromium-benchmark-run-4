cf8a7420bf660848f070aa982297d18d9863582f
mlippautz <mlippautz@chromium.org>
2016-11-24 11:43:41 -0800

    [wrapper-tracing] Fix leak in RemotePlayback round 2
    BUG=chromium:667746
    
    Review-Url: https://codereview.chromium.org/2531563002
    Cr-Commit-Position: refs/heads/master@{#434382}

--- third_party/WebKit/Source/modules/remoteplayback/RemotePlayback.cpp
+++ third_party/WebKit/Source/modules/remoteplayback/RemotePlayback.cpp
@@ -203,6 +203,7 @@ bool RemotePlayback::hasPendingActivity() const {
 
 void RemotePlayback::contextDestroyed() {
   m_availabilityCallbacks.clear();
+  m_promptPromiseResolver = nullptr;
 }
 
 void RemotePlayback::notifyInitialAvailability(int callbackId) {
