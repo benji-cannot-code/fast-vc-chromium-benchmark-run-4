a0ddddab9580e8a34d419955bd6195ef52bba5a7
jif <jif@chromium.org>
2016-11-24 06:56:43 -0800

    Removes the WebStateObserverUserDataWrapper when the webstate is destroyed.
    In turn, this destroys the ReadingListWebStateObserver. This is necessary because
    the WebStateObserver's destructor requires a valid webstate.
    Previously we were waiting for the webstate's superclass, SupportsUserData, to
    destroy the WebStateObserverUserDataWrapper (and thus the
    ReadingListWebStateObserver).
    
    BUG=None.
    
    Review-Url: https://codereview.chromium.org/2525343002
    Cr-Commit-Position: refs/heads/master@{#434321}

--- ios/chrome/browser/reading_list/reading_list_web_state_observer.mm
+++ ios/chrome/browser/reading_list/reading_list_web_state_observer.mm
@@ -91,6 +91,7 @@ void ReadingListWebStateObserver::PageLoaded(
 
 void ReadingListWebStateObserver::WebStateDestroyed() {
   timer_.reset();
+  web_state()->RemoveUserData(kObserverKey);
 }
 
 void ReadingListWebStateObserver::StartCheckingProgress() {
