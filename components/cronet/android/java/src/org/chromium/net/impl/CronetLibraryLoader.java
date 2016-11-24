2b94e691c1364b5e17de14fa0f25008946c836b3
pauljensen <pauljensen@chromium.org>
2016-11-24 06:45:42 -0800

    [Cronet] Remove unused CronetEngine.Builder.setLibraryName() API
    BUG=629299
    R=kapishnikov
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_cronet_tester
    
    Review-Url: https://codereview.chromium.org/2441463002
    Cr-Commit-Position: refs/heads/master@{#434319}

--- components/cronet/android/java/src/org/chromium/net/impl/CronetLibraryLoader.java
+++ components/cronet/android/java/src/org/chromium/net/impl/CronetLibraryLoader.java
@@ -23,6 +23,7 @@ import org.chromium.net.NetworkChangeNotifier;
 public class CronetLibraryLoader {
     // Synchronize initialization.
     private static final Object sLoadLock = new Object();
+    private static final String LIBRARY_NAME = "cronet";
     private static final String TAG = "CronetLibraryLoader";
     // Has library loading commenced?  Setting guarded by sLoadLock.
     private static volatile boolean sInitStarted = false;
@@ -42,9 +43,9 @@ public class CronetLibraryLoader {
             sInitStarted = true;
             ContextUtils.initApplicationContext(applicationContext);
             if (builder.libraryLoader() != null) {
-                builder.libraryLoader().loadLibrary(builder.libraryName());
+                builder.libraryLoader().loadLibrary(LIBRARY_NAME);
             } else {
-                System.loadLibrary(builder.libraryName());
+                System.loadLibrary(LIBRARY_NAME);
             }
             ContextUtils.initApplicationContextForNative();
             if (!ImplVersion.CRONET_VERSION.equals(nativeGetCronetVersion())) {
