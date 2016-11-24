21360d12503233ba8dc1ef82450707075efdd6d0
fdoray <fdoray@chromium.org>
2016-11-24 09:33:08 -0800

    Initialize TaskScheduler in Mash.
    This CL initializes TaskScheduler in MashRunner::RunMain() and sets
    a flag to redirect all SequencedWorkerPool tasks to it
    
    That means that all tasks posted to the |blocking_pool_| in
    service_manager::Context will be redirected to TaskScheduler. Call
    sites will be migrated to the base/task_scheduler/post_task.h API
    and the |blocking_pool_| will be removed in separate CLs.
    
    BUG=664996
    
    Review-Url: https://codereview.chromium.org/2525073002
    Cr-Commit-Position: refs/heads/master@{#434360}

--- chrome/app/mash/mash_runner.cc
+++ chrome/app/mash/mash_runner.cc
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "base/process/launch.h"
 #include "base/run_loop.h"
+#include "base/task_scheduler/task_scheduler.h"
+#include "base/threading/sequenced_worker_pool.h"
 #include "base/trace_event/trace_event.h"
 #include "components/tracing/common/trace_to_console.h"
 #include "components/tracing/common/tracing_switches.h"
@@ -36,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/service_manager/runner/host/child_process.h"
 #include "services/service_manager/runner/host/child_process_base.h"
 #include "services/service_manager/runner/init.h"
+#include "services/service_manager/standalone/context.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/ui_base_paths.h"
 #include "ui/base/ui_base_switches.h"
@@ -128,6 +131,10 @@ int MashRunner::Run() {
 }
 
 void MashRunner::RunMain() {
+  base::TaskScheduler::CreateAndSetSimpleTaskScheduler(
+      service_manager::kThreadPoolMaxThreads);
+  base::SequencedWorkerPool::EnableWithRedirectionToTaskSchedulerForProcess();
+
   // TODO(sky): refactor BackgroundServiceManager so can supply own context, we
   // shouldn't we using context as it has a lot of stuff we don't really want
   // in chrome.
@@ -170,9 +177,16 @@ void MashRunner::RunMain() {
   // Ping mash_session to ensure an instance is brought up
   context_->connector()->Connect("mash_session");
   base::RunLoop().Run();
+
+  base::TaskScheduler::GetInstance()->Shutdown();
 }
 
 int MashRunner::RunChild() {
+  // TODO(fdoray): Add TaskScheduler initialization code in
+  // service_manager::ServiceRunner. TaskScheduler can't be initialized here
+  // because it wouldn't be visible to the service's dynamic library.
+  // https://crbug.com/664996
+
   base::FilePath path =
       base::CommandLine::ForCurrentProcess()->GetSwitchValuePath(
           switches::kChildProcess);
