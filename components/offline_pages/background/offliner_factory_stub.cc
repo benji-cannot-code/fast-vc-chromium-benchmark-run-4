b1f80cc4673816858fa77c041d7f5e9cda643294
fgorski <fgorski@chromium.org>
2016-11-24 13:44:52 -0800

    [Offline pages] Refactoring request_coordinator_unittest.cc to create RC builder
    Extracting classes defined in the RC request_coordinator_unittest in
    order to include them in test_request_coordinator_factory.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2523603004
    Cr-Commit-Position: refs/heads/master@{#434391}

---
+++ components/offline_pages/background/offliner_factory_stub.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/offline_pages/background/offliner_factory_stub.h"
+
+#include "components/offline_pages/background/offliner_stub.h"
+
+namespace offline_pages {
+
+OfflinerFactoryStub::OfflinerFactoryStub() : offliner_(nullptr) {}
+
+OfflinerFactoryStub::~OfflinerFactoryStub() {}
+
+Offliner* OfflinerFactoryStub::GetOffliner(const OfflinerPolicy* policy) {
+  if (offliner_.get() == nullptr) {
+    offliner_.reset(new OfflinerStub());
+  }
+  return offliner_.get();
+}
+
+}  // namespace offline_pages
