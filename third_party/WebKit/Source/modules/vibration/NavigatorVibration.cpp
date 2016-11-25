dda0a0264abebc682c9b28ad0a56a5af2bed2011
lunalu <lunalu@chromium.org>
2016-11-24 18:09:41 -0800

    Implementation for feature policy - vibrate. Disable Navigator.vibrate() unless enabled through feature policy.
    Added tests with runtime flag is on
    
    Also modified IsFeatureEnabledInFrame for the case EnableForSelf, should only be enabled when with same origin.
    
    BUG=623682
    
    Review-Url: https://codereview.chromium.org/2492623002
    Cr-Commit-Position: refs/heads/master@{#434423}

--- third_party/WebKit/Source/modules/vibration/NavigatorVibration.cpp
+++ third_party/WebKit/Source/modules/vibration/NavigatorVibration.cpp
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "modules/vibration/NavigatorVibration.h"
 
+#include "bindings/core/v8/ConditionalFeatures.h"
 #include "core/dom/Document.h"
 #include "core/frame/LocalDOMWindow.h"
 #include "core/frame/LocalFrame.h"
@@ -78,12 +79,18 @@ bool NavigatorVibration::vibrate(Navigator& navigator,
   if (!frame->page()->isPageVisible())
     return false;
 
-  if (frame->isCrossOriginSubframe()) {
-    // TODO(binlu): Once FeaturePolicy is ready, exploring using it to
-    // remove the API instead of having it return false.
-    frame->localDOMWindow()->printErrorMessage(
-        "A call of navigator.vibrate will be no-op inside cross-origin "
-        "iframes: https://www.chromestatus.com/feature/5682658461876224.");
+  // TODO(lunalu): When FeaturePolicy is ready, take out the check for the
+  // runtime flag.
+  if (!isFeatureEnabledInFrame(blink::kVibrateFeature, frame)) {
+    if (RuntimeEnabledFeatures::featurePolicyEnabled()) {
+      frame->localDOMWindow()->printErrorMessage(
+          "Navigator.vibrate() is not enabled in feature policy for this "
+          "frame.");
+    } else {
+      frame->localDOMWindow()->printErrorMessage(
+          "A call of navigator.vibrate will be no-op inside cross-origin "
+          "iframes: https://www.chromestatus.com/feature/5682658461876224.");
+    }
     return false;
   }
 
