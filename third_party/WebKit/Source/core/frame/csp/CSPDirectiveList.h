d7fa4ec7b4e6f057d2db03ad8222a84df61b2da3
magjed <magjed@chromium.org>
2016-11-25 04:04:29 -0800

    Revert of Embedding-CSP: Refactoring directive strings into enum (patchset #6 id:140001 of https://codereview.chromium.org/2516383002/ )
    Reason for revert:
    Causing WebKit Win x64 Builder to fail on webkit_unit_tests on Windows-7-SP1 ContentSecurityPolicyTest.DirectiveType:
    ContentSecurityPolicyTest.DirectiveType (run #1):
    [ RUN      ] ContentSecurityPolicyTest.DirectiveType
    c:\c\win_layout\src	hird_party\webkit\source\core
    rame\csp\contentsecuritypolicytest.cpp(950): error: Death test: ContentSecurityPolicy::getDirectiveName( ContentSecurityPolicy::DirectiveType::Undefined)
        Result: failed to die.
     Error msg:
    [  DEATH   ] [3244:5672:1125/030930:3521831:WARNING:test_suite.cc(236)] Test launcher output path C:\Users\CHROME~2\AppData\Local\Temp!6_27934	est_results.xml exists. Not adding test launcher result printer.
    [  DEATH   ]
    [  FAILED  ] ContentSecurityPolicyTest.DirectiveType (67 ms)
    
    https://uberchromegw.corp.google.com/i/chromium.webkit/builders/WebKit%20Win%20x64%20Builder/builds/101390
    
    Original issue's description:
    > Embedding-CSP: Refactoring directive strings into enum.
    >
    > This is an effort as part of the experimental feature. Causing
    > patch: https://codereview.chromium.org/2474903002
    >
    > BUG=647588
    >
    > Committed: https://crrev.com/a5e9944b7169dd3a3c9f2cb352aff3551b32cd12
    > Cr-Commit-Position: refs/heads/master@{#434474}
    
    TBR=mkwst@chromium.org,jochen@chromium.org,amalika@google.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=647588
    
    Review-Url: https://codereview.chromium.org/2528133002
    Cr-Commit-Position: refs/heads/master@{#434483}

--- third_party/WebKit/Source/core/frame/csp/CSPDirectiveList.h
+++ third_party/WebKit/Source/core/frame/csp/CSPDirectiveList.h
@@ -191,17 +191,17 @@ class CORE_EXPORT CSPDirectiveList
   SourceListDirective* operativeDirective(SourceListDirective*,
                                           SourceListDirective* override) const;
   void reportViolation(const String& directiveText,
-                       const ContentSecurityPolicy::DirectiveType&,
+                       const String& effectiveDirective,
                        const String& consoleMessage,
                        const KURL& blockedURL,
                        ResourceRequest::RedirectStatus) const;
   void reportViolationWithFrame(const String& directiveText,
-                                const ContentSecurityPolicy::DirectiveType&,
+                                const String& effectiveDirective,
                                 const String& consoleMessage,
                                 const KURL& blockedURL,
                                 LocalFrame*) const;
   void reportViolationWithLocation(const String& directiveText,
-                                   const ContentSecurityPolicy::DirectiveType&,
+                                   const String& effectiveDirective,
                                    const String& consoleMessage,
                                    const KURL& blockedURL,
                                    const String& contextURL,
@@ -209,7 +209,7 @@ class CORE_EXPORT CSPDirectiveList
                                    Element*) const;
   void reportViolationWithState(
       const String& directiveText,
-      const ContentSecurityPolicy::DirectiveType&,
+      const String& effectiveDirective,
       const String& message,
       const KURL& blockedURL,
       ScriptState*,
@@ -248,11 +248,10 @@ class CORE_EXPORT CSPDirectiveList
                                      bool isScript,
                                      const String& hashValue) const;
 
-  bool checkSourceAndReportViolation(
-      SourceListDirective*,
-      const KURL&,
-      const ContentSecurityPolicy::DirectiveType&,
-      ResourceRequest::RedirectStatus) const;
+  bool checkSourceAndReportViolation(SourceListDirective*,
+                                     const KURL&,
+                                     const String& effectiveDirective,
+                                     ResourceRequest::RedirectStatus) const;
   bool checkMediaTypeAndReportViolation(MediaListDirective*,
                                         const String& type,
                                         const String& typeAttribute,
