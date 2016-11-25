9ed373f295a01f89c6390a1301b8c69a9380dc02
mstensho <mstensho@opera.com>
2016-11-25 00:53:18 -0800

    [LayoutNG] Unit tests for MinAndMaxContentSizes::ShrinkToFit().
    Also DCHECK in the implementation that max_content isn't less than min_content.
    
    Review-Url: https://codereview.chromium.org/2528433006
    Cr-Commit-Position: refs/heads/master@{#434458}

--- third_party/WebKit/Source/core/layout/ng/ng_units.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_units.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 LayoutUnit MinAndMaxContentSizes::ShrinkToFit(LayoutUnit available_size) const {
+  DCHECK_GE(max_content, min_content);
   return std::min(max_content, std::max(min_content, available_size));
 }
 
