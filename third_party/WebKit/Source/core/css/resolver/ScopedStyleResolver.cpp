143fa38808f64c3239cd6dcf9b7cacaefa86083f
rune <rune@opera.com>
2016-11-24 21:41:35 -0800

    Check for styleResolver() in preparation for async style update.
    Currently, active stylesheets are appended to ScopedStyleResolver
    through the StyleResolver. When we move to async stylesheet update with
    ActiveStyleSheets being appended from StyleEngine, styleResolver() is
    typically null the first time we update the active stylesheets.
    
    Add a null check before accessing styleResolver() when adding
    @font-face rules.
    
    R=nainar@chromium.org,meade@chromium.org
    BUG=567021
    
    Review-Url: https://codereview.chromium.org/2522423002
    Cr-Commit-Position: refs/heads/master@{#434437}

--- third_party/WebKit/Source/core/css/resolver/ScopedStyleResolver.cpp
+++ third_party/WebKit/Source/core/css/resolver/ScopedStyleResolver.cpp
@@ -80,7 +80,7 @@ void ScopedStyleResolver::addFontFaceRules(const RuleSet& ruleSet) {
       cssFontSelector->fontFaceCache()->add(cssFontSelector, fontFaceRule,
                                             fontFace);
   }
-  if (fontFaceRules.size())
+  if (fontFaceRules.size() && document.styleResolver())
     document.styleResolver()->invalidateMatchedPropertiesCache();
 }
 
