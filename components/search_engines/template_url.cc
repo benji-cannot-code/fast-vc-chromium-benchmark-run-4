6edc3f22c9c7fa715a001d598c74fc6fe75977ca
a-v-y <a-v-y@yandex-team.ru>
2016-11-25 00:39:47 -0800

    Fix for google:instantExtendedEnabledKey replacement
    This is fix for minor bug that was inserted in
    google:instantExtendedEnabledKey replacement in
    https://codereview.chromium.org/2516963002/.
    Incorrect string literal was used in replacement.
    
    R=treib@chromium.org, pkasting@chromium.org
    
    Review-Url: https://codereview.chromium.org/2524733008
    Cr-Commit-Position: refs/heads/master@{#434456}

--- components/search_engines/template_url.cc
+++ components/search_engines/template_url.cc
@@ -69,6 +69,8 @@ const char kDefaultCount[] = "10";
 const char kOutputEncodingType[] = "UTF-8";
 
 constexpr char kGoogleInstantExtendedEnabledKey[] =
+    "google:instantExtendedEnabledKey";
+constexpr char kGoogleInstantExtendedEnabledKeyFull[] =
     "{google:instantExtendedEnabledKey}";
 
 // Attempts to encode |terms| and |original_query| in |encoding| and escape
@@ -172,10 +174,10 @@ bool SearchTermsReplacementKeysMatch(
   if (search_terms_replacement_key1 == search_terms_replacement_key2)
     return true;
   if (search_terms_replacement_key1 == google_util::kInstantExtendedAPIParam &&
-      search_terms_replacement_key2 == kGoogleInstantExtendedEnabledKey)
+      search_terms_replacement_key2 == kGoogleInstantExtendedEnabledKeyFull)
     return true;
   if (search_terms_replacement_key2 == google_util::kInstantExtendedAPIParam &&
-      search_terms_replacement_key1 == kGoogleInstantExtendedEnabledKey)
+      search_terms_replacement_key1 == kGoogleInstantExtendedEnabledKeyFull)
     return true;
   return false;
 }
@@ -1214,7 +1216,8 @@ TemplateURL::TemplateURL(const TemplateURLData& data, Type type)
   ResizeURLRefVector();
   SetPrepopulateId(data_.prepopulate_id);
 
-  if (data_.search_terms_replacement_key == kGoogleInstantExtendedEnabledKey)
+  if (data_.search_terms_replacement_key ==
+      kGoogleInstantExtendedEnabledKeyFull)
     data_.search_terms_replacement_key = google_util::kInstantExtendedAPIParam;
 }
 
