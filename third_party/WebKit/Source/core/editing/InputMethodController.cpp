494f6f67c3abb346589f227e149d57e155365c21
yoichio <yoichio@chromium.org>
2016-11-25 01:30:08 -0800

    [Editing] Let InputMethodController::hasComposition check Range::connected().
    InputMethodConroller(abbr as IMC) has members of |bool m_hasComposition| and
     |Range m_compositionRange| and a function hasComposition(), which returns
     m_hasComposition;
    Usually they sync, or m_hasComposition == (m_compositionRange.connected());
    
    However, it brakes when m_compositionRange crosses over node which will be removed.
    In the situation, m_hasComposition is true but m_compositionRange.connected()
     returns false.
    If we call IMC::finishCompositionText under the condition, calling composingText,
     which creates text from range, causes crash because the range is not connected.
    
    This CL change hasCompostion to check also m_compositionRange.
    This CL also insert DCHECK(hasComposition()) before where m_compositionRange is used.
    
    Attached test checks the crash but also confirms IMC::textInputType
     while changing focus.
    
    BUG=666659,664317
    
    Review-Url: https://codereview.chromium.org/2529803002
    Cr-Commit-Position: refs/heads/master@{#434467}

--- third_party/WebKit/Source/core/editing/InputMethodController.cpp
+++ third_party/WebKit/Source/core/editing/InputMethodController.cpp
@@ -183,7 +183,8 @@ Document& InputMethodController::document() const {
 }
 
 bool InputMethodController::hasComposition() const {
-  return m_hasComposition;
+  return m_hasComposition && !m_compositionRange->collapsed() &&
+         m_compositionRange->isConnected();
 }
 
 inline Editor& InputMethodController::editor() const {
@@ -314,6 +315,7 @@ bool InputMethodController::replaceCompositionAndMoveCaret(
   Element* rootEditableElement = frame().selection().rootEditableElement();
   if (!rootEditableElement)
     return false;
+  DCHECK(hasComposition());
   PlainTextRange compositionRange =
       PlainTextRange::create(*rootEditableElement, *m_compositionRange);
   if (compositionRange.isNull())
