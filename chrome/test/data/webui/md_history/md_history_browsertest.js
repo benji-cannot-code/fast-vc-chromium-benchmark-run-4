be3c5dab8b446aa4b83d6f87b5c0df424fba19f8
tsergeant <tsergeant@chromium.org>
2016-11-24 20:35:48 -0800

    MD History: Remove unnecessary namespacing from tests (part 1)
    These tests followed the (outdated) practice of defining tests
    inside a dedicated `registerTests` function. This no longer has any
    benefit for isolating tests, and just reduces the amount of horizontal
    space available for test code.
    
    This CL only removes these registerTests functions and deindents
    the corresponding code -- there are no functional changes to any
    tests.
    
    BUG=647115
    
    Review-Url: https://codereview.chromium.org/2504813002
    Cr-Commit-Position: refs/heads/master@{#434433}

--- chrome/test/data/webui/md_history/md_history_browsertest.js
+++ chrome/test/data/webui/md_history/md_history_browsertest.js
@@ -112,7 +112,6 @@ MaterialHistoryItemTest.prototype = {
 };
 
 TEST_F('MaterialHistoryItemTest', 'All', function() {
-  md_history.history_item_test.registerTests();
   mocha.run();
 });
 
@@ -127,7 +126,6 @@ MaterialHistoryListTest.prototype = {
 };
 
 TEST_F('MaterialHistoryListTest', 'All', function() {
-  md_history.history_list_test.registerTests();
   mocha.run();
 });
 
@@ -142,7 +140,6 @@ MaterialHistoryMetricsTest.prototype = {
 };
 
 TEST_F('MaterialHistoryMetricsTest', 'All', function() {
-  md_history.history_metrics_test.registerTests();
   mocha.run();
 });
 
@@ -157,7 +154,6 @@ MaterialHistoryOverflowMenuTest.prototype = {
 };
 
 TEST_F('MaterialHistoryOverflowMenuTest', 'All', function() {
-  md_history.history_overflow_menu_test.registerTests();
   mocha.run();
 });
 
@@ -221,7 +217,6 @@ MaterialHistorySyncedTabsTest.prototype = {
 
 // Fails on Mac, http://crbug.com/640862
 TEST_F('MaterialHistorySyncedTabsTest', 'DISABLED_All', function() {
-  md_history.history_synced_tabs_test.registerTests();
   mocha.run();
 });
 
@@ -242,7 +237,6 @@ MaterialHistorySupervisedUserTest.prototype = {
 };
 
 TEST_F('MaterialHistorySupervisedUserTest', 'All', function() {
-  md_history.history_supervised_user_test.registerTests();
   mocha.run();
 });
 
