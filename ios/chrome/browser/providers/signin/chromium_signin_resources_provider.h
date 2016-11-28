f91470302f0b1120853533e3d7b94abb4f49e73e
rohitrao <rohitrao@chromium.org>
2016-11-28 06:07:32 -0800

    [ios] Adds signin resources and styled text fields to  ChromiumBrowserProvider.
    These additions prevent crashes when running the signin tests and when trying to
    edit a bookmark.
    
    BUG=666375
    
    Review-Url: https://codereview.chromium.org/2529203002
    Cr-Commit-Position: refs/heads/master@{#434649}

---
+++ ios/chrome/browser/providers/signin/chromium_signin_resources_provider.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_PROVIDERS_SIGNIN_CHROMIUM_SIGNIN_RESOURCES_PROVIDER_H_
+#define IOS_CHROME_BROWSER_PROVIDERS_SIGNIN_CHROMIUM_SIGNIN_RESOURCES_PROVIDER_H_
+
+#include "base/macros.h"
+#include "ios/public/provider/chrome/browser/signin/signin_resources_provider.h"
+
+// ChromiumSigninResourcesProvider provides resources that the app expects to
+// exist, even when signin is disabled.
+class ChromiumSigninResourcesProvider : public ios::SigninResourcesProvider {
+ public:
+  ChromiumSigninResourcesProvider();
+  ~ChromiumSigninResourcesProvider() override;
+
+  // SigninResourcesProvider implementation:
+  UIImage* GetDefaultAvatar() override;
+  NSString* GetLocalizedString(ios::SigninStringID string_id) override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(ChromiumSigninResourcesProvider);
+};
+
+#endif  // IOS_CHROME_BROWSER_PROVIDERS_SIGNIN_CHROMIUM_SIGNIN_RESOURCES_PROVIDER_H_
