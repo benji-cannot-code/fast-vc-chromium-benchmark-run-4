9d329bf6f80fe2ce260c13adc6529e164d1bc5c5
jbroman <jbroman@chromium.org>
2016-11-28 09:59:53 -0800

    Enable V8BasedStructuredClone by default.
    Layout test expected results are changed for the following reasons:
    
    - Some exception messages are formatted slightly differently
      (e.g. to convey the type of the object which failed to be cloned).
    
    - blink::ScriptValueSerializer emitted frequent "object count validation"
      tags (the most frequently seen is 3f00) which are not required by
      either deserializer, but the new one does not.
    
    - Excessive depth throws a RangeError, like JSON.stringify and other
      functions that internally reach excessive depth (this is consistent
      with Safari, and similar to how Edge also throws the same exception as
      for an ordinary stack overflow in script).
    
    - Certain large numbers are encoded differently; specifically, V8 now
      chooses between signed-int and double representations depending on
      whether the value has a small-integer (SMI) encoding in memory (this
      value range varies by CPU architecture). The old implementation tried
      to fit in a signed-int, then tried to fit in an unsigned-int, and only
      used double representation if that failed. Both implementations can
      read all representations, so this is a compatible change. Tests that
      are sensitive to variation by CPU architecture are commented out; there
      are unit tests that ensure that this serialization works correctly.
    
    BUG=148757
    
    Review-Url: https://codereview.chromium.org/2517813002
    Cr-Commit-Position: refs/heads/master@{#434685}

--- third_party/WebKit/LayoutTests/fast/dom/Window/script-tests/postmessage-clone-really-deep-array.js
+++ third_party/WebKit/LayoutTests/fast/dom/Window/script-tests/postmessage-clone-really-deep-array.js
@@ -3,5 +3,5 @@ document.getElementById("description").innerHTML = "Tests that we abort cloning 
 reallyDeepArray=[];
 for (var i = 0; i < 100000; i++)
     reallyDeepArray=[reallyDeepArray];
-tryPostMessage('reallyDeepArray', true, null, DOMException.DATA_CLONE_ERR);
+tryPostMessage('reallyDeepArray', true, null, RangeError);
 tryPostMessage('"done"');
