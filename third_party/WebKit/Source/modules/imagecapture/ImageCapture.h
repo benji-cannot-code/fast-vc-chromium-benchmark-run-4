2f6720f38fa3e05709ae63bb48e9af7b06352ed9
mek <mek@chromium.org>
2016-11-28 12:20:55 -0800

    Revert of ImageCapture: move image_capture.mojom from media/mojo/interfaces/ to media/capture/mojo/ (patchset #2 id:40001 of https://codereview.chromium.org/2526953002/ )
    Reason for revert:
    imagecapture layout tests started failing in https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Mac10.9/builds/40056
    
    Original issue's description:
    > ImageCapture: move image_capture.mojom to media/capture/mojo/
    >
    > This CL moves image_capture.mojom from media/mojo/interfaces/ to
    > media/capture/mojo/ where it belongs logically speaking, and
    > updates BUILD.gns and include paths.
    >
    > image_capture.mojom was in media/mojo/interfaces because
    > when it landed, media/capture/mojo was not existing.
    >
    > BUG=518807
    > TEST=No code touched, if compile passes, it works
    > (but there's also content_browsertests).
    >
    > TBR=xhwang@chromium.org for the removal in media/mojo/interfaces/BUILD.gn
    >
    > Committed: https://crrev.com/d7c801bb7ed0393d1634e547a696ced5040bc93c
    > Cr-Commit-Position: refs/heads/master@{#434675}
    
    TBR=mkwst@chromium.org,chfremer@chromium.org,jochen@chromium.org,tsepez@chromium.org,mcasas@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=518807
    
    Review-Url: https://codereview.chromium.org/2537573002
    Cr-Commit-Position: refs/heads/master@{#434705}

--- third_party/WebKit/Source/modules/imagecapture/ImageCapture.h
+++ third_party/WebKit/Source/modules/imagecapture/ImageCapture.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/ScriptPromise.h"
 #include "core/dom/ContextLifecycleObserver.h"
 #include "core/events/EventTarget.h"
-#include "media/capture/mojo/image_capture.mojom-blink.h"
+#include "media/mojo/interfaces/image_capture.mojom-blink.h"
 #include "modules/EventTargetModules.h"
 #include "modules/ModulesExport.h"
 #include "platform/AsyncMethodRunner.h"
