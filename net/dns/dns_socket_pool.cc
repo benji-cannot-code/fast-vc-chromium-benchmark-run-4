c040f3b675b08f39a000cf899e1659dbdef8204d
juliatuttle <juliatuttle@chromium.org>
2016-11-28 14:48:43 -0800

    Make DnsSocketPool copy RandIntCallback
    DnsSocketPool is created by DnsClient, and passed a reference to the
    RandIntCallback in DnsClient. It is then stored in DnsSession, which is
    ref-counted (so running transactions can continue while DnsSession is
    recreated on config changes).
    
    Unfortunately, if the DnsClient itself is destroyed, the DnsSession
    hangs around, holding on to the DnsSocketPool, which is holding on to a
    reference to the RandIntCallback in the destroyed DnsClient.
    
    Fix this by copying the RandIntCallback into the DnsSocketPool when it
    is constructed.
    
    (Also, rearrange destructor to satisfy the linter.)
    
    BUG=664457
    
    Review-Url: https://codereview.chromium.org/2533753003
    Cr-Commit-Position: refs/heads/master@{#434752}

--- net/dns/dns_socket_pool.cc
+++ net/dns/dns_socket_pool.cc
@@ -154,6 +154,8 @@ class DefaultDnsSocketPool : public DnsSocketPool {
   DISALLOW_COPY_AND_ASSIGN(DefaultDnsSocketPool);
 };
 
+DnsSocketPool::~DnsSocketPool() {}
+
 // static
 std::unique_ptr<DnsSocketPool> DnsSocketPool::CreateDefault(
     ClientSocketFactory* factory,
