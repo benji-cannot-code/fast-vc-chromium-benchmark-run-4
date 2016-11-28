58721fc3b628c25eb85f67475496db23bbc4686f
cco3 <cco3@chromium.org>
2016-11-28 12:48:32 -0800

    Notify PW data source of URLs on first register
    Currently, when we create the Physical Web data source, we do not
    populate it with any initial data, which can lead to onLosts occuring
    when there has not even been an onFound.  This change call onFound for
    currently known URLs when first creating the data source.
    
    BUG=667754
    
    Review-Url: https://codereview.chromium.org/2521333002
    Cr-Commit-Position: refs/heads/master@{#434712}

--- chrome/android/java/src/org/chromium/chrome/browser/physicalweb/UrlManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/physicalweb/UrlManager.java
@@ -694,6 +694,9 @@ class UrlManager {
                             @Override
                             public void onSuccess(boolean alreadyStarted) {
                                 mNativePhysicalWebDataSourceAndroid = nativeInit();
+                                for (UrlInfo urlInfo : getUrls()) {
+                                    safeNotifyNativeListenersOnFound(urlInfo.getUrl());
+                                }
                             }
 
                             @Override
