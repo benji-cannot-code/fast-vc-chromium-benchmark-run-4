439305dd24ddd21f65b80ddc106812ff17beba5b
tkent <tkent@chromium.org>
2016-11-28 01:06:23 -0800

    Make DEFINE_GLOBAL macro compatible with clang-format.
    This CL has no behavior changes.
    
    Review-Url: https://codereview.chromium.org/2530333002
    Cr-Commit-Position: refs/heads/master@{#434621}

--- third_party/WebKit/Source/wtf/StaticConstructors.h
+++ third_party/WebKit/Source/wtf/StaticConstructors.h
@@ -35,6 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // alignment issue.
 #define DEFINE_GLOBAL(type, name, ...)                                     \
   void* name##Storage[(sizeof(type) + sizeof(void*) - 1) / sizeof(void*)]; \
-  const type& name = *reinterpret_cast<type*>(&name##Storage);
+  const type& name = *reinterpret_cast<type*>(&name##Storage)
 
 #endif  // StaticConstructors_h
