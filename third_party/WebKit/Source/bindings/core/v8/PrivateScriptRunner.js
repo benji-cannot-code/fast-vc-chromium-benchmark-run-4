2bd1027184ea6897e056573fa28b8d615439857f
marius.mlynski <marius.mlynski@gmail.com>
2016-11-28 02:18:01 -0800

    Don't touch the prototype chain to get the private script controller.
    Prior to this patch, private scripts attempted to get the
    "privateScriptController" property off the global object without verifying if
    the property actually exists on the global. If the property hasn't been set yet,
    this operation could descend into the prototype chain and potentially return
    a named property from the WindowProperties object, leading to release asserts
    and general confusion.
    
    BUG=668552
    
    Review-Url: https://codereview.chromium.org/2529163002
    Cr-Commit-Position: refs/heads/master@{#434627}

--- third_party/WebKit/Source/bindings/core/v8/PrivateScriptRunner.js
+++ third_party/WebKit/Source/bindings/core/v8/PrivateScriptRunner.js
@@ -140,7 +140,7 @@ PrivateScriptController.prototype = {
     },
 }
 
-if (typeof window.privateScriptController === 'undefined')
+if (!window.hasOwnProperty("privateScriptController"))
     window.privateScriptController = new PrivateScriptController();
 
 // This line must be the last statement of this JS file.
