cd7948ffb33def623ac8be5ba54721f8fdb09048
haraken <haraken@chromium.org>
2016-11-28 00:51:33 -0800

    Add a missing ScriptForbiddenScope::AllowUserAgentScript to HTMLMarqueeElement's constructor
    HTMLMarqueeElement's constructor can run Blink-in-JS.
    So we need to have ScriptForbiddenScope::AllowUserAgentScript.
    
    BUG=667099
    
    Review-Url: https://codereview.chromium.org/2535673002
    Cr-Commit-Position: refs/heads/master@{#434619}

--- third_party/WebKit/Source/core/html/HTMLMarqueeElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLMarqueeElement.cpp
@@ -28,12 +28,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/HTMLNames.h"
 #include "core/dom/Document.h"
 #include "core/frame/UseCounter.h"
+#include "platform/ScriptForbiddenScope.h"
 
 namespace blink {
 
 inline HTMLMarqueeElement::HTMLMarqueeElement(Document& document)
     : HTMLElement(HTMLNames::marqueeTag, document) {
   if (document.contextDocument()) {
+    ScriptForbiddenScope::AllowUserAgentScript script;
     v8::Local<v8::Value> classObject =
         PrivateScriptRunner::installClassIfNeeded(&document,
                                                   "HTMLMarqueeElement");
