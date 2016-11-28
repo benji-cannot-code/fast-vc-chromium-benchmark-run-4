c30463e4ca8b079dc55d90825b34b7c1f5202241
tnagel <tnagel@chromium.org>
2016-11-28 06:13:52 -0800

    Remove unnecessary plumbing for policy verification key
    The key is hard-coded anyways, thus there's no benefit in passing it
    around.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2507423002
    Cr-Commit-Position: refs/heads/master@{#434652}

--- chrome/browser/chromeos/policy/device_cloud_policy_store_chromeos.cc
+++ chrome/browser/chromeos/policy/device_cloud_policy_store_chromeos.cc
@@ -62,8 +62,7 @@ void DeviceCloudPolicyStoreChromeOS::Store(
   std::unique_ptr<DeviceCloudPolicyValidator> validator(
       CreateValidator(policy));
   validator->ValidateSignatureAllowingRotation(
-      public_key->as_string(), GetPolicyVerificationKey(),
-      install_attributes_->GetDomain());
+      public_key->as_string(), install_attributes_->GetDomain());
   validator->ValidateAgainstCurrentPolicy(
       device_settings_service_->policy_data(),
       CloudPolicyValidatorBase::TIMESTAMP_FULLY_VALIDATED,
@@ -91,8 +90,7 @@ void DeviceCloudPolicyStoreChromeOS::InstallInitialPolicy(
 
   std::unique_ptr<DeviceCloudPolicyValidator> validator(
       CreateValidator(policy));
-  validator->ValidateInitialKey(GetPolicyVerificationKey(),
-                                install_attributes_->GetDomain());
+  validator->ValidateInitialKey(install_attributes_->GetDomain());
   validator.release()->StartValidation(
       base::Bind(&DeviceCloudPolicyStoreChromeOS::OnPolicyToStoreValidated,
                  weak_factory_.GetWeakPtr()));
