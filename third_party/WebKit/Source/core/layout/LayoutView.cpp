e9648483a84c7ffb1c1a1e77c0ca4320f4c10cfa
chrishtr <chrishtr@chromium.org>
2016-11-28 09:17:47 -0800

    Add tracing for LayoutView::commitPendingSelection
    Committing selection can sometimes be slow; see the attached bug for an
    example.
    
    BUG=668122
    
    Review-Url: https://codereview.chromium.org/2536733003
    Cr-Commit-Position: refs/heads/master@{#434671}

--- third_party/WebKit/Source/core/layout/LayoutView.cpp
+++ third_party/WebKit/Source/core/layout/LayoutView.cpp
@@ -818,6 +818,7 @@ bool LayoutView::hasPendingSelection() const {
 }
 
 void LayoutView::commitPendingSelection() {
+  TRACE_EVENT0("blink", "LayoutView::commitPendingSelection");
   m_frameView->frame().selection().commitAppearanceIfNeeded(*this);
 }
 
