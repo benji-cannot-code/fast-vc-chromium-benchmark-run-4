3c8220e2843c94e2c0ebe6c3a54852ab654f5d60
drott <drott@chromium.org>
2016-11-29 02:20:22 -0800

    Improve fallback for Burmese with leading punctuation + spacing mark
    Reland after revert in 1b8f4236e2dbf4e07bf0171300003168633b8660,
    previous CL https://codereview.chromium.org/2530153002/
    Previously failing Win10 test needs a rebaseline.
    
    Issue 618178 describes an example where a Burmese text run starts with a
    leading punctuation character followed by a combining spacing mark. This
    grapheme cannot be shaped with the default font, since Times for example
    cannot display the combination of a left quote with a Burmese combining
    mark. Our fallback code attempts to find a fallback font based on the
    first character at the beginning of an extracted unshaped sub-run, which
    does not lead to finding a font suitable for Myanmar text in this case.
    So in a way it runs into a fallback trap, where no fallback hint helps
    to find the right fallback font and the whole run ends up as notdef
    glyphs.
    
    This CL attempts to resolve this by looking for a better fallback hint
    character, which is not script common or inherited, if such is
    available. This improves the situation for the Burmese text from the
    issue report.
    
    In addition, as a better fix we should give higher importance to the
    locale information in font fallback, filed as issue 668706.
    
    BUG=618178
    TBR=eae,kojii,behdad
    
    Review-Url: https://codereview.chromium.org/2539733002
    Cr-Commit-Position: refs/heads/master@{#434944}

--- third_party/WebKit/LayoutTests/inspector-protocol/layout-fonts/resources/layout-font-test.js
+++ third_party/WebKit/LayoutTests/inspector-protocol/layout-fonts/resources/layout-font-test.js
@@ -52,6 +52,10 @@ function test()
         InspectorTest.evaluateInInspectedPage("injectCollectedResultsInPage(" +
                                               JSON.stringify(collectedFontUsage) +
                                               ")");
+        InspectorTest.evaluateInInspectedPage("postTestHookWithFontResults(" +
+                                              JSON.stringify(collectedFontUsage) +
+                                              ")");
+
     }
 
     function platformFontsForElementWithSelector(selector)
