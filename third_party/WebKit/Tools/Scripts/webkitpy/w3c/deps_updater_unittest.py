b6703c251550414d97374275396fd39ae69b668a
jeffcarp <jeffcarp@chromium.org>
2016-11-29 03:16:05 -0800

    Add NOEXPORT=true to imported commits
    BUG=667975
    
    Review-Url: https://codereview.chromium.org/2535893002
    Cr-Commit-Position: refs/heads/master@{#434958}

--- third_party/WebKit/Tools/Scripts/webkitpy/w3c/deps_updater_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/w3c/deps_updater_unittest.py
@@ -71,7 +71,8 @@ class DepsUpdaterTest(unittest.TestCase):
         self.assertEqual(
             description,
             ('Last commit message\n'
-             'TBR=qyearsley@chromium.org'))
+             'TBR=qyearsley@chromium.org\n'
+             'NOEXPORT=true'))
         self.assertEqual(host.executive.calls, [['git', 'log', '-1', '--format=%B']])
 
     def test_cl_description_with_environ_variables(self):
@@ -86,5 +87,6 @@ class DepsUpdaterTest(unittest.TestCase):
             description,
             ('Last commit message\n'
              'Build: https://build.chromium.org/p/my.master/builders/b/builds/123\n\n'
-             'TBR=qyearsley@chromium.org'))
+             'TBR=qyearsley@chromium.org\n'
+             'NOEXPORT=true'))
         self.assertEqual(host.executive.calls, [['git', 'log', '-1', '--format=%B']])
