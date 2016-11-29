032978914df984d92b5b225976c4bfc407f048e7
yhirano <yhirano@chromium.org>
2016-11-28 19:50:22 -0800

    Remove ResourceLoaderInfo::encodedDataLength
    It is redundant as we have the field in ResourceResponse.
    
    BUG=666217
    
    Review-Url: https://codereview.chromium.org/2521483002
    Cr-Commit-Position: refs/heads/master@{#434875}

--- third_party/WebKit/Source/platform/network/ResourceLoadInfo.h
+++ third_party/WebKit/Source/platform/network/ResourceLoadInfo.h
@@ -33,11 +33,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 struct ResourceLoadInfo : RefCounted<ResourceLoadInfo> {
-  ResourceLoadInfo() : httpStatusCode(0), encodedDataLength(-1) {}
+  ResourceLoadInfo() : httpStatusCode(0) {}
 
   int httpStatusCode;
   String httpStatusText;
-  long long encodedDataLength;
   HTTPHeaderMap requestHeaders;
   HTTPHeaderMap responseHeaders;
   String requestHeadersText;
