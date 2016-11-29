fb89598ce86d487d8635e638c35b54ec1026a836
yusufo <yusufo@chromium.org>
2016-11-29 09:55:56 -0800

    [CCT-Reparenting] Remove tab observer when the tab is removed from model
    In tab reparenting, we never close but only remove the tab from the
    model. During this process, both tab observer and also the metrics
    observer was still kept, which leaked over some custom tab features into
    the tab even after it was reparented. This removes the observers and
    fixes the related issues.
    
    TEST=CustomTabActivityTest#testTabReparentingBasic
    BUG=669169
    
    Review-Url: https://codereview.chromium.org/2539643002
    Cr-Commit-Position: refs/heads/master@{#435028}

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTest.java
@@ -42,6 +42,7 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import org.chromium.base.ObserverList.RewindableIterator;
 import org.chromium.base.PathUtils;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.library_loader.LibraryLoader;
@@ -66,6 +67,7 @@ import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabObserver;
+import org.chromium.chrome.browser.tab.TabTestUtils;
 import org.chromium.chrome.browser.tabmodel.EmptyTabModelObserver;
 import org.chromium.chrome.browser.tabmodel.TabModel.TabLaunchType;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
@@ -1727,6 +1729,10 @@ public class CustomTabActivityTest extends CustomTabActivityTestBase {
         assertEquals("The tab should never be hidden during the reparenting process",
                 0, tabHiddenHelper.getCallCount());
         tabToBeReparented.removeObserver(observer);
+        RewindableIterator<TabObserver> observers = TabTestUtils.getTabObservers(tabToBeReparented);
+        while (observers.hasNext()) {
+            assertFalse(observers.next() instanceof CustomTabObserver);
+        }
         return newActivity;
     }
 
