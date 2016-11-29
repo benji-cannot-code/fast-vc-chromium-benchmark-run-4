a3ae5c79a52bed18026aefb7d9f555592ea01a8e
mthiesse <mthiesse@chromium.org>
2016-11-28 17:05:04 -0800

    Implement Virtual Display class for Android.
    This CL defines a base class for DisplayAndroid, and separates out an implementation for an Android Display backed DisplayAndroidImpl, and a VirtualDisplayAndroid (currently intended for use by Chrome's VRShell and WebVR). Unlike the DisplayAndroidImpl, VirtualDisplayAndroid is mutable to display clients.
    
    The display ID for a DisplayAndroid can no longer be assumed to match that of an Android Display.
    
    BUG=643480
    
    Review-Url: https://codereview.chromium.org/2523273002
    Cr-Commit-Position: refs/heads/master@{#434806}

---
+++ ui/android/java/src/org/chromium/ui/display/VirtualDisplayAndroid.java
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.ui.display;
+
+import android.graphics.Point;
+
+/**
+ * An instance of DisplayAndroid not associated with any physical display.
+ */
+public class VirtualDisplayAndroid extends DisplayAndroid {
+    public static VirtualDisplayAndroid createVirtualDisplay() {
+        return getManager().addVirtualDisplay();
+    }
+
+    /* package */ VirtualDisplayAndroid(int displayId) {
+        super(displayId);
+    }
+
+    /**
+     * @param other Sets the properties of this display to those of the other display.
+     */
+    public void setTo(DisplayAndroid other) {
+        update(new Point(other.getDisplayWidth(), other.getDisplayHeight()),
+                new Point(other.getPhysicalDisplayWidth(), other.getPhysicalDisplayHeight()),
+                other.getDipScale(), other.getBitsPerPixel(),
+                other.getBitsPerComponent(), other.getRotation());
+    }
+
+    @Override
+    public void update(Point size, Point physicalSize, Float dipScale, Integer bitsPerPixel,
+            Integer bitsPerComponent, Integer rotation) {
+        super.update(size, physicalSize, dipScale, bitsPerPixel, bitsPerComponent, rotation);
+    }
+
+    /**
+     * Removes this Virtual Display from the DisplayManger.
+     */
+    public void destroy() {
+        getManager().removeVirtualDisplay(this);
+    }
+}
