126ac456fbcced57da2739267982b2021a8d61b4
spqchan <spqchan@chromium.org>
2016-11-29 00:10:53 -0800

    [Mac] Omnibox text and icon spacing
    - Moved the BubbleDecoration one pt to the left
    - Moved the Omnibox text one pt to the left
    
    BUG=666990, 666977
    
    Review-Url: https://codereview.chromium.org/2520003007
    Cr-Commit-Position: refs/heads/master@{#434929}

--- chrome/browser/ui/cocoa/location_bar/bubble_decoration.mm
+++ chrome/browser/ui/cocoa/location_bar/bubble_decoration.mm
@@ -17,8 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// This is used to increase the right margin of this decoration.
-const CGFloat kLeftSidePadding = 6.0;
+// This is used to increase the left padding of this decoration.
+const CGFloat kLeftSidePadding = 5.0;
 
 // Padding between the icon/label and bubble edges.
 const CGFloat kBubblePadding = 7.0;
