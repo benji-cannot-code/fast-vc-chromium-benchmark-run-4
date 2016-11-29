3fb40b8ee72d84553842bc6607a2f0061117fb4e
jcivelli <jcivelli@chromium.org>
2016-11-29 08:15:05 -0800

    Provide a Mojo equivalent of ThreadSafeSender.
    Introduces the ThreadSafeAssociatedInterfacePtrProvider class that
    provides functionalities similar to the content::ThreadSafeSender.
    You create it with a ChannelProxy and you can then retrieve
    ThreadSafeInterfacePtr's from it that you can call methods on from any
    thread and even before the actual channel is connected.
    
    BUG=668317
    
    Review-Url: https://codereview.chromium.org/2522333002
    Cr-Commit-Position: refs/heads/master@{#435005}

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -112,6 +112,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/media/render_media_client.h"
 #include "content/renderer/media/renderer_gpu_video_accelerator_factories.h"
 #include "content/renderer/media/video_capture_impl_manager.h"
+#include "content/renderer/mojo/thread_safe_associated_interface_ptr_provider.h"
 #include "content/renderer/net_info_helper.h"
 #include "content/renderer/p2p/socket_dispatcher.h"
 #include "content/renderer/render_frame_proxy.h"
@@ -582,6 +583,13 @@ mojom::RenderMessageFilter* RenderThreadImpl::current_render_message_filter() {
 }
 
 // static
+const scoped_refptr<mojom::ThreadSafeRenderMessageFilterAssociatedPtr>&
+RenderThreadImpl::current_thread_safe_render_message_filter() {
+  DCHECK(current());
+  return current()->thread_safe_render_message_filter();
+}
+
+// static
 void RenderThreadImpl::SetRenderMessageFilterForTesting(
     mojom::RenderMessageFilter* render_message_filter) {
   g_render_message_filter_for_testing = render_message_filter;
@@ -687,6 +695,12 @@ void RenderThreadImpl::Init(
   AddFilter(blob_message_filter_.get());
   db_message_filter_ = new DBMessageFilter();
   AddFilter(db_message_filter_.get());
+
+  thread_safe_associated_interface_ptr_provider_ =
+      base::MakeUnique<ThreadSafeAssociatedInterfacePtrProvider>(channel());
+  thread_safe_render_message_filter_ =
+      thread_safe_associated_interface_ptr_provider_
+          ->CreateInterfacePtr<mojom::RenderMessageFilter>();
 
   vc_manager_.reset(new VideoCaptureImplManager());
 
@@ -2109,6 +2123,11 @@ mojom::RenderMessageFilter* RenderThreadImpl::render_message_filter() {
   if (!render_message_filter_)
     GetChannel()->GetRemoteAssociatedInterface(&render_message_filter_);
   return render_message_filter_.get();
+}
+
+const scoped_refptr<mojom::ThreadSafeRenderMessageFilterAssociatedPtr>&
+RenderThreadImpl::thread_safe_render_message_filter() {
+  return thread_safe_render_message_filter_;
 }
 
 gpu::GpuChannelHost* RenderThreadImpl::GetGpuChannel() {
