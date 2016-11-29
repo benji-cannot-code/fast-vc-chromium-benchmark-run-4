cf9fc5a77eb21007e53777e9db2c54fdb5abad03
xhwang <xhwang@chromium.org>
2016-11-29 14:06:46 -0800

    media: Add |bad_message_cb_| to MojoRendererService
    This helps move the StrongBindingPtr out of MojoRendererService, so that
    we can bind MojoRendererService using other type of bindings, e.g.
    mojo::Binding.
    
    When we do use MojoRendererService::Create(), which uses
    mojo::StrongBinding, now the StrongBindingPtr isimplicitly owned by the
    MojoRendererService through the |bad_message_cb_|.
    
    BUG=604912,669606
    TEST=Covered by media_mojo_unittests
    
    Review-Url: https://codereview.chromium.org/2539703002
    Cr-Commit-Position: refs/heads/master@{#435088}

--- media/mojo/services/mojo_renderer_service.cc
+++ media/mojo/services/mojo_renderer_service.cc
@@ -19,6 +19,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
+namespace {
+
+void CloseBindingOnBadMessage(mojo::StrongBindingPtr<mojom::Renderer> binding) {
+  LOG(ERROR) << __func__;
+  DCHECK(binding);
+  binding->Close();
+}
+
+}  // namespace
+
 // Time interval to update media time.
 const int kTimeUpdateIntervalMs = 50;
 
@@ -38,7 +48,7 @@ mojo::StrongBindingPtr<mojom::Renderer> MojoRendererService::Create(
       mojo::MakeStrongBinding<mojom::Renderer>(base::WrapUnique(service),
                                                std::move(request));
 
-  service->binding_ = binding;
+  service->set_bad_message_cb(base::Bind(&CloseBindingOnBadMessage, binding));
 
   return binding;
 }
@@ -272,7 +282,10 @@ void MojoRendererService::InitiateScopedSurfaceRequest(
     // |renderer_| is likely not of type MediaPlayerRenderer.
     // This is an unexpected call, and the connection should be closed.
     mojo::ReportBadMessage("Unexpected call to InitiateScopedSurfaceRequest.");
-    binding_->Close();
+
+    // This may cause |this| to be destructed.
+    DCHECK(!bad_message_cb_.is_null());
+    bad_message_cb_.Run();
     return;
   }
 
