880be7f8fecd1b7b189b425f48656eeaee7ecb47
dominicc <dominicc@chromium.org>
2016-11-29 01:23:48 -0800

    After detach, there's no document to notify parsing is finished.
    HTMLViewSourceParser was tripping the DCHECK in
    DocumentParser::document when it recieved the last bytes of the
    document it was parsing after it was detached.
    
    BUG=664915
    
    Review-Url: https://codereview.chromium.org/2537043002
    Cr-Commit-Position: refs/heads/master@{#434937}

--- third_party/WebKit/Source/core/html/HTMLViewSourceDocument.h
+++ third_party/WebKit/Source/core/html/HTMLViewSourceDocument.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef HTMLViewSourceDocument_h
 #define HTMLViewSourceDocument_h
 
+#include "core/CoreExport.h"
 #include "core/html/HTMLDocument.h"
 
 namespace blink {
@@ -34,7 +35,7 @@ class HTMLTableCellElement;
 class HTMLTableSectionElement;
 class HTMLToken;
 
-class HTMLViewSourceDocument final : public HTMLDocument {
+class CORE_EXPORT HTMLViewSourceDocument final : public HTMLDocument {
  public:
   enum SourceAnnotation { AnnotateSourceAsSafe, AnnotateSourceAsXSS };
 
