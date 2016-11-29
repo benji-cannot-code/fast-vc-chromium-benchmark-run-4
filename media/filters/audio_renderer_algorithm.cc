8cce37b9a81c1a4e1fc987cb595d7d45dba87d42
dalecurtis <dalecurtis@chromium.org>
2016-11-28 19:28:32 -0800

    Fix implicit channel layout configurations at non-1.0 playback rates.
    Change http://crrev.com/21d17ca06e742cbfdcaaa54e0f4964248e3fbfff
    incorrectly assumed the input channel layout never changes. This
    patch fixes that issue by allowing the channel mask to change as
    layouts change.
    
    BUG=666813
    TEST=updated unittests
    TEST=http://storage.googleapis.com/chcunningham-chrome-shared/534301/aac_test.html
    
    Review-Url: https://codereview.chromium.org/2536013002
    Cr-Commit-Position: refs/heads/master@{#434868}

--- media/filters/audio_renderer_algorithm.cc
+++ media/filters/audio_renderer_algorithm.cc
@@ -83,11 +83,9 @@ AudioRendererAlgorithm::AudioRendererAlgorithm()
 
 AudioRendererAlgorithm::~AudioRendererAlgorithm() {}
 
-void AudioRendererAlgorithm::Initialize(const AudioParameters& params,
-                                        std::vector<bool> channel_mask) {
+void AudioRendererAlgorithm::Initialize(const AudioParameters& params) {
   CHECK(params.IsValid());
 
-  channel_mask_ = std::move(channel_mask);
   channels_ = params.channels();
   samples_per_second_ = params.sample_rate();
   initial_capacity_ = capacity_ =
@@ -127,8 +125,14 @@ void AudioRendererAlgorithm::Initialize(const AudioParameters& params,
 
   // If no mask is provided, assume all channels are valid.
   if (channel_mask_.empty())
-    channel_mask_ = std::vector<bool>(channels_, true);
-  DCHECK_EQ(channel_mask_.size(), static_cast<size_t>(channels_));
+    SetChannelMask(std::vector<bool>(channels_, true));
+}
+
+void AudioRendererAlgorithm::SetChannelMask(std::vector<bool> channel_mask) {
+  DCHECK_EQ(channel_mask.size(), static_cast<size_t>(channels_));
+  channel_mask_ = std::move(channel_mask);
+  if (ola_window_)
+    CreateSearchWrappers();
 }
 
 int AudioRendererAlgorithm::FillBuffer(AudioBus* dest,
@@ -204,21 +208,8 @@ int AudioRendererAlgorithm::FillBuffer(AudioBus* dest,
         channels_, num_candidate_blocks_ + (ola_window_size_ - 1));
     target_block_ = AudioBus::Create(channels_, ola_window_size_);
 
-    // WSOLA is quite expensive to run, so if a channel mask exists, use it to
-    // reduce the size of our search space.
-    std::vector<float*> active_target_channels;
-    std::vector<float*> active_search_channels;
-    for (int ch = 0; ch < channels_; ++ch) {
-      if (channel_mask_[ch]) {
-        active_target_channels.push_back(target_block_->channel(ch));
-        active_search_channels.push_back(search_block_->channel(ch));
-      }
-    }
-
-    target_block_wrapper_ =
-        AudioBus::WrapVector(target_block_->frames(), active_target_channels);
-    search_block_wrapper_ =
-        AudioBus::WrapVector(search_block_->frames(), active_search_channels);
+    // Create potentially smaller wrappers for playback rate adaptation.
+    CreateSearchWrappers();
   }
 
   int rendered_frames = 0;
@@ -431,6 +422,24 @@ void AudioRendererAlgorithm::PeekAudioWithZeroPrepend(
   }
   audio_buffer_.PeekFrames(num_frames_to_read, read_offset_frames,
                            write_offset, dest);
+}
+
+void AudioRendererAlgorithm::CreateSearchWrappers() {
+  // WSOLA is quite expensive to run, so if a channel mask exists, use it to
+  // reduce the size of our search space.
+  std::vector<float*> active_target_channels;
+  std::vector<float*> active_search_channels;
+  for (int ch = 0; ch < channels_; ++ch) {
+    if (channel_mask_[ch]) {
+      active_target_channels.push_back(target_block_->channel(ch));
+      active_search_channels.push_back(search_block_->channel(ch));
+    }
+  }
+
+  target_block_wrapper_ =
+      AudioBus::WrapVector(target_block_->frames(), active_target_channels);
+  search_block_wrapper_ =
+      AudioBus::WrapVector(search_block_->frames(), active_search_channels);
 }
 
 }  // namespace media
