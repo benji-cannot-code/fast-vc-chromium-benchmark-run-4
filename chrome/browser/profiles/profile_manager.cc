3963bdfec30c695092a4dfb3b3a774b4c8f46c4e
palar <palar@yandex-team.ru>
2016-11-29 07:20:14 -0800

    Profile deletion: forbid fallback to guest profile.
    BUG=669420
    
    R=anthonyvd@chromium.org, bauerb@chromium.org
    
    Review-Url: https://codereview.chromium.org/2538753002
    Cr-Commit-Position: refs/heads/master@{#434993}

--- chrome/browser/profiles/profile_manager.cc
+++ chrome/browser/profiles/profile_manager.cc
@@ -1317,8 +1317,9 @@ void ProfileManager::EnsureActiveProfileExistsBeforeDeletion(
   // In case we delete non-active profile, just proceed.
   const base::FilePath last_used_profile =
       GetLastUsedProfileDir(user_data_dir_);
+  const base::FilePath guest_profile_path = GetGuestProfilePath();
   if (last_used_profile != profile_dir &&
-      last_used_profile != GetGuestProfilePath()) {
+      last_used_profile != guest_profile_path) {
     FinishDeletingProfile(profile_dir, last_used_profile);
     return;
   }
@@ -1328,6 +1329,7 @@ void ProfileManager::EnsureActiveProfileExistsBeforeDeletion(
     Profile* profile = browser->profile();
     base::FilePath cur_path = profile->GetPath();
     if (cur_path != profile_dir &&
+        cur_path != guest_profile_path &&
         !profile->IsLegacySupervised() &&
         !IsProfileDirectoryMarkedForDeletion(cur_path)) {
       OnNewActiveProfileLoaded(profile_dir, cur_path, callback, profile,
@@ -1346,6 +1348,7 @@ void ProfileManager::EnsureActiveProfileExistsBeforeDeletion(
     // Make sure that this profile is not pending deletion, and is not
     // legacy-supervised.
     if (cur_path != profile_dir &&
+        cur_path != guest_profile_path &&
         !entry->IsLegacySupervised() &&
         !IsProfileDirectoryMarkedForDeletion(cur_path)) {
       fallback_profile_path = cur_path;
