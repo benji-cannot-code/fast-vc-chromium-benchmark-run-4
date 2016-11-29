5e053d2691ea4fa956203ab8b752cb0bb0d83bd5
meade <meade@chromium.org>
2016-11-28 23:34:39 -0800

    Remove unused ExceptionState argument from CSSAngleValue constructor
    Review-Url: https://codereview.chromium.org/2528263004
    Cr-Commit-Position: refs/heads/master@{#434923}

--- third_party/WebKit/Source/core/css/cssom/CSSAngleValue.cpp
+++ third_party/WebKit/Source/core/css/cssom/CSSAngleValue.cpp
@@ -11,9 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-CSSAngleValue* CSSAngleValue::create(double value,
-                                     const String& unit,
-                                     ExceptionState& exceptionState) {
+CSSAngleValue* CSSAngleValue::create(double value, const String& unit) {
   CSSPrimitiveValue::UnitType primitiveUnit =
       CSSPrimitiveValue::stringToUnitType(unit);
   DCHECK(CSSPrimitiveValue::isAngle(primitiveUnit));
