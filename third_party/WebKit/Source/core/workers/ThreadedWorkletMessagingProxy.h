4335ebcab3ca5c3595cc227f7959897822268829
nhiroki <nhiroki@chromium.org>
2016-11-30 02:47:46 -0800

    Worker: Fix possible UAF in closures posted from ThreadedWorkletObjectProxy
    This fix is the same with the following CL for InProcessWorkerObjectProxy
    created by alexclarke@chromium.org: https://codereview.chromium.org/2478113002
    
    Tasks posted by ThreadedWorkletObjectProxy can be arbitrarily delayed so it's
    not safe to post a closure with an crossThreadUnretained pointer to the
    messaging proxy because we can't guarantee it's still there when the closure
    eventually gets run.
    
    We fix this with a WeakPtr.
    
    BUG=660427
    
    Review-Url: https://codereview.chromium.org/2534803003
    Cr-Commit-Position: refs/heads/master@{#435208}

--- third_party/WebKit/Source/core/workers/ThreadedWorkletMessagingProxy.h
+++ third_party/WebKit/Source/core/workers/ThreadedWorkletMessagingProxy.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/CoreExport.h"
 #include "core/workers/ThreadedMessagingProxyBase.h"
 #include "core/workers/WorkletGlobalScopeProxy.h"
+#include "wtf/WeakPtr.h"
 
 namespace blink {
 
@@ -33,6 +34,8 @@ class CORE_EXPORT ThreadedWorkletMessagingProxy
 
  private:
   std::unique_ptr<ThreadedWorkletObjectProxy> m_workletObjectProxy;
+
+  WeakPtrFactory<ThreadedWorkletMessagingProxy> m_weakPtrFactory;
 };
 
 }  // namespace blink
--- third_party/WebKit/Source/core/workers/ThreadedWorkletObjectProxy.h
+++ third_party/WebKit/Source/core/workers/ThreadedWorkletObjectProxy.h
@@ -13,21 +13,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class ConsoleMessage;
 class ExecutionContext;
 class ThreadedWorkletMessagingProxy;
 
 // A proxy to talk to the parent worklet object. This object is created on the
 // main thread, passed on to the worklet thread, and used just to proxy
 // messages to the ThreadedWorkletMessagingProxy on the main thread.
+// ThreadedWorkletMessagingProxy always outlives this proxy.
 class CORE_EXPORT ThreadedWorkletObjectProxy : public WorkerReportingProxy {
   USING_FAST_MALLOC(ThreadedWorkletObjectProxy);
   WTF_MAKE_NONCOPYABLE(ThreadedWorkletObjectProxy);
 
  public:
   static std::unique_ptr<ThreadedWorkletObjectProxy> create(
-      ThreadedWorkletMessagingProxy*);
-  ~ThreadedWorkletObjectProxy() override {}
+      const WeakPtr<ThreadedWorkletMessagingProxy>&);
+  ~ThreadedWorkletObjectProxy() override;
 
   void reportPendingActivity(bool hasPendingActivity);
 
@@ -47,13 +47,15 @@ class CORE_EXPORT ThreadedWorkletObjectProxy : public WorkerReportingProxy {
   void didTerminateWorkerThread() override;
 
  protected:
-  ThreadedWorkletObjectProxy(ThreadedWorkletMessagingProxy*);
+  ThreadedWorkletObjectProxy(const WeakPtr<ThreadedWorkletMessagingProxy>&);
 
  private:
   ExecutionContext* getExecutionContext() const;
 
-  // This object always outlives this proxy.
-  ThreadedWorkletMessagingProxy* m_messagingProxy;
+  // No guarantees about the lifetimes of tasks posted by this proxy wrt the
+  // ThreadedWorkletMessagingProxy so a weak pointer must be used when posting
+  // the tasks.
+  WeakPtr<ThreadedWorkletMessagingProxy> m_messagingProxyWeakPtr;
 };
 
 }  // namespace blink
