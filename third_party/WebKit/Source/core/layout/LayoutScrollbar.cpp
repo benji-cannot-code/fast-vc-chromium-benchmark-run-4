ff7e08fb3f22cab691fcf5032e78d65cb0f0598a
wangxianzhu <wangxianzhu@chromium.org>
2016-11-30 14:05:12 -0800

    Invalidate scrollbar part when it's created or destroyed
    BUG=667009
    
    Review-Url: https://codereview.chromium.org/2537243003
    Cr-Commit-Position: refs/heads/master@{#435436}

--- third_party/WebKit/Source/core/layout/LayoutScrollbar.cpp
+++ third_party/WebKit/Source/core/layout/LayoutScrollbar.cpp
@@ -293,10 +293,12 @@ void LayoutScrollbar::updateScrollbarPart(ScrollbarPart partType,
     partLayoutObject = LayoutScrollbarPart::createAnonymous(
         &owningLayoutObject()->document(), m_scrollableArea, this, partType);
     m_parts.set(partType, partLayoutObject);
+    setNeedsPaintInvalidation(partType);
   } else if (partLayoutObject && !needLayoutObject) {
     m_parts.remove(partType);
     partLayoutObject->destroy();
-    partLayoutObject = 0;
+    partLayoutObject = nullptr;
+    setNeedsPaintInvalidation(partType);
   }
 
   if (partLayoutObject)
