f6ed5bd458657ed8e497943a812018578128aa38
lhchavez <lhchavez@chromium.org>
2016-11-29 17:48:35 -0800

    Propagate information about how ARC apps are launched
    This change lets ARC know whether an app should be launched in Touch
    Mode[1] or not.
    
    1: http://android-developers.blogspot.com/2008/12/touch-mode.html
    
    BUG=669146
    TEST=ARC app starts in touch mode when activated using
         mouse/trackpad/touch
    TEST=ARC app starts in focus mode when activated using
         keyboard.
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2540433002
    Cr-Commit-Position: refs/heads/master@{#435115}

--- chrome/browser/ui/ash/launcher/arc_app_deferred_launcher_controller.h
+++ chrome/browser/ui/ash/launcher/arc_app_deferred_launcher_controller.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_ASH_LAUNCHER_ARC_APP_DEFERRED_LAUNCHER_CONTROLLER_H_
 
 #include <map>
+#include <string>
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
@@ -18,6 +19,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class ArcAppDeferredLauncherItemController;
 class ChromeLauncherControllerImpl;
 
+// ArcAppDeferredLauncherController displays visual feedback that the ARC
+// application the user has just activated is waiting for ARC to be ready, and
+// will be asynchronously launched as soon as it can.
 class ArcAppDeferredLauncherController
     : public ArcAppListPrefs::Observer,
       public arc::ArcSessionManager::Observer {
@@ -30,8 +34,10 @@ class ArcAppDeferredLauncherController
 
   base::TimeDelta GetActiveTime(const std::string& app_id) const;
 
-  // Registers deferred Arc app launch.
-  void RegisterDeferredLaunch(const std::string& app_id);
+  // Registers deferred ARC app launch. |app_id| is the app to be launched, and
+  // |event_flags| describes the original event flags that triggered the app's
+  // activation.
+  void RegisterDeferredLaunch(const std::string& app_id, int event_flags);
 
   // Applies spinning effect if requested app is handled by deferred controller.
   void MaybeApplySpinningEffect(const std::string& app_id,
