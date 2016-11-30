bffc5317dbb5baaa81bfe2d6f49f7dee7ae5dafd
stkhapugin <stkhapugin@chromium.org>
2016-11-30 09:51:44 -0800

    [ObjC ARC] Converts ios/chrome/browser/invalidation:invalidation to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2512123003
    Cr-Commit-Position: refs/heads/master@{#435322}

--- ios/chrome/browser/invalidation/ios_chrome_profile_invalidation_provider_factory.mm
+++ ios/chrome/browser/invalidation/ios_chrome_profile_invalidation_provider_factory.mm
@@ -28,6 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/web/public/web_client.h"
 #include "net/url_request/url_request_context_getter.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 using invalidation::InvalidatorStorage;
 using invalidation::ProfileInvalidationProvider;
 using invalidation::TiclInvalidationService;
