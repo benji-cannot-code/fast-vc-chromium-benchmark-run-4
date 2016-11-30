2abd23de47ba3edb4f356e885f1b50be7db13085
xhwang <xhwang@chromium.org>
2016-11-29 23:51:30 -0800

    media: Fix lifetime of InterfaceFactoryImpl and created impls
    Makes sure that when InterfaceFactoryImpl is destructed, all mojo
    interface implemenations (e.g. MojoAudioDecoderService) are destructed.
    
    To achieve this, a helper class StrongBindingSet is introduced such
    that:
    - When connection error happens, the entry will be removed from the
      binding set, and the bound impl object will be destructed.
    - When StrongBindingSet is destructed, it'll destruct all outstanding
      impl objects in the binding set.
    
    BUG=604912
    TEST=Added a unittest for StrongBindingSet.
    
    Review-Url: https://codereview.chromium.org/2530613003
    Cr-Commit-Position: refs/heads/master@{#435141}

--- media/mojo/services/interface_factory_impl.cc
+++ media/mojo/services/interface_factory_impl.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // defined(ENABLE_MOJO_VIDEO_DECODER)
 
 #if defined(ENABLE_MOJO_RENDERER)
+#include "base/bind_helpers.h"
 #include "media/base/audio_renderer_sink.h"
 #include "media/base/renderer_factory.h"
 #include "media/base/video_renderer_sink.h"
@@ -69,7 +70,7 @@ void InterfaceFactoryImpl::CreateAudioDecoder(
     return;
   }
 
-  mojo::MakeStrongBinding(
+  audio_decoder_bindings_.AddBinding(
       base::MakeUnique<MojoAudioDecoderService>(
           cdm_service_context_.GetWeakPtr(), std::move(audio_decoder)),
       std::move(request));
@@ -79,7 +80,7 @@ void InterfaceFactoryImpl::CreateAudioDecoder(
 void InterfaceFactoryImpl::CreateVideoDecoder(
     mojom::VideoDecoderRequest request) {
 #if defined(ENABLE_MOJO_VIDEO_DECODER)
-  mojo::MakeStrongBinding(
+  video_decoder_bindings_.AddBinding(
       base::MakeUnique<MojoVideoDecoderService>(mojo_media_client_),
       std::move(request));
 #endif  // defined(ENABLE_MOJO_VIDEO_DECODER)
@@ -106,10 +107,23 @@ void InterfaceFactoryImpl::CreateRenderer(
     return;
   }
 
-  MojoRendererService::Create(
-      cdm_service_context_.GetWeakPtr(), std::move(audio_sink),
-      std::move(video_sink), std::move(renderer),
-      MojoRendererService::InitiateSurfaceRequestCB(), std::move(request));
+  std::unique_ptr<MojoRendererService> mojo_renderer_service =
+      base::MakeUnique<MojoRendererService>(
+          cdm_service_context_.GetWeakPtr(), std::move(audio_sink),
+          std::move(video_sink), std::move(renderer),
+          MojoRendererService::InitiateSurfaceRequestCB());
+
+  MojoRendererService* mojo_renderer_service_ptr = mojo_renderer_service.get();
+
+  StrongBindingSet<mojom::Renderer>::BindingId binding_id =
+      renderer_bindings_.AddBinding(std::move(mojo_renderer_service),
+                                    std::move(request));
+
+  // base::Unretained() is safe because the callback will be fired by
+  // |mojo_renderer_service|, which is owned by |renderer_bindings_|.
+  mojo_renderer_service_ptr->set_bad_message_cb(base::Bind(
+      base::IgnoreResult(&StrongBindingSet<mojom::Renderer>::RemoveBinding),
+      base::Unretained(&renderer_bindings_), binding_id));
 #endif  // defined(ENABLE_MOJO_RENDERER)
 }
 
@@ -120,9 +134,9 @@ void InterfaceFactoryImpl::CreateCdm(
   if (!cdm_factory)
     return;
 
-  mojo::MakeStrongBinding(base::MakeUnique<MojoCdmService>(
-                              cdm_service_context_.GetWeakPtr(), cdm_factory),
-                          std::move(request));
+  cdm_bindings_.AddBinding(base::MakeUnique<MojoCdmService>(
+                               cdm_service_context_.GetWeakPtr(), cdm_factory),
+                           std::move(request));
 #endif  // defined(ENABLE_MOJO_CDM)
 }
 
