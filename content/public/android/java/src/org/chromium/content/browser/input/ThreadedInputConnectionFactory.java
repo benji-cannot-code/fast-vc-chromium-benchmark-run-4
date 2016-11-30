af4406b4dfc15f1f8bbd8c840c1da3330f320bd6
changwan <changwan@chromium.org>
2016-11-29 23:44:34 -0800

    Fix leaks in InputConnectionHandlerThread
    On destruction of ContentViewCore and on view detachment, we reset mHandler,
    allowing it a chance to be garbage-collected.
    
    BUG=668692
    
    Review-Url: https://codereview.chromium.org/2537073002
    Cr-Commit-Position: refs/heads/master@{#435136}

--- content/public/android/java/src/org/chromium/content/browser/input/ThreadedInputConnectionFactory.java
+++ content/public/android/java/src/org/chromium/content/browser/input/ThreadedInputConnectionFactory.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 
 import org.chromium.base.Log;
+import org.chromium.base.ThreadUtils;
 import org.chromium.base.VisibleForTesting;
 
 /**
@@ -31,7 +32,7 @@ public class ThreadedInputConnectionFactory implements ChromiumBaseInputConnecti
     // UI message loop until View#hasWindowFocus() is aligned with what IMMS sees.
     private static final int CHECK_REGISTER_RETRY = 1;
 
-    private final Handler mHandler;
+    private Handler mHandler;
     private final InputMethodManagerWrapper mInputMethodManagerWrapper;
     private final InputMethodUma mInputMethodUma;
     private ThreadedInputConnectionProxyView mProxyView;
@@ -57,7 +58,6 @@ public class ThreadedInputConnectionFactory implements ChromiumBaseInputConnecti
 
     ThreadedInputConnectionFactory(InputMethodManagerWrapper inputMethodManagerWrapper) {
         mInputMethodManagerWrapper = inputMethodManagerWrapper;
-        mHandler = createHandler();
         mInputMethodUma = createInputMethodUma();
     }
 
@@ -67,6 +67,25 @@ public class ThreadedInputConnectionFactory implements ChromiumBaseInputConnecti
                 new HandlerThread("InputConnectionHandlerThread", HandlerThread.NORM_PRIORITY);
         thread.start();
         return new Handler(thread.getLooper());
+    }
+
+    private void destroyHandler() {
+        if (mHandler == null) return;
+        final Handler handler = mHandler;
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                ThreadUtils.postOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (handler != null) {
+                            handler.getLooper().quit();
+                        }
+                    }
+                });
+            }
+        });
+        mHandler = null;
     }
 
     @VisibleForTesting
@@ -118,6 +137,8 @@ public class ThreadedInputConnectionFactory implements ChromiumBaseInputConnecti
         // mServedConnecting.
         if (mCheckInvalidator != null) mCheckInvalidator.invalidate();
 
+        if (mHandler == null) mHandler = createHandler();
+
         if (shouldTriggerDelayedOnCreateInputConnection()) {
             triggerDelayedOnCreateInputConnection(view);
             return null;
@@ -146,6 +167,7 @@ public class ThreadedInputConnectionFactory implements ChromiumBaseInputConnecti
 
         if (!view.hasWindowFocus()) mCheckInvalidator.invalidate();
 
+        if (mHandler == null) return;
         // We cannot reuse the existing proxy view, if any, due to crbug.com/664402.
         mProxyView = createProxyView(mHandler, view);
 
@@ -174,6 +196,7 @@ public class ThreadedInputConnectionFactory implements ChromiumBaseInputConnecti
 
                 // Step 3: Check that the above hack worked.
                 // Do not check until activation finishes inside InputMethodManager (on IME thread).
+                if (mHandler == null) return;
                 mHandler.post(new Runnable() {
                     @Override
                     public void run() {
@@ -252,6 +275,7 @@ public class ThreadedInputConnectionFactory implements ChromiumBaseInputConnecti
     public void onViewAttachedToWindow() {
         if (DEBUG_LOGS) Log.d(TAG, "onViewAttachedToWindow");
         if (mProxyView != null) mProxyView.onOriginalViewAttachedToWindow();
+        if (mHandler == null) mHandler = createHandler();
     }
 
     @Override
@@ -259,5 +283,12 @@ public class ThreadedInputConnectionFactory implements ChromiumBaseInputConnecti
         if (DEBUG_LOGS) Log.d(TAG, "onViewDetachedFromWindow");
         if (mCheckInvalidator != null) mCheckInvalidator.invalidate();
         if (mProxyView != null) mProxyView.onOriginalViewDetachedFromWindow();
+        destroyHandler();
+    }
+
+    @Override
+    public void destroy() {
+        if (mCheckInvalidator != null) mCheckInvalidator.invalidate();
+        destroyHandler();
     }
 }
