325c2bbd21e9cfab651ce5ffbaf2f9e5899ab383
sky <sky@chromium.org>
2016-11-30 13:05:27 -0800

    Makes InputMethodMus deal with no connection
    Tests don't always have a connection, so this code should deal well
    with that.
    
    BUG=669964
    TEST=none
    R=moshayedi@chromium.org
    
    Review-Url: https://codereview.chromium.org/2545543002
    Cr-Commit-Position: refs/heads/master@{#435413}

--- ui/aura/mus/input_method_mus.cc
+++ ui/aura/mus/input_method_mus.cc
@@ -34,7 +34,8 @@ InputMethodMus::InputMethodMus(ui::internal::InputMethodDelegate* delegate,
 InputMethodMus::~InputMethodMus() {}
 
 void InputMethodMus::Init(service_manager::Connector* connector) {
-  connector->ConnectToInterface(ui::mojom::kServiceName, &ime_server_);
+  if (connector)
+    connector->ConnectToInterface(ui::mojom::kServiceName, &ime_server_);
 }
 
 void InputMethodMus::DispatchKeyEvent(
@@ -125,8 +126,10 @@ void InputMethodMus::OnDidChangeFocusedClient(
   UpdateTextInputType();
 
   text_input_client_ = base::MakeUnique<TextInputClientImpl>(focused);
-  ime_server_->StartSession(text_input_client_->CreateInterfacePtrAndBind(),
-                            GetProxy(&input_method_));
+  if (ime_server_) {
+    ime_server_->StartSession(text_input_client_->CreateInterfacePtrAndBind(),
+                              GetProxy(&input_method_));
+  }
 }
 
 void InputMethodMus::UpdateTextInputType() {
