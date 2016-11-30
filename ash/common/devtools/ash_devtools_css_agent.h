f69b9c4ef80e185e084cadab003b9d094b2dc168
mhashmi <mhashmi@chromium.org>
2016-11-30 14:25:34 -0800

    Add live updates for AshDevToolsCSSAgent
    - This CL adds an AshDevToolsDOMAgentObserver so that other agents, namely
    CSS and Animation agents can observer when windows are added or removed
    from the frontend.
    - Add styleSheetChanged method to CSS protocol and call it from the
    AshDevToolsCSSAgent whenever the bounds for any object is changed.
    styleSheetChanged method called with an empty string simply invalidates
    all styles in the inspector. There is currently no way to invalidate
    styles on a single node.
    
    BUG=648701
    
    Review-Url: https://codereview.chromium.org/2526103002
    Cr-Commit-Position: refs/heads/master@{#435446}

--- ash/common/devtools/ash_devtools_css_agent.h
+++ ash/common/devtools/ash_devtools_css_agent.h
@@ -8,26 +8,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/common/devtools/ash_devtools_dom_agent.h"
 #include "components/ui_devtools/CSS.h"
+#include "ui/views/widget/widget_observer.h"
 
 namespace ash {
 namespace devtools {
 
 class ASH_EXPORT AshDevToolsCSSAgent
     : public NON_EXPORTED_BASE(ui::devtools::UiDevToolsBaseAgent<
-                               ui::devtools::protocol::CSS::Metainfo>) {
+                               ui::devtools::protocol::CSS::Metainfo>),
+      public AshDevToolsDOMAgentObserver {
  public:
   explicit AshDevToolsCSSAgent(AshDevToolsDOMAgent* dom_agent);
   ~AshDevToolsCSSAgent() override;
 
   // CSS::Backend
+  ui::devtools::protocol::Response enable() override;
+  ui::devtools::protocol::Response disable() override;
   ui::devtools::protocol::Response getMatchedStylesForNode(
-      int nodeId,
+      int node_id,
       ui::devtools::protocol::Maybe<ui::devtools::protocol::CSS::CSSStyle>*
-          inlineStyle) override;
+          inline_style) override;
+
+  // AshDevToolsDOMAgentObserver
+  void OnWindowBoundsChanged(WmWindow* window) override;
+  void OnWidgetBoundsChanged(views::Widget* widget) override;
+  void OnViewBoundsChanged(views::View* view) override;
 
  private:
   std::unique_ptr<ui::devtools::protocol::CSS::CSSStyle> GetStylesForNode(
-      int nodeId);
+      int node_id);
+  void InvalidateStyleSheet(int node_id);
 
   AshDevToolsDOMAgent* dom_agent_;
 
