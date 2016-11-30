c162e14971281e976abd5f227ceaf20e7e7b8e06
sdefresne <sdefresne@chromium.org>
2016-11-30 01:03:11 -0800

    Remove duplicate includes in ios_internal/ files.
    Automatically generated using the following command:
    
      for name in $(find ios -name '*.h' -o -name '*.mm' -o -name '*.cc'); do
        uniq < $name > $name~ && mv $name~ $name;
      done
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2541803002
    Cr-Commit-Position: refs/heads/master@{#435188}

--- ios/chrome/browser/application_context_impl.cc
+++ ios/chrome/browser/application_context_impl.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/default_tick_clock.h"
 #include "base/tracked_objects.h"
 #include "components/component_updater/component_updater_service.h"
-#include "components/component_updater/component_updater_service.h"
 #include "components/gcm_driver/gcm_client_factory.h"
 #include "components/gcm_driver/gcm_desktop_utils.h"
 #include "components/gcm_driver/gcm_driver.h"
