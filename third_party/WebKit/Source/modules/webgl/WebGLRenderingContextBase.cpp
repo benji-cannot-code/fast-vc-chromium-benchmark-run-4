f6d5720f7a59d892b9dba07e7823768330e714ad
jschuh <jschuh@chromium.org>
2016-11-30 09:24:49 -0800

    Fix WebGL use of base/numerics
    TBR=kbr@chromium.org
    BUG=668713
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2540153003
    Cr-Commit-Position: refs/heads/master@{#435310}

--- third_party/WebKit/Source/modules/webgl/WebGLRenderingContextBase.cpp
+++ third_party/WebKit/Source/modules/webgl/WebGLRenderingContextBase.cpp
@@ -4102,8 +4102,7 @@ void WebGLRenderingContextBase::readPixelsHelper(GLint x,
   }
   if (!validateReadPixelsFuncParameters(
           width, height, format, type, pixels,
-          static_cast<long long>(pixels->byteLength() -
-                                 offsetInBytes.ValueOrDie()))) {
+          (pixels->byteLength() - offsetInBytes).ValueOrDie())) {
     return;
   }
   clearIfComposited();
--- third_party/WebKit/Source/platform/graphics/gpu/WebGLImageConversion.cpp
+++ third_party/WebKit/Source/platform/graphics/gpu/WebGLImageConversion.cpp
@@ -2661,7 +2661,7 @@ GLenum WebGLImageConversion::computeImageSizeInBytes(
   }
 
   unsigned padding = 0;
-  unsigned residual = checkedValue.ValueOrDie() % params.alignment;
+  unsigned residual = (checkedValue % params.alignment).ValueOrDie();
   if (residual) {
     padding = params.alignment - residual;
     checkedValue += padding;
@@ -2676,7 +2676,7 @@ GLenum WebGLImageConversion::computeImageSizeInBytes(
   rows += height;
   if (!rows.IsValid())
     return GL_INVALID_VALUE;
-  checkedValue *= (rows.ValueOrDie() - 1);
+  checkedValue *= (rows - 1);
   // Last row is not affected by ROW_LENGTH parameter.
   checkedValue += lastRowSize;
   if (!checkedValue.IsValid())
