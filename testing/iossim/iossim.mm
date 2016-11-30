a2143231680c37f83ece4e4db7cedc28d8ded6c8
smut <smut@google.com>
2016-11-29 17:25:38 -0800

    Do not ignore -t, -u arguments to iossim
    BUG=663603
    R=justincohen@chromium.org
    TBR=rohitrao
    
    Review URL: https://codereview.chromium.org/2520993005 .
    
    Cr-Commit-Position: refs/heads/master@{#435110}

--- testing/iossim/iossim.mm
+++ testing/iossim/iossim.mm
@@ -360,9 +360,6 @@ int main(int argc, char* const argv[]) {
       case 'h':
         PrintUsage();
         exit(kExitSuccess);
-      case 'u':
-      case 't':
-        // Ignore 'u' and 't', used by old version of iossim.
         break;
       default:
         PrintUsage();
