0ba77b7a73e48ff9ede95583a57996cb2b932e94
fdoray <fdoray@chromium.org>
2016-11-30 12:33:37 -0800

    Use TaskScheduler instead of WorkerPool in keystone_glue.mm.
    This CL replaces a base::WorkerPool::PostTask() call with a
    base::PostTaskWithTraits() call. The following traits are used:
    
    Priority: BACKGROUND
      User won't notice if this task takes an arbitrarily long time
      to complete.
    
    Shutdown behavior: CONTINUE_ON_SHUTDOWN
      Tasks posted with this mode which have not started executing before
      shutdown is initiated will never run. Tasks with this mode running at
      shutdown will be ignored (the worker will not be joined).
    
    With File IO:
      The task is allowed to perform synchronous IO operations.
    
    No Wait (default):
      The task does not wait on things other than synchronous file IO
      operations (e.g. WaitableEvent, ConditionVariable, join a thread,
      join a process, blocking system call that doesn't involve
      interactions with the file system).
    
    BUG=659191
    
    Review-Url: https://codereview.chromium.org/2525483006
    Cr-Commit-Position: refs/heads/master@{#435404}

--- chrome/browser/mac/keystone_glue.mm
+++ chrome/browser/mac/keystone_glue.mm
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/sys_string_conversions.h"
-#include "base/threading/worker_pool.h"
+#include "base/task_scheduler/post_task.h"
 #include "build/build_config.h"
 #import "chrome/browser/mac/keystone_registration.h"
 #include "chrome/common/channel_info.h"
@@ -61,8 +61,7 @@ NSString* SystemBrandFilePath() {
   return [kBrandSystemFile stringByStandardizingPath];
 }
 
-// Adaptor for scheduling an Objective-C method call on a |WorkerPool|
-// thread.
+// Adaptor for scheduling an Objective-C method call in TaskScheduler.
 class PerformBridge : public base::RefCountedThreadSafe<PerformBridge> {
  public:
 
@@ -73,8 +72,13 @@ class PerformBridge : public base::RefCountedThreadSafe<PerformBridge> {
     DCHECK(sel);
 
     scoped_refptr<PerformBridge> op = new PerformBridge(target, sel, arg);
-    base::WorkerPool::PostTask(
-        FROM_HERE, base::Bind(&PerformBridge::Run, op.get()), true);
+    base::PostTaskWithTraits(
+        FROM_HERE, base::TaskTraits()
+                       .WithFileIO()
+                       .WithPriority(base::TaskPriority::BACKGROUND)
+                       .WithShutdownBehavior(
+                           base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN),
+        base::Bind(&PerformBridge::Run, op.get()));
   }
 
   // Convenience for the no-argument case.
--- chrome/browser/mac/keystone_glue_unittest.mm
+++ chrome/browser/mac/keystone_glue_unittest.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/Foundation.h>
 #import <objc/objc-class.h>
 
+#include "base/test/scoped_task_scheduler.h"
 #import "chrome/browser/mac/keystone_glue.h"
 #import "chrome/browser/mac/keystone_registration.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -179,6 +180,8 @@ namespace ksr = keystone_registration;
 namespace {
 
 class KeystoneGlueTest : public PlatformTest {
+ private:
+  base::test::ScopedTaskScheduler task_scheduler_;
 };
 
 // DISABLED because the mocking isn't currently working.
