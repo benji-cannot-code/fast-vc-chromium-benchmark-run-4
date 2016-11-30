702a58313a5b75466cdc4320e15618bcc25edc17
hzl <hzl@google.com>
2016-11-30 06:03:53 -0800

    Insert logcat as part of test result for android instrumentation tests.
    In this cl, we are trying to add the logcat of each instrumentation
    test into the corresponding test result object, so that when we are
    checking the result of each test case, we would be able to see the
    corresponding logcat for that test case at the same time.
    
    BUG=631213
    
    Review-Url: https://codereview.chromium.org/2537813003
    Cr-Commit-Position: refs/heads/master@{#435247}

--- build/android/pylib/instrumentation/instrumentation_test_instance.py
+++ build/android/pylib/instrumentation/instrumentation_test_instance.py
@@ -443,6 +443,9 @@ class InstrumentationTestInstance(test_instance.TestInstance):
     self._store_tombstones = False
     self._initializeTombstonesAttributes(args)
 
+    self._should_save_logcat = None
+    self._initializeLogAttributes(args)
+
   def _initializeApkAttributes(self, args, error_func):
     if args.apk_under_test:
       apk_under_test_path = args.apk_under_test
@@ -595,6 +598,9 @@ class InstrumentationTestInstance(test_instance.TestInstance):
   def _initializeTombstonesAttributes(self, args):
     self._store_tombstones = args.store_tombstones
 
+  def _initializeLogAttributes(self, args):
+    self._should_save_logcat = bool(args.json_results_file)
+
   @property
   def additional_apks(self):
     return self._additional_apks
@@ -626,6 +632,10 @@ class InstrumentationTestInstance(test_instance.TestInstance):
   @property
   def flags(self):
     return self._flags
+
+  @property
+  def should_save_logcat(self):
+    return self._should_save_logcat
 
   @property
   def package_info(self):
