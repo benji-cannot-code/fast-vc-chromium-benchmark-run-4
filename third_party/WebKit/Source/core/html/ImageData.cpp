88b4f3a461281018ec08586cb518c386c806e0bd
jschuh <jschuh@chromium.org>
2016-11-30 07:40:32 -0800

    Fix ImageData use of CheckedNumeric
    TBR=eae@chromium.org
    R=eae@chromium.org
    BUG=668713
    
    Review-Url: https://codereview.chromium.org/2546453002
    Cr-Commit-Position: refs/heads/master@{#435268}

--- third_party/WebKit/Source/core/html/ImageData.cpp
+++ third_party/WebKit/Source/core/html/ImageData.cpp
@@ -40,10 +40,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 ImageData* ImageData::create(const IntSize& size) {
-  CheckedNumeric<int> dataSize = 4;
+  CheckedNumeric<unsigned> dataSize = 4;
   dataSize *= size.width();
   dataSize *= size.height();
-  if (!dataSize.IsValid() || dataSize.ValueOrDie() < 0)
+  if (!dataSize.IsValid())
     return nullptr;
 
   DOMUint8ClampedArray* byteArray =
@@ -56,14 +56,13 @@ ImageData* ImageData::create(const IntSize& size) {
 
 ImageData* ImageData::create(const IntSize& size,
                              DOMUint8ClampedArray* byteArray) {
-  CheckedNumeric<int> dataSize = 4;
+  CheckedNumeric<unsigned> dataSize = 4;
   dataSize *= size.width();
   dataSize *= size.height();
   if (!dataSize.IsValid())
     return nullptr;
 
-  if (dataSize.ValueOrDie() < 0 ||
-      static_cast<unsigned>(dataSize.ValueOrDie()) > byteArray->length())
+  if (!dataSize.IsValid() || dataSize.ValueOrDie() > byteArray->length())
     return nullptr;
 
   return new ImageData(size, byteArray);
