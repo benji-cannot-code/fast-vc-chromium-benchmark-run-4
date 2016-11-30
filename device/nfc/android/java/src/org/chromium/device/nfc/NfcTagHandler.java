967ea53f81a6a7e42613bf10d57631038c0da0fc
alexander.shalamov <alexander.shalamov@intel.com>
2016-11-30 01:04:17 -0800

    [webnfc] Implement watch method for Android nfc mojo service.
    Implementation of nfc.watch [1] and nfc.cancelWatch [2] methods
    for Android nfc mojo service that is used from W3C WebNFC module.
    
    [1] https://w3c.github.io/web-nfc/#dom-nfc-watch
    [2] https://w3c.github.io/web-nfc/#dom-nfc-cancelwatch
    
    BUG=520391
    BUG=625589
    
    Review-Url: https://codereview.chromium.org/1765533004
    Cr-Commit-Position: refs/heads/master@{#435189}

--- device/nfc/android/java/src/org/chromium/device/nfc/NfcTagHandler.java
+++ device/nfc/android/java/src/org/chromium/device/nfc/NfcTagHandler.java
@@ -18,7 +18,7 @@ import java.io.IOException;
 /**
  * Utility class that provides I/O operations for NFC tags.
  */
-public final class NfcTagHandler {
+public class NfcTagHandler {
     private final TagTechnology mTech;
     private final TagTechnologyHandler mTechHandler;
     private boolean mWasConnected;
@@ -50,7 +50,7 @@ public final class NfcTagHandler {
     private interface TagTechnologyHandler {
         public void write(NdefMessage message)
                 throws IOException, TagLostException, FormatException;
-        // TODO(crbug.com/625589): add read method for nfc.watch.
+        public NdefMessage read() throws IOException, TagLostException, FormatException;
     }
 
     /**
@@ -67,6 +67,10 @@ public final class NfcTagHandler {
         public void write(NdefMessage message)
                 throws IOException, TagLostException, FormatException {
             mNdef.writeNdefMessage(message);
+        }
+
+        public NdefMessage read() throws IOException, TagLostException, FormatException {
+            return mNdef.getNdefMessage();
         }
     }
 
@@ -85,9 +89,13 @@ public final class NfcTagHandler {
                 throws IOException, TagLostException, FormatException {
             mNdefFormattable.format(message);
         }
+
+        public NdefMessage read() throws IOException, TagLostException, FormatException {
+            return NfcTypeConverter.emptyNdefMessage();
+        }
     }
 
-    private NfcTagHandler(TagTechnology tech, TagTechnologyHandler handler) {
+    protected NfcTagHandler(TagTechnology tech, TagTechnologyHandler handler) {
         mTech = tech;
         mTechHandler = handler;
     }
@@ -103,6 +111,13 @@ public final class NfcTagHandler {
     }
 
     /**
+     * Checks if NFC tag is connected.
+     */
+    public boolean isConnected() {
+        return mTech.isConnected();
+    }
+
+    /**
      * Closes connection.
      */
     public void close() throws IOException {
@@ -114,6 +129,10 @@ public final class NfcTagHandler {
      */
     public void write(NdefMessage message) throws IOException, TagLostException, FormatException {
         mTechHandler.write(message);
+    }
+
+    public NdefMessage read() throws IOException, TagLostException, FormatException {
+        return mTechHandler.read();
     }
 
     /**
