b299ee9b61d9c89e3892c8bdcf7ec8b7f80c1515
rch <rch@chromium.org>
2016-11-30 00:59:34 -0800

    Make QuicCryptoClientConfig only cache server configs for 1 week.
    Merge internal change:  140540620
    
    BUG=669671
    
    Review-Url: https://codereview.chromium.org/2534303002
    Cr-Commit-Position: refs/heads/master@{#435185}

--- net/quic/core/crypto/quic_crypto_client_config.cc
+++ net/quic/core/crypto/quic_crypto_client_config.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/quic/core/crypto/quic_crypto_client_config.h"
 
+#include <algorithm>
 #include <memory>
 
 #include "base/memory/ptr_util.h"
@@ -88,17 +89,17 @@ bool QuicCryptoClientConfig::CachedState::IsComplete(QuicWallTime now) const {
     return false;
   }
 
-  if (now.IsAfter(expiration_time_)) {
-    UMA_HISTOGRAM_CUSTOM_TIMES(
-        "Net.QuicClientHelloServerConfig.InvalidDuration",
-        base::TimeDelta::FromSeconds(now.ToUNIXSeconds() -
-                                     expiration_time_.ToUNIXSeconds()),
-        base::TimeDelta::FromMinutes(1), base::TimeDelta::FromDays(20), 50);
-    RecordInchoateClientHelloReason(SERVER_CONFIG_EXPIRED);
-    return false;
+  if (now.IsBefore(expiration_time_)) {
+    return true;
   }
 
-  return true;
+  UMA_HISTOGRAM_CUSTOM_TIMES(
+      "Net.QuicClientHelloServerConfig.InvalidDuration",
+      base::TimeDelta::FromSeconds(now.ToUNIXSeconds() -
+                                   expiration_time_.ToUNIXSeconds()),
+      base::TimeDelta::FromMinutes(1), base::TimeDelta::FromDays(20), 50);
+  RecordInchoateClientHelloReason(SERVER_CONFIG_EXPIRED);
+  return false;
 }
 
 bool QuicCryptoClientConfig::CachedState::IsEmpty() const {
@@ -726,7 +727,9 @@ QuicErrorCode QuicCryptoClientConfig::CacheNewServerConfig(
   QuicWallTime expiration_time = QuicWallTime::Zero();
   uint64_t expiry_seconds;
   if (message.GetUint64(kSTTL, &expiry_seconds) == QUIC_NO_ERROR) {
-    expiration_time = now.Add(QuicTime::Delta::FromSeconds(expiry_seconds));
+    // Only cache configs for a maximum of 1 week.
+    expiration_time = now.Add(QuicTime::Delta::FromSeconds(
+        std::min(expiry_seconds, kNumSecondsPerWeek)));
   }
 
   CachedState::ServerConfigState state =
--- net/quic/core/quic_constants.h
+++ net/quic/core/quic_constants.h
@@ -20,6 +20,7 @@ namespace net {
 // Simple time constants.
 const uint64_t kNumSecondsPerMinute = 60;
 const uint64_t kNumSecondsPerHour = kNumSecondsPerMinute * 60;
+const uint64_t kNumSecondsPerWeek = kNumSecondsPerHour * 24 * 7;
 const uint64_t kNumMicrosPerMilli = 1000;
 const uint64_t kNumMicrosPerSecond = 1000 * 1000;
 
