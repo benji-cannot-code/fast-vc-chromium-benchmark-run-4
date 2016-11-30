6fd4ac6e8671b632ba98d71875d7a8bde992b5df
skau <skau@chromium.org>
2016-11-30 00:09:05 -0800

    Update CUPS printers setup flow.
    Disable concurrent requests for manfuacturer and model since model is
    guaranteed to be empty and causes a crash.  Also disable automatic IPP
    Everywhere setup as it will not always work.
    
    BUG=663592,664245
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2495503003
    Cr-Commit-Position: refs/heads/master@{#435155}

--- chrome/browser/resources/settings/printing_page/cups_add_printer_dialog.js
+++ chrome/browser/resources/settings/printing_page/cups_add_printer_dialog.js
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Subdialogs include:
  * - 'add-printer-discovery-dialog' is a dialog showing discovered printers on
  *   the network that are available for setup.
- * - 'add-printer-maually-dialog' is a dialog in which user can manually enter
+ * - 'add-printer-manually-dialog' is a dialog in which user can manually enter
  *   the information to set up a new printer.
  * - 'add-printer-configuring-dialog' is the configuring-in-progress dialog.
  * - 'add-printer-manufacturer-model-dialog' is a dialog in which the user can
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 var AddPrinterDialogs = {
   DISCOVERY: 'add-printer-discovery-dialog',
-  MANUALLY: 'add-printer-maually-dialog',
+  MANUALLY: 'add-printer-manually-dialog',
   CONFIGURING: 'add-printer-configuring-dialog',
   MANUFACTURER: 'add-printer-manufacturer-model-dialog',
 };
@@ -119,7 +119,7 @@ Polymer({
 });
 
 Polymer({
-  is: 'add-printer-maually-dialog',
+  is: 'add-printer-manually-dialog',
 
   properties: {
     /** @type {!CupsPrinterInfo} */
@@ -155,9 +155,9 @@ Polymer({
   },
 
   /** @private */
-  switchToConfiguringDialog_: function() {
+  switchToManufacturerDialog_: function() {
     this.$$('add-printer-dialog').close();
-    this.fire('open-configuring-printer-dialog');
+    this.fire('open-manufacturer-model-dialog');
   },
 
   /** @private */
@@ -213,11 +213,17 @@ Polymer({
             this.manufacturerListChanged_.bind(this));
   },
 
-  /** @private */
+  /**
+   * @param {string} manufacturer The manufacturer for which we are retrieving
+   *     models.
+   * @private
+   */
   selectedManufacturerChanged_: function(manufacturer) {
-    settings.CupsPrintersBrowserProxyImpl.getInstance().
-        getCupsPrinterModelsList(manufacturer).then(
-            this.modelListChanged_.bind(this));
+    if (manufacturer) {
+      settings.CupsPrintersBrowserProxyImpl.getInstance()
+          .getCupsPrinterModelsList(manufacturer)
+          .then(this.modelListChanged_.bind(this));
+    }
   },
 
   /** @private */
@@ -413,19 +419,19 @@ Polymer({
 
   /** @private */
   openConfiguringPrinterDialog_: function() {
-    this.switchDialog_(this.currentDialog_, AddPrinterDialogs.CONFIGURING,
-                       'showConfiguringDialog_');
+    this.switchDialog_(
+        this.currentDialog_, AddPrinterDialogs.CONFIGURING,
+        'showConfiguringDialog_');
     if (this.previousDialog_ == AddPrinterDialogs.DISCOVERY) {
       this.configuringDialogTitle =
           loadTimeData.getString('addPrintersNearbyTitle');
-      settings.CupsPrintersBrowserProxyImpl.getInstance().
-          addCupsPrinter(this.selectedPrinter);
-    } else if (this.previousDialog_ == AddPrinterDialogs.MANUALLY ||
-               this.previousDialog_ == AddPrinterDialogs.MANUFACTURER) {
+      settings.CupsPrintersBrowserProxyImpl.getInstance().addCupsPrinter(
+          this.selectedPrinter);
+    } else if (this.previousDialog_ == AddPrinterDialogs.MANUFACTURER) {
       this.configuringDialogTitle =
           loadTimeData.getString('addPrintersManuallyTitle');
-      settings.CupsPrintersBrowserProxyImpl.getInstance().
-          addCupsPrinter(this.newPrinter);
+      settings.CupsPrintersBrowserProxyImpl.getInstance().addCupsPrinter(
+          this.newPrinter);
     }
   },
 
@@ -493,9 +499,6 @@ Polymer({
 
     if (this.previousDialog_ == AddPrinterDialogs.MANUFACTURER) {
       this.setupFailed = true;
-    } else if (this.previousDialog_ == AddPrinterDialogs.MANUALLY) {
-      this.switchDialog_(this.currentDialog_, AddPrinterDialogs.MANUFACTURER,
-                         'showManufacturerDialog_');
     }
   },
 });
