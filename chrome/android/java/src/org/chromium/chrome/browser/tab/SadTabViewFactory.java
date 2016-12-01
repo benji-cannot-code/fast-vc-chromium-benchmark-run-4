1adcc36f900b739d5481588105f46e55336dd848
jwanda <jwanda@chromium.org>
2016-12-01 15:52:57 -0800

    Show 'Send Feedback' on the sad tab page on multiple failures
    Fixed bug 665237
    
    Fixed a bug where if you reloaded twice it would not show you "Send Feedback" as the primary button. As this is how it is in both all other Chrome versions we emulated this feature here as well.
    
    BUG=665237
    
    Review-Url: https://codereview.chromium.org/2534333002
    Cr-Commit-Position: refs/heads/master@{#435774}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/SadTabViewFactory.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/SadTabViewFactory.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.tab;
 
 import android.content.Context;
+import android.support.annotation.StringRes;
 import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,16 +23,18 @@ import org.chromium.ui.text.SpanApplier.SpanInfo;
  * A factory class for creating the "Sad Tab" view, which is shown in place of a crashed renderer.
  */
 public class SadTabViewFactory {
+
     /**
      * @param context Context of the resulting Sad Tab view.
      * @param suggestionAction Action to be executed when user clicks "try these suggestions".
      * @param reloadButtonAction Action to be executed when Reload button is pressed.
      *                           (e.g., refreshing the page)
+     * @param buttonTextId The string resource for the button text label.
      * @return A "Sad Tab" view instance which is used in place of a crashed renderer.
      */
     public static View createSadTabView(
             Context context, final OnClickListener suggestionAction,
-            OnClickListener reloadButtonAction) {
+            OnClickListener reloadButtonAction, @StringRes int buttonTextId) {
         // Inflate Sad tab and initialize.
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(
                 Context.LAYOUT_INFLATER_SERVICE);
@@ -42,6 +45,7 @@ public class SadTabViewFactory {
         messageText.setMovementMethod(LinkMovementMethod.getInstance());
 
         Button reloadButton = (Button) sadTabView.findViewById(R.id.sad_tab_reload_button);
+        reloadButton.setText(buttonTextId);
         reloadButton.setOnClickListener(reloadButtonAction);
 
         return sadTabView;
