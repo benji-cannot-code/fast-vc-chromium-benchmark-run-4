3fc21c6a2480afe0057b770b421ded2bc055a377
igorcov <igorcov@chromium.org>
2016-12-01 08:07:56 -0800

    Implementation for chrome.certificateProvider.requestPin/stopPinRequest
    The IDL for proposed API has been reviewed: https://codereview.chromium.org/2174423002/
    
    This is the actual implementation of the API. The dialog to request PIN is defined in request_pin_view.h
    
    The CertificateProviderApi is receiving the requests from extensions and uses the service object to manage the view. CertificateProviderService object keeps the state of the view and forwards updates to it. And the RequestPinView is notifying the CertificateProviderApi about the user input through callback.
    
    BUG=612886
    
    Review-Url: https://codereview.chromium.org/2094333002
    Cr-Commit-Position: refs/heads/master@{#435620}

--- chrome/browser/chromeos/certificate_provider/certificate_provider_service.h
+++ chrome/browser/chromeos/certificate_provider/certificate_provider_service.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_checker.h"
 #include "chrome/browser/chromeos/certificate_provider/certificate_info.h"
 #include "chrome/browser/chromeos/certificate_provider/certificate_requests.h"
+#include "chrome/browser/chromeos/certificate_provider/pin_dialog_manager.h"
 #include "chrome/browser/chromeos/certificate_provider/sign_requests.h"
 #include "chrome/browser/chromeos/certificate_provider/thread_safe_certificate_map.h"
 #include "components/keyed_service/core/keyed_service.h"
@@ -151,6 +152,8 @@ class CertificateProviderService : public KeyedService {
   // corresponding notification of the ExtensionRegistry is triggered.
   void OnExtensionUnloaded(const std::string& extension_id);
 
+  PinDialogManager* pin_dialog_manager() { return &pin_dialog_manager_; }
+
  private:
   class CertKeyProviderImpl;
   class CertificateProviderImpl;
@@ -191,6 +194,10 @@ class CertificateProviderService : public KeyedService {
   // An instance of net::ClientKeyStore::CertKeyProvider that is registered at
   // the net::ClientKeyStore singleton.
   std::unique_ptr<CertKeyProviderImpl> cert_key_provider_;
+
+  // The object to manage the dialog displayed when requestPin is called by the
+  // extension.
+  PinDialogManager pin_dialog_manager_;
 
   // State about all pending sign requests.
   certificate_provider::SignRequests sign_requests_;
