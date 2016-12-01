b30c533a4803a1c182fec85078381aa5d056ec7f
tbarzic <tbarzic@chromium.org>
2016-12-01 11:04:32 -0800

    Use initializer_list in ChromePermissionMessageRule ctors
    This is cleaner than using
    ChromePermissionMessageRule::PermissionIDSetInitializer (and resolves
    TODO associated with said class).
    
    Also, avoids "Single-parameter constructors should be marked explicit."
    presubmit warning when changing chrome_permission_message_rules.cc file.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2540613003
    Cr-Commit-Position: refs/heads/master@{#435667}

--- chrome/common/extensions/permissions/chrome_permission_message_rules.h
+++ chrome/common/extensions/permissions/chrome_permission_message_rules.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_COMMON_EXTENSIONS_PERMISSIONS_CHROME_PERMISSION_MESSAGE_RULES_H_
 #define CHROME_COMMON_EXTENSIONS_PERMISSIONS_CHROME_PERMISSION_MESSAGE_RULES_H_
 
+#include <initializer_list>
 #include <set>
 #include <vector>
 
@@ -70,17 +71,17 @@ class ChromePermissionMessageRule {
       const PermissionIDSet& permissions) const;
 
  private:
-  class PermissionIDSetInitializer;
-
   // Create a rule using the default formatter (display the message with ID
   // |message_id|).
-  ChromePermissionMessageRule(int message_id,
-                              const PermissionIDSetInitializer& required,
-                              const PermissionIDSetInitializer& optional);
+  ChromePermissionMessageRule(
+      int message_id,
+      const std::initializer_list<APIPermission::ID>& required,
+      const std::initializer_list<APIPermission::ID>& optional);
   // Create a rule with a custom formatter. Takes ownership of |formatter|.
-  ChromePermissionMessageRule(ChromePermissionMessageFormatter* formatter,
-                              const PermissionIDSetInitializer& required,
-                              const PermissionIDSetInitializer& optional);
+  ChromePermissionMessageRule(
+      ChromePermissionMessageFormatter* formatter,
+      const std::initializer_list<APIPermission::ID>& required,
+      const std::initializer_list<APIPermission::ID>& optional);
 
   std::set<APIPermission::ID> required_permissions_;
   std::set<APIPermission::ID> optional_permissions_;
