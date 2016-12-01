fae1da6591fcf534f751c0a97972b3b9d35ab36f
jbudorick <jbudorick@chromium.org>
2016-12-01 07:02:33 -0800

    Revert of (Reland) Insert logcat as part of test result for android instrumentation tests. (patchset #3 id:40001 of https://codereview.chromium.org/2545653002/ )
    Reason for revert:
    Failing on large suites: https://build.chromium.org/p/chromium.android/builders/Lollipop%20Tablet%20Tester/builds/5996/steps/chrome_public_test_apk/logs/stdio
    
    Original issue's description:
    > (Reland) Insert logcat as part of test result for android instrumentation tests.
    >
    > In this cl, we are trying to add the logcat of each instrumentation
    > test into the corresponding test result object, so that when we are
    > checking the result of each test case, we would be able to see the
    > corresponding logcat for that test case at the same time.
    >
    > The reason of reland is to fix the issue where environmental variables are not set.
    >
    > BUG=631213
    >
    > Committed: https://crrev.com/79e6121247da1485bda4ae4b0d4b4249eb4ff1a9
    > Cr-Commit-Position: refs/heads/master@{#435550}
    
    TBR=hzl@google.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=631213
    
    Review-Url: https://codereview.chromium.org/2548503002
    Cr-Commit-Position: refs/heads/master@{#435606}

--- build/android/pylib/instrumentation/instrumentation_test_instance.py
+++ build/android/pylib/instrumentation/instrumentation_test_instance.py
@@ -443,9 +443,6 @@ class InstrumentationTestInstance(test_instance.TestInstance):
     self._store_tombstones = False
     self._initializeTombstonesAttributes(args)
 
-    self._should_save_logcat = None
-    self._initializeLogAttributes(args)
-
   def _initializeApkAttributes(self, args, error_func):
     if args.apk_under_test:
       apk_under_test_path = args.apk_under_test
@@ -596,9 +593,6 @@ class InstrumentationTestInstance(test_instance.TestInstance):
   def _initializeTombstonesAttributes(self, args):
     self._store_tombstones = args.store_tombstones
 
-  def _initializeLogAttributes(self, args):
-    self._should_save_logcat = bool(args.json_results_file)
-
   @property
   def additional_apks(self):
     return self._additional_apks
@@ -630,10 +624,6 @@ class InstrumentationTestInstance(test_instance.TestInstance):
   @property
   def flags(self):
     return self._flags
-
-  @property
-  def should_save_logcat(self):
-    return self._should_save_logcat
 
   @property
   def package_info(self):
