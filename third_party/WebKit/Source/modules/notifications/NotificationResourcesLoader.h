71f9b58cb5a67238a30b6d241b1ebde365e96924
jbroman <jbroman@chromium.org>
2016-12-01 08:25:15 -0800

    Revert "Notifications: Split up image loading histograms by image type"
    This reverts commit 468ed78ec76f5db7d89a0829b33c0f6427553411.
    https://codereview.chromium.org/2540763002/
    
    Failing on bots.
    
    NotificationImageLoaderTest.SuccessTest (run #1):
    [ RUN      ] NotificationImageLoaderTest.SuccessTest
    ../../base/test/histogram_tester.cc:158: Failure
    Value of: actual_count
      Actual: 1
    Expected: expected_count
    Which is: 0
    Histogram "Notifications.LoadFinishTime.Icon" does not have the right number of samples (0) in the expected bucket (0). It has (1).
    [  FAILED  ] NotificationImageLoaderTest.SuccessTest (5 ms)
    
    https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux%20Trusty/builds/20543
    
    BUG=669621,614456
    TBR=johnme@chromium.org
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2540423003
    Cr-Commit-Position: refs/heads/master@{#435622}

--- third_party/WebKit/Source/modules/notifications/NotificationResourcesLoader.h
+++ third_party/WebKit/Source/modules/notifications/NotificationResourcesLoader.h
@@ -57,7 +57,6 @@ class MODULES_EXPORT NotificationResourcesLoader final
 
  private:
   void loadImage(ExecutionContext*,
-                 NotificationImageLoader::Type,
                  const KURL&,
                  std::unique_ptr<NotificationImageLoader::ImageCallback>);
   void didLoadImage(const SkBitmap& image);
