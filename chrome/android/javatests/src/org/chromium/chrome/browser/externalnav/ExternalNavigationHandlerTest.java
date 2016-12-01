d8c9dcc6fd3bde60e934612f2c92ddb2872b766a
mariakhomenko <mariakhomenko@chromium.org>
2016-11-30 17:36:47 -0800

    Instant apps: cleanup an unused parameter.
    BUG=669698
    
    Review-Url: https://codereview.chromium.org/2536373006
    Cr-Commit-Position: refs/heads/master@{#435520}

--- chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/ExternalNavigationHandlerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/ExternalNavigationHandlerTest.java
@@ -1211,7 +1211,7 @@ public class ExternalNavigationHandlerTest extends NativeLibraryTestBase {
         }
 
         @Override
-        public boolean isSerpReferrer(String referrerUrl, Tab tab) {
+        public boolean isSerpReferrer(Tab tab) {
             return false;
         }
 
