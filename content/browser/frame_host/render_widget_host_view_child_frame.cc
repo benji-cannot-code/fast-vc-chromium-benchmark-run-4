02d6abcb338eb89a024251dc95dbe11ad1be85fb
ekaramad <ekaramad@chromium.org>
2016-12-01 14:44:47 -0800

    Do not try to convert point to root coordinate space when |local_frame_id_| is invalid.
    It appears that we might receive IPC which would trigger this code path
    while |local_frame_id_| is not valid. It is reasonable to verify the
    validity of |local_frame_id_| before passing the SurfaceId to
    CrossProcessFrameConnector for point transform.
    
    This CL also attempts to re-enable
    SSLUITest.TestUnauthenticatedFrameNavigation with --site-per-process (which was
    most probably flaky due to the issue above).
    
    BUG=668913
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2542463002
    Cr-Commit-Position: refs/heads/master@{#435744}

--- content/browser/frame_host/render_widget_host_view_child_frame.cc
+++ content/browser/frame_host/render_widget_host_view_child_frame.cc
@@ -520,7 +520,7 @@ void RenderWidgetHostViewChildFrame::ProcessGestureEvent(
 
 gfx::Point RenderWidgetHostViewChildFrame::TransformPointToRootCoordSpace(
     const gfx::Point& point) {
-  if (!frame_connector_)
+  if (!frame_connector_ || !local_frame_id_.is_valid())
     return point;
 
   return frame_connector_->TransformPointToRootCoordSpace(
