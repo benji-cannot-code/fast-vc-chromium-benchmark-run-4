8e1afc4e0b244fdbfe107489dad176f7d176efea
dmazzoni <dmazzoni@chromium.org>
2016-11-30 21:03:09 -0800

    Select-to-speak extension code
    This is an initial implementation of Select-to-speak. Given mouse events
    that are "captured" - currently by holding down Search - it draws a box
    around the area the user selects, and then speaks everything within that
    box based on its accessible name.
    
    This is not meant to be a final, complete implementation, just a good
    starting point to begin experimenting.
    
    Depends on: https://codereview.chromium.org/2493923002/
    
    BUG=593887
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2509883002
    Cr-Commit-Position: refs/heads/master@{#435555}

--- chrome/browser/resources/chromeos/select_to_speak/test_support.js
+++ chrome/browser/resources/chromeos/select_to_speak/test_support.js
@@ -2,3 +2,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2016 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
+/**
+ * Stubs out extension API functions so that SelectToSpeakUnitTest
+ * can load.
+ */
+
+chrome.automation = {};
+
+/**
+ * Stub
+ */
+chrome.automation.getDesktop = function() {};
--- third_party/closure_compiler/externs/compiled_resources2.gyp
+++ third_party/closure_compiler/externs/compiled_resources2.gyp
@@ -10,7 +10,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
   'targets': [
     {
+      'target_name': 'accessibility_private',
+      'includes': ['../include_js.gypi'],
+    },
+    {
       'target_name': 'autofill_private',
+      'includes': ['../include_js.gypi'],
+    },
+    {
+      'target_name': 'automation',
       'includes': ['../include_js.gypi'],
     },
     {
