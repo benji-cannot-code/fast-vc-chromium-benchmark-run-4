32385c4232969151a5acbfc51df24cce54e04dea
mattreynolds <mattreynolds@chromium.org>
2016-12-01 12:25:26 -0800

    Stop providing Physical Web metadata after Bluetooth is disabled
    When the user disables Bluetooth, the Physical Web feature stops scanning
    for nearby URLs but retains information about URLs that have already been
    discovered. With this change, the retained information will not be returned
    when clients request the list of Physical Web URLs with Bluetooth disabled.
    
    BUG=663566
    
    Review-Url: https://codereview.chromium.org/2507213003
    Cr-Commit-Position: refs/heads/master@{#435699}

--- ios/chrome/common/physical_web/physical_web_scanner.mm
+++ ios/chrome/common/physical_web/physical_web_scanner.mm
@@ -162,9 +162,15 @@ enum BeaconType {
   }
   pendingStart_ = NO;
   started_ = NO;
+  if (onLostDetectionEnabled_ && [devices_ count]) {
+    [delegate_ scannerUpdatedDevices:self];
+  }
 }
 
 - (NSArray*)devices {
+  if (![self bluetoothEnabled]) {
+    return [NSArray array];
+  }
   return [devices_ sortedArrayUsingComparator:^(id obj1, id obj2) {
     PhysicalWebDevice* device1 = obj1;
     PhysicalWebDevice* device2 = obj2;
@@ -267,11 +273,6 @@ enum BeaconType {
 }
 
 - (void)reallyStop {
-  if (updateTimer_.get()) {
-    [updateTimer_ invalidate];
-    updateTimer_.reset();
-  }
-
   [centralManager_ stopScan];
 }
 
@@ -322,12 +323,19 @@ enum BeaconType {
   // For unknown reasons, when scanning for longer periods (on the order of
   // minutes), the scanner is less reliable at detecting all nearby URLs. As a
   // workaround, we restart the scanner each time we check for lost URLs.
-  NSArray* serviceUUIDs = @[
-    [CBUUID UUIDWithString:kUriBeaconServiceUUID],
-    [CBUUID UUIDWithString:kEddystoneBeaconServiceUUID]
-  ];
-  [centralManager_ stopScan];
-  [centralManager_ scanForPeripheralsWithServices:serviceUUIDs options:nil];
+  if (!pendingStart_) {
+    NSArray* serviceUUIDs = @[
+      [CBUUID UUIDWithString:kUriBeaconServiceUUID],
+      [CBUUID UUIDWithString:kEddystoneBeaconServiceUUID]
+    ];
+    [centralManager_ stopScan];
+    [centralManager_ scanForPeripheralsWithServices:serviceUUIDs options:nil];
+  }
+
+  if (updateTimer_.get() && pendingStart_ && [devices_ count] == 0) {
+    [updateTimer_ invalidate];
+    updateTimer_.reset();
+  }
 }
 
 #pragma mark -
@@ -340,7 +348,6 @@ enum BeaconType {
   } else {
     if (started_ && !pendingStart_) {
       pendingStart_ = YES;
-      [self reallyStop];
     }
   }
   [delegate_ scannerBluetoothStatusUpdated:self];
