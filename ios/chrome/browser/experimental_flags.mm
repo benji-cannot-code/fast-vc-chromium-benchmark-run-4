1f1b9e75c183c7fb781c703ca2da4aa33282368d
rohitrao <rohitrao@chromium.org>
2016-11-30 16:08:26 -0800

    Revert of [ios] Use pending navigation index for Back Forward navigation by default (patchset #2 id:20001 of https://codereview.chromium.org/2523643002/ )
    Reason for revert:
    Breaks ToolsPopupMenuTestCase and BackForwardNavigationTestCase in the internal tree.
    
    Original issue's description:
    > [ios] Use pending navigation index for Back Forward navigation by default
    >
    > Design doc:
    > https://docs.google.com/document/d/1GvsaoNtMO7d87FzoysOW5GqH6XErI5g2cinKhkPk7bQ/edit#
    >
    > BUG=661858
    >
    > Committed: https://crrev.com/93df626d0de8a7af99127f34159eb4020989d93b
    > Cr-Commit-Position: refs/heads/master@{#435426}
    
    TBR=kkhorimoto@chromium.org,eugenebut@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=661858
    
    Review-Url: https://codereview.chromium.org/2543853002
    Cr-Commit-Position: refs/heads/master@{#435487}

--- ios/chrome/browser/experimental_flags.mm
+++ ios/chrome/browser/experimental_flags.mm
@@ -30,8 +30,8 @@ NSString* const kHeuristicsForPasswordGeneration =
     @"HeuristicsForPasswordGeneration";
 NSString* const kEnableNewClearBrowsingDataUI = @"EnableNewClearBrowsingDataUI";
 NSString* const kMDMIntegrationDisabled = @"MDMIntegrationDisabled";
-NSString* const kPendingIndexNavigationDisabled =
-    @"PendingIndexNavigationDisabled";
+NSString* const kPendingIndexNavigationEnabled =
+    @"PendingIndexNavigationEnabled";
 const base::Feature kIOSDownloadImageRenaming{
     "IOSDownloadImageRenaming", base::FEATURE_DISABLED_BY_DEFAULT};
 }  // namespace
@@ -186,8 +186,8 @@ bool IsMDMIntegrationEnabled() {
 }
 
 bool IsPendingIndexNavigationEnabled() {
-  return ![[NSUserDefaults standardUserDefaults]
-      boolForKey:kPendingIndexNavigationDisabled];
+  return [[NSUserDefaults standardUserDefaults]
+      boolForKey:kPendingIndexNavigationEnabled];
 }
 
 bool IsDownloadRenamingEnabled() {
