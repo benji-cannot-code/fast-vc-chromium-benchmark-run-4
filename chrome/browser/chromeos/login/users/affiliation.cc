0ca2e5980184e7070f6253aec7cc20f43abaeb70
antrim <antrim@chromium.org>
2016-12-01 15:56:59 -0800

    As YAPS server does not handle affiliation_ids correctly yet, we need to  have a flag that would allow us to avoid that check during development.
    Review-Url: https://codereview.chromium.org/2545523003
    Cr-Commit-Position: refs/heads/master@{#435777}

--- chrome/browser/chromeos/login/users/affiliation.cc
+++ chrome/browser/chromeos/login/users/affiliation.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/login/users/affiliation.h"
 
+#include "base/command_line.h"
 #include "chrome/browser/chromeos/policy/device_local_account.h"
+#include "components/policy/core/common/policy_switches.h"
 #include "google_apis/gaia/gaia_auth_util.h"
 
 namespace chromeos {
@@ -38,6 +40,14 @@ bool IsUserAffiliated(const AffiliationIDSet& user_affiliation_ids,
   }
 
   if (policy::IsDeviceLocalAccountUser(email, NULL)) {
+    return true;
+  }
+
+  // Not all test servers correctly support affiliation ids so far, so
+  // this is a work-around.
+  // TODO(antrim): remove this once all test servers support affiliation ids.
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+  if (command_line->HasSwitch(policy::switches::kUserAlwaysAffiliated)) {
     return true;
   }
 
