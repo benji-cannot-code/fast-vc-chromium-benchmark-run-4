5c7e6df99e3ad2c6bf92141c87f872f773245fc4
khmel <khmel@chromium.org>
2016-12-01 15:56:05 -0800

    arc: Extend app.mojom by adding intent parameter to OnTaskCreated.
    This CL passes extra parameter for OnTaskCrated that contains
    intent used to create Android task. This parameter will be used
    in Arc app shelf grouping work and to restore Android session.
    Next CL: http://crrev.com/2520033006
    
    BUG=b/32334966
    BUG=b/668319
    TEST=unit_tests and browser_tests
    
    Review-Url: https://codereview.chromium.org/2536353002
    Cr-Commit-Position: refs/heads/master@{#435776}

--- chrome/browser/ui/app_list/arc/arc_app_unittest.cc
+++ chrome/browser/ui/app_list/arc/arc_app_unittest.cc
@@ -1114,7 +1114,7 @@ TEST_F(ArcAppModelBuilderTest, NonLaunchableApp) {
 
   EXPECT_FALSE(prefs->IsRegistered(app_id));
   EXPECT_FALSE(FindArcItem(app_id));
-  app_instance()->SendTaskCreated(0, fake_apps()[0]);
+  app_instance()->SendTaskCreated(0, fake_apps()[0], std::string());
   // App should not appear now in the model but should be registered.
   EXPECT_FALSE(FindArcItem(app_id));
   EXPECT_TRUE(prefs->IsRegistered(app_id));
