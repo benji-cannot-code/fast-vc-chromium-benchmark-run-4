2a92535d29e2744278bb99f27007f6c1ffdca7c3
rajendrant <rajendrant@chromium.org>
2016-12-01 12:59:56 -0800

    Add data usage tracking for sync service
    BUG=655749
    
    Review-Url: https://codereview.chromium.org/2539063003
    Cr-Commit-Position: refs/heads/master@{#435708}

--- components/sync/driver/sync_stopped_reporter.cc
+++ components/sync/driver/sync_stopped_reporter.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "base/strings/stringprintf.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "components/data_use_measurement/core/data_use_user_data.h"
 #include "components/sync/protocol/sync.pb.h"
 #include "net/base/load_flags.h"
 #include "net/http/http_status_code.h"
@@ -73,6 +74,8 @@ void SyncStoppedReporter::ReportSyncStopped(const std::string& access_token,
   fetcher_->SetLoadFlags(net::LOAD_BYPASS_CACHE | net::LOAD_DISABLE_CACHE |
                          net::LOAD_DO_NOT_SAVE_COOKIES |
                          net::LOAD_DO_NOT_SEND_COOKIES);
+  data_use_measurement::DataUseUserData::AttachToFetcher(
+      fetcher_.get(), data_use_measurement::DataUseUserData::SYNC);
   fetcher_->Start();
   timer_.Start(FROM_HERE, base::TimeDelta::FromSeconds(kRequestTimeoutSeconds),
                this, &SyncStoppedReporter::OnTimeout);
