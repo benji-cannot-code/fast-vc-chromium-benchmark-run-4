8363ccbee18110c872a798f5e2d1c6f5b71d1d19
jsbell <jsbell@chromium.org>
2016-12-02 13:19:39 -0800

    IndexedDB: Simplify backing store error reporting following r435315
    Refactor error reporting within database/transaction classes to eliminate
    redundant argument passing and logic.
    
    R=dmurph@chromium.org,cmumford@chromium.org
    
    Review-Url: https://codereview.chromium.org/2544893004
    Cr-Commit-Position: refs/heads/master@{#436024}

--- content/browser/indexed_db/indexed_db_transaction.cc
+++ content/browser/indexed_db/indexed_db_transaction.cc
@@ -38,22 +38,6 @@ void CommitUnused(base::WeakPtr<IndexedDBTransaction> transaction) {
   DCHECK(status.ok());
 }
 
-// The database will be closed during this call.
-void ReportError(leveldb::Status status,
-                 const scoped_refptr<IndexedDBDatabase> database,
-                 IndexedDBFactory* factory) {
-  DCHECK(!status.ok());
-  if (status.IsCorruption()) {
-    IndexedDBDatabaseError error(blink::WebIDBDatabaseExceptionUnknownError,
-                                 base::ASCIIToUTF16(status.ToString()));
-    factory->HandleBackingStoreCorruption(database->origin(), error);
-  } else {
-    factory->HandleBackingStoreFailure(database->origin());
-  }
-}
-
-
-
 }  // namespace
 
 IndexedDBTransaction::TaskQueue::TaskQueue() {}
@@ -286,7 +270,7 @@ leveldb::Status IndexedDBTransaction::BlobWriteComplete(
       s = CommitPhaseTwo();
       if (!s.ok() &&
           result == IndexedDBBackingStore::BlobWriteResult::SUCCESS_ASYNC)
-        ReportError(s, database, database->factory());
+        database->ReportError(s);
       break;
     }
   }
@@ -440,7 +424,7 @@ void IndexedDBTransaction::ProcessTaskQueue() {
     }
     if (!result.ok()) {
       processing_event_queue_ = false;
-      ReportError(result, database_, database_->factory());
+      database_->ReportError(result);
       return;
     }
 
@@ -456,7 +440,7 @@ void IndexedDBTransaction::ProcessTaskQueue() {
     // This can delete |this|.
     leveldb::Status result = Commit();
     if (!result.ok())
-      ReportError(result, database_, database_->factory());
+      database_->ReportError(result);
     return;
   }
 
