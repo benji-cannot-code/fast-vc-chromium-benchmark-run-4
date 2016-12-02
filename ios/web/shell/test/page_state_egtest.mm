dbdc67af7e94c2c540403120716f7cbea6f8f6b5
baxley <baxley@chromium.org>
2016-12-02 09:11:04 -0800

    Disable testScrollPositionRestoring on iOS 9.
    This test is flaky on iOS 9, but works on iOS 10.
    
    BUG=670700
    
    Review-Url: https://codereview.chromium.org/2551503002
    Cr-Commit-Position: refs/heads/master@{#435960}

--- ios/web/shell/test/page_state_egtest.mm
+++ ios/web/shell/test/page_state_egtest.mm
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/Foundation.h>
 #import <XCTest/XCTest.h>
 
+#include "base/ios/ios_util.h"
+#import "ios/testing/earl_grey/disabled_test_macros.h"
 #import "ios/web/public/test/http_server.h"
 #include "ios/web/public/test/http_server_util.h"
 #import "ios/web/shell/test/earl_grey/shell_base_test_case.h"
@@ -59,6 +61,10 @@ using web::test::HttpServer;
 // Tests that page scroll position of a page is restored upon returning to the
 // page via the back/forward buttons.
 - (void)testScrollPositionRestoring {
+  // TODO(crbug.com/670700): Re-enable this test.
+  if (!base::ios::IsRunningOnIOS10OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Test disabled on pre-iOS 10");
+  }
   web::test::SetUpFileBasedHttpServer();
 
   // Load first URL which is a long page.
