7d78f2e499f55de90b444b2b6f054543fe77a15d
toyoshim <toyoshim@chromium.org>
2016-12-02 03:11:30 -0800

    Revert of Place CHECKs to check FontResource::startLoadLimiTimers is called appropriately (patchset #1 id:1 of https://codereview.chromium.org/2544603003/ )
    Reason for revert:
    Confirmed that the DCHECK at line 113 was wrong.
    
    Original issue's description:
    > Place CHECKs to check FontResource::startLoadLimiTimers is called appropriately
    >
    > BUG=666227
    >
    > Committed: https://crrev.com/2b57ae8716a7b6e19c1f9928c9c8a4551d472fb2
    > Cr-Commit-Position: refs/heads/master@{#435595}
    
    TBR=yhirano@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=666227
    
    Review-Url: https://codereview.chromium.org/2549593005
    Cr-Commit-Position: refs/heads/master@{#435911}

--- third_party/WebKit/Source/core/loader/resource/FontResource.cpp
+++ third_party/WebKit/Source/core/loader/resource/FontResource.cpp
@@ -111,7 +111,7 @@ void FontResource::setRevalidatingRequest(const ResourceRequest& request) {
 }
 
 void FontResource::startLoadLimitTimers() {
-  CHECK(isLoading());
+  DCHECK(isLoading());
   DCHECK_EQ(m_loadLimitState, LoadNotStarted);
   m_loadLimitState = UnderLimit;
   m_fontLoadShortLimitTimer.startOneShot(fontLoadWaitShortLimitSec,
@@ -145,7 +145,6 @@ FontPlatformData FontResource::platformDataFromCustomData(
 }
 
 void FontResource::fontLoadShortLimitCallback(TimerBase*) {
-  CHECK(isLoading());
   if (!isLoading())
     return;
   DCHECK_EQ(m_loadLimitState, UnderLimit);
@@ -156,7 +155,6 @@ void FontResource::fontLoadShortLimitCallback(TimerBase*) {
 }
 
 void FontResource::fontLoadLongLimitCallback(TimerBase*) {
-  CHECK(isLoading());
   if (!isLoading())
     return;
   DCHECK_EQ(m_loadLimitState, ShortLimitExceeded);
