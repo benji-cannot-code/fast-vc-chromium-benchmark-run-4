b88bd2f17ebc63cf2280f7b0820984567f6b47c8
yabinh <yabinh@chromium.org>
2016-12-02 02:03:01 -0800

    Introduce 'TextCompositionCancel' for cancelComposition
    cancelComposition and other functions (e.g finishComposingText) share the same
    tag "TextCompositionConfirm". In order to keep the former's behavior and change
    the others', this CL introduces "TextCompositionCancel" for cancelComposition,
    and assigns the old tag to other functions. This is a pre-CL for
    https://codereview.chromium.org/2530843003/
    
    BUG=620549
    
    Review-Url: https://codereview.chromium.org/2143923002
    Cr-Commit-Position: refs/heads/master@{#435898}

--- third_party/WebKit/Source/core/editing/InputMethodController.cpp
+++ third_party/WebKit/Source/core/editing/InputMethodController.cpp
@@ -85,9 +85,11 @@ void insertTextDuringCompositionWithEvents(
   DCHECK(compositionType ==
              TypingCommand::TextCompositionType::TextCompositionUpdate ||
          compositionType ==
-             TypingCommand::TextCompositionType::TextCompositionConfirm)
+             TypingCommand::TextCompositionType::TextCompositionConfirm ||
+         compositionType ==
+             TypingCommand::TextCompositionType::TextCompositionCancel)
       << "compositionType should be TextCompositionUpdate or "
-         "TextCompositionConfirm, but got "
+         "TextCompositionConfirm  or TextCompositionCancel, but got "
       << static_cast<int>(compositionType);
   if (!frame.document())
     return;
@@ -127,6 +129,7 @@ void insertTextDuringCompositionWithEvents(
                                 compositionType);
       break;
     case TypingCommand::TextCompositionType::TextCompositionConfirm:
+    case TypingCommand::TextCompositionType::TextCompositionCancel:
       // TODO(chongz): Use TypingCommand::insertText after TextEvent was
       // removed. (Removed from spec since 2012)
       // See TextEvent.idl.
@@ -380,7 +383,7 @@ void InputMethodController::cancelComposition() {
   dispatchCompositionUpdateEvent(frame(), emptyString());
   insertTextDuringCompositionWithEvents(
       frame(), emptyString(), 0,
-      TypingCommand::TextCompositionType::TextCompositionConfirm);
+      TypingCommand::TextCompositionType::TextCompositionCancel);
   // Event handler might destroy document.
   if (!isAvailable())
     return;
--- third_party/WebKit/Source/core/editing/commands/TypingCommand.h
+++ third_party/WebKit/Source/core/editing/commands/TypingCommand.h
@@ -46,7 +46,8 @@ class CORE_EXPORT TypingCommand final : public CompositeEditCommand {
   enum TextCompositionType {
     TextCompositionNone,
     TextCompositionUpdate,
-    TextCompositionConfirm
+    TextCompositionConfirm,
+    TextCompositionCancel
   };
 
   enum Option {
