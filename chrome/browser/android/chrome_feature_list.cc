4fe3cb77cfaff73af763906877e68e9de87b7756
rouslan <rouslan@chromium.org>
2016-12-01 18:19:19 -0800

    Show modified total price for payment instruments.
    If the merchant specifies custom pricing for a specific payment method,
    show the custom total next to the payment method. For example, a custom
    price for "visa" cards changes:
    
       Visa*1111, Jon Smith
    
    to:
    
       Visa*1111, Jon Smith, $45.00
    
    The feature is hidden behind chrome://flags#web-payments-modifiers until
    it is complete.
    
    BUG=660926
    
    Review-Url: https://codereview.chromium.org/2542493004
    Cr-Commit-Position: refs/heads/master@{#435827}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -54,6 +54,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kSpecialLocaleWrapper,
     &kTabReparenting,
     &kWebApks,
+    &kWebPaymentsModifiers,
     &ntp_snippets::kContentSuggestionsFeature,
     &ntp_snippets::kIncreasedVisibility,
     &ntp_snippets::kForeignSessionsSuggestionsFeature,
@@ -93,11 +94,11 @@ const base::Feature kImportantSitesInCBD{"ImportantSitesInCBD",
 const base::Feature kNoCreditCardAbort{"NoCreditCardAbort",
                                        base::FEATURE_DISABLED_BY_DEFAULT};
 
-const base::Feature kNTPOfflinePagesFeature{"NTPOfflinePages",
-                                            base::FEATURE_ENABLED_BY_DEFAULT};
-
 const base::Feature kNTPFakeOmniboxTextFeature{
     "NTPFakeOmniboxText", base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kNTPOfflinePagesFeature{"NTPOfflinePages",
+                                            base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kPhysicalWebFeature{"PhysicalWeb",
                                         base::FEATURE_ENABLED_BY_DEFAULT};
@@ -120,6 +121,9 @@ const base::Feature kUserMediaScreenCapturing{
 // Makes "Add to Home screen" in the app menu generate an APK for the shortcut
 // URL which opens Chrome in fullscreen.
 const base::Feature kWebApks{"WebApks", base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kWebPaymentsModifiers{"WebPaymentsModifiers",
+                                          base::FEATURE_DISABLED_BY_DEFAULT};
 
 static jboolean IsEnabled(JNIEnv* env,
                           const JavaParamRef<jclass>& clazz,
