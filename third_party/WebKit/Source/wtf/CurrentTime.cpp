402abf7d7ad806dc498aea74cacbfefd5d76d024
majidvp <majidvp@chromium.org>
2016-12-02 15:53:10 -0800

    Provide simple wrappers for base/time types in WTF
    - WTF::TimeDelta aliased base::TimeDelta
    - WTF::{Time,TimeTicks} are a thin wrapper around base::{Time,TimeTicks} this is
      mainly because we want to control the API surface we expose in particular we
      want to avoid exposing string parsing functions at this time.
    
    Once we have these types we can start replacing usage of double and int to
    represent time in blink with more accurate and type safe representations.
    
    Relevant discussion: https://groups.google.com/a/chromium.org/d/msg/platform-architecture-dev/7kg1FAsXkng/l19cEeBPBAAJ
    
    BUG=625680, 402027
    TEST=Critical functionality is already covered in base/time/time_unittest.cc but
    added a smoke test in TimeTest.cpp
    
    Review-Url: https://codereview.chromium.org/2543663002
    Cr-Commit-Position: refs/heads/master@{#436084}

--- third_party/WebKit/Source/wtf/CurrentTime.cpp
+++ third_party/WebKit/Source/wtf/CurrentTime.cpp
@@ -56,4 +56,8 @@ TimeFunction setTimeFunctionsForTesting(TimeFunction newFunction) {
   return oldFunction;
 }
 
+TimeFunction getTimeFunctionForTesting() {
+  return mockTimeFunctionForTesting;
+}
+
 }  // namespace WTF
