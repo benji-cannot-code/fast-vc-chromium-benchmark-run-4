6013eaedb4123cc6bf51205bebf9ec63c5f87f9b
sky <sky@chromium.org>
2016-12-01 19:34:38 -0800

    Makes WindowTreeHostMus supply properties directly to window creation
    This way properties that don't make there way to Window properties can
    be passed to the wm. For example,
    ui::mojom::WindowManager::kInitialBounds_Property is only needed at
    creation time and need not be stored in the aura::Window.
    
    BUG=664625 669964
    TEST=covered by test
    R=msw@chromium.org
    
    Review-Url: https://codereview.chromium.org/2539263005
    Cr-Commit-Position: refs/heads/master@{#435845}

--- ui/aura/mus/window_tree_host_mus_delegate.h
+++ ui/aura/mus/window_tree_host_mus_delegate.h
@@ -6,6 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_AURA_MUS_WINDOW_TREE_HOST_MUS_DELEGATE_H_
 #define UI_AURA_MUS_WINDOW_TREE_HOST_MUS_DELEGATE_H_
 
+#include <stdint.h>
+
+#include <map>
+#include <string>
+#include <vector>
+
 #include "ui/aura/aura_export.h"
 
 namespace gfx {
@@ -38,7 +44,9 @@ class AURA_EXPORT WindowTreeHostMusDelegate {
       const base::Optional<gfx::Rect>& mask_rect) = 0;
 
   // Called when a WindowTreeHostMus is created without a WindowPort.
-  virtual std::unique_ptr<WindowPortMus> CreateWindowPortForTopLevel() = 0;
+  // TODO: this should take an unordered_map, see http://crbug.com/670515.
+  virtual std::unique_ptr<WindowPortMus> CreateWindowPortForTopLevel(
+      const std::map<std::string, std::vector<uint8_t>>* properties) = 0;
 
   // Called from WindowTreeHostMus's constructor once the Window has been
   // created.
