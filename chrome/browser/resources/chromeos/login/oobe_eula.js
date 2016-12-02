96872c36191fc48516bd5f5ed3061d7678569f44
alemate <alemate@chromium.org>
2016-12-01 16:17:54 -0800

    ChromeOS MD-OOBE: "Back" button for EULA screen.
    This CL adds "Back" button to EULA screen.
    
    BUG=604119
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2541333002
    Cr-Commit-Position: refs/heads/master@{#435785}

--- chrome/browser/resources/chromeos/login/oobe_eula.js
+++ chrome/browser/resources/chromeos/login/oobe_eula.js
@@ -77,7 +77,7 @@ Polymer({
   /**
    * On-change event handler for usageStats.
    *
-   * * @private
+   * @private
    */
   onUsageChanged_: function() {
     this.screen.onUsageStatsClicked_(this.$.usageStats.checked);
@@ -86,7 +86,7 @@ Polymer({
   /**
    * On-tap event handler for installationSettings.
    *
-   * * @private
+   * @private
    */
   onInstallationSettingsClicked_: function() {
     chrome.send('eulaOnInstallationSettingsPopupOpened');
@@ -97,9 +97,18 @@ Polymer({
   /**
    * On-tap event handler for stats-help-link.
    *
-   * * @private
+   * @private
    */
   onUsageStatsHelpLinkClicked_: function() {
     chrome.send('eulaOnLearnMore');
+  },
+
+  /**
+   * On-tap event handler for back button.
+   *
+   * @private
+   */
+  onEulaBackButtonPressed_: function() {
+    chrome.send('login.EulaScreen.userActed', ['back-button']);
   },
 });
