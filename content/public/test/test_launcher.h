f6529114c420eded97eac5006fb97cc69a5b0c3a
rockot <rockot@chromium.org>
2016-12-02 13:33:31 -0800

    Revert of Add thread checking to RunLoop, deprecate MessageLoopRunner. (patchset #4 id:20002 of https://codereview.chromium.org/2537893002/ )
    Reason for revert:
    Speculative revert in an effort to pinpoint the cause of http://crbug.com/670844 - will reland if the flake doesn't clear
    
    Original issue's description:
    > Add thread checker to base::RunLoop.
    >
    > The purpose is to make sure that after switching users of
    > MessageLoopRunner to RunLoop we won't lose any correctness checks.
    >
    > Also add a comment that encourages to use RunLoop where possible, and
    > rename GetQuitTaskForRunLoop to GetDeferredQuitTaskForRunLoop to convey
    > is purpose clearly.
    >
    > BUG=668707
    >
    > Committed: https://crrev.com/dd10720676efff002490f702118e052dcd210f18
    > Cr-Commit-Position: refs/heads/master@{#435924}
    
    TBR=avi@chromium.org,thakis@chromium.org,asargent@chromium.org,ahest@yandex-team.ru
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=668707
    
    Review-Url: https://codereview.chromium.org/2548883002
    Cr-Commit-Position: refs/heads/master@{#436035}

--- content/public/test/test_launcher.h
+++ content/public/test/test_launcher.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 class CommandLine;
 class FilePath;
+class RunLoop;
 }
 
 namespace content {
@@ -47,6 +48,8 @@ class TestLauncherDelegate {
   virtual bool AdjustChildProcessCommandLine(
       base::CommandLine* command_line,
       const base::FilePath& temp_data_dir) = 0;
+  virtual void PreRunMessageLoop(base::RunLoop* run_loop) {}
+  virtual void PostRunMessageLoop() {}
   virtual ContentMainDelegate* CreateContentMainDelegate() = 0;
 
   // Called prior to running each test. The delegate may alter the CommandLine
