e248636e4b4e104b244a050a3c4f09be8fd134c0
dmazzoni <dmazzoni@chromium.org>
2016-12-02 09:29:25 -0800

    Revert "Android shouldn't fire focus events when the WebView itself isn't focused"
    Caused http://crbug.com/667559 - when clicking on a <select> pop-up and
    then returning to the page, focus was being reset to the whole page rather
    than back to the select element.
    
    Original changelist: https://chromiumcodereview.appspot.com/2416293002
    
    BUG=648391,667559
    TBR=dtseng@chromium.org
    
    Review-Url: https://codereview.chromium.org/2547903002
    Cr-Commit-Position: refs/heads/master@{#435964}

--- content/public/android/java/src/org/chromium/content/browser/accessibility/BrowserAccessibilityManager.java
+++ content/public/android/java/src/org/chromium/content/browser/accessibility/BrowserAccessibilityManager.java
@@ -773,16 +773,13 @@ public class BrowserAccessibilityManager {
     @CalledByNative
     private void handleFocusChanged(int id) {
         if (mNativeObj == 0) return;
-        if (mAccessibilityFocusId != View.NO_ID) {
-            sendAccessibilityEvent(id, AccessibilityEvent.TYPE_VIEW_FOCUSED);
-            moveAccessibilityFocusToId(id);
-        }
+        sendAccessibilityEvent(id, AccessibilityEvent.TYPE_VIEW_FOCUSED);
+        moveAccessibilityFocusToId(id);
     }
 
     @CalledByNative
     private void handleCheckStateChanged(int id) {
         if (mNativeObj == 0) return;
-        if (mAccessibilityFocusId == View.NO_ID) return;
         sendAccessibilityEvent(id, AccessibilityEvent.TYPE_VIEW_CLICKED);
     }
 
@@ -795,21 +792,18 @@ public class BrowserAccessibilityManager {
     @CalledByNative
     private void handleTextSelectionChanged(int id) {
         if (mNativeObj == 0) return;
-        if (mAccessibilityFocusId == View.NO_ID) return;
         sendAccessibilityEvent(id, AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED);
     }
 
     @CalledByNative
     private void handleEditableTextChanged(int id) {
         if (mNativeObj == 0) return;
-        if (mAccessibilityFocusId == View.NO_ID) return;
         sendAccessibilityEvent(id, AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED);
     }
 
     @CalledByNative
     private void handleSliderChanged(int id) {
         if (mNativeObj == 0) return;
-        if (mAccessibilityFocusId == View.NO_ID) return;
         sendAccessibilityEvent(id, AccessibilityEvent.TYPE_VIEW_SCROLLED);
     }
 
@@ -844,9 +838,7 @@ public class BrowserAccessibilityManager {
     @CalledByNative
     private void handleScrolledToAnchor(int id) {
         if (mNativeObj == 0) return;
-        if (mAccessibilityFocusId != View.NO_ID) {
-            moveAccessibilityFocusToId(id);
-        }
+        moveAccessibilityFocusToId(id);
     }
 
     @CalledByNative
@@ -865,7 +857,6 @@ public class BrowserAccessibilityManager {
 
     @CalledByNative
     private void announceLiveRegionText(String text) {
-        if (mAccessibilityFocusId == View.NO_ID) return;
         mView.announceForAccessibility(text);
     }
 
