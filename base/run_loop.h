986485fff1cd84572c8b7b32727c3dde0f13989f
ahest <ahest@yandex-team.ru>
2016-12-02 04:32:34 -0800

    Add thread checker to base::RunLoop.
    The purpose is to make sure that after switching users of
    MessageLoopRunner to RunLoop we won't lose any correctness checks.
    
    Also add a comment that encourages to use RunLoop where possible, and
    rename GetQuitTaskForRunLoop to GetDeferredQuitTaskForRunLoop to convey
    is purpose clearly.
    
    BUG=668707
    
    Review-Url: https://codereview.chromium.org/2537893002
    Cr-Commit-Position: refs/heads/master@{#435924}

--- base/run_loop.h
+++ base/run_loop.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/message_loop/message_loop.h"
+#include "base/threading/thread_checker.h"
 #include "build/build_config.h"
 
 namespace base {
@@ -105,6 +106,8 @@ class BASE_EXPORT RunLoop {
   // Used to record that QuitWhenIdle() was called on the MessageLoop, meaning
   // that we should quit Run once it becomes idle.
   bool quit_when_idle_received_;
+
+  base::ThreadChecker thread_checker_;
 
   // WeakPtrFactory for QuitClosure safety.
   base::WeakPtrFactory<RunLoop> weak_factory_;
