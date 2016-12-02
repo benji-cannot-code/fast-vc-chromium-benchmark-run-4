32ec24d885ff992946e61d2992a731229c169989
wangxianzhu <wangxianzhu@chromium.org>
2016-12-02 15:31:09 -0800

    setNeedsPaintPropertyUpdate on FrameView contents size change
    This ensures that the bounds of scroll is updated.
    
    BUG=645667
    
    Review-Url: https://codereview.chromium.org/2545093002
    Cr-Commit-Position: refs/heads/master@{#436077}

--- third_party/WebKit/Source/core/frame/FrameView.cpp
+++ third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -637,6 +637,10 @@ void FrameView::setContentsSize(const IntSize& size) {
   updateScrollbars();
   ScrollableArea::contentsResized();
 
+  if (RuntimeEnabledFeatures::slimmingPaintInvalidationEnabled() &&
+      !RuntimeEnabledFeatures::rootLayerScrollingEnabled())
+    setNeedsPaintPropertyUpdate();
+
   Page* page = frame().page();
   if (!page)
     return;
