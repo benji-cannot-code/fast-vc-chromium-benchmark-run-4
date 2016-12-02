7ef5754a08cc3722e3be60efcca670052d30bdd1
rego <rego@igalia.com>
2016-12-02 05:20:15 -0800

    [css-grid] Avoid layout positioned items that don't need it
    We were doing some unneeded extra operations for the case of
    positioned items that don't need layout.
    Layout is called several times, if the positioned items
    don't need layout we can skip those operations.
    
    No new tests tests as it's covered by current ones.
    
    BUG=273898
    
    Review-Url: https://codereview.chromium.org/2543163002
    Cr-Commit-Position: refs/heads/master@{#435926}

--- third_party/WebKit/Source/core/layout/LayoutGrid.cpp
+++ third_party/WebKit/Source/core/layout/LayoutGrid.cpp
@@ -2569,6 +2569,9 @@ void LayoutGrid::layoutPositionedObjects(bool relayoutChildren,
     return;
 
   for (auto* child : *positionedDescendants) {
+    if (!child->needsLayout())
+      continue;
+
     if (isOrthogonalChild(*child)) {
       // FIXME: Properly support orthogonal writing mode.
       continue;
