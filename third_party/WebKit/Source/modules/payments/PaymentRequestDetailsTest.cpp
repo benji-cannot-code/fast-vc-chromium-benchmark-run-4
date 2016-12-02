3418b058492c56399ca513eba8275cbbf5fa34b9
rouslan <rouslan@chromium.org>
2016-12-01 16:00:07 -0800

    Add data parameter to payment details modifier.
    This patch adds the data parameter for payment details modifier that is
    a JSON-serializable object with optional information that might be
    needed by the supported payment methods. For example, {"gateway":
    "company"}. The data parameter is behind a runtime flag.
    
    Added in:
    https://github.com/w3c/browser-payment-api/pull/261/files
    
    Spec:
    https://w3c.github.io/browser-payment-api/#paymentdetailsmodifier-dictionary
    
    Intent to implement:
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/Dpc5Ftqgxlg/ZYenHNR0AgAJ
    
    Example:
    var supportedMethods = [{supportedMethods: ["basic-card"]}];
    var shoppingCart = {total: {title: "Total",
                                currency: {code: "USD", value: "5.00"}}};
    shoppingCart.modifiers = [{
        data: {supportedType: ["debit"]}, // THE NEW FEATURE
        supportedMethods: ["basic-card"],
        total: {title: "Total",
                currency: {code: "USD", value: "4.63"}}}];
    new PaymentRequest(supportedMethods, shoppingCart);
    
    To better share the object validation and stringification logic, this
    patch merges the validation and conversion code for PaymentMethodData,
    PaymentDetails, and PaymentDetailsModifier. This results in saving
    around 50 lines of code.
    
    BUG=660926
    
    Review-Url: https://codereview.chromium.org/2470463002
    Cr-Commit-Position: refs/heads/master@{#435780}

--- third_party/WebKit/Source/modules/payments/PaymentRequestDetailsTest.cpp
+++ third_party/WebKit/Source/modules/payments/PaymentRequestDetailsTest.cpp
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/Document.h"
 #include "core/dom/ExceptionCode.h"
 #include "modules/payments/PaymentDetails.h"
+#include "modules/payments/PaymentOptions.h"
 #include "modules/payments/PaymentTestHelper.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include <ostream>  // NOLINT
@@ -130,9 +131,11 @@ TEST_P(PaymentRequestDetailsTest, ValidatesDetails) {
   V8TestingScope scope;
   scope.document().setSecurityOrigin(
       SecurityOrigin::create(KURL(KURL(), "https://www.example.com/")));
-  PaymentRequest::create(scope.getScriptState(),
-                         buildPaymentMethodDataForTest(),
-                         GetParam().buildDetails(), scope.getExceptionState());
+  PaymentOptions options;
+  options.setRequestShipping(true);
+  PaymentRequest::create(
+      scope.getScriptState(), buildPaymentMethodDataForTest(),
+      GetParam().buildDetails(), options, scope.getExceptionState());
 
   EXPECT_EQ(GetParam().expectException(),
             scope.getExceptionState().hadException());
