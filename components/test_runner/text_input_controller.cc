504572c36cb58cd1d09a34b8b72b21e6f9d05f9a
ekaramad <ekaramad@chromium.org>
2016-12-01 19:57:52 -0800

    Consider TextInputController::inputMethodController() could be nullptr.
    The assumption that, while in layout tests, WebFrameWidget::getActiveWebInputMethodController()
    always returns a (non-null) pointer is incorrect. For example, if the WebViewImpl loses focus, then the bit m_imeAcceptEvents is set to false which will stop WebViewImpl from processing any IME events.
    
    Alternatively, if there are no focused frames within the local root
    associated with WebViewImpl (i.e., focusedFrame() is null) then again there
    will be no focused frames for IME and conequently, no
    WebInputMethodControllers.
    
    This patch will first rename
    TextInputController()::inputMethodController() to
    GetInputMethodController() and then makes sure all the references to the
    method are taking potential nullptr-ness of the result into account.
    
    BUG=668827
    
    Review-Url: https://codereview.chromium.org/2535303004
    Cr-Commit-Position: refs/heads/master@{#435848}

--- components/test_runner/text_input_controller.cc
+++ components/test_runner/text_input_controller.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gin/object_template_builder.h"
 #include "gin/wrappable.h"
 #include "third_party/WebKit/public/platform/WebInputEvent.h"
+#include "third_party/WebKit/public/platform/WebInputEventResult.h"
 #include "third_party/WebKit/public/web/WebCompositionUnderline.h"
 #include "third_party/WebKit/public/web/WebFrameWidget.h"
 #include "third_party/WebKit/public/web/WebInputMethodController.h"
@@ -169,12 +170,16 @@ void TextInputController::Install(blink::WebLocalFrame* frame) {
 }
 
 void TextInputController::InsertText(const std::string& text) {
-  inputMethodController()->commitText(blink::WebString::fromUTF8(text), 0);
+  if (auto* controller = GetInputMethodController()) {
+    controller->commitText(blink::WebString::fromUTF8(text), 0);
+  }
 }
 
 void TextInputController::UnmarkText() {
-  inputMethodController()->finishComposingText(
-      blink::WebInputMethodController::KeepSelection);
+  if (auto* controller = GetInputMethodController()) {
+    controller->finishComposingText(
+        blink::WebInputMethodController::KeepSelection);
+  }
 }
 
 void TextInputController::DoCommand(const std::string& text) {
@@ -213,8 +218,9 @@ void TextInputController::SetMarkedText(const std::string& text,
     underlines.push_back(underline);
   }
 
-  inputMethodController()->setComposition(web_text, underlines, start,
-                                          start + length);
+  if (auto* controller = GetInputMethodController()) {
+    controller->setComposition(web_text, underlines, start, start + length);
+  }
 }
 
 bool TextInputController::HasMarkedText() {
@@ -303,9 +309,11 @@ void TextInputController::SetComposition(const std::string& text) {
   std::vector<blink::WebCompositionUnderline> underlines;
   underlines.push_back(blink::WebCompositionUnderline(
       0, textLength, SK_ColorBLACK, false, SK_ColorTRANSPARENT));
-  inputMethodController()->setComposition(
-      newText, blink::WebVector<blink::WebCompositionUnderline>(underlines),
-      textLength, textLength);
+  if (auto* controller = GetInputMethodController()) {
+    controller->setComposition(
+        newText, blink::WebVector<blink::WebCompositionUnderline>(underlines),
+        textLength, textLength);
+  }
 }
 
 void TextInputController::ForceTextInputStateUpdate() {
@@ -317,7 +325,11 @@ blink::WebView* TextInputController::view() {
   return web_view_test_proxy_base_->web_view();
 }
 
-blink::WebInputMethodController* TextInputController::inputMethodController() {
+blink::WebInputMethodController*
+TextInputController::GetInputMethodController() {
+  if (!view()->mainFrame())
+    return nullptr;
+
   blink::WebLocalFrame* mainFrame = view()->mainFrame()->toWebLocalFrame();
   if (!mainFrame) {
     CHECK(false) << "WebView does not have a local main frame and"
