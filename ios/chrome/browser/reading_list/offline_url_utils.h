6ad884721b96b3ce56502a2ddba923fc70f8cc57
olivierrobin <olivierrobin@chromium.org>
2016-12-02 03:21:15 -0800

    Reading List: add the virtualURL in offline URL query.
    Add the Original URL to the offline URL so the online URL can be
    displayed to the user.
    Also upstream reading_list_entry_loader.* and add a single method to load
    the distilled page of a Reading List entry.
    
    BUG=669824
    
    Review-Url: https://codereview.chromium.org/2542603002
    Cr-Commit-Position: refs/heads/master@{#435913}

--- ios/chrome/browser/reading_list/offline_url_utils.h
+++ ios/chrome/browser/reading_list/offline_url_utils.h
@@ -14,7 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace reading_list {
 
 // The distilled URL chrome://offline/... that will load the file at |path|.
-GURL DistilledURLForPath(const base::FilePath& path);
+GURL DistilledURLForPath(const base::FilePath& path, const GURL& virtual_url);
+
+// If |distilled_url| has a query "virtualURL" query params that is a URL,
+// returns it. If not, return |distilled_url|
+GURL VirtualURLForDistilledURL(const GURL& distilled_url);
 
 // The file URL pointing to the local file to load to display |distilled_url|.
 // If |resources_root_url| is not nullptr, it is set to a file URL to the
