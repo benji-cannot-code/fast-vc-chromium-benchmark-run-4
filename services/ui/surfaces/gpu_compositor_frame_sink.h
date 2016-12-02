8dc4ed87eb83f60488333ef0605daf7d2974180b
fsamuel <fsamuel@chromium.org>
2016-12-02 13:18:39 -0800

    Mus: Move creation of cc::Display to DisplayCompositor
    What type of ContextProvider, and OutputSurface we create is a display-
    compositor-specific detail and not a CompositorFrameSink specific detail.
    A top-level GpuCompositorFrameSink needs a cc::Display to update the display's
    surface, but it doesn't need to know how the cc::Display was set up.
    This CL moves cc::Display creation to DisplayCompositor and passes in
    a unique_ptr to GpuCompositorFrameSink. That way, GpuCompositorFrameSink becomes
    a generic implementation that doesn't care whether an InProcessContextProvider
    or some other ContextProvider was used. This will allow this code to be
    reused elsewhere in Chrome.
    
    BUG=668126
    
    Review-Url: https://codereview.chromium.org/2543123004
    Cr-Commit-Position: refs/heads/master@{#436023}

--- services/ui/surfaces/gpu_compositor_frame_sink.h
+++ services/ui/surfaces/gpu_compositor_frame_sink.h
@@ -14,10 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "cc/ipc/compositor_frame.mojom.h"
 #include "cc/ipc/mojo_compositor_frame_sink.mojom.h"
-#include "cc/output/context_provider.h"
-#include "cc/output/in_process_context_provider.h"
 #include "cc/scheduler/begin_frame_source.h"
-#include "cc/surfaces/display.h"
 #include "cc/surfaces/display_client.h"
 #include "cc/surfaces/frame_sink_id.h"
 #include "cc/surfaces/surface_factory.h"
@@ -26,12 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/surfaces/surface_id_allocator.h"
 #include "mojo/public/cpp/bindings/binding.h"
 
-namespace base {
-class SingleThreadTaskRunner;
-}
-
-namespace gpu {
-class GpuMemoryBufferManager;
+namespace cc {
+class Display;
 }
 
 namespace ui {
@@ -48,9 +41,7 @@ class GpuCompositorFrameSink : public cc::mojom::MojoCompositorFrameSink,
   GpuCompositorFrameSink(
       DisplayCompositor* display_compositor,
       const cc::FrameSinkId& frame_sink_id,
-      gpu::SurfaceHandle widget,
-      gpu::GpuMemoryBufferManager* gpu_memory_buffer_manager,
-      scoped_refptr<cc::InProcessContextProvider> context_provider,
+      std::unique_ptr<cc::Display> display,
       cc::mojom::MojoCompositorFrameSinkRequest request,
       cc::mojom::MojoCompositorFrameSinkPrivateRequest private_request,
       cc::mojom::MojoCompositorFrameSinkClientPtr client);
@@ -68,11 +59,6 @@ class GpuCompositorFrameSink : public cc::mojom::MojoCompositorFrameSink,
       const cc::FrameSinkId& child_frame_sink_id) override;
 
  private:
-  void InitDisplay(
-      gpu::SurfaceHandle widget,
-      gpu::GpuMemoryBufferManager* gpu_memory_buffer_manager,
-      scoped_refptr<cc::InProcessContextProvider> context_provider);
-
   void DidReceiveCompositorFrameAck();
 
   // cc::DisplayClient implementation.
@@ -96,9 +82,8 @@ class GpuCompositorFrameSink : public cc::mojom::MojoCompositorFrameSink,
   void OnPrivateConnectionLost();
 
   const cc::FrameSinkId frame_sink_id_;
-  scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
 
-  DisplayCompositor* display_compositor_;  // owns this.
+  DisplayCompositor* const display_compositor_;  // owns this.
 
   // GpuCompositorFrameSink holds a cc::Display if it created with
   // non-null gpu::SurfaceHandle. In the window server, the display root
