e3a99db2c521fa9db4c8cd75ae50e251f07dd549
fdoray <fdoray@chromium.org>
2016-12-02 16:04:30 -0800

    Use TaskScheduler instead of WorkerPool in client_cert_store_chromeos.cc.
    This CL replaces base::WorkerPool::PostTask() with
    base::PostTaskWithTraits(). The following traits are used:
    
    Priority: Inherited (default)
      The priority is inherited from the calling context (i.e. TaskTraits
      are initialized with the priority of the current task).
    
    Shutdown behavior: CONTINUE_ON_SHUTDOWN
      Tasks posted with this mode which have not started executing before
      shutdown is initiated will never run. Tasks with this mode running at
      shutdown will be ignored (the worker will not be joined).
    
    With File IO:
      The task waits on synchronous file IO operations.
    
    With Wait:
      The task waits on things other than synchronous file IO
      operations (e.g. WaitableEvent, ConditionVariable, join a thread,
      join a process, blocking system call that doesn't involve
      interactions with the file system).
    
    BUG=659191
    
    Review-Url: https://codereview.chromium.org/2533473002
    Cr-Commit-Position: refs/heads/master@{#436087}

--- chrome/browser/chromeos/net/client_cert_store_chromeos_unittest.cc
+++ chrome/browser/chromeos/net/client_cert_store_chromeos_unittest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/net/client_cert_store_chromeos.h"
 
-#include <memory>
 #include <string>
 
 #include "base/callback.h"
@@ -16,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
+#include "base/test/scoped_task_scheduler.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/chromeos/certificate_provider/certificate_provider.h"
 #include "crypto/scoped_test_nss_db.h"
@@ -79,7 +79,7 @@ class TestCertFilter : public ClientCertStoreChromeOS::CertFilter {
 
 class ClientCertStoreChromeOSTest : public ::testing::Test {
  public:
-  ClientCertStoreChromeOSTest() : message_loop_(new base::MessageLoopForIO()) {}
+  ClientCertStoreChromeOSTest() {}
 
   scoped_refptr<net::X509Certificate> ImportCertToSlot(
       const std::string& cert_filename,
@@ -90,7 +90,8 @@ class ClientCertStoreChromeOSTest : public ::testing::Test {
   }
 
  private:
-  std::unique_ptr<base::MessageLoop> message_loop_;
+  base::test::ScopedTaskScheduler scoped_task_scheduler_;
+  base::MessageLoopForIO message_loop_;
 };
 
 // Ensure that cert requests, that are started before the filter is initialized,
