c5102aa336616b2cdc5bf21438c5088ee1b27a97
carlosk <carlosk@chromium.org>
2016-12-02 16:01:25 -0800

    Failed offline snapshots won't erase a successful one from the same page load.
    If a second offline snapshot starts for the same page load where a
    previous one was saved, it should not immediately delete it: as it might
    fail the user would end up with no offline copy of the page. This change
    fixes this issue.
    
    Three new tests are added: one to cover this precise case and a couple
    more to check for related situations. Also many comments were added or
    updated.
    
    BUG=655697
    
    Review-Url: https://codereview.chromium.org/2542833003
    Cr-Commit-Position: refs/heads/master@{#436086}

--- chrome/browser/android/offline_pages/recent_tab_helper.h
+++ chrome/browser/android/offline_pages/recent_tab_helper.h
@@ -99,6 +99,7 @@ class RecentTabHelper
   void ContinueSnapshotAfterPurge(OfflinePageModel::DeletePageResult result);
   void SavePageCallback(OfflinePageModel::SavePageResult result,
                         int64_t offline_id);
+  void PreviousSaveWasPurged(OfflinePageModel::DeletePageResult result);
   void ReportSnapshotCompleted();
   void ReportDownloadStatusToRequestCoordinator();
   bool IsSamePage() const;
@@ -119,6 +120,10 @@ class RecentTabHelper
   // Info for the offline page to capture. Null if the tab is not capturing
   // current page.
   std::unique_ptr<DownloadPageInfo> download_info_;
+
+  // Stores the request/offline ID of successful snapshots that happened during
+  // a single ongoing page load.
+  int64_t previous_request_id_for_load_;
 
   // If empty, the tab does not have AndroidId and can not capture pages.
   std::string tab_id_;
