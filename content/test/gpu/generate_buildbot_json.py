b3d4022f5fc4e200d8b56abd9ddd0dc8ba2d1309
zmo <zmo@chromium.org>
2016-12-02 16:13:30 -0800

    Update WebGL conformance test suite version to 2.0.1
    BUG=662644
    TEST=GPU FYI bots
    NOTRY=true
    R=kbr@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2549513003
    Cr-Commit-Position: refs/heads/master@{#436090}

--- content/test/gpu/generate_buildbot_json.py
+++ content/test/gpu/generate_buildbot_json.py
@@ -1235,7 +1235,7 @@ TELEMETRY_GPU_INTEGRATION_TESTS = {
     ],
     'target_name': 'webgl_conformance',
     'args': [
-      '--webgl-conformance-version=2.0.0',
+      '--webgl-conformance-version=2.0.1',
       # The current working directory when run via isolate is
       # out/Debug or out/Release. Reference this file relatively to
       # it.
@@ -1274,7 +1274,7 @@ TELEMETRY_GPU_INTEGRATION_TESTS = {
       '--use-gl=angle',
     ],
     'args': [
-      '--webgl-conformance-version=2.0.0',
+      '--webgl-conformance-version=2.0.1',
       # The current working directory when run via isolate is
       # out/Debug or out/Release. Reference this file relatively to
       # it.
