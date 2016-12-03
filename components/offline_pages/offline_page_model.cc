e78b19f5ecf8482f0100217d15d191226f0f0e82
dewittj <dewittj@chromium.org>
2016-12-02 17:15:28 -0800

    [Offline Pages] Adds storage for kInvalidOfflineId.
    Since kInvalidOfflineId is a static data member, it needs explicitly
    defined storage as soon as it becomes 'odr-used'.  Replace a temporary
    variable in RecentTabHelper with storage in offline_page_model.cc both
    for clarity and to allow further references to kInvalidOfflineId to be
    taken.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2548033002
    Cr-Commit-Position: refs/heads/master@{#436116}

--- components/offline_pages/offline_page_model.cc
+++ components/offline_pages/offline_page_model.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace offline_pages {
 
+const int64_t OfflinePageModel::kInvalidOfflineId;
+
 OfflinePageModel::SavePageParams::SavePageParams()
     : proposed_offline_id(OfflinePageModel::kInvalidOfflineId) {
 }
