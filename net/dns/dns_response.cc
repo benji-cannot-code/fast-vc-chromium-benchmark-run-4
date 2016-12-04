61af35915501037187376f887f34c1b398bc322b
tfarina <tfarina@chromium.org>
2016-12-04 07:21:48 -0800

    net: move kRcodeMask into dns_response.cc
    It is used only there and it is not part of the public protocol.
    
    BUG=None
    TEST=net_unittests
    R=juliatuttle@chromium.org
    
    Review-Url: https://codereview.chromium.org/2545163002
    Cr-Commit-Position: refs/heads/master@{#436194}

--- net/dns/dns_response.cc
+++ net/dns/dns_response.cc
@@ -20,6 +20,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 
+namespace {
+
+const uint8_t kRcodeMask = 0xf;
+
+}  // namespace
+
 DnsResourceRecord::DnsResourceRecord() {
 }
 
@@ -222,12 +228,12 @@ bool DnsResponse::IsValid() const {
 
 uint16_t DnsResponse::flags() const {
   DCHECK(parser_.IsValid());
-  return base::NetToHost16(header()->flags) & ~(dns_protocol::kRcodeMask);
+  return base::NetToHost16(header()->flags) & ~(kRcodeMask);
 }
 
 uint8_t DnsResponse::rcode() const {
   DCHECK(parser_.IsValid());
-  return base::NetToHost16(header()->flags) & dns_protocol::kRcodeMask;
+  return base::NetToHost16(header()->flags) & kRcodeMask;
 }
 
 unsigned DnsResponse::answer_count() const {
