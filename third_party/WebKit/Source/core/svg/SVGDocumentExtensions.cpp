b7712372efa8f05dbda2ae4e0bf645c0ad33c904
sigbjornf <sigbjornf@opera.com>
2016-12-05 10:15:00 -0800

    Disallow off-heap containers containing raw on-heap pointers.
    R=
    BUG=
    
    Review-Url: https://codereview.chromium.org/2553673002
    Cr-Commit-Position: refs/heads/master@{#436351}

--- third_party/WebKit/Source/core/svg/SVGDocumentExtensions.cpp
+++ third_party/WebKit/Source/core/svg/SVGDocumentExtensions.cpp
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/dom/Document.h"
 #include "core/inspector/ConsoleMessage.h"
+#include "core/layout/svg/LayoutSVGResourceContainer.h"
 #include "core/svg/SVGSVGElement.h"
 #include "core/svg/animation/SMILTimeContainer.h"
 #include "wtf/AutoReset.h"
