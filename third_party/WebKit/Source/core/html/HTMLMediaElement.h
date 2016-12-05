5fb9f61b3137c852ce9f567f2fc55b92d6fb3c3e
zqzhang <zqzhang@chromium.org>
2016-12-05 10:25:42 -0800

    [MediaElement] Allow preloading for non-network scheme src
    This CL weakens the restriction of media element preloading. The
    reason for this change is that the preload attribute for media
    elements is overriden as "none" when data saver is
    enabled. However there is no need to apply this override for
    non-network media sources, since they do not cost network
    traffic.
    
    BUG=661220
    
    Review-Url: https://codereview.chromium.org/2546993003
    Cr-Commit-Position: refs/heads/master@{#436358}

--- third_party/WebKit/Source/core/html/HTMLMediaElement.h
+++ third_party/WebKit/Source/core/html/HTMLMediaElement.h
@@ -725,6 +725,7 @@ class CORE_EXPORT HTMLMediaElement : public HTMLElement,
   friend class Internals;
   friend class TrackDisplayUpdateScope;
   friend class MediaControlsTest;
+  friend class HTMLMediaElementTest;
   friend class HTMLVideoElementTest;
 
   Member<AutoplayUmaHelper> m_autoplayUmaHelper;
