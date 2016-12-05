d4fa342fd7e413b3cc9e5ad87b13e4763048a0c6
jack <jack@nottheoilrig.com>
2016-12-05 11:44:24 -0800

    Allow redirects for requests that require preflight.
    BUG=580796
    
    Review-Url: https://codereview.chromium.org/2421093003
    Cr-Commit-Position: refs/heads/master@{#436374}

--- third_party/WebKit/LayoutTests/http/tests/fetch/script-tests/thorough/redirect.js
+++ third_party/WebKit/LayoutTests/http/tests/fetch/script-tests/thorough/redirect.js
@@ -212,20 +212,21 @@ var TEST_TARGETS = [
   //  [fetchResolved, noContentLength, noServerHeader, hasBody, typeCors],
   //  [methodIsPOST]],
 
-  // Once CORS preflight flag is set, redirecting to the cross-origin is not
-  // allowed.
   // Custom method
   [OTHER_REDIRECT_URL +
    encodeURIComponent(BASE_URL + 'ACAOrigin=*&ACAMethods=PUT') +
    '&mode=cors&credentials=same-origin&method=PUT&ACAOrigin=*&ACAMethods=PUT',
-   [fetchRejected]],
+   [fetchResolved, noContentLength, noServerHeader, hasBody, typeCors],
+   [methodIsPUT, authCheckNone]],
   // Custom header
   [OTHER_REDIRECT_URL +
    encodeURIComponent(
        BASE_URL +
-       'ACAOrigin=' + BASE_ORIGIN + '&ACAHeaders=x-serviceworker-test') +
-   '&mode=cors&credentials=same-origin&method=GET&headers=CUSTOM&ACAOrigin=*',
-   [fetchRejected]],
+       'ACAOrigin=*&ACAHeaders=x-serviceworker-test') +
+   '&mode=cors&credentials=same-origin&method=GET&headers=CUSTOM' +
+   '&ACAOrigin=*&ACAHeaders=x-serviceworker-test',
+   [fetchResolved, noContentLength, noServerHeader, hasBody, typeCors],
+   [methodIsGET, hasCustomHeader, authCheckNone]],
 
   // Redirect: other origin -> other origin
   [OTHER_REDIRECT_URL + encodeURIComponent(OTHER_BASE_URL) +
@@ -294,13 +295,12 @@ var TEST_TARGETS = [
    [fetchResolved, noContentLength, hasServerHeader, hasBody, typeCors],
    [methodIsGET, authCheckNone]],
 
-  // Once CORS preflight flag is set, redirecting to the cross-origin is not
-  // allowed.
   // Custom method
   [OTHER_REDIRECT_URL +
    encodeURIComponent(OTHER_BASE_URL + 'ACAOrigin=*&ACAMethods=PUT') +
    '&mode=cors&credentials=same-origin&method=PUT&ACAOrigin=*&ACAMethods=PUT',
-   [fetchRejected]],
+   [fetchResolved, noContentLength, noServerHeader, hasBody, typeCors],
+   [methodIsPUT, authCheckNone]],
   // Custom header
   [OTHER_REDIRECT_URL +
    encodeURIComponent(
@@ -308,7 +308,8 @@ var TEST_TARGETS = [
      'ACAOrigin=' + BASE_ORIGIN + '&ACAHeaders=x-serviceworker-test') +
    '&mode=cors&credentials=same-origin&method=GET&headers=CUSTOM' +
    '&ACAOrigin=' + BASE_ORIGIN + '&ACAHeaders=x-serviceworker-test',
-   [fetchRejected]],
+   [fetchResolved, noContentLength, noServerHeader, hasBody, typeCors],
+   [methodIsGET, hasCustomHeader, authCheckNone]],
 ];
 
 if (self.importScripts) {
