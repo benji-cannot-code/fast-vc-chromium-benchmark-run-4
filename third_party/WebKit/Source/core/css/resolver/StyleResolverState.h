b794b59b215acdcf99de9bf7b3da40ea26ebcb64
alancutter <alancutter@chromium.org>
2016-12-04 21:51:15 -0800

    Apply custom property animations
    This change enables CSS and Web animations to apply animated
    effects on CSS custom properties.
    
    This change involves a restructure of style resolving detailed
    in the design doc:
    https://docs.google.com/a/chromium.org/document/d/1V27q30H-pQZVbzHCJjdFThBSeuvxkGLdGmb1hLWBAiY
    
    This is a reland of https://codereview.chromium.org/2309963002
    with fixes for when the baseComputedStyle optimisation is active.
    The revert was due to crbug.com/669790 where behaviour differs
    depending on whether asserts are enabled, thus none of the try
    bots picked up on the bug.
    
    BUG=644148
    
    Review-Url: https://codereview.chromium.org/2532953008
    Cr-Commit-Position: refs/heads/master@{#436235}

--- third_party/WebKit/Source/core/css/resolver/StyleResolverState.h
+++ third_party/WebKit/Source/core/css/resolver/StyleResolverState.h
@@ -101,6 +101,20 @@ class CORE_EXPORT StyleResolverState {
 
   CSSAnimationUpdate& animationUpdate() { return m_animationUpdate; }
 
+  bool isAnimationInterpolationMapReady() const {
+    return m_isAnimationInterpolationMapReady;
+  }
+  void setIsAnimationInterpolationMapReady() {
+    m_isAnimationInterpolationMapReady = true;
+  }
+
+  bool isAnimatingCustomProperties() const {
+    return m_isAnimatingCustomProperties;
+  }
+  void setIsAnimatingCustomProperties(bool value) {
+    m_isAnimatingCustomProperties = value;
+  }
+
   void setParentStyle(PassRefPtr<ComputedStyle> parentStyle) {
     m_parentStyle = parentStyle;
   }
@@ -202,6 +216,8 @@ class CORE_EXPORT StyleResolverState {
   RefPtr<ComputedStyle> m_parentStyle;
 
   CSSAnimationUpdate m_animationUpdate;
+  bool m_isAnimationInterpolationMapReady;
+  bool m_isAnimatingCustomProperties;
 
   bool m_applyPropertyToRegularStyle;
   bool m_applyPropertyToVisitedLinkStyle;
