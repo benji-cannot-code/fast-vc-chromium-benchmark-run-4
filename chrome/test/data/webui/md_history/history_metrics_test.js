0c652f9a96e978d3f937b1db62de9856b55ab269
skym <skym@chromium.org>
2016-12-05 09:56:32 -0800

    [Sync] Removing complexity around PROXY_TABS now that empty sessions are provided when disabled.
    Now that https://codereview.chromium.org/2504433002/ has landed,
    sessions data is always empty when PROXY_TABS aka open tabs is
    disabled. This makes the isTabSyncEnabled boolean that is passed
    through the UI logic unnecessary.
    
    It is imaginable to have a scenario where you would want to
    differentiate between having no sessions data and have PROXY_TABS being
    disabled. However, it doesn't seem that we currently do this anywhere,
    and the percentage of the population that disables PROXY_TABS is so
    small, it may never make sense to do this. Worst case we can add this
    logic back.
    
    BUG=665532
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2517243002
    Cr-Commit-Position: refs/heads/master@{#436345}

--- chrome/test/data/webui/md_history/history_metrics_test.js
+++ chrome/test/data/webui/md_history/history_metrics_test.js
@@ -169,7 +169,7 @@ suite('Metrics', function() {
             ]
         ),
       ];
-      setForeignSessions(sessionList, true);
+      setForeignSessions(sessionList);
       return PolymerTest.flushTasks();
     }).then(() => {
       assertEquals(1, histogram[SyncedTabsHistogram.HAS_FOREIGN_DATA]);
