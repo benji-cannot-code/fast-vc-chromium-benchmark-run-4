bb8100dc7389cda9a281b9d9768b3ae17f91935e
kotenkov <kotenkov@yandex-team.ru>
2016-12-04 17:46:32 -0800

    Fix search restart after zero entries.
    BUG=669983
    
    Review-Url: https://codereview.chromium.org/2537773008
    Cr-Commit-Position: refs/heads/master@{#436215}

--- third_party/WebKit/Source/web/TextFinder.cpp
+++ third_party/WebKit/Source/web/TextFinder.cpp
@@ -208,6 +208,9 @@ bool TextFinder::find(int identifier,
                               identifier);
   }
 
+  // We found something, so the result of the previous scoping may be outdated.
+  m_lastFindRequestCompletedWithNoMatches = false;
+
   return true;
 }
 
