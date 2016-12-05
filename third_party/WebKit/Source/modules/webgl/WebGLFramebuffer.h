535a0fe14ce29b84dbe21a807abd92b7ac82449c
mlippautz <mlippautz@chromium.org>
2016-12-05 03:38:42 -0800

    [wrapper-tracing] Fix WebGL extension and attachment handling
    Fixes a wrapper tracing issue where ExtensionTracker and WebGLAttachment were
    not handled properly. Has no affect on regular Oilpan garbage collection.
    
    With wrapper tracing enabled this could've resulting in wrappers for typed
    extensions and attachments being prematurely collected.
    
    The issue was flushed out when we were able to generate compile-time errors
    instead of having a catch-all dispatcher at runtime in
    http://crrev.com/2533383003.
    
    BUG=chromium:468240
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Committed: https://crrev.com/fb0beca59e6cc67a6f5b599476c8a3b27ce0418b
    Review-Url: https://codereview.chromium.org/2539113002
    Cr-Original-Commit-Position: refs/heads/master@{#435262}
    Cr-Commit-Position: refs/heads/master@{#436263}

--- third_party/WebKit/Source/modules/webgl/WebGLFramebuffer.h
+++ third_party/WebKit/Source/modules/webgl/WebGLFramebuffer.h
@@ -46,7 +46,8 @@ class WebGLFramebuffer final : public WebGLContextObject {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
-  class WebGLAttachment : public GarbageCollected<WebGLAttachment> {
+  class WebGLAttachment : public GarbageCollected<WebGLAttachment>,
+                          public TraceWrapperBase {
    public:
     virtual WebGLSharedObject* object() const = 0;
     virtual bool isSharedObject(WebGLSharedObject*) const = 0;
