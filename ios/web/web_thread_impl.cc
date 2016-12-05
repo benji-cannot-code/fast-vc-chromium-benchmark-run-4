ea72d307fee52b600ed6e7559183f438eeead75b
fdoray <fdoray@chromium.org>
2016-12-05 13:24:53 -0800

    Remove ThreadRestrictions::ScopedAllowSingleton.
    It's used in two files that don't really need it.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2536803002
    Cr-Commit-Position: refs/heads/master@{#436410}

--- ios/web/web_thread_impl.cc
+++ ios/web/web_thread_impl.cc
@@ -369,11 +369,6 @@ bool WebThread::IsThreadInitialized(ID identifier) {
 
 // static
 bool WebThread::CurrentlyOn(ID identifier) {
-  // This shouldn't use MessageLoop::current() since it uses LazyInstance which
-  // may be deleted by ~AtExitManager when a WorkerPool thread calls this
-  // function.
-  // http://crbug.com/63678
-  base::ThreadRestrictions::ScopedAllowSingleton allow_singleton;
   WebThreadGlobals& globals = g_globals.Get();
   base::AutoLock lock(globals.lock);
   DCHECK(identifier >= 0 && identifier < ID_COUNT);
@@ -457,11 +452,6 @@ bool WebThread::GetCurrentThreadIdentifier(ID* identifier) {
   if (g_globals == nullptr)
     return false;
 
-  // This shouldn't use MessageLoop::current() since it uses LazyInstance which
-  // may be deleted by ~AtExitManager when a WorkerPool thread calls this
-  // function.
-  // http://crbug.com/63678
-  base::ThreadRestrictions::ScopedAllowSingleton allow_singleton;
   base::MessageLoop* cur_message_loop = base::MessageLoop::current();
   WebThreadGlobals& globals = g_globals.Get();
   for (int i = 0; i < ID_COUNT; ++i) {
