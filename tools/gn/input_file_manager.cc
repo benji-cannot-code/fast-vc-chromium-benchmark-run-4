3460012c6a840655540a2a60b1447fd4e9479cb7
fdoray <fdoray@chromium.org>
2016-12-05 10:36:55 -0800

    gn: Remove Scheduler::pool().
    InputFileManager used this accessor to post a BLOCK_SHUTDOWN task.
    This can be replaced by a call to ScheduleWork(). In both cases,
    Scheduler::Run() doesn't return before the task runs.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2515383005
    Cr-Commit-Position: refs/heads/master@{#436362}

--- tools/gn/input_file_manager.cc
+++ tools/gn/input_file_manager.cc
@@ -148,9 +148,7 @@ bool InputFileManager::AsyncLoadFile(const LocationRange& origin,
       }
     }
   }
-  g_scheduler->pool()->PostWorkerTaskWithShutdownBehavior(
-      FROM_HERE, schedule_this,
-      base::SequencedWorkerPool::BLOCK_SHUTDOWN);
+  g_scheduler->ScheduleWork(schedule_this);
   return true;
 }
 
