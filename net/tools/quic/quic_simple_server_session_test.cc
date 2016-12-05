da914bcec4772d034c3d7dddcf75cf2e18b0c409
vasilvv <vasilvv@chromium.org>
2016-12-05 11:25:51 -0800

    Remove the unused GenerateBody method
    R=rch@chromium.org
    BUG=
    
    Review-Url: https://codereview.chromium.org/2551763004
    Cr-Commit-Position: refs/heads/master@{#436373}

--- net/tools/quic/quic_simple_server_session_test.cc
+++ net/tools/quic/quic_simple_server_session_test.cc
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 using net::test::CryptoTestUtils;
-using net::test::GenerateBody;
 using net::test::MockQuicConnection;
 using net::test::MockQuicConnectionHelper;
 using net::test::QuicConfigPeer;
