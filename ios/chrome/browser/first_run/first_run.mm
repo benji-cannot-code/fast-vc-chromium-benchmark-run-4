0adecf19e0cf4a255d18bf5c3096f8299a315bd4
stkhapugin <stkhapugin@chromium.org>
2016-12-05 04:48:26 -0800

    [ObjC ARC] Converts ios/chrome/browser/first_run:first_run to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2532533002
    Cr-Commit-Position: refs/heads/master@{#436269}

--- ios/chrome/browser/first_run/first_run.mm
+++ ios/chrome/browser/first_run/first_run.mm
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "ios/chrome/browser/chrome_paths.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace {
 
 // The absence of kSentinelFile file will tell us it is a first run.
