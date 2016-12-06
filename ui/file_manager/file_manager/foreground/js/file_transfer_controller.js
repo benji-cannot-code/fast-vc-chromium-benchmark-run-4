42af55623d6728c7449a9c91c82f70f1a3334a3d
yamaguchi <yamaguchi@chromium.org>
2016-12-06 02:24:01 -0800

    Disallow dropping or pasting files to a volume with unrecognized partition.
    BUG=631957
    TEST=manual test as noted in the bug
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2548363004
    Cr-Commit-Position: refs/heads/master@{#436560}

--- ui/file_manager/file_manager/foreground/js/file_transfer_controller.js
+++ ui/file_manager/file_manager/foreground/js/file_transfer_controller.js
@@ -1203,6 +1203,8 @@ FileTransferController.prototype.canPasteOrDrop_ =
     return false;
   var destinationLocationInfo =
       this.volumeManager_.getLocationInfo(destinationEntry);
+  if (destinationLocationInfo.volumeInfo.error)
+    return false;
   if (!destinationLocationInfo || destinationLocationInfo.isReadOnly)
     return false;
   if (!clipboardData.types || clipboardData.types.indexOf('fs/tag') === -1)
@@ -1328,6 +1330,8 @@ FileTransferController.prototype.selectDropEffect_ =
   var destinationLocationInfo =
       this.volumeManager_.getLocationInfo(destinationEntry);
   if (!destinationLocationInfo)
+    return new DropEffectAndLabel(DropEffectType.NONE, null);
+  if (destinationLocationInfo.volumeInfo.error)
     return new DropEffectAndLabel(DropEffectType.NONE, null);
   if (destinationLocationInfo.isReadOnly) {
     if (destinationLocationInfo.isSpecialSearchRoot) {
