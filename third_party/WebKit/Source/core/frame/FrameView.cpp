07d2cff0937734768f8e863769cd2c5da0d35b84
alemate <alemate@chromium.org>
2016-12-06 13:17:32 -0800

    Make ifdefs consistent in WebKit/Source/core/frame/.
    This CL addresses a few problems with #ifdefs:
    
    1) DCHECK requires the presence of variables used in it even if DCHECK_IS_ON()
    evaluates to 0. Therefore, if debug variable is defined only when DCHECK_IS_ON()
    is true, its usage must be guarded the same way.
    
    2) If debug method in base class is defined only when DCHECK_IS_ON() is true,
    its descendants must be guarded with the same check.
    
    BUG=666660
    
    Review-Url: https://codereview.chromium.org/2556723002
    Cr-Commit-Position: refs/heads/master@{#436728}

--- third_party/WebKit/Source/core/frame/FrameView.cpp
+++ third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -1261,7 +1261,7 @@ void FrameView::layout() {
   if (m_nestedLayoutCount)
     return;
 
-#if ENABLE(ASSERT)
+#if DCHECK_IS_ON()
   // Post-layout assert that nobody was re-marked as needing layout during
   // layout.
   layoutView()->assertSubtreeIsLaidOut();
@@ -1290,7 +1290,7 @@ void FrameView::invalidateTreeIfNeeded(
   invalidatePaintIfNeeded(paintInvalidationState);
   rootForPaintInvalidation.invalidateTreeIfNeeded(paintInvalidationState);
 
-#if ENABLE(ASSERT)
+#if DCHECK_IS_ON()
   layoutView()->assertSubtreeClearedPaintInvalidationFlags();
 #endif
 
@@ -3115,7 +3115,7 @@ void FrameView::updateStyleAndLayoutIfNeededRecursiveInternal() {
   // These asserts ensure that parent frames are clean, when child frames
   // finished updating layout and style.
   checkDoesNotNeedLayout();
-#if ENABLE(ASSERT)
+#if DCHECK_IS_ON()
   m_frame->document()->layoutView()->assertLaidOut();
 #endif
 
