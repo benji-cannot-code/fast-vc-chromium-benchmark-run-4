da69ebde421fa469a718d123b919bd98de1d2a5b
adithyas <adithyas@chromium.org>
2016-12-06 09:41:43 -0800

    Move <marquee> implementation to HTMLMarqueeElement.cpp
    - Moves HTMLMarqueeElement off Blink-in-JS
    
    - Reimplements HTMLMarqueeElement in C++: implementation matches
      HTMLMarqueeElement.js and uses standard DOM API wherever possible
    
    - Removes HTMLMarqueeElement.js
    
    BUG=669656,668618
    
    Review-Url: https://codereview.chromium.org/2549443003
    Cr-Commit-Position: refs/heads/master@{#436628}

--- third_party/WebKit/Source/core/html/HTMLMarqueeElement.h
+++ third_party/WebKit/Source/core/html/HTMLMarqueeElement.h
@@ -24,7 +24,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef HTMLMarqueeElement_h
 #define HTMLMarqueeElement_h
 
+#include "core/animation/Animation.h"
+#include "core/animation/KeyframeEffectModel.h"
+#include "core/dom/FrameRequestCallback.h"
 #include "core/html/HTMLElement.h"
+#include "wtf/Noncopyable.h"
 
 namespace blink {
 
@@ -32,6 +36,8 @@ class HTMLMarqueeElement final : public HTMLElement {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
+  DECLARE_VIRTUAL_TRACE();
+
   static HTMLMarqueeElement* create(Document&);
 
   void attributeChanged(const QualifiedName&,
@@ -43,8 +49,102 @@ class HTMLMarqueeElement final : public HTMLElement {
 
   bool isHorizontal() const;
 
+  int scrollAmount() const;
+  void setScrollAmount(int, ExceptionState&);
+
+  int scrollDelay() const;
+  void setScrollDelay(int, ExceptionState&);
+
+  int loop() const;
+  void setLoop(int, ExceptionState&);
+
+  void start();
+  void stop();
+
  private:
   explicit HTMLMarqueeElement(Document&);
+
+  class RequestAnimationFrameCallback final : public FrameRequestCallback {
+    WTF_MAKE_NONCOPYABLE(RequestAnimationFrameCallback);
+
+   public:
+    explicit RequestAnimationFrameCallback(HTMLMarqueeElement* marquee)
+        : m_marquee(marquee) {}
+    void handleEvent(double) override;
+
+    DEFINE_INLINE_VIRTUAL_TRACE() {
+      visitor->trace(m_marquee);
+      FrameRequestCallback::trace(visitor);
+    }
+
+   private:
+    Member<HTMLMarqueeElement> m_marquee;
+  };
+
+  class AnimationFinished final : public EventListener {
+    WTF_MAKE_NONCOPYABLE(AnimationFinished);
+
+   public:
+    explicit AnimationFinished(HTMLMarqueeElement* marquee)
+        : EventListener(CPPEventListenerType), m_marquee(marquee) {}
+
+    bool operator==(const EventListener& that) const override {
+      return this == &that;
+    }
+
+    void handleEvent(ExecutionContext*, Event*) override;
+
+    DEFINE_INLINE_VIRTUAL_TRACE() {
+      visitor->trace(m_marquee);
+      EventListener::trace(visitor);
+    }
+
+   private:
+    Member<HTMLMarqueeElement> m_marquee;
+  };
+
+  struct AnimationParameters {
+    String transformBegin;
+    String transformEnd;
+    double distance;
+  };
+
+  struct Metrics {
+    double contentWidth;
+    double contentHeight;
+    double marqueeWidth;
+    double marqueeHeight;
+  };
+
+  void attributeChangedCallback(const QualifiedName& attr,
+                                const String& newValue);
+
+  StringKeyframeEffectModel* createEffectModel(AnimationParameters&);
+
+  void continueAnimation();
+  bool shouldContinue();
+
+  enum Behavior { Scroll, Slide, Alternate };
+  Behavior behavior() const;
+
+  enum Direction { Left, Right, Up, Down };
+  Direction direction() const;
+
+  bool trueSpeed() const;
+
+  Metrics getMetrics();
+  AnimationParameters getAnimationParameters();
+  AtomicString createTransform(double value) const;
+
+  static const int kDefaultScrollAmount = 6;
+  static const int kDefaultScrollDelayMS = 85;
+  static const int kMinimumScrollDelayMS = 60;
+  static const int kDefaultLoopLimit = -1;
+
+  int m_continueCallbackRequestId = 0;
+  int m_loopCount = 0;
+  Member<Element> m_mover;
+  Member<Animation> m_player;
 };
 
 }  // namespace blink
