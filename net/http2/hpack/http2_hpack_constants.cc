8edf1f11016923e5121c30afa69553984e17d363
bnc <bnc@chromium.org>
2016-12-06 08:17:50 -0800

    Add new HTTP/2 and HPACK decoder in net/http2/.
    It this breaks builders again, please feel free to revert first
    and ask questions later.
    
    This re-lands https://crrev.com/2293613002 with
    DecodeBufferDeathTest.NonNullBufferRequired disabled.
    
    Patch Set 1 is identical to https://crrev.com/2293613002.
    Patch Set 2 disables DecodeBufferDeathTest.NonNullBufferRequired.
    
    BUG=488484
    
    TBR=rch@chromium.org
    
    Review-Url: https://codereview.chromium.org/2552703004
    Cr-Commit-Position: refs/heads/master@{#436611}

---
+++ net/http2/hpack/http2_hpack_constants.cc
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/http2/hpack/http2_hpack_constants.h"
+
+#include <sstream>
+
+namespace net {
+
+std::string HpackEntryTypeToString(HpackEntryType v) {
+  switch (v) {
+    case HpackEntryType::kIndexedHeader:
+      return "kIndexedHeader";
+    case HpackEntryType::kDynamicTableSizeUpdate:
+      return "kDynamicTableSizeUpdate";
+    case HpackEntryType::kIndexedLiteralHeader:
+      return "kIndexedLiteralHeader";
+    case HpackEntryType::kUnindexedLiteralHeader:
+      return "kUnindexedLiteralHeader";
+    case HpackEntryType::kNeverIndexedLiteralHeader:
+      return "kNeverIndexedLiteralHeader";
+  }
+  std::stringstream ss;
+  ss << "UnknownHpackEntryType(" << static_cast<int>(v) << ")";
+  return ss.str();
+}
+
+std::ostream& operator<<(std::ostream& out, HpackEntryType v) {
+  return out << HpackEntryTypeToString(v);
+}
+
+}  // namespace net
