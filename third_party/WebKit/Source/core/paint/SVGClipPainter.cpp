9acf2f09445356403746db851b65281337e54f01
fs <fs@opera.com>
2016-12-06 08:01:43 -0800

    Don't fail clip-paths with empty bounds
    An empty nested clip-path should result in an empty clip-path (clipping
    away everything.)
    
    BUG=671543
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2555483003
    Cr-Commit-Position: refs/heads/master@{#436605}

--- third_party/WebKit/Source/core/paint/SVGClipPainter.cpp
+++ third_party/WebKit/Source/core/paint/SVGClipPainter.cpp
@@ -49,7 +49,7 @@ bool SVGClipPainter::prepareEffect(const LayoutObject& target,
 
   m_clip.clearInvalidationMask();
 
-  if (visualRect.isEmpty() || m_clip.hasCycle())
+  if (m_clip.hasCycle())
     return false;
 
   SVGClipExpansionCycleHelper inClipExpansionChange(m_clip);
