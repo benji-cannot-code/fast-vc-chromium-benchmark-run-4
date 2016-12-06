942e18f8024090df69262f5f3db0e1fd7c0dea8c
kerrnel <kerrnel@chromium.org>
2016-12-06 14:05:26 -0800

    Disable flash component updates if Chrome OS version is pinned.
    Disable flash component updates for enterprise bound Chromebooks which
    disable automatic updates or pin their version to a specific prefix.
    These admins are explicitly choosing to reject the latest security
    updates in favor of version stability.
    
    BUG=630420
    
    Review-Url: https://codereview.chromium.org/2553753003
    Cr-Commit-Position: refs/heads/master@{#436755}

--- chrome/browser/chromeos/settings/device_settings_provider.cc
+++ chrome/browser/chromeos/settings/device_settings_provider.cc
@@ -94,6 +94,7 @@ const char* const kKnownSettings[] = {
     kSystemLogUploadEnabled,
     kSystemTimezonePolicy,
     kSystemUse24HourClock,
+    kTargetVersionPrefix,
     kUpdateDisabled,
     kVariationsRestrictParameter,
 };
@@ -331,6 +332,12 @@ void DecodeAutoUpdatePolicies(
       new_values_cache->SetBoolean(kUpdateDisabled,
                                    au_settings_proto.update_disabled());
     }
+
+    if (au_settings_proto.has_target_version_prefix()) {
+      new_values_cache->SetString(kTargetVersionPrefix,
+                                  au_settings_proto.target_version_prefix());
+    }
+
     const RepeatedField<int>& allowed_connection_types =
         au_settings_proto.allowed_connection_types();
     std::unique_ptr<base::ListValue> list(new base::ListValue());
