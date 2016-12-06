6eaa93462b6787e11913535ff4c0a147faed9e33
rohitrao <rohitrao@chromium.org>
2016-12-05 18:49:58 -0800

    [ios] Adds JS resources for various features.
    BUG=662936
    
    Review-Url: https://codereview.chromium.org/2553563004
    Cr-Commit-Position: refs/heads/master@{#436500}

---
+++ ios/chrome/browser/web/resources/print.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This file adheres to closure-compiler conventions in order to enable
+// compilation with ADVANCED_OPTIMIZATIONS. In particular, members that are to
+// be accessed externally should be specified in this['style'] as opposed to
+// this.style because member identifiers are minified by default.
+// See http://goo.gl/FwOgy
+
+// Intercept window.print calls.
+
+/**
+ * Namespace for this module.
+ */
+__gCrWeb['print'] = {};
+
+new function() {
+  // Overwrites window.print function to invoke chrome command.
+  window.print = function() {
+    __gCrWeb.message.invokeOnHost({'command': 'print.window.print'});
+  };
+}
