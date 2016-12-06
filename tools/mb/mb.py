603ddb038c595fb0fde947331792e46af39e67b7
thomasanderson <thomasanderson@google.com>
2016-12-06 10:21:12 -0800

    Remove build_dir arg from xvfb.py
    R=dpranke@chromium.org
    BUG=670470
    
    Review-Url: https://codereview.chromium.org/2521353003
    Cr-Commit-Position: refs/heads/master@{#436652}

--- tools/mb/mb.py
+++ tools/mb/mb.py
@@ -1111,7 +1111,6 @@ class MetaBuildWrapper(object):
       ]
       cmdline = [
         '../../testing/xvfb.py',
-        '.',
         './' + str(executable) + executable_suffix,
         '--brave-new-test-launcher',
         '--test-launcher-bot-mode',
