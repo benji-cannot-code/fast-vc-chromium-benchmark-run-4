9fb6a41cf590741ca45f040f179667331d58a9e1
dmurph <dmurph@chromium.org>
2016-12-05 18:28:12 -0800

    [BlobStorage] Enabling disk storage and paging.
    Adds support for saving blobs to disk by either paging them to disk
    or sending files directly to the renderer.
    
    Note: Disk is turned off by default (limit=0). So other than tests
    that explicitly set a higher disk limit, this patch should be a NOOP.
    The next patch will configure this limit (and the current RAM limit)
    by the OS, disk size, and available RAM.
    
    R=michaeln,kinuko
    BUG=375297
    
    Committed: https://crrev.com/89bdfe0bd9499962f9f05c30a72d46fc6510d45a
    Review-Url: https://codereview.chromium.org/2516713002
    Cr-Original-Commit-Position: refs/heads/master@{#436415}
    Cr-Commit-Position: refs/heads/master@{#436485}

--- content/browser/blob_storage/chrome_blob_storage_context.h
+++ content/browser/blob_storage/chrome_blob_storage_context.h
@@ -11,12 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/files/file_path.h"
 #include "base/memory/ref_counted.h"
 #include "base/sequenced_task_runner_helpers.h"
 #include "content/common/content_export.h"
 
 namespace base {
 class FilePath;
+class TaskRunner;
 class Time;
 }
 
@@ -45,7 +47,8 @@ class CONTENT_EXPORT ChromeBlobStorageContext
   static ChromeBlobStorageContext* GetFor(
       BrowserContext* browser_context);
 
-  void InitializeOnIOThread();
+  void InitializeOnIOThread(base::FilePath blob_storage_dir,
+                            scoped_refptr<base::TaskRunner> file_task_runner);
 
   storage::BlobStorageContext* context() const { return context_.get(); }
 
