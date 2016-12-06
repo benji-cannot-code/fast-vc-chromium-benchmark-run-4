06e1322415bc6b025950f01cf559e513d158eaa5
alexilin <alexilin@chromium.org>
2016-12-06 02:43:59 -0800

    predictors: Add browsertests that check fetching through javascript.
    Make sure that ResourcePrefetchPredictor awares about javascript initiated
    requests + test for iframe.
    
    BUG=650253
    
    Review-Url: https://codereview.chromium.org/2540803002
    Cr-Commit-Position: refs/heads/master@{#436566}

---
+++ chrome/test/data/predictors/append_child.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var img = document.createElement("img");
+img.src = "/handled-by-test/image.png"
+document.body.appendChild(img);
+
+var link = document.createElement("link");
+link.rel = "stylesheet";
+link.href = "/handled-by-test/style.css";
+document.head.appendChild(link);
+
+var script = document.createElement("script");
+script.src = "/handled-by-test/script.js";
+document.body.appendChild(script);
---
+++ chrome/test/data/predictors/xhr.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function requestSynchronously(url) {
+  var request = new XMLHttpRequest();
+  request.open('GET', url, false);
+  request.send(null);
+}
+
+requestSynchronously('/handled-by-test/image.png');
+requestSynchronously("/handled-by-test/style.css");
+requestSynchronously("/handled-by-test/script.js");
