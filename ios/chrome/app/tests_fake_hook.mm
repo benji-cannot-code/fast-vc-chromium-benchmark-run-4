026914152ceae999e8f12333ab2a7c650af7321b
rohitrao <rohitrao@chromium.org>
2016-12-06 07:33:50 -0800

    [ios] Adds test hooks to enable/disable functionality while testing.
    BUG=671589
    
    Review-Url: https://codereview.chromium.org/2557613004
    Cr-Commit-Position: refs/heads/master@{#436603}

---
+++ ios/chrome/app/tests_fake_hook.mm
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/app/tests_hook.h"
+
+namespace tests_hook {
+
+bool DisableContextualSearch() {
+  return false;
+}
+bool DisableFirstRun() {
+  return false;
+}
+bool DisableSigninRecallPromo() {
+  return false;
+}
+bool DisableUpdateService() {
+  return false;
+}
+void SetUpTestsIfPresent() {}
+void RunTestsIfPresent() {}
+
+}  // namespace tests_hook
