0841c983d3649fa30f684d0ebb686fa573dddb5c
mlippautz <mlippautz@chromium.org>
2016-12-07 03:55:10 -0800

    [wrapper-tracing] WebGLRenderingContextBase: Fix wrapper tracing
    BUG=671942
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2560473005
    Cr-Commit-Position: refs/heads/master@{#436919}

--- third_party/WebKit/Source/modules/webgl/WebGLRenderingContextBase.cpp
+++ third_party/WebKit/Source/modules/webgl/WebGLRenderingContextBase.cpp
@@ -7677,9 +7677,7 @@ DEFINE_TRACE(WebGLRenderingContextBase) {
 }
 
 DEFINE_TRACE_WRAPPERS(WebGLRenderingContextBase) {
-  if (isContextLost()) {
-    return;
-  }
+  visitor->traceWrappers(m_contextGroup);
   visitor->traceWrappers(m_boundArrayBuffer);
   visitor->traceWrappers(m_renderbufferBinding);
   visitor->traceWrappers(m_framebufferBinding);
