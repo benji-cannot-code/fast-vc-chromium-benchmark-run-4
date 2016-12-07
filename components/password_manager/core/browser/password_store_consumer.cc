26960df053cb6a7c970d2a8abc7df03b513bbdea
vabr <vabr@chromium.org>
2016-12-07 02:28:31 -0800

    Pass InteractionStats by value
    The APIs related to PasswordStore currently pass vectors of unique_ptrs of
    InteractionStats. Thanks to C++11 move semantics it is no performance hit to
    pass around vectors of the stats themselves, leading to a simpler API. This CL
    drops the unique_ptr part.
    
    BUG=621355
    
    Review-Url: https://codereview.chromium.org/2552263002
    Cr-Commit-Position: refs/heads/master@{#436908}

--- components/password_manager/core/browser/password_store_consumer.cc
+++ components/password_manager/core/browser/password_store_consumer.cc
@@ -16,6 +16,6 @@ PasswordStoreConsumer::~PasswordStoreConsumer() {
 }
 
 void PasswordStoreConsumer::OnGetSiteStatistics(
-    std::vector<std::unique_ptr<InteractionsStats>> stats) {}
+    std::vector<InteractionsStats> stats) {}
 
 }  // namespace password_manager
