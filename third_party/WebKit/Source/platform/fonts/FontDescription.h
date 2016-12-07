c3f1ce6b3327d14199f99d32a2ccec04c48efbfd
drott <drott@chromium.org>
2016-12-07 04:13:21 -0800

    Create a representation for OpenType variation settings
    Combine this with FontFeatureSettings as it is a very similar data
    structure. The intention was to avoid duplication of the
    FontFeatureSettings code and files.
    
    No tests, no functional change.
    
    BUG=670754
    R=eae,kojii
    
    Review-Url: https://codereview.chromium.org/2547923002
    Cr-Commit-Position: refs/heads/master@{#436924}

--- third_party/WebKit/Source/platform/fonts/FontDescription.h
+++ third_party/WebKit/Source/platform/fonts/FontDescription.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/LayoutLocale.h"
 #include "platform/fonts/FontCacheKey.h"
 #include "platform/fonts/FontFamily.h"
-#include "platform/fonts/FontFeatureSettings.h"
 #include "platform/fonts/FontOrientation.h"
 #include "platform/fonts/FontSmoothingMode.h"
 #include "platform/fonts/FontTraits.h"
@@ -40,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/fonts/FontWidthVariant.h"
 #include "platform/fonts/TextRenderingMode.h"
 #include "platform/fonts/TypesettingFeatures.h"
+#include "platform/fonts/opentype/FontSettings.h"
 #include "wtf/Allocator.h"
 #include "wtf/MathExtras.h"
 
