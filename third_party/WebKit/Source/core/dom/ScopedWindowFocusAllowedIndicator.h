ccda1f56a9c22f81f4e4fe369c1120624dd8755b
haraken <haraken@chromium.org>
2016-12-07 01:37:26 -0800

    Remove ContextLifecycleObserver from ScopedWindowFocusAllowedIndicator
    - This CL removes ScopedWindowFocusAllowedIndicator::Observer
    since the observer indirection is unnecessary.
    
    - This CL makes ScopedWindowFocusAllowedIndicator hold a strong member
    to ExecutionContext and removes ContextLifecycleObserver.
    
    BUG=610176
    
    Review-Url: https://codereview.chromium.org/2555113002
    Cr-Commit-Position: refs/heads/master@{#436898}

--- third_party/WebKit/Source/core/dom/ScopedWindowFocusAllowedIndicator.h
+++ third_party/WebKit/Source/core/dom/ScopedWindowFocusAllowedIndicator.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ScopedWindowFocusAllowedIndicator_h
 #define ScopedWindowFocusAllowedIndicator_h
 
-#include "core/dom/ContextLifecycleObserver.h"
 #include "core/dom/ExecutionContext.h"
 #include "wtf/Noncopyable.h"
 
@@ -18,32 +17,17 @@ class ScopedWindowFocusAllowedIndicator final {
 
  public:
   explicit ScopedWindowFocusAllowedIndicator(ExecutionContext* executionContext)
-      : m_observer(new Observer(executionContext)) {}
-  ~ScopedWindowFocusAllowedIndicator() { m_observer->dispose(); }
+      : m_executionContext(executionContext) {
+    executionContext->allowWindowInteraction();
+  }
+  ~ScopedWindowFocusAllowedIndicator() {
+    m_executionContext->consumeWindowInteraction();
+  }
 
  private:
-  class Observer final : public GarbageCollected<Observer>,
-                         public ContextLifecycleObserver {
-    USING_GARBAGE_COLLECTED_MIXIN(Observer);
-
-   public:
-    explicit Observer(ExecutionContext* executionContext)
-        : ContextLifecycleObserver(executionContext) {
-      if (executionContext)
-        executionContext->allowWindowInteraction();
-    }
-
-    void dispose() {
-      if (getExecutionContext())
-        getExecutionContext()->consumeWindowInteraction();
-    }
-  };
-
-  // In Oilpan, destructors are not allowed to touch other on-heap objects.
-  // The Observer indirection is needed to keep
-  // ScopedWindowFocusAllowedIndicator off-heap and thus allows its destructor
-  // to call getExecutionContext()->consumeWindowInteraction().
-  Persistent<Observer> m_observer;
+  // This doesn't create a cycle because ScopedWindowFocusAllowedIndicator
+  // is used only on a machine stack.
+  Persistent<ExecutionContext> m_executionContext;
 };
 
 }  // namespace blink
