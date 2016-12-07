0fcbe6c3d80890223dd74ebbd0fde8cd14be215f
haraken <haraken@chromium.org>
2016-12-07 07:20:51 -0800

    Remove ContextLifecycleObserver from V8V0CustomElementLifecycleCallbacks
    It's not needed because V8V0CustomElementLifecycleCallbacks already holds
    ScriptState.
    
    BUG=610176
    
    Review-Url: https://codereview.chromium.org/2561573002
    Cr-Commit-Position: refs/heads/master@{#436960}

--- third_party/WebKit/Source/bindings/core/v8/V8V0CustomElementLifecycleCallbacks.cpp
+++ third_party/WebKit/Source/bindings/core/v8/V8V0CustomElementLifecycleCallbacks.cpp
@@ -105,7 +105,6 @@ V8V0CustomElementLifecycleCallbacks::V8V0CustomElementLifecycleCallbacks(
     v8::MaybeLocal<v8::Function> attributeChanged)
     : V0CustomElementLifecycleCallbacks(
           flagSet(attached, detached, attributeChanged)),
-      ContextLifecycleObserver(scriptState->getExecutionContext()),
       m_scriptState(scriptState),
       m_prototype(scriptState->isolate(), prototype),
       m_created(scriptState->isolate(), created),
@@ -123,7 +122,7 @@ V8V0CustomElementLifecycleCallbacks::V8V0CustomElementLifecycleCallbacks(
 }
 
 V8PerContextData* V8V0CustomElementLifecycleCallbacks::creationContextData() {
-  if (!getExecutionContext())
+  if (!m_scriptState->contextIsValid())
     return 0;
 
   v8::Local<v8::Context> context = m_scriptState->context();
@@ -152,9 +151,6 @@ void V8V0CustomElementLifecycleCallbacks::created(Element* element) {
   // FIXME: callbacks while paused should be queued up for execution to
   // continue then be delivered in order rather than delivered immediately.
   // Bug 329665 tracks similar behavior for other synchronous events.
-  if (!getExecutionContext() || getExecutionContext()->isContextDestroyed())
-    return;
-
   if (!m_scriptState->contextIsValid())
     return;
 
@@ -182,8 +178,8 @@ void V8V0CustomElementLifecycleCallbacks::created(Element* element) {
 
   v8::TryCatch exceptionCatcher(isolate);
   exceptionCatcher.SetVerbose(true);
-  V8ScriptRunner::callFunction(callback, getExecutionContext(), receiver, 0, 0,
-                               isolate);
+  V8ScriptRunner::callFunction(callback, m_scriptState->getExecutionContext(),
+                               receiver, 0, 0, isolate);
 }
 
 void V8V0CustomElementLifecycleCallbacks::attached(Element* element) {
@@ -202,9 +198,6 @@ void V8V0CustomElementLifecycleCallbacks::attributeChanged(
   // FIXME: callbacks while paused should be queued up for execution to
   // continue then be delivered in order rather than delivered immediately.
   // Bug 329665 tracks similar behavior for other synchronous events.
-  if (!getExecutionContext() || getExecutionContext()->isContextDestroyed())
-    return;
-
   if (!m_scriptState->contextIsValid())
     return;
   ScriptState::Scope scope(m_scriptState.get());
@@ -227,8 +220,8 @@ void V8V0CustomElementLifecycleCallbacks::attributeChanged(
 
   v8::TryCatch exceptionCatcher(isolate);
   exceptionCatcher.SetVerbose(true);
-  V8ScriptRunner::callFunction(callback, getExecutionContext(), receiver,
-                               WTF_ARRAY_LENGTH(argv), argv, isolate);
+  V8ScriptRunner::callFunction(callback, m_scriptState->getExecutionContext(),
+                               receiver, WTF_ARRAY_LENGTH(argv), argv, isolate);
 }
 
 void V8V0CustomElementLifecycleCallbacks::call(
@@ -237,9 +230,6 @@ void V8V0CustomElementLifecycleCallbacks::call(
   // FIXME: callbacks while paused should be queued up for execution to
   // continue then be delivered in order rather than delivered immediately.
   // Bug 329665 tracks similar behavior for other synchronous events.
-  if (!getExecutionContext() || getExecutionContext()->isContextDestroyed())
-    return;
-
   if (!m_scriptState->contextIsValid())
     return;
   ScriptState::Scope scope(m_scriptState.get());
@@ -255,13 +245,12 @@ void V8V0CustomElementLifecycleCallbacks::call(
 
   v8::TryCatch exceptionCatcher(isolate);
   exceptionCatcher.SetVerbose(true);
-  V8ScriptRunner::callFunction(callback, getExecutionContext(), receiver, 0, 0,
-                               isolate);
+  V8ScriptRunner::callFunction(callback, m_scriptState->getExecutionContext(),
+                               receiver, 0, 0, isolate);
 }
 
 DEFINE_TRACE(V8V0CustomElementLifecycleCallbacks) {
   V0CustomElementLifecycleCallbacks::trace(visitor);
-  ContextLifecycleObserver::trace(visitor);
 }
 
 }  // namespace blink
--- third_party/WebKit/Source/bindings/core/v8/V8V0CustomElementLifecycleCallbacks.h
+++ third_party/WebKit/Source/bindings/core/v8/V8V0CustomElementLifecycleCallbacks.h
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "bindings/core/v8/ScopedPersistent.h"
 #include "bindings/core/v8/ScriptState.h"
-#include "core/dom/ContextLifecycleObserver.h"
 #include "core/dom/custom/V0CustomElementLifecycleCallbacks.h"
 #include "wtf/PassRefPtr.h"
 #include <memory>
@@ -47,10 +46,7 @@ class Element;
 class V8PerContextData;
 
 class V8V0CustomElementLifecycleCallbacks final
-    : public V0CustomElementLifecycleCallbacks,
-      public ContextLifecycleObserver {
-  USING_GARBAGE_COLLECTED_MIXIN(V8V0CustomElementLifecycleCallbacks);
-
+    : public V0CustomElementLifecycleCallbacks {
  public:
   static V8V0CustomElementLifecycleCallbacks* create(
       ScriptState*,
