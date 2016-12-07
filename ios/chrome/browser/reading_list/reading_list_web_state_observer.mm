88cd3457dcbf2d0aab61d5de10b2272dcc2f0993
stkhapugin <stkhapugin@chromium.org>
2016-12-07 06:20:51 -0800

    [ObjC ARC] Converts ios/chrome/browser/reading_list:reading_list to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2552113003
    Cr-Commit-Position: refs/heads/master@{#436943}

--- ios/chrome/browser/reading_list/reading_list_web_state_observer.mm
+++ ios/chrome/browser/reading_list/reading_list_web_state_observer.mm
@@ -16,6 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/web/public/navigation_manager.h"
 #include "ios/web/public/web_state/web_state_user_data.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 #pragma mark - ReadingListWebStateObserverUserDataWrapper
 
 namespace {
