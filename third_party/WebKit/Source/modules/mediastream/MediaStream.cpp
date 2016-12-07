434083be674b198a41f4427af8893579b55d593b
haraken <haraken@chromium.org>
2016-12-07 01:56:16 -0800

    Remove ContextLifecycleObserver from MediaStream
    MediaStream doesn't need to inherit from ContextLifecycleObserver.
    It just needs to hold a strong member to ExecutionContext.
    
    BUG=610176
    
    Review-Url: https://codereview.chromium.org/2549353005
    Cr-Commit-Position: refs/heads/master@{#436903}

--- third_party/WebKit/Source/modules/mediastream/MediaStream.cpp
+++ third_party/WebKit/Source/modules/mediastream/MediaStream.cpp
@@ -97,10 +97,9 @@ MediaStream* MediaStream::create(ExecutionContext* context,
 
 MediaStream::MediaStream(ExecutionContext* context,
                          MediaStreamDescriptor* streamDescriptor)
-    : ContextLifecycleObserver(context),
-      m_stopped(false),
-      m_descriptor(streamDescriptor),
-      m_scheduledEventTimer(this, &MediaStream::scheduledEventTimerFired) {
+    : m_descriptor(streamDescriptor),
+      m_scheduledEventTimer(this, &MediaStream::scheduledEventTimerFired),
+      m_executionContext(context) {
   m_descriptor->setClient(this);
 
   size_t numberOfAudioTracks = m_descriptor->numberOfAudioComponents();
@@ -129,9 +128,8 @@ MediaStream::MediaStream(ExecutionContext* context,
 MediaStream::MediaStream(ExecutionContext* context,
                          const MediaStreamTrackVector& audioTracks,
                          const MediaStreamTrackVector& videoTracks)
-    : ContextLifecycleObserver(context),
-      m_stopped(false),
-      m_scheduledEventTimer(this, &MediaStream::scheduledEventTimerFired) {
+    : m_scheduledEventTimer(this, &MediaStream::scheduledEventTimerFired),
+      m_executionContext(context) {
   MediaStreamComponentVector audioComponents;
   MediaStreamComponentVector videoComponents;
 
@@ -298,7 +296,7 @@ void MediaStream::trackEnded() {
 }
 
 void MediaStream::streamEnded() {
-  if (m_stopped)
+  if (m_executionContext->isContextDestroyed())
     return;
 
   if (active()) {
@@ -312,34 +310,25 @@ bool MediaStream::addEventListenerInternal(
     EventListener* listener,
     const AddEventListenerOptionsResolved& options) {
   if (eventType == EventTypeNames::active)
-    UseCounter::count(getExecutionContext(), UseCounter::MediaStreamOnActive);
+    UseCounter::count(m_executionContext, UseCounter::MediaStreamOnActive);
   else if (eventType == EventTypeNames::inactive)
-    UseCounter::count(getExecutionContext(), UseCounter::MediaStreamOnInactive);
+    UseCounter::count(m_executionContext, UseCounter::MediaStreamOnInactive);
 
   return EventTargetWithInlineData::addEventListenerInternal(eventType,
                                                              listener, options);
-}
-
-void MediaStream::contextDestroyed() {
-  ContextLifecycleObserver::contextDestroyed();
-  m_stopped = true;
 }
 
 const AtomicString& MediaStream::interfaceName() const {
   return EventTargetNames::MediaStream;
 }
 
-ExecutionContext* MediaStream::getExecutionContext() const {
-  return ContextLifecycleObserver::getExecutionContext();
-}
-
 void MediaStream::addRemoteTrack(MediaStreamComponent* component) {
   DCHECK(component);
-  if (m_stopped)
+  if (m_executionContext->isContextDestroyed())
     return;
 
   MediaStreamTrack* track =
-      MediaStreamTrack::create(getExecutionContext(), component);
+      MediaStreamTrack::create(m_executionContext, component);
   switch (component->source()->type()) {
     case MediaStreamSource::TypeAudio:
       m_audioTracks.append(track);
@@ -362,7 +351,7 @@ void MediaStream::addRemoteTrack(MediaStreamComponent* component) {
 
 void MediaStream::removeRemoteTrack(MediaStreamComponent* component) {
   DCHECK(component);
-  if (m_stopped)
+  if (m_executionContext->isContextDestroyed())
     return;
 
   MediaStreamTrackVector* tracks = 0;
@@ -407,7 +396,7 @@ void MediaStream::scheduleDispatchEvent(Event* event) {
 }
 
 void MediaStream::scheduledEventTimerFired(TimerBase*) {
-  if (m_stopped)
+  if (m_executionContext->isContextDestroyed())
     return;
 
   HeapVector<Member<Event>> events;
@@ -429,8 +418,8 @@ DEFINE_TRACE(MediaStream) {
   visitor->trace(m_videoTracks);
   visitor->trace(m_descriptor);
   visitor->trace(m_scheduledEvents);
+  visitor->trace(m_executionContext);
   EventTargetWithInlineData::trace(visitor);
-  ContextLifecycleObserver::trace(visitor);
   MediaStreamDescriptorClient::trace(visitor);
 }
 
--- third_party/WebKit/Source/modules/mediastream/MediaStream.h
+++ third_party/WebKit/Source/modules/mediastream/MediaStream.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MediaStream_h
 #define MediaStream_h
 
-#include "core/dom/ContextLifecycleObserver.h"
+#include "core/dom/ExecutionContext.h"
 #include "core/html/URLRegistry.h"
 #include "modules/EventTargetModules.h"
 #include "modules/ModulesExport.h"
@@ -41,8 +41,7 @@ class ExceptionState;
 
 class MODULES_EXPORT MediaStream final : public EventTargetWithInlineData,
                                          public URLRegistrable,
-                                         public MediaStreamDescriptorClient,
-                                         public ContextLifecycleObserver {
+                                         public MediaStreamDescriptorClient {
   USING_GARBAGE_COLLECTED_MIXIN(MediaStream);
   DEFINE_WRAPPERTYPEINFO();
 
@@ -80,7 +79,9 @@ class MODULES_EXPORT MediaStream final : public EventTargetWithInlineData,
 
   // EventTarget
   const AtomicString& interfaceName() const override;
-  ExecutionContext* getExecutionContext() const override;
+  ExecutionContext* getExecutionContext() const override {
+    return m_executionContext;
+  }
 
   // URLRegistrable
   URLRegistry& registry() const override;
@@ -99,9 +100,6 @@ class MODULES_EXPORT MediaStream final : public EventTargetWithInlineData,
               const MediaStreamTrackVector& audioTracks,
               const MediaStreamTrackVector& videoTracks);
 
-  // ContextLifecycleObserver
-  void contextDestroyed() override;
-
   // MediaStreamDescriptorClient
   void addRemoteTrack(MediaStreamComponent*) override;
   void removeRemoteTrack(MediaStreamComponent*) override;
@@ -111,14 +109,13 @@ class MODULES_EXPORT MediaStream final : public EventTargetWithInlineData,
   void scheduleDispatchEvent(Event*);
   void scheduledEventTimerFired(TimerBase*);
 
-  bool m_stopped;
-
   MediaStreamTrackVector m_audioTracks;
   MediaStreamTrackVector m_videoTracks;
   Member<MediaStreamDescriptor> m_descriptor;
 
   Timer<MediaStream> m_scheduledEventTimer;
   HeapVector<Member<Event>> m_scheduledEvents;
+  Member<ExecutionContext> m_executionContext;
 };
 
 typedef HeapVector<Member<MediaStream>> MediaStreamVector;
