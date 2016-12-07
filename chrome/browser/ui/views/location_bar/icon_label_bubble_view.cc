7d5a3013d9c56c08f29623f456abe92708262a94
patricialor <patricialor@chromium.org>
2016-12-06 18:04:29 -0800

    Views: Add a11y information for IconLabelBubbleView and LocationIconView.
    IconLabelBubbleView is a View for displaying an icon + label; LocationIconView
    specifically is the icon + text shown on the left on the Omnibox URL (typically
    when a site is deemed to be secure). Neither of these classes have any
    accessibility information set on them, despite LocationIconView being a
    focusable and clickable target. This change adds a11y information to both.
    
    BUG=668930
    
    Review-Url: https://codereview.chromium.org/2530353002
    Cr-Commit-Position: refs/heads/master@{#436832}

--- chrome/browser/ui/views/location_bar/icon_label_bubble_view.cc
+++ chrome/browser/ui/views/location_bar/icon_label_bubble_view.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/location_bar/icon_label_bubble_view.h"
 
 #include "chrome/browser/ui/layout_constants.h"
+#include "ui/accessibility/ax_node_data.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/color_utils.h"
 #include "ui/gfx/scoped_canvas.h"
@@ -129,6 +130,10 @@ void IconLabelBubbleView::Layout() {
   const int label_width =
       std::max(0, width() - label_x - bubble_trailing_padding);
   label_->SetBounds(label_x, 0, label_width, height());
+}
+
+void IconLabelBubbleView::GetAccessibleNodeData(ui::AXNodeData* node_data) {
+  label_->GetAccessibleNodeData(node_data);
 }
 
 void IconLabelBubbleView::OnNativeThemeChanged(
