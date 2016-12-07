51b6320f9741ff67f27d9690d01512ac67abc62e
kojii <kojii@chromium.org>
2016-12-07 08:40:27 -0800

    Prioritize hyphenations over break-word/break-all
    When these mid-word break properties are used together, this patch
    tries to hyphenate first.
    
    This behavior matches to WebKit, and to Gecko/Edge for break-word. The
    priority isn't defined in the spec, currently an open issue[1]. But for
    break-word, 4 browsers are interoperable with this change.
    
    Also fixes one of failing cases in fast/text/basic/015.html that is
    related with additionalWidthFromAncestors when break-word.
    
    [1] https://github.com/w3c/csswg-drafts/issues/791
    
    BUG=671341
    
    Review-Url: https://codereview.chromium.org/2560453002
    Cr-Commit-Position: refs/heads/master@{#436979}

--- third_party/WebKit/Source/core/layout/line/BreakingContextInlineHeaders.h
+++ third_party/WebKit/Source/core/layout/line/BreakingContextInlineHeaders.h
@@ -1114,17 +1114,8 @@ inline bool BreakingContext::handleText(WordMeasurements& wordMeasurements,
 
     midWordBreak = false;
     if (!m_width.fitsOnLine()) {
-      if (canBreakMidWord) {
-        m_width.addUncommittedWidth(-wordMeasurement.width);
-        if (rewindToMidWordBreak(layoutText, style, font, breakAll,
-                                 wordMeasurement)) {
-          lastWidthMeasurement = wordMeasurement.width + lastSpaceWordSpacing;
-          midWordBreak = true;
-        }
-        m_width.addUncommittedWidth(wordMeasurement.width);
-      } else if (hyphenation &&
-                 (m_nextObject || isLineEmpty ||
-                  hasVisibleText(layoutText, m_current.offset()))) {
+      if (hyphenation && (m_nextObject || isLineEmpty ||
+                          hasVisibleText(layoutText, m_current.offset()))) {
         m_width.addUncommittedWidth(-wordMeasurement.width);
         DCHECK(lastSpace == static_cast<unsigned>(wordMeasurement.startOffset));
         DCHECK(m_current.offset() ==
@@ -1135,6 +1126,15 @@ inline bool BreakingContext::handleText(WordMeasurements& wordMeasurements,
           hyphenated = true;
           m_atEnd = true;
           return false;
+        }
+        m_width.addUncommittedWidth(wordMeasurement.width);
+      }
+      if (canBreakMidWord) {
+        m_width.addUncommittedWidth(-wordMeasurement.width);
+        if (rewindToMidWordBreak(layoutText, style, font, breakAll,
+                                 wordMeasurement)) {
+          lastWidthMeasurement = wordMeasurement.width + lastSpaceWordSpacing;
+          midWordBreak = true;
         }
         m_width.addUncommittedWidth(wordMeasurement.width);
       }
@@ -1228,7 +1228,6 @@ inline bool BreakingContext::handleText(WordMeasurements& wordMeasurements,
   float lastWidthMeasurement = 0;
   wordMeasurement.startOffset = lastSpace;
   wordMeasurement.endOffset = m_current.offset();
-  midWordBreak = false;
   if (!m_ignoringSpaces) {
     lastWidthMeasurement =
         textWidth(layoutText, lastSpace, m_current.offset() - lastSpace, font,
@@ -1237,13 +1236,6 @@ inline bool BreakingContext::handleText(WordMeasurements& wordMeasurements,
     wordMeasurement.width =
         lastWidthMeasurement + wordSpacingForWordMeasurement;
     wordMeasurement.glyphBounds.move(wordSpacingForWordMeasurement, 0);
-
-    if (canBreakMidWord && !m_width.fitsOnLine(lastWidthMeasurement) &&
-        rewindToMidWordBreak(layoutText, style, font, breakAll,
-                             wordMeasurement)) {
-      lastWidthMeasurement = wordMeasurement.width;
-      midWordBreak = true;
-    }
   }
   lastWidthMeasurement += lastSpaceWordSpacing;
 
@@ -1260,15 +1252,12 @@ inline bool BreakingContext::handleText(WordMeasurements& wordMeasurements,
 
   m_includeEndWidth = false;
 
-  if (midWordBreak) {
-    m_width.commit();
-    m_atEnd = true;
-  } else if (!m_width.fitsOnLine()) {
+  if (!m_ignoringSpaces && !m_width.fitsOnLine()) {
     if (hyphenation && (m_nextObject || isLineEmpty)) {
       m_width.addUncommittedWidth(-wordMeasurement.width);
-      DCHECK(lastSpace == static_cast<unsigned>(wordMeasurement.startOffset));
-      DCHECK(m_current.offset() ==
-             static_cast<unsigned>(wordMeasurement.endOffset));
+      DCHECK_EQ(lastSpace, static_cast<unsigned>(wordMeasurement.startOffset));
+      DCHECK_EQ(m_current.offset(),
+                static_cast<unsigned>(wordMeasurement.endOffset));
       if (hyphenate(layoutText, style, font, *hyphenation, lastSpaceWordSpacing,
                     wordMeasurement)) {
         hyphenated = true;
@@ -1279,9 +1268,19 @@ inline bool BreakingContext::handleText(WordMeasurements& wordMeasurements,
     if (!hyphenated && isBreakAtSoftHyphen() && !disableSoftHyphen) {
       hyphenated = true;
       m_atEnd = true;
-    } else if (!m_ignoringSpaces && canBreakMidWord &&
-               m_width.committedWidth()) {
-      m_atEnd = true;
+    }
+    if (!hyphenated && canBreakMidWord) {
+      m_width.addUncommittedWidth(-wordMeasurement.width);
+      if (rewindToMidWordBreak(layoutText, style, font, breakAll,
+                               wordMeasurement)) {
+        m_width.addUncommittedWidth(wordMeasurement.width);
+        m_width.commit();
+        m_atEnd = true;
+      } else {
+        m_width.addUncommittedWidth(wordMeasurement.width);
+        if (m_width.committedWidth())
+          m_atEnd = true;
+      }
     }
   }
   return false;
