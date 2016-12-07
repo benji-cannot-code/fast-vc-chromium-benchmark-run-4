8d8e97601742d86ada10d35170d01bcbfbbc96de
estade <estade@chromium.org>
2016-12-07 15:24:55 -0800

    Fix NativeThemeMacTest
    not sure why this isn't covered by trybots.
    
    BUG=672267
    TBR=sky@chromium.org
    NOTRY=true
    NOTREECHECKS=true
    
    Review-Url: https://codereview.chromium.org/2555363002
    Cr-Commit-Position: refs/heads/master@{#437106}

--- ui/native_theme/native_theme_mac.h
+++ ui/native_theme/native_theme_mac.h
@@ -69,6 +69,7 @@ class NATIVE_THEME_EXPORT NativeThemeMac : public NativeThemeBase {
 
  protected:
   friend class NativeTheme;
+  friend class NativeThemeMacTest;
   static NativeThemeMac* instance();
 
  private:
