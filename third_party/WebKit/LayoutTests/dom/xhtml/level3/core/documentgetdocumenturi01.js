8e1a0b0e0295629119ec50ff7357d021cd9626e3
qyearsley <qyearsley@chromium.org>
2016-12-07 12:12:30 -0800

    Run spellchecker on LayoutTests/dom/.
    Review-Url: https://codereview.chromium.org/2552343002
    Cr-Commit-Position: refs/heads/master@{#437044}

--- third_party/WebKit/LayoutTests/dom/xhtml/level3/core/documentgetdocumenturi01.js
+++ third_party/WebKit/LayoutTests/dom/xhtml/level3/core/documentgetdocumenturi01.js
@@ -70,7 +70,7 @@ function loadComplete() {
 
 /**
 *
-    Retreive the documentURI of this document, and verify if it is not null.
+    Retrieve the documentURI of this document, and verify if it is not null.
 
 * @author IBM
 * @author Neil Delima
--- third_party/WebKit/LayoutTests/dom/xhtml/level3/core/nodeisequalnode10.js
+++ third_party/WebKit/LayoutTests/dom/xhtml/level3/core/nodeisequalnode10.js
@@ -70,7 +70,7 @@ function loadComplete() {
 
 /**
 *
-    Retreive 2 different "em" nodes of this Document   Use isEqualNode
+    Retrieve 2 different "em" nodes of this Document   Use isEqualNode
     check if nodes are not equal.
 
 * @author IBM
