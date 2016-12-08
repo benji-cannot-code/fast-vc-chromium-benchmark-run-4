2ef536cc256a4361989b1f56bb91f59450cfc8de
scottmg <scottmg@chromium.org>
2016-12-08 09:44:02 -0800

    Remove more unnecessary includes, limits.h and climits in base/numerics
    BUG=668713
    R=jschuh@chromium.org
    
    Review-Url: https://codereview.chromium.org/2558993002
    Cr-Commit-Position: refs/heads/master@{#437276}

--- base/numerics/safe_conversions_impl.h
+++ base/numerics/safe_conversions_impl.h
@@ -6,10 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_NUMERICS_SAFE_CONVERSIONS_IMPL_H_
 #define BASE_NUMERICS_SAFE_CONVERSIONS_IMPL_H_
 
-#include <limits.h>
 #include <stdint.h>
 
-#include <climits>
 #include <limits>
 #include <type_traits>
 
