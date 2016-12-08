f07481d3f40db099335bfb38cc97830cbeec024e
ellyjones <ellyjones@chromium.org>
2016-12-08 05:59:25 -0800

    Revert of Add a dead zone when dragging tabs horizontally on Mac (patchset #2 id:20001 of https://codereview.chromium.org/2541653002/ )
    Reason for revert:
    Likely introduced https://crbug.com/672427. Quoting from that bug:
    
    """
    I believe this is caused by https://codereview.chromium.org/2541653002/:
    
    In [TabStripDragController continueDrag:], tabWasDragged_ is still set to YES even if the tab drag is not done because of the newly-introduced horizontal dead zone. Then, in [TabStripDragController endDrag:], the expected target placeholder tab is missing, but tabWasDragged_ is still YES, so [dropController moveTabViews:fromController:] causes a fatality.
    """
    
    Original issue's description:
    > Add a dead zone when dragging tabs horizontally on Mac
    >
    > When trying to drag tabs vertically out of their windows, they don't drag until the cursor has been moved a certain distance away from the mouse down point. Adding the same behaviour when moving tabs horizontally.
    >
    > BUG=90526
    > TEST=Create multiple tabs, slowly drag one of them directly to the left or right. It won't actually start moving until it's torn out of its dead zone.
    >
    > Committed: https://crrev.com/b74d1db1e9f18844ac6929d6ca7116ef00f0379f
    > Cr-Commit-Position: refs/heads/master@{#437058}
    
    TBR=avi@chromium.org,erikchen@chromium.org,njagsarran@google.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=90526
    
    Review-Url: https://codereview.chromium.org/2557343002
    Cr-Commit-Position: refs/heads/master@{#437237}

--- chrome/browser/ui/cocoa/tabs/tab_strip_drag_controller.mm
+++ chrome/browser/ui/cocoa/tabs/tab_strip_drag_controller.mm
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/cocoa/cocoa_base_utils.h"
 #include "ui/gfx/mac/scoped_cocoa_disable_screen_updates.h"
 
-const CGFloat kHorizontalTearDistance = 10.0; // Using the same value as Views.
 const CGFloat kTearDistance = 36.0;
 const NSTimeInterval kTearDuration = 0.333;
 
@@ -173,17 +172,9 @@ static BOOL PointIsInsideView(NSPoint screenPoint, NSView* view) {
   if (draggingWithinTabStrip_) {
     NSPoint thisPoint = [NSEvent mouseLocation];
     CGFloat offset = thisPoint.x - dragOrigin_.x;
-    // Only begin dragging the tab horizontally if it's torn out of its dead
-    // zone.
-    if (!outOfTabHorizontalDeadZone_ &&
-        fabs(offset) > kHorizontalTearDistance) {
-      outOfTabHorizontalDeadZone_ = YES;
-    }
-    if (outOfTabHorizontalDeadZone_) {
-      [sourceController_ insertPlaceholderForTab:[draggedTab_ tabView]
-                                           frame:NSOffsetRect(sourceTabFrame_,
-                                                              offset, 0)];
-    }
+    [sourceController_ insertPlaceholderForTab:[draggedTab_ tabView]
+                                         frame:NSOffsetRect(sourceTabFrame_,
+                                                            offset, 0)];
     // Check that we haven't pulled the tab too far to start a drag. This
     // can include either pulling it too far down, or off the side of the tab
     // strip that would cause it to no longer be fully visible.
@@ -413,7 +404,6 @@ static BOOL PointIsInsideView(NSPoint screenPoint, NSView* view) {
 - (void)endDrag:(NSEvent*)event {
   // Cancel any delayed -continueDrag: requests that may still be pending.
   [NSObject cancelPreviousPerformRequestsWithTarget:self];
-  outOfTabHorizontalDeadZone_ = NO;
 
   // Special-case this to keep the logic below simpler.
   if (moveWindowOnDrag_) {
