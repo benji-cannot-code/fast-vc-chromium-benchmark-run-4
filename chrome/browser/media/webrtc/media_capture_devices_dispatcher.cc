f80f96ebbb32eef82509b8d56945ba2124ca392d
isandrk <isandrk@chromium.org>
2016-12-08 07:03:24 -0800

    Public Sessions - prompt the user for audioCapture/videoCapture requests
    In Public Sessions extensions are force installed by admin policy and some of the permissions are granted at install time so the end user has no say on it. audioCapture and videoCapture APIs are dangerous from a security/privacy standpoint therefore we want to give the user a runtime prompt whether to allow such API.
    
    Only platform apps (and some built-in whitelisted extensions) support audioCapture[1]/videoCapture[2] manifest permission features therefore this filtering will be limited to platform apps.
    
    [1]: https://cs.chromium.org/chromium/src/extensions/common/api/_permission_features.json?rcl=0&l=102
    [2]: https://cs.chromium.org/chromium/src/extensions/common/api/_permission_features.json?rcl=0&l=491
    
    BUG=669521
    
    Review-Url: https://codereview.chromium.org/2532323003
    Cr-Commit-Position: refs/heads/master@{#437243}

--- chrome/browser/media/webrtc/media_capture_devices_dispatcher.cc
+++ chrome/browser/media/webrtc/media_capture_devices_dispatcher.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_CHROMEOS)
 #include "ash/shell.h"
+#include "chrome/browser/media/public_session_media_access_handler.h"
 #endif  // defined(OS_CHROMEOS)
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
@@ -85,7 +86,12 @@ MediaCaptureDevicesDispatcher::MediaCaptureDevicesDispatcher()
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
+#if defined(OS_CHROMEOS)
+  // Wrapper around ExtensionMediaAccessHandler used in Public Sessions.
+  media_access_handlers_.push_back(new PublicSessionMediaAccessHandler());
+#else
   media_access_handlers_.push_back(new ExtensionMediaAccessHandler());
+#endif
   media_access_handlers_.push_back(new DesktopCaptureAccessHandler());
   media_access_handlers_.push_back(new TabCaptureAccessHandler());
 #endif
