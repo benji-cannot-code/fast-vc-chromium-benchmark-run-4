c8f002e8cd8a959f476883b442a16c80fca37461
kmackay <kmackay@chromium.org>
2016-12-08 14:54:02 -0800

    [Chromecast] Log the audio config when FFmpegAudioDecoder fails to init
    BUG= internal b/32755903
    
    Review-Url: https://codereview.chromium.org/2558353002
    Cr-Commit-Position: refs/heads/master@{#437384}

---
+++ chromecast/media/cma/base/decoder_config_logging.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMECAST_MEDIA_CMA_BASE_DECODER_CONFIG_LOGGING_H_
+#define CHROMECAST_MEDIA_CMA_BASE_DECODER_CONFIG_LOGGING_H_
+
+#include <ostream>
+
+#include "chromecast/public/media/decoder_config.h"
+
+std::ostream& operator<<(std::ostream& stream,
+                         ::chromecast::media::AudioCodec codec);
+std::ostream& operator<<(std::ostream& stream,
+                         ::chromecast::media::SampleFormat format);
+
+#endif  // CHROMECAST_MEDIA_CMA_BASE_DECODER_CONFIG_LOGGING_H_
--- chromecast/media/cma/decoder/cast_audio_decoder_linux.cc
+++ chromecast/media/cma/decoder/cast_audio_decoder_linux.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/media/cma/base/decoder_buffer_adapter.h"
 #include "chromecast/media/cma/base/decoder_buffer_base.h"
 #include "chromecast/media/cma/base/decoder_config_adapter.h"
+#include "chromecast/media/cma/base/decoder_config_logging.h"
 #include "media/base/audio_buffer.h"
 #include "media/base/audio_bus.h"
 #include "media/base/cdm_context.h"
@@ -124,14 +125,22 @@ class CastAudioDecoderImpl : public CastAudioDecoder {
 
   void OnInitialized(bool success) {
     DCHECK(!initialized_);
-    LOG_IF(ERROR, !success) << "Failed to initialize FFmpegAudioDecoder";
-    if (success)
+    if (success) {
       initialized_ = true;
-
-    if (success && !decode_queue_.empty()) {
-      const auto& d = decode_queue_.front();
-      DecodeNow(d.first, d.second);
-      decode_queue_.pop();
+      if (!decode_queue_.empty()) {
+        const auto& d = decode_queue_.front();
+        DecodeNow(d.first, d.second);
+        decode_queue_.pop();
+      }
+    } else {
+      LOG(ERROR) << "Failed to initialize FFmpegAudioDecoder";
+      LOG(INFO) << "Config:";
+      LOG(INFO) << "\tEncrypted: "
+                << (config_.is_encrypted() ? "true" : "false");
+      LOG(INFO) << "\tCodec: " << config_.codec;
+      LOG(INFO) << "\tSample format: " << config_.sample_format;
+      LOG(INFO) << "\tChannels: " << config_.channel_number;
+      LOG(INFO) << "\tSample rate: " << config_.samples_per_second;
     }
 
     if (!initialized_callback_.is_null())
