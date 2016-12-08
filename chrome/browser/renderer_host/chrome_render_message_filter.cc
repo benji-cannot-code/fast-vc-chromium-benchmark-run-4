028c856de197e4050936020ef29349618099a7c1
nigeltao <nigeltao@chromium.org>
2016-12-07 18:09:50 -0800

    Convert FieldTrialActivated to use mojo.
    Manually tested by printf'ing the arguments to FieldTrialActivated. The
    args looked the same as for ChromeRenderMessageFilter prior to this CL.
    
    BUG=577685
    
    Review-Url: https://codereview.chromium.org/2453313004
    Cr-Commit-Position: refs/heads/master@{#437142}

--- chrome/browser/renderer_host/chrome_render_message_filter.cc
+++ chrome/browser/renderer_host/chrome_render_message_filter.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "base/logging.h"
 #include "base/macros.h"
-#include "base/metrics/field_trial.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/content_settings/cookie_settings_factory.h"
@@ -88,8 +87,6 @@ bool ChromeRenderMessageFilter::OnMessageReceived(const IPC::Message& message) {
     IPC_MESSAGE_HANDLER(ChromeViewHostMsg_IsCrashReportingEnabled,
                         OnIsCrashReportingEnabled)
 #endif
-    IPC_MESSAGE_HANDLER(ChromeViewHostMsg_FieldTrialActivated,
-                        OnFieldTrialActivated)
     IPC_MESSAGE_UNHANDLED(handled = false)
   IPC_END_MESSAGE_MAP()
 
@@ -347,11 +344,3 @@ void ChromeRenderMessageFilter::OnIsCrashReportingEnabled(bool* enabled) {
   *enabled = ChromeMetricsServiceAccessor::IsMetricsAndCrashReportingEnabled();
 }
 #endif
-
-void ChromeRenderMessageFilter::OnFieldTrialActivated(
-    const std::string& trial_name) {
-  // Activate the trial in the browser process to match its state in the
-  // renderer. This is done by calling FindFullName which finalizes the group
-  // and activates the trial.
-  base::FieldTrialList::FindFullName(trial_name);
-}
