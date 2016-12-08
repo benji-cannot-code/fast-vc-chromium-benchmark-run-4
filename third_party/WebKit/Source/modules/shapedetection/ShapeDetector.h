dc517e6a0207a0d92cd8d5deb93d7fc83494679f
mcasas <mcasas@chromium.org>
2016-12-07 18:12:42 -0800

    ShapeDetection: add support for all CanvasImageSource input types
    Currently ShapeDetector::detectFacesOn{ImageBitmap,Videoelement}()
    extract the pixels out of the corresponding object; I haven't noticed
    that CanvasImageSource::getSourceImageForCanvas() is a virtual method,
    implemented for each CanvasImageSource, doing exactly what I needed,
    which is extracting an WTF::Image.
    
    getSourceImageForCanvas() implementations have the same checks
    being performed on ToT now, so swapping one for the other is
    not reducing the early-bail outs.
    
    So, this CL factors out the code to use getSourceImageForCanvas(),
    making the Shape Detection detect() work for all these different
    input types, and adds LayoutTests for them.
    
    BUG=670977, 659138
    
    Review-Url: https://codereview.chromium.org/2553343003
    Cr-Commit-Position: refs/heads/master@{#437144}

--- third_party/WebKit/Source/modules/shapedetection/ShapeDetector.h
+++ third_party/WebKit/Source/modules/shapedetection/ShapeDetector.h
@@ -29,9 +29,6 @@ class MODULES_EXPORT ShapeDetector
  private:
   ScriptPromise detectShapesOnImageElement(ScriptPromiseResolver*,
                                            const HTMLImageElement*);
-  ScriptPromise detectShapesOnImageBitmap(ScriptPromiseResolver*, ImageBitmap*);
-  ScriptPromise detectShapesOnVideoElement(ScriptPromiseResolver*,
-                                           const HTMLVideoElement*);
 
   virtual ScriptPromise doDetect(ScriptPromiseResolver*,
                                  mojo::ScopedSharedBufferHandle,
