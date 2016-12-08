f6c9c64a39d3933a732ba9cfcc2b13efdd122aed
horo <horo@chromium.org>
2016-12-08 07:46:33 -0800

    Introduce ServiceWorker.MainFramePageLoad.CoreTransition and RedirectChainLength UMAs.
    The result of the experiment of Speculative launch Service Workers was not so good as we thought.
    We think this is because:
     - Low percentage of navigations by link click
     - High percentage of navigations with redirects
    
    To confirm this, I'd like to introduce 2 UMAs.
     - ServiceWorker.MainFramePageLoad.CoreTransition
     - ServiceWorker.MainFramePageLoad.RedirectChainLength
    
    BUG=616502
    
    Review-Url: https://codereview.chromium.org/2551013003
    Cr-Commit-Position: refs/heads/master@{#437250}

--- content/browser/service_worker/service_worker_controllee_request_handler.cc
+++ content/browser/service_worker/service_worker_controllee_request_handler.cc
@@ -20,11 +20,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/service_worker/service_worker_types.h"
 #include "content/common/service_worker/service_worker_utils.h"
 #include "content/public/browser/content_browser_client.h"
+#include "content/public/browser/resource_request_info.h"
 #include "content/public/common/content_client.h"
 #include "content/public/common/resource_response_info.h"
 #include "net/base/load_flags.h"
 #include "net/base/url_util.h"
 #include "net/url_request/url_request.h"
+#include "ui/base/page_transition_types.h"
 
 namespace content {
 
@@ -44,6 +46,14 @@ bool MaybeForwardToServiceWorker(ServiceWorkerURLRequestJob* job,
 
   job->FallbackToNetworkOrRenderer();
   return false;
+}
+
+ui::PageTransition GetPageTransition(net::URLRequest* request) {
+  const ResourceRequestInfo* info = ResourceRequestInfo::ForRequest(request);
+  // ResourceRequestInfo may not be set in some tests.
+  if (!info)
+    return ui::PAGE_TRANSITION_LINK;
+  return info->GetPageTransition();
 }
 
 }  // namespace
@@ -286,9 +296,9 @@ void ServiceWorkerControlleeRequestHandler::
 
   DCHECK_NE(active_version->fetch_handler_existence(),
             ServiceWorkerVersion::FetchHandlerExistence::UNKNOWN);
-
   ServiceWorkerMetrics::CountControlledPageLoad(
-      active_version->site_for_uma(), stripped_url_, is_main_frame_load_);
+      active_version->site_for_uma(), stripped_url_, is_main_frame_load_,
+      GetPageTransition(job_->request()), job_->request()->url_chain().size());
 
   bool is_forwarded =
       MaybeForwardToServiceWorker(job_.get(), active_version.get());
@@ -320,7 +330,8 @@ void ServiceWorkerControlleeRequestHandler::OnVersionStatusChanged(
   DCHECK_NE(version->fetch_handler_existence(),
             ServiceWorkerVersion::FetchHandlerExistence::UNKNOWN);
   ServiceWorkerMetrics::CountControlledPageLoad(
-      version->site_for_uma(), stripped_url_, is_main_frame_load_);
+      version->site_for_uma(), stripped_url_, is_main_frame_load_,
+      GetPageTransition(job_->request()), job_->request()->url_chain().size());
 
   provider_host_->AssociateRegistration(registration,
                                         false /* notify_controllerchange */);
--- content/browser/service_worker/service_worker_metrics.h
+++ content/browser/service_worker/service_worker_metrics.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/service_worker/service_worker_database.h"
 #include "content/common/service_worker/service_worker_types.h"
 #include "third_party/WebKit/public/platform/modules/serviceworker/WebServiceWorkerResponseError.h"
+#include "ui/base/page_transition_types.h"
 
 class GURL;
 
@@ -174,7 +175,9 @@ class ServiceWorkerMetrics {
   // Counts the number of page loads controlled by a Service Worker.
   static void CountControlledPageLoad(Site site,
                                       const GURL& url,
-                                      bool is_main_frame_load);
+                                      bool is_main_frame_load,
+                                      ui::PageTransition page_transition,
+                                      size_t redirect_chain_length);
 
   // Records the result of trying to start a worker. |is_installed| indicates
   // whether the version has been installed.
