2bd3b387f255967132ecd57ba2e03b7ae4575380
erikchen <erikchen@chromium.org>
2016-12-08 12:58:04 -0800

    mac: Add tracing for -[BrowserCrApplication sendEvent:].
    This captures work triggered from NSEvents for tracing.
    
    BUG=669116
    
    Review-Url: https://codereview.chromium.org/2566443002
    Cr-Commit-Position: refs/heads/master@{#437334}

--- chrome/browser/chrome_browser_application_mac.mm
+++ chrome/browser/chrome_browser_application_mac.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/call_with_eh_frame.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/sys_string_conversions.h"
+#include "base/trace_event/trace_event.h"
 #import "chrome/browser/app_controller_mac.h"
 #import "chrome/browser/mac/exception_processor.h"
 #include "chrome/common/chrome_switches.h"
@@ -254,6 +255,7 @@ void CancelTerminate() {
 }
 
 - (void)sendEvent:(NSEvent*)event {
+  TRACE_EVENT0("toplevel", "BrowserCrApplication::sendEvent");
   base::debug::ScopedCrashKey crash_key(
       crash_keys::mac::kNSEvent, base::SysNSStringToUTF8([event description]));
 
