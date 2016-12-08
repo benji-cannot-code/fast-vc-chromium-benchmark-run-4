eddeea7947987447347e2dc14e0dfda7eaf65e0d
mfomitchev <mfomitchev@chromium.org>
2016-12-08 13:36:19 -0800

    Revert of Making WTH::InitCompositor() protected instead of public. (patchset #5 id:70001 of https://codereview.chromium.org/2556803002/ )
    Reason for revert:
    Reverting because https://codereview.chromium.org/2552003002/ needs to be reverted
    
    Original issue's description:
    > Making WTH::InitCompositor() protected instead of public.
    >
    > BUG=NONE
    >
    > Committed: https://crrev.com/98f0f335c82dbbd932d3d5bad9fbb22585dafb65
    > Cr-Commit-Position: refs/heads/master@{#437292}
    
    TBR=sky@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=NONE
    
    Review-Url: https://codereview.chromium.org/2565563003
    Cr-Commit-Position: refs/heads/master@{#437352}

--- ui/aura/window_tree_host.h
+++ ui/aura/window_tree_host.h
@@ -61,6 +61,8 @@ class AURA_EXPORT WindowTreeHost : public ui::internal::InputMethodDelegate,
 
   void InitHost();
 
+  void InitCompositor();
+
   void AddObserver(WindowTreeHostObserver* observer);
   void RemoveObserver(WindowTreeHostObserver* observer);
 
@@ -192,7 +194,6 @@ class AURA_EXPORT WindowTreeHost : public ui::internal::InputMethodDelegate,
   void DestroyDispatcher();
 
   void CreateCompositor();
-  void InitCompositor();
   void OnAcceleratedWidgetAvailable();
 
   // Returns the location of the RootWindow on native screen.
