b25258ce3d3580868fccad09769c59dee68d6672
rouslan <rouslan@chromium.org>
2016-12-08 13:28:31 -0800

    Show additional display items when instruments change.
    If the user selects a payment instrument that matches a modifier, then
    the contents of the shopping cart should show the additional display
    items from the modifier. For example, if the merchant provides
    discounts for "visa", then selecting a "visa" type card should show the
    "VISA discount $5.00" line item in the shopping cart.
    
    This is accomplished by keeping track of the full modifier, instead of
    just the total. This will also be useful in upcoming patch where the
    modifier contents will be sent to the payment app.
    
    This feature is behind chrome://flags#web-payments-modifiers flag.
    
    This patch additionally removes the no longer used variable
    "totalCurrency".
    
    BUG=660926
    
    Review-Url: https://codereview.chromium.org/2549313002
    Cr-Commit-Position: refs/heads/master@{#437347}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/ui/ShoppingCart.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ui/ShoppingCart.java
@@ -5,14 +5,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.payments.ui;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import javax.annotation.Nullable;
 
 /**
  * The shopping cart contents and total.
  */
 public class ShoppingCart {
     private LineItem mTotal;
-    private List<LineItem> mContents;
+    @Nullable private List<LineItem> mContents;
+    @Nullable private List<LineItem> mAdditionalContents;
 
     /**
      * Builds the shopping cart UI data model.
@@ -20,7 +25,7 @@ public class ShoppingCart {
      * @param totalPrice The total price.
      * @param contents The shopping cart contents. The breakdown of the total price. OK to be null.
      */
-    public ShoppingCart(LineItem totalPrice, List<LineItem> contents) {
+    public ShoppingCart(LineItem totalPrice, @Nullable List<LineItem> contents) {
         mTotal = totalPrice;
         mContents = contents;
     }
@@ -44,12 +49,19 @@ public class ShoppingCart {
     }
 
     /**
-     * Returns the shopping cart items.
+     * Returns the shopping cart items, including both the original items and the additional items
+     * that vary depending on the selected payment instrument, e.g., debut card discounts.
      *
-     * @return The shopping cart items. Can be null.
+     * @return The shopping cart items. Can be null. Should not be modified.
      */
-    public List<LineItem> getContents() {
-        return mContents;
+    @Nullable public List<LineItem> getContents() {
+        if (mContents == null && mAdditionalContents == null) return null;
+
+        List<LineItem> result = new ArrayList<>();
+        if (mContents != null) result.addAll(mContents);
+        if (mAdditionalContents != null) result.addAll(mAdditionalContents);
+
+        return Collections.unmodifiableList(result);
     }
 
     /**
@@ -57,7 +69,17 @@ public class ShoppingCart {
      *
      * @param contents The shopping cart items. Can be null.
      */
-    public void setContents(List<LineItem> contents) {
+    public void setContents(@Nullable List<LineItem> contents) {
         mContents = contents;
+    }
+
+    /**
+     * Update the additional shopping cart items that vary depending on the selected payment
+     * instrument, e.g., debit card discounts.
+     *
+     * @param additionalContents The additional shopping cart items. Can be null.
+     */
+    public void setAdditionalContents(@Nullable List<LineItem> additionalContents) {
+        mAdditionalContents = additionalContents;
     }
 }
