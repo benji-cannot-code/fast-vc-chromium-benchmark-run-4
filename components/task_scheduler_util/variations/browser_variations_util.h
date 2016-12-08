3849ff0279497ebf1cddedb609bbb3f13d426daa
robliao <robliao@chromium.org>
2016-12-07 17:26:15 -0800

    Split initialization_util into a Hermetic Library and a Variations Library
    This split allows callers to easily use some of the task_scheduler_init
    structures without taking a dependency on variations.
    
    Also adds unit tests for some of the functions.
    
    BUG=662052
    
    TBR=asvitkine
    
    Review-Url: https://codereview.chromium.org/2553953002
    Cr-Commit-Position: refs/heads/master@{#437113}

---
+++ components/task_scheduler_util/variations/browser_variations_util.h
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_TASK_SCHEDULER_UTIL_VARIATIONS_BROWSER_VARIATIONS_UTIL_H_
+#define COMPONENTS_TASK_SCHEDULER_UTIL_VARIATIONS_BROWSER_VARIATIONS_UTIL_H_
+
+#include <vector>
+
+#include "base/task_scheduler/scheduler_worker_pool_params.h"
+
+namespace task_scheduler_util {
+namespace variations {
+
+// Gets the browser SchedulerWorkerPoolParams vector based off of variations.
+// Returns an empty vector on failure.
+std::vector<base::SchedulerWorkerPoolParams>
+GetBrowserSchedulerWorkerPoolParamsFromVariations();
+
+// Redirects zero-to-many PostTask APIs to the browser task scheduler based off
+// of input from the variations parameters.
+void MaybePerformBrowserTaskSchedulerRedirection();
+
+}  // variations
+}  // namespace task_scheduler_util
+
+#endif  // COMPONENTS_TASK_SCHEDULER_UTIL_VARIATIONS_BROWSER_VARIATIONS_UTIL_H_
