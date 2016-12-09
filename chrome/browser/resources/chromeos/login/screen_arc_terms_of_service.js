9ccc59793accf11bd48db2f720f88f77069a3848
khmel <khmel@chromium.org>
2016-12-08 17:59:31 -0800

    arc: Change default button to 'Agree' for Arc OOBE OptIn page.
    There is new requirement for buttons in Arc OOBE OptIn page. Skip and
    Agree buttons must be grayed until ToS is loaded. Once ToS is loaded
    both are enabled and 'Agree' button has the focus. This also moves
    focus to 'Retry' button in case of ToS loading failure.
    
    TEST=Manually on device.
    BUG=672145
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2565573002
    Cr-Commit-Position: refs/heads/master@{#437427}

--- chrome/browser/resources/chromeos/login/screen_arc_terms_of_service.js
+++ chrome/browser/resources/chromeos/login/screen_arc_terms_of_service.js
@@ -152,6 +152,8 @@ login.createScreen('ArcTermsOfServiceScreen', 'arc-tos',
 
       var skipButton = this.ownerDocument.createElement('button');
       skipButton.id = 'arc-tos-skip-button';
+      skipButton.disabled = this.classList.contains('arc-tos-loading');
+      skipButton.classList.add('preserve-disabled-state');
       skipButton.textContent =
           loadTimeData.getString('arcTermsOfServiceSkipButton');
       skipButton.addEventListener('click', function(event) {
@@ -234,6 +236,9 @@ login.createScreen('ArcTermsOfServiceScreen', 'arc-tos',
       this.classList.remove('arc-tos-loaded');
       this.classList.remove('error');
       this.classList.add('arc-tos-loading');
+
+      $('arc-tos-accept-button').disabled = true;
+      $('arc-tos-skip-button').disabled = true;
     },
 
     /**
@@ -251,10 +256,9 @@ login.createScreen('ArcTermsOfServiceScreen', 'arc-tos',
       var acceptButton = $('arc-tos-accept-button');
       var skipButton = $('arc-tos-skip-button');
 
+      skipButton.disabled = false;
       acceptButton.disabled = false;
-      if (document.activeElement != skipButton) {
-        acceptButton.focus();
-      }
+      acceptButton.focus();
 
       var termsView = $('arc-tos-view');
       var termsViewContainer = $('arc-tos-view-container');
@@ -277,6 +281,8 @@ login.createScreen('ArcTermsOfServiceScreen', 'arc-tos',
       this.classList.remove('arc-tos-loading');
       this.classList.remove('arc-tos-loaded');
       this.classList.add('error');
+
+      $('arc-tos-retry-button').focus();
     },
 
     /**
