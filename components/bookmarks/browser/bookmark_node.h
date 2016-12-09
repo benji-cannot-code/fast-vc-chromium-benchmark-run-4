05cd4f8ad1fc699ebaafab74fa39c3efed28b24f
mattreynolds <mattreynolds@chromium.org>
2016-12-09 15:07:29 -0800

    Add TitledUrlNode interface and refactor BookmarkIndex to use it
    As the first step in making BookmarkIndex usable on non-bookmark items, add
    a TitledUrlNode interface to represent objects like bookmarks that expose a
    title and URL. BookmarkIndex now expects its items to be TitledUrlNodes and
    BookmarkNode implements the TitledUrlNode interface.
    
    BUG=630769
    
    Review-Url: https://codereview.chromium.org/2537223008
    Cr-Commit-Position: refs/heads/master@{#437680}

--- components/bookmarks/browser/bookmark_node.h
+++ components/bookmarks/browser/bookmark_node.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/task/cancelable_task_tracker.h"
 #include "base/time/time.h"
+#include "components/bookmarks/browser/titled_url_node.h"
 #include "components/favicon_base/favicon_types.h"
 #include "ui/base/models/tree_node_model.h"
 #include "ui/gfx/image/image.h"
@@ -26,7 +27,7 @@ class BookmarkModel;
 
 // BookmarkNode contains information about a starred entry: title, URL, favicon,
 // id and type. BookmarkNodes are returned from BookmarkModel.
-class BookmarkNode : public ui::TreeNode<BookmarkNode> {
+class BookmarkNode : public ui::TreeNode<BookmarkNode>, public TitledUrlNode {
  public:
   enum Type {
     URL,
@@ -115,6 +116,10 @@ class BookmarkNode : public ui::TreeNode<BookmarkNode> {
     sync_transaction_version_ = sync_transaction_version;
   }
   int64_t sync_transaction_version() const { return sync_transaction_version_; }
+
+  // TitledUrlNode interface methods.
+  const base::string16& GetTitledUrlNodeTitle() const override;
+  const GURL& GetTitledUrlNodeUrl() const override;
 
   // TODO(sky): Consider adding last visit time here, it'll greatly simplify
   // HistoryContentsProvider.
--- components/omnibox/browser/bookmark_provider.cc
+++ components/omnibox/browser/bookmark_provider.cc
@@ -25,9 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/url_constants.h"
 
 using bookmarks::BookmarkMatch;
+using BookmarkMatches = std::vector<BookmarkMatch>;
 using bookmarks::BookmarkNode;
-
-typedef std::vector<BookmarkMatch> BookmarkMatches;
 
 namespace {
 
@@ -166,11 +165,11 @@ AutocompleteMatch BookmarkProvider::BookmarkMatchToACMatch(
   // unlikely to be what the user intends.
   AutocompleteMatch match(this, 0, false,
                           AutocompleteMatchType::BOOKMARK_TITLE);
-  base::string16 title(bookmark_match.node->GetTitle());
+  base::string16 title(bookmark_match.node->GetTitledUrlNodeTitle());
   BookmarkMatch::MatchPositions new_title_match_positions =
       bookmark_match.title_match_positions;
   CorrectTitleAndMatchPositions(&title, &new_title_match_positions);
-  const GURL& url(bookmark_match.node->url());
+  const GURL& url(bookmark_match.node->GetTitledUrlNodeUrl());
   const base::string16& url_utf16 = base::UTF8ToUTF16(url.spec());
   size_t inline_autocomplete_offset = URLPrefix::GetInlineAutocompleteOffset(
       input.text(), fixed_up_input_text, false, url_utf16);
@@ -282,7 +281,8 @@ AutocompleteMatch BookmarkProvider::BookmarkMatchToACMatch(
   ScoringFunctor url_position_functor =
       for_each(bookmark_match.url_match_positions.begin(),
                bookmark_match.url_match_positions.end(),
-               ScoringFunctor(bookmark_match.node->url().spec().length()));
+               ScoringFunctor(
+                   bookmark_match.node->GetTitledUrlNodeUrl().spec().length()));
   const double title_match_strength = title_position_functor.ScoringFactor();
   const double summed_factors = title_match_strength +
       url_position_functor.ScoringFactor();
