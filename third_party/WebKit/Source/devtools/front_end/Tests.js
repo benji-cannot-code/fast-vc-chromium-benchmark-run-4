e7c399187b2fd660d589720b2791724d371c4ebc
dgozman <dgozman@chromium.org>
2016-12-09 00:13:21 -0800

    Allow temporary file system for incognito DevTools.
    It's an essential part of Timeline which should work in incognito mode.
    
    BUG=490148
    
    Review-Url: https://codereview.chromium.org/2556803003
    Cr-Commit-Position: refs/heads/master@{#437495}

--- third_party/WebKit/Source/devtools/front_end/Tests.js
+++ third_party/WebKit/Source/devtools/front_end/Tests.js
@@ -872,6 +872,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     Bindings.TempFile.ensureTempStorageCleared().then(() => this.releaseControl());
   };
 
+  TestSuite.prototype.testTempFile = function() {
+    this.takeControl();
+    Bindings.TempFile.create('test-file', 'test')
+        .then(() => this.releaseControl(), (error) => this.fail(String(error)));
+  };
+
   TestSuite.prototype.waitForTestResultsInConsole = function() {
     var messages = SDK.multitargetConsoleModel.messages();
     for (var i = 0; i < messages.length; ++i) {
