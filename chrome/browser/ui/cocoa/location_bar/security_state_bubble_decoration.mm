a165753c65445fd9824a32ea31c300c582294763
lgrey <lgrey@chromium.org>
2016-12-09 12:39:28 -0800

    [Mac] RTL-aware rendering for BubbleDecoration and SecurityStateBubbleDecoration
    This is almost the same exact code, so it might be worthwhile to find a way
    to abstract it out, but I think it's probably not in scope for this CL.
    
    BUG=648557
    
    Review-Url: https://codereview.chromium.org/2563593003
    Cr-Commit-Position: refs/heads/master@{#437631}

--- chrome/browser/ui/cocoa/location_bar/security_state_bubble_decoration.mm
+++ chrome/browser/ui/cocoa/location_bar/security_state_bubble_decoration.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/mac/mac_util.h"
 #include "base/strings/sys_string_conversions.h"
+#include "chrome/browser/ui/cocoa/l10n_util.h"
 #import "chrome/browser/ui/cocoa/location_bar/location_bar_view_mac.h"
 #import "chrome/browser/ui/cocoa/location_bar/location_icon_decoration.h"
 #import "chrome/browser/ui/cocoa/themed_window.h"
@@ -60,7 +61,7 @@ const NSTimeInterval kOutAnimationDuration = 250;
 
 // Transformation values at the beginning of the animation.
 const CGFloat kStartScale = 0.25;
-const CGFloat kStartx_offset = -15.0;
+const CGFloat kStartx_offset = 15.0;
 
 }  // namespace
 
@@ -151,7 +152,9 @@ CGFloat SecurityStateBubbleDecoration::GetWidthForSpace(CGFloat width) {
 void SecurityStateBubbleDecoration::DrawInFrame(NSRect frame,
                                                 NSView* control_view) {
   const NSRect decoration_frame = NSInsetRect(frame, 0.0, kBackgroundYInset);
-  CGFloat text_offset = NSMinX(decoration_frame);
+  CGFloat text_left_offset = NSMinX(decoration_frame);
+  CGFloat text_right_offset = NSMaxX(decoration_frame);
+  const BOOL is_rtl = cocoa_l10n_util::ShouldDoExperimentalRTLLayout();
   if (image_) {
     // The image should fade in if we're animating in.
     CGFloat image_alpha =
@@ -162,7 +165,12 @@ void SecurityStateBubbleDecoration::DrawInFrame(NSRect frame,
     NSRect image_rect = decoration_frame;
     image_rect.origin.y +=
         std::floor((NSHeight(decoration_frame) - image_size.height) / 2.0);
-    image_rect.origin.x += kLeftSidePadding;
+    if (is_rtl) {
+      image_rect.origin.x =
+          NSMaxX(decoration_frame) - image_size.width - kLeftSidePadding;
+    } else {
+      image_rect.origin.x += kLeftSidePadding;
+    }
     image_rect.size = image_size;
     [image_ drawInRect:image_rect
               fromRect:NSZeroRect  // Entire image
@@ -170,7 +178,10 @@ void SecurityStateBubbleDecoration::DrawInFrame(NSRect frame,
               fraction:image_alpha
         respectFlipped:YES
                  hints:nil];
-    text_offset = NSMaxX(image_rect) + kIconLabelPadding;
+    if (is_rtl)
+      text_right_offset = NSMinX(image_rect) - kIconLabelPadding;
+    else
+      text_left_offset = NSMaxX(image_rect) + kIconLabelPadding;
   }
 
   // Set the text color and draw the text.
@@ -199,9 +210,9 @@ void SecurityStateBubbleDecoration::DrawInFrame(NSRect frame,
     NSRect text_rect = frame;
     CGFloat textHeight = [text size].height;
 
-    text_rect.origin.x = text_offset;
+    text_rect.origin.x = text_left_offset;
     text_rect.origin.y = std::round(NSMidY(text_rect) - textHeight / 2.0) - 1;
-    text_rect.size.width = NSMaxX(decoration_frame) - NSMinX(text_rect);
+    text_rect.size.width = text_right_offset - text_left_offset;
     text_rect.size.height = textHeight;
 
     NSAffineTransform* transform = [NSAffineTransform transform];
@@ -216,8 +227,9 @@ void SecurityStateBubbleDecoration::DrawInFrame(NSRect frame,
 
     double x_origin_offset = NSMinX(text_rect) * (1 - scale);
     double y_origin_offset = NSMinY(text_rect) * (1 - scale);
+    double start_x_offset = is_rtl ? -kStartx_offset : kStartx_offset;
     double x_offset =
-        gfx::Tween::DoubleValueBetween(progress, kStartx_offset, 0);
+        gfx::Tween::DoubleValueBetween(progress, start_x_offset, 0);
     double y_offset = NSHeight(text_rect) * (1 - scale) / 2.0;
 
     [transform translateXBy:x_offset + x_origin_offset
@@ -230,11 +242,14 @@ void SecurityStateBubbleDecoration::DrawInFrame(NSRect frame,
 
     // Draw the divider.
     if (state() == DecorationMouseState::NONE && !active()) {
+      const CGFloat divider_x_position =
+          is_rtl ? NSMinX(decoration_frame) + DividerPadding()
+                 : NSMaxX(decoration_frame) - DividerPadding();
       NSBezierPath* line = [NSBezierPath bezierPath];
       [line setLineWidth:line_width];
-      [line moveToPoint:NSMakePoint(NSMaxX(decoration_frame) - DividerPadding(),
+      [line moveToPoint:NSMakePoint(divider_x_position,
                                     NSMinY(decoration_frame))];
-      [line lineToPoint:NSMakePoint(NSMaxX(decoration_frame) - DividerPadding(),
+      [line lineToPoint:NSMakePoint(divider_x_position,
                                     NSMaxY(decoration_frame))];
 
       NSColor* divider_color = GetDividerColor(in_dark_mode);
