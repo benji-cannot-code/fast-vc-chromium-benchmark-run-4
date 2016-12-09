d4d1f2ba76617b1891448bf1ba3c0f2bac8fe74a
tfarina <tfarina@chromium.org>
2016-12-09 05:46:24 -0800

    net: remove unused parameter from EpollEvent
    We don't assign is_epoll_wait to anything in EpollEvent, and thus it is
    unused. So there is no reason to keep it there from now on.
    
    BUG=None
    TEST=net_unittests
    R=rch@chromium.org
    
    Review-Url: https://codereview.chromium.org/2562623002
    Cr-Commit-Position: refs/heads/master@{#437527}

--- net/tools/epoll_server/epoll_server.h
+++ net/tools/epoll_server/epoll_server.h
@@ -48,7 +48,7 @@ class EpollAlarmCallbackInterface;
 class ReadPipeCallback;
 
 struct EpollEvent {
-  EpollEvent(int events, bool is_epoll_wait)
+  EpollEvent(int events)
       : in_events(events),
         out_ready_mask(0) {
   }
--- net/tools/quic/end_to_end_test.cc
+++ net/tools/quic/end_to_end_test.cc
@@ -260,7 +260,7 @@ class ClientDelegate : public PacketDroppingTestWriter::Delegate {
   explicit ClientDelegate(QuicClient* client) : client_(client) {}
   ~ClientDelegate() override {}
   void OnCanWrite() override {
-    EpollEvent event(EPOLLOUT, false);
+    EpollEvent event(EPOLLOUT);
     client_->OnEvent(client_->GetLatestFD(), &event);
   }
 
@@ -401,7 +401,7 @@ class EndToEndTest : public ::testing::TestWithParam<TestParams> {
     StartServer();
 
     CreateClientWithWriter();
-    static EpollEvent event(EPOLLOUT, false);
+    static EpollEvent event(EPOLLOUT);
     if (client_writer_ != nullptr) {
       client_writer_->Initialize(
           QuicConnectionPeer::GetHelper(
@@ -2913,7 +2913,7 @@ TEST_P(EndToEndTest, DISABLED_TestHugeResponseWithPacketLoss) {
   client->UseWriter(client_writer_);
   client->Connect();
   client_.reset(client);
-  static EpollEvent event(EPOLLOUT, false);
+  static EpollEvent event(EPOLLOUT);
   client_writer_->Initialize(
       QuicConnectionPeer::GetHelper(client_->client()->session()->connection()),
       QuicConnectionPeer::GetAlarmFactory(
