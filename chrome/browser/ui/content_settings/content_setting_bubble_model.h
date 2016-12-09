8d1396e7bcbda0bb9bb9402345425f86c5373702
tommycli <tommycli@chromium.org>
2016-12-09 13:08:51 -0800

    [HBD] Remove radio buttons from the Plugins Blocked Omnibox popup.
    This CL does two things to the Plugins Block Omnibox popup:
    
     1. Removes the radio buttons. They are misleading and irrelevant in the HBD world, and weren't actually that useful in the pre-HBD world either.
    
     2. Hides the Run All Plugins link in the policy-managed BLOCK case or the HBD BLOCK case.
    
     3. Hides the Manage... link in the policy-managed case. User cannot manage the setting anyways.
    
    BUG=655899
    
    Review-Url: https://codereview.chromium.org/2552343005
    Cr-Commit-Position: refs/heads/master@{#437653}

--- chrome/browser/ui/content_settings/content_setting_bubble_model.h
+++ chrome/browser/ui/content_settings/content_setting_bubble_model.h
@@ -46,9 +46,9 @@ class RapporServiceImpl;
 //     ContentSettingRPHBubbleModel                - protocol handlers
 //     ContentSettingMidiSysExBubbleModel          - midi sysex
 //     ContentSettingDomainListBubbleModel         - domain list (geolocation)
+//     ContentSettingPluginBubbleModel             - plugins
 //     ContentSettingSingleRadioGroup              - radio group
 //       ContentSettingCookiesBubbleModel            - cookies
-//       ContentSettingPluginBubbleModel             - plugins
 //       ContentSettingPopupBubbleModel              - popups
 //   ContentSettingSubresourceFilterBubbleModel  - filtered subresources
 
@@ -229,12 +229,6 @@ class ContentSettingBubbleModel : public content::NotificationObserver {
   void set_selected_device(const content::MediaStreamDevice& device) {
     bubble_content_.media_menus[device.type].selected_device = device;
   }
-  bool setting_is_managed() {
-    return setting_is_managed_;
-  }
-  void set_setting_is_managed(bool managed) {
-    setting_is_managed_ = managed;
-  }
   rappor::RapporServiceImpl* rappor_service() const { return rappor_service_; }
 
  private:
@@ -247,9 +241,6 @@ class ContentSettingBubbleModel : public content::NotificationObserver {
   BubbleContent bubble_content_;
   // A registrar for listening for WEB_CONTENTS_DESTROYED notifications.
   content::NotificationRegistrar registrar_;
-  // A flag that indicates if the content setting managed i.e. can't be
-  // controlled by the user.
-  bool setting_is_managed_;
   // The service used to record Rappor metrics. Can be set for testing.
   rappor::RapporServiceImpl* rappor_service_;
 
