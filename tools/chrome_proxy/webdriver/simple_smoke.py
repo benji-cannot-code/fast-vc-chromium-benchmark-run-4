9f7d50ff83ee1f14b2b98d9245e926e68f94ba97
robertogden <robertogden@chromium.org>
2016-12-08 17:36:16 -0800

    Use python's unittest framework.
    BUG=669956
    
    Review-Url: https://codereview.chromium.org/2559113005
    Cr-Commit-Position: refs/heads/master@{#437413}

--- tools/chrome_proxy/webdriver/simple_smoke.py
+++ tools/chrome_proxy/webdriver/simple_smoke.py
@@ -3,16 +3,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import sys
 import time
+import unittest
 
-from common import IntegrationTest
+import common
 from common import TestDriver
 
 
-class SimpleSmoke(IntegrationTest):
+class SimpleSmoke(unittest.TestCase):
 
   # Simple example integration test.
-  def TestCheckPageWithProxy(self):
+  def testCheckPageWithProxy(self):
     with TestDriver() as t:
       t.AddChromeArg('--enable-spdy-proxy-auth')
       t.SetURL('http://check.googlezip.net/test.html')
@@ -26,7 +28,7 @@ class SimpleSmoke(IntegrationTest):
           response.ResponseHasViaHeader(), response.WasXHR())
 
   # Show how to get a histogram.
-  def TestPingbackHistogram(self):
+  def testPingbackHistogram(self):
     with TestDriver() as t:
       t.AddChromeArg('--enable-spdy-proxy-auth')
       t.SetURL('http://check.googlezip.net/test.html')
@@ -35,5 +37,6 @@ class SimpleSmoke(IntegrationTest):
       print t.GetHistogram('DataReductionProxy.Pingback.Attempted')
 
 if __name__ == '__main__':
-  test = SimpleSmoke()
-  test.RunAllTests()
+  # The unittest library uses sys.argv itself and is easily confused by our
+  # command line options. Pass it a simpler argv instead.
+  unittest.main(argv=[sys.argv[0]], verbosity=2)
