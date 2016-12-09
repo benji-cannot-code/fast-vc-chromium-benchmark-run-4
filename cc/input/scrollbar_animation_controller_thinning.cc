a9c9d5c4d7e5b489461bfc066a95bec747dd81bd
sahel <sahel@chromium.org>
2016-12-09 09:58:39 -0800

    Overlay scrollbars shouldn't fade out while mouse is over/near.
    BUG=669668
    TEST=ScrollbarAnimationControllerThinningTest.DontFadeAfterReleasedNear,
    ScrollbarAnimationControllerThinningTest.FadeAfterReleasedFar,
    ScrollbarAnimationControllerThinningTest.MoveNearAndDontFadeOut,
    ScrollbarAnimationControllerThinningTest.MoveOverAndDontFadeOut
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2552813002
    Cr-Commit-Position: refs/heads/master@{#437585}

--- cc/input/scrollbar_animation_controller_thinning.cc
+++ cc/input/scrollbar_animation_controller_thinning.cc
@@ -73,7 +73,8 @@ void ScrollbarAnimationControllerThinning::RunAnimationFrame(float progress) {
     if (current_animating_property_ == THICKNESS) {
       thickness_change_ = NONE;
       SetCurrentAnimatingProperty(OPACITY);
-      PostDelayedAnimationTask(false);
+      if (!mouse_is_near_scrollbar_)
+        PostDelayedAnimationTask(false);
     }
   }
 }
@@ -108,7 +109,6 @@ void ScrollbarAnimationControllerThinning::DidMouseUp() {
     StartAnimation();
   } else {
     SetCurrentAnimatingProperty(OPACITY);
-    PostDelayedAnimationTask(false);
   }
 }
 
@@ -136,6 +136,18 @@ void ScrollbarAnimationControllerThinning::DidScrollUpdate(bool on_resize) {
   ApplyThumbThicknessScale(mouse_is_near_scrollbar_ ? 1.f
                                                     : kIdleThicknessScale);
   SetCurrentAnimatingProperty(OPACITY);
+
+  // Don't fade out the scrollbar when mouse is near.
+  if (mouse_is_near_scrollbar_)
+    StopAnimation();
+}
+
+void ScrollbarAnimationControllerThinning::DidScrollEnd() {
+  ScrollbarAnimationController::DidScrollEnd();
+
+  // Don't fade out the scrollbar when mouse is near.
+  if (mouse_is_near_scrollbar_)
+    StopAnimation();
 }
 
 void ScrollbarAnimationControllerThinning::DidMouseMoveNear(float distance) {
