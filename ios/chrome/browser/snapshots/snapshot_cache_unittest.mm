654cc4723058e0c229f8a67ba253374d71d0e551
stkhapugin <stkhapugin@chromium.org>
2016-12-09 09:28:19 -0800

    Moves include of objc_property_releaser out of a header.
    Moves include of ojbc_property_releaser to the implementation file to
    allow this to be included from ARC code.
    Also moves ivars to the implementation file, per style guide.
    
    BUG=672510
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2561133002
    Cr-Commit-Position: refs/heads/master@{#437575}

--- ios/chrome/browser/snapshots/snapshot_cache_unittest.mm
+++ ios/chrome/browser/snapshots/snapshot_cache_unittest.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/mac/bind_objc_block.h"
 #include "base/mac/scoped_nsautorelease_pool.h"
+#include "base/mac/scoped_nsobject.h"
 #include "base/run_loop.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/time/time.h"
