82b7080dda086ea5730c0ec473923c8f09fa6611
watk <watk@chromium.org>
2016-12-08 18:22:30 -0800

    media: AVDA now doesn't queue EOS for a flush if the codec is empty
    Apparently some MediaCodecs consider it an error to receive an EOS
    buffer as their first input. So now we skip queueing the EOS if there
    are no pending buffers in the decoder and no pending inputs.
    
    BUG=672268
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2563533002
    Cr-Commit-Position: refs/heads/master@{#437436}

--- media/gpu/android_video_decode_accelerator.h
+++ media/gpu/android_video_decode_accelerator.h
@@ -95,7 +95,6 @@ class MEDIA_GPU_EXPORT AndroidVideoDecodeAccelerator
   };
 
   enum DrainType {
-    DRAIN_TYPE_NONE,
     DRAIN_FOR_FLUSH,
     DRAIN_FOR_RESET,
     DRAIN_FOR_DESTROY,
@@ -139,9 +138,8 @@ class MEDIA_GPU_EXPORT AndroidVideoDecodeAccelerator
   // used.
   void DoIOTask(bool start_timer);
 
-  // Feeds input data to |media_codec_|. This checks
-  // |pending_bitstream_buffers_| and queues a buffer to |media_codec_|.
-  // Returns true if any input was processed.
+  // Feeds buffers in |pending_bitstream_records_| to |media_codec_|. Returns
+  // true if one was queued.
   bool QueueInput();
 
   // Dequeues output from |media_codec_| and feeds the decoded frame to the
@@ -283,9 +281,8 @@ class MEDIA_GPU_EXPORT AndroidVideoDecodeAccelerator
   // Time at which we last did useful work on io_timer_.
   base::TimeTicks most_recent_work_;
 
-  // Type of a drain request. We need to distinguish between DRAIN_FOR_FLUSH
-  // and other types, see IsDrainingForResetOrDestroy().
-  DrainType drain_type_;
+  // The ongoing drain operation, if any.
+  base::Optional<DrainType> drain_type_;
 
   // Holds a ref-count to the CDM to avoid using the CDM after it's destroyed.
   scoped_refptr<MediaKeys> cdm_for_reference_holding_only_;
