ccf9284e510500282251945f59814d26f7314660
hywu <hywu@google.com>
2016-12-08 19:45:15 -0800

    VEA unittest: fix cache line-alignment test hang issue
    If VEA encoder fails to initialize because the driver
    detects plane size is unalign with cache line and
    returns VIDIOC_S_FMT error, the test hangs at waiting
    undefinitely in error state. This change makes the test
    to fail properly.
    
    BUG=672377
    TEST=Run VEA unittest on Kevin.
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2561063002
    Cr-Commit-Position: refs/heads/master@{#437455}

--- media/gpu/video_encode_accelerator_unittest.cc
+++ media/gpu/video_encode_accelerator_unittest.cc
@@ -2114,7 +2114,11 @@ void SimpleTestFunc() {
                                           CS_FINISHED};
 
   for (const auto& state : state_transitions) {
-    EXPECT_EQ(state, note->Wait());
+    ClientState wait_state = note->Wait();
+    EXPECT_EQ(state, wait_state);
+    if (state != wait_state) {
+      break;
+    }
   }
 
   encoder_thread.task_runner()->PostTask(
