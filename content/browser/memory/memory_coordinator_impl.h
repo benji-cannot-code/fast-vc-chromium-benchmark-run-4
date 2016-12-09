961def3bdcea774b6d9f192c69bd0db1394a9e87
bashi <bashi@chromium.org>
2016-12-08 18:38:13 -0800

    Add test for MemoryCoordinatorImpl::ForceSetGlobalState()
    This is a follow-up CL of crrev.com/2550183007.
    
    BUG=617492
    
    Review-Url: https://codereview.chromium.org/2561863003
    Cr-Commit-Position: refs/heads/master@{#437443}

--- content/browser/memory/memory_coordinator_impl.h
+++ content/browser/memory/memory_coordinator_impl.h
@@ -70,6 +70,7 @@ class CONTENT_EXPORT MemoryCoordinatorImpl : public MemoryCoordinator,
   FRIEND_TEST_ALL_PREFIXES(MemoryCoordinatorImplTest, CalculateNextState);
   FRIEND_TEST_ALL_PREFIXES(MemoryCoordinatorImplTest, UpdateState);
   FRIEND_TEST_ALL_PREFIXES(MemoryCoordinatorImplTest, SetMemoryStateForTesting);
+  FRIEND_TEST_ALL_PREFIXES(MemoryCoordinatorImplTest, ForceSetGlobalState);
 
   friend struct MemoryCoordinatorSingletonTraits;
 
