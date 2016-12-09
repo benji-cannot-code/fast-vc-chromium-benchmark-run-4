2aeb725e3cb3c888dc954ae2c4d998a9cb5d017e
vmiura <vmiura@chromium.org>
2016-12-08 21:36:46 -0800

    Fix GeneratedImage::drawPattern() use of SkPictureBuilder.
    SkPictureBuilder records DisplayItems and plays them into
    an SkPicture.  GeneratedImage::drawPattern() was misusing
    SkPictureBuilder; using it to create a GraphicsContext and
    draw to it directly.  It results in the pattern:
    
    1) GraphicsContext::beginRecording()
    2) Draw content to GraphicsContext
    3) GraphicsContext::beginRecording() again
    5) SkPicture picture = GraphicsContext::endRecording()
    
    This is hitting undefined behavior in SkPictureRecorder which
    happens to work but is a bug.
    
    This change removes SkPictureBuilder and records directly
    to a GraphicsContext.
    
    BUG=672618
    
    Review-Url: https://codereview.chromium.org/2565433003
    Cr-Commit-Position: refs/heads/master@{#437478}

--- third_party/WebKit/Source/platform/graphics/GeneratedImage.cpp
+++ third_party/WebKit/Source/platform/graphics/GeneratedImage.cpp
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "platform/geometry/FloatRect.h"
 #include "platform/graphics/GraphicsContext.h"
-#include "platform/graphics/paint/SkPictureBuilder.h"
+#include "platform/graphics/paint/PaintController.h"
 #include "third_party/skia/include/core/SkImage.h"
 #include "third_party/skia/include/core/SkPicture.h"
 
@@ -49,10 +49,11 @@ void GeneratedImage::drawPattern(GraphicsContext& destContext,
   FloatRect tileRect = srcRect;
   tileRect.expand(FloatSize(repeatSpacing));
 
-  SkPictureBuilder builder(tileRect, nullptr, &destContext);
-  builder.context().beginRecording(tileRect);
-  drawTile(builder.context(), srcRect);
-  sk_sp<SkPicture> tilePicture = builder.endRecording();
+  std::unique_ptr<PaintController> paintController = PaintController::create();
+  GraphicsContext context(*paintController);
+  context.beginRecording(tileRect);
+  drawTile(context, srcRect);
+  sk_sp<SkPicture> tilePicture = context.endRecording();
 
   SkMatrix patternMatrix = SkMatrix::MakeTrans(phase.x(), phase.y());
   patternMatrix.preScale(scale.width(), scale.height());
