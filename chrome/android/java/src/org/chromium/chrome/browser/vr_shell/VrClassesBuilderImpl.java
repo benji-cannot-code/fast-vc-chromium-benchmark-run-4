a5667b4803734ba47688a72759bf2b4fd1c54bbd
bshe <bshe@chromium.org>
2016-12-09 08:34:18 -0800

    Fix disk read violation when create GvrLayout
    BUG=672477
    
    Review-Url: https://codereview.chromium.org/2556253006
    Cr-Commit-Position: refs/heads/master@{#437561}

--- chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrClassesBuilderImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrClassesBuilderImpl.java
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.vr_shell;
 
 import android.app.Activity;
+import android.os.StrictMode;
 
+import org.chromium.base.Log;
 import org.chromium.base.annotations.UsedByReflection;
 
 /**
@@ -15,6 +17,7 @@ import org.chromium.base.annotations.UsedByReflection;
  */
 @UsedByReflection("VrShellDelegate.java")
 public class VrClassesBuilderImpl implements VrClassesBuilder {
+    private static final String TAG = "VrClassesBuilderImpl";
     private final Activity mActivity;
 
     @UsedByReflection("VrShellDelegate.java")
@@ -24,12 +27,28 @@ public class VrClassesBuilderImpl implements VrClassesBuilder {
 
     @Override
     public NonPresentingGvrContext createNonPresentingGvrContext() {
-        return new NonPresentingGvrContextImpl(mActivity);
+        StrictMode.ThreadPolicy oldPolicy = StrictMode.allowThreadDiskReads();
+        try {
+            return new NonPresentingGvrContextImpl(mActivity);
+        } catch (Exception ex) {
+            Log.e(TAG, "Unable to instantiate NonPresentingGvrContextImpl", ex);
+            return null;
+        } finally {
+            StrictMode.setThreadPolicy(oldPolicy);
+        }
     }
 
     @Override
     public VrShell createVrShell() {
-        return new VrShellImpl(mActivity);
+        StrictMode.ThreadPolicy oldPolicy = StrictMode.allowThreadDiskReads();
+        try {
+            return new VrShellImpl(mActivity);
+        } catch (Exception ex) {
+            Log.e(TAG, "Unable to instantiate VrShellImpl", ex);
+            return null;
+        } finally {
+            StrictMode.setThreadPolicy(oldPolicy);
+        }
     }
 
     @Override
