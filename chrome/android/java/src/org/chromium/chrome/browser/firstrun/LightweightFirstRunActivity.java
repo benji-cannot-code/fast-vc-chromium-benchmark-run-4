8ad6385410130e150a4289dcab3d2e655888f776
tedchoc <tedchoc@chromium.org>
2016-12-09 14:10:42 -0800

    Disable UI options that allow new tabs before FRE.
    Prior to completing the FRE on Android, we should disallow
    UI options that allow creating new tabs in Chrome.
    
    This disables "Web Search" from the action mode menu and
    also the various "Open in X" menu options.
    
    BUG=671149
    
    Review-Url: https://codereview.chromium.org/2559573002
    Cr-Commit-Position: refs/heads/master@{#437670}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/LightweightFirstRunActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/LightweightFirstRunActivity.java
@@ -79,7 +79,7 @@ public class LightweightFirstRunActivity extends FirstRunActivity {
 
     @Override
     public void completeFirstRunExperience() {
-        FirstRunStatus.setLightweightFirstRunFlowComplete(LightweightFirstRunActivity.this, true);
+        FirstRunStatus.setLightweightFirstRunFlowComplete(true);
         Intent intent = new Intent();
         intent.putExtras(mFreProperties);
         finishAllTheActivities(getLocalClassName(), Activity.RESULT_OK, intent);
