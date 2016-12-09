6a6dfbe0a6a46d83a71ac2ebfb6de0527cd2f035
toyoshim <toyoshim@chromium.org>
2016-12-08 22:58:41 -0800

    Mechanical style fixes: History.cpp and Location.cpp
    This patch fixes some style errors caused at the Blink style change,
    and some long living other style nits.
    
    BUG=670237
    
    Review-Url: https://codereview.chromium.org/2553383002
    Cr-Commit-Position: refs/heads/master@{#437487}

--- third_party/WebKit/Source/core/frame/History.cpp
+++ third_party/WebKit/Source/core/frame/History.cpp
@@ -85,7 +85,7 @@ SerializedScriptValue* History::stateInternal() const {
 }
 
 void History::setScrollRestoration(const String& value) {
-  ASSERT(value == "manual" || value == "auto");
+  DCHECK(value == "manual" || value == "auto");
   if (!frame() || !frame()->loader().client())
     return;
 
@@ -134,14 +134,15 @@ void History::go(ExecutionContext* context, int delta) {
   if (!frame() || !frame()->loader().client())
     return;
 
-  ASSERT(isMainThread());
+  DCHECK(isMainThread());
   Document* activeDocument = toDocument(context);
   if (!activeDocument)
     return;
 
   if (!activeDocument->frame() ||
-      !activeDocument->frame()->canNavigate(*frame()))
+      !activeDocument->frame()->canNavigate(*frame())) {
     return;
+  }
   if (!NavigationDisablerForUnload::isNavigationAllowed())
     return;
 
@@ -186,8 +187,9 @@ bool History::canChangeToUrl(const KURL& url,
 
   RefPtr<SecurityOrigin> requestedOrigin = SecurityOrigin::create(url);
   if (requestedOrigin->isUnique() ||
-      !requestedOrigin->isSameSchemeHostPort(documentOrigin))
+      !requestedOrigin->isSameSchemeHostPort(documentOrigin)) {
     return false;
+  }
 
   return true;
 }
