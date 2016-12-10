041f40594b6911d868650d5319b4a0cd4fa73522
fs <fs@opera.com>
2016-12-10 02:35:32 -0800

    Strength-reduce the "scale-factor changed" condition in LayoutSVGRoot
    Spend some cycles examining the difference between the old and new
    local-to-border-box transforms, and only signal scale-factor changes
    if that part of the transform changed.
    This also means that we now detect changes scale that we previously
    didn't (like [1].)
    
    [1] paint/invalidation/svg/absolute-sized-content-with-resources.xhtml
    
    BUG=603956,664961
    
    Review-Url: https://codereview.chromium.org/2559123003
    Cr-Commit-Position: refs/heads/master@{#437767}

--- third_party/WebKit/Source/core/layout/svg/LayoutSVGRoot.h
+++ third_party/WebKit/Source/core/layout/svg/LayoutSVGRoot.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class SVGElement;
+enum class SVGTransformChange;
 
 class CORE_EXPORT LayoutSVGRoot final : public LayoutReplaced {
  public:
@@ -157,7 +158,7 @@ class CORE_EXPORT LayoutSVGRoot final : public LayoutReplaced {
   void descendantIsolationRequirementsChanged(DescendantIsolationState) final;
 
   void updateCachedBoundaries();
-  void buildLocalToBorderBoxTransform();
+  SVGTransformChange buildLocalToBorderBoxTransform();
 
   PositionWithAffinity positionForPoint(const LayoutPoint&) final;
 
