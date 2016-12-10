78ff3a9549d21d68f94ee2b1ca622ce8ae5330d4
brettw <brettw@chromium.org>
2016-12-09 21:59:08 -0800

    Remove enable_configuration_policy build define.
    This build flag is always set for non-iOS platforms. The flag is not
    overridable in the build and we never test other settings of it. The policy
    code is stable and doesn't change.
    
    This patch removes the flag and replaces it with !ios conditionals.
    
    BUG=671706
    
    Review-Url: https://codereview.chromium.org/2558043004
    Cr-Commit-Position: refs/heads/master@{#437759}

--- components/sync_preferences/pref_service_syncable_factory.cc
+++ components/sync_preferences/pref_service_syncable_factory.cc
@@ -6,13 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync_preferences/pref_service_syncable_factory.h"
 
 #include "base/trace_event/trace_event.h"
+#include "build/build_config.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/default_pref_store.h"
 #include "components/prefs/pref_notifier_impl.h"
 #include "components/prefs/pref_value_store.h"
 #include "components/sync_preferences/pref_service_syncable.h"
 
-#if defined(SYNC_PREFERENCES_USE_POLICY)
+#if !defined(OS_IOS)
 #include "components/policy/core/browser/browser_policy_connector.h"
 #include "components/policy/core/browser/configuration_policy_pref_store.h"
 #include "components/policy/core/common/policy_service.h"  // nogncheck
@@ -28,7 +29,7 @@ PrefServiceSyncableFactory::~PrefServiceSyncableFactory() {}
 void PrefServiceSyncableFactory::SetManagedPolicies(
     policy::PolicyService* service,
     policy::BrowserPolicyConnector* connector) {
-#if defined(SYNC_PREFERENCES_USE_POLICY)
+#if !defined(OS_IOS)
   set_managed_prefs(new policy::ConfigurationPolicyPrefStore(
       service, connector->GetHandlerList(), policy::POLICY_LEVEL_MANDATORY));
 #else
@@ -39,7 +40,7 @@ void PrefServiceSyncableFactory::SetManagedPolicies(
 void PrefServiceSyncableFactory::SetRecommendedPolicies(
     policy::PolicyService* service,
     policy::BrowserPolicyConnector* connector) {
-#if defined(SYNC_PREFERENCES_USE_POLICY)
+#if !defined(OS_IOS)
   set_recommended_prefs(new policy::ConfigurationPolicyPrefStore(
       service, connector->GetHandlerList(), policy::POLICY_LEVEL_RECOMMENDED));
 #else
