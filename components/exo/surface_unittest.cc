8142f5d95d9b819494fc82707329b154fa2db113
staraz <staraz@chromium.org>
2016-12-10 15:49:20 -0800

    Moved exo::SurfaceFactoryOwner to its own file and renamed it to exo::CompositorFrameSink.
    CompositorFrameSink implements cc::mojom::MojoCompositorFrameSink.
    
    CompositorFrameSink is no longer a friend class of exo::Surface.
    
    Added exo::CompositorFrameSinkHolder class that implements cc::mojom::MojoCompositorFrameSinkClient.
    
    BUG=659601
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2493223002
    Review-Url: https://codereview.chromium.org/2493223002
    Cr-Commit-Position: refs/heads/master@{#437780}

--- components/exo/surface_unittest.cc
+++ components/exo/surface_unittest.cc
@@ -51,6 +51,11 @@ TEST_F(SurfaceTest, Attach) {
   // attached buffer.
   surface->Attach(nullptr);
   surface->Commit();
+  // CompositorFrameSinkHolder::ReclaimResources() gets called via
+  // MojoCompositorFrameSinkClient interface. We need to wait here for the mojo
+  // call to finish so that the release callback finishes running before
+  // the assertion below.
+  RunAllPendingInMessageLoop();
   ASSERT_EQ(1, release_buffer_call_count);
 }
 
@@ -206,6 +211,7 @@ TEST_F(SurfaceTest, SetBlendMode) {
   surface->Attach(buffer.get());
   surface->SetBlendMode(SkBlendMode::kSrc);
   surface->Commit();
+  RunAllPendingInMessageLoop();
 
   const cc::CompositorFrame& frame = GetFrameFromSurface(surface.get());
   ASSERT_EQ(1u, frame.render_pass_list.size());
@@ -223,6 +229,7 @@ TEST_F(SurfaceTest, OverlayCandidate) {
 
   surface->Attach(buffer.get());
   surface->Commit();
+  RunAllPendingInMessageLoop();
 
   const cc::CompositorFrame& frame = GetFrameFromSurface(surface.get());
   ASSERT_EQ(1u, frame.render_pass_list.size());
