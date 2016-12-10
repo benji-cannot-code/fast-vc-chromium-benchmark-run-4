e2c44684ae89081bc7a90ffcc3cbe08e9a225c56
bashi <bashi@chromium.org>
2016-12-09 17:57:17 -0800

    bindings: Stop using forward declarations for typed array in union containers
    Typed arrays in blink (e.g. DOMFloat32Array) are typedef of DOMTypedArray<T, U>
    so using forward declarations causes conflicts. Stop using forward
    declarations for typed arrays in union containers. Instead, include
    headers directly.
    
    BUG=671755
    
    Review-Url: https://codereview.chromium.org/2559323004
    Cr-Commit-Position: refs/heads/master@{#437717}

--- third_party/WebKit/Source/bindings/tests/results/core/TestInterface2OrUint8Array.cpp
+++ third_party/WebKit/Source/bindings/tests/results/core/TestInterface2OrUint8Array.cpp
@@ -10,10 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "TestInterface2OrUint8Array.h"
 
 #include "bindings/core/v8/ToV8.h"
-#include "bindings/core/v8/V8ArrayBufferView.h"
 #include "bindings/core/v8/V8TestInterface2.h"
-#include "bindings/core/v8/V8Uint8Array.h"
-#include "core/dom/FlexibleArrayBufferView.h"
 
 namespace blink {
 
