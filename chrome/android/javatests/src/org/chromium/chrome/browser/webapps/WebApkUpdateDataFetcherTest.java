7b484d14cd8054dfc7fcd75ebb4bd1149a374783
pkotwicz <pkotwicz@chromium.org>
2016-12-10 18:58:04 -0800

    [WebAPKs] Compute the default scope from the manifest start URL.
    This CL changes how the scope is computed if the scope is unspecified in the
    Android Manifest.
    We used to compute the default scope based on the "URL the WebAPK navigates to
    when launched." Deep links can open a WebAPK at an arbirtrary URL.
    This CL changes the computation to be based on the Web Manifest start URL.
    
    This change enables cleaning up
    ManifestUpgradeDetectorFetcher#onDataAvailable(). We can now create the fetched
    WebApkInfo using the current WebAPK's WebApkInfo#uri().
    
    BUG=639536
    
    Review-Url: https://codereview.chromium.org/2548333006
    Cr-Commit-Position: refs/heads/master@{#437785}

--- chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebApkUpdateDataFetcherTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebApkUpdateDataFetcherTest.java
@@ -108,7 +108,7 @@ public class WebApkUpdateDataFetcherTest extends ChromeTabbedActivityTestBase {
             @Override
             public void run() {
                 WebApkInfo oldInfo = WebApkInfo.create("", "", scopeUrl, null, null, null, -1, -1,
-                        -1, -1, -1, "random.package", -1, manifestUrl, null,
+                        -1, -1, -1, "random.package", -1, manifestUrl, "",
                         new HashMap<String, String>());
                 fetcher.start(mTab, oldInfo, observer);
             }
