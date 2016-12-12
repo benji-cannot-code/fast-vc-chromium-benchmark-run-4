96261fcc00241593ef537abca208e4d792ccdbaf
einbinder <einbinder@chromium.org>
2016-12-12 15:19:02 -0800

    DevTools: Sources: Use URL over content for drag and drop on files.
    This lets you drag files into the url bar to open them.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2566753003
    Cr-Commit-Position: refs/heads/master@{#437950}

--- third_party/WebKit/Source/devtools/front_end/sources/NavigatorView.js
+++ third_party/WebKit/Source/devtools/front_end/sources/NavigatorView.js
@@ -951,20 +951,7 @@ Sources.NavigatorSourceTreeElement = class extends TreeElement {
     this.listItemElement.draggable = true;
     this.listItemElement.addEventListener('click', this._onclick.bind(this), false);
     this.listItemElement.addEventListener('contextmenu', this._handleContextMenuEvent.bind(this), false);
-    this.listItemElement.addEventListener('mousedown', this._onmousedown.bind(this), false);
     this.listItemElement.addEventListener('dragstart', this._ondragstart.bind(this), false);
-  }
-
-  _onmousedown(event) {
-    if (event.which === 1)  // Warm-up data for drag'n'drop
-      this._uiSourceCode.requestContent().then(callback.bind(this));
-    /**
-     * @param {?string} content
-     * @this {Sources.NavigatorSourceTreeElement}
-     */
-    function callback(content) {
-      this._warmedUpContent = content;
-    }
   }
 
   _shouldRenameOnMouseDown() {
@@ -993,10 +980,12 @@ Sources.NavigatorSourceTreeElement = class extends TreeElement {
     }
   }
 
+  /**
+   * @param {!DragEvent} event
+   */
   _ondragstart(event) {
-    event.dataTransfer.setData('text/plain', this._warmedUpContent);
+    event.dataTransfer.setData('text/plain', this._uiSourceCode.url());
     event.dataTransfer.effectAllowed = 'copy';
-    return true;
   }
 
   /**
