81ea9aea1cd76c7f1ff1316faff4f1d34ed711c6
xiaochengh <xiaochengh@chromium.org>
2016-12-12 02:08:18 -0800

    Add new RuntimeEnabledFeature: IdleTimeSpellChecking
    As the first step of implementing Idle Time Spell Checker, this patch adds
    a new runtime enabled feature IdleTimeSpellChecking, and exits the existing
    spell-checking code paths when the feature is enabled.
    
    BUG=671922
    
    Review-Url: https://codereview.chromium.org/2566043003
    Cr-Commit-Position: refs/heads/master@{#437840}

--- third_party/WebKit/Source/core/editing/spellcheck/SpellChecker.cpp
+++ third_party/WebKit/Source/core/editing/spellcheck/SpellChecker.cpp
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/loader/EmptyClients.h"
 #include "core/page/Page.h"
 #include "core/page/SpellCheckerClient.h"
+#include "platform/RuntimeEnabledFeatures.h"
 #include "platform/text/TextBreakIterator.h"
 #include "platform/text/TextCheckerClient.h"
 
@@ -188,6 +189,9 @@ void SpellChecker::toggleSpellCheckingEnabled() {
 }
 
 void SpellChecker::didBeginEditing(Element* element) {
+  if (RuntimeEnabledFeatures::idleTimeSpellCheckingEnabled())
+    return;
+
   if (!isSpellCheckingEnabled())
     return;
 
@@ -367,6 +371,9 @@ void SpellChecker::clearMisspellingsForMovingParagraphs(
 
 void SpellChecker::markMisspellingsForMovingParagraphs(
     const VisibleSelection& movingSelection) {
+  if (RuntimeEnabledFeatures::idleTimeSpellCheckingEnabled())
+    return;
+
   // TODO(xiaochengh): The use of updateStyleAndLayoutIgnorePendingStylesheets
   // needs to be audited.  See http://crbug.com/590369 for more details.
   // In the long term we should use idle time spell checker to prevent
@@ -399,6 +406,9 @@ void SpellChecker::markMisspellingsInternal(const VisibleSelection& selection) {
 
 void SpellChecker::markMisspellingsAfterApplyingCommand(
     const CompositeEditCommand& cmd) {
+  if (RuntimeEnabledFeatures::idleTimeSpellCheckingEnabled())
+    return;
+
   if (!isSpellCheckingEnabled())
     return;
   if (!isSpellCheckingEnabledFor(cmd.endingSelection()))
@@ -869,6 +879,9 @@ static bool shouldCheckOldSelection(const Position& oldSelectionStart) {
 void SpellChecker::respondToChangedSelection(
     const Position& oldSelectionStart,
     FrameSelection::SetSelectionOptions options) {
+  if (RuntimeEnabledFeatures::idleTimeSpellCheckingEnabled())
+    return;
+
   TRACE_EVENT0("blink", "SpellChecker::respondToChangedSelection");
   if (!isSpellCheckingEnabledFor(oldSelectionStart))
     return;
@@ -925,6 +938,9 @@ void SpellChecker::removeSpellingMarkersUnderWords(
 }
 
 void SpellChecker::spellCheckAfterBlur() {
+  if (RuntimeEnabledFeatures::idleTimeSpellCheckingEnabled())
+    return;
+
   if (!frame().selection().selection().isContentEditable())
     return;
 
