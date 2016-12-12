ea58847c1a54702185e309db58cd45c174cf2c0b
corona10 <corona10@gmail.com>
2016-12-12 13:30:12 -0800

    [Mac] Fix unit_tests build in 10.12 SDK.
    exception_processor_unittest.mm:
    error: null passed to a callee that requires a non-null argument
    
    Review-Url: https://codereview.chromium.org/2565093002
    Cr-Commit-Position: refs/heads/master@{#437917}

--- chrome/browser/mac/exception_processor_unittest.mm
+++ chrome/browser/mac/exception_processor_unittest.mm
@@ -96,7 +96,7 @@ void RaiseExceptionInRunLoop() {
   CFRunLoopRef run_loop = CFRunLoopGetCurrent();
 
   CFRunLoopPerformBlock(run_loop, kCFRunLoopCommonModes, ^{
-    [NSException raise:@"ThrowExceptionInRunLoop" format:nil];
+    [NSException raise:@"ThrowExceptionInRunLoop" format:@""];
   });
   CFRunLoopPerformBlock(run_loop, kCFRunLoopCommonModes, ^{
     CFRunLoopStop(run_loop);
@@ -130,7 +130,7 @@ void ThrowAndCatchExceptionInRunLoop() {
   CFRunLoopRef run_loop = CFRunLoopGetCurrent();
   CFRunLoopPerformBlock(run_loop, kCFRunLoopCommonModes, ^{
     @try {
-      [NSException raise:@"ObjcExceptionPreprocessCaught" format:nil];
+      [NSException raise:@"ObjcExceptionPreprocessCaught" format:@""];
     } @catch (id exception) {
     }
   });
