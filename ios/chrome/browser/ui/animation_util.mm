cc860cb50bbd5cb086fecd249a31be3c40e06ddc
stkhapugin <stkhapugin@chromium.org>
2016-12-12 08:59:49 -0800

    [ObjC ARC] Converts ios/chrome/browser/ui:ui to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2569713002
    Cr-Commit-Position: refs/heads/master@{#437891}

--- ios/chrome/browser/ui/animation_util.mm
+++ ios/chrome/browser/ui/animation_util.mm
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ios/chrome/browser/ui/reversed_animation.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 CAAnimation* FrameAnimationMake(CALayer* layer,
                                 CGRect beginFrame,
                                 CGRect endFrame) {
@@ -60,7 +64,7 @@ CAAnimation* AnimationGroupMake(NSArray* animations) {
 
 CAAnimation* DelayedAnimationMake(CAAnimation* animation,
                                   CFTimeInterval delay) {
-  CAAnimation* delayedAnimation = [[animation copy] autorelease];
+  CAAnimation* delayedAnimation = [animation copy];
   if (delayedAnimation) {
     delayedAnimation.beginTime = delay;
     delayedAnimation = AnimationGroupMake(@[ delayedAnimation ]);
