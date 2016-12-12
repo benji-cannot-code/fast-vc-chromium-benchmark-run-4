2968cce4c0318eac17dd6b7a169ce9384302e572
twellington <twellington@chromium.org>
2016-12-12 14:24:32 -0800

    [Android History] Add clear browsing data button and privacy disclaimers
    Adds an optional header to DateDividedAdapter and adds a new layout
    with a clear browsing data button and privacy disclaimers as a header
    in the history ui.
    
    BUG=654071
    
    Review-Url: https://codereview.chromium.org/2562123003
    Cr-Commit-Position: refs/heads/master@{#437931}

--- chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadHistoryAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadHistoryAdapter.java
@@ -341,6 +341,7 @@ public class DownloadHistoryAdapter extends DateDividedAdapter implements Downlo
         mRegularDownloadItems.filter(mFilter, mFilteredItems);
         mIncognitoDownloadItems.filter(mFilter, mFilteredItems);
         mOfflinePageItems.filter(mFilter, mFilteredItems);
+        clear(false);
         loadItems(mFilteredItems);
     }
 
