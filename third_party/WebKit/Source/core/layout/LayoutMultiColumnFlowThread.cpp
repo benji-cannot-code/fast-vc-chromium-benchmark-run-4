efc6449d0f0301ae24237bf980eb600c51112a28
mstensho <mstensho@opera.com>
2016-12-12 14:17:40 -0800

    Disable layout optimization when column height may be non-uniform.
    We have no way of telling what changes beyond the first column break, so if we
    cannot guarantee that the column height *is* and *was* non-uniform, we need to
    re-lay out children that may stretch into the unknown.
    
    Review-Url: https://codereview.chromium.org/2562273003
    Cr-Commit-Position: refs/heads/master@{#437928}

--- third_party/WebKit/Source/core/layout/LayoutMultiColumnFlowThread.cpp
+++ third_party/WebKit/Source/core/layout/LayoutMultiColumnFlowThread.cpp
@@ -366,6 +366,15 @@ bool LayoutMultiColumnFlowThread::isPageLogicalHeightKnown() const {
   return false;
 }
 
+bool LayoutMultiColumnFlowThread::mayHaveNonUniformPageLogicalHeight() const {
+  const LayoutMultiColumnSet* columnSet = firstMultiColumnSet();
+  if (!columnSet)
+    return false;
+  if (columnSet->nextSiblingMultiColumnSet())
+    return true;
+  return enclosingFragmentationContext();
+}
+
 LayoutSize LayoutMultiColumnFlowThread::flowThreadTranslationAtOffset(
     LayoutUnit offsetInFlowThread,
     PageBoundaryRule rule,
