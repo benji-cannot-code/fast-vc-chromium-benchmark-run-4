3b60cc4fce0ceb4e90fd68665077a70852eaf948
pauljensen <pauljensen@chromium.org>
2016-12-12 13:40:21 -0800

    [Cronet] Make some test infrastructure public rather than package-private.
    There's no reason to have it package-private, and this prevents sharing with
    tests in other packages (e.g. org.chromium.net.urlconnection).
    
    BUG=629299
    R=xunjieli
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_cronet_tester
    
    Review-Url: https://codereview.chromium.org/2568213002
    Cr-Commit-Position: refs/heads/master@{#437921}

--- components/cronet/android/test/javatests/src/org/chromium/net/CronetTestBase.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/CronetTestBase.java
@@ -153,7 +153,7 @@ public class CronetTestBase extends AndroidTestCase {
         }
     }
 
-    void assertResponseEquals(UrlResponseInfo expected, UrlResponseInfo actual) {
+    public void assertResponseEquals(UrlResponseInfo expected, UrlResponseInfo actual) {
         assertEquals(expected.getAllHeaders(), actual.getAllHeaders());
         assertEquals(expected.getAllHeadersAsList(), actual.getAllHeadersAsList());
         assertEquals(expected.getHttpStatusCode(), actual.getHttpStatusCode());
@@ -169,7 +169,7 @@ public class CronetTestBase extends AndroidTestCase {
         }
     }
 
-    static void assertContains(String expectedSubstring, String actualString) {
+    public static void assertContains(String expectedSubstring, String actualString) {
         assertNotNull(actualString);
         if (!actualString.contains(expectedSubstring)) {
             fail("String [" + actualString + "] doesn't contain substring [" + expectedSubstring
