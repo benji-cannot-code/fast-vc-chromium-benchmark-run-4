343d86fce5f2dda3e27a7b0ddb70ae96a4cbf9c2
dalecurtis <dalecurtis@chromium.org>
2016-12-13 13:56:41 -0800

    Cleanup AudioDeviceThread to avoid accidental misuse.
    The code as written returned the current task runner whenever
    AudioDeviceThread::GetTaskRunner() was called, when actually
    what we want is for it to always be bound to the constructed
    thread.
    
    This isn't an issue given that we didn't expose AudioDeviceThread
    outside of BrowserMainLoop, but this prevents any future misuse
    and cleans up some style issues.
    
    Note: This also renames AudioDeviceThread to AudioManagerThread
    since we already have an AudioDeviceThread in the renderer.
    
    BUG=656923
    TEST=none
    
    Review-Url: https://codereview.chromium.org/2558303004
    Cr-Commit-Position: refs/heads/master@{#438301}

--- content/browser/audio_manager_thread.cc
+++ content/browser/audio_manager_thread.cc
@@ -3,27 +3,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "content/browser/audio_manager_thread.h"
+
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
-#include "content/browser/audio_device_thread.h"
 
 namespace content {
 
-scoped_refptr<base::SingleThreadTaskRunner> AudioDeviceThread::GetTaskRunner() {
-#if defined(OS_MACOSX)
-    // On Mac audio task runner must belong to the main thread.
-    // See http://crbug.com/158170.
-    return base::ThreadTaskRunnerHandle::Get();
-#else
-    return thread_.task_runner();
-#endif  // defined(OS_MACOSX)
-  }
-
-AudioDeviceThread::AudioDeviceThread() : thread_("AudioThread") {
+AudioManagerThread::AudioManagerThread() : thread_("AudioThread") {
 #if defined(OS_WIN)
   thread_.init_com_with_mta(true);
 #endif
   CHECK(thread_.Start());
+
+#if defined(OS_MACOSX)
+  // On Mac, the audio task runner must belong to the main thread.
+  // See http://crbug.com/158170.
+  task_runner_ = base::ThreadTaskRunnerHandle::Get();
+#else
+  task_runner_ = thread_.task_runner();
+#endif
 }
+
+AudioManagerThread::~AudioManagerThread() {}
 
 }  // namespace content
