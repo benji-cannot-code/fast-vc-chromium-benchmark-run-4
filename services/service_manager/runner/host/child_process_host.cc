5c96aff66fd07a0bddfa247fd6ae0bdd67329b44
rockot <rockot@chromium.org>
2016-12-12 22:53:09 -0800

    Service Manager: Override DLL search path when launching services in component builds
    Service executables don't live alongside other build artifacts, so in component builds
    Windows doesn't normally know where to look for them at runtime. This overrides the
    DLL search path temporarily before launching any service child process.
    
    BUG=None
    R=ben@chromium.org
    
    Review-Url: https://codereview.chromium.org/2574653002
    Cr-Commit-Position: refs/heads/master@{#438086}

--- services/service_manager/runner/host/child_process_host.cc
+++ services/service_manager/runner/host/child_process_host.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_paths.h"
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/files/file_path.h"
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/macros.h"
@@ -41,6 +42,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_MACOSX)
 #include "services/service_manager/public/cpp/standalone_service/mach_broker.h"
 #endif
+
+#if defined(OS_WIN) && defined(COMPONENT_BUILD)
+#include <windows.h>
+
+namespace {
+
+class ScopedDllDirectoryOverride {
+ public:
+  explicit ScopedDllDirectoryOverride(const base::FilePath& path) {
+    SetDllDirectory(path.value().c_str());
+  }
+
+  ~ScopedDllDirectoryOverride() {
+    SetDllDirectory(NULL);
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(ScopedDllDirectoryOverride);
+};
+
+}  // namespace
+#endif  // defined(OS_WIN) && defined(COMPONENT_BUILD)
 
 namespace service_manager {
 
@@ -206,6 +229,8 @@ void ChildProcessHost::DoLaunch(
 #if defined(OS_MACOSX)
     MachBroker* mach_broker = MachBroker::GetInstance();
     base::AutoLock locker(mach_broker->GetLock());
+#elif defined(OS_WIN) && defined(COMPONENT_BUILD)
+    ScopedDllDirectoryOverride dll_override(exe_dir);
 #endif
     child_process_ = base::LaunchProcess(*child_command_line, options);
 #if defined(OS_MACOSX)
