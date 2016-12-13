e5d5776c50edf8dfce02e7cbc4e250d956363a97
zqzhang <zqzhang@chromium.org>
2016-12-12 17:21:57 -0800

    [Autoplay] Add more rappor metrics for autoplay in cross-origin iframes
    This CL adds rappor metrics for recording the eTLD+1 of the top-level
    frame and cross-origin iframe when the user pauses an autoplaying video.
    
    BUG=672526
    
    Review-Url: https://codereview.chromium.org/2563723002
    Cr-Commit-Position: refs/heads/master@{#437976}

--- third_party/WebKit/Source/core/html/AutoplayUmaHelper.h
+++ third_party/WebKit/Source/core/html/AutoplayUmaHelper.h
@@ -87,6 +87,7 @@ class CORE_EXPORT AutoplayUmaHelper : public EventListener,
   virtual void handleContextDestroyed();  // Make virtual for testing.
 
   void maybeUnregisterContextDestroyedObserver();
+  void maybeUnregisterMediaElementPauseListener();
 
   void maybeStartRecordingMutedVideoPlayMethodBecomeVisible();
   void maybeStopRecordingMutedVideoPlayMethodBecomeVisible(bool isVisible);
@@ -94,10 +95,13 @@ class CORE_EXPORT AutoplayUmaHelper : public EventListener,
   void maybeStartRecordingMutedVideoOffscreenDuration();
   void maybeStopRecordingMutedVideoOffscreenDuration();
 
+  void maybeRecordUserPausedAutoplayingCrossOriginVideo();
+
   void onVisibilityChangedForMutedVideoOffscreenDuration(bool isVisibile);
   void onVisibilityChangedForMutedVideoPlayMethodBecomeVisible(bool isVisible);
 
   bool shouldListenToContextDestroyed() const;
+  bool shouldRecordUserPausedAutoplayingCrossOriginVideo() const;
 
   // The autoplay source. Use AutoplaySource::NumberOfSources for invalid
   // source.
@@ -126,6 +130,9 @@ class CORE_EXPORT AutoplayUmaHelper : public EventListener,
   bool m_isVisible;
 
   std::set<CrossOriginAutoplayResult> m_recordedCrossOriginAutoplayResults;
+
+  // Whether the UMA helper has recorded user pausing a cross-origin video.
+  bool m_hasRecordedUserPausedAutoplayingCrossOriginVideo;
 
   // The observer is used to observer an autoplaying muted video changing it's
   // visibility, which is used for offscreen duration UMA.  The UMA is pending
