9adba7ca440ca099ae247167ee5da20e98847c11
pasko <pasko@chromium.org>
2016-12-13 00:36:13 -0800

    Prerender: remove PrerenderHandle::Matches()
    The function is unused now. It was used in PrerenderLocalPredictor before, which
    was removed at http://crrev.com/325673.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2569763002
    Cr-Commit-Position: refs/heads/master@{#438102}

--- chrome/browser/prerender/prerender_handle.h
+++ chrome/browser/prerender/prerender_handle.h
@@ -12,12 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/prerender/prerender_manager.h"
 
-class GURL;
-
-namespace content {
-class SessionStorageNamespace;
-}
-
 namespace prerender {
 
 class PrerenderContents;
@@ -77,11 +71,6 @@ class PrerenderHandle : public PrerenderContents::Observer {
   bool IsAbandoned() const;
 
   PrerenderContents* contents() const;
-
-  // Returns whether the prerender matches the URL provided.
-  bool Matches(
-      const GURL& url,
-      const content::SessionStorageNamespace* session_storage_namespace) const;
 
   // Returns whether this PrerenderHandle represents the same prerender as
   // the other PrerenderHandle object specified.
