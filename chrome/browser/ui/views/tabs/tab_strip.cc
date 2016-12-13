66500b3a0492f77016e4b124cc7f9beeb94a7f15
edwardjung <edwardjung@chromium.org>
2016-12-13 03:37:03 -0800

    Add tab status to accessibility labels We display various visual tab indicators but there is no corresponding indicator for accessible labels. The updated accessible label is in the format of:
    Page title - Tab status
    
    For tabs in incognito this is a format:
    Page title - Tab status (incognito)
    
    BUG=635610
    
    Review-Url: https://codereview.chromium.org/2497373003
    Cr-Commit-Position: refs/heads/master@{#438128}

--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -1325,6 +1325,11 @@ SkColor TabStrip::GetToolbarTopSeparatorColor() const {
   return controller_->GetToolbarTopSeparatorColor();
 }
 
+// Returns the accessible tab name for the tab.
+base::string16 TabStrip::GetAccessibleTabName() const {
+  return controller_->GetAccessibleTabName();
+}
+
 int TabStrip::GetBackgroundResourceId(bool* custom_image) const {
   const ui::ThemeProvider* tp = GetThemeProvider();
 
