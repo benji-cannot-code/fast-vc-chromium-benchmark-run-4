28e19a9b3dd1008a75ca7b608f1251f5bd843d17
stkhapugin <stkhapugin@chromium.org>
2016-12-13 06:59:30 -0800

    [ObjC ARC] Converts ios/chrome/browser/ui/autofill:autofill to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2569713003
    Cr-Commit-Position: refs/heads/master@{#438167}

--- ios/chrome/browser/ui/autofill/autofill_client_ios.mm
+++ ios/chrome/browser/ui/autofill/autofill_client_ios.mm
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "base/bind.h"
 #include "base/memory/ptr_util.h"
 #include "components/autofill/core/browser/autofill_credit_card_filling_infobar_delegate_mobile.h"
 #include "components/autofill/core/browser/autofill_save_card_infobar_delegate_mobile.h"
@@ -27,6 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/infobars/infobar_utils.h"
 #include "ios/chrome/browser/web_data_service_factory.h"
 #include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
 
 namespace autofill {
 
