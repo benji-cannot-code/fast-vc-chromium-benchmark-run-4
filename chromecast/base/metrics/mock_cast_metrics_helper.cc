0fc12d0d58a8dc8315648d1c0744fc9620da8d0b
esum <esum@chromium.org>
2016-12-12 19:41:19 -0800

    [Chromecast] Add mock implementation of CastMetricsHelper.
    It will be used in multiple places to unit tests metrics recording,
    so a separate file has been created rather than having every test
    define its own.
    
    BUG=internal b/33482814
    
    Review-Url: https://codereview.chromium.org/2563073002
    Cr-Commit-Position: refs/heads/master@{#438036}

---
+++ chromecast/base/metrics/mock_cast_metrics_helper.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromecast/base/metrics/mock_cast_metrics_helper.h"
+
+namespace chromecast {
+namespace metrics {
+
+MockCastMetricsHelper::MockCastMetricsHelper() = default;
+
+MockCastMetricsHelper::~MockCastMetricsHelper() = default;
+
+}  // namespace metrics
+}  // namespace chromecast
