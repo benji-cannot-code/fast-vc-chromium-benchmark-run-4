f8a69d1932f5e1e8148eced6ecc6741eb83fc3fe
alph <alph@chromium.org>
2016-12-12 20:28:34 -0800

    DevTools: Hide overview pane on timeline landing page.
    BUG=570441
    
    Review-Url: https://codereview.chromium.org/2570553003
    Cr-Commit-Position: refs/heads/master@{#438056}

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelinePanel.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelinePanel.js
@@ -726,9 +726,9 @@ Timeline.TimelinePanel = class extends UI.Panel {
   _showLandingPage() {
     if (this._landingPage)
       return;
-    this._detailsSplitWidget.detach();
+    this._timelinePane.detach();
     this._landingPage = new Timeline.TimelineLandingPage();
-    this._landingPage.show(this._timelinePane.element);
+    this._landingPage.show(this.element);
   }
 
   _hideLandingPage() {
@@ -736,7 +736,7 @@ Timeline.TimelinePanel = class extends UI.Panel {
       return;
     this._landingPage.detach();
     this._landingPage = null;
-    this._detailsSplitWidget.show(this._timelinePane.element);
+    this._timelinePane.show(this.element);
   }
 
   /**
