4d0690cc0fce1ba03bb9e656c73d5133dce36cf3
lukasza <lukasza@chromium.org>
2016-12-12 20:40:55 -0800

    Rename m_currentTask and s_currentTask/ to help rewrite_to_chrome_style tool.
    This CL helps rewrite_to_chrome_style tool, by preventing the tool from
    suggesting the following conflicting renames:
    - |s_field| -> |field_|
    - |m_field| -> |field_|
    
    BUG=641004
    
    Review-Url: https://codereview.chromium.org/2569433002
    Cr-Commit-Position: refs/heads/master@{#438058}

--- third_party/WebKit/Source/core/animation/AnimationClock.h
+++ third_party/WebKit/Source/core/animation/AnimationClock.h
@@ -36,10 +36,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/Allocator.h"
 #include "wtf/CurrentTime.h"
 #include "wtf/Noncopyable.h"
+
 #include <limits>
 
 namespace blink {
 
+// Maintains a stationary clock time during script execution.  Tries to track
+// the glass time (the moment photons leave the screen) of the current animation
+// frame.
 class CORE_EXPORT AnimationClock {
   DISALLOW_NEW();
   WTF_MAKE_NONCOPYABLE(AnimationClock);
@@ -49,19 +53,21 @@ class CORE_EXPORT AnimationClock {
                               WTF::monotonicallyIncreasingTime)
       : m_monotonicallyIncreasingTime(monotonicallyIncreasingTime),
         m_time(0),
-        m_currentTask(std::numeric_limits<unsigned>::max()) {}
+        m_taskForWhichTimeWasCalculated(std::numeric_limits<unsigned>::max()) {}
 
   void updateTime(double time);
   double currentTime();
   void resetTimeForTesting(double time = 0);
 
-  static void notifyTaskStart() { ++s_currentTask; }
+  // notifyTaskStart should be called right before the main message loop starts
+  // to run the next task from the message queue.
+  static void notifyTaskStart() { ++s_currentlyRunningTask; }
 
  private:
   WTF::TimeFunction m_monotonicallyIncreasingTime;
   double m_time;
-  unsigned m_currentTask;
-  static unsigned s_currentTask;
+  unsigned m_taskForWhichTimeWasCalculated;
+  static unsigned s_currentlyRunningTask;
 };
 
 }  // namespace blink
