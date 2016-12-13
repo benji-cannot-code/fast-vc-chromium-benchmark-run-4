299a406b261061ea6cf03944fb7e7522a1883c00
zmin <zmin@chromium.org>
2016-12-13 09:01:13 -0800

    Hide supervised user creation from UserManager when force sign in is enabled.
    BUG=642059
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2561243002
    Cr-Commit-Position: refs/heads/master@{#438194}

--- chrome/test/data/webui/md_user_manager/create_profile_tests.js
+++ chrome/test/data/webui/md_user_manager/create_profile_tests.js
@@ -67,7 +67,8 @@ cr.define('user_manager.create_profile_tests', function() {
           assertFalse(!!createProfileElement.$$('paper-dropdown-menu'));
 
           // Simulate checking the supervised user checkbox.
-          MockInteractions.tap(createProfileElement.$.makeSupervisedCheckbox);
+          MockInteractions.tap(
+            createProfileElement.$$("#makeSupervisedCheckbox"));
           Polymer.dom.flush();
 
           // The dropdown menu is visible and is populated with signed in users.
@@ -116,7 +117,8 @@ cr.define('user_manager.create_profile_tests', function() {
 
       test('Has to select a custodian for the supervised profile', function() {
         // Simulate checking the supervised user checkbox.
-        MockInteractions.tap(createProfileElement.$.makeSupervisedCheckbox);
+        MockInteractions.tap(
+          createProfileElement.$$("#makeSupervisedCheckbox"));
         Polymer.dom.flush();
 
         // Simulate clicking 'Create'.
@@ -136,7 +138,8 @@ cr.define('user_manager.create_profile_tests', function() {
 
       test('Supervised profile name is duplicate (on the device)', function() {
         // Simulate checking the supervised user checkbox.
-        MockInteractions.tap(createProfileElement.$.makeSupervisedCheckbox);
+        MockInteractions.tap(
+          createProfileElement.$$("#makeSupervisedCheckbox"));
         Polymer.dom.flush();
 
         // There is an existing supervised user with this name on the device.
@@ -167,7 +170,8 @@ cr.define('user_manager.create_profile_tests', function() {
 
       test('Supervised profile name is duplicate (remote)', function() {
         // Simulate checking the supervised user checkbox.
-        MockInteractions.tap(createProfileElement.$.makeSupervisedCheckbox);
+        MockInteractions.tap(
+          createProfileElement.$$("#makeSupervisedCheckbox"));
         Polymer.dom.flush();
 
         // There is an existing supervised user with this name on the device.
@@ -200,7 +204,8 @@ cr.define('user_manager.create_profile_tests', function() {
         browserProxy.setExistingSupervisedUsers([]);
 
         // Simulate checking the supervised user checkbox.
-        MockInteractions.tap(createProfileElement.$.makeSupervisedCheckbox);
+        MockInteractions.tap(
+          createProfileElement.$$("#makeSupervisedCheckbox"));
         Polymer.dom.flush();
 
         // Select the first signed in user.
@@ -228,7 +233,8 @@ cr.define('user_manager.create_profile_tests', function() {
 
       test('Create supervised profile', function() {
         // Simulate checking the supervised user checkbox.
-        MockInteractions.tap(createProfileElement.$.makeSupervisedCheckbox);
+        MockInteractions.tap(
+          createProfileElement.$$("#makeSupervisedCheckbox"));
         Polymer.dom.flush();
 
         // Select the first signed in user.
@@ -419,7 +425,8 @@ cr.define('user_manager.create_profile_tests', function() {
           assertEquals(0, createProfileElement.signedInUsers_.length);
 
           // Simulate checking the supervised user checkbox.
-          MockInteractions.tap(createProfileElement.$.makeSupervisedCheckbox);
+          MockInteractions.tap(
+            createProfileElement.$$("#makeSupervisedCheckbox"));
           Polymer.dom.flush();
 
           // The dropdown menu is not visible when there are no signed in users.
@@ -506,6 +513,45 @@ cr.define('user_manager.create_profile_tests', function() {
           assertEquals('', args.supervisedUserId);
           assertEquals('', args.custodianProfilePath);
         });
+      });
+    });
+
+    suite('CreateProfileTestsForceSigninPolicy', function() {
+      setup(function() {
+        browserProxy = new TestProfileBrowserProxy();
+        // Replace real proxy with mock proxy.
+        signin.ProfileBrowserProxyImpl.instance_ = browserProxy;
+        browserProxy.setIcons([{url: 'icon1.png', label: 'icon1'}]);
+      });
+
+      teardown(function(done) {
+        createProfileElement.remove();
+        // Allow asynchronous tasks to finish.
+        setTimeout(done);
+      });
+
+      test('force sign in policy enabled', function () {
+        loadTimeData.overrideValues({
+          isForceSigninEnabled: true,
+        });
+        createProfileElement = createElement();
+        Polymer.dom.flush();
+
+        var createSupervisedUserCheckbox =
+          createProfileElement.$$("#makeSupervisedCheckbox");
+        assertFalse(!!createSupervisedUserCheckbox);
+      });
+
+      test('force sign in policy not enabled', function () {
+        loadTimeData.overrideValues({
+          isForceSigninEnabled: false,
+        });
+        createProfileElement = createElement();
+        Polymer.dom.flush();
+
+        var createSupervisedUserCheckbox =
+          createProfileElement.$$("#makeSupervisedCheckbox");
+        assertTrue(createSupervisedUserCheckbox.clientHeight > 0);
       });
     });
   }
