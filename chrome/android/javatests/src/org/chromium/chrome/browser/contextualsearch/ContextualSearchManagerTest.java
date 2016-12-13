473d9ae6dc621862c302d351f75645f12edea5d0
nednguyen <nednguyen@google.com>
2016-12-13 08:01:03 -0800

    Disable flaky tests in ContextualSearchManagerTest.java suite
    BUG=673681
    TBR=donnd@chromium.org,twellington@chromium.org
    
    Review-Url: https://codereview.chromium.org/2571783002
    Cr-Commit-Position: refs/heads/master@{#438180}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
@@ -2740,6 +2740,7 @@ public class ContextualSearchManagerTest extends ChromeActivityTestCaseBase<Chro
      */
     @SmallTest
     @Feature({"ContextualSearch"})
+    @RetryOnFailure  // crbug.com/673681
     public void testQuickActionCaptionAndImage() throws InterruptedException, TimeoutException {
         // Simulate a tap to show the Bar, then set the quick action data.
         simulateTapSearch("search");
