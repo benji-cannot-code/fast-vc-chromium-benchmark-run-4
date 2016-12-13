eec295f683a46d8a0210f38083b9a899243a51c8
leon.han <leon.han@intel.com>
2016-12-12 18:52:16 -0800

    Forbid components/ui_devtools:ui_devtools exporting symbols.
    components/ui_devtools:ui_devtools is a source_set target depended on
    by chrome/browser/ui:ui and ash:ash, which just embed ui_devtools and do not
    expect it to export any symbols out.
    
    BUG=669352
    
    Review-Url: https://codereview.chromium.org/2568003002
    Cr-Commit-Position: refs/heads/master@{#438002}

--- components/ui_devtools/devtools_server.h
+++ components/ui_devtools/devtools_server.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/ui_devtools/Forward.h"
 #include "components/ui_devtools/Protocol.h"
 #include "components/ui_devtools/devtools_client.h"
-#include "components/ui_devtools/devtools_export.h"
 #include "components/ui_devtools/string_util.h"
 #include "net/server/http_server.h"
 
@@ -27,12 +26,12 @@ class UiDevToolsServer : public net::HttpServer::Delegate {
 
   // Returns an empty unique_ptr if ui devtools flag isn't enabled or if a
   // server instance has already been created.
-  static UI_DEVTOOLS_EXPORT std::unique_ptr<UiDevToolsServer> Create(
+  static std::unique_ptr<UiDevToolsServer> Create(
       scoped_refptr<base::SingleThreadTaskRunner> io_thread_task_runner);
 
   // Returns a list of attached UiDevToolsClient name + URL
   using NameUrlPair = std::pair<std::string, std::string>;
-  static UI_DEVTOOLS_EXPORT std::vector<NameUrlPair> GetClientNamesAndUrls();
+  static std::vector<NameUrlPair> GetClientNamesAndUrls();
 
   void AttachClient(std::unique_ptr<UiDevToolsClient> client);
   void SendOverWebSocket(int connection_id, const String& message);
