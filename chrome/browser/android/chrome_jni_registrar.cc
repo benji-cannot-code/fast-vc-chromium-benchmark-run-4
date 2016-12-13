18641eaaff1955849e0349a753300a06611a2b87
tommyt <tommyt@opera.com>
2016-12-13 12:06:48 -0800

    PaymentApp: Implement the JNI bridge
    Add two native methods to ServiceWorkerPaymentAppBridge, and implement
    them in service_worker_payment_app_bridge.cc. The two methods are
    GetAllAppManifests and InvokePaymentApp. At the moment, they are just
    stubs, although GetAllAppManifest contains a bit of code, mainly
    for illustration and to avoid compile errors for unused @CalledFromNative
    methods in ServiceWorkerPaymentAppBridge.
    
    BUG=661608
    
    Review-Url: https://codereview.chromium.org/2556753002
    Cr-Commit-Position: refs/heads/master@{#438269}

--- chrome/browser/android/chrome_jni_registrar.cc
+++ chrome/browser/android/chrome_jni_registrar.cc
@@ -77,6 +77,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/omnibox/autocomplete_controller_android.h"
 #include "chrome/browser/android/omnibox/omnibox_prerender.h"
 #include "chrome/browser/android/password_ui_view_android.h"
+#include "chrome/browser/android/payments/service_worker_payment_app_bridge.h"
 #include "chrome/browser/android/physical_web/physical_web_data_source_android.h"
 #include "chrome/browser/android/policy/policy_auditor.h"
 #include "chrome/browser/android/precache/precache_launcher.h"
@@ -371,6 +372,7 @@ static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
     {"SceneLayer", RegisterSceneLayer},
     {"ScreenshotTask", chrome::android::RegisterScreenshotTask},
     {"ServiceTabLauncher", ServiceTabLauncher::Register},
+    {"ServiceWorkerPaymentAppBridge", RegisterServiceWorkerPaymentAppBridge},
     {"SessionTabHelper", RegisterSessionTabHelper},
     {"SigninInvestigator", SigninInvestigatorAndroid::Register},
     {"SigninManager", SigninManagerAndroid::Register},
