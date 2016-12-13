4128ec7ec953276cea99f3a6efcc7c089bfebcb6
boliu <boliu@chromium.org>
2016-12-13 08:54:12 -0800

    Revert of Mark org.chromium.android_webview.test.ClientOnPageFinishedTest#testPassesCorrectUrl flaky (patchset #1 id:1 of https://codereview.chromium.org/2568413003/ )
    Reason for revert:
    Infra problem, not test problem. And the problematic bot has been taken off of cq.
    
    Original issue's description:
    > Mark org.chromium.android_webview.test.ClientOnPageFinishedTest#testPassesCorrectUrl flaky
    >
    > BUG=672885
    > TBR=boliu@chromium.org
    >
    > Committed: https://crrev.com/7d0a2e6fd2a182eb8fa8888cd9539c6072bbf136
    > Cr-Commit-Position: refs/heads/master@{#438190}
    
    TBR=nednguyen@google.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=672885
    
    Review-Url: https://codereview.chromium.org/2573833002
    Cr-Commit-Position: refs/heads/master@{#438191}

--- android_webview/javatests/src/org/chromium/android_webview/test/ClientOnPageFinishedTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/ClientOnPageFinishedTest.java
@@ -40,7 +40,6 @@ public class ClientOnPageFinishedTest extends AwTestBase {
 
     @MediumTest
     @Feature({"AndroidWebView"})
-    @FlakyTest(message = "crbug.com/672885")
     public void testPassesCorrectUrl() throws Throwable {
         TestCallbackHelperContainer.OnPageFinishedHelper onPageFinishedHelper =
                 mContentsClient.getOnPageFinishedHelper();
