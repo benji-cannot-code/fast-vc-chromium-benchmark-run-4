760d2e7bcac773adbf4aea5517a064397364ebcc
hidehiko <hidehiko@chromium.org>
2016-12-12 17:59:02 -0800

    Remove unused ArcUserDataService.
    It used to be used for data/ dir removal, but it is now
    integrated into ArcSessionManager.
    Remove the unused class now.
    
    BUG=657687
    BUG=b/31079732
    TEST=Ran bots.
    
    Review-Url: https://codereview.chromium.org/2573463002
    Cr-Commit-Position: refs/heads/master@{#437983}

--- chrome/browser/chromeos/arc/arc_service_launcher.cc
+++ chrome/browser/chromeos/arc/arc_service_launcher.cc
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/obb_mounter/arc_obb_mounter_bridge.h"
 #include "components/arc/power/arc_power_bridge.h"
 #include "components/arc/storage_manager/arc_storage_manager.h"
-#include "components/arc/user_data/arc_user_data_service.h"
 #include "components/prefs/pref_member.h"
 #include "content/public/browser/browser_thread.h"
 #include "ui/arc/notification/arc_notification_manager.h"
--- chrome/browser/chromeos/arc/enterprise/arc_enterprise_reporting_service.cc
+++ chrome/browser/chromeos/arc/enterprise/arc_enterprise_reporting_service.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/arc/arc_session_manager.h"
 #include "components/arc/arc_bridge_service.h"
 #include "components/arc/arc_service_manager.h"
-#include "components/arc/user_data/arc_user_data_service.h"
 
 namespace arc {
 
