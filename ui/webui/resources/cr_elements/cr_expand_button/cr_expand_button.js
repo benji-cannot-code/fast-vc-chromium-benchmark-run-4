948f4674333b05aad78233c0ce528e464a6910f0
stevenjb <stevenjb@chromium.org>
2016-12-13 15:51:12 -0800

    MD WebUI: Clean up cr-expand-button style plus minor fixes.
    This eliminates some old, unwanted .css files.
    It also:
    * Eliminates unwanted padding from the paper-icon-button
    * Prevents on-tap event propagation (so that e.g. when embedded in a
      div that also toggles the expand state it doesn't toggle twice).
    
    BUG=none
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2561943002
    Cr-Commit-Position: refs/heads/master@{#438343}

--- ui/webui/resources/cr_elements/cr_expand_button/cr_expand_button.js
+++ ui/webui/resources/cr_elements/cr_expand_button/cr_expand_button.js
@@ -41,5 +41,13 @@ Polymer({
 
   iconName_: function(expanded) {
     return expanded ? 'cr:expand-less' : 'cr:expand-more';
-  }
+  },
+
+  /**
+   * @param {Event} event
+   * @private
+   */
+  stopTap_: function(event) {
+    event.stopPropagation();
+  },
 });
