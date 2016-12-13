96fc8a3becf32708f6abc11a95e87ec62cde4068
aazzam <aazzam@google.com>
2016-12-12 20:36:33 -0800

    Added CSSPropertyAPI and CSS padding properties which implement this API
    Added the CSSPropertyAPI for a single method parseSingleValue. Made
    CSSPaddingProperties which implements this API for a few css
    properties. Added CSS property descriptor which uses a struct to store
    function pointers for the property's functions.
    
    BUG=668012
    
    Review-Url: https://codereview.chromium.org/2533673002
    Cr-Commit-Position: refs/heads/master@{#438057}

---
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyAPIPadding.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CSSPropertyAPIPadding_h
+#define CSSPropertyAPIPadding_h
+
+#include "core/CSSPropertyNames.h"
+#include "core/css/properties/CSSPropertyAPI.h"
+
+namespace blink {
+
+// TODO(aazzam): Generate API .h files
+
+class CSSParserTokenRange;
+class CSSParserContext;
+
+class CSSPropertyAPIWebkitPadding : public CSSPropertyAPI {
+ public:
+  static const CSSValue* parseSingleValue(CSSParserTokenRange&,
+                                          const CSSParserContext&);
+};
+
+}  // namespace blink
+
+#endif  // CSSPropertyAPIPadding_h
