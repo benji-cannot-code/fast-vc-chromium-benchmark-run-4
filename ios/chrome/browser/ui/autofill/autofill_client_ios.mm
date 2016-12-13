3f4839f1fe5161564ebc09a8cc762c9d9bc3391d
olivierrobin <olivierrobin@chromium.org>
2016-12-13 08:29:30 -0800

    Revert of [ObjC ARC] Converts ios/chrome/browser/ui/autofill:autofill to ARC. (patchset #3 id:40001 of https://codereview.chromium.org/2569713003/ )
    Reason for revert:
    Breaks compilation of autofill_controller_unittest.mm
    
    Original issue's description:
    > [ObjC ARC] Converts ios/chrome/browser/ui/autofill:autofill to ARC.
    >
    > Automatically generated ARCMigrate commit
    > Notable issues:None
    > BUG=624363
    > TEST=None
    >
    > Committed: https://crrev.com/01e99d0af3a84ae96857d34e096ff4f876e56c2a
    > Cr-Commit-Position: refs/heads/master@{#438167}
    
    TBR=noyau@chromium.org,stkhapugin@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=624363
    
    Review-Url: https://codereview.chromium.org/2577443002
    Cr-Commit-Position: refs/heads/master@{#438188}

--- ios/chrome/browser/ui/autofill/autofill_client_ios.mm
+++ ios/chrome/browser/ui/autofill/autofill_client_ios.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/bind.h"
 #include "base/memory/ptr_util.h"
 #include "components/autofill/core/browser/autofill_credit_card_filling_infobar_delegate_mobile.h"
 #include "components/autofill/core/browser/autofill_save_card_infobar_delegate_mobile.h"
@@ -26,10 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/infobars/infobar_utils.h"
 #include "ios/chrome/browser/web_data_service_factory.h"
 #include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
-
-#if !defined(__has_feature) || !__has_feature(objc_arc)
-#error "This file requires ARC support."
-#endif
 
 namespace autofill {
 
