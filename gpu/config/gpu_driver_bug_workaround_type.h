54933b6754b56dee70dd7d80081a77d4df20bbed
qiankun.miao <qiankun.miao@intel.com>
2016-12-13 13:41:07 -0800

    Hook up REWRITE_FLOAT_UNARY_MINUS_OPERATOR workaround in ANGLE
    ANGLE CL is at: https://chromium-review.googlesource.com/#/c/417169.
    
    BUG=672380
    TEST=conformance/glsl/bugs/unary-minus-operator-float-bug.html
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2567983002
    Cr-Commit-Position: refs/heads/master@{#438287}

--- gpu/config/gpu_driver_bug_workaround_type.h
+++ gpu/config/gpu_driver_bug_workaround_type.h
@@ -154,6 +154,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
          reverse_point_sprite_coord_origin)                  \
   GPU_OP(REWRITE_DO_WHILE_LOOPS,                             \
          rewrite_do_while_loops)                             \
+  GPU_OP(REWRITE_FLOAT_UNARY_MINUS_OPERATOR,                 \
+         rewrite_float_unary_minus_operator)                 \
   GPU_OP(REWRITE_TEXELFETCHOFFSET_TO_TEXELFETCH,             \
          rewrite_texelfetchoffset_to_texelfetch)             \
   GPU_OP(SCALARIZE_VEC_AND_MAT_CONSTRUCTOR_ARGS,             \
