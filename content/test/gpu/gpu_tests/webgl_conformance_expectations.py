84751f36c6ea162291bb16f2963d4b57206c53e8
kainino <kainino@chromium.org>
2016-12-14 13:50:04 -0800

    Disable ETC1 textures on WebGL 1 on ANGLE
    BUG=658929
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2576813002
    Cr-Commit-Position: refs/heads/master@{#438638}

--- content/test/gpu/gpu_tests/webgl_conformance_expectations.py
+++ content/test/gpu/gpu_tests/webgl_conformance_expectations.py
@@ -52,7 +52,7 @@ class WebGLConformanceExpectations(GpuTestExpectations):
     self.Skip('WebglExtension_WEBGL_compressed_texture_atc',
         ['win', 'mac', 'linux'])
     self.Skip('WebglExtension_WEBGL_compressed_texture_etc1',
-        ['mac', 'linux'])
+        ['win', 'mac', 'linux'])
     self.Skip('WebglExtension_WEBGL_compressed_texture_pvrtc',
         ['win', 'mac', 'linux'])
     self.Skip('WebglExtension_WEBGL_compressed_texture_s3tc_srgb',
@@ -62,8 +62,6 @@ class WebGLConformanceExpectations(GpuTestExpectations):
     self.Fail('WebglExtension_EXT_disjoint_timer_query',
         ['win', 'd3d9'])
     self.Fail('WebglExtension_EXT_sRGB',
-        ['win', 'd3d9'])
-    self.Fail('WebglExtension_WEBGL_compressed_texture_etc1',
         ['win', 'd3d9'])
 
     self.Fail('WebglExtension_WEBGL_depth_texture',
