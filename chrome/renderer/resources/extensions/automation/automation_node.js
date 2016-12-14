efd2db4bb418ef37e4398ce3e8e1f686a45b221c
dtseng <dtseng@chromium.org>
2016-12-14 07:58:02 -0800

    Expose media controls to accessibility
    This cl wires up media controls given a web contents -> AX tree mapping.
    
    Actions:
    startDuckingMedia: requests the associated MediaSession start ducking. This gets passed back to the MediaSessionController which eventually relays the new volume multiplier.
    stopDuckingMedia: same as above with volume multiplier set to 1.0.
    resumeMedia: resumes suspended media
    suspendMedia: suspends currently playing media
    
    Events:
    mediaStartedPlaying
    mediaStoppedPlaying
    
    Note: to see this work, use flag --enable-default-media-session
    
    This would allow a client extension to:
    - duck audio on a specific web contents
    - observe when a media session is started/stopped
    
    For ChromeVox, we can then allow audio not coming from the TTS extension or the ChromeVox extension (e.g. earcons) to be ducked. This also allows us to duck web contents in the background, longer running media sessions, foreground web contents based on a key command, after a tts start event, etc.
    
    TEST=observe events; play media, stop media; verify media started/stopped come through. Explicitly call start/stop ducking; verify audio ducks/unducks. Also, call resume/suspend and verify audio resumes/suspends.
    BUG=621697
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2550593003
    Cr-Commit-Position: refs/heads/master@{#438525}

--- chrome/renderer/resources/extensions/automation/automation_node.js
+++ chrome/renderer/resources/extensions/automation/automation_node.js
@@ -378,6 +378,10 @@ AutomationNodeImpl.prototype = {
     this.performAction_('makeVisible');
   },
 
+    resumeMedia: function() {
+    this.performAction_('resumeMedia');
+  },
+
   setSelection: function(startIndex, endIndex) {
     if (this.role == 'textField' || this.role == 'textBox') {
       this.performAction_('setSelection',
@@ -393,6 +397,18 @@ AutomationNodeImpl.prototype = {
 
   showContextMenu: function() {
     this.performAction_('showContextMenu');
+  },
+
+  startDuckingMedia: function() {
+    this.performAction_('startDuckingMedia');
+  },
+
+  stopDuckingMedia: function() {
+    this.performAction_('stopDuckingMedia');
+  },
+
+  suspendMedia: function() {
+    this.performAction_('suspendMedia');
   },
 
   domQuerySelector: function(selector, callback) {
@@ -1073,9 +1089,13 @@ utils.expose(AutomationNode, AutomationNodeImpl, {
     'getImageData',
     'makeVisible',
     'matches',
+    'resumeMedia',
     'setSelection',
     'setSequentialFocusNavigationStartingPoint',
     'showContextMenu',
+    'startDuckingMedia',
+    'stopDuckingMedia',
+    'suspendMedia',
     'addEventListener',
     'removeEventListener',
     'domQuerySelector',
