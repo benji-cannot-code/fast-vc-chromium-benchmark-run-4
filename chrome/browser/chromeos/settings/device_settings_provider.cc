1eb6cdac52b5cf5c9960fad652104bb2c94296c7
xdai <xdai@chromium.org>
2016-12-14 11:34:20 -0800

    [Chrome OS] Add a chrome os device wallpaper policy.
    Today, even if domain admins set a wallpaper policy, every device
    shows up with the branding of the OEM until a user logs into the
    device (and then the user wallpaper policy kicks in). We introduce
    a new device policy in this CL for domain admins to set a device
    level policy rather than a user level policy for setting a wallpaper.
    That way the wallpaper will change after being enrolled to the domain
    without requiring users to log in. This only impacts the startscreen
    wallpaper, if the domain admin has set a different wallpaper in the
    user policy or has allowed the users to select their own wallpaper,
    that one will be displayed within a session.
    
    Note this CL only introduces the policy, the implementation to reflect
    the policy is addressed in another CL:
    https://codereview.chromium.org/2572793003/.
    
    BUG=475998
    
    Review-Url: https://codereview.chromium.org/2544693002
    Cr-Commit-Position: refs/heads/master@{#438592}

--- chrome/browser/chromeos/settings/device_settings_provider.cc
+++ chrome/browser/chromeos/settings/device_settings_provider.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/callback.h"
+#include "base/json/json_reader.h"
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/metrics/histogram_macros.h"
@@ -65,6 +66,7 @@ const char* const kKnownSettings[] = {
     kDeviceDisabledMessage,
     kDeviceOwner,
     kDeviceQuirksDownloadEnabled,
+    kDeviceWallpaperImage,
     kDisplayRotationDefault,
     kExtensionCacheSize,
     kHeartbeatEnabled,
@@ -523,6 +525,14 @@ void DecodeGenericPolicies(
     new_values_cache->SetBoolean(
         kDeviceQuirksDownloadEnabled,
         policy.quirks_download_enabled().quirks_download_enabled());
+  }
+
+  if (policy.has_device_wallpaper_image() &&
+      policy.device_wallpaper_image().has_device_wallpaper_image()) {
+    std::unique_ptr<base::DictionaryValue> dict_val =
+        base::DictionaryValue::From(base::JSONReader::Read(
+            policy.device_wallpaper_image().device_wallpaper_image()));
+    new_values_cache->SetValue(kDeviceWallpaperImage, std::move(dict_val));
   }
 }
 
