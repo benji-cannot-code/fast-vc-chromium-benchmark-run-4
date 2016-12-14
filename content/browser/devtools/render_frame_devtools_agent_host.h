9e229951075e232fd104e446c2b8277ab9e8f3a6
dgozman <dgozman@chromium.org>
2016-12-13 22:49:53 -0800

    [DevTools] Migrate storage domain to new generator.
    BUG=664683
    TBR=pfeldman
    
    Review-Url: https://codereview.chromium.org/2572933002
    Cr-Commit-Position: refs/heads/master@{#438449}

--- content/browser/devtools/render_frame_devtools_agent_host.h
+++ content/browser/devtools/render_frame_devtools_agent_host.h
@@ -43,7 +43,6 @@ class RenderFrameHostImpl;
 namespace devtools {
 namespace input { class InputHandler; }
 namespace service_worker { class ServiceWorkerHandler; }
-namespace storage { class StorageHandler; }
 namespace target { class TargetHandler; }
 }
 
@@ -56,6 +55,7 @@ class NetworkHandler;
 class PageHandler;
 class SchemaHandler;
 class SecurityHandler;
+class StorageHandler;
 class TracingHandler;
 }  // namespace protocol
 
@@ -194,8 +194,7 @@ class CONTENT_EXPORT RenderFrameDevToolsAgentHost
   std::unique_ptr<protocol::SecurityHandler> security_handler_;
   std::unique_ptr<devtools::service_worker::ServiceWorkerHandler>
       service_worker_handler_;
-  std::unique_ptr<devtools::storage::StorageHandler>
-      storage_handler_;
+  std::unique_ptr<protocol::StorageHandler> storage_handler_;
   std::unique_ptr<devtools::target::TargetHandler> target_handler_;
   std::unique_ptr<protocol::TracingHandler> tracing_handler_;
   std::unique_ptr<protocol::EmulationHandler> emulation_handler_;
