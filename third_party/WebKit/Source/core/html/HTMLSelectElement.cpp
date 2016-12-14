0571fbbbf54fb8dfd98e6f53bd55f910b62de922
tkent <tkent@chromium.org>
2016-12-14 00:28:21 -0800

    SELECT element: Do not update popup content if CharacterData or attribute value is not changed.
    BUG=667148
    
    Review-Url: https://codereview.chromium.org/2568333004
    Cr-Commit-Position: refs/heads/master@{#438462}

--- third_party/WebKit/Source/core/html/HTMLSelectElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLSelectElement.cpp
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/MutationCallback.h"
 #include "core/dom/MutationObserver.h"
 #include "core/dom/MutationObserverInit.h"
+#include "core/dom/MutationRecord.h"
 #include "core/dom/NodeComputedStyle.h"
 #include "core/dom/NodeListsNodeData.h"
 #include "core/dom/NodeTraversal.h"
@@ -1978,13 +1979,24 @@ class HTMLSelectElement::PopupUpdater : public MutationCallback {
   void dispose() { m_observer->disconnect(); }
 
  private:
-  void call(const HeapVector<Member<MutationRecord>>&,
+  void call(const HeapVector<Member<MutationRecord>>& records,
             MutationObserver*) override {
     // We disconnect the MutationObserver when a popuup is closed.  However
     // MutationObserver can call back after disconnection.
     if (!m_select->popupIsVisible())
       return;
-    m_select->didMutateSubtree();
+    for (const auto& record : records) {
+      if (record->type() == "attributes") {
+        const Element& element = *toElement(record->target());
+        if (record->oldValue() == element.getAttribute(record->attributeName()))
+          continue;
+      } else if (record->type() == "characterData") {
+        if (record->oldValue() == record->target()->nodeValue())
+          continue;
+      }
+      m_select->didMutateSubtree();
+      return;
+    }
   }
 
   ExecutionContext* getExecutionContext() const override {
@@ -2006,9 +2018,11 @@ HTMLSelectElement::PopupUpdater::PopupUpdater(HTMLSelectElement& select)
   filter.append(String("selected"));
   filter.append(String("value"));
   MutationObserverInit init;
+  init.setAttributeOldValue(true);
   init.setAttributes(true);
   init.setAttributeFilter(filter);
   init.setCharacterData(true);
+  init.setCharacterDataOldValue(true);
   init.setChildList(true);
   init.setSubtree(true);
   m_observer->observe(&select, init, ASSERT_NO_EXCEPTION);
