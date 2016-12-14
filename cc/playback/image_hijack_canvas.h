dd2657dbde1cba5287a4f987ed1b6859c23e4a55
ericrk <ericrk@chromium.org>
2016-12-14 14:23:06 -0800

    ImageHijackCanvas should skip non-lazy images from SkImageShader
    ImageHijack canvas can currently hit a DCHECK when we try to process
    images from SkImageShaders, as it handles them even if they are
    non-lazy. Non-lazy images should be skipped.
    
    BUG=673981
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2572783005
    Cr-Commit-Position: refs/heads/master@{#438650}

--- cc/playback/image_hijack_canvas.h
+++ cc/playback/image_hijack_canvas.h
@@ -7,13 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CC_PLAYBACK_IMAGE_HIJACK_CANVAS_H_
 
 #include "base/macros.h"
+#include "cc/base/cc_export.h"
 #include "third_party/skia/include/utils/SkNWayCanvas.h"
 
 namespace cc {
 
 class ImageDecodeCache;
 
-class ImageHijackCanvas : public SkNWayCanvas {
+class CC_EXPORT ImageHijackCanvas : public SkNWayCanvas {
  public:
   ImageHijackCanvas(int width,
                     int height,
