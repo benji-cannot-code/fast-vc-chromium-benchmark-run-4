03dc97e4bce80032b37fc02c6ec2d6a8c5e6e409
ericwilligers <ericwilligers@chromium.org>
2016-12-13 21:06:45 -0800

    CSS Motion Path: offset-anchor should default to auto
    Spec:
    https://drafts.fxtf.org/motion-1/#offset-anchor-property
    
    BUG=668563
    
    Review-Url: https://codereview.chromium.org/2527303002
    Cr-Commit-Position: refs/heads/master@{#438438}

--- third_party/WebKit/Source/core/style/ComputedStyle.cpp
+++ third_party/WebKit/Source/core/style/ComputedStyle.cpp
@@ -1394,6 +1394,8 @@ void ComputedStyle::applyMotionPathTransform(
   if (!motionData.m_path) {
     return;
   }
+  const LengthPoint& position = offsetPosition();
+  const LengthPoint& anchor = offsetAnchor();
   const StylePath& motionPath = *motionData.m_path;
   float pathLength = motionPath.length();
   float distance = floatValueForLength(motionData.m_distance, pathLength);
@@ -1415,9 +1417,10 @@ void ComputedStyle::applyMotionPathTransform(
 
   float originShiftX = 0;
   float originShiftY = 0;
-  if (RuntimeEnabledFeatures::cssOffsetPositionAnchorEnabled()) {
-    // TODO(ericwilligers): crbug.com/638055 Support offset-anchor: auto.
-    const LengthPoint& anchor = offsetAnchor();
+  // If offset-Position and offset-anchor properties are not yet enabled,
+  // they will have the default value, auto.
+  if (position.x() != Length(Auto) || anchor.x() != Length(Auto)) {
+    // Shift the origin from transform-origin to offset-anchor.
     originShiftX = floatValueForLength(anchor.x(), boundingBox.width()) -
                    floatValueForLength(transformOriginX(), boundingBox.width());
     originShiftY =
@@ -1429,9 +1432,9 @@ void ComputedStyle::applyMotionPathTransform(
                       point.y() - originY + originShiftY);
   transform.rotate(angle + motionData.m_rotation.angle);
 
-  if (RuntimeEnabledFeatures::cssOffsetPositionAnchorEnabled()) {
+  if (position.x() != Length(Auto) || anchor.x() != Length(Auto))
+    // Shift the origin back to transform-origin.
     transform.translate(-originShiftX, -originShiftY);
-  }
 }
 
 void ComputedStyle::setTextShadow(PassRefPtr<ShadowList> s) {
