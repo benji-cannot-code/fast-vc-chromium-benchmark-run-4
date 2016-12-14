3671520cff47dc6ec8d59b9892e329b9a33cf9a3
wnwen <wnwen@chromium.org>
2016-12-14 14:01:36 -0800

    Revert of Android: Switch to thread pool executor (patchset #7 id:120001 of https://codereview.chromium.org/2562643004/ )
    Reason for revert:
    BackgroundSyncLauncherTest#testNewLauncherDisablesNextOnline fails.
    
    Original issue's description:
    > Android: Switch to thread pool executor.
    >
    > Switches BackgroundSyncLauncher to the thread pool executor and add
    > comments explaining why AsyncTask is necessary.
    >
    > BUG=601053
    >
    > Committed: https://crrev.com/ad3ee42510afda09e00cb73c694bfa4626980d1d
    > Cr-Commit-Position: refs/heads/master@{#438573}
    
    TBR=iclelland@chromium.org,mariakhomenko@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=601053
    
    Review-Url: https://codereview.chromium.org/2580503002
    Cr-Commit-Position: refs/heads/master@{#438645}

--- chrome/android/javatests/src/org/chromium/chrome/browser/BackgroundSyncLauncherTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/BackgroundSyncLauncherTest.java
@@ -14,7 +14,6 @@ import org.chromium.base.test.util.AdvancedMockContext;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.RetryOnFailure;
 
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Semaphore;
 
 /**
@@ -53,7 +52,7 @@ public class BackgroundSyncLauncherTest extends InstrumentationTestCase {
                     }
                 };
 
-        BackgroundSyncLauncher.shouldLaunchBrowserIfStopped(callback);
+        BackgroundSyncLauncher.shouldLaunchBrowserIfStopped(mContext, callback);
         try {
             // Wait on the callback to be called.
             semaphore.acquire();
@@ -61,16 +60,6 @@ public class BackgroundSyncLauncherTest extends InstrumentationTestCase {
             fail("Failed to acquire semaphore");
         }
         return mShouldLaunchResult;
-    }
-
-    private void waitForLaunchBrowserTask() {
-        try {
-            mLauncher.mLaunchBrowserIfStoppedTask.get();
-        } catch (InterruptedException e) {
-            fail("Launch task was interrupted");
-        } catch (ExecutionException e) {
-            fail("Launch task had execution exception");
-        }
     }
 
     @SmallTest
@@ -93,11 +82,9 @@ public class BackgroundSyncLauncherTest extends InstrumentationTestCase {
     @RetryOnFailure
     public void testSetLaunchWhenNextOnline() {
         assertFalse(shouldLaunchBrowserIfStoppedSync());
-        mLauncher.launchBrowserIfStopped(true, 0);
-        waitForLaunchBrowserTask();
+        mLauncher.launchBrowserIfStopped(mContext, true, 0);
         assertTrue(shouldLaunchBrowserIfStoppedSync());
-        mLauncher.launchBrowserIfStopped(false, 0);
-        waitForLaunchBrowserTask();
+        mLauncher.launchBrowserIfStopped(mContext, false, 0);
         assertFalse(shouldLaunchBrowserIfStoppedSync());
     }
 
@@ -105,8 +92,7 @@ public class BackgroundSyncLauncherTest extends InstrumentationTestCase {
     @Feature({"BackgroundSync"})
     @RetryOnFailure
     public void testNewLauncherDisablesNextOnline() {
-        mLauncher.launchBrowserIfStopped(true, 0);
-        waitForLaunchBrowserTask();
+        mLauncher.launchBrowserIfStopped(mContext, true, 0);
         assertTrue(shouldLaunchBrowserIfStoppedSync());
 
         // Simulate restarting the browser by deleting the launcher and creating a new one.
