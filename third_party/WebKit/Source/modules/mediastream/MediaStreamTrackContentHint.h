0cf2b26d51a8683c8ad1e3406a00f9b5052542ad
pbos <pbos@chromium.org>
2016-12-14 12:06:28 -0800

    Move MediaStreamTrack contentHint to separate IDL.
    BUG=chromium:653531
    R=esprehn@chromium.org, mcasas@chromium.org
    
    Review-Url: https://codereview.chromium.org/2578623002
    Cr-Commit-Position: refs/heads/master@{#438603}

---
+++ third_party/WebKit/Source/modules/mediastream/MediaStreamTrackContentHint.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MediaStreamTrackContentHint_h
+#define MediaStreamTrackContentHint_h
+
+#include "modules/mediastream/MediaStreamTrack.h"
+
+namespace blink {
+
+class MediaStreamTrackContentHint final {
+ public:
+  static String contentHint(const MediaStreamTrack& track) {
+    return track.contentHint();
+  }
+  static void setContentHint(MediaStreamTrack& track, const String& hint) {
+    track.setContentHint(hint);
+  }
+};
+
+}  // namespace blink
+
+#endif  // MediaStreamTrack_h
