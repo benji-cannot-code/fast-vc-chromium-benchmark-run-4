b9e19e4c7c7226fd6f6ee7235877ce180253a756
robliao <robliao@chromium.org>
2016-12-13 16:51:49 -0800

    Move SetupMetrics() to PreMainMessageLoopRun
    This allows the Browser Task Scheduler to initialize during
    CreateThreads since SetupMetrics() ultimately requires use of the
    blocking pool. The blocking pool has to be redirected by the Browser
    Task Scheduler before this point
    
    Originating Chrome Change:
    http://crrev.com/1892c914719015f06c135127fd1dfd13dec39d33
    
    BUG=662052
    
    Review-Url: https://codereview.chromium.org/2569863003
    Cr-Commit-Position: refs/heads/master@{#438367}

--- ios/chrome/browser/ios_chrome_main_parts.mm
+++ ios/chrome/browser/ios_chrome_main_parts.mm
@@ -134,8 +134,6 @@ void IOSChromeMainParts::PreCreateThreads() {
 
   // Task Scheduler initialization needs to be here for the following reasons:
   //   * After |SetupFieldTrials()|: Initialization uses variations.
-  //   * Before |SetupMetrics()|: |SetupMetrics()| uses the blocking pool. The
-  //         Task Scheduler must do any necessary redirection before then.
   //   * Near the end of |PreCreateThreads()|: The TaskScheduler needs to be
   //         created before any other threads are (by contract) but it creates
   //         threads itself so instantiating it earlier is also incorrect.
@@ -143,7 +141,6 @@ void IOSChromeMainParts::PreCreateThreads() {
   // shutdown call may also need to be moved.
   task_scheduler_util::InitializeDefaultBrowserTaskScheduler();
 
-  SetupMetrics();
 
   // Initialize FieldTrialSynchronizer system.
   field_trial_synchronizer_.reset(new ios::FieldTrialSynchronizer);
@@ -152,6 +149,10 @@ void IOSChromeMainParts::PreCreateThreads() {
 }
 
 void IOSChromeMainParts::PreMainMessageLoopRun() {
+  // This must occur at PreMainMessageLoopRun because |SetupMetrics()| uses the
+  // blocking pool, which is disabled until the CreateThreads phase of startup.
+  SetupMetrics();
+
   // Now that the file thread has been started, start recording.
   StartMetricsRecording();
 
