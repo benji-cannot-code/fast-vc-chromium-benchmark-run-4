c0642e3b53e53baa57facdecd4536efd9316f32d
lgarron <lgarron@chromium.org>
2016-12-14 14:39:08 -0800

    Introduce security_state::ShouldAlwaysShowIcon() for mobile omnibox calculations.
    BUG=673530
    
    Review-Url: https://codereview.chromium.org/2574733003
    Cr-Commit-Position: refs/heads/master@{#438654}

---
+++ components/security_state/core/security_state_ui.cc
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/security_state/core/security_state_ui.h"
+
+namespace security_state {
+
+bool ShouldAlwaysShowIcon(SecurityLevel security_level) {
+  // Enumerate all |SecurityLevel| values for compile-time enforcement that all
+  // cases are explicitly handled.
+  switch (security_level) {
+    case NONE:
+      return false;
+    case HTTP_SHOW_WARNING:
+    case EV_SECURE:
+    case SECURE:
+    case SECURITY_WARNING:
+    case SECURE_WITH_POLICY_INSTALLED_CERT:
+    case DANGEROUS:
+      return true;
+  }
+  NOTREACHED();
+  return false;
+}
+
+}  // namespace security_state
