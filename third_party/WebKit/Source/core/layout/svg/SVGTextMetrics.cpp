fa5cb1828caa8eccb41861d45005524f05873c94
fs <fs@opera.com>
2016-12-14 07:36:43 -0800

    Remove unused SVGTextMetrics constructor
    Review-Url: https://codereview.chromium.org/2565173007
    Cr-Commit-Position: refs/heads/master@{#438523}

--- third_party/WebKit/Source/core/layout/svg/SVGTextMetrics.cpp
+++ third_party/WebKit/Source/core/layout/svg/SVGTextMetrics.cpp
@@ -24,8 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-SVGTextMetrics::SVGTextMetrics() : m_width(0), m_height(0), m_length(0) {}
-
 SVGTextMetrics::SVGTextMetrics(unsigned length, float width, float height)
     : m_width(width), m_height(height), m_length(length) {}
 
