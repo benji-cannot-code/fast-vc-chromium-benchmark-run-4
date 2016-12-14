b6f9eb6ba1c7bbc1a8e4054445ce54ada1c1e8b9
dcastagna <dcastagna@chromium.org>
2016-12-14 15:33:45 -0800

    ozone: Fix use_drm_atomic=true compile error.
    Setting use_drm_atomic to true and compiling chrome is broken.
    |gfx| namespace is not defined in hardware_display_plane_atomic.h.
    
    This CL fixes that.
    
    TEST=use_drm_atomic=true and compile.
    
    Review-Url: https://codereview.chromium.org/2575213002
    Cr-Commit-Position: refs/heads/master@{#438672}

--- ui/ozone/platform/drm/gpu/hardware_display_plane_atomic.h
+++ ui/ozone/platform/drm/gpu/hardware_display_plane_atomic.h
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 #include <xf86drmMode.h>
 
+namespace gfx {
+class Rect;
+}  // namespace gfx
+
 namespace ui {
 
 class CrtcController;
