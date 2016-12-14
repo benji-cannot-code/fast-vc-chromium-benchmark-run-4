c4b6c706cbec7e743ab42aa0d357a51a74d181f9
xdai <xdai@chromium.org>
2016-12-14 15:20:07 -0800

    [Chrome OS] Implement the device wallpaper policy.
    This CL implements the device policy to allow the domain admins to
    set a device level policy controlled wallpaper for an enterprise
    managed device. The device policy wallpaper will display on the login
    screen all the time without requiring a user to log in. The device
    policy wallpaper is also set as a user's wallpaper if there is no user
    policy controlled wallpaper and the user hasn't changed his default
    wallpaper in a user's session.
    
    Detailed description of the expected behavior is described as below:
    
    - In the login screen, if the device wallpaper policy present, use the
    device policy wallpaper, otherwise, check if user-selected wallpaper
    exists, if so, use the user-selected wallpaper, otherwise use OEM
    default wallpaper.
    
    - Within a user session, if the user wallpaper policy present, user the
    user policy wallpaper, otherwise, check if user has changed his default
    wallpaper before, if so, use user-slected wallpaper, otherwise, check
    if the device wallpaper policy present, if so, use the device policy
    wallpaper, otherwise, use OEM default wallpaper.
    
    This is a follow-up CL of https://codereview.chromium.org/2544693002/.
    
    BUG=475998
    
    Review-Url: https://codereview.chromium.org/2572793003
    Cr-Commit-Position: refs/heads/master@{#438667}

--- chrome/browser/chromeos/login/ui/webui_login_display.cc
+++ chrome/browser/chromeos/login/ui/webui_login_display.cc
@@ -228,8 +228,11 @@ void WebUILoginDisplay::LoadWallpaper(const AccountId& account_id) {
 }
 
 void WebUILoginDisplay::LoadSigninWallpaper() {
-  WallpaperManager::Get()->SetDefaultWallpaperDelayed(
-      user_manager::SignInAccountId());
+  if (!WallpaperManager::Get()->SetDeviceWallpaperIfApplicable(
+          user_manager::SignInAccountId())) {
+    WallpaperManager::Get()->SetDefaultWallpaperDelayed(
+        user_manager::SignInAccountId());
+  }
 }
 
 void WebUILoginDisplay::OnSigninScreenReady() {
