348571ff0ebca98124a68476d64c5fa188c7cf59
pkotwicz <pkotwicz@chromium.org>
2016-12-13 20:12:46 -0800

    Make chrome://flags control whether the WebAPK feature is on
    This CL removes the need for special code in variations to allow users to
    experiment with WebAPKs. Since "variations experiments" are time limited, this
    is a good thing.
    
    This CL removes the ability to disable the WebAPK feature through variations.
    This is OK because very few users currently use WebAPKs. In order to use
    WebAPKs users need to turn the feature on via chrome://flags Once we start
    rolling out WebAPKs to all users we will need to add a variations parameter as
    a kill switch
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2544973002
    Cr-Commit-Position: refs/heads/master@{#438425}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -46,6 +46,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kContextualSearchSingleActions,
     &kDownloadsUiFeature,
     &kImportantSitesInCBD,
+    &kImprovedA2HS,
     &kNoCreditCardAbort,
     &kNTPFakeOmniboxTextFeature,
     &kNTPOfflinePagesFeature,
@@ -54,7 +55,6 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kSpecialLocaleFeature,
     &kSpecialLocaleWrapper,
     &kTabReparenting,
-    &kWebApks,
     &kWebPaymentsModifiers,
     &ntp_snippets::kContentSuggestionsFeature,
     &ntp_snippets::kIncreasedVisibility,
@@ -92,6 +92,11 @@ const base::Feature kDownloadsUiFeature{"DownloadsUi",
 const base::Feature kImportantSitesInCBD{"ImportantSitesInCBD",
                                          base::FEATURE_DISABLED_BY_DEFAULT};
 
+// Makes "Add to Home screen" in the app menu generate an APK for the shortcut
+// URL which opens Chrome in fullscreen.
+const base::Feature kImprovedA2HS{"ImprovedA2HS",
+                                  base::FEATURE_DISABLED_BY_DEFAULT};
+
 const base::Feature kNoCreditCardAbort{"NoCreditCardAbort",
                                        base::FEATURE_DISABLED_BY_DEFAULT};
 
@@ -118,10 +123,6 @@ const base::Feature kTabReparenting{"TabReparenting",
 
 const base::Feature kUserMediaScreenCapturing{
     "UserMediaScreenCapturing", base::FEATURE_DISABLED_BY_DEFAULT};
-
-// Makes "Add to Home screen" in the app menu generate an APK for the shortcut
-// URL which opens Chrome in fullscreen.
-const base::Feature kWebApks{"WebApks", base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kWebPaymentsModifiers{"WebPaymentsModifiers",
                                           base::FEATURE_DISABLED_BY_DEFAULT};
