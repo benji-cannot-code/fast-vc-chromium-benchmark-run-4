a35a514cf5c071888831f38f1a6603db53cbbda0
rohitrao <rohitrao@chromium.org>
2016-12-14 12:25:45 -0800

    [ios] Adds code for Omaha and the upgrade center.
    BUG=663349
    
    Review-Url: https://codereview.chromium.org/2568003005
    Cr-Commit-Position: refs/heads/master@{#438607}

---
+++ ios/chrome/browser/upgrade/upgrade_recommended_details.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UPGRADE_UPGRADE_RECOMMENDED_DETAILS_H_
+#define IOS_CHROME_BROWSER_UPGRADE_UPGRADE_RECOMMENDED_DETAILS_H_
+
+#include <string>
+
+#include "url/gurl.h"
+
+struct UpgradeRecommendedDetails {
+  GURL upgrade_url;
+  std::string next_version;
+};
+
+#endif  // IOS_CHROME_BROWSER_UPGRADE_UPGRADE_RECOMMENDED_DETAILS_H_
