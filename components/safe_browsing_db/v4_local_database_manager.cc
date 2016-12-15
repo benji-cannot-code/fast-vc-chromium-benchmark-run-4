c0697b1407c231f14ccb4d8dd986e47e7a137988
nparker <nparker@chromium.org>
2016-12-15 15:31:05 -0800

    Correctly hash the input of MatchModuleWhitelistString() in pver4.
    Also add a test.
    
    BUG=673827
    
    Review-Url: https://codereview.chromium.org/2577963004
    Cr-Commit-Position: refs/heads/master@{#438951}

--- components/safe_browsing_db/v4_local_database_manager.cc
+++ components/safe_browsing_db/v4_local_database_manager.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing_db/v4_feature_list.h"
 #include "components/safe_browsing_db/v4_protocol_manager_util.h"
 #include "content/public/browser/browser_thread.h"
+#include "crypto/sha2.h"
 
 using content::BrowserThread;
 using base::TimeTicks;
@@ -313,8 +314,10 @@ bool V4LocalDatabaseManager::MatchModuleWhitelistString(
     return true;
   }
 
+  // str is the module's filename.  Convert to hash.
+  FullHash hash = crypto::SHA256HashString(str);
   return HandleHashSynchronously(
-      str, StoresToCheck({GetChromeFilenameClientIncidentId()}));
+      hash, StoresToCheck({GetChromeFilenameClientIncidentId()}));
 }
 
 ThreatSource V4LocalDatabaseManager::GetThreatSource() const {
