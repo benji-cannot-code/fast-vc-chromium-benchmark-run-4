5aa29232ce30d6adfa5e30eed1ef9e8923e8e4fb
yhirano <yhirano@chromium.org>
2016-12-14 21:29:14 -0800

    Handle MOJO_RESULT_BUSY result in URLResponseBodyConsumer
    URLResponseBodyConsumer reads data from a mojo data pipe and dispatches it as a
    URLResponseBodyConsumer::ReceivedData. When the dispatched data is destructed,
    mojo::EndReadDataRaw is called. Until then, mojo::BeginRead will return
    MOJO_RESULT_BUSY. This CL implements the correct handling for such a case.
    
    This mis-handling was the cause of the failure of
    virtual/mojo-loading/http/tests/inspector/network/network-fetch.html. This CL
    also adds text() call in makeFetch function so that the backpressure mechanism
    won't stop the loading.
    
    BUG=669357
    
    Review-Url: https://codereview.chromium.org/2573743002
    Cr-Commit-Position: refs/heads/master@{#438756}

--- third_party/WebKit/LayoutTests/http/tests/inspector/network-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/network-test.js
@@ -46,7 +46,11 @@ function makeXHRForJSONArguments(jsonArgs)
 
 function makeFetch(url, requestInitializer)
 {
-    return fetch(url, requestInitializer).catch(e => e);
+    return fetch(url, requestInitializer).then(res => {
+        // Call text(). Otherwise the backpressure mechanism may block loading.
+        res.text();
+        return res;
+    }).catch(e => e);
 }
 
 var initialize_NetworkTest = function() {
