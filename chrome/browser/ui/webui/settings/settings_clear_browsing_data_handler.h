0281dd734699ce4e1ac9a6624c2c027865375e57
dbeam <dbeam@chromium.org>
2016-12-15 13:36:06 -0800

    Define a new policy to disable clearing the browsing history from the UI.
    The pref "history.deleting_enabled" was already present for supervised
    users, so it was only a matter of wiring it to a new policy.
    
    Written by pastarmovj@chromium.org http://crrev.com/2531623002
    Improved by dbeam@chromium.org here.
    
    BUG=401064
    TEST=browser_tests,unit_tests
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2581643002
    Cr-Commit-Position: refs/heads/master@{#438929}

--- chrome/browser/ui/webui/settings/settings_clear_browsing_data_handler.h
+++ chrome/browser/ui/webui/settings/settings_clear_browsing_data_handler.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/settings/settings_page_ui_handler.h"
 #include "components/browser_sync/profile_sync_service.h"
 #include "components/browsing_data/core/counters/browsing_data_counter.h"
-#include "components/prefs/pref_change_registrar.h"
 
 namespace base {
 class ListValue;
@@ -52,9 +51,6 @@ class ClearBrowsingDataHandler : public SettingsPageUIHandler,
   // Called when a clearing task finished. |webui_callback_id| is provided
   // by the WebUI action that initiated it.
   void OnClearingTaskFinished(const std::string& webui_callback_id);
-
-  // Updates UI when the pref to allow clearing history changes.
-  virtual void OnBrowsingHistoryPrefChanged();
 
   // Initializes the dialog UI. Called by JavaScript when the DOM is ready.
   void HandleInitialize(const base::ListValue* args);
@@ -96,9 +92,6 @@ class ClearBrowsingDataHandler : public SettingsPageUIHandler,
   browser_sync::ProfileSyncService* sync_service_;
   ScopedObserver<browser_sync::ProfileSyncService, syncer::SyncServiceObserver>
       sync_service_observer_;
-
-  // Used to listen for pref changes to allow / disallow deleting browsing data.
-  PrefChangeRegistrar profile_pref_registrar_;
 
   // Whether the sentence about other forms of history stored in user's account
   // should be displayed in the footer. This value is retrieved asynchronously,
