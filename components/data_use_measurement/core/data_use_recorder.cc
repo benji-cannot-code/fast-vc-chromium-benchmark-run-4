c3fb7d0141ca21cdda9f3e9b1e6ec551253748df
rajendrant <rajendrant@chromium.org>
2016-12-15 13:04:23 -0800

    Propogate the tab foreground, background change events to data use
    This CL observes WebContents visibility changes to record if a data use
    entity (Chromium tab) is in foreground vs background. Further CLs will
    record the histograms.
    
    BUG=673820
    
    Review-Url: https://codereview.chromium.org/2572823002
    Cr-Commit-Position: refs/heads/master@{#438922}

--- components/data_use_measurement/core/data_use_recorder.cc
+++ components/data_use_measurement/core/data_use_recorder.cc
@@ -9,7 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace data_use_measurement {
 
-DataUseRecorder::DataUseRecorder() : main_url_request_(nullptr) {}
+DataUseRecorder::DataUseRecorder()
+    : main_url_request_(nullptr), is_visible_(false) {}
 
 DataUseRecorder::~DataUseRecorder() {}
 
