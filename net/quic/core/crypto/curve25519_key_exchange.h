478b29587337371f5912eda363adf5b09c19bb84
rch <rch@chromium.org>
2016-12-14 16:10:07 -0800

    QUIC use curve25519 from BoringSSL rather than crypto::
    Merge internal change: 142062136
    
    Review-Url: https://codereview.chromium.org/2577773002
    Cr-Commit-Position: refs/heads/master@{#438686}

--- net/quic/core/crypto/curve25519_key_exchange.h
+++ net/quic/core/crypto/curve25519_key_exchange.h
@@ -6,8 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_QUIC_CORE_CRYPTO_CURVE25519_KEY_EXCHANGE_H_
 #define NET_QUIC_CORE_CRYPTO_CURVE25519_KEY_EXCHANGE_H_
 
-#include <stdint.h>
-
+#include <cstdint>
 #include <string>
 
 #include "base/compiler_specific.h"
