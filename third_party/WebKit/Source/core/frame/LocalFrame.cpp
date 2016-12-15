7a320bf35a4dd06c3560f78c7b0f77f5b5ef3ae4
toyoshim <toyoshim@chromium.org>
2016-12-14 22:25:19 -0800

    FasterLocationReload: use ReloadMainResource for JS exposed reloads
    Now all reloads triggered by user interfaces are replaced by
    ReloadMainResource, but Location.reload() and History.go() still call
    the traditional Reload.
    
    This patch changes them to call ReloadMainResource insteads under a
    runtime flag. This flag will be wired to a field study flag later.
    
    This is discussed at loading-dev@ in the following thread.
    https://groups.google.com/a/chromium.org/d/topic/loading-dev/gD-MPRcfwVA/discussion
    
    BUG=670237
    
    Review-Url: https://codereview.chromium.org/2555963003
    Cr-Commit-Position: refs/heads/master@{#438766}

--- third_party/WebKit/Source/core/frame/LocalFrame.cpp
+++ third_party/WebKit/Source/core/frame/LocalFrame.cpp
@@ -378,7 +378,10 @@ void LocalFrame::reload(FrameLoadType loadType,
     request.setClientRedirect(clientRedirectPolicy);
     m_loader.load(request, loadType);
   } else {
-    DCHECK_EQ(FrameLoadTypeReload, loadType);
+    if (RuntimeEnabledFeatures::fasterLocationReloadEnabled())
+      DCHECK_EQ(FrameLoadTypeReloadMainResource, loadType);
+    else
+      DCHECK_EQ(FrameLoadTypeReload, loadType);
     m_navigationScheduler->scheduleReload();
   }
 }
