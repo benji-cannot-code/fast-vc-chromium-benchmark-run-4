c5079fe4f84b6fa3d4907603f3c50e6fb51b902a
stkhapugin <stkhapugin@chromium.org>
2016-12-15 10:53:23 -0800

    [ObjC ARC] Converts ios/chrome/browser/web:web to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2518583002
    Cr-Commit-Position: refs/heads/master@{#438883}

--- ios/chrome/browser/web/dom_altering_lock.mm
+++ ios/chrome/browser/web/dom_altering_lock.mm
@@ -8,6 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "ios/web/public/web_thread.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 DEFINE_WEB_STATE_USER_DATA_KEY(DOMAlteringLock);
 
 DOMAlteringLock::DOMAlteringLock(web::WebState* web_state) {
