6018b7f4a7a62c724b4877a76462851f6381d1b2
justincarlson <justincarlson@chromium.org>
2016-12-16 10:34:36 -0800

    Base64-decode encoded ppds from the quirksserver, and update tests.
    BUG=674699
    
    Review-Url: https://codereview.chromium.org/2580973002
    Cr-Commit-Position: refs/heads/master@{#439157}

--- chromeos/printing/ppd_provider.cc
+++ chromeos/printing/ppd_provider.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unordered_map>
 #include <utility>
 
+#include "base/base64.h"
 #include "base/bind_helpers.h"
 #include "base/files/file_util.h"
 #include "base/json/json_parser.h"
@@ -309,7 +310,7 @@ class PpdProviderImpl : public PpdProvider {
       ppd_contents->clear();
       return false;
     }
-    return true;
+    return base::Base64Decode(*ppd_contents, ppd_contents);
   }
 
   // Return the ResolveFetchData associated with |source|.
