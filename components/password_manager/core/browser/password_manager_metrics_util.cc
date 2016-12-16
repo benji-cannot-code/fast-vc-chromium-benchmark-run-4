5665c16bb448c8af8fe0d64f770667e88c9843dd
vasilii <vasilii@chromium.org>
2016-12-16 01:24:56 -0800

    Add PasswordManager.SignInPromoCountTil* histograms for the sign-in promo in the password bubble.
    The two histograms replace PasswordManager.SignInPromoCountTilClick. It used to record the same number for both buttons.
    
    BUG=672163
    
    Review-Url: https://codereview.chromium.org/2578923003
    Cr-Commit-Position: refs/heads/master@{#439075}

--- components/password_manager/core/browser/password_manager_metrics_util.cc
+++ components/password_manager/core/browser/password_manager_metrics_util.cc
@@ -100,7 +100,7 @@ void LogAccountChooserUserActionManyAccounts(AccountChooserUserAction action) {
       ACCOUNT_CHOOSER_ACTION_COUNT);
 }
 
-void LogAutoSigninPromoUserAction(SyncSignInUserAction action) {
+void LogSyncSigninPromoUserAction(SyncSignInUserAction action) {
   UMA_HISTOGRAM_ENUMERATION("PasswordManager.SignInPromo", action,
                             CHROME_SIGNIN_ACTION_COUNT);
 }
