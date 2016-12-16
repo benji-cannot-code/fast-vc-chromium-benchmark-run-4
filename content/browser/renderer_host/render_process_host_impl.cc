9b39b1308a58383b193f7105296bf407124b213b
jinho.bang <jinho.bang@samsung.com>
2016-12-16 06:33:58 -0800

    PaymentApp: Introduce PaymentAppDatabase class.
    Write PaymentAppDatabase class and then just move most of logics to access
    database from PaymentAppManager to the new class.
    
    The class is providing APIs to read/write payment app related data(e.g. manifest
    and associated service worker). It can be shared between payment_app_manager.cc
    and payment_app_context.cc.
    
    The payment_app_manager.cc contains a actual implementation of the
    PaymentAppManager interface[1] used on the page or worker.
    Also, the payment_app_context.cc can be used to query all manifests data.
    in order to update payment request UI in Chrome layer.
    
    [1] https://w3c.github.io/webpayments-payment-apps-api/#payment-app-manager
    
    BUG=661608
    
    Review-Url: https://codereview.chromium.org/2572183002
    Cr-Commit-Position: refs/heads/master@{#439100}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1210,7 +1210,7 @@ void RenderProcessHostImpl::RegisterMojoInterfaces() {
 
   AddUIThreadInterface(
       registry.get(),
-      base::Bind(&PaymentAppContextImpl::CreateService,
+      base::Bind(&PaymentAppContextImpl::CreatePaymentAppManager,
                  base::Unretained(
                      storage_partition_impl_->GetPaymentAppContext())));
 
