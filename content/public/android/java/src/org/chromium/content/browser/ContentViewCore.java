277e49a3229a81d18d793a1643079ae89583dd50
amaralp <amaralp@chromium.org>
2016-12-15 23:38:29 -0800

    Removing SelectionPopupController.updateActionModeVisibility()
    SelectionPopupController.updateActionModeVisibility() is confusing and unnecessary.
    This patch refactors code to remove it.
    
    This shouldn't have any behavior changes because
    |ContentViewCore.setTouchScrollInProgress()| can't be called if the selection is
    being dragged. Likewise when the selection handle dragging stops there shouldn't be
    a touch scroll in progress.
    
    Review-Url: https://codereview.chromium.org/2581863002
    Cr-Commit-Position: refs/heads/master@{#439062}

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -1111,7 +1111,7 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Displa
     private void setTouchScrollInProgress(boolean inProgress) {
         if (mTouchScrollInProgress == inProgress) return;
         mTouchScrollInProgress = inProgress;
-        mSelectionPopupController.updateActionModeVisibility(inProgress);
+        mSelectionPopupController.hideActionMode(inProgress);
     }
 
     @SuppressWarnings("unused")
