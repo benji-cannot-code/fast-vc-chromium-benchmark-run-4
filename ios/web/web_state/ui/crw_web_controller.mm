dbcba7a7764f3c4e163e096804d286d0dad7d207
mastiz <mastiz@chromium.org>
2016-12-16 01:49:34 -0800

    Disallow [in]equality operator for PageTransition
    Direct comparison of the enum is error-prone since users can easily
    forget about the qualifier bits. The available helper functions should
    be used instead as documented in the comment.
    
    Most occurrences were addressed in earlier patches and a few leftovers
    are updated here, together with the change in page_transition_types.h
    that causes compiler errors if [in]equality operator is used for the
    enum.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2562993002
    Cr-Commit-Position: refs/heads/master@{#439079}

--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -3401,7 +3401,8 @@ const NSTimeInterval kSnapshotOverlayTransition = 0.5;
       !_webStateImpl->GetNavigationManager()->GetItemCount();
   BOOL isPossibleLinkClick = [self isLinkNavigation:action.navigationType];
   if (isPossibleLinkClick || isOpenInNewTabNavigation ||
-      [self currentTransition] == ui::PAGE_TRANSITION_AUTO_BOOKMARK) {
+      PageTransitionCoreTypeIs([self currentTransition],
+                               ui::PAGE_TRANSITION_AUTO_BOOKMARK)) {
     // Check If the URL is handled by a native app.
     if ([self urlTriggersNativeAppLaunch:requestURL
                                sourceURL:[self currentNavigationURL]
