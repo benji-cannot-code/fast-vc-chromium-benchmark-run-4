c2b0eae3ebc4c74d4fe481fdcf8441a6ee71cf50
zqzhang <zqzhang@chromium.org>
2016-12-16 13:58:06 -0800

    Follow-up fix for leaking mock object in AutoplayUmaHelperTest
    A previous attempt for fixing AutoplayUmaHelperTest uses
    ::testing::Mock::VerifyAndClear to clean up the expectations on the
    mock. However it does not ignore the mock object to be leaked. This CL
    solves the issue by calling ::testing::Mock::AllowLeak in SetUp().
    
    BUG=672831
    
    Review-Url: https://codereview.chromium.org/2580003002
    Cr-Commit-Position: refs/heads/master@{#439205}

--- third_party/WebKit/Source/core/html/AutoplayUmaHelperTest.cpp
+++ third_party/WebKit/Source/core/html/AutoplayUmaHelperTest.cpp
@@ -58,6 +58,7 @@ class AutoplayUmaHelperTest : public testing::Test {
     HTMLMediaElement& element = mediaElement();
     m_umaHelper = new MockAutoplayUmaHelper(&element);
     element.m_autoplayUmaHelper = m_umaHelper;
+    ::testing::Mock::AllowLeak(&umaHelper());
   }
 
   void TearDown() override { m_umaHelper.clear(); }
