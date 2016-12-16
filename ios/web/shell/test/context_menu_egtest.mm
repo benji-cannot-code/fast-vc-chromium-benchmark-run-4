2163931c46733f35d17122658795ec382084be72
baxley <baxley@chromium.org>
2016-12-16 12:48:16 -0800

    Disable EarlGrey web shell tests that fail on device.
    7 tests, at least some of which use context menus, are
    failing when run on devices.
    
    BUG=675015
    
    Review-Url: https://codereview.chromium.org/2583683004
    Cr-Commit-Position: refs/heads/master@{#439179}

--- ios/web/shell/test/context_menu_egtest.mm
+++ ios/web/shell/test/context_menu_egtest.mm
@@ -34,8 +34,14 @@ using testing::elementToDismissContextMenu;
 
 @implementation ContextMenuTestCase
 
+// TODO(crbug.com/675015): Re-enable this test on device.
+#if TARGET_IPHONE_SIMULATOR
+#define MAYBE_testContextMenu testContextMenu
+#else
+#define MAYBE_testContextMenu FLAKY_testContextMenu
+#endif
 // Tests context menu appears on a regular link.
-- (void)testContextMenu {
+- (void)MAYBE_testContextMenu {
   // Create map of canned responses and set up the test HTML server.
   std::map<GURL, std::string> responses;
   GURL initialURL = web::test::HttpServer::MakeUrl("http://contextMenuOpen");
@@ -70,8 +76,16 @@ using testing::elementToDismissContextMenu;
   [[EarlGrey selectElementWithMatcher:copyItem] assertWithMatcher:grey_nil()];
 }
 
+// TODO(crbug.com/675015): Re-enable this test on device.
+#if TARGET_IPHONE_SIMULATOR
+#define MAYBE_testContextMenuWebkitTouchCalloutNone \
+  testContextMenuWebkitTouchCalloutNone
+#else
+#define MAYBE_testContextMenuWebkitTouchCalloutNone \
+  FLAKY_testContextMenuWebkitTouchCalloutNone
+#endif
 // Tests context menu on element that has WebkitTouchCallout set to none.
-- (void)testContextMenuWebkitTouchCalloutNone {
+- (void)MAYBE_testContextMenuWebkitTouchCalloutNone {
   // Create map of canned responses and set up the test HTML server.
   std::map<GURL, std::string> responses;
   GURL initialURL =
@@ -99,9 +113,17 @@ using testing::elementToDismissContextMenu;
   [[EarlGrey selectElementWithMatcher:copyItem] assertWithMatcher:grey_nil()];
 }
 
+// TODO(crbug.com/675015): Re-enable this test on device.
+#if TARGET_IPHONE_SIMULATOR
+#define MAYBE_testContextMenuWebkitTouchCalloutNoneFromAncestor \
+  testContextMenuWebkitTouchCalloutNoneFromAncestor
+#else
+#define MAYBE_testContextMenuWebkitTouchCalloutNoneFromAncestor \
+  FLAKY_testContextMenuWebkitTouchCalloutNoneFromAncestor
+#endif
 // Tests context menu on element that has WebkitTouchCallout set to none from an
 // ancestor.
-- (void)testContextMenuWebkitTouchCalloutNoneFromAncestor {
+- (void)MAYBE_testContextMenuWebkitTouchCalloutNoneFromAncestor {
   // Create map of canned responses and set up the test HTML server.
   std::map<GURL, std::string> responses;
   GURL initialURL =
@@ -130,9 +152,17 @@ using testing::elementToDismissContextMenu;
   [[EarlGrey selectElementWithMatcher:copyItem] assertWithMatcher:grey_nil()];
 }
 
+// TODO(crbug.com/675015): Re-enable this test on device.
+#if TARGET_IPHONE_SIMULATOR
+#define MAYBE_testContextMenuWebkitTouchCalloutOverride \
+  testContextMenuWebkitTouchCalloutOverride
+#else
+#define MAYBE_testContextMenuWebkitTouchCalloutOverride \
+  FLAKY_testContextMenuWebkitTouchCalloutOverride
+#endif
 // Tests context menu on element that has WebkitTouchCallout set to none from an
 // ancestor and overridden.
-- (void)testContextMenuWebkitTouchCalloutOverride {
+- (void)MAYBE_testContextMenuWebkitTouchCalloutOverride {
   // Create map of canned responses and set up the test HTML server.
   std::map<GURL, std::string> responses;
   GURL initialURL =
