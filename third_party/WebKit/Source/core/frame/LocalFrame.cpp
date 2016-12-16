80ccc7836bbc5ee986596d634747ce263399e36c
xiaochengh <xiaochengh@chromium.org>
2016-12-15 22:12:55 -0800

    Add IdleSpellCheckCallback to LocalFrame
    This patch makes each LocalFrame own an IdleSpellCheckCallback. Future
    patches will make this callback handle spell checking in the frame when
    IdleTimeSpellChecking is enabled.
    
    BUG=517298
    
    Review-Url: https://codereview.chromium.org/2581063002
    Cr-Commit-Position: refs/heads/master@{#439042}

--- third_party/WebKit/Source/core/frame/LocalFrame.cpp
+++ third_party/WebKit/Source/core/frame/LocalFrame.cpp
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/FrameSelection.h"
 #include "core/editing/InputMethodController.h"
 #include "core/editing/serializers/Serialization.h"
+#include "core/editing/spellcheck/IdleSpellCheckCallback.h"
 #include "core/editing/spellcheck/SpellChecker.h"
 #include "core/events/Event.h"
 #include "core/fetch/ResourceFetcher.h"
@@ -346,6 +347,7 @@ DEFINE_TRACE(LocalFrame) {
   visitor->trace(m_eventHandler);
   visitor->trace(m_console);
   visitor->trace(m_inputMethodController);
+  visitor->trace(m_idleSpellCheckCallback);
   Frame::trace(visitor);
   Supplementable<LocalFrame>::trace(visitor);
 }
@@ -886,6 +888,7 @@ inline LocalFrame::LocalFrame(FrameLoaderClient* client,
       m_eventHandler(new EventHandler(*this)),
       m_console(FrameConsole::create(*this)),
       m_inputMethodController(InputMethodController::create(*this)),
+      m_idleSpellCheckCallback(IdleSpellCheckCallback::create(*this)),
       m_navigationDisableCount(0),
       m_pageZoomFactor(parentPageZoomFactor(this)),
       m_textZoomFactor(parentTextZoomFactor(this)),
--- third_party/WebKit/Source/core/frame/LocalFrame.h
+++ third_party/WebKit/Source/core/frame/LocalFrame.h
@@ -59,6 +59,7 @@ class FloatSize;
 class FrameConsole;
 class FrameSelection;
 class FrameView;
+class IdleSpellCheckCallback;
 class InputMethodController;
 class InstrumentingAgents;
 class InterfaceProvider;
@@ -145,6 +146,7 @@ class CORE_EXPORT LocalFrame final : public Frame,
   ScriptController& script() const;
   SpellChecker& spellChecker() const;
   FrameConsole& console() const;
+  IdleSpellCheckCallback& idleSpellCheckCallback() const;
 
   // This method is used to get the highest level LocalFrame in this
   // frame's in-process subtree.
@@ -259,6 +261,7 @@ class CORE_EXPORT LocalFrame final : public Frame,
   const Member<EventHandler> m_eventHandler;
   const Member<FrameConsole> m_console;
   const Member<InputMethodController> m_inputMethodController;
+  const Member<IdleSpellCheckCallback> m_idleSpellCheckCallback;
 
   int m_navigationDisableCount;
 
@@ -326,6 +329,11 @@ inline void LocalFrame::setInViewSourceMode(bool mode) {
 inline EventHandler& LocalFrame::eventHandler() const {
   ASSERT(m_eventHandler);
   return *m_eventHandler;
+}
+
+inline IdleSpellCheckCallback& LocalFrame::idleSpellCheckCallback() const {
+  DCHECK(m_idleSpellCheckCallback);
+  return *m_idleSpellCheckCallback;
 }
 
 DEFINE_TYPE_CASTS(LocalFrame,
