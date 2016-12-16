6faf1bcf732f0a194d8c69d7dfa25c7bd9f5d5d5
tasak <tasak@google.com>
2016-12-16 02:18:25 -0800

    Remove is_renderer_suspended_.
    Since tab_manager manages whether a renderer is suspended or not, there is no need to keep the state in RenderThreadImpl.
    
    BUG=607077
    
    Review-Url: https://codereview.chromium.org/2564833002
    Cr-Commit-Position: refs/heads/master@{#439083}

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -917,7 +917,6 @@ void RenderThreadImpl::Init(
 
   record_purge_suspend_metric_closure_.Reset(base::Bind(
       &RenderThreadImpl::RecordPurgeAndSuspendMetrics, base::Unretained(this)));
-  is_renderer_suspended_ = false;
 
   base::MemoryCoordinatorClientRegistry::GetInstance()->Register(this);
 
@@ -1780,16 +1779,13 @@ void RenderThreadImpl::OnProcessBackgrounded(bool backgrounded) {
     record_purge_suspend_metric_closure_.Reset(
         base::Bind(&RenderThreadImpl::RecordPurgeAndSuspendMetrics,
                    base::Unretained(this)));
-    is_renderer_suspended_ = false;
   }
 }
 
 void RenderThreadImpl::OnProcessPurgeAndSuspend() {
   ChildThreadImpl::OnProcessPurgeAndSuspend();
-  DCHECK(!is_renderer_suspended_);
   if (!RendererIsHidden())
     return;
-  is_renderer_suspended_ = true;
   if (base::FeatureList::IsEnabled(features::kPurgeAndSuspend)) {
     // TODO(tasak): After enabling MemoryCoordinator, remove this Notify
     // and follow MemoryCoordinator's request.
@@ -1848,7 +1844,7 @@ static size_t GetMallocUsage() {
 // before and after purging by using memory-infra.
 void RenderThreadImpl::RecordPurgeAndSuspendMetrics() const {
   // If this renderer is resumed, we should not update UMA.
-  if (!is_renderer_suspended_)
+  if (!RendererIsHidden())
     return;
 
   // TODO(tasak): Compare memory metrics between purge-enabled renderers and
@@ -1899,8 +1895,8 @@ void RenderThreadImpl::RecordPurgeAndSuspendMetrics() const {
 void RenderThreadImpl::OnProcessResume() {
   ChildThreadImpl::OnProcessResume();
 
-  DCHECK(is_renderer_suspended_);
-  is_renderer_suspended_ = false;
+  if (!RendererIsHidden())
+    return;
   if (base::FeatureList::IsEnabled(features::kPurgeAndSuspend)) {
     // TODO(tasak): after enabling MemoryCoordinator, remove this Notify
     // and follow MemoryCoordinator's request.
