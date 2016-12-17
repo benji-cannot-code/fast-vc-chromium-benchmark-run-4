b6ea1b532dcbbe7c7a7c0f3cf080ad5fcd5dee44
zhongyi <zhongyi@chromium.org>
2016-12-16 16:08:01 -0800

    Pass pref_task_runner through HttpServerPropertiesManager's cxtor so that tests could inject a TestMockTimeTaskRunner. HttpServerPropertiesManager used to get pref_task_runner by calling base::ThreadTaskRunnerHandle::Get() itself, now changes to take the task runner passed in. No functional change.
    There are two SingleThreadTaskRunners in HttpServerPropertiesManager:
    pref_task_runner: which is used to post cache update on pref thread, and should be registered with pref_cache_update_timer_;
    network_task_runner: which is used to post pref update on network thread, and should be registered with network_prefs_update_timer.
    
    BUG=670519
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_cronet_tester
    
    Review-Url: https://codereview.chromium.org/2567893002
    Cr-Commit-Position: refs/heads/master@{#439242}

--- chrome/browser/net/http_server_properties_manager_factory.cc
+++ chrome/browser/net/http_server_properties_manager_factory.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/net/http_server_properties_manager_factory.h"
 
+#include "base/threading/thread_task_runner_handle.h"
 #include "chrome/common/pref_names.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_change_registrar.h"
@@ -64,6 +65,7 @@ HttpServerPropertiesManagerFactory::CreateManager(PrefService* pref_service) {
   using content::BrowserThread;
   return new net::HttpServerPropertiesManager(
       new PrefServiceAdapter(pref_service),  // Transfers ownership.
+      base::ThreadTaskRunnerHandle::Get(),
       BrowserThread::GetTaskRunnerForThread(BrowserThread::IO));
 }
 
