d42ca5db11706be3bf2222ff3188289a43a93861
John Abd-El-Malek <jam@chromium.org>
2016-12-17 12:38:50 -0800

    Temporarily enable PlzNavigate to see perf bot results.
    BUG=368813
    TBR=clamy@chromium.org
    
    Review-Url: https://codereview.chromium.org/2584093003 .
    Cr-Commit-Position: refs/heads/master@{#439350}

--- content/public/common/browser_side_navigation_policy.cc
+++ content/public/common/browser_side_navigation_policy.cc
@@ -11,8 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 bool IsBrowserSideNavigationEnabled() {
-  return base::CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kEnableBrowserSideNavigation);
+  return true;
 }
 
 }  // namespace content
