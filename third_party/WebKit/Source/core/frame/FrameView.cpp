54dc9caef449797830e14d0299fc820c66de948b
pdr <pdr@chromium.org>
2016-12-16 20:24:14 -0800

    Update paint properties on FrameView visibility changes
    When a FrameView becomes hidden, we need to update scroll paint properties to
    ensure scrollability based on hit testing is accounted for. This patch fixes
    the FrameView-specific bug. https://crbug.com/675296 has been filed to
    address stale scrollability values which is a remaining bug for LayoutView.
    
    TEST=scrollingcoordinator/non-fast-scrollable-visibility-change.html with
    --additional-driver-flag=--enable-slimming-paint-invalidation
    
    BUG=645667
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2579363003
    Cr-Commit-Position: refs/heads/master@{#439317}

--- third_party/WebKit/Source/core/frame/FrameView.cpp
+++ third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -4428,6 +4428,11 @@ void FrameView::show() {
       scrollingCoordinator->frameViewVisibilityDidChange();
     setNeedsCompositingUpdate(layoutViewItem(), CompositingUpdateRebuildTree);
     updateParentScrollableAreaSet();
+    if (RuntimeEnabledFeatures::slimmingPaintInvalidationEnabled()) {
+      // The existance of scrolling properties depends on visibility through
+      // isScrollable() so ensure properties are updated if visibility changes.
+      setNeedsPaintPropertyUpdate();
+    }
     if (isParentVisible()) {
       for (const auto& child : m_children)
         child->setParentVisible(true);
@@ -4449,6 +4454,11 @@ void FrameView::hide() {
       scrollingCoordinator->frameViewVisibilityDidChange();
     setNeedsCompositingUpdate(layoutViewItem(), CompositingUpdateRebuildTree);
     updateParentScrollableAreaSet();
+    if (RuntimeEnabledFeatures::slimmingPaintInvalidationEnabled()) {
+      // The existance of scrolling properties depends on visibility through
+      // isScrollable() so ensure properties are updated if visibility changes.
+      setNeedsPaintPropertyUpdate();
+    }
   }
 
   Widget::hide();
