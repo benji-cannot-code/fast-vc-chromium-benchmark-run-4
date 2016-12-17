024349fe0021d088a7eccb794695a7fd0231248d
csharrison <csharrison@chromium.org>
2016-12-16 18:19:42 -0800

    Merge logic for SameDomainOrHost for GURLs and Origins
    Previously, we used to just call:
    SameDomainOrHost(origin1.GetURL(), origin2.GetURL())
    
    This isn't necessary, as we have all the information in the Origin itself.
    Additionally, GetURL() isn't free. We do a bunch of string copies,
    allocations, and some non-cheap policy decisions.
    
    This patch moves GURL::HostIsIPAddress logic to url_util, and reuses
    that code for both GURLs and Origins.
    
    BUG=664174,348655
    
    Review-Url: https://codereview.chromium.org/2562813003
    Cr-Commit-Position: refs/heads/master@{#439287}

--- url/url_util.h
+++ url/url_util.h
@@ -134,7 +134,7 @@ URL_EXPORT bool GetStandardSchemeType(const char* spec,
                                       const Component& scheme,
                                       SchemeType* type);
 
-// Domains ---------------------------------------------------------------------
+// Hosts  ----------------------------------------------------------------------
 
 // Returns true if the |canonicalized_host| matches or is in the same domain as
 // the given |lower_ascii_domain| string. For example, if the canonicalized
@@ -146,6 +146,10 @@ URL_EXPORT bool GetStandardSchemeType(const char* spec,
 // canonicalized host.
 URL_EXPORT bool DomainIs(base::StringPiece canonicalized_host,
                          base::StringPiece lower_ascii_domain);
+
+// Returns true if the hostname is an IP address. Note: this function isn't very
+// cheap, as it must re-parse the host to verify.
+URL_EXPORT bool HostIsIPAddress(base::StringPiece host);
 
 // URL library wrappers --------------------------------------------------------
 
