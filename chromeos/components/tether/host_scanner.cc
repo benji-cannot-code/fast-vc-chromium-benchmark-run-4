50792ae366b9e922a06466316c62f4b3da026ebc
khorimoto <khorimoto@chromium.org>
2016-12-16 19:29:52 -0800

    CrOS Tether: Create HostScanScheduler, which schedules scans for devices which can serve as tether hotspots.
    Scans can be triggered manually (via a function call) or automatically (when conditions change such that a scan should be performed). These conditions are:
    (1) Must be logged in and active.
    (2) Must not have a current Internet connection.
    (3) Must have synced CryptAuth tether host devices.
    
    Note that this CL does not integrate this code yet. In order to do that, a CryptAuthService must be created to provide the CryptAuthDeviceManager. Once that code is in place, the scheduler will be created an initialized by the Initializer.
    
    BUG=672263
    
    Review-Url: https://codereview.chromium.org/2564653004
    Cr-Commit-Position: refs/heads/master@{#439307}

---
+++ chromeos/components/tether/host_scanner.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/components/tether/host_scanner.h"
+
+namespace chromeos {
+
+namespace tether {
+
+HostScanner::HostScanner() {}
+
+HostScanner::~HostScanner() {}
+
+void HostScanner::StartScan() {}
+
+}  // namespace tether
+
+}  // namespace chromeos
---
+++ chromeos/components/tether/host_scanner.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_COMPONENTS_TETHER_HOST_SCANNER_H
+#define CHROMEOS_COMPONENTS_TETHER_HOST_SCANNER_H
+
+namespace chromeos {
+
+namespace tether {
+
+// Scans for nearby tether hosts.
+// TODO(khorimoto): Implement.
+class HostScanner {
+ public:
+  HostScanner();
+  virtual ~HostScanner();
+
+  virtual void StartScan();
+};
+
+}  // namespace tether
+
+}  // namespace chromeos
+
+#endif  // CHROMEOS_COMPONENTS_TETHER_HOST_SCANNER_H
