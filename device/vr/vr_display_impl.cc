a1d3861d8efb9c6b819d8f4dadddaf5e862208a1
mthiesse <mthiesse@chromium.org>
2016-12-16 23:35:55 -0800

    Fix leak in VRServiceImpl
    This makes the implicit Strong Binding between the service and clients explicit, and fixes the service leaking when the binding is closed.
    
    No functional changes here.
    
    BUG=651245
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    #TBR-ing a simple rename to match style.
    TBR=brettw@chromium.org
    
    Review-Url: https://codereview.chromium.org/2571323002
    Cr-Commit-Position: refs/heads/master@{#439333}

--- device/vr/vr_display_impl.cc
+++ device/vr/vr_display_impl.cc
@@ -24,7 +24,9 @@ VRDisplayImpl::VRDisplayImpl(device::VRDevice* device, VRServiceImpl* service)
   }
 }
 
-VRDisplayImpl::~VRDisplayImpl() {}
+VRDisplayImpl::~VRDisplayImpl() {
+  device_->RemoveDisplay(this);
+}
 
 void VRDisplayImpl::GetPose(const GetPoseCallback& callback) {
   if (!device_->IsAccessAllowed(this)) {
