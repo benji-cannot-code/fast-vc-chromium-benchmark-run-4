894b23c5f9d79f6d34aee7060e925dbd139dc8b4
jcliang <jcliang@chromium.org>
2016-12-16 20:45:23 -0800

    Add PIXEL_FORMAT_I422.
    The Mediatek JPEG decoder uses the multi-planar buffer format
    V4L2_PIX_FMT_YUV422M.
    
    (https://linuxtv.org/downloads/v4l-dvb-apis/uapi/v4l/pixfmt-yuv422m.html)
    
    Apart from how the buffer is allocated the format is basically
    idential to I422 (a.k.a YU16).
    
    BUG=chrome-os-partner:43703
    TEST=Build chrome and test with jpeg decoder that uses I422 format.
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2571163002
    Cr-Commit-Position: refs/heads/master@{#439324}

--- media/base/video_frame.cc
+++ media/base/video_frame.cc
@@ -124,6 +124,7 @@ bool RequiresEvenSizeAllocation(VideoPixelFormat format) {
     case PIXEL_FORMAT_YUV444P12:
     case PIXEL_FORMAT_YV12A:
     case PIXEL_FORMAT_UYVY:
+    case PIXEL_FORMAT_I422:
       return true;
     case PIXEL_FORMAT_UNKNOWN:
       break;
@@ -158,7 +159,7 @@ bool VideoFrame::IsValidConfig(VideoPixelFormat format,
     return true;
 
   // Make sure new formats are properly accounted for in the method.
-  static_assert(PIXEL_FORMAT_MAX == 26,
+  static_assert(PIXEL_FORMAT_MAX == 27,
                 "Added pixel format, please review IsValidConfig()");
 
   if (format == PIXEL_FORMAT_UNKNOWN) {
@@ -528,6 +529,7 @@ size_t VideoFrame::NumPlanes(VideoPixelFormat format) {
     case PIXEL_FORMAT_YUV420P12:
     case PIXEL_FORMAT_YUV422P12:
     case PIXEL_FORMAT_YUV444P12:
+    case PIXEL_FORMAT_I422:
       return 3;
     case PIXEL_FORMAT_YV12A:
       return 4;
@@ -1030,6 +1032,7 @@ gfx::Size VideoFrame::SampleSize(VideoPixelFormat format, size_t plane) {
           return gfx::Size(1, 1);
 
         case PIXEL_FORMAT_YV16:
+        case PIXEL_FORMAT_I422:
         case PIXEL_FORMAT_YUV422P9:
         case PIXEL_FORMAT_YUV422P10:
         case PIXEL_FORMAT_YUV422P12:
@@ -1098,6 +1101,7 @@ int VideoFrame::BytesPerElement(VideoPixelFormat format, size_t plane) {
     case PIXEL_FORMAT_YV12A:
     case PIXEL_FORMAT_YV24:
     case PIXEL_FORMAT_Y8:
+    case PIXEL_FORMAT_I422:
       return 1;
     case PIXEL_FORMAT_MJPEG:
       return 0;
--- media/remoting/rpc/proto_enum_utils.cc
+++ media/remoting/rpc/proto_enum_utils.cc
@@ -336,6 +336,7 @@ base::Optional<::media::VideoPixelFormat> ToMediaVideoPixelFormat(
     CASE_RETURN_OTHER(PIXEL_FORMAT_YUV444P12);
     CASE_RETURN_OTHER(PIXEL_FORMAT_Y8);
     CASE_RETURN_OTHER(PIXEL_FORMAT_Y16);
+    CASE_RETURN_OTHER(PIXEL_FORMAT_I422);
   }
   return base::nullopt;  // Not a 'default' to ensure compile-time checks.
 }
@@ -372,6 +373,7 @@ base::Optional<pb::VideoDecoderConfig::Format> ToProtoVideoDecoderConfigFormat(
     CASE_RETURN_OTHER(PIXEL_FORMAT_YUV444P12);
     CASE_RETURN_OTHER(PIXEL_FORMAT_Y8);
     CASE_RETURN_OTHER(PIXEL_FORMAT_Y16);
+    CASE_RETURN_OTHER(PIXEL_FORMAT_I422);
   }
   return base::nullopt;  // Not a 'default' to ensure compile-time checks.
 }
