8a5aab5b8c5085b4805aa0564685d1b936f0ca63
glebl <glebl@chromium.org>
2016-12-17 13:50:06 -0800

    Remove obsolete NGConstraintSpace::ChildSpace
    After http://crrev.com/2562703003 NGConstraintSpace::ChildSpace became obsolete. Therefore this patch removes it.
    
    BUG=635619
    
    Review-Url: https://codereview.chromium.org/2582913002
    Cr-Commit-Position: refs/heads/master@{#439354}

--- third_party/WebKit/Source/core/layout/ng/ng_constraint_space.h
+++ third_party/WebKit/Source/core/layout/ng/ng_constraint_space.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class ComputedStyle;
 class LayoutBox;
 class NGFragment;
 class NGLayoutOpportunityIterator;
@@ -111,7 +110,6 @@ class CORE_EXPORT NGConstraintSpace final
 
   DEFINE_INLINE_VIRTUAL_TRACE() {}
 
-  NGConstraintSpace* ChildSpace(const ComputedStyle* style) const;
   String ToString() const;
 
  private:
