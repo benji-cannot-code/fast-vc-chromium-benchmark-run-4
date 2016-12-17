3d929f6b5c8c9436cbbedce241585a2fdfe63b20
rdevlin.cronin <rdevlin.cronin@chromium.org>
2016-12-16 17:53:46 -0800

    [Extensions Bindings] Make an APISignature class
    Future hooks (like updateArgumentsPostValidate) will require some
    tweaking of how we parse arguments. The argument parsing is involved
    enough to warrant being in its own file, rather than continuing to
    expand APIBinding. Move it out.
    
    This CL is purely a mechanical refactor and should have no behavior
    changes.
    
    BUG=653596
    
    Review-Url: https://codereview.chromium.org/2582093002
    Cr-Commit-Position: refs/heads/master@{#439283}

--- extensions/renderer/api_binding_types.h
+++ extensions/renderer/api_binding_types.h
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "v8/include/v8.h"
 
 namespace extensions {
-class ArgumentSpec;
-
 namespace binding {
 
 // A callback to execute the given v8::Function with the provided context and
@@ -23,8 +21,6 @@ using RunJSFunction = base::Callback<void(v8::Local<v8::Function>,
                                           v8::Local<v8::Context>,
                                           int argc,
                                           v8::Local<v8::Value>[])>;
-
-using APISignature = std::vector<std::unique_ptr<ArgumentSpec>>;
 
 }  // namespace binding
 }  // namespace extensions
