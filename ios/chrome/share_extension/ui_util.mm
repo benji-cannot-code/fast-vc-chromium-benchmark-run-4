6fe75537efd04743b8324eadd6d0811f7910020f
sdefresne <sdefresne@chromium.org>
2016-12-17 16:06:01 -0800

    Upstream code and resources for Chrome on iOS extensions.
    Upstream all code and resources for Chrome on iOS today and share
    extensions.
    
    BUG=534433
    
    Review-Url: https://codereview.chromium.org/2586713002
    Cr-Commit-Position: refs/heads/master@{#439357}

---
+++ ios/chrome/share_extension/ui_util.mm
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <UIKit/UIKit.h>
+
+#include "ios/chrome/share_extension/ui_util.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+namespace ui_util {
+
+CGFloat AlignValueToPixel(CGFloat value) {
+  CGFloat scale = [[UIScreen mainScreen] scale];
+  return floor(value * scale) / scale;
+}
+
+void ConstrainAllSidesOfViewToView(UIView* container, UIView* filler) {
+  [NSLayoutConstraint activateConstraints:@[
+    [filler.leadingAnchor constraintEqualToAnchor:container.leadingAnchor],
+    [filler.trailingAnchor constraintEqualToAnchor:container.trailingAnchor],
+    [filler.topAnchor constraintEqualToAnchor:container.topAnchor],
+    [filler.bottomAnchor constraintEqualToAnchor:container.bottomAnchor],
+  ]];
+}
+
+}  // namespace ui_util
