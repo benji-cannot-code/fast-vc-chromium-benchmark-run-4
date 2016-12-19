81b5e24414e61cb568d42b40fbee7afdf657c694
toyoshim <toyoshim@chromium.org>
2016-12-19 00:35:47 -0800

    FasterLocationReload: Add a field trial flag to enable the feature
    The feature is planned to be enabled step by step through a server side
    configuration. This patch add a field trial flag and plumb it to the
    Blink runtime flag to enable it.
    
    BUG=670237
    
    Review-Url: https://codereview.chromium.org/2572063002
    Cr-Commit-Position: refs/heads/master@{#439434}

--- third_party/WebKit/Source/web/WebRuntimeFeatures.cpp
+++ third_party/WebKit/Source/web/WebRuntimeFeatures.cpp
@@ -127,6 +127,10 @@ void WebRuntimeFeatures::enableExperimentalCanvasFeatures(bool enable) {
   RuntimeEnabledFeatures::setExperimentalCanvasFeaturesEnabled(enable);
 }
 
+void WebRuntimeFeatures::enableFasterLocationReload(bool enable) {
+  RuntimeEnabledFeatures::setFasterLocationReloadEnabled(enable);
+}
+
 void WebRuntimeFeatures::enableFastMobileScrolling(bool enable) {
   RuntimeEnabledFeatures::setFastMobileScrollingEnabled(enable);
 }
