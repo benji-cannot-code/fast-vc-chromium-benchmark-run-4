b0d2db2f247ad0c969feacfe0d503449f7215f4b
tkent <tkent@chromium.org>
2016-12-18 20:58:51 -0800

    Revert of Extend CopyTextureCHROMIUM to more ES 3.0 texture formats. (patchset #27 id:620001 of https://codereview.chromium.org/2479513002/ )
    Reason for revert:
    Caused crashes in dozens of layout tests.
    https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux%20Trusty/builds/21138
    
    Original issue's description:
    > Extend CopyTextureCHROMIUM to more ES 3.0 texture formats.
    >
    > Add support for the following copying techniques:
    >
    > 1) Using CopyTexImage2D when the source is color-renderable.
    >
    > 2) Drawing a quad when the destination is color-renderable.
    >
    > 3) Drawing to an intermediate texture, and copying from that
    > intermediate texture to the destination, when neither is
    > color-renderable.
    >
    > Add support for nearly all of the new ES 3.0 texture formats.
    >
    > Follow-on work remains, including some scenarios described in the bug,
    > such as copying to faces of currently-incomplete cube maps.
    >
    > BUG=612542
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    >
    > Committed: https://crrev.com/bcf132827ebd0436fd3620bbda4cbcf3febdc161
    > Cr-Commit-Position: refs/heads/master@{#439312}
    
    TBR=kbr@chromium.org,zmo@chromium.org,yunchao.he@intel.com,qiankun.miao@intel.com
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=612542
    
    Review-Url: https://codereview.chromium.org/2589613002
    Cr-Commit-Position: refs/heads/master@{#439411}

--- gpu/command_buffer/service/texture_manager.h
+++ gpu/command_buffer/service/texture_manager.h
@@ -326,10 +326,6 @@ class GPU_EXPORT Texture final : public TextureBase {
 
   bool EmulatingRGB();
 
-  static bool ColorRenderable(const FeatureInfo* feature_info,
-                              GLenum internal_format,
-                              bool immutable);
-
  private:
   friend class MailboxManagerImpl;
   friend class MailboxManagerSync;
@@ -506,6 +502,10 @@ class GPU_EXPORT Texture final : public TextureBase {
                                  GLsizei depth,
                                  GLenum format,
                                  GLenum type);
+
+  static bool ColorRenderable(const FeatureInfo* feature_info,
+                              GLenum internal_format,
+                              bool immutable);
 
   static bool TextureFilterable(const FeatureInfo* feature_info,
                                 GLenum internal_format,
