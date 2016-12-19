519b221f017d087bb20a7897ba6dbbb810b929bf
khmel <khmel@chromium.org>
2016-12-19 13:08:26 -0800

    arc: Add explicit UMA that describes Silent Auth Code state.
    BUG=b/28987931
    TEST=Manually on device.
    
    Review-Url: https://codereview.chromium.org/2561203004
    Cr-Commit-Position: refs/heads/master@{#439555}

--- chrome/browser/chromeos/arc/arc_optin_uma.cc
+++ chrome/browser/chromeos/arc/arc_optin_uma.cc
@@ -50,4 +50,9 @@ void UpdateProvisioningTiming(const base::TimeDelta& elapsed_time,
       ->AddTime(elapsed_time);
 }
 
+void UpdateSilentAuthCodeUMA(OptInSilentAuthCode state) {
+  UMA_HISTOGRAM_ENUMERATION("Arc.OptInSilentAuthCode", static_cast<int>(state),
+                            static_cast<int>(OptInSilentAuthCode::SIZE));
+}
+
 }  // namespace arc
--- chrome/browser/chromeos/arc/auth/arc_background_auth_code_fetcher.h
+++ chrome/browser/chromeos/arc/auth/arc_background_auth_code_fetcher.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/chromeos/arc/arc_auth_context.h"
+#include "chrome/browser/chromeos/arc/arc_optin_uma.h"
 #include "chrome/browser/chromeos/arc/auth/arc_auth_code_fetcher.h"
 #include "google_apis/gaia/oauth2_token_service.h"
 #include "net/url_request/url_fetcher_delegate.h"
@@ -51,6 +52,9 @@ class ArcBackgroundAuthCodeFetcher : public ArcAuthCodeFetcher,
 
   // net::URLFetcherDelegate:
   void OnURLFetchComplete(const net::URLFetcher* source) override;
+
+  void ReportResult(const std::string& auth_code,
+                    OptInSilentAuthCode uma_status);
 
   // Unowned pointers.
   Profile* const profile_;
