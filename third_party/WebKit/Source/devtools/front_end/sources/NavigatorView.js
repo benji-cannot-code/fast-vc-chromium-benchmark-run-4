5bc6e4b4bce092105cbdef8b66fb3efceb9721ad
lushnikov <lushnikov@chromium.org>
2016-12-19 15:46:15 -0800

    DevTools: migrate navigator icons to UI.Icon
    This patch migrates navigator icons to UI.Icon. As a part of the effort,
    the patch introduces TreeElement.setLeadingIcons() method, which
    is symmetrical to the TreeElement.setTrailingIcons() method.
    
    BUG=669323
    R=dgozman
    
    Review-Url: https://codereview.chromium.org/2580163002
    Cr-Commit-Position: refs/heads/master@{#439618}

--- third_party/WebKit/Source/devtools/front_end/sources/NavigatorView.js
+++ third_party/WebKit/Source/devtools/front_end/sources/NavigatorView.js
@@ -92,7 +92,6 @@ Sources.NavigatorView = class extends UI.VBox {
       var weights = {};
       var types = Sources.NavigatorView.Types;
       weights[types.Root] = 1;
-      weights[types.Category] = 1;
       weights[types.Domain] = 10;
       weights[types.FileSystemFolder] = 1;
       weights[types.NetworkFolder] = 1;
@@ -823,7 +822,6 @@ Sources.NavigatorView = class extends UI.VBox {
 };
 
 Sources.NavigatorView.Types = {
-  Category: 'category',
   Domain: 'domain',
   File: 'file',
   FileSystem: 'fs',
@@ -834,7 +832,6 @@ Sources.NavigatorView.Types = {
   SourceMapFolder: 'sm-folder',
   Worker: 'worker'
 };
-
 
 /**
  * @unrestricted
@@ -852,9 +849,16 @@ Sources.NavigatorFolderTreeElement = class extends TreeElement {
     this._nodeType = type;
     this.title = title;
     this.tooltip = title;
-    this.createIcon();
     this._navigatorView = navigatorView;
     this._hoverCallback = hoverCallback;
+    var iconType = 'largeicon-navigator-folder';
+    if (type === Sources.NavigatorView.Types.Domain)
+      iconType = 'largeicon-navigator-domain';
+    else if (type === Sources.NavigatorView.Types.Frame)
+      iconType = 'largeicon-navigator-frame';
+    else if (type === Sources.NavigatorView.Types.Worker)
+      iconType = 'largeicon-navigator-worker';
+    this.setLeadingIcons([UI.Icon.create(iconType, 'icon')]);
   }
 
   /**
@@ -938,7 +942,10 @@ Sources.NavigatorSourceTreeElement = class extends TreeElement {
     this.listItemElement.classList.add(
         'navigator-' + uiSourceCode.contentType().name() + '-tree-item', 'navigator-file-tree-item');
     this.tooltip = uiSourceCode.url();
-    this.createIcon();
+    var iconType = 'largeicon-navigator-file';
+    if (uiSourceCode.contentType() === Common.resourceTypes.Snippet)
+      iconType = 'largeicon-navigator-snippet';
+    this.setLeadingIcons([UI.Icon.create(iconType, 'icon')]);
 
     this._navigatorView = navigatorView;
     this._uiSourceCode = uiSourceCode;
