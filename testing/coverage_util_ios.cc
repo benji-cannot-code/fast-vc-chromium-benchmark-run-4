b32eebf65ae940ef9899b7fc516696a20290d627
lpromero <lpromero@chromium.org>
2016-12-19 13:18:07 -0800

    Clean up in iOS code coverage code.
    BUG=none
    R=eugenebut@chromium.org
    
    Review-Url: https://codereview.chromium.org/2585293003
    Cr-Commit-Position: refs/heads/master@{#439563}

--- testing/coverage_util_ios.cc
+++ testing/coverage_util_ios.cc
@@ -3,12 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-extern "C" void __gcov_flush();
+extern "C" void __gcov_flush(void);
 
 namespace coverage_util {
 
 void FlushCoverageDataIfNecessary() {
-#if defined(ENABLE_TEST_CODE_COVERAGE)
+#if !defined(NDEBUG) && defined(ENABLE_TEST_CODE_COVERAGE)
   __gcov_flush();
 #endif
 }
