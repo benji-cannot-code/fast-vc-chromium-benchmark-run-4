7eafe0fc4f73dc0f2b88b1f73c44378f14f4c77f
mlamouri <mlamouri@chromium.org>
2016-12-19 10:54:53 -0800

    Media Controls: add comments regarding cast/remote related calls from element.
    We can't really make the calls use an event in the current situation.
    Adding TODO's to explain how it should be done and rename methods for
    future usage
    
    BUG=662761
    R=zqzhang@chromium.org
    
    Review-Url: https://codereview.chromium.org/2587083002
    Cr-Commit-Position: refs/heads/master@{#439514}

--- third_party/WebKit/Source/core/html/shadow/MediaControls.h
+++ third_party/WebKit/Source/core/html/shadow/MediaControls.h
@@ -66,9 +66,6 @@ class CORE_EXPORT MediaControls final : public HTMLDivElement {
   void enteredFullscreen();
   void exitedFullscreen();
 
-  void startedCasting();
-  void stoppedCasting();
-  void refreshCastButtonVisibility();
   void showOverlayCastButtonIfNeeded();
   // Update cast button visibility, but don't try to update our panel
   // button visibility for space.
@@ -103,6 +100,18 @@ class CORE_EXPORT MediaControls final : public HTMLDivElement {
   // HTMLTrackElement failed to load because there is no web exposed way to
   // be notified on the TextTrack object. See https://crbug.com/669977
   void onTrackElementFailedToLoad() { onTextTracksAddedOrRemoved(); }
+
+  // TODO(mlamouri): the following methods will be able to become private when
+  // the controls have moved to modules/ and have access to RemotePlayback.
+  void onRemotePlaybackAvailabilityChanged() { refreshCastButtonVisibility(); }
+  void onRemotePlaybackConnecting() { startedCasting(); }
+  void onRemotePlaybackDisconnected() { stoppedCasting(); }
+
+  // TODO(mlamouri): this method is needed in order to notify the controls that
+  // the attribute have changed.
+  void onDisableRemotePlaybackAttributeChanged() {
+    refreshCastButtonVisibility();
+  }
 
   DECLARE_VIRTUAL_TRACE();
 
@@ -163,6 +172,11 @@ class CORE_EXPORT MediaControls final : public HTMLDivElement {
   void onTextTracksChanged();
   void onError();
   void onLoadedMetadata();
+
+  // Internal cast related methods.
+  void startedCasting();
+  void stoppedCasting();
+  void refreshCastButtonVisibility();
 
   Member<HTMLMediaElement> m_mediaElement;
 
