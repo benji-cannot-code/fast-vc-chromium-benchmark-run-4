4c5c9be5111ce0cd140a02937a2b90933f483753
sdefresne <sdefresne@chromium.org>
2016-12-19 05:45:54 -0800

    Upstream Chrome on iOS source code [5/11].
    Upstream part of Chrome on iOS source code. Nothing is built yet,
    just new files added. The files will be added to the build as part
    of the last CL to avoid breaking downstream tree.
    
    BUG=653086
    
    Review-Url: https://codereview.chromium.org/2590473002
    Cr-Commit-Position: refs/heads/master@{#439462}

---
+++ ios/chrome/browser/ui/main/main_coordinator.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_MAIN_MAIN_COORDINATOR_H_
+#define IOS_CHROME_BROWSER_UI_MAIN_MAIN_COORDINATOR_H_
+
+#import "ios/chrome/browser/root_coordinator.h"
+
+#import <Foundation/Foundation.h>
+
+@class MainViewController;
+
+@interface MainCoordinator : RootCoordinator
+
+// The view controller this coordinator creates and manages.
+// (This is only public while the view controller architecture is being
+// refactored).
+@property(nonatomic, readonly, nullable) MainViewController* mainViewController;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_MAIN_MAIN_COORDINATOR_H_
