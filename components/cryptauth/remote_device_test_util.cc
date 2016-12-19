1ecd461455ae80f00d4b2f68080e50caf27ad87f
khorimoto <khorimoto@chromium.org>
2016-12-19 13:28:44 -0800

    [Chrome OS Tether] Create BleAdvertisementDeviceQueue.
    Each BLE device has a limited number of BLE advertisement slots; this class queues devices to which to advertise and provides functionality for moving devices to the end of the queue and requesting the devices which should be advertised.
    
    BUG=672263
    
    Review-Url: https://codereview.chromium.org/2576283002
    Cr-Commit-Position: refs/heads/master@{#439570}

---
+++ components/cryptauth/remote_device_test_util.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/cryptauth/remote_device_test_util.h"
+
+#include "base/base64.h"
+
+namespace cryptauth {
+
+std::vector<RemoteDevice> GenerateTestRemoteDevices(size_t num_to_create) {
+  std::vector<RemoteDevice> generated_devices;
+
+  for (size_t i = 0; i < num_to_create; i++) {
+    RemoteDevice device;
+    device.public_key = "publicKey" + std::to_string(i);
+    generated_devices.push_back(device);
+  }
+
+  return generated_devices;
+}
+
+}  // namespace cryptauth
