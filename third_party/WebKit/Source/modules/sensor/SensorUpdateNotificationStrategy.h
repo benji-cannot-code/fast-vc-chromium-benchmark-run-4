4c594c2ee4ef1688c3283f6ab6b03bb9bb69e071
wjmaclean <wjmaclean@chromium.org>
2016-12-19 08:26:30 -0800

    Revert of [Sensors] Align sensor reading updates and 'onchange' notification with rAF. (patchset #6 id:140001 of https://codereview.chromium.org/2551223003/ )
    Reason for revert:
    Seems like a likely cause for failures on:
    
    https://test-results.appspot.com/dashboards/flakiness_dashboard.html#tests=sensor%2Faccelerometer.html&testType=webkit_tests
    
    Original issue's description:
    > [Sensors] Align sensor reading updates and 'onchange' notification with rAF.
    >
    > For all sensors new reading values are read and 'onchange' notfication is send from rAF callbacks, thus avoiding possible Critical Rendering Path interruption.
    >
    > Before this change a timers were used and this could unnecessarily drain CPU and battery.
    >
    > BUG=668052
    > BUG=606766
    >
    > Committed: https://crrev.com/6b071fe7dc3bd64a2914eadd5c67b483d064a6cb
    > Cr-Commit-Position: refs/heads/master@{#439467}
    
    TBR=reillyg@chromium.org,alexander.shalamov@intel.com,haraken@chromium.org,foolip@chromium.org,mikhail.pozdnyakov@intel.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=668052
    
    Review-Url: https://codereview.chromium.org/2590513002
    Cr-Commit-Position: refs/heads/master@{#439487}

---
+++ third_party/WebKit/Source/modules/sensor/SensorUpdateNotificationStrategy.h
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SensorUpdateNotificationStrategy_h
+#define SensorUpdateNotificationStrategy_h
+
+#include "wtf/Functional.h"
+
+namespace blink {
+
+// This class encapsulates sensor reading update notification logic:
+// the callback is invoked after client calls 'onSensorReadingChanged()'
+// however considering the given sample frequency:
+// guaranteed not to be called more often than expected.
+class SensorUpdateNotificationStrategy {
+ public:
+  static std::unique_ptr<SensorUpdateNotificationStrategy> create(
+      double frequency,
+      std::unique_ptr<Function<void()>>);
+
+  virtual void onSensorReadingChanged() = 0;
+  virtual void cancelPendingNotifications() = 0;
+  virtual ~SensorUpdateNotificationStrategy() {}
+};
+
+}  // namespace blink
+
+#endif  // SensorUpdateNotificationStrategy_h
