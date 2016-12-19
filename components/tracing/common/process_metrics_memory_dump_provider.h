a496d36316978ec216f53352046a07e620186b28
ssid <ssid@chromium.org>
2016-12-18 20:11:12 -0800

    [memory-infra] Implement PollFastMemoryTotal in ProcessMetricsMemoryDumpProvider.
    For fast polling the dump provider keeps the /proc/<pid>/statm files
    open during the tracing session.
    
    BUG=607533
    
    Review-Url: https://codereview.chromium.org/2568313004
    Cr-Commit-Position: refs/heads/master@{#439405}

--- components/tracing/common/process_metrics_memory_dump_provider.h
+++ components/tracing/common/process_metrics_memory_dump_provider.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/files/scoped_file.h"
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "base/process/process_handle.h"
@@ -34,11 +35,15 @@ class TRACING_EXPORT ProcessMetricsMemoryDumpProvider
   // MemoryDumpProvider implementation.
   bool OnMemoryDump(const base::trace_event::MemoryDumpArgs& args,
                     base::trace_event::ProcessMemoryDump* pmd) override;
+  void PollFastMemoryTotal(uint64_t* memory_total) override;
+  void SuspendFastMemoryPolling() override;
 
  private:
   FRIEND_TEST_ALL_PREFIXES(ProcessMetricsMemoryDumpProviderTest,
                            ParseProcSmaps);
   FRIEND_TEST_ALL_PREFIXES(ProcessMetricsMemoryDumpProviderTest, DumpRSS);
+  FRIEND_TEST_ALL_PREFIXES(ProcessMetricsMemoryDumpProviderTest,
+                           TestPollFastMemoryTotal);
 
   ProcessMetricsMemoryDumpProvider(base::ProcessId process);
 
@@ -51,6 +56,9 @@ class TRACING_EXPORT ProcessMetricsMemoryDumpProvider
 
 #if defined(OS_LINUX) || defined(OS_ANDROID)
   static FILE* proc_smaps_for_testing;
+  static int fast_polling_statm_fd_for_testing;
+
+  base::ScopedFD fast_polling_statm_fd_;
 #endif
 
   base::ProcessId process_;
