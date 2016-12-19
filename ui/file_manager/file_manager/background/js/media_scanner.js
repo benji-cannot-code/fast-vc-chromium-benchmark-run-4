638b6cb8765ba0257de5fca73f7570bc626192b1
yamaguchi <yamaguchi@chromium.org>
2016-12-18 23:05:47 -0800

    Add scan mode parameter to choose whether to do content duplication check or just check the import history.
    This change will not actually change the behavior of the app but will be used by a following change https://codereview.chromium.org/2569163002/ .
    
    BUG=668574
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2568723005
    Cr-Commit-Position: refs/heads/master@{#439422}

--- ui/file_manager/file_manager/background/js/media_scanner.js
+++ ui/file_manager/file_manager/background/js/media_scanner.js
@@ -14,6 +14,7 @@ importer.MediaScanner = function() {};
  * Initiates scanning.
  *
  * @param {!DirectoryEntry} directory
+ * @param {!importer.ScanMode} mode
  * @return {!importer.ScanResult} ScanResult object representing the scan
  *     job both while in-progress and when completed.
  */
@@ -25,6 +26,7 @@ importer.MediaScanner.prototype.scanDirectory;
  * @param {!Array<!FileEntry>} entries Must be non-empty, and all entires
  *     must be of a supported media type. Individually supplied files
  *     are not subject to deduplication.
+ * @param {!importer.ScanMode} mode The method to detect new files.
  * @return {!importer.ScanResult} ScanResult object representing the scan
  *     job for the explicitly supplied entries.
  */
@@ -53,7 +55,8 @@ importer.MediaScanner.prototype.removeObserver;
  * @implements {importer.MediaScanner}
  *
  * @param {function(!FileEntry): !Promise.<string>} hashGenerator
- * @param {function(!FileEntry, !importer.Destination):
+ * @param {function(!FileEntry, !importer.Destination,
+ *                  !importer.ScanMode):
  *     !Promise<!importer.Disposition>} dispositionChecker
  * @param {!importer.DirectoryWatcherFactory} watcherFactory
  */
@@ -63,10 +66,11 @@ importer.DefaultMediaScanner =
   /**
    * A little factory for DefaultScanResults which allows us to forgo
    * the saving it's dependencies in our fields.
+   * @param {importer.ScanMode} mode Mode of the scan to find new files.
    * @return {!importer.DefaultScanResult}
    */
-  this.createScanResult_ = function() {
-    return new importer.DefaultScanResult(hashGenerator);
+  this.createScanResult_ = function(mode) {
+    return new importer.DefaultScanResult(mode, hashGenerator);
   };
 
   /** @private {!Array<!importer.ScanObserver>} */
@@ -75,6 +79,7 @@ importer.DefaultMediaScanner =
   /**
    * @param {!FileEntry} entry
    * @param {!importer.Destination} destination
+   * @param {!importer.ScanMode} mode
    * @return {!Promise<!importer.Disposition>}
    */
   this.getDisposition_ = dispositionChecker;
@@ -102,8 +107,9 @@ importer.DefaultMediaScanner.prototype.removeObserver = function(observer) {
 };
 
 /** @override */
-importer.DefaultMediaScanner.prototype.scanDirectory = function(directory) {
-  var scan = this.createScanResult_();
+importer.DefaultMediaScanner.prototype.scanDirectory = function(directory,
+                                                                mode) {
+  var scan = this.createScanResult_(mode);
   console.info(scan.name + ': Scanning directory ' + directory.fullPath);
 
   var watcher = this.watcherFactory_(
@@ -132,11 +138,11 @@ importer.DefaultMediaScanner.prototype.scanDirectory = function(directory) {
 };
 
 /** @override */
-importer.DefaultMediaScanner.prototype.scanFiles = function(entries) {
+importer.DefaultMediaScanner.prototype.scanFiles = function(entries, mode) {
   if (entries.length === 0) {
     throw new Error('Cannot scan empty list.');
   }
-  var scan = this.createScanResult_();
+  var scan = this.createScanResult_(mode);
   console.info(
       scan.name + ': Scanning fixed set of ' +
       entries.length + ' entries.');
@@ -277,7 +283,8 @@ importer.DefaultMediaScanner.prototype.crawlDirectory_ =
 importer.DefaultMediaScanner.prototype.onFileEntryFound_ =
     function(scan, entry) {
 
-  return this.getDisposition_(entry, importer.Destination.GOOGLE_DRIVE)
+  return this.getDisposition_(entry, importer.Destination.GOOGLE_DRIVE,
+                              scan.mode)
       .then(
           /**
            * @param {!importer.Disposition} disposition The disposition
@@ -443,10 +450,17 @@ importer.ScanResult.Statistics;
  * @struct
  * @implements {importer.ScanResult}
  *
+ * @param {importer.ScanMode} mode The scan mode applied for finding new files.
  * @param {function(!FileEntry): !Promise.<string>} hashGenerator Hash-code
  *     generator used to dedupe within the scan results itself.
  */
-importer.DefaultScanResult = function(hashGenerator) {
+importer.DefaultScanResult = function(mode, hashGenerator) {
+  /**
+   * The scan mode applied for finding new files.
+   * @type {importer.ScanMode}
+   */
+  this.mode = mode;
+
   /** @private {number} */
   this.scanId_ = importer.generateId();
 
