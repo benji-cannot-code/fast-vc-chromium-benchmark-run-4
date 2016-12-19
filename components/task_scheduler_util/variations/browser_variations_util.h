e1078d2c86ce6db3cba48de10f903a6dc1f89500
fdoray <fdoray@chromium.org>
2016-12-19 07:58:37 -0800

    Split browser-specific and generic code in components/task_scheduler_util/.
    This will allow code to be reused to initialize TaskScheduler based on
    variation params in renderers.
    
    To avoid breaking ios-internal, this CL keeps existing
    components/task_scheduler_util APIs. Code in ChromeContentBrowserClient
    and ios-internal will be migrated to the new APIs in separate CLs.
    Then, old components/task_scheduler_util APIs will be deleted.
    
    TBR=blundell@chromium.org
    BUG=664996
    
    Review-Url: https://codereview.chromium.org/2565013002
    Cr-Commit-Position: refs/heads/master@{#439481}

--- components/task_scheduler_util/variations/browser_variations_util.h
+++ components/task_scheduler_util/variations/browser_variations_util.h
@@ -3,6 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// TODO(fdoray): Remove this file once TaskScheduler initialization in the
+// browser process uses the components/task_scheduler_util/browser/ API on all
+// platforms.
+
 #ifndef COMPONENTS_TASK_SCHEDULER_UTIL_VARIATIONS_BROWSER_VARIATIONS_UTIL_H_
 #define COMPONENTS_TASK_SCHEDULER_UTIL_VARIATIONS_BROWSER_VARIATIONS_UTIL_H_
 
