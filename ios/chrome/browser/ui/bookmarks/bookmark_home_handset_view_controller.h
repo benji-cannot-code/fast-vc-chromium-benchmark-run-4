61cfcf2cc3626279fae74d8f9ba2a0a453a0831b
sdefresne <sdefresne@chromium.org>
2016-12-19 05:30:43 -0800

    Upstream Chrome on iOS source code [3/11].
    Upstream part of Chrome on iOS source code. Nothing is built yet,
    just new files added. The files will be added to the build as part
    of the last CL to avoid breaking downstream tree.
    
    BUG=653086
    
    Review-Url: https://codereview.chromium.org/2586993002
    Cr-Commit-Position: refs/heads/master@{#439460}

---
+++ ios/chrome/browser/ui/bookmarks/bookmark_home_handset_view_controller.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_BOOKMARKS_BOOKMARK_HOME_HANDSET_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_UI_BOOKMARKS_BOOKMARK_HOME_HANDSET_VIEW_CONTROLLER_H_
+
+#import "ios/chrome/browser/ui/bookmarks/bookmark_home_view_controller.h"
+
+// Navigate/edit the bookmark hierarchy on a handset.
+@interface BookmarkHomeHandsetViewController : BookmarkHomeViewController
+// Designated initializer.
+- (instancetype)initWithLoader:(id<UrlLoader>)loader
+                  browserState:(ios::ChromeBrowserState*)browserState;
+@end
+
+@interface BookmarkHomeHandsetViewController (ExposedForTesting)
+// Creates the default view to show all bookmarks, if it doesn't already exist.
+- (void)ensureAllViewExists;
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_BOOKMARKS_BOOKMARK_HOME_HANDSET_VIEW_CONTROLLER_H_
