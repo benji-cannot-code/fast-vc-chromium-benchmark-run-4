2f55030367c092024dbf4535bf50504bb5e519e9
rtoy <rtoy@chromium.org>
2016-12-20 09:23:47 -0800

    Add constructor for MediaStreamAudioDestinationNode
    MediaStreamAudioDestinationNode needs a constructor.  This was
    inadvertently missed in issue 626449.
    
    Also, modified the exception type when an invalid channel count is
    specified.  All other AudioNodes throw NotSupportedError instead of
    IndexSizeError.  This requires updating one test.
    
    BUG=674508, 626449
    TEST=constructor/mediastreamaudiodestination.html,
    mediastreamaudiodestinationnode.html
    
    Review-Url: https://codereview.chromium.org/2578243002
    Cr-Commit-Position: refs/heads/master@{#439833}

--- third_party/WebKit/Source/modules/webaudio/MediaStreamAudioDestinationNode.cpp
+++ third_party/WebKit/Source/modules/webaudio/MediaStreamAudioDestinationNode.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/ExceptionState.h"
 #include "core/dom/ExceptionCode.h"
 #include "modules/webaudio/AudioNodeInput.h"
+#include "modules/webaudio/AudioNodeOptions.h"
 #include "modules/webaudio/BaseAudioContext.h"
 #include "platform/UUID.h"
 #include "platform/mediastream/MediaStreamCenter.h"
@@ -116,9 +117,9 @@ void MediaStreamAudioDestinationHandler::setChannelCount(
   // which is constrained by m_source (WebAudioCapturereSource). Although
   // it has its own safety check for the excessive channels, throwing an
   // exception here is useful to developers.
-  if (channelCount > maxChannelCount()) {
+  if (channelCount < 1 || channelCount > maxChannelCount()) {
     exceptionState.throwDOMException(
-        IndexSizeError,
+        NotSupportedError,
         ExceptionMessages::indexOutsideRange<unsigned>(
             "channel count", channelCount, 1, ExceptionMessages::InclusiveBound,
             maxChannelCount(), ExceptionMessages::InclusiveBound));
@@ -158,6 +159,28 @@ MediaStreamAudioDestinationNode* MediaStreamAudioDestinationNode::create(
   }
 
   return new MediaStreamAudioDestinationNode(context, numberOfChannels);
+}
+
+MediaStreamAudioDestinationNode* MediaStreamAudioDestinationNode::create(
+    BaseAudioContext* context,
+    const AudioNodeOptions& options,
+    ExceptionState& exceptionState) {
+  DCHECK(isMainThread());
+
+  // Default to stereo; |options| will update it approriately if needed.
+  MediaStreamAudioDestinationNode* node =
+      new MediaStreamAudioDestinationNode(*context, 2);
+
+  // Need to handle channelCount here ourselves because the upper
+  // limit is different from the normal AudioNode::setChannelCount
+  // limit of 32.  Error messages will sometimes show the wrong
+  // limits.
+  if (options.hasChannelCount())
+    node->setChannelCount(options.channelCount(), exceptionState);
+
+  node->handleChannelOptions(options, exceptionState);
+
+  return node;
 }
 
 MediaStream* MediaStreamAudioDestinationNode::stream() const {
