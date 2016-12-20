70fc2407f8ae8b45231b9e850974fdebebc3c8c4
pwnall <pwnall@chromium.org>
2016-12-19 16:08:23 -0800

    Remove special DataTransfer permissions for file: origins.
    Blink currently uses an overly permissive mode for DataTransfer
    instances given to the dragenter/dragover/dragleave event handlers of a
    page served from a file: origin. According to the specification, the
    DataTransfer instance should only allow limited metadata to be read.
    Blink allows the page to read all the drag information.
    
    This change aligns the DataTransfer mode used in these event handlers to
    the current drag and drop specification.
    
    BUG=104681
    
    Review-Url: https://codereview.chromium.org/2572023002
    Cr-Commit-Position: refs/heads/master@{#439627}

--- third_party/WebKit/Source/core/page/DragController.cpp
+++ third_party/WebKit/Source/core/page/DragController.cpp
@@ -214,11 +214,7 @@ void DragController::dragExited(DragData* dragData, LocalFrame& localRoot) {
 
   FrameView* frameView(localRoot.view());
   if (frameView) {
-    DataTransferAccessPolicy policy =
-        (!m_documentUnderMouse ||
-         m_documentUnderMouse->getSecurityOrigin()->isLocal())
-            ? DataTransferReadable
-            : DataTransferTypesReadable;
+    DataTransferAccessPolicy policy = DataTransferTypesReadable;
     DataTransfer* dataTransfer = createDraggingDataTransfer(policy, dragData);
     dataTransfer->setSourceOperation(dragData->draggingSourceOperationMask());
     localRoot.eventHandler().cancelDragAndDrop(createMouseEvent(dragData),
@@ -719,10 +715,7 @@ bool DragController::tryDHTMLDrag(DragData* dragData,
   if (!localRoot.view())
     return false;
 
-  DataTransferAccessPolicy policy =
-      m_documentUnderMouse->getSecurityOrigin()->isLocal()
-          ? DataTransferReadable
-          : DataTransferTypesReadable;
+  DataTransferAccessPolicy policy = DataTransferTypesReadable;
   DataTransfer* dataTransfer = createDraggingDataTransfer(policy, dragData);
   DragOperation srcOpMask = dragData->draggingSourceOperationMask();
   dataTransfer->setSourceOperation(srcOpMask);
