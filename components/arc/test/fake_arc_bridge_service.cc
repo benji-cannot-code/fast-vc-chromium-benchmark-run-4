82cdb575e777766efe2193b60657d5deff9fb232
hidehiko <hidehiko@chromium.org>
2016-12-20 12:19:50 -0800

    Refactor ArcBridgeServiceImpl part 2.
    This CL is the preparation to split ArcSession running parts
    from ArcBridgeService.
    - Rename ArcBridgeService::State names to be consistent with
      terms used in other ARC modules.
    - Remove unused CONNECTED from State.
    - Migrate PrerequisitesChanged() and StopInstance() into
      RequestStart() and RequestStop().
    - Rename some fields of ArcBridgeServiceImpl for consistency.
    - Cleaned up include directives.
    
    BUG=657687
    BUG=b/31079732
    TEST=Ran bots.
    
    Review-Url: https://codereview.chromium.org/2577373002
    Cr-Commit-Position: refs/heads/master@{#439875}

--- components/arc/test/fake_arc_bridge_service.cc
+++ components/arc/test/fake_arc_bridge_service.cc
@@ -26,8 +26,8 @@ void FakeArcBridgeService::OnShutdown() {
 }
 
 void FakeArcBridgeService::SetReady() {
-  if (state() != State::READY)
-    SetState(State::READY);
+  if (state() != State::RUNNING)
+    SetState(State::RUNNING);
 }
 
 void FakeArcBridgeService::SetStopped() {
