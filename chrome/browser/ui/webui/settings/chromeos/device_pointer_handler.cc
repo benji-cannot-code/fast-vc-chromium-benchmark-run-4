5ce4eea2d62849a7fae0e7c464861fb8b46a1a00
michaelpg <michaelpg@chromium.org>
2016-12-20 14:53:23 -0800

    MD settings: remove unused include
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2578193005
    Cr-Commit-Position: refs/heads/master@{#439901}

--- chrome/browser/ui/webui/settings/chromeos/device_pointer_handler.cc
+++ chrome/browser/ui/webui/settings/chromeos/device_pointer_handler.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/settings/chromeos/device_pointer_handler.h"
 
 #include "base/bind.h"
-#include "base/sys_info.h"
 #include "base/values.h"
 #include "content/public/browser/web_ui.h"
 
