035d7a50d658f1447548c1faa5a193c439b6deb4
wangyix <wangyix@google.com>
2016-12-20 15:58:02 -0800

    Add unbounded single-file mode to BoundedFileNetLogObserver
    BoundedFileNetLogObserver::FileWriter has been made into an interface,
    with two implementations:
    BoundedFileWriter, which has the same behavior as the old FileWriter.
    UnboundedFileWriter, which writes everything to one log file.
    
    Depending on what mode BoundedFileNetLogObserver is set to, one of
    these classes will be instantiated as file_writer_.
    
    BUG=438656
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_cronet_tester
    
    Review-Url: https://codereview.chromium.org/2553373002
    Cr-Commit-Position: refs/heads/master@{#439939}

--- components/cronet/android/cronet_url_request_context_adapter.cc
+++ components/cronet/android/cronet_url_request_context_adapter.cc
@@ -9,8 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include <limits>
 #include <map>
 #include <utility>
+#include <vector>
 
 #include "base/android/jni_android.h"
 #include "base/android/jni_array.h"
@@ -53,7 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cookies/cookie_monster.h"
 #include "net/http/http_auth_handler_factory.h"
 #include "net/http/http_server_properties_manager.h"
-#include "net/log/bounded_file_net_log_observer.h"
+#include "net/log/file_net_log_observer.h"
 #include "net/log/write_to_file_net_log_observer.h"
 #include "net/nqe/external_estimate_provider.h"
 #include "net/proxy/proxy_config_service_android.h"
@@ -931,15 +933,15 @@ void CronetURLRequestContextAdapter::StartNetLogToBoundedFileOnNetworkThread(
   DCHECK(base::PathIsWritable(file_path));
 
   bounded_file_observer_.reset(
-      new net::BoundedFileNetLogObserver(GetFileThread()->task_runner()));
-  if (include_socket_bytes) {
-    bounded_file_observer_->set_capture_mode(
-        net::NetLogCaptureMode::IncludeSocketBytes());
-  }
+      new net::FileNetLogObserver(GetFileThread()->task_runner()));
 
-  bounded_file_observer_->StartObserving(g_net_log.Get().net_log(), file_path,
-                                         /*constants=*/nullptr, context_.get(),
-                                         size, kNumNetLogEventFiles);
+  net::NetLogCaptureMode capture_mode =
+      include_socket_bytes ? net::NetLogCaptureMode::IncludeSocketBytes()
+                           : net::NetLogCaptureMode::Default();
+
+  bounded_file_observer_->StartObservingBounded(
+      g_net_log.Get().net_log(), capture_mode, file_path,
+      /*constants=*/nullptr, context_.get(), size, kNumNetLogEventFiles);
 }
 
 void CronetURLRequestContextAdapter::StopBoundedFileNetLogOnNetworkThread() {
