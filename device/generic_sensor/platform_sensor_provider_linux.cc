de19641233f030b7ce15bea8dc952023416a3b04
maksim.sisov <maksim.sisov@intel.com>
2016-12-20 06:37:53 -0800

    [sensors](Linux) Fix tsan data race in sensor reader
    This CL fixes a tsan data race, which is caused by calling StopFetchingData
    from different threads. It must not be allowed. Use the same thread by using
    a PostTask.
    
    BUG=673760
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_chromium_tsan_rel_ng
    
    Review-Url: https://codereview.chromium.org/2569763004
    Cr-Commit-Position: refs/heads/master@{#439798}

--- device/generic_sensor/platform_sensor_provider_linux.cc
+++ device/generic_sensor/platform_sensor_provider_linux.cc
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/generic_sensor/platform_sensor_provider_linux.h"
 
 #include "base/memory/singleton.h"
+#include "base/task_runner_util.h"
 #include "base/threading/thread.h"
 #include "device/generic_sensor/linux/sensor_data_linux.h"
 #include "device/generic_sensor/platform_sensor_linux.h"
+#include "device/generic_sensor/platform_sensor_reader_linux.h"
 
 namespace device {
 
@@ -60,8 +62,9 @@ void PlatformSensorProviderLinux::SensorDeviceFound(
     mojom::SensorType type,
     mojo::ScopedSharedBufferMapping mapping,
     const PlatformSensorProviderBase::CreateSensorCallback& callback,
-    SensorInfoLinux* sensor_device) {
+    const SensorInfoLinux* sensor_device) {
   DCHECK(CalledOnValidThread());
+  DCHECK(sensor_device);
 
   if (!StartPollingThread()) {
     callback.Run(nullptr);
