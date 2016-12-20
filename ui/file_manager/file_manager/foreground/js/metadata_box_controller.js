a4b3861941a556e6c2864b30246c6dbb2ed37a90
oka <oka@chromium.org>
2016-12-20 06:36:33 -0800

    Quick view lazy load part 1: initialize controllers lazily.
    Initialize MetadataBoxController and QuickViewController only when
    it is needed.
    This CL doesn't lazy load polymer element of quick view. It will be
    done with subsequent CL(s).
    
    BUG=675536
    TEST=manually.
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2589133002
    Cr-Commit-Position: refs/heads/master@{#439797}

--- ui/file_manager/file_manager/foreground/js/metadata_box_controller.js
+++ ui/file_manager/file_manager/foreground/js/metadata_box_controller.js
@@ -5,18 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * Controller of metadata box.
+ * This should be initialized with |init| method.
  *
  * @param{!MetadataModel} metadataModel
- * @param{!FilesMetadataBox} metadataBox
- * @param{!FilesQuickView} quickView
  * @param{!QuickViewModel} quickViewModel
  * @param{!FileMetadataFormatter} fileMetadataFormatter
  *
  * @constructor
  */
 function MetadataBoxController(
-    metadataModel, metadataBox, quickView, quickViewModel,
-    fileMetadataFormatter) {
+    metadataModel, quickViewModel, fileMetadataFormatter) {
   /**
    * @type {!MetadataModel}
    * @private
@@ -24,40 +22,49 @@ function MetadataBoxController(
   this.metadataModel_ = metadataModel;
 
   /**
-   * @type {!FilesMetadataBox}
+   * @type {!QuickViewModel}
    * @private
    */
-  this.metadataBox_ = metadataBox;
+  this.quickViewModel_ = quickViewModel;
+
+ /**
+  * @type {FilesMetadataBox} metadataBox
+  * @private
+  */
+  this.metadataBox_ = null;
 
   /**
-   * @type {!FilesQuickView}
+   * @type {FilesQuickView} quickView
    * @private
    */
-  this.quickView_ = quickView;
+  this.quickView_ = null;
 
   /**
    * @type {!FileMetadataFormatter}
    * @private
    */
   this.fileMetadataFormatter_ = fileMetadataFormatter;
+}
 
+/**
+ * Initialize the controller with quick view which will be lazily loaded.
+ * @param{!FilesQuickView} quickView
+ */
+MetadataBoxController.prototype.init = function(quickView) {
   // TODO(oka): Add storage to persist the value of
   // quickViewModel_.metadataBoxActive.
-  /**
-   * @type {!QuickViewModel}
-   * @private
-   */
-  this.quickViewModel_ = quickViewModel;
-
-  fileMetadataFormatter.addEventListener(
+  this.fileMetadataFormatter_.addEventListener(
       'date-time-format-changed', this.updateView_.bind(this));
 
   quickView.addEventListener(
       'metadata-box-active-changed', this.updateView_.bind(this));
 
-  quickViewModel.addEventListener(
+  this.quickViewModel_.addEventListener(
       'selected-entry-changed', this.updateView_.bind(this));
-}
+
+  this.metadataBox_ = quickView.getFilesMetadataBox();
+  this.quickView_ = quickView;
+};
 
 /**
  * @const {!Array<string>}
