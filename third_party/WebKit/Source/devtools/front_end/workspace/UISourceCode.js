53939aae3f96d787fb67fdbc7d76bead7fb6bbf6
pfeldman <pfeldman@chromium.org>
2016-12-19 17:12:00 -0800

    DevTools: consistently use relative name for filesystem resources.
    Review-Url: https://codereview.chromium.org/2587533002
    Cr-Commit-Position: refs/heads/master@{#439648}

--- third_party/WebKit/Source/devtools/front_end/workspace/UISourceCode.js
+++ third_party/WebKit/Source/devtools/front_end/workspace/UISourceCode.js
@@ -110,12 +110,7 @@ Workspace.UISourceCode = class extends Common.Object {
    * @return {string}
    */
   fullDisplayName() {
-    var parentPath = this._parentURL.replace(/^(?:https?|file)\:\/\//, '');
-    try {
-      parentPath = decodeURI(parentPath);
-    } catch (e) {
-    }
-    return parentPath + '/' + this.displayName(true);
+    return this._project.fullDisplayName(this);
   }
 
   /**
--- third_party/WebKit/Source/devtools/front_end/workspace/Workspace.js
+++ third_party/WebKit/Source/devtools/front_end/workspace/Workspace.js
@@ -117,6 +117,12 @@ Workspace.Project.prototype = {
   setFileContent(uiSourceCode, newContent, callback) {},
 
   /**
+   * @param {!Workspace.UISourceCode} uiSourceCode
+   * @return {string}
+   */
+  fullDisplayName(uiSourceCode) {},
+
+  /**
    * @return {boolean}
    */
   canRename() {},
