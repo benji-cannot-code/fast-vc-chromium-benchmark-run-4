4bc264967074907dd6e8a440f6b5fabe4a5ed81b
yamaguchi <yamaguchi@chromium.org>
2016-12-19 20:17:03 -0800

    Avoid UI jankiness caused by triggering selection scan too frequently.
    BUG=671517
    TEST=manual test by the repro step noted in the bug
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2589823002
    Cr-Commit-Position: refs/heads/master@{#439702}

--- ui/file_manager/file_manager/foreground/js/import_controller.js
+++ ui/file_manager/file_manager/foreground/js/import_controller.js
@@ -1212,7 +1212,7 @@ importer.RuntimeControllerEnvironment.prototype.addDirectoryChangedListener =
 importer.RuntimeControllerEnvironment.prototype.addSelectionChangedListener =
     function(listener) {
   this.selectionHandler_.addEventListener(
-      FileSelectionHandler.EventType.CHANGE,
+      FileSelectionHandler.EventType.CHANGE_THROTTLED,
       listener);
 };
 
