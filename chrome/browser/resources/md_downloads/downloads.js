1d40e0a1863ffeb48bed62fbdc7819060ac0f22a
dbeam <dbeam@chromium.org>
2016-12-20 15:33:55 -0800

    MD Downloads: add first meaningful paint metrics to UMA
    Note: I played around with a bunch of random delayers. rIC seemed the best.
    
    R=tsergeant@chromium.org
    BUG=653332
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2592573002
    Cr-Commit-Position: refs/heads/master@{#439924}

--- chrome/browser/resources/md_downloads/downloads.js
+++ chrome/browser/resources/md_downloads/downloads.js
@@ -4,6 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 window.addEventListener('load', function() {
-  downloads.Manager.onLoad();
-  document.fonts.load('bold 12px Roboto');
+  downloads.Manager.onLoad().then(function() {
+    requestIdleCallback(function() {
+      chrome.send('metricsHandler:recordTime', [
+        'Download.ResultsRenderedTime',
+        window.performance.now()
+      ]);
+      document.fonts.load('bold 12px Roboto');
+    });
+  });
 });
