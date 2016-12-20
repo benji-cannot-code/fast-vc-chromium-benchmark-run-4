b3f9ba7e838ddde7017630617aa88afd36445baf
yhirano <yhirano@chromium.org>
2016-12-19 23:44:02 -0800

    Most css1/ tests should wait for basebg.gif to be loaded
    Many css1/ tests compare the image output with the expected one. They expected
    that tests finish when the layout result becomes stable.
    
    But the test runner may finish tests before loading some resources. Even worse,
    the document load event may be dispatched before loading some resources. They're
    working by chance, and changing the loading timing a bit will break them.
    
    This CL makes those tests wait for css1/resources/basebg.gif to be loaded.
    
    BUG=603396, 675870
    
    Review-Url: https://codereview.chromium.org/2588283002
    Cr-Commit-Position: refs/heads/master@{#439733}

---
+++ third_party/WebKit/LayoutTests/css1/resources/base.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+if (window.testRunner) {
+  // This is a workaroud for an issue that the load event may be dispatched
+  // before loading ../resources/basebg.gif which is linked from
+  // ../resources/base.css.
+  testRunner.waitUntilDone();
+  window.addEventListener('load', () => {
+    const id = setInterval(() => {
+      // Force layout.
+      document.body.offsetTop;
+      if (internals.isLoading('../resources/basebg.gif'))
+        return;
+
+      clearInterval(id);
+      testRunner.notifyDone();
+    }, 50);
+  });
+}
