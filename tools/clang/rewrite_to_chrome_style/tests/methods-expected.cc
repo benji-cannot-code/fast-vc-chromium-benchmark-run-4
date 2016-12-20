b06800a5a070c884e96ed92d971a50f51a011c6f
lukasza <lukasza@chromium.org>
2016-12-20 08:29:04 -0800

    Add a hardcoded list of methods that need a "Get" prefix.
    BUG=582312
    
    Review-Url: https://codereview.chromium.org/2589923002
    Cr-Commit-Position: refs/heads/master@{#439818}

--- tools/clang/rewrite_to_chrome_style/tests/methods-expected.cc
+++ tools/clang/rewrite_to_chrome_style/tests/methods-expected.cc
@@ -174,6 +174,10 @@ class BitVector {
   class Baz {};
   class FooBar {};
 
+  // Should be renamed to GetReadyState, because of
+  // ShouldPrefixFunctionName heuristic.
+  int GetReadyState() { return 123; }
+
   template <typename T>
   class MyRefPtr {};
 
--- tools/clang/rewrite_to_chrome_style/tests/methods-original.cc
+++ tools/clang/rewrite_to_chrome_style/tests/methods-original.cc
@@ -178,6 +178,10 @@ class BitVector {
   class Baz {};
   class FooBar {};
 
+  // Should be renamed to GetReadyState, because of
+  // ShouldPrefixFunctionName heuristic.
+  int readyState() { return 123; }
+
   template <typename T>
   class MyRefPtr {};
 
