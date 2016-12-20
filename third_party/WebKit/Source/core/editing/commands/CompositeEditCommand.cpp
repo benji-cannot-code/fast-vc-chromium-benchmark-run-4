10d3e68d9905b70edb4d488ecd169ab7143d80ff
chongz <chongz@chromium.org>
2016-12-19 16:26:43 -0800

    [Editing] Introduce |CompositeEditCommand::willApplyEditing()| in prepare for 'beforeinput' (1/3)
    This is the first sub-patch (1/3) of unifying 'beforeinput' logic. (Original CL
    https://crrev.com/2558643003)
    
    This CL:
    1. Added attribute |m_inputType| to |TypingCommand|
    2. Added calling path |CompositeEditCommand::apply()| =>
        *|CompositeEditCommand::willApply()| =>
        *|CompositeEditCommand::willApplyEditing()|
    3. Added calling path |static TypingCommand::deleteSelection()/insertText()/...| =>
        *|TypingCommand::willAddTypingToOpenCommand()| =>
        *|CompositeEditCommand::willApplyEditing()|
    
    * Marks new methods.
    
    The next step is to move 'beforeinput' logic to
    |CompositeEditCommand::willApplyEditing()|.
    
    This CL shouldn't have any behavior change.
    
    To help reviewing, here is a simple doc describing the following plans:
    https://docs.google.com/a/chromium.org/document/d/1M9bQaLKdRpR1qJ9i7zZKN-hKwd4ZkgJs5GPde2ZsDy0/edit?usp=sharing
    
    BUG=670035
    
    Review-Url: https://codereview.chromium.org/2583993002
    Cr-Commit-Position: refs/heads/master@{#439633}

--- third_party/WebKit/Source/core/editing/commands/CompositeEditCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/CompositeEditCommand.cpp
@@ -188,8 +188,7 @@ CompositeEditCommand::~CompositeEditCommand() {
   DCHECK(isTopLevelCommand() || !m_composition);
 }
 
-// TODO(chongz): Fire 'beforeinput' based on |EditCommandSource|.
-bool CompositeEditCommand::apply(EditCommandSource) {
+bool CompositeEditCommand::apply(EditCommandSource source) {
   DCHECK(!isCommandGroupWrapper());
   if (!endingSelection().isContentRichlyEditable()) {
     switch (inputType()) {
@@ -225,6 +224,9 @@ bool CompositeEditCommand::apply(EditCommandSource) {
   // the creation of VisiblePositions).
   document().updateStyleAndLayoutIgnorePendingStylesheets();
 
+  if (!willApplyEditing(source))
+    return false;
+
   LocalFrame* frame = document().frame();
   DCHECK(frame);
   EditingState editingState;
@@ -249,6 +251,11 @@ EditCommandComposition* CompositeEditCommand::ensureComposition() {
     command->m_composition = EditCommandComposition::create(
         &document(), startingSelection(), endingSelection(), inputType());
   return command->m_composition.get();
+}
+
+bool CompositeEditCommand::willApplyEditing(EditCommandSource) {
+  // TODO(chongz): Move all the 'beforeinput' dispatching logic here.
+  return true;
 }
 
 bool CompositeEditCommand::preservesTypingStyle() const {
