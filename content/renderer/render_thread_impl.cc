94710623914bcea77f862d12509763dc3c87f454
bashi <bashi@chromium.org>
2016-12-19 20:49:30 -0800

    Disable suspending renderer when memory coordinator is enabled
    Just calling SuspendRenderer() in OnMemoryStateChange() breaks
    many things. We need a reliable and consistent way to suspend
    renderers. Until we come up with a solution, disable renderer
    suspending when memory coordinator is enabled. In follow-up
    CLs I'll add some tests to prevent this kind of regressions.
    
    BUG=675735,675811
    
    Review-Url: https://codereview.chromium.org/2590073002
    Cr-Commit-Position: refs/heads/master@{#439708}

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -1780,10 +1780,9 @@ void RenderThreadImpl::OnProcessPurgeAndSuspend() {
   if (!RendererIsHidden())
     return;
   if (base::FeatureList::IsEnabled(features::kPurgeAndSuspend)) {
-    // TODO(tasak): After enabling MemoryCoordinator, remove this Notify
-    // and follow MemoryCoordinator's request.
-    base::MemoryCoordinatorClientRegistry::GetInstance()->Notify(
-        base::MemoryState::SUSPENDED);
+    // TODO(tasak,bashi): After enabling MemoryCoordinator, stop calling
+    // SuspendRenderer() here.
+    SuspendRenderer();
   }
   // Since purging is not a synchronous task (e.g. v8 GC, oilpan GC, ...),
   // we need to wait until the task is finished. So wait 15 seconds and
@@ -1891,10 +1890,9 @@ void RenderThreadImpl::OnProcessResume() {
   if (!RendererIsHidden())
     return;
   if (base::FeatureList::IsEnabled(features::kPurgeAndSuspend)) {
-    // TODO(tasak): after enabling MemoryCoordinator, remove this Notify
-    // and follow MemoryCoordinator's request.
-    base::MemoryCoordinatorClientRegistry::GetInstance()->Notify(
-        base::MemoryState::NORMAL);
+    // TODO(tasak,bashi): After enabling MemoryCoordinator, stop calling
+    // ResumeRenderer() here.
+    ResumeRenderer();
   }
 }
 
@@ -2262,10 +2260,10 @@ void RenderThreadImpl::OnMemoryStateChange(base::MemoryState state) {
   }
   switch (state) {
     case base::MemoryState::NORMAL:
-      ResumeRenderer();
+      // TODO(bashi): When the renderer is suspended, resume it.
       break;
     case base::MemoryState::THROTTLED:
-      ResumeRenderer();
+      // TODO(bashi): When the renderer is suspended, resume it.
       // TODO(bashi): Figure out what kind of strategy is suitable on
       // THROTTLED state. crbug.com/674815
 #if defined(OS_ANDROID)
@@ -2277,7 +2275,10 @@ void RenderThreadImpl::OnMemoryStateChange(base::MemoryState state) {
       ReleaseFreeMemory();
       break;
     case base::MemoryState::SUSPENDED:
-      SuspendRenderer();
+      // TODO(bashi): Suspend the renderer.
+      OnTrimMemoryImmediately();
+      ReleaseFreeMemory();
+      ClearMemory();
       break;
     case base::MemoryState::UNKNOWN:
       NOTREACHED();
