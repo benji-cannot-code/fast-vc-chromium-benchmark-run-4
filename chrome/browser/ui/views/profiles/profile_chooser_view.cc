5a1a709bf290bce9594e9bafa641b096022597a5
estade <estade@chromium.org>
2016-12-20 11:07:08 -0800

    Don't request focus on press for buttons in profile menu.
    This prevents showing a dotted outline around a user row
    (in addition to the grey bg hover effect) when you click and hold.
    
    The only reason these were all explicitly set to request focus on
    press is because the default value for that bit was flipped back in
    May and most existing buttons were mechanically updated to stick with
    the old default.
    
    The focus effect should probably also be updated to something more MD-
    looking for when you do tab to a row, but that's left as an exercise for
    the future.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2589863002
    Cr-Commit-Position: refs/heads/master@{#439861}

--- chrome/browser/ui/views/profiles/profile_chooser_view.cc
+++ chrome/browser/ui/views/profiles/profile_chooser_view.cc
@@ -178,7 +178,6 @@ views::ImageButton* CreateBackButton(views::ButtonListener* listener) {
   back_button->SetImage(views::ImageButton::STATE_DISABLED,
                         rb->GetImageSkiaNamed(IDR_BACK_D));
   back_button->SetFocusForPlatform();
-  back_button->set_request_focus_on_press(true);
   return back_button;
 }
 
@@ -198,7 +197,6 @@ class BackgroundColorHoverButton : public views::LabelButton {
                                   : views::kButtonHEdgeMarginNew;
     SetBorder(views::CreateEmptyBorder(0, button_margin, 0, button_margin));
     SetFocusForPlatform();
-    set_request_focus_on_press(true);
 
     if (switches::IsMaterialDesignUserMenu()) {
       label()->SetHandlesTooltips(false);
@@ -1624,7 +1622,6 @@ views::View* ProfileChooserView::CreateCurrentProfileView(
             gfx::CreateVectorIcon(gfx::VectorIconId::WARNING, 18,
                                   gfx::kChromeIconGrey));
         auth_error_email_button_->SetFocusForPlatform();
-        auth_error_email_button_->set_request_focus_on_press(true);
         gfx::Insets insets =
             views::LabelButtonAssetBorder::GetDefaultInsetsForStyle(
                 views::Button::STYLE_TEXTBUTTON);
