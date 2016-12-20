c08c6faa80074a059f16d9f5ff5d3edd64243c6c
scottmg <scottmg@chromium.org>
2016-12-20 15:20:33 -0800

    SyzyASAN: Block for crash handler startup as temporary workaround for startup hang
    TBR=grt@chromium.org
    BUG=675710
    
    Review-Url: https://codereview.chromium.org/2591043002
    Cr-Commit-Position: refs/heads/master@{#439920}

--- chrome/app/chrome_exe_main_win.cc
+++ chrome/app/chrome_exe_main_win.cc
@@ -199,6 +199,11 @@ bool RemoveAppCompatFlagsEntry() {
 
 }  // namespace
 
+#if defined(SYZYASAN)
+// This is in chrome_elf.
+extern "C" void BlockUntilHandlerStartedImpl();
+#endif  // SYZYASAN
+
 #if !defined(WIN_CONSOLE_APP)
 int APIENTRY wWinMain(HINSTANCE instance, HINSTANCE prev, wchar_t*, int) {
 #else
@@ -232,6 +237,14 @@ int main() {
 
   // Signal Chrome Elf that Chrome has begun to start.
   SignalChromeElf();
+
+#if defined(SYZYASAN)
+  if (process_type.empty()) {
+    // This is a temporary workaround for a race during startup with the
+    // syzyasan_rtl.dll. See https://crbug.com/675710.
+    BlockUntilHandlerStartedImpl();
+  }
+#endif  // SYZYASAN
 
   // The exit manager is in charge of calling the dtors of singletons.
   base::AtExitManager exit_manager;
