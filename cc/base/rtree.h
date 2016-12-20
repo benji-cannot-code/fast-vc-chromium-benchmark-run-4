d1ae40c26f757ef46bb1091daec677a37b786329
vmpstr <vmpstr@chromium.org>
2016-12-19 19:05:02 -0800

    cc: Change RTree nodes container to be a vector (with reserve).
    This patch changes the nodes container in the rtree to use a vector.
    In order to preserve the consistent access to elements, it reserves the
    vector to the needed capacity. The wasted space is <= 6 elements.
    
    Added a unittest to verify the reserve path is correct for up to 1000
    rects. However, locally I've tested this for up to 520k rects (and
    still testing for up to 1m rects).
    
    The perf results from N7v2 are below.
    Before the patch:
    [ RUN      ] RTreePerfTest.Construct
    *RESULT rtree_construct: 100= 75723.8671875 runs/s
    *RESULT rtree_construct: 1000= 11985 runs/s
    *RESULT rtree_construct: 10000= 715.2415161132812 runs/s
    *RESULT rtree_construct: 100000= 54.19050216674805 runs/s
    [       OK ] RTreePerfTest.Construct (8243 ms)
    [ RUN      ] RTreePerfTest.Search
    *RESULT rtree_search: 100= 352105 runs/s
    *RESULT rtree_search: 1000= 97542.0234375 runs/s
    *RESULT rtree_search: 10000= 10274.0595703125 runs/s
    *RESULT rtree_search: 100000= 866.167236328125 runs/s
    [       OK ] RTreePerfTest.Search (8122 ms)
    
    After the patch:
    [ RUN      ] RTreePerfTest.Construct
    *RESULT rtree_construct: 100= 112420 runs/s (+48%)
    *RESULT rtree_construct: 1000= 13482.32421875 runs/s (+12%)
    *RESULT rtree_construct: 10000= 666.6328125 runs/s (-7%)
    *RESULT rtree_construct: 100000= 55.98017501831055 runs/s (+3%)
    [       OK ] RTreePerfTest.Construct (8340 ms)
    [ RUN      ] RTreePerfTest.Search
    *RESULT rtree_search: 100= 365225 runs/s
    *RESULT rtree_search: 1000= 98002.0078125 runs/s
    *RESULT rtree_search: 10000= 10344.048828125 runs/s
    *RESULT rtree_search: 100000= 866.0883178710938 runs/s
    [       OK ] RTreePerfTest.Search (8092 ms)
    
    BUG=674169
    R=danakj@chromium.org, dskiba@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2591533002
    Cr-Commit-Position: refs/heads/master@{#439686}

--- cc/base/rtree.h
+++ cc/base/rtree.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "cc/base/cc_export.h"
-#include "cc/base/contiguous_container.h"
 #include "ui/gfx/geometry/rect.h"
 
 namespace cc {
@@ -62,14 +61,33 @@ class CC_EXPORT RTree {
 
     num_data_elements_ = branches.size();
     if (num_data_elements_ == 1u) {
+      nodes_.reserve(1);
       Node* node = AllocateNodeAtLevel(0);
       node->num_children = 1;
       node->children[0] = branches[0];
       root_.subtree = node;
       root_.bounds = branches[0].bounds;
     } else if (num_data_elements_ > 1u) {
+      // Determine a reasonable upper bound on the number of nodes to prevent
+      // reallocations. This is basically (n**d - 1) / (n - 1), which is the
+      // number of nodes in a complete tree with n branches at each node. In the
+      // code n = |branch_count|, d = |depth|. However, we normally would have
+      // kMaxChildren branch factor, but that can be broken if some children
+      // don't have enough nodes. That can happen for at most kMinChildren nodes
+      // (since otherwise, we'd create a new node).
+      size_t branch_count = kMaxChildren;
+      double depth = log(branches.size()) / log(branch_count);
+      size_t node_count =
+          static_cast<size_t>((std::pow(branch_count, depth) - 1) /
+                              (branch_count - 1)) +
+          kMinChildren;
+      nodes_.reserve(node_count);
+
       root_ = BuildRecursive(&branches, 0);
     }
+    // We should've wasted at most kMinChildren nodes.
+    DCHECK_LE(nodes_.capacity() - nodes_.size(),
+              static_cast<size_t>(kMinChildren));
   }
 
   template <typename Container>
@@ -84,7 +102,8 @@ class CC_EXPORT RTree {
  private:
   // These values were empirically determined to produce reasonable performance
   // in most cases.
-  enum { MIN_CHILDREN = 6, MAX_CHILDREN = 11 };
+  enum { kMinChildren = 6 };
+  enum { kMaxChildren = 11 };
 
   struct Node;
   struct Branch {
@@ -102,7 +121,7 @@ class CC_EXPORT RTree {
   struct Node {
     uint16_t num_children;
     uint16_t level;
-    Branch children[MAX_CHILDREN];
+    Branch children[kMaxChildren];
   };
 
   void SearchRecursive(Node* root,
@@ -116,7 +135,7 @@ class CC_EXPORT RTree {
   // This is the count of data elements (rather than total nodes in the tree)
   size_t num_data_elements_;
   Branch root_;
-  ContiguousContainer<Node> nodes_;
+  std::vector<Node> nodes_;
 };
 
 }  // namespace cc
