b876c2bfc39502aacf7e11fb330f1854c9a585b8
zhaobin <zhaobin@chromium.org>
2016-12-20 15:16:34 -0800

    [Presentation API] use media_source().id instead of media_source().url to initialize presentation_url_ in RouteRequestResult
    In PresentationServiceDelegateImpl::OnRouteResponse, result.presentation_url() is empty, and cast fails. When we get route from MRP, media_source is created with plain string, leaving media_source.url() empty. Use media_source().id to initialize RouteRequestResult.presentation_url instead.
    
    BUG=675790
    
    Review-Url: https://codereview.chromium.org/2587943003
    Cr-Commit-Position: refs/heads/master@{#439917}

--- chrome/browser/media/router/media_source.cc
+++ chrome/browser/media/router/media_source.cc
@@ -7,9 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "chrome/browser/media/router/media_source_helper.h"
+
 namespace media_router {
 
 MediaSource::MediaSource(const MediaSource::Id& source_id) : id_(source_id) {
+  GURL url(source_id);
+  if (IsValidPresentationUrl(url))
+    url_ = url;
 }
 
 MediaSource::MediaSource(const GURL& presentation_url)
