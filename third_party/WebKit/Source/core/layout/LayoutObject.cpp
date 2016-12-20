0ee9b094c70e2919fe527f4cfbc313254aae3ff6
mstensho <mstensho@opera.com>
2016-12-20 10:50:14 -0800

    Need to be inside the flow thread before converting a visual point.
    Before we can convert from a visual point to a flow thread point, we need the
    input point to be exactly relative to the flow thread, or we might end up
    mapping it to the wrong column.
    
    In other words, we need to add the flow thread object's location before
    converting into the flow thread coordinate space. While the flow thread indeed
    is at 0,0 (or at least close enough to 0,0) relatively to the multicol
    container in very many cases, this isn't true when the multicol container is in
    rtl writing-mode, or when it's scrollable, or even when the multicol container
    has borders and/or padding.
    
    BUG=663062
    
    Review-Url: https://codereview.chromium.org/2593633002
    Cr-Commit-Position: refs/heads/master@{#439855}

--- third_party/WebKit/Source/core/layout/LayoutObject.cpp
+++ third_party/WebKit/Source/core/layout/LayoutObject.cpp
@@ -2165,15 +2165,6 @@ void LayoutObject::mapAncestorToLocal(const LayoutBoxModelObject* ancestor,
     container->mapAncestorToLocal(ancestor, transformState, mode);
 
   LayoutSize containerOffset = offsetFromContainer(container);
-  if (isLayoutFlowThread()) {
-    // Descending into a flow thread. Convert to the local coordinate space,
-    // i.e. flow thread coordinates.
-    LayoutPoint visualPoint = LayoutPoint(transformState.mappedPoint());
-    transformState.move(
-        visualPoint -
-        toLayoutFlowThread(this)->visualPointToFlowThreadPoint(visualPoint));
-  }
-
   bool preserve3D =
       mode & UseTransforms &&
       (container->style()->preserves3D() || style()->preserves3D());
@@ -2187,6 +2178,15 @@ void LayoutObject::mapAncestorToLocal(const LayoutBoxModelObject* ancestor,
     transformState.move(containerOffset.width(), containerOffset.height(),
                         preserve3D ? TransformState::AccumulateTransform
                                    : TransformState::FlattenTransform);
+  }
+
+  if (isLayoutFlowThread()) {
+    // Descending into a flow thread. Convert to the local coordinate space,
+    // i.e. flow thread coordinates.
+    LayoutPoint visualPoint = LayoutPoint(transformState.mappedPoint());
+    transformState.move(
+        visualPoint -
+        toLayoutFlowThread(this)->visualPointToFlowThreadPoint(visualPoint));
   }
 
   if (applyContainerFlip) {
