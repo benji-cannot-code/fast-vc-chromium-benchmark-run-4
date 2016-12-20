e838bf2de98df57d3681114427f4916c9683ef9b
ecobos <ecobos@igalia.com>
2016-12-20 07:13:16 -0800

    Import display: contents tests from csswg-test.
    This were reviewed upstream in https://github.com/w3c/csswg-test/pull/1155
    
    This should reduce the noise in https://codereview.chromium.org/2450093005, and
    make it more manageable and straight-forward to review.
    
    BUG=657748
    
    Review-Url: https://codereview.chromium.org/2588093003
    Cr-Commit-Position: refs/heads/master@{#439805}

---
+++ third_party/WebKit/LayoutTests/imported/csswg-test/css-display-3/support/util.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function eachDisplayContentsElementIn(document, window, callbackDo, callbackUndo) {
+  var elements = [];
+
+  document.body.offsetHeight;
+
+  // NOTE: Doing qsa('*') and getComputedStyle is just for the
+  // test's sake, since it's easier to mess it up when
+  // getComputedStyle is involved.
+  var all = document.querySelectorAll('*');
+  for (var i = 0; i < all.length; ++i) {
+    if (window.getComputedStyle(all[i]).display === "contents") {
+      callbackDo(all[i]);
+      elements.push(all[i]);
+    }
+  }
+
+  document.body.offsetHeight;
+
+  for (var i = 0; i < elements.length; ++i)
+    callbackUndo(elements[i]);
+
+  document.body.offsetHeight;
+}
