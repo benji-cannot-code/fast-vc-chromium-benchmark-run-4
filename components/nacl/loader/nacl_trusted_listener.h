b5033f3853e1ad7d74e315b3fa50e7448ee0c0c0
blundell <blundell@chromium.org>
2016-12-20 05:21:06 -0800

    Convert NaCl renderer-loader messages to mojo.
    Author: Sam McNally <sammc@chromium.org>
    
    Reland of https://codereview.chromium.org/2525483002/, with trivial fixes
    applied to fix bustage that occurred due to that CL racing with
    https://codereview.chromium.org/2589663003/.
    
    BUG=577685
    TBR=sammc, dcheng
    
    Review-Url: https://codereview.chromium.org/2590173002
    Cr-Commit-Position: refs/heads/master@{#439783}

--- components/nacl/loader/nacl_trusted_listener.h
+++ components/nacl/loader/nacl_trusted_listener.h
@@ -7,32 +7,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_NACL_LOADER_NACL_TRUSTED_LISTENER_H_
 
 #include "base/macros.h"
-#include "base/memory/ref_counted.h"
-#include "ipc/ipc_channel_handle.h"
-#include "ipc/ipc_listener.h"
-#include "ipc/ipc_sync_channel.h"
+#include "components/nacl/common/nacl.mojom.h"
 
 namespace base {
 class SingleThreadTaskRunner;
 }
 
-class NaClTrustedListener : public base::RefCounted<NaClTrustedListener>,
-                            public IPC::Listener {
+class NaClTrustedListener {
  public:
-  NaClTrustedListener(const IPC::ChannelHandle& handle,
-                      base::SingleThreadTaskRunner* ipc_task_runner,
-                      base::WaitableEvent* shutdown_event);
+  NaClTrustedListener(nacl::mojom::NaClRendererHostPtr renderer_host,
+                      base::SingleThreadTaskRunner* io_task_runner);
+  ~NaClTrustedListener();
 
-  // Listener implementation.
-  bool OnMessageReceived(const IPC::Message& message) override;
-
-  bool Send(IPC::Message* msg);
+  nacl::mojom::NaClRendererHost* renderer_host() {
+    return renderer_host_.get();
+  }
 
  private:
-  friend class base::RefCounted<NaClTrustedListener>;
-  ~NaClTrustedListener() override;
-  IPC::ChannelHandle channel_handle_;
-  std::unique_ptr<IPC::SyncChannel> channel_;
+  nacl::mojom::NaClRendererHostPtr renderer_host_;
 
   DISALLOW_COPY_AND_ASSIGN(NaClTrustedListener);
 };
