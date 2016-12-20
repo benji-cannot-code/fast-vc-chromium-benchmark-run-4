2c6ebbd9429616a78f19a72377a98ef38fe5b7a7
vabr <vabr@chromium.org>
2016-12-19 23:52:29 -0800

    Remove Finch support for PasswordBranding
    PasswordBranding has launched in the form of 100% being SAVE_PROMPT_ONLY.
    
    This CL removes the code to obtain the Finch group for the experiment, related
    tests, and also the functionality only executed for the unused FULL variant of
    the experiment (mainly the SmartLock warm welcome).
    
    BUG=486739
    
    Review-Url: https://codereview.chromium.org/2579823003
    Cr-Commit-Position: refs/heads/master@{#439740}

--- chrome/browser/password_manager/update_password_infobar_delegate_android.cc
+++ chrome/browser/password_manager/update_password_infobar_delegate_android.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/browser_sync/profile_sync_service.h"
 #include "components/infobars/core/infobar.h"
 #include "components/password_manager/core/browser/password_bubble_experiment.h"
+#include "components/strings/grit/components_strings.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/base/l10n/l10n_util.h"
 
@@ -26,7 +27,7 @@ void UpdatePasswordInfoBarDelegate::Create(
     content::WebContents* web_contents,
     std::unique_ptr<password_manager::PasswordFormManager> form_to_save) {
   const bool is_smartlock_branding_enabled =
-      password_bubble_experiment::IsSmartLockBrandingSavePromptEnabled(
+      password_bubble_experiment::IsSmartLockUser(
           ProfileSyncServiceFactory::GetForProfile(
               Profile::FromBrowserContext(web_contents->GetBrowserContext())));
   InfoBarService::FromWebContents(web_contents)
@@ -39,10 +40,9 @@ void UpdatePasswordInfoBarDelegate::Create(
 UpdatePasswordInfoBarDelegate::~UpdatePasswordInfoBarDelegate() {}
 
 base::string16 UpdatePasswordInfoBarDelegate::GetBranding() const {
-  return l10n_util::GetStringUTF16(
-      is_smartlock_branding_enabled_
-          ? IDS_PASSWORD_MANAGER_SMART_LOCK_FOR_PASSWORDS
-          : IDS_PASSWORD_MANAGER_TITLE_BRAND);
+  return l10n_util::GetStringUTF16(is_smartlock_branding_enabled_
+                                       ? IDS_PASSWORD_MANAGER_SMART_LOCK
+                                       : IDS_PASSWORD_MANAGER_TITLE_BRAND);
 }
 
 bool UpdatePasswordInfoBarDelegate::ShowMultipleAccounts() const {
