b8cbd93938796dd4d0f50f19364a27a7e7376130
hcarmona <hcarmona@chromium.org>
2016-12-20 15:51:03 -0800

    [MD-Settings] Add Ellipsis for the Address List Pieces.
    BUG=669117
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2569313002
    Cr-Commit-Position: refs/heads/master@{#439934}

--- chrome/browser/resources/settings/passwords_and_forms_page/autofill_section.js
+++ chrome/browser/resources/settings/passwords_and_forms_page/autofill_section.js
@@ -48,15 +48,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     },
 
     /**
-     * Formats an AddressEntry so it's displayed as an address.
-     * @param {!chrome.autofillPrivate.AddressEntry} item
-     * @return {string}
-     */
-    address_: function(item) {
-      return item.metadata.summaryLabel + item.metadata.summarySublabel;
-    },
-
-    /**
      * Formats the expiration date so it's displayed as MM/YYYY.
      * @param {!chrome.autofillPrivate.CreditCardEntry} item
      * @return {string}
