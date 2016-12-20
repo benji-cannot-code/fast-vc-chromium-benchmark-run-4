5e256d9c9ae74ab78ecd3b60357ae22eeb0c4f69
raymes <raymes@chromium.org>
2016-12-19 20:08:42 -0800

    Move kill switch and secure origin checking logic
    Currently the permission kill switch check and secure origin checks happen in
    PermissionContextBase::GetPermissionStatus. This function is virtual which
    means that it's easy to accidentally override this logic unintentionally,
    and hard to insert extra permission checks which take precedence over the user
    setting but not over the kill switch.
    
    This CL makes GetPermissionStatus non-virtual such that the kill switch and
    origin checking logic is always called and adds a new virtual
    GetPermissionStatusInternal function which subclasses can override as needed.
    
    TBR=tommycli@chromium.org
    
    BUG=674389
    
    Review-Url: https://codereview.chromium.org/2586153002
    Cr-Commit-Position: refs/heads/master@{#439700}

--- chrome/browser/permissions/permission_context_base.h
+++ chrome/browser/permissions/permission_context_base.h
@@ -82,9 +82,8 @@ class PermissionContextBase : public KeyedService {
                                  const BrowserPermissionCallback& callback);
 
   // Returns whether the permission has been granted, denied...
-  virtual ContentSetting GetPermissionStatus(
-      const GURL& requesting_origin,
-      const GURL& embedding_origin) const;
+  ContentSetting GetPermissionStatus(const GURL& requesting_origin,
+                                     const GURL& embedding_origin) const;
 
   // Resets the permission to its default value.
   virtual void ResetPermission(const GURL& requesting_origin,
@@ -101,6 +100,10 @@ class PermissionContextBase : public KeyedService {
   bool IsPermissionKillSwitchOn() const;
 
  protected:
+  virtual ContentSetting GetPermissionStatusInternal(
+      const GURL& requesting_origin,
+      const GURL& embedding_origin) const;
+
   // Decide whether the permission should be granted.
   // Calls PermissionDecided if permission can be decided non-interactively,
   // or NotifyPermissionSet if permission decided by presenting an infobar.
