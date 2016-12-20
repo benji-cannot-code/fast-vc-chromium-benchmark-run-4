fafd244836184d6c6011e81d87c1ddd38d60ce51
jam <jam@chromium.org>
2016-12-19 23:41:06 -0800

    Revert of Temporarily enable PlzNavigate to see perf bot results. (patchset #1 id:1 of https://codereview.chromium.org/2584093003/ )
    Reason for revert:
    Change cycled to perf bots.
    
    Original issue's description:
    > Temporarily enable PlzNavigate to see perf bot results.
    >
    > BUG=368813
    > TBR=clamy@chromium.org
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    >
    > Committed: https://chromium.googlesource.com/chromium/src/+/3d001c287618b8298e5b95bd98556a5beffc0316
    > Committed: https://crrev.com/722be6f390eba4c47669ad4307d2580dc09762dc
    > Cr-Commit-Position: refs/heads/master@{#439731}
    
    TBR=clamy@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=368813
    
    Review-Url: https://codereview.chromium.org/2587333002
    Cr-Commit-Position: refs/heads/master@{#439732}

--- content/public/common/browser_side_navigation_policy.cc
+++ content/public/common/browser_side_navigation_policy.cc
@@ -11,7 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 bool IsBrowserSideNavigationEnabled() {
-  return true;
+  return base::CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kEnableBrowserSideNavigation);
 }
 
 }  // namespace content
