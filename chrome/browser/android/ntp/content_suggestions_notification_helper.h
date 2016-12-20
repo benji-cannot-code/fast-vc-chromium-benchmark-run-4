e0f16a38fea412d996aaf36cc651b15501ef34fc
sfiera <sfiera@chromium.org>
2016-12-20 05:13:38 -0800

    Add helper to open URL in new activity
    BUG=675561
    
    Review-Url: https://codereview.chromium.org/2586293002
    Cr-Commit-Position: refs/heads/master@{#439779}

---
+++ chrome/browser/android/ntp/content_suggestions_notification_helper.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ANDROID_NTP_CONTENT_SUGGESTIONS_NOTIFICATION_HELPER_H_
+#define CHROME_BROWSER_ANDROID_NTP_CONTENT_SUGGESTIONS_NOTIFICATION_HELPER_H_
+
+#include "base/macros.h"
+#include "url/gurl.h"
+
+namespace ntp_snippets {
+
+class ContentSuggestionsNotificationHelper {
+ public:
+  static void OpenURL(const GURL& url);
+
+ private:
+  DISALLOW_IMPLICIT_CONSTRUCTORS(ContentSuggestionsNotificationHelper);
+};
+
+}  // namespace ntp_snippets
+
+#endif  // CHROME_BROWSER_ANDROID_NTP_CONTENT_SUGGESTIONS_NOTIFICATION_HELPER_H_
