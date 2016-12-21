25e11fdd8f34d5f8e9024bbc92cb481dacf23f50
rsorokin <rsorokin@chromium.org>
2016-12-21 07:53:29 -0800

    Enable Active Directory shortcut on the welcome screen
    BUG=668459
    TEST=manual
    
    Review-Url: https://codereview.chromium.org/2577093002
    Cr-Commit-Position: refs/heads/master@{#440110}

--- chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
@@ -1217,7 +1217,6 @@ void SigninScreenHandler::HandleToggleEnrollmentAd() {
   chromeos::DBusThreadManager::Get()
       ->GetUpstartClient()
       ->StartAuthPolicyService();
-  HandleToggleEnrollmentScreen();
 }
 
 void SigninScreenHandler::HandleToggleEnableDebuggingScreen() {
--- ui/login/display_manager.js
+++ ui/login/display_manager.js
@@ -345,7 +345,10 @@ cr.define('cr.ui.login', function() {
                 currentStepId) != -1) {
           chrome.send('toggleEnableDebuggingScreen');
         }
-      } else if (name == ACCELERATOR_ENROLLMENT) {
+      } else if (name == ACCELERATOR_ENROLLMENT ||
+                 name == ACCELERATOR_ENROLLMENT_AD) {
+        if (name == ACCELERATOR_ENROLLMENT_AD)
+          chrome.send('toggleEnrollmentAd');
         if (currentStepId == SCREEN_GAIA_SIGNIN ||
             currentStepId == SCREEN_ACCOUNT_PICKER) {
           chrome.send('toggleEnrollmentScreen');
@@ -354,11 +357,6 @@ cr.define('cr.ui.login', function() {
           // In this case update check will be skipped and OOBE will
           // proceed straight to enrollment screen when EULA is accepted.
           chrome.send('skipUpdateEnrollAfterEula');
-        }
-      } else if (name == ACCELERATOR_ENROLLMENT_AD) {
-        if (currentStepId == SCREEN_GAIA_SIGNIN ||
-            currentStepId == SCREEN_ACCOUNT_PICKER) {
-          chrome.send('toggleEnrollmentAd');
         }
       } else if (name == ACCELERATOR_KIOSK_ENABLE) {
         if (currentStepId == SCREEN_GAIA_SIGNIN ||
