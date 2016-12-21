4098d3ce8633566dfb7fea3af22391299b063470
reed <reed@google.com>
2016-12-21 15:48:34 -0800

    remove SK_SUPPORT_LEGACY_PREROLL flag
    BUG=
    
    Review-Url: https://codereview.chromium.org/2591673003
    Cr-Commit-Position: refs/heads/master@{#440267}

--- third_party/WebKit/Source/platform/graphics/DeferredImageDecoderTestWoPlatform.cpp
+++ third_party/WebKit/Source/platform/graphics/DeferredImageDecoderTestWoPlatform.cpp
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/SharedBuffer.h"
 #include "platform/image-decoders/ImageDecoderTestHelpers.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkImage.h"
+#include "third_party/skia/include/core/SkSurface.h"
 #include "wtf/RefPtr.h"
 #include <memory>
 
@@ -20,8 +22,8 @@ namespace blink {
  *  SkImage* imageA = decoder.createFrameAtIndex(0);
  *  // supply more (but not all) data to the decoder
  *  SkImage* imageB = decoder.createFrameAtIndex(laterFrame);
- *  imageB->preroll();
- *  imageA->preroll();
+ *  draw(imageB);
+ *  draw(imageA);
  *
  *  This results in using the same ImageDecoder (in the ImageDecodingStore) to
  *  decode less data the second time. This test ensures that it is safe to do
@@ -50,8 +52,11 @@ static void mixImages(const char* fileName,
   decoder->setData(almostCompleteFile, false);
   sk_sp<SkImage> imageWithMoreData = decoder->createFrameAtIndex(laterFrame);
 
-  imageWithMoreData->preroll();
-  partialImage->preroll();
+  // we now want to ensure we don't crash if we access these in this order
+  SkImageInfo info = SkImageInfo::MakeN32Premul(10, 10);
+  sk_sp<SkSurface> surf = SkSurface::MakeRaster(info);
+  surf->getCanvas()->drawImage(imageWithMoreData, 0, 0);
+  surf->getCanvas()->drawImage(partialImage, 0, 0);
 }
 
 TEST(DeferredImageDecoderTestWoPlatform, mixImagesGif) {
