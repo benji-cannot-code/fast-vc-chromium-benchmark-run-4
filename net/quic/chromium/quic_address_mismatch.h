44ccecd00c68d693e6938aff8b52999fbe1471e7
rch <rch@chromium.org>
2016-12-20 22:21:28 -0800

    Move quic_address_mismatch from net/quic/core to net/quic/chromium and remove unused quic_stream_sequencer_buffer_interface.h.
    Review-Url: https://codereview.chromium.org/2593923002
    Cr-Commit-Position: refs/heads/master@{#440041}

--- net/quic/chromium/quic_address_mismatch.h
+++ net/quic/chromium/quic_address_mismatch.h
@@ -3,11 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef NET_QUIC_CORE_QUIC_ADDRESS_MISMATCH_H_
-#define NET_QUIC_CORE_QUIC_ADDRESS_MISMATCH_H_
+#ifndef NET_QUIC_CHROMIUM_QUIC_ADDRESS_MISMATCH_H_
+#define NET_QUIC_CHROMIUM_QUIC_ADDRESS_MISMATCH_H_
 
 #include "net/base/ip_endpoint.h"
-#include "net/quic/platform/api/quic_export.h"
+#include "net/base/net_export.h"
 
 namespace net {
 
@@ -36,9 +36,9 @@ enum QuicAddressMismatch {
 // is empty.
 //
 // Only used by the Net.QuicSession.PublicResetAddressMismatch histogram.
-QUIC_EXPORT_PRIVATE int GetAddressMismatch(const IPEndPoint& first_address,
-                                           const IPEndPoint& second_address);
+NET_EXPORT_PRIVATE int GetAddressMismatch(const IPEndPoint& first_address,
+                                          const IPEndPoint& second_address);
 
 }  // namespace net
 
-#endif  // NET_QUIC_CORE_QUIC_ADDRESS_MISMATCH_H_
+#endif  // NET_QUIC_CHROMIUM_QUIC_ADDRESS_MISMATCH_H_
