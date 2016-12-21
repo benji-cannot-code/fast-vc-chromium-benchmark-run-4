7316275f7c8e974c32ae7cb888bbf7484089c2e9
staraz <staraz@chromium.org>
2016-12-21 12:03:45 -0800

    GpuCompositorFrameSink implements cc::mojom::DisplayPrivate
    Added DisplayPrivate mojo interface to hide cc::Display from ContextFactory.
    
    The intent of this change is to content mus-ws with mus-gpu for cc::Display
    operations.
    
    ColorSpace and OutputIsSecure and so on is passed along from display management
    code which resides in Mus-ws. cc::Display lives in mus-gpu and so we need a mojo
    interface for cc::Display.
    
    Currently ash talks to cc::Display through ContextFactoryPrivate. This mojo
    interface would eventually replace ContextFactoryPrivate or would be used by
    ContextFactoryPrivate.
    
    BUG=670364
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    TBR=danakj@chromium.org
    
    Review-Url: https://codereview.chromium.org/2579693004
    Cr-Commit-Position: refs/heads/master@{#440192}

--- services/ui/surfaces/display_compositor.h
+++ services/ui/surfaces/display_compositor.h
@@ -89,7 +89,8 @@ class DisplayCompositor : public cc::SurfaceObserver,
       gpu::SurfaceHandle surface_handle,
       cc::mojom::MojoCompositorFrameSinkRequest request,
       cc::mojom::MojoCompositorFrameSinkPrivateRequest private_request,
-      cc::mojom::MojoCompositorFrameSinkClientPtr client) override;
+      cc::mojom::MojoCompositorFrameSinkClientPtr client,
+      cc::mojom::DisplayPrivateRequest display_request) override;
   void CreateOffscreenCompositorFrameSink(
       const cc::FrameSinkId& frame_sink_id,
       cc::mojom::MojoCompositorFrameSinkRequest request,
@@ -114,7 +115,8 @@ class DisplayCompositor : public cc::SurfaceObserver,
       std::unique_ptr<cc::SyntheticBeginFrameSource> begin_frame_source,
       cc::mojom::MojoCompositorFrameSinkRequest request,
       cc::mojom::MojoCompositorFrameSinkPrivateRequest private_request,
-      cc::mojom::MojoCompositorFrameSinkClientPtr client);
+      cc::mojom::MojoCompositorFrameSinkClientPtr client,
+      cc::mojom::DisplayPrivateRequest display_private_request);
 
   const cc::SurfaceId& GetRootSurfaceId() const;
 
