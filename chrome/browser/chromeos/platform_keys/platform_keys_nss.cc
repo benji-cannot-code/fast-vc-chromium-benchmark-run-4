b322a556f414bf515950aeb8db3755a4e344305a
fdoray <fdoray@chromium.org>
2016-12-21 13:21:37 -0800

    Update TaskTraits in platform_keys_nss.cc.
    The .WithFileIO() and .WithWait() traits were deprecated in favor of
    .MayBlock() and .WithSyncPrimitives(). See rationale in
    https://docs.google.com/a/chromium.org/document/d/1ynRvQKah3Cx_eLqPS7KzIp8u73HeKIjX7HQqwSWOnF8/edit?usp=sharing
    
    .MayBlock()
    Tasks with this trait may block. This includes but is not limited to tasks
    that wait on synchronous file I/O operations: read or write a file from
    disk, interact with a pipe or a socket, rename or delete a file, enumerate
    files in a directory, etc. This trait does not allow tasks to wait on
    synchronization primitives (thread or process handles, waitable events,
    condition variables).
    
    .WithSyncPrimitives()
    Tasks with this trait are allowed to block on synchronization primitives
    (wait on a waitable event/condition variable, join a process/thread).
    This trait implies MayBlock(), i.e. you do not need to specify MayBlock()
    for a task that blocks exclusively on synchronization primitives.
    
    R=gab@chromium.org
    BUG=675660
    
    Review-Url: https://codereview.chromium.org/2595903002
    Cr-Commit-Position: refs/heads/master@{#440217}

--- chrome/browser/chromeos/platform_keys/platform_keys_nss.cc
+++ chrome/browser/chromeos/platform_keys/platform_keys_nss.cc
@@ -442,11 +442,10 @@ void GenerateRSAKeyWithDB(std::unique_ptr<GenerateRSAKeyState> state,
                           net::NSSCertDatabase* cert_db) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
   // Only the slot and not the NSSCertDatabase is required. Ignore |cert_db|.
-  // This task interacts with the TPM, hence WithFileIO() and WithWait().
+  // This task interacts with the TPM, hence MayBlock().
   base::PostTaskWithTraits(
       FROM_HERE, base::TaskTraits()
-                     .WithFileIO()
-                     .WithWait()
+                     .MayBlock()
                      .WithPriority(base::TaskPriority::BACKGROUND)
                      .WithShutdownBehavior(
                          base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN),
@@ -539,11 +538,10 @@ void SignRSAWithDB(std::unique_ptr<SignRSAState> state,
                    net::NSSCertDatabase* cert_db) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
   // Only the slot and not the NSSCertDatabase is required. Ignore |cert_db|.
-  // This task interacts with the TPM, hence WithFileIO() and WithWait().
+  // This task interacts with the TPM, hence MayBlock().
   base::PostTaskWithTraits(
       FROM_HERE, base::TaskTraits()
-                     .WithFileIO()
-                     .WithWait()
+                     .MayBlock()
                      .WithPriority(base::TaskPriority::BACKGROUND)
                      .WithShutdownBehavior(
                          base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN),
@@ -610,11 +608,10 @@ void DidGetCertificates(std::unique_ptr<GetCertificatesState> state,
                         std::unique_ptr<net::CertificateList> all_certs) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
   state->certs_ = std::move(all_certs);
-  // This task interacts with the TPM, hence WithFileIO() and WithWait().
+  // This task interacts with the TPM, hence MayBlock().
   base::PostTaskWithTraits(
       FROM_HERE, base::TaskTraits()
-                     .WithFileIO()
-                     .WithWait()
+                     .MayBlock()
                      .WithPriority(base::TaskPriority::BACKGROUND)
                      .WithShutdownBehavior(
                          base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN),
