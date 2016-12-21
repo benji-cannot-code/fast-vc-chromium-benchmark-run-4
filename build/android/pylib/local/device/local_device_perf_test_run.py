c4a183312bd0b8b0ad35343bfd59564a12b39db7
jbudorick <jbudorick@chromium.org>
2016-12-21 07:18:11 -0800

    [android] Use contextlib_ext.Optional for conditional context managers.
    Scripts in //build/android have been using hand-rolled workarounds for
    conditional context managers in the past. This switches them to a new
    implementation I added to catapult in
    https://codereview.chromium.org/2585763004/
    
    Review-Url: https://codereview.chromium.org/2593903002
    Cr-Commit-Position: refs/heads/master@{#440103}

--- build/android/pylib/local/device/local_device_perf_test_run.py
+++ build/android/pylib/local/device/local_device_perf_test_run.py
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 import collections
-import contextlib
 import io
 import json
 import logging
@@ -32,6 +31,7 @@ from pylib.constants import host_paths
 from pylib.local.device import local_device_environment
 from pylib.local.device import local_device_test_run
 from py_trace_event import trace_event
+from py_utils import contextlib_ext
 
 
 class HeartBeat(object):
@@ -97,20 +97,12 @@ class TestShard(object):
 
     self._LogTest(test, cmd, timeout)
 
-    @contextlib.contextmanager
-    def trace_if_enabled(test):
-      try:
-        if self._test_instance.trace_output:
-          trace_event.trace_begin(test)
-        yield
-      finally:
-        if self._test_instance.trace_output:
-          trace_event.trace_end(test)
-
     try:
       start_time = time.time()
 
-      with trace_if_enabled(test):
+      with contextlib_ext.Optional(
+          trace_event.trace(test),
+          self._test_instance.trace_output):
         exit_code, output = cmd_helper.GetCmdStatusAndOutputWithTimeout(
             cmd, timeout, cwd=cwd, shell=True)
       end_time = time.time()
