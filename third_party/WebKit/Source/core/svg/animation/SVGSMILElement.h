3d9c3d4a533f7aadf4b212899049c52b6814d1ef
fs <fs@opera.com>
2016-12-21 11:11:53 -0800

    Improve separation between "SMIL times" and timestamps
    The 'elapsed' time does not need to be a SMILTime in general - it can
    only be non-finite in the case where the document is not active, and
    then the timeline should not be running/animations updated.
    Thread the double value further from SMILTimeContainer down into
    SVGSMILElement. Simplify some computations.
    
    BUG=641437,676137
    
    Review-Url: https://codereview.chromium.org/2592103002
    Cr-Commit-Position: refs/heads/master@{#440171}

--- third_party/WebKit/Source/core/svg/animation/SVGSMILElement.h
+++ third_party/WebKit/Source/core/svg/animation/SVGSMILElement.h
@@ -86,8 +86,8 @@ class CORE_EXPORT SVGSMILElement : public SVGElement, public SVGTests {
   SMILTime previousIntervalBegin() const { return m_previousIntervalBegin; }
   SMILTime simpleDuration() const;
 
-  void seekToIntervalCorrespondingToTime(SMILTime elapsed);
-  bool progress(SMILTime elapsed, bool seekToTime);
+  void seekToIntervalCorrespondingToTime(double elapsed);
+  bool progress(double elapsed, bool seekToTime);
   SMILTime nextProgressTime() const;
   void updateAnimatedValue(SVGSMILElement* resultElement) {
     updateAnimation(m_lastPercent, m_lastRepeat, resultElement);
@@ -98,7 +98,7 @@ class CORE_EXPORT SVGSMILElement : public SVGElement, public SVGTests {
   static SMILTime parseClockValue(const String&);
   static SMILTime parseOffsetValue(const String&);
 
-  bool isContributing(SMILTime elapsed) const;
+  bool isContributing(double elapsed) const;
   bool isFrozen() const;
 
   unsigned documentOrderIndex() const { return m_documentOrderIndex; }
@@ -177,7 +177,7 @@ class CORE_EXPORT SVGSMILElement : public SVGElement, public SVGTests {
 
   enum RestartedInterval { DidNotRestartInterval, DidRestartInterval };
 
-  RestartedInterval maybeRestartInterval(SMILTime elapsed);
+  RestartedInterval maybeRestartInterval(double elapsed);
   void beginListChanged(SMILTime eventTime);
   void endListChanged(SMILTime eventTime);
 
@@ -248,9 +248,9 @@ class CORE_EXPORT SVGSMILElement : public SVGElement, public SVGTests {
   QualifiedName m_attributeName;
 
   ActiveState determineActiveState(SMILTime elapsed) const;
-  float calculateAnimationPercentAndRepeat(SMILTime elapsed,
+  float calculateAnimationPercentAndRepeat(double elapsed,
                                            unsigned& repeat) const;
-  SMILTime calculateNextProgressTime(SMILTime elapsed) const;
+  SMILTime calculateNextProgressTime(double elapsed) const;
 
   Member<SVGElement> m_targetElement;
 
