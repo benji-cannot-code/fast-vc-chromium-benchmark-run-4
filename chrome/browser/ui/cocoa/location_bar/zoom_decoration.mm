0a1039676976156ed4bc84b1e76df617d37e1ba8
lgrey <lgrey@chromium.org>
2016-12-21 12:56:13 -0800

    [Mac] Fix bubble anchors in RTL
    Previously, Bubble decoration and subclasses used slightly different
    code to calculate where the image *should* be drawn, and where it was
    drawn in practice. This change unites that code and removes code that
    compensated for the difference.
    
    The change in ContentSettingsBubble isn't RTL specific and fixes a
    general regression (content settings bubbles in Canary are anchored
    too far to the right.)
    
    BUG=648557
    
    Review-Url: https://codereview.chromium.org/2594543004
    Cr-Commit-Position: refs/heads/master@{#440206}

--- chrome/browser/ui/cocoa/location_bar/zoom_decoration.mm
+++ chrome/browser/ui/cocoa/location_bar/zoom_decoration.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "base/strings/string_number_conversions.h"
 #include "chrome/app/chrome_command_ids.h"
+#import "chrome/browser/ui/cocoa/l10n_util.h"
 #import "chrome/browser/ui/cocoa/location_bar/autocomplete_text_field.h"
 #import "chrome/browser/ui/cocoa/location_bar/autocomplete_text_field_cell.h"
 #import "chrome/browser/ui/cocoa/location_bar/location_bar_view_mac.h"
@@ -117,7 +118,10 @@ void ZoomDecoration::ShowAndUpdateUI(zoom::ZoomController* zoom_controller,
 }
 
 NSPoint ZoomDecoration::GetBubblePointInFrame(NSRect frame) {
-  return NSMakePoint(NSMaxX(frame), NSMaxY(frame));
+  return NSMakePoint(cocoa_l10n_util::ShouldDoExperimentalRTLLayout()
+                         ? NSMinX(frame)
+                         : NSMaxX(frame),
+                     NSMaxY(frame));
 }
 
 bool ZoomDecoration::IsAtDefaultZoom() const {
