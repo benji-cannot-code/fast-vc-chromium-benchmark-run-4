b8b14561d5a3cd616a06874842d29b9e661112f7
emircan <emircan@chromium.org>
2016-12-21 14:24:42 -0800

    Avoid float-cast-overflow in content::VideoCapturerSource::GetCurrentSupportedFormats
    This CL addresses the fuzz issue below by limiting the max fps given to
    canvas capture by Chrome defined media::limits::kMaxFramesPerSecond. Note
    that Chrome expects every video source to report GetCurrentSupportedFormats()
    within the expected fps range. This limit isn't related to the blink side.
    
    BUG=675309
    
    Review-Url: https://codereview.chromium.org/2590823004
    Cr-Commit-Position: refs/heads/master@{#440241}

--- content/renderer/media/canvas_capture_handler.cc
+++ content/renderer/media/canvas_capture_handler.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/media/media_stream_video_track.h"
 #include "content/renderer/media/webrtc_uma_histograms.h"
 #include "content/renderer/render_thread_impl.h"
+#include "media/base/limits.h"
 #include "third_party/WebKit/public/platform/WebMediaStreamSource.h"
 #include "third_party/WebKit/public/platform/WebString.h"
 #include "third_party/libyuv/include/libyuv.h"
@@ -39,11 +40,14 @@ namespace content {
 // This class is single threaded and pinned to main render thread.
 class VideoCapturerSource : public media::VideoCapturerSource {
  public:
-  explicit VideoCapturerSource(base::WeakPtr<CanvasCaptureHandler>
-                                   canvas_handler,
-                               double frame_rate)
-      : frame_rate_(frame_rate),
-        canvas_handler_(canvas_handler) {}
+  VideoCapturerSource(base::WeakPtr<CanvasCaptureHandler> canvas_handler,
+                      double frame_rate)
+      : frame_rate_(static_cast<float>(
+            std::min(static_cast<double>(media::limits::kMaxFramesPerSecond),
+                     frame_rate))),
+        canvas_handler_(canvas_handler) {
+    DCHECK_LE(0, frame_rate_);
+  }
 
  protected:
   void GetCurrentSupportedFormats(
@@ -80,7 +84,7 @@ class VideoCapturerSource : public media::VideoCapturerSource {
   }
 
  private:
-  const double frame_rate_;
+  const float frame_rate_;
   // Bound to Main Render thread.
   base::ThreadChecker main_render_thread_checker_;
   // CanvasCaptureHandler is owned by CanvasDrawListener in blink and might be
