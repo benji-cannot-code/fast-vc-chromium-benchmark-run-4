ddde085b2651b465c3bc15fbc8d597295de60da6
mikecase <mikecase@chromium.org>
2016-12-20 16:53:16 -0800

    Revert of Add option to output json formatted perf results. (patchset #3 id:40001 of https://codereview.chromium.org/2577853002/ )
    Reason for revert:
    Broke resource_size test for Cronet bot
    
    Original issue's description:
    > Add option to output json formatted perf results.
    >
    > JSON formatted perf results are required to determine which
    > pages/stories failed. This will be used by recipes to output
    > the failed pages in the buildbot step_text.
    >
    > Committed: https://crrev.com/36aa507718e67f9e553c3d815386c681f9e0e333
    > Cr-Commit-Position: refs/heads/master@{#439922}
    
    TBR=jbudorick@chromium.org,rnephew@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2596663003
    Cr-Commit-Position: refs/heads/master@{#439958}

--- build/android/pylib/local/device/local_device_perf_test_run.py
+++ build/android/pylib/local/device/local_device_perf_test_run.py
@@ -114,9 +114,7 @@ class TestShard(object):
         exit_code, output = cmd_helper.GetCmdStatusAndOutputWithTimeout(
             cmd, timeout, cwd=cwd, shell=True)
       end_time = time.time()
-      chart_json_output = self._test_instance.ReadChartjsonOutput(
-          self._output_dir)
-      json_output = self._test_instance.ReadJsonOutput(self._output_dir)
+      json_output = self._test_instance.ReadChartjsonOutput(self._output_dir)
       if exit_code == 0:
         result_type = base_test_result.ResultType.PASS
       else:
@@ -125,12 +123,10 @@ class TestShard(object):
       end_time = time.time()
       exit_code = -1
       output = e.output
-      chart_json_output = ''
       json_output = ''
       result_type = base_test_result.ResultType.TIMEOUT
     return self._ProcessTestResult(test, cmd, start_time, end_time, exit_code,
-                                   output, chart_json_output, json_output,
-                                   result_type)
+                                   output, json_output, result_type)
 
   def _CreateCmd(self, test):
     cmd = []
@@ -139,7 +135,6 @@ class TestShard(object):
     cmd.append(self._tests[test]['cmd'])
     if self._output_dir:
       cmd.append('--output-dir=%s' % self._output_dir)
-      cmd.append('--output-format=json')
     return ' '.join(self._ExtendCmd(cmd))
 
   def _ExtendCmd(self, cmd): # pylint: disable=no-self-use
@@ -156,7 +151,7 @@ class TestShard(object):
     raise NotImplementedError
 
   def _ProcessTestResult(self, test, cmd, start_time, end_time, exit_code,
-                         output, chart_json_output, json_output, result_type):
+                         output, json_output, result_type):
     if exit_code is None:
       exit_code = -1
 
@@ -172,8 +167,7 @@ class TestShard(object):
     persisted_result = {
         'name': test,
         'output': [output],
-        'chartjson': chart_json_output,
-        'json': json_output,
+        'chartjson': json_output,
         'archive_bytes': archive_bytes,
         'exit_code': exit_code,
         'actual_exit_code': actual_exit_code,
