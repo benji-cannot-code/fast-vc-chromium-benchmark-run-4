0762f9fc7f5b5b60c5b054fa46f6127a03e99a54
rsorokin <rsorokin@chromium.org>
2016-12-21 10:11:00 -0800

    Add account_type into AccountId
    - Add Active Directory type account
    - Rename gaia_id_ to id_.
    - Add |AcccountType| param to user_manager::known_user::GetAccountId
    - Add |AdFromUserEmailObjGuid| |AdFromUserEmail| functions to create Active Directory account ids.
    
    This CL a preparation for crbug.com/668130
    Should be fixed after migration (see crbug.com/668130)
    
    BUG=656992
    TEST=none
    
    Review-Url: https://codereview.chromium.org/2529103002
    Cr-Commit-Position: refs/heads/master@{#440152}

--- chrome/browser/chromeos/login/easy_unlock/bootstrap_manager.cc
+++ chrome/browser/chromeos/login/easy_unlock/bootstrap_manager.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
 #include "components/prefs/scoped_user_pref_update.h"
+#include "components/signin/core/account_id/account_id.h"
 #include "components/user_manager/known_user.h"
 
 namespace chromeos {
@@ -67,7 +68,8 @@ void BootstrapManager::RemoveAllPendingBootstrap() {
     if (users->GetString(i, &current_user_email)) {
       delegate_->RemovePendingBootstrapUser(
           user_manager::known_user::GetAccountId(current_user_email,
-                                                 std::string() /* gaia_id */));
+                                                 std::string() /* id */,
+                                                 AccountType::UNKNOWN));
     }
   }
 
