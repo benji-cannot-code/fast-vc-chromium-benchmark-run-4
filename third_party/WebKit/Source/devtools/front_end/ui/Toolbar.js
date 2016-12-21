2e752b810767fdbe7a101ebf9497f43671ebef32
pfeldman <pfeldman@chromium.org>
2016-12-20 19:44:42 -0800

    DevTools: make toolbar enable/disable respect individual button states.
    Review-Url: https://codereview.chromium.org/2593883002
    Cr-Commit-Position: refs/heads/master@{#440002}

--- third_party/WebKit/Source/devtools/front_end/ui/Toolbar.js
+++ third_party/WebKit/Source/devtools/front_end/ui/Toolbar.js
@@ -44,6 +44,7 @@ UI.Toolbar = class {
     this.element = parentElement ? parentElement.createChild('div') : createElement('div');
     this.element.className = className;
     this.element.classList.add('toolbar');
+    this._enabled = true;
     this._shadowRoot = UI.createShadowRootWithCoreStyles(this.element, 'ui/toolbar.css');
     this._contentElement = this._shadowRoot.createChild('div', 'toolbar-shadow');
     this._insertionPoint = this._contentElement.createChild('content');
@@ -220,8 +221,9 @@ UI.Toolbar = class {
    * @param {boolean} enabled
    */
   setEnabled(enabled) {
+    this._enabled = enabled;
     for (var item of this._items)
-      item.setEnabled(enabled);
+      item._applyEnabledState();
   }
 
   /**
@@ -230,6 +232,8 @@ UI.Toolbar = class {
   appendToolbarItem(item) {
     this._items.push(item);
     item._toolbar = this;
+    if (!this._enabled)
+      item._applyEnabledState();
     if (this._reverse)
       this._contentElement.insertBefore(item.element, this._insertionPoint.nextSibling);
     else
@@ -400,7 +404,7 @@ UI.ToolbarItem = class extends Common.Object {
   }
 
   _applyEnabledState() {
-    this.element.disabled = !this._enabled;
+    this.element.disabled = !this._enabled || (this._toolbar && !this._toolbar._enabled);
   }
 
   /**
@@ -825,7 +829,7 @@ UI.ToolbarComboBox = class extends UI.ToolbarItem {
    * @override
    */
   _applyEnabledState() {
-    this._selectElement.disabled = !this._enabled;
+    this._selectElement.disabled = !this._enabled || (this._toolbar && !this._toolbar._enabled);
   }
 
   /**
