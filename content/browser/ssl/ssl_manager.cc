ab52890a6746f0204b38d6b00a2618035bdcf337
ananta <ananta@chromium.org>
2016-12-22 09:11:55 -0800

    Ensure that duplicate navigations are tagged as reload in the browser. This includes the cases where we are navigating to a URL which was committed previously or to a URL for which we haven't received a commit.
    scottmg's patch for reference is here https://codereview.chromium.org/2381503003. This was reverted due to failures on Android.
    
    The failure was seen in cases like this. Navigation to url1->url2->url1.
    If the first navigation is not committed yet, then we would treat the last
    navigation to url1 as a reload. This causes inbox on Android to hang.
    
    The proposed fix is to use the last pending navigation if it exists or
    the last committed navigation to compare whether the current navigation is
    a reload. This fixes the issues with Android
    
    Changes in this patch are as below:
    1. Add members in the NavigationControllerImpl class to hold the last
       pending navigation entry, its index, and the last transient index.
       The last transient index is used to avoid treating navigations as
       reloads if we have an interstitial being displayed.
    
    2. Add a flag in the NavigationEntryImpl class to indicate whether there
       was a SSL error in the navigation. This was added to fix browser test
       failures. We avoid treating navigations as reloads when the last
        pending navigation has a SSL error.
    
    3. We compare the url of the last pending or committed entry with the
       current navigation entry to see if it is a reload. There are a number
       of cases to consider here. Please look at the code.
    
    4. Added a unittest NavigationControllerTest.MultipleNavigationsAndReload
       which tests various combinations of pending and committed navigations
       to ensure that the navigation is tagged correctly as reload or not.
    
    The non plznavigate case does not have the bug when the EnsureSamePageNavigationUpdatesFrameNavigationEntry test because Blink tells us the correct navigation history, i.e. whether a navigation is a
    reload, etc.
    
    R=nasko@chromium.org,clamy@chromium.org
    BUG=630103, 475027, 536102, 664319, 676235
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2580753002
    Cr-Commit-Position: refs/heads/master@{#440443}

--- content/browser/ssl/ssl_manager.cc
+++ content/browser/ssl/ssl_manager.cc
@@ -106,9 +106,11 @@ void HandleSSLErrorOnUI(
     return;
   }
 
-  SSLManager* manager =
-      static_cast<NavigationControllerImpl*>(&web_contents->GetController())
-          ->ssl_manager();
+  NavigationControllerImpl* controller =
+      static_cast<NavigationControllerImpl*>(&web_contents->GetController());
+  controller->SetPendingNavigationSSLError(true);
+
+  SSLManager* manager = controller->ssl_manager();
   manager->OnCertError(std::move(handler));
 }
 
