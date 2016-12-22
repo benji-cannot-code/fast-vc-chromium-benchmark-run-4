d63ff2bf879b53ada127d6e8dd864e63249e1a86
xdai <xdai@chromium.org>
2016-12-21 21:38:39 -0800

    [Chrome OS] Replace the Chrome OS Wallpaper App with Android Wallpapers App if Arc is enabled.
    Currently this behavior is behind a Finch experiment (experiment name:
    "AndroidWallpapersAppTrial") or a Chrome flag (flag name:
    --enable-android-wallpapers-app). The finch experiment and the chrome flag
    were added in seperated CLs.
    
    After this change, right clicking on desktop and clicking "Set wallpaper"
    will launch the Android Wallpapers App instead of the old Chrome OS Wallpaper
    Picker App if 1) the user is the primary user 2) the user has opted in to ARC++
    3) Android Wallpapers App has been installed 4) the finch experiment or the
    Chrome flag for the Anroid Wallpapers App is enabled for this device.
    
    BUG=670328
    
    Review-Url: https://codereview.chromium.org/2589773003
    Cr-Commit-Position: refs/heads/master@{#440346}

--- chrome/browser/chromeos/extensions/wallpaper_manager_util.h
+++ chrome/browser/chromeos/extensions/wallpaper_manager_util.h
@@ -6,7 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_EXTENSIONS_WALLPAPER_MANAGER_UTIL_H_
 #define CHROME_BROWSER_CHROMEOS_EXTENSIONS_WALLPAPER_MANAGER_UTIL_H_
 
+class Profile;
+
 namespace wallpaper_manager_util {
+
+bool ShouldUseAndroidWallpapersApp(Profile* profile);
 
 // Opens wallpaper manager application.
 void OpenWallpaperManager();
