2f59f60888e706da8ba3cd2835b3929600f70498
dcastagna <dcastagna@chromium.org>
2016-12-22 11:57:53 -0800

    ozone: Always compile drm atomic support, add 'enable-drm-atomic' switch.
    With the latest changes landed to support AFBC, we can now use drm atomic
    on specific boards.
    
    This change makes it so that support for drm atomic is always compiled
    when ozone is built with gbm platform.
    It also puts drm atomic behind 'enable-drm-atomic' command line
    switch so that we can enable it only on boards where it works.
    
    BUG=442575
    TEST=Restarted ui with and without enable-drm-atomic, checked that drmModeAtomicCommit or drmModePageFlip is used.
    
    Review-Url: https://codereview.chromium.org/2584303002
    Cr-Commit-Position: refs/heads/master@{#440480}

--- content/browser/gpu/gpu_data_manager_impl_private.cc
+++ content/browser/gpu/gpu_data_manager_impl_private.cc
@@ -48,6 +48,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_switches.h"
 #include "ui/gl/gpu_switching_manager.h"
 
+#if defined(USE_OZONE)
+#include "ui/ozone/public/ozone_switches.h"
+#endif
 #if defined(OS_MACOSX)
 #include <ApplicationServices/ApplicationServices.h>
 #endif  // OS_MACOSX
@@ -769,6 +772,12 @@ void GpuDataManagerImplPrivate::AppendGpuCommandLine(
     command_line->AppendSwitch(switches::kCreateDefaultGLContext);
   }
 
+#if defined(USE_OZONE)
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kEnableDrmAtomic)) {
+    command_line->AppendSwitch(switches::kEnableDrmAtomic);
+  }
+#endif
 #if defined(OS_WIN)
   if (IsFeatureBlacklisted(gpu::GPU_FEATURE_TYPE_ACCELERATED_VPX_DECODE) &&
       gpu_preferences) {
