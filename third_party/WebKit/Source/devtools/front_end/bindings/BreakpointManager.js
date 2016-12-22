e559948574a64926b30c9511768025583a1126af
pfeldman <pfeldman@chromium.org>
2016-12-21 18:42:06 -0800

    DevTools: survive debugging theverge that generates hundreds of scripts per second.
    Committed: https://crrev.com/9d1411bdc9acf5b9728bcdc4b658e69acdbb82fa
    Review-Url: https://codereview.chromium.org/2595803002
    Cr-Original-Commit-Position: refs/heads/master@{#440021}
    Cr-Commit-Position: refs/heads/master@{#440318}

--- third_party/WebKit/Source/devtools/front_end/bindings/BreakpointManager.js
+++ third_party/WebKit/Source/devtools/front_end/bindings/BreakpointManager.js
@@ -57,6 +57,7 @@ Bindings.BreakpointManager = class extends Common.Object {
     this._workspace.addEventListener(Workspace.Workspace.Events.ProjectRemoved, this._projectRemoved, this);
     this._workspace.addEventListener(Workspace.Workspace.Events.UISourceCodeAdded, this._uiSourceCodeAdded, this);
     this._workspace.addEventListener(Workspace.Workspace.Events.UISourceCodeRemoved, this._uiSourceCodeRemoved, this);
+    this._debuggerWorkspaceBinding.addEventListener(Bindings.DebuggerWorkspaceBinding.Events.SourceMappingChanged, this._uiSourceCodeMappingChanged, this);
 
     targetManager.observeTargets(this, SDK.Target.Capability.JS);
   }
@@ -156,10 +157,6 @@ Bindings.BreakpointManager = class extends Common.Object {
   _uiSourceCodeAdded(event) {
     var uiSourceCode = /** @type {!Workspace.UISourceCode} */ (event.data);
     this._restoreBreakpoints(uiSourceCode);
-    if (uiSourceCode.contentType().hasScripts()) {
-      uiSourceCode.addEventListener(
-          Workspace.UISourceCode.Events.SourceMappingChanged, this._uiSourceCodeMappingChanged, this);
-    }
   }
 
   /**
@@ -194,8 +191,6 @@ Bindings.BreakpointManager = class extends Common.Object {
       if (breakpoints[i].enabled())
         this._provisionalBreakpoints.set(uiSourceCode.url(), breakpoints[i]);
     }
-    uiSourceCode.removeEventListener(
-        Workspace.UISourceCode.Events.SourceMappingChanged, this._uiSourceCodeMappingChanged, this);
     this._breakpointsForPrimaryUISourceCode.remove(uiSourceCode);
   }
 
--- third_party/WebKit/Source/devtools/front_end/sdk/DebuggerModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/DebuggerModel.js
@@ -60,6 +60,9 @@ SDK.DebuggerModel = class extends SDK.SDKModel {
     /** @type {!Map<string, string>} */
     this._fileURLToNodeJSPath = new Map();
     this.enableDebugger();
+
+    /** @type {!Map<string, string>} */
+    this._stringMap = new Map();
   }
 
   /**
@@ -320,6 +323,7 @@ SDK.DebuggerModel = class extends SDK.SDKModel {
   _reset() {
     this._scripts = {};
     this._scriptsBySourceURL.clear();
+    this._stringMap.clear();
   }
 
   /**
@@ -485,10 +489,13 @@ SDK.DebuggerModel = class extends SDK.SDKModel {
     if (this.target().isNodeJS() && sourceURL && sourceURL.startsWith('/')) {
       var nodeJSPath = sourceURL;
       sourceURL = Common.ParsedURL.platformPathToURL(nodeJSPath);
+      sourceURL = this._internString(sourceURL);
       this._fileURLToNodeJSPath.set(sourceURL, nodeJSPath);
+    } else {
+      sourceURL = this._internString(sourceURL);
     }
     var script = new SDK.Script(
-        this, scriptId, sourceURL, startLine, startColumn, endLine, endColumn, executionContextId, hash,
+        this, scriptId, sourceURL, startLine, startColumn, endLine, endColumn, executionContextId, this._internString(hash),
         isContentScript, isLiveEdit, sourceMapURL, hasSourceURL);
     this._registerScript(script);
     if (!hasSyntaxError)
@@ -792,6 +799,16 @@ SDK.DebuggerModel = class extends SDK.SDKModel {
       this.enableDebugger(fulfill);
     }
   }
+
+  /**
+   * @param {string} string
+   * @return {string} string
+   */
+  _internString(string) {
+     if (!this._stringMap.has(string))
+       this._stringMap.set(string, string);
+     return this._stringMap.get(string);
+  }
 };
 
 /** @typedef {{location: ?SDK.DebuggerModel.Location, functionName: string}} */
@@ -818,7 +835,8 @@ SDK.DebuggerModel.Events = {
   FailedToParseScriptSource: Symbol('FailedToParseScriptSource'),
   GlobalObjectCleared: Symbol('GlobalObjectCleared'),
   CallFrameSelected: Symbol('CallFrameSelected'),
-  ConsoleCommandEvaluatedInSelectedCallFrame: Symbol('ConsoleCommandEvaluatedInSelectedCallFrame')
+  ConsoleCommandEvaluatedInSelectedCallFrame: Symbol('ConsoleCommandEvaluatedInSelectedCallFrame'),
+  SourceMapURLAdded: Symbol('SourceMapURLAdded')
 };
 
 /** @enum {string} */
