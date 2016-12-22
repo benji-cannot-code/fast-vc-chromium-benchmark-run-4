03ec5cce5b116795f135675c3ae682e9beeff8f0
dschuyler <dschuyler@chromium.org>
2016-12-21 16:12:41 -0800

    [MD settings] rename handlers with Handle prefix
    This CL changes the names of protocol handler functions that handle JS
    calls. The Handle prefix is used in other handlers. This CL updates this
    code to follow that style.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2593233002
    Cr-Commit-Position: refs/heads/master@{#440274}

--- chrome/browser/ui/webui/settings/protocol_handlers_handler.cc
+++ chrome/browser/ui/webui/settings/protocol_handlers_handler.cc
@@ -42,22 +42,22 @@ void ProtocolHandlersHandler::OnJavascriptDisallowed() {
 
 void ProtocolHandlersHandler::RegisterMessages() {
   web_ui()->RegisterMessageCallback("initializeProtocolHandlerList",
-      base::Bind(&ProtocolHandlersHandler::InitializeProtocolHandlerList,
+      base::Bind(&ProtocolHandlersHandler::HandleInitializeList,
                  base::Unretained(this)));
   web_ui()->RegisterMessageCallback("clearDefault",
-      base::Bind(&ProtocolHandlersHandler::ClearDefault,
+      base::Bind(&ProtocolHandlersHandler::HandleClearDefault,
                  base::Unretained(this)));
   web_ui()->RegisterMessageCallback("removeHandler",
-      base::Bind(&ProtocolHandlersHandler::RemoveHandler,
+      base::Bind(&ProtocolHandlersHandler::HandleRemoveHandler,
                  base::Unretained(this)));
   web_ui()->RegisterMessageCallback("setHandlersEnabled",
-      base::Bind(&ProtocolHandlersHandler::SetHandlersEnabled,
+      base::Bind(&ProtocolHandlersHandler::HandleSetHandlersEnabled,
                  base::Unretained(this)));
   web_ui()->RegisterMessageCallback("setDefault",
-      base::Bind(&ProtocolHandlersHandler::SetDefault,
+      base::Bind(&ProtocolHandlersHandler::HandleSetDefault,
                  base::Unretained(this)));
   web_ui()->RegisterMessageCallback("removeIgnoredHandler",
-      base::Bind(&ProtocolHandlersHandler::RemoveIgnoredHandler,
+      base::Bind(&ProtocolHandlersHandler::HandleRemoveIgnoredHandler,
                  base::Unretained(this)));
 }
 
@@ -132,7 +132,7 @@ void ProtocolHandlersHandler::UpdateHandlerList() {
                          *ignored_handlers);
 }
 
-void ProtocolHandlersHandler::InitializeProtocolHandlerList(
+void ProtocolHandlersHandler::HandleInitializeList(
     const base::ListValue* args) {
   AllowJavascript();
   SendHandlersEnabledValue();
@@ -146,7 +146,7 @@ void ProtocolHandlersHandler::SendHandlersEnabledValue() {
                              GetProtocolHandlerRegistry()->enabled()));
 }
 
-void ProtocolHandlersHandler::RemoveHandler(const base::ListValue* args) {
+void ProtocolHandlersHandler::HandleRemoveHandler(const base::ListValue* args) {
   const base::ListValue* list;
   if (!args->GetList(0, &list)) {
     NOTREACHED();
@@ -161,7 +161,7 @@ void ProtocolHandlersHandler::RemoveHandler(const base::ListValue* args) {
   // then.
 }
 
-void ProtocolHandlersHandler::RemoveIgnoredHandler(
+void ProtocolHandlersHandler::HandleRemoveIgnoredHandler(
     const base::ListValue* args) {
   const base::ListValue* list;
   if (!args->GetList(0, &list)) {
@@ -173,7 +173,8 @@ void ProtocolHandlersHandler::RemoveIgnoredHandler(
   GetProtocolHandlerRegistry()->RemoveIgnoredHandler(handler);
 }
 
-void ProtocolHandlersHandler::SetHandlersEnabled(const base::ListValue* args) {
+void ProtocolHandlersHandler::HandleSetHandlersEnabled(
+    const base::ListValue* args) {
   bool enabled = true;
   CHECK(args->GetBoolean(0, &enabled));
   if (enabled)
@@ -182,7 +183,7 @@ void ProtocolHandlersHandler::SetHandlersEnabled(const base::ListValue* args) {
     GetProtocolHandlerRegistry()->Disable();
 }
 
-void ProtocolHandlersHandler::ClearDefault(const base::ListValue* args) {
+void ProtocolHandlersHandler::HandleClearDefault(const base::ListValue* args) {
   const base::Value* value;
   CHECK(args->Get(0, &value));
   std::string protocol_to_clear;
@@ -190,7 +191,7 @@ void ProtocolHandlersHandler::ClearDefault(const base::ListValue* args) {
   GetProtocolHandlerRegistry()->ClearDefault(protocol_to_clear);
 }
 
-void ProtocolHandlersHandler::SetDefault(const base::ListValue* args) {
+void ProtocolHandlersHandler::HandleSetDefault(const base::ListValue* args) {
   const base::ListValue* list;
   CHECK(args->GetList(0, &list));
   const ProtocolHandler& handler(ParseHandlerFromArgs(list));
