5969b1392d2dadcc08718a8e8d7248b476d2e52b
yabinh <yabinh@chromium.org>
2016-12-22 03:22:03 -0800

    Fix some flaky IME tests caused by incorrect selection update
    Some IME tests are flaky because of wrong selection update. We suspect
    it's because sometimes when we focus on input_text, the selection update
    comes after reseting the state. We should wait to ensure the correct order.
    
    BUG=628964
    
    Review-Url: https://codereview.chromium.org/2600453002
    Cr-Commit-Position: refs/heads/master@{#440381}

--- content/public/android/javatests/src/org/chromium/content/browser/input/ImeTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/input/ImeTest.java
@@ -95,6 +95,7 @@ public class ImeTest extends ContentShellTestBase {
         waitForEventLogs("selectionchange");
         clearEventLogs();
 
+        waitAndVerifyUpdateSelection(0, 0, 0, -1, -1);
         resetAllStates();
     }
 
