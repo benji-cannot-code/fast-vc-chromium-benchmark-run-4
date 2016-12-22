dd65681720421199b6f5532d72a0eba4e9034ed1
sadrul <sadrul@chromium.org>
2016-12-21 16:00:44 -0800

    discardable shared memory: Allow resetting the allocator instance.
    Make sure the allocator can be unset when it is destroyed. This is
    necessary when running tests, when each test recreates the allocator
    instance. In case of views_aura_mus_unittests, this causes a number of
    crashing failures, causing a lot of tests to be re-run separately.
    Without this fix, the test step takes ~40 seconds to complete. With this
    fix, it takes ~7 seconds.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2596903002
    Cr-Commit-Position: refs/heads/master@{#440270}

--- ui/views/mus/mus_client.cc
+++ ui/views/mus/mus_client.cc
@@ -129,6 +129,7 @@ MusClient::~MusClient() {
         ViewsDelegate::NativeWidgetFactory());
   }
 
+  base::DiscardableMemoryAllocator::SetInstance(nullptr);
   DCHECK_EQ(instance_, this);
   instance_ = nullptr;
 }
