5ad8c24343110d72b18aee0546e9aea1e1cc261a
qiankun.miao <qiankun.miao@intel.com>
2016-12-22 07:58:30 -0800

    Remove unused variable
    BUG=NONE
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2600463002
    Cr-Commit-Position: refs/heads/master@{#440431}

--- gpu/config/gpu_control_list_jsons.h
+++ gpu/config/gpu_control_list_jsons.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gpu {
 
 GPU_EXPORT extern const char kGpuDriverBugListJson[];
-GPU_EXPORT extern const char kGpuSwitchingListJson[];
 GPU_EXPORT extern const char kSoftwareRenderingListJson[];
 
 }  // namespace gpu
