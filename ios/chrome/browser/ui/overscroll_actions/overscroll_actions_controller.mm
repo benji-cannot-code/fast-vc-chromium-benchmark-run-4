abfc0eb8cdcc3b085ede8c985d55d89e9ef21912
eugenebut <eugenebut@chromium.org>
2016-12-21 16:07:06 -0800

    [ios] Fixed web-compat bug related to resetting top content inset.
    OverscrollActionsController resets top content inset every time when
    scolling is finished. Before this change it assumed that bottom inset
    should be always reset to 0. This is not correct for cases when soft
    keyboard is displayed and bottom offset is non-zero to accomodate the
    keyboard.
    
    This CL adds resetScrollViewTopContentInset method and calls it when
    scrolling is finished. Remaining fixed will be landed in separate CLs
    to make changes less risky.
    
    BUG=669908,675763
    
    Review-Url: https://codereview.chromium.org/2589973004
    Cr-Commit-Position: refs/heads/master@{#440273}

--- ios/chrome/browser/ui/overscroll_actions/overscroll_actions_controller.mm
+++ ios/chrome/browser/ui/overscroll_actions/overscroll_actions_controller.mm
@@ -197,6 +197,8 @@ NSString* const kOverscollActionsDidEnd = @"OverscollActionsDidStop";
 // Setup/tearDown methods are used to register values when the delegate is set.
 - (void)tearDown;
 - (void)setup;
+// Resets scroll view's top content inset to |self.initialContentInset|.
+- (void)resetScrollViewTopContentInset;
 // Access the headerView from the delegate.
 - (UIView<RelaxedBoundsConstraintsHitTestSupport>*)headerView;
 // Locking/unlocking methods used to disable/enable the overscroll actions
@@ -382,8 +384,7 @@ NSString* const kOverscollActionsDidEnd = @"OverscollActionsDidStop";
   // restored to initial value.
   if (contentOffset.y >= 0 ||
       self.overscrollState == ios_internal::OverscrollState::NO_PULL_STARTED) {
-    [self setScrollViewContentInset:UIEdgeInsetsMake(self.initialContentInset,
-                                                     0, 0, 0)];
+    [self resetScrollViewTopContentInset];
   }
 
   [self triggerActionIfNeeded];
@@ -589,6 +590,12 @@ NSString* const kOverscollActionsDidEnd = @"OverscollActionsDidStop";
     [_scrollview setContentInset:contentInset];
   else
     [_webViewScrollViewProxy setContentInset:contentInset];
+}
+
+- (void)resetScrollViewTopContentInset {
+  UIEdgeInsets contentInset = self.scrollView.contentInset;
+  contentInset.top = self.initialContentInset;
+  [self setScrollViewContentInset:contentInset];
 }
 
 - (UIView<RelaxedBoundsConstraintsHitTestSupport>*)headerView {
