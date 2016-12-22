4adf5fce9e2e08d33efff2522285bfad5da5565e
fs <fs@opera.com>
2016-12-22 02:10:50 -0800

    Clamp rgba(...) alpha value in the CSS fast-path parser
    The general CSS parser will clamp the alpha value to the [0, 1] range,
    while the fast-path parser does not. This means that large alpha values
    will result in overflow in the cast to int, yielding the wrong color
    value.
    Add a clamp to the [0, 1] range before converting to the [0, 256) range
    and converting to int. Because negative values are handled separately,
    we only need to apply the clamp for the range above zero.
    
    BUG=675158
    
    Review-Url: https://codereview.chromium.org/2588293004
    Cr-Commit-Position: refs/heads/master@{#440374}

--- third_party/WebKit/Source/core/css/parser/CSSParserFastPathsTest.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSParserFastPathsTest.cpp
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/css/parser/CSSParserFastPaths.h"
 
+#include "core/css/CSSColorValue.h"
 #include "core/css/CSSIdentifierValue.h"
 #include "core/css/CSSValueList.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -97,6 +98,14 @@ TEST(CSSParserFastPathsTest, ParseInvalidTransform) {
       CSSPropertyTransform, "translateZ(1px) (1px, 1px) rotateX(1deg",
       HTMLStandardMode);
   ASSERT_EQ(nullptr, value);
+}
+
+TEST(CSSParserFastPathsTest, ParseColorWithLargeAlpha) {
+  CSSValue* value = CSSParserFastPaths::parseColor("rgba(0,0,0,1893205797.13)",
+                                                   HTMLStandardMode);
+  EXPECT_NE(nullptr, value);
+  EXPECT_TRUE(value->isColorValue());
+  EXPECT_EQ(Color::black, toCSSColorValue(*value).value());
 }
 
 }  // namespace blink
