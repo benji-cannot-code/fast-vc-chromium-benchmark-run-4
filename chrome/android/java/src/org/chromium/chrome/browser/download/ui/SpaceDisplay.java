e29810f1c305b5e3aa47a0b2c4af41e451a4e842
dfalcantara <dfalcantara@chromium.org>
2016-12-21 17:03:58 -0800

    [Downloads] Add a horizontal material-styled ProgressBar
    The support library's ProgressBar doesn't support JB or KK,
    and the ToolbarProgressBar doesn't support JB.  In the
    tradition of re-inventing the wheel, introduce a ProgressBar
    that is styled according to Material Design that works on all
    versions of Android that Clank supports.
    
    * Introduce the MaterialProgressBar, which mimics the Material
      Design spec on how horizontal progress bars are supposed to
      look for both determinate and indeterminate modes.
    
    * Switch DownloadItemView and SpaceDisplay to use the newfangled
      widget, then nuke unnecessary styling.
    
    * Delete old, unused, experimental ProgressBars.
    
    Video:
    https://drive.google.com/a/chromium.org/file/d/0B8k2SmEN8xS3ZGtzU3FxMGFMWlU/view?usp=sharing
    
    BUG=658246
    
    Review-Url: https://codereview.chromium.org/2586643002
    Cr-Commit-Position: refs/heads/master@{#440287}

--- chrome/android/java/src/org/chromium/chrome/browser/download/ui/SpaceDisplay.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/ui/SpaceDisplay.java
@@ -11,7 +11,6 @@ import android.os.Environment;
 import android.os.StatFs;
 import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import org.chromium.base.ApiCompatibilityUtils;
@@ -20,6 +19,7 @@ import org.chromium.base.ObserverList;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.widget.MaterialProgressBar;
 
 import java.io.File;
 import java.util.concurrent.ExecutionException;
@@ -109,7 +109,7 @@ public class SpaceDisplay extends RecyclerView.AdapterDataObserver {
     private TextView mSpaceUsedByDownloadsTextView;
     private TextView mSpaceUsedByOtherAppsTextView;
     private TextView mSpaceFreeTextView;
-    private ProgressBar mSpaceBar;
+    private MaterialProgressBar mSpaceBar;
     private long mFreeBytes;
 
     SpaceDisplay(final ViewGroup parent, DownloadHistoryAdapter historyAdapter) {
@@ -117,7 +117,7 @@ public class SpaceDisplay extends RecyclerView.AdapterDataObserver {
         mSpaceUsedByDownloadsTextView = (TextView) parent.findViewById(R.id.size_downloaded);
         mSpaceUsedByOtherAppsTextView = (TextView) parent.findViewById(R.id.size_other_apps);
         mSpaceFreeTextView = (TextView) parent.findViewById(R.id.size_free);
-        mSpaceBar = (ProgressBar) parent.findViewById(R.id.space_bar);
+        mSpaceBar = (MaterialProgressBar) parent.findViewById(R.id.space_bar);
         mFileSystemBytesTask =
                 new StorageSizeTask(true).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
