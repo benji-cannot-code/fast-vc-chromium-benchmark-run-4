ba8e88c537a4d309016c7698d3a4000afca1e0fa
sigbjornf <sigbjornf@opera.com>
2016-12-23 03:43:18 -0800

    Remove unwanted declaration of storage for NeedsAdjustAndMark<>.
    This rolls back the change made in
    
     https://codereview.chromium.org/429793003/
    
    We shouldn't have to do this if the class type has been fully
    instantiated and "used". Traits used to direct compilation won't run
    into this (as the expression will clearly have to be evaluated at
    compile-time), but gtest usage of traits may end up evaluating their
    predicate expressions in compiled code, thus requiring that the trait
    has been instantiated in the compilation unit already. Declaring one
    for all uses of NeedsAdjustAndMark<> is too strong, and isn't needed
    really (now) -- retire, but avoid running into the issue for the
    corresponding unit test.
    
    R=
    BUG=
    
    Review-Url: https://codereview.chromium.org/2599643004
    Cr-Commit-Position: refs/heads/master@{#440618}

--- third_party/WebKit/Source/platform/heap/GarbageCollected.h
+++ third_party/WebKit/Source/platform/heap/GarbageCollected.h
@@ -243,8 +243,6 @@ class NeedsAdjustAndMark<T, true> {
  public:
   static const bool value = false;
 };
-template <typename T>
-const bool NeedsAdjustAndMark<T, true>::value;
 
 template <typename T>
 class NeedsAdjustAndMark<T, false> {
@@ -254,8 +252,6 @@ class NeedsAdjustAndMark<T, false> {
   static const bool value =
       IsGarbageCollectedMixin<typename std::remove_const<T>::type>::value;
 };
-template <typename T>
-const bool NeedsAdjustAndMark<T, false>::value;
 
 template <typename T,
           bool = std::is_base_of<TraceWrapperBase,
@@ -269,8 +265,6 @@ class CanTraceWrappers<T, true> {
  public:
   static const bool value = true;
 };
-template <typename T>
-const bool CanTraceWrappers<T, true>::value;
 
 template <typename T>
 class CanTraceWrappers<T, false> {
@@ -279,8 +273,6 @@ class CanTraceWrappers<T, false> {
  public:
   static const bool value = false;
 };
-template <typename T>
-const bool CanTraceWrappers<T, false>::value;
 
 // TODO(sof): migrate to wtf/TypeTraits.h
 template <typename T>
