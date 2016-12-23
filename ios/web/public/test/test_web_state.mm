a5e33ceddb87da9061bcf0129a168c2184885829
olivierrobin <olivierrobin@chromium.org>
2016-12-23 00:46:38 -0800

    Add unittests for ReadingListWebStateObserver.
    This follows cl/2578973002.
    
    BUG=671160
    
    Review-Url: https://codereview.chromium.org/2599023002
    Cr-Commit-Position: refs/heads/master@{#440603}

--- ios/web/public/test/test_web_state.mm
+++ ios/web/public/test/test_web_state.mm
@@ -62,11 +62,16 @@ UIView* TestWebState::GetView() {
 }
 
 const NavigationManager* TestWebState::GetNavigationManager() const {
-  return nullptr;
+  return navigation_manager_.get();
 }
 
 NavigationManager* TestWebState::GetNavigationManager() {
-  return nullptr;
+  return navigation_manager_.get();
+}
+
+void TestWebState::SetNavigationManager(
+    std::unique_ptr<NavigationManager> navigation_manager) {
+  navigation_manager_ = std::move(navigation_manager);
 }
 
 CRWJSInjectionReceiver* TestWebState::GetJSInjectionReceiver() const {
@@ -146,6 +151,12 @@ void TestWebState::SetLoading(bool is_loading) {
     for (auto& observer : observers_)
       observer.DidStopLoading();
   }
+}
+
+void TestWebState::OnPageLoaded(
+    PageLoadCompletionStatus load_completion_status) {
+  for (auto& observer : observers_)
+    observer.PageLoaded(load_completion_status);
 }
 
 void TestWebState::SetCurrentURL(const GURL& url) {
