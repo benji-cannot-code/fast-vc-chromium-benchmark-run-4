d679b38f70886e8c4b9eb4c55b90b1d1eed01acd
xunjieli <xunjieli@chromium.org>
2016-12-22 17:39:34 -0800

    Fix StreamFailBeforeReadIsExecutedOnNetworkThread flake
    R=mef@chromium.org
    BUG=676323
    
    Review-Url: https://codereview.chromium.org/2601523002
    Cr-Commit-Position: refs/heads/master@{#440577}

--- components/grpc_support/bidirectional_stream_unittest.cc
+++ components/grpc_support/bidirectional_stream_unittest.cc
@@ -556,7 +556,8 @@ TEST_P(BidirectionalStreamTest, StreamFailBeforeReadIsExecutedOnNetworkThread) {
                              "POST", &kTestHeadersArray, false);
   test.BlockForDone();
   ASSERT_EQ(TestBidirectionalStreamCallback::ON_FAILED, test.response_step);
-  ASSERT_EQ(net::ERR_QUIC_PROTOCOL_ERROR, test.net_error);
+  ASSERT_TRUE(test.net_error == net::ERR_QUIC_PROTOCOL_ERROR ||
+              test.net_error == net::ERR_CONNECTION_REFUSED);
   bidirectional_stream_destroy(test.stream);
 }
 
