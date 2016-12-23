f9f7962c30f5d624d4ffbc158a5bcee3db9e6131
alph <alph@chromium.org>
2016-12-22 21:35:08 -0800

    DevTools: Move static properties out of FlameChart data provider.
    Review-Url: https://codereview.chromium.org/2599223002
    Cr-Commit-Position: refs/heads/master@{#440593}

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineFlameChartView.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineFlameChartView.js
@@ -3,11 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-Timeline.FlameChartStyle = {};
-Timeline.FlameChartStyle.barHeight = 17;
-Timeline.FlameChartStyle.textBaseline = 5;
-Timeline.FlameChartStyle.textPadding = 5;
-Timeline.FlameChartStyle.textColor = '#333';
+Timeline.FlameChartStyle = {
+  textColor: '#333'
+};
+
 /**
  * @enum {symbol}
  */
