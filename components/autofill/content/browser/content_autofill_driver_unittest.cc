eb2700fb0d14cd1140c66ea5cd2520e1947cee26
vabr <vabr@chromium.org>
2016-12-23 05:37:52 -0800

    Add user-gesture-related tests for ContentAutofillDriver
    The methods ContentAutofillDriver::FirstUserGestureObserved and
    ContentAutofillDriver::NotifyFirstUserGestureObservedInTab do not seem to be
    tested yet. This CL adds tests for that.
    
    This is in preparation to fixing https://crbug.com/669045, to avoid inflating
    the CL with the fix by adding tests for existing code.
    
    BUG=669045
    
    Review-Url: https://codereview.chromium.org/2603583002
    Cr-Commit-Position: refs/heads/master@{#440624}

--- components/autofill/content/browser/content_autofill_driver_unittest.cc
+++ components/autofill/content/browser/content_autofill_driver_unittest.cc
@@ -138,6 +138,9 @@ class FakeAutofillAgent : public mojom::AutofillAgent {
     return true;
   }
 
+  // Mocked mojom::AutofillAgent methods:
+  MOCK_METHOD0(FirstUserGestureObservedInTab, void());
+
  private:
   void CallDone() {
     if (!quit_closure_.is_null()) {
@@ -146,9 +149,7 @@ class FakeAutofillAgent : public mojom::AutofillAgent {
     }
   }
 
-  // mojom::AutofillAgent methods:
-  void FirstUserGestureObservedInTab() override {}
-
+  // mojom::AutofillAgent:
   void FillForm(int32_t id, const FormData& form) override {
     fill_form_id_ = id;
     fill_form_form_ = form;
@@ -237,6 +238,11 @@ class MockAutofillManager : public AutofillManager {
   MOCK_METHOD0(Reset, void());
 };
 
+class MockAutofillClient : public TestAutofillClient {
+ public:
+  MOCK_METHOD0(OnFirstUserGestureObserved, void());
+};
+
 class TestContentAutofillDriver : public ContentAutofillDriver {
  public:
   TestContentAutofillDriver(content::RenderFrameHost* rfh,
@@ -260,7 +266,7 @@ class ContentAutofillDriverTest : public content::RenderViewHostTestHarness {
   void SetUp() override {
     content::RenderViewHostTestHarness::SetUp();
 
-    test_autofill_client_.reset(new TestAutofillClient());
+    test_autofill_client_.reset(new MockAutofillClient());
     driver_.reset(new TestContentAutofillDriver(web_contents()->GetMainFrame(),
                                                 test_autofill_client_.get()));
 
@@ -280,7 +286,7 @@ class ContentAutofillDriverTest : public content::RenderViewHostTestHarness {
   }
 
  protected:
-  std::unique_ptr<TestAutofillClient> test_autofill_client_;
+  std::unique_ptr<MockAutofillClient> test_autofill_client_;
   std::unique_ptr<TestContentAutofillDriver> driver_;
 
   FakeAutofillAgent fake_agent_;
@@ -478,6 +484,17 @@ TEST_F(ContentAutofillDriverTest, CreditCardFormInteractionOnHTTPS) {
   EXPECT_EQ(url, entry->GetURL());
   EXPECT_FALSE(!!(entry->GetSSL().content_status &
                   content::SSLStatus::DISPLAYED_CREDIT_CARD_FIELD_ON_HTTP));
+}
+
+TEST_F(ContentAutofillDriverTest, NotifyFirstUserGestureObservedInTab) {
+  driver_->NotifyFirstUserGestureObservedInTab();
+  EXPECT_CALL(fake_agent_, FirstUserGestureObservedInTab());
+  base::RunLoop().RunUntilIdle();
+}
+
+TEST_F(ContentAutofillDriverTest, FirstUserGestureObserved) {
+  EXPECT_CALL(*test_autofill_client_, OnFirstUserGestureObserved());
+  driver_->FirstUserGestureObserved();
 }
 
 }  // namespace autofill
