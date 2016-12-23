8a9a3a48816224af27b2b3ef21bcea0f84b31de9
jdoerrie <jdoerrie@chromium.org>
2016-12-23 03:01:22 -0800

    Remove usages of raw new
    This change introduces a cleanup mentioned in http://crrev.com/2598003002.
    Raw usages of new are replaced by base::MakeUnique.
    
    BUG=676785
    R=vabr@chromium.org
    
    Review-Url: https://codereview.chromium.org/2605483002
    Cr-Commit-Position: refs/heads/master@{#440616}

--- chrome/browser/password_manager/credential_manager_browsertest.cc
+++ chrome/browser/password_manager/credential_manager_browsertest.cc
@@ -215,10 +215,9 @@ IN_PROC_BROWSER_TEST_F(CredentialManagerBrowserTest,
   // Wait for the password store before checking the prompt because it pops up
   // after the store replies.
   WaitForPasswordStore();
-  std::unique_ptr<BubbleObserver> prompt_observer(
-      new BubbleObserver(WebContents()));
-  EXPECT_FALSE(prompt_observer->IsShowingSavePrompt());
-  EXPECT_FALSE(prompt_observer->IsShowingUpdatePrompt());
+  BubbleObserver prompt_observer(WebContents());
+  EXPECT_FALSE(prompt_observer.IsShowingSavePrompt());
+  EXPECT_FALSE(prompt_observer.IsShowingUpdatePrompt());
 
   // There should be an entry for both psl.example.com and www.example.com.
   password_manager::TestPasswordStore::PasswordMap passwords =
@@ -264,11 +263,10 @@ IN_PROC_BROWSER_TEST_F(CredentialManagerBrowserTest,
   observer.SetPathToWaitFor("/password/done.html");
   observer.Wait();
 
-  std::unique_ptr<BubbleObserver> prompt_observer(
-      new BubbleObserver(WebContents()));
+  BubbleObserver prompt_observer(WebContents());
   // The autofill password manager shouldn't react to the successful login
   // because it was suppressed when the site got the credential back.
-  EXPECT_FALSE(prompt_observer->IsShowingSavePrompt());
+  EXPECT_FALSE(prompt_observer.IsShowingSavePrompt());
 }
 
 IN_PROC_BROWSER_TEST_F(CredentialManagerBrowserTest, SaveViaAPIAndAutofill) {
@@ -292,10 +290,9 @@ IN_PROC_BROWSER_TEST_F(CredentialManagerBrowserTest, SaveViaAPIAndAutofill) {
   form_submit_observer.Wait();
 
   WaitForPasswordStore();
-  std::unique_ptr<BubbleObserver> prompt_observer(
-      new BubbleObserver(WebContents()));
-  ASSERT_TRUE(prompt_observer->IsShowingSavePrompt());
-  prompt_observer->AcceptSavePrompt();
+  BubbleObserver prompt_observer(WebContents());
+  ASSERT_TRUE(prompt_observer.IsShowingSavePrompt());
+  prompt_observer.AcceptSavePrompt();
 
   WaitForPasswordStore();
   password_manager::TestPasswordStore::PasswordMap stored =
@@ -351,10 +348,9 @@ IN_PROC_BROWSER_TEST_F(CredentialManagerBrowserTest, UpdateViaAPIAndAutofill) {
   // Wait for the password store before checking the prompt because it pops up
   // after the store replies.
   WaitForPasswordStore();
-  std::unique_ptr<BubbleObserver> prompt_observer(
-      new BubbleObserver(WebContents()));
-  EXPECT_FALSE(prompt_observer->IsShowingSavePrompt());
-  EXPECT_FALSE(prompt_observer->IsShowingUpdatePrompt());
+  BubbleObserver prompt_observer(WebContents());
+  EXPECT_FALSE(prompt_observer.IsShowingSavePrompt());
+  EXPECT_FALSE(prompt_observer.IsShowingUpdatePrompt());
   signin_form.skip_zero_click = false;
   signin_form.times_used = 1;
   signin_form.password_value = base::ASCIIToUTF16("API");
