a49e0c6575367adf6bae627e41a90bf1cca6fb4f
avi <avi@chromium.org>
2016-12-24 12:41:42 -0800

    Remove base::ScopedPtrHashMap from chrome/browser/android.
    BUG=579229
    
    Review-Url: https://codereview.chromium.org/2598153003
    Cr-Commit-Position: refs/heads/master@{#440674}

--- chrome/browser/android/compositor/tab_content_manager.h
+++ chrome/browser/android/compositor/tab_content_manager.h
@@ -8,10 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <jni.h>
 
+#include <unordered_map>
+
 #include "base/android/jni_android.h"
 #include "base/android/jni_weak_ref.h"
 #include "base/containers/hash_tables.h"
-#include "base/containers/scoped_ptr_hash_map.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "cc/layers/ui_resource_layer.h"
@@ -102,10 +103,10 @@ class TabContentManager : public ThumbnailCacheObserver {
 
  private:
   class TabReadbackRequest;
-  typedef base::hash_map<int, scoped_refptr<cc::Layer>> LayerMap;
-  typedef base::hash_map<int, scoped_refptr<ThumbnailLayer>> ThumbnailLayerMap;
-  typedef base::ScopedPtrHashMap<int, std::unique_ptr<TabReadbackRequest>>
-      TabReadbackRequestMap;
+  using LayerMap = base::hash_map<int, scoped_refptr<cc::Layer>>;
+  using ThumbnailLayerMap = base::hash_map<int, scoped_refptr<ThumbnailLayer>>;
+  using TabReadbackRequestMap =
+      std::unordered_map<int, std::unique_ptr<TabReadbackRequest>>;
 
   void PutThumbnailIntoCache(int tab_id,
                              float thumbnail_scale,
