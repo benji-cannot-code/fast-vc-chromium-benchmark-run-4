4f7f1fd84b81cfce37293bf0290db93d6ae4e523
wfh <wfh@chromium.org>
2016-12-25 13:30:26 -0800

    Revert of Convert utility process out-of-process file patching to mojo (patchset #15 id:420001 of https://codereview.chromium.org/2566053002/ )
    Reason for revert:
    broke component updater - see crbug.com/676960
    
    Original issue's description:
    > Convert utility process out-of-process file patching to mojo
    >
    > Define interface chrome::mojom::FilePatcher for out-of-process file
    > patching. Add that interface to the utility process, and expose the
    > interface to the browser process by mojo policy [1].
    >
    > ChromeOutOfProcessPatcher: remove PatchHost() helper class, replace
    > it with a content::UtilityProcessMojoClient() and use that to start
    > and stop the utility process needed for out-of-process file patches
    > and to perform out-of-process file patches using mojo calls, rather
    > than IPC::Send().
    >
    > Add a in-process browser test for the ChromeOutOfProcessPatcher (it
    > had no tests http://bit.ly/2h8mafX it seems). Copy the golden files
    > to random locations (input_file, patch_file) and patch them: checks
    > the patched output_file matches the golden output file.
    >
    > Delete all the IPC related to out-of-process file patching since we
    > no longer use them, we use chrome::mojom::FilePatcher instead.
    >
    > [1] chrome_content_utility_manifest_overlay.json
    >
    > TBR=waffles@chromium.org
    > BUG=597124, 660325
    >
    > Committed: https://crrev.com/32afc44b332255130c247e3bfc4a27261d11ddfb
    > Cr-Commit-Position: refs/heads/master@{#440601}
    
    TBR=jochen@chromium.org,dcheng@chromium.org,sammc@chromium.org,tibell@chromium.org,waffles@chromium.org,sorin@chromium.org,noel@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=597124, 660325,676960
    
    Review-Url: https://codereview.chromium.org/2599393002
    Cr-Commit-Position: refs/heads/master@{#440686}

--- chrome/browser/component_updater/component_patcher_operation_out_of_process.h
+++ chrome/browser/component_updater/component_patcher_operation_out_of_process.h
@@ -11,9 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_forward.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
-#include "chrome/common/file_patcher.mojom.h"
 #include "components/update_client/component_patcher_operation.h"
-#include "content/public/browser/utility_process_mojo_client.h"
 
 namespace base {
 class FilePath;
@@ -22,11 +20,14 @@ class SequencedTaskRunner;
 
 namespace component_updater {
 
+class PatchHost;
+
+// Implements the DeltaUpdateOpPatch out-of-process patching.
 class ChromeOutOfProcessPatcher : public update_client::OutOfProcessPatcher {
  public:
   ChromeOutOfProcessPatcher();
 
-  // update_client::OutOfProcessPatcher:
+  // DeltaUpdateOpPatch::OutOfProcessPatcher implementation.
   void Patch(const std::string& operation,
              scoped_refptr<base::SequencedTaskRunner> task_runner,
              const base::FilePath& input_abs_path,
@@ -37,16 +38,7 @@ class ChromeOutOfProcessPatcher : public update_client::OutOfProcessPatcher {
  private:
   ~ChromeOutOfProcessPatcher() override;
 
-  // Patch() request operation result.
-  void PatchDone(int result);
-
-  // Used to signal the operation result back to the Patch() requester.
-  scoped_refptr<base::SequencedTaskRunner> task_runner_;
-  base::Callback<void(int result)> callback_;
-
-  // Utility process used for out-of-process file patching.
-  std::unique_ptr<content::UtilityProcessMojoClient<chrome::mojom::FilePatcher>>
-      utility_process_mojo_client_;
+  scoped_refptr<PatchHost> host_;
 
   DISALLOW_COPY_AND_ASSIGN(ChromeOutOfProcessPatcher);
 };
