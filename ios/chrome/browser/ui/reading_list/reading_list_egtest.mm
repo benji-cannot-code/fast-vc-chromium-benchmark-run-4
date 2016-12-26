e1c8be925a09958b9254cc422f137ee3d580810f
olivierrobin <olivierrobin@chromium.org>
2016-12-26 12:52:18 -0800

    [ReadingList] Fix testSavingToReadingList egtest on iPad
    The test in AssertEntryVisible matched the tab containing the title of
    the page.
    Narrow the test by adding
    grey_ancestor(grey_kindOfClass([ReadingListCell class]))
    This is a follow up of https://codereview.chromium.org/2594383002/.
    
    BUG=None.
    
    Review-Url: https://codereview.chromium.org/2601743002
    Cr-Commit-Position: refs/heads/master@{#440722}

--- ios/chrome/browser/ui/reading_list/reading_list_egtest.mm
+++ ios/chrome/browser/ui/reading_list/reading_list_egtest.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/reading_list/reading_list_model_factory.h"
 #import "ios/chrome/browser/ui/commands/generic_chrome_command.h"
 #include "ios/chrome/browser/ui/commands/ios_command_ids.h"
+#import "ios/chrome/browser/ui/reading_list/reading_list_collection_view_item.h"
 #include "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/test/app/chrome_test_util.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
@@ -89,11 +90,12 @@ void TapEntry(std::string title) {
 // Asserts that the entry |title| is visible.
 void AssertEntryVisible(std::string title) {
   [[GREYUIThreadExecutor sharedInstance] drainUntilIdle];
-  [[EarlGrey selectElementWithMatcher:
-                 grey_allOf(chrome_test_util::staticTextWithAccessibilityLabel(
-                                base::SysUTF8ToNSString(title)),
-                            grey_sufficientlyVisible(), nil)]
-      assertWithMatcher:grey_sufficientlyVisible()];
+  [[EarlGrey
+      selectElementWithMatcher:
+          grey_allOf(chrome_test_util::staticTextWithAccessibilityLabel(
+                         base::SysUTF8ToNSString(title)),
+                     grey_ancestor(grey_kindOfClass([ReadingListCell class])),
+                     nil)] assertWithMatcher:grey_sufficientlyVisible()];
 }
 
 // Asserts that all the entries are visible.
@@ -113,11 +115,12 @@ void AssertAllEntriesVisible() {
 // Asserts that the entry |title| is not visible.
 void AssertEntryNotVisible(std::string title) {
   [[GREYUIThreadExecutor sharedInstance] drainUntilIdle];
-  [[EarlGrey selectElementWithMatcher:
-                 grey_allOf(chrome_test_util::staticTextWithAccessibilityLabel(
-                                base::SysUTF8ToNSString(title)),
-                            grey_sufficientlyVisible(), nil)]
-      assertWithMatcher:grey_notVisible()];
+  [[EarlGrey
+      selectElementWithMatcher:
+          grey_allOf(chrome_test_util::staticTextWithAccessibilityLabel(
+                         base::SysUTF8ToNSString(title)),
+                     grey_ancestor(grey_kindOfClass([ReadingListCell class])),
+                     nil)] assertWithMatcher:grey_notVisible()];
 }
 
 // Asserts |header| is visible.
