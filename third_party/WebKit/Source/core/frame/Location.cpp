b9c7cefb8d566f61c2ee6b5521747052ee6def82
yukishiino <yukishiino@chromium.org>
2016-12-26 05:05:56 -0800

    binding: Makes location.assign not work on cross-origin.
    This CL makes location.assign(url) not navigate to a new page
    while location.assign itself is still cross-origin accessible.
    
    location.assign shouldn't be cross-origin accessible, but
    we need assignOriginSafeMethodGetter to simulate the Incumbent
    realm.  See the comment in Location.idl for details.
    
    Other browsers' behavior is:
    FF, Safari = location.assign is not cross-origin accessible.
    Edge = cross-origin representation of |location| is not
    supported.
    So, no one seems supporting location.assign on cross-origin
    except for Blink.
    
    BUG=674987
    
    Review-Url: https://codereview.chromium.org/2589423002
    Cr-Commit-Position: refs/heads/master@{#440711}

--- third_party/WebKit/Source/core/frame/Location.cpp
+++ third_party/WebKit/Source/core/frame/Location.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/frame/Location.h"
 
+#include "bindings/core/v8/BindingSecurity.h"
 #include "bindings/core/v8/ExceptionState.h"
 #include "bindings/core/v8/V8DOMActivityLogger.h"
 #include "core/dom/DOMURLUtilsReadOnly.h"
@@ -229,6 +230,14 @@ void Location::assign(LocalDOMWindow* currentWindow,
                       LocalDOMWindow* enteredWindow,
                       const String& url,
                       ExceptionState& exceptionState) {
+  // TODO(yukishiino): Remove this check once we remove [CrossOrigin] from
+  // the |assign| DOM operation's definition in Location.idl.  See the comment
+  // in Location.idl for details.
+  if (!BindingSecurity::shouldAllowAccessTo(currentWindow, this,
+                                            exceptionState)) {
+    return;
+  }
+
   if (!m_frame)
     return;
   setLocation(url, currentWindow, enteredWindow, &exceptionState);
