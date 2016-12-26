a76c709c10c1aed423d337205fe0a254e0bdc869
falken <falken@chromium.org>
2016-12-25 21:49:28 -0800

    Revert of [ServiceWorker] Mojofy PushEvent of Service Worker. (patchset #3 id:40001 of https://codereview.chromium.org/2569993002/ )
    Reason for revert:
    Causes crashes on canary. See https://crbug.com/676984
    
    0x5eb4a4fa	(chrome.dll + 0x000fa4fa )	base::Callback<void ,1,1>::Callback<void ,1,1>(base::Callback<void ,1,1> const &)
    0x6003ec7d	(chrome.dll -service_worker_version.cc:1095 )	content::ServiceWorkerVersion::OnSimpleEventFinished(int,content::ServiceWorkerStatusCode,base::Time)
    0x5ff64f33	(chrome.dll -bind_internal.h:214 )	base::internal::FunctorTraits<void ( content::ServiceWorkerVersion::*)(int,content::ServiceWorkerStatusCode,base::Time),void>::Invoke<scoped_refptr<content::ServiceWorkerVersion> const &,int const &,content::ServiceWorkerStatusCode,base::Time>(void ( content::ServiceWorkerVersion::*)(int,content::ServiceWorkerStatusCode,base::Time),scoped_refptr<content::ServiceWorkerVersion> const &,int const &,content::ServiceWorkerStatusCode &&,base::Time &&)
    0x5ff50241	(chrome.dll -bind_internal.h:285 )	base::internal::InvokeHelper<0,void>::MakeItSo<void ( content::ServiceWorkerVersion::*const &)(int,blink::WebServiceWorkerEventResult,base::Time),scoped_refptr<content::ServiceWorkerVersion> const &,int,blink::WebServiceWorkerEventResult,base::Time>(void ( content::ServiceWorkerVersion::*const &)(int,blink::WebServiceWorkerEventResult,base::Time),scoped_refptr<content::ServiceWorkerVersion> const &,int &&,blink::WebServiceWorkerEventResult &&,base::Time &&)
    0x5ff655a6	(chrome.dll -bind_internal.h:339 )	base::internal::Invoker<base::internal::BindState<void ( content::ServiceWorkerVersion::*)(int,content::ServiceWorkerStatusCode,base::Time),scoped_refptr<content::ServiceWorkerVersion>,int>,void >::Run(base::internal::BindStateBase *,content::ServiceWorkerStatusCode &&,base::Time &&)
    0x5fbe1b09	(chrome.dll -callback.h:85 )	base::internal::RunMixin<base::Callback<void ,1,1> >::Run(filesystem::mojom::FileError,__int64)
    0x5fba0984	(chrome.dll -service_worker_event_dispatcher.mojom.cc:225 )	content::mojom::ServiceWorkerEventDispatcher_DispatchPushEvent_ForwardToCallback::Accept(mojo::Message *)
    0x6037605b	(chrome.dll -interface_endpoint_client.cc:336 )	mojo::InterfaceEndpointClient::HandleValidatedMessage(mojo::Message *)
    
    Original issue's description:
    > [ServiceWorker] Mojofy PushEvent of Service Worker.
    >
    > This CL converts push event IPCs into mojo interface:
    >   ServiceWorkerMsg_PushEvent
    >   ServiceWorkerHostMsg_PushEventFinished
    >
    > BUG=629701
    > TEST=content_unittests
    >
    > Committed: https://crrev.com/35842853527f50a04662a6dfe2c3cd1b10cf5915
    > Cr-Commit-Position: refs/heads/master@{#439739}
    
    TBR=leon.han@intel.com,peter@chromium.org,shimazu@chromium.org,tsepez@chromium.org,xiaofeng.zhang@intel.com
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=629701
    
    Review-Url: https://codereview.chromium.org/2600863002
    Cr-Commit-Position: refs/heads/master@{#440693}

--- content/browser/service_worker/embedded_worker_test_helper.h
+++ content/browser/service_worker/embedded_worker_test_helper.h
@@ -198,10 +198,9 @@ class EmbeddedWorkerTestHelper : public IPC::Sender,
                             const ServiceWorkerFetchRequest& request,
                             mojom::FetchEventPreloadHandlePtr preload_handle,
                             const FetchCallback& callback);
-  virtual void OnPushEvent(
-      const PushEventPayload& payload,
-      const mojom::ServiceWorkerEventDispatcher::DispatchPushEventCallback&
-          callback);
+  virtual void OnPushEvent(int embedded_worker_id,
+                           int request_id,
+                           const PushEventPayload& payload);
 
   // These functions simulate sending an EmbeddedHostMsg message to the
   // browser.
@@ -241,10 +240,7 @@ class EmbeddedWorkerTestHelper : public IPC::Sender,
                         const ServiceWorkerFetchRequest& request,
                         mojom::FetchEventPreloadHandlePtr preload_handle,
                         const FetchCallback& callback);
-  void OnPushEventStub(
-      const PushEventPayload& payload,
-      const mojom::ServiceWorkerEventDispatcher::DispatchPushEventCallback&
-          callback);
+  void OnPushEventStub(int request_id, const PushEventPayload& payload);
 
   MessagePortMessageFilter* NewMessagePortMessageFilter();
 
