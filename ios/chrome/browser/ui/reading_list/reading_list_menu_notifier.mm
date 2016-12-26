662ecf373828d2914230ff2dde143eab020572ec
olivierrobin <olivierrobin@chromium.org>
2016-12-26 00:56:46 -0800

    Fix unseen logic for Reading List menu entry.
    The entry is blue iff at least one entry has the UNSEEN status.
    Add an API to get the number of unseen items and use it in the
    menu.
    Add convenience methods to update unread, read and unseen
    counter.
    Add a "MarkAllRead" method to mark all items read.
    
    BUG=669396
    
    Review-Url: https://codereview.chromium.org/2592723002
    Cr-Commit-Position: refs/heads/master@{#440702}

--- ios/chrome/browser/ui/reading_list/reading_list_menu_notifier.mm
+++ ios/chrome/browser/ui/reading_list/reading_list_menu_notifier.mm
@@ -87,7 +87,7 @@ class ReadingListObserverBridge : public ReadingListModelObserver {
 
 - (void)readingListModelCompletedBatchUpdates:(const ReadingListModel*)model {
   [_delegate unreadCountChanged:model->unread_size()];
-  [_delegate unseenStateChanged:model->HasUnseenEntries()];
+  [_delegate unseenStateChanged:[self readingListUnseenItemsExist]];
 }
 
 - (NSInteger)readingListUnreadCount {
@@ -95,7 +95,7 @@ class ReadingListObserverBridge : public ReadingListModelObserver {
 }
 
 - (BOOL)readingListUnseenItemsExist {
-  return _readingListModel->HasUnseenEntries();
+  return _readingListModel->unseen_size() > 0;
 }
 
 @end
--- ios/chrome/browser/ui/reading_list/reading_list_view_controller.mm
+++ ios/chrome/browser/ui/reading_list/reading_list_view_controller.mm
@@ -304,7 +304,6 @@ using ItemsMapByDate = std::multimap<int64_t, ReadingListCollectionViewItem*>;
 #pragma mark - ReadingListModelBridgeObserver
 
 - (void)readingListModelLoaded:(const ReadingListModel*)model {
-  _readingListModel->ResetUnseenEntries();
   [self loadModel];
   UMA_HISTOGRAM_COUNTS_1000("ReadingList.Unread.Number", model->unread_size());
   UMA_HISTOGRAM_COUNTS_1000("ReadingList.Read.Number",
@@ -444,6 +443,7 @@ using ItemsMapByDate = std::multimap<int64_t, ReadingListCollectionViewItem*>;
 }
 
 - (void)dismiss {
+  _readingListModel->MarkAllSeen();
   // Reset observer to prevent further model update notifications.
   _modelBridge.reset();
   [_actionSheet stop];
