13e8b45c31045f88e0818f9ed570feec5fb0b13f
avi <avi@chromium.org>
2016-12-26 09:58:54 -0800

    Remove base::ScopedPtrHashMap from components/signin/.
    BUG=579229
    
    Review-Url: https://codereview.chromium.org/2605563002
    Cr-Commit-Position: refs/heads/master@{#440717}

--- components/signin/core/browser/account_fetcher_service.cc
+++ components/signin/core/browser/account_fetcher_service.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/command_line.h"
+#include "base/memory/ptr_util.h"
 #include "base/metrics/field_trial.h"
 #include "base/profiler/scoped_tracker.h"
 #include "base/trace_event/trace_event.h"
@@ -202,13 +203,16 @@ void AccountFetcherService::StartFetchingUserInfo(
   DCHECK(CalledOnValidThread());
   DCHECK(network_fetches_enabled_);
 
-  if (!ContainsKey(user_info_requests_, account_id)) {
+  std::unique_ptr<AccountInfoFetcher>& request =
+      user_info_requests_[account_id];
+  if (!request) {
     DVLOG(1) << "StartFetching " << account_id;
-    std::unique_ptr<AccountInfoFetcher> fetcher(new AccountInfoFetcher(
-        token_service_, signin_client_->GetURLRequestContext(), this,
-        account_id));
-    user_info_requests_.set(account_id, std::move(fetcher));
-    user_info_requests_.get(account_id)->Start();
+    std::unique_ptr<AccountInfoFetcher> fetcher =
+        base::MakeUnique<AccountInfoFetcher>(
+            token_service_, signin_client_->GetURLRequestContext(), this,
+            account_id);
+    request = std::move(fetcher);
+    request->Start();
   }
 }
 
@@ -272,7 +276,7 @@ void AccountFetcherService::SendRefreshTokenAnnotationRequest(
     // If request was sent AccountFetcherService needs to own request till it
     // finishes.
     if (request)
-      refresh_token_annotation_requests_.set(account_id, std::move(request));
+      refresh_token_annotation_requests_[account_id] = std::move(request);
   }
 #endif
 }
