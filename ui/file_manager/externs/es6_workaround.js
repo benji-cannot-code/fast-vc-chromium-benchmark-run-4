a9463c1f552f634bbd0da08e3bb8d00e441aed1f
yamaguchi <yamaguchi@chromium.org>
2016-12-25 18:38:45 -0800

    Make anchor texts inside the Cloud Backup accessible by keyboard.
    - Give appropriate tabindex attribute value to make them focusable by [tab] key right after the cloud import details button.
    - Use <action-link> custom element instead of <span> to allow opening it with [enter] key.
    
    TEST=browser_test: FileManagerJsTest.*
    BUG=475184
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2590913006
    Cr-Commit-Position: refs/heads/master@{#440690}

--- ui/file_manager/externs/es6_workaround.js
+++ ui/file_manager/externs/es6_workaround.js
@@ -15,3 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @param {number=} opt_position
  */
 String.prototype.startsWith = function(searchString, opt_position) {};
+
+/**
+ * @param {?function(this:S, T, number, !NodeList<T>): ?} callback
+ * @template T,S
+ * @return {undefined}
+ */
+NodeList.prototype.forEach = function(callback) {};
