f5ab87753f968dc7dee9de0085e6b7eb910f794f
rockot <rockot@chromium.org>
2016-12-27 10:41:00 -0800

    Revert of Disable UndoTabModelTest#testUndoNotSupported (patchset #1 id:1 of https://codereview.chromium.org/2575323004/ )
    Reason for revert:
    The root cause has been fixed, so this can be re-enabled.
    
    Original issue's description:
    > Disable UndoTabModelTest#testUndoNotSupported
    >
    > BUG=674654
    > TBR=dfalcantara@chromium.org
    >
    > Committed: https://crrev.com/722592581993a0a4171b92a9e604d6ac24f6615c
    > Cr-Commit-Position: refs/heads/master@{#438947}
    
    TBR=dfalcantara@chromium.org,twellington@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=674654
    
    Review-Url: https://codereview.chromium.org/2604823002
    Cr-Commit-Position: refs/heads/master@{#440769}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/UndoTabModelTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/UndoTabModelTest.java
@@ -1391,7 +1391,6 @@ public class UndoTabModelTest extends ChromeTabbedActivityTestBase {
      */
     @MediumTest
     @RetryOnFailure
-    @DisabledTest(message = "crbug.com/674654")
     public void testUndoNotSupported() throws InterruptedException {
         TabModel model = getActivity().getTabModelSelector().getModel(true);
         ChromeTabCreator tabCreator = getActivity().getTabCreator(true);
