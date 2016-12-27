9093bf18e2bbec39ef15f90be0b1920bd549ba18
gambard <gambard@chromium.org>
2016-12-27 00:45:03 -0800

    Add histogram for Online/Offline Reading List display
    This CL records if the Reading List entry is displayed as the online or
    offline version.
    
    BUG=651041
    
    Review-Url: https://codereview.chromium.org/2570463006
    Cr-Commit-Position: refs/heads/master@{#440743}

--- ios/chrome/browser/reading_list/reading_list_web_state_observer.mm
+++ ios/chrome/browser/reading_list/reading_list_web_state_observer.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/Foundation.h>
 
 #include "base/memory/ptr_util.h"
+#include "base/metrics/histogram_macros.h"
 #include "components/reading_list/ios/reading_list_model.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/chrome_url_constants.h"
@@ -201,6 +202,7 @@ void ReadingListWebStateObserver::PageLoaded(
 
   if (load_completion_status == web::PageLoadCompletionStatus::SUCCESS) {
     reading_list_model_->SetReadStatus(pending_url_, true);
+    UMA_HISTOGRAM_BOOLEAN("ReadingList.OfflineVersionDisplayed", false);
   } else {
     LoadOfflineReadingListEntry(item);
   }
@@ -273,4 +275,5 @@ void ReadingListWebStateObserver::LoadOfflineReadingListEntry(
   item->SetVirtualURL(pending_url_);
   web_state()->GetNavigationManager()->Reload(false);
   reading_list_model_->SetReadStatus(entry->URL(), true);
+  UMA_HISTOGRAM_BOOLEAN("ReadingList.OfflineVersionDisplayed", true);
 }
