98a79786b73b3367ad113beac9799781023dd36c
chrishtr <chrishtr@chromium.org>
2016-12-27 11:19:57 -0800

    Implement merging non-composited paint property nodes in the PACompositor.
    Currently, the PaintArtifactCompositor naively puts every PaintChunk
    into its own composited layer. This patch improves on that by merging
    a PaintChunk into the same layer as the PaintChunk which precedes it,
    if the new PaintChunk has a compatible PropertyTreeState (no directly
    composited property tree nodes between the state of the new PaintChunk
    and the old one).
    
    This algorithm is spelled out in a design doc, see crbug.com/668342.
    
    A large amount of this patch is additional tests and testing machinery
    to support the change in behavior.
    
    BUG=668342
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2581843002
    Cr-Commit-Position: refs/heads/master@{#440775}

--- third_party/WebKit/Source/platform/testing/PictureMatchers.h
+++ third_party/WebKit/Source/platform/testing/PictureMatchers.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef PictureMatchers_h
 #define PictureMatchers_h
 
+#include "platform/geometry/FloatRect.h"
 #include "platform/graphics/Color.h"
 #include "testing/gmock/include/gmock/gmock.h"
 
@@ -16,9 +17,24 @@ namespace blink {
 class FloatRect;
 
 // Matches if the picture draws exactly one rectangle, which (after accounting
-// for the total transformation matrix) matches the rect provided, and whose
-// paint has the color requested.
+// for the total transformation matrix and applying any clips inside that
+// transform) matches the rect provided, and whose paint has the color
+// requested.
+// Note that clips which appear outside of a transform are not currently
+// supported.
 ::testing::Matcher<const SkPicture&> drawsRectangle(const FloatRect&, Color);
+
+struct RectWithColor {
+  RectWithColor(const FloatRect& rectArg, const Color& colorArg)
+      : rect(rectArg), color(colorArg) {}
+  FloatRect rect;
+  Color color;
+};
+
+// Same as above, but matches a number of rectangles equal to the size of the
+// given vector.
+::testing::Matcher<const SkPicture&> drawsRectangles(
+    const Vector<RectWithColor>&);
 
 }  // namespace blink
 
