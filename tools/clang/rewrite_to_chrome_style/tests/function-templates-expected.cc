22055d62bcc54f92841864ecbd003d1d081237d7
lukasza <lukasza@chromium.org>
2016-12-28 11:40:31 -0800

    Blacklisting of method names coming from std library.
    BUG=672902
    
    Review-Url: https://codereview.chromium.org/2601063002
    Cr-Commit-Position: refs/heads/master@{#440879}

--- tools/clang/rewrite_to_chrome_style/tests/function-templates-expected.cc
+++ tools/clang/rewrite_to_chrome_style/tests/function-templates-expected.cc
@@ -37,7 +37,7 @@ class Checked {
 };
 
 template <typename To, typename From>
-To Bitwise_cast(From from) {
+To bitwise_cast(From from) {
   static_assert(sizeof(To) == sizeof(From), "msg");
   return reinterpret_cast<To>(from);
 }
@@ -61,5 +61,5 @@ struct ArrayTraits<WTF::Checked<U, int>> {
 
 }  // namespace mojo
 
-using WTF::Bitwise_cast;
+using WTF::bitwise_cast;
 using WTF::SafeCast;
