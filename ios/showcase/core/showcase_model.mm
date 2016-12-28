6980e32ba40cea4debffa801167ec92bf0bdb304
edchin <edchin@chromium.org>
2016-12-28 08:43:24 -0800

    Simple strip container that can reveal/hide tab strip.
    The simple strip container is WIP and this CL has only
    minimal functionality. This CL also showcases this
    strip container in Showcase.
    
    BUG=none.
    
    Review-Url: https://codereview.chromium.org/2594023002
    Cr-Commit-Position: refs/heads/master@{#440853}

--- ios/showcase/core/showcase_model.mm
+++ ios/showcase/core/showcase_model.mm
@@ -37,6 +37,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       showcase::kClassForInstantiationKey : @"SCTabGridCoordinator",
       showcase::kUseCaseKey : @"Tab grid",
     },
+    @{
+      showcase::kClassForDisplayKey : @"StripContainerViewController",
+      showcase::kClassForInstantiationKey : @"SCStripCoordinator",
+      showcase::kUseCaseKey : @"Tab strip container",
+    },
   ];
 }
 
