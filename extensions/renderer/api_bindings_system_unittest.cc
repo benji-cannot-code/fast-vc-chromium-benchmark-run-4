57d385582da2f49702b645d732e815f77742aca0
rdevlin.cronin <rdevlin.cronin@chromium.org>
2016-12-28 15:38:37 -0800

    [Extensions Bindings] Add a GetStringPropertyFromObject test method
    Common test practice: compare a stringified property on an object to an
    expected value (we use strings for easy readability if something is
    unexpected). Add a method to streamline the process a bit.
    
    BUG=653596
    
    Review-Url: https://codereview.chromium.org/2601143002
    Cr-Commit-Position: refs/heads/master@{#440904}

--- extensions/renderer/api_bindings_system_unittest.cc
+++ extensions/renderer/api_bindings_system_unittest.cc
@@ -233,10 +233,9 @@ TEST_F(APIBindingsSystemTest, TestInitializationAndCallbacks) {
     bindings_system()->CompleteRequest(last_request()->request_id,
                                        *expected_args);
 
-    std::unique_ptr<base::Value> result = GetBaseValuePropertyFromObject(
-        context->Global(), context, "callbackArguments");
-    ASSERT_TRUE(result);
-    EXPECT_EQ(ReplaceSingleQuotes(kResponseArgsJson), ValueToString(*result));
+    EXPECT_EQ(ReplaceSingleQuotes(kResponseArgsJson),
+              GetStringPropertyFromObject(context->Global(), context,
+                                          "callbackArguments"));
     reset_last_request();
   }
 
@@ -256,10 +255,8 @@ TEST_F(APIBindingsSystemTest, TestInitializationAndCallbacks) {
     bindings_system()->CompleteRequest(last_request()->request_id,
                                        base::ListValue());
 
-    std::unique_ptr<base::Value> result = GetBaseValuePropertyFromObject(
-        context->Global(), context, "callbackArguments");
-    ASSERT_TRUE(result);
-    EXPECT_EQ("[]", ValueToString(*result));
+    EXPECT_EQ("[]", GetStringPropertyFromObject(context->Global(), context,
+                                                "callbackArguments"));
     reset_last_request();
   }
 
@@ -277,10 +274,9 @@ TEST_F(APIBindingsSystemTest, TestInitializationAndCallbacks) {
     bindings_system()->FireEventInContext("alpha.alphaEvent", context,
                                           *expected_args);
 
-    std::unique_ptr<base::Value> result = GetBaseValuePropertyFromObject(
-        context->Global(), context, "eventArguments");
-    ASSERT_TRUE(result);
-    EXPECT_EQ(ReplaceSingleQuotes(kResponseArgsJson), ValueToString(*result));
+    EXPECT_EQ(ReplaceSingleQuotes(kResponseArgsJson),
+              GetStringPropertyFromObject(context->Global(), context,
+                                          "eventArguments"));
   }
 
   {
@@ -340,10 +336,9 @@ TEST_F(APIBindingsSystemTest, TestCustomHooks) {
     CallFunctionOnObject(context, alpha_api, kTestCall);
     EXPECT_TRUE(did_call);
 
-    std::unique_ptr<base::Value> result = GetBaseValuePropertyFromObject(
-        context->Global(), context, "callbackArguments");
-    ASSERT_TRUE(result);
-    EXPECT_EQ("[\"bar\"]", ValueToString(*result));
+    EXPECT_EQ("[\"bar\"]",
+              GetStringPropertyFromObject(context->Global(), context,
+                                          "callbackArguments"));
   }
 }
 
@@ -486,16 +481,13 @@ TEST_F(APIBindingsSystemTestWithRealAPI, RealAPIs) {
         "  this.idleState = state;\n"
         "});\n";
     ExecuteScript(context, kTestCall);
-    v8::Local<v8::Value> v8_result =
-        GetPropertyFromObject(context->Global(), context, "idleState");
-    EXPECT_TRUE(v8_result->IsUndefined());
+    EXPECT_EQ("undefined", GetStringPropertyFromObject(context->Global(),
+                                                       context, "idleState"));
     bindings_system()->FireEventInContext("idle.onStateChanged", context,
                                           *ListValueFromString("['active']"));
 
-    std::unique_ptr<base::Value> result =
-        GetBaseValuePropertyFromObject(context->Global(), context, "idleState");
-    ASSERT_TRUE(result);
-    EXPECT_EQ("\"active\"", ValueToString(*result));
+    EXPECT_EQ("\"active\"", GetStringPropertyFromObject(context->Global(),
+                                                        context, "idleState"));
   }
 }
 
