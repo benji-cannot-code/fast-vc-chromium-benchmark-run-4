eb2eb66061bd127679062d57345616a8854cbb8e
chrishtr <chrishtr@chromium.org>
2016-12-29 13:07:29 -0800

    Remove infinite clip in PaintArtifactCompositor.
    BUG=668342
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2604083002
    Cr-Commit-Position: refs/heads/master@{#440985}

--- third_party/WebKit/Source/platform/graphics/paint/GeometryMapper.h
+++ third_party/WebKit/Source/platform/graphics/paint/GeometryMapper.h
@@ -126,6 +126,13 @@ class PLATFORM_EXPORT GeometryMapper {
       const PropertyTreeState& ancestorState,
       bool& success);
 
+  // Returns the "clip visual rect" between |localTransformState| and
+  // |ancestorState|. See above for the definition of "clip visual rect".
+  FloatRect localToAncestorClipRect(
+      const PropertyTreeState& localTransformState,
+      const PropertyTreeState& ancestorState,
+      bool& success);
+
  private:
   // Used by mapToVisualRectInDestinationSpace() after fast mapping (assuming
   // destination is an ancestor of source) failed.
@@ -141,13 +148,6 @@ class PLATFORM_EXPORT GeometryMapper {
       const FloatRect&,
       const PropertyTreeState& sourceState,
       const PropertyTreeState& destinationState,
-      bool& success);
-
-  // Returns the "clip visual rect" between |localTransformState| and
-  // |ancestorState|. See above for the definition of "clip visual rect".
-  FloatRect localToAncestorClipRect(
-      const PropertyTreeState& localTransformState,
-      const PropertyTreeState& ancestorState,
       bool& success);
 
   // Returns the precomputed data if already set, or adds and memoizes a new
