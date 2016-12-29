653b733281e84ea99b55afa505b40427407529b8
yigu <yigu@chromium.org>
2016-12-28 18:14:51 -0800

    Fix CSS clips cannot be correctly handled by composited scroller
    BUG=645957
    TEST=PaintLayerScrollableAreaTest.OnlyAutoClippedScrollingContentsLayerPromoted
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2595593002
    Cr-Commit-Position: refs/heads/master@{#440925}

--- third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.cpp
@@ -1763,7 +1763,8 @@ bool PaintLayerScrollableArea::computeNeedsCompositedScrolling(
   // with the results.
   return !(layer->size().isEmpty() || layer->hasDescendantWithClipPath() ||
            layer->hasAncestorWithClipPath() ||
-           layer->layoutObject()->style()->hasBorderRadius());
+           layer->layoutObject()->style()->hasBorderRadius() ||
+           layer->layoutObject()->hasClip());
 }
 
 void PaintLayerScrollableArea::addStyleRelatedMainThreadScrollingReasons(
