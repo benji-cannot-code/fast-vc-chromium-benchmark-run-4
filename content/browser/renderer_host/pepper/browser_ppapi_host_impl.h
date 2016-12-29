648b3b5da04b8ac1ba40f0cbec269c95fd4b8698
avi <avi@chromium.org>
2016-12-29 12:29:16 -0800

    Remove base::ScopedPtrHashMap from content/browser/renderer_host/pepper/.
    BUG=579229
    
    Review-Url: https://codereview.chromium.org/2607773002
    Cr-Commit-Position: refs/heads/master@{#440982}

--- content/browser/renderer_host/pepper/browser_ppapi_host_impl.h
+++ content/browser/renderer_host/pepper/browser_ppapi_host_impl.h
@@ -8,10 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 #include <string>
+#include <unordered_map>
 #include <utility>
 
 #include "base/compiler_specific.h"
-#include "base/containers/scoped_ptr_hash_map.h"
 #include "base/files/file_path.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
@@ -163,8 +163,7 @@ class CONTENT_EXPORT BrowserPpapiHostImpl : public BrowserPpapiHost {
   scoped_refptr<SSLContextHelper> ssl_context_helper_;
 
   // Tracks all PP_Instances in this plugin and associated data.
-  base::ScopedPtrHashMap<PP_Instance, std::unique_ptr<InstanceData>>
-      instance_map_;
+  std::unordered_map<PP_Instance, std::unique_ptr<InstanceData>> instance_map_;
 
   scoped_refptr<HostMessageFilter> message_filter_;
 
