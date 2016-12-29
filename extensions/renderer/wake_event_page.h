889a145b98202b81b59238c1cf400838a8e6c265
avi <avi@chromium.org>
2016-12-28 17:26:28 -0800

    Remove base::ScopedPtrHashMap from extensions/.
    BUG=579229
    
    Review-Url: https://codereview.chromium.org/2605953002
    Cr-Commit-Position: refs/heads/master@{#440921}

--- extensions/renderer/wake_event_page.h
+++ extensions/renderer/wake_event_page.h
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <string>
+#include <unordered_map>
 
 #include "base/callback.h"
-#include "base/containers/scoped_ptr_hash_map.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
@@ -104,7 +104,7 @@ class WakeEventPage : public content::RenderThreadObserver {
 
   // All in-flight requests, keyed by request ID. Used on multiple threads, so
   // must be guarded by |requests_lock_|.
-  base::ScopedPtrHashMap<int, std::unique_ptr<RequestData>> requests_;
+  std::unordered_map<int, std::unique_ptr<RequestData>> requests_;
 
   // Lock for |requests_|.
   base::Lock requests_lock_;
