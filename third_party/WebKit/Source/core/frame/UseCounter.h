682619670f8ca203a9f2f1b012473d6b1fe2fa65
wangxianzhu <wangxianzhu@chromium.org>
2016-12-29 14:45:58 -0800

    Remove clip css/path use counters
    Review-Url: https://codereview.chromium.org/2606083002
    Cr-Commit-Position: refs/heads/master@{#440999}

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -708,8 +708,6 @@ class CORE_EXPORT UseCounter {
     // The above items are available in M46 branch.
 
     HTMLImportsHasStyleSheets = 940,
-    ClipPathOfPositionedElement = 944,
-    ClipCssOfPositionedElement = 945,
     NetInfoType = 946,
     NetInfoDownlinkMax = 947,
     NetInfoOnChange = 948,
@@ -1231,7 +1229,6 @@ class CORE_EXPORT UseCounter {
     MetaSetCookieWhenCSPBlocksInlineScript = 1549,
     MetaRefreshWhenCSPBlocksInlineScript = 1550,
     MiddleClickAutoscrollStart = 1551,
-    ClipCssOfFixedPositionElement = 1552,
     RTCPeerConnectionCreateOfferOptionsOfferToReceive = 1553,
     DragAndDropScrollStart = 1554,
     PresentationConnectionListConnectionAvailableEventListener = 1555,
