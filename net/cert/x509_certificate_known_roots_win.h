3eb46187c4a0928c208e584b10fb6616cecf4ad4
brucedawson <brucedawson@chromium.org>
2016-12-28 16:49:06 -0800

    Mark kKnownRootCertSHA256Hashes as const
    Marking kKnownRootCertSHA256Hashes as const puts it into the read-only
    data segment which has various minor advantages such as making data
    corruption impossible and guaranteeing that the data will always be
    in shareable pages.
    
    R=rsleevi@chromium.org
    BUG=677351
    
    Review-Url: https://codereview.chromium.org/2608643002
    Cr-Commit-Position: refs/heads/master@{#440914}

--- net/cert/x509_certificate_known_roots_win.h
+++ net/cert/x509_certificate_known_roots_win.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // Note that these *are not* trust anchors for Chromium. They are only used to
 // distinguish `real' root CAs from roots that were user-installed.
-static uint8_t kKnownRootCertSHA256Hashes[][32] = {
+static const uint8_t kKnownRootCertSHA256Hashes[][32] = {
     // C=US, O=Network Solutions L.L.C., CN=Network Solutions Certificate
     // Authority
     {0x00, 0x16, 0x86, 0xCD, 0x18, 0x1F, 0x83, 0xA1, 0xB1, 0x21, 0x7D, 0x30,
