07fd214db65b2cb1a1bb04dfe664f5ee76443ee6
caseq <caseq@chromium.org>
2016-12-28 18:19:18 -0800

    DevTools: move front-end URL handling to DevToolsUIBindingds
    BUG=662859
    
    Review-Url: https://codereview.chromium.org/2607833002
    Cr-Commit-Position: refs/heads/master@{#440926}

--- chrome/browser/devtools/devtools_ui_bindings.h
+++ chrome/browser/devtools/devtools_ui_bindings.h
@@ -43,6 +43,8 @@ class DevToolsUIBindings : public DevToolsEmbedderMessageDispatcher::Delegate,
   static DevToolsUIBindings* ForWebContents(
       content::WebContents* web_contents);
 
+  static GURL SanitizeFrontendURL(const GURL& url);
+
   class Delegate {
    public:
     virtual ~Delegate() {}
@@ -199,6 +201,7 @@ class DevToolsUIBindings : public DevToolsEmbedderMessageDispatcher::Delegate,
   typedef base::Callback<void(bool)> InfoBarCallback;
   void ShowDevToolsConfirmInfoBar(const base::string16& message,
                                   const InfoBarCallback& callback);
+  void UpdateFrontendHost();
 
   // Extensions support.
   void AddDevToolsExtensionsToClient();
