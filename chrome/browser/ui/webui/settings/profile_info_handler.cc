ec744fcaf7fe26614a919690ab3dd4acda161155
stevenjb <stevenjb@chromium.org>
2017-01-03 11:33:27 -0800

    MD Settings: users: Register UserImageSource in handler
    BUG=669003
    
    Review-Url: https://codereview.chromium.org/2607883002
    Cr-Commit-Position: refs/heads/master@{#441182}

--- chrome/browser/ui/webui/settings/profile_info_handler.cc
+++ chrome/browser/ui/webui/settings/profile_info_handler.cc
@@ -42,7 +42,13 @@ const char ProfileInfoHandler::kProfileStatsCountReadyEventName[] =
 
 ProfileInfoHandler::ProfileInfoHandler(Profile* profile)
     : profile_(profile),
-      profile_observer_(this) {}
+      profile_observer_(this) {
+#if defined(OS_CHROMEOS)
+  // Set up the chrome://userimage/ source.
+  content::URLDataSource::Add(profile,
+                              new chromeos::options::UserImageSource());
+#endif
+}
 
 ProfileInfoHandler::~ProfileInfoHandler() {}
 
