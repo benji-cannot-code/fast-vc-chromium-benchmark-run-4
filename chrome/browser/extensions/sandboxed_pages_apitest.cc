7e7d2fb97aed2a35cf87670c4f6114048919ccab
lazyboy <lazyboy@chromium.org>
2017-01-03 13:17:30 -0800

    Restrict app sandbox's CSP to disallow loading web content in them.
    The CL restricts loading subframes and scripts inside chrome app
    sandbox pages (https://developer.chrome.com/apps/manifest/sandbox).
    The restriction would only allow loading these resources from
    'self', i.e. the app's resources.
    
    1) scripts
    Two CSP directives can affect subframe restriction:
    script-src or
    default-src fallback when script-src is not present.
    
    2) subframes
    Three possible CSP directives can affect script restriction:
    frame-src or (deprecated)
    child-src or
    default-src fallback when none of frame-src/child-src is present.
    
    This CL adds a GetEffectiveSandboxedPageCSP function, which
    returns the effective CSP chrome will use when a certain
    sandbox CSP (sandbox.content_security_policy key) is specified in
    app's manifest.
    The effective CSP is a restricted CSP that strips out any remote
    hosts from #1 and #2. Any directive source value that is not
    surrounded by single quotes are considered remote hosts. The
    approach is to add 'self' as source value by default if 'none' was
    not specified.
    
    TODO: Update sandbox documentation page.
    
    BUG=615585
    Test=Loading a remote script from foo.com inside a chrome
    apps sandbox page should not be possible, even with setting
    the following "sandbox.content_security_policy" key in the
    manifest:
    script-src: foo.com
    Same would go with loading <iframe> elements. It should not
    be possible to load web content <iframe>s inside apps.
    
    Review-Url: https://codereview.chromium.org/2563843002
    Cr-Commit-Position: refs/heads/master@{#441208}

--- chrome/browser/extensions/sandboxed_pages_apitest.cc
+++ chrome/browser/extensions/sandboxed_pages_apitest.cc
@@ -8,3 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 IN_PROC_BROWSER_TEST_F(ExtensionApiTest, SandboxedPages) {
   EXPECT_TRUE(RunExtensionSubtest("sandboxed_pages", "main.html")) << message_;
 }
+
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, SandboxedPagesCSP) {
+  ASSERT_TRUE(StartEmbeddedTestServer());
+
+  // This app attempts to load remote web content inside a sandboxed page.
+  // Loading web content will fail because of CSP. In addition to that we will
+  // show manifest warnings, hence the kFlagIgnoreManifestWarnings.
+  EXPECT_TRUE(RunExtensionSubtest("sandboxed_pages_csp", "main.html",
+                                  kFlagIgnoreManifestWarnings))
+      << message_;
+}
