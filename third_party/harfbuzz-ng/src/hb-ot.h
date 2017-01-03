068b13c46a7ff1b7c2392dd293dac70aaf983596
eae <eae@chromium.org>
2017-01-03 11:49:26 -0800

    Roll HarfBuzz to 1.3.4
    BUG=668338
    TBR=drott@chromium.org
    
    Review-Url: https://codereview.chromium.org/2609123003
    Cr-Commit-Position: refs/heads/master@{#441189}

--- third_party/harfbuzz-ng/src/hb-ot.h
+++ third_party/harfbuzz-ng/src/hb-ot.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "hb-ot-font.h"
 #include "hb-ot-layout.h"
+#include "hb-ot-math.h"
 #include "hb-ot-tag.h"
 #include "hb-ot-shape.h"
 
