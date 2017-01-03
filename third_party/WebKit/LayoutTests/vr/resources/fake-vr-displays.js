787d4c13d50754a37ce70dd52fe4a2c342502985
leilei <leilei@chromium.org>
2017-01-02 20:48:09 -0800

    Add the first layout tests for WebVR to test navigator.getVRDisplays with mocked vr_service mojo interfaces.
    We will add more layout tests to cover more WebVR APIs later with same
    approach.
    
    BUG=675325
    
    Review-Url: https://codereview.chromium.org/2599113002
    Cr-Commit-Position: refs/heads/master@{#441088}

---
+++ third_party/WebKit/LayoutTests/vr/resources/fake-vr-displays.js
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+'use strict';
+
+function fakeVRDisplays(){
+  return [{
+  index : 0,
+  displayName : "FakeVRDevice",
+  capabilities : {
+    hasOrientation : true,
+    hasPosition : false,
+    hasExternalDisplay : false,
+    canPresent : false,
+  },
+  stageParameters : null,
+  leftEye : {
+    fieldOfView : {
+      upDegrees : 45,
+      downDegrees : 45,
+      leftDegrees : 45,
+      rightDegrees : 45,
+    },
+    offset : [
+      -0.03, 0, 0
+    ],
+    renderWidth : 1024,
+    renderHeight : 1024,
+  },
+  rightEye : {
+    fieldOfView : {
+      upDegrees : 45,
+      downDegrees : 45,
+      leftDegrees : 45,
+      rightDegrees : 45,
+    },
+    offset : [
+      0.03, 0, 0
+    ],
+    renderWidth : 1024,
+    renderHeigth : 1024,
+  }
+}];
+}
