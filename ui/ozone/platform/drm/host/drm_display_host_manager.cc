7a7556109f16204bdbd8d8509d2489aceaa26a6a
fdoray <fdoray@chromium.org>
2017-01-03 09:09:16 -0800

    Use TaskScheduler instead of WorkerPool in drm_display_host_manager.cc.
    The following traits are used:
    
    Priority: Inherited (default)
      The priority is inherited from the calling context (i.e. TaskTraits
      are initialized with the priority of the current task).
    
    Shutdown behavior: CONTINUE_ON_SHUTDOWN
      Tasks posted with this mode which have not started executing before
      shutdown is initiated will never run. Tasks with this mode running at
      shutdown will be ignored (the worker will not be joined).
    
      Note: Tasks that were previously posted to base::WorkerPool should
      use this shutdown behavior because this is how base::WorkerPool
      handles all its tasks.
    
    May Block:
      Tasks posted with MayBlock() may block. This includes but is not
      limited to tasks that wait on synchronous file I/O operations:
      read or write a file from disk, interact with a pipe or a socket,
      rename or delete a file, enumerate files in a directory, etc. This
      trait isn't required for the mere use of locks.
    
    BUG=659191
    
    Review-Url: https://codereview.chromium.org/2603153003
    Cr-Commit-Position: refs/heads/master@{#441141}

--- ui/ozone/platform/drm/host/drm_display_host_manager.cc
+++ ui/ozone/platform/drm/host/drm_display_host_manager.cc
@@ -15,9 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/stringprintf.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/thread_restrictions.h"
 #include "base/threading/thread_task_runner_handle.h"
-#include "base/threading/worker_pool.h"
 #include "ui/display/types/display_snapshot.h"
 #include "ui/events/ozone/device/device_event.h"
 #include "ui/events/ozone/device/device_manager.h"
@@ -53,10 +53,9 @@ base::FilePath MapDevPathToSysPath(const base::FilePath& device_path) {
       base::FilePath("/sys/class/drm").Append(device_path.BaseName()));
 }
 
-void OpenDeviceOnWorkerThread(
-    const base::FilePath& device_path,
-    const scoped_refptr<base::TaskRunner>& reply_runner,
-    const OnOpenDeviceReplyCallback& callback) {
+void OpenDeviceAsync(const base::FilePath& device_path,
+                     const scoped_refptr<base::TaskRunner>& reply_runner,
+                     const OnOpenDeviceReplyCallback& callback) {
   base::FilePath sys_path = MapDevPathToSysPath(device_path);
 
   std::unique_ptr<DrmDeviceHandle> handle(new DrmDeviceHandle());
@@ -246,13 +245,17 @@ void DrmDisplayHostManager::ProcessEvent() {
     switch (event.action_type) {
       case DeviceEvent::ADD:
         if (drm_devices_.find(event.path) == drm_devices_.end()) {
-          task_pending_ = base::WorkerPool::PostTask(
+          base::PostTaskWithTraits(
               FROM_HERE,
-              base::Bind(&OpenDeviceOnWorkerThread, event.path,
+              base::TaskTraits()
+                  .WithShutdownBehavior(
+                      base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN)
+                  .MayBlock(),
+              base::Bind(&OpenDeviceAsync, event.path,
                          base::ThreadTaskRunnerHandle::Get(),
                          base::Bind(&DrmDisplayHostManager::OnAddGraphicsDevice,
-                                    weak_ptr_factory_.GetWeakPtr())),
-              false /* task_is_slow */);
+                                    weak_ptr_factory_.GetWeakPtr())));
+          task_pending_ = true;
         }
         break;
       case DeviceEvent::CHANGE:
