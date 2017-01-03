6a81d3caa50a161ae8a1a86d09ec6f7db8e9cd6d
varkha <varkha@chromium.org>
2017-01-03 11:48:11 -0800

    [ash-md] Adds sticky headers in audio detailed page
    BUG=676175
    TEST=Visual
    
    Review-Url: https://codereview.chromium.org/2593583003
    Cr-Commit-Position: refs/heads/master@{#441188}

--- ash/common/system/chromeos/audio/audio_detailed_view.h
+++ ash/common/system/chromeos/audio/audio_detailed_view.h
@@ -13,6 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/audio/audio_device.h"
 #include "ui/gfx/font.h"
 
+namespace gfx {
+struct VectorIcon;
+}
+
 namespace views {
 class View;
 }
@@ -35,8 +39,7 @@ class AudioDetailedView : public TrayDetailsView {
   // list.
   void AddInputHeader();
   void AddOutputHeader();
-  void AddScrollListInfoItem(const base::string16& text,
-                             const gfx::ImageSkia& image);
+  void AddScrollListInfoItem(int text_id, const gfx::VectorIcon& icon);
 
   HoverHighlightView* AddScrollListItem(const base::string16& text,
                                         bool highlight,
