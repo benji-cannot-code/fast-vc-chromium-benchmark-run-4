d62ff7cc87d9b9ae508c79e9febf6297e124abd6
sashab <sashab@chromium.org>
2017-01-02 21:33:41 -0800

    Added functionality in ComputedStyleBase to use platform/ types
    Added functionality in ComputedStyleBase to use platform/ types that
    conform to naming guidelines.
    
    Also converted the direction property to be used by this new generator,
    which uses the external enum type platform/text/TextDirection.
    
    BUG=628043
    
    Review-Url: https://codereview.chromium.org/2563913002
    Cr-Commit-Position: refs/heads/master@{#441091}

--- third_party/WebKit/Source/build/scripts/css_properties.py
+++ third_party/WebKit/Source/build/scripts/css_properties.py
@@ -33,6 +33,8 @@ class CSSProperties(in_generator.Writer):
         'builder_skip': False,
         'direction_aware': False,
         'api_class': None,
+        # Generated ComputedStyle annotations.
+        'field_storage_type': None,
         # Typed OM annotations.
         'typedom_types': [],
         'keywords': [],
