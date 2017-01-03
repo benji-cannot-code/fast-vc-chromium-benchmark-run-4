2a760414c8f104f04410884d17015440dc6b188b
pauljensen <pauljensen@chromium.org>
2017-01-03 11:44:31 -0800

    [Cronet] Enforce implementation does not call through API classes
    Such calls could fail if an older API is used that
    does not contain newer methods.  Calls should instead call through a
    wrapper class from VersionSafeCallbacks.
    
    R=kapishnikov
    BUG=629299
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_cronet_tester
    
    Review-Url: https://codereview.chromium.org/2440613003
    Cr-Commit-Position: refs/heads/master@{#441186}

---
+++ components/cronet/tools_unittest.py
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/python
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Run tools/ unittests."""
+
+import sys
+import unittest
+
+if __name__ == '__main__':
+  suite = unittest.TestLoader().discover('tools', pattern = "*_unittest.py")
+  sys.exit(0 if unittest.TextTestRunner().run(suite).wasSuccessful() else 1)
