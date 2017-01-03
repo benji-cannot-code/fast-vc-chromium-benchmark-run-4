408c0594f66b06b44ada6b1b2a5ca1316843a749
eugenebut <eugenebut@chromium.org>
2017-01-03 10:16:11 -0800

    [ios] Moved string_util file from ios/web to components/translate.
    string_util.h has one function (GetStringByClippingLastWord) which
    is used only by language_detection_controller.mm. So string_util* files
    were moved to components/translate/ios/browser and renamed to
    string_clipping_util.
    
    Also removed obsoleted TODO since crbug.com/324304 was closed.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2585953003
    Cr-Commit-Position: refs/heads/master@{#441159}

--- components/translate/ios/browser/string_clipping_util_unittest.cc
+++ components/translate/ios/browser/string_clipping_util_unittest.cc
@@ -3,14 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ios/web/public/string_util.h"
+#include "components/translate/ios/browser/string_clipping_util.h"
 
 #include <stddef.h>
 
 #include "base/strings/utf_string_conversions.h"
 #include "testing/gtest/include/gtest/gtest.h"
-
-namespace web {
 
 // Tests that a regular sentence is clipped correctly.
 TEST(StringByClippingLastWordTest, ClipRegularSentence) {
@@ -44,5 +42,3 @@ TEST(StringByClippingLastWordTest, ClipLongTextContentNoSpace) {
   EXPECT_EQ(kLongStringLength, result.size());
   EXPECT_EQ(0u, long_string.find_first_of(result));
 }
-
-}  // namespace web
