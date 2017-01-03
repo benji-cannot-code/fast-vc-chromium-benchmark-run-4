7bec814f3220a99ea4a54d7b60003fc0bb3888f6
vitaliii <vitaliii@chromium.org>
2017-01-03 09:08:13 -0800

    [NTP::SectionOrder] Make ClickBasedRanker move dismissed sections down.
    Move dissmised sections 2 positions down in ClickBasedRanker. Dismissing a
    section is a strong negative signal, thus, category rankers can react to it.
    
    BUG=677781
    
    Review-Url: https://codereview.chromium.org/2610553002
    Cr-Commit-Position: refs/heads/master@{#441140}

--- components/ntp_snippets/category_rankers/fake_category_ranker.cc
+++ components/ntp_snippets/category_rankers/fake_category_ranker.cc
@@ -35,4 +35,8 @@ void FakeCategoryRanker::OnSuggestionOpened(Category category) {
   // Ignored.
 }
 
+void FakeCategoryRanker::OnCategoryDismissed(Category category) {
+  // Ignored.
+}
+
 }  // namespace ntp_snippets
