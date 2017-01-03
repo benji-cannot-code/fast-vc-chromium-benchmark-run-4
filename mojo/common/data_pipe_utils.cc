b1429ea897091a76c6bcdcfc81ccaf4d351c37b0
sammc <sammc@chromium.org>
2017-01-03 14:45:01 -0800

    Remove unused parts of //mojo/common.
    BUG=608017
    
    Review-Url: https://codereview.chromium.org/2608173002
    Cr-Commit-Position: refs/heads/master@{#441228}

--- mojo/common/data_pipe_utils.cc
+++ mojo/common/data_pipe_utils.cc
@@ -5,16 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "mojo/common/data_pipe_utils.h"
 
-#include <stddef.h>
-#include <stdint.h>
-#include <stdio.h>
 #include <utility>
 
-#include "base/files/file_path.h"
-#include "base/files/file_util.h"
-#include "base/files/scoped_file.h"
-#include "base/message_loop/message_loop.h"
-#include "base/task_runner_util.h"
+#include "base/bind.h"
 
 namespace mojo {
 namespace common {
@@ -59,12 +52,7 @@ size_t CopyToStringHelper(
   return num_bytes;
 }
 
-size_t CopyToFileHelper(FILE* fp, const void* buffer, uint32_t num_bytes) {
-  return fwrite(buffer, 1, num_bytes, fp);
-}
-
-} // namespace
-
+}  // namespace
 
 // TODO(hansmuller): Add a max_size parameter.
 bool BlockingCopyToString(ScopedDataPipeConsumerHandle source,
@@ -106,26 +94,6 @@ bool MOJO_COMMON_EXPORT BlockingCopyFromString(
       return result == MOJO_RESULT_FAILED_PRECONDITION;
     }
   }
-}
-
-bool BlockingCopyToFile(ScopedDataPipeConsumerHandle source,
-                        const base::FilePath& destination) {
-  base::ScopedFILE fp(base::OpenFile(destination, "wb"));
-  if (!fp)
-    return false;
-  return BlockingCopyHelper(std::move(source),
-                            base::Bind(&CopyToFileHelper, fp.get()));
-}
-
-void CopyToFile(ScopedDataPipeConsumerHandle source,
-                const base::FilePath& destination,
-                base::TaskRunner* task_runner,
-                const base::Callback<void(bool)>& callback) {
-  base::PostTaskAndReplyWithResult(
-      task_runner,
-      FROM_HERE,
-      base::Bind(&BlockingCopyToFile, base::Passed(&source), destination),
-      callback);
 }
 
 }  // namespace common
