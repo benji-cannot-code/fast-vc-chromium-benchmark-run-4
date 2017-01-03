0fdbe59e361231b0995d05bb4d322846811238e3
einbinder <einbinder@chromium.org>
2017-01-03 15:28:00 -0800

    DevTools: Preserve the selection across shadow DOM in StylesSideBar
    If a selection crossed between shadow roots, it wouldn't be detected
    and a new blank property would be added on mouse up.
    
    The last patch about this was missing a vital exclamation point.
    
    BUG=654163
    
    Review-Url: https://codereview.chromium.org/2609243002
    Cr-Commit-Position: refs/heads/master@{#441251}

--- third_party/WebKit/Source/devtools/front_end/elements/StylesSidebarPane.js
+++ third_party/WebKit/Source/devtools/front_end/elements/StylesSidebarPane.js
@@ -1242,7 +1242,7 @@ Elements.StylePropertiesSection = class {
       return;
 
     if (this.propertiesTreeOutline.element.shadowRoot.firstChild &&
-        this.propertiesTreeOutline.element.shadowRoot.firstChild.isComponentSelectionCollapsed())
+        !this.propertiesTreeOutline.element.shadowRoot.firstChild.isComponentSelectionCollapsed())
       return;
 
     if (this._checkWillCancelEditing())
