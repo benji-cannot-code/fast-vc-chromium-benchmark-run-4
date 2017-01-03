be2aa0e6a350a79be73288f386d337ba9b275d06
davidben <davidben@chromium.org>
2017-01-03 14:30:16 -0800

    Remove CryptAcquireContextLocked.
    With keygen gone, this is no longer called.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2606393002
    Cr-Commit-Position: refs/heads/master@{#441226}

--- crypto/capi_util.h
+++ crypto/capi_util.h
@@ -10,25 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include "crypto/crypto_export.h"
-#include "crypto/wincrypt_shim.h"
 
 namespace crypto {
-
-// CryptAcquireContext when passed CRYPT_NEWKEYSET or CRYPT_DELETEKEYSET in
-// flags is not thread-safe. For such calls, we create a global lock to
-// synchronize it.
-//
-// From "Threading Issues with Cryptographic Service Providers",
-// <http://msdn.microsoft.com/en-us/library/aa388149(v=VS.85).aspx>:
-//
-// "The CryptAcquireContext function is generally thread safe unless
-// CRYPT_NEWKEYSET or CRYPT_DELETEKEYSET is specified in the dwFlags
-// parameter."
-CRYPTO_EXPORT BOOL CryptAcquireContextLocked(HCRYPTPROV* prov,
-                                             LPCWSTR container,
-                                             LPCWSTR provider,
-                                             DWORD prov_type,
-                                             DWORD flags);
 
 // Wrappers of malloc and free for CryptoAPI routines that need memory
 // allocators, such as in CRYPT_DECODE_PARA. Such routines require WINAPI
