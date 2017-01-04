3bd1324f8efc4935dd96deda2b310874764185bd
jiaxi <jiaxi@google.com>
2017-01-03 21:51:17 -0800

    [MD Bookmarks] Add UI for Material Bookmarks.
    This CL adds the basic UI for the material design bookmark manager.
    - list.html is the list of bookmarks.
    - sidebar.html contains the folder tree.
    
    BUG=651980
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2603303002
    Cr-Commit-Position: refs/heads/master@{#441325}

---
+++ chrome/browser/resources/md_bookmarks/list.js
@@ -0,0 +1,50 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+Polymer({
+  is: 'bookmarks-list',
+
+  properties: {
+    /** @type {BookmarkTreeNode} */
+    selectedNode: Object,
+  },
+
+  listeners: {
+    'toggle-menu': 'onToggleMenu_'
+  },
+
+  /**
+   * @param {Event} e
+   * @private
+   */
+  onToggleMenu_: function(e) {
+    var menu = /** @type {!CrActionMenuElement} */ (
+        this.$.dropdown);
+    menu.showAt(/** @type {!Element} */ (e.detail.target));
+  },
+
+  // TODO(jiaxi): change these dummy click event handlers later.
+  /** @private */
+  onEditTap_: function() {
+    this.closeDropdownMenu_();
+  },
+
+  /** @private */
+  onCopyURLTap_: function() {
+    this.closeDropdownMenu_();
+  },
+
+  /** @private */
+  onDeleteTap_: function() {
+    this.closeDropdownMenu_();
+  },
+
+  /** @private */
+  closeDropdownMenu_: function() {
+    var menu = /** @type {!CrActionMenuElement} */ (
+        this.$.dropdown);
+    menu.close();
+  }
+});
