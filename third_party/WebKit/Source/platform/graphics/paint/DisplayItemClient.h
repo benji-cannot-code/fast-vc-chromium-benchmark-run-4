1170cddc48b6224e74f7aa0d76e7dbb695d9c6a0
wangxianzhu <wangxianzhu@chromium.org>
2017-01-04 11:24:48 -0800

    Disable copy and assignment for DisplayItemClient
    The copy constructor breaks DisplayItemClient aliveness checking.
    We could fix the copy constructor but it looks better not to support
    the copy constructor.
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2611923002
    Cr-Commit-Position: refs/heads/master@{#441432}

--- third_party/WebKit/Source/platform/graphics/paint/DisplayItemClient.h
+++ third_party/WebKit/Source/platform/graphics/paint/DisplayItemClient.h
@@ -46,6 +46,7 @@ class PLATFORM_EXPORT DisplayItemClient {
   static void endShouldKeepAliveAllClients(const void* owner);
   static void endShouldKeepAliveAllClients();
 #else
+  DisplayItemClient() {}
   virtual ~DisplayItemClient() {}
 #endif
 
@@ -172,6 +173,8 @@ class PLATFORM_EXPORT DisplayItemClient {
 
   mutable CacheGenerationOrInvalidationReason
       m_cacheGenerationOrInvalidationReason;
+
+  DISALLOW_COPY_AND_ASSIGN(DisplayItemClient);
 };
 
 inline bool operator==(const DisplayItemClient& client1,
