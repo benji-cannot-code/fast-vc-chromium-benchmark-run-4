80af0feb9b12cf445b8c565f3a2941c31b0340e3
dullweber <dullweber@chromium.org>
2017-01-04 09:05:18 -0800

    Refactor cache counting code as a preparation for a more exact implementation.
    Move code from StoragePartitionHttpCacheRemover to CacheCountingHelper. Improve tests by adding a CacheTestUtil class for common functionality of cache counting and cache deletion tests.
    
    BUG=671196
    TBR=achuith@chromium.org
    
    Review-Url: https://codereview.chromium.org/2556363003
    Cr-Commit-Position: refs/heads/master@{#441399}

--- chrome/browser/ui/webui/settings/chromeos/device_storage_handler.cc
+++ chrome/browser/ui/webui/settings/chromeos/device_storage_handler.cc
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/grit/generated_resources.h"
 #include "chromeos/cryptohome/homedir_methods.h"
-#include "components/browsing_data/content/storage_partition_http_cache_data_remover.h"
+#include "components/browsing_data/content/conditional_cache_counting_helper.h"
 #include "components/drive/chromeos/file_system_interface.h"
 #include "components/user_manager/user_manager.h"
 #include "content/public/browser/browser_context.h"
@@ -233,11 +233,11 @@ void StorageHandler::UpdateBrowsingDataSize() {
   has_browser_site_data_size_ = false;
   Profile* const profile = Profile::FromWebUI(web_ui());
   // Fetch the size of http cache in browsing data.
-  // StoragePartitionHttpCacheDataRemover deletes itself when it is done.
-  browsing_data::StoragePartitionHttpCacheDataRemover::CreateForRange(
+  // ConditionalCacheCountingHelper deletes itself when it is done.
+  browsing_data::ConditionalCacheCountingHelper::CreateForRange(
       content::BrowserContext::GetDefaultStoragePartition(profile),
-      base::Time(),
-      base::Time::Max())->Count(
+      base::Time(), base::Time::Max())
+      ->CountAndDestroySelfWhenFinished(
           base::Bind(&StorageHandler::OnGetBrowsingDataSize,
                      base::Unretained(this), false));
 
