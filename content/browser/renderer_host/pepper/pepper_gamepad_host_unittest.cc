3130eaa0752137a2a22413bfbab4ade8c65c801c
aelias <aelias@chromium.org>
2017-01-04 15:13:36 -0800

    Delete WebGamepads::length.
    This field has only one use, the "i < gamepads.length" check in
    NavigatorGamepad.cpp.  This check doesn't do anything useful and
    actually introduces the following bug: if two gamepads are connected and
    then the first one is disconnected, the second one remained at index 1
    and was therefore dropped.  (There's no identifier for a gamepad other
    than its index, so this bug can't be fixed by moving it to the beginning
    of the WebGamepads structure.)
    
    So I deleted the dynamic length and rely purely on itemsLengthCap
    instead.
    
    As a drive-by, add an initialization of displayId to 0 (was unset).
    
    (This patch is an improved version of http://crrev.com/2457703002/)
    
    BUG=648015
    
    Review-Url: https://codereview.chromium.org/2572323002
    Cr-Commit-Position: refs/heads/master@{#441503}

--- content/browser/renderer_host/pepper/pepper_gamepad_host_unittest.cc
+++ content/browser/renderer_host/pepper/pepper_gamepad_host_unittest.cc
@@ -77,8 +77,6 @@ TEST_F(PepperGamepadHostTest, ValidateGamepadsMatch) {
                 "gamepads data must match");
   ppapi::WebKitGamepads ppapi_gamepads;
   blink::WebGamepads web_gamepads;
-  EXPECT_EQ(AddressDiff(&web_gamepads.length, &web_gamepads),
-            AddressDiff(&ppapi_gamepads.length, &ppapi_gamepads));
 
   // See comment below on storage & the EXPECT macro.
   size_t webkit_items_length_cap = blink::WebGamepads::itemsLengthCap;
@@ -138,7 +136,6 @@ TEST_F(PepperGamepadHostTest, ValidateGamepadMatch) {
 TEST_F(PepperGamepadHostTest, MAYBE_WaitForReply) {
   blink::WebGamepads default_data;
   memset(&default_data, 0, sizeof(blink::WebGamepads));
-  default_data.length = 1;
   default_data.items[0].connected = true;
   default_data.items[0].buttonsLength = 1;
   ConstructService(default_data);
@@ -186,7 +183,6 @@ TEST_F(PepperGamepadHostTest, MAYBE_WaitForReply) {
   const ppapi::ContentGamepadHardwareBuffer* buffer =
       static_cast<const ppapi::ContentGamepadHardwareBuffer*>(
           shared_memory.memory());
-  EXPECT_EQ(button_down_data.length, buffer->buffer.length);
   EXPECT_EQ(button_down_data.items[0].buttonsLength,
             buffer->buffer.items[0].buttons_length);
   for (size_t i = 0; i < ppapi::WebKitGamepad::kButtonsLengthCap; i++) {
--- third_party/WebKit/public/platform/Platform.h
+++ third_party/WebKit/public/platform/Platform.h
@@ -273,7 +273,7 @@ class BLINK_PLATFORM_EXPORT Platform {
 
   // Gamepad -------------------------------------------------------------
 
-  virtual void sampleGamepads(WebGamepads& into) { into.length = 0; }
+  virtual void sampleGamepads(WebGamepads& into) {}
 
   // History -------------------------------------------------------------
 
