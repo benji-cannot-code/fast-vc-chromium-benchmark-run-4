292f62f055dc3d05f8001f1b13b1a180fd735999
vitaliii <vitaliii@chromium.org>
2017-01-03 23:57:11 -0800

    [NTP::SectionOrder] Preserve added categories on ClearHistory.
    Before this CL all categories, which were added using
    AppendCategoryIfNecessary, were removed on ClearHistory. Since it is not
    required to reregister them, there were simply not shown at all. After
    this CL they are preserved (sorted by id).
    
    BUG=675953
    
    Review-Url: https://codereview.chromium.org/2605353004
    Cr-Commit-Position: refs/heads/master@{#441336}

--- components/ntp_snippets/content_suggestions_service.cc
+++ components/ntp_snippets/content_suggestions_service.cc
@@ -131,6 +131,7 @@ void ContentSuggestionsService::ClearHistory(
   for (const auto& provider : providers_) {
     provider->ClearHistory(begin, end, filter);
   }
+  category_ranker_->ClearHistory(begin, end);
 }
 
 void ContentSuggestionsService::ClearAllCachedSuggestions() {
