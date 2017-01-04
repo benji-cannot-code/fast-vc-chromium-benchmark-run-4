a5e937a4e41e0388e9a67bbfede513b70e4e73ba
qinmin <qinmin@chromium.org>
2017-01-03 16:27:23 -0800

    Fix a crash when downloading a dangerous download
    When downloading a dangerous download, Chrome should show an infobar in current tab.
    However, the java side only checks the filename for dangerous download.
    As a result, in some cases it may close a blank tab even if the download is dangerous.
    That causes the download to be removed and cause a crash when returning to the caller.
    
    BUG=667215
    
    Review-Url: https://codereview.chromium.org/2609113004
    Cr-Commit-Position: refs/heads/master@{#441266}

--- chrome/android/java/src/org/chromium/chrome/browser/download/ChromeDownloadDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/ChromeDownloadDelegate.java
@@ -319,10 +319,8 @@ public class ChromeDownloadDelegate {
      */
     @CalledByNative
     private void onDownloadStarted(String filename) {
-        if (!isDangerousFile(filename)) {
-            DownloadUtils.showDownloadStartToast(mContext);
-            closeBlankTab();
-        }
+        DownloadUtils.showDownloadStartToast(mContext);
+        closeBlankTab();
     }
 
     /**
@@ -376,16 +374,6 @@ public class ChromeDownloadDelegate {
             if (index > 0) return filename.substring(index + 1);
         }
         return MimeTypeMap.getFileExtensionFromUrl(url);
-    }
-
-    /**
-     * Check whether a file is dangerous.
-     *
-     * @param filename Name of the file.
-     * @return true if the file is dangerous, or false otherwise.
-     */
-    protected boolean isDangerousFile(String filename) {
-        return nativeIsDownloadDangerous(filename);
     }
 
     /**
@@ -476,7 +464,6 @@ public class ChromeDownloadDelegate {
 
     private native void nativeInit(WebContents webContents);
     private static native String nativeGetDownloadWarningText(String filename);
-    private static native boolean nativeIsDownloadDangerous(String filename);
     private static native void nativeLaunchDuplicateDownloadInfoBar(ChromeDownloadDelegate delegate,
             Tab tab, DownloadInfo downloadInfo, String filePath, boolean isIncognito);
     private static native void nativeLaunchPermissionUpdateInfoBar(
