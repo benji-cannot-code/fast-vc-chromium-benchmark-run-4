7475de12d81a3ddd69c660bf60b0499e911f8e64
aboxhall <aboxhall@chromium.org>
2017-01-03 18:35:44 -0800

    Don't sync DOM selection for root a11y node
    BUG=677885
    
    Review-Url: https://codereview.chromium.org/2614553002
    Cr-Commit-Position: refs/heads/master@{#441299}

--- third_party/WebKit/Source/devtools/front_end/accessibility/AXTreePane.js
+++ third_party/WebKit/Source/devtools/front_end/accessibility/AXTreePane.js
@@ -6,9 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @unrestricted
  */
 Accessibility.AXTreePane = class extends Accessibility.AccessibilitySubPane {
-  constructor() {
+  /**
+   * @param {!Accessibility.AccessibilitySidebarView} axSidebarView
+   */
+  constructor(axSidebarView) {
     super(Common.UIString('Accessibility Tree'));
 
+    this._axSidebarView = axSidebarView;
     this._treeOutline = this.createTreeOutline();
 
     this.element.classList.add('accessibility-computed');
@@ -78,6 +82,21 @@ Accessibility.AXTreePane = class extends Accessibility.AccessibilitySubPane {
   }
 
   /**
+   * @param {!Accessibility.AccessibilityNode} axNode
+   */
+  setSelectedNode(axNode) {
+    if (axNode.parentNode()) {
+      Common.Revealer.reveal(axNode.deferredDOMNode());
+    } else {
+      // Only set the node for the accessibility panel, not the Elements tree.
+      var axSidebarView = this._axSidebarView;
+      axNode.deferredDOMNode().resolve((node) => {
+        axSidebarView.setNode(node);
+      });
+    }
+  }
+
+  /**
    * @param {boolean} selectedByUser
    */
   setSelectedByUser(selectedByUser) {
@@ -138,7 +157,7 @@ Accessibility.InspectNodeButton = class {
    */
   _handleMouseDown(event) {
     this._treePane.setSelectedByUser(true);
-    Common.Revealer.reveal(this._axNode.deferredDOMNode());
+    this._treePane.setSelectedNode(this._axNode);
   }
 };
 
@@ -206,7 +225,7 @@ Accessibility.AXNodeTreeElement = class extends UI.TreeElement {
 
   inspectDOMNode() {
     this._treePane.setSelectedByUser(true);
-    Common.Revealer.reveal(this._axNode.deferredDOMNode());
+    this._treePane.setSelectedNode(this._axNode);
   }
 
   /**
--- third_party/WebKit/Source/devtools/front_end/accessibility/AccessibilitySidebarView.js
+++ third_party/WebKit/Source/devtools/front_end/accessibility/AccessibilitySidebarView.js
@@ -11,7 +11,7 @@ Accessibility.AccessibilitySidebarView = class extends UI.ThrottledWidget {
     this._node = null;
     this._axNode = null;
     this._sidebarPaneStack = UI.viewManager.createStackLocation();
-    this._treeSubPane = new Accessibility.AXTreePane();
+    this._treeSubPane = new Accessibility.AXTreePane(this);
     this._sidebarPaneStack.showView(this._treeSubPane);
     this._ariaSubPane = new Accessibility.ARIAAttributesPane();
     this._sidebarPaneStack.showView(this._ariaSubPane);
@@ -27,6 +27,14 @@ Accessibility.AccessibilitySidebarView = class extends UI.ThrottledWidget {
    */
   node() {
     return this._node;
+  }
+
+  /**
+   * @param {?SDK.DOMNode} node
+   */
+  setNode(node) {
+    this._node = node;
+    this.update();
   }
 
   /**
@@ -99,8 +107,7 @@ Accessibility.AccessibilitySidebarView = class extends UI.ThrottledWidget {
   }
 
   _pullNode() {
-    this._node = UI.context.flavor(SDK.DOMNode);
-    this.update();
+    this.setNode(UI.context.flavor(SDK.DOMNode));
   }
 
   /**
