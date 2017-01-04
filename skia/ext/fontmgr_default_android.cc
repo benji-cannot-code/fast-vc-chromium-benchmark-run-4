4595b8e6607b374aca976f56805fa540d0165d21
jbudorick <jbudorick@chromium.org>
2017-01-04 09:52:48 -0800

    [Android] Stop using skia globals for font overriding in layout tests.
    The layout tests have been using globals in skia to provide custom
    font files. Skia would like to get rid of those globals. This change
    adds a chromium-side SkFontMgr::Factory for the layout tests to use
    instead.
    
    BUG=chromium:567947,skia:2817
    
    Review-Url: https://codereview.chromium.org/2599933002
    Cr-Commit-Position: refs/heads/master@{#441404}

---
+++ skia/ext/fontmgr_default_android.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "skia/ext/fontmgr_default_android.h"
+
+#include "third_party/skia/include/ports/SkFontMgr.h"
+#include "third_party/skia/include/ports/SkFontMgr_android.h"
+
+namespace {
+SkFontMgr* g_default_fontmgr;
+}  // namespace
+
+SK_API void SetDefaultSkiaFactory(SkFontMgr* fontmgr) {
+  g_default_fontmgr = fontmgr;
+}
+
+SK_API SkFontMgr* SkFontMgr::Factory() {
+  return g_default_fontmgr ? SkRef(g_default_fontmgr)
+                           : SkFontMgr_New_Android(nullptr);
+}
