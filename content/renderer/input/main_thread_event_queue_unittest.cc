601469535ebe1e0974d761be72b45cadb5349dfd
dtapuska <dtapuska@chromium.org>
2017-01-04 14:00:09 -0800

    Remove the requirement to only rAF align non-blocking touch moves.
    We want to align all touchmoves regardless of whether they cause
    a regression in scroll start. The expected regression would on average
    half a frame.
    
    BUG=642368
    
    Review-Url: https://codereview.chromium.org/2612823002
    Cr-Commit-Position: refs/heads/master@{#441478}

--- content/renderer/input/main_thread_event_queue_unittest.cc
+++ content/renderer/input/main_thread_event_queue_unittest.cc
@@ -267,7 +267,7 @@ TEST_P(MainThreadEventQueueTest, BlockingTouch) {
   HandleEvent(kEvents[3], INPUT_EVENT_ACK_STATE_SET_NON_BLOCKING);
   EXPECT_EQ(1u, event_queue().size());
   RunPendingTasksWithSimulatedRaf();
-  histogram_tester.ExpectUniqueSample(kCoalescedCountHistogram, 2, 1);
+  histogram_tester.ExpectUniqueSample(kCoalescedCountHistogram, 2, 2);
 }
 
 TEST_P(MainThreadEventQueueTest, InterleavedEvents) {
@@ -428,7 +428,7 @@ TEST_P(MainThreadEventQueueTest, RafAlignedTouchInput) {
 
   EXPECT_EQ(3u, event_queue().size());
   EXPECT_TRUE(main_task_runner_->HasPendingTask());
-  EXPECT_FALSE(needs_main_frame_);
+  EXPECT_TRUE(needs_main_frame_);
   main_task_runner_->RunUntilIdle();
 }
 
@@ -456,7 +456,7 @@ TEST_P(MainThreadEventQueueTest, RafAlignedTouchInputCoalescedMoves) {
   EXPECT_TRUE(needs_main_frame_);
   HandleEvent(kEvents[1], INPUT_EVENT_ACK_STATE_NOT_CONSUMED);
   EXPECT_EQ(1u, event_queue().size());
-  EXPECT_TRUE(main_task_runner_->HasPendingTask());
+  EXPECT_FALSE(main_task_runner_->HasPendingTask());
   EXPECT_TRUE(needs_main_frame_);
   RunPendingTasksWithSimulatedRaf();
   EXPECT_EQ(0u, event_queue().size());
