6e2b5105602fcc87bf2ebebba592988ca713885b
mfomitchev <mfomitchev@chromium.org>
2017-01-04 14:30:38 -0800

    Mustash: Ensure surfaces submitted to Mus by WM and embedders contain Surfaces with embeded content.
    - Use SurfaceLayers in embedders and Window Manager to properly position the
      embedded content in the layer tree.
    
    - WM no longer uses underlay surfaces for window decorations. Instead they are
      submitted to Mus as part of the WM's surface.
    
    - FrameGenerator now only creates SurfaceDrawQuad for the top-level window
      rather than recursively creating SurfaceDrawQuad for all windows in the
      window tree, since all surfaces are now part of the tree rooted at the
      surface of the top-level window.
    
    BUG=672943,669964
    
    Review-Url: https://codereview.chromium.org/2580063002
    Cr-Commit-Position: refs/heads/master@{#441492}

--- services/ui/ws/platform_display_default.cc
+++ services/ui/ws/platform_display_default.cc
@@ -249,6 +249,10 @@ void PlatformDisplayDefault::OnAcceleratedWidgetDestroyed() {
 
 void PlatformDisplayDefault::OnActivationChanged(bool active) {}
 
+ServerWindow* PlatformDisplayDefault::GetActiveRootWindow() {
+  return delegate_->GetActiveRootWindow();
+}
+
 bool PlatformDisplayDefault::IsInHighContrastMode() {
   return delegate_ ? delegate_->IsInHighContrastMode() : false;
 }
