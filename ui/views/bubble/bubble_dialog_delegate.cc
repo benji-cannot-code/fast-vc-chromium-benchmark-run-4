4dd27354b5fb03d97f3b5ee0921fa9d877d3f969
estade <estade@chromium.org>
2017-01-04 08:44:06 -0800

    Apply new WM shadows to app list.
    Also clean up/streamline AppListView.
    1. Remove code that supported arrows + view anchoring
    2. Remove code that supported !CrOS
    3. Remove dead code for making bubble borders accept events
    4. Remove AppListViewObserver which is dead and who knows what it was used for.
    5. Stop building //ui/app_list on all aura (just build on CrOS)
    
    BUG=608852
    TBR=derat@chromium.org
    
    Review-Url: https://codereview.chromium.org/2588103004
    Cr-Commit-Position: refs/heads/master@{#441394}

--- ui/views/bubble/bubble_dialog_delegate.cc
+++ ui/views/bubble/bubble_dialog_delegate.cc
@@ -218,7 +218,6 @@ BubbleDialogDelegateView::BubbleDialogDelegateView(View* anchor_view,
                kPanelVertMargin,
                kPanelHorizMargin),
       accept_events_(true),
-      border_accepts_events_(true),
       adjust_if_offscreen_(true),
       parent_window_(NULL) {
   if (anchor_view)
