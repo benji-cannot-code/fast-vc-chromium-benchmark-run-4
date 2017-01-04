18458ccb4d846f8b1fd16b5e653b7f2546c0e46d
xunjieli <xunjieli@chromium.org>
2017-01-04 14:17:42 -0800

    Instrument SdchManager using MemoryDumpProvider
    This CL instruments SdchManager using MemoryDumpProvider.
    Even though SDCH might be unshipped (see blink-dev),
    it is useful to get an idea of how much memory dictionaries
    cost.
    
    BUG=669108
    
    Review-Url: https://codereview.chromium.org/2541073003
    Cr-Commit-Position: refs/heads/master@{#441485}

--- net/url_request/url_request_context.cc
+++ net/url_request/url_request_context.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/memory_allocator_dump.h"
 #include "base/trace_event/memory_dump_manager.h"
 #include "base/trace_event/process_memory_dump.h"
+#include "net/base/sdch_manager.h"
 #include "net/cookies/cookie_store.h"
 #include "net/dns/host_resolver.h"
 #include "net/http/http_transaction_factory.h"
@@ -139,6 +140,8 @@ bool URLRequestContext::OnMemoryDump(
       network_session->DumpMemoryStats(pmd, dump->absolute_name());
   }
   SSLClientSocketImpl::DumpSSLClientSessionMemoryStats(pmd);
+  if (sdch_manager_)
+    sdch_manager_->DumpMemoryStats(pmd, dump->absolute_name());
   return true;
 }
 
