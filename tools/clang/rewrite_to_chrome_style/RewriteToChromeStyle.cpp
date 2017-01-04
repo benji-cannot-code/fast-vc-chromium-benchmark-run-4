ac30a931331e9e7123a83d40a9ee42e818f65fcc
nasko <nasko@chromium.org>
2017-01-04 11:17:51 -0800

    Update methods requiring Get prefix to avoid collisions with Blink rewrite.
    This CL adds a few more methods that cause name collisions when Blink
    is rewritten to match Chromium style. In addition, it removes the hash
    method from the blacklist.
    
    BUG=582312, 677166
    
    Review-Url: https://codereview.chromium.org/2608423003
    Cr-Commit-Position: refs/heads/master@{#441429}

--- tools/clang/rewrite_to_chrome_style/RewriteToChromeStyle.cpp
+++ tools/clang/rewrite_to_chrome_style/RewriteToChromeStyle.cpp
@@ -215,9 +215,7 @@ bool IsBlacklistedFunctionName(llvm::StringRef name) {
   if (name.find('_') != llvm::StringRef::npos)
     return true;
 
-  // https://crbug.com/677166: Have to avoid renaming |hash| -> |Hash| to avoid
-  // colliding with a struct already named |Hash|.
-  return name == "hash";
+  return false;
 }
 
 bool IsBlacklistedFreeFunctionName(llvm::StringRef name) {
@@ -359,8 +357,8 @@ AST_MATCHER_P(clang::QualType, hasString, std::string, ExpectedString) {
 }
 
 bool ShouldPrefixFunctionName(const std::string& old_method_name) {
-  // Methods that are named similarily to a type - they should be prefixed
-  // with a "get" prefix.
+  // Functions that are named similarily to a type - they should be prefixed
+  // with a "Get" prefix.
   static const char* kConflictingMethods[] = {
       "animationWorklet",
       "audioWorklet",
@@ -379,6 +377,9 @@ bool ShouldPrefixFunctionName(const std::string& old_method_name) {
       "font",
       "frame",
       "frameBlameContext",
+      "frontend",
+      "hash",
+      "heapObjectHeader",
       "iconURL",
       "inputMethodController",
       "inputType",
@@ -388,8 +389,12 @@ bool ShouldPrefixFunctionName(const std::string& old_method_name) {
       "layoutSize",
       "length",
       "lineCap",
+      "lineEndings",
       "lineJoin",
+      "listItems",
       "matchedProperties",
+      "midpointState",
+      "mouseEvent",
       "name",
       "navigationType",
       "node",
@@ -399,6 +404,8 @@ bool ShouldPrefixFunctionName(const std::string& old_method_name) {
       "path",
       "processingInstruction",
       "readyState",
+      "relList",
+      "resource",
       "response",
       "sandboxSupport",
       "screenInfo",
@@ -407,13 +414,18 @@ bool ShouldPrefixFunctionName(const std::string& old_method_name) {
       "signalingState",
       "state",
       "string",
+      "styleSheet",
       "text",
       "textAlign",
       "textBaseline",
       "theme",
+      "thread",
       "timing",
       "topLevelBlameContext",
+      "vector",
       "widget",
+      "wordBoundaries",
+      "wrapperTypeInfo",
   };
   for (const auto& conflicting_method : kConflictingMethods) {
     if (old_method_name == conflicting_method)
