ffacf0a5fb5543fc7ea48374ca75539436758ec8
tedchoc <tedchoc@chromium.org>
2017-01-04 10:48:53 -0800

    Fix AccountManagerHelper assert access from FRE.
    FRE was made to lazily load the libraries, but it attempts
    to access the AccountManagerHelper before it has been
    fully initialized.  To avoid this, use ChromeBrowserInitializer
    to the pre native initialization early, which will call into:
    ProcessInitializationHandler#handlePreNativeInitialization, which
    does AccountManagerHelper.initializeAccountManagerHelper.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2610173002
    Cr-Commit-Position: refs/heads/master@{#441419}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunActivity.java
@@ -25,6 +25,7 @@ import org.chromium.chrome.browser.customtabs.CustomTabActivity;
 import org.chromium.chrome.browser.customtabs.CustomTabsConnection;
 import org.chromium.chrome.browser.document.ChromeLauncherActivity;
 import org.chromium.chrome.browser.init.ChromeBrowserInitializer;
+import org.chromium.chrome.browser.init.EmptyBrowserParts;
 import org.chromium.chrome.browser.metrics.UmaUtils;
 import org.chromium.chrome.browser.net.spdyproxy.DataReductionProxySettings;
 import org.chromium.chrome.browser.preferences.datareduction.DataReductionPromoUtils;
@@ -173,6 +174,8 @@ public class FirstRunActivity extends AppCompatActivity implements FirstRunPageD
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ChromeBrowserInitializer.getInstance(this).handlePreNativeStartup(new EmptyBrowserParts());
 
         if (savedInstanceState != null) {
             mFreProperties = savedInstanceState;
@@ -514,7 +517,8 @@ public class FirstRunActivity extends AppCompatActivity implements FirstRunPageD
     private void ensureBrowserProcessInitialized() {
         if (mNativeSideIsInitialized) return;
         try {
-            ChromeBrowserInitializer.getInstance(this).handleSynchronousStartup();
+            ChromeBrowserInitializer.getInstance(this).handlePostNativeStartup(
+                    false, new EmptyBrowserParts());
             mNativeSideIsInitialized = true;
         } catch (ProcessInitException e) {
             Log.e(TAG, "Unable to load native library.", e);
