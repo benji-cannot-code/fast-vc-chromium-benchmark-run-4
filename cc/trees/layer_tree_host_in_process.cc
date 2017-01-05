ecaa2f680bba9047a643d51361afb2c4652ae7ac
ajuma <ajuma@chromium.org>
2017-01-05 13:01:49 -0800

    cc: Remove map lookup from RenderSurfaceImpl::EffectTreeIndex
    This relands http://crrev.com/2540333002 with a fix for the crashes
    that caused that CL to get reverted.
    
    The previous CL set the effect tree index on surfaces during the surface
    update step (when EffectNode::render_surface gets updated). This is not
    sufficient, however, since the surface update step will be skipped when
    we can't draw, even though property trees can still change and surfaces
    can still get destroyed (because of their owning layer getting destroyed).
    This CL also updates the effect tree index on surfaces when new property
    trees are set (that is, at commit time and at activation time).
    
    BUG=670074
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2571263002
    Cr-Commit-Position: refs/heads/master@{#441747}

--- cc/trees/layer_tree_host_in_process.cc
+++ cc/trees/layer_tree_host_in_process.cc
@@ -601,8 +601,10 @@ static int GetLayersUpdateTimeHistogramBucket(size_t numLayers) {
 }
 
 bool LayerTreeHostInProcess::UpdateLayers() {
-  if (!layer_tree_->root_layer())
+  if (!layer_tree_->root_layer()) {
+    layer_tree_->property_trees()->clear();
     return false;
+  }
   DCHECK(!layer_tree_->root_layer()->parent());
   base::ElapsedTimer timer;
 
--- cc/trees/property_tree_builder.cc
+++ cc/trees/property_tree_builder.cc
@@ -1377,8 +1377,6 @@ void BuildPropertyTreesTopLevelInternal(
     return;
   }
 
-  property_trees->sequence_number++;
-
   DataForRecursion<LayerType> data_for_recursion;
   data_for_recursion.property_trees = property_trees;
   data_for_recursion.transform_tree_parent = nullptr;
