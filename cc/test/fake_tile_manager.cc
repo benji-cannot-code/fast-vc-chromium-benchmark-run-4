78608ae57ebc1eef26327f482e1b8bc7abf001e3
vmpstr <vmpstr@chromium.org>
2017-01-04 17:46:53 -0800

    cc: Add a flag to check for tile priority inversion in AssignGpuMemory.
    This patch adds a flag and code to ensure that after all of the tiles
    have been processed in the tile manager, whatever remains in the queue
    is not in NOW bin and not required for either draw or activation.
    
    The flag is --check-tile-priority-inversion, which could be used to
    debug various activation issues.
    
    R=danakj@chromium.org, sunnyps@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2612123002
    Cr-Commit-Position: refs/heads/master@{#441551}

--- cc/test/fake_tile_manager.cc
+++ cc/test/fake_tile_manager.cc
@@ -35,7 +35,8 @@ FakeTileManager::FakeTileManager(TileManagerClient* client)
     : TileManager(client,
                   base::ThreadTaskRunnerHandle::Get().get(),
                   std::numeric_limits<size_t>::max(),
-                  false /* use_partial_raster */),
+                  false /* use_partial_raster */,
+                  false /* check_tile_priority_inversion */),
       image_decode_cache_(
           ResourceFormat::RGBA_8888,
           LayerTreeSettings().software_decoded_image_budget_bytes) {
@@ -51,7 +52,8 @@ FakeTileManager::FakeTileManager(TileManagerClient* client,
     : TileManager(client,
                   base::ThreadTaskRunnerHandle::Get().get(),
                   std::numeric_limits<size_t>::max(),
-                  false /* use_partial_raster */),
+                  false /* use_partial_raster */,
+                  false /* check_tile_priority_inversion */),
       image_decode_cache_(
           ResourceFormat::RGBA_8888,
           LayerTreeSettings().software_decoded_image_budget_bytes) {
