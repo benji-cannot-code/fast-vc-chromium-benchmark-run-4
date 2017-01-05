22e93fe9e19c8f9094c6d6c03893525905d689a5
bzanotti <bzanotti@chromium.org>
2017-01-05 02:50:49 -0800

    Fix username absent right after signing in in main Settings.
    BUG=676237
    
    Review-Url: https://codereview.chromium.org/2596763002
    Cr-Commit-Position: refs/heads/master@{#441621}

--- ios/chrome/browser/ui/settings/settings_collection_view_controller.mm
+++ ios/chrome/browser/ui/settings/settings_collection_view_controller.mm
@@ -908,10 +908,10 @@ void SigninObserverBridge::GoogleSignedOut(const std::string& account_id,
     return;
   }
   identityAccountItem.image = [self userAccountImage];
+  identityAccountItem.text = [_identity userFullName];
 
   SyncSetupService* syncSetupService =
       SyncSetupServiceFactory::GetForBrowserState(_mainBrowserState);
-
   if (!syncSetupService->HasFinishedInitialSetup()) {
     identityAccountItem.detailText =
         l10n_util::GetNSString(IDS_IOS_SYNC_SETUP_IN_PROGRESS);
@@ -921,7 +921,6 @@ void SigninObserverBridge::GoogleSignedOut(const std::string& account_id,
   identityAccountItem.shouldDisplayError =
       !ios_internal::sync::IsTransientSyncError(
           syncSetupService->GetSyncServiceState());
-  identityAccountItem.text = [_identity userFullName];
   if (identityAccountItem.shouldDisplayError) {
     identityAccountItem.detailText =
         ios_internal::sync::GetSyncErrorDescriptionForBrowserState(
