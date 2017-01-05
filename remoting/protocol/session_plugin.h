7aaba0974ffac50f4b244d184d56a1e15a9d22f9
kjellander <kjellander@chromium.org>
2017-01-05 00:18:21 -0800

    Update xmllite reference to new source location
    In https://codereview.chromium.org/2443903004/ all of
    third_party/webrtc/libjingle was replaced with a new imported
    copy in third_party/libjingle_xmpp.
    Unfortunately https://codereview.chromium.org/2586403003
    was submitted in the meantime, before the code was deleted
    in the WebRTC repo and rolled into Chromium.
    
    This CL updates the last references to the old copy of the code,
    which should enable deleting it from WebRTC.
    
    BUG=webrtc:5539
    TBR=sergeyu@chromium.org
    
    Review-Url: https://codereview.chromium.org/2616873002
    Cr-Commit-Position: refs/heads/master@{#441609}

--- remoting/protocol/session_plugin.h
+++ remoting/protocol/session_plugin.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "remoting/protocol/jingle_messages.h"
 #include "remoting/protocol/session.h"
-#include "third_party/webrtc/libjingle/xmllite/xmlelement.h"
+#include "third_party/libjingle_xmpp/xmllite/xmlelement.h"
 
 namespace remoting {
 namespace protocol {
