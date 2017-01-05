68da6e48899741f02efa9b2185178c1a34358a7d
rsesek <rsesek@chromium.org>
2017-01-05 07:18:01 -0800

    Fix ExceptionProcessorTest.ThrowAndCatchExceptionInRunLoop on official builds.
    Official builds are stripped, so unw_get_proc_name() does not succeed. If that
    is the case, test if the exception handler is in the same image as the exception
    preprocessor. If it is (i.e., it's part of the Chromium Framework), then let the
    handler run.
    
    BUG=675808
    R=mark@chromium.org
    
    Review-Url: https://codereview.chromium.org/2614863002
    Cr-Commit-Position: refs/heads/master@{#441656}

--- chrome/browser/mac/exception_processor.mm
+++ chrome/browser/mac/exception_processor.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "chrome/browser/mac/exception_processor.h"
 
+#include <dlfcn.h>
 #import <Foundation/Foundation.h>
 #include <libunwind.h>
 #include <objc/objc-exception.h>
@@ -130,6 +131,14 @@ static id ObjcExceptionPreprocessor(id exception) {
   unw_cursor_t cursor;
   unw_init_local(&cursor, &context);
 
+  // Get the base address for the image that contains this function.
+  Dl_info dl_info;
+  const void* this_base_address = 0;
+  if (dladdr(reinterpret_cast<const void*>(&ObjcExceptionPreprocessor),
+             &dl_info) != 0) {
+    this_base_address = dl_info.dli_fbase;
+  }
+
   while (unw_step(&cursor) > 0) {
     unw_proc_info_t frame_info;
     if (unw_get_proc_info(&cursor, &frame_info) != UNW_ESUCCESS) {
@@ -142,6 +151,17 @@ static id ObjcExceptionPreprocessor(id exception) {
       unw_word_t offset;
       if (unw_get_proc_name(&cursor, proc_name, sizeof(proc_name),
                             &offset) != UNW_ESUCCESS) {
+        // The symbol has no name, so see if it belongs to the same image as
+        // this function.
+        if (dladdr(reinterpret_cast<const void*>(frame_info.start_ip),
+                   &dl_info) != 0) {
+          if (dl_info.dli_fbase == this_base_address) {
+            // This is a handler in our image, so allow it to run.
+            break;
+          }
+        }
+
+        // This handler does not belong to us, so continue the search.
         continue;
       }
 
