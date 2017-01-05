169981bcacda39b5308c398e355eef1c35072b71
fmalita <fmalita@chromium.org>
2017-01-05 13:13:56 -0800

    Delete Mac & Cairo skia::BitmapPlatformDevice impls
    We are no longer performing native drawing on these platforms, use the
    Skia placeholder instead.
    
    BUG=675977
    R=reed@google.com
    TBR=
    
    Review-Url: https://codereview.chromium.org/2611153002
    Cr-Commit-Position: refs/heads/master@{#441750}

--- ui/native_theme/native_theme_win.cc
+++ ui/native_theme/native_theme_win.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/scoped_select_object.h"
 #include "base/win/win_util.h"
 #include "base/win/windows_version.h"
-#include "skia/ext/bitmap_platform_device.h"
 #include "skia/ext/platform_canvas.h"
 #include "skia/ext/skia_utils_win.h"
 #include "third_party/skia/include/core/SkCanvas.h"
