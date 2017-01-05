3ed4409cc3d7deccdd6bcbf5d7aaa1bbfdff6b5c
aazzam <aazzam@google.com>
2017-01-04 22:21:14 -0800

    Implemented CSSPropertyAPI for translate property.
    Added CSSPropertyAPITranslate which implements CSSPropertyAPI for
    the translate property. Parsing logic is the same as before,
    just was moved from CSSPropertyParser to the .cpp file. Added flag
    to CSSProperties.in so that the .h file will be generated.
    
    BUG=668012
    
    Review-Url: https://codereview.chromium.org/2614743003
    Cr-Commit-Position: refs/heads/master@{#441596}

---
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyAPITranslate.cpp
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/properties/CSSPropertyAPITranslate.h"
+
+#include "core/css/CSSValueList.h"
+#include "core/css/parser/CSSParserTokenRange.h"
+#include "core/css/parser/CSSPropertyParser.h"
+#include "core/css/parser/CSSPropertyParserHelpers.h"
+
+namespace blink {
+
+const CSSValue* CSSPropertyAPITranslate::parseSingleValue(
+    CSSParserTokenRange& range,
+    const CSSParserContext& context) {
+  DCHECK(RuntimeEnabledFeatures::cssIndependentTransformPropertiesEnabled());
+  CSSValue* translate = CSSPropertyParserHelpers::consumeLengthOrPercent(
+      range, context.mode(), ValueRangeAll);
+  if (!translate)
+    return nullptr;
+  CSSValueList* list = CSSValueList::createSpaceSeparated();
+  list->append(*translate);
+  translate = CSSPropertyParserHelpers::consumeLengthOrPercent(
+      range, context.mode(), ValueRangeAll);
+  if (translate) {
+    list->append(*translate);
+    translate = CSSPropertyParserHelpers::consumeLength(range, context.mode(),
+                                                        ValueRangeAll);
+    if (translate)
+      list->append(*translate);
+  }
+
+  return list;
+}
+
+}  // namespace blink
