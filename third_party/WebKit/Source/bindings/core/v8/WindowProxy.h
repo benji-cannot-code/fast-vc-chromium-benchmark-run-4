feaf32ae74a12265586698724562284687ba9e74
haraken <haraken@chromium.org>
2017-01-05 06:03:52 -0800

    Remove the declaration of WindowProxy::updateDocumentWrapper
    The method is not defined.
    
    BUG=677253
    
    Review-Url: https://codereview.chromium.org/2612953003
    Cr-Commit-Position: refs/heads/master@{#441646}

--- third_party/WebKit/Source/bindings/core/v8/WindowProxy.h
+++ third_party/WebKit/Source/bindings/core/v8/WindowProxy.h
@@ -79,7 +79,6 @@ class WindowProxy final : public GarbageCollectedFinalized<WindowProxy> {
   bool isGlobalInitialized() { return !m_globalProxy.isEmpty(); }
 
   void initializeIfNeeded();
-  void updateDocumentWrapper(v8::Local<v8::Object> wrapper);
 
   void clearForNavigation();
   void clearForClose();
