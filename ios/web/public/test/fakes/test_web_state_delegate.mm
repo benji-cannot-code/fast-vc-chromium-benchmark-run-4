75795e141b142f2d5915e24e5d5ffdd6677dfaef
eugenebut <eugenebut@chromium.org>
2017-01-06 10:42:40 -0800

    [ios] Made TestWebStateDelegate and TestJavaScriptDialogPresenter reusable.
    These fakes will be resued in CRWWebControllerPageDialogOpenPolicyTest
    instead of CRWWebUserInterfaceDelegate mock.
    
    BUG=661445
    TRR=kkhorimoto@chromium.org
    
    Review-Url: https://codereview.chromium.org/2615093004
    Cr-Commit-Position: refs/heads/master@{#441991}

---
+++ ios/web/public/test/fakes/test_web_state_delegate.mm
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/web/public/test/fakes/test_web_state_delegate.h"
+
+namespace web {
+
+JavaScriptDialogPresenter* TestWebStateDelegate::GetJavaScriptDialogPresenter(
+    WebState*) {
+  get_java_script_dialog_presenter_called_ = true;
+  return &java_script_dialog_presenter_;
+}
+
+void TestWebStateDelegate::LoadProgressChanged(WebState*, double) {
+  load_progress_changed_called_ = true;
+}
+
+bool TestWebStateDelegate::HandleContextMenu(WebState*,
+                                             const ContextMenuParams&) {
+  handle_context_menu_called_ = true;
+  return NO;
+}
+
+TestJavaScriptDialogPresenter*
+TestWebStateDelegate::GetTestJavaScriptDialogPresenter() {
+  return &java_script_dialog_presenter_;
+}
+
+}  // namespace web
