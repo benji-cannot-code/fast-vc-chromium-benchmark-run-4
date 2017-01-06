34872c8459abdf16c21d58a128890feeb6fb6fc9
skau <skau@chromium.org>
2017-01-05 17:57:02 -0800

    Let the incognito window print.
    Provide the original profile to incognito so that users can still use
    their printers.
    
    BUG=678796
    
    Review-Url: https://codereview.chromium.org/2610373003
    Cr-Commit-Position: refs/heads/master@{#441812}

--- chrome/browser/chromeos/printing/printer_pref_manager_factory.cc
+++ chrome/browser/chromeos/printing/printer_pref_manager_factory.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/printing/printer_pref_manager_factory.h"
 
+#include "chrome/browser/profiles/incognito_helpers.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "content/public/browser/browser_context.h"
@@ -29,6 +30,11 @@ PrinterPrefManager* PrinterPrefManagerFactory::GetForBrowserContext(
 // static
 PrinterPrefManagerFactory* PrinterPrefManagerFactory::GetInstance() {
   return g_printer_pref_manager.Pointer();
+}
+
+content::BrowserContext* PrinterPrefManagerFactory::GetBrowserContextToUse(
+    content::BrowserContext* context) const {
+  return chrome::GetBrowserContextRedirectedInIncognito(context);
 }
 
 PrinterPrefManagerFactory::PrinterPrefManagerFactory()
