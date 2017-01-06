a32d4ee45b08b0ad6c055c9ad61a0000caef8320
calamity <calamity@chromium.org>
2017-01-05 21:52:44 -0800

    [MD History] Make first URL on Synced Device page instead of menu button.
    BUG=653398
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2618633002
    Cr-Commit-Position: refs/heads/master@{#441884}

--- chrome/browser/resources/md_history/synced_device_manager.js
+++ chrome/browser/resources/md_history/synced_device_manager.js
@@ -191,7 +191,7 @@ Polymer({
           .forEach(function(row) {
             this.focusGrid_.addRow(row);
           }.bind(this));
-      this.focusGrid_.ensureRowActive();
+      this.focusGrid_.ensureRowActive(1);
     });
   },
 
