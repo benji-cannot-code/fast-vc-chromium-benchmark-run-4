4e88edc844a71715615b11590f9320832f4d9f14
khmel <khmel@chromium.org>
2017-01-05 17:31:00 -0800

    arc: Restore UMA statistics for failures in LSO flow.
    This restores UMA reporting for failures in preparing HTTP contexts
    and also adds UMA reporting for auth failures in LSO to keep balanced
    with silent auth flow. Also fixes error when error page is not shown
    in case of GAIA server returns error.
    
    BUG=b/34111604
    BUG=b/34108212
    TEST=Manually on device. Simulated error case. Made sure that UMA
         is reported and expected error page is shown
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2618603003
    Cr-Commit-Position: refs/heads/master@{#441792}

--- chrome/browser/chromeos/arc/auth/arc_manual_auth_code_fetcher.cc
+++ chrome/browser/chromeos/arc/auth/arc_manual_auth_code_fetcher.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "base/logging.h"
 #include "chrome/browser/chromeos/arc/arc_auth_context.h"
+#include "chrome/browser/chromeos/arc/arc_optin_uma.h"
 
 namespace arc {
 
@@ -41,6 +42,7 @@ void ArcManualAuthCodeFetcher::OnContextPrepared(
     net::URLRequestContextGetter* request_context_getter) {
   DCHECK(!pending_callback_.is_null());
   if (!request_context_getter) {
+    UpdateOptInCancelUMA(OptInCancelReason::NETWORK_ERROR);
     support_host_->ShowError(ArcSupportHost::Error::SIGN_IN_NETWORK_ERROR,
                              false);
     return;
@@ -52,6 +54,12 @@ void ArcManualAuthCodeFetcher::OnContextPrepared(
 void ArcManualAuthCodeFetcher::OnAuthSucceeded(const std::string& auth_code) {
   DCHECK(!pending_callback_.is_null());
   base::ResetAndReturn(&pending_callback_).Run(auth_code);
+}
+
+void ArcManualAuthCodeFetcher::OnAuthFailed() {
+  // Don't report via callback. Extension is already showing more detailed
+  // information. Update only UMA here.
+  UpdateOptInCancelUMA(OptInCancelReason::NETWORK_ERROR);
 }
 
 void ArcManualAuthCodeFetcher::OnRetryClicked() {
