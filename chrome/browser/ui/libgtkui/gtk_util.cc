0d2b6fc2d48b66babe918444f8a3e75d7bd5aab5
thomasanderson <thomasanderson@google.com>
2017-01-06 12:10:12 -0800

    Gtk3: Fix background colors
    This CL fixes the selection color on Ambiance being darker than it's
    supposed to be.
    
    The reason this was broken is the Ambiance theme adds:
    *:selected { background-color: alpha(@selected_bg_color, 0.9); }
    
    Since we were always rendering onto a black background, the selection
    color was picking up 10% black when it should have been using white
    from the GtkEntry.  The solution is simply to render parent widget
    backgrounds first.
    
    BUG=132847
    R=erg@chromium.org
    
    Review-Url: https://codereview.chromium.org/2612363004
    Cr-Commit-Position: refs/heads/master@{#442020}

--- chrome/browser/ui/libgtkui/gtk_util.cc
+++ chrome/browser/ui/libgtkui/gtk_util.cc
@@ -395,6 +395,13 @@ class PixelSurface {
   cairo_t* cairo_;
 };
 
+void RenderBackground(cairo_t* cr, GtkStyleContext* context) {
+  if (!context)
+    return;
+  RenderBackground(cr, gtk_style_context_get_parent(context));
+  gtk_render_background(context, cr, 0, 0, 1, 1);
+}
+
 SkColor GetBgColor(const char* css_selector) {
   // Backgrounds are more general than solid colors (eg. gradients),
   // but chromium requires us to boil this down to one color.  We
@@ -405,7 +412,7 @@ SkColor GetBgColor(const char* css_selector) {
   auto context = GetStyleContextFromCss(css_selector);
   RemoveBorders(context);
   PixelSurface surface;
-  gtk_render_background(context, surface.cairo(), 0, 0, 1, 1);
+  RenderBackground(surface.cairo(), context);
   return surface.GetPixelValue();
 }
 
@@ -427,6 +434,7 @@ SkColor GetBorderColor(const char* css_selector) {
 
   AddBorders(context);
   PixelSurface surface;
+  RenderBackground(surface.cairo(), context);
   gtk_render_frame(context, surface.cairo(), 0, 0, 1, 1);
   return surface.GetPixelValue();
 }
