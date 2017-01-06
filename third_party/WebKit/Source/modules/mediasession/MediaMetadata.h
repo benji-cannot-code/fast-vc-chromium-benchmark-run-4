a2eadd9d7e4175eaff10344eb1063f1dff414d8b
mlamouri <mlamouri@chromium.org>
2017-01-06 09:27:59 -0800

    Media Session: use dictionary instead of an interface for MediaImage.
    Using an interface makes the usage of MediaImage needlessly more
    complicated than needed. A dictionary should keep the feature working
    roughly the same way and improve developer's ergonomics.
    
    Spec change: https://github.com/WICG/mediasession/pull/162
    
    BUG=676995
    R=zqzhang@chromium.org
    
    Review-Url: https://codereview.chromium.org/2612003002
    Cr-Commit-Position: refs/heads/master@{#441962}

--- third_party/WebKit/Source/modules/mediasession/MediaMetadata.h
+++ third_party/WebKit/Source/modules/mediasession/MediaMetadata.h
@@ -14,10 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class ExecutionContext;
+class ExceptionState;
 class MediaImage;
 class MediaMetadataInit;
 class MediaSession;
+class ScriptState;
 
 // Implementation of MediaMetadata interface from the Media Session API.
 // The MediaMetadata object is linked to a MediaSession that owns it. When one
@@ -31,17 +32,23 @@ class MODULES_EXPORT MediaMetadata final
   DEFINE_WRAPPERTYPEINFO();
 
  public:
-  static MediaMetadata* create(ExecutionContext*, const MediaMetadataInit&);
+  static MediaMetadata* create(ScriptState*,
+                               const MediaMetadataInit&,
+                               ExceptionState&);
 
   String title() const;
   String artist() const;
   String album() const;
-  const HeapVector<Member<MediaImage>>& artwork() const;
+  Vector<v8::Local<v8::Value>> artwork(ScriptState*) const;
+
+  // Internal use only, returns a reference to m_artwork instead of a Frozen
+  // copy of a MediaImage array.
+  const HeapVector<MediaImage>& artwork() const;
 
   void setTitle(const String&);
   void setArtist(const String&);
   void setAlbum(const String&);
-  void setArtwork(const HeapVector<Member<MediaImage>>&);
+  void setArtwork(ScriptState*, const HeapVector<MediaImage>&, ExceptionState&);
 
   // Called by MediaSession to associate or de-associate itself.
   void setSession(MediaSession*);
@@ -49,7 +56,7 @@ class MODULES_EXPORT MediaMetadata final
   DECLARE_VIRTUAL_TRACE();
 
  private:
-  MediaMetadata(ExecutionContext*, const MediaMetadataInit&);
+  MediaMetadata(ScriptState*, const MediaMetadataInit&, ExceptionState&);
 
   // Called when one of the metadata fields is updated from script. It will
   // notify the session asynchronously in order to bundle multiple call in one
@@ -60,10 +67,16 @@ class MODULES_EXPORT MediaMetadata final
   // modified.
   void notifySessionTimerFired(TimerBase*);
 
+  // Make an internal copy of the MediaImage vector with some internal steps
+  // such as parsing of the src property.
+  void setArtworkInternal(ScriptState*,
+                          const HeapVector<MediaImage>&,
+                          ExceptionState&);
+
   String m_title;
   String m_artist;
   String m_album;
-  HeapVector<Member<MediaImage>> m_artwork;
+  HeapVector<MediaImage> m_artwork;
 
   Member<MediaSession> m_session;
   Timer<MediaMetadata> m_notifySessionTimer;
