5816de9a2162bcda06acf1337ab4b4f1b36d8bb7
kmackay <kmackay@chromium.org>
2017-01-05 18:19:08 -0800

    [Chromecast] Fix AudioVideoPipelineDeviceTest failures
    The patch to add support for different playback rates had an error so that
    sometimes the OnEndOfStream() callback was called before the last
    OnPushBufferComplete() callback.
    
    BUG= internal b/34093164
    TEST= cast_media_unittests
    
    Review-Url: https://codereview.chromium.org/2612353003
    Cr-Commit-Position: refs/heads/master@{#441826}

--- chromecast/media/cma/backend/alsa/audio_decoder_alsa.h
+++ chromecast/media/cma/backend/alsa/audio_decoder_alsa.h
@@ -78,6 +78,7 @@ class AudioDecoderAlsa : public MediaPipelineBackend::AudioDecoder,
   void OnBufferDecoded(uint64_t input_bytes,
                        CastAudioDecoder::Status status,
                        const scoped_refptr<DecoderBufferBase>& decoded);
+  void CheckBufferComplete();
   void PushRateShifted();
   void PushMorePcm();
   void RunEos();
