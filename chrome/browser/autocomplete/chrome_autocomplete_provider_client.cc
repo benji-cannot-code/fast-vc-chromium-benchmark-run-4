f7f199bcc095a08f885d20ff7fd38c568c59863e
cco3 <cco3@chromium.org>
2017-01-06 12:15:22 -0800

    Add Physical Web omnibox support for Android
    This adds some degree of feature parity with iOS, which also has a
    Physical Web omnibox completion and zero-suggest feature.
    
    BUG=663842
    
    Review-Url: https://codereview.chromium.org/2604243002
    Cr-Commit-Position: refs/heads/master@{#442024}

--- chrome/browser/autocomplete/chrome_autocomplete_provider_client.cc
+++ chrome/browser/autocomplete/chrome_autocomplete_provider_client.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/bitmap_fetcher/bitmap_fetcher_service.h"
 #include "chrome/browser/bitmap_fetcher/bitmap_fetcher_service_factory.h"
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/history/top_sites_factory.h"
@@ -154,7 +155,7 @@ ChromeAutocompleteProviderClient::GetKeywordExtensionsDelegate(
 
 physical_web::PhysicalWebDataSource*
 ChromeAutocompleteProviderClient::GetPhysicalWebDataSource() {
-  return nullptr;
+  return g_browser_process->GetPhysicalWebDataSource();
 }
 
 std::string ChromeAutocompleteProviderClient::GetAcceptLanguages() const {
--- components/omnibox/browser/autocomplete_classifier.cc
+++ components/omnibox/browser/autocomplete_classifier.cc
@@ -18,8 +18,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // static
 const int AutocompleteClassifier::kDefaultOmniboxProviders =
-#if !defined(OS_ANDROID) && !defined(OS_IOS)
-    // Custom search engines cannot be used on mobile..
+#if defined(OS_ANDROID) || defined(OS_IOS)
+    // The Physical Web currently is only implemented on mobile devices.
+    AutocompleteProvider::TYPE_PHYSICAL_WEB |
+#else
+    // Custom search engines cannot be used on mobile.
     AutocompleteProvider::TYPE_KEYWORD |
 #endif
 #if !defined(OS_IOS)
@@ -30,8 +33,6 @@ const int AutocompleteClassifier::kDefaultOmniboxProviders =
 #else
     // "URL from clipboard" can only be used on iOS.
     AutocompleteProvider::TYPE_CLIPBOARD_URL |
-    // Physical Web omnibox results are only implemented on iOS.
-    AutocompleteProvider::TYPE_PHYSICAL_WEB |
 #endif
     AutocompleteProvider::TYPE_BOOKMARK |
     AutocompleteProvider::TYPE_HISTORY_QUICK |
