33842e0521c3c90e59c2d8a46f6e4fe87e8c8e95
chfremer <chfremer@chromium.org>
2017-01-06 13:51:58 -0800

    [Mojo Video Capture] Simplify media::VideoCaptureDevice::Client:Buffer to a struct
    In interface media::VideoCaptureDevice::Client, change interface Buffer to a
    move-only struct. The new struct separates the concerns of providing access
    handles (for in-process as well as for inter-process transit) from access
    permissions. This allows clients to explicitly obtain in-process memory-mapped
    handles when they need it. Clients who only want to pass along the buffer or
    move it across process boundaries do not need memory-mapped handles.
    
    In interface media::VideoCaptureBufferPool, renamed
     GetBufferHandle() -> GetHandleForInProcessAccess()
     GetHandleForTransit() -> GetHandleForInterProcessTransit().
    
    Removed outdated implementation in video_capture Mojo service. We leave the
    implementation empty, until refactoring the interfaces has been completed.
    
    This CL is part of the Mojo Video Capture work. For the bigger picture,
    see [1] CL1.9.10
    
    For a peek at the follow-up CLs, please see
    https://codereview.chromium.org/2607203002/
    https://codereview.chromium.org/2602983002/
    
    BUG=584797
    TEST=
      content_unittests --gtest_filter="*Video*",
      video_capture_unittests,
      Apprtc loopback on Debug,
      Desktop Capture Example extension on Release
    
    [1] https://docs.google.com/a/chromium.org/document/d/1Qw7rw1AJy0QHXjha36jZNiEuxsxWslJ_X-zpOhijvI8/edit?usp=sharing
    
    Review-Url: https://codereview.chromium.org/2573223002
    Cr-Commit-Position: refs/heads/master@{#442066}

--- media/capture/video/video_capture_buffer_pool_impl.cc
+++ media/capture/video/video_capture_buffer_pool_impl.cc
@@ -28,8 +28,8 @@ VideoCaptureBufferPoolImpl::VideoCaptureBufferPoolImpl(
 
 VideoCaptureBufferPoolImpl::~VideoCaptureBufferPoolImpl() {}
 
-mojo::ScopedSharedBufferHandle VideoCaptureBufferPoolImpl::GetHandleForTransit(
-    int buffer_id) {
+mojo::ScopedSharedBufferHandle
+VideoCaptureBufferPoolImpl::GetHandleForInterProcessTransit(int buffer_id) {
   base::AutoLock lock(lock_);
 
   VideoCaptureBufferTracker* tracker = GetTracker(buffer_id);
@@ -41,17 +41,16 @@ mojo::ScopedSharedBufferHandle VideoCaptureBufferPoolImpl::GetHandleForTransit(
 }
 
 std::unique_ptr<VideoCaptureBufferHandle>
-VideoCaptureBufferPoolImpl::GetBufferHandle(int buffer_id) {
+VideoCaptureBufferPoolImpl::GetHandleForInProcessAccess(int buffer_id) {
   base::AutoLock lock(lock_);
 
   VideoCaptureBufferTracker* tracker = GetTracker(buffer_id);
   if (!tracker) {
     NOTREACHED() << "Invalid buffer_id.";
-    return std::unique_ptr<VideoCaptureBufferHandle>();
+    return nullptr;
   }
 
-  DCHECK(tracker->held_by_producer());
-  return tracker->GetBufferHandle();
+  return tracker->GetMemoryMappedAccess();
 }
 
 int VideoCaptureBufferPoolImpl::ReserveForProducer(
--- services/video_capture/device_media_to_mojo_adapter.cc
+++ services/video_capture/device_media_to_mojo_adapter.cc
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "media/capture/video/video_capture_buffer_pool_impl.h"
+#include "media/capture/video/video_capture_buffer_tracker_factory_impl.h"
 #include "media/capture/video/video_capture_jpeg_decoder.h"
-#include "services/video_capture/buffer_tracker_factory_impl.h"
 #include "services/video_capture/receiver_mojo_to_media_adapter.h"
 
 namespace video_capture {
@@ -39,7 +39,8 @@ void DeviceMediaToMojoAdapter::Start(const CaptureSettings& requested_settings,
 
   // Create a dedicated buffer pool for the device usage session.
   const int kMaxBufferCount = 2;
-  auto buffer_tracker_factory = base::MakeUnique<BufferTrackerFactoryImpl>();
+  auto buffer_tracker_factory =
+      base::MakeUnique<media::VideoCaptureBufferTrackerFactoryImpl>();
   scoped_refptr<media::VideoCaptureBufferPool> buffer_pool(
       new media::VideoCaptureBufferPoolImpl(std::move(buffer_tracker_factory),
                                             kMaxBufferCount));
