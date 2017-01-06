c08f2f1025159daf871c3fc684b2bc8997c50e92
rouslan <rouslan@chromium.org>
2017-01-06 14:17:55 -0800

    Cache web contents in credit card scanner.
    It's much safer to cache the WebContents instead of WindowAndroid to
    deal with tab reparenting, for example. This patch changes the credit
    card OCR scanner extension point to cache the WebContents instead of
    WindowAndroid.
    
    This patch also adds chrome://flags/#autofill-scan-theme-dialog to be
    used by Chrome's implementation of the credit card scanner. This flag is
    disabled by default and will be removed once the best theme for the scan
    dialog is determined.
    
    BUG=678642
    
    Review-Url: https://codereview.chromium.org/2614883003
    Cr-Commit-Position: refs/heads/master@{#442071}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -42,6 +42,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kAndroidPayIntegrationV1,
     &kAndroidPayIntegrationV2,
     &kAndroidPaymentApps,
+    &kAutofillScanThemeDialog,
     &kCCTExternalLinkHandling,
     &kCCTPostMessageAPI,
     &kChromeHomeFeature,
@@ -82,6 +83,9 @@ const base::Feature kAndroidPayIntegrationV2{"AndroidPayIntegrationV2",
 
 const base::Feature kAndroidPaymentApps{"AndroidPaymentApps",
                                         base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kAutofillScanThemeDialog{"AutofillScanThemeDialog",
+                                             base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kCCTExternalLinkHandling{"CCTExternalLinkHandling",
                                              base::FEATURE_ENABLED_BY_DEFAULT};
