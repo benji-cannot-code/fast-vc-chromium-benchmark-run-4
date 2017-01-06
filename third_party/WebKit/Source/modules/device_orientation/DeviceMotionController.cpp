e0f450187040b278cfae34fe972bbb50e7a4b72d
haraken <haraken@chromium.org>
2017-01-06 03:24:50 -0800

    Use a new Supplement constructor in DeviceOrientationController's hierarchy
    We're deprecating the default constructor of Supplement.
    
    BUG=610176
    
    Review-Url: https://codereview.chromium.org/2615713006
    Cr-Commit-Position: refs/heads/master@{#441920}

--- third_party/WebKit/Source/modules/device_orientation/DeviceMotionController.cpp
+++ third_party/WebKit/Source/modules/device_orientation/DeviceMotionController.cpp
@@ -19,7 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 DeviceMotionController::DeviceMotionController(Document& document)
-    : DeviceSingleWindowEventController(document) {}
+    : DeviceSingleWindowEventController(document),
+      Supplement<Document>(document) {}
 
 DeviceMotionController::~DeviceMotionController() {}
 
--- third_party/WebKit/Source/modules/device_orientation/DeviceOrientationController.cpp
+++ third_party/WebKit/Source/modules/device_orientation/DeviceOrientationController.cpp
@@ -20,7 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 DeviceOrientationController::DeviceOrientationController(Document& document)
-    : DeviceSingleWindowEventController(document) {}
+    : DeviceSingleWindowEventController(document),
+      Supplement<Document>(document) {}
 
 DeviceOrientationController::~DeviceOrientationController() {}
 
