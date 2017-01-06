521324a4a902e4c0da8ac2fea6d6189bc1a3d543
fmalita <fmalita@chromium.org>
2017-01-06 12:47:16 -0800

    Use a raster SkImage in DragImageBuilder
    DragImageBuilder is currently instantiating a picture-backed SkImage.
    But there is no advantage to that, because DragImage::create() is
    immediately rasterizing it (via SkImage::asLegacyBitmap).
    
    Since we're trying to reduce the use of picture-backed images in Blink,
    refactor to rasterize upfront.
    
    R=reed@google.com
    BUG=skia:6100
    
    Review-Url: https://codereview.chromium.org/2619573002
    Cr-Commit-Position: refs/heads/master@{#442039}

--- third_party/WebKit/Source/core/frame/LocalFrame.cpp
+++ third_party/WebKit/Source/core/frame/LocalFrame.cpp
@@ -95,6 +95,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/platform/WebScreenInfo.h"
 #include "public/platform/WebViewScheduler.h"
 #include "third_party/skia/include/core/SkImage.h"
+#include "third_party/skia/include/core/SkSurface.h"
 #include "wtf/PtrUtil.h"
 #include "wtf/StdLibExtras.h"
 #include <memory>
@@ -144,10 +145,15 @@ class DragImageBuilder {
     // TODO(fmalita): endRecording() should return a non-const SKP.
     sk_sp<SkPicture> recording(
         const_cast<SkPicture*>(m_pictureBuilder->endRecording().release()));
-    sk_sp<SkImage> skImage = SkImage::MakeFromPicture(
-        std::move(recording),
-        SkISize::Make(m_bounds.width(), m_bounds.height()), nullptr, nullptr);
-    RefPtr<Image> image = StaticBitmapImage::create(std::move(skImage));
+
+    // Rasterize upfront, since DragImage::create() is going to do it anyway
+    // (SkImage::asLegacyBitmap).
+    sk_sp<SkSurface> surface =
+        SkSurface::MakeRasterN32Premul(m_bounds.width(), m_bounds.height());
+    surface->getCanvas()->drawPicture(recording);
+    RefPtr<Image> image =
+        StaticBitmapImage::create(surface->makeImageSnapshot());
+
     float screenDeviceScaleFactor =
         m_localFrame->page()->chromeClient().screenInfo().deviceScaleFactor;
 
