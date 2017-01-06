bea638edc5daa854efd5c326387291ddbea880a1
haraken <haraken@chromium.org>
2017-01-06 01:09:11 -0800

    Remove isGlobalInitialized
    Now it's unused. We can remove it.
    
    BUG=677253
    
    Review-Url: https://codereview.chromium.org/2619523003
    Cr-Commit-Position: refs/heads/master@{#441906}

--- third_party/WebKit/Source/bindings/core/v8/WindowProxy.h
+++ third_party/WebKit/Source/bindings/core/v8/WindowProxy.h
@@ -76,8 +76,6 @@ class WindowProxy final : public GarbageCollectedFinalized<WindowProxy> {
   bool isContextInitialized() {
     return m_scriptState && !!m_scriptState->perContextData();
   }
-  bool isGlobalInitialized() { return !m_globalProxy.isEmpty(); }
-
   void initializeIfNeeded();
 
   void clearForNavigation();
