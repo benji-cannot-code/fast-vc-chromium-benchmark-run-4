7b4b519cba278c9801617a43c88a2aa899428bc0
gambard <gambard@chromium.org>
2017-01-06 02:36:55 -0800

    Display the status bar in the ReadingList in Landscape
    BUG=678393
    
    Review-Url: https://codereview.chromium.org/2611973003
    Cr-Commit-Position: refs/heads/master@{#441913}

--- ios/chrome/browser/ui/reading_list/reading_list_view_controller_container.mm
+++ ios/chrome/browser/ui/reading_list/reading_list_view_controller_container.mm
@@ -85,6 +85,10 @@ typedef NS_ENUM(NSInteger, LayoutPriority) {
   constraint.active = YES;
 }
 
+- (BOOL)prefersStatusBarHidden {
+  return NO;
+}
+
 #pragma mark - ReadingListViewControllerAudience
 
 - (void)setCollectionHasItems:(BOOL)hasItems {
