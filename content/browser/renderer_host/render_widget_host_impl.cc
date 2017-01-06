d2791b9295282d33acc2af8e1afd6596fa97c172
tonikitoo <tonikitoo@igalia.com>
2017-01-05 17:21:47 -0800

    Early return in ::SetTooltipText in case GetView returns null
    Driven by change: there is no point in doing the wrapping of
    the tooltip text with RTL/LTR formatting, in case there is no view.
    Simply bail out earlier.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2611943003
    Cr-Commit-Position: refs/heads/master@{#441784}

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -1760,6 +1760,9 @@ void RenderWidgetHostImpl::OnClose() {
 void RenderWidgetHostImpl::OnSetTooltipText(
     const base::string16& tooltip_text,
     WebTextDirection text_direction_hint) {
+  if (!GetView())
+    return;
+
   // First, add directionality marks around tooltip text if necessary.
   // A naive solution would be to simply always wrap the text. However, on
   // windows, Unicode directional embedding characters can't be displayed on
@@ -1785,8 +1788,7 @@ void RenderWidgetHostImpl::OnSetTooltipText(
       base::i18n::WrapStringWithRTLFormatting(&wrapped_tooltip_text);
     }
   }
-  if (GetView())
-    view_->SetTooltipText(wrapped_tooltip_text);
+  view_->SetTooltipText(wrapped_tooltip_text);
 }
 
 void RenderWidgetHostImpl::OnUpdateScreenRectsAck() {
