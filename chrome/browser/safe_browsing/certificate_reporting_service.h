93112da576c94c57413c201abb8e79d7a7423aa5
meacer <meacer@chromium.org>
2017-01-06 11:30:25 -0800

    Fix flaky CertificateReportingService browser tests.
    CertificateReportingService browser tests wait for the IO thread
    after sending a report or resetting the service. This doesn't seem
    reliable and the tests occasionally fail. A better approach is to:
    - For reports, wait for requests to be destroyed
    (CertificateReportingService unit test already does this).
    - For service resets, wait for a callback to be called.
    
    This CL does these: It removes WaitForIOThread calls from the browser
    test and replaces them with the above. It also refactors the following:
    - RequestObserver is shared between browser and unit tests.
    - NetworkDelegate is no longer used in the unit tests. RequestObserver is
    used instead.
    - All tests now expect no URL requests to remain during tear down.
    - The code to check expected reports is now shared.
    
    BUG=677560
    
    Review-Url: https://codereview.chromium.org/2605403002
    Cr-Commit-Position: refs/heads/master@{#442008}

--- chrome/browser/safe_browsing/certificate_reporting_service.h
+++ chrome/browser/safe_browsing/certificate_reporting_service.h
@@ -153,7 +153,8 @@ class CertificateReportingService : public KeyedService {
       uint32_t server_public_key_version,
       size_t max_queued_report_count,
       base::TimeDelta max_report_age,
-      base::Clock* clock);
+      base::Clock* clock,
+      const base::Callback<void()>& reset_callback);
 
   ~CertificateReportingService() override;
 
@@ -226,6 +227,9 @@ class CertificateReportingService : public KeyedService {
   const base::TimeDelta max_report_age_;
 
   base::Clock* const clock_;
+
+  // Called when the service is reset. Used for testing.
+  base::Callback<void()> reset_callback_;
 
   // Encryption parameters.
   uint8_t* server_public_key_;
--- chrome/browser/safe_browsing/certificate_reporting_service_factory.cc
+++ chrome/browser/safe_browsing/certificate_reporting_service_factory.cc
@@ -61,6 +61,11 @@ void CertificateReportingServiceFactory::SetMaxQueuedReportCountForTesting(
   max_queued_report_count_ = max_queued_report_count;
 }
 
+void CertificateReportingServiceFactory::SetServiceResetCallbackForTesting(
+    const base::Callback<void()>& service_reset_callback) {
+  service_reset_callback_ = service_reset_callback;
+}
+
 CertificateReportingServiceFactory::CertificateReportingServiceFactory()
     : BrowserContextKeyedServiceFactory(
           "cert_reporting::Factory",
@@ -69,7 +74,8 @@ CertificateReportingServiceFactory::CertificateReportingServiceFactory()
       server_public_key_version_(0),
       clock_(new base::DefaultClock()),
       queued_report_ttl_(base::TimeDelta::FromSeconds(kMaxReportAgeInSeconds)),
-      max_queued_report_count_(kMaxReportCountInQueue) {}
+      max_queued_report_count_(kMaxReportCountInQueue),
+      service_reset_callback_(base::Bind(&base::DoNothing)) {}
 
 CertificateReportingServiceFactory::~CertificateReportingServiceFactory() {}
 
@@ -81,7 +87,7 @@ KeyedService* CertificateReportingServiceFactory::BuildServiceInstanceFor(
       safe_browsing_service, safe_browsing_service->url_request_context(),
       static_cast<Profile*>(profile), server_public_key_,
       server_public_key_version_, max_queued_report_count_, queued_report_ttl_,
-      clock_.get());
+      clock_.get(), service_reset_callback_);
 }
 
 content::BrowserContext*
