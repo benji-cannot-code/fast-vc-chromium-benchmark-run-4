bf149dc0387b6609d8e5cb1d946509667ab9752b
msw <msw@chromium.org>
2017-01-06 12:59:50 -0800

    Use the correct profile for shelf prefs.
    Used the attached profile; not the active profile.
    Regressed in: https://codereview.chromium.org/2391253004
    
    BUG=675599
    TEST=Multi-login cros users get correct shelf prefs.
    R=jamescook@chromium.org
    
    Review-Url: https://codereview.chromium.org/2613303003
    Cr-Commit-Position: refs/heads/master@{#442045}

--- chrome/browser/ui/ash/launcher/chrome_launcher_controller.cc
+++ chrome/browser/ui/ash/launcher/chrome_launcher_controller.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ptr_util.h"
 #include "chrome/browser/extensions/extension_app_icon_loader.h"
-#include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/app_list/arc/arc_app_icon_loader.h"
 #include "chrome/browser/ui/ash/ash_util.h"
 #include "chrome/browser/ui/ash/chrome_launcher_prefs.h"
@@ -81,7 +81,7 @@ void ChromeLauncherController::SetShelfAutoHideBehaviorFromPrefs() {
     return;
 
   // The pref helper functions return default values for invalid display ids.
-  PrefService* prefs = ProfileManager::GetActiveUserProfile()->GetPrefs();
+  PrefService* prefs = profile_->GetPrefs();
   for (const auto& display : display::Screen::GetScreen()->GetAllDisplays()) {
     shelf_controller_->SetAutoHideBehavior(
         ash::launcher::GetShelfAutoHideBehaviorPref(prefs, display.id()),
@@ -94,7 +94,7 @@ void ChromeLauncherController::SetShelfAlignmentFromPrefs() {
     return;
 
   // The pref helper functions return default values for invalid display ids.
-  PrefService* prefs = ProfileManager::GetActiveUserProfile()->GetPrefs();
+  PrefService* prefs = profile_->GetPrefs();
   for (const auto& display : display::Screen::GetScreen()->GetAllDisplays()) {
     shelf_controller_->SetAlignment(
         ash::launcher::GetShelfAlignmentPref(prefs, display.id()),
