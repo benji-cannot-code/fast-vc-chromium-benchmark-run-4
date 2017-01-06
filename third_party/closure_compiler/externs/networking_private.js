1c325704e0bb3c533d68be288e01a13b69a6600b
stevenjb <stevenjb@chromium.org>
2017-01-05 18:42:22 -0800

    Add SignalStrength to Cellular {Managed}Properties
    SignalStrength was a member of CellularStateProperties, but not
    Cellular{Managed}Properties which are supposed to be a superset.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2618793004
    Cr-Commit-Position: refs/heads/master@{#441843}

--- third_party/closure_compiler/externs/networking_private.js
+++ third_party/closure_compiler/externs/networking_private.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2015 The Chromium Authors. All rights reserved.
+// Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -413,7 +413,7 @@ chrome.networkingPrivate.IPSecProperties;
  *   ClientCertType: (!chrome.networkingPrivate.ManagedDOMString|undefined),
  *   EAP: (!chrome.networkingPrivate.ManagedEAPProperties|undefined),
  *   Group: (!chrome.networkingPrivate.ManagedDOMString|undefined),
- *   IKEVersion: !chrome.networkingPrivate.ManagedLong,
+ *   IKEVersion: (!chrome.networkingPrivate.ManagedLong|undefined),
  *   PSK: (!chrome.networkingPrivate.ManagedDOMString|undefined),
  *   SaveCredentials: (!chrome.networkingPrivate.ManagedBoolean|undefined),
  *   ServerCARefs: (!chrome.networkingPrivate.ManagedDOMStringList|undefined),
@@ -675,6 +675,7 @@ chrome.networkingPrivate.ManagedThirdPartyVPNProperties;
  *   ServingOperator: (!chrome.networkingPrivate.CellularProviderProperties|undefined),
  *   SIMLockStatus: (!chrome.networkingPrivate.SIMLockStatus|undefined),
  *   SIMPresent: (boolean|undefined),
+ *   SignalStrength: (number|undefined),
  *   SupportNetworkScan: (boolean|undefined),
  *   SupportedCarriers: (!Array<string>|undefined)
  * }}
@@ -712,6 +713,7 @@ chrome.networkingPrivate.CellularProperties;
  *   ServingOperator: (!chrome.networkingPrivate.CellularProviderProperties|undefined),
  *   SIMLockStatus: (!chrome.networkingPrivate.SIMLockStatus|undefined),
  *   SIMPresent: (boolean|undefined),
+ *   SignalStrength: (number|undefined),
  *   SupportNetworkScan: (boolean|undefined),
  *   SupportedCarriers: (!Array<string>|undefined)
  * }}
@@ -1263,6 +1265,7 @@ chrome.networkingPrivate.getCaptivePortalStatus = function(networkGuid, callback
  * succeeds (|puk| is valid), the PIN will be set to |pin|.   (If |pin| is empty
  * or invalid the operation will fail).
  * @param {string} networkGuid The GUID of the cellular network to unlock.
+ *     If empty, the default cellular device will be used.
  * @param {string} pin The current SIM PIN, or the new PIN if PUK is provided.
  * @param {string=} puk The operator provided PUK for unblocking a blocked SIM.
  * @param {function():void=} callback Called when the operation has completed.
@@ -1279,7 +1282,7 @@ chrome.networkingPrivate.unlockCellularSim = function(networkGuid, pin, puk, cal
  * unlockCellularSim() before this can be called (otherwise it will fail and
  * chrome.runtime.lastError will be set to Error.SimLocked).
  * @param {string} networkGuid The GUID of the cellular network to set the SIM
- *     state of.
+ *     state of.     If empty, the default cellular device will be used.
  * @param {!chrome.networkingPrivate.CellularSimState} simState The SIM state to
  *     set.
  * @param {function():void=} callback Called when the operation has completed.
