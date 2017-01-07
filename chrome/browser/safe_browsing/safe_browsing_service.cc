cae7e69cea73b5fdd035565cfa4edb5c66659144
ntfschr <ntfschr@chromium.org>
2017-01-06 18:09:53 -0800

    Revert of componentize SafeBrowsingService (patchset #6 id:100001 of https://codereview.chromium.org/2605213002/ )
    Reason for revert:
    We've decided to not componentize SafeBrowsingService and instead manually create the DataBaseManager and UIManager
    
    Original issue's description:
    > componentize SafeBrowsingService
    >
    > Create BaseSafeBrowsingService in the components layer and derive
    > SafeBrowsingService from that.
    >
    > BUG=488675
    >
    > Committed: https://crrev.com/e5f4a58871b512092e37080597014271bbb8fbc6
    > Cr-Commit-Position: refs/heads/master@{#441305}
    
    TBR=jialiul@chromium.org,mmenke@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=488675
    
    Review-Url: https://codereview.chromium.org/2615713008
    Cr-Commit-Position: refs/heads/master@{#442139}

--- chrome/browser/safe_browsing/safe_browsing_service.cc
+++ chrome/browser/safe_browsing/safe_browsing_service.cc
@@ -287,6 +287,7 @@ SafeBrowsingService* SafeBrowsingService::CreateSafeBrowsingService() {
 
 SafeBrowsingService::SafeBrowsingService()
     : services_delegate_(ServicesDelegate::Create(this)),
+      enabled_(false),
       enabled_by_prefs_(false),
       enabled_v4_only_(safe_browsing::V4FeatureList::IsV4OnlyEnabled()) {}
 
@@ -393,8 +394,6 @@ SafeBrowsingService::url_request_context() {
   return url_request_context_getter_;
 }
 
-// TODO(ntfschr): componentize this once BaseSafeBrowsingUIManager contains a
-// SafeBrowsingService
 const scoped_refptr<SafeBrowsingUIManager>&
 SafeBrowsingService::ui_manager() const {
   return ui_manager_;
@@ -465,8 +464,6 @@ void SafeBrowsingService::OnResourceRequest(const net::URLRequest* request) {
 #endif
 }
 
-// TODO(ntfschr): componentize this once BaseSafeBrowsingUIManager contains a
-// SafeBrowsingService
 SafeBrowsingUIManager* SafeBrowsingService::CreateUIManager() {
   return new SafeBrowsingUIManager(this);
 }
