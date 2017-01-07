d9d35db6d5597cfe088aac22318db07936c1f4a4
alph <alph@chromium.org>
2017-01-06 18:03:28 -0800

    DevTools: Split Timeline started UMA metric into two parts.
    "Timeline recording" (as it was previously) and "Timeline recording for page reload".
    
    Review-Url: https://codereview.chromium.org/2617553002
    Cr-Commit-Position: refs/heads/master@{#442137}

--- third_party/WebKit/Source/devtools/front_end/host/UserMetrics.js
+++ third_party/WebKit/Source/devtools/front_end/host/UserMetrics.js
@@ -80,7 +80,8 @@ Host.UserMetrics.Action = {
   StyleRuleEdited: 14,
   CommandEvaluatedInConsolePanel: 15,
   DOMPropertiesExpanded: 16,
-  ResizedViewInResponsiveMode: 17
+  ResizedViewInResponsiveMode: 17,
+  TimelinePageReloadStarted: 18
 };
 
 Host.UserMetrics._PanelCodes = {
