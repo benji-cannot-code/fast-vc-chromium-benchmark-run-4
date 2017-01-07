7f636d79ee85648362a39f5f087b60b379a32f3e
reillyg <reillyg@chromium.org>
2017-01-06 16:18:17 -0800

    Allow top-level frames to request any USB device.
    This patch changes the rules for WebUSB so that a top level frame can
    request access to any USB device, while embedded frames must have an
    origin matching the allowed origins structure read from the device.
    
    Integration tests for WebUSB are added which exercise this.
    
    BUG=492204
    
    Review-Url: https://codereview.chromium.org/2611773004
    Cr-Commit-Position: refs/heads/master@{#442105}

--- device/usb/mock_usb_service.cc
+++ device/usb/mock_usb_service.cc
@@ -8,11 +8,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/threading/thread_task_runner_handle.h"
 #include "device/usb/usb_device.h"
 
 namespace device {
 
-MockUsbService::MockUsbService() : UsbService(nullptr, nullptr) {}
+MockUsbService::MockUsbService()
+    : UsbService(base::ThreadTaskRunnerHandle::IsSet()
+                     ? base::ThreadTaskRunnerHandle::Get()
+                     : nullptr,
+                 nullptr) {}
 
 MockUsbService::~MockUsbService() {
   // Shutdown() must be called before the base class destructor.
