c050ea8d91da87c3f89fd07c1ad03058fda25de4
pdr <pdr@chromium.org>
2017-01-06 19:18:47 -0800

    Remove multicol special-cases from invalidation and the pre-paint tree walk
    Multicolumn spanners paint out-of-flow through placeholder LayoutObjects
    but their paint properties still follow the layout tree in DOM order[1].
    This patch switches the pre-paint tree walk to be entirely DOM order
    instead of having a special-case for the paint-order multicolumn
    spanner. This patch also changes paint invalidation of multicolumn
    to follow DOM ancestors.
    
    With this approach, the pre-paint tree walk and paint invalidation
    can be simplified, and only paint offset needs special adjustment
    for multicolumn during the pre-paint tree walk.
    
    [1] For example, the following layout tree:
    LayoutBlockFlow DIV style="columns: 2"
      LayoutMultiColumnFlowThread (anonymous)
        LayoutBlockFlow (column spanner) 	DIV id="spanner"
      LayoutMultiColumnSpannerPlaceholder (anonymous)
    
    paint order:
      LayoutBlockFlow DIV
      LayoutMultiColumnFlowThread (anonymous)
      LayoutMultiColumnSpannerPlaceholder (anonymous)
      LayoutBlockFlow (column spanner) 	DIV id="spanner"
    
    DOM order:
      LayoutBlockFlow DIV
      LayoutMultiColumnFlowThread (anonymous)
      LayoutBlockFlow (column spanner) 	DIV id="spanner"
      LayoutMultiColumnSpannerPlaceholder (anonymous)
    
    BUG=648274
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2615203002
    Cr-Commit-Position: refs/heads/master@{#442149}

--- third_party/WebKit/Source/core/layout/LayoutObject.h
+++ third_party/WebKit/Source/core/layout/LayoutObject.h
@@ -2068,11 +2068,8 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
 
   static bool isAllowedToModifyLayoutTreeStructure(Document&);
 
-  // Returns the parent for paint invalidation.
-  // - For LayoutView, returns the owner layout object in the containing frame
-  //   if any or nullptr;
-  // - For multi-column spanner, returns the spanner placeholder;
-  // - Otherwise returns parent().
+  // Returns the parent for paint invalidation. For LayoutView, returns the
+  // owner layout object in the containing frame if any, or nullptr.
   inline LayoutObject* paintInvalidationParent() const;
   LayoutObject* slowPaintInvalidationParentForTesting() const;
 
