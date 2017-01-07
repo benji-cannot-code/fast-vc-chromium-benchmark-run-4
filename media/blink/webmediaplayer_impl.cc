09ba44da94a27fb8bec6b4f2ac2fb7415ba646cb
avayvod <avayvod@chromium.org>
2017-01-06 16:33:03 -0800

    [Video] Pause videos with no audio when in the background.
    Pause video-only players when they go to background and continue playback when they go into foreground and hasn't been paused explicitly in the mean time.
    Behind the experiment for optimizing background video playback.
    
    BUG=671381
    TEST=Manual
    
    Review-Url: https://codereview.chromium.org/2567833002
    Cr-Commit-Position: refs/heads/master@{#442110}

--- media/blink/webmediaplayer_impl.cc
+++ media/blink/webmediaplayer_impl.cc
@@ -206,6 +206,7 @@ WebMediaPlayerImpl::WebMediaPlayerImpl(
       opaque_(false),
       playback_rate_(0.0),
       paused_(true),
+      paused_when_hidden_(false),
       seeking_(false),
       pending_suspend_resume_cycle_(false),
       ended_(false),
@@ -448,6 +449,9 @@ void WebMediaPlayerImpl::pause() {
   // called when casting begins, and when we exit casting we should end up in a
   // paused state.
   paused_ = true;
+
+  // No longer paused because it was hidden.
+  paused_when_hidden_ = false;
 
 #if defined(OS_ANDROID)  // WMPI_CAST
   if (isRemote()) {
@@ -1343,11 +1347,20 @@ void WebMediaPlayerImpl::OnVideoOpacityChange(bool opaque) {
 void WebMediaPlayerImpl::OnHidden() {
   DCHECK(main_task_runner_->BelongsToCurrentThread());
 
-  if (IsBackgroundVideoTrackOptimizationEnabled())
-    selectedVideoTrackChanged(nullptr);
-
   if (watch_time_reporter_)
     watch_time_reporter_->OnHidden();
+
+  if (IsBackgroundVideoTrackOptimizationEnabled()) {
+    if (ShouldPauseWhenHidden()) {
+      // OnPause() will set |paused_when_hidden_| to false and call
+      // UpdatePlayState(), so set the flag to true after and then return.
+      OnPause();
+      paused_when_hidden_ = true;
+      return;
+    }
+
+    selectedVideoTrackChanged(nullptr);
+  }
 
   UpdatePlayState();
 
@@ -1366,10 +1379,17 @@ void WebMediaPlayerImpl::OnShown() {
       base::Bind(&VideoFrameCompositor::SetForegroundTime,
                  base::Unretained(compositor_), base::TimeTicks::Now()));
 
-  if (IsBackgroundVideoTrackOptimizationEnabled() &&
-      client_->hasSelectedVideoTrack()) {
-    WebMediaPlayer::TrackId trackId = client_->getSelectedVideoTrackId();
-    selectedVideoTrackChanged(&trackId);
+  if (IsBackgroundVideoTrackOptimizationEnabled()) {
+    if (paused_when_hidden_) {
+      paused_when_hidden_ = false;
+      OnPlay();  // Calls UpdatePlayState() so return afterwards.
+      return;
+    }
+
+    if (client_->hasSelectedVideoTrack()) {
+      WebMediaPlayer::TrackId trackId = client_->getSelectedVideoTrackId();
+      selectedVideoTrackChanged(&trackId);
+    }
   }
 
   must_suspend_ = false;
@@ -2050,6 +2070,15 @@ void WebMediaPlayerImpl::ActivateViewportIntersectionMonitoring(bool activate) {
   DCHECK(main_task_runner_->BelongsToCurrentThread());
 
   client_->activateViewportIntersectionMonitoring(activate);
+}
+
+bool WebMediaPlayerImpl::ShouldPauseWhenHidden() const {
+#if defined(OS_ANDROID)  // WMPI_CAST
+  if (isRemote())
+    return false;
+#endif  // defined(OS_ANDROID)  // WMPI_CAST
+
+  return hasVideo() && !hasAudio();
 }
 
 }  // namespace media
