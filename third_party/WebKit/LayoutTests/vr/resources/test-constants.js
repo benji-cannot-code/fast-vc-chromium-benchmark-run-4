eed0b036db4d9de76e8d63e8185a90381c38e904
bsheedy <bsheedy@chromium.org>
2017-01-06 17:55:25 -0800

    Add more WebVR layout tests, adjust test format
    Adds a handful of new WebVR layout tests using mocking.
    
    Adjusts the vr_test to pass the provided function an async_test handle. This
    was done due to how easy it was for the previous promise_test implementation
    to resolve earlier than expected, causing the test to pass before all code was
    run with no visible difference in the output. The use of the async_test makes
    the tests slightly more verbose, but ensures that we're only ending the test
    when we expect to.
    
    Add a fix for VRDisplay not initializing layer bounds to defaults correctly.
    
    BUG=675325,670490,650703
    
    Review-Url: https://codereview.chromium.org/2617183002
    Cr-Commit-Position: refs/heads/master@{#442135}

---
+++ third_party/WebKit/LayoutTests/vr/resources/test-constants.js
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// assert_equals can fail when comparing floats due to precision errors, so
+// use assert_approx_equals with this constant instead
+var FLOAT_EPSILON = 0.0000001;
