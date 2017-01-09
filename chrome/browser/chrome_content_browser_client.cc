1768a7a5dc821158238935b9423c11129753f397
xianglu <xianglu@chromium.org>
2017-01-09 10:12:36 -0800

    Shape Detection: Add Text Detection in Chrome Android
    This CL adds text detection API in addition to face detection and barcode
    detection. It uses com.google.android.gms package and is registered in
    ChromeInterfaceRegistrar.java.
    
    Intent to implement:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/JkdoxpINjxQ
    
    BUG=676124
    SPEC=
     https://wicg.github.io/shape-detection-api/#text-detection-api
    TEST(Layout)=
     third_party/WebKit/LayoutTests/fast/shapedetection/shapedetection-creation.html
     third_party/WebKit/LayoutTests/fast/shapedetection/shapedetection-empty-input.html
     third_party/WebKit/LayoutTests/shapedetection/detection-HTMLCanvasElement.html
     third_party/WebKit/LayoutTests/shapedetection/detection-HTMLImageElement.html
     third_party/WebKit/LayoutTests/shapedetection/detection-HTMLVideoElement.html
     third_party/WebKit/LayoutTests/shapedetection/detection-ImageBitmap.html
     third_party/WebKit/LayoutTests/shapedetection/detection-ImageData.html
    DEMO=
     https://s.codepen.io/xianglu_google/debug/pNGBKv
     https://s.codepen.io/xianglu_google/debug/QGoaYe
     https://drive.google.com/file/d/0B3T2kSOMuTq3aFhZelAxTFdNWUk/view?usp=sharing (video)
    
    Review-Url: https://codereview.chromium.org/2588293005
    Cr-Commit-Position: refs/heads/master@{#442293}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -207,6 +207,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/service_manager/public/cpp/service.h"
 #include "storage/browser/fileapi/external_mount_points.h"
 #include "third_party/WebKit/public/platform/modules/shapedetection/barcodedetection.mojom.h"
+#include "third_party/WebKit/public/platform/modules/shapedetection/textdetection.mojom.h"
 #include "third_party/WebKit/public/platform/modules/webshare/webshare.mojom.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
@@ -3033,6 +3034,9 @@ void ChromeContentBrowserClient::RegisterRenderFrameMojoInterfaces(
       registry->AddInterface(
           web_contents->GetJavaInterfaces()
               ->CreateInterfaceFactory<blink::mojom::BarcodeDetection>());
+      registry->AddInterface(
+          web_contents->GetJavaInterfaces()
+              ->CreateInterfaceFactory<blink::mojom::TextDetection>());
     }
   }
 #endif
--- third_party/WebKit/Source/modules/shapedetection/BarcodeDetector.cpp
+++ third_party/WebKit/Source/modules/shapedetection/BarcodeDetector.cpp
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/dom/DOMException.h"
 #include "core/dom/DOMRect.h"
-#include "core/frame/LocalDOMWindow.h"
 #include "core/frame/LocalFrame.h"
 #include "core/html/canvas/CanvasImageSource.h"
 #include "modules/imagecapture/Point2D.h"
