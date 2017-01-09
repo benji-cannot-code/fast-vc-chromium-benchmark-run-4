323f9a7cae7d93a238392885bd24c8e33e7c2846
vitaliii <vitaliii@chromium.org>
2017-01-09 04:16:11 -0800

    Revert of Add more WebVR layout tests, adjust test format (patchset #4 id:60001 of https://codereview.chromium.org/2617183002/ )
    Reason for revert:
    Two of the added tests crash:
    vr/requestAnimationFrame_invalidhandle.html
    vr/requestAnimationFrame_unregister.html
    
    BUG=679288
    
    Original issue's description:
    > Add more WebVR layout tests, adjust test format
    >
    > Adds a handful of new WebVR layout tests using mocking.
    >
    > Adjusts the vr_test to pass the provided function an async_test handle. This
    > was done due to how easy it was for the previous promise_test implementation
    > to resolve earlier than expected, causing the test to pass before all code was
    > run with no visible difference in the output. The use of the async_test makes
    > the tests slightly more verbose, but ensures that we're only ending the test
    > when we expect to.
    >
    > Add a fix for VRDisplay not initializing layer bounds to defaults correctly.
    >
    > BUG=675325,670490,650703
    >
    > Review-Url: https://codereview.chromium.org/2617183002
    > Cr-Commit-Position: refs/heads/master@{#442135}
    > Committed: https://chromium.googlesource.com/chromium/src/+/ed03ff2faec8339e93a7c48ef5213d4d73fc4f41
    
    TBR=bajones@chromium.org,dglazkov@chromium.org,bsheedy@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=675325,670490,650703
    
    Review-Url: https://codereview.chromium.org/2616403002
    Cr-Commit-Position: refs/heads/master@{#442237}

--- third_party/WebKit/Source/modules/vr/VRDisplay.cpp
+++ third_party/WebKit/Source/modules/vr/VRDisplay.cpp
@@ -559,7 +559,6 @@ void VRDisplay::updateLayerBounds() {
     leftBounds->top = 0.0f;
     leftBounds->width = 0.5f;
     leftBounds->height = 1.0f;
-    m_layer.setLeftBounds({0.0f, 0.0f, 0.5f, 1.0f});
   }
 
   if (m_layer.rightBounds().size() == 4) {
@@ -573,7 +572,6 @@ void VRDisplay::updateLayerBounds() {
     rightBounds->top = 0.0f;
     rightBounds->width = 0.5f;
     rightBounds->height = 1.0f;
-    m_layer.setRightBounds({0.5f, 0.0f, 0.5f, 1.0f});
   }
 
   m_display->UpdateLayerBounds(std::move(leftBounds), std::move(rightBounds));
