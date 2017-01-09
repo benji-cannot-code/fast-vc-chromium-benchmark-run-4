085681766ee2e22b77c1be05bfa392cc48524bcf
dullweber <dullweber@chromium.org>
2017-01-09 02:54:56 -0800

    Cleanup deleted profile directories if they still exist after a restart.
    Normally profile directories are deleted on shutdown but for example a crash can prevent this. To solve this issue, I store a list of profile paths that should be deleted and check if they were deleted on next startup. If that is not the case, I try to delete them again.
    
    BUG=236244
    
    Review-Url: https://codereview.chromium.org/2546523003
    Cr-Commit-Position: refs/heads/master@{#442230}

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -1266,6 +1266,8 @@ void ChromeBrowserMainParts::PreProfileInit() {
 
   // Ephemeral profiles may have been left behind if the browser crashed.
   g_browser_process->profile_manager()->CleanUpEphemeralProfiles();
+  // Files of deleted profiles can also be left behind after a crash.
+  g_browser_process->profile_manager()->CleanUpDeletedProfiles();
 #endif  // !defined(OS_ANDROID)
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
