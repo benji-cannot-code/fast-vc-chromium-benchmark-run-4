183b257e547c4b733e1d3fafae3639ca75d01e1d
tobiasjs <tobiasjs@chromium.org>
2017-01-09 05:19:03 -0800

    Revert of Ensure navigator.getVRDisplays always resolves. (patchset #6 id:100001 of https://codereview.chromium.org/2614873002/ )
    Reason for revert:
    Caused builder failures(e.g. https://uberchromegw.corp.google.com/i/internal.client.clank/builders/x64-builder) when building blimp.
    
    undefined reference to `device::mojom::VRService::Name_'
    
    Original issue's description:
    > Ensure navigator.getVRDisplays always resolves.
    >
    > In the case that the backing VR service is not available the call will now
    > resolve to an empty array.
    >
    > BUG=678283
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    >
    > Review-Url: https://codereview.chromium.org/2614873002
    > Cr-Commit-Position: refs/heads/master@{#442117}
    > Committed: https://chromium.googlesource.com/chromium/src/+/d83aedbf719e5346d2de69a79b5a1d7e9b6c8462
    
    TBR=rockot@chromium.org,alexmos@chromium.org,ddorwin@chromium.org,bajones@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=678283
    
    Review-Url: https://codereview.chromium.org/2614353002
    Cr-Commit-Position: refs/heads/master@{#442244}

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -126,7 +126,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/frame_host/popup_menu_helper_mac.h"
 #endif
 
+#if defined(ENABLE_WEBVR)
 #include "device/vr/vr_service_impl.h"  // nogncheck
+#endif
 
 using base::TimeDelta;
 
@@ -237,11 +239,6 @@ class RemoterFactoryImpl final : public media::mojom::RemoterFactory {
   DISALLOW_COPY_AND_ASSIGN(RemoterFactoryImpl);
 };
 #endif  // BUILDFLAG(ENABLE_MEDIA_REMOTING)
-
-template <typename Interface>
-void IgnoreInterfaceRequest(mojo::InterfaceRequest<Interface> request) {
-  // Intentionally ignore the interface request.
-}
 
 }  // namespace
 
@@ -2312,11 +2309,7 @@ void RenderFrameHostImpl::RegisterMojoInterfaces() {
 #if defined(ENABLE_WEBVR)
   GetInterfaceRegistry()->AddInterface<device::mojom::VRService>(
       base::Bind(&device::VRServiceImpl::Create));
-#else
-  GetInterfaceRegistry()->AddInterface<device::mojom::VRService>(
-      base::Bind(&IgnoreInterfaceRequest<device::mojom::VRService>));
 #endif
-
   if (base::FeatureList::IsEnabled(features::kGenericSensor)) {
     GetInterfaceRegistry()->AddInterface(
         base::Bind(&device::SensorProviderImpl::Create,
