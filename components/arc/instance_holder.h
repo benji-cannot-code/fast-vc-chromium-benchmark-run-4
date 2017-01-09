c67c897002c6513f2b90c4e63750848de6bec9f8
yusukes <yusukes@chromium.org>
2017-01-09 11:46:02 -0800

    arc: rename GetInstanceForVersion
    to GetInstanceForVersionDoNotCallDirectly so developers will
    always use ARC_GET_INSTANCE_FOR_METHOD instead.
    
    BUG=649782
    TEST=try
    
    Review-Url: https://codereview.chromium.org/2619843002
    Cr-Commit-Position: refs/heads/master@{#442317}

--- components/arc/instance_holder.h
+++ components/arc/instance_holder.h
@@ -15,12 +15,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/threading/thread_checker.h"
 
-// A convenience macro to call arc::InstanceHolder<T>::GetInstanceForVersion().
+// A macro to call InstanceHolder<T>::GetInstanceForVersionDoNotCallDirectly().
 // In order to avoid exposing method names from within the Mojo bindings, we
 // will rely on stringification and the fact that the method min versions have a
 // consistent naming scheme.
 #define ARC_GET_INSTANCE_FOR_METHOD(holder, method_name)        \
-  (holder)->GetInstanceForVersion(                              \
+  (holder)->GetInstanceForVersionDoNotCallDirectly(             \
       std::remove_pointer<decltype(                             \
           holder)>::type::Instance::k##method_name##MinVersion, \
       #method_name)
@@ -61,8 +61,9 @@ class InstanceHolder {
   // not have the requested version, and logs appropriately.
   // This function should not be called directly. Instead, use the
   // ARC_GET_INSTANCE_FOR_METHOD() macro.
-  T* GetInstanceForVersion(uint32_t min_version,
-                           const char method_name_for_logging[]) {
+  T* GetInstanceForVersionDoNotCallDirectly(
+      uint32_t min_version,
+      const char method_name_for_logging[]) {
     if (!instance_) {
       VLOG(1) << "Instance for " << T::Name_ << "::" << method_name_for_logging
               << " not available.";
