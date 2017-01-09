e8b9e445ec3821bb9fe8896cdcd816dc8c9e9bde
pavely <pavely@chromium.org>
2017-01-09 13:24:26 -0800

    [Sync] ModelTypeWorker should delete tracker for entity deleted on the server
    The issue is that when entity is deleted, worker retains entity tracker object.
    Later when entity is recreated information from old tracker object is used.
    
    I'm changing worker to delete tracker object when it receives commit response
    about deleted entity.
    
    BUG=677792
    R=maxbogue@chromium.org
    
    Review-Url: https://codereview.chromium.org/2611123005
    Cr-Commit-Position: refs/heads/master@{#442350}

--- components/sync/test/engine/mock_model_type_processor.h
+++ components/sync/test/engine/mock_model_type_processor.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 #include <memory>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -140,6 +141,10 @@ class MockModelTypeProcessor : public ModelTypeProcessor {
   std::map<const std::string, int64_t> sequence_numbers_;
   std::map<const std::string, int64_t> base_versions_;
   std::map<const std::string, std::string> assigned_ids_;
+
+  // Set of tag hashes which were deleted with DeleteRequest but haven't yet
+  // been confirmed by the server with OnCommitCompleted.
+  std::set<std::string> pending_deleted_hashes_;
 
   // Callback which will be call during disconnection
   DisconnectCallback disconnect_callback_;
