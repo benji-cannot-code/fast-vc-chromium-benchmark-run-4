fed4d28e31461de5896defd38a9a9b242f7ac061
alexilin <alexilin@chromium.org>
2017-01-09 08:19:59 -0800

    predictors: browser test for client-side redirect.
    ResourcePrefetchPredictor doesn't receive the information from observers about
    redirects initiated from javascript. However such redirects initiate the new
    navigation that treated by predictor as usual.
    This browser test documents the current behavior that could be a subject of
    change.
    
    Also, this CL changes all existing redirect tests to be cross-host. Redirect to
    the same host could be ambiguous because of host-based learning.
    
    BUG=650253
    
    Review-Url: https://codereview.chromium.org/2609403007
    Cr-Commit-Position: refs/heads/master@{#442259}

---
+++ chrome/test/data/predictors/javascript_redirect.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function getUrlParameters() {
+  var result = {};
+  var parts = window.location.search.substring(1).split('&');
+  for (var i = 0; i < parts.length; i++) {
+    var pair = parts[i].split('=');
+    result[pair[0]] = decodeURIComponent(pair[1]);
+  }
+  return result;
+}
+
+var params = getUrlParameters();
+var redirectUrl = params["url"];
+location.href = redirectUrl;
