7a620f9b5e41f9529f767336a39c01a504b62c34
a-v-y <a-v-y@yandex-team.ru>
2017-01-09 09:27:00 -0800

    Updated GetPrepopulatedEngines function interface
    Since many places ignore GetPrepopulatedEngines second output param,
    |default_search_provider_index| passed by pointer, made it optional,
    allow nullptr.
    
    Review-Url: https://codereview.chromium.org/2601683002
    Cr-Commit-Position: refs/heads/master@{#442282}

--- components/search_engines/default_search_manager.cc
+++ components/search_engines/default_search_manager.cc
@@ -201,10 +201,9 @@ void DefaultSearchManager::MergePrefsDataWithPrepopulated() {
   if (!prefs_default_search_ || !prefs_default_search_->prepopulate_id)
     return;
 
-  size_t default_search_index;
   std::vector<std::unique_ptr<TemplateURLData>> prepopulated_urls =
       TemplateURLPrepopulateData::GetPrepopulatedEngines(pref_service_,
-                                                         &default_search_index);
+                                                         nullptr);
 
   for (auto& engine : prepopulated_urls) {
     if (engine->prepopulate_id != prefs_default_search_->prepopulate_id)
