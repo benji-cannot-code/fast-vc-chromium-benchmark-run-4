2b4d3304d3d1a6384c27335fa9e6211175d921f9
fdoray <fdoray@chromium.org>
2017-01-09 13:34:15 -0800

    Use TaskScheduler instead of WorkerPool in address_sorter_win.cc.
    The following traits are used:
    
    Priority: Inherited (default)
      The priority is inherited from the calling context (i.e. TaskTraits
      are initialized with the priority of the current task).
    
    Shutdown behavior: CONTINUE_ON_SHUTDOWN
      Tasks posted with this mode which have not started executing before
      shutdown is initiated will never run. Tasks with this mode running at
      shutdown will be ignored (the worker will not be joined).
    
      Note: Tasks that were previously posted to base::WorkerPool should
      use this shutdown behavior because this is how base::WorkerPool
      handles all its tasks.
    
    May Block:
      Tasks posted with MayBlock() may block. This includes but is not
      limited to tasks that wait on synchronous file I/O operations:
      read or write a file from disk, interact with a pipe or a socket,
      rename or delete a file, enumerate files in a directory, etc. This
      trait isn't required for the mere use of locks.
    
    BUG=659191
    
    Review-Url: https://codereview.chromium.org/2607283002
    Cr-Commit-Position: refs/heads/master@{#442356}

--- net/dns/address_sorter_win.cc
+++ net/dns/address_sorter_win.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/memory/free_deleter.h"
-#include "base/threading/worker_pool.h"
+#include "base/task_scheduler/post_task.h"
 #include "net/base/address_list.h"
 #include "net/base/ip_address.h"
 #include "net/base/ip_endpoint.h"
@@ -40,7 +40,7 @@ class AddressSorterWin : public AddressSorter {
   }
 
  private:
-  // Executes the SIO_ADDRESS_LIST_SORT ioctl on the WorkerPool, and
+  // Executes the SIO_ADDRESS_LIST_SORT ioctl asynchronously, and
   // performs the necessary conversions to/from AddressList.
   class Job : public base::RefCountedThreadSafe<Job> {
    public:
@@ -74,21 +74,19 @@ class AddressSorterWin : public AddressSorter {
         input_buffer_->Address[i].iSockaddrLength = addr_len;
       }
 
-      if (!base::WorkerPool::PostTaskAndReply(
-          FROM_HERE,
-          base::Bind(&Job::Run, this),
-          base::Bind(&Job::OnComplete, this),
-          false /* task is slow */)) {
-        LOG(ERROR) << "WorkerPool::PostTaskAndReply failed";
-        OnComplete();
-      }
+      base::PostTaskWithTraitsAndReply(
+          FROM_HERE, base::TaskTraits()
+                         .WithShutdownBehavior(
+                             base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN)
+                         .MayBlock(),
+          base::Bind(&Job::Run, this), base::Bind(&Job::OnComplete, this));
     }
 
    private:
     friend class base::RefCountedThreadSafe<Job>;
     ~Job() {}
 
-    // Executed on the WorkerPool.
+    // Executed asynchronously in TaskScheduler.
     void Run() {
       SOCKET sock = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
       if (sock == INVALID_SOCKET)
