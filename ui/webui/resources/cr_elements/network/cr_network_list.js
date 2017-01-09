5f43c5afbdf0f302ad26fcf6daeaed85ff85df4f
stevenjb <stevenjb@chromium.org>
2017-01-09 14:07:36 -0800

    MD Settings: Elim unused show-separator from cr-network-list
    A bit of cleanup. The separators were from an older UX design.
    
    BUG=none
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2614713003
    Cr-Commit-Position: refs/heads/master@{#442366}

--- ui/webui/resources/cr_elements/network/cr_network_list.js
+++ ui/webui/resources/cr_elements/network/cr_network_list.js
@@ -52,13 +52,6 @@ Polymer({
       reflectToAttribute: true,
     },
 
-    /** Whether to show separators between all items. */
-    showSeparators: {
-      type: Boolean,
-      value: false,
-      reflectToAttribute: true,
-    },
-
     /**
      * Reflects the iron-list selecteditem property.
      * @type {!CrNetworkList.CrNetworkListItemType}
@@ -91,7 +84,7 @@ Polymer({
   getItems_: function() {
     let customItems = this.customItems.slice();
     // Flag the first custom item with isFirstCustomItem = true.
-    if (!this.showSeparators && customItems.length > 0)
+    if (customItems.length > 0)
       customItems[0].isFirstCustomItem = true;
     return this.networks.concat(customItems);
   },
--- ui/webui/resources/cr_elements/network/cr_network_select.js
+++ ui/webui/resources/cr_elements/network/cr_network_select.js
@@ -65,15 +65,6 @@ Polymer({
     },
 
     /**
-     * Show separators between all items.
-     */
-    showSeparators: {
-      type: Boolean,
-      value: false,
-      reflectToAttribute: true,
-    },
-
-    /**
      * List of all network state data for all visible networks.
      * @type {!Array<!CrOnc.NetworkStateProperties>}
      */
