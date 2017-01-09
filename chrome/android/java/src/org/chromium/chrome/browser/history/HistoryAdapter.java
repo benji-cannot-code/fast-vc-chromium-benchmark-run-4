d363441f49c13d87f60f22602c8e27a8fe579930
twellington <twellington@chromium.org>
2017-01-09 09:20:59 -0800

    [Android History] Add more tests
    Add tests for opening items, onHistoryDeleted(), privacy disclaimers,
    search, and loading more items.
    
    BUG=654071
    
    Review-Url: https://codereview.chromium.org/2619433002
    Cr-Commit-Position: refs/heads/master@{#442280}

--- chrome/android/java/src/org/chromium/chrome/browser/history/HistoryAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/history/HistoryAdapter.java
@@ -15,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import org.chromium.base.ContextUtils;
+import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.history.HistoryProvider.BrowsingHistoryObserver;
 import org.chromium.chrome.browser.widget.DateDividedAdapter;
@@ -275,5 +276,20 @@ public class HistoryAdapter extends DateDividedAdapter implements BrowsingHistor
         mSignedInSyncedTextView.setVisibility(mHasSyncedData ? View.VISIBLE : View.GONE);
         mOtherFormsOfBrowsingHistoryTextView.setVisibility(
                 mHasOtherFormsOfBrowsingData ? View.VISIBLE : View.GONE);
+    }
+
+    @VisibleForTesting
+    TextView getSignedInNotSyncedViewForTests() {
+        return mSignedInNotSyncedTextView;
+    }
+
+    @VisibleForTesting
+    TextView getSignedInSyncedViewForTests() {
+        return mSignedInSyncedTextView;
+    }
+
+    @VisibleForTesting
+    TextView getOtherFormsOfBrowsingHistoryViewForTests() {
+        return mOtherFormsOfBrowsingHistoryTextView;
     }
 }
