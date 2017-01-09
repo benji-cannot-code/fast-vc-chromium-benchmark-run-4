d8bf783281092dcba0ced971f748896610976ade
lilyhoughton <lilyhoughton@google.com>
2017-01-09 11:42:02 -0800

    make startNetLogToFile generate full path before passing to C++ function
    additionally, this adds a couple lines to the C++ backend so that it branches on
    whether the input file is relative or absolute, and leaves absolute paths unchanged
    
    BUG=616918
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_cronet_tester
    
    Review-Url: https://codereview.chromium.org/2465303002
    Cr-Commit-Position: refs/heads/master@{#442314}

--- components/cronet/ios/cronet_environment.h
+++ components/cronet/ios/cronet_environment.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/files/file_path.h"
+#include "base/files/scoped_file.h"
 #include "base/macros.h"
 #include "base/message_loop/message_loop.h"
 #include "base/strings/sys_string_conversions.h"
@@ -58,7 +59,7 @@ class CronetEnvironment {
 
   // Creates a new net log (overwrites existing file with this name). If
   // actively logging, this call is ignored.
-  void StartNetLog(base::FilePath::StringType file_name, bool log_bytes);
+  bool StartNetLog(base::FilePath::StringType file_name, bool log_bytes);
   // Stops logging and flushes file. If not currently logging this call is
   // ignored.
   void StopNetLog();
@@ -112,8 +113,7 @@ class CronetEnvironment {
                                     const base::Closure& task);
 
   // Helper methods that start/stop net logging on the network thread.
-  void StartNetLogOnNetworkThread(const base::FilePath::StringType& file_name,
-                                  bool log_bytes);
+  void StartNetLogOnNetworkThread(base::ScopedFILE file, bool log_bytes);
   void StopNetLogOnNetworkThread(base::WaitableEvent* log_stopped_event);
 
   // Returns the HttpNetworkSession object from the passed in
