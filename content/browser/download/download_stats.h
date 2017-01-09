8bbe8be8203bda219a6ff37bd139137eadb639c6
asanka <asanka@chromium.org>
2017-01-09 09:12:02 -0800

    [Download] Measure page transition for downloads
    Automatic top-level page transition are unlikely sources for intentional
    downloads. If the count of downloads initiated via this transition type
    is small, we should consider blocking automatic top-level transitions
    from initiating downloads.
    
    For a list of possible sources of auto top-level transitions, see
    //ui/base/page_transition_types.h. In addition to that list, automatic
    transitions also happen when opening URLs via command line in Chrome.
    See issue 334474 for one possible consequence of this.
    
    As a first step, start measuring the page transition type.
    
    BUG=334474
    BUG=7445
    
    Review-Url: https://codereview.chromium.org/2595863003
    Cr-Commit-Position: refs/heads/master@{#442277}

--- content/browser/download/download_stats.h
+++ content/browser/download/download_stats.h
@@ -14,9 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/optional.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/download_danger_type.h"
 #include "content/public/browser/download_interrupt_reasons.h"
+#include "ui/base/page_transition_types.h"
 #include "url/gurl.h"
 
 namespace base {
@@ -241,6 +243,9 @@ void RecordOriginStateOnResumption(bool is_partial,
 
 void RecordDownloadConnectionSecurity(const GURL& download_url,
                                       const std::vector<GURL>& url_chain);
+
+void RecordDownloadSourcePageTransitionType(
+    const base::Optional<ui::PageTransition>& transition);
 
 }  // namespace content
 
