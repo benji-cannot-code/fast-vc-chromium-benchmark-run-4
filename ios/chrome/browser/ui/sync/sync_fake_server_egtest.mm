4edf27b53da2b64d0e30d1cd1f5b361b741f7d59
pavely <pavely@chromium.org>
2017-01-10 10:20:21 -0800

    Increase kSyncOperationTimeout to let SyncFakeServerTest pass on slow lab machines
    testSyncTypedURLDeleteFromServer test failed with timeout waiting for
    FakeServer to receive updates. I'm trying to verify if it happened because of
    the slow hardware in the lab. To do so I'm increasing kSyncOperationTimeout to
    10 seconds.
    
    BUG=672286
    R=bzanotti@chromium.org
    
    Review-Url: https://codereview.chromium.org/2624543003
    Cr-Commit-Position: refs/heads/master@{#442633}

--- ios/chrome/browser/ui/sync/sync_fake_server_egtest.mm
+++ ios/chrome/browser/ui/sync/sync_fake_server_egtest.mm
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Constant for timeout while waiting for asynchronous sync operations.
-const NSTimeInterval kSyncOperationTimeout = 5.0;
+const NSTimeInterval kSyncOperationTimeout = 10.0;
 
 // Returns a fake identity.
 ChromeIdentity* GetFakeIdentity1() {
