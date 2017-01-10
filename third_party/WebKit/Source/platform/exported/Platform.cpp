beac8d5632b01463c94bbae38c21524a1ae03209
tzik <tzik@chromium.org>
2017-01-10 04:33:24 -0800

    Initialize Partition allocator in Platform constructor
    PartitionAlloc was initialized in Platform::initialize in the Blink
    initialization sequence, and before that, any object cannot created using
    PartitionAlloc. Specifically, objects tagged as USING_FAST_MALLOC cannot
    allocated. That prevents Platform to have such a object.
    
    This CL moves the initialization of PartitionAlloc from Platform::initialize
    to the constructor of Platform. So that Platform object can make objects
    in PartitionAlloc.
    
    Review-Url: https://codereview.chromium.org/2623833002
    Cr-Commit-Position: refs/heads/master@{#442559}

--- third_party/WebKit/Source/platform/exported/Platform.cpp
+++ third_party/WebKit/Source/platform/exported/Platform.cpp
@@ -49,8 +49,6 @@ static Platform* s_platform = nullptr;
 
 static GCTaskRunner* s_gcTaskRunner = nullptr;
 
-Platform::Platform() : m_mainThread(0) {}
-
 static void maxObservedSizeFunction(size_t sizeInMB) {
   const size_t supportedMaxSizeInMB = 4 * 1024;
   if (sizeInMB >= supportedMaxSizeInMB)
@@ -70,13 +68,16 @@ static void callOnMainThreadFunction(WTF::MainThreadFunction function,
       crossThreadBind(function, crossThreadUnretained(context)));
 }
 
+Platform::Platform() : m_mainThread(0) {
+  WTF::Partitions::initialize(maxObservedSizeFunction);
+}
+
 void Platform::initialize(Platform* platform) {
   ASSERT(!s_platform);
   ASSERT(platform);
   s_platform = platform;
   s_platform->m_mainThread = platform->currentThread();
 
-  WTF::Partitions::initialize(maxObservedSizeFunction);
   WTF::initialize(callOnMainThreadFunction);
 
   ProcessHeap::init();
