20a5ac30982efe7e6b82eed1fb0bdc9c1cb246d9
rtoy <rtoy@chromium.org>
2017-01-10 12:22:29 -0800

    Convert AudioBufferSource tests using Audit to testharness
    Only the tests using Audit are converted here, using the script:
    
    for f in `grep -l Audit *.html`
    do
      # Replace js-test with testharness and add testharnessreport.js
      sed -i 's;\(.*\)\(<script.*\)js-test.js\(.*\);\1\2testharness.js\3\
    \1\2testharnessreport.js\3 ;' $f
      # Remove old js-test methods that no longer exist.
      sed -i '/^[ 	]*\(description(".*")\|finishJSTest()\);[ 	]*/d' $f
      # Remove jsTestIsAsync stuff
      sed -i '/^[ 	]*window.jsTestIsAsync *= *true;[ 	]*$/d' $f
      # Remove expected results file.
      base=`basename $f .html`
      git rm $base-expected.txt
    done
    
    Additional manual changes were needed to replace methods defined by
    js-test.  A few tests needed updated expectations due to how
    audit-util.js needed to be changed for these tests.
    
    BUG=676355
    TEST=
    
    Review-Url: https://codereview.chromium.org/2593043003
    Cr-Commit-Position: refs/heads/master@{#442672}

--- third_party/WebKit/LayoutTests/webaudio/resources/audiobuffersource-testing.js
+++ third_party/WebKit/LayoutTests/webaudio/resources/audiobuffersource-testing.js
@@ -46,8 +46,8 @@ function checkSingleTest(renderedBuffer, i) {
                 renderedArray[j] = renderedData[offsetFrame + j];
 
             var s = description + ": expected: " + expected + " actual: " + renderedArray;
-            testFailed(s);
             success = false;
+            Should(s, success).beEqualTo(true);
             break;
         }
     }
@@ -61,14 +61,14 @@ function checkSingleTest(renderedBuffer, i) {
                 renderedArray[j - framesToTest] = renderedData[offsetFrame + j];
 
             var s = description + ": expected: all zeroes actual: " + renderedArray;
-            testFailed(s);
             success = false;
+            Should(s, success).beEqualTo(true);
             break;
         }
     }
 
-    if (success)
-        testPassed(description);
+    Should("", success)
+        .summarize(description, description);
 
     return success;
 }
