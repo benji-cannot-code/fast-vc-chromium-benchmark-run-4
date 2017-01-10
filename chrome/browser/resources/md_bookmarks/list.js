b54c27f6a76916509c7dc4e1b573cfd87cee598f
jiaxi <jiaxi@google.com>
2017-01-09 20:37:41 -0800

    [MD Bookmarks] Edit Bookmarks.
    This CL enables edit function for bookmarks. When the edit button in the
    dropdown menu for bookmarks is tapped, it will open an edit dialog.
    
    BUG=651980
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2617943002
    Cr-Commit-Position: refs/heads/master@{#442470}

--- chrome/browser/resources/md_bookmarks/list.js
+++ chrome/browser/resources/md_bookmarks/list.js
@@ -33,6 +33,8 @@ Polymer({
   /** @private */
   onEditTap_: function() {
     this.closeDropdownMenu_();
+    if (this.menuItem_.url)
+      this.$.editBookmark.showModal();
   },
 
   /** @private */
@@ -56,6 +58,20 @@ Polymer({
       }.bind(this));
     }
     this.closeDropdownMenu_();
+  },
+
+  /** @private */
+  onSaveEditTap_: function() {
+    chrome.bookmarks.update(this.menuItem_.id, {
+      'title': this.menuItem_.title,
+      'url': this.menuItem_.url,
+    });
+    this.$.editBookmark.close();
+  },
+
+  /** @private */
+  onCancelEditTap_: function() {
+    this.$.editBookmark.cancel();
   },
 
   /** @private */
