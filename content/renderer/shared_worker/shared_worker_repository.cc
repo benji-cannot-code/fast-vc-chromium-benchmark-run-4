6531fb80d468d84f6bb17ca941a99dffdaea0278
nhiroki <nhiroki@chromium.org>
2017-01-10 00:13:30 -0800

    SharedWorker: Move classes under content/renderer to content/renderer/shared_worker
    There would be no reason to place SharedWorkerRepository and
    WebSharedWorkerProxy under content/renderer. This CL moves them into
    content/renderer/shared_worker.
    
    BUG=612308
    
    Review-Url: https://codereview.chromium.org/2623613004
    Cr-Commit-Position: refs/heads/master@{#442532}

--- content/renderer/shared_worker/shared_worker_repository.cc
+++ content/renderer/shared_worker/shared_worker_repository.cc
@@ -3,12 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/renderer/shared_worker_repository.h"
+#include "content/renderer/shared_worker/shared_worker_repository.h"
 
 #include "content/child/child_thread_impl.h"
 #include "content/common/view_messages.h"
 #include "content/renderer/render_frame_impl.h"
-#include "content/renderer/websharedworker_proxy.h"
+#include "content/renderer/shared_worker/websharedworker_proxy.h"
 
 namespace content {
 
--- content/renderer/shared_worker/shared_worker_repository.h
+++ content/renderer/shared_worker/shared_worker_repository.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_RENDERER_SHARED_WORKER_REPOSITORY_H_
-#define CONTENT_RENDERER_SHARED_WORKER_REPOSITORY_H_
+#ifndef CONTENT_RENDERER_SHARED_WORKER_SHARED_WORKER_REPOSITORY_H_
+#define CONTENT_RENDERER_SHARED_WORKER_SHARED_WORKER_REPOSITORY_H_
 
 #include <memory>
 #include <set>
@@ -46,4 +46,4 @@ class SharedWorkerRepository final
 
 }  // namespace content
 
-#endif  // CONTENT_RENDERER_SHARED_WORKER_REPOSITORY_H_
+#endif  // CONTENT_RENDERER_SHARED_WORKER_SHARED_WORKER_REPOSITORY_H_
