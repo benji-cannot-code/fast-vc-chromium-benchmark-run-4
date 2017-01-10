bebe7fda3cf67f99eeb6ad8b0088633e00503345
ltian <ltian@chromium.org>
2017-01-10 10:48:06 -0800

    [Android] Fix bugs for menu item display in CCT
    Fix several bugs related to the menu items displayed in CCT, including:
    1. The text message shown on the snackbar for bookmarked page is
    incorrect. Current text message displayed is "Open in [Chrome Dev]" and
    the correct one should be "Bookmarked in [Chrome Dev"].
    2. "Find in page", "Add to Home screen",  "Request desktop site" and
    "Open in browser" items should not be displayed in Media Viewer CCT.
    Hide these items if it is Media Viewer and also add a test case for it.
    3. Disable download item if CCT loads a chrome native page.
    
    BUG=679255,679256
    
    Review-Url: https://codereview.chromium.org/2620763004
    Cr-Commit-Position: refs/heads/master@{#442638}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkUtils.java
@@ -94,7 +94,7 @@ public class BookmarkUtils {
                 if (fromCustomTab) {
                     String packageLabel = BuildInfo.getPackageLabel(activity);
                     snackbar = Snackbar.make(
-                            activity.getString(R.string.menu_open_in_product, packageLabel),
+                            activity.getString(R.string.bookmark_page_saved, packageLabel),
                             snackbarController, Snackbar.TYPE_ACTION, Snackbar.UMA_BOOKMARK_ADDED);
                 } else {
                     snackbar = Snackbar.make(
