22059d51650d8ad34fca4ef8b071f3cf49ba1cd0
chcunningham <chcunningham@chromium.org>
2017-01-10 13:50:22 -0800

    MSE: Fix logic bugs with high precision duration
    Upon end of stream, chunk demuxer should decrease the duration to
    match the highest buffered end time. This was not happening for
    user-set durations which exceeded the highest buffered end time by
    less than a microsecond.
    
    Additionally, some style refactoring. For MSE, we pull duration
    directly from the demuxer, which stores the user set value with full
    precision. Moving this logic to WMPI from HTMLMediaElement ensures
    duration WMPI uses matches that of HTMLMediaElement.
    
    TEST=New layout test and manual testing of bug report steps
    BUG=669997
    
    Review-Url: https://codereview.chromium.org/2581533002
    Cr-Commit-Position: refs/heads/master@{#442699}

--- third_party/WebKit/Source/core/html/shadow/MediaControlsOrientationLockDelegateTest.cpp
+++ third_party/WebKit/Source/core/html/shadow/MediaControlsOrientationLockDelegateTest.cpp
@@ -342,6 +342,10 @@ TEST_F(MediaControlsOrientationLockDelegateTest,
   simulateEnterFullscreen();
   // State set to PendingMetadata.
 
+  // Set up the WebMediaPlayer instance.
+  video().setSrc("http://example.com");
+  testing::runPendingTasks();
+
   simulateVideoNetworkState(HTMLMediaElement::kNetworkIdle);
   simulateVideoReadyState(HTMLMediaElement::kHaveMetadata);
   testing::runPendingTasks();
@@ -356,6 +360,10 @@ TEST_F(MediaControlsOrientationLockDelegateTest, ReceivedMetadataLater) {
   simulateEnterFullscreen();
   // State set to PendingMetadata.
   simulateExitFullscreen();
+
+  // Set up the WebMediaPlayer instance.
+  video().setSrc("http://example.com");
+  testing::runPendingTasks();
 
   simulateVideoNetworkState(HTMLMediaElement::kNetworkIdle);
   simulateVideoReadyState(HTMLMediaElement::kHaveMetadata);
