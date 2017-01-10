b0d8f0017ec140109a65e135555efe007b204b1b
ltian <ltian@chromium.org>
2017-01-09 16:35:39 -0800

    Make TemplateUrlServiceAndroid as a friend of TemplateUrlService
    Change TemplateUrlServiceAndroid to be a friend of TemplateUrlService
    class and hide its UpdateTemplateURLVisitTime() as a privarte function.
    
    Also add some comments to the fuctions for testing in
    template_url_serivce_android.h.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2616233002
    Cr-Commit-Position: refs/heads/master@{#442423}

--- components/search_engines/template_url_service.h
+++ components/search_engines/template_url_service.h
@@ -245,9 +245,6 @@ class TemplateURLService : public WebDataServiceConsumer,
                         const base::string16& keyword,
                         const std::string& search_url);
 
-  // Updates the last_visited time of |url| to the current time.
-  void UpdateTemplateURLVisitTime(TemplateURL* url);
-
   // Return true if the given |url| can be made the default. This returns false
   // regardless of |url| if the default search provider is managed by policy or
   // controlled by an extension.
@@ -430,6 +427,7 @@ class TemplateURLService : public WebDataServiceConsumer,
 
   friend class InstantUnitTestBase;
   friend class TemplateURLServiceTestUtil;
+  friend class TemplateUrlServiceAndroid;
 
   using GUIDToTURL = std::map<std::string, TemplateURL*>;
 
@@ -573,6 +571,9 @@ class TemplateURLService : public WebDataServiceConsumer,
   // For each TemplateURL whose url matches the visited url
   // SetKeywordSearchTermsForURL is invoked.
   void UpdateKeywordSearchTermsForURL(const URLVisitedDetails& details);
+
+  // Updates the last_visited time of |url| to the current time.
+  void UpdateTemplateURLVisitTime(TemplateURL* url);
 
   // If necessary, generates a visit for the site http:// + t_url.keyword().
   void AddTabToSearchVisit(const TemplateURL& t_url);
