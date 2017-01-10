a57ecb72e94de9be7ebada194e314872bac5810c
stkhapugin <stkhapugin@chromium.org>
2017-01-10 10:17:51 -0800

    [ObjC ARC] Converts ios/chrome/browser/device_sharing:unit_tests to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2623533003
    Cr-Commit-Position: refs/heads/master@{#442632}

--- ios/chrome/browser/device_sharing/device_sharing_manager_unittest.mm
+++ ios/chrome/browser/device_sharing/device_sharing_manager_unittest.mm
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/mac/foundation_util.h"
-#include "base/mac/scoped_nsobject.h"
 #import "components/handoff/handoff_manager.h"
 #include "components/handoff/pref_names_ios.h"
 #include "components/sync_preferences/testing_pref_service_syncable.h"
@@ -18,6 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "third_party/ocmock/OCMock/OCMock.h"
 #import "third_party/ocmock/gtest_support.h"
 #include "url/gurl.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
 
 @interface TestDeviceSharingManager : DeviceSharingManager
 + (HandoffManager*)createHandoffManager;
@@ -42,19 +45,19 @@ class DeviceSharingManagerTest : public PlatformTest {
     PlatformTest::SetUp();
     TestChromeBrowserState::Builder mainBrowserStateBuilder;
     chrome_browser_state_ = mainBrowserStateBuilder.Build();
-    sharing_manager_.reset([[TestDeviceSharingManager alloc] init]);
+    sharing_manager_ = [[TestDeviceSharingManager alloc] init];
   }
 
   void TearDown() override {
     [sharing_manager_ updateBrowserState:NULL];
-    sharing_manager_.reset();
+    sharing_manager_ = nil;
   }
 
   const GURL kTestURL1;
   const GURL kTestURL2;
   web::TestWebThreadBundle thread_bundle_;
   std::unique_ptr<TestChromeBrowserState> chrome_browser_state_;
-  base::scoped_nsobject<DeviceSharingManager> sharing_manager_;
+  DeviceSharingManager* sharing_manager_;
 };
 
 TEST_F(DeviceSharingManagerTest, NoMainBrowserState) {
