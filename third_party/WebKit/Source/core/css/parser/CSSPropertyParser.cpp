05a852875bfaa708a66ef0610187f51abad890be
aazzam <aazzam@google.com>
2017-01-09 21:38:54 -0800

    Implements CSSPropertyAPI for the text-decoration-skip property.
    A part of Project Ribbon, separating the parsing logic for CSS
    properties from the parser into an API. This patch removes
    CSSPropertyTextDecorationSkip from the switch statement in
    parseSingleValue, and calls the API instead.
    
    A function pointer to the parseSingleValue function from the API for the
    page property is stored in a CSSPropertyDescriptor, and is called from
    CSSPropertyParser.
    
    This patch:
    - Adds CSSPropertyAPITextDecorationSkip.cpp to the BUILD.gn file.
    - Adds api_class flag to CSSProperties.in, which indicates that
      CSSPropertyAPITextDecorationSkip.h is generated.
    - Moves the parsing logic for page from CSSPropertyParser.cpp to
      CSSPropertyAPITextDecorationSkip.cpp, which implements
      CSSPropertyAPI.h.
    
    BUG=668012
    
    Review-Url: https://codereview.chromium.org/2611843006
    Cr-Commit-Position: refs/heads/master@{#442499}

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
@@ -1402,24 +1402,6 @@ static CSSValue* consumeTextDecorationLine(CSSParserTokenRange& range) {
   return list;
 }
 
-static CSSValue* consumeTextDecorationSkip(CSSParserTokenRange& range) {
-  CSSValueList* list = CSSValueList::createSpaceSeparated();
-  while (true) {
-    CSSIdentifierValue* ident =
-        consumeIdent<CSSValueObjects, CSSValueInk>(range);
-    if (!ident)
-      break;
-    if (list->hasValue(*ident))
-      return nullptr;
-    list->append(*ident);
-  }
-
-  if (!list->length())
-    return nullptr;
-
-  return list;
-}
-
 // none | strict | content | [ layout || style || paint || size ]
 static CSSValue* consumeContain(CSSParserTokenRange& range) {
   CSSValueID id = range.peek().id();
@@ -3527,9 +3509,6 @@ const CSSValue* CSSPropertyParser::parseSingleValue(
     case CSSPropertyWebkitTextDecorationsInEffect:
     case CSSPropertyTextDecorationLine:
       return consumeTextDecorationLine(m_range);
-    case CSSPropertyTextDecorationSkip:
-      DCHECK(RuntimeEnabledFeatures::css3TextDecorationsEnabled());
-      return consumeTextDecorationSkip(m_range);
     case CSSPropertyOffsetAnchor:
       return consumeOffsetAnchor(m_range, m_context.mode());
     case CSSPropertyOffsetPosition:
