908e24330294ba37d4346220ba8e2069422b536d
tonikitoo <tonikitoo@igalia.com>
2017-01-10 05:32:17 -0800

    Send an empty tooltip text when hovering over a different node
    Chromium differs from IE/Edge in the way it handles
    tooltip placement when moving the mouse cursor
    over different node elements that have the same tooltip
    text.
    
    - Chromium: as long that the tooltip text does not change,
    the tooltip banner stays stationary when hovering over
    different nodes.
    
    - IE/Edge: we the mouse cursor hovers over different node than
    before, the tooltip gets hidden regardless whether the its
    text has changed or not.
    
    This CL makes Chromium behave similarly to IE.
    
    Note: Although this CL sends an extra "clear tooltip text" IPC message,
    it intentionally only happens in one specific situation: when
    mouse cursor is hovering over a node different that before that
    *has* the same tooltip text as before, *and* the the text is
    non-empty.
    It is unlikely that this will flood the IPC channel with more
    tooltip messages.
    
    BUG=142002,157122,158320
    
    Review-Url: https://codereview.chromium.org/2592263003
    Cr-Commit-Position: refs/heads/master@{#442570}

--- third_party/WebKit/Source/core/page/ChromeClient.cpp
+++ third_party/WebKit/Source/core/page/ChromeClient.cpp
@@ -39,6 +39,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+DEFINE_TRACE(ChromeClient) {
+  visitor->trace(m_lastMouseOverNode);
+  HostWindow::trace(visitor);
+}
+
 void ChromeClient::setWindowRectWithAdjustment(const IntRect& pendingRect,
                                                LocalFrame& frame) {
   IntRect screen = screenInfo().availableRect;
@@ -191,8 +196,20 @@ void ChromeClient::setToolTip(LocalFrame& frame, const HitTestResult& result) {
   if (m_lastToolTipPoint == result.hitTestLocation().point() &&
       m_lastToolTipText == toolTip)
     return;
+
+  // If a tooltip was displayed earlier, and mouse cursor moves over
+  // a different node with the same tooltip text, make sure the previous
+  // tooltip is unset, so that it does not get stuck positioned relative
+  // to the previous node).
+  // The ::setToolTip overload, which is be called down the road,
+  // ensures a new tooltip to be displayed with the new context.
+  if (result.innerNodeOrImageMapImage() != m_lastMouseOverNode &&
+      !m_lastToolTipText.isEmpty() && toolTip == m_lastToolTipText)
+    clearToolTip(frame);
+
   m_lastToolTipPoint = result.hitTestLocation().point();
   m_lastToolTipText = toolTip;
+  m_lastMouseOverNode = result.innerNodeOrImageMapImage();
   setToolTip(frame, toolTip, toolTipDirection);
 }
 
