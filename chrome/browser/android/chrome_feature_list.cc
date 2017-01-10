78dac8bbf2a14463538a15a228fc000ea43cadb4
rouslan <rouslan@chromium.org>
2017-01-10 14:03:43 -0800

    Remove flag for OCR theme.
    This patch removes the flag for OCR dialog theme, which is no longer
    used.
    
    BUG=678642
    
    Review-Url: https://codereview.chromium.org/2620963003
    Cr-Commit-Position: refs/heads/master@{#442703}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -42,7 +42,6 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kAndroidPayIntegrationV1,
     &kAndroidPayIntegrationV2,
     &kAndroidPaymentApps,
-    &kAutofillScanThemeDialog,
     &kCCTExternalLinkHandling,
     &kCCTPostMessageAPI,
     &kChromeHomeFeature,
@@ -84,9 +83,6 @@ const base::Feature kAndroidPayIntegrationV2{"AndroidPayIntegrationV2",
 
 const base::Feature kAndroidPaymentApps{"AndroidPaymentApps",
                                         base::FEATURE_DISABLED_BY_DEFAULT};
-
-const base::Feature kAutofillScanThemeDialog{"AutofillScanThemeDialog",
-                                             base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kCCTExternalLinkHandling{"CCTExternalLinkHandling",
                                              base::FEATURE_ENABLED_BY_DEFAULT};
