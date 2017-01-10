1ac5cb53801817b34dac7bc206a0853e71d7035c
tmartino <tmartino@chromium.org>
2017-01-10 14:30:17 -0800

    Enabling kUseConsolidatedStartupFlow by default on trunk.
    Code changes:
    - Disabling onboarding content for ChromeOS. Promotions for CrOS are out-of-scope for this launch.
    - Ensuring that preference-specified tabs do not appear in new windows when a tabbed browser already exists for the profile.
    
    Test changes:
    - Removed 12 outdated tests
    - Fixed or updated 9 tests
    
    Details of affected tests, and rationale, can be found in this doc: https://docs.google.com/a/google.com/document/d/1PjAemEEFrl0gIDVQfnr1oAnHkb6DcV47HlOyyo7ICZI/edit?usp=sharing
    
    BUG=608875,314819,313856
    
    Review-Url: https://codereview.chromium.org/2475913003
    Cr-Original-Commit-Position: refs/heads/master@{#442487}
    Committed: https://chromium.googlesource.com/chromium/src/+/a52322134679a349366417e04e502bc8ce1f68e4
    Review-Url: https://codereview.chromium.org/2475913003
    Cr-Commit-Position: refs/heads/master@{#442712}

--- chrome/browser/app_controller_mac_browsertest.mm
+++ chrome/browser/app_controller_mac_browsertest.mm
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/bookmarks/test/bookmark_test_helpers.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/core/common/profile_management_switches.h"
+#include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_navigation_observer.h"
@@ -394,7 +395,8 @@ class AppControllerOpenShortcutBrowserTest : public InProcessBrowserTest {
 
 IN_PROC_BROWSER_TEST_F(AppControllerOpenShortcutBrowserTest,
                        OpenShortcutOnStartup) {
-  EXPECT_EQ(1, browser()->tab_strip_model()->count());
+  // The two tabs expected are the Welcome page and the desired URL.
+  EXPECT_EQ(2, browser()->tab_strip_model()->count());
   EXPECT_EQ(g_open_shortcut_url,
       browser()->tab_strip_model()->GetActiveWebContents()
           ->GetLastCommittedURL());
@@ -421,6 +423,15 @@ IN_PROC_BROWSER_TEST_F(AppControllerReplaceNTPBrowserTest,
   // Ensure that there is exactly 1 tab showing, and the tab is the NTP.
   GURL ntp(chrome::kChromeUINewTabURL);
   EXPECT_EQ(1, browser()->tab_strip_model()->count());
+  browser()->tab_strip_model()->GetActiveWebContents()->GetController().LoadURL(
+      GURL(chrome::kChromeUINewTabURL), content::Referrer(),
+      ui::PageTransition::PAGE_TRANSITION_LINK, std::string());
+
+  // Wait for one navigation on the active web contents.
+  content::TestNavigationObserver ntp_navigation_observer(
+      browser()->tab_strip_model()->GetActiveWebContents());
+  ntp_navigation_observer.Wait();
+
   EXPECT_EQ(ntp,
             browser()
                 ->tab_strip_model()
@@ -430,11 +441,10 @@ IN_PROC_BROWSER_TEST_F(AppControllerReplaceNTPBrowserTest,
   GURL simple(embedded_test_server()->GetURL("/simple.html"));
   SendAppleEventToOpenUrlToAppController(simple);
 
-  // Wait for one navigation on the active web contents.
   EXPECT_EQ(1, browser()->tab_strip_model()->count());
-  content::TestNavigationObserver obs(
-      browser()->tab_strip_model()->GetActiveWebContents(), 1);
-  obs.Wait();
+  content::TestNavigationObserver event_navigation_observer(
+      browser()->tab_strip_model()->GetActiveWebContents());
+  event_navigation_observer.Wait();
 
   EXPECT_EQ(simple,
             browser()
