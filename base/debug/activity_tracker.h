1884ed825141e7edef53286e5a828ea098c08f81
manzagop <manzagop@chromium.org>
2017-01-10 11:06:54 -0800

    Collect stability data from global and activity scopes
    BUG=620813
    
    Review-Url: https://codereview.chromium.org/2583223002
    Cr-Commit-Position: refs/heads/master@{#442647}

--- base/debug/activity_tracker.h
+++ base/debug/activity_tracker.h
@@ -133,7 +133,7 @@ extern const ActivityData kNullActivityData;
 // A helper class that is used for managing memory allocations within a
 // persistent memory allocator. Instances of this class are NOT thread-safe.
 // Use from a single thread or protect access with a lock.
-class ActivityTrackerMemoryAllocator {
+class BASE_EXPORT ActivityTrackerMemoryAllocator {
  public:
   using Reference = PersistentMemoryAllocator::Reference;
 
@@ -308,6 +308,8 @@ class BASE_EXPORT ActivityUserData {
     TypedValue(const TypedValue& other);
     ~TypedValue();
 
+    ValueType type() const { return type_; }
+
     // These methods return the extracted value in the correct format.
     StringPiece Get() const;
     StringPiece GetString() const;
@@ -330,10 +332,10 @@ class BASE_EXPORT ActivityUserData {
    private:
     friend class ActivityUserData;
 
-    ValueType type;
-    uint64_t short_value;    // Used to hold copy of numbers, etc.
-    std::string long_value;  // Used to hold copy of raw/string data.
-    StringPiece ref_value;   // Used to hold reference to external data.
+    ValueType type_;
+    uint64_t short_value_;    // Used to hold copy of numbers, etc.
+    std::string long_value_;  // Used to hold copy of raw/string data.
+    StringPiece ref_value_;   // Used to hold reference to external data.
   };
 
   using Snapshot = std::map<std::string, TypedValue>;
@@ -538,6 +540,7 @@ class BASE_EXPORT ThreadActivityTracker {
     // An identifier that indicates a specific activity on the stack.
     ActivityId activity_id_;
 
+   private:
     DISALLOW_COPY_AND_ASSIGN(ScopedActivity);
   };
 
