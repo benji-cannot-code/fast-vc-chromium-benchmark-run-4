ba81be83c9fd38c6d65dea480eb3b3cdabeac5c6
einbinder <einbinder@chromium.org>
2017-01-09 22:11:23 -0800

    DevTools: SuggestBox: Remove unneeded removeFromElement method
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2615213005
    Cr-Commit-Position: refs/heads/master@{#442511}

--- third_party/WebKit/Source/devtools/front_end/ui/SuggestBox.js
+++ third_party/WebKit/Source/devtools/front_end/ui/SuggestBox.js
@@ -198,10 +198,6 @@ UI.SuggestBox = class {
     delete this._lastAnchorBox;
   }
 
-  removeFromElement() {
-    this.hide();
-  }
-
   /**
    * @param {boolean=} isIntermediateSuggestion
    * @return {boolean}
