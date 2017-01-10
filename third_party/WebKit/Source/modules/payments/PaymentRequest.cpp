d2b2edd4c8d1a563d5a6a2021482968eacd3146e
sammc <sammc@chromium.org>
2017-01-09 22:14:04 -0800

    Remove mojo::WTFArray.
    BUG=674766
    
    Review-Url: https://codereview.chromium.org/2604113002
    Cr-Commit-Position: refs/heads/master@{#442513}

--- third_party/WebKit/Source/modules/payments/PaymentRequest.cpp
+++ third_party/WebKit/Source/modules/payments/PaymentRequest.cpp
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/payments/PaymentShippingOption.h"
 #include "modules/payments/PaymentsValidators.h"
 #include "mojo/public/cpp/bindings/interface_request.h"
-#include "mojo/public/cpp/bindings/wtf_array.h"
 #include "platform/RuntimeEnabledFeatures.h"
 #include "platform/mojo/MojoHelper.h"
 #include "public/platform/InterfaceProvider.h"
