d147f60a29d6c82b4c37d3ffd96891d912252982
mikhail.pozdnyakov <mikhail.pozdnyakov@intel.com>
2017-01-10 01:14:11 -0800

    media::SilentSinkSuspender should simulate |delay| and |delay_timestamp|
    This patch provides estimation of the |delay| and |delay_timestamp| values for the wrapped |callback_|, otherwise the implementation of the 'AudioContext.getOutputTimestamp()' [1] returns inappropriate values when the "fake sink" is used.
    
    BUG=619533
    
    [1] https://codereview.chromium.org/2060833002/
    
    Review-Url: https://codereview.chromium.org/2567143002
    Cr-Commit-Position: refs/heads/master@{#442536}

--- media/base/audio_bus_perftest.cc
+++ media/base/audio_bus_perftest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 
 static const int kBenchmarkIterations = 20;
+static const int kSampleRate = 48000;
 
 template <typename T>
 void RunInterleaveBench(AudioBus* bus, const std::string& trace_name) {
@@ -45,8 +46,8 @@ void RunInterleaveBench(AudioBus* bus, const std::string& trace_name) {
 
 // Benchmark the FromInterleaved() and ToInterleaved() methods.
 TEST(AudioBusPerfTest, Interleave) {
-  std::unique_ptr<AudioBus> bus = AudioBus::Create(2, 48000 * 120);
-  FakeAudioRenderCallback callback(0.2);
+  std::unique_ptr<AudioBus> bus = AudioBus::Create(2, kSampleRate * 120);
+  FakeAudioRenderCallback callback(0.2, kSampleRate);
   callback.Render(base::TimeDelta(), base::TimeTicks::Now(), 0, bus.get());
 
   RunInterleaveBench<int8_t>(bus.get(), "int8_t");
