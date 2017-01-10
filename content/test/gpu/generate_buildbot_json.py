830d0343bbc5f7b3948b69e7188708164544d200
jbauman <jbauman@chromium.org>
2017-01-09 17:46:49 -0800

    Reland of Run video_decode_accelerator_unittest on windows GPU bots.
    This tests our hardware video decoding path.
    
    BUG=663126,676053
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2617263002
    Cr-Commit-Position: refs/heads/master@{#442445}

--- content/test/gpu/generate_buildbot_json.py
+++ content/test/gpu/generate_buildbot_json.py
@@ -1090,6 +1090,16 @@ COMMON_GTESTS = {
       'tab_capture_end2end_tests_run',
     ],
   },
+  'video_decode_accelerator_unittest': {
+    'tester_configs': [
+      {
+        'os_types': ['win']
+      },
+    ],
+    'args': [
+      '--use-test-data-path',
+    ],
+  },
 }
 
 # This requires a hack because the isolate's name is different than
