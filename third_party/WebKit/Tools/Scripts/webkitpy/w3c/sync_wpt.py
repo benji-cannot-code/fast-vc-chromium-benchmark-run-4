3d0787ded2e6efe029a3f48a94e2b0d7bf72712f
jeffcarp <jeffcarp@chromium.org>
2017-01-10 19:42:21 -0800

    [WPT Export] Clean up TODOs
    BUG=657117
    R=qyearsley@chromium.org
    
    Review-Url: https://codereview.chromium.org/2622033002
    Cr-Commit-Position: refs/heads/master@{#442783}

--- third_party/WebKit/Tools/Scripts/webkitpy/w3c/sync_wpt.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/w3c/sync_wpt.py
@@ -5,11 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 """A script for exporting and importing changes between the Chromium repo
 and the web-platform-tests repo.
-
-TODO(jeffcarp): does not handle reverted changes right now
-TODO(jeffcarp): it also doesn't handle changes to -expected.html files
-TODO(jeffcarp): Currently this script only does export; also add an option
-import as well.
 """
 
 import argparse
