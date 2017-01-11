6c18db986d7d0132e6c1f7234aaa3ad6855c40b7
eroman <eroman@chromium.org>
2017-01-11 15:37:58 -0800

    Minor comment updates to CertVerify related code.
    Also removes an oddity with CertVerifyResult::operator== by handling the null case instead of documenting it as invalid.
    
    operator== was only used by a unit-test (components/cronet/android/cert/cert_verifier_cache_serializer_unittest.cc), so this generalization does not weaken nullability guarantees.
    
    Review-Url: https://codereview.chromium.org/2624283002
    Cr-Commit-Position: refs/heads/master@{#443054}

--- net/cert/cert_verify_result.h
+++ net/cert/cert_verify_result.h
@@ -27,7 +27,6 @@ class NET_EXPORT CertVerifyResult {
 
   void Reset();
 
-  // Comparing CertVerifyResult with a nullptr |verified_cert| is invalid.
   bool operator==(const CertVerifyResult& other) const;
 
   // The certificate chain that was constructed during verification.
@@ -54,7 +53,8 @@ class NET_EXPORT CertVerifyResult {
   // chain.
   CertStatus cert_status;
 
-  // Properties of the certificate chain.
+  // Hash algorithms used by the certificate chain, excluding the trust
+  // anchor.
   bool has_md2;
   bool has_md4;
   bool has_md5;
