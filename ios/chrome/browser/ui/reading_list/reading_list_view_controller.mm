9cec86c9668ddc1565201f28a9e4c761fcd58b85
gambard <gambard@chromium.org>
2017-01-11 02:31:14 -0800

    Remove the scroll if the Reading List is empty.
    BUG=678688, 680007, 680008
    
    Review-Url: https://codereview.chromium.org/2623853002
    Cr-Commit-Position: refs/heads/master@{#442857}

--- ios/chrome/browser/ui/reading_list/reading_list_view_controller.mm
+++ ios/chrome/browser/ui/reading_list/reading_list_view_controller.mm
@@ -125,6 +125,8 @@ using ItemsMapByDate = std::multimap<int64_t, ReadingListCollectionViewItem*>;
 // Returns whether there are elements in the section identified by
 // |sectionIdentifier|.
 - (BOOL)hasItemInSection:(SectionIdentifier)sectionIdentifier;
+// Adds an empty background if needed.
+- (void)collectionIsEmpty;
 // Updates the toolbar state according to the selected items.
 - (void)updateToolbarState;
 // Displays an action sheet to let the user choose to mark all the elements as
@@ -487,10 +489,9 @@ using ItemsMapByDate = std::multimap<int64_t, ReadingListCollectionViewItem*>;
   [super loadModel];
 
   if (self.readingListModel->size() == 0) {
-    // The collection is empty, add background.
-    self.collectionView.backgroundView = _emptyCollectionBackground;
-    [self.audience setCollectionHasItems:NO];
+    [self collectionIsEmpty];
   } else {
+    self.collectionView.alwaysBounceVertical = YES;
     [self loadItems];
     self.collectionView.backgroundView = nil;
     [self.audience setCollectionHasItems:YES];
@@ -621,6 +622,16 @@ using ItemsMapByDate = std::multimap<int64_t, ReadingListCollectionViewItem*>;
       [self.collectionViewModel numberOfItemsInSection:section];
 
   return numberOfItems > 0;
+}
+
+- (void)collectionIsEmpty {
+  if (self.collectionView.backgroundView) {
+    return;
+  }
+  // The collection is empty, add background.
+  self.collectionView.alwaysBounceVertical = NO;
+  self.collectionView.backgroundView = _emptyCollectionBackground;
+  [self.audience setCollectionHasItems:NO];
 }
 
 #pragma mark - ReadingListToolbarDelegate
@@ -1084,6 +1095,9 @@ using ItemsMapByDate = std::multimap<int64_t, ReadingListCollectionViewItem*>;
     }
   }
                                 completion:nil];
+  if (_readingListModel->size() == 0) {
+    [self collectionIsEmpty];
+  }
 }
 
 - (void)exitEditingModeAnimated:(BOOL)animated {
