99bd184e0c77ef5525102ede7ed1aaf0c97c534b
aazzam <aazzam@google.com>
2017-01-11 04:03:35 -0800

    Implements CSSPropertyAPI for the outline-offset property.
    A part of Project Ribbon, separating the parsing logic for CSS
    properties from the parser into an API. This patch removes
    CSSPropertyOutlineOffset from the switch statement in parseSingleValue,
    and calls the API instead.
    
    A function pointer to the parseSingleValue function from the API for the
    page property is stored in a CSSPropertyDescriptor, and is called from
    CSSPropertyParser.
    
    This patch:
    - Adds CSSPropertyAPIOutlineOffset.cpp to the BUILD.gn file.
    - Adds api_class flag to CSSProperties.in, which indicates that
      CSSPropertyAPIOutlineOffset.h is generated.
    - Moves the parsing logic for page from CSSPropertyParser.cpp to
      CSSPropertyAPIOutlineOffset.cpp, which implements
      CSSPropertyAPI.h.
    
    BUG=668012
    
    Review-Url: https://codereview.chromium.org/2610813004
    Cr-Commit-Position: refs/heads/master@{#442876}

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
@@ -3443,8 +3443,6 @@ const CSSValue* CSSPropertyParser::parseSingleValue(
       return consumeTextEmphasisStyle(m_range);
     case CSSPropertyOutlineColor:
       return consumeOutlineColor(m_range, m_context.mode());
-    case CSSPropertyOutlineOffset:
-      return consumeLength(m_range, m_context.mode(), ValueRangeAll);
     case CSSPropertyOutlineWidth:
       return consumeLineWidth(m_range, m_context.mode(), UnitlessQuirk::Forbid);
     case CSSPropertyTransform:
