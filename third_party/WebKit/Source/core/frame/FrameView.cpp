603455d6c0cd409fd762689eb61faed930a5ab73
pdr <pdr@chromium.org>
2017-01-11 11:16:13 -0800

    Move "prePaint" under the "Update Layer Tree" devtools timeline step
    Slimming paint invalidation (--enable-slimming-paint-invalidation) moves
    paint invalidation to a new "pre-paint" document lifecycle stage. This
    patch moves prePaint under the "Update Layer Tree" timeline category, and
    adds a comment to move this under "Paint" for slimming paint v2 (see
    discussion in https://codereview.chromium.org/2623613008).
    
    BUG=679403
    
    Review-Url: https://codereview.chromium.org/2622983002
    Cr-Commit-Position: refs/heads/master@{#442975}

--- third_party/WebKit/Source/core/frame/FrameView.cpp
+++ third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -2939,10 +2939,12 @@ void FrameView::updateLifecyclePhasesInternal(
 
         updateCompositedSelectionIfNeeded();
       }
-    }
 
-    if (targetState >= DocumentLifecycle::PrePaintClean)
-      prePaint();
+      // TODO(pdr): prePaint should be under the "Paint" devtools timeline step
+      // for slimming paint v2.
+      if (targetState >= DocumentLifecycle::PrePaintClean)
+        prePaint();
+    }
 
     if (RuntimeEnabledFeatures::slimmingPaintV2Enabled())
       DocumentAnimations::updateAnimations(layoutView()->document());
