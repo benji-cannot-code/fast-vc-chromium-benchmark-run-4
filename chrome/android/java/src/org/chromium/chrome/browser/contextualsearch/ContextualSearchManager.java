60b135213cda115dd8dd343e9fb56c416fc571dc
twellington <twellington@chromium.org>
2017-01-11 14:12:02 -0800

    [Contextual Search] Do not log contextual cards data shown if quick action was shown
    If a quick action was shown, contextual cards data for a thumbnail/caption
    were not shown, so the histograms for whether contextual cards data
    was shown should log "false".
    
    BUG=680229
    
    Review-Url: https://codereview.chromium.org/2621413003
    Cr-Commit-Position: refs/heads/master@{#443006}

--- chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManager.java
@@ -697,7 +697,9 @@ public class ContextualSearchManager implements ContextualSearchManagementDelega
             doLiteralSearch = true;
         }
 
-        boolean receivedContextualCardsData = !TextUtils.isEmpty(caption)
+        boolean quickActionShown =
+                mSearchPanel.getSearchBarControl().getQuickActionControl().hasQuickAction();
+        boolean receivedContextualCardsData = !quickActionShown && !TextUtils.isEmpty(caption)
                 || !TextUtils.isEmpty(thumbnailUrl);
         if (ContextualSearchFieldTrial.shouldHideContextualCardsData()) {
             // Clear the thumbnail URL and caption so that they are not displayed in the bar. This
@@ -724,8 +726,6 @@ public class ContextualSearchManager implements ContextualSearchManagementDelega
         }
 
         if (ContextualSearchFieldTrial.isContextualSearchSingleActionsEnabled()) {
-            boolean quickActionShown =
-                    mSearchPanel.getSearchBarControl().getQuickActionControl().hasQuickAction();
             ContextualSearchUma.logQuickActionShown(quickActionShown, quickActionCategory);
             mSearchPanel.getPanelMetrics().setWasQuickActionShown(quickActionShown,
                     quickActionCategory);
