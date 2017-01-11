a7b5dc89c666886688b4f217ff4612a675641107
bauerb <bauerb@chromium.org>
2017-01-11 09:50:26 -0800

    Correctly hide the content suggestions menu item by default.
    Setting android:visible="false" only seems to work on groups,
    not individual items.
    
    Review-Url: https://codereview.chromium.org/2621203004
    Cr-Commit-Position: refs/heads/master@{#442950}

--- chrome/android/java/src/org/chromium/chrome/browser/appmenu/AppMenuPropertiesDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/appmenu/AppMenuPropertiesDelegate.java
@@ -193,11 +193,10 @@ public class AppMenuPropertiesDelegate {
 
             // Only display the standalone content suggestions UI if the corresponding feature
             // is enabled.
-            if (ChromeFeatureList.isEnabled(ChromeFeatureList.NTP_SUGGESTIONS_STANDALONE_UI)) {
-                MenuItem item = menu.findItem(R.id.content_suggestions_standalone_ui);
-                item.setTitle("🔰🆕👌");
-                item.setVisible(true);
-            }
+            MenuItem item = menu.findItem(R.id.content_suggestions_standalone_ui);
+            item.setTitle("🔰🆕👌");
+            item.setVisible(
+                    ChromeFeatureList.isEnabled(ChromeFeatureList.NTP_SUGGESTIONS_STANDALONE_UI));
         }
 
         if (isOverviewMenu) {
