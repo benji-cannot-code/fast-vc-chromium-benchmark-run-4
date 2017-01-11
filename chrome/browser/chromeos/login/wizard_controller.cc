fc6176a3512b9a3451dd433806caee9b552af64b
alemate <alemate@chromium.org>
2017-01-11 14:40:45 -0800

    ChromeOS MD-OOBE: Add HID detection screen.
    This CL implements HID detection screen for material design ChromeOS OOBE.
    
    BUG=604119
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2600983003
    Cr-Commit-Position: refs/heads/master@{#443023}

--- chrome/browser/chromeos/login/wizard_controller.cc
+++ chrome/browser/chromeos/login/wizard_controller.cc
@@ -604,8 +604,6 @@ void WizardController::ShowSupervisedUserCreationScreen() {
 
 void WizardController::ShowHIDDetectionScreen() {
   VLOG(1) << "Showing HID discovery screen.";
-  // TODO(drcrash): Remove this after testing (http://crbug.com/647411).
-  SetShowMdOobe(false);  // Disable the MD OOBE from there on.
   SetStatusAreaVisible(true);
   SetCurrentScreen(GetScreen(OobeScreen::SCREEN_OOBE_HID_DETECTION));
   // In HID detection screen, puts the Bluetooth in discoverable mode and waits
@@ -985,6 +983,8 @@ void WizardController::SetCurrentScreenSmooth(BaseScreen* new_current,
 
   previous_screen_ = current_screen_;
   current_screen_ = new_current;
+
+  oobe_ui_->UpdateLocalizedStringsIfNeeded();
 
   if (use_smoothing) {
     smooth_show_timer_.Start(
--- chrome/browser/ui/webui/chromeos/login/hid_detection_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/hid_detection_screen_handler.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/oobe_screen.h"
 #include "chrome/browser/chromeos/login/screens/core_oobe_actor.h"
 #include "chrome/browser/chromeos/login/screens/hid_detection_model.h"
+#include "chrome/browser/ui/webui/chromeos/login/oobe_ui.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/grit/generated_resources.h"
 #include "chromeos/chromeos_switches.h"
