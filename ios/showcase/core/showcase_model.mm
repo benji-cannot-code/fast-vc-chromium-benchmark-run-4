ecaf1ff46ddc55fb2efe91612a338614c2bca4bd
gambard <gambard@chromium.org>
2017-01-11 02:23:08 -0800

    Creates the Suggestions UI
    This CL creates the Suggestions UI with a Collection View.
    
    BUG=679369
    
    Review-Url: https://codereview.chromium.org/2619353004
    Cr-Commit-Position: refs/heads/master@{#442850}

--- ios/showcase/core/showcase_model.mm
+++ ios/showcase/core/showcase_model.mm
@@ -23,6 +23,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       showcase::kUseCaseKey : @"Main settings screen",
     },
     @{
+      showcase::kClassForDisplayKey : @"SuggestionsViewController",
+      showcase::kClassForInstantiationKey : @"SCSuggestionsCoordinator",
+      showcase::kUseCaseKey : @"New Suggestions UI",
+    },
+    @{
       showcase::kClassForDisplayKey : @"MenuViewController",
       showcase::kClassForInstantiationKey : @"MenuViewController",
       showcase::kUseCaseKey : @"Tools menu",
