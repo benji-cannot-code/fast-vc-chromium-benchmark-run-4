23768976c87a2df40c9ea8a370b155c24bcb98f0
rdevlin.cronin <rdevlin.cronin@chromium.org>
2017-01-11 11:40:30 -0800

    [Extensions] Enable sideloading prompt on mac on canary
    BUG=669277
    
    Review-Url: https://codereview.chromium.org/2623033002
    Cr-Commit-Position: refs/heads/master@{#442983}

--- chrome/browser/extensions/external_install_manager.h
+++ chrome/browser/extensions/external_install_manager.h
@@ -35,6 +35,9 @@ class ExternalInstallManager : public ExtensionRegistryObserver,
                          bool is_first_run);
   ~ExternalInstallManager() override;
 
+  // Returns true if prompting for external extensions is enabled.
+  static bool IsPromptingEnabled();
+
   // Removes the error associated with a given extension.
   void RemoveExternalInstallError(const std::string& extension_id);
 
