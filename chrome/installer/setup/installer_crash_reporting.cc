1a1ea943968a091b5deed2e7fcfa4a99dd59bb7d
grt <grt@chromium.org>
2017-01-11 04:32:23 -0800

    Multi-install cleanups in setup.
    Including:
    - Removes the "multi-install" crash key from setup.exe.
    - Removes some dead test code.
    - Removes duplicated code in IsActivelyUsedIn.
    
    BUG=577816
    
    Review-Url: https://codereview.chromium.org/2619963005
    Cr-Commit-Position: refs/heads/master@{#442883}

--- chrome/installer/setup/installer_crash_reporting.cc
+++ chrome/installer/setup/installer_crash_reporting.cc
@@ -33,7 +33,6 @@ namespace {
 // Crash Keys
 
 const char kCurrentVersion[] = "current-version";
-const char kIsMultiInstall[] = "multi-install";
 const char kIsSystemLevel[] = "system-level";
 const char kOperation[] = "operation";
 const char kStateKey[] = "state-key";
@@ -106,7 +105,6 @@ size_t RegisterCrashKeys() {
   const base::debug::CrashKey kFixedKeys[] = {
     { crash_keys::kMetricsClientId, crash_keys::kSmallSize },
     { kCurrentVersion, crash_keys::kSmallSize },
-    { kIsMultiInstall, crash_keys::kSmallSize },
     { kIsSystemLevel, crash_keys::kSmallSize },
     { kOperation, crash_keys::kSmallSize },
 
@@ -127,7 +125,6 @@ void SetInitialCrashKeys(const InstallerState& state) {
   using base::debug::SetCrashKeyValue;
 
   SetCrashKeyValue(kOperation, OperationToString(state.operation()));
-  SetCrashKeyValue(kIsMultiInstall, "false");
   SetCrashKeyValue(kIsSystemLevel, state.system_install() ? "true" : "false");
 
   const base::string16 state_key = state.state_key();
