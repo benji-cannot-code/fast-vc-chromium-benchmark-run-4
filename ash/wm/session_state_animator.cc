0490eb9f08ff9656632c760c519a82644d0710cd
warx <warx@chromium.org>
2017-01-11 14:19:42 -0800

    ash: Tune shutdown timer in TabletPowerButtonController and shutdown white animation speed
    Changes:
    (1) shutdown_timer_ in TabletPowerButtonController changes from 1000ms to 500ms.
    (2) animation_speed_shutdown is set to 2500ms for touch view enabled device case.
    
    This change follows the new timing guideline in go/touchview-power.
    
    BUG=633304
    TEST=tested on device
    
    Review-Url: https://codereview.chromium.org/2622103003
    Cr-Commit-Position: refs/heads/master@{#443012}

--- ash/wm/session_state_animator.cc
+++ ash/wm/session_state_animator.cc
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/wm/session_state_animator.h"
 
+#include "ash/common/ash_switches.h"
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/shell.h"
 #include "ash/wm/window_animations.h"
+#include "base/command_line.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/window_event_dispatcher.h"
 #include "ui/compositor/layer_animation_observer.h"
@@ -16,6 +18,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/widget.h"
 
 namespace ash {
+
+namespace {
+
+bool IsTouchViewEnabled() {
+  return base::CommandLine::ForCurrentProcess()->HasSwitch(
+             switches::kAshEnableTouchView) ||
+         base::CommandLine::ForCurrentProcess()->HasSwitch(
+             switches::kAshEnableTouchViewTesting);
+}
+
+}  // namespace
 
 const int SessionStateAnimator::kAllLockScreenContainersMask =
     SessionStateAnimator::LOCK_SCREEN_WALLPAPER |
@@ -83,7 +96,8 @@ base::TimeDelta SessionStateAnimator::GetDuration(
     case ANIMATION_SPEED_UNDO_MOVE_WINDOWS:
       return base::TimeDelta::FromMilliseconds(350);
     case ANIMATION_SPEED_SHUTDOWN:
-      return base::TimeDelta::FromMilliseconds(1000);
+      return IsTouchViewEnabled() ? base::TimeDelta::FromMilliseconds(2500)
+                                  : base::TimeDelta::FromMilliseconds(1000);
     case ANIMATION_SPEED_REVERT_SHUTDOWN:
       return base::TimeDelta::FromMilliseconds(500);
   }
