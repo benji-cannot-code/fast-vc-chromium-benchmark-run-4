e938c1ec178f610b0e5fab40c305cc7f87af5136
yoav <yoav@yoav.ws>
2017-01-11 05:12:18 -0800

    Fix HTMLPreloadScanner handling of type in link preload.
    Handling of the `type` attribute in `<link rel=preload>` was not
    properly added, which meant preloads that included a valid type were
    dismissed due to rel=stylesheet related logic.
    
    This CL adds proper handling for the `type` attribute in
    `<link rel=preload>` for fonts, images and styles.
    
    BUG=659640
    
    Review-Url: https://codereview.chromium.org/2620993002
    Cr-Commit-Position: refs/heads/master@{#442887}

--- third_party/WebKit/Source/core/html/parser/HTMLPreloadScanner.cpp
+++ third_party/WebKit/Source/core/html/parser/HTMLPreloadScanner.cpp
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/instrumentation/tracing/TraceEvent.h"
 #include "platform/network/mime/ContentType.h"
 #include "platform/network/mime/MIMETypeRegistry.h"
+#include "wtf/Optional.h"
 #include <memory>
 
 namespace blink {
@@ -121,7 +122,7 @@ static String initiatorFor(const StringImpl* tagImpl) {
     return scriptTag.localName();
   if (match(tagImpl, videoTag))
     return videoTag.localName();
-  ASSERT_NOT_REACHED();
+  NOTREACHED();
   return emptyString();
 }
 
@@ -206,13 +207,17 @@ class TokenPreloadScanner::StartTagScanner {
       const ReferrerPolicy documentReferrerPolicy) {
     PreloadRequest::RequestType requestType =
         PreloadRequest::RequestTypePreload;
+    WTF::Optional<Resource::Type> type;
     if (shouldPreconnect()) {
       requestType = PreloadRequest::RequestTypePreconnect;
     } else {
       if (isLinkRelPreload()) {
         requestType = PreloadRequest::RequestTypeLinkRelPreload;
+        type = resourceTypeForLinkPreload();
+        if (type == WTF::nullopt)
+          return nullptr;
       }
-      if (!shouldPreload()) {
+      if (!shouldPreload(type)) {
         return nullptr;
       }
     }
@@ -231,9 +236,8 @@ class TokenPreloadScanner::StartTagScanner {
       resourceWidth.isSet = true;
     }
 
-    Resource::Type type;
-    if (!resourceType(type))
-      return nullptr;
+    if (type == WTF::nullopt)
+      type = resourceType();
 
     // The element's 'referrerpolicy' attribute (if present) takes precedence
     // over the document's referrer policy.
@@ -241,8 +245,9 @@ class TokenPreloadScanner::StartTagScanner {
                                         ? m_referrerPolicy
                                         : documentReferrerPolicy;
     auto request = PreloadRequest::createIfNeeded(
-        initiatorFor(m_tagImpl), position, m_urlToLoad, predictedBaseURL, type,
-        referrerPolicy, resourceWidth, clientHintsPreferences, requestType);
+        initiatorFor(m_tagImpl), position, m_urlToLoad, predictedBaseURL,
+        type.value(), referrerPolicy, resourceWidth, clientHintsPreferences,
+        requestType);
     if (!request)
       return nullptr;
 
@@ -350,8 +355,7 @@ class TokenPreloadScanner::StartTagScanner {
     } else if (match(attributeName, asAttr)) {
       m_asAttributeValue = attributeValue.lower();
     } else if (match(attributeName, typeAttr)) {
-      m_matched &= MIMETypeRegistry::isSupportedStyleSheetMIMEType(
-          ContentType(attributeValue).type());
+      m_typeAttributeValue = attributeValue;
     } else if (!m_referrerPolicySet &&
                match(attributeName, referrerpolicyAttr) &&
                !attributeValue.isNull()) {
@@ -444,25 +448,26 @@ class TokenPreloadScanner::StartTagScanner {
     return m_charset;
   }
 
-  bool resourceType(Resource::Type& type) const {
+  WTF::Optional<Resource::Type> resourceTypeForLinkPreload() const {
+    DCHECK(m_linkIsPreload);
+    return LinkLoader::getResourceTypeFromAsAttribute(m_asAttributeValue);
+  }
+
+  Resource::Type resourceType() const {
     if (match(m_tagImpl, scriptTag)) {
-      type = Resource::Script;
+      return Resource::Script;
     } else if (match(m_tagImpl, imgTag) || match(m_tagImpl, videoTag) ||
                (match(m_tagImpl, inputTag) && m_inputIsImage)) {
-      type = Resource::Image;
+      return Resource::Image;
     } else if (match(m_tagImpl, linkTag) && m_linkIsStyleSheet) {
-      type = Resource::CSSStyleSheet;
+      return Resource::CSSStyleSheet;
     } else if (m_linkIsPreconnect) {
-      type = Resource::Raw;
-    } else if (m_linkIsPreload) {
-      if (!LinkLoader::getResourceTypeFromAsAttribute(m_asAttributeValue, type))
-        return false;
+      return Resource::Raw;
     } else if (match(m_tagImpl, linkTag) && m_linkIsImport) {
-      type = Resource::ImportResource;
-    } else {
-      ASSERT_NOT_REACHED();
+      return Resource::ImportResource;
     }
-    return true;
+    NOTREACHED();
+    return Resource::Raw;
   }
 
   bool shouldPreconnect() const {
@@ -475,14 +480,39 @@ class TokenPreloadScanner::StartTagScanner {
            !m_urlToLoad.isEmpty();
   }
 
-  bool shouldPreload() const {
+  bool shouldPreloadLink(WTF::Optional<Resource::Type>& type) const {
+    if (m_linkIsStyleSheet) {
+      return m_typeAttributeValue.isEmpty() ||
+             MIMETypeRegistry::isSupportedStyleSheetMIMEType(
+                 ContentType(m_typeAttributeValue).type());
+    } else if (m_linkIsPreload) {
+      if (m_typeAttributeValue.isEmpty())
+        return true;
+      String typeFromAttribute = ContentType(m_typeAttributeValue).type();
+      if ((type == Resource::Font &&
+           !MIMETypeRegistry::isSupportedFontMIMEType(typeFromAttribute)) ||
+          (type == Resource::Image &&
+           !MIMETypeRegistry::isSupportedImagePrefixedMIMEType(
+               typeFromAttribute)) ||
+          (type == Resource::CSSStyleSheet &&
+           !MIMETypeRegistry::isSupportedStyleSheetMIMEType(
+               typeFromAttribute))) {
+        return false;
+      }
+    } else if (!m_linkIsImport) {
+      return false;
+    }
+
+    return true;
+  }
+
+  bool shouldPreload(WTF::Optional<Resource::Type>& type) const {
     if (m_urlToLoad.isEmpty())
       return false;
     if (!m_matched)
       return false;
-    if (match(m_tagImpl, linkTag) && !m_linkIsStyleSheet && !m_linkIsImport &&
-        !m_linkIsPreload)
-      return false;
+    if (match(m_tagImpl, linkTag))
+      return shouldPreloadLink(type);
     if (match(m_tagImpl, inputTag) && !m_inputIsImage)
       return false;
     if (match(m_tagImpl, scriptTag) &&
