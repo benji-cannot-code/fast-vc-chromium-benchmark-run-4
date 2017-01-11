46670f198961cd66ef33e3d05e5fe13b377f3f79
reed <reed@google.com>
2017-01-10 19:52:05 -0800

    remove unneeded include, plus SkDraw.h is going private
    will enable https://skia-review.googlesource.com/c/6845/
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2623913002
    Cr-Commit-Position: refs/heads/master@{#442785}

--- skia/ext/analysis_canvas.cc
+++ skia/ext/analysis_canvas.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/trace_event/trace_event.h"
 #include "skia/ext/analysis_canvas.h"
-#include "third_party/skia/include/core/SkDraw.h"
 #include "third_party/skia/include/core/SkPath.h"
 #include "third_party/skia/include/core/SkRRect.h"
 #include "third_party/skia/include/core/SkShader.h"
