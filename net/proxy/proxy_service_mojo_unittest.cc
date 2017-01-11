e4bf335bf7d34ff1f934d12e9e63662289ac50a4
scottmg <scottmg@chromium.org>
2017-01-11 14:14:55 -0800

    Remove --v8-pac-mojo-in-process
    Part of removing the dependency of the browser DLL on Windows on V8.
    
    BUG=581766
    
    Review-Url: https://codereview.chromium.org/2625853002
    Cr-Commit-Position: refs/heads/master@{#443008}

--- net/proxy/proxy_service_mojo_unittest.cc
+++ net/proxy/proxy_service_mojo_unittest.cc
@@ -12,8 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_helpers.h"
 #include "base/memory/ptr_util.h"
+#include "base/memory/singleton.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
+#include "mojo/public/cpp/bindings/strong_binding.h"
 #include "net/base/network_delegate_impl.h"
 #include "net/base/test_completion_callback.h"
 #include "net/dns/mock_host_resolver.h"
@@ -22,9 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/log/test_net_log.h"
 #include "net/log/test_net_log_entry.h"
 #include "net/proxy/dhcp_proxy_script_fetcher.h"
-#include "net/proxy/in_process_mojo_proxy_resolver_factory.h"
 #include "net/proxy/mock_proxy_script_fetcher.h"
 #include "net/proxy/mojo_proxy_resolver_factory.h"
+#include "net/proxy/mojo_proxy_resolver_factory_impl.h"
 #include "net/proxy/proxy_config_service_fixed.h"
 #include "net/proxy/proxy_service.h"
 #include "net/test/event_waiter.h"
@@ -121,6 +123,34 @@ class LoggingMockHostResolver : public MockHostResolver {
     return MockHostResolver::Resolve(info, priority, addresses, callback,
                                      out_req, net_log);
   }
+};
+
+class InProcessMojoProxyResolverFactory : public MojoProxyResolverFactory {
+ public:
+  static InProcessMojoProxyResolverFactory* GetInstance() {
+    return base::Singleton<InProcessMojoProxyResolverFactory>::get();
+  }
+
+  // Overridden from MojoProxyResolverFactory:
+  std::unique_ptr<base::ScopedClosureRunner> CreateResolver(
+      const std::string& pac_script,
+      mojo::InterfaceRequest<interfaces::ProxyResolver> req,
+      interfaces::ProxyResolverFactoryRequestClientPtr client) override {
+    factory_->CreateResolver(pac_script, std::move(req), std::move(client));
+    return nullptr;
+  }
+
+ private:
+  InProcessMojoProxyResolverFactory() {
+    mojo::MakeStrongBinding(base::MakeUnique<MojoProxyResolverFactoryImpl>(),
+                            mojo::MakeRequest(&factory_));
+  }
+  ~InProcessMojoProxyResolverFactory() override = default;
+  friend struct base::DefaultSingletonTraits<InProcessMojoProxyResolverFactory>;
+
+  interfaces::ProxyResolverFactoryPtr factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(InProcessMojoProxyResolverFactory);
 };
 
 }  // namespace
