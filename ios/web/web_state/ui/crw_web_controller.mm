d60177241d85ad0f248e5ca2ed3ae67b66350854
eugenebut <eugenebut@chromium.org>
2017-01-12 08:09:54 -0800

    [ios] Removed -[CRWWebDelegate webDidAddPendingURL].
    Use webState:didStartProvisionalNavigationForURL: instead.
    
    BUG=674991
    
    Review-Url: https://codereview.chromium.org/2626733003
    Cr-Commit-Position: refs/heads/master@{#443247}

--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -1710,7 +1710,6 @@ const NSTimeInterval kSnapshotOverlayTransition = 0.5;
                             rendererInitiated:YES];
   }
   _webStateImpl->SetIsLoading(true);
-  [_delegate webDidAddPendingURL];
   _webStateImpl->OnProvisionalNavigationStarted(requestURL);
 }
 
