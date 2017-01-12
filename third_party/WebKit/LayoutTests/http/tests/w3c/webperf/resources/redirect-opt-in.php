9c770550fce12c0d7af5c1adf461edc6ff1655df
sunjian <sunjian@chromium.org>
2017-01-11 16:37:00 -0800

    Move nav timing 2's wpt tests into w3c/webperf/approved directory
    BUG=80267
    
    Review-Url: https://codereview.chromium.org/2624553002
    Cr-Commit-Position: refs/heads/master@{#443084}

---
+++ third_party/WebKit/LayoutTests/http/tests/w3c/webperf/resources/redirect-opt-in.php
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+  $code = ctype_digit($_GET["code"]) ? $_GET["code"] : "302";
+  $location = $_GET["location"] ? $_GET["location"] : $_SERVER["SCRIPT_NAME"] . "?followed";
+
+  header("HTTP/1.1 $code");
+  header("Location: $location");
+  header("Timing-Allow-Origin: *");
+  exit;
+?>
