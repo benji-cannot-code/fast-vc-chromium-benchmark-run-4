d025d718d364f0bf5fb26e2bdfb3fce1a8834c24
twifkak <twifkak@chromium.org>
2017-01-12 12:48:16 -0800

    Create a synthetic field trial for precache.
    For clients in a Precache trial group who've started a precache in the last 15
    days, registers them in a matching group in the PrecacheSynthetic15D trial. For
    those who've started a precache in the last 1 day, registers in them in the
    PrecacheSynthetic1D trial also.
    
    BUG=663987
    
    Review-Url: https://codereview.chromium.org/2596093002
    Cr-Commit-Position: refs/heads/master@{#443352}

--- components/precache/content/precache_manager.h
+++ components/precache/content/precache_manager.h
@@ -58,8 +58,9 @@ namespace precache {
 class PrecacheDatabase;
 class PrecacheUnfinishedWork;
 
-// Visible for test.
 extern const char kPrecacheFieldTrialName[];
+
+// Visible for test.
 extern const char kMinCacheSizeParam[];
 size_t NumTopHosts();
 
@@ -120,13 +121,15 @@ class PrecacheManager : public KeyedService,
   // Update precache about an URL being fetched. Metrics related to precache are
   // updated and any ongoing precache will be cancelled if this is an user
   // initiated request. Should be called on UI thread.
-  void UpdatePrecacheMetricsAndState(const GURL& url,
-                                     const GURL& referrer,
-                                     const base::TimeDelta& latency,
-                                     const base::Time& fetch_time,
-                                     const net::HttpResponseInfo& info,
-                                     int64_t size,
-                                     bool is_user_traffic);
+  void UpdatePrecacheMetricsAndState(
+      const GURL& url,
+      const GURL& referrer,
+      const base::TimeDelta& latency,
+      const base::Time& fetch_time,
+      const net::HttpResponseInfo& info,
+      int64_t size,
+      bool is_user_traffic,
+      const base::Callback<void(base::Time)>& register_synthetic_trial);
 
  private:
   friend class PrecacheManagerTest;
@@ -154,6 +157,11 @@ class PrecacheManager : public KeyedService,
   // From PrecacheFetcher::PrecacheDelegate.
   void OnDone() override;
 
+  // Registers the precache synthetic field trial for users whom the precache
+  // task was run recently. |last_precache_time| is the last time precache task
+  // was run.
+  void RegisterSyntheticFieldTrial(const base::Time last_precache_time);
+
   // Callback when fetching unfinished work from storage is done.
   void OnGetUnfinishedWorkDone(
       std::unique_ptr<PrecacheUnfinishedWork> unfinished_work);
@@ -180,12 +188,15 @@ class PrecacheManager : public KeyedService,
   AllowedType PrecachingAllowed() const;
 
   // Update precache-related metrics in response to a URL being fetched.
-  void RecordStatsForFetch(const GURL& url,
-                           const GURL& referrer,
-                           const base::TimeDelta& latency,
-                           const base::Time& fetch_time,
-                           const net::HttpResponseInfo& info,
-                           int64_t size);
+  void RecordStatsForFetch(
+      const GURL& url,
+      const GURL& referrer,
+      const base::TimeDelta& latency,
+      const base::Time& fetch_time,
+      const net::HttpResponseInfo& info,
+      int64_t size,
+      const base::Callback<void(base::Time)>& register_synthetic_trial,
+      base::Time last_precache_time);
 
   // Update precache-related metrics in response to a URL being fetched. Called
   // by RecordStatsForFetch() by way of an asynchronous HistoryService callback.
