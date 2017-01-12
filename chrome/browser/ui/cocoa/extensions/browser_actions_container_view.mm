c7934745d1fa8f4c7f4e56f65a2f9d5f72c9594f
lgrey <lgrey@chromium.org>
2017-01-12 12:32:21 -0800

    [Mac] Flip toolbar in RTL (reland)
    This was reverted due to crbug.com/679174 which is fixed by using the
    browser action container view's |animationEndFrame| instead of its
    frame when calculating the size of the location bar.
    
    Browser actions will be reordered in a future change.
    
    BUG=648558, 648563, 673362
    
    Review-Url: https://codereview.chromium.org/2607533004
    Cr-Commit-Position: refs/heads/master@{#441925}
    Committed: https://chromium.googlesource.com/chromium/src/+/8b0fd02afc2107ba123774e0a464a43407a108cf
    
    patch from issue 2607533004 at patchset 80001 (http://crrev.com/2607533004#ps80001)
    
    Review-Url: https://codereview.chromium.org/2631463004
    Cr-Commit-Position: refs/heads/master@{#443345}

--- chrome/browser/ui/cocoa/extensions/browser_actions_container_view.mm
+++ chrome/browser/ui/cocoa/extensions/browser_actions_container_view.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <utility>
 
+#import "chrome/browser/ui/cocoa/l10n_util.h"
 #import "chrome/browser/ui/cocoa/view_id_util.h"
 #include "ui/base/cocoa/appkit_utils.h"
 #include "ui/events/keycodes/keyboard_code_conversion_mac.h"
@@ -59,6 +60,9 @@ const CGFloat kMinimumContainerWidth = 3.0;
 - (id)initWithFrame:(NSRect)frameRect {
   if ((self = [super initWithFrame:frameRect])) {
     grippyRect_ = NSMakeRect(0.0, 0.0, kGrippyWidth, NSHeight([self bounds]));
+    if (cocoa_l10n_util::ShouldDoExperimentalRTLLayout())
+      grippyRect_.origin.x = NSWidth(frameRect) - NSWidth(grippyRect_);
+
     canDragLeft_ = YES;
     canDragRight_ = YES;
     resizable_ = YES;
@@ -163,6 +167,8 @@ const CGFloat kMinimumContainerWidth = 3.0;
 }
 
 - (void)resetCursorRects {
+  if (cocoa_l10n_util::ShouldDoExperimentalRTLLayout())
+    grippyRect_.origin.x = NSWidth([self frame]) - NSWidth(grippyRect_);
   [self addCursorRect:grippyRect_ cursor:[self appropriateCursorForGrippy]];
 }
 
@@ -215,23 +221,29 @@ const CGFloat kMinimumContainerWidth = 3.0;
   NSRect containerFrame = [self frame];
   CGFloat dX = [theEvent deltaX];
   CGFloat withDelta = location.x - dX;
-  canDragRight_ = (withDelta >= initialDragPoint_.x) &&
-      (NSWidth(containerFrame) > kMinimumContainerWidth);
+  BOOL isRTL = cocoa_l10n_util::ShouldDoExperimentalRTLLayout();
+
   CGFloat maxAllowedWidth = [self maxAllowedWidth];
-  containerFrame.size.width =
-      std::max(NSWidth(containerFrame) - dX, kMinimumContainerWidth);
-  canDragLeft_ = withDelta <= initialDragPoint_.x &&
-      NSWidth(containerFrame) < maxDesiredWidth_ &&
-      NSWidth(containerFrame) < maxAllowedWidth;
 
-  if ((dX < 0.0 && !canDragLeft_) || (dX > 0.0 && !canDragRight_))
+  const CGFloat maxWidth = std::min(maxAllowedWidth, maxDesiredWidth_);
+  CGFloat newWidth = NSWidth(containerFrame) + (isRTL ? dX : -dX);
+  newWidth = std::min(std::max(newWidth, kMinimumContainerWidth), maxWidth);
+
+  BOOL canGrow = NSWidth(containerFrame) < maxWidth;
+  BOOL canShrink = NSWidth(containerFrame) > kMinimumContainerWidth;
+
+  canDragLeft_ =
+      withDelta <= initialDragPoint_.x && (isRTL ? canShrink : canGrow);
+  canDragRight_ =
+      (withDelta >= initialDragPoint_.x) && (isRTL ? canGrow : canShrink);
+  if ((dX < 0.0 && !canDragLeft_) || (dX > 0.0 && !canDragRight_) ||
+      fabs(dX) < FLT_EPSILON)
     return;
 
-  if (NSWidth(containerFrame) <= kMinimumContainerWidth)
-    return;
-
-  grippyPinned_ = NSWidth(containerFrame) >= maxAllowedWidth;
-  containerFrame.origin.x += dX;
+  grippyPinned_ = newWidth >= maxAllowedWidth;
+  if (!isRTL)
+    containerFrame.origin.x += dX;
+  containerFrame.size.width = newWidth;
 
   [self setFrame:containerFrame];
   [self setNeedsDisplay:YES];
@@ -272,14 +284,18 @@ const CGFloat kMinimumContainerWidth = 3.0;
 
 - (void)resizeToWidth:(CGFloat)width animate:(BOOL)animate {
   width = std::max(width, kMinimumContainerWidth);
-  NSRect frame = [self frame];
+  NSRect newFrame = [self frame];
 
   CGFloat maxAllowedWidth = [self maxAllowedWidth];
   width = std::min(maxAllowedWidth, width);
 
-  CGFloat dX = frame.size.width - width;
-  frame.size.width = width;
-  NSRect newFrame = NSOffsetRect(frame, dX, 0);
+  if (cocoa_l10n_util::ShouldDoExperimentalRTLLayout()) {
+    newFrame.size.width = width;
+  } else {
+    CGFloat dX = NSWidth(newFrame) - width;
+    newFrame.size.width = width;
+    newFrame.origin.x += dX;
+  }
 
   grippyPinned_ = width == maxAllowedWidth;
 
