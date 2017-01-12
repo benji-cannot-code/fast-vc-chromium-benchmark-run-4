3ab675c9cf5af54d3ef75c0d99cb28a90fe8f26b
jcivelli <jcivelli@chromium.org>
2017-01-11 16:59:08 -0800

    Fix IPC and base tests on ASAN Android bot.
    The argv list passed to the main method when running a test in a
    subprocess was not NULL terminated, something that the gtest launcher
    takes advantage of.
    This was causing an invalid memory read causing failures on the ASAN
    bots.
    
    BUG=680155,680156
    
    Review-Url: https://codereview.chromium.org/2622073004
    Cr-Commit-Position: refs/heads/master@{#443095}

--- testing/android/native_test/main_runner.cc
+++ testing/android/native_test/main_runner.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/posix/global_descriptors.h"
 #include "jni/MainRunner_jni.h"
+#include "testing/android/native_test/native_test_util.h"
 
 extern int main(int argc, char** argv);
 
@@ -45,12 +46,9 @@ static jint RunMain(
   std::vector<std::string> cpp_command_line;
   AppendJavaStringArrayToStringVector(env, command_line, &cpp_command_line);
 
-  std::vector<char*> c_command_line;
-  for (auto& entry : cpp_command_line) {
-    c_command_line.push_back(&entry[0]);
-  }
-
-  return main(c_command_line.size(), &c_command_line[0]);
+  std::vector<char*> argv;
+  int argc = ArgsToArgv(cpp_command_line, &argv);
+  return main(argc, &argv[0]);
 }
 
 }  // namespace android
