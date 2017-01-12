916907a896da4cd4ac2867b1c6d3168db826b91b
warx <warx@chromium.org>
2017-01-12 08:18:43 -0800

    ash: Add one second grace period For LockScreenIfRequired
    Changes:
    Add one second grace period For LockScreenIfRequired. Basically, an one second OneShotTimer is used to set/reset the behavior.
    
    BUG=633304
    BUG=680509
    TEST=device test, and modify test coverage
    
    Review-Url: https://codereview.chromium.org/2620383003
    Cr-Commit-Position: refs/heads/master@{#443254}

--- ash/system/chromeos/power/tablet_power_button_controller.h
+++ ash/system/chromeos/power/tablet_power_button_controller.h
@@ -43,6 +43,12 @@ class ASH_EXPORT TabletPowerButtonController
     // Emulates |shutdown_timer_| timeout.
     void TriggerShutdownTimeout();
 
+    // Returns true when |lock_screen_timer_| is running.
+    bool LockScreenTimerIsRunning() const;
+
+    // Emulates |lock_screen_timer_| timeout.
+    void TriggerLockScreenTimeout();
+
    private:
     TabletPowerButtonController* controller_;  // Not owned.
 
@@ -102,6 +108,9 @@ class ASH_EXPORT TabletPowerButtonController
   // and locking is possible.
   void LockScreenIfRequired();
 
+  // Called by |lock_screen_timer_| to start locking screen.
+  void OnLockScreenTimeout();
+
   // True if the brightness level is currently set to off.
   bool brightness_level_is_zero_ = false;
 
@@ -124,6 +133,10 @@ class ASH_EXPORT TabletPowerButtonController
   // Started when the tablet power button is pressed and stopped when it's
   // released. Runs OnShutdownTimeout() to start shutdown.
   base::OneShotTimer shutdown_timer_;
+
+  // Used to provide a grace period between forcing the display off and locking
+  // the screen. Runs OnLockScreenTimeout().
+  base::OneShotTimer lock_screen_timer_;
 
   LockStateController* controller_;  // Not owned.
 
