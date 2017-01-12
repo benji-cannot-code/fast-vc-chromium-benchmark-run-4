2d12571014b498d763d2f48cbd792b16872830d2
tapted <tapted@chromium.org>
2017-01-11 18:55:21 -0800

    MacViews: Harmony for TabDialogs' dialogs (starting with Collected Cookies)
    Collected cookies when invoked from the site info bubble (padlock ->
    Cookies -> click the "X in use" link) is already hooked up to
    --secondary-ui-md. However, it can also be shown via the content
    settings bubble when a cookie is blocked.
    
    We also need TabDialogs' Signin confirmation dialog switched to Harmony,
    so set up a way to show Harmony dialogs from the TabDialogs interface.
    
    Opt collected cookies into DialogBrowserTest to make it easy to invoke
    for testing.
    
    BUG=654151, 677012, 610428
    
    Review-Url: https://codereview.chromium.org/2534743002
    Cr-Commit-Position: refs/heads/master@{#443130}

--- chrome/browser/ui/cocoa/tab_dialogs_cocoa.mm
+++ chrome/browser/ui/cocoa/tab_dialogs_cocoa.mm
@@ -10,14 +10,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/hung_renderer_controller.h"
 #import "chrome/browser/ui/cocoa/passwords/passwords_bubble_cocoa.h"
 #import "chrome/browser/ui/cocoa/profiles/profile_signin_confirmation_dialog_cocoa.h"
+#include "chrome/browser/ui/cocoa/tab_dialogs_views_mac.h"
 #import "chrome/browser/ui/cocoa/validation_message_bubble_cocoa.h"
 #include "content/public/browser/web_contents.h"
+#include "ui/base/material_design/material_design_controller.h"
 
 // static
 void TabDialogs::CreateForWebContents(content::WebContents* contents) {
   DCHECK(contents);
-  if (!FromWebContents(contents))
-    contents->SetUserData(UserDataKey(), new TabDialogsCocoa(contents));
+
+  if (!FromWebContents(contents)) {
+    TabDialogs* tab_dialogs =
+        ui::MaterialDesignController::IsSecondaryUiMaterial()
+            ? new TabDialogsViewsMac(contents)
+            : new TabDialogsCocoa(contents);
+    contents->SetUserData(UserDataKey(), tab_dialogs);
+  }
 }
 
 TabDialogsCocoa::TabDialogsCocoa(content::WebContents* contents)
---
+++ chrome/browser/ui/cocoa/tab_dialogs_views_mac.mm
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/cocoa/tab_dialogs_views_mac.h"
+
+#include "chrome/browser/ui/views/collected_cookies_views.h"
+
+TabDialogsViewsMac::TabDialogsViewsMac(content::WebContents* contents)
+    : TabDialogsCocoa(contents) {}
+
+TabDialogsViewsMac::~TabDialogsViewsMac() {}
+
+void TabDialogsViewsMac::ShowCollectedCookies() {
+  // Deletes itself on close.
+  new CollectedCookiesViews(web_contents());
+}
