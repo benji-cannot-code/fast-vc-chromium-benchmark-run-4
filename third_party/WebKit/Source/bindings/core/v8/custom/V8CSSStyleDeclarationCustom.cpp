4423ee6594df53d08ff8f515c4187dadb4507121
timloh <timloh@chromium.org>
2017-01-12 01:00:32 -0800

    Fix CSSPropertySetterGetterMethods regression
    A recent patch crrev.com/2607403002 introduced a 10% regression in
    setters and getters for CSSOM style objects. This patch fixes this
    by removing the additional binding to access the registry, instead
    using already existing members to access it.
    
    BUG=679511
    
    Review-Url: https://codereview.chromium.org/2625303002
    Cr-Commit-Position: refs/heads/master@{#443188}

--- third_party/WebKit/Source/bindings/core/v8/custom/V8CSSStyleDeclarationCustom.cpp
+++ third_party/WebKit/Source/bindings/core/v8/custom/V8CSSStyleDeclarationCustom.cpp
@@ -230,7 +230,7 @@ void V8CSSStyleDeclaration::namedPropertySetterCustom(
       info.GetIsolate(), ExceptionState::SetterContext, "CSSStyleDeclaration",
       getPropertyName(resolveCSSPropertyID(unresolvedProperty)));
   impl->setPropertyInternal(unresolvedProperty, String(), propertyValue, false,
-                            nullptr, exceptionState);
+                            exceptionState);
 
   v8SetReturnValue(info, value);
 }
