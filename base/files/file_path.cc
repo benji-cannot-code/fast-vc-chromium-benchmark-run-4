5059717b1f2f991c5639574a1036e9880eb2a3ec
dmurph <dmurph@chromium.org>
2017-01-11 18:54:12 -0800

    Move constructor for base::FilePath
    R=danakj@chromium.org
    BUG=670043
    
    Review-Url: https://codereview.chromium.org/2627713002
    Cr-Commit-Position: refs/heads/master@{#443129}

--- base/files/file_path.cc
+++ base/files/file_path.cc
@@ -175,6 +175,7 @@ FilePath::FilePath() {
 
 FilePath::FilePath(const FilePath& that) : path_(that.path_) {
 }
+FilePath::FilePath(FilePath&& that) = default;
 
 FilePath::FilePath(StringPieceType path) {
   path.CopyToString(&path_);
@@ -190,6 +191,8 @@ FilePath& FilePath::operator=(const FilePath& that) {
   path_ = that.path_;
   return *this;
 }
+
+FilePath& FilePath::operator=(FilePath&& that) = default;
 
 bool FilePath::operator==(const FilePath& that) const {
 #if defined(FILE_PATH_USES_DRIVE_LETTERS)
