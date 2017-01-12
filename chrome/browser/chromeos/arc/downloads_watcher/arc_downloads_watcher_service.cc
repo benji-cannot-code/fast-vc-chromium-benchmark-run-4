40e8e758a45e1735a1eb6861423d1e83e6364f64
fdoray <fdoray@chromium.org>
2017-01-12 05:28:11 -0800

    Use TaskScheduler instead of blocking pool in arc_downloads_watcher_service.cc.
    The following traits are used:
    
    Priority: Inherited (default)
      The priority is inherited from the calling context (i.e. TaskTraits
      are initialized with the priority of the current task).
    
    Shutdown behavior: SKIP_ON_SHUTDOWN (default)
      Tasks posted with this mode that have not started executing at
      shutdown will never run. However, any task that has already begun
      executing when shutdown is invoked will be allowed to continue and
      will block shutdown until completion.
    
      Note: Previously, the task was posted to the blocking pool with
      BLOCK_SHUTDOWN (default in SequencedWorkerPool).
    
    May Block:
      Tasks posted with MayBlock() may block. This includes but is not
      limited to tasks that wait on synchronous file I/O operations:
      read or write a file from disk, interact with a pipe or a socket,
      rename or delete a file, enumerate files in a directory, etc. This
      trait isn't required for the mere use of locks.
    
    BUG=667892
    
    Review-Url: https://codereview.chromium.org/2628673003
    Cr-Commit-Position: refs/heads/master@{#443217}

--- chrome/browser/chromeos/arc/downloads_watcher/arc_downloads_watcher_service.cc
+++ chrome/browser/chromeos/arc/downloads_watcher/arc_downloads_watcher_service.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path_watcher.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/string_util.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/time/time.h"
 #include "chrome/browser/download/download_prefs.h"
 #include "chrome/browser/profiles/profile_manager.h"
@@ -277,8 +278,8 @@ void ArcDownloadsWatcherService::DownloadsWatcher::OnFilePathChanged(
 void ArcDownloadsWatcherService::DownloadsWatcher::DelayBuildTimestampMap() {
   DCHECK_CURRENTLY_ON(BrowserThread::FILE);
   DCHECK(outstanding_task_);
-  base::PostTaskAndReplyWithResult(
-      BrowserThread::GetBlockingPool(), FROM_HERE,
+  base::PostTaskWithTraitsAndReplyWithResult(
+      FROM_HERE, base::TaskTraits().MayBlock(),
       base::Bind(&BuildTimestampMapCallback, downloads_dir_),
       base::Bind(&DownloadsWatcher::OnBuildTimestampMap,
                  weak_ptr_factory_.GetWeakPtr()));
@@ -302,15 +303,10 @@ void ArcDownloadsWatcherService::DownloadsWatcher::OnBuildTimestampMap(
   BrowserThread::PostTask(
       BrowserThread::UI, FROM_HERE,
       base::Bind(callback_, base::Passed(std::move(string_paths))));
-  if (last_notify_time_ > snapshot_time) {
-    base::PostTaskAndReplyWithResult(
-        BrowserThread::GetBlockingPool(), FROM_HERE,
-        base::Bind(&BuildTimestampMapCallback, downloads_dir_),
-        base::Bind(&DownloadsWatcher::OnBuildTimestampMap,
-                   weak_ptr_factory_.GetWeakPtr()));
-  } else {
+  if (last_notify_time_ > snapshot_time)
+    DelayBuildTimestampMap();
+  else
     outstanding_task_ = false;
-  }
 }
 
 ArcDownloadsWatcherService::ArcDownloadsWatcherService(
