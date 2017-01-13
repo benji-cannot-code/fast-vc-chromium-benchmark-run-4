03ac07593e15d82afc0f71626d4bee64192ebea4
tommi <tommi@chromium.org>
2017-01-13 12:50:09 -0800

    Fix getUserMedia so that failure is reported for invalid audio sources.
    This is a reland:
    * Original CL https://codereview.chromium.org/2623443002/
    * The originally reviewed CL is in the first patch.
    * A fix for the issue discovered on the "Win7 Tests (dbg)(1)" bot is in the second patch set.
    
    This changes getUserMedia to wait for initialization of local audio sources before issuing the completion callback (either success or failure). Previously, if an error occurs between attempting to start a local audio source and the render side OnStreamCreated callback, getUserMedia would report successful completion with an audio track but no audio would actually be delivered for that track.
    
    BUG=679210
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2622073003
    Cr-Commit-Position: refs/heads/master@{#443660}

--- content/renderer/media/user_media_client_impl.h
+++ content/renderer/media/user_media_client_impl.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
-#include "base/memory/scoped_vector.h"
 #include "base/memory/weak_ptr.h"
 #include "base/threading/non_thread_safe.h"
 #include "content/common/content_export.h"
@@ -120,7 +119,8 @@ class CONTENT_EXPORT UserMediaClientImpl
   // These are virtual for test purposes.
   virtual MediaStreamAudioSource* CreateAudioSource(
       const StreamDeviceInfo& device,
-      const blink::WebMediaConstraints& constraints);
+      const blink::WebMediaConstraints& constraints,
+      const MediaStreamSource::ConstraintsCallback& source_ready);
   virtual MediaStreamVideoSource* CreateVideoSource(
       const StreamDeviceInfo& device,
       const MediaStreamSource::SourceStoppedCallback& stop_callback);
@@ -147,7 +147,8 @@ class CONTENT_EXPORT UserMediaClientImpl
     blink::WebMediaStream web_stream;
     blink::WebUserMediaRequest request;
 
-    void StartAudioTrack(const blink::WebMediaStreamTrack& track);
+    void StartAudioTrack(const blink::WebMediaStreamTrack& track,
+                         bool is_pending);
 
     blink::WebMediaStreamTrack CreateAndStartVideoTrack(
         const blink::WebMediaStreamSource& source,
@@ -157,16 +158,22 @@ class CONTENT_EXPORT UserMediaClientImpl
     // successfully started, or a source has failed to start.
     void CallbackOnTracksStarted(const ResourcesReady& callback);
 
-    bool IsSourceUsed(const blink::WebMediaStreamSource& source) const;
-    void RemoveSource(const blink::WebMediaStreamSource& source);
-
     bool HasPendingSources() const;
+
+    // Called when a local audio source has finished (or failed) initializing.
+    void OnAudioSourceStarted(MediaStreamSource* source,
+                              MediaStreamRequestResult result,
+                              const blink::WebString& result_name);
 
    private:
     void OnTrackStarted(
         MediaStreamSource* source,
         MediaStreamRequestResult result,
         const blink::WebString& result_name);
+
+    // Cheks if the sources for all tracks have been started and if so,
+    // invoke the |ready_callback_|.  Note that the caller should expect
+    // that |this| might be deleted when the function returns.
     void CheckAllTracksStarted();
 
     ResourcesReady ready_callback_;
@@ -176,7 +183,7 @@ class CONTENT_EXPORT UserMediaClientImpl
     std::vector<blink::WebMediaStreamSource> sources_;
     std::vector<MediaStreamSource*> sources_waiting_for_callback_;
   };
-  typedef ScopedVector<UserMediaRequestInfo> UserMediaRequests;
+  typedef std::vector<std::unique_ptr<UserMediaRequestInfo>> UserMediaRequests;
 
  protected:
   // These methods can be accessed in unit tests.
@@ -194,11 +201,14 @@ class CONTENT_EXPORT UserMediaClientImpl
 
   // Creates a WebKit representation of stream sources based on
   // |devices| from the MediaStreamDispatcher.
-  void InitializeSourceObject(
+  blink::WebMediaStreamSource InitializeVideoSourceObject(
       const StreamDeviceInfo& device,
-      blink::WebMediaStreamSource::Type type,
+      const blink::WebMediaConstraints& constraints);
+
+  blink::WebMediaStreamSource InitializeAudioSourceObject(
+      const StreamDeviceInfo& device,
       const blink::WebMediaConstraints& constraints,
-      blink::WebMediaStreamSource* webkit_source);
+      bool* is_pending);
 
   void CreateVideoTracks(
       const StreamDeviceInfoArray& devices,
@@ -223,6 +233,22 @@ class CONTENT_EXPORT UserMediaClientImpl
       const StreamDeviceInfoArray& audio_array,
       const StreamDeviceInfoArray& video_array);
 
+  static void OnAudioSourceStartedOnAudioThread(
+      scoped_refptr<base::SingleThreadTaskRunner> task_runner,
+      base::WeakPtr<UserMediaClientImpl> weak_ptr,
+      MediaStreamSource* source,
+      MediaStreamRequestResult result,
+      const blink::WebString& result_name);
+
+  void OnAudioSourceStarted(MediaStreamSource* source,
+                            MediaStreamRequestResult result,
+                            const blink::WebString& result_name);
+
+  void NotifyAllRequestsOfAudioSourceStarted(
+      MediaStreamSource* source,
+      MediaStreamRequestResult result,
+      const blink::WebString& result_name);
+
   using EnumerationResult = std::vector<MediaDeviceInfoArray>;
   void FinalizeEnumerateDevices(blink::WebMediaDevicesRequest request,
                                 const EnumerationResult& result);
@@ -232,7 +258,21 @@ class CONTENT_EXPORT UserMediaClientImpl
   // Returns the source that use a device with |device.session_id|
   // and |device.device.id|. NULL if such source doesn't exist.
   const blink::WebMediaStreamSource* FindLocalSource(
+      const StreamDeviceInfo& device) const {
+    return FindLocalSource(local_sources_, device);
+  }
+  const blink::WebMediaStreamSource* FindPendingLocalSource(
+      const StreamDeviceInfo& device) const {
+    return FindLocalSource(pending_local_sources_, device);
+  }
+  const blink::WebMediaStreamSource* FindLocalSource(
+      const LocalStreamSources& sources,
       const StreamDeviceInfo& device) const;
+
+  // Looks up a local source and returns it if found. If not found, prepares
+  // a new WebMediaStreamSource with a NULL extraData pointer.
+  blink::WebMediaStreamSource FindOrInitializeSourceObject(
+      const StreamDeviceInfo& device);
 
   // Returns true if we do find and remove the |source|.
   // Otherwise returns false.
@@ -275,6 +315,7 @@ class CONTENT_EXPORT UserMediaClientImpl
   ::mojom::MediaDevicesDispatcherHostPtr media_devices_dispatcher_;
 
   LocalStreamSources local_sources_;
+  LocalStreamSources pending_local_sources_;
 
   UserMediaRequests user_media_requests_;
   MediaDevicesEventDispatcher::SubscriptionIdList
--- content/renderer/media/webrtc/processed_local_audio_source_unittest.cc
+++ content/renderer/media/webrtc/processed_local_audio_source_unittest.cc
@@ -103,9 +103,11 @@ class ProcessedLocalAudioSourceTest : public testing::Test {
         StreamDeviceInfo(MEDIA_DEVICE_AUDIO_CAPTURE, "Mock audio device",
                          "mock_audio_device_id", kSampleRate, kChannelLayout,
                          kRequestedBufferSize),
+        constraints,
+        base::Bind(&ProcessedLocalAudioSourceTest::OnAudioSourceStarted,
+                   base::Unretained(this)),
         &mock_dependency_factory_);
     source->SetAllowInvalidRenderFrameIdForTesting(true);
-    source->SetSourceConstraints(constraints);
     blink_audio_source_.setExtraData(source);  // Takes ownership.
   }
 
@@ -138,6 +140,10 @@ class ProcessedLocalAudioSourceTest : public testing::Test {
     return blink_audio_track_;
   }
 
+  void OnAudioSourceStarted(MediaStreamSource* source,
+                            MediaStreamRequestResult result,
+                            const blink::WebString& result_name) {}
+
  private:
   base::MessageLoop main_thread_message_loop_;  // Needed for MSAudioProcessor.
   MockAudioDeviceFactory mock_audio_device_factory_;
@@ -169,7 +175,10 @@ TEST_F(ProcessedLocalAudioSourceTest, VerifyAudioFlowWithoutAudioProcessing) {
       .WillOnce(WithArg<0>(Invoke(this, &ThisTest::CheckSourceFormatMatches)));
   EXPECT_CALL(*mock_audio_device_factory()->mock_capturer_source(),
               SetAutomaticGainControl(true));
-  EXPECT_CALL(*mock_audio_device_factory()->mock_capturer_source(), Start());
+  EXPECT_CALL(*mock_audio_device_factory()->mock_capturer_source(), Start())
+      .WillOnce(Invoke(
+          capture_source_callback(),
+          &media::AudioCapturerSource::CaptureCallback::OnCaptureStarted));
   ASSERT_TRUE(audio_source()->ConnectToTrack(blink_audio_track()));
   CheckOutputFormatMatches(audio_source()->GetAudioParameters());
 
