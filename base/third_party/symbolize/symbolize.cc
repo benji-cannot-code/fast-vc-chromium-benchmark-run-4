bcaa4482d46b3ee02e58339be751b5ba029fb074
bjornr <bjornr@opera.com>
2017-01-13 08:29:54 -0800

    Fixed google::FindSymbol reading past end of a section
    The symbol reading logic of google::FindSymbol was reading symbols in
    blocks of N, not accounting for that a section might not have a multiple
    of N symbols in it.
    
    This makes it read in blocks of N or the number of symbols remaining,
    whichever is smallest.
    
    BUG=672481
    
    Review-Url: https://codereview.chromium.org/2566623003
    Cr-Commit-Position: refs/heads/master@{#443571}

--- base/third_party/symbolize/symbolize.cc
+++ base/third_party/symbolize/symbolize.cc
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(HAVE_SYMBOLIZE)
 
+#include <algorithm>
 #include <limits>
 
 #include "symbolize.h"
@@ -297,10 +298,12 @@ FindSymbol(uint64_t pc, const int fd, char *out, int out_size,
 
     // Read at most NUM_SYMBOLS symbols at once to save read() calls.
     ElfW(Sym) buf[NUM_SYMBOLS];
-    const ssize_t len = ReadFromOffset(fd, &buf, sizeof(buf), offset);
+    int num_symbols_to_read = std::min(NUM_SYMBOLS, num_symbols - i);
+    const ssize_t len =
+        ReadFromOffset(fd, &buf, sizeof(buf[0]) * num_symbols_to_read, offset);
     SAFE_ASSERT(len % sizeof(buf[0]) == 0);
     const ssize_t num_symbols_in_buf = len / sizeof(buf[0]);
-    SAFE_ASSERT(num_symbols_in_buf <= sizeof(buf)/sizeof(buf[0]));
+    SAFE_ASSERT(num_symbols_in_buf <= num_symbols_to_read);
     for (int j = 0; j < num_symbols_in_buf; ++j) {
       const ElfW(Sym)& symbol = buf[j];
       uint64_t start_address = symbol.st_value;
