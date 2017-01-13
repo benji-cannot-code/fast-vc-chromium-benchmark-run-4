b9147168895728dcf7c7cf895d1f52aa6e361f15
tsergeant <tsergeant@chromium.org>
2017-01-12 17:20:20 -0800

    MD WebUI: Fix incorrect positioning of action menu after window resize
    Showing the menu, resizing the browser window horizontally and then
    showing the menu a second time could result in incorrectly positioning
    the window, as position values from the first instance would affect
    layout of the second instance. This fixes the issue by clearing position
    alues before reshowing the menu.
    
    BUG=667198
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2614663011
    Cr-Commit-Position: refs/heads/master@{#443429}

--- ui/webui/resources/cr_elements/cr_action_menu/cr_action_menu.js
+++ ui/webui/resources/cr_elements/cr_action_menu/cr_action_menu.js
@@ -131,6 +131,11 @@ Polymer({
     }.bind(this);
     window.addEventListener('resize', this.onWindowResize_);
 
+    // Reset position to prevent previous values from affecting layout.
+    this.style.left = '';
+    this.style.right = '';
+    this.style.top = '';
+
     this.showModal();
 
     var rect = this.anchorElement_.getBoundingClientRect();
