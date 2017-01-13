cca34d1f80b1455069b15e4c8839057b0414f72b
kbr <kbr@chromium.org>
2017-01-13 12:43:26 -0800

    Add support for ASAN-specific test expectations to WebGL tests.
    Because each test harness will need to explicitly add code to handle
    ASAN expectations, add the command-line flag only to the WebGL harness
    for now.
    
    Pass the appropriate command line argument on the Mac ASAN bot. Added
    unit tests for the new functionality.
    
    Fixed a preexisting bug with capitalization of test expectations.
    Added tests for it.
    
    BUG=680845
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2632603002
    Cr-Commit-Position: refs/heads/master@{#443655}

--- content/test/gpu/generate_buildbot_json.py
+++ content/test/gpu/generate_buildbot_json.py
@@ -445,6 +445,7 @@ FYI_WATERFALL = {
       'build_config': 'Release',
       'swarming': True,
       'os_type': 'mac',
+      'is_asan': True,
     },
     'Linux Release (NVIDIA)': {
       'swarming_dimensions': [
@@ -1253,6 +1254,7 @@ TELEMETRY_GPU_INTEGRATION_TESTS = {
         'allow_on_android': True,
       },
     ],
+    'asan_args': ['--is-asan'],
   },
   'webgl_conformance_d3d9_tests': {
     'tester_configs': [
@@ -1266,6 +1268,7 @@ TELEMETRY_GPU_INTEGRATION_TESTS = {
     'extra_browser_args': [
       '--use-angle=d3d9',
     ],
+    'asan_args': ['--is-asan'],
   },
   'webgl_conformance_gl_tests': {
     'tester_configs': [
@@ -1307,6 +1310,7 @@ TELEMETRY_GPU_INTEGRATION_TESTS = {
     'extra_browser_args': [
       '--use-angle=gl',
     ],
+    'asan_args': ['--is-asan'],
   },
   'webgl_conformance_angle_tests': {
     'tester_configs': [
@@ -1320,6 +1324,7 @@ TELEMETRY_GPU_INTEGRATION_TESTS = {
     'extra_browser_args': [
       '--use-gl=angle',
     ],
+    'asan_args': ['--is-asan'],
   },
   'webgl_conformance_d3d11_passthrough': {
     'tester_configs': [
@@ -1334,6 +1339,7 @@ TELEMETRY_GPU_INTEGRATION_TESTS = {
       '--use-angle=d3d11',
       '--use-passthrough-cmd-decoder',
     ],
+    'asan_args': ['--is-asan'],
   },
   'webgl2_conformance_tests': {
     'tester_configs': [
@@ -1363,6 +1369,7 @@ TELEMETRY_GPU_INTEGRATION_TESTS = {
       '--read-abbreviated-json-results-from=' + \
       '../../content/test/data/gpu/webgl2_conformance_tests_output.json',
     ],
+    'asan_args': ['--is-asan'],
     'swarming': {
       # These tests currently take about an hour and fifteen minutes
       # to run. Split them into roughly 5-minute shards.
@@ -1406,6 +1413,7 @@ TELEMETRY_GPU_INTEGRATION_TESTS = {
       '--read-abbreviated-json-results-from=' + \
       '../../content/test/data/gpu/webgl2_conformance_tests_output.json',
     ],
+    'asan_args': ['--is-asan'],
     'swarming': {
       # These tests currently take about an hour and fifteen minutes
       # to run. Split them into roughly 5-minute shards.
@@ -1458,6 +1466,9 @@ def matches_swarming_dimensions(tester_config, dimension_sets):
 
 def is_android(tester_config):
   return tester_config['os_type'] == 'android'
+
+def is_asan(tester_config):
+  return tester_config.get('is_asan', False)
 
 def tester_config_matches_tester(tester_name, tester_config, tc, is_fyi,
                                  check_waterfall):
@@ -1629,6 +1640,9 @@ def generate_isolated_test(tester_name, tester_config, test, test_config,
   if 'android_args' in test_config and is_android(tester_config):
     test_args.extend(substitute_args(tester_config,
                                      test_config['android_args']))
+  if 'asan_args' in test_config and is_asan(tester_config):
+    test_args.extend(substitute_args(tester_config,
+                                     test_config['asan_args']))
   # The step name must end in 'test' or 'tests' in order for the
   # results to automatically show up on the flakiness dashboard.
   # (At least, this was true some time ago.) Continue to use this
