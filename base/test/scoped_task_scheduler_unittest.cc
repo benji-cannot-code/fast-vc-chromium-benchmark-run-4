9229c217319c8fd7c11c58f92b5481050d4f3cdb
fdoray <fdoray@chromium.org>
2017-01-13 10:44:28 -0800

    Allow explicit shutdown of ScopedTaskScheduler.
    This will allow tests to verify that code behaves correctly after
    TaskScheduler shutdown (i.e. when SKIP_ON_SHUTDOWN and
    CONTINUE_ON_SHUTDOWN tasks are destroyed as they are being posted).
    These tests can't just destroy ScopedTaskScheduler, because that would
    unregister the TaskScheduler.
    
    BUG=553459
    
    Review-Url: https://codereview.chromium.org/2628193002
    Cr-Commit-Position: refs/heads/master@{#443624}

--- base/test/scoped_task_scheduler_unittest.cc
+++ base/test/scoped_task_scheduler_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/sequence_checker.h"
 #include "base/task_scheduler/post_task.h"
+#include "base/task_scheduler/task_scheduler.h"
 #include "base/task_scheduler/test_utils.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "base/threading/thread_checker.h"
@@ -222,7 +223,25 @@ TEST(ScopedTaskSchedulerTest, CreateSingleThreadTaskRunnerAndPostTask) {
   EXPECT_TRUE(second_task_ran);
 }
 
-TEST(ScopedTaskSchedulerTest, ShutdownBehavior) {
+TEST(ScopedTaskSchedulerTest, NonBlockShutdownTasksPostedAfterShutdownDontRun) {
+  ScopedTaskScheduler scoped_task_scheduler;
+  TaskScheduler::GetInstance()->Shutdown();
+  PostTaskWithTraits(FROM_HERE, TaskTraits().WithShutdownBehavior(
+                                    TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN),
+                     Bind([]() {
+                       ADD_FAILURE()
+                           << "CONTINUE_ON_SHUTDOWN task should not run";
+                     }));
+  PostTaskWithTraits(
+      FROM_HERE,
+      TaskTraits().WithShutdownBehavior(TaskShutdownBehavior::SKIP_ON_SHUTDOWN),
+      Bind([]() { ADD_FAILURE() << "SKIP_ON_SHUTDOWN task should not run"; }));
+
+  // This should not run anything.
+  RunLoop().RunUntilIdle();
+}
+
+TEST(ScopedTaskSchedulerTest, DestructorRunsBlockShutdownTasksOnly) {
   bool block_shutdown_task_ran = false;
   {
     ScopedTaskScheduler scoped_task_scheduler;
