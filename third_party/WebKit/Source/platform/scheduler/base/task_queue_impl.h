4e13952924315c1a58bffa10d648bced981eaad7
alexclarke <alexclarke@chromium.org>
2017-01-12 17:49:00 -0800

    Use WTF::Deque instead of std::queue in the blink scheduler
    In theory this will reduce memory wasted by std::deque. It may also help diagnose
    some puzzling crashes we've seen related to the immediate incomming and work queues.
    
    BUG=674625, 674895
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2579773002
    Cr-Commit-Position: refs/heads/master@{#443450}

--- third_party/WebKit/Source/platform/scheduler/base/task_queue_impl.h
+++ third_party/WebKit/Source/platform/scheduler/base/task_queue_impl.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/scheduler/base/enqueue_order.h"
 #include "platform/scheduler/base/intrusive_heap.h"
 #include "public/platform/scheduler/base/task_queue.h"
+#include "wtf/Deque.h"
 
 namespace blink {
 namespace scheduler {
@@ -231,7 +232,7 @@ class BLINK_PLATFORM_EXPORT TaskQueueImpl final : public TaskQueue {
     TaskQueueManager* task_queue_manager;
     TimeDomain* time_domain;
 
-    std::queue<Task> immediate_incoming_queue;
+    WTF::Deque<Task> immediate_incoming_queue;
   };
 
   struct MainThreadOnly {
@@ -295,7 +296,7 @@ class BLINK_PLATFORM_EXPORT TaskQueueImpl final : public TaskQueue {
   bool BlockedByFenceLocked() const;
 
   void TraceQueueSize(bool is_locked) const;
-  static void QueueAsValueInto(const std::queue<Task>& queue,
+  static void QueueAsValueInto(const WTF::Deque<Task>& queue,
                                base::trace_event::TracedValue* state);
   static void QueueAsValueInto(const std::priority_queue<Task>& queue,
                                base::trace_event::TracedValue* state);
