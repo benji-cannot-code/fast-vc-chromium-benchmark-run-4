0f4336d90e67257a0d46f0c4e8bc036411b5cd1a
ananta <ananta@chromium.org>
2017-01-12 17:59:23 -0800

    PlzNavigate: Fix anchor-no-multiple-windows.html layout test.
    This layouttest turns off the WebKitSupportsMultipleWindows property which means that popups
    open in the same window. It then initiates a navigation to a data URL by clicking on the
    corresponding href. It then compares the window counts and expects the window counts to
    remain unchanged.
    
    Without PlzNavigate the data URL navigation creates a provisional loader in blink which
    is then cancelled when data is written to the main document, via Document::writeln.
    
    With PlzNavigate the top level navigation for the data URL is sent to the browser. The
    provisional loader is created when the navigation is committed via the
    FrameMsg_CommitNavigation IPC. This is too late and hence the data written to the page
    via the Document::writeln calls never shows up.
    
    Fix is to add a check for whether there is a client side navigation in process for PlzNavigate
    and if yes stop ongoing loaders.
    
    BUG=673744, 673742
    
    Review-Url: https://codereview.chromium.org/2628153002
    Cr-Commit-Position: refs/heads/master@{#443458}

--- third_party/WebKit/Source/web/WebLocalFrameImpl.cpp
+++ third_party/WebKit/Source/web/WebLocalFrameImpl.cpp
@@ -2030,7 +2030,7 @@ bool WebLocalFrameImpl::isLoading() const {
   if (!frame() || !frame()->document())
     return false;
   return frame()->loader().stateMachine()->isDisplayingInitialEmptyDocument() ||
-         frame()->loader().provisionalDocumentLoader() ||
+         frame()->loader().hasProvisionalNavigation() ||
          !frame()->document()->loadEventFinished();
 }
 
