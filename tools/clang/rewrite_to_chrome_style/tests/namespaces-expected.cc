8aa3261b927edf0b534aa7016e8770727186dea3
lukasza <lukasza@chromium.org>
2017-01-12 17:24:56 -0800

    Blacklist renaming inside blink::protocol namespace.
    BUG=680343
    
    Review-Url: https://codereview.chromium.org/2625903006
    Cr-Commit-Position: refs/heads/master@{#443432}

--- tools/clang/rewrite_to_chrome_style/tests/namespaces-expected.cc
+++ tools/clang/rewrite_to_chrome_style/tests/namespaces-expected.cc
@@ -13,6 +13,11 @@ namespace nested {
 void Foo();
 }  // namespace nested
 
+// blink::protocol namespace is blacklisted.
+namespace protocol {
+void foo();
+}  // namespace protocol
+
 }  // namespace blink
 
 namespace WTF {
--- tools/clang/rewrite_to_chrome_style/tests/namespaces-original.cc
+++ tools/clang/rewrite_to_chrome_style/tests/namespaces-original.cc
@@ -13,6 +13,11 @@ namespace nested {
 void foo();
 }  // namespace nested
 
+// blink::protocol namespace is blacklisted.
+namespace protocol {
+void foo();
+}  // namespace protocol
+
 }  // namespace blink
 
 namespace WTF {
