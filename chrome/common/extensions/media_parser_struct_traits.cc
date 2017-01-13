3db62a3a13d8949cf982087b3c347c29038a5789
noel <noel@chromium.org>
2017-01-13 08:47:47 -0800

    Convert utility process ParseMediaMetadata IPC to mojo
    Convert IPC media parse messages ChromeUtilityMsg_ParseMediaMetadata
    and ChromeUtilityHostMsg_ParseMediaMetadata_Finished to mojo.
    
    Add media_parser.mojom, add StructTraits for metadata::AttachedImage
    and use mojo common traits for base::DictionaryValue metadata.
    
    Resolve a ChromeContentUtilityClient TODO: if the utility process is
    running with elevated privileges, we only add elevated Mojo services
    to the service_manager::InterfaceRegistry. Add ExtensionsHandler API
    ExtensionsHandler::ExposeInterfacesToBrowser, and again, only expose
    its elevated Mojo services if the process is running elevated.
    
    Manually tested using the MGA media gallery sample app [1]. There is
    a browser test MediaGalleriesPlatformAppBrowserTest.GetMetadata that
    runs on chrome mac.
    
    [1] http://bit.ly/2i8qPSN
    
    BUG=680928
    
    Review-Url: https://codereview.chromium.org/2615423002
    Cr-Commit-Position: refs/heads/master@{#443579}

---
+++ chrome/common/extensions/media_parser_struct_traits.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/common/extensions/media_parser_struct_traits.h"
+#include "mojo/public/cpp/bindings/array_data_view.h"
+
+namespace {
+
+using TypeImageDataView = extensions::mojom::AttachedImageDataView;
+using TypeImage = ::metadata::AttachedImage;
+
+}  // namespace
+
+namespace mojo {
+
+// static
+bool StructTraits<TypeImageDataView, TypeImage>::Read(TypeImageDataView view,
+                                                      TypeImage* out) {
+  if (!view.ReadType(&out->type))
+    return false;
+
+  ArrayDataView<uint8_t> data;
+  view.GetDataDataView(&data);
+
+  out->data.assign(reinterpret_cast<const char*>(data.data()), data.size());
+  return true;
+}
+
+}  // namespace mojo
--- chrome/utility/extensions/extensions_handler.h
+++ chrome/utility/extensions/extensions_handler.h
@@ -23,15 +23,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class ChromeContentUtilityClient;
 
+namespace service_manager {
+class InterfaceRegistry;
+}
+
 namespace extensions {
 
 // Dispatches IPCs for Chrome extensions utility messages.
 class ExtensionsHandler : public UtilityMessageHandler {
  public:
-  explicit ExtensionsHandler(ChromeContentUtilityClient* utility_client);
+  ExtensionsHandler();
   ~ExtensionsHandler() override;
 
   static void PreSandboxStartup();
+
+  // TODO(noel): consider moving this API to the UtilityMessageHandler
+  // interface.
+  static void ExposeInterfacesToBrowser(
+      service_manager::InterfaceRegistry* registry,
+      ChromeContentUtilityClient* utility_client,
+      bool running_elevated);
 
   // UtilityMessageHandler:
   bool OnMessageReceived(const IPC::Message& message) override;
@@ -40,10 +51,6 @@ class ExtensionsHandler : public UtilityMessageHandler {
   // IPC message handlers.
   void OnCheckMediaFile(int64_t milliseconds_of_decoding,
                         const IPC::PlatformFileForTransit& media_file);
-
-  void OnParseMediaMetadata(const std::string& mime_type,
-                            int64_t total_size,
-                            bool get_attached_images);
 
 #if defined(OS_WIN)
   void OnParseITunesPrefXml(const std::string& itunes_xml_data);
@@ -66,9 +73,6 @@ class ExtensionsHandler : public UtilityMessageHandler {
 #endif  // defined(OS_WIN)
 
   UtilityHandler utility_handler_;
-
-  // The client that owns this.
-  ChromeContentUtilityClient* const utility_client_;
 
   DISALLOW_COPY_AND_ASSIGN(ExtensionsHandler);
 };
