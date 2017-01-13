2ceed6432c2ebaa6fe46c5ded59f4c3196d18df0
nisse <nisse@chromium.org>
2017-01-13 06:04:17 -0800

    Use new location of webrtc video frame headers, under webrtc/api.
    Replace a gn dependency on webrtc's common_video by video_frame_api.
    
    BUG=webrtc:5880
    
    Review-Url: https://codereview.chromium.org/2628283003
    Cr-Commit-Position: refs/heads/master@{#443552}

--- content/renderer/media/gpu/rtc_video_decoder.cc
+++ content/renderer/media/gpu/rtc_video_decoder.cc
@@ -19,10 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/bind_to_current_loop.h"
 #include "media/renderers/gpu_video_accelerator_factories.h"
 #include "third_party/skia/include/core/SkBitmap.h"
+#include "third_party/webrtc/api/video/video_frame.h"
 #include "third_party/webrtc/base/bind.h"
 #include "third_party/webrtc/base/refcount.h"
 #include "third_party/webrtc/modules/video_coding/codecs/h264/include/h264.h"
-#include "third_party/webrtc/video_frame.h"
 
 #if defined(OS_WIN)
 #include "base/command_line.h"
