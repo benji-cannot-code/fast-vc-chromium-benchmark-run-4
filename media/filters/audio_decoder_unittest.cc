6710f42cf27733f88084b112ddbbe0d15fca28f8
xhwang <xhwang@chromium.org>
2017-01-13 17:36:17 -0800

    media: Simplify audio test names
    Examples:
    
    AudioRendererMixerBehavioralTest/AudioRendererMixerBehavioralTest.OnRenderError
    -> AudioRendererMixerBehavioralTest.OnRenderError
    
    MediaCodecAudioDecoderTest/AudioDecoderTest.Reset/0
    -> MediaCodec/AudioDecoderTest.Reset/0
    
    Review-Url: https://codereview.chromium.org/2626283003
    Cr-Commit-Position: refs/heads/master@{#443760}

--- media/filters/audio_decoder_unittest.cc
+++ media/filters/audio_decoder_unittest.cc
@@ -381,8 +381,6 @@ class AudioDecoderTest : public testing::TestWithParam<DecoderTestData> {
   DISALLOW_COPY_AND_ASSIGN(AudioDecoderTest);
 };
 
-class FFmpegAudioDecoderBehavioralTest : public AudioDecoderTest {};
-
 TEST_P(AudioDecoderTest, Initialize) {
   SKIP_TEST_IF_NO_MEDIA_CODEC();
   ASSERT_NO_FATAL_FAILURE(Initialize());
@@ -484,7 +482,7 @@ const DecoderTestData kMediaCodecTests[] = {
 #endif
 };
 
-INSTANTIATE_TEST_CASE_P(MediaCodecAudioDecoderTest,
+INSTANTIATE_TEST_CASE_P(MediaCodec,
                         AudioDecoderTest,
                         testing::ValuesIn(kMediaCodecTests));
 #endif  // defined(OS_ANDROID)
@@ -572,16 +570,8 @@ const DecoderTestData kFFmpegTests[] = {
      CHANNEL_LAYOUT_STEREO},
 };
 
-// Dummy data for behavioral tests.
-const DecoderTestData kFFmpegBehavioralTest[] = {
-    {FFMPEG, kUnknownAudioCodec, "", NULL, 0, 0, CHANNEL_LAYOUT_NONE},
-};
-
-INSTANTIATE_TEST_CASE_P(FFmpegAudioDecoderTest,
+INSTANTIATE_TEST_CASE_P(FFmpeg,
                         AudioDecoderTest,
                         testing::ValuesIn(kFFmpegTests));
-INSTANTIATE_TEST_CASE_P(FFmpegAudioDecoderBehavioralTest,
-                        FFmpegAudioDecoderBehavioralTest,
-                        testing::ValuesIn(kFFmpegBehavioralTest));
 
 }  // namespace media
