a8555b2a464d3e48bfd30bbf9dec31f0ab04a22c
fgorski <fgorski@chromium.org>
2017-01-13 16:01:51 -0800

    [Android Omnibox] Fixing Emphasizer missing ConnectionSecurityLevel entry in switch
    Adding a case statement for HTTP_SHOW_WARNING with a fall-through same
    as for NONE per usage in LocationBarLayout.java
    
    BUG=681115
    R=tedchoc@chromium.org
    
    Review-Url: https://codereview.chromium.org/2629543011
    Cr-Commit-Position: refs/heads/master@{#443733}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/OmniboxUrlEmphasizer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/OmniboxUrlEmphasizer.java
@@ -153,6 +153,8 @@ public class OmniboxUrlEmphasizer {
                 switch (securityLevel) {
                     case ConnectionSecurityLevel.NONE:
                     // Intentional fall-through:
+                    case ConnectionSecurityLevel.HTTP_SHOW_WARNING:
+                    // Intentional fall-through:
                     case ConnectionSecurityLevel.SECURITY_WARNING:
                         break;
                     case ConnectionSecurityLevel.DANGEROUS:
