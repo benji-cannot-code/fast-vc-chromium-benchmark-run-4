56eede9209643cb7fd051b3e89bb5291cba3c3f5
stevenjb <stevenjb@chromium.org>
2017-01-13 16:12:24 -0800

    cr-network-icon: Protect against undefined state
    BUG=none
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2637473003
    Cr-Commit-Position: refs/heads/master@{#443739}

--- ui/webui/resources/cr_elements/network/cr_network_icon.js
+++ ui/webui/resources/cr_elements/network/cr_network_icon.js
@@ -36,6 +36,8 @@ Polymer({
    * @private
    */
   getIcon_: function() {
+    if (!this.networkState)
+      return '';
     let showDisconnected =
         !this.isListItem && (!this.networkState.ConnectionState ||
                              this.networkState.ConnectionState ==
@@ -93,6 +95,8 @@ Polymer({
    */
   getTechnology_: function() {
     let networkState = this.networkState;
+    if (!networkState)
+      return '';
     let type = networkState.Type;
     if (type == CrOnc.Type.WI_MAX)
       return 'network:4g';
@@ -141,6 +145,8 @@ Polymer({
    */
   showSecure_: function() {
     let networkState = this.networkState;
+    if (!this.networkState)
+      return false;
     if (networkState.Type == CrOnc.Type.WI_FI && networkState.WiFi) {
       let security = networkState.WiFi.Security;
       return !!security && security != 'None';
