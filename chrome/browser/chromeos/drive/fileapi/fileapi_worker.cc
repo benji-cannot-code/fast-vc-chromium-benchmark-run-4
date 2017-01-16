3c1fe04ab6a898e37046b93c19ca2bf878c3def8
fdoray <fdoray@chromium.org>
2017-01-16 07:13:10 -0800

    Use TaskScheduler instead of blocking pool in fileapi_worker.cc.
    The following traits are used:
    
    Priority: Inherited (default)
      The priority is inherited from the calling context (i.e. TaskTraits
      are initialized with the priority of the current task).
    
    Shutdown behavior: SKIP_ON_SHUTDOWN (default)
      Tasks posted with this mode that have not started executing at
      shutdown will never run. However, any task that has already begun
      executing when shutdown is invoked will be allowed to continue and
      will block shutdown until completion.
    
      Note: Previously, the task was posted to the blocking pool with
      BLOCK_SHUTDOWN (default in SequencedWorkerPool).
    
    May Block:
      Tasks posted with MayBlock() may block. This includes but is not
      limited to tasks that wait on synchronous file I/O operations:
      read or write a file from disk, interact with a pipe or a socket,
      rename or delete a file, enumerate files in a directory, etc. This
      trait isn't required for the mere use of locks.
    
    BUG=667892
    
    Review-Url: https://codereview.chromium.org/2625063003
    Cr-Commit-Position: refs/heads/master@{#443895}

--- chrome/browser/chromeos/drive/fileapi/fileapi_worker.cc
+++ chrome/browser/chromeos/drive/fileapi/fileapi_worker.cc
@@ -10,8 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/logging.h"
-#include "base/task_runner_util.h"
-#include "base/threading/sequenced_worker_pool.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/chromeos/drive/file_system_util.h"
 #include "components/drive/chromeos/file_system_interface.h"
@@ -186,11 +185,10 @@ void OpenFileAfterFileSystemOpenFile(int file_flags,
   }
 
   // Cache file prepared for modification is available. Open it locally.
-  bool posted = base::PostTaskAndReplyWithResult(
-      BrowserThread::GetBlockingPool(), FROM_HERE,
+  base::PostTaskWithTraitsAndReplyWithResult(
+      FROM_HERE, base::TaskTraits().MayBlock(),
       base::Bind(&OpenFile, local_path, file_flags),
       base::Bind(&RunOpenFileCallback, callback, close_callback));
-  DCHECK(posted);
 }
 
 }  // namespace
