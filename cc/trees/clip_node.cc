8a19ab30660422b6851799a820fd28fdafbb7544
ajuma <ajuma@chromium.org>
2017-01-16 14:27:52 -0800

    cc: Use kInvalidNodeId for invalid property tree node ids
    This replaces usage of -1 literals for referring to invalid property
    tree node ids with the (already-existing) kInvalidNodeId constant.
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2635103002
    Cr-Commit-Position: refs/heads/master@{#443954}

--- cc/trees/clip_node.cc
+++ cc/trees/clip_node.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/trace_event/trace_event_argument.h"
 #include "cc/base/math_util.h"
+#include "cc/layers/layer.h"
 #include "cc/proto/cc_conversions.h"
 #include "cc/proto/gfx_conversions.h"
 #include "cc/trees/clip_node.h"
@@ -14,13 +15,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace cc {
 
 ClipNode::ClipNode()
-    : id(-1),
-      parent_id(-1),
-      owning_layer_id(-1),
+    : id(ClipTree::kInvalidNodeId),
+      parent_id(ClipTree::kInvalidNodeId),
+      owning_layer_id(Layer::INVALID_ID),
       clip_type(ClipType::NONE),
-      transform_id(-1),
-      target_transform_id(-1),
-      target_effect_id(-1),
+      transform_id(TransformTree::kInvalidNodeId),
+      target_transform_id(TransformTree::kInvalidNodeId),
+      target_effect_id(EffectTree::kInvalidNodeId),
       layer_clipping_uses_only_local_clip(false),
       layers_are_clipped(false),
       layers_are_clipped_when_surfaces_disabled(false),
