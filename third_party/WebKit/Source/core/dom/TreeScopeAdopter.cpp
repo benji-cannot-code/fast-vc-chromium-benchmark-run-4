8f952fe6de09ba674aed45a6b1c23aab9ea3e439
wjmaclean <wjmaclean@chromium.org>
2017-01-16 10:06:29 -0800

    Detach Event Listener Properties before moving Node.
    In preparation for storing EventListenerProperties on a per-frame basis,
    this CL makes sure old properties are cleared before moving the node
    iterators between the frameHosts.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2585353002
    Cr-Commit-Position: refs/heads/master@{#443920}

--- third_party/WebKit/Source/core/dom/TreeScopeAdopter.cpp
+++ third_party/WebKit/Source/core/dom/TreeScopeAdopter.cpp
@@ -129,6 +129,8 @@ inline void TreeScopeAdopter::moveNodeToNewDocument(
     Document& oldDocument,
     Document& newDocument) const {
   DCHECK_NE(oldDocument, newDocument);
+  // Note: at the start of this function, node.document() may already have
+  // changed to match |newDocument|, which is why |oldDocument| is passed in.
 
   if (node.hasRareData()) {
     NodeRareData* rareData = node.rareData();
@@ -136,6 +138,7 @@ inline void TreeScopeAdopter::moveNodeToNewDocument(
       rareData->nodeLists()->adoptDocument(oldDocument, newDocument);
   }
 
+  node.willMoveToNewDocument(oldDocument, newDocument);
   oldDocument.moveNodeIteratorsToNewDocument(node, newDocument);
 
   if (node.getCustomElementState() == CustomElementState::Custom) {
