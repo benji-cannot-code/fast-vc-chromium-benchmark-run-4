22750e2ba8c622e7eec3817863f438c650cd68a1
pkalinnikov <pkalinnikov@chromium.org>
2017-01-16 06:11:32 -0800

    Revert of mediaview: Enable by default. (patchset #1 id:1 of https://codereview.chromium.org/2638713002/ )
    Reason for revert:
    ArcSessionManagerTest.ConsumerAccount and 3 more ArcSessionManagerTest.* tests are failing on "Linux Chromium OS ASan LSan Tests (1)" builder.
    
    Original issue's description:
    > mediaview: Enable by default.
    >
    > Now all required changes are in. Let us enable the feature by
    > default.
    >
    > BUG=chromium:671511
    > TEST=Media view works on samus without flags
    >
    > Review-Url: https://codereview.chromium.org/2638713002
    > Cr-Commit-Position: refs/heads/master@{#443855}
    > Committed: https://chromium.googlesource.com/chromium/src/+/6d266375ae1ebcd12933e946a1ce455fbec9b7b6
    
    TBR=hidehiko@chromium.org,nya@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=chromium:671511
    
    Review-Url: https://codereview.chromium.org/2635943002
    Cr-Commit-Position: refs/heads/master@{#443888}

--- chrome/browser/chromeos/arc/fileapi/arc_media_view_util.cc
+++ chrome/browser/chromeos/arc/fileapi/arc_media_view_util.cc
@@ -14,7 +14,7 @@ constexpr char kMediaViewVolumeIdPrefix[] = "media_view:";
 }  // namespace
 
 const base::Feature kMediaViewFeature{"ArcMediaView",
-                                      base::FEATURE_ENABLED_BY_DEFAULT};
+                                      base::FEATURE_DISABLED_BY_DEFAULT};
 
 const char kMediaDocumentsProviderAuthority[] =
     "com.android.providers.media.documents";
