1278552de138dc14a4f8a4abc2d711accb479fa6
sdefresne <sdefresne@chromium.org>
2017-01-17 09:41:26 -0800

    Remove unused method ChromeBrowserProvider::DataReductionProxyAvailability.
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2630353002
    Cr-Commit-Position: refs/heads/master@{#444084}

--- ios/public/provider/chrome/browser/chrome_browser_provider.mm
+++ ios/public/provider/chrome/browser/chrome_browser_provider.mm
@@ -54,10 +54,6 @@ ChromeBrowserProvider::GetGeolocationUpdaterProvider() {
   return nullptr;
 }
 
-std::string ChromeBrowserProvider::DataReductionProxyAvailability() {
-  return "default";
-}
-
 std::string ChromeBrowserProvider::GetRiskData() {
   return std::string();
 }
