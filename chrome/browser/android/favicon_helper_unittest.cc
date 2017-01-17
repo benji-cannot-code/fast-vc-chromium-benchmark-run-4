8732245aaa847b806c97ac2aaa2c33b9877b4536
michaelbai <michaelbai@chromium.org>
2017-01-17 12:51:26 -0800

    Fix the crash in favicon::GetLargestSizeIndex.
    BUG=678817
    
    Review-Url: https://codereview.chromium.org/2630673002
    Cr-Commit-Position: refs/heads/master@{#444135}

---
+++ chrome/browser/android/favicon_helper_unittest.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/android/favicon_helper.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+TEST(FaviconHelperTest, GetLargestSizeIndex) {
+  std::vector<gfx::Size> sizes;
+  gfx::Size size1 = gfx::Size(INT_MAX, INT_MAX);
+  sizes.push_back(size1);
+  gfx::Size size2 = gfx::Size(16, 16);
+  sizes.push_back(size2);
+  gfx::Size size3 = gfx::Size(32, 32);
+  sizes.push_back(size3);
+  EXPECT_EQ(2u, FaviconHelper::GetLargestSizeIndex(sizes));
+  sizes.clear();
+  sizes.push_back(size1);
+  sizes.push_back(size1);
+  EXPECT_EQ(0u, FaviconHelper::GetLargestSizeIndex(sizes));
+}
