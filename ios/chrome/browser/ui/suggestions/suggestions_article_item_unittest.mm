dc72eab3b8d8880a2c250f877637849e41e313b5
gambard <gambard@chromium.org>
2017-01-17 08:29:20 -0800

    Use ARC for Suggestions tests
    BUG=679369
    
    Review-Url: https://codereview.chromium.org/2633243003
    Cr-Commit-Position: refs/heads/master@{#444068}

--- ios/chrome/browser/ui/suggestions/suggestions_article_item_unittest.mm
+++ ios/chrome/browser/ui/suggestions/suggestions_article_item_unittest.mm
@@ -5,11 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/suggestions/suggestions_article_item.h"
 
-#import "ios/chrome/browser/ui/collection_view/cells/collection_view_item.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
 
 namespace {
 
+// Tests that configureCell: set all the fields of the cell.
 TEST(SuggestionsArticleItemTest, CellIsConfigured) {
   NSString* title = @"testTitle";
   NSString* subtitle = @"testSubtitle";
--- ios/chrome/browser/ui/suggestions/suggestions_expandable_item_unittest.mm
+++ ios/chrome/browser/ui/suggestions/suggestions_expandable_item_unittest.mm
@@ -5,10 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/suggestions/suggestions_expandable_item.h"
 
-#import "ios/chrome/browser/ui/collection_view/cells/collection_view_item.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #import "third_party/ocmock/OCMock/OCMock.h"
 #include "third_party/ocmock/gtest_support.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
 
 // Test subclass of the SuggestionsExpandableCell.
 @interface TestSuggestionsExpandableCell : SuggestionsExpandableCell
