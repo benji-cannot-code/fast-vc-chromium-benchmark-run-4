95574bf2d1facb2233b5ad5e8f5f79f51d40b6bb
xiyuan <xiyuan@chromium.org>
2017-01-17 14:23:31 -0800

    kiosk: Add kiosk.always_update manifest key
    If the key is specified and its value is true, it allows the app to
    be always updated regardless of whether the underlying platform is
    compliant or not. If the value is false or the key is not specified,
    the required platform version is respected and the app update is
    deferred until the underlying platform becomes compliant.
    
    BUG=673527
    
    Review-Url: https://codereview.chromium.org/2615633010
    Cr-Commit-Position: refs/heads/master@{#444163}

--- extensions/common/manifest_constants.cc
+++ extensions/common/manifest_constants.cc
@@ -69,6 +69,7 @@ const char kJs[] = "js";
 const char kKey[] = "key";
 const char kKeycode[] = "keyCode";
 const char kKiosk[] = "kiosk";
+const char kKioskAlwaysUpdate[] = "kiosk.always_update";
 const char kKioskEnabled[] = "kiosk_enabled";
 const char kKioskOnly[] = "kiosk_only";
 const char kKioskMode[] = "kiosk_mode";
@@ -473,6 +474,8 @@ const char kInvalidKeyBindingTooMany[] =
 const char kInvalidKeyBindingUnknownPlatform[] =
     "Unknown platform for 'command[*]': *. Valid values are: 'windows', 'mac'"
     " 'chromeos', 'linux' and 'default'.";
+const char kInvalidKioskAlwaysUpdate[] =
+    "Invalid value for 'kiosk.always_update'.";
 const char kInvalidKioskEnabled[] =
     "Invalid value for 'kiosk_enabled'.";
 const char kInvalidKioskOnly[] =
--- extensions/common/manifest_handlers/kiosk_mode_info.h
+++ extensions/common/manifest_handlers/kiosk_mode_info.h
@@ -26,7 +26,8 @@ struct KioskModeInfo : public Extension::ManifestData {
 
   KioskModeInfo(KioskStatus kiosk_status,
                 const std::vector<std::string>& secondary_app_ids,
-                const std::string& required_platform_version);
+                const std::string& required_platform_version,
+                bool always_update);
   ~KioskModeInfo() override;
 
   // Gets the KioskModeInfo for |extension|, or NULL if none was
@@ -52,6 +53,7 @@ struct KioskModeInfo : public Extension::ManifestData {
   const std::vector<std::string> secondary_app_ids;
 
   const std::string required_platform_version;
+  const bool always_update;
 };
 
 // Parses the "kiosk_enabled" and "kiosk_only" manifest keys.
