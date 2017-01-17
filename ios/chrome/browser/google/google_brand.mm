174f5f222499d46fa06e5877ca592c2ad13dd1f8
sdefresne <sdefresne@chromium.org>
2017-01-17 06:07:26 -0800

    Access GetDistributionBrandCode() via AppDistributionProvider.
    There is two way to access GetDistributionBrandCode(), either directly
    via the ChromeBrowserProvider or via AppDistributionProvider. Access
    through AppDistributionProvider is recommended.
    
    This is the first step, change all client code to go through the
    AppDistributionProvider so that the downstream override can safely
    be removed.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2631213002
    Cr-Commit-Position: refs/heads/master@{#444042}

--- ios/chrome/browser/google/google_brand.mm
+++ ios/chrome/browser/google/google_brand.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/google/google_brand.h"
 
 #include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
+#include "ios/public/provider/chrome/browser/distribution/app_distribution_provider.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -18,7 +19,9 @@ bool GetBrand(std::string* brand) {
   if (!ios::GetChromeBrowserProvider())
     return false;
 
-  brand->assign(ios::GetChromeBrowserProvider()->GetDistributionBrandCode());
+  brand->assign(ios::GetChromeBrowserProvider()
+                    ->GetAppDistributionProvider()
+                    ->GetDistributionBrandCode());
   return true;
 }
 
