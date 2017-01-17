04e4afaa8f5460dfa79de1d5f94eb2da20537c03
horo <horo@chromium.org>
2017-01-17 01:57:53 -0800

    Stop using DEFINE_STATIC_LOCAL in ResourceResponse.cpp.
    There are several AtomicStrings defined using DEFINE_STATIC_LOCAL in ResourceResponse.cpp.
    When those strings are touched on the worker thread, the ThreadRestrictionVerifier check in StringImpl.h fails.
    https://crbug.com/681581#c5
    
    To avoid this check failure, this CL makes those strings static.
    
    BUG=681581
    
    Review-Url: https://codereview.chromium.org/2638623004
    Cr-Commit-Position: refs/heads/master@{#444016}

--- third_party/WebKit/Source/platform/network/ResourceResponse.cpp
+++ third_party/WebKit/Source/platform/network/ResourceResponse.cpp
@@ -46,6 +46,9 @@ Vector<Interface> isolatedCopy(const Vector<Interface>& src) {
   return result;
 }
 
+static const char cacheControlHeader[] = "cache-control";
+static const char pragmaHeader[] = "pragma";
+
 }  // namespace
 
 ResourceResponse::SignedCertificateTimestamp::SignedCertificateTimestamp(
@@ -358,28 +361,16 @@ const AtomicString& ResourceResponse::httpHeaderField(
   return m_httpHeaderFields.get(name);
 }
 
-static const AtomicString& cacheControlHeaderString() {
-  DEFINE_STATIC_LOCAL(const AtomicString, cacheControlHeader,
-                      ("cache-control"));
-  return cacheControlHeader;
-}
-
-static const AtomicString& pragmaHeaderString() {
-  DEFINE_STATIC_LOCAL(const AtomicString, pragmaHeader, ("pragma"));
-  return pragmaHeader;
-}
-
 void ResourceResponse::updateHeaderParsedState(const AtomicString& name) {
-  DEFINE_STATIC_LOCAL(const AtomicString, ageHeader, ("age"));
-  DEFINE_STATIC_LOCAL(const AtomicString, dateHeader, ("date"));
-  DEFINE_STATIC_LOCAL(const AtomicString, expiresHeader, ("expires"));
-  DEFINE_STATIC_LOCAL(const AtomicString, lastModifiedHeader,
-                      ("last-modified"));
+  static const char ageHeader[] = "age";
+  static const char dateHeader[] = "date";
+  static const char expiresHeader[] = "expires";
+  static const char lastModifiedHeader[] = "last-modified";
 
   if (equalIgnoringCase(name, ageHeader))
     m_haveParsedAgeHeader = false;
-  else if (equalIgnoringCase(name, cacheControlHeaderString()) ||
-           equalIgnoringCase(name, pragmaHeaderString()))
+  else if (equalIgnoringCase(name, cacheControlHeader) ||
+           equalIgnoringCase(name, pragmaHeader))
     m_cacheControlHeader = CacheControlHeader();
   else if (equalIgnoringCase(name, dateHeader))
     m_haveParsedDateHeader = false;
@@ -441,50 +432,54 @@ const HTTPHeaderMap& ResourceResponse::httpHeaderFields() const {
 }
 
 bool ResourceResponse::cacheControlContainsNoCache() const {
-  if (!m_cacheControlHeader.parsed)
-    m_cacheControlHeader = parseCacheControlDirectives(
-        m_httpHeaderFields.get(cacheControlHeaderString()),
-        m_httpHeaderFields.get(pragmaHeaderString()));
+  if (!m_cacheControlHeader.parsed) {
+    m_cacheControlHeader =
+        parseCacheControlDirectives(m_httpHeaderFields.get(cacheControlHeader),
+                                    m_httpHeaderFields.get(pragmaHeader));
+  }
   return m_cacheControlHeader.containsNoCache;
 }
 
 bool ResourceResponse::cacheControlContainsNoStore() const {
-  if (!m_cacheControlHeader.parsed)
-    m_cacheControlHeader = parseCacheControlDirectives(
-        m_httpHeaderFields.get(cacheControlHeaderString()),
-        m_httpHeaderFields.get(pragmaHeaderString()));
+  if (!m_cacheControlHeader.parsed) {
+    m_cacheControlHeader =
+        parseCacheControlDirectives(m_httpHeaderFields.get(cacheControlHeader),
+                                    m_httpHeaderFields.get(pragmaHeader));
+  }
   return m_cacheControlHeader.containsNoStore;
 }
 
 bool ResourceResponse::cacheControlContainsMustRevalidate() const {
-  if (!m_cacheControlHeader.parsed)
-    m_cacheControlHeader = parseCacheControlDirectives(
-        m_httpHeaderFields.get(cacheControlHeaderString()),
-        m_httpHeaderFields.get(pragmaHeaderString()));
+  if (!m_cacheControlHeader.parsed) {
+    m_cacheControlHeader =
+        parseCacheControlDirectives(m_httpHeaderFields.get(cacheControlHeader),
+                                    m_httpHeaderFields.get(pragmaHeader));
+  }
   return m_cacheControlHeader.containsMustRevalidate;
 }
 
 bool ResourceResponse::hasCacheValidatorFields() const {
-  DEFINE_STATIC_LOCAL(const AtomicString, lastModifiedHeader,
-                      ("last-modified"));
-  DEFINE_STATIC_LOCAL(const AtomicString, eTagHeader, ("etag"));
+  static const char lastModifiedHeader[] = "last-modified";
+  static const char eTagHeader[] = "etag";
   return !m_httpHeaderFields.get(lastModifiedHeader).isEmpty() ||
          !m_httpHeaderFields.get(eTagHeader).isEmpty();
 }
 
 double ResourceResponse::cacheControlMaxAge() const {
-  if (!m_cacheControlHeader.parsed)
-    m_cacheControlHeader = parseCacheControlDirectives(
-        m_httpHeaderFields.get(cacheControlHeaderString()),
-        m_httpHeaderFields.get(pragmaHeaderString()));
+  if (!m_cacheControlHeader.parsed) {
+    m_cacheControlHeader =
+        parseCacheControlDirectives(m_httpHeaderFields.get(cacheControlHeader),
+                                    m_httpHeaderFields.get(pragmaHeader));
+  }
   return m_cacheControlHeader.maxAge;
 }
 
 double ResourceResponse::cacheControlStaleWhileRevalidate() const {
-  if (!m_cacheControlHeader.parsed)
-    m_cacheControlHeader = parseCacheControlDirectives(
-        m_httpHeaderFields.get(cacheControlHeaderString()),
-        m_httpHeaderFields.get(pragmaHeaderString()));
+  if (!m_cacheControlHeader.parsed) {
+    m_cacheControlHeader =
+        parseCacheControlDirectives(m_httpHeaderFields.get(cacheControlHeader),
+                                    m_httpHeaderFields.get(pragmaHeader));
+  }
   return m_cacheControlHeader.staleWhileRevalidate;
 }
 
@@ -505,7 +500,7 @@ static double parseDateValueInHeader(const HTTPHeaderMap& headers,
 
 double ResourceResponse::date() const {
   if (!m_haveParsedDateHeader) {
-    DEFINE_STATIC_LOCAL(const AtomicString, headerName, ("date"));
+    static const char headerName[] = "date";
     m_date = parseDateValueInHeader(m_httpHeaderFields, headerName);
     m_haveParsedDateHeader = true;
   }
@@ -514,7 +509,7 @@ double ResourceResponse::date() const {
 
 double ResourceResponse::age() const {
   if (!m_haveParsedAgeHeader) {
-    DEFINE_STATIC_LOCAL(const AtomicString, headerName, ("age"));
+    static const char headerName[] = "age";
     const AtomicString& headerValue = m_httpHeaderFields.get(headerName);
     bool ok;
     m_age = headerValue.toDouble(&ok);
@@ -527,7 +522,7 @@ double ResourceResponse::age() const {
 
 double ResourceResponse::expires() const {
   if (!m_haveParsedExpiresHeader) {
-    DEFINE_STATIC_LOCAL(const AtomicString, headerName, ("expires"));
+    static const char headerName[] = "expires";
     m_expires = parseDateValueInHeader(m_httpHeaderFields, headerName);
     m_haveParsedExpiresHeader = true;
   }
@@ -536,7 +531,7 @@ double ResourceResponse::expires() const {
 
 double ResourceResponse::lastModified() const {
   if (!m_haveParsedLastModifiedHeader) {
-    DEFINE_STATIC_LOCAL(const AtomicString, headerName, ("last-modified"));
+    static const char headerName[] = "last-modified";
     m_lastModified = parseDateValueInHeader(m_httpHeaderFields, headerName);
     m_haveParsedLastModifiedHeader = true;
   }
@@ -544,13 +539,13 @@ double ResourceResponse::lastModified() const {
 }
 
 bool ResourceResponse::isAttachment() const {
-  DEFINE_STATIC_LOCAL(const AtomicString, headerName, ("content-disposition"));
+  static const char headerName[] = "content-disposition";
+  static const char attachmentString[] = "attachment";
   String value = m_httpHeaderFields.get(headerName);
   size_t loc = value.find(';');
   if (loc != kNotFound)
     value = value.left(loc);
   value = value.stripWhiteSpace();
-  DEFINE_STATIC_LOCAL(const AtomicString, attachmentString, ("attachment"));
   return equalIgnoringCase(value, attachmentString);
 }
 
--- third_party/WebKit/Source/platform/network/ResourceResponseTest.cpp
+++ third_party/WebKit/Source/platform/network/ResourceResponseTest.cpp
@@ -5,9 +5,44 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "platform/network/ResourceResponse.h"
 
+#include "platform/CrossThreadFunctional.h"
+#include "platform/WebTaskRunner.h"
+#include "public/platform/Platform.h"
+#include "public/platform/WebThread.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace blink {
+
+namespace {
+
+ResourceResponse createTestResponse() {
+  ResourceResponse response;
+  response.addHTTPHeaderField("age", "0");
+  response.addHTTPHeaderField("cache-control", "no-cache");
+  response.addHTTPHeaderField("date", "Tue, 17 Jan 2017 04:01:00 GMT");
+  response.addHTTPHeaderField("expires", "Tue, 17 Jan 2017 04:11:00 GMT");
+  response.addHTTPHeaderField("last-modified", "Tue, 17 Jan 2017 04:00:00 GMT");
+  response.addHTTPHeaderField("pragma", "public");
+  response.addHTTPHeaderField("etag", "abc");
+  response.addHTTPHeaderField("content-disposition",
+                              "attachment; filename=a.txt");
+  return response;
+}
+
+void runHeaderRelatedTest(const ResourceResponse& response) {
+  EXPECT_EQ(0, response.age());
+  EXPECT_NE(0, response.date());
+  EXPECT_NE(0, response.expires());
+  EXPECT_NE(0, response.lastModified());
+  EXPECT_EQ(true, response.cacheControlContainsNoCache());
+}
+
+void runInThread() {
+  ResourceResponse response(createTestResponse());
+  runHeaderRelatedTest(response);
+}
+
+}  // namespace
 
 TEST(ResourceResponseTest, SignedCertificateTimestampIsolatedCopy) {
   ResourceResponse::SignedCertificateTimestamp src(
@@ -31,6 +66,18 @@ TEST(ResourceResponseTest, SignedCertificateTimestampIsolatedCopy) {
   EXPECT_NE(src.m_signatureAlgorithm.impl(), dest.m_signatureAlgorithm.impl());
   EXPECT_EQ(src.m_signatureData, dest.m_signatureData);
   EXPECT_NE(src.m_signatureData.impl(), dest.m_signatureData.impl());
+}
+
+TEST(ResourceResponseTest, CrossThreadAtomicStrings) {
+  // This test checks that AtomicStrings in ResourceResponse doesn't cause the
+  // failure of ThreadRestrictionVerifier check.
+  ResourceResponse response(createTestResponse());
+  runHeaderRelatedTest(response);
+  std::unique_ptr<WebThread> thread =
+      WTF::wrapUnique(Platform::current()->createThread("WorkerThread"));
+  thread->getWebTaskRunner()->postTask(BLINK_FROM_HERE,
+                                       crossThreadBind(&runInThread));
+  thread.reset();
 }
 
 }  // namespace blink
