98a66068864a1934e228756d9a80909bc9ca80f2
nya <nya@chromium.org>
2017-01-16 22:50:57 -0800

    Avoid leaving dangling pointers to User in ProfileHelper.
    FakeChromeUserManager::AddUser() calls
    ProfileHelper::SetProfileToUserMappingForTesting() to inject a new user
    to ProfileHelper. However, FakeChromeUserManager does not remove
    the added users from ProfileHelper unless we explicitly call
    FakeChromeUserManager::RemoveUserFromList(), which ends up leaving
    dangling pointers in ProfileHelper.user_list_for_testing_.
    
    This caused use-after-free in crrev.com/2638713002 because
    ProfileHelper::GetUserByProfile() is called in VolumeManager::Shutdown()
    after a FakeChromeUserManager is destructed.
    
    BUG=chromium:681752
    TEST=browser_tests --gtest_filter='ArcSessionManagerTest.*'  # with ASAN
    
    Review-Url: https://codereview.chromium.org/2639483003
    Cr-Commit-Position: refs/heads/master@{#443993}

--- chrome/browser/chromeos/arc/arc_session_manager_browsertest.cc
+++ chrome/browser/chromeos/arc/arc_session_manager_browsertest.cc
@@ -62,6 +62,7 @@ constexpr char kManagedAuthToken[] = "managed-auth-token";
 constexpr char kUnmanagedAuthToken[] = "unmanaged-auth-token";
 constexpr char kWellKnownConsumerName[] = "test@gmail.com";
 constexpr char kFakeUserName[] = "test@example.com";
+constexpr char kFakeGaiaId[] = "1234567890";
 
 }  // namespace
 
@@ -184,7 +185,7 @@ class ArcSessionManagerTest : public InProcessBrowserTest {
     profile()->GetPrefs()->SetBoolean(prefs::kArcTermsAccepted, true);
 
     const AccountId account_id(
-        AccountId::FromUserEmailGaiaId(kFakeUserName, "1234567890"));
+        AccountId::FromUserEmailGaiaId(kFakeUserName, kFakeGaiaId));
     GetFakeUserManager()->AddUser(account_id);
     GetFakeUserManager()->LoginUser(account_id);
 
@@ -193,6 +194,13 @@ class ArcSessionManagerTest : public InProcessBrowserTest {
   }
 
   void TearDownOnMainThread() override {
+    // Explicitly removing the user is required; otherwise ProfileHelper keeps
+    // a dangling pointer to the User.
+    // TODO(nya): Consider removing all users from ProfileHelper in the
+    // destructor of FakeChromeUserManager.
+    const AccountId account_id(
+        AccountId::FromUserEmailGaiaId(kFakeUserName, kFakeGaiaId));
+    GetFakeUserManager()->RemoveUserFromList(account_id);
     ArcSessionManager::Get()->Shutdown();
     ArcServiceManager::Get()->Shutdown();
     profile_.reset();
