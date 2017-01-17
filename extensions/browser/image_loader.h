b540494d39d36fa46614857917d0e5d66a62793b
estade <estade@chromium.org>
2017-01-17 12:37:47 -0800

    Load extension icons for more scale factors.
    The "supported" scale factors are just the ones that we load raster
    assets for (in ResourceBundle). This patch additionally looks for
    icons in scale factors that correlate with the currently attached
    displays.
    
    Practically speaking, this means a machine that's running at 1.5x such
    as a Surface Pro will have better looking extension icons in the
    renderer context menu.
    
    BUG=596757
    
    Review-Url: https://codereview.chromium.org/2609853003
    Cr-Commit-Position: refs/heads/master@{#444128}

--- extensions/browser/image_loader.h
+++ extensions/browser/image_loader.h
@@ -50,7 +50,7 @@ class ImageLoader : public KeyedService {
     ImageRepresentation(const ExtensionResource& resource,
                         ResizeCondition resize_condition,
                         const gfx::Size& desired_size,
-                        ui::ScaleFactor scale_factor);
+                        float scale_factor);
     ~ImageRepresentation();
 
     // Extension resource to load.
@@ -63,7 +63,7 @@ class ImageLoader : public KeyedService {
     gfx::Size desired_size;
 
     // |scale_factor| is used to construct the loaded gfx::ImageSkia.
-    ui::ScaleFactor scale_factor;
+    float scale_factor;
   };
 
   struct LoadResult;
@@ -88,8 +88,10 @@ class ImageLoader : public KeyedService {
                       const gfx::Size& max_size,
                       const ImageLoaderImageCallback& callback);
 
-  // Loads and returns a gfx::Image that has representations at all supported
-  // scale factors.
+  // Loads a gfx::Image that has representations at all scale factors we are
+  // likely to care about. That includes every scale for which we pack resources
+  // in ResourceBundle plus the scale for all currently attached displays. The
+  // image is returned via |callback|.
   void LoadImageAtEveryScaleFactorAsync(
       const Extension* extension,
       const gfx::Size& dip_size,
