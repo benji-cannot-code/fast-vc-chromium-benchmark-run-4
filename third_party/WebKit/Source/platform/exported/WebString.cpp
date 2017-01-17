64f0892346d22c601dd1f990fafbfdce258bd23e
kinuko <kinuko@chromium.org>
2017-01-17 01:08:47 -0800

    Allow WebString.utf8() to take UTF8ConversionMode
    Currently WebString.utf8() always converts the internal string to UTF-8
    with LenientUTF8Conversion mode (if the string is not ascii), but
    WebString::fromUTF8() only takes valid, strict UTF-8 string.
    
    therefore
    the same string may not be able to be converted back to WebString.
    
    This has been causing a few issues:
    - UTF16 (WTFString) -> UTF8 -> UTF16 (WTFString) round trip doesn't work
    - Works differently from base::UTF16ToUTF8, and some code explicitly
      calls base version via .utf16() to avoid any problems while it could
      be more costly than .utf8() if the internal string is ascii/latin.
    
    Similar but opposite attempt has been made in the past in
    https://codereview.chromium.org/1768063002/
    but hadn't landed as leniently converted UTF8 is not valid and probably
    should not be accepted.
    
    BUG=667131
    
    Review-Url: https://codereview.chromium.org/2629573002
    Cr-Commit-Position: refs/heads/master@{#444008}

--- third_party/WebKit/Source/platform/exported/WebString.cpp
+++ third_party/WebKit/Source/platform/exported/WebString.cpp
@@ -39,6 +39,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/text/StringView.h"
 #include "wtf/text/WTFString.h"
 
+#define STATIC_ASSERT_ENUM(a, b)                            \
+  static_assert(static_cast<int>(a) == static_cast<int>(b), \
+                "mismatching enums: " #a)
+
+STATIC_ASSERT_ENUM(WTF::LenientUTF8Conversion,
+                   blink::WebString::UTF8ConversionMode::kLenient);
+STATIC_ASSERT_ENUM(WTF::StrictUTF8Conversion,
+                   blink::WebString::UTF8ConversionMode::kStrict);
+STATIC_ASSERT_ENUM(
+    WTF::StrictUTF8ConversionReplacingUnpairedSurrogatesWithFFFD,
+    blink::WebString::UTF8ConversionMode::kStrictReplacingErrorsWithFFFD);
+
 namespace blink {
 
 void WebString::reset() {
@@ -69,8 +81,9 @@ const WebUChar* WebString::data16() const {
   return !m_private.isNull() && !is8Bit() ? m_private->characters16() : 0;
 }
 
-std::string WebString::utf8() const {
-  StringUTF8Adaptor utf8(m_private.get());
+std::string WebString::utf8(UTF8ConversionMode mode) const {
+  StringUTF8Adaptor utf8(m_private.get(),
+                         static_cast<WTF::UTF8ConversionMode>(mode));
   return std::string(utf8.data(), utf8.length());
 }
 
