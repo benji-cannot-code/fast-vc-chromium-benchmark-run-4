c9cdb8d795f2027f8fd1e2dd330ac9911c289947
lanwei <lanwei@chromium.org>
2017-01-17 10:31:02 -0800

    Replace touch actions in pointer event tests with pointerActionSequence
    Replace touchScrollInTarget and pinchZoomInTarget in pointerevent_common_input.js
    with pointerActionSequence.
    
    BUG=680330
    
    Review-Url: https://codereview.chromium.org/2627463002
    Cr-Commit-Position: refs/heads/master@{#444096}

--- content/renderer/gpu/actions_parser.cc
+++ content/renderer/gpu/actions_parser.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/values.h"
+#include "cc/output/begin_frame_args.h"
 
 namespace content {
 
@@ -132,9 +133,6 @@ bool ActionsParser::ParsePointerActions(const base::DictionaryValue& pointer) {
     return false;
   }
 
-  if (actions->GetSize() > longest_action_sequence_)
-    longest_action_sequence_ = actions->GetSize();
-
   if (!ParseActions(*actions))
     return false;
 
@@ -153,6 +151,10 @@ bool ActionsParser::ParseActions(const base::ListValue& actions) {
       return false;
     }
   }
+
+  if (param_list.size() > longest_action_sequence_)
+    longest_action_sequence_ = param_list.size();
+
   pointer_actions_list_.push_back(param_list);
   return true;
 }
@@ -191,6 +193,24 @@ bool ActionsParser::ParseAction(
     return false;
   }
 
+  double duration = 0;
+  int num_idle = 0;
+  if (pointer_action_type ==
+      SyntheticPointerActionParams::PointerActionType::IDLE) {
+    num_idle = 1;
+    if (action.HasKey("duration") && !action.GetDouble("duration", &duration)) {
+      error_message_ = base::StringPrintf(
+          "actions[%d].actions.x is not a number", action_index_);
+      return false;
+    }
+  }
+
+  // If users pause for given seconds, we convert to the number of idle frames.
+  if (duration > 0) {
+    num_idle = static_cast<int>(std::ceil(
+        duration / cc::BeginFrameArgs::DefaultInterval().InSecondsF()));
+  }
+
   SyntheticPointerActionParams action_param(pointer_action_type);
   action_param.set_index(action_index_);
   if (pointer_action_type ==
@@ -200,6 +220,12 @@ bool ActionsParser::ParseAction(
     action_param.set_position(gfx::PointF(position_x, position_y));
   }
   param_list.push_back(action_param);
+
+  // We queue all the IDLE actions in the action parameter list to make sure we
+  // will pause long enough on the given pointer.
+  for (int count = 1; count < num_idle; ++count)
+    param_list.push_back(action_param);
+
   return true;
 }
 
