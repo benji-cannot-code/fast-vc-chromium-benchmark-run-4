b095b4d763b0174d85194a93b3873bea446a55e0
aazzam <aazzam@google.com>
2017-01-16 17:52:11 -0800

    Implements CSSPropertyAPI for the stroke-dasharray property.
    A part of Project Ribbon, separating the parsing logic for CSS
    properties from the parser into an API. This patch removes
    CSSPropertyStrokeDasharray from the switch statement in
    parseSingleValue, and calls the API instead.
    
    A function pointer to the parseSingleValue function from the API for the
    page property is stored in a CSSPropertyDescriptor, and is called from
    CSSPropertyParser.
    
    This patch:
    - Adds CSSPropertyAPIStrokeDasharray.cpp to the BUILD.gn file.
    - Adds api_class flag to CSSProperties.in, which indicates that
      CSSPropertyAPIStrokeDasharray.h is generated.
    - Moves the parsing logic for page from CSSPropertyParser.cpp to
      CSSPropertyAPIStrokeDasharray.cpp, which implements CSSPropertyAPI.h.
    
    BUG=668012
    
    Review-Url: https://codereview.chromium.org/2610893003
    Cr-Commit-Position: refs/heads/master@{#443968}

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
@@ -1712,22 +1712,6 @@ static CSSValue* consumeNoneOrURI(CSSParserTokenRange& range) {
   return consumeUrl(range);
 }
 
-static CSSValue* consumeStrokeDasharray(CSSParserTokenRange& range) {
-  CSSValueID id = range.peek().id();
-  if (id == CSSValueNone)
-    return consumeIdent(range);
-
-  CSSValueList* dashes = CSSValueList::createCommaSeparated();
-  do {
-    CSSPrimitiveValue* dash =
-        consumeLengthOrPercent(range, SVGAttributeMode, ValueRangeNonNegative);
-    if (!dash || (consumeCommaIncludingWhitespace(range) && range.atEnd()))
-      return nullptr;
-    dashes->append(*dash);
-  } while (!range.atEnd());
-  return dashes;
-}
-
 static CSSValue* consumeBaselineShift(CSSParserTokenRange& range) {
   CSSValueID id = range.peek().id();
   if (id == CSSValueBaseline || id == CSSValueSub || id == CSSValueSuper)
@@ -3333,8 +3317,6 @@ const CSSValue* CSSPropertyParser::parseSingleValue(
     case CSSPropertyFlexGrow:
     case CSSPropertyFlexShrink:
       return consumeNumber(m_range, ValueRangeNonNegative);
-    case CSSPropertyStrokeDasharray:
-      return consumeStrokeDasharray(m_range);
     case CSSPropertyColumnRuleWidth:
       return consumeColumnRuleWidth(m_range, m_context->mode());
     case CSSPropertyStrokeOpacity:
