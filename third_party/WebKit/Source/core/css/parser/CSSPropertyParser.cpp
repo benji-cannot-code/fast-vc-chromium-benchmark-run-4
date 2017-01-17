ff4470f7a213a7c374d1178905a5cb6ac7419a53
aazzam <aazzam@google.com>
2017-01-16 20:18:04 -0800

    Implements CSSPropertyAPI for the webkit-text-emphasis-style property.
    A part of Project Ribbon, separating the parsing logic for CSS
    properties from the parser into an API. This patch removes
    CSSPropertyWebkitTextEmphasis from the switch statement in
    parseSingleValue, and calls the API instead.
    
    A function pointer to the parseSingleValue function from the API for the
    page property is stored in a CSSPropertyDescriptor, and is called from
    CSSPropertyParser.
    
    This patch:
    - Adds CSSPropertyAPIWebkitTextEmphasis.cpp to the BUILD.gn file.
    - Adds api_class flag to CSSProperties.in, which indicates that
      CSSPropertyAPIWebkitTextEmphasis.h is generated.
    - Moves the parsing logic for page from CSSPropertyParser.cpp to
      CSSPropertyAPIWebkitTextEmphasis.cpp, which implements
      CSSPropertyAPI.h.
    
    BUG=668012
    
    Review-Url: https://codereview.chromium.org/2609933005
    Cr-Commit-Position: refs/heads/master@{#443987}

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
@@ -1350,33 +1350,6 @@ bool CSSPropertyParser::consumeOffsetShorthand(bool important) {
   return true;
 }
 
-static CSSValue* consumeTextEmphasisStyle(CSSParserTokenRange& range) {
-  CSSValueID id = range.peek().id();
-  if (id == CSSValueNone)
-    return consumeIdent(range);
-
-  if (CSSValue* textEmphasisStyle = consumeString(range))
-    return textEmphasisStyle;
-
-  CSSIdentifierValue* fill = consumeIdent<CSSValueFilled, CSSValueOpen>(range);
-  CSSIdentifierValue* shape =
-      consumeIdent<CSSValueDot, CSSValueCircle, CSSValueDoubleCircle,
-                   CSSValueTriangle, CSSValueSesame>(range);
-  if (!fill)
-    fill = consumeIdent<CSSValueFilled, CSSValueOpen>(range);
-  if (fill && shape) {
-    CSSValueList* parsedValues = CSSValueList::createSpaceSeparated();
-    parsedValues->append(*fill);
-    parsedValues->append(*shape);
-    return parsedValues;
-  }
-  if (fill)
-    return fill;
-  if (shape)
-    return shape;
-  return nullptr;
-}
-
 static CSSValue* consumeOutlineColor(CSSParserTokenRange& range,
                                      CSSParserMode cssParserMode) {
   // Allow the special focus color even in HTML Standard parsing mode.
@@ -3214,8 +3187,6 @@ const CSSValue* CSSPropertyParser::parseSingleValue(
     case CSSPropertyOffsetRotate:
     case CSSPropertyOffsetRotation:
       return consumeOffsetRotate(m_range);
-    case CSSPropertyWebkitTextEmphasisStyle:
-      return consumeTextEmphasisStyle(m_range);
     case CSSPropertyOutlineColor:
       return consumeOutlineColor(m_range, m_context->mode());
     case CSSPropertyOutlineWidth:
