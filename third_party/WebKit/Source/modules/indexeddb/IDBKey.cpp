9dfecca2af479c0ea58b4dab2a41f6a05f62611c
jsbell <jsbell@chromium.org>
2017-01-18 13:30:55 -0800

    IndexedDB: Replace O(n^2) algorithm computing multientry index keys
    Replace a stupid O(n^2) algorithm for ensuring that multientry keys are
    unique with an O(n log n) (sort then remove duplicates)
    
    BUG=681980
    
    Review-Url: https://codereview.chromium.org/2635403002
    Cr-Commit-Position: refs/heads/master@{#444489}

--- third_party/WebKit/Source/modules/indexeddb/IDBKey.cpp
+++ third_party/WebKit/Source/modules/indexeddb/IDBKey.cpp
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "modules/indexeddb/IDBKey.h"
 
+#include <algorithm>
+
 namespace blink {
 
 IDBKey::~IDBKey() {}
@@ -101,6 +103,25 @@ bool IDBKey::isEqual(const IDBKey* other) const {
     return false;
 
   return !compare(other);
+}
+
+IDBKey::KeyArray IDBKey::toMultiEntryArray() const {
+  DCHECK_EQ(m_type, ArrayType);
+  KeyArray result;
+  result.reserveCapacity(m_array.size());
+  std::copy_if(m_array.begin(), m_array.end(), std::back_inserter(result),
+               [](const Member<IDBKey> key) { return key->isValid(); });
+
+  // Remove duplicates using std::sort/std::unique rather than a hashtable to
+  // avoid the complexity of implementing DefaultHash<IDBKey>.
+  std::sort(result.begin(), result.end(),
+            [](const Member<IDBKey> a, const Member<IDBKey> b) {
+              return a->isLessThan(b);
+            });
+  const auto end = std::unique(result.begin(), result.end());
+  DCHECK_LE(static_cast<size_t>(end - result.begin()), result.size());
+  result.resize(end - result.begin());
+  return result;
 }
 
 }  // namespace blink
