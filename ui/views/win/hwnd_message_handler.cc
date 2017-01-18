b87fc9b7cf57cc5551ad2c3ca8b75ea3910f133a
estark <estark@chromium.org>
2017-01-17 17:29:07 -0800

    Call base::Time::NowFromSystemTime() on WM_TIMECHANGE
    On Windows, base::Time only consults the system clock once every 60
    seconds. Thus, prior to this CL, if the system clock changed,
    base::Time::Now() would continue to reflect the old system clock for up
    to 60 seconds. This was somewhat weird and caused problems for UI that
    instructed the user to fix their clock. (A warning would tell the user
    to fix their clock in order to make a secure connection, but after
    fixing their clock and refreshing, they still would not be able to
    establish a connection.)
    
    To fix this, we now listen for WM_TIMECHANGE messages and call
    NowFromSystemTime() upon receiving them. NowFromSystemTime()
    reinitializes base::Time from the current system clock.
    
    BUG=672906
    TEST=Set system clock to the year 2010 and load https://google.com.
    Observe an interstitial page prompting you to fix your clock. Fix your
    system clock back to the true current date, and refresh. Observe that
    the page loads as normal.
    
    Review-Url: https://codereview.chromium.org/2628423002
    Cr-Commit-Position: refs/heads/master@{#444228}

--- ui/views/win/hwnd_message_handler.cc
+++ ui/views/win/hwnd_message_handler.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/single_thread_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "base/time/time.h"
 #include "base/trace_event/trace_event.h"
 #include "base/win/scoped_gdi_object.h"
 #include "base/win/windows_version.h"
@@ -2166,6 +2167,13 @@ void HWNDMessageHandler::OnSysCommand(UINT notification_code,
 
 void HWNDMessageHandler::OnThemeChanged() {
   ui::NativeThemeWin::CloseHandles();
+}
+
+void HWNDMessageHandler::OnTimeChange() {
+  // Call NowFromSystemTime() to force base::Time to re-initialize the clock
+  // from system time. Otherwise base::Time::Now() might continue to reflect the
+  // old system clock for some amount of time. See https://crbug.com/672906#c5
+  base::Time::NowFromSystemTime();
 }
 
 LRESULT HWNDMessageHandler::OnTouchEvent(UINT message,
