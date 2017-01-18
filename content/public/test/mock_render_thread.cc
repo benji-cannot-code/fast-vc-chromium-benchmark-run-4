248c722996d87029818dfe8afe33a961500db7b0
altimin <altimin@chromium.org>
2017-01-18 05:11:14 -0800

    [content] Expose renderer scheduler task queues in content::RenderThread.
    Expose renderer-wide timer and loading task queues to content to avoid
    posting tasks to default queue, giving scheduler fine-grained control.
    
    BUG=676805
    
    Review-Url: https://codereview.chromium.org/2635053002
    Cr-Commit-Position: refs/heads/master@{#444341}

--- content/public/test/mock_render_thread.cc
+++ content/public/test/mock_render_thread.cc
@@ -250,6 +250,16 @@ int32_t MockRenderThread::GetClientId() {
   return 1;
 }
 
+scoped_refptr<base::SingleThreadTaskRunner>
+MockRenderThread::GetTimerTaskRunner() {
+  return base::ThreadTaskRunnerHandle::Get();
+}
+
+scoped_refptr<base::SingleThreadTaskRunner>
+MockRenderThread::GetLoadingTaskRunner() {
+  return base::ThreadTaskRunnerHandle::Get();
+}
+
 #if defined(OS_WIN)
 void MockRenderThread::PreCacheFont(const LOGFONT& log_font) {
 }
