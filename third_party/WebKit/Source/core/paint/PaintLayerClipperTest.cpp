8b8ac3807e953b8e8ac4e9144e5f80602fd04bee
chrishtr <chrishtr@chromium.org>
2017-01-18 15:07:20 -0800

    Support control clipping for PaintLayers.
    Now such clips behave like other overflow clips.
    
    BUG=671864
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2629983003
    Cr-Commit-Position: refs/heads/master@{#444532}

--- third_party/WebKit/Source/core/paint/PaintLayerClipperTest.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayerClipperTest.cpp
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/LayoutTestHelper.h"
 #include "core/layout/LayoutView.h"
 #include "core/paint/PaintLayer.h"
+#include "platform/LayoutTestSupport.h"
 #include "platform/testing/RuntimeEnabledFeaturesTestHelpers.h"
 #include "platform/testing/UnitTestHelpers.h"
 
@@ -21,6 +22,16 @@ class PaintLayerClipperTest : public ::testing::WithParamInterface<bool>,
   PaintLayerClipperTest()
       : ScopedSlimmingPaintV2ForTest(GetParam()),
         RenderingTest(EmptyFrameLoaderClient::create()) {}
+
+  void SetUp() override {
+    LayoutTestSupport::setMockThemeEnabledForTest(true);
+    RenderingTest::SetUp();
+  }
+
+  void TearDown() override {
+    LayoutTestSupport::setMockThemeEnabledForTest(false);
+    RenderingTest::TearDown();
+  }
 
   bool geometryMapperCacheEmpty(const PaintLayerClipper& clipper) {
     return clipper.m_geometryMapper->m_data.isEmpty();
@@ -54,6 +65,41 @@ TEST_P(PaintLayerClipperTest, LayoutSVGRoot) {
   EXPECT_EQ(LayoutRect(8, 8, 200, 300), backgroundRect.rect());
   EXPECT_EQ(LayoutRect(8, 8, 200, 300), foregroundRect.rect());
   EXPECT_EQ(LayoutRect(8, 8, 200, 300), layerBounds);
+}
+
+TEST_P(PaintLayerClipperTest, ControlClip) {
+  setBodyInnerHTML(
+      "<!DOCTYPE html>"
+      "<input id=target style='position:absolute; width: 200px; height: 300px'"
+      "    type=button>");
+  Element* target = document().getElementById("target");
+  PaintLayer* targetPaintLayer =
+      toLayoutBoxModelObject(target->layoutObject())->layer();
+  ClipRectsContext context(document().layoutView()->layer(), UncachedClipRects);
+  // When RLS is enabled, the LayoutView will have a composited scrolling layer,
+  // so don't apply an overflow clip.
+  if (RuntimeEnabledFeatures::rootLayerScrollingEnabled())
+    context.setIgnoreOverflowClip();
+  LayoutRect layerBounds;
+  ClipRect backgroundRect, foregroundRect;
+  targetPaintLayer->clipper().calculateRects(
+      context, LayoutRect(LayoutRect::infiniteIntRect()), layerBounds,
+      backgroundRect, foregroundRect);
+#if OS(MACOSX)
+  // If the PaintLayer clips overflow, the background rect is intersected with
+  // the PaintLayer bounds...
+  EXPECT_EQ(LayoutRect(3, 4, 210, 28), backgroundRect.rect());
+  // and the foreground rect is intersected with the control clip in this case.
+  EXPECT_EQ(LayoutRect(8, 8, 200, 18), foregroundRect.rect());
+  EXPECT_EQ(LayoutRect(8, 8, 200, 18), layerBounds);
+#else
+  // If the PaintLayer clips overflow, the background rect is intersected with
+  // the PaintLayer bounds...
+  EXPECT_EQ(LayoutRect(8, 8, 200, 300), backgroundRect.rect());
+  // and the foreground rect is intersected with the control clip in this case.
+  EXPECT_EQ(LayoutRect(10, 10, 196, 296), foregroundRect.rect());
+  EXPECT_EQ(LayoutRect(8, 8, 200, 300), layerBounds);
+#endif
 }
 
 TEST_P(PaintLayerClipperTest, LayoutSVGRootChild) {
