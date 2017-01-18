dff2012b6dc4c26778e85355e1060fb6c5a3fc40
scottmg <scottmg@chromium.org>
2017-01-18 11:54:06 -0800

    Move gin and v8 portion of profiling to child from common
    This breaks up profiling.cc into separate common and child-only
    parts to remove the dependency of the browser DLL on Windows on v8.
    
    R=thakis@chromium.org
    TBR=jochen@chromium.org
    BUG=581766
    
    Review-Url: https://codereview.chromium.org/2627753004
    Cr-Commit-Position: refs/heads/master@{#444443}

--- chrome/app/chrome_main_delegate.cc
+++ chrome/app/chrome_main_delegate.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/browser/chrome_content_browser_client.h"
 #include "chrome/browser/defaults.h"
+#include "chrome/child/child_profiling.h"
 #include "chrome/common/channel_info.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_content_client.h"
@@ -529,6 +530,9 @@ bool ChromeMainDelegate::BasicStartupComplete(int* exit_code) {
   chrome::common::mac::EnableCFBundleBlocker();
 #endif
 
+#if !defined(CHROME_MULTIPLE_DLL_BROWSER)
+  ChildProfiling::ProcessStarted();
+#endif
   Profiling::ProcessStarted();
 
   base::trace_event::TraceLog::GetInstance()->SetArgumentFilterPredicate(
@@ -1018,6 +1022,7 @@ void ChromeMainDelegate::ZygoteStarting(
 }
 
 void ChromeMainDelegate::ZygoteForked() {
+  ChildProfiling::ProcessStarted();
   Profiling::ProcessStarted();
   if (Profiling::BeingProfiled()) {
     base::debug::RestartProfilingAfterFork();
