0dc88a390155fe35d0ca16d158b9d71e0417c24a
tkent <tkent@chromium.org>
2017-01-18 01:35:56 -0800

    testharnessreport: Remove isCSSWGTest() and isJSTest().
    Some tests remove <script> from a document, and isJSTest() returned false for
    testharness-based tests unexpectedly.
    Only testharness-based tests use testharnessreport.js, so we don't need these
    runtime checks.
    
    BUG=681764
    
    Review-Url: https://codereview.chromium.org/2639113002
    Cr-Commit-Position: refs/heads/master@{#444314}

--- third_party/WebKit/LayoutTests/external/wpt/resources/testharnessreport.js
+++ third_party/WebKit/LayoutTests/external/wpt/resources/testharnessreport.js
@@ -77,18 +77,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         return text;
     }
 
-    // If the test has a meta tag named flags and the content contains "dom",
-    // then it's a CSSWG test.
-    function isCSSWGTest() {
-        var flags = document.querySelector('meta[name=flags]'),
-            content = flags ? flags.getAttribute('content') : null;
-        return content && content.match(/\bdom\b/);
-    }
-
-    function isJSTest() {
-        return !!document.querySelector('script[src*="/resources/testharness"]');
-    }
-
     function isWPTManualTest() {
         var path = location.pathname;
         if (location.hostname == 'web-platform.test' && path.endsWith('-manual.html'))
@@ -220,12 +208,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                 // test.
                 testRunner.setDumpConsoleMessages(false);
 
-                if (isCSSWGTest() || isJSTest()) {
-                    // Anything isn't material to the testrunner output, so
-                    // should be hidden from the text dump.
-                    if (output_document.body && output_document.body.tagName == 'BODY')
-                        output_document.body.textContent = '';
-                }
+                // Anything isn't material to the testrunner output, so should
+                // be hidden from the text dump.
+                if (output_document.body && output_document.body.tagName == 'BODY')
+                    output_document.body.textContent = '';
             }
 
             // Add results element to output_document.
