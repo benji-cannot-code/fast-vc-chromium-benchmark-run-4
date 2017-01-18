ea2d98120094469e8d22ac47c1fad42584e2941b
ianwen <ianwen@chromium.org>
2017-01-18 12:48:10 -0800

    Fix a bug that infobars sometimes are not attached properly
    This CL fixes three issues of infobars:
    1. After reparenting, infobars are not shown.
    2. After a restart, the first tab will not show infobars.
    3. A new infobar might show up in the tab switcher overview.
    
    BUG=681474,681797
    
    Review-Url: https://codereview.chromium.org/2635373002
    Cr-Commit-Position: refs/heads/master@{#444472}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchTapEventTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchTapEventTest.java
@@ -12,6 +12,7 @@ import android.net.Uri;
 import android.support.test.filters.SmallTest;
 import android.widget.LinearLayout;
 
+import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.base.test.util.RetryOnFailure;
@@ -199,7 +200,13 @@ public class ContextualSearchTapEventTest extends ChromeActivityTestCaseBase<Chr
      */
     private void mockTapText(String text) {
         mContextualSearchManager.getBaseContentView().setSelectedText(text);
-        mContextualSearchClient.onSelectionEvent(SelectionEventType.SELECTION_HANDLES_SHOWN, 0, 0);
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
+            @Override
+            public void run() {
+                mContextualSearchClient.onSelectionEvent(SelectionEventType.SELECTION_HANDLES_SHOWN,
+                        0, 0);
+            }
+        });
     }
 
     /**
