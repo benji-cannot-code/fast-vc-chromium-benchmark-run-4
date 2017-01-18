027ea06562c5ca64b1141e70f1e155cc077f7206
dominickn <dominickn@chromium.org>
2017-01-17 17:21:28 -0800

    Disable app banners in incognito.
    The add to homescreen menu item is disabled in incognito mode. For
    consistency, this CL disables app banners when a site is opened in
    incognito. Browser tests and instrumentation tests are added to ensure
    the correct functionality.
    
    BUG=680778
    
    Review-Url: https://codereview.chromium.org/2633603002
    Cr-Commit-Position: refs/heads/master@{#444223}

--- chrome/browser/installable/installable_logging.cc
+++ chrome/browser/installable/installable_logging.cc
@@ -62,6 +62,8 @@ static const char kIdsDoNotMatchMessage[] =
     "but they do not match";
 static const char kUrlNotSupportedForWebApkMessage[] =
     "a URL in the web manifest contains a username, password, or port";
+static const char kInIncognitoMessage[] =
+    "the page is loaded in an incognito window";
 
 }  // namespace
 
@@ -145,6 +147,9 @@ void LogErrorToConsole(content::WebContents* web_contents,
       break;
     case URL_NOT_SUPPORTED_FOR_WEBAPK:
       pattern = kUrlNotSupportedForWebApkMessage;
+      break;
+    case IN_INCOGNITO:
+      pattern = kInIncognitoMessage;
       break;
   }
 
