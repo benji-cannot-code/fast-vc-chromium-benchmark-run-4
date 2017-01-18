3e0242900db7998fbbb06865c7fc9d59eda6702e
dfalcantara <dfalcantara@chromium.org>
2017-01-18 12:14:58 -0800

    [Download Home] Show more detailed progress
    * Add support for showing download percentage/bytes downloaded/time remaining
      to DownloadItemView.
    
    * Change what strings are displayed to represent an in-progress download's
      status.
    
    * Move percentage string creation to DownloadUtils.
    
    * Change how margins are allocated to the buttons in the DownloadItemView.
    
    * Attempt to prevent the progress bar from restarting its animation every time
      the download updates by directly telling DownloadItemViews to update
      themselves instead of telling the RecyclerView that a particular item has
      changed.  This prevents the RecyclerView from detaching and reattaching
      views and messing up animations.
    
    * Slightly slow down the second phase of the progress bar animation.  Looks
      a little better.
    
    BUG=681555,658246
    
    Review-Url: https://codereview.chromium.org/2639053004
    Cr-Commit-Position: refs/heads/master@{#444456}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadItem.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadItem.java
@@ -16,6 +16,8 @@ import org.chromium.base.annotations.CalledByNative;
  */
 public class DownloadItem {
     static final long INVALID_DOWNLOAD_ID = -1L;
+    static final int INVALID_DOWNLOAD_PERCENTAGE = -1;
+
     private boolean mUseAndroidDownloadManager;
     private DownloadInfo mDownloadInfo;
     private long mDownloadId = INVALID_DOWNLOAD_ID;
@@ -121,5 +123,12 @@ public class DownloadItem {
         downloadItem.setStartTime(startTimestamp);
         downloadItem.setHasBeenExternallyRemoved(hasBeenExternallyRemoved);
         return downloadItem;
+    }
+
+    /**
+     * @return Whether or not the download has an indeterminate percentage.
+     */
+    public boolean isIndeterminate() {
+        return getDownloadInfo().getPercentCompleted() == INVALID_DOWNLOAD_PERCENTAGE;
     }
 }
