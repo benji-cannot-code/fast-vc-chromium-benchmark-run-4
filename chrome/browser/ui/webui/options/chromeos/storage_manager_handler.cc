c8c3b2cebbea4b5329faa8ce73c236b5608667dc
dullweber <dullweber@chromium.org>
2017-01-18 03:35:46 -0800

    Show the exact cache size for time ranges if supported. This will show the right cache size when a time range is selected on linux and android, where SimpleBackend is used for caching. Windows will still show the upper estimate because there is no efficient way to determine the cache size in the blockfile cache.
    BUG=671196
    BASE=2626173003
    
    Review-Url: https://codereview.chromium.org/2626223002
    Cr-Commit-Position: refs/heads/master@{#444335}

--- chrome/browser/ui/webui/options/chromeos/storage_manager_handler.cc
+++ chrome/browser/ui/webui/options/chromeos/storage_manager_handler.cc
@@ -306,8 +306,8 @@ void StorageManagerHandler::UpdateBrowsingDataSize() {
       content::BrowserContext::GetDefaultStoragePartition(profile),
       base::Time(), base::Time::Max())
       ->CountAndDestroySelfWhenFinished(
-          base::Bind(&StorageManagerHandler::OnGetBrowsingDataSize,
-                     weak_ptr_factory_.GetWeakPtr(), false));
+          base::Bind(&StorageManagerHandler::OnGetCacheSize,
+                     weak_ptr_factory_.GetWeakPtr()));
 
   // Fetch the size of site data in browsing data.
   if (!site_data_size_collector_.get()) {
@@ -333,6 +333,11 @@ void StorageManagerHandler::UpdateBrowsingDataSize() {
   site_data_size_collector_->Fetch(
       base::Bind(&StorageManagerHandler::OnGetBrowsingDataSize,
                  weak_ptr_factory_.GetWeakPtr(), true));
+}
+
+void StorageManagerHandler::OnGetCacheSize(int64_t size, bool is_upper_limit) {
+  DCHECK(!is_upper_limit);
+  OnGetBrowsingDataSize(false, size);
 }
 
 void StorageManagerHandler::OnGetBrowsingDataSize(bool is_site_data,
