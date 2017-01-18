6f46cd754d24a8457c87e05f712184e1baf01c75
ntfschr <ntfschr@chromium.org>
2017-01-18 12:12:37 -0800

    Use WebView-specific resource throttle
    This switches to using the WebView-specific resource throttle.
    
    BUG=488676
    
    Review-Url: https://codereview.chromium.org/2636363002
    Cr-Commit-Position: refs/heads/master@{#444454}

--- android_webview/browser/renderer_host/aw_resource_dispatcher_host_delegate.cc
+++ android_webview/browser/renderer_host/aw_resource_dispatcher_host_delegate.cc
@@ -13,19 +13,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "android_webview/browser/aw_contents_io_thread_client.h"
 #include "android_webview/browser/aw_login_delegate.h"
 #include "android_webview/browser/aw_resource_context.h"
+#include "android_webview/browser/aw_resource_throttle.h"
 #include "android_webview/browser/net/aw_web_resource_request.h"
 #include "android_webview/browser/renderer_host/auto_login_parser.h"
 #include "android_webview/common/url_constants.h"
 #include "base/memory/scoped_vector.h"
 #include "components/navigation_interception/intercept_navigation_delegate.h"
-#include "components/safe_browsing/base_resource_throttle.h"
 #include "components/safe_browsing_db/safe_browsing_api_handler.h"
 #include "components/web_restrictions/browser/web_restrictions_resource_throttle.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/resource_dispatcher_host.h"
 #include "content/public/browser/resource_dispatcher_host_login_delegate.h"
 #include "content/public/browser/resource_request_info.h"
-#include "content/public/browser/resource_throttle.h"
 #include "content/public/browser/web_contents.h"
 #include "net/base/load_flags.h"
 #include "net/base/net_errors.h"
@@ -275,7 +274,7 @@ void AwResourceDispatcherHostDelegate::RequestBeginning(
 
   if (safe_browsing::SafeBrowsingApiHandler::GetInstance()) {
     content::ResourceThrottle* throttle =
-        safe_browsing::BaseResourceThrottle::MaybeCreate(
+        AwSafeBrowsingResourceThrottle::MaybeCreate(
             request, resource_type,
             AwBrowserContext::GetDefault()->GetSafeBrowsingDBManager(),
             AwBrowserContext::GetDefault()->GetSafeBrowsingUIManager());
