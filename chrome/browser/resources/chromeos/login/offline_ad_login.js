e2742cda008d6d0899e76feb1033167ec2e61346
rsorokin <rsorokin@chromium.org>
2017-01-18 13:23:15 -0800

    Handle login and password error for the domain join
    BUG=659984
    TEST=manual
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2622273002
    Cr-Commit-Position: refs/heads/master@{#444487}

--- chrome/browser/resources/chromeos/login/offline_ad_login.js
+++ chrome/browser/resources/chromeos/login/offline_ad_login.js
@@ -7,6 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview Polymer element for displaying AD domain joining and AD
  * Authenticate user screens.
  */
+// Possible error states of the screen. Must be in the same order as
+// ActiveDirectoryErrorState enum values.
+/** @enum {number} */ var ACTIVE_DIRECTORY_ERROR_STATE = {
+  NONE: 0,
+  MACHINE_NAME_INVALID: 1,
+  MACHINE_NAME_TOO_LONG: 2,
+  BAD_USERNAME: 3,
+  BAD_PASSWORD: 4,
+};
 
 Polymer({
   is: 'offline-ad-login',
@@ -41,7 +50,11 @@ Polymer({
     /**
      * Welcome message on top of the UI.
      */
-    adWelcomeMessage: String
+    adWelcomeMessage: String,
+    /**
+     * Error message for the machine name input.
+     */
+    machineNameError: String,
   },
 
   /** @private */
@@ -74,8 +87,36 @@ Polymer({
     this.$.userInput.value = user || '';
     this.$.machineNameInput.value = machineName || '';
     this.$.passwordInput.value = '';
-    this.$.passwordInput.isInvalid = !!user;
     this.focus();
+  },
+
+  /**
+   * @param {ACTIVE_DIRECTORY_ERROR_STATE} error_state
+   */
+  setInvalid: function(error_state) {
+    this.$.machineNameInput.isInvalid = false;
+    this.$.userInput.isInvalid = false;
+    this.$.passwordInput.isInvalid = false;
+    switch (error_state) {
+      case ACTIVE_DIRECTORY_ERROR_STATE.NONE:
+        break;
+      case ACTIVE_DIRECTORY_ERROR_STATE.MACHINE_NAME_INVALID:
+        this.machineNameError =
+            loadTimeData.getString('adJoinErrorMachineNameInvalid');
+        this.$.machineNameInput.isInvalid = true;
+        break;
+      case ACTIVE_DIRECTORY_ERROR_STATE.MACHINE_NAME_TOO_LONG:
+        this.machineNameError =
+            loadTimeData.getString('adJoinErrorMachineNameTooLong');
+        this.$.machineNameInput.isInvalid = true;
+        break;
+      case ACTIVE_DIRECTORY_ERROR_STATE.BAD_USERNAME:
+        this.$.userInput.isInvalid = true;
+        break;
+      case ACTIVE_DIRECTORY_ERROR_STATE.BAD_PASSWORD:
+        this.$.passwordInput.isInvalid = true;
+        break;
+    }
   },
 
   /** @private */
