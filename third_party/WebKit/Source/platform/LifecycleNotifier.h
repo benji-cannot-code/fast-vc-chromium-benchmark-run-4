7b5aacecb9d5e5518af61870214454dfc64ac134
lukasza <lukasza@chromium.org>
2017-01-18 15:10:57 -0800

    Rename |class NotifyContextDestroyed| to |ContextDestroyedNotifier|.
    The rename is needed to avoid problems after the Great Blink Rename.
    In particular, without the rename, the old *class* name would clash
    with PascalCased version of the contextDestroyedNotifier *method*.
    
    BUG=582312
    
    Review-Url: https://codereview.chromium.org/2640123002
    Cr-Commit-Position: refs/heads/master@{#444535}

--- third_party/WebKit/Source/platform/LifecycleNotifier.h
+++ third_party/WebKit/Source/platform/LifecycleNotifier.h
@@ -113,8 +113,8 @@ class HasContextDestroyed {
 template <typename Observer,
           typename T,
           bool = HasContextDestroyed<Observer, T>::value>
-class NotifyContextDestroyed {
-  STATIC_ONLY(NotifyContextDestroyed);
+class ContextDestroyedNotifier {
+  STATIC_ONLY(ContextDestroyedNotifier);
 
  public:
   static void call(Observer* observer, T* context) {
@@ -123,8 +123,8 @@ class NotifyContextDestroyed {
 };
 
 template <typename Observer, typename T>
-class NotifyContextDestroyed<Observer, T, false> {
-  STATIC_ONLY(NotifyContextDestroyed);
+class ContextDestroyedNotifier<Observer, T, false> {
+  STATIC_ONLY(ContextDestroyedNotifier);
 
  public:
   static void call(Observer*, T*) {}
@@ -138,7 +138,7 @@ inline void LifecycleNotifier<T, Observer>::notifyContextDestroyed() {
   m_observers.swap(observers);
   for (Observer* observer : observers) {
     DCHECK(observer->lifecycleContext() == context());
-    NotifyContextDestroyed<Observer, T>::call(observer, context());
+    ContextDestroyedNotifier<Observer, T>::call(observer, context());
     observer->clearContext();
   }
 }
