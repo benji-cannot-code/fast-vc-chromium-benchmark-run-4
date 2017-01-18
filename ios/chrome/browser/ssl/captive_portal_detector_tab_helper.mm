727c52efac0e99cf5052d562e1c0d1de49b00d5a
michaeldo <michaeldo@chromium.org>
2017-01-18 15:00:49 -0800

    Add metric for determining if SSL error  was caused by a Captive Portal.
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2634473004
    Cr-Commit-Position: refs/heads/master@{#444529}

---
+++ ios/chrome/browser/ssl/captive_portal_detector_tab_helper.mm
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ssl/captive_portal_detector_tab_helper.h"
+
+#include "base/memory/ptr_util.h"
+#include "components/captive_portal/captive_portal_detector.h"
+#include "ios/web/public/browser_state.h"
+#import "ios/web/public/web_state/web_state.h"
+
+DEFINE_WEB_STATE_USER_DATA_KEY(CaptivePortalDetectorTabHelper);
+
+captive_portal::CaptivePortalDetector*
+CaptivePortalDetectorTabHelper::detector() {
+  return detector_.get();
+}
+
+CaptivePortalDetectorTabHelper::CaptivePortalDetectorTabHelper(
+    web::WebState* web_state)
+    : detector_(base::MakeUnique<captive_portal::CaptivePortalDetector>(
+          web_state->GetBrowserState()->GetRequestContext())) {}
+
+CaptivePortalDetectorTabHelper::~CaptivePortalDetectorTabHelper() = default;
