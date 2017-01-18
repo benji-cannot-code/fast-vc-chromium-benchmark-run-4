9e46d599a31dbeb5a5eb0c98ba43c696995e84a2
pdr <pdr@chromium.org>
2017-01-18 09:38:04 -0800

    Move paint offset paint invalidation into PaintPropertyTreeBuilder
    Paint offset changes should cause full paint invalidation in SPV2,
    but this tracking logic can be squirreled away in
    PaintPropertyTreeBuilder. This removes SPV2-specific paint offset
    code from PrePaintTreeWalk::walk which was visibile on profiles
    of the blink_perf.paint paint-offset-changes test.
    
    This approach also lets us use the paint offset under-invalidation
    checking in FindPropertiesNeedingUpdate which would miss changes
    to a LayoutObject's paint offset outside the under-invalidation
    checking scopes in PaintPropertyTreeBuilder.
    
    BUG=678597
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2644453002
    Cr-Commit-Position: refs/heads/master@{#444411}

--- third_party/WebKit/Source/core/paint/PrePaintTreeWalk.cpp
+++ third_party/WebKit/Source/core/paint/PrePaintTreeWalk.cpp
@@ -104,7 +104,6 @@ bool PrePaintTreeWalk::walk(const LayoutObject& object,
   // Ensure the current context takes into account the box's position. This can
   // force a subtree update due to paint offset changes and must precede any
   // early out from the treewalk.
-  LayoutPoint oldPaintOffset = object.paintOffset();
   m_propertyTreeBuilder.updateContextForBoxPosition(object,
                                                     context.treeBuilderContext);
 
@@ -122,7 +121,7 @@ bool PrePaintTreeWalk::walk(const LayoutObject& object,
 
   m_propertyTreeBuilder.updatePropertiesForSelf(object,
                                                 context.treeBuilderContext);
-  m_paintInvalidator.invalidatePaintIfNeeded(object, oldPaintOffset,
+  m_paintInvalidator.invalidatePaintIfNeeded(object,
                                              context.paintInvalidatorContext);
   m_propertyTreeBuilder.updatePropertiesForChildren(object,
                                                     context.treeBuilderContext);
