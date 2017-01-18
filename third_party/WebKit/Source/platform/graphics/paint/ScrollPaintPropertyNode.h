c01b8d37e156906ab8d6ee6f010ca823e67cc8bf
chrishtr <chrishtr@chromium.org>
2017-01-17 16:18:22 -0800

    SPv2: Paint SVG images and self-contained recorded pictures with their own trees.
    A followup patch will implement flattening the trees into a single display list.
    
    BUG=665259
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2632823002
    Cr-Commit-Position: refs/heads/master@{#444199}

--- third_party/WebKit/Source/platform/graphics/paint/ScrollPaintPropertyNode.h
+++ third_party/WebKit/Source/platform/graphics/paint/ScrollPaintPropertyNode.h
@@ -31,6 +31,7 @@ using MainThreadScrollingReasons = uint32_t;
 class PLATFORM_EXPORT ScrollPaintPropertyNode
     : public RefCounted<ScrollPaintPropertyNode> {
  public:
+  // This node is really a sentinel, and does not represent a real scroll.
   static ScrollPaintPropertyNode* root();
 
   static PassRefPtr<ScrollPaintPropertyNode> create(
