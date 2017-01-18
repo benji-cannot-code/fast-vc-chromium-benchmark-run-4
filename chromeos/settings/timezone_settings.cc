76eb8bd46efe618ff24001f7ef1396b09a5b564b
fdoray <fdoray@chromium.org>
2017-01-17 18:00:57 -0800

    Use TaskScheduler instead of WorkerPool in timezone_settings.cc.
    The following traits are used:
    
    Priority: BACKGROUND
      User won't notice if this task takes an arbitrarily long time
      to complete.
    
    Shutdown behavior: CONTINUE_ON_SHUTDOWN
      Tasks posted with this mode which have not started executing before
      shutdown is initiated will never run. Tasks with this mode running at
      shutdown will be ignored (the worker will not be joined).
    
      Note: Tasks that were previously posted to base::WorkerPool should
      use this shutdown behavior because this is how base::WorkerPool
      handles all its tasks.
    
    May Block:
      Tasks posted with MayBlock() may block. This includes but is not
      limited to tasks that wait on synchronous file I/O operations:
      read or write a file from disk, interact with a pipe or a socket,
      rename or delete a file, enumerate files in a directory, etc. This
      trait isn't required for the mere use of locks.
    
    BUG=659191
    
    Review-Url: https://codereview.chromium.org/2611433002
    Cr-Commit-Position: refs/heads/master@{#444240}

--- chromeos/settings/timezone_settings.cc
+++ chromeos/settings/timezone_settings.cc
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/sys_info.h"
 #include "base/task_runner.h"
-#include "base/threading/worker_pool.h"
+#include "base/task_scheduler/post_task.h"
 #include "chromeos/settings/timezone_settings_helper.h"
 
 namespace chromeos {
@@ -391,8 +391,13 @@ void TimezoneSettingsImpl::SetTimezone(const icu::TimeZone& timezone) {
   VLOG(1) << "Setting timezone to " << id;
   // It's safe to change the timezone config files in the background as the
   // following operations don't depend on the completion of the config change.
-  base::WorkerPool::GetTaskRunner(true /* task is slow */)->
-      PostTask(FROM_HERE, base::Bind(&SetTimezoneIDFromString, id));
+  base::PostTaskWithTraits(
+      FROM_HERE, base::TaskTraits()
+                     .WithShutdownBehavior(
+                         base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN)
+                     .WithPriority(base::TaskPriority::BACKGROUND)
+                     .MayBlock(),
+      base::Bind(&SetTimezoneIDFromString, id));
   icu::TimeZone::setDefault(*known_timezone);
   for (auto& observer : observers_)
     observer.TimezoneChanged(*known_timezone);
