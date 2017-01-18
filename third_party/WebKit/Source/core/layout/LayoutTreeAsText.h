5a6a5a22bc78a889b93f2d1165e99969d91d94ab
yoichio <yoichio@chromium.org>
2017-01-18 00:55:29 -0800

    [CodeHealth] Make LayoutTreeAsText::nodePositionAsStringForTesting local
    nodePositionAsStringForTesting was only used from TextControlElement except local,
     but that usage was removed:
     https://codereview.chromium.org/2628733003/
    
    TEST=No change in behavior
    
    Review-Url: https://codereview.chromium.org/2634233002
    Cr-Commit-Position: refs/heads/master@{#444307}

--- third_party/WebKit/Source/core/layout/LayoutTreeAsText.h
+++ third_party/WebKit/Source/core/layout/LayoutTreeAsText.h
@@ -39,7 +39,6 @@ class PaintLayer;
 class Element;
 class LayoutRect;
 class LocalFrame;
-class Node;
 class LayoutBlockFlow;
 class LayoutObject;
 class TextStream;
@@ -106,8 +105,6 @@ String quoteAndEscapeNonPrintables(const String&);
 CORE_EXPORT String counterValueForElement(Element*);
 
 CORE_EXPORT String markerTextForListItem(Element*);
-
-CORE_EXPORT String nodePositionAsStringForTesting(Node*);
 
 TextStream& operator<<(TextStream&, const Color&);
 
