8d098bcb6ea02c8984540dd660392b101314adf2
jbauman <jbauman@chromium.org>
2017-01-17 19:01:17 -0800

    Report new GPU process exit code when it's DOA.
    This will allow us to separate out in our metrics the difference
    between it crashing and it just failing to launch because there's
    something wrong with the GPU.
    
    BUG=660084
    
    Review-Url: https://codereview.chromium.org/2631743004
    Cr-Commit-Position: refs/heads/master@{#444255}

--- content/gpu/gpu_main.cc
+++ content/gpu/gpu_main.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_client.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/main_function_params.h"
+#include "content/public/common/result_codes.h"
 #include "gpu/command_buffer/service/gpu_switches.h"
 #include "gpu/config/gpu_driver_bug_list.h"
 #include "gpu/config/gpu_info_collector.h"
@@ -305,7 +306,7 @@ int GpuMain(const MainFunctionParams& parameters) {
     base::RunLoop().Run();
   }
 
-  return dead_on_arrival ? 2 : 0;
+  return dead_on_arrival ? RESULT_CODE_GPU_DEAD_ON_ARRIVAL : 0;
 }
 
 namespace {
