7b90dbd33c13db940cff89e0fd5a02fc1694df80
flackr <flackr@chromium.org>
2017-01-18 14:57:23 -0800

    Add offset of content in composited layer to sticky position offset.
    The actual sticky layoutobject may have an offset within its composited layer
    which needs to be included in the compositing update of sticky positioning.
    
    BUG=670274
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2636233002
    Cr-Commit-Position: refs/heads/master@{#444527}

--- third_party/WebKit/Source/core/layout/compositing/CompositedLayerMapping.cpp
+++ third_party/WebKit/Source/core/layout/compositing/CompositedLayerMapping.cpp
@@ -236,7 +236,6 @@ void CompositedLayerMapping::createPrimaryGraphicsLayer() {
   updateTransform(layoutObject()->styleRef());
   updateFilters(layoutObject()->styleRef());
   updateBackdropFilters(layoutObject()->styleRef());
-  updateStickyConstraints(layoutObject()->styleRef());
   updateLayerBlendMode(layoutObject()->styleRef());
   updateIsRootForIsolatedGroup();
 }
@@ -316,7 +315,9 @@ void CompositedLayerMapping::updateStickyConstraints(
                                enclosingLayerOffset);
     FloatPoint stickyBoxOffset =
         constraints.scrollContainerRelativeStickyBoxRect().location();
-    stickyBoxOffset.moveBy(FloatPoint(-enclosingLayerOffset));
+    DCHECK(!m_contentOffsetInCompositingLayerDirty);
+    stickyBoxOffset.moveBy(FloatPoint(-enclosingLayerOffset) -
+                           FloatSize(contentOffsetInCompositingLayer()));
 
     webConstraint.isSticky = true;
     webConstraint.isAnchoredLeft =
@@ -944,8 +945,6 @@ void CompositedLayerMapping::updateGraphicsLayerGeometry(
   if (!layoutObject()->style()->isRunningBackdropFilterAnimationOnCompositor())
     updateBackdropFilters(layoutObject()->styleRef());
 
-  updateStickyConstraints(layoutObject()->styleRef());
-
   // We compute everything relative to the enclosing compositing layer.
   IntRect ancestorCompositingBounds;
   if (compositingContainer) {
@@ -982,6 +981,7 @@ void CompositedLayerMapping::updateGraphicsLayerGeometry(
                                           graphicsLayerParentLocation);
   updateContentsOffsetInCompositingLayer(snappedOffsetFromCompositedAncestor,
                                          graphicsLayerParentLocation);
+  updateStickyConstraints(layoutObject()->styleRef());
   updateSquashingLayerGeometry(
       graphicsLayerParentLocation, compositingContainer, m_squashedLayers,
       m_squashingLayer.get(), &m_squashingLayerOffsetFromTransformedAncestor,
