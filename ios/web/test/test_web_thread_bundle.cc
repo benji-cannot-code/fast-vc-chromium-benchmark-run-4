b2e2b4a9224cc76f70a7beff5f0677340335a9db
fdoray <fdoray@chromium.org>
2017-01-19 08:44:49 -0800

    Add TaskScheduler in web::TestWebThreadBundle.
    content::TestBrowserThreadBundle initializes TaskScheduler.
    With this CL, web::TestWebThreadBundle does the same.
    
    This is a prerequisite for https://codereview.chromium.org/2602223002/
    
    BUG=553459
    
    Review-Url: https://codereview.chromium.org/2640653002
    Cr-Commit-Position: refs/heads/master@{#444765}

--- ios/web/test/test_web_thread_bundle.cc
+++ ios/web/test/test_web_thread_bundle.cc
@@ -5,8 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ios/web/public/test/test_web_thread_bundle.h"
 
+#include "base/memory/ptr_util.h"
 #include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_async_task_scheduler.h"
+#include "base/test/scoped_task_scheduler.h"
 #include "ios/web/public/test/test_web_thread.h"
 #include "ios/web/web_thread_impl.h"
 
@@ -49,6 +52,9 @@ TestWebThreadBundle::~TestWebThreadBundle() {
   base::RunLoop().RunUntilIdle();
   ui_thread_.reset();
   base::RunLoop().RunUntilIdle();
+
+  scoped_async_task_scheduler_.reset();
+  scoped_task_scheduler_.reset();
 }
 
 void TestWebThreadBundle::Init(int options) {
@@ -59,6 +65,14 @@ void TestWebThreadBundle::Init(int options) {
   }
 
   ui_thread_.reset(new TestWebThread(WebThread::UI, message_loop_.get()));
+
+  if (options & REAL_TASK_SCHEDULER) {
+    scoped_async_task_scheduler_ =
+        base::MakeUnique<base::test::ScopedAsyncTaskScheduler>();
+  } else {
+    scoped_task_scheduler_ =
+        base::MakeUnique<base::test::ScopedTaskScheduler>(message_loop_.get());
+  }
 
   if (options & TestWebThreadBundle::REAL_DB_THREAD) {
     db_thread_.reset(new TestWebThread(WebThread::DB));
