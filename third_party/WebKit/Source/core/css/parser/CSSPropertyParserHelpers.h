6e16e74477152184af746dc8fa31fb40b3b8aea0
ktyliu <ktyliu@chromium.org>
2017-01-19 15:22:02 -0800

    Added CSSPropertyShapeUtils which holds shape parsing methods.
    This allows CSSPropertyShapeOutside as well as CSSPropertyClipPath to
    use the consumeBasicShape method and for CSSPropertyShapeOutside and
    CSSPropertyBorderRadius to use the consumeRadii method.
    
    Moved complete4Sides into CSSPropertyParserHelpers as that method is
    quite generic and simple, and used by several parser methods.
    
    This patch:
    - Adds CSSPropertyShapeUtils (.h and .cpp files) to the BUILD.gn
      file.
    - Moves consumeBasicShape() and consumeRadii() from
      CSSPropertyParser.cpp to CSSPropertyShapeUtils.cpp, as well as helper
      methods consumeShapeRadius, consumeBasicShapeEllipse,
      consumeBasicShapePolygon, and consumeBasicShapeInset that are placed
      in anonymous namespace.
    - Move complete4Sides method from CSSPropertyParser.cpp to
      CSSPropertyParserHelpers.cpp
    
    BUG=668012
    
    Review-Url: https://codereview.chromium.org/2645683003
    Cr-Commit-Position: refs/heads/master@{#444885}

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParserHelpers.h
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParserHelpers.h
@@ -27,6 +27,8 @@ class CSSValuePair;
 // will not be modified.
 namespace CSSPropertyParserHelpers {
 
+void complete4Sides(CSSValue* side[4]);
+
 // TODO(timloh): These should probably just be consumeComma and consumeSlash.
 bool consumeCommaIncludingWhitespace(CSSParserTokenRange&);
 bool consumeSlashIncludingWhitespace(CSSParserTokenRange&);
---
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyShapeUtils.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CSSPropertyShapeUtils_h
+#define CSSPropertyShapeUtils_h
+
+#include "core/css/parser/CSSParserMode.h"
+#include "wtf/Allocator.h"
+
+namespace blink {
+
+class CSSValue;
+class CSSParserContext;
+class CSSParserTokenRange;
+
+class CSSPropertyShapeUtils {
+  STATIC_ONLY(CSSPropertyShapeUtils);
+
+  static CSSValue* consumeBasicShape(CSSParserTokenRange&,
+                                     const CSSParserContext*);
+  static bool consumeRadii(CSSValue* horizontalRadii[4],
+                           CSSValue* verticalRadii[4],
+                           CSSParserTokenRange&,
+                           CSSParserMode,
+                           bool useLegacyParsing);
+};
+
+}  // namespace blink
+
+#endif  // CSSPropertyShapeUtils_h
