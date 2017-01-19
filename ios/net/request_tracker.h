571da46777cfee21f9c7bd8390e93c21d1087589
sdefresne <sdefresne@chromium.org>
2017-01-19 02:32:56 -0800

    Remove CacheMode from RequestTracker.
    The CacheMode from RequestTracker was never changed from its initial
    value RequestTracker::CACHE_MODE so remove the setter and getter and
    inline the value in the only location it was read.
    
    BUG=585700
    
    Review-Url: https://codereview.chromium.org/2640823003
    Cr-Commit-Position: refs/heads/master@{#444699}

--- ios/net/request_tracker.h
+++ ios/net/request_tracker.h
@@ -28,14 +28,6 @@ namespace net {
 // The RequestTracker can be created on one thread and used on a different one.
 class RequestTracker {
  public:
-  enum CacheMode {
-    CACHE_NORMAL,
-    CACHE_RELOAD,
-    CACHE_HISTORY,
-    CACHE_BYPASS,
-    CACHE_ONLY,
-  };
-
   typedef base::Callback<void(bool)> SSLCallback;
 
   class RequestTrackerFactory {
@@ -109,10 +101,6 @@ class RequestTracker {
                                      bool recoverable,
                                      const SSLCallback& should_continue) = 0;
 
-  // Gets and sets the cache mode.
-  CacheMode GetCacheMode() const;
-  void SetCacheMode(RequestTracker::CacheMode mode);
-
  protected:
   virtual ~RequestTracker();
 
@@ -120,7 +108,6 @@ class RequestTracker {
 
  private:
   bool initialized_;
-  CacheMode cache_mode_;
   base::ThreadChecker thread_checker_;
 
   base::WeakPtrFactory<RequestTracker> weak_ptr_factory_;
