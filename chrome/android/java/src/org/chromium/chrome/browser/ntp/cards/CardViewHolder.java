b008aecf78a70cca3aa32c22490c15522cf63907
bauerb <bauerb@chromium.org>
2017-01-19 10:03:37 -0800

    Update and unify nine-patch files for content suggestion and omnibox cards
    Screenshots: https://goo.gl/photos/GFukuPJesZCRHVaMA
    
    Change the calculation in CardViewHolder to not depend on the exact size
    of the card shadow, but get it instead from the drawable's padding.
    
    Review-Url: https://codereview.chromium.org/2638353002
    Cr-Commit-Position: refs/heads/master@{#444785}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/CardViewHolder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/CardViewHolder.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.ntp.cards;
 
+import android.graphics.Rect;
 import android.support.annotation.CallSuper;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
@@ -19,6 +20,7 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.animation.Interpolator;
 
+import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ntp.ContextMenuManager;
 import org.chromium.chrome.browser.ntp.UiConfig;
@@ -49,11 +51,16 @@ public abstract class CardViewHolder extends NewTabPageViewHolder {
     private final int mMaxPeekPadding;
 
     /**
-     * Due to the card background being a 9 patch file - the card border shadow will be part of
-     * the card width and height. This value will be used to adjust values to account for the
-     * borders.
+     * The card shadow is part of the drawable nine-patch and not drawn via setElevation(),
+     * so it is included in the height and width of the drawable. This member contains the
+     * dimensions of the shadow (from the drawable's padding), so it can be used to offset the
+     * position in calculations.
      */
-    private final int mCards9PatchAdjustment;
+    private final Rect mCardShadow = new Rect();
+
+    private final int mCardGap;
+
+    private final int mDefaultLateralMargin;
 
     protected final NewTabPageRecyclerView mRecyclerView;
 
@@ -79,8 +86,10 @@ public abstract class CardViewHolder extends NewTabPageViewHolder {
             UiConfig uiConfig, final ContextMenuManager contextMenuManager) {
         super(inflateView(layoutId, recyclerView));
 
-        mCards9PatchAdjustment = recyclerView.getResources().getDimensionPixelSize(
-                R.dimen.snippets_card_9_patch_adjustment);
+        ApiCompatibilityUtils.getDrawable(recyclerView.getResources(), R.drawable.card_single)
+                .getPadding(mCardShadow);
+
+        mCardGap = recyclerView.getResources().getDimensionPixelSize(R.dimen.snippets_card_gap);
 
         mMaxPeekPadding = recyclerView.getResources().getDimensionPixelSize(
                 R.dimen.snippets_padding);
@@ -116,7 +125,11 @@ public abstract class CardViewHolder extends NewTabPageViewHolder {
 
         // Configure the resizer to use negative margins on regular display to balance out the
         // lateral shadow of the card 9-patch and avoid a rounded corner effect.
-        mMarginResizer.setMargins(-mCards9PatchAdjustment);
+        int cardCornerRadius = recyclerView.getResources().getDimensionPixelSize(
+                R.dimen.snippets_card_corner_radius);
+        assert mCardShadow.left == mCardShadow.right;
+        mDefaultLateralMargin = -(mCardShadow.left + cardCornerRadius);
+        mMarginResizer.setMargins(mDefaultLateralMargin);
     }
 
     /**
@@ -177,7 +190,11 @@ public abstract class CardViewHolder extends NewTabPageViewHolder {
             hasCardBelow = isCard(belowViewType) && belowViewType != ItemViewType.PROMO;
         }
 
-        getParams().bottomMargin = hasCardBelow ? -mCards9PatchAdjustment : 0;
+        // By default the apparent distance between two cards is the sum of the bottom and top
+        // height of their shadows. We want |mCardGap| instead, so we set the bottom margin to
+        // the difference.
+        getParams().bottomMargin =
+                hasCardBelow ? (mCardGap - (mCardShadow.top + mCardShadow.bottom)) : 0;
 
         @DrawableRes
         int selectedBackground = selectBackground(hasCardAbove, hasCardBelow);
@@ -248,9 +265,8 @@ public abstract class CardViewHolder extends NewTabPageViewHolder {
         }
         itemView.setPadding(lateralPadding, mMaxPeekPadding, lateralPadding, mMaxPeekPadding);
 
-        // Adjust the margins by |mCards9PatchAdjustment| so the card width
-        // is the actual width not including the elevation shadow, so we can have full bleed.
-        mMarginResizer.setMargins(mMaxPeekPadding - (peekPadding + mCards9PatchAdjustment));
+        // Adjust the margins. The shadow width is offset via the default lateral margin.
+        mMarginResizer.setMargins(mDefaultLateralMargin + mMaxPeekPadding - peekPadding);
 
         // Set the opacity of the card content to be 0 when peeking and 1 when full width.
         int itemViewChildCount = ((ViewGroup) itemView).getChildCount();
@@ -286,10 +302,10 @@ public abstract class CardViewHolder extends NewTabPageViewHolder {
 
     @DrawableRes
     protected int selectBackground(boolean hasCardAbove, boolean hasCardBelow) {
-        if (hasCardAbove && hasCardBelow) return R.drawable.ntp_card_middle;
-        if (!hasCardAbove && hasCardBelow) return R.drawable.ntp_card_top;
-        if (hasCardAbove && !hasCardBelow) return R.drawable.ntp_card_bottom;
-        return R.drawable.ntp_card_single;
+        if (hasCardAbove && hasCardBelow) return R.drawable.card_middle;
+        if (!hasCardAbove && hasCardBelow) return R.drawable.card_top;
+        if (hasCardAbove && !hasCardBelow) return R.drawable.card_bottom;
+        return R.drawable.card_single;
     }
 
     protected NewTabPageRecyclerView getRecyclerView() {
