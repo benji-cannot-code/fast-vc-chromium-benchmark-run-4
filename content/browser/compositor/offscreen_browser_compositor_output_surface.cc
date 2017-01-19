679b8da301349f8a684ad48dde4405e58e1533d7
eseckler <eseckler@chromium.org>
2017-01-19 06:46:21 -0800

    [headless] Fix screenshots with gpu enabled.
    Turns out this is due to two things:
    1) We were overriding ShouldCreateCompositorFrameSink in the browser
       compositor to always return false (which is unnecessary).
    2) Contrary to what GpuBrowserCompositorOutputSurface and
       SoftwareBrowserCompositorOutputSurface do, the
       OffscreenBrowserCompositorOutputSurface wasn't letting the RWHI know
       about swapped frames.
    
    This fixes both issues.
    
    BUG=681614
    
    Review-Url: https://codereview.chromium.org/2631263003
    Cr-Original-Commit-Position: refs/heads/master@{#444694}
    Committed: https://chromium.googlesource.com/chromium/src/+/f5077ffe4f7a3d8c053e8bd8b2070bc699ca937f
    Review-Url: https://codereview.chromium.org/2631263003
    Cr-Commit-Position: refs/heads/master@{#444730}

--- content/browser/compositor/offscreen_browser_compositor_output_surface.cc
+++ content/browser/compositor/offscreen_browser_compositor_output_surface.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/display_compositor/compositor_overlay_candidate_validator.h"
 #include "content/browser/compositor/reflector_impl.h"
 #include "content/browser/compositor/reflector_texture.h"
+#include "content/browser/renderer_host/render_widget_host_impl.h"
 #include "content/public/browser/browser_thread.h"
 #include "gpu/command_buffer/client/context_support.h"
 #include "gpu/command_buffer/client/gles2_interface.h"
@@ -158,7 +159,7 @@ void OffscreenBrowserCompositorOutputSurface::SwapBuffers(
       sync_token,
       base::Bind(
           &OffscreenBrowserCompositorOutputSurface::OnSwapBuffersComplete,
-          weak_ptr_factory_.GetWeakPtr()));
+          weak_ptr_factory_.GetWeakPtr(), frame.latency_info));
 }
 
 bool OffscreenBrowserCompositorOutputSurface::IsDisplayedAsOverlayPlane()
@@ -187,7 +188,9 @@ void OffscreenBrowserCompositorOutputSurface::OnReflectorChanged() {
   }
 }
 
-void OffscreenBrowserCompositorOutputSurface::OnSwapBuffersComplete() {
+void OffscreenBrowserCompositorOutputSurface::OnSwapBuffersComplete(
+    const std::vector<ui::LatencyInfo>& latency_info) {
+  RenderWidgetHostImpl::CompositorFrameDrawn(latency_info);
   client_->DidReceiveSwapBuffersAck();
 }
 
