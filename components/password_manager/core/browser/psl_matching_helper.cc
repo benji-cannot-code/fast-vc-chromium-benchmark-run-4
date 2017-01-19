886628854002e7a459069187639cd273438eb4de
jdoerrie <jdoerrie@chromium.org>
2017-01-19 01:26:44 -0800

    Fetch federated PSL-matches from the password store.
    This change adds the possibility to apply PSL matches to federated logins. In
    order to do so it adds a PSL matching helper method and modifies the SQL
    statement used.
    
    BUG=666340
    R=vasilii@chromium.org
    
    Review-Url: https://codereview.chromium.org/2634163002
    Cr-Commit-Position: refs/heads/master@{#444689}

--- components/password_manager/core/browser/psl_matching_helper.cc
+++ components/password_manager/core/browser/psl_matching_helper.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/password_form.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 #include "url/gurl.h"
+#include "url/url_constants.h"
 
 using autofill::PasswordForm;
 
@@ -58,4 +59,26 @@ bool IsFederatedMatch(const std::string& signon_realm, const GURL& origin) {
                           base::CompareCase::INSENSITIVE_ASCII);
 }
 
+bool IsFederatedPSLMatch(const std::string& signon_realm, const GURL& origin) {
+  // The format should be "federation://origin.host/federation.host;
+  // Check for presence of "federation://" prefix.
+  static constexpr char federation_prefix[] = "federation://";
+  if (!base::StartsWith(signon_realm, federation_prefix,
+                        base::CompareCase::INSENSITIVE_ASCII))
+    return false;
+
+  // Replace federation scheme with HTTPS. This results in correct parsing of
+  // host and path, and forces origin to have a HTTPS scheme in order to return
+  // true.
+  GURL::Replacements replacements;
+  replacements.SetSchemeStr(url::kHttpsScheme);
+  GURL https_signon_realm = GURL(signon_realm).ReplaceComponents(replacements);
+
+  // Check for non-empty federation.host.
+  if (!https_signon_realm.has_path() || https_signon_realm.path_piece() == "/")
+    return false;
+
+  return IsPublicSuffixDomainMatch(https_signon_realm.GetOrigin().spec(),
+                                   origin.GetOrigin().spec());
+}
 }  // namespace password_manager
