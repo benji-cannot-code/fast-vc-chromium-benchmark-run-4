bad32b59ad3de9200abafd5fe42b0fa1e4ff2941
csharrison <csharrison@chromium.org>
2017-01-18 19:44:15 -0800

    Use TaskRunnerTimer in PingLoader
    Blocker for the per-frame scheduler.
    
    BUG=624694
    
    Review-Url: https://codereview.chromium.org/2646523002
    Cr-Commit-Position: refs/heads/master@{#444627}

--- third_party/WebKit/Source/core/loader/PingLoader.cpp
+++ third_party/WebKit/Source/core/loader/PingLoader.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/DOMArrayBufferView.h"
 #include "core/dom/Document.h"
 #include "core/dom/SecurityContext.h"
+#include "core/dom/TaskRunnerHelper.h"
 #include "core/fetch/CrossOriginAccessControl.h"
 #include "core/fetch/FetchContext.h"
 #include "core/fetch/FetchInitiatorTypeNames.h"
@@ -54,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/loader/FrameLoaderClient.h"
 #include "core/loader/MixedContentChecker.h"
 #include "core/page/Page.h"
+#include "platform/Timer.h"
 #include "platform/WebFrameScheduler.h"
 #include "platform/exported/WrappedResourceRequest.h"
 #include "platform/exported/WrappedResourceResponse.h"
@@ -219,7 +221,7 @@ class PingLoaderImpl : public GarbageCollectedFinalized<PingLoaderImpl>,
   void didFailLoading(LocalFrame*);
 
   std::unique_ptr<WebURLLoader> m_loader;
-  Timer<PingLoaderImpl> m_timeout;
+  TaskRunnerTimer<PingLoaderImpl> m_timeout;
   String m_url;
   unsigned long m_identifier;
   SelfKeepAlive<PingLoaderImpl> m_keepAlive;
@@ -237,7 +239,9 @@ PingLoaderImpl::PingLoaderImpl(LocalFrame* frame,
                                StoredCredentials credentialsAllowed,
                                bool isBeacon)
     : ContextClient(frame),
-      m_timeout(this, &PingLoaderImpl::timeout),
+      m_timeout(TaskRunnerHelper::get(TaskType::Networking, frame),
+                this,
+                &PingLoaderImpl::timeout),
       m_url(request.url()),
       m_identifier(createUniqueIdentifier()),
       m_keepAlive(this),
