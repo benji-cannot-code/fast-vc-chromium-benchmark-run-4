d7039373df7b95f3448961bf90c767d57625a0b0
tkent <tkent@chromium.org>
2017-01-19 00:17:00 -0800

    SUMMARY element: click() for invisible SUMMARY should toggle DETAILS element.
    We had unreasonable layoutObject() existence check for DOMActivate event
    handling. It's not defined by the stadard.
    
    BUG=681711
    
    Review-Url: https://codereview.chromium.org/2644693003
    Cr-Commit-Position: refs/heads/master@{#444679}

--- third_party/WebKit/Source/core/html/HTMLSummaryElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLSummaryElement.cpp
@@ -99,7 +99,7 @@ bool HTMLSummaryElement::supportsFocus() const {
 }
 
 void HTMLSummaryElement::defaultEventHandler(Event* event) {
-  if (isMainSummary() && layoutObject()) {
+  if (isMainSummary()) {
     if (event->type() == EventTypeNames::DOMActivate &&
         !isClickableControl(event->target()->toNode())) {
       if (HTMLDetailsElement* details = detailsElement())
@@ -141,10 +141,7 @@ void HTMLSummaryElement::defaultEventHandler(Event* event) {
 }
 
 bool HTMLSummaryElement::willRespondToMouseClickEvents() {
-  if (isMainSummary() && layoutObject())
-    return true;
-
-  return HTMLElement::willRespondToMouseClickEvents();
+  return isMainSummary() || HTMLElement::willRespondToMouseClickEvents();
 }
 
 }  // namespace blink
