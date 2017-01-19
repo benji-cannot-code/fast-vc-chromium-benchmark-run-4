6736aae49ec843b6936026933c0124c41a856c3b
nhiroki <nhiroki@chromium.org>
2017-01-18 22:11:21 -0800

    FileSystem: Migrate ExecutionContextTask to WTF::Closure
    BUG=625927
    
    Review-Url: https://codereview.chromium.org/2645473005
    Cr-Commit-Position: refs/heads/master@{#444661}

--- third_party/WebKit/Source/modules/filesystem/DOMFileSystem.cpp
+++ third_party/WebKit/Source/modules/filesystem/DOMFileSystem.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/filesystem/DOMFileSystem.h"
 
 #include "core/fileapi/BlobCallback.h"
+#include "core/inspector/InspectorInstrumentation.h"
 #include "modules/filesystem/DOMFilePath.h"
 #include "modules/filesystem/DirectoryEntry.h"
 #include "modules/filesystem/FileEntry.h"
@@ -41,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/filesystem/FileWriterCallback.h"
 #include "modules/filesystem/MetadataCallback.h"
 #include "platform/FileMetadata.h"
+#include "platform/WebTaskRunner.h"
 #include "platform/weborigin/SecurityOrigin.h"
 #include "public/platform/Platform.h"
 #include "public/platform/WebFileSystem.h"
@@ -51,6 +53,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 namespace blink {
+
+namespace {
+
+void runCallback(ExecutionContext* executionContext,
+                 std::unique_ptr<WTF::Closure> task) {
+  if (!executionContext)
+    return;
+  DCHECK(executionContext->isContextThread());
+  InspectorInstrumentation::AsyncTask asyncTask(executionContext, task.get(),
+                                                true /* isInstrumented */);
+  (*task)();
+}
+
+}  // namespace
 
 // static
 DOMFileSystem* DOMFileSystem::create(ExecutionContext* context,
@@ -123,10 +139,10 @@ void DOMFileSystem::reportError(ErrorCallbackBase* errorCallback,
 void DOMFileSystem::reportError(ExecutionContext* executionContext,
                                 ErrorCallbackBase* errorCallback,
                                 FileError::ErrorCode fileError) {
-  if (errorCallback)
-    scheduleCallback(
-        executionContext,
-        createSameThreadTask(&ErrorCallbackBase::invoke,
+  if (!errorCallback)
+    return;
+  scheduleCallback(executionContext,
+                   WTF::bind(&ErrorCallbackBase::invoke,
                              wrapPersistent(errorCallback), fileError));
 }
 
@@ -188,6 +204,17 @@ void DOMFileSystem::createFile(const FileEntry* fileEntry,
       fileSystemURL,
       SnapshotFileCallback::create(this, fileEntry->name(), fileSystemURL,
                                    successCallback, errorCallback, m_context));
+}
+
+void DOMFileSystem::scheduleCallback(ExecutionContext* executionContext,
+                                     std::unique_ptr<WTF::Closure> task) {
+  DCHECK(executionContext->isContextThread());
+  InspectorInstrumentation::asyncTaskScheduled(
+      executionContext, taskNameForInstrumentation(), task.get());
+  TaskRunnerHelper::get(TaskType::FileReading, executionContext)
+      ->postTask(BLINK_FROM_HERE,
+                 WTF::bind(&runCallback, wrapWeakPersistent(executionContext),
+                           WTF::passed(std::move(task))));
 }
 
 DEFINE_TRACE(DOMFileSystem) {
