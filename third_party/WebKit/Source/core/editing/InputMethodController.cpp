3064c46ded4a87b739db00a9537fc0d254cbcabb
aelias <aelias@chromium.org>
2017-01-18 17:38:43 -0800

    Revert 'Make "compositionend" event fired after setting caret position'
    This reverts http://crrev.com/2493703002, which was bisected to cause Google
    Docs Korean IME issue 674295.  Note the revert is unclean in that the
    structural changes to InputMethodControllerTest are preserved (only the
    new tests are deleted) and a new updateStyleAndLayoutIgnorePendingStylesheets
    was needed because of changes since the original landing.
    
    TBR=yosin@chromium.org
    BUG=674295,663944
    
    Review-Url: https://codereview.chromium.org/2644623003
    Cr-Commit-Position: refs/heads/master@{#444593}

--- third_party/WebKit/Source/core/editing/InputMethodController.cpp
+++ third_party/WebKit/Source/core/editing/InputMethodController.cpp
@@ -252,18 +252,13 @@ bool InputMethodController::finishComposingText(
     PlainTextRange oldOffsets = getSelectionOffsets();
     Editor::RevealSelectionScope revealSelectionScope(&editor());
 
-    const String& composing = composingText();
-    const bool result = replaceComposition(composing);
+    bool result = replaceComposition(composingText());
 
     // TODO(xiaochengh): The use of updateStyleAndLayoutIgnorePendingStylesheets
     // needs to be audited. see http://crbug.com/590369 for more details.
     document().updateStyleAndLayoutIgnorePendingStylesheets();
 
     setSelectionOffsets(oldOffsets);
-
-    // No DOM update after 'compositionend'.
-    dispatchCompositionEndEvent(frame(), composing);
-
     return result;
   }
 
@@ -318,6 +313,9 @@ bool InputMethodController::replaceComposition(const String& text) {
   if (!isAvailable())
     return false;
 
+  // No DOM update after 'compositionend'.
+  dispatchCompositionEndEvent(frame(), text);
+
   return true;
 }
 
@@ -365,17 +363,15 @@ bool InputMethodController::replaceCompositionAndMoveCaret(
   if (!replaceComposition(text))
     return false;
 
+  // TODO(xiaochengh): The use of updateStyleAndLayoutIgnorePendingStylesheets
+  // needs to be audited. see http://crbug.com/590369 for more details.
+  document().updateStyleAndLayoutIgnorePendingStylesheets();
+
   addCompositionUnderlines(underlines, rootEditableElement, textStart);
 
   int absoluteCaretPosition = computeAbsoluteCaretPosition(
       textStart, text.length(), relativeCaretPosition);
-  if (!moveCaret(absoluteCaretPosition))
-    return false;
-
-  // No DOM update after 'compositionend'.
-  dispatchCompositionEndEvent(frame(), text);
-
-  return true;
+  return moveCaret(absoluteCaretPosition);
 }
 
 bool InputMethodController::insertText(const String& text) {
@@ -544,9 +540,7 @@ void InputMethodController::setComposition(
     document().updateStyleAndLayoutIgnorePendingStylesheets();
 
     setEditableSelectionOffsets(selectedRange);
-
-    // No DOM update after 'compositionend'.
-    return dispatchCompositionEndEvent(frame(), text);
+    return;
   }
 
   // We should send a 'compositionstart' event only when the given text is not
