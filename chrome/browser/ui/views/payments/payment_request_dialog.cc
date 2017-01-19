ef2d1d1852519ed6496e9d4f7a5d5811ec7d9362
mathp <mathp@chromium.org>
2017-01-19 12:01:15 -0800

    [Payments] Introduce basic interactive browsertests for Payment Request
    According to code documentation [1], the tests need to be in
    "interactive_ui_tests" because they will need focus, and thus can't be
    sharded like regular browsertests.
    
    I also went with the approach of putting several test classes in the
    same file for a couple of reasons: we may not have many tests at first
    (thus little clutter), and it's easier to add a test when one can see
    what other tests are doing nearby. Certainly later on as these things
    are less true, we can split into multiple files as needed.
    
    Also want to mention that the tests are simplistic now; a follow-up CL
    will focus on View elements and click them.
    
    [1]
    https://cs.chromium.org/chromium/src/chrome/test/base/interactive_test_utils.h?rcl=0&l=37
    
    BUG=679734
    TEST=./out/Default/interactive_ui_tests --gtest_filter=PaymentRequest*
    
    Review-Url: https://codereview.chromium.org/2631133003
    Cr-Commit-Position: refs/heads/master@{#444825}

--- chrome/browser/ui/views/payments/payment_request_dialog.cc
+++ chrome/browser/ui/views/payments/payment_request_dialog.cc
@@ -22,8 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chrome {
 
 void ShowPaymentRequestDialog(payments::PaymentRequest* request) {
-  constrained_window::ShowWebModalDialogViews(
-      new payments::PaymentRequestDialog(request), request->web_contents());
+  payments::PaymentRequestDialog::ShowWebModalPaymentDialog(
+      new payments::PaymentRequestDialog(request, /* no observer */ nullptr),
+      request);
 }
 
 }  // namespace chrome
@@ -49,8 +50,10 @@ std::unique_ptr<views::View> CreateViewAndInstallController(
 
 }  // namespace
 
-PaymentRequestDialog::PaymentRequestDialog(PaymentRequest* request)
-    : request_(request) {
+PaymentRequestDialog::PaymentRequestDialog(
+    PaymentRequest* request,
+    PaymentRequestDialog::ObserverForTest* observer)
+    : request_(request), observer_(observer) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   SetLayoutManager(new views::FillLayout());
 
@@ -110,10 +113,19 @@ void PaymentRequestDialog::CloseDialog() {
   GetWidget()->Close();
 }
 
+// static
+void PaymentRequestDialog::ShowWebModalPaymentDialog(
+    PaymentRequestDialog* dialog,
+    PaymentRequest* request) {
+  constrained_window::ShowWebModalDialogViews(dialog, request->web_contents());
+}
+
 void PaymentRequestDialog::ShowInitialPaymentSheet() {
   view_stack_.Push(CreateViewAndInstallController<PaymentSheetViewController>(
                        &controller_map_, request_, this),
                    false);
+  if (observer_)
+    observer_->OnDialogOpened();
 }
 
 gfx::Size PaymentRequestDialog::GetPreferredSize() const {
---
+++ chrome/browser/ui/views/payments/test_chrome_payment_request_delegate.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/payments/test_chrome_payment_request_delegate.h"
+
+#include "content/public/browser/web_contents.h"
+
+namespace payments {
+
+TestChromePaymentRequestDelegate::TestChromePaymentRequestDelegate(
+    content::WebContents* web_contents,
+    PaymentRequestDialog::ObserverForTest* observer)
+    : ChromePaymentRequestDelegate(web_contents), observer_(observer) {}
+
+void TestChromePaymentRequestDelegate::ShowPaymentRequestDialog(
+    PaymentRequest* request) {
+  PaymentRequestDialog::ShowWebModalPaymentDialog(
+      new PaymentRequestDialog(request, observer_), request);
+}
+
+}  // namespace payments
