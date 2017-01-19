b18423b7d94d14ed49d3beed9713233708148933
tbansal <tbansal@chromium.org>
2017-01-18 19:10:58 -0800

    Write network quality estimates to prefs in Cronet
    Write network quality estimates to prefs (managed by Cronet). In a
    subsequent CL, the prefs will be read and used if the embedder has
    enabled cached estimates. The pref is written every time there is a
    change in the effective connection type.
    
    Also, change the network quality store behavior so that it notifies
    the observer of cached network qualities (as soon as the observer is
    added).
    
    BUG=490870
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_cronet_tester
    
    Review-Url: https://codereview.chromium.org/2617413004
    Cr-Commit-Position: refs/heads/master@{#444620}

--- net/nqe/network_quality_store.cc
+++ net/nqe/network_quality_store.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/nqe/network_quality_store.h"
 
+#include "base/location.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "net/base/network_change_notifier.h"
 
 namespace net {
@@ -13,7 +15,8 @@ namespace nqe {
 
 namespace internal {
 
-NetworkQualityStore::NetworkQualityStore() {
+NetworkQualityStore::NetworkQualityStore()
+    : disable_offline_check_(false), weak_ptr_factory_(this) {
   static_assert(kMaximumNetworkQualityCacheSize > 0,
                 "Size of the network quality cache must be > 0");
   // This limit should not be increased unless the logic for removing the
@@ -33,9 +36,13 @@ void NetworkQualityStore::Add(
   DCHECK_LE(cached_network_qualities_.size(),
             static_cast<size_t>(kMaximumNetworkQualityCacheSize));
 
-  // If the network name is unavailable, caching should not be performed.
-  if (network_id.type != net::NetworkChangeNotifier::CONNECTION_ETHERNET &&
-      network_id.id.empty()) {
+  // If the network name is unavailable, caching should not be performed. If
+  // |disable_offline_check_| is set to true, cache the network quality even if
+  // the network is set to offline.
+  if (network_id.type != NetworkChangeNotifier::CONNECTION_ETHERNET &&
+      network_id.id.empty() &&
+      (network_id.type != NetworkChangeNotifier::CONNECTION_NONE ||
+       !disable_offline_check_)) {
     return;
   }
 
@@ -81,17 +88,37 @@ bool NetworkQualityStore::GetById(
 }
 
 void NetworkQualityStore::AddNetworkQualitiesCacheObserver(
-    nqe::internal::NetworkQualityStore::NetworkQualitiesCacheObserver*
-        observer) {
+    NetworkQualitiesCacheObserver* observer) {
   DCHECK(thread_checker_.CalledOnValidThread());
   network_qualities_cache_observer_list_.AddObserver(observer);
+
+  // Notify the |observer| on the next message pump since |observer| may not
+  // be completely set up for receiving the callbacks.
+  base::ThreadTaskRunnerHandle::Get()->PostTask(
+      FROM_HERE, base::Bind(&NetworkQualityStore::NotifyCacheObserverIfPresent,
+                            weak_ptr_factory_.GetWeakPtr(), observer));
 }
 
 void NetworkQualityStore::RemoveNetworkQualitiesCacheObserver(
-    nqe::internal::NetworkQualityStore::NetworkQualitiesCacheObserver*
-        observer) {
+    NetworkQualitiesCacheObserver* observer) {
   DCHECK(thread_checker_.CalledOnValidThread());
   network_qualities_cache_observer_list_.RemoveObserver(observer);
+}
+
+void NetworkQualityStore::DisableOfflineCheckForTesting(
+    bool disable_offline_check) {
+  DCHECK(thread_checker_.CalledOnValidThread());
+  disable_offline_check_ = disable_offline_check;
+}
+
+void NetworkQualityStore::NotifyCacheObserverIfPresent(
+    NetworkQualitiesCacheObserver* observer) const {
+  DCHECK(thread_checker_.CalledOnValidThread());
+
+  if (!network_qualities_cache_observer_list_.HasObserver(observer))
+    return;
+  for (const auto it : cached_network_qualities_)
+    observer->OnChangeInCachedNetworkQuality(it.first, it.second);
 }
 
 }  // namespace internal
