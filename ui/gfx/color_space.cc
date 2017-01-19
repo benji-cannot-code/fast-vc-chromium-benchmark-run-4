19f924d7f4c2d05e68dcf59b7de16ffd7d736454
hubbe <hubbe@chromium.org>
2017-01-19 15:09:10 -0800

    HDR preparations
    1. Make HDR transfer functions return values greater than 1.0
    2. Add IsHDR() to gfx::ColorSpace
    3. Add CreateSCRGB() to gfx::ColorSpace
    
    BUG=682416
    
    Review-Url: https://codereview.chromium.org/2640393002
    Cr-Commit-Position: refs/heads/master@{#444878}

--- ui/gfx/color_space.cc
+++ ui/gfx/color_space.cc
@@ -87,6 +87,12 @@ ColorSpace ColorSpace::CreateSRGB() {
   return result;
 }
 
+// static
+ColorSpace ColorSpace::CreateSCRGBLinear() {
+  return ColorSpace(PrimaryID::BT709, TransferID::LINEAR, MatrixID::RGB,
+                    RangeID::FULL);
+}
+
 // Static
 ColorSpace ColorSpace::CreateXYZD50() {
   return ColorSpace(PrimaryID::XYZ_D50, TransferID::LINEAR, MatrixID::RGB,
@@ -120,6 +126,11 @@ bool ColorSpace::operator==(const ColorSpace& other) const {
              sizeof(custom_primary_matrix_)))
     return false;
   return true;
+}
+
+bool ColorSpace::IsHDR() const {
+  return transfer_ == TransferID::SMPTEST2084 ||
+         transfer_ == TransferID::ARIB_STD_B67;
 }
 
 bool ColorSpace::operator!=(const ColorSpace& other) const {
