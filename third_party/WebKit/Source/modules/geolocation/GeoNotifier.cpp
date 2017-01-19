77e3898d49fd4e97fbf06561f47432dde0a777a2
jianli <jianli@chromium.org>
2017-01-19 12:49:55 -0800

    Revert of Move geolocation timer to frame-specific TaskRunnerTimer. (patchset #2 id:20001 of https://codereview.chromium.org/2642863002/ )
    Reason for revert:
    Seems to be the cause for failed test fast/dom/Geolocation/window-close-crash.html
    
    https://uberchromegw.corp.google.com/i/chromium.webkit/builders/WebKit%20Linux%20Trusty%20Leak/builds/1635
    
    Original issue's description:
    > Move geolocation timer to frame-specific TaskRunnerTimer.
    >
    > Move geolocation timer |m_timer| to frame-specific TaskRunnerTimer. This
    > associates it with the geolocation frame's timer task queue. Note that the
    > geolocation specification under specifies exactly what kind of timeout
    > should be used, and we will use MiscPlatformAPI. See:
    >
    > https://www.w3.org/TR/geolocation-API/
    >
    > This m_timer is used to support, in part, the geolocation
    > 'position options'.
    >
    > BUG=624694
    >
    > R=haraken,scheib
    >
    > Review-Url: https://codereview.chromium.org/2642863002
    > Cr-Commit-Position: refs/heads/master@{#444794}
    > Committed: https://chromium.googlesource.com/chromium/src/+/d620cdbebcf22fbc7a76c269cf289ab87f5c646d
    
    TBR=haraken@chromium.org,scheib@chromium.org,dougt@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=624694
    
    Review-Url: https://codereview.chromium.org/2640403002
    Cr-Commit-Position: refs/heads/master@{#444839}

--- third_party/WebKit/Source/modules/geolocation/GeoNotifier.cpp
+++ third_party/WebKit/Source/modules/geolocation/GeoNotifier.cpp
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "modules/geolocation/GeoNotifier.h"
 
-#include "core/dom/TaskRunnerHelper.h"
 #include "modules/geolocation/Geolocation.h"
 #include "modules/geolocation/PositionError.h"
 #include "modules/geolocation/PositionOptions.h"
@@ -22,10 +21,7 @@ GeoNotifier::GeoNotifier(Geolocation* geolocation,
       m_successCallback(successCallback),
       m_errorCallback(errorCallback),
       m_options(options),
-      m_timer(TaskRunnerHelper::get(TaskType::MiscPlatformAPI,
-                                    geolocation->frame()),
-              this,
-              &GeoNotifier::timerFired),
+      m_timer(this, &GeoNotifier::timerFired),
       m_useCachedPosition(false) {
   DCHECK(m_geolocation);
   DCHECK(m_successCallback);
