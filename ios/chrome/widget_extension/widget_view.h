a64acee85aa3b9f17f21df0df294959cbb6feeb1
lod <lod@chromium.org>
2017-01-19 02:52:37 -0800

    Add a prototype today extension behind gn flag
    As each extension requires a mobileprovision, in order not to request one
    unnecessarily, this prototype re-uses the current today extension's.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2632333002
    Cr-Commit-Position: refs/heads/master@{#444702}

---
+++ ios/chrome/widget_extension/widget_view.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_WIDGET_EXTENSION_WIDGET_VIEW_H_
+#define IOS_CHROME_WIDGET_EXTENSION_WIDGET_VIEW_H_
+
+#import <UIKit/UIKit.h>
+
+@interface WidgetView : UIView
+
+- (instancetype)init NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithFrame:(CGRect)frame NS_UNAVAILABLE;
+- (instancetype)initWithCoder:(NSCoder*)aDecoder NS_UNAVAILABLE;
+
+@end
+
+#endif  // IOS_CHROME_WIDGET_EXTENSION_WIDGET_VIEW_H_
