07332a3d6cfa20fbb5e04ba3a4a079ce33757d4c
nhiroki <nhiroki@chromium.org>
2017-01-19 01:22:07 -0800

    Worker: Move instrumentation flag check from WorkerThread to WorkerOrWorkletGlobalScope
    WorkerThread::postTask checks a flag of DevTools instrumentation, but this flag
    is set here only when it's called from WorkerOrWorkletGlobalScope::postTask.
    For cleanup, this CL moves the check from WorkerThread to
    WorkerOrWorkletGlobalScope.
    
    BUG=625927
    
    Review-Url: https://codereview.chromium.org/2640813006
    Cr-Commit-Position: refs/heads/master@{#444688}

--- third_party/WebKit/Source/core/workers/WorkerOrWorkletGlobalScope.cpp
+++ third_party/WebKit/Source/core/workers/WorkerOrWorkletGlobalScope.cpp
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/frame/Deprecation.h"
 #include "core/inspector/ConsoleMessage.h"
+#include "core/inspector/InspectorInstrumentation.h"
 #include "core/workers/WorkerReportingProxy.h"
 #include "core/workers/WorkerThread.h"
 
@@ -40,8 +41,27 @@ void WorkerOrWorkletGlobalScope::postTask(
     const String& taskNameForInstrumentation) {
   if (!thread())
     return;
-  thread()->postTask(location, std::move(task),
-                     !taskNameForInstrumentation.isEmpty());
+
+  bool isInstrumented = !taskNameForInstrumentation.isEmpty();
+  if (isInstrumented) {
+    InspectorInstrumentation::asyncTaskScheduled(this, "Worker task",
+                                                 task.get());
+  }
+
+  std::unique_ptr<ExecutionContextTask> wrappedTask = createCrossThreadTask(
+      &WorkerOrWorkletGlobalScope::runTask, wrapCrossThreadWeakPersistent(this),
+      WTF::passed(std::move(task)), isInstrumented);
+  thread()->postTask(location, std::move(wrappedTask));
+}
+
+void WorkerOrWorkletGlobalScope::runTask(
+    std::unique_ptr<ExecutionContextTask> task,
+    bool isInstrumented,
+    ExecutionContext*) {
+  DCHECK(thread()->isCurrentThread());
+  InspectorInstrumentation::AsyncTask asyncTask(this, task.get(),
+                                                isInstrumented);
+  task->performTask(this);
 }
 
 }  // namespace blink
