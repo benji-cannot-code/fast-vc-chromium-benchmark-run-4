455164f44b9636fa2044a284c2332882f093623a
donnd <donnd@chromium.org>
2017-01-19 14:18:32 -0800

    [TTS] Fix a test that's failing with PlzNavigate enabled.
    Fix ContextualSearchManagerTest_testPrefetchFailoverRequestMadeAfterOpen
    failing with PlzNavigate.  When we simulate a navigation failure we
    follow that with a call to stop navigation.  This causes
    NavigationHandleImpl::GetPageTransition to fail an assert because
    the navigation has not actually started yet, due to the way we
    partially fake navigation in tests.
    
    This CL vectors stopping the navigation through our NetworkCommunicator,
    since network control actions should not run live in tests.
    
    BUG=674303
    
    Review-Url: https://codereview.chromium.org/2644933002
    Cr-Commit-Position: refs/heads/master@{#444860}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchFakeServer.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchFakeServer.java
@@ -551,6 +551,13 @@ class ContextualSearchFakeServer
     }
 
     @Override
+    public void stopPanelContentsNavigation() {
+        // Stub out stop() of the WebContents.
+        // Navigation of the content in the overlay may have been faked in tests,
+        // so stopping the WebContents navigation is unsafe.
+    }
+
+    @Override
     @Nullable
     public URL getBasePageUrl() {
         URL baseUrl = mBaseManager.getBasePageUrl();
