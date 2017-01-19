cb1d46ea85bfd42f1ddb00c23fdd5efc94ff8ba1
sigbjornf <sigbjornf@opera.com>
2017-01-18 22:41:10 -0800

    LocalFileSystem::deleteFileSystem() is no longer used.
    This method stopped being used when DevTools retired
    its FileSystem API support (crbug.com/588817); follow
    through and remove the unused implementation.
    
    R=nhiroki,kinuko,haraken
    BUG=
    
    Review-Url: https://codereview.chromium.org/2640963002
    Cr-Commit-Position: refs/heads/master@{#444667}

--- content/browser/fileapi/fileapi_message_filter.h
+++ content/browser/fileapi/fileapi_message_filter.h
@@ -91,9 +91,6 @@ class CONTENT_EXPORT FileAPIMessageFilter : public BrowserMessageFilter {
                         storage::FileSystemType type);
   void OnResolveURL(int request_id,
                     const GURL& filesystem_url);
-  void OnDeleteFileSystem(int request_id,
-                          const GURL& origin_url,
-                          storage::FileSystemType type);
   void OnMove(int request_id,
               const GURL& src_path,
               const GURL& dest_path);
@@ -169,8 +166,6 @@ class CONTENT_EXPORT FileAPIMessageFilter : public BrowserMessageFilter {
                      const storage::FileSystemInfo& info,
                      const base::FilePath& file_path,
                      storage::FileSystemContext::ResolvedEntryType type);
-  void DidDeleteFileSystem(int request_id,
-                           base::File::Error result);
   void DidCreateSnapshot(
       int request_id,
       const storage::FileSystemURL& url,
--- third_party/WebKit/Source/modules/filesystem/LocalFileSystem.h
+++ third_party/WebKit/Source/modules/filesystem/LocalFileSystem.h
@@ -68,9 +68,6 @@ class LocalFileSystem final : public GarbageCollectedFinalized<LocalFileSystem>,
                          FileSystemType,
                          long long size,
                          std::unique_ptr<AsyncFileSystemCallbacks>);
-  void deleteFileSystem(ExecutionContext*,
-                        FileSystemType,
-                        std::unique_ptr<AsyncFileSystemCallbacks>);
 
   FileSystemClient* client() const { return m_client.get(); }
 
@@ -91,9 +88,6 @@ class LocalFileSystem final : public GarbageCollectedFinalized<LocalFileSystem>,
                                  FileSystemType,
                                  CallbackWrapper*);
   void resolveURLInternal(ExecutionContext*, const KURL&, CallbackWrapper*);
-  void deleteFileSystemInternal(ExecutionContext*,
-                                FileSystemType,
-                                CallbackWrapper*);
 
   std::unique_ptr<FileSystemClient> m_client;
 };
