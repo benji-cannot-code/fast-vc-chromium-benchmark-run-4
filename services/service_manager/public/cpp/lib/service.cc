3c9f57af1754be6cbacbb7bba3cf52cc70b52f06
ben <ben@chromium.org>
2017-01-19 14:27:33 -0800

    Add OnBindInterface() to client lib Service contract.
    Default implementation just calls thru to OnConnect(). Subsequent changes will convert callsites & introduce a binder registry object.
    
    R=rockot@chromium.org
    
    Review-Url: https://codereview.chromium.org/2640813004
    Cr-Commit-Position: refs/heads/master@{#444864}

--- services/service_manager/public/cpp/lib/service.cc
+++ services/service_manager/public/cpp/lib/service.cc
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/service_manager/public/cpp/service.h"
 
 #include "base/logging.h"
+#include "services/service_manager/public/cpp/service_context.h"
+#include "services/service_manager/public/interfaces/interface_provider.mojom.h"
+#include "services/service_manager/public/interfaces/interface_provider_spec.mojom.h"
 
 namespace service_manager {
 
@@ -18,6 +21,26 @@ void Service::OnStart() {}
 bool Service::OnConnect(const ServiceInfo& remote_info,
                         InterfaceRegistry* registry) {
   return false;
+}
+
+void Service::OnBindInterface(const ServiceInfo& source_info,
+                              const std::string& interface_name,
+                              mojo::ScopedMessagePipeHandle interface_pipe) {
+  // TODO(beng): Eliminate this implementation once everyone is migrated to
+  //             OnBindInterface().
+  mojom::InterfaceProviderPtr interface_provider;
+  InterfaceProviderSpec source_spec, target_spec;
+  GetInterfaceProviderSpec(
+      mojom::kServiceManager_ConnectorSpec,
+      service_context_->local_info().interface_provider_specs,
+      &target_spec);
+  GetInterfaceProviderSpec(
+      mojom::kServiceManager_ConnectorSpec,
+      source_info.interface_provider_specs,
+      &source_spec);
+  service_context_->CallOnConnect(source_info, source_spec, target_spec,
+                                  MakeRequest(&interface_provider));
+  interface_provider->GetInterface(interface_name, std::move(interface_pipe));
 }
 
 bool Service::OnStop() { return true; }
