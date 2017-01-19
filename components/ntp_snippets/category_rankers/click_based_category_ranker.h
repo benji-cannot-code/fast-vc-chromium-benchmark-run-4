e5b530240f59109b7ae165a518ec92641576bd27
tschumann <tschumann@chromium.org>
2017-01-19 08:04:12 -0800

    Add experimental category boosting to the click based ranker.
    This is a simple override that if the client is in a category-promotion experiment,
    a selected category will always be pushed to the top of the suggestions surface.
    If the user dismissed that section, the promotion will be surpressed for 2 weeks.
    
    BUG=680210
    
    Review-Url: https://codereview.chromium.org/2640983003
    Cr-Commit-Position: refs/heads/master@{#444747}

--- components/ntp_snippets/category_rankers/click_based_category_ranker.h
+++ components/ntp_snippets/category_rankers/click_based_category_ranker.h
@@ -3,12 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_NTP_SNIPPETS_SECTION_RANKERS_CLICK_BASED_SECTION_RANKER_H_
-#define COMPONENTS_NTP_SNIPPETS_SECTION_RANKERS_CLICK_BASED_SECTION_RANKER_H_
+#ifndef COMPONENTS_NTP_SNIPPETS_CATEGORY_RANKERS_CLICK_BASED_CATEGORY_RANKER_H_
+#define COMPONENTS_NTP_SNIPPETS_CATEGORY_RANKERS_CLICK_BASED_CATEGORY_RANKER_H_
 
 #include <memory>
 #include <vector>
 
+#include "base/optional.h"
 #include "base/time/clock.h"
 #include "base/time/time.h"
 #include "components/ntp_snippets/category.h"
@@ -60,10 +61,14 @@ class ClickBasedCategoryRanker : public CategoryRanker {
   struct RankedCategory {
     Category category;
     int clicks;
+    base::Time last_dismissed;
 
-    RankedCategory(Category category, int clicks);
+    RankedCategory(Category category,
+                   int clicks,
+                   const base::Time& last_dismissed);
   };
 
+  base::Optional<Category> DeterminePromotedCategory();
   int GetPositionPassingMargin(
       std::vector<RankedCategory>::const_iterator category_position) const;
   void RestoreDefaultOrder();
@@ -80,12 +85,12 @@ class ClickBasedCategoryRanker : public CategoryRanker {
 
   std::vector<RankedCategory> ordered_categories_;
   PrefService* pref_service_;
-  // Allow for an injectable clock for testing.
   std::unique_ptr<base::Clock> clock_;
+  base::Optional<Category> promoted_category_;
 
   DISALLOW_COPY_AND_ASSIGN(ClickBasedCategoryRanker);
 };
 
 }  // namespace ntp_snippets
 
-#endif  // COMPONENTS_NTP_SNIPPETS_SECTION_RANKERS_CLICK_BASED_SECTION_RANKER_H_
+#endif  // COMPONENTS_NTP_SNIPPETS_CATEGORY_RANKERS_CLICK_BASED_CATEGORY_RANKER_H_
