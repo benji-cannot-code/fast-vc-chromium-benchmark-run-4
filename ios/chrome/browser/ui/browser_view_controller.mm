8e5173d4ecb444244b25f0bbb264358c1f9c2bf1
gambard <gambard@chromium.org>
2017-01-19 06:40:03 -0800

    Set ReadingList Snackbar duration to 2s
    BUG=682132
    
    Review-Url: https://codereview.chromium.org/2641023004
    Cr-Commit-Position: refs/heads/master@{#444728}

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -162,7 +162,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/public/provider/chrome/browser/voice/voice_search_controller.h"
 #include "ios/public/provider/chrome/browser/voice/voice_search_controller_delegate.h"
 #include "ios/public/provider/chrome/browser/voice/voice_search_provider.h"
-#import "ios/third_party/material_components_ios/src/components/Snackbar/src/MaterialSnackbar.h"
 #import "ios/web/navigation/crw_session_controller.h"
 #import "ios/web/navigation/crw_session_entry.h"
 #include "ios/web/navigation/navigation_manager_impl.h"
@@ -288,8 +287,6 @@ bool IsURLAllowedInIncognito(const GURL& url) {
 // Temporary key to use when storing native controllers vended to tabs before
 // they are added to the tab model.
 NSString* const kNativeControllerTemporaryKey = @"NativeControllerTemporaryKey";
-
-NSString* const kReadingListSnackbarCategory = @"ReadingListSnackbarCategory";
 
 }  // anonymous namespace
 
@@ -3505,12 +3502,8 @@ class BrowserBookmarkModelBridge : public bookmarks::BookmarkModelObserver {
   readingModel->AddEntry(URL, base::SysNSStringToUTF8(title),
                          reading_list::ADDED_VIA_CURRENT_APP);
 
-  NSString* snackBarMessage =
-      l10n_util::GetNSString(IDS_IOS_READING_LIST_SNACKBAR_MESSAGE);
-  MDCSnackbarMessage* message =
-      [MDCSnackbarMessage messageWithText:snackBarMessage];
-  message.category = kReadingListSnackbarCategory;
-  [MDCSnackbarManager showMessage:message];
+  [self showSnackbar:l10n_util::GetNSString(
+                         IDS_IOS_READING_LIST_SNACKBAR_MESSAGE)];
 }
 
 #pragma mark - Keyboard commands management
