545e46dc44dbeed6c9734801ad9d26bbd3596384
ericwilligers <ericwilligers@chromium.org>
2017-01-19 04:41:41 -0800

    Implements CSSPropertyAPI for the content property
    A part of Project Ribbon, separating the parsing logic for CSS
    properties from the parser into an API. This patch removes
    CSSPropertyContent from the switch statement in
    parseSingleValue, and calls the API instead.
    
    A function pointer to the parseSingleValue function from the API for the
    content property is stored in a CSSPropertyDescriptor, and is
    called from CSSPropertyParser.
    
    This patch:
    - Adds CSSPropertyAPIContent.cpp to the BUILD.gn file.
    - Adds api_class flag to CSSProperties.in, which indicates that
      CSSPropertyAPIContent.h is generated.
    - Moves the parsing logic for content from CSSPropertyParser.cpp to
      CSSPropertyAPIContent.cpp, which implements
      CSSPropertyAPI.h.
    
    BUG=668012
    
    Review-Url: https://codereview.chromium.org/2640023006
    Cr-Commit-Position: refs/heads/master@{#444712}

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/CSSBasicShapeValues.h"
 #include "core/css/CSSBorderImage.h"
 #include "core/css/CSSContentDistributionValue.h"
-#include "core/css/CSSCounterValue.h"
 #include "core/css/CSSCursorImageValue.h"
 #include "core/css/CSSCustomIdentValue.h"
 #include "core/css/CSSFontFaceSrcValue.h"
@@ -1470,89 +1469,6 @@ static CSSValue* consumeRxOrRy(CSSParserTokenRange& range) {
                                 UnitlessQuirk::Forbid);
 }
 
-static CSSValue* consumeAttr(CSSParserTokenRange args,
-                             const CSSParserContext* context) {
-  if (args.peek().type() != IdentToken)
-    return nullptr;
-
-  AtomicString attrName =
-      args.consumeIncludingWhitespace().value().toAtomicString();
-  if (!args.atEnd())
-    return nullptr;
-
-  // TODO(esprehn): This should be lowerASCII().
-  if (context->isHTMLDocument())
-    attrName = attrName.lower();
-
-  CSSFunctionValue* attrValue = CSSFunctionValue::create(CSSValueAttr);
-  attrValue->append(*CSSCustomIdentValue::create(attrName));
-  return attrValue;
-}
-
-static CSSValue* consumeCounterContent(CSSParserTokenRange args,
-                                       bool counters) {
-  CSSCustomIdentValue* identifier = consumeCustomIdent(args);
-  if (!identifier)
-    return nullptr;
-
-  CSSStringValue* separator = nullptr;
-  if (!counters) {
-    separator = CSSStringValue::create(String());
-  } else {
-    if (!consumeCommaIncludingWhitespace(args) ||
-        args.peek().type() != StringToken)
-      return nullptr;
-    separator = CSSStringValue::create(
-        args.consumeIncludingWhitespace().value().toString());
-  }
-
-  CSSIdentifierValue* listStyle = nullptr;
-  if (consumeCommaIncludingWhitespace(args)) {
-    CSSValueID id = args.peek().id();
-    if ((id != CSSValueNone &&
-         (id < CSSValueDisc || id > CSSValueKatakanaIroha)))
-      return nullptr;
-    listStyle = consumeIdent(args);
-  } else {
-    listStyle = CSSIdentifierValue::create(CSSValueDecimal);
-  }
-
-  if (!args.atEnd())
-    return nullptr;
-  return CSSCounterValue::create(identifier, listStyle, separator);
-}
-
-static CSSValue* consumeContent(CSSParserTokenRange& range,
-                                const CSSParserContext* context) {
-  if (identMatches<CSSValueNone, CSSValueNormal>(range.peek().id()))
-    return consumeIdent(range);
-
-  CSSValueList* values = CSSValueList::createSpaceSeparated();
-
-  do {
-    CSSValue* parsedValue = consumeImage(range, context);
-    if (!parsedValue)
-      parsedValue =
-          consumeIdent<CSSValueOpenQuote, CSSValueCloseQuote,
-                       CSSValueNoOpenQuote, CSSValueNoCloseQuote>(range);
-    if (!parsedValue)
-      parsedValue = consumeString(range);
-    if (!parsedValue) {
-      if (range.peek().functionId() == CSSValueAttr)
-        parsedValue = consumeAttr(consumeFunction(range), context);
-      else if (range.peek().functionId() == CSSValueCounter)
-        parsedValue = consumeCounterContent(consumeFunction(range), false);
-      else if (range.peek().functionId() == CSSValueCounters)
-        parsedValue = consumeCounterContent(consumeFunction(range), true);
-      if (!parsedValue)
-        return nullptr;
-    }
-    values->append(*parsedValue);
-  } while (!range.atEnd());
-
-  return values;
-}
-
 static CSSValue* consumePerspective(CSSParserTokenRange& range,
                                     const CSSParserContext* context,
                                     CSSPropertyID unresolvedProperty) {
@@ -2956,8 +2872,6 @@ const CSSValue* CSSPropertyParser::parseSingleValue(
     case CSSPropertyRx:
     case CSSPropertyRy:
       return consumeRxOrRy(m_range);
-    case CSSPropertyContent:
-      return consumeContent(m_range, m_context);
     case CSSPropertyListStyleImage:
     case CSSPropertyBorderImageSource:
     case CSSPropertyWebkitMaskBoxImageSource:
