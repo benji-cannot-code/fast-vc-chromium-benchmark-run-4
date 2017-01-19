b66620b77924033480b479b6d084d5da7a01dcf5
fs <fs@opera.com>
2017-01-19 11:13:15 -0800

    Add SVGResources helper for resource-bounds invalidation
    Add a new helper method removeClientFromCacheAffectingObjectBounds to
    SVGResources to better describe the intention of code that performs this
    operation (removeFromCacheAndInvalidateDependencies.) It also makes
    LayoutSVGResourceContainer not depend on its derived classes.
    Also make use of it in the generic SVGResources::removeClientFromCache.
    
    Review-Url: https://codereview.chromium.org/2647443004
    Cr-Commit-Position: refs/heads/master@{#444811}

--- third_party/WebKit/Source/core/layout/svg/SVGResources.cpp
+++ third_party/WebKit/Source/core/layout/svg/SVGResources.cpp
@@ -324,6 +324,19 @@ void SVGResources::layoutIfNeeded() {
     m_linkedResource->layoutIfNeeded();
 }
 
+void SVGResources::removeClientFromCacheAffectingObjectBounds(
+    LayoutObject* object,
+    bool markForInvalidation) const {
+  if (!m_clipperFilterMaskerData)
+    return;
+  if (LayoutSVGResourceClipper* clipper = m_clipperFilterMaskerData->clipper)
+    clipper->removeClientFromCache(object, markForInvalidation);
+  if (LayoutSVGResourceFilter* filter = m_clipperFilterMaskerData->filter)
+    filter->removeClientFromCache(object, markForInvalidation);
+  if (LayoutSVGResourceMasker* masker = m_clipperFilterMaskerData->masker)
+    masker->removeClientFromCache(object, markForInvalidation);
+}
+
 void SVGResources::removeClientFromCache(LayoutObject* object,
                                          bool markForInvalidation) const {
   if (!hasResourceData())
@@ -337,17 +350,7 @@ void SVGResources::removeClientFromCache(LayoutObject* object,
     return;
   }
 
-  if (m_clipperFilterMaskerData) {
-    if (m_clipperFilterMaskerData->clipper)
-      m_clipperFilterMaskerData->clipper->removeClientFromCache(
-          object, markForInvalidation);
-    if (m_clipperFilterMaskerData->filter)
-      m_clipperFilterMaskerData->filter->removeClientFromCache(
-          object, markForInvalidation);
-    if (m_clipperFilterMaskerData->masker)
-      m_clipperFilterMaskerData->masker->removeClientFromCache(
-          object, markForInvalidation);
-  }
+  removeClientFromCacheAffectingObjectBounds(object, markForInvalidation);
 
   if (m_markerData) {
     if (m_markerData->markerStart)
