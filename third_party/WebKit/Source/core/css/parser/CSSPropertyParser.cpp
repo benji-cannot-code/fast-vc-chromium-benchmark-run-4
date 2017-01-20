326f837ecc6ce6150d53fc71ef9f1b58cee8bfd3
aboxhall <aboxhall@chromium.org>
2017-01-19 22:13:02 -0800

    Implements CSSPropertyAPI for the padding-{top,right,bottom,left} properties.
    A part of Project Ribbon, separating the parsing logic for CSS
    properties from the parser into an API. This patch removes
    CSSPropertyPaddingTop, CSSPropertyPaddingBottom, CSSPropertyPaddingLeft
    and CSSPropertyPaddingRight from the switch statement in
    parseSingleValue, and calls the API instead.
    
    A function pointer to the parseSingleValue function from the API for the
    padding properties is stored in a CSSPropertyDescriptor, and is called
    from CSSPropertyParser.
    
    This patch:
    - Adds CSSPropertyAPIPadding.cpp to the BUILD.gn file.
    - Adds api_class=CSSPropertyAPIPadding flag to each border class property
      in CSSProperties.in, which indicates that CSSPropertyAPIPadding.h is
      generated.
    - Moves the parsing logic for padding from CSSPropertyParser.cpp to
      CSSPropertyAPIPadding.cpp, which implements CSSPropertyAPI.h.
    
    Review-Url: https://codereview.chromium.org/2645763003
    Cr-Commit-Position: refs/heads/master@{#444995}

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
@@ -2369,13 +2369,6 @@ const CSSValue* CSSPropertyParser::parseSingleValue(
     case CSSPropertyWebkitMarginAfter:
       return consumeMarginOrOffset(m_range, m_context->mode(),
                                    UnitlessQuirk::Forbid);
-    case CSSPropertyPaddingTop:
-    case CSSPropertyPaddingRight:
-    case CSSPropertyPaddingBottom:
-    case CSSPropertyPaddingLeft:
-      return consumeLengthOrPercent(m_range, m_context->mode(),
-                                    ValueRangeNonNegative,
-                                    UnitlessQuirk::Allow);
     case CSSPropertyTouchAction:
       return consumeTouchAction(m_range);
     case CSSPropertyScrollSnapDestination:
