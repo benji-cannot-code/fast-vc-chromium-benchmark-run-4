f740496a5ea50fbd3a0d2a8b8de482cc24b9265d
einbinder <einbinder@chromium.org>
2017-01-19 17:40:56 -0800

    DevTools: Show CSS variables in the inherited properties cascade
    BUG=680693
    
    Review-Url: https://codereview.chromium.org/2644703002
    Cr-Commit-Position: refs/heads/master@{#444929}

--- third_party/WebKit/Source/devtools/front_end/sdk/CSSMetadata.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/CSSMetadata.js
@@ -181,7 +181,8 @@ SDK.CSSMetadata = class {
    */
   isPropertyInherited(propertyName) {
     propertyName = propertyName.toLowerCase();
-    return this._inherited.has(this.canonicalPropertyName(propertyName)) || this._inherited.has(propertyName);
+    return propertyName.startsWith('--') || this._inherited.has(this.canonicalPropertyName(propertyName)) ||
+        this._inherited.has(propertyName);
   }
 
   /**
