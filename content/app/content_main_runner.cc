64a0d32a1cbf81e695d5e526058fa5fda35ed4be
scottmg <scottmg@chromium.org>
2017-01-20 11:37:56 -0800

    Make small sub-target of gin that can be used in browser without v8 dep
    This severs some of the remaining ties between browser dll and gin-proper.
    
    R=jochen@chromium.org
    BUG=581766
    
    Review-Url: https://codereview.chromium.org/2621143007
    Cr-Commit-Position: refs/heads/master@{#445133}

--- content/app/content_main_runner.cc
+++ content/app/content_main_runner.cc
@@ -72,7 +72,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ui_base_paths.h"
 #include "ui/base/ui_base_switches.h"
 
-#ifdef V8_USE_EXTERNAL_STARTUP_DATA
+#if defined(V8_USE_EXTERNAL_STARTUP_DATA) && \
+    !defined(CHROME_MULTIPLE_DLL_BROWSER)
 #include "gin/v8_initializer.h"
 #endif
 
@@ -733,8 +734,10 @@ class ContentMainRunnerImpl : public ContentMainRunner {
       gin::V8Initializer::LoadV8Natives();
     }
 #else
+#if !defined(CHROME_MULTIPLE_DLL_BROWSER)
     gin::V8Initializer::LoadV8Snapshot();
     gin::V8Initializer::LoadV8Natives();
+#endif  // !CHROME_MULTIPLE_DLL_BROWSER
 #endif  // OS_POSIX && !OS_MACOSX
 #endif  // V8_USE_EXTERNAL_STARTUP_DATA
 
