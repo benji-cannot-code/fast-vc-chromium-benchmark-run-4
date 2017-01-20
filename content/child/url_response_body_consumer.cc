ba9cdb507029df32b8db3518dd095a773a71950b
yhirano <yhirano@chromium.org>
2017-01-19 22:41:24 -0800

    [Mojo-Loading] Split too large data chunk in renderer
    With this CL, URLResponseBodyConsumer splits a too large data chunk to smaller
    chunks. Also, it stops sending chunks and defers the remaining jobs to
    subsequent tasks if it has already sent many bytes to the client in the task.
    
    The main motivation is performance (thread janks), but splitting a large chunk
    is needed also because some existing clients (e.g., NaCl compiler) cannot
    handle large chunks.
    
    BUG=603396
    
    Review-Url: https://codereview.chromium.org/2644053002
    Cr-Commit-Position: refs/heads/master@{#445002}

--- content/child/url_response_body_consumer.cc
+++ content/child/url_response_body_consumer.cc
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+constexpr uint32_t URLResponseBodyConsumer::kMaxNumConsumedBytesInTask;
+
 class URLResponseBodyConsumer::ReceivedData final
     : public RequestPeer::ReceivedData {
  public:
@@ -99,6 +101,7 @@ void URLResponseBodyConsumer::OnReadable(MojoResult unused) {
     return;
 
   DCHECK(!is_in_on_readable_);
+  uint32_t num_bytes_consumed = 0;
 
   // Protect |this| as RequestPeer::OnReceivedData may call deref.
   scoped_refptr<URLResponseBodyConsumer> protect(this);
@@ -123,6 +126,20 @@ void URLResponseBodyConsumer::OnReadable(MojoResult unused) {
       NotifyCompletionIfAppropriate();
       return;
     }
+    DCHECK_LE(num_bytes_consumed, kMaxNumConsumedBytesInTask);
+    available =
+        std::min(available, kMaxNumConsumedBytesInTask - num_bytes_consumed);
+    if (available == 0) {
+      // We've already consumed many bytes in this task. Defer the remaining
+      // to the next task.
+      result = mojo::EndReadDataRaw(handle_.get(), 0);
+      DCHECK_EQ(result, MOJO_RESULT_OK);
+      task_runner_->PostTask(FROM_HERE,
+                             base::Bind(&URLResponseBodyConsumer::OnReadable,
+                                        AsWeakPtr(), MOJO_RESULT_OK));
+      return;
+    }
+    num_bytes_consumed += available;
     ResourceDispatcher::PendingRequestInfo* request_info =
         resource_dispatcher_->GetPendingRequestInfo(request_id_);
     DCHECK(request_info);
