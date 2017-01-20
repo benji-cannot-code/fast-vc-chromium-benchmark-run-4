c0a1646a8924b0c06b3c062b78f8dc7be855e9a1
piperc <piperc@chromium.org>
2017-01-19 17:04:49 -0800

    Add APDU command and response packet classes
    APDU commands are defined in ISO 7816-4 and are used to
    communicate with FIDO U2F devices. The protocol is based
    on a request-response communication pair.
    
    BUG=NONE
    
    Review-Url: https://codereview.chromium.org/2610223002
    Cr-Commit-Position: refs/heads/master@{#444907}

---
+++ device/u2f/u2f_apdu_fuzzer.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stddef.h>
+#include <stdint.h>
+#include <algorithm>
+
+#include "u2f_apdu_command.h"
+#include "u2f_apdu_response.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  std::vector<uint8_t> input(data, data + size);
+  scoped_refptr<device::U2fApduCommand> cmd =
+      device::U2fApduCommand::CreateFromMessage(input);
+  scoped_refptr<device::U2fApduResponse> rsp =
+      device::U2fApduResponse::CreateFromMessage(input);
+  return 0;
+}
