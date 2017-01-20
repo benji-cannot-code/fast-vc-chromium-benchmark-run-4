0513f95de53f856e1251bdca1fd04f922f04e45c
jam <jam@chromium.org>
2017-01-19 22:37:33 -0800

    Convert SSLErrorHandler to use the new navigation callbacks.
    BUG=682002
    
    Review-Url: https://codereview.chromium.org/2642853003
    Cr-Commit-Position: refs/heads/master@{#445001}

--- chrome/browser/ssl/ssl_error_handler.cc
+++ chrome/browser/ssl/ssl_error_handler.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/network_time/network_time_tracker.h"
 #include "components/ssl_errors/error_classification.h"
 #include "components/ssl_errors/error_info.h"
+#include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_source.h"
 #include "content/public/browser/render_frame_host.h"
@@ -469,9 +470,11 @@ void SSLErrorHandler::Observe(
 #endif
 }
 
-void SSLErrorHandler::DidStartNavigationToPendingEntry(
-    const GURL& /* url */,
-    content::ReloadType /* reload_type */) {
+void SSLErrorHandler::DidStartNavigation(
+    content::NavigationHandle* navigation_handle) {
+  if (!navigation_handle->IsInMainFrame() || navigation_handle->IsSamePage())
+    return;
+
   // Destroy the error handler on all new navigations. This ensures that the
   // handler is properly recreated when a hanging page is navigated to an SSL
   // error, even when the tab's WebContents doesn't change.
