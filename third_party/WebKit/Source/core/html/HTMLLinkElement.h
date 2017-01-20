14e3ae3a02ded5697d2a63f348542f7009a10a60
hiroshige <hiroshige@chromium.org>
2017-01-19 19:18:25 -0800

    Call checkCompleted() synchronously in HTMLLinkElement::dispatchPendingEvent()
    Previously, there was an asynchronous hop from <link>'s load event to
    document's load event, because HTMLLinkElement::dispatchPendingEvent()
    scheduled checkCompleted() asynchronously via IncrementLoadEventDelayCount.
    
    This CL makes HTMLLinkElement::dispatchPendingEvent() to call checkCompleted()
    synchronously, by using
    IncrementLoadEventDelayCount::clearAndCheckLoadEvent().
    
    Because checkCompleted() is called at the end of asynchronously called
    HTMLLinkElement::dispatchPendingEvent(), this CL doesn't increase synchronous
    timing dependencies around Document load events.
    
    This CL fixes the performance regression of smoothness.top_25_smooth on
    Mac 10.10 caused by https://codereview.chromium.org/2275493002/.
    
    BUG=648887, 624697
    
    Review-Url: https://codereview.chromium.org/2391923002
    Cr-Commit-Position: refs/heads/master@{#444965}

--- third_party/WebKit/Source/core/html/HTMLLinkElement.h
+++ third_party/WebKit/Source/core/html/HTMLLinkElement.h
@@ -89,7 +89,6 @@ class CORE_EXPORT HTMLLinkElement final : public HTMLElement,
 
   DOMTokenList* sizes() const;
 
-  void dispatchPendingEvent(std::unique_ptr<IncrementLoadEventDelayCount>);
   void scheduleEvent();
 
   // From LinkLoaderClient
@@ -120,6 +119,10 @@ class CORE_EXPORT HTMLLinkElement final : public HTMLElement,
 
   void process();
   static void processCallback(Node*);
+
+  // Always call this asynchronously because this can cause synchronous
+  // Document load event and JavaScript execution.
+  void dispatchPendingEvent(std::unique_ptr<IncrementLoadEventDelayCount>);
 
   // From Node and subclassses
   void parseAttribute(const AttributeModificationParams&) override;
