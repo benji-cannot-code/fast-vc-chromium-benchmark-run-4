ac4a7129651492fdbebdbb5269662677c532748f
aboxhall <aboxhall@chromium.org>
2017-01-19 21:56:37 -0800

    [Devtools] Fix left margin for selection element
    BUG=679918
    
    Review-Url: https://codereview.chromium.org/2623053002
    Cr-Commit-Position: refs/heads/master@{#444990}

--- third_party/WebKit/Source/devtools/front_end/accessibility/AXTreePane.js
+++ third_party/WebKit/Source/devtools/front_end/accessibility/AXTreePane.js
@@ -14,6 +14,7 @@ Accessibility.AXTreePane = class extends Accessibility.AccessibilitySubPane {
 
     this._axSidebarView = axSidebarView;
     this._treeOutline = this.createTreeOutline();
+    this._treeOutline.setPaddingSize(12);
 
     this.element.classList.add('accessibility-computed');
 
