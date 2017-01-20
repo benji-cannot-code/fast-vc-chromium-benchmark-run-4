44f0b1a1d560bc637838ac24def234b4fe6b59bb
amoylan <amoylan@chromium.org>
2017-01-19 20:18:45 -0800

    Standardize ECursor enum value names
    This includes two updates to value names in the ECursor enum that
    are required in preparation for this enum to be automatically
    generated in future:
    * Use k-prefix
    * Rename Cross -> Crosshair
    
    BUG=628043
    
    Review-Url: https://codereview.chromium.org/2640713002
    Cr-Commit-Position: refs/heads/master@{#444981}

--- third_party/WebKit/Source/core/style/ComputedStyle.h
+++ third_party/WebKit/Source/core/style/ComputedStyle.h
@@ -1994,7 +1994,7 @@ class CORE_EXPORT ComputedStyle : public ComputedStyleBase,
   void setVerticalBorderSpacing(short);
 
   // cursor
-  static ECursor initialCursor() { return ECursor::Auto; }
+  static ECursor initialCursor() { return ECursor::kAuto; }
   ECursor cursor() const {
     return static_cast<ECursor>(m_inheritedData.m_cursorStyle);
   }
--- third_party/WebKit/Source/web/WebViewImpl.cpp
+++ third_party/WebKit/Source/web/WebViewImpl.cpp
@@ -1393,7 +1393,7 @@ static Node* findCursorDefiningAncestor(Node* node, LocalFrame* frame) {
   while (node) {
     if (node->layoutObject()) {
       ECursor cursor = node->layoutObject()->style()->cursor();
-      if (cursor != ECursor::Auto ||
+      if (cursor != ECursor::kAuto ||
           frame->eventHandler().useHandCursor(node, node->isLink()))
         break;
     }
@@ -1408,8 +1408,8 @@ static bool showsHandCursor(Node* node, LocalFrame* frame) {
     return false;
 
   ECursor cursor = node->layoutObject()->style()->cursor();
-  return cursor == ECursor::Pointer ||
-         (cursor == ECursor::Auto &&
+  return cursor == ECursor::kPointer ||
+         (cursor == ECursor::kAuto &&
           frame->eventHandler().useHandCursor(node, node->isLink()));
 }
 
