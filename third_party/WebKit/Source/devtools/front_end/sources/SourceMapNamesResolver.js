2805e46ea3fb61279e073eaecebac426b95d507f
luoe <luoe@chromium.org>
2017-01-20 15:51:52 -0800

    DevTools: add missing check for null script in SourceMapNamesResolver
    SourceMapNamesResolver.js was missing a check due to the possibility for
    SDK.DebuggerModel.Location.script() to return null.
    
    BUG=681419
    
    Review-Url: https://codereview.chromium.org/2649593002
    Cr-Commit-Position: refs/heads/master@{#445212}

--- third_party/WebKit/Source/devtools/front_end/sources/SourceMapNamesResolver.js
+++ third_party/WebKit/Source/devtools/front_end/sources/SourceMapNamesResolver.js
@@ -32,7 +32,8 @@ Sources.SourceMapNamesResolver._scopeIdentifiers = function(scope) {
   var endLocation = scope.endLocation();
 
   if (scope.type() === Protocol.Debugger.ScopeType.Global || !startLocation || !endLocation ||
-      !startLocation.script().sourceMapURL || (startLocation.script() !== endLocation.script()))
+      !startLocation.script() || !startLocation.script().sourceMapURL ||
+      (startLocation.script() !== endLocation.script()))
     return Promise.resolve(/** @type {!Array<!Sources.SourceMapNamesResolver.Identifier>}*/ ([]));
 
   var script = startLocation.script();
@@ -333,7 +334,8 @@ Sources.SourceMapNamesResolver.resolveScopeInObject = function(scope) {
   var endLocation = scope.endLocation();
 
   if (scope.type() === Protocol.Debugger.ScopeType.Global || !startLocation || !endLocation ||
-      !startLocation.script().sourceMapURL || startLocation.script() !== endLocation.script())
+      !startLocation.script() || !startLocation.script().sourceMapURL ||
+      startLocation.script() !== endLocation.script())
     return scope.object();
 
   return new Sources.SourceMapNamesResolver.RemoteObject(scope);
