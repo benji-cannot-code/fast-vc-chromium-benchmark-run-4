47dafe061bba5d37ea750ae2bb40db85376e2cf5
lazyboy <lazyboy@chromium.org>
2017-01-19 16:57:12 -0800

    Remove usage of AsyncExtensionFunction::results_ in system_network_api.cc
    Convert relevant classes to directly inherit from UIThreadExtensionFunction.
    
    BUG=648275
    
    Review-Url: https://codereview.chromium.org/2646703002
    Cr-Commit-Position: refs/heads/master@{#444903}

--- extensions/browser/api/system_network/system_network_api.cc
+++ extensions/browser/api/system_network/system_network_api.cc
@@ -20,14 +20,15 @@ SystemNetworkGetNetworkInterfacesFunction::
     ~SystemNetworkGetNetworkInterfacesFunction() {
 }
 
-bool SystemNetworkGetNetworkInterfacesFunction::RunAsync() {
+ExtensionFunction::ResponseAction
+SystemNetworkGetNetworkInterfacesFunction::Run() {
   content::BrowserThread::PostTask(
       content::BrowserThread::FILE,
       FROM_HERE,
       base::Bind(
           &SystemNetworkGetNetworkInterfacesFunction::GetListOnFileThread,
           this));
-  return true;
+  return RespondLater();
 }
 
 void SystemNetworkGetNetworkInterfacesFunction::GetListOnFileThread() {
@@ -53,8 +54,7 @@ void SystemNetworkGetNetworkInterfacesFunction::GetListOnFileThread() {
 
 void SystemNetworkGetNetworkInterfacesFunction::HandleGetListError() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  error_ = kNetworkListError;
-  SendResponse(false);
+  Respond(Error(kNetworkListError));
 }
 
 void SystemNetworkGetNetworkInterfacesFunction::SendResponseOnUIThread(
@@ -71,9 +71,8 @@ void SystemNetworkGetNetworkInterfacesFunction::SendResponseOnUIThread(
     create_arg.push_back(std::move(info));
   }
 
-  results_ =
-      api::system_network::GetNetworkInterfaces::Results::Create(create_arg);
-  SendResponse(true);
+  Respond(ArgumentList(
+      api::system_network::GetNetworkInterfaces::Results::Create(create_arg)));
 }
 
 }  // namespace api
--- extensions/browser/api/system_network/system_network_api.h
+++ extensions/browser/api/system_network/system_network_api.h
@@ -14,7 +14,7 @@ namespace extensions {
 namespace api {
 
 class SystemNetworkGetNetworkInterfacesFunction
-    : public AsyncExtensionFunction {
+    : public UIThreadExtensionFunction {
  public:
   DECLARE_EXTENSION_FUNCTION("system.network.getNetworkInterfaces",
                              SYSTEM_NETWORK_GETNETWORKINTERFACES)
@@ -24,8 +24,8 @@ class SystemNetworkGetNetworkInterfacesFunction
  protected:
   ~SystemNetworkGetNetworkInterfacesFunction() override;
 
-  // AsyncApiFunction:
-  bool RunAsync() override;
+  // ExtensionFunction:
+  ResponseAction Run() override;
 
  private:
   void GetListOnFileThread();
