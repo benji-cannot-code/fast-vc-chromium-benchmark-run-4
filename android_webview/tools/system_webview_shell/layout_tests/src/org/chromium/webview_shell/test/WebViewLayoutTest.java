aecc0a061aadc3f5a957f83d645727f18faac389
jbudorick <jbudorick@chromium.org>
2017-01-20 15:09:19 -0800

    [android] Disable WebViewLayoutTest.testWebViewIncludedStableInterfaces.
    Broken by a blink layout test rebaseline.
    
    BUG=683153
    TBR=timvolodine@chromium.org
    
    Review-Url: https://codereview.chromium.org/2638403007
    Cr-Commit-Position: refs/heads/master@{#445199}

--- android_webview/tools/system_webview_shell/layout_tests/src/org/chromium/webview_shell/test/WebViewLayoutTest.java
+++ android_webview/tools/system_webview_shell/layout_tests/src/org/chromium/webview_shell/test/WebViewLayoutTest.java
@@ -12,6 +12,7 @@ import junit.framework.ComparisonFailure;
 
 import org.chromium.base.Log;
 import org.chromium.base.test.util.DisableIf;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.webview_shell.WebViewLayoutTestActivity;
 
@@ -157,6 +158,7 @@ public class WebViewLayoutTest
         assertEquals("Unexpected webview interfaces found", "", unexpected.toString());
     }
 
+    @DisabledTest(message = "crbug.com/683153")
     @MediumTest
     public void testWebViewIncludedStableInterfaces() throws Exception {
         ensureJsTestCopied();
