e4c3979bc94b92696d9827ff1c4acbfd9519bfde
tonikitoo <tonikitoo@igalia.com>
2017-01-20 13:29:41 -0800

    Add a KeyboardLayoutEngine::SetCurrentLayoutFromBuffer implementation
    Some servers (read Wayland here) do the XKB initialization and provide
    clients with a file descriptor that can be mmap'ed in order to retrieve
    the keyboard layout string.
    
    Today the method XkbKeyboardLayoutEngine::SetCurrentLayoutByName is
    (ab)used to support this case (where the client provides the keyboard
    layout string not the name), which is not ideal.
    
    CL adds a method that solely handles this case, cleaning up
    XkbKeyboardLayoutEngine::SetCurrentLayoutByName. The method will be used
    later on when the CL that adds keyboard/wayland support lands.
    
    BUG=578890
    
    Review-Url: https://codereview.chromium.org/2645703005
    Cr-Commit-Position: refs/heads/master@{#445171}

--- ui/events/ozone/layout/stub/stub_keyboard_layout_engine.cc
+++ ui/events/ozone/layout/stub/stub_keyboard_layout_engine.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/events/ozone/layout/stub/stub_keyboard_layout_engine.h"
 
-#include <algorithm>
-
 #include "base/strings/string16.h"
 #include "ui/events/keycodes/dom/dom_code.h"
 #include "ui/events/keycodes/dom/dom_key.h"
@@ -27,6 +25,12 @@ bool StubKeyboardLayoutEngine::CanSetCurrentLayout() const {
 
 bool StubKeyboardLayoutEngine::SetCurrentLayoutByName(
     const std::string& layout_name) {
+  return false;
+}
+
+bool StubKeyboardLayoutEngine::SetCurrentLayoutFromBuffer(
+    const char* keymap_str,
+    size_t size) {
   return false;
 }
 
