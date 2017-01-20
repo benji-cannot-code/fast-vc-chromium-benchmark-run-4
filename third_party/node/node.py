f6489d3175bd2348371fe89b829a8869b67007ba
dbeam <dbeam@chromium.org>
2017-01-20 10:08:17 -0800

    nodejs: Add light python wrappers for running node and friends
    Adds third_party/node/node.py which automagically selects or runs the
    right platform-specific node binary.
    
    Also adds third_party/node/node_modules.py which provides paths to
    specific node modules.
    
    BUG=673825
    R=dpapad@chromium.org
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2648693002
    Cr-Commit-Position: refs/heads/master@{#445088}

---
+++ third_party/node/node.py
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from os import path as os_path
+import platform
+
+
+def GetBinaryPath():
+  return os_path.join(os_path.dirname(__file__), *{
+    'Darwin': ('mac', 'node-darwin-x64', 'bin', 'node'),
+    'Linux': ('linux', 'node-linux-x64', 'bin', 'node'),
+    'Windows': ('win', 'node.exe'),
+  }[platform.system()])
