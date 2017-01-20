4034f4868fb42e437de2b68852a725ac186c05ce
ericwilligers <ericwilligers@chromium.org>
2017-01-19 23:08:02 -0800

    Implements CSSPropertyAPI for the -webkit-text-stroke-width property
    A part of Project Ribbon, separating the parsing logic for CSS
    properties from the parser into an API. This patch removes
    CSSPropertyWebkitTextStrokeWidth from the switch statement in
    parseSingleValue, and calls the API instead.
    
    A function pointer to the parseSingleValue function from the API for the
    -webkit-text-stroke-width property is stored in a CSSPropertyDescriptor,
    and is called from CSSPropertyParser.
    
    This patch:
    - Adds CSSPropertyAPIWebkitTextStrokeWidth.cpp to the BUILD.gn file.
    - Adds api_class flag to CSSProperties.in, which indicates that
      CSSPropertyAPIWebkitTextStrokeWidth.h is generated.
    - Moves the parsing logic for -webkit-text-stroke-width from
      CSSPropertyParser.cpp to CSSPropertyAPIWebkitTextStrokeWidth.cpp,
      which implements CSSPropertyAPI.h.
    
    BUG=668012
    
    Review-Url: https://codereview.chromium.org/2642203002
    Cr-Commit-Position: refs/heads/master@{#445005}

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParserHelpers.h
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParserHelpers.h
@@ -75,6 +75,8 @@ CSSValue* consumeColor(CSSParserTokenRange&,
                        CSSParserMode,
                        bool acceptQuirkyColors = false);
 
+CSSValue* consumeLineWidth(CSSParserTokenRange&, CSSParserMode, UnitlessQuirk);
+
 CSSValuePair* consumePosition(CSSParserTokenRange&,
                               CSSParserMode,
                               UnitlessQuirk);
