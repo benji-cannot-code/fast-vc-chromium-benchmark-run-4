b4ee2857006063f31bc548d82b0bd80c9c4d1a8d
lanwei <lanwei@chromium.org>
2017-01-20 06:46:07 -0800

    Replace mouse actions in pointer event tests with pointerActionSequence
    Replace mouseMove, mouseClick and mouseDrag in pointerevent_common_input.js
    with pointerActionSequence.
    
    BUG=680330
    
    Review-Url: https://codereview.chromium.org/2621353003
    Cr-Commit-Position: refs/heads/master@{#445052}

--- third_party/WebKit/LayoutTests/external/wpt_automation/pointerevents/pointerevent_touch-action-mouse-manual-automation.js
+++ third_party/WebKit/LayoutTests/external/wpt_automation/pointerevents/pointerevent_touch-action-mouse-manual-automation.js
@@ -2,10 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 importAutomationScript('/pointerevents/pointerevent_common_input.js');
 
 function inject_input() {
-  return mouseClickInTarget('#target0').then(function() {
-    return mouseWheelScroll('down');
-  }).then(function() {
-    return mouseWheelScroll('right');
+  return mouseWheelScroll('#target0', 'down').then(function() {
+    return mouseWheelScroll('#target0', 'right');
   });
 }
 
