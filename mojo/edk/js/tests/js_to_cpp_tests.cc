e34109e4576cced1f20a597b789a059f2191a15c
alokp <alokp@chromium.org>
2017-01-20 12:36:26 -0800

    Cleanup mojo-js tests.
    1. Moves unittests to mojo/public/js where rest of the unittests
       are located. Once we make the switch to WebIDL-based core
       bindings, these tests will run as layout tests.
    2. Deletes redundant mojo/edk/test directory
    3. Cleanup test targets
    
    BUG=647036
    
    Review-Url: https://codereview.chromium.org/2645873003
    Cr-Commit-Position: refs/heads/master@{#445149}

--- mojo/edk/js/tests/js_to_cpp_tests.cc
+++ mojo/edk/js/tests/js_to_cpp_tests.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/common/data_pipe_utils.h"
 #include "mojo/edk/js/mojo_runner_delegate.h"
 #include "mojo/edk/js/tests/js_to_cpp.mojom.h"
-#include "mojo/edk/test/test_utils.h"
 #include "mojo/public/cpp/bindings/binding.h"
 #include "mojo/public/cpp/bindings/lib/validation_errors.h"
 #include "mojo/public/cpp/system/core.h"
