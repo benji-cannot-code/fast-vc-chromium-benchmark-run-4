7263ca7911b475a443a131f3a0d7d4da0ca289ca
pkotwicz <pkotwicz@chromium.org>
2017-01-20 14:35:41 -0800

    Do not show "Please enable unsigned sources" dialog if WebAPK Google Play install is enabled
    BUG=683324
    
    Review-Url: https://codereview.chromium.org/2646173003
    Cr-Commit-Position: refs/heads/master@{#445188}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/ChromeWebApkHost.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/ChromeWebApkHost.java
@@ -97,6 +97,7 @@ public class ChromeWebApkHost {
         if (!ChromeVersionInfo.isCanaryBuild() && !ChromeVersionInfo.isDevBuild()) return;
 
         if (ChromeFeatureList.isEnabled(ChromeFeatureList.IMPROVED_A2HS)
+                && !canUseGooglePlayToInstallWebApk()
                 && !installingFromUnknownSourcesAllowed()) {
             showUnknownSourcesNeededDialog(context);
         }
