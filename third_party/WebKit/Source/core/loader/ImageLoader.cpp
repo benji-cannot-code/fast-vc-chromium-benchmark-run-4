80b2ddcdc5f4b1f1c8f3083e848f9f4ff19797ef
haraken <haraken@chromium.org>
2017-01-19 23:32:02 -0800

    Revert of Move ImageLoader timer to frame-specific TaskRunnerTimer. (patchset #2 id:20001 of https://codereview.chromium.org/2642103002/ )
    Reason for revert:
    To the best of my knowledge, I guess this CL is going to be a culprit of the memory leak:
    
    https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux%20Trusty%20Leak/builds/1645
    
    Let me revert this CL and see how it goes. I'll reland it if my guess is wrong.
    
    Original issue's description:
    > Move ImageLoader timer to frame-specific TaskRunnerTimer.
    >
    > Move ImageLoader timer m_derefElementTimer to frame-specific TaskRunnerTimer.
    > This associates it with the frame's Networking timer task queue.
    >
    > BUG=624694
    >
    > Review-Url: https://codereview.chromium.org/2642103002
    > Cr-Commit-Position: refs/heads/master@{#444927}
    > Committed: https://chromium.googlesource.com/chromium/src/+/ce1a9c0e454b08c96ca73788a1b4dccb405ce027
    
    TBR=gogag2@gmail.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=624694
    
    Review-Url: https://codereview.chromium.org/2644933003
    Cr-Commit-Position: refs/heads/master@{#445007}

--- third_party/WebKit/Source/core/loader/ImageLoader.cpp
+++ third_party/WebKit/Source/core/loader/ImageLoader.cpp
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/Document.h"
 #include "core/dom/Element.h"
 #include "core/dom/IncrementLoadEventDelayCount.h"
-#include "core/dom/TaskRunnerHelper.h"
 #include "core/events/Event.h"
 #include "core/events/EventSender.h"
 #include "core/fetch/FetchRequest.h"
@@ -157,10 +156,7 @@ class ImageLoader::Task {
 
 ImageLoader::ImageLoader(Element* element)
     : m_element(element),
-      m_derefElementTimer(TaskRunnerHelper::get(TaskType::Networking,
-                                                element->document().frame()),
-                          this,
-                          &ImageLoader::timerFired),
+      m_derefElementTimer(this, &ImageLoader::timerFired),
       m_hasPendingLoadEvent(false),
       m_hasPendingErrorEvent(false),
       m_imageComplete(true),
