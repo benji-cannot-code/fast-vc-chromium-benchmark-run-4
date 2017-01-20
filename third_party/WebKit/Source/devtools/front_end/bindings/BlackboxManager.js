a81b4388f649e08ad56bc1f24c81641dd836cd08
dgozman <dgozman@chromium.org>
2017-01-19 19:50:52 -0800

    [DevTools] Migrate CallStackSidebarPane to UI.ListControl.
    This pane is one of the two clients of UIList, which is going
    to be replaced by UI.ListControl.
    
    This patch also moves call stacks pane to shadow dom.
    
    BUG=chromium:630554
    
    Review-Url: https://codereview.chromium.org/2609653002
    Cr-Commit-Position: refs/heads/master@{#444971}

--- third_party/WebKit/Source/devtools/front_end/bindings/BlackboxManager.js
+++ third_party/WebKit/Source/devtools/front_end/bindings/BlackboxManager.js
@@ -20,6 +20,9 @@ Bindings.BlackboxManager = class {
     Common.moduleSetting('skipStackFramesPattern').addChangeListener(this._patternChanged.bind(this));
     Common.moduleSetting('skipContentScripts').addChangeListener(this._patternChanged.bind(this));
 
+    /** @type {!Set<function()>} */
+    this._listeners = new Set();
+
     /** @type {!Map<!SDK.DebuggerModel, !Map<string, !Array<!Protocol.Debugger.ScriptPosition>>>} */
     this._debuggerModelData = new Map();
     /** @type {!Map<string, boolean>} */
@@ -29,19 +32,17 @@ Bindings.BlackboxManager = class {
   }
 
   /**
-   * @param {function(!Common.Event)} listener
-   * @param {!Object=} thisObject
+   * @param {function()} listener
    */
-  addChangeListener(listener, thisObject) {
-    Common.moduleSetting('skipStackFramesPattern').addChangeListener(listener, thisObject);
+  addChangeListener(listener) {
+    this._listeners.add(listener);
   }
 
   /**
-   * @param {function(!Common.Event)} listener
-   * @param {!Object=} thisObject
+   * @param {function()} listener
    */
-  removeChangeListener(listener, thisObject) {
-    Common.moduleSetting('skipStackFramesPattern').removeChangeListener(listener, thisObject);
+  removeChangeListener(listener) {
+    this._listeners.delete(listener);
   }
 
   /**
@@ -286,7 +287,12 @@ Bindings.BlackboxManager = class {
         promises.push(this._addScript(script).then(loadSourceMap.bind(this, script)));
       }
     }
-    Promise.all(promises).then(this._patternChangeFinishedForTests.bind(this));
+    Promise.all(promises).then(() => {
+      var listeners = Array.from(this._listeners);
+      for (var listener of listeners)
+        listener();
+      this._patternChangeFinishedForTests();
+    });
 
     /**
      * @param {!SDK.Script} script
