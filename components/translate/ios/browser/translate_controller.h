ead8ad9eb05c8309207cef179bec03ddd7520782
stkhapugin <stkhapugin@chromium.org>
2017-01-20 07:34:45 -0800

    Fix testAutoTranslate downstream.
    Fixes testAutoTranslate by reintroducing the scoped_nsobject that
    was removed, hence modifying object lifetimes in a way that caused
    testAutoTranslate to fail downstream.
    
    CL that removed it: https://codereview.chromium.org/2645603002/
    
    BUG=None
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2642263004
    Cr-Commit-Position: refs/heads/master@{#445059}

--- components/translate/ios/browser/translate_controller.h
+++ components/translate/ios/browser/translate_controller.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/gtest_prod_util.h"
+#include "base/mac/scoped_nsobject.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "ios/web/public/web_state/web_state_observer.h"
@@ -68,9 +69,7 @@ class TranslateController : public web::WebStateObserver {
 
   // Changes the JsTranslateManager used by this TranslateController.
   // Only used for testing.
-  void SetJsTranslateManagerForTesting(JsTranslateManager* manager) {
-    js_manager_ = manager;
-  }
+  void SetJsTranslateManagerForTesting(JsTranslateManager* manager);
 
  private:
   FRIEND_TEST_ALL_PREFIXES(TranslateControllerTest,
@@ -95,7 +94,7 @@ class TranslateController : public web::WebStateObserver {
   void WebStateDestroyed() override;
 
   Observer* observer_;
-  JsTranslateManager* js_manager_;
+  base::scoped_nsobject<JsTranslateManager> js_manager_;
   base::WeakPtrFactory<TranslateController> weak_method_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(TranslateController);
--- components/translate/ios/browser/translate_controller.mm
+++ components/translate/ios/browser/translate_controller.mm
@@ -62,6 +62,11 @@ void TranslateController::CheckTranslateStatus() {
   [js_manager_ injectTranslateStatusScript];
 }
 
+void TranslateController::SetJsTranslateManagerForTesting(
+    JsTranslateManager* manager) {
+  js_manager_.reset(manager);
+}
+
 bool TranslateController::OnJavascriptCommandReceived(
     const base::DictionaryValue& command,
     const GURL& url,
