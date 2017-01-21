a85f63ac71f45227456766bc96904b9cf4d030c4
cco3 <cco3@chromium.org>
2017-01-20 17:46:41 -0800

    Suppress unchecked cast warnings
    In a couple places, we are doing unchecked casts of generics
    (no nice way around this).  This change suppresses the warnings
    they produce.
    
    Review-Url: https://codereview.chromium.org/2645973008
    Cr-Commit-Position: refs/heads/master@{#445246}

--- chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadManagerUi.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadManagerUi.java
@@ -180,6 +180,7 @@ public class DownloadManagerUi implements OnMenuItemClickListener, SearchDelegat
     private DownloadManagerToolbar mToolbar;
     private SelectableListLayout<DownloadHistoryItemWrapper> mSelectableListLayout;
 
+    @SuppressWarnings("unchecked")  // mSelectableListLayout
     public DownloadManagerUi(
             Activity activity, boolean isOffTheRecord, ComponentName parentComponent) {
         mActivity = activity;
