3b5a45b5835a837c228175a41b59394487157ff3
tedchoc <tedchoc@chromium.org>
2017-01-20 18:20:11 -0800

    Add workaround for the omnibox capture on Android.
    BUG=681877
    
    Review-Url: https://codereview.chromium.org/2650563003
    Cr-Commit-Position: refs/heads/master@{#445252}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/ViewResourceFrameLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/ViewResourceFrameLayout.java
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.widget;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Rect;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewParent;
@@ -59,6 +61,15 @@ public class ViewResourceFrameLayout extends FrameLayout {
      */
     protected boolean isReadyForCapture() {
         return true;
+    }
+
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        super.dispatchDraw(canvas);
+        // TODO(tedchoc): Switch to a better API when available. crbug.com/681877
+        if (Build.VERSION.CODENAME.equals("O") && isReadyForCapture()) {
+            mResourceAdapter.invalidate(null);
+        }
     }
 
     @Override
