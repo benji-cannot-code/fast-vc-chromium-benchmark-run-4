33bd16fa49fd1d288c05187689c15a01f43f21b2
kpaulhamus <kpaulhamus@chromium.org>
2017-01-20 17:24:24 -0800

    Add WebAuthn bindings and client interface.
    This CL adds the WebAuthn bindings and interface to support
    WebAuthentication.makeCredential and WebAuthentication.getAssertion.
    
    The draft spec is here: https://w3c.github.io/webauthn/
    
    The interface is likely to change as the spec matures. We're tracking the
    comments and suggestions from this review via issues opened with the working
    group:
    https://github.com/w3c/webauthn/issues/310
    https://github.com/w3c/webauthn/issues/311
    https://github.com/w3c/webauthn/issues/312
    https://github.com/w3c/webauthn/issues/313
    
    This patch also proactively implements the change to WebAuthnAttestation
    suggested here: https://github.com/w3c/webauthn/issues/244
    
    BUG=664630
    
    Review-Url: https://codereview.chromium.org/2533863002
    Cr-Commit-Position: refs/heads/master@{#445239}

---
+++ third_party/WebKit/Source/modules/webauth/WebAuthentication.cpp
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "modules/webauth/WebAuthentication.h"
+
+#include "bindings/core/v8/ScriptPromise.h"
+
+namespace blink {
+
+WebAuthentication::WebAuthentication(LocalFrame& frame) {}
+
+WebAuthentication::~WebAuthentication() {}
+
+void WebAuthentication::dispose() {}
+
+ScriptPromise WebAuthentication::makeCredential(
+    ScriptState* scriptState,
+    const RelyingPartyAccount& accountInformation,
+    const HeapVector<ScopedCredentialParameters> cryptoParameters,
+    const BufferSource& attestationChallenge,
+    ScopedCredentialOptions& options) {
+  NOTREACHED();
+  return ScriptPromise();
+}
+
+ScriptPromise WebAuthentication::getAssertion(
+    ScriptState* scriptState,
+    const BufferSource& assertionChallenge,
+    const AuthenticationAssertionOptions& options) {
+  NOTREACHED();
+  return ScriptPromise();
+}
+
+}  // namespace blink
