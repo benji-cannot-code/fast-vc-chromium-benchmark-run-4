5add26093b953442aab77e219308ca2cd17e1f04
qyearsley <qyearsley@chromium.org>
2017-01-20 16:19:11 -0800

    Add NOEXPORT=true commit message whenever import script is run.
    BUG=682340
    
    Review-Url: https://codereview.chromium.org/2639283005
    Cr-Commit-Position: refs/heads/master@{#445223}

--- third_party/WebKit/Tools/Scripts/webkitpy/w3c/deps_updater.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/w3c/deps_updater.py
@@ -225,7 +225,9 @@ class DepsUpdater(object):
         return return_code == 1
 
     def _commit_message(self, chromium_commit, import_commit):
-        return ('Import %s\n\nUsing update-w3c-deps in Chromium %s.\n\n' %
+        return ('Import %s\n\n'
+                'Using update-w3c-deps in Chromium %s.\n\n'
+                'NOEXPORT=true' %
                 (import_commit, chromium_commit))
 
     @staticmethod
@@ -337,6 +339,8 @@ class DepsUpdater(object):
         if build_link:
             description += 'Build: %s\n\n' % build_link
         description += 'TBR=qyearsley@chromium.org\n'
+        # Move any NOEXPORT tag to the end of the description.
+        description = description.replace('NOEXPORT=true', '')
         description += 'NOEXPORT=true'
         return description
 
