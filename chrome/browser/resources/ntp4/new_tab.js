ba12b0c4bc0d375c27986aae2dc8d58048ec9a5c
rune <rune@opera.com>
2017-01-21 01:40:45 -0800

    Initially load new_tab.css with a timestamp for chrome://apps.
    This is what incognito_tab.html already does. Changing the url to add a
    timestamp query on DOMContentLoaded caused a FoUC.
    
    BUG=681495
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2647653002
    Cr-Commit-Position: refs/heads/master@{#445285}

--- chrome/browser/resources/ntp4/new_tab.js
+++ chrome/browser/resources/ntp4/new_tab.js
@@ -91,9 +91,6 @@ cr.define('ntp', function() {
     }
     measureNavDots();
 
-    // Load the current theme colors.
-    themeChanged();
-
     newTabView = new NewTabView();
 
     if (!loadTimeData.getBoolean('showWebStoreIcon')) {
