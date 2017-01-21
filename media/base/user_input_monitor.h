10f5b2d6857883bbeaf73f8416eb7258e779dc8c
lethalantidote <lethalantidote@chromium.org>
2017-01-20 20:20:14 -0800

    Removes mouse listeners from UserInputMonitor.
    MouseEventListener was found to be unused, and thus removed.
    
    BUG=673494
    
    Review-Url: https://codereview.chromium.org/2577573002
    Cr-Commit-Position: refs/heads/master@{#445270}

--- media/base/user_input_monitor.h
+++ media/base/user_input_monitor.h
@@ -12,11 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
-#include "base/observer_list_threadsafe.h"
 #include "base/synchronization/lock.h"
 #include "media/base/media_export.h"
-
-struct SkIPoint;
 
 namespace base {
 class SingleThreadTaskRunner;
@@ -24,23 +21,10 @@ class SingleThreadTaskRunner;
 
 namespace media {
 
-// Monitors and notifies about mouse movements and keyboard events.
-// Thread safe. The listeners are called on the thread where the listeners are
-// added.
+// Monitors and notifies about keyboard events.
+// Thread safe.
 class MEDIA_EXPORT UserInputMonitor {
  public:
-  // The interface to receive mouse movement events.
-  class MEDIA_EXPORT MouseEventListener {
-   public:
-    // |position| is the new mouse position.
-    virtual void OnMouseMoved(const SkIPoint& position) = 0;
-
-   protected:
-    virtual ~MouseEventListener() {}
-  };
-  typedef base::ObserverListThreadSafe<UserInputMonitor::MouseEventListener>
-      MouseListenerList;
-
   UserInputMonitor();
   virtual ~UserInputMonitor();
 
@@ -50,12 +34,6 @@ class MEDIA_EXPORT UserInputMonitor {
   static std::unique_ptr<UserInputMonitor> Create(
       const scoped_refptr<base::SingleThreadTaskRunner>& io_task_runner,
       const scoped_refptr<base::SingleThreadTaskRunner>& ui_task_runner);
-
-  // The same |listener| should only be added once.
-  // The clients should make sure to call Remove*Listener before |listener| is
-  // destroyed.
-  void AddMouseListener(MouseEventListener* listener);
-  void RemoveMouseListener(MouseEventListener* listener);
 
   // A caller must call EnableKeyPressMonitoring and
   // DisableKeyPressMonitoring in pair.
@@ -70,21 +48,12 @@ class MEDIA_EXPORT UserInputMonitor {
   // any assumption on the initial value.
   virtual size_t GetKeyPressCount() const = 0;
 
- protected:
-  scoped_refptr<MouseListenerList> mouse_listeners() {
-    return mouse_listeners_;
-  }
-
  private:
   virtual void StartKeyboardMonitoring() = 0;
   virtual void StopKeyboardMonitoring() = 0;
-  virtual void StartMouseMonitoring() = 0;
-  virtual void StopMouseMonitoring() = 0;
 
   base::Lock lock_;
   size_t key_press_counter_references_;
-  size_t mouse_listeners_count_;
-  scoped_refptr<MouseListenerList> mouse_listeners_;
 
   DISALLOW_COPY_AND_ASSIGN(UserInputMonitor);
 };
--- media/base/user_input_monitor_unittest.cc
+++ media/base/user_input_monitor_unittest.cc
@@ -22,36 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
-class MockMouseListener : public UserInputMonitor::MouseEventListener {
- public:
-  MOCK_METHOD1(OnMouseMoved, void(const SkIPoint& position));
-
-  virtual ~MockMouseListener() {}
-};
-
-#if defined(OS_LINUX) || defined(OS_WIN)
-TEST(UserInputMonitorTest, KeyPressCounter) {
-  KeyboardEventCounter counter;
-
-  EXPECT_EQ(0u, counter.GetKeyPressCount());
-
-  counter.OnKeyboardEvent(ui::ET_KEY_PRESSED, ui::VKEY_0);
-  EXPECT_EQ(1u, counter.GetKeyPressCount());
-
-  // Holding the same key without releasing it does not increase the count.
-  counter.OnKeyboardEvent(ui::ET_KEY_PRESSED, ui::VKEY_0);
-  EXPECT_EQ(1u, counter.GetKeyPressCount());
-
-  // Releasing the key does not affect the total count.
-  counter.OnKeyboardEvent(ui::ET_KEY_RELEASED, ui::VKEY_0);
-  EXPECT_EQ(1u, counter.GetKeyPressCount());
-
-  counter.OnKeyboardEvent(ui::ET_KEY_PRESSED, ui::VKEY_0);
-  counter.OnKeyboardEvent(ui::ET_KEY_RELEASED, ui::VKEY_0);
-  EXPECT_EQ(2u, counter.GetKeyPressCount());
-}
-#endif  // defined(OS_LINUX) || defined(OS_WIN)
-
 TEST(UserInputMonitorTest, CreatePlatformSpecific) {
 #if defined(OS_LINUX)
   base::MessageLoopForIO message_loop;
@@ -70,11 +40,6 @@ TEST(UserInputMonitorTest, CreatePlatformSpecific) {
   MockMouseListener listener;
   // Ignore any callbacks.
   EXPECT_CALL(listener, OnMouseMoved(testing::_)).Times(testing::AnyNumber());
-
-#if !defined(OS_MACOSX)
-  monitor->AddMouseListener(&listener);
-  monitor->RemoveMouseListener(&listener);
-#endif  // !define(OS_MACOSX)
 
   monitor->EnableKeyPressMonitoring();
   monitor->DisableKeyPressMonitoring();
