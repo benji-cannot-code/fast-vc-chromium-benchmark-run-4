fddcfd4cb00e4752628a0309f0f2794607924607
divyanshi <divyanshi@google.com>
2017-01-22 21:40:08 -0800

    Implements CSSPropertyAPI for the CSSPropertyWebkitFontSizeDelta property.
    A part of Project Ribbon, separating the parsing logic
    for CSS properties from the parser into an API. This
    patch removes CSSPropertyWebkitFontSizeDelta from the
    switch statement in parseSingleValue, and calls the API
    instead.
    
    A function pointer to the parseSingleValue function from
    the API for the CSSPropertyWebkitFontSizeDelta property
    is stored in a CSSPropertyDescriptor, and is called from
    CSSPropertyParser.
    
    This patch:
    - Adds CSSPropertyWebkitFontSizeDelta.cpp to the BUILD.gn
      file.
    - Adds api_class flag to CSSProperties.in, which indicates
      that CSSPropertyWebkitFontSizeDelta.h is generated.
    - Moves the parsing logic for CSSPropertyWebkitFontSizeDelta
      from CSSPropertyParser.cpp to
      CSSPropertyWebkitFontSizeDelta.cpp, which implements
      CSSPropertyAPI.h.
    
    BUG=668012
    
    Review-Url: https://codereview.chromium.org/2647503003
    Cr-Commit-Position: refs/heads/master@{#445327}

---
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyAPIWebkitFontSizeDelta.cpp
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/properties/CSSPropertyAPIWebkitFontSizeDelta.h"
+
+#include "core/css/parser/CSSParserContext.h"
+#include "core/css/parser/CSSPropertyParserHelpers.h"
+
+namespace blink {
+
+const CSSValue* CSSPropertyAPIWebkitFontSizeDelta::parseSingleValue(
+    CSSParserTokenRange& range,
+    const CSSParserContext* context) {
+  return CSSPropertyParserHelpers::consumeLength(
+      range, context->mode(), ValueRangeAll,
+      CSSPropertyParserHelpers::UnitlessQuirk::Allow);
+}
+
+}  // namespace blink
