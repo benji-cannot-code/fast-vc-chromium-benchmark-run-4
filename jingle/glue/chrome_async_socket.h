510b397504404995b94d2bd9ca25039a04881714
deadbeef <deadbeef@chromium.org>
2017-01-23 03:05:25 -0800

    Remove unneeded #defines from libjingle_xmpp.
    Specifically, "FEATURE_ENABLE_SSL" and "FEATURE_ENABLE_VOICEMAIL".
    
    Compilation without these defines no longer works or is tested, so
    removing them should reduce some confusion.
    
    BUG=webrtc:7025
    
    Review-Url: https://codereview.chromium.org/2648063002
    Cr-Commit-Position: refs/heads/master@{#445351}

--- jingle/glue/chrome_async_socket.h
+++ jingle/glue/chrome_async_socket.h
@@ -8,10 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef JINGLE_GLUE_CHROME_ASYNC_SOCKET_H_
 #define JINGLE_GLUE_CHROME_ASYNC_SOCKET_H_
 
-#if !defined(FEATURE_ENABLE_SSL)
-#error ChromeAsyncSocket expects FEATURE_ENABLE_SSL to be defined
-#endif
-
 #include <stddef.h>
 
 #include <memory>
