cb1755f1aed1a815e290f407691a06cd89bdd7e5
arthursonzogni <arthursonzogni@chromium.org>
2017-01-23 10:37:47 -0800

    ContentSecurityPolicy: Fix bug when CSPSource host-part = "*"
    There were two problems. The first one was that when the host-part was
    "*" then CSPSource::IsSchemeOnly() returned true, even if there was a
    path or a port to enforce. For instance when source-expression =
    'http://*:111' then 'http://a.com:222' was allowed.
    The second problem was that in CSPSource::hostmatches(), the
    host-part == '*' case was not handled.
    
    This patch adds tests for these special cases.
    
    BUG=682673
    
    Review-Url: https://codereview.chromium.org/2646773002
    Cr-Commit-Position: refs/heads/master@{#445417}

--- third_party/WebKit/Source/core/frame/csp/CSPSource.cpp
+++ third_party/WebKit/Source/core/frame/csp/CSPSource.cpp
@@ -58,7 +58,13 @@ bool CSPSource::hostMatches(const String& host) const {
 
   bool equalHosts = m_host == host;
   if (m_hostWildcard == HasWildcard) {
-    match = host.endsWith(String("." + m_host), TextCaseUnicodeInsensitive);
+    if (m_host.isEmpty()) {
+      // host-part = "*"
+      match = true;
+    } else {
+      // host-part = "*." 1*host-char *( "." 1*host-char )
+      match = host.endsWith(String("." + m_host), TextCaseUnicodeInsensitive);
+    }
 
     // Chrome used to, incorrectly, match *.x.y to x.y. This was fixed, but
     // the following count measures when a match fails that would have
@@ -68,6 +74,7 @@ bool CSPSource::hostMatches(const String& host) const {
       UseCounter::count(*document,
                         UseCounter::CSPSourceWildcardWouldMatchExactHost);
   } else {
+    // host-part = 1*host-char *( "." 1*host-char )
     match = equalHosts;
   }
 
@@ -174,7 +181,7 @@ CSPSource* CSPSource::intersect(CSPSource* other) const {
 }
 
 bool CSPSource::isSchemeOnly() const {
-  return m_host.isEmpty();
+  return m_host.isEmpty() && (m_hostWildcard == NoWildcard);
 }
 
 bool CSPSource::firstSubsumesSecond(
