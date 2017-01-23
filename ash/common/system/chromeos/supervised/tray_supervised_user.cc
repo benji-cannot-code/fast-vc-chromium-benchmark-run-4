c00e18ae1953f87166b76940e4aa3eaefc3d7983
yiyix <yiyix@chromium.org>
2017-01-23 14:20:30 -0800

    Remove dead code related to Notification View Bubble
    BUG=630641
    
    Review-Url: https://codereview.chromium.org/2651723002
    Cr-Commit-Position: refs/heads/master@{#445512}

--- ash/common/system/chromeos/supervised/tray_supervised_user.cc
+++ ash/common/system/chromeos/supervised/tray_supervised_user.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/common/system/system_notifier.h"
 #include "ash/common/system/tray/label_tray_view.h"
 #include "ash/common/system/tray/system_tray_delegate.h"
-#include "ash/common/system/tray/tray_notification_view.h"
 #include "ash/common/wm_shell.h"
 #include "base/callback.h"
 #include "base/logging.h"
--- ash/system/chromeos/screen_layout_observer.cc
+++ ash/system/chromeos/screen_layout_observer.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/common/system/tray/system_tray_controller.h"
 #include "ash/common/system/tray/system_tray_delegate.h"
 #include "ash/common/system/tray/tray_constants.h"
-#include "ash/common/system/tray/tray_notification_view.h"
 #include "ash/common/wm_shell.h"
 #include "ash/display/screen_orientation_controller_chromeos.h"
 #include "ash/shell.h"
