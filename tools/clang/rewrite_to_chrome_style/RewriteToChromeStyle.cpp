d9556de12c61faf1f5b7dfe14a102cd86700ad1b
lukasza <lukasza@chromium.org>
2017-01-23 11:51:16 -0800

    Prepend "Get" prefix to methods named "type".
    This is needed to avoid conflicts like the one below:
    ../../third_party/WebKit/Source/web/WebDOMFileSystem.cpp:93:21:
    error: member 'Type' found in multiple base classes of different types
      switch (private_->Type()) {
                        ^
    ../../third_party/WebKit/Source/modules/filesystem/DOMFileSystemBase.h:88:18:
    note: member found by ambiguous name lookup
      FileSystemType Type() const { return type_; }
                     ^
    ../../third_party/WebKit/Source/core/dom/ContextLifecycleObserver.h:71:8:
    note: member found by ambiguous name lookup
      enum Type {
           ^
    
    BUG=582312
    
    Review-Url: https://codereview.chromium.org/2648833002
    Cr-Commit-Position: refs/heads/master@{#445442}

--- tools/clang/rewrite_to_chrome_style/RewriteToChromeStyle.cpp
+++ tools/clang/rewrite_to_chrome_style/RewriteToChromeStyle.cpp
@@ -688,6 +688,7 @@ bool ShouldPrefixFunctionName(const std::string& old_method_name) {
       "thread",
       "timing",
       "topLevelBlameContext",
+      "type",
       "vector",
       "visibleSelection",
       "webFrame",
