18831361593ca4a16273703fce367ed7e1d2aef0
skobes <skobes@chromium.org>
2017-01-23 10:08:33 -0800

    Remove some unused plumbing around AnimationHost::ScrollAnimationAbort.
    This removes test case "ScrollAnimatedFinishedByMainThreadScrollingReason",
    which didn't cover the codepath that is actually used.  We have equivalent
    coverage in ElementAnimationsTest.ImplThreadTakeoverAnimationGetsDeleted and
    LayerTreeHostAnimationTestScrollOffsetAnimationTakeover.
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2644423002
    Cr-Commit-Position: refs/heads/master@{#445407}

--- cc/trees/layer_tree_host_impl.h
+++ cc/trees/layer_tree_host_impl.h
@@ -677,8 +677,6 @@ class CC_EXPORT LayerTreeHostImpl
 
   void UpdateRootLayerStateForSynchronousInputHandler();
 
-  void ScrollAnimationAbort(LayerImpl* layer_impl);
-
   bool ScrollAnimationUpdateTarget(ScrollNode* scroll_node,
                                    const gfx::Vector2dF& scroll_delta,
                                    base::TimeDelta delayed_by);
--- cc/trees/mutator_host.h
+++ cc/trees/mutator_host.h
@@ -125,7 +125,7 @@ class MutatorHost {
       base::TimeTicks frame_monotonic_time,
       base::TimeDelta delayed_by) = 0;
 
-  virtual void ScrollAnimationAbort(bool needs_completion) = 0;
+  virtual void ScrollAnimationAbort() = 0;
 };
 
 class MutatorEvents {
