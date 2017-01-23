6109bfd9f74204b53fbbcf43db4647e2d29e1023
sdefresne <sdefresne@chromium.org>
2017-01-23 08:42:43 -0800

    Remove obsolete forward-declaration of CRWRequestTrackerDelegate protocol.
    BUG=585700
    
    Review-Url: https://codereview.chromium.org/2647313003
    Cr-Commit-Position: refs/heads/master@{#445387}

--- ios/web/web_state/web_state_impl.h
+++ ios/web/web_state/web_state_impl.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/web_state/web_state_delegate.h"
 #include "url/gurl.h"
 
-@protocol CRWRequestTrackerDelegate;
 @class CRWWebController;
 @protocol CRWWebViewProxy;
 @class NSURLRequest;
