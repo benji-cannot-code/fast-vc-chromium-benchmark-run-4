04d9e6c0db4ffa1d84acf505e483838e9d69f6fd
yzshen <yzshen@chromium.org>
2017-01-23 12:11:33 -0800

    Mojo C++ bindings: add a "deprecated" comment to type_converter.h
    BUG=621383
    
    Review-Url: https://codereview.chromium.org/2652783002
    Cr-Commit-Position: refs/heads/master@{#445455}

--- mojo/public/cpp/bindings/type_converter.h
+++ mojo/public/cpp/bindings/type_converter.h
@@ -12,6 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace mojo {
 
+// NOTE: TypeConverter is deprecated. Please consider StructTraits /
+// UnionTraits / EnumTraits / ArrayTraits / MapTraits / StringTraits if you
+// would like to convert between custom types and the wire format of mojom
+// types.
+//
 // Specialize the following class:
 //   template <typename T, typename U> struct TypeConverter;
 // to perform type conversion for Mojom-defined structs and arrays. Here, T is
