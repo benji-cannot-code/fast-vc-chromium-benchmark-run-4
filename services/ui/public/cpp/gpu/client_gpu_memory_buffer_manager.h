eeefd02b826849822cdb7734158182bbfd3754b0
rockot <rockot@chromium.org>
2017-01-23 11:37:43 -0800

    Fix potential shutdown deadlock in UI service
    If the GpuPtr is disconnected while there are pending buffer allocations
    in flight, this can lead to the main thread deadlocking on a WaitableEvent
    that never signals.
    
    Fixes that by tracking pending allocations and ensuring they're always
    signalled on connection error or other shutdown events.
    
    BUG=
    R=sadrul@chromium.org
    
    Review-Url: https://codereview.chromium.org/2651443002
    Cr-Commit-Position: refs/heads/master@{#445434}

--- services/ui/public/cpp/gpu/client_gpu_memory_buffer_manager.h
+++ services/ui/public/cpp/gpu/client_gpu_memory_buffer_manager.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define SERVICES_UI_PUBLIC_CPP_GPU_CLIENT_GPU_MEMORY_BUFFER_MANAGER_H_
 
 #include <memory>
+#include <set>
 #include <vector>
 
 #include "base/macros.h"
@@ -30,11 +31,16 @@ class ClientGpuMemoryBufferManager : public gpu::GpuMemoryBufferManager {
  private:
   void InitThread(mojom::GpuPtrInfo gpu_info);
   void TearDownThread();
+  void DisconnectGpuOnThread();
   void AllocateGpuMemoryBufferOnThread(const gfx::Size& size,
                                        gfx::BufferFormat format,
                                        gfx::BufferUsage usage,
                                        gfx::GpuMemoryBufferHandle* handle,
                                        base::WaitableEvent* wait);
+  void OnGpuMemoryBufferAllocatedOnThread(
+      gfx::GpuMemoryBufferHandle* ret_handle,
+      base::WaitableEvent* wait,
+      const gfx::GpuMemoryBufferHandle& handle);
   void DeletedGpuMemoryBuffer(gfx::GpuMemoryBufferId id,
                               const gpu::SyncToken& sync_token);
 
@@ -46,11 +52,13 @@ class ClientGpuMemoryBufferManager : public gpu::GpuMemoryBufferManager {
       gpu::SurfaceHandle surface_handle) override;
   void SetDestructionSyncToken(gfx::GpuMemoryBuffer* buffer,
                                const gpu::SyncToken& sync_token) override;
+
   int counter_ = 0;
   // TODO(sad): Explore the option of doing this from an existing thread.
   base::Thread thread_;
   mojom::GpuPtr gpu_;
   base::WeakPtr<ClientGpuMemoryBufferManager> weak_ptr_;
+  std::set<base::WaitableEvent*> pending_allocation_waiters_;
   base::WeakPtrFactory<ClientGpuMemoryBufferManager> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(ClientGpuMemoryBufferManager);
