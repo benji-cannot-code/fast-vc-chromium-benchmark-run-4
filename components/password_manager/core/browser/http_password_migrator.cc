eaf2ab4e4e468372885d8753fc7fd6f5c3946ed6
vasilii <vasilii@chromium.org>
2017-01-23 04:00:08 -0800

    Add a histogram for a number of HTTP passwords migrated to HTTPS.
    BUG=571580
    
    Review-Url: https://codereview.chromium.org/2648783002
    Cr-Commit-Position: refs/heads/master@{#445360}

--- components/password_manager/core/browser/http_password_migrator.cc
+++ components/password_manager/core/browser/http_password_migrator.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/password_manager/core/browser/http_password_migrator.h"
 
+#include "components/password_manager/core/browser/password_manager_metrics_util.h"
 #include "components/password_manager/core/browser/password_store.h"
 #include "url/gurl.h"
 #include "url/url_constants.h"
@@ -53,6 +54,8 @@ void HttpPasswordMigrator::OnGetPasswordStoreResults(
     form->skip_zero_click = false;
     password_store_->AddLogin(*form);
   }
+
+  metrics_util::LogCountHttpMigratedPasswords(results.size());
 
   if (consumer_)
     consumer_->ProcessMigratedForms(std::move(results));
