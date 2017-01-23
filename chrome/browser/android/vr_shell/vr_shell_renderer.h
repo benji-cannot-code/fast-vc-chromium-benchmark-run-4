6c62d3f78b16f49d753f1d8534d56b8b0851c85a
tiborg <tiborg@google.com>
2017-01-23 13:55:37 -0800

    Draws white ground grid into the VR Shell.
    - Adds BackgroundRenderer, which is responsible for drawing the VR Shell background.
    - Background renderer is inserted into the render chain.
    - Background renderer creates a grid and draws it.
    
    Review-Url: https://codereview.chromium.org/2640153005
    Cr-Commit-Position: refs/heads/master@{#445501}

--- chrome/browser/android/vr_shell/vr_shell_renderer.h
+++ chrome/browser/android/vr_shell/vr_shell_renderer.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_ANDROID_VR_SHELL_VR_SHELL_RENDERER_H_
 
 #include <memory>
+#include <vector>
 
 #include "base/macros.h"
 #include "chrome/browser/android/vr_shell/vr_math.h"
@@ -27,7 +28,20 @@ enum ShaderID {
   RETICLE_FRAGMENT_SHADER,
   LASER_VERTEX_SHADER,
   LASER_FRAGMENT_SHADER,
+  BACKGROUND_VERTEX_SHADER,
+  BACKGROUND_FRAGMENT_SHADER,
   SHADER_ID_MAX
+};
+
+struct Vertex3d {
+  float x;
+  float y;
+  float z;
+};
+
+struct Line3d {
+  Vertex3d start;
+  Vertex3d end;
 };
 
 class BaseRenderer {
@@ -122,6 +136,21 @@ class LaserRenderer : public BaseRenderer {
   DISALLOW_COPY_AND_ASSIGN(LaserRenderer);
 };
 
+class BackgroundRenderer : public BaseRenderer {
+ public:
+  BackgroundRenderer();
+  ~BackgroundRenderer() override;
+
+  void Draw(const gvr::Mat4f& combined_matrix);
+
+ private:
+  GLuint combined_matrix_handle_;
+  GLuint grid_size_handle_;
+  std::vector<Line3d>  ground_grid_lines_;
+
+  DISALLOW_COPY_AND_ASSIGN(BackgroundRenderer);
+};
+
 class VrShellRenderer {
  public:
   VrShellRenderer();
@@ -143,11 +172,16 @@ class VrShellRenderer {
     return laser_renderer_.get();
   }
 
+  BackgroundRenderer* GetBackgroundRenderer() {
+    return background_renderer_.get();
+  }
+
  private:
   std::unique_ptr<TexturedQuadRenderer> textured_quad_renderer_;
   std::unique_ptr<WebVrRenderer> webvr_renderer_;
   std::unique_ptr<ReticleRenderer> reticle_renderer_;
   std::unique_ptr<LaserRenderer> laser_renderer_;
+  std::unique_ptr<BackgroundRenderer> background_renderer_;
 
   DISALLOW_COPY_AND_ASSIGN(VrShellRenderer);
 };
