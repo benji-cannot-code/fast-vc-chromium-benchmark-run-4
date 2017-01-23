a926ec0fe9ebc578360524afc799b04fbcbd342d
sky <sky@chromium.org>
2017-01-23 12:35:25 -0800

    ash: removes unnecessary includes and dep
    BUG=670798
    TEST=none
    R=jamescook@chromium.org
    
    Review-Url: https://codereview.chromium.org/2652763002
    Cr-Commit-Position: refs/heads/master@{#445470}

--- ash/common/cast_config_controller.cc
+++ ash/common/cast_config_controller.cc
@@ -8,10 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
-#include "base/logging.h"
-#include "content/public/common/service_names.mojom.h"
-#include "services/service_manager/public/cpp/connector.h"
-
 namespace ash {
 
 CastConfigController::CastConfigController() {}
