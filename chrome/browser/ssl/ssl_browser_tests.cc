9e969a84ae02eac49ea196880b01c9feeef38226
estark <estark@chromium.org>
2017-01-23 17:52:28 -0800

    Update NetworkTimeQueries field trial config
    Also, this updates the SSLErrorHandler browser tests to use their own
    NetworkTimeTracker, instead of the global one. The global one can be
    influenced by the field trial config (e.g. might perform a query on startup
    before the test has a chance to disable background queries). So the tests
    must use their own NetworkTimeTracker to ensure that it behaves as
    desired.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2653443002
    Cr-Commit-Position: refs/heads/master@{#445589}

--- chrome/browser/ssl/ssl_browser_tests.cc
+++ chrome/browser/ssl/ssl_browser_tests.cc
@@ -62,6 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/security_state/core/switches.h"
 #include "components/ssl_errors/error_classification.h"
 #include "components/variations/variations_associated_data.h"
+#include "components/variations/variations_switches.h"
 #include "components/web_modal/web_contents_modal_dialog_manager.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/interstitial_page.h"
@@ -102,6 +103,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_context.h"
 #include "net/url_request/url_request_filter.h"
 #include "net/url_request/url_request_job.h"
+#include "net/url_request/url_request_test_util.h"
 
 #if defined(USE_NSS_CERTS)
 #include "chrome/browser/net/nss_context.h"
@@ -3009,11 +3011,22 @@ void CleanUpOnIOThread() {
 // request to be issued during the test.
 class SSLNetworkTimeBrowserTest : public SSLUITest {
  public:
-  SSLNetworkTimeBrowserTest()
-      : SSLUITest(),
-        field_trial_test_(network_time::FieldTrialTest::CreateForBrowserTest()),
-        interceptor_(nullptr) {}
+  SSLNetworkTimeBrowserTest() : SSLUITest(), interceptor_(nullptr) {}
   ~SSLNetworkTimeBrowserTest() override {}
+
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    command_line->AppendSwitchASCII(
+        switches::kEnableFeatures,
+        std::string(network_time::kNetworkTimeServiceQuerying.name) +
+            "<SSLNetworkTimeBrowserTestFieldTrial");
+    command_line->AppendSwitchASCII(
+        switches::kForceFieldTrials,
+        "SSLNetworkTimeBrowserTestFieldTrial/Enabled/");
+    command_line->AppendSwitchASCII(
+        variations::switches::kForceFieldTrialParams,
+        "SSLNetworkTimeBrowserTestFieldTrial.Enabled:FetchBehavior/"
+        "on-demand-only");
+  }
 
   void SetUpOnMainThread() override { SetUpNetworkTimeServer(); }
 
@@ -3023,16 +3036,8 @@ class SSLNetworkTimeBrowserTest : public SSLUITest {
   }
 
  protected:
-  network_time::FieldTrialTest* field_trial_test() const {
-    return field_trial_test_.get();
-  }
-
   void SetUpNetworkTimeServer() {
-    field_trial_test()->SetNetworkQueriesWithVariationsService(
-        true, 0.0, network_time::NetworkTimeTracker::FETCHES_ON_DEMAND_ONLY);
-
-    // Install the URL interceptor that serves delayed network time
-    // responses.
+    // Install the URL interceptor that serves delayed network time responses.
     interceptor_ = new DelayedNetworkTimeInterceptor();
     content::BrowserThread::PostTask(
         content::BrowserThread::IO, FROM_HERE,
@@ -3059,7 +3064,6 @@ class SSLNetworkTimeBrowserTest : public SSLUITest {
   }
 
  private:
-  std::unique_ptr<network_time::FieldTrialTest> field_trial_test_;
   DelayedNetworkTimeInterceptor* interceptor_;
 
   DISALLOW_COPY_AND_ASSIGN(SSLNetworkTimeBrowserTest);
