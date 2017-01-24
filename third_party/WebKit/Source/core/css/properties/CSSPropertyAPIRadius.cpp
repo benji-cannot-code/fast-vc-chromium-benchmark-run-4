794897857326446333b436c0b6621ca3cbd45660
gozzard <gozzard@google.com>
2017-01-23 19:52:56 -0800

    Implements CSSPropertyAPI for the radius properties.
    A part of Project Ribbon, separating the parsing logic for CSS
    properties from the parser into an API. This patch removes
    CSSPropertyRx and CSSPropertyRy from the switch statement in
    parseSingleValue, and calls the API instead.
    
    A function pointer to the parseSingleValue function from the API for the
    border radius properties is stored in a CSSPropertyDescriptor, and is
    called from CSSPropertyParser.
    
    This patch:
    - Adds CSSPropertyAPIRadius.cpp to the BUILD.gn file.
    - Adds api_class=CSSPropertyAPIRadius flag to each radius class
      property in CSSProperties.in, which indicates that
      CSSPropertyAPIRadius.h is generated.
    - Moves the parsing logic for radius from CSSPropertyParser.cpp
      to CSSPropertyAPIRadius.cpp, which implements CSSPropertyAPI.h.
    
    BUG=668012
    
    Review-Url: https://codereview.chromium.org/2642853005
    Cr-Commit-Position: refs/heads/master@{#445634}

---
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyAPIRadius.cpp
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/properties/CSSPropertyAPIRadius.h"
+
+#include "core/css/parser/CSSPropertyParserHelpers.h"
+
+namespace blink {
+
+const CSSValue* CSSPropertyAPIRadius::parseSingleValue(
+    CSSParserTokenRange& range,
+    const CSSParserContext* context) {
+  if (range.peek().id() == CSSValueAuto)
+    return CSSPropertyParserHelpers::consumeIdent(range);
+  return CSSPropertyParserHelpers::consumeLengthOrPercent(
+      range, SVGAttributeMode, ValueRangeAll,
+      CSSPropertyParserHelpers::UnitlessQuirk::Forbid);
+}
+
+}  // namespace blink
