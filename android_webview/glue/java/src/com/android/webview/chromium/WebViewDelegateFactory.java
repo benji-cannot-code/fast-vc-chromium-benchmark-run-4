dfa0fa5a8c8ce4fe80042653f891e39465ac24e5
torne <torne@chromium.org>
2017-01-24 03:59:34 -0800

    Prepare to get the multiprocess setting from WebViewDelegate.
    In future OS versions, we should fetch the multiprocess-enabled setting
    from WebViewDelegate instead of querying the setting directly. Add the
    skeleton code to get this from the delegate under a build version
    condition.
    
    BUG=684489
    
    Review-Url: https://codereview.chromium.org/2645413002
    Cr-Commit-Position: refs/heads/master@{#445705}

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewDelegateFactory.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewDelegateFactory.java
@@ -83,6 +83,9 @@ class WebViewDelegateFactory {
 
         /** @see android.webkit.WebViewDelegate#addWebViewAssetPath */
         void addWebViewAssetPath(Context context);
+
+        /** @see android.webkit.WebViewDelegate#isMultiProcessEnabled */
+        boolean isMultiProcessEnabled();
     }
 
     /**
@@ -111,7 +114,7 @@ class WebViewDelegateFactory {
      * A {@link WebViewDelegate com.android.webview.chromium.WebViewDelegate} that proxies requests
      * to a {@link android.webkit.WebViewDelegate android.webkit.WebViewDelegate}.
      */
-    private static class ProxyDelegate implements WebViewDelegate {
+    static class ProxyDelegate implements WebViewDelegate {
         android.webkit.WebViewDelegate mDelegate;
 
         ProxyDelegate(android.webkit.WebViewDelegate delegate) {
@@ -198,6 +201,11 @@ class WebViewDelegateFactory {
                     return getResources().getAssets();
                 }
             });
+        }
+
+        @Override
+        public boolean isMultiProcessEnabled() {
+            throw new UnsupportedOperationException();
         }
     }
 
@@ -383,6 +391,11 @@ class WebViewDelegateFactory {
             } catch (Exception e) {
                 throw new RuntimeException("Invalid reflection", e);
             }
+        }
+
+        @Override
+        public boolean isMultiProcessEnabled() {
+            throw new UnsupportedOperationException();
         }
     }
 }
