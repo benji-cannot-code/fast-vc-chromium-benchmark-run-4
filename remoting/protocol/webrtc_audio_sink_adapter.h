2cd5b781d77551de16db912f15a433345ac1e488
sergeyu <sergeyu@chromium.org>
2017-01-23 23:11:01 -0800

    Fix threading issues in the audio pipeline for WebRTC remoting protocol.
    1. Updated unittests to use a separate thread for audio encoding and
       decoding.
    2. WebrtcAudioSinkAdapter was calling AudioStub on a wrong thread.
    3. WebrtcAudioModule was destroying base::Timer on a thread different
       from the one on which it is created.
    
    Review-Url: https://codereview.chromium.org/2650633003
    Cr-Original-Commit-Position: refs/heads/master@{#445566}
    Committed: https://chromium.googlesource.com/chromium/src/+/25680c62320767f590d037d301edfe15e9c55650
    Review-Url: https://codereview.chromium.org/2650633003
    Cr-Commit-Position: refs/heads/master@{#445672}

--- remoting/protocol/webrtc_audio_sink_adapter.h
+++ remoting/protocol/webrtc_audio_sink_adapter.h
@@ -8,7 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "third_party/webrtc/api/mediastreaminterface.h"
+
+namespace base {
+class SingleThreadTaskRunner;
+}  // namespace base
 
 namespace remoting {
 namespace protocol {
@@ -28,10 +33,10 @@ class WebrtcAudioSinkAdapter : public webrtc::AudioTrackSinkInterface {
               size_t number_of_frames) override;
 
  private:
+  scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
+  base::WeakPtr<AudioStub> audio_stub_;
   scoped_refptr<webrtc::MediaStreamInterface> media_stream_;
   scoped_refptr<webrtc::AudioTrackInterface> audio_track_;
-
-  base::WeakPtr<AudioStub> audio_stub_;
 };
 
 }  // namespace protocol
