b35d35a86dc84803b5041e234e786f09461043e1
Sergey Ulanov <sergeyu@chromium.org>
2017-01-23 17:03:28 -0800

    Include thread_checker.h in connection_unittests.cc
    TBR=rjwright@chromium.org
    
    Review-Url: https://codereview.chromium.org/2653853002 .
    Cr-Commit-Position: refs/heads/master@{#445572}

--- remoting/protocol/connection_unittest.cc
+++ remoting/protocol/connection_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/threading/thread.h"
+#include "base/threading/thread_checker.h"
 #include "remoting/base/constants.h"
 #include "remoting/proto/audio.pb.h"
 #include "remoting/protocol/audio_source.h"
