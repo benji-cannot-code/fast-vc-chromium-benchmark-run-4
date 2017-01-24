db88776629f86f7e9192767f5ebebcb3b80b0c2c
gogerald <gogerald@chromium.org>
2017-01-24 14:29:24 -0800

    Rename function to avoid unintentional override
    isValid in AutofillPaymentInstrument overrides isValid in PaymentOption unintentionally,
    
    BUG=684613
    
    Review-Url: https://codereview.chromium.org/2652063002
    Cr-Commit-Position: refs/heads/master@{#445842}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentRequestImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentRequestImpl.java
@@ -1233,7 +1233,7 @@ public class PaymentRequestImpl
 
             // If there's a card on file with a valid number and a name, then
             // PaymentRequest.canMakePayment() returns true.
-            mCanMakePayment |= creditCard.isValid();
+            mCanMakePayment |= creditCard.isValidCard();
         }
 
         // List order:
