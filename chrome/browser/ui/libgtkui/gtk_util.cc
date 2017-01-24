abc0c8159ca103b093ebb4e031943aa36d4a8164
thomasanderson <thomasanderson@google.com>
2017-01-24 13:24:42 -0800

    Gtk3: Render a GtkHeaderBar as the background of the tab strip
    This CL changes the Gtk3 theme to render a GtkHeaderBar background behind the
    tab strip.  This should give a more native look-and-feel for Linux UI.  A
    demonstration of the change can be seen in:
    https://bugs.chromium.org/p/chromium/issues/detail?id=132847#c42
    
    BUG=132847,590301
    R=pkasting@chromium.org,erg@chromium.org,sky@chromium.org
    
    Review-Url: https://codereview.chromium.org/2628043002
    Cr-Commit-Position: refs/heads/master@{#445818}

--- chrome/browser/ui/libgtkui/gtk_util.cc
+++ chrome/browser/ui/libgtkui/gtk_util.cc
@@ -66,6 +66,12 @@ void* GetGtk3SharedLibrary() {
 
 namespace libgtkui {
 
+// TODO(erg): ThemeService has a whole interface just for reading default
+// constants. Figure out what to do with that more long term; for now, just
+// copy the constants themselves here.
+const color_utils::HSL kDefaultTintFrameIncognito = {-1, 0.2f, 0.35f};
+const color_utils::HSL kDefaultTintFrameIncognitoInactive = {-1, 0.3f, 0.6f};
+
 // Theme colors returned by GetSystemColor().
 const SkColor kInvalidColorIdColor = SkColorSetRGB(255, 0, 128);
 const SkColor kURLTextColor = SkColorSetRGB(0x0b, 0x80, 0x43);
@@ -422,11 +428,13 @@ class PixelSurface {
   cairo_t* cairo_;
 };
 
-void RenderBackground(cairo_t* cr, GtkStyleContext* context) {
+void RenderBackground(const gfx::Size& size,
+                      cairo_t* cr,
+                      GtkStyleContext* context) {
   if (!context)
     return;
-  RenderBackground(cr, gtk_style_context_get_parent(context));
-  gtk_render_background(context, cr, 0, 0, 1, 1);
+  RenderBackground(size, cr, gtk_style_context_get_parent(context));
+  gtk_render_background(context, cr, 0, 0, size.width(), size.height());
 }
 
 SkColor GetBgColor(const char* css_selector) {
@@ -439,7 +447,7 @@ SkColor GetBgColor(const char* css_selector) {
   auto context = GetStyleContextFromCss(css_selector);
   RemoveBorders(context);
   PixelSurface surface;
-  RenderBackground(surface.cairo(), context);
+  RenderBackground(gfx::Size(1, 1), surface.cairo(), context);
   return surface.GetPixelValue();
 }
 
@@ -461,7 +469,7 @@ SkColor GetBorderColor(const char* css_selector) {
 
   AddBorders(context);
   PixelSurface surface;
-  RenderBackground(surface.cairo(), context);
+  RenderBackground(gfx::Size(1, 1), surface.cairo(), context);
   gtk_render_frame(context, surface.cairo(), 0, 0, 1, 1);
   return surface.GetPixelValue();
 }
