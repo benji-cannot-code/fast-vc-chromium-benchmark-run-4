27a4d5cee3b168abd7e1ada80de585db257876d0
ntfschr <ntfschr@chromium.org>
2017-01-24 11:12:18 -0800

    Enable SafeBrowsing by default on O
    SafeBrowsing no longer needs a commandline flag to be enabled in O. It
    will be enabled by default if the verify-apps setting is enabled.
    
    BUG=684117
    
    Review-Url: https://codereview.chromium.org/2654693002
    Cr-Commit-Position: refs/heads/master@{#445773}

--- android_webview/java/src/org/chromium/android_webview/AwBrowserContext.java
+++ android_webview/java/src/org/chromium/android_webview/AwBrowserContext.java
@@ -8,7 +8,6 @@ package org.chromium.android_webview;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import org.chromium.base.CommandLine;
 import org.chromium.components.safe_browsing.SafeBrowsingApiBridge;
 import org.chromium.components.safe_browsing.SafeBrowsingApiHandler;
 import org.chromium.content.browser.AppWebMessagePortService;
@@ -36,7 +35,7 @@ public class AwBrowserContext {
         mSharedPreferences = sharedPreferences;
         mApplicationContext = applicationContext;
 
-        if (CommandLine.getInstance().hasSwitch(AwSwitches.WEBVIEW_ENABLE_SAFEBROWSING_SUPPORT)) {
+        if (AwContentsStatics.getSafeBrowsingEnabled()) {
             initSafeBrowsingApiHandler();
         }
     }
--- android_webview/java/src/org/chromium/android_webview/AwContentsStatics.java
+++ android_webview/java/src/org/chromium/android_webview/AwContentsStatics.java
@@ -80,6 +80,10 @@ public class AwContentsStatics {
         nativeSetServiceWorkerIoThreadClient(ioThreadClient, browserContext);
     }
 
+    public static boolean getSafeBrowsingEnabled() {
+        return nativeGetSafeBrowsingEnabled();
+    }
+
     public static void setSafeBrowsingEnabled(boolean enable) {
         nativeSetSafeBrowsingEnabled(enable);
     }
@@ -93,5 +97,6 @@ public class AwContentsStatics {
     private static native String nativeGetProductVersion();
     private static native void nativeSetServiceWorkerIoThreadClient(
             AwContentsIoThreadClient ioThreadClient, AwBrowserContext browserContext);
+    private static native boolean nativeGetSafeBrowsingEnabled();
     private static native void nativeSetSafeBrowsingEnabled(boolean enable);
 }
