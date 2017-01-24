d130bbc83168ef085e794bbf7d94fe1696e6bf50
gozzard <gozzard@google.com>
2017-01-23 19:28:01 -0800

    Implements CSSPropertyAPI for the order property.
    A part of Project Ribbon, separating the parsing logic for CSS
    properties from the parser into an API. This patch removes
    CSSPropertyOrder from the switch statement in parseSingleValue,
    and calls the API instead.
    
    A function pointer to the parseSingleValue function from the API for the
    order property is stored in a CSSPropertyDescriptor, and is called from
    CSSPropertyParser.
    
    This patch:
    - Adds CSSPropertyAPIOrder.cpp to the BUILD.gn file.
    - Adds api_class flag to CSSProperties.in, which indicates that
      CSSPropertyAPIOrder.h is generated.
    - Moves the parsing logic for page from CSSPropertyParser.cpp to
      CSSPropertyAPIOrder.cpp, which implements
      CSSPropertyAPI.h.
    
    BUG=668012
    
    Review-Url: https://codereview.chromium.org/2642993002
    Cr-Commit-Position: refs/heads/master@{#445627}

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
@@ -2377,8 +2377,6 @@ const CSSValue* CSSPropertyParser::parseSingleValue(
     case CSSPropertyScrollSnapPointsX:
     case CSSPropertyScrollSnapPointsY:
       return consumeScrollSnapPoints(m_range, m_context->mode());
-    case CSSPropertyOrder:
-      return consumeInteger(m_range);
     case CSSPropertyJustifyContent:
     case CSSPropertyAlignContent:
       ASSERT(RuntimeEnabledFeatures::cssGridLayoutEnabled());
