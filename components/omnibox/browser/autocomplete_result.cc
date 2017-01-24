692b463df541009591e082e2e7235077f80b0aa0
treib <treib@chromium.org>
2017-01-24 01:03:21 -0800

    Cleanup: Remove some unnecessary includes of components/search/search.h
    BUG=627747
    
    Review-Url: https://codereview.chromium.org/2651633003
    Cr-Commit-Position: refs/heads/master@{#445686}

--- components/omnibox/browser/autocomplete_result.cc
+++ components/omnibox/browser/autocomplete_result.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/match_compare.h"
 #include "components/omnibox/browser/omnibox_field_trial.h"
 #include "components/omnibox/browser/omnibox_switches.h"
-#include "components/search/search.h"
 #include "components/url_formatter/url_fixer.h"
 
 // static
