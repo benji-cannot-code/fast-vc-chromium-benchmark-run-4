67be6cde4f57c48825af3b73077a1227cf95d0d8
karandeepb <karandeepb@chromium.org>
2017-01-24 01:12:25 -0800

    MacViews: Enable views based External Protocol dialog behind secondary-ui-md flag.
    This CL puts the views based External Protocol dialog behind the secondary-ui-md
    flag. As a result, the views based version is shown with the flag "secondary-ui-
    md" enabled and the Cocoa version is shown without it.
    
    BUG=662128, 652508
    
    Review-Url: https://codereview.chromium.org/2632653002
    Cr-Commit-Position: refs/heads/master@{#445688}

--- chrome/browser/ui/cocoa/external_protocol_dialog_cocoa.mm
+++ chrome/browser/ui/cocoa/external_protocol_dialog_cocoa.mm
@@ -17,18 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/text_elider.h"
 
 ///////////////////////////////////////////////////////////////////////////////
-// ExternalProtocolHandler
-
-// static
-void ExternalProtocolHandler::RunExternalProtocolDialog(
-    const GURL& url, int render_process_host_id, int routing_id,
-    ui::PageTransition page_transition, bool has_user_gesture) {
-  [[ExternalProtocolDialogController alloc] initWithGURL:&url
-                                     renderProcessHostId:render_process_host_id
-                                               routingId:routing_id];
-}
-
-///////////////////////////////////////////////////////////////////////////////
 // ExternalProtocolDialogController
 
 @interface ExternalProtocolDialogController(Private)
