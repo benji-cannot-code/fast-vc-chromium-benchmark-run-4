91c4e87d00975dfa717e621d9490507f3585f68a
shenghuazhang <shenghuazhang@chromium.org>
2017-01-23 20:33:51 -0800

    [Android] Add ability to generate trace html for instrumentation test.
    This adds the ability for instrumentation test to generate a json html
    file which can be open with the trace viewer (chrome://tracing) that
    describes what tests are running where and timelines.
    
    Add trace to SetUp and TearDown for instrumentation test, gtests and perf
    tests.
    
    BUG=568352
    
    Review-Url: https://codereview.chromium.org/2649463002
    Cr-Commit-Position: refs/heads/master@{#445645}

--- build/android/pylib/local/device/local_device_environment.py
+++ build/android/pylib/local/device/local_device_environment.py
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import contextlib
 import datetime
 import functools
 import logging
@@ -98,7 +97,8 @@ class LocalDeviceEnvironment(environment.Environment):
 
   #override
   def SetUp(self):
-    pass
+    if self.trace_output:
+      self.EnableTracing()
 
   def _InitDevices(self):
     device_arg = 'default'
@@ -242,6 +242,9 @@ class LocalDeviceEnvironment(environment.Environment):
            if os.path.exists(m.output_file)])
       shutil.rmtree(self._logcat_output_dir)
 
+    if self.trace_output:
+      self.DisableTracing()
+
   def BlacklistDevice(self, device, reason='local_device_failure'):
     device_serial = device.adb.GetDeviceSerial()
     if self._blacklist:
@@ -262,11 +265,3 @@ class LocalDeviceEnvironment(environment.Environment):
       logging.warning('Tracing is already running.')
     else:
       trace_event.trace_enable(self._trace_output + '.json')
-
-  @contextlib.contextmanager
-  def Tracing(self):
-    try:
-      self.EnableTracing()
-      yield
-    finally:
-      self.DisableTracing()
