1898ef34e2c23e5e246747e53f0d085d187a3129
wnwen <wnwen@chromium.org>
2017-01-24 12:03:51 -0800

    Android: Re-order deferred async task UMA record
    Ensure that when the UMA record for duration is written the entire async
    task is done.
    
    BUG=683944
    
    Review-Url: https://codereview.chromium.org/2653733002
    Cr-Commit-Position: refs/heads/master@{#445789}

--- chrome/android/java/src/org/chromium/chrome/browser/DeferredStartupHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/DeferredStartupHandler.java
@@ -235,22 +235,20 @@ public class DeferredStartupHandler {
 
     private void initAsyncDiskTask() {
         new AsyncTask<Void, Void, Void>() {
+            private long mAsyncTaskStartTime;
+
             @Override
             protected Void doInBackground(Void... params) {
                 try {
                     TraceEvent.begin("ChromeBrowserInitializer.onDeferredStartup.doInBackground");
-                    long asyncTaskStartTime = SystemClock.uptimeMillis();
-
-                    // Initialize the WebappRegistry if it's not already initialized. Must be in
-                    // async task due to shared preferences disk access on N.
-                    WebappRegistry.getInstance();
+                    mAsyncTaskStartTime = SystemClock.uptimeMillis();
 
                     boolean crashDumpDisabled = CommandLine.getInstance().hasSwitch(
                             ChromeSwitches.DISABLE_CRASH_DUMP_UPLOAD);
                     if (!crashDumpDisabled) {
                         RecordHistogram.recordLongTimesHistogram(
                                 "UMA.Debug.EnableCrashUpload.Uptime3",
-                                asyncTaskStartTime - UmaUtils.getForegroundStartTime(),
+                                mAsyncTaskStartTime - UmaUtils.getForegroundStartTime(),
                                 TimeUnit.MILLISECONDS);
                         PrivacyPreferencesManager.getInstance().enablePotentialCrashUploading();
                         MinidumpUploadService.tryUploadAllCrashDumps(mAppContext);
@@ -261,6 +259,10 @@ public class DeferredStartupHandler {
 
                     MinidumpUploadService.storeBreakpadUploadStatsInUma(
                             ChromePreferenceManager.getInstance(mAppContext));
+
+                    // Initialize the WebappRegistry if it's not already initialized. Must be in
+                    // async task due to shared preferences disk access on N.
+                    WebappRegistry.getInstance();
 
                     // Force a widget refresh in order to wake up any possible zombie widgets.
                     // This is needed to ensure the right behavior when the process is suddenly
@@ -278,11 +280,6 @@ public class DeferredStartupHandler {
 
                     cacheIsChromeDefaultBrowser();
 
-                    RecordHistogram.recordLongTimesHistogram(
-                            "UMA.Debug.EnableCrashUpload.DeferredStartUpDurationAsync",
-                            SystemClock.uptimeMillis() - asyncTaskStartTime,
-                            TimeUnit.MILLISECONDS);
-
                     // Warm up all web app shared prefs. This must be run after the WebappRegistry
                     // instance is initialized.
                     WebappRegistry.warmUpSharedPrefs();
@@ -297,6 +294,11 @@ public class DeferredStartupHandler {
             protected void onPostExecute(Void params) {
                 // Must be run on the UI thread after the WebappRegistry has been completely warmed.
                 WebappRegistry.getInstance().unregisterOldWebapps(System.currentTimeMillis());
+
+                RecordHistogram.recordLongTimesHistogram(
+                        "UMA.Debug.EnableCrashUpload.DeferredStartUpAsyncTaskDuration",
+                        SystemClock.uptimeMillis() - mAsyncTaskStartTime,
+                        TimeUnit.MILLISECONDS);
             }
         }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
