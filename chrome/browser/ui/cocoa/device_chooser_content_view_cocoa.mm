8953e96a0deaa422cf51c5718cdb8c22e02320cb
juncai <juncai@chromium.org>
2017-01-24 15:23:49 -0800

    Change the Bluetooth chooser buttons order to be Cancel/Pair on MacOS
    This CL changes the Bluetooth chooser buttons order to be Cancel/Pair
    to make them consistent with other dialog buttons in Chrome.
    
    I uploaded a screenshot on the issue page.
    
    BUG=684479
    
    Review-Url: https://codereview.chromium.org/2649323006
    Cr-Commit-Position: refs/heads/master@{#445853}

--- chrome/browser/ui/cocoa/device_chooser_content_view_cocoa.mm
+++ chrome/browser/ui/cocoa/device_chooser_content_view_cocoa.mm
@@ -403,7 +403,7 @@ void ChooserContentViewController::UpdateTableView() {
   // | |                              | |
   // | |                              | |
   // | -------------------------------- |
-  // |           [ Connect ] [ Cancel ] |
+  // |           [ Cancel ] [ Connect ] |
   // |----------------------------------|
   // | Get help                         |
   // ------------------------------------
@@ -435,14 +435,14 @@ void ChooserContentViewController::UpdateTableView() {
         IDS_BLUETOOTH_DEVICE_CHOOSER_TURN_ADAPTER_OFF, base::string16()));
     CGFloat adapterOffMessageWidth = NSWidth([adapterOffMessage_ frame]);
 
-    // Connect button.
-    connectButton_ = [self createConnectButton];
-    CGFloat connectButtonWidth = NSWidth([connectButton_ frame]);
-    CGFloat connectButtonHeight = NSHeight([connectButton_ frame]);
-
     // Cancel button.
     cancelButton_ = [self createCancelButton];
     CGFloat cancelButtonWidth = NSWidth([cancelButton_ frame]);
+    CGFloat cancelButtonHeight = NSHeight([cancelButton_ frame]);
+
+    // Connect button.
+    connectButton_ = [self createConnectButton];
+    CGFloat connectButtonWidth = NSWidth([connectButton_ frame]);
 
     // Separator.
     separator_ = [self createSeparator];
@@ -481,13 +481,13 @@ void ChooserContentViewController::UpdateTableView() {
         kChooserHeight - 2 * kMarginY -
         (chooserController_->ShouldShowFootnoteView() ? 4 * kVerticalPadding
                                                       : 2 * kVerticalPadding) -
-        titleHeight - connectButtonHeight - helpButtonHeight;
+        titleHeight - cancelButtonHeight - helpButtonHeight;
     CGFloat scrollViewOriginX = kMarginX;
     CGFloat scrollViewOriginY =
         kMarginY + helpButtonHeight +
         (chooserController_->ShouldShowFootnoteView() ? 3 * kVerticalPadding
                                                       : kVerticalPadding) +
-        connectButtonHeight;
+        cancelButtonHeight;
     NSRect scrollFrame = NSMakeRect(scrollViewOriginX, scrollViewOriginY,
                                     scrollViewWidth, scrollViewHeight);
     scrollView_.reset([[NSScrollView alloc] initWithFrame:scrollFrame]);
@@ -559,25 +559,26 @@ void ChooserContentViewController::UpdateTableView() {
     [spinner_ setHidden:YES];
     [self addSubview:spinner_];
 
-    // Connect button.
-    CGFloat connectButtonOriginX = kChooserWidth - kMarginX -
-                                   kHorizontalPadding - connectButtonWidth -
-                                   cancelButtonWidth;
-    CGFloat connectButtonOriginY =
+    // Cancel button.
+    CGFloat cancelButtonOriginX = kChooserWidth - kMarginX -
+                                  kHorizontalPadding - cancelButtonWidth -
+                                  connectButtonWidth;
+    CGFloat cancelButtonOriginY =
         kMarginY + helpButtonHeight +
         (chooserController_->ShouldShowFootnoteView() ? 2 * kVerticalPadding
                                                       : 0.0f);
+    [cancelButton_
+        setFrameOrigin:NSMakePoint(cancelButtonOriginX, cancelButtonOriginY)];
+    [self addSubview:cancelButton_];
+
+    // Connect button.
+    CGFloat connectButtonOriginX =
+        kChooserWidth - kMarginX - connectButtonWidth;
+    CGFloat connectButtonOriginY = cancelButtonOriginY;
     [connectButton_
         setFrameOrigin:NSMakePoint(connectButtonOriginX, connectButtonOriginY)];
     [connectButton_ setEnabled:NO];
     [self addSubview:connectButton_];
-
-    // Cancel button.
-    CGFloat cancelButtonOriginX = kChooserWidth - kMarginX - cancelButtonWidth;
-    CGFloat cancelButtonOriginY = connectButtonOriginY;
-    [cancelButton_
-        setFrameOrigin:NSMakePoint(cancelButtonOriginX, cancelButtonOriginY)];
-    [self addSubview:cancelButton_];
 
     if (chooserController_->ShouldShowFootnoteView()) {
       // Separator.
