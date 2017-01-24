840075305ad57faf89733df94f5f944149543381
zpeng <zpeng@chromium.org>
2017-01-23 16:18:01 -0800

    Require WebApp & WebAPK primary icons to have IconPurpose::ANY
    Icons with only IconPurpose::BADGE as icon purposes should not be used
    as primary icons for WebApps or WebAPKs. The "purpose" attribute of
    icons, if not set, defaults to IconPurpose::ANY.
    
    BUG=649771
    
    Review-Url: https://codereview.chromium.org/2642233002
    Cr-Commit-Position: refs/heads/master@{#445554}

--- chrome/browser/installable/installable_manager.cc
+++ chrome/browser/installable/installable_manager.cc
@@ -5,7 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/installable/installable_manager.h"
 
+#include <algorithm>
+
 #include "base/bind.h"
+#include "base/stl_util.h"
 #include "base/strings/string_util.h"
 #include "chrome/browser/manifest/manifest_icon_downloader.h"
 #include "chrome/browser/manifest/manifest_icon_selector.h"
@@ -20,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/url_util.h"
 #include "third_party/WebKit/public/platform/WebDisplayMode.h"
 
+using IconPurpose = content::Manifest::Icon::IconPurpose;
+
 namespace {
 
 const char kPngExtension[] = ".png";
@@ -31,7 +36,8 @@ const char kPngExtension[] = ".png";
 // TODO(dominickn): consolidate with minimum_icon_size_in_px across platforms.
 const int kIconMinimumSizeInPx = 144;
 
-// Returns true if |manifest| specifies a PNG icon >= 144x144px (or size "any").
+// Returns true if |manifest| specifies a PNG icon >= 144x144px (or size "any"),
+// and of icon purpose IconPurpose::ANY.
 bool DoesManifestContainRequiredIcon(const content::Manifest& manifest) {
   for (const auto& icon : manifest.icons) {
     // The type field is optional. If it isn't present, fall back on checking
@@ -40,6 +46,9 @@ bool DoesManifestContainRequiredIcon(const content::Manifest& manifest) {
         !(icon.type.empty() && base::EndsWith(
             icon.src.ExtractFileName(), kPngExtension,
             base::CompareCase::INSENSITIVE_ASCII)))
+      continue;
+
+    if (!base::ContainsValue(icon.purpose, IconPurpose::ANY))
       continue;
 
     for (const auto& size : icon.sizes) {
--- chrome/browser/installable/installable_manager_unittest.cc
+++ chrome/browser/installable/installable_manager_unittest.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/WebKit/public/platform/WebDisplayMode.h"
 
+using IconPurpose = content::Manifest::Icon::IconPurpose;
+
 class InstallableManagerUnitTest : public testing::Test {
  public:
   InstallableManagerUnitTest() : manager_(new InstallableManager(nullptr)) { }
@@ -28,6 +30,7 @@ class InstallableManagerUnitTest : public testing::Test {
     content::Manifest::Icon icon;
     icon.type = base::ASCIIToUTF16("image/png");
     icon.sizes.push_back(gfx::Size(144, 144));
+    icon.purpose.push_back(IconPurpose::ANY);
     manifest.icons.push_back(icon);
 
     return manifest;
@@ -130,6 +133,20 @@ TEST_F(InstallableManagerUnitTest, ManifestRequiresImagePNG) {
   manifest.icons[0].src = GURL("http://example.com/icon.gif");
   EXPECT_FALSE(IsManifestValid(manifest));
   EXPECT_EQ(MANIFEST_MISSING_SUITABLE_ICON, GetErrorCode());
+}
+
+TEST_F(InstallableManagerUnitTest, ManifestRequiresPurposeAny) {
+  content::Manifest manifest = GetValidManifest();
+
+  // The icon MUST have IconPurpose::ANY at least.
+  manifest.icons[0].purpose[0] = IconPurpose::BADGE;
+  EXPECT_FALSE(IsManifestValid(manifest));
+  EXPECT_EQ(MANIFEST_MISSING_SUITABLE_ICON, GetErrorCode());
+
+  // If one of the icon purposes match the requirement, it should be accepted.
+  manifest.icons[0].purpose.push_back(IconPurpose::ANY);
+  EXPECT_TRUE(IsManifestValid(manifest));
+  EXPECT_EQ(NO_ERROR_DETECTED, GetErrorCode());
 }
 
 TEST_F(InstallableManagerUnitTest, ManifestRequiresMinimalSize) {
