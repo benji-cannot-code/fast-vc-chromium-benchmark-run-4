95697ca8a197ff7523d2a7fe33054a1e4de9254c
gozzard <gozzard@google.com>
2017-01-23 18:49:49 -0800

    Implements CSSPropertyAPI for the color properties.
    A part of Project Ribbon, separating the parsing logic for CSS
    properties from the parser into an API. This patch removes
    CSSPropertyWebkitTextFillColor, CSSPropertyWebkitTapHighlightColor,
    CSSPropertyWebkitTextEmphasisColor, CSSPropertyWebkitBorderStartColor,
    CSSPropertyWebkitBorderEndColor, CSSPropertyWebkitBorderBeforeColor,
    CSSPropertyWebkitBorderAfterColor, CSSPropertyWebkitTextStrokeColor,
    CSSPropertyStopColor, CSSPropertyFloodColor, CSSPropertyLightingColor,
    and CSSPropertyColumnRuleColor from the switch statement in
    parseSingleValue, and calls the API instead.
    
    A function pointer to the parseSingleValue function from the API for the
    color properties is stored in a CSSPropertyDescriptor, and is called
    from CSSPropertyParser.
    
    This patch:
    - Adds CSSPropertyAPIColor.cpp to the BUILD.gn file.
    - Adds api_class flag to CSSProperties.in, which indicates that
      CSSPropertyAPIColor.h is generated.
    - Moves the parsing logic for color from CSSPropertyParser.cpp to
      CSSPropertyAPIColor.cpp, which implements CSSPropertyAPI.h.
    
    BUG=668012
    
    Review-Url: https://codereview.chromium.org/2647613002
    Cr-Commit-Position: refs/heads/master@{#445619}

---
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyAPIColor.cpp
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/properties/CSSPropertyAPIColor.h"
+
+#include "core/css/parser/CSSParserContext.h"
+#include "core/css/parser/CSSPropertyParserHelpers.h"
+
+namespace blink {
+
+const CSSValue* CSSPropertyAPIColor::parseSingleValue(
+    CSSParserTokenRange& range,
+    const CSSParserContext* context) {
+  return CSSPropertyParserHelpers::consumeColor(range, context->mode());
+}
+
+}  // namespace blink
