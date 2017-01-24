a32d6d3d3b4881860c655ade0316a75bd415bf1e
oysteine <oysteine@chromium.org>
2017-01-24 15:56:12 -0800

    Added accessor for the ukm service to the browser process singleton
    Split out from https://codereview.chromium.org/2617883004/
    
    R=holte@chromium.org,sky@chromium.org
    BUG=684673
    
    Review-Url: https://codereview.chromium.org/2649983005
    Cr-Commit-Position: refs/heads/master@{#445866}

--- chrome/browser/browser_process_impl.cc
+++ chrome/browser/browser_process_impl.cc
@@ -542,6 +542,11 @@ rappor::RapporServiceImpl* BrowserProcessImpl::rappor_service() {
   return GetMetricsServicesManager()->GetRapporServiceImpl();
 }
 
+ukm::UkmService* BrowserProcessImpl::ukm_service() {
+  DCHECK(CalledOnValidThread());
+  return GetMetricsServicesManager()->GetUkmService();
+}
+
 IOThread* BrowserProcessImpl::io_thread() {
   DCHECK(CalledOnValidThread());
   DCHECK(io_thread_.get());
--- chrome/test/base/testing_browser_process.cc
+++ chrome/test/base/testing_browser_process.cc
@@ -118,6 +118,10 @@ rappor::RapporServiceImpl* TestingBrowserProcess::rappor_service() {
   return rappor_service_;
 }
 
+ukm::UkmService* TestingBrowserProcess::ukm_service() {
+  return ukm_service_;
+}
+
 IOThread* TestingBrowserProcess::io_thread() {
   return io_thread_;
 }
@@ -464,6 +468,10 @@ void TestingBrowserProcess::SetRulesetService(
 void TestingBrowserProcess::SetRapporServiceImpl(
     rappor::RapporServiceImpl* rappor_service) {
   rappor_service_ = rappor_service;
+}
+
+void TestingBrowserProcess::SetUkmService(ukm::UkmService* ukm_service) {
+  ukm_service_ = ukm_service;
 }
 
 void TestingBrowserProcess::SetShuttingDown(bool is_shutting_down) {
