68b4ad902bf258539a02a7129d3bf9788948d003
rogerta <rogerta@chromium.org>
2017-01-24 07:49:30 -0800

    Undo most changes in https://crrev.com/d162213add439d3439540ee66c6911d5f91dd95a
    This is not just a revert of the previous CL since there were changes made
    since that would conflict.  Also there was a little bit of cleanup in the way
    the default service was being accessed that I think is worth keeping.
    
    BUG=633256
    
    Review-Url: https://codereview.chromium.org/2650023002
    Cr-Commit-Position: refs/heads/master@{#445734}

--- chrome/browser/signin/chrome_signin_client.h
+++ chrome/browser/signin/chrome_signin_client.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/macros.h"
-#include "base/memory/weak_ptr.h"
 #include "build/build_config.h"
 #include "components/signin/core/browser/signin_client.h"
 #include "components/signin/core/browser/signin_error_controller.h"
@@ -18,10 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if !defined(OS_CHROMEOS)
 #include "net/base/network_change_notifier.h"
 #endif
-
-namespace net {
-class URLRequestContext;
-}
 
 class Profile;
 
@@ -104,7 +99,6 @@ class ChromeSigninClient
 #endif
 
   void AfterCredentialsCopied() override;
-  int number_of_request_context_pointer_changes() const override;
 
  protected:
   virtual void ShowUserManager(const base::FilePath& profile_path);
@@ -115,7 +109,6 @@ class ChromeSigninClient
   void OnCloseBrowsersSuccess(const base::Callback<void()>& sign_out,
                               const base::FilePath& profile_path);
   void OnCloseBrowsersAborted(const base::FilePath& profile_path);
-  void RequestContextPointerReply(net::URLRequestContext* pointer);
 
   Profile* profile_;
 
@@ -129,12 +122,6 @@ class ChromeSigninClient
 
   std::unique_ptr<gaia::GaiaOAuthClient> oauth_client_;
   std::unique_ptr<OAuth2TokenService::Request> oauth_request_;
-
-  // These members are used to debug channel id binding problems in chrome.
-  void* request_context_pointer_;
-  int number_of_request_context_pointer_changes_;
-
-  base::WeakPtrFactory<ChromeSigninClient> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(ChromeSigninClient);
 };
