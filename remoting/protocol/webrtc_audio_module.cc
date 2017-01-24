ee0eff08712942cd06b157f109ecda40eda9fead
Sergey Ulanov <sergeyu@chromium.org>
2017-01-23 17:24:46 -0800

    Revert 25680c62320767f590d037d301edfe15e9c55650
    TBR=sergeyu@chromium.org
    
    Review-Url: https://codereview.chromium.org/2653563004 .
    Cr-Commit-Position: refs/heads/master@{#445578}

--- remoting/protocol/webrtc_audio_module.cc
+++ remoting/protocol/webrtc_audio_module.cc
@@ -6,10 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/protocol/webrtc_audio_module.h"
 
 #include "base/bind.h"
-#include "base/memory/ptr_util.h"
 #include "base/stl_util.h"
 #include "base/threading/thread_task_runner_handle.h"
-#include "base/timer/timer.h"
 
 namespace remoting {
 namespace protocol {
@@ -505,15 +503,14 @@ int WebrtcAudioModule::GetRecordAudioParameters(
 
 void WebrtcAudioModule::StartPlayoutOnAudioThread() {
   DCHECK(audio_task_runner_->BelongsToCurrentThread());
-  poll_timer_ = base::MakeUnique<base::RepeatingTimer>();
-  poll_timer_->Start(
+  poll_timer_.Start(
       FROM_HERE, kPollInterval,
       base::Bind(&WebrtcAudioModule::PollFromSource, base::Unretained(this)));
 }
 
 void WebrtcAudioModule::StopPlayoutOnAudioThread() {
   DCHECK(audio_task_runner_->BelongsToCurrentThread());
-  poll_timer_.reset();
+  poll_timer_.Stop();
 }
 
 void WebrtcAudioModule::PollFromSource() {
