bb9ea95694e5a403a14027c0fecb2ace856d9310
jam <jam@chromium.org>
2017-01-24 10:54:58 -0800

    Convert DistillabilityDriver to use the new navigation callbacks.
    BUG=682002
    
    Review-Url: https://codereview.chromium.org/2649303003
    Cr-Commit-Position: refs/heads/master@{#445768}

--- components/dom_distiller/content/browser/distillability_driver.cc
+++ components/dom_distiller/content/browser/distillability_driver.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/dom_distiller/content/browser/distillability_driver.h"
 
 #include "base/memory/ptr_util.h"
+#include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
@@ -89,10 +90,10 @@ void DistillabilityDriver::RenderFrameHostChanged(
   SetupMojoService(new_host);
 }
 
-void DistillabilityDriver::DidStartProvisionalLoadForFrame(
-    content::RenderFrameHost* render_frame_host, const GURL& validated_url,
-    bool is_error_page) {
-  SetupMojoService(render_frame_host);
+void DistillabilityDriver::ReadyToCommitNavigation(
+      content::NavigationHandle* navigation_handle) {
+  if (!navigation_handle->IsSamePage())
+    SetupMojoService(navigation_handle->GetRenderFrameHost());
 }
 
 void DistillabilityDriver::SetupMojoService(
