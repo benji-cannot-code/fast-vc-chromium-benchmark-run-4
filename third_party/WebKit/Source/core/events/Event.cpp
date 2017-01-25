f41926e241a3375608def081817747ed8aa0becd
lpy <lpy@chromium.org>
2017-01-25 13:13:00 -0800

    Remove setting cancelBubble to false.
    According to spec https://dom.spec.whatwg.org/#dom-event-cancelbubble, setting
    cancelBubble to true is considered as an alias to stopPropagation(), setting
    cancelBubble to false is a no-op. Detail discussion is on
    https://github.com/whatwg/dom/issues/211.
    
    BUG=675549
    
    Review-Url: https://codereview.chromium.org/2641953006
    Cr-Commit-Position: refs/heads/master@{#446118}

--- third_party/WebKit/Source/core/events/Event.cpp
+++ third_party/WebKit/Source/core/events/Event.cpp
@@ -88,7 +88,6 @@ Event::Event(const AtomicString& eventType,
       m_immediatePropagationStopped(false),
       m_defaultPrevented(false),
       m_defaultHandled(false),
-      m_cancelBubble(false),
       m_wasInitialized(true),
       m_isTrusted(false),
       m_preventDefaultCalledOnUncancelableEvent(false),
@@ -338,11 +337,8 @@ double Event::timeStamp(ScriptState* scriptState) const {
 }
 
 void Event::setCancelBubble(ExecutionContext* context, bool cancel) {
-  if (!m_cancelBubble && cancel)
-    UseCounter::count(context, UseCounter::EventCancelBubbleWasChangedToTrue);
-  else if (m_cancelBubble && !cancel)
-    UseCounter::count(context, UseCounter::EventCancelBubbleWasChangedToFalse);
-  m_cancelBubble = cancel;
+  if (cancel)
+    m_propagationStopped = true;
 }
 
 DEFINE_TRACE(Event) {
--- third_party/WebKit/Source/core/events/EventDispatcher.cpp
+++ third_party/WebKit/Source/core/events/EventDispatcher.cpp
@@ -219,11 +219,6 @@ inline void EventDispatcher::dispatchEventAtBubbling() {
     } else if (m_event->bubbles() && !m_event->cancelBubble()) {
       m_event->setEventPhase(Event::kBubblingPhase);
     } else {
-      if (m_event->bubbles() && m_event->cancelBubble() &&
-          eventContext.node() &&
-          eventContext.node()->hasEventListeners(m_event->type()))
-        UseCounter::count(eventContext.node()->document(),
-                          UseCounter::EventCancelBubbleAffected);
       continue;
     }
     eventContext.handleLocalEvents(*m_event);
@@ -233,17 +228,6 @@ inline void EventDispatcher::dispatchEventAtBubbling() {
   if (m_event->bubbles() && !m_event->cancelBubble()) {
     m_event->setEventPhase(Event::kBubblingPhase);
     m_event->eventPath().windowEventContext().handleLocalEvents(*m_event);
-  } else if (m_event->bubbles() &&
-             m_event->eventPath().windowEventContext().window() &&
-             m_event->eventPath()
-                 .windowEventContext()
-                 .window()
-                 ->hasEventListeners(m_event->type())) {
-    UseCounter::count(m_event->eventPath()
-                          .windowEventContext()
-                          .window()
-                          ->getExecutionContext(),
-                      UseCounter::EventCancelBubbleAffected);
   }
 }
 
