f1efd97a3400d6b2305686f456fde58b1dbcaa1b
pfeldman <pfeldman@chromium.org>
2017-01-24 17:08:55 -0800

    DevTools: do not consider 'force update on reload' as error.
    BUG=681078
    
    Review-Url: https://codereview.chromium.org/2657493002
    Cr-Commit-Position: refs/heads/master@{#445890}

--- content/browser/service_worker/service_worker_version.h
+++ content/browser/service_worker/service_worker_version.h
@@ -355,6 +355,8 @@ class CONTENT_EXPORT ServiceWorkerVersion
   void ReportError(ServiceWorkerStatusCode status,
                    const std::string& status_message);
 
+  void ReportForceUpdateToDevTools();
+
   // Sets the status code to pass to StartWorker callbacks if start fails.
   void SetStartWorkerStatusCode(ServiceWorkerStatusCode status);
 
