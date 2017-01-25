fd7cdca372e15bd87cb7b811d908ad0d6c5ce23e
rockot <rockot@chromium.org>
2017-01-25 11:22:52 -0800

    Mojo C++ Bindings: Make SyncHandleRegistry Leaky
    This avoids a potential data race on the ThreadLocalPointer
    if someone happens to leave a Mojo-using thread alive during
    shutdown.
    
    BUG=685157
    R=yzshen@chromium.org
    
    Review-Url: https://codereview.chromium.org/2658583003
    Cr-Commit-Position: refs/heads/master@{#446080}

--- mojo/public/cpp/bindings/lib/sync_handle_registry.cc
+++ mojo/public/cpp/bindings/lib/sync_handle_registry.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace mojo {
 namespace {
 
-base::LazyInstance<base::ThreadLocalPointer<SyncHandleRegistry>>
+base::LazyInstance<base::ThreadLocalPointer<SyncHandleRegistry>>::Leaky
     g_current_sync_handle_watcher = LAZY_INSTANCE_INITIALIZER;
 
 }  // namespace
