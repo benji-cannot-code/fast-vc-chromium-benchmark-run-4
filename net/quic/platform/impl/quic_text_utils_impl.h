049c0e63e68354bc5e5de332b041fc3d17e09dfe
fayang <fayang@chromium.org>
2017-01-25 13:19:32 -0800

    Add methods converting String to Int to quic_text_utils.
    Merge internal change: 145459066.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2650183003
    Cr-Commit-Position: refs/heads/master@{#446121}

--- net/quic/platform/impl/quic_text_utils_impl.h
+++ net/quic/platform/impl/quic_text_utils_impl.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
+#include "net/base/parse_number.h"
 
 namespace net {
 
@@ -45,6 +46,24 @@ class QuicTextUtilsImpl {
   // |out|.
   static bool StringToUint64(base::StringPiece in, uint64_t* out) {
     return base::StringToUint64(in, out);
+  }
+
+  // Returns true if |in| represents a valid int, and stores that value in
+  // |out|.
+  static bool StringToInt(base::StringPiece in, int* out) {
+    return base::StringToInt(in, out);
+  }
+
+  // Returns true if |in| represents a valid uint32, and stores that value in
+  // |out|.
+  static bool StringToUint32(base::StringPiece in, uint32_t* out) {
+    return ParseUint32(in, out, nullptr);
+  }
+
+  // Returns true if |in| represents a valid size_t, and stores that value in
+  // |out|.
+  static bool StringToSizeT(base::StringPiece in, size_t* out) {
+    return base::StringToSizeT(in, out);
   }
 
   // Returns a new std::string representing |in|.
