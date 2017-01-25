533ab3e46f99710c7efcace5f2dc89068b23116e
sky <sky@chromium.org>
2017-01-25 14:59:09 -0800

    chromeos: wires transients using the WindowTreeHost window
    The expectation is transients are honored at the widget level, which
    corresponds to wiring using the window created by the WindowTreeHost.
    
    BUG=685226
    TEST=covered by test.
    R=erg@chromium.org
    
    Review-Url: https://codereview.chromium.org/2649103009
    Cr-Commit-Position: refs/heads/master@{#446151}

--- ui/views/mus/desktop_window_tree_host_mus_unittest.cc
+++ ui/views/mus/desktop_window_tree_host_mus_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ptr_util.h"
 #include "ui/aura/client/cursor_client.h"
+#include "ui/aura/client/transient_window_client.h"
 #include "ui/aura/mus/in_flight_change.h"
 #include "ui/aura/test/mus/change_completion_waiter.h"
 #include "ui/aura/window.h"
@@ -30,12 +31,14 @@ class DesktopWindowTreeHostMusTest : public ViewsTestBase,
   ~DesktopWindowTreeHostMusTest() override {}
 
   // Creates a test widget. Takes ownership of |delegate|.
-  std::unique_ptr<Widget> CreateWidget(WidgetDelegate* delegate) {
+  std::unique_ptr<Widget> CreateWidget(WidgetDelegate* delegate = nullptr,
+                                       aura::Window* parent = nullptr) {
     std::unique_ptr<Widget> widget = base::MakeUnique<Widget>();
     Widget::InitParams params = CreateParams(Widget::InitParams::TYPE_WINDOW);
     params.delegate = delegate;
     params.ownership = Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET;
     params.bounds = gfx::Rect(0, 1, 111, 123);
+    params.parent = parent;
     widget->Init(params);
     widget->AddObserver(this);
     return widget;
@@ -106,7 +109,7 @@ class ExpectsNullCursorClientDuringTearDown : public aura::WindowObserver {
 };
 
 TEST_F(DesktopWindowTreeHostMusTest, Visibility) {
-  std::unique_ptr<Widget> widget(CreateWidget(nullptr));
+  std::unique_ptr<Widget> widget(CreateWidget());
   EXPECT_FALSE(widget->IsVisible());
   EXPECT_FALSE(widget->GetNativeView()->IsVisible());
   // It's important the parent is also hidden as this value is sent to the
@@ -123,10 +126,10 @@ TEST_F(DesktopWindowTreeHostMusTest, Visibility) {
 }
 
 TEST_F(DesktopWindowTreeHostMusTest, Deactivate) {
-  std::unique_ptr<Widget> widget1(CreateWidget(nullptr));
+  std::unique_ptr<Widget> widget1(CreateWidget());
   widget1->Show();
 
-  std::unique_ptr<Widget> widget2(CreateWidget(nullptr));
+  std::unique_ptr<Widget> widget2(CreateWidget());
   widget2->Show();
 
   widget1->Activate();
@@ -139,7 +142,7 @@ TEST_F(DesktopWindowTreeHostMusTest, Deactivate) {
 }
 
 TEST_F(DesktopWindowTreeHostMusTest, CursorClientDuringTearDown) {
-  std::unique_ptr<Widget> widget(CreateWidget(nullptr));
+  std::unique_ptr<Widget> widget(CreateWidget());
   widget->Show();
 
   std::unique_ptr<aura::Window> window(new aura::Window(nullptr));
@@ -151,10 +154,10 @@ TEST_F(DesktopWindowTreeHostMusTest, CursorClientDuringTearDown) {
 }
 
 TEST_F(DesktopWindowTreeHostMusTest, StackAtTop) {
-  std::unique_ptr<Widget> widget1(CreateWidget(nullptr));
+  std::unique_ptr<Widget> widget1(CreateWidget());
   widget1->Show();
 
-  std::unique_ptr<Widget> widget2(CreateWidget(nullptr));
+  std::unique_ptr<Widget> widget2(CreateWidget());
   widget2->Show();
 
   aura::test::ChangeCompletionWaiter waiter(
@@ -169,10 +172,10 @@ TEST_F(DesktopWindowTreeHostMusTest, StackAtTop) {
 }
 
 TEST_F(DesktopWindowTreeHostMusTest, StackAtTopAlreadyOnTop) {
-  std::unique_ptr<Widget> widget1(CreateWidget(nullptr));
+  std::unique_ptr<Widget> widget1(CreateWidget());
   widget1->Show();
 
-  std::unique_ptr<Widget> widget2(CreateWidget(nullptr));
+  std::unique_ptr<Widget> widget2(CreateWidget());
   widget2->Show();
 
   aura::test::ChangeCompletionWaiter waiter(
@@ -180,6 +183,23 @@ TEST_F(DesktopWindowTreeHostMusTest, StackAtTopAlreadyOnTop) {
       aura::ChangeType::REORDER, true);
   widget2->StackAtTop();
   waiter.Wait();
+}
+
+TEST_F(DesktopWindowTreeHostMusTest, TransientParentWiredToHostWindow) {
+  std::unique_ptr<Widget> widget1(CreateWidget());
+  widget1->Show();
+
+  std::unique_ptr<Widget> widget2(
+      CreateWidget(nullptr, widget1->GetNativeView()));
+  widget2->Show();
+
+  aura::client::TransientWindowClient* transient_window_client =
+      aura::client::GetTransientWindowClient();
+  // Even though the widget1->GetNativeView() was specified as the parent we
+  // expect the transient parents to be marked at the host level.
+  EXPECT_EQ(widget1->GetNativeView()->GetHost()->window(),
+            transient_window_client->GetTransientParent(
+                widget2->GetNativeView()->GetHost()->window()));
 }
 
 }  // namespace views
