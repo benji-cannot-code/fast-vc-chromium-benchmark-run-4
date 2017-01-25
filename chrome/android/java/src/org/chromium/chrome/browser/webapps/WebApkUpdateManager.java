fac8cd7cfbf4bb83da684fac34ee50e4797cab32
pkotwicz <pkotwicz@chromium.org>
2017-01-24 20:36:22 -0800

    Remove unused WebApkUpdateManager#installingFromUnknownSourcesAllowed()
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2649423003
    Cr-Commit-Position: refs/heads/master@{#445945}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkUpdateManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkUpdateManager.java
@@ -5,11 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.webapps;
 
-import android.content.ContentResolver;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.provider.Settings;
 import android.text.TextUtils;
 
 import org.chromium.base.CommandLine;
@@ -242,21 +240,6 @@ public class WebApkUpdateManager implements WebApkUpdateDataFetcher.Observer {
         // but a result from the previous request.
         storage.updateTimeOfLastWebApkUpdateRequestCompletion();
         storage.updateDidLastWebApkUpdateRequestSucceed(success);
-    }
-
-    /**
-     * Returns whether the user has enabled installing apps from sources other than the Google
-     * Play Store.
-     */
-    private static boolean installingFromUnknownSourcesAllowed() {
-        ContentResolver contentResolver = ContextUtils.getApplicationContext().getContentResolver();
-        try {
-            int setting = Settings.Secure.getInt(
-                    contentResolver, Settings.Secure.INSTALL_NON_MARKET_APPS);
-            return setting == 1;
-        } catch (Settings.SettingNotFoundException e) {
-            return false;
-        }
     }
 
     /**
