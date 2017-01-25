37764e087eaf082891278ad528a7815e186a6b0f
pkasting <pkasting@chromium.org>
2017-01-25 15:45:24 -0800

    Revert of Fix for google:instantExtendedEnabledKey replacement (patchset #2 id:20001 of https://codereview.chromium.org/2524733008/ )
    Reason for revert:
    Part 1 of 2: Reverting change that caused bug 680197
    
    Original issue's description:
    > Fix for google:instantExtendedEnabledKey replacement
    >
    > This is fix for minor bug that was inserted in
    > google:instantExtendedEnabledKey replacement in
    > https://codereview.chromium.org/2516963002/.
    > Incorrect string literal was used in replacement.
    >
    > R=treib@chromium.org, pkasting@chromium.org
    >
    > Committed: https://crrev.com/e128f9d8f6ebe7064709a42805e3d3233f521e31
    > Cr-Commit-Position: refs/heads/master@{#434456}
    
    TBR=treib@chromium.org,a-v-y@yandex-team.ru
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    
    Review-Url: https://codereview.chromium.org/2656893002
    Cr-Commit-Position: refs/heads/master@{#446165}

--- components/search_engines/template_url.cc
+++ components/search_engines/template_url.cc
@@ -69,8 +69,6 @@ const char kDefaultCount[] = "10";
 const char kOutputEncodingType[] = "UTF-8";
 
 constexpr char kGoogleInstantExtendedEnabledKey[] =
-    "google:instantExtendedEnabledKey";
-constexpr char kGoogleInstantExtendedEnabledKeyFull[] =
     "{google:instantExtendedEnabledKey}";
 
 // Attempts to encode |terms| and |original_query| in |encoding| and escape
@@ -174,10 +172,10 @@ bool SearchTermsReplacementKeysMatch(
   if (search_terms_replacement_key1 == search_terms_replacement_key2)
     return true;
   if (search_terms_replacement_key1 == google_util::kInstantExtendedAPIParam &&
-      search_terms_replacement_key2 == kGoogleInstantExtendedEnabledKeyFull)
+      search_terms_replacement_key2 == kGoogleInstantExtendedEnabledKey)
     return true;
   if (search_terms_replacement_key2 == google_util::kInstantExtendedAPIParam &&
-      search_terms_replacement_key1 == kGoogleInstantExtendedEnabledKeyFull)
+      search_terms_replacement_key1 == kGoogleInstantExtendedEnabledKey)
     return true;
   return false;
 }
@@ -1180,8 +1178,7 @@ TemplateURL::TemplateURL(const TemplateURLData& data, Type type)
   ResizeURLRefVector();
   SetPrepopulateId(data_.prepopulate_id);
 
-  if (data_.search_terms_replacement_key ==
-      kGoogleInstantExtendedEnabledKeyFull)
+  if (data_.search_terms_replacement_key == kGoogleInstantExtendedEnabledKey)
     data_.search_terms_replacement_key = google_util::kInstantExtendedAPIParam;
 }
 
