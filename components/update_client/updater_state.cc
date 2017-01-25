13f5fc7e85a343d10f806e8857e7910f97c517e1
sorin <sorin@chromium.org>
2017-01-25 09:39:11 -0800

    Include build/build_config.h in all component updater files.
    All files using conditional compilation must include the build_config,
    otherwise the platform symbols are not defined.
    
    BUG=684950
    
    Review-Url: https://codereview.chromium.org/2655813002
    Cr-Commit-Position: refs/heads/master@{#446050}

--- components/update_client/updater_state.cc
+++ components/update_client/updater_state.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
+#include "build/build_config.h"
 
 namespace update_client {
 
--- components/update_client/updater_state_unittest.cc
+++ components/update_client/updater_state_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/time/time.h"
 #include "base/version.h"
+#include "build/build_config.h"
 #include "components/update_client/updater_state.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
