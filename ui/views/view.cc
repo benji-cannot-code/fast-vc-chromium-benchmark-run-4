5bca68e885c8e6d1ee89dd0c6d7ca62aa75cc7cc
sky <sky@chromium.org>
2017-01-25 11:31:22 -0800

    Wires up ShouldDescendIntoChildForEventHandling() for DesktopNativeWidgetAura
    I promoted the implementation from NativeWidgetAura to Widget and both
    call it. As Widget is used by mac I had to pass in gfx::NativeView and
    the layers. I could have ifdef'd the function, but hopefully mac will
    eventually be aura based and will want to use the Widget
    implementation too.
    
    BUG=640365
    TEST=none
    R=erg@chromium.org
    
    Review-Url: https://codereview.chromium.org/2651753003
    Cr-Commit-Position: refs/heads/master@{#446085}

--- ui/views/view.cc
+++ ui/views/view.cc
@@ -220,7 +220,7 @@ void View::AddChildViewAt(View* view, int index) {
   const bool did_reparent_any_layers = view->UpdateParentLayers();
   Widget* widget = GetWidget();
   if (did_reparent_any_layers && widget)
-    widget->UpdateRootLayers();
+    widget->LayerTreeChanged();
 
   ReorderLayers();
 
@@ -566,14 +566,14 @@ void View::DestroyLayer() {
 
   Widget* widget = GetWidget();
   if (widget)
-    widget->UpdateRootLayers();
+    widget->LayerTreeChanged();
 }
 
 std::unique_ptr<ui::Layer> View::RecreateLayer() {
   std::unique_ptr<ui::Layer> old_layer = LayerOwner::RecreateLayer();
   Widget* widget = GetWidget();
   if (widget)
-    widget->UpdateRootLayers();
+    widget->LayerTreeChanged();
   return old_layer;
 }
 
@@ -1942,7 +1942,7 @@ void View::DoRemoveChildView(View* view,
   // removed.
   view->OrphanLayers();
   if (widget)
-    widget->UpdateRootLayers();
+    widget->LayerTreeChanged();
 
   view->PropagateRemoveNotifications(this, new_parent);
   view->parent_ = nullptr;
@@ -2236,7 +2236,7 @@ void View::CreateLayer(ui::LayerType layer_type) {
 
   Widget* widget = GetWidget();
   if (widget)
-    widget->UpdateRootLayers();
+    widget->LayerTreeChanged();
 
   // Before having its own Layer, this View may have painted in to a Layer owned
   // by an ancestor View. Scheduling a paint on the parent View will erase this
