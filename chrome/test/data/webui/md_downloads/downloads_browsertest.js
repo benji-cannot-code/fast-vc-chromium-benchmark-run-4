1bfd1bd5fbf2b86c8c5cd3e2325fbf97f4036626
dbeam <dbeam@chromium.org>
2017-01-24 19:34:19 -0800

    MD Downloads: make browser tests work with !use_vulcanize
    R=dpapad@chromium.org
    BUG=673825
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2651673008
    Cr-Commit-Position: refs/heads/master@{#445931}

--- chrome/test/data/webui/md_downloads/downloads_browsertest.js
+++ chrome/test/data/webui/md_downloads/downloads_browsertest.js
@@ -16,10 +16,26 @@ GEN_INCLUDE(
  * @constructor
  * @extends {PolymerTest}
  */
+function DownloadsTest() {}
+
+DownloadsTest.prototype = {
+  __proto__: PolymerTest.prototype,
+
+  /** @override */
+  setUp: function() {
+    PolymerTest.prototype.setUp.call(this);
+    this.accessibilityAuditConfig.ignoreSelectors('humanLangMissing', 'html');
+  },
+};
+
+/**
+ * @constructor
+ * @extends {DownloadsTest}
+ */
 function DownloadsItemTest() {}
 
 DownloadsItemTest.prototype = {
-  __proto__: PolymerTest.prototype,
+  __proto__: DownloadsTest.prototype,
 
   /** @override */
   browsePreload: 'chrome://downloads/item.html',
@@ -34,18 +50,17 @@ TEST_F('DownloadsItemTest', 'All', function() {
   mocha.run();
 });
 
-
 /**
  * @constructor
- * @extends {PolymerTest}
+ * @extends {DownloadsTest}
  */
 function DownloadsLayoutTest() {}
 
 DownloadsLayoutTest.prototype = {
-  __proto__: PolymerTest.prototype,
+  __proto__: DownloadsTest.prototype,
 
   /** @override */
-  browsePreload: 'chrome://downloads/manager.html',
+  browsePreload: 'chrome://downloads/',
 
   /** @override */
   extraLibraries: PolymerTest.getLibraries(ROOT_PATH).concat([
@@ -59,12 +74,12 @@ TEST_F('DownloadsLayoutTest', 'All', function() {
 
 /**
  * @constructor
- * @extends {PolymerTest}
+ * @extends {DownloadsTest}
  */
 function DownloadsToolbarTest() {}
 
 DownloadsToolbarTest.prototype = {
-  __proto__: PolymerTest.prototype,
+  __proto__: DownloadsTest.prototype,
 
   /** @override */
   browsePreload: 'chrome://downloads/toolbar.html',
