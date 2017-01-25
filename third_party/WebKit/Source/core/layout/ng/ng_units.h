2b89d2a94f85755896e5e56e5faa108696214c8e
glebl <glebl@chromium.org>
2017-01-24 17:28:20 -0800

    Rewrite LayoutNG margin collapsing/floats unit tests
    This patch rewrites PositionFloatFragments, CollapsingMarginsCase2WithFloats,
    CollapsingMarginsCase1WithFloats and PositionFloatInsideEmptyBlocks(new).
    
    Also it makes NGBlockLayoutAlgorithmTest to be based on RenderingTest. That
    allows to use setBodyInnerHTML to set HTML representation of tests and enables
    unittests to access old layout objects like FloatingObjects etc.
    
    The tests that are being changed here are temporarily marked as DISABLED.
    That's because the old tests are not fully correct. For the new float/margins
    collapsing algorithm these new tests will set correct expectations and will
    be enabled once the new code checked in.
    
    BUG=635619
    
    Review-Url: https://codereview.chromium.org/2641253002
    Cr-Commit-Position: refs/heads/master@{#445900}

--- third_party/WebKit/Source/core/layout/ng/ng_units.h
+++ third_party/WebKit/Source/core/layout/ng/ng_units.h
@@ -45,13 +45,6 @@ struct NGLogicalSize {
   bool IsEmpty() const {
     return inline_size == LayoutUnit() || block_size == LayoutUnit();
   }
-
- private:
-  friend class NGBlockLayoutAlgorithmTest;
-  // Used in tests.
-  NGLogicalSize(int inline_size, int block_size)
-      : inline_size(LayoutUnit(inline_size)),
-        block_size(LayoutUnit(block_size)) {}
 };
 
 inline std::ostream& operator<<(std::ostream& stream,
@@ -97,13 +90,6 @@ struct NGLogicalOffset {
   bool operator<=(const NGLogicalOffset& other) const;
 
   String ToString() const;
-
- private:
-  friend class NGBlockLayoutAlgorithmTest;
-  // Used in tests.
-  NGLogicalOffset(int inline_offset, int block_offset)
-      : inline_offset(LayoutUnit(inline_offset)),
-        block_offset(LayoutUnit(block_offset)) {}
 };
 
 CORE_EXPORT inline std::ostream& operator<<(std::ostream& os,
@@ -131,7 +117,7 @@ struct NGPhysicalOffset {
   }
 };
 
-struct NGPhysicalSize {
+struct CORE_EXPORT NGPhysicalSize {
   NGPhysicalSize() {}
   NGPhysicalSize(LayoutUnit width, LayoutUnit height)
       : width(width), height(height) {}
