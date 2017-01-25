867e1919a93c5470ec7b891594245bd26a652a51
gogerald <gogerald@chromium.org>
2017-01-25 12:48:42 -0800

    Surface settings UIs to allow users enable and disable Android payment apps in payment request
    The feature is disabled by default. It can be enabled via a flag:
    
      chrome://flags/#android-payment-apps
    
    To test the new functionality:
    
      1) Install BobPayWithIsReadyPayService.apk:
         https://drive.google.com/a/google.com/file/d/0B3ISiXgGE1MNSnFXcnkwQ1FSbGc/view?usp=sharing
      2) Enable chrome://flags/#android-payment-apps.
      3) Go to setttings/Autofill and payments/Payment apps
    
    BUG=679743
    
    patch from issue 2636023002 at patchset 140001 (http://crrev.com/2636023002#ps140001)
    
    Review-Url: https://codereview.chromium.org/2647573005
    Cr-Commit-Position: refs/heads/master@{#446111}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/autofill/AndroidPaymentAppPreference.java
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.preferences.autofill;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.chromium.chrome.R;
+import org.chromium.chrome.browser.preferences.ChromeSwitchPreference;
+
+/** ChromeSwitchPreference with fixed icon size for Android payment apps. */
+public class AndroidPaymentAppPreference extends ChromeSwitchPreference {
+    public AndroidPaymentAppPreference(Context context) {
+        super(context, null);
+    }
+
+    @Override
+    protected View onCreateView(ViewGroup parent) {
+        View view = super.onCreateView(parent);
+
+        int iconSize =
+                getContext().getResources().getDimensionPixelSize(R.dimen.payments_favicon_size);
+        View iconView = view.findViewById(android.R.id.icon);
+        ViewGroup.LayoutParams layoutParams = iconView.getLayoutParams();
+        layoutParams.width = iconSize;
+        layoutParams.height = iconSize;
+        iconView.setLayoutParams(layoutParams);
+
+        return view;
+    }
+}
