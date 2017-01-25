16ef4308c7385e18707a432042ab90de596f8a3d
kcarattini <kcarattini@chromium.org>
2017-01-24 18:17:07 -0800

    Safe Browsing: Move enabled_ to the base DatabaseManager class.
    Moves the enabled_ member to the SafeBrowsingDatabaseManager base class and checks enabled_ in CheckApiBlacklistUrl().
    
    Note that this changes the order in which enabled_ gets set in some of the subclasses.
    
    BUG=683791
    
    Review-Url: https://codereview.chromium.org/2648193002
    Cr-Commit-Position: refs/heads/master@{#445913}

--- components/safe_browsing_db/test_database_manager.h
+++ components/safe_browsing_db/test_database_manager.h
@@ -42,6 +42,9 @@ class TestSafeBrowsingDatabaseManager
   bool IsDownloadProtectionEnabled() const override;
   bool IsMalwareKillSwitchOn() override;
   bool IsSupported() const override;
+  void StartOnIOThread(net::URLRequestContextGetter* request_context_getter,
+                       const V4ProtocolConfig& config) override;
+  void StopOnIOThread(bool shutdown) override;
 
  protected:
   ~TestSafeBrowsingDatabaseManager() override {};
