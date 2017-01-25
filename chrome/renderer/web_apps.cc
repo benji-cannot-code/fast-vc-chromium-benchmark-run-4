b6b7ac11821a22ab94568379f641345760f8bdf1
kinuko <kinuko@chromium.org>
2017-01-25 07:22:34 -0800

    Use explicit WebString conversions in remaining chrome/renderer files
    As we're deprecating implicit conversions for WebString <-> string16
    replace them with explicit methods. (See crbug.com/667131 for more
    details)
    
    BUG=667131
    
    Review-Url: https://codereview.chromium.org/2651543007
    Cr-Commit-Position: refs/heads/master@{#446022}

--- chrome/renderer/web_apps.cc
+++ chrome/renderer/web_apps.cc
@@ -168,9 +168,9 @@ void ParseWebAppFromWebDocument(WebFrame* frame,
       std::string name = elem.getAttribute("name").utf8();
       WebString content = elem.getAttribute("content");
       if (name == "application-name") {
-        app_info->title = content;
+        app_info->title = content.utf16();
       } else if (name == "description") {
-        app_info->description = content;
+        app_info->description = content.utf16();
       } else if (name == "application-url") {
         std::string url = content.utf8();
         app_info->app_url = document_url.is_valid() ?
@@ -178,12 +178,10 @@ void ParseWebAppFromWebDocument(WebFrame* frame,
         if (!app_info->app_url.is_valid())
           app_info->app_url = GURL();
       } else if (name == "mobile-web-app-capable" &&
-                 base::LowerCaseEqualsASCII(base::StringPiece16(content),
-                                            "yes")) {
+                 base::LowerCaseEqualsASCII(content.utf16(), "yes")) {
         app_info->mobile_capable = WebApplicationInfo::MOBILE_CAPABLE;
       } else if (name == "apple-mobile-web-app-capable" &&
-                 base::LowerCaseEqualsASCII(
-                     base::StringPiece16(content), "yes") &&
+                 base::LowerCaseEqualsASCII(content.utf16(), "yes") &&
                  app_info->mobile_capable ==
                      WebApplicationInfo::MOBILE_CAPABLE_UNSPECIFIED) {
         app_info->mobile_capable = WebApplicationInfo::MOBILE_CAPABLE_APPLE;
