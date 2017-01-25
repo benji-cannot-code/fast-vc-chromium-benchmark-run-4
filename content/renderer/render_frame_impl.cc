c01673393a4654bd5db1e43364239bf132cf6f18
creis <creis@chromium.org>
2017-01-25 15:06:21 -0800

    Remove report_frame_name_change RendererPreference.
    This is no longer needed, now that we report name changes by default.
    
    BUG=169110
    TEST=Tests still pass.
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2650683005
    Cr-Commit-Position: refs/heads/master@{#446154}

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -3164,7 +3164,6 @@ void RenderFrameImpl::willCommitProvisionalLoad(blink::WebLocalFrame* frame) {
 
 void RenderFrameImpl::didChangeName(const blink::WebString& name,
                                     const blink::WebString& unique_name) {
-  // TODO(creis): Remove report_frame_name_changes from RendererPreferences.
   Send(new FrameHostMsg_DidChangeName(
       routing_id_, base::UTF16ToUTF8(base::StringPiece16(name)),
       base::UTF16ToUTF8(base::StringPiece16(unique_name))));
