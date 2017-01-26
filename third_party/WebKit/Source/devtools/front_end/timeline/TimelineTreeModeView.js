23a1a13bca2657db2e9f8b1215c7158d6a94186a
alph <alph@chromium.org>
2017-01-25 18:24:08 -0800

    DevTools: Bring back multiple Performance main views experiment.
    The CPU profiler users should find the interface more familiar.
    
    Review-Url: https://codereview.chromium.org/2632703003
    Cr-Commit-Position: refs/heads/master@{#446210}

---
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineTreeModeView.js
@@ -0,0 +1,87 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @implements {Timeline.TimelineModeView}
+ */
+Timeline.TimelineTreeModeView = class extends UI.VBox {
+  /**
+   * @param {!Timeline.TimelineModeViewDelegate} delegate
+   * @param {!Timeline.TimelineTreeView} innerTreeView
+   */
+  constructor(delegate, innerTreeView) {
+    super();
+    this._treeView = innerTreeView;
+    this._treeView.show(this.element);
+  }
+
+  /**
+   * @override
+   */
+  dispose() {
+  }
+
+  /**
+   * @override
+   * @return {?Element}
+   */
+  resizerElement() {
+    return null;
+  }
+
+  /**
+   * @override
+   */
+  highlightSearchResult() {
+  }
+
+  /**
+   * @override
+   * @param {?SDK.TracingModel.Event} event
+   */
+  highlightEvent(event) {
+  }
+
+  /**
+   * @override
+   */
+  refreshRecords() {
+  }
+
+  /**
+   * @override
+   */
+  extensionDataAdded() {
+  }
+
+  /**
+   * @override
+   */
+  reset() {
+  }
+
+  /**
+   * @override
+   */
+  setSelection() {
+  }
+
+  /**
+   * @override
+   * @param {number} startTime
+   * @param {number} endTime
+   */
+  setWindowTimes(startTime, endTime) {
+    this._treeView.setRange(startTime, endTime);
+  }
+
+  /**
+   * @override
+   * @return {!UI.Widget}
+   */
+  view() {
+    return this;
+  }
+};
