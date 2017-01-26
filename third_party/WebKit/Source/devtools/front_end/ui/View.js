62028ab65191ca3a10f7d3456525aa98d4e1030c
lushnikov <lushnikov@chromium.org>
2017-01-26 15:13:12 -0800

    DevTools: migrate UI.ViewManager's expand icon over to UI.Icon
    This patch migrates ViewManager's expand icon over to UI.Icon.
    This is the triangle icon in the "CallStack" sidebar pane title element.
    
    BUG=669323
    R=dgozman
    
    Review-Url: https://codereview.chromium.org/2654043004
    Cr-Commit-Position: refs/heads/master@{#446487}

--- third_party/WebKit/Source/devtools/front_end/ui/View.js
+++ third_party/WebKit/Source/devtools/front_end/ui/View.js
@@ -465,7 +465,9 @@ UI.ViewManager._ExpandableContainerWidget = class extends UI.VBox {
     this.registerRequiredCSS('ui/viewContainers.css');
 
     this._titleElement = createElementWithClass('div', 'expandable-view-title');
-    this._titleElement.textContent = view.title();
+    this._titleExpandIcon = UI.Icon.create('smallicon-triangle-right', 'title-expand-icon');
+    this._titleElement.appendChild(this._titleExpandIcon);
+    this._titleElement.createTextChild(view.title());
     this._titleElement.tabIndex = 0;
     this._titleElement.addEventListener('click', this._toggleExpanded.bind(this), false);
     this._titleElement.addEventListener('keydown', this._onTitleKeyDown.bind(this), false);
@@ -501,6 +503,7 @@ UI.ViewManager._ExpandableContainerWidget = class extends UI.VBox {
     if (this._titleElement.classList.contains('expanded'))
       return this._materialize();
     this._titleElement.classList.add('expanded');
+    this._titleExpandIcon.setIconType('smallicon-triangle-down');
     return this._materialize().then(() => this._widget.show(this.element));
   }
 
@@ -508,6 +511,7 @@ UI.ViewManager._ExpandableContainerWidget = class extends UI.VBox {
     if (!this._titleElement.classList.contains('expanded'))
       return;
     this._titleElement.classList.remove('expanded');
+    this._titleExpandIcon.setIconType('smallicon-triangle-right');
     this._materialize().then(() => this._widget.detach());
   }
 
