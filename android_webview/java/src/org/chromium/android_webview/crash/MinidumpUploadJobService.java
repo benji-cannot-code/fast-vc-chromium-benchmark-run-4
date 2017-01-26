4f87066085e656d20e7740854a4acedb81015af2
gsennton <gsennton@chromium.org>
2017-01-26 12:37:23 -0800

    [Android WebView] Ensure we have user consent before uploading minidumps
    WebView won't upload minidumps unless the Android Checkbox is toggled
    on (or the enabled-for-testing command-line flag is toggled on and we
    are using a debuggable device).
    With this CL we check the Checkbox value (in the app process) just
    before copying minidumps to the separate minidump uploading service. If
    we do not have user consent the minidumps are deleted.
    
    BUG=657314
    BUG=679693
    BUG=683882
    
    Review-Url: https://codereview.chromium.org/2628863004
    Cr-Commit-Position: refs/heads/master@{#446432}

--- android_webview/java/src/org/chromium/android_webview/crash/MinidumpUploadJobService.java
+++ android_webview/java/src/org/chromium/android_webview/crash/MinidumpUploadJobService.java
@@ -24,7 +24,6 @@ public class MinidumpUploadJobService extends JobService {
     @Override
     public void onCreate() {
         super.onCreate();
-        SynchronizedWebViewCommandLine.initOnSeparateThread();
     }
 
     @Override
