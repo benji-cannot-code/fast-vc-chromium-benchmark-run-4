3b5cdf3835463f7cc77e274aaf52a4c0bd99caa3
pkl <pkl@chromium.org>
2017-01-25 17:12:35 -0800

    Removed unused forward decl from tab.h
    Review-Url: https://codereview.chromium.org/2650823009
    Cr-Commit-Position: refs/heads/master@{#446186}

--- ios/chrome/browser/tabs/tab.h
+++ ios/chrome/browser/tabs/tab.h
@@ -44,8 +44,6 @@ class GURL;
 @protocol TabHeadersDelegate;
 @class TabModel;
 @protocol TabSnapshottingDelegate;
-@class TranslateControllerImplIOS;
-@class WebControllerSnapshotHelper;
 
 namespace infobars {
 class InfoBarManager;
