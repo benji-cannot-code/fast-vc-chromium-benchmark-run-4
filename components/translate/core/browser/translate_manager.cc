d8cc5e3b2cb9d2f1764f85f221406c1aaadae9f5
dbeam <dbeam@chromium.org>
2017-01-25 21:26:25 -0800

    Revert of Don't offer to translate between Simplified and Traditional Chinese (patchset #5 id:80001 of https://codereview.chromium.org/2650003004/ )
    Reason for revert:
    Causing sizes failure on Linux:
    https://build.chromium.org/p/chromium/builders/Linux%20x64/builds/31922/steps/sizes/logs/stdio
    
    # translate_manager.cc std::ios_base::Init::Init()@plt
    # translate_manager.cc std::__ioinit [#includes <iostream>, use <ostream> instead]
    # translate_manager.cc __cxa_atexit@plt [registers a dtor to run at exit]
    # translate_manager.cc __dso_handle
    # translate_manager.cc __init_array_end+0x968
    
    Original issue's description:
    > Don't offer to translate between Simplified and Traditional Chinese.
    >
    > BUG=683164
    >
    > Review-Url: https://codereview.chromium.org/2650003004
    > Cr-Commit-Position: refs/heads/master@{#446213}
    > Committed: https://chromium.googlesource.com/chromium/src/+/889fb1a6efab0b3e425b81575bbd7a62ed0c4eb1
    
    TBR=groby@chromium.org,riesa@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=683164
    
    Review-Url: https://codereview.chromium.org/2653183009
    Cr-Commit-Position: refs/heads/master@{#446232}

--- components/translate/core/browser/translate_manager.cc
+++ components/translate/core/browser/translate_manager.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/translate/core/browser/translate_manager.h"
 
-#include <iostream>
 #include <map>
 
 #include "base/bind.h"
@@ -226,14 +225,12 @@ void TranslateManager::InitiateTranslation(const std::string& page_lang) {
   InitTranslateEvent(language_code, target_lang, *translate_prefs);
 
   // Don't translate similar languages (ex: en-US to en).
-  // Also do not offer to translate between Simplified and Traditional Chinese.
-  if (language_code == target_lang ||
-      (language_code == "zh-CN" && target_lang == "zh-TW") ||
-      (language_code == "zh-TW" && target_lang == "zh-CN")) {
+  if (language_code == target_lang) {
     TranslateBrowserMetrics::ReportInitiationStatus(
         TranslateBrowserMetrics::INITIATION_STATUS_SIMILAR_LANGUAGES);
     return;
   }
+
   // Nothing to do if either the language Chrome is in or the language of the
   // page is not supported by the translation server.
   if (target_lang.empty() ||
