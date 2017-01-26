54b2b22e8c384f4cac93fd0863689e79288a0b69
falken <falken@chromium.org>
2017-01-26 04:19:19 -0800

    service worker: Remove debugging code for null http response info.
    The CHECK no longer fails after
    https://crrev.com/815dc48c9d00b0142a2421de8f10b7cb9c5b34ab
    
    BUG=485900
    
    Review-Url: https://codereview.chromium.org/2655823004
    Cr-Commit-Position: refs/heads/master@{#446305}

--- content/browser/service_worker/service_worker_context_request_handler.cc
+++ content/browser/service_worker/service_worker_context_request_handler.cc
@@ -53,8 +53,6 @@ ServiceWorkerContextRequestHandler::ServiceWorkerContextRequestHandler(
                                   resource_type),
       version_(provider_host_->running_hosted_version()) {
   DCHECK(provider_host_->IsHostToRunningServiceWorker());
-  if (resource_type_ == RESOURCE_TYPE_SERVICE_WORKER)
-    version_->NotifyMainScriptRequestHandlerCreated();
 }
 
 ServiceWorkerContextRequestHandler::~ServiceWorkerContextRequestHandler() {
@@ -114,8 +112,6 @@ net::URLRequestJob* ServiceWorkerContextRequestHandler::MaybeCreateJob(
   const bool is_main_script = resource_type_ == RESOURCE_TYPE_SERVICE_WORKER;
   ServiceWorkerMetrics::RecordContextRequestHandlerStatus(
       status, IsInstalled(version_.get()), is_main_script);
-  if (is_main_script)
-    version_->NotifyMainScriptJobCreated(status);
   if (job)
     return job;
 
--- content/browser/service_worker/service_worker_script_cache_map.h
+++ content/browser/service_worker/service_worker_script_cache_map.h
@@ -31,9 +31,6 @@ class ServiceWorkerResponseMetadataWriter;
 // for a particular version's implicit script resources.
 class CONTENT_EXPORT ServiceWorkerScriptCacheMap {
  public:
-  enum class StartStatus { UNINITIALIZED, NO_CONTEXT, STARTED };
-  enum class FinishStatus { UNINITIALIZED, NO_CONTEXT, NET_ERROR, FINISHED };
-
   int64_t LookupResourceId(const GURL& url);
 
   // Used during the initial run of a new version to build the map
@@ -88,15 +85,11 @@ class CONTENT_EXPORT ServiceWorkerScriptCacheMap {
       const net::CompletionCallback& callback,
       int result);
 
-  bool IsMainScript(const GURL& url);
-
   ServiceWorkerVersion* owner_;
   base::WeakPtr<ServiceWorkerContextCore> context_;
   ResourceMap resource_map_;
   net::URLRequestStatus main_script_status_;
   std::string main_script_status_message_;
-  StartStatus main_script_start_status_ = StartStatus::UNINITIALIZED;
-  FinishStatus main_script_finish_status_ = FinishStatus::UNINITIALIZED;
 
   base::WeakPtrFactory<ServiceWorkerScriptCacheMap> weak_factory_;
 
