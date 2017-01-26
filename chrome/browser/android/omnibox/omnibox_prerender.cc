6de31f0f274940e44c7b62697982319fe12e61bf
treib <treib@chromium.org>
2017-01-26 08:50:51 -0800

    Cleanup: Remove the last traces of Instant Search on Android
    - Usage of InstantSearchPrerenderer in omnibox_prerender.cc.
    - Some unnecessary includes in toolbar_model_android.cc.
    
    Instant was never launched on Android; it's time to get rid of the code.
    
    BUG=627747
    
    Review-Url: https://codereview.chromium.org/2652763013
    Cr-Commit-Position: refs/heads/master@{#446355}

--- chrome/browser/android/omnibox/omnibox_prerender.cc
+++ chrome/browser/android/omnibox/omnibox_prerender.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/predictors/autocomplete_action_predictor_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_android.h"
-#include "chrome/browser/ui/search/instant_search_prerenderer.h"
 #include "components/omnibox/browser/autocomplete_match.h"
 #include "components/omnibox/browser/autocomplete_result.h"
 #include "content/public/browser/web_contents.h"
@@ -92,26 +91,15 @@ void OmniboxPrerender::PrerenderMaybe(
   if (default_match == autocomplete_result->end())
     return;
 
-  AutocompleteActionPredictor::Action recommended_action =
-      AutocompleteActionPredictor::ACTION_NONE;
-  InstantSearchPrerenderer* prerenderer =
-      InstantSearchPrerenderer::GetForProfile(profile);
-  if (prerenderer &&
-      prerenderer->IsAllowed(*default_match, web_contents)) {
-    recommended_action = AutocompleteActionPredictor::ACTION_PRERENDER;
-  } else {
-    AutocompleteActionPredictor* action_predictor =
-        AutocompleteActionPredictorFactory::GetForProfile(profile);
-    if (!action_predictor)
-      return;
+  AutocompleteActionPredictor* action_predictor =
+      AutocompleteActionPredictorFactory::GetForProfile(profile);
+  if (!action_predictor)
+    return;
 
-    if (action_predictor) {
-      action_predictor->
-          RegisterTransitionalMatches(url_string, *autocomplete_result);
-      recommended_action =
-          action_predictor->RecommendAction(url_string, *default_match);
-    }
-  }
+  action_predictor->
+      RegisterTransitionalMatches(url_string, *autocomplete_result);
+  AutocompleteActionPredictor::Action recommended_action =
+      action_predictor->RecommendAction(url_string, *default_match);
 
   GURL current_url = GURL(current_url_string);
   switch (recommended_action) {
@@ -146,14 +134,6 @@ void OmniboxPrerender::DoPrerender(const AutocompleteMatch& match,
   if (!web_contents)
     return;
   gfx::Rect container_bounds = web_contents->GetContainerBounds();
-  InstantSearchPrerenderer* prerenderer =
-      InstantSearchPrerenderer::GetForProfile(profile);
-  if (prerenderer && prerenderer->IsAllowed(match, web_contents)) {
-    prerenderer->Init(
-        web_contents->GetController().GetDefaultSessionStorageNamespace(),
-        container_bounds.size());
-    return;
-  }
   predictors::AutocompleteActionPredictorFactory::GetForProfile(profile)->
       StartPrerendering(
           match.destination_url,
--- chrome/browser/ui/android/toolbar/toolbar_model_android.cc
+++ chrome/browser/ui/android/toolbar/toolbar_model_android.cc
@@ -6,17 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/android/toolbar/toolbar_model_android.h"
 
 #include "base/android/jni_string.h"
-#include "base/metrics/field_trial.h"
-#include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/search/search.h"
-#include "chrome/browser/search_engines/ui_thread_search_terms_data.h"
 #include "components/toolbar/toolbar_model_impl.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/ssl_status.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_constants.h"
 #include "jni/ToolbarModel_jni.h"
-#include "net/cert/x509_certificate.h"
 
 using base::android::JavaParamRef;
 using base::android::ScopedJavaLocalRef;
