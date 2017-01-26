fd5e2744015b7b6963b4321cfccd19803f56e3e0
pfeldman <pfeldman@chromium.org>
2017-01-25 20:47:19 -0800

    DevTools: make sure go-to-file scorer stays optimized.
    Review-Url: https://codereview.chromium.org/2648383009
    Cr-Commit-Position: refs/heads/master@{#446229}

--- third_party/WebKit/Source/devtools/front_end/sources/FilePathScoreFunction.js
+++ third_party/WebKit/Source/devtools/front_end/sources/FilePathScoreFunction.js
@@ -80,7 +80,7 @@ Sources.FilePathScoreFunction = class {
     }
     if (matchIndexes)
       this._restoreMatchIndexes(sequence, n, m, matchIndexes);
-    const maxDataLength = 256;
+    var maxDataLength = 256;
     return score[n * m - 1] * maxDataLength + (maxDataLength - data.length);
   }
 
@@ -90,8 +90,11 @@ Sources.FilePathScoreFunction = class {
    * @return {boolean}
    */
   _testWordStart(data, j) {
+    if (j === 0)
+      return true;
+
     var prevChar = data.charAt(j - 1);
-    return j === 0 || prevChar === '_' || prevChar === '-' || prevChar === '/' ||
+    return prevChar === '_' || prevChar === '-' || prevChar === '/' ||
         (data[j - 1] !== this._dataUpperCase[j - 1] && data[j] === this._dataUpperCase[j]);
   }
 
