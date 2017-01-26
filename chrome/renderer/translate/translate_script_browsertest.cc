e1aed26b5d27b63c4123fb74a8b9d6186e04d805
kinuko <kinuko@chromium.org>
2017-01-26 01:38:02 -0800

    Use explicit WebString conversions in plugins code
    As we're deprecating implicit conversions for WebString <-> string16
    replace them with explicit methods. (See crbug.com/667131 for more
    details)
    
    BUG=667131
    R=toyoshim@chromium.org
    
    Review-Url: https://codereview.chromium.org/2657613004
    Cr-Commit-Position: refs/heads/master@{#446284}

--- chrome/renderer/translate/translate_script_browsertest.cc
+++ chrome/renderer/translate/translate_script_browsertest.cc
@@ -89,7 +89,8 @@ class TranslateScriptBrowserTest : public ChromeRenderViewTest {
   }
 
   void ExecuteScript(const std::string& script) {
-    WebScriptSource source = WebScriptSource(base::ASCIIToUTF16(script));
+    WebScriptSource source =
+        WebScriptSource(blink::WebString::fromASCII(script));
     GetMainFrame()->executeScript(source);
   }
 
@@ -111,7 +112,8 @@ class TranslateScriptBrowserTest : public ChromeRenderViewTest {
   void TearDown() override { ChromeRenderViewTest::TearDown(); }
 
   double ExecuteScriptAndGetNumberResult(const std::string& script) {
-    WebScriptSource source = WebScriptSource(base::ASCIIToUTF16(script));
+    WebScriptSource source =
+        WebScriptSource(blink::WebString::fromASCII(script));
     v8::HandleScope handle_scope(v8::Isolate::GetCurrent());
     v8::Local<v8::Value> result =
         GetMainFrame()->executeScriptAndReturnValue(source);
@@ -125,7 +127,8 @@ class TranslateScriptBrowserTest : public ChromeRenderViewTest {
   }
 
   bool ExecuteScriptAndGetBoolResult(const std::string& script) {
-    WebScriptSource source = WebScriptSource(base::ASCIIToUTF16(script));
+    WebScriptSource source =
+        WebScriptSource(blink::WebString::fromASCII(script));
     v8::HandleScope handle_scope(v8::Isolate::GetCurrent());
     v8::Local<v8::Value> result =
         GetMainFrame()->executeScriptAndReturnValue(source);
--- components/translate/content/renderer/translate_helper.cc
+++ components/translate/content/renderer/translate_helper.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 #include "v8/include/v8.h"
 
-using base::ASCIIToUTF16;
 using blink::WebDocument;
 using blink::WebLocalFrame;
 using blink::WebScriptSource;
@@ -190,7 +189,7 @@ void TranslateHelper::ExecuteScript(const std::string& script) {
   if (!main_frame)
     return;
 
-  WebScriptSource source = WebScriptSource(ASCIIToUTF16(script));
+  WebScriptSource source = WebScriptSource(WebString::fromASCII(script));
   main_frame->executeScriptInIsolatedWorld(world_id_, &source, 1);
 }
 
@@ -202,7 +201,7 @@ bool TranslateHelper::ExecuteScriptAndGetBoolResult(const std::string& script,
 
   v8::HandleScope handle_scope(v8::Isolate::GetCurrent());
   WebVector<v8::Local<v8::Value> > results;
-  WebScriptSource source = WebScriptSource(ASCIIToUTF16(script));
+  WebScriptSource source = WebScriptSource(WebString::fromASCII(script));
   main_frame->executeScriptInIsolatedWorld(world_id_, &source, 1, &results);
   if (results.size() != 1 || results[0].IsEmpty() || !results[0]->IsBoolean()) {
     NOTREACHED();
@@ -220,7 +219,7 @@ std::string TranslateHelper::ExecuteScriptAndGetStringResult(
 
   v8::HandleScope handle_scope(v8::Isolate::GetCurrent());
   WebVector<v8::Local<v8::Value> > results;
-  WebScriptSource source = WebScriptSource(ASCIIToUTF16(script));
+  WebScriptSource source = WebScriptSource(WebString::fromASCII(script));
   main_frame->executeScriptInIsolatedWorld(world_id_, &source, 1, &results);
   if (results.size() != 1 || results[0].IsEmpty() || !results[0]->IsString()) {
     NOTREACHED();
@@ -242,7 +241,7 @@ double TranslateHelper::ExecuteScriptAndGetDoubleResult(
 
   v8::HandleScope handle_scope(v8::Isolate::GetCurrent());
   WebVector<v8::Local<v8::Value> > results;
-  WebScriptSource source = WebScriptSource(ASCIIToUTF16(script));
+  WebScriptSource source = WebScriptSource(WebString::fromASCII(script));
   main_frame->executeScriptInIsolatedWorld(world_id_, &source, 1, &results);
   if (results.size() != 1 || results[0].IsEmpty() || !results[0]->IsNumber()) {
     NOTREACHED();
