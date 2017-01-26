545225226b8830e0d3db184ba12ab37c4a5aeba1
tommycli <tommycli@chromium.org>
2017-01-26 11:45:19 -0800

    MD Settings: Update some dialogs to accept Enter key.
    Updates the following dialogs to accept the Enter key:
     1. Certificate decryption.
     2. Certificate encryption.
     3. Startup URL.
     4. Search Engine.
    
    BUG=622816
    R=dpapad@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2653023003
    Cr-Commit-Position: refs/heads/master@{#446402}

--- chrome/browser/resources/settings/certificate_manager_page/certificate_password_decryption_dialog.js
+++ chrome/browser/resources/settings/certificate_manager_page/certificate_password_decryption_dialog.js
@@ -49,4 +49,13 @@ Polymer({
               this.fire('certificates-error', error);
             }.bind(this));
   },
+
+  /**
+   * @param {!KeyboardEvent} e
+   * @private
+   */
+  onKeypress_: function(e) {
+    if (e.key == 'Enter' && !this.$.ok.disabled)
+      this.onOkTap_();
+  },
 });
--- chrome/browser/resources/settings/search_engines_page/search_engine_dialog.js
+++ chrome/browser/resources/settings/search_engines_page/search_engine_dialog.js
@@ -93,6 +93,15 @@ Polymer({
   },
 
   /**
+   * @param {!KeyboardEvent} e
+   * @private
+   */
+  onKeypress_: function(e) {
+    if (e.key == 'Enter' && !this.$.actionButton.disabled)
+      this.onActionButtonTap_();
+  },
+
+  /**
    * @param {!Event} event
    * @private
    */
