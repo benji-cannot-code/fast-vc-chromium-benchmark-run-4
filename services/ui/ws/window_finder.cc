364b5914980674f3164448802c1780b70949abce
sky <sky@chromium.org>
2017-01-26 11:21:21 -0800

    mash: changes event dispatch to consider non-client area of all windows
    Previously if a window was marked as not accepting events then events
    would never target it. This means you could never drag a window as
    marked as not accepting events. This patch makes it so the non-client
    area can be targeted for these windows so that they can still be
    dragged and what not.
    
    BUG=685226
    TEST=covered by test.
    R=erg@chromium.org
    
    Review-Url: https://codereview.chromium.org/2655213002
    Cr-Commit-Position: refs/heads/master@{#446384}

--- services/ui/ws/window_finder.cc
+++ services/ui/ws/window_finder.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 namespace ws {
+namespace {
 
 bool IsValidWindowForEvents(ServerWindow* window) {
   ServerWindowCompositorFrameSinkManager* compositor_frame_sink_manager =
@@ -27,20 +28,45 @@ bool IsValidWindowForEvents(ServerWindow* window) {
          compositor_frame_sink_manager->HasCompositorFrameSink();
 }
 
-ServerWindow* FindDeepestVisibleWindowForEvents(ServerWindow* window,
-                                                gfx::Point* location) {
-  if (!window->can_accept_events())
-    return nullptr;
+bool IsLocationInNonclientArea(const ServerWindow* target,
+                               const gfx::Point& location) {
+  if (!target->parent())
+    return false;
+
+  gfx::Rect client_area(target->bounds().size());
+  client_area.Inset(target->client_area());
+  if (client_area.Contains(location))
+    return false;
+
+  for (const auto& rect : target->additional_client_areas()) {
+    if (rect.Contains(location))
+      return false;
+  }
+
+  return true;
+}
+
+bool FindDeepestVisibleWindowForEventsImpl(ServerWindow* window,
+                                           const gfx::Point& location,
+                                           DeepestWindow* deepest_window) {
+  if (!window->can_accept_events()) {
+    if (!IsLocationInNonclientArea(window, location) ||
+        !IsValidWindowForEvents(window)) {
+      return false;
+    }
+    deepest_window->window = window;
+    deepest_window->in_non_client_area = true;
+    return true;
+  }
 
   const ServerWindow::Windows& children = window->children();
-  const gfx::Point original_location = *location;
   for (ServerWindow* child : base::Reversed(children)) {
-    if (!child->visible() || !child->can_accept_events())
+    if (!child->visible())
       continue;
 
     // TODO(sky): support transform.
-    gfx::Point location_in_child(original_location.x() - child->bounds().x(),
-                                 original_location.y() - child->bounds().y());
+    gfx::Point location_in_child(location.x() - child->bounds().x(),
+                                 location.y() - child->bounds().y());
     gfx::Rect child_bounds(child->bounds().size());
     child_bounds.Inset(-child->extended_hit_test_region().left(),
                        -child->extended_hit_test_region().top(),
@@ -52,12 +78,28 @@ ServerWindow* FindDeepestVisibleWindowForEvents(ServerWindow* window,
       continue;
     }
 
-    *location = location_in_child;
-    ServerWindow* result = FindDeepestVisibleWindowForEvents(child, location);
-    if (result)
-      return result;
+    if (FindDeepestVisibleWindowForEventsImpl(child, location_in_child,
+                                              deepest_window)) {
+      return true;
+    }
   }
-  return IsValidWindowForEvents(window) ? window : nullptr;
+
+  if (!IsValidWindowForEvents(window))
+    return false;
+
+  deepest_window->window = window;
+  deepest_window->in_non_client_area =
+      IsLocationInNonclientArea(window, location);
+  return true;
+}
+
+}  // namespace
+
+DeepestWindow FindDeepestVisibleWindowForEvents(ServerWindow* root_window,
+                                                const gfx::Point& location) {
+  DeepestWindow result;
+  FindDeepestVisibleWindowForEventsImpl(root_window, location, &result);
+  return result;
 }
 
 gfx::Transform GetTransformToWindow(ServerWindow* window) {
