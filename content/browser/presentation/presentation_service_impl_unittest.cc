e8fc3fdfc1e9f0270bb7c58eafa06be56e9aa492
imcheng <imcheng@chromium.org>
2017-01-26 14:01:52 -0800

    [Presentation API] Fix race condition in which state change is lost.
    Currently there is a race condition between the PresentationConnection
    being returned to PresentationDispatcher on a successful start() or
    reconnect(), and PresentationDispatcher being notified by the browser
    that the state change has become connected.
    
    Although PresentationServiceImpl issues the state change strictly after
    executing the callback returning the PresentationConnection, it appears
    that they are using distinct independent message pipes, which do not
    guarantee order of arrival between these two messages. If the state
    change message arrives first, then it will be lost due to the
    connection not yet registered with Blink at that point in time.
    
    The fix here is to change state to connected in Blink, after the
    PresentationConnection is registered.
    
    This fix needs to be merged back to 57.
    
    Additional test assertions will be in a followup patch.
    
    BUG=684664
    
    Review-Url: https://codereview.chromium.org/2653683006
    Cr-Commit-Position: refs/heads/master@{#446465}

--- content/browser/presentation/presentation_service_impl_unittest.cc
+++ content/browser/presentation/presentation_service_impl_unittest.cc
@@ -545,7 +545,7 @@ TEST_F(PresentationServiceImplTest, SetDefaultPresentationUrls) {
 }
 
 TEST_F(PresentationServiceImplTest,
-       ListenForConnectionStateChangeAndChangeState) {
+       ListenForConnectionStateChange) {
   content::PresentationSessionInfo connection(presentation_url1_,
                                               kPresentationId);
   content::PresentationConnectionStateChangedCallback state_changed_cb;
@@ -556,11 +556,7 @@ TEST_F(PresentationServiceImplTest,
 
   EXPECT_CALL(mock_delegate_, ListenForConnectionStateChange(_, _, _, _))
       .WillOnce(SaveArg<3>(&state_changed_cb));
-  EXPECT_CALL(mock_client_,
-              OnConnectionStateChanged(
-                  SessionInfoEquals(ByRef(presentation_connection)),
-                  blink::mojom::PresentationConnectionState::CONNECTED));
-  service_impl_->ListenForConnectionStateChangeAndChangeState(connection);
+  service_impl_->ListenForConnectionStateChange(connection);
 
   {
     base::RunLoop run_loop;
@@ -581,7 +577,7 @@ TEST_F(PresentationServiceImplTest, ListenForConnectionClose) {
   content::PresentationConnectionStateChangedCallback state_changed_cb;
   EXPECT_CALL(mock_delegate_, ListenForConnectionStateChange(_, _, _, _))
       .WillOnce(SaveArg<3>(&state_changed_cb));
-  service_impl_->ListenForConnectionStateChangeAndChangeState(connection);
+  service_impl_->ListenForConnectionStateChange(connection);
 
   // Trigger connection close. It should be propagated back up to
   // |mock_client_|.
