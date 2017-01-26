9d33af47497c91ed2263541bcf57317e0c51d675
waltercacau <waltercacau@chromium.org>
2017-01-26 09:39:04 -0800

    Adding/fixing tests and enabling optimized single payment app flow.
    This feature is controlled by
    chrome://flags#enable-web-payments-single-app-ui-skip
    
    BUG=675773
    
    Review-Url: https://codereview.chromium.org/2654073004
    Cr-Commit-Position: refs/heads/master@{#446366}

--- chrome/test/data/payments/bobpay_ui_skip.js
+++ chrome/test/data/payments/bobpay_ui_skip.js
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright 2016 The Chromium Authors. All rights reserved.
+ * Copyright 2017 The Chromium Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
@@ -8,13 +8,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* global PaymentRequest:false */
 
 /**
- * Launches the PaymentRequest UI with Bob Pay as one of multiple payment
- * methods.
+ * Launches the PaymentRequest UI with Bob Pay as the only payment method.
+ *
+ * <p>When the developer chooses a single payment method and requests no other
+ * data (no shipping, no email, no phone, ...), Payment Request will apply
+ * the UI skip optimization, skipping its own UI enterily and going directly to
+ * Bob Pay.
  */
 function buy() {  // eslint-disable-line no-unused-vars
   try {
     new PaymentRequest(
-        [{supportedMethods: ['https://bobpay.com', 'https://alicepay.com']}],
+        [{supportedMethods: ['https://bobpay.com']}],
         {total: {label: 'Total', amount: {currency: 'USD', value: '5.00'}}})
         .show()
         .then(function(resp) {
