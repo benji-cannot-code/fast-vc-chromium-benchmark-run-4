62edc52e884a9508c77768f90b398959f9901b73
xhwang <xhwang@chromium.org>
2017-01-25 17:19:00 -0800

    media: Pass CdmContext in decoder reinitialization
    Fix DecoderStream::ReinitializeDecoder to always pass in the CdmContext.
    
    Also added unittests to cover the case where neither DecryptionVideoDecoder nor
    DecryptingDemuxerStream is selected for encrypted config, but another
    VideoDecoder that supports encrypted config is selected, initialized, and
    reinitialized. In practice, this could be the case when the CDM doesn't support
    the Decryptor interface, e.g. MediaDrmBridge on Android.
    
    BUG=679095
    
    Review-Url: https://codereview.chromium.org/2650823006
    Cr-Commit-Position: refs/heads/master@{#446190}

--- media/filters/fake_video_decoder.h
+++ media/filters/fake_video_decoder.h
@@ -40,6 +40,9 @@ class FakeVideoDecoder : public VideoDecoder {
                    const BytesDecodedCB& bytes_decoded_cb);
   ~FakeVideoDecoder() override;
 
+  // Enables encrypted config supported. Must be called before Initialize().
+  void EnableEncryptedConfigSupport();
+
   // VideoDecoder implementation.
   std::string GetDisplayName() const override;
   void Initialize(const VideoDecoderConfig& config,
@@ -99,6 +102,8 @@ class FakeVideoDecoder : public VideoDecoder {
   const size_t decoding_delay_;
   const int max_parallel_decoding_requests_;
   BytesDecodedCB bytes_decoded_cb_;
+
+  bool supports_encrypted_config_ = false;
 
   State state_;
 
