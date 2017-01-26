921eb278e7c38d50702efe81c1a388492f2a6edd
lukasza <lukasza@chromium.org>
2017-01-26 07:04:22 -0800

    Prepending of "Get" prefix to interpolationTypes() method.
    BUG=582312
    
    Review-Url: https://codereview.chromium.org/2654843006
    Cr-Commit-Position: refs/heads/master@{#446327}

--- tools/clang/rewrite_to_chrome_style/RewriteToChromeStyle.cpp
+++ tools/clang/rewrite_to_chrome_style/RewriteToChromeStyle.cpp
@@ -659,6 +659,7 @@ bool ShouldPrefixFunctionName(const std::string& old_method_name) {
       "image",
       "inputMethodController",
       "inputType",
+      "interpolationTypes",
       "layout",
       "layoutBlock",
       "layoutObject",
