93798b6a2ed2c349e0e92b4fc6f90774f0425160
lindsayw <lindsayw@chromium.org>
2017-01-27 12:47:46 -0800

    Add a11y test coverage for the view and edit pages of autofill.
    testAccessibilityOnAutofillProfileViewPage
    testAccessibilityOnAutofillProfileEditPage
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2660523004
    Cr-Commit-Position: refs/heads/master@{#446759}

--- ios/chrome/browser/ui/settings/autofill_settings_egtest.mm
+++ ios/chrome/browser/ui/settings/autofill_settings_egtest.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/tools_menu/tools_menu_view_controller.h"
 #include "ios/chrome/grit/ios_strings.h"
 #include "ios/chrome/test/app/web_view_interaction_test_util.h"
+#include "ios/chrome/test/earl_grey/accessibility_util.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
@@ -219,6 +220,28 @@ void ClearCountryValue() {
                                  expectation.expected_result])]
         assertWithMatcher:grey_notNil()];
   }
+
+  [self exitSettingsMenu];
+}
+
+// Test that the page for viewing autofill profile details is accessible.
+- (void)testAccessibilityOnAutofillProfileViewPage {
+  [self loadAndSubmitTheForm];
+  [self openEditAddress:@"George Washington, 1600 Pennsylvania Ave NW"];
+  chrome_test_util::VerifyAccessibilityForCurrentScreen();
+
+  [self exitSettingsMenu];
+}
+
+// Test that the page for editing autofill profile details is accessible.
+- (void)testAccessibilityOnAutofillProfileEditPage {
+  [self loadAndSubmitTheForm];
+  [self openEditAddress:@"George Washington, 1600 Pennsylvania Ave NW"];
+  // Switch on edit mode.
+  [[EarlGrey selectElementWithMatcher:ButtonWithAccessibilityLabelId(
+                                          IDS_IOS_NAVIGATION_BAR_EDIT_BUTTON)]
+      performAction:grey_tap()];
+  chrome_test_util::VerifyAccessibilityForCurrentScreen();
 
   [self exitSettingsMenu];
 }
