4361884ad09a896ee5f3e93c2a1fe8dc7c0d3344
alito <alito@chromium.org>
2017-01-26 18:49:16 -0800

    Add a model for the settings reset prompt.
    This patch introduces the SettingsResetPromptModel class.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2632403003
    Cr-Commit-Position: refs/heads/master@{#446553}

---
+++ chrome/browser/safe_browsing/settings_reset_prompt/mock_settings_reset_prompt_config.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SAFE_BROWSING_SETTINGS_RESET_PROMPT_MOCK_SETTINGS_RESET_PROMPT_CONFIG_H_
+#define CHROME_BROWSER_SAFE_BROWSING_SETTINGS_RESET_PROMPT_MOCK_SETTINGS_RESET_PROMPT_CONFIG_H_
+
+#include "chrome/browser/safe_browsing/settings_reset_prompt/settings_reset_prompt_config.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "url/gurl.h"
+
+namespace safe_browsing {
+
+class MockSettingsResetPromptConfig : public SettingsResetPromptConfig {
+ public:
+  MockSettingsResetPromptConfig();
+  ~MockSettingsResetPromptConfig() override;
+
+  MOCK_CONST_METHOD1(UrlToResetDomainId, int(const GURL& URL));
+};
+
+}  // namespace safe_browsing
+
+#endif  // CHROME_BROWSER_SAFE_BROWSING_SETTINGS_RESET_PROMPT_MOCK_SETTINGS_RESET_PROMPT_CONFIG_H_
