9ebf50152f6f9afdff0c35540f9c351e3e3bfd43
tbarzic <tbarzic@chromium.org>
2017-01-26 20:30:44 -0800

    chrome.audio API: treat mute as system wide property
    As part of audio API cleanup, simplify handling of mute flag.
    Current implementation has few problems:
    * Setting/getting mute state is done per device, even though mute is
      system property (on the other hand mute state listeners are not
      bound to a specific device).
    * Muting input or output is not intuitive - one would have to
      find an active device and set it's mute property - this would also
      set mute for all remaining active devices.
    
    This CL fixes these problems by decoupling mute state setter/getter
    from particular device properties, and bounding states to stream type.
    
    Additionally, StreamType enum is introduced - the type specifies type
    of stream an audio device supports (input or output) and is intended
    to replace existing isInput booleans used across the API.
    
    Existing methods for managing mute state are kept around, but will not
    be exposed to stable channel once the API is opened up (modulo
    current API whitelist).
    
    BUG=680358
    
    Review-Url: https://codereview.chromium.org/2578473002
    Cr-Commit-Position: refs/heads/master@{#446592}

--- extensions/test/data/api_test/audio/output_mute_change/background.js
+++ extensions/test/data/api_test/audio/output_mute_change/background.js
@@ -3,15 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-chrome.test.runTests([
-  function waitForMuteChangedEventTests() {
-    chrome.test.listenOnce(
-        chrome.audio.OnMuteChanged,
-        function(isInput, isMuted) {
-          chrome.test.assertFalse(isInput);
-          chrome.test.assertFalse(isMuted);
-        });
-  }
-]);
+chrome.test.runTests([function waitForMuteChangedEventTests() {
+  chrome.test.listenOnce(chrome.audio.onMuteChanged, function(evt) {
+    chrome.test.assertEq('OUTPUT', evt.streamType);
+    chrome.test.assertFalse(evt.isMuted);
+  });
+}]);
 
 chrome.test.sendMessage('loaded');
