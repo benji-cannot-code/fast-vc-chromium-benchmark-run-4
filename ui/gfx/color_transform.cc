84750386ffb28564e3c079803f55e6a04e5329c3
hubbe <hubbe@chromium.org>
2017-01-27 15:49:13 -0800

    Add LINEAR_HDR so that the linear scRGB colorspace will be HDR
    BUG=682416
    
    Review-Url: https://codereview.chromium.org/2641143004
    Cr-Commit-Position: refs/heads/master@{#446837}

--- ui/gfx/color_transform.cc
+++ ui/gfx/color_transform.cc
@@ -202,6 +202,7 @@ GFX_EXPORT float FromLinear(ColorSpace::TransferID id, float v) {
     }
 
     case ColorSpace::TransferID::LINEAR:
+    case ColorSpace::TransferID::LINEAR_HDR:
       return v;
 
     case ColorSpace::TransferID::LOG:
@@ -331,6 +332,7 @@ GFX_EXPORT float ToLinear(ColorSpace::TransferID id, float v) {
     }
 
     case ColorSpace::TransferID::LINEAR:
+    case ColorSpace::TransferID::LINEAR_HDR:
       return v;
 
     case ColorSpace::TransferID::LOG:
@@ -619,7 +621,12 @@ class ColorTransformMatrix : public ColorTransformInternal {
 class ColorTransformFromLinear : public ColorTransformInternal {
  public:
   explicit ColorTransformFromLinear(ColorSpace::TransferID transfer)
-      : transfer_(transfer) {}
+      : transfer_(transfer) {
+    // Map LINEAR_HDR to LINEAR for optimizations.
+    if (transfer_ == ColorSpace::TransferID::LINEAR_HDR) {
+      transfer_ = ColorSpace::TransferID::LINEAR;
+    }
+  }
   bool Prepend(ColorTransformInternal* prev) override {
     return prev->Join(*this);
   }
@@ -642,7 +649,12 @@ class ColorTransformFromLinear : public ColorTransformInternal {
 class ColorTransformToLinear : public ColorTransformInternal {
  public:
   explicit ColorTransformToLinear(ColorSpace::TransferID transfer)
-      : transfer_(transfer) {}
+      : transfer_(transfer) {
+    // Map LINEAR_HDR to LINEAR for optimizations.
+    if (transfer_ == ColorSpace::TransferID::LINEAR_HDR) {
+      transfer_ = ColorSpace::TransferID::LINEAR;
+    }
+  }
 
   bool Prepend(ColorTransformInternal* prev) override {
     return prev->Join(*this);
