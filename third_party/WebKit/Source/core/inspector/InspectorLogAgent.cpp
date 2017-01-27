1395c5de3cc6becaa5e7b583a36a544e2b4f8b6b
pfeldman <pfeldman@chromium.org>
2017-01-27 14:40:54 -0800

    DevTools: render console level filter as drop box.
    BUG=682521
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2650543002
    Cr-Commit-Position: refs/heads/master@{#446802}

--- third_party/WebKit/Source/core/inspector/InspectorLogAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorLogAgent.cpp
@@ -213,7 +213,7 @@ void InspectorLogAgent::reportLongLayout(double duration) {
       String::format("Forced reflow while executing JavaScript took %ldms",
                      lround(duration * 1000));
   ConsoleMessage* message = ConsoleMessage::create(
-      ViolationMessageSource, WarningMessageLevel, messageText);
+      ViolationMessageSource, VerboseMessageLevel, messageText);
   consoleMessageAdded(message);
 }
 
@@ -222,7 +222,7 @@ void InspectorLogAgent::reportGenericViolation(PerformanceMonitor::Violation,
                                                double time,
                                                SourceLocation* location) {
   ConsoleMessage* message = ConsoleMessage::create(
-      ViolationMessageSource, WarningMessageLevel, text, location->clone());
+      ViolationMessageSource, VerboseMessageLevel, text, location->clone());
   consoleMessageAdded(message);
 };
 
--- third_party/WebKit/Source/devtools/front_end/sdk/ConsoleModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/ConsoleModel.js
@@ -274,7 +274,7 @@ SDK.ConsoleMessage = class {
       workerId) {
     this._target = target;
     this.source = source;
-    this.level = level;
+    this.level = /** @type {?SDK.ConsoleMessage.MessageLevel} */ (level);
     this.messageText = messageText;
     this.type = type || SDK.ConsoleMessage.MessageType.Log;
     /** @type {string|undefined} */
@@ -520,6 +520,19 @@ SDK.ConsoleMessage.MessageLevel = {
   Error: 'error'
 };
 
+/**
+ * @param {!SDK.ConsoleMessage.MessageLevel} level
+ * @return {number}
+ */
+SDK.ConsoleMessage.MessageLevel.ordinal = function(level) {
+  if (level === SDK.ConsoleMessage.MessageLevel.Verbose)
+    return 0;
+  if (level === SDK.ConsoleMessage.MessageLevel.Info)
+    return 1;
+  if (level === SDK.ConsoleMessage.MessageLevel.Warning)
+    return 2;
+  return 3;
+};
 
 /**
  * @implements {Protocol.LogDispatcher}
