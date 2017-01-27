90a8b28cbbf3a866cbaeb41462e2cbe37a0d052f
khorimoto <khorimoto@chromium.org>
2017-01-27 14:11:11 -0800

    [CrOS Tether] Refactor BluetoothLowEnergyWeaveClientConnection to take a bluetooth address (as a string) in its constructor.
    This change is needed because tether hosts in CryptAuth do not necessarily sync their Bluetooth addresses, which means that their associated RemoteDevice objects may not have Bluetooth addresses present. Instead, chromeos::tether::BleScanner uses an EID scheme to discover devices, whose addresses are passed to the constructor of BluetoothLowEnergyWeaveClientConnection. Bluetooth addresses are used rather than BluetoothDevice* pointers themselves because the BluetoothAdapter documentation states that BluetoothDevice* objects should not be cached.
    
    BUG=672263
    
    Review-Url: https://codereview.chromium.org/2656023002
    Cr-Commit-Position: refs/heads/master@{#446786}

--- chromeos/components/tether/ble_scanner.h
+++ chromeos/components/tether/ble_scanner.h
@@ -30,7 +30,7 @@ class BleScanner : public device::BluetoothAdapter::Observer {
   class Observer {
    public:
     virtual void OnReceivedAdvertisementFromDevice(
-        const device::BluetoothDevice* bluetooth_device,
+        const std::string& device_address,
         cryptauth::RemoteDevice remote_device) = 0;
   };
 
