de75213dd587788df2604055ae2fd64d56b77bd8
jam <jam@chromium.org>
2017-01-26 16:59:57 -0800

    Convert UberUI to use the new navigation callbacks.
    BUG=682002
    
    Review-Url: https://codereview.chromium.org/2651793009
    Cr-Commit-Position: refs/heads/master@{#446510}

--- chrome/browser/ui/webui/uber/uber_ui.cc
+++ chrome/browser/ui/webui/uber/uber_ui.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/navigation_entry.h"
+#include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/notification_source.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
@@ -145,10 +146,12 @@ SubframeLogger::SubframeLogger(content::WebContents* contents)
 
 SubframeLogger::~SubframeLogger() {}
 
-void SubframeLogger::DidCommitProvisionalLoadForFrame(
-    content::RenderFrameHost* render_frame_host,
-    const GURL& url,
-    ui::PageTransition transition_type) {
+void SubframeLogger::DidFinishNavigation(
+    content::NavigationHandle* navigation_handle) {
+  if (!navigation_handle->HasCommitted())
+    return;
+
+  const GURL& url = navigation_handle->GetURL();
   if (url == chrome::kChromeUIExtensionsFrameURL ||
       url == chrome::kChromeUIHelpFrameURL ||
       url == chrome::kChromeUIHistoryFrameURL ||
