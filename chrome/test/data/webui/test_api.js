0dd8a6a2069e46d1333090ab6f2b414714fa89e7
mbrunson <mbrunson@chromium.org>
2017-01-26 17:36:40 -0800

    bluetooth: Add control for reading/writing of characteristics to internals page.
    Adds ValueControl to allow a user to read values from and write values to
        characteristics.
    Adds Permission enum to device.mojom for allowing reading/writing based on
        permissions/properties specified by the characteristic.
    Adds WriteValueForCharacteristic and ReadValueForCharacteristic to Device Mojo
        service.
    
    GIF: https://goo.gl/photos/izDvvyH7d56Uh4kE7
    
    BUG=651282
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2627243002
    Cr-Commit-Position: refs/heads/master@{#446526}

--- chrome/test/data/webui/test_api.js
+++ chrome/test/data/webui/test_api.js
@@ -943,6 +943,18 @@ var testing = {};
   }
 
   /**
+   * @param {Function} testFunction
+   * @param {Function=|string=|RegExp=} opt_expected The expected Error
+   *     constructor, partial or complete error message string, or RegExp to
+   *     test the error message.
+   * @param {string=} opt_message Additional error message.
+   * @throws {Error}
+   */
+  function assertThrows(testFunction, opt_expected, opt_message) {
+    chai.assert.throws(testFunction, opt_expected, opt_message);
+  }
+
+  /**
    * Run an accessibility audit on the current page state.
    * @type {Function}
    * @param {Array} a11yResults
@@ -1686,6 +1698,7 @@ var testing = {};
     exports.assertLT = assertLT;
     exports.assertNotEquals = assertNotEquals;
     exports.assertNotReached = assertNotReached;
+    exports.assertThrows = assertThrows;
   }
 
   /**
@@ -1704,6 +1717,7 @@ var testing = {};
     exports.expectNotEquals = createExpect(assertNotEquals);
     exports.expectNotReached = createExpect(assertNotReached);
     exports.expectAccessibilityOk = createExpect(assertAccessibilityOk);
+    exports.expectThrows = createExpect(assertThrows);
   }
 
   /**
