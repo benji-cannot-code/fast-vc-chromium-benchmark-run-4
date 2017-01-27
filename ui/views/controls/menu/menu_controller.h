657746f138ed438a954b9bd4b6a0a62e5f872dbd
jonross <jonross@chromium.org>
2017-01-26 16:15:48 -0800

    Fix MenuRunner Releasing
    It is possible for MenuRunner to be released while there is no currently active
    MenuController. However the MenuController which it created has not yet been
    destroyed.
    
    It is possible for there to be multiple non-destroyed MenuControllers. Due to
    this MenuController::GetActiveInstance is not an appropriate way to determine if
    the controller you are working with has been destroyed or not.
    
    This change updates MenuController to begin providing a WeakPtr of itself. This
    allows its internal code to detect when delegates have deleted it in response
    to callbacks. This also allows MenuRunner to track the lifetime of the
    controller is created.
    
    This change also updates MenuRunner to make sure that it destroys the
    controller it created when it is being released.
    
    TEST=MenuRunnerImplTest.MenuRunnerDestroyedWithNoActiveController
    BUG=683087
    
    Review-Url: https://codereview.chromium.org/2654093005
    Cr-Commit-Position: refs/heads/master@{#446498}

--- ui/views/controls/menu/menu_controller.h
+++ ui/views/controls/menu/menu_controller.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "base/timer/timer.h"
 #include "build/build_config.h"
 #include "ui/events/event.h"
@@ -57,7 +58,9 @@ class MenuControllerTestApi;
 // MenuController is used internally by the various menu classes to manage
 // showing, selecting and drag/drop for menus. All relevant events are
 // forwarded to the MenuController from SubmenuView and MenuHost.
-class VIEWS_EXPORT MenuController : public WidgetObserver {
+class VIEWS_EXPORT MenuController
+    : public base::SupportsWeakPtr<MenuController>,
+      public WidgetObserver {
  public:
   // Enumeration of how the menu should exit.
   enum ExitType {
@@ -186,6 +189,10 @@ class VIEWS_EXPORT MenuController : public WidgetObserver {
 
   // Only used for testing.
   bool IsCancelAllTimerRunningForTest();
+
+  // Only used for testing. Clears |state_| and |pending_state_| without
+  // notifying any menu items.
+  void ClearStateForTest();
 
   // Only used for testing.
   static void TurnOffMenuSelectionHoldForTest();
