790d1c2d99f8ab457ef0f8a1cec9ed98b4bbd255
kerrnel <kerrnel@chromium.org>
2017-01-26 21:22:26 -0800

    Add "Relaunch Chrome" dialog to Mac Views.
    This implements the Relaunch Chrome dialog as a Views based dialog on the mac
    platform.
    
    BUG=684167
    TEST=./out/Default/browser_tests --gtest_filter=BrowserDialogTest.Invoke
    --interactive --dialog=UpdateRecommendedDialogTest.InvokeDialog_default
    
    Review-Url: https://codereview.chromium.org/2652823003
    Cr-Commit-Position: refs/heads/master@{#446601}

--- chrome/browser/ui/cocoa/browser_window_cocoa.mm
+++ chrome/browser/ui/cocoa/browser_window_cocoa.mm
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_command_controller.h"
 #include "chrome/browser/ui/browser_commands.h"
+#include "chrome/browser/ui/browser_dialogs.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window_state.h"
 #import "chrome/browser/ui/cocoa/autofill/save_card_bubble_view_bridge.h"
@@ -67,6 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_system.h"
 #include "extensions/common/constants.h"
 #include "ui/base/l10n/l10n_util_mac.h"
+#include "ui/base/material_design/material_design_controller.h"
 #include "ui/events/keycodes/keyboard_codes.h"
 #include "ui/gfx/geometry/rect.h"
 
@@ -526,7 +528,11 @@ void BrowserWindowCocoa::UpdateAlertState(TabAlertState alert_state) {
 }
 
 void BrowserWindowCocoa::ShowUpdateChromeDialog() {
-  restart_browser::RequestRestart(window());
+  if (ui::MaterialDesignController::IsSecondaryUiMaterial()) {
+    chrome::ShowUpdateChromeDialogViews(GetNativeWindow());
+  } else {
+    restart_browser::RequestRestart(window());
+  }
 }
 
 void BrowserWindowCocoa::ShowBookmarkBubble(const GURL& url,
