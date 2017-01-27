86a2bc472b076bbabf6e10c710ef51cafcf868f6
mtomasz <mtomasz@chromium.org>
2017-01-26 19:36:26 -0800

    Reenable disabled component extensions with profile resetter.
    Sice disabled extensions are synced and component extensions do not
    have a UI for reenabling them, once disabled will stay disabled forever.
    
    It happened that for some reason some component extensions were
    disabled. The reason is unknown, but at least users will be able to
    recover from this situation by using the profile resetter.
    
    Note, that we allow to disable component extensions programatically,
    which was used at least for the Hotword component extension.
    
    TEST=Disable a component extension programatically, then reset the
         profile in chrome://settings.
    BUG=680429
    
    Review-Url: https://codereview.chromium.org/2647783003
    Cr-Commit-Position: refs/heads/master@{#446575}

--- chrome/browser/profile_resetter/profile_resetter.cc
+++ chrome/browser/profile_resetter/profile_resetter.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include <string>
+#include <vector>
 
 #include "base/macros.h"
 #include "base/synchronization/cancellation_flag.h"
@@ -38,8 +39,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search_engines/template_url_prepopulate_data.h"
 #include "components/search_engines/template_url_service.h"
 #include "content/public/browser/browser_thread.h"
+#include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_system.h"
 #include "extensions/browser/management_policy.h"
+#include "extensions/common/extension_id.h"
+#include "extensions/common/manifest.h"
 
 #if defined(OS_WIN)
 #include "base/base_paths.h"
@@ -271,6 +275,24 @@ void ProfileResetter::ResetExtensions() {
       extensions::ExtensionSystem::Get(profile_)->extension_service();
   DCHECK(extension_service);
   extension_service->DisableUserExtensionsExcept(brandcode_extensions);
+
+  // Reenable all disabled external component extensions.
+  // BrandcodedDefaultSettings does not contain information about component
+  // extensions, so fetch them from the existing registry. This may be not very
+  // robust, as the profile resetter may be invoked when the registry is in some
+  // iffy state. However, we can't enable an extension which is not in the
+  // registry anyway.
+  extensions::ExtensionRegistry* extension_registry =
+      extensions::ExtensionRegistry::Get(profile_);
+  DCHECK(extension_registry);
+  std::vector<extensions::ExtensionId> extension_ids_to_reenable;
+  for (const auto& extension : extension_registry->disabled_extensions()) {
+    if (extension->location() == extensions::Manifest::EXTERNAL_COMPONENT)
+      extension_ids_to_reenable.push_back(extension->id());
+  }
+  for (const auto& extension_id : extension_ids_to_reenable) {
+    extension_service->EnableExtension(extension_id);
+  }
 
   MarkAsDone(EXTENSIONS);
 }
