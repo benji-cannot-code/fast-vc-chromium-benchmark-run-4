bbd41a6cb70ae2cb6be02bcf22caeba3e0bed3b5
olivierrobin <olivierrobin@chromium.org>
2017-01-27 12:35:26 -0800

    Pass the correct webState to the nativeContent.
    If an offline content is created in a new tab, currentWebState in the
    BVC will return the webState from the original tab (created tab is in
    background).
    
    Pass the webState from the webController instead.
    
    BUG=685969
    
    Review-Url: https://codereview.chromium.org/2655463014
    Cr-Commit-Position: refs/heads/master@{#446753}

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -2915,7 +2915,8 @@ class BrowserBookmarkModelBridge : public bookmarks::BookmarkModelObserver {
          host == kChromeUITermsHost || host == kChromeUIOfflineHost;
 }
 
-- (id<CRWNativeContent>)controllerForURL:(const GURL&)url {
+- (id<CRWNativeContent>)controllerForURL:(const GURL&)url
+                                webState:(web::WebState*)webState {
   DCHECK(url.SchemeIs(kChromeUIScheme));
 
   id<CRWNativeContent> nativeController = nil;
@@ -2968,7 +2969,7 @@ class BrowserBookmarkModelBridge : public bookmarks::BookmarkModelObserver {
     StaticHtmlNativeContent* staticNativeController =
         [[[OfflinePageNativeContent alloc] initWithLoader:self
                                              browserState:_browserState
-                                                 webState:[self currentWebState]
+                                                 webState:webState
                                                       URL:url] autorelease];
     [self setOverScrollActionControllerToStaticNativeContent:
               staticNativeController];
