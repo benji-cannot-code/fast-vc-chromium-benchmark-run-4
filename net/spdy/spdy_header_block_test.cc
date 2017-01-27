6e5af917af893fbe40cbde97bc173a456d51becc
diannahu <diannahu@chromium.org>
2017-01-27 15:06:40 -0800

    Add std:: to STL types.
    This CL lands server change 144236053 by bnouri.
    
    BUG=488484
    
    Review-Url: https://codereview.chromium.org/2658153003
    Cr-Commit-Position: refs/heads/master@{#446813}

--- net/spdy/spdy_header_block_test.cc
+++ net/spdy/spdy_header_block_test.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using base::StringPiece;
 using std::make_pair;
 using std::string;
-using std::vector;
 using ::testing::ElementsAre;
 
 namespace net {
@@ -198,7 +197,7 @@ TEST(SpdyHeaderBlockTest, AppendHeaders) {
 }
 
 TEST(JoinTest, JoinEmpty) {
-  vector<StringPiece> empty;
+  std::vector<StringPiece> empty;
   StringPiece separator = ", ";
   char buf[10] = "";
   size_t written = Join(buf, empty, separator);
@@ -206,7 +205,7 @@ TEST(JoinTest, JoinEmpty) {
 }
 
 TEST(JoinTest, JoinOne) {
-  vector<StringPiece> v = {"one"};
+  std::vector<StringPiece> v = {"one"};
   StringPiece separator = ", ";
   char buf[15];
   size_t written = Join(buf, v, separator);
@@ -215,7 +214,7 @@ TEST(JoinTest, JoinOne) {
 }
 
 TEST(JoinTest, JoinMultiple) {
-  vector<StringPiece> v = {"one", "two", "three"};
+  std::vector<StringPiece> v = {"one", "two", "three"};
   StringPiece separator = ", ";
   char buf[15];
   size_t written = Join(buf, v, separator);
