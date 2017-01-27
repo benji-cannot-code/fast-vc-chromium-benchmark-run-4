4414445fb4c99c5a4515633e9c75d614b8c829eb
manzagop <manzagop@chromium.org>
2017-01-27 14:29:48 -0800

    Add the chrome module's debug identifier to the stability file
    This enables basic symbolization.
    
    BUG=620813
    
    Review-Url: https://codereview.chromium.org/2635553002
    Cr-Commit-Position: refs/heads/master@{#446795}

--- components/browser_watcher/stability_data_names.h
+++ components/browser_watcher/stability_data_names.h
@@ -11,9 +11,6 @@ namespace browser_watcher {
 // Alphabetical list of stability data names.
 extern const char kStabilityChannel[];
 extern const char kStabilityExecutionPhase[];
-extern const char kStabilityModuleAddress[];
-extern const char kStabilityModuleSize[];
-extern const char kStabilityModuleTimestamp[];
 extern const char kStabilityPlatform[];
 extern const char kStabilityProduct[];
 extern const char kStabilitySpecialBuild[];
