6bd4033f5812699c4261459203bc5181abc59247
grt <grt@chromium.org>
2017-01-27 09:04:05 -0800

    Handle spaces in the name of the mini_installer.
    BUG=682277
    R=bcwhite@chromium.org
    
    Review-Url: https://codereview.chromium.org/2655473002
    Cr-Commit-Position: refs/heads/master@{#446694}

---
+++ chrome/installer/mini_installer/mini_installer_unittest.cc
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/installer/mini_installer/mini_installer.h"
+
+#include "chrome/installer/mini_installer/configuration.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace mini_installer {
+
+TEST(MiniInstallerTest, AppendCommandLineFlags) {
+  static constexpr struct {
+    const wchar_t* command_line;
+    const wchar_t* args;
+  } kData[] = {
+      {L"", L"foo.exe"},
+      {L"mini_installer.exe", L"foo.exe"},
+      {L"mini_installer.exe --verbose-logging", L"foo.exe --verbose-logging"},
+      {L"C:\\Temp\\mini_installer.exe --verbose-logging",
+       L"foo.exe --verbose-logging"},
+      {L"C:\\Temp\\mini_installer --verbose-logging",
+       L"foo.exe --verbose-logging"},
+      {L"\"C:\\Temp\\mini_installer (1).exe\" --verbose-logging",
+       L"foo.exe --verbose-logging"},
+      {L"\"mini_installer.exe\"--verbose-logging",
+       L"foo.exe --verbose-logging"},
+  };
+
+  CommandString buffer;
+
+  for (const auto& data : kData) {
+    buffer.assign(L"foo.exe");
+    AppendCommandLineFlags(data.command_line, &buffer);
+    EXPECT_STREQ(data.args, buffer.get()) << data.command_line;
+  }
+}
+
+}  // namespace mini_installer
