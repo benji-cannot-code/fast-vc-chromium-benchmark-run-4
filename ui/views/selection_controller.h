4b2649b94534edf8eb0ab66374e6803ab16b751a
karandeepb <karandeepb@chromium.org>
2017-01-26 20:12:45 -0800

    MacViews: Select all text on right clicking an unfocused text view.
    When an unfocused Cocoa text view is right clicked, all its text is selected.
    This CL implements this behavior on MacViews for views::Textfield and
    views::Label. A new variable kSelectAllOnRightClickWhenUnfocused is introduced
    to the PlatformStyle class to account for the platform specific differences.
    SelectionController::OnMousePressed is also modified to take the initial focus
    state of its client view as an argument. A test is also added to
    textfield_unittest.cc.
    
    BUG=676296
    
    Review-Url: https://codereview.chromium.org/2650963002
    Cr-Commit-Position: refs/heads/master@{#446588}

--- ui/views/selection_controller.h
+++ ui/views/selection_controller.h
@@ -29,6 +29,13 @@ class SelectionControllerDelegate;
 // TODO(karandeepb): Also make this class handle gesture events.
 class VIEWS_EXPORT SelectionController {
  public:
+  // Describes whether the view managing the delegate was initially focused when
+  // the mouse press was received.
+  enum InitialFocusStateOnMousePress {
+    FOCUSED,
+    UNFOCUSED,
+  };
+
   // |delegate| must be non-null.
   explicit SelectionController(SelectionControllerDelegate* delegate);
 
@@ -36,7 +43,9 @@ class VIEWS_EXPORT SelectionController {
   // the event has already been handled by the |delegate_|. If |handled| is
   // true, the mouse event is just used to update the internal state without
   // updating the state of the associated RenderText instance.
-  bool OnMousePressed(const ui::MouseEvent& event, bool handled);
+  bool OnMousePressed(const ui::MouseEvent& event,
+                      bool handled,
+                      InitialFocusStateOnMousePress initial_focus_state);
   bool OnMouseDragged(const ui::MouseEvent& event);
   void OnMouseReleased(const ui::MouseEvent& event);
   void OnMouseCaptureLost();
@@ -56,6 +65,9 @@ class VIEWS_EXPORT SelectionController {
 
   // Selects the word at the given |point|.
   void SelectWord(const gfx::Point& point);
+
+  // Selects all the text.
+  void SelectAll();
 
   // Returns the associated render text instance via the |delegate_|.
   gfx::RenderText* GetRenderText();
