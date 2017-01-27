9dd6fab5db489a6808383037f743aba80312bdec
hubbe <hubbe@chromium.org>
2017-01-27 13:04:09 -0800

    Create FP16 LUTs when needed
    When maping HDR data to scRGB, the output can be outside of 0-1.
    Using half-floats neatly solves this problem.
    Also, we need the LUTs to be a bit larger in this case.
    
    BUG=682416
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2648043002
    Cr-Commit-Position: refs/heads/master@{#446766}

---
+++ ui/gfx/half_float.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_GFX_COLOR_HALF_FLOAT_H_
+#define UI_GFX_COLOR_HALF_FLOAT_H_
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "ui/gfx/gfx_export.h"
+
+namespace gfx {
+
+typedef uint16_t HalfFloat;
+
+// Floats are expected to be within +/- 65535.0;
+GFX_EXPORT void FloatToHalfFloat(const float* input,
+                                 HalfFloat* output,
+                                 size_t num);
+};
+#endif
