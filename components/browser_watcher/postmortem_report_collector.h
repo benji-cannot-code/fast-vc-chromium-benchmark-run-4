a36c76e2e9d407451f677730a8c64aee9eb42f24
manzagop <manzagop@chromium.org>
2017-01-27 14:49:31 -0800

    Collect module information from the stability file
    BUG=620813
    
    Review-Url: https://codereview.chromium.org/2649773008
    Cr-Commit-Position: refs/heads/master@{#446808}

--- components/browser_watcher/postmortem_report_collector.h
+++ components/browser_watcher/postmortem_report_collector.h
@@ -79,6 +79,9 @@ class PostmortemReportCollector {
   FRIEND_TEST_ALL_PREFIXES(
       PostmortemReportCollectorCollectionFromGlobalTrackerTest,
       GlobalUserDataCollection);
+  FRIEND_TEST_ALL_PREFIXES(
+      PostmortemReportCollectorCollectionFromGlobalTrackerTest,
+      ModuleCollection);
 
   // Virtual for unittesting.
   virtual std::vector<base::FilePath> GetDebugStateFilePaths(
