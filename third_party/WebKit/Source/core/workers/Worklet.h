d94af0e4e8eb99d7571a176b4fce590ee8760b3d
nhiroki <nhiroki@chromium.org>
2017-01-27 13:43:43 -0800

    Worklet: Move WorkletScriptLoader from core/workers to core/loader
    This CL moves WorkletScriptLoader from core/workers to core/loader because
    clients of Resource classes should be placed under core/loader in terms of
    modularity. This is a simple moving patch and should not change behavior.
    
    BUG=627945
    
    Review-Url: https://codereview.chromium.org/2659033002
    Cr-Commit-Position: refs/heads/master@{#446784}

--- third_party/WebKit/Source/core/workers/Worklet.h
+++ third_party/WebKit/Source/core/workers/Worklet.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/ScriptWrappable.h"
 #include "core/CoreExport.h"
 #include "core/dom/ContextLifecycleObserver.h"
-#include "core/workers/WorkletScriptLoader.h"
+#include "core/loader/WorkletScriptLoader.h"
 #include "platform/heap/Handle.h"
 #include "platform/loader/fetch/ResourceFetcher.h"
 
