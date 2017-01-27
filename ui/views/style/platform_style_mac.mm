c9780160046bb1706d6f4f60d93679e546cb8dd2
Dan Beam <dbeam@chromium.org>
2017-01-27 14:15:36 -0800

    Actually fix Google Chrome Mac compile failure from vector_icon.h #include
    TBR=estade@chromium.org
    BUG=684911
    
    Review-Url: https://codereview.chromium.org/2656983004 .
    Cr-Commit-Position: refs/heads/master@{#446788}

--- ui/views/style/platform_style_mac.mm
+++ ui/views/style/platform_style_mac.mm
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/resources/grit/ui_resources.h"
 #include "ui/views/controls/button/label_button.h"
 #import "ui/views/controls/scrollbar/cocoa_scroll_bar.h"
-#include "ui/views/vector_icons/vector_icons.h"
+#include "ui/views/vector_icons.h"
 
 #import <Cocoa/Cocoa.h>
 
