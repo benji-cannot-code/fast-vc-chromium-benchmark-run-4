20d4e8d90b2176cb2f034eaa937d4d6f80666010
rockot <rockot@chromium.org>
2017-01-27 11:31:50 -0800

    Fix image_decoder unit tests
    Blink initialization was broken at some point and wasn't caught
    because we don't run these tests on the waterfall yet.
    
    BUG=685847
    TBR=ben@chromium.org
    
    Review-Url: https://codereview.chromium.org/2657133003
    Cr-Commit-Position: refs/heads/master@{#446735}

--- services/image_decoder/image_decoder_impl_unittest.cc
+++ services/image_decoder/image_decoder_impl_unittest.cc
@@ -82,6 +82,8 @@ class BlinkInitializer : public blink::Platform {
 
   ~BlinkInitializer() override {}
 
+  blink::WebThread* currentThread() override { return main_thread_.get(); }
+
  private:
   std::unique_ptr<blink::scheduler::WebThreadImplForUtilityThread> main_thread_;
 
