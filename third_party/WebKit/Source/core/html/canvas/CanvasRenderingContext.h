d720c0f066e918c6e024aca542a3acf5dec7d7e1
zakerinasab <zakerinasab@chromium.org>
2017-01-27 12:31:00 -0800

    Add rec-2020 and p3 color spaces to canvas
    This change adds two new color spaces to canvas: linear-rec-2020 and
    linear-p3. It also adds two layout tests that examines if the Skia
    rendering pipeline correctly renders the canvases with these color
    space tags.
    
    We still do not have end-to-end layout tests for these color spaces
    (and also linear-rgb). This issue will be addressed later.
    
    This change also inherits this issue from the previous CL:
    "getImage data had to work around some output format limitations in skia."
    
    BUG=634542
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2656623002
    Cr-Commit-Position: refs/heads/master@{#446751}

--- third_party/WebKit/Source/core/html/canvas/CanvasRenderingContext.h
+++ third_party/WebKit/Source/core/html/canvas/CanvasRenderingContext.h
@@ -53,6 +53,8 @@ enum CanvasColorSpace {
   kLegacyCanvasColorSpace,
   kSRGBCanvasColorSpace,
   kLinearRGBCanvasColorSpace,
+  kRec2020CanvasColorSpace,
+  kP3CanvasColorSpace,
 };
 
 class CORE_EXPORT CanvasRenderingContext
