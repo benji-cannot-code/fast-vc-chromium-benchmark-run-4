5cb11d5222a6edc35d89a410ba0b91aa38d607a7
lanwei <lanwei@chromium.org>
2017-01-27 15:20:55 -0800

    Replace pen actions in pointer event tests with pointerActionSequence
    BUG=680330
    
    Review-Url: https://codereview.chromium.org/2629693002
    Cr-Commit-Position: refs/heads/master@{#446824}

--- content/renderer/gpu/actions_parser.cc
+++ content/renderer/gpu/actions_parser.cc
@@ -34,6 +34,8 @@ SyntheticGestureParams::GestureSourceType ToSyntheticGestureSourceType(
     return SyntheticGestureParams::TOUCH_INPUT;
   else if (source_type == "mouse")
     return SyntheticGestureParams::MOUSE_INPUT;
+  else if (source_type == "pen")
+    return SyntheticGestureParams::PEN_INPUT;
   else
     return SyntheticGestureParams::DEFAULT_INPUT;
 }
@@ -61,7 +63,8 @@ ActionsParser::~ActionsParser() {}
 
 bool ActionsParser::ParsePointerActionSequence() {
   const base::ListValue* pointer_list;
-  if (!pointer_actions_value_->GetAsList(&pointer_list)) {
+  if (!pointer_actions_value_ ||
+      !pointer_actions_value_->GetAsList(&pointer_list)) {
     error_message_ =
         base::StringPrintf("pointer_list is missing or not a list");
     return false;
@@ -107,7 +110,7 @@ bool ActionsParser::ParsePointerActions(const base::DictionaryValue& pointer) {
         base::StringPrintf("source type is missing or not a string");
     return false;
   } else if (source_type != "touch" && source_type != "mouse" &&
-             source_type != "pointer") {
+             source_type != "pen") {
     error_message_ =
         base::StringPrintf("source type is an unsupported input source");
     return false;
