2b163b0b1a7e363bd87b6085d576c27227736bab
jonross <jonross@chromium.org>
2017-01-27 14:31:00 -0800

    New MenuController Checks
    Add additional checks to MenuController and MenuRunnerImpl to help find the root
    cause of a use-after-free.
    
    BUG=683087
    
    Review-Url: https://codereview.chromium.org/2656823008
    Cr-Commit-Position: refs/heads/master@{#446796}

--- ui/views/controls/menu/menu_controller.cc
+++ ui/views/controls/menu/menu_controller.cc
@@ -434,6 +434,12 @@ MenuItemView* MenuController::Run(Widget* parent,
   } else {
     showing_ = true;
 
+    // TODO(jonross): remove after tracking down the cause of
+    // (crbug.com/683087).
+    // If we are not showing we should be shutting down, this could lead to
+    // incorrect delegate states.
+    CHECK(!running_);
+
     if (owner_)
       owner_->RemoveObserver(this);
     owner_ = parent;
@@ -470,6 +476,10 @@ MenuItemView* MenuController::Run(Widget* parent,
   // Make sure Chrome doesn't attempt to shut down while the menu is showing.
   if (ViewsDelegate::GetInstance())
     ViewsDelegate::GetInstance()->AddRef();
+
+  // TODO(jonross): remove after tracking down the cause of (crbug.com/683087).
+  // About to either exit and run async, or nest message loop.
+  running_ = true;
 
   if (async_run_)
     return nullptr;
@@ -519,6 +529,11 @@ void MenuController::Cancel(ExitType type) {
   SetSelection(NULL, SELECTION_UPDATE_IMMEDIATELY | SELECTION_EXIT);
 
   if (!blocking_run_) {
+    // TODO(jonross): remove after tracking down the cause of
+    // (crbug.com/683087).
+    bool nested = delegate_stack_.size() > 1;
+    CHECK(!nested);
+    base::WeakPtr<MenuController> this_ref = AsWeakPtr();
     // If we didn't block the caller we need to notify the menu, which
     // triggers deleting us.
     DCHECK(selected);
@@ -526,6 +541,7 @@ void MenuController::Cancel(ExitType type) {
     delegate_->OnMenuClosed(internal::MenuControllerDelegate::NOTIFY_DELEGATE,
                             selected->GetRootMenuItem(), accept_event_flags_);
     // WARNING: the call to MenuClosed deletes us.
+    CHECK(!this_ref);
     return;
   }
 
@@ -545,6 +561,12 @@ void MenuController::Cancel(ExitType type) {
 
 void MenuController::AddNestedDelegate(
     internal::MenuControllerDelegate* delegate) {
+  // TODO(jonross): remove after tracking down the cause of (crbug.com/683087).
+  for (auto delegates : delegate_stack_) {
+    // Having the same delegate in the stack could cause deletion order issues.
+    CHECK_NE(delegates.first, delegate);
+  }
+
   delegate_stack_.push_back(std::make_pair(delegate, async_run_));
   delegate_ = delegate;
 }
@@ -980,9 +1002,13 @@ int MenuController::OnPerformDrop(SubmenuView* source,
     drop_target = drop_target->GetParentMenuItem();
 
   if (!IsBlockingRun()) {
+    // TODO(jonross): remove after tracking down the cause of
+    // (crbug.com/683087).
+    base::WeakPtr<MenuController> this_ref = AsWeakPtr();
     delegate_->OnMenuClosed(
         internal::MenuControllerDelegate::DONT_NOTIFY_DELEGATE,
         item->GetRootMenuItem(), accept_event_flags_);
+    CHECK(!this_ref);
   }
 
   // WARNING: the call to MenuClosed deletes us.
@@ -1381,6 +1407,7 @@ MenuController::MenuController(bool blocking,
                                internal::MenuControllerDelegate* delegate)
     : blocking_run_(blocking),
       showing_(false),
+      running_(false),
       exit_type_(EXIT_NONE),
       did_capture_(false),
       result_(NULL),
@@ -2659,6 +2686,9 @@ MenuItemView* MenuController::ExitMenuRun() {
 
     showing_ = false;
     did_capture_ = false;
+    // TODO(jonross): remove after tracking down the cause of
+    // (crbug.com/683087).
+    running_ = false;
   }
 
   MenuItemView* result = result_;
