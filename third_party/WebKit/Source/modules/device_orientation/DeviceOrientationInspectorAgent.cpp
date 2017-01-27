30aacd556d3a277685e65d8e82e026a0b35978bc
dgozman <dgozman@chromium.org>
2017-01-26 19:09:06 -0800

    [DevTools] Migrate DeviceOrientationInspectorAgent from Page to LocalFrame.
    This also fixes the crash where we access unexisitng document on frame destruction.
    
    BUG=679310
    
    Review-Url: https://codereview.chromium.org/2647323008
    Cr-Commit-Position: refs/heads/master@{#446561}

--- third_party/WebKit/Source/modules/device_orientation/DeviceOrientationInspectorAgent.cpp
+++ third_party/WebKit/Source/modules/device_orientation/DeviceOrientationInspectorAgent.cpp
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/device_orientation/DeviceOrientationInspectorAgent.h"
 
 #include "core/frame/LocalFrame.h"
-#include "core/page/Page.h"
+#include "core/inspector/InspectedFrames.h"
 #include "modules/device_orientation/DeviceOrientationController.h"
 #include "modules/device_orientation/DeviceOrientationData.h"
 #include "wtf/Assertions.h"
@@ -20,26 +20,20 @@ static const char gamma[] = "gamma";
 static const char overrideEnabled[] = "overrideEnabled";
 }
 
-// static
-DeviceOrientationInspectorAgent* DeviceOrientationInspectorAgent::create(
-    Page* page) {
-  return new DeviceOrientationInspectorAgent(*page);
-}
-
 DeviceOrientationInspectorAgent::~DeviceOrientationInspectorAgent() {}
 
-DeviceOrientationInspectorAgent::DeviceOrientationInspectorAgent(Page& page)
-    : m_page(&page) {}
+DeviceOrientationInspectorAgent::DeviceOrientationInspectorAgent(
+    InspectedFrames* inspectedFrames)
+    : m_inspectedFrames(inspectedFrames) {}
 
 DEFINE_TRACE(DeviceOrientationInspectorAgent) {
-  visitor->trace(m_page);
+  visitor->trace(m_inspectedFrames);
   InspectorBaseAgent::trace(visitor);
 }
 
-DeviceOrientationController& DeviceOrientationInspectorAgent::controller() {
-  DCHECK(toLocalFrame(m_page->mainFrame())->document());
-  return DeviceOrientationController::from(
-      *m_page->deprecatedLocalMainFrame()->document());
+DeviceOrientationController* DeviceOrientationInspectorAgent::controller() {
+  Document* document = m_inspectedFrames->root()->document();
+  return document ? &DeviceOrientationController::from(*document) : nullptr;
 }
 
 Response DeviceOrientationInspectorAgent::setDeviceOrientationOverride(
@@ -51,26 +45,32 @@ Response DeviceOrientationInspectorAgent::setDeviceOrientationOverride(
   m_state->setDouble(DeviceOrientationInspectorAgentState::alpha, alpha);
   m_state->setDouble(DeviceOrientationInspectorAgentState::beta, beta);
   m_state->setDouble(DeviceOrientationInspectorAgentState::gamma, gamma);
-  controller().setOverride(
-      DeviceOrientationData::create(alpha, beta, gamma, false));
+  if (controller()) {
+    controller()->setOverride(
+        DeviceOrientationData::create(alpha, beta, gamma, false));
+  }
   return Response::OK();
 }
 
 Response DeviceOrientationInspectorAgent::clearDeviceOrientationOverride() {
   m_state->setBoolean(DeviceOrientationInspectorAgentState::overrideEnabled,
                       false);
-  controller().clearOverride();
+  if (controller())
+    controller()->clearOverride();
   return Response::OK();
 }
 
 Response DeviceOrientationInspectorAgent::disable() {
   m_state->setBoolean(DeviceOrientationInspectorAgentState::overrideEnabled,
                       false);
-  controller().clearOverride();
+  if (controller())
+    controller()->clearOverride();
   return Response::OK();
 }
 
 void DeviceOrientationInspectorAgent::restore() {
+  if (!controller())
+    return;
   if (m_state->booleanProperty(
           DeviceOrientationInspectorAgentState::overrideEnabled, false)) {
     double alpha = 0;
@@ -79,20 +79,18 @@ void DeviceOrientationInspectorAgent::restore() {
     m_state->getDouble(DeviceOrientationInspectorAgentState::beta, &beta);
     double gamma = 0;
     m_state->getDouble(DeviceOrientationInspectorAgentState::gamma, &gamma);
-    controller().setOverride(
+    controller()->setOverride(
         DeviceOrientationData::create(alpha, beta, gamma, false));
   }
 }
 
 void DeviceOrientationInspectorAgent::didCommitLoadForLocalFrame(
     LocalFrame* frame) {
-  // FIXME(dgozman): adapt this for out-of-process iframes.
-  if (frame != m_page->mainFrame())
-    return;
-
-  // New document in main frame - apply override there.
-  // No need to cleanup previous one, as it's already gone.
-  restore();
+  if (frame == m_inspectedFrames->root()) {
+    // New document in main frame - apply override there.
+    // No need to cleanup previous one, as it's already gone.
+    restore();
+  }
 }
 
 }  // namespace blink
--- third_party/WebKit/Source/modules/device_orientation/DeviceOrientationInspectorAgent.h
+++ third_party/WebKit/Source/modules/device_orientation/DeviceOrientationInspectorAgent.h
@@ -9,20 +9,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/inspector/InspectorBaseAgent.h"
 #include "core/inspector/protocol/DeviceOrientation.h"
 #include "modules/ModulesExport.h"
-#include "wtf/text/WTFString.h"
 
 namespace blink {
 
 class DeviceOrientationController;
-class Page;
+class InspectedFrames;
 
 class MODULES_EXPORT DeviceOrientationInspectorAgent final
     : public InspectorBaseAgent<protocol::DeviceOrientation::Metainfo> {
   WTF_MAKE_NONCOPYABLE(DeviceOrientationInspectorAgent);
 
  public:
-  static DeviceOrientationInspectorAgent* create(Page*);
-
+  explicit DeviceOrientationInspectorAgent(InspectedFrames*);
   ~DeviceOrientationInspectorAgent() override;
   DECLARE_VIRTUAL_TRACE();
 
@@ -30,15 +28,14 @@ class MODULES_EXPORT DeviceOrientationInspectorAgent final
   Response setDeviceOrientationOverride(double, double, double) override;
   Response clearDeviceOrientationOverride() override;
 
-  // Inspector Controller API.
   Response disable() override;
   void restore() override;
   void didCommitLoadForLocalFrame(LocalFrame*) override;
 
  private:
-  explicit DeviceOrientationInspectorAgent(Page&);
-  DeviceOrientationController& controller();
-  Member<Page> m_page;
+  DeviceOrientationController* controller();
+
+  Member<InspectedFrames> m_inspectedFrames;
 };
 
 }  // namespace blink
