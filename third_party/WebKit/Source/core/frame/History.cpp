b9ca7b995c3d76706e5368c92fb6bd2a2f5ae6fe
sunjian <sunjian@chromium.org>
2017-01-27 12:39:46 -0800

    Surface pushState event
    BUG=685729
    
    Review-Url: https://codereview.chromium.org/2652213005
    Cr-Commit-Position: refs/heads/master@{#446755}

--- third_party/WebKit/Source/core/frame/History.cpp
+++ third_party/WebKit/Source/core/frame/History.cpp
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/loader/NavigationScheduler.h"
 #include "core/page/Page.h"
 #include "platform/RuntimeEnabledFeatures.h"
+#include "platform/instrumentation/tracing/TraceEvent.h"
 #include "platform/weborigin/KURL.h"
 #include "platform/weborigin/SecurityOrigin.h"
 #include "wtf/text/StringView.h"
@@ -160,6 +161,15 @@ void History::go(ScriptState* scriptState, int delta) {
             : FrameLoadTypeReload;
     frame()->reload(reloadType, ClientRedirectPolicy::ClientRedirect);
   }
+}
+
+void History::pushState(PassRefPtr<SerializedScriptValue> data,
+                        const String& title,
+                        const String& url,
+                        ExceptionState& exceptionState) {
+  TRACE_EVENT0("blink", "History::pushState");
+  stateObjectAdded(std::move(data), title, url, scrollRestorationInternal(),
+                   FrameLoadTypeStandard, exceptionState);
 }
 
 KURL History::urlForState(const String& urlString) {
