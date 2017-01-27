aff180d9e881dca66a8055f78aa712ab95dc30ca
lukasza <lukasza@chromium.org>
2017-01-27 07:07:58 -0800

    Blacklisting renaming of |size()| and |length()| methods.
    BUG=672902
    
    Review-Url: https://codereview.chromium.org/2657773003
    Cr-Commit-Position: refs/heads/master@{#446667}

--- tools/clang/rewrite_to_chrome_style/RewriteToChromeStyle.cpp
+++ tools/clang/rewrite_to_chrome_style/RewriteToChromeStyle.cpp
@@ -440,7 +440,7 @@ bool IsBlacklistedInstanceMethodName(llvm::StringRef name) {
 
       // https://crbug.com/672902: Should not rewrite names that mimick methods
       // from std library.
-      "back", "empty", "erase", "front", "insert",
+      "back", "empty", "erase", "front", "insert", "length", "size",
   };
   for (const auto& b : kBlacklistedNames) {
     if (name == b)
@@ -664,7 +664,6 @@ bool ShouldPrefixFunctionName(const std::string& old_method_name) {
       "layoutBlock",
       "layoutObject",
       "layoutSize",
-      "length",
       "lineCap",
       "lineEndings",
       "lineJoin",
