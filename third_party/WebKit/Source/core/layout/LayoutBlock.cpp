e2893406ee2499f7729472a7e4529f7f4eb2e446
yosin <yosin@chromium.org>
2017-01-26 17:08:45 -0800

    Introduce FrameSelection::hasCaretIn()
    This patch introduces |FrameSelection::hasCaretIn()| as replacement of
    |FrameSelection::caretLayoutObject()| to provide high-level API as similar as
    |DragController|[1] for improving code health.
    
    This patch is similar to [1].
    
    [1] http://crrev.com/2272343002 Introduce DragController::hasCaretIn()
    
    BUG=n/a
    TEST=n/a; no behavior changes
    
    Review-Url: https://codereview.chromium.org/2658813002
    Cr-Commit-Position: refs/heads/master@{#446513}

--- third_party/WebKit/Source/core/layout/LayoutBlock.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBlock.cpp
@@ -1843,8 +1843,7 @@ inline bool LayoutBlock::isInlineBoxWrapperActuallyChild() const {
 
 bool LayoutBlock::hasCursorCaret() const {
   LocalFrame* frame = this->frame();
-  return frame->selection().caretLayoutObject() == this &&
-         frame->selection().hasEditableStyle();
+  return frame->selection().hasCaretIn(*this);
 }
 
 bool LayoutBlock::hasDragCaret() const {
