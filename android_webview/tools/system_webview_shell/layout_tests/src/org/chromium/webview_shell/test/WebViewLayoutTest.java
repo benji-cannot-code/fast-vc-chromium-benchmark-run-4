473366daee43b37c8db5f5348b4163eadc962257
timvolodine <timvolodine@chromium.org>
2017-01-27 08:34:30 -0800

    [SystemWebViewLayoutTest] Update expectations and re-enable testWebViewExcludedInterfaces.
    Update non-exposed interfaces and re-enable broken test, due to
    https://codereview.chromium.org/2642873002
    
    non exposed attributes:
    attribute mojo -> attribute gin
    
    BUG=683153
    TBR=tobiasjs@chromium.org
    
    Review-Url: https://codereview.chromium.org/2658923002
    Cr-Commit-Position: refs/heads/master@{#446683}

--- android_webview/tools/system_webview_shell/layout_tests/src/org/chromium/webview_shell/test/WebViewLayoutTest.java
+++ android_webview/tools/system_webview_shell/layout_tests/src/org/chromium/webview_shell/test/WebViewLayoutTest.java
@@ -114,7 +114,6 @@ public class WebViewLayoutTest
         }
     }
 
-    @DisabledTest(message = "crbug.com/683153")
     @MediumTest
     public void testWebViewExcludedInterfaces() throws Exception {
         ensureJsTestCopied();
