956251ea77a67399ca8dcac134d5e87d6c83ce00
mtomasz <mtomasz@chromium.org>
2017-01-26 18:34:21 -0800

    Simplify managing visibility of extensions in settings.
    Summary of behavior:
    - chrome.management API will expose everything which is not component
      including themes and hosted apps (no change). It will never expose
      components, even with --show-component-extensions-options.
    - chrome://extensions will show the same as before this CL, so no
      themes, sometimes hosted documents (only if unpacked), etc (no
      change). What changed is that we will show all components in
      chrome://extensions if --show-component-extensions-options is passed.
      Before we would only show those not hidden from the app launcher
      (sic).
    
    We have too many methods for determining visibility, and they were
    contradicting each other. Eg. ShouldNotBeVisible() which is documented
    as whether it should be visible *anywhere* can return true, while
    ShouldDisplayInAppLauncher could return false in the same time.
    
    In different places we use different of the above methods causing
    inconsistent behavior, eg. component extensions would be shown
    in chrome://extensions when --show-component-extensions-options is
    used, but excluding component extensions which are hidden from
    launcher.
    
    Finally, the logic in methods for determining visibility was
    too complex, and as a result it was difficult to say whether
    something will be visible in some parts of Chrome UI or not.
    
    This CL removes the umbrella ShouldNotBeVisible() method, and introduces
    ShouldExposeInManagementAPI.
    
    Also, callers have been updated to use either
    ShouldDisplayInExtensionSettings or ShouldDisplayInAppLancher or
    ShouldDisplayInNewTabPage or ShouldExposeInManagementAPI depending on
    what is the caller.
    
    Ideally, all of the Should* should rather be moved to the callers, as
    the callers better know what to display or show, rather than the
    Extension class. However, for consistency ShouldExposeInManagementAPI
    was added.
    
    Finally, this CL simplifies the logic for determining visibility
    by assuming that by default everything is visible, and filtering
    out things which we want to hide (no more return true/false every
    second line).
    
    BUG=680429
    TEST=Confirm that all component extensions are visible in
         chrome://extensions when --show-component-extensions-options is
         passed. Also, confirm there are no regressions, so no component
         extensions shown in chrome://extensions by default. Also,
         confirm that chrome.management API doesn't regress by using
         the "Chrome Apps & Extensions Developer Tool".
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2645863004
    Cr-Commit-Position: refs/heads/master@{#446543}

--- chrome/browser/extensions/extension_ui_util.cc
+++ chrome/browser/extensions/extension_ui_util.cc
@@ -52,10 +52,5 @@ bool ShouldDisplayInExtensionSettings(const Extension* extension,
   return extension->ShouldDisplayInExtensionSettings();
 }
 
-bool ShouldNotBeVisible(const Extension* extension,
-                        content::BrowserContext* context) {
-  return extension->ShouldNotBeVisible();
-}
-
 }  // namespace ui_util
 }  // namespace extensions
