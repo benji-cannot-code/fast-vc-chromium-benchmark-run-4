3e1234a7ab41eba14733ead209292edeaac1359b
jam <jam@chromium.org>
2017-01-27 16:37:28 -0800

    Convert ContentSettingBubbleContents to use the new navigation callbacks.
    BUG=682002
    
    Review-Url: https://codereview.chromium.org/2665443002
    Cr-Commit-Position: refs/heads/master@{#446852}

--- chrome/browser/ui/views/content_setting_bubble_contents.cc
+++ chrome/browser/ui/views/content_setting_bubble_contents.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/strings/grit/components_strings.h"
+#include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/plugin_service.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/base/cursor/cursor.h"
@@ -463,9 +464,11 @@ base::string16 ContentSettingBubbleContents::GetDialogButtonLabel(
   return l10n_util::GetStringUTF16(IDS_DONE);
 }
 
-void ContentSettingBubbleContents::DidNavigateMainFrame(
-    const content::LoadCommittedDetails& details,
-    const content::FrameNavigateParams& params) {
+void ContentSettingBubbleContents::DidFinishNavigation(
+    content::NavigationHandle* navigation_handle) {
+  if (!navigation_handle->IsInMainFrame() || !navigation_handle->HasCommitted())
+    return;
+
   // Content settings are based on the main frame, so if it switches then
   // close up shop.
   GetWidget()->Close();
