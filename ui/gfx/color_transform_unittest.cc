f862e324c10b5ad5875c0e5b8324eedee3c1cd76
ccameron <ccameron@chromium.org>
2017-01-28 09:39:32 -0800

    Use SkICC in gfx::ICCProfile and gfx::ColorSpace
    This finally reaches the original goal of having gfx::ColorSpace be
    a (potentially lossily) compressed version of gfx::ICCProfile.
    
    The ICC profile's primaries and transfer funtion are extracted and
    stored in the gfx::ColorSpace, and are used in the event that the
    underlying ICC profile is purged from the cache.
    
    Update ICCProfile's IPC methods to ensure that the profile is touched
    in the cache and that its SkColorSpace is computed when it is
    received via IPC.
    
    BUG=634102
    
    Review-Url: https://codereview.chromium.org/2652503002
    Cr-Commit-Position: refs/heads/master@{#446923}

--- ui/gfx/color_transform_unittest.cc
+++ ui/gfx/color_transform_unittest.cc
@@ -14,10 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gfx {
 
 // Internal functions, exposted for testing.
-GFX_EXPORT Transform GetPrimaryMatrix(ColorSpace::PrimaryID id);
 GFX_EXPORT Transform GetTransferMatrix(ColorSpace::MatrixID id);
-GFX_EXPORT float ToLinear(ColorSpace::TransferID id, float v);
-GFX_EXPORT float FromLinear(ColorSpace::TransferID id, float v);
 
 ColorSpace::PrimaryID all_primaries[] = {
     ColorSpace::PrimaryID::BT709,        ColorSpace::PrimaryID::BT470M,
@@ -118,6 +115,7 @@ TEST(SimpleColorSpace, GetColorSpace) {
   ICCProfile srgb_icc = ICCProfileForTestingSRGB();
   ColorSpace sRGB = srgb_icc.GetColorSpace();
   ColorSpace sRGB2 = sRGB;
+  const float kEpsilon = 1.5f / 255.f;
 
   // Prevent sRGB2 from using a cached ICC profile.
   sRGB2.icc_profile_id_ = 0;
@@ -127,27 +125,27 @@ TEST(SimpleColorSpace, GetColorSpace) {
 
   ColorTransform::TriStim tmp(1.0f, 1.0f, 1.0f);
   t->transform(&tmp, 1);
-  EXPECT_NEAR(tmp.x(), 1.0f, 0.001f);
-  EXPECT_NEAR(tmp.y(), 1.0f, 0.001f);
-  EXPECT_NEAR(tmp.z(), 1.0f, 0.001f);
+  EXPECT_NEAR(tmp.x(), 1.0f, kEpsilon);
+  EXPECT_NEAR(tmp.y(), 1.0f, kEpsilon);
+  EXPECT_NEAR(tmp.z(), 1.0f, kEpsilon);
 
   tmp = ColorTransform::TriStim(1.0f, 0.0f, 0.0f);
   t->transform(&tmp, 1);
-  EXPECT_NEAR(tmp.x(), 1.0f, 0.001f);
-  EXPECT_NEAR(tmp.y(), 0.0f, 0.001f);
-  EXPECT_NEAR(tmp.z(), 0.0f, 0.001f);
+  EXPECT_NEAR(tmp.x(), 1.0f, kEpsilon);
+  EXPECT_NEAR(tmp.y(), 0.0f, kEpsilon);
+  EXPECT_NEAR(tmp.z(), 0.0f, kEpsilon);
 
   tmp = ColorTransform::TriStim(0.0f, 1.0f, 0.0f);
   t->transform(&tmp, 1);
-  EXPECT_NEAR(tmp.x(), 0.0f, 0.001f);
-  EXPECT_NEAR(tmp.y(), 1.0f, 0.001f);
-  EXPECT_NEAR(tmp.z(), 0.0f, 0.001f);
+  EXPECT_NEAR(tmp.x(), 0.0f, kEpsilon);
+  EXPECT_NEAR(tmp.y(), 1.0f, kEpsilon);
+  EXPECT_NEAR(tmp.z(), 0.0f, kEpsilon);
 
   tmp = ColorTransform::TriStim(0.0f, 0.0f, 1.0f);
   t->transform(&tmp, 1);
-  EXPECT_NEAR(tmp.x(), 0.0f, 0.001f);
-  EXPECT_NEAR(tmp.y(), 0.0f, 0.001f);
-  EXPECT_NEAR(tmp.z(), 1.0f, 0.001f);
+  EXPECT_NEAR(tmp.x(), 0.0f, kEpsilon);
+  EXPECT_NEAR(tmp.y(), 0.0f, kEpsilon);
+  EXPECT_NEAR(tmp.z(), 1.0f, kEpsilon);
 }
 
 TEST(SimpleColorSpace, UnknownToSRGB) {
@@ -175,18 +173,6 @@ TEST(SimpleColorSpace, UnknownToSRGB) {
   EXPECT_GT(tmp.z(), tmp.y());
 }
 
-class PrimaryTest : public testing::TestWithParam<ColorSpace::PrimaryID> {};
-
-TEST_P(PrimaryTest, checkInvertible) {
-  EXPECT_EQ(GetPrimaryMatrix(GetParam()).matrix().get(3, 3), 1.0f);
-  // Check that all primary matrices are invertable.
-  EXPECT_TRUE(GetPrimaryMatrix(GetParam()).IsInvertible());
-}
-
-INSTANTIATE_TEST_CASE_P(ColorSpace,
-                        PrimaryTest,
-                        testing::ValuesIn(all_primaries));
-
 class MatrixTest : public testing::TestWithParam<ColorSpace::MatrixID> {};
 
 TEST_P(MatrixTest, checkInvertible) {
@@ -203,8 +189,8 @@ class TransferTest : public testing::TestWithParam<ColorSpace::TransferID> {};
 
 TEST_P(TransferTest, basicTest) {
   for (float x = 0.0f; x <= 1.0f; x += 1.0f / 128.0f) {
-    float linear = ToLinear(GetParam(), x);
-    float x2 = FromLinear(GetParam(), linear);
+    float linear = ColorTransform::ToLinearForTesting(GetParam(), x);
+    float x2 = ColorTransform::FromLinearForTesting(GetParam(), linear);
     EXPECT_NEAR(x, x2, 0.001f);
   }
 }
