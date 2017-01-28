a7df9be926bb8e06e5e2191517994b3cd8e1d129
scottchen <scottchen@chromium.org>
2017-01-27 16:45:39 -0800

    MD Settings: fix settings-toggle-button label text overflow.
    This is to fix appearance page's "[Show home button] long URLs breaks page" bug.
    Once this lands, consumers of settings-toggle-button will be able to specify label-class and sub-label-class to be passed in and applied to the label elements.
    
    BUG=684152
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2661533002
    Cr-Commit-Position: refs/heads/master@{#446856}

--- chrome/browser/resources/settings/controls/settings_toggle_button.js
+++ chrome/browser/resources/settings/controls/settings_toggle_button.js
@@ -10,6 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'settings-toggle-button',
 
+  properties: {
+    elideLabel: {
+      type: Boolean,
+      reflectToAttribute: true,
+    },
+  },
+
   behaviors: [SettingsBooleanControlBehavior],
 
   /** @private */
