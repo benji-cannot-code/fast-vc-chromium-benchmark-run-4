f0e96208a73a6683e9892992bdeed4409a88e648
qyearsley <qyearsley@chromium.org>
2017-01-27 17:33:42 -0800

    Initialize scm in test_importer, and add unit test.
    The latest attempt at auto-import failed because the Git object
    of the Host in TestImporter wasn't initialized. See:
    
    https://build.chromium.org/p/chromium.infra.cron/builders/w3c-test-autoroller/builds/7901
    
    Stack trace excerpt:
    File ".../webkitpy/w3c/test_importer.py", line 371, in get_directory_owners
      changed_files = self.host.scm().changed_files()
    AttributeError: 'NoneType' object has no attribute 'changed_files'
    step returned non-zero exit code: 1
    
    Review-Url: https://codereview.chromium.org/2663563002
    Cr-Commit-Position: refs/heads/master@{#446870}

--- third_party/WebKit/Tools/Scripts/webkitpy/w3c/test_importer_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/w3c/test_importer_unittest.py
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import unittest
 
+from webkitpy.common.checkout.scm.git_mock import MockGit
 from webkitpy.common.host_mock import MockHost
 from webkitpy.common.system.executive_mock import MockExecutive
 from webkitpy.w3c.test_importer import TestImporter
@@ -128,3 +129,24 @@ class TestImporterTest(unittest.TestCase):
                     '/mock-checkout/third_party/WebKit/LayoutTests/external/wpt/MANIFEST.json'
                 ]
             ])
+
+    def test_get_directory_owners(self):
+        host = MockHost()
+        host.filesystem.write_text_file(
+            '/mock-checkout/third_party/WebKit/LayoutTests/W3CImportExpectations',
+            '## Owners: someone@chromium.org\n'
+            '# external/wpt/foo [ Pass ]\n')
+        git = MockGit()
+        git.changed_files = lambda: ['third_party/WebKit/LayoutTests/external/wpt/foo/x.html']
+        host.scm = lambda: git
+        importer = TestImporter(host)
+        self.assertEqual(importer.get_directory_owners(), {'someone@chromium.org': 'external/wpt/foo'})
+
+    def test_get_directory_owners_no_changed_files(self):
+        host = MockHost()
+        host.filesystem.write_text_file(
+            '/mock-checkout/third_party/WebKit/LayoutTests/W3CImportExpectations',
+            '## Owners: someone@chromium.org\n'
+            '# external/wpt/foo [ Pass ]\n')
+        importer = TestImporter(host)
+        self.assertEqual(importer.get_directory_owners(), {})
