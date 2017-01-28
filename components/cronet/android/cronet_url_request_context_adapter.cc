6c487de5805c1043b7428c926e9a5b0c318c7324
wangyix <wangyix@google.com>
2017-01-27 17:47:40 -0800

    Move net-export thread-hopping code into NetLogFileWriter and add IO polled data.
    (1) Use net::FileNetLogObserver in place of net::WriteToFileNetLogObserver for chrome://net-export (which is a big part of crbug.com/679030).
    
    (2) Move NetLogFileWriter's public interface from the FILE_USER_BLOCKING to the UI thread (internalizing the post tasks to file thread/UI thread to simplify things)
    
    (3) Add plumbing for passing polled data, which will be collected on both the UI thread and IO thread (crbug.com/438656). Some of the refactor done will also facilitate crbug.com/679021, which is just a few extra lines. In this CL, only the polled data from the IO thread will be retrieved; the UI thread polled data will be added in a follow-up CL.
    
    BUG=679030,438656,679021
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_cronet_tester
    
    Review-Url: https://codereview.chromium.org/2603523002
    Cr-Commit-Position: refs/heads/master@{#446875}

--- components/cronet/android/cronet_url_request_context_adapter.cc
+++ components/cronet/android/cronet_url_request_context_adapter.cc
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_auth_handler_factory.h"
 #include "net/http/http_server_properties_manager.h"
 #include "net/log/file_net_log_observer.h"
+#include "net/log/net_log_util.h"
 #include "net/log/write_to_file_net_log_observer.h"
 #include "net/nqe/external_estimate_provider.h"
 #include "net/nqe/network_qualities_prefs_manager.h"
@@ -1037,7 +1038,7 @@ void CronetURLRequestContextAdapter::StartNetLogToBoundedFileOnNetworkThread(
 void CronetURLRequestContextAdapter::StopBoundedFileNetLogOnNetworkThread() {
   DCHECK(GetNetworkTaskRunner()->BelongsToCurrentThread());
   bounded_file_observer_->StopObserving(
-      context_.get(),
+      net::GetNetInfo(context_.get(), net::NET_INFO_ALL_SOURCES),
       base::Bind(&CronetURLRequestContextAdapter::StopNetLogCompleted,
                  base::Unretained(this)));
   bounded_file_observer_.reset();
