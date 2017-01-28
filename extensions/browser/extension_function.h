94000a9595152e63b516021f0152a508ef31faaa
lazyboy <lazyboy@chromium.org>
2017-01-27 17:13:47 -0800

    Better crash stacktraces for ExtensionFunction bad messages.
    Record crash trace right where we set bad_message in ExtensionFunction
    
    This will give us clue about which particular extension functions are
    crashing. Previously, we used to record the trace from common response
    callback where we already lost track of which function actually set
    a bad_message.
    
    Turn ExtensionFunction::set_bad_message(bool) to EF::SetBadMessage(void)
    since one should only use this function to set bad_message state, not
    clear it.
    
    BUG=642794
    Test=No visible change. Beware though, similar to
    https://codereview.chromium.org/2629783002, this might show up as a
    blame CL for extension function crashes. These will not really
    be new crashes, so file a (non-release-blocking) bug.
    
    Review-Url: https://codereview.chromium.org/2656013005
    Cr-Commit-Position: refs/heads/master@{#446865}

--- extensions/browser/extension_function.h
+++ extensions/browser/extension_function.h
@@ -54,7 +54,7 @@ class QuotaLimitHeuristic;
 #define EXTENSION_FUNCTION_VALIDATE(test) \
   do {                                    \
     if (!(test)) {                        \
-      this->set_bad_message(true);        \
+      this->SetBadMessage();              \
       return ValidationFailure(this);     \
     }                                     \
   } while (0)
@@ -66,7 +66,7 @@ class QuotaLimitHeuristic;
 #define EXTENSION_FUNCTION_PRERUN_VALIDATE(test) \
   do {                                           \
     if (!(test)) {                               \
-      this->set_bad_message(true);               \
+      this->SetBadMessage();                     \
       return false;                              \
     }                                            \
   } while (0)
@@ -77,7 +77,7 @@ class QuotaLimitHeuristic;
 #define EXTENSION_FUNCTION_ERROR(error) \
   do {                                  \
     error_ = error;                     \
-    this->set_bad_message(true);        \
+    this->SetBadMessage();              \
     return ValidationFailure(this);     \
   } while (0)
 
@@ -242,7 +242,7 @@ class ExtensionFunction
   // Retrieves any error string from the function.
   virtual const std::string& GetError() const;
 
-  void set_bad_message(bool bad_message) { bad_message_ = bad_message; }
+  virtual void SetBadMessage();
 
   // Specifies the name of the function. A long-lived string (such as a string
   // literal) must be provided.
@@ -507,6 +507,7 @@ class UIThreadExtensionFunction : public ExtensionFunction {
   UIThreadExtensionFunction* AsUIThreadExtensionFunction() override;
 
   bool PreRunValidation(std::string* error) override;
+  void SetBadMessage() final;
 
   // Called when a message was received.
   // Should return true if it processed the message.
@@ -605,6 +606,7 @@ class IOThreadExtensionFunction : public ExtensionFunction {
   IOThreadExtensionFunction();
 
   IOThreadExtensionFunction* AsIOThreadExtensionFunction() override;
+  void SetBadMessage() final;
 
   void set_ipc_sender(
       base::WeakPtr<extensions::IOThreadExtensionMessageFilter> ipc_sender,
