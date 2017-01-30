6bc86b50e3bf6a84aadf18252d6134bd0787c263
rdevlin.cronin <rdevlin.cronin@chromium.org>
2017-01-30 13:36:07 -0800

    [Extensions] Remove ExtensionService::show_extensions_prompts
    Remove ExtensionService::show_extension_prompts. This variable, despite
    its common naming, was only used to trigger a prompt from
    UnpackedInstaller::ShowInstallPrompt(), and then only if the extension
    had associated plugins. Additionally, it was only set in tests, and none
    of those tests relied on it.
    
    Remove it completely.
    
    BUG=None
    TEST=None, no behavior change
    
    Review-Url: https://codereview.chromium.org/2661783002
    Cr-Commit-Position: refs/heads/master@{#447072}

--- chrome/browser/extensions/extension_service_unittest.cc
+++ chrome/browser/extensions/extension_service_unittest.cc
@@ -2682,7 +2682,6 @@ TEST_F(ExtensionServiceTest, LoadExtensionsWithPlugins) {
 
   InitPluginService();
   InitializeEmptyExtensionService();
-  service()->set_show_extensions_prompts(true);
 
   // Start by canceling any install prompts.
   std::unique_ptr<extensions::ScopedTestDialogAutoConfirm> auto_confirm(
