32be9013f76670fbc99b1bcaf0c0761fabb6deb3
alph <alph@chromium.org>
2017-01-30 13:19:49 -0800

    DevTools: Reland Performance panel UX
    BUG=548353
    
    Review-Url: https://codereview.chromium.org/2656233005
    Cr-Commit-Position: refs/heads/master@{#447067}

--- third_party/WebKit/LayoutTests/inspector/profiler/profiler-test.js
+++ third_party/WebKit/LayoutTests/inspector/profiler/profiler-test.js
@@ -1,13 +1,13 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var initialize_ProfilerTest = function() {
 
-InspectorTest.preloadPanel("heap_profiler");
+InspectorTest.preloadPanel("js_profiler");
 Bindings.TempFile = InspectorTest.TempFileMock;
 
 InspectorTest.startProfilerTest = function(callback)
 {
     InspectorTest.addResult("Profiler was enabled.");
-    InspectorTest.addSniffer(UI.panels.heap_profiler, "_addProfileHeader", InspectorTest._profileHeaderAdded, true);
+    InspectorTest.addSniffer(UI.panels.js_profiler, "_addProfileHeader", InspectorTest._profileHeaderAdded, true);
     InspectorTest.addSniffer(Profiler.ProfileView.prototype, "refresh", InspectorTest._profileViewRefresh, true);
     InspectorTest.safeWrap(callback)();
 };
@@ -47,14 +47,14 @@ InspectorTest.showProfileWhenAdded = function(title)
 InspectorTest._profileHeaderAdded = function(profile)
 {
     if (InspectorTest._showProfileWhenAdded === profile.title)
-        UI.panels.heap_profiler.showProfile(profile);
+        UI.panels.js_profiler.showProfile(profile);
 };
 
 InspectorTest.waitUntilProfileViewIsShown = function(title, callback)
 {
     callback = InspectorTest.safeWrap(callback);
 
-    var profilesPanel = UI.panels.heap_profiler;
+    var profilesPanel = UI.panels.js_profiler;
     if (profilesPanel.visibleView && profilesPanel.visibleView.profile && profilesPanel.visibleView._profileHeader.title === title)
         callback(profilesPanel.visibleView);
     else
--- third_party/WebKit/Source/devtools/front_end/profiler/HeapProfilerPanel.js
+++ third_party/WebKit/Source/devtools/front_end/profiler/HeapProfilerPanel.js
@@ -12,10 +12,7 @@ Profiler.HeapProfilerPanel = class extends Profiler.ProfilesPanel {
     var registry = Profiler.ProfileTypeRegistry.instance;
     super(
         'heap_profiler',
-        [
-          registry.cpuProfileType, registry.heapSnapshotProfileType, registry.samplingHeapProfileType,
-          registry.trackingHeapSnapshotProfileType
-        ],
+        [registry.heapSnapshotProfileType, registry.samplingHeapProfileType, registry.trackingHeapSnapshotProfileType],
         'profiler.heap-toggle-recording');
   }
 
