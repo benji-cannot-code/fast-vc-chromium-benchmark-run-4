81dfba177ffb9dd79aeb221b022a4d1ce9158264
guidou <guidou@chromium.org>
2017-01-30 05:58:43 -0800

    Add IPC to query capabilities of video input devices.
    This IPC is intended to support new spec-compliant functionality for getUserMedia
    such as device and settings selection based on constraints, and the applyConstraints
    function.
    
    BUG=657733
    
    Review-Url: https://codereview.chromium.org/2646833002
    Cr-Commit-Position: refs/heads/master@{#446977}

---
+++ content/browser/media/media_devices_util.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_MEDIA_MEDIA_DEVICES_UTIL_H_
+#define CONTENT_BROWSER_MEDIA_MEDIA_DEVICES_UTIL_H_
+
+#include <string>
+
+#include "base/callback.h"
+#include "content/common/content_export.h"
+#include "content/common/media/media_devices.h"
+
+namespace content {
+
+// Returns the ID of the user-default device ID via |callback|.
+// If no such device ID can be found, |callback| receives an empty string.
+void CONTENT_EXPORT GetDefaultMediaDeviceID(
+    MediaDeviceType device_type,
+    int render_process_id,
+    int render_frame_id,
+    const base::Callback<void(const std::string&)>& callback);
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_MEDIA_MEDIA_DEVICES_UTIL_H_
--- content/browser/renderer_host/media/media_devices_dispatcher_host_unittest.cc
+++ content/browser/renderer_host/media/media_devices_dispatcher_host_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/memory/ptr_util.h"
 #include "base/run_loop.h"
+#include "base/strings/stringprintf.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "content/browser/renderer_host/media/media_stream_manager.h"
 #include "content/browser/renderer_host/media/media_stream_ui_proxy.h"
@@ -42,6 +43,8 @@ namespace {
 
 const int kProcessId = 5;
 const int kRenderId = 6;
+const size_t kNumFakeVideoDevices = 3;
+const char kDefaultVideoDeviceID[] = "/dev/video2";
 
 void PhysicalDevicesEnumerated(base::Closure quit_closure,
                                MediaDeviceEnumeration* out,
@@ -73,8 +76,10 @@ class MediaDevicesDispatcherHostTest : public testing::Test {
       : thread_bundle_(content::TestBrowserThreadBundle::IO_MAINLOOP),
         origin_(GURL("https://test.com")) {
     // Make sure we use fake devices to avoid long delays.
-    base::CommandLine::ForCurrentProcess()->AppendSwitch(
-        switches::kUseFakeDeviceForMediaStream);
+    base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+        switches::kUseFakeDeviceForMediaStream,
+        base::StringPrintf("device-count=%zu, video-input-default-id=%s",
+                           kNumFakeVideoDevices, kDefaultVideoDeviceID));
     audio_manager_.reset(
         new media::MockAudioManager(base::ThreadTaskRunnerHandle::Get()));
     media_stream_manager_.reset(new MediaStreamManager(audio_manager_.get()));
@@ -109,6 +114,26 @@ class MediaDevicesDispatcherHostTest : public testing::Test {
                void(const std::vector<std::vector<MediaDeviceInfo>>&));
   MOCK_METHOD1(ValidOriginCallback,
                void(const std::vector<std::vector<MediaDeviceInfo>>&));
+  MOCK_METHOD0(MockVideoInputCapabilitiesCallback, void());
+
+  void VideoInputCapabilitiesCallback(
+      std::vector<::mojom::VideoInputDeviceCapabilitiesPtr> capabilities) {
+    MockVideoInputCapabilitiesCallback();
+    std::string expected_first_device_id = GetHMACForMediaDeviceID(
+        browser_context_.GetResourceContext()->GetMediaDeviceIDSalt(), origin_,
+        kDefaultVideoDeviceID);
+    EXPECT_EQ(kNumFakeVideoDevices, capabilities.size());
+    EXPECT_EQ(expected_first_device_id, capabilities[0]->device_id);
+    for (const auto& capability : capabilities) {
+      EXPECT_GT(capability->formats.size(), 1u);
+      EXPECT_GT(capability->formats[0].frame_size.width(), 1);
+      EXPECT_GT(capability->formats[0].frame_size.height(), 1);
+      EXPECT_GT(capability->formats[0].frame_rate, 1);
+      EXPECT_GT(capability->formats[1].frame_size.width(), 1);
+      EXPECT_GT(capability->formats[1].frame_size.height(), 1);
+      EXPECT_GT(capability->formats[1].frame_rate, 1);
+    }
+  }
 
  protected:
   void DevicesEnumerated(
@@ -336,6 +361,25 @@ TEST_F(MediaDevicesDispatcherHostTest, EnumerateAllDevicesUniqueOrigin) {
   // On Windows, the underlying MediaStreamManager uses a separate thread for
   // video capture which must be flushed to guarantee that the callback bound to
   // EnumerateDevices above is invoked before the end of this test's body.
+  media_stream_manager_->FlushVideoCaptureThreadForTesting();
+  base::RunLoop().RunUntilIdle();
+#endif
+}
+
+TEST_F(MediaDevicesDispatcherHostTest, GetVideoInputCapabilities) {
+  EXPECT_CALL(*this, MockVideoInputCapabilitiesCallback());
+  host_->GetVideoInputCapabilities(
+      origin_,
+      base::Bind(
+          &MediaDevicesDispatcherHostTest::VideoInputCapabilitiesCallback,
+          base::Unretained(this)));
+  base::RunLoop().RunUntilIdle();
+
+#if defined(OS_WIN)
+  // On Windows, the underlying MediaStreamManager uses a separate thread for
+  // video capture which must be flushed to guarantee that the callback bound to
+  // GetVIdeoInputCapabilities above is invoked before the end of this test's
+  // body.
   media_stream_manager_->FlushVideoCaptureThreadForTesting();
   base::RunLoop().RunUntilIdle();
 #endif
