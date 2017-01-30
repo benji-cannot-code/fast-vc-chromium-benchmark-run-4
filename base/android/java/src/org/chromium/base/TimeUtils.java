00b3a10066b892a477491b0eb823af57268bb2ad
estevenson <estevenson@chromium.org>
2017-01-30 08:40:15 -0800

    Add @MainDex annotation to classes used in child processes.
    These classes are used by render processes during telemetry_perf_unittests on Android. This doesn't cause any problems currently, but is necessary to be fixed for implementing selective JNI registration (https://crbug.com/674937).
    
    BUG=657024
    
    Review-Url: https://codereview.chromium.org/2622303005
    Cr-Commit-Position: refs/heads/master@{#446999}

--- base/android/java/src/org/chromium/base/TimeUtils.java
+++ base/android/java/src/org/chromium/base/TimeUtils.java
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.base;
 
 import org.chromium.base.annotations.JNINamespace;
+import org.chromium.base.annotations.MainDex;
 
 /** Time-related utilities. */
 @JNINamespace("base::android")
+@MainDex
 public class TimeUtils {
     private TimeUtils() {}
 
