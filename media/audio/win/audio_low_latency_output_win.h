f8bddd8b322e8d284f63ed3c9a1d834af0d2b341
tommi <tommi@chromium.org>
2017-01-30 02:51:27 -0800

    Remove unused function: HardwareSampleRate
    BUG=
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2661853002
    Cr-Commit-Position: refs/heads/master@{#446968}

--- media/audio/win/audio_low_latency_output_win.h
+++ media/audio/win/audio_low_latency_output_win.h
@@ -34,9 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //    o Construction and destruction must take place from the same thread.
 //    o All APIs must be called from the creating thread as well.
 // - It is required to first acquire the native audio parameters of the default
-//   output device and then use the same rate when creating this object. Use
-//   e.g. WASAPIAudioOutputStream::HardwareSampleRate() to retrieve the sample
-//   rate. Open() will fail unless "perfect" audio parameters are utilized.
+//   output device and then use the same rate when creating this object.
+//   Open() will fail unless "perfect" audio parameters are utilized.
 // - Calling Close() also leads to self destruction.
 // - Support for 8-bit audio has not yet been verified and tested.
 //
@@ -88,7 +87,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //   the lowest possible latencies we can achieve on this machine are:
 //     o ~3.3333ms @ 48kHz <=> 160 audio frames per buffer.
 //     o ~3.6281ms @ 44.1kHz <=> 160 audio frames per buffer.
-// - See http://msdn.microsoft.com/en-us/library/windows/desktop/dd370844(v=vs.85).aspx
+// - See
+// http://msdn.microsoft.com/en-us/library/windows/desktop/dd370844(v=vs.85).aspx
 //   for more details.
 
 #ifndef MEDIA_AUDIO_WIN_AUDIO_LOW_LATENCY_OUTPUT_WIN_H_
@@ -141,11 +141,6 @@ class MEDIA_EXPORT WASAPIAudioOutputStream :
   void Close() override;
   void SetVolume(double volume) override;
   void GetVolume(double* volume) override;
-
-  // Retrieves the sample rate the audio engine uses for its internal
-  // processing/mixing of shared-mode streams.  To fetch the settings for the
-  // default device, pass an empty string as the |device_id|.
-  static int HardwareSampleRate(const std::string& device_id);
 
   // Returns AUDCLNT_SHAREMODE_EXCLUSIVE if --enable-exclusive-mode is used
   // as command-line flag and AUDCLNT_SHAREMODE_SHARED otherwise (default).
