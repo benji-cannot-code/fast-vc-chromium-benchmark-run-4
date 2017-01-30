c13da874092e9e5eb5f0dac4d1b2af7c2fe04123
vitaliii <vitaliii@chromium.org>
2017-01-30 06:50:22 -0800

    Move #endif in about_flags.mm.
    As noyau@ pointed out in https://codereview.chromium.org/2651793005, #if
    block checking for ARC support was accidentaly inserted into another #if
    block (in automatically generated CL
    https://codereview.chromium.org/2566993002). This CL moves the inner
    block out.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2659363002
    Cr-Commit-Position: refs/heads/master@{#446981}

--- ios/chrome/browser/about_flags.mm
+++ ios/chrome/browser/about_flags.mm
@@ -39,10 +39,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if !defined(OFFICIAL_BUILD)
 #include "components/variations/variations_switches.h"
+#endif
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
-#endif
 #endif
 
 namespace {
