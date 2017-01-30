e8306f4424c9c2a7bc4e73f7452a652bac3aef49
jiameng <jiameng@chromium.org>
2017-01-29 17:45:36 -0800

    Replace IPC messages in layout_test_render_frame_observer.
    Also remove some forward declarations that are becoming useless.
    
    BUG=577685
    
    Review-Url: https://codereview.chromium.org/2594913002
    Cr-Commit-Position: refs/heads/master@{#446952}

--- content/shell/browser/layout_test/blink_test_controller.cc
+++ content/shell/browser/layout_test/blink_test_controller.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/service_worker_context.h"
 #include "content/public/browser/storage_partition.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/common/associated_interface_provider.h"
 #include "content/public/common/bindings_policy.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/url_constants.h"
@@ -281,6 +282,7 @@ bool BlinkTestController::PrepareForLayoutTest(
   all_observed_render_process_hosts_.clear();
   main_window_render_process_hosts_.clear();
   accumulated_layout_test_runtime_flags_changes_.Clear();
+  layout_test_control_map_.clear();
   ShellBrowserContext* browser_context =
       ShellContentBrowserClient::Get()->browser_context();
   is_compositing_test_ =
@@ -606,24 +608,26 @@ void BlinkTestController::HandleNewRenderFrameHost(RenderFrameHost* frame) {
 
     // Make sure the new renderer process has a test configuration shared with
     // other renderers.
-    ShellTestConfiguration params;
-    params.current_working_directory = current_working_directory_;
-    params.temp_path = temp_path_;
-    params.test_url = test_url_;
-    params.enable_pixel_dumping = enable_pixel_dumping_;
-    params.allow_external_pages =
+    mojom::ShellTestConfigurationPtr params =
+        mojom::ShellTestConfiguration::New();
+    params->enable_pixel_dumping = true;
+    params->allow_external_pages = false;
+    params->current_working_directory = current_working_directory_;
+    params->temp_path = temp_path_;
+    params->test_url = test_url_;
+    params->enable_pixel_dumping = enable_pixel_dumping_;
+    params->allow_external_pages =
         base::CommandLine::ForCurrentProcess()->HasSwitch(
             switches::kAllowExternalPages);
-    params.expected_pixel_hash = expected_pixel_hash_;
-    params.initial_size = initial_size_;
+    params->expected_pixel_hash = expected_pixel_hash_;
+    params->initial_size = initial_size_;
 
     if (did_send_initial_test_configuration_) {
-      frame->Send(new ShellViewMsg_ReplicateTestConfiguration(
-          frame->GetRoutingID(), params));
+      GetLayoutTestControlPtr(frame)->ReplicateTestConfiguration(
+          std::move(params));
     } else {
       did_send_initial_test_configuration_ = true;
-      frame->Send(
-          new ShellViewMsg_SetTestConfiguration(frame->GetRoutingID(), params));
+      GetLayoutTestControlPtr(frame)->SetTestConfiguration(std::move(params));
     }
   }
 
@@ -633,8 +637,7 @@ void BlinkTestController::HandleNewRenderFrameHost(RenderFrameHost* frame) {
     all_observed_render_process_hosts_.insert(process);
 
     if (!main_window) {
-      frame->Send(
-          new ShellViewMsg_SetupSecondaryRenderer(frame->GetRoutingID()));
+      GetLayoutTestControlPtr(frame)->SetupSecondaryRenderer();
     }
 
     process->Send(new LayoutTestMsg_ReplicateLayoutTestRuntimeFlagsChanges(
@@ -705,8 +708,16 @@ void BlinkTestController::OnTextDump(const std::string& dump) {
 }
 
 void BlinkTestController::OnInitiateLayoutDump() {
-  pending_layout_dumps_ = main_window_->web_contents()->SendToAllFrames(
-      new ShellViewMsg_LayoutDumpRequest(MSG_ROUTING_NONE));
+  int number_of_messages = 0;
+  for (RenderFrameHost* rfh : main_window_->web_contents()->GetAllFrames()) {
+    if (!rfh->IsRenderFrameLive())
+      continue;
+
+    ++number_of_messages;
+    GetLayoutTestControlPtr(rfh)->LayoutDumpRequest();
+  }
+
+  pending_layout_dumps_ = number_of_messages;
 }
 
 void BlinkTestController::OnLayoutTestRuntimeFlagsChanged(
@@ -952,6 +963,16 @@ void BlinkTestController::OnSendBluetoothManualChooserEvent(
     return;
   }
   bluetooth_chooser_factory_->SendEvent(event, argument);
+}
+
+mojom::LayoutTestControl* BlinkTestController::GetLayoutTestControlPtr(
+    RenderFrameHost* frame) {
+  if (layout_test_control_map_.find(frame) == layout_test_control_map_.end()) {
+    frame->GetRemoteAssociatedInterfaces()->GetInterface(
+        &layout_test_control_map_[frame]);
+  }
+  DCHECK(layout_test_control_map_[frame].get());
+  return layout_test_control_map_[frame].get();
 }
 
 }  // namespace content
--- content/shell/renderer/layout_test/layout_test_render_frame_observer.cc
+++ content/shell/renderer/layout_test/layout_test_render_frame_observer.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/test_runner/web_test_interfaces.h"
 #include "components/test_runner/web_test_runner.h"
+#include "content/public/common/associated_interface_registry.h"
 #include "content/public/renderer/render_frame.h"
 #include "content/shell/common/shell_messages.h"
 #include "content/shell/renderer/layout_test/blink_test_runner.h"
@@ -20,36 +21,28 @@ namespace content {
 
 LayoutTestRenderFrameObserver::LayoutTestRenderFrameObserver(
     RenderFrame* render_frame)
-    : RenderFrameObserver(render_frame) {
+    : RenderFrameObserver(render_frame), binding_(this) {
   render_frame->GetWebFrame()->setContentSettingsClient(
       LayoutTestRenderThreadObserver::GetInstance()
           ->test_interfaces()
           ->TestRunner()
           ->GetWebContentSettings());
+  render_frame->GetAssociatedInterfaceRegistry()->AddInterface(base::Bind(
+      &LayoutTestRenderFrameObserver::BindRequest, base::Unretained(this)));
 }
 
-bool LayoutTestRenderFrameObserver::OnMessageReceived(
-    const IPC::Message& message) {
-  bool handled = true;
-  IPC_BEGIN_MESSAGE_MAP(LayoutTestRenderFrameObserver, message)
-    IPC_MESSAGE_HANDLER(ShellViewMsg_LayoutDumpRequest, OnLayoutDumpRequest)
-    IPC_MESSAGE_HANDLER(ShellViewMsg_ReplicateTestConfiguration,
-                        OnReplicateTestConfiguration)
-    IPC_MESSAGE_HANDLER(ShellViewMsg_SetTestConfiguration,
-                        OnSetTestConfiguration)
-    IPC_MESSAGE_HANDLER(ShellViewMsg_SetupSecondaryRenderer,
-                        OnSetupSecondaryRenderer)
-    IPC_MESSAGE_UNHANDLED(handled = false)
-  IPC_END_MESSAGE_MAP()
+LayoutTestRenderFrameObserver::~LayoutTestRenderFrameObserver() = default;
 
-  return handled;
+void LayoutTestRenderFrameObserver::BindRequest(
+    mojom::LayoutTestControlAssociatedRequest request) {
+  binding_.Bind(std::move(request));
 }
 
 void LayoutTestRenderFrameObserver::OnDestruct() {
   delete this;
 }
 
-void LayoutTestRenderFrameObserver::OnLayoutDumpRequest() {
+void LayoutTestRenderFrameObserver::LayoutDumpRequest() {
   std::string dump =
       LayoutTestRenderThreadObserver::GetInstance()
           ->test_interfaces()
@@ -58,19 +51,19 @@ void LayoutTestRenderFrameObserver::OnLayoutDumpRequest() {
   Send(new ShellViewHostMsg_LayoutDumpResponse(routing_id(), dump));
 }
 
-void LayoutTestRenderFrameObserver::OnReplicateTestConfiguration(
-    const ShellTestConfiguration& test_config) {
+void LayoutTestRenderFrameObserver::ReplicateTestConfiguration(
+    mojom::ShellTestConfigurationPtr config) {
   BlinkTestRunner::Get(render_frame()->GetRenderView())
-      ->OnReplicateTestConfiguration(test_config);
+      ->OnReplicateTestConfiguration(std::move(config));
 }
 
-void LayoutTestRenderFrameObserver::OnSetTestConfiguration(
-    const ShellTestConfiguration& test_config) {
+void LayoutTestRenderFrameObserver::SetTestConfiguration(
+    mojom::ShellTestConfigurationPtr config) {
   BlinkTestRunner::Get(render_frame()->GetRenderView())
-      ->OnSetTestConfiguration(test_config);
+      ->OnSetTestConfiguration(std::move(config));
 }
 
-void LayoutTestRenderFrameObserver::OnSetupSecondaryRenderer() {
+void LayoutTestRenderFrameObserver::SetupSecondaryRenderer() {
   BlinkTestRunner::Get(render_frame()->GetRenderView())
       ->OnSetupSecondaryRenderer();
 }
