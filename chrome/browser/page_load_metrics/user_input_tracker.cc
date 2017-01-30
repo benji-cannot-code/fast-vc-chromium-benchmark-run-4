8e8e9e91d315f3e12c64f092fb5788a362d65e2d
ckulakowski <ckulakowski@opera.com>
2017-01-30 08:42:48 -0800

    Chromium doesn't compile with -Wglobal-constructors
    We compile chromium with clang flag -Wglobal-constructors in our
    project. We have following compilation error during compilation
    of user_input_tracker.cc:
    
    ../../chrome/browser/page_load_metrics/user_input_tracker.cc:27:11:
    error: declaration requires a global constructor [-Werror,-Wglobal-constructors]
    const int kRateLimitClampMillis = (kOldestAllowedEventAgeSeconds * 1000) /
    
    In order to get rid of this global constructor I recommend to declare
    static class variable UserInputTracker::kMaxTrackedEvents and global
    variable kRateLimitClampMillis as constexpr which will be expanded to
    numerical value during compilation.
    
    BUG=686669
    
    Review-Url: https://codereview.chromium.org/2662803002
    Cr-Commit-Position: refs/heads/master@{#447000}

--- chrome/browser/page_load_metrics/user_input_tracker.cc
+++ chrome/browser/page_load_metrics/user_input_tracker.cc
@@ -25,8 +25,8 @@ const int kOldestAllowedEventAgeSeconds = kMaxEventAgeSeconds * 2;
 // In order to limit to at most kMaxTrackedEvents, we rate limit the recorded
 // events,
 // allowing one per rate limit period.
-const int kRateLimitClampMillis = (kOldestAllowedEventAgeSeconds * 1000) /
-                                  UserInputTracker::kMaxTrackedEvents;
+constexpr int kRateLimitClampMillis = (kOldestAllowedEventAgeSeconds * 1000) /
+                                      UserInputTracker::kMaxTrackedEvents;
 
 bool IsInterestingInputEvent(const blink::WebInputEvent& event) {
   // Ignore synthesized auto repeat events.
@@ -58,13 +58,13 @@ base::TimeTicks GetTimeTicksFromSeconds(double seconds) {
 
 }  // namespace
 
+constexpr size_t UserInputTracker::kMaxTrackedEvents;
+
 UserInputTracker::UserInputTracker() {
   sorted_event_times_.reserve(kMaxTrackedEvents);
 }
 
 UserInputTracker::~UserInputTracker() {}
-
-const size_t UserInputTracker::kMaxTrackedEvents = 100;
 
 // static
 base::TimeTicks UserInputTracker::GetEventTime(
