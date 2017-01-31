c94027219e9c36d07609f2c9bb3ed51fa5b4a505
dbeam <dbeam@chromium.org>
2017-01-31 11:03:51 -0800

    Revert of Drop .clang-format in ui/webui now that Chromium JS defaults are the same (patchset #1 id:1 of https://codereview.chromium.org/2664613002/ )
    Reason for revert:
    clang-format binaries were reverted, so the defaults are not as good at the moment.
    
    Original issue's description:
    > Drop .clang-format in ui/webui now that Chromium JS defaults are the same
    >
    > Additionally, grit now deals with ' as well as " in <include> and <if>.
    >
    > R=dpapad@chromium.org
    > BUG=567770
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    >
    > Review-Url: https://codereview.chromium.org/2664613002
    > Cr-Commit-Position: refs/heads/master@{#447126}
    > Committed: https://chromium.googlesource.com/chromium/src/+/9fb56fe502090f3cf70b4c531b763c7ba82937ee
    
    TBR=dpapad@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=567770
    
    Review-Url: https://codereview.chromium.org/2662323002
    Cr-Commit-Position: refs/heads/master@{#447284}

--- ui/webui/resources/js/jstemplate_compiled.js
+++ ui/webui/resources/js/jstemplate_compiled.js
@@ -6,4 +6,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This file serves as a proxy to bring the included js file from /third_party
 // into its correct location under the resources directory tree, whence it is
 // delivered via a chrome://resources URL.  See ../webui_resources.grd.
-<include src='../../../../third_party/jstemplate/jstemplate_compiled.js'>
+<include src="../../../../third_party/jstemplate/jstemplate_compiled.js">
