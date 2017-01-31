0ab4b88e81bacc74a8dc571842a756f8cd8ecf14
shend <shend@chromium.org>
2017-01-30 19:07:06 -0800

    Change setIsLink to conform with other nonproperty setters.
    In ComputedStyle, there are a few fields that are not CSS properties.
    They are mostly boolean fields, with getters and setters. The setters
    take no arguments and simply sets the boolean field to true. One
    exception is the isLink field, whose setter takes an argument and assigns
    the field to the argument (like a normal setter). However, we only use
    the setter as 'setIsLink(true)'. This patch changes the setIsLink setter
    to conform to the other nonproperty setters.
    
    This is prework for generating nonproperty fields in ComputedStyle. This
    patch allows us to generate the setter for isLink like the other
    nonproperty fields.
    
    BUG=628043
    
    Review-Url: https://codereview.chromium.org/2665603002
    Cr-Commit-Position: refs/heads/master@{#447173}

--- third_party/WebKit/Source/core/css/resolver/StyleResolver.cpp
+++ third_party/WebKit/Source/core/css/resolver/StyleResolver.cpp
@@ -692,7 +692,7 @@ PassRefPtr<ComputedStyle> StyleResolver::styleForElement(
   }
 
   if (element->isLink()) {
-    state.style()->setIsLink(true);
+    state.style()->setIsLink();
     EInsideLink linkState = state.elementLinkState();
     if (linkState != EInsideLink::kNotInsideLink) {
       bool forceVisited = InspectorInstrumentation::forcePseudoState(
