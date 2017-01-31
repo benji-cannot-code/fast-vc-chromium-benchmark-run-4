22923c04e436b72d9f015a013675fe1bf8301978
fdoray <fdoray@chromium.org>
2017-01-31 12:36:14 -0800

    Add TaskScheduler::GetMaxConcurrentTasksWithTraitsDeprecated().
    This method returns the maximum number of non-single-threaded tasks
    posted with given TaskTraits that can run concurrently.
    
    This method is required to implement
    gin::V8Platform::NumberOfAvailableBackgroundThread() correctly
    once gin::V8Platform::CallOnForegroundThread() starts using
    TaskScheduler.
    
    In the future, we would like to remove
    gin::V8Platform::NumberOfAvailableBackgroundThreads() and the
    method introduced by this CL.
    
    BUG=659191
    
    Review-Url: https://codereview.chromium.org/2664443004
    Cr-Commit-Position: refs/heads/master@{#447316}

--- base/test/scoped_task_scheduler.cc
+++ base/test/scoped_task_scheduler.cc
@@ -52,6 +52,8 @@ class TestTaskScheduler : public TaskScheduler {
   scoped_refptr<SingleThreadTaskRunner> CreateSingleThreadTaskRunnerWithTraits(
       const TaskTraits& traits) override;
   std::vector<const HistogramBase*> GetHistograms() const override;
+  int GetMaxConcurrentTasksWithTraitsDeprecated(
+      const TaskTraits& traits) const override;
   void Shutdown() override;
   void FlushForTesting() override;
   void JoinForTesting() override;
@@ -155,6 +157,11 @@ TestTaskScheduler::CreateSingleThreadTaskRunnerWithTraits(
 std::vector<const HistogramBase*> TestTaskScheduler::GetHistograms() const {
   NOTREACHED();
   return std::vector<const HistogramBase*>();
+}
+
+int TestTaskScheduler::GetMaxConcurrentTasksWithTraitsDeprecated(
+    const TaskTraits& traits) const {
+  return 1;
 }
 
 void TestTaskScheduler::Shutdown() {
