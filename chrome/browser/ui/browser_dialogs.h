501f954a088adbad087a0ef74fd40d8096e52618
mgiuca <mgiuca@chromium.org>
2017-01-30 20:16:49 -0800

    Add picker view for Web Share Target.
    This is temporary UI which has not been approved by UX and will only be
    used for an experimental feature behind a flag.
    
    Hooked up to the share service for navigator.share on Desktop. Currently
    does not show any real apps (just the dummy app which is currently
    hard-coded in the share service).
    
    BUG=668389
    
    Review-Url: https://codereview.chromium.org/2644793006
    Cr-Commit-Position: refs/heads/master@{#447187}

--- chrome/browser/ui/browser_dialogs.h
+++ chrome/browser/ui/browser_dialogs.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "build/build_config.h"
 #include "chrome/browser/ui/bookmarks/bookmark_editor.h"
+#include "chrome/browser/webshare/share_service_impl.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/gfx/native_widget_types.h"
 
@@ -157,6 +158,15 @@ void ShowBookmarkEditorViews(gfx::NativeWindow parent_window,
 
 payments::PaymentRequestDialog* CreatePaymentRequestDialog(
     payments::PaymentRequest* request);
+
+// Shows the dialog to choose a share target app. |targets| is a list of app
+// titles that will be shown in a list. Calls |callback| with SHARE if an app
+// was chosen, or CANCEL if the dialog was cancelled.
+// TODO(mgiuca): Callback should provide info about the picked app.
+void ShowWebShareTargetPickerDialog(
+    gfx::NativeWindow parent_window,
+    const std::vector<base::string16>& targets,
+    const base::Callback<void(SharePickerResult)>& callback);
 
 #if defined(OS_MACOSX)
 
--- chrome/browser/webshare/share_service_impl.h
+++ chrome/browser/webshare/share_service_impl.h
@@ -9,10 +9,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/gtest_prod_util.h"
+#include "base/strings/string16.h"
 #include "mojo/public/cpp/bindings/interface_request.h"
 #include "third_party/WebKit/public/platform/modules/webshare/webshare.mojom.h"
 
 class GURL;
+
+enum class SharePickerResult {
+  CANCEL,
+  SHARE
+};
 
 // Desktop implementation of the ShareService Mojo service.
 class ShareServiceImpl : public blink::mojom::ShareService {
@@ -31,6 +37,13 @@ class ShareServiceImpl : public blink::mojom::ShareService {
  private:
   FRIEND_TEST_ALL_PREFIXES(ShareServiceImplUnittest, ReplacePlaceholders);
 
+  // Shows the share picker dialog with |targets| as the list of applications
+  // presented to the user. Passes the result to |callback|. Virtual for
+  // testing.
+  virtual void ShowPickerDialog(
+      const std::vector<base::string16>& targets,
+      const base::Callback<void(SharePickerResult)>& callback);
+
   // Opens a new tab and navigates to |target_url|.
   // Virtual for testing purposes.
   virtual void OpenTargetURL(const GURL& target_url);
@@ -48,6 +61,12 @@ class ShareServiceImpl : public blink::mojom::ShareService {
                                   base::StringPiece text,
                                   const GURL& share_url,
                                   std::string* url_template_filled);
+
+  void OnPickerClosed(const std::string& title,
+                      const std::string& text,
+                      const GURL& share_url,
+                      const ShareCallback& callback,
+                      SharePickerResult result);
 
   DISALLOW_COPY_AND_ASSIGN(ShareServiceImpl);
 };
