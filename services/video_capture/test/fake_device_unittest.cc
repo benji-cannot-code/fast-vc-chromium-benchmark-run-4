cc494f0cf9dbea135d5f4fc5a814ff1b8cacc313
rockot <rockot@chromium.org>
2017-01-30 19:26:10 -0800

    Merge video_capture tests into service_unittests
    BUG=686329
    
    Review-Url: https://codereview.chromium.org/2660743002
    Cr-Commit-Position: refs/heads/master@{#447176}

--- services/video_capture/test/fake_device_unittest.cc
+++ services/video_capture/test/fake_device_unittest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/video_capture/public/interfaces/device_factory.mojom.h"
 #include "services/video_capture/test/fake_device_test.h"
 #include "services/video_capture/test/mock_receiver.h"
-#include "services/video_capture/test/service_test.h"
 
 using testing::_;
 using testing::Invoke;
@@ -31,7 +30,11 @@ struct FrameInfo {
 
 namespace video_capture {
 
-TEST_F(FakeDeviceTest, DISABLED_FrameCallbacksArrive) {
+// This alias ensures test output is easily attributed to this service's tests.
+// TODO(rockot/chfremer): Consider just renaming the type.
+using FakeVideoCaptureDeviceTest = FakeDeviceTest;
+
+TEST_F(FakeVideoCaptureDeviceTest, DISABLED_FrameCallbacksArrive) {
   base::RunLoop wait_loop;
   // These two constants must be static as a workaround
   // for a MSVC++ bug about lambda captures, see the discussion at
@@ -55,7 +58,8 @@ TEST_F(FakeDeviceTest, DISABLED_FrameCallbacksArrive) {
 
 // Tests that frames received from a fake capture device match the requested
 // format and have increasing timestamps.
-TEST_F(FakeDeviceTest, DISABLED_ReceiveFramesFromFakeCaptureDevice) {
+TEST_F(FakeVideoCaptureDeviceTest,
+       DISABLED_ReceiveFramesFromFakeCaptureDevice) {
   base::RunLoop wait_loop;
   mojom::ReceiverPtr receiver_proxy;
   // These two constants must be static as a workaround
