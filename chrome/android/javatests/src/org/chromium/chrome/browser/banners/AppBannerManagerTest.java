e5866ce17ac70c96ec80189b22804659c925363c
gonzalon <gonzalon@chromium.org>
2017-01-31 07:08:20 -0800

    The method WebappRegistry#isWebApkInstalled now reuses InstallerDelegate#isInstalled so as not to repeat code.
    BUG=639428
    
    Review-Url: https://codereview.chromium.org/2632693002
    Cr-Commit-Position: refs/heads/master@{#447233}

--- chrome/android/javatests/src/org/chromium/chrome/browser/banners/AppBannerManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/banners/AppBannerManagerTest.java
@@ -12,6 +12,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.support.test.filters.MediumTest;
 import android.support.test.filters.SmallTest;
@@ -119,16 +120,15 @@ public class AppBannerManagerTest extends ChromeTabbedActivityTestBase {
         public boolean isInstalled = false;
 
         @Override
-        public List<PackageInfo> getInstalledPackages(int flags) {
-            List<PackageInfo> packages = new ArrayList<PackageInfo>();
-
+        public PackageInfo getPackageInfo(String packageName, int flags)
+                throws NameNotFoundException {
             if (isInstalled) {
                 PackageInfo info = new PackageInfo();
                 info.packageName = NATIVE_APP_PACKAGE;
-                packages.add(info);
+                return info;
+            } else {
+                throw new PackageManager.NameNotFoundException();
             }
-
-            return packages;
         }
     }
 
