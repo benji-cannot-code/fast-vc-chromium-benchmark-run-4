b6f5fa04447a5caa7dcaa0666ca38450909721c1
thomasanderson <thomasanderson@google.com>
2017-01-31 12:27:49 -0800

    Linux Aura: Use Aura theme on all windows when 'Use Classic theme' is used
    Previously, windows like tooltips, context menus, and the task manager
    always used the Gtk theme even when "Use Classic theme" was selected
    (whereas browser windows would be affected).  This CL makes it so the
    Aura theme is used on all windows if the classic theme is used.
    
    BUG=132847
    R=sky@chromium.org,erg@chromium.org
    
    Review-Url: https://codereview.chromium.org/2667703002
    Cr-Commit-Position: refs/heads/master@{#447312}

--- chrome/browser/ui/views/chrome_browser_main_extra_parts_views_linux.cc
+++ chrome/browser/ui/views/chrome_browser_main_extra_parts_views_linux.cc
@@ -31,14 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 ui::NativeTheme* GetNativeThemeForWindow(aura::Window* window) {
-  if (!window)
-    return nullptr;
-
   Profile* profile = nullptr;
-  // Window types not listed here (such as tooltips) will never use Chrome
-  // theming.
-  if (window->type() == ui::wm::WINDOW_TYPE_NORMAL ||
-      window->type() == ui::wm::WINDOW_TYPE_POPUP) {
+  if (window) {
     profile = reinterpret_cast<Profile*>(
         window->GetNativeWindowProperty(Profile::kProfileKey));
   }
