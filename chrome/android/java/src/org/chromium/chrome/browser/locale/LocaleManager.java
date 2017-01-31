f75dcdc558442376d861fd4a6b3f884ae2c0d4b0
aberent <aberent@chromium.org>
2017-01-31 06:29:14 -0800

    Revert of Use new app-level object instantiation mechanism (patchset #3 id:40001 of https://codereview.chromium.org/2652383002/ )
    Reason for revert:
    This causes Clank to crash on startup (consistently).
    
    BUG=687142
    
    Original issue's description:
    > Use new app-level object instantiation mechanism
    >
    > This change uses a new mechanism for instantiating objects from the
    > Chrome application for several classes.
    >
    > BUG=685383
    >
    > Review-Url: https://codereview.chromium.org/2652383002
    > Cr-Commit-Position: refs/heads/master@{#447078}
    > Committed: https://chromium.googlesource.com/chromium/src/+/f77ebec0c7db165ee6039e617432faa12c436641
    
    TBR=mariakhomenko@chromium.org,cco3@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=685383
    
    Review-Url: https://codereview.chromium.org/2666913002
    Cr-Commit-Position: refs/heads/master@{#447230}

--- chrome/android/java/src/org/chromium/chrome/browser/locale/LocaleManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/locale/LocaleManager.java
@@ -71,7 +71,8 @@ public class LocaleManager {
     public static LocaleManager getInstance() {
         assert ThreadUtils.runningOnUiThread();
         if (sInstance == null) {
-            sInstance = ChromeApplication.createObject(LocaleManager.class);
+            sInstance = ((ChromeApplication) ContextUtils.getApplicationContext())
+                    .createLocaleManager();
         }
         return sInstance;
     }
--- chrome/android/java/src/org/chromium/chrome/browser/physicalweb/PhysicalWebBleClient.java
+++ chrome/android/java/src/org/chromium/chrome/browser/physicalweb/PhysicalWebBleClient.java
@@ -15,6 +15,7 @@ import com.google.android.gms.nearby.messages.Message;
 import com.google.android.gms.nearby.messages.MessageFilter;
 import com.google.android.gms.nearby.messages.MessageListener;
 
+import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 import org.chromium.chrome.browser.ChromeApplication;
 
@@ -70,7 +71,8 @@ public class PhysicalWebBleClient {
      */
     public static PhysicalWebBleClient getInstance() {
         if (sInstance == null) {
-            sInstance = ChromeApplication.createObject(PhysicalWebBleClient.class);
+            sInstance = ((ChromeApplication) ContextUtils.getApplicationContext())
+                    .createPhysicalWebBleClient();
         }
         return sInstance;
     }
