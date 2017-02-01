ba1304caf1eb65b77bd9739ce1c9055bdf9bc643
afakhry <afakhry@chromium.org>
2017-01-31 17:38:04 -0800

    Detect and fix overlapping displays
    Overlapping displays prevent us from correctly creating the mouse warp
    regions. This CL adds code to detect and fix any overlap when the display
    layout is applied.
    This was initially a bug in the options UI that was recently fixed. The backend
    still needs to prevent this buggy condition.
    
    BUG=614660
    TEST=ash_unittests --gtest_filter=DisplayManagerTest.NoOverlappedDisplays*
    
    Review-Url: https://codereview.chromium.org/2573673003
    Cr-Commit-Position: refs/heads/master@{#447402}

--- chrome/browser/extensions/display_info_provider_chromeos.cc
+++ chrome/browser/extensions/display_info_provider_chromeos.cc
@@ -516,7 +516,7 @@ bool DisplayInfoProviderChromeOS::SetDisplayLayout(
   display::DisplayManager* display_manager =
       ash::Shell::GetInstance()->display_manager();
   display::DisplayLayoutBuilder builder(
-      display_manager->GetCurrentDisplayLayout());
+      display_manager->GetCurrentResolvedDisplayLayout());
 
   bool have_root = false;
   builder.ClearPlacements();
@@ -627,7 +627,7 @@ DisplayInfoProviderChromeOS::GetDisplayLayout() {
   DisplayLayoutList result;
   for (const display::Display& display : displays) {
     const display::DisplayPlacement placement =
-        display_manager->GetCurrentDisplayLayout().FindPlacementById(
+        display_manager->GetCurrentResolvedDisplayLayout().FindPlacementById(
             display.id());
     if (placement.display_id == display::kInvalidDisplayId)
       continue;
