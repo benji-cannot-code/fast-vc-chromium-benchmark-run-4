38dc3bc4b3d62cc8e5505ff233c406d2cea4e7c9
reveman <reveman@chromium.org>
2017-02-01 14:43:46 -0800

    exo: Pipeline cursor updates.
    This changes the HW cursor code from using a cancelable callback
    to weak ptrs for cursor capture requests. This allows cursor
    changes to be pipelined properly instead of having all but the last
    request be canceled.
    
    This is important when the client is providing a new cursor each
    frame (e.g. animated cursors) as canceling previous requests will
    result in no cursor update until capture client stops sending
    new cursor updates.
    
    BUG=b/31798695
    
    Review-Url: https://codereview.chromium.org/2661163005
    Cr-Commit-Position: refs/heads/master@{#447623}

--- components/exo/pointer.h
+++ components/exo/pointer.h
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "base/cancelable_callback.h"
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "components/exo/surface_delegate.h"
 #include "components/exo/surface_observer.h"
 #include "components/exo/wm_helper.h"
@@ -104,9 +104,8 @@ class Pointer : public ui::EventHandler,
   // The current cursor.
   ui::Cursor cursor_;
 
-  // Cancelable callback for pending cursor capture.
-  base::CancelableCallback<void(std::unique_ptr<cc::CopyOutputResult>)>
-      cursor_captured_callback_;
+  // Weak pointer factory used for cursor capture callbacks.
+  base::WeakPtrFactory<Pointer> cursor_capture_weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(Pointer);
 };
