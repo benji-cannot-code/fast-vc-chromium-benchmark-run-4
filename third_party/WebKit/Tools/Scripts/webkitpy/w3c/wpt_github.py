4f7488ef78546e8ce0990d402a9376cd257bc6e4
qyearsley <qyearsley@chromium.org>
2017-01-31 18:45:54 -0800

    Support passing GitHub username and token via command line arguments.
    This is a minor usability enhancement -- when run with nothing set,
    wpt-export will now print:
    
      usage: wpt-export [-h] [--dry-run] [--user USER] [--token TOKEN]
      wpt-export: error: Must provide both user and token for GitHub.
    
    And the help methods says:
    
      --user USER    Github user name. Can also be provided using the GH_USER
                     environment variable.
      --token TOKEN  Github token or password. Can also be provided using the
                     GH_TOKEN environment variable.
    
    Review-Url: https://codereview.chromium.org/2667453002
    Cr-Commit-Position: refs/heads/master@{#447428}

--- third_party/WebKit/Tools/Scripts/webkitpy/w3c/wpt_github.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/w3c/wpt_github.py
@@ -6,12 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import base64
 import json
 import logging
-import os
-import sys
 import urllib2
-
-from webkitpy.common.system.filesystem import FileSystem
-from webkitpy.common.webkit_finder import WebKitFinder
 
 
 _log = logging.getLogger(__name__)
@@ -21,12 +16,11 @@ EXPORT_LABEL = 'chromium-export'
 
 class WPTGitHub(object):
 
-    def __init__(self, host):
+    def __init__(self, host, user, token):
         self.host = host
-        self.user = self.host.environ.get('GH_USER')
-        self.token = self.host.environ.get('GH_TOKEN')
-
-        assert self.user and self.token, 'must have GH_USER and GH_TOKEN env vars'
+        self.user = user
+        self.token = token
+        assert self.user and self.token
 
     def auth_token(self):
         return base64.encodestring('{}:{}'.format(self.user, self.token)).strip()
