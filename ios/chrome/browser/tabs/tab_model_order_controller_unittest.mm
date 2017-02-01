24d4a7bf60ff4030e7f9c99827d34168712108c9
pavely <pavely@chromium.org>
2017-01-31 16:51:45 -0800

    Get ModelTypeStoreFactory for reading list from ProfileSyncService
    This CL switches ReadingListModelFactory to using ProfileSyncService's factory
    for ModelTypeStore. This allows ReadingList to share store with other types.
    
    R=olivierrobin@chromium.org
    BUG=664920
    
    Review-Url: https://codereview.chromium.org/2663553003
    Cr-Commit-Position: refs/heads/master@{#447390}

--- ios/chrome/browser/tabs/tab_model_order_controller_unittest.mm
+++ ios/chrome/browser/tabs/tab_model_order_controller_unittest.mm
@@ -6,11 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/scoped_nsautorelease_pool.h"
 #include "base/memory/ptr_util.h"
 #include "ios/chrome/browser/browser_state/test_chrome_browser_state.h"
+#include "ios/chrome/browser/browser_state/test_chrome_browser_state_manager.h"
 #import "ios/chrome/browser/sessions/session_window.h"
 #import "ios/chrome/browser/sessions/test_session_service.h"
 #import "ios/chrome/browser/tabs/tab.h"
 #import "ios/chrome/browser/tabs/tab_model.h"
 #import "ios/chrome/browser/tabs/tab_model_order_controller.h"
+#include "ios/chrome/test/ios_chrome_scoped_testing_chrome_browser_state_manager.h"
 #include "ios/web/public/referrer.h"
 #include "ios/web/public/test/test_web_thread_bundle.h"
 #include "ios/web/public/web_thread.h"
@@ -22,7 +24,10 @@ namespace {
 class TabModelOrderControllerTest : public PlatformTest {
  protected:
   TabModelOrderControllerTest()
-      : thread_bundle_(web::TestWebThreadBundle::IO_MAINLOOP) {}
+      : thread_bundle_(web::TestWebThreadBundle::IO_MAINLOOP),
+        scoped_browser_state_manager_(
+            base::MakeUnique<TestChromeBrowserStateManager>(base::FilePath())) {
+  }
 
   void SetUp() override {
     DCHECK_CURRENTLY_ON(web::WebThread::UI);
@@ -79,6 +84,7 @@ class TabModelOrderControllerTest : public PlatformTest {
   GURL url_;
   web::Referrer referrer_;
   web::TestWebThreadBundle thread_bundle_;
+  IOSChromeScopedTestingChromeBrowserStateManager scoped_browser_state_manager_;
   base::scoped_nsobject<SessionWindowIOS> sessionWindow_;
   std::unique_ptr<TestChromeBrowserState> chrome_browser_state_;
   base::scoped_nsobject<Tab> dummy_tab_;
