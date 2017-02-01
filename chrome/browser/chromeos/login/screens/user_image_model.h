009a064a5351e6ac855be460ff4343667c5d2d9b
rsorokin <rsorokin@chromium.org>
2017-02-01 07:12:42 -0800

    Delete "Google Profile photo" for the Active Directory
    Delete "Google Profile photo" choice from the image choice screen for
    the Active Directory users.
    
    BUG=682236
    TEST=manual
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2666893002
    Cr-Commit-Position: refs/heads/master@{#447513}

--- chrome/browser/chromeos/login/screens/user_image_model.h
+++ chrome/browser/chromeos/login/screens/user_image_model.h
@@ -20,6 +20,7 @@ class UserImageModel : public BaseScreen {
   static const char kContextKeyIsCameraPresent[];
   static const char kContextKeySelectedImageURL[];
   static const char kContextKeyProfilePictureDataURL[];
+  static const char kContextKeyHasGaiaAccount[];
 
   explicit UserImageModel(BaseScreenDelegate* base_screen_delegate);
   ~UserImageModel() override;
