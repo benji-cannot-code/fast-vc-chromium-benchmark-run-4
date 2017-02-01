ed1864fecf124f8395a73e54aae538639a37da72
dgn <dgn@chromium.org>
2017-02-01 05:17:17 -0800

    [NTP Client] Remove the NTP_SUGGESTIONS_SECTION_DISMISSAL feature flag
    BUG=687167
    
    Review-Url: https://codereview.chromium.org/2667803005
    Cr-Commit-Position: refs/heads/master@{#447499}

--- chrome/android/junit/src/org/chromium/chrome/browser/ntp/cards/SuggestionsSectionTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/ntp/cards/SuggestionsSectionTest.java
@@ -38,7 +38,6 @@ import org.robolectric.annotation.Config;
 
 import org.chromium.base.Callback;
 import org.chromium.base.test.util.Feature;
-import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.DisableHistogramsRule;
 import org.chromium.chrome.browser.EnableFeatures;
 import org.chromium.chrome.browser.ntp.cards.ContentSuggestionsTestUtils.CategoryInfoBuilder;
@@ -93,7 +92,6 @@ public class SuggestionsSectionTest {
 
     @Test
     @Feature({"Ntp"})
-    @EnableFeatures(ChromeFeatureList.NTP_SUGGESTIONS_SECTION_DISMISSAL)
     public void testDismissSibling() {
         List<SnippetArticle> snippets = createDummySuggestions(3, TEST_CATEGORY_ID);
         SuggestionsSection section = createSectionWithReloadAction(true);
@@ -108,32 +106,6 @@ public class SuggestionsSectionTest {
         assertEquals(setOf(1, 2), section.getItemDismissalGroup(1));
         assertEquals(ItemViewType.ACTION, section.getItemViewType(2));
         assertEquals(setOf(1, 2), section.getItemDismissalGroup(2));
-
-        // With snippets.
-        section.setSuggestions(snippets, CategoryStatus.AVAILABLE, /* replaceExisting = */ true);
-        assertEquals(ItemViewType.HEADER, section.getItemViewType(0));
-        assertEquals(Collections.emptySet(), section.getItemDismissalGroup(0));
-        assertEquals(ItemViewType.SNIPPET, section.getItemViewType(1));
-        assertEquals(Collections.singleton(1), section.getItemDismissalGroup(1));
-    }
-
-    @Test
-    @Feature({"Ntp"})
-    @EnableFeatures({})
-    public void testDismissSiblingWithSectionDismissalDisabled() {
-        List<SnippetArticle> snippets = createDummySuggestions(3, TEST_CATEGORY_ID);
-        SuggestionsSection section = createSectionWithReloadAction(true);
-
-        section.setStatus(CategoryStatus.AVAILABLE);
-        assertNotNull(section.getActionItemForTesting());
-
-        // Without snippets.
-        assertEquals(ItemViewType.HEADER, section.getItemViewType(0));
-        assertEquals(Collections.emptySet(), section.getItemDismissalGroup(0));
-        assertEquals(ItemViewType.STATUS, section.getItemViewType(1));
-        assertEquals(Collections.emptySet(), section.getItemDismissalGroup(1));
-        assertEquals(ItemViewType.ACTION, section.getItemViewType(2));
-        assertEquals(Collections.emptySet(), section.getItemDismissalGroup(2));
 
         // With snippets.
         section.setSuggestions(snippets, CategoryStatus.AVAILABLE, /* replaceExisting = */ true);
@@ -260,7 +232,6 @@ public class SuggestionsSectionTest {
 
     @Test
     @Feature({"Ntp"})
-    @EnableFeatures({ChromeFeatureList.NTP_SUGGESTIONS_SECTION_DISMISSAL})
     public void testDismissSection() {
         SuggestionsSection section = createSectionWithReloadAction(false);
         section.setStatus(CategoryStatus.AVAILABLE);
@@ -725,7 +696,6 @@ public class SuggestionsSectionTest {
 
     @Test
     @Feature({"Ntp"})
-    @EnableFeatures(ChromeFeatureList.NTP_SUGGESTIONS_SECTION_DISMISSAL)
     public void testGetItemDismissalGroupWithSuggestions() {
         List<SnippetArticle> suggestions = createDummySuggestions(5, TEST_CATEGORY_ID);
         SuggestionsSection section = createSectionWithReloadAction(false);
@@ -737,7 +707,6 @@ public class SuggestionsSectionTest {
 
     @Test
     @Feature({"Ntp"})
-    @EnableFeatures(ChromeFeatureList.NTP_SUGGESTIONS_SECTION_DISMISSAL)
     public void testGetItemDismissalGroupWithActionItem() {
         SuggestionsSection section = createSectionWithReloadAction(true);
         assertThat(section.getItemDismissalGroup(1).size(), is(2));
@@ -746,7 +715,6 @@ public class SuggestionsSectionTest {
 
     @Test
     @Feature({"Ntp"})
-    @EnableFeatures(ChromeFeatureList.NTP_SUGGESTIONS_SECTION_DISMISSAL)
     public void testGetItemDismissalGroupWithoutActionItem() {
         SuggestionsSection section = createSectionWithReloadAction(false);
         assertThat(section.getItemDismissalGroup(1).size(), is(1));
