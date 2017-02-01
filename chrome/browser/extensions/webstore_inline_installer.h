2bce4b7a5d70717d2695ab47309d79420cde8e8a
robertshield <robertshield@chromium.org>
2017-02-01 07:18:33 -0800

    Include referrer chain with inline install requests.
    BUG=685905
    
    Review-Url: https://codereview.chromium.org/2655823002
    Cr-Commit-Position: refs/heads/master@{#447515}

--- chrome/browser/extensions/webstore_inline_installer.h
+++ chrome/browser/extensions/webstore_inline_installer.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_EXTENSIONS_WEBSTORE_INLINE_INSTALLER_H_
 #define CHROME_BROWSER_EXTENSIONS_WEBSTORE_INLINE_INSTALLER_H_
 
+#include <memory>
 #include <string>
 
 #include "base/macros.h"
@@ -50,6 +51,7 @@ class WebstoreInlineInstaller : public WebstoreStandaloneInstaller,
   ~WebstoreInlineInstaller() override;
 
   // Implementations WebstoreStandaloneInstaller Template Method's hooks.
+  std::string GetJsonPostData() override;
   bool CheckRequestorAlive() const override;
   const GURL& GetRequestorURL() const override;
   bool ShouldShowPostInstallUI() const override;
