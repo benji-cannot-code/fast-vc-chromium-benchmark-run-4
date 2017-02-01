86cd6e212fb91daa027ef40d0836384b4d2140e4
hs1217.lee <hs1217.lee@samsung.com>
2017-01-31 16:40:57 -0800

    [GeometryInterface] add fromQuad function in DOMQuad.
    The fromQuad(other) static method on DOMQuad must create
    a DOMQuad from the DOMQuadInit dictionary other.
    To create a DOMQuad from a DOMQuadInit dictionary other,
    follow these steps:
    1.Let point 1 be the result of invoking create a DOMPoint
     from the dictionary p1 dictionary member of other, if it exists.
    2.Let point 2 be the result of invoking create a DOMPoint
     from the dictionary p2 dictionary member of other, if it exists.
    3.Let point 3 be the result of invoking create a DOMPoint
     from the dictionary p3 dictionary member of other, if it exists.
    4.Let point 4 be the result of invoking create a DOMPoint
     from the dictionary p4 dictionary member of other, if it exists.
    5.Return a new DOMQuad with p1 set to point 1, p2 set to point 2,
     p3 set to point 3 and p4 set to point 4.
    
    spec list:
    https://drafts.fxtf.org/geometry/#dom-domquad-fromquad
    
    BUG=678237
    
    Review-Url: https://codereview.chromium.org/2638413005
    Cr-Commit-Position: refs/heads/master@{#447383}

--- third_party/WebKit/Source/core/dom/DOMQuad.cpp
+++ third_party/WebKit/Source/core/dom/DOMQuad.cpp
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "bindings/core/v8/V8ObjectBuilder.h"
 #include "core/dom/DOMPoint.h"
+#include "core/dom/DOMQuadInit.h"
 #include "core/dom/DOMRectInit.h"
 
 namespace blink {
@@ -20,6 +21,13 @@ DOMQuad* DOMQuad::create(const DOMPointInit& p1,
 
 DOMQuad* DOMQuad::fromRect(const DOMRectInit& other) {
   return new DOMQuad(other.x(), other.y(), other.width(), other.height());
+}
+
+DOMQuad* DOMQuad::fromQuad(const DOMQuadInit& other) {
+  return new DOMQuad(other.hasP1() ? other.p1() : DOMPointInit(),
+                     other.hasP2() ? other.p2() : DOMPointInit(),
+                     other.hasP3() ? other.p3() : DOMPointInit(),
+                     other.hasP3() ? other.p4() : DOMPointInit());
 }
 
 DOMQuad::DOMQuad(const DOMPointInit& p1,
