f7203a32cbe6a2bab8458c08b001092d6a12ee77
blundell <blundell@chromium.org>
2017-02-01 09:27:27 -0800

    Move impl of identity.GetProfileUserInfo() extension API to its own file
    As a precursor to a refactoring of the identity extension API implementation
    to use the Identity Service, I am moving the implementations of the six
    identity extension API functions to their own files, rather than all
    being lumped together in identity_api.*. This will make it easier to
    analyze each of these function implementations independently and
    incrementally convert them to use the Identity Service.
    
    Of note: This CL makes a minor code change to eliminate usage of the
    GetPrimaryAccountId() helper function defined in identity_api.cc.
    The new code is functionally equivalent to the old code: both are
    effectively AccountTrackerService::GetAccountInfo(
        SigninManager::GetAuthenticatedAccountId()), wherein the
    AccountTrackerService and SigninManager instances are the ones
    corresponding to the Profile in question.
    
    BUG=685654
    
    Review-Url: https://codereview.chromium.org/2663883003
    Cr-Commit-Position: refs/heads/master@{#447548}

--- chrome/browser/extensions/api/identity/identity_apitest.cc
+++ chrome/browser/extensions/api/identity/identity_apitest.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #include "chrome/browser/extensions/api/identity/identity_api.h"
 #include "chrome/browser/extensions/api/identity/identity_constants.h"
+#include "chrome/browser/extensions/api/identity/identity_get_profile_user_info_function.h"
 #include "chrome/browser/extensions/api/identity/identity_launch_web_auth_flow_function.h"
 #include "chrome/browser/extensions/api/identity/identity_remove_cached_auth_token_function.h"
 #include "chrome/browser/extensions/component_loader.h"
