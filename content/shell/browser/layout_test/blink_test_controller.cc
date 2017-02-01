e112b4462777ae4455609dee4ee7f1ca9359a160
lukasza <lukasza@chromium.org>
2017-02-01 12:21:05 -0800

    Revert of Replace IPC messages in layout_test_render_frame_observer. (patchset #17 id:330001 of https://codereview.chromium.org/2594913002/ )
    Reason for revert:
    This CL breaks linux_site_isolation trybot required by the CQ for some CLs - let's try to revert.  See https://crbug.com/687353 for more information.
    
    Original issue's description:
    > Replace IPC messages in layout_test_render_frame_observer.
    >
    > Also remove some forward declarations that are becoming useless.
    >
    > BUG=577685
    >
    > Review-Url: https://codereview.chromium.org/2594913002
    > Cr-Commit-Position: refs/heads/master@{#446952}
    > Committed: https://chromium.googlesource.com/chromium/src/+/165c6d9ac3870f1c12553eff57ea52b1c67a1fa1
    
    TBR=mkwst@chromium.org,jam@chromium.org,sammc@chromium.org,tibell@chromium.org,jiameng@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=577685
    
    Review-Url: https://codereview.chromium.org/2670573002
    Cr-Commit-Position: refs/heads/master@{#447594}

--- content/shell/browser/layout_test/blink_test_controller.cc
+++ content/shell/browser/layout_test/blink_test_controller.cc
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/service_worker_context.h"
 #include "content/public/browser/storage_partition.h"
 #include "content/public/browser/web_contents.h"
-#include "content/public/common/associated_interface_provider.h"
 #include "content/public/common/bindings_policy.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/url_constants.h"
@@ -282,7 +281,6 @@ bool BlinkTestController::PrepareForLayoutTest(
   all_observed_render_process_hosts_.clear();
   main_window_render_process_hosts_.clear();
   accumulated_layout_test_runtime_flags_changes_.Clear();
-  layout_test_control_map_.clear();
   ShellBrowserContext* browser_context =
       ShellContentBrowserClient::Get()->browser_context();
   is_compositing_test_ =
@@ -608,26 +606,24 @@ void BlinkTestController::HandleNewRenderFrameHost(RenderFrameHost* frame) {
 
     // Make sure the new renderer process has a test configuration shared with
     // other renderers.
-    mojom::ShellTestConfigurationPtr params =
-        mojom::ShellTestConfiguration::New();
-    params->enable_pixel_dumping = true;
-    params->allow_external_pages = false;
-    params->current_working_directory = current_working_directory_;
-    params->temp_path = temp_path_;
-    params->test_url = test_url_;
-    params->enable_pixel_dumping = enable_pixel_dumping_;
-    params->allow_external_pages =
+    ShellTestConfiguration params;
+    params.current_working_directory = current_working_directory_;
+    params.temp_path = temp_path_;
+    params.test_url = test_url_;
+    params.enable_pixel_dumping = enable_pixel_dumping_;
+    params.allow_external_pages =
         base::CommandLine::ForCurrentProcess()->HasSwitch(
             switches::kAllowExternalPages);
-    params->expected_pixel_hash = expected_pixel_hash_;
-    params->initial_size = initial_size_;
+    params.expected_pixel_hash = expected_pixel_hash_;
+    params.initial_size = initial_size_;
 
     if (did_send_initial_test_configuration_) {
-      GetLayoutTestControlPtr(frame)->ReplicateTestConfiguration(
-          std::move(params));
+      frame->Send(new ShellViewMsg_ReplicateTestConfiguration(
+          frame->GetRoutingID(), params));
     } else {
       did_send_initial_test_configuration_ = true;
-      GetLayoutTestControlPtr(frame)->SetTestConfiguration(std::move(params));
+      frame->Send(
+          new ShellViewMsg_SetTestConfiguration(frame->GetRoutingID(), params));
     }
   }
 
@@ -637,7 +633,8 @@ void BlinkTestController::HandleNewRenderFrameHost(RenderFrameHost* frame) {
     all_observed_render_process_hosts_.insert(process);
 
     if (!main_window) {
-      GetLayoutTestControlPtr(frame)->SetupSecondaryRenderer();
+      frame->Send(
+          new ShellViewMsg_SetupSecondaryRenderer(frame->GetRoutingID()));
     }
 
     process->Send(new LayoutTestMsg_ReplicateLayoutTestRuntimeFlagsChanges(
@@ -708,16 +705,8 @@ void BlinkTestController::OnTextDump(const std::string& dump) {
 }
 
 void BlinkTestController::OnInitiateLayoutDump() {
-  int number_of_messages = 0;
-  for (RenderFrameHost* rfh : main_window_->web_contents()->GetAllFrames()) {
-    if (!rfh->IsRenderFrameLive())
-      continue;
-
-    ++number_of_messages;
-    GetLayoutTestControlPtr(rfh)->LayoutDumpRequest();
-  }
-
-  pending_layout_dumps_ = number_of_messages;
+  pending_layout_dumps_ = main_window_->web_contents()->SendToAllFrames(
+      new ShellViewMsg_LayoutDumpRequest(MSG_ROUTING_NONE));
 }
 
 void BlinkTestController::OnLayoutTestRuntimeFlagsChanged(
@@ -963,16 +952,6 @@ void BlinkTestController::OnSendBluetoothManualChooserEvent(
     return;
   }
   bluetooth_chooser_factory_->SendEvent(event, argument);
-}
-
-mojom::LayoutTestControl* BlinkTestController::GetLayoutTestControlPtr(
-    RenderFrameHost* frame) {
-  if (layout_test_control_map_.find(frame) == layout_test_control_map_.end()) {
-    frame->GetRemoteAssociatedInterfaces()->GetInterface(
-        &layout_test_control_map_[frame]);
-  }
-  DCHECK(layout_test_control_map_[frame].get());
-  return layout_test_control_map_[frame].get();
 }
 
 }  // namespace content
