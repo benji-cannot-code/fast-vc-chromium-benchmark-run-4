6d31a7774035efb7161bf0095f44846986193b2c
dbeam <dbeam@chromium.org>
2017-02-01 12:29:27 -0800

    MD Downloads: add a <base> tag to fix chrome://download/a/b/
    R=dpapad@chromium.org
    BUG=none
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2666123003
    Cr-Commit-Position: refs/heads/master@{#447597}

--- chrome/test/data/webui/md_downloads/downloads_browsertest.js
+++ chrome/test/data/webui/md_downloads/downloads_browsertest.js
@@ -26,6 +26,9 @@ DownloadsTest.prototype = {
     PolymerTest.prototype.setUp.call(this);
     this.accessibilityAuditConfig.ignoreSelectors('humanLangMissing', 'html');
   },
+
+  /** @override */
+  extraLibraries: PolymerTest.getLibraries(ROOT_PATH),
 };
 
 /**
@@ -41,7 +44,7 @@ DownloadsItemTest.prototype = {
   browsePreload: 'chrome://downloads/item.html',
 
   /** @override */
-  extraLibraries: PolymerTest.getLibraries(ROOT_PATH).concat([
+  extraLibraries: DownloadsTest.prototype.extraLibraries.concat([
     'item_tests.js',
   ]),
 };
@@ -63,7 +66,7 @@ DownloadsLayoutTest.prototype = {
   browsePreload: 'chrome://downloads/',
 
   /** @override */
-  extraLibraries: PolymerTest.getLibraries(ROOT_PATH).concat([
+  extraLibraries: DownloadsTest.prototype.extraLibraries.concat([
     'layout_tests.js',
   ]),
 };
@@ -85,11 +88,33 @@ DownloadsToolbarTest.prototype = {
   browsePreload: 'chrome://downloads/toolbar.html',
 
   /** @override */
-  extraLibraries: PolymerTest.getLibraries(ROOT_PATH).concat([
+  extraLibraries: DownloadsTest.prototype.extraLibraries.concat([
     'toolbar_tests.js',
   ]),
 };
 
 TEST_F('DownloadsToolbarTest', 'All', function() {
+  mocha.run();
+});
+
+/**
+ * @constructor
+ * @extends {DownloadsTest}
+ */
+function DownloadsUrlTest() {}
+
+DownloadsUrlTest.prototype = {
+  __proto__: DownloadsTest.prototype,
+
+  /** @override */
+  browsePreload: 'chrome://downloads/a/b/',
+};
+
+TEST_F('DownloadsUrlTest', 'All', function() {
+  suite('loading a nonexistent URL of /a/b/', function() {
+    test('should yield no console errors', function() {
+      assertEquals(location.href, DownloadsUrlTest.prototype.browsePreload);
+    });
+  });
   mocha.run();
 });
