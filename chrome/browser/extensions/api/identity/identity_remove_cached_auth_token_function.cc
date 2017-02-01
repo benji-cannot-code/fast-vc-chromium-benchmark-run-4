9d9777c5ff5b8b76e036cf6286a557ef83076ab2
blundell <blundell@chromium.org>
2017-01-31 23:16:40 -0800

    Identity extension API impl: Split constants into their own file
    As part of the process of making the individual identity extension API
    function impls easier to analyze in isolation, this CL splits constants
    out of identity_api.* into their own separate file. Doing so makes it
    clear (1) which files actually need IdentityAPI and (2) which other
    dependencies these files need (since identity_api.h has a fairly large
    transitive set of includes).
    
    BUG=685654
    
    Review-Url: https://codereview.chromium.org/2661913002
    Cr-Commit-Position: refs/heads/master@{#447467}

--- chrome/browser/extensions/api/identity/identity_remove_cached_auth_token_function.cc
+++ chrome/browser/extensions/api/identity/identity_remove_cached_auth_token_function.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/identity/identity_remove_cached_auth_token_function.h"
 
 #include "chrome/browser/extensions/api/identity/identity_api.h"
+#include "chrome/browser/extensions/api/identity/identity_constants.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/extensions/api/identity.h"
 
