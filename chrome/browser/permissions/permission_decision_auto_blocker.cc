46fa61eaa3a3ee37e1c89b0431057f4924e6372b
raymes <raymes@chromium.org>
2017-02-01 12:03:35 -0800

    Migrate content setting PROMPT_NO_DECISION_COUNT to PERMISSION_AUTOBLOCKER_DATA
    The PROMPT_NO_DECISION_COUNT name is no longer accurate so we migrate
    the data to a new content setting. The old content setting can be
    removed after M60. A test is added to ensure the migration works
    properly.
    
    BUG=681709
    
    Review-Url: https://codereview.chromium.org/2651123002
    Cr-Commit-Position: refs/heads/master@{#447585}

--- chrome/browser/permissions/permission_decision_auto_blocker.cc
+++ chrome/browser/permissions/permission_decision_auto_blocker.cc
@@ -48,14 +48,12 @@ int g_dismissal_embargo_days = 7;
 // TODO(meredithl): Revisit this once UMA metrics have data about request time.
 const int kCheckUrlTimeoutMs = 2000;
 
-// TODO(meredithl): Migrate to a new and more fitting type, once metrics have
-// been gathered, and deprecate CONTENT_SETTINGS_TYPE_PROMPT_NO_DECISION_COUNT.
 std::unique_ptr<base::DictionaryValue> GetOriginDict(
     HostContentSettingsMap* settings,
     const GURL& origin_url) {
   std::unique_ptr<base::DictionaryValue> dict =
       base::DictionaryValue::From(settings->GetWebsiteSetting(
-          origin_url, GURL(), CONTENT_SETTINGS_TYPE_PROMPT_NO_DECISION_COUNT,
+          origin_url, GURL(), CONTENT_SETTINGS_TYPE_PERMISSION_AUTOBLOCKER_DATA,
           std::string(), nullptr));
   if (!dict)
     return base::MakeUnique<base::DictionaryValue>();
@@ -93,7 +91,7 @@ int RecordActionInWebsiteSettings(const GURL& url,
   permission_dict->SetInteger(key, ++current_count);
 
   map->SetWebsiteSettingDefaultScope(
-      url, GURL(), CONTENT_SETTINGS_TYPE_PROMPT_NO_DECISION_COUNT,
+      url, GURL(), CONTENT_SETTINGS_TYPE_PERMISSION_AUTOBLOCKER_DATA,
       std::string(), std::move(dict));
 
   return current_count;
@@ -194,7 +192,7 @@ void PermissionDecisionAutoBlocker::RemoveCountsByUrl(
 
   std::unique_ptr<ContentSettingsForOneType> settings(
       new ContentSettingsForOneType);
-  map->GetSettingsForOneType(CONTENT_SETTINGS_TYPE_PROMPT_NO_DECISION_COUNT,
+  map->GetSettingsForOneType(CONTENT_SETTINGS_TYPE_PERMISSION_AUTOBLOCKER_DATA,
                              std::string(), settings.get());
 
   for (const auto& site : *settings) {
@@ -202,7 +200,7 @@ void PermissionDecisionAutoBlocker::RemoveCountsByUrl(
 
     if (origin.is_valid() && filter.Run(origin)) {
       map->SetWebsiteSettingDefaultScope(
-          origin, GURL(), CONTENT_SETTINGS_TYPE_PROMPT_NO_DECISION_COUNT,
+          origin, GURL(), CONTENT_SETTINGS_TYPE_PERMISSION_AUTOBLOCKER_DATA,
           std::string(), nullptr);
     }
   }
@@ -335,7 +333,6 @@ bool PermissionDecisionAutoBlocker::IsUnderEmbargo(
   return is_under_dismiss_embargo || is_under_blacklist_embargo;
 }
 
-// static
 void PermissionDecisionAutoBlocker::CheckSafeBrowsingResult(
     content::PermissionType permission,
     const GURL& request_origin,
@@ -350,7 +347,6 @@ void PermissionDecisionAutoBlocker::CheckSafeBrowsingResult(
   callback.Run(should_be_embargoed /* permission blocked */);
 }
 
-// static
 void PermissionDecisionAutoBlocker::PlaceUnderEmbargo(
     content::PermissionType permission,
     const GURL& request_origin,
@@ -363,7 +359,7 @@ void PermissionDecisionAutoBlocker::PlaceUnderEmbargo(
       dict.get(), PermissionUtil::GetPermissionString(permission));
   permission_dict->SetDouble(key, clock_->Now().ToInternalValue());
   map->SetWebsiteSettingDefaultScope(
-      request_origin, GURL(), CONTENT_SETTINGS_TYPE_PROMPT_NO_DECISION_COUNT,
+      request_origin, GURL(), CONTENT_SETTINGS_TYPE_PERMISSION_AUTOBLOCKER_DATA,
       std::string(), std::move(dict));
 }
 
