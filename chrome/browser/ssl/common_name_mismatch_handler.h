2d1a0684b56cab306b19f99a6a0b3882aadace45
meacer <meacer@chromium.org>
2017-01-31 17:42:58 -0800

    Refactor SSLErrorHandler for better testability.
    This CL adds a SSLErrorHandler::Delegate class which the unit tests can
    provide, instead of overriding SSLErrorHandler methods. This allows
    testing of UMA histograms in the unit tests, and provides a slightly
    better code coverage for SSLErrorHandler code.
    
    BUG=640835
    
    Review-Url: https://codereview.chromium.org/2658263003
    Cr-Commit-Position: refs/heads/master@{#447403}

--- chrome/browser/ssl/common_name_mismatch_handler.h
+++ chrome/browser/ssl/common_name_mismatch_handler.h
@@ -42,8 +42,9 @@ class CommonNameMismatchHandler : public net::URLFetcherDelegate,
     IGNORE_REQUESTS_FOR_TESTING
   };
 
-  typedef base::Callback<void(const SuggestedUrlCheckResult& result,
-                              const GURL& suggested_url)> CheckUrlCallback;
+  typedef base::Callback<void(SuggestedUrlCheckResult result,
+                              const GURL& suggested_url)>
+      CheckUrlCallback;
 
   CommonNameMismatchHandler(
       const GURL& request_url,
