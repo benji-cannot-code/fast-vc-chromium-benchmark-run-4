4a96928ec8fbb87190913a131d5d3cbf855a20ff
cwallez <cwallez@chromium.org>
2017-02-01 08:02:09 -0800

    Revert of Copy GPU workaround flags from browser to GPU process (patchset #4 id:20002 of https://codereview.chromium.org/2644733005/ )
    Reason for revert:
    There is a gpu_unittests failure on OSX:
    AssertionError: Browser and GPU process list of driver bugworkarounds are not equal
    
    Starting from https://build.chromium.org/p/chromium.gpu.fyi/builders/Mac%20Retina%20Release/builds/10498
    
    This CL seems like the culprit, reverting.
    
    Original issue's description:
    > Copy GPU workaround flags from browser to GPU process
    >
    > BUG=682912
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    >
    > Review-Url: https://codereview.chromium.org/2644733005
    > Cr-Commit-Position: refs/heads/master@{#447356}
    > Committed: https://chromium.googlesource.com/chromium/src/+/ded062d6c02a0cbfa89c90dae364c48f9118a328
    
    TBR=kbr@chromium.org,zmo@chromium.org,kainino@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=682912
    
    Review-Url: https://codereview.chromium.org/2666353002
    Cr-Commit-Position: refs/heads/master@{#447523}

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -58,7 +58,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/service_names.mojom.h"
 #include "gpu/command_buffer/service/gpu_preferences.h"
 #include "gpu/command_buffer/service/gpu_switches.h"
-#include "gpu/config/gpu_driver_bug_list.h"
 #include "gpu/ipc/host/shader_disk_cache.h"
 #include "gpu/ipc/service/switches.h"
 #include "ipc/ipc_channel_handle.h"
@@ -1057,11 +1056,6 @@ bool GpuProcessHost::LaunchGpuProcess(gpu::GpuPreferences* gpu_preferences) {
   cmd_line->CopySwitchesFrom(
       browser_command_line, switches::kGLSwitchesCopiedFromGpuProcessHost,
       switches::kGLSwitchesCopiedFromGpuProcessHostNumSwitches);
-
-  std::vector<const char*> gpu_workarounds;
-  gpu::GpuDriverBugList::AppendAllWorkarounds(&gpu_workarounds);
-  cmd_line->CopySwitchesFrom(browser_command_line,
-      gpu_workarounds.data(), gpu_workarounds.size());
 
   GetContentClient()->browser()->AppendExtraCommandLineSwitches(
       cmd_line.get(), process_->GetData().id);
