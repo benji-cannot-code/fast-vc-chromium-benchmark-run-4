797419474ada651daa20e802b0de6c535a7095c4
aazzam <aazzam@google.com>
2017-01-31 18:51:10 -0800

    Implements CSSPropertyAPI for the margin properties.
    A part of Project Ribbon, separating the parsing logic for CSS
    properties from the parser into an API. This patch removes
    CSSPropertyMargin{Top,Right,Bottom,Left},
    CSSProperty{Top,Right,Bottom,Left}, and
    CSSPropertyWebkitMargin[Top|Right|Bottom|Left] from the switch statement
    in parseSingleValue, and calls the API instead.
    
    A function pointer to the parseSingleValue function from the API for the
    margin properties is stored in a CSSPropertyDescriptor, and is
    called from CSSPropertyParser.
    
    This patch:
    - Adds CSSPropertyAPIMargin.cpp and CSSPropertyAPIWebkitMargin.cpp to
      the BUILD.gn file.
    - Adds api_class=CSSPropertyAPIMargin and
      api_class=CSSPropertyAPIWebkitMargin flag in CSSProperties.in to each
      of the margin and -webkit-margin properties respectively, which
      indicates that CSSPropertyAPIMargin.h and CSSPropertyWebkitMargin.h
      are generated.
    - Moves the parsing logic for radius from CSSPropertyParser.cpp
      to CSSPropertyAPI[Webkit]Margin.cpp, which implements CSSPropertyAPI.h.
    
    BUG=668012
    
    Review-Url: https://codereview.chromium.org/2648703002
    Cr-Commit-Position: refs/heads/master@{#447430}

---
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyAPIMargin.cpp
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/properties/CSSPropertyAPIMargin.h"
+
+#include "core/css/parser/CSSParserContext.h"
+#include "core/css/parser/CSSPropertyParserHelpers.h"
+#include "core/css/properties/CSSPropertyMarginUtils.h"
+
+namespace blink {
+
+const CSSValue* CSSPropertyAPIMargin::parseSingleValue(
+    CSSParserTokenRange& range,
+    const CSSParserContext* context) {
+  return CSSPropertyMarginUtils::consumeMarginOrOffset(
+      range, context->mode(), CSSPropertyParserHelpers::UnitlessQuirk::Allow);
+}
+
+}  // namespace blink
