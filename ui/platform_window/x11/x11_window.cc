b0550e8115ca985f601088b101097d7056cf386b
kylechar <kylechar@chromium.org>
2017-01-31 19:28:52 -0800

    Fix aura_unittests and view_unittests for Ozone X11.
    There are assumptions in the code that PlatformWindow::SetBounds() will
    change PlatformWindow bounds immediately. This isn't quite true with
    X11, as the operation is async, but we can just assume SetBounds() was
    successful. This is what WindowTreeHostX11 does.
    
    The PlatformWindow will get a ConfigureNotify message with the actual
    bounds later and if they don't match the requested bounds it will notify
    the delegate of the new bounds.
    
    Also, change Ozone CrOS bot to run aura_unittests and view_unittest with
    Ozone X11 now that they work.
    
    BUG=664570
    
    Review-Url: https://codereview.chromium.org/2665193002
    Cr-Commit-Position: refs/heads/master@{#447437}

--- ui/platform_window/x11/x11_window.cc
+++ ui/platform_window/x11/x11_window.cc
@@ -18,8 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
-X11Window::X11Window(PlatformWindowDelegate* delegate)
-    : X11WindowBase(delegate) {
+X11Window::X11Window(PlatformWindowDelegate* delegate, const gfx::Rect& bounds)
+    : X11WindowBase(delegate, bounds) {
   DCHECK(PlatformEventSource::GetInstance());
   PlatformEventSource::GetInstance()->AddPlatformEventDispatcher(this);
 }
