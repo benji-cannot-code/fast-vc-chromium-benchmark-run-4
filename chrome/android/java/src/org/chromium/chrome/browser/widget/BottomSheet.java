1578f3ca545307f4ca11138e91a6f4a44d10135e
mdjones <mdjones@chromium.org>
2017-02-01 15:27:57 -0800

    [Home] Add placeholder if bottom sheet has no content
    This change is to avoid a transparent hole in the bottom sheet if the
    content has not been set. Instead, a blank white view will be shown.
    This view is detached once content is loaded.
    
    BUG=671361
    
    Review-Url: https://codereview.chromium.org/2668503007
    Cr-Commit-Position: refs/heads/master@{#447642}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/BottomSheet.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/BottomSheet.java
@@ -22,6 +22,7 @@ import android.view.animation.DecelerateInterpolator;
 import android.view.animation.Interpolator;
 import android.widget.FrameLayout;
 
+import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.suggestions.SuggestionsBottomSheetContent;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
@@ -113,6 +114,9 @@ public class BottomSheet extends FrameLayout {
 
     /** A handle to the toolbar control container. */
     private View mControlContainer;
+
+    /** A placeholder for if there is no content in the bottom sheet. */
+    private View mPlaceholder;
 
     /** A handle to the FrameLayout that holds the content of the bottom sheet. */
     private FrameLayout mBottomSheetContentContainer;
@@ -343,6 +347,13 @@ public class BottomSheet extends FrameLayout {
                 setSheetState(mCurrentState, false);
             }
         });
+
+        mPlaceholder = new View(getContext());
+        LayoutParams placeHolderParams =
+                new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        mPlaceholder.setBackgroundColor(
+                ApiCompatibilityUtils.getColor(getResources(), android.R.color.white));
+        mBottomSheetContentContainer.addView(mPlaceholder, placeHolderParams);
     }
 
     /**
@@ -385,8 +396,12 @@ public class BottomSheet extends FrameLayout {
             mSheetContent = null;
         }
 
-        if (content == null) return;
+        if (content == null) {
+            mBottomSheetContentContainer.addView(mPlaceholder);
+            return;
+        }
 
+        mBottomSheetContentContainer.removeView(mPlaceholder);
         mSheetContent = content;
         mBottomSheetContentContainer.addView(mSheetContent.getScrollingContentView());
     }
