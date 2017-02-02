a3813a976e5632288d8aecd711fd10137a9ad4b5
msw <msw@chromium.org>
2017-02-02 11:34:53 -0800

    Reland: mash: Fix MusPropertyMirrorAsh for owned properties; add test.
    The original CL (PS1) was reverted for test failures, which I fixed here.
    Differences from the the original CL:
    -Make ash/public/cpp a component to support exporting symbols
    (this allows ash and clients to share window property keys)
    (rename cpp component to ash_public_cpp to avoid name conflict)
    -Move ShelfItem back to ash/common/shelf (shouldn't be used externally)
    -Move the shelf item types into ash/public/cpp/shelf_types.h
    -TODO: rename ash/common/shelf/shelf_item_types.* to shelf_item.*
    
    Modified Original Description:
    
    mash: Fix MusPropertyMirrorAsh for owned properties; add test.
    
    I fixed gfx::ImageSkia in: https://codereview.chromium.org/2667743003
    This CL fixes the owned string properties similarly.
    Move the ash mirror to it's own file; add mash_unittest fixtures.
    Remove the ShelfID mirroring (this prop hopefully shouldn't be needed)
    
    Fallout from move to ash/public/cpp:
    -Move some shelf_item_types.h to ash/public/cpp/shelf_types.h
    -Move kInvalidShelfID to avoid moving shelf_constants.h (better home)
    -Move two mirrored ash window properties to ash/public/cpp
    
    BUG=686936
    TEST=automated; no crashes running apps on chrome --mash.
    R=sky@chromium.org
    
    Review-Url: https://codereview.chromium.org/2669633003
    Cr-Commit-Position: refs/heads/master@{#447455}
    Committed: https://chromium.googlesource.com/chromium/src/+/612e45de4250e73fa0df767121d7d6ae4f2c9c83
    
    patch from issue 2669633003 at patchset 100001 (http://crrev.com/2669633003#ps100001)
    
    Review-Url: https://codereview.chromium.org/2664113004
    Cr-Commit-Position: refs/heads/master@{#447819}

--- chrome/browser/ui/views/apps/chrome_native_app_window_views_aura_ash.cc
+++ chrome/browser/ui/views/apps/chrome_native_app_window_views_aura_ash.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/common/wm/window_state_observer.h"
 #include "ash/common/wm_window.h"
 #include "ash/public/cpp/shell_window_ids.h"
+#include "ash/public/cpp/window_properties.h"
 #include "ash/screen_util.h"
 #include "ash/shared/app_types.h"
 #include "ash/shared/immersive_fullscreen_controller.h"
