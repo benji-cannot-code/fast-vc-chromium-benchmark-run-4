c07754e05bf8308c2f47510ce31697fddab53b29
riajiang <riajiang@chromium.org>
2017-02-01 17:50:31 -0800

    Remove virtual function WindowTreeClientDelegate::GetCaptureClient() and WindowTreeClient::GetCaptureClient().
    This CL is separated out from https://codereview.chromium.org/2626013005
    
    BUG=676002, 678057
    
    Review-Url: https://codereview.chromium.org/2657873006
    Cr-Commit-Position: refs/heads/master@{#447680}

--- mash/simple_wm/simple_wm.cc
+++ mash/simple_wm/simple_wm.cc
@@ -400,10 +400,6 @@ void SimpleWM::OnPointerEventObserved(const ui::PointerEvent& event,
   // Don't care.
 }
 
-aura::client::CaptureClient* SimpleWM::GetCaptureClient() {
-  return wm_state_.capture_controller();
-}
-
 aura::PropertyConverter* SimpleWM::GetPropertyConverter() {
   return &property_converter_;
 }
--- ui/views/mus/mus_client.cc
+++ ui/views/mus/mus_client.cc
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/views_delegate.h"
 #include "ui/views/widget/desktop_aura/desktop_native_widget_aura.h"
 #include "ui/views/widget/widget_delegate.h"
-#include "ui/wm/core/capture_controller.h"
 #include "ui/wm/core/wm_state.h"
 
 // Widget::InitParams::Type must match that of ui::mojom::WindowType.
@@ -271,10 +270,6 @@ void MusClient::OnPointerEventObserved(const ui::PointerEvent& event,
 void MusClient::OnWindowManagerFrameValuesChanged() {
   for (auto& observer : observer_list_)
     observer.OnWindowManagerFrameValuesChanged();
-}
-
-aura::client::CaptureClient* MusClient::GetCaptureClient() {
-  return wm::CaptureController::Get();
 }
 
 aura::PropertyConverter* MusClient::GetPropertyConverter() {
