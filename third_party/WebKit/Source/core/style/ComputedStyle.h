7633836ea30495d9ce043873219703ec82d3bd3f
shend <shend@chromium.org>
2017-02-01 22:39:27 -0800

    Rename EOverflow values with k prefix.
    Rename the values of the EOverflow enum with the k prefix to match
    the style generated by make_computed_style_base.py, which is in line with
    the Blink rename to match Chromium style. This also prefixes PagedX and
    PagedY with Webkit since they are webkit specific keywords. This is
    prework for generating EOverflow.
    
    BUG=684966
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2660423004
    Cr-Commit-Position: refs/heads/master@{#447711}

--- third_party/WebKit/Source/core/style/ComputedStyle.h
+++ third_party/WebKit/Source/core/style/ComputedStyle.h
@@ -1466,7 +1466,7 @@ class CORE_EXPORT ComputedStyle : public ComputedStyleBase,
   }
 
   // overflow-x
-  static EOverflow initialOverflowX() { return EOverflow::Visible; }
+  static EOverflow initialOverflowX() { return EOverflow::kVisible; }
   EOverflow overflowX() const {
     return static_cast<EOverflow>(m_nonInheritedData.m_overflowX);
   }
@@ -1475,7 +1475,7 @@ class CORE_EXPORT ComputedStyle : public ComputedStyleBase,
   }
 
   // overflow-y
-  static EOverflow initialOverflowY() { return EOverflow::Visible; }
+  static EOverflow initialOverflowY() { return EOverflow::kVisible; }
   EOverflow overflowY() const {
     return static_cast<EOverflow>(m_nonInheritedData.m_overflowY);
   }
@@ -3382,11 +3382,12 @@ class CORE_EXPORT ComputedStyle : public ComputedStyleBase,
   // It's sufficient to just check one direction, since it's illegal to have
   // visible on only one overflow value.
   bool isOverflowVisible() const {
-    DCHECK(overflowX() != EOverflow::Visible || overflowX() == overflowY());
-    return overflowX() == EOverflow::Visible;
+    DCHECK(overflowX() != EOverflow::kVisible || overflowX() == overflowY());
+    return overflowX() == EOverflow::kVisible;
   }
   bool isOverflowPaged() const {
-    return overflowY() == EOverflow::PagedX || overflowY() == EOverflow::PagedY;
+    return overflowY() == EOverflow::kWebkitPagedX ||
+           overflowY() == EOverflow::kWebkitPagedY;
   }
 
   // Visibility utility functions.
@@ -3585,8 +3586,10 @@ class CORE_EXPORT ComputedStyle : public ComputedStyleBase,
     // If the pagination axis is parallel with the writing mode inline axis,
     // columns may be laid out along the inline axis, just like for regular
     // multicol. Otherwise, we need to lay out along the block axis.
-    if (isOverflowPaged())
-      return (overflowY() == EOverflow::PagedX) == isHorizontalWritingMode();
+    if (isOverflowPaged()) {
+      return (overflowY() == EOverflow::kWebkitPagedX) ==
+             isHorizontalWritingMode();
+    }
     return false;
   }
 
