e631996a86c3ce19c66b917f614e0b17adfe282a
zpeng <zpeng@chromium.org>
2017-02-02 15:19:04 -0800

    Update AddToHomescreenDataFetcher to accept badge icon for WebAPK.
    Update AddToHomescreenDataFetcher to accept badge icon if data is
    requested for WebAPK. As badge icon is processed for notification
    purpose in NotificationBuilderBase::setSmallIcon, we do not process the
    badge icon the same way we process the primary/launcher icon.
    
    BUG=649771
    
    Review-Url: https://codereview.chromium.org/2669133003
    Cr-Commit-Position: refs/heads/master@{#447870}

--- chrome/browser/android/webapps/add_to_homescreen_data_fetcher_unittest.cc
+++ chrome/browser/android/webapps/add_to_homescreen_data_fetcher_unittest.cc
@@ -132,7 +132,8 @@ class ObserverWaiter : public AddToHomescreenDataFetcher::Observer {
   }
 
   void OnDataAvailable(const ShortcutInfo& info,
-                       const SkBitmap& icon) override {
+                       const SkBitmap& primary_icon,
+                       const SkBitmap& badge_icon) override {
     data_available_ = true;
     if (!quit_closure_.is_null())
       quit_closure_.Run();
@@ -204,7 +205,7 @@ class AddToHomescreenDataFetcherTest : public ChromeRenderViewHostTestHarness {
   scoped_refptr<AddToHomescreenDataFetcher> BuildFetcher(
       bool check_webapk_compatible,
       AddToHomescreenDataFetcher::Observer* observer) {
-    return new AddToHomescreenDataFetcher(web_contents(), 1, 1, 1, 1,
+    return new AddToHomescreenDataFetcher(web_contents(), 1, 1, 1, 1, 1,
                                           check_webapk_compatible, observer);
   }
 
