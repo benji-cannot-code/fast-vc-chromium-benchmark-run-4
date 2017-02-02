3b28f21c285adaa3fd6e10b2efd61f2b92e5e90a
jrummell <jrummell@chromium.org>
2017-02-02 14:15:09 -0800

    Add checked_cast to size_t conversion in multibuffer_data_source.cc
    /analyze on Windows complains about the size_t conversion, so add a
    checked_cast call to do the conversion. Memory usage is limited to 4Gb,
    so this should never fail.
    
    BUG=687394
    TEST=compiles
    
    Review-Url: https://codereview.chromium.org/2671613003
    Cr-Commit-Position: refs/heads/master@{#447855}

--- media/blink/multibuffer_data_source.cc
+++ media/blink/multibuffer_data_source.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "base/location.h"
 #include "base/macros.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/single_thread_task_runner.h"
 #include "media/base/media_log.h"
 #include "media/blink/buffered_data_source_host_impl.h"
@@ -336,7 +337,7 @@ void MultibufferDataSource::OnBufferingHaveEnough(bool always_cancel) {
 
 int64_t MultibufferDataSource::GetMemoryUsage() const {
   // TODO(hubbe): Make more accurate when url_data_ is shared.
-  return url_data_->CachedSize()
+  return base::checked_cast<int64_t>(url_data_->CachedSize())
          << url_data_->multibuffer()->block_size_shift();
 }
 
