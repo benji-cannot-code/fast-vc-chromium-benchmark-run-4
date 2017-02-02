6f88765510b37a3a70e301ffc05b7b7f982c8587
ccameron <ccameron@chromium.org>
2017-02-02 15:29:14 -0800

    Fix id tracking bug in ICCProfile
    ICCProfiles are given an id when they are created, and are assumed to
    be unique.
    
    This breaks down in some tests, where we create ICCProfiles in the
    browser and send them to the renderer (as usual), but then also create
    test ICCProfiles in the in the renderer, that can conflict with the
    ids provided by the browser.
    
    Fix this by using hard-coded ids for the test ICCProfiles that are
    created by the renderer.
    
    BUG=634102
    
    Review-Url: https://codereview.chromium.org/2663153002
    Cr-Commit-Position: refs/heads/master@{#447874}

--- ui/gfx/icc_profile.cc
+++ ui/gfx/icc_profile.cc
@@ -16,6 +16,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gfx {
 
+const uint64_t ICCProfile::test_id_adobe_rgb_ = 1;
+const uint64_t ICCProfile::test_id_color_spin_ = 2;
+const uint64_t ICCProfile::test_id_generic_rgb_ = 3;
+const uint64_t ICCProfile::test_id_srgb_ = 4;
+
 namespace {
 const size_t kMinProfileLength = 128;
 const size_t kMaxProfileLength = 4 * 1024 * 1024;
@@ -29,7 +34,7 @@ struct Cache {
   Cache() : id_to_icc_profile_mru(kMaxCachedICCProfiles) {}
   ~Cache() {}
 
-  // Start from-ICC-data IDs at the end of the hard-coded list.
+  // Start from-ICC-data IDs at the end of the hard-coded test id list above.
   uint64_t next_unused_id = 5;
   base::MRUCache<uint64_t, ICCProfile> id_to_icc_profile_mru;
   base::Lock lock;
@@ -55,13 +60,19 @@ bool ICCProfile::operator!=(const ICCProfile& other) const {
 
 // static
 ICCProfile ICCProfile::FromData(const void* data, size_t size) {
+  return FromDataWithId(data, size, 0);
+}
+
+// static
+ICCProfile ICCProfile::FromDataWithId(const void* data,
+                                      size_t size,
+                                      uint64_t new_profile_id) {
   if (!IsValidProfileLength(size)) {
     if (size != 0)
       DLOG(ERROR) << "Invalid ICC profile length: " << size << ".";
     return ICCProfile();
   }
 
-  uint64_t new_profile_id = 0;
   const char* data_as_char = reinterpret_cast<const char*>(data);
   {
     // Linearly search the cached ICC profiles to find one with the same data.
@@ -76,7 +87,8 @@ ICCProfile ICCProfile::FromData(const void* data, size_t size) {
       auto found = cache.id_to_icc_profile_mru.Get(iter->second.id_);
       return found->second;
     }
-    new_profile_id = cache.next_unused_id++;
+    if (!new_profile_id)
+      new_profile_id = cache.next_unused_id++;
   }
 
   // Create a new cached id and add it to the cache.
