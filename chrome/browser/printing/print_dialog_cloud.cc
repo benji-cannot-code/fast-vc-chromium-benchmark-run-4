5a9a101cfd6e229651d980029a231c48de6a8492
jam <jam@chromium.org>
2017-02-01 16:00:41 -0800

    Convert print_dialog_cloud::SignInObserver to use the new navigation callbacks.
    BUG=682002
    
    Review-Url: https://codereview.chromium.org/2665303003
    Cr-Commit-Position: refs/heads/master@{#447649}

--- chrome/browser/printing/print_dialog_cloud.cc
+++ chrome/browser/printing/print_dialog_cloud.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/browser/signin_metrics.h"
 #include "components/signin/core/common/profile_management_switches.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
 
@@ -36,10 +37,14 @@ class SignInObserver : public content::WebContentsObserver {
 
  private:
   // Overridden from content::WebContentsObserver:
-  void DidNavigateMainFrame(
-      const content::LoadCommittedDetails& details,
-      const content::FrameNavigateParams& params) override {
-    if (cloud_devices::IsCloudPrintURL(params.url)) {
+  void DidFinishNavigation(
+      content::NavigationHandle* navigation_handle) override {
+    if (!navigation_handle->IsInMainFrame() ||
+        !navigation_handle->HasCommitted()) {
+      return;
+    }
+
+    if (cloud_devices::IsCloudPrintURL(navigation_handle->GetURL())) {
       base::ThreadTaskRunnerHandle::Get()->PostTask(
           FROM_HERE, base::Bind(&SignInObserver::OnSignIn,
                                 weak_ptr_factory_.GetWeakPtr()));
