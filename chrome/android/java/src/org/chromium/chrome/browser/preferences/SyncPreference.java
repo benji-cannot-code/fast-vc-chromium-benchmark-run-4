8cac92a110d86d79e0601a33c33f27141c9419c2
bauerb <bauerb@chromium.org>
2017-02-02 02:00:49 -0800

    Remove special sync status message for child accounts.
    BUG=687683
    
    Review-Url: https://codereview.chromium.org/2669803003
    Cr-Commit-Position: refs/heads/master@{#447735}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/SyncPreference.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/SyncPreference.java
@@ -15,7 +15,6 @@ import android.util.AttributeSet;
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.BuildInfo;
 import org.chromium.chrome.R;
-import org.chromium.chrome.browser.childaccounts.ChildAccountService;
 import org.chromium.chrome.browser.sync.GoogleServiceAuthError;
 import org.chromium.chrome.browser.sync.ProfileSyncService;
 import org.chromium.components.signin.ChromeSigninController;
@@ -88,10 +87,6 @@ public class SyncPreference extends Preference {
 
         ProfileSyncService profileSyncService = ProfileSyncService.get();
         Resources res = context.getResources();
-
-        if (ChildAccountService.isChildAccount()) {
-            return res.getString(R.string.kids_account);
-        }
 
         if (!AndroidSyncSettings.isMasterSyncEnabled(context)) {
             return res.getString(R.string.sync_android_master_sync_disabled);
