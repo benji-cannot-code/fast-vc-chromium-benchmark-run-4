07953a2489ccd38f8d208569b01813daba0aa570
lpz <lpz@chromium.org>
2017-02-02 12:05:46 -0800

    Assign a unique ID to every element node collected by ThreatDOMDetails.
    This CL should be a no-op. It is the first step to collecting
    non-resource elements like divs (which can't be keyed by their URLs).
    Next step will be to update ThreatDetails to use the new IDs as keys
    for all of its maps.
    
    BUG=674618
    
    Review-Url: https://codereview.chromium.org/2657933002
    Cr-Commit-Position: refs/heads/master@{#447827}

--- chrome/renderer/safe_browsing/threat_dom_details_browsertest.cc
+++ chrome/renderer/safe_browsing/threat_dom_details_browsertest.cc
@@ -35,7 +35,11 @@ TEST_F(ThreatDOMDetailsTest, Everything) {
     std::vector<SafeBrowsingHostMsg_ThreatDOMDetails_Node> params;
     details->ExtractResources(&params);
     ASSERT_EQ(1u, params.size());
-    EXPECT_EQ(GURL(urlprefix + html), params[0].url);
+    auto& param = params[0];
+    EXPECT_EQ(GURL(urlprefix + html), param.url);
+    EXPECT_EQ(0, param.node_id);
+    EXPECT_EQ(0, param.parent_node_id);
+    EXPECT_TRUE(param.child_node_ids.empty());
   }
 
   {
@@ -53,11 +57,25 @@ TEST_F(ThreatDOMDetailsTest, Everything) {
     std::vector<SafeBrowsingHostMsg_ThreatDOMDetails_Node> params;
     details->ExtractResources(&params);
     ASSERT_EQ(3u, params.size());
-    EXPECT_EQ(script1_url, params[0].url);
-    EXPECT_EQ("SCRIPT", params[0].tag_name);
-    EXPECT_EQ(script2_url, params[1].url);
-    EXPECT_EQ("SCRIPT", params[0].tag_name);
-    EXPECT_EQ(url, params[2].url);
+    auto& param = params[0];
+    EXPECT_EQ(script1_url, param.url);
+    EXPECT_EQ("SCRIPT", param.tag_name);
+    EXPECT_EQ(1, param.node_id);
+    EXPECT_EQ(0, param.parent_node_id);
+    EXPECT_TRUE(param.child_node_ids.empty());
+
+    param = params[1];
+    EXPECT_EQ(script2_url, param.url);
+    EXPECT_EQ("SCRIPT", param.tag_name);
+    EXPECT_EQ(2, param.node_id);
+    EXPECT_EQ(0, param.parent_node_id);
+    EXPECT_TRUE(param.child_node_ids.empty());
+
+    param = params[2];
+    EXPECT_EQ(url, param.url);
+    EXPECT_EQ(0, param.node_id);
+    EXPECT_EQ(0, param.parent_node_id);
+    EXPECT_TRUE(param.child_node_ids.empty());
   }
 
   {
@@ -87,19 +105,23 @@ TEST_F(ThreatDOMDetailsTest, Everything) {
     std::vector<SafeBrowsingHostMsg_ThreatDOMDetails_Node> params;
     details->ExtractResources(&params);
     ASSERT_EQ(2u, params.size());
-    std::sort(params.begin(), params.end(),
-              [](const SafeBrowsingHostMsg_ThreatDOMDetails_Node& a,
-                 const SafeBrowsingHostMsg_ThreatDOMDetails_Node& b) -> bool {
-                return a.parent < b.parent;
-              });
 
-    EXPECT_EQ(url, params[0].url);
-    EXPECT_EQ(GURL(), params[0].parent);
-    EXPECT_EQ(1u, params[0].children.size());
-    EXPECT_EQ(iframe1_url, params[0].children[0]);
-    EXPECT_EQ(iframe1_url, params[1].url);
-    EXPECT_EQ(url, params[1].parent);
-    EXPECT_EQ(0u, params[1].children.size());
+    auto& param = params[0];
+    EXPECT_EQ(iframe1_url, param.url);
+    EXPECT_EQ(url, param.parent);
+    EXPECT_EQ("IFRAME", param.tag_name);
+    EXPECT_EQ(0u, param.children.size());
+    EXPECT_EQ(1, param.node_id);
+    EXPECT_EQ(0, param.parent_node_id);
+    EXPECT_TRUE(param.child_node_ids.empty());
+
+    param = params[1];
+    EXPECT_EQ(url, param.url);
+    EXPECT_EQ(GURL(), param.parent);
+    EXPECT_EQ(1u, param.children.size());
+    EXPECT_EQ(0, param.node_id);
+    EXPECT_EQ(0, param.parent_node_id);
+    EXPECT_TRUE(param.child_node_ids.empty());
   }
 
   {
@@ -117,6 +139,15 @@ TEST_F(ThreatDOMDetailsTest, Everything) {
     std::vector<SafeBrowsingHostMsg_ThreatDOMDetails_Node> params;
     details->ExtractResources(&params);
     ASSERT_EQ(51u, params.size());
+
+    // The element nodes should all have node IDs.
+    for (size_t i = 0; i < params.size() - 1; ++i) {
+      auto& param = params[i];
+      const int expected_id = i + 1;
+      EXPECT_EQ(expected_id, param.node_id);
+      EXPECT_EQ(0, param.parent_node_id);
+      EXPECT_TRUE(param.child_node_ids.empty());
+    }
   }
 
   {
@@ -134,5 +165,14 @@ TEST_F(ThreatDOMDetailsTest, Everything) {
     std::vector<SafeBrowsingHostMsg_ThreatDOMDetails_Node> params;
     details->ExtractResources(&params);
     ASSERT_EQ(51u, params.size());
+
+    // The element nodes should all have node IDs.
+    for (size_t i = 0; i < params.size() - 1; ++i) {
+      auto& param = params[i];
+      const int expected_id = i + 1;
+      EXPECT_EQ(expected_id, param.node_id);
+      EXPECT_EQ(0, param.parent_node_id);
+      EXPECT_TRUE(param.child_node_ids.empty());
+    }
   }
 }
--- components/safe_browsing/common/safebrowsing_messages.h
+++ components/safe_browsing/common/safebrowsing_messages.h
@@ -16,6 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // A node is essentially a frame.
 IPC_STRUCT_BEGIN(SafeBrowsingHostMsg_ThreatDOMDetails_Node)
+  // A unique ID for this node, unique to the current Render Frame.
+  IPC_STRUCT_MEMBER(int, node_id)
+
   // URL of this resource. Can be empty.
   IPC_STRUCT_MEMBER(GURL, url)
 
@@ -26,8 +29,14 @@ IPC_STRUCT_BEGIN(SafeBrowsingHostMsg_ThreatDOMDetails_Node)
   // URL of the parent node. Can be empty.
   IPC_STRUCT_MEMBER(GURL, parent)
 
+  // The unique ID of the parent node. Can be 0 if this is the top node.
+  IPC_STRUCT_MEMBER(int, parent_node_id)
+
   // children of this node. Can be emtpy.
   IPC_STRUCT_MEMBER(std::vector<GURL>, children)
+
+  // The unique IDs of the child nodes. Can be empty if there are no children.
+  IPC_STRUCT_MEMBER(std::vector<int>, child_node_ids)
 IPC_STRUCT_END()
 
 // SafeBrowsing client-side detection messages sent from the renderer to the
