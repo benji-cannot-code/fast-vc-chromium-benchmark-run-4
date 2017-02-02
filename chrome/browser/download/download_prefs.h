f6b7c481cfcc228cca3feb4b1c13acdaf9f47195
pastarmovj <pastarmovj@chromium.org>
2017-02-02 08:14:34 -0800

    Revert changes to download_prefs regarding PDF auto open.
    In https://codereview.chromium.org/2369353002/ we introduced logic
    to automatically open the program associated with PDF when the
    always open PDF in external viewer was selected. This causes some
    issues where saving of PDFs becomes harder or cumbersome.
    
    Until we figre better way to decided when to auto-open and when not
    revert this logic.
    
    BUG=680202
    TEST=This is revert to existing state covered by existing tests.
    
    Review-Url: https://codereview.chromium.org/2669773002
    Cr-Commit-Position: refs/heads/master@{#447773}

--- chrome/browser/download/download_prefs.h
+++ chrome/browser/download/download_prefs.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 
 #include "base/files/file_path.h"
-#include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "build/build_config.h"
 #include "components/prefs/pref_member.h"
@@ -88,11 +87,7 @@ class DownloadPrefs {
   // Return whether the user prefers to open PDF downloads in the platform's
   // default reader.
   bool ShouldOpenPdfInSystemReader() const;
-
-  // Used by tests to disable version checks for Adobe.
-  void DisableAdobeVersionCheckForTests();
 #endif
-
 
   void ResetAutoOpen();
 
@@ -117,7 +112,6 @@ class DownloadPrefs {
 
 #if defined(OS_WIN) || defined(OS_LINUX) || defined(OS_MACOSX)
   bool should_open_pdf_in_system_reader_;
-  bool disable_adobe_version_check_for_tests_;
 #endif
 
   DISALLOW_COPY_AND_ASSIGN(DownloadPrefs);
