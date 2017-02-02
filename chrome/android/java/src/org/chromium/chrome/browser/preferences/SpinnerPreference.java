f1d4f9abae4fccdf6732594abbf917611a05663b
dullweber <dullweber@chromium.org>
2017-02-02 01:58:01 -0800

    Implement additional parts of the new ui for CBD.
    Change UI to use single-line time selector.
    Move clear button to footer.
    Fix RTL behavior of ViewPager.
    
    All changes are behind the "tabs-in-cbd" flag
    
    https://screenshot.googleplex.com/BwcNiqwedVf.png
    
    BUG=681523
    
    Review-Url: https://codereview.chromium.org/2646313005
    Cr-Commit-Position: refs/heads/master@{#447733}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/SpinnerPreference.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/SpinnerPreference.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.preferences;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.preference.Preference;
 import android.util.AttributeSet;
 import android.view.View;
@@ -31,7 +32,13 @@ public class SpinnerPreference extends Preference {
      */
     public SpinnerPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setLayoutResource(R.layout.preference_spinner);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.SpinnerPreference);
+        boolean single_line = a.getBoolean(R.styleable.SpinnerPreference_singleLine, false);
+        if (single_line) {
+            setLayoutResource(R.layout.preference_spinner_single_line);
+        } else {
+            setLayoutResource(R.layout.preference_spinner);
+        }
     }
 
     /**
@@ -41,8 +48,7 @@ public class SpinnerPreference extends Preference {
      * @param selectedIndex Index of the initially selected option.
      */
     public void setOptions(Object[] options, int selectedIndex) {
-        mAdapter = new ArrayAdapter<Object>(
-                getContext(), android.R.layout.simple_spinner_item, options);
+        mAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, options);
         mAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mSelectedIndex = selectedIndex;
     }
