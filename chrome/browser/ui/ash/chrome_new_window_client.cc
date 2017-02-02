548a38c262ae02847ff885e35f5c1cb36f6fe331
warx <warx@chromium.org>
2017-02-02 12:23:34 -0800

    ChromeOS: Fix NewWindow's bug with browser on other user's desktop and no browser on current's
    Changes:
    Regression comes from https://codereview.chromium.org/2434463004. The CL introduces two regressions, one for crbug.com/675265, one for crbug.com/608840.
    
    For crbug.com/675265, regression comes from changing chrome::FindBrowserWithWindow(ash::wm::GetActiveWindow()) to BrowserList::GetInstance()->GetLastActive(). ash::wm::GetActiveWindow() makes sure that GetActiveWindow from primary root window, however, it has dependency on ash/. This CL is to fix this.
    
    We want to get browser that has active window associated with it, return nullptr if that doesn't exist.
    
    BUG=675265
    TEST=emulator tests see bug 675265 fixed; a test coverage is added:
    MultiUserWindowManagerChromeOSTest.GetActiveBrowserTest
    
    Review-Url: https://codereview.chromium.org/2599833003
    Cr-Commit-Position: refs/heads/master@{#447830}

--- chrome/browser/ui/ash/chrome_new_window_client.cc
+++ chrome/browser/ui/ash/chrome_new_window_client.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/constants.h"
 #include "services/service_manager/public/cpp/connector.h"
 #include "ui/base/window_open_disposition.h"
+#include "ui/wm/public/activation_client.h"
 
 namespace {
 
@@ -44,6 +45,19 @@ void RestoreTabUsingProfile(Profile* profile) {
 }
 
 }  // namespace
+
+// static
+Browser* ChromeNewWindowClient::GetActiveBrowser() {
+  Browser* browser = BrowserList::GetInstance()->GetLastActive();
+  if (browser) {
+    aura::Window* window = browser->window()->GetNativeWindow();
+    aura::client::ActivationClient* client =
+        aura::client::GetActivationClient(window->GetRootWindow());
+    if (client->GetActiveWindow() == window)
+      return browser;
+  }
+  return nullptr;
+}
 
 ChromeNewWindowClient::ChromeNewWindowClient() : binding_(this) {
   service_manager::Connector* connector =
@@ -103,7 +117,7 @@ class ChromeNewWindowClient::TabRestoreHelper
 };
 
 void ChromeNewWindowClient::NewTab() {
-  Browser* browser = BrowserList::GetInstance()->GetLastActive();
+  Browser* browser = GetActiveBrowser();
   if (browser && browser->is_type_tabbed()) {
     chrome::NewTab(browser);
     return;
@@ -121,7 +135,7 @@ void ChromeNewWindowClient::NewTab() {
 }
 
 void ChromeNewWindowClient::NewWindow(bool is_incognito) {
-  Browser* browser = BrowserList::GetInstance()->GetLastActive();
+  Browser* browser = GetActiveBrowser();
   Profile* profile = (browser && browser->profile())
                          ? browser->profile()->GetOriginalProfile()
                          : ProfileManager::GetActiveUserProfile();
@@ -174,8 +188,8 @@ void ChromeNewWindowClient::RestoreTab() {
     return;
   }
 
-  Browser* browser = BrowserList::GetInstance()->GetLastActive();
-  Profile* profile = browser ? browser->profile() : NULL;
+  Browser* browser = GetActiveBrowser();
+  Profile* profile = browser ? browser->profile() : nullptr;
   if (!profile)
     profile = ProfileManager::GetActiveUserProfile();
   if (profile->IsOffTheRecord())
@@ -202,9 +216,9 @@ void ChromeNewWindowClient::ShowKeyboardOverlay() {
 }
 
 void ChromeNewWindowClient::ShowTaskManager() {
-  chrome::OpenTaskManager(NULL);
+  chrome::OpenTaskManager(nullptr);
 }
 
 void ChromeNewWindowClient::OpenFeedbackPage() {
-  chrome::OpenFeedbackDialog(BrowserList::GetInstance()->GetLastActive());
+  chrome::OpenFeedbackDialog(GetActiveBrowser());
 }
