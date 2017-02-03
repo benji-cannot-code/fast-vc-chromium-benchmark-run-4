ebc580dce060dc618e3e94b925406e79a5b0f127
eugenebut <eugenebut@chromium.org>
2017-02-03 11:24:03 -0800

    Made testCachingBehaviorOnOpenNewTab more robust.
    Wait until new Tab is open after opening it using DOM.
    |waitForPageToFinishLoading| does not wait for the new Tab.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2671613006
    Cr-Commit-Position: refs/heads/master@{#448041}

--- ios/chrome/browser/web/cache_egtest.mm
+++ ios/chrome/browser/web/cache_egtest.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/test/app/history_test_util.h"
 #include "ios/chrome/test/app/navigation_test_util.h"
 #include "ios/chrome/test/app/web_view_interaction_test_util.h"
+#import "ios/chrome/test/earl_grey/chrome_assertions.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
@@ -244,6 +245,7 @@ class ScopedBlockPopupsPref {
   // first allow popups.
   ScopedBlockPopupsPref prefSetter(CONTENT_SETTING_ALLOW);
   chrome_test_util::TapWebViewElementWithId(kCacheTestLinkID);
+  chrome_test_util::AssertMainTabCount(2);
   [ChromeEarlGrey waitForPageToFinishLoading];
   [[EarlGrey selectElementWithMatcher:WebViewContainingText("First Page")]
       assertWithMatcher:grey_notNil()];
