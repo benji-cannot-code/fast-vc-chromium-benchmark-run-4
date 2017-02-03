2e4ce768dd6c5b0505429650160f62fca919de29
blundell <blundell@chromium.org>
2017-02-03 07:00:05 -0800

    Move impl of identity.GetAccounts() extension API to its own file
    As a precursor to a refactoring of the identity extension API implementation
    to use the Identity Service, I am moving the implementations of the six
    identity extension API functions to their own files, rather than all
    being lumped together in identity_api.*. This will make it easier to
    analyze each of these function implementations independently and
    incrementally convert them to use the Identity Service.
    
    BUG=685654
    
    Review-Url: https://codereview.chromium.org/2671653004
    Cr-Commit-Position: refs/heads/master@{#447998}

--- chrome/browser/extensions/api/identity/identity_api.h
+++ chrome/browser/extensions/api/identity/identity_api.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/browser/extensions/api/identity/extension_token_key.h"
 #include "chrome/browser/extensions/api/identity/gaia_web_auth_flow.h"
+#include "chrome/browser/extensions/api/identity/identity_get_accounts_function.h"
 #include "chrome/browser/extensions/api/identity/identity_get_auth_token_function.h"
 #include "chrome/browser/extensions/api/identity/identity_get_profile_user_info_function.h"
 #include "chrome/browser/extensions/api/identity/identity_launch_web_auth_flow_function.h"
@@ -133,20 +134,6 @@ class IdentityAPI : public BrowserContextKeyedAPI,
 
 template <>
 void BrowserContextKeyedAPIFactory<IdentityAPI>::DeclareFactoryDependencies();
-
-class IdentityGetAccountsFunction : public ChromeUIThreadExtensionFunction {
- public:
-  DECLARE_EXTENSION_FUNCTION("identity.getAccounts",
-                             IDENTITY_GETACCOUNTS);
-
-  IdentityGetAccountsFunction();
-
- private:
-  ~IdentityGetAccountsFunction() override;
-
-  // UIThreadExtensionFunction implementation.
-  ExtensionFunction::ResponseAction Run() override;
-};
 
 }  // namespace extensions
 
