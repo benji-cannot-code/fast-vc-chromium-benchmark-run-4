36fee6cd70d770e317108b3ba32a6d1a4bd12838
mariakhomenko <mariakhomenko@chromium.org>
2017-02-03 11:43:07 -0800

    Instant Apps: Remove experiment.
    Since we are launching to 100%, remove the experiment and all
    the flags associated with it.
    
    BUG=687234
    
    Review-Url: https://codereview.chromium.org/2668553003
    Cr-Commit-Position: refs/heads/master@{#448054}

--- chrome/android/java/src/org/chromium/chrome/browser/util/FeatureUtilities.java
+++ chrome/android/java/src/org/chromium/chrome/browser/util/FeatureUtilities.java
@@ -28,7 +28,6 @@ import org.chromium.chrome.browser.ApplicationLifetime;
 import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.firstrun.FirstRunGlueImpl;
-import org.chromium.chrome.browser.instantapps.InstantAppsHandler;
 import org.chromium.chrome.browser.preferences.ChromePreferenceManager;
 import org.chromium.chrome.browser.tabmodel.DocumentModeAssassin;
 import org.chromium.chrome.browser.webapps.ChromeWebApkHost;
@@ -188,7 +187,6 @@ public class FeatureUtilities {
      */
     public static void cacheNativeFlags() {
         cacheHerbFlavor();
-        InstantAppsHandler.getInstance().cacheInstantAppsEnabled();
         ChromeWebApkHost.cacheEnabledStateForNextLaunch();
         cacheChromeHomeEnabled();
         FirstRunGlueImpl.cacheFirstRunPrefs();
