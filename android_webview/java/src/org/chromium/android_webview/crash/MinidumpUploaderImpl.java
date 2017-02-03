31f0c7c5fe69b7a2be3b345e419559be36f3b4ac
gsennton <gsennton@chromium.org>
2017-02-03 01:44:57 -0800

    [Android WebView] Ensure we use free connection when uploading minidumps
    By using the JobScheduler to upload minidumps we are restricting
    ourselves to not start a job uploading minidumps on costly connections
    (we allow only unmetered connections). In this CL we explicitly ensure
    that we are using an unmetered connection just before performing
    uploads as well to ensure we aren't hitting a race where we just
    switched connection and the JobScheduler hasn't yet shut our job down.
    
    NOTE: This commit adds the permission
    android.permission.ACCESS_NETWORK_STATE to WebView.
    
    BUG=678924
    BUG=673280
    
    Review-Url: https://codereview.chromium.org/2666603002
    Cr-Commit-Position: refs/heads/master@{#447972}

--- android_webview/java/src/org/chromium/android_webview/crash/MinidumpUploaderImpl.java
+++ android_webview/java/src/org/chromium/android_webview/crash/MinidumpUploaderImpl.java
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.android_webview.crash;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
 import org.chromium.base.Log;
 import org.chromium.base.VisibleForTesting;
@@ -26,6 +28,7 @@ import java.io.File;
 public class MinidumpUploaderImpl implements MinidumpUploader {
     private static final String TAG = "MinidumpUploaderImpl";
     private Thread mWorkerThread;
+    private final ConnectivityManager mConnectivityManager;
     private final CrashFileManager mFileManager;
 
     private Object mCancelLock = new Object();
@@ -57,6 +60,8 @@ public class MinidumpUploaderImpl implements MinidumpUploader {
 
     @VisibleForTesting
     public MinidumpUploaderImpl(Context context, boolean cleanOutMinidumps) {
+        mConnectivityManager =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         File webviewCrashDir = CrashReceiverService.createWebViewCrashDir(context);
         mFileManager = new CrashFileManager(webviewCrashDir);
         if (!mFileManager.ensureCrashDirExists()) {
@@ -90,7 +95,9 @@ public class MinidumpUploaderImpl implements MinidumpUploader {
             public boolean isNetworkAvailableForCrashUploads() {
                 // JobScheduler will call onStopJob causing our upload to be interrupted when our
                 // network requirements no longer hold.
-                return true;
+                NetworkInfo networkInfo = mConnectivityManager.getActiveNetworkInfo();
+                if (networkInfo == null || !networkInfo.isConnected()) return false;
+                return !mConnectivityManager.isActiveNetworkMetered();
             }
             @Override
             public boolean isCrashUploadDisabledByCommandLine() {
