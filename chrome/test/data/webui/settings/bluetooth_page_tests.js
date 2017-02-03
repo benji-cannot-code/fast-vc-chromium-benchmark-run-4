7c665e9570dc8bd381ef17518c29f2ac6e0b39d0
dpapad <dpapad@chromium.org>
2017-02-02 20:22:26 -0800

    MD Settings: Fix bluetooth_page_test.js in Vulcanized mode.
    The test can't explicitly import load_time_data.js, because in Vulcanized mode
    it ends up importing that file twice, whcih throws an error. Also, using
    loadTimeData.overrideValues() is a more established way of providing test dummy
    test data.
    
    BUG=673825
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2670833007
    Cr-Commit-Position: refs/heads/master@{#447928}

--- chrome/test/data/webui/settings/bluetooth_page_tests.js
+++ chrome/test/data/webui/settings/bluetooth_page_tests.js
@@ -39,8 +39,7 @@ suite('Bluetooth', function() {
   ];
 
   suiteSetup(function() {
-    window.loadTimeData = new LoadTimeData;
-    loadTimeData.data = {
+    loadTimeData.overrideValues({
       bluetoothEnabled: 'bluetoothEnabled',
       bluetoothDisabled: 'bluetoothDisabled',
       bluetoothOn: 'bluetoothOn',
@@ -49,8 +48,7 @@ suite('Bluetooth', function() {
       bluetoothDisconnect: 'bluetoothDisconnect',
       bluetoothPair: 'bluetoothPair',
       bluetoothStartConnecting: 'bluetoothStartConnecting',
-
-    };
+    });
 
     bluetoothApi_ = new settings.FakeBluetooth();
     bluetoothPrivateApi_ = new settings.FakeBluetoothPrivate(bluetoothApi_);
