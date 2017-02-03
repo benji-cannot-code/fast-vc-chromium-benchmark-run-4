7e813e053d03c44f1e8eb8cd2d9e39e91ae04b25
dgn <dgn@chromium.org>
2017-02-03 08:39:36 -0800

    [Home] Restore interactivity when context menu closes
    Makes the context menu manager from the bottom sheet listen to
    context menu close events from the parent ChromeActivity
    
    BUG=688391
    
    Review-Url: https://codereview.chromium.org/2675723004
    Cr-Commit-Position: refs/heads/master@{#448003}

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsBottomSheetContent.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsBottomSheetContent.java
@@ -12,6 +12,7 @@ import android.view.View;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.ntp.ContextMenuManager;
+import org.chromium.chrome.browser.ntp.NewTabPage.DestructionObserver;
 import org.chromium.chrome.browser.ntp.cards.NewTabPageAdapter;
 import org.chromium.chrome.browser.ntp.cards.NewTabPageRecyclerView;
 import org.chromium.chrome.browser.ntp.snippets.SnippetsBridge;
@@ -35,7 +36,7 @@ public class SuggestionsBottomSheetContent implements BottomSheet.BottomSheetCon
     private final SnippetsBridge mSnippetsBridge;
 
     public SuggestionsBottomSheetContent(
-            ChromeActivity activity, Tab tab, TabModelSelector tabModelSelector) {
+            final ChromeActivity activity, Tab tab, TabModelSelector tabModelSelector) {
         mRecyclerView = (NewTabPageRecyclerView) LayoutInflater.from(activity).inflate(
                 R.layout.new_tab_page_recycler_view, null, false);
 
@@ -48,7 +49,15 @@ public class SuggestionsBottomSheetContent implements BottomSheet.BottomSheetCon
 
         mSuggestionsManager = new SuggestionsUiDelegateImpl(
                 mSnippetsBridge, mSnippetsBridge, navigationDelegate, profile, tab);
+
         mContextMenuManager = new ContextMenuManager(activity, navigationDelegate, mRecyclerView);
+        activity.getWindowAndroid().addContextMenuCloseListener(mContextMenuManager);
+        mSuggestionsManager.addDestructionObserver(new DestructionObserver() {
+            @Override
+            public void onDestroy() {
+                activity.getWindowAndroid().removeContextMenuCloseListener(mContextMenuManager);
+            }
+        });
 
         NewTabPageAdapter adapter = new NewTabPageAdapter(mSuggestionsManager, null, uiConfig,
                 OfflinePageBridge.getForProfile(profile), mContextMenuManager);
