92eed65b93faef85d68e84c7541f13f641503766
dtu <dtu@chromium.org>
2017-02-02 20:48:49 -0800

    Revert of Update component updater URLs. (patchset #2 id:20001 of https://codereview.chromium.org/2654033009/ )
    Reason for revert:
    Lots of perf benchmarks started failing on Android. Bisect pointed to this change.
    
    https://build.chromium.org/p/chromium.perf/builders/Android%20Nexus5%20Perf%20%281%29/builds/5173
    https://build.chromium.org/p/chromium.perf/builders/Android%20Nexus5X%20Perf%20%281%29/builds/4443
    https://build.chromium.org/p/chromium.perf/builders/Android%20Nexus6%20Perf%20%281%29/builds/4826
    https://build.chromium.org/p/chromium.perf/builders/Android%20Nexus7v2%20Perf%20%281%29/builds/4667
    https://build.chromium.org/p/chromium.perf/builders/Android%20Nexus9%20Perf%20%281%29/builds/4012
    
    Original issue's description:
    > Update component updater URLs.
    >
    > BUG=685897
    >
    > Review-Url: https://codereview.chromium.org/2654033009
    > Cr-Commit-Position: refs/heads/master@{#447564}
    > Committed: https://chromium.googlesource.com/chromium/src/+/9fefd20253ceb6af4fc3604ca29fcafc4c8454cd
    
    TBR=sorin@chromium.org,rkaplow@chromium.org,pastarmovj@chromium.org,waffles@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=685897
    
    Review-Url: https://codereview.chromium.org/2675903002
    Cr-Commit-Position: refs/heads/master@{#447940}

--- components/component_updater/configurator_impl.cc
+++ components/component_updater/configurator_impl.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/command_line.h"
-#include "base/feature_list.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/version.h"
@@ -55,9 +54,6 @@ const char kSwitchDisableDeltaUpdates[] = "disable-delta-updates";
 // Disables background downloads.
 const char kSwitchDisableBackgroundDownloads[] = "disable-background-downloads";
 #endif  // defined(OS_WIN)
-
-const base::Feature kAlternateComponentUrls{"AlternateComponentUrls",
-                                            base::FEATURE_DISABLED_BY_DEFAULT};
 
 // Returns true if and only if |test| is contained in |vec|.
 bool HasSwitchValue(const std::vector<std::string>& vec, const char* test) {
@@ -153,14 +149,8 @@ std::vector<GURL> ConfiguratorImpl::UpdateUrl() const {
     return urls;
   }
 
-  if (base::FeatureList::IsEnabled(kAlternateComponentUrls)) {
-    urls.push_back(GURL(kUpdaterDefaultUrlAlt));
-    urls.push_back(GURL(kUpdaterFallbackUrlAlt));
-  } else {
-    urls.push_back(GURL(kUpdaterDefaultUrl));
-    urls.push_back(GURL(kUpdaterFallbackUrl));
-  }
-
+  urls.push_back(GURL(kUpdaterDefaultUrl));
+  urls.push_back(GURL(kUpdaterFallbackUrl));
   if (require_encryption_)
     update_client::RemoveUnsecureUrls(&urls);
 
