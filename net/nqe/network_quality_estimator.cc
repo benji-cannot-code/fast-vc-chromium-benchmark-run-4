df112ae3e4b81ed901feedebd9319266a7c3e98f
tbansal <tbansal@chromium.org>
2017-02-03 16:35:48 -0800

    Read NQE prefs to network quality store under all cases
    In Network Quality Estimator (NQE), change the logic to read the
    prefs even if caching is not enabled via finch trial. If the caching
    is  disabled, the network quality store is not queried by NQE.
    
    BUG=688121
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_cronet_tester
    
    Review-Url: https://codereview.chromium.org/2672733002
    Cr-Commit-Position: refs/heads/master@{#448118}

--- net/nqe/network_quality_estimator.cc
+++ net/nqe/network_quality_estimator.cc
@@ -290,6 +290,8 @@ NetworkQualityEstimator::NetworkQualityEstimator(
               variation_params)),
       forced_effective_connection_type_(
           nqe::internal::forced_effective_connection_type(variation_params)),
+      persistent_cache_reading_enabled_(
+          nqe::internal::persistent_cache_reading_enabled(variation_params)),
       event_creator_(net_log),
       weak_ptr_factory_(this) {
   // None of the algorithms can have an empty name.
@@ -1411,6 +1413,9 @@ nqe::internal::NetworkID NetworkQualityEstimator::GetCurrentNetworkID() const {
 bool NetworkQualityEstimator::ReadCachedNetworkQualityEstimate() {
   DCHECK(thread_checker_.CalledOnValidThread());
 
+  if (!persistent_cache_reading_enabled_)
+    return false;
+
   nqe::internal::CachedNetworkQuality cached_network_quality;
 
   const bool cached_estimate_available = network_quality_store_->GetById(
@@ -1700,6 +1705,9 @@ void NetworkQualityEstimator::MaybeUpdateNetworkQualityFromCache(
     const nqe::internal::NetworkID& network_id,
     const nqe::internal::CachedNetworkQuality& cached_network_quality) {
   DCHECK(thread_checker_.CalledOnValidThread());
+
+  if (!persistent_cache_reading_enabled_)
+    return;
   if (network_id != current_network_id_)
     return;
 
