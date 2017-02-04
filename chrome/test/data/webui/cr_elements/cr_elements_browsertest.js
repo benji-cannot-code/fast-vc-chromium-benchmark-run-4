ce247a1d7994b687b6453fc003fc8b4baf1c77c6
stevenjb <stevenjb@chromium.org>
2017-02-03 18:34:03 -0800

    Add cr_scrollable_behavior tests
    BUG=669248
    
    Review-Url: https://codereview.chromium.org/2670223002
    Cr-Commit-Position: refs/heads/master@{#448150}

--- chrome/test/data/webui/cr_elements/cr_elements_browsertest.js
+++ chrome/test/data/webui/cr_elements/cr_elements_browsertest.js
@@ -40,6 +40,10 @@ CrElementsBrowserTest.prototype = {
 
 function CrElementsLazyRenderTest() {}
 
+/**
+ * @constructor
+ * @extends {CrElementsBrowserTest}
+ */
 CrElementsLazyRenderTest.prototype = {
   __proto__: CrElementsBrowserTest.prototype,
 
@@ -57,6 +61,10 @@ TEST_F('CrElementsLazyRenderTest', 'All', function() {
   mocha.run();
 });
 
+/**
+ * @constructor
+ * @extends {CrElementsBrowserTest}
+ */
 function CrElementsProfileAvatarSelectorTest() {}
 
 CrElementsProfileAvatarSelectorTest.prototype = {
@@ -78,6 +86,10 @@ TEST_F('CrElementsProfileAvatarSelectorTest', 'All', function() {
   mocha.grep(cr_profile_avatar_selector.TestNames.Basic).run();
 });
 
+/**
+ * @constructor
+ * @extends {CrElementsBrowserTest}
+ */
 function CrElementsToolbarSearchFieldTest() {}
 
 CrElementsToolbarSearchFieldTest.prototype = {
@@ -98,6 +110,10 @@ TEST_F('CrElementsToolbarSearchFieldTest', 'All', function() {
   mocha.run();
 });
 
+/**
+ * @constructor
+ * @extends {CrElementsBrowserTest}
+ */
 function CrElementsSliderTest() {}
 
 CrElementsSliderTest.prototype = {
@@ -117,6 +133,10 @@ TEST_F('CrElementsSliderTest', 'All', function() {
   mocha.run();
 });
 
+/**
+ * @constructor
+ * @extends {CrElementsBrowserTest}
+ */
 function CrElementsDrawerTest() {}
 
 CrElementsDrawerTest.prototype = {
@@ -131,5 +151,27 @@ CrElementsDrawerTest.prototype = {
 };
 
 TEST_F('CrElementsDrawerTest', 'All', function() {
+  mocha.run();
+});
+
+/**
+ * @constructor
+ * @extends {CrElementsBrowserTest}
+ */
+function CrElementsScrollableBehaviorTest() {}
+
+CrElementsScrollableBehaviorTest.prototype = {
+  __proto__: CrElementsBrowserTest.prototype,
+
+  /** @override */
+  browsePreload: 'chrome://resources/cr_elements/cr_scrollable_behavior.html',
+
+  /** @override */
+  extraLibraries: CrElementsBrowserTest.prototype.extraLibraries.concat([
+    'cr_scrollable_behavior_tests.js',
+  ]),
+};
+
+TEST_F('CrElementsScrollableBehaviorTest', 'All', function() {
   mocha.run();
 });
