0ba71ee78cb2ebbbb1d47b9c12c4ca019cecd5a8
stevenjb <stevenjb@chromium.org>
2017-02-03 16:56:35 -0800

    MD Settings: Cros Users: Use display name for non gaia accounts
    BUG=680879
    
    Review-Url: https://codereview.chromium.org/2671813003
    Cr-Commit-Position: refs/heads/master@{#448134}

--- chrome/browser/ui/webui/settings/profile_info_handler.cc
+++ chrome/browser/ui/webui/settings/profile_info_handler.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/memory/ptr_util.h"
+#include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_attributes_entry.h"
@@ -22,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_manager/user_manager.h"
 #include "content/public/browser/notification_service.h"
 #else
-#include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/profiles/profile_avatar_icon_util.h"
 #include "chrome/browser/profiles/profile_statistics.h"
 #include "chrome/browser/profiles/profile_statistics_factory.h"
@@ -190,18 +190,23 @@ ProfileInfoHandler::GetAccountNameAndIcon() const {
 
 #if defined(OS_CHROMEOS)
   name = profile_->GetProfileUserName();
+  std::string user_email;
   if (name.empty()) {
+    // User is not associated with a gaia account, use the display name.
     const user_manager::User* user =
         chromeos::ProfileHelper::Get()->GetUserByProfile(profile_);
-    if (user && (user->GetType() != user_manager::USER_TYPE_GUEST))
-      name = user->GetAccountId().GetUserEmail();
-  }
-  if (!name.empty())
+    // Note: We don't show the User section in Guest mode.
+    DCHECK(user && (user->GetType() != user_manager::USER_TYPE_GUEST));
+    name = base::UTF16ToUTF8(user->GetDisplayName());
+    user_email = user->GetAccountId().GetUserEmail();
+  } else {
     name = gaia::SanitizeEmail(gaia::CanonicalizeEmail(name));
+    user_email = name;
+  }
 
   // Get image as data URL instead of using chrome://userimage source to avoid
   // issues with caching.
-  const AccountId account_id(AccountId::FromUserEmail(name));
+  const AccountId account_id(AccountId::FromUserEmail(user_email));
   scoped_refptr<base::RefCountedMemory> image =
       chromeos::options::UserImageSource::GetUserImage(account_id);
   icon_url = webui::GetPngDataUrl(image->front(), image->size());
