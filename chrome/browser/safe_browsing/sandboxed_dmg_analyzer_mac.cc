929abe48d49f2a2ed217e83fa1921749214b8fbd
fdoray <fdoray@chromium.org>
2017-02-05 14:03:10 -0800

    Use TaskScheduler instead of blocking pool in sandboxed_dmg_analyzer_mac.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=jialiul@chromium.org
    
    Review-Url: https://codereview.chromium.org/2673083002
    Cr-Commit-Position: refs/heads/master@{#448188}

--- chrome/browser/safe_browsing/sandboxed_dmg_analyzer_mac.cc
+++ chrome/browser/safe_browsing/sandboxed_dmg_analyzer_mac.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
+#include "base/task_scheduler/post_task.h"
 #include "chrome/common/chrome_utility_messages.h"
 #include "chrome/common/safe_browsing/zip_analyzer_results.h"
 #include "chrome/grit/generated_resources.h"
@@ -31,16 +32,16 @@ SandboxedDMGAnalyzer::~SandboxedDMGAnalyzer() {}
 
 void SandboxedDMGAnalyzer::Start() {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  if (!BrowserThread::GetBlockingPool()->PostWorkerTaskWithShutdownBehavior(
-          FROM_HERE, base::Bind(&SandboxedDMGAnalyzer::OpenDMGFile, this),
-          base::SequencedWorkerPool::CONTINUE_ON_SHUTDOWN)) {
-    NOTREACHED();
-  }
+  base::PostTaskWithTraits(
+      FROM_HERE, base::TaskTraits()
+                     .MayBlock()
+                     .WithPriority(base::TaskPriority::BACKGROUND)
+                     .WithShutdownBehavior(
+                         base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN),
+      base::Bind(&SandboxedDMGAnalyzer::OpenDMGFile, this));
 }
 
 void SandboxedDMGAnalyzer::OpenDMGFile() {
-  DCHECK(BrowserThread::GetBlockingPool()->RunsTasksOnCurrentThread());
-
   file_.Initialize(file_path_, base::File::FLAG_OPEN | base::File::FLAG_READ);
   if (!file_.IsValid()) {
     DLOG(ERROR) << "Could not open DMG file at path " << file_path_.value();
