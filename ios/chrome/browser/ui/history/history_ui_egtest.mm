487e0eba974a0c1d993b3b4eda91b481e7b31156
stkhapugin <stkhapugin@chromium.org>
2017-02-06 11:06:16 -0800

    [ObjC ARC] Converts ios/chrome/browser/ui/history:eg_tests to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2673553003
    Cr-Commit-Position: refs/heads/master@{#448333}

--- ios/chrome/browser/ui/history/history_ui_egtest.mm
+++ ios/chrome/browser/ui/history/history_ui_egtest.mm
@@ -37,6 +37,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "net/base/mac/url_conversions.h"
 #include "ui/base/l10n/l10n_util.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 using chrome_test_util::ButtonWithAccessibilityLabelId;
 
 namespace {
