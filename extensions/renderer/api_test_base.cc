2b1e6375a457e4408441a1051a1a6a316a5d12a7
dcheng <dcheng@chromium.org>
2017-02-05 21:39:29 -0800

    Construct fewer temporary strings when initializing extension resources.
    The module name is eventually turned into a string when inserted into
    the source map, but this resulted in one extra string copy. As a bonus,
    this slightly reduces binary size.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2672253002
    Cr-Commit-Position: refs/heads/master@{#448212}

--- extensions/renderer/api_test_base.cc
+++ extensions/renderer/api_test_base.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
+#include "base/strings/string_piece.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "extensions/common/extension_urls.h"
 #include "extensions/renderer/dispatcher.h"
@@ -119,14 +120,12 @@ ApiTestEnvironment::~ApiTestEnvironment() {
 
 void ApiTestEnvironment::RegisterModules() {
   v8_schema_registry_.reset(new V8SchemaRegistry);
-  const std::vector<std::pair<std::string, int> > resources =
+  const std::vector<std::pair<const char*, int>> resources =
       Dispatcher::GetJsResources();
-  for (std::vector<std::pair<std::string, int> >::const_iterator resource =
-           resources.begin();
-       resource != resources.end();
-       ++resource) {
-    if (resource->first != "test_environment_specific_bindings")
-      env()->RegisterModule(resource->first, resource->second);
+  for (const auto& resource : resources) {
+    if (base::StringPiece(resource.first) !=
+        "test_environment_specific_bindings")
+      env()->RegisterModule(resource.first, resource.second);
   }
   Dispatcher::RegisterNativeHandlers(env()->module_system(),
                                      env()->context(),
