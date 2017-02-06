b30f1727efeb99b96469d4d37cd4cbc610875232
raphael.kubo.da.costa <raphael.kubo.da.costa@intel.com>
2017-02-06 07:01:08 -0800

    IDL: Prevent some iterable/{map,set}like operations from being overridden
    According to the WebIDL spec, the following operations which are added
    automatically when an interface has an iterable<>, maplike<> or setlike<>
    declaration must not be overridable:
    * entries
    * forEach
    * get (for maplike)
    * has (for maplike/setlike)
    * keys
    * size (for maplike/setlike, currently not implemented)
    * values
    
    The following operations are added automatically when an interface has a
    read-write maplike<> or setlike<> declaration and can be overridden in an
    interface:
    * add (for setlike)
    * clear (for maplike/setlike)
    * delete (for maplike/setlike)
    * set (for maplike)
    
    Adjust the bindings generation code to raise an error if any of the methods
    in the first category are declared in an interface.
    
    BUG=432683
    R=jl@opera.com,haraken@chromium.org,yukishiino@chromium.org
    
    Review-Url: https://codereview.chromium.org/2672333002
    Cr-Commit-Position: refs/heads/master@{#448249}

--- third_party/WebKit/Source/bindings/tests/results/core/WebTestInterface3.cpp
+++ third_party/WebKit/Source/bindings/tests/results/core/WebTestInterface3.cpp
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // TODO(dglazkov): Implement generating includes.
 #include "wtf/text/WTFString.h.h"
-#include "Iterator.h"
 
 namespace blink {
 namespace api {
@@ -29,7 +28,6 @@ namespace api {
 
 // TODO(dglazkov): Implement method generation
 // void CppTestInterface3::voidMethodDocument
-// Iterator CppTestInterface3::keys
 
 }  // namespace api
 }  // namespace blink
