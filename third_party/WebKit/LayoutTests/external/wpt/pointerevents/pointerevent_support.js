b39938ce90c70314e05f39d43a4f7876eced8cc4
qyearsley <qyearsley@chromium.org>
2017-02-06 10:09:50 -0800

    Import wpt@97e16bef6d6599ae805521e2007a9430a12aa144
    Using wpt-import in Chromium 92dc22973ed15b4e4683d4ac73ea329763f65c25.
    
    Manual import, with the following change:
    Add Failure expectation for testharness test "hr-time/window-worker-time-origin.html", and clean up TestExpectations.
    
    NOEXPORT=true
    BUG=688854
    
    Review-Url: https://codereview.chromium.org/2675133002
    Cr-Commit-Position: refs/heads/master@{#448305}

--- third_party/WebKit/LayoutTests/external/wpt/pointerevents/pointerevent_support.js
+++ third_party/WebKit/LayoutTests/external/wpt/pointerevents/pointerevent_support.js
@@ -214,6 +214,10 @@ function MultiPointerTypeTest(testName, types) {
     this.createNextTest();
 }
 
+MultiPointerTypeTest.prototype.step = function(op) {
+    this.currentTest.step(op);
+}
+
 MultiPointerTypeTest.prototype.skip = function() {
     var prevTest = this.currentTest;
     this.createNextTest();
@@ -221,10 +225,12 @@ MultiPointerTypeTest.prototype.skip = function() {
 }
 
 MultiPointerTypeTest.prototype.done = function() {
-    var prevTest = this.currentTest;
-    this.createNextTest();
-    if (prevTest != null)
-        prevTest.done();
+    if (this.currentTest.status != 1) {
+        var prevTest = this.currentTest;
+        this.createNextTest();
+        if (prevTest != null)
+            prevTest.done();
+    }
 }
 
 MultiPointerTypeTest.prototype.step = function(stepFunction) {
@@ -243,7 +249,6 @@ MultiPointerTypeTest.prototype.createNextTest = function() {
     }
     resetTestState();
 }
-
 
 function setup_pointerevent_test(testName, supportedPointerTypes) {
     return globalPointerEventTest = new MultiPointerTypeTest(testName, supportedPointerTypes);
