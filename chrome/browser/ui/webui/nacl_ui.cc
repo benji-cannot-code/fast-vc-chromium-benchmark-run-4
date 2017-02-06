59f28d9cc9955cf30f51430e19a4e83a10c89c02
fdoray <fdoray@chromium.org>
2017-02-06 07:38:59 -0800

    Use TaskScheduler instead of blocking pool in nacl_ui.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=bauerb@chromium.org
    
    Review-Url: https://codereview.chromium.org/2673913002
    Cr-Commit-Position: refs/heads/master@{#448256}

--- chrome/browser/ui/webui/nacl_ui.cc
+++ chrome/browser/ui/webui/nacl_ui.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/sequenced_worker_pool.h"
 #include "base/values.h"
 #include "build/build_config.h"
@@ -364,9 +365,9 @@ void NaClDomHandler::MaybeRespondToPage() {
 
   if (!pnacl_path_validated_) {
     std::string* version_string = new std::string;
-    base::PostTaskAndReplyWithResult(
-        BrowserThread::GetBlockingPool(),
-        FROM_HERE,
+    base::PostTaskWithTraitsAndReplyWithResult(
+        FROM_HERE, base::TaskTraits().MayBlock().WithPriority(
+                       base::TaskPriority::BACKGROUND),
         base::Bind(&CheckPathAndVersion, version_string),
         base::Bind(&NaClDomHandler::DidCheckPathAndVersion,
                    weak_ptr_factory_.GetWeakPtr(),
