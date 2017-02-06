6a0dbee6d4d07d3e7040cc44898e57a544e25347
stkhapugin <stkhapugin@chromium.org>
2017-02-06 10:11:04 -0800

    [ObjC ARC] Converts ios/chrome/browser/ui/print:eg_tests to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2677253002
    Cr-Commit-Position: refs/heads/master@{#448306}

--- ios/chrome/browser/ui/print/print_controller_egtest.mm
+++ ios/chrome/browser/ui/print/print_controller_egtest.mm
@@ -18,6 +18,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util_mac.h"
 #include "url/gurl.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace {
 // URL which leads to a PDF file.
 const char kPDFURL[] = "http://ios/testing/data/http_server_files/testpage.pdf";
