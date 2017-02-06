6930678acab443937e463f2ea2fcab1a4c8743dc
mustaq <mustaq@chromium.org>
2017-02-06 14:05:01 -0800

    Added a UseCounter for hover:on-demand media query parsing.
    BUG=654861
    TBR=rkaplow@chromium.org
    
    Review-Url: https://codereview.chromium.org/2678933002
    Cr-Commit-Position: refs/heads/master@{#448407}

--- third_party/WebKit/Source/core/css/MediaQueryEvaluator.cpp
+++ third_party/WebKit/Source/core/css/MediaQueryEvaluator.cpp
@@ -645,6 +645,9 @@ static bool hoverMediaFeatureEval(const MediaQueryExpValue& value,
   if (!value.isID)
     return false;
 
+  if (value.id == CSSValueOnDemand)
+    UseCounter::count(mediaValues.document(), UseCounter::CSSValueOnDemand);
+
   return (hover == HoverTypeNone && value.id == CSSValueNone) ||
          (hover == HoverTypeOnDemand && value.id == CSSValueOnDemand) ||
          (hover == HoverTypeHover && value.id == CSSValueHover);
@@ -665,6 +668,7 @@ static bool anyHoverMediaFeatureEval(const MediaQueryExpValue& value,
     case CSSValueNone:
       return availableHoverTypes & HoverTypeNone;
     case CSSValueOnDemand:
+      UseCounter::count(mediaValues.document(), UseCounter::CSSValueOnDemand);
       return availableHoverTypes & HoverTypeOnDemand;
     case CSSValueHover:
       return availableHoverTypes & HoverTypeHover;
