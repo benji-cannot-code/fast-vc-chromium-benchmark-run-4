b4fb604cc19ef75476e96f312c1acd4437463121
gogerald <gogerald@chromium.org>
2017-02-06 13:12:18 -0800

    Rename mIsShowingEditDialog to mIsExpandedToFullHeight
    BUG=687312
    
    Review-Url: https://codereview.chromium.org/2678003004
    Cr-Commit-Position: refs/heads/master@{#448388}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/ui/PaymentRequestUI.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ui/PaymentRequestUI.java
@@ -328,7 +328,7 @@ public class PaymentRequestUI implements DialogInterface.OnDismissListener, View
     private List<SectionSeparator> mSectionSeparators;
 
     private PaymentRequestSection mSelectedSection;
-    private boolean mIsShowingEditDialog;
+    private boolean mIsExpandedToFullHeight;
     private boolean mIsProcessingPayClicked;
     private boolean mIsClientClosing;
     private boolean mIsClientCheckingSelection;
@@ -834,7 +834,7 @@ public class PaymentRequestUI implements DialogInterface.OnDismissListener, View
         } else if (v == mPayButton) {
             processPayButton();
         } else if (v == mEditButton) {
-            if (mIsShowingEditDialog) {
+            if (mIsExpandedToFullHeight) {
                 dismissDialog(true);
             } else {
                 expand(mOrderSummarySection);
@@ -931,7 +931,7 @@ public class PaymentRequestUI implements DialogInterface.OnDismissListener, View
             mCloseButton.setVisibility(View.VISIBLE);
             mSpinnyLayout.setVisibility(View.GONE);
 
-            if (mIsShowingEditDialog) {
+            if (mIsExpandedToFullHeight) {
                 ((FrameLayout.LayoutParams) mRequestView.getLayoutParams()).height =
                         LayoutParams.MATCH_PARENT;
                 mRequestView.requestLayout();
@@ -981,7 +981,7 @@ public class PaymentRequestUI implements DialogInterface.OnDismissListener, View
     }
 
     private void expand(PaymentRequestSection section) {
-        if (!mIsShowingEditDialog) {
+        if (!mIsExpandedToFullHeight) {
             // Container now takes the full height of the screen, animating towards it.
             mRequestView.getLayoutParams().height = LayoutParams.MATCH_PARENT;
             mRequestView.addOnLayoutChangeListener(new SheetEnlargingAnimator(true));
@@ -1030,7 +1030,7 @@ public class PaymentRequestUI implements DialogInterface.OnDismissListener, View
                     false /* leftIsSingleLine */, null /* rightTruncate */,
                     false /* rightIsSingleLine */);
 
-            mIsShowingEditDialog = true;
+            mIsExpandedToFullHeight = true;
         }
 
         // Update the section contents when they're selected.
