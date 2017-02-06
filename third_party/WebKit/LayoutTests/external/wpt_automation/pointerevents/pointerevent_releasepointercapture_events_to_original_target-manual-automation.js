fb19e6f1aa2dc897faaefe458b51aff96575f567
nzolghadr <nzolghadr@chromium.org>
2017-02-06 14:21:54 -0800

    Update a few pointerevent test and the automations
    Update a few pointerevent automation instructions
    to use the latest common synthetic pointer action
    functions. Also update the corresponding tests to
    support multiple pointer types.
    
    BUG=629722
    
    Review-Url: https://codereview.chromium.org/2678523002
    Cr-Commit-Position: refs/heads/master@{#448413}

--- third_party/WebKit/LayoutTests/external/wpt_automation/pointerevents/pointerevent_releasepointercapture_events_to_original_target-manual-automation.js
+++ third_party/WebKit/LayoutTests/external/wpt_automation/pointerevents/pointerevent_releasepointercapture_events_to_original_target-manual-automation.js
@@ -2,6 +2,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 importAutomationScript('/pointerevents/pointerevent_common_input.js');
 
 function inject_input() {
-  return mouseDragInTarget('#target0');
+  return pointerDragInTarget('mouse', '#target0', 'right').then(function() {
+    return pointerDragInTarget('touch', '#target0', 'right');
+  }).then(function() {
+    return pointerDragInTarget('pen', '#target0', 'right');
+  });
 }
-
