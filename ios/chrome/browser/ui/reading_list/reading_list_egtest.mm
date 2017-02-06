dda911ccad588b53cc70b285b5887a178860016e
lod <lod@chromium.org>
2017-02-06 09:46:56 -0800

    [ObjC ARC] Converts ios/chrome/browser/ui/reading_list:eg_tests to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2672393002
    Cr-Commit-Position: refs/heads/master@{#448293}

--- ios/chrome/browser/ui/reading_list/reading_list_egtest.mm
+++ ios/chrome/browser/ui/reading_list/reading_list_egtest.mm
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
-#include "base/mac/scoped_nsobject.h"
 #include "base/strings/sys_string_conversions.h"
 #include "components/reading_list/ios/reading_list_model.h"
 #include "ios/chrome/browser/reading_list/reading_list_model_factory.h"
@@ -25,6 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/third_party/material_components_ios/src/components/Snackbar/src/MaterialSnackbar.h"
 #import "ios/web/public/test/http_server.h"
 #import "ios/web/public/test/http_server_util.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
 
 namespace {
 const char kReadTitle[] = "foobar";
@@ -134,8 +137,8 @@ void AssertHeaderNotVisible(std::string header) {
 
 // Opens the reading list menu using command line.
 void OpenReadingList() {
-  base::scoped_nsobject<GenericChromeCommand> command(
-      [[GenericChromeCommand alloc] initWithTag:IDC_SHOW_READING_LIST]);
+  GenericChromeCommand* command =
+      [[GenericChromeCommand alloc] initWithTag:IDC_SHOW_READING_LIST];
   chrome_test_util::RunCommandWithActiveViewController(command);
 }
 
