d13f71ae5eaa327f4d3e5cbfdf318c3279d96adf
pastarmovj <pastarmovj@chromium.org>
2017-02-06 01:03:12 -0800

    [sync] Replace a CHECK for PathService::Get with error-handling code.
    It was not expected that the OS will fail to provide this info but
    it seems to be the case in the wild. Instead of crashing the browser
    we can gracefully fail in this case and log the problem to the system
    log for the admin to think about a solution.
    
    BUG=687882
    TEST=none
    
    Review-Url: https://codereview.chromium.org/2670253003
    Cr-Commit-Position: refs/heads/master@{#448221}

--- components/sync/driver/sync_service_base.cc
+++ components/sync/driver/sync_service_base.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/memory/ptr_util.h"
 #include "base/path_service.h"
+#include "base/syslog_logging.h"
 #include "components/invalidation/public/invalidation_service.h"
 #include "components/sync/base/report_unrecoverable_error.h"
 #include "components/sync/device_info/local_device_info_provider.h"
@@ -150,8 +151,11 @@ bool SyncServiceBase::GetLocalSyncConfig(
   if (local_sync_backend_folder->empty()) {
     // TODO(pastarmovj): Add DIR_ROAMING_USER_DATA to PathService to simplify
     // this code and move the logic in its right place. See crbug/657810.
-    CHECK(
-        base::PathService::Get(base::DIR_APP_DATA, local_sync_backend_folder));
+    if (!base::PathService::Get(base::DIR_APP_DATA,
+                                local_sync_backend_folder)) {
+      SYSLOG(WARNING) << "Local sync can not get the roaming profile folder.";
+      return false;
+    }
     *local_sync_backend_folder = local_sync_backend_folder->Append(
         FILE_PATH_LITERAL("Chrome/User Data"));
   }
