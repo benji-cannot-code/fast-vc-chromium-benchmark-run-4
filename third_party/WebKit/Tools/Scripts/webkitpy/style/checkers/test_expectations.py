775f94a24c8da023c8472940d063c6ce40dcc752
qyearsley <qyearsley@chromium.org>
2017-02-05 16:37:02 -0800

    Reland of Simplify the initialization of Git objects in Host. (patchset #1 id:1 of https://codereview.chromium.org/2671583002/ )
    Reason for revert:
    Reverting speculative revert; the flaky failure on Mac was unrelated to this CL.
    
    Original issue's description:
    > Revert of Simplify the initialization of Git objects in Host. (patchset #3 id:40001 of https://codereview.chromium.org/2663623003/ )
    >
    > Reason for revert:
    > Speculative revert. This might be the cause of build flakiness, see crbug.com/687707
    >
    > Original issue's description:
    > > Simplify the initialization of Git objects in Host.
    > >
    > > This CL makes calling Host.initialize_scm() unnecessary by making
    > > it so that the Git object is initialized when required. It also
    > > merges scm() and scm_for_path(), and moves initialization logic
    > > into the Git class.
    > >
    > > BUG=676012
    > >
    > > Review-Url: https://codereview.chromium.org/2663623003
    > > Cr-Commit-Position: refs/heads/master@{#447582}
    > > Committed: https://chromium.googlesource.com/chromium/src/+/7091f37e5b763d57e4752a4e69cf46719b8ea1c4
    >
    > TBR=jeffcarp@chromium.org,qyearsley@chromium.org
    > # Skipping CQ checks because original CL landed less than 1 days ago.
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=676012
    >
    > Review-Url: https://codereview.chromium.org/2671583002
    > Cr-Commit-Position: refs/heads/master@{#447650}
    > Committed: https://chromium.googlesource.com/chromium/src/+/6cd1e0590f82ef11669abfbf00d846d759235460
    
    TBR=jeffcarp@chromium.org,lushnikov@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=676012
    
    Review-Url: https://codereview.chromium.org/2676673006
    Cr-Commit-Position: refs/heads/master@{#448196}

--- third_party/WebKit/Tools/Scripts/webkitpy/style/checkers/test_expectations.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/style/checkers/test_expectations.py
@@ -48,7 +48,6 @@ class TestExpectationsChecker(object):
 
         # FIXME: host should be a required parameter, not an optional one.
         host = host or Host()
-        host.initialize_scm()
 
         self._port_obj = host.port_factory.get()
 
