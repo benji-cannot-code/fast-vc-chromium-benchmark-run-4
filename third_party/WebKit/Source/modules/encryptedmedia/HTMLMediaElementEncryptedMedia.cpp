e0f7af799b6f45c2d4af0d315043683149aff6e8
gogag2 <gogag2@gmail.com>
2017-02-06 06:03:23 -0800

    Move SetMediaKeysHandler timer to frame-specific TaskRunnerTimer.
    Move SetMediaKeysHandler timer m_timer to frame-specific TaskRunnerTimer.
    This associates it with the frame's MiscPlatformAPI timer task queue.
    
    Spec Link: https://w3c.github.io/encrypted-media/#htmlmediaelement-extensions
    
    This CL is migrating tasks/timers from a per-renderer scheduler to a per-frame
    scheduler:
      https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/80dsbEEOcWY
    
    BUG=624694
    
    Review-Url: https://codereview.chromium.org/2642193009
    Cr-Commit-Position: refs/heads/master@{#448243}

--- third_party/WebKit/Source/modules/encryptedmedia/HTMLMediaElementEncryptedMedia.cpp
+++ third_party/WebKit/Source/modules/encryptedmedia/HTMLMediaElementEncryptedMedia.cpp
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/DOMException.h"
 #include "core/dom/DOMTypedArray.h"
 #include "core/dom/ExceptionCode.h"
+#include "core/dom/TaskRunnerHelper.h"
 #include "core/html/HTMLMediaElement.h"
 #include "modules/encryptedmedia/ContentDecryptionModuleResultPromise.h"
 #include "modules/encryptedmedia/EncryptedMediaUtils.h"
@@ -52,7 +53,7 @@ class SetMediaKeysHandler : public ScriptPromiseResolver {
   Member<HTMLMediaElement> m_element;
   Member<MediaKeys> m_newMediaKeys;
   bool m_madeReservation;
-  Timer<SetMediaKeysHandler> m_timer;
+  TaskRunnerTimer<SetMediaKeysHandler> m_timer;
 };
 
 typedef Function<void()> SuccessCallback;
@@ -124,7 +125,9 @@ SetMediaKeysHandler::SetMediaKeysHandler(ScriptState* scriptState,
       m_element(element),
       m_newMediaKeys(mediaKeys),
       m_madeReservation(false),
-      m_timer(this, &SetMediaKeysHandler::timerFired) {
+      m_timer(TaskRunnerHelper::get(TaskType::MiscPlatformAPI, scriptState),
+              this,
+              &SetMediaKeysHandler::timerFired) {
   DVLOG(EME_LOG_LEVEL) << __func__;
 
   // 5. Run the following steps in parallel.
