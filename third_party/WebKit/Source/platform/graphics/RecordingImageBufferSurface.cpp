8eebcef5ac2dbd016a70a93dd8f320194c5d8fa7
junov <junov@chromium.org>
2017-02-06 13:48:40 -0800

    Add null pointer check in RecordingImageBufferSurface
    BUG=687556
    
    Review-Url: https://codereview.chromium.org/2671213004
    Cr-Commit-Position: refs/heads/master@{#448402}

--- third_party/WebKit/Source/platform/graphics/RecordingImageBufferSurface.cpp
+++ third_party/WebKit/Source/platform/graphics/RecordingImageBufferSurface.cpp
@@ -105,8 +105,9 @@ void RecordingImageBufferSurface::fallBackToRasterCanvas(
   }
 
   if (m_currentFrame) {
-    m_currentFrame->finishRecordingAsPicture()->playback(
-        m_fallbackSurface->canvas());
+    sk_sp<PaintRecord> record = m_currentFrame->finishRecordingAsPicture();
+    if (record)
+      record->playback(m_fallbackSurface->canvas());
     m_currentFrame.reset();
   }
 
