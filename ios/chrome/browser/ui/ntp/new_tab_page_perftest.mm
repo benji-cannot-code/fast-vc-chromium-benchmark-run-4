25b063475895f327d01dee693cbfdbbd6b1bc4af
lod <lod@chromium.org>
2017-02-06 09:48:00 -0800

    [ObjC ARC] Converts ios/chrome/browser/ui/ntp:perf_tests to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2675253003
    Cr-Commit-Position: refs/heads/master@{#448294}

--- ios/chrome/browser/ui/ntp/new_tab_page_perftest.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_perftest.mm
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/browser_view_controller.h"
 #import "ios/chrome/browser/ui/browser_view_controller_dependency_factory.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace {
 
 const NSTimeInterval kMaxUICatchupDelay = 2.0;  // seconds
