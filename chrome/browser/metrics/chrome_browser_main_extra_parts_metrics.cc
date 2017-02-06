998d955f78c1e66c0df3cb8e697f3feb21de08d9
gab <gab@chromium.org>
2017-02-06 11:29:41 -0800

    Add .SingleTab and .MultiTabs suffix to Startup.FirstWebContents.MainNavigationStart histogram.
    BUG=684046
    
    Review-Url: https://codereview.chromium.org/2651823002
    Cr-Commit-Position: refs/heads/master@{#448348}

--- chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
+++ chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
@@ -413,7 +413,7 @@ void ChromeBrowserMainExtraPartsMetrics::PostBrowserStart() {
   is_screen_observer_ = true;
 
 #if !defined(OS_ANDROID)
-  FirstWebContentsProfiler::Start();
+  metrics::BeginFirstWebContentsProfiling();
   metrics::TabUsageRecorder::Initialize();
 #endif  // !defined(OS_ANDROID)
 }
