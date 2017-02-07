fab028bc3939043fa579a4d3f73ed978385411e2
fdoray <fdoray@chromium.org>
2017-02-07 09:36:32 -0800

    Use TaskScheduler instead of blocking pool in kiosk_app_data.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=stevenjb@chromium.org
    
    Review-Url: https://codereview.chromium.org/2675043002
    Cr-Commit-Position: refs/heads/master@{#448656}

--- chrome/browser/chromeos/app_mode/kiosk_app_data.cc
+++ chrome/browser/chromeos/app_mode/kiosk_app_data.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_writer.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted_memory.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/sequenced_worker_pool.h"
 #include "base/values.h"
 #include "chrome/browser/browser_process.h"
@@ -427,8 +428,9 @@ void KioskAppData::ClearCache() {
   dict_update->Remove(app_key, NULL);
 
   if (!icon_path_.empty()) {
-    BrowserThread::PostBlockingPoolTask(
-        FROM_HERE,
+    base::PostTaskWithTraits(
+        FROM_HERE, base::TaskTraits().MayBlock().WithPriority(
+                       base::TaskPriority::BACKGROUND),
         base::Bind(base::IgnoreResult(&base::DeleteFile), icon_path_, false));
   }
 }
