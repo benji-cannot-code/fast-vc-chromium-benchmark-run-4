0fc602d115b9ad43c70d64f415e97607d20b2410
marq <marq@chromium.org>
2017-02-07 07:55:23 -0800

    This CL abstracts out WebContentsViewController's WebState dependency by creating a mediator and a consumer protocol.
    WebContentsConsumer is a "push" interface for a provider object (here the mediator) to
    inform a consumer (here the view controller) about changes it should reflect in its UI.
    
    Review-Url: https://codereview.chromium.org/2675763002
    Cr-Commit-Position: refs/heads/master@{#448629}

---
+++ ios/clean/chrome/browser/ui/web_contents/web_contents_consumer.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CLEAN_CHROME_BROWSER_UI_WEB_CONTENTS_WEB_CONTENTS_CONSUMER_H_
+#define IOS_CLEAN_CHROME_BROWSER_UI_WEB_CONTENTS_WEB_CONTENTS_CONSUMER_H_
+
+#import <UIKit/UIKit.h>
+
+// A WebContentsConsumer (typically a view controller) uses data provided by
+// this protocol to display a web view.
+@protocol WebContentsConsumer
+// Called when the content view that the reciever should display changes.
+- (void)contentViewDidChange:(UIView*)contentView;
+@end
+
+#endif  // IOS_CLEAN_CHROME_BROWSER_UI_WEB_CONTENTS_WEB_CONTENTS_CONSUMER_H_
--- ios/clean/chrome/browser/ui/web_contents/web_coordinator.mm
+++ ios/clean/chrome/browser/ui/web_contents/web_coordinator.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/clean/chrome/browser/ui/web_contents/web_coordinator.h"
 
 #import "ios/clean/chrome/browser/browser_coordinator+internal.h"
+#import "ios/clean/chrome/browser/ui/web_contents/web_contents_mediator.h"
 #import "ios/clean/chrome/browser/ui/web_contents/web_contents_view_controller.h"
 #import "ios/shared/chrome/browser/coordinator_context/coordinator_context.h"
 #include "ios/web/public/web_state/web_state.h"
@@ -19,19 +20,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 @interface WebCoordinator ()
-
 @property(nonatomic, strong) WebContentsViewController* viewController;
-
+@property(nonatomic, strong) WebContentsMediator* mediator;
 @end
 
 @implementation WebCoordinator
 @synthesize webState = _webState;
 @synthesize viewController = _viewController;
+@synthesize mediator = _mediator;
+
+- (instancetype)init {
+  if ((self = [super init])) {
+    _mediator = [[WebContentsMediator alloc] init];
+  }
+  return self;
+}
+
+- (void)setWebState:(web::WebState*)webState {
+  _webState = webState;
+  self.mediator.webState = self.webState;
+}
 
 - (void)start {
-  self.webState->SetWebUsageEnabled(true);
-  self.viewController =
-      [[WebContentsViewController alloc] initWithWebState:self.webState];
+  self.viewController = [[WebContentsViewController alloc] init];
+  self.mediator.consumer = self.viewController;
 
   // Reminder: this is a no-op if |baseViewController| is nil, for example
   // when this coordinator's view controller will be contained instead of
@@ -42,7 +54,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)stop {
-  self.webState->SetWebUsageEnabled(false);
+  // PLACEHOLDER: This is how the webUsageEnabled is set to false. Find a
+  // better way in the future.
+  self.mediator.webState = nullptr;
 }
 
 @end
