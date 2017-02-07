39de91b35543d2c9abdcedb9f60734f8c5f7f5b4
fdoray <fdoray@chromium.org>
2017-02-06 17:45:43 -0800

    Use TaskScheduler instead of blocking pool in wizard_controller.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=alemate@chromium.org
    
    Review-Url: https://codereview.chromium.org/2671263002
    Cr-Commit-Position: refs/heads/master@{#448504}

--- chrome/browser/chromeos/login/wizard_controller.cc
+++ chrome/browser/chromeos/login/wizard_controller.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_process_platform_part.h"
@@ -223,7 +224,6 @@ bool NetworkAllowUpdate(const chromeos::NetworkState* network) {
 #if defined(GOOGLE_CHROME_BUILD)
 void InitializeCrashReporter() {
   // The crash reporter initialization needs IO to complete.
-  DCHECK(BrowserThread::GetBlockingPool()->RunsTasksOnCurrentThread());
   breakpad::InitCrashReporter(std::string());
 }
 #endif
@@ -725,8 +725,10 @@ void WizardController::OnChangedMetricsReportingState(bool enabled) {
   if (!enabled)
     return;
 #if defined(GOOGLE_CHROME_BUILD)
-  if (!content::BrowserThread::PostBlockingPoolTask(
-          FROM_HERE, base::Bind(&InitializeCrashReporter))) {
+  if (!base::PostTaskWithTraits(FROM_HERE,
+                                base::TaskTraits().MayBlock().WithPriority(
+                                    base::TaskPriority::BACKGROUND),
+                                base::Bind(&InitializeCrashReporter))) {
     LOG(ERROR) << "Failed to start crash reporter initialization.";
   }
 #endif
