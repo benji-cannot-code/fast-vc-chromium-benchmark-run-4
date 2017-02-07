6e243ef118cf98b99d60ecd690f85c8428128297
fdoray <fdoray@chromium.org>
2017-02-07 08:15:17 -0800

    Use TaskScheduler instead of blocking pool in chrome_browser_main_linux.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=jochen@chromium.org
    
    Review-Url: https://codereview.chromium.org/2677743003
    Cr-Commit-Position: refs/heads/master@{#448638}

--- chrome/browser/chrome_browser_main_linux.cc
+++ chrome/browser/chrome_browser_main_linux.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/task_scheduler/post_task.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/grit/chromium_strings.h"
@@ -49,8 +50,9 @@ void ChromeBrowserMainPartsLinux::PreProfileInit() {
   // Needs to be called after we have chrome::DIR_USER_DATA and
   // g_browser_process.  This happens in PreCreateThreads.
   // base::GetLinuxDistro() will initialize its value if needed.
-  content::BrowserThread::PostBlockingPoolTask(
-      FROM_HERE,
+  base::PostTaskWithTraits(
+      FROM_HERE, base::TaskTraits().MayBlock().WithPriority(
+                     base::TaskPriority::BACKGROUND),
       base::Bind(base::IgnoreResult(&base::GetLinuxDistro)));
 #endif
 
