c81bafda193ee52cffb38102bd8a8b061f8b9edb
pkasting <pkasting@chromium.org>
2017-02-07 15:52:38 -0800

    Fix regression that added 32 DIP of extra padding to pre-Harmony dialogs.
    This was caused by https://codereview.chromium.org/2664073006 , which
    unintentionally changed the top inset returned by GetDialogButtonInsets() in
    pre-Harmony from 0 to 20.  This restores the previous 0 value.
    
    I looked for a better way to do this, but the code in dialog_client_view.cc is
    insane, and it turns out even if it's made not-insane, pre-Harmony would still
    want a different value for this top inset than the other three insets.  So the
    fix here would be needed roughly as-is anyway, it's just that I could write a
    better comment for it.
    
    BUG=687842
    TEST=none
    
    Review-Url: https://codereview.chromium.org/2682583002
    Cr-Commit-Position: refs/heads/master@{#448800}

--- chrome/browser/ui/views/harmony/layout_delegate.h
+++ chrome/browser/ui/views/harmony/layout_delegate.h
@@ -11,9 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class LayoutDelegate {
  public:
   enum class Metric {
-    // Horizontal or vertical margin between the edge of a dialog and a
+    // Margin between the edge of a dialog and the left, right, or bottom of a
     // contained button.
     DIALOG_BUTTON_MARGIN,
+    // In theory, this is the spacing between a dialog button and the content
+    // above it.  In practice, what the code does with this value, at least
+    // pre-Harmony, defies easy explanation.
+    DIALOG_BUTTON_TOP_SPACING,
     // Horizontal or vertical margin between the edge of a dialog and the close
     // button in the upper trailing corner.
     DIALOG_CLOSE_BUTTON_MARGIN,
