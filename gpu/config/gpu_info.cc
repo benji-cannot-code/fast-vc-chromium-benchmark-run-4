6632b262c34fe64b49a10e712642729f88a8945a
cwallez <cwallez@chromium.org>
2017-02-07 09:57:35 -0800

    GPU Info: Remove some unused collected values
    Namely Lenovo Dcute and Adapter Luid that were used only by the
    about:gpu page, as well as display_link_version that was put in a UMA
    histograms that hasn't been updated in forever.
    
    BUG=angleproject:1874
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2679773002
    Cr-Commit-Position: refs/heads/master@{#448667}

--- gpu/config/gpu_info.cc
+++ gpu/config/gpu_info.cc
@@ -69,8 +69,6 @@ GPUInfo::GPUDevice::~GPUDevice() { }
 GPUInfo::GPUInfo()
     : optimus(false),
       amd_switchable(false),
-      lenovo_dcute(false),
-      adapter_luid(0),
       gl_reset_notification_strategy(0),
       software_rendering(false),
       direct_rendering(true),
@@ -101,11 +99,8 @@ void GPUInfo::EnumerateFields(Enumerator* enumerator) const {
     base::TimeDelta initialization_time;
     bool optimus;
     bool amd_switchable;
-    bool lenovo_dcute;
-    base::Version display_link_version;
     GPUDevice gpu;
     std::vector<GPUDevice> secondary_gpus;
-    uint64_t adapter_luid;
     std::string driver_vendor;
     std::string driver_version;
     std::string driver_date;
@@ -163,12 +158,6 @@ void GPUInfo::EnumerateFields(Enumerator* enumerator) const {
                                      initialization_time);
   enumerator->AddBool("optimus", optimus);
   enumerator->AddBool("amdSwitchable", amd_switchable);
-  enumerator->AddBool("lenovoDcute", lenovo_dcute);
-  if (display_link_version.IsValid()) {
-    enumerator->AddString("displayLinkVersion",
-                          display_link_version.GetString());
-  }
-  enumerator->AddInt64("adapterLuid", adapter_luid);
   enumerator->AddString("driverVendor", driver_vendor);
   enumerator->AddString("driverVersion", driver_version);
   enumerator->AddString("driverDate", driver_date);
