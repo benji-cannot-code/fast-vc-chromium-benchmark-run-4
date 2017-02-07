e30c67239b5eadb13cb21d4dcf1f49953d2f4097
fdoray <fdoray@chromium.org>
2017-02-07 06:00:18 -0800

    Use TaskScheduler instead of blocking pool in storage_manager_handler.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=achuith@chromium.org
    
    Review-Url: https://codereview.chromium.org/2676693004
    Cr-Commit-Position: refs/heads/master@{#448614}

--- chrome/browser/ui/webui/options/chromeos/storage_manager_handler.cc
+++ chrome/browser/ui/webui/options/chromeos/storage_manager_handler.cc
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/drive/chromeos/file_system_interface.h"
 #include "components/user_manager/user_manager.h"
 #include "content/public/browser/browser_context.h"
-#include "content/public/browser/browser_thread.h"
 #include "content/public/browser/storage_partition.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/text/bytes_formatting.h"
@@ -252,9 +251,9 @@ void StorageManagerHandler::UpdateDownloadsSize() {
   const base::FilePath downloads_path =
       file_manager::util::GetDownloadsFolderForProfile(profile);
 
-  base::PostTaskAndReplyWithResult(
-      content::BrowserThread::GetBlockingPool(),
-      FROM_HERE,
+  base::PostTaskWithTraitsAndReplyWithResult(
+      FROM_HERE, base::TaskTraits().MayBlock().WithPriority(
+                     base::TaskPriority::BACKGROUND),
       base::Bind(&base::ComputeDirectorySize, downloads_path),
       base::Bind(&StorageManagerHandler::OnGetDownloadsSize,
                  weak_ptr_factory_.GetWeakPtr()));
