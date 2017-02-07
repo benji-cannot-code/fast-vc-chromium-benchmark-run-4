ac3b96afada7e613bc0762b99a1963e98a04c690
dschuyler <dschuyler@chromium.org>
2017-02-07 14:46:59 -0800

    [MD settings] idle load basic and advanced pages
    This CL uses lazy loading templates to preload the advanced page while
    the basic page is idle. The result is that the advanced page toggles
    much faster (addressing the 'jank' mentioned in the bug).
    
    This CL is replacing CL 2652443002.
    
    BUG=681238
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2660383002
    Cr-Commit-Position: refs/heads/master@{#448757}

--- chrome/browser/resources/settings/basic_page/basic_page.js
+++ chrome/browser/resources/settings/basic_page/basic_page.js
@@ -30,6 +30,7 @@ Polymer({
     advancedToggleExpanded: {
       type: Boolean,
       notify: true,
+      observer: 'advancedToggleExpandedChanged_',
     },
 
     /**
@@ -102,7 +103,7 @@ Polymer({
 
     if (this.pageVisibility.advancedSettings !== false) {
       assert(whenSearchDone === settings.getSearchManager().search(
-          query, assert(this.$$('#advancedPage'))));
+          query, assert(this.$.advancedPageTemplate.get())));
     }
 
     return whenSearchDone;
@@ -129,6 +130,15 @@ Polymer({
    */
   onSubpageExpanded_: function() {
     this.hasExpandedSection_ = true;
+  },
+
+  /**
+   * Render the advanced page now (don't wait for idle).
+   * @private
+   */
+  advancedToggleExpandedChanged_: function() {
+    if (this.advancedToggleExpanded)
+      this.$.advancedPageTemplate.get();
   },
 
   /**
