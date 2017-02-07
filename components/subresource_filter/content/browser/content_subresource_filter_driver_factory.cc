cd8bd51064a609dc9ae99c5d223f17e355076641
csharrison <csharrison@chromium.org>
2017-02-07 09:04:44 -0800

    Ensure non http/https/file main frames do not trigger activation
    A previous change [1] removed subresource filtering for these unsupported
    URLs on the renderer side. This patch ensures that logic is mirrored in
    the browser process with a unit test and DCHECK.
    
    [1] https://codereview.chromium.org/2595313004
    
    BUG=609747
    
    Review-Url: https://codereview.chromium.org/2677243003
    Cr-Commit-Position: refs/heads/master@{#448648}

--- components/subresource_filter/content/browser/content_subresource_filter_driver_factory.cc
+++ components/subresource_filter/content/browser/content_subresource_filter_driver_factory.cc
@@ -148,8 +148,13 @@ bool ContentSubresourceFilterDriverFactory::ShouldActivateForMainFrameURL(
     const GURL& url) const {
   switch (GetCurrentActivationScope()) {
     case ActivationScope::ALL_SITES:
-      return !IsWhitelisted(url);
+      return url.SchemeIsHTTPOrHTTPS() && !IsWhitelisted(url);
     case ActivationScope::ACTIVATION_LIST:
+      // The logic to ensure only http/https URLs are activated lives in
+      // AddActivationListMatch to ensure the activation list only has relevant
+      // entries.
+      DCHECK(url.SchemeIsHTTPOrHTTPS() ||
+             !DidURLMatchCurrentActivationList(url));
       return DidURLMatchCurrentActivationList(url) && !IsWhitelisted(url);
     default:
       return false;
@@ -316,7 +321,7 @@ bool ContentSubresourceFilterDriverFactory::DidURLMatchCurrentActivationList(
 void ContentSubresourceFilterDriverFactory::AddActivationListMatch(
     const GURL& url,
     ActivationList match_type) {
-  if (!url.host().empty() && url.SchemeIsHTTPOrHTTPS())
+  if (url.has_host() && url.SchemeIsHTTPOrHTTPS())
     activation_list_matches_[DistillURLToHostAndPath(url)].insert(match_type);
 }
 
