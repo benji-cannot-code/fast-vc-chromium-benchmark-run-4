a151b7f8e1f268a192acbcfb7cf037b1a07bcc79
bnc <bnc@chromium.org>
2017-02-06 19:12:03 -0800

    Change some spdy_protocol enum underlying types to unsigned.
    * Change SpdyFrameType and SpdySettingsIds enum underlying type
      to the unsigned integer type with the number of bits
      that these values take on the wire.
    * Add <<(ostream, SpdySettingsIds) to avoid compile errors.
    * Change IsDefinedFrameType(), Parse*(), and ValidateFrameHeader()
      argument types.
    * Remove unnecessary MIN_FRAME_TYPE.
    * Remove kDataFrameType, use SpdyFrameType::DATA instead.
    * In SpdyFramer::ValidateFrameHeader(), log uint8 argument through
      StringPrintf.
    
    This CL lands server changes 145013960, 145081454, 146661388,
    and 146701324 by bnc.  Note that some of 145013960 (changing
    underlying type of SpdyErrorCode to unsigned) has already
    landed as part of https://crrev.com/2675593002.
    
    BUG=488484
    
    Review-Url: https://codereview.chromium.org/2661283003
    Cr-Commit-Position: refs/heads/master@{#448533}

--- net/spdy/spdy_protocol.h
+++ net/spdy/spdy_protocol.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include <iosfwd>
 #include <limits>
 #include <map>
 #include <memory>
@@ -68,10 +69,9 @@ const int32_t kPaddingSizePerFrame = 256;
 NET_EXPORT_PRIVATE extern const char* const kHttp2ConnectionHeaderPrefix;
 const int kHttp2ConnectionHeaderPrefixSize = 24;
 
-// Types of HTTP2 frames.
-enum SpdyFrameType {
+// Wire values for HTTP2 frame types.
+enum SpdyFrameType : uint8_t {
   DATA = 0x00,
-  MIN_FRAME_TYPE = DATA,
   HEADERS = 0x01,
   PRIORITY = 0x02,
   RST_STREAM = 0x03,
@@ -126,7 +126,8 @@ enum Http2SettingsControlFlags {
   SETTINGS_FLAG_ACK = 0x01,
 };
 
-enum SpdySettingsIds {
+// Wire values of HTTP/2 setting identifiers.
+enum SpdySettingsIds : uint16_t {
   // HPACK header table maximum size.
   SETTINGS_HEADER_TABLE_SIZE = 0x1,
   SETTINGS_MIN = SETTINGS_HEADER_TABLE_SIZE,
@@ -142,6 +143,11 @@ enum SpdySettingsIds {
   SETTINGS_MAX_HEADER_LIST_SIZE = 0x6,
   SETTINGS_MAX = SETTINGS_MAX_HEADER_LIST_SIZE
 };
+
+// This explicit operator is needed, otherwise compiler finds
+// overloaded operator to be ambiguous.
+NET_EXPORT_PRIVATE std::ostream& operator<<(std::ostream& out,
+                                            SpdySettingsIds id);
 
 using SettingsMap = std::map<SpdySettingsIds, uint32_t>;
 
@@ -205,12 +211,12 @@ typedef uint64_t SpdyPingId;
 
 // Returns true if a given on-the-wire enumeration of a frame type is defined
 // in a standardized HTTP/2 specification, false otherwise.
-NET_EXPORT_PRIVATE bool IsDefinedFrameType(int frame_type_field);
+NET_EXPORT_PRIVATE bool IsDefinedFrameType(uint8_t frame_type_field);
 
 // Parses a frame type from an on-the-wire enumeration.
 // Behavior is undefined for invalid frame type fields; consumers should first
 // use IsValidFrameType() to verify validity of frame type fields.
-NET_EXPORT_PRIVATE SpdyFrameType ParseFrameType(int frame_type_field);
+NET_EXPORT_PRIVATE SpdyFrameType ParseFrameType(uint8_t frame_type_field);
 
 // (HTTP/2) All standard frame types except WINDOW_UPDATE are
 // (stream-specific xor connection-level). Returns false iff we know
@@ -224,7 +230,7 @@ const char* FrameTypeToString(SpdyFrameType frame_type);
 
 // If |wire_setting_id| is the on-the-wire representation of a defined SETTINGS
 // parameter, parse it to |*setting_id| and return true.
-NET_EXPORT_PRIVATE bool ParseSettingsId(int wire_setting_id,
+NET_EXPORT_PRIVATE bool ParseSettingsId(uint16_t wire_setting_id,
                                         SpdySettingsIds* setting_id);
 
 // Return if |id| corresponds to a defined setting;
@@ -241,8 +247,6 @@ NET_EXPORT_PRIVATE SpdyErrorCode ParseErrorCode(uint32_t wire_error_code);
 // for logging/debugging.
 const char* ErrorCodeToString(SpdyErrorCode error_code);
 
-// Frame type for non-control (i.e. data) frames.
-const int kDataFrameType = 0;
 // Number of octets in the frame header.
 const size_t kFrameHeaderSize = 9;
 // Size, in bytes, of the data frame header.
