70484cf66a55fb713d64e8412bb4c26bba29d0fb
haraken <haraken@chromium.org>
2017-02-07 02:37:40 -0800

    Fix a returned pointer type of InspectorDOMAgent::revalidateTask
    BUG=
    
    Review-Url: https://codereview.chromium.org/2682633002
    Cr-Commit-Position: refs/heads/master@{#448587}

--- third_party/WebKit/Source/core/inspector/InspectorDOMAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorDOMAgent.cpp
@@ -2050,7 +2050,7 @@ void InspectorDOMAgent::characterDataModified(CharacterData* characterData) {
   frontend()->characterDataModified(id, characterData->data());
 }
 
-Member<InspectorRevalidateDOMTask> InspectorDOMAgent::revalidateTask() {
+InspectorRevalidateDOMTask* InspectorDOMAgent::revalidateTask() {
   if (!m_revalidateTask)
     m_revalidateTask = new InspectorRevalidateDOMTask(this);
   return m_revalidateTask.get();
