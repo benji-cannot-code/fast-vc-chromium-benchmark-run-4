0d8fd3896d9f81b863c26fe870ee1c2735f63663
mek <mek@chromium.org>
2017-02-07 12:13:44 -0800

    FileReader.abort() should not fire an error event.
    The 'error', 'abort' and 'load' events are mutually exclusive. Each read
    operation should only result in one of these events. So stop the error
    event from being fired by the abort method, to align our behavior with
    at least firefox and edge.
    
    (at the time our implementation was written, the spec did still require
    an error event as well for this case, but that was changed fairly soon after
    in the spec)
    
    Test landed upstream in https://github.com/w3c/web-platform-tests/pull/4747
    
    BUG=689208
    
    Review-Url: https://codereview.chromium.org/2680603002
    Cr-Commit-Position: refs/heads/master@{#448714}

--- chrome/test/data/extensions/api_test/file_system_provider/read_file/test.js
+++ chrome/test/data/extensions/api_test/file_system_provider/read_file/test.js
@@ -341,7 +341,7 @@ function runTests() {
             chrome.test.callbackPass(function(fileEntry) {
               fileEntry.file(chrome.test.callbackPass(function(file) {
                 var fileReader = new FileReader();
-                fileReader.onerror = chrome.test.callbackPass(function(e) {
+                fileReader.onabort = chrome.test.callbackPass(function(e) {
                   chrome.test.assertEq(
                       'AbortError', fileReader.error.name);
                 });
@@ -372,7 +372,7 @@ function runTests() {
             chrome.test.callbackPass(function(fileEntry) {
               fileEntry.file(chrome.test.callbackPass(function(file) {
                 var fileReader = new FileReader();
-                fileReader.onerror = chrome.test.callbackPass(function(e) {
+                fileReader.onabort = chrome.test.callbackPass(function(e) {
                   chrome.test.assertEq(
                       'AbortError', fileReader.error.name);
                   // Confirm that the file is closed on the provider side.
@@ -414,7 +414,7 @@ function runTests() {
               fileEntry.file(chrome.test.callbackPass(function(file) {
                 var fileReader = new FileReader();
                 var fileReader2 = new FileReader();
-                fileReader.onerror = chrome.test.callbackPass(function(e) {
+                fileReader.onabort = chrome.test.callbackPass(function(e) {
                   chrome.test.assertEq(
                       'AbortError', fileReader.error.name);
                   // Confirm that the file is closed on the provider side.
