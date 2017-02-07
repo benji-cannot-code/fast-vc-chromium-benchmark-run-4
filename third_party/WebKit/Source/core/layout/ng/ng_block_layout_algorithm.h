3692196a0c9b2a0369a760104efe5fc688d6cda1
kojii <kojii@chromium.org>
2017-02-06 20:58:15 -0800

    [LayoutNG] Merge NGInlineLayoutAlgorithm to NGBlockLayoutAlgorithm
    This patch merges the two algorithms. While inline creates line boxes
    and block creates block boxes, the way we compute constraint spaces and
    the way we flow such boxes in block progression are the same.
    
    NGTextLayoutAlgorithm and NGLineBuilder own creating line boxes.
    NGBlockLayoutAlgorithm owns laying out line boxes in block progression.
    
    BUG=636993
    
    Review-Url: https://codereview.chromium.org/2669493002
    Cr-Commit-Position: refs/heads/master@{#448552}

--- third_party/WebKit/Source/core/layout/ng/ng_block_layout_algorithm.h
+++ third_party/WebKit/Source/core/layout/ng/ng_block_layout_algorithm.h
@@ -22,6 +22,7 @@ class NGBlockBreakToken;
 class NGBoxFragment;
 class NGConstraintSpace;
 class NGConstraintSpaceBuilder;
+class NGInlineNode;
 class NGPhysicalFragment;
 
 // A class for general block layout (e.g. a <div> with no special style).
@@ -37,7 +38,7 @@ class CORE_EXPORT NGBlockLayoutAlgorithm : public NGLayoutAlgorithm {
   //              fragment within.
   NGBlockLayoutAlgorithm(LayoutObject* layout_object,
                          PassRefPtr<const ComputedStyle> style,
-                         NGBlockNode* first_child,
+                         NGLayoutInputNode* first_child,
                          NGConstraintSpace* space,
                          NGBreakToken* break_token = nullptr);
 
@@ -51,6 +52,9 @@ class CORE_EXPORT NGBlockLayoutAlgorithm : public NGLayoutAlgorithm {
   // Creates a new constraint space for the current child.
   NGConstraintSpace* CreateConstraintSpaceForCurrentChild();
   void FinishCurrentChildLayout(RefPtr<NGPhysicalBoxFragment>);
+
+  // Layout inline children.
+  void LayoutInlineChildren(NGInlineNode*);
 
   // Proceed to the next sibling that still needs layout.
   //
@@ -106,7 +110,7 @@ class CORE_EXPORT NGBlockLayoutAlgorithm : public NGLayoutAlgorithm {
   // Read-only Getters.
   const ComputedStyle& CurrentChildStyle() const {
     DCHECK(current_child_);
-    return current_child_->Style();
+    return toNGBlockNode(current_child_)->Style();
   }
 
   const NGConstraintSpace& ConstraintSpace() const {
@@ -121,7 +125,7 @@ class CORE_EXPORT NGBlockLayoutAlgorithm : public NGLayoutAlgorithm {
 
   RefPtr<const ComputedStyle> style_;
 
-  Persistent<NGBlockNode> first_child_;
+  Persistent<NGLayoutInputNode> first_child_;
   Persistent<NGConstraintSpace> constraint_space_;
 
   // The break token from which we are currently resuming layout.
@@ -130,7 +134,7 @@ class CORE_EXPORT NGBlockLayoutAlgorithm : public NGLayoutAlgorithm {
   std::unique_ptr<NGFragmentBuilder> builder_;
   Persistent<NGConstraintSpaceBuilder> space_builder_;
   Persistent<NGConstraintSpace> space_for_current_child_;
-  Persistent<NGBlockNode> current_child_;
+  Persistent<NGLayoutInputNode> current_child_;
 
   // Mapper from the fragmented flow coordinate space coordinates to visual
   // coordinates. Only set on fragmentation context roots, such as multicol
