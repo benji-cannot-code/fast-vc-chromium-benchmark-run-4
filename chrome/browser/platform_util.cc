107a56baca99cedaaecb52df2efd5d200b569798
fdoray <fdoray@chromium.org>
2017-02-07 07:59:37 -0800

    Use TaskScheduler instead of blocking pool in platform_util.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=jochen@chromium.org
    
    Review-Url: https://codereview.chromium.org/2677943002
    Cr-Commit-Position: refs/heads/master@{#448632}

--- chrome/browser/platform_util.cc
+++ chrome/browser/platform_util.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file.h"
 #include "base/files/file_util.h"
 #include "base/logging.h"
+#include "base/task_scheduler/post_task.h"
 #include "chrome/browser/platform_util_internal.h"
 #include "content/public/browser/browser_thread.h"
 
@@ -58,9 +59,11 @@ void OpenItem(Profile* profile,
               OpenItemType item_type,
               const OpenOperationCallback& callback) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  BrowserThread::PostBlockingPoolTask(
-      FROM_HERE, base::Bind(&VerifyAndOpenItemOnBlockingThread, full_path,
-                            item_type, callback));
+  base::PostTaskWithTraits(FROM_HERE,
+                           base::TaskTraits().MayBlock().WithPriority(
+                               base::TaskPriority::BACKGROUND),
+                           base::Bind(&VerifyAndOpenItemOnBlockingThread,
+                                      full_path, item_type, callback));
 }
 
 }  // namespace platform_util
