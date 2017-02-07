4fb10c6a4ee8ff659a2ae4586d888e46d7fa0f24
lukasza <lukasza@chromium.org>
2017-02-06 17:04:32 -0800

    Adapt String::find method via StringUtil adapter.
    This is needed to insulate generated code from blink::protocol namespace
    from naming changes that we plan to do in the Great Blink Rename (which
    in particular will rename wtf::StringBuilder::find method into Find).
    
    This CL also includes roll of inspector_protocol which starts to
    generate code that uses the new StringUtil::find adapter method:
    rolling third_party/inspector to 1a7cbe8ba8fa0d622586f549a97c73d9b52efbea
    
    BUG=683447
    
    Review-Url: https://codereview.chromium.org/2668393004
    Cr-Commit-Position: refs/heads/master@{#448482}

--- third_party/WebKit/Source/wtf/NotFound.h
+++ third_party/WebKit/Source/wtf/NotFound.h
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NotFound_h
 #define NotFound_h
 
+#include <stddef.h>
+
 namespace WTF {
 const size_t kNotFound = static_cast<size_t>(-1);
 }
