1ddba2341fadf49506d2704514289367a6bd151d
michaeldo <michaeldo@chromium.org>
2017-02-06 16:05:55 -0800

    Fix import guard.
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2676193004
    Cr-Commit-Position: refs/heads/master@{#448453}

--- ios/web_view/internal/criwv_web_view_internal.h
+++ ios/web_view/internal/criwv_web_view_internal.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_WEB_VIEW_INTERNAL_CRIWV_WEB_VIEW_IMPL_H_
-#define IOS_WEB_VIEW_INTERNAL_CRIWV_WEB_VIEW_IMPL_H_
+#ifndef IOS_WEB_VIEW_INTERNAL_CRIWV_WEB_VIEW_INTERNAL_H_
+#define IOS_WEB_VIEW_INTERNAL_CRIWV_WEB_VIEW_INTERNAL_H_
 
 #import "ios/web_view/public/criwv_web_view.h"
 
@@ -21,4 +21,4 @@ class CRIWVBrowserState;
 
 @end
 
-#endif  // IOS_WEB_VIEW_INTERNAL_CRIWV_WEB_VIEW_IMPL_H_
+#endif  // IOS_WEB_VIEW_INTERNAL_CRIWV_WEB_VIEW_INTERNAL_H_
