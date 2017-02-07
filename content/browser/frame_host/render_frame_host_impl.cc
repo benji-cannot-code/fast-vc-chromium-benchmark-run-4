5a2e5a5129df932dbfab65d01ba44aeaae4fe04a
jam <jam@chromium.org>
2017-02-06 16:07:46 -0800

    Convert HistoryTabHelper to use the new navigation callbacks.
    Also fix redirect chain in NavigationHandle to include navigations starting with client side redirects. This matches the old API, which is necessary to make the tests pass after this conversion.
    
    BUG=682002
    
    Review-Url: https://codereview.chromium.org/2657323003
    Cr-Commit-Position: refs/heads/master@{#448454}

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -1132,14 +1132,15 @@ void RenderFrameHostImpl::OnDocumentOnLoadCompleted(
 
 void RenderFrameHostImpl::OnDidStartProvisionalLoad(
     const GURL& url,
+    const std::vector<GURL>& redirect_chain,
     const base::TimeTicks& navigation_start) {
   // TODO(clamy): Check if other navigation methods (OpenURL,
   // DidFailProvisionalLoad, ...) should also be ignored if the RFH is no longer
   // active.
   if (!is_active())
     return;
-  frame_tree_node_->navigator()->DidStartProvisionalLoad(this, url,
-                                                         navigation_start);
+  frame_tree_node_->navigator()->DidStartProvisionalLoad(
+      this, url, redirect_chain, navigation_start);
 }
 
 void RenderFrameHostImpl::OnDidFailProvisionalLoadWithError(
@@ -3405,7 +3406,7 @@ RenderFrameHostImpl::TakeNavigationHandleForCommit(
     }
 
     return NavigationHandleImpl::Create(
-        params.url, frame_tree_node_, is_renderer_initiated,
+        params.url, params.redirects, frame_tree_node_, is_renderer_initiated,
         params.was_within_same_page, base::TimeTicks::Now(),
         pending_nav_entry_id, false);  // started_from_context_menu
   }
@@ -3457,7 +3458,7 @@ RenderFrameHostImpl::TakeNavigationHandleForCommit(
   // pending_nav_entry_id. If the previous handle was a prematurely aborted
   // navigation loaded via LoadDataWithBaseURL, propagate the entry id.
   return NavigationHandleImpl::Create(
-      params.url, frame_tree_node_, is_renderer_initiated,
+      params.url, params.redirects, frame_tree_node_, is_renderer_initiated,
       params.was_within_same_page, base::TimeTicks::Now(),
       entry_id_for_data_nav, false);  // started_from_context_menu
 }
