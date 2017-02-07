3229cf27d3d33364d1e797e26b3fe5b369a8f2bc
fdoray <fdoray@chromium.org>
2017-02-06 18:37:14 -0800

    Use TaskScheduler instead of blocking pool in gaia_screen_handler.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=alemate@chromium.org
    
    Review-Url: https://codereview.chromium.org/2672013003
    Cr-Commit-Position: refs/heads/master@{#448523}

--- chrome/browser/ui/webui/chromeos/login/gaia_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/gaia_screen_handler.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/stl_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/values.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_shutdown.h"
@@ -241,8 +242,9 @@ GaiaScreenHandler::~GaiaScreenHandler() {
 }
 
 void GaiaScreenHandler::LoadGaia(const GaiaContext& context) {
-  base::PostTaskAndReplyWithResult(
-      content::BrowserThread::GetBlockingPool(), FROM_HERE,
+  base::PostTaskWithTraitsAndReplyWithResult(
+      FROM_HERE, base::TaskTraits().MayBlock().WithPriority(
+                     base::TaskPriority::BACKGROUND),
       base::Bind(&version_loader::GetVersion, version_loader::VERSION_SHORT),
       base::Bind(&GaiaScreenHandler::LoadGaiaWithVersion,
                  weak_factory_.GetWeakPtr(), context));
