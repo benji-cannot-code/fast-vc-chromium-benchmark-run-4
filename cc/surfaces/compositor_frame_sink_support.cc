a02e76fa0eaa7e31518cd7bc03e00b63dc9e8417
jam <jam@chromium.org>
2017-02-08 15:17:06 -0800

    Revert of Move surface reference code to CompositorFrameSinkSupport. (patchset #3 id:60001 of https://codereview.chromium.org/2687433002/ )
    Reason for revert:
    need to revert this to be able to revert https://codereview.chromium.org/2612083002
    
    Original issue's description:
    > Move surface reference code to CompositorFrameSinkSupport.
    >
    > The code that handled adding/removing surface references was in
    > GpuCompositorFrameSink which is only used with mustash. We want to
    > experiment with using surface references with non-mustash Chrome, so
    > move code to CompositorFrameSinkSupport.
    >
    > Only add/remove references if SurfaceManager is in surface reference
    > mode, otherwise behaviour in CompositorFrameSinkSupport is unchanged.
    >
    > BUG=676384
    > CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    >
    > Review-Url: https://codereview.chromium.org/2687433002
    > Cr-Commit-Position: refs/heads/master@{#449010}
    > Committed: https://chromium.googlesource.com/chromium/src/+/cec9e67c67ec7872e95917acf6f764656a0c7b98
    
    TBR=fsamuel@chromium.org,jbauman@chromium.org,kylechar@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=676384
    
    Review-Url: https://codereview.chromium.org/2683953004
    Cr-Commit-Position: refs/heads/master@{#449132}

--- cc/surfaces/compositor_frame_sink_support.cc
+++ cc/surfaces/compositor_frame_sink_support.cc
@@ -5,17 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "cc/surfaces/compositor_frame_sink_support.h"
 
-#include <algorithm>
-#include <utility>
-#include <vector>
-
 #include "cc/output/compositor_frame.h"
 #include "cc/scheduler/begin_frame_source.h"
 #include "cc/surfaces/compositor_frame_sink_support_client.h"
 #include "cc/surfaces/display.h"
 #include "cc/surfaces/surface.h"
 #include "cc/surfaces/surface_manager.h"
-#include "cc/surfaces/surface_reference.h"
 
 namespace cc {
 
@@ -32,7 +27,6 @@ CompositorFrameSinkSupport::CompositorFrameSinkSupport(
       display_(display),
       surface_factory_(frame_sink_id_, surface_manager_, this),
       handles_frame_sink_id_invalidation_(handles_frame_sink_id_invalidation),
-      reference_tracker_(frame_sink_id),
       weak_factory_(this) {
   if (handles_frame_sink_id_invalidation_)
     surface_manager_->RegisterFrameSinkId(frame_sink_id_);
@@ -48,12 +42,6 @@ CompositorFrameSinkSupport::~CompositorFrameSinkSupport() {
   // Unregister |this| as a BeginFrameObserver so that the BeginFrameSource does
   // not call into |this| after it's deleted.
   SetNeedsBeginFrame(false);
-
-  // For display root surfaces, the surface is no longer going to be visible
-  // so make it unreachable from the top-level root.
-  if (surface_manager_->using_surface_references() && display_ &&
-      reference_tracker_.current_surface_id().is_valid())
-    RemoveTopLevelRootReference(reference_tracker_.current_surface_id());
 
   for (auto& child_frame_sink_id : child_frame_sinks_) {
     DCHECK(child_frame_sink_id.is_valid());
@@ -86,27 +74,10 @@ void CompositorFrameSinkSupport::SubmitCompositorFrame(
     display_->SetLocalSurfaceId(local_surface_id,
                                 frame.metadata.device_scale_factor);
   }
-
-  if (surface_manager_->using_surface_references()) {
-    SurfaceId last_surface_id = reference_tracker_.current_surface_id();
-
-    // Populate surface references to add/remove based on reference surfaces in
-    // current frame compared with the last frame.
-    reference_tracker_.UpdateReferences(local_surface_id,
-                                        frame.metadata.referenced_surfaces);
-
-    surface_factory_.SubmitCompositorFrame(
-        local_surface_id, std::move(frame),
-        base::Bind(&CompositorFrameSinkSupport::DidReceiveCompositorFrameAck,
-                   weak_factory_.GetWeakPtr()));
-
-    UpdateSurfaceReferences(last_surface_id, local_surface_id);
-  } else {
-    surface_factory_.SubmitCompositorFrame(
-        local_surface_id, std::move(frame),
-        base::Bind(&CompositorFrameSinkSupport::DidReceiveCompositorFrameAck,
-                   weak_factory_.GetWeakPtr()));
-  }
+  surface_factory_.SubmitCompositorFrame(
+      local_surface_id, std::move(frame),
+      base::Bind(&CompositorFrameSinkSupport::DidReceiveCompositorFrameAck,
+                 weak_factory_.GetWeakPtr()));
 }
 
 void CompositorFrameSinkSupport::Require(const LocalSurfaceId& local_surface_id,
@@ -117,51 +88,6 @@ void CompositorFrameSinkSupport::Require(const LocalSurfaceId& local_surface_id,
 
 void CompositorFrameSinkSupport::Satisfy(const SurfaceSequence& sequence) {
   surface_manager_->SatisfySequence(sequence);
-}
-
-void CompositorFrameSinkSupport::UpdateSurfaceReferences(
-    const SurfaceId& last_surface_id,
-    const LocalSurfaceId& local_surface_id) {
-  const bool surface_id_changed =
-      last_surface_id.local_surface_id() != local_surface_id;
-
-  // If this is a display root surface and the SurfaceId is changing, make the
-  // new SurfaceId reachable from the top-level root.
-  if (display_ && surface_id_changed)
-    AddTopLevelRootReference(reference_tracker_.current_surface_id());
-
-  // Add references based on CompositorFrame referenced surfaces. If the
-  // SurfaceId has changed all referenced surfaces will be in this list.
-  if (!reference_tracker_.references_to_add().empty()) {
-    surface_manager_->AddSurfaceReferences(
-        reference_tracker_.references_to_add());
-  }
-
-  // If this is a display root surface and the SurfaceId is changing, make the
-  // old SurfaceId unreachable from the top-level root. This needs to happen
-  // after adding all references for the new SurfaceId.
-  if (display_ && surface_id_changed && last_surface_id.is_valid())
-    RemoveTopLevelRootReference(last_surface_id);
-
-  // Remove references based on CompositorFrame referenced surfaces. If the
-  // SurfaceId has changed this list will be empty.
-  if (!reference_tracker_.references_to_remove().empty()) {
-    DCHECK(!surface_id_changed);
-    surface_manager_->RemoveSurfaceReferences(
-        reference_tracker_.references_to_remove());
-  }
-}
-
-void CompositorFrameSinkSupport::AddTopLevelRootReference(
-    const SurfaceId& surface_id) {
-  SurfaceReference reference(surface_manager_->GetRootSurfaceId(), surface_id);
-  surface_manager_->AddSurfaceReferences({reference});
-}
-
-void CompositorFrameSinkSupport::RemoveTopLevelRootReference(
-    const SurfaceId& surface_id) {
-  SurfaceReference reference(surface_manager_->GetRootSurfaceId(), surface_id);
-  surface_manager_->RemoveSurfaceReferences({reference});
 }
 
 void CompositorFrameSinkSupport::DidReceiveCompositorFrameAck() {
--- cc/surfaces/compositor_frame_sink_support.h
+++ cc/surfaces/compositor_frame_sink_support.h
@@ -6,19 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CC_SURFACES_COMPOSITOR_FRAME_SINK_SUPPORT_H_
 #define CC_SURFACES_COMPOSITOR_FRAME_SINK_SUPPORT_H_
 
-#include <memory>
-#include <unordered_set>
-
 #include "base/compiler_specific.h"
 #include "base/memory/weak_ptr.h"
 #include "cc/output/compositor_frame.h"
 #include "cc/scheduler/begin_frame_source.h"
 #include "cc/surfaces/display.h"
 #include "cc/surfaces/display_client.h"
-#include "cc/surfaces/referenced_surface_tracker.h"
 #include "cc/surfaces/surface_factory.h"
 #include "cc/surfaces/surface_factory_client.h"
-#include "cc/surfaces/surface_id.h"
 #include "cc/surfaces/surfaces_export.h"
 
 namespace cc {
@@ -61,16 +56,6 @@ class CC_SURFACES_EXPORT CompositorFrameSinkSupport
   Display* display() { return display_; }
 
  private:
-  // Update surface references with SurfaceManager for current CompositorFrame
-  // that has |local_surface_id|. UpdateReferences() must be called on
-  // |reference_tracker_| before calling this. Will add and remove top-level
-  // root references if |display_| is not null.
-  void UpdateSurfaceReferences(const SurfaceId& last_surface_id,
-                               const LocalSurfaceId& local_surface_id);
-
-  void AddTopLevelRootReference(const SurfaceId& surface_id);
-  void RemoveTopLevelRootReference(const SurfaceId& surface_id);
-
   void DidReceiveCompositorFrameAck();
 
   // DisplayClient implementation.
@@ -127,10 +112,6 @@ class CC_SURFACES_EXPORT CompositorFrameSinkSupport
   // is established. Once we switch to SurfaceReferences, this ordering concern
   // goes away and we can remove this bool.
   const bool handles_frame_sink_id_invalidation_;
-
-  // Track the surface references for the surface corresponding to this
-  // compositor frame sink.
-  ReferencedSurfaceTracker reference_tracker_;
 
   // The set of BeginFrame children of this CompositorFrameSink.
   std::unordered_set<FrameSinkId, FrameSinkIdHash> child_frame_sinks_;
