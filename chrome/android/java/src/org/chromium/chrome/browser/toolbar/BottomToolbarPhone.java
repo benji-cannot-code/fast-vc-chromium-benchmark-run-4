c88099075a851e87e0b7877f7475fc0fb6fd9979
mdjones <mdjones@chromium.org>
2017-02-07 18:21:46 -0800

    [Home] Move progress bar to the top of the screen
    Previously the progress bar was part of the toolbar for both top and
    bottom versions of the toolbar. Now the bottom toolbar version is at
    the top of the screen, outside the toolbar. This change allows the
    progress bar to be attached to different view groups based on the
    version of the toolbar using it.
    
    BUG=671361
    
    Review-Url: https://codereview.chromium.org/2682493002
    Cr-Commit-Position: refs/heads/master@{#448862}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/BottomToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/BottomToolbarPhone.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.toolbar;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.ViewGroup;
 
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.widget.BottomSheet;
@@ -34,8 +35,8 @@ public class BottomToolbarPhone extends ToolbarPhone {
     @Override
     protected int getProgressBarTopMargin() {
         // In the case where the toolbar is at the bottom of the screen, the progress bar should
-        // be at the top of the toolbar.
-        return getContext().getResources().getDimensionPixelSize(R.dimen.toolbar_shadow_height);
+        // be at the top of the screen.
+        return 0;
     }
 
     @Override
@@ -60,5 +61,14 @@ public class BottomToolbarPhone extends ToolbarPhone {
         // Only detect swipes if the bottom sheet in the peeking state and not animating.
         return mBottomSheet.getSheetState() != BottomSheet.SHEET_STATE_PEEK
                 || mBottomSheet.isRunningSettleAnimation() || super.shouldIgnoreSwipeGesture();
+    }
+
+    @Override
+    protected void addProgressBarToHierarchy() {
+        if (mProgressBar == null) return;
+
+        ViewGroup coordinator = (ViewGroup) getRootView().findViewById(R.id.coordinator);
+        coordinator.addView(mProgressBar);
+        mProgressBar.setProgressBarContainer(coordinator);
     }
 }
