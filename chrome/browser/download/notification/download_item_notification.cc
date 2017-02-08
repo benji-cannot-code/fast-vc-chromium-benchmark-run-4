d7298b56a33377a06ff073bab2a9b1e0b639d3ab
fdoray <fdoray@chromium.org>
2017-02-07 16:01:36 -0800

    Use TaskScheduler instead of blocking pool in download_item_notification.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=asanka@chromium.org
    
    Review-Url: https://codereview.chromium.org/2675903004
    Cr-Commit-Position: refs/heads/master@{#448804}

--- chrome/browser/download/notification/download_item_notification.cc
+++ chrome/browser/download/notification/download_item_notification.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/task_scheduler/post_task.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/download/download_crx_util.h"
@@ -65,8 +66,6 @@ const SkColor kImageBackgroundColor = SK_ColorWHITE;
 const int64_t kMaxImagePreviewSize = 10 * 1024 * 1024;  // 10 MB
 
 std::string ReadNotificationImage(const base::FilePath& file_path) {
-  DCHECK(content::BrowserThread::GetBlockingPool()->RunsTasksOnCurrentThread());
-
   std::string data;
   bool ret = base::ReadFileToString(file_path, &data);
   if (!ret)
@@ -482,8 +481,9 @@ void DownloadItemNotification::UpdateNotificationData(
 
     if (model.HasSupportedImageMimeType()) {
       base::FilePath file_path = item_->GetFullPath();
-      base::PostTaskAndReplyWithResult(
-          content::BrowserThread::GetBlockingPool(), FROM_HERE,
+      base::PostTaskWithTraitsAndReplyWithResult(
+          FROM_HERE, base::TaskTraits().MayBlock().WithPriority(
+                         base::TaskPriority::BACKGROUND),
           base::Bind(&ReadNotificationImage, file_path),
           base::Bind(&DownloadItemNotification::OnImageLoaded,
                      weak_factory_.GetWeakPtr()));
@@ -600,8 +600,9 @@ void DownloadItemNotification::OnImageDecoded(const SkBitmap& decoded_bitmap) {
     return;
   }
 
-  base::PostTaskAndReplyWithResult(
-      content::BrowserThread::GetBlockingPool(), FROM_HERE,
+  base::PostTaskWithTraitsAndReplyWithResult(
+      FROM_HERE, base::TaskTraits().MayBlock().WithPriority(
+                     base::TaskPriority::BACKGROUND),
       base::Bind(&CropImage, decoded_bitmap),
       base::Bind(&DownloadItemNotification::OnImageCropped,
                  weak_factory_.GetWeakPtr()));
