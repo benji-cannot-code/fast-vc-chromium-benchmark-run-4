c181ccd4db17e8409a7dcde9287578f0202af98e
edchin <edchin@chromium.org>
2017-02-07 19:04:25 -0800

    [tab_grid] Hack-in NewTabButton.
    This is complete reuse of code from tab_switcher_view.
    
    BUG=686770
    
    Review-Url: https://codereview.chromium.org/2678293002
    Cr-Commit-Position: refs/heads/master@{#448881}

--- ios/clean/chrome/browser/ui/tab_grid/tab_grid_view_controller.mm
+++ ios/clean/chrome/browser/ui/tab_grid/tab_grid_view_controller.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/clean/chrome/browser/ui/commands/settings_commands.h"
 #import "ios/clean/chrome/browser/ui/commands/tab_commands.h"
 #import "ios/clean/chrome/browser/ui/commands/tab_grid_commands.h"
+#import "ios/clean/chrome/browser/ui/tab_grid/mdc_floating_button+cr_tab_grid.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -33,6 +34,7 @@ const CGFloat kTabSize = 150;
                                     UICollectionViewDelegate,
                                     SessionCellDelegate>
 @property(nonatomic, weak) UICollectionView* grid;
+@property(nonatomic, strong) MDCFloatingButton* floatingNewTabButton;
 @end
 
 @implementation TabGridViewController
@@ -42,6 +44,7 @@ const CGFloat kTabSize = 150;
 @synthesize tabGridCommandHandler = _tabGridCommandHandler;
 @synthesize tabCommandHandler = _tabCommandHandler;
 @synthesize grid = _grid;
+@synthesize floatingNewTabButton = _floatingNewTabButton;
 
 - (void)viewDidLoad {
   // Placeholder dark grey stripe at the top of the view.
@@ -99,6 +102,11 @@ const CGFloat kTabSize = 150;
 
 - (void)viewWillAppear:(BOOL)animated {
   [self.grid reloadData];
+  self.floatingNewTabButton = [MDCFloatingButton cr_tabGridNewTabButton];
+  [self.floatingNewTabButton
+      setFrame:[MDCFloatingButton
+                   cr_frameForTabGridNewTabButtonInRect:self.view.bounds]];
+  [self.view addSubview:self.floatingNewTabButton];
 }
 
 #pragma mark - UICollectionViewDataSource methods
