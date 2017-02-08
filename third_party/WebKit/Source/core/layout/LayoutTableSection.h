d72e3222e9e6e7ff3bb2a95a54aacfe8ed1974c7
robhogan <robhogan@gmail.com>
2017-02-08 14:07:08 -0800

    Avoid unnecessary cell layout on nested percent height tables
    In the test, the nested percent height tables mean that each cell will get at
    least three layouts (LayoutTableRow::layout(),calcRowLogicalHeight(), layoutRows())
    during the layout of the section - and this cascades down the tree with the lowest
    children getting hundreds of repeated layouts.
    
    This inefficiency has always been there, we've just introduced it for the
    percent height situation with crrev.com/2441373002.
    
    To avoid this don't bother computing the height of the section based on row sizes (calcRowLogicalHeight()),
    unless the section itself needed a layout - the previously calculated value will do.
    
    The only complication this introduces is: any extra logical height from the
    table that we previously discarded when distributing to the rows we will now
    try to distribute again. This can change the size of the section and cause extra
    paints.
    
    BUG=687061
    
    Review-Url: https://codereview.chromium.org/2670603002
    Cr-Commit-Position: refs/heads/master@{#449101}

--- third_party/WebKit/Source/core/layout/LayoutTableSection.h
+++ third_party/WebKit/Source/core/layout/LayoutTableSection.h
@@ -330,6 +330,8 @@ class CORE_EXPORT LayoutTableSection final : public LayoutTableBoxComponent {
 
   bool isRepeatingHeaderGroup() const;
 
+  void layout() override;
+
  protected:
   void styleDidChange(StyleDifference, const ComputedStyle* oldStyle) override;
   bool nodeAtPoint(HitTestResult&,
@@ -343,8 +345,6 @@ class CORE_EXPORT LayoutTableSection final : public LayoutTableBoxComponent {
   }
 
   void willBeRemovedFromTree() override;
-
-  void layout() override;
 
   int borderSpacingForRow(unsigned row) const {
     return m_grid[row].rowLayoutObject ? table()->vBorderSpacing() : 0;
