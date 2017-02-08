9252e34f14d76b25d41fbc3b8eed2123380f096f
michaelbai <michaelbai@chromium.org>
2017-02-07 18:09:38 -0800

    Wait 5 times for render process crash callback
    The test is flaky on some device because of timeout, wait multiple
    times for callback
    
    BUG=689292
    
    Review-Url: https://codereview.chromium.org/2679303002
    Cr-Commit-Position: refs/heads/master@{#448855}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientOnRenderProcessGoneTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientOnRenderProcessGoneTest.java
@@ -16,10 +16,13 @@ import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.parameter.ParameterizedTest;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Tests for AwContentsClient.onRenderProcessGone callback.
  */
 public class AwContentsClientOnRenderProcessGoneTest extends AwTestBase {
+    private static final String TAG = "AwRendererGone";
     private static class GetRenderProcessGoneHelper extends CallbackHelper {
         private AwRenderProcessGoneDetail mDetail;
 
@@ -66,8 +69,8 @@ public class AwContentsClientOnRenderProcessGoneTest extends AwTestBase {
         GetRenderProcessGoneHelper helper = contentsClient.getGetRenderProcessGoneHelper();
         loadUrlAsync(awContents, "chrome://crash");
         int callCount = helper.getCallCount();
-        helper.waitForCallback(callCount);
-
+        helper.waitForCallback(callCount, 1, CallbackHelper.WAIT_TIMEOUT_SECONDS * 5,
+                TimeUnit.SECONDS);
         assertEquals(callCount + 1, helper.getCallCount());
         assertTrue(helper.getAwRenderProcessGoneDetail().didCrash());
         assertEquals(RendererPriority.HIGH,
