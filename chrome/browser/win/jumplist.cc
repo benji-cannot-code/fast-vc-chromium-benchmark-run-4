a67b7614f746809590757b56af6f4cc842969aba
chengx <chengx@chromium.org>
2017-02-08 15:23:38 -0800

    Fix Jumplist favicons to have high-resolution in HDPI Windows displays
    On a HDPI windows machine, if right-click Chrome in the taskbar, the
    jumplist favicons are not displayed in high-resolution. They look
    blurry, different from other favicons in the rest of Chrome, e.g., tabs.
    As reported, Internet Explorer doesn't have this issue.
    
    The root cause is that 1x bitmap (16x16) is always used to create
    jumplist icon file by the current jumplist implementation.
    
    This CL fixes this issue. The idea is that for all the display scales
    supported by the platform, get/compute all corresponding bitmaps and
    write them to the disk. In this way, the appropriate icon bitmap is used
    when a user changes the screen display scale.
    
    BUG=676901
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    
    Review-Url: https://codereview.chromium.org/2665623002
    Cr-Commit-Position: refs/heads/master@{#449134}

--- chrome/browser/win/jumplist.cc
+++ chrome/browser/win/jumplist.cc
@@ -45,7 +45,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/codec/png_codec.h"
 #include "ui/gfx/favicon_size.h"
 #include "ui/gfx/icon_util.h"
+#include "ui/gfx/image/image.h"
 #include "ui/gfx/image/image_family.h"
+#include "ui/gfx/image/image_skia.h"
+#include "ui/gfx/image/image_skia_rep.h"
 #include "url/gurl.h"
 
 using content::BrowserThread;
@@ -74,7 +77,7 @@ scoped_refptr<ShellLinkItem> CreateShellLink() {
 }
 
 // Creates a temporary icon file to be shown in JumpList.
-bool CreateIconFile(const SkBitmap& bitmap,
+bool CreateIconFile(const gfx::ImageSkia& image_skia,
                     const base::FilePath& icon_dir,
                     base::FilePath* icon_path) {
   // Retrieve the path to a temporary file.
@@ -87,7 +90,16 @@ bool CreateIconFile(const SkBitmap& bitmap,
   // Create an icon file from the favicon attached to the given |page|, and
   // save it as the temporary file.
   gfx::ImageFamily image_family;
-  image_family.Add(gfx::Image::CreateFrom1xBitmap(bitmap));
+  if (!image_skia.isNull()) {
+    std::vector<float> supported_scales = image_skia.GetSupportedScales();
+    for (auto& scale : supported_scales) {
+      gfx::ImageSkiaRep image_skia_rep = image_skia.GetRepresentation(scale);
+      if (!image_skia_rep.is_null())
+        image_family.Add(
+            gfx::Image::CreateFrom1xBitmap(image_skia_rep.sk_bitmap()));
+    }
+  }
+
   if (!IconUtil::CreateIconFileFromImageFamily(image_family, path,
                                                IconUtil::NORMAL_WRITE))
     return false;
@@ -105,7 +117,7 @@ void CreateIconFiles(const base::FilePath& icon_dir,
   for (ShellLinkItemList::const_iterator item = item_list.begin();
       item != item_list.end(); ++item) {
     base::FilePath icon_path;
-    if (CreateIconFile((*item)->icon_data(), icon_dir, &icon_path))
+    if (CreateIconFile((*item)->icon_image(), icon_dir, &icon_path))
       (*item)->set_icon(icon_path.value(), 0);
   }
 }
@@ -542,10 +554,12 @@ void JumpList::OnFaviconDataAvailable(
     base::AutoLock auto_lock(data->list_lock_);
     // Attach the received data to the ShellLinkItem object.
     // This data will be decoded by the RunUpdateOnFileThread method.
-    if (!image_result.image.IsEmpty()) {
-      if (!data->icon_urls_.empty() && data->icon_urls_.front().second.get())
-        data->icon_urls_.front().second->set_icon_data(
-            image_result.image.AsBitmap());
+    if (!image_result.image.IsEmpty() && !data->icon_urls_.empty() &&
+        data->icon_urls_.front().second.get()) {
+      gfx::ImageSkia image_skia = image_result.image.AsImageSkia();
+      image_skia.EnsureRepsForSupportedScales();
+      std::unique_ptr<gfx::ImageSkia> deep_copy(image_skia.DeepCopy());
+      data->icon_urls_.front().second->set_icon_image(*deep_copy);
     }
 
     if (!data->icon_urls_.empty())
--- chrome/browser/win/jumplist_updater.h
+++ chrome/browser/win/jumplist_updater.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/win/scoped_comptr.h"
-#include "third_party/skia/include/core/SkBitmap.h"
+#include "ui/gfx/image/image_skia.h"
 
 // Represents a class used for creating an IShellLink object.
 // Even though an IShellLink also needs the absolute path to an application to
@@ -30,7 +30,7 @@ class ShellLinkItem : public base::RefCountedThreadSafe<ShellLinkItem> {
   const std::wstring& title() const { return title_; }
   const std::wstring& icon_path() const { return icon_path_; }
   int icon_index() const { return icon_index_; }
-  const SkBitmap& icon_data() const { return icon_data_; }
+  const gfx::ImageSkia& icon_image() const { return icon_image_; }
 
   std::wstring GetArguments() const;
   base::CommandLine* GetCommandLine();
@@ -44,8 +44,8 @@ class ShellLinkItem : public base::RefCountedThreadSafe<ShellLinkItem> {
     icon_index_ = index;
   }
 
-  void set_icon_data(const SkBitmap& data) {
-    icon_data_ = data;
+  void set_icon_image(const gfx::ImageSkia& image) {
+    icon_image_ = image;
   }
 
  private:
@@ -66,10 +66,10 @@ class ShellLinkItem : public base::RefCountedThreadSafe<ShellLinkItem> {
   // one icon, this value is 0.
   int icon_index_;
 
-  // Icon bitmap. Used by the browser JumpList.
+  // Icon image. Used by the browser JumpList.
   // Note that an icon path must be supplied to IShellLink, so users of this
   // class must save icon data to disk.
-  SkBitmap icon_data_;
+  gfx::ImageSkia icon_image_;
 
   DISALLOW_COPY_AND_ASSIGN(ShellLinkItem);
 };
