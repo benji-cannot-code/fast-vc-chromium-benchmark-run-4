004fc95204addf9d03e4acd8415cabdba4c8d050
lod <lod@chromium.org>
2017-02-08 13:26:37 -0800

    [ObjC ARC] Converts ios/chrome/browser/ui/activity_services:eg_tests to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2680973004
    Cr-Commit-Position: refs/heads/master@{#449098}

--- ios/chrome/browser/ui/activity_services/activity_service_controller_egtest.mm
+++ ios/chrome/browser/ui/activity_services/activity_service_controller_egtest.mm
@@ -23,6 +23,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/web/public/test/response_providers/response_provider.h"
 #include "ui/base/l10n/l10n_util_mac.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace {
 
 // Assert the activity service is visible by checking the "copy" button.
