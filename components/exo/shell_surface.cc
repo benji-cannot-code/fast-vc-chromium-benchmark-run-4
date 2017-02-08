dba5d2342e06f61b42becfe8b0567ba5799311e2
penghuang <penghuang@chromium.org>
2017-02-08 14:29:42 -0800

    exo: Do not call UpdateShelfStateForFullscreenChange() in ShellSurface
    ash::WorkspaceLayoutManager::OnPostWindowStateTypeChange() will call
    UpdateShelfVisibility() to update shelf visibility for window state
    changes for every window. So we don't need update shell visibility
    for exo Window anymore.
    
    BUG=670496
    
    Review-Url: https://codereview.chromium.org/2687683004
    Cr-Commit-Position: refs/heads/master@{#449110}

--- components/exo/shell_surface.cc
+++ components/exo/shell_surface.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/common/wm/window_resizer.h"
 #include "ash/common/wm/window_state.h"
 #include "ash/common/wm/window_state_delegate.h"
-#include "ash/common/wm_shell.h"
 #include "ash/common/wm_window.h"
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/wm/window_state_aura.h"
@@ -57,11 +56,6 @@ const struct Accelerator {
     {ui::VKEY_W, ui::EF_CONTROL_DOWN},
     {ui::VKEY_W, ui::EF_SHIFT_DOWN | ui::EF_CONTROL_DOWN},
     {ui::VKEY_F4, ui::EF_ALT_DOWN}};
-
-void UpdateShelfStateForFullscreenChange(views::Widget* widget) {
-  for (ash::WmWindow* root_window : ash::WmShell::Get()->GetAllRootWindows())
-    ash::WmShelf::ForWindow(root_window)->UpdateVisibilityState();
-}
 
 class CustomFrameView : public views::NonClientFrameView {
  public:
@@ -172,7 +166,6 @@ class CustomWindowStateDelegate : public ash::wm::WindowStateDelegate,
       ash::wm::WindowState* window_state =
           ash::wm::GetWindowState(widget_->GetNativeWindow());
       window_state->set_in_immersive_fullscreen(enter_fullscreen);
-      UpdateShelfStateForFullscreenChange(widget_);
     }
     return true;
   }
