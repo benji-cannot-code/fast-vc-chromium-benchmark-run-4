72778420a41aaa784f3698fdc2e608b3f9728c50
fmalita <fmalita@chromium.org>
2017-02-08 09:30:10 -0800

    Scalable spelling/grammar markers
    Replace the inlined bitmap markers with vector drawings, to improve rendering
    at arbitrary zoom levels.  Use cached SKP recordings for marker fragments, and
    a picture shader for tiling in X.
    
    In order to minimize disruption, this change aims to keep rendering as close
    to the original bitmaps as possible.  This can be revisited in the future
    (to e.g. improve dot spacing/distribution on Mac).
    
    Performance should be comparable to the current bitmap shader impl: Skia
    caches picture shader tiles, so for a given zoom level all markers should be
    hitting the same pre-rasterized cache entry (and essentially devolve into
    bitmap shaders).
    
    R=schenney@chromium.org
    BUG=686792
    
    Review-Url: https://codereview.chromium.org/2674003002
    Cr-Commit-Position: refs/heads/master@{#449024}

--- third_party/WebKit/Source/platform/graphics/GraphicsContext.h
+++ third_party/WebKit/Source/platform/graphics/GraphicsContext.h
@@ -48,7 +48,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/Noncopyable.h"
 #include <memory>
 
-class SkBitmap;
 class SkPath;
 class SkRRect;
 struct SkRect;
@@ -388,11 +387,6 @@ class PLATFORM_EXPORT GraphicsContext {
   static inline int focusRingOffset(int offset) { return offset + 2; }
 #else
   static inline int focusRingOffset(int offset) { return 0; }
-  static SkPMColor lineColors(int);
-  static SkPMColor antiColors1(int);
-  static SkPMColor antiColors2(int);
-  static void draw1xMarker(SkBitmap*, int);
-  static void draw2xMarker(SkBitmap*, int);
 #endif
 
   void saveLayer(const SkRect* bounds, const PaintFlags*);
