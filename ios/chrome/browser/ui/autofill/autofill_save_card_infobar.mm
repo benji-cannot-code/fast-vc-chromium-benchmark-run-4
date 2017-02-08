c21d453ebc4f9f4a7b92019d70f447e17bf8d4db
lod <lod@chromium.org>
2017-02-08 05:31:37 -0800

    [ObjC ARC] Converts ios/chrome/browser/ui/autofill:autofill to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:
    autofill_client_ios had a variable commented as weak but not actually handled as such. Tried using Weak_NS_Object but it had incorrect behavior wrt autorelease pools. Since the file is only currently included in arc converted targets, __weak was used which functions correctly.
    
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2686583002
    Cr-Commit-Position: refs/heads/master@{#448979}

--- ios/chrome/browser/ui/autofill/autofill_save_card_infobar.mm
+++ ios/chrome/browser/ui/autofill/autofill_save_card_infobar.mm
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/infobars/core/infobar.h"
 #include "ios/chrome/browser/infobars/infobar_utils.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace autofill {
 
 std::unique_ptr<infobars::InfoBar> CreateSaveCardInfoBarMobile(
--- ios/chrome/browser/ui/autofill/storage_switch_tooltip.mm
+++ ios/chrome/browser/ui/autofill/storage_switch_tooltip.mm
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/third_party/material_roboto_font_loader_ios/src/src/MaterialRobotoFontLoader.h"
 #include "ui/base/l10n/l10n_util.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace {
 
 const CGFloat kCornerRadius = 2.0f;
