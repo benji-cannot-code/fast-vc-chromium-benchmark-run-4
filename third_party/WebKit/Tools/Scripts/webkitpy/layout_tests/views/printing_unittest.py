f5467fee1a53b5113c76b3113fd6f0b1aacaee4b
qyearsley <qyearsley@chromium.org>
2017-02-07 16:36:44 -0800

    Remove unused methods in webkitpy/layout_tests/.
    BUG=677561
    
    Review-Url: https://codereview.chromium.org/2678273004
    Cr-Commit-Position: refs/heads/master@{#448820}

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/views/printing_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/views/printing_unittest.py
@@ -80,9 +80,6 @@ class FakeShard(object):
 
 class Testprinter(unittest.TestCase):
 
-    def assertEmpty(self, stream):
-        self.assertFalse(stream.getvalue())
-
     def assertNotEmpty(self, stream):
         self.assertTrue(stream.getvalue())
 
