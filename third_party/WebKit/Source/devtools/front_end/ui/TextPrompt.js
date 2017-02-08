4d458af291230de867221ac65bf90c41af90d2f6
einbinder <einbinder@chromium.org>
2017-02-07 19:27:41 -0800

    DevTools: Combine TextPrompt events into UI.TextPrompt.TextChangedEvent
    This patch fires TextChangedEvent whenever the textWithCurrentSuggestion
    is changed. This prevents clients of text prompt from needing to listen
    to ItemApplied, ItemAccepted, and input events.
    
    BUG=none
    R=lushnikov
    
    Review-Url: https://codereview.chromium.org/2681043002
    Cr-Commit-Position: refs/heads/master@{#448892}

--- third_party/WebKit/Source/devtools/front_end/ui/TextPrompt.js
+++ third_party/WebKit/Source/devtools/front_end/ui/TextPrompt.js
@@ -308,6 +308,7 @@ UI.TextPrompt = class extends Common.Object {
       this._queryRange.endColumn += text.length - this._previousText.length;
     this._refreshGhostText();
     this._previousText = text;
+    this.emit(new UI.TextPrompt.TextChangedEvent());
 
     this.autoCompleteSoon();
   }
@@ -335,7 +336,7 @@ UI.TextPrompt = class extends Common.Object {
     this._refreshGhostText();
 
     if (beforeText !== this.textWithCurrentSuggestion())
-      this.dispatchEventToListeners(UI.TextPrompt.Events.ItemApplied);
+      this.emit(new UI.TextPrompt.TextChangedEvent());
   }
 
   _refreshGhostText() {
@@ -509,7 +510,7 @@ UI.TextPrompt = class extends Common.Object {
     this._currentSuggestion = suggestion;
     this._refreshGhostText();
     if (isIntermediateSuggestion)
-      this.dispatchEventToListeners(UI.TextPrompt.Events.ItemApplied);
+      this.emit(new UI.TextPrompt.TextChangedEvent());
   }
 
   /**
@@ -532,7 +533,7 @@ UI.TextPrompt = class extends Common.Object {
         this._queryRange.startColumn + this._currentSuggestion.length);
 
     this.clearAutocomplete();
-    this.dispatchEventToListeners(UI.TextPrompt.Events.ItemAccepted);
+    this.emit(new UI.TextPrompt.TextChangedEvent());
 
     return true;
   }
@@ -639,8 +640,5 @@ UI.TextPrompt = class extends Common.Object {
 
 UI.TextPrompt.DefaultAutocompletionTimeout = 250;
 
-/** @enum {symbol} */
-UI.TextPrompt.Events = {
-  ItemApplied: Symbol('text-prompt-item-applied'),
-  ItemAccepted: Symbol('text-prompt-item-accepted')
-};
+/** @implements {Common.Emittable} */
+UI.TextPrompt.TextChangedEvent = class {};
