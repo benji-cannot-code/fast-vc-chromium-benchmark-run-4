ee6d16cd2bcfa85c41cb31358308dc8ec3f71ca0
sorin <sorin@chromium.org>
2017-02-08 15:35:19 -0800

    Skeleton mechanical impl. for the RecoveryImprovedComponent.
    * defines a command line argument --enable-features=ImprovedRecoveryComponent" and a Finch experiment.
    * uses the default component installer
    * does nothing otherwise.
    
    BUG=687231
    
    Review-Url: https://codereview.chromium.org/2661113003
    Cr-Commit-Position: refs/heads/master@{#449139}

--- chrome/browser/component_updater/component_updater_prefs.cc
+++ chrome/browser/component_updater/component_updater_prefs.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/component_updater/chrome_component_updater_configurator.h"
 #include "chrome/browser/component_updater/recovery_component_installer.h"
+#include "chrome/browser/component_updater/recovery_improved_component_installer.h"
 #include "chrome/browser/component_updater/supervised_user_whitelist_installer.h"
 
 namespace component_updater {
@@ -14,6 +15,7 @@ namespace component_updater {
 void RegisterPrefs(PrefRegistrySimple* registry) {
   RegisterPrefsForChromeComponentUpdaterConfigurator(registry);
   RegisterPrefsForRecoveryComponent(registry);
+  RegisterPrefsForRecoveryImprovedComponent(registry);
   SupervisedUserWhitelistInstaller::RegisterPrefs(registry);
 }
 
