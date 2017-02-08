9b72b465d17325a05fe4c51a8c62c448a0017fed
fdoray <fdoray@chromium.org>
2017-02-08 05:20:45 -0800

    Use TaskScheduler instead of blocking pool in networking_private_credentials_getter_browsertest.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=stevenjb@chromium.org
    
    Review-Url: https://codereview.chromium.org/2671283002
    Cr-Commit-Position: refs/heads/master@{#448973}

--- chrome/browser/extensions/api/networking_private/networking_private_credentials_getter_browsertest.cc
+++ chrome/browser/extensions/api/networking_private/networking_private_credentials_getter_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/macros.h"
 #include "base/run_loop.h"
+#include "base/task_scheduler/post_task.h"
 #include "chrome/browser/extensions/api/networking_private/networking_private_credentials_getter.h"
 #include "chrome/common/extensions/wifi_credentials_getter.mojom.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -28,8 +29,9 @@ class NetworkingPrivateCredentialsGetterTest : public InProcessBrowserTest {
       network_ = extensions::mojom::WiFiCredentialsGetter::kWiFiTestNetwork;
 
     done_called_ = false;
-    content::BrowserThread::PostBlockingPoolTask(
-        FROM_HERE,
+    base::PostTaskWithTraits(
+        FROM_HERE, base::TaskTraits().MayBlock().WithPriority(
+                       base::TaskPriority::BACKGROUND),
         base::Bind(&NetworkingPrivateCredentialsGetterTest::GetCredentials,
                    base::Unretained(this)));
     run_loop.Run();
