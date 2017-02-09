4b742ad406aaa15193c42db6377335de8692938d
japhet <japhet@chromium.org>
2017-02-09 14:25:56 -0800

    Actually handle the Vary header in blink, rather than punting on sight.
    BUG=527789,537940,602198,614945
    TEST=ResourceTest.Vary
    
    Review-Url: https://codereview.chromium.org/2681233002
    Cr-Commit-Position: refs/heads/master@{#449438}

--- third_party/WebKit/Source/platform/loader/fetch/ResourceFetcherTest.cpp
+++ third_party/WebKit/Source/platform/loader/fetch/ResourceFetcherTest.cpp
@@ -155,7 +155,7 @@ TEST_F(ResourceFetcherTest, Vary) {
   response.setHTTPHeaderField(HTTPNames::Vary, "*");
   resource->responseReceived(response, nullptr);
   resource->finish();
-  ASSERT_TRUE(resource->hasVaryHeader());
+  ASSERT_TRUE(resource->mustReloadDueToVaryHeader(url));
 
   ResourceFetcher* fetcher = ResourceFetcher::create(context());
   ResourceRequest resourceRequest(url);
@@ -221,7 +221,7 @@ TEST_F(ResourceFetcherTest, VaryOnBack) {
   response.setHTTPHeaderField(HTTPNames::Vary, "*");
   resource->responseReceived(response, nullptr);
   resource->finish();
-  ASSERT_TRUE(resource->hasVaryHeader());
+  ASSERT_TRUE(resource->mustReloadDueToVaryHeader(url));
 
   ResourceRequest resourceRequest(url);
   resourceRequest.setRequestContext(WebURLRequest::RequestContextInternal);
@@ -246,7 +246,7 @@ TEST_F(ResourceFetcherTest, VaryResource) {
   Resource* resource = MockResource::fetch(fetchRequestOriginal, fetcher);
   ASSERT_TRUE(resource);
   Platform::current()->getURLLoaderMockFactory()->serveAsynchronousRequests();
-  ASSERT_TRUE(resource->hasVaryHeader());
+  ASSERT_TRUE(resource->mustReloadDueToVaryHeader(url));
 
   FetchRequest fetchRequest = FetchRequest(url, FetchInitiatorInfo());
   Resource* newResource = MockResource::fetch(fetchRequest, fetcher);
