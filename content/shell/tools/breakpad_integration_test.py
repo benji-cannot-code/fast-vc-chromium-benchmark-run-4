64b9dfed39ff8563b0d88de3117fb3609cec2c7a
jochen <jochen@chromium.org>
2017-02-09 09:01:19 -0800

    Run content shell crash integration tests
    BUG=688737, 689568
    R=phajdan.jr@chromium.org
    
    Review-Url: https://codereview.chromium.org/2681713002
    Cr-Commit-Position: refs/heads/master@{#449326}

--- content/shell/tools/breakpad_integration_test.py
+++ content/shell/tools/breakpad_integration_test.py
@@ -12,6 +12,7 @@ well as that the tools can symbolize a stack trace."""
 
 
 import glob
+import json
 import optparse
 import os
 import shutil
@@ -35,6 +36,8 @@ def main():
                     type='int', help='Number of parallel tasks to run.')
   parser.add_option('-v', '--verbose', action='store_true',
                     help='Print verbose status output.')
+  parser.add_option('', '--json', default='',
+                    help='Path to JSON output.')
 
   (options, _) = parser.parse_args()
 
@@ -131,10 +134,17 @@ def main():
 
   except:
     print "FAIL: %s" % failure
+    if options.json:
+      with open(options.json, 'w') as json_file:
+        json.dump([failure], json_file)
+
     return 1
 
   else:
     print "PASS: Breakpad integration test ran successfully."
+    if options.json:
+      with open(options.json, 'w') as json_file:
+        json.dump([], json_file)
     return 0
 
   finally:
