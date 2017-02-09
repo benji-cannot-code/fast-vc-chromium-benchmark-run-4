71994296f84844425b6311558815bc024e2a2521
dskiba <dskiba@chromium.org>
2017-02-09 15:21:04 -0800

    [memory-infra] Whitelist TabRestoreService MDP.
    This CL whitelists TabRestoreService MDP to make it available in
    slow reports.
    
    BUG=645123
    
    Review-Url: https://codereview.chromium.org/2674203003
    Cr-Commit-Position: refs/heads/master@{#449458}

--- base/trace_event/memory_infra_background_whitelist.cc
+++ base/trace_event/memory_infra_background_whitelist.cc
@@ -36,6 +36,7 @@ const char* const kDumpProviderWhitelist[] = {
     "V8Isolate",
     "WinHeap",
     "SyncDirectory",
+    "TabRestoreServiceHelper",
     nullptr  // End of list marker.
 };
 
@@ -126,6 +127,9 @@ const char* const kAllocatorDumpNameWhitelist[] = {
     "sync/0x?/model_type/TYPED_URL",
     "sync/0x?/model_type/WALLET_METADATA",
     "sync/0x?/model_type/WIFI_CREDENTIAL",
+    "tab_restore/service_helper_0x?/entries",
+    "tab_restore/service_helper_0x?/entries/tab_0x?",
+    "tab_restore/service_helper_0x?/entries/window_0x?",
     nullptr  // End of list marker.
 };
 
