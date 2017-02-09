2828c9b1cf75d707d02b1981c7f5cc234590e045
gmanikpure <gmanikpure@chromium.org>
2017-02-09 14:42:46 -0800

    [chromedriver] Drop support for M54 and add M57
    BUG=
    
    Review-Url: https://codereview.chromium.org/2687503002
    Cr-Commit-Position: refs/heads/master@{#449443}

--- chrome/test/chromedriver/chrome/version.cc
+++ chrome/test/chromedriver/chrome/version.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // This variable must be able to be found and parsed by the upload script.
-const int kMinimumSupportedChromeVersion[] = {54, 0, 2840, 0};
+const int kMinimumSupportedChromeVersion[] = {55, 0, 2883, 0};
 
 }  // namespace
 
