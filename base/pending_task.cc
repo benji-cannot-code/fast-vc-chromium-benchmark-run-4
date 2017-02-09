812781c3e8711f044718e0ae5653c55d483acc0f
ajwong <ajwong@chromium.org>
2017-02-09 15:52:40 -0800

    Record async "task backtraces"
    Places a small array in PendingTask that records the past 4 PostTasks
    that result in this task being run. In essence, it creates a backtrace
    of the asynchronous call stack resulting in this task being run.
    
    This array is copied onto the stack during execution so that it is
    available in crash dumps.
    
    BUG=690197
    
    Review-Url: https://codereview.chromium.org/1044413002
    Cr-Commit-Position: refs/heads/master@{#449471}

--- base/pending_task.cc
+++ base/pending_task.cc
@@ -5,18 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/pending_task.h"
 
+#include "base/message_loop/message_loop.h"
 #include "base/tracked_objects.h"
 
 namespace base {
 
 PendingTask::PendingTask(const tracked_objects::Location& posted_from,
                          OnceClosure task)
-    : base::TrackingInfo(posted_from, TimeTicks()),
-      task(std::move(task)),
-      posted_from(posted_from),
-      sequence_num(0),
-      nestable(true),
-      is_high_res(false) {}
+    : PendingTask(posted_from, std::move(task), TimeTicks(), true) {}
 
 PendingTask::PendingTask(const tracked_objects::Location& posted_from,
                          OnceClosure task,
@@ -27,7 +23,19 @@ PendingTask::PendingTask(const tracked_objects::Location& posted_from,
       posted_from(posted_from),
       sequence_num(0),
       nestable(nestable),
-      is_high_res(false) {}
+      is_high_res(false) {
+  const PendingTask* parent_task =
+      MessageLoop::current() ? MessageLoop::current()->current_pending_task_
+                             : nullptr;
+  if (parent_task) {
+    task_backtrace[0] = parent_task->posted_from.program_counter();
+    std::copy(parent_task->task_backtrace.begin(),
+              parent_task->task_backtrace.end() - 1,
+              task_backtrace.begin() + 1);
+  } else {
+    task_backtrace.fill(nullptr);
+  }
+}
 
 PendingTask::PendingTask(PendingTask&& other) = default;
 
--- base/pending_task.h
+++ base/pending_task.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_PENDING_TASK_H_
 #define BASE_PENDING_TASK_H_
 
+#include <array>
 #include <queue>
 
 #include "base/base_export.h"
@@ -37,6 +38,9 @@ struct BASE_EXPORT PendingTask : public TrackingInfo {
 
   // The site this PendingTask was posted from.
   tracked_objects::Location posted_from;
+
+  // Task backtrace.
+  std::array<const void*, 4> task_backtrace;
 
   // Secondary sort key for run time.
   int sequence_num;
