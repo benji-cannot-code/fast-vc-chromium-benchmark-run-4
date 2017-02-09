fec08298d03999641b250b240ce20b14ff78d54e
sdefresne <sdefresne@chromium.org>
2017-02-09 05:08:14 -0800

    Add mechanism to get the Tab from the associated WebState.
    BUG=687207
    
    Review-Url: https://codereview.chromium.org/2681843002
    Cr-Commit-Position: refs/heads/master@{#449274}

--- ios/chrome/browser/tabs/tab.mm
+++ ios/chrome/browser/tabs/tab.mm
@@ -89,6 +89,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/ssl/ios_security_state_tab_helper.h"
 #import "ios/chrome/browser/storekit_launcher.h"
 #include "ios/chrome/browser/sync/ios_chrome_synced_tab_delegate.h"
+#import "ios/chrome/browser/tabs/legacy_tab_helper.h"
 #import "ios/chrome/browser/tabs/tab_delegate.h"
 #import "ios/chrome/browser/tabs/tab_dialog_delegate.h"
 #import "ios/chrome/browser/tabs/tab_headers_delegate.h"
@@ -535,6 +536,10 @@ void TabInfoBarObserver::OnInfoBarReplaced(infobars::InfoBar* old_infobar,
     webStateImpl_.reset(static_cast<web::WebStateImpl*>(webState.release()));
     webStateObserver_.reset(
         new web::WebStateObserverBridge(webStateImpl_.get(), self));
+
+    // Do not respect |attachTabHelpers| as this tab helper is required for
+    // proper conversion from WebState to Tab.
+    LegacyTabHelper::CreateForWebState(webStateImpl_.get(), self);
 
     [self.webController setDelegate:self];
 
@@ -1249,6 +1254,8 @@ void TabInfoBarObserver::OnInfoBarReplaced(infobars::InfoBar* old_infobar,
   // to drive its own destruction.
   base::scoped_nsobject<Tab> kungFuDeathGrip([self retain]);
   [parentTabModel_ didCloseTab:self];  // Inform parent of tab closure.
+
+  LegacyTabHelper::RemoveFromWebState(webStateImpl_.get());
   webStateImpl_.reset();
 }
 
