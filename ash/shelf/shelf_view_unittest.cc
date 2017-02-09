aacc8f3f7fd43b37da545e3b669065a1b0e675dc
mohsen <mohsen@chromium.org>
2017-02-09 08:57:22 -0800

    Remove non-MD test coverage form ShelfViewTest and subclasses
    Removed touch feedback tests from ShelfViewTest as it is a non-MD
    feature. Also removed forcing EXPERIMENTAL mode in ShelfViewInkDropTest
    and its subclasses as ink drop is graduated out of experimental; and
    material mode is on by default.
    
    BUG=685837
    TEST=ShelfViewTest.*, ShelfViewInkDropTest.*, OverflowButtonInkDropTest.*,
         OverflowButtonActiveInkDropTest.* in ash_unittests
    
    Review-Url: https://codereview.chromium.org/2686883003
    Cr-Commit-Position: refs/heads/master@{#449323}

--- ash/shelf/shelf_view_unittest.cc
+++ ash/shelf/shelf_view_unittest.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/common/shelf/wm_shelf.h"
 #include "ash/common/shelf/wm_shelf_observer.h"
 #include "ash/common/system/web_notification/web_notification_tray.h"
-#include "ash/common/test/material_design_controller_test_api.h"
 #include "ash/common/test/test_shelf_delegate.h"
 #include "ash/common/test/test_shelf_item_delegate.h"
 #include "ash/common/test/test_system_tray_delegate.h"
@@ -1807,54 +1806,6 @@ TEST_F(ShelfViewTest, CheckOverflowStatusPinOpenedAppToShelf) {
   EXPECT_FALSE(GetButtonByID(platform_app_id)->visible());
 }
 
-// Tests that the AppListButton renders as active in response to touches.
-TEST_F(ShelfViewTest, AppListButtonTouchFeedback) {
-  // Touch feedback is not available in material mode.
-  if (ash::MaterialDesignController::IsShelfMaterial())
-    return;
-
-  AppListButton* app_list_button = shelf_view_->GetAppListButton();
-  EXPECT_FALSE(app_list_button->draw_background_as_active());
-
-  ui::test::EventGenerator& generator = GetEventGenerator();
-  generator.set_current_location(
-      app_list_button->GetBoundsInScreen().CenterPoint());
-  generator.PressTouch();
-  EXPECT_TRUE(app_list_button->draw_background_as_active());
-
-  generator.ReleaseTouch();
-  EXPECT_FALSE(app_list_button->draw_background_as_active());
-  EXPECT_TRUE(WmShell::Get()->GetAppListTargetVisibility());
-}
-
-// Tests that a touch that slides out of the bounds of the AppListButton leads
-// to the end of rendering an active state.
-TEST_F(ShelfViewTest, AppListButtonTouchFeedbackCancellation) {
-  // Touch feedback is not available in material mode.
-  if (ash::MaterialDesignController::IsShelfMaterial())
-    return;
-
-  AppListButton* app_list_button = shelf_view_->GetAppListButton();
-  EXPECT_FALSE(app_list_button->draw_background_as_active());
-
-  ui::test::EventGenerator& generator = GetEventGenerator();
-  generator.set_current_location(
-      app_list_button->GetBoundsInScreen().CenterPoint());
-  generator.PressTouch();
-  EXPECT_TRUE(app_list_button->draw_background_as_active());
-
-  gfx::Point moved_point(
-      app_list_button->GetBoundsInScreen().right() + 1,
-      app_list_button->GetBoundsInScreen().CenterPoint().y());
-  generator.MoveTouch(moved_point);
-  EXPECT_FALSE(app_list_button->draw_background_as_active());
-
-  generator.set_current_location(moved_point);
-  generator.ReleaseTouch();
-  EXPECT_FALSE(app_list_button->draw_background_as_active());
-  EXPECT_FALSE(WmShell::Get()->GetAppListTargetVisibility());
-}
-
 // Verifies that Launcher_ButtonPressed_* UMA user actions are recorded when an
 // item is selected.
 TEST_F(ShelfViewTest,
@@ -2026,8 +1977,7 @@ class ListMenuShelfItemDelegate : public TestShelfItemDelegate {
 
 }  // namespace
 
-// Test fixture that forces material design mode in order to test ink drop
-// ripples on shelf.
+// Test fixture for testing material design ink drop ripples on shelf.
 class ShelfViewInkDropTest : public ShelfViewTest {
  public:
   ShelfViewInkDropTest() {}
@@ -2035,8 +1985,6 @@ class ShelfViewInkDropTest : public ShelfViewTest {
 
   void SetUp() override {
     ash_test_helper()->set_test_shell_delegate(CreateTestShellDelegate());
-
-    set_material_mode(ash::MaterialDesignController::MATERIAL_EXPERIMENTAL);
 
     ShelfViewTest::SetUp();
   }
