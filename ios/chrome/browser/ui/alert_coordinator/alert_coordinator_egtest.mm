c9a77854926b1fa4ac412f98459044a80f808855
lod <lod@chromium.org>
2017-02-09 01:34:11 -0800

    [ObjC ARC] Converts ios/chrome/browser/ui/alert_coordinator:eg_tests to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2680803003
    Cr-Commit-Position: refs/heads/master@{#449246}

--- ios/chrome/browser/ui/alert_coordinator/alert_coordinator_egtest.mm
+++ ios/chrome/browser/ui/alert_coordinator/alert_coordinator_egtest.mm
@@ -7,12 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
-#import "base/mac/scoped_nsobject.h"
 #include "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/ui/alert_coordinator/alert_coordinator.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
 #import "ios/testing/earl_grey/disabled_test_macros.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
 
 namespace {
 NSString* kTitle = @"Foo Title";
@@ -40,16 +43,16 @@ NSString* kTitle = @"Foo Title";
   UIViewController* topViewController =
       [[[UIApplication sharedApplication] keyWindow] rootViewController];
 
-  base::scoped_nsobject<AlertCoordinator> alertCoordinator(
+  AlertCoordinator* alertCoordinator =
       [[AlertCoordinator alloc] initWithBaseViewController:topViewController
                                                      title:kTitle
-                                                   message:nil]);
+                                                   message:nil];
 
   [alertCoordinator start];
 
   GREYAssertTrue([self isPresentingAlert], @"An alert should be presented");
 
-  alertCoordinator.reset();
+  alertCoordinator = nil;
 
   GREYAssertFalse([self isPresentingAlert], @"The alert should be removed");
 }
@@ -65,10 +68,10 @@ NSString* kTitle = @"Foo Title";
   UIViewController* topViewController =
       [[[UIApplication sharedApplication] keyWindow] rootViewController];
 
-  base::scoped_nsobject<AlertCoordinator> alertCoordinator(
+  AlertCoordinator* alertCoordinator =
       [[AlertCoordinator alloc] initWithBaseViewController:topViewController
                                                      title:kTitle
-                                                   message:nil]);
+                                                   message:nil];
 
   __block BOOL blockCalled = NO;
 
