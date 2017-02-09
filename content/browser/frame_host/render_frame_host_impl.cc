1af64ffdcf9fec8be3f6eacc0966678a291a6abc
iclelland <iclelland@chromium.org>
2017-02-09 14:44:03 -0800

    Introduce content-side Feature Policy object and maintain in parallel with renderer policy.
    This is part 3 of 5 in the effort to move the FeaturePolicy implementation
    into the content layer, which will facilitate use of the framework for browser-
    based policy decisions.
    
    See the other CLs in this series:
    [1/5] https://codereview.chromium.org/2648423002/ (Rename classes)
    [2/5] https://codereview.chromium.org/2654873004/ (Centralize content-side code)
    [3/5] (This CL)                                   (Maintain parallel FP in browser)
    [4/5] https://codereview.chromium.org/2651883008/ (Use content/ FP in blink)
    [5/5] https://codereview.chromium.org/2656533004/ (Remove unused blink code.)
    
    BUG=685195
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2655663004
    Cr-Commit-Position: refs/heads/master@{#449444}

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -1851,6 +1851,8 @@ void RenderFrameHostImpl::OnDidChangeName(const std::string& name,
 void RenderFrameHostImpl::OnDidSetFeaturePolicyHeader(
     const ParsedFeaturePolicyHeader& parsed_header) {
   frame_tree_node()->SetFeaturePolicyHeader(parsed_header);
+  ResetFeaturePolicy();
+  feature_policy_->SetHeaderPolicy(parsed_header);
 }
 
 void RenderFrameHostImpl::OnDidAddContentSecurityPolicy(
@@ -3390,6 +3392,14 @@ void RenderFrameHostImpl::DeleteWebBluetoothService(
       });
   DCHECK(it != web_bluetooth_services_.end());
   web_bluetooth_services_.erase(it);
+}
+
+void RenderFrameHostImpl::ResetFeaturePolicy() {
+  RenderFrameHostImpl* parent_frame_host = GetParent();
+  const FeaturePolicy* parent_policy =
+      parent_frame_host ? parent_frame_host->get_feature_policy() : nullptr;
+  feature_policy_ = FeaturePolicy::CreateFromParentPolicy(
+      parent_policy, last_committed_origin_);
 }
 
 void RenderFrameHostImpl::Create(
