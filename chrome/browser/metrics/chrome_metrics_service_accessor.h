e8ae1ced0fd8af30d3eb30fb196b508be8896a89
jialiul <jialiul@chromium.org>
2017-02-10 14:26:21 -0800

    Remove observer when download is being destructed.
    Also consolidate DownloadSBClient and DownloadUrlSBClient into one.
    
    BUG=689525
    
    Review-Url: https://codereview.chromium.org/2686713002
    Cr-Commit-Position: refs/heads/master@{#449763}

--- chrome/browser/metrics/chrome_metrics_service_accessor.h
+++ chrome/browser/metrics/chrome_metrics_service_accessor.h
@@ -60,7 +60,7 @@ bool IsOmniboxEnabled(Profile* profile);
 }
 
 namespace safe_browsing {
-class DownloadSBClient;
+class DownloadUrlSBClient;
 class IncidentReportingService;
 class ReporterRunner;
 class SafeBrowsingService;
@@ -120,7 +120,7 @@ class ChromeMetricsServiceAccessor : public metrics::MetricsServiceAccessor {
   friend class speech::ChromeSpeechRecognitionManagerDelegate;
   friend class system_logs::ChromeInternalLogSource;
   friend class UmaSessionStats;
-  friend class safe_browsing::DownloadSBClient;
+  friend class safe_browsing::DownloadUrlSBClient;
   friend class safe_browsing::IncidentReportingService;
   friend class safe_browsing::ReporterRunner;
   friend class safe_browsing::SRTFetcher;
