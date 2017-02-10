6670bda1d7deed6781cd540421b32678ece9bf19
lod <lod@chromium.org>
2017-02-10 07:52:32 -0800

    [ObjC ARC] Converts ios/chrome/browser/ui/main:unit_tests to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2687913002
    Cr-Commit-Position: refs/heads/master@{#449626}

--- ios/chrome/browser/ui/main/main_coordinator_unittest.mm
+++ ios/chrome/browser/ui/main/main_coordinator_unittest.mm
@@ -7,16 +7,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
-#include "base/mac/scoped_nsobject.h"
 #include "ios/chrome/browser/ui/main/main_view_controller.h"
 #include "testing/gtest_mac.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
 
 TEST(MainCoordinatorTest, SizeViewController) {
   CGRect rect = [UIScreen mainScreen].bounds;
   UIWindow* window = [UIApplication sharedApplication].keyWindow;
-  base::scoped_nsobject<MainCoordinator> coordinator(
-      [[MainCoordinator alloc] initWithWindow:window]);
+  MainCoordinator* coordinator =
+      [[MainCoordinator alloc] initWithWindow:window];
   [coordinator start];
   EXPECT_TRUE(
-      CGRectEqualToRect(rect, coordinator.get().mainViewController.view.frame));
+      CGRectEqualToRect(rect, coordinator.mainViewController.view.frame));
 }
