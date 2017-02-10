ea8d7a14684dc8ba393bf7dd20203f63d073f9e2
oka <oka@google.com>
2017-02-10 15:04:53 -0800

    nit: Remove a redundant header.
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2685163005
    Cr-Commit-Position: refs/heads/master@{#449775}

--- ui/keyboard/content/keyboard_ui_content.cc
+++ ui/keyboard/content/keyboard_ui_content.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/site_instance.h"
 #include "content/public/browser/web_contents.h"
-#include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_delegate.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_ui.h"
