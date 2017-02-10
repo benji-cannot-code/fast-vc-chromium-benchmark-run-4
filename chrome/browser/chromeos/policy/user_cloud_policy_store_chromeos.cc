db133e04a53b54a21a8a7a1c776b268eb56d773a
jdufault <jdufault@chromium.org>
2017-02-09 18:17:05 -0800

    Remove UserPolicyDiskCache; it is not used.
    BUG=690665
    
    Review-Url: https://codereview.chromium.org/2681093005
    Cr-Commit-Position: refs/heads/master@{#449516}

--- chrome/browser/chromeos/policy/user_cloud_policy_store_chromeos.cc
+++ chrome/browser/chromeos/policy/user_cloud_policy_store_chromeos.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequenced_task_runner.h"
 #include "base/stl_util.h"
 #include "base/strings/stringprintf.h"
-#include "chrome/browser/chromeos/policy/user_policy_disk_cache.h"
 #include "chrome/browser/chromeos/policy/user_policy_token_loader.h"
 #include "chromeos/cryptohome/cryptohome_parameters.h"
 #include "chromeos/dbus/cryptohome_client.h"
