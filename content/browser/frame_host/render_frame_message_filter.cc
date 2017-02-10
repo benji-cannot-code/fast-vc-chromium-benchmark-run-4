8a1832acfa08897e3ef77a66f7be7466b3c93da3
tzik <tzik@chromium.org>
2017-02-09 19:57:30 -0800

    Simplify SequencedTaskRunner::{Delete,Releaose}Soon impl
    This CL removes SequencedTaskRunner-like type support from DeleteHelper.
    There no longer exists such a class, and the removal simplify the
    implementation.
    
    TBR=kinuko@chromium.org, shess@chromium.org, asvitkine@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2686033002
    Cr-Commit-Position: refs/heads/master@{#449544}

--- content/browser/frame_host/render_frame_message_filter.cc
+++ content/browser/frame_host/render_frame_message_filter.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/frame_host/render_frame_message_filter.h"
 
 #include "base/command_line.h"
+#include "base/debug/alias.h"
 #include "base/macros.h"
 #include "base/strings/string_util.h"
 #include "build/build_config.h"
--- sql/connection.cc
+++ sql/connection.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
+#include "base/debug/alias.h"
 #include "base/debug/dump_without_crashing.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
