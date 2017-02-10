b74984b6f8a8568d87a38b47d467cbc49f5b90aa
jonross <jonross@chromium.org>
2017-02-10 10:17:19 -0800

    Stop MenuItemView From Directly Caching MenuController
    When a MenuController closes its associated Widget is destroyed later on the
    MessageLoop. Due to this it is possible for a MenuHostRootView to receive input
    while the associated MenuController has been destroyed.
    
    MenuHostRootView accesses the MenuController through MenuItemView which simply
    caches it. This change updates MenuItemView to take a WeakPtr to the controller.
    
    TEST=MenuControllerTest.HostReceivesInputBeforeDestruction
    BUG=690097
    
    Review-Url: https://codereview.chromium.org/2684993009
    Cr-Commit-Position: refs/heads/master@{#449663}

--- ui/views/controls/menu/menu_item_view.h
+++ ui/views/controls/menu/menu_item_view.h
@@ -12,11 +12,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/logging.h"
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "base/strings/string16.h"
 #include "build/build_config.h"
 #include "ui/base/models/menu_separator_types.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/views/controls/menu/menu_config.h"
+#include "ui/views/controls/menu/menu_controller.h"
 #include "ui/views/controls/menu/menu_types.h"
 #include "ui/views/view.h"
 
@@ -413,7 +415,12 @@ class VIEWS_EXPORT MenuItemView : public View {
     actual_menu_position_ = actual_menu_position;
   }
 
-  void set_controller(MenuController* controller) { controller_ = controller; }
+  void set_controller(MenuController* controller) {
+    if (controller)
+      controller_ = controller->AsWeakPtr();
+    else
+      controller_.reset();
+  }
 
   // Returns true if this MenuItemView contains a single child
   // that is responsible for rendering the content.
@@ -437,7 +444,7 @@ class VIEWS_EXPORT MenuItemView : public View {
   MenuDelegate* delegate_;
 
   // The controller for the run operation, or NULL if the menu isn't showing.
-  MenuController* controller_;
+  base::WeakPtr<MenuController> controller_;
 
   // Used to detect when Cancel was invoked.
   bool canceled_;
