40a8d3d06c4117d00cbc404afccdecf665ebfcf0
qyearsley <qyearsley@chromium.org>
2017-02-10 10:34:30 -0800

    WPT Auto-import: fix for owner list in import CLs.
    This is a follow-up fix for http://crrev.com/2684663003.
    
    The mistake was: In one part of the code I was assuming that
    get_directory_owners() would return a map from owner to list of
    directories, but in reality it returned a dict from owner to single
    directory (string).
    
    We want to have lists of directories, since a single import CL
    will often have modifications to many directories for one owner.
    
    This resulted in silly-looking CL descriptions like
    http://crrev.com/2687343002.
    
    BUG=687270
    
    Review-Url: https://codereview.chromium.org/2684423004
    Cr-Commit-Position: refs/heads/master@{#449666}

--- third_party/WebKit/Tools/Scripts/webkitpy/w3c/directory_owners_extractor.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/w3c/directory_owners_extractor.py
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import collections
 import re
 
 from webkitpy.common.system.filesystem import FileSystem
@@ -62,14 +63,15 @@ class DirectoryOwnersExtractor(object):
             changed_files: A list of file paths relative to the repository root.
 
         Returns:
-            A dict mapping (owner) email addresses to (owned) directories.
+            A dict mapping owner email addresses to lists of owned directories.
         """
         tests = [self.finder.layout_test_name(path) for path in changed_files]
         tests = [t for t in tests if t is not None]
-        email_map = {}
+        email_map = collections.defaultdict(list)
         for directory, owners in self.owner_map.iteritems():
             owned_tests = [t for t in tests if t.startswith(directory)]
-            if owned_tests:
-                for owner in owners:
-                    email_map[owner] = directory
+            if not owned_tests:
+                continue
+            for owner in owners:
+                email_map[owner].append(directory)
         return email_map
