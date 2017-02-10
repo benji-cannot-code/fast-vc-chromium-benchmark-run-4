4597b484b3edfb033be602c8e7f39b6f96b6aadc
yosin <yosin@chromium.org>
2017-02-10 02:17:49 -0800

    Make SpellChecker::replaceMisspelledRange() not to use FrameSelection::firstRange()
    This patch makes |SpellChecker::replaceMisspelledRange()| not to use
    |FrameSelection::firstRange()| as preparation of getting rid of |firstRange()|
    from |FrameSelection| and |SelectionEditor|.
    
    This is follow-up of [1].
    
    [1] http://crrev.com/2653523003 Make DOMSelection cache Range
    
    BUG=490206
    TEST=n/a; no behavior changes
    
    Review-Url: https://codereview.chromium.org/2686253002
    Cr-Commit-Position: refs/heads/master@{#449576}

--- third_party/WebKit/Source/core/editing/spellcheck/SpellChecker.cpp
+++ third_party/WebKit/Source/core/editing/spellcheck/SpellChecker.cpp
@@ -847,8 +847,8 @@ void SpellChecker::replaceMisspelledRange(const String& text) {
 
   // Dispatch 'beforeinput'.
   Element* const target = frame().editor().findEventTargetFromSelection();
-  RangeVector* const ranges =
-      new RangeVector(1, frame().selection().firstRange());
+  Range* const range = createRange(markerRange);
+  RangeVector* const ranges = new RangeVector(1, range);
   DataTransfer* const dataTransfer = DataTransfer::create(
       DataTransfer::DataTransferType::InsertReplacementText,
       DataTransferAccessPolicy::DataTransferReadable,
