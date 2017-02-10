43fee354996530d8a52620dc95d5d15db1507341
gogerald <gogerald@chromium.org>
2017-02-10 10:04:18 -0800

    Request focus and hide keyboard when clicking dropdown field
    BUG=690958
    
    Review-Url: https://codereview.chromium.org/2692503002
    Cr-Commit-Position: refs/heads/master@{#449656}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/ui/EditorDropdownField.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ui/EditorDropdownField.java
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.payments.ui;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.accessibility.AccessibilityEvent;
@@ -101,6 +103,14 @@ class EditorDropdownField implements EditorFieldView {
             @Override
             public void onNothingSelected(AdapterView<?> parent) {}
         });
+        mDropdown.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) requestFocusAndHideKeyboard();
+                return false;
+            }
+        });
     }
 
     /** @return The View containing everything. */
@@ -139,6 +149,10 @@ class EditorDropdownField implements EditorFieldView {
     @Override
     public void scrollToAndFocus() {
         updateDisplayedError(!isValid());
+        requestFocusAndHideKeyboard();
+    }
+
+    private void requestFocusAndHideKeyboard() {
         UiUtils.hideKeyboard(mDropdown);
         ViewGroup parent = (ViewGroup) mDropdown.getParent();
         if (parent != null) parent.requestChildFocus(mDropdown, mDropdown);
