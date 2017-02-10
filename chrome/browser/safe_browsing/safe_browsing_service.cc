e60634e01810729b4e47d96b2a53bc505032321f
hiroshige <hiroshige@chromium.org>
2017-02-10 12:59:16 -0800

    Revert of Browser tests for using the new SafeBrowsing protocol (v4) (patchset #8 id:220001 of https://codereview.chromium.org/2675063002/ )
    Reason for revert:
    Suspected to cause sizes test failing due to static
    initializers added to v4_database.cc.
    
    BUG=691069
    
    Original issue's description:
    > Adding browser tests for PVer4.
    >
    > Just adding some basic tests for now.
    > More coming soon (http://crrev.com/2684663002)
    >
    > BUG=682495
    >
    > Review-Url: https://codereview.chromium.org/2675063002
    > Cr-Commit-Position: refs/heads/master@{#449703}
    > Committed: https://chromium.googlesource.com/chromium/src/+/d9bfc537eac0e031058b6df63b4290a645318cf8
    
    TBR=asanka@chromium.org,csharrison@chromium.org,nparker@chromium.org,shess@chromium.org,vakh@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=682495
    
    Review-Url: https://codereview.chromium.org/2687023007
    Cr-Commit-Position: refs/heads/master@{#449731}

--- chrome/browser/safe_browsing/safe_browsing_service.cc
+++ chrome/browser/safe_browsing/safe_browsing_service.cc
@@ -258,7 +258,7 @@ SafeBrowsingServiceFactory* SafeBrowsingService::factory_ = NULL;
 class SafeBrowsingServiceFactoryImpl : public SafeBrowsingServiceFactory {
  public:
   SafeBrowsingService* CreateSafeBrowsingService() override {
-    return new SafeBrowsingService(V4FeatureList::GetV4UsageStatus());
+    return new SafeBrowsingService();
   }
 
  private:
@@ -293,16 +293,12 @@ SafeBrowsingService* SafeBrowsingService::CreateSafeBrowsingService() {
   return factory_->CreateSafeBrowsingService();
 }
 
-SafeBrowsingService::SafeBrowsingService(
-    V4FeatureList::V4UsageStatus v4_usage_status)
+SafeBrowsingService::SafeBrowsingService()
     : services_delegate_(ServicesDelegate::Create(this)),
       estimated_extended_reporting_by_prefs_(SBER_LEVEL_OFF),
       enabled_(false),
       enabled_by_prefs_(false),
-      use_v4_only_(v4_usage_status == V4FeatureList::V4UsageStatus::V4_ONLY),
-      v4_enabled_(v4_usage_status ==
-                      V4FeatureList::V4UsageStatus::V4_INSTANTIATED ||
-                  v4_usage_status == V4FeatureList::V4UsageStatus::V4_ONLY) {}
+      enabled_v4_only_(safe_browsing::V4FeatureList::IsV4OnlyEnabled()) {}
 
 SafeBrowsingService::~SafeBrowsingService() {
   // We should have already been shut down. If we're still enabled, then the
@@ -320,7 +316,7 @@ void SafeBrowsingService::Initialize() {
 
   ui_manager_ = CreateUIManager();
 
-  if (!use_v4_only_) {
+  if (!enabled_v4_only_) {
     database_manager_ = CreateDatabaseManager();
   }
 
@@ -329,7 +325,7 @@ void SafeBrowsingService::Initialize() {
     navigation_observer_manager_ = new SafeBrowsingNavigationObserverManager();
   }
 
-  services_delegate_->Initialize(v4_enabled_);
+  services_delegate_->Initialize();
   services_delegate_->InitializeCsdService(url_request_context_getter_.get());
 
   // Track the safe browsing preference of existing profiles.
@@ -420,7 +416,7 @@ SafeBrowsingService::ui_manager() const {
 
 const scoped_refptr<SafeBrowsingDatabaseManager>&
 SafeBrowsingService::database_manager() const {
-  return use_v4_only_ ? v4_local_database_manager() : database_manager_;
+  return enabled_v4_only_ ? v4_local_database_manager() : database_manager_;
 }
 
 scoped_refptr<SafeBrowsingNavigationObserverManager>
@@ -431,7 +427,7 @@ SafeBrowsingService::navigation_observer_manager() {
 SafeBrowsingProtocolManager* SafeBrowsingService::protocol_manager() const {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
 #if defined(SAFE_BROWSING_DB_LOCAL)
-  DCHECK(!use_v4_only_);
+  DCHECK(!enabled_v4_only_);
   return protocol_manager_.get();
 #else
   return nullptr;
@@ -561,7 +557,7 @@ std::string SafeBrowsingService::GetProtocolConfigClientName() const {
 SafeBrowsingProtocolManagerDelegate*
 SafeBrowsingService::GetProtocolManagerDelegate() {
 #if defined(SAFE_BROWSING_DB_LOCAL)
-  DCHECK(!use_v4_only_);
+  DCHECK(!enabled_v4_only_);
   return static_cast<LocalSafeBrowsingDatabaseManager*>(
       database_manager_.get());
 #else
@@ -583,14 +579,14 @@ void SafeBrowsingService::StartOnIOThread(
   services_delegate_->StartOnIOThread(url_request_context_getter, v4_config);
 
 #if defined(SAFE_BROWSING_DB_LOCAL) || defined(SAFE_BROWSING_DB_REMOTE)
-  if (!use_v4_only_) {
+  if (!enabled_v4_only_) {
     DCHECK(database_manager_.get());
     database_manager_->StartOnIOThread(url_request_context_getter, v4_config);
   }
 #endif
 
 #if defined(SAFE_BROWSING_DB_LOCAL)
-  if (!use_v4_only_) {
+  if (!enabled_v4_only_) {
     SafeBrowsingProtocolManagerDelegate* protocol_manager_delegate =
         GetProtocolManagerDelegate();
     if (protocol_manager_delegate) {
@@ -610,7 +606,7 @@ void SafeBrowsingService::StopOnIOThread(bool shutdown) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
 
 #if defined(SAFE_BROWSING_DB_LOCAL) || defined(SAFE_BROWSING_DB_REMOTE)
-  if (!use_v4_only_) {
+  if (!enabled_v4_only_) {
     database_manager_->StopOnIOThread(shutdown);
   }
 #endif
