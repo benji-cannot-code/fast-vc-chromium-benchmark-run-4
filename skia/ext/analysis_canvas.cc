0b335008aeea93ae45e2eb3d671323414c020159
reed <reed@google.com>
2017-02-10 10:20:24 -0800

    include what you use -- SkCanvas is removing SkRegion.h
    see https://skia-review.googlesource.com/c/8231/
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2691443003
    Cr-Commit-Position: refs/heads/master@{#449664}

--- skia/ext/analysis_canvas.cc
+++ skia/ext/analysis_canvas.cc
@@ -3,11 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "skia/ext/analysis_canvas.h"
 #include "base/logging.h"
 #include "base/trace_event/trace_event.h"
-#include "skia/ext/analysis_canvas.h"
 #include "third_party/skia/include/core/SkPath.h"
 #include "third_party/skia/include/core/SkRRect.h"
+#include "third_party/skia/include/core/SkRegion.h"
 #include "third_party/skia/include/core/SkShader.h"
 
 namespace {
