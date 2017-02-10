108a2919984a35ce81466a223b588c5946d12e64
xunjieli <xunjieli@chromium.org>
2017-02-10 07:42:51 -0800

    Add shim for EstimateMemoryUsage in Quic
    This mirrors bnc@'s Spdy change at crrev.com/2686733004.
    
    BUG=669108
    
    Review-Url: https://codereview.chromium.org/2687333002
    Cr-Commit-Position: refs/heads/master@{#449625}

---
+++ net/quic/platform/api/quic_estimate_memory_usage.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_QUIC_PLATFORM_API_QUIC_ESTIMATE_MEMORY_USAGE_H_
+#define NET_QUIC_PLATFORM_API_QUIC_ESTIMATE_MEMORY_USAGE_H_
+
+#include <cstddef>
+
+#include "net/quic/platform/impl/quic_estimate_memory_usage_impl.h"
+
+namespace net {
+
+template <class T>
+size_t QuicEstimateMemoryUsage(const T& object) {
+  return QuicEstimateMemoryUsageImpl(object);
+}
+
+}  // namespace net
+
+#endif  // NET_QUIC_PLATFORM_API_QUIC_ESTIMATE_MEMORY_USAGE_H_
