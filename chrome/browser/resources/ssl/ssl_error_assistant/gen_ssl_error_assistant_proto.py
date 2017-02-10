290db3cdaaf8870107f5042db8eddaa394c0b7a7
meacer <meacer@chromium.org>
2017-02-09 18:35:38 -0800

    Add validation of sha256 hash formats to ssl_error_assistant proto generator
    BUG=640835
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2686173004
    Cr-Commit-Position: refs/heads/master@{#449528}

--- chrome/browser/resources/ssl/ssl_error_assistant/gen_ssl_error_assistant_proto.py
+++ chrome/browser/resources/ssl/ssl_error_assistant/gen_ssl_error_assistant_proto.py
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  Convert the ASCII ssl_error_assistant.asciipb proto into a binary resource.
 """
 
+import base64
 import os
 import sys
 
@@ -30,6 +31,10 @@ class SSLErrorAssistantProtoGenerator(BinaryProtoGenerator):
   def ValidatePb(self, opts, pb):
     assert pb.version_id > 0
     assert len(pb.captive_portal_cert) > 0
+    for cert in pb.captive_portal_cert:
+      assert(cert.sha256_hash.startswith("sha256/"))
+      decoded_hash = base64.b64decode(cert.sha256_hash[len("sha256/"):])
+      assert(len(decoded_hash) == 32)
 
   def ProcessPb(self, opts, pb):
     binary_pb_str = pb.SerializeToString()
