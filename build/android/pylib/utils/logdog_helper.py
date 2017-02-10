46d8e0dd4d18a12d44a2ed0209861db87dc12481
mikecase <mikecase@chromium.org>
2017-02-10 13:39:23 -0800

    Adding debugging logs to logdog_helper.
    Currently, some things we expect to be uploaded to logdog are
    not getting there. Adding logs to help debug.
    
    Review-Url: https://codereview.chromium.org/2685053005
    Cr-Commit-Position: refs/heads/master@{#449742}

--- build/android/pylib/utils/logdog_helper.py
+++ build/android/pylib/utils/logdog_helper.py
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 """Helper functions to upload data to logdog."""
 
+import logging
 import os
 import sys
 
@@ -27,6 +28,7 @@ def text(name, data):
   Returns:
     Link to view uploaded text in logdog viewer.
   """
+  logging.debug('Writing text to logdog stream, %s', name)
   with get_logdog_client().text(name) as stream:
     stream.write(data)
     return stream.get_viewer_url()
@@ -42,6 +44,7 @@ def open_text(name):
   Returns:
     A file like object. close() file when done.
   """
+  logging.debug('Opening text logdog stream, %s', name)
   return get_logdog_client().open_text(name)
 
 
@@ -56,6 +59,7 @@ def binary(name, binary_path):
   Returns:
     Link to view uploaded binary in logdog viewer.
   """
+  logging.debug('Writing binary to logdog stream, %s', name)
   with get_logdog_client().binary(name) as stream:
     with open(binary_path, 'r') as f:
       stream.write(f.read())
@@ -77,5 +81,6 @@ def get_viewer_url(name):
 
 @decorators.Memoize
 def get_logdog_client():
+  logging.debug('Getting logdog client.')
   return bootstrap.ButlerBootstrap.probe().stream_client()
 
