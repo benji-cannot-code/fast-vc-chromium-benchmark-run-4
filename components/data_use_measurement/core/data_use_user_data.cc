3d30c4e74a58095ad20bc739901ab1e716c2c124
jkrcal <jkrcal@chromium.org>
2017-02-09 23:53:50 -0800

    [Remote suggestions] Measure separate data use for JSONs and thumbnails
    Before this CL, these two cases are measured in one DataUseUserData
    enum element. This CL adds another enum element and renames the
    previous one for better clarity.
    
    It is important to split the measurements in order to get clearer insights because there are of very different nature:
     - The JSONs are background data that is used for every user (to some extent) independently of usage of Chrome.
     - On the other hand, the thumbnails are loaded on demand only when the feature is used.
    
    BUG=688945
    
    Review-Url: https://codereview.chromium.org/2685523002
    Cr-Commit-Position: refs/heads/master@{#449563}

--- components/data_use_measurement/core/data_use_user_data.cc
+++ components/data_use_measurement/core/data_use_user_data.cc
@@ -79,8 +79,8 @@ std::string DataUseUserData::GetServiceNameAsString(ServiceName service_name) {
       return "Policy";
     case SPELL_CHECKER:
       return "SpellChecker";
-    case NTP_SNIPPETS:
-      return "NTPSnippets";
+    case NTP_SNIPPETS_OBSOLETE:
+      return "NTPSnippetsObsolete";
     case SAFE_BROWSING:
       return "SafeBrowsing";
     case DATA_REDUCTION_PROXY:
@@ -119,6 +119,10 @@ std::string DataUseUserData::GetServiceNameAsString(ServiceName service_name) {
       return "WebResourceService";
     case SIGNIN:
       return "Signin";
+    case NTP_SNIPPETS_SUGGESTIONS:
+      return "NTPSnippetsSuggestions";
+    case NTP_SNIPPETS_THUMBNAILS:
+      return "NTPSnippetsThumbnails";
   }
   return "INVALID";
 }
