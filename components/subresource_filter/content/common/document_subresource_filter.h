c8eb6c2ac4b7d5cf4bcb5904965be136a48e6b66
pkalinnikov <pkalinnikov@chromium.org>
2017-02-10 07:29:40 -0800

    Add DocumentSubresourceFilter::GetLoadPolicyForSubdocument.
    The method is needed on the browser side to perform filtering of subframe
    document loads with browser-side navigation enabled.
    
    This CL also slightly changes the meaning of
    "SubresourceFilter.SubresourceLoad.Evaluation.*" histograms. Now they are
    recorded only when a request is actually evaluated against the ruleset.
    
    BUG=637415
    
    Review-Url: https://codereview.chromium.org/2678413002
    Cr-Commit-Position: refs/heads/master@{#449623}

--- components/subresource_filter/content/common/document_subresource_filter.h
+++ components/subresource_filter/content/common/document_subresource_filter.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/subresource_filter/core/common/activation_level.h"
 #include "components/subresource_filter/core/common/activation_state.h"
 #include "components/subresource_filter/core/common/indexed_ruleset.h"
+#include "components/subresource_filter/core/common/proto/rules.pb.h"
 #include "third_party/WebKit/public/platform/WebDocumentSubresourceFilter.h"
 #include "url/gurl.h"
 #include "url/origin.h"
@@ -78,7 +79,12 @@ class DocumentSubresourceFilter
                            blink::WebURLRequest::RequestContext) override;
   void reportDisallowedLoad() override;
 
+  LoadPolicy GetLoadPolicyForSubdocument(const GURL& subdocument_url);
+
  private:
+  LoadPolicy EvaluateLoadPolicy(const GURL& resource_url,
+                                proto::ElementType element_type);
+
   const ActivationState activation_state_;
   const scoped_refptr<const MemoryMappedRuleset> ruleset_;
   const IndexedRulesetMatcher ruleset_matcher_;
