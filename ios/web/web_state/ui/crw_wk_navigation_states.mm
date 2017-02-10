3da3ab6f27ba94074c086ece0d18c634bcb8ee02
stkhapugin <stkhapugin@chromium.org>
2017-02-10 04:10:09 -0800

    Revert of Fixed title updating for back forward navigation. (patchset #3 id:40001 of https://codereview.chromium.org/2685803002/ )
    Reason for revert:
    Breaks -[TabHistoryPopupControllerTestCase testTabHistoryMenu] tab_history_popup_controller_egtest.mm downstream, because New Tab in history popup (when long pressed on back) displays "chrome://newtab" instead of "New Tab".
    
    I actually don't know why this test is not ran upstream, but I have to revert, sorry!
    
    Original issue's description:
    > Fixed title updating for back forward navigation.
    >
    > Chrome relies on KVO compliant "title" property to subscribe for
    > title updates. If this KVO change happens during then navigation
    > it is unclear if title was changes for the previous page, or the
    > navigation was committed and title was changed for the new page.
    >
    > So if there is a navigation in progress WebController should ignore
    > KVO change, but it should always attempt to update title when
    > navigation is committed.
    >
    > BUG=688047, 677356
    >
    > Review-Url: https://codereview.chromium.org/2685803002
    > Cr-Commit-Position: refs/heads/master@{#449527}
    > Committed: https://chromium.googlesource.com/chromium/src/+/487e4cfd53bac5944dad49cbce3b939a0c7bf035
    
    TBR=kkhorimoto@chromium.org,eugenebut@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=688047, 677356
    
    Review-Url: https://codereview.chromium.org/2684343003
    Cr-Commit-Position: refs/heads/master@{#449595}

--- ios/web/web_state/ui/crw_wk_navigation_states.mm
+++ ios/web/web_state/ui/crw_wk_navigation_states.mm
@@ -106,13 +106,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return result;
 }
 
-- (web::WKNavigationState)lastAddedNavigationState {
-  CRWWKNavigationsStateRecord* lastAddedRecord = nil;
-  WKNavigation* lastAddedNavigation = [self lastAddedNavigation];
-  if (lastAddedNavigation)
-    lastAddedRecord = [_records objectForKey:lastAddedNavigation];
-
-  return lastAddedRecord.state;
-}
-
 @end
