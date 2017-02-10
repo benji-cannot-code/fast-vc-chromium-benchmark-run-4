63641c87624c8d16180b42c5e655940276dc4440
dpapad <dpapad@chromium.org>
2017-02-10 14:48:54 -0800

    MD Settings: Fix remaining tests that fail in Vulcanized mode.
    BUG=673825
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2668623002
    Cr-Commit-Position: refs/heads/master@{#449771}

--- chrome/test/data/webui/settings/settings_passwords_section_browsertest.js
+++ chrome/test/data/webui/settings/settings_passwords_section_browsertest.js
@@ -35,6 +35,7 @@ SettingsPasswordSectionBrowserTest.prototype = {
   /** @override */
   setUp: function() {
     PolymerTest.prototype.setUp.call(this);
+    PolymerTest.clearBody();
 
     // Test is run on an individual element that won't have a page language.
     this.accessibilityAuditConfig.auditRulesToIgnore.push('humanLangMissing');
@@ -171,8 +172,6 @@ TEST_F('SettingsPasswordSectionBrowserTest', 'uiTests', function() {
 
   suite('PasswordsSection', function() {
     test('verifyNoSavedPasswords', function() {
-      assertEquals(self.browsePreload, document.location.href);
-
       var passwordsSection = self.createPasswordsSection_([], []);
 
       self.validatePasswordList(passwordsSection.$.passwordList, []);
@@ -182,8 +181,6 @@ TEST_F('SettingsPasswordSectionBrowserTest', 'uiTests', function() {
     });
 
     test('verifySavedPasswordLength', function() {
-      assertEquals(self.browsePreload, document.location.href);
-
       var passwordList = [
         FakeDataMaker.passwordEntry('site1.com', 'luigi', 1),
         FakeDataMaker.passwordEntry('longwebsite.com', 'peach', 7),
