6dfc9dfd57179e7485bf272edf1bd4657190bdd8
ecobos <ecobos@igalia.com>
2017-02-10 06:39:39 -0800

    Cleanup SVGElement::layoutObjectIsNeeded.
    This spawned off https://codereview.chromium.org/2684833003/, since it's now an
    unrelated cleanup.
    
    Review-Url: https://codereview.chromium.org/2685113002
    Cr-Commit-Position: refs/heads/master@{#449618}

--- third_party/WebKit/Source/core/svg/SVGGElement.cpp
+++ third_party/WebKit/Source/core/svg/SVGGElement.cpp
@@ -50,7 +50,7 @@ bool SVGGElement::layoutObjectIsNeeded(const ComputedStyle&) {
   // even if display is set to 'none' - which is special to SVG <g> container
   // elements.
   return parentOrShadowHostElement() &&
-         parentOrShadowHostElement()->isSVGElement();
+         parentOrShadowHostElement()->isSVGElement() && isValid();
 }
 
 }  // namespace blink
