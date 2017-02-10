c5e2ae557ffdd54091a93734edf4e5e03281bbf1
yosin <yosin@chromium.org>
2017-02-09 18:57:25 -0800

    Temporary disable DCHECK for transform ancestor checking in CompositedLayerMapping
    This patch disables |DCHECK()| fro transform ancestor checking in
    |CompositedLayerMapping| for landing [1] and [2].
    
    As of offline discussion, this DCHECK itself can be safely ignored since there
    is no stability or security implication, and doesn't result in rendering glitch
    in this particular case. However, it is hard to make minimal reproduce case;
    this DCHECK hits on |LoginTest.GaiaAuthOffline| which is run on ChromeOS only.
    
    [1] http://crrev.com/2680943004 Make FrameSelection to hold non-canonicalized positions
    [2] http://crrev.com/2340463002 Set default focus on offline signin page
    
    BUG=646437
    TEST=n/a; no behavior changes
    
    Review-Url: https://codereview.chromium.org/2687043002
    Cr-Commit-Position: refs/heads/master@{#449539}

--- third_party/WebKit/Source/core/layout/compositing/CompositedLayerMapping.cpp
+++ third_party/WebKit/Source/core/layout/compositing/CompositedLayerMapping.cpp
@@ -847,7 +847,10 @@ void CompositedLayerMapping::updateSquashingLayerGeometry(
     compositingContainerOffsetFromParentGraphicsLayer +=
         compositingContainer->subpixelAccumulation();
 
-#if DCHECK_IS_ON()
+#if 0 && DCHECK_IS_ON()
+  // TODO(trchen): We should enable this for below comment out |DCHECK()| once
+  // we have simple reproduce case and fix it. See http://crbug.com/646437 for
+  // details.
   const PaintLayer* commonTransformAncestor = nullptr;
   if (compositingContainer && compositingContainer->transform())
     commonTransformAncestor = compositingContainer;
@@ -868,7 +871,9 @@ void CompositedLayerMapping::updateSquashingLayerGeometry(
     // Store the local bounds of the Layer subtree before applying the offset.
     layers[i].compositedBounds = squashedBounds;
 
-#if DCHECK_IS_ON()
+#if 0 && DCHECK_IS_ON()
+    // TODO(trchen): We should enable this |DCHECK()| once we have simple
+    // reproduce case and fix it. See http://crbug.com/646437 for details.
     DCHECK(layers[i].paintLayer->transformAncestor() ==
            commonTransformAncestor);
 #endif
