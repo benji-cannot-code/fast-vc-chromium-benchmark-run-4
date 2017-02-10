8892d8bfc162be3f16827c623830d795e9b8bad5
ekaramad <ekaramad@chromium.org>
2017-02-09 18:23:26 -0800

    Fix a recent regression in IME inside OOPIFs
    IME events are only processed at the renderer if there is widget/page
    focus. This used to be done by checking the variabel m_imeAcceptEvents
    inside WebFrameWidgetImpl which technically tracks the page focus.
    
    After a recent refactoring, the logic for verifying page focus was moved
    to RenderWidget which saves the unnecessary call to
    WebInputMethodController IME methods when page is not focused. But
    unfortunately this broke IME because page focus
    (RenderWidget::has_focus_) does not get updated for RenderWidgets
    corresponding to OOPIFs. The only reason it was working before was that
    m_imeAcceptEvents is initialized to |true| and stay because we never
    update page focus for WebFrameWidgetImpl.
    
    This CL will temporarily allow handling all IME events by checking if
    the RenderWidget is for an OOPIF. This will revert the behvaior to that
    of prior to the refactoring.
    
    This CL also adds an interactive browser test which catches regressions
    of this sort on Mac and Aura platforms.
    
    BUG=688842, 602723
    
    Review-Url: https://codereview.chromium.org/2681473002
    Cr-Commit-Position: refs/heads/master@{#449521}

--- content/renderer/render_widget.cc
+++ content/renderer/render_widget.cc
@@ -671,7 +671,13 @@ void RenderWidget::SendOrCrash(IPC::Message* message) {
 }
 
 bool RenderWidget::ShouldHandleImeEvents() const {
-  return GetWebWidget() && GetWebWidget()->isWebFrameWidget() && has_focus_;
+  // TODO(ekaramad): We track page focus in all RenderViews on the page but the
+  // RenderWidgets corresponding to OOPIFs do not get the update. For now, this
+  // method returns true when the RenderWidget is for an OOPIF, i.e., IME events
+  // will be processed regardless of page focus. We should revisit this after
+  // page focus for OOPIFs has been fully resolved (https://crbug.com/689777).
+  return GetWebWidget() && GetWebWidget()->isWebFrameWidget() &&
+         (has_focus_ || for_oopif_);
 }
 
 void RenderWidget::SetWindowRectSynchronously(
@@ -1606,7 +1612,7 @@ void RenderWidget::OnImeCommitText(
 #endif
   ImeEventGuard guard(this);
   input_handler_->set_handling_input_event(true);
-  if (auto* controller = GetInputMethodController())
+  if (auto* controller = GetInputMethodController()) {
     controller->commitText(
         WebString::fromUTF16(text),
         WebVector<WebCompositionUnderline>(underlines),
@@ -1614,6 +1620,7 @@ void RenderWidget::OnImeCommitText(
             ? WebRange(replacement_range.start(), replacement_range.length())
             : WebRange(),
         relative_cursor_pos);
+  }
   input_handler_->set_handling_input_event(false);
   UpdateCompositionInfo(false /* not an immediate request */);
 }
