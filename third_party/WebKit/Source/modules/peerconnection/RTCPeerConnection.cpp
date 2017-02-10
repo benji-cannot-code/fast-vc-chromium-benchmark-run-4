960c4cf6f9023080cc8484700cd642647ddb042a
altimin <altimin@chromium.org>
2017-02-10 09:04:35 -0800

    [scheduler] Plumb information about active webrtc connections to scheduler
    Notify scheduler about webrtc connections.
    
    BUG=690929
    
    Review-Url: https://codereview.chromium.org/2683993006
    Cr-Commit-Position: refs/heads/master@{#449640}

--- third_party/WebKit/Source/modules/peerconnection/RTCPeerConnection.cpp
+++ third_party/WebKit/Source/modules/peerconnection/RTCPeerConnection.cpp
@@ -522,6 +522,9 @@ RTCPeerConnection::RTCPeerConnection(ExecutionContext* context,
         NotSupportedError, "Failed to initialize native PeerConnection.");
     return;
   }
+
+  m_connectionHandleForScheduler =
+      document->frame()->frameScheduler()->onActiveConnectionCreated();
 }
 
 RTCPeerConnection::~RTCPeerConnection() {
@@ -1353,6 +1356,8 @@ void RTCPeerConnection::releasePeerConnectionHandler() {
   m_dispatchScheduledEventRunner->stop();
 
   m_peerHandler.reset();
+
+  m_connectionHandleForScheduler.reset();
 }
 
 void RTCPeerConnection::closePeerConnection() {
@@ -1427,6 +1432,8 @@ void RTCPeerConnection::closeInternal() {
   Document* document = toDocument(getExecutionContext());
   HostsUsingFeatures::countAnyWorld(
       *document, HostsUsingFeatures::Feature::RTCPeerConnectionUsed);
+
+  m_connectionHandleForScheduler.reset();
 }
 
 void RTCPeerConnection::scheduleDispatchEvent(Event* event) {
