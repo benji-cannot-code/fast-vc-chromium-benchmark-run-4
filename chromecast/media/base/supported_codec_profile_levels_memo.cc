4f9f06f8ccac5eff7f9bed0d7bd8d26a3a440c4d
sanfin <sanfin@chromium.org>
2017-02-09 18:33:12 -0800

    [Chromecast] Explicitly initialize members of SupportedCodecProfileLevelsMemo.
    When compiling on gcc in release mode, the differing constructor
    behaviors for ThreadChecker cause the build to fail.
    
    BUG=None
    TEST=build Chromecast with gcc
    
    Review-Url: https://codereview.chromium.org/2686143003
    Cr-Commit-Position: refs/heads/master@{#449526}

--- chromecast/media/base/supported_codec_profile_levels_memo.cc
+++ chromecast/media/base/supported_codec_profile_levels_memo.cc
@@ -11,7 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromecast {
 namespace media {
 
-SupportedCodecProfileLevelsMemo::SupportedCodecProfileLevelsMemo() {}
+SupportedCodecProfileLevelsMemo::SupportedCodecProfileLevelsMemo()
+    : codec_profile_levels_(), thread_checker_() {}
 
 SupportedCodecProfileLevelsMemo::~SupportedCodecProfileLevelsMemo() {}
 
