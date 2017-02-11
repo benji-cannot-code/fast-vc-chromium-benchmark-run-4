e84b3fff2f9db9e226d1122d229dfa1c0857d825
hiroshige <hiroshige@chromium.org>
2017-02-10 16:03:38 -0800

    Temporarily introduce ImageResourceInfo::setIsPlaceholder()
    This is for https://codereview.chromium.org/2543073002, and
    setIsPlaceholder() will be removed later in the refactoring.
    
    BUG=657995, 667641, 677188
    
    Review-Url: https://codereview.chromium.org/2638383008
    Cr-Commit-Position: refs/heads/master@{#449795}

--- third_party/WebKit/Source/core/loader/resource/ImageResourceContent.cpp
+++ third_party/WebKit/Source/core/loader/resource/ImageResourceContent.cpp
@@ -55,6 +55,7 @@ class NullImageResourceInfo final
 
   void decodeError(bool allDataReceived) override {}
   void setDecodedSize(size_t) override {}
+  void setIsPlaceholder(bool) override {}
   void willAddClientOrObserver() override {}
   void didRemoveClientOrObserver() override {}
   void emulateLoadStartedForInspector(
