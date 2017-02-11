a1929673028c6936f200386005b870c86ffcec27
mfoltz <mfoltz@chromium.org>
2017-02-10 17:00:29 -0800

    Remove obsolete Cast extension IDs from various whitelists and files.
    List:
    * Cast Canary hfaagokkkhdbgiakmmlclaapfelnkoah B11A93E7E5B541F8010245EBDE2C74647D6C14B9
    * Cast Staging fmfcbgogabcbclcofgocippekhfcmgfj FA01E0B81978950F2BC5A50512FD769725F57510
    * Cast Beta dliochdbjfkdbacpmhlcpmleaejidimm F155646B5D1CA545F7E1E4E20D573DFDD44C2540
    * Cast Stable boadgeojelhgndaghljhdicfkmllpafd 16CA7A47AAE4BE49B1E75A6B960C3875E945B264
    * Old MRP Stable fjhoaacokmgbjemoflkofnenfaiekifl EF2AB692559EA97C3BBDEA018A8C45F92457BD4E
    * MRP Beta ekpaaapppgpmolpcldedioblbkmijaca C17CD9E6868D7B9C67926E0EC612EA25C768418F
    * MRP Staging lhkfccafpkdlaodkicmokbmfapjadkij A45DABDB47A31CC812E5490AB748C7D05E2D32E9
    * MRP Canary ibiljbkambkbohapfhoonkcpcikdglop BFEE2E3B80BE21A645E63E9346DFC383E7CB3BDA
    
    BUG=636154
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2678423004
    Cr-Commit-Position: refs/heads/master@{#449811}

--- ui/file_manager/video_player/js/cast/cast_extension_discoverer.js
+++ ui/file_manager/video_player/js/cast/cast_extension_discoverer.js
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// TODO(crbug.com/689773): Update to use <script>
+
 /**
  * Discover the ID of installed cast extension.
  * @constructor
@@ -11,17 +13,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 function CastExtensionDiscoverer() {}
 
 /**
- * Tentatice IDs to try.
+ * Tentative IDs to try.
  * @type {!Array<string>}
  * @const
  */
 CastExtensionDiscoverer.CAST_EXTENSION_IDS = [
-  'pkedcjkdefgpdelpbcmbmeomcjbeemfm', // MR official
-  'boadgeojelhgndaghljhdicfkmllpafd', // release
-  'dliochdbjfkdbacpmhlcpmleaejidimm', // beta
-  'enhhojjnijigcajfphajepfemndkmdlo', // dev
-  'fmfcbgogabcbclcofgocippekhfcmgfj', // staging
-  'fjhoaacokmgbjemoflkofnenfaiekifl'  // stable used during MR development.
+  'enhhojjnijigcajfphajepfemndkmdlo',  // Media Router Dev
+  'pkedcjkdefgpdelpbcmbmeomcjbeemfm'   // Media Router Stable
 ];
 
 /**
