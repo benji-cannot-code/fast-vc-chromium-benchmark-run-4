b7ed58d4b527dbaee74fb9980ca2b39f66b821a5
hiroshige <hiroshige@chromium.org>
2017-02-11 19:44:05 -0800

    Remove unintendedly resurrected MockImageResourceClient.h
    This CL removes MockImageResourceClient.h that
    https://codereview.chromium.org/2584423002 seems to resurrect
    unintendedly.
    
    BUG=655920
    
    Review-Url: https://codereview.chromium.org/2680163010
    Cr-Commit-Position: refs/heads/master@{#449884}

--- third_party/WebKit/Source/core/loader/resource/ImageResourceTest.cpp
+++ third_party/WebKit/Source/core/loader/resource/ImageResourceTest.cpp
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/loader/resource/ImageResource.h"
 
 #include <memory>
-#include "core/loader/resource/MockImageResourceClient.h"
 #include "core/loader/resource/MockImageResourceObserver.h"
 #include "platform/SharedBuffer.h"
 #include "platform/exported/WrappedResourceResponse.h"
