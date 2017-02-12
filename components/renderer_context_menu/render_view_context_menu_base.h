8bddc7f0bdf0629c2ca25eded52945958db67273
ke.he <ke.he@intel.com>
2017-02-11 17:17:01 -0800

    Remove ScopedVector in //components/renderer_context_menu/
    BUG=554289
    
    Review-Url: https://codereview.chromium.org/2682333005
    Cr-Commit-Position: refs/heads/master@{#449879}

--- components/renderer_context_menu/render_view_context_menu_base.h
+++ components/renderer_context_menu/render_view_context_menu_base.h
@@ -12,9 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 #include <utility>
+#include <vector>
 
 #include "base/macros.h"
-#include "base/memory/scoped_vector.h"
 #include "base/observer_list.h"
 #include "base/strings/string16.h"
 #include "components/renderer_context_menu/context_menu_content_type.h"
@@ -195,7 +195,7 @@ class RenderViewContextMenuBase : public ui::SimpleMenuModel::Delegate,
 
   std::unique_ptr<ToolkitDelegate> toolkit_delegate_;
 
-  ScopedVector<ui::SimpleMenuModel> custom_submenus_;
+  std::vector<std::unique_ptr<ui::SimpleMenuModel>> custom_submenus_;
 
   DISALLOW_COPY_AND_ASSIGN(RenderViewContextMenuBase);
 };
