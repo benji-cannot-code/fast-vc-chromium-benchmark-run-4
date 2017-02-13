27fe870bc88e7099f0cb1e5017c6ac5f1a1d23c1
olivierrobin <olivierrobin@chromium.org>
2017-02-13 02:25:53 -0800

    Add CRWSessionEntry isEqual
    EGTest have DCHECKs on CRWSessionEntry equality (e.g.
    TabHistoryPopupControllerTestCase testTabHistoryMenu).
    This fails because entry can be different even if they have the same
    navigation item.
    
    This is a tentative fix by the sheriff to make the tree green.
    Please review this CL and revert it later if there is a better solution.
    
    BUG=691469
    
    Review-Url: https://codereview.chromium.org/2690973002
    Cr-Commit-Position: refs/heads/master@{#449923}

--- ios/web/navigation/crw_session_entry.mm
+++ ios/web/navigation/crw_session_entry.mm
@@ -60,4 +60,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return _item;
 }
 
+- (BOOL)isEqual:(CRWSessionEntry*)object {
+  return _item == [object navigationItem];
+}
+
 @end
