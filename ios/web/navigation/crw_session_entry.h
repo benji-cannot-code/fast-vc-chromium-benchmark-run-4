ed177e7816617c41fbd1c886590fb904b0868685
kkhorimoto <kkhorimoto@chromium.org>
2017-02-13 15:52:24 -0800

    Revert "Updated ownership of NavigationItems within CRWSessionController."
    The original CL updated behavior in a couple ways that were introducing many bugs:
    - New CRWSessionEntries were vended every time a session entry property was
      accessed, which which broke logic comparing CRWSessionEntries.
    - CRWSessionEntries held an unowned pointer to their corresponding
      NavigationItems, making it possible for the CRWSessionEntry to outlive its backing
      NavigationItem.
    
    The CL will be re-landed once callers have been updated to reflect this new
    behavior.
    
    This reverts 3 CLs:
    https://codereview.chromium.org/2672953005 (original CL)
    https://codereview.chromium.org/2690973002 (hacky fix)
    https://codereview.chromium.org/2680313004 (hacky fix)
    
    BUG=689358, 691634, 545227, 691492, 691469
    
    Review-Url: https://codereview.chromium.org/2690913003
    Cr-Commit-Position: refs/heads/master@{#450156}

--- ios/web/navigation/crw_session_entry.h
+++ ios/web/navigation/crw_session_entry.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/Foundation.h>
 #include <stdint.h>
 
+#include <memory>
+
 #include "base/strings/string16.h"
 #include "base/time/time.h"
 #include "ui/base/page_transition_types.h"
@@ -26,7 +28,7 @@ class NavigationItemImpl;
 // DEPRECATED, do not use this class and do not add any methods to it.
 // Use web::NavigationItem instead.
 // TODO(crbug.com/454984): Remove this class.
-@interface CRWSessionEntry : NSObject
+@interface CRWSessionEntry : NSObject<NSCopying>
 
 // Pointer to the NavigationItem associated with this CRWSessionEntry.
 // Eventually, this will replace CRWSessionEntry entirely.
@@ -36,7 +38,8 @@ class NavigationItemImpl;
 @property(nonatomic, readonly) web::NavigationItemImpl* navigationItemImpl;
 
 // Initialize the session entry with the given NavigationItem.
-- (instancetype)initWithNavigationItem:(web::NavigationItem*)item;
+- (instancetype)initWithNavigationItem:
+    (std::unique_ptr<web::NavigationItem>)item;
 
 @end
 
