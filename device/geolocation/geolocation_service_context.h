36871bf62b8adecd0811326857a47da9d04b3a0e
ke.he <ke.he@intel.com>
2017-02-13 03:10:53 -0800

    Remove ScopedVector in //device/geolocation/
    BUG=554289
    
    Review-Url: https://codereview.chromium.org/2687123003
    Cr-Commit-Position: refs/heads/master@{#449930}

--- device/geolocation/geolocation_service_context.h
+++ device/geolocation/geolocation_service_context.h
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef DEVICE_GEOLOCATION_GEOLOCATION_SERVICE_CONTEXT_H_
 #define DEVICE_GEOLOCATION_GEOLOCATION_SERVICE_CONTEXT_H_
 
+#include <memory>
+#include <vector>
+
 #include "base/macros.h"
-#include "base/memory/scoped_vector.h"
 #include "device/geolocation/geolocation_export.h"
 #include "device/geolocation/public/interfaces/geolocation.mojom.h"
 
@@ -42,7 +44,7 @@ class DEVICE_GEOLOCATION_EXPORT GeolocationServiceContext {
   void ClearOverride();
 
  private:
-  ScopedVector<GeolocationServiceImpl> services_;
+  std::vector<std::unique_ptr<GeolocationServiceImpl>> services_;
 
   std::unique_ptr<Geoposition> geoposition_override_;
 
