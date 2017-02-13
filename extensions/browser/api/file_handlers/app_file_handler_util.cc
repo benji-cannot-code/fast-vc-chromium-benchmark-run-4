61e928ae3cb81d2516d64b4f1c51bbcfb5792f2a
michaelpg <michaelpg@chromium.org>
2017-02-13 15:17:27 -0800

    Move file_handlers API from //chrome to //extensions
    Largely mechanical CL via move_source_file.py, subbing some Profile usage
    for BrowserContext.
    
    Results in removing some DEPS exceptions and #includes from //apps.
    
    BUG=679870
    
    Review-Url: https://codereview.chromium.org/2685453002
    Cr-Original-Commit-Position: refs/heads/master@{#449460}
    Committed: https://chromium.googlesource.com/chromium/src/+/10d5e4e8430e41ca148452f2f8582db517846799
    Review-Url: https://codereview.chromium.org/2685453002
    Cr-Commit-Position: refs/heads/master@{#450144}

--- extensions/browser/api/file_handlers/app_file_handler_util.cc
+++ extensions/browser/api/file_handlers/app_file_handler_util.cc
@@ -3,13 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/extensions/api/file_handlers/app_file_handler_util.h"
+#include "extensions/browser/api/file_handlers/app_file_handler_util.h"
 
 #include "base/files/file.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "build/build_config.h"
-#include "chrome/browser/profiles/profile.h"
+#include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/child_process_security_policy.h"
 #include "extensions/browser/api/extensions_api_client.h"
@@ -103,7 +103,7 @@ class WritableFileChecker
  public:
   WritableFileChecker(
       const std::vector<base::FilePath>& paths,
-      Profile* profile,
+      content::BrowserContext* context,
       const std::set<base::FilePath>& directory_paths,
       const base::Closure& on_success,
       const base::Callback<void(const base::FilePath&)>& on_failure);
@@ -129,9 +129,9 @@ class WritableFileChecker
   void OnPrepareFileDone(const base::FilePath& path, bool success);
 
   const std::vector<base::FilePath> paths_;
-  Profile* profile_;
+  content::BrowserContext* context_;
   const std::set<base::FilePath> directory_paths_;
-  int outstanding_tasks_;
+  size_t outstanding_tasks_;
   base::FilePath error_path_;
   base::Closure on_success_;
   base::Callback<void(const base::FilePath&)> on_failure_;
@@ -139,12 +139,12 @@ class WritableFileChecker
 
 WritableFileChecker::WritableFileChecker(
     const std::vector<base::FilePath>& paths,
-    Profile* profile,
+    content::BrowserContext* context,
     const std::set<base::FilePath>& directory_paths,
     const base::Closure& on_success,
     const base::Callback<void(const base::FilePath&)>& on_failure)
     : paths_(paths),
-      profile_(profile),
+      context_(context),
       directory_paths_(directory_paths),
       outstanding_tasks_(1),
       on_success_(on_success),
@@ -157,16 +157,14 @@ void WritableFileChecker::Check() {
 #if defined(OS_CHROMEOS)
     NonNativeFileSystemDelegate* delegate =
         ExtensionsAPIClient::Get()->GetNonNativeFileSystemDelegate();
-    if (delegate && delegate->IsUnderNonNativeLocalPath(profile_, path)) {
+    if (delegate && delegate->IsUnderNonNativeLocalPath(context_, path)) {
       if (is_directory) {
         delegate->IsNonNativeLocalPathDirectory(
-            profile_,
-            path,
+            context_, path,
             base::Bind(&WritableFileChecker::OnPrepareFileDone, this, path));
       } else {
         delegate->PrepareNonNativeLocalFileForWritableApp(
-            profile_,
-            path,
+            context_, path,
             base::Bind(&WritableFileChecker::OnPrepareFileDone, this, path));
       }
       continue;
@@ -260,7 +258,7 @@ bool FileHandlerCanHandleEntry(const FileHandlerInfo& handler,
          FileHandlerCanHandleFileWithExtension(handler, entry.path);
 }
 
-GrantedFileEntry CreateFileEntry(Profile* profile,
+GrantedFileEntry CreateFileEntry(content::BrowserContext* context,
                                  const Extension* extension,
                                  int renderer_id,
                                  const base::FilePath& path,
@@ -292,12 +290,12 @@ GrantedFileEntry CreateFileEntry(Profile* profile,
 
 void PrepareFilesForWritableApp(
     const std::vector<base::FilePath>& paths,
-    Profile* profile,
+    content::BrowserContext* context,
     const std::set<base::FilePath>& directory_paths,
     const base::Closure& on_success,
     const base::Callback<void(const base::FilePath&)>& on_failure) {
   scoped_refptr<WritableFileChecker> checker(new WritableFileChecker(
-      paths, profile, directory_paths, on_success, on_failure));
+      paths, context, directory_paths, on_success, on_failure));
   checker->Check();
 }
 
