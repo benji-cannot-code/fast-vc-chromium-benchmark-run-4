f44cfb9260b1cb28eceaa6e166c8cb12771f2905
jiahuiguo <jiahuiguo@google.com>
2017-02-13 13:17:34 -0800

    Credit Card Autofill Last Used Date Experiment
    Implemented displaying the last used date in credit card autofill suggestions.
    
    BUG=671880
    TEST=TimeFormattingTest.TimeFormatWithPattern(base_unittests),
    CreditCardTest.GetLastUsedDateForDisplay(components_unittests),
    PersonalDataManagerTest.*(components_unittests)
    
    Review-Url: https://codereview.chromium.org/2607043002
    Cr-Commit-Position: refs/heads/master@{#450089}

--- components/autofill/core/browser/personal_data_manager_unittest.cc
+++ components/autofill/core/browser/personal_data_manager_unittest.cc
@@ -18,8 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/guid.h"
+#include "base/i18n/time_formatting.h"
 #include "base/memory/ptr_util.h"
-#include "base/metrics/field_trial.h"
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/synchronization/waitable_event.h"
@@ -49,8 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/browser/fake_signin_manager.h"
 #include "components/signin/core/browser/test_signin_client.h"
 #include "components/signin/core/common/signin_pref_names.h"
-#include "components/variations/entropy_provider.h"
-#include "components/variations/variations_associated_data.h"
+#include "components/variations/variations_params_manager.h"
 #include "components/webdata/common/web_data_service_base.h"
 #include "components/webdata/common/web_database_service.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -154,9 +153,6 @@ class PersonalDataManagerTest : public testing::Test {
 
     test::DisableSystemServices(prefs_.get());
     ResetPersonalDataManager(USER_MODE_NORMAL);
-
-    // There are no field trials enabled by default.
-    field_trial_list_.reset();
 
     // Reset the deduping pref to its default value.
     personal_data_->pref_service_->SetInteger(
@@ -314,28 +310,6 @@ class PersonalDataManagerTest : public testing::Test {
                                             imported_credit_card);
   }
 
-  // Sets up the profile order field trial group and parameter. Sets up the
-  // suggestions limit parameter to |limit_param|.
-  void EnableAutofillProfileLimitFieldTrial(const std::string& limit_param) {
-    DCHECK(!limit_param.empty());
-
-    // Clear the existing |field_trial_list_| and variation parameters.
-    field_trial_list_.reset(NULL);
-    field_trial_list_.reset(
-        new base::FieldTrialList(
-            base::MakeUnique<metrics::SHA1EntropyProvider>("foo")));
-    variations::testing::ClearAllVariationParams();
-
-    std::map<std::string, std::string> params;
-    params[kFrecencyFieldTrialLimitParam] = limit_param;
-    variations::AssociateVariationParams(kFrecencyFieldTrialName, "LimitToN",
-                                         params);
-
-    field_trial_ = base::FieldTrialList::CreateFieldTrial(
-        kFrecencyFieldTrialName, "LimitToN");
-    field_trial_->group();
-  }
-
   void SubmitFormAndExpectImportedCardWithData(const FormData& form,
                                                const char* exp_name,
                                                const char* exp_cc_num,
@@ -375,8 +349,7 @@ class PersonalDataManagerTest : public testing::Test {
   PersonalDataLoadedObserverMock personal_data_observer_;
   std::unique_ptr<PersonalDataManager> personal_data_;
 
-  std::unique_ptr<base::FieldTrialList> field_trial_list_;
-  scoped_refptr<base::FieldTrial> field_trial_;
+  variations::testing::VariationParamsManager variation_params_;
 };
 
 TEST_F(PersonalDataManagerTest, AddProfile) {
@@ -3457,7 +3430,8 @@ TEST_F(PersonalDataManagerTest, GetProfileSuggestions_NumberOfSuggestions) {
   EXPECT_EQ(3U, suggestions.size());
 
   // Verify that only two profiles are suggested.
-  EnableAutofillProfileLimitFieldTrial("2");
+  variation_params_.SetVariationParams(kFrecencyFieldTrialName,
+                                       {{kFrecencyFieldTrialLimitParam, "2"}});
 
   suggestions = personal_data_->GetProfileSuggestions(
       AutofillType(NAME_FIRST), base::string16(), false,
@@ -3470,7 +3444,8 @@ TEST_F(PersonalDataManagerTest, GetProfileSuggestions_NumberOfSuggestions) {
 // than three profiles.
 TEST_F(PersonalDataManagerTest,
        GetProfileSuggestions_LimitIsMoreThanProfileSuggestions) {
-  EnableAutofillProfileLimitFieldTrial("3");
+  variation_params_.SetVariationParams(kFrecencyFieldTrialName,
+                                       {{kFrecencyFieldTrialLimitParam, "3"}});
 
   // Set up 2 different profiles.
   AutofillProfile profile1(base::GenerateGUID(), "https://www.example.com");
