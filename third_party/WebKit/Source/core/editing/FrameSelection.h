db5a87f78c9da8b55b70266f861f35ea03639da1
yosin <yosin@chromium.org>
2017-02-13 02:55:13 -0800

    Make FrameSelection to hold non-canonicalized DOM positions
    This patch makes |FrameSelection| to hold non-canonicalized DOM positions in
    |SelectionEditor| to align Selection API specification[1] for improving
    interoperatbility[2].
    
    Before this patch we holds selection as |VisibleSelection| as canonicalized
    DOM positions. This behavior is not align with Selection API specification[1]
    then the most complained issue of Blink from editing-tf@w3c.
    
    The heart of this patch is holding selection as |SelectionInDOMTree| and
    compute |VisibleSelection| on-demand with cache of computed |VisibleSelection|.
    
    |VisibleSelection| cache is invalidate each DOM tree change and style change
    since canonicalization referes CSS style properties, e.g display, visibility,
    -webkit-user-modify, etc, and layout dimension.
    
    |SelectionEditor| utilizes |SynchronousMutationObserver| to relocate
    |m_selectionInDOMTree| instead of |FrameSelection|. Before this patch
    |FrameSelection| relocates |VisibleSelection| with dirty layout tree then
    sets |FrameSelection::setSelection()|. To void cyclic reference between
    |FrameSelection| and |SelectionEditor|, we could not move relocation part to
    |SelectionEditor|.
    
    This patch also updates
    |FrameSelection::updatePostionAfterAdoptingTextNodesMerged()| to handle
    |PositonAnchorType|.
    
    # Highlight of changes
    ## FrameCaret
    - Compute caret position after "layout clean" rather than each selection change
    to align rendering pipeline.
    
    ## CharacterData
    Changes timing of notifying character data update for ease of relocation of
    positions.
    
    ## FrameSelection
    - Move |m_isHandleVisible| to |SelectionInDOMTree| as follow-up of [5].
    - Move selection relocation to |SelectionEditor|; following patch will move
    implementations to "SelectionEditor.cpp"
    
    ## SelecitonEdtior
    - Make it to hold |SelectionInDOMTree| with relocation at DOM mutation.
    - Caching |VisibleSelection|
    
    # Brief description of test expectation changes:
    ## ImeTest.java:
    This patch gets rid of redundant selection change event from
     - |testImePaste|,
     - |testContentEditableEvents_DeleteSurroundingText|
     - |testInputTextEvents_DeleteSurroundingText|
    
    ## LayoutTests
    Before this patch, Blink uses |VisibleSelection| when it sets even if style and
    layout changed. This is wrong and unexpected behavior since positions in
    |VisibleSelection| can no longer be canonicalized positions. This patch changes
    this behavior to return "sane" canonicalized positions with clean style and
    layout tree.
    
    This patch is the result of many attempts. Previous changes can be found in
    [3][4].
    
    [1] https://www.w3.org/TR/selection-api/ W3C Selection API
    [2] https://goo.gl/9v1zOK Improving Interoperatbility of Selection
    [3] http://crrev.com/1958093002
    [4] http://crrev.com/2637013002
    [5] http://crrev.com/2651803007 Added isHandleVisible to |SelectionTemplate|
    
    BUG=139552, 603684, 605499, 606499, 625533, 644648, 679991
    TEST=See changes in this patch
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2680943004
    Cr-Commit-Position: refs/heads/master@{#449928}

--- third_party/WebKit/Source/core/editing/FrameSelection.h
+++ third_party/WebKit/Source/core/editing/FrameSelection.h
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class CharacterData;
 class DisplayItemClient;
 class LayoutBlock;
 class LocalFrame;
@@ -56,7 +55,6 @@ class GraphicsContext;
 class HTMLFormElement;
 class SelectionEditor;
 class PendingSelection;
-class Text;
 class TextIteratorBehavior;
 struct PaintInvalidatorContext;
 
@@ -92,6 +90,7 @@ class CORE_EXPORT FrameSelection final
     DoNotSetFocus = 1 << 3,
     DoNotUpdateAppearance = 1 << 4,
     DoNotClearStrategy = 1 << 5,
+    // TODO(yosin): We should get rid of |DoNotAdjustInFlatTree|.
     DoNotAdjustInFlatTree = 1 << 6,
   };
   // Union of values in SetSelectionOption and EUserTriggered
@@ -103,8 +102,7 @@ class CORE_EXPORT FrameSelection final
 
   bool isAvailable() const { return lifecycleContext(); }
   // You should not call |document()| when |!isAvailable()|.
-  const Document& document() const;
-  Document& document();
+  Document& document() const;
   LocalFrame* frame() const { return m_frame; }
   Element* rootEditableElement() const {
     return selection().rootEditableElement();
@@ -123,6 +121,8 @@ class CORE_EXPORT FrameSelection final
 
   template <typename Strategy>
   const VisibleSelectionTemplate<Strategy>& visibleSelection() const;
+  const VisibleSelection& computeVisibleSelectionInDOMTree() const;
+  const VisibleSelectionInFlatTree& computeVisibleSelectionInFlatTree() const;
 
   const VisibleSelection& selection() const;
 
@@ -204,11 +204,13 @@ class CORE_EXPORT FrameSelection final
 
   void didChangeFocus();
 
+  const SelectionInDOMTree& selectionInDOMTree() const;
+  // TODO(yosin): We should rename |isNone()| to |isVisibleNone()|.
   bool isNone() const { return selection().isNone(); }
   bool isCaret() const { return selection().isCaret(); }
   bool isRange() const { return selection().isRange(); }
   bool isInPasswordField() const;
-  bool isDirectional() const { return selection().isDirectional(); }
+  bool isDirectional() const { return selectionInDOMTree().isDirectional(); }
 
   // If this FrameSelection has a logical range which is still valid, this
   // function return its clone. Otherwise, the return value from underlying
@@ -246,9 +248,7 @@ class CORE_EXPORT FrameSelection final
 
   void setUseSecureKeyboardEntryWhenActive(bool);
 
-  bool isHandleVisible() const {
-    return m_handleVisibility == HandleVisibility::Visible;
-  }
+  bool isHandleVisible() const;
 
   void updateSecureKeyboardEntryIfActive();
 
@@ -312,19 +312,6 @@ class CORE_EXPORT FrameSelection final
   // use |visibleSelection<EditingInFlatTreeStrategy>()|.
   const VisibleSelectionInFlatTree& selectionInFlatTree() const;
 
-  template <typename Strategy>
-  void setSelectionAlgorithm(const VisibleSelectionTemplate<Strategy>&,
-                             HandleVisibility,
-                             SetSelectionOptions,
-                             CursorAlignOnScroll,
-                             TextGranularity);
-
-  void respondToNodeModification(Node&,
-                                 bool baseRemoved,
-                                 bool extentRemoved,
-                                 bool startRemoved,
-                                 bool endRemoved);
-
   void notifyAccessibilityForSelectionChange();
   void notifyCompositorForSelectionChange();
   void notifyEventHandlerForSelectionChange();
@@ -344,14 +331,6 @@ class CORE_EXPORT FrameSelection final
   void contextDestroyed(Document*) final;
   void nodeChildrenWillBeRemoved(ContainerNode&) final;
   void nodeWillBeRemoved(Node&) final;
-  void didUpdateCharacterData(CharacterData*,
-                              unsigned offset,
-                              unsigned oldLength,
-                              unsigned newLength) final;
-  void didMergeTextNodes(const Text& mergedNode,
-                         const NodeWithIndex& nodeToBeRemovedWithIndex,
-                         unsigned oldLength) final;
-  void didSplitTextNode(const Text& oldNode) final;
 
   Member<LocalFrame> m_frame;
   const Member<PendingSelection> m_pendingSelection;
@@ -361,8 +340,6 @@ class CORE_EXPORT FrameSelection final
   LayoutUnit m_xPosForVerticalArrowNavigation;
 
   bool m_focused : 1;
-
-  HandleVisibility m_handleVisibility = HandleVisibility::NotVisible;
 
   // Controls text granularity used to adjust the selection's extent in
   // moveRangeSelectionExtent.
