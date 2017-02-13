f72b90d2c166f477caf9eece2f4931fdee06ddd9
samans <samans@chromium.org>
2017-02-13 11:00:28 -0800

    Switching to CompositorFrameSinkSupport in android_webview::SurfacesInstance
    This CL gets rid of SurfaceFactory in android_webview::SurfacesInstance
    and replaces it with CompositorFrameSinkSupport. We're doing this to
    unify the CompositorFrameSink API used by clients, and to simplify
    transitioning to SurfaceReferences.
    
    BUG=674102
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2647583002
    Cr-Commit-Position: refs/heads/master@{#450035}

--- android_webview/browser/surfaces_instance.h
+++ android_webview/browser/surfaces_instance.h
@@ -10,15 +10,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/memory/ref_counted.h"
+#include "cc/surfaces/compositor_frame_sink_support_client.h"
 #include "cc/surfaces/display_client.h"
 #include "cc/surfaces/frame_sink_id.h"
-#include "cc/surfaces/surface_factory_client.h"
 #include "cc/surfaces/surface_id.h"
 
 namespace cc {
+class BeginFrameSource;
+class CompositorFrameSinkSupport;
 class Display;
 class SurfaceIdAllocator;
-class SurfaceFactory;
 class SurfaceManager;
 }
 
@@ -34,7 +35,7 @@ class ParentOutputSurface;
 
 class SurfacesInstance : public base::RefCounted<SurfacesInstance>,
                          public cc::DisplayClient,
-                         public cc::SurfaceFactoryClient {
+                         public cc::CompositorFrameSinkSupportClient {
  public:
   static scoped_refptr<SurfacesInstance> GetOrCreateInstance();
 
@@ -63,9 +64,11 @@ class SurfacesInstance : public base::RefCounted<SurfacesInstance>,
       const cc::RenderPassList& render_passes) override {}
   void DisplayDidDrawAndSwap() override {}
 
-  // cc::SurfaceFactoryClient implementation.
-  void ReturnResources(const cc::ReturnedResourceArray& resources) override;
-  void SetBeginFrameSource(cc::BeginFrameSource* begin_frame_source) override;
+  // cc::CompositorFrameSinkSupportClient implementation.
+  void DidReceiveCompositorFrameAck() override;
+  void OnBeginFrame(const cc::BeginFrameArgs& args) override;
+  void WillDrawSurface() override;
+  void ReclaimResources(const cc::ReturnedResourceArray& resources) override;
 
   void SetEmptyRootFrame();
 
@@ -77,7 +80,7 @@ class SurfacesInstance : public base::RefCounted<SurfacesInstance>,
   std::unique_ptr<cc::BeginFrameSource> begin_frame_source_;
   std::unique_ptr<cc::Display> display_;
   std::unique_ptr<cc::SurfaceIdAllocator> surface_id_allocator_;
-  std::unique_ptr<cc::SurfaceFactory> surface_factory_;
+  std::unique_ptr<cc::CompositorFrameSinkSupport> support_;
 
   cc::LocalSurfaceId root_id_;
   std::vector<cc::SurfaceId> child_ids_;
