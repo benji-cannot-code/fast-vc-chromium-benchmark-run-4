f33075938e8ec996b1568406bef5588853aa3532
fsamuel <fsamuel@chromium.org>
2017-02-13 11:55:02 -0800

    Surfaces: Don't copy referenced_surfaces from CompositorFrameMetadata
    There was a lot of copying of referenced_surfaces going on in
    CompositorFrameMetadata. First of all, cc::Surface held a copy of
    referenced_surfaces if the frame was pending, and then when it activated,
    it cleared the pending_referenced_surfaces vector and copied the
    referenced_surfaces AGAIN, from CompositorFrameMetadata.
    CompositorFrameSinkSupport copied over the same references to a local
    vector just so it can pass a single vector ReferenceSurfaceTracker.
    
    This CL provides a convenience method to access referenced_surfaces directly
    in the pending and active CompositorFrames from cc::Surface instead of storing
    a local copy. ReferenceSurfaceTracker::UpdateReferences now takes in pointers to
    the pending and active vectors, and merges them into a set internally.
    
    BUG=672962
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2694653003
    Cr-Commit-Position: refs/heads/master@{#450052}

--- cc/surfaces/surface.cc
+++ cc/surfaces/surface.cc
@@ -66,7 +66,6 @@ void Surface::QueueFrame(CompositorFrame frame, const DrawCallback& callback) {
 
   if (!blocking_surfaces_.empty()) {
     pending_frame_ = std::move(frame);
-    pending_referenced_surfaces_ = pending_frame_->metadata.referenced_surfaces;
     // Ask the surface manager to inform |this| when its dependencies are
     // resolved.
     factory_->manager()->RequestSurfaceResolution(this);
@@ -152,7 +151,6 @@ void Surface::ActivatePendingFrame() {
   DCHECK(pending_frame_);
   ActivateFrame(std::move(pending_frame_.value()));
   pending_frame_.reset();
-  pending_referenced_surfaces_.clear();
 }
 
 // A frame is activated if all its Surface ID dependences are active or a
@@ -176,8 +174,6 @@ void Surface::ActivateFrame(CompositorFrame frame) {
 
   if (previous_frame)
     UnrefFrameResources(*previous_frame);
-
-  active_referenced_surfaces_ = active_frame_->metadata.referenced_surfaces;
 
   for (auto& observer : observers_)
     observer.OnSurfaceActivated(this);
