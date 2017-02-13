df09014954c6d066108e5e0bf5fd56b76f705b8e
fdoray <fdoray@chromium.org>
2017-02-13 10:29:44 -0800

    Use TaskScheduler instead of blocking pool in widevine_cdm_component_installer.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=waffles@chromium.org
    
    Review-Url: https://codereview.chromium.org/2678713003
    Cr-Commit-Position: refs/heads/master@{#450021}

--- chrome/browser/component_updater/widevine_cdm_component_installer.cc
+++ chrome/browser/component_updater/widevine_cdm_component_installer.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/values.h"
 #include "build/build_config.h"
 #include "chrome/common/chrome_paths.h"
@@ -311,12 +312,11 @@ void WidevineCdmComponentInstallerTraits::ComponentReady(
     return;
   }
 
-  BrowserThread::PostBlockingPoolTask(
-      FROM_HERE,
+  base::PostTaskWithTraits(
+      FROM_HERE, base::TaskTraits().MayBlock().WithPriority(
+                     base::TaskPriority::BACKGROUND),
       base::Bind(&WidevineCdmComponentInstallerTraits::UpdateCdmAdapter,
-                 base::Unretained(this),
-                 version,
-                 path,
+                 base::Unretained(this), version, path,
                  base::Passed(&manifest)));
 }
 
