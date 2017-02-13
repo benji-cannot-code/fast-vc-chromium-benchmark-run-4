b75d0dd0d5d213777197ceaaeec08c3d0cd33bc5
lod <lod@chromium.org>
2017-02-13 06:37:14 -0800

    [ObjC ARC] Converts ios/chrome/browser/crash_report:unit_tests to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2688163002
    Cr-Commit-Position: refs/heads/master@{#449951}

--- ios/chrome/browser/crash_report/crash_restore_helper_unittest.mm
+++ ios/chrome/browser/crash_report/crash_restore_helper_unittest.mm
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/logging.h"
-#include "base/mac/scoped_nsobject.h"
 #include "base/strings/sys_string_conversions.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/browser_state/test_chrome_browser_state.h"
@@ -20,6 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/platform_test.h"
 #import "third_party/ocmock/OCMock/OCMock.h"
 #include "third_party/ocmock/gtest_support.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
 
 using testing::Return;
 
@@ -36,15 +39,15 @@ class CrashRestoreHelperTest : public PlatformTest {
     chrome_browser_state_ = test_cbs_builder.Build();
     otr_chrome_browser_state_ =
         chrome_browser_state_->GetOffTheRecordChromeBrowserState();
-    helper_.reset([[CrashRestoreHelper alloc]
-        initWithBrowserState:chrome_browser_state_.get()]);
+    helper_ = [[CrashRestoreHelper alloc]
+        initWithBrowserState:chrome_browser_state_.get()];
   }
 
  protected:
   web::TestWebThreadBundle thread_bundle_;
   std::unique_ptr<TestChromeBrowserState> chrome_browser_state_;
   ios::ChromeBrowserState* otr_chrome_browser_state_;
-  base::scoped_nsobject<CrashRestoreHelper> helper_;
+  CrashRestoreHelper* helper_;
 };
 
 TEST_F(CrashRestoreHelperTest, MoveAsideTest) {
