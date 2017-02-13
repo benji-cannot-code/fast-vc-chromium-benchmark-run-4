ddfe94e1e7a346850dbb5a291e0363a58019c1d0
jkrcal <jkrcal@chromium.org>
2017-02-13 06:39:00 -0800

    [Field trial params] Getting field trial params by feature name in Java.
    This CL adds support to read field trial params in Java using the name
    of the feature they are associated to. It also brings parsing values to
    primitive types on par to what is available in c++ code.
    
    In follow-up CLs I plan to
     - extend ChromeFeatureList to support overriding param values from junit tests;
     - use the new functions from ntp_snippets java code.
    
    BUG=689937
    
    Review-Url: https://codereview.chromium.org/2686763003
    Cr-Commit-Position: refs/heads/master@{#449952}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -22,7 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "jni/ChromeFeatureList_jni.h"
 
 using base::android::ConvertJavaStringToUTF8;
+using base::android::ConvertUTF8ToJavaString;
 using base::android::JavaParamRef;
+using base::android::ScopedJavaLocalRef;
 
 namespace chrome {
 namespace android {
@@ -74,6 +76,16 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &offline_pages::kOfflinePagesSharingFeature,
     &password_manager::features::kViewPasswords,
 };
+
+const base::Feature* FindFeatureExposedToJava(const std::string& feature_name) {
+  for (size_t i = 0; i < arraysize(kFeaturesExposedToJava); ++i) {
+    if (kFeaturesExposedToJava[i]->name == feature_name)
+      return kFeaturesExposedToJava[i];
+  }
+  NOTREACHED() << "Features queried via ChromeFeatureList must be present in "
+                  "|kFeaturesExposedToJava|.";
+  return nullptr;
+}
 
 }  // namespace
 
@@ -160,14 +172,22 @@ const base::Feature kWebVRCardboardSupport{
 static jboolean IsEnabled(JNIEnv* env,
                           const JavaParamRef<jclass>& clazz,
                           const JavaParamRef<jstring>& jfeature_name) {
-  const std::string feature_name = ConvertJavaStringToUTF8(env, jfeature_name);
-  for (size_t i = 0; i < arraysize(kFeaturesExposedToJava); ++i) {
-    if (kFeaturesExposedToJava[i]->name == feature_name)
-      return base::FeatureList::IsEnabled(*kFeaturesExposedToJava[i]);
-  }
-  // Features queried via this API must be present in |kFeaturesExposedToJava|.
-  NOTREACHED();
-  return false;
+  const base::Feature* feature =
+      FindFeatureExposedToJava(ConvertJavaStringToUTF8(env, jfeature_name));
+  return base::FeatureList::IsEnabled(*feature);
+}
+
+static ScopedJavaLocalRef<jstring> GetFieldTrialParamByFeature(
+    JNIEnv* env,
+    const JavaParamRef<jclass>& clazz,
+    const JavaParamRef<jstring>& jfeature_name,
+    const JavaParamRef<jstring>& jparam_name) {
+  const base::Feature* feature =
+      FindFeatureExposedToJava(ConvertJavaStringToUTF8(env, jfeature_name));
+  const std::string& param_name = ConvertJavaStringToUTF8(env, jparam_name);
+  const std::string& param_value =
+      base::GetFieldTrialParamValueByFeature(*feature, param_name);
+  return ConvertUTF8ToJavaString(env, param_value);
 }
 
 static jint GetFieldTrialParamByFeatureAsInt(
@@ -176,18 +196,37 @@ static jint GetFieldTrialParamByFeatureAsInt(
     const JavaParamRef<jstring>& jfeature_name,
     const JavaParamRef<jstring>& jparam_name,
     const jint jdefault_value) {
-  const std::string feature_name = ConvertJavaStringToUTF8(env, jfeature_name);
-  const std::string param_name = ConvertJavaStringToUTF8(env, jparam_name);
-  int default_value = static_cast<int>(jdefault_value);
+  const base::Feature* feature =
+      FindFeatureExposedToJava(ConvertJavaStringToUTF8(env, jfeature_name));
+  const std::string& param_name = ConvertJavaStringToUTF8(env, jparam_name);
+  return base::GetFieldTrialParamByFeatureAsInt(*feature, param_name,
+                                                jdefault_value);
+}
 
-  for (size_t i = 0; i < arraysize(kFeaturesExposedToJava); ++i) {
-    if (kFeaturesExposedToJava[i]->name == feature_name)
-      return base::GetFieldTrialParamByFeatureAsInt(
-          *kFeaturesExposedToJava[i], param_name, default_value);
-  }
-  // Features queried via this API must be present in |kFeaturesExposedToJava|.
-  NOTREACHED();
-  return jdefault_value;
+static jdouble GetFieldTrialParamByFeatureAsDouble(
+    JNIEnv* env,
+    const JavaParamRef<jclass>& clazz,
+    const JavaParamRef<jstring>& jfeature_name,
+    const JavaParamRef<jstring>& jparam_name,
+    const jdouble jdefault_value) {
+  const base::Feature* feature =
+      FindFeatureExposedToJava(ConvertJavaStringToUTF8(env, jfeature_name));
+  const std::string& param_name = ConvertJavaStringToUTF8(env, jparam_name);
+  return base::GetFieldTrialParamByFeatureAsDouble(*feature, param_name,
+                                                   jdefault_value);
+}
+
+static jboolean GetFieldTrialParamByFeatureAsBoolean(
+    JNIEnv* env,
+    const JavaParamRef<jclass>& clazz,
+    const JavaParamRef<jstring>& jfeature_name,
+    const JavaParamRef<jstring>& jparam_name,
+    const jboolean jdefault_value) {
+  const base::Feature* feature =
+      FindFeatureExposedToJava(ConvertJavaStringToUTF8(env, jfeature_name));
+  const std::string& param_name = ConvertJavaStringToUTF8(env, jparam_name);
+  return base::GetFieldTrialParamByFeatureAsBool(*feature, param_name,
+                                                 jdefault_value);
 }
 
 bool RegisterChromeFeatureListJni(JNIEnv* env) {
