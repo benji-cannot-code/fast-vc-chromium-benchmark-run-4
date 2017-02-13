3efd218c9e67cbd2aa46ae4d1e4d202304498cc0
sunjian <sunjian@chromium.org>
2017-02-13 13:37:45 -0800

    Surface Location::setHash event in tracing
    BUG=691089
    
    Review-Url: https://codereview.chromium.org/2682223007
    Cr-Commit-Position: refs/heads/master@{#450100}

--- third_party/WebKit/Source/core/frame/Location.cpp
+++ third_party/WebKit/Source/core/frame/Location.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/LocalDOMWindow.h"
 #include "core/frame/LocalFrame.h"
 #include "core/loader/FrameLoader.h"
+#include "platform/instrumentation/tracing/TraceEvent.h"
 #include "platform/weborigin/KURL.h"
 #include "platform/weborigin/SecurityOrigin.h"
 
@@ -210,6 +211,7 @@ void Location::setHash(LocalDOMWindow* currentWindow,
                        LocalDOMWindow* enteredWindow,
                        const String& hash,
                        ExceptionState& exceptionState) {
+  TRACE_EVENT0("blink", "Location::setHash");
   if (!m_frame)
     return;
   KURL url = toLocalFrame(m_frame)->document()->url();
