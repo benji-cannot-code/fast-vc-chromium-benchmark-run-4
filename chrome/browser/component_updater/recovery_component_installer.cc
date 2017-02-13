19b9ebe9fef44870f0c00d68509198d7198b3c5a
sorin <sorin@chromium.org>
2017-02-13 12:05:48 -0800

    Simplify the registration of the Chrome Recovery component.
    BUG=690574
    
    Review-Url: https://codereview.chromium.org/2686863004
    Cr-Original-Commit-Position: refs/heads/master@{#449439}
    Committed: https://chromium.googlesource.com/chromium/src/+/ff39aeb77a4a4e1679cfc5feed58e40e98f472ee
    Review-Url: https://codereview.chromium.org/2686863004
    Cr-Commit-Position: refs/heads/master@{#450057}

--- chrome/browser/component_updater/recovery_component_installer.cc
+++ chrome/browser/component_updater/recovery_component_installer.cc
@@ -45,6 +45,9 @@ using content::BrowserThread;
 
 namespace component_updater {
 
+#if defined(GOOGLE_CHROME_BUILD)
+#if defined(OS_WIN) || defined(OS_MACOSX)
+
 namespace {
 
 // CRX hash. The extension id is: npdjjkjlcidkjlamlmmdelcjbcpdjocm.
@@ -89,14 +92,12 @@ void RecordRecoveryComponentUMAEvent(RecoveryComponentEvent event) {
   UMA_HISTOGRAM_ENUMERATION("RecoveryComponent.Event", event, RCE_COUNT);
 }
 
-#if !defined(OS_CHROMEOS) && defined(GOOGLE_CHROME_BUILD)
 // Checks if elevated recovery simulation switch was present on the command
 // line. This is for testing purpose.
 bool SimulatingElevatedRecovery() {
   return base::CommandLine::ForCurrentProcess()->HasSwitch(
       switches::kSimulateElevatedRecovery);
 }
-#endif  // !defined(OS_CHROMEOS) && defined(GOOGLE_CHROME_BUILD)
 
 std::vector<std::string> GetRecoveryInstallArguments(
     const base::DictionaryValue& manifest,
@@ -138,7 +139,6 @@ base::CommandLine BuildRecoveryInstallCommandLine(
   return command_line;
 }
 
-#if defined(OS_WIN) || defined(OS_MACOSX)
 std::unique_ptr<base::DictionaryValue> ReadManifest(
     const base::FilePath& manifest) {
   JSONFileValueDeserializer deserializer(manifest);
@@ -244,7 +244,6 @@ void ElevatedInstallRecoveryComponent(const base::FilePath& installer_path) {
                      .MayBlock(),
       base::Bind(&DoElevatedInstallRecoveryComponent, installer_path));
 }
-#endif  // defined(OS_WIN)
 
 }  // namespace
 
@@ -333,7 +332,6 @@ void RecoveryComponentInstaller::OnUpdateError(int error) {
   NOTREACHED() << "Recovery component update error: " << error;
 }
 
-#if defined(OS_WIN) || defined(OS_MACOSX)
 void WaitForInstallToComplete(base::Process process,
                               const base::FilePath& installer_folder,
                               PrefService* prefs) {
@@ -388,13 +386,6 @@ bool RecoveryComponentInstaller::RunInstallCommand(
   // install later.
   return true;
 }
-#else
-bool RecoveryComponentInstaller::RunInstallCommand(
-    const base::CommandLine& cmdline,
-    const base::FilePath&) const {
-  return base::LaunchProcess(cmdline, base::LaunchOptions()).IsValid();
-}
-#endif  // defined(OS_WIN)
 
 #if defined(OS_POSIX)
 // Sets the POSIX executable permissions on a file
@@ -492,9 +483,13 @@ bool RecoveryComponentInstaller::Uninstall() {
   return false;
 }
 
+#endif  // defined(OS_WIN) || defined(OS_MACOSX)
+#endif  // defined(GOOGLE_CHROME_BUILD)
+
 void RegisterRecoveryComponent(ComponentUpdateService* cus,
                                PrefService* prefs) {
-#if !defined(OS_CHROMEOS) && defined(GOOGLE_CHROME_BUILD)
+#if defined(GOOGLE_CHROME_BUILD)
+#if defined(OS_WIN) || defined(OS_MACOSX)
   if (SimulatingElevatedRecovery()) {
     BrowserThread::PostTask(
         BrowserThread::UI,
@@ -509,7 +504,8 @@ void RegisterRecoveryComponent(ComponentUpdateService* cus,
       FROM_HERE,
       base::Bind(&RecoveryRegisterHelper, cus, prefs),
       base::TimeDelta::FromSeconds(6));
-#endif  // !defined(OS_CHROMEOS) && defined(GOOGLE_CHROME_BUILD)
+#endif
+#endif
 }
 
 void RegisterPrefsForRecoveryComponent(PrefRegistrySimple* registry) {
@@ -522,10 +518,13 @@ void RegisterPrefsForRecoveryComponent(PrefRegistrySimple* registry) {
 void AcceptedElevatedRecoveryInstall(PrefService* prefs) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
+#if defined(GOOGLE_CHROME_BUILD)
 #if defined(OS_WIN) || defined(OS_MACOSX)
   ElevatedInstallRecoveryComponent(
       prefs->GetFilePath(prefs::kRecoveryComponentUnpackPath));
-#endif  // OS_WIN
+#endif
+#endif
+
   prefs->SetBoolean(prefs::kRecoveryComponentNeedsElevation, false);
 }
 
