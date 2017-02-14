b23d9f1de6fed103ab026c2ec72e4c7a890e5477
lod <lod@chromium.org>
2017-02-14 09:11:59 -0800

    [ObjC ARC] Converts ios/chrome/common:unit_tests to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:
    Separated block_unittest.mm into its own target as it checks memory management and does not apply to an ARC environment. Bug opened at https://bugs.chromium.org/p/chromium/issues/detail?id=690941 to create a test to check for correct interaction of C++ and blocks in the ARC world.
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2681353002
    Cr-Commit-Position: refs/heads/master@{#450391}

--- ios/chrome/common/string_util_unittest.mm
+++ ios/chrome/common/string_util_unittest.mm
@@ -7,10 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
-#include "base/mac/scoped_nsobject.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/gtest_mac.h"
 #include "testing/platform_test.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
 
 namespace {
 
