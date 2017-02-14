aa59a9849833b869571f821a36ffe7893d71de30
mgersh <mgersh@chromium.org>
2017-02-14 11:33:56 -0800

    Fix cronet_unittests build on ios CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_cronet_tester
    Review-Url: https://codereview.chromium.org/2693703006
    Cr-Commit-Position: refs/heads/master@{#450436}

--- components/cronet/stale_host_resolver_unittest.cc
+++ components/cronet/stale_host_resolver_unittest.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/values.h"
 #include "components/cronet/url_request_context_config.h"
-#include "net/android/network_change_notifier_factory_android.h"
 #include "net/base/net_errors.h"
 #include "net/base/network_change_notifier.h"
 #include "net/cert/cert_verifier.h"
@@ -28,6 +27,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_context.h"
 #include "net/url_request/url_request_context_builder.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+#if defined(OS_ANDROID)
+#include "net/android/network_change_notifier_factory_android.h"
+#endif
 
 namespace cronet {
 
@@ -139,8 +142,10 @@ class StaleHostResolverTest : public testing::Test {
   }
 
   void CreateNetworkChangeNotifier() {
+#if defined(OS_ANDROID)
     net::NetworkChangeNotifier::SetFactory(
         new net::NetworkChangeNotifierFactoryAndroid());
+#endif
     net::NetworkChangeNotifier::Create();
   }
 
