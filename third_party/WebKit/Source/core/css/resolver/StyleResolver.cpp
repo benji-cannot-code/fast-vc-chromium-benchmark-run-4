c08db71d5105ab55882024323e58be25c64bc73b
fs <fs@opera.com>
2017-02-14 05:24:44 -0800

    Remove unused includes of SVGDocumentExtensions.h
    Review-Url: https://codereview.chromium.org/2696803002
    Cr-Commit-Position: refs/heads/master@{#450334}

--- third_party/WebKit/Source/core/css/resolver/StyleResolver.cpp
+++ third_party/WebKit/Source/core/css/resolver/StyleResolver.cpp
@@ -91,7 +91,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/inspector/InspectorInstrumentation.h"
 #include "core/layout/GeneratedChildren.h"
 #include "core/style/StyleInheritedVariables.h"
-#include "core/svg/SVGDocumentExtensions.h"
 #include "core/svg/SVGElement.h"
 #include "platform/RuntimeEnabledFeatures.h"
 #include "wtf/StdLibExtras.h"
--- third_party/WebKit/Source/core/input/EventHandler.cpp
+++ third_party/WebKit/Source/core/input/EventHandler.cpp
@@ -87,7 +87,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/paint/PaintLayer.h"
 #include "core/style/ComputedStyle.h"
 #include "core/style/CursorData.h"
-#include "core/svg/SVGDocumentExtensions.h"
 #include "platform/RuntimeEnabledFeatures.h"
 #include "platform/WindowsKeyboardCodes.h"
 #include "platform/geometry/FloatPoint.h"
