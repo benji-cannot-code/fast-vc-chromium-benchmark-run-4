33abb5a092f04677fde329c904356153d71be9c5
mgiuca <mgiuca@chromium.org>
2017-02-13 17:45:42 -0800

    Remove conditional compile statements in ShareServiceImpl (and test).
    These were previously added to allow this code to compile (but be
    useless) on Mac and Android. As of r449905, these files are not compiled
    at all on Mac and Android, so these if statements are no longer needed.
    
    BUG=668389
    
    Review-Url: https://codereview.chromium.org/2694643002
    Cr-Commit-Position: refs/heads/master@{#450193}

--- chrome/browser/webshare/share_service_impl_unittest.cc
+++ chrome/browser/webshare/share_service_impl_unittest.cc
@@ -155,8 +155,6 @@ class ShareServiceImplUnittest : public ChromeRenderViewHostTestHarness {
 
 }  // namespace
 
-#if defined(OS_LINUX) || defined(OS_WIN)
-
 // Basic test to check the Share method calls the callback with the expected
 // parameters.
 TEST_F(ShareServiceImplUnittest, ShareCallbackParams) {
@@ -265,8 +263,6 @@ TEST_F(ShareServiceImplUnittest, ShareWithSomeInsufficientlyEngagedTargets) {
 
   run_loop.Run();
 }
-
-#endif  // defined(OS_LINUX) || defined(OS_WIN)
 
 // Replace various numbers of placeholders in various orders. Placeholders are
 // adjacent to eachother; there are no padding characters.
