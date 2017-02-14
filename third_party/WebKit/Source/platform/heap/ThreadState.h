c71ce5dd25375b527573859364a01d4dcc378d44
haraken <haraken@chromium.org>
2017-02-14 05:39:27 -0800

    Remove thread-local weak processing
    It is now unused.
    
    BUG=671856
    
    Review-Url: https://codereview.chromium.org/2694173002
    Cr-Commit-Position: refs/heads/master@{#450339}

--- third_party/WebKit/Source/platform/heap/ThreadState.h
+++ third_party/WebKit/Source/platform/heap/ThreadState.h
@@ -55,7 +55,6 @@ class Isolate;
 namespace blink {
 
 class BasePage;
-class CallbackStack;
 class GarbageCollectedMixinConstructorMarker;
 class PersistentNode;
 class PersistentRegion;
@@ -187,9 +186,7 @@ class PLATFORM_EXPORT ThreadState {
   static ThreadState* fromObject(const void*);
 
   bool isMainThread() const { return this == mainThreadState(); }
-#if DCHECK_IS_ON()
   bool checkThread() const { return m_thread == currentThread(); }
-#endif
 
   ThreadHeap& heap() const { return *m_heap; }
 
@@ -389,10 +386,6 @@ class PLATFORM_EXPORT ThreadState {
     Vector<size_t> liveSize;
     Vector<size_t> deadSize;
   };
-
-  void pushThreadLocalWeakCallback(void*, WeakCallback);
-  bool popAndInvokeThreadLocalWeakCallback(Visitor*);
-  void threadLocalWeakProcessing();
 
   size_t objectPayloadSizeForTesting();
 
@@ -664,8 +657,6 @@ class PLATFORM_EXPORT ThreadState {
 
   bool m_shouldFlushHeapDoesNotContainCache;
   GCState m_gcState;
-
-  std::unique_ptr<CallbackStack> m_threadLocalWeakCallbackStack;
 
   using PreFinalizerCallback = bool (*)(void*);
   using PreFinalizer = std::pair<void*, PreFinalizerCallback>;
