5bf6a84ce034b61dc3333a4f79dddda2b6c16b40
dmurph <dmurph@chromium.org>
2017-02-13 21:27:57 -0800

    IndexedDB: Added external memory counting for IDBValue.
    Should also fix https://code.google.com/p/chrome-os-partner/issues/detail?id=62731
    
    R=cmumford@chromium.org
    BUG=691607, 690517
    
    Review-Url: https://codereview.chromium.org/2691163002
    Cr-Commit-Position: refs/heads/master@{#450265}

--- third_party/WebKit/Source/modules/indexeddb/IDBValue.h
+++ third_party/WebKit/Source/modules/indexeddb/IDBValue.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class BlobDataHandle;
+class SerializedScriptValue;
 class WebBlobInfo;
 struct WebIDBValue;
 
@@ -31,7 +32,7 @@ class MODULES_EXPORT IDBValue final : public RefCounted<IDBValue> {
 
   bool isNull() const;
   Vector<String> getUUIDs() const;
-  const SharedBuffer* data() const;
+  RefPtr<SerializedScriptValue> createSerializedValue() const;
   Vector<WebBlobInfo>* blobInfo() const { return m_blobInfo.get(); }
   const IDBKey* primaryKey() const { return m_primaryKey; }
   const IDBKeyPath& keyPath() const { return m_keyPath; }
@@ -45,11 +46,14 @@ class MODULES_EXPORT IDBValue final : public RefCounted<IDBValue> {
            const IDBKeyPath&);
   IDBValue(const IDBValue*, IDBKey*, const IDBKeyPath&);
 
+  // Keep this private to prevent new refs because we manually bookkeep the
+  // memory to V8.
   const RefPtr<SharedBuffer> m_data;
   const std::unique_ptr<Vector<RefPtr<BlobDataHandle>>> m_blobData;
   const std::unique_ptr<Vector<WebBlobInfo>> m_blobInfo;
   const Persistent<IDBKey> m_primaryKey;
   const IDBKeyPath m_keyPath;
+  int64_t m_externalAllocatedSize = 0;
 };
 
 }  // namespace blink
