593f55ea9138cb0a4173e525bb5644f106d72ee2
tkent <tkent@chromium.org>
2017-02-13 21:30:02 -0800

    Selection API: anchorNode, anchorOffset, focusNode, focusOffset, isCollapsed, and type should be based on a registered Range.
    We computed them with normalized selection, a.k.a. VisibleSelection.
    This CL improves external/wpt/selection/isCollapsed.html.
    
    Total new PASS: 116
    Total new FAIL: 77
    
    Following CLs will resolve FAILs which this CL introduces.
    
    BUG=691887
    
    Review-Url: https://codereview.chromium.org/2694043004
    Cr-Commit-Position: refs/heads/master@{#450266}

--- third_party/WebKit/Source/core/editing/DOMSelection.h
+++ third_party/WebKit/Source/core/editing/DOMSelection.h
@@ -90,7 +90,7 @@ class CORE_EXPORT DOMSelection final : public GarbageCollected<DOMSelection>,
   void collapseToEnd(ExceptionState&);
   void collapseToStart(ExceptionState&);
   void extend(Node*, int offset, ExceptionState&);
-  Range* getRangeAt(int, ExceptionState&);
+  Range* getRangeAt(int, ExceptionState&) const;
   void removeAllRanges();
   void addRange(Range*);
   void deleteFromDocument();
@@ -118,11 +118,11 @@ class CORE_EXPORT DOMSelection final : public GarbageCollected<DOMSelection>,
   bool isValidForPosition(Node*) const;
 
   void addConsoleError(const String& message);
-
-  Range* createRangeFromSelectionEditor();
+  Range* primaryRangeOrNull() const;
+  Range* createRangeFromSelectionEditor() const;
 
   bool isSelectionOfDocument() const;
-  void cacheRangeIfSelectionOfDocument(Range*);
+  void cacheRangeIfSelectionOfDocument(Range*) const;
   Range* documentCachedRange() const;
   void clearCachedRangeIfSelectionOfDocument();
 
