2bc81c752855de17863a916fcf506b1818e8157e
melandory <melandory@chromium.org>
2017-02-14 03:57:52 -0800

    Use Smart Lock preference instead of Chrome Password manager preference.
    After this CL the ability to change  Chrome Password manager preference will be not exposed anymore. The two preferences have same values, because they were reconciled previously.
    
    Smart Lock preference: credentials_enable_service
    Chrome Password Manager preference: profile.password_manager_enabled
    
    BUG=517087, 658683
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/1273053002
    Cr-Commit-Position: refs/heads/master@{#450326}

--- chrome/browser/password_manager/chrome_password_manager_client_unittest.cc
+++ chrome/browser/password_manager/chrome_password_manager_client_unittest.cc
@@ -201,7 +201,7 @@ void ChromePasswordManagerClientTest::SetUp() {
                                        base::Unretained(&fake_agent_)));
 
   prefs_.registry()->RegisterBooleanPref(
-      password_manager::prefs::kPasswordManagerSavingEnabled, true);
+      password_manager::prefs::kCredentialsEnableService, true);
   ChromePasswordManagerClient::CreateForWebContentsWithAutofillClient(
       web_contents(), nullptr);
 
@@ -327,10 +327,10 @@ TEST_F(ChromePasswordManagerClientTest,
   // Test that saving passwords depends on the password manager enabled
   // preference.
   ChromePasswordManagerClient* client = GetClient();
-  prefs()->SetUserPref(password_manager::prefs::kPasswordManagerSavingEnabled,
+  prefs()->SetUserPref(password_manager::prefs::kCredentialsEnableService,
                        new base::FundamentalValue(true));
   EXPECT_TRUE(client->IsSavingAndFillingEnabledForCurrentPage());
-  prefs()->SetUserPref(password_manager::prefs::kPasswordManagerSavingEnabled,
+  prefs()->SetUserPref(password_manager::prefs::kCredentialsEnableService,
                        new base::FundamentalValue(false));
   EXPECT_FALSE(client->IsSavingAndFillingEnabledForCurrentPage());
 }
@@ -349,7 +349,7 @@ TEST_F(ChromePasswordManagerClientTest, SavingAndFillingEnabledConditionsTest) {
 
   // Functionality disabled if there are SSL errors and the manager itself is
   // disabled.
-  prefs()->SetUserPref(password_manager::prefs::kPasswordManagerSavingEnabled,
+  prefs()->SetUserPref(password_manager::prefs::kCredentialsEnableService,
                        new base::FundamentalValue(false));
   EXPECT_FALSE(client->IsSavingAndFillingEnabledForCurrentPage());
   EXPECT_FALSE(client->IsFillingEnabledForCurrentPage());
@@ -358,7 +358,7 @@ TEST_F(ChromePasswordManagerClientTest, SavingAndFillingEnabledConditionsTest) {
   // is disabled.
   EXPECT_CALL(*client, DidLastPageLoadEncounterSSLErrors())
       .WillRepeatedly(Return(false));
-  prefs()->SetUserPref(password_manager::prefs::kPasswordManagerSavingEnabled,
+  prefs()->SetUserPref(password_manager::prefs::kCredentialsEnableService,
                        new base::FundamentalValue(false));
   EXPECT_FALSE(client->IsSavingAndFillingEnabledForCurrentPage());
   EXPECT_TRUE(client->IsFillingEnabledForCurrentPage());
@@ -367,7 +367,7 @@ TEST_F(ChromePasswordManagerClientTest, SavingAndFillingEnabledConditionsTest) {
   // enabled.
   EXPECT_CALL(*client, DidLastPageLoadEncounterSSLErrors())
       .WillRepeatedly(Return(false));
-  prefs()->SetUserPref(password_manager::prefs::kPasswordManagerSavingEnabled,
+  prefs()->SetUserPref(password_manager::prefs::kCredentialsEnableService,
                        new base::FundamentalValue(true));
   EXPECT_TRUE(client->IsSavingAndFillingEnabledForCurrentPage());
   EXPECT_TRUE(client->IsFillingEnabledForCurrentPage());
@@ -379,7 +379,7 @@ TEST_F(ChromePasswordManagerClientTest, SavingAndFillingEnabledConditionsTest) {
 
   // Functionality disabled in Incognito mode also when manager itself is
   // enabled.
-  prefs()->SetUserPref(password_manager::prefs::kPasswordManagerSavingEnabled,
+  prefs()->SetUserPref(password_manager::prefs::kCredentialsEnableService,
                        new base::FundamentalValue(true));
   EXPECT_FALSE(client->IsSavingAndFillingEnabledForCurrentPage());
   EXPECT_TRUE(client->IsFillingEnabledForCurrentPage());
