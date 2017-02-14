04be2433c49a747e6fa23db3a15e8043739d970f
sfiera <sfiera@chromium.org>
2017-02-14 09:08:56 -0800

    NTP: cap number of notifications/day
    1 by default; Finch-configurable.
    
    BUG=689465
    
    Review-Url: https://codereview.chromium.org/2691023002
    Cr-Commit-Position: refs/heads/master@{#450389}

--- chrome/browser/ntp_snippets/ntp_snippets_features.cc
+++ chrome/browser/ntp_snippets/ntp_snippets_features.cc
@@ -15,5 +15,6 @@ const char kContentSuggestionsNotificationsUseSnippetAsTextParam[] =
 const char
     kContentSuggestionsNotificationsKeepNotificationWhenFrontmostParam[] =
         "keep_notification_when_frontmost";
+const char kContentSuggestionsNotificationsDailyLimit[] = "daily_limit";
 const char kContentSuggestionsNotificationsIgnoredLimitParam[] =
     "ignored_limit";
