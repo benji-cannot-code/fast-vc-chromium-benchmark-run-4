103e5c36c3a761ed67d96dfbbca1c4f883f561e1
keishi <keishi@chromium.org>
2017-02-13 20:47:46 -0800

    Move postGC out of CrossThreadPersistentRegion::LockScope
    Some finalizers/prefinalizers acquire locks, causing a dead lock when CrossThreadPersistent tries to acquire the CrossThreadPersistentRegion lock.
    
    This CL moves the postGC step outside of the CrossThreadPersistentRegion::LockScope. This should be okay as the CrossThreadPersistentRegion::LockScope only exists to avoid add/deletetion of CrossThreadPersistentNodes during marking.
    
    BUG=684856,685624,685624
    
    Review-Url: https://codereview.chromium.org/2686533003
    Cr-Commit-Position: refs/heads/master@{#450251}

--- third_party/WebKit/Source/platform/heap/Heap.cpp
+++ third_party/WebKit/Source/platform/heap/Heap.cpp
@@ -392,6 +392,11 @@ void ThreadHeap::postGC(BlinkGC::GCType gcType) {
     state->postGC(gcType);
 }
 
+void ThreadHeap::preSweep(BlinkGC::GCType gcType) {
+  for (ThreadState* state : m_threads)
+    state->preSweep(gcType);
+}
+
 void ThreadHeap::processMarkingStack(Visitor* visitor) {
   // Ephemeron fixed point loop.
   do {
