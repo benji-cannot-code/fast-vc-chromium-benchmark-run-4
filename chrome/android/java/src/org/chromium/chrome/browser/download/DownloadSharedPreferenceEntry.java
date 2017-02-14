7fe7b4cb4e22ac17e2eaa29ae57a99baf9980b13
qinmin <qinmin@chromium.org>
2017-02-14 10:38:23 -0800

    Add new UMA to record the bytes wasted due to download resumption
    When resuming a download, it is possible chrome discards some of the data received.
    This CL adds a new UMA histogram to track how much data is wasted due to this.
    To make things simple, we check if bytes received keep on increasing on each update.
    If the data received drops, that means the data is wasted.
    This logic is not 100% accurate, as the update is not called on every byte change.
    However, it should work in most of the cases, and we are only interested in a rough number.
    This CL also changes the UMA SharedPrefs.
    So have to do some conversions from old sharedpref version.
    
    BUG=679092
    
    , whenever download updates, we check if the bytes received
    
    Review-Url: https://codereview.chromium.org/2681033002
    Cr-Commit-Position: refs/heads/master@{#450415}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadSharedPreferenceEntry.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadSharedPreferenceEntry.java
@@ -158,6 +158,7 @@ public class DownloadSharedPreferenceEntry {
                 .setDownloadGuid(downloadGuid)
                 .setFileName(fileName)
                 .setIsOffTheRecord(isOffTheRecord)
+                .setBytesReceived(DownloadManagerService.UNKNOWN_BYTES_RECEIVED)
                 .build();
         return new DownloadItem(false, info);
     }
