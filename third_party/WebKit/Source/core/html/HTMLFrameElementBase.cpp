8d4b4c16d99d7c5a4ee08c1087b2c2d8454d16d4
davidsac <davidsac@chromium.org>
2017-02-15 09:05:55 -0800

    Fix infinite loop of frames being created when one or more frames contain nested IFrames to one another.
    To reproduce this issue, run Chrome in site-per-process mode and visit
    either http://dudeguy409.github.io/coreferencingframe1 or
    https://davidsac.github.io/coreferencingframe2.html.
    
    The current check refuses to load a URL if any two ancestors in the
    frame tree have the same URL. It is currently inadequate for two
    reasons:
    
    1) This doesn't work for OOPIFs.  If the parent frames are in a
    different process, they will be remote frames which don't have URLs,
    so the check is simply skipped.
    
    (2) it's only applied in a subset of cases and doesn't handle
    redirects, for example.
    
    To fix these issues, this CL moves these checks to the browser
    process, where it's applied inside NavigationHandleImpl any time a
    request is started or a redirect is received.
    
    BUG=650332, 527367
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2528813002
    Cr-Commit-Position: refs/heads/master@{#450728}

--- third_party/WebKit/Source/core/html/HTMLFrameElementBase.cpp
+++ third_party/WebKit/Source/core/html/HTMLFrameElementBase.cpp
@@ -72,11 +72,6 @@ bool HTMLFrameElementBase::isURLAllowed() const {
             BindingSecurity::ErrorReportOption::Report))
       return false;
   }
-
-  LocalFrame* parentFrame = document().frame();
-  if (parentFrame)
-    return parentFrame->isURLAllowed(completeURL);
-
   return true;
 }
 
