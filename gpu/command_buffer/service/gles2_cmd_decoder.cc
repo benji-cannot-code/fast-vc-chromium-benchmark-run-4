5db117f863aaad98925ca093314aa55039596b85
vmiura <vmiura@chromium.org>
2017-02-15 13:28:02 -0800

    Handle GLES2::ConsumeTextureCHROMIUM consuming the currently bound texture.
    If the consumed mailbox corresponded to the currently bound texture,
    the same texture was first deleted, then consumed causing use-after-free.
    
    R=zmo@chromium.org
    BUG=690216
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2693273002
    Cr-Commit-Position: refs/heads/master@{#450806}

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -17280,6 +17280,9 @@ void GLES2DecoderImpl::DoConsumeTextureCHROMIUM(GLenum target,
     return;
   }
 
+  if (texture_ref->texture() == texture)
+    return;
+
   DeleteTexturesHelper(1, &client_id);
   texture_ref = texture_manager()->Consume(client_id, texture);
   glBindTexture(target, texture_ref->service_id());
