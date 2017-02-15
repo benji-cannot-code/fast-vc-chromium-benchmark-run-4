57fc9c45f4060e8242ba1ce1a1dd5887f6cad01b
chrishtr <chrishtr@chromium.org>
2017-02-14 22:56:36 -0800

    Fix GeometryMapper clip caching when there is more than one clip in source transform.
    Previously, the key for the cache was the transform node of the source, which
    is incorrect when there is more than one clip within that transform space.
    
    Also, this CL:
    * Cleans up the cache representation to separate clip and transform
    caches.
    
    * Represents infinite rects explicitly, reducing the incidence of
    the need for infiniteIntRect, which is not reliable. It's expected to use
    this in more places elsewhere in the future.
    
    * Replaces an instance of using an infinite rect with one that does not need it.
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2694513002
    Cr-Commit-Position: refs/heads/master@{#450613}

--- third_party/WebKit/Source/platform/graphics/paint/FloatClipRect.h
+++ third_party/WebKit/Source/platform/graphics/paint/FloatClipRect.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define FloatClipRect_h
 
 #include "platform/geometry/FloatRect.h"
+#include "platform/geometry/LayoutRect.h"
 #include "wtf/Allocator.h"
 
 namespace blink {
@@ -15,23 +16,51 @@ class PLATFORM_EXPORT FloatClipRect {
   USING_FAST_MALLOC(FloatClipRect);
 
  public:
-  FloatClipRect() : m_hasRadius(false) {}
+  FloatClipRect()
+      : m_rect(FloatRect(LayoutRect::infiniteIntRect())),
+        m_hasRadius(false),
+        m_isInfinite(true) {}
 
-  FloatClipRect(const FloatRect& rect) : m_rect(rect), m_hasRadius(false) {}
+  FloatClipRect(const FloatRect& rect)
+      : m_rect(rect), m_hasRadius(false), m_isInfinite(false) {}
 
   const FloatRect& rect() const { return m_rect; }
 
-  void intersect(const FloatRect& other) { m_rect.intersect(other); }
+  void intersect(const FloatRect& other) {
+    if (m_isInfinite) {
+      m_rect = other;
+      m_isInfinite = false;
+    } else {
+      m_rect.intersect(other);
+    }
+  }
 
   bool hasRadius() const { return m_hasRadius; }
-  void setHasRadius(bool hasRadius) { m_hasRadius = hasRadius; }
+  void setHasRadius(bool hasRadius) {
+    m_hasRadius = hasRadius;
+    m_isInfinite = false;
+  }
 
-  void setRect(const FloatRect& rect) { m_rect = rect; }
+  void setRect(const FloatRect& rect) {
+    m_rect = rect;
+    m_isInfinite = false;
+  }
+
+  bool isInfinite() const { return m_isInfinite; }
 
  private:
   FloatRect m_rect;
-  bool m_hasRadius;
+  bool m_hasRadius : 1;
+  bool m_isInfinite : 1;
 };
+
+inline bool operator==(const FloatClipRect& a, const FloatClipRect& b) {
+  if (a.isInfinite() && b.isInfinite())
+    return true;
+  if (!a.isInfinite() && !b.isInfinite())
+    return a.rect() == b.rect() && a.hasRadius() == b.hasRadius();
+  return false;
+}
 
 }  // namespace blink
 
