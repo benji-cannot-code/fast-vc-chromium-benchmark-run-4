e633c3a6bbc2e5dbe4e5a728083e09e03a9f7070
rhalavati <rhalavati@chromium.org>
2017-02-15 12:54:03 -0800

    Network traffic annotation template added to DevToolsUIBindings.
    Network traffic annotation is added to network request of DevToolsUIBindings.
    
    BUG=656607
    
    Review-Url: https://codereview.chromium.org/2696573002
    Cr-Commit-Position: refs/heads/master@{#450793}

--- chrome/browser/devtools/devtools_ui_bindings.cc
+++ chrome/browser/devtools/devtools_ui_bindings.cc
@@ -72,6 +72,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/url_util.h"
 #include "net/cert/x509_certificate.h"
 #include "net/http/http_response_headers.h"
+#include "net/traffic_annotation/network_traffic_annotation.h"
 #include "net/url_request/url_fetcher.h"
 #include "net/url_request/url_fetcher_response_writer.h"
 #include "third_party/WebKit/public/public_features.h"
@@ -701,9 +702,35 @@ void DevToolsUIBindings::LoadNetworkResource(const DispatchCallback& callback,
     callback.Run(&response);
     return;
   }
+  // Create traffic annotation tag.
+  net::NetworkTrafficAnnotationTag traffic_annotation =
+      net::DefineNetworkTrafficAnnotation("devtools_network_resource", R"(
+        semantics {
+          sender: "Developer Tools"
+          description:
+            "When user opens Developer Tools, the browser may fetch additional "
+            "resources from the network to enrich the debugging experience "
+            "(e.g. source map resources)."
+          trigger: "User opens Developer Tools to debug a web page."
+          data: "Any resources requested by Developer Tools"
+          destination: WEBSITE
+        }
+        policy {
+          cookies_allowed: true
+          cookies_store: "user"
+          setting:
+            "It's not possible to disable this feature from settings."
+          policy {
+            DeveloperToolsDisabled {
+              policy_options {mode: MANDATORY}
+              value: true
+            }
+          }
+        })");
 
-  net::URLFetcher* fetcher =
-      net::URLFetcher::Create(gurl, net::URLFetcher::GET, this).release();
+  net::URLFetcher* fetcher = net::URLFetcher::Create(gurl, net::URLFetcher::GET,
+                                                     this, traffic_annotation)
+                                 .release();
   pending_requests_[fetcher] = callback;
   fetcher->SetRequestContext(profile_->GetRequestContext());
   fetcher->SetExtraRequestHeaders(headers);
