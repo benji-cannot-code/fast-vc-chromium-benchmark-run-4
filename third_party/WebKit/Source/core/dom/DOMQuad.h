8e29b0adb40c7f426f58425cb900152f9837f852
hs1217.lee <hs1217.lee@samsung.com>
2017-02-14 21:25:36 -0800

    [GeometryInterface] add getBounds function in DOMQaud.
    The getBounds() method, when invoked,
    must run the following algorithm:
    1.Let bounds be a DOMRect object.
    2.Let left be the minimum of p1.x, p2.x, p3.x and p4.x.
    3.Let top be the minimum of p1.y, p2.y, p3.y and p4.y.
    4.Let right be the maximum of p1.x, p2.x, p3.x and p4.x.
    5.Let bottom be the maximum of p1.y, p2.y, p3.y and p4.y.
    6.Let x coordinate of bounds be left,
     y coordinate of bounds be top,
     width dimension of bounds be right - left
     and height dimension of bounds be bottom - top.
    7.Return bounds.
    
    spec list:
    https://drafts.fxtf.org/geometry/#dom-domquad-getbounds
    
    BUG=678237
    
    Review-Url: https://codereview.chromium.org/2680023003
    Cr-Commit-Position: refs/heads/master@{#450556}

--- third_party/WebKit/Source/core/dom/DOMQuad.h
+++ third_party/WebKit/Source/core/dom/DOMQuad.h
@@ -15,6 +15,7 @@ namespace blink {
 class DOMPoint;
 class DOMPointInit;
 class DOMQuadInit;
+class DOMRect;
 class DOMRectInit;
 
 class CORE_EXPORT DOMQuad : public GarbageCollected<DOMQuad>,
@@ -34,6 +35,8 @@ class CORE_EXPORT DOMQuad : public GarbageCollected<DOMQuad>,
   DOMPoint* p3() const { return m_p3; }
   DOMPoint* p4() const { return m_p4; }
 
+  DOMRect* getBounds();
+
   ScriptValue toJSONForBinding(ScriptState*) const;
 
   DEFINE_INLINE_TRACE() {
@@ -50,10 +53,17 @@ class CORE_EXPORT DOMQuad : public GarbageCollected<DOMQuad>,
           const DOMPointInit& p4);
   DOMQuad(double x, double y, double width, double height);
 
+  void calculateBounds();
+
   Member<DOMPoint> m_p1;
   Member<DOMPoint> m_p2;
   Member<DOMPoint> m_p3;
   Member<DOMPoint> m_p4;
+
+  double m_left;
+  double m_right;
+  double m_top;
+  double m_bottom;
 };
 
 }  // namespace blink
