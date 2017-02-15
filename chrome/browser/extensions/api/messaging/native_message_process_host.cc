1d288107c118ebb2803b57ead4ec18d8377d7279
fdoray <fdoray@chromium.org>
2017-02-14 22:52:37 -0800

    Use TaskScheduler instead of blocking pool in native_message_process_host.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=rdevlin.cronin@chromium.org
    
    Review-Url: https://codereview.chromium.org/2676243002
    Cr-Commit-Position: refs/heads/master@{#450610}

--- chrome/browser/extensions/api/messaging/native_message_process_host.cc
+++ chrome/browser/extensions/api/messaging/native_message_process_host.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/process/kill.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/sequenced_worker_pool.h"
 #include "build/build_config.h"
 #include "chrome/browser/extensions/api/messaging/native_messaging_host_manifest.h"
@@ -71,8 +72,9 @@ NativeMessageProcessHost::~NativeMessageProcessHost() {
     // On OSX base::EnsureProcessTerminated() may block, so we have to post a
     // task on the blocking pool.
 #if defined(OS_MACOSX)
-    content::BrowserThread::PostBlockingPoolTask(
-        FROM_HERE,
+    base::PostTaskWithTraits(
+        FROM_HERE, base::TaskTraits().MayBlock().WithPriority(
+                       base::TaskPriority::BACKGROUND),
         base::Bind(&base::EnsureProcessTerminated, Passed(&process_)));
 #else
     base::EnsureProcessTerminated(std::move(process_));
