8a9eb32b42a88b2056a01c9d72935dce181d02bc
xdai <xdai@chromium.org>
2017-02-15 10:50:54 -0800

    Put the new harmonized dialog implementation behind flag.
    This CL does the following:
    1) Put the harmony work behind the flag --secondary-ui-md so
       nothing is changed for pre-harmony dialogs.
    2) Remove the padding to its right "eye" button.
    3) For harmonized dialog, the size of the dialog is calculated by its
    client view, not from the pre-determined values (as for pre-harmonized dialogs).
    
    BUG=680381
    
    Review-Url: https://codereview.chromium.org/2692403002
    Cr-Commit-Position: refs/heads/master@{#450746}

--- chrome/browser/chromeos/options/network_config_view.cc
+++ chrome/browser/chromeos/options/network_config_view.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/aura/window_event_dispatcher.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/base/material_design/material_design_controller.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/image/image.h"
@@ -238,7 +239,9 @@ void NetworkConfigView::ShowAdvancedView() {
   gfx::Size predefined_size = views::Widget::GetLocalizedContentsSize(
       IDS_JOIN_WIFI_NETWORK_DIALOG_ADVANCED_WIDTH_CHARS,
       IDS_JOIN_WIFI_NETWORK_DIALOG_ADVANCED_MINIMUM_HEIGHT_LINES);
-  size.SetToMax(predefined_size);
+  // Use the pre-determined value to determine its size for non-harmony dialog.
+  if (!ui::MaterialDesignController::IsSecondaryUiMaterial())
+    size.SetToMax(predefined_size);
 
   // Get the new bounds with desired size at the same center point.
   gfx::Rect bounds = GetWidget()->GetWindowBoundsInScreen();
