889895aed370de2953230f5313f8fe16b416147f
sclittle <sclittle@chromium.org>
2017-02-15 15:09:33 -0800

    Don't show a placeholder if the response contains the full image.
    When attempting to load a placeholder image, it's possible that the
    response contains the full image, e.g. if the image is smaller than the
    requested range (e.g. requested the first 2KB of a 1KB image), if the
    server ignored the range request header and returned a full 200
    response, etc.
    
    In these cases, this CL causes the full image to be shown and not get
    treated as a placeholder image, instead of just showing a nondescript
    placeholder image.
    
    In order to detect when range responses contain the full image, this CL
    refactors Content-Range response header parsing into http_util and
    exposes it to Blink.
    
    BUG=657995
    
    Review-Url: https://codereview.chromium.org/2543073002
    Cr-Commit-Position: refs/heads/master@{#450830}

--- third_party/WebKit/Source/platform/network/HTTPParsers.h
+++ third_party/WebKit/Source/platform/network/HTTPParsers.h
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/Vector.h"
 #include "wtf/text/StringHash.h"
 
+#include <stdint.h>
 #include <memory>
 
 namespace blink {
@@ -158,6 +159,18 @@ PLATFORM_EXPORT bool parseMultipartHeadersFromBody(const char* bytes,
 // cause an empty return value.
 PLATFORM_EXPORT std::unique_ptr<JSONArray> parseJSONHeader(const String& header,
                                                            int maxParseDepth);
+
+// Extracts the values in a Content-Range header and returns true if all three
+// values are present and valid for a 206 response; otherwise returns false.
+// The following values will be outputted:
+// |*first_byte_position| = inclusive position of the first byte of the range
+// |*last_byte_position| = inclusive position of the last byte of the range
+// |*instance_length| = size in bytes of the object requested
+// If this method returns false, then all of the outputs will be -1.
+PLATFORM_EXPORT bool parseContentRangeHeaderFor206(const String& contentRange,
+                                                   int64_t* firstBytePosition,
+                                                   int64_t* lastBytePosition,
+                                                   int64_t* instanceLength);
 
 }  // namespace blink
 
