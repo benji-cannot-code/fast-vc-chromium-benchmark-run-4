ddc42f1933c69c5d5c4c68d468670555d17b220e
geofflang <geofflang@chromium.org>
2017-02-15 14:23:42 -0800

    Check for some extensions before calling potentially NULL GL entry points.
    Some extensions are exposed by the command buffer unconditionally, add
    some tracking of which extensions are natively supported and call the
    correct entry point.
    
    BUG=602737
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2689203002
    Cr-Commit-Position: refs/heads/master@{#450816}

--- gpu/command_buffer/service/gles2_cmd_decoder_passthrough.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder_passthrough.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "gpu/command_buffer/service/gles2_cmd_decoder_passthrough.h"
 
+#include "base/strings/string_split.h"
 #include "gpu/command_buffer/service/feature_info.h"
 #include "gpu/command_buffer/service/gl_utils.h"
 #include "ui/gl/gl_version_info.h"
@@ -174,7 +175,9 @@ bool GLES2DecoderPassthroughImpl::Initialize(
 
   // Check for required extensions
   if (!feature_info_->feature_flags().angle_robust_client_memory ||
-      !feature_info_->feature_flags().chromium_bind_generates_resource) {
+      !feature_info_->feature_flags().chromium_bind_generates_resource ||
+      !feature_info_->feature_flags().chromium_copy_texture ||
+      !feature_info_->feature_flags().chromium_copy_compressed_texture) {
     // TODO(geofflang): Verify that ANGLE_webgl_compatibility is enabled if this
     // is a WebGL context (depends on crbug.com/671217).
     Destroy(true);
