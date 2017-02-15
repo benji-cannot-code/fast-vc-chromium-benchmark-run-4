f1114ce693790b08db20bd024a2fb278f6fd0be0
tzik <tzik@chromium.org>
2017-02-15 02:52:28 -0800

    Add missing #include around SequencedWorkerPool in //components
    This is a //components part of https://codereview.chromium.org/2690533002/ PS6.
    
    These files depend on indirectly-included sequenced_worker_pool.h, which is being removed.
    
    TBR=blundell@chromium.org
    
    Review-Url: https://codereview.chromium.org/2691353003
    Cr-Commit-Position: refs/heads/master@{#450659}

--- components/ntp_tiles/popular_sites_impl.h
+++ components/ntp_tiles/popular_sites_impl.h
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 namespace base {
+class TaskRunner;
+class SequencedWorkerPool;
 class Value;
 }
 
