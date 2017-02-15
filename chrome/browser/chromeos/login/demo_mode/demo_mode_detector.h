8828d91de2e823660dc63c843e78689b7269a963
jdufault <jdufault@chromium.org>
2017-02-15 11:19:37 -0800

    cros: Move DemoModeDetector out of webui directory.
    TBR=pam@chromium.org
    BUG=672142
    
    Review-Url: https://codereview.chromium.org/2692793007
    Cr-Commit-Position: refs/heads/master@{#450763}

--- chrome/browser/chromeos/login/demo_mode/demo_mode_detector.h
+++ chrome/browser/chromeos/login/demo_mode/demo_mode_detector.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_WEBUI_CHROMEOS_LOGIN_DEMO_MODE_DETECTOR_H_
-#define CHROME_BROWSER_UI_WEBUI_CHROMEOS_LOGIN_DEMO_MODE_DETECTOR_H_
+#ifndef CHROME_BROWSER_CHROMEOS_LOGIN_DEMO_MODE_DEMO_MODE_DETECTOR_H_
+#define CHROME_BROWSER_CHROMEOS_LOGIN_DEMO_MODE_DEMO_MODE_DETECTOR_H_
 
 #include <memory>
 #include <string>
@@ -56,7 +56,7 @@ class DemoModeDetector {
   // Time between updating our total time on oobe.
   base::TimeDelta oobe_timer_update_interval_;
 
-  bool demo_launched_;
+  bool demo_launched_ = false;
 
   base::WeakPtrFactory<DemoModeDetector> weak_ptr_factory_;
 
@@ -65,4 +65,4 @@ class DemoModeDetector {
 
 }  // namespace chromeos
 
-#endif  // CHROME_BROWSER_UI_WEBUI_CHROMEOS_LOGIN_DEMO_MODE_DETECTOR_H_
+#endif  // CHROME_BROWSER_CHROMEOS_LOGIN_DEMO_MODE_DEMO_MODE_DETECTOR_H_
