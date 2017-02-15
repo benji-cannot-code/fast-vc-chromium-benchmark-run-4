96882a9191b74fd0d559702b659af2cf0db8016c
bajones <bajones@chromium.org>
2017-02-14 22:48:58 -0800

    Optimize VRShell renderers
    Changes the renderers to use GPU-side vertex buffers instead of client-side
    arrays. This is probably a pretty minor optimization, but it's generally good
    practice in OpenGL and it never hurts to give the page content more wiggle room.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2694833004
    Cr-Commit-Position: refs/heads/master@{#450607}

--- chrome/browser/android/vr_shell/vr_shell_renderer.h
+++ chrome/browser/android/vr_shell/vr_shell_renderer.h
@@ -54,7 +54,9 @@ class BaseRenderer {
   virtual ~BaseRenderer();
 
  protected:
-  BaseRenderer(ShaderID vertex_id, ShaderID fragment_id);
+  BaseRenderer(ShaderID vertex_id,
+               ShaderID fragment_id,
+               bool setup_vertex_buffer);
 
   void PrepareToDraw(GLuint view_proj_matrix_handle,
                      const gvr::Mat4f& view_proj_matrix);
@@ -62,6 +64,7 @@ class BaseRenderer {
   GLuint program_handle_;
   GLuint position_handle_;
   GLuint tex_coord_handle_;
+  GLuint vertex_buffer_;
 
   DISALLOW_COPY_AND_ASSIGN(BaseRenderer);
 };
@@ -100,7 +103,6 @@ class WebVrRenderer : public BaseRenderer {
   static constexpr size_t VERTEX_OFFSET = 0;
 
   GLuint tex_uniform_handle_;
-  GLuint vertex_buffer_;
 
   DISALLOW_COPY_AND_ASSIGN(WebVrRenderer);
 };
