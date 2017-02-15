5e1767e62a29b4828c48489acf236fd7873f795f
jhorwich <jhorwich@chromium.org>
2017-02-15 00:57:17 -0800

    Include sequenced_worker_pool.h to fix build.
    https://codereview.chromium.org/2690533002 removed an implicit
    include, and https://codereview.chromium.org/2671263002 was
    reverted, so wizard_controller.cc stopped building on some
    build configurations.
    
    This includes the needed file sequenced_worker_pool.h to fix the
    build - this can be removed once SeqeuncedWorkerPool is deprecated.
    
    BUG=667892
    TEST=simplechrome compile BOARD=lumpy
    
    Review-Url: https://codereview.chromium.org/2698503002
    Cr-Commit-Position: refs/heads/master@{#450641}

--- chrome/browser/chromeos/login/wizard_controller.cc
+++ chrome/browser/chromeos/login/wizard_controller.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/threading/sequenced_worker_pool.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_process_platform_part.h"
