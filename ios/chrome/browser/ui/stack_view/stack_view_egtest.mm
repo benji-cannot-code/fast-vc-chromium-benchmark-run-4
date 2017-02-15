9dbef8ec1c9d28ca8c8ad243fb7a0989590c070e
stkhapugin <stkhapugin@chromium.org>
2017-02-15 11:48:55 -0800

    [ObjC ARC] Converts ios/chrome/browser/ui/stack_view:eg_tests to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2681463002
    Cr-Commit-Position: refs/heads/master@{#450779}

--- ios/chrome/browser/ui/stack_view/stack_view_egtest.mm
+++ ios/chrome/browser/ui/stack_view/stack_view_egtest.mm
@@ -26,6 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
 #include "ios/testing/earl_grey/disabled_test_macros.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace {
 // Returns a GREYMatcher that matches |view|.
 // TODO(crbug.com/642619): Evaluate whether this should be shared code.
