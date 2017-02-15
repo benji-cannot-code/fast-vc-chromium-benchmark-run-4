15c886dd1e7c3705ff68fd8da1a77b75e58099d5
sammiequon <sammiequon@chromium.org>
2017-02-14 22:46:23 -0800

    MD Settings: Make fingerprint settings use fingerprint browser proxy.
    This CL moves the manual testing code to fingerprint_handler.* and replaces all the local array stuff in fingerprint_list.js with chrome.send via fingerprint browser proxy.
    
    TEST=none
    BUG=672269
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2681943003
    Cr-Commit-Position: refs/heads/master@{#450605}

--- chrome/browser/resources/settings/people_page/fingerprint_browser_proxy.js
+++ chrome/browser/resources/settings/people_page/fingerprint_browser_proxy.js
@@ -29,13 +29,24 @@ settings.FingerprintResultType = {
  */
 settings.FingerprintScan;
 
+/**
+ * An object describing the necessary info to display on the fingerprint
+ * settings. The structure of this data must be kept in sync with
+ * C++ FingerprintHandler.
+ * @typedef {{
+ *   fingerprintsList: Array<number>,
+ *   isMaxed: boolean,
+ * }}
+ */
+settings.FingerprintInfo;
+
 cr.define('settings', function() {
   /** @interface */
   function FingerprintBrowserProxy() {}
 
   FingerprintBrowserProxy.prototype = {
     /**
-     * @return {!Promise<!Array<string>>}
+     * @return {!Promise<!Array<!settings.FingerprintInfo>>}
      */
     getFingerprintsList: function () {},
 
@@ -51,7 +62,7 @@ cr.define('settings', function() {
 
     /**
      * @param {number} index
-     * @return {!Promise<!Array<string>>}
+     * @return {!Promise<boolean>}
      */
     removeEnrollment: function(index) {},
 
@@ -64,6 +75,13 @@ cr.define('settings', function() {
     startAuthentication: function() {},
 
     endCurrentAuthentication: function() {},
+
+    /**
+     * TODO(sammiequon): Temporary function to let the handler know when a
+     * completed scan has been sent via click on the setup fingerprint dialog.
+     * Remove this when real scans are implemented.
+     */
+    fakeScanComplete: function() {},
   };
 
   /**
@@ -112,6 +130,11 @@ cr.define('settings', function() {
     /** @override */
     endCurrentAuthentication: function() {
       chrome.send('endCurrentAuthentication');
+    },
+
+    /** @override */
+    fakeScanComplete: function() {
+      chrome.send('fakeScanComplete');
     },
   };
 
--- chrome/browser/ui/webui/settings/chromeos/fingerprint_handler.h
+++ chrome/browser/ui/webui/settings/chromeos/fingerprint_handler.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_SETTINGS_CHROMEOS_FINGERPRINT_HANDLER_H_
 #define CHROME_BROWSER_UI_WEBUI_SETTINGS_CHROMEOS_FINGERPRINT_HANDLER_H_
 
+#include "base/strings/string16.h"
 #include "chrome/browser/ui/webui/settings/settings_page_ui_handler.h"
 
 namespace base {
@@ -35,6 +36,13 @@ class FingerprintHandler : public ::settings::SettingsPageUIHandler {
   void HandleChangeEnrollmentLabel(const base::ListValue* args);
   void HandleStartAuthentication(const base::ListValue* args);
   void HandleEndCurrentAuthentication(const base::ListValue* args);
+
+  // TODO(sammiequon): Remove this when we can hook up to the fingerprint mojo
+  // service. This is used to help manual testing in the meantime.
+  void HandleFakeScanComplete(const base::ListValue* args);
+
+  // TODO(sammiequon): Remove this when HandleFakeScanComplete is removed.
+  std::vector<base::string16> fingerprints_list_;
 
   DISALLOW_COPY_AND_ASSIGN(FingerprintHandler);
 };
