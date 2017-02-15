34b92901b94c578dd7b5ebd7ace3c857e0c5a72d
stkhapugin <stkhapugin@chromium.org>
2017-02-15 11:08:04 -0800

    [ObjC ARC] Converts ios/chrome/browser/ui/find_bar:eg_tests to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2676253003
    Cr-Commit-Position: refs/heads/master@{#450755}

--- ios/chrome/browser/ui/find_bar/find_in_page_egtest.mm
+++ ios/chrome/browser/ui/find_bar/find_in_page_egtest.mm
@@ -23,6 +23,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/test/http_server_util.h"
 #include "ui/base/l10n/l10n_util_mac.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace {
 
 // Test web page content.
