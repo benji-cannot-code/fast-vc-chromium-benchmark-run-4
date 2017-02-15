2f02b4509d69172f7268d8ba4efcbf32209de217
samans <samans@chromium.org>
2017-02-14 21:28:28 -0800

    CopyOutputResult must have a working release callback when received over mojo
    Right now, CopyOutputResult when received over mojo has an empty release
    callback. This CL introduces TextureMailboxReleaser, which is a mojo
    interface that has a Release method with the same signature as
    ReleaseCallback. On the sender side, the ReleaseCallback of the
    CopyOutputResult is retained in a TextureMailboxReleaserImpl and
    a TextureMailboxReleaserPtr will be sent to the receiver. When
    the receiver calls the Release method, the sender calls the release
    callback.
    
    BUG=672071
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2686833003
    Cr-Commit-Position: refs/heads/master@{#450558}

--- cc/ipc/copy_output_result_struct_traits.h
+++ cc/ipc/copy_output_result_struct_traits.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CC_IPC_COPY_OUTPUT_RESULT_STRUCT_TRAITS_H_
 
 #include "cc/ipc/copy_output_result.mojom-shared.h"
+#include "cc/ipc/texture_mailbox_releaser.mojom.h"
 #include "cc/ipc/texture_mailbox_struct_traits.h"
 #include "cc/output/copy_output_result.h"
 #include "skia/public/interfaces/bitmap_skbitmap_struct_traits.h"
@@ -29,6 +30,9 @@ struct StructTraits<cc::mojom::CopyOutputResultDataView,
       const std::unique_ptr<cc::CopyOutputResult>& result) {
     return result->texture_mailbox_;
   }
+
+  static cc::mojom::TextureMailboxReleaserPtr releaser(
+      const std::unique_ptr<cc::CopyOutputResult>& result);
 
   static bool Read(cc::mojom::CopyOutputResultDataView data,
                    std::unique_ptr<cc::CopyOutputResult>* out_p);
