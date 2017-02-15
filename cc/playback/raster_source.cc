8d5ebe93d82f652083ed175fe4b9f96f1c72441c
reed <reed@google.com>
2017-02-15 07:56:18 -0800

    remove unneeded include
    This will allow https://skia-review.googlesource.com/c/8481/ to land
    
    BUG=
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    TBR=vmpstr
    
    Review-Url: https://codereview.chromium.org/2696103002
    Cr-Commit-Position: refs/heads/master@{#450714}

--- cc/playback/raster_source.cc
+++ cc/playback/raster_source.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/playback/skip_image_canvas.h"
 #include "skia/ext/analysis_canvas.h"
 #include "third_party/skia/include/core/SkCanvas.h"
-#include "third_party/skia/include/core/SkClipStack.h"
 #include "third_party/skia/include/core/SkPictureRecorder.h"
 #include "ui/gfx/geometry/rect_conversions.h"
 
