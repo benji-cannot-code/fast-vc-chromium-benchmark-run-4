e6a2abd46ae46f4b5762e789db7f4dbab6be3e17
dpapad <dpapad@chromium.org>
2017-02-14 23:54:14 -0800

    MD Settings: Disable tests for ASAN bots.
    JS unit tests do not exercise the C++ handlers, so there is no value in running
    them in ASAN bots.
    
    BUG=691965
    
    Review-Url: https://codereview.chromium.org/2690303003
    Cr-Commit-Position: refs/heads/master@{#450632}

--- chrome/test/data/webui/settings/cr_settings_browsertest.js
+++ chrome/test/data/webui/settings/cr_settings_browsertest.js
@@ -1004,13 +1004,6 @@ TEST_F('CrSettingsNonExistentRouteTest', 'All', function() {
   mocha.run();
 });
 
-// Hangs on ASAN builder for unknown reasons. TODO(michaelpg): Find reason.
-GEN('#if defined(ADDRESS_SANITIZER)');
-GEN('#define MAYBE_All DISABLED_All');
-GEN('#else');
-GEN('#define MAYBE_All All');
-GEN('#endif');
-
 /**
  * @constructor
  * @extends {SettingsPageBrowserTest}
@@ -1027,7 +1020,7 @@ CrSettingsRouteDynamicParametersTest.prototype = {
   runAccessibilityChecks: false,
 };
 
-TEST_F('CrSettingsRouteDynamicParametersTest', 'MAYBE_All', function() {
+TEST_F('CrSettingsRouteDynamicParametersTest', 'All', function() {
   suite('DynamicParameters', function() {
     test('get parameters from URL and navigation', function(done) {
       assertEquals(settings.Route.PEOPLE, settings.getCurrentRoute());
