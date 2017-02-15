1f941930bfb36f8d192e6bcf5eae98d91c0422e3
perkj <perkj@chromium.org>
2017-02-14 23:35:29 -0800

    Revert of RELAND: ShapeDetection: use mojom::Bitmap for mojo interface. (patchset #4 id:180001 of https://codereview.chromium.org/2681913003/ )
    Reason for revert:
    Sorry - but this breaks blink layout tests on mac:
    
    https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Mac10.11/builds/16336
    
    Original issue's description:
    > RELAND: ShapeDetection: use mojom::Bitmap for mojo interface.
    >
    > Original CL got reverted due to conflit with another landed CL that need
    > to use mojom::bitmap.
    >
    > Original CL description ------------------------------------------------
    > ShapeDetection: use mojom::Bitmap for mojo interface.
    >
    > This CL uses mojo::Bitmap for mojo ShapeDetection interfaces definition,
    > so that the Detect API is completely flexible and friendly.
    >
    > BUG=665488
    > TEST(Layout)=
    >  third_party/WebKit/LayoutTests/fast/shapedetection/shapedetection-creation.html
    >  third_party/WebKit/LayoutTests/fast/shapedetection/shapedetection-empty-input.html
    >  third_party/WebKit/LayoutTests/shapedetection/detection-HTMLCanvasElement.html
    >  third_party/WebKit/LayoutTests/shapedetection/detection-HTMLImageElement.html
    >  third_party/WebKit/LayoutTests/shapedetection/detection-HTMLVideoElement.html
    >  third_party/WebKit/LayoutTests/shapedetection/detection-ImageBitmap.html
    >  third_party/WebKit/LayoutTests/shapedetection/detection-ImageData.html
    >
    > Review-Url: https://codereview.chromium.org/2629433003
    > Cr-Commit-Position: refs/heads/master@{#446142}
    > (cherry picked from commit 33adf4c85dd553eb607bcf9c02c5fa60c8738a68)
    >
    > Conflicts:
    > 	third_party/WebKit/Source/modules/shapedetection/FaceDetector.cpp
    > 	third_party/WebKit/Source/modules/shapedetection/TextDetector.cpp
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.mac:mac_optional_gpu_tests_rel
    >
    > Review-Url: https://codereview.chromium.org/2681913003
    > Cr-Commit-Position: refs/heads/master@{#450582}
    > Committed: https://chromium.googlesource.com/chromium/src/+/ab1a9331a441fa315edb669486ac1964788e47c7
    
    TBR=rockot@chromium.org,dtrainor@chromium.org,haraken@chromium.org,leon.han@intel.com,mcasas@chromium.org,tsepez@chromium.org,xianglu@chromium.org,reed@google.com,bsalomon@google.com,junwei.fu@intel.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=665488
    
    Review-Url: https://codereview.chromium.org/2696143002
    Cr-Commit-Position: refs/heads/master@{#450625}

--- services/shape_detection/barcode_detection_impl_mac.h
+++ services/shape_detection/barcode_detection_impl_mac.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/mac/scoped_nsobject.h"
 #include "services/shape_detection/public/interfaces/barcodedetection.mojom.h"
-#include "third_party/skia/include/core/SkBitmap.h"
 
 @class CIDetector;
 
@@ -20,7 +19,9 @@ class BarcodeDetectionImplMac
   BarcodeDetectionImplMac();
   ~BarcodeDetectionImplMac() override;
 
-  void Detect(const SkBitmap& bitmap,
+  void Detect(mojo::ScopedSharedBufferHandle frame_data,
+              uint32_t width,
+              uint32_t height,
               const shape_detection::mojom::BarcodeDetection::DetectCallback&
                   callback) override;
 
--- services/shape_detection/face_detection_impl_mac.h
+++ services/shape_detection/face_detection_impl_mac.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/mac/scoped_nsobject.h"
 #include "services/shape_detection/public/interfaces/facedetection.mojom.h"
-#include "third_party/skia/include/core/SkBitmap.h"
 
 @class CIDetector;
 
@@ -20,7 +19,9 @@ class FaceDetectionImplMac : public shape_detection::mojom::FaceDetection {
       shape_detection::mojom::FaceDetectorOptionsPtr options);
   ~FaceDetectionImplMac() override;
 
-  void Detect(const SkBitmap& bitmap,
+  void Detect(mojo::ScopedSharedBufferHandle frame_data,
+              uint32_t width,
+              uint32_t height,
               const shape_detection::mojom::FaceDetection::DetectCallback&
                   callback) override;
 
