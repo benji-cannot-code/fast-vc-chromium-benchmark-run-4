62d56ffd8add54ea88eaaaaa62e78ac8a50a1305
khmel <khmel@chromium.org>
2017-02-16 14:24:21 -0800

    arc: Fix crash on accessing app info for secondary user.
    This fix the crash whem user activates Chrome app info page
    for secondary user profile. This alse adds unit test that
    cover this case and generic functionality.
    
    TEST=Manually, unit tests extended
    BUG=689263
    
    Review-Url: https://codereview.chromium.org/2700523002
    Cr-Commit-Position: refs/heads/master@{#451118}

--- chrome/browser/ui/views/apps/app_info_dialog/app_info_dialog_views.cc
+++ chrome/browser/ui/views/apps/app_info_dialog/app_info_dialog_views.cc
@@ -36,6 +36,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/chromeos/arc/arc_session_manager.h"
+#include "chrome/browser/ui/app_list/arc/arc_app_list_prefs.h"
+#include "chrome/browser/ui/app_list/arc/arc_app_utils.h"
 #include "chrome/browser/ui/views/apps/app_info_dialog/arc_app_info_links_panel.h"
 #endif
 
@@ -111,12 +113,7 @@ void ShowAppInfoInNativeDialog(content::WebContents* web_contents,
 AppInfoDialog::AppInfoDialog(gfx::NativeWindow parent_window,
                              Profile* profile,
                              const extensions::Extension* app)
-    : dialog_header_(NULL),
-      dialog_body_(NULL),
-      dialog_footer_(NULL),
-      profile_(profile),
-      app_id_(app->id()),
-      extension_registry_(NULL) {
+    : profile_(profile), app_id_(app->id()) {
   views::BoxLayout* layout =
       new views::BoxLayout(views::BoxLayout::kVertical, 0, 0, 0);
   SetLayoutManager(layout);
@@ -147,10 +144,17 @@ AppInfoDialog::AppInfoDialog(gfx::NativeWindow parent_window,
   dialog_body_contents->AddChildView(new AppInfoPermissionsPanel(profile, app));
 
 #if defined(OS_CHROMEOS)
-  // When ARC is enabled, show the "Manage supported links" link for Chrome.
-  if (arc::ArcSessionManager::Get()->IsArcPlayStoreEnabled() &&
-      app->id() == extension_misc::kChromeAppId)
-    dialog_body_contents->AddChildView(new ArcAppInfoLinksPanel(profile, app));
+  // When ARC is enabled and the Settings app is available, show the
+  // "Manage supported links" link for Chrome.
+  if (app->id() == extension_misc::kChromeAppId &&
+      arc::ArcSessionManager::Get()->IsArcPlayStoreEnabled()) {
+    const ArcAppListPrefs* arc_app_list_prefs = ArcAppListPrefs::Get(profile);
+    if (arc_app_list_prefs &&
+        arc_app_list_prefs->IsRegistered(arc::kSettingsAppId)) {
+      arc_app_info_links_ = new ArcAppInfoLinksPanel(profile, app);
+      dialog_body_contents->AddChildView(arc_app_info_links_);
+    }
+  }
 #endif
 
   // Clip the scrollable view so that the scrollbar appears. As long as this
