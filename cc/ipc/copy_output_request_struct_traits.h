462994d87b83e95134b329ec60d347e35ff4ba35
samans <samans@chromium.org>
2017-02-16 11:51:54 -0800

    CopyOutputRequest must have a working result_callback_ when received over mojo
    BUG=672071
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2700533002
    Cr-Commit-Position: refs/heads/master@{#451064}

--- cc/ipc/copy_output_request_struct_traits.h
+++ cc/ipc/copy_output_request_struct_traits.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CC_IPC_COPY_OUTPUT_REQUEST_STRUCT_TRAITS_H_
 #define CC_IPC_COPY_OUTPUT_REQUEST_STRUCT_TRAITS_H_
 
-#include "cc/ipc/copy_output_request.mojom-shared.h"
+#include "cc/ipc/copy_output_request.mojom.h"
 #include "cc/ipc/texture_mailbox_struct_traits.h"
 #include "cc/output/copy_output_request.h"
 #include "mojo/common/common_custom_types_struct_traits.h"
@@ -36,6 +36,9 @@ struct StructTraits<cc::mojom::CopyOutputRequestDataView,
       const std::unique_ptr<cc::CopyOutputRequest>& request) {
     return request->texture_mailbox_;
   }
+
+  static cc::mojom::CopyOutputResultSenderPtr result_sender(
+      const std::unique_ptr<cc::CopyOutputRequest>& request);
 
   static bool Read(cc::mojom::CopyOutputRequestDataView data,
                    std::unique_ptr<cc::CopyOutputRequest>* out_p);
