352e0ade8cd983cb56be0ed4a89404d3180430f1
dgozman <dgozman@chromium.org>
2017-02-16 02:42:52 -0800

    [DevTools] Make profiles sidebar section non-collapsible.
    BUG=691994
    
    Review-Url: https://codereview.chromium.org/2698783002
    Cr-Commit-Position: refs/heads/master@{#450927}

--- third_party/WebKit/Source/devtools/front_end/profiler/ProfilesPanel.js
+++ third_party/WebKit/Source/devtools/front_end/profiler/ProfilesPanel.js
@@ -454,6 +454,7 @@ Profiler.ProfileTypeSidebarSection = class extends UI.TreeElement {
     this._profileGroups = {};
     this.expand();
     this.hidden = true;
+    this.setCollapsible(false);
   }
 
   /**
