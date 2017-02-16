a0ffb9153b7568ee287ee8d7efbf5b839509d2be
atotic <atotic@chromium.org>
2017-02-15 22:34:15 -0800

    Fix orthogonal mode legacy mismatch
    Orthogonal styles currently assert inside legacy layout, because width is -1.
    Ex:
    <div style="writing-mode:vertical-rl; border:1px solid red;">
      <p style="writing-mode:horizontal-tb; border:1px solid blue;">Hello, world.</p>
    </div>
    
    These two routines did not handle orthogonal modes properly.
    
    NGConstraintSpace::CreateFromLayoutObject
    NGBlockNode::RunOldLayout
    
    I am not sure if this fix is correct, it is my best guess. With the fix, assert
    is avoided, but <p> is too tall. The constraint space used for layout looks correct,
    bug might be something else.
    
    Meta question: Should we bother with legacy orthogonal modes at all?
    I've stumbled upon several other orthogonal mode bugs.
    
    BUG=635619
    
    [ng_orthogonal_legacy]
    
    Review-Url: https://codereview.chromium.org/2691093004
    Cr-Commit-Position: refs/heads/master@{#450878}

--- third_party/WebKit/Source/core/layout/ng/ng_writing_mode.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_writing_mode.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/layout/ng/ng_writing_mode.h"
 
+#include "core/layout/LayoutObject.h"
 #include "wtf/Assertions.h"
 
 namespace blink {
@@ -21,6 +22,10 @@ NGWritingMode FromPlatformWritingMode(WritingMode mode) {
       NOTREACHED();
       return kHorizontalTopBottom;
   }
+}
+
+bool IsParallelWritingMode(NGWritingMode a, NGWritingMode b) {
+  return (a == kHorizontalTopBottom) == (b == kHorizontalTopBottom);
 }
 
 }  // namespace blink
