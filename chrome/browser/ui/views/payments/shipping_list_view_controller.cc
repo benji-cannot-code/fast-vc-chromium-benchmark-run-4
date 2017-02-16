b8229042623cc8afca7c282e6b778c93d428b556
tmartino <tmartino@chromium.org>
2017-02-16 15:58:57 -0800

    [WebPayments] Updating Shipping List to use PaymentRequestRowView
    BUG=679449
    
    Review-Url: https://codereview.chromium.org/2700713005
    Cr-Commit-Position: refs/heads/master@{#451143}

--- chrome/browser/ui/views/payments/shipping_list_view_controller.cc
+++ chrome/browser/ui/views/payments/shipping_list_view_controller.cc
@@ -9,11 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "chrome/browser/ui/views/payments/payment_request_dialog_view.h"
+#include "chrome/browser/ui/views/payments/payment_request_row_view.h"
 #include "chrome/browser/ui/views/payments/payment_request_views_util.h"
 #include "components/payments/payment_request.h"
 #include "components/strings/grit/components_strings.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/views/layout/box_layout.h"
+#include "ui/views/layout/grid_layout.h"
 
 namespace payments {
 
@@ -37,8 +39,8 @@ std::unique_ptr<views::View> ShippingListViewController::CreateView() {
   for (auto* profile : request()->shipping_profiles()) {
     // TODO(tmartino): Pass an actual locale in place of empty string.
     content_view->AddChildView(
-        GetShippingAddressLabel(AddressStyleType::DETAILED, std::string(),
-                                *profile)
+        CreateAddressRow(GetShippingAddressLabel(AddressStyleType::DETAILED,
+                                                 std::string(), *profile))
             .release());
   }
 
@@ -48,6 +50,30 @@ std::unique_ptr<views::View> ShippingListViewController::CreateView() {
           l10n_util::GetStringUTF16(IDS_PAYMENT_REQUEST_SHIPPING_SECTION_NAME),
           this),
       std::move(content_view));
+}
+
+std::unique_ptr<views::Button> ShippingListViewController::CreateAddressRow(
+    std::unique_ptr<views::View> content) {
+  std::unique_ptr<PaymentRequestRowView> row =
+      base::MakeUnique<PaymentRequestRowView>(this);
+  views::GridLayout* layout = new views::GridLayout(row.get());
+  row->SetLayoutManager(layout);
+
+  layout->SetInsets(
+      kPaymentRequestRowVerticalInsets, kPaymentRequestRowHorizontalInsets,
+      kPaymentRequestRowVerticalInsets,
+      kPaymentRequestRowHorizontalInsets + kPaymentRequestRowExtraRightInset);
+
+  // Add a column listing the address.
+  views::ColumnSet* columns = layout->AddColumnSet(0);
+  columns->AddColumn(views::GridLayout::FILL, views::GridLayout::FILL, 1,
+                     views::GridLayout::USE_PREF, 0, 0);
+
+  layout->StartRow(0, 0);
+  content->set_can_process_events_within_subtree(false);
+  layout->AddView(content.release());
+
+  return std::move(row);
 }
 
 }  // namespace payments
