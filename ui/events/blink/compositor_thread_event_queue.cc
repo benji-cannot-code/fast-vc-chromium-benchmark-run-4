eb48097464d167a55c22728e0244d5a39aaec190
chongz <chongz@chromium.org>
2017-02-16 12:45:33 -0800

    [VSync Queue] Add tracing for event queuing time and coalesced count
    We need these tracing to understand when the events are push and
    popped, and how many events are coalesced together.
    
    The tracing works as follow:
      1. For each new event, we first check if it could be coalesced with
        existing events or have to be pushed to the end
      2. If it could be coalesced, we simply coalesced it
      3. Otherwise we push and begin a new tracing with
        |id == new_event->first_original_event()|
      4. Each time an queue was popped we end the tracing with
        |id == new_event->first_original_event()|
    
    Example flow of original events:
    
    Case 1 - |ScrollUpdate| + |ScrollUpdate| => |ScrollUpdate|
    Original Events - [o1, o2] + [o3, o4] => [o1, o2, o3, o4]
    
    Case 2 - |PinchUpdate| + |ScrollUpdate| =>
                 |ScrollUpdate| + |PinchUpdate|
    Original Events - [o1, o2] + [o3, o4] =>
                 [] + [o1, o2, o3, o4]
    
    Please see bug for an example tracing picture.
    
    BUG=636332
    
    Review-Url: https://codereview.chromium.org/2695603004
    Cr-Commit-Position: refs/heads/master@{#451094}

--- ui/events/blink/compositor_thread_event_queue.cc
+++ ui/events/blink/compositor_thread_event_queue.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/blink/compositor_thread_event_queue.h"
 
 #include "base/memory/ptr_util.h"
+#include "base/trace_event/trace_event.h"
 #include "ui/events/blink/blink_event_util.h"
 #include "ui/events/blink/web_input_event_traits.h"
 
@@ -21,6 +22,13 @@ void CompositorThreadEventQueue::Queue(
   if (queue_.empty() || !IsContinuousGestureEvent(new_event->event().type()) ||
       !IsCompatibleScrollorPinch(ToWebGestureEvent(new_event->event()),
                                  ToWebGestureEvent(queue_.back()->event()))) {
+    if (new_event->first_original_event()) {
+      // Trace could be nested as there might be multiple events in queue.
+      // e.g. |ScrollUpdate|, |ScrollEnd|, and another scroll sequence.
+      TRACE_EVENT_NESTABLE_ASYNC_BEGIN0("input",
+                                        "CompositorThreadEventQueue::Queue",
+                                        new_event->first_original_event());
+    }
     queue_.emplace_back(std::move(new_event));
     return;
   }
@@ -88,6 +96,13 @@ std::unique_ptr<EventWithCallback> CompositorThreadEventQueue::Pop() {
   if (!queue_.empty()) {
     result = std::move(queue_.front());
     queue_.pop_front();
+  }
+
+  if (result->first_original_event()) {
+    TRACE_EVENT_NESTABLE_ASYNC_END2(
+        "input", "CompositorThreadEventQueue::Queue",
+        result->first_original_event(), "type", result->event().type(),
+        "coalesced_count", result->coalesced_count());
   }
   return result;
 }
