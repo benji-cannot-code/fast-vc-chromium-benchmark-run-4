a8f7177aeb2b41ebe1b117147c62eb3fe5c78005
alph <alph@chromium.org>
2017-02-16 11:20:36 -0800

    DevTools: Do not do deep search in case of bottom-up profile tree.
    Perform the search among top level items only.
    
    Review-Url: https://codereview.chromium.org/2703433002
    Cr-Commit-Position: refs/heads/master@{#451044}

--- third_party/WebKit/Source/devtools/front_end/timeline_model/TimelineProfileTree.js
+++ third_party/WebKit/Source/devtools/front_end/timeline_model/TimelineProfileTree.js
@@ -50,6 +50,20 @@ TimelineModel.TimelineProfileTree.Node = class {
   children() {
     throw 'Not implemented';
   }
+
+  /**
+   * @param {function(!SDK.TracingModel.Event):boolean} matchFunction
+   * @param {!Array<!TimelineModel.TimelineProfileTree.Node>=} results
+   * @return {!Array<!TimelineModel.TimelineProfileTree.Node>}
+   */
+  searchTree(matchFunction, results) {
+    results = results || [];
+    if (this.event && matchFunction(this.event))
+      results.push(this);
+    for (var child of this.children().values())
+      child.searchTree(matchFunction, results);
+    return results;
+  }
 };
 
 TimelineModel.TimelineProfileTree.TopDownNode = class extends TimelineModel.TimelineProfileTree.Node {
@@ -505,6 +519,19 @@ TimelineModel.TimelineProfileTree.BottomUpNode = class extends TimelineModel.Tim
 
     this._cachedChildren = nodeById;
     return nodeById;
+  }
+
+  /**
+   * @override
+   * @param {function(!SDK.TracingModel.Event):boolean} matchFunction
+   * @param {!Array<!TimelineModel.TimelineProfileTree.Node>=} results
+   * @return {!Array<!TimelineModel.TimelineProfileTree.Node>}
+   */
+  searchTree(matchFunction, results) {
+    results = results || [];
+    if (this.event && matchFunction(this.event))
+      results.push(this);
+    return results;
   }
 };
 
