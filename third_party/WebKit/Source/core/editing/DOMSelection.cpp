9d51d9377787e5c0a6a7303620eca3df3dacc6f0
tkent <tkent@chromium.org>
2017-02-15 22:24:59 -0800

    Selection API: setBaseAndExtent() and selectAllChildren() should create new Range based on the specified nodes.
    * ConsoleViewport._textOffsetInNode():
      It supported only Text node |container|. This CL adds Element support.
    
    This CL removes the following tests because they are covered
    by external/wpt/selection/:
     - editing/selection/getRangeAt.html
     - editing/selection/select_all/select_all_html.html
     - editing/selection/set_base_and_extent/set_base_and_extent_html.html
    
    This CL fixes 2056 sub-tests in external/wpt/selection/.
    
    BUG=692400
    
    Review-Url: https://codereview.chromium.org/2695193002
    Cr-Commit-Position: refs/heads/master@{#450876}

--- third_party/WebKit/Source/core/editing/DOMSelection.cpp
+++ third_party/WebKit/Source/core/editing/DOMSelection.cpp
@@ -75,6 +75,13 @@ const VisibleSelection& DOMSelection::visibleSelection() const {
   return frame()->selection().selection();
 }
 
+bool DOMSelection::isBaseFirstInSelection() const {
+  DCHECK(frame());
+  const SelectionInDOMTree& selection =
+      frame()->selection().selectionInDOMTree();
+  return selection.base() <= selection.extent();
+}
+
 static Position anchorPosition(const VisibleSelection& selection) {
   Position anchor =
       selection.isBaseFirst() ? selection.start() : selection.end();
@@ -97,7 +104,7 @@ static Position extentPosition(const VisibleSelection& selection) {
 
 Node* DOMSelection::anchorNode() const {
   if (Range* range = primaryRangeOrNull()) {
-    if (!frame() || visibleSelection().isBaseFirst())
+    if (!frame() || isBaseFirstInSelection())
       return range->startContainer();
     return range->endContainer();
   }
@@ -106,7 +113,7 @@ Node* DOMSelection::anchorNode() const {
 
 int DOMSelection::anchorOffset() const {
   if (Range* range = primaryRangeOrNull()) {
-    if (!frame() || visibleSelection().isBaseFirst())
+    if (!frame() || isBaseFirstInSelection())
       return range->startOffset();
     return range->endOffset();
   }
@@ -115,7 +122,7 @@ int DOMSelection::anchorOffset() const {
 
 Node* DOMSelection::focusNode() const {
   if (Range* range = primaryRangeOrNull()) {
-    if (!frame() || visibleSelection().isBaseFirst())
+    if (!frame() || isBaseFirstInSelection())
       return range->endContainer();
     return range->startContainer();
   }
@@ -124,7 +131,7 @@ Node* DOMSelection::focusNode() const {
 
 int DOMSelection::focusOffset() const {
   if (Range* range = primaryRangeOrNull()) {
-    if (!frame() || visibleSelection().isBaseFirst())
+    if (!frame() || isBaseFirstInSelection())
       return range->endOffset();
     return range->startOffset();
   }
@@ -318,12 +325,26 @@ void DOMSelection::setBaseAndExtent(Node* baseNode,
   // See "svg/text/textpath-reference-crash.html"
   frame()->document()->updateStyleAndLayoutIgnorePendingStylesheets();
 
+  Position basePosition(baseNode, baseOffset);
+  Position extentPosition(extentNode, extentOffset);
   frame()->selection().setSelection(
       SelectionInDOMTree::Builder()
-          .setBaseAndExtentDeprecated(Position(baseNode, baseOffset),
-                                      Position(extentNode, extentOffset))
+          .setBaseAndExtentDeprecated(basePosition, extentPosition)
           .setIsDirectional(true)
           .build());
+
+  Range* newRange = Range::create(baseNode->document());
+  if (extentPosition.isNull()) {
+    newRange->setStart(baseNode, baseOffset);
+    newRange->setEnd(baseNode, baseOffset);
+  } else if (basePosition < extentPosition) {
+    newRange->setStart(baseNode, baseOffset);
+    newRange->setEnd(extentNode, extentOffset);
+  } else {
+    newRange->setStart(extentNode, extentOffset);
+    newRange->setEnd(baseNode, baseOffset);
+  }
+  cacheRangeIfSelectionOfDocument(newRange);
 }
 
 void DOMSelection::modify(const String& alterString,
@@ -472,6 +493,8 @@ bool DOMSelection::isSelectionOfDocument() const {
 
 void DOMSelection::cacheRangeIfSelectionOfDocument(Range* range) const {
   if (!isSelectionOfDocument())
+    return;
+  if (!frame())
     return;
   frame()->selection().cacheRangeOfDocument(range);
 }
