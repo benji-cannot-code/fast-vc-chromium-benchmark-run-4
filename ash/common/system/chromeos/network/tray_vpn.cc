ead84c3a8c36a8a45356ba3193cea389dd570d48
estade <estade@chromium.org>
2017-02-15 16:57:22 -0800

    Remove non-md code from tray_vpn.cc
    BUG=686233
    
    Review-Url: https://codereview.chromium.org/2696193002
    Cr-Commit-Position: refs/heads/master@{#450851}

--- ash/common/system/chromeos/network/tray_vpn.cc
+++ ash/common/system/chromeos/network/tray_vpn.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/common/system/chromeos/network/tray_vpn.h"
 
-#include "ash/common/material_design/material_design_controller.h"
 #include "ash/common/session/session_state_delegate.h"
 #include "ash/common/system/chromeos/network/network_icon.h"
 #include "ash/common/system/chromeos/network/network_icon_animation.h"
@@ -115,15 +114,11 @@ class VpnDefaultView : public TrayItemMore,
         NetworkHandler::Get()->network_state_handler();
     const NetworkState* vpn =
         handler->FirstNetworkByType(NetworkTypePattern::VPN());
-    if (MaterialDesignController::IsSystemTrayMenuMaterial()) {
-      *image = gfx::CreateVectorIcon(
-          kNetworkVpnIcon, TrayPopupItemStyle::GetIconColor(
-                               vpn && vpn->IsConnectedState()
-                                   ? TrayPopupItemStyle::ColorStyle::ACTIVE
-                                   : TrayPopupItemStyle::ColorStyle::INACTIVE));
-    } else {
-      *image = network_icon::GetVpnImage();
-    }
+    *image = gfx::CreateVectorIcon(
+        kNetworkVpnIcon, TrayPopupItemStyle::GetIconColor(
+                             vpn && vpn->IsConnectedState()
+                                 ? TrayPopupItemStyle::ColorStyle::ACTIVE
+                                 : TrayPopupItemStyle::ColorStyle::INACTIVE));
     if (!IsVpnConnected()) {
       if (label) {
         *label =
