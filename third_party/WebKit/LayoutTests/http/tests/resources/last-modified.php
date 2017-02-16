7c8349dc06a69fe97f4b884dd4c5ad3345829936
sigbjornf <sigbjornf@opera.com>
2017-02-16 14:31:39 -0800

    document.lastModified: treat invalid dates like unknown ones.
    If the value supplied via Last-Modified: is unparseable, treat the
    modification date as unknown and return the current time (rather than
    00-00-0000 00:00:00)
    
    This aligns behavior with all other browsers.
    
    R=foolip
    BUG=693053
    
    Review-Url: https://codereview.chromium.org/2698773005
    Cr-Commit-Position: refs/heads/master@{#451123}

--- third_party/WebKit/LayoutTests/http/tests/resources/last-modified.php
+++ third_party/WebKit/LayoutTests/http/tests/resources/last-modified.php
@@ -7,7 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <script>
 var expected = '<?=$expected?>';
 var lastModified = document.lastModified;
-if (expected == lastModified) {
+
+// If expected is 'illformed-date', then the Last-Modified: header
+// is assumed to contain an unparseable date. For which we're supposed
+// to return the current time per spec. Simply verify that by checking
+// that the |lastModified| parses.
+if (expected === lastModified ||
+    (expected === "illformed-date" && !isNaN(Date.parse(lastModified)))) {
     document.write('PASS');
 } else {
     document.write('FAIL: expect ' + expected + ', but got ' + lastModified);
