f47c2a82ac7ded60982ca7f8daf3d4475f127337
fdoray <fdoray@chromium.org>
2017-02-16 09:28:55 -0800

    Use TaskScheduler instead of blocking pool in native_process_launcher.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=rdevlin.cronin@chromium.org
    
    Review-Url: https://codereview.chromium.org/2678753002
    Cr-Commit-Position: refs/heads/master@{#451004}

--- chrome/browser/extensions/api/messaging/native_process_launcher.cc
+++ chrome/browser/extensions/api/messaging/native_process_launcher.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/strings/stringprintf.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/sequenced_worker_pool.h"
 #include "build/build_config.h"
 #include "chrome/browser/extensions/api/messaging/native_messaging_host_manifest.h"
@@ -107,17 +108,17 @@ void NativeProcessLauncherImpl::Core::Launch(
     const GURL& origin,
     const std::string& native_host_name,
     const LaunchedCallback& callback) {
-  content::BrowserThread::PostBlockingPoolTask(
-      FROM_HERE, base::Bind(&Core::DoLaunchOnThreadPool, this,
-                            origin, native_host_name, callback));
+  base::PostTaskWithTraits(FROM_HERE,
+                           base::TaskTraits().MayBlock().WithPriority(
+                               base::TaskPriority::USER_VISIBLE),
+                           base::Bind(&Core::DoLaunchOnThreadPool, this, origin,
+                                      native_host_name, callback));
 }
 
 void NativeProcessLauncherImpl::Core::DoLaunchOnThreadPool(
     const GURL& origin,
     const std::string& native_host_name,
     const LaunchedCallback& callback) {
-  DCHECK(content::BrowserThread::GetBlockingPool()->RunsTasksOnCurrentThread());
-
   if (!NativeMessagingHostManifest::IsValidName(native_host_name)) {
     PostErrorResult(callback, RESULT_INVALID_NAME);
     return;
