31dd0f9dec12e521a3d06738640588e767a6c743
nhiroki <nhiroki@chromium.org>
2017-02-16 07:30:55 -0800

    Worklet: Avoid import() on a detached frame
    Without this fix, import() on a detached frame eventually crashes because of
    a null execution context.
    
    BUG=689265
    
    Review-Url: https://codereview.chromium.org/2697243003
    Cr-Commit-Position: refs/heads/master@{#450972}

--- third_party/WebKit/Source/core/workers/Worklet.cpp
+++ third_party/WebKit/Source/core/workers/Worklet.cpp
@@ -21,8 +21,11 @@ Worklet::Worklet(LocalFrame* frame)
 
 ScriptPromise Worklet::import(ScriptState* scriptState, const String& url) {
   DCHECK(isMainThread());
-  if (!isInitialized())
-    initialize();
+  if (!getExecutionContext()) {
+    return ScriptPromise::rejectWithDOMException(
+        scriptState, DOMException::create(InvalidStateError,
+                                          "This frame is already detached"));
+  }
 
   KURL scriptURL = getExecutionContext()->completeURL(url);
   if (!scriptURL.isValid()) {
@@ -30,6 +33,9 @@ ScriptPromise Worklet::import(ScriptState* scriptState, const String& url) {
         scriptState,
         DOMException::create(SyntaxError, "'" + url + "' is not a valid URL."));
   }
+
+  if (!isInitialized())
+    initialize();
 
   ScriptPromiseResolver* resolver = ScriptPromiseResolver::create(scriptState);
   ScriptPromise promise = resolver->promise();
