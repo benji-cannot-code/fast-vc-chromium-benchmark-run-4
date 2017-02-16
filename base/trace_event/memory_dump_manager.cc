7d9305f85909b59418c90321c54aff98b85de987
siggi <siggi@chromium.org>
2017-02-16 08:23:54 -0800

    Only enable heap tracking under --enable-heap-profiling=task-profiler.
    BUG=692045
    
    Review-Url: https://codereview.chromium.org/2695013005
    Cr-Commit-Position: refs/heads/master@{#450984}

--- base/trace_event/memory_dump_manager.cc
+++ base/trace_event/memory_dump_manager.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/debug/debugging_flags.h"
 #include "base/debug/stack_trace.h"
+#include "base/debug/thread_heap_usage_tracker.h"
 #include "base/memory/ptr_util.h"
 #include "base/threading/thread.h"
 #include "base/threading/thread_task_runner_handle.h"
@@ -187,18 +188,20 @@ void MemoryDumpManager::EnableHeapProfilingIfNeeded() {
   if (profiling_mode == "") {
     AllocationContextTracker::SetCaptureMode(
         AllocationContextTracker::CaptureMode::PSEUDO_STACK);
-  }
-  else if (profiling_mode == switches::kEnableHeapProfilingModeNative) {
 #if HAVE_TRACE_STACK_FRAME_POINTERS && \
     (BUILDFLAG(ENABLE_PROFILING) || !defined(NDEBUG))
+  } else if (profiling_mode == switches::kEnableHeapProfilingModeNative) {
     // We need frame pointers for native tracing to work, and they are
     // enabled in profiling and debug builds.
     AllocationContextTracker::SetCaptureMode(
         AllocationContextTracker::CaptureMode::NATIVE_STACK);
-#else
-    CHECK(false) << "'" << profiling_mode << "' mode for "
-                 << switches::kEnableHeapProfiling << " flag is not supported "
-                 << "for this platform / build type.";
+#endif
+#if BUILDFLAG(ENABLE_MEMORY_TASK_PROFILER)
+  } else if (profiling_mode == switches::kEnableHeapProfilingTaskProfiler) {
+    // Enable heap tracking, which in turn enables capture of heap usage
+    // tracking in tracked_objects.cc.
+    if (!base::debug::ThreadHeapUsageTracker::IsHeapTrackingEnabled())
+      base::debug::ThreadHeapUsageTracker::EnableHeapTracking();
 #endif
   } else {
     CHECK(false) << "Invalid mode '" << profiling_mode << "' for "
--- base/tracked_objects.cc
+++ base/tracked_objects.cc
@@ -784,14 +784,6 @@ void ThreadData::EnsureTlsInitialization() {
   // we get the lock earlier in this method.
   base::subtle::Release_Store(&status_, kInitialStartupState);
   DCHECK(base::subtle::NoBarrier_Load(&status_) != UNINITIALIZED);
-
-#if BUILDFLAG(ENABLE_MEMORY_TASK_PROFILER)
-  // Make sure heap tracking is enabled ASAP if the default state is active.
-  if (kInitialStartupState == PROFILING_ACTIVE &&
-      !base::debug::ThreadHeapUsageTracker::IsHeapTrackingEnabled()) {
-    base::debug::ThreadHeapUsageTracker::EnableHeapTracking();
-  }
-#endif  // BUILDFLAG(ENABLE_MEMORY_TASK_PROFILER)
 }
 
 // static
@@ -801,14 +793,9 @@ void ThreadData::InitializeAndSetTrackingStatus(Status status) {
 
   EnsureTlsInitialization();  // No-op if already initialized.
 
-  if (status > DEACTIVATED) {
+  if (status > DEACTIVATED)
     status = PROFILING_ACTIVE;
 
-#if BUILDFLAG(ENABLE_MEMORY_TASK_PROFILER)
-    if (!base::debug::ThreadHeapUsageTracker::IsHeapTrackingEnabled())
-      base::debug::ThreadHeapUsageTracker::EnableHeapTracking();
-#endif  // BUILDFLAG(ENABLE_MEMORY_TASK_PROFILER)
-  }
   base::subtle::Release_Store(&status_, status);
 }
 
