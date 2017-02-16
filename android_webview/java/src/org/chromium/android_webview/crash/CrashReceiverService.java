263048df2207f58bfde6846c24c0e24d8d1388df
gsennton <gsennton@chromium.org>
2017-02-16 02:26:53 -0800

    [Android WebView] Always schedule new upload job after copying minidump.
    When we copy a minidump from an app directory to WebView's directory we
    schedule a new minidump-uploading job. Before this CL we would only
    schedule such a job if no previous job was running - but doing so means
    there are cases where the old job might be finishing up, while we
    are in the process of determining whether to run the new job. In such
    cases we wouldn't schedule a new job for the new minidump - thus
    postponing the upload of the new minidump until the next time a crash
    occurs and a new job is scheduled.
    Synchronizing the job-starting code and the code running in the jobs
    themselves would add complexity to the current code - instead we
    schedule a new job unconditionally.
    Note that a drawback with this new approach is that if we schedule new
    jobs too often we might never run our jobs (since scheduling a new job
    cancels the old one). Though as long as a job starts it will try to
    upload at least one minidump, ensuring we make progress in that case.
    
    Add a test for the cancellation behaviour, and fix a previous
    cancellation test that wasn't waiting for the upload-code to run
    before asserting that a file hadn't been uploaded.
    
    BUG=687993
    
    Review-Url: https://codereview.chromium.org/2682913006
    Cr-Commit-Position: refs/heads/master@{#450924}

--- android_webview/java/src/org/chromium/android_webview/crash/CrashReceiverService.java
+++ android_webview/java/src/org/chromium/android_webview/crash/CrashReceiverService.java
@@ -23,7 +23,6 @@ import org.chromium.components.minidump_uploader.CrashFileManager;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
 /**
  * Service that is responsible for receiving crash dumps from an application, for upload.
@@ -77,7 +76,7 @@ public class CrashReceiverService extends Service {
             boolean copySucceeded = copyMinidumps(context, uid, fileDescriptors);
             if (copySucceeded && scheduleUploads) {
                 // Only schedule a new job if there actually are any files to upload.
-                scheduleNewJobIfNoJobsActive();
+                scheduleNewJob();
             }
         } finally {
             synchronized (mCopyingLock) {
@@ -106,23 +105,8 @@ public class CrashReceiverService extends Service {
         }
     }
 
-    /**
-     * @return the currently pending job with ID MINIDUMP_UPLOADING_JOB_ID, or null if no such job
-     * exists.
-     */
-    private static JobInfo getCurrentPendingJob(JobScheduler jobScheduler) {
-        List<JobInfo> pendingJobs = jobScheduler.getAllPendingJobs();
-        for (JobInfo job : pendingJobs) {
-            if (job.getId() == MINIDUMP_UPLOADING_JOB_ID) return job;
-        }
-        return null;
-    }
-
-    private void scheduleNewJobIfNoJobsActive() {
+    private void scheduleNewJob() {
         JobScheduler jobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
-        if (getCurrentPendingJob(jobScheduler) != null) {
-            return;
-        }
         JobInfo newJob = new JobInfo
                 .Builder(MINIDUMP_UPLOADING_JOB_ID /* jobId */,
                     new ComponentName(this, MinidumpUploadJobService.class))
