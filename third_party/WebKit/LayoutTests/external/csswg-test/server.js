705b39d8144cbae2d82ad21c8d3868c8f2dd2c01
blink-w3c-test-autoroller <blink-w3c-test-autoroller@chromium.org>
2017-02-16 09:51:36 -0800

    Import csswg-test@80891189c650f1c3d8c4f2d3c75fe32b18b8613d
    Using wpt-import in Chromium 820f7df12cfab494d649d7e99d3a060d6b4e7c91.
    
    Build: https://build.chromium.org/p/chromium.infra.cron/builders/w3c-test-autoroller/builds/7963
    
    TBR=qyearsley@chromium.org
    NOEXPORT=true
    
    Review-Url: https://codereview.chromium.org/2694713007
    Cr-Commit-Position: refs/heads/master@{#451012}

--- third_party/WebKit/LayoutTests/external/csswg-test/server.js
+++
@@ -1,6 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-var express = require("express"); 
-var app = express(); 
-var port = process.env.PORT || 5000; 
-app.use(express.static(__dirname )); 
-app.listen(port, function() { console.log("Listening on " + port); });
