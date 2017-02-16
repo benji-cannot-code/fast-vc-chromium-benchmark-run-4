d49086099ee45126a4bde7ad8dfeef8347747f74
maxmorin <maxmorin@chromium.org>
2017-02-16 11:28:36 -0800

    Remove unnecessary casts in AudioManagerDeleter on Mac.
    The code currently does a const_cast (ewwww), and then a
    static_cast<AudioManagerMac*>. The const cast is not needed, since
    delete works on const pointers, and the static_cast is not needed
    either, since the destructor is virtual so the correct one will be
    called either way.
    
    This also means that tests currently invokes undefined behavior when
    using ScopedAudioManagerPtr with a mock/fake AudioManager. Oops.
    This Cl fixes that.
    
    BUG=693041
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2698503005
    Cr-Commit-Position: refs/heads/master@{#451050}

--- media/audio/audio_manager.cc
+++ media/audio/audio_manager.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <utility>
+
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/command_line.h"
@@ -288,9 +290,7 @@ void AudioManagerDeleter::operator()(const AudioManager* instance) const {
   // uses a state that is destroyed in ~BrowserMainLoop().
   // See http://crbug.com/623703 for more details.
   DCHECK(instance->GetTaskRunner()->BelongsToCurrentThread());
-  AudioManagerMac* mac_instance =
-      static_cast<AudioManagerMac*>(const_cast<AudioManager*>(instance));
-  delete mac_instance;
+  delete instance;
 #else
   // AudioManager must be destroyed on the audio thread.
   if (!instance->GetTaskRunner()->DeleteSoon(FROM_HERE, instance)) {
