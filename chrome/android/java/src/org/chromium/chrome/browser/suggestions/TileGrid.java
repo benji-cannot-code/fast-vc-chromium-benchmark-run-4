469b26675db8460075455d259cc4f916846e89fb
mvanouwerkerk <mvanouwerkerk@chromium.org>
2017-02-17 06:26:08 -0800

    Make number of tile suggestion rows finch configurable.
    BUG=689086
    
    Review-Url: https://codereview.chromium.org/2695123006
    Cr-Commit-Position: refs/heads/master@{#451303}

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/TileGrid.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/TileGrid.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.ntp.ContextMenuManager;
 import org.chromium.chrome.browser.ntp.cards.ItemViewType;
 import org.chromium.chrome.browser.ntp.cards.NewTabPageViewHolder;
@@ -18,8 +19,16 @@ import org.chromium.chrome.browser.ntp.cards.OptionalLeaf;
  * The model and controller for a group of site suggestion tiles that will be rendered in a grid.
  */
 public class TileGrid extends OptionalLeaf implements TileGroup.Observer {
-    private static final int MAX_TILES = 4;
-    private static final int MAX_ROWS = 1;
+    /**
+     * The maximum number of tiles to try and fit in a row. On smaller screens, there may not be
+     * enough space to fit all of them.
+     */
+    private static final int MAX_TILE_COLUMNS = 4;
+
+    /**
+     * Experiment parameter for the maximum number of tile suggestion rows to show.
+     */
+    private static final String PARAM_CHROME_HOME_MAX_TILE_ROWS = "chrome_home_max_tile_rows";
 
     private final TileGroup mTileGroup;
 
@@ -27,7 +36,7 @@ public class TileGrid extends OptionalLeaf implements TileGroup.Observer {
             TileGroup.Delegate tileGroupDelegate) {
         mTileGroup = new TileGroup(
                 uiDelegate, contextMenuManager, tileGroupDelegate, /* observer = */ this);
-        mTileGroup.startObserving(MAX_TILES);
+        mTileGroup.startObserving(getMaxTileRows() * MAX_TILE_COLUMNS);
     }
 
     @Override
@@ -64,6 +73,12 @@ public class TileGrid extends OptionalLeaf implements TileGroup.Observer {
     @Override
     public void onLoadTaskCompleted() {}
 
+    private static int getMaxTileRows() {
+        int defaultValue = 1;
+        return ChromeFeatureList.getFieldTrialParamByFeatureAsInt(
+                ChromeFeatureList.CHROME_HOME, PARAM_CHROME_HOME_MAX_TILE_ROWS, defaultValue);
+    }
+
     /**
      * The {@code ViewHolder} for the {@link TileGrid}.
      */
@@ -77,7 +92,7 @@ public class TileGrid extends OptionalLeaf implements TileGroup.Observer {
         }
 
         public void onBindViewHolder(TileGroup tileGroup) {
-            mLayout.setMaxRows(MAX_ROWS);
+            mLayout.setMaxRows(getMaxTileRows());
             tileGroup.renderTileViews(mLayout, /* trackLoadTasks = */ false);
         }
 
