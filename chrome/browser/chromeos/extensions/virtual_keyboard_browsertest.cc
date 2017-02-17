b7ee79eb961641db981a7a9e394aef119a05233d
oka <oka@chromium.org>
2017-02-16 18:46:38 -0800

    nit: Remove a needless header.
    BUG=None
    Test=compiled browser_tests.
    
    Review-Url: https://codereview.chromium.org/2697953003
    Cr-Commit-Position: refs/heads/master@{#451197}

--- chrome/browser/chromeos/extensions/virtual_keyboard_browsertest.cc
+++ chrome/browser/chromeos/extensions/virtual_keyboard_browsertest.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension.h"
 #include "ui/aura/window_tree_host.h"
 #include "ui/base/ime/input_method.h"
-#include "ui/keyboard/keyboard_controller.h"
 #include "ui/keyboard/keyboard_switches.h"
 
 namespace {
