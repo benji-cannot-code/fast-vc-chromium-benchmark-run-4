e6494c34af012304e169ae546f221d02d8f7dfe3
fdoray <fdoray@chromium.org>
2017-02-17 09:51:39 -0800

    Use TaskScheduler instead of WorkerPool in polling_proxy_config_service.cc.
    This CL replaces base::WorkerPool::PostTask() with
    base::PostTaskWithTraits(). The following traits are used:
    
    Priority: Inherited (default)
      The priority is inherited from the calling context (i.e. TaskTraits
      are initialized with the priority of the current task).
    
    Shutdown behavior: CONTINUE_ON_SHUTDOWN
      Tasks posted with this mode which have not started executing before
      shutdown is initiated will never run. Tasks with this mode running at
      shutdown will be ignored (the worker will not be joined).
    
      Note: Tasks that were previously posted to base::WorkerPool should
      use this shutdown behavior because this is how base::WorkerPool
      handles all its tasks.
    
    May Block:
      Tasks posted with MayBlock() may block. This includes but is not
      limited to tasks that wait on synchronous file I/O operations:
      read or write a file from disk, interact with a pipe or a socket,
      rename or delete a file, enumerate files in a directory, etc. This
      trait isn't required for the mere use of locks.
    
    BUG=659191
    
    Review-Url: https://codereview.chromium.org/2679233003
    Cr-Commit-Position: refs/heads/master@{#451335}

--- net/proxy/polling_proxy_config_service.cc
+++ net/proxy/polling_proxy_config_service.cc
@@ -12,8 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/single_thread_task_runner.h"
 #include "base/synchronization/lock.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/thread_task_runner_handle.h"
-#include "base/threading/worker_pool.h"
 #include "net/proxy/proxy_config.h"
 
 namespace net {
@@ -91,17 +91,17 @@ class PollingProxyConfigService::Core
     last_poll_time_ = base::TimeTicks::Now();
     poll_task_outstanding_ = true;
     poll_task_queued_ = false;
-    base::WorkerPool::PostTask(
-        FROM_HERE,
-        base::Bind(&Core::PollOnWorkerThread, this, get_config_func_),
-        true);
+    base::PostTaskWithTraits(
+        FROM_HERE, base::TaskTraits().MayBlock().WithShutdownBehavior(
+                       base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN),
+        base::Bind(&Core::PollAsync, this, get_config_func_));
   }
 
  private:
   friend class base::RefCountedThreadSafe<Core>;
   ~Core() {}
 
-  void PollOnWorkerThread(GetConfigFunction func) {
+  void PollAsync(GetConfigFunction func) {
     ProxyConfig config;
     func(&config);
 
