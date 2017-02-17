8c32f792617e25836fdf2d7f23444b676b6e2caf
avi <avi@chromium.org>
2017-02-16 16:18:07 -0800

    Clean up the Mac shortcut tables, and make them clang-format proof.
    BUG=none
    TEST=none
    
    Review-Url: https://codereview.chromium.org/2696353003
    Cr-Commit-Position: refs/heads/master@{#451153}

--- chrome/browser/global_keyboard_shortcuts_views_mac.mm
+++ chrome/browser/global_keyboard_shortcuts_views_mac.mm
@@ -11,14 +11,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // (crbug.com/25946).
 
 const std::vector<KeyboardShortcutData>& GetWindowKeyboardShortcutTable() {
+  // clang-format off
   CR_DEFINE_STATIC_LOCAL(std::vector<KeyboardShortcutData>, result, ({
-      // cmd   shift  cntrl  option
-      // ---   -----  -----  ------
-      // '{' / '}' characters should be matched earlier than virtual key code
-      // (therefore we can match alt-8 as '{' on german keyboards).
-      {true, false, false, false, 0, '}', IDC_SELECT_NEXT_TAB},
-      {true, false, false, false, 0, '{', IDC_SELECT_PREVIOUS_TAB},
+    //cmd  shift  cntrl  option vkeycode char command
+    //---  -----  -----  ------ -------- ---- -------
+    // '{' / '}' characters should be matched earlier than virtual key codes
+    // (so we can match alt-8 as '{' on German keyboards).
+    {true, false, false, false, 0,       '}', IDC_SELECT_NEXT_TAB},
+    {true, false, false, false, 0,       '{', IDC_SELECT_PREVIOUS_TAB},
   }));
+  // clang-format on
   return result;
 }
 
