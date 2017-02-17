96891abe63573ee5e3acf4714d720b4382ad8c3b
markusheintz <markusheintz@chromium.org>
2017-02-17 09:55:28 -0800

    [remote suggestions] Add separte fetch interval for NTP open trigger
    BUG=692727
    
    Review-Url: https://codereview.chromium.org/2699613002
    Cr-Commit-Position: refs/heads/master@{#451337}

--- components/ntp_snippets/pref_names.cc
+++ components/ntp_snippets/pref_names.cc
@@ -18,6 +18,9 @@ const char kSnippetLastFetchAttempt[] =
 const char kSnippetSoftFetchingIntervalOnUsageEvent[] =
     "ntp_snippets.soft_fetching_interval_on_usage_event";
 
+const char kSnippetSoftFetchingIntervalOnNtpOpened[] =
+    "ntp_snippets.soft_fetching_interval_on_ntp_opened";
+
 const char kSnippetPersistentFetchingIntervalWifi[] =
     "ntp_snippets.fetching_interval_wifi";
 
