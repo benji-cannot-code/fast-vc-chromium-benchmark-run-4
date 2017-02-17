5c71c0eb25eb05370c8544d2767adeeab6f930a0
grt <grt@chromium.org>
2017-02-17 08:08:15 -0800

    Fix unit_tests assert failure.
    Install a default InstallDetails instance for the test binary.
    
    BUG=373987,692568
    R=phajdan.jr@chromium.org
    
    Review-Url: https://codereview.chromium.org/2696403003
    Cr-Commit-Position: refs/heads/master@{#451315}

--- chrome/test/base/run_all_unittests.cc
+++ chrome/test/base/run_all_unittests.cc
@@ -7,9 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/test/launcher/unit_test_launcher.h"
 #include "base/test/test_io_thread.h"
+#include "build/build_config.h"
 #include "chrome/test/base/chrome_unit_test_suite.h"
 #include "content/public/test/unittest_test_suite.h"
 #include "mojo/edk/embedder/scoped_ipc_support.h"
+
+#if defined(OS_WIN)
+#include "chrome/install_static/test/scoped_install_details.h"
+#endif
 
 int main(int argc, char **argv) {
   content::UnitTestTestSuite test_suite(new ChromeUnitTestSuite(argc, argv));
@@ -18,6 +23,10 @@ int main(int argc, char **argv) {
   mojo::edk::ScopedIPCSupport ipc_support(
       test_io_thread.task_runner(),
       mojo::edk::ScopedIPCSupport::ShutdownPolicy::FAST);
+
+#if defined(OS_WIN)
+  install_static::ScopedInstallDetails scoped_install_details;
+#endif
 
   return base::LaunchUnitTests(
       argc, argv, base::Bind(&content::UnitTestTestSuite::Run,
