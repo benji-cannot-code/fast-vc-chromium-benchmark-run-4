805eda8788741f06010fbc0466aa22dc213cb6ff
rdevlin.cronin <rdevlin.cronin@chromium.org>
2017-02-17 15:49:11 -0800

    [Extensions Bindings] Move request dispatch to APIRequestHandler
    Move the logic to dispatch the request (via a curried-in callback) to
    APIRequestHandler instead of APIBinding. This has a few advantages:
    - Makes a bit more sense; the request handler should handle the
      requests.
    - Reduces a bit of logic in the very-busy APIBinding.
    - Will make subsequent CLs (dispatching requests from custom types)
      easier.
    The only reason the logic currently lives in APIBinding is because it
    predates the APIRequestHandler.
    
    BUG=653596
    
    Review-Url: https://codereview.chromium.org/2697363003
    Cr-Commit-Position: refs/heads/master@{#451403}

--- extensions/renderer/api_bindings_system.h
+++ extensions/renderer/api_bindings_system.h
@@ -38,7 +38,7 @@ class APIBindingsSystem {
   APIBindingsSystem(const binding::RunJSFunction& call_js,
                     const binding::RunJSFunctionSync& call_js_sync,
                     const GetAPISchemaMethod& get_api_schema,
-                    const APIBinding::SendRequestMethod& send_request,
+                    const APIRequestHandler::SendRequestMethod& send_request,
                     const APIEventHandler::EventListenersChangedMethod&
                         event_listeners_changed,
                     APILastError last_error);
@@ -106,11 +106,6 @@ class APIBindingsSystem {
   // The method to retrieve the DictionaryValue describing a given extension
   // API. Curried in for testing purposes so we can use fake APIs.
   GetAPISchemaMethod get_api_schema_;
-
-  // The method to call when a new API call is triggered. Curried in for testing
-  // purposes. Typically, this would send an IPC to the browser to begin the
-  // function work.
-  APIBinding::SendRequestMethod send_request_;
 
   DISALLOW_COPY_AND_ASSIGN(APIBindingsSystem);
 };
