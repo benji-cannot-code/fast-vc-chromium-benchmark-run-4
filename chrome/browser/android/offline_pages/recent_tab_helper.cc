a5aa41573636d1dece75465d88e48b3ba1c2126e
carlosk <carlosk@chromium.org>
2017-02-16 20:47:34 -0800

    Last_n: disable on Svelte devices.
    This CL disables last_n on Svelte devices (less than 512 MB of RAM) as
    suggested during discussions. It also adds a test that checks that it
    actually does that but still allows user triggered snapshots.
    
    BUG=690753
    
    Review-Url: https://codereview.chromium.org/2705453003
    Cr-Commit-Position: refs/heads/master@{#451219}

--- chrome/browser/android/offline_pages/recent_tab_helper.cc
+++ chrome/browser/android/offline_pages/recent_tab_helper.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/sys_info.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "chrome/browser/android/offline_pages/downloads/offline_page_notification_bridge.h"
@@ -36,7 +37,7 @@ DEFINE_WEB_CONTENTS_USER_DATA_KEY(offline_pages::RecentTabHelper);
 namespace {
 class DefaultDelegate: public offline_pages::RecentTabHelper::Delegate {
  public:
-  DefaultDelegate() {}
+  DefaultDelegate() : is_low_end_device_(base::SysInfo::IsLowEndDevice()) {}
 
   // offline_pages::RecentTabHelper::Delegate
   std::unique_ptr<offline_pages::OfflinePageArchiver> CreatePageArchiver(
@@ -50,6 +51,11 @@ class DefaultDelegate: public offline_pages::RecentTabHelper::Delegate {
   bool GetTabId(content::WebContents* web_contents, int* tab_id) override {
     return offline_pages::OfflinePageUtils::GetTabId(web_contents, tab_id);
   }
+  bool IsLowEndDevice() override { return is_low_end_device_; }
+
+ private:
+  // Cached value of whether low end device.
+  bool is_low_end_device_;
 };
 }  // namespace
 
@@ -213,7 +219,8 @@ void RecentTabHelper::DidFinishNavigation(
 
   if (!can_save)
     snapshot_controller_->Stop();
-  last_n_listen_to_tab_hidden_ = can_save && IsOffliningRecentPagesEnabled();
+  last_n_listen_to_tab_hidden_ = can_save && !delegate_->IsLowEndDevice() &&
+                                 IsOffliningRecentPagesEnabled();
   last_n_latest_saved_quality_ = PageQuality::POOR;
 }
 
