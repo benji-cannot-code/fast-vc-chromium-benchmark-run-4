64d61397ca1c693bdbec8d9761e52f22a87b0a7c
avi <avi@chromium.org>
2017-02-17 23:04:17 -0800

    [Mac RTL] Correctly align tab titles.
    BUG=693791
    TEST=as in bug
    
    Review-Url: https://codereview.chromium.org/2704803002
    Cr-Commit-Position: refs/heads/master@{#451439}

--- chrome/browser/ui/cocoa/tabs/tab_view.mm
+++ chrome/browser/ui/cocoa/tabs/tab_view.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 #include "chrome/browser/themes/theme_properties.h"
 #include "chrome/browser/themes/theme_service.h"
+#include "chrome/browser/ui/cocoa/l10n_util.h"
 #import "chrome/browser/ui/cocoa/tabs/alert_indicator_button_cocoa.h"
 #import "chrome/browser/ui/cocoa/tabs/tab_controller.h"
 #import "chrome/browser/ui/cocoa/tabs/tab_window_controller.h"
@@ -202,6 +203,8 @@ CGFloat LineWidthFromContext(CGContextRef context) {
     base::scoped_nsobject<GTMFadeTruncatingTextFieldCell> labelCell(
         [[GTMFadeTruncatingTextFieldCell alloc] initTextCell:@"Label"]);
     [labelCell setControlSize:NSSmallControlSize];
+    if (cocoa_l10n_util::ShouldDoExperimentalRTLLayout())
+      [labelCell setAlignment:NSRightTextAlignment];
     [titleView_ setCell:labelCell];
     titleViewCell_ = labelCell;
 
