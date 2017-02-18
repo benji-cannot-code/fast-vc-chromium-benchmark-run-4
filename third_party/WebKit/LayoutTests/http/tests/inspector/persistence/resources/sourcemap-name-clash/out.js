592c25ca4cea7215d24c0ccc16cc0a7c6ed6f75a
lushnikov <lushnikov@chromium.org>
2017-02-17 22:36:45 -0800

    DevTools: teach automapping to map complex sourcemap setups.
    Consider a sourcemap with inlined sources, which has one of the source
    urls equals to the compiled url.
    
    In this case, we append the ' [sm]' suffix to the source URL so that
    the UISourceCode would not clash in the Workspace. (see crbug.com/562870).
    This, however, kills the ability to map this file to the workspace.
    
    This patch exploits the ability of automapping to handle query parameters.
    By appending '? [sm]' instead of just ' [sm]', the automapping is able
    to establish a binding based on the source size.
    
    BUG=596422, 605548
    R=dgozman
    
    Review-Url: https://codereview.chromium.org/2698183003
    Cr-Commit-Position: refs/heads/master@{#451430}

---
+++ third_party/WebKit/LayoutTests/http/tests/inspector/persistence/resources/sourcemap-name-clash/out.js
@@ -0,0 +1,3 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+"use strict";var foo=1;console.log(foo);var baz=function(){};console.log(baz);
+//# sourceMappingURL=out.js.map
