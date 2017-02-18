77465e63f0723bf1db680dc202ff9014eb87c9bd
reillyg <reillyg@chromium.org>
2017-02-17 23:40:07 -0800

    Add support for reading USB descriptors to the new Windows backend.
    This patch adds support to the new Windows USB backend for reading the
    device, configuration and string descriptors from a device. This is done
    by making requests to the USB hub driver and usually returns cached
    information but could make actual requests to the device and so full
    support for overlapped I/O is present.
    
    This will form the basis for full I/O handling in later patches.
    
    BUG=422562
    
    Review-Url: https://codereview.chromium.org/2702623002
    Cr-Commit-Position: refs/heads/master@{#451442}

--- device/usb/usb_descriptors.cc
+++ device/usb/usb_descriptors.cc
@@ -83,10 +83,14 @@ void OnDoneReadingConfigDescriptors(
     std::unique_ptr<UsbDeviceDescriptor> desc,
     const base::Callback<void(std::unique_ptr<UsbDeviceDescriptor>)>&
         callback) {
-  if (desc->num_configurations == desc->configurations.size())
+  if (desc->num_configurations == desc->configurations.size()) {
     callback.Run(std::move(desc));
-  else
+  } else {
+    LOG(ERROR) << "Failed to read all configuration descriptors. Expected "
+               << static_cast<int>(desc->num_configurations) << ", got "
+               << desc->configurations.size() << ".";
     callback.Run(nullptr);
+  }
 }
 
 void OnReadConfigDescriptor(UsbDeviceDescriptor* desc,
@@ -94,8 +98,14 @@ void OnReadConfigDescriptor(UsbDeviceDescriptor* desc,
                             UsbTransferStatus status,
                             scoped_refptr<IOBuffer> buffer,
                             size_t length) {
-  if (status == USB_TRANSFER_COMPLETED)
-    desc->Parse(std::vector<uint8_t>(buffer->data(), buffer->data() + length));
+  if (status == USB_TRANSFER_COMPLETED) {
+    if (!desc->Parse(
+            std::vector<uint8_t>(buffer->data(), buffer->data() + length))) {
+      LOG(ERROR) << "Failed to parse configuration descriptor.";
+    }
+  } else {
+    LOG(ERROR) << "Failed to read configuration descriptor.";
+  }
   closure.Run();
 }
 
@@ -117,6 +127,8 @@ void OnReadConfigDescriptorHeader(scoped_refptr<UsbDeviceHandle> device_handle,
         kControlTransferTimeout,
         base::Bind(&OnReadConfigDescriptor, desc, closure));
   } else {
+    LOG(ERROR) << "Failed to read length for configuration "
+               << static_cast<int>(index) << ".";
     closure.Run();
   }
 }
@@ -128,6 +140,7 @@ void OnReadDeviceDescriptor(
     scoped_refptr<IOBuffer> buffer,
     size_t length) {
   if (status != USB_TRANSFER_COMPLETED) {
+    LOG(ERROR) << "Failed to read device descriptor.";
     callback.Run(nullptr);
     return;
   }
@@ -135,6 +148,7 @@ void OnReadDeviceDescriptor(
   std::unique_ptr<UsbDeviceDescriptor> desc(new UsbDeviceDescriptor());
   if (!desc->Parse(
           std::vector<uint8_t>(buffer->data(), buffer->data() + length))) {
+    LOG(ERROR) << "Device descriptor parsing error.";
     callback.Run(nullptr);
     return;
   }
@@ -431,6 +445,9 @@ bool UsbDeviceDescriptor::Parse(const std::vector<uint8_t>& buffer) {
         vendor_id = data[8] | data[9] << 8;
         product_id = data[10] | data[11] << 8;
         device_version = data[12] | data[13] << 8;
+        i_manufacturer = data[14];
+        i_product = data[15];
+        i_serial_number = data[16];
         num_configurations = data[17];
         break;
       case kConfigurationDescriptorType:
--- device/usb/usb_device.h
+++ device/usb/usb_device.h
@@ -133,6 +133,7 @@ class UsbDevice : public base::RefCountedThreadSafe<UsbDevice> {
   friend class base::RefCountedThreadSafe<UsbDevice>;
   friend class UsbDeviceHandleImpl;
   friend class UsbDeviceHandleUsbfs;
+  friend class UsbDeviceHandleWin;
   friend class UsbServiceAndroid;
   friend class UsbServiceImpl;
   friend class UsbServiceLinux;
