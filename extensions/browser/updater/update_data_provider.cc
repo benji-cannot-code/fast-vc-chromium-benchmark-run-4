2de3328e8cc54b6157c6674d8f52e9071a69c5fc
fdoray <fdoray@chromium.org>
2017-02-19 11:04:44 -0800

    Use TaskScheduler instead of blocking pool in update_data_provider.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=finnur@chromium.org
    
    Review-Url: https://codereview.chromium.org/2674263002
    Cr-Commit-Position: refs/heads/master@{#451526}

--- extensions/browser/updater/update_data_provider.cc
+++ extensions/browser/updater/update_data_provider.cc
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/strings/string_util.h"
+#include "base/task_scheduler/post_task.h"
 #include "components/update_client/update_client.h"
-#include "content/public/browser/browser_thread.h"
 #include "crypto/sha2.h"
 #include "extensions/browser/content_verifier.h"
 #include "extensions/browser/extension_registry.h"
@@ -60,8 +60,9 @@ void UpdateDataProvider::GetData(
 void UpdateDataProvider::RunInstallCallback(const std::string& extension_id,
                                             const base::FilePath& temp_dir) {
   if (!context_) {
-    content::BrowserThread::PostBlockingPoolTask(
-        FROM_HERE,
+    base::PostTaskWithTraits(
+        FROM_HERE, base::TaskTraits().MayBlock().WithPriority(
+                       base::TaskPriority::BACKGROUND),
         base::Bind(base::IgnoreResult(&base::DeleteFile), temp_dir, false));
     return;
   } else {
