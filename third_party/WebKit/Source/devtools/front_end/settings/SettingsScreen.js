8b0ed5965af706adb742fd3d4bba06b16d44b5ec
dgozman <dgozman@chromium.org>
2017-02-19 08:52:04 -0800

    [DevTools] Continue GlassPane refactoring.
    - turned it into Widget, which allows to show widget inside of it properly;
    - removed extra layering in Dialog;
    - improved performance by reducing the number of relayouts;
    - easier API for GlassPane/Dialog;
    - fixed Document.deepElementFromPoint returning null for elements containing UA shadow root.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2702523003
    Cr-Commit-Position: refs/heads/master@{#451523}

--- third_party/WebKit/Source/devtools/front_end/settings/SettingsScreen.js
+++ third_party/WebKit/Source/devtools/front_end/settings/SettingsScreen.js
@@ -72,8 +72,8 @@ Settings.SettingsScreen = class extends UI.VBox {
       return;
     var dialog = new UI.Dialog();
     dialog.addCloseButton();
-    settingsScreen.show(dialog.element);
-    dialog.show();
+    settingsScreen.show(dialog.contentElement);
+    dialog.showDialog();
     settingsScreen._selectTab(name || 'preferences');
   }
 
