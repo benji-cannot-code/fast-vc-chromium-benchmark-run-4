cd5af44c28b6336314a0d885525a2d55add5c20e
alancutter <alancutter@chromium.org>
2017-02-19 17:59:27 -0800

    Add TransitionKeyframe class for CSS Transition refactor
    This patch is part of refactoring CSS Transitions to use
    CSSInterpolationTypes instead of AnimatableValues:
    https://codereview.chromium.org/2680923005
    
    The TransitionKeyframe class represents animation keyframes
    of a CSS transition. It is created to replace the use of
    AnimatableValueKeyframes while using InterpolationValues
    instead of AnimatableValues.
    
    BUG=681424
    
    Review-Url: https://codereview.chromium.org/2651553004
    Cr-Commit-Position: refs/heads/master@{#451542}

--- third_party/WebKit/Source/core/animation/animatable/AnimatableValueKeyframe.h
+++ third_party/WebKit/Source/core/animation/animatable/AnimatableValueKeyframe.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+// TODO(alancutter): Delete this class once TransitionKeyframe has completely
+// replaced it.
 class CORE_EXPORT AnimatableValueKeyframe : public Keyframe {
  public:
   static PassRefPtr<AnimatableValueKeyframe> create() {
