f8159d1005b14908d79a4440d8571ee12bea915b
skyostil <skyostil@chromium.org>
2017-02-20 06:20:01 -0800

    Revert of headless: Add a dummy include for version.h (patchset #1 id:1 of https://codereview.chromium.org/2708643003/ )
    Reason for revert:
    Looks like this isn't needed after all.
    
    Original issue's description:
    > headless: Add a dummy include for version.h
    >
    > Add a dummy include for the generated version.h to make sure it will
    > always be available for users of the headless API.
    >
    > BUG=546953
    > TBR=altimin@chromium.org
    >
    > Review-Url: https://codereview.chromium.org/2708643003
    > Cr-Commit-Position: refs/heads/master@{#451595}
    > Committed: https://chromium.googlesource.com/chromium/src/+/e859ab1300be6e103b62bc5409214e5584287815
    
    TBR=altimin@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=546953
    
    Review-Url: https://codereview.chromium.org/2706073003
    Cr-Commit-Position: refs/heads/master@{#451620}

--- headless/lib/browser/headless_browser_impl.h
+++ headless/lib/browser/headless_browser_impl.h
@@ -22,9 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "headless/lib/browser/headless_window_tree_host.h"
 #endif
 
-// Note: unused but needed to make sure this header gets generated.
-#include "headless/public/version.h"
-
 namespace headless {
 
 class HeadlessBrowserContextImpl;
