a2be5c77a9977adcbcd40f31a4272bdab823dffd
mcasas <mcasas@chromium.org>
2017-02-19 21:28:16 -0800

    MediaRecorder: make sure ondataavailable+onstop events are fired before onerror
    This CL makes sure that "error" is the last event to be fired, after
    any "dataavailable" and "stop" events have been received, which
    was brought up in [1];
    
    A typical situation triggering an error condition is if the amount
    of tracks of the MediaStream being recorded changes.  Checking
    for this amount of tracks is moved to the renderer/
    implementation so all the errors are now sent via onError(), which
    forces the recording to stop().
    
    Added a new Layout Test for it (using <canvas>.captureStream() ISO
    getUserMedia() which makes it more upstream-able).
    
    [1] https://github.com/w3c/mediacapture-record/issues/52
    
    BUG=693179
    
    Review-Url: https://codereview.chromium.org/2697703003
    Cr-Commit-Position: refs/heads/master@{#451561}

--- content/renderer/media/recorder/media_recorder_handler.h
+++ content/renderer/media/recorder/media_recorder_handler.h
@@ -75,6 +75,9 @@ class CONTENT_EXPORT MediaRecorderHandler final
                       base::TimeTicks timestamp);
   void WriteData(base::StringPiece data);
 
+  // Updates |video_tracks_|,|audio_tracks_| and returns true if any changed.
+  bool UpdateTracksAndCheckIfChanged();
+
   void OnVideoFrameForTesting(const scoped_refptr<media::VideoFrame>& frame,
                               const base::TimeTicks& timestamp);
   void OnAudioBusForTesting(const media::AudioBus& audio_bus,
@@ -99,6 +102,8 @@ class CONTENT_EXPORT MediaRecorderHandler final
 
   bool recording_;
   blink::WebMediaStream media_stream_;  // The MediaStream being recorded.
+  blink::WebVector<blink::WebMediaStreamTrack> video_tracks_;
+  blink::WebVector<blink::WebMediaStreamTrack> audio_tracks_;
 
   // |client_| is a weak pointer, and is valid for the lifetime of this object.
   blink::WebMediaRecorderHandlerClient* client_;
