440e5fd9e31e36c5ecc4078813a0604e5de1ea74
tzik <tzik@chromium.org>
2017-02-19 21:32:25 -0800

    Make MockImageCaptureClient RefCountedThreadSafe
    MockImageCaptureClient has non-thread-safe ref count, and touched from
    multiple threads. Though the access ordering to the ref count is well
    controlled and there's no data race, the usage causes a false positive
    DCHECK failure on an upcoming strict execution sequence check by:
    https://codereview.chromium.org/2666423002/
    
    This CL converts MockImageCaptureClient to RefCountedThreadSafe to
    avoid the failure.
    
    BUG=688072
    
    Review-Url: https://codereview.chromium.org/2699073002
    Cr-Commit-Position: refs/heads/master@{#451562}

--- media/capture/video/video_capture_device_unittest.cc
+++ media/capture/video/video_capture_device_unittest.cc
@@ -159,7 +159,8 @@ class MockVideoCaptureClient : public VideoCaptureDevice::Client {
   base::Callback<void(const VideoCaptureFormat&)> frame_cb_;
 };
 
-class MockImageCaptureClient : public base::RefCounted<MockImageCaptureClient> {
+class MockImageCaptureClient
+    : public base::RefCountedThreadSafe<MockImageCaptureClient> {
  public:
   // GMock doesn't support move-only arguments, so we use this forward method.
   void DoOnPhotoTaken(mojom::BlobPtr blob) {
@@ -198,7 +199,7 @@ class MockImageCaptureClient : public base::RefCounted<MockImageCaptureClient> {
   const mojom::PhotoCapabilities* capabilities() { return capabilities_.get(); }
 
  private:
-  friend class base::RefCounted<MockImageCaptureClient>;
+  friend class base::RefCountedThreadSafe<MockImageCaptureClient>;
   virtual ~MockImageCaptureClient() {}
 
   mojom::PhotoCapabilitiesPtr capabilities_;
