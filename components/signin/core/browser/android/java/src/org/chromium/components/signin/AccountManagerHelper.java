c8f66e77e9f487ec4facc077614533acb1e76b36
bsazonov <bsazonov@chromium.org>
2017-02-20 01:46:19 -0800

    Moved hasGetAccountsPermission() from AccountManagerHelper to its delegate
    hasGetAccountsPermission() is much more suited to be inside SystemAccountManagerDelegate
    instead of AccountManagerHelper. Moving it removes cyclic dependency between
    AccountManagerHelper and it's delegates and a bit cleans up public interface of
    AccountManagerHelper. Related CL: https://codereview.chromium.org/2699623002/
    
    BUG=NONE
    
    Review-Url: https://codereview.chromium.org/2693863008
    Cr-Commit-Position: refs/heads/master@{#451585}

--- components/signin/core/browser/android/java/src/org/chromium/components/signin/AccountManagerHelper.java
+++ components/signin/core/browser/android/java/src/org/chromium/components/signin/AccountManagerHelper.java
@@ -5,16 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.signin;
 
-import android.Manifest;
 import android.accounts.Account;
 import android.accounts.AuthenticatorDescription;
 import android.app.Activity;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.os.AsyncTask;
-import android.os.Process;
 
-import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.Callback;
 import org.chromium.base.Log;
 import org.chromium.base.VisibleForTesting;
@@ -315,12 +311,6 @@ public class AccountManagerHelper {
                 callback.tokenUnavailable(isTransientError);
             }
         });
-    }
-
-    public boolean hasGetAccountsPermission() {
-        return ApiCompatibilityUtils.checkPermission(mApplicationContext,
-                       Manifest.permission.GET_ACCOUNTS, Process.myPid(), Process.myUid())
-                == PackageManager.PERMISSION_GRANTED;
     }
 
     /**
