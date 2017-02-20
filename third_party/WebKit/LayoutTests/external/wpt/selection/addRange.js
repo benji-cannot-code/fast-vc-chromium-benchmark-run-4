0d662f9440cf8c9167035b116f0c9e0c0138f1c9
tkent <tkent@chromium.org>
2017-02-19 18:23:53 -0800

    selection: Do not run invalid sub-tests with unselectable endpoints.
    * addRange.js:
    It assumes any Range can be added to Selection. However the
    latest draft defines that addRange() rejects Ranges in a root
    different from Selection's document.
    Some tests after addRange() with such Ranges were invalid.
    
    https://w3c.github.io/selection-api/#dom-selection-addrange
    > The method must set the context object's range to range by a
    > strong reference (not by making a copy) if the root ([DOM4]) of
    > the range's boundary points are the document associated with
    > context object. Otherwise, this method must do nothing.
    
    * exntend.js
    extend() requires rangeCount>=1. setSelectionForwards() and
    setSelectionBackwards() are assumed to ensure it. However, these
    function can't set specified endpoints if they are in roots different
    from Selection's document. We should skip such endpoints.
    
    This CL removes 25,200 invalid tests.
    
    BUG=683066
    
    Review-Url: https://codereview.chromium.org/2700673005
    Cr-Commit-Position: refs/heads/master@{#451545}

--- third_party/WebKit/LayoutTests/external/wpt/selection/addRange.js
+++ third_party/WebKit/LayoutTests/external/wpt/selection/addRange.js
@@ -2,6 +2,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 "use strict";
 
 function testAddRange(exception, range, endpoints, qualifier, testName) {
+    if (!isSelectableNode(endpoints[0]) || !isSelectableNode(endpoints[2])) {
+        test(function() {
+            assert_equals(exception, null, "Test setup must not throw exceptions");
+
+            assertSelectionNoChange(function() { selection.addRange(range); });
+            assert_equals(range.startContainer, endpoints[0],
+                "addRange() must not modify the startContainer of the Range it's given");
+            assert_equals(range.startOffset, endpoints[1],
+                "addRange() must not modify the startOffset of the Range it's given");
+            assert_equals(range.endContainer, endpoints[2],
+                "addRange() must not modify the endContainer of the Range it's given");
+            assert_equals(range.endOffset, endpoints[3],
+                "addRange() must not modify the endOffset of the Range it's given");
+        }, testName + ": " + qualifier + " addRange() must do nothing");
+        return;
+    }
+
     test(function() {
         assert_equals(exception, null, "Test setup must not throw exceptions");
 
@@ -177,7 +194,8 @@ function testAddRangeSubSet(startIndex, optionalEndIndex) {
             }
 
             testAddRange(exception, range1, endpoints1, "first", testName);
-            testAddRange(exception, range2, endpoints2, "second", testName);
+            if (selection.rangeCount > 0)
+                testAddRange(exception, range2, endpoints2, "second", testName);
         }
     }
 }
