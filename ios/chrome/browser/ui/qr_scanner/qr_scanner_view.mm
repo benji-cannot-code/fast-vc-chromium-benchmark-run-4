944dbe31ee2e4083e0cb8f67ff0a7b7f40b406c4
jif <jif@chromium.org>
2017-02-21 08:04:35 -0800

    Removed obsolete comment in the iOS QR Code scanner.
    BUG=629427
    
    Review-Url: https://codereview.chromium.org/2706233002
    Cr-Commit-Position: refs/heads/master@{#451771}

--- ios/chrome/browser/ui/qr_scanner/qr_scanner_view.mm
+++ ios/chrome/browser/ui/qr_scanner/qr_scanner_view.mm
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util_mac.h"
 
 namespace {
-// TODO(crbug.com/629427): Replace the temporary UI constants with correct
-// values for all device types.
 
 // Padding for buttons in the QR scanner UI.
 const CGFloat kButtonPadding = 16.0;
