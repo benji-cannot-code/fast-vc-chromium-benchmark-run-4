1dcbe5fd4b701d4179f099d13f5bd48f4ce12d28
yosin <yosin@chromium.org>
2017-02-20 16:29:24 -0800

    Get rid of unused enum fields from TextEventInputType
    This patch removes following unused enum fields from |TextEventInpytType|:
     - TextEventInputBackTab
     - TextEventInputDictation
     - TextEventInputOther
    for improving code health.
    
    BUG=n/a
    TEST=n/a; no behavior changes
    
    Review-Url: https://codereview.chromium.org/2703213002
    Cr-Commit-Position: refs/heads/master@{#451675}

--- third_party/WebKit/Source/core/events/TextEventInputType.h
+++ third_party/WebKit/Source/core/events/TextEventInputType.h
@@ -36,12 +36,9 @@ enum TextEventInputType {
   // Any tab characters in the text are backtabs.
   TextEventInputLineBreak,
   TextEventInputComposition,
-  TextEventInputBackTab,
   TextEventInputPaste,
   TextEventInputDrop,
-  TextEventInputDictation,
   TextEventInputIncrementalInsertion,
-  TextEventInputOther,
 };
 
 }  // namespace blink
