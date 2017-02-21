c4e8adaca3a63c4674a774a23b181b88e469723c
nicholss <nicholss@chromium.org>
2017-02-21 09:55:05 -0800

    Extending OAuthTokenGetter to also accept AuthCodes as creds.
    The aim here is to be able to reuse the oauth token getter class with the
    client. But the client needs to start with only an access token and it
    will convert that into a refresh token and an email address. There
    needs to be a way to notify the caller of the new refresh token and email
    address, so that was also added.
    
    BUG=652781
    R=sergeyu@chromium.org
    
    Review-Url: https://codereview.chromium.org/2689983005
    Cr-Commit-Position: refs/heads/master@{#451793}

--- remoting/base/oauth_token_getter.cc
+++ remoting/base/oauth_token_getter.cc
@@ -7,12 +7,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace remoting {
 
-OAuthTokenGetter::OAuthCredentials::OAuthCredentials(
+// OAuthAuthorizationCredentials implementation.
+
+OAuthTokenGetter::OAuthAuthorizationCredentials::OAuthAuthorizationCredentials(
     const std::string& login,
     const std::string& refresh_token,
     bool is_service_account)
     : login(login),
       refresh_token(refresh_token),
       is_service_account(is_service_account) {}
+
+OAuthTokenGetter::OAuthAuthorizationCredentials::
+    ~OAuthAuthorizationCredentials() {}
+
+// OAuthIntermediateCredentials implementation.
+
+OAuthTokenGetter::OAuthIntermediateCredentials::OAuthIntermediateCredentials(
+    const std::string& authorization_code,
+    bool is_service_account)
+    : authorization_code(authorization_code),
+      is_service_account(is_service_account) {}
+
+OAuthTokenGetter::OAuthIntermediateCredentials::
+    ~OAuthIntermediateCredentials() {}
 
 }  // namespace remoting
