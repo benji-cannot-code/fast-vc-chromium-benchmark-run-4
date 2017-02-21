cecbb1a0a63ef7dd5500e250724d2a87ee12d897
johnme <johnme@chromium.org>
2017-02-21 14:17:06 -0800

    Media Controls: Remove download button for infinite streams
    Specifically, removes the download button for media with infinite
    duration, since there is no clear end at which to finish the download.
    
    Also adds tests for the download button being hidden for empty URLs and
    HLS streams, and fixes the test for the download button being hidden
    when hideDownloadUI setting is disabled by making its video non-local.
    
    BUG=691561,654507,650738
    
    Review-Url: https://codereview.chromium.org/2710713003
    Cr-Commit-Position: refs/heads/master@{#451852}

--- third_party/WebKit/Source/core/html/shadow/MediaControlsTest.cpp
+++ third_party/WebKit/Source/core/html/shadow/MediaControlsTest.cpp
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/html/shadow/MediaControls.h"
 
+#include <limits>
+#include <memory>
 #include "core/HTMLNames.h"
 #include "core/css/StylePropertySet.h"
 #include "core/dom/Document.h"
@@ -21,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/platform/modules/remoteplayback/WebRemotePlaybackAvailability.h"
 #include "public/platform/modules/remoteplayback/WebRemotePlaybackClient.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include <memory>
 
 namespace blink {
 
@@ -29,6 +30,7 @@ namespace {
 
 class MockVideoWebMediaPlayer : public WebMediaPlayer {
  public:
+  // WebMediaPlayer overrides:
   void load(LoadType, const WebMediaPlayerSource&, CORSMode) override{};
   void play() override{};
   void pause() override{};
@@ -168,6 +170,8 @@ class MediaControlsTest : public ::testing::Test {
   void simulateHideMediaControlsTimerFired() {
     m_mediaControls->hideMediaControlsTimerFired(nullptr);
   }
+
+  void simulateLoadedMetadata() { m_mediaControls->onLoadedMetadata(); }
 
   MediaControls& mediaControls() { return *m_mediaControls; }
   Document& document() { return m_pageHolder->document(); }
@@ -330,6 +334,66 @@ TEST_F(MediaControlsTest, KeepControlsVisibleIfOverflowListVisible) {
   simulateHideMediaControlsTimerFired();
   EXPECT_TRUE(isElementVisible(*overflowList));
   EXPECT_TRUE(isElementVisible(*panel));
+}
+
+TEST_F(MediaControlsTest, DownloadButtonDisplayed) {
+  ensureLayout();
+
+  Element* downloadButton = getElementByShadowPseudoId(
+      mediaControls(), "-internal-media-controls-download-button");
+  ASSERT_NE(nullptr, downloadButton);
+
+  mediaControls().mediaElement().setSrc("https://example.com/foo.mp4");
+  testing::runPendingTasks();
+  simulateLoadedMetadata();
+
+  // Download button should normally be displayed.
+  EXPECT_TRUE(isElementVisible(*downloadButton));
+}
+
+TEST_F(MediaControlsTest, DownloadButtonNotDisplayedEmptyUrl) {
+  ensureLayout();
+
+  Element* downloadButton = getElementByShadowPseudoId(
+      mediaControls(), "-internal-media-controls-download-button");
+  ASSERT_NE(nullptr, downloadButton);
+
+  // Download button should not be displayed when URL is empty.
+  mediaControls().mediaElement().setSrc("");
+  testing::runPendingTasks();
+  simulateLoadedMetadata();
+  EXPECT_FALSE(isElementVisible(*downloadButton));
+}
+
+TEST_F(MediaControlsTest, DownloadButtonNotDisplayedInfiniteDuration) {
+  ensureLayout();
+
+  Element* downloadButton = getElementByShadowPseudoId(
+      mediaControls(), "-internal-media-controls-download-button");
+  ASSERT_NE(nullptr, downloadButton);
+
+  mediaControls().mediaElement().setSrc("https://example.com/foo.mp4");
+  testing::runPendingTasks();
+
+  // Download button should not be displayed when duration is infinite.
+  mediaControls().mediaElement().durationChanged(
+      std::numeric_limits<double>::infinity(), false /* requestSeek */);
+  simulateLoadedMetadata();
+  EXPECT_FALSE(isElementVisible(*downloadButton));
+}
+
+TEST_F(MediaControlsTest, DownloadButtonNotDisplayedHLS) {
+  ensureLayout();
+
+  Element* downloadButton = getElementByShadowPseudoId(
+      mediaControls(), "-internal-media-controls-download-button");
+  ASSERT_NE(nullptr, downloadButton);
+
+  // Download button should not be displayed for HLS streams.
+  mediaControls().mediaElement().setSrc("https://example.com/foo.m3u8");
+  testing::runPendingTasks();
+  simulateLoadedMetadata();
+  EXPECT_FALSE(isElementVisible(*downloadButton));
 }
 
 }  // namespace blink
