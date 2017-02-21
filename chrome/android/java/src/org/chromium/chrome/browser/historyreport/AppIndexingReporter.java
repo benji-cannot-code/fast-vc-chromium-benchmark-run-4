624edc21f4f67f4dc5be348e8119a92f9a1c53c2
dproctor <dproctor@google.com>
2017-02-21 11:35:45 -0800

    Add app indexing reporter public class.
    BUG=693633
    
    Review-Url: https://codereview.chromium.org/2622313002
    Cr-Commit-Position: refs/heads/master@{#451818}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/historyreport/AppIndexingReporter.java
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.historyreport;
+
+import org.chromium.base.ContextUtils;
+import org.chromium.chrome.browser.ChromeApplication;
+
+/** Base class for reporting entities to App Indexing. */
+public class AppIndexingReporter {
+    private static final String TAG = "AppIndexingReporter";
+    private static AppIndexingReporter sInstance;
+
+    public static AppIndexingReporter getInstance() {
+        if (sInstance == null) {
+            sInstance = ((ChromeApplication) ContextUtils.getApplicationContext())
+                                .createAppIndexingReporter();
+        }
+        return sInstance;
+    }
+
+    /**
+     * Reports provided entity to on-device index.
+     * Base class does not implement any reporting, and call is a no-op. Child classes should
+     * implement this functionality.
+     */
+    public void reportEntityJsonLd(String url, String json) {
+        // Overriden by private class. Base class does nothing.
+    }
+}
