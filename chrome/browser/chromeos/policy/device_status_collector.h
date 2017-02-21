4ba3b6fb58b43c1da416aeddc0d6bc890276b63d
pmarko <pmarko@chromium.org>
2017-02-21 11:00:57 -0800

    Extend AndroidStatus reporting to regular user sessions
    Introduce support for ARC++ reporting in regular user sessions by:
    - Moving the kReportArcStatusEnabled pref from local state to Profile
      prefs (governed by ReportArcStatusEnabled user policy).
    - Change the default of kReportArcStatusEnabled (if no value is given
      by policy) to false for privacy reasons.
    - Male DeviceStatusCollector evaluate the primary user's
      kReportArcStatusEnabled to decide if AndroidStatus should be included
      and also check if the user is affiliated. (Note: Device local
      accounts are automatically affiliated, so this does not break
      kiosk ARC++ reporting).
    - Extend DeviceStatusCollector to include the primary user's DMToken for
      affiliated regular user sessions if anything has been reported.
    
    BUG=668501
    TEST=browser_tests --gtest_filter=DeviceStatusCollector.* and manual tests with YAPS
    
    Review-Url: https://codereview.chromium.org/2695713012
    Cr-Commit-Position: refs/heads/master@{#451808}

--- chrome/browser/chromeos/policy/device_status_collector.h
+++ chrome/browser/chromeos/policy/device_status_collector.h
@@ -37,8 +37,13 @@ class StatisticsProvider;
 }
 }
 
+namespace user_manager {
+class User;
+}
+
 class PrefRegistrySimple;
 class PrefService;
+class Profile;
 
 namespace policy {
 
@@ -106,6 +111,7 @@ class DeviceStatusCollector {
   void OnSubmittedSuccessfully();
 
   static void RegisterPrefs(PrefRegistrySimple* registry);
+  static void RegisterProfilePrefs(PrefRegistrySimple* registry);
 
   // Returns the DeviceLocalAccount associated with the currently active
   // kiosk session, if the session was auto-launched with zero delay
@@ -131,6 +137,10 @@ class DeviceStatusCollector {
 
   // Gets the version of the passed app. Virtual to allow mocking.
   virtual std::string GetAppVersion(const std::string& app_id);
+
+  // Gets the DMToken associated with a profile. Returns an empty string if no
+  // DMToken could be retrieved. Virtual to allow mocking.
+  virtual std::string GetDMTokenForProfile(Profile* profile);
 
   // Samples the current hardware resource usage to be sent up with the
   // next device status update.
@@ -169,6 +179,10 @@ class DeviceStatusCollector {
   void GetDeviceStatus(scoped_refptr<GetStatusState> state);
   void GetSessionStatus(scoped_refptr<GetStatusState> state);
 
+  bool GetSessionStatusForUser(
+      scoped_refptr<GetStatusState> state,
+      enterprise_management::SessionStatusReportRequest* status,
+      const user_manager::User* user);
   // Helpers for the various portions of DEVICE STATUS. Return true if they
   // actually report any status. Functions that queue async queries take
   // a |GetStatusState| instance.
@@ -260,7 +274,6 @@ class DeviceStatusCollector {
   bool report_users_ = false;
   bool report_hardware_status_ = false;
   bool report_kiosk_session_status_ = false;
-  bool report_android_status_ = false;
   bool report_os_update_status_ = false;
   bool report_running_kiosk_app_ = false;
 
@@ -282,7 +295,6 @@ class DeviceStatusCollector {
       os_update_status_subscription_;
   std::unique_ptr<chromeos::CrosSettings::ObserverSubscription>
       running_kiosk_app_subscription_;
-  BooleanPrefMember report_arc_status_pref_;
 
   // Task runner in the creation thread where responses are sent to.
   scoped_refptr<base::SequencedTaskRunner> task_runner_;
--- chrome/browser/chromeos/profiles/profile_helper.h
+++ chrome/browser/chromeos/profiles/profile_helper.h
@@ -40,6 +40,10 @@ class MultiUserWindowManagerChromeOSTest;
 }  // namespace test
 }  // namespace ash
 
+namespace policy {
+class DeviceStatusCollectorTest;
+}  // namespace policy
+
 namespace test {
 class BrowserFinderChromeOSTest;
 }  // namespace test
@@ -154,6 +158,7 @@ class ProfileHelper
   // to access private test methods.
   friend class CryptohomeAuthenticatorTest;
   friend class DeviceSettingsTestBase;
+  friend class policy::DeviceStatusCollectorTest;
   friend class ExistingUserControllerTest;
   friend class extensions::ExtensionGarbageCollectorChromeOSUnitTest;
   friend class FakeChromeUserManager;
