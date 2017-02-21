660a793f4329e8e9d4d0f9a178ba7afbae53ca98
dcheng <dcheng@chromium.org>
2017-02-21 03:25:56 -0800

    Simplify conditionals for blink::Location::isActive().
    On navigation or detach, DOMWindow's back pointer to Frame is cleared,
    so it's only necessary to check that the back pointer to Frame is
    non-null.
    
    BUG=694111
    
    Review-Url: https://codereview.chromium.org/2704303002
    Cr-Commit-Position: refs/heads/master@{#451732}

--- third_party/WebKit/Source/core/frame/Location.cpp
+++ third_party/WebKit/Source/core/frame/Location.cpp
@@ -287,7 +287,7 @@ Document* Location::document() const {
 }
 
 bool Location::isAttached() const {
-  return m_domWindow->frame() && m_domWindow->frame()->host();
+  return m_domWindow->frame();
 }
 
 }  // namespace blink
