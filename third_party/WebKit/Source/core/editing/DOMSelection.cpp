788ce75df8dfeee90fe5665509b711eddb0a84b8
tkent <tkent@chromium.org>
2017-02-20 20:36:13 -0800

    Selection API: collapse(), setBaseAndExtent(), and extend() don't need to have code to check negative offsets.
    Range::checkNodeWOffset() produces IndexSizeError for negative offsets.
    Code to check negative offsets in these functions were redundant.
    
    This CL changes behavior for DOCTYPE. If a DOCTYPE node and a negative offset are
    specified, the functions throw InvalidNodeTypeError instead of
    IndexSizeError. This fixes 111 tests in external/wpt/selection/.
    
    Also, setBaseAndExtent(null, -1, ...) doesn't throw an IndexSizeError. This is
    consistent with a behavior that setBaseAndExtent(null, 10000, ...) doesn't throw.
    The offset argument should be unsigned actually, and we can't check offset
    validity for null node.
    
    BUG=683066
    
    Review-Url: https://codereview.chromium.org/2710593002
    Cr-Commit-Position: refs/heads/master@{#451700}

--- third_party/WebKit/Source/core/editing/DOMSelection.cpp
+++ third_party/WebKit/Source/core/editing/DOMSelection.cpp
@@ -225,11 +225,6 @@ void DOMSelection::collapse(Node* node,
 
   // 2. The method must throw an IndexSizeError exception if offset is longer
   // than node's length ([DOM4]) and abort these steps.
-  if (offset < 0) {
-    exceptionState.throwDOMException(
-        IndexSizeError, String::number(offset) + " is not a valid offset.");
-    return;
-  }
   Range::checkNodeWOffset(node, offset, exceptionState);
   if (exceptionState.hadException())
     return;
@@ -322,20 +317,6 @@ void DOMSelection::setBaseAndExtent(Node* baseNode,
                                     ExceptionState& exceptionState) {
   if (!isAvailable())
     return;
-
-  if (baseOffset < 0) {
-    exceptionState.throwDOMException(
-        IndexSizeError,
-        String::number(baseOffset) + " is not a valid base offset.");
-    return;
-  }
-
-  if (extentOffset < 0) {
-    exceptionState.throwDOMException(
-        IndexSizeError,
-        String::number(extentOffset) + " is not a valid extent offset.");
-    return;
-  }
 
   // TODO(editing-dev): Behavior on where base or extent is null is still
   // under discussion: https://github.com/w3c/selection-api/issues/72
@@ -472,11 +453,6 @@ void DOMSelection::extend(Node* node,
     return;
   }
 
-  if (offset < 0) {
-    exceptionState.throwDOMException(
-        IndexSizeError, String::number(offset) + " is not a valid offset.");
-    return;
-  }
   Range::checkNodeWOffset(node, offset, exceptionState);
   if (exceptionState.hadException())
     return;
