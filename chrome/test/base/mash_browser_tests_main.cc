d441eb2e21e3d1b4ef23e357a43feaa3528a123e
sadrul <sadrul@chromium.org>
2017-02-21 14:14:18 -0800

    mash: Add --use-test-config for mash_browser_tests.
    Add the test config flag, so that the window server (with x11 backend) is
    initialized properly (i.e. set to turn on override_redirect flag for the
    x11 windows it creates). This is a speculative fix for the flaky time
    outs in mash_browser_tests.
    
    BUG=693231
    
    Review-Url: https://codereview.chromium.org/2707013003
    Cr-Commit-Position: refs/heads/master@{#451850}

--- chrome/test/base/mash_browser_tests_main.cc
+++ chrome/test/base/mash_browser_tests_main.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/service_manager/public/cpp/standalone_service/standalone_service.h"
 #include "services/service_manager/runner/common/switches.h"
 #include "services/service_manager/runner/init.h"
+#include "services/ui/common/switches.h"
 #include "ui/aura/env.h"
 
 namespace {
@@ -171,12 +172,11 @@ void StartChildApp(service_manager::mojom::ServiceRequest service_request) {
 
 bool RunMashBrowserTests(int argc, char** argv, int* exit_code) {
   base::CommandLine::Init(argc, argv);
-  const base::CommandLine& command_line =
-      *base::CommandLine::ForCurrentProcess();
-  if (!command_line.HasSwitch("run-in-mash"))
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+  if (!command_line->HasSwitch("run-in-mash"))
     return false;
 
-  if (command_line.HasSwitch(MojoTestConnector::kMashApp)) {
+  if (command_line->HasSwitch(MojoTestConnector::kMashApp)) {
 #if defined(OS_LINUX)
     base::AtExitManager exit_manager;
 #endif
@@ -186,6 +186,7 @@ bool RunMashBrowserTests(int argc, char** argv, int* exit_code) {
     base::debug::EnableInProcessStackDumping();
 #endif
 
+    command_line->AppendSwitch(ui::switches::kUseTestConfig);
     service_manager::RunStandaloneService(base::Bind(&StartChildApp));
     *exit_code = 0;
     return true;
@@ -198,8 +199,8 @@ bool RunMashBrowserTests(int argc, char** argv, int* exit_code) {
   // ServiceManagerConnection
   // as though we were embedded.
   content::ServiceManagerConnection::Factory service_manager_connection_factory;
-  if (command_line.HasSwitch(content::kSingleProcessTestsFlag) &&
-      !command_line.HasSwitch(switches::kPrimordialPipeToken)) {
+  if (command_line->HasSwitch(content::kSingleProcessTestsFlag) &&
+      !command_line->HasSwitch(switches::kPrimordialPipeToken)) {
     service_manager_connection_factory =
         base::Bind(&CreateServiceManagerConnection, &delegate);
     content::ServiceManagerConnection::SetFactoryForTest(
