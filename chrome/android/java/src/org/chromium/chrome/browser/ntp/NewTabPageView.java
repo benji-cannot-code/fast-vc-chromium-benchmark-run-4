0ca83a4b901ab369a6bff1fed27735c413460de3
bauerb <bauerb@chromium.org>
2017-02-21 07:47:40 -0800

    [Android NTP] Allow overlapping snap scroll regions.
    Previously the code assumed that snap scroll regions would never overlap.
    That assumption became false with the condensed NTP layout, with the result that
    only the first snap would be applied and the NTP would come to rest in a
    position it should have scrolled out of.
    
    We now apply all snaps in order and have an assertion that the desired position
    does not change after one pass.
    
    BUG=693095
    
    Review-Url: https://codereview.chromium.org/2708653002
    Cr-Commit-Position: refs/heads/master@{#451767}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
@@ -440,8 +440,7 @@ public class NewTabPageView
                 assert mPendingSnapScroll;
                 mPendingSnapScroll = false;
 
-                mRecyclerView.snapScroll(mSearchBoxView,
-                        mRecyclerView.computeVerticalScrollOffset(), getHeight());
+                mRecyclerView.snapScroll(mSearchBoxView, getHeight());
             }
         };
 
@@ -766,8 +765,7 @@ public class NewTabPageView
         mRecyclerView.updatePeekingCardAndHeader();
         // The positioning of elements may have been changed (since the elements expand to fill
         // the available vertical space), so adjust the scroll.
-        mRecyclerView.snapScroll(mSearchBoxView,
-                mRecyclerView.computeVerticalScrollOffset(), getHeight());
+        mRecyclerView.snapScroll(mSearchBoxView, getHeight());
     }
 
     /**
