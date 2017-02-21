ecf77451269676526c81c84b972823a972d198f3
tedchoc <tedchoc@chromium.org>
2017-02-21 11:44:16 -0800

    [Android] Explicitly request all needed runtime permissions.
    Previously, we only requested a single permission in
    a particular permission group to get access to everything
    in the group.  Going forward, it is a best practice to
    request them all and rely on the framework to handle
    granting.
    
    This changes to requesting the groups to requesting all
    needed runtime permissions.
    
    BUG=666603
    
    Review-Url: https://codereview.chromium.org/2682863002
    Cr-Commit-Position: refs/heads/master@{#451823}

--- chrome/browser/android/preferences/pref_service_bridge.h
+++ chrome/browser/android/preferences/pref_service_bridge.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <jni.h>
 #include <string>
+#include <vector>
 
 #include "components/content_settings/core/common/content_settings.h"
 
@@ -23,10 +24,11 @@ class PrefServiceBridge {
       const std::string& locale,
       std::string* accept_languages);
 
-  // Return the corresponding Android permission associated with the
-  // ContentSettingsType specified (or an empty string if no permission exists).
-  static std::string GetAndroidPermissionForContentSetting(
-      ContentSettingsType content_type);
+  // Populate the list of corresponding Android permissions associated with the
+  // ContentSettingsType specified.
+  static void GetAndroidPermissionsForContentSetting(
+      ContentSettingsType content_type,
+      std::vector<std::string>* out);
 };
 
 #endif  // CHROME_BROWSER_ANDROID_PREFERENCES_PREF_SERVICE_BRIDGE_H_
