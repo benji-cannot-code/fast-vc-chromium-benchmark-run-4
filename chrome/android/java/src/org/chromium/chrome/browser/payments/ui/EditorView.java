fe739717f61b8e723eb64a5ab16b4b0373aa2224
gogerald <gogerald@chromium.org>
2017-02-21 11:38:25 -0800

    [Payments] Cancel edit after clicking back button
    BUG=693901
    
    Review-Url: https://codereview.chromium.org/2703333005
    Cr-Commit-Position: refs/heads/master@{#451820}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/ui/EditorView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ui/EditorView.java
@@ -60,8 +60,9 @@ import javax.annotation.Nullable;
  * The PaymentRequest editor dialog. Can be used for editing contact information, shipping address,
  * billing address, and credit cards.
  */
-public class EditorView
-        extends AlwaysDismissedDialog implements OnClickListener, DialogInterface.OnShowListener {
+public class EditorView extends AlwaysDismissedDialog implements OnClickListener,
+                                                                 DialogInterface.OnShowListener,
+                                                                 DialogInterface.OnDismissListener {
     /** The indicator for input fields that are required. */
     public static final String REQUIRED_FIELD_INDICATOR = "*";
 
@@ -199,7 +200,7 @@ public class EditorView
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                cancelEdit();
+                dismissDialog();
             }
         });
 
@@ -263,25 +264,19 @@ public class EditorView
         if (view.getId() == R.id.payments_edit_done_button) {
             if (validateForm()) {
                 mEditorModel.done();
+                mEditorModel = null;
                 dismissDialog();
                 return;
             }
 
             if (mObserverForTest != null) mObserverForTest.onPaymentRequestEditorValidationError();
         } else if (view.getId() == R.id.payments_edit_cancel_button) {
-            cancelEdit();
+            dismissDialog();
         }
-    }
-
-    private void cancelEdit() {
-        mEditorModel.cancel();
-        dismissDialog();
     }
 
     private void dismissDialog() {
         if (mDialogInOutAnimator != null || !isShowing()) return;
-
-        removeTextChangedListenersAndInputFilters();
 
         Animator dropDown =
                 ObjectAnimator.ofFloat(mLayout, View.TRANSLATION_Y, 0f, mLayout.getHeight());
@@ -301,6 +296,12 @@ public class EditorView
         });
 
         mDialogInOutAnimator.start();
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        if (mEditorModel != null) mEditorModel.cancel();
+        removeTextChangedListenersAndInputFilters();
     }
 
     private void prepareButtons() {
@@ -447,6 +448,7 @@ public class EditorView
      */
     public void show(final EditorModel editorModel) {
         setOnShowListener(this);
+        setOnDismissListener(this);
         mEditorModel = editorModel;
 
         mLayout = LayoutInflater.from(mContext).inflate(R.layout.payment_request_editor, null);
