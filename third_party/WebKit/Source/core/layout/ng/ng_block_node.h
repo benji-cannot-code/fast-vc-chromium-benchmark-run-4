df455c67f92b191bd2f2c50f1872a9ec823174eb
kojii <kojii@chromium.org>
2017-02-20 17:04:53 -0800

    [LayoutNG] Fix crash when NGBlockNode::NextSibling is inline
    This patch applies the same fix as FirstChild to NextSibling.
    
    This patch fixes 6 crashes in fast/block/float and fast/css2.1 when
    LayoutNG and LayoutNGInline flags are enabled.
    
    BUG=636993
    
    Review-Url: https://codereview.chromium.org/2708463002
    Cr-Commit-Position: refs/heads/master@{#451679}

--- third_party/WebKit/Source/core/layout/ng/ng_block_node.h
+++ third_party/WebKit/Source/core/layout/ng/ng_block_node.h
@@ -36,7 +36,7 @@ class CORE_EXPORT NGBlockNode final : public NGLayoutInputNode {
 
   RefPtr<NGPhysicalFragment> Layout(
       NGConstraintSpace* constraint_space) override;
-  NGBlockNode* NextSibling() override;
+  NGLayoutInputNode* NextSibling() override;
   LayoutObject* GetLayoutObject() override;
 
   // Computes the value of min-content and max-content for this box.
@@ -51,7 +51,7 @@ class CORE_EXPORT NGBlockNode final : public NGLayoutInputNode {
 
   NGLayoutInputNode* FirstChild();
 
-  void SetNextSibling(NGBlockNode*);
+  void SetNextSibling(NGLayoutInputNode*);
   void SetFirstChild(NGLayoutInputNode*);
 
   void SetFragment(NGPhysicalBoxFragment* fragment) { fragment_ = fragment; }
@@ -85,7 +85,7 @@ class CORE_EXPORT NGBlockNode final : public NGLayoutInputNode {
   // combination.
   LayoutBox* layout_box_;
   RefPtr<ComputedStyle> style_;
-  Member<NGBlockNode> next_sibling_;
+  Member<NGLayoutInputNode> next_sibling_;
   Member<NGLayoutInputNode> first_child_;
   // TODO(mstensho): An input node may produce multiple fragments, so this
   // should probably be renamed to last_fragment_ or something like that, since
