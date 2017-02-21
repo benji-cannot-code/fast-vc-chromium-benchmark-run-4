0a17aea49355015dcfe14cfecff0e63006a921e4
siggi <siggi@chromium.org>
2017-02-21 09:07:17 -0800

    chrome://profiler: Only show memory metrics in when heap tracking is enabled.
    BUG=644385
    
    Review-Url: https://codereview.chromium.org/2705243002
    Cr-Commit-Position: refs/heads/master@{#451783}

--- chrome/browser/ui/webui/profiler_ui.cc
+++ chrome/browser/ui/webui/profiler_ui.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/debug/debugging_flags.h"
+#include "base/debug/thread_heap_usage_tracker.h"
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/string_util.h"
@@ -108,8 +109,9 @@ content::WebUIDataSource* CreateProfilerHTMLSource() {
   source->AddResourcePath("profiler.js", IDR_PROFILER_JS);
   source->SetDefaultResource(IDR_PROFILER_HTML);
   source->UseGzip(std::unordered_set<std::string>());
-  source->AddBoolean("enableMemoryTaskProfiler",
-                     BUILDFLAG(ENABLE_MEMORY_TASK_PROFILER));
+  source->AddBoolean(
+      "enableMemoryTaskProfiler",
+      base::debug::ThreadHeapUsageTracker::IsHeapTrackingEnabled());
 
   return source;
 }
