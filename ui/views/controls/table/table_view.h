dccccf800c25343a63d25d1063384d990b81652b
estade <estade@chromium.org>
2017-02-21 09:47:57 -0800

    Remove unused TableViewRowBackgroundPainter class.
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2702673002
    Cr-Commit-Position: refs/heads/master@{#451791}

--- ui/views/controls/table/table_view.h
+++ ui/views/controls/table/table_view.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_VIEWS_CONTROLS_TABLE_TABLE_VIEW_VIEWS_H_
 #define UI_VIEWS_CONTROLS_TABLE_TABLE_VIEW_VIEWS_H_
 
-#include <memory>
 #include <vector>
 
 #include "base/macros.h"
@@ -38,7 +37,6 @@ struct GroupRange;
 class TableGrouper;
 class TableHeader;
 class TableViewObserver;
-class TableViewRowBackgroundPainter;
 class TableViewTestHelper;
 
 // The cells in the first column of a table can contain:
@@ -106,9 +104,6 @@ class VIEWS_EXPORT TableView
 
   // Returns a new ScrollView that contains the receiver.
   View* CreateParentIfNecessary();
-
-  void SetRowBackgroundPainter(
-      std::unique_ptr<TableViewRowBackgroundPainter> painter);
 
   // Sets the TableGrouper. TableView does not own |grouper| (common use case is
   // to have TableModel implement TableGrouper).
@@ -347,8 +342,6 @@ class VIEWS_EXPORT TableView
   // Mappings used when sorted.
   std::vector<int> view_to_model_;
   std::vector<int> model_to_view_;
-
-  std::unique_ptr<TableViewRowBackgroundPainter> row_background_painter_;
 
   TableGrouper* grouper_;
 
