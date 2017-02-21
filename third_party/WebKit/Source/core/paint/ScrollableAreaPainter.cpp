fa9fb4704fb063bbb635bcc375c475eab019898a
malaykeshav <malaykeshav@chromium.org>
2017-02-21 14:05:49 -0800

    Resizes corner rect to match device scale factor
    Corner rect resizer for scrollable area now considers device scale
    factor when computing its bounds.
    
    BUG=692238
    COMPONENT=ScrollableAreaPainter, Corner Resizer
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2698543002
    Cr-Commit-Position: refs/heads/master@{#451844}

--- third_party/WebKit/Source/core/paint/ScrollableAreaPainter.cpp
+++ third_party/WebKit/Source/core/paint/ScrollableAreaPainter.cpp
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/paint/PaintLayerScrollableArea.h"
 #include "core/paint/ScrollbarPainter.h"
 #include "core/paint/TransformRecorder.h"
+#include "platform/HostWindow.h"
 #include "platform/graphics/GraphicsContext.h"
 #include "platform/graphics/GraphicsContextStateSaver.h"
 #include "platform/graphics/paint/ClipRecorder.h"
@@ -69,8 +70,15 @@ void ScrollableAreaPainter::paintResizer(GraphicsContext& context,
 void ScrollableAreaPainter::drawPlatformResizerImage(
     GraphicsContext& context,
     IntRect resizerCornerRect) {
-  float deviceScaleFactor =
+  float oldDeviceScaleFactor =
       blink::deviceScaleFactor(getScrollableArea().box().frame());
+  // |blink::deviceScaleFactor| returns different values between MAC (2 or 1)
+  // and other platforms (always 1). For this reason we cannot hardcode the
+  // value of 1 in the call for |windowToViewportScalar|. Since zoom-for-dsf is
+  // disabled on MAC, |windowToViewportScalar| will be a no-op on it.
+  float deviceScaleFactor =
+      getScrollableArea().getHostWindow()->windowToViewportScalar(
+          oldDeviceScaleFactor);
 
   RefPtr<Image> resizeCornerImage;
   IntSize cornerResizerSize;
@@ -79,7 +87,8 @@ void ScrollableAreaPainter::drawPlatformResizerImage(
                       (Image::loadPlatformResource("textAreaResizeCorner@2x")));
     resizeCornerImage = resizeCornerImageHiRes;
     cornerResizerSize = resizeCornerImage->size();
-    cornerResizerSize.scale(0.5f);
+    if (oldDeviceScaleFactor >= 2)
+      cornerResizerSize.scale(0.5f);
   } else {
     DEFINE_STATIC_REF(Image, resizeCornerImageLoRes,
                       (Image::loadPlatformResource("textAreaResizeCorner")));
