48e1dc386096b1e74c176a9af442ceca2bfdb908
tzik <tzik@chromium.org>
2017-02-22 14:53:16 -0800

    Move IconLoader::ReadIconThreadID from FILE to UI
    gfx::Image is not thread safe due to non-thread-safe ref count of its
    gfx::ImageStorage. Especially, Image instances in ResourceBundle are
    cached and shared globally. So, touching it from non-UI thread causes
    a data race on its ref count.
    
    BUG=688072, 468010
    
    Review-Url: https://codereview.chromium.org/2707753004
    Cr-Commit-Position: refs/heads/master@{#452250}

--- chrome/browser/icon_loader_chromeos.cc
+++ chrome/browser/icon_loader_chromeos.cc
@@ -193,7 +193,9 @@ IconLoader::IconGroup IconLoader::GroupForFilepath(
 
 // static
 content::BrowserThread::ID IconLoader::ReadIconThreadID() {
-  return content::BrowserThread::FILE;
+  // ReadIcon touches non thread safe ResourceBundle images, so it must be on
+  // the UI thread.
+  return content::BrowserThread::UI;
 }
 
 void IconLoader::ReadIcon() {
