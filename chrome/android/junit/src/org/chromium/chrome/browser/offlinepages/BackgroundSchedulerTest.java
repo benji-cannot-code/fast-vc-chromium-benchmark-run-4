c717873bb91ba2e606f7cc4c573f2cb77aca1aea
fgorski <fgorski@chromium.org>
2017-02-21 17:16:47 -0800

    [Offline pages] Creating BackgroundJobScheduler, which uses JobScheduler
    In order to enable scheduling using JobScheduler, this patch:
    * Refactors BackgroundScheduler to expose its functionality through
      instance methods.
    * Adds BackgroundScheduler#getInstance method to get appropriate
      implementation of BackgroundScheduler (default uses GCM Network
      Manager)
    * Introduces BackgroundJobScheduler, which implements basic
      scheduling using JobScheduler
    * Updates TasksExtrasPacker to handle PersistableBundle, which
      is required by JobScheduler (handling BaseBudnle would work if
      min API level was 21).
    * Updates/adds tests.
    
    BUG=683802,692151
    R=petewil@chromium.org,nyquist@chromium.org
    
    Review-Url: https://codereview.chromium.org/2686203002
    Cr-Commit-Position: refs/heads/master@{#451866}

--- chrome/android/junit/src/org/chromium/chrome/browser/offlinepages/BackgroundSchedulerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/offlinepages/BackgroundSchedulerTest.java
@@ -39,7 +39,7 @@ public class BackgroundSchedulerTest {
 
     @Before
     public void setUp() throws Exception {
-        mContext =  RuntimeEnvironment.application;
+        mContext = RuntimeEnvironment.application;
         mGcmNetworkManager = (ShadowGcmNetworkManager) ShadowExtractor.extract(
                 GcmNetworkManager.getInstance(mContext));
         mGcmNetworkManager.clear();
@@ -49,7 +49,7 @@ public class BackgroundSchedulerTest {
     @Feature({"OfflinePages"})
     public void testSchedule() {
         assertNull(mGcmNetworkManager.getScheduledTask());
-        BackgroundScheduler.schedule(mContext, mConditions1);
+        BackgroundScheduler.getInstance(mContext).schedule(mConditions1);
         // Check with gcmNetworkManagerShadow that schedule got called.
         assertNotNull(mGcmNetworkManager.getScheduledTask());
 
@@ -64,13 +64,13 @@ public class BackgroundSchedulerTest {
 
     @Test
     @Feature({"OfflinePages"})
-    public void testUnschedule() {
+    public void testCancel() {
         assertNull(mGcmNetworkManager.getScheduledTask());
-        BackgroundScheduler.schedule(mContext, mConditions1);
+        BackgroundScheduler.getInstance(mContext).schedule(mConditions1);
         assertNotNull(mGcmNetworkManager.getScheduledTask());
 
         assertNull(mGcmNetworkManager.getCanceledTask());
-        BackgroundScheduler.unschedule(mContext);
+        BackgroundScheduler.getInstance(mContext).cancel();
         assertNotNull(mGcmNetworkManager.getCanceledTask());
     }
 }
