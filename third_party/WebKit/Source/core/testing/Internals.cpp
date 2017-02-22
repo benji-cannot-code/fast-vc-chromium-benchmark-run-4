d0537b15844eb6aef25f9bd0172b950e89c5b421
hiroshige <hiroshige@chromium.org>
2017-02-22 00:29:22 -0800

    FrameView can be null in addTextMatchMaker() after appendChild(iframe)
    Range's owner document's FrameView can be cleared after the iframe
    to which the range belongs to is appendChild()ed.
    This CL fixes Internals::addTextMatchMarker() to handle such cases.
    
    BUG=693848
    
    Review-Url: https://codereview.chromium.org/2706913003
    Cr-Commit-Position: refs/heads/master@{#451929}

--- third_party/WebKit/Source/core/testing/Internals.cpp
+++ third_party/WebKit/Source/core/testing/Internals.cpp
@@ -1042,6 +1042,9 @@ String Internals::markerDescriptionForNode(Node* node,
 
 void Internals::addTextMatchMarker(const Range* range, bool isActive) {
   DCHECK(range);
+  if (!range->ownerDocument().view())
+    return;
+
   range->ownerDocument().updateStyleAndLayoutIgnorePendingStylesheets();
   range->ownerDocument().markers().addTextMatchMarker(EphemeralRange(range),
                                                       isActive);
