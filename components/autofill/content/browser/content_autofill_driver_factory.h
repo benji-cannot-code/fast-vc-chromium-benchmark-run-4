cdd1c8abcf8c0c7594695223bf32373442b18a9b
vabr <vabr@chromium.org>
2017-02-22 15:08:42 -0800

    Split non-//content parts from ContentAutofillDriverFactory
    This CL extracts the logic for managing AutofillDriver instances from
    ContentAutofillDriverFactory and puts it in a separate AutofillDriverFactory
    class. The goal is to achieve better testability. More details are in the
    design doc
    https://docs.google.com/a/chromium.org/document/d/1CZRzRv5p1SnClgOYydZ7epVpEqYxJdgRSCEpRXml-xU/edit?usp=sharing.
    
    BUG=669045
    
    Review-Url: https://codereview.chromium.org/2715483002
    Cr-Commit-Position: refs/heads/master@{#452258}

--- components/autofill/content/browser/content_autofill_driver_factory.h
+++ components/autofill/content/browser/content_autofill_driver_factory.h
@@ -6,12 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_AUTOFILL_CONTENT_BROWSER_CONTENT_AUTOFILL_DRIVER_FACTORY_H_
 #define COMPONENTS_AUTOFILL_CONTENT_BROWSER_CONTENT_AUTOFILL_DRIVER_FACTORY_H_
 
-#include <map>
-#include <memory>
 #include <string>
 
 #include "base/supports_user_data.h"
 #include "components/autofill/content/common/autofill_driver.mojom.h"
+#include "components/autofill/core/browser/autofill_driver_factory.h"
 #include "components/autofill/core/browser/autofill_manager.h"
 #include "content/public/browser/web_contents_observer.h"
 
@@ -25,7 +24,8 @@ class ContentAutofillDriver;
 
 // Manages lifetime of ContentAutofillDriver. One Factory per WebContents
 // creates one Driver per RenderFrame.
-class ContentAutofillDriverFactory : public content::WebContentsObserver,
+class ContentAutofillDriverFactory : public AutofillDriverFactory,
+                                     public content::WebContentsObserver,
                                      public base::SupportsUserData::Data {
  public:
   ~ContentAutofillDriverFactory() override;
@@ -54,20 +54,15 @@ class ContentAutofillDriverFactory : public content::WebContentsObserver,
 
   static const char kContentAutofillDriverFactoryWebContentsUserDataKey[];
 
- protected:
+ private:
   ContentAutofillDriverFactory(
       content::WebContents* web_contents,
       AutofillClient* client,
       const std::string& app_locale,
       AutofillManager::AutofillDownloadManagerState enable_download_manager);
 
- private:
-  AutofillClient* client_;
   std::string app_locale_;
   AutofillManager::AutofillDownloadManagerState enable_download_manager_;
-
-  std::map<content::RenderFrameHost*, std::unique_ptr<ContentAutofillDriver>>
-      frame_driver_map_;
 };
 
 }  // namespace autofill
