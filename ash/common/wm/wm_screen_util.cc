d6172afd06bf8de9b05103338b238e6cc0c1a23c
Mitsuru Oshima <oshima@chromium.org>
2017-02-21 20:46:21 -0800

    Remove unnecessary GetDisplayWorkAreaBounds
    BUG=None
    R=sky@chromium.org
    
    Review-Url: https://codereview.chromium.org/2706043003 .
    Cr-Commit-Position: refs/heads/master@{#451884}

--- ash/common/wm/wm_screen_util.cc
+++ ash/common/wm/wm_screen_util.cc
@@ -16,10 +16,6 @@ namespace ash {
 namespace wm {
 
 gfx::Rect GetDisplayWorkAreaBoundsInParent(WmWindow* window) {
-  return GetDisplayWorkAreaBounds(window->GetParent());
-}
-
-gfx::Rect GetDisplayWorkAreaBounds(WmWindow* window) {
   display::Display display = window->GetDisplayNearestWindow();
   return window->GetParent()->ConvertRectFromScreen(display.work_area());
 }
--- ash/common/wm/wm_screen_util.h
+++ ash/common/wm/wm_screen_util.h
@@ -19,7 +19,6 @@ class WmWindow;
 namespace wm {
 
 ASH_EXPORT gfx::Rect GetDisplayWorkAreaBoundsInParent(WmWindow* window);
-ASH_EXPORT gfx::Rect GetDisplayWorkAreaBounds(WmWindow* window);
 ASH_EXPORT gfx::Rect GetDisplayBoundsInParent(WmWindow* window);
 ASH_EXPORT gfx::Rect GetMaximizedWindowBoundsInParent(WmWindow* window);
 
