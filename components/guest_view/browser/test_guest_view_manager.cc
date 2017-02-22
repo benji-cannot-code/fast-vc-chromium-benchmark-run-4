07fdf41d0a907a38aa725a23abf359fe015e2c20
ke.he <ke.he@intel.com>
2017-02-22 04:09:42 -0800

    Use std::unique_ptr instead of linked_ptr in //components/guest_view
    BUG=556939
    
    Review-Url: https://codereview.chromium.org/2707743002
    Cr-Commit-Position: refs/heads/master@{#452006}

--- components/guest_view/browser/test_guest_view_manager.cc
+++ components/guest_view/browser/test_guest_view_manager.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/memory/ptr_util.h"
 #include "components/guest_view/browser/guest_view_manager_delegate.h"
 
 namespace guest_view {
@@ -97,8 +98,8 @@ void TestGuestViewManager::AddGuest(int guest_instance_id,
   GuestViewManager::AddGuest(guest_instance_id, guest_web_contents);
 
   guest_web_contents_watchers_.push_back(
-      linked_ptr<content::WebContentsDestroyedWatcher>(
-          new content::WebContentsDestroyedWatcher(guest_web_contents)));
+      base::MakeUnique<content::WebContentsDestroyedWatcher>(
+          guest_web_contents));
 
   if (created_message_loop_runner_.get())
     created_message_loop_runner_->Quit();
--- components/guest_view/browser/test_guest_view_manager.h
+++ components/guest_view/browser/test_guest_view_manager.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
-#include "base/memory/linked_ptr.h"
 #include "components/guest_view/browser/guest_view_manager.h"
 #include "components/guest_view/browser/guest_view_manager_factory.h"
 #include "content/public/test/test_utils.h"
@@ -101,7 +100,7 @@ class TestGuestViewManager : public GuestViewManager {
   int num_views_garbage_collected_;
   bool waiting_for_guests_created_;
 
-  std::vector<linked_ptr<content::WebContentsDestroyedWatcher>>
+  std::vector<std::unique_ptr<content::WebContentsDestroyedWatcher>>
       guest_web_contents_watchers_;
   scoped_refptr<content::MessageLoopRunner> created_message_loop_runner_;
   scoped_refptr<content::MessageLoopRunner> num_created_message_loop_runner_;
