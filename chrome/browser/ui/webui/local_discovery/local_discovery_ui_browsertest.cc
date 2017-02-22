4e0f2e3515ebffbf2c584c26527774bfa180e693
vitalybuka <vitalybuka@chromium.org>
2017-02-22 15:38:28 -0800

    Fix flaky tests by disabling DeviceDiscoveryNotification service.
    Tests assume that nothing else uses local discovery code.
    
    BUG=689305,660669
    
    Review-Url: https://codereview.chromium.org/2706183002
    Cr-Commit-Position: refs/heads/master@{#452275}

--- chrome/browser/ui/webui/local_discovery/local_discovery_ui_browsertest.cc
+++ chrome/browser/ui/webui/local_discovery/local_discovery_ui_browsertest.cc
@@ -448,6 +448,7 @@ class LocalDiscoveryUITest : public WebUIBrowserTest {
     command_line->AppendSwitchASCII(chromeos::switches::kLoginProfile,
                                     chrome::kTestUserProfileDir);
 #endif
+    command_line->AppendSwitch(switches::kDisableDeviceDiscoveryNotifications);
     WebUIBrowserTest::SetUpCommandLine(command_line);
   }
 
@@ -517,7 +518,7 @@ IN_PROC_BROWSER_TEST_F(LocalDiscoveryUITest, AddRowTest) {
 }
 
 // Flaky: http://crbug.com/660669.
-IN_PROC_BROWSER_TEST_F(LocalDiscoveryUITest, DISABLED_RegisterTest) {
+IN_PROC_BROWSER_TEST_F(LocalDiscoveryUITest, RegisterTest) {
   TestMessageLoopCondition condition_token_claimed;
 
   ui_test_utils::NavigateToURL(browser(), GURL(
