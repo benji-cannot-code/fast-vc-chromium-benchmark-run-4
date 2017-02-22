7bdc63c7e388dad02f085bb2715365f05adb58fc
vmpstr <vmpstr@chromium.org>
2017-02-22 12:09:02 -0800

    cc: Fix const value pattern by replacing it by non-const or reference.
    This patch changes cc to ensure that we don't have a const value
    parameters. These are replaced by either non-const value or by
    reference to const depending on the situation.
    
    R=danakj@chromium.org
    BUG=691787
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2710593003
    Cr-Commit-Position: refs/heads/master@{#452177}

--- cc/trees/layer_tree_host_unittest_scroll.cc
+++ cc/trees/layer_tree_host_unittest_scroll.cc
@@ -1866,7 +1866,7 @@ class LayerTreeHostScrollTestElasticOverscroll
     PostSetNeedsCommitToMainThread();
   }
 
-  void BindInputHandler(const base::WeakPtr<InputHandler> input_handler) {
+  void BindInputHandler(base::WeakPtr<InputHandler> input_handler) {
     DCHECK(task_runner_provider()->IsImplThread());
     input_handler->BindToClient(&input_handler_client_);
     scroll_elasticity_helper_ = input_handler->CreateScrollElasticityHelper();
