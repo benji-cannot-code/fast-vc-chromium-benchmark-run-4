f9aaae52ce9516e10e0c6e4e6caf7b9f045fca1d
thakis <thakis@chromium.org>
2017-02-22 11:07:21 -0800

    Try to unbreak win/asan build
    BUG=695101
    
    Review-Url: https://codereview.chromium.org/2705333004
    Cr-Commit-Position: refs/heads/master@{#452153}

--- tools/ipc_fuzzer/message_replay/replay_process.cc
+++ tools/ipc_fuzzer/message_replay/replay_process.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
-#include "base/posix/global_descriptors.h"
 #include "base/run_loop.h"
 #include "build/build_config.h"
 #include "chrome/common/chrome_switches.h"
@@ -25,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/edk/embedder/scoped_ipc_support.h"
 
 #if defined(OS_POSIX)
+#include "base/posix/global_descriptors.h"
 #include "content/public/common/content_descriptors.h"
 #endif
 
