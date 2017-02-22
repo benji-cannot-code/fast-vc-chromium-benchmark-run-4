380d27637d09b973725e4aac6917b30fa336cfa0
nicholss <nicholss@chromium.org>
2017-02-22 09:19:32 -0800

    Adding domain and facade supporting classes for remoting iOS app.
    Adding domain type objects that come from other services and are
    stored locally to keep state for the app to display and act on.
    There will be more in the future, but this is all I am using
    for the demo app at the moment.
    The facade class is all mock data at the moment, the goal will be
    for it to hide the impl details internal to remoting and act as
    the facade between c++ and Obj-C.
    
    R=sergeyu@chromium.org
    BUG=652781
    
    Review-Url: https://codereview.chromium.org/2681173002
    Cr-Commit-Position: refs/heads/master@{#452105}

---
+++ remoting/client/ios/facade/remoting_service.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_CLIENT_IOS_FACADE_REMOTING_SERCICE_H_
+#define REMOTING_CLIENT_IOS_FACADE_REMOTING_SERCICE_H_
+
+#import "remoting/client/ios/domain/host_info.h"
+#import "remoting/client/ios/domain/user_info.h"
+
+@interface RemotingService : NSObject
+
++ (RemotingService*)sharedInstance;
+
+- (BOOL)isAuthenticated;
+- (void)authenticateWithAuthorizationCode:(NSString*)authorizationCode;
+- (UserInfo*)getUser;
+- (NSArray<HostInfo*>*)getHosts;
+
+@end
+
+#endif  // REMOTING_CLIENT_IOS_FACADE_REMOTING_SERCICE_H_
