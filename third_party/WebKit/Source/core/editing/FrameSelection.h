049ba522cbaf5adda0395e0a6afaddc05a69ddfa
yosin <yosin@chromium.org>
2017-02-22 01:38:19 -0800

    Expand FrameSeleciton::base() to increase chances of hoisting update layout
    This patch expands |FrameSeleciton::base()| to increase chance of hoisting
    update layout.
    
    Note: This is mechanical change to replace |selection().base()|
    by |selection().computeVisibleSelectionInDOMTreeDeprecated().base()|
    and modify some explicit case use |computeVisibleSelectionInDOMTree()|.
    Following patch will remove deprecated version.
    
    This patch is apart of expanding expensive member functions in |FrameSelection|:
    - rootEditableElement()
    - hasEditableStyle()
    - isContentEditable()
    - isContentRichlyEditable()
    - getSelectionType()
    - base()
    - extent()
    - start()
    - end()
    - isNone()
    - isCaret()
    - isRange()
    
    BUG=694360
    TEST=n/a; no behavior changes
    
    Review-Url: https://codereview.chromium.org/2709793003
    Cr-Commit-Position: refs/heads/master@{#451957}

--- third_party/WebKit/Source/core/editing/FrameSelection.h
+++ third_party/WebKit/Source/core/editing/FrameSelection.h
@@ -187,9 +187,6 @@ class CORE_EXPORT FrameSelection final
 
   TextGranularity granularity() const { return m_granularity; }
 
-  Position base() const {
-    return computeVisibleSelectionInDOMTreeDeprecated().base();
-  }
   Position extent() const {
     return computeVisibleSelectionInDOMTreeDeprecated().extent();
   }
