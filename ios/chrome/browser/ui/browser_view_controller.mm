9f19a16e05851ffa6e8d51f319c7530b5a5ca5a0
kkhorimoto <kkhorimoto@chromium.org>
2017-02-22 14:38:53 -0800

    Updated tab history classes to use NavigationItemLists.
    This CL also updates these classes to use modern Objective-C syntax
    and to follow the style guide (i.e. no property accessors in init).
    
    BUG=454984, 546355
    
    Review-Url: https://codereview.chromium.org/2693013005
    Cr-Commit-Position: refs/heads/master@{#452241}

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -3362,7 +3362,7 @@ class BrowserBookmarkModelBridge : public bookmarks::BookmarkModelObserver {
   DCHECK([tab navigationManager]);
   CRWSessionController* sc = [tab navigationManager]->GetSessionController();
   [_toolbarController showTabHistoryPopupInView:[self view]
-                             withSessionEntries:[sc backwardEntries]
+                                      withItems:[sc backwardItems]
                                  forBackHistory:YES];
 }
 
@@ -3378,14 +3378,14 @@ class BrowserBookmarkModelBridge : public bookmarks::BookmarkModelObserver {
   DCHECK([tab navigationManager]);
   CRWSessionController* sc = [tab navigationManager]->GetSessionController();
   [_toolbarController showTabHistoryPopupInView:[self view]
-                             withSessionEntries:[sc forwardEntries]
+                                      withItems:[sc forwardItems]
                                  forBackHistory:NO];
 }
 
 - (void)navigateToSelectedEntry:(id)sender {
   DCHECK([sender isKindOfClass:[TabHistoryCell class]]);
   TabHistoryCell* selectedCell = (TabHistoryCell*)sender;
-  [[_model currentTab] goToItem:selectedCell.entry.navigationItem];
+  [[_model currentTab] goToItem:selectedCell.item];
   [_toolbarController dismissTabHistoryPopup];
 }
 
