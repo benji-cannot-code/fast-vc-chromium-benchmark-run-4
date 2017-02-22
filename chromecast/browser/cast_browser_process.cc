b79be6c2a692f9e139235ca2c7c65dceaf79be9c
jam <jam@chromium.org>
2017-02-21 21:48:30 -0800

    Fix RendererPrelauncherTest.ReusedRenderer with PlzNavigate.
    There was a UAF with PlzNavigate in ChromeCast. ResourceDispatcherHostImpl::BeginNavigationRequest would run after the CastBrowserProcess object was destructed, which held on to the ResourceDispatcherHostDelegate. Moved ownership to CastContentBrowserClient instead.
    
    Review-Url: https://codereview.chromium.org/2705343002
    Cr-Commit-Position: refs/heads/master@{#451891}

--- chromecast/browser/cast_browser_process.cc
+++ chromecast/browser/cast_browser_process.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chromecast/base/metrics/cast_metrics_helper.h"
 #include "chromecast/browser/cast_browser_context.h"
-#include "chromecast/browser/cast_resource_dispatcher_host_delegate.h"
 #include "chromecast/browser/devtools/remote_debugging_server.h"
 #include "chromecast/browser/metrics/cast_metrics_service_client.h"
 #include "chromecast/net/connectivity_checker.h"
@@ -101,12 +100,6 @@ void CastBrowserProcess::SetRemoteDebuggingServer(
     std::unique_ptr<RemoteDebuggingServer> remote_debugging_server) {
   DCHECK(!remote_debugging_server_);
   remote_debugging_server_.swap(remote_debugging_server);
-}
-
-void CastBrowserProcess::SetResourceDispatcherHostDelegate(
-    std::unique_ptr<CastResourceDispatcherHostDelegate> delegate) {
-  DCHECK(!resource_dispatcher_host_delegate_);
-  resource_dispatcher_host_delegate_.swap(delegate);
 }
 
 void CastBrowserProcess::SetConnectivityChecker(
--- chromecast/browser/cast_browser_process.h
+++ chromecast/browser/cast_browser_process.h
@@ -31,7 +31,6 @@ class CastMetricsServiceClient;
 namespace shell {
 class CastBrowserContext;
 class CastContentBrowserClient;
-class CastResourceDispatcherHostDelegate;
 class RemoteDebuggingServer;
 
 class CastBrowserProcess {
@@ -57,8 +56,6 @@ class CastBrowserProcess {
   void SetPrefService(std::unique_ptr<PrefService> pref_service);
   void SetRemoteDebuggingServer(
       std::unique_ptr<RemoteDebuggingServer> remote_debugging_server);
-  void SetResourceDispatcherHostDelegate(
-      std::unique_ptr<CastResourceDispatcherHostDelegate> delegate);
   void SetConnectivityChecker(
       scoped_refptr<ConnectivityChecker> connectivity_checker);
   void SetNetLog(net::NetLog* net_log);
@@ -75,10 +72,6 @@ class CastBrowserProcess {
     return metrics_service_client_.get();
   }
   PrefService* pref_service() const { return pref_service_.get(); }
-  CastResourceDispatcherHostDelegate* resource_dispatcher_host_delegate()
-      const {
-    return resource_dispatcher_host_delegate_.get();
-  }
   ConnectivityChecker* connectivity_checker() const {
     return connectivity_checker_.get();
   }
@@ -95,8 +88,6 @@ class CastBrowserProcess {
   scoped_refptr<ConnectivityChecker> connectivity_checker_;
   std::unique_ptr<CastBrowserContext> browser_context_;
   std::unique_ptr<metrics::CastMetricsServiceClient> metrics_service_client_;
-  std::unique_ptr<CastResourceDispatcherHostDelegate>
-      resource_dispatcher_host_delegate_;
   std::unique_ptr<RemoteDebuggingServer> remote_debugging_server_;
 
   CastContentBrowserClient* cast_content_browser_client_;
