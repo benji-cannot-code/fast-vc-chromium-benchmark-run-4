e543f780693f4d4fb6f99b1bb161929c6e9fb3ae
fdoray <fdoray@chromium.org>
2017-02-22 15:34:58 -0800

    Use TaskScheduler instead of blocking pool in extension_loader_handler.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=finnur@chromium.org
    
    Review-Url: https://codereview.chromium.org/2676613005
    Cr-Commit-Position: refs/heads/master@{#452272}

--- chrome/browser/ui/webui/extensions/extension_loader_handler.cc
+++ chrome/browser/ui/webui/extensions/extension_loader_handler.cc
@@ -15,12 +15,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/threading/sequenced_worker_pool.h"
+#include "base/task_scheduler/post_task.h"
 #include "chrome/browser/extensions/path_util.h"
 #include "chrome/browser/extensions/unpacked_installer.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/grit/generated_resources.h"
-#include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
@@ -161,15 +160,13 @@ void ExtensionLoaderHandler::OnLoadFailure(
 
   // This will read the manifest and call AddFailure with the read manifest
   // contents.
-  base::PostTaskAndReplyWithResult(
-      content::BrowserThread::GetBlockingPool(),
+  base::PostTaskWithTraitsAndReplyWithResult(
       FROM_HERE,
+      base::TaskTraits().MayBlock().WithPriority(
+          base::TaskPriority::USER_BLOCKING),
       base::Bind(&ReadFileToString, file_path.Append(kManifestFilename)),
       base::Bind(&ExtensionLoaderHandler::AddFailure,
-                 weak_ptr_factory_.GetWeakPtr(),
-                 file_path,
-                 error,
-                 line));
+                 weak_ptr_factory_.GetWeakPtr(), file_path, error, line));
 }
 
 void ExtensionLoaderHandler::DidStartNavigation(
