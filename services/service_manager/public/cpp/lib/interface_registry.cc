8b96a07194d30c0f6331a6d34ea0ab42078b1593
rockot <rockot@chromium.org>
2017-02-22 11:34:20 -0800

    Service Manager: Reduce InterfaceProvider spec spam to DVLOG(1)
    Demotes this spam from LOG(WARNING) to DVLOG(1) because it's too
    noisy otherwise.
    
    BUG=694939
    
    Review-Url: https://codereview.chromium.org/2704303007
    Cr-Commit-Position: refs/heads/master@{#452164}

--- services/service_manager/public/cpp/lib/interface_registry.cc
+++ services/service_manager/public/cpp/lib/interface_registry.cc
@@ -215,7 +215,7 @@ void InterfaceRegistry::GetInterface(const std::string& interface_name,
 
       std::stringstream details;
       Serialize(&details);
-      LOG(WARNING) << details.str();
+      DVLOG(1) << details.str();
     }
   } else {
     std::stringstream error;
@@ -227,7 +227,7 @@ void InterfaceRegistry::GetInterface(const std::string& interface_name,
 
     std::stringstream details;
     Serialize(&details);
-    LOG(WARNING) << details.str();
+    DVLOG(1) << details.str();
   }
 }
 
