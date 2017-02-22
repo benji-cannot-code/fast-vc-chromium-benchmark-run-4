0ab048a9e062afc4cc1a728263b3e2c72ded3e27
pkl <pkl@chromium.org>
2017-02-22 15:57:04 -0800

    Remove IsRunningOnIOS9OrLater().
    No more users of this call.
    
    BUG=633626
    
    Review-Url: https://codereview.chromium.org/2711463006
    Cr-Commit-Position: refs/heads/master@{#452285}

--- base/ios/ios_util.h
+++ base/ios/ios_util.h
@@ -14,9 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 namespace ios {
 
-// Returns whether the operating system is iOS 9 or later.
-BASE_EXPORT bool IsRunningOnIOS9OrLater();
-
 // Returns whether the operating system is iOS 10 or later.
 BASE_EXPORT bool IsRunningOnIOS10OrLater();
 
