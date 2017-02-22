8f61cb1453f07da843572c4059f31a271510c57c
kkhorimoto <kkhorimoto@chromium.org>
2017-02-22 15:25:39 -0800

    Remove CRWSessionEntry from CRWSessionCertificatePolicyManager.
    BUG=454984
    
    Review-Url: https://codereview.chromium.org/2703923002
    Cr-Commit-Position: refs/heads/master@{#452267}

--- ios/web/navigation/crw_session_certificate_policy_manager.h
+++ ios/web/navigation/crw_session_certificate_policy_manager.h
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "net/cert/cert_status_flags.h"
 
-@class CRWSessionEntry;
-
 namespace net {
 class X509Certificate;
 }
