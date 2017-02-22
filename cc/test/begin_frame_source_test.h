8694003265f75f1bd72063002f668e45a7d35c69
eseckler <eseckler@chromium.org>
2017-02-22 03:25:09 -0800

    [cc] Track observer status in ExternalBeginFrameSource.
    This introduces a BeginFrameObserverAckTracker, which tracks which
    active observers have finished the current BeginFrame, and hooks it up
    to ExternalBeginFrameSource.
    
    BUG=646774, 401331
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2691453002
    Cr-Commit-Position: refs/heads/master@{#451997}

--- cc/test/begin_frame_source_test.h
+++ cc/test/begin_frame_source_test.h
@@ -38,6 +38,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       .InSequence((obs).sequence)                                              \
       .WillOnce(::testing::SaveArg<0>(&((obs).last_begin_frame_args)))
 
+#define EXPECT_BEGIN_FRAME_ARGS_DROP(obs, args) \
+  EXPECT_CALL((obs), OnBeginFrame(args)).Times(1).InSequence((obs).sequence)
+
+#define EXPECT_BEGIN_FRAME_ARGS_USED(obs, args) \
+  EXPECT_CALL((obs), OnBeginFrame(args))        \
+      .InSequence((obs).sequence)               \
+      .WillOnce(::testing::SaveArg<0>(&((obs).last_begin_frame_args)))
+
 #define EXPECT_BEGIN_FRAME_SOURCE_PAUSED(obs, paused)         \
   EXPECT_CALL((obs), OnBeginFrameSourcePausedChanged(paused)) \
       .Times(1)                                               \
