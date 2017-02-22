9e46ea0001c0edc104df8fd9dfa30c94ac11cae4
jianli <jianli@chromium.org>
2017-02-22 12:53:03 -0800

    Should not skip link element that could appear within body.
    BUG=694643
    TEST=test updated
    
    Review-Url: https://codereview.chromium.org/2701313005
    Cr-Commit-Position: refs/heads/master@{#452192}

--- third_party/WebKit/Source/web/WebFrameSerializer.cpp
+++ third_party/WebKit/Source/web/WebFrameSerializer.cpp
@@ -132,7 +132,7 @@ bool MHTMLFrameSerializerDelegate::shouldIgnoreHiddenElement(
     return false;
   if (isHTMLHeadElement(element) || isHTMLMetaElement(element) ||
       isHTMLStyleElement(element) || isHTMLDataListElement(element) ||
-      isHTMLOptionElement(element)) {
+      isHTMLOptionElement(element) || isHTMLLinkElement(element)) {
     return false;
   }
   Element* parent = element.parentElement();
