3f47a3f68133eedc8240f51d36b10463ecb58a38
dfalcantara <dfalcantara@chromium.org>
2017-02-22 12:11:33 -0800

    Temporarily expose "backKeyPressed"
    Allows subclasses to handle the back key, as well.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2706063005
    Cr-Commit-Position: refs/heads/master@{#452179}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
@@ -1990,7 +1990,8 @@ public class LocationBarLayout extends FrameLayout
         }
     }
 
-    private void backKeyPressed() {
+    // TODO(dfalcantara): Make private again after M58.
+    protected void backKeyPressed() {
         hideSuggestions();
         UiUtils.hideKeyboard(mUrlBar);
         // Revert the URL to match the current page.
