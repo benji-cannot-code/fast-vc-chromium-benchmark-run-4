f8c500a2bbef5d3addeddbd8271c45e9241ee110
sigbjornf <sigbjornf@opera.com>
2017-02-23 03:00:47 -0800

    Avoid needless InstanceCounter.h inclusion.
    Only needed if RefCounted<> is compiled specially.
    
    R=
    BUG=
    
    Review-Url: https://codereview.chromium.org/2714703002
    Cr-Commit-Position: refs/heads/master@{#452453}

--- third_party/WebKit/Source/wtf/RefCounted.h
+++ third_party/WebKit/Source/wtf/RefCounted.h
@@ -24,9 +24,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "wtf/Allocator.h"
 #include "wtf/Assertions.h"
-#include "wtf/InstanceCounter.h"
 #include "wtf/Noncopyable.h"
 #include "wtf/WTFExport.h"
+
+#if ENABLE(INSTANCE_COUNTER)
+#include "wtf/InstanceCounter.h"
+#endif
 
 #if DCHECK_IS_ON()
 #define CHECK_REF_COUNTED_LIFECYCLE 1
@@ -153,7 +156,7 @@ class RefCounted : public RefCountedBase {
   }
 
  protected:
-#ifdef ENABLE_INSTANCE_COUNTER
+#if ENABLE(INSTANCE_COUNTER)
   RefCounted() { incrementInstanceCount<T>(static_cast<T*>(this)); }
 
   ~RefCounted() { decrementInstanceCount<T>(static_cast<T*>(this)); }
