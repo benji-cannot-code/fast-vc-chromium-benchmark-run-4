d9375799b9a1e0b941c2dc968d67a3d953dbedf5
bauerb <bauerb@chromium.org>
2017-02-23 09:14:04 -0800

    [Webview, Child Accounts] Always allow Google Play Services to show the reauthentication page.
    The reauthentication page is necessary to fix account issues e.g. after
    a password change. The whitelisting is only for accounts.google.com
    and only if the calling package is the Google Play Services UI.
    
    BUG=695048
    
    Review-Url: https://codereview.chromium.org/2713513004
    Cr-Commit-Position: refs/heads/master@{#452528}

--- chrome/android/junit/src/org/chromium/chrome/browser/superviseduser/SupervisedUserContentProviderUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/superviseduser/SupervisedUserContentProviderUnitTest.java
@@ -54,6 +54,8 @@ public class SupervisedUserContentProviderUnitTest {
 
     private SupervisedUserContentProvider mSupervisedUserContentProvider;
 
+    private static final String DEFAULT_CALLING_PACKAGE = "com.example.some.app";
+
     @Before
     public void setUp() {
         // Ensure clean state (in particular not signed in).
@@ -101,7 +103,8 @@ public class SupervisedUserContentProviderUnitTest {
     public void testShouldProceed_PermittedUrl() {
         mSupervisedUserContentProvider.setNativeSupervisedUserContentProviderForTesting(1234L);
         // Mock the native call for a permitted URL
-        WebRestrictionsResult result = mSupervisedUserContentProvider.shouldProceed("url");
+        WebRestrictionsResult result =
+                mSupervisedUserContentProvider.shouldProceed(DEFAULT_CALLING_PACKAGE, "url");
         assertThat(result.shouldProceed(), is(true));
         verify(mSupervisedUserContentProvider)
                 .nativeShouldProceed(eq(1234L),
@@ -128,7 +131,8 @@ public class SupervisedUserContentProviderUnitTest {
                 .nativeShouldProceed(anyLong(),
                         any(SupervisedUserContentProvider.SupervisedUserQueryReply.class),
                         anyString());
-        WebRestrictionsResult result = mSupervisedUserContentProvider.shouldProceed("url");
+        WebRestrictionsResult result =
+                mSupervisedUserContentProvider.shouldProceed(DEFAULT_CALLING_PACKAGE, "url");
         assertThat(result.shouldProceed(), is(false));
         assertThat(result.errorIntCount(), is(3));
         assertThat(result.getErrorInt(0), is(1));
@@ -188,7 +192,8 @@ public class SupervisedUserContentProviderUnitTest {
         ChromeBrowserInitializer mockBrowserInitializer = mock(ChromeBrowserInitializer.class);
         ChromeBrowserInitializer.setForTesting(mockBrowserInitializer);
 
-        WebRestrictionsResult result = mSupervisedUserContentProvider.shouldProceed("url");
+        WebRestrictionsResult result =
+                mSupervisedUserContentProvider.shouldProceed(DEFAULT_CALLING_PACKAGE, "url");
 
         assertThat(result.shouldProceed(), is(true));
         verify(mockBrowserInitializer).handleSynchronousStartup();
@@ -210,7 +215,8 @@ public class SupervisedUserContentProviderUnitTest {
         Account account = new Account("Google", "Dummy");
         when(mockDelegate.getAccountsByType("Google")).thenReturn(new Account[] {account});
 
-        WebRestrictionsResult result = mSupervisedUserContentProvider.shouldProceed("url");
+        WebRestrictionsResult result =
+                mSupervisedUserContentProvider.shouldProceed(DEFAULT_CALLING_PACKAGE, "url");
 
         assertThat(result.shouldProceed(), is(true));
         verify(mockBrowserInitializer).handleSynchronousStartup();
@@ -225,7 +231,7 @@ public class SupervisedUserContentProviderUnitTest {
     }
 
     @Test
-    public void testShouldProceed_cannotSignIn() throws ProcessInitException {
+    public void testShouldProceed_cannotSignIn() {
         // Mock things called during startup
         ChromeBrowserInitializer mockBrowserInitializer = mock(ChromeBrowserInitializer.class);
         ChromeBrowserInitializer.setForTesting(mockBrowserInitializer);
@@ -246,9 +252,46 @@ public class SupervisedUserContentProviderUnitTest {
                 .when(mSupervisedUserContentProvider)
                 .startForcedSigninProcessor(any(Context.class), any(Runnable.class));
 
-        WebRestrictionsResult result = mSupervisedUserContentProvider.shouldProceed("url");
+        WebRestrictionsResult result =
+                mSupervisedUserContentProvider.shouldProceed(DEFAULT_CALLING_PACKAGE, "url");
 
         assertThat(result.shouldProceed(), is(false));
         assertThat(result.getErrorInt(0), is(5));
+    }
+
+    @Test
+    public void testShouldProceed_requestWhitelisted() {
+        mSupervisedUserContentProvider.setNativeSupervisedUserContentProviderForTesting(1234L);
+
+        // Modify the result of the native call to block any URL.
+        doAnswer(new Answer<Void>() {
+            @Override
+            public Void answer(InvocationOnMock invocation) throws Throwable {
+                invocation.<SupervisedUserQueryReply>getArgument(1).onQueryFailed(1, 2, 3, "url1",
+                        "url2", "custodian", "custodianEmail", "secondCustodian",
+                        "secondCustodianEmail");
+                return null;
+            }
+        })
+                .when(mSupervisedUserContentProvider)
+                .nativeShouldProceed(eq(1234L),
+                        any(SupervisedUserContentProvider.SupervisedUserQueryReply.class),
+                        anyString());
+
+        WebRestrictionsResult allowed = mSupervisedUserContentProvider.shouldProceed(
+                "com.google.android.gms.ui", "https://accounts.google.com/reauth");
+        assertThat(allowed.shouldProceed(), is(true));
+
+        WebRestrictionsResult wrongUrl = mSupervisedUserContentProvider.shouldProceed(
+                "com.google.android.gms.ui", "http://www.example.com");
+        assertThat(wrongUrl.shouldProceed(), is(false));
+
+        WebRestrictionsResult wrongCallingPackage = mSupervisedUserContentProvider.shouldProceed(
+                DEFAULT_CALLING_PACKAGE, "https://accounts.google.com/reauth");
+        assertThat(wrongCallingPackage.shouldProceed(), is(false));
+
+        WebRestrictionsResult nullCallingPackage = mSupervisedUserContentProvider.shouldProceed(
+                null, "https://accounts.google.com/reauth");
+        assertThat(nullCallingPackage.shouldProceed(), is(false));
     }
 }
