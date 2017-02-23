50af1d5a24a2cf4978b00e81fa4a5e626b310aa8
afakhry <afakhry@chromium.org>
2017-02-23 13:50:28 -0800

    Make the tab scrubber speed proportional with the number of tabs
    When the number of tabs increases, tab scrubbing speed becomes too fast
    that it's very hard to target a specific tab.
    
    This CL makes the speed proportional with the number of tabs.
    
    BUG=678316
    TEST=When you add a lot of tabs, tab scrubbing is still easy.
    
    Review-Url: https://codereview.chromium.org/2703373009
    Cr-Commit-Position: refs/heads/master@{#452644}

--- chrome/browser/ui/views/ash/tab_scrubber.cc
+++ chrome/browser/ui/views/ash/tab_scrubber.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <algorithm>
+
 #include "ash/shell.h"
 #include "ash/wm/window_util.h"
 #include "base/metrics/histogram_macros.h"
@@ -26,7 +28,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/glow_hover_controller.h"
 
 namespace {
+
 const int64_t kActivationDelayMS = 200;
+
+inline float Clamp(float value, float low, float high) {
+  return std::min(high, std::max(value, low));
+}
 }
 
 // static
@@ -307,7 +314,12 @@ void TabScrubber::UpdateSwipeX(float x_offset) {
   DCHECK(tab_strip_);
   DCHECK(scrubbing_);
 
-  swipe_x_ += x_offset;
+  // Make the swipe speed inversely proportional with the number or tabs:
+  // Each added tab introduces a reduction of 2% in |x_offset|, with a value of
+  // one fourth of |x_offset| as the minimum (i.e. we need 38 tabs to reach
+  // that minimum reduction).
+  swipe_x_ += Clamp(x_offset - (tab_strip_->tab_count() * 0.02f * x_offset),
+                    0.25f * x_offset, x_offset);
 
   // In an RTL layout, everything is mirrored, i.e. the index of the first tab
   // (with the smallest X mirrored co-ordinates) is actually the index of the
