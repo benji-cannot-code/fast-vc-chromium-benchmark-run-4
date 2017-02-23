2c9549598c4794081b74f59d3b6aaf333cb34ec8
bsazonov <bsazonov@chromium.org>
2017-02-23 03:03:04 -0800

    Revised AccountsChangedReceiver.AccountsChangedObserver and related methods
    This CL removes Context and Intent parameters from of onAccountsChanged.
    These parameters are not used by AccountsChangedObserver implementations.
    Considering shift from broadcast to OnAccountsUpdateListener (that seems more
    appropriate for handling account changes), it makes sense to remove these
    parameters altogher.
    
    BUG=NONE
    
    Review-Url: https://codereview.chromium.org/2709253002
    Cr-Commit-Position: refs/heads/master@{#452454}

--- chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
@@ -7,7 +7,6 @@ package org.chromium.chrome.browser.init;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.view.View;
 
 import com.google.ipc.invalidation.external.client.android.service.AndroidLogger;
@@ -211,7 +210,7 @@ public class ProcessInitializationHandler {
                 AccountsChangedReceiver.addObserver(
                         new AccountsChangedReceiver.AccountsChangedObserver() {
                             @Override
-                            public void onAccountsChanged(Context context, Intent intent) {
+                            public void onAccountsChanged() {
                                 ThreadUtils.runOnUiThread(new Runnable() {
                                     @Override
                                     public void run() {
