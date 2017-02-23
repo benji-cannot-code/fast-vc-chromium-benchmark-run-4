bce62a86709951672479d6e89f59c397bac73d5f
geofflang <geofflang@chromium.org>
2017-02-23 09:46:49 -0800

    Hook up the ANGLE extensions for disabling client-side data.
    BUG=602737
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2695063003
    Cr-Commit-Position: refs/heads/master@{#452542}

--- ui/gl/gl_bindings.h
+++ ui/gl/gl_bindings.h
@@ -111,6 +111,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // GL_ANGLE_instanced_arrays
 #define GL_VERTEX_ATTRIB_ARRAY_DIVISOR_ANGLE             0x88FE
 
+// GL_ANGLE_client_arrays
+#define GL_CLIENT_ARRAYS_ANGLE 0x93AA
+
 // GL_EXT_occlusion_query_boolean
 #define GL_ANY_SAMPLES_PASSED_EXT                        0x8C2F
 #define GL_ANY_SAMPLES_PASSED_CONSERVATIVE_EXT           0x8D6A
