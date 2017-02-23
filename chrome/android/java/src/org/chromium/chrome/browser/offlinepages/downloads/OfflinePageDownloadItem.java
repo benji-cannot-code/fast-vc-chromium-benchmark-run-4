711728dff92f2babf34a1c9475aadded2b486516
shaktisahu <shaktisahu@chromium.org>
2017-02-22 18:57:54 -0800

    [Download Home] Displaying offline page bundle per day
    This CL adds suggested offline pages to the DownloadHistoryAdapter.
    The suggested pages are grouped into a bundle for each day and placed
    at the bottom of each day's download history. The pages can be shown or
    hidden through tapping an up/down arrow on the subsection header.
    
    Key points of this CL:
    1- To keep DateDividedAdapter unaware of offline suggestions,
    DateDividedAdapter.ItemGroup was subclassed into a separate
    DownloadItemGroup class which contains the special sorting function for
    this purpose. Based on whether the offline pages section is expanded or
    not, the offline items are filtered out before adding to the adapter.
    
    2- For displaying the section header for the suggested offline pages,
    a TimedItem is added to the adapter which is passed the required
    information about the group it is associated to. OfflineGroupHeaderView
    and OfflineGroupHeaderViewHolder classes represent the view portion
    for the header. The section header is not selectable at the moment
    which would be addressed in a future CL.
    
    BUG=689801
    
    Review-Url: https://codereview.chromium.org/2670083002
    Cr-Commit-Position: refs/heads/master@{#452361}

--- chrome/android/java/src/org/chromium/chrome/browser/offlinepages/downloads/OfflinePageDownloadItem.java
+++ chrome/android/java/src/org/chromium/chrome/browser/offlinepages/downloads/OfflinePageDownloadItem.java
@@ -76,4 +76,9 @@ public class OfflinePageDownloadItem {
     public long getTotalBytes() {
         return mTotalBytes;
     }
+
+    /** @return Whether this page is to be shown in the suggested reading section. */
+    public boolean isSuggested() {
+        return false;
+    }
 }
