45cf8a6a6c8052e4625441b0b40f5937dde73fca
grt <grt@chromium.org>
2017-02-23 00:12:17 -0800

    Use InstallDetails in installer_util.
    This change removes the following functionality in install_util that is
    now performed once during startup in chrome_elf or setup.exe:
    
    - ChannelInfo::GetChannelName
    - InstallUtil::IsPerUserInstall
    - InstallUtil::IsMultiInstall
    - InstallUtil::IsChromeSxSProcess
    
    The latter three functions now simply return the values held in the
    process's InstallDetails instance, which is valid in both chrome and
    setup. The same is true for GoogleUpdateSettings::GetChromeChannel and
    GoogleUpdateSettings::GetChromeChannelAndModifiers.
    
    Finally, the CHROME_PROBED_PROGRAM_FILES_PATH environment variable hack
    to make IsPerUserInstall work in sandboxed procs has been removed, as
    SHGetFolderPath is no longer used to determine whether or not Chrome is
    running from within C:\Program Files{, (x86)}.
    
    BUG=373987
    
    Review-Url: https://codereview.chromium.org/2476573004
    Cr-Commit-Position: refs/heads/master@{#452431}

--- chrome/browser/google/google_update_win_unittest.cc
+++ chrome/browser/google/google_update_win_unittest.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/registry.h"
 #include "base/win/scoped_comptr.h"
 #include "chrome/common/chrome_version.h"
+#include "chrome/install_static/test/scoped_install_details.h"
 #include "chrome/installer/util/browser_distribution.h"
 #include "chrome/installer/util/google_update_settings.h"
 #include "chrome/installer/util/helper.h"
@@ -532,7 +533,8 @@ class GoogleUpdateWinTest : public ::testing::TestWithParam<bool> {
   GoogleUpdateWinTest()
       : task_runner_(new base::TestSimpleTaskRunner()),
         task_runner_handle_(task_runner_),
-        system_level_install_(GetParam()) {}
+        system_level_install_(GetParam()),
+        scoped_install_details_(system_level_install_, 0, true) {}
 
   void SetUp() override {
     ::testing::TestWithParam<bool>::SetUp();
@@ -625,6 +627,7 @@ class GoogleUpdateWinTest : public ::testing::TestWithParam<bool> {
   scoped_refptr<base::TestSimpleTaskRunner> task_runner_;
   base::ThreadTaskRunnerHandle task_runner_handle_;
   bool system_level_install_;
+  install_static::ScopedInstallDetails scoped_install_details_;
   std::unique_ptr<base::ScopedPathOverride> file_exe_override_;
   std::unique_ptr<base::ScopedPathOverride> program_files_override_;
   std::unique_ptr<base::ScopedPathOverride> program_files_x86_override_;
--- chrome/install_static/install_util_unittest.cc
+++ chrome/install_static/install_util_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/test/test_reg_util_win.h"
 #include "chrome/install_static/install_details.h"
+#include "chrome/install_static/install_modes.h"
 #include "chrome/install_static/test/scoped_install_details.h"
 #include "chrome_elf/nt_registry/nt_registry.h"
 #include "testing/gmock/include/gmock/gmock.h"
