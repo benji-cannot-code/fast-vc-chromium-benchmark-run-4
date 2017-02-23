60dece474ae7e70fef3eb1a3fb9a367ad60084ad
dalecurtis <dalecurtis@chromium.org>
2017-02-23 15:34:01 -0800

    Fix WebAudio support for discrete channel layouts.
    The code would fire DCHECKs in debug builds since it was not properly
    handling creation requests for discrete layouts > 8 channels.
    
    BUG=671308
    TEST=new unittest.
    
    Review-Url: https://codereview.chromium.org/2708933005
    Cr-Commit-Position: refs/heads/master@{#452672}

--- content/renderer/media/renderer_webaudiodevice_impl.h
+++ content/renderer/media/renderer_webaudiodevice_impl.h
@@ -36,6 +36,7 @@ class CONTENT_EXPORT RendererWebAudioDeviceImpl
 
   static RendererWebAudioDeviceImpl* Create(
       media::ChannelLayout layout,
+      int channels,
       const blink::WebAudioLatencyHint& latency_hint,
       blink::WebAudioDevice::RenderCallback* callback,
       int session_id,
@@ -58,6 +59,10 @@ class CONTENT_EXPORT RendererWebAudioDeviceImpl
   void SetMediaTaskRunnerForTesting(
       const scoped_refptr<base::SingleThreadTaskRunner>& media_task_runner);
 
+  const media::AudioParameters& get_sink_params_for_testing() {
+    return sink_params_;
+  }
+
  protected:
   // Callback to get output device params (for tests).
   using OutputDeviceParamsCallback = base::Callback<media::AudioParameters(
@@ -70,6 +75,7 @@ class CONTENT_EXPORT RendererWebAudioDeviceImpl
   using RenderFrameIdCallback = base::Callback<int()>;
 
   RendererWebAudioDeviceImpl(media::ChannelLayout layout,
+                             int channels,
                              const blink::WebAudioLatencyHint& latency_hint,
                              blink::WebAudioDevice::RenderCallback* callback,
                              int session_id,
