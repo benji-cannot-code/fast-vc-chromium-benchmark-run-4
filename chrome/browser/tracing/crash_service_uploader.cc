dd15f017d046eca61713b3ea5fb3d3bbe4eda020
rhalavati <rhalavati@chromium.org>
2017-02-22 23:01:30 -0800

    Cookies disabled for crash_service_uploader.
    Cookies disabled for network request of crash_service_uploader.cc in
    chrome/browser/tracing.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2705363003
    Cr-Commit-Position: refs/heads/master@{#452424}

--- chrome/browser/tracing/crash_service_uploader.cc
+++ chrome/browser/tracing/crash_service_uploader.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/version_info/version_info.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/common/content_switches.h"
+#include "net/base/load_flags.h"
 #include "net/base/mime_util.h"
 #include "net/base/network_delegate.h"
 #include "net/proxy/proxy_config.h"
@@ -314,6 +315,8 @@ void TraceCrashServiceUploader::CreateAndStartURLFetcher(
   data_use_measurement::DataUseUserData::AttachToFetcher(
       url_fetcher_.get(),
       data_use_measurement::DataUseUserData::TRACING_UPLOADER);
+  url_fetcher_->SetLoadFlags(net::LOAD_DO_NOT_SAVE_COOKIES |
+                             net::LOAD_DO_NOT_SEND_COOKIES);
   url_fetcher_->SetRequestContext(request_context_);
   url_fetcher_->SetUploadData(content_type, post_data);
   url_fetcher_->Start();
