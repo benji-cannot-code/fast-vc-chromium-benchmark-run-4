0a60d7910e23078dd23fda60eae19dec42d1355b
dgozman <dgozman@chromium.org>
2017-02-23 12:20:18 -0800

    [DevTools] Do not access heapProfilerAgent directly.
    This patch wraps all calls to heapProfilerAgent with HeapProfilerModel methods.
    
    Next patch will create HeapProfilerModel conditionally, ensuring proper JS capability check,
    and adapt all callers to account for that.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2710963003
    Cr-Commit-Position: refs/heads/master@{#452604}

--- third_party/WebKit/LayoutTests/inspector/profiler/heap-snapshot-test.js
+++ third_party/WebKit/LayoutTests/inspector/profiler/heap-snapshot-test.js
@@ -697,7 +697,7 @@ InspectorTest.takeAndOpenSnapshot = function(generator, callback)
         }
         snapshot.snapshot.typeId = "HEAP";
         profileType._addHeapSnapshotChunk({data: JSON.stringify(snapshot)});
-        setTimeout(callback2, 0);
+        return Promise.resolve(callback2(null));
     }
     InspectorTest.override(InspectorTest.HeapProfilerAgent, "takeHeapSnapshot", pushGeneratedSnapshot);
     InspectorTest._takeAndOpenSnapshotCallback = callback;
