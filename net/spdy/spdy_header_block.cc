35f112918af3a3f4c7c65c2dff7b9bc55c4d58a2
bnc <bnc@chromium.org>
2017-02-23 15:14:08 -0800

    Add bucket count to linked_hash_map; use in SpdyHeaderBlock.
    This lands parts of server side changes
    112518607 and 112960768 by birenroy.
    
    Review-Url: https://codereview.chromium.org/2688473006
    Cr-Commit-Position: refs/heads/master@{#452666}

--- net/spdy/spdy_header_block.cc
+++ net/spdy/spdy_header_block.cc
@@ -29,6 +29,14 @@ using std::string;
 namespace net {
 namespace {
 
+// By default, linked_hash_map's internal map allocates space for 100 map
+// buckets on construction, which is larger than necessary.  Standard library
+// unordered map implementations use a list of prime numbers to set the bucket
+// count for a particular capacity.  |kInitialMapBuckets| is chosen to reduce
+// memory usage for small header blocks, at the cost of having to rehash for
+// large header blocks.
+const size_t kInitialMapBuckets = 11;
+
 // SpdyHeaderBlock::Storage allocates blocks of this size by default.
 const size_t kDefaultStorageBlockSize = 2048;
 
@@ -98,6 +106,8 @@ class SpdyHeaderBlock::Storage {
 
  private:
   UnsafeArena arena_;
+
+  DISALLOW_COPY_AND_ASSIGN(Storage);
 };
 
 SpdyHeaderBlock::HeaderValue::HeaderValue(Storage* storage,
@@ -213,12 +223,9 @@ string SpdyHeaderBlock::ValueProxy::as_string() const {
   }
 }
 
-SpdyHeaderBlock::SpdyHeaderBlock() {}
+SpdyHeaderBlock::SpdyHeaderBlock() : block_(kInitialMapBuckets) {}
 
-SpdyHeaderBlock::SpdyHeaderBlock(SpdyHeaderBlock&& other) {
-  block_.swap(other.block_);
-  storage_.swap(other.storage_);
-}
+SpdyHeaderBlock::SpdyHeaderBlock(SpdyHeaderBlock&& other) = default;
 
 SpdyHeaderBlock::~SpdyHeaderBlock() {}
 
@@ -248,6 +255,7 @@ string SpdyHeaderBlock::DebugString() const {
   if (empty()) {
     return "{}";
   }
+
   string output = "\n{\n";
   for (auto it = begin(); it != end(); ++it) {
     output +=
