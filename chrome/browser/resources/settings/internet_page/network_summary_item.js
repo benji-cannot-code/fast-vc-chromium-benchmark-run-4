9df4931d2dcce8ab14aa991925f33cc3186e5b7d
stevenjb <stevenjb@chromium.org>
2017-02-23 15:22:36 -0800

    MD Settings: Internet: Use custom a11y labels for network toggle buttons
    BUG=673944
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2707033006
    Cr-Commit-Position: refs/heads/master@{#452671}

--- chrome/browser/resources/settings/internet_page/network_summary_item.js
+++ chrome/browser/resources/settings/internet_page/network_summary_item.js
@@ -14,7 +14,7 @@ var DeviceStateProperties;
 Polymer({
   is: 'network-summary-item',
 
-  behaviors: [Polymer.IronA11yKeysBehavior],
+  behaviors: [Polymer.IronA11yKeysBehavior, I18nBehavior],
 
   properties: {
     /**
@@ -31,9 +31,6 @@ Polymer({
      * @type {!CrOnc.NetworkStateProperties|undefined}
      */
     activeNetworkState: Object,
-
-    /** String for a11y purposes. */
-    itemName: String,
 
     /**
      * List of all network state data for the network type.
@@ -236,6 +233,26 @@ Polymer({
   enableToggleIsEnabled_: function(deviceState) {
     return deviceState.State !=
         chrome.networkingPrivate.DeviceStateType.PROHIBITED;
+  },
+
+  /**
+   * @param {!DeviceStateProperties} deviceState
+   * @return {string}
+   * @private
+   */
+  getToggleA11yString_: function(deviceState) {
+    if (!this.enableToggleIsVisible_(deviceState))
+      return '';
+    switch (deviceState.Type) {
+      case CrOnc.Type.CELLULAR:
+        return this.i18n('internetToggleMobileA11yLabel');
+      case CrOnc.Type.WI_FI:
+        return this.i18n('internetToggleWiFiA11yLabel');
+      case CrOnc.Type.WI_MAX:
+        return this.i18n('internetToggleWiMAXA11yLabel');
+    }
+    assertNotReached();
+    return '';
   },
 
   /**
