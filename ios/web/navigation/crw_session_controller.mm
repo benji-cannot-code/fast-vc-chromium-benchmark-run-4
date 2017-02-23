73d2aa612a18a52a52e07769f8af74363b2604c5
liaoyuke <liaoyuke@chromium.org>
2017-02-23 11:00:06 -0800

    (Set)IsOverridingUserAgent should be called on VisibleItem
    As explained in the associated bug, it always makes sense to call
    IsOverridingUserAgent() and SetIsOverridingUserAgent() on visible item
    instead of current item. Thus this CL changes CurrentItem to VisibleItem
    wherever necessary.
    
    Besides, this CL also does the following refactoring to have
    NavigationManagerImpl encapsulate VisibleItem for calling
    (Set)IsOverridingUserAgent() to provide one more layer of abstraction.
    
    BUG=693794
    
    Review-Url: https://codereview.chromium.org/2711683002
    Cr-Commit-Position: refs/heads/master@{#452570}

--- ios/web/navigation/crw_session_controller.mm
+++ ios/web/navigation/crw_session_controller.mm
@@ -75,10 +75,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // interval since 1970.
   NSTimeInterval _lastVisitedTimestamp;
 
-  // If |YES|, override |currentEntry.useDesktopUserAgent| and create the
-  // pending entry using the desktop user agent.
-  BOOL _useDesktopUserAgentForNextPendingItem;
-
   // The browser state associated with this CRWSessionController;
   web::BrowserState* _browserState;  // weak
 
@@ -497,11 +493,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                 transition:(ui::PageTransition)transition {
   DCHECK(![self pendingEntry]);
   DCHECK([self currentEntry]);
-  web::NavigationItem* currentItem = [self currentEntry].navigationItem;
-  CHECK(web::history_state_util::IsHistoryStateChangeValid(
-      currentItem->GetURL(), URL));
-  web::Referrer referrer(currentItem->GetURL(), web::ReferrerPolicyDefault);
 
+  web::NavigationItem* lastCommittedItem =
+      self.lastCommittedEntry.navigationItem;
+  CHECK(web::history_state_util::IsHistoryStateChangeValid(
+      lastCommittedItem->GetURL(), URL));
+
+  web::Referrer referrer(lastCommittedItem->GetURL(),
+                         web::ReferrerPolicyDefault);
   base::scoped_nsobject<CRWSessionEntry> pushedEntry([self
       sessionEntryWithURL:URL
                  referrer:referrer
@@ -509,11 +508,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
            initiationType:web::NavigationInitiationType::USER_INITIATED]);
 
   web::NavigationItemImpl* pushedItem = [pushedEntry navigationItemImpl];
-  pushedItem->SetIsOverridingUserAgent(currentItem->IsOverridingUserAgent());
+  pushedItem->SetIsOverridingUserAgent(
+      lastCommittedItem->IsOverridingUserAgent());
   pushedItem->SetSerializedStateObject(stateObject);
   pushedItem->SetIsCreatedFromPushState(true);
-  web::SSLStatus& sslStatus = [self currentEntry].navigationItem->GetSSL();
-  pushedEntry.get().navigationItem->GetSSL() = sslStatus;
+  pushedItem->GetSSL() = lastCommittedItem->GetSSL();
 
   [self clearForwardItems];
   // Add the new entry at the end.
