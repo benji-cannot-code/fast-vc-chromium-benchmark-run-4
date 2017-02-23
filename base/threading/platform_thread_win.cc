a26c41ce557fbaa0fd36fed920fdf7b6b8c06d8e
chengx <chengx@chromium.org>
2017-02-23 14:14:10 -0800

    Use Windows 10 thread naming API, SetThreadDescription
    Starting from version 1607 of Windows 10, there is a SetThreadDescription
    API for setting thread names. This API has several advantages over
    the SetNameInternal function which is currently used to set thread
    names in Chrome.
    
    The only issue with this new API is that there is no tool support
    for it yet. However, once tool support catches up, it should give
    a better thread naming experience.
    
    A command-line tool "ShowThreadNames" for testing this
    SetThreadDescription API has been developed and checked in using
    another CL. It can be found via
    https://codereview.chromium.org/2713773002
    
    BUG=684203
    
    Review-Url: https://codereview.chromium.org/2692213003
    Cr-Commit-Position: refs/heads/master@{#452653}

--- base/threading/platform_thread_win.cc
+++ base/threading/platform_thread_win.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/debug/alias.h"
 #include "base/debug/profiler.h"
 #include "base/logging.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/threading/thread_id_name_manager.h"
 #include "base/threading/thread_restrictions.h"
 #include "base/tracked_objects.h"
@@ -30,6 +31,10 @@ typedef struct tagTHREADNAME_INFO {
   DWORD dwThreadID;  // Thread ID (-1=caller thread).
   DWORD dwFlags;  // Reserved for future use, must be zero.
 } THREADNAME_INFO;
+
+// The SetThreadDescription API was brought in version 1607 of Windows 10.
+typedef HRESULT(WINAPI* SetThreadDescription)(HANDLE hThread,
+                                              PCWSTR lpThreadDescription);
 
 // This function has try handling, so it is separated out of its caller.
 void SetNameInternal(PlatformThreadId thread_id, const char* name) {
@@ -172,6 +177,15 @@ void PlatformThread::SetName(const std::string& name) {
   // impact is not terrible, but there is no reason to do initialize it.
   if (name != "BrokerEvent")
     tracked_objects::ThreadData::InitializeThreadContext(name);
+
+  // The SetThreadDescription API works even if no debugger is attached.
+  auto set_thread_description_func =
+      reinterpret_cast<SetThreadDescription>(::GetProcAddress(
+          ::GetModuleHandle(L"Kernel32.dll"), "SetThreadDescription"));
+  if (set_thread_description_func) {
+    set_thread_description_func(::GetCurrentThread(),
+                                base::UTF8ToWide(name).c_str());
+  }
 
   // The debugger needs to be around to catch the name in the exception.  If
   // there isn't a debugger, we are just needlessly throwing an exception.
