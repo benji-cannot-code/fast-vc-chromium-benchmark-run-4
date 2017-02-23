be324bcbc8f0467bd86d677ede6bcc40e02f9415
jlebel <jlebel@chromium.org>
2017-02-23 02:55:37 -0800

    Linux/Windows: Setting focus to the first profile in profile switcher
    When opening the profile switcher menu using the shortcut (ctrl-shift-m), the first
    profile (other than the current one) in the list should be highlighted. This should
    happens only when opening the menu with the keyboard (and not with the mouse).
    
    For macOS: crrev.com/2638853003 and crrev.com/2682153002
    
    BUG=674343
    
    Review-Url: https://codereview.chromium.org/2687693002
    Cr-Commit-Position: refs/heads/master@{#452449}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -2470,7 +2470,8 @@ void BrowserView::UpdateAcceleratorMetrics(const ui::Accelerator& accelerator,
 void BrowserView::ShowAvatarBubbleFromAvatarButton(
     AvatarBubbleMode mode,
     const signin::ManageAccountsParams& manage_accounts_params,
-    signin_metrics::AccessPoint access_point) {
+    signin_metrics::AccessPoint access_point,
+    bool focus_first_profile_button) {
 #if !defined(OS_CHROMEOS)
   // Do not show avatar bubble if there is no avatar menu button.
   if (!frame_->GetNewAvatarMenuButton())
@@ -2485,7 +2486,8 @@ void BrowserView::ShowAvatarBubbleFromAvatarButton(
   } else {
     ProfileChooserView::ShowBubble(bubble_view_mode, tutorial_mode,
                                    manage_accounts_params, access_point,
-                                   frame_->GetNewAvatarMenuButton(), browser());
+                                   frame_->GetNewAvatarMenuButton(), browser(),
+                                   focus_first_profile_button);
     ProfileMetrics::LogProfileOpenMethod(ProfileMetrics::ICON_AVATAR_BUBBLE);
   }
 #else
