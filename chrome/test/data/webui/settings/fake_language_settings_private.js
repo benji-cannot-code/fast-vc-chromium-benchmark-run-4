a45bb444a81b86adb206d02d6c52e4a872652ee4
scottchen <scottchen@chromium.org>
2017-02-23 13:19:37 -0800

    MD Settings: add empty list message for spell-check edit page.
    Also move the word list to a template dom-if, and fix auto-scroll when new words are added.
    
    BUG=684152
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2690263002
    Cr-Commit-Position: refs/heads/master@{#452633}

--- chrome/test/data/webui/settings/fake_language_settings_private.js
+++ chrome/test/data/webui/settings/fake_language_settings_private.js
@@ -23,9 +23,15 @@ cr.define('settings', function() {
   /**
    * Fake of the chrome.languageSettingsPrivate API.
    * @constructor
+   * @extends {TestBrowserProxy}
    * @implements {LanguageSettingsPrivate}
    */
   function FakeLanguageSettingsPrivate() {
+    // List of method names expected to be tested with whenCalled()
+    settings.TestBrowserProxy.call(this, [
+      'getSpellcheckWords',
+    ]);
+
     /** @type {!Array<!chrome.languageSettingsPrivate.Language>} */
     this.languages = [{
       // English and some variants.
@@ -100,6 +106,7 @@ cr.define('settings', function() {
   }
 
   FakeLanguageSettingsPrivate.prototype = {
+    __proto__: settings.TestBrowserProxy.prototype,
     // Methods for use in testing.
 
     /** @param {SettingsPrefsElement} */
@@ -173,13 +180,18 @@ cr.define('settings', function() {
      * Gets the custom spell check words, in sorted order.
      * @param {function(!Array<string>):void} callback
      */
-    getSpellcheckWords: wrapAssertNotReached('getSpellcheckWords'),
+    getSpellcheckWords: function(callback) {
+      callback([]);
+      this.methodCalled('getSpellcheckWords');
+    },
 
     /**
      * Adds a word to the custom dictionary.
      * @param {string} word
      */
-    addSpellcheckWord: wrapAssertNotReached('addSpellcheckWord'),
+    addSpellcheckWord: function(word) {
+      // Current tests don't actually care about this implementation.
+    },
 
     /**
      * Removes a word from the custom dictionary.
