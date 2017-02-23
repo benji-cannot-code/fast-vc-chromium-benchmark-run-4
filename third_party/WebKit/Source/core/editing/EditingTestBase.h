c3d3319de3de44dde5594a9a5299a748267a1168
xiaochengh <xiaochengh@chromium.org>
2017-02-22 23:43:42 -0800

    Ensure spell checking is enabled in SpellCheckerTest
    This patch introduces SpellCheckTestBase, which sets up a non-empty
    SpellCheckerClient that enables spell checking, so that
    SpellCheckerTest can utilize this base class to correctly perform
    unit tests that requires spell checking to be enabled.
    
    The new base class will also be useful when adding unit test for
    idle time spell checker.
    
    This patch also removes spell checking related stuff from
    TextFormControlElementTest, since it is leftover after we previously
    moved a spell checking unit test from TextFromControlElementTest to
    SpellCheckerTest.
    
    BUG=695286
    
    Review-Url: https://codereview.chromium.org/2711053002
    Cr-Commit-Position: refs/heads/master@{#452427}

--- third_party/WebKit/Source/core/editing/EditingTestBase.h
+++ third_party/WebKit/Source/core/editing/EditingTestBase.h
@@ -6,15 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef EditingTestBase_h
 #define EditingTestBase_h
 
-#include "core/editing/Position.h"
-#include "wtf/Forward.h"
 #include <gtest/gtest.h>
 #include <memory>
 #include <string>
+#include "core/editing/Position.h"
+#include "core/testing/DummyPageHolder.h"
+#include "wtf/Forward.h"
 
 namespace blink {
 
-class DummyPageHolder;
 class FrameSelection;
 class LocalFrame;
 
@@ -26,6 +26,8 @@ class EditingTestBase : public ::testing::Test {
   ~EditingTestBase() override;
 
   void SetUp() override;
+
+  void setupPageWithClients(Page::PageClients*);
 
   Document& document() const;
   DummyPageHolder& dummyPageHolder() const { return *m_dummyPageHolder; }
---
+++ third_party/WebKit/Source/core/editing/spellcheck/SpellCheckTestBase.cpp
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/editing/spellcheck/SpellCheckTestBase.h"
+
+namespace blink {
+
+void SpellCheckTestBase::SetUp() {
+  Page::PageClients pageClients;
+  fillWithEmptyClients(pageClients);
+  m_spellCheckerClient = WTF::wrapUnique(new DummySpellCheckerClient);
+  pageClients.spellCheckerClient = m_spellCheckerClient.get();
+  setupPageWithClients(&pageClients);
+}
+
+}  // namespace blink
