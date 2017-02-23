859890fb0f5ea6875bbceefb017d7959721a764c
sigbjornf <sigbjornf@opera.com>
2017-02-23 12:51:19 -0800

    Tidy ScriptRunner pending script handling.
    R=hiroshige
    BUG=686281
    
    Review-Url: https://codereview.chromium.org/2710233002
    Cr-Commit-Position: refs/heads/master@{#452619}

--- third_party/WebKit/Source/core/dom/ScriptRunner.cpp
+++ third_party/WebKit/Source/core/dom/ScriptRunner.cpp
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/dom/ScriptRunner.h"
 
+#include <algorithm>
 #include "core/dom/Document.h"
 #include "core/dom/Element.h"
 #include "core/dom/ScriptLoader.h"
@@ -110,7 +111,7 @@ void ScriptRunner::notifyScriptReady(ScriptLoader* scriptLoader,
   SECURITY_CHECK(scriptLoader);
   switch (executionType) {
     case Async:
-      // RELEASE_ASSERT makes us crash in a controlled way in error cases
+      // SECURITY_CHECK() makes us crash in a controlled way in error cases
       // where the ScriptLoader is associated with the wrong ScriptRunner
       // (otherwise we'd cause a use-after-free in ~ScriptRunner when it tries
       // to detach).
@@ -137,37 +138,34 @@ void ScriptRunner::notifyScriptReady(ScriptLoader* scriptLoader,
 }
 
 bool ScriptRunner::removePendingInOrderScript(ScriptLoader* scriptLoader) {
-  for (auto it = m_pendingInOrderScripts.begin();
-       it != m_pendingInOrderScripts.end(); ++it) {
-    if (*it == scriptLoader) {
-      m_pendingInOrderScripts.remove(it);
-      SECURITY_CHECK(m_numberOfInOrderScriptsWithPendingNotification > 0);
-      m_numberOfInOrderScriptsWithPendingNotification--;
-      return true;
-    }
-  }
-  return false;
+  auto it = std::find(m_pendingInOrderScripts.begin(),
+                      m_pendingInOrderScripts.end(), scriptLoader);
+  if (it == m_pendingInOrderScripts.end())
+    return false;
+  m_pendingInOrderScripts.remove(it);
+  SECURITY_CHECK(m_numberOfInOrderScriptsWithPendingNotification > 0);
+  m_numberOfInOrderScriptsWithPendingNotification--;
+  return true;
 }
 
 void ScriptRunner::notifyScriptLoadError(ScriptLoader* scriptLoader,
                                          AsyncExecutionType executionType) {
   switch (executionType) {
     case Async: {
-      // SECURITY_CHECK makes us crash in a controlled way in error cases
-      // where the ScriptLoader is associated with the wrong ScriptRunner
-      // (otherwise we'd cause a use-after-free in ~ScriptRunner when it tries
-      // to detach).
+      // See notifyScriptReady() comment.
       SECURITY_CHECK(m_pendingAsyncScripts.contains(scriptLoader));
       m_pendingAsyncScripts.erase(scriptLoader);
       break;
     }
-    case InOrder:
+    case InOrder: {
       SECURITY_CHECK(removePendingInOrderScript(scriptLoader));
       scheduleReadyInOrderScripts();
       break;
-    case None:
+    }
+    case None: {
       NOTREACHED();
       break;
+    }
   }
   m_document->decrementLoadEventDelayCount();
 }
@@ -202,9 +200,10 @@ void ScriptRunner::movePendingScript(Document& oldDocument,
 
 void ScriptRunner::movePendingScript(ScriptRunner* newRunner,
                                      ScriptLoader* scriptLoader) {
-  if (m_pendingAsyncScripts.contains(scriptLoader)) {
+  auto it = m_pendingAsyncScripts.find(scriptLoader);
+  if (it != m_pendingAsyncScripts.end()) {
     newRunner->queueScriptForExecution(scriptLoader, Async);
-    m_pendingAsyncScripts.erase(scriptLoader);
+    m_pendingAsyncScripts.erase(it);
     m_document->decrementLoadEventDelayCount();
     return;
   }
