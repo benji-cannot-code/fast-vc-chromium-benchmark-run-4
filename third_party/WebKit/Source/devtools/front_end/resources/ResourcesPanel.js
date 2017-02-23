dd57dbeea8023cf7706eb774cf5e79c229c79023
eostroukhov <eostroukhov@chromium.org>
2017-02-23 10:29:25 -0800

    [DevTools] Use a single CookieItemsView instance
    BUG=
    
    Review-Url: https://codereview.chromium.org/2707283003
    Cr-Commit-Position: refs/heads/master@{#452559}

--- third_party/WebKit/Source/devtools/front_end/resources/ResourcesPanel.js
+++ third_party/WebKit/Source/devtools/front_end/resources/ResourcesPanel.js
@@ -104,13 +104,13 @@ Resources.ResourcesPanel = class extends UI.PanelWithSidebar {
     this._databaseTreeElements = new Map();
     /** @type {!Map.<!Resources.DOMStorage, !Resources.DOMStorageTreeElement>} */
     this._domStorageTreeElements = new Map();
-    /** @type {!Object.<string, !Resources.CookieItemsView>} */
-    this._cookieViews = {};
     /** @type {!Object.<string, boolean>} */
     this._domains = {};
 
     /** @type {?Resources.DOMStorageItemsView} */
     this._domStorageView = null;
+    /** @type {?Resources.CookieItemsView} */
+    this._cookieView = null;
 
     this.panelSidebarElement().addEventListener('mousemove', this._onmousemove.bind(this), false);
     this.panelSidebarElement().addEventListener('mouseleave', this._onmouseleave.bind(this), false);
@@ -270,7 +270,6 @@ Resources.ResourcesPanel = class extends UI.PanelWithSidebar {
       this.visibleView.detach();
       delete this.visibleView;
     }
-    this._cookieViews = {};
     this.cookieListTreeElement.removeChildren();
   }
 
@@ -597,21 +596,23 @@ Resources.ResourcesPanel = class extends UI.PanelWithSidebar {
    * @param {!SDK.Target} cookieFrameTarget
    */
   showCookies(treeElement, cookieDomain, cookieFrameTarget) {
-    var view = this._cookieViews[cookieDomain];
-    if (!view) {
-      view = new Resources.CookieItemsView(treeElement, cookieFrameTarget, cookieDomain);
-      this._cookieViews[cookieDomain] = view;
-    }
-
-    this._innerShowView(view);
+    var model = SDK.CookieModel.fromTarget(cookieFrameTarget);
+    if (!this._cookieView)
+      this._cookieView = new Resources.CookieItemsView(treeElement, model, cookieDomain);
+    else
+      this._cookieView.setCookiesDomain(model, cookieDomain);
+    this._innerShowView(this._cookieView);
   }
 
   /**
+   * @param {!SDK.Target} target
    * @param {string} cookieDomain
    */
-  _clearCookies(cookieDomain) {
-    if (this._cookieViews[cookieDomain])
-      this._cookieViews[cookieDomain].deleteAllItems();
+  _clearCookies(target, cookieDomain) {
+    SDK.CookieModel.fromTarget(target).clear(cookieDomain, () => {
+      if (this._cookieView)
+        this._cookieView.refreshItems();
+    });
   }
 
   showApplicationCache(frameId) {
@@ -1946,13 +1947,15 @@ Resources.DOMStorageTreeElement = class extends Resources.BaseStorageTreeElement
   }
 };
 
-/**
- * @unrestricted
- */
 Resources.CookieTreeElement = class extends Resources.BaseStorageTreeElement {
+  /**
+   * @param {!Resources.ResourcesPanel} storagePanel
+   * @param {!SDK.ResourceTreeFrame} frame
+   * @param {string} cookieDomain
+   */
   constructor(storagePanel, frame, cookieDomain) {
     super(storagePanel, cookieDomain ? cookieDomain : Common.UIString('Local Files'), false);
-    this._frame = frame;
+    this._target = frame.target();
     this._cookieDomain = cookieDomain;
     var icon = UI.Icon.create('mediumicon-cookie', 'resource-tree-item');
     this.setLeadingIcons([icon]);
@@ -1975,7 +1978,8 @@ Resources.CookieTreeElement = class extends Resources.BaseStorageTreeElement {
    */
   _handleContextMenuEvent(event) {
     var contextMenu = new UI.ContextMenu(event);
-    contextMenu.appendItem(Common.UIString('Clear'), () => this._storagePanel._clearCookies(this._cookieDomain));
+    contextMenu.appendItem(
+        Common.UIString('Clear'), () => this._storagePanel._clearCookies(this._target, this._cookieDomain));
     contextMenu.show();
   }
 
@@ -1985,7 +1989,7 @@ Resources.CookieTreeElement = class extends Resources.BaseStorageTreeElement {
    */
   onselect(selectedByUser) {
     super.onselect(selectedByUser);
-    this._storagePanel.showCookies(this, this._cookieDomain, this._frame.target());
+    this._storagePanel.showCookies(this, this._cookieDomain, this._target);
     return false;
   }
 };
