d61b5a89d7650c0b91f82c69de043d60ca9556e8
chongz <chongz@chromium.org>
2017-02-22 16:17:46 -0800

    [Android] Add switch to disable native event batching
    This CL is the preparation for enabling compositor event queue.
    
    We should disable Android native event batching once the compositor event queue
    was enabled.
    
    BUG=625689
    
    Review-Url: https://codereview.chromium.org/2710683003
    Cr-Commit-Position: refs/heads/master@{#452298}

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -41,6 +41,7 @@ import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
 import android.view.inputmethod.InputMethodManager;
 
+import org.chromium.base.CommandLine;
 import org.chromium.base.ObserverList;
 import org.chromium.base.ObserverList.RewindableIterator;
 import org.chromium.base.TraceEvent;
@@ -60,6 +61,7 @@ import org.chromium.content.browser.input.SelectPopup;
 import org.chromium.content.browser.input.SelectPopupDialog;
 import org.chromium.content.browser.input.SelectPopupDropdown;
 import org.chromium.content.browser.input.SelectPopupItem;
+import org.chromium.content.common.ContentSwitches;
 import org.chromium.content_public.browser.AccessibilitySnapshotCallback;
 import org.chromium.content_public.browser.AccessibilitySnapshotNode;
 import org.chromium.content_public.browser.ActionModeCallbackHelper;
@@ -354,6 +356,9 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Displa
     private float mCurrentTouchOffsetX;
     private float mCurrentTouchOffsetY;
 
+    // True if we want to disable Android native event batching and use compositor event queue.
+    private boolean mShouldRequestUnbufferedDispatch;
+
     // Whether the ContentViewCore requires the WebContents to be fullscreen in order to lock the
     // screen orientation.
     private boolean mFullscreenRequiredForOrientationLock = true;
@@ -585,6 +590,9 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Displa
         mSelectionPopupController.setContainerView(getContainerView());
 
         mWebContentsObserver = new ContentViewWebContentsObserver(this);
+
+        mShouldRequestUnbufferedDispatch = Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP
+                && CommandLine.getInstance().hasSwitch(ContentSwitches.REQUEST_UNBUFFERED_DISPATCH);
     }
 
     /**
@@ -997,6 +1005,9 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Displa
             int eventAction = event.getActionMasked();
 
             if (eventAction == MotionEvent.ACTION_DOWN) {
+                if (mShouldRequestUnbufferedDispatch) {
+                    requestUnbufferedDispatch(event);
+                }
                 cancelRequestToScrollFocusedEditableNodeIntoView();
             }
 
@@ -2456,6 +2467,11 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Displa
             createVirtualStructure(viewNode.asyncNewChild(i), node.children.get(i), true);
         }
         viewNode.asyncCommit();
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void requestUnbufferedDispatch(MotionEvent touchDownEvent) {
+        mContainerView.requestUnbufferedDispatch(touchDownEvent);
     }
 
     @TargetApi(Build.VERSION_CODES.KITKAT)
