7fff1337c4edca94678edeb25c5950900bb8502f
fdoray <fdoray@chromium.org>
2017-02-24 14:21:43 -0800

    Use TaskScheduler instead of blocking pool in touch_log_source_x11.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=steel@chromium.org
    
    Review-Url: https://codereview.chromium.org/2677103002
    Cr-Commit-Position: refs/heads/master@{#452951}

--- chrome/browser/chromeos/system_logs/touch_log_source_x11.cc
+++ chrome/browser/chromeos/system_logs/touch_log_source_x11.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/json/json_string_value_serializer.h"
 #include "base/process/launch.h"
+#include "base/task_scheduler/post_task.h"
 #include "components/feedback/feedback_util.h"
 #include "content/public/browser/browser_thread.h"
 
@@ -62,9 +63,11 @@ void TouchLogSource::Fetch(const SysLogsSourceCallback& callback) {
   DCHECK(!callback.is_null());
 
   SystemLogsResponse* response = new SystemLogsResponse;
-  BrowserThread::PostBlockingPoolTaskAndReply(
-      FROM_HERE, base::Bind(&GetTouchLogsX11, response),
-      base::Bind(callback, base::Owned(response)));
+  base::PostTaskWithTraitsAndReply(FROM_HERE,
+                                   base::TaskTraits().MayBlock().WithPriority(
+                                       base::TaskPriority::BACKGROUND),
+                                   base::Bind(&GetTouchLogsX11, response),
+                                   base::Bind(callback, base::Owned(response)));
 }
 
 }  // namespace system_logs
