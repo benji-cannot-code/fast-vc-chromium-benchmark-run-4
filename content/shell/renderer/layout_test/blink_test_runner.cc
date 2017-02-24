b638cab4bba8e0c00f0fe5b6a7119a939055092b
peary2 <peary2@gmail.com>
2017-02-24 08:52:59 -0800

    Remove |remote| and |readonly| members of MediaStreamTrack.
    Spec reference:
    http://w3c.github.io/mediacapture-main/getusermedia.html#attributes-1
    
    removal was in the February 22, 2016'.
    [#321] Remove track attributes "remote" and "readonly" :
    https://github.com/w3c/mediacapture-main/pull/321
    
    Intent to Deprecate and Remove :
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/d20ECb2sWzI
    
    BUG=598704
    
    Review-Url: https://codereview.chromium.org/2425703002
    Cr-Commit-Position: refs/heads/master@{#452851}

--- content/shell/renderer/layout_test/blink_test_runner.cc
+++ content/shell/renderer/layout_test/blink_test_runner.cc
@@ -758,8 +758,6 @@ bool BlinkTestRunner::AddMediaStreamVideoSourceAndTrack(
   DCHECK(stream);
 #if BUILDFLAG(ENABLE_WEBRTC)
   return AddVideoTrackToMediaStream(base::MakeUnique<MockVideoCapturerSource>(),
-                                    false,  // is_remote
-                                    false,  // is_readonly
                                     stream);
 #else
   return false;
@@ -775,8 +773,6 @@ bool BlinkTestRunner::AddMediaStreamAudioSourceAndTrack(
       48000,  // sample rate
       media::CHANNEL_LAYOUT_STEREO,
       480,  // sample frames per buffer
-      false,  // is_remote
-      false,  // is_readonly
       stream);
 #else
   return false;
