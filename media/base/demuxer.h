0d480daa85a80610f2b4af75cfd4bfeef5aa934d
servolk <servolk@chromium.org>
2017-02-23 17:20:11 -0800

    Use base::Optional for selected video track.
    Previously we used a one-element std::vector for passing around the
    selected video track, but we can use base::Optional instead, since
    there is never more than one selected video track.
    
    Review-Url: https://codereview.chromium.org/2718483003
    Cr-Commit-Position: refs/heads/master@{#452705}

--- media/base/demuxer.h
+++ media/base/demuxer.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/macros.h"
+#include "base/optional.h"
 #include "base/time/time.h"
 #include "media/base/data_source.h"
 #include "media/base/demuxer_stream.h"
@@ -144,12 +145,13 @@ class MEDIA_EXPORT Demuxer : public MediaResource {
 
   virtual void OnEnabledAudioTracksChanged(
       const std::vector<MediaTrack::Id>& track_ids,
-      base::TimeDelta currTime) = 0;
+      base::TimeDelta curr_time) = 0;
 
-  // |track_ids| is either empty or contains a single video track id.
+  // |track_id| either contains the selected video track id or is null,
+  // indicating that all video tracks are deselected/disabled.
   virtual void OnSelectedVideoTrackChanged(
-      const std::vector<MediaTrack::Id>& track_ids,
-      base::TimeDelta currTime) = 0;
+      base::Optional<MediaTrack::Id> track_id,
+      base::TimeDelta curr_time) = 0;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(Demuxer);
