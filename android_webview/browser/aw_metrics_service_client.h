3021450092ae7959e581b68065fa312d96c6bd14
paulmiller <paulmiller@chromium.org>
2017-02-24 11:29:01 -0800

    WebView: fix header file guards
    BUG=546754
    
    Review-Url: https://codereview.chromium.org/2718593003
    Cr-Commit-Position: refs/heads/master@{#452901}

--- android_webview/browser/aw_metrics_service_client.h
+++ android_webview/browser/aw_metrics_service_client.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ANDROID_WEBVIEW_BROWSER_AW_METRICS_SERVICE_CLIENT_IMPL_H_
-#define ANDROID_WEBVIEW_BROWSER_AW_METRICS_SERVICE_CLIENT_IMPL_H_
+#ifndef ANDROID_WEBVIEW_BROWSER_AW_METRICS_SERVICE_CLIENT_H_
+#define ANDROID_WEBVIEW_BROWSER_AW_METRICS_SERVICE_CLIENT_H_
 
 #include "components/metrics/enabled_state_provider.h"
 #include "components/metrics/metrics_service_client.h"
@@ -39,4 +39,4 @@ class AwMetricsServiceClient : public metrics::MetricsServiceClient,
 
 }  // namespace android_webview
 
-#endif  // ANDROID_WEBVIEW_BROWSER_AW_METRICS_SERVICE_CLIENT_IMPL_H_
+#endif  // ANDROID_WEBVIEW_BROWSER_AW_METRICS_SERVICE_CLIENT_H_
