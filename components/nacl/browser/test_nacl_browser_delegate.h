86b6291cda1982afba62d0979f8b1d041d79e9af
wez <wez@chromium.org>
2017-02-23 16:53:35 -0800

    Add thread checks to NaClBrowser, and make it leaky.
    This makes the following fixes to NaClBrowser:
    
    1. Removes use of WeakPtr to allow in-progress file I/O to be abandoned
       when NaClBrowser is torn-down; this never worked as intended, since
       the WeakPtrs were used on the I/O thread while NaClBrowser was torn-
       down via the Singleton::OnExit handler, on the UI thread.
    
    2. Adds thread checks to NaClBrowser to ensure it is only called from
       the browser's I/O thread.
    
    3. Changes the signature of SetDelegate() to express that the caller no
       longer owns the supplied NaClBrowserDelegate.
    
    4. Leaks the NaClBrowser and NaClBrowserDelegate, rather than cleaning
       them up on exit, since at present they have no cleanup to do.
    
    Review-Url: https://codereview.chromium.org/2630443003
    Cr-Commit-Position: refs/heads/master@{#452697}

--- components/nacl/browser/test_nacl_browser_delegate.h
+++ components/nacl/browser/test_nacl_browser_delegate.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // and
 //
-//   NaClBrowser::SetDelegate(NULL);   // frees the test delegate.
+//   NaClBrowser::ClearAndDeleteDelegateForTest();
 class TestNaClBrowserDelegate : public NaClBrowserDelegate {
  public:
   TestNaClBrowserDelegate();
--- extensions/shell/browser/shell_browser_main_parts.cc
+++ extensions/shell/browser/shell_browser_main_parts.cc
@@ -204,9 +204,8 @@ void ShellBrowserMainParts::PreMainMessageLoopRun() {
       cmd->GetSwitchValueASCII(switches::kAppShellRefreshToken)));
 
 #if !defined(DISABLE_NACL)
-  // Takes ownership.
   nacl::NaClBrowser::SetDelegate(
-      new ShellNaClBrowserDelegate(browser_context_.get()));
+      base::MakeUnique<ShellNaClBrowserDelegate>(browser_context_.get()));
   // Track the task so it can be canceled if app_shell shuts down very quickly,
   // such as in browser tests.
   task_tracker_.PostTask(
@@ -243,7 +242,6 @@ void ShellBrowserMainParts::PostMainMessageLoopRun() {
 
 #if !defined(DISABLE_NACL)
   task_tracker_.TryCancelAll();
-  nacl::NaClBrowser::SetDelegate(nullptr);
 #endif
 
   oauth2_token_service_.reset();
