7a775fb7ffb98c9d819b7c78c9ef28a4266853ed
sfiera <sfiera@chromium.org>
2017-02-24 05:53:35 -0800

    NTP: add param for linking notification to NTP
    Still missing:
    * Appropriate strings for this, to imply that we're linking to a group
      of articles instead of a specific one.
    * Starting out scrolled to Articles for You--but maybe that's not
      necessary with the condensed NTP.
    * When opening the notification, the omnibox is briefly visible. Maybe
      the experiment with opening directly to the NTP has this problem too
      or even a fix for it?
    
    BUG=694957
    
    Review-Url: https://codereview.chromium.org/2707313003
    Cr-Commit-Position: refs/heads/master@{#452810}

--- chrome/browser/ntp_snippets/ntp_snippets_features.cc
+++ chrome/browser/ntp_snippets/ntp_snippets_features.cc
@@ -15,6 +15,7 @@ const char kNotificationsAlwaysNotifyParam[] = "always_notify";
 const char kNotificationsUseSnippetAsTextParam[] = "use_snippet_as_text";
 const char kNotificationsKeepWhenFrontmostParam[] =
     "keep_notification_when_frontmost";
+const char kNotificationsOpenToNTPParam[] = "open_to_ntp";
 const char kNotificationsDailyLimit[] = "daily_limit";
 const char kNotificationsIgnoredLimitParam[] = "ignored_limit";
 
