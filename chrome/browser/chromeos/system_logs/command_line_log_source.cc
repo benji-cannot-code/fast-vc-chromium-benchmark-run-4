8f2519407030402722bf27320e108b216ef1dcff
fdoray <fdoray@chromium.org>
2017-02-24 14:10:46 -0800

    Use TaskScheduler instead of blocking pool in command_line_log_source.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=steel@chromium.org
    
    Review-Url: https://codereview.chromium.org/2677923002
    Cr-Commit-Position: refs/heads/master@{#452946}

--- chrome/browser/chromeos/system_logs/command_line_log_source.cc
+++ chrome/browser/chromeos/system_logs/command_line_log_source.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/process/launch.h"
 #include "base/sys_info.h"
+#include "base/task_scheduler/post_task.h"
 #include "content/public/browser/browser_thread.h"
 
 using content::BrowserThread;
@@ -107,10 +108,11 @@ void CommandLineLogSource::Fetch(const SysLogsSourceCallback& callback) {
   DCHECK(!callback.is_null());
 
   SystemLogsResponse* response = new SystemLogsResponse;
-  BrowserThread::PostBlockingPoolTaskAndReply(
-      FROM_HERE,
-      base::Bind(&ExecuteCommandLines, response),
-      base::Bind(callback, base::Owned(response)));
+  base::PostTaskWithTraitsAndReply(FROM_HERE,
+                                   base::TaskTraits().MayBlock().WithPriority(
+                                       base::TaskPriority::BACKGROUND),
+                                   base::Bind(&ExecuteCommandLines, response),
+                                   base::Bind(callback, base::Owned(response)));
 }
 
 }  // namespace system_logs
