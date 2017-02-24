3091643a887b48f02ae52f4f5c43bf4388c2fca9
xhwang <xhwang@chromium.org>
2017-02-24 14:57:33 -0800

    media: Prefer decrypting pipeline when CDM is attached
    In DecoderSelector, if a CDM is attached, always try the decrypting
    pipeline first (decoders that support encrypted streams, or decrypting
    demuxer stream plus regular decoders), so that the pipeline can handle
    encrytped streams later.
    
    BUG=597443
    TEST=New tests enabled.
    
    Review-Url: https://codereview.chromium.org/2701203003
    Cr-Commit-Position: refs/heads/master@{#452975}

--- media/base/audio_decoder_config.cc
+++ media/base/audio_decoder_config.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "media/base/limits.h"
+#include "media/base/media_util.h"
 
 namespace media {
 
@@ -94,6 +95,21 @@ std::string AudioDecoderConfig::AsHumanReadableString() const {
     << (extra_data().empty() ? "false" : "true") << " encrypted? "
     << (is_encrypted() ? "true" : "false");
   return s.str();
+}
+
+void AudioDecoderConfig::SetIsEncrypted(bool is_encrypted) {
+  if (!is_encrypted) {
+    DCHECK(encryption_scheme_.is_encrypted()) << "Config is already clear.";
+    encryption_scheme_ = Unencrypted();
+  } else {
+    DCHECK(!encryption_scheme_.is_encrypted())
+        << "Config is already encrypted.";
+    // TODO(xhwang): This is only used to guide decoder selection, so set
+    // a common encryption scheme that should be supported by all decrypting
+    // decoders. We should be able to remove this when we support switching
+    // decoders at run time. See http://crbug.com/695595
+    encryption_scheme_ = AesCtrEncryptionScheme();
+  }
 }
 
 }  // namespace media
--- media/filters/video_frame_stream_unittest.cc
+++ media/filters/video_frame_stream_unittest.cc
@@ -58,7 +58,6 @@ class VideoFrameStreamTest
       : demuxer_stream_(new FakeDemuxerStream(kNumConfigs,
                                               kNumBuffersInOneConfig,
                                               GetParam().is_encrypted)),
-        cdm_context_(new StrictMock<MockCdmContext>()),
         is_initialized_(false),
         num_decoded_frames_(0),
         pending_initialize_(false),
@@ -80,7 +79,7 @@ class VideoFrameStreamTest
                                      bytes_decoded_cb);
 
     // TODO(xhwang): We should test the case where only certain decoder
-    // supports encrypted streams. Currently this is hard to test becasue we use
+    // supports encrypted streams. Currently this is hard to test because we use
     // parameterized tests which need to pass in all combinations.
     if (GetParam().is_encrypted && !GetParam().has_decryptor) {
       decoder1_->EnableEncryptedConfigSupport();
@@ -96,7 +95,7 @@ class VideoFrameStreamTest
     video_frame_stream_.reset(new VideoFrameStream(
         message_loop_.task_runner(), std::move(decoders), new MediaLog()));
 
-    if (GetParam().has_decryptor) {
+    if (GetParam().is_encrypted && GetParam().has_decryptor) {
       decryptor_.reset(new NiceMock<MockDecryptor>());
 
       // Decryptor can only decrypt (not decrypt-and-decode) so that
@@ -107,8 +106,12 @@ class VideoFrameStreamTest
           .WillRepeatedly(Invoke(this, &VideoFrameStreamTest::Decrypt));
     }
 
-    EXPECT_CALL(*cdm_context_, GetDecryptor())
-        .WillRepeatedly(Return(decryptor_.get()));
+    if (GetParam().is_encrypted) {
+      cdm_context_.reset(new StrictMock<MockCdmContext>());
+
+      EXPECT_CALL(*cdm_context_, GetDecryptor())
+          .WillRepeatedly(Return(decryptor_.get()));
+    }
   }
 
   ~VideoFrameStreamTest() {
