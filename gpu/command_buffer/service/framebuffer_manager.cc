4b87806a18c534f5975a90e91e7f3f2b1ffaece1
ccameron <ccameron@chromium.org>
2017-02-24 11:53:23 -0800

    Add GL_EXT_color_buffer_half_float support
    Only expose for GLES2 contexts.
    
    For desktop support, add simliar checks to GL_EXT_color_buffer_float.
    
    Change a number of instances of accidentally referring to the extension
    as "color_buffer_float" instead of "color_buffer_half_float".
    
    BUG=687439
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2713553005
    Cr-Commit-Position: refs/heads/master@{#452909}

--- gpu/command_buffer/service/framebuffer_manager.cc
+++ gpu/command_buffer/service/framebuffer_manager.cc
@@ -270,6 +270,10 @@ class TextureAttachment
         internal_format == GL_LUMINANCE_ALPHA) {
       return false;
     }
+    // Disallow RGB16F, because it is only supported in ES2 and not ES3.
+    if (internal_format == GL_RGB16F) {
+      return false;
+    }
     return (need & have) != 0;
   }
 
