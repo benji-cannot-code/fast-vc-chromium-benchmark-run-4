f47fa40709d996fd0518ac10b718108be1a37a7a
smcgruer <smcgruer@chromium.org>
2017-02-24 14:07:18 -0800

    Handle nested position:sticky elements correctly (main thread)
    In order to correctly calculate the sticky offset for nested sticky, we
    must track the accumulated sticky offset from our ancestor sticky
    elements. This accumulation must then be used to adjust the location of
    the two constraint rects.
    
    This patch fixed nested position: sticky only for the main thread side.
    A compositor fix will follow in a later patch. To ensure that nested
    sticky behaves properly in the meantime, we also disallow compositing of
    sticky elements if nesting is detected.
    
    BUG=672710
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2708883005
    Cr-Commit-Position: refs/heads/master@{#452944}

--- third_party/WebKit/Source/core/page/scrolling/StickyPositionScrollingConstraints.cpp
+++ third_party/WebKit/Source/core/page/scrolling/StickyPositionScrollingConstraints.cpp
@@ -8,16 +8,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 FloatSize StickyPositionScrollingConstraints::computeStickyOffset(
-    const FloatRect& viewportRect) const {
-  FloatRect boxRect = m_scrollContainerRelativeStickyBoxRect;
+    const FloatRect& viewportRect,
+    const StickyPositionScrollingConstraints* ancestorStickyBoxConstraints,
+    const StickyPositionScrollingConstraints*
+        ancestorContainingBlockConstraints) {
+  // Adjust the constraint rect locations based on our ancestor sticky elements
+  // These adjustments are necessary to avoid double offsetting in the case of
+  // nested sticky elements.
+  FloatSize ancestorStickyBoxOffset =
+      ancestorStickyBoxConstraints
+          ? ancestorStickyBoxConstraints->getTotalStickyBoxStickyOffset()
+          : FloatSize();
+  FloatSize ancestorContainingBlockOffset =
+      ancestorContainingBlockConstraints
+          ? ancestorContainingBlockConstraints
+                ->getTotalContainingBlockStickyOffset()
+          : FloatSize();
+  FloatRect stickyBoxRect = m_scrollContainerRelativeStickyBoxRect;
+  FloatRect containingBlockRect = m_scrollContainerRelativeContainingBlockRect;
+  stickyBoxRect.move(ancestorStickyBoxOffset + ancestorContainingBlockOffset);
+  containingBlockRect.move(ancestorContainingBlockOffset);
+
+  FloatRect boxRect = stickyBoxRect;
 
   if (hasAnchorEdge(AnchorEdgeRight)) {
     float rightLimit = viewportRect.maxX() - m_rightOffset;
-    float rightDelta = std::min<float>(
-        0, rightLimit - m_scrollContainerRelativeStickyBoxRect.maxX());
+    float rightDelta = std::min<float>(0, rightLimit - stickyBoxRect.maxX());
     float availableSpace =
-        std::min<float>(0, m_scrollContainerRelativeContainingBlockRect.x() -
-                               m_scrollContainerRelativeStickyBoxRect.x());
+        std::min<float>(0, containingBlockRect.x() - stickyBoxRect.x());
     if (rightDelta < availableSpace)
       rightDelta = availableSpace;
 
@@ -26,11 +44,9 @@ FloatSize StickyPositionScrollingConstraints::computeStickyOffset(
 
   if (hasAnchorEdge(AnchorEdgeLeft)) {
     float leftLimit = viewportRect.x() + m_leftOffset;
-    float leftDelta = std::max<float>(
-        0, leftLimit - m_scrollContainerRelativeStickyBoxRect.x());
+    float leftDelta = std::max<float>(0, leftLimit - stickyBoxRect.x());
     float availableSpace =
-        std::max<float>(0, m_scrollContainerRelativeContainingBlockRect.maxX() -
-                               m_scrollContainerRelativeStickyBoxRect.maxX());
+        std::max<float>(0, containingBlockRect.maxX() - stickyBoxRect.maxX());
     if (leftDelta > availableSpace)
       leftDelta = availableSpace;
 
@@ -39,11 +55,9 @@ FloatSize StickyPositionScrollingConstraints::computeStickyOffset(
 
   if (hasAnchorEdge(AnchorEdgeBottom)) {
     float bottomLimit = viewportRect.maxY() - m_bottomOffset;
-    float bottomDelta = std::min<float>(
-        0, bottomLimit - m_scrollContainerRelativeStickyBoxRect.maxY());
+    float bottomDelta = std::min<float>(0, bottomLimit - stickyBoxRect.maxY());
     float availableSpace =
-        std::min<float>(0, m_scrollContainerRelativeContainingBlockRect.y() -
-                               m_scrollContainerRelativeStickyBoxRect.y());
+        std::min<float>(0, containingBlockRect.y() - stickyBoxRect.y());
     if (bottomDelta < availableSpace)
       bottomDelta = availableSpace;
 
@@ -52,18 +66,22 @@ FloatSize StickyPositionScrollingConstraints::computeStickyOffset(
 
   if (hasAnchorEdge(AnchorEdgeTop)) {
     float topLimit = viewportRect.y() + m_topOffset;
-    float topDelta = std::max<float>(
-        0, topLimit - m_scrollContainerRelativeStickyBoxRect.y());
+    float topDelta = std::max<float>(0, topLimit - stickyBoxRect.y());
     float availableSpace =
-        std::max<float>(0, m_scrollContainerRelativeContainingBlockRect.maxY() -
-                               m_scrollContainerRelativeStickyBoxRect.maxY());
+        std::max<float>(0, containingBlockRect.maxY() - stickyBoxRect.maxY());
     if (topDelta > availableSpace)
       topDelta = availableSpace;
 
     boxRect.move(0, topDelta);
   }
 
-  return boxRect.location() - m_scrollContainerRelativeStickyBoxRect.location();
+  FloatSize stickyOffset = boxRect.location() - stickyBoxRect.location();
+
+  m_totalStickyBoxStickyOffset = ancestorStickyBoxOffset + stickyOffset;
+  m_totalContainingBlockStickyOffset =
+      ancestorStickyBoxOffset + ancestorContainingBlockOffset + stickyOffset;
+
+  return stickyOffset;
 }
 
 }  // namespace blink
