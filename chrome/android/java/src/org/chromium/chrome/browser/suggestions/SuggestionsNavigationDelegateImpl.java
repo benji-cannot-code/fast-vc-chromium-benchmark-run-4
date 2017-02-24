8a4db33fc0ff7d2948718999d9f6226bb20847c5
dgn <dgn@chromium.org>
2017-02-24 05:30:16 -0800

    📰 Use the correct windowDisposition for "Open in new tab"
    Replaces the usage of WindowOpenDisposition.NEW_FOREGROUND_TAB in
    the suggestions related code with
    WindowOpenDisposition.NEW_BACKGROUND_TAB. We use it in the context of
    opening a tab in the background, but since we never directly pass the
    value and used the wrong one consistently, the actual value being used
    wasn't a problem.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2712603005
    Cr-Commit-Position: refs/heads/master@{#452807}

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsNavigationDelegateImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsNavigationDelegateImpl.java
@@ -97,7 +97,7 @@ public class SuggestionsNavigationDelegateImpl implements SuggestionsNavigationD
         if (article.isAssetDownload()) {
             assert windowOpenDisposition == WindowOpenDisposition.CURRENT_TAB
                     || windowOpenDisposition == WindowOpenDisposition.NEW_WINDOW
-                    || windowOpenDisposition == WindowOpenDisposition.NEW_FOREGROUND_TAB;
+                    || windowOpenDisposition == WindowOpenDisposition.NEW_BACKGROUND_TAB;
             DownloadUtils.openFile(
                     article.getAssetDownloadFile(), article.getAssetDownloadMimeType(), false);
             return;
@@ -124,7 +124,7 @@ public class SuggestionsNavigationDelegateImpl implements SuggestionsNavigationD
             assert article.getOfflinePageOfflineId() != null;
             assert windowOpenDisposition == WindowOpenDisposition.CURRENT_TAB
                     || windowOpenDisposition == WindowOpenDisposition.NEW_WINDOW
-                    || windowOpenDisposition == WindowOpenDisposition.NEW_FOREGROUND_TAB;
+                    || windowOpenDisposition == WindowOpenDisposition.NEW_BACKGROUND_TAB;
             loadUrlParams = OfflinePageUtils.getLoadUrlParamsForOpeningOfflineVersion(
                     article.mUrl, article.getOfflinePageOfflineId());
             // Extra headers are not read in loadUrl, but verbatim headers are.
@@ -150,7 +150,7 @@ public class SuggestionsNavigationDelegateImpl implements SuggestionsNavigationD
             case WindowOpenDisposition.CURRENT_TAB:
                 mHost.loadUrl(loadUrlParams);
                 break;
-            case WindowOpenDisposition.NEW_FOREGROUND_TAB:
+            case WindowOpenDisposition.NEW_BACKGROUND_TAB:
                 openUrlInNewTab(loadUrlParams, false);
                 break;
             case WindowOpenDisposition.OFF_THE_RECORD:
