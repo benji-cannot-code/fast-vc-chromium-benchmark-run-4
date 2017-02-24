e9e70330debbf4f6195eb83b13eb2479142e396a
sigbjornf <sigbjornf@opera.com>
2017-02-24 07:45:07 -0800

    Verify that a new heap page isn't also marked as being off heap.
    Attempt to diagnose a rare assert failure, where a conservative
    GC stack scan finds a potential pointer in both the heap's
    negative heap page cache and in the map of in-use heap pages.
    
    Those two mapping should be mutually exclusive by construction,
    and must be -- the negative page cache must not contain false
    positives. Hence, add verification when a new page is committed
    & used, it does not already have a mapping in that negative cache.
    
    R=haraken
    BUG=649485
    
    Review-Url: https://codereview.chromium.org/2715713005
    Cr-Commit-Position: refs/heads/master@{#452833}

--- third_party/WebKit/Source/platform/heap/ThreadState.h
+++ third_party/WebKit/Source/platform/heap/ThreadState.h
@@ -380,6 +380,8 @@ class PLATFORM_EXPORT ThreadState {
     m_shouldFlushHeapDoesNotContainCache = true;
   }
 
+  bool isAddressInHeapDoesNotContainCache(Address);
+
   void registerTraceDOMWrappers(
       v8::Isolate* isolate,
       void (*traceDOMWrappers)(v8::Isolate*, Visitor*),
