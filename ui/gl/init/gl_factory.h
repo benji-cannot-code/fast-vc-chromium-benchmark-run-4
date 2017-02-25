c4d40fef5d036a47506414cb48fdc6878c7e54e1
stanisc <stanisc@chromium.org>
2017-02-24 18:39:11 -0800

    Support GPU VSync when DirectComposition isn't enabled
    This fixes a few additional issues that came up in private testing.
    
    1) Blank screen with DirectComposition isn't enabled. What happens is
       that GPU VSync ends up being activated on Compositor side but not
       on GPU side. I'd prototyped this solution when I tested GPU VSync
       on Win 7 but decided to leave it out because I mistakenly assumed
       that DirectComposition is always available on Win 8+. I verified
       this but running Chrome with --disable-direct-composition flag.
    
    2) In gpu_vsync_provider_win.cc "GpuVSyncWorker::SendVSyncUpdate"
       trace event always logs adjustment = 0 due to incorrect variable
       scope.
    
    3) It was pointed out to me that if D3DKMTWaitForVerticalBlankEvent
       keep returning an error for whatever reason the worker thread
       would just spin. It might be a better idea to just crash in that
       unlikely case so that we can investigate the crash dump.
    
    What would be a condition for GL Implementation being anything other
    than kGLImplementationEGLGLES2? Should we care about that case?
    
    BUG=467617
    
    Review-Url: https://codereview.chromium.org/2710183004
    Cr-Commit-Position: refs/heads/master@{#453050}

--- ui/gl/init/gl_factory.h
+++ ui/gl/init/gl_factory.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_GL_INIT_GL_FACTORY_H_
 #define UI_GL_INIT_GL_FACTORY_H_
 
+#include <memory>
 #include <vector>
 
 #include "base/memory/ref_counted.h"
@@ -15,6 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_surface_format.h"
 #include "ui/gl/gpu_preference.h"
 #include "ui/gl/init/gl_init_export.h"
+
+namespace gfx {
+class VSyncProvider;
+}  // namespace gfx
 
 namespace gl {
 
@@ -59,6 +64,13 @@ GL_INIT_EXPORT scoped_refptr<GLContext> CreateGLContext(
 // Creates a GL surface that renders directly to a view.
 GL_INIT_EXPORT scoped_refptr<GLSurface> CreateViewGLSurface(
     gfx::AcceleratedWidget window);
+
+#if defined(OS_WIN)
+// Creates a GL surface that renders directly into a native window.
+GL_INIT_EXPORT scoped_refptr<GLSurface> CreateNativeViewGLSurfaceEGL(
+    gfx::AcceleratedWidget window,
+    std::unique_ptr<gfx::VSyncProvider> sync_provider);
+#endif
 
 #if defined(USE_OZONE)
 // Creates a GL surface that renders directly into a window with surfaceless
