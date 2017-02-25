b6eadbac08ddebbed82ad50bc72319e2f1ec9f4f
chasej <chasej@chromium.org>
2017-02-24 20:11:35 -0800

    Use asserts in origin trials layout tests
    The layout tests for exposure of interfaces were using console messages
    with comparisons against -expected.txt files. Although they were using
    testharness.js, the tests were not using asserts.
    
    This CL changes the tests to use asserts, and remove the -expected.txt
    files wherever possible.
    
    Discussion on console messages in testharness tests can be found here:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/3Ob2lq14KEs
    
    BUG=687492
    
    Review-Url: https://codereview.chromium.org/2712843005
    Cr-Commit-Position: refs/heads/master@{#453063}

---
+++ third_party/WebKit/LayoutTests/http/tests/origin_trials/webexposed/resources/foreignfetch-origin-trial-interfaces-worker-disabled.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+importScripts('../../../resources/testharness.js');
+importScripts('../../../resources/origin-trials-helper.js');
+
+test(t => {
+  OriginTrialsHelper.check_properties_missing(this,
+    {'InstallEvent':['registerForeignFetch'],
+     'global':['onforeignfetch']});
+}, 'ForeignFetch related properties on interfaces in service worker, without trial.');
+
+test(t => {
+  OriginTrialsHelper.check_interfaces_missing(this,
+    ['ForeignFetchEvent']);
+}, 'ForeignFetch related interfaces in service worker, without trial.');
