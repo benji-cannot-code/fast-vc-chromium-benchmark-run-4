add3d9666692c541d42445b805cddcac91ec0159
lfg <lfg@chromium.org>
2017-02-24 16:58:05 -0800

    Delete RenderWidgetHostViewChildFrame on Destroy().
    This CL changes RenderWidgetHostViewChildFrame to be synchronously deleted when Destroy() is called. This makes sure that the destructor is called on tests, and prevents complicated lifetime management for objects that depend on RenderWidgetHostViewChildFrame.
    
    BUG=624065
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2710023008
    Cr-Commit-Position: refs/heads/master@{#453032}

--- content/browser/frame_host/render_widget_host_view_child_frame.cc
+++ content/browser/frame_host/render_widget_host_view_child_frame.cc
@@ -296,7 +296,8 @@ void RenderWidgetHostViewChildFrame::Destroy() {
 
   host_->SetView(nullptr);
   host_ = nullptr;
-  base::ThreadTaskRunnerHandle::Get()->DeleteSoon(FROM_HERE, this);
+
+  delete this;
 }
 
 void RenderWidgetHostViewChildFrame::SetTooltipText(
