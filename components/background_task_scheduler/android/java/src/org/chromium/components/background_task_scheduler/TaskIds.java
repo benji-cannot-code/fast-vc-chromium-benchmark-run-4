483b30351186cd5174d11c3a3309caffd2801fd5
nyquist <nyquist@chromium.org>
2017-02-24 18:34:56 -0800

    [android] Add JobScheduler-based BackgroundTaskScheduler.
    In Android M+ it is encouraged to use JobScheduler for all background jobs,
    instead of using things like IntentService or polling using alarms. Using the
    system API is beneficial as it has a full view of what goes on in the system and
    can schedule jobs accordingly.
    
    However, this functionality was introduced in Android L, and
    the API has been very stable since Android M. This means that we also need a
    similar framework for older versions of Android, which is provided by
    Google Play services. We prefer system APIs, since they do not require including
    external libraries which bloats the APK size of Chrome and adds unnecessary
    complexity. The GcmNetworkManager isonly used when the system API is not
    available.
    
    The background_task_scheduler component provides a new framework for use
    within chromium to schedule and execute background jobs using the frameworks
    available on a given version of Android.
    The public API of the framework is similar to that of the Android
    JobScheduler, but it is backed by either the system JobScheduler API or by
    GcmNetworkManager. What service is used to back the framework remains unknown to
    callers of the API.
    
    This first CL only adds support for the JobScheduler based backend.
    
    BUG=680757
    
    Review-Url: https://codereview.chromium.org/2714463002
    Cr-Commit-Position: refs/heads/master@{#453049}

---
+++ components/background_task_scheduler/android/java/src/org/chromium/components/background_task_scheduler/TaskIds.java
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.background_task_scheduler;
+
+/**
+ * This class lists all the unique task IDs used around in Chromium. These are listed here to ensure
+ * that there is no overlap of task IDs between different users of the BackgroundTaskScheduler.
+ */
+public final class TaskIds {
+    public static final int TEST = 0x00008378;
+
+    private TaskIds() {}
+}
