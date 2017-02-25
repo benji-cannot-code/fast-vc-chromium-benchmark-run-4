1b9afb2bd2948ade99cb70546cbde52c4260d3db
dfalcantara <dfalcantara@chromium.org>
2017-02-24 17:23:59 -0800

    Revert of Allow LocationBarLayout subclasses to permit empty search query setting (patchset #1 id:1 of https://chromiumcodereview.appspot.com/2710413002/ )
    Reason for revert:
    Not needed anymore.
    
    Original issue's description:
    > Allow LocationBarLayout subclasses to permit empty search query setting
    >
    > This allows focusing the Omnibox and running other logic required
    > from subclasses.
    >
    > BUG=None
    >
    > Review-Url: https://codereview.chromium.org/2710413002
    > Cr-Commit-Position: refs/heads/master@{#452887}
    > Committed: https://chromium.googlesource.com/chromium/src/+/835b6a9f085f73e4551b474744d0640618be9973
    
    TBR=tedchoc@chromium.org,yusufo@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2712013005
    Cr-Commit-Position: refs/heads/master@{#453038}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
@@ -1820,11 +1820,6 @@ public class LocationBarLayout extends FrameLayout
         }
     }
 
-    /** TODO(dfalcantara): Revisit after M58. */
-    protected boolean allowsEmptySearchQueries() {
-        return false;
-    }
-
     /**
      * Sets the query string in the omnibox (ensuring the URL bar has focus and triggering
      * autocomplete for the specified query) as if the user typed it.
@@ -1832,7 +1827,7 @@ public class LocationBarLayout extends FrameLayout
      * @param query The query to be set in the omnibox.
      */
     public void setSearchQuery(final String query) {
-        if (!allowsEmptySearchQueries() && TextUtils.isEmpty(query)) return;
+        if (TextUtils.isEmpty(query)) return;
 
         if (!mNativeInitialized) {
             mDeferredNativeRunnables.add(new Runnable() {
