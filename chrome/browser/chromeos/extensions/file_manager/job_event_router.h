0332dd6810ea55c00102a629219eb1aff4469391
hajimehoshi <hajimehoshi@chromium.org>
2017-02-26 17:16:25 -0800

    Revert of Some linked_ptr removal in chromeos file_browser_handler. (patchset #5 id:80001 of https://codereview.chromium.org/2707193005/ )
    Reason for revert:
    This might be the cause of crash: crbug/696375
    
    Original issue's description:
    > Some linked_ptr removal in chromeos file_browser_handler.
    >
    > BUG=556939
    >
    > Review-Url: https://codereview.chromium.org/2707193005
    > Cr-Commit-Position: refs/heads/master@{#453064}
    > Committed: https://chromium.googlesource.com/chromium/src/+/37ab3be841440d4ec5abf1160acf32c905796d13
    
    TBR=hirono@chromium.org,lazyboy@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=556939
    
    Review-Url: https://codereview.chromium.org/2717213002
    Cr-Commit-Position: refs/heads/master@{#453134}

--- chrome/browser/chromeos/extensions/file_manager/job_event_router.h
+++ chrome/browser/chromeos/extensions/file_manager/job_event_router.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/macros.h"
+#include "base/memory/linked_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/threading/thread_checker.h"
 #include "base/time/time.h"
@@ -82,7 +83,7 @@ class JobEventRouter : public drive::JobListObserver {
   base::TimeDelta event_delay_;
 
   // Set of job that are in the job schedular.
-  std::map<drive::JobID, std::unique_ptr<drive::JobInfo>> drive_jobs_;
+  std::map<drive::JobID, linked_ptr<drive::JobInfo>> drive_jobs_;
 
   // Job info of pending event. |ScheduleDriveFileTransferEvent| registers
   // timeout callback to dispatch this.
