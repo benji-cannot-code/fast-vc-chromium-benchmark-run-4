9c92379482adb960e3b8e2e84e3369e896f1a520
dgn <dgn@chromium.org>
2017-02-27 07:47:58 -0800

    🏠 Close the bottom sheet when opening link in incognito
    Opening links in the regular profile closes the bottom sheet,
    so this makes the behaviour for incognito consistent with that
    and fixes the issue of the sheet being open while the tab model
    switches from regular to incognito.
    
    BUG=690059
    
    Review-Url: https://codereview.chromium.org/2710323003
    Cr-Commit-Position: refs/heads/master@{#453221}

--- chrome/android/java/src/org/chromium/chrome/browser/BasicNativePage.java
+++ chrome/android/java/src/org/chromium/chrome/browser/BasicNativePage.java
@@ -87,6 +87,6 @@ public abstract class BasicNativePage implements NativePage {
      */
     public void onStateChange(String url) {
         if (url.equals(mUrl)) return;
-        mHost.loadUrl(new LoadUrlParams(url));
+        mHost.loadUrl(new LoadUrlParams(url), /* incognito = */ false);
     }
 }
