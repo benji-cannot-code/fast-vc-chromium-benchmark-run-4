b29b1824b88d36e813916b2f93ef1951e1352230
edchin <edchin@chromium.org>
2017-02-27 09:53:07 -0800

    [tab_container] Hack-in fix for broken constraint on animation
    Custom view controller transition animations must be designed
    so as not to break constraints on the presented/dismissed VC's view.
    
    This CL addresses a broken constraint due to the zoom animator.
    The zoom animator will resize the VC's view to a small rect.
    However, that rect is smaller in height than the sum of the
    height constraints on the subviews of the tab_container.
    
    This CL simply reduces the subview height constant. Since the
    transition animation will likely change, there is no need to modify
    the zoom animator at this time.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2717133002
    Cr-Commit-Position: refs/heads/master@{#453249}

--- ios/clean/chrome/browser/ui/tab/tab_container_view_controller.mm
+++ ios/clean/chrome/browser/ui/tab/tab_container_view_controller.mm
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 CGFloat kToolbarHeight = 44.0f;
-CGFloat kTabStripHeight = 200.0f;
+CGFloat kTabStripHeight = 90.0f;
 }
 
 @interface TabContainerViewController ()
