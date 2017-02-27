f40e97973ac958f1d22e6859520522dd7f0465ca
rtoy <rtoy@chromium.org>
2017-02-27 10:12:00 -0800

    Remove unnecessary window WebAudio attributes
    These attributes were added to measure the usage of the new WebAudio constructors.  This has been replaced with a different scheme using the IDL files to specify measurement.
    
    Thus, these are obsolete and should be removed.  The corresponding UseCounter entries are also removed.
    
    BUG=693735
    TEST=none
    
    Review-Url: https://codereview.chromium.org/2700233003
    Cr-Commit-Position: refs/heads/master@{#453259}

--- third_party/WebKit/Source/modules/webaudio/DOMWindowWebAudio.h
+++
@@ -1,7 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// TODO: This is a dummy header file required by the generated binding code.
-// This file should be removed after fixing the code generator.
