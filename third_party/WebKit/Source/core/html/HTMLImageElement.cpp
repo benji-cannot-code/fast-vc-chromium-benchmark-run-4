3edfe7bed8653751ce84dde813987b31e2bb37cc
junov <junov@chromium.org>
2017-02-27 14:48:34 -0800

    Add use counters for recently launched canvas features
    This change adds use counters for the following features:
    * 2D Canvas filter
    * 2D Canvas imageSmoothingQuality
    * <canvas> toBlob and toDataURL
    * OffscreenCanvas.convertToBlob
    * using SVG images as source images for ctx2d.drawImage and
      gl.texImage2d
    
    Use counter for imageSmoothingEnabled is removed. This feature was being
    tracked for the purpose deprecating and removing its webkit-prefixed
    version.  We no longer need it
    
    The Canvas.ContextType histogram was missing an entry for the
    "bitmaprenderer" context type.
    
    BUG=653599
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2703423009
    Cr-Commit-Position: refs/heads/master@{#453368}

--- third_party/WebKit/Source/core/html/HTMLImageElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLImageElement.cpp
@@ -646,6 +646,7 @@ PassRefPtr<Image> HTMLImageElement::getSourceImageForCanvas(
 
   RefPtr<Image> sourceImage;
   if (cachedImage()->getImage()->isSVGImage()) {
+    UseCounter::count(document(), UseCounter::SVGInCanvas2D);
     SVGImage* svgImage = toSVGImage(cachedImage()->getImage());
     IntSize imageSize =
         roundedIntSize(svgImage->concreteObjectSize(defaultObjectSize));
