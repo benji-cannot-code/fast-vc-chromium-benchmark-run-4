1da8639ca391b8147e44e9a6b0492b703de414a5
eseckler <eseckler@chromium.org>
2017-02-27 03:17:18 -0800

    [headless] Use version for product name and user agent.
    Also sets the product name on DevTools server.
    
    BUG=696457
    
    Review-Url: https://codereview.chromium.org/2720673003
    Cr-Commit-Position: refs/heads/master@{#453189}

--- headless/public/headless_browser.cc
+++ headless/public/headless_browser.cc
@@ -8,17 +8,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "content/public/common/user_agent.h"
+#include "headless/public/version.h"
 
 using Options = headless::HeadlessBrowser::Options;
 using Builder = headless::HeadlessBrowser::Options::Builder;
 
 namespace headless {
 
-// Product name for building the default user agent string.
 namespace {
+// Product name for building the default user agent string.
 const char kProductName[] = "HeadlessChrome";
 constexpr gfx::Size kDefaultWindowSize(800, 600);
+
+std::string GetProductNameAndVersion() {
+  return std::string(kProductName) + "/" + PRODUCT_VERSION;
 }
+}  // namespace
 
 Options::Options(int argc, const char** argv)
     : argc(argc),
@@ -27,7 +32,8 @@ Options::Options(int argc, const char** argv)
       single_process_mode(false),
       disable_sandbox(false),
       gl_implementation("osmesa"),
-      user_agent(content::BuildUserAgentFromProduct(kProductName)),
+      product_name_and_version(GetProductNameAndVersion()),
+      user_agent(content::BuildUserAgentFromProduct(product_name_and_version)),
       window_size(kDefaultWindowSize),
       incognito_mode(true),
       enable_crash_reporter(false) {}
@@ -43,6 +49,12 @@ Builder::Builder(int argc, const char** argv) : options_(argc, argv) {}
 Builder::Builder() : options_(0, nullptr) {}
 
 Builder::~Builder() {}
+
+Builder& Builder::SetProductNameAndVersion(
+    const std::string& product_name_and_version) {
+  options_.product_name_and_version = product_name_and_version;
+  return *this;
+}
 
 Builder& Builder::SetUserAgent(const std::string& user_agent) {
   options_.user_agent = user_agent;
