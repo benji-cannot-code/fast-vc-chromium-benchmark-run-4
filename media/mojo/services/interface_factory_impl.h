dfbc855b7be5d8b9aad92d0ac208db331c7c6b96
alokp <alokp@chromium.org>
2017-02-27 14:38:58 -0800

    Replaces media::StrongBindingSet with mojo::StrongBindingSet.
    BUG=604912
    
    Review-Url: https://codereview.chromium.org/2716873003
    Cr-Commit-Position: refs/heads/master@{#453362}

--- media/mojo/services/interface_factory_impl.h
+++ media/mojo/services/interface_factory_impl.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "media/mojo/interfaces/interface_factory.mojom.h"
 #include "media/mojo/services/mojo_cdm_service_context.h"
-#include "media/mojo/services/strong_binding_set.h"
+#include "mojo/public/cpp/bindings/strong_binding_set.h"
 #include "services/service_manager/public/cpp/connector.h"
 #include "services/service_manager/public/cpp/service_context_ref.h"
 
@@ -50,22 +50,22 @@ class InterfaceFactoryImpl : public mojom::InterfaceFactory {
   MojoCdmServiceContext cdm_service_context_;
 
 #if defined(ENABLE_MOJO_AUDIO_DECODER)
-  StrongBindingSet<mojom::AudioDecoder> audio_decoder_bindings_;
+  mojo::StrongBindingSet<mojom::AudioDecoder> audio_decoder_bindings_;
 #endif  // defined(ENABLE_MOJO_AUDIO_DECODER)
 
 #if defined(ENABLE_MOJO_VIDEO_DECODER)
-  StrongBindingSet<mojom::VideoDecoder> video_decoder_bindings_;
+  mojo::StrongBindingSet<mojom::VideoDecoder> video_decoder_bindings_;
 #endif  // defined(ENABLE_MOJO_VIDEO_DECODER)
 
 #if defined(ENABLE_MOJO_RENDERER)
   std::unique_ptr<RendererFactory> renderer_factory_;
-  StrongBindingSet<mojom::Renderer> renderer_bindings_;
+  mojo::StrongBindingSet<mojom::Renderer> renderer_bindings_;
 #endif  // defined(ENABLE_MOJO_RENDERER)
 
 #if defined(ENABLE_MOJO_CDM)
   std::unique_ptr<CdmFactory> cdm_factory_;
   service_manager::mojom::InterfaceProviderPtr interfaces_;
-  StrongBindingSet<mojom::ContentDecryptionModule> cdm_bindings_;
+  mojo::StrongBindingSet<mojom::ContentDecryptionModule> cdm_bindings_;
 #endif  // defined(ENABLE_MOJO_CDM)
 
   scoped_refptr<MediaLog> media_log_;
