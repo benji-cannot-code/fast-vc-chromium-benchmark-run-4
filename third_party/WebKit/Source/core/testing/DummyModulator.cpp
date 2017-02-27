56979c8c79315e38c838a60657e6a40708a81c92
kouhei <kouhei@chromium.org>
2017-02-26 19:51:55 -0800

    [ES6 modules] Introduce ModuleMap
    This CL introduces ModuleMap, which corresponds to "module map" spec concept.
    ModuleMap keeps track of every ModuleScripts loaded inside a single Modulator.
    When a ModuleScript instance is needed, we query ModuleMap to find if there is
    a existing instance of the ModuleScript. If there isn't an existing instance,
    ModuleMap is responsible for fetching it.
    
    See below diagram for where it stands in the stack:
    https://docs.google.com/document/d/1vjiWxwhg9D0GNNOYgw3AxMG0iKOC9I3jlID4GTgZsac/edit#heading=h.47x0qrpzjbj4
    
    BUG=594639
    
    Review-Url: https://codereview.chromium.org/2704323002
    Cr-Commit-Position: refs/heads/master@{#453141}

---
+++ third_party/WebKit/Source/core/testing/DummyModulator.cpp
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/testing/DummyModulator.h"
+
+namespace blink {
+
+DummyModulator::DummyModulator() {}
+
+DummyModulator::~DummyModulator() {}
+
+DEFINE_TRACE(DummyModulator) {}
+
+ScriptModuleResolver* DummyModulator::scriptModuleResolver() {
+  NOTREACHED();
+  return nullptr;
+}
+
+WebTaskRunner* DummyModulator::taskRunner() {
+  NOTREACHED();
+  return nullptr;
+};
+
+void DummyModulator::fetchNewSingleModule(const ModuleScriptFetchRequest&,
+                                          ModuleGraphLevel,
+                                          ModuleScriptLoaderClient*) {
+  NOTREACHED();
+}
+
+}  // namespace blink
