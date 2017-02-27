2764d23c5700d40a916fe665bb09128d7351724a
hiroshige <hiroshige@chromium.org>
2017-02-26 20:52:29 -0800

    Revert of Check whether ScriptResource is still loading due to revalidation (patchset #2 id:20001 of https://codereview.chromium.org/2706303002/ )
    Reason for revert:
    Sufficient data are collected and thus finishing the
    investigation.
    
    BUG=696305
    
    Original issue's description:
    > Check whether ScriptResource is still loading due to revalidation
    >
    > ScriptResource is assumed to be already loaded when it is used by
    > ScriptResource::script() and PendingScript.
    > However, there might slight chances that isLoaded() is false due to
    > revalidation, possibly causing Issue 692856.
    >
    > In order to check whether and how often revalidation can cause
    > isLoaded() to be false in existing DCHECK()s, this CL replaces
    > DCHECK(isLoaded()) with three CHECK()s, each of which fails when:
    > 1. isLoaded() is false not because of revalidation,
    > 2. isLoaded() is false because revalidation is ongoing and response is
    >    already received, and
    > 3. isLoaded() is false because revalidation is ongoing and response is
    >    not yet received.
    >
    > BUG=692856
    >
    > Review-Url: https://codereview.chromium.org/2706303002
    > Cr-Commit-Position: refs/heads/master@{#453057}
    > Committed: https://chromium.googlesource.com/chromium/src/+/6dc58b5746363ddbb99addb4c06c69a385212b7d
    
    TBR=japhet@chromium.org,kouhei@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=692856
    
    Review-Url: https://codereview.chromium.org/2714403002
    Cr-Commit-Position: refs/heads/master@{#453150}

--- third_party/WebKit/Source/platform/loader/fetch/Resource.cpp
+++ third_party/WebKit/Source/platform/loader/fetch/Resource.cpp
@@ -569,7 +569,6 @@ void Resource::setRevalidatingRequest(const ResourceRequest& request) {
   DCHECK(!request.isNull());
   CHECK(!m_isRevalidationStartForbidden);
   m_isRevalidating = true;
-  m_hasRevalidated = true;
   m_resourceRequest = request;
   m_status = ResourceStatus::NotStarted;
 }
