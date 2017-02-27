7fa4cf2acc90ab0b59591dcbb9cde6a304c2e214
dtapuska <dtapuska@chromium.org>
2017-02-26 17:14:26 -0800

    Add a passthrough touch event queue.
    The passthrough touch event queue does no queuing of sending events. It
    does handle the fact that touch events can be ack'd out of order and
    it makes it appear to the higher layers in the browsers that the acks
    are all in order. ie; You can get an ack for a touchend before a touchstart
    from the renderer. If the compositor acks the touchend and the touchstart
    is sent to the main thread. This class makes this appear logical to the
    browser which relies upon things like the touchstart being ack before the
    touchend.
    
    This change allows coalescing to only happen in the main thread event queue.
    It also removes the async sending of touchmoves. All touchmoves are
    sent right away.
    
    BUG=642368
    TBR=clamy@chromium.org
    
    Review-Url: https://codereview.chromium.org/2715623002
    Cr-Commit-Position: refs/heads/master@{#453133}

--- content/browser/renderer_host/input/input_router_impl.cc
+++ content/browser/renderer_host/input/input_router_impl.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/input/input_ack_handler.h"
 #include "content/browser/renderer_host/input/input_router_client.h"
 #include "content/browser/renderer_host/input/legacy_touch_event_queue.h"
+#include "content/browser/renderer_host/input/passthrough_touch_event_queue.h"
 #include "content/browser/renderer_host/input/touch_event_queue.h"
 #include "content/browser/renderer_host/input/touchpad_tap_suppression_controller.h"
 #include "content/common/content_constants_internal.h"
@@ -92,9 +93,15 @@ InputRouterImpl::InputRouterImpl(IPC::Sender* sender,
       wheel_event_queue_(this,
                          base::FeatureList::IsEnabled(
                              features::kTouchpadAndWheelScrollLatching)),
-      touch_event_queue_(new LegacyTouchEventQueue(this, config.touch_config)),
       gesture_event_queue_(this, this, config.gesture_config),
       device_scale_factor_(1.f) {
+  if (base::FeatureList::IsEnabled(features::kRafAlignedTouchInputEvents))
+    touch_event_queue_.reset(
+        new PassthroughTouchEventQueue(this, config.touch_config));
+  else
+    touch_event_queue_.reset(
+        new LegacyTouchEventQueue(this, config.touch_config));
+
   DCHECK(sender);
   DCHECK(client);
   DCHECK(ack_handler);
