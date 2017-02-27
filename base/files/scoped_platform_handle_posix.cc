38f5dca25941c4b4bbd53a5923319a9cc6f9fae6
rockot <rockot@chromium.org>
2017-02-27 09:21:17 -0800

    Introduce base::ScopedPlatformHandle
    ScopedPlatformHandle encapsulates ownership of one of various types
    of platform-specific resource handles including POSIX file descriptors,
    and Windows handles.
    
    The purpose of this type is to have a consistent representation of such
    resources for the sake of safe ownership semantics across various API
    boundaries, relegating platform-specific details to either the code which
    initially acquires the resource or the code which actually uses it.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2705743002
    Cr-Commit-Position: refs/heads/master@{#453242}

---
+++ base/files/scoped_platform_handle_posix.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/files/scoped_platform_handle.h"
+
+namespace base {
+
+bool ScopedPlatformHandle::is_valid() const {
+  return handle_.is_valid();
+}
+
+void ScopedPlatformHandle::reset() {
+  handle_.reset();
+}
+
+ScopedPlatformHandle::HandleType ScopedPlatformHandle::get() const {
+  return handle_.get();
+}
+
+ScopedPlatformHandle::HandleType ScopedPlatformHandle::release() {
+  return handle_.release();
+}
+
+}  // namespace base
---
+++ base/files/scoped_platform_handle_win.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/files/scoped_platform_handle.h"
+
+namespace base {
+
+bool ScopedPlatformHandle::is_valid() const {
+  return handle_.IsValid();
+}
+
+void ScopedPlatformHandle::reset() {
+  handle_.Close();
+}
+
+ScopedPlatformHandle::HandleType ScopedPlatformHandle::get() const {
+  return handle_.Get();
+}
+
+ScopedPlatformHandle::HandleType ScopedPlatformHandle::release() {
+  return handle_.Take();
+}
+
+}  // namespace base
