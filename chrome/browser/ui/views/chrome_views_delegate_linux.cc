079253c8be96e036eea25edbeecce5012bcf3963
sky <sky@chromium.org>
2017-02-27 14:06:53 -0800

    Refactors ChromeViewsDelegate::OnBeforeWidgetInit to be more readable
    ChromeViewsDelegate::OnBeforeWidgetInit has grown into an impenetrable
    long function. This splits it up to be more readable. Specifically:
    
    . Adds ChromeViewsDelegate::CreateNativeWidget(), with implementations
      in per platform appropriate files.
    . Linux and windows calls into native_widget_factory to create the
      NativeWidget (once they have decided which type to create). This
      both creates the NativeWidget and assigns the profile.
    
    BUG=none
    TEST=none
    R=erg@chromium.org
    
    Review-Url: https://codereview.chromium.org/2724443002
    Cr-Commit-Position: refs/heads/master@{#453353}

---
+++ chrome/browser/ui/views/chrome_views_delegate_linux.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/chrome_views_delegate.h"
+
+#include "chrome/browser/ui/views/native_widget_factory.h"
+
+views::NativeWidget* ChromeViewsDelegate::CreateNativeWidget(
+    views::Widget::InitParams* params,
+    views::internal::NativeWidgetDelegate* delegate) {
+  NativeWidgetType native_widget_type =
+      (params->parent && params->type != views::Widget::InitParams::TYPE_MENU &&
+       params->type != views::Widget::InitParams::TYPE_MENU)
+          ? NativeWidgetType::NATIVE_WIDGET_AURA
+          : NativeWidgetType::DESKTOP_NATIVE_WIDGET_AURA;
+  return ::CreateNativeWidget(native_widget_type, params, delegate);
+}
---
+++ chrome/browser/ui/views/chrome_views_delegate_mac.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/chrome_views_delegate.h"
+
+views::NativeWidget* ChromeViewsDelegate::CreateNativeWidget(
+    views::Widget::InitParams* params,
+    views::internal::NativeWidgetDelegate* delegate) {
+  // By returning null Widget creates the default NativeWidget implementation.
+  return nullptr;
+}
