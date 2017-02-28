7f1be506cb1eb43b0b25ddf1e1ebecbf8d5782c9
ramyasharma <ramyasharma@chromium.org>
2017-02-27 18:37:01 -0800

    Mac: Fix crash in ExternalProtocolDialogController due to null WebContents.
    Since r448894 ExternalProtocolDialogController started dereferencing
    a possibly-null WebContents that it did not previously.
    
    To fix, check for null in before proceeding with setting block state, and
    just release the dialog window.
    
    BUG=696456
    
    Review-Url: https://codereview.chromium.org/2719243002
    Cr-Commit-Position: refs/heads/master@{#453472}

--- chrome/browser/ui/cocoa/external_protocol_dialog_cocoa.mm
+++ chrome/browser/ui/cocoa/external_protocol_dialog_cocoa.mm
@@ -98,22 +98,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   content::WebContents* web_contents =
       tab_util::GetWebContentsByID(render_process_host_id_, routing_id_);
-  Profile* profile =
-      Profile::FromBrowserContext(web_contents->GetBrowserContext());
 
-  bool isChecked = [[alert_ suppressionButton] state] == NSOnState;
-  // Set the "don't warn me again" info.
-  if (isChecked)
-    ExternalProtocolHandler::SetBlockState(url_.scheme(), blockState, profile);
+  if (web_contents) {
+    Profile* profile =
+        Profile::FromBrowserContext(web_contents->GetBrowserContext());
+    bool isChecked = [[alert_ suppressionButton] state] == NSOnState;
+    // Set the "don't warn me again" info.
+    if (isChecked)
+      ExternalProtocolHandler::SetBlockState(url_.scheme(), blockState,
+                                             profile);
 
-  ExternalProtocolHandler::RecordCheckboxStateMetrics(isChecked);
-  ExternalProtocolHandler::RecordHandleStateMetrics(isChecked, blockState);
+    ExternalProtocolHandler::RecordCheckboxStateMetrics(isChecked);
+    ExternalProtocolHandler::RecordHandleStateMetrics(isChecked, blockState);
 
-  if (blockState == ExternalProtocolHandler::DONT_BLOCK) {
-    UMA_HISTOGRAM_LONG_TIMES("clickjacking.launch_url",
-                             base::Time::Now() - creation_time_);
+    if (blockState == ExternalProtocolHandler::DONT_BLOCK) {
+      UMA_HISTOGRAM_LONG_TIMES("clickjacking.launch_url",
+                               base::Time::Now() - creation_time_);
 
-    ExternalProtocolHandler::LaunchUrlWithoutSecurityCheck(url_, web_contents);
+      ExternalProtocolHandler::LaunchUrlWithoutSecurityCheck(url_,
+                                                             web_contents);
+    }
   }
 
   [self autorelease];
