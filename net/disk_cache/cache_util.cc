489b8965a9b5ed60eb9e36277ff67ee2694022a3
fdoray <fdoray@chromium.org>
2017-02-27 19:00:41 -0800

    Use TaskScheduler instead of WorkerPool in cache_util.cc.
    The following traits are used:
    
    Priority: BACKGROUND
      User won't notice if this task takes an arbitrarily long time
      to complete.
    
    Shutdown behavior: CONTINUE_ON_SHUTDOWN
      Tasks posted with this mode which have not started executing before
      shutdown is initiated will never run. Tasks with this mode running at
      shutdown will be ignored (the worker will not be joined).
    
      Note: Tasks that were previously posted to base::WorkerPool should
      use this shutdown behavior because this is how base::WorkerPool
      handles all its tasks.
    
    May Block:
      Tasks posted with MayBlock() may block. This includes but is not
      limited to tasks that wait on synchronous file I/O operations:
      read or write a file from disk, interact with a pipe or a socket,
      rename or delete a file, enumerate files in a directory, etc. This
      trait isn't required for the mere use of locks.
    
    BUG=659191
    
    Review-Url: https://codereview.chromium.org/2674163003
    Cr-Commit-Position: refs/heads/master@{#453479}

--- net/disk_cache/cache_util.cc
+++ net/disk_cache/cache_util.cc
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/thread_restrictions.h"
-#include "base/threading/worker_pool.h"
 
 namespace {
 
@@ -137,8 +137,13 @@ bool DelayedCacheCleanup(const base::FilePath& full_path) {
     return false;
   }
 
-  base::WorkerPool::PostTask(
-      FROM_HERE, base::Bind(&CleanupCallback, path, name_str), true);
+  base::PostTaskWithTraits(
+      FROM_HERE, base::TaskTraits()
+                     .MayBlock()
+                     .WithPriority(base::TaskPriority::BACKGROUND)
+                     .WithShutdownBehavior(
+                         base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN),
+      base::Bind(&CleanupCallback, path, name_str));
   return true;
 }
 
