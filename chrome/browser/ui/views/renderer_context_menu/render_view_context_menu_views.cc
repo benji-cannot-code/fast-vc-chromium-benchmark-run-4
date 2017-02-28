389db179d653e542fd1e0009298550ec84c0a602
thestig <thestig@chromium.org>
2017-02-28 15:50:47 -0800

    Make RenderViewContextMenuViews::GetBrowserAcceleratorProvider() work for GuestViews.
    Thereby making F11 work for the PDF Viewer in the context menu.
    
    BUG=674108
    
    Review-Url: https://codereview.chromium.org/2720963002
    Cr-Commit-Position: refs/heads/master@{#453750}

--- chrome/browser/ui/views/renderer_context_menu/render_view_context_menu_views.cc
+++ chrome/browser/ui/views/renderer_context_menu/render_view_context_menu_views.cc
@@ -228,7 +228,7 @@ bool RenderViewContextMenuViews::IsCommandIdEnabled(int command_id) const {
 
 ui::AcceleratorProvider*
 RenderViewContextMenuViews::GetBrowserAcceleratorProvider() const {
-  Browser* browser = chrome::FindBrowserWithWebContents(source_web_contents_);
+  Browser* browser = GetBrowser();
   if (!browser)
     return nullptr;
 
