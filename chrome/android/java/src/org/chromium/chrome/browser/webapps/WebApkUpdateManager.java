f37168594bad68e91cd96ee7de691b878806d401
pkotwicz <pkotwicz@chromium.org>
2017-02-28 13:07:10 -0800

    Add integration tests for WebAPKs
    This CL adds an integration test to verify that launching a WebAPK:
    - Does not crash
    - The initial URL eventually finishes loading
    - The splash screen eventually hides
    
    BUG=691848
    
    Review-Url: https://codereview.chromium.org/2701333002
    Cr-Commit-Position: refs/heads/master@{#453700}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkUpdateManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkUpdateManager.java
@@ -47,6 +47,9 @@ public class WebApkUpdateManager implements WebApkUpdateDataFetcher.Observer {
      */
     private static final int MAX_UPDATE_ATTEMPTS = 3;
 
+    /** Whether updates are enabled. Some tests disable updates. */
+    private static boolean sUpdatesEnabled = true;
+
     /** Data extracted from the WebAPK's launch intent and from the WebAPK's Android Manifest. */
     private WebApkInfo mInfo;
 
@@ -123,6 +126,10 @@ public class WebApkUpdateManager implements WebApkUpdateDataFetcher.Observer {
 
     public boolean getHasPendingUpdateForTesting() {
         return mPendingUpdate != null;
+    }
+
+    public static void setUpdatesEnabledForTesting(boolean enabled) {
+        sUpdatesEnabled = enabled;
     }
 
     @Override
@@ -309,6 +316,10 @@ public class WebApkUpdateManager implements WebApkUpdateDataFetcher.Observer {
      */
     private boolean shouldCheckIfWebManifestUpdated(
             WebappDataStorage storage, WebApkInfo info, boolean previousUpdateSucceeded) {
+        if (!sUpdatesEnabled) {
+            return false;
+        }
+
         if (CommandLine.getInstance().hasSwitch(
                     ChromeSwitches.CHECK_FOR_WEB_MANIFEST_UPDATE_ON_STARTUP)) {
             return true;
