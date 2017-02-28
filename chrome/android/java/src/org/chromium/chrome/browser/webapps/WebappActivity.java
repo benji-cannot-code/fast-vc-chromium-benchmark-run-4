c8d4432715220ecd64e64526c73fc058bd23567e
sergeyu <sergeyu@chromium.org>
2017-02-28 14:34:37 -0800

    Reland of Add integration tests for WebAPKs (patchset #1 id:1 of https://codereview.chromium.org/2719403005/ )
    Reason for revert:
    Build breakage not related to this CL.
    
    Original issue's description:
    > Revert of Add integration tests for WebAPKs (patchset #5 id:180001 of https://codereview.chromium.org/2701333002/ )
    >
    > Reason for revert:
    > Broke android build: https://uberchromegw.corp.google.com/i/chromium.linux/builders/Android%20Builder
    >
    > Original issue's description:
    > > Add integration tests for WebAPKs
    > >
    > > This CL adds an integration test to verify that launching a WebAPK:
    > > - Does not crash
    > > - The initial URL eventually finishes loading
    > > - The splash screen eventually hides
    > >
    > > BUG=691848
    > >
    > > Review-Url: https://codereview.chromium.org/2701333002
    > > Cr-Commit-Position: refs/heads/master@{#453700}
    > > Committed: https://chromium.googlesource.com/chromium/src/+/6a6e60653e99e4f6ae358a282e2893d05ef04427
    >
    > TBR=dfalcantara@chromium.org,agrieve@chromium.org,pkotwicz@chromium.org
    > # Skipping CQ checks because original CL landed less than 1 days ago.
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=691848
    >
    > Review-Url: https://codereview.chromium.org/2719403005
    > Cr-Commit-Position: refs/heads/master@{#453728}
    > Committed: https://chromium.googlesource.com/chromium/src/+/61822c770a48bfdaa5a5d0e3b525db37123b1ce9
    
    TBR=dfalcantara@chromium.org,agrieve@chromium.org,pkotwicz@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=691848
    
    Review-Url: https://codereview.chromium.org/2724543003
    Cr-Commit-Position: refs/heads/master@{#453730}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappActivity.java
@@ -642,7 +642,7 @@ public class WebappActivity extends FullScreenActivity {
     }
 
     @VisibleForTesting
-    boolean isSplashScreenVisibleForTests() {
+    public boolean isSplashScreenVisibleForTests() {
         return mSplashScreen != null;
     }
 
