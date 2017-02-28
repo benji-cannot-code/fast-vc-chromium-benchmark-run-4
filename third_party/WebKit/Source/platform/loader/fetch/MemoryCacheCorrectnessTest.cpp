046db77d3c6ead2c07c7ed3a67dd8dad74e974c8
kouhei <kouhei@chromium.org>
2017-02-27 22:14:25 -0800

    mv FetchTestingPlatformSupport to platform/loader/testing
    This CL moves FetchTestingPlatformSupport to platform/loader/testing dir and under test_support build target.
    This CL prepares its usage in ModuleMapTest to be introduced in:
    https://codereview.chromium.org/2704323002/
    
    BUG=594639
    
    Review-Url: https://codereview.chromium.org/2718943003
    Cr-Commit-Position: refs/heads/master@{#453518}

--- third_party/WebKit/Source/platform/loader/fetch/MemoryCacheCorrectnessTest.cpp
+++ third_party/WebKit/Source/platform/loader/fetch/MemoryCacheCorrectnessTest.cpp
@@ -33,11 +33,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "platform/loader/fetch/FetchContext.h"
 #include "platform/loader/fetch/FetchRequest.h"
-#include "platform/loader/fetch/MockFetchContext.h"
 #include "platform/loader/fetch/MockResource.h"
 #include "platform/loader/fetch/RawResource.h"
 #include "platform/loader/fetch/Resource.h"
 #include "platform/loader/fetch/ResourceFetcher.h"
+#include "platform/loader/testing/MockFetchContext.h"
 #include "platform/network/ResourceRequest.h"
 #include "platform/testing/TestingPlatformSupport.h"
 #include "testing/gtest/include/gtest/gtest.h"
--- third_party/WebKit/Source/platform/loader/testing/FetchTestingPlatformSupport.h
+++ third_party/WebKit/Source/platform/loader/testing/FetchTestingPlatformSupport.h
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef FetchTestingPlatformSupport_h
 #define FetchTestingPlatformSupport_h
 
+#include <memory>
 #include "platform/heap/Persistent.h"
 #include "platform/testing/TestingPlatformSupport.h"
-#include <memory>
 
 namespace blink {
 
