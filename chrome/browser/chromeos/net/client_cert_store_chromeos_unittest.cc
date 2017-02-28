57eea62e4ec9e82b12bab0ae4188d23d8f2e5209
fdoray <fdoray@chromium.org>
2017-02-27 18:19:38 -0800

    Revert of Use TaskScheduler instead of WorkerPool in client_cert_store_chromeos.cc. (patchset #7 id:120001 of https://codereview.chromium.org/2533473002/ )
    Reason for revert:
    Using TaskScheduler with NSS is not safe until TaskScheduler supports dynamically growing its thread pool.
    
    Original issue's description:
    > Use TaskScheduler instead of WorkerPool in client_cert_store_chromeos.cc.
    >
    > This CL replaces base::WorkerPool::PostTask() with
    > base::PostTaskWithTraits(). The following traits are used:
    >
    > Priority: Inherited (default)
    >   The priority is inherited from the calling context (i.e. TaskTraits
    >   are initialized with the priority of the current task).
    >
    > Shutdown behavior: CONTINUE_ON_SHUTDOWN
    >   Tasks posted with this mode which have not started executing before
    >   shutdown is initiated will never run. Tasks with this mode running at
    >   shutdown will be ignored (the worker will not be joined).
    >
    > With File IO:
    >   The task waits on synchronous file IO operations.
    >
    > With Wait:
    >   The task waits on things other than synchronous file IO
    >   operations (e.g. WaitableEvent, ConditionVariable, join a thread,
    >   join a process, blocking system call that doesn't involve
    >   interactions with the file system).
    >
    > BUG=659191
    >
    > Committed: https://crrev.com/14589e9ca09b5cf03be779b0c1158cc5388def05
    > Cr-Commit-Position: refs/heads/master@{#436087}
    
    TBR=gab@chromium.org,mattm@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=659191
    
    Review-Url: https://codereview.chromium.org/2720143003
    Cr-Commit-Position: refs/heads/master@{#453460}

--- chrome/browser/chromeos/net/client_cert_store_chromeos_unittest.cc
+++ chrome/browser/chromeos/net/client_cert_store_chromeos_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/net/client_cert_store_chromeos.h"
 
+#include <memory>
 #include <string>
 
 #include "base/callback.h"
@@ -14,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
-#include "base/test/scoped_task_scheduler.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/chromeos/certificate_provider/certificate_provider.h"
 #include "crypto/scoped_test_nss_db.h"
@@ -78,7 +78,7 @@ class TestCertFilter : public ClientCertStoreChromeOS::CertFilter {
 
 class ClientCertStoreChromeOSTest : public ::testing::Test {
  public:
-  ClientCertStoreChromeOSTest() {}
+  ClientCertStoreChromeOSTest() : message_loop_(new base::MessageLoopForIO()) {}
 
   scoped_refptr<net::X509Certificate> ImportCertToSlot(
       const std::string& cert_filename,
@@ -89,7 +89,7 @@ class ClientCertStoreChromeOSTest : public ::testing::Test {
   }
 
  private:
-  base::test::ScopedTaskScheduler scoped_task_scheduler_;
+  std::unique_ptr<base::MessageLoop> message_loop_;
 };
 
 // Ensure that cert requests, that are started before the filter is initialized,
