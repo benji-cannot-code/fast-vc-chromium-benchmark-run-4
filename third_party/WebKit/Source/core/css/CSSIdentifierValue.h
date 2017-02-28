6f36f12d79c623990f1064cb492fe797e18e8956
shend <shend@chromium.org>
2017-02-28 10:11:44 -0800

    Generate mappings between CSSValueID and ComputedStyle enums.
    To convert between CSSValueIDs and ComputedStyle enums, we currently
    use handwritten switch statements. This patch generates, for every
    'keyword' field in CSSProperties.json5, a pair of mapping functions
    that convert between a CSSValueID and the corresponding enum of the
    field. This makes it easier to add new CSS properties and reduces the
    risk of typos in the handwritten mapping functions.
    
    As we generate more CSS properties, we can remove more of the
    handwritten switch statements. For special cases that we can't
    generate (e.g. when the mapping is not bijective), we can overload/
    specialize the mapping functions in C++.
    
    The generated mappings file CSSValueMappings.h is at:
    https://gist.github.com/anonymous/93e797f4e99c3e8aba829139f612abe0
    
    The names for the mapping functions comes from:
    https://codereview.chromium.org/2366243002/
    
    BUG=665272
    
    Review-Url: https://codereview.chromium.org/2702173002
    Cr-Commit-Position: refs/heads/master@{#453643}

--- third_party/WebKit/Source/core/css/CSSIdentifierValue.h
+++ third_party/WebKit/Source/core/css/CSSIdentifierValue.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/CSSValueKeywords.h"
 #include "core/css/CSSValue.h"
+#include "core/css/CSSValueIDMappings.h"
 #include "wtf/TypeTraits.h"
 
 namespace blink {
@@ -42,7 +43,10 @@ class CORE_EXPORT CSSIdentifierValue : public CSSValue {
   }
 
   template <typename T>
-  inline T convertTo() const;  // Defined in CSSPrimitiveValueMappings.h
+  inline T convertTo()
+      const {  // Overridden for special cases in CSSPrimitiveValueMappings.h
+    return cssValueIDToPlatformEnum<T>(m_valueID);
+  }
 
   DECLARE_TRACE_AFTER_DISPATCH();
 
@@ -52,7 +56,10 @@ class CORE_EXPORT CSSIdentifierValue : public CSSValue {
   // TODO(sashab): Remove this function, and update mapping methods to
   // specialize the create() method instead.
   template <typename T>
-  CSSIdentifierValue(T);  // Defined in CSSPrimitiveValueMappings.h
+  CSSIdentifierValue(
+      T t)  // Overriden for special cases in CSSPrimitiveValueMappings.h
+      : CSSValue(IdentifierClass),
+        m_valueID(platformEnumToCSSValueID(t)) {}
 
   CSSIdentifierValue(const Length&);
 
