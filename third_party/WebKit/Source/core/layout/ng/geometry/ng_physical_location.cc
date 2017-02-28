db3e974bf607fabd1213dbfa03eec841de1a34b4
eae <eae@chromium.org>
2017-02-27 17:27:31 -0800

    [LayoutNG] Split geometry structs into separate files
    Move NGLogicalOffset, NGLogicalSize, NGPhysicalLocation, NGPhysicalSize,
    NGPhysicalOffset and NGPhysicalRect into their own files under geometry.
    
    R=cbiesinger@chromium.org
    
    Review-Url: https://codereview.chromium.org/2721513002
    Cr-Commit-Position: refs/heads/master@{#453437}

---
+++ third_party/WebKit/Source/core/layout/ng/geometry/ng_physical_location.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/layout/ng/geometry/ng_physical_location.h"
+
+#include "wtf/text/WTFString.h"
+
+namespace blink {
+
+bool NGPhysicalLocation::operator==(const NGPhysicalLocation& other) const {
+  return other.left == left && other.top == top;
+}
+
+String NGPhysicalLocation::ToString() const {
+  return String::format("%dx%d", left.toInt(), top.toInt());
+}
+
+}  // namespace blink
