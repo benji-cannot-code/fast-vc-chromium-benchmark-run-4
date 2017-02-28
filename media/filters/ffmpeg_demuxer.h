4f6eca385bf93348fd688faa5cda5986f70a9cf1
dalecurtis <dalecurtis@chromium.org>
2017-02-27 18:34:28 -0800

    Replace FFmpegDemuxer thread per element with base::TaskScheduler.
    I don't think we need to keep starting a thread per <audio> or
    <video> element anymore. Instead we can use a shared pool that
    only spins up a new thread when needed.
    
    This change switches FFmpegDemuxer from creating its own thread
    to using base::TaskScheduler for executing blocking network reads.
    The TaskScheduler API is perfectly suited for our use case and
    required little changes.
    
    Doing this required some changes to our BlockingUrlProtocol to
    make it safe to access after FFmpegDemuxer and potentially the
    DataSource have been destroyed.
    
    This should reduce the number of out of threads crashes we see on
    a fairly regular basis.
    
    BUG=61293
    TEST=existing tests all pass, no slowdown noticed on pages like gfycat.com
    
    Review-Url: https://codereview.chromium.org/2710133003
    Cr-Commit-Position: refs/heads/master@{#453470}

--- media/filters/ffmpeg_demuxer.h
+++ media/filters/ffmpeg_demuxer.h
@@ -33,7 +33,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/macros.h"
-#include "base/threading/thread.h"
+#include "base/memory/weak_ptr.h"
+#include "base/sequenced_task_runner.h"
+#include "base/single_thread_task_runner.h"
 #include "media/base/audio_decoder_config.h"
 #include "media/base/decoder_buffer.h"
 #include "media/base/decoder_buffer_queue.h"
@@ -296,8 +298,12 @@ class MEDIA_EXPORT FFmpegDemuxer : public Demuxer {
 
   scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
 
-  // Thread on which all blocking FFmpeg operations are executed.
-  base::Thread blocking_thread_;
+  // Task runner on which all blocking FFmpeg operations are executed; retrieved
+  // from base::TaskScheduler.
+  scoped_refptr<base::SequencedTaskRunner> blocking_task_runner_;
+
+  // Indicates if Stop() has been called.
+  bool stopped_;
 
   // Tracks if there's an outstanding av_read_frame() operation.
   //
