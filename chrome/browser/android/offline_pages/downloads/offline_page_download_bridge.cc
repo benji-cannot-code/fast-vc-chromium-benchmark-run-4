09c382f6547a5b1ce2fdb6a3693368e682aa6e27
dimich <dimich@chromium.org>
2017-02-27 17:08:51 -0800

    This fixes issues that prevent progress indication for Offline Pages in Download Home: - The OfflinePageItemWrapper has to produce status string. - The DownloadUIAdapter wrong hookup was causing byte counts to not update.
    The David's CL https://codereview.chromium.org/2704853002/ is still necessary as it adds buttons hookup.
    
    BUG=681269
    
    Review-Url: https://codereview.chromium.org/2716013003
    Cr-Commit-Position: refs/heads/master@{#453431}

--- chrome/browser/android/offline_pages/downloads/offline_page_download_bridge.cc
+++ chrome/browser/android/offline_pages/downloads/offline_page_download_bridge.cc
@@ -43,10 +43,6 @@ using base::android::JavaParamRef;
 using base::android::ScopedJavaGlobalRef;
 using base::android::ScopedJavaLocalRef;
 
-namespace {
-const char kDownloadUIAdapterKey[] = "download-ui-adapter";
-}
-
 namespace offline_pages {
 namespace android {
 
@@ -477,8 +473,8 @@ static jlong Init(JNIEnv* env,
       OfflinePageModelFactory::GetForBrowserContext(browser_context);
   DCHECK(offline_page_model);
 
-  DownloadUIAdapter* adapter = static_cast<DownloadUIAdapter*>(
-      offline_page_model->GetUserData(kDownloadUIAdapterKey));
+  DownloadUIAdapter* adapter =
+      DownloadUIAdapter::FromOfflinePageModel(offline_page_model);
 
   if (!adapter) {
     RequestCoordinator* request_coordinator =
@@ -487,7 +483,7 @@ static jlong Init(JNIEnv* env,
     adapter = new DownloadUIAdapter(
         offline_page_model, request_coordinator,
         base::MakeUnique<DownloadUIAdapterDelegate>(offline_page_model));
-    offline_page_model->SetUserData(kDownloadUIAdapterKey, adapter);
+    DownloadUIAdapter::AttachToOfflinePageModel(adapter, offline_page_model);
   }
 
   return reinterpret_cast<jlong>(
--- chrome/browser/android/offline_pages/prerendering_offliner.cc
+++ chrome/browser/android/offline_pages/prerendering_offliner.cc
@@ -20,9 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 
 namespace offline_pages {
-namespace {
-const char kDownloadUIAdapterKey[] = "download-ui-adapter";
-}
 
 PrerenderingOffliner::PrerenderingOffliner(
     content::BrowserContext* browser_context,
@@ -42,10 +39,8 @@ void PrerenderingOffliner::OnNetworkProgress(const SavePageRequest& request,
                                              int64_t bytes) {
   if (!pending_request_)
     return;
-
-  DownloadUIAdapter* ui_adapter = static_cast<DownloadUIAdapter*>(
-      offline_page_model_->GetUserData(kDownloadUIAdapterKey));
-
+  DownloadUIAdapter* ui_adapter =
+      DownloadUIAdapter::FromOfflinePageModel(offline_page_model_);
   if (!ui_adapter)
     return;
 
