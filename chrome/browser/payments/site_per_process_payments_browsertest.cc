bdd63213722224470e695e918d7e6638dc7b082d
mathp <mathp@chromium.org>
2017-02-28 04:24:42 -0800

    [Payments] Put Web Payments feature on desktop behind flag
    --enable-features=WebPayments
    
    This is in addition to --enable-experimental-web-features
    
    BUG=696032
    TEST=builds, all tests
    
    Review-Url: https://codereview.chromium.org/2715013002
    Cr-Commit-Position: refs/heads/master@{#453575}

--- chrome/browser/payments/site_per_process_payments_browsertest.cc
+++ chrome/browser/payments/site_per_process_payments_browsertest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_utils.h"
@@ -35,6 +36,8 @@ class SitePerProcessPaymentsBrowserTest : public InProcessBrowserTest {
     command_line->AppendSwitch(switches::kIgnoreCertificateErrors);
     command_line->AppendSwitch(
         switches::kEnableExperimentalWebPlatformFeatures);
+    command_line->AppendSwitchASCII(switches::kEnableFeatures,
+                                    features::kWebPayments.name);
     // Append --site-per-process flag.
     content::IsolateAllSitesForTesting(command_line);
   }
