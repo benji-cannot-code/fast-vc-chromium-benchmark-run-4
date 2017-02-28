1b2d6a0321d7e9144fc79cb50aa38ea8ba117d5a
mxnguyen <mxnguyen@chromium.org>
2017-02-28 09:51:43 -0800

    Change the update frequency for components to 5 hours. This is to make the component updater frequency conform with Omaha/Extension update frequency.
    BUG=
    
    R=cpu@chromium.org,sorin@chromium.org,waffles@chromium.org
    
    Review-Url: https://codereview.chromium.org/2718233002
    Cr-Commit-Position: refs/heads/master@{#453637}

--- components/component_updater/configurator_impl.cc
+++ components/component_updater/configurator_impl.cc
@@ -131,7 +131,7 @@ int ConfiguratorImpl::InitialDelay() const {
 }
 
 int ConfiguratorImpl::NextCheckDelay() const {
-  return 6 * kDelayOneHour;
+  return 5 * kDelayOneHour;
 }
 
 int ConfiguratorImpl::StepDelay() const {
