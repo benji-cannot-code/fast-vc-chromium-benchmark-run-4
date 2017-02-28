010057487106b5c7cadc1deb5660069624f2611e
asvitkine <asvitkine@chromium.org>
2017-02-28 10:09:01 -0800

    Remove use of TimeFormatShortDateAndTime() in omnibox internals.
    UMA sampling profiler data has shown that this code path is very
    slow - taking as much as 20ms on startup, since it involves calls
    to ICU which needs to do a lot of work.
    
    Since this is used for debugging only, we don't need to do a fancy
    localized time format, instead it's useful to just print how long
    ago this took place, in hours. This CL does that.
    
    Also changes the base::Time param to be passed by value, which is
    what's recommended by its header file and adds some missing
    includes suggested by git cl lint.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2715243010
    Cr-Commit-Position: refs/heads/master@{#453641}

--- components/omnibox/browser/autocomplete_match.cc
+++ components/omnibox/browser/autocomplete_match.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/omnibox/browser/autocomplete_match.h"
 
-#include "base/i18n/time_formatting.h"
+#include <algorithm>
+#include <utility>
+
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/strings/string16.h"
@@ -13,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_piece.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
-#include "base/strings/utf_string_conversions.h"
+#include "base/strings/stringprintf.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "components/omnibox/browser/autocomplete_provider.h"
@@ -546,10 +548,10 @@ void AutocompleteMatch::RecordAdditionalInfo(const std::string& property,
 }
 
 void AutocompleteMatch::RecordAdditionalInfo(const std::string& property,
-                                             const base::Time& value) {
-  RecordAdditionalInfo(property,
-                       base::UTF16ToUTF8(
-                           base::TimeFormatShortDateAndTime(value)));
+                                             base::Time value) {
+  RecordAdditionalInfo(
+      property, base::StringPrintf("%d hours ago",
+                                   (base::Time::Now() - value).InHours()));
 }
 
 std::string AutocompleteMatch::GetAdditionalInfo(
