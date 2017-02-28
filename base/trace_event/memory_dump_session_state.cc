b8778ab600046115b27bce47ed2eebb037b100b0
thakis <thakis@chromium.org>
2017-02-28 14:11:19 -0800

    Fix ProcessMemoryDumpTest.TakeAllDumpsFrom under msan.
    https://codereview.chromium.org/2582453002 broke this.
    
    BUG=697172
    
    Review-Url: https://codereview.chromium.org/2720093004
    Cr-Commit-Position: refs/heads/master@{#453721}

--- base/trace_event/memory_dump_session_state.cc
+++ base/trace_event/memory_dump_session_state.cc
@@ -8,7 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 namespace trace_event {
 
-MemoryDumpSessionState::MemoryDumpSessionState() {}
+MemoryDumpSessionState::MemoryDumpSessionState()
+    : heap_profiler_breakdown_threshold_bytes_(0) {}
 MemoryDumpSessionState::~MemoryDumpSessionState() {}
 
 void MemoryDumpSessionState::SetStackFrameDeduplicator(
