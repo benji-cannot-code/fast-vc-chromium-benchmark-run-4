3e84ba199e58f343cb6058eac4513a3fd5fe7e9b
zqzhang <zqzhang@chromium.org>
2017-02-28 07:40:32 -0800

    Revert of Fix a notification update issue when navigating to a different URL in the same origin (patchset #2 id:20001 of https://codereview.chromium.org/2720073002/ )
    Reason for revert:
    The CL is an incomplete fix, as there's another underlying issue in MediaSessionServiceImpl wrt RenderFrame loading.
    
    Original issue's description:
    > Fix a notification update issue when navigating to a different URL in the same origin
    >
    > We should reset the notification info for all non-in-page navigation.
    >
    > BUG=696716
    >
    > Review-Url: https://codereview.chromium.org/2720073002
    > Cr-Commit-Position: refs/heads/master@{#453573}
    > Committed: https://chromium.googlesource.com/chromium/src/+/7ed877a5634408b1b4c5791f57ad7ffbfc03d13f
    
    TBR=avayvod@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=696716
    
    Review-Url: https://codereview.chromium.org/2718373003
    Cr-Commit-Position: refs/heads/master@{#453607}

--- chrome/android/java/src/org/chromium/chrome/browser/media/ui/MediaSessionTabHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/ui/MediaSessionTabHelper.java
@@ -301,6 +301,7 @@ public class MediaSessionTabHelper implements MediaImageCallback {
                                 + "Using the full URL instead.");
             }
 
+            if (mOrigin != null && mOrigin.equals(origin)) return;
             mOrigin = origin;
             mFavicon = null;
             mPageMediaImage = null;
