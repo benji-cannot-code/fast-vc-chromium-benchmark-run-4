7c9c1e78b3914673b900dda9728258c75b25412f
carlosk <carlosk@chromium.org>
2017-02-28 12:21:42 -0800

    Only show Last N Pages in the UI when the corresponding tab is visible.
    This patch takes two approaches:
    * NTP suggestions now use a DownloadUIAdapter to manage the set of
      items on the page. This adapter is notified about tab addition
      and removal, and only notifies about pages that correspond to a
      tab.
    * Deletes pages when a tab is closed.
    
    patch from issue 2684973014 at patchset 180001 (http://crrev.com/2684973014#ps180001)
    
    Note: dewittj@, who is currently OOO, is the original author of this patch
    at https://crrev.com/2684973014. I (carlosk@) am taking over it because it
    needed addressing of latest comments and I have another change that depends
    on this one.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2706343007
    Cr-Commit-Position: refs/heads/master@{#453685}

--- chrome/browser/android/offline_pages/offline_page_bridge.cc
+++ chrome/browser/android/offline_pages/offline_page_bridge.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/offline_pages/core/offline_page_feature.h"
 #include "components/offline_pages/core/offline_page_item.h"
 #include "components/offline_pages/core/offline_page_model.h"
+#include "components/offline_pages/core/recent_tabs/recent_tabs_ui_adapter_delegate.h"
 #include "components/offline_pages/core/request_header/offline_page_header.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/web_contents.h"
@@ -571,6 +572,32 @@ void OfflinePageBridge::RemoveRequestsFromQueue(
 
   coordinator->RemoveRequests(
       request_ids, base::Bind(&OnRemoveRequestsDone, j_callback_ref));
+}
+
+void OfflinePageBridge::RegisterRecentTab(JNIEnv* env,
+                                          const JavaParamRef<jobject>& obj,
+                                          int tab_id) {
+  RequestCoordinator* request_coordinator =
+      RequestCoordinatorFactory::GetForBrowserContext(browser_context_);
+
+  RecentTabsUIAdapterDelegate* ui_adapter_delegate =
+      RecentTabsUIAdapterDelegate::FromDownloadUIAdapter(
+          RecentTabsUIAdapterDelegate::GetOrCreateRecentTabsUIAdapter(
+              offline_page_model_, request_coordinator));
+  ui_adapter_delegate->RegisterTab(tab_id);
+}
+
+void OfflinePageBridge::UnregisterRecentTab(JNIEnv* env,
+                                            const JavaParamRef<jobject>& obj,
+                                            int tab_id) {
+  RequestCoordinator* request_coordinator =
+      RequestCoordinatorFactory::GetForBrowserContext(browser_context_);
+
+  RecentTabsUIAdapterDelegate* ui_adapter_delegate =
+      RecentTabsUIAdapterDelegate::FromDownloadUIAdapter(
+          RecentTabsUIAdapterDelegate::GetOrCreateRecentTabsUIAdapter(
+              offline_page_model_, request_coordinator));
+  ui_adapter_delegate->UnregisterTab(tab_id);
 }
 
 void OfflinePageBridge::NotifyIfDoneLoading() const {
