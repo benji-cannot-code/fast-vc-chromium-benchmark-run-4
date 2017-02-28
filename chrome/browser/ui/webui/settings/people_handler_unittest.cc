6dd34bd584ad6a9033111a09f56ed7fb896a25da
pavely <pavely@chromium.org>
2017-02-27 18:15:30 -0800

    Ensure PeopleHandler only calls PSS::RequestStart in response to intention to configure sync
    Currently PSS::RequestStart is called from PushSyncPrefs() which can be
    triggered by internal sync state changes. This call unsuppresses sync.
    
    Instead PSS::RequestStart should be called only in response to user actions to
    configure sync. I think OpenSyncSetup() is a good place.
    
    BUG=694762
    R=tommycli@chromium.org
    
    Review-Url: https://codereview.chromium.org/2711993002
    Cr-Commit-Position: refs/heads/master@{#453457}

--- chrome/browser/ui/webui/settings/people_handler_unittest.cc
+++ chrome/browser/ui/webui/settings/people_handler_unittest.cc
@@ -292,6 +292,8 @@ class PeopleHandlerTest : public testing::Test {
       handler_->sync_startup_tracker_->OnStateChanged(mock_pss_);
   }
 
+  void NotifySyncStateChanged() { handler_->OnStateChanged(mock_pss_); }
+
   virtual std::string GetTestUser() {
     return std::string(kTestUser);
   }
@@ -371,6 +373,7 @@ TEST_F(PeopleHandlerTest, DisplayConfigureWithEngineDisabledAndCancel) {
   EXPECT_CALL(*mock_pss_, IsFirstSetupComplete()).WillRepeatedly(Return(false));
   error_ = GoogleServiceAuthError::AuthErrorNone();
   EXPECT_CALL(*mock_pss_, IsEngineInitialized()).WillRepeatedly(Return(false));
+  EXPECT_CALL(*mock_pss_, RequestStart());
 
   // We're simulating a user setting up sync, which would cause the engine to
   // kick off initialization, but not download user data types. The sync
@@ -395,6 +398,7 @@ TEST_F(PeopleHandlerTest,
   error_ = GoogleServiceAuthError::AuthErrorNone();
   // Sync engine is stopped initially, and will start up.
   EXPECT_CALL(*mock_pss_, IsEngineInitialized()).WillRepeatedly(Return(false));
+  EXPECT_CALL(*mock_pss_, RequestStart());
   SetDefaultExpectationsForConfigPage();
 
   handler_->OpenSyncSetup();
@@ -432,6 +436,7 @@ TEST_F(PeopleHandlerTest,
   EXPECT_CALL(*mock_pss_, IsEngineInitialized())
       .WillOnce(Return(false))
       .WillRepeatedly(Return(true));
+  EXPECT_CALL(*mock_pss_, RequestStart());
   SetDefaultExpectationsForConfigPage();
   handler_->OpenSyncSetup();
 
@@ -452,6 +457,7 @@ TEST_F(PeopleHandlerTest, DisplayConfigureWithEngineDisabledAndSigninFailed) {
   EXPECT_CALL(*mock_pss_, IsFirstSetupComplete()).WillRepeatedly(Return(false));
   error_ = GoogleServiceAuthError::AuthErrorNone();
   EXPECT_CALL(*mock_pss_, IsEngineInitialized()).WillRepeatedly(Return(false));
+  EXPECT_CALL(*mock_pss_, RequestStart());
 
   handler_->OpenSyncSetup();
   ExpectPageStatusChanged(PeopleHandler::kSpinnerPageStatus);
@@ -465,6 +471,14 @@ TEST_F(PeopleHandlerTest, DisplayConfigureWithEngineDisabledAndSigninFailed) {
   EXPECT_EQ(NULL,
             LoginUIServiceFactory::GetForProfile(
                 profile_)->current_login_ui());
+}
+
+// Tests that signals not related to user intention to configure sync don't
+// trigger sync engine start.
+TEST_F(PeopleHandlerTest, OnlyStartEngineWhenConfiguringSync) {
+  EXPECT_CALL(*mock_pss_, IsEngineInitialized()).WillRepeatedly(Return(false));
+  EXPECT_CALL(*mock_pss_, RequestStart()).Times(0);
+  NotifySyncStateChanged();
 }
 
 #if !defined(OS_CHROMEOS)
