23b9eb9b7fbb1d5dfd692baaf02846533f96a710
yyanagisawa <yyanagisawa@chromium.org>
2017-02-27 18:23:36 -0800

    Fix case for WebKit.h and UIKit.h.
    Let me make it work on case sensitive filesystem.
    
    Review-Url: https://codereview.chromium.org/2719763003
    Cr-Commit-Position: refs/heads/master@{#453463}

--- ios/chrome/browser/ui/main/main_view_controller.h
+++ ios/chrome/browser/ui/main/main_view_controller.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_UI_MAIN_MAIN_VIEW_CONTROLLER_H_
 #define IOS_CHROME_BROWSER_UI_MAIN_MAIN_VIEW_CONTROLLER_H_
 
-#import <UIKit/UIKIt.h>
+#import <UIKit/UIKit.h>
 
 // A UIViewController that provides a property to maintain a single child view
 // controller.
--- ios/web/web_state/ui/crw_context_menu_controller.h
+++ ios/web/web_state/ui/crw_context_menu_controller.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_WEB_WEB_STATE_UI_CRW_CONTEXT_MENU_CONTROLLER_H_
 
 #import <Foundation/Foundation.h>
-#import <Webkit/Webkit.h>
+#import <WebKit/WebKit.h>
 
 #import "ios/web/public/block_types.h"
 
