ff8ba35e8960fb88ea6d6e79c5fbb1904a5e6193
benwells <benwells@chromium.org>
2017-02-28 16:44:53 -0800

    Add switch to disable showing the search geolocation disclosure UI.
    This UI can impact perf tests, so a switch is being added to allow the
    impact to be disabled.
    
    BUG=681663
    
    Review-Url: https://codereview.chromium.org/2718313003
    Cr-Commit-Position: refs/heads/master@{#453777}

--- chrome/browser/android/search_geolocation/search_geolocation_disclosure_tab_helper.cc
+++ chrome/browser/android/search_geolocation/search_geolocation_disclosure_tab_helper.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
+#include "base/command_line.h"
 #include "base/feature_list.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
@@ -17,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/chrome_features.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/content_settings/core/common/content_settings.h"
@@ -71,8 +73,10 @@ DEFINE_WEB_CONTENTS_USER_DATA_KEY(SearchGeolocationDisclosureTabHelper);
 SearchGeolocationDisclosureTabHelper::SearchGeolocationDisclosureTabHelper(
     content::WebContents* contents)
     : content::WebContentsObserver(contents) {
-  consistent_geolocation_enabled_ =
-      base::FeatureList::IsEnabled(features::kConsistentOmniboxGeolocation);
+  consistent_geolocation_disclosure_enabled_ =
+      base::FeatureList::IsEnabled(features::kConsistentOmniboxGeolocation) &&
+      !base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kDisableSearchGeolocationDisclosure);
 }
 
 SearchGeolocationDisclosureTabHelper::~SearchGeolocationDisclosureTabHelper() {}
@@ -84,7 +88,7 @@ void SearchGeolocationDisclosureTabHelper::NavigationEntryCommitted(
 
 void SearchGeolocationDisclosureTabHelper::MaybeShowDisclosure(
     const GURL& gurl) {
-  if (!consistent_geolocation_enabled_)
+  if (!consistent_geolocation_disclosure_enabled_)
     return;
 
   if (!ShouldShowDisclosureForUrl(gurl))
