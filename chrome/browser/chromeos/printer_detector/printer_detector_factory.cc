25ed99eb2d2630f0f2e4d30764fa5c58b05dc718
justincarlson <justincarlson@chromium.org>
2017-03-01 10:00:41 -0800

    Split PrinterDetector unified implementation into CUPS/Legacy implementations.
    This is the second step in refactoring printer detector functionality
    before adding more implementation to the CUPS use case.  This cl
    changes no visible functionality.
    
    BUG=616866
    
    Review-Url: https://codereview.chromium.org/2721063002
    Cr-Commit-Position: refs/heads/master@{#453969}

--- chrome/browser/chromeos/printer_detector/printer_detector_factory.cc
+++ chrome/browser/chromeos/printer_detector/printer_detector_factory.cc
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/printer_detector/printer_detector_factory.h"
 
+#include "base/command_line.h"
 #include "chrome/browser/chromeos/printer_detector/printer_detector.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/common/chrome_switches.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "extensions/browser/extensions_browser_client.h"
 
@@ -42,8 +44,14 @@ PrinterDetectorFactory::~PrinterDetectorFactory() {
 
 KeyedService* PrinterDetectorFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
-  return PrinterDetector::Create(Profile::FromBrowserContext(context))
-      .release();
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          ::switches::kEnableNativeCups)) {
+    return PrinterDetector::CreateCups(Profile::FromBrowserContext(context))
+        .release();
+  } else {
+    return PrinterDetector::CreateLegacy(Profile::FromBrowserContext(context))
+        .release();
+  }
 }
 
 bool PrinterDetectorFactory::ServiceIsCreatedWithBrowserContext() const {
--- chrome/browser/chromeos/printer_detector/printer_detector_unittest.cc
+++ chrome/browser/chromeos/printer_detector/printer_detector_unittest.cc
@@ -53,7 +53,7 @@ const char kPrinterAppNotFoundDelegateIDTemplate[] =
 
 std::unique_ptr<KeyedService> CreatePrinterDetector(
     content::BrowserContext* context) {
-  return PrinterDetector::Create(Profile::FromBrowserContext(context));
+  return PrinterDetector::CreateLegacy(Profile::FromBrowserContext(context));
 }
 
 }  // namespace
