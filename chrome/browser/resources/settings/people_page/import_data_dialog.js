a7718c65878889dd5c19a7ca40e3a608be53f6dd
dbeam <dbeam@chromium.org>
2017-02-28 19:23:15 -0800

    Revert of MD Settings: Stop using prefs to populate import data dialog. (patchset #2 id:20001 of https://codereview.chromium.org/2717013002/ )
    Reason for revert:
    We want to continue respecting policy and master prefs in the importData dialog.
    
    Original issue's description:
    > MD Settings: Stop using prefs to populate import data dialog.
    >
    > BUG=695611
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    >
    > Review-Url: https://codereview.chromium.org/2717013002
    > Cr-Commit-Position: refs/heads/master@{#453332}
    > Committed: https://chromium.googlesource.com/chromium/src/+/5a915bf6d281b1142802a87491a1d17e811f3a8b
    
    TBR=dpapad@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=695611
    
    Review-Url: https://codereview.chromium.org/2727513002
    Cr-Commit-Position: refs/heads/master@{#453821}

--- chrome/browser/resources/settings/people_page/import_data_dialog.js
+++ chrome/browser/resources/settings/people_page/import_data_dialog.js
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'settings-import-data-dialog',
 
-  behaviors: [I18nBehavior, WebUIListenerBehavior],
+  behaviors: [I18nBehavior, WebUIListenerBehavior, PrefsBehavior],
 
   properties: {
     /** @private {!Array<!settings.BrowserProfile>} */
@@ -42,10 +42,11 @@ Polymer({
       type: Object,
       value: settings.ImportDataStatus,
     },
-
-    /** @private */
-    showBookmarkSuccess_: Boolean,
   },
+
+  observers: [
+    'prefsChanged_(selected_, prefs.*)',
+  ],
 
   /** @private {?settings.ImportDataBrowserProxy} */
   browserProxy_: null,
@@ -69,24 +70,22 @@ Polymer({
         /** @param {settings.ImportDataStatus} importStatus */
         function(importStatus) {
           this.importStatus_ = importStatus;
-          if (this.hasImportStatus_(settings.ImportDataStatus.SUCCEEDED)) {
-            this.showBookmarkSuccess_ =
-                this.$.favorites.checked && this.selected_.favorites;
-          }
-
           if (this.hasImportStatus_(settings.ImportDataStatus.FAILED))
             this.closeDialog_();
         }.bind(this));
   },
 
   /** @private */
-  onCheckboxChange_: function() {
+  prefsChanged_: function() {
     this.noImportDataTypeSelected_ =
-        !(this.selected_.history && this.$.history.checked) &&
-        !(this.selected_.favorites && this.$.favorites.checked) &&
-        !(this.selected_.passwords && this.$.passwords.checked) &&
-        !(this.selected_.search && this.$.search.checked) &&
-        !(this.selected_.autofillFormData && this.$.autofillFormData.checked);
+        !(this.getPref('import_history').value && this.selected_.history) &&
+        !(this.getPref('import_bookmarks').value && this.selected_.favorites) &&
+        !(this.getPref('import_saved_passwords').value &&
+            this.selected_.passwords) &&
+        !(this.getPref('import_search_engine').value &&
+            this.selected_.search) &&
+        !(this.getPref('import_autofill_form_data').value &&
+            this.selected_.autofillFormData);
   },
 
   /**
@@ -117,7 +116,6 @@ Polymer({
   /** @private */
   onBrowserProfileSelectionChange_: function() {
     this.selected_ = this.browserProfiles_[this.$.browserSelect.selectedIndex];
-    this.onCheckboxChange_();
   },
 
   /** @private */
