c4d9a41a5ab43ee0429600259d9fb986aeda5ad2
iannucci <iannucci@chromium.org>
2017-02-28 19:53:19 -0800

    Allow $TMPDIR to set temporary directory on OS X.
    This allows the continuous integration builds to be more hermetic by
    supplying a managed value for $TMPDIR and having chromium + tests respect
    it.
    
    NSTemporaryDirectory takes its value directly from a non-overridable
    confstr(_CS_DARWIN_USER_TEMP_DIR) call internally. This CL brings OS X
    more in line with Windows and Linux which both respect an environment
    variable ($TEMP and $TMPDIR respectively).
    
    BUG=696803
    
    Review-Url: https://codereview.chromium.org/2713293002
    Cr-Commit-Position: refs/heads/master@{#453835}

--- remoting/host/mac/me2me_preference_pane.mm
+++ remoting/host/mac/me2me_preference_pane.mm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <fstream>
 #include <memory>
 
+#include "base/files/file_util.h"
 #include "base/mac/authorization_util.h"
 #include "base/mac/launchd.h"
 #include "base/mac/mac_logging.h"
@@ -34,12 +35,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 bool GetTemporaryConfigFilePath(std::string* path) {
-  NSString* filename = NSTemporaryDirectory();
-  if (filename == nil)
-    return false;
+  base::FilePath ret;
 
-  *path = [[NSString stringWithFormat:@"%@/%s",
-            filename, remoting::kHostConfigFileName] UTF8String];
+  if (!base::GetTempPath(&ret)) {
+    return false;
+  }
+
+  ret = ret.Append(remoting::kHostConfigFileName);
+
+  // This should be safe on OS X because utf8 is always supported.
+  *path = ret.AsUTF8Unsafe();
   return true;
 }
 
