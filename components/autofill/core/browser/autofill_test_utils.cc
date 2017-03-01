2d8f079824908340e77b67ce5b11e3a8c44e8cb0
sebsg <sebsg@chromium.org>
2017-03-01 13:19:06 -0800

    Revert of [Autofill] Split server card data and metadata updates. (patchset #3 id:60001 of https://codereview.chromium.org/2719723002/ )
    Reason for revert:
    Breaks the Wallet address conversion feature
    
    Original issue's description:
    > This caused a bug where the use_date would be reset when adding a new server card.
    > The metadata should not be updated in this case.
    >
    > BUG=696365
    >
    > Review-Url: https://codereview.chromium.org/2719723002
    > Cr-Commit-Position: refs/heads/master@{#453210}
    > Committed: https://chromium.googlesource.com/chromium/src/+/464db18fe97e6e7f73667db90a761239447d8e97
    
    TBR=mathp@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=696365
    
    Review-Url: https://codereview.chromium.org/2728733002
    Cr-Commit-Position: refs/heads/master@{#454032}

--- components/autofill/core/browser/autofill_test_utils.cc
+++ components/autofill/core/browser/autofill_test_utils.cc
@@ -326,7 +326,6 @@ void SetServerCreditCards(AutofillTable* table,
     std::string type = card.type();
     card.SetNumber(card.LastFourDigits());
     card.SetTypeForMaskedCard(type.c_str());
-    table->UpdateServerCardMetadata(card);
   }
   table->SetServerCreditCards(as_masked_cards);
 
