191a637c75fda46dee7f144d813718df67bfca89
xiaoyinh <xiaoyinh@chromium.org>
2017-03-01 13:11:08 -0800

    Reland "Hosting fingerprint mojo service within the device service" BUG=687393
    Original CL: https://codereview.chromium.org/2664353002/
    This service will get fingerprint information from biod through dbus and provide it to md-settings.
    Since biod APIs are not finalized yet, FingerprintChromeOS is created as a stub for now.
    
    Review-Url: https://codereview.chromium.org/2717993004
    Cr-Commit-Position: refs/heads/master@{#454027}

--- services/device/device_service.cc
+++ services/device/device_service.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/memory/weak_ptr.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "services/device/fingerprint/fingerprint.h"
 #include "services/device/power_monitor/power_monitor_message_broadcaster.h"
 #include "services/device/time_zone_monitor/time_zone_monitor.h"
 #include "services/service_manager/public/cpp/connection.h"
@@ -42,9 +43,15 @@ void DeviceService::OnStart() {}
 
 bool DeviceService::OnConnect(const service_manager::ServiceInfo& remote_info,
                               service_manager::InterfaceRegistry* registry) {
+  registry->AddInterface<mojom::Fingerprint>(this);
   registry->AddInterface<mojom::PowerMonitor>(this);
   registry->AddInterface<mojom::TimeZoneMonitor>(this);
   return true;
+}
+
+void DeviceService::Create(const service_manager::Identity& remote_identity,
+                           mojom::FingerprintRequest request) {
+  Fingerprint::Create(std::move(request));
 }
 
 void DeviceService::Create(const service_manager::Identity& remote_identity,
