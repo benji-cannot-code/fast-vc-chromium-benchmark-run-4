0ba536e3c425d98b4c62c4018bc184bf35f84bf3
brianosman <brianosman@google.com>
2017-03-01 04:39:51 -0800

    Remove unnecessary includes
    These headers are going to be made private in Skia
    
    BUG=
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2718333003
    Cr-Commit-Position: refs/heads/master@{#453920}

--- cc/resources/resource_provider.cc
+++ cc/resources/resource_provider.cc
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkSurface.h"
 #include "third_party/skia/include/gpu/GrContext.h"
-#include "third_party/skia/include/gpu/GrTextureProvider.h"
 #include "third_party/skia/include/gpu/gl/GrGLTypes.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/vector2d.h"
