88bcf91a13fec32a9a73956ac097dba20ff786d6
bokan <bokan@chromium.org>
2017-03-01 03:36:02 -0800

    Revert of Make document.rootScroller into an origin trial (patchset #3 id:40001 of https://codereview.chromium.org/2628923002/ )
    Reason for revert:
    Origin trial has been delayed for now
    
    Original issue's description:
    > Make document.rootScroller into an origin trial
    >
    > Intent-to-experiment:
    > https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/COF0YKkjZVQ
    >
    > BUG=680190
    >
    > Review-Url: https://codereview.chromium.org/2628923002
    > Cr-Commit-Position: refs/heads/master@{#443956}
    > Committed: https://chromium.googlesource.com/chromium/src/+/f2b6b4c6b17f90d48d986c3856f4a422ddbdbc2c
    
    TBR=iclelland@chromium.org,rbyers@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=680190
    
    Review-Url: https://codereview.chromium.org/2707133008
    Cr-Commit-Position: refs/heads/master@{#453905}

--- third_party/WebKit/Source/bindings/core/v8/ConditionalFeaturesForCore.cpp
+++ third_party/WebKit/Source/bindings/core/v8/ConditionalFeaturesForCore.cpp
@@ -45,11 +45,6 @@ void installConditionalFeaturesCore(const WrapperTypeInfo* wrapperTypeInfo,
           isolate, world, v8::Local<v8::Object>(), prototypeObject,
           interfaceObject);
     }
-  } else if (wrapperTypeInfo == &V8Document::wrapperTypeInfo) {
-    if (OriginTrials::setRootScrollerEnabled(executionContext)) {
-      V8Document::installRootScroller(isolate, world, v8::Local<v8::Object>(),
-                                      prototypeObject, interfaceObject);
-    }
   }
 }
 
@@ -72,14 +67,6 @@ void installPendingConditionalFeatureCore(const String& feature,
       V8HTMLLinkElement::installLinkServiceWorker(
           isolate, world, v8::Local<v8::Object>(), prototypeObject,
           interfaceObject);
-    }
-    return;
-  }
-  if (feature == "RootScroller") {
-    if (contextData->getExistingConstructorAndPrototypeForType(
-            &V8Document::wrapperTypeInfo, &prototypeObject, &interfaceObject)) {
-      V8Document::installRootScroller(isolate, world, v8::Local<v8::Object>(),
-                                      prototypeObject, interfaceObject);
     }
     return;
   }
