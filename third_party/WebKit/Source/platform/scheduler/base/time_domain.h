3115ff217d45d04c43a8b2b8d406346d8ec7a5b5
skyostil <skyostil@chromium.org>
2017-03-01 05:57:56 -0800

    scheduler: Ensure consistent delayed task ordering between task queues
    When wakeups are scheduled for distinct task queues, they should be
    ordered according to the tasks which caused the wakeups. Previously this
    was only being done by looking at the the scheduled run times fo the
    tasks, but we should also consider the sequence numbers for tasks with
    identical run times.
    
    BUG=696001
    
    Review-Url: https://codereview.chromium.org/2718293003
    Cr-Commit-Position: refs/heads/master@{#453927}

--- third_party/WebKit/Source/platform/scheduler/base/time_domain.h
+++ third_party/WebKit/Source/platform/scheduler/base/time_domain.h
@@ -95,7 +95,7 @@ class BLINK_PLATFORM_EXPORT TimeDomain {
   // TimeDomain reaches |delayed_run_time|.  This supersedes any previously
   // registered wakeup for |queue|.
   void ScheduleDelayedWork(internal::TaskQueueImpl* queue,
-                           base::TimeTicks delayed_run_time,
+                           internal::TaskQueueImpl::DelayedWakeUp wake_up,
                            base::TimeTicks now);
 
   // Cancels any scheduled calls to TaskQueueImpl::WakeUpForDelayedWork for
@@ -138,14 +138,12 @@ class BLINK_PLATFORM_EXPORT TimeDomain {
   }
 
  private:
-  struct DelayedWakeup {
-    base::TimeTicks time;
+  struct ScheduledDelayedWakeUp {
+    internal::TaskQueueImpl::DelayedWakeUp wake_up;
     internal::TaskQueueImpl* queue;
 
-    bool operator<=(const DelayedWakeup& other) const {
-      if (time == other.time)
-        return queue <= other.queue;
-      return time < other.time;
+    bool operator<=(const ScheduledDelayedWakeUp& other) const {
+      return wake_up <= other.wake_up;
     }
 
     void SetHeapHandle(HeapHandle handle) {
@@ -162,7 +160,7 @@ class BLINK_PLATFORM_EXPORT TimeDomain {
     }
   };
 
-  IntrusiveHeap<DelayedWakeup> delayed_wakeup_queue_;
+  IntrusiveHeap<ScheduledDelayedWakeUp> delayed_wakeup_queue_;
 
   Observer* const observer_;  // NOT OWNED.
 
