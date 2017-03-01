983ab7b4872472b759aada15d4f335d2f32c7c34
eugenebut <eugenebut@chromium.org>
2017-03-01 14:53:02 -0800

    Cleaned up code duplication in -[CRWWebController goToItemAtIndex:].
    Moved updateDesktopUserAgentForEntry:fromEntry: out of conditional
    blocks, because the calls order does not matter as long as |toEntry| is
    correct.
    
    Removed unnecessary webWillFinishHistoryNavigationFromEntry: call. For
    same-document navigations Tab gets webState:didFinishNavigation: call
    and for different-document navigations Tab gets
    webState:didStartProvisionalNavigationForURL:. Both calls update the
    toolbar, which is the only thing done in
    webWillFinishHistoryNavigationFromEntry:.
    
    BUG=684098
    
    Review-Url: https://codereview.chromium.org/2728723002
    Cr-Commit-Position: refs/heads/master@{#454079}

--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -2134,23 +2134,17 @@ const NSTimeInterval kSnapshotOverlayTransition = 0.5;
   NSUserDefaults* userDefaults = [NSUserDefaults standardUserDefaults];
   if (![userDefaults boolForKey:@"PendingIndexNavigationDisabled"]) {
     [self clearTransientContentView];
+    [self updateDesktopUserAgentForEntry:toEntry fromEntry:fromEntry];
 
     BOOL sameDocumentNavigation = [sessionController
         isSameDocumentNavigationBetweenItem:fromEntry.navigationItem
                                     andItem:toEntry.navigationItem];
     if (sameDocumentNavigation) {
-      [self.sessionController goToItemAtIndex:index];
-      // TODO(crbug.com/684098): move this call out this block to avoid code
-      // duplication.
-      [self webWillFinishHistoryNavigationFromEntry:fromEntry];
+      [sessionController goToItemAtIndex:index];
       [self updateHTML5HistoryState];
     } else {
       [sessionController discardNonCommittedItems];
       [sessionController setPendingItemIndex:index];
-
-      // TODO(crbug.com/684098): move this call out this block to avoid code
-      // duplication.
-      [self webWillFinishHistoryNavigationFromEntry:fromEntry];
 
       web::NavigationItemImpl* pendingItem =
           sessionController.pendingEntry.navigationItemImpl;
@@ -2160,7 +2154,7 @@ const NSTimeInterval kSnapshotOverlayTransition = 0.5;
       [self loadCurrentURL];
     }
   } else {
-    [self.sessionController goToItemAtIndex:index];
+    [sessionController goToItemAtIndex:index];
     if (fromEntry)
       [self finishHistoryNavigationFromEntry:fromEntry];
   }
