703df3fdfe42caab81b8f1a499655d934accfdb3
palmer <palmer@chromium.org>
2017-02-28 18:55:21 -0800

    Use the Oilpan heap canary only on 64-bit.
    On 64-bit machines, the canary is free, space-wise. But on 32-bit, it doubles
    the size of |HeapObjectHeader|, which has incurred a large run-time memory size
    increase.
    
    Turn off the canary for now, on 32-bit. Ultimately we may solve the original bug
    (633030) by moving the metadata out of line instead of guarding it with a
    canary.
    
    BUG=692931,692624,633030
    
    Review-Url: https://codereview.chromium.org/2723743003
    Cr-Commit-Position: refs/heads/master@{#453802}

--- third_party/WebKit/Source/platform/heap/HeapPage.cpp
+++ third_party/WebKit/Source/platform/heap/HeapPage.cpp
@@ -89,7 +89,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-#if DCHECK_IS_ON()
+#if DCHECK_IS_ON() && CPU(64BIT)
 NO_SANITIZE_ADDRESS
 void HeapObjectHeader::zapMagic() {
   ASSERT(checkHeader());
