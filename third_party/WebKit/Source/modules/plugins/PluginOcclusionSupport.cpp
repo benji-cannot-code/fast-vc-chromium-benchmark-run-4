e0fd1132f3d8ebddba96557af7c9f431d96831df
joelhockey <joelhockey@chromium.org>
2017-03-01 03:42:23 -0800

    Rename platform/Widget to platform/FrameViewBase in modules.
    BUG=697351
    
    Review-Url: https://codereview.chromium.org/2728613002
    Cr-Commit-Position: refs/heads/master@{#453907}

--- third_party/WebKit/Source/modules/plugins/PluginOcclusionSupport.cpp
+++ third_party/WebKit/Source/modules/plugins/PluginOcclusionSupport.cpp
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/HTMLFrameOwnerElement.h"
 #include "core/layout/LayoutBox.h"
 #include "core/layout/LayoutObject.h"
-#include "platform/Widget.h"
+#include "platform/FrameViewBase.h"
 #include "wtf/HashSet.h"
 
 // This file provides a utility function to support rendering certain elements
@@ -154,7 +154,7 @@ static const Element* topLayerAncestor(const Element* element) {
 // page. In a nutshell, iframe elements should occlude plugins when
 // they occur higher in the stacking order.
 void getPluginOcclusions(Element* element,
-                         Widget* parentWidget,
+                         FrameViewBase* parentFrameViewBase,
                          const IntRect& frameRect,
                          Vector<IntRect>& occlusions) {
   LayoutObject* pluginNode = element->layoutObject();
@@ -165,10 +165,10 @@ void getPluginOcclusions(Element* element,
   Vector<const LayoutObject*> iframeZstack;
   getObjectStack(pluginNode, &pluginZstack);
 
-  if (!parentWidget->isFrameView())
+  if (!parentFrameViewBase->isFrameView())
     return;
 
-  FrameView* parentFrameView = toFrameView(parentWidget);
+  FrameView* parentFrameView = toFrameView(parentFrameViewBase);
 
   // Occlusions by iframes.
   const FrameView::ChildrenWidgetSet* children = parentFrameView->children();
