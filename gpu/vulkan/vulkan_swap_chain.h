c85bc4de589b3ae908ef28e1fe8f422aba440a2d
joone.hur <joone.hur@intel.com>
2017-03-01 12:14:15 -0800

    Build fix for vulkan_tests.
    This CL fixes the following build breaks:
    
    In file included from ../../gpu/vulkan/vulkan_swap_chain.cc:5:
    ../../gpu/vulkan/vulkan_swap_chain.h:41:5: error: use of undeclared identifier 'DCHECK_LT'
        DCHECK_LT(index, images_.size());
        ^
    ../../gpu/vulkan/vulkan_swap_chain.h:50:5: error: use of undeclared identifier 'DCHECK_LT'
        DCHECK_LT(current_image_, images_.size());
        ^
    
    BUG=none
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2713083002
    Cr-Commit-Position: refs/heads/master@{#454010}

--- gpu/vulkan/vulkan_swap_chain.h
+++ gpu/vulkan/vulkan_swap_chain.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 #include <vulkan/vulkan.h>
 
+#include "base/logging.h"
 #include "gpu/vulkan/vulkan_export.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/swap_result.h"
