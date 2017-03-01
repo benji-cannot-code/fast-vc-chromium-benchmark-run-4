e1eb6fcce5fea770e33f828ac900cfe3dc6f6f7f
tonikitoo <tonikitoo@igalia.com>
2017-03-01 11:01:35 -0800

    Mus Demo: Add a comment line switch for launching multiple windows in external mode.
    Make it possible to launch multiple (external) mus_demo windows from
    the command line:
    
    TEST=$ <out>/mash --ozone-platform={x11|wayland} --service=mus_demo --external-window-count=N
    
    BUG=666958
    
    Note that this will used by mus_demo_unittests too in [1].
    
    [1] https://codereview.chromium.org/2715533005/
    
    Review-Url: https://codereview.chromium.org/2723853004
    Cr-Commit-Position: refs/heads/master@{#453984}

--- services/ui/demo/mus_demo_external.cc
+++ services/ui/demo/mus_demo_external.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/ui/demo/mus_demo_external.h"
 
+#include "base/command_line.h"
+#include "base/strings/string_number_conversions.h"
 #include "services/service_manager/public/cpp/service_context.h"
 #include "services/ui/demo/window_tree_data.h"
 #include "services/ui/public/interfaces/constants.mojom.h"
@@ -37,10 +39,6 @@ class WindowTreeDataExternal : public WindowTreeData {
   DISALLOW_COPY_AND_ASSIGN(WindowTreeDataExternal);
 };
 
-// Number of windows to open.
-// TODO(tonikitoo,fwang): Open multiple windows.
-const size_t kNumberOfWindows = 1;
-
 int GetSquareSizeForWindow(int window_index) {
   return 50 * window_index + 400;
 };
@@ -59,6 +57,17 @@ MusDemoExternal::CreateWindowTreeClient() {
 }
 
 void MusDemoExternal::OnStartImpl() {
+  const base::CommandLine* command_line =
+      base::CommandLine::ForCurrentProcess();
+  if (command_line->HasSwitch("external-window-count")) {
+    if (!base::StringToSizeT(
+            command_line->GetSwitchValueASCII("external-window-count"),
+            &number_of_windows_)) {
+      LOG(FATAL) << "Invalid value for \'external-window-count\'";
+      return;
+    }
+  }
+
   // TODO(tonikitoo,fwang): Extend the WindowTreeClient API to allow connection
   // to the window tree host factory using window_tree_client().
   context()->connector()->BindInterface(ui::mojom::kServiceName,
@@ -69,10 +78,11 @@ void MusDemoExternal::OnStartImpl() {
   // aura::GetDeviceScaleFactorFromDisplay().
   AddPrimaryDisplay(display::Display(0));
 
-  // The number of windows to open is specified by kNumberOfWindows. The windows
-  // are opened sequentially (the first one here and the others after each call
-  // to OnEmbed) to ensure that the WindowTreeHostMus passed to OnEmbed
-  // corresponds to the WindowTreeDataExternal::host_ created in OpenNewWindow.
+  // The number of windows to open is specified by number_of_windows_. The
+  // windows are opened sequentially (the first one here and the others after
+  // each call to OnEmbed) to ensure that the WindowTreeHostMus passed to
+  // OnEmbed corresponds to the WindowTreeDataExternal::host_ created in
+  // OpenNewWindow.
   OpenNewWindow();
 }
 
@@ -93,7 +103,7 @@ void MusDemoExternal::OnEmbed(
   initialized_windows_count_++;
 
   // Open the next window until the requested number of windows is reached.
-  if (initialized_windows_count_ < kNumberOfWindows)
+  if (initialized_windows_count_ < number_of_windows_)
     OpenNewWindow();
 }
 
