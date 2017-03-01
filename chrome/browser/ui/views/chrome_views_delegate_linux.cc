20343eedcc993be05cc1d6d616cfdeccbf29ddce
sky <sky@chromium.org>
2017-03-01 10:47:23 -0800

    Further cleanup of ChromeViewsDelegate
    Specifically moves platform specific functionality into platform
    specific files.
    
    BUG=none
    TEST=none
    R=erg@chromium.org
    
    Review-Url: https://codereview.chromium.org/2728583002
    Cr-Commit-Position: refs/heads/master@{#453982}

--- chrome/browser/ui/views/chrome_views_delegate_linux.cc
+++ chrome/browser/ui/views/chrome_views_delegate_linux.cc
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/chrome_views_delegate.h"
 
 #include "chrome/browser/ui/views/native_widget_factory.h"
+#include "chrome/grit/chrome_unscaled_resources.h"
+#include "ui/base/resource/resource_bundle.h"
+#include "ui/views/linux_ui/linux_ui.h"
 
 views::NativeWidget* ChromeViewsDelegate::CreateNativeWidget(
     views::Widget::InitParams* params,
@@ -16,4 +19,16 @@ views::NativeWidget* ChromeViewsDelegate::CreateNativeWidget(
           ? NativeWidgetType::NATIVE_WIDGET_AURA
           : NativeWidgetType::DESKTOP_NATIVE_WIDGET_AURA;
   return ::CreateNativeWidget(native_widget_type, params, delegate);
+}
+
+gfx::ImageSkia* ChromeViewsDelegate::GetDefaultWindowIcon() const {
+  ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
+  return rb.GetImageSkiaNamed(IDR_PRODUCT_LOGO_64);
+}
+
+bool ChromeViewsDelegate::WindowManagerProvidesTitleBar(bool maximized) {
+  // On Ubuntu Unity, the system always provides a title bar for maximized
+  // windows.
+  views::LinuxUI* ui = views::LinuxUI::instance();
+  return maximized && ui && ui->UnityIsRunning();
 }
