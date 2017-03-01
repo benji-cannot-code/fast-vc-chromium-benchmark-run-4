79551e620606044b83163ba4b978cfdb70365e7b
noel <noel@chromium.org>
2017-02-28 19:28:44 -0800

    Convert utility process zip creator IPC to mojo
    Convert the OS_CHROMEOS-only zip creator IPC to use mojo rather
    than IPC. Expose the mojo to the browser process using the mojo
    utility process policy file. Update the client to call the mojo
    service using a mojo utility client, remove all dependencies on
    UtilityProcessHostClient. Add a new mojo utility client api:
    
      utility_process_mojo_client_->set_exposed_directory(dir)
    
    to allow mojo utility clients to expose the target directory in
    the utility process sandbox just like UtilityProcessHostClient.
    Minor name changes, no abbrvs.
    
    Now in chrome/utility/chrome_content_utility_client.cc continue
    the saga of deleting the Send and ReleaseProcessIfNeeded helper
    routines: make remaining users call these services directly and
    remove the helpers. Add the mojo ZipFileCreatorImpl class.
    
    Covered by existing tests: ZipFileCreatorTest.SomeFilesZip, per
    https://codereview.chromium.org/2703463005
    
    BUG=597124
    
    Review-Url: https://codereview.chromium.org/2705613003
    Cr-Commit-Position: refs/heads/master@{#453825}

--- content/public/browser/utility_process_mojo_client.h
+++ content/public/browser/utility_process_mojo_client.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/callback.h"
+#include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/strings/string16.h"
@@ -39,8 +40,15 @@ class UtilityProcessMojoClient {
 
   // Sets the error callback. A valid callback must be set before calling
   // Start().
-  void set_error_callback(const base::Closure& on_error_callback) {
-    on_error_callback_ = on_error_callback;
+  void set_error_callback(const base::Closure& error_callback) {
+    error_callback_ = error_callback;
+  }
+
+  // Allows a directory to be opened through the utility process sandbox.
+  void set_exposed_directory(const base::FilePath& directory) {
+    DCHECK(!start_called_);
+    DCHECK(!directory.empty());
+    helper_->set_exposed_directory(directory);
   }
 
   // Disables the sandbox in the utility process.
@@ -50,24 +58,24 @@ class UtilityProcessMojoClient {
   }
 
 #if defined(OS_WIN)
-  // Allow the utility process to run with elevated privileges.
+  // Allows the utility process to run with elevated privileges.
   void set_run_elevated() {
     DCHECK(!start_called_);
     helper_->set_run_elevated();
   }
 #endif  // defined(OS_WIN)
 
-  // Starts the utility process and connect to the remote Mojo service.
+  // Starts the utility process and connects to the remote Mojo service.
   void Start() {
     DCHECK(thread_checker_.CalledOnValidThread());
-    DCHECK(!on_error_callback_.is_null());
+    DCHECK(error_callback_);
     DCHECK(!start_called_);
 
     start_called_ = true;
 
-    mojo::InterfaceRequest<MojoInterface> req(&service_);
-    service_.set_connection_error_handler(on_error_callback_);
-    helper_->Start(MojoInterface::Name_, req.PassMessagePipe());
+    mojo::InterfaceRequest<MojoInterface> request(&service_);
+    service_.set_connection_error_handler(error_callback_);
+    helper_->Start(MojoInterface::Name_, request.PassMessagePipe());
   }
 
   // Returns the Mojo service used to make calls to the utility process.
@@ -103,6 +111,10 @@ class UtilityProcessMojoClient {
                      mojo_interface_name, base::Passed(&interface_pipe)));
     }
 
+    void set_exposed_directory(const base::FilePath& directory) {
+      exposed_directory_ = directory;
+    }
+
     void set_disable_sandbox() { disable_sandbox_ = true; }
 
 #if defined(OS_WIN)
@@ -121,6 +133,9 @@ class UtilityProcessMojoClient {
       utility_host_ = UtilityProcessHost::Create(nullptr, nullptr)->AsWeakPtr();
       utility_host_->SetName(process_name_);
 
+      if (!exposed_directory_.empty())
+        utility_host_->SetExposedDir(exposed_directory_);
+
       if (disable_sandbox_)
         utility_host_->DisableSandbox();
 #if defined(OS_WIN)
@@ -138,6 +153,7 @@ class UtilityProcessMojoClient {
 
     // Properties of the utility process.
     base::string16 process_name_;
+    base::FilePath exposed_directory_;
     bool disable_sandbox_ = false;
 #if defined(OS_WIN)
     bool run_elevated_ = false;
@@ -152,7 +168,7 @@ class UtilityProcessMojoClient {
   std::unique_ptr<Helper> helper_;
 
   // Called when a connection error happens or if the process didn't start.
-  base::Closure on_error_callback_;
+  base::Closure error_callback_;
 
   mojo::InterfacePtr<MojoInterface> service_;
 
