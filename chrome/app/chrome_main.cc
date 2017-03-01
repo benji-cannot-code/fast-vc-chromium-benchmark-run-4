d39fe357859a43b6f2c96b31d700bc21bb414593
dvallet <dvallet@chromium.org>
2017-03-01 14:34:50 -0800

    Rollfowrad of crrev.com/2720863003/: Make --headless flag work on Mac Fix added child dependency in headless_lib This is pending on splitting headless library so it can be property build in windows. BUG=687407
    Review-Url: https://codereview.chromium.org/2723933003
    Cr-Commit-Position: refs/heads/master@{#454074}

--- chrome/app/chrome_main.cc
+++ chrome/app/chrome_main.cc
@@ -23,6 +23,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/service_manager/runner/common/client_util.h"
 #endif
 
+#if defined(OS_MACOSX)
+#include "chrome/app/chrome_main_mac.h"
+#endif
+
 #if defined(OS_WIN)
 #include "base/debug/dump_without_crashing.h"
 #include "base/win/win_util.h"
@@ -94,10 +98,14 @@ int ChromeMain(int argc, const char** argv) {
   ALLOW_UNUSED_LOCAL(command_line);
 #endif
 
-#if defined(OS_LINUX)
-  if (command_line->HasSwitch(switches::kHeadless))
+#if defined(OS_LINUX) || defined(OS_MACOSX)
+  if (command_line->HasSwitch(switches::kHeadless)) {
+#if defined(OS_MACOSX)
+    SetUpBundleOverrides();
+#endif
     return headless::HeadlessShellMain(argc, argv);
-#endif  // defined(OS_LINUX)
+  }
+#endif  // defined(OS_LINUX) || defined(OS_MACOSX)
 
 #if BUILDFLAG(ENABLE_PACKAGE_MASH_SERVICES)
   version_info::Channel channel = chrome::GetChannel();
