334becdd8c3daa7551e172c7cc4853ff04b5ae5f
hunyadym <hunyadym@chromium.org>
2017-03-01 07:55:59 -0800

    Initialize source field of PolicyMap::Entry
    Defaults to lowest priority source (POLICY_SOURCE_ENTERPRISE_DEFAULT).
    
    BUG=697174
    
    Review-Url: https://codereview.chromium.org/2724763002
    Cr-Commit-Position: refs/heads/master@{#453940}

--- components/policy/core/common/policy_map.cc
+++ components/policy/core/common/policy_map.cc
@@ -13,8 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace policy {
 
-PolicyMap::Entry::Entry()
-    : level(POLICY_LEVEL_RECOMMENDED), scope(POLICY_SCOPE_USER) {}
+PolicyMap::Entry::Entry() = default;
 
 PolicyMap::Entry::~Entry() = default;
 
