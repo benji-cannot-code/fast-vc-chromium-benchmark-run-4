8aa3a91afdc951ca290d60a1f03053befa96cd22
altimin <altimin@chromium.org>
2017-02-28 16:54:39 -0800

    [scheduler] Add a CHECK for renderer main thread load metrics.
    Change DCHECK to CHECK in RendererScheduler's
    Report{Foreground,Background}RendererThreadLoad. This should help pin
    down strange anomalies from metrics.
    
    This patch should be Canary-channel only, and expected to be extremely
    rare (less than 1 failure/day on Canary). Please revert without asking
    if something goes wrong.
    
    BUG=693542
    
    Review-Url: https://codereview.chromium.org/2720023003
    Cr-Commit-Position: refs/heads/master@{#453783}

--- third_party/WebKit/Source/platform/scheduler/renderer/renderer_scheduler_impl.cc
+++ third_party/WebKit/Source/platform/scheduler/renderer/renderer_scheduler_impl.cc
@@ -56,7 +56,8 @@ void ReportForegroundRendererTaskLoad(base::TimeTicks time, double load) {
     return;
 
   int load_percentage = static_cast<int>(load * 100);
-  DCHECK_LE(load_percentage, 100);
+  // TODO(altimin): Revert back to DCHECK.
+  CHECK_LE(load_percentage, 100);
 
   UMA_HISTOGRAM_PERCENTAGE("RendererScheduler.ForegroundRendererMainThreadLoad",
                            load_percentage);
@@ -69,7 +70,8 @@ void ReportBackgroundRendererTaskLoad(base::TimeTicks time, double load) {
     return;
 
   int load_percentage = static_cast<int>(load * 100);
-  DCHECK_LE(load_percentage, 100);
+  // TODO(altimin): Revert back to DCHECK.
+  CHECK_LE(load_percentage, 100);
 
   UMA_HISTOGRAM_PERCENTAGE("RendererScheduler.BackgroundRendererMainThreadLoad",
                            load_percentage);
