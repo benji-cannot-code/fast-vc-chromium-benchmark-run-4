cb64be82fd0f64540a31916c63f7d9eb6689f1a0
sdefresne <sdefresne@chromium.org>
2017-03-02 14:09:34 -0800

    Remove unused notification kTabModelUserNavigatedNavigation.
    This notification is sent but no code register to receive it so
    removing it in the hope that no code register to receive all the
    notifications from Tabs (as -addObserver:selector:name:object:
    accepts a nil name and will invoke the selector without filtering).
    
    BUG=320877
    
    Review-Url: https://codereview.chromium.org/2711343002
    Cr-Commit-Position: refs/heads/master@{#454398}

--- ios/chrome/browser/tabs/tab_model.mm
+++ ios/chrome/browser/tabs/tab_model.mm
@@ -58,7 +58,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 NSString* const kTabModelTabWillStartLoadingNotification =
     @"kTabModelTabWillStartLoadingNotification";
-NSString* const kTabModelUserNavigatedNotification = @"kTabModelUserNavigation";
 NSString* const kTabModelTabDidStartLoadingNotification =
     @"kTabModelTabDidStartLoadingNotification";
 NSString* const kTabModelTabDidFinishLoadingNotification =
