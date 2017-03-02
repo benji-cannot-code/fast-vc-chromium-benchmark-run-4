427702897193feb61559229a48e1295ed516b2e9
stevenjb <stevenjb@chromium.org>
2017-03-01 16:44:34 -0800

    MD Settings: Input method: add show-in-shelf toggle
    BUG=694928
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2727693003
    Cr-Commit-Position: refs/heads/master@{#454126}

--- chrome/browser/extensions/api/settings_private/prefs_util.cc
+++ chrome/browser/extensions/api/settings_private/prefs_util.cc
@@ -156,6 +156,8 @@ const PrefsUtil::TypedPrefMap& PrefsUtil::GetWhitelistedKeys() {
       settings_private::PrefType::PREF_TYPE_BOOLEAN;
   (*s_whitelist)[::prefs::kSearchSuggestEnabled] =
       settings_private::PrefType::PREF_TYPE_BOOLEAN;
+
+  // Languages page
   (*s_whitelist)[spellcheck::prefs::kSpellCheckDictionaries] =
       settings_private::PrefType::PREF_TYPE_LIST;
   (*s_whitelist)[spellcheck::prefs::kSpellCheckUseSpellingService] =
@@ -164,6 +166,10 @@ const PrefsUtil::TypedPrefMap& PrefsUtil::GetWhitelistedKeys() {
       settings_private::PrefType::PREF_TYPE_BOOLEAN;
   (*s_whitelist)[translate::TranslatePrefs::kPrefTranslateBlockedLanguages] =
       settings_private::PrefType::PREF_TYPE_LIST;
+#if defined(OS_CHROMEOS)
+  (*s_whitelist)[::prefs::kLanguageImeMenuActivated] =
+      settings_private::PrefType::PREF_TYPE_BOOLEAN;
+#endif
 
   // Search page.
   (*s_whitelist)[::prefs::kDefaultSearchProviderEnabled] =
