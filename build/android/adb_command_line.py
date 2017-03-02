58ea6d17d1b4655a46df5f9aeb4b0781166d0d7b
perezju <perezju@chromium.org>
2017-03-02 09:58:57 -0800

    [build/android] Remove --device-path option from adb_command_line.py
    Option is no longer used by any clients.
    
    BUG=catapult:#3112
    
    Review-Url: https://codereview.chromium.org/2729863002
    Cr-Commit-Position: refs/heads/master@{#454303}

--- build/android/adb_command_line.py
+++ build/android/adb_command_line.py
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import argparse
 import os
-import posixpath
 import sys
 
 import devil_chromium
@@ -31,19 +30,11 @@ Otherwise: Writes command-line file.
                       default=[], help='Target device serial (repeatable).')
   parser.add_argument('--name', required=True,
                       help='Name of file where to store flags on the device.')
-  parser.add_argument('--device-path', help='(deprecated) No longer needed to'
-                      ' supply a device path.')
   parser.add_argument('-e', '--executable', dest='executable', default='chrome',
                       help='(deprecated) No longer used.')
   parser.add_argument('--adb-path', type=os.path.abspath,
                       help='Path to the adb binary.')
   args, remote_args = parser.parse_known_args()
-
-  if args.device_path:
-    args.name = posixpath.basename(args.device_path)
-    print ('warning: --device-path option is deprecated,'
-           ' --name %s is now enough.'
-           % cmd_helper.SingleQuote(args.name))
 
   devil_chromium.Initialize(adb_path=args.adb_path)
 
