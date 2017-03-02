9e9c26aa4a72e2ef1d57e96da7e6131f97f1b47d
eseckler <eseckler@chromium.org>
2017-03-02 14:12:14 -0800

    [cc] Forward BeginFrameAcks through DirectCFS and CFSSupport.
    Also updates unit tests to check that acks are indeed forwarded.
    
    BUG=697086,646774, 696030, 480361
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2727553004
    Cr-Commit-Position: refs/heads/master@{#454400}

--- cc/test/fake_compositor_frame_sink_client.cc
+++ cc/test/fake_compositor_frame_sink_client.cc
@@ -8,6 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc {
 
+void FakeCompositorFrameSinkClient::SetBeginFrameSource(
+    BeginFrameSource* source) {
+  begin_frame_source_ = source;
+}
+
 void FakeCompositorFrameSinkClient::DidReceiveCompositorFrameAck() {
   ack_count_++;
 }
