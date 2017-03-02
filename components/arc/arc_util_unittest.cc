0a7197a1a39e1673fb7991e804095f4fce95a572
elijahtaylor <elijahtaylor@chromium.org>
2017-03-02 12:13:42 -0800

    Enable ARC kiosk mode always when ARC is available
    This removes 'installed-only-kiosk-supported' from arc-availability
    
    BUG=697741
    TEST=./unit_tests --gtest_filter="ChromeArcUtilTest.*"
    TEST=./components_unittests --gtest_filter="ArcUtilTest.*"
    
    Review-Url: https://codereview.chromium.org/2725113002
    Cr-Commit-Position: refs/heads/master@{#454360}

--- components/arc/arc_util_unittest.cc
+++ components/arc/arc_util_unittest.cc
@@ -81,7 +81,9 @@ TEST_F(ArcUtilTest, IsArcAvailable_Installed) {
 
   // Not available, by-default, too.
   EXPECT_FALSE(IsArcAvailable());
-  EXPECT_FALSE(IsArcKioskAvailable());
+
+  // ARC is available in kiosk mode if installed.
+  EXPECT_TRUE(IsArcKioskAvailable());
 
   {
     ScopedArcFeature feature(true);
@@ -91,7 +93,7 @@ TEST_F(ArcUtilTest, IsArcAvailable_Installed) {
   {
     ScopedArcFeature feature(false);
     EXPECT_FALSE(IsArcAvailable());
-    EXPECT_FALSE(IsArcKioskAvailable());
+    EXPECT_TRUE(IsArcKioskAvailable());
   }
 
   // If ARC is installed, IsArcAvailable() should return true when EnableARC
@@ -100,7 +102,9 @@ TEST_F(ArcUtilTest, IsArcAvailable_Installed) {
 
   // Not available, by-default, too.
   EXPECT_FALSE(IsArcAvailable());
-  EXPECT_FALSE(IsArcKioskAvailable());
+
+  // ARC is available in kiosk mode if installed.
+  EXPECT_TRUE(IsArcKioskAvailable());
 
   {
     ScopedArcFeature feature(true);
@@ -110,21 +114,8 @@ TEST_F(ArcUtilTest, IsArcAvailable_Installed) {
   {
     ScopedArcFeature feature(false);
     EXPECT_FALSE(IsArcAvailable());
-    EXPECT_FALSE(IsArcKioskAvailable());
+    EXPECT_TRUE(IsArcKioskAvailable());
   }
-}
-
-TEST_F(ArcUtilTest, IsArcAvailable_OnlyKioskSupported) {
-  // Regardless of FeatureList, IsArcAvailable() should return true.
-  auto* command_line = base::CommandLine::ForCurrentProcess();
-  command_line->InitFromArgv({"", "--enable-arc"});
-  EXPECT_TRUE(IsArcAvailable());
-  EXPECT_TRUE(IsArcKioskAvailable());
-
-  command_line->InitFromArgv(
-      {"", "--arc-availability=installed-only-kiosk-supported"});
-  EXPECT_FALSE(IsArcAvailable());
-  EXPECT_TRUE(IsArcKioskAvailable());
 }
 
 TEST_F(ArcUtilTest, IsArcAvailable_OfficiallySupported) {
