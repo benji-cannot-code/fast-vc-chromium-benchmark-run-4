6b0ea5e4a5fce930fde20fffb5fe9082ed65212d
olivierrobin <olivierrobin@chromium.org>
2017-03-02 02:07:07 -0800

    Test Scheme of page before adding a ReadingListActivity.
    If user has a physical keyboard, it is possible to show the share menu
    on non http[s] pages, which are not supported in the Chrome iOS Reading List.
    This CL checks the scheme of the page before adding the "Read Later" entry.
    
    Review-Url: https://codereview.chromium.org/2722293003
    Cr-Commit-Position: refs/heads/master@{#454223}

--- ios/chrome/browser/ui/activity_services/activity_service_controller.mm
+++ ios/chrome/browser/ui/activity_services/activity_service_controller.mm
@@ -224,7 +224,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [printActivity setResponder:controller];
     [applicationActivities addObject:printActivity];
   }
-  if (reading_list::switches::IsReadingListEnabled()) {
+  if (reading_list::switches::IsReadingListEnabled() &&
+      data.url.SchemeIsHTTPOrHTTPS()) {
     ReadingListActivity* readingListActivity =
         [[ReadingListActivity alloc] initWithURL:data.url
                                            title:data.title
