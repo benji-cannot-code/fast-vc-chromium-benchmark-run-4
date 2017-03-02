c941b4ed593a6c5396627ea622bb7eb493607fb3
vasilii <vasilii@chromium.org>
2017-03-02 09:24:34 -0800

    Stop password migration from the Keychain on Mac.
    Chrome is changing the certificate soon because the current one expires. Thus, the passwords created by Chrome in the Keychain become inaccessible. There is no point in trying the migration anymore. We just clean the database (it has no password values, only metadata) and start from scratch.
    
    BUG=595468
    
    Review-Url: https://codereview.chromium.org/2729853005
    Cr-Commit-Position: refs/heads/master@{#454293}

--- chrome/browser/password_manager/password_store_proxy_mac_unittest.cc
+++ chrome/browser/password_manager/password_store_proxy_mac_unittest.cc
@@ -212,8 +212,7 @@ MigrationStatus PasswordStoreProxyMacTest::GetTargetStatus(
   if (GetParam() == MigrationStatus::NOT_STARTED ||
       GetParam() == MigrationStatus::FAILED_ONCE ||
       GetParam() == MigrationStatus::FAILED_TWICE) {
-    return keychain_locked ? MigrationStatus::MIGRATED_PARTIALLY
-                           : MigrationStatus::MIGRATED;
+    return MigrationStatus::MIGRATION_STOPPED;
   }
   if (GetParam() == MigrationStatus::MIGRATED)
     return MigrationStatus::MIGRATED_DELETED;
@@ -433,7 +432,8 @@ INSTANTIATE_TEST_CASE_P(,
                                         MigrationStatus::FAILED_ONCE,
                                         MigrationStatus::FAILED_TWICE,
                                         MigrationStatus::MIGRATED_DELETED,
-                                        MigrationStatus::MIGRATED_PARTIALLY));
+                                        MigrationStatus::MIGRATED_PARTIALLY,
+                                        MigrationStatus::MIGRATION_STOPPED));
 
 // Test the migration process.
 class PasswordStoreProxyMacMigrationTest : public PasswordStoreProxyMacTest {
@@ -487,7 +487,7 @@ void PasswordStoreProxyMacMigrationTest::TestMigration(bool lock_keychain) {
   MockPasswordStoreConsumer mock_consumer;
   store()->GetLogins(PasswordStore::FormDigest(form), &mock_consumer);
   mock_consumer.WaitForResult();
-  if (before_migration && lock_keychain)
+  if (before_migration)
     EXPECT_THAT(mock_consumer.forms(), IsEmpty());
   else
     EXPECT_THAT(mock_consumer.forms(), ElementsAre(Pointee(form)));
@@ -526,6 +526,7 @@ INSTANTIATE_TEST_CASE_P(,
                                         MigrationStatus::FAILED_ONCE,
                                         MigrationStatus::FAILED_TWICE,
                                         MigrationStatus::MIGRATED_DELETED,
-                                        MigrationStatus::MIGRATED_PARTIALLY));
+                                        MigrationStatus::MIGRATED_PARTIALLY,
+                                        MigrationStatus::MIGRATION_STOPPED));
 
 }  // namespace
