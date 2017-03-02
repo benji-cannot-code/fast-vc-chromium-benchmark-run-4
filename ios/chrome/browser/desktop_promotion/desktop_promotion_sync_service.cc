e2821e4375fd35615a3730373ef7039eaf4ab135
mrefaat <mrefaat@chromium.org>
2017-03-01 23:41:46 -0800

    Log Desktop to iOS promotion promo_variation if set in prefs to UMA
    Check the promo_variation preference set by desktop and if it exists
    log its value to UMA.
    
    BUG=681885
    
    Review-Url: https://codereview.chromium.org/2726953002
    Cr-Commit-Position: refs/heads/master@{#454206}

--- ios/chrome/browser/desktop_promotion/desktop_promotion_sync_service.cc
+++ ios/chrome/browser/desktop_promotion/desktop_promotion_sync_service.cc
@@ -35,4 +35,7 @@ void DesktopPromotionSyncService::RegisterDesktopPromotionUserPrefs(
   registry->RegisterBooleanPref(
       prefs::kDesktopIOSPromotionDone, false,
       user_prefs::PrefRegistrySyncable::SYNCABLE_PRIORITY_PREF);
+  registry->RegisterIntegerPref(
+      prefs::kDesktopIOSPromotionVariationId, 0,
+      user_prefs::PrefRegistrySyncable::SYNCABLE_PRIORITY_PREF);
 }
