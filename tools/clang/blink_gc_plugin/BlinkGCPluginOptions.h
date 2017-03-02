5e57929a782519edfc721b2f312c45ddae73b603
sigbjornf <sigbjornf@opera.com>
2017-03-02 10:10:56 -0800

    blink_gc_plugin: always enable warn_stack_allocated_trace_method check.
    R=haraken,thakis
    BUG=689864
    
    Review-Url: https://codereview.chromium.org/2730673003
    Cr-Commit-Position: refs/heads/master@{#454307}

--- tools/clang/blink_gc_plugin/BlinkGCPluginOptions.h
+++ tools/clang/blink_gc_plugin/BlinkGCPluginOptions.h
@@ -13,9 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 struct BlinkGCPluginOptions {
   bool dump_graph = false;
 
-  // If |true|, emit warning if stack allocated class types provide
-  // a trace method.
-  bool warn_stack_allocated_trace_method = false;
   // If |true|, emit warning for class types which derive from from
   // GarbageCollectedFinalized<> when just GarbageCollected<> will do.
   bool warn_unneeded_finalizer = false;
