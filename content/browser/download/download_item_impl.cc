8a30192c9baadb9786d4ad72c2d71cd470f7ead8
qinmin <qinmin@chromium.org>
2017-03-01 22:21:31 -0800

    Add a utility function to calculate the next slice to download
    For parallel downloading, the received slices may not be next to each other.
    This CL adds a helper function to calculate the next slice to download.
    This new slice will fill up the first hole in the received slice vector.
    
    BUG=644352
    
    Review-Url: https://codereview.chromium.org/2728673003
    Cr-Commit-Position: refs/heads/master@{#454194}

--- content/browser/download/download_item_impl.cc
+++ content/browser/download/download_item_impl.cc
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/download/download_net_log_parameters.h"
 #include "content/browser/download/download_request_handle.h"
 #include "content/browser/download/download_stats.h"
+#include "content/browser/download/parallel_download_utils.h"
 #include "content/browser/renderer_host/render_view_host_impl.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/browser/browser_context.h"
@@ -1954,7 +1955,13 @@ void DownloadItemImpl::ResumeInterruptedDownload(
       new DownloadUrlParameters(GetURL(),
                                 storage_partition->GetURLRequestContext()));
   download_params->set_file_path(GetFullPath());
-  download_params->set_offset(GetReceivedBytes());
+  if (received_slices_.size() > 0) {
+    ReceivedSlice next_slice = FindNextSliceToDownload(received_slices_);
+    download_params->set_offset(next_slice.offset);
+    download_params->set_length(next_slice.received_bytes);
+  } else {
+    download_params->set_offset(GetReceivedBytes());
+  }
   download_params->set_last_modified(GetLastModifiedTime());
   download_params->set_etag(GetETag());
   download_params->set_hash_of_partial_file(hash_);
