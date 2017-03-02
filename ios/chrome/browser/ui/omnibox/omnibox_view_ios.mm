4f6536dee2f78bee9bf80819c1b50dc33a9b15d3
lgarron <lgarron@chromium.org>
2017-03-02 14:10:53 -0800

    Update the Bling omnibox to be aware of HTTP_SHOW_WARNING and use ShouldAlwaysShowIcon().
    BUG=647559, 673530
    
    Review-Url: https://codereview.chromium.org/2692173002
    Cr-Commit-Position: refs/heads/master@{#454399}

--- ios/chrome/browser/ui/omnibox/omnibox_view_ios.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_view_ios.mm
@@ -657,6 +657,7 @@ NSAttributedString* OmniboxViewIOS::ApplyTextAttributes(
       UIColor* color = nil;
       switch (controller_->GetToolbarModel()->GetSecurityLevel(false)) {
         case security_state::NONE:
+        case security_state::HTTP_SHOW_WARNING:
           break;
         case security_state::SECURITY_WARNING:
           // Don't color strikethrough schemes. See https://crbug.com/635004#c6
@@ -679,7 +680,6 @@ NSAttributedString* OmniboxViewIOS::ApplyTextAttributes(
                      value:[NSNumber numberWithInteger:NSUnderlineStyleSingle]
                      range:ComponentToNSRange(scheme)];
           break;
-        case security_state::HTTP_SHOW_WARNING:
         case security_state::SECURE_WITH_POLICY_INSTALLED_CERT:
           NOTREACHED();
       }
