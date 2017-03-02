e604d303a7258dc00c6a788307bafac931192531
dtrainor <dtrainor@chromium.org>
2017-03-02 11:32:00 -0800

    Log Android LibraryLoader failures
    We recently had a build break that removed a library dependency from Chrome, but was still required by Android.  Our LibraryLoader attempted to load it and failed, but didn't actually output the failing library.  This made it harder to track down the root cause than it should have been.
    
    Make a Log.e() call for LibraryLoader failures that prints out the
    library that we can't load.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2728513003
    Cr-Commit-Position: refs/heads/master@{#454339}

--- base/android/java/src/org/chromium/base/library_loader/LibraryLoader.java
+++ base/android/java/src/org/chromium/base/library_loader/LibraryLoader.java
@@ -299,8 +299,13 @@ public class LibraryLoader {
                             Log.i(TAG, "Loading " + library);
                         }
 
-                        // Load the library using this Linker. May throw UnsatisfiedLinkError.
-                        loadLibrary(linker, zipFilePath, libFilePath);
+                        try {
+                            // Load the library using this Linker. May throw UnsatisfiedLinkError.
+                            loadLibrary(linker, zipFilePath, libFilePath);
+                        } catch (UnsatisfiedLinkError e) {
+                            Log.e(TAG, "Unable to load library: " + library);
+                            throw(e);
+                        }
                     }
 
                     linker.finishLibraryLoad();
@@ -310,7 +315,12 @@ public class LibraryLoader {
                     }
                     // Load libraries using the system linker.
                     for (String library : NativeLibraries.LIBRARIES) {
-                        System.loadLibrary(library);
+                        try {
+                            System.loadLibrary(library);
+                        } catch (UnsatisfiedLinkError e) {
+                            Log.e(TAG, "Unable to load library: " + library);
+                            throw(e);
+                        }
                     }
                 }
 
