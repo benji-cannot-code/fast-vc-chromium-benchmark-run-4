6443e2cbce2fed56c5ffca28e3176b0935ec4015
mfomitchev <mfomitchev@chromium.org>
2017-03-02 13:25:22 -0800

    Adding LatencyInfo to mojom::Event.
    Also updating the struct traits.
    
    BUG=686865
    
    Review-Url: https://codereview.chromium.org/2728883002
    Cr-Commit-Position: refs/heads/master@{#454380}

--- ui/events/mojo/event_struct_traits.cc
+++ ui/events/mojo/event_struct_traits.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event_utils.h"
 #include "ui/events/keycodes/dom/keycode_converter.h"
 #include "ui/events/mojo/event_constants.mojom.h"
+#include "ui/events/mojo/latency_info_struct_traits.h"
 
 namespace mojo {
 namespace {
@@ -138,6 +139,12 @@ int64_t StructTraits<ui::mojom::EventDataView, EventUniquePtr>::time_stamp(
   return event->time_stamp().ToInternalValue();
 }
 
+const ui::LatencyInfo&
+StructTraits<ui::mojom::EventDataView, EventUniquePtr>::latency(
+    const EventUniquePtr& event) {
+  return *event->latency();
+}
+
 ui::mojom::KeyDataPtr
 StructTraits<ui::mojom::EventDataView, EventUniquePtr>::key_data(
     const EventUniquePtr& event) {
@@ -232,6 +239,8 @@ StructTraits<ui::mojom::EventDataView, EventUniquePtr>::pointer_data(
 bool StructTraits<ui::mojom::EventDataView, EventUniquePtr>::Read(
     ui::mojom::EventDataView event,
     EventUniquePtr* out) {
+  DCHECK(!out->get());
+
   switch (event.action()) {
     case ui::mojom::EventType::KEY_PRESSED:
     case ui::mojom::EventType::KEY_RELEASED: {
@@ -243,15 +252,15 @@ bool StructTraits<ui::mojom::EventDataView, EventUniquePtr>::Read(
         out->reset(new ui::KeyEvent(
             static_cast<base::char16>(key_data->character),
             static_cast<ui::KeyboardCode>(key_data->key_code), event.flags()));
-        return true;
-      }
-      out->reset(new ui::KeyEvent(
-          event.action() == ui::mojom::EventType::KEY_PRESSED
-              ? ui::ET_KEY_PRESSED
-              : ui::ET_KEY_RELEASED,
 
-          static_cast<ui::KeyboardCode>(key_data->key_code), event.flags()));
-      return true;
+      } else {
+        out->reset(new ui::KeyEvent(
+            event.action() == ui::mojom::EventType::KEY_PRESSED
+                ? ui::ET_KEY_PRESSED
+                : ui::ET_KEY_RELEASED,
+            static_cast<ui::KeyboardCode>(key_data->key_code), event.flags()));
+      }
+      break;
     }
     case ui::mojom::EventType::POINTER_DOWN:
     case ui::mojom::EventType::POINTER_UP:
@@ -284,7 +293,7 @@ bool StructTraits<ui::mojom::EventDataView, EventUniquePtr>::Read(
                   : ui::PointerDetails(
                         ui::EventPointerType::POINTER_TYPE_MOUSE),
               ui::EventTimeForNow()));
-          return true;
+          break;
         }
         case ui::mojom::PointerKind::TOUCH: {
           out->reset(new ui::PointerEvent(
@@ -298,18 +307,22 @@ bool StructTraits<ui::mojom::EventDataView, EventUniquePtr>::Read(
                                  pointer_data->brush_data->tilt_x,
                                  pointer_data->brush_data->tilt_y),
               ui::EventTimeForNow()));
-          return true;
+          break;
         }
         case ui::mojom::PointerKind::PEN:
           NOTIMPLEMENTED();
           return false;
       }
+      break;
     }
     case ui::mojom::EventType::UNKNOWN:
       return false;
   }
 
-  return false;
+  if (!out->get())
+    return false;
+
+  return event.ReadLatency((*out)->latency());
 }
 
 }  // namespace mojo
--- ui/events/mojo/event_struct_traits.h
+++ ui/events/mojo/event_struct_traits.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 class Event;
+class LatencyInfo;
 }
 
 namespace mojo {
@@ -21,6 +22,7 @@ struct StructTraits<ui::mojom::EventDataView, EventUniquePtr> {
   static ui::mojom::EventType action(const EventUniquePtr& event);
   static int32_t flags(const EventUniquePtr& event);
   static int64_t time_stamp(const EventUniquePtr& event);
+  static const ui::LatencyInfo& latency(const EventUniquePtr& event);
   static ui::mojom::KeyDataPtr key_data(const EventUniquePtr& event);
   static ui::mojom::PointerDataPtr pointer_data(const EventUniquePtr& event);
   static bool Read(ui::mojom::EventDataView r, EventUniquePtr* out);
