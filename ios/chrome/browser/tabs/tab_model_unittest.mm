420c7347f5a3312d70032b0801f49c3507b68a44
sdefresne <sdefresne@chromium.org>
2017-03-01 20:38:18 -0800

    Allow creation of CRWSessionStorage outside of serialisation API.
    For recreating fake session during unit tests, allow creation of
    CRWSessionStorage instance outside of the serialization code (this
    required supporting null in AddSerializableUserData method).
    
    Fix typos in serializable_user_data_manager_impl.mm, convert the
    code to use base::MakeUnique<> and to pass std::unique_ptr<> to
    base::SupportsUserData::SetUserData method.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2726713002
    Cr-Commit-Position: refs/heads/master@{#454180}

--- ios/chrome/browser/tabs/tab_model_unittest.mm
+++ ios/chrome/browser/tabs/tab_model_unittest.mm
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/test/ios_chrome_scoped_testing_chrome_browser_state_manager.h"
 #import "ios/web/navigation/crw_session_controller.h"
 #import "ios/web/navigation/navigation_manager_impl.h"
+#import "ios/web/public/crw_session_storage.h"
 #import "ios/web/public/navigation_manager.h"
 #include "ios/web/public/referrer.h"
 #import "ios/web/public/serializable_user_data_manager.h"
@@ -152,7 +153,7 @@ class TabModelTest : public PlatformTest {
     SessionWindowIOS* window = [[SessionWindowIOS alloc] init];
     for (int i = 0; i < entries; i++) {
       CRWSessionStorage* session_storage =
-          CreateWebState()->BuildSessionStorage();
+          [[[CRWSessionStorage alloc] init] autorelease];
       [window addSerializedSessionStorage:session_storage];
     }
     if (entries)
