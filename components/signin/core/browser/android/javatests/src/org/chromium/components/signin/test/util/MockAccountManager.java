b27a5d447ef455c8cfa86cab1aef328e72c7ea84
bauerb <bauerb@chromium.org>
2017-03-03 11:14:02 -0800

    Revert of Immediately handle user-recoverable auth errors for child accounts. (patchset #5 id:80001 of https://codereview.chromium.org/2724023002/ )
    Reason for revert:
    Causes an UnsatisfiedLinkError: https://crbug.com/697979
    
    Original issue's description:
    > Immediately handle user-recoverable auth errors for child accounts.
    >
    > This will open a reauth activity e.g. when the password has changed. For child
    > accounts, that is preferable to letting the user use Chrome unauthenticated.
    >
    > BUG=697412
    >
    > Review-Url: https://codereview.chromium.org/2724023002
    > Cr-Commit-Position: refs/heads/master@{#453981}
    > Committed: https://chromium.googlesource.com/chromium/src/+/94af3b0124e78f1f5f00a515e1f569a194c6b1dc
    
    TBR=gogerald@google.com,msarda@chromium.org,bsazonov@chromium.org,rogerta@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=697412
    
    Review-Url: https://codereview.chromium.org/2729203002
    Cr-Commit-Position: refs/heads/master@{#454642}

--- components/signin/core/browser/android/javatests/src/org/chromium/components/signin/test/util/MockAccountManager.java
+++ components/signin/core/browser/android/javatests/src/org/chromium/components/signin/test/util/MockAccountManager.java
@@ -160,12 +160,6 @@ public class MockAccountManager implements AccountManagerDelegate {
     }
 
     @Override
-    public String getAuthTokenAndFixUserRecoverableErrorIfNeeded(
-            Account account, String authTokenScope) {
-        return getAuthToken(account, authTokenScope);
-    }
-
-    @Override
     public void invalidateAuthToken(String authToken) {
         if (authToken == null) {
             throw new IllegalArgumentException("AuthToken can not be null");
