a5f1f51c90b9537733cfb90f9f2c704779a9063f
bratell <bratell@opera.com>
2017-03-03 09:16:27 -0800

    Remove some unnecessary 'using namespace'
    The construct "using namespace" is not allowed by the Chromium
    coding style and since these are not even necessary I'm just
    removing them.
    
    Review-Url: https://codereview.chromium.org/2729093003
    Cr-Commit-Position: refs/heads/master@{#454605}

--- third_party/WebKit/Source/core/dom/SpaceSplitString.cpp
+++ third_party/WebKit/Source/core/dom/SpaceSplitString.cpp
@@ -26,8 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/HashMap.h"
 #include "wtf/text/AtomicStringHash.h"
 
-using namespace WTF;
-
 namespace blink {
 
 template <typename CharacterType>
