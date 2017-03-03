05df83de51e132cd4778ec2385071dd2a51d999e
emaxx <emaxx@chromium.org>
2017-03-03 10:24:36 -0800

    Fix crash during sync load of the signin profile
    This fixes hitting a CHECK during restore-after-crash of the
    signin profile in case of enrolled device:
    [FATAL:device_cloud_policy_manager_chromeos.cc(252)] Check
    failed: signin_profile_forwarding_schema_registry_.
    
    The crash was caused by an attempt to start connection for
    the device cloud policy manager _before_ the schema
    registry of the sign-in profile is passed to it.
    
    This has regressed recently due to the change in commit
    afc9d60e96d31a11f756d5c56c2be54a28130960, which, in
    particular, made the DeviceSettingsService load much
    earlier than previously in case when restore-after-crash
    happens. The DeviceSettingsService loading triggers
    DeviceCloudPolicyManagerChromeOS::StartConnection(),
    which hits a CHECK because this all happens before the
    schema registry gets a chance to be passed by
    ProfilePolicyConnectorFactory::CreateForBrowserContextInternal().
    
    The fix is to pass the schema registry from a different
    location - directly from
    SchemaRegistryServiceFactory::CreateForContextInternal() that
    constructs the schema registry. This is guaranteed by the
    current code to be run before the DeviceSettingsService load
    happens.
    
    BUG=698136
    TEST=manual: induce a crash on signin screen on an enrolled
        device and verify that automatic restart works
    
    Review-Url: https://codereview.chromium.org/2729093002
    Cr-Commit-Position: refs/heads/master@{#454627}

--- chrome/browser/policy/profile_policy_connector_factory.cc
+++ chrome/browser/policy/profile_policy_connector_factory.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/singleton.h"
 #include "build/build_config.h"
-#include "chrome/browser/browser_process.h"
 #include "chrome/browser/policy/profile_policy_connector.h"
 #include "chrome/browser/policy/schema_registry_service.h"
 #include "chrome/browser/policy/schema_registry_service_factory.h"
@@ -22,8 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/chromeos/policy/active_directory_policy_manager.h"
-#include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
-#include "chrome/browser/chromeos/policy/device_cloud_policy_manager_chromeos.h"
 #include "chrome/browser/chromeos/policy/user_cloud_policy_manager_chromeos.h"
 #include "chrome/browser/chromeos/policy/user_policy_manager_factory_chromeos.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
@@ -111,17 +108,7 @@ ProfilePolicyConnectorFactory::CreateForBrowserContextInternal(
 
 #if defined(OS_CHROMEOS)
   Profile* const profile = Profile::FromBrowserContext(context);
-  if (chromeos::ProfileHelper::IsSigninProfile(profile)) {
-    policy::DeviceCloudPolicyManagerChromeOS* device_cloud_policy_manager =
-        g_browser_process->platform_part()
-            ->browser_policy_connector_chromeos()
-            ->GetDeviceCloudPolicyManager();
-    // TODO(tnagel): Do we need to do something for Active Directory management?
-    if (device_cloud_policy_manager) {
-      device_cloud_policy_manager->SetSigninProfileSchemaRegistry(
-          schema_registry);
-    }
-  } else {
+  if (!chromeos::ProfileHelper::IsSigninProfile(profile)) {
     user = chromeos::ProfileHelper::Get()->GetUserByProfile(profile);
     CHECK(user);
   }
