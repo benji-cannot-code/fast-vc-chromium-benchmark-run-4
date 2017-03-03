f068f94ef367b148f021b9056bf7747d4a76061f
chaopeng <chaopeng@chromium.org>
2017-03-03 08:47:42 -0800

    Correct method naming in ScrollbarAnimationController
    In this patch, we rename mouse_is_over_scrollbar, mouse_is_near_scrollbar and
    mouse_is_near_any_scrollbar to CamelCase since they are not inline method in
    header file any more.
    
    BUG=
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2728673002
    Cr-Commit-Position: refs/heads/master@{#454597}

--- cc/input/scrollbar_animation_controller.cc
+++ cc/input/scrollbar_animation_controller.cc
@@ -189,7 +189,7 @@ void ScrollbarAnimationController::DidScrollUpdate(bool on_resize) {
   if (!currently_scrolling_) {
     // We don't fade out scrollbar if they need thinning animation and mouse is
     // near.
-    if (!need_thinning_animation_ || !mouse_is_near_any_scrollbar())
+    if (!need_thinning_animation_ || !MouseIsNearAnyScrollbar())
       PostDelayedFadeOut(on_resize);
   } else {
     scroll_gesture_has_scrolled_ = true;
@@ -211,7 +211,7 @@ void ScrollbarAnimationController::DidScrollEnd() {
 
   // We don't fade out scrollbar if they need thinning animation and mouse is
   // near.
-  if (need_thinning_animation_ && mouse_is_near_any_scrollbar())
+  if (need_thinning_animation_ && MouseIsNearAnyScrollbar())
     return;
 
   if (has_scrolled)
@@ -233,7 +233,7 @@ void ScrollbarAnimationController::DidMouseUp() {
   vertical_controller_->DidMouseUp();
   horizontal_controller_->DidMouseUp();
 
-  if (!mouse_is_near_any_scrollbar())
+  if (!MouseIsNearAnyScrollbar())
     PostDelayedFadeOut(false);
 }
 
@@ -275,7 +275,7 @@ void ScrollbarAnimationController::DidMouseMoveNear(
       }
     }
   } else {
-    if (mouse_is_near_any_scrollbar()) {
+    if (MouseIsNearAnyScrollbar()) {
       Show();
       StopAnimation();
     } else if (!is_animating_) {
@@ -305,19 +305,19 @@ bool ScrollbarAnimationController::CalcNeedTriggerScrollbarShow(
   return false;
 }
 
-bool ScrollbarAnimationController::mouse_is_over_scrollbar(
+bool ScrollbarAnimationController::MouseIsOverScrollbar(
     ScrollbarOrientation orientation) const {
   DCHECK(need_thinning_animation_);
   return GetScrollbarAnimationController(orientation).mouse_is_over_scrollbar();
 }
 
-bool ScrollbarAnimationController::mouse_is_near_scrollbar(
+bool ScrollbarAnimationController::MouseIsNearScrollbar(
     ScrollbarOrientation orientation) const {
   DCHECK(need_thinning_animation_);
   return GetScrollbarAnimationController(orientation).mouse_is_near_scrollbar();
 }
 
-bool ScrollbarAnimationController::mouse_is_near_any_scrollbar() const {
+bool ScrollbarAnimationController::MouseIsNearAnyScrollbar() const {
   DCHECK(need_thinning_animation_);
   return vertical_controller_->mouse_is_near_scrollbar() ||
          horizontal_controller_->mouse_is_near_scrollbar();
