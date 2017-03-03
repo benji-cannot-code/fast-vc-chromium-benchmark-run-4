fb02362df0168c569ec2c133cda43f8ce5675333
chaopeng <chaopeng@chromium.org>
2017-03-03 12:47:30 -0800

    Add Open my editor
    OME is an tools for opening file in local editor by right click menu in
    Chromium Code Search, Chromium Code Review and Chromium Build.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2720793005
    Cr-Commit-Position: refs/heads/master@{#454665}

---
+++ tools/chrome_extensions/open_my_editor/ext/cr-content.js
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// For codereview.chromium.org
+
+let clicked_element = null;
+
+document.addEventListener('mousedown', (event) => {
+  // right click
+  if (event.button == 2)
+    clicked_element = event.target;
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request == 'getFiles') {
+    let element = clicked_element;
+    while (element != null && element.tagName != 'TABLE')
+      element = element.parentElement;
+
+    let trs = element.getElementsByTagName('TR');
+    if (trs.length == 0)
+      alert('Please toggle one patchset.');
+
+    let files = [];
+    for (let i = 1; i < trs.length; ++i) {
+      let tr = trs[i];
+      if (tr.getAttribute('name') != 'patch')
+        continue;
+      // Skip deleted file.
+      if (tr.children[1].firstChild.data == 'D')
+        continue;
+
+      files.push(tr.children[2].children[0].text.replace(/\s*/g, ''));
+    }
+
+    sendResponse({files: files});
+  } else if (request == 'getFile' && clicked_element.tagName == 'A') {
+    sendResponse({file: clicked_element.text});
+  }
+});
---
+++ tools/chrome_extensions/open_my_editor/ext/cs-content.js
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// For cs.chromium.org
+
+let line = 0;
+
+document.addEventListener('mousedown', (event) => {
+  // right click
+  if (event.button == 2) {
+    let element = event.target;
+    while (element != null && element.tagName == 'SPAN') {
+      if (element.className == 'stx-line') {
+        line = parseInt(element.id.split('_')[1]);
+        break;
+      } else {
+        element = element.parentElement;
+      }
+    }
+  }
+}, true);
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request == 'getLine') {
+    sendResponse({line: line});
+  }
+});
