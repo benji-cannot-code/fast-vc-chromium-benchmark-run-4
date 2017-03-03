127fe3d1bcb56ca3c20356c1563c3d353a1382f8
pdr <pdr@chromium.org>
2017-03-03 12:07:05 -0800

    Fully disable LayerTreeHostScrollTestImplSideInvalidation due to crashes
    This test is flaky on all platforms. This patch temporarily disables it
    until a fix is found.
    
    This test crashes on OSX with:
    ./out/Debug/cc_unittests --gtest_filter=LayerTreeHostScrollTestImplSideInvalidation.RunMultiThread_DelegatingRenderer --gtest_repeat=10
    
    BUG=697652
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2734503002
    Cr-Commit-Position: refs/heads/master@{#454657}

--- cc/trees/layer_tree_host_unittest_scroll.cc
+++ cc/trees/layer_tree_host_unittest_scroll.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/single_thread_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
-#include "build/build_config.h"
 #include "cc/animation/animation_host.h"
 #include "cc/base/completion_event.h"
 #include "cc/input/main_thread_scrolling_reason.h"
@@ -2073,20 +2072,12 @@ class LayerTreeHostScrollTestPropertyTreeUpdate
 
 SINGLE_AND_MULTI_THREAD_TEST_F(LayerTreeHostScrollTestPropertyTreeUpdate);
 
-// Disabled due to flakes/crashes on Linux TSan Tests, linux_chromium_asan_rel
-// and Android N5X Swarm; see https://crbug.com/697652.
-#if defined(OS_LINUX) || defined(OS_ANDROID)
-#define MAYBE_LayerTreeHostScrollTestImplSideInvalidation \
-  DISABLED_LayerTreeHostScrollTestImplSideInvalidation
-#else
-#define MAYBE_LayerTreeHostScrollTestImplSideInvalidation \
-  LayerTreeHostScrollTestImplSideInvalidation
-#endif
-class MAYBE_LayerTreeHostScrollTestImplSideInvalidation
+// Disabled due to flakes/crashes, see https://crbug.com/697652.
+class DISABLED_LayerTreeHostScrollTestImplSideInvalidation
     : public LayerTreeHostScrollTest {
   void BeginTest() override {
     layer_tree_host()->outer_viewport_scroll_layer()->set_did_scroll_callback(
-        base::Bind(&MAYBE_LayerTreeHostScrollTestImplSideInvalidation::
+        base::Bind(&DISABLED_LayerTreeHostScrollTestImplSideInvalidation::
                        DidScrollOuterViewport,
                    base::Unretained(this)));
     PostSetNeedsCommitToMainThread();
@@ -2099,7 +2090,7 @@ class MAYBE_LayerTreeHostScrollTestImplSideInvalidation
       CompletionEvent completion;
       task_runner_provider()->ImplThreadTaskRunner()->PostTask(
           FROM_HERE,
-          base::Bind(&MAYBE_LayerTreeHostScrollTestImplSideInvalidation::
+          base::Bind(&DISABLED_LayerTreeHostScrollTestImplSideInvalidation::
                          WaitForInvalidationOnImplThread,
                      base::Unretained(this), &completion));
       completion.Wait();
@@ -2250,7 +2241,7 @@ class MAYBE_LayerTreeHostScrollTestImplSideInvalidation
   int num_of_deltas_ = 0;
 };
 
-MULTI_THREAD_TEST_F(MAYBE_LayerTreeHostScrollTestImplSideInvalidation);
+MULTI_THREAD_TEST_F(DISABLED_LayerTreeHostScrollTestImplSideInvalidation);
 
 }  // namespace
 }  // namespace cc
