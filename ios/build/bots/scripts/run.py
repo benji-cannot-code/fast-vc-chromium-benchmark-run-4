a0565d118c3cd5672784570b6cf7baa8e5e6f343
smut <smut@google.com>
2017-03-02 18:09:26 -0800

    Retry each failed iOS test case separately
    BUG=697230
    
    Review-Url: https://codereview.chromium.org/2726143002
    Cr-Commit-Position: refs/heads/master@{#454466}

--- ios/build/bots/scripts/run.py
+++ ios/build/bots/scripts/run.py
@@ -43,6 +43,7 @@ def main(args, test_args):
         args.xcode_version,
         args.out_dir,
         env_vars=args.env_var,
+        retries=args.retries,
         test_args=test_args,
         xctest=args.xctest,
       )
@@ -52,6 +53,7 @@ def main(args, test_args):
         args.xcode_version,
         args.out_dir,
         env_vars=args.env_var,
+        retries=args.retries,
         test_args=test_args,
         xctest=args.xctest,
       )
@@ -119,6 +121,13 @@ if __name__ == '__main__':
     '--platform',
     help='Platform to simulate.',
     metavar='sim',
+  )
+  parser.add_argument(
+    '-r',
+    '--retries',
+    help='Number of times to retry failed test cases.',
+    metavar='n',
+    type=int,
   )
   parser.add_argument(
     '-v',
