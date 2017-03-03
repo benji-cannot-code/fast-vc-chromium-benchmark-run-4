227e171fd6906513605b9b33d92d27384e74adbf
smcgruer <smcgruer@chromium.org>
2017-03-02 16:04:15 -0800

    Correct for enclosing layers scroll position in parentRelativeStickyBoxOffset
    The offset calculated in CompositedLayerMapping for
    parent_relative_sticky_box_offset calls |convertToLayerCoords| on the
    enclosing layer. This method relies on location() however which includes
    the scroll offset if the enclosing layer is not already the scroll
    ancestor, e.g. if you had the following layer setup:
    
    ScrollAncestor
    |
    +-- IntermediateLayer
        |
        +-- StickyLayer
    
    To correct for this, add back in the scroll offset for the case where
    the enclosing layer is not the scroll ancestor.
    
    BUG=692263
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2698843004
    Cr-Commit-Position: refs/heads/master@{#454444}

--- third_party/WebKit/Source/core/layout/compositing/CompositedLayerMapping.h
+++ third_party/WebKit/Source/core/layout/compositing/CompositedLayerMapping.h
@@ -248,8 +248,6 @@ class CORE_EXPORT CompositedLayerMapping final : public GraphicsLayerClient {
   void updateFilters(const ComputedStyle&);
   void updateBackdropFilters(const ComputedStyle&);
 
-  void updateStickyConstraints(const ComputedStyle&);
-
   void setBlendMode(WebBlendMode);
 
   bool needsGraphicsLayerUpdate() {
@@ -366,6 +364,7 @@ class CORE_EXPORT CompositedLayerMapping final : public GraphicsLayerClient {
       const FloatSize& relativeCompositingBoundsSize);
   void updateScrollingLayerGeometry(const IntRect& localCompositingBounds);
   void updateChildClippingMaskLayerGeometry();
+  void updateStickyConstraints(const ComputedStyle&, const PaintLayer*);
 
   void createPrimaryGraphicsLayer();
   void destroyGraphicsLayers();
