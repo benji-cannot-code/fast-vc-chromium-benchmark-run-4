3586a795085502d9654302cdfc5f5388603d460a
cwallez <cwallez@chromium.org>
2017-03-03 11:17:01 -0800

    gpu/config: remove CollectGpuID
    Use CollectBasicGraphicsInfo instead, which is a superset. The
    additional gathering done in CollectBasicGraphicsInfo is ok as it
    CollectGpuID was only called in tests, and on the infrequent display
    reconfigure on Mac (and collecting info is fast on Mac).
    
    BUG=angleproject:1874
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2729103004
    Cr-Commit-Position: refs/heads/master@{#454644}

--- gpu/config/gpu_info_collector.h
+++ gpu/config/gpu_info_collector.h
@@ -14,10 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gpu {
 
-// Collect GPU vendor_id and device ID.
-GPU_EXPORT CollectInfoResult CollectGpuID(uint32_t* vendor_id,
-                                          uint32_t* device_id);
-
 // Collects basic GPU info without creating a GL/DirectX context (and without
 // the danger of crashing), including vendor_id and device_id.
 // This is called at browser process startup time.
