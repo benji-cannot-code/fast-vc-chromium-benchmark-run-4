9b4e83e6e5147f3fcd09db734d410aa888c98c0f
fmalita <fmalita@chromium.org>
2017-03-03 12:44:48 -0800

    Remove unneeded GraphicsContext.cpp include
    As suggested in https://codereview.chromium.org/2674003002/#msg39.
    
    TBR=senorblanco@chromium.org
    
    Review-Url: https://codereview.chromium.org/2731773002
    Cr-Commit-Position: refs/heads/master@{#454663}

--- third_party/WebKit/Source/platform/graphics/GraphicsContext.cpp
+++ third_party/WebKit/Source/platform/graphics/GraphicsContext.cpp
@@ -45,7 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkData.h"
 #include "third_party/skia/include/core/SkRRect.h"
 #include "third_party/skia/include/core/SkRefCnt.h"
-#include "third_party/skia/include/core/SkUnPreMultiply.h"
 #include "third_party/skia/include/effects/SkGradientShader.h"
 #include "third_party/skia/include/effects/SkLumaColorFilter.h"
 #include "third_party/skia/include/effects/SkPictureImageFilter.h"
