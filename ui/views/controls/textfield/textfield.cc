0af7c3336ba2a349dc2879b3f955e0acd68bb6a8
mmarkowicz <mmarkowicz@opera.com>
2017-03-03 09:50:11 -0800

    Update views::Textfield cursor color on text color change
    views::Textfield::SetTextColor should apply new color to the cursor as well.
    Currently this will happen but only after Textfield::OnNativeThemeChanged
    is triggered in some way.
    
    BUG=697939
    
    Review-Url: https://codereview.chromium.org/2730623003
    Cr-Commit-Position: refs/heads/master@{#454616}

--- ui/views/controls/textfield/textfield.cc
+++ ui/views/controls/textfield/textfield.cc
@@ -514,6 +514,7 @@ size_t Textfield::GetCursorPosition() const {
 
 void Textfield::SetColor(SkColor value) {
   GetRenderText()->SetColor(value);
+  cursor_view_.layer()->SetColor(value);
   SchedulePaint();
 }
 
