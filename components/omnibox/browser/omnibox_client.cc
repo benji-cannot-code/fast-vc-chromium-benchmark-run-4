c33c658122227c1d7976f031bcdecb2e854882fe
pkasting <pkasting@chromium.org>
2017-03-03 17:56:24 -0800

    Pull "placeholder" implementations to OmniboxClient to simplify test overrides.
    I intend to add another such override class in the future, and the amount of
    boilerplate this saves is significant.
    
    BUG=none
    TEST=none
    
    Review-Url: https://codereview.chromium.org/2729083003
    Cr-Commit-Position: refs/heads/master@{#454745}

---
+++ components/omnibox/browser/omnibox_client.cc
@@ -0,0 +1,82 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/omnibox/browser/omnibox_client.h"
+
+#include "base/strings/string_util.h"
+#include "ui/gfx/image/image.h"
+
+std::unique_ptr<OmniboxNavigationObserver>
+OmniboxClient::CreateOmniboxNavigationObserver(
+    const base::string16& text,
+    const AutocompleteMatch& match,
+    const AutocompleteMatch& alternate_nav_match) {
+  return nullptr;
+}
+
+bool OmniboxClient::CurrentPageExists() const {
+  return true;
+}
+
+const GURL& OmniboxClient::GetURL() const {
+  return GURL::EmptyGURL();
+}
+
+const base::string16& OmniboxClient::GetTitle() const {
+  return base::EmptyString16();
+}
+
+gfx::Image OmniboxClient::GetFavicon() const {
+  return gfx::Image();
+}
+
+bool OmniboxClient::IsInstantNTP() const {
+  return false;
+}
+
+bool OmniboxClient::IsSearchResultsPage() const {
+  return false;
+}
+
+bool OmniboxClient::IsLoading() const {
+  return false;
+}
+
+bool OmniboxClient::IsPasteAndGoEnabled() const {
+  return false;
+}
+
+bool OmniboxClient::IsNewTabPage(const std::string& url) const {
+  return false;
+}
+
+bool OmniboxClient::IsHomePage(const std::string& url) const {
+  return false;
+}
+
+bookmarks::BookmarkModel* OmniboxClient::GetBookmarkModel() {
+  return nullptr;
+}
+
+TemplateURLService* OmniboxClient::GetTemplateURLService() {
+  return nullptr;
+}
+
+AutocompleteClassifier* OmniboxClient::GetAutocompleteClassifier() {
+  return nullptr;
+}
+
+gfx::Image OmniboxClient::GetIconIfExtensionMatch(
+    const AutocompleteMatch& match) const {
+  return gfx::Image();
+}
+
+bool OmniboxClient::ProcessExtensionKeyword(
+    TemplateURL* template_url,
+    const AutocompleteMatch& match,
+    WindowOpenDisposition disposition,
+    OmniboxNavigationObserver* observer) {
+  return false;
+}
