c963b7ed42887c573c1df84af9baa397e7fb2894
miu <miu@chromium.org>
2017-03-04 01:09:09 -0800

    MediaRouter: Cache MediaRoutesObserver queries to prevent ext wake-ups
    Resolved a performance issue where the Media Router component extension
    was being woken-up each time a new tab was opened. The solution is to
    cache route list updates so that the registration of new observers does
    not force the extension to wake-up to report on something we can cache
    browser-side. (This is the same as what is already being done for the
    MediaSinksObservers.)
    
    BUG=621246
    TEST=Ran chrome with --enable-logging=stderr --vmodule=media_router_mojo_impl=1 to confirm extension wake-ups do not happen when opening new tabs.
    
    Review-Url: https://codereview.chromium.org/2735493002
    Cr-Commit-Position: refs/heads/master@{#454769}

--- chrome/browser/media/router/mojo/media_router_mojo_impl.h
+++ chrome/browser/media/router/mojo/media_router_mojo_impl.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
+#include "base/optional.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
 #include "chrome/browser/media/router/issue.h"
@@ -171,13 +172,10 @@ class MediaRouterMojoImpl : public MediaRouterBase,
     // True if the query has been sent to the MRPM.
     bool is_active = false;
 
-    // True if cached result is available.
-    bool has_cached_result = false;
-
-    // Cached list of sinks for the query, if |has_cached_result| is true.
-    // Empty otherwise.
-    std::vector<MediaSink> cached_sink_list;
+    // Cached list of sinks and origins for the query.
+    base::Optional<std::vector<MediaSink>> cached_sink_list;
     std::vector<url::Origin> origins;
+
     base::ObserverList<MediaSinksObserver> observers;
 
    private:
@@ -191,6 +189,11 @@ class MediaRouterMojoImpl : public MediaRouterBase,
 
     // True if the query has been sent to the MRPM.  False otherwise.
     bool is_active = false;
+
+    // Cached list of routes and joinable route IDs for the query.
+    base::Optional<std::vector<MediaRoute>> cached_route_list;
+    std::vector<std::string> joinable_route_ids;
+
     base::ObserverList<MediaRoutesObserver> observers;
 
    private:
@@ -232,6 +235,11 @@ class MediaRouterMojoImpl : public MediaRouterBase,
   void UnregisterIssuesObserver(IssuesObserver* observer) override;
   void RegisterRouteMessageObserver(RouteMessageObserver* observer) override;
   void UnregisterRouteMessageObserver(RouteMessageObserver* observer) override;
+
+  // Notifies |observer| of any existing cached routes, if it is still
+  // registered.
+  void NotifyOfExistingRoutesIfRegistered(const MediaSource::Id& source_id,
+                                          MediaRoutesObserver* observer) const;
 
   // These calls invoke methods in the component extension via Mojo.
   void DoCreateRoute(const MediaSource::Id& source_id,
