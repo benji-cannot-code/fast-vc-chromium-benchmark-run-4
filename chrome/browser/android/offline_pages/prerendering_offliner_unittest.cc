a3782cd5cb772b5a997b38f8ee6182bf77e50ffb
chili <chili@chromium.org>
2017-03-03 17:23:56 -0800

    [Offline Pages] Turn Offliner::Cancel into an async operation to resolve conflicting assumptions between the Offliner and RequestCoordinator.
    Upon Offliner::Cancel, RequestCoordinator assumes that the offliner is free, but
    offliner may be waiting until save operation to complete. This discrepancy causes
    RequestCoordinator to bombard the offliner with all the pending requests, potentially
    blocking the main thread for the save operation callback to be called and causing
    all pending requests to be labeled as EXPIRED
    
    BUG=694864
    
    Review-Url: https://codereview.chromium.org/2715433006
    Cr-Commit-Position: refs/heads/master@{#454740}

--- chrome/browser/android/offline_pages/prerendering_offliner_unittest.cc
+++ chrome/browser/android/offline_pages/prerendering_offliner_unittest.cc
@@ -173,6 +173,10 @@ class PrerenderingOfflinerTest : public testing::Test {
     return base::Bind(&PrerenderingOfflinerTest::OnCompletion,
                       base::Unretained(this));
   }
+  Offliner::CancelCallback const cancel_callback() {
+    return base::Bind(&PrerenderingOfflinerTest::OnCancel,
+                      base::Unretained(this));
+  }
 
   bool SaveInProgress() const { return model_->mock_saving(); }
   MockPrerenderingLoader* loader() { return loader_; }
@@ -180,10 +184,12 @@ class PrerenderingOfflinerTest : public testing::Test {
   bool completion_callback_called() { return completion_callback_called_; }
   Offliner::RequestStatus request_status() { return request_status_; }
   OfflinerPolicy* policy() { return policy_; }
+  bool cancel_callback_called() { return cancel_callback_called_; }
 
  private:
   void OnCompletion(const SavePageRequest& request,
                     Offliner::RequestStatus status);
+  void OnCancel(int64_t offline_id);
 
   content::TestBrowserThreadBundle thread_bundle_;
   TestingProfile profile_;
@@ -192,6 +198,7 @@ class PrerenderingOfflinerTest : public testing::Test {
   MockPrerenderingLoader* loader_;
   MockOfflinePageModel* model_;
   bool completion_callback_called_;
+  bool cancel_callback_called_;
   Offliner::RequestStatus request_status_;
   OfflinerPolicy* policy_;
 
@@ -201,6 +208,7 @@ class PrerenderingOfflinerTest : public testing::Test {
 PrerenderingOfflinerTest::PrerenderingOfflinerTest()
     : thread_bundle_(content::TestBrowserThreadBundle::IO_MAINLOOP),
       completion_callback_called_(false),
+      cancel_callback_called_(false),
       request_status_(Offliner::RequestStatus::UNKNOWN) {}
 
 PrerenderingOfflinerTest::~PrerenderingOfflinerTest() {}
@@ -220,6 +228,11 @@ void PrerenderingOfflinerTest::OnCompletion(const SavePageRequest& request,
   DCHECK(!completion_callback_called_);  // Expect single callback per request.
   completion_callback_called_ = true;
   request_status_ = status;
+}
+
+void PrerenderingOfflinerTest::OnCancel(int64_t offline_id) {
+  DCHECK(!cancel_callback_called_);
+  cancel_callback_called_ = true;
 }
 
 TEST_F(PrerenderingOfflinerTest, LoadAndSaveBadUrl) {
@@ -286,7 +299,9 @@ TEST_F(PrerenderingOfflinerTest, CancelWhenLoading) {
   EXPECT_TRUE(offliner()->LoadAndSave(request, callback()));
   EXPECT_FALSE(loader()->IsIdle());
 
-  offliner()->Cancel();
+  offliner()->Cancel(cancel_callback());
+  PumpLoop();
+  EXPECT_TRUE(cancel_callback_called());
   EXPECT_TRUE(loader()->IsIdle());
 }
 
@@ -304,9 +319,10 @@ TEST_F(PrerenderingOfflinerTest, CancelWhenLoaded) {
   EXPECT_TRUE(loader()->IsLoaded());
   EXPECT_TRUE(SaveInProgress());
 
-  offliner()->Cancel();
+  offliner()->Cancel(cancel_callback());
   PumpLoop();
   EXPECT_FALSE(completion_callback_called());
+  EXPECT_TRUE(cancel_callback_called());
   EXPECT_FALSE(loader()->IsLoaded());
   // Note: save still in progress since it does not support canceling.
   EXPECT_TRUE(SaveInProgress());
