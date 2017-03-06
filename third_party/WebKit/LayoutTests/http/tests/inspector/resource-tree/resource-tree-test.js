1dc08e69ad5c2cccc4225632f8108057cb654bb5
eostroukhov <eostroukhov@chromium.org>
2017-03-06 13:13:03 -0800

    [DevTools] Separate frames and resources handling
    BUG=698027
    
    Review-Url: https://codereview.chromium.org/2712043002
    Cr-Commit-Position: refs/heads/master@{#454956}

--- third_party/WebKit/LayoutTests/http/tests/inspector/resource-tree/resource-tree-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/resource-tree/resource-tree-test.js
@@ -65,7 +65,7 @@ InspectorTest.dumpResourcesTree = function()
             dump(children[i], prefix + "    ");
     }
 
-    dump(UI.panels.resources.resourcesListTreeElement, "");
+    dump(UI.panels.resources._resourcesSection._treeElement, "");
     if (!InspectorTest._testSourceNavigator) {
         InspectorTest._testSourceNavigator = new Sources.SourcesNavigatorView();
         InspectorTest._testSourceNavigator.show(UI.inspectorView.element);
