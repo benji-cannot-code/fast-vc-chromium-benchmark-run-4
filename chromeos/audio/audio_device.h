1b37bba2c4b082926ddd785602ade4264ce5a54e
jennyz <jennyz@chromium.org>
2017-03-06 12:14:02 -0800

    Handle the dual microphones and dual cameras cases. Activate the proper microphone when user activates the front or rear camera on basking devices.
    BUG=698327
    TEST=See the feature spec document in the bug.
    TBR=dalecurtis@chromium.org
    
    Review-Url: https://codereview.chromium.org/2721733003
    Cr-Commit-Position: refs/heads/master@{#454933}

--- chromeos/audio/audio_device.h
+++ chromeos/audio/audio_device.h
@@ -64,6 +64,8 @@ struct CHROMEOS_EXPORT AudioDevice {
             type == AUDIO_TYPE_LINEOUT);
   }
 
+  bool IsExternalDevice() const;
+
   bool is_input = false;
 
   // Id of this audio device. The legacy |id| is assigned to be unique everytime
