f046186482ec2b6463e6bfbeb9ce6edd07791ebf
manzagop <manzagop@chromium.org>
2017-03-07 10:09:44 -0800

    Flush stability file after initialization
    Maximize the chance of persisting minimal information (version details)
    in stability files. This is to defend against system instability issues
    for which the system would not persist the in memory content.
    
    BUG=620813
    
    Review-Url: https://codereview.chromium.org/2720353002
    Cr-Commit-Position: refs/heads/master@{#455133}

--- components/browser_watcher/features.h
+++ components/browser_watcher/features.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace browser_watcher {
 
 extern const base::Feature kStabilityDebuggingFeature;
+extern const base::Feature kStabilityDebuggingFlushFeature;
 
 }  // namespace browser_watcher
 
