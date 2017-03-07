b43202c5ea963e24bccc2f25add1ef9454ea1574
dewittj <dewittj@chromium.org>
2017-03-07 10:39:29 -0800

    [Offline Pages] Destroy a typo.
    BUG=
    
    Review-Url: https://codereview.chromium.org/2719963003
    Cr-Commit-Position: refs/heads/master@{#455151}

--- chrome/android/java/src/org/chromium/chrome/browser/offlinepages/evaluation/OfflinePageEvaluationBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/offlinepages/evaluation/OfflinePageEvaluationBridge.java
@@ -108,9 +108,9 @@ public class OfflinePageEvaluationBridge {
     private OutputStreamWriter mLogOutput;
 
     /** Destroys the native portion of the bridge. */
-    public void destory() {
+    public void destroy() {
         if (mNativeOfflinePageEvaluationBridge != 0) {
-            nativeDestory(mNativeOfflinePageEvaluationBridge);
+            nativeDestroy(mNativeOfflinePageEvaluationBridge);
             mNativeOfflinePageEvaluationBridge = 0;
             mIsOfflinePageModelLoaded = false;
         }
@@ -263,7 +263,7 @@ public class OfflinePageEvaluationBridge {
 
     private native long nativeCreateBridgeForProfile(
             Profile profile, boolean useEvaluationScheduler, boolean useBackgroundLoader);
-    private native void nativeDestory(long nativeOfflinePageEvaluationBridge);
+    private native void nativeDestroy(long nativeOfflinePageEvaluationBridge);
 
     private native void nativeGetAllPages(long nativeOfflinePageEvaluationBridge,
             List<OfflinePageItem> offlinePages, final Callback<List<OfflinePageItem>> callback);
