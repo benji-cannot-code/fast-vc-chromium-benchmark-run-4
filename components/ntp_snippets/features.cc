f1b88d3b313fbecb4c9a6357e094a2db1a61c500
vitaliii <vitaliii@chromium.org>
2017-03-07 06:59:32 -0800

    [NTP::SectionOrder] Disable ClickBasedRanker by default.
    BUG=646841
    
    Review-Url: https://codereview.chromium.org/2739633002
    Cr-Commit-Position: refs/heads/master@{#455076}

--- components/ntp_snippets/features.cc
+++ components/ntp_snippets/features.cc
@@ -55,7 +55,7 @@ CategoryRankerChoice GetSelectedCategoryRanker() {
 
   if (category_ranker_value.empty()) {
     // Default, Enabled or Disabled.
-    return CategoryRankerChoice::CLICK_BASED;
+    return CategoryRankerChoice::CONSTANT;
   }
   if (category_ranker_value == kCategoryRankerConstantRanker) {
     return CategoryRankerChoice::CONSTANT;
