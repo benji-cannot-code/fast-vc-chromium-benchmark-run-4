3bdcad49303ab84b695f81eba581683c26956f95
bugsnash <bugsnash@chromium.org>
2017-03-06 21:20:10 -0800

    Added CSSPropertyFontUtils which holds font parsing methods.
    Moved all font parsing methods that are used in multiple places from
    CSSPropertyParser to new CSSPropertyFontUtils class. This is in
    preparation for implementing APIs for font properties, which will need
    to call these methods.
    
    This patch
    - Adds new files CSSPropertyFontUtils.h and CSSPropertyFontUtils.cpp
    - Moves consumeFontFamily(), consumeFontSize(), consumeLineHeight(),
      consumeGenericFamily(), consumeFamilyName(), concatenateFamilyName(),
      consumeFontWeight(), consumeFontFeatureSettings(), and
      consumeFontFeatureTag() from CSSPropertyParser.cpp to
      CSSPropertyFontUtils.cpp.
    - Deletes font related includes that are no longer used from
      CSSPropertyParser.
    
    BUG=668012
    
    Review-Url: https://codereview.chromium.org/2733773002
    Cr-Commit-Position: refs/heads/master@{#455036}

---
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyFontUtils.h
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CSSPropertyFontUtils_h
+#define CSSPropertyFontUtils_h
+
+#include "core/css/parser/CSSParserMode.h"
+#include "core/css/parser/CSSPropertyParserHelpers.h"
+#include "wtf/Allocator.h"
+
+namespace blink {
+
+class CSSFontFeatureValue;
+class CSSParserTokenRange;
+class CSSValue;
+class CSSValueList;
+
+class CSSPropertyFontUtils {
+  STATIC_ONLY(CSSPropertyFontUtils);
+
+  static CSSValue* consumeFontSize(
+      CSSParserTokenRange&,
+      CSSParserMode,
+      CSSPropertyParserHelpers::UnitlessQuirk =
+          CSSPropertyParserHelpers::UnitlessQuirk::Forbid);
+
+  static CSSValue* consumeLineHeight(CSSParserTokenRange&, CSSParserMode);
+
+  static CSSValueList* consumeFontFamily(CSSParserTokenRange&);
+  static CSSValue* consumeGenericFamily(CSSParserTokenRange&);
+  static CSSValue* consumeFamilyName(CSSParserTokenRange&);
+  static String concatenateFamilyName(CSSParserTokenRange&);
+
+  static CSSIdentifierValue* consumeFontWeight(CSSParserTokenRange&);
+
+  static CSSValue* consumeFontFeatureSettings(CSSParserTokenRange&);
+  static CSSFontFeatureValue* consumeFontFeatureTag(CSSParserTokenRange&);
+};
+
+}  // namespace blink
+
+#endif  // CSSPropertyFontUtils_h
