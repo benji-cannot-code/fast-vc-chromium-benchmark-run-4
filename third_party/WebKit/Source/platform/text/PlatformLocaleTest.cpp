724af6f683fad05cbd0f7d247d133edae9a8356b
tkent <tkent@chromium.org>
2017-03-07 15:28:46 -0800

    <input type=number>: accept localized digits without showing them.
    Locale::stripInvalidNumberCharacters() missed to call initializeLocaleData().
    
    BUG=694391
    
    Review-Url: https://codereview.chromium.org/2738463003
    Cr-Commit-Position: refs/heads/master@{#455286}

---
+++ third_party/WebKit/Source/platform/text/PlatformLocaleTest.cpp
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/text/PlatformLocale.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace blink {
+
+TEST(PlatformLocaleTest, StripInvalidNumberCharacters) {
+  std::unique_ptr<Locale> locale = Locale::create("ar");
+  String result = locale->stripInvalidNumberCharacters(
+      String::fromUTF8("abc\xD9\xA0ghi"), "0123456789");
+  // ARABIC-INDIC DIGIT ZERO U+0660 = \xD9\xA0 in UTF-8
+  EXPECT_EQ(String::fromUTF8("\xD9\xA0"), result);
+}
+}
