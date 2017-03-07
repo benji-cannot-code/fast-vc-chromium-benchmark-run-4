e464196d2b4c1c49e7fe37f9805fde8a462aa121
skare <skare@chromium.org>
2017-03-07 09:30:27 -0800

    Remove GoogleNowNotification stats collector
    BUG=539674
    
    Review-Url: https://codereview.chromium.org/2727103002
    Cr-Commit-Position: refs/heads/master@{#455117}

--- chrome/browser/notifications/message_center_notification_manager.h
+++ chrome/browser/notifications/message_center_notification_manager.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
-#include "chrome/browser/notifications/google_now_notification_stats_collector.h"
 #include "chrome/browser/notifications/message_center_stats_collector.h"
 #include "chrome/browser/notifications/notification.h"
 #include "chrome/browser/notifications/notification_system_observer.h"
@@ -109,9 +108,6 @@ class MessageCenterNotificationManager
 
   // Keeps track of all notification statistics for UMA purposes.
   MessageCenterStatsCollector stats_collector_;
-
-  // Keeps track of notifications specific to Google Now for UMA purposes.
-  GoogleNowNotificationStatsCollector google_now_stats_collector_;
 
   DISALLOW_COPY_AND_ASSIGN(MessageCenterNotificationManager);
 };
