fc556af23da76f2a421b85af15283fb628b7a597
dtrainor <dtrainor@chromium.org>
2017-03-06 17:07:17 -0800

    Initial checkin of OfflineContentProvider.
    This patch adds the initial OfflineContentProvider interface and
    aggregator.  This will be the framework that exposes different types of
    downloads to the front end UI.  The aggregator groups multiple OfflineContentProviders and
    exposes them as a single list of OfflineItems.
    
    BUG=691805
    
    Review-Url: https://codereview.chromium.org/2690333002
    Cr-Commit-Position: refs/heads/master@{#455011}

---
+++ components/offline_items_collection/core/test_support/scoped_mock_offline_content_provider.cc
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/offline_items_collection/core/test_support/scoped_mock_offline_content_provider.h"
+
+#include "components/offline_items_collection/core/offline_content_aggregator.h"
+#include "components/offline_items_collection/core/offline_content_provider.h"
+
+namespace offline_items_collection {
+
+ScopedMockOfflineContentProvider::ScopedMockObserver::ScopedMockObserver(
+    OfflineContentProvider* provider)
+    : provider_(provider) {
+  provider_->AddObserver(this);
+}
+
+ScopedMockOfflineContentProvider::ScopedMockObserver::~ScopedMockObserver() {
+  provider_->RemoveObserver(this);
+}
+
+ScopedMockOfflineContentProvider::ScopedMockOfflineContentProvider(
+    const std::string& name_space,
+    OfflineContentAggregator* aggregator)
+    : name_space_(name_space), aggregator_(aggregator) {
+  aggregator_->RegisterProvider(name_space_, this);
+}
+
+ScopedMockOfflineContentProvider::~ScopedMockOfflineContentProvider() {
+  Unregister();
+}
+
+void ScopedMockOfflineContentProvider::Unregister() {
+  if (!aggregator_)
+    return;
+
+  aggregator_->UnregisterProvider(name_space_);
+  aggregator_ = nullptr;
+}
+
+}  // namespace offline_items_collection
