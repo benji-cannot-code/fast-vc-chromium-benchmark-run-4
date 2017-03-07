fd7c492164d53fdf105f64ea6144eec8b170855c
sdefresne <sdefresne@chromium.org>
2017-03-07 13:02:09 -0800

    Add unittests checking that old saved session can be restored.
    The session serialisation code has support for converting a
    serialised session from an old format to the new one when a
    class is renamed or a property is moved so add unittests to
    check the conversion works properly by loading old sessions.
    
    Clean up the code to use proper idioms (use leading underscore
    for Objective-C ivar, use macro to mark designated initialiser,
    remove redundant ivar backing synthesised property, ...).
    
    BUG=698750
    
    Review-Url: https://codereview.chromium.org/2732063003
    Cr-Commit-Position: refs/heads/master@{#455217}

--- ios/chrome/browser/tabs/tab_model_unittest.mm
+++ ios/chrome/browser/tabs/tab_model_unittest.mm
@@ -42,17 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using web::WebStateImpl;
 
-// For some of the unit tests, we need to make sure the session is saved
-// immediately so we can read it back in to verify various attributes. This
-// is not a situation we normally expect to be in because we never
-// want the session being saved on the main thread in the production app.
-// We could expose this as part of the service's public API, but again that
-// might encourage use where we don't want it. As a result, just use the
-// known private-for-testing method directly.
-@interface SessionServiceIOS (Testing)
-- (void)performSaveWindow:(SessionWindowIOS*)window
-              toDirectory:(NSString*)directory;
-@end
 
 @interface TabModel (VisibleForTesting)
 - (SessionWindowIOS*)windowForSavingSession;
