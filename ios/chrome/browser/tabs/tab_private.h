41723da2410059d3bd92562e8a28405bd07a05f2
sdefresne <sdefresne@chromium.org>
2017-03-07 10:14:02 -0800

    Remove Tab -currentNavigationItem.
    This returns CRWSessionController -currentEntry which should not be
    used. Fix all direct calls in Tab (but leave the call in Tab -url as
    it requires fixing all individual clients) by calling the correct
    method on the WebState NavigationManager.
    
    BUG=620465
    
    Review-Url: https://codereview.chromium.org/2724103005
    Cr-Commit-Position: refs/heads/master@{#455136}

--- ios/chrome/browser/tabs/tab_private.h
+++ ios/chrome/browser/tabs/tab_private.h
@@ -18,7 +18,6 @@ class WebStateImpl;
 
 - (OpenInController*)openInController;
 - (void)closeThisTab;
-- (web::NavigationItem*)currentNavigationItem;
 - (void)setShouldObserveInfoBarManager:(BOOL)shouldObserveInfoBarManager;
 - (void)setShouldObserveFaviconChanges:(BOOL)shouldObserveFaviconChanges;
 
--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -4796,9 +4796,9 @@ const NSTimeInterval kSnapshotOverlayTransition = 0.5;
 
 - (void)SSLStatusUpdater:(CRWSSLStatusUpdater*)SSLStatusUpdater
     didChangeSSLStatusForNavigationItem:(web::NavigationItem*)navigationItem {
-  web::NavigationItem* currentNavigationItem =
+  web::NavigationItem* lastCommittedNavigationItem =
       self.webState->GetNavigationManager()->GetLastCommittedItem();
-  if (navigationItem == currentNavigationItem) {
+  if (navigationItem == lastCommittedNavigationItem) {
     [self didUpdateSSLStatusForCurrentNavigationItem];
   }
 }
