1e6f262d8d0683537777494ecaed9b96d98b46ab
skobes <skobes@chromium.org>
2017-03-07 10:31:17 -0800

    Fix null pointer dereference in PLSA's scrollbarStyleSource.
    The scroller inside the alt-text shadow tree created by HTMLImageFallbackHelper
    was trying to read scrollbar styles from the <img> element, which had no
    LayoutObject because it was styled with "display: contents".
    
    BUG=697247
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2734993002
    Cr-Commit-Position: refs/heads/master@{#455148}

--- third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.cpp
@@ -1152,8 +1152,10 @@ static inline const LayoutObject& scrollbarStyleSource(
       return layoutObject;
 
     if (ShadowRoot* shadowRoot = node->containingShadowRoot()) {
-      if (shadowRoot->type() == ShadowRootType::UserAgent)
-        return *shadowRoot->host().layoutObject();
+      if (shadowRoot->type() == ShadowRootType::UserAgent) {
+        if (LayoutObject* hostLayoutObject = shadowRoot->host().layoutObject())
+          return *hostLayoutObject;
+      }
     }
   }
 
