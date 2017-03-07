326f78d173fa0bc003bb84224b15f320e4ea06ae
yosin <yosin@chromium.org>
2017-03-06 17:27:28 -0800

    Get rid of FrameSelection::setSelection() taking VisibleSelection and SetSelectionOptions
    This patch gets rid of |FrameSelection::SetSelectionOptions()| taking
    |VisibleSelection| and |SetSelectionOptions| since it is used only in one
    call site to reduce source code size for improving code health.
    
    BUG=698633
    TEST=n/a; no behavior changes
    
    Review-Url: https://codereview.chromium.org/2733793002
    Cr-Commit-Position: refs/heads/master@{#455019}

--- third_party/WebKit/Source/core/editing/FrameSelection.cpp
+++ third_party/WebKit/Source/core/editing/FrameSelection.cpp
@@ -320,11 +320,6 @@ void FrameSelection::setSelection(const VisibleSelection& newSelection,
       options, align, granularity);
 }
 
-void FrameSelection::setSelection(const VisibleSelection& newSelection,
-                                  SetSelectionOptions options) {
-  setSelection(newSelection.asSelection(), options);
-}
-
 void FrameSelection::nodeChildrenWillBeRemoved(ContainerNode& container) {
   if (!container.inActiveDocument())
     return;
@@ -398,7 +393,7 @@ bool FrameSelection::modify(EAlteration alter,
 
   const SetSelectionOptions options =
       CloseTyping | ClearTypingStyle | userTriggered;
-  setSelection(selectionModifier.selection(), options);
+  setSelection(selectionModifier.selection().asSelection(), options);
 
   if (granularity == LineGranularity || granularity == ParagraphGranularity)
     m_xPosForVerticalArrowNavigation =
--- third_party/WebKit/Source/core/editing/FrameSelection.h
+++ third_party/WebKit/Source/core/editing/FrameSelection.h
@@ -132,7 +132,6 @@ class CORE_EXPORT FrameSelection final
                     SetSelectionOptions = CloseTyping | ClearTypingStyle,
                     CursorAlignOnScroll = CursorAlignOnScroll::IfNeeded,
                     TextGranularity = CharacterGranularity);
-  void setSelection(const VisibleSelection&, SetSelectionOptions);
   bool setSelectedRange(
       const EphemeralRange&,
       TextAffinity,
