9e6261bde9b9e7e9717aa3a718f351f17b21fdb7
vabr <vabr@chromium.org>
2017-03-07 15:34:30 -0800

    Check Swaps and MergeDictionary for base::Value
    The following is a dangerous practice:
    
    1: DictionaryValue d;
    2: Value *p = &d;
    3: *p = Value(1.234);
    4: DictionaryValue().Swap(&d);
    
    After line 3, |d| is no longer a DictionaryValue.
    
    Before r451296, it caused no bad memory access, because the storage
    for the double and for the dictionary did not overlap. After that CL, this code would crash. In any case, it is dangerous.
    
    Similar issue is there for DictionaryValue::MergeDictionary and ListValue::Swap.
    
    This issue is only temporary, because DictionaryValue will eventually become
    just Value. But until then, the code needs to be sure to avoid such scenarios.
    
    Therefore this CL introduces CHECKS for the correct value types.
    
    The CL also introduces two more CHECKS in Internal*AssignFrom to prevent
    executing a desctructor on an arbitrary address.
    
    All the above are CHECKS, as opposed to DCHECKS, because if the checked
    conditions do not hold, execution of arbitrary parts of memory might be
    possible.
    
    BUG=697817
    
    Review-Url: https://codereview.chromium.org/2728773005
    Cr-Commit-Position: refs/heads/master@{#455288}

--- base/values.cc
+++ base/values.cc
@@ -176,7 +176,7 @@ Value::Value(std::vector<char>&& in_blob) : type_(Type::BINARY) {
 Value& Value::operator=(const Value& that) {
   if (this != &that) {
     if (type_ == that.type_) {
-      InternalCopyAssignFrom(that);
+      InternalCopyAssignFromSameType(that);
     } else {
       InternalCleanup();
       InternalCopyConstructFrom(that);
@@ -189,7 +189,7 @@ Value& Value::operator=(const Value& that) {
 Value& Value::operator=(Value&& that) {
   if (this != &that) {
     if (type_ == that.type_) {
-      InternalMoveAssignFrom(std::move(that));
+      InternalMoveAssignFromSameType(std::move(that));
     } else {
       InternalCleanup();
       InternalMoveConstructFrom(std::move(that));
@@ -533,8 +533,8 @@ void Value::InternalMoveConstructFrom(Value&& that) {
   }
 }
 
-void Value::InternalCopyAssignFrom(const Value& that) {
-  type_ = that.type_;
+void Value::InternalCopyAssignFromSameType(const Value& that) {
+  CHECK_EQ(type_, that.type_);
 
   switch (type_) {
     case Type::NONE:
@@ -563,8 +563,8 @@ void Value::InternalCopyAssignFrom(const Value& that) {
   }
 }
 
-void Value::InternalMoveAssignFrom(Value&& that) {
-  type_ = that.type_;
+void Value::InternalMoveAssignFromSameType(Value&& that) {
+  CHECK_EQ(type_, that.type_);
 
   switch (type_) {
     case Type::NONE:
@@ -1037,6 +1037,7 @@ std::unique_ptr<DictionaryValue> DictionaryValue::DeepCopyWithoutEmptyChildren()
 }
 
 void DictionaryValue::MergeDictionary(const DictionaryValue* dictionary) {
+  CHECK(dictionary->is_dict());
   for (DictionaryValue::Iterator it(*dictionary); !it.IsAtEnd(); it.Advance()) {
     const Value* merge_value = &it.value();
     // Check whether we have to merge dictionaries.
@@ -1055,6 +1056,7 @@ void DictionaryValue::MergeDictionary(const DictionaryValue* dictionary) {
 }
 
 void DictionaryValue::Swap(DictionaryValue* other) {
+  CHECK(other->is_dict());
   dict_ptr_->swap(*(other->dict_ptr_));
 }
 
@@ -1329,6 +1331,7 @@ ListValue::const_iterator ListValue::Find(const Value& value) const {
 }
 
 void ListValue::Swap(ListValue* other) {
+  CHECK(other->is_list());
   list_->swap(*(other->list_));
 }
 
