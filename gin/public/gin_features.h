10fa00e36bf924503801c019a8017cabbde65259
rmcilroy <rmcilroy@chromium.org>
2017-03-07 14:38:18 -0800

    [Gin] Add V8NoTurbo experiment to disable Ignition+TurboFan.
    Adds a V8NoTurbo experiment to disable the new Ignition + TurboFan
    compiler pipeline. Also changes the description of V8Future to refer to
    future V8 execution features rather than the new compilation pipeline
    which will soon be enabled by default.
    
    BUG=chromium:692409
    
    Review-Url: https://codereview.chromium.org/2740543002
    Cr-Commit-Position: refs/heads/master@{#455259}

--- gin/public/gin_features.h
+++ gin/public/gin_features.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace features {
 
 GIN_EXPORT extern const base::Feature kV8Future;
+GIN_EXPORT extern const base::Feature kV8NoTurbo;
 GIN_EXPORT extern const base::Feature kV8IgnitionLowEnd;
 
 }  // namespace features
