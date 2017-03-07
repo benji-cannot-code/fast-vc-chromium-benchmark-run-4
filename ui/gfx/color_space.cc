1355f25b973116a8fdd13b9f34ee47c3ae340fd2
ccameron <ccameron@chromium.org>
2017-03-07 15:05:08 -0800

    HDR: Do nonlinear blending in the compositor
    Specify a color space for blending to the SurfaceAggregator, in addition
    to the output color space. If the color space for blending is not the
    same as the output color space, then add a separate render pass to
    convert from the blending color space to the output color space.
    Add a SurfaceAggregator test for this.
    
    This is used by HDR output, because the output is in linear space, while
    web content expects to be blended according in sRGB value space.
    Add gfx::ColorSpace::CreateExtendedSRGB as the default space in which
    to do blending for HDR content.
    
    Change the meaning of the flag kEnableHDROutput to not enable color
    aware rasterization, only compositing.
    
    Change the GLRenderer to use half-float textures for render passes that
    are output-ing to HDR color spaces. This can't be tested (yet) because
    of Mesa limitations.
    
    R=enne
    TBR=sky (for mash)
    BUG=687357
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2728173004
    Cr-Commit-Position: refs/heads/master@{#455274}

--- ui/gfx/color_space.cc
+++ ui/gfx/color_space.cc
@@ -202,6 +202,12 @@ ColorSpace ColorSpace::CreateSRGB() {
 }
 
 // static
+ColorSpace ColorSpace::CreateExtendedSRGB() {
+  return ColorSpace(PrimaryID::BT709, TransferID::IEC61966_2_1_HDR,
+                    MatrixID::RGB, RangeID::FULL);
+}
+
+// static
 ColorSpace ColorSpace::CreateCustom(const SkMatrix44& to_XYZD50,
                                     const SkColorSpaceTransferFn& fn) {
   ColorSpace result(ColorSpace::PrimaryID::CUSTOM,
@@ -273,7 +279,8 @@ bool ColorSpace::operator==(const ColorSpace& other) const {
 bool ColorSpace::IsHDR() const {
   return transfer_ == TransferID::SMPTEST2084 ||
          transfer_ == TransferID::ARIB_STD_B67 ||
-         transfer_ == TransferID::LINEAR_HDR;
+         transfer_ == TransferID::LINEAR_HDR ||
+         transfer_ == TransferID::IEC61966_2_1_HDR;
 }
 
 bool ColorSpace::operator!=(const ColorSpace& other) const {
@@ -617,6 +624,7 @@ bool ColorSpace::GetTransferFunction(SkColorSpaceTransferFn* fn) const {
       fn->fG = 2.222222222222f;
       return true;
     case ColorSpace::TransferID::IEC61966_2_1:
+    case ColorSpace::TransferID::IEC61966_2_1_HDR:
       fn->fA = 0.947867345704f;
       fn->fB = 0.052132654296f;
       fn->fC = 0.077399380805f;
@@ -650,6 +658,11 @@ bool ColorSpace::GetInverseTransferFunction(SkColorSpaceTransferFn* fn) const {
     return false;
   *fn = SkTransferFnInverse(*fn);
   return true;
+}
+
+bool ColorSpace::HasExtendedSkTransferFn() const {
+  return transfer_ == TransferID::LINEAR_HDR ||
+         transfer_ == TransferID::IEC61966_2_1_HDR;
 }
 
 void ColorSpace::GetTransferMatrix(SkMatrix44* matrix) const {
