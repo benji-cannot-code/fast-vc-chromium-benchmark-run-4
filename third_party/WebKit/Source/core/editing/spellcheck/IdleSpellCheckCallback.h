32339d159d868949ef80bc3e4a9257d1ff3dcf6b
xiaochengh <xiaochengh@chromium.org>
2017-03-07 14:19:07 -0800

    Implement hot mode invocation for idle time spell checker
    This patch implements IdleSpellCheckCallback::hotModeInvocation and
    its helper functions.
    
    BUG=517298
    TEST=existing layout tests in editing/spelling
    
    Review-Url: https://codereview.chromium.org/2720193002
    Cr-Commit-Position: refs/heads/master@{#455248}

--- third_party/WebKit/Source/core/editing/spellcheck/IdleSpellCheckCallback.h
+++ third_party/WebKit/Source/core/editing/spellcheck/IdleSpellCheckCallback.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/dom/IdleRequestCallback.h"
 #include "core/dom/SynchronousMutationObserver.h"
+#include "core/editing/EphemeralRange.h"
+#include "core/editing/Position.h"
 #include "platform/Timer.h"
 
 namespace blink {
@@ -94,7 +96,7 @@ class CORE_EXPORT IdleSpellCheckCallback final
   int m_idleCallbackHandle;
   mutable bool m_needsMoreColdModeInvocationForTesting;
   const Member<LocalFrame> m_frame;
-
+  uint64_t m_lastProcessedUndoStepSequence;
   TaskRunnerTimer<IdleSpellCheckCallback> m_coldModeTimer;
 };
 
