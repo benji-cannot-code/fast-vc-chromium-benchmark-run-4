9a093ae091e4c685c5ac69e19d53414dca0a05ca
noyau <noyau@chromium.org>
2017-03-07 11:40:39 -0800

    Remove Popular Sites experiment on iOS.
    Switch all population to Popular sites.
    
    BUG=698857
    
    Review-Url: https://codereview.chromium.org/2737573003
    Cr-Commit-Position: refs/heads/master@{#455176}

--- components/ntp_tiles/field_trial.h
+++ components/ntp_tiles/field_trial.h
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ntp_tiles {
 
-void SetUpFirstLaunchFieldTrial(bool is_stable_channel);
-
 bool ShouldShowPopularSites();
 
 }  // namespace ntp_tiles
--- ios/chrome/browser/ios_chrome_field_trials.cc
+++ ios/chrome/browser/ios_chrome_field_trials.cc
@@ -13,8 +13,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 void SetupIOSFieldTrials() {
   // Activate the iOS tab eviction dynamic field trials.
   base::FieldTrialList::FindValue("TabEviction");
-
-  // Setup a field trial for a first run experiment on Popular sites.
-  ntp_tiles::SetUpFirstLaunchFieldTrial(GetChannel() ==
-                                        version_info::Channel::STABLE);
 }
