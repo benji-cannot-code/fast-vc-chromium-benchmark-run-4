caa8a3ec303e646ddd2ad3fcd25811f0e5be6ba3
rhalavati <rhalavati@chromium.org>
2017-03-07 01:33:38 -0800

    Network traffic annotation added to remoting/test.
    Network traffic annotation is added to network request of
    remoting/test.
    
    BUG=656607
    
    Review-Url: https://codereview.chromium.org/2735683002
    Cr-Commit-Position: refs/heads/master@{#455049}

--- remoting/test/host_list_fetcher.cc
+++ remoting/test/host_list_fetcher.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/values.h"
 #include "net/http/http_status_code.h"
+#include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
 #include "net/url_request/url_fetcher.h"
 #include "remoting/base/url_request_context_getter.h"
 
@@ -39,8 +40,8 @@ void HostListFetcher::RetrieveHostlist(const std::string& access_token,
       /*network_runner=*/base::ThreadTaskRunnerHandle::Get(),
       /*file_runner=*/base::ThreadTaskRunnerHandle::Get());
 
-  request_ =
-      net::URLFetcher::Create(GURL(target_url), net::URLFetcher::GET, this);
+  request_ = net::URLFetcher::Create(GURL(target_url), net::URLFetcher::GET,
+                                     this, TRAFFIC_ANNOTATION_FOR_TESTS);
   request_->SetRequestContext(request_context_getter_.get());
   request_->AddExtraRequestHeader("Authorization: OAuth " + access_token);
   request_->Start();
