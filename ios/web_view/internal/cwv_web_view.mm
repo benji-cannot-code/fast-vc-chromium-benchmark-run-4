b84a78fa77bf752829db47a0e4b74a8e82e0248a
ichikawa <ichikawa@chromium.org>
2017-03-06 22:27:24 -0800

    Delete initialization of _estimatedProgress with 0.0 per the style guide.
    Review-Url: https://codereview.chromium.org/2734083002
    Cr-Commit-Position: refs/heads/master@{#455042}

--- ios/web_view/internal/cwv_web_view.mm
+++ ios/web_view/internal/cwv_web_view.mm
@@ -64,7 +64,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self = [super initWithFrame:frame];
   if (self) {
     _configuration = [configuration copy];
-    _estimatedProgress = 0.0;
 
     web::WebState::CreateParams webStateCreateParams(
         [configuration.websiteDataStore browserState]);
