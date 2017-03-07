e24425264c7584a063eecd83cedb4efa033de4ee
olivierrobin <olivierrobin@chromium.org>
2017-03-07 12:07:02 -0800

    [Reading List] Expand all Wikipedia sections on distillation.
    Most part of Wikipedia articles is hidden when the page loads.
    DOM distiller will eventually handle this issue, but for the time being, this is a workaround.
    The workaround set the style to block before distilling the page.
    
    BUG=647667, 699215
    
    Review-Url: https://codereview.chromium.org/2730863002
    Cr-Commit-Position: refs/heads/master@{#455186}

--- ios/chrome/browser/reading_list/reading_list_distiller_page.mm
+++ ios/chrome/browser/reading_list/reading_list_distiller_page.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/mac/foundation_util.h"
+#include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "components/favicon/ios/web_favicon_driver.h"
@@ -39,6 +40,13 @@ const char* kGetIframeURLJavaScript =
     "    return link.getAttribute('href');"
     "  }"
     "  return document.getElementsByTagName('iframe')[0].src;"
+    "})()";
+
+const char* kWikipediaWorkaround =
+    "(() => {"
+    "  var s = document.createElement('style');"
+    "  s.innerHTML='.client-js .collapsible-block { display: block }';"
+    "  document.head.appendChild(s);"
     "})()";
 }  // namespace
 
@@ -176,9 +184,16 @@ void ReadingListDistillerPage::DelayedOnLoadURLDone(int delayed_task_id) {
   if (IsGoogleCachedAMPPage()) {
     // Workaround for Google AMP pages.
     HandleGoogleCachedAMPPage();
-  } else {
-    ContinuePageDistillation();
+    return;
   }
+  if (IsWikipediaPage()) {
+    // Workaround for Wikipedia pages.
+    // TODO(crbug.com/647667): remove workaround once DOM distiller handle this
+    // case.
+    HandleWikipediaPage();
+    return;
+  }
+  ContinuePageDistillation();
 }
 
 void ReadingListDistillerPage::ContinuePageDistillation() {
@@ -256,6 +271,28 @@ bool ReadingListDistillerPage::HandleGoogleCachedAMPPageJavaScriptResult(
   web::NavigationManager::WebLoadParams params(new_gurl);
   CurrentWebState()->GetNavigationManager()->LoadURLWithParams(params);
   return true;
+}
+
+bool ReadingListDistillerPage::IsWikipediaPage() {
+  // All wikipedia pages are in the form "https://xxx.m.wikipedia.org/..."
+  const GURL& url = CurrentWebState()->GetLastCommittedURL();
+  if (!url.is_valid() || !url.SchemeIs(url::kHttpsScheme)) {
+    return false;
+  }
+  return (base::EndsWith(url.host(), ".m.wikipedia.org",
+                         base::CompareCase::SENSITIVE));
+}
+
+void ReadingListDistillerPage::HandleWikipediaPage() {
+  base::WeakPtr<ReadingListDistillerPage> weak_this =
+      weak_ptr_factory_.GetWeakPtr();
+  [CurrentWebState()->GetJSInjectionReceiver()
+      executeJavaScript:@(kWikipediaWorkaround)
+      completionHandler:^(id result, NSError* error) {
+        if (weak_this) {
+          weak_this->ContinuePageDistillation();
+        }
+      }];
 }
 
 }  // namespace reading_list
