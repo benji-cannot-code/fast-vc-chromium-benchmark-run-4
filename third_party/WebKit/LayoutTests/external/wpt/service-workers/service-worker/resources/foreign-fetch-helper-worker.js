15d62b063901e16236a64d2873bf0c9e8a738bdf
horo <horo@chromium.org>
2017-03-08 01:43:55 -0800

    Fix foreign fetch wpt tests by setting the path of get-host-info.sub.js
    BUG=688116
    
    Review-Url: https://codereview.chromium.org/2736203002
    Cr-Commit-Position: refs/heads/master@{#455419}

--- third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/foreign-fetch-helper-worker.js
+++ third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/foreign-fetch-helper-worker.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-importScripts('get-host-info.sub.js');
+importScripts('/common/get-host-info.sub.js');
 const host_info = get_host_info();
 
 self.onfetch = e => {
