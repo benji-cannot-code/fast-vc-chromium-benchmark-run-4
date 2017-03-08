208948986443da3d32d6a8805dde498d67323c22
jinsukkim <jinsukkim@chromium.org>
2017-03-08 14:34:18 -0800

    Convert non-WHATWG text encoding to ASCII
    CED is returning text encodings not supported by WHATWG
    standard, which Blink refused to accept. It can cause
    an unexpected bug. This CL converts those encoding to ASCII
    so that raw bytes of the text remain intact.
    
    BUG=698605
    
    Review-Url: https://codereview.chromium.org/2737033003
    Cr-Commit-Position: refs/heads/master@{#455560}

--- third_party/WebKit/Source/platform/text/TextEncodingDetectorTest.cpp
+++ third_party/WebKit/Source/platform/text/TextEncodingDetectorTest.cpp
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-TEST(TextResourceDecoderTest, RespectIso2022Jp) {
+TEST(TextEncodingDetectorTest, RespectIso2022Jp) {
   // ISO-2022-JP is the only 7-bit encoding defined in WHATWG standard.
   std::string iso2022jp =
       " \x1B"
@@ -22,7 +22,7 @@ TEST(TextResourceDecoderTest, RespectIso2022Jp) {
   EXPECT_EQ(WTF::TextEncoding("ISO-2022-JP"), encoding);
 }
 
-TEST(TextResourceDecoderTest, Ignore7BitEncoding) {
+TEST(TextEncodingDetectorTest, Ignore7BitEncoding) {
   // 7-bit encodings except ISO-2022-JP are not supported by WHATWG.
   // They should be detected as plain text (US-ASCII).
   std::string hzGb2312 =
@@ -30,6 +30,19 @@ TEST(TextResourceDecoderTest, Ignore7BitEncoding) {
   WTF::TextEncoding encoding;
   bool result = detectTextEncoding(hzGb2312.c_str(), hzGb2312.length(), nullptr,
                                    nullptr, nullptr, &encoding);
+  EXPECT_TRUE(result);
+  EXPECT_EQ(WTF::TextEncoding("US-ASCII"), encoding);
+}
+
+TEST(TextEncodingDetectorTest, NonWHATWGEncodingBecomesAscii) {
+  std::string pseudoJpg =
+      "\xff\xd8\xff\xe0\x00\x10JFIF foo bar baz\xff\xe1\x00\xa5"
+      "\x01\xd7\xff\x01\x57\x33\x44\x55\x66\x77\xed\xcb\xa9\x87"
+      "\xff\xd7\xff\xe0\x00\x10JFIF foo bar baz\xff\xe1\x00\xa5"
+      "\x87\x01\xd7\xff\x01\x57\x33\x44\x55\x66\x77\xed\xcb\xa9";
+  WTF::TextEncoding encoding;
+  bool result = detectTextEncoding(pseudoJpg.c_str(), pseudoJpg.length(),
+                                   nullptr, nullptr, nullptr, &encoding);
   EXPECT_TRUE(result);
   EXPECT_EQ(WTF::TextEncoding("US-ASCII"), encoding);
 }
