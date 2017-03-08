f30fb2addc60c7fb3c89f0075d819dc012edb081
treib <treib@chromium.org>
2017-03-08 01:34:04 -0800

    Third-party NTPs: Set "instant support" flag earlier.
    This fixes a regression introduced in
    https://codereview.chromium.org/2429523002, which removed some
    unnecessary logging calls that turned out to have unexpected side
    effects.
    
    This CL is a workaround rather than a proper fix; as far as I can tell,
    it'll still be racy, though no worse than it was before. In fact, it's
    kind of incidental that this ever worked at all...
    
    BUG=698675
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2733873002
    Cr-Commit-Position: refs/heads/master@{#455416}

--- chrome/browser/resources/local_ntp/most_visited_util.js
+++ chrome/browser/resources/local_ntp/most_visited_util.js
@@ -8,7 +8,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview Utilities for rendering most visited thumbnails and titles.
  */
 
+// Don't remove; see crbug.com/678778.
 // <include src="instant_iframe_validation.js">
+
+
+/**
+  * The different types of events that are logged from the NTP. The multi-iframe
+  * version of the NTP does *not* actually log any statistics anymore; this is
+  * only required as a workaround for crbug.com/698675.
+  * Note: Keep in sync with common/ntp_logging_events.h
+  * @enum {number}
+  * @const
+  */
+var NTP_LOGGING_EVENT_TYPE = {
+  NTP_ALL_TILES_RECEIVED: 12,
+};
 
 
 /**
@@ -213,6 +227,11 @@ function fillMostVisited(location, fill) {
     };
   } else {
     var apiHandle = chrome.embeddedSearch.newTabPage;
+    // Note: This does not actually result in any logging; it's a workaround for
+    // crbug.com/698675. It effectively sets the "instant support" state of the
+    // tab to true, which makes later calls to fetch the most visited items
+    // succeed.
+    apiHandle.logEvent(NTP_LOGGING_EVENT_TYPE.NTP_ALL_TILES_RECEIVED);
     data = apiHandle.getMostVisitedItemData(params.rid);
     if (!data)
       return;
