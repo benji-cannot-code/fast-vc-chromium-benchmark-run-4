543bc2b45522349ef919cf9818b646e5a3954fd1
pkl <pkl@chromium.org>
2017-03-08 13:24:31 -0800

    Use MakeUnique to create WebStateObserverBridge.
    Review-Url: https://codereview.chromium.org/2733263003
    Cr-Commit-Position: refs/heads/master@{#455543}

--- ios/chrome/browser/itunes_links/itunes_links_observer.mm
+++ ios/chrome/browser/itunes_links/itunes_links_observer.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/logging.h"
+#include "base/memory/ptr_util.h"
 #include "base/strings/sys_string_conversions.h"
 #import "ios/chrome/browser/store_kit/store_kit_tab_helper.h"
 #import "ios/web/public/web_state/web_state.h"
@@ -36,8 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (instancetype)initWithWebState:(web::WebState*)webState {
   self = [super init];
   if (self) {
-    _webStateObserverBridge.reset(
-        new web::WebStateObserverBridge(webState, self));
+    _webStateObserverBridge =
+        base::MakeUnique<web::WebStateObserverBridge>(webState, self);
     _webState = webState;
   }
   return self;
