e08782821a6a956d8aa441c4bbf76e03105f980a
kkhorimoto <kkhorimoto@chromium.org>
2017-03-08 15:01:40 -0800

    Removed CRWSessionEntry unittests.
    This CL also adds a debug description method to NavigationItemImpl and
    transfers CRWSessionEntry's description tests to NavigationItemImpl.
    
    BUG=454984
    
    Review-Url: https://codereview.chromium.org/2699253002
    Cr-Commit-Position: refs/heads/master@{#455576}

--- ios/web/navigation/crw_session_entry.mm
+++ ios/web/navigation/crw_session_entry.mm
@@ -51,18 +51,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (NSString*)description {
-  return [NSString
-      stringWithFormat:
-          @"url:%@ originalurl:%@ title:%@ transition:%d displayState:%@ "
-          @"userAgentType:%s",
-          base::SysUTF8ToNSString(_navigationItem->GetURL().spec()),
-          base::SysUTF8ToNSString(
-              _navigationItem->GetOriginalRequestURL().spec()),
-          base::SysUTF16ToNSString(_navigationItem->GetTitle()),
-          _navigationItem->GetTransitionType(),
-          _navigationItem->GetPageDisplayState().GetDescription(),
-          web::GetUserAgentTypeDescription(_navigationItem->GetUserAgentType())
-              .c_str()];
+#ifndef NDEBUG
+  return _navigationItem->GetDescription();
+#else
+  return [super description];
+#endif
 }
 
 - (web::NavigationItem*)navigationItem {
