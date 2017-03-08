e433daed8998a46ac2944e136d7a9f18ae41003a
alokp <alokp@chromium.org>
2017-03-08 09:01:43 -0800

    Implements JS bindings for mojo data pipe.
    BUG=647036
    
    Review-Url: https://codereview.chromium.org/2732163002
    Cr-Commit-Position: refs/heads/master@{#455477}

--- third_party/WebKit/Source/core/mojo/Mojo.h
+++ third_party/WebKit/Source/core/mojo/Mojo.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class MojoCreateDataPipeOptions;
+class MojoCreateDataPipeResult;
 class MojoCreateMessagePipeResult;
 class MojoCreateSharedBufferResult;
 
@@ -43,6 +45,8 @@ class Mojo final : public GarbageCollected<Mojo>, public ScriptWrappable {
   static const MojoResult kResultShouldWait = MOJO_RESULT_SHOULD_WAIT;
 
   static void createMessagePipe(MojoCreateMessagePipeResult&);
+  static void createDataPipe(const MojoCreateDataPipeOptions&,
+                             MojoCreateDataPipeResult&);
   static void createSharedBuffer(unsigned numBytes,
                                  MojoCreateSharedBufferResult&);
 
