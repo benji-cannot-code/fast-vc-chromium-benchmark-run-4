28ebe70e288ed7dafbd8893c4f9ac5e8571b5797
xiaochengh <xiaochengh@chromium.org>
2017-03-07 18:11:12 -0800

    Implement cold mode invocation for idle time spell checker
    This patch implements IdleSpellCheckCallback::coldModeInvocation and
    its helper functions.
    
    It also removes editing/spelling/focusing-other-frame.html as it's
    not testing anything special (with idle time spell checker), but
    unnecessarily complicates the test logic.
    
    BUG=517298
    TEST=editing/spelling/cold_mode_static_page.html
    editing/spelling/code_mode_type_idle.html
    
    Review-Url: https://codereview.chromium.org/2734013002
    Cr-Commit-Position: refs/heads/master@{#455344}

--- third_party/WebKit/LayoutTests/editing/spelling/spellcheck_test.js
+++ third_party/WebKit/LayoutTests/editing/spelling/spellcheck_test.js
@@ -291,6 +291,8 @@ const kTitle = 'title';
 const kCallback = 'callback';
 /** @type {string} */
 const kIsSpellcheckTest = 'isSpellcheckTest';
+/** @type {string} */
+const kNeedsFullCheck = 'needsFullCheck';
 
 // Spellchecker gets triggered not only by text and selection change, but also
 // by focus change. For example, misspelling markers in <INPUT> disappear when
@@ -358,9 +360,13 @@ function invokeSpellcheckTest(testObject, input, tester, expectedText) {
       });
     };
 
-    if (internals.idleTimeSpellCheckerState !== undefined &&
-        internals.idleTimeSpellCheckerState(sample.document) === 'HotModeRequested') {
+    if (internals.idleTimeSpellCheckerState !== undefined) {
+      if (internals.idleTimeSpellCheckerState(sample.document) === 'HotModeRequested')
         internals.runIdleTimeSpellChecker(sample.document);
+      if (testObject.properties[kNeedsFullCheck]) {
+        while (internals.idleTimeSpellCheckerState(sample.document) !== 'Inactive')
+          internals.runIdleTimeSpellChecker(sample.document);
+      }
     }
 
     // For a test that does not create new spell check request, a synchronous
@@ -408,7 +414,7 @@ add_result_callback(testObj => {
 function getTestArguments(passedArgs) {
   const args = {};
   args[kIsSpellcheckTest] = true;
-  [kTitle, kCallback].forEach(key => args[key] = undefined);
+  [kTitle, kCallback, kNeedsFullCheck].forEach(key => args[key] = undefined);
   if (!passedArgs)
     return args;
 
@@ -417,7 +423,8 @@ function getTestArguments(passedArgs) {
     return args;
   }
 
-  [kTitle, kCallback].forEach(key => args[key] = passedArgs[key]);
+  [kTitle, kCallback, kNeedsFullCheck].forEach(
+      key => args[key] = passedArgs[key]);
   return args;
 }
 
