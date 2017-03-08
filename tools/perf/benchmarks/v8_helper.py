6efb6a228fed25845b41872eff8e54f2bdcc410b
Mythri Alle <mythria@chromium.org>
2017-03-08 15:35:21 +0000

    Removes the following benchmarks: * speedometer-ignition * v8.browse_desktop_ignition * v8.browse_mobile_ignition, * v8.infiniter_scroll-ignition_tbmv2 * v8.todoMVC-ignition
    The ignition-staging configuration of v8 used in these benchmarks is no longer the
    shipping configuration. The corresponding turbo benchmarks are tracking the future
    shipping configuration of v8.
    
    BUG=chromium:670087
    
    Change-Id: Ic7b8d8e69ec2f1aafff750fa07c516d90824e611
    Reviewed-on: https://chromium-review.googlesource.com/451657
    Commit-Queue: Mythri Alle <mythria@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#455709}

--- tools/perf/benchmarks/v8_helper.py
+++ tools/perf/benchmarks/v8_helper.py
@@ -6,10 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 _JS_FLAGS_SWITCH = '--js-flags='
 
 
-def EnableIgnition(options):
-  AppendJSFlags(options, '--ignition-staging')
-
-
 def EnableTurbo(options):
   AppendJSFlags(options, '--ignition-staging')
   AppendJSFlags(options, '--turbo')
