ef1e1e3179dd81e11679a3636922ae66cce68caa
glebl <glebl@chromium.org>
2017-03-08 00:27:08 -0800

    Add ShowLayoutOpportunityTree to NGLayoutOpportunityIterator
    ShowLayoutOpportunityTree is used for debug purposes.
    
    BUG=635619
    
    Review-Url: https://codereview.chromium.org/2731893003
    Cr-Commit-Position: refs/heads/master@{#455400}

--- third_party/WebKit/Source/core/layout/ng/ng_layout_opportunity_iterator.h
+++ third_party/WebKit/Source/core/layout/ng/ng_layout_opportunity_iterator.h
@@ -39,6 +39,11 @@ class CORE_EXPORT NGLayoutOpportunityIterator final {
   // TODO(chrome-layout-team): Refactor with using C++ <iterator> library.
   const NGLayoutOpportunity Next();
 
+#ifndef NDEBUG
+  // Prints Layout Opportunity tree for debug purposes.
+  void ShowLayoutOpportunityTree() const;
+#endif
+
  private:
   // Mutable Getters.
   NGLayoutOpportunityTreeNode* MutableOpportunityTreeRoot() {
