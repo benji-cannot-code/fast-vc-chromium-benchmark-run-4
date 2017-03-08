cb16c623a8ad52e0271e3cd78aa6d5a468f956b2
bsazonov <bsazonov@chromium.org>
2017-03-08 05:26:29 -0800

    Removed MockAccountManager.postAsyncAccountChangedEvent() and related methods
    This CL removes code for sending LOGIN_ACCOUNTS_CHANGED_ACTION broadcasts from
    MockAccountManager and clears up related methods.
    Since MockAccountManager contains it's own fake accounts, sending broadcasts
    after adding/removing these accounts seems a bit illogical. Moreover, there's
    only one call to removeAccountHolderExplicitly that actually passes 'true' to
    the 'broadcastEvent' parameter. It looks like this broadcasts-related calls
    are remnants of the old MockAccountManager implementation that was mimicking
    android.Accounts.AccountManager.
    
    BUG=NONE
    
    Review-Url: https://codereview.chromium.org/2725663002
    Cr-Commit-Position: refs/heads/master@{#455442}

--- components/signin/core/browser/android/javatests/src/org/chromium/components/signin/test/util/MockAccountManager.java
+++ components/signin/core/browser/android/javatests/src/org/chromium/components/signin/test/util/MockAccountManager.java
@@ -6,11 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.components.signin.test.util;
 
 import android.accounts.Account;
-import android.accounts.AccountManager;
 import android.accounts.AuthenticatorDescription;
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.os.AsyncTask;
 
 import org.chromium.base.Callback;
@@ -99,45 +97,24 @@ public class MockAccountManager implements AccountManagerDelegate {
         mGetAccountsTaskCounter.waitUntilZero();
     }
 
-    @VisibleForTesting
-    public boolean addAccountHolderExplicitly(AccountHolder accountHolder) {
-        return addAccountHolderExplicitly(accountHolder, false);
-    }
-
     /**
      * Add an AccountHolder directly.
      *
      * @param accountHolder the account holder to add
-     * @param broadcastEvent whether to broadcast an AccountChangedEvent
      * @return whether the account holder was added successfully
      */
-    public boolean addAccountHolderExplicitly(AccountHolder accountHolder, boolean broadcastEvent) {
-        boolean result = mAccounts.add(accountHolder);
-        if (broadcastEvent) {
-            postAsyncAccountChangedEvent();
-        }
-        return result;
-    }
-
-    @VisibleForTesting
-    public boolean removeAccountHolderExplicitly(AccountHolder accountHolder) {
-        return removeAccountHolderExplicitly(accountHolder, false);
+    public boolean addAccountHolderExplicitly(AccountHolder accountHolder) {
+        return mAccounts.add(accountHolder);
     }
 
     /**
      * Remove an AccountHolder directly.
      *
      * @param accountHolder the account holder to remove
-     * @param broadcastEvent whether to broadcast an AccountChangedEvent
      * @return whether the account holder was removed successfully
      */
-    public boolean removeAccountHolderExplicitly(
-            AccountHolder accountHolder, boolean broadcastEvent) {
-        boolean result = mAccounts.remove(accountHolder);
-        if (broadcastEvent) {
-            postAsyncAccountChangedEvent();
-        }
-        return result;
+    public boolean removeAccountHolderExplicitly(AccountHolder accountHolder) {
+        return mAccounts.remove(accountHolder);
     }
 
     @Override
@@ -226,17 +203,6 @@ public class MockAccountManager implements AccountManagerDelegate {
             }
         }
         throw new IllegalArgumentException("Can not find AccountHolder for account " + account);
-    }
-
-    private void postAsyncAccountChangedEvent() {
-        // Mimic that this does not happen on the main thread.
-        new AsyncTask<Void, Void, Void>() {
-            @Override
-            protected Void doInBackground(Void... params) {
-                mContext.sendBroadcast(new Intent(AccountManager.LOGIN_ACCOUNTS_CHANGED_ACTION));
-                return null;
-            }
-        }.execute();
     }
 
     /**
