4a6ef80c9db2f9a47844c520ec07831eb38452f0
machenbach <machenbach@chromium.org>
2017-03-09 01:13:51 -0800

    Revert of Improve performance of GeometryMapper cache. (patchset #39 id:690001 of https://codereview.chromium.org/2729243002/ )
    Reason for revert:
    Breaks msan:
    https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux%20Trusty%20MSAN/builds/781
    
    Original issue's description:
    > Improve performance of GeometryMapper cache.
    >
    > 1. Store clips and transforms in vectors attached to the clip and transform nodes.
    > This avoids all hash map lookups. Invalidation uses a global cache generation id
    > (currently we don't support anything other than global invalidation of the
    > GeometryMapper caches).
    >
    > 2. Return clips and transforms by reference, to avoid copying.
    >
    > #1 is expected to be a good optimization because we almost always compute clips
    > and transforms relative to the same ancestor, so the vectors are short and hash
    > lookups are reduced.
    >
    > BUG=692614
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    >
    > Review-Url: https://codereview.chromium.org/2729243002
    > Cr-Commit-Position: refs/heads/master@{#455571}
    > Committed: https://chromium.googlesource.com/chromium/src/+/ee72c4b5d2fd1e4882bddca66e0f89fe8784a7fc
    
    TBR=pdr@chromium.org,wangxianzhu@chromium.org,chrishtr@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=692614
    
    Review-Url: https://codereview.chromium.org/2738113003
    Cr-Commit-Position: refs/heads/master@{#455706}

--- third_party/WebKit/Source/platform/graphics/paint/GeometryMapper.cpp
+++ third_party/WebKit/Source/platform/graphics/paint/GeometryMapper.cpp
@@ -215,6 +215,31 @@ FloatRect GeometryMapper::ancestorToLocalRect(
   return transformMatrix.inverse().mapRect(rect);
 }
 
+GeometryMapper::TransformCache& GeometryMapper::getTransformCache(
+    const TransformPaintPropertyNode* ancestor) {
+  auto addResult = m_transformCache.insert(ancestor, nullptr);
+  if (addResult.isNewEntry)
+    addResult.storedValue->value = WTF::wrapUnique(new TransformCache);
+  return *addResult.storedValue->value;
+}
+
+GeometryMapper::ClipCache& GeometryMapper::getClipCache(
+    const ClipPaintPropertyNode* ancestorClip,
+    const TransformPaintPropertyNode* ancestorTransform) {
+  auto addResultTransform = m_clipCache.insert(ancestorClip, nullptr);
+  if (addResultTransform.isNewEntry) {
+    addResultTransform.storedValue->value =
+        WTF::wrapUnique(new TransformToClip);
+  }
+
+  auto addResultClip =
+      addResultTransform.storedValue->value->insert(ancestorTransform, nullptr);
+  if (addResultClip.isNewEntry)
+    addResultClip.storedValue->value = WTF::wrapUnique(new ClipCache);
+
+  return *addResultClip.storedValue->value;
+}
+
 FloatClipRect GeometryMapper::localToAncestorClipRect(
     const PropertyTreeState& localState,
     const PropertyTreeState& ancestorState) {
@@ -250,8 +275,7 @@ FloatClipRect GeometryMapper::sourceToDestinationClipRectInternal(
   if (success)
     return result;
 
-  // Otherwise first map to the lowest common ancestor, then map to
-  // destination.
+  // Otherwise first map to the lowest common ancestor, then map to destination.
   const TransformPaintPropertyNode* lcaTransform = lowestCommonAncestor(
       sourceState.transform(), destinationState.transform());
   DCHECK(lcaTransform);
@@ -268,9 +292,9 @@ FloatClipRect GeometryMapper::sourceToDestinationClipRectInternal(
     if (!RuntimeEnabledFeatures::slimmingPaintV2Enabled()) {
       // On SPv1 we may fail when the paint invalidation container creates an
       // overflow clip (in ancestorState) which is not in localState of an
-      // out-of-flow positioned descendant. See crbug.com/513108 and layout
-      // test compositing/overflow/handle-non-ancestor-clip-parent.html (run
-      // with --enable-prefer-compositing-to-lcd-text) for details.
+      // out-of-flow positioned descendant. See crbug.com/513108 and layout test
+      // compositing/overflow/handle-non-ancestor-clip-parent.html (run with
+      // --enable-prefer-compositing-to-lcd-text) for details.
       // Ignore it for SPv1 for now.
       success = true;
     }
@@ -284,7 +308,7 @@ FloatClipRect GeometryMapper::sourceToDestinationClipRectInternal(
   return result;
 }
 
-const FloatClipRect& GeometryMapper::localToAncestorClipRectInternal(
+FloatClipRect GeometryMapper::localToAncestorClipRectInternal(
     const ClipPaintPropertyNode* descendant,
     const ClipPaintPropertyNode* ancestorClip,
     const TransformPaintPropertyNode* ancestorTransform,
@@ -292,29 +316,28 @@ const FloatClipRect& GeometryMapper::localToAncestorClipRectInternal(
   FloatClipRect clip;
   if (descendant == ancestorClip) {
     success = true;
-    return m_infiniteClip;
+    // Return an infinite clip.
+    return clip;
   }
 
+  ClipCache& clipCache = getClipCache(ancestorClip, ancestorTransform);
   const ClipPaintPropertyNode* clipNode = descendant;
   Vector<const ClipPaintPropertyNode*> intermediateNodes;
 
-  GeometryMapperClipCache::ClipAndTransform clipAndTransform(ancestorClip,
-                                                             ancestorTransform);
   // Iterate over the path from localState.clip to ancestorState.clip. Stop if
   // we've found a memoized (precomputed) clip for any particular node.
   while (clipNode && clipNode != ancestorClip) {
-    if (const FloatClipRect* cachedClip =
-            clipNode->getClipCache().getCachedClip(clipAndTransform)) {
-      clip = *cachedClip;
+    auto it = clipCache.find(clipNode);
+    if (it != clipCache.end()) {
+      clip = it->value;
       break;
     }
-
     intermediateNodes.push_back(clipNode);
     clipNode = clipNode->parent();
   }
   if (!clipNode) {
     success = false;
-    return m_infiniteClip;
+    return clip;
   }
 
   // Iterate down from the top intermediate node found in the previous loop,
@@ -325,20 +348,16 @@ const FloatClipRect& GeometryMapper::localToAncestorClipRectInternal(
     const TransformationMatrix& transformMatrix = localToAncestorMatrixInternal(
         (*it)->localTransformSpace(), ancestorTransform, success);
     if (!success)
-      return m_infiniteClip;
+      return clip;
     FloatRect mappedRect = transformMatrix.mapRect((*it)->clipRect().rect());
     clip.intersect(mappedRect);
     if ((*it)->clipRect().isRounded())
       clip.setHasRadius();
-    (*it)->getClipCache().setCachedClip(clipAndTransform, clip);
+    clipCache.set(*it, clip);
   }
 
   success = true;
-
-  const FloatClipRect* cachedClip =
-      descendant->getClipCache().getCachedClip(clipAndTransform);
-  DCHECK(cachedClip);
-  return *cachedClip;
+  return clipCache.find(descendant)->value;
 }
 
 const TransformationMatrix& GeometryMapper::localToAncestorMatrix(
@@ -360,6 +379,8 @@ const TransformationMatrix& GeometryMapper::localToAncestorMatrixInternal(
     return m_identity;
   }
 
+  TransformCache& transformCache = getTransformCache(ancestorTransformNode);
+
   const TransformPaintPropertyNode* transformNode = localTransformNode;
   Vector<const TransformPaintPropertyNode*> intermediateNodes;
   TransformationMatrix transformMatrix;
@@ -368,13 +389,11 @@ const TransformationMatrix& GeometryMapper::localToAncestorMatrixInternal(
   // Stop if we've found a memoized (precomputed) transform for any particular
   // node.
   while (transformNode && transformNode != ancestorTransformNode) {
-    if (const TransformationMatrix* cachedMatrix =
-            transformNode->getTransformCache().getCachedTransform(
-                ancestorTransformNode)) {
-      transformMatrix = *cachedMatrix;
+    auto it = transformCache.find(transformNode);
+    if (it != transformCache.end()) {
+      transformMatrix = it->value;
       break;
     }
-
     intermediateNodes.push_back(transformNode);
     transformNode = transformNode->parent();
   }
@@ -390,20 +409,15 @@ const TransformationMatrix& GeometryMapper::localToAncestorMatrixInternal(
     TransformationMatrix localTransformMatrix = (*it)->matrix();
     localTransformMatrix.applyTransformOrigin((*it)->origin());
     transformMatrix = transformMatrix * localTransformMatrix;
-    (*it)->getTransformCache().setCachedTransform(ancestorTransformNode,
-                                                  transformMatrix);
+    transformCache.set(*it, transformMatrix);
   }
   success = true;
-  const TransformationMatrix* cachedMatrix =
-      localTransformNode->getTransformCache().getCachedTransform(
-          ancestorTransformNode);
-  DCHECK(cachedMatrix);
-  return *cachedMatrix;
+  return transformCache.find(localTransformNode)->value;
 }
 
 void GeometryMapper::clearCache() {
-  GeometryMapperTransformCache::clearCache();
-  GeometryMapperClipCache::clearCache();
+  m_transformCache.clear();
+  m_clipCache.clear();
 }
 
 namespace {
