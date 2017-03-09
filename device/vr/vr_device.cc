bbb7521d99bc73e1253c1c8e6a1efc15952521b0
sammc <sammc@chromium.org>
2017-03-08 19:11:35 -0800

    Revert of WebVR compositor bypass via BrowserMain context + mailbox (patchset #32 id:610001 of https://codereview.chromium.org/2729523002/ )
    Reason for revert:
    Speculative revert for several failing vr layout tests:
    https://luci-milo.appspot.com/buildbot/chromium.webkit/WebKit%20Mac10.11/17274
    https://luci-milo.appspot.com/buildbot/chromium.webkit/WebKit%20Mac10.11%20%28retina%29/13252
    https://luci-milo.appspot.com/buildbot/chromium.webkit/WebKit%20Mac10.10/31124
    https://luci-milo.appspot.com/buildbot/chromium.webkit/WebKit%20Mac10.9/43536
    https://luci-milo.appspot.com/buildbot/chromium.webkit/WebKit%20Win7/50908
    https://luci-milo.appspot.com/buildbot/chromium.webkit/WebKit%20Win10/20452
    
    Original issue's description:
    > WebVR compositor bypass via BrowserMain context + mailbox
    >
    > Implement compositor bypass for WebVR via mailbox copy:
    >
    > - Create a GPU command buffer GL context in the VrShellGl thread that's
    >   backed by a native Surface. This is encapsulated inside the new
    >   MailboxToSurfaceBridge class.
    >
    > - WebKit's VRDisplay sends a MailboxHolder for each frame (as drawn
    >   via WebGL on the GPU process) as argument to the SubmitFrame mojo
    >   call.
    >
    > - MailboxToSurfaceLayerBridge uses its GPU command buffer GL context
    >   to draw the mailbox content to a Surface.
    >
    > - VrShellGl picks up the frame from the corresponding SurfaceTexture
    >   and  proceeds with rendering as usual with its native GL context
    >   as needed for the low-level GVR API.
    >
    > Also implements on-the-fly canvas resizing, the transfer surface and
    > GVR renderbuffer are resized as needed to match the source canvas.
    >
    > Added frame number as an argument to various TRACE_EVENTs for timing
    > analysis.
    >
    > It includes two separate modes for frame scheduling, the more
    > aggressive one is conditional on
    > chrome:://flags#enable-webvr-experimental-rendering since the
    > framerate seems a bit less stable.
    >
    > BUG=698643
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:closure_compilation;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    >
    > Review-Url: https://codereview.chromium.org/2729523002
    > Cr-Commit-Position: refs/heads/master@{#455592}
    > Committed: https://chromium.googlesource.com/chromium/src/+/6280ec5b6e3b4c307926eb1bf0167acc2a0d4a74
    
    TBR=mthiesse@chromium.org,bajones@chromium.org,khushalsagar@chromium.org,dcheng@chromium.org,piman@chromium.org,klausw@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=698643
    
    Review-Url: https://codereview.chromium.org/2739973002
    Cr-Commit-Position: refs/heads/master@{#455653}

--- device/vr/vr_device.cc
+++ device/vr/vr_device.cc
@@ -20,6 +20,12 @@ VRDevice::VRDevice()
 
 VRDevice::~VRDevice() {}
 
+void VRDevice::RequestPresent(const base::Callback<void(bool)>& callback) {
+  callback.Run(true);
+}
+
+void VRDevice::SetSecureOrigin(bool secure_origin) {}
+
 void VRDevice::AddDisplay(VRDisplayImpl* display) {
   displays_.insert(display);
 }
