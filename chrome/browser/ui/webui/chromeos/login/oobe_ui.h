499c725580689ed53131a40525deb027cf162c6f
jdufault <jdufault@chromium.org>
2017-03-09 11:31:36 -0800

    cros: Remove CoreOobeHandler::Delegate.
    It was only used for communication within the JS layer, and CoreOobeHandler already has access to the only implementer, OobeUI.
    
    BUG=685287
    
    Review-Url: https://codereview.chromium.org/2731103002
    Cr-Commit-Position: refs/heads/master@{#455829}

--- chrome/browser/ui/webui/chromeos/login/oobe_ui.h
+++ chrome/browser/ui/webui/chromeos/login/oobe_ui.h
@@ -68,7 +68,6 @@ class WrongHWIDScreenView;
 // - eula screen (CrOS (+ OEM) EULA content/TPM password/crash reporting).
 // - update screen.
 class OobeUI : public content::WebUIController,
-               public CoreOobeHandler::Delegate,
                public ShutdownPolicyHandler::Delegate {
  public:
   // List of known types of OobeUI. Type added as path in chrome://oobe url, for
@@ -128,6 +127,9 @@ class OobeUI : public content::WebUIController,
   // Initializes the handlers.
   void InitializeHandlers();
 
+  // Called when the screen has changed.
+  void CurrentScreenChanged(OobeScreen screen);
+
   // Invoked after the async assets load. The screen handler that has the same
   // async assets load id will be initialized.
   void OnScreenAssetsLoaded(const std::string& async_assets_load_id);
@@ -184,9 +186,6 @@ class OobeUI : public content::WebUIController,
 
   void AddWebUIHandler(std::unique_ptr<BaseWebUIHandler> handler);
   void AddScreenHandler(std::unique_ptr<BaseScreenHandler> handler);
-
-  // CoreOobeHandler::Delegate implementation:
-  void OnCurrentScreenChanged(OobeScreen screen) override;
 
   // Type of UI.
   std::string display_type_;
