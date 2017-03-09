5d26da46965a5126f774367c4e5908aa61fe2ac6
chrishtr <chrishtr@chromium.org>
2017-03-08 16:33:32 -0800

    Set an initial background for RenderFrameHosts during commit.
    This was originally implemented in https://codereview.chromium.org/2466413009,
    but was broken by https://codereview.chromium.org/2496233003. Now re-adding
    with more explicit code, plus a unittest.
    
    BUG=470669
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2733093002
    Cr-Commit-Position: refs/heads/master@{#455612}

--- content/browser/frame_host/render_frame_host_manager_unittest.cc
+++ content/browser/frame_host/render_frame_host_manager_unittest.cc
@@ -935,6 +935,8 @@ TEST_F(RenderFrameHostManagerTest, Navigate) {
   EXPECT_FALSE(host->GetSiteInstance()->HasSite());
   host->GetSiteInstance()->SetSite(kUrl1);
 
+  manager->GetRenderWidgetHostView()->SetBackgroundColor(SK_ColorRED);
+
   // 2) Navigate to next site. -------------------------
   const GURL kUrl2("http://www.google.com/foo");
   NavigationEntryImpl entry2(
@@ -953,6 +955,9 @@ TEST_F(RenderFrameHostManagerTest, Navigate) {
   EXPECT_TRUE(host == manager->current_frame_host());
   ASSERT_TRUE(host);
   EXPECT_TRUE(host->GetSiteInstance()->HasSite());
+
+  EXPECT_EQ(SK_ColorRED,
+            manager->GetRenderWidgetHostView()->background_color());
 
   // 3) Cross-site navigate to next site. --------------
   const GURL kUrl3("http://webkit.org/");
@@ -979,6 +984,9 @@ TEST_F(RenderFrameHostManagerTest, Navigate) {
 
   // We should observe RVH changed event.
   EXPECT_TRUE(change_observer.DidHostChange());
+
+  EXPECT_EQ(SK_ColorRED,
+            manager->GetRenderWidgetHostView()->background_color());
 }
 
 // Tests WebUI creation.
