684d8e9c69cdbf84d87d9ca5a4fc32b67ef86c28
rhalavati <rhalavati@chromium.org>
2017-03-09 05:56:37 -0800

    Network traffic annotation added to sync.
    Network traffic annotation is added to network request of sync.
    
    BUG=656607
    
    Review-Url: https://codereview.chromium.org/2707363004
    Cr-Commit-Position: refs/heads/master@{#455732}

--- components/sync/driver/sync_stopped_reporter.cc
+++ components/sync/driver/sync_stopped_reporter.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/protocol/sync.pb.h"
 #include "net/base/load_flags.h"
 #include "net/http/http_status_code.h"
+#include "net/traffic_annotation/network_traffic_annotation.h"
 
 namespace {
 
@@ -62,8 +63,29 @@ void SyncStoppedReporter::ReportSyncStopped(const std::string& access_token,
   std::string msg;
   event_request.SerializeToString(&msg);
 
-  fetcher_ =
-      net::URLFetcher::Create(sync_event_url_, net::URLFetcher::POST, this);
+  net::NetworkTrafficAnnotationTag traffic_annotation =
+      net::DefineNetworkTrafficAnnotation("sync_stop_reporter", R"(
+        semantics {
+          sender: "Chrome Sync"
+          description:
+            "A network request to inform Chrome Sync that sync has been "
+            "disabled for this device."
+          trigger: "User disables sync."
+          data: "Sync device identifier and metadata."
+          destination: GOOGLE_OWNED_SERVICE
+        }
+        policy {
+          cookies_allowed: false
+          setting: "This feature cannot be disabled by settings."
+          chrome_policy {
+            SyncDisabled {
+              policy_options {mode: MANDATORY}
+              SyncDisabled: true
+            }
+          }
+        })");
+  fetcher_ = net::URLFetcher::Create(sync_event_url_, net::URLFetcher::POST,
+                                     this, traffic_annotation);
   fetcher_->AddExtraRequestHeader(base::StringPrintf(
       "%s: Bearer %s", net::HttpRequestHeaders::kAuthorization,
       access_token.c_str()));
--- components/sync/engine_impl/attachments/attachment_downloader_impl.cc
+++ components/sync/engine_impl/attachments/attachment_downloader_impl.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_response_headers.h"
 #include "net/http/http_status_code.h"
 #include "net/http/http_util.h"
+#include "net/traffic_annotation/network_traffic_annotation.h"
 #include "net/url_request/url_fetcher.h"
 #include "net/url_request/url_request_status.h"
 
@@ -215,8 +216,35 @@ void AttachmentDownloaderImpl::OnURLFetchComplete(
 std::unique_ptr<net::URLFetcher> AttachmentDownloaderImpl::CreateFetcher(
     const AttachmentUrl& url,
     const std::string& access_token) {
-  std::unique_ptr<net::URLFetcher> url_fetcher =
-      net::URLFetcher::Create(GURL(url), net::URLFetcher::GET, this);
+  net::NetworkTrafficAnnotationTag traffic_annotation =
+      net::DefineNetworkTrafficAnnotation("sync_attachment_downloader", R"(
+        semantics {
+          sender: "Chrome Sync"
+          description:
+            "Chrome Sync synchronizes profile data between Chromium clients "
+            "and Google for a given user account."
+          trigger:
+            "User makes a change to syncable profile data after enabling sync "
+            "on the device."
+          data:
+            "The device and user identifiers, along with any profile data that "
+            "is changing."
+          destination: GOOGLE_OWNED_SERVICE
+        }
+        policy {
+          cookies_allowed: false
+          setting:
+            "Users can disable Chrome Sync by going into the profile settings "
+            "and choosing to Sign Out."
+          chrome_policy {
+            SyncDisabled {
+              policy_options {mode: MANDATORY}
+              SyncDisabled: true
+            }
+          }
+        })");
+  std::unique_ptr<net::URLFetcher> url_fetcher = net::URLFetcher::Create(
+      GURL(url), net::URLFetcher::GET, this, traffic_annotation);
   AttachmentUploaderImpl::ConfigureURLFetcherCommon(
       url_fetcher.get(), access_token, raw_store_birthday_, model_type_,
       url_request_context_getter_.get());
