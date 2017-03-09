ab19e620fcaa777f27b27be77221e1c5f19a7b83
michaeldo <michaeldo@chromium.org>
2017-03-08 17:11:16 -0800

    Add ios_web_view_shell_egtests target and accessibility labels to shell.
    BUG=698657
    
    Review-Url: https://codereview.chromium.org/2736243003
    Cr-Commit-Position: refs/heads/master@{#455618}

--- ios/web_view/shell/shell_view_controller.h
+++ ios/web_view/shell/shell_view_controller.h
@@ -11,6 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web_view/public/cwv_navigation_delegate.h"
 #import "ios/web_view/public/cwv_ui_delegate.h"
 
+// Accessibility label added to the back button.
+extern NSString* const kWebViewShellBackButtonAccessibilityLabel;
+// Accessibility label added to the forward button.
+extern NSString* const kWebViewShellForwardButtonAccessibilityLabel;
+// Accessibility label added to the URL address text field.
+extern NSString* const kWebViewShellAddressFieldAccessibilityLabel;
 // Accessibility identifier added to the text field of JavaScript prompts.
 extern NSString* const
     kWebViewShellJavaScriptDialogTextFieldAccessibiltyIdentifier;
--- ios/web_view/shell/shell_view_controller.m
+++ ios/web_view/shell/shell_view_controller.m
@@ -17,6 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 // Externed accessibility identifier.
+NSString* const kWebViewShellBackButtonAccessibilityLabel = @"Back";
+NSString* const kWebViewShellForwardButtonAccessibilityLabel = @"Forward";
+NSString* const kWebViewShellAddressFieldAccessibilityLabel = @"Address field";
 NSString* const kWebViewShellJavaScriptDialogTextFieldAccessibiltyIdentifier =
     @"WebViewShellJavaScriptDialogTextFieldAccessibiltyIdentifier";
 
@@ -89,6 +92,7 @@ NSString* const kWebViewShellJavaScriptDialogTextFieldAccessibiltyIdentifier =
   [_field setKeyboardType:UIKeyboardTypeWebSearch];
   [_field setAutocorrectionType:UITextAutocorrectionTypeNo];
   [_field setClearButtonMode:UITextFieldViewModeWhileEditing];
+  [_field setAccessibilityLabel:kWebViewShellAddressFieldAccessibilityLabel];
 
   // Set up the toolbar buttons.
   // Back.
@@ -102,6 +106,7 @@ NSString* const kWebViewShellJavaScriptDialogTextFieldAccessibiltyIdentifier =
   [back addTarget:self
                 action:@selector(back)
       forControlEvents:UIControlEventTouchUpInside];
+  [back setAccessibilityLabel:kWebViewShellBackButtonAccessibilityLabel];
 
   // Forward.
   UIButton* forward = [UIButton buttonWithType:UIButtonTypeCustom];
@@ -113,6 +118,7 @@ NSString* const kWebViewShellJavaScriptDialogTextFieldAccessibiltyIdentifier =
   [forward addTarget:self
                 action:@selector(forward)
       forControlEvents:UIControlEventTouchUpInside];
+  [forward setAccessibilityLabel:kWebViewShellForwardButtonAccessibilityLabel];
 
   // Stop.
   UIButton* stop = [UIButton buttonWithType:UIButtonTypeCustom];
