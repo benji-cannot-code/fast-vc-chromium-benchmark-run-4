c1600823bd56853208fb7bb622198a1a001679e2
dcheng <dcheng@chromium.org>
2017-03-09 03:21:47 -0800

    Move asserts for tracking global object detachment to WindowProxy.
    RemoteWindowProxy won't have a ScriptState in the future, so move the
    DCHECKs so it's possible to continue asserting invariants that should
    always hold true in WindowProxy.
    
    BUG=527190
    
    Review-Url: https://codereview.chromium.org/2744483002
    Cr-Commit-Position: refs/heads/master@{#455715}

--- third_party/WebKit/Source/bindings/core/v8/LocalWindowProxy.cpp
+++ third_party/WebKit/Source/bindings/core/v8/LocalWindowProxy.cpp
@@ -93,6 +93,10 @@ void LocalWindowProxy::disposeContext(GlobalDetachmentBehavior behavior) {
     }
     V8DOMWrapper::clearNativeInfo(isolate(), context->Global());
     m_scriptState->detachGlobalObject();
+
+#if DCHECK_IS_ON()
+    didDetachGlobalObject();
+#endif
   }
 
   m_scriptState->disposePerContextData();
@@ -187,6 +191,10 @@ void LocalWindowProxy::createContext() {
                          m_globalProxy.newLocal(isolate()));
   }
   CHECK(!context.IsEmpty());
+
+#if DCHECK_IS_ON()
+  didAttachGlobalObject();
+#endif
 
   m_scriptState = ScriptState::create(context, m_world);
 
