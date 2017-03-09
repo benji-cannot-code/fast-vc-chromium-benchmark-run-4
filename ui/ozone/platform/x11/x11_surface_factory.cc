a49ffcb768e39d2b6fa81da8e3d52fe51414f7a5
rockot <rockot@chromium.org>
2017-03-08 23:06:41 -0800

    Revert of Finish OSMesa support for Ozone X11. (patchset #2 id:40001 of https://codereview.chromium.org/2730993002/ )
    Reason for revert:
    Sorry, speculatively reverting based on persistent ozone builder flake. Started with this CL as best as I can tell.
    
    Appears to be a sync command buffer IPC hang on shutdown, affecting only tests which force single-process mode. Possibly a deadlock caused by ... something in the OSMesa/X11?
    
    https://luci-logdog.appspot.com/v/?s=chromium%2Fbb%2Ftryserver.chromium.linux%2Flinux_chromium_chromeos_ozone_rel_ng%2F336922%2F%2B%2Frecipes%2Fsteps%2Fcontent_browsertests__with_patch_%2F0%2Flogs%2FSavableResourcesTest.GetSavableResourceLinksWithPageHasInvalidLinks%2F0
    
    Original issue's description:
    > Finish OSMesa support for Ozone X11.
    >
    > Modify the GLOzone implementation for Ozone X11 OSMesa to use
    > GLSurfaceOSMesaX11. This copies the bitmap that OSMesa generated to the
    > XWindow so it's visible.
    >
    > BUG=698780
    >
    > Review-Url: https://codereview.chromium.org/2730993002
    > Cr-Commit-Position: refs/heads/master@{#455306}
    > Committed: https://chromium.googlesource.com/chromium/src/+/1df13216e40fed4c8fa2531fc4c764eca1aae018
    
    TBR=kbr@chromium.org,kylechar@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=698780
    
    Review-Url: https://codereview.chromium.org/2742713002
    Cr-Commit-Position: refs/heads/master@{#455684}

--- ui/ozone/platform/x11/x11_surface_factory.cc
+++ ui/ozone/platform/x11/x11_surface_factory.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/x/x11_types.h"
 #include "ui/gl/egl_util.h"
 #include "ui/gl/gl_surface_egl.h"
-#include "ui/gl/gl_surface_osmesa_x11.h"
 #include "ui/ozone/common/egl_util.h"
 #include "ui/ozone/common/gl_ozone_egl.h"
 #include "ui/ozone/common/gl_ozone_osmesa.h"
@@ -125,13 +124,11 @@ GLSurfaceEGLOzoneX11::~GLSurfaceEGLOzoneX11() {
   Destroy();
 }
 
-// GLOzoneEGL implementation that draws to an XWindow.
 class GLOzoneEGLX11 : public GLOzoneEGL {
  public:
   GLOzoneEGLX11() {}
   ~GLOzoneEGLX11() override {}
 
-  // GLOzoneEGL:
   scoped_refptr<gl::GLSurface> CreateViewGLSurface(
       gfx::AcceleratedWidget window) override {
     return gl::InitializeGLSurface(new GLSurfaceEGLOzoneX11(window));
@@ -153,29 +150,12 @@ class GLOzoneEGLX11 : public GLOzoneEGL {
   DISALLOW_COPY_AND_ASSIGN(GLOzoneEGLX11);
 };
 
-// GLOzoneOSMesa implementation that draws to an XWindow.
-class GLOzoneOSMesaX11 : public GLOzoneOSMesa {
- public:
-  GLOzoneOSMesaX11() {}
-  ~GLOzoneOSMesaX11() override {}
-
-  // GLOzoneOSMesa:
-  bool InitializeGLOneOffPlatform() override {
-    return gl::GLSurfaceOSMesaX11::InitializeOneOff();
-  }
-
-  scoped_refptr<gl::GLSurface> CreateViewGLSurface(
-      gfx::AcceleratedWidget window) override {
-    return gl::InitializeGLSurface(new gl::GLSurfaceOSMesaX11(window));
-  }
-};
-
 }  // namespace
 
 X11SurfaceFactory::X11SurfaceFactory()
     : glx_implementation_(base::MakeUnique<GLOzoneGLX>()),
       egl_implementation_(base::MakeUnique<GLOzoneEGLX11>()),
-      osmesa_implementation_(base::MakeUnique<GLOzoneOSMesaX11>()) {}
+      osmesa_implementation_(base::MakeUnique<GLOzoneOSMesa>()) {}
 
 X11SurfaceFactory::~X11SurfaceFactory() {}
 
