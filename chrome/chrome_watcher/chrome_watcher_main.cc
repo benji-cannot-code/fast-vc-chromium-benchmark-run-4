97c8cef8a1697caf821a5240f2d049f8ba16a285
manzagop <manzagop@chromium.org>
2017-03-09 10:52:12 -0800

    Ensure chrome watcher exits cleanly on unexpected errors
    This mirrors what the installer does.
    https://cs.chromium.org/chromium/src/chrome/installer/setup/setup_main.cc?l=1367
    
    BUG=699983
    
    Review-Url: https://codereview.chromium.org/2737263004
    Cr-Commit-Position: refs/heads/master@{#455814}

--- chrome/chrome_watcher/chrome_watcher_main.cc
+++ chrome/chrome_watcher/chrome_watcher_main.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging_win.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
+#include "base/process/memory.h"
 #include "base/process/process.h"
 #include "base/run_loop.h"
 #include "base/sequenced_task_runner.h"
@@ -32,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
+#include "base/win/process_startup_helper.h"
 #include "base/win/scoped_handle.h"
 #include "base/win/win_util.h"
 #include "chrome/chrome_watcher/chrome_watcher_main_api.h"
@@ -202,6 +204,13 @@ extern "C" int WatcherMain(const base::char16* registry_path,
   // Arrange to be shut down as late as possible, as we want to outlive
   // chrome.exe in order to report its exit status.
   ::SetProcessShutdownParameters(0x100, SHUTDOWN_NORETRY);
+
+  // Make sure the process exits cleanly on unexpected errors.
+  base::EnableTerminationOnHeapCorruption();
+  base::EnableTerminationOnOutOfMemory();
+  base::win::RegisterInvalidParamHandler();
+  const base::CommandLine& cmd_line = *base::CommandLine::ForCurrentProcess();
+  base::win::SetupCRT(cmd_line);
 
   // Run a UI message loop on the main thread.
   base::PlatformThread::SetName("WatcherMainThread");
