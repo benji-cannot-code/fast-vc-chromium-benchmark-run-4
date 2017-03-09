9c50c3ca83d7a55edcf5e082b170fccd10a81756
jdufault <jdufault@chromium.org>
2017-03-09 14:46:54 -0800

    cros: Dump login/lock/oobe JS error stack traces to logs.
    BUG=699798
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2741673003
    Cr-Commit-Position: refs/heads/master@{#455889}

--- chrome/browser/resources/chromeos/login/login_shared.js
+++ chrome/browser/resources/chromeos/login/login_shared.js
@@ -442,5 +442,10 @@ disableTextSelectAndDrag(function(e) {
   document.addEventListener('DOMContentLoaded', function() {
     Oobe.initialize();
   });
+
+  // Install a global error handler so stack traces are included in logs.
+  window.onerror = function(message, file, line, column, error) {
+    console.error(error.stack);
+  }
 })();
 
