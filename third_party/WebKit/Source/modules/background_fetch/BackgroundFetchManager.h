7f0808d785645d10bf2c271fbb75511283ff7963
peter <peter@chromium.org>
2017-03-10 09:59:41 -0800

    Introduce the Background Fetch module.
    This CL introduces the BackgroundFetchManager object and exposes it on
    the ServiceWorkerRegistration, as part of a new Background Fetch module.
    
    More functionality and tests will be added iteratively.
    
    Intent to Implement:
      https://groups.google.com/a/chromium.org/d/topic/blink-dev/Ia9_-CVrl1U/discussion
    
    WPT directory addition + IDL harness tests:
      https://github.com/w3c/web-platform-tests/pull/5094
    
    Based on the following CL by awdf@:
      https://codereview.chromium.org/2739653002/
    
    BUG=692534
    
    Review-Url: https://codereview.chromium.org/2736143003
    Cr-Commit-Position: refs/heads/master@{#456102}

---
+++ third_party/WebKit/Source/modules/background_fetch/BackgroundFetchManager.h
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BackgroundFetchManager_h
+#define BackgroundFetchManager_h
+
+#include "bindings/core/v8/ScriptPromise.h"
+#include "bindings/core/v8/ScriptWrappable.h"
+#include "platform/heap/GarbageCollected.h"
+#include "platform/heap/Handle.h"
+
+namespace blink {
+
+class ScriptState;
+class ServiceWorkerRegistration;
+
+// Implementation of the BackgroundFetchManager JavaScript object, accessible
+// by developers through ServiceWorkerRegistration.backgroundFetch.
+class BackgroundFetchManager final
+    : public GarbageCollected<BackgroundFetchManager>,
+      public ScriptWrappable {
+  DEFINE_WRAPPERTYPEINFO();
+
+ public:
+  static BackgroundFetchManager* create(
+      ServiceWorkerRegistration* registration) {
+    return new BackgroundFetchManager(registration);
+  }
+
+  ScriptPromise getTags(ScriptState*);
+
+  DECLARE_TRACE();
+
+ private:
+  explicit BackgroundFetchManager(ServiceWorkerRegistration*);
+
+  Member<ServiceWorkerRegistration> m_registration;
+};
+
+}  // namespace blink
+
+#endif  // BackgroundFetchManager_h
