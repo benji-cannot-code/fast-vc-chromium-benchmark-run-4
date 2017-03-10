995273e82a7eda64c3c4327e77ebed4475bbfd49
benwells <benwells@chromium.org>
2017-03-09 21:30:00 -0800

    Reflect device status in geolocation permissions.request calls.
    In some situations origins may have geolocation permission, but the user
    will see prompts, or geolocation won't work. In these cases, the
    permissions API should indicate this, with either ASK or DENIED.
    
    The situations are as follows:
    1. When the device has location turned off
    2. When Chrome does not have geolocation permission at the Androd level.
    
    In both of these cases, Chrome may be able to prompt the user to rectify
    the problem and if possible will do so when permission is requested.
    
    This patch does not correctly model the second situation, as the plumbing
    required to see if Chrome is allowed to prompt is not implemented. In
    this case prompt is always return ASK, even if Chrome cannot prompt and
    requests will fail. This is better than returning GRANTED however, as
    it is closer to the truth.
    
    BUG=672301
    
    Review-Url: https://codereview.chromium.org/2743603002
    Cr-Commit-Position: refs/heads/master@{#456003}

--- chrome/browser/geolocation/geolocation_permission_context_android.h
+++ chrome/browser/geolocation/geolocation_permission_context_android.h
@@ -72,13 +72,17 @@ class GeolocationPermissionContextAndroid
                            const BrowserPermissionCallback& callback,
                            bool persist,
                            ContentSetting content_setting) override;
+  PermissionResult UpdatePermissionStatusWithDeviceStatus(
+      PermissionResult result,
+      const GURL& requesting_origin,
+      const GURL& embedding_origin) const override;
 
   bool IsLocationAccessPossible(content::WebContents* web_contents,
                                 const GURL& requesting_origin,
                                 bool user_gesture);
 
   LocationSettingsDialogContext GetLocationSettingsDialogContext(
-      const GURL& requesting_origin);
+      const GURL& requesting_origin) const;
 
   void HandleUpdateAndroidPermissions(const PermissionRequestID& id,
                                       const GURL& requesting_frame_origin,
@@ -91,7 +95,7 @@ class GeolocationPermissionContextAndroid
   // be shown, any gesture requirements for the origin are met, and the dialog
   // is not being suppressed for backoff.
   bool CanShowLocationSettingsDialog(const GURL& requesting_origin,
-                                     bool user_gesture);
+                                     bool user_gesture) const;
 
   void OnLocationSettingsDialogShown(
       const PermissionRequestID& id,
