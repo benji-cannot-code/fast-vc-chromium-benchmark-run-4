5e93cc5dddd01753a4bbd62720ff5c6d44470582
skyostil <skyostil@chromium.org>
2017-03-10 09:56:43 -0800

    perf: Disable v8.browsing_desktop_turbo on reference build
    BUG=700390
    TBR=rmcilroy@chromium.org
    
    Review-Url: https://codereview.chromium.org/2739423002
    Cr-Commit-Position: refs/heads/master@{#456100}

--- tools/perf/benchmarks/v8_browsing.py
+++ tools/perf/benchmarks/v8_browsing.py
@@ -168,6 +168,7 @@ class V8MobileBrowsingBenchmark(_V8MobileBrowsingBenchmark):
   TEST_SUFFIX = ''
 
 
+@benchmark.Disabled('reference')  # http://crbug.com/700390
 class V8DesktopTurboBrowsingBenchmark(_V8DesktopBrowsingBenchmark):
   PLATFORM = 'desktop'
   TEST_SUFFIX = '_turbo'
@@ -198,6 +199,7 @@ class V8RuntimeStatsDesktopBrowsingBenchmark(
     return possible_browser.platform.GetDeviceTypeName() != 'Desktop'
 
 
+@benchmark.Disabled('reference')  # http://crbug.com/700390
 class V8RuntimeStatsDesktopTurboBrowsingBenchmark(
     _V8RuntimeStatsBrowsingBenchmark):
   PLATFORM = 'desktop'
