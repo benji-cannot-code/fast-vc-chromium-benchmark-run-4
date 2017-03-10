66c66f59f9c2fcdf2534e37dcc70a6c9e561967c
treib <treib@chromium.org>
2017-03-10 04:31:05 -0800

    Cleanup: Remove SearchTabHelperDelegate
    Its one remaining member, GetOmniboxView, can be replaced easily by
    telling the SearchTabHelper when its tab gets attached to a new window
    (i.e. Browser::TabInsertedAt), at which point it can grab the
    OmniboxView* itself. This also correctly handles the case where a tab
    is dragged between windows.
    
    BUG=627747
    
    Review-Url: https://codereview.chromium.org/2739943005
    Cr-Commit-Position: refs/heads/master@{#456046}

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -963,9 +963,9 @@ void Browser::TabInsertedAt(TabStripModel* tab_strip_model,
                             bool foreground) {
   SetAsDelegate(contents, true);
 
-  SessionTabHelper* session_tab_helper =
-      SessionTabHelper::FromWebContents(contents);
-  session_tab_helper->SetWindowID(session_id());
+  SessionTabHelper::FromWebContents(contents)->SetWindowID(session_id());
+
+  SearchTabHelper::FromWebContents(contents)->OnTabAttachedToWindow(window_);
 
   content::NotificationService::current()->Notify(
       chrome::NOTIFICATION_TAB_PARENTED,
@@ -1984,13 +1984,6 @@ bool Browser::CanSaveContents(content::WebContents* web_contents) const {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-// Browser, SearchTabHelperDelegate implementation:
-
-OmniboxView* Browser::GetOmniboxView() {
-  return window_->GetLocationBar()->GetOmniboxView();
-}
-
-///////////////////////////////////////////////////////////////////////////////
 // Browser, web_modal::WebContentsModalDialogManagerDelegate implementation:
 
 void Browser::SetWebContentsBlocked(content::WebContents* web_contents,
@@ -2430,7 +2423,6 @@ void Browser::SetAsDelegate(WebContents* web_contents, bool set_delegate) {
   WebContentsModalDialogManager::FromWebContents(web_contents)->
       SetDelegate(delegate);
   CoreTabHelper::FromWebContents(web_contents)->set_delegate(delegate);
-  SearchTabHelper::FromWebContents(web_contents)->set_delegate(delegate);
   translate::ContentTranslateDriver& content_translate_driver =
       ChromeTranslateClient::FromWebContents(web_contents)->translate_driver();
   if (delegate) {
--- chrome/browser/ui/browser.h
+++ chrome/browser/ui/browser.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/chrome_web_modal_dialog_manager_delegate.h"
 #include "chrome/browser/ui/exclusive_access/exclusive_access_manager.h"
 #include "chrome/browser/ui/profile_chooser_constants.h"
-#include "chrome/browser/ui/search/search_tab_helper_delegate.h"
 #include "chrome/browser/ui/signin_view_controller.h"
 #include "chrome/browser/ui/tab_contents/core_tab_helper_delegate.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
@@ -107,7 +106,6 @@ class WebContentsModalDialogHost;
 class Browser : public TabStripModelObserver,
                 public content::WebContentsDelegate,
                 public CoreTabHelperDelegate,
-                public SearchTabHelperDelegate,
                 public ChromeWebModalDialogManagerDelegate,
                 public BookmarkTabHelperDelegate,
                 public zoom::ZoomObserver,
@@ -719,9 +717,6 @@ class Browser : public TabStripModelObserver,
                        bool did_finish_load) override;
   bool CanReloadContents(content::WebContents* web_contents) const override;
   bool CanSaveContents(content::WebContents* web_contents) const override;
-
-  // Overridden from SearchTabHelperDelegate:
-  OmniboxView* GetOmniboxView() override;
 
   // Overridden from WebContentsModalDialogManagerDelegate:
   void SetWebContentsBlocked(content::WebContents* web_contents,
