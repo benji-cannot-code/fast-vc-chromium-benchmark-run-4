b5f1d1727bbe871f0d921b4a7f2735ba6019c491
almasrymina <almasrymina@chromium.org>
2017-03-10 10:27:50 -0800

    [Chromecast] Fix ordering in PreMainMessageLoopRun
    UrlRequestContextFactory needs to be initialized before GetSystemGetter
    is called.
    
    Net log should be set before network activity starts.
    
    BUG= internal b/35963633
    TEST= Manual attempt to reproduce the crash
    
    Review-Url: https://codereview.chromium.org/2738583003
    Cr-Commit-Position: refs/heads/master@{#456113}

--- chromecast/browser/url_request_context_factory.cc
+++ chromecast/browser/url_request_context_factory.cc
@@ -254,6 +254,7 @@ void URLRequestContextFactory::InitializeSystemContextDependencies() {
   // in the future.
   http_server_properties_.reset(new net::HttpServerPropertiesImpl);
 
+  DCHECK(proxy_config_service_);
   proxy_service_ = net::ProxyService::CreateUsingSystemProxyResolver(
       std::move(proxy_config_service_), 0, NULL);
   system_dependencies_initialized_ = true;
