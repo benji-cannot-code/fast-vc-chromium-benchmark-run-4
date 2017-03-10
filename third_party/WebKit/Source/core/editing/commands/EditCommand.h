ed81a68695f7c1b57fd3ad8e0c66cb0003f62c13
yosin <yosin@chromium.org>
2017-03-09 16:57:15 -0800

    Get rid of redundant #include "VisibleSelection.h" from EditCommand.h
    This patch gets rid of include-directive for "VisibleSelection.h" from
    "EditCommand.h" to reduce source code size and faster compilation for
    improving code health.
    
    BUG=n/a
    TEST=n/a; no behavior changes
    
    Review-Url: https://codereview.chromium.org/2744583002
    Cr-Commit-Position: refs/heads/master@{#455934}

--- third_party/WebKit/Source/core/editing/commands/EditCommand.h
+++ third_party/WebKit/Source/core/editing/commands/EditCommand.h
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define EditCommand_h
 
 #include "core/CoreExport.h"
-#include "core/editing/VisibleSelection.h"
 #include "core/events/InputEvent.h"
 #include "platform/heap/Handle.h"
 
