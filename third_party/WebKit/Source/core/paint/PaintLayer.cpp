32c6e1a3f2c02eba8fb59df761c04e424c4edcca
chrishtr <chrishtr@chromium.org>
2017-03-09 19:55:52 -0800

    Fix position of composited, paginated inlines.
    Previously, we were not including the offset from the PaintLayer of the composited layer when calling PaintLayer::visualOffsetFromAncestor to compute GraphicsLayer
    offset from the composited ancestor.
    
    It's important to include this offset when converting from logical to visual
    coordinates across a paginated layer boundary, because the offset influences the
    fragment offset of the composited layer.
    
    BUG=667213
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2738943004
    Cr-Commit-Position: refs/heads/master@{#455982}

--- third_party/WebKit/Source/core/paint/PaintLayer.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayer.cpp
@@ -1470,8 +1470,8 @@ void PaintLayer::convertToLayerCoords(const PaintLayer* ancestorLayer,
 }
 
 LayoutPoint PaintLayer::visualOffsetFromAncestor(
-    const PaintLayer* ancestorLayer) const {
-  LayoutPoint offset;
+    const PaintLayer* ancestorLayer,
+    LayoutPoint offset) const {
   if (ancestorLayer == this)
     return offset;
   PaintLayer* paginationLayer = enclosingPaginationLayer();
