f8c9e5277293fc9ff803741aaf3c7a70b6922a8d
mcasas <mcasas@chromium.org>
2017-03-10 14:45:50 -0800

    Image Capture: move files out of content/browser/media/capture to replicate WebKit's structure
    No new code, just shuffling things around so WebKit/Source/modules
    and content/browser have a more related structure, also in terms
    of bug components and OWNERS.
    
    BUG=none  (no new code)
    
    Review-Url: https://codereview.chromium.org/2745583005
    Cr-Commit-Position: refs/heads/master@{#456198}

--- content/browser/image_capture/image_capture_impl.h
+++ content/browser/image_capture/image_capture_impl.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_BROWSER_MEDIA_CAPTURE_IMAGE_CAPTURE_IMPL_H_
-#define CONTENT_BROWSER_MEDIA_CAPTURE_IMAGE_CAPTURE_IMPL_H_
+#ifndef CONTENT_BROWSER_IMAGE_CAPTURE_IMAGE_CAPTURE_IMPL_H_
+#define CONTENT_BROWSER_IMAGE_CAPTURE_IMAGE_CAPTURE_IMPL_H_
 
 #include "media/capture/mojo/image_capture.mojom.h"
 
@@ -33,4 +33,4 @@ class ImageCaptureImpl : public media::mojom::ImageCapture {
 
 }  // namespace content
 
-#endif  // CONTENT_BROWSER_MEDIA_CAPTURE_IMAGE_CAPTURE_IMPL_H_
+#endif  // CONTENT_BROWSER_IMAGE_CAPTURE_IMAGE_CAPTURE_IMPL_H_
