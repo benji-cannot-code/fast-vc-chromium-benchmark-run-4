8266463004b38cd985f344ef881c9300eb6f5b75
peter <peter@chromium.org>
2017-03-10 06:22:00 -0800

    Migrate two std::remove_if usages to use base::EraseIf instead
    BUG=
    
    Review-Url: https://codereview.chromium.org/2742973002
    Cr-Commit-Position: refs/heads/master@{#456056}

--- content/browser/notifications/platform_notification_context_impl.cc
+++ content/browser/notifications/platform_notification_context_impl.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/memory/ptr_util.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/stl_util.h"
 #include "base/threading/sequenced_worker_pool.h"
 #include "content/browser/notifications/blink_notification_service_impl.h"
 #include "content/browser/notifications/notification_database.h"
@@ -132,13 +133,11 @@ void PlatformNotificationContextImpl::CreateServiceOnIO(
 void PlatformNotificationContextImpl::RemoveService(
     BlinkNotificationServiceImpl* service) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
-  auto services_to_remove = std::remove_if(
-      services_.begin(), services_.end(),
+  base::EraseIf(
+      services_,
       [service](const std::unique_ptr<BlinkNotificationServiceImpl>& ptr) {
         return ptr.get() == service;
       });
-
-  services_.erase(services_to_remove, services_.end());
 }
 
 void PlatformNotificationContextImpl::ReadNotificationData(
