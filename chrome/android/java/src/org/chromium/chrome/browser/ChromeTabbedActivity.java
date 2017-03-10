80fffdcdf3abe60750369434fd4baace0c9a5856
peconn <peconn@chromium.org>
2017-03-10 06:03:05 -0800

    📺 Move VideoPersistence to ChromeActivity.
    This allows Custom Tabs and Web Apps to use video persistence.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2742983002
    Cr-Commit-Position: refs/heads/master@{#456052}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -76,7 +76,6 @@ import org.chromium.chrome.browser.fullscreen.ComposedBrowserControlsVisibilityD
 import org.chromium.chrome.browser.incognito.IncognitoNotificationManager;
 import org.chromium.chrome.browser.infobar.DataReductionPromoInfoBar;
 import org.chromium.chrome.browser.locale.LocaleManager;
-import org.chromium.chrome.browser.media.VideoPersister;
 import org.chromium.chrome.browser.metrics.ActivityStopMetrics;
 import org.chromium.chrome.browser.metrics.LaunchMetrics;
 import org.chromium.chrome.browser.metrics.MainIntentBehaviorMetrics;
@@ -513,7 +512,6 @@ public class ChromeTabbedActivity extends ChromeActivity implements OverviewMode
             mMergeTabsOnResume = false;
         }
 
-        VideoPersister.getInstance().stopPersist(this);
         // TODO(mthiesse): Move this call into ChromeActivity. crbug.com/697694
         VrShellDelegate.maybeResumeVR(this);
 
@@ -525,12 +523,6 @@ public class ChromeTabbedActivity extends ChromeActivity implements OverviewMode
         } else {
             SnippetsBridge.notifySchedulerAboutColdStart();
         }
-    }
-
-    @Override
-    protected void onUserLeaveHint() {
-        VideoPersister.getInstance().attemptPersist(this);
-        super.onUserLeaveHint();
     }
 
     @Override
