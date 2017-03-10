c6554936b75e645e700f40e8a91be35804caf873
rnephew <rnephew@chromium.org>
2017-03-09 16:34:11 -0800

    [Telemetry] Move from telemetry/trace_data to tracing/trace_data in chromium
    This is the chromium side version of https://codereview.chromium.org/2736903005
    
    Review-Url: https://codereview.chromium.org/2741533003
    Cr-Commit-Position: refs/heads/master@{#455924}

--- tools/chrome_proxy/common/inspector_network.py
+++ tools/chrome_proxy/common/inspector_network.py
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import logging
 
 from telemetry.core import exceptions
-from telemetry.timeline import trace_data
 from telemetry.timeline import model
+from tracing.trace_data import trace_data
 
 
 class InspectorNetworkException(Exception):
