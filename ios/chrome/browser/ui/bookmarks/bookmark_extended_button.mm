eebe77a7ff381dfb06c4816778f216fb2eef1fa4
stkhapugin <stkhapugin@chromium.org>
2017-03-10 07:25:56 -0800

    [ObjC ARC] Converts ios/chrome/browser/ui/bookmarks:bookmarks_arc_2 to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2737603007
    Cr-Commit-Position: refs/heads/master@{#456064}

--- ios/chrome/browser/ui/bookmarks/bookmark_extended_button.mm
+++ ios/chrome/browser/ui/bookmarks/bookmark_extended_button.mm
@@ -7,6 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace {
 // Apple's guidelines indicate that buttons should have a minimum touch area of
 // 44pt x 44pt.
