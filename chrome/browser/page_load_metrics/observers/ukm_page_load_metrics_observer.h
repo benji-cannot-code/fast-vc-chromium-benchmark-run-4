74e78592c59890939c6c0a6735425a4ad202a2a1
bmcquade <bmcquade@chromium.org>
2017-03-10 15:54:18 -0800

    Persist EffectiveConnectionType in UKM on navigation start.
    This change emits a UKM PageLoad metric for the EffectiveConnectionType
    observed at navigation start.
    
    This change also corrects an issue where we logged some information in
    OnStart. When OnStart is invoked, we don't yet know if we're observing
    a page load, so we move all logging to later in the flow, when we know
    we're observing a page load.
    
    BUG=700537
    
    Review-Url: https://codereview.chromium.org/2740403002
    Cr-Commit-Position: refs/heads/master@{#456219}

--- chrome/browser/page_load_metrics/observers/ukm_page_load_metrics_observer.h
+++ chrome/browser/page_load_metrics/observers/ukm_page_load_metrics_observer.h
@@ -8,6 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "chrome/browser/page_load_metrics/page_load_metrics_observer.h"
+#include "net/nqe/network_quality_estimator.h"
+
+namespace content {
+class WebContents;
+}
 
 namespace internal {
 
@@ -19,6 +24,7 @@ extern const char kUkmLoadEventName[];
 extern const char kUkmFirstContentfulPaintName[];
 extern const char kUkmFirstMeaningfulPaintName[];
 extern const char kUkmForegroundDurationName[];
+extern const char kUkmEffectiveConnectionType[];
 
 }  // namespace internal
 
@@ -29,9 +35,11 @@ class UkmPageLoadMetricsObserver
  public:
   // Returns a UkmPageLoadMetricsObserver, or nullptr if it is not needed.
   static std::unique_ptr<page_load_metrics::PageLoadMetricsObserver>
-  CreateIfNeeded();
+  CreateIfNeeded(content::WebContents* web_contents);
 
-  UkmPageLoadMetricsObserver();
+  explicit UkmPageLoadMetricsObserver(
+      net::NetworkQualityEstimator::NetworkQualityProvider*
+          network_quality_provider);
   ~UkmPageLoadMetricsObserver() override;
 
   // page_load_metrics::PageLoadMetricsObserver implementation:
@@ -66,8 +74,14 @@ class UkmPageLoadMetricsObserver
       const page_load_metrics::PageLoadExtraInfo& info,
       base::TimeTicks app_background_time);
 
+  net::NetworkQualityEstimator::NetworkQualityProvider* const
+      network_quality_provider_;
+
   // Unique UKM identifier for the page load we are recording metrics for.
   const int32_t source_id_;
+
+  net::EffectiveConnectionType effective_connection_type_ =
+      net::EFFECTIVE_CONNECTION_TYPE_UNKNOWN;
 
   DISALLOW_COPY_AND_ASSIGN(UkmPageLoadMetricsObserver);
 };
