957ccf283d853178205242c9b794ccca0812b998
sashab <sashab@chromium.org>
2017-03-09 19:52:06 -0800

    Make TopDocumentRootScrollerController store Page instead of FrameHost
    This is pre-work to move FrameHost::m_topDocumentRootScrollerController
    to Page.
    
    BUG=691794
    
    Review-Url: https://codereview.chromium.org/2732363003
    Cr-Commit-Position: refs/heads/master@{#455980}

--- third_party/WebKit/Source/core/page/scrolling/TopDocumentRootScrollerController.h
+++ third_party/WebKit/Source/core/page/scrolling/TopDocumentRootScrollerController.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class Element;
-class FrameHost;
+class Page;
 class GraphicsLayer;
 class PaintLayer;
 class RootFrameViewport;
@@ -31,7 +31,7 @@ class ViewportScrollCallback;
 class CORE_EXPORT TopDocumentRootScrollerController
     : public GarbageCollected<TopDocumentRootScrollerController> {
  public:
-  static TopDocumentRootScrollerController* create(FrameHost&);
+  static TopDocumentRootScrollerController* create(Page&);
 
   DECLARE_TRACE();
 
@@ -79,7 +79,7 @@ class CORE_EXPORT TopDocumentRootScrollerController
   IntSize rootScrollerVisibleArea() const;
 
  private:
-  TopDocumentRootScrollerController(FrameHost&);
+  TopDocumentRootScrollerController(Page&);
 
   // Calculates the Element that should be the globalRootScroller. On a
   // simple page, this will simply the root frame's effectiveRootScroller but
@@ -107,7 +107,7 @@ class CORE_EXPORT TopDocumentRootScrollerController
   // Element.
   WeakMember<Element> m_globalRootScroller;
 
-  WeakMember<FrameHost> m_frameHost;
+  WeakMember<Page> m_page;
 };
 
 }  // namespace blink
