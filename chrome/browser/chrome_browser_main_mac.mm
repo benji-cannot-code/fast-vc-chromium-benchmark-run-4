59db802c47a246ca5b17cb3769476bf8d4f36ee6
kerrnel <kerrnel@chromium.org>
2017-03-10 11:03:43 -0800

    Activate keychain reauthorization.
    Activate the keychain reauthorization. Reauthorization will be performed
    if the users preferences (cfprefs) indicate that the operation has been
    attempted less than 2 times previously, and that the operation has not
    previously succeeded. The number of attempts is set before the
    operation, so that a crashy operation stops running after a finite
    number of attempts. After successful completion, the preferences are
    updated so that the operation is never run again. This CL also updates
    the designated requirement with a new hash.
    
    BUG=629906
    
    Review-Url: https://codereview.chromium.org/2738053007
    Cr-Commit-Position: refs/heads/master@{#456122}

--- chrome/browser/chrome_browser_main_mac.mm
+++ chrome/browser/chrome_browser_main_mac.mm
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #import "chrome/browser/chrome_browser_application_mac.h"
 #include "chrome/browser/mac/install_from_dmg.h"
+#include "chrome/browser/mac/keychain_reauthorize.h"
 #import "chrome/browser/mac/keystone_glue.h"
 #include "chrome/browser/mac/mac_startup_profiler.h"
 #include "chrome/browser/ui/app_list/app_list_service.h"
@@ -157,6 +158,17 @@ void ChromeBrowserMainPartsMac::PreMainMessageLoopStart() {
     [object retain];
   // Make sure the app controller has been created.
   DCHECK([NSApp delegate]);
+
+  // Do Keychain reauthorization. This gets two chances to run. If the first
+  // try doesn't complete successfully (crashes or is interrupted for any
+  // reason), there will be a second chance. Once this step completes
+  // successfully, it should never have to run again.
+  NSString* const keychain_reauthorize_pref =
+      @"KeychainReauthorizeInAppSpring2017";
+  const int kKeychainReauthorizeMaxTries = 2;
+
+  chrome::KeychainReauthorizeIfNeeded(keychain_reauthorize_pref,
+                                      kKeychainReauthorizeMaxTries);
 }
 
 void ChromeBrowserMainPartsMac::PostMainMessageLoopStart() {
