6096c033a686c822e5ae8075329a52623e71a4e0
japhet <japhet@chromium.org>
2017-03-10 11:29:30 -0800

    Reset Frame::m_hasReceivedUserGesture for main frames on navigation
    Child frames have some degree of continuity across navigations: the
    contents are still embedded in the same document. Let them continue to
    propagate user gesture state across navigations. However, main frames
    don't have that continuity and should reset their user gesture state
    on navigation.
    
    TEST=DocumentUserGestureTokenTest.Navigate
    BUG=chromedriver:922
    
    Review-Url: https://codereview.chromium.org/2721423003
    Cr-Commit-Position: refs/heads/master@{#456134}

--- third_party/WebKit/Source/core/frame/LocalFrame.cpp
+++ third_party/WebKit/Source/core/frame/LocalFrame.cpp
@@ -516,6 +516,8 @@ void LocalFrame::documentAttached() {
   selection().documentAttached(document());
   inputMethodController().documentAttached(document());
   spellChecker().documentAttached(document());
+  if (isMainFrame())
+    m_hasReceivedUserGesture = false;
 }
 
 LocalDOMWindow* LocalFrame::domWindow() const {
