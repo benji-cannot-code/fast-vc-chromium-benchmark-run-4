ad54f458825cab577427aef6e5b05592e8d08e5d
alokp <alokp@chromium.org>
2017-03-09 16:35:27 -0800

    [mojo-js] Converts binding unittest into layout test.
    This patch prepares to remove dependency on //mojo/edk/js to run
    js-bindings unittets.
    
    BUG=647036
    
    Review-Url: https://codereview.chromium.org/2743623003
    Cr-Commit-Position: refs/heads/master@{#455925}

--- mojo/edk/js/tests/run_js_unittests.cc
+++ mojo/edk/js/tests/run_js_unittests.cc
@@ -46,10 +46,6 @@ void RunTest(std::string test, bool run_until_idle) {
 }
 
 // TODO(abarth): Should we autogenerate these stubs from GYP?
-TEST(JSTest, Binding) {
-  RunTest("binding_unittest.js", false);
-}
-
 TEST(JSTest, Codec) {
   RunTest("codec_unittest.js", true);
 }
