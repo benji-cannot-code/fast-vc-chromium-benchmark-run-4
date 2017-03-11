a81ddbe9d7b39cf612832faa10f4ad526bf1864c
khorimoto <khorimoto@chromium.org>
2017-03-10 17:43:02 -0800

    [CrOS Tether] Create KeepAliveScheduler, a class which schedules keep-alive tickles to be sent to a connected host every 4 minutes until the device disconnects.
    BUG=672263
    
    Review-Url: https://codereview.chromium.org/2741253002
    Cr-Commit-Position: refs/heads/master@{#456249}

--- chromeos/components/tether/keep_alive_operation.h
+++ chromeos/components/tether/keep_alive_operation.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMEOS_COMPONENTS_TETHER_KEEP_ALIVE_OPERATION_H_
 #define CHROMEOS_COMPONENTS_TETHER_KEEP_ALIVE_OPERATION_H_
 
+#include "base/observer_list.h"
 #include "chromeos/components/tether/message_transfer_operation.h"
 
 namespace chromeos {
@@ -15,9 +16,8 @@ namespace tether {
 class BleConnectionManager;
 
 // Operation which sends a keep-alive message to a tether host.
-// TODO(khorimoto/hansberry): Consider changing protocol to receive a
-//                            DeviceStatus update after sending the
-//                            KeepAliveTickle message.
+// TODO(khorimoto/hansberry): Change protocol to receive a DeviceStatus update
+//                            after sending the KeepAliveTickle message.
 class KeepAliveOperation : public MessageTransferOperation {
  public:
   class Factory {
@@ -30,11 +30,18 @@ class KeepAliveOperation : public MessageTransferOperation {
 
    protected:
     virtual std::unique_ptr<KeepAliveOperation> BuildInstance(
-        const cryptauth::RemoteDevice& devices_to_connect,
+        const cryptauth::RemoteDevice& device_to_connect,
         BleConnectionManager* connection_manager);
 
    private:
     static Factory* factory_instance_;
+  };
+
+  class Observer {
+   public:
+    // TODO(khorimoto): This function should take a DeviceStatus once there is
+    //                  keep-alive tickle response.
+    virtual void OnOperationFinished() = 0;
   };
 
   KeepAliveOperation(const cryptauth::RemoteDevice& device_to_connect,
@@ -48,10 +55,14 @@ class KeepAliveOperation : public MessageTransferOperation {
   // MessageTransferOperation:
   void OnDeviceAuthenticated(
       const cryptauth::RemoteDevice& remote_device) override;
+  void OnOperationFinished() override;
   MessageType GetMessageTypeForConnection() override;
 
  private:
   friend class KeepAliveOperationTest;
+
+  base::ObserverList<Observer> observer_list_;
+  bool has_authenticated_;
 
   DISALLOW_COPY_AND_ASSIGN(KeepAliveOperation);
 };
--- chromeos/components/tether/keep_alive_operation_unittest.cc
+++ chromeos/components/tether/keep_alive_operation_unittest.cc
@@ -20,6 +20,20 @@ namespace tether {
 
 namespace {
 
+class TestObserver : public KeepAliveOperation::Observer {
+ public:
+  TestObserver() : has_run_callback_(false) {}
+
+  virtual ~TestObserver() {}
+
+  bool has_run_callback() { return has_run_callback_; }
+
+  void OnOperationFinished() override { has_run_callback_ = true; }
+
+ private:
+  bool has_run_callback_;
+};
+
 std::string CreateKeepAliveTickleString() {
   KeepAliveTickle tickle;
   return MessageWrapper(tickle).ToRawMessage();
@@ -38,6 +52,10 @@ class KeepAliveOperationTest : public testing::Test {
 
     operation_ = base::WrapUnique(new KeepAliveOperation(
         test_device_, fake_ble_connection_manager_.get()));
+
+    test_observer_ = base::WrapUnique(new TestObserver());
+    operation_->AddObserver(test_observer_.get());
+
     operation_->Initialize();
   }
 
@@ -56,6 +74,8 @@ class KeepAliveOperationTest : public testing::Test {
   const cryptauth::RemoteDevice test_device_;
 
   std::unique_ptr<FakeBleConnectionManager> fake_ble_connection_manager_;
+  std::unique_ptr<TestObserver> test_observer_;
+
   std::unique_ptr<KeepAliveOperation> operation_;
 
  private:
@@ -63,7 +83,9 @@ class KeepAliveOperationTest : public testing::Test {
 };
 
 TEST_F(KeepAliveOperationTest, TestSendsKeepAliveTickle) {
+  EXPECT_FALSE(test_observer_->has_run_callback());
   SimulateDeviceAuthenticationAndVerifyMessageSent();
+  EXPECT_TRUE(test_observer_->has_run_callback());
 }
 
 }  // namespace tether
