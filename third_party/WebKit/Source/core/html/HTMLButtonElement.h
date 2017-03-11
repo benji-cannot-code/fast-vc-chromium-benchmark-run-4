c15e0972f5a40b777fe56272b00162d3c7550580
luoe <luoe@chromium.org>
2017-03-10 16:29:30 -0800

    Revert of evaluating clipboard event target acording to w3c specification (patchset #7 id:120001 of https://codereview.chromium.org/2685723005/ )
    Reason for revert:
    Changing this functionality improved spec conformance, but introduced regressions where text that used to be copied is no longer copied:
    crbug.com/695568
    crbug.com/700163
    
    A proposal to change the spec was made here:
    https://github.com/w3c/clipboard-apis/issues/40
    
    Original issue's description:
    > Make Editor::findEventTargetFrom() to align Clipboard API specification
    >
    > This patch changes |Editor::findEventTargetFrom()| to return focused element if
    > selection start is not editable to align Clipboard API specification[1] for
    > improving interoperability.
    >
    > [1] https://w3c.github.io/clipboard-apis/#to-fire-a-clipboard-event
    >
    > BUG=690104
    > TEST=webkit_unittests --gtest_filter=ClipboardEventFlowTest.*
    >
    > Review-Url: https://codereview.chromium.org/2685723005
    > Cr-Commit-Position: refs/heads/master@{#451716}
    > Committed: https://chromium.googlesource.com/chromium/src/+/7afbab0f7619c89aa7a5896024d1628a59cc0ef1
    
    TBR=tkent@chromium.org,ojan@chromium.org,yosin@chromium.org,yutak@chromium.org,sigbjornf@opera.com,mwrobel@opera.com
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=690104
    
    Review-Url: https://codereview.chromium.org/2745813002
    Cr-Commit-Position: refs/heads/master@{#456232}

--- third_party/WebKit/Source/core/html/HTMLButtonElement.h
+++ third_party/WebKit/Source/core/html/HTMLButtonElement.h
@@ -25,12 +25,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef HTMLButtonElement_h
 #define HTMLButtonElement_h
 
-#include "core/CoreExport.h"
 #include "core/html/HTMLFormControlElement.h"
 
 namespace blink {
 
-class CORE_EXPORT HTMLButtonElement final : public HTMLFormControlElement {
+class HTMLButtonElement final : public HTMLFormControlElement {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
