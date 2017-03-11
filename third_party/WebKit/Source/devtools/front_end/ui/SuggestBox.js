b65e9ed1899504be1d3e08965eaf4ad5524ad536
lushnikov <lushnikov@chromium.org>
2017-03-11 09:50:32 -0800

    DevTools: initial console suggestion should be greyed out
    This patch makes initial console suggestion greyed out until
    user explicitly interactes with the suggest box.
    
    BUG=700570
    R=dgozman, pfeldman, einbinder
    
    Review-Url: https://codereview.chromium.org/2742573007
    Cr-Commit-Position: refs/heads/master@{#456283}

--- third_party/WebKit/Source/devtools/front_end/ui/SuggestBox.js
+++ third_party/WebKit/Source/devtools/front_end/ui/SuggestBox.js
@@ -79,6 +79,21 @@ UI.SuggestBox = class {
   }
 
   /**
+   * @param {boolean} value
+   */
+  setDefaultSelectionIsDimmed(value) {
+    this._element.classList.toggle('default-selection-is-dimmed', value);
+  }
+
+  /**
+   * @param {boolean} value
+   */
+  _setUserInteracted(value) {
+    this._userInteracted = value;
+    this._element.classList.toggle('user-has-interacted', value);
+  }
+
+  /**
    * @return {boolean}
    */
   visible() {
@@ -138,7 +153,7 @@ UI.SuggestBox = class {
   hide() {
     if (!this.visible())
       return;
-    this._userInteracted = false;
+    this._setUserInteracted(false);
     this._glassPane.hide();
   }
 
@@ -210,7 +225,7 @@ UI.SuggestBox = class {
 
     element.addEventListener('click', event => {
       this._list.selectItem(item);
-      this._userInteracted = true;
+      this._setUserInteracted(true);
       event.consume(true);
       this.acceptSuggestion();
     });
@@ -336,7 +351,7 @@ UI.SuggestBox = class {
         return false;
     }
     if (selected) {
-      this._userInteracted = true;
+      this._setUserInteracted(true);
       return true;
     }
     return false;
