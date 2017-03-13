b4ad6687f960075c72e174f63acd759c87046fcd
dtseng <dtseng@chromium.org>
2017-03-13 14:09:42 -0700

    Re-enable support for Math in ChromeVox Next
    TEST=navigate to mathjax.org. Tab to the first math expression. Observe help message (prompting to hit shift+space). Verify exploration (arrow keys) leads to verbal output.
    BUG=624124
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2746433004
    Cr-Commit-Position: refs/heads/master@{#456482}

--- chrome/browser/resources/chromeos/chromevox/chromevox/injected/api.js
+++ chrome/browser/resources/chromeos/chromevox/chromevox/injected/api.js
@@ -130,6 +130,23 @@ if (typeof(goog) != 'undefined' && goog.require) {
    };
 
 
+   /**
+    * Maybe enable MathJaX support.
+    */
+  function maybeEnableMathJaX() {
+     if (!MathJax || !MathJax.Hub || !MathJax.Hub.Register ||
+         !MathJax.Hub.Register.LoadHook)
+       return;
+
+     MathJax.Hub.Register.LoadHook(
+         '[a11y]/explorer.js',
+         function() {
+           MathJax.Extension.explorer.Enable(true, true);
+         });
+     MathJax.Ajax.Require('[a11y]/explorer.js');
+   }
+
+
    /*
     * Public API.
     */
@@ -165,6 +182,7 @@ if (typeof(goog) != 'undefined' && goog.require) {
     */
    cvox.Api.internalEnable = function() {
      isActive_ = true;
+     maybeEnableMathJaX();
      if (!implementation) {
        connect_();
      }
