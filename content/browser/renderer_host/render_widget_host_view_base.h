4c6ad61017697b30b3b161a4edbbdf8096965aa2
qyearsley <qyearsley@chromium.org>
2017-03-13 14:05:31 -0700

    Revert of Add a window property to associate RWHVA with its child AX tree ID (patchset #4 id:60001 of https://codereview.chromium.org/2742533003/ )
    Reason for revert:
    It looks like this causes the (static initializers) sizes step to fail:
    
    https://luci-milo.appspot.com/buildbot/chromium/Linux%20x64/35021
    
    Original issue's description:
    > Add a window property to associate RWHVA with its child AX tree ID
    >
    > Working on making the Chrome OS virtual keyboard accessible to select-to-speak
    > and other accessibility services. Currently we link a views::WebView to its
    > inner web accessibility tree, but the VK just puts a WebContents directly in
    > an aura Window.
    >
    > This change hooks them up by adding the child AX tree ID as a window property.
    >
    > BUG=699617
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    >
    > Review-Url: https://codereview.chromium.org/2742533003
    > Cr-Commit-Position: refs/heads/master@{#456445}
    > Committed: https://chromium.googlesource.com/chromium/src/+/05107990b575defc12ba29da8ae17a8660a29b67
    
    TBR=sky@chromium.org,dtseng@chromium.org,jam@chromium.org,dmazzoni@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=699617
    
    Review-Url: https://codereview.chromium.org/2751503002
    Cr-Commit-Position: refs/heads/master@{#456479}

--- content/browser/renderer_host/render_widget_host_view_base.h
+++ content/browser/renderer_host/render_widget_host_view_base.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/web/WebPopupType.h"
 #include "third_party/WebKit/public/web/WebTextDirection.h"
 #include "third_party/skia/include/core/SkImageInfo.h"
-#include "ui/accessibility/ax_tree_id_registry.h"
 #include "ui/base/ime/text_input_mode.h"
 #include "ui/base/ime/text_input_type.h"
 #include "ui/display/display.h"
@@ -212,7 +211,7 @@ class CONTENT_EXPORT RenderWidgetHostViewBase : public RenderWidgetHostView,
   virtual gfx::Point AccessibilityOriginInScreen(const gfx::Rect& bounds);
   virtual gfx::AcceleratedWidget AccessibilityGetAcceleratedWidget();
   virtual gfx::NativeViewAccessible AccessibilityGetNativeViewAccessible();
-  virtual void SetMainFrameAXTreeID(ui::AXTreeIDRegistry::AXTreeID id) {}
+
   // Informs that the focused DOM node has changed.
   virtual void FocusedNodeChanged(bool is_editable_node,
                                   const gfx::Rect& node_bounds_in_screen) {}
--- ui/views/accessibility/ax_window_obj_wrapper.cc
+++ ui/views/accessibility/ax_window_obj_wrapper.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/utf_string_conversions.h"
 #include "ui/accessibility/ax_node_data.h"
-#include "ui/accessibility/platform/aura_window_properties.h"
 #include "ui/aura/window.h"
 #include "ui/views/accessibility/ax_aura_obj_cache.h"
 #include "ui/views/widget/widget.h"
@@ -56,11 +55,6 @@ void AXWindowObjWrapper::Serialize(ui::AXNodeData* out_node_data) {
                                     base::UTF16ToUTF8(window_->GetTitle()));
   out_node_data->state = 0;
   out_node_data->location = gfx::RectF(window_->bounds());
-
-  ui::AXTreeIDRegistry::AXTreeID child_ax_tree_id =
-      window_->GetProperty(ui::kChildAXTreeID);
-  if (child_ax_tree_id != ui::AXTreeIDRegistry::kNoAXTreeID)
-    out_node_data->AddIntAttribute(ui::AX_ATTR_CHILD_TREE_ID, child_ax_tree_id);
 }
 
 int32_t AXWindowObjWrapper::GetID() {
