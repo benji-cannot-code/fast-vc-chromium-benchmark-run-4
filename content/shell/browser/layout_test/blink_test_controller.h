9f21343b03cb1056393929f5608dd78605856bc0
delphick <delphick@chromium.org>
2017-03-13 06:29:31 -0700

    Hide console log messages for imported WPT tests
    Move console output messages for WPT tests from the test output file into stderr so they no longer affect whether the test passes or fails but can still be used when debugging a test. Remove all CONSOLE lines from external/wpt/**/*-expected.txt.
    
    Update tests in following directories to add a console.warn statement to prevent the presubmit from trying to remove the -expected.txt file since these tests rely on console messages to determine whether they pass/fail:
    
    third_party/WebKit/LayoutTests/csspaint (logs are from worklets which have no other way to communicate the test status)
    
    Discussion on console messages in testharness tests can be found here:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/3Ob2lq14KEs
    
    BUG=679742
    
    Review-Url: https://codereview.chromium.org/2678043002
    Cr-Commit-Position: refs/heads/master@{#456362}

--- content/shell/browser/layout_test/blink_test_controller.h
+++ content/shell/browser/layout_test/blink_test_controller.h
@@ -86,6 +86,7 @@ class BlinkTestResultPrinter {
   void PrintAudioBlock(const std::vector<unsigned char>& audio_data);
   void PrintAudioFooter();
 
+  void AddMessageToStderr(const std::string& message);
   void AddMessage(const std::string& message);
   void AddMessageRaw(const std::string& message);
   void AddErrorMessage(const std::string& message);
@@ -203,6 +204,7 @@ class BlinkTestController : public base::NonThreadSafe,
   void OnTextDump(const std::string& dump);
   void OnInitiateLayoutDump();
   void OnLayoutDumpResponse(RenderFrameHost* sender, const std::string& dump);
+  void OnPrintMessageToStderr(const std::string& message);
   void OnPrintMessage(const std::string& message);
   void OnOverridePreferences(const WebPreferences& prefs);
   void OnTestFinished();
