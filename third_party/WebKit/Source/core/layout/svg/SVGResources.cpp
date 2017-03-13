03b2668db70a3c8d5dcc1342c80fd50f59f6e4cb
fs <fs@opera.com>
2017-03-13 08:11:49 -0700

    Remove <filter> from the chainableResourceTags set
    We don't actually support inheritance for <filter>, so having it in the
    set is of no use.
    
    BUG=661598
    
    Review-Url: https://codereview.chromium.org/2743293002
    Cr-Commit-Position: refs/heads/master@{#456380}

--- third_party/WebKit/Source/core/layout/svg/SVGResources.cpp
+++ third_party/WebKit/Source/core/layout/svg/SVGResources.cpp
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/svg/LayoutSVGResourceMasker.h"
 #include "core/layout/svg/LayoutSVGResourcePaintServer.h"
 #include "core/style/ComputedStyle.h"
-#include "core/svg/SVGFilterElement.h"
 #include "core/svg/SVGGradientElement.h"
 #include "core/svg/SVGPatternElement.h"
 #include "core/svg/SVGURIReference.h"
@@ -103,8 +102,8 @@ static HashSet<AtomicString>& fillAndStrokeTags() {
 static HashSet<AtomicString>& chainableResourceTags() {
   DEFINE_STATIC_LOCAL(HashSet<AtomicString>, s_tagList,
                       ({
-                          linearGradientTag.localName(), filterTag.localName(),
-                          patternTag.localName(), radialGradientTag.localName(),
+                          linearGradientTag.localName(), patternTag.localName(),
+                          radialGradientTag.localName(),
                       }));
   return s_tagList;
 }
@@ -115,8 +114,6 @@ static inline AtomicString targetReferenceFromResource(SVGElement& element) {
     target = toSVGPatternElement(element).href()->currentValue()->value();
   else if (isSVGGradientElement(element))
     target = toSVGGradientElement(element).href()->currentValue()->value();
-  else if (isSVGFilterElement(element))
-    target = toSVGFilterElement(element).href()->currentValue()->value();
   else
     ASSERT_NOT_REACHED();
 
