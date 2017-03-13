f749f1ad9ab6ee20916a38ea934c9fc8d1f15cad
teravest <teravest@chromium.org>
2017-03-13 14:45:26 -0700

    Revert of Split LivenessService out from LibCrosService. (patchset #3 id:40001 of https://codereview.chromium.org/2741803005/ )
    Reason for revert:
    Another ChromeOS change has to land which grants permissions to this name.
    
    Original issue's description:
    > Split LivenessService out from LibCrosService.
    >
    > This is part of a larger effort to split LibCrosService into smaller
    > services for mus+ash.
    >
    > I've added a TODO to remove the old service once we've migrated
    > clients over to call the new one.
    >
    > BUG=692246
    >
    > Review-Url: https://codereview.chromium.org/2741803005
    > Cr-Commit-Position: refs/heads/master@{#456411}
    > Committed: https://chromium.googlesource.com/chromium/src/+/074cfdfc2e36001a72f2e2dfbcd7cc91819221e4
    
    TBR=derat@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=692246
    
    Review-Url: https://codereview.chromium.org/2751543002
    Cr-Commit-Position: refs/heads/master@{#456499}

--- chrome/browser/chromeos/chrome_browser_main_chromeos.cc
+++ chrome/browser/chromeos/chrome_browser_main_chromeos.cc
@@ -239,10 +239,7 @@ class DBusServices {
       service_providers.push_back(base::MakeUnique<DisplayPowerServiceProvider>(
           base::MakeUnique<ChromeDisplayPowerServiceProviderDelegate>()));
     }
-    // TODO(teravest): Remove this service provider once clients use
-    // LivenessService instead of LibCrosService.
-    service_providers.push_back(
-        base::MakeUnique<LivenessServiceProvider>(kLibCrosServiceInterface));
+    service_providers.push_back(base::MakeUnique<LivenessServiceProvider>());
     service_providers.push_back(base::MakeUnique<ScreenLockServiceProvider>());
     if (ash_util::IsRunningInMash()) {
       service_providers.push_back(base::MakeUnique<ConsoleServiceProvider>(
@@ -255,13 +252,6 @@ class DBusServices {
     cros_dbus_service_ = CrosDBusService::Create(
         kLibCrosServiceName, dbus::ObjectPath(kLibCrosServicePath),
         std::move(service_providers));
-
-    CrosDBusService::ServiceProviderList liveness_service_providers;
-    liveness_service_providers.push_back(
-        base::MakeUnique<LivenessServiceProvider>(kLivenessServiceInterface));
-    liveness_service_ = CrosDBusService::Create(
-        kLivenessServiceName, dbus::ObjectPath(kLivenessServicePath),
-        std::move(liveness_service_providers));
 
     // Initialize PowerDataCollector after DBusThreadManager is initialized.
     PowerDataCollector::Initialize();
@@ -312,7 +302,6 @@ class DBusServices {
     CertLoader::Shutdown();
     TPMTokenLoader::Shutdown();
     cros_dbus_service_.reset();
-    liveness_service_.reset();
     PowerDataCollector::Shutdown();
     PowerPolicyController::Shutdown();
     device::BluetoothAdapterFactory::Shutdown();
@@ -329,8 +318,6 @@ class DBusServices {
   // TODO(derat): Move these providers into more-specific services that are
   // split between different processes: http://crbug.com/692246
   std::unique_ptr<CrosDBusService> cros_dbus_service_;
-
-  std::unique_ptr<CrosDBusService> liveness_service_;
 
   std::unique_ptr<NetworkConnectDelegateChromeOS> network_connect_delegate_;
 
