a8893ccf2a190399182d9bf951739eed94bcc0bf
robliao <robliao@chromium.org>
2017-03-13 11:30:20 -0700

    Redirect SingleThreadTaskRunner creation to the SchedulerSingleThreadTaskRunnerManager
    Reference Change:
    https://codereview.chromium.org/2686593003/
    
    BUG=684080
    
    Review-Url: https://codereview.chromium.org/2700253002
    Cr-Commit-Position: refs/heads/master@{#456438}

--- base/task_scheduler/task_scheduler_impl.cc
+++ base/task_scheduler/task_scheduler_impl.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "base/memory/ptr_util.h"
 #include "base/task_scheduler/delayed_task_manager.h"
+#include "base/task_scheduler/scheduler_single_thread_task_runner_manager.h"
 #include "base/task_scheduler/scheduler_worker_pool_params.h"
 #include "base/task_scheduler/sequence_sort_key.h"
 #include "base/task_scheduler/task.h"
@@ -67,8 +68,8 @@ TaskSchedulerImpl::CreateSequencedTaskRunnerWithTraits(
 scoped_refptr<SingleThreadTaskRunner>
 TaskSchedulerImpl::CreateSingleThreadTaskRunnerWithTraits(
     const TaskTraits& traits) {
-  return GetWorkerPoolForTraits(traits)->CreateSingleThreadTaskRunnerWithTraits(
-      traits);
+  return single_thread_task_runner_manager_
+      ->CreateSingleThreadTaskRunnerWithTraits(traits);
 }
 
 std::vector<const HistogramBase*> TaskSchedulerImpl::GetHistograms() const {
@@ -99,6 +100,7 @@ void TaskSchedulerImpl::JoinForTesting() {
 #if DCHECK_IS_ON()
   DCHECK(!join_for_testing_returned_.IsSet());
 #endif
+  single_thread_task_runner_manager_->JoinForTesting();
   for (const auto& worker_pool : worker_pools_)
     worker_pool->DisallowWorkerDetachmentForTesting();
   for (const auto& worker_pool : worker_pools_)
@@ -148,6 +150,11 @@ void TaskSchedulerImpl::Initialize(
   // thread to get its task_runner().
   delayed_task_manager_ =
       base::MakeUnique<DelayedTaskManager>(service_thread_.task_runner());
+
+  single_thread_task_runner_manager_ =
+      MakeUnique<SchedulerSingleThreadTaskRunnerManager>(
+          worker_pool_params_vector, worker_pool_index_for_traits_callback_,
+          task_tracker_.get(), delayed_task_manager_.get());
 
   // Callback invoked by workers to re-enqueue a sequence in the appropriate
   // PriorityQueue.
