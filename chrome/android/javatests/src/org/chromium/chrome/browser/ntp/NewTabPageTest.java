691907bf1eaecd69e89050e4ae44b6591091fe8b
cblume <cblume@chromium.org>
2017-03-13 13:36:27 -0700

    Fix typo in getUrlFocusAnimatonsDisabled()
    There is a typo. It should be "Animations".
    
    R=tedchoc@chromium.org
    BUG=701012
    
    Review-Url: https://codereview.chromium.org/2745263002
    Cr-Commit-Position: refs/heads/master@{#456468}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
@@ -287,7 +287,7 @@ public class NewTabPageTest extends ChromeTabbedActivityTestBase {
     @MediumTest
     @Feature({"NewTabPage"})
     public void testUrlFocusAnimationsDisabledOnLoad() throws InterruptedException {
-        assertFalse(getUrlFocusAnimatonsDisabled());
+        assertFalse(getUrlFocusAnimationsDisabled());
         ChromeTabUtils.waitForTabPageLoaded(mTab, new Runnable() {
             @Override
             public void run() {
@@ -299,13 +299,13 @@ public class NewTabPageTest extends ChromeTabbedActivityTestBase {
                         mTab.loadUrl(new LoadUrlParams(mTestServer.getURL(TEST_PAGE),
                                 pageTransition));
                         // It should be disabled as soon as a load URL is triggered.
-                        assertTrue(getUrlFocusAnimatonsDisabled());
+                        assertTrue(getUrlFocusAnimationsDisabled());
                     }
                 });
             }
         });
         // Ensure it is still marked as disabled once the new page is fully loaded.
-        assertTrue(getUrlFocusAnimatonsDisabled());
+        assertTrue(getUrlFocusAnimationsDisabled());
     }
 
     @LargeTest
@@ -329,7 +329,7 @@ public class NewTabPageTest extends ChromeTabbedActivityTestBase {
                     "/ntp_test.html",
                     "<html><body></body></html>", null, delayAction);
 
-            assertFalse(getUrlFocusAnimatonsDisabled());
+            assertFalse(getUrlFocusAnimationsDisabled());
 
             clickFakebox();
             UrlBar urlBar = (UrlBar) getActivity().findViewById(R.id.url_bar);
@@ -363,7 +363,7 @@ public class NewTabPageTest extends ChromeTabbedActivityTestBase {
             waitForUrlFocusAnimationsDisabledState(false);
             delaySemaphore.release();
             loadedCallback.waitForCallback(0);
-            assertFalse(getUrlFocusAnimatonsDisabled());
+            assertFalse(getUrlFocusAnimationsDisabled());
         } finally {
             webServer.shutdown();
         }
@@ -471,7 +471,7 @@ public class NewTabPageTest extends ChromeTabbedActivityTestBase {
         mNtp.captureThumbnail(canvas);
     }
 
-    private boolean getUrlFocusAnimatonsDisabled() {
+    private boolean getUrlFocusAnimationsDisabled() {
         return ThreadUtils.runOnUiThreadBlockingNoException(new Callable<Boolean>() {
             @Override
             public Boolean call() throws Exception {
@@ -484,7 +484,7 @@ public class NewTabPageTest extends ChromeTabbedActivityTestBase {
         CriteriaHelper.pollInstrumentationThread(Criteria.equals(disabled, new Callable<Boolean>() {
             @Override
             public Boolean call() {
-                return getUrlFocusAnimatonsDisabled();
+                return getUrlFocusAnimationsDisabled();
             }
         }));
     }
