26f415b08bfeaf4a459009c8e9a6701f98a84c53
andypaicu <andypaicu@chromium.org>
2017-03-13 07:37:10 -0700

    Stop CSP from matching independent scheme/port upgrades
    Content-Security-Policy allows an url to match a source-expression even
    if the scheme or the port doesn't matches, but in this case it must be an
    upgrade to a more secure scheme(http->https) and more secure port(80->443).
    The problem is that it happens independently, so it is allowed to have an
    upgrade of the port without the scheme (http over 443) or an upgrade of the
    scheme without the port (https over 80).
    
    This is a change to force the upgrade to be both over port and scheme.
    
    BUG=692499,692442
    
    Review-Url: https://codereview.chromium.org/2708873002
    Cr-Commit-Position: refs/heads/master@{#456376}

--- third_party/WebKit/Source/core/frame/csp/ContentSecurityPolicy.cpp
+++ third_party/WebKit/Source/core/frame/csp/ContentSecurityPolicy.cpp
@@ -1480,10 +1480,12 @@ bool ContentSecurityPolicy::urlMatchesSelf(const KURL& url) const {
   return m_selfSource->matches(url, RedirectStatus::NoRedirect);
 }
 
-bool ContentSecurityPolicy::protocolMatchesSelf(const KURL& url) const {
-  if (equalIgnoringCase("http", m_selfProtocol))
-    return url.protocolIsInHTTPFamily();
-  return equalIgnoringCase(url.protocol(), m_selfProtocol);
+bool ContentSecurityPolicy::protocolEqualsSelf(const String& protocol) const {
+  return equalIgnoringCase(protocol, m_selfProtocol);
+}
+
+const String& ContentSecurityPolicy::getSelfProtocol() const {
+  return m_selfProtocol;
 }
 
 bool ContentSecurityPolicy::shouldBypassMainWorld(
