b617a198d0d55b2b58014579eb8007416d37db12
eugenebut <eugenebut@chromium.org>
2017-03-14 11:04:15 -0700

    Call verifyTrust:completionHandler: callback on shutdown.
    WKWebView throws NSInternalInconsistencyException if completion handler
    passed to webView:didReceiveAuthenticationChallenge:completionHandler:
    was not called on shutdown.
    
    To preven this verifyTrust:completionHandler: should always call its
    completion handlers (even during shutdown).
    
    BUG=663305
    
    Review-Url: https://codereview.chromium.org/2734113003
    Cr-Commit-Position: refs/heads/master@{#456761}

--- ios/web/net/crw_cert_verification_controller.mm
+++ ios/web/net/crw_cert_verification_controller.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/mac/bind_objc_block.h"
 #include "base/memory/ref_counted.h"
 #include "base/strings/sys_string_conversions.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/worker_pool.h"
 #include "ios/web/public/browser_state.h"
 #include "ios/web/public/certificate_policy_cache.h"
@@ -193,18 +194,20 @@ decideLoadPolicyForRejectedTrustResult:(SecTrustResultType)trustResult
   DCHECK(completionHandler);
   // SecTrustEvaluate performs trust evaluation synchronously, possibly making
   // network requests. The UI thread should not be blocked by that operation.
-  base::WorkerPool::PostTask(
-      FROM_HERE, base::BindBlockArc(^{
+  base::PostTaskWithTraits(
+      FROM_HERE,
+      base::TaskTraits().WithShutdownBehavior(
+          base::TaskShutdownBehavior::BLOCK_SHUTDOWN),
+      base::BindBlockArc(^{
         SecTrustResultType trustResult = kSecTrustResultInvalid;
         if (SecTrustEvaluate(trust.get(), &trustResult) != errSecSuccess) {
           trustResult = kSecTrustResultInvalid;
         }
-        web::WebThread::PostTask(web::WebThread::UI, FROM_HERE,
-                                 base::BindBlockArc(^{
-                                   completionHandler(trustResult);
-                                 }));
-      }),
-      false /* task_is_slow */);
+        // Use GCD API, which is guaranteed to be called during shutdown.
+        dispatch_async(dispatch_get_main_queue(), ^{
+          completionHandler(trustResult);
+        });
+      }));
 }
 
 - (web::CertAcceptPolicy)
