cc009c384b032a41454f67ed612278817e616d48
jbudorick <jbudorick@chromium.org>
2017-03-14 14:20:28 -0700

    [android] Clean up test_runner.py arguments.
    This includes:
      - reformatting
      - purging dead / deprecated / unused arguments
    
    Review-Url: https://codereview.chromium.org/2605083002
    Cr-Commit-Position: refs/heads/master@{#456835}

--- build/android/pylib/local/machine/local_machine_environment.py
+++ build/android/pylib/local/machine/local_machine_environment.py
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import devil_chromium
+from pylib import constants
 from pylib.base import environment
 
 
@@ -10,6 +12,9 @@ class LocalMachineEnvironment(environment.Environment):
 
   def __init__(self, _args, _error_func):
     super(LocalMachineEnvironment, self).__init__()
+
+    devil_chromium.Initialize(
+        output_directory=constants.GetOutDirectory())
 
   #override
   def SetUp(self):
