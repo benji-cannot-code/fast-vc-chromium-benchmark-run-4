a7c3f0e428562c70fa1190599f87d4544a95d3f0
vabr <vabr@chromium.org>
2017-03-14 13:06:11 -0700

    Temporarily CHECK use after free in Value
    One of the hypotheses for https://crbug.com/697817 is that a Value is used
    after free (see #5 there). To verify/refute that hypothesis, this CL
    temporarily introduces and CHECKS an |alive_| flag. This change will be
    reverted after a couple of days in Canary, and only serves investigation
    purposes.
    
    BUG=697817
    
    Review-Url: https://codereview.chromium.org/2743363004
    Cr-Commit-Position: refs/heads/master@{#456801}

--- base/values.cc
+++ base/values.cc
@@ -201,6 +201,7 @@ Value& Value::operator=(Value&& that) {
 
 Value::~Value() {
   InternalCleanup();
+  alive_ = false;
 }
 
 // static
@@ -590,6 +591,8 @@ void Value::InternalMoveAssignFromSameType(Value&& that) {
 }
 
 void Value::InternalCleanup() {
+  CHECK(alive_);
+
   switch (type_) {
     case Type::NONE:
     case Type::BOOLEAN:
