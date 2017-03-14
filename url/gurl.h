c0f52013191edd49dbcf94bc9e142e20936d0ab6
sclittle <sclittle@chromium.org>
2017-03-14 14:08:41 -0700

    Add move constructors and assignment operators to GURL
    This CL implements a cheap move constructor and assignment operator for
    GURL, so that code that moves around GURLs a lot (e.g. an
    std::vector<GURL>) doesn't need to perform so many copies.
    
    BUG=698978
    
    Review-Url: https://codereview.chromium.org/2737693003
    Cr-Commit-Position: refs/heads/master@{#456828}

--- url/gurl.h
+++ url/gurl.h
@@ -55,6 +55,7 @@ class URL_EXPORT GURL {
   // Copy construction is relatively inexpensive, with most of the time going
   // to reallocating the string. It does not re-parse.
   GURL(const GURL& other);
+  GURL(GURL&& other);
 
   // The strings to this contructor should be UTF-8 / UTF-16.
   explicit GURL(base::StringPiece url_string);
@@ -77,7 +78,8 @@ class URL_EXPORT GURL {
 
   ~GURL();
 
-  GURL& operator=(GURL other);
+  GURL& operator=(const GURL& other);
+  GURL& operator=(GURL&& other);
 
   // Returns true when this object represents a valid parsed URL. When not
   // valid, other functions will still succeed, but you will not get canonical
