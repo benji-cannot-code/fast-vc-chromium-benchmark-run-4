cbb6d2f30592578f5b8c0a47952b71299ec5c9ae
lizeb <lizeb@chromium.org>
2017-03-14 03:51:18 -0700

    predictors: Correctly set the referrer on the prefetching requests.
    BUG=700886
    
    Review-Url: https://codereview.chromium.org/2748583003
    Cr-Commit-Position: refs/heads/master@{#456670}

--- chrome/browser/predictors/resource_prefetcher.cc
+++ chrome/browser/predictors/resource_prefetcher.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/trace_event/trace_event.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/common/referrer.h"
 #include "net/base/io_buffer.h"
 #include "net/base/load_flags.h"
 #include "net/base/request_priority.h"
@@ -147,7 +148,13 @@ void ResourcePrefetcher::SendRequest(const GURL& url) {
   url_request->set_method("GET");
   url_request->set_first_party_for_cookies(main_frame_url_);
   url_request->set_initiator(url::Origin(main_frame_url_));
-  url_request->SetReferrer(main_frame_url_.spec());
+
+  content::Referrer referrer(main_frame_url_, blink::WebReferrerPolicyDefault);
+  content::Referrer sanitized_referrer =
+      content::Referrer::SanitizeForRequest(url, referrer);
+  content::Referrer::SetReferrerForRequest(url_request.get(),
+                                           sanitized_referrer);
+
   url_request->SetLoadFlags(url_request->load_flags() | net::LOAD_PREFETCH);
   StartURLRequest(url_request.get());
   inflight_requests_.insert(
