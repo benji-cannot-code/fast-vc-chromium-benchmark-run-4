09ea6ae41188322d72997154cc020300772ceb8d
irisu <irisu@chromium.org>
2017-03-14 02:34:34 -0700

    Add headless chrome as an accepted browser string in chromedriver.
    This is working towards selenium support for headless see https://bugs.chromium.org/p/chromium/issues/detail?id=604324
    
    BUG=699382,604324
    
    Review-Url: https://codereview.chromium.org/2731403008
    Cr-Commit-Position: refs/heads/master@{#456660}

--- chrome/test/chromedriver/chrome/browser_info.cc
+++ chrome/test/chromedriver/chrome/browser_info.cc
@@ -20,6 +20,9 @@ namespace {
 const char kVersionPrefix[] = "Chrome/";
 const size_t kVersionPrefixLen = sizeof(kVersionPrefix) - 1;
 
+const char kHeadlessVersionPrefix[] = "HeadlessChrome/";
+const size_t kHeadlessVersionPrefixLen = sizeof(kHeadlessVersionPrefix) - 1;
+
 }  // namespace
 
 BrowserInfo::BrowserInfo()
@@ -87,10 +90,21 @@ Status ParseBrowserString(bool has_android_package,
     return Status(kOk);
   }
 
+  const Status error =
+      Status(kUnknownError, "unrecognized Chrome version: " + browser_string);
+
   int build_no = 0;
   if (base::StartsWith(browser_string, kVersionPrefix,
+                       base::CompareCase::SENSITIVE) ||
+      base::StartsWith(browser_string, kHeadlessVersionPrefix,
                        base::CompareCase::SENSITIVE)) {
     std::string version = browser_string.substr(kVersionPrefixLen);
+    bool headless = false;
+    if (base::StartsWith(browser_string, kHeadlessVersionPrefix,
+                         base::CompareCase::SENSITIVE)) {
+      version = browser_string.substr(kHeadlessVersionPrefixLen);
+      headless = true;
+    }
 
     Status status = ParseBrowserVersionString(
         version, &browser_info->major_version, &build_no);
@@ -98,7 +112,10 @@ Status ParseBrowserString(bool has_android_package,
       return status;
 
     if (build_no != 0) {
-      browser_info->browser_name = "chrome";
+      if (headless)
+        browser_info->browser_name = "headless chrome";
+      else
+        browser_info->browser_name = "chrome";
       browser_info->browser_version = version;
       browser_info->build_no = build_no;
       return Status(kOk);
@@ -119,8 +136,7 @@ Status ParseBrowserString(bool has_android_package,
     return Status(kOk);
   }
 
-  return Status(kUnknownError,
-                "unrecognized Chrome version: " + browser_string);
+  return error;
 }
 
 Status ParseBrowserVersionString(const std::string& browser_version,
