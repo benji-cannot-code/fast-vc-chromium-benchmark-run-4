05290d4dfc4efd14730b529c2daeafa79eacf1da
jam <jam@chromium.org>
2017-03-14 13:29:13 -0700

    Fix LoadDataWithBaseUrlTest#testloadDataWithBaseUrlCallsOnPageStarted with PlzNavigate.
    Ensure that when loadDataWithBaseUrl is called, onPageStarted uses the base url for the data url.
    
    BUG=645983
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2745033002
    Cr-Commit-Position: refs/heads/master@{#456814}

--- content/browser/frame_host/navigation_handle_impl.h
+++ content/browser/frame_host/navigation_handle_impl.h
@@ -163,6 +163,7 @@ class CONTENT_EXPORT NavigationHandleImpl : public NavigationHandle {
   const std::string& GetSearchableFormEncoding() override;
   ReloadType GetReloadType() override;
   RestoreType GetRestoreType() override;
+  const GURL& GetBaseURLForDataURL() override;
   const GlobalRequestID& GetGlobalRequestID() override;
 
   NavigationData* GetNavigationData() override;
@@ -352,6 +353,10 @@ class CONTENT_EXPORT NavigationHandleImpl : public NavigationHandle {
     complete_callback_for_testing_ = callback;
   }
 
+  void set_base_url_for_data_url(const GURL& url) {
+    base_url_for_data_url_ = url;
+  }
+
  private:
   friend class NavigationHandleImplTest;
 
@@ -513,6 +518,7 @@ class CONTENT_EXPORT NavigationHandleImpl : public NavigationHandle {
 
   GURL previous_url_;
   GURL base_url_;
+  GURL base_url_for_data_url_;
   net::HostPortPair socket_address_;
   NavigationType navigation_type_;
 
