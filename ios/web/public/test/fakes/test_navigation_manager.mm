04ffa8c549a95de4c7d732f223d2887d71c3ac44
pkl <pkl@chromium.org>
2017-03-14 15:48:28 -0700

    Removed use of CRWSessionController from NativeAppNavigationUtilsTest.
    Previous implementation uses WebStateImpl, NavigationManagerImpl, and
    CRWSessionController which are all private. This new version uses a
    TestNavigationManager that is all "faked".
    
    BUG=619984
    
    Review-Url: https://codereview.chromium.org/2742173002
    Cr-Commit-Position: refs/heads/master@{#456873}

--- ios/web/public/test/fakes/test_navigation_manager.mm
+++ ios/web/public/test/fakes/test_navigation_manager.mm
@@ -8,7 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace web {
 
 TestNavigationManager::TestNavigationManager()
-    : pending_item_(nullptr),
+    : items_index_(-1),
+      pending_item_(nullptr),
       last_committed_item_(nullptr),
       visible_item_(nullptr) {}
 
@@ -72,18 +73,20 @@ void TestNavigationManager::AddTransientURLRewriter(
 }
 
 int TestNavigationManager::GetItemCount() const {
-  NOTREACHED();
-  return 0;
+  return items_.size();
 }
 
 web::NavigationItem* TestNavigationManager::GetItemAtIndex(size_t index) const {
-  NOTREACHED();
-  return nullptr;
+  return items_[index].get();
 }
 
 int TestNavigationManager::GetCurrentItemIndex() const {
-  NOTREACHED();
-  return 0;
+  return items_index_;
+}
+
+void TestNavigationManager::SetCurrentItemIndex(const int index) {
+  DCHECK(index == -1 || index >= 0 && index < GetItemCount());
+  items_index_ = index;
 }
 
 int TestNavigationManager::GetLastCommittedItemIndex() const {
@@ -97,8 +100,13 @@ int TestNavigationManager::GetPendingItemIndex() const {
 }
 
 bool TestNavigationManager::RemoveItemAtIndex(int index) {
-  NOTREACHED();
-  return false;
+  if (index < 0 || index >= GetItemCount())
+    return false;
+  DCHECK(items_index_ != index);
+  items_.erase(items_.begin() + index);
+  if (items_index_ > index)
+    --items_index_;
+  return true;
 }
 
 bool TestNavigationManager::CanGoBack() const {
@@ -145,6 +153,16 @@ NavigationItemList TestNavigationManager::GetForwardItems() const {
 
 void TestNavigationManager::OverrideDesktopUserAgentForNextPendingItem() {
   NOTREACHED();
+}
+
+// Adds a new navigation item of |transition| type at the end of this
+// navigation manager.
+void TestNavigationManager::AddItem(const GURL& url,
+                                    ui::PageTransition transition) {
+  items_.push_back(web::NavigationItem::Create());
+  items_.back()->SetTransitionType(transition);
+  items_.back()->SetURL(url);
+  SetCurrentItemIndex(GetItemCount() - 1);
 }
 
 }  // namespace web
