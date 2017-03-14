b02381ab019d3012af66af1e69b6019eda928b1e
tapted <tapted@chromium.org>
2017-03-13 23:41:32 -0700

    GridLayout: Guard against layout queries while adding a view.
    Adds a DCHECK to GridLayout to ensure client code doesn't ask it to
    perform layout calculations while inside GridLayout::AddViewState().
    GridLayout has not yet updated its internal data structures, so it can
    only give invalid results at this point.
    
    Adds a BrowserDialogTest for the edit bookmark view which would have
    triggered this DCHECK without the fix in r455438.
    
    Adds a DCHECK_DEATH test for GridLayout.
    
    BUG=699912, 699390
    
    Review-Url: https://codereview.chromium.org/2743743003
    Cr-Commit-Position: refs/heads/master@{#456638}

---
+++ chrome/browser/ui/views/bookmarks/bookmark_editor_view_browsertest.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/bookmarks/bookmark_editor_view.h"
+
+#include "chrome/browser/ui/bookmarks/bookmark_utils_desktop.h"
+#include "chrome/browser/ui/test/test_browser_dialog.h"
+
+// Test harness for integration tests using BookmarkEditorView.
+class BookmarkEditorViewBrowserTest : public DialogBrowserTest {
+ public:
+  BookmarkEditorViewBrowserTest() {}
+
+  // DialogBrowserTest:
+  void ShowDialog(const std::string& name) override {
+    DCHECK_EQ("all_tabs", name);
+    chrome::ShowBookmarkAllTabsDialog(browser());
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(BookmarkEditorViewBrowserTest);
+};
+
+// Shows the dialog for bookmarking all tabs. This shows a BookmarkEditorView
+// dialog, with a tree view, where a user can rename and select a parent folder.
+// Can be interactive when run with --gtest_filter=BrowserDialogTest.Invoke.
+IN_PROC_BROWSER_TEST_F(BookmarkEditorViewBrowserTest, InvokeDialog_all_tabs) {
+  RunDialog();
+}
