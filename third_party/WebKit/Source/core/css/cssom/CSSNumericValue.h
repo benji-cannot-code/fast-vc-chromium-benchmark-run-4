36acffa537aa2c370bb67c3d470e411812a3dbdd
meade <meade@chromium.org>
2017-03-13 23:49:28 -0700

    [CSS Typed OM] Add new files for CSSNumericValue.
    CSSNumericValue is a new base type that will be used for lengths,
    angles and numbers according to a spec change for Typed OM. The amended
    spec is here:
    https://drafts.css-houdini.org/css-typed-om/#cssnumericvalue
    
    BUG=545318
    
    Review-Url: https://codereview.chromium.org/2741683003
    Cr-Commit-Position: refs/heads/master@{#456641}

---
+++ third_party/WebKit/Source/core/css/cssom/CSSNumericValue.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CSSNumericValue_h
+#define CSSNumericValue_h
+
+#include "bindings/core/v8/ScriptWrappable.h"
+#include "core/CoreExport.h"
+#include "core/css/cssom/CSSStyleValue.h"
+#include "wtf/text/WTFString.h"
+
+namespace blink {
+
+class ExceptionState;
+
+class CORE_EXPORT CSSNumericValue : public CSSStyleValue {
+  WTF_MAKE_NONCOPYABLE(CSSNumericValue);
+  DEFINE_WRAPPERTYPEINFO();
+
+ public:
+  static CSSNumericValue* parse(const String& cssText, ExceptionState&);
+
+  virtual CSSNumericValue* add(const CSSNumericValue*, ExceptionState&) = 0;
+  virtual CSSNumericValue* sub(const CSSNumericValue*, ExceptionState&) = 0;
+  virtual CSSNumericValue* mul(double, ExceptionState&) = 0;
+  virtual CSSNumericValue* div(double, ExceptionState&) = 0;
+
+  virtual CSSNumericValue* to(const String&, ExceptionState&) = 0;
+};
+
+}  // namespace blink
+
+#endif  // CSSNumericValue_h
