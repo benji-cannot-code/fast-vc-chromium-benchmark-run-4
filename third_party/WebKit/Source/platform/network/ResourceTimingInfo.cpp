282e608c2ef8f04d6bc2fcb1131f1365eb7ff080
sunjian <sunjian@chromium.org>
2017-03-14 15:25:18 -0700

    Make ResourceTimingInfo RefCounted.
    Currently, ResourceTimingInfo's creation only returns a std::unique_ptr.
    However, a ref-counted ResourceTimingInfo instance is needed so that a
    single instance can be shared by multiple owners. The reason that this
    new feature is requested is because now we expose PerformanceNavigationTiming
    in a timely manner, which entails dynamically updating PerformanceNavigationTiming
    instance as more information gets collected. One ResourceTimingInfo instance will
    be held by a ResourceFether and a PerformanceNavigationTiming instance at the same
    time. ResourceFether will keep updating ResourceTimingInfo while
    PerformanceNavigationTiming consumes it. Therefore, making ResourceTimingInfo
    ref-counted is needed.
    
    BUG=701113
    
    Review-Url: https://codereview.chromium.org/2747933002
    Cr-Commit-Position: refs/heads/master@{#456865}

--- third_party/WebKit/Source/platform/network/ResourceTimingInfo.cpp
+++ third_party/WebKit/Source/platform/network/ResourceTimingInfo.cpp
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-std::unique_ptr<ResourceTimingInfo> ResourceTimingInfo::adopt(
+PassRefPtr<ResourceTimingInfo> ResourceTimingInfo::adopt(
     std::unique_ptr<CrossThreadResourceTimingInfoData> data) {
-  std::unique_ptr<ResourceTimingInfo> info = ResourceTimingInfo::create(
+  RefPtr<ResourceTimingInfo> info = ResourceTimingInfo::create(
       AtomicString(data->m_type), data->m_initialTime, data->m_isMainResource);
   info->m_originalTimingAllowOrigin =
       AtomicString(data->m_originalTimingAllowOrigin);
@@ -23,7 +23,7 @@ std::unique_ptr<ResourceTimingInfo> ResourceTimingInfo::adopt(
   for (auto& responseData : data->m_redirectChain)
     info->m_redirectChain.push_back(ResourceResponse(responseData.get()));
   info->m_transferSize = data->m_transferSize;
-  return info;
+  return info.release();
 }
 
 std::unique_ptr<CrossThreadResourceTimingInfoData>
