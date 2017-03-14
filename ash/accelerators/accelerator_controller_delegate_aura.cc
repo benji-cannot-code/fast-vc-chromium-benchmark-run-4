9b7adb9ce430d9b772469f6fdf8572ce1644d696
jamescook <jamescook@chromium.org>
2017-03-14 11:23:29 -0700

    chromeos: Enable Alt-Shift-S to show system tray bubble for mash
    It needed to be moved to the common accelerator handling code.
    
    This fixes SystemTrayTest.Activation in mash_unittests.
    
    BUG=695559,612331
    TEST=ash_unittests, mash_unittests
    
    Review-Url: https://codereview.chromium.org/2752643002
    Cr-Commit-Position: refs/heads/master@{#456765}

--- ash/accelerators/accelerator_controller_delegate_aura.cc
+++ ash/accelerators/accelerator_controller_delegate_aura.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/common/shelf/wm_shelf.h"
 #include "ash/common/shell_delegate.h"
 #include "ash/common/system/system_notifier.h"
-#include "ash/common/system/tray/system_tray.h"
 #include "ash/common/wm/maximize_mode/maximize_mode_controller.h"
 #include "ash/common/wm/window_state.h"
 #include "ash/common/wm/wm_event.h"
@@ -188,17 +187,6 @@ void HandleRotateActiveWindow() {
   }
 }
 
-void HandleShowSystemTrayBubble() {
-  base::RecordAction(UserMetricsAction("Accel_Show_System_Tray_Bubble"));
-  RootWindowController* controller =
-      RootWindowController::ForTargetRootWindow();
-  SystemTray* tray = controller->GetSystemTray();
-  if (!tray->HasSystemBubble()) {
-    tray->ShowDefaultView(BUBBLE_CREATE_NEW);
-    tray->ActivateBubble();
-  }
-}
-
 void HandleTakeWindowScreenshot(ScreenshotDelegate* screenshot_delegate) {
   base::RecordAction(UserMetricsAction("Accel_Take_Window_Screenshot"));
   DCHECK(screenshot_delegate);
@@ -295,7 +283,6 @@ bool AcceleratorControllerDelegateAura::HandlesAction(
     case SCALE_UI_RESET:
     case SCALE_UI_UP:
     case SHOW_MESSAGE_CENTER_BUBBLE:
-    case SHOW_SYSTEM_TRAY_BUBBLE:
     case SWAP_PRIMARY_DISPLAY:
     case TAKE_PARTIAL_SCREENSHOT:
     case TAKE_SCREENSHOT:
@@ -344,7 +331,6 @@ bool AcceleratorControllerDelegateAura::CanPerformAction(
     case POWER_RELEASED:
     case ROTATE_SCREEN:
     case ROTATE_WINDOW:
-    case SHOW_SYSTEM_TRAY_BUBBLE:
     case TAKE_PARTIAL_SCREENSHOT:
     case TAKE_SCREENSHOT:
     case TAKE_WINDOW_SCREENSHOT:
@@ -429,9 +415,6 @@ void AcceleratorControllerDelegateAura::PerformAction(
       break;
     case SCALE_UI_UP:
       accelerators::ZoomInternalDisplay(true /* up */);
-      break;
-    case SHOW_SYSTEM_TRAY_BUBBLE:
-      HandleShowSystemTrayBubble();
       break;
     case SWAP_PRIMARY_DISPLAY:
       HandleSwapPrimaryDisplay();
--- ash/common/accelerators/accelerator_controller.cc
+++ ash/common/accelerators/accelerator_controller.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/common/system/keyboard_brightness_control_delegate.h"
 #include "ash/common/system/status_area_widget.h"
 #include "ash/common/system/system_notifier.h"
+#include "ash/common/system/tray/system_tray.h"
 #include "ash/common/system/tray/system_tray_delegate.h"
 #include "ash/common/system/tray/system_tray_notifier.h"
 #include "ash/common/system/web_notification/web_notification_tray.h"
@@ -230,6 +231,16 @@ void HandleShowMessageCenterBubble() {
         status_area_widget->web_notification_tray();
     if (notification_tray->visible())
       notification_tray->ShowMessageCenterBubble();
+  }
+}
+
+void HandleShowSystemTrayBubble() {
+  base::RecordAction(UserMetricsAction("Accel_Show_System_Tray_Bubble"));
+  WmWindow* target_root = Shell::GetWmRootWindowForNewWindows();
+  SystemTray* tray = target_root->GetRootWindowController()->GetSystemTray();
+  if (!tray->HasSystemBubble()) {
+    tray->ShowDefaultView(BUBBLE_CREATE_NEW);
+    tray->ActivateBubble();
   }
 }
 
@@ -840,6 +851,7 @@ bool AcceleratorController::CanPerformAction(
     case RESTORE_TAB:
     case SHOW_IME_MENU_BUBBLE:
     case SHOW_KEYBOARD_OVERLAY:
+    case SHOW_SYSTEM_TRAY_BUBBLE:
     case SHOW_TASK_MANAGER:
     case SUSPEND:
     case TOGGLE_FULLSCREEN:
@@ -1018,6 +1030,9 @@ void AcceleratorController::PerformAction(AcceleratorAction action,
       break;
     case SHOW_STYLUS_TOOLS:
       HandleShowStylusTools();
+      break;
+    case SHOW_SYSTEM_TRAY_BUBBLE:
+      HandleShowSystemTrayBubble();
       break;
     case SHOW_TASK_MANAGER:
       HandleShowTaskManager();
