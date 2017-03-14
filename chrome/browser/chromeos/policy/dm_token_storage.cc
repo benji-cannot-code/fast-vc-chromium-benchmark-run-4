dc2581d000d306874ae2e0995bfca6980532c931
fdoray <fdoray@chromium.org>
2017-03-14 08:49:30 -0700

    Use TaskScheduler instead of blocking pool in dm_token_storage.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=bartfab@chromium.org
    
    Review-Url: https://codereview.chromium.org/2673783003
    Cr-Commit-Position: refs/heads/master@{#456712}

--- chrome/browser/chromeos/policy/dm_token_storage.cc
+++ chrome/browser/chromeos/policy/dm_token_storage.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/policy/dm_token_storage.h"
 
 #include "base/bind.h"
-#include "base/threading/sequenced_worker_pool.h"
+#include "base/task_scheduler/post_task.h"
 #include "chrome/browser/chromeos/settings/token_encryptor.h"
 #include "chrome/common/pref_names.h"
 #include "chromeos/cryptohome/system_salt_getter.h"
@@ -18,14 +18,12 @@ namespace {
 
 std::string EncryptToken(const std::string& system_salt,
                          const std::string& dm_token) {
-  DCHECK(content::BrowserThread::GetBlockingPool()->RunsTasksOnCurrentThread());
   chromeos::CryptohomeTokenEncryptor encryptor(system_salt);
   return encryptor.EncryptWithSystemSalt(dm_token);
 }
 
 std::string DecryptToken(const std::string& system_salt,
                          const std::string encrypted_dm_token) {
-  DCHECK(content::BrowserThread::GetBlockingPool()->RunsTasksOnCurrentThread());
   chromeos::CryptohomeTokenEncryptor encryptor(system_salt);
   return encryptor.DecryptWithSystemSalt(encrypted_dm_token);
 }
@@ -130,8 +128,10 @@ void DMTokenStorage::EncryptAndStoreToken() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   DCHECK(!system_salt_.empty());
   DCHECK(!dm_token_.empty());
-  base::PostTaskAndReplyWithResult(
-      content::BrowserThread::GetBlockingPool(), FROM_HERE,
+  base::PostTaskWithTraitsAndReplyWithResult(
+      FROM_HERE,
+      base::TaskTraits().MayBlock().WithPriority(
+          base::TaskPriority::BACKGROUND),
       base::Bind(&EncryptToken, system_salt_, dm_token_),
       base::Bind(&DMTokenStorage::OnTokenEncrypted,
                  weak_ptr_factory_.GetWeakPtr()));
@@ -153,8 +153,10 @@ void DMTokenStorage::LoadAndDecryptToken() {
   std::string encrypted_dm_token =
       local_state_->GetString(prefs::kDeviceDMToken);
   if (!encrypted_dm_token.empty()) {
-    base::PostTaskAndReplyWithResult(
-        content::BrowserThread::GetBlockingPool(), FROM_HERE,
+    base::PostTaskWithTraitsAndReplyWithResult(
+        FROM_HERE,
+        base::TaskTraits().MayBlock().WithPriority(
+            base::TaskPriority::BACKGROUND),
         base::Bind(&DecryptToken, system_salt_, encrypted_dm_token),
         base::Bind(&DMTokenStorage::FlushRetrieveTokenCallback,
                    weak_ptr_factory_.GetWeakPtr()));
