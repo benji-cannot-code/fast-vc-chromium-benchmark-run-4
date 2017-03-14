2561e95d97a84e1a2ac881e0b44d7f29e7d5f62e
rsesek <rsesek@chromium.org>
2017-03-14 11:51:13 -0700

    In make_hfs.sh, explicitly mount the new volume.
    This should happen automatically after erasedisk, but perhaps it's not
    completing synchronously.
    
    BUG=696529
    R=thakis@chromium.org
    
    Review-Url: https://codereview.chromium.org/2749033002
    Cr-Commit-Position: refs/heads/master@{#456775}

--- chrome/test/data/safe_browsing/dmg/make_hfs.sh
+++ chrome/test/data/safe_browsing/dmg/make_hfs.sh
@@ -34,6 +34,7 @@ fi
 
 RAMDISK_VOLUME=$(hdiutil attach -nomount ram://$RAMDISK_SIZE)
 diskutil erasevolume "${FILESYSTEM_TYPE}" "${VOLUME_NAME}" ${RAMDISK_VOLUME}
+diskutil mount ${RAMDISK_VOLUME}
 
 pushd "/Volumes/${VOLUME_NAME}"
 
