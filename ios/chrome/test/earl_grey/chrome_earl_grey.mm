2e966017225d0e71e177a14cd5140a789ecbeb9c
liaoyuke <liaoyuke@chromium.org>
2017-03-14 15:36:39 -0700

    Refactor Assert[No]CookieExists to return a boolean
    Instead of asserting inside helper functions, this CL refactors
    Assert[NO]CookieExists functions to return a boolean value and have the
    test asserts explicitly.
    
    This not only makes the intention of the functions more clear but also
    allows more flexible handling of the error, such as wrap it inside a
    wait condition or customize error message etc.
    
    BUG=693580
    
    Review-Url: https://codereview.chromium.org/2734963004
    Cr-Commit-Position: refs/heads/master@{#456868}

--- ios/chrome/test/earl_grey/chrome_earl_grey.mm
+++ ios/chrome/test/earl_grey/chrome_earl_grey.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/Foundation.h>
 #import <WebKit/WebKit.h>
 
+#include "base/mac/foundation_util.h"
 #include "base/strings/sys_string_conversions.h"
 #import "ios/chrome/test/app/chrome_test_util.h"
 #import "ios/chrome/test/app/history_test_util.h"
@@ -65,6 +66,33 @@ id ExecuteJavaScript(NSString* javascript,
   // After clearing browsing history via code, wait for the UI to be done
   // with any updates. This includes icons from the new tab page being removed.
   [[GREYUIThreadExecutor sharedInstance] drainUntilIdle];
+}
+
+#pragma mark - Cookie Utilities
+
++ (NSDictionary*)cookies {
+  NSString* const kGetCookiesScript =
+      @"document.cookie ? document.cookie.split(/;\\s*/) : [];";
+
+  // TODO(crbug.com/690057): Remove __unsafe_unretained once all callers of
+  // |ExecuteJavaScript| are converted to ARC.
+  NSError* __unsafe_unretained error = nil;
+  id result = chrome_test_util::ExecuteJavaScript(kGetCookiesScript, &error);
+
+  GREYAssertTrue(result && !error, @"Failed to get cookies.");
+
+  NSArray* nameValuePairs = base::mac::ObjCCastStrict<NSArray>(result);
+  NSMutableDictionary* cookies = [NSMutableDictionary dictionary];
+  for (NSString* nameValuePair in nameValuePairs) {
+    NSArray* cookieNameValue = [nameValuePair componentsSeparatedByString:@"="];
+    GREYAssertEqual(2U, cookieNameValue.count, @"Cookie has invalid format.");
+
+    NSString* cookieName = cookieNameValue[0];
+    NSString* cookieValue = cookieNameValue[1];
+    cookies[cookieName] = cookieValue;
+  }
+
+  return cookies;
 }
 
 #pragma mark - Navigation Utilities
