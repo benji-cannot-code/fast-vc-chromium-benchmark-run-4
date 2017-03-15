a14ca89b634a375f007f28f1208d1b829964505f
pilgrim <pilgrim@chromium.org>
2017-03-15 09:05:56 -0700

    Migrate WTF::Deque::remove() to ::erase()
    As discussed on platform-architecture-dev [0], there is interest in
    normalizing the methods of WTF classes to better align with std classes.
    
    There are no functional changes.
    
    [0] https://groups.google.com/a/chromium.org/d/topic/platform-architecture-dev/I7jnz4p1h84/discussion
    
    BUG=662431
    
    Review-Url: https://codereview.chromium.org/2747373004
    Cr-Commit-Position: refs/heads/master@{#457097}

--- third_party/WebKit/Source/bindings/core/v8/RejectedPromises.cpp
+++ third_party/WebKit/Source/bindings/core/v8/RejectedPromises.cpp
@@ -205,7 +205,7 @@ void RejectedPromises::handlerAdded(v8::PromiseRejectMessage data) {
   // by processQueue().
   for (auto it = m_queue.begin(); it != m_queue.end(); ++it) {
     if (!(*it)->isCollected() && (*it)->hasPromise(data.GetPromise())) {
-      m_queue.remove(it);
+      m_queue.erase(it);
       return;
     }
   }
--- third_party/WebKit/Source/core/editing/spellcheck/SpellCheckRequester.cpp
+++ third_party/WebKit/Source/core/editing/spellcheck/SpellCheckRequester.cpp
@@ -226,7 +226,7 @@ void SpellCheckRequester::enqueueRequest(SpellCheckRequest* request) {
                               queuedRequest->rootEditableElement();
                      });
     if (sameElementRequest != m_requestQueue.end())
-      m_requestQueue.remove(sameElementRequest);
+      m_requestQueue.erase(sameElementRequest);
   }
 
   m_requestQueue.push_back(request);
