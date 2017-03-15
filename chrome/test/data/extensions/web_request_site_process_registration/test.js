b9eb69744ac2177f27a2f13e35c628c40a0a4702
nasko <nasko@chromium.org>
2017-03-14 17:42:58 -0700

    Check for already existing entry when adding to SiteProcessMap.
    The SiteProcessMap::RegisterProcess can overwrite existing entries
    in the map, which can lead to incorrect process being mapped to
    a site in process-per-site mode. This CL checks for existence and
    avoids the overwrite.
    
    BUG=700514
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2739193004
    Cr-Commit-Position: refs/heads/master@{#456920}

---
+++ chrome/test/data/extensions/web_request_site_process_registration/test.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    if (details.url == chrome.extension.getURL("/blocked.html")) {
+      return {cancel: true};
+    }
+  },
+  {urls: ["<all_urls>"]},
+  ["blocking"]
+);
