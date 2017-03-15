fae89f85178bf7f3a99d1c3cad46ccb6f9edccec
chrishtr <chrishtr@chromium.org>
2017-03-14 22:27:02 -0700

    Factor gfx::Transform and skia_util into their own component.
    This allows us to remove a circular dependency between //cc/paint
    and //ui/gfx that will arise when we add new classes in cc to
    //cc/paint.
    
    BUG=665259
    
    Review-Url: https://codereview.chromium.org/2752693002
    Cr-Commit-Position: refs/heads/master@{#457000}

--- ui/gfx/skia_util.cc
+++ ui/gfx/skia_util.cc
@@ -19,8 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/quad_f.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rect_f.h"
-#include "ui/gfx/image/image_skia_rep.h"
-#include "ui/gfx/shadow_value.h"
 #include "ui/gfx/transform.h"
 
 namespace gfx {
