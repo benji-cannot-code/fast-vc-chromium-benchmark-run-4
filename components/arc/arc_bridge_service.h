49b3727829b6024180583049b2b8efe767bc9bde
tzik <tzik@chromium.org>
2017-03-14 22:16:08 -0700

    Revert of arc: enable Android tracing from chrome://tracing in dev mode (patchset #17 id:310001 of https://codereview.chromium.org/2699833003/ )
    Reason for revert:
    This CL seems to cause a compile failure on the CI.
    The error log is:
    https://luci-logdog.appspot.com/v/?s=chromium%2Fbb%2Fchromium.chromiumos%2FLinux_ChromiumOS_Builder__dbg_%2F84546%2F%2B%2Frecipes%2Fsteps%2Fcompile%2F0%2Fstdout
    
    [10030/10041] LINK ./chrome_app_unittests
    FAILED: chrome_app_unittests
    (snip)
    ../../chrome/browser/chromeos/arc/tracing/arc_tracing_bridge.cc:35: error: undefined reference to 'content::ArcTracingAgent::GetInstance()'
    ../../chrome/browser/chromeos/arc/tracing/arc_tracing_bridge.cc:39: error: undefined reference to 'content::ArcTracingAgent::GetInstance()'
    ../../chrome/browser/chromeos/arc/tracing/arc_tracing_bridge.cc:41: error: undefined reference to 'content::ArcTracingAgent::Delegate::~Delegate()'
    ../../content/browser/tracing/arc_tracing_agent.h:21: error: undefined reference to 'vtable for content::ArcTracingAgent::Delegate'
    ../../third_party/binutils/Linux_x64/Release/bin/ld.gold: the vtable symbol may be undefined because the class is missing its key function
    clang: error: linker command failed with exit code 1 (use -v to see invocation)
    
    Original issue's description:
    > arc: enable Android tracing from chrome://tracing in dev mode
    >
    > To enable tracing from chrome://tracing, we create a Mojo interface
    > for host side to trigger tracing in Android.
    >
    > The trace is written into debugfs/tracefs in dev-mode, and is read back
    > by debugd.
    >
    > Related changes:
    > - http://crrev.com/2699833003
    > - http://ag/1915548
    >
    > BUG=653795
    > TEST=Run chrome://tracing and see events from Android.
    >
    > Review-Url: https://codereview.chromium.org/2699833003
    > Cr-Commit-Position: refs/heads/master@{#456985}
    > Committed: https://chromium.googlesource.com/chromium/src/+/0b110db84a13d9087ddf55b5e99d961e54f55e16
    
    TBR=yusukes@chromium.org,lhchavez@chromium.org,bccheng@chromium.org,reveman@chromium.org,shunhsingou@google.com,dsinclair@chromium.org,dcheng@chromium.org,derat@chromium.org,primiano@chromium.org,oysteine@chromium.org,alexmos@chromium.org,shunhsingou@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=653795
    
    Review-Url: https://codereview.chromium.org/2747363002
    Cr-Commit-Position: refs/heads/master@{#456999}

--- components/arc/arc_bridge_service.h
+++ components/arc/arc_bridge_service.h
@@ -37,7 +37,6 @@ class PowerInstance;
 class PrintInstance;
 class ProcessInstance;
 class StorageManagerInstance;
-class TracingInstance;
 class TtsInstance;
 class VideoInstance;
 class WallpaperInstance;
@@ -91,7 +90,6 @@ class ArcBridgeService {
   InstanceHolder<mojom::StorageManagerInstance>* storage_manager() {
     return &storage_manager_;
   }
-  InstanceHolder<mojom::TracingInstance>* tracing() { return &tracing_; }
   InstanceHolder<mojom::TtsInstance>* tts() { return &tts_; }
   InstanceHolder<mojom::VideoInstance>* video() { return &video_; }
   InstanceHolder<mojom::WallpaperInstance>* wallpaper() { return &wallpaper_; }
@@ -119,7 +117,6 @@ class ArcBridgeService {
   InstanceHolder<mojom::PrintInstance> print_;
   InstanceHolder<mojom::ProcessInstance> process_;
   InstanceHolder<mojom::StorageManagerInstance> storage_manager_;
-  InstanceHolder<mojom::TracingInstance> tracing_;
   InstanceHolder<mojom::TtsInstance> tts_;
   InstanceHolder<mojom::VideoInstance> video_;
   InstanceHolder<mojom::WallpaperInstance> wallpaper_;
