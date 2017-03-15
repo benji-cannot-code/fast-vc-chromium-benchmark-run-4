1496349c663f7b13f352b79c9daa4205dd8ed22e
esprehn <esprehn@chromium.org>
2017-03-14 19:58:48 -0700

    Remove dead Element::wrapCustomElement declaration.
    Review-Url: https://codereview.chromium.org/2708863003
    Cr-Commit-Position: refs/heads/master@{#456971}

--- third_party/WebKit/Source/core/dom/Element.h
+++ third_party/WebKit/Source/core/dom/Element.h
@@ -940,10 +940,6 @@ class CORE_EXPORT Element : public ContainerNode {
   void detachAttrNodeFromElementWithValue(Attr*, const AtomicString& value);
   void detachAttrNodeAtIndex(Attr*, size_t index);
 
-  v8::Local<v8::Object> wrapCustomElement(
-      v8::Isolate*,
-      v8::Local<v8::Object> creationContext);
-
   Member<ElementData> m_elementData;
 };
 
