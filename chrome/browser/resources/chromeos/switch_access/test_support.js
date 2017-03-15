337e17f7fe1098788391076b73ca60ae10566e64
elichtenberg <elichtenberg@google.com>
2017-03-14 18:57:31 -0700

    Added unit tests for switch_access.js.
    BUG=593885
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2748853002
    Cr-Commit-Position: refs/heads/master@{#456943}

---
+++ chrome/browser/resources/chromeos/switch_access/test_support.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Stubs out extension API types and functions, and DOM elements, so
+ * SwitchAccessUnitTest can load.
+ */
+chrome.automation = {};
+
+chrome.automation.getDesktop = function() {};
+
+window = {};
