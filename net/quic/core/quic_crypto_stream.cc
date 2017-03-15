007e17a86d35f01c2f0b7ba3b99187c6cca0bb68
rch <rch@chromium.org>
2017-03-15 13:56:36 -0700

    Change the QUIC_FLAG_COUNT macro to use the flag name in a DVLOG() in order to ensure that the flag name is spelled correctly. Also fix misspelled flag names QUIC_FLAG_COUNT()s.
    Merge internal change: 150229293
    
    Review-Url: https://codereview.chromium.org/2750133002
    Cr-Commit-Position: refs/heads/master@{#457203}

--- net/quic/core/quic_crypto_stream.cc
+++ net/quic/core/quic_crypto_stream.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/quic/core/quic_flags.h"
 #include "net/quic/core/quic_session.h"
 #include "net/quic/core/quic_utils.h"
+#include "net/quic/platform/api/quic_flag_utils.h"
 #include "net/quic/platform/api/quic_logging.h"
 
 using std::string;
@@ -72,6 +73,7 @@ void QuicCryptoStream::OnDataAvailable() {
     sequencer()->MarkConsumed(iov.iov_len);
     if (handshake_confirmed_ && crypto_framer_.InputBytesRemaining() == 0 &&
         FLAGS_quic_reloadable_flag_quic_release_crypto_stream_buffer) {
+      QUIC_FLAG_COUNT(quic_reloadable_flag_quic_release_crypto_stream_buffer);
       // If the handshake is complete and the current message has been fully
       // processed then no more handshake messages are likely to arrive soon
       // so release the memory in the stream sequencer.
