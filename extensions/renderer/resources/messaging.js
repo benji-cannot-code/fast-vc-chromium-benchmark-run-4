c7dceadcd662a191cb4ab44bb7dc3a2aa7398498
rdevlin.cronin <rdevlin.cronin@chromium.org>
2017-03-15 13:15:53 -0700

    [Extensions Bindings] Let custom bindings create custom events
    Some of the custom JS we have needs to be able to create a custom event
    that isn't directly specified on the API. For instance, the messaging
    bindings (used by the runtime API) creates custom events on ports for
    onMessage and onDisconnect. The guest view APIs also liberally create
    their own custom events.
    
    Expose the ability to create custom events to the custom bindings to
    handle this use case. Ideally, we'll be able to eliminate these in
    the future and handle them all natively, but we're not there yet.
    
    BUG=653596
    
    Review-Url: https://codereview.chromium.org/2734213002
    Cr-Commit-Position: refs/heads/master@{#457183}

--- extensions/renderer/resources/messaging.js
+++ extensions/renderer/resources/messaging.js
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   // TODO(kalman): factor requiring chrome out of here.
   var chrome = requireNative('chrome').GetChrome();
-  var Event = require('event_bindings').Event;
   var lastError = require('lastError');
   var logActivity = requireNative('activityLogger');
   var logging = requireNative('logging');
@@ -23,6 +22,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   var kMessageChannel = "chrome.runtime.sendMessage";
   var kNativeMessageChannel = "chrome.runtime.sendNativeMessage";
   var kPortClosedError = 'Attempting to use a disconnected port object';
+
+  var jsEvent;
+  function createAnonymousEvent(schema, options) {
+    if (bindingUtil) {
+      // Native custom events ignore schema.
+      return bindingUtil.createCustomEvent(undefined, undefined, options);
+    }
+    if (!jsEvent)
+      jsEvent = require('event_bindings').Event;
+    return new jsEvent(undefined, schema, options);
+  }
+
+  function invalidateEvent(event) {
+    if (bindingUtil)
+      bindingUtil.invalidateEvent(event);
+    else
+      privates(event).impl.destroy_();
+  }
 
   // Map of port IDs to port object.
   var ports = {__proto__: null};
@@ -49,8 +66,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       __proto__: null,
       unmanaged: true,
     };
-    this.onDisconnect = new Event(null, [portSchema], options);
-    this.onMessage = new Event(null, [messageSchema, portSchema], options);
+    this.onDisconnect = createAnonymousEvent([portSchema], options);
+    this.onMessage = createAnonymousEvent([messageSchema, portSchema], options);
   }
   $Object.setPrototypeOf(PortImpl.prototype, null);
 
@@ -96,8 +113,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   };
 
   PortImpl.prototype.destroy_ = function() {
-    privates(this.onDisconnect).impl.destroy_();
-    privates(this.onMessage).impl.destroy_();
+    invalidateEvent(this.onDisconnect);
+    invalidateEvent(this.onMessage);
     delete ports[this.portId_];
   };
 
