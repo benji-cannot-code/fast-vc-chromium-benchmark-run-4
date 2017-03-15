cd9b3395697190f4cb7f17b32be26b3d935c6459
wutao <wutao@chromium.org>
2017-03-14 17:49:22 -0700

    Clean up DisplayConfigurationController user_action
    The parameter |user_action| is passed as true in all the cases.
    Therefore remove this parameter.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2726233002
    Cr-Commit-Position: refs/heads/master@{#456924}

--- chrome/browser/ui/webui/options/chromeos/display_options_handler.cc
+++ chrome/browser/ui/webui/options/chromeos/display_options_handler.cc
@@ -410,8 +410,7 @@ void DisplayOptionsHandler::HandleMirroring(const base::ListValue* args) {
     NOTREACHED();
   content::RecordAction(
       base::UserMetricsAction("Options_DisplayToggleMirroring"));
-  GetDisplayConfigurationController()->SetMirrorMode(is_mirroring,
-                                                     true /* user_action */);
+  GetDisplayConfigurationController()->SetMirrorMode(is_mirroring);
 }
 
 void DisplayOptionsHandler::HandleSetPrimary(const base::ListValue* args) {
@@ -421,8 +420,7 @@ void DisplayOptionsHandler::HandleSetPrimary(const base::ListValue* args) {
     return;
 
   content::RecordAction(base::UserMetricsAction("Options_DisplaySetPrimary"));
-  GetDisplayConfigurationController()->SetPrimaryDisplayId(
-      display_id, true /* user_action */);
+  GetDisplayConfigurationController()->SetPrimaryDisplayId(display_id);
 }
 
 void DisplayOptionsHandler::HandleSetDisplayLayout(
@@ -470,8 +468,7 @@ void DisplayOptionsHandler::HandleSetDisplayLayout(
   }
 
   VLOG(1) << "Updating display layout: " << layout->ToString();
-  GetDisplayConfigurationController()->SetDisplayLayout(std::move(layout),
-                                                        true /* user_action */);
+  GetDisplayConfigurationController()->SetDisplayLayout(std::move(layout));
 }
 
 void DisplayOptionsHandler::HandleSetDisplayMode(const base::ListValue* args) {
