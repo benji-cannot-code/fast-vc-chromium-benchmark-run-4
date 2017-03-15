f344c7c2350bc691a747e97d48a011ca357fb748
sdy <sdy@chromium.org>
2017-03-15 04:32:41 -0700

    [Mac] Lay out the browser window when adding the download shelf.
    ## Summary
    This fixes a regression when a browser window uses Auto Layout: if the download
    shelf is added to the window without ever being shown (e.g. when installing an
    extension), then the window can't be made as narrow as usual.
    
    ## Explanation
    Before this CL, BrowserWindowController doesn't lay out the window after adding
    the download shelf. When the download shelf is shown, -layoutSubviews is called
    via -resizeView:newHeight:, but in some cases, like a quick extension install,
    it's added and left hidden. In that case, the shelf will be positioned like
    this (but you can't see it, since it's hidden and has zero height):
    
        /---------------------------------------\
        |---------------------------------------|
        |                                       |
        |                                       |
        |                                       |
        |                                       |
        |                                       |
        |                                       |
        |                                       |
        |---------------------------            |
        |                         x|            |
        \---------------------------------------/
    
    The shelf's autoresizing mask has a flexible width but fixed margins. If the
    window is wider than the shelf when it's added, then it will have a right
    margin. As the user makes the window narrower, the download shelf is made
    narrower and eventually reaches a minimum size.
    
    If the user makes the window even smaller, one of two things will happen
    depending on whether Auto Layout is handling the window:
    
    - No Auto Layout: the layout breaks (but the user never sees it happen, because
      -layoutSubviews fixes it up if the download shelf is shown).
    
    - Auto Layout: The layout engine treats the fixed margin as a constraint and
      prevents the window from becoming any narrower.
    
    The next time -[BrowserWindowController layoutSubviews] runs, the download
    shelf is made the same width as the window, and the problem goes away because
    it no longer has a margin.
    
    ## Future work
    Only add the download shelf to the view hierarchy when it's visible.
    
    BUG=667698, 695577
    
    Review-Url: https://codereview.chromium.org/2742813003
    Cr-Commit-Position: refs/heads/master@{#457055}

--- chrome/browser/ui/cocoa/browser_window_controller.h
+++ chrome/browser/ui/cocoa/browser_window_controller.h
@@ -66,6 +66,9 @@ namespace extensions {
 class Command;
 }
 
+constexpr const gfx::Size kMinCocoaTabbedWindowSize(400, 272);
+constexpr const gfx::Size kMinCocoaPopupWindowSize(100, 122);
+
 @interface BrowserWindowController
     : TabWindowController<BookmarkBarControllerDelegate,
                           ViewResizer,
