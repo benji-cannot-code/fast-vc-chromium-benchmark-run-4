f6ee5f64c59070353d221dbffdce03a89f3244cc
hamelphi <hamelphi@chromium.org>
2017-03-15 15:52:30 -0700

    Enable TranslateRankerLogging by default.
    BUG=668549
    
    Review-Url: https://codereview.chromium.org/2724693005
    Cr-Commit-Position: refs/heads/master@{#457247}

--- components/translate/core/browser/translate_ranker_unittest.cc
+++ components/translate/core/browser/translate_ranker_unittest.cc
@@ -144,7 +144,7 @@ TEST_F(TranslateRankerTest, EnableEnforcement) {
 TEST_F(TranslateRankerTest, EnableQueryAndEnforcement) {
   InitFeatures({kTranslateRankerQuery, kTranslateRankerEnforcement}, {});
   EXPECT_TRUE(TranslateRanker::IsEnabled());
-  EXPECT_FALSE(TranslateRanker::IsLoggingEnabled());
+  EXPECT_TRUE(TranslateRanker::IsLoggingEnabled());
 }
 
 TEST_F(TranslateRankerTest, EnableLogging) {
@@ -187,7 +187,6 @@ TEST_F(TranslateRankerTest, ShouldOfferTranslation) {
 }
 
 TEST_F(TranslateRankerTest, RecordAndFlushEvents) {
-  InitFeatures({kTranslateRankerLogging}, {});
   std::unique_ptr<translate::TranslateRanker> ranker = GetRankerForTest(0.0f);
   std::vector<metrics::TranslateEventProto> flushed_events;
 
