12093de8abe345212b8e01452aac0327ee2f7422
jbroman <jbroman@chromium.org>
2017-03-15 09:43:12 -0700

    Require an explicit isolate for SerializedScriptValue::deserialize.
    v8::Isolate::GetCurrent is discouraged (and uses TLS). Taking an explicit
    isolate is preferred, so update all users to do so.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2709573007
    Cr-Commit-Position: refs/heads/master@{#457108}

--- third_party/WebKit/Source/bindings/modules/v8/V8ServiceWorkerMessageEventInternal.h
+++ third_party/WebKit/Source/bindings/modules/v8/V8ServiceWorkerMessageEventInternal.h
@@ -98,7 +98,7 @@ void V8ServiceWorkerMessageEventInternal::dataAttributeGetterCustom(
       event->setSerializedData(
           SerializedScriptValue::serializeAndSwallowExceptions(
               info.GetIsolate(), mainWorldData));
-      data = event->serializedData()->deserialize();
+      data = event->serializedData()->deserialize(isolate);
     }
   }
   if (data.IsEmpty())
--- third_party/WebKit/public/web/WebSerializedScriptValue.h
+++ third_party/WebKit/public/web/WebSerializedScriptValue.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/platform/WebPrivatePtr.h"
 
 namespace v8 {
+class Isolate;
 class Value;
 template <class T>
 class Local;
@@ -61,7 +62,8 @@ class WebSerializedScriptValue {
   // Creates a serialized script value from its wire format data.
   BLINK_EXPORT static WebSerializedScriptValue fromString(const WebString&);
 
-  BLINK_EXPORT static WebSerializedScriptValue serialize(v8::Local<v8::Value>);
+  BLINK_EXPORT static WebSerializedScriptValue serialize(v8::Isolate*,
+                                                         v8::Local<v8::Value>);
 
   // Create a WebSerializedScriptValue that represents a serialization error.
   BLINK_EXPORT static WebSerializedScriptValue createInvalid();
@@ -75,7 +77,7 @@ class WebSerializedScriptValue {
   BLINK_EXPORT WebString toString() const;
 
   // Convert the serialized value to a parsed v8 value.
-  BLINK_EXPORT v8::Local<v8::Value> deserialize();
+  BLINK_EXPORT v8::Local<v8::Value> deserialize(v8::Isolate*);
 
 #if BLINK_IMPLEMENTATION
   WebSerializedScriptValue(WTF::PassRefPtr<SerializedScriptValue>);
