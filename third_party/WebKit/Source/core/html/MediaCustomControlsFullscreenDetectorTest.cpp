ac551e09b50fc3e2bc679c404c00b78748289b35
zqzhang <zqzhang@chromium.org>
2017-03-15 06:24:03 -0700

    Let MockWebMediaPlayers in blink unit tests inherit from EmptyWebMediaPlayer
    BUG=700848
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2746103002
    Cr-Commit-Position: refs/heads/master@{#457066}

--- third_party/WebKit/Source/core/html/MediaCustomControlsFullscreenDetectorTest.cpp
+++ third_party/WebKit/Source/core/html/MediaCustomControlsFullscreenDetectorTest.cpp
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/HTMLVideoElement.h"
 #include "core/testing/DummyPageHolder.h"
 #include "platform/geometry/IntRect.h"
-#include "public/platform/WebMediaPlayer.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace blink {
