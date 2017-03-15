d2bede854ac52e44eac5d98a6fa0fbf53f02d7b2
Jeff Carpenter <jeffcarp@chromium.org>
2017-03-15 16:38:22 -0700

    [WPT Export] Support merging all in-flight PRs at once
    This change has no real effect currently, but it prepares the exporter for being able to merge multiple in-flight branches at once.
    
    BUG=686471
    R=qyearsley@chromium.org
    
    Change-Id: I79938cd9e3ba7cb96723cc5922bfef438a4dc510
    Reviewed-on: https://chromium-review.googlesource.com/455536
    Commit-Queue: Quinten Yearsley <qyearsley@chromium.org>
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#457847}

--- third_party/WebKit/Tools/Scripts/webkitpy/w3c/wpt_github_mock.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/w3c/wpt_github_mock.py
@@ -3,14 +3,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from webkitpy.w3c.wpt_github import MergeError
+
 
 class MockWPTGitHub(object):
 
-    def __init__(self, pull_requests, unsuccessful_merge=False):
+    def __init__(self, pull_requests, unsuccessful_merge_index=-1):
         self.pull_requests = pull_requests
-        self.unsuccessful_merge = unsuccessful_merge
         self.calls = []
         self.pull_requests_created = []
+        self.pull_requests_merged = []
+        self.unsuccessful_merge_index = unsuccessful_merge_index
 
     def in_flight_pull_requests(self):
         self.calls.append('in_flight_pull_requests')
@@ -18,8 +21,12 @@ class MockWPTGitHub(object):
 
     def merge_pull_request(self, number):
         self.calls.append('merge_pull_request')
-        if self.unsuccessful_merge:
-            raise Exception('PR could not be merged: %d' % number)
+
+        for index, pr in enumerate(self.pull_requests):
+            if pr.number == number and index == self.unsuccessful_merge_index:
+                raise MergeError()
+
+        self.pull_requests_merged.append(number)
 
     def create_pr(self, remote_branch_name, desc_title, body):
         self.calls.append('create_pr')
@@ -34,3 +41,7 @@ class MockWPTGitHub(object):
 
     def delete_remote_branch(self, _):
         self.calls.append('delete_remote_branch')
+
+    def get_pr_branch(self, number):
+        self.calls.append('get_pr_branch')
+        return 'fake branch for PR {}'.format(number)
