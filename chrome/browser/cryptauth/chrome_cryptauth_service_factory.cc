96dca20e18162c2e3d0baea962bbcbc3c662fbce
khorimoto <khorimoto@chromium.org>
2017-03-15 13:40:51 -0700

    [CrOS Tether] Create CryptAuthService and implement it via ChromeCryptAuthService and ChromeCryptAuthServiceFactory.
    These classes are currently unused. In a follow-up CL, I will integrate the new service into Chrome and refactor some of the code shared between this new class and the various EasyUnlock classes which share similar functionality.
    
    BUG=672263
    
    Review-Url: https://codereview.chromium.org/2739053002
    Cr-Commit-Position: refs/heads/master@{#457194}

---
+++ chrome/browser/cryptauth/chrome_cryptauth_service_factory.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/cryptauth/chrome_cryptauth_service_factory.h"
+
+#include "chrome/browser/cryptauth/chrome_cryptauth_service.h"
+#include "chrome/browser/profiles/profile.h"
+#include "components/keyed_service/content/browser_context_dependency_manager.h"
+
+// static
+cryptauth::CryptAuthService* CryptAuthServiceFactory::GetForBrowserContext(
+    content::BrowserContext* context) {
+  return static_cast<ChromeCryptAuthService*>(
+      GetInstance()->GetServiceForBrowserContext(context, true));
+}
+
+// static
+CryptAuthServiceFactory* CryptAuthServiceFactory::GetInstance() {
+  return base::Singleton<CryptAuthServiceFactory>::get();
+}
+
+CryptAuthServiceFactory::CryptAuthServiceFactory()
+    : BrowserContextKeyedServiceFactory(
+          "CryptAuthService",
+          BrowserContextDependencyManager::GetInstance()) {}
+
+CryptAuthServiceFactory::~CryptAuthServiceFactory() {}
+
+KeyedService* CryptAuthServiceFactory::BuildServiceInstanceFor(
+    content::BrowserContext* context) const {
+  Profile* profile = Profile::FromBrowserContext(context);
+  return ChromeCryptAuthService::Create(profile).release();
+}
