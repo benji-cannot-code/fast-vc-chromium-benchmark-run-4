62ffe253c095a2d7037b4fa854dded32cf8800d1
carlosk <carlosk@chromium.org>
2017-03-14 19:21:07 -0700

    xvfb.py complains if command points to a directory.
    An update to xvfb.py changed its input arguments and the previously
    required first one pointing to the execution directory has been removed.
    This change adds makes it backwards-aware: if the first argument, now
    expected to be the command, points to a directory it exists gracefully
    with a helpful error message.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2748123004
    Cr-Commit-Position: refs/heads/master@{#456957}

--- testing/xvfb.py
+++ testing/xvfb.py
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Runs tests with Xvfb and Openbox on Linux and normally on other platforms."""
 
 import os
+import os.path
 import platform
 import signal
 import subprocess
@@ -81,10 +82,19 @@ def run_executable(cmd, env):
 
 
 def main():
+  USAGE = 'Usage: xvfb.py [command args...]'
   if len(sys.argv) < 2:
-    print >> sys.stderr, (
-        'Usage: xvfb.py [command args...]')
+    print >> sys.stderr, USAGE
     return 2
+
+  # If the user still thinks the first argument is the execution directory then
+  # print a friendly error message and quit.
+  if os.path.isdir(sys.argv[1]):
+    print >> sys.stderr, (
+        'Invalid command: \"%s\" is a directory' % sys.argv[1])
+    print >> sys.stderr, USAGE
+    return 3
+
   return run_executable(sys.argv[1:], os.environ.copy())
 
 
