1e7830d1396530492871e90691767885a8ab78b2
khorimoto <khorimoto@chromium.org>
2017-03-16 11:51:31 -0700

    [CrOS Tether] Create NotificationPresenter class, which displays tethering notifications.
    Note that this CL uses no styling, icons, or click targets. These will be done in a future CL.
    
    BUG=672263
    
    Review-Url: https://codereview.chromium.org/2754693002
    Cr-Commit-Position: refs/heads/master@{#457506}

---
+++ chromeos/components/tether/fake_notification_presenter.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/components/tether/fake_notification_presenter.h"
+
+#include "base/bind.h"
+#include "base/memory/ptr_util.h"
+#include "ui/message_center/message_center.h"
+
+namespace chromeos {
+
+namespace tether {
+
+FakeNotificationPresenter::FakeNotificationPresenter()
+    : NotificationPresenter(nullptr) {}
+
+FakeNotificationPresenter::~FakeNotificationPresenter() {}
+
+void FakeNotificationPresenter::NotifyConnectionToHostFailed(
+    const std::string& host_device_name) {
+  connection_failed_notification_device_name_ = host_device_name;
+}
+
+void FakeNotificationPresenter::RemoveConnectionToHostFailedNotification() {
+  connection_failed_notification_device_name_ = "";
+}
+
+}  // namespace tether
+
+}  // namespace chromeos
