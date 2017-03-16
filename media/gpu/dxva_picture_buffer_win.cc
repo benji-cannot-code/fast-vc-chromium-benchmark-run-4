268fe114f2921e8108f3bf17ef9e25989f49e00c
jbauman <jbauman@chromium.org>
2017-03-15 17:50:03 -0700

    [dxvavda] Mark NV12 textures as allowing overlays.
    These can be used as overlays, though the type of image transport
    surface (DirectComposition or standard) and the scene layout will decide
    if they're actually used that way.
    
    BUG=678800
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2752143002
    Cr-Commit-Position: refs/heads/master@{#457292}

--- media/gpu/dxva_picture_buffer_win.cc
+++ media/gpu/dxva_picture_buffer_win.cc
@@ -371,6 +371,10 @@ bool PbufferPictureBuffer::CopySurfaceComplete(
   return true;
 }
 
+bool PbufferPictureBuffer::AllowOverlay() const {
+  return false;
+}
+
 PbufferPictureBuffer::PbufferPictureBuffer(const PictureBuffer& buffer)
     : DXVAPictureBuffer(buffer),
       decoding_surface_(NULL),
@@ -506,6 +510,10 @@ bool EGLStreamPictureBuffer::BindSampleToTexture(
   DCHECK(gl_image_dxgi);
 
   gl_image_dxgi->SetTexture(dx11_decoding_texture_, subresource);
+  return true;
+}
+
+bool EGLStreamPictureBuffer::AllowOverlay() const {
   return true;
 }
 
@@ -669,6 +677,10 @@ bool EGLStreamCopyPictureBuffer::ReusePictureBuffer() {
     EGLBoolean result = eglStreamConsumerReleaseKHR(egl_display, stream_);
     RETURN_ON_FAILURE(result, "Could not release stream", false);
   }
+  return true;
+}
+
+bool EGLStreamCopyPictureBuffer::AllowOverlay() const {
   return true;
 }
 
