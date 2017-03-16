b64a427d405728291af2f377427eeecac87ce322
yoichio <yoichio@chromium.org>
2017-03-15 20:45:01 -0700

    Let Range::expand not crash when Range's start node is not connected  to document.
    In the condition, Range::expand calls create VisiblePosition and
     DCHECK(node.isConnected()) failed.
    This CL lets Range::expand not do nothing in the condition.
    
    BUG=698123
    
    Review-Url: https://codereview.chromium.org/2747113003
    Cr-Commit-Position: refs/heads/master@{#457340}

--- third_party/WebKit/Source/core/dom/RangeTest.cpp
+++ third_party/WebKit/Source/core/dom/RangeTest.cpp
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/EditingTestBase.h"
 #include "core/frame/Settings.h"
 #include "core/html/HTMLBodyElement.h"
+#include "core/html/HTMLDivElement.h"
 #include "core/html/HTMLDocument.h"
 #include "core/html/HTMLElement.h"
 #include "core/html/HTMLHtmlElement.h"
@@ -229,6 +230,14 @@ TEST_F(RangeTest, NotMarkedValidByIrrelevantTextRemove) {
   EXPECT_EQ(0u, range->startOffset());
   EXPECT_EQ(div, range->endContainer());
   EXPECT_EQ(2u, range->endOffset());
+}
+
+// Regression test for crbug.com/698123
+TEST_F(RangeTest, ExpandNotCrash) {
+  Range* range = Range::create(document());
+  Node* div = HTMLDivElement::create(document());
+  range->setStart(div, 0, ASSERT_NO_EXCEPTION);
+  range->expand("", ASSERT_NO_EXCEPTION);
 }
 
 }  // namespace blink
