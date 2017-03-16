988fd90c22b88649f4eac64beb7081e66a5589f2
charleszhao <charleszhao@chromium.org>
2017-03-15 17:35:30 -0700

    Add UMA histogram for popup actions on Android.
    This CL adds metrics for the pop-up blocked infobar on Android.
    
    BUG=650132
    
    Review-Url: https://codereview.chromium.org/2662163002
    Cr-Commit-Position: refs/heads/master@{#457285}

--- chrome/browser/ui/android/content_settings/popup_blocked_infobar_delegate.cc
+++ chrome/browser/ui/android/content_settings/popup_blocked_infobar_delegate.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 #include <utility>
-
+#include "chrome/browser/content_settings/chrome_content_settings_utils.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/infobars/infobar_service.h"
 #include "chrome/browser/profiles/profile.h"
@@ -50,6 +50,9 @@ void PopupBlockedInfoBarDelegate::Create(content::WebContents* web_contents,
   }
 
   infobar_service->AddInfoBar(std::move(infobar));
+
+  content_settings::RecordPopupsAction(
+      content_settings::POPUPS_ACTION_DISPLAYED_INFOBAR_ON_MOBILE);
 }
 
 PopupBlockedInfoBarDelegate::~PopupBlockedInfoBarDelegate() {
@@ -117,5 +120,7 @@ bool PopupBlockedInfoBarDelegate::Accept() {
       it != blocked_popups.end(); ++it)
     popup_blocker_helper->ShowBlockedPopup(it->first);
 
+  content_settings::RecordPopupsAction(
+      content_settings::POPUPS_ACTION_CLICKED_ALWAYS_SHOW_ON_MOBILE);
   return true;
 }
