4c6bac66ad420afed0ffd362bbe51283592bde27
abhishekbh <abhishekbh@google.com>
2017-03-16 14:35:14 -0700

    arc: Update device owner check implementation
    Update the device owner check to compare the account id of the active
    user with the account id of the owner as provided by the user manager
    API. The old implementation uses the logged in user state API which for
    unbeknownst reasons returns the owner as a regular user. It can also
    change asynchronously at boot resulting in racy behavior as its used
    right now.
    
    BUG=b:34661187
    BUG=b:33488433
    TEST=run cts -c android.net.wifi.cts.WifiEnterpriseConfigTest -m
    testAddEapNetwork
    TEST=run cts -c android.net.wifi.cts.WifiManagerTest -m
    testWifiManagerNetWork
    
    Review-Url: https://codereview.chromium.org/2756443003
    Cr-Commit-Position: refs/heads/master@{#457563}

--- components/arc/net/arc_net_host_impl.cc
+++ components/arc/net/arc_net_host_impl.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/network/network_util.h"
 #include "chromeos/network/onc/onc_utils.h"
 #include "components/arc/arc_bridge_service.h"
+#include "components/user_manager/user_manager.h"
 
 namespace {
 
@@ -44,10 +45,12 @@ chromeos::NetworkConnectionHandler* GetNetworkConnectionHandler() {
 }
 
 bool IsDeviceOwner() {
-  // Check whether the logged-in Chrome OS user is allowed to add or
-  // remove WiFi networks.
-  return chromeos::LoginState::Get()->GetLoggedInUserType() ==
-         chromeos::LoginState::LOGGED_IN_USER_OWNER;
+  // Check whether the logged-in Chrome OS user is allowed to add or remove WiFi
+  // networks. The user account state changes immediately after boot. There is a
+  // small window when this may return an incorrect state. However, after things
+  // settle down this is guranteed to reflect the correct user account state.
+  return user_manager::UserManager::Get()->GetActiveUser()->GetAccountId() ==
+         user_manager::UserManager::Get()->GetOwnerAccountId();
 }
 
 std::string GetStringFromOncDictionary(const base::DictionaryValue* dict,
