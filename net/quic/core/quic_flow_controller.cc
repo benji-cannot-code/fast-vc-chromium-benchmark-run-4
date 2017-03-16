b822b07a6314c7b1a1b06f484767573e228839e9
jri <jri@chromium.org>
2017-03-15 17:09:53 -0700

    Landing Recent QUIC changes until 18:17, Mar 13, 2017, UTC.
    Only boost QUIC's BBR CWND after exiting STARTUP mode to prevent
    oversending. Protected by existing disabled flag
    FLAGS_FLAGS_quic_reloadable_flag_quic_bbr_ack_spacing2.
    Deprecates now unused FLAGS_quic_reloadable_flag_quic_bbr_ack_spacing.
    Merge internal change: 149965469
    https://codereview.chromium.org/2750093002/
    
    Disable BbrSenderTest::SimpleTransfer2RTTAggregation because it's flaky.
    Merge internal change: 149659218
    https://codereview.chromium.org/2751523006/
    
    add QUIC_FLAG_COUNT for flag
    FLAGS_quic_reloadable_flag_quic_release_crypto_stream_buffer.
    Merge internal change: 149649659
    https://codereview.chromium.org/2749643006/
    
    Only save the ack spacing when more than 1 packet is acked in QUIC
    BBR. Protected by flag
    FLAGS_quic_reloadable_flag_quic_bbr_ack_spacing2.
    Merge internal change: 149447863
    https://codereview.chromium.org/2750083002/
    
    Deprecate flag FLAGS_quic_reloadable_flag_quic_buffer_packets_after_chlo.
    Merge internal change: 149427074
    https://codereview.chromium.org/2750073002/
    
    Add enough CWND to accomadate inter-ack spacing in QUIC's BBR
    implementation.  Protected by
    FLAGS_quic_reloadable_flag_quic_bbr_ack_spacing.
    Merge internal change: 149375528
    https://codereview.chromium.org/2749703004/
    
    Make QUIC's flow control autotuning as fast for the first increase as
    subsequent window increases by setting prev_window_update_time_ on the
    first bytes consumed.  Protected by
    FLAGS_quic_reloadable_flag_quic_flow_control_faster_autotune.
    Merge internal change: 149315429
    https://codereview.chromium.org/2745253006/
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2751813003
    Cr-Commit-Position: refs/heads/master@{#457273}

--- net/quic/core/quic_flow_controller.cc
+++ net/quic/core/quic_flow_controller.cc
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstdint>
 
 #include "net/quic/core/quic_connection.h"
+#include "net/quic/core/quic_flags.h"
 #include "net/quic/core/quic_packets.h"
 #include "net/quic/platform/api/quic_bug_tracker.h"
+#include "net/quic/platform/api/quic_flag_utils.h"
 #include "net/quic/platform/api/quic_logging.h"
 #include "net/quic/platform/api/quic_str_cat.h"
 
@@ -184,6 +186,14 @@ void QuicFlowController::MaybeSendWindowUpdate() {
   DCHECK_LE(bytes_consumed_, receive_window_offset_);
   QuicStreamOffset available_window = receive_window_offset_ - bytes_consumed_;
   QuicByteCount threshold = WindowUpdateThreshold();
+
+  if (FLAGS_quic_reloadable_flag_quic_flow_control_faster_autotune &&
+      !prev_window_update_time_.IsInitialized()) {
+    QUIC_FLAG_COUNT(quic_reloadable_flag_quic_flow_control_faster_autotune);
+    // Treat the initial window as if it is a window update, so if 1/2 the
+    // window is used in less than 2 RTTs, the window is increased.
+    prev_window_update_time_ = connection_->clock()->ApproximateNow();
+  }
 
   if (available_window >= threshold) {
     QUIC_DVLOG(1) << ENDPOINT << "Not sending WindowUpdate for stream " << id_
