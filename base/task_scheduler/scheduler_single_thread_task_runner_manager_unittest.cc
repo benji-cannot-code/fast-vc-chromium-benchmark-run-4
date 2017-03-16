c073e1b09f2013faee93269063329428e50d0d01
robliao <robliao@chromium.org>
2017-03-16 10:54:30 -0700

    Followup Feedback on https://codereview.chromium.org/2726073002/
    BUG=684080
    
    Review-Url: https://codereview.chromium.org/2753443005
    Cr-Commit-Position: refs/heads/master@{#457486}

--- base/task_scheduler/scheduler_single_thread_task_runner_manager_unittest.cc
+++ base/task_scheduler/scheduler_single_thread_task_runner_manager_unittest.cc
@@ -281,6 +281,7 @@ class CallJoinFromDifferentThread : public SimpleThread {
  private:
   SchedulerSingleThreadTaskRunnerManager* const manager_to_join_;
   WaitableEvent run_started_event_;
+
   DISALLOW_COPY_AND_ASSIGN(CallJoinFromDifferentThread);
 };
 
@@ -303,6 +304,8 @@ class TaskSchedulerSingleThreadTaskRunnerManagerJoinTest
 }  // namespace
 
 TEST_F(TaskSchedulerSingleThreadTaskRunnerManagerJoinTest, ConcurrentJoin) {
+  // Exercises the codepath where the workers are unavailable for unregistration
+  // because of a Join call.
   WaitableEvent task_running(WaitableEvent::ResetPolicy::MANUAL,
                              WaitableEvent::InitialState::NOT_SIGNALED);
   WaitableEvent task_blocking(WaitableEvent::ResetPolicy::MANUAL,
@@ -329,6 +332,8 @@ TEST_F(TaskSchedulerSingleThreadTaskRunnerManagerJoinTest, ConcurrentJoin) {
 
 TEST_F(TaskSchedulerSingleThreadTaskRunnerManagerJoinTest,
        ConcurrentJoinExtraSkippedTask) {
+  // Tests to make sure that tasks are properly cleaned up at Join, allowing
+  // SingleThreadTaskRunners to unregister themselves.
   WaitableEvent task_running(WaitableEvent::ResetPolicy::MANUAL,
                              WaitableEvent::InitialState::NOT_SIGNALED);
   WaitableEvent task_blocking(WaitableEvent::ResetPolicy::MANUAL,
