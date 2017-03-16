3bf7294a1e134056dbbf4403ae34a706f236da8f
peter <peter@chromium.org>
2017-03-16 08:53:00 -0700

    Support the full `request` attribute types for the BGFetchManager.fetch()
    Raphael kindly improved the bindings generator to support nested union
    types, so we can now properly support this!
    
    BUG=700370
    
    Review-Url: https://codereview.chromium.org/2758493003
    Cr-Commit-Position: refs/heads/master@{#457446}

--- third_party/WebKit/Source/modules/background_fetch/BackgroundFetchManager.h
+++ third_party/WebKit/Source/modules/background_fetch/BackgroundFetchManager.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class BackgroundFetchOptions;
-class RequestOrUSVString;
+class RequestOrUSVStringOrRequestOrUSVStringSequence;
 class ScriptState;
 class ServiceWorkerRegistration;
 
@@ -32,10 +32,11 @@ class BackgroundFetchManager final
   }
 
   // Web Exposed methods defined in the IDL file.
-  ScriptPromise fetch(ScriptState*,
-                      String tag,
-                      HeapVector<RequestOrUSVString> requests,
-                      const BackgroundFetchOptions&);
+  ScriptPromise fetch(
+      ScriptState*,
+      String tag,
+      const RequestOrUSVStringOrRequestOrUSVStringSequence& requests,
+      const BackgroundFetchOptions&);
   ScriptPromise get(ScriptState*, String tag);
   ScriptPromise getTags(ScriptState*);
 
