100af8aa99d38b3586ee7021222a3269ea2edfd8
raphael.kubo.da.costa <raphael.kubo.da.costa@intel.com>
2017-03-16 05:15:31 -0700

    bindings: Add the proper includes for unions inside records
    While https://codereview.chromium.org/2749253002 took care of adding all
    required includes and forward-declarations for unions inside records inside
    other unions, it did not solve it for unions inside records which were _not_
    inside other unions: in this case, v8_union is not used at all, so we need
    to patch code_generator_v8.depending_union_type() instead.
    
    BUG=685754
    R=bashi@chromium.org,haraken@chromium.org,yukishiino@chromium.org
    
    Review-Url: https://codereview.chromium.org/2752223002
    Cr-Commit-Position: refs/heads/master@{#457404}

--- third_party/WebKit/Source/bindings/tests/results/core/TestDictionary.h
+++ third_party/WebKit/Source/bindings/tests/results/core/TestDictionary.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/DoubleOrString.h"
 #include "bindings/core/v8/FloatOrBoolean.h"
 #include "bindings/core/v8/IDLDictionaryBase.h"
+#include "bindings/core/v8/LongOrBoolean.h"
 #include "bindings/core/v8/ScriptValue.h"
 #include "bindings/core/v8/TestInterface2OrUint8Array.h"
 #include "bindings/tests/idls/core/TestInterface2.h"
@@ -185,6 +186,10 @@ class CORE_EXPORT TestDictionary : public IDLDictionaryBase {
   DOMUint8Array* uint8ArrayMember() const;
   void setUint8ArrayMember(DOMUint8Array*);
 
+  bool hasUnionInRecordMember() const;
+  const HeapVector<std::pair<String, LongOrBoolean>>& unionInRecordMember() const;
+  void setUnionInRecordMember(const HeapVector<std::pair<String, LongOrBoolean>>&);
+
   bool hasUnionWithTypedefs() const;
   const FloatOrBoolean& unionWithTypedefs() const;
   void setUnionWithTypedefs(const FloatOrBoolean&);
@@ -247,6 +252,8 @@ class CORE_EXPORT TestDictionary : public IDLDictionaryBase {
   bool m_hasTestObjectSequenceMember = false;
   HeapVector<Member<TestObject>> m_testObjectSequenceMember;
   Member<DOMUint8Array> m_uint8ArrayMember;
+  bool m_hasUnionInRecordMember = false;
+  HeapVector<std::pair<String, LongOrBoolean>> m_unionInRecordMember;
   FloatOrBoolean m_unionWithTypedefs;
   bool m_hasUnrestrictedDoubleMember = false;
   double m_unrestrictedDoubleMember;
