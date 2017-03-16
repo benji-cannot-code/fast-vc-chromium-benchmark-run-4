91e43df3feb39b07992f85a9f8b62ab1eb6dee1d
peconn <peconn@chromium.org>
2017-03-16 06:38:39 -0700

    📺 Rename stopIfPersisted to cleanup.
    I believe this captures the purpose of the function more cleanly.
    
    BUG=690006
    
    Review-Url: https://codereview.chromium.org/2757603002
    Cr-Commit-Position: refs/heads/master@{#457419}

--- chrome/android/java/src/org/chromium/chrome/browser/media/VideoPersister.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/VideoPersister.java
@@ -45,5 +45,5 @@ public class VideoPersister {
     /**
      * If the video has been persisted, perform cleanup.
      */
-    public void stopIfPersisted(ChromeActivity activity) {}
+    public void cleanup(ChromeActivity activity) {}
 }
