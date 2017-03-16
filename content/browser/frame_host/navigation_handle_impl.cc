0210d862a20b96e21666d61fbe649e54463cd250
arthursonzogni <arthursonzogni@chromium.org>
2017-03-16 02:55:25 -0700

    PlzNavigate: add support for BLOCK_REQUEST during redirects
    This CL adds support for blocking requests in NavigationRequest during
    redirects. It also fixes an issue in NavigationHandle, without
    PlzNavigate activated, where the NavigationHandle would not be properly
    recognized at error page commit if the navigation was blocked by a
    NavigationThrottle during a redirect.
    
    BUG=685074, 695421
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2698623006
    Cr-Commit-Position: refs/heads/master@{#457387}

--- content/browser/frame_host/navigation_handle_impl.cc
+++ content/browser/frame_host/navigation_handle_impl.cc
@@ -696,9 +696,9 @@ NavigationHandleImpl::CheckWillStartRequest() {
       case NavigationThrottle::PROCEED:
         continue;
 
+      case NavigationThrottle::BLOCK_REQUEST:
       case NavigationThrottle::CANCEL:
       case NavigationThrottle::CANCEL_AND_IGNORE:
-      case NavigationThrottle::BLOCK_REQUEST:
         state_ = CANCELING;
         return result;
 
@@ -728,6 +728,9 @@ NavigationHandleImpl::CheckWillRedirectRequest() {
       case NavigationThrottle::PROCEED:
         continue;
 
+      case NavigationThrottle::BLOCK_REQUEST:
+        CHECK(IsBrowserSideNavigationEnabled())
+            << "BLOCK_REQUEST must not be used on redirect without PlzNavigate";
       case NavigationThrottle::CANCEL:
       case NavigationThrottle::CANCEL_AND_IGNORE:
         state_ = CANCELING;
@@ -738,7 +741,6 @@ NavigationHandleImpl::CheckWillRedirectRequest() {
         next_index_ = i + 1;
         return result;
 
-      case NavigationThrottle::BLOCK_REQUEST:
       case NavigationThrottle::BLOCK_RESPONSE:
         NOTREACHED();
     }
