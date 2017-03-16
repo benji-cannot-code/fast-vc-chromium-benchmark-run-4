f315ff073ae843a176ff1fc80bc4723eaba63087
mcchou <mcchou@google.com>
2017-03-15 19:35:46 -0700

    bluetooth: Stop support of reconnecting devices not supporting pairing
    This stops the support of reconnection to the following Bluetooth device due to
    their lack of pairing mechanism.
    - Verbatim Bluetooth Mouse, model 96674
    - Microsoft Bluetooth Notebook Mouse 5000, model X807028-001
    - Sony PlayStation Dualshock3
    
    BUG=701992
    TBR=ortuno@chromium.org
    
    Review-Url: https://codereview.chromium.org/2751223002
    Cr-Commit-Position: refs/heads/master@{#457325}

--- device/bluetooth/bluetooth_adapter.h
+++ device/bluetooth/bluetooth_adapter.h
@@ -102,7 +102,6 @@ class DEVICE_BLUETOOTH_EXPORT BluetoothAdapter
     //  * IsConnecting()
     //  * IsGattConnected()
     //  * IsPaired()
-    //  * IsTrustable()
     //
     // On Android and MacOS this method is called for each advertisement packet
     // received. On Chrome OS and Linux, we can't guarantee that this method
