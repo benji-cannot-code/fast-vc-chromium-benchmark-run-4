1d8add87d752603dd3790826b68c8bf40b287e09
eugenebut <eugenebut@chromium.org>
2017-03-16 09:55:17 -0700

    Cleaned up old navigation code that did not use pending navigation item.
    Also removed PendingIndexNavigationDisabled experimental setting.
    
    BUG=692331,701717
    TEST=Back forward navigation works correctly.
    
    Review-Url: https://codereview.chromium.org/2751793002
    Cr-Commit-Position: refs/heads/master@{#457459}

--- ios/chrome/browser/experimental_flags.mm
+++ ios/chrome/browser/experimental_flags.mm
@@ -42,8 +42,6 @@ NSString* const kHeuristicsForPasswordGeneration =
     @"HeuristicsForPasswordGeneration";
 NSString* const kMDMIntegrationDisabled = @"MDMIntegrationDisabled";
 NSString* const kOriginServerHost = @"AlternateOriginServerHost";
-NSString* const kPendingIndexNavigationDisabled =
-    @"PendingIndexNavigationDisabled";
 NSString* const kSafariVCSignInDisabled = @"SafariVCSignInDisabled";
 NSString* const kWhatsNewPromoStatus = @"WhatsNewPromoStatus";
 
@@ -200,11 +198,6 @@ bool IsPaymentRequestEnabled() {
   // Check if the Finch experiment is turned on.
   return base::StartsWith(group_name, "Enabled",
                           base::CompareCase::INSENSITIVE_ASCII);
-}
-
-bool IsPendingIndexNavigationEnabled() {
-  return ![[NSUserDefaults standardUserDefaults]
-      boolForKey:kPendingIndexNavigationDisabled];
 }
 
 bool IsPhysicalWebEnabled() {
