164fec5a78e8e4bd718ad43fa34aa1964eb622d0
Dan Beam <dbeam@chromium.org>
2017-03-15 21:22:31 -0700

    Roll closure compiler
    Change log:
    https://github.com/google/closure-compiler/compare/72c88ff7e9e85b4c2413274539897b6d43024d48...b2f2d2f3309c7ee61816e068050651af27bccdfa
    chrome_extensions.js: 9f5d27bb1a9ad197c67f9eab61ea511d99aa4e8c -> a3f41820d1260b0d7ed13acac526b803ac6c1870
    
    TBR=fukino@chromium.org
    BUG=
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2753443007 .
    Cr-Commit-Position: refs/heads/master@{#457350}

--- ui/file_manager/externs/webview_tag.js
+++ ui/file_manager/externs/webview_tag.js
@@ -189,12 +189,12 @@ var SelectionRect;
 
 /**
  * @constructor
- * @see https://developer.chrome.com/apps/tags/webview#type-WebRequestEventInteface
+ * @see https://developer.chrome.com/apps/tags/webview#type-WebRequestEventInterface
  */
-function WebRequestEventInteface() {}
+function WebRequestEventInterface() {}
 
-/** @type {!WebRequestEvent} */
-WebRequestEventInteface.prototype.onBeforeSendHeaders;
+/** @type {!WebRequestOptionallySynchronousEvent} */
+WebRequestEventInterface.prototype.onBeforeSendHeaders;
 
 /**
  * @constructor
@@ -209,7 +209,7 @@ function WebView() {}
 WebView.prototype.contentWindow;
 
 /**
- * @type {!WebRequestEventInteface}
+ * @type {!WebRequestEventInterface}
  * @see https://developer.chrome.com/apps/tags/webview#property-request
  */
 WebView.prototype.request;
