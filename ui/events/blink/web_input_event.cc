45692803dbeb7f373d092778e669cdd8d4641bbf
dtapuska <dtapuska@chromium.org>
2017-03-16 06:05:54 -0700

    Treat NotifyVirtual events as mouse move events for blink.
    This restores the old logic we had for blink to deal with these as mouse
    move events but keeping the mouseleave events when we leave the window.
    
    NotifyInferior messages are already dropped at the X11 desktop tree
    host but I didn't want to risk breaking any views with addressing this
    release block stable issue.
    
    BUG=701637,450631,240300,352106,386896,569998,575208
    
    Review-Url: https://codereview.chromium.org/2751833006
    Cr-Commit-Position: refs/heads/master@{#457411}

--- ui/events/blink/web_input_event.cc
+++ ui/events/blink/web_input_event.cc
@@ -17,6 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/blink/web_input_event_builders_win.h"
 #endif
 
+#if defined(USE_X11)
+#include <X11/Xlib.h>
+#endif
+
 namespace ui {
 
 namespace {
@@ -389,6 +393,18 @@ blink::WebMouseEvent MakeWebMouseEventFromUiEvent(const MouseEvent& event) {
       click_count = event.GetClickCount();
       break;
     case ET_MOUSE_EXITED: {
+#if defined(USE_X11)
+      // NotifyVirtual events are created for intermediate windows that the
+      // pointer crosses through. These occur when middle clicking.
+      // Change these into mouse move events.
+      const base::NativeEvent& native_event = event.native_event();
+
+      if (native_event && native_event->type == LeaveNotify &&
+          native_event->xcrossing.detail == NotifyVirtual) {
+        type = blink::WebInputEvent::MouseMove;
+        break;
+      }
+#endif
       static bool s_send_leave =
           base::FeatureList::IsEnabled(features::kSendMouseLeaveEvents);
       type = s_send_leave ? blink::WebInputEvent::MouseLeave
