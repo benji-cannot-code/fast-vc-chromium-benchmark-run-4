adeace8a53fe553884d6023cdcd6157a8c42cd92
olivierrobin <olivierrobin@chromium.org>
2017-03-16 08:33:22 -0700

    Dismiss popups on Tab deselected
    Popups are ephemeral views used to send commands.
    When tab is deselected, they should be dismissed, specially popups relative to
    the tab (page info).
    
    BUG=697506, 697470
    
    Review-Url: https://codereview.chromium.org/2729563003
    Cr-Commit-Position: refs/heads/master@{#457442}

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -1516,7 +1516,7 @@ class BrowserBookmarkModelBridge : public bookmarks::BookmarkModelObserver {
   Tab* tab = notify.userInfo[kTabModelTabKey];
   DCHECK(tab);
   [tab wasHidden];
-  [_toolbarController dismissTabHistoryPopup];
+  [self dismissPopups];
 }
 
 - (void)tabWasAdded:(NSNotification*)notify {
