c8b8a7b3ddf257e06ae4d32b0ad0ae7335275203
seantopping <seantopping@chromium.org>
2017-03-17 13:48:02 -0700

    [Chromecast] Specify number of frame buffers from command line
    This allows us to specify the number of GPU frame buffers to use in the
    GPU process.
    
    BUG=internal b/36065692
    
    Review-Url: https://codereview.chromium.org/2749023007
    Cr-Commit-Position: refs/heads/master@{#457880}

--- chromecast/base/chromecast_switches.h
+++ chromecast/base/chromecast_switches.h
@@ -54,6 +54,7 @@ extern const char kMemPressureSystemReservedKb[];
 // GPU process switches
 extern const char kCastInitialScreenWidth[];
 extern const char kCastInitialScreenHeight[];
+extern const char kUseDoubleBuffering[];
 
 // Graphics switches
 extern const char kDesktopWindow1080p[];
