c6987118b099ed93323e71c8276eda45db148ef6
nektar <nektar@chromium.org>
2017-03-16 19:49:39 -0700

    WIP: Modified AXPosition to work with objects with both embedded object characters and text.
    On Windows, I had assumed that we would use tree positions for non text nobjects and text positions for text objects. This can work only if IA2 hypertext contains either a series of embedded object characters or text, but not both.
    However, the fact that we append the text of each static text object to its immediate parent for performance reasons, means that we could potentially have IA2 hypertext that contains both embedded object characters and text.
    BUG=700214
    R=dmazzoni@chromium.org
    TESTED=not yet
    
    Review-Url: https://codereview.chromium.org/2745713002
    Cr-Commit-Position: refs/heads/master@{#457665}

--- ui/accessibility/ax_node.cc
+++ ui/accessibility/ax_node.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/strings/string16.h"
+#include "ui/accessibility/ax_enums.h"
 #include "ui/gfx/transform.h"
 
 namespace ui {
@@ -18,6 +19,12 @@ AXNode::AXNode(AXNode* parent, int32_t id, int32_t index_in_parent)
 }
 
 AXNode::~AXNode() {
+}
+
+bool AXNode::IsTextNode() const {
+  return data().role == AX_ROLE_STATIC_TEXT ||
+         data().role == AX_ROLE_LINE_BREAK ||
+         data().role == AX_ROLE_INLINE_TEXT_BOX;
 }
 
 void AXNode::SetData(const AXNodeData& src) {
