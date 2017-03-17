586f8bc5baa69f36180caa84ec8aa7811ac8e399
jif <jif@chromium.org>
2017-03-17 09:00:39 -0700

    Unregister from the InstallationNotifier in the dealloc.
    When the NativeAppsCollectionViewController's parent controller is dismissed,
    |isMovingFromParentViewController| is false.
    The consequence is that the NativeAppsCollectionViewController was not unregistering itself from the InstallationNotifier. This led to crashes.
    
    TEST=On iPhone:
         1/ Open the Google Apps settings menu (Settings > Google Apps).
         2/ Click on "Install" on one of the app (at least one app must not
            be installed).
         3/ From that AppStore UI, actually start the installation of one
            of the app.
         4/ As the installation is happening, close the AppStore UI, and
            close the Google Apps menu by pressing the "FINISHED" button
            (*not* the back arrow).
         5/ Wait for the download to finish without switching to an other
            app.
         6/ Notice that Chrome crashes.
    
    BUG=701541
    
    Review-Url: https://codereview.chromium.org/2760643002
    Cr-Commit-Position: refs/heads/master@{#457781}

--- ios/chrome/browser/ui/settings/native_apps_collection_view_controller.mm
+++ ios/chrome/browser/ui/settings/native_apps_collection_view_controller.mm
@@ -114,6 +114,13 @@ typedef NS_ENUM(NSInteger, ItemType) {
   return self;
 }
 
+- (void)dealloc {
+  [[InstallationNotifier sharedInstance] unregisterForNotifications:self];
+  if (!_userDidSomething)
+    [self recordUserAction:settings::kNativeAppsActionDidNothing];
+  [super dealloc];
+}
+
 #pragma mark - View lifecycle
 
 - (void)viewDidLoad {
@@ -140,12 +147,6 @@ typedef NS_ENUM(NSInteger, ItemType) {
       removeObserver:self
                 name:UIApplicationDidBecomeActiveNotification
               object:nil];
-  if ([self isMovingFromParentViewController]) {
-    // The view controller is popped.
-    [[InstallationNotifier sharedInstance] unregisterForNotifications:self];
-    if (!_userDidSomething)
-      [self recordUserAction:settings::kNativeAppsActionDidNothing];
-  }
 }
 
 #pragma mark - CollectionViewController
