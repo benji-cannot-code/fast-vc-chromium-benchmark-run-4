340c509985027c6eaf53c1949e333f1c65f4026d
reillyg <reillyg@chromium.org>
2017-03-17 12:27:01 -0700

    Remove non-standard DeviceOrientation Event initializers.
    This patch removes DeviceOrientationEvent.initDeviceOrientationEvent
    and DeviceMotionEvent.initDeviceMotionEvent and rewrites the LayoutTests
    to use the constructors for these objects instead.
    
    https://www.w3.org/TR/2016/CR-orientation-event-20160818/
    
    BUG=697598
    
    Review-Url: https://codereview.chromium.org/2728963004
    Cr-Commit-Position: refs/heads/master@{#457849}

--- third_party/WebKit/Source/modules/device_orientation/DeviceMotionEvent.h
+++ third_party/WebKit/Source/modules/device_orientation/DeviceMotionEvent.h
@@ -52,11 +52,6 @@ class DeviceMotionEvent final : public Event {
     return new DeviceMotionEvent(eventType, deviceMotionData);
   }
 
-  void initDeviceMotionEvent(const AtomicString& type,
-                             bool bubbles,
-                             bool cancelable,
-                             DeviceMotionData*);
-
   DeviceMotionData* getDeviceMotionData() const {
     return m_deviceMotionData.get();
   }
