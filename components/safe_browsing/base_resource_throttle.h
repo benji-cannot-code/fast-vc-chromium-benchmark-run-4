c20c5dc96969636c36df75cfd8896e43a2e5c1fc
vakh <vakh@chromium.org>
2017-03-16 18:25:38 -0700

    Remove the debug logic added for crbug/660293
    BUG=660293
    
    Review-Url: https://codereview.chromium.org/2757573003
    Cr-Commit-Position: refs/heads/master@{#457645}

--- components/safe_browsing/base_resource_throttle.h
+++ components/safe_browsing/base_resource_throttle.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_SAFE_BROWSING_BASE_RESOURCE_THROTTLE_H_
 #define COMPONENTS_SAFE_BROWSING_BASE_RESOURCE_THROTTLE_H_
 
-#include <set>
-#include <string>
 #include <vector>
 
 #include "base/macros.h"
@@ -188,11 +186,6 @@ class BaseResourceThrottle
 
   const content::ResourceType resource_type_;
   net::NetLogWithSource net_log_with_source_;
-
-  // TODO(vakh): The following set should be removed after fixing
-  // http://crbug.com/660293
-  // URLs that timed out waiting for a SafeBrowsing reputation check.
-  std::set<GURL> timed_out_urls_;
 
   DISALLOW_COPY_AND_ASSIGN(BaseResourceThrottle);
 };
