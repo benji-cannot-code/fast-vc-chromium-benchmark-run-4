1b11d52977aff9276266013532a68e7dcc5940aa
kojii <kojii@chromium.org>
2017-03-16 20:33:26 -0700

    [LayoutNG] Implement atomic inlines for LayoutNGInline
    This patch implements the initial support for atomic inlines in
    LayoutNGInline.
    
    Atomic inlines are the first type of objects in LayoutNGInline that
    needs be laid out to compute in-flow inline size. NGLineBuilder now
    keeps NGLayoutResult for atomic inlines while it builds a line box.
    
    Some tests turned from false-passes to real-failures. Because atomic
    inlines were not rendered before this patch, tests using images and
    inline-blocks passed by comparing blank result with blank reference.
    
    Some work are not included in this patch and will follow, such as:
    * Keep NGBlockNode for atomic inlines in NGLayoutInlineItem.
    * Use NGLayoutResult not only for layout but also for fragment
      generation.
    * Margin, padding, borders.
    * BreakToken if we support breaking atomic inlines[1].
    * ...and obviously more.
    
    [1] https://github.com/w3c/csswg-drafts/issues/1111
    
    BUG=636993
    
    Review-Url: https://codereview.chromium.org/2745973002
    Cr-Commit-Position: refs/heads/master@{#457673}

--- third_party/WebKit/Source/core/layout/ng/ng_inline_node.h
+++ third_party/WebKit/Source/core/layout/ng/ng_inline_node.h
@@ -138,6 +138,10 @@ class NGLayoutInlineItem {
   const ComputedStyle* Style() const { return style_; }
   LayoutObject* GetLayoutObject() const { return layout_object_; }
 
+  bool IsAtomicInlineLevel() const {
+    return layout_object_ && layout_object_->isAtomicInlineLevel();
+  }
+
   void SetEndOffset(unsigned);
 
   LayoutUnit InlineSize() const;
