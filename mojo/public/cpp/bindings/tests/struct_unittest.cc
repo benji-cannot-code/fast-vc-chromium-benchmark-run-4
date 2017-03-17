08c54ade54a6103907eb691f518cf07f91a1bef8
yzshen <yzshen@chromium.org>
2017-03-17 09:29:58 -0700

    Mojo C++ Bindings: fix mojom::Foo::Deserialize() for component build.
    Without this fix, if the mojom target containing mojom::Foo is linked into a
    component, mojom::Foo::Deserialize() doesn't work outside of that component.
    
    The reason is that the method calls mojom::Foo_Data::Valiate() which is not
    exported.
    
    BUG=702397
    
    Review-Url: https://codereview.chromium.org/2754003005
    Cr-Commit-Position: refs/heads/master@{#457791}

--- mojo/public/cpp/bindings/tests/struct_unittest.cc
+++ mojo/public/cpp/bindings/tests/struct_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "mojo/public/cpp/bindings/lib/fixed_buffer.h"
 #include "mojo/public/cpp/system/message_pipe.h"
+#include "mojo/public/interfaces/bindings/tests/test_export2.mojom.h"
 #include "mojo/public/interfaces/bindings/tests/test_structs.mojom.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -436,7 +437,7 @@ TEST_F(StructTest, Serialization_PublicAPI) {
 
     // Initialize it to non-null.
     RectPtr output(Rect::New());
-    ASSERT_TRUE(Rect::Deserialize(std::move(data), &output));
+    ASSERT_TRUE(Rect::Deserialize(data, &output));
     EXPECT_TRUE(output.is_null());
   }
 
@@ -447,7 +448,7 @@ TEST_F(StructTest, Serialization_PublicAPI) {
     EXPECT_FALSE(data.empty());
 
     EmptyStructPtr output;
-    ASSERT_TRUE(EmptyStruct::Deserialize(std::move(data), &output));
+    ASSERT_TRUE(EmptyStruct::Deserialize(data, &output));
     EXPECT_FALSE(output.is_null());
   }
 
@@ -458,7 +459,7 @@ TEST_F(StructTest, Serialization_PublicAPI) {
     auto data = Rect::Serialize(&rect);
 
     RectPtr output;
-    ASSERT_TRUE(Rect::Deserialize(std::move(data), &output));
+    ASSERT_TRUE(Rect::Deserialize(data, &output));
     EXPECT_TRUE(output.Equals(cloned_rect));
   }
 
@@ -476,7 +477,7 @@ TEST_F(StructTest, Serialization_PublicAPI) {
     // Make sure that the serialized result gets pointers encoded properly.
     auto cloned_data = data;
     NamedRegionPtr output;
-    ASSERT_TRUE(NamedRegion::Deserialize(std::move(cloned_data), &output));
+    ASSERT_TRUE(NamedRegion::Deserialize(cloned_data, &output));
     EXPECT_TRUE(output.Equals(cloned_region));
   }
 
@@ -486,7 +487,17 @@ TEST_F(StructTest, Serialization_PublicAPI) {
     auto data = Rect::Serialize(&rect);
 
     NamedRegionPtr output;
-    EXPECT_FALSE(NamedRegion::Deserialize(std::move(data), &output));
+    EXPECT_FALSE(NamedRegion::Deserialize(data, &output));
+  }
+
+  {
+    // A struct from another component.
+    auto pair = test_export2::StringPair::New("hello", "world");
+    auto data = test_export2::StringPair::Serialize(&pair);
+
+    test_export2::StringPairPtr output;
+    ASSERT_TRUE(test_export2::StringPair::Deserialize(data, &output));
+    EXPECT_TRUE(output.Equals(pair));
   }
 }
 
