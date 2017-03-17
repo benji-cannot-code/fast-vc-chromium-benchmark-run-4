7ad7d9a3af3cd25b267a5c8cc5ef250eb15a6735
iankc <iankc@google.com>
2017-03-17 13:24:30 -0700

    Add Intent Filter to listen for BLE state change
    This small change adds an intent listener to correctly handle when
    bluetooth is turned off mid-broadcasting.
    
    BUG=685856
    
    Review-Url: https://codereview.chromium.org/2751343003
    Cr-Commit-Position: refs/heads/master@{#457874}

--- chrome/android/java/src/org/chromium/chrome/browser/physicalweb/PhysicalWebBroadcastService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/physicalweb/PhysicalWebBroadcastService.java
@@ -9,6 +9,7 @@ import android.annotation.TargetApi;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -25,6 +26,7 @@ import org.chromium.chrome.browser.notifications.ChromeNotificationBuilder;
 import org.chromium.chrome.browser.notifications.NotificationConstants;
 import org.chromium.chrome.browser.notifications.NotificationManagerProxy;
 import org.chromium.chrome.browser.notifications.NotificationManagerProxyImpl;
+import org.chromium.chrome.browser.util.IntentUtils;
 
 /**
  * Broadcasts Physical Web URLs via Bluetooth Low Energy (BLE).
@@ -55,6 +57,7 @@ public class PhysicalWebBroadcastService extends Service {
     private static final String STOP_SERVICE =
             "org.chromium.chrome.browser.physicalweb.stop_service";
     private static final String TAG = "PhysicalWebSharing";
+    private static final int BLUETOOTH_ADAPTER_DEFAULT_STATE = -1;
 
     private boolean mStartedByRestart;
 
@@ -63,7 +66,13 @@ public class PhysicalWebBroadcastService extends Service {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
 
-            if (STOP_SERVICE.equals(action)) {
+            if (BluetoothAdapter.ACTION_STATE_CHANGED.equals(action)) {
+                int state = IntentUtils.safeGetIntExtra(
+                        intent, BluetoothAdapter.EXTRA_STATE, BLUETOOTH_ADAPTER_DEFAULT_STATE);
+                if (state == BluetoothAdapter.STATE_OFF) {
+                    stopSelf();
+                }
+            } else if (STOP_SERVICE.equals(action)) {
                 stopSelf();
             } else {
                 Log.e(TAG, "Unrecognized Broadcast Received");
@@ -82,7 +91,9 @@ public class PhysicalWebBroadcastService extends Service {
             return START_STICKY;
         }
 
-        IntentFilter filter = new IntentFilter(STOP_SERVICE);
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        filter.addAction(STOP_SERVICE);
         registerReceiver(mBroadcastReceiver, filter);
 
         // TODO(iankc): implement parsing, broadcasting, Url Shortener.
