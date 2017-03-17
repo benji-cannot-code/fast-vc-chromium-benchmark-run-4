79e2ed2e3812db7ab7eacd206037e4a0f3bedadb
sky <sky@chromium.org>
2017-03-17 10:26:08 -0700

    Fixs gn check for wayland
    If OZONE_PLATFORM_GBM is false, as happens with
    ozone_auto_platforms=false, then gn check gives an error.
    
    BUG=none
    TEST=none
    R=dcastagna@chromium.org,reveman@chromium.org
    TBR=dcastagna@chromium.org,reveman@chromium.org
    
    Review-Url: https://codereview.chromium.org/2754333002
    Cr-Commit-Position: refs/heads/master@{#457803}

--- components/exo/wayland/clients/client_base.cc
+++ components/exo/wayland/clients/client_base.cc
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <gbm.h>
 #include <xf86drm.h>
 
-#include "ui/ozone/public/ozone_platform.h"
+#include "ui/ozone/public/ozone_platform.h"  // nogncheck
 #endif
 
 namespace exo {
