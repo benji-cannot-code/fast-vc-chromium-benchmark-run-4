139850281970a39e6902d6c5e3fdfc4ca0632676
dtapuska <dtapuska@chromium.org>
2017-03-16 17:41:04 -0700

    Remove setting the pointer cursor when the context menu event is sent.
    I thought I checked WebFrameWidgetImpl as well but it appears I hadn't.
    Remove the useless code there as well. WebViewImpl change was originally
    done in https://chromium.googlesource.com/chromium/src.git/+/7e40fe40d19b2c766df8d860169b11e2cacd9771
    
    BUG=699232
    R=bokan@chromium.org
    
    Review-Url: https://codereview.chromium.org/2754003004
    Cr-Commit-Position: refs/heads/master@{#457629}

--- third_party/WebKit/Source/web/WebFrameWidgetImpl.cpp
+++ third_party/WebKit/Source/web/WebFrameWidgetImpl.cpp
@@ -800,10 +800,6 @@ void WebFrameWidgetImpl::mouseContextMenu(const WebMouseEvent& event) {
 
   LocalFrame* targetLocalFrame = toLocalFrame(targetFrame);
 
-#if OS(WIN)
-  targetLocalFrame->view()->setCursor(pointerCursor());
-#endif
-
   {
     ContextMenuAllowedScope scope;
     targetLocalFrame->eventHandler().sendContextMenuEvent(transformedEvent,
