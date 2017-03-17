b12e7d2e6b5eea059bde5acf0f64267dd58ff1a5
weiliangc <weiliangc@chromium.org>
2017-03-17 12:28:30 -0700

    cc: Move Layer Id to Node Map to Individual Property Tree Private
    Move the layer id to node index map to individual property tree, and
    make this map private. This helps control of access to these maps.
    
    BUG=695681
    R=ajuma
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2753933005
    Cr-Commit-Position: refs/heads/master@{#457850}

--- cc/layers/layer_impl.h
+++ cc/layers/layer_impl.h
@@ -531,6 +531,7 @@ class CC_EXPORT LayerImpl {
 
  private:
   PropertyTrees* GetPropertyTrees() const;
+  ClipTree& GetClipTree() const;
   EffectTree& GetEffectTree() const;
   ScrollTree& GetScrollTree() const;
   TransformTree& GetTransformTree() const;
