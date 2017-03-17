4ad2730e3473b119fec042a9647f40c3ca074ac8
xiaochu <xiaochu@chromium.org>
2017-03-16 21:17:50 -0700

    On-demand cros component install
    With this CL, chrome OS component can be installed by simply calling
    InstallCrOSComponent. This API installs the component asynchronously and
    trigger the callback you passed in upon finish.
    
    BUG=690521
    TEST=install escpr on a chrome book.
    
    Review-Url: https://codereview.chromium.org/2748183003
    Cr-Commit-Position: refs/heads/master@{#457681}

--- components/component_updater/component_updater_service.h
+++ components/component_updater/component_updater_service.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/memory/ref_counted.h"
 #include "base/version.h"
+#include "build/build_config.h"
 #include "components/update_client/update_client.h"
 #include "url/gurl.h"
 
@@ -156,6 +157,9 @@ class OnDemandUpdater {
   friend class SupervisedUserWhitelistInstaller;
   friend class ::ComponentsUI;
   friend class ::PluginObserver;
+#if defined(OS_CHROMEOS)
+  friend class CrOSComponent;
+#endif  // defined(OS_CHROMEOS)
 
   // Triggers an update check for a component. |id| is a value
   // returned by GetCrxComponentID(). If an update for this component is already
