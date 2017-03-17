340eafd672fa4374ee62143f52247bfe780b9599
dtrainor <dtrainor@chromium.org>
2017-03-17 13:18:09 -0700

    Fix the download summary notification icons
    Make the icon ordering follow the expected behavior: progress, pending,
    failed, paused, completed.  This requires additional changes to support
    listening to when completed and failed notifications are dismissed so we
    can update the icon state, so I added a pending intent to those delete
    actions as well, but prevented it from restarting the service in case
    it's the last notification.  For proper handling of all intent actions,
    I still added code to handle the intent for the case where the service
    does get started with that action.
    
    BUG=699687
    
    Review-Url: https://codereview.chromium.org/2751813004
    Cr-Commit-Position: refs/heads/master@{#457873}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadBroadcastReceiver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadBroadcastReceiver.java
@@ -32,6 +32,7 @@ public class DownloadBroadcastReceiver extends BroadcastReceiver {
             case DownloadNotificationService.ACTION_DOWNLOAD_CANCEL:
             case DownloadNotificationService.ACTION_DOWNLOAD_PAUSE:
             case DownloadNotificationService.ACTION_DOWNLOAD_OPEN:
+            case DownloadNotificationService.ACTION_DOWNLOAD_UPDATE_SUMMARY_ICON:
                 performDownloadOperation(context, intent);
                 break;
             default:
