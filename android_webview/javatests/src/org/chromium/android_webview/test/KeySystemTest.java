ab2e404891b982743e8d171ddb8a3a7368ee60df
jrummell <jrummell@chromium.org>
2017-03-16 17:52:08 -0700

    Fix KeySystemTests on Android
    It appears that getting back an empty string from JavaScript ends up
    as "" in Java, so remove the double quotes if they exist.
    
    Also modify some of the test to make it clearer what it is testing. As vp8
    is more likely to be supported on the Android platform, pass it as one of
    the capabilities expected.
    
    BUG=699310
    TEST=updated tests pass on Android
    
    Review-Url: https://codereview.chromium.org/2741943003
    Cr-Commit-Position: refs/heads/master@{#457635}

--- android_webview/javatests/src/org/chromium/android_webview/test/KeySystemTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/KeySystemTest.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.android_webview.test;
 
-import android.os.Build;
 import android.support.test.filters.SmallTest;
 
 import org.chromium.android_webview.AwContents;
@@ -71,8 +70,8 @@ public class KeySystemTest extends AwTestBase {
     }
 
     private boolean areProprietaryCodecsSupported() throws Exception {
-        String result = executeJavaScriptAndWaitForResult(
-                mAwContents, mContentsClient, "areProprietaryCodecsSupported()");
+        String result = maybeStripDoubleQuotes(executeJavaScriptAndWaitForResult(
+                mAwContents, mContentsClient, "areProprietaryCodecsSupported()"));
         return !result.isEmpty();
     }
 
@@ -88,21 +87,23 @@ public class KeySystemTest extends AwTestBase {
     }
 
     private String getPlatformKeySystemExpectations() throws Exception {
-        // Android key systems only support non-proprietary codecs on Lollipop+.
-        // When neither is true isKeySystemSupported() will return an error for
-        // all key systems except ClearKey (which is handled by Chrome itself).
-        if (!areProprietaryCodecsSupported()
-                && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            return "\"NotSupportedError\"";
+        // isKeySystemSupported() calls navigator.requestMediaKeySystemAccess()
+        // with a video/mp4 configuration. mp4 is only supported if
+        // areProprietaryCodecsSupported().
+        if (areProprietaryCodecsSupported()) {
+            return "\"supported\"";
         }
 
-        return "\"supported\"";
+        return "\"NotSupportedError\"";
     }
 
     @Feature({"AndroidWebView"})
     @SmallTest
     public void testSupportClearKeySystem() throws Throwable {
-        assertEquals("\"supported\"", isKeySystemSupported("org.w3.clearkey"));
+        // Clear Key is always supported. However, isKeySystemSupported()
+        // specifies a video/mp4 configuration, so it only succeeds if
+        // proprietary codecs are supported.
+        assertEquals(getPlatformKeySystemExpectations(), isKeySystemSupported("org.w3.clearkey"));
     }
 
     @Feature({"AndroidWebView"})
