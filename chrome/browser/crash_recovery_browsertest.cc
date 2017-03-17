483932e63da0ce80dd11b2501604d658838e215c
jam <jam@chromium.org>
2017-03-16 17:34:14 -0700

    Ensure that with PlzNavigate, reloads after process crash display the RenderView.
    BUG=699384
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2753093002
    Cr-Commit-Position: refs/heads/master@{#457626}

--- chrome/browser/crash_recovery_browsertest.cc
+++ chrome/browser/crash_recovery_browsertest.cc
@@ -21,6 +21,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_types.h"
+#include "content/public/browser/render_frame_host.h"
+#include "content/public/browser/render_process_host.h"
+#include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test_utils.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
@@ -107,6 +110,9 @@ IN_PROC_BROWSER_TEST_F(CrashRecoveryBrowserTest, Reload) {
   ASSERT_TRUE(ui_test_utils::GetCurrentTabTitle(browser(),
                                                 &title_after_crash));
   EXPECT_NE(title_before_crash, title_after_crash);
+  ASSERT_TRUE(GetActiveWebContents()->GetMainFrame()->GetView()->IsShowing());
+  ASSERT_FALSE(
+      GetActiveWebContents()->GetRenderProcessHost()->IsProcessBackgrounded());
 }
 
 // Test that reload after a crash forces a cache revalidation.
