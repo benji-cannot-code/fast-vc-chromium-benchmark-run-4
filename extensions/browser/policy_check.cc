9bf6bd68e2d13117d968c30efb528f3abda812cb
michaelpg <michaelpg@chromium.org>
2017-03-17 20:00:15 -0700

    Add PreloadCheck class for extension pre-install checks
    PreloadCheck is an abstract preload check for an extension that may
    be asynchronous. The idea is to de-duplicate ad-hoc checks into
    PreloadCheck subclasses, then use these classes from places like
    ExtensionInstallChecker, CrxInstaller, and ManagementAPI.
    
    This will enable some of these classes to be moved into //extensions,
    because we would be able to pick and choose which checks to run. E.g.,
    AppShell would only use checks from //extensions, while Chrome would
    also add checks from //chrome like BlacklistCheck.
    
    See go/loading-extensions-in-appshell for more details on how this
    relates to AppShell and launching apps/extensions.
    
    BUG=679971
    
    Review-Url: https://codereview.chromium.org/2693373003
    Cr-Commit-Position: refs/heads/master@{#457943}

---
+++ extensions/browser/policy_check.cc
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "extensions/browser/policy_check.h"
+
+#include "content/public/browser/browser_context.h"
+#include "extensions/browser/extension_system.h"
+#include "extensions/browser/management_policy.h"
+
+namespace extensions {
+
+PolicyCheck::PolicyCheck(content::BrowserContext* context,
+                         scoped_refptr<const Extension> extension)
+    : PreloadCheck(extension), context_(context) {}
+
+PolicyCheck::~PolicyCheck() {}
+
+void PolicyCheck::Start(ResultCallback callback) {
+  Errors errors;
+  if (!ExtensionSystem::Get(context_)->management_policy()->UserMayLoad(
+          extension(), &error_)) {
+    DCHECK(!error_.empty());
+    errors.insert(DISALLOWED_BY_POLICY);
+  }
+  std::move(callback).Run(errors);
+}
+
+base::string16 PolicyCheck::GetErrorMessage() const {
+  return error_;
+}
+
+}  // namespace extensions
