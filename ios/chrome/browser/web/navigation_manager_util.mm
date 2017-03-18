05a64ec137053f7118c314ba4695b75165de6f1c
pkl <pkl@chromium.org>
2017-03-17 18:22:43 -0700

    Adds NavigationManager util functions to ios/chrome/browser/web/
    NavigationManager public API is rich enough for most operations. This
    is where utility functions built on top of NavigationManager public
    API can live and can gradually phase out uses of private
    CRWSessionController APIs.
    
    BUG=546365
    
    Review-Url: https://codereview.chromium.org/2756483006
    Cr-Commit-Position: refs/heads/master@{#457917}

---
+++ ios/chrome/browser/web/navigation_manager_util.mm
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/web/navigation_manager_util.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+web::NavigationItem* GetLastNonRedirectedItem(
+    web::NavigationManager* nav_manager) {
+  if (!nav_manager->GetItemCount())
+    return nullptr;
+  int index = nav_manager->GetCurrentItemIndex();
+  web::NavigationItem* item = nullptr;
+  while (index >= 0) {
+    item = nav_manager->GetItemAtIndex(index);
+    // Returns the first non-Redirect item found.
+    if ((item->GetTransitionType() & ui::PAGE_TRANSITION_IS_REDIRECT_MASK) == 0)
+      break;
+    --index;
+  }
+  return item;
+}
