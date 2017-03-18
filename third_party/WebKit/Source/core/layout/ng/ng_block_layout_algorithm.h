4c77229d0921591f2dd6cead0447589a5c8eef02
ikilpatrick <ikilpatrick@chromium.org>
2017-03-17 20:57:59 -0700

    [LayoutNG] Split apart CreateConstraintSpaceForChild.
    Introduces PrepareChildLayout which takes now does all of the pre-layout
    margin calculation, margin-collapsing, etc.
    
    Going to try and follow up with a patch which bundles margin,
    bfc_offset, etc, together instead of using the class curr_*_ variables.
    
    BUG=635619
    
    Review-Url: https://codereview.chromium.org/2754303003
    Cr-Commit-Position: refs/heads/master@{#457953}

--- third_party/WebKit/Source/core/layout/ng/ng_block_layout_algorithm.h
+++ third_party/WebKit/Source/core/layout/ng/ng_block_layout_algorithm.h
@@ -43,6 +43,7 @@ class CORE_EXPORT NGBlockLayoutAlgorithm : public NGLayoutAlgorithm {
 
   // Creates a new constraint space for the current child.
   RefPtr<NGConstraintSpace> CreateConstraintSpaceForChild(NGLayoutInputNode*);
+  void PrepareChildLayout(NGLayoutInputNode*);
   void FinishChildLayout(NGLayoutInputNode*,
                          NGConstraintSpace*,
                          RefPtr<NGLayoutResult>);
