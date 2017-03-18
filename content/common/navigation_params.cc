246b498040544a1b253504621c26edc7a1271a1f
arthursonzogni <arthursonzogni@chromium.org>
2017-03-18 02:00:53 -0700

    PlzNavigate: Enforce 'form-action' CSP on the browser-side.
    Use a NavigationThrottle to enforce the 'form-action' CSP on the
    browser-side. Before this patch, this CSP wasn't checked on
    redirections.
    
    BUG=685074
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation;master.tryserver.chromium.linux:linux_chromium_browser_side_navigation_rel
    
    Review-Url: https://codereview.chromium.org/2689653003
    Cr-Commit-Position: refs/heads/master@{#457966}

--- content/common/navigation_params.cc
+++ content/common/navigation_params.cc
@@ -98,8 +98,8 @@ BeginNavigationParams::BeginNavigationParams()
       has_user_gesture(false),
       skip_service_worker(false),
       request_context_type(REQUEST_CONTEXT_TYPE_LOCATION),
-      mixed_content_context_type(blink::WebMixedContentContextType::Blockable) {
-}
+      mixed_content_context_type(blink::WebMixedContentContextType::Blockable),
+      is_form_submission(false) {}
 
 BeginNavigationParams::BeginNavigationParams(
     std::string headers,
@@ -108,6 +108,7 @@ BeginNavigationParams::BeginNavigationParams(
     bool skip_service_worker,
     RequestContextType request_context_type,
     blink::WebMixedContentContextType mixed_content_context_type,
+    bool is_form_submission,
     const base::Optional<url::Origin>& initiator_origin)
     : headers(headers),
       load_flags(load_flags),
@@ -115,6 +116,7 @@ BeginNavigationParams::BeginNavigationParams(
       skip_service_worker(skip_service_worker),
       request_context_type(request_context_type),
       mixed_content_context_type(mixed_content_context_type),
+      is_form_submission(is_form_submission),
       initiator_origin(initiator_origin) {}
 
 BeginNavigationParams::BeginNavigationParams(
