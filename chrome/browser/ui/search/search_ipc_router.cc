4b67558451be10f35c51729a4ea24eda67192616
yzshen <yzshen@chromium.org>
2017-03-19 00:51:19 -0700

    Mojo C++ bindings: rename GetIsolatedProxy to MakeIsolatedRequest to better match other functions.
    This CL also removes MakeRequestForTesting.
    
    BUG=None
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2705503002
    Cr-Commit-Position: refs/heads/master@{#457994}

--- chrome/browser/ui/search/search_ipc_router.cc
+++ chrome/browser/ui/search/search_ipc_router.cc
@@ -44,7 +44,7 @@ class SearchBoxClientFactoryImpl
     DCHECK(web_contents);
     DCHECK(binding);
     // Before we are connected to a frame we throw away all messages.
-    mojo::GetIsolatedProxy(&search_box_);
+    mojo::MakeIsolatedRequest(&search_box_);
   }
 
   chrome::mojom::SearchBox* GetSearchBox() override {
