112b4e3e703dfa3c3129d38057dec48697460a56
zqzhang <zqzhang@chromium.org>
2017-03-20 12:17:08 -0700

    Fix the conditions when fullscreen detector listeners are registered
    Previously, we rely on the DOMNode[Inserted/Removed]IntoDocument events
    to attach/detach the detector. However this won't work if the video
    element is created from the parsing process. This CL fixes the issue by
    overriding insertedInto() and removedFrom() callbacks, which always
    works regardless of how the video element is inserted into the document.
    
    BUG=703210
    
    Review-Url: https://codereview.chromium.org/2758233003
    Cr-Commit-Position: refs/heads/master@{#458147}

--- third_party/WebKit/Source/core/html/MediaCustomControlsFullscreenDetector.h
+++ third_party/WebKit/Source/core/html/MediaCustomControlsFullscreenDetector.h
@@ -26,6 +26,8 @@ class CORE_EXPORT MediaCustomControlsFullscreenDetector final
   // EventListener implementation.
   bool operator==(const EventListener&) const override;
 
+  void attach();
+  void detach();
   void contextDestroyed();
 
   DECLARE_VIRTUAL_TRACE();
@@ -36,9 +38,6 @@ class CORE_EXPORT MediaCustomControlsFullscreenDetector final
 
   // EventListener implementation.
   void handleEvent(ExecutionContext*, Event*) override;
-
-  void attach();
-  void detach();
 
   HTMLVideoElement& videoElement() { return *m_videoElement; }
 
