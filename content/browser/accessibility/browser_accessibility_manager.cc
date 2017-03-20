da03907f2ac745f9e32601f5dfc34ee0ed9244c9
dmazzoni <dmazzoni@chromium.org>
2017-03-20 11:09:53 -0700

    Accessible bounds should include device scale factor on Windows.
    When reporting the bounding box of an object from the web
    via an accessibility API on Windows, we need to take the device
    scale factor into account. Other platforms either don't need this
    (Mac) or already take it into account elsewhere (Android).
    
    BUG=694605
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2759903002
    Cr-Commit-Position: refs/heads/master@{#458120}

--- content/browser/accessibility/browser_accessibility_manager.cc
+++ content/browser/accessibility/browser_accessibility_manager.cc
@@ -137,7 +137,9 @@ BrowserAccessibilityManager::BrowserAccessibilityManager(
       last_focused_manager_(nullptr),
       connected_to_parent_tree_node_(false),
       ax_tree_id_(ui::AXTreeIDRegistry::kNoAXTreeID),
-      parent_node_id_from_parent_tree_(0) {
+      parent_node_id_from_parent_tree_(0),
+      device_scale_factor_(1.0f),
+      use_custom_device_scale_factor_for_testing_(false) {
   tree_->SetDelegate(this);
 }
 
@@ -153,7 +155,9 @@ BrowserAccessibilityManager::BrowserAccessibilityManager(
       last_focused_node_(nullptr),
       last_focused_manager_(nullptr),
       ax_tree_id_(ui::AXTreeIDRegistry::kNoAXTreeID),
-      parent_node_id_from_parent_tree_(0) {
+      parent_node_id_from_parent_tree_(0),
+      device_scale_factor_(1.0f),
+      use_custom_device_scale_factor_for_testing_(false) {
   tree_->SetDelegate(this);
   Initialize(initial_tree);
 }
@@ -337,6 +341,10 @@ bool BrowserAccessibilityManager::UseRootScrollOffsetsWhenComputingBounds() {
 
 void BrowserAccessibilityManager::OnAccessibilityEvents(
     const std::vector<AXEventNotificationDetails>& details) {
+  // Update the cached device scale factor.
+  if (delegate_ && !use_custom_device_scale_factor_for_testing_)
+    device_scale_factor_ = delegate_->AccessibilityGetDeviceScaleFactor();
+
   // Process all changes to the accessibility tree first.
   for (uint32_t index = 0; index < details.size(); ++index) {
     const AXEventNotificationDetails& detail = details[index];
@@ -1198,6 +1206,12 @@ BrowserAccessibilityManager::SnapshotAXTreeForTesting() {
   ui::AXTreeUpdate update;
   serializer.SerializeChanges(tree_->root(), &update);
   return update;
+}
+
+void BrowserAccessibilityManager::UseCustomDeviceScaleFactorForTesting(
+    float device_scale_factor) {
+  use_custom_device_scale_factor_for_testing_ = true;
+  device_scale_factor_ = device_scale_factor;
 }
 
 BrowserAccessibility* BrowserAccessibilityManager::CachingAsyncHitTest(
