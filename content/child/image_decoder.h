78d110ead7f118a82d03d5bb3611c281bb3202f9
bratell <bratell@opera.com>
2017-03-20 06:45:34 -0700

    Add include guards to header files in content missing them
    Review-Url: https://codereview.chromium.org/2759103002
    Cr-Commit-Position: refs/heads/master@{#458053}

--- content/child/image_decoder.h
+++ content/child/image_decoder.h
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifndef CONTENT_CHILD_IMAGE_DECODER_H_
+#define CONTENT_CHILD_IMAGE_DECODER_H_
+
 #include <stddef.h>
 
 #include <vector>
@@ -45,3 +48,5 @@ class ImageDecoder {
 };
 
 }  // namespace content
+
+#endif  // CONTENT_CHILD_IMAGE_DECODER_H_
