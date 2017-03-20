5e2fb26322dfc875e7bb3310ee917f45233af96a
rogerm <rogerm@chromium.org>
2017-03-20 15:21:53 -0700

    Make ScopedURLFetcherFactory sequenced-safe.
    This CL allows ScopedURLFetcherFactory to be used in a test
    environment exposing a sequenced task runner instead of
    requiring a single thread task runner, a relaxation of the
    requirements (single thread is a stricter instance of a
    sequened task runner).
    
    The URL fetcher code has already been relaxed in a previous
    CL to only require a sequenced runner, but the corresponding
    update to ScopedURLFetcherFactory was overlooked.
    
    BUG=675631
    
    Review-Url: https://codereview.chromium.org/2757233003
    Cr-Commit-Position: refs/heads/master@{#458201}

--- net/url_request/test_url_fetcher_factory.cc
+++ net/url_request/test_url_fetcher_factory.cc
@@ -14,9 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/memory/weak_ptr.h"
-#include "base/single_thread_task_runner.h"
+#include "base/sequenced_task_runner.h"
+#include "base/threading/sequenced_task_runner_handle.h"
 #include "base/threading/thread_restrictions.h"
-#include "base/threading/thread_task_runner_handle.h"
 #include "net/base/host_port_pair.h"
 #include "net/base/io_buffer.h"
 #include "net/base/net_errors.h"
@@ -390,7 +390,7 @@ FakeURLFetcher::~FakeURLFetcher() {}
 
 void FakeURLFetcher::Start() {
   TestURLFetcher::Start();
-  base::ThreadTaskRunnerHandle::Get()->PostTask(
+  base::SequencedTaskRunnerHandle::Get()->PostTask(
       FROM_HERE,
       base::Bind(&FakeURLFetcher::RunDelegate, weak_factory_.GetWeakPtr()));
 }
