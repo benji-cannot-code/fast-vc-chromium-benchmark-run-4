3972c9baa917394130b3e3f7d7bd54f97eff3471
zqzhang <zqzhang@chromium.org>
2017-03-20 16:38:04 -0700

    Notify the initial information when a MediaSessionObserver is newly added
    When a new MediaSessionObserver is added, we should notify it about the
    current state, metadata and actions. This is to avoid having the need of
    exposing interfaces to let the observer check these information.
    
    BUG=702153
    
    Review-Url: https://codereview.chromium.org/2754353003
    Cr-Commit-Position: refs/heads/master@{#458239}

--- chrome/android/java/src/org/chromium/chrome/browser/media/ui/MediaSessionTabHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/ui/MediaSessionTabHelper.java
@@ -255,10 +255,10 @@ public class MediaSessionTabHelper implements MediaImageCallback {
         }
 
         cleanupMediaSessionObserver();
+        mMediaImageManager.setWebContents(webContents);
         if (mediaSession != null) {
             mMediaSessionObserver = createMediaSessionObserver(mediaSession);
         }
-        mMediaImageManager.setWebContents(webContents);
     }
 
     private void cleanupMediaSessionObserver() {
