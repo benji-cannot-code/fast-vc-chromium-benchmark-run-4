d192c9f05b1e1d6de99ea9ec7edb5b77f1d08edb
kkhorimoto <kkhorimoto@chromium.org>
2017-03-20 10:01:26 -0700

    Display the preload Tab if it replaces the current Tab.
    BUG=698833, 702431
    
    Review-Url: https://codereview.chromium.org/2759563003
    Cr-Commit-Position: refs/heads/master@{#458100}

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -4575,6 +4575,10 @@ class BrowserBookmarkModelBridge : public bookmarks::BookmarkModelObserver {
           atIndex:(NSUInteger)index {
   [self uninstallDelegatesForTab:oldTab];
   [self installDelegatesForTab:newTab];
+
+  // Add |newTab|'s view to the hierarchy if it's the current Tab.
+  if (self.active && model.currentTab == newTab)
+    [self displayTab:newTab isNewSelection:NO];
 }
 
 // A tab has been removed, remove its views from display if necessary.
