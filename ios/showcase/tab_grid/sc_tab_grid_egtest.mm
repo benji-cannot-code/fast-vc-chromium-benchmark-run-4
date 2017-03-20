45b00409bed0fc7405743af3b1c36d188ff5ae37
baxley <baxley@chromium.org>
2017-03-20 11:14:33 -0700

    Add disabled_test_macro.h import to showcase base test case.
    Remove import from test that needs to disable a test. This makes
    it easier for tests to be disabled in teh future, not requiring an
    import statment to be added. This code is often never removed, so
    it will make maintenance easier when tests are re-enabled.
    
    BUG=70004
    
    Review-Url: https://codereview.chromium.org/2761563002
    Cr-Commit-Position: refs/heads/master@{#458123}

--- ios/showcase/tab_grid/sc_tab_grid_egtest.mm
+++ ios/showcase/tab_grid/sc_tab_grid_egtest.mm
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <EarlGrey/EarlGrey.h>
 
 #import "ios/showcase/test/showcase_test_case.h"
-#import "ios/testing/earl_grey/disabled_test_macros.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
--- ios/showcase/test/showcase_test_case.h
+++ ios/showcase/test/showcase_test_case.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <XCTest/XCTest.h>
 
+#import "ios/testing/earl_grey/disabled_test_macros.h"
+
 // Base class for all Showcase Earl Grey tests.
 @interface ShowcaseTestCase : XCTestCase
 @end
