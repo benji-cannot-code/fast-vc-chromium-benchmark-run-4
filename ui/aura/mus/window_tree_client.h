e4c4312586c8ec5c61c4b9257b99f363fbc743b1
moshayedi <moshayedi@chromium.org>
2017-03-20 08:15:31 -0700

    Inform window manager about modal windows in mus+ash.
    We need to inform window manager about modal windows. This is necessary because
    for example SystemModalContainerLayoutManager needs to know about modal types
    so it can create the fullscreen grey background for system modals.
    
    After this change we have some of the WM effects for modal windows (e.g.
    animating system modal windows when clicked outside of it), but for
    the rest we need more wiring (e.g. no grey background yet) which will
    be done in subsequent CLs.
    
    BUG=645996
    
    Review-Url: https://codereview.chromium.org/2745143004
    Cr-Commit-Position: refs/heads/master@{#458065}

--- ui/aura/mus/window_tree_client.h
+++ ui/aura/mus/window_tree_client.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/mus/mus_types.h"
 #include "ui/aura/mus/window_manager_delegate.h"
 #include "ui/aura/mus/window_tree_host_mus_delegate.h"
+#include "ui/base/ui_base_types.h"
 
 namespace base {
 class Thread;
@@ -405,6 +406,7 @@ class AURA_EXPORT WindowTreeClient
       Id window_id,
       const std::string& name,
       const base::Optional<std::vector<uint8_t>>& transit_data) override;
+  void WmSetModalType(Id window_id, ui::ModalType type) override;
   void WmSetCanFocus(Id window_id, bool can_focus) override;
   void WmCreateTopLevelWindow(
       uint32_t change_id,
