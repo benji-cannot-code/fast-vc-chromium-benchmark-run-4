7b510bf7ff33db083a1e26d649993dff69b09f9e
ulan <ulan@chromium.org>
2017-03-20 09:37:52 -0700

    Enable EQT metric for v8.browsing* benchmarks.
    BUG=chromium:625701
    
    Review-Url: https://codereview.chromium.org/2757283002
    Cr-Commit-Position: refs/heads/master@{#458086}

--- tools/perf/benchmarks/v8_browsing.py
+++ tools/perf/benchmarks/v8_browsing.py
@@ -43,6 +43,11 @@ class _V8BrowsingBenchmark(perf_benchmark.PerfBenchmark):
       '-*',
       # Memory categories.
       'disabled-by-default-memory-infra',
+      # EQT categories.
+      'blink.user_timing',
+      'loading',
+      'navigation',
+      'toplevel',
       # V8 categories.
       'blink.console',
       'disabled-by-default-v8.gc',
@@ -62,7 +67,8 @@ class _V8BrowsingBenchmark(perf_benchmark.PerfBenchmark):
     memory_dump_config = chrome_trace_config.MemoryDumpConfig()
     memory_dump_config.AddTrigger('light', 1000)
     options.config.chrome_trace_config.SetMemoryDumpConfig(memory_dump_config)
-    options.SetTimelineBasedMetrics(['v8AndMemoryMetrics'])
+    options.SetTimelineBasedMetrics([
+      'expectedQueueingTimeMetric', 'v8AndMemoryMetrics'])
     return options
 
   def CreateStorySet(self, options):
@@ -78,8 +84,8 @@ class _V8BrowsingBenchmark(perf_benchmark.PerfBenchmark):
     if 'v8-gc' in value.name:
       return (_V8_GC_HIGH_LEVEL_STATS_RE.search(value.name) and
               not _IGNORED_V8_STATS_RE.search(value.name))
-    # Allow all other non-GC metrics.
-    return 'v8' in value.name
+    # Allow all other metrics.
+    return True
 
   @classmethod
   def ShouldTearDownStateAfterEachStoryRun(cls):
@@ -102,6 +108,11 @@ class _V8RuntimeStatsBrowsingBenchmark(perf_benchmark.PerfBenchmark):
       # UE categories requred by runtimeStatsTotalMetric to bucket
       # runtimeStats by UE.
       'rail',
+      # EQT categories.
+      'blink.user_timing',
+      'loading',
+      'navigation',
+      'toplevel',
       # V8 categories.
       'blink.console',
       'disabled-by-default-v8.gc',
@@ -119,7 +130,8 @@ class _V8RuntimeStatsBrowsingBenchmark(perf_benchmark.PerfBenchmark):
     memory_dump_config.AddTrigger('light', 1000)
     options.config.chrome_trace_config.SetMemoryDumpConfig(memory_dump_config)
 
-    options.SetTimelineBasedMetrics(['runtimeStatsTotalMetric', 'gcMetric'])
+    options.SetTimelineBasedMetrics([
+      'expectedQueueingTimeMetric', 'runtimeStatsTotalMetric', 'gcMetric'])
     return options
 
   def CreateStorySet(self, options):
