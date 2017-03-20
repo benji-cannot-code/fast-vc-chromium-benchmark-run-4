dbf73c63da6ba7c1148edbb55aec3bc6f04aeb36
vasilii <vasilii@chromium.org>
2017-03-20 03:21:08 -0700

    Revert of RELAND: ShapeDetection: use mojom::Bitmap for mojo interface. (patchset #2 id:20001 of https://codereview.chromium.org/2753413002/ )
    Reason for revert:
    Broke shapedetection/detection-on-worker.html
    shapedetection/detection-security-test.html reliably:
    
    18:14:35.412 4150   [10306:39683:0319/181429.446067:1229254778376:WARNING:url_request_job_manager.cc(90)] Failed to map: layout-test-mojom://content/shell/renderer/layout_test/frame_interface_registry
    18:14:35.413 4150   [10306:39683:0319/181429.446194:1229254899173:WARNING:url_request_job_manager.cc(90)] Failed to map: layout-test-mojom://content/shell/renderer/layout_test/interface_registry
    18:14:35.413 4150   [10308:771:0319/181429.450430:1229259136455:ERROR:mojo_context_state.cc(199)] Failed to fetch source for module "content/shell/renderer/layout_test/frame_interface_registry"
    18:14:35.413 4150   [10308:771:0319/181429.450505:1229259209121:ERROR:mojo_context_state.cc(199)] Failed to fetch source for module "content/shell/renderer/layout_test/interface_registry"
    18:14:35.413 4150   [10308:771:0319/181429.452582:1229261287723:ERROR:mojo_context_state.cc(199)] Failed to fetch source for module "skia/public/interfaces/bitmap.mojom"
    18:14:35.414 4111 [46631/51882] shapedetection/detection-security-test.html failed unexpectedly (test timed out)
    
    Original issue's description:
    > RELAND: ShapeDetection: use mojom::Bitmap for mojo interface.
    >
    > Original CL got reverted due to shapedetection/detection-on-worker.html
    > fail to fetch source for module skia/public/interfaces/bitmap.mojom.
    >
    > This CL includes the generated JS bindings for skia/public/interfaces/bitmap.mojom
    > in the layout tests archive.
    >
    > Original CL description ------------------------------------------------
    > ShapeDetection: use mojom::Bitmap for mojo interface.
    >
    > This CL uses mojo::Bitmap for mojo ShapeDetection interfaces definition,
    > so that the Detect API is completely flexible and friendly.
    >
    > BUG=665488
    > TEST(Layout)=
    >  third_party/WebKit/LayoutTests/fast/shapedetection/shapedetection-creation.html
    >  third_party/WebKit/LayoutTests/fast/shapedetection/shapedetection-empty-input.html
    >  third_party/WebKit/LayoutTests/shapedetection/detection-HTMLCanvasElement.html
    >  third_party/WebKit/LayoutTests/shapedetection/detection-HTMLImageElement.html
    >  third_party/WebKit/LayoutTests/shapedetection/detection-HTMLVideoElement.html
    >  third_party/WebKit/LayoutTests/shapedetection/detection-ImageBitmap.html
    >  third_party/WebKit/LayoutTests/shapedetection/detection-ImageData.html
    >
    > Review-Url: https://codereview.chromium.org/2629433003
    > Cr-Commit-Position: refs/heads/master@{#446142}
    > (cherry picked from commit 33adf4c85dd553eb607bcf9c02c5fa60c8738a68)
    >
    > Conflicts:
    > 	third_party/WebKit/Source/modules/shapedetection/FaceDetector.cpp
    > 	third_party/WebKit/Source/modules/shapedetection/TextDetector.cpp
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.mac:mac_optional_gpu_tests_rel
    >
    > TBR=rockot@chromium.org, dtrainor@chromium.org, haraken@chromium.org, bsalomon@google.com, tsepez@chromium.org
    > since the gist of the CL hasn't changed.
    >
    > Review-Url: https://codereview.chromium.org/2681913003
    > Cr-Original-Commit-Position: refs/heads/master@{#450582}
    > Review-Url: https://codereview.chromium.org/2753413002
    > Cr-Commit-Position: refs/heads/master@{#458010}
    > Committed: https://chromium.googlesource.com/chromium/src/+/b1caeee1ee08c8b3bcaa423c8aa4a9f06e9ce40a
    
    TBR=bsalomon@google.com,dtrainor@chromium.org,haraken@chromium.org,rockot@chromium.org,tsepez@chromium.org,mcasas@chromium.org,dpranke@chromium.org,junwei.fu@intel.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=665488
    
    Review-Url: https://codereview.chromium.org/2755393002
    Cr-Commit-Position: refs/heads/master@{#458033}

--- third_party/WebKit/Source/modules/shapedetection/ShapeDetector.h
+++ third_party/WebKit/Source/modules/shapedetection/ShapeDetector.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/imagebitmap/ImageBitmapFactories.h"
 #include "modules/ModulesExport.h"
 #include "modules/canvas2d/CanvasRenderingContext2D.h"
-#include "skia/public/interfaces/bitmap.mojom-blink.h"
 
 namespace blink {
 
@@ -29,7 +28,9 @@ class MODULES_EXPORT ShapeDetector
                                            const HTMLImageElement*);
 
   virtual ScriptPromise doDetect(ScriptPromiseResolver*,
-                                 skia::mojom::blink::BitmapPtr) = 0;
+                                 mojo::ScopedSharedBufferHandle,
+                                 int imageWidth,
+                                 int imageHeight) = 0;
 };
 
 }  // namespace blink
