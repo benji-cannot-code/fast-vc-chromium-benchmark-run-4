325d6f951df8fae0df2c606e5ec353d21c9474cf
Vasilii Sukhanov <vasilii@chromium.org>
2017-03-20 11:00:22 +0100

    Disable RenderWidgetHostViewChildFrameTest.FrameEvictionKeepsLocalSurfaceId on Win
    BUG=696919
    TBR=tzik@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2761723002 .
    Cr-Commit-Position: refs/heads/master@{#458031}

--- content/browser/frame_host/render_widget_host_view_child_frame_unittest.cc
+++ content/browser/frame_host/render_widget_host_view_child_frame_unittest.cc
@@ -143,9 +143,12 @@ cc::CompositorFrame CreateDelegatedFrame(float scale_factor,
 #if defined(OS_WIN)
 #define MAYBE_VisibilityTest DISABLED_VisibilityTest
 #define MAYBE_SwapCompositorFrame DISABLED_SwapCompositorFrame
+#define MAYBE_FrameEvictionKeepsLocalSurfaceId \
+  DISABLED_FrameEvictionKeepsLocalSurfaceId
 #else
 #define MAYBE_VisibilityTest VisibilityTest
 #define MAYBE_SwapCompositorFrame SwapCompositorFrame
+#define MAYBE_FrameEvictionKeepsLocalSurfaceId FrameEvictionKeepsLocalSurfaceId
 #endif
 
 TEST_F(RenderWidgetHostViewChildFrameTest, MAYBE_VisibilityTest) {
@@ -190,7 +193,8 @@ TEST_F(RenderWidgetHostViewChildFrameTest, MAYBE_SwapCompositorFrame) {
 
 // Check that frame eviction does not trigger allocation of a new local surface
 // id.
-TEST_F(RenderWidgetHostViewChildFrameTest, FrameEvictionKeepsLocalSurfaceId) {
+TEST_F(RenderWidgetHostViewChildFrameTest,
+       MAYBE_FrameEvictionKeepsLocalSurfaceId) {
   gfx::Size view_size(100, 100);
   gfx::Rect view_rect(view_size);
   float scale_factor = 1.f;
