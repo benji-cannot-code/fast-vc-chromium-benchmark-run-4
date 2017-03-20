4b3dbb534fdde59d78f838aaf0e79f3789acef69
dpapad <dpapad@chromium.org>
2017-03-20 15:23:40 -0700

    MD Settings: Restore message shown when no "zoom" exceptions exist.
    BUG=703030
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2757333003
    Cr-Commit-Position: refs/heads/master@{#458202}

--- chrome/test/data/webui/settings/zoom_levels_tests.js
+++ chrome/test/data/webui/settings/zoom_levels_tests.js
@@ -76,6 +76,9 @@ cr.define('zoom_levels', function() {
         var list = testElement.$.list;
         assertTrue(!!list);
         assertEquals(0, list.items.length);
+        assertEquals(
+            0, testElement.shadowRoot.querySelectorAll('.list-item').length);
+        assertTrue(!!testElement.$$('#empty'));
       });
 
       test('non-empty zoom state', function() {
@@ -85,6 +88,9 @@ cr.define('zoom_levels', function() {
           var list = testElement.$.list;
           assertTrue(!!list);
           assertEquals(2, list.items.length);
+          assertFalse(!!testElement.$$('#empty'));
+          assertEquals(
+              2, testElement.shadowRoot.querySelectorAll('.list-item').length);
 
           var removeButton =
               getRemoveButton(testElement.$.listContainer, 0);
