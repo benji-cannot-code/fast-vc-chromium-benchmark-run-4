5df8ada2be18103c2573c73fb8e2b1c6fe9ff1c8
maxmorin <maxmorin@chromium.org>
2017-03-20 01:39:31 -0700

    Remove dangling PlayingState pointers in WebRtcAudioRenderer.
    This solution is a bit of a hack, but it works. Did manual testing and ran
    unit tests. Bug 697256 does not reproduce with this patch.
    
    R=tommi@chromium.org
    
    BUG=697256
    
    Review-Url: https://codereview.chromium.org/2758453004
    Cr-Commit-Position: refs/heads/master@{#458025}

--- content/renderer/media/webrtc_audio_renderer.h
+++ content/renderer/media/webrtc_audio_renderer.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <map>
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -193,6 +194,9 @@ class CONTENT_EXPORT WebRtcAudioRenderer
   // of playing renderers.
   void OnPlayStateChanged(const blink::WebMediaStream& media_stream,
                           PlayingState* state);
+
+  // Called when |state| is about to be destructed.
+  void OnPlayStateRemoved(PlayingState* state);
 
   // Updates |sink_params_| and |audio_fifo_| based on |sink_|, and initializes
   // |sink_|.
