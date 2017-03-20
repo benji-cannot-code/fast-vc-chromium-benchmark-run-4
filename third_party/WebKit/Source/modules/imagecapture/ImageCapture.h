0157262fe9012a097ca9aaccb553a261cc67a137
mcasas <mcasas@chromium.org>
2017-03-20 14:10:02 -0700

    Image Capture: cache constraints and append them to MediaStreamTrack.getConstraints()
    This CL teaches Image Capture to cache the constraints passed via
    applyConstraints() and wires MediaStreamTrack::getConstraints() to
    append those upon request.
    
    Note that constraints are accumulative, i.e. two calls to
    applyConstraints() with different constraints will essentially
    be understood by the platform as a single applyConstraints()
    call with the constraint dictionaries merged.
    
    (Also moved MediaStreamTrack::getConstraints() to follow the
    declaration order in MediaStreamTrack.h).
    
    Added LayoutTests.
    
    BUG=700607
    
    Review-Url: https://codereview.chromium.org/2758063002
    Cr-Commit-Position: refs/heads/master@{#458183}

--- third_party/WebKit/Source/modules/imagecapture/ImageCapture.h
+++ third_party/WebKit/Source/modules/imagecapture/ImageCapture.h
@@ -15,13 +15,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/EventTargetModules.h"
 #include "modules/ModulesExport.h"
 #include "modules/mediastream/MediaTrackCapabilities.h"
+#include "modules/mediastream/MediaTrackConstraintSet.h"
 #include "platform/AsyncMethodRunner.h"
 
 namespace blink {
 
 class ExceptionState;
 class MediaStreamTrack;
-class MediaTrackConstraintSet;
 class PhotoSettings;
 class ScriptPromiseResolver;
 class WebImageCaptureFrameGrabber;
@@ -65,6 +65,7 @@ class MODULES_EXPORT ImageCapture final
 
   void setMediaTrackConstraints(ScriptPromiseResolver*,
                                 const MediaTrackConstraintSet&);
+  const MediaTrackConstraintSet& getMediaTrackConstraints() const;
 
   DECLARE_VIRTUAL_TRACE();
 
@@ -82,6 +83,8 @@ class MODULES_EXPORT ImageCapture final
   std::unique_ptr<WebImageCaptureFrameGrabber> m_frameGrabber;
   media::mojom::blink::ImageCapturePtr m_service;
   MediaTrackCapabilities m_capabilities;
+
+  MediaTrackConstraintSet m_currentConstraints;
 
   HeapHashSet<Member<ScriptPromiseResolver>> m_serviceRequests;
 };
