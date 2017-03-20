3b33c87b1c73bb5d4bcc67f33f503d6c53d04ab6
daniel.bevenius <daniel.bevenius@gmail.com>
2017-03-20 02:00:58 -0700

    Adding checks for V8 functions returning Maybe<bool>
    While working on an V8 issue [1], involving adding V8_WARN_UNUSED_RESULT
    to functions that were missing it, we discovered that a few calls to
    these functions were not using the returned value.
    
    This commit allows compliation to succeed, but in most cases only adds a
    comment that something should be done. I'm opening this CL to get
    feedback on what the appropriate actions should be.
    
    To try this out I used a .gclient with a custom_deps like this:
    u'custom_deps': {
      "src/v8":
    "git@github.com:danbev/v8.git@064718a8921608eaf9b5eadbb7d734ec04068a87"
    },
    
    [1] https://codereview.chromium.org/2135973002/
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2144093002
    Cr-Commit-Position: refs/heads/master@{#458028}

--- third_party/WebKit/Source/bindings/core/v8/ScriptPromise.cpp
+++ third_party/WebKit/Source/bindings/core/v8/ScriptPromise.cpp
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "bindings/core/v8/ScriptPromise.h"
-
 #include "bindings/core/v8/ExceptionMessages.h"
 #include "bindings/core/v8/ExceptionState.h"
 #include "bindings/core/v8/ToV8.h"
@@ -189,15 +188,15 @@ void ScriptPromise::InternalResolver::resolve(v8::Local<v8::Value> value) {
   if (m_resolver.isEmpty())
     return;
   m_resolver.v8Value().As<v8::Promise::Resolver>()->Resolve(
-      m_resolver.context(), value);
+      m_resolver.context(), value).ToChecked();
   clear();
 }
 
 void ScriptPromise::InternalResolver::reject(v8::Local<v8::Value> value) {
   if (m_resolver.isEmpty())
     return;
-  m_resolver.v8Value().As<v8::Promise::Resolver>()->Reject(m_resolver.context(),
-                                                           value);
+  m_resolver.v8Value().As<v8::Promise::Resolver>()->Reject(
+      m_resolver.context(), value).ToChecked();
   clear();
 }
 
@@ -312,7 +311,7 @@ v8::Local<v8::Promise> ScriptPromise::rejectRaw(ScriptState* scriptState,
   if (!v8::Promise::Resolver::New(scriptState->context()).ToLocal(&resolver))
     return v8::Local<v8::Promise>();
   v8::Local<v8::Promise> promise = resolver->GetPromise();
-  resolver->Reject(scriptState->context(), value);
+  resolver->Reject(scriptState->context(), value).ToChecked();
   return promise;
 }
 
--- third_party/WebKit/Source/bindings/core/v8/V8Binding.cpp
+++ third_party/WebKit/Source/bindings/core/v8/V8Binding.cpp
@@ -918,7 +918,7 @@ void removeHiddenValueFromArray(v8::Isolate* isolate,
     if (!array->Get(isolate->GetCurrentContext(), i).ToLocal(&item))
       return;
     if (item->StrictEquals(value)) {
-      array->Delete(isolate->GetCurrentContext(), i);
+      array->Delete(isolate->GetCurrentContext(), i).ToChecked();
       return;
     }
   }
