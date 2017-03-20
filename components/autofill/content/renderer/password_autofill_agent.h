295ad34560e0b63080671d17c36f0d563da63cf0
sense <sense@yandex-team.ru>
2017-03-20 03:12:07 -0700

    Add password form search using blink::WebNode reference comparison.
    After an XHR is completed, PasswordAutofillAgent tries to check if the
    form is still visible (i.e. if the user has successfuly logged in or
    still sees the form) by searching the form with the same action URL.
    This works until the form has action URL. If it doesn't, the search is
    performed using field matching. This will fail if the form is modified
    by the website right before the XHR has completed.
    This CL adds support for the form search using blink::WebFormElement
    and blink::WebInputElement (for unowned forms) reference comparison.
    
    BUG=700862
    R=dvadym@chromium.org
    
    Review-Url: https://codereview.chromium.org/2746033004
    Cr-Commit-Position: refs/heads/master@{#458032}

--- components/autofill/content/renderer/password_autofill_agent.h
+++ components/autofill/content/renderer/password_autofill_agent.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/content/common/autofill_driver.mojom.h"
 #include "components/autofill/content/renderer/autofill_agent.h"
 #include "components/autofill/content/renderer/password_form_conversion_utils.h"
+#include "components/autofill/content/renderer/provisionally_saved_password_form.h"
 #include "components/autofill/core/common/form_data_predictions.h"
 #include "components/autofill/core/common/password_form.h"
 #include "components/autofill/core/common/password_form_field_prediction_map.h"
@@ -240,14 +241,16 @@ class PasswordAutofillAgent : public content::RenderFrameObserver,
   void ClearPreview(blink::WebInputElement* username,
                     blink::WebInputElement* password);
 
-  // Saves |password_form| in |provisionally_saved_form_|, as long as it
-  // satisfies |restriction|.
+  // Saves |password_form|, |form| and |input| in |provisionally_saved_form_|,
+  // as long as it satisfies |restriction|. |form| and |input| are the elements
+  // user has just been interacting with before the form save. |form| or |input|
+  // can be null but not both at the same time. For example: if the form is
+  // unowned, |form| will be null; if the user has submitted the form, |input|
+  // will be null.
   void ProvisionallySavePassword(std::unique_ptr<PasswordForm> password_form,
+                                 const blink::WebFormElement& form,
+                                 const blink::WebInputElement& input,
                                  ProvisionallySaveRestriction restriction);
-
-  // Returns true if |provisionally_saved_form_| has enough information that
-  // it is likely filled out.
-  bool ProvisionallySavedPasswordIsValid();
 
   // Helper function called when in-page navigation completed
   void OnSamePageNavigationCompleted();
@@ -261,7 +264,7 @@ class PasswordAutofillAgent : public content::RenderFrameObserver,
 
   // Set if the user might be submitting a password form on the current page,
   // but the submit may still fail (i.e. doesn't pass JavaScript validation).
-  std::unique_ptr<PasswordForm> provisionally_saved_form_;
+  ProvisionallySavedPasswordForm provisionally_saved_form_;
 
   // Map WebFormControlElement to the pair of:
   // 1) The most recent text that user typed or PasswordManager autofilled in
