757b0777c8310543ec7a94cb9b62ffc15e3aab54
shend <shend@chromium.org>
2017-03-19 19:17:45 -0700

    Move cursor property to be generated in ComputedStyleBase.
    Move the inherited property 'cursor' and its enum, ECursor,
    to be generated in ComputedStyleBase. Also moved getter / setter /
    initial value methods to be generated in ComputedStyleBase. Finally
    replaced handwritten conversion between CSSValueID and ECursor with
    generated code.
    
    Note that this renames m_cursorStyle to m_cursor to match
    the name of the getter/setter. This shouldn't affect anything
    since the member is private and only used within the generated
    class.
    
    The changes in generated code is triggered by adding the 'keyword'
    field_template to the cursor property in CSSProperties.json5.
    
    Diff: https://gist.github.com/darrnshn/2a3770b1721a7354110633a5ea6fcec1/revisions
    
    BUG=628043
    
    Review-Url: https://codereview.chromium.org/2742253002
    Cr-Commit-Position: refs/heads/master@{#458012}

--- third_party/WebKit/Source/core/css/CSSValueIDMappings.h
+++ third_party/WebKit/Source/core/css/CSSValueIDMappings.h
@@ -66,6 +66,15 @@ inline WritingMode cssValueIDToPlatformEnum(CSSValueID v) {
   return WritingMode::kHorizontalTb;
 }
 
+template <>
+inline ECursor cssValueIDToPlatformEnum(CSSValueID v) {
+  if (v == CSSValueWebkitZoomIn)
+    return ECursor::kZoomIn;
+  if (v == CSSValueWebkitZoomOut)
+    return ECursor::kZoomOut;
+  return detail::cssValueIDToPlatformEnumGenerated<ECursor>(v);
+}
+
 }  // namespace blink
 
 #endif
