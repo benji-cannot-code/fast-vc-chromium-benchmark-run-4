cccc5eb589bb67decfbb833837b3c9ea4dc05e98
sky <sky@chromium.org>
2017-03-20 09:16:45 -0700

    Remove ui::Event::name
    It's value is initialized from the type, so rather than maintaining it
    as a member add a getter that computes the name based on the type.
    
    BUG=none
    TEST=none
    R=sadrul@chromium.org
    
    Review-Url: https://codereview.chromium.org/2756863003
    Cr-Commit-Position: refs/heads/master@{#458080}

--- ui/chromeos/touch_accessibility_enabler.cc
+++ ui/chromeos/touch_accessibility_enabler.cc
@@ -87,7 +87,7 @@ void TouchAccessibilityEnabler::HandleTouchEvent(const ui::TouchEvent& event) {
       return;
     }
   } else {
-    NOTREACHED() << "Unexpected event type received: " << event.name();
+    NOTREACHED() << "Unexpected event type received: " << event.GetName();
     return;
   }
 
