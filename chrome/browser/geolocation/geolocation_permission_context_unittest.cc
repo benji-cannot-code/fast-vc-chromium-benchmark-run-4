2288364842697b9aac6b06998fd55ea8459b66a9
benwells <benwells@chromium.org>
2017-03-20 00:15:49 -0700

    Fix flaky GeolocationPermissionContext test.
    The test suite did not initialize state correctly, resulting on a test
    only passing if other tests had run first.
    
    BUG=701719
    
    Review-Url: https://codereview.chromium.org/2760823002
    Cr-Commit-Position: refs/heads/master@{#458019}

--- chrome/browser/geolocation/geolocation_permission_context_unittest.cc
+++ chrome/browser/geolocation/geolocation_permission_context_unittest.cc
@@ -323,6 +323,8 @@ void GeolocationPermissionContextTests::SetUp() {
           std::unique_ptr<LocationSettings>(new MockLocationSettings()));
   MockLocationSettings::SetLocationStatus(true, true);
   MockLocationSettings::SetCanPromptForAndroidPermission(true);
+  MockLocationSettings::SetLocationSettingsDialogStatus(false /* enabled */,
+                                                        GRANTED);
 #else
   SetupRequestManager(web_contents());
 #endif
@@ -1135,6 +1137,8 @@ TEST_F(GeolocationPermissionContextTests, GeolocationStatusSystemDisabled) {
                                CONTENT_SETTING_ALLOW);
   MockLocationSettings::SetLocationStatus(true /* android */,
                                           false /* system */);
+  MockLocationSettings::SetLocationSettingsDialogStatus(true /* enabled */,
+                                                        DENIED);
   ASSERT_EQ(blink::mojom::PermissionStatus::ASK,
             PermissionManager::Get(profile())->GetPermissionStatus(
                 content::PermissionType::GEOLOCATION, requesting_frame,
