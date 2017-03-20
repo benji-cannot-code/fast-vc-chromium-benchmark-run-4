d21b43effe202ea8891d7f352029771bb808f313
noel <noel@chromium.org>
2017-03-20 01:08:32 -0700

    ImageWriter: Add ShouldResetImageWriter helper
    Move the code for resetting the image_writer_ into a common helper.
    Add comments, refer back to the original crbug.com issue and change
    set that resolved issue 352442.
    
    Covered by existing tests:
      image_writer_utility_client_browsertest, image_writer_unittest
    
    BUG=680928,352442
    
    Review-Url: https://codereview.chromium.org/2734773002
    Cr-Commit-Position: refs/heads/master@{#458024}

--- chrome/utility/image_writer/image_writer_handler.h
+++ chrome/utility/image_writer/image_writer_handler.h
@@ -42,6 +42,9 @@ class ImageWriterHandler {
  private:
   void Cancel();
 
+  bool ShouldResetImageWriter(const base::FilePath& image,
+                              const base::FilePath& device);
+
   extensions::mojom::RemovableStorageWriterClientPtr client_;
   std::unique_ptr<ImageWriter> image_writer_;
 };
