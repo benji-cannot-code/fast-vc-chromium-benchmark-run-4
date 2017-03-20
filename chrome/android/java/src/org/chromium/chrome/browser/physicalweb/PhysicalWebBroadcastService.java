d288a9725463dc657293977f0123f0eb85eec41f
iankc <iankc@google.com>
2017-03-20 15:52:00 -0700

    Build a Bridge to Eddystone Encoder in Components
    This change links the Eddystone Encoder in components to the android
    Physical Web Share feature through the Java Native Interface.
    
    Add Platform Independent Eddystone Encoder
    
    This change creates an eddystone encoder in cpp, which can be used
    across platforms for future physical web projects. Right now,
    specifically, it will be used for Physical Web Sharing. Here are the
    eddystone specs:
    https://github.com/google/eddystone/blob/master/protocol-specification.md
    
    BUG=685856
    
    Review-Url: https://codereview.chromium.org/2753683002
    Cr-Commit-Position: refs/heads/master@{#458214}

--- chrome/android/java/src/org/chromium/chrome/browser/physicalweb/PhysicalWebBroadcastService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/physicalweb/PhysicalWebBroadcastService.java
@@ -96,7 +96,7 @@ public class PhysicalWebBroadcastService extends Service {
         filter.addAction(STOP_SERVICE);
         registerReceiver(mBroadcastReceiver, filter);
 
-        // TODO(iankc): implement parsing, broadcasting, Url Shortener.
+        // TODO(iankc): implement broadcasting, Url Shortener.
         createBroadcastNotification(displayUrl);
         return START_STICKY;
     }
@@ -162,4 +162,6 @@ public class PhysicalWebBroadcastService extends Service {
                 (NotificationManager) getSystemService(NOTIFICATION_SERVICE));
         notificationManager.cancel(NotificationConstants.NOTIFICATION_ID_PHYSICAL_WEB);
     }
+
+    private native byte[] nativeEncodeUrl(String url);
 }
--- chrome/browser/android/chrome_jni_registrar.cc
+++ chrome/browser/android/chrome_jni_registrar.cc
@@ -78,6 +78,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/omnibox/omnibox_prerender.h"
 #include "chrome/browser/android/password_ui_view_android.h"
 #include "chrome/browser/android/payments/service_worker_payment_app_bridge.h"
+#include "chrome/browser/android/physical_web/eddystone_encoder_bridge.h"
 #include "chrome/browser/android/physical_web/physical_web_data_source_android.h"
 #include "chrome/browser/android/policy/policy_auditor.h"
 #include "chrome/browser/android/precache/precache_launcher.h"
@@ -296,6 +297,7 @@ static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
     {"DownloadController", DownloadController::RegisterDownloadController},
     {"DownloadManagerService",
      DownloadManagerService::RegisterDownloadManagerService},
+    {"EddystoneEncoderBridge", RegisterEddystoneEncoderBridge},
     {"ExternalDataUseObserverBridge",
      chrome::android::RegisterExternalDataUseObserver},
     {"ExternalPrerenderRequestHandler",
