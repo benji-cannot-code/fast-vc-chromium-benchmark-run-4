67acf7eed82ee6c400c6b2fe53278835ee5d39cf
grt <grt@chromium.org>
2017-03-21 14:01:29 -0700

    IWYU fix in chrome/browser/ui/views/apps.
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2767583002
    Cr-Commit-Position: refs/heads/master@{#458544}

--- chrome/browser/ui/views/apps/chrome_native_app_window_views_win.cc
+++ chrome/browser/ui/views/apps/chrome_native_app_window_views_win.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/apps/glass_app_window_frame_view_win.h"
 #include "chrome/browser/web_applications/web_app.h"
 #include "chrome/browser/web_applications/web_app_win.h"
-#include "chrome/common/chrome_icon_resources_win.h"
 #include "chrome/common/chrome_switches.h"
 #include "content/public/browser/browser_thread.h"
 #include "extensions/browser/app_window/app_window.h"
