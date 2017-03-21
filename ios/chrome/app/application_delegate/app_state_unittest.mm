449f611542f9ffda90ae405e15ddeed8b690eaa7
gambard <gambard@chromium.org>
2017-03-21 08:07:49 -0700

    Mark the BVC as inactive on App shutdown
    Marking the BVC as inactive all it to cancel all current dialogs.
    
    BUG=663305
    
    Review-Url: https://codereview.chromium.org/2746333006
    Cr-Commit-Position: refs/heads/master@{#458414}

--- ios/chrome/app/application_delegate/app_state_unittest.mm
+++ ios/chrome/app/application_delegate/app_state_unittest.mm
@@ -497,6 +497,8 @@ TEST_F(AppStateWithThreadTest, willTerminate) {
   IOSChromeScopedTestingChromeBrowserProvider provider_(
       base::MakeUnique<FakeChromeBrowserProvider>());
 
+  id browserViewController = OCMClassMock([BrowserViewController class]);
+  OCMExpect([browserViewController setActive:NO]);
   id browserLauncher =
       [OCMockObject mockForProtocol:@protocol(BrowserLauncher)];
   id applicationDelegate =
@@ -508,6 +510,7 @@ TEST_F(AppStateWithThreadTest, willTerminate) {
       browserInitializationStage];
   [[[browserLauncher stub] andReturn:browserViewInformation]
       browserViewInformation];
+  [[[browserViewInformation stub] andReturn:browserViewController] currentBVC];
 
   id settingsNavigationController =
       [OCMockObject mockForClass:[SettingsNavigationController class]];
@@ -539,6 +542,7 @@ TEST_F(AppStateWithThreadTest, willTerminate) {
                applicationNavigation:appNavigation];
 
   // Test.
+  EXPECT_OCMOCK_VERIFY(browserViewController);
   EXPECT_OCMOCK_VERIFY(startupInformation);
   EXPECT_OCMOCK_VERIFY(appNavigation);
   EXPECT_OCMOCK_VERIFY(browserViewInformation);
