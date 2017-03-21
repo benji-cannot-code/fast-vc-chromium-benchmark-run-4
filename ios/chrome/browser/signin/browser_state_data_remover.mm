172d2245a3e6cda9095bfba96f66788d5ab46289
olivierrobin <olivierrobin@chromium.org>
2017-03-21 05:03:14 -0700

    Remove all ReadingList entries on managed account signout.
    Clearing happens after the actual signout, so only local data is deleted.
    Also interrupt current distillation on signout as profile may change.
    
    BUG=699590
    
    Review-Url: https://codereview.chromium.org/2745313004
    Cr-Commit-Position: refs/heads/master@{#458377}

--- ios/chrome/browser/signin/browser_state_data_remover.mm
+++ ios/chrome/browser/signin/browser_state_data_remover.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/common/signin_pref_names.h"
 #include "ios/chrome/browser/bookmarks/bookmarks_utils.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
+#include "ios/chrome/browser/reading_list/reading_list_remover_helper.h"
 #import "ios/chrome/browser/ui/commands/UIKit+ChromeExecuteCommand.h"
 #import "ios/chrome/browser/ui/commands/clear_browsing_data_command.h"
 
@@ -62,11 +63,24 @@ void BrowserStateDataRemover::RemoveBrowserStateData(ProceduralBlock callback) {
 
 void BrowserStateDataRemover::NotifyWithDetails(
     const IOSChromeBrowsingDataRemover::NotificationDetails& details) {
-  // Remove bookmarks once all browsing data was removed.
+  // Remove bookmarks and Reading List entriesonce all browsing data was
+  // removed.
   // Removal of browsing data waits for the bookmark model to be loaded, so
   // there should be no issue calling the function here.
   CHECK(RemoveAllUserBookmarksIOS(browser_state_))
       << "Failed to remove all user bookmarks.";
+  reading_list_remover_helper_ =
+      base::MakeUnique<reading_list::ReadingListRemoverHelper>(browser_state_);
+  reading_list_remover_helper_->RemoveAllUserReadingListItemsIOS(
+      base::Bind(&BrowserStateDataRemover::ReadingListCleaned,
+                 base::Unretained(this), details));
+}
+
+void BrowserStateDataRemover::ReadingListCleaned(
+    const IOSChromeBrowsingDataRemover::NotificationDetails& details,
+    bool reading_list_cleaned) {
+  CHECK(reading_list_cleaned)
+      << "Failed to remove all user reading list items.";
 
   if (details.removal_mask != kRemoveAllDataMask) {
     NOTREACHED() << "Unexpected partial remove browsing data request "
