91ba0fb7160bb37de7da3dd5b343db95e1b95b17
nzolghadr <nzolghadr@chromium.org>
2017-03-21 14:20:51 -0700

    Revert of Add use count for click retargeting due to capture
    Reason for revert:
    Reverting all the changes related to the click target in capturing as they are splitted across multiple commits.
    
    This reverts commit 18a3165ddb7326845a6028b1e3f93f7d347bde95.
    
    Original issue's description
    Since pointer capture changes the target of the
    following events for that pointer it potentially
    changes the target of the click. Considering the
    changed target for calculating the click target
    may cause some websites to break. So this CL
    adds a usecount for that case. Note this is an
    upper bound on the breakage as lots of pages
    may not have any listener for these click events
    or just not break due to this retargetting.
    
    BUG=699933
    
    TBR=dtapuska@chromium.org,mustaq@chromium.org,bokan@chromium.org
    
    Review-Url: https://codereview.chromium.org/2769433002
    Cr-Commit-Position: refs/heads/master@{#458550}

--- third_party/WebKit/Source/core/input/PointerEventManager.cpp
+++ third_party/WebKit/Source/core/input/PointerEventManager.cpp
@@ -170,7 +170,6 @@ WebInputEventResult PointerEventManager::dispatchPointerEvent(
     DCHECK(!m_dispatchingPointerId);
     AutoReset<int> dispatchHolder(&m_dispatchingPointerId, pointerId);
     DispatchEventResult dispatchResult = target->dispatchEvent(pointerEvent);
-
     return EventHandlingUtil::toWebInputEventResult(dispatchResult);
   }
   return WebInputEventResult::NotHandled;
@@ -508,7 +507,7 @@ WebInputEventResult PointerEventManager::sendMousePointerEvent(
         mouseEventType == EventTypeNames::mouseup) {
       WebInputEventResult clickEventResult =
           m_mouseEventManager->dispatchMouseClickIfNeeded(
-              mouseTarget->toNode(), mouseEvent, canvasRegionId, target);
+              mouseTarget->toNode(), mouseEvent, canvasRegionId);
       result = EventHandlingUtil::mergeEventResult(clickEventResult, result);
     }
   }
