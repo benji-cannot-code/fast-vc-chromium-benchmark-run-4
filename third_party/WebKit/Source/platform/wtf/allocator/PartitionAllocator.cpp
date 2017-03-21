6fe07cf14dc9579b5cab3991a5ffa439161bed79
yutak <yutak@chromium.org>
2017-03-21 02:50:08 -0700

    Move files in wtf/ to platform/wtf/ (Part 6).
    At this time the following files are moved to platform/wtf/:
    
        Allocator.h
        WTF.h (note: WTF.cpp remains in wtf/)
        allocator/PartitionAllocator.{h,cpp}
        allocator/Partitions.{h,cpp}
    
    WTF.cpp is not moved in this change, but this seems OK since "platform_wtf"
    target is defined as source_set. WTF.cpp has heavy dependencies and it's
    hard to move WTF.cpp now.
    
    BUG=691465
    
    Review-Url: https://codereview.chromium.org/2762943002
    Cr-Commit-Position: refs/heads/master@{#458361}

--- third_party/WebKit/Source/platform/wtf/allocator/PartitionAllocator.cpp
+++ third_party/WebKit/Source/platform/wtf/allocator/PartitionAllocator.cpp
@@ -3,10 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "wtf/allocator/PartitionAllocator.h"
+#include "platform/wtf/allocator/PartitionAllocator.h"
 
 #include "base/allocator/partition_allocator/partition_alloc.h"
-#include "wtf/allocator/Partitions.h"
+#include "platform/wtf/allocator/Partitions.h"
 
 namespace WTF {
 
--- third_party/WebKit/Source/platform/wtf/allocator/Partitions.cpp
+++ third_party/WebKit/Source/platform/wtf/allocator/Partitions.cpp
@@ -29,11 +29,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "wtf/allocator/Partitions.h"
+#include "platform/wtf/allocator/Partitions.h"
 
 #include "base/allocator/partition_allocator/page_allocator.h"
 #include "base/debug/alias.h"
-#include "wtf/allocator/PartitionAllocator.h"
+#include "platform/wtf/allocator/PartitionAllocator.h"
 
 namespace WTF {
 
