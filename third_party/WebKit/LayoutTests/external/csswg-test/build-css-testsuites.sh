ac15adde8751042016cbf56ace806cd9f7033494
Blink W3C Test Autoroller <blink-w3c-test-autoroller@chromium.org>
2017-03-21 09:06:23 -0700

    Import csswg-test@75f2d22ee75fe21aa01dbfad0ecf375910cff667
    Using wpt-import in Chromium 378b4e8a2660f9dfc74abdc2b04f3ffcbdf93671.
    
    Build: https://build.chromium.org/p/chromium.infra.cron/builders/w3c-test-autoroller/builds/8039
    
    TBR=qyearsley@chromium.org
    NOEXPORT=true
    
    Change-Id: If918ca2fffe61e5209c73d78bbb17e16f0f20703
    Reviewed-on: https://chromium-review.googlesource.com/457022
    Reviewed-by: Blink W3C Test Autoroller <blink-w3c-test-autoroller@chromium.org>
    Commit-Queue: Blink W3C Test Autoroller <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#458524}

--- third_party/WebKit/LayoutTests/external/csswg-test/build-css-testsuites.sh
+++ third_party/WebKit/LayoutTests/external/csswg-test/build-css-testsuites.sh
@@ -31,7 +31,7 @@ if [ ! -d $VENV ]; then
         exit 1
     fi
 
-    $PYTHON -m virtualenv $VENV || { echo "Please ensure virtualenv is installed"; exit 2; }
+    virtualenv -p $PYTHON $VENV || { echo "Please ensure virtualenv is installed"; exit 2; }
 fi
 
 # Install dependencies
