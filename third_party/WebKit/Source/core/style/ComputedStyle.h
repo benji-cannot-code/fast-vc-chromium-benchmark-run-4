bc55c9fcd2628daffa3f6d9fae89d047fc2cf1ca
wangxianzhu <wangxianzhu@chromium.org>
2017-03-20 22:00:51 -0700

    Add StyleDifference::needsVisualRectUpdate
    It's set when a style change will change visual rect (without changing
    layout, as visual rect update is implied by layout change).
    
    BTW optimized paint invalidation of box with resizer. We no longer
    force full paint invalidation for box with resizer when resized.
    The resizer will be invlaidated in PaintInvalidationCapableScrollableArea
    if needed.
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2761863002
    Cr-Commit-Position: refs/heads/master@{#458313}

--- third_party/WebKit/Source/core/style/ComputedStyle.h
+++ third_party/WebKit/Source/core/style/ComputedStyle.h
@@ -3692,6 +3692,7 @@ class CORE_EXPORT ComputedStyle : public ComputedStyleBase,
   bool diffNeedsPaintInvalidationObjectForPaintImage(
       const StyleImage*,
       const ComputedStyle& other) const;
+  bool diffNeedsVisualRectUpdate(const ComputedStyle& other) const;
   void updatePropertySpecificDifferences(const ComputedStyle& other,
                                          StyleDifference&) const;
 
