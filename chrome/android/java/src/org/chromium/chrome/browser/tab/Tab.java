af13374109c2ee93135fe385543222c9555749d5
carlosk <carlosk@chromium.org>
2017-03-21 13:21:02 -0700

    Adds metric to help assess last_n impact on tab restores.
    This change adds a new metric to help us assess the impact of last_n on
    the cases where an existing tab has to have its contents restored. This
    change only affects Chrome on Android.
    
    We track tab restores in Tab and call the appropriate, newly introduced
    methods in OfflinePageUtils where the reporting actually happens.
    
    BUG=688588
    
    Review-Url: https://codereview.chromium.org/2754103003
    Cr-Commit-Position: refs/heads/master@{#458530}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
@@ -896,7 +896,9 @@ public class Tab
     public void stopLoading() {
         if (isLoading()) {
             RewindableIterator<TabObserver> observers = getTabObservers();
-            while (observers.hasNext()) observers.next().onPageLoadFinished(this);
+            while (observers.hasNext()) {
+                observers.next().onPageLoadFinished(this);
+            }
         }
         if (getWebContents() != null) getWebContents().stop();
     }
@@ -1526,7 +1528,6 @@ public class Tab
      * Called when a page has finished loading.
      */
     protected void didFinishPageLoad() {
-        mIsBeingRestored = false;
         mIsTabStateDirty = true;
         updateTitle();
         updateFullscreenEnabledState();
@@ -1541,6 +1542,7 @@ public class Tab
         if (mTabUma != null) mTabUma.onPageLoadFinished();
 
         for (TabObserver observer : mObservers) observer.onPageLoadFinished(this);
+        mIsBeingRestored = false;
     }
 
     /**
@@ -1548,9 +1550,11 @@ public class Tab
      * @param errorCode The error code causing the page to fail loading.
      */
     protected void didFailPageLoad(int errorCode) {
-        mIsBeingRestored = false;
         if (mTabUma != null) mTabUma.onLoadFailed(errorCode);
-        for (TabObserver observer : mObservers) observer.onPageLoadFailed(this, errorCode);
+        for (TabObserver observer : mObservers) {
+            observer.onPageLoadFailed(this, errorCode);
+        }
+        mIsBeingRestored = false;
     }
 
     /**
@@ -2562,9 +2566,15 @@ public class Tab
      */
     void handleTabCrash() {
         mIsLoading = false;
-        mIsBeingRestored = false;
 
         if (mTabUma != null) mTabUma.onRendererCrashed();
+
+        boolean sadTabShown = isShowingSadTab();
+        RewindableIterator<TabObserver> observers = getTabObservers();
+        while (observers.hasNext()) {
+            observers.next().onCrash(this, sadTabShown);
+        }
+        mIsBeingRestored = false;
     }
 
     /**
