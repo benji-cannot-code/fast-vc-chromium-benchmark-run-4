9b16f39a988441931b7029ae2990f60e6f814879
gambard <gambard@chromium.org>
2017-03-21 07:49:02 -0700

    Update ContentSuggestions Articles UI
    BUG=694220
    
    Review-Url: https://codereview.chromium.org/2760283003
    Cr-Commit-Position: refs/heads/master@{#458409}

--- ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_article_item.mm
+++ ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_article_item.mm
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 const CGFloat kImageSize = 72;
 // When updating this, make sure to update |layoutSubviews|.
-const CGFloat kStandardSpacing = 8;
+const CGFloat kStandardSpacing = 16;
 }
 
 @interface ContentSuggestionsArticleItem ()
@@ -120,7 +120,7 @@ const CGFloat kStandardSpacing = 8;
     _publisherLabel = [[UILabel alloc] initWithFrame:CGRectZero];
 
     _titleLabel.numberOfLines = 2;
-    _subtitleLabel.numberOfLines = 0;
+    _subtitleLabel.numberOfLines = 2;
     [_subtitleLabel setContentHuggingPriority:UILayoutPriorityDefaultHigh
                                       forAxis:UILayoutConstraintAxisVertical];
     [_titleLabel setContentHuggingPriority:UILayoutPriorityDefaultHigh
@@ -188,6 +188,7 @@ const CGFloat kStandardSpacing = 8;
   [NSLayoutConstraint activateConstraints:@[
     [_imageView.widthAnchor constraintEqualToConstant:kImageSize],
     [_imageView.heightAnchor constraintEqualToAnchor:_imageView.widthAnchor],
+    [_imageView.topAnchor constraintEqualToAnchor:_titleLabel.topAnchor],
     [_publisherLabel.topAnchor
         constraintGreaterThanOrEqualToAnchor:_imageView.bottomAnchor
                                     constant:kStandardSpacing],
@@ -200,9 +201,8 @@ const CGFloat kStandardSpacing = 8;
       @[
         @"H:|-(space)-[title]-(space)-[image]-(space)-|",
         @"H:|-(space)-[text]-(space)-[image]",
-        @"V:|-[title]-[text]",
-        @"V:|-[image]",
-        @"H:|-[publish]-|",
+        @"V:|-(space)-[title]-[text]",
+        @"H:|-(space)-[publish]-(space)-|",
         @"V:[publish]-|",
       ],
       @{
