faeb0502fef8ed3c15d399361867964be9382f52
alexilin <alexilin@chromium.org>
2017-03-21 08:50:40 -0700

    predictors: Fix incorrect page load metrics histogram name.
    s/ParseTiming/PaintTiming
    histograms.xml contains correct (PaintTiming) name.
    
    BUG=680049
    
    Review-Url: https://codereview.chromium.org/2761223003
    Cr-Commit-Position: refs/heads/master@{#458426}

--- chrome/browser/page_load_metrics/observers/resource_prefetch_predictor_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/resource_prefetch_predictor_page_load_metrics_observer.cc
@@ -14,10 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace internal {
 
 const char kHistogramResourcePrefetchPredictorFirstContentfulPaint[] =
-    "PageLoad.Clients.ResourcePrefetchPredictor.ParseTiming."
+    "PageLoad.Clients.ResourcePrefetchPredictor.PaintTiming."
     "NavigationToFirstContentfulPaint.Prefetchable";
 const char kHistogramResourcePrefetchPredictorFirstMeaningfulPaint[] =
-    "PageLoad.Clients.ResourcePrefetchPredictor.ParseTiming."
+    "PageLoad.Clients.ResourcePrefetchPredictor.PaintTiming."
     "NavigationToFirstMeaningfulPaint.Prefetchable";
 
 }  // namespace internal
