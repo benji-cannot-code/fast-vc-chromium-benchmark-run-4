e47ad98d22f8a0049824df23ef1b31116a013a50
ricea <ricea@chromium.org>
2017-03-20 23:26:29 -0700

    Use SimpleQueue in WritableStream implementation
    Split out Queue into a separate file from ReadableStream and rename is
    SimpleQueue to reduce the risk of name collisions on the binding object.
    
    Add a peek() method as it is needed by WritableStream.
    
    Modify WritableStream to use SimpleQueue.
    
    BUG=681493
    
    Review-Url: https://codereview.chromium.org/2752133003
    Cr-Commit-Position: refs/heads/master@{#458324}

--- third_party/WebKit/Source/core/streams/WritableStream.js
+++ third_party/WebKit/Source/core/streams/WritableStream.js
@@ -142,11 +142,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     setDefaultControllerFlag(controller, FLAG_INCLOSE, value);
   }
 
-  function rejectPromises(array, e) {
-    // array is an InternalPackedArray so forEach won't work.
-    for (let i = 0; i < array.length; ++i) {
-      v8.rejectPromise(array[i], e);
-    }
+  function rejectPromises(queue, e) {
+    queue.forEach(promise => v8.rejectPromise(promise, e));
   }
 
   // https://tc39.github.io/ecma262/#sec-ispropertykey
@@ -179,7 +176,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       this[_pendingWriteRequest] = undefined;
       this[_pendingCloseRequest] = undefined;
       this[_pendingAbortRequest] = undefined;
-      this[_writeRequests] = new v8.InternalPackedArray();
+      this[_writeRequests] = new binding.SimpleQueue();
       const type = underlyingSink.type;
       if (type !== undefined) {
         throw new RangeError(streamErrors.invalidType);
@@ -332,7 +329,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     const storedError = stream[_storedError];
     rejectPromises(stream[_writeRequests], storedError);
-    stream[_writeRequests] = new v8.InternalPackedArray();
+    stream[_writeRequests] = new binding.SimpleQueue();
 
     if (stream[_pendingCloseRequest] !== undefined) {
       TEMP_ASSERT(
@@ -624,7 +621,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       }
       this[_controlledWritableStream] = stream;
       this[_underlyingSink] = underlyingSink;
-      this[_queue] = new v8.InternalPackedArray();
+      this[_queue] = new binding.SimpleQueue();
       this[_queueSize] = 0;
       this[_defaultControllerFlags] = 0;
       const normalizedStrategy =
@@ -666,7 +663,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   function WritableStreamDefaultControllerAbort(controller, reason) {
-    controller[_queue] = v8.InternalPackedArray();
+    controller[_queue] = new binding.SimpleQueue();
     controller[_queueSize] = 0;
     const sinkAbortPromise =
         PromiseInvokeOrNoop(controller[_underlyingSink], 'abort', [reason]);
@@ -806,7 +803,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     stream[_pendingWriteRequest] = stream[_writeRequests].shift();
 
     const sinkWritePromise = PromiseInvokeOrNoop(controller[_underlyingSink],
-                                               'write', [chunk, controller]);
+                                                 'write', [chunk, controller]);
     thenPromise(
         sinkWritePromise,
         () => {
@@ -882,15 +879,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     TEMP_ASSERT(state === WRITABLE || state === CLOSING,
                 'stream.[[state]] is "writable" or "closing".');
     WritableStreamError(stream, e);
-    controller[_queue] = new v8.InternalPackedArray();
+    controller[_queue] = new binding.SimpleQueue();
     controller[_queueSize] = 0;
   }
 
   // Queue-with-Sizes Operations
-  //
-  // These differ from the versions in the standard: they take a controller
-  // argument in order to cache the total queue size. This is necessary to avoid
-  // O(N^2) behaviour.
   //
   // TODO(ricea): Share these operations with ReadableStream.js.
   function DequeueValueForController(controller) {
@@ -918,7 +911,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   function PeekQueueValue(queue) {
     TEMP_ASSERT(queue.length !== 0,
                 'queue is not empty.');
-    return queue[0].value;
+    return queue.peek().value;
   }
 
   // Miscellaneous Operations
