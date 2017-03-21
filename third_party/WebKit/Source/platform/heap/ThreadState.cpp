ab59b96f765954fba970fe999e24dcb286484da6
sigbjornf <sigbjornf@opera.com>
2017-03-21 07:42:00 -0700

    Enable idle GC for worker thread heaps.
    Idle task processing is now supported on threads other than
    the main thread, hence go ahead and enable idle GCs for
    worker thread heaps.
    
    R=haraken
    BUG=702902
    
    Review-Url: https://codereview.chromium.org/2765843002
    Cr-Commit-Position: refs/heads/master@{#458405}

--- third_party/WebKit/Source/platform/heap/ThreadState.cpp
+++ third_party/WebKit/Source/platform/heap/ThreadState.cpp
@@ -571,7 +571,6 @@ ThreadState* ThreadState::fromObject(const void* object) {
 
 void ThreadState::performIdleGC(double deadlineSeconds) {
   ASSERT(checkThread());
-  ASSERT(isMainThread());
   ASSERT(Platform::current()->currentThread()->scheduler());
 
   if (gcState() != IdleGCScheduled)
@@ -604,7 +603,6 @@ void ThreadState::performIdleGC(double deadlineSeconds) {
 
 void ThreadState::performIdleLazySweep(double deadlineSeconds) {
   ASSERT(checkThread());
-  ASSERT(isMainThread());
 
   // If we are not in a sweeping phase, there is nothing to do here.
   if (!isSweepingInProgress())
@@ -647,10 +645,6 @@ void ThreadState::performIdleLazySweep(double deadlineSeconds) {
 }
 
 void ThreadState::scheduleIdleGC() {
-  // TODO(haraken): Idle GC should be supported in worker threads as well.
-  if (!isMainThread())
-    return;
-
   if (isSweepingInProgress()) {
     setGCState(SweepingAndIdleGCScheduled);
     return;
@@ -667,10 +661,6 @@ void ThreadState::scheduleIdleGC() {
 }
 
 void ThreadState::scheduleIdleLazySweep() {
-  // TODO(haraken): Idle complete sweep should be supported in worker threads.
-  if (!isMainThread())
-    return;
-
   // Some threads (e.g. PPAPI thread) don't have a scheduler.
   if (!Platform::current()->currentThread()->scheduler())
     return;
