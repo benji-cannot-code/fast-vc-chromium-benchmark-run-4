ee7785645b1a9f5f2dce7c6b2cc6985409417988
holte <holte@chromium.org>
2017-03-21 14:25:12 -0700

    Make UKM Observers ignore ChromeOS SigninProfile
    BUG=701220
    
    Review-Url: https://codereview.chromium.org/2759313002
    Cr-Commit-Position: refs/heads/master@{#458552}

--- chrome/browser/metrics/chrome_metrics_service_client.cc
+++ chrome/browser/metrics/chrome_metrics_service_client.cc
@@ -107,6 +107,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(OS_CHROMEOS)
+#include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/metrics/chromeos_metrics_provider.h"
 #include "chrome/browser/signin/signin_status_metrics_provider_chromeos.h"
 #endif
@@ -905,6 +906,11 @@ void ChromeMetricsServiceClient::RegisterForNotifications() {
 }
 
 void ChromeMetricsServiceClient::RegisterForProfileEvents(Profile* profile) {
+#if defined(OS_CHROMEOS)
+  // Ignore the signin profile for sync disables / history deletion.
+  if (chromeos::ProfileHelper::IsSigninProfile(profile))
+    return;
+#endif
   history::HistoryService* history_service =
       HistoryServiceFactory::GetForProfile(profile,
                                            ServiceAccessType::IMPLICIT_ACCESS);
