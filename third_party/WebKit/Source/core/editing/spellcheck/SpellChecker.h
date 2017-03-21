fb4897d9587d8d6c8c6bb71917c41a77d1b25d66
xiaochengh <xiaochengh@chromium.org>
2017-03-21 00:35:20 -0700

    Minor revision related to SpellChecker::advanceToNextMisspelling()
    This patch removes an unused default parameter value from the above
    function, and make it called directly in unit test
    SpellCheckerTest.AdvaneToNextMisspellingWithEmptyInputNoCrash.
    
    R=yosin@chromium.org
    BUG=n/a
    TEST=n/a; no behavior change
    
    Review-Url: https://codereview.chromium.org/2748083006
    Cr-Commit-Position: refs/heads/master@{#458343}

--- third_party/WebKit/Source/core/editing/spellcheck/SpellChecker.h
+++ third_party/WebKit/Source/core/editing/spellcheck/SpellChecker.h
@@ -66,7 +66,7 @@ class CORE_EXPORT SpellChecker final : public GarbageCollected<SpellChecker> {
   bool isSpellCheckingEnabledInFocusedNode() const;
   void markMisspellingsAfterApplyingCommand(const CompositeEditCommand&);
   void markAndReplaceFor(SpellCheckRequest*, const Vector<TextCheckingResult>&);
-  void advanceToNextMisspelling(bool startBeforeSelection = false);
+  void advanceToNextMisspelling(bool startBeforeSelection);
   void showSpellingGuessPanel();
   void didBeginEditing(Element*);
   void clearMisspellingsForMovingParagraphs(const SelectionInDOMTree&);
