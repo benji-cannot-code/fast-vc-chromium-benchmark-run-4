a716f4be676f1df6f05c64a1642df21cd022526d
xingliu <xingliu@chromium.org>
2017-03-21 12:47:37 -0700

    Propagate server response error and interrupt the download.
    This CL simply propagate anything to DownloadItemImpl, later we should
    revisit it.
    
    We need to wait for a couple of recent CL landed to start to test the
    whole flow of parallel download.
    
    BUG=644352
    
    Review-Url: https://codereview.chromium.org/2752603002
    Cr-Commit-Position: refs/heads/master@{#458519}

--- content/browser/download/download_worker.h
+++ content/browser/download/download_worker.h
@@ -32,6 +32,9 @@ class DownloadWorker : public UrlDownloader::Delegate {
     virtual void OnByteStreamReady(
         DownloadWorker* worker,
         std::unique_ptr<ByteStreamReader> stream_reader) = 0;
+    // Called when there is an error caused by the server response.
+    virtual void OnServerResponseError(DownloadWorker* worker,
+                                       DownloadInterruptReason reason) = 0;
   };
 
   DownloadWorker(DownloadWorker::Delegate* delegate,
@@ -62,7 +65,7 @@ class DownloadWorker : public UrlDownloader::Delegate {
       std::unique_ptr<UrlDownloader, BrowserThread::DeleteOnIOThread>
           downloader);
 
-  DownloadWorker::Delegate* delegate_;
+  DownloadWorker::Delegate* const delegate_;
 
   // The starting position of the content for this worker to download.
   int64_t offset_;
