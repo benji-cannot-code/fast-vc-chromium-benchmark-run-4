19c10d271416f13e92c75371d60ee10c62519b09
xingliu <xingliu@chromium.org>
2017-03-21 11:18:40 -0700

    Pump the http version to download system.
    We need to know the http version to determine if we can use parallel
    download. So pump the http version from url request.
    
    BUG=644352
    
    Review-Url: https://codereview.chromium.org/2759843002
    Cr-Commit-Position: refs/heads/master@{#458478}

--- content/browser/download/download_create_info.h
+++ content/browser/download/download_create_info.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/content_export.h"
 #include "content/public/browser/download_interrupt_reasons.h"
 #include "content/public/browser/download_save_info.h"
+#include "net/http/http_response_info.h"
 #include "net/log/net_log_with_source.h"
 #include "ui/base/page_transition_types.h"
 #include "url/gurl.h"
@@ -120,6 +121,9 @@ struct CONTENT_EXPORT DownloadCreateInfo {
 
   // If "Accept-Ranges:bytes" header presents in the response header.
   bool accept_range;
+
+  // The HTTP connection type.
+  net::HttpResponseInfo::ConnectionInfo connection_info;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(DownloadCreateInfo);
