da11910f705cece92aa8974112602ed6120ceb14
dgozman <dgozman@chromium.org>
2017-03-22 00:23:12 -0700

    [DevTools] Rework Popover API
    - Client now provides a method which resolves event to UI.PopoverRequest.
    - We can be sure that show/hide methods are called in pairs.
    - We can now scope additional work and local variables instead of passing them strangely through the AnchorBox.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2747553002
    Cr-Commit-Position: refs/heads/master@{#458663}

--- third_party/WebKit/Source/devtools/front_end/profiler/HeapSnapshotGridNodes.js
+++ third_party/WebKit/Source/devtools/front_end/profiler/HeapSnapshotGridNodes.js
@@ -134,10 +134,10 @@ Profiler.HeapSnapshotGridNode = class extends DataGrid.DataGridNode {
 
   /**
    * @param {!SDK.Target} target
-   * @param {function(!SDK.RemoteObject)} callback
    * @param {string} objectGroupName
+   * @return {!Promise<!SDK.RemoteObject>}
    */
-  queryObjectContent(target, callback, objectGroupName) {
+  queryObjectContent(target, objectGroupName) {
   }
 
   /**
@@ -606,15 +606,18 @@ Profiler.HeapSnapshotGenericObjectNode = class extends Profiler.HeapSnapshotGrid
   /**
    * @override
    * @param {!SDK.Target} target
-   * @param {function(!SDK.RemoteObject)} callback
    * @param {string} objectGroupName
+   * @return {!Promise<!SDK.RemoteObject>}
    */
-  queryObjectContent(target, callback, objectGroupName) {
+  queryObjectContent(target, objectGroupName) {
+    var fulfill;
+    var promise = new Promise(x => fulfill = x);
+
     /**
      * @param {?SDK.RemoteObject} object
      */
     function onResult(object) {
-      callback(
+      fulfill(
           object ||
           target.runtimeModel.createRemoteObjectFromPrimitiveValue(Common.UIString('Preview is not available')));
     }
@@ -626,6 +629,7 @@ Profiler.HeapSnapshotGenericObjectNode = class extends Profiler.HeapSnapshotGrid
       onResult(null);
     else
       heapProfilerModel.objectForSnapshotObjectId(String(this.snapshotNodeId), objectGroupName).then(onResult);
+    return promise;
   }
 
   updateHasChildren() {
--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineUIUtils.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineUIUtils.js
@@ -695,7 +695,7 @@ Timeline.TimelineUIUtils = class {
       var url = TimelineModel.TimelineData.forEvent(event).url;
       event[Timeline.TimelineUIUtils._previewElementSymbol] = await new Promise(fulfill => {
         if (url)
-          Components.DOMPresentationUtils.buildImagePreviewContents(target, url, false, fulfill);
+          Components.DOMPresentationUtils.buildImagePreviewContents(target, url, false).then(fulfill);
         else if (TimelineModel.TimelineData.forEvent(event).picture)
           Timeline.TimelineUIUtils.buildPicturePreviewContent(event, target, fulfill);
         else
@@ -1132,13 +1132,15 @@ Timeline.TimelineUIUtils = class {
      * @param {function(?Element)} fulfill
      */
     function action(fulfill) {
-      Components.DOMPresentationUtils.buildImagePreviewContents(
-          /** @type {!SDK.Target} */ (target), request.url, false, saveImage);
+      Components.DOMPresentationUtils
+          .buildImagePreviewContents(
+              /** @type {!SDK.Target} */ (target), request.url, false)
+          .then(saveImage);
       /**
-       * @param {!Element=} element
+       * @param {?Element} element
        */
       function saveImage(element) {
-        request.previewElement = element || null;
+        request.previewElement = element;
         fulfill(request.previewElement);
       }
     }
