36e9f22f0481d152ac613dd36378c326d0b97a9a
jkarlin <jkarlin@chromium.org>
2017-03-22 09:08:59 -0700

    This change forces states called by DoLoop to set the next state before returning. The real difference is that before there was a default next_state_ = STATE_NONE, and now the state must set that itself, which improves readability and ensures that the state machine isn't escaping unexpectedly.
    BUG=703923
    
    Review-Url: https://codereview.chromium.org/2766953002
    Cr-Commit-Position: refs/heads/master@{#458763}

--- net/http/http_cache_transaction.h
+++ net/http/http_cache_transaction.h
@@ -180,6 +180,8 @@ class HttpCache::Transaction : public HttpTransaction {
   };
 
   enum State {
+    STATE_UNSET,
+
     // Normally, states are traversed in approximately this order.
     STATE_NONE,
     STATE_GET_BACKEND,
