3c2f5a0f2a0e177b98fdb35e77d5a7feba455ad0
einbinder <einbinder@chromium.org>
2017-03-22 15:11:55 -0700

    DevTools: Don't give up autocomplete after a fancy character in a string
    BUG=469473
    
    Review-Url: https://codereview.chromium.org/2760583002
    Cr-Commit-Position: refs/heads/master@{#458892}

--- third_party/WebKit/Source/devtools/front_end/ui/TextEditor.js
+++ third_party/WebKit/Source/devtools/front_end/ui/TextEditor.js
@@ -71,7 +71,14 @@ UI.TextEditor.prototype = {
    */
   configureAutocomplete(config) {},
 
-  clearAutocomplete() {}
+  clearAutocomplete() {},
+
+  /**
+   * @param {number} lineNumber
+   * @param {number} columnNumber
+   * @return {?{startColumn: number, endColumn: number, type: string}}
+   */
+  tokenAtTextPosition(lineNumber, columnNumber) {}
 };
 
 /**
@@ -89,7 +96,7 @@ UI.TextEditor.Options;
 /**
  * @typedef {{
  *     substituteRangeCallback: ((function(number, number):?Common.TextRange)|undefined),
- *     suggestionsCallback: ((function(!Common.TextRange, !Common.TextRange, boolean=, string=):?Promise.<!UI.SuggestBox.Suggestions>)|undefined),
+ *     suggestionsCallback: ((function(!Common.TextRange, !Common.TextRange, boolean=):?Promise.<!UI.SuggestBox.Suggestions>)|undefined),
  *     isWordChar: ((function(string):boolean)|undefined),
  *     captureEnter: (boolean|undefined)
  * }}
