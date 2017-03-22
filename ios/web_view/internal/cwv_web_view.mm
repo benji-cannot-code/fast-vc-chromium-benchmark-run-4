040f5e349caabceaf762525b82aea83f833118a8
ichikawa <ichikawa@chromium.org>
2017-03-22 16:18:16 -0700

    Delete deprecated -[CWVWebView view].
    BUG=703981
    
    Review-Url: https://codereview.chromium.org/2762983004
    Cr-Commit-Position: refs/heads/master@{#458923}

--- ios/web_view/internal/cwv_web_view.mm
+++ ios/web_view/internal/cwv_web_view.mm
@@ -102,10 +102,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self addSubview:subview];
 }
 
-- (UIView*)view {
-  return _webState->GetView();
-}
-
 - (BOOL)canGoBack {
   return _webState && _webState->GetNavigationManager()->CanGoBack();
 }
--- ios/web_view/public/cwv_web_view.h
+++ ios/web_view/public/cwv_web_view.h
@@ -21,9 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Concrete instances can be created through CWV.
 @interface CWVWebView : UIView
 
-// The view used to display web content.
-@property(nonatomic, readonly) UIView* view;
-
 // This web view's navigation delegate.
 @property(nonatomic, weak) id<CWVNavigationDelegate> navigationDelegate;
 
