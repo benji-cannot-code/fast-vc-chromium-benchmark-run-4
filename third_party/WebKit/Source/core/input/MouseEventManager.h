031d1950a0a41ce408805791ef8ca1f15cf2392f
nzolghadr <nzolghadr@chromium.org>
2017-03-22 07:29:53 -0700

    Revert "Send click event after pointer capturing retarget"
    Reason for revert:
    Reverting all the changes related to the click target in capturing as they are splitted across multiple commits.
    
    This reverts commit 14cea387ce9359e5f9fe3e90c996adf75e092e64.
    
    BUG=689158
    
    TBR=dtapuska@chromium.org,mustaq@chromium.org,bokan@chromium.org
    
    Review-Url: https://codereview.chromium.org/2771463002
    Cr-Commit-Position: refs/heads/master@{#458743}

--- third_party/WebKit/Source/core/input/MouseEventManager.h
+++ third_party/WebKit/Source/core/input/MouseEventManager.h
@@ -57,9 +57,8 @@ class CORE_EXPORT MouseEventManager final
       const AtomicString& eventType,
       const WebMouseEvent&);
 
-  WebInputEventResult dispatchMouseClickIfNeeded(Node* target,
-                                                 const WebMouseEvent&,
-                                                 const String& canvasRegionId);
+  WebInputEventResult dispatchMouseClickIfNeeded(
+      const MouseEventWithHitTestResults&);
 
   WebInputEventResult dispatchDragSrcEvent(const AtomicString& eventType,
                                            const WebMouseEvent&);
