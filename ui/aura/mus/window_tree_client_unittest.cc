3504814ce2bed97dbaee272e6fe1ede49289b314
fsamuel <fsamuel@chromium.org>
2017-03-22 03:00:25 -0700

    mus-ws: Plumb FrameSinkId to Children
    This CL takes us a step closer to surface synchronization by plumbing the
    correct FrameSinkId to children and delaying CompositorFrameSink creation
    until the FrameSinkId is propagated to the child.
    
    FrameSinkIds are plumbed to children via:
    
    1. WindowTreeClient::OnTopLevelCreated
      -- for top level windows like Chrome
    2. WindowTreeClient::OnEmbed
      -- for embedded apps and OOPIFs
    3. WindowManager::WmNewDisplayAdded
      -- for the window manager's CompositorFrameSink
    
    Requesting a CompositorFrameSink from WindowPortMus is now async,
    and completes when the FrameSinkId is available.
    
    BUG=672962
    TBR=piman@chromium.org
    
    Review-Url: https://codereview.chromium.org/2764433003
    Cr-Commit-Position: refs/heads/master@{#458703}

--- ui/aura/mus/window_tree_client_unittest.cc
+++ ui/aura/mus/window_tree_client_unittest.cc
@@ -575,7 +575,8 @@ class InputEventBasicTestEventHandler : public ui::test::TestEventHandler {
 
 TEST_F(WindowTreeClientClientTest, InputEventBasic) {
   InputEventBasicTestWindowDelegate window_delegate(window_tree());
-  WindowTreeHostMus window_tree_host(window_tree_client_impl());
+  WindowTreeHostMus window_tree_host(window_tree_client_impl(),
+                                     cc::FrameSinkId(1, 1));
   Window* top_level = window_tree_host.window();
   const gfx::Rect bounds(0, 0, 100, 100);
   window_tree_host.SetBoundsInPixels(bounds);
@@ -608,7 +609,8 @@ TEST_F(WindowTreeClientClientTest, InputEventBasic) {
 }
 
 TEST_F(WindowTreeClientClientTest, InputEventFindTargetAndConversion) {
-  WindowTreeHostMus window_tree_host(window_tree_client_impl());
+  WindowTreeHostMus window_tree_host(window_tree_client_impl(),
+                                     cc::FrameSinkId(1, 1));
   Window* top_level = window_tree_host.window();
   const gfx::Rect bounds(0, 0, 100, 100);
   window_tree_host.SetBoundsInPixels(bounds);
@@ -1082,8 +1084,8 @@ TEST_F(WindowTreeClientClientTest, NewTopLevelWindow) {
   ui::mojom::WindowDataPtr data = ui::mojom::WindowData::New();
   data->window_id = server_id(top_level);
   const int64_t display_id = 1;
-  window_tree_client()->OnTopLevelCreated(change_id, std::move(data),
-                                          display_id, false);
+  window_tree_client()->OnTopLevelCreated(
+      change_id, std::move(data), display_id, false, cc::FrameSinkId(1, 1));
 
   EXPECT_FALSE(window_tree_host->window()->TargetVisibility());
 
@@ -1122,8 +1124,8 @@ TEST_F(WindowTreeClientClientTest, NewTopLevelWindowGetsPropertiesFromData) {
   uint32_t change_id;
   ASSERT_TRUE(window_tree()->GetAndRemoveFirstChangeOfType(
       WindowTreeChangeType::NEW_TOP_LEVEL, &change_id));
-  window_tree_client()->OnTopLevelCreated(change_id, std::move(data),
-                                          display_id, true);
+  window_tree_client()->OnTopLevelCreated(
+      change_id, std::move(data), display_id, true, cc::FrameSinkId(1, 1));
   EXPECT_EQ(
       0u, window_tree()->GetChangeCountForType(WindowTreeChangeType::VISIBLE));
 
@@ -1174,7 +1176,8 @@ TEST_F(WindowTreeClientClientTest, NewWindowGetsAllChangesInFlight) {
   ASSERT_TRUE(window_tree()->GetAndRemoveFirstChangeOfType(
       WindowTreeChangeType::NEW_TOP_LEVEL, &new_window_in_flight_change_id));
   window_tree_client()->OnTopLevelCreated(new_window_in_flight_change_id,
-                                          std::move(data), display_id, true);
+                                          std::move(data), display_id, true,
+                                          cc::FrameSinkId(1, 1));
 
   // The only value that should take effect is the property for 'yy' as it was
   // not in flight.
@@ -1396,8 +1399,8 @@ TEST_F(WindowTreeClientClientTest,
       WindowTreeChangeType::NEW_TOP_LEVEL, &change_id));
 
   const int64_t display_id = 1;
-  window_tree_client()->OnTopLevelCreated(change_id, std::move(data),
-                                          display_id, true);
+  window_tree_client()->OnTopLevelCreated(
+      change_id, std::move(data), display_id, true, cc::FrameSinkId(1, 1));
   EXPECT_EQ(initial_root_count, window_tree_client_impl()->GetRoots().size());
 }
 
@@ -1414,7 +1417,7 @@ TEST_F(WindowTreeClientClientTest, NewTopLevelWindowGetsProperties) {
       mojo::ConvertTo<std::vector<uint8_t>>(kUnknownPropertyValue);
   std::unique_ptr<WindowTreeHostMus> window_tree_host =
       base::MakeUnique<WindowTreeHostMus>(window_tree_client_impl(),
-                                          &properties);
+                                          cc::FrameSinkId(1, 1), &properties);
   window_tree_host->InitHost();
   window_tree_host->window()->Show();
   // Verify the property made it to the window.
@@ -1701,8 +1704,8 @@ TEST_F(WindowTreeClientClientTest, TwoWindowTreesRequestCapture) {
   data->window_id = server_id(top_level);
   data->visible = true;
   const int64_t display_id = 1;
-  window_tree_client()->OnTopLevelCreated(change_id, std::move(data),
-                                          display_id, true);
+  window_tree_client()->OnTopLevelCreated(
+      change_id, std::move(data), display_id, true, cc::FrameSinkId(1, 1));
   EXPECT_EQ(
       0u, window_tree()->GetChangeCountForType(WindowTreeChangeType::VISIBLE));
   EXPECT_TRUE(top_level->TargetVisibility());
@@ -1910,8 +1913,8 @@ TEST_F(WindowTreeClientClientTestHighDPI, NewTopLevelWindowBounds) {
   uint32_t change_id;
   ASSERT_TRUE(window_tree()->GetAndRemoveFirstChangeOfType(
       WindowTreeChangeType::NEW_TOP_LEVEL, &change_id));
-  window_tree_client()->OnTopLevelCreated(change_id, std::move(data),
-                                          display_id, true);
+  window_tree_client()->OnTopLevelCreated(
+      change_id, std::move(data), display_id, true, cc::FrameSinkId(1, 1));
 
   // aura::Window should operate in DIP and aura::WindowTreeHost should operate
   // in pixels.
