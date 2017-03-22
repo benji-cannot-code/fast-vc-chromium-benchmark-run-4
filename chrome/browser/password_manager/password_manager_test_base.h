8ba001fe23d26728244fcc4fcf0804361aec8b64
vasilii <vasilii@chromium.org>
2017-03-22 04:44:31 -0700

    Enable CredentialManagerBrowserTest.ObsoleteHttpCredentialMovedOnMigrationToHstsSite
    The CL does
    - introduce a new observer for the password UI state,
    - uses it for ObsoleteHttpCredentialMovedOnMigrationToHstsSite,
    - fixes all other tests broken by PasswordManagerBrowserTestBase changes.
    
    BUG=703305
    
    Review-Url: https://codereview.chromium.org/2759353004
    Cr-Commit-Position: refs/heads/master@{#458721}

--- chrome/browser/password_manager/password_manager_test_base.h
+++ chrome/browser/password_manager/password_manager_test_base.h
@@ -9,10 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
+#include "base/run_loop.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "components/password_manager/core/browser/password_store_consumer.h"
 #include "content/public/browser/web_contents_observer.h"
-#include "content/public/test/test_utils.h"
 #include "net/cert/mock_cert_verifier.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 
@@ -54,7 +54,7 @@ class NavigationObserver : public content::WebContentsObserver {
   std::string wait_for_path_;
   content::RenderFrameHost* render_frame_host_;
   bool quit_on_entry_committed_;
-  scoped_refptr<content::MessageLoopRunner> message_loop_runner_;
+  base::RunLoop run_loop_;
 
   DISALLOW_COPY_AND_ASSIGN(NavigationObserver);
 };
@@ -83,6 +83,11 @@ class BubbleObserver {
   // Expecting that the prompt is shown, update |form| with the password from
   // observed form. Checks that the prompt is no longer visible afterwards.
   void AcceptUpdatePrompt(const autofill::PasswordForm& form) const;
+
+  // Returns once the account chooser pops up or it's already shown.
+  // |web_contents| must be the custom one returned by
+  // PasswordManagerBrowserTestBase.
+  void WaitForAccountChooser() const;
 
  private:
   ManagePasswordsUIController* const passwords_ui_controller_;
@@ -144,6 +149,7 @@ class PasswordManagerBrowserTestBase : public InProcessBrowserTest {
   void AddHSTSHost(const std::string& host);
 
   // Accessors
+  // Return the first created tab with a custom ManagePasswordsUIController.
   content::WebContents* WebContents();
   content::RenderViewHost* RenderViewHost();
   net::EmbeddedTestServer& https_test_server() { return https_test_server_; }
@@ -152,6 +158,8 @@ class PasswordManagerBrowserTestBase : public InProcessBrowserTest {
  private:
   net::EmbeddedTestServer https_test_server_;
   net::MockCertVerifier mock_cert_verifier_;
+  // A tab with some hooks injected.
+  content::WebContents* web_contents_;
   DISALLOW_COPY_AND_ASSIGN(PasswordManagerBrowserTestBase);
 };
 
