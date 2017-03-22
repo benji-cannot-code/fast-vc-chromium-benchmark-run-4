cb9c30fd3e9f380e573b0e5f77167dbd7527f0ae
ntfschr <ntfschr@chromium.org>
2017-03-22 12:21:16 -0700

    WebView: fix proceeding through malicious subresource
    Proceeding through malicious subresource was broken by crrev/447665.
    This CL:
     * reverts the changes from crrev/447665 to fix the "Proceed" case.
     * properly fixes the "back to safety" case.
    
    For WebView, we want to always treat "back to safety" as cancelling
    the resource, since we create a network error for that specific
    resource.
    
    This also changes color names in the WebView-side tests for
    readability.
    
    BUG=698995
    
    Review-Url: https://codereview.chromium.org/2762033002
    Cr-Commit-Position: refs/heads/master@{#458833}

--- components/safe_browsing/base_blocking_page.h
+++ components/safe_browsing/base_blocking_page.h
@@ -33,7 +33,7 @@ class BaseBlockingPage
   ~BaseBlockingPage() override;
 
   static const SafeBrowsingErrorUI::SBErrorDisplayOptions
-  CreateDefaultDisplayOptions();
+  CreateDefaultDisplayOptions(const UnsafeResourceList& unsafe_resources);
 
   // Shows a blocking page warning the user about phishing/malware for a
   // specific resource.
