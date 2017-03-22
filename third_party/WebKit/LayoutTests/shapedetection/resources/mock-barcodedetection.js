5d6d5dd4422f9685ca3a7ed5331e3ca2150ed45c
junwei.fu <junwei.fu@intel.com>
2017-03-22 00:09:29 -0700

    RELAND: ShapeDetection: use mojom::Bitmap for mojo interface.
    Original CL got reverted due to shapedetection/detection-on-worker.html
    fail to fetch source for module skia/public/interfaces/bitmap.mojom.
    
    This CL add gen/skia/public/interfaces to generated bindings paths.
    
    Original CL description ------------------------------------------------
    ShapeDetection: use mojom::Bitmap for mojo interface.
    
    This CL uses mojo::Bitmap for mojo ShapeDetection interfaces definition,
    so that the Detect API is completely flexible and friendly.
    
    BUG=665488
    TEST(Layout)=
     third_party/WebKit/LayoutTests/fast/shapedetection/shapedetection-creation.html
     third_party/WebKit/LayoutTests/fast/shapedetection/shapedetection-empty-input.html
     third_party/WebKit/LayoutTests/shapedetection/detection-HTMLCanvasElement.html
     third_party/WebKit/LayoutTests/shapedetection/detection-HTMLImageElement.html
     third_party/WebKit/LayoutTests/shapedetection/detection-HTMLVideoElement.html
     third_party/WebKit/LayoutTests/shapedetection/detection-ImageBitmap.html
     third_party/WebKit/LayoutTests/shapedetection/detection-ImageData.html
    
    Review-Url: https://codereview.chromium.org/2629433003
    Cr-Commit-Position: refs/heads/master@{#446142}
    (cherry picked from commit 33adf4c85dd553eb607bcf9c02c5fa60c8738a68)
    
    Conflicts:
    	third_party/WebKit/Source/modules/shapedetection/FaceDetector.cpp
    	third_party/WebKit/Source/modules/shapedetection/TextDetector.cpp
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.mac:mac_optional_gpu_tests_rel
    
    TBR=rockot@chromium.org, dtrainor@chromium.org, haraken@chromium.org, bsalomon@google.com, tsepez@chromium.org
    since the gist of the CL hasn't changed.
    
    Review-Url: https://codereview.chromium.org/2681913003
    Cr-Original-Original-Commit-Position: refs/heads/master@{#450582}
    Review-Url: https://codereview.chromium.org/2753413002
    Cr-Original-Commit-Position: refs/heads/master@{#458010}
    Review-Url: https://codereview.chromium.org/2765723002
    Cr-Commit-Position: refs/heads/master@{#458657}

--- third_party/WebKit/LayoutTests/shapedetection/resources/mock-barcodedetection.js
+++ third_party/WebKit/LayoutTests/shapedetection/resources/mock-barcodedetection.js
@@ -19,8 +19,8 @@ let mockBarcodeDetectionReady = define(
           handle => this.bindingSet_.addBinding(this, handle));
     }
 
-    detect(frame_data, width, height) {
-      let receivedStruct = mojo.mapBuffer(frame_data, 0, width*height*4, 0);
+    detect(bitmap_data) {
+      let receivedStruct = new Uint8Array(bitmap_data.pixel_data);
       this.buffer_data_ = new Uint32Array(receivedStruct.buffer);
       return Promise.resolve({
         results: [
@@ -46,7 +46,6 @@ let mockBarcodeDetectionReady = define(
           },
         ],
       });
-      mojo.unmapBuffer(receivedStruct.buffer);
     }
 
     getFrameData() {
