0317219eec605ccb920045a8e624781e7e9b20d2
rockot <rockot@chromium.org>
2017-03-21 21:34:05 -0700

    Make WebContentsBindingSet override API accessible from embedders
    As an oversight, this testing API was introduced in a way that only
    content internal code can use. This shuffles things around a bit
    to make access possible from embedder code.
    
    BUG=577685
    R=jam@chromium.org
    
    Review-Url: https://codereview.chromium.org/2771433002
    Cr-Commit-Position: refs/heads/master@{#458626}

--- content/public/browser/web_contents_binding_set.h
+++ content/public/browser/web_contents_binding_set.h
@@ -40,6 +40,11 @@ class CONTENT_EXPORT WebContentsBindingSet {
     binder_for_testing_ = std::move(binder);
   }
 
+  template <typename Interface>
+  static WebContentsBindingSet* GetForWebContents(WebContents* web_contents) {
+    return GetForWebContents(web_contents, Interface::Name_);
+  }
+
  protected:
   WebContentsBindingSet(WebContents* web_contents,
                         const std::string& interface_name,
@@ -48,6 +53,9 @@ class CONTENT_EXPORT WebContentsBindingSet {
 
  private:
   friend class WebContentsImpl;
+
+  static WebContentsBindingSet* GetForWebContents(WebContents* web_contents,
+                                                  const char* interface_name);
 
   void CloseAllBindings();
   void OnRequestForFrame(RenderFrameHost* render_frame_host,
