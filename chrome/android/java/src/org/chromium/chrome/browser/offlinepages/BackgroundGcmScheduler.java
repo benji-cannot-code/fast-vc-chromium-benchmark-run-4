1bd405a07b1c3fc1decc1da9f88fb5977245f0ee
fgorski <fgorski@chromium.org>
2017-03-23 12:23:48 -0700

    [Offline pages] Ensuring tasks scheduled through GCM NM are persisted
    We are currently not persisting tasks scheduled through GCM Network
    Manager, which means requests to offline pages in the background will
    not be resumed after a device restart.
    
    This patch fixes the issue.
    
    BUG=704610
    
    Review-Url: https://codereview.chromium.org/2769233003
    Cr-Commit-Position: refs/heads/master@{#459174}

--- chrome/android/java/src/org/chromium/chrome/browser/offlinepages/BackgroundGcmScheduler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/offlinepages/BackgroundGcmScheduler.java
@@ -51,6 +51,7 @@ public class BackgroundGcmScheduler extends BackgroundScheduler {
                                             ? Task.NETWORK_STATE_UNMETERED
                                             : Task.NETWORK_STATE_CONNECTED)
                             .setRequiresCharging(triggerConditions.requirePowerConnected())
+                            .setPersisted(true)
                             .setExtras(taskExtras)
                             .build();
 
