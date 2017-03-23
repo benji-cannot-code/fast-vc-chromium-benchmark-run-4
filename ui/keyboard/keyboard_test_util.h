bb3b08937475f16cc5a05c722ae867e89066a1a6
oka <oka@chromium.org>
2017-03-22 21:11:01 -0700

    Add an end to end test: virtual keyboard opens on click of text input
    BUG=703488
    TEST=try
    
    Review-Url: https://codereview.chromium.org/2702933002
    Cr-Commit-Position: refs/heads/master@{#458999}

---
+++ ui/keyboard/keyboard_test_util.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_KEYBOARD_KEYBOARD_TEST_UTIL_
+#define UI_KEYBOARD_KEYBOARD_TEST_UTIL_
+
+namespace keyboard {
+
+// Waits until the keyboard is shown. Return false if there is no keyboard
+// window created.
+bool WaitUntilShown();
+
+// Waits until the keyboard is hidden. Return false if there is no keyboard
+// window created.
+bool WaitUntilHidden();
+
+}  // namespace keyboard
+
+#endif  // UI_KEYBOARD_KEYBOARD_TEST_UTIL_
