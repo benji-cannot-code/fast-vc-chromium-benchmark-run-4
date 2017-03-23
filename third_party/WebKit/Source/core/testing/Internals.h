d8e409c9d27b0bb156d9f35ba269716be5f26782
esprehn <esprehn@chromium.org>
2017-03-23 00:53:03 -0700

    Switch Shadow DOM V0 <content> FeatureSet tests to SimTest.
    This lets us remove three methods from internals. Once we have this new SimTest
    we can also port lots of other tests and remove more internals methods specific
    to testing shadow dom.
    
    BUG=682982
    
    Review-Url: https://codereview.chromium.org/2647843002
    Cr-Commit-Position: refs/heads/master@{#459018}

--- third_party/WebKit/Source/core/testing/Internals.h
+++ third_party/WebKit/Source/core/testing/Internals.h
@@ -137,15 +137,6 @@ class Internals final : public GarbageCollected<Internals>,
   bool isValidContentSelect(Element* insertionPoint, ExceptionState&);
   Node* treeScopeRootNode(Node*);
   Node* parentTreeScope(Node*);
-  bool hasSelectorForIdInShadow(Element* host,
-                                const AtomicString& idValue,
-                                ExceptionState&);
-  bool hasSelectorForClassInShadow(Element* host,
-                                   const AtomicString& className,
-                                   ExceptionState&);
-  bool hasSelectorForAttributeInShadow(Element* host,
-                                       const AtomicString& attributeName,
-                                       ExceptionState&);
   unsigned short compareTreeScopePosition(const Node*,
                                           const Node*,
                                           ExceptionState&) const;
