a1a7aa6636b8433b82eca3e17d18b81d075d9caf
dgozman <dgozman@chromium.org>
2017-03-22 17:35:34 -0700

    Reland of Simplify ExtensionInstallChecker into a single-use class (patchset #1 id:1 of https://codereview.chromium.org/2769813004/ )
    Reason for revert:
    The issue fixed itself without a revert.
    
    Original issue's description:
    > Revert of Simplify ExtensionInstallChecker into a single-use class (patchset #7 id:120001 of https://codereview.chromium.org/2751013002/ )
    >
    > Reason for revert:
    > Speculative revert for ServiceWorkerTest.ServiceWorkerSuspensionOnExtensionUnload failure on the following bot:
    > https://uberchromegw.corp.google.com/i/chromium.win/builders/Win7%20Tests%20%28dbg%29%281%29/builds/58372
    >
    > Original issue's description:
    > > Simplify ExtensionInstallChecker into a single-use class
    > >
    > > ExtensionInstallChecker does asynchronous checks, and its set of checks
    > > can be started more than once. This functionality isn't being used and
    > > makes the class more complicated. Remove
    > > ExtensionInstallChecker::ResetResults().
    > >
    > > It's also unclear why the users of this class (CrxInstaller and
    > > UnpackedInstaller) are intentionally storing their Profile and Extension
    > > pointers in ExtensionInstallChecker class instead of remembering them
    > > locally, so update that and remove
    > > ExtensionInstallChecker::set_extension().
    > >
    > > Lastly, allocate the ExtensionInstallChecker dynamically since we don't
    > > always use it.
    > >
    > > BUG=none
    > > R=rdevlin.cronin@chromium.org
    > >
    > > Review-Url: https://codereview.chromium.org/2751013002
    > > Cr-Commit-Position: refs/heads/master@{#458883}
    > > Committed: https://chromium.googlesource.com/chromium/src/+/b24adc5bcbfde7fd67c6b23c3b0721610c77f63b
    >
    > TBR=rdevlin.cronin@chromium.org,michaelpg@chromium.org
    > # Skipping CQ checks because original CL landed less than 1 days ago.
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=none
    >
    > Review-Url: https://codereview.chromium.org/2769813004
    > Cr-Commit-Position: refs/heads/master@{#458932}
    > Committed: https://chromium.googlesource.com/chromium/src/+/9cc4c29fd455f27d818bcca95934a2da2332459e
    
    TBR=rdevlin.cronin@chromium.org,michaelpg@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2766303003
    Cr-Commit-Position: refs/heads/master@{#458955}

--- chrome/browser/extensions/unpacked_installer.cc
+++ chrome/browser/extensions/unpacked_installer.cc
@@ -8,9 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/files/file_util.h"
+#include "base/memory/ptr_util.h"
 #include "base/strings/string_util.h"
 #include "base/threading/thread_restrictions.h"
 #include "chrome/browser/extensions/extension_error_reporter.h"
+#include "chrome/browser/extensions/extension_install_checker.h"
 #include "chrome/browser/extensions/extension_install_prompt.h"
 #include "chrome/browser/extensions/extension_management.h"
 #include "chrome/browser/extensions/extension_service.h"
@@ -111,10 +113,10 @@ scoped_refptr<UnpackedInstaller> UnpackedInstaller::Create(
 
 UnpackedInstaller::UnpackedInstaller(ExtensionService* extension_service)
     : service_weak_(extension_service->AsWeakPtr()),
+      profile_(extension_service->profile()),
       prompt_for_plugins_(true),
       require_modern_manifest_version_(true),
-      be_noisy_on_failure_(true),
-      install_checker_(extension_service->profile()) {
+      be_noisy_on_failure_(true) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 }
 
@@ -150,9 +152,8 @@ bool UnpackedInstaller::LoadFromCommandLine(const base::FilePath& path_in,
   }
 
   std::string error;
-  install_checker_.set_extension(
-      file_util::LoadExtension(
-          extension_path_, Manifest::COMMAND_LINE, GetFlags(), &error).get());
+  extension_ = file_util::LoadExtension(extension_path_, Manifest::COMMAND_LINE,
+                                        GetFlags(), &error);
 
   if (!extension() ||
       !extension_l10n_util::ValidateExtensionLocales(
@@ -196,8 +197,7 @@ void UnpackedInstaller::ShowInstallPrompt() {
       PluginInfo::HasPlugins(extension()) &&
       !disabled_extensions.Contains(extension()->id())) {
     SimpleExtensionLoadPrompt* prompt = new SimpleExtensionLoadPrompt(
-        extension(),
-        install_checker_.profile(),
+        extension(), profile_,
         base::Bind(&UnpackedInstaller::StartInstallChecks, this));
     prompt->ShowPrompt();
     return;
@@ -242,24 +242,26 @@ void UnpackedInstaller::StartInstallChecks() {
     }
   }
 
-  install_checker_.Start(
+  install_checker_ = base::MakeUnique<ExtensionInstallChecker>(
+      profile_, extension_,
       ExtensionInstallChecker::CHECK_REQUIREMENTS |
           ExtensionInstallChecker::CHECK_MANAGEMENT_POLICY,
-      true /* fail fast */,
+      true /* fail fast */);
+  install_checker_->Start(
       base::Bind(&UnpackedInstaller::OnInstallChecksComplete, this));
 }
 
 void UnpackedInstaller::OnInstallChecksComplete(int failed_checks) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
-  if (!install_checker_.policy_error().empty()) {
-    ReportExtensionLoadError(install_checker_.policy_error());
+  if (!install_checker_->policy_error().empty()) {
+    ReportExtensionLoadError(install_checker_->policy_error());
     return;
   }
 
-  if (!install_checker_.requirement_errors().empty()) {
+  if (!install_checker_->requirement_errors().empty()) {
     ReportExtensionLoadError(
-        base::JoinString(install_checker_.requirement_errors(), " "));
+        base::JoinString(install_checker_->requirement_errors(), " "));
     return;
   }
 
@@ -330,9 +332,8 @@ void UnpackedInstaller::LoadWithFileAccess(int flags) {
   DCHECK_CURRENTLY_ON(BrowserThread::FILE);
 
   std::string error;
-  install_checker_.set_extension(
-      file_util::LoadExtension(
-          extension_path_, Manifest::UNPACKED, flags, &error).get());
+  extension_ = file_util::LoadExtension(extension_path_, Manifest::UNPACKED,
+                                        flags, &error);
 
   if (!extension() ||
       !extension_l10n_util::ValidateExtensionLocales(
