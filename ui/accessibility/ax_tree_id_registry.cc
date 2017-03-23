83456792dd0b53848f3e81453d0a72f5bef9d4e2
dmazzoni <dmazzoni@chromium.org>
2017-03-23 11:48:13 -0700

    Re-land: Add a window property to associate RWHVA with its child AX tree ID
    Originally landed: https://codereview.chromium.org/2742533003/
    Reverted: https://codereview.chromium.org/2751503002/
    
    Only change: makes kNoAXTreeID into a constexpr to avoid a static initializer
    
    BUG=699617
    TBR=sky@chromium.org,jam@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2774683002
    Cr-Commit-Position: refs/heads/master@{#459161}

--- ui/accessibility/ax_tree_id_registry.cc
+++ ui/accessibility/ax_tree_id_registry.cc
@@ -11,9 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 
 // static
-const AXTreeIDRegistry::AXTreeID AXTreeIDRegistry::kNoAXTreeID = -1;
-
-// static
 AXTreeIDRegistry* AXTreeIDRegistry::GetInstance() {
   return base::Singleton<AXTreeIDRegistry>::get();
 }
