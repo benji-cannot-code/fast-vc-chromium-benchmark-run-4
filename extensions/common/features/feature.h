e2fc7f00161540417361a05ddff722fd54aa57ff
karandeepb <karandeepb@chromium.org>
2017-03-23 16:12:38 -0700

    Revert of Extensions: Only create Web request rules registry if Declarative Web Request is enabled. (patchset #4 id:60001 of https://codereview.chromium.org/2705513002/ )
    Reason for revert:
    Declarative Web Request is enabled on Stable through <webview>. Hence we can't disable creation of rules registry on Stable.
    
    Original issue's description:
    > Extensions: Only create Web request rules registry if Declarative Web Request is enabled.
    >
    > Currently the Web Request rules registry required by the Declarative Web
    > Request API is created on all Chrome channels, even though the Declarative Web
    > Request API is disabled on Stable. The UMA metric
    > Extensions.NetworkDelayRegistryLoad shows significant delay for some web
    > requests which are blocked due to the loading of the Web Request rules
    > registry even on Stable. The mean delay is around 1.7 sec and the median delay
    > is around 50ms.
    >
    > This CL modifies RulesRegistryService::EnsureDefaultRulesRegistriesRegistered
    > to ensure the Web Request rules registry is only created for the channels on
    > which the Declarative Web Request API is enabled. To do this,
    > Feature::IsAvailableToChannel is introduced.
    >
    > BUG=693243
    >
    > Review-Url: https://codereview.chromium.org/2705513002
    > Cr-Commit-Position: refs/heads/master@{#452240}
    > Committed: https://chromium.googlesource.com/chromium/src/+/a77b53baca5f2a4ff102592c17c8ea3f53dd5307
    
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=693243
    
    Review-Url: https://codereview.chromium.org/2773593003
    Cr-Commit-Position: refs/heads/master@{#459269}

--- extensions/common/features/feature.h
+++ extensions/common/features/feature.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/string_piece.h"
 #include "base/values.h"
-#include "components/version_info/version_info.h"
 #include "extensions/common/manifest.h"
 
 class GURL;
@@ -137,12 +136,6 @@ class Feature {
                                             Context context,
                                             const GURL& url,
                                             Platform platform) const = 0;
-
-  // Returns the availability of the feature on the given |channel|. The
-  // availability result will either be IS_AVAILABLE, UNSUPPORTED_CHANNEL or
-  // NOT_PRESENT.
-  virtual Availability IsAvailableToChannel(
-      version_info::Channel channel) const = 0;
 
   // Returns true if the feature is available to the current environment,
   // without needing to know information about an Extension or any other
