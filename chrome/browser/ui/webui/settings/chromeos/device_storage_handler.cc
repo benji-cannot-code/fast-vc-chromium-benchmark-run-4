7e1edfbdf01433ab62e8bb944430eb0693ca876c
fdoray <fdoray@chromium.org>
2017-03-23 05:59:09 -0700

    Use TaskScheduler instead of blocking pool in device_storage_handler.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=dbeam@chromium.org
    
    Review-Url: https://codereview.chromium.org/2770793002
    Cr-Commit-Position: refs/heads/master@{#459061}

--- chrome/browser/ui/webui/settings/chromeos/device_storage_handler.cc
+++ chrome/browser/ui/webui/settings/chromeos/device_storage_handler.cc
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/drive/chromeos/file_system_interface.h"
 #include "components/user_manager/user_manager.h"
 #include "content/public/browser/browser_context.h"
-#include "content/public/browser/browser_thread.h"
 #include "content/public/browser/storage_partition.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/text/bytes_formatting.h"
@@ -144,9 +143,13 @@ void StorageHandler::UpdateSizeStat() {
 
   int64_t* total_size = new int64_t(0);
   int64_t* available_size = new int64_t(0);
-  content::BrowserThread::PostBlockingPoolTaskAndReply(
-      FROM_HERE, base::Bind(&GetSizeStatBlocking, downloads_path, total_size,
-                            available_size),
+  base::PostTaskWithTraitsAndReply(
+      FROM_HERE,
+      base::TaskTraits()
+          .WithPriority(base::TaskPriority::USER_VISIBLE)
+          .MayBlock(),
+      base::Bind(&GetSizeStatBlocking, downloads_path, total_size,
+                 available_size),
       base::Bind(&StorageHandler::OnGetSizeStat, base::Unretained(this),
                  base::Owned(total_size), base::Owned(available_size)));
 }
