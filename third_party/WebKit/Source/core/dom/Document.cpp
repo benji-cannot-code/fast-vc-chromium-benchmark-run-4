59c4308ef60321103e73291eb11fb4ad6eaed3a2
wittman <wittman@chromium.org>
2017-03-23 11:30:29 -0700

    Revert of Set default document urls to 'about:blank'. (patchset #4 id:60001 of https://codereview.chromium.org/2754933004/ )
    Reason for revert:
    Appears to be causing failures on WebKit Mac10.12: https://uberchromegw.corp.google.com/i/chromium.webkit/builders/WebKit%20Mac10.12/builds/583
    
    01:50:58.061 5302 worker/7 external/wpt/dom/interfaces.html output stderr lines:
    01:50:58.061 5302   CONSOLE MESSAGE: line 251: callback not yet supported
    01:50:58.082 5261 [18796/51893] external/wpt/dom/interfaces.html failed unexpectedly (text diff)
    01:50:58.081 5302 worker/7 external/wpt/dom/interfaces.html failed:
    
    Original issue's description:
    > Set default document urls to 'about:blank'.
    >
    > The patch ensures that the document urls are set to 'about:blank', as is
    > specified in [1], if a url has not been specified. It fixes the tests in
    > [2] and [3].
    >
    > The functionality is implemented by binding the document urls to
    > 'about:blank' if m_url is empty. To achieve this behavior the
    > Document::urlForBinding() function was added. The choice for a binding
    > was made rather than not using an intial value of 'about:blank' for the
    > document urls, as other parts of the code rely on the document urls
    > initially being empty.
    >
    > More details regarding the binding can be found under:
    > https://codereview.chromium.org/2749803003/#msg37
    >
    > [1] https://dom.spec.whatwg.org/#interface-document
    > [2] http://w3c-test.org/dom/nodes/DOMImplementation-createDocument.html
    > [3] http://w3c-test.org/dom/nodes/DOMImplementation-createHTMLDocument.html
    >
    > BUG=563986
    >
    > Review-Url: https://codereview.chromium.org/2754933004
    > Cr-Commit-Position: refs/heads/master@{#459019}
    > Committed: https://chromium.googlesource.com/chromium/src/+/45009c8984881526e99cdb26a752a3acd29a0b60
    
    TBR=tkent@chromium.org,martin@martinrogalla.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=563986
    
    Review-Url: https://codereview.chromium.org/2775693002
    Cr-Commit-Position: refs/heads/master@{#459153}

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -3306,13 +3306,6 @@ void Document::exceptionThrown(ErrorEvent* event) {
   MainThreadDebugger::instance()->exceptionThrown(this, event);
 }
 
-KURL Document::urlForBinding() {
-  if (!url().isNull()) {
-    return url();
-  }
-  return blankURL();
-}
-
 void Document::setURL(const KURL& url) {
   const KURL& newURL = url.isEmpty() ? blankURL() : url;
   if (newURL == m_url)
