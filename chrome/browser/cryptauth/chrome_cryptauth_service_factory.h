b819162a5091f5fa84fbf44b4f706ca2514ca942
ksakamoto <ksakamoto@chromium.org>
2017-03-22 22:45:55 -0700

    Revert of [CrOS Tether] Integrate CryptAuthService into EasyUnlock and Tether. (patchset #8 id:140001 of https://codereview.chromium.org/2751973006/ )
    Reason for revert:
    Broke unit_tests on Linux ChromiumOS Tests (dbg)(1):
    - EasyUnlockPrivateApiTest.GetRemoteDevicesExperimental
    - EasyUnlockPrivateApiTest.GetPermitAccessExperimental
    
    https://uberchromegw.corp.google.com/i/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%28dbg%29%281%29/builds/23905
    https://uberchromegw.corp.google.com/i/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%28dbg%29%281%29/builds/23906
    
    Original issue's description:
    > [CrOS Tether] Integrate CryptAuthService into EasyUnlock and Tether.
    >
    > This CL adds three missing CryptAuthService functions and deletes redundant code in EasyUnlock code.
    >
    > It also passes a CryptAuthService instance to the Tether initialization code when a user logs in.
    >
    > BUG=672263
    >
    > Review-Url: https://codereview.chromium.org/2751973006
    > Cr-Commit-Position: refs/heads/master@{#458981}
    > Committed: https://chromium.googlesource.com/chromium/src/+/7cbc0c2fa215a66f620a77054250a0d635446577
    
    TBR=tengs@chromium.org,stevenjb@chromium.org,erg@chromium.org,khorimoto@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=672263
    
    Review-Url: https://codereview.chromium.org/2773663002
    Cr-Commit-Position: refs/heads/master@{#459009}

--- chrome/browser/cryptauth/chrome_cryptauth_service_factory.h
+++ chrome/browser/cryptauth/chrome_cryptauth_service_factory.h
@@ -12,24 +12,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
 
 // Factory which is used to access the CryptAuthService singleton.
-class ChromeCryptAuthServiceFactory : public BrowserContextKeyedServiceFactory {
+class CryptAuthServiceFactory : public BrowserContextKeyedServiceFactory {
  public:
   static cryptauth::CryptAuthService* GetForBrowserContext(
       content::BrowserContext* context);
 
-  static ChromeCryptAuthServiceFactory* GetInstance();
+  static CryptAuthServiceFactory* GetInstance();
 
  private:
-  friend struct base::DefaultSingletonTraits<ChromeCryptAuthServiceFactory>;
+  friend struct base::DefaultSingletonTraits<CryptAuthServiceFactory>;
 
-  ChromeCryptAuthServiceFactory();
-  ~ChromeCryptAuthServiceFactory() override;
+  CryptAuthServiceFactory();
+  ~CryptAuthServiceFactory() override;
 
   // BrowserContextKeyedServiceFactory
   KeyedService* BuildServiceInstanceFor(
       content::BrowserContext* context) const override;
 
-  DISALLOW_COPY_AND_ASSIGN(ChromeCryptAuthServiceFactory);
+  DISALLOW_COPY_AND_ASSIGN(CryptAuthServiceFactory);
 };
 
 #endif  // CHROME_BROWSER_CRYPTAUTH_CHROME_CRYPTAUTH_SERVICE_FACTORY_H_
--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -127,7 +127,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/data_usage/data_use_ui_tab_model_factory.h"
 #include "chrome/browser/android/search_geolocation/search_geolocation_service.h"
 #else
-#include "chrome/browser/cryptauth/chrome_cryptauth_service_factory.h"
 #include "chrome/browser/ui/global_error/global_error_service_factory.h"
 #include "chrome/browser/usb/usb_chooser_context_factory.h"
 #endif
@@ -214,9 +213,6 @@ EnsureBrowserContextKeyedServiceFactoriesBuilt() {
   CloudPrintProxyServiceFactory::GetInstance();
 #endif
   CookieSettingsFactory::GetInstance();
-#if !defined(OS_ANDROID)
-  ChromeCryptAuthServiceFactory::GetInstance();
-#endif
 #if BUILDFLAG(ENABLE_EXTENSIONS)
   ExtensionWelcomeNotificationFactory::GetInstance();
 #endif
