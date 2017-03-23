07cebd34f134e78f3525d42c89ae15bfd8008605
peleg3 <peleg3@gmail.com>
2017-03-23 15:38:35 -0700

    Allow DOM.getNodeForLocation to skip to nearest non-shadow ancestor
    Added optional param, includeUserAgentShadowDOM, to allow
    DOM.getNodeForLocation to skip to the nearest non-shadow ancestor.
    
    BUG=669939
    TEST=Run blink test with: python third_party/WebKit/Tools/Scripts/run-webkit-tests -t Default third_party/WebKit/LayoutTests/inspector-protocol/dom/dom-getNodeForLocation-skip-shadow.html
    
    Review-Url: https://codereview.chromium.org/2763913002
    Cr-Commit-Position: refs/heads/master@{#459261}

--- third_party/WebKit/Source/core/inspector/InspectorDOMAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorDOMAgent.cpp
@@ -1472,7 +1472,13 @@ Response InspectorDOMAgent::getBoxModel(
   return Response::OK();
 }
 
-Response InspectorDOMAgent::getNodeForLocation(int x, int y, int* nodeId) {
+Response InspectorDOMAgent::getNodeForLocation(
+    int x,
+    int y,
+    Maybe<bool> optionalIncludeUserAgentShadowDOM,
+    int* nodeId) {
+  bool includeUserAgentShadowDOM =
+      optionalIncludeUserAgentShadowDOM.fromMaybe(false);
   Response response = pushDocumentUponHandlelessOperation();
   if (!response.isSuccess())
     return response;
@@ -1480,6 +1486,8 @@ Response InspectorDOMAgent::getNodeForLocation(int x, int y, int* nodeId) {
                          HitTestRequest::AllowChildFrameContent);
   HitTestResult result(request, IntPoint(x, y));
   m_document->frame()->contentLayoutItem().hitTest(result);
+  if (!includeUserAgentShadowDOM)
+    result.setToShadowHostIfInRestrictedShadowRoot();
   Node* node = result.innerPossiblyPseudoNode();
   while (node && node->getNodeType() == Node::kTextNode)
     node = node->parentNode();
