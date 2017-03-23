2d2a7e0f15d978c68fb62719775963ff6f28d870
raphael.kubo.da.costa <raphael.kubo.da.costa@intel.com>
2017-03-22 17:17:42 -0700

    bluetooth: Remove unused include.
    |ScriptRegexp| is not used anywhere, so there is no need to include
    ScriptRegexp.h.
    
    R=jyasskin@chromium.org,ortuno@chromium.org,scheib@chromium.org
    
    Review-Url: https://codereview.chromium.org/2766223002
    Cr-Commit-Position: refs/heads/master@{#458947}

--- third_party/WebKit/Source/modules/bluetooth/BluetoothUUID.cpp
+++ third_party/WebKit/Source/modules/bluetooth/BluetoothUUID.cpp
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/bluetooth/BluetoothUUID.h"
 
 #include "bindings/core/v8/ExceptionState.h"
-#include "bindings/core/v8/ScriptRegexp.h"
 #include "core/dom/ExceptionCode.h"
 #include "platform/UUID.h"
 #include "wtf/HashMap.h"
