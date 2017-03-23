876d162928201577870d6705a5404b60b9a1705b
treib <treib@chromium.org>
2017-03-23 12:31:27 -0700

    [Doodle] Pass parameters to the server endpoint
    ntp:1 identifies the request as being for Chrome NTP
    graybg:0/1 says whether we want an image with gray background
    
    BUG=690467
    
    Review-Url: https://codereview.chromium.org/2768333002
    Cr-Commit-Position: refs/heads/master@{#459179}

--- chrome/browser/doodle/doodle_service_factory.cc
+++ chrome/browser/doodle/doodle_service_factory.cc
@@ -21,6 +21,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/safe_json/safe_json_parser.h"
 
+#if defined(OS_ANDROID)
+#include "chrome/browser/android/chrome_feature_list.h"
+#endif
+
 // static
 DoodleServiceFactory* DoodleServiceFactory::GetInstance() {
   return base::Singleton<DoodleServiceFactory>::get();
@@ -47,10 +51,15 @@ KeyedService* DoodleServiceFactory::BuildServiceInstanceFor(
   // We don't show doodles in incognito profiles (for now?).
   DCHECK(!profile->IsOffTheRecord());
 
+  bool use_gray_background = false;
+#if defined(OS_ANDROID)
+  use_gray_background =
+      !base::FeatureList::IsEnabled(chrome::android::kChromeHomeFeature);
+#endif
   auto fetcher = base::MakeUnique<doodle::DoodleFetcherImpl>(
       profile->GetRequestContext(),
       GoogleURLTrackerFactory::GetForProfile(profile),
-      base::Bind(&safe_json::SafeJsonParser::Parse));
+      base::Bind(&safe_json::SafeJsonParser::Parse), use_gray_background);
   return new doodle::DoodleService(profile->GetPrefs(), std::move(fetcher),
                                    base::MakeUnique<base::OneShotTimer>(),
                                    base::MakeUnique<base::DefaultClock>(),
