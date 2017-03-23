fcf48cd1270b0906f688d6d8d5330a0e88db88bb
rune <rune@opera.com>
2017-03-23 03:24:34 -0700

    Notify inspector of stylesheet changes when removing tree scopes.
    We did not update active stylesheet lists when removing a shadow since
    there is no collection to update. However, the inspector expects a
    notification since it receives a flat vector of active stylesheets from
    all scopes.
    
    Add a m_treeScopesRemoved member to not early return from
    updateActiveStyleSheets and still call probe::activeStyleSheetsUpdated.
    Unless other tree scopes are dirty, calling the inspector is all that
    happens.
    
    R=kochi@chromium.org
    BUG=703427
    
    Review-Url: https://codereview.chromium.org/2766373002
    Cr-Commit-Position: refs/heads/master@{#459039}

--- third_party/WebKit/Source/core/dom/StyleEngine.cpp
+++ third_party/WebKit/Source/core/dom/StyleEngine.cpp
@@ -346,6 +346,7 @@ void StyleEngine::updateActiveStyleSheets() {
   m_dirtyTreeScopes.clear();
   m_documentScopeDirty = false;
   m_allTreeScopesDirty = false;
+  m_treeScopesRemoved = false;
 }
 
 void StyleEngine::updateViewport() {
@@ -392,6 +393,7 @@ void StyleEngine::shadowRootRemovedFromDocument(ShadowRoot* shadowRoot) {
   m_styleSheetCollectionMap.erase(shadowRoot);
   m_activeTreeScopes.erase(shadowRoot);
   m_dirtyTreeScopes.erase(shadowRoot);
+  m_treeScopesRemoved = true;
   resetAuthorStyle(*shadowRoot);
 }
 
