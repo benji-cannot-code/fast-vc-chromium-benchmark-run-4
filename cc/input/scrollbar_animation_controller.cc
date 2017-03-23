cd5041be7c91b98ea33b52339bae7a4ca0c3a29d
chaopeng <chaopeng@chromium.org>
2017-03-23 13:59:30 -0700

    Reset need_trigger_scrollbar_show_ in SAC::DidMouseLeave
    This issue is caused by need_trigger_scrollbar_show_ state not reset then
    no show event posted.
    
    BUG=704245
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2766383002
    Cr-Commit-Position: refs/heads/master@{#459213}

--- cc/input/scrollbar_animation_controller.cc
+++ cc/input/scrollbar_animation_controller.cc
@@ -266,6 +266,7 @@ void ScrollbarAnimationController::DidMouseLeave() {
   horizontal_controller_->DidMouseLeave();
 
   delayed_scrollbar_show_.Cancel();
+  need_trigger_scrollbar_show_ = false;
 
   if (ScrollbarsHidden() || Captured())
     return;
