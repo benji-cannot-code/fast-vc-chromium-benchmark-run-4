bc581cc209214b371332f20aca78e42a008bef59
jbroman <jbroman@chromium.org>
2017-03-23 09:44:29 -0700

    Sync SerializedScriptValue::wireFormatVersion with V8ScriptValueSerializer.
    This updates the version seen by IndexedDB to match what
    V8ScriptValueSerializer believes. This doesn't capture the V8-side versioning,
    but is a step toward making things consistent.
    
    BUG=704293
    
    Review-Url: https://codereview.chromium.org/2767243002
    Cr-Commit-Position: refs/heads/master@{#459118}

--- third_party/WebKit/Source/bindings/core/v8/serialization/V8ScriptValueSerializer.cpp
+++ third_party/WebKit/Source/bindings/core/v8/serialization/V8ScriptValueSerializer.cpp
@@ -27,7 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 // The "Blink-side" serialization version, which defines how Blink will behave
-// during the serialization process. The serialization format has two
+// during the serialization process, is in
+// SerializedScriptValue::wireFormatVersion. The serialization format has two
 // "envelopes": an outer one controlled by Blink and an inner one by V8.
 //
 // They are formatted as follows:
@@ -41,7 +42,6 @@ namespace blink {
 // This version number must be incremented whenever any incompatible changes are
 // made to how Blink writes data. Purely V8-side changes do not require an
 // adjustment to this value.
-static const uint32_t kLatestVersion = 16;
 
 V8ScriptValueSerializer::V8ScriptValueSerializer(
     RefPtr<ScriptState> scriptState,
@@ -69,7 +69,7 @@ RefPtr<SerializedScriptValue> V8ScriptValueSerializer::serialize(
 
   // Write out the file header.
   writeTag(VersionTag);
-  writeUint32(kLatestVersion);
+  writeUint32(SerializedScriptValue::wireFormatVersion);
   m_serializer.WriteHeader();
 
   // Serialize the value and handle errors.
