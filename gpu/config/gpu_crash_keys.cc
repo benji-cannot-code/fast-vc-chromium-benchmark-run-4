a263e4c8cbc86db71da6d16f0dff8af36fc0d2bf
sadrul <sadrul@chromium.org>
2017-03-23 13:30:05 -0700

    gpu: Add a util method to set crash-keys from a GPUInfo.
    Use the newly introduced util method to set crash-keys from a GPUInfo
    in both chrome and android_webview. This will also be used in mus.
    
    BUG=643746
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Earlier landed in r458276, reverted in r458968 because of size regression.
    
    Review-Url: https://codereview.chromium.org/2751103008
    Cr-Commit-Position: refs/heads/master@{#459202}

---
+++ gpu/config/gpu_crash_keys.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "gpu/config/gpu_crash_keys.h"
+
+namespace gpu {
+namespace crash_keys {
+#if !defined(OS_ANDROID)
+const char kGPUVendorID[] = "gpu-venid";
+const char kGPUDeviceID[] = "gpu-devid";
+#endif
+const char kGPUDriverVersion[] = "gpu-driver";
+const char kGPUPixelShaderVersion[] = "gpu-psver";
+const char kGPUVertexShaderVersion[] = "gpu-vsver";
+#if defined(OS_MACOSX)
+const char kGPUGLVersion[] = "gpu-glver";
+#elif defined(OS_POSIX)
+const char kGPUVendor[] = "gpu-gl-vendor";
+const char kGPURenderer[] = "gpu-gl-renderer";
+#endif
+}  // namespace crash_keys
+}  // namespace gpu
---
+++ gpu/config/gpu_crash_keys.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef GPU_CONFIG_GPU_CRASH_KEYS_H_
+#define GPU_CONFIG_GPU_CRASH_KEYS_H_
+
+#include "build/build_config.h"
+
+namespace gpu {
+namespace crash_keys {
+
+// Keys that can be used for crash reporting.
+#if !defined(OS_ANDROID)
+extern const char kGPUVendorID[];
+extern const char kGPUDeviceID[];
+#endif
+extern const char kGPUDriverVersion[];
+extern const char kGPUPixelShaderVersion[];
+extern const char kGPUVertexShaderVersion[];
+#if defined(OS_MACOSX)
+extern const char kGPUGLVersion[];
+#elif defined(OS_POSIX)
+extern const char kGPUVendor[];
+extern const char kGPURenderer[];
+#endif
+}  // namespace crash_keys
+}  // namespace gpu
+
+#endif  // GPU_CONFIG_GPU_CRASH_KEYS_H_
