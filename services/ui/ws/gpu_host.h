7fc4889e6691bd03abbbf635ee10c1c4ae92434a
jonross <jonross@chromium.org>
2017-03-23 15:12:36 -0700

    While testing expanded mash_browser_tests a flaky shutdown crash was found in GpuClient.
    GpuClient::EstablishGpuChannel could be called after GpuHost or GpuClient had been deleted.
    
    GpuClient was holding a raw pointer back to GpuHost. It's lifetime can exceed GpuHost as it exists in a StrongBindingPtr. It also was registering a callback with and Unretained pointer to itself. So it could be called post delete.
    
    This change updates GpuHost to hold all GpuClients in a StrongBindingSet which deletes them upon shutdown.
    
    This also updates GpuClient to register the callback with a WeakPtr to itself, so that the callback can be invalidated upon deletion.
    
    TEST=GpuHostTest.GpuClientDestructionOrder,
    GpuHostTest.HostDeletionInvalidatesGpuClientCallback
    TEST=BrowserTests
    BUG=701380
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2741343003
    Cr-Commit-Position: refs/heads/master@{#459248}

--- services/ui/ws/gpu_host.h
+++ services/ui/ws/gpu_host.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/ipc/client/gpu_channel_host.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
 #include "mojo/public/cpp/bindings/interface_request.h"
+#include "mojo/public/cpp/bindings/strong_binding_set.h"
 #include "services/ui/gpu/gpu_main.h"
 #include "services/ui/gpu/interfaces/gpu_host.mojom.h"
 #include "services/ui/gpu/interfaces/gpu_service.mojom.h"
@@ -23,6 +24,12 @@ namespace ui {
 class ServerGpuMemoryBufferManager;
 
 namespace ws {
+
+class GpuClient;
+
+namespace test {
+class GpuHostTest;
+}  // namespace test
 
 class GpuHostDelegate;
 
@@ -43,6 +50,9 @@ class GpuHost : public mojom::GpuHost {
                                cc::mojom::DisplayCompositorClientPtr client);
 
  private:
+  friend class test::GpuHostTest;
+
+  GpuClient* AddInternal(mojom::GpuRequest request);
   void OnBadMessageFromGpu();
 
   // mojom::GpuHost:
@@ -75,6 +85,8 @@ class GpuHost : public mojom::GpuHost {
   // TODO(fsamuel): GpuHost should not be holding onto |gpu_main_impl|
   // because that will live in another process soon.
   std::unique_ptr<GpuMain> gpu_main_impl_;
+
+  mojo::StrongBindingSet<mojom::Gpu> gpu_bindings_;
 
   DISALLOW_COPY_AND_ASSIGN(GpuHost);
 };
