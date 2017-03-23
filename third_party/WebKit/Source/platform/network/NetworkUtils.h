d9ad457e58d6ce7ebd37f24a5246a827daa055d0
hiroshige <hiroshige@chromium.org>
2017-03-23 01:33:02 -0700

    Set the same headers for data URLs in WebURLLoaderImpl and ResourceFetcher
    Response headers are appended by net::URLRequestDataJob::BuildResponse()
    when WebURLLoaderImpl returns data URL response in a renderer process,
    but not when ResourceFetcher returns data URL response synchronously by
    ResourceFetcher::resourceForStaticData().
    
    This CL makes the response headers to be added in both cases, by making
    NetworkUtils::parseDataURL() (that is used from resourceForStaticData())
    to call net::URLRequestDataJob::BuildResponse() and set response headers.
    
    BUG=704032
    
    Review-Url: https://codereview.chromium.org/2766583002
    Cr-Commit-Position: refs/heads/master@{#459026}

--- third_party/WebKit/Source/platform/network/NetworkUtils.h
+++ third_party/WebKit/Source/platform/network/NetworkUtils.h
@@ -13,6 +13,7 @@ namespace blink {
 
 class KURL;
 class SharedBuffer;
+class ResourceResponse;
 
 namespace NetworkUtils {
 
@@ -28,11 +29,11 @@ PLATFORM_EXPORT bool isLocalHostname(const String& host, bool* isLocal6);
 PLATFORM_EXPORT String getDomainAndRegistry(const String& host,
                                             PrivateRegistryFilter);
 
-// Returns the decoded data url if url had a supported mimetype and parsing was
-// successful.
-PLATFORM_EXPORT PassRefPtr<SharedBuffer> parseDataURL(const KURL&,
-                                                      AtomicString& mimetype,
-                                                      AtomicString& charset);
+// Returns the decoded data url as ResourceResponse and SharedBuffer
+// if url had a supported mimetype and parsing was successful.
+PLATFORM_EXPORT PassRefPtr<SharedBuffer> parseDataURLAndPopulateResponse(
+    const KURL&,
+    ResourceResponse&);
 
 PLATFORM_EXPORT bool isRedirectResponseCode(int);
 
