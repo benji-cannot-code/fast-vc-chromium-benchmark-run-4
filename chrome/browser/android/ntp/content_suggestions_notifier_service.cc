060a5d8ea49c3a08041e581f6fa9a2f576d48570
sfiera <sfiera@chromium.org>
2017-03-23 08:58:49 -0700

    NTP: Replace "always_notify" param with ID
    The server now treats experiment ID 3313312 as meaning "always_notify",
    so when the (always notify) option is selected in about://flags, use the
    ID instead of a feature param. This makes our testing configuration more
    like our production configuration.
    
    Review-Url: https://codereview.chromium.org/2763353002
    Cr-Commit-Position: refs/heads/master@{#459098}

--- chrome/browser/android/ntp/content_suggestions_notifier_service.cc
+++ chrome/browser/android/ntp/content_suggestions_notifier_service.cc
@@ -30,7 +30,6 @@ using ntp_snippets::ContentSuggestion;
 using ntp_snippets::ContentSuggestionsNotificationHelper;
 using ntp_snippets::ContentSuggestionsService;
 using ntp_snippets::KnownCategories;
-using params::ntp_snippets::kNotificationsAlwaysNotifyParam;
 using params::ntp_snippets::kNotificationsDailyLimit;
 using params::ntp_snippets::kNotificationsDefaultDailyLimit;
 using params::ntp_snippets::kNotificationsDefaultPriority;
@@ -178,16 +177,6 @@ class ContentSuggestionsNotifierService::NotifyingObserver
  private:
   const ContentSuggestion* GetSuggestionToNotifyAbout(Category category) {
     const auto& suggestions = service_->GetSuggestionsForCategory(category);
-    // TODO(sfiera): replace with AlwaysNotifyAboutContentSuggestions().
-    if (variations::GetVariationParamByFeatureAsBool(
-            kNotificationsFeature, kNotificationsAlwaysNotifyParam, false)) {
-      if (category.IsKnownCategory(KnownCategories::ARTICLES) &&
-          !suggestions.empty()) {
-        return &suggestions[0];
-      }
-      return nullptr;
-    }
-
     for (const ContentSuggestion& suggestion : suggestions) {
       if (suggestion.notification_extra()) {
         return &suggestion;
