2d7652a1b001e041ed7a25b3ccf12d57b9270342
lanwei <lanwei@chromium.org>
2017-03-23 12:01:11 -0700

    Make stylus's eraser button work on Mac
    In order to make stylus's eraser button work on Mac, I set the pointer type to
    blink::WebPointerProperties::PointerType::Eraser when we use the eraser button on the tablet.
    
    BUG=685252
    
    Review-Url: https://codereview.chromium.org/2739773004
    Cr-Commit-Position: refs/heads/master@{#459166}

--- content/browser/renderer_host/render_widget_host_view_mac.h
+++ content/browser/renderer_host/render_widget_host_view_mac.h
@@ -201,6 +201,7 @@ struct TextInputState;
 - (void)updateCursor:(NSCursor*)cursor;
 - (NSRect)firstViewRectForCharacterRange:(NSRange)theRange
                              actualRange:(NSRangePointer)actualRange;
+- (void)tabletEvent:(NSEvent*)theEvent;
 - (void)quickLookWithEvent:(NSEvent*)event;
 - (void)showLookUpDictionaryOverlayAtPoint:(NSPoint)point;
 - (void)showLookUpDictionaryOverlayFromRange:(NSRange)range
