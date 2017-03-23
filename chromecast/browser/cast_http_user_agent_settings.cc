f30ebca97231abaee26996c533e277845cb2237a
slan <slan@chromium.org>
2017-03-23 13:24:14 -0700

    [Chromecast] Remove chromecast_repack_locales.py.
    This script predates GN templates, which allow us to do all of our
    resource packing in GN. Eliminate the script and correct some bad out
    directory conventions.
    
    BUG=697090
    TEST= diff the .pak output before and after the change <= no diff
    
    Review-Url: https://codereview.chromium.org/2773453003
    Cr-Commit-Position: refs/heads/master@{#459200}

--- chromecast/browser/cast_http_user_agent_settings.cc
+++ chromecast/browser/cast_http_user_agent_settings.cc
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/rtl.h"
 #include "base/logging.h"
 #include "build/build_config.h"
+#include "chromecast/app/grit/chromecast_settings.h"
 #include "chromecast/common/cast_content_client.h"
 #include "content/public/browser/browser_thread.h"
-#include "grit/chromecast_settings.h"
 #include "net/http/http_util.h"
 #include "ui/base/l10n/l10n_util.h"
 
