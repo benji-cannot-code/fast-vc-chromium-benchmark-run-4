89c76e93359488a6a467a612f1e33214ac919fb5
dtseng <dtseng@chromium.org>
2017-03-24 15:41:30 -0700

    Fixes what should have been caught by Closure compiler
    |init| has a param of function, but when passed a boolean, not flagged by Closure.
    
    TBR=dtseng@chromium.org
    
    NOTRY=true
    NOPRESUBMIT=true
    
    Review-Url: https://codereview.chromium.org/2765353006
    Cr-Commit-Position: refs/heads/master@{#459581}

--- chrome/browser/resources/chromeos/chromevox/cvox2/injected/loader.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/injected/loader.js
@@ -11,7 +11,7 @@ goog.require('KeyboardHandler');
  */
 function initMin() {
   if (cvox.ChromeVox.isChromeOS) {
-    cvox.ApiImplementation.init(true);
+    cvox.ApiImplementation.init();
     return;
   }
 
