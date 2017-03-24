6f9f088afeb43b2dedfa9c565fddfa623efcce2f
ssid <ssid@chromium.org>
2017-03-24 12:21:31 -0700

    [memory-infra] Make MemoryDumpScheduler non-thread safe singleton
    The MemoryDumpScheduler thread restrictions and lifetime were
    complicated to follow. So, make it singleton with thread safety handled
    by MemoryDumpManager. MDM always accesses the schduler holding a lock.
    
    BUG=607533
    
    Review-Url: https://codereview.chromium.org/2743993004
    Cr-Commit-Position: refs/heads/master@{#459515}

--- base/trace_event/memory_dump_manager_unittest.cc
+++ base/trace_event/memory_dump_manager_unittest.cc
@@ -276,7 +276,8 @@ class MemoryDumpManagerTest : public testing::Test {
   void DisableTracing() { TraceLog::GetInstance()->SetDisabled(); }
 
   bool IsPeriodicDumpingEnabled() const {
-    return mdm_->dump_scheduler_->IsPeriodicTimerRunningForTesting();
+    return MemoryDumpScheduler::GetInstance()
+        ->IsPeriodicTimerRunningForTesting();
   }
 
   int GetMaxConsecutiveFailuresCount() const {
