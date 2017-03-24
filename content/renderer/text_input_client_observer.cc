de244c6bb1bd5167aaaaaf435e3c65f33394b171
wychen <wychen@chromium.org>
2017-03-23 23:04:04 -0700

    Fix all GN missing headers in WebKit/public
    These were reported by tzik@:
    https://groups.google.com/a/chromium.org/forum/#!topic/chromium-dev/1kK45mVemBc
    
    BUG=661774
    
    Review-Url: https://codereview.chromium.org/2771923003
    Cr-Commit-Position: refs/heads/master@{#459369}

--- content/renderer/text_input_client_observer.cc
+++ content/renderer/text_input_client_observer.cc
@@ -22,8 +22,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/platform/WebString.h"
 #include "third_party/WebKit/public/web/WebLocalFrame.h"
 #include "third_party/WebKit/public/web/WebView.h"
-#include "third_party/WebKit/public/web/mac/WebSubstringUtil.h"
 #include "ui/gfx/geometry/rect.h"
+
+#if defined(OS_MACOSX)
+#include "third_party/WebKit/public/web/mac/WebSubstringUtil.h"
+#endif
 
 namespace content {
 
