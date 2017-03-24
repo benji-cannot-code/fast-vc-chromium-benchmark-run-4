61e9c05e072935a8909fda202b11d06dacb46c72
emircan <emircan@chromium.org>
2017-03-24 16:35:03 -0700

    Fix multiple Initialize() calls and reenable VEA usage in MediaRecorder for Win8
    This Cl does two things:
    - Reverts the CL that disables VEA usage until flaky test is addressed
    https://codereview.chromium.org/2727633008
    - Fixes the underlying issue of flakiness by not allowing |encoder_| to be
    initialized multiple times.
    
    BUG=698441
    TEST=Local run of tests consistently pass.
    
    Review-Url: https://codereview.chromium.org/2775453003
    Cr-Commit-Position: refs/heads/master@{#459598}

--- content/renderer/media_recorder/video_track_recorder.cc
+++ content/renderer/media_recorder/video_track_recorder.cc
@@ -31,10 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/libyuv/include/libyuv.h"
 #include "ui/gfx/geometry/size.h"
 
-#if defined(OS_WIN)
-#include "base/win/windows_version.h"
-#endif
-
 #if BUILDFLAG(RTC_USE_H264)
 #include "third_party/openh264/src/codec/api/svc/codec_api.h"
 #include "third_party/openh264/src/codec/api/svc/codec_app_def.h"
@@ -120,12 +116,6 @@ CodecEnumerator::CodecEnumerator() {
 #if defined(OS_ANDROID)
   // See https://crbug.com/653864.
   return;
-#endif
-
-#if defined(OS_WIN)
-  // See https://crbug.com/698441.
-  if (base::win::GetVersion() < base::win::VERSION_WIN10)
-    return;
 #endif
 
   content::RenderThreadImpl* const render_thread_impl =
@@ -1303,6 +1293,11 @@ void VideoTrackRecorder::InitializeEncoder(
     base::TimeTicks capture_time) {
   DVLOG(3) << __func__ << frame->visible_rect().size().ToString();
   DCHECK(main_render_thread_checker_.CalledOnValidThread());
+
+  // Avoid reinitializing |encoder_| when there are multiple frames sent to the
+  // sink to initialize, https://crbug.com/698441.
+  if (encoder_)
+    return;
 
   MediaStreamVideoSink::DisconnectFromTrack();
 
