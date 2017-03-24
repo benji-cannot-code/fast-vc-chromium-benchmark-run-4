6dd8c7d44d41a2cc9f5fc044aef8f90101279d96
fsamuel <fsamuel@chromium.org>
2017-03-24 13:18:18 -0700

    Aura-Mus: InFlightBoundsChange should include an optional cc::LocalSurfaceId
    The LocalSurfaceId to use should be tied with the Bounds including on revert.
    This CL adds base::Optional<cc::LocalSurfaceId> to InFlightBoundsChange.
    
    This CL also trivially updates WindowTreeClient::SetWindowBoundsFromServer
    to take in a base::Optional<cc::LocalSurface> which is unused.
    
    BUG=672962
    TBR=jbauman@chromium.org
    
    Review-Url: https://codereview.chromium.org/2769073005
    Cr-Commit-Position: refs/heads/master@{#459531}

--- ui/aura/mus/in_flight_change.h
+++ ui/aura/mus/in_flight_change.h
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
+#include "base/optional.h"
+#include "cc/surfaces/local_surface_id.h"
 #include "ui/aura/window_observer.h"
 #include "ui/base/ui_base_types.h"
 #include "ui/gfx/geometry/rect.h"
@@ -140,9 +142,12 @@ class InFlightChange {
 
 class InFlightBoundsChange : public InFlightChange {
  public:
-  InFlightBoundsChange(WindowTreeClient* window_tree_client,
-                       WindowMus* window,
-                       const gfx::Rect& revert_bounds);
+  InFlightBoundsChange(
+      WindowTreeClient* window_tree_client,
+      WindowMus* window,
+      const gfx::Rect& revert_bounds,
+      const base::Optional<cc::LocalSurfaceId>& local_surface_id);
+  ~InFlightBoundsChange() override;
 
   // InFlightChange:
   void SetRevertValueFrom(const InFlightChange& change) override;
@@ -151,6 +156,7 @@ class InFlightBoundsChange : public InFlightChange {
  private:
   WindowTreeClient* window_tree_client_;
   gfx::Rect revert_bounds_;
+  base::Optional<cc::LocalSurfaceId> revert_local_surface_id_;
 
   DISALLOW_COPY_AND_ASSIGN(InFlightBoundsChange);
 };
