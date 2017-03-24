7c85a73dfccea654d52f640960b3d9eefe5fa55e
mdjones <mdjones@chromium.org>
2017-03-24 12:36:58 -0700

    Remove unused feedback class in dom distiller component
    The external feedback reporter for dom distiller is no longer used
    since the feature has launched. The file is not included in any gn
    build files and is one of many tracked here:
    
    https://docs.google.com/spreadsheets/d/15az3FMl-jAS0mx4E9XVSBVHVpmEzo-9EAGY0ywe7bZs
    
    This change also removes the unused feedback form that was part of
    the dom distiller UI and all of its internals.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2768093004
    Cr-Commit-Position: refs/heads/master@{#459522}

--- components/dom_distiller/core/dom_distiller_request_view_base.cc
+++ components/dom_distiller/core/dom_distiller_request_view_base.cc
@@ -57,10 +57,6 @@ void DomDistillerRequestViewBase::OnArticleReady(
     SendJavaScript(viewer::GetSetTitleJs(article_proto->title()));
     SendJavaScript(viewer::GetSetTextDirectionJs(text_direction));
     SendJavaScript(viewer::GetUnsafeArticleContentJs(article_proto));
-    // If any content was loaded, show the feedback form.
-    if (ShouldShowFeedbackForm()) {
-      SendJavaScript(viewer::GetShowFeedbackFormJs());
-    }
   } else {
     // It's possible that we didn't get some incremental updates from the
     // distiller. Ensure all remaining pages are flushed to the viewer.
@@ -92,10 +88,6 @@ void DomDistillerRequestViewBase::OnArticleUpdated(
       // client.
       SendJavaScript(viewer::GetSetTitleJs(page.title()));
       SendJavaScript(viewer::GetSetTextDirectionJs(page.text_direction()));
-      // If any content was loaded, show the feedback form.
-      if (ShouldShowFeedbackForm()) {
-        SendJavaScript(viewer::GetShowFeedbackFormJs());
-      }
     }
   }
 }
--- components/dom_distiller/core/experiments.h
+++ components/dom_distiller/core/experiments.h
@@ -15,7 +15,6 @@ namespace dom_distiller {
   };
 
   DistillerHeuristicsType GetDistillerHeuristicsType();
-  bool ShouldShowFeedbackForm();
 }
 
 #endif  // COMPONENTS_DOM_DISTILLER_CORE_EXPERIMENTS_H_
