ab2832ad27620f40e144fe7c214f2700db544192
ananta <ananta@chromium.org>
2017-03-23 21:08:37 -0700

    Remove the LogResourceRequestTimeOnUI function from resource_dispatcher_host_impl.cc
    This function is used to UMA log the time a network request started. This functionality
    has now been moved to the LoaderDelegate interface.
    
    This is part of the changes to make the content/browser/loader folder not depend
    on extraneous content, which will facilitate
    this code becoming part of the network service.
    
    BUG=598073
    
    Review-Url: https://codereview.chromium.org/2764023004
    Cr-Commit-Position: refs/heads/master@{#459359}

--- content/browser/loader/loader_delegate.h
+++ content/browser/loader/loader_delegate.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/time/time.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/resource_request_details.h"
 #include "content/public/browser/resource_request_info.h"
@@ -49,6 +50,12 @@ class CONTENT_EXPORT LoaderDelegate {
   virtual void DidGetRedirectForResourceRequest(
       const ResourceRequestInfo::WebContentsGetter& web_contents_getter,
       std::unique_ptr<ResourceRedirectDetails> details) = 0;
+
+  // Called when the network stack started handling the navigation request.
+  virtual void LogResourceRequestTime(base::TimeTicks timestamp,
+                                      int render_process_id,
+                                      int render_frame_id,
+                                      const GURL& url) = 0;
 };
 
 }  // namespace content
--- content/browser/loader_delegate_impl.cc
+++ content/browser/loader_delegate_impl.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/loader_delegate_impl.h"
 
+#include "content/browser/frame_host/frame_tree.h"
+#include "content/browser/frame_host/navigator.h"
+#include "content/browser/frame_host/render_frame_host_impl.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/browser/browser_thread.h"
 
@@ -29,6 +32,22 @@ void DidGetRedirectForResourceRequestOnUI(
   if (!web_contents)
     return;
   web_contents->DidGetRedirectForResourceRequest(*details.get());
+}
+
+// This method is called in the UI thread to send the timestamp of a resource
+// request to the respective Navigator (for an UMA histogram).
+void DidGetLogResourceRequestTimeOnUI(base::TimeTicks timestamp,
+                                      int render_process_id,
+                                      int render_frame_id,
+                                      const GURL& url) {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  RenderFrameHostImpl* host =
+      RenderFrameHostImpl::FromID(render_process_id, render_frame_id);
+  if (host != nullptr) {
+    DCHECK(host->frame_tree_node()->IsMainFrame());
+    host->frame_tree_node()->navigator()->LogResourceRequestTime(
+      timestamp, url);
+  }
 }
 
 }  // namespace
@@ -64,6 +83,17 @@ void LoaderDelegateImpl::DidGetRedirectForResourceRequest(
       BrowserThread::UI, FROM_HERE,
       base::Bind(&DidGetRedirectForResourceRequestOnUI, web_contents_getter,
                  base::Passed(std::move(details))));
+}
+
+void LoaderDelegateImpl::LogResourceRequestTime(base::TimeTicks timestamp,
+                                                int render_process_id,
+                                                int render_frame_id,
+                                                const GURL& url) {
+  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  BrowserThread::PostTask(
+      BrowserThread::UI, FROM_HERE,
+      base::Bind(&DidGetLogResourceRequestTimeOnUI, timestamp,
+                 render_process_id, render_frame_id, url));
 }
 
 }  // namespace content
