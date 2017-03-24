ed05d14cd36e2b3d1e3ab455944e0ad21ee254a4
brettw <brettw@chromium.org>
2017-03-24 09:36:42 -0700

    Enable noexcept on Windows, use for a few move constructors.
    MSVC complains if you use noexcept with no exception handling mode specified (as we do).
    This code disables the warning. noexcept on move constructors allows better optimizations
    in some cases.
    http://en.cppreference.com/w/cpp/language/noexcept_spec
    
    Updates a few common classes' move constructors to use this.
    
    Reland of https://codereview.chromium.org/2771643002 with landmine. There were some
    reports of PCH headers not getting updated and the warning change no taking effect.
    I investigated briefly but could not see why PCH dependencies are incorrect.
    
    Review-Url: https://codereview.chromium.org/2769283002
    Cr-Commit-Position: refs/heads/master@{#459455}

--- base/android/jni_weak_ref.cc
+++ base/android/jni_weak_ref.cc
@@ -20,7 +20,8 @@ JavaObjectWeakGlobalRef::JavaObjectWeakGlobalRef(
   Assign(orig);
 }
 
-JavaObjectWeakGlobalRef::JavaObjectWeakGlobalRef(JavaObjectWeakGlobalRef&& orig)
+JavaObjectWeakGlobalRef::JavaObjectWeakGlobalRef(
+    JavaObjectWeakGlobalRef&& orig) noexcept
     : obj_(orig.obj_) {
   orig.obj_ = nullptr;
 }
