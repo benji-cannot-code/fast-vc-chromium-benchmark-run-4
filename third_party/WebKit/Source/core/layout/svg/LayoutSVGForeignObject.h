1c4a40e84793c97da23da1141382ead31489f42a
pdr <pdr@chromium.org>
2017-03-23 18:35:00 -0700

    Revert: "Let SVGForeignObject's local SVG coordinates mean what it should"
    This patch caused a hit testing regression with foreign objects. While
    the fix is not hard, we are going to roll out as a precaution (and merge
    to stable).
    
    This is a manual revert of https://crrev.com/f451d9b5de5a1b4691d60d8c74341ac8a466cf57
    (commit number #435337) due to revert conflicts.
    
    BUG=704643
    
    Review-Url: https://codereview.chromium.org/2767343003
    Cr-Commit-Position: refs/heads/master@{#459330}

--- third_party/WebKit/Source/core/layout/svg/LayoutSVGForeignObject.h
+++ third_party/WebKit/Source/core/layout/svg/LayoutSVGForeignObject.h
@@ -54,7 +54,7 @@ class LayoutSVGForeignObject final : public LayoutSVGBlock {
   void layout() override;
 
   FloatRect objectBoundingBox() const override {
-    return FloatRect(frameRect());
+    return FloatRect(FloatPoint(), FloatSize(size()));
   }
   FloatRect strokeBoundingBox() const override { return objectBoundingBox(); }
   FloatRect visualRectInLocalSVGCoordinates() const override {
@@ -70,6 +70,8 @@ class LayoutSVGForeignObject final : public LayoutSVGBlock {
   }
 
   void setNeedsTransformUpdate() override { m_needsTransformUpdate = true; }
+
+  AffineTransform localToSVGParentTransform() const override;
 
  private:
   LayoutUnit elementX() const;
