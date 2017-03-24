6527ebd0c147c70b61673cf484563ec304ccdfd5
nasko <nasko@chromium.org>
2017-03-23 17:56:16 -0700

    Prefix WebFrameClient::relatedAppsFetcher with Get.
    This CL prefixes the relatedAppsFetcher method in WebFrameClient, so it
    doesn't conflict with the actual type when the Blink style rewriter
    is used to rewrite the code.
    
    BUG=582312
    
    Review-Url: https://codereview.chromium.org/2775663006
    Cr-Commit-Position: refs/heads/master@{#459311}

--- third_party/WebKit/Source/web/ChromeClientImpl.cpp
+++ third_party/WebKit/Source/web/ChromeClientImpl.cpp
@@ -1227,7 +1227,7 @@ void ChromeClientImpl::installSupplements(LocalFrame& frame) {
                                      new AudioOutputDeviceClientImpl(frame));
   }
   if (RuntimeEnabledFeatures::installedAppEnabled())
-    InstalledAppController::provideTo(frame, client->relatedAppsFetcher());
+    InstalledAppController::provideTo(frame, client->getRelatedAppsFetcher());
 }
 
 }  // namespace blink
--- third_party/WebKit/public/web/WebFrameClient.h
+++ third_party/WebKit/public/web/WebFrameClient.h
@@ -467,7 +467,7 @@ class BLINK_EXPORT WebFrameClient {
   // InstalledApp API ----------------------------------------------------
 
   // Used to access the embedder for the InstalledApp API.
-  virtual WebRelatedAppsFetcher* relatedAppsFetcher() { return nullptr; }
+  virtual WebRelatedAppsFetcher* getRelatedAppsFetcher() { return nullptr; }
 
   // Editing -------------------------------------------------------------
 
