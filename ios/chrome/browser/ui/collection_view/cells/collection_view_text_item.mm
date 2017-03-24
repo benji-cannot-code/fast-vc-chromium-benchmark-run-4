409523737a54aa3ab46aec7f3654cc43328384cc
gambard <gambard@chromium.org>
2017-03-24 04:59:00 -0700

    Create CollectionViewTextCell
    This CL creates a CollectionViewTextCell having the same behavior as
    MDCCollectionViewTextCell but using auto layout.
    
    BUG=704082
    
    Review-Url: https://codereview.chromium.org/2764783006
    Cr-Commit-Position: refs/heads/master@{#459395}

--- ios/chrome/browser/ui/collection_view/cells/collection_view_text_item.mm
+++ ios/chrome/browser/ui/collection_view/cells/collection_view_text_item.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/collection_view/cells/collection_view_text_item.h"
 
+#import "ios/chrome/browser/ui/collection_view/cells/collection_view_text_cell.h"
 #import "ios/third_party/material_components_ios/src/components/Palettes/src/MaterialPalettes.h"
 #import "ios/third_party/material_roboto_font_loader_ios/src/src/MaterialRobotoFontLoader.h"
 
@@ -17,16 +18,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @synthesize accessoryType = _accessoryType;
 @synthesize text = _text;
 @synthesize detailText = _detailText;
-@synthesize image = _image;
 @synthesize textFont = _textFont;
 @synthesize textColor = _textColor;
+@synthesize numberOfTextLines = _numberOfTextLines;
 @synthesize detailTextFont = _detailTextFont;
 @synthesize detailTextColor = _detailTextColor;
+@synthesize numberOfDetailTextLines = _numberOfDetailTextLines;
 
 - (instancetype)initWithType:(NSInteger)type {
   self = [super initWithType:type];
   if (self) {
-    self.cellClass = [MDCCollectionViewTextCell class];
+    self.cellClass = [CollectionViewTextCell class];
+    _numberOfTextLines = 1;
+    _numberOfDetailTextLines = 1;
   }
   return self;
 }
@@ -62,12 +66,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #pragma mark CollectionViewItem
 
-- (void)configureCell:(MDCCollectionViewTextCell*)cell {
+- (void)configureCell:(CollectionViewTextCell*)cell {
   [super configureCell:cell];
   cell.accessoryType = self.accessoryType;
   cell.textLabel.text = self.text;
   cell.detailTextLabel.text = self.detailText;
-  cell.imageView.image = self.image;
   cell.isAccessibilityElement = YES;
   if (self.detailText.length == 0) {
     cell.accessibilityLabel = self.text;
@@ -79,8 +82,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // Styling.
   cell.textLabel.font = self.textFont;
   cell.textLabel.textColor = self.textColor;
+  cell.textLabel.numberOfLines = self.numberOfTextLines;
   cell.detailTextLabel.font = self.detailTextFont;
   cell.detailTextLabel.textColor = self.detailTextColor;
+  cell.detailTextLabel.numberOfLines = self.numberOfDetailTextLines;
 }
 
 @end
