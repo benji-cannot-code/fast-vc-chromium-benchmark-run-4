4389fa7df2fa129bb9f1f91f972d98978e87f5dd
hjd <hjd@chromium.org>
2017-03-24 05:29:43 -0700

    memory-infra: Add a struct to carry memory use information
    This (currently unused) struct will carry information back to the
    memory coordinator service where it will eventually be collated
    and make its way to UMA.
    
    BUG=703184
    
    Review-Url: https://codereview.chromium.org/2755343003
    Cr-Commit-Position: refs/heads/master@{#459399}

--- base/trace_event/memory_dump_request_args.cc
+++ base/trace_event/memory_dump_request_args.cc
@@ -61,5 +61,9 @@ MemoryDumpLevelOfDetail StringToMemoryDumpLevelOfDetail(
   return MemoryDumpLevelOfDetail::LAST;
 }
 
+MemoryDumpCallbackResult::MemoryDumpCallbackResult() {}
+
+MemoryDumpCallbackResult::~MemoryDumpCallbackResult() {}
+
 }  // namespace trace_event
 }  // namespace base
