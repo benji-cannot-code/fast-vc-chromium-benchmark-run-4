f68eb3524d37ecd6165a4060b8d81b9c941c907d
oka <oka@chromium.org>
2017-03-23 22:54:31 -0700

    Remove resolved TODO.
    BUG=224832
    TEST=None
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2773053002
    Cr-Commit-Position: refs/heads/master@{#459368}

--- ui/file_manager/file_manager/foreground/js/ui/drag_selector.js
+++ ui/file_manager/file_manager/foreground/js/ui/drag_selector.js
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * Drag selector used on the file list or the grid table.
- * TODO(hirono): Support drag selection for grid view. crbug.com/224832
  * @constructor
  * @struct
  */
