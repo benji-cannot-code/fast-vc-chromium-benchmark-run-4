d8d5f04e4ca161e3cc08d51f968fe4b607698e8c
dougt <dougt@chromium.org>
2017-03-24 12:15:47 -0700

    Make BrowserAccessibility implement AXPlatformNodeDelegete.
    As part of the fefactoring of BrowserAccessibility, we want to end up with
    BrowserAccessibility inheriting from AXPlatformNodeDelegate and owning a
    AXPlatformNode. This change has BrowserAccessibility implmenting a
    AXPlatformNodeDelegete.  This implementation should do nothing.
    
    BUG=703369
    
    Review-Url: https://codereview.chromium.org/2759113004
    Cr-Commit-Position: refs/heads/master@{#459511}

--- content/browser/accessibility/browser_accessibility.h
+++ content/browser/accessibility/browser_accessibility.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/accessibility/ax_range.h"
 #include "ui/accessibility/ax_text_utils.h"
+#include "ui/accessibility/platform/ax_platform_node_delegate.h"
 
 // Set PLATFORM_HAS_NATIVE_ACCESSIBILITY_IMPL if this platform has
 // a platform-specific subclass of BrowserAccessibility and
@@ -65,7 +66,7 @@ class BrowserAccessibilityManager;
 // for tests.
 //
 ////////////////////////////////////////////////////////////////////////////////
-class CONTENT_EXPORT BrowserAccessibility {
+class CONTENT_EXPORT BrowserAccessibility : public ui::AXPlatformNodeDelegate {
  public:
   // Creates a platform specific BrowserAccessibility. Ownership passes to the
   // caller.
@@ -237,7 +238,6 @@ class CONTENT_EXPORT BrowserAccessibility {
   int32_t GetIndexInParent() const;
 
   int32_t GetId() const;
-  const ui::AXNodeData& GetData() const;
   gfx::RectF GetLocation() const;
   ui::AXRole GetRole() const;
   int32_t GetState() const;
@@ -365,6 +365,19 @@ class CONTENT_EXPORT BrowserAccessibility {
 
   // Gets the text offsets where new lines start.
   std::vector<int> GetLineStartOffsets() const;
+
+  // AXPlatformNodeDelegate.
+  const ui::AXNodeData& GetData() const override;
+  gfx::NativeWindow GetTopLevelWidget() override;
+  gfx::NativeViewAccessible GetParent() override;
+  int GetChildCount() override;
+  gfx::NativeViewAccessible ChildAtIndex(int index) override;
+  gfx::Vector2d GetGlobalCoordinateOffset() override;
+  gfx::NativeViewAccessible HitTestSync(int x, int y) override;
+  gfx::NativeViewAccessible GetFocus() override;
+  gfx::AcceleratedWidget GetTargetForNativeAccessibilityEvent() override;
+  bool AccessibilityPerformAction(const ui::AXActionData& data) override;
+  void DoDefaultAction() override;
 
  protected:
   using AXPlatformPositionInstance = AXPlatformPosition::AXPositionInstance;
