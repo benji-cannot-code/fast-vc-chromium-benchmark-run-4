1ec40d20294c868991104353bdf436186885e994
wkorman <wkorman@chromium.org>
2017-03-24 16:25:08 -0700

    Add operator<< to StyleDifference for debug logging.
    Similar to http://crrev.com/2732643004. I wanted this while debugging animation
    related style changes and thought it would be potentially useful for others
    ongoing.
    
    BUG=695125
    
    Review-Url: https://codereview.chromium.org/2763643003
    Cr-Commit-Position: refs/heads/master@{#459595}

--- third_party/WebKit/Source/core/style/StyleDifference.h
+++ third_party/WebKit/Source/core/style/StyleDifference.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef StyleDifference_h
 #define StyleDifference_h
 
+#include <iosfwd>
+#include "core/CoreExport.h"
 #include "wtf/Allocator.h"
 #include "wtf/Assertions.h"
 
@@ -28,6 +30,8 @@ class StyleDifference {
     ScrollAnchorDisablingPropertyChanged = 1 << 7,
     // If you add a value here, be sure to update the number of bits on
     // m_propertySpecificDifferences.
+
+    PropertyDifferenceMax = TextDecorationOrColorChanged
   };
 
   StyleDifference()
@@ -140,6 +144,9 @@ class StyleDifference {
   }
 
  private:
+  friend CORE_EXPORT std::ostream& operator<<(std::ostream&,
+                                              const StyleDifference&);
+
   enum PaintInvalidationType {
     NoPaintInvalidation,
     PaintInvalidationObject,
@@ -153,6 +160,8 @@ class StyleDifference {
   unsigned m_visualRectUpdate : 1;
   unsigned m_propertySpecificDifferences : 8;
 };
+
+CORE_EXPORT std::ostream& operator<<(std::ostream&, const StyleDifference&);
 
 }  // namespace blink
 
