e62eee9ea5aacb5806695eae9ba8a5f8a1a29872
mcasas <mcasas@chromium.org>
2017-03-23 22:47:27 -0700

    Image Capture: prune Photo{Capabilities/Settings} and add MediaTrackConstraintSet.pointsOfInterest
    - This CL removes from PhotoCapabilities.idl the fields that are
    not there anymore according to the spec [1], leaving just four
    of them.  The corresponding code and LayoutTests are updated.
    Same for PhotoSettings.idl [2].
    
    - This CL also adds ConstrainPoint2D data type [3] and uses it
    for MediaTrackConstraintSet.pointsOfInterest, wiring it down
    to the mojo interface and in the mock.
    
    - This is the one and only important change visible to the users.
    (There is an Origin Trial going on for Image Capture in
    M56-57-58, but these changes will land in 59.x.y.z, so shouldn't
    affect them, in any case an update to blink-dev@ is being
    brewed and there's a migration guide:
      https://tinyurl.com/image-capture-migration-guide
    )
    
    - No more MediaStreamTrack idl changes are expected after
    this CL \o/
    
    TEST= Layouttests etc updated.
    BUG=700607
    
    [1] https://w3c.github.io/mediacapture-image/##photocapabilities-section
    [2] https://w3c.github.io/mediacapture-image/##photosettings-section
    [3] https://w3c.github.io/mediacapture-image/#additional-constrainable-props
    
    Review-Url: https://codereview.chromium.org/2773593004
    Cr-Commit-Position: refs/heads/master@{#459367}

--- third_party/WebKit/Source/modules/imagecapture/PhotoCapabilities.cpp
+++ third_party/WebKit/Source/modules/imagecapture/PhotoCapabilities.cpp
@@ -7,41 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-namespace {
-
-String meteringModeToString(media::mojom::blink::MeteringMode mode) {
-  switch (mode) {
-    case media::mojom::blink::MeteringMode::NONE:
-      return "none";
-    case media::mojom::blink::MeteringMode::MANUAL:
-      return "manual";
-    case media::mojom::blink::MeteringMode::SINGLE_SHOT:
-      return "single-shot";
-    case media::mojom::blink::MeteringMode::CONTINUOUS:
-      return "continuous";
-    default:
-      NOTREACHED();
-  }
-  return emptyString;
-}
-
-}  // anonymous namespace
-
 // static
 PhotoCapabilities* PhotoCapabilities::create() {
   return new PhotoCapabilities();
-}
-
-String PhotoCapabilities::focusMode() const {
-  return meteringModeToString(m_focusMode);
-}
-
-String PhotoCapabilities::exposureMode() const {
-  return meteringModeToString(m_exposureMode);
-}
-
-String PhotoCapabilities::whiteBalanceMode() const {
-  return meteringModeToString(m_whiteBalanceMode);
 }
 
 String PhotoCapabilities::fillLightMode() const {
@@ -63,16 +31,8 @@ String PhotoCapabilities::fillLightMode() const {
 }
 
 DEFINE_TRACE(PhotoCapabilities) {
-  visitor->trace(m_iso);
   visitor->trace(m_imageHeight);
   visitor->trace(m_imageWidth);
-  visitor->trace(m_zoom);
-  visitor->trace(m_exposureCompensation);
-  visitor->trace(m_colorTemperature);
-  visitor->trace(m_brightness);
-  visitor->trace(m_contrast);
-  visitor->trace(m_saturation);
-  visitor->trace(m_sharpness);
 }
 
 }  // namespace blink
