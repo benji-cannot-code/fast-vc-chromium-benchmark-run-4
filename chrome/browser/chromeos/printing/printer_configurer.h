a21f261131253bbcf44f3730bb3258bb54c0e532
skau <skau@chromium.org>
2017-03-23 18:59:29 -0700

    Parse error codes from CUPS printer setup operations.
    Use the error codes from debugd specified in the system api and convert
    them to the appropriate enumerations.
    
    BUG=683241
    
    Review-Url: https://codereview.chromium.org/2744183002
    Cr-Commit-Position: refs/heads/master@{#459336}

--- chrome/browser/chromeos/printing/printer_configurer.h
+++ chrome/browser/chromeos/printing/printer_configurer.h
@@ -16,7 +16,6 @@ class Profile;
 namespace chromeos {
 
 enum PrinterSetupResult {
-  UNKNOWN,
   FATAL_ERROR,
   SUCCESS,              // Printer set up successfully
   PRINTER_UNREACHABLE,  // Could not reach printer
