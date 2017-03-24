2e765bcf5c7fe3f72d54e46f4243dc13cd125e88
stkhapugin <stkhapugin@chromium.org>
2017-03-24 06:29:17 -0700

    [ObjC ARC] Converts ios/chrome/browser/ui/bookmarks:unit_tests to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2742713007
    Cr-Commit-Position: refs/heads/master@{#459405}

--- ios/chrome/browser/ui/bookmarks/bookmark_ios_unittest.mm
+++ ios/chrome/browser/ui/bookmarks/bookmark_ios_unittest.mm
@@ -13,6 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/browser_state/test_chrome_browser_state.h"
 #include "ios/web/public/test/test_web_thread.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 using bookmarks::BookmarkNode;
 
 BookmarkIOSUnitTest::BookmarkIOSUnitTest() {}
