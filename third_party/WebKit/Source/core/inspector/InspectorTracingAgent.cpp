2021e823e17183c0995b3e25317f30d2062b7e81
toyoshim <toyoshim@chromium.org>
2017-03-23 20:28:50 -0700

    InspectorTraceAgent does not handle reloading blanket correctly
    Now, InspectorTraceAgent show reloading blanket only for
    FrameLoadTypeReload. But now FrameLoadTypeReloadMainResource and
    FrameLoadTypeReloadBypassingCache are used instead.
    
    This change modifies InspectorTracingAgent::frameStartedLoading()
    to use isReloadLoadType() with FrameLoadType to know if the started
    loading is for reload or not.
    
    BUG=674404
    
    Review-Url: https://codereview.chromium.org/2769153002
    Cr-Commit-Position: refs/heads/master@{#459354}

--- third_party/WebKit/Source/core/inspector/InspectorTracingAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorTracingAgent.cpp
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/inspector/InspectedFrames.h"
 #include "core/inspector/InspectorTraceEvents.h"
 #include "core/inspector/InspectorWorkerAgent.h"
+#include "core/loader/FrameLoader.h"
 #include "platform/instrumentation/tracing/TraceEvent.h"
 
 namespace blink {
@@ -48,7 +49,7 @@ void InspectorTracingAgent::restore() {
 
 void InspectorTracingAgent::frameStartedLoading(LocalFrame* frame,
                                                 FrameLoadType type) {
-  if (frame != m_inspectedFrames->root() || type != FrameLoadTypeReload)
+  if (frame != m_inspectedFrames->root() || !isReloadLoadType(type))
     return;
   m_client->showReloadingBlanket();
 }
