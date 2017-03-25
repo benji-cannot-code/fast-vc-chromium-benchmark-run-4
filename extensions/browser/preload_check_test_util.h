7d0bc8dbd8cfe6dcaa33f56b5f9f88b7e6f24c19
michaelpg <michaelpg@chromium.org>
2017-03-24 19:02:38 -0700

    Update ExtensionInstallChecker to use PreloadCheck classes
    Replaces some ad-hoc checks with their encapsulated equivalents.
    
    Later, the individual OnFooCheckDone methods will be eliminated too.
    
    BUG=679971
    R=rdevlin.cronin@chromium.org
    
    Review-Url: https://codereview.chromium.org/2768723002
    Cr-Commit-Position: refs/heads/master@{#459621}

--- extensions/browser/preload_check_test_util.h
+++ extensions/browser/preload_check_test_util.h
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/macros.h"
+#include "base/memory/weak_ptr.h"
+#include "base/strings/string16.h"
 #include "extensions/browser/preload_check.h"
 
 namespace base {
@@ -50,6 +53,34 @@ class PreloadCheckRunner {
   std::unique_ptr<base::RunLoop> run_loop_;
 
   DISALLOW_COPY_AND_ASSIGN(PreloadCheckRunner);
+};
+
+// Stub for a PreloadCheck that returns the desired error(s).
+class PreloadCheckStub : public PreloadCheck {
+ public:
+  PreloadCheckStub();
+  ~PreloadCheckStub() override;
+
+  void AddError(Error error);
+  void set_error_message(const base::string16& message) { message_ = message; }
+
+  bool is_async() const { return is_async_; }
+  void set_is_async(bool is_async) { is_async_ = is_async; }
+
+  // PreloadCheck:
+  void Start(ResultCallback callback) override;
+  base::string16 GetErrorMessage() const override;
+
+ private:
+  void RunCallback(ResultCallback callback);
+
+  bool is_async_ = false;
+  Errors errors_;
+  base::string16 message_;
+
+  base::WeakPtrFactory<PreloadCheckStub> weak_ptr_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(PreloadCheckStub);
 };
 
 }  // namespace extensions
