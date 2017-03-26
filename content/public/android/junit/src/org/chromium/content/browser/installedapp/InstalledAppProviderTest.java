10d22e1de70e4416f5fde2e4b71d1381574617d3
mgiuca <mgiuca@chromium.org>
2017-03-26 16:55:47 -0700

    InstalledAppProviderTest: Remove explicit "final" (requires Java 8).
    This change requires Java 8, which should now be enabled on all Android
    builders (see https://crbug.com/704013). This small change serves as a
    test to make sure we aren't building with Java 7 on any bots.
    
    Sheriffs: Please revert this if it fails to compile on Android!
    
    This takes advantage of the new "effectively final" rule in Java 8: an
    explicit "final" is not required on a captured variable as it is added
    automatically by the compiler.
    
    BUG=704013
    
    Review-Url: https://codereview.chromium.org/2768883003
    Cr-Commit-Position: refs/heads/master@{#459682}

--- content/public/android/junit/src/org/chromium/content/browser/installedapp/InstalledAppProviderTest.java
+++ content/public/android/junit/src/org/chromium/content/browser/installedapp/InstalledAppProviderTest.java
@@ -247,7 +247,7 @@ public class InstalledAppProviderTest {
      * returned.
      */
     private void verifyInstalledApps(RelatedApplication[] manifestRelatedApps,
-            final RelatedApplication[] expectedInstalledRelatedApps) {
+            RelatedApplication[] expectedInstalledRelatedApps) {
         mInstalledAppProvider.filterInstalledApps(
                 manifestRelatedApps, new InstalledAppProvider.FilterInstalledAppsResponse() {
                     @Override
