471657403e954c33f5887dc10281db3d6691c5f7
mathp <mathp@chromium.org>
2017-03-25 20:47:59 -0700

    [Payments] Return the preferred payment method name to the merchant
    In the PaymentResponse, the method_name field can vary between
    "basic-card" and the card network directly (e.g., "visa"), depending
    where the network was first specified.
    
    BUG=701952
    TEST=components_unittests
    
    Review-Url: https://codereview.chromium.org/2779453002
    Cr-Commit-Position: refs/heads/master@{#459671}

--- components/payments/content/payment_request_spec.cc
+++ components/payments/content/payment_request_spec.cc
@@ -11,10 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace payments {
 
-namespace {
-// Identifier for the basic card payment method in the PaymentMethodData.
-static const char* const kBasicCardMethodName = "basic-card";
-}  // namespace
+const char kBasicCardMethodName[] = "basic-card";
 
 PaymentRequestSpec::PaymentRequestSpec(
     mojom::PaymentOptionsPtr options,
@@ -38,6 +35,11 @@ void PaymentRequestSpec::AddObserver(Observer* observer) {
 
 void PaymentRequestSpec::RemoveObserver(Observer* observer) {
   observers_.RemoveObserver(observer);
+}
+
+bool PaymentRequestSpec::IsMethodSupportedThroughBasicCard(
+    const std::string& method_name) {
+  return basic_card_specified_networks_.count(method_name) > 0;
 }
 
 base::string16 PaymentRequestSpec::GetFormattedCurrencyAmount(
@@ -95,6 +97,8 @@ void PaymentRequestSpec::PopulateValidatedMethodData(
           supported_card_networks_.insert(supported_card_networks_.end(),
                                           card_networks.begin(),
                                           card_networks.end());
+          basic_card_specified_networks_.insert(card_networks.begin(),
+                                                card_networks.end());
           // Clear the set so that no further networks are added to
           // |supported_card_networks_|.
           card_networks.clear();
@@ -118,6 +122,8 @@ void PaymentRequestSpec::PopulateValidatedMethodData(
             auto card_it = card_networks.find(networks[supported_network]);
             if (card_it != card_networks.end()) {
               supported_card_networks_.push_back(networks[supported_network]);
+              basic_card_specified_networks_.insert(
+                  networks[supported_network]);
               card_networks.erase(card_it);
             }
           }
