c5b669312f4a17af22738095a1312eba894eb1de
thakis <thakis@chromium.org>
2017-03-25 18:33:28 -0700

    Revert of [Chromoting] Send DesktopFrame::capturer_id() to client through WebrtcVideoStream (patchset #2 id:40001 of https://codereview.chromium.org/2767193007/ )
    Reason for revert:
    Speculative, looks like this broke Webrtc_ConnectionTest.SecondCaptureFailed_0 on msan: https://build.chromium.org/p/chromium.memory.full/builders/Linux%20MSan%20Tests/builds/6646
    
    Original issue's description:
    > [Chromoting] Send DesktopFrame::capturer_id() to client through WebrtcVideoStream
    >
    > Once DirectX Capturer is enabled or being experimented, we can use the
    > capturer_id() logged in clients to analyze the failure rate.
    >
    > BUG=650926, 679523
    >
    > Review-Url: https://codereview.chromium.org/2767193007
    > Cr-Commit-Position: refs/heads/master@{#459625}
    > Committed: https://chromium.googlesource.com/chromium/src/+/f478d76ee3acd92b6a2cf5ff2e5a0e4094c756db
    
    TBR=sergeyu@chromium.org,zijiehe@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=650926, 679523
    
    Review-Url: https://codereview.chromium.org/2779483002
    Cr-Commit-Position: refs/heads/master@{#459665}

--- remoting/protocol/frame_stats.h
+++ remoting/protocol/frame_stats.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define REMOTING_PROTOCOL_FRAME_STATS_H_
 
 #include "base/time/time.h"
-#include "third_party/webrtc/modules/desktop_capture/desktop_capture_types.h"
 
 namespace remoting {
 
@@ -43,7 +42,6 @@ struct HostFrameStats {
   base::TimeDelta send_pending_delay = base::TimeDelta::Max();
   base::TimeDelta rtt_estimate = base::TimeDelta::Max();
   int bandwidth_estimate_kbps = -1;
-  uint32_t capturer_id = webrtc::DesktopCapturerId::kUnknown;
 };
 
 struct ClientFrameStats {
