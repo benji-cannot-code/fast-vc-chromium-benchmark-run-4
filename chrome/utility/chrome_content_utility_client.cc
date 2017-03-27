7a1584a1d31f1807388315c42cbabad14d5f8aa2
zhaobin <zhaobin@chromium.org>
2017-03-27 15:16:11 -0700

    [Media Router] Parse device description xml in utility process
    - Add dial_device_description_parser.mojom to support communication between browser and utility process
    - Add DialDeviceDescriptionParser in chrome/utility to handle xml parsing and scrubbing
    - Add SafeDialDeviceDescriptionParser in chrome/browser to start parsing in utility process
    
    BUG=687375
    
    Review-Url: https://codereview.chromium.org/2745653008
    Cr-Commit-Position: refs/heads/master@{#459902}

--- chrome/utility/chrome_content_utility_client.cc
+++ chrome/utility/chrome_content_utility_client.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/time/time.h"
 #include "chrome/common/file_patcher.mojom.h"
+#include "chrome/utility/media_router/dial_device_description_parser_impl.h"
 #include "chrome/utility/utility_message_handler.h"
 #include "components/safe_json/utility/safe_json_parser_mojo_impl.h"
 #include "content/public/common/content_switches.h"
@@ -294,6 +295,10 @@ void ChromeContentUtilityClient::ExposeInterfacesToBrowser(
 #if defined(FULL_SAFE_BROWSING)
   registry->AddInterface(base::Bind(&SafeArchiveAnalyzerImpl::Create));
 #endif
+#if defined(ENABLE_MEDIA_ROUTER)
+  registry->AddInterface(
+      base::Bind(&media_router::DialDeviceDescriptionParserImpl::Create));
+#endif  // defined(ENABLE_MEDIA_ROUTER)
 }
 
 // static
