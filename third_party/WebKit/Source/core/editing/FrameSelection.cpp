c6a2b47f7936fab029e284afd81f09788578ea4e
yoichio <yoichio@chromium.org>
2017-03-27 00:23:28 -0700

    Expand FrameSelection/SelectionEditor::firstRange()
    Expand firstRange() to simplify code.
    
    BUG=693975
    TEST=No change in behavior
    
    Review-Url: https://codereview.chromium.org/2776983003
    Cr-Commit-Position: refs/heads/master@{#459714}

--- third_party/WebKit/Source/core/editing/FrameSelection.cpp
+++ third_party/WebKit/Source/core/editing/FrameSelection.cpp
@@ -666,10 +666,6 @@ bool FrameSelection::setSelectedRange(const EphemeralRange& range,
   return true;
 }
 
-Range* FrameSelection::firstRange() const {
-  return m_selectionEditor->firstRange();
-}
-
 void FrameSelection::notifyAccessibilityForSelectionChange() {
   if (selectionInDOMTree().isNone())
     return;
