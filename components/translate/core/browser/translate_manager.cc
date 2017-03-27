3fc4e5cdbdadc7f36e19ae540dae0f747e0dfa93
pdyson <pdyson@chromium.org>
2017-03-26 18:27:37 -0700

    Fix counting of ULP and DISABLED_BY_PREF.
    * Move counting of DISABLED_BY_PREF to the correct place.
    * Count when translate is not used due to ULP using a new enum: LANGUAGE_IN_ULP.
    
    BUG=701608
    
    Review-Url: https://codereview.chromium.org/2757173002
    Cr-Commit-Position: refs/heads/master@{#459687}

--- components/translate/core/browser/translate_manager.cc
+++ components/translate/core/browser/translate_manager.cc
@@ -183,6 +183,7 @@ void TranslateManager::InitiateTranslation(const std::string& page_lang) {
   if (!prefs->GetBoolean(prefs::kEnableTranslate)) {
     TranslateBrowserMetrics::ReportInitiationStatus(
         TranslateBrowserMetrics::INITIATION_STATUS_DISABLED_BY_PREFS);
+    RecordTranslateEvent(metrics::TranslateEventProto::DISABLED_BY_PREF);
     const std::string& locale =
         TranslateDownloadManager::GetInstance()->application_locale();
     TranslateBrowserMetrics::ReportLocalesOnDisabledByPrefs(locale);
@@ -296,7 +297,7 @@ void TranslateManager::InitiateTranslation(const std::string& page_lang) {
   if (LanguageInULP(language_code)) {
     TranslateBrowserMetrics::ReportInitiationStatus(
         TranslateBrowserMetrics::INITIATION_STATUS_LANGUAGE_IN_ULP);
-    RecordTranslateEvent(metrics::TranslateEventProto::DISABLED_BY_PREF);
+    RecordTranslateEvent(metrics::TranslateEventProto::LANGUAGE_IN_ULP);
     return;
   }
 
