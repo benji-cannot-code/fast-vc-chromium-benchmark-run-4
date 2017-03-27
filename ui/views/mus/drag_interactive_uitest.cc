fdf1f4158ac09c75a7834d7f62589961e53c2d92
fsamuel <fsamuel@chromium.org>
2017-03-27 16:15:19 -0700

    Simplify WindowManager::OnWmSetBounds
    If a top level window requests a change in bounds, that request will get plumbed
    into the window manager. At that point, the window manager can veto the bounds
    change.
    
    This change simplifies the control flow about by having the window manager
    always responding "no" (false) to the client while also setting the window
    bounds itself based on the request.
    
    By giving a response of false to the client, it will revert the currently
    pending InFlightBoundsChange. If the bounds change initiated by the window
    manager arrives first to the client, then the inflight change's revert will
    be updated, and when the negative response arrives, it's a noop.
    
    BUG=672151
    
    Review-Url: https://codereview.chromium.org/2778823002
    Cr-Commit-Position: refs/heads/master@{#459928}

--- ui/views/mus/drag_interactive_uitest.cc
+++ ui/views/mus/drag_interactive_uitest.cc
@@ -152,7 +152,7 @@ TEST_F(DragTestInteractive, DragTest) {
   source_widget->Show();
 
   aura::test::ChangeCompletionWaiter source_waiter(
-      MusClient::Get()->window_tree_client(), aura::ChangeType::BOUNDS, true);
+      MusClient::Get()->window_tree_client(), aura::ChangeType::BOUNDS, false);
   source_widget->SetBounds(gfx::Rect(0, 0, 20, 20));
   source_waiter.Wait();
 
@@ -162,7 +162,7 @@ TEST_F(DragTestInteractive, DragTest) {
   target_widget->Show();
 
   aura::test::ChangeCompletionWaiter target_waiter(
-      MusClient::Get()->window_tree_client(), aura::ChangeType::BOUNDS, true);
+      MusClient::Get()->window_tree_client(), aura::ChangeType::BOUNDS, false);
   target_widget->SetBounds(gfx::Rect(20, 20, 20, 20));
   target_waiter.Wait();
 
