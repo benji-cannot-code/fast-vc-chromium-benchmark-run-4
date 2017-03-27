0b6548a2afca1b93ed86f6707809e1c4190bf767
fsamuel <fsamuel@chromium.org>
2017-03-27 10:21:47 -0700

    renderer-mus: Push LocalSurfaceId into RenderWidgetCompositor
    With surface synchronization enabled, when the browser updates the renderer's
    bounds, the window server tells the renderer via RendererWindowTreeClient about
    the new bounds and the LocalSurfaceId to use to submit a CompositorFrame.
    
    This CL propagates the LocalSurfaceId to the RenderWidgetCompositor from
    RendererWindowTreeClient. Note that this does not yet support OOPIFs.
    
    BUG=672962
    
    Review-Url: https://codereview.chromium.org/2773243002
    Cr-Commit-Position: refs/heads/master@{#459814}

--- content/renderer/mus/renderer_window_tree_client.h
+++ content/renderer/mus/renderer_window_tree_client.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "mojo/public/cpp/bindings/binding.h"
 #include "services/ui/common/types.h"
+#include "services/ui/public/cpp/client_compositor_frame_sink.h"
 #include "services/ui/public/interfaces/window_tree.mojom.h"
 
 namespace cc {
@@ -21,6 +22,8 @@ class GpuMemoryBufferManager;
 }
 
 namespace content {
+
+class RenderWidget;
 
 // ui.mojom.WindowTreeClient implementation for RenderWidget. This lives and
 // operates on the renderer's main thread.
@@ -39,6 +42,10 @@ class RendererWindowTreeClient : public ui::mojom::WindowTreeClient {
   // nullptr if none exists.
   static RendererWindowTreeClient* Get(int routing_id);
 
+  const cc::LocalSurfaceId& local_surface_id() const {
+    return current_local_surface_id_;
+  }
+
   void Bind(ui::mojom::WindowTreeClientRequest request);
 
   using CompositorFrameSinkCallback =
@@ -56,6 +63,8 @@ class RendererWindowTreeClient : public ui::mojom::WindowTreeClient {
       scoped_refptr<cc::ContextProvider> context_provider,
       gpu::GpuMemoryBufferManager* gpu_memory_buffer_manager,
       const CompositorFrameSinkCallback& callback);
+
+  RenderWidget* GetRenderWidgetFromRoutingId(int routing_id);
 
   void DestroySelf();
 
@@ -158,10 +167,12 @@ class RendererWindowTreeClient : public ui::mojom::WindowTreeClient {
   const int routing_id_;
   ui::Id root_window_id_;
   cc::FrameSinkId frame_sink_id_;
+  bool enable_surface_synchronization_ = false;
   scoped_refptr<cc::ContextProvider> pending_context_provider_;
   gpu::GpuMemoryBufferManager* pending_gpu_memory_buffer_manager_ = nullptr;
   CompositorFrameSinkCallback pending_compositor_frame_sink_callback_;
   ui::mojom::WindowTreePtr tree_;
+  cc::LocalSurfaceId current_local_surface_id_;
   mojo::Binding<ui::mojom::WindowTreeClient> binding_;
 
   DISALLOW_COPY_AND_ASSIGN(RendererWindowTreeClient);
