814855032e47cc8669b25ab422d978b614c3705c
mkwst <mkwst@chromium.org>
2017-03-27 07:50:10 -0700

    Enable blocking of subresource requests whose URLs include credentials.
    This patch flips the 'BlockCredentialedSubresources' flag to 'stable', and
    ties it to a feature flag in //content that we can use as a kill switch if
    it turns out that enterprise usage of the feature is higher than we hope
    (the overall numbers still look reasonably low[1]).
    
    Intent: https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/lx-U_JR2BF0
    
    [1]: https://www.chromestatus.com/metrics/feature/timeline/popularity/532
    
    BUG=504300,435547
    
    Review-Url: https://codereview.chromium.org/2779603002
    Cr-Commit-Position: refs/heads/master@{#459781}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -367,6 +367,11 @@ void SetRuntimeFeaturesDefaultsAndUpdateFromArgs(
   if (base::FeatureList::IsEnabled(features::kLoadingWithMojo))
     WebRuntimeFeatures::enableLoadingWithMojo(true);
 
+  if (!base::FeatureList::IsEnabled(features::kBlockCredentialedSubresources)) {
+    WebRuntimeFeatures::enableFeatureFromString("BlockCredentialedSubresources",
+                                                false);
+  }
+
   // Enable explicitly enabled features, and then disable explicitly disabled
   // ones.
   if (command_line.HasSwitch(switches::kEnableBlinkFeatures)) {
