4aa881e5db2f0273325a792b3b42abed7abbe408
tkent <tkent@chromium.org>
2017-03-26 20:47:21 -0700

    Form validation: Validation bubble should be closed on document unload process.
    This CL fixes a bug that a validation bubble is not closed by page navigation in
    some cases.
    
    We close a validation message on Page::documentDetached(). However it seems it was
    too late to communicate with the browser process in some cases. So, this CL moves
    it to Document unload timing.
    
     * Add ValidationMessage::willUnloadDocument(), which closes a validation bubble,
      and Document::dispatchUnloadEvents() calls it indirectly through Page.
    
     * HTMLFormControlElement prevents from showing a validation message after the
      unload processing.
    
    BUG=704560
    
    Review-Url: https://codereview.chromium.org/2771193002
    Cr-Commit-Position: refs/heads/master@{#459701}

--- third_party/WebKit/Source/core/page/Page.h
+++ third_party/WebKit/Source/core/page/Page.h
@@ -155,6 +155,7 @@ class CORE_EXPORT Page final : public GarbageCollectedFinalized<Page>,
     return toLocalFrame(m_mainFrame);
   }
 
+  void willUnloadDocument(const Document&);
   void documentDetached(Document*);
 
   bool openedByDOM() const;
--- third_party/WebKit/Source/web/ValidationMessageClientImpl.h
+++ third_party/WebKit/Source/web/ValidationMessageClientImpl.h
@@ -61,6 +61,7 @@ class ValidationMessageClientImpl final
                              TextDirection subMessageDir) override;
   void hideValidationMessage(const Element& anchor) override;
   bool isValidationMessageVisible(const Element& anchor) override;
+  void willUnloadDocument(const Document&) override;
   void documentDetached(const Document&) override;
   void willBeDestroyed() override;
 
