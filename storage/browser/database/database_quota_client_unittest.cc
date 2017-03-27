13b00297635f621f3a846eb41714fa9442e2b88d
pwnall <pwnall@chromium.org>
2017-03-27 16:48:19 -0700

    Move some database tests next to the files they cover.
    BUG=653751
    
    Review-Url: https://codereview.chromium.org/2776253002
    Cr-Commit-Position: refs/heads/master@{#459941}

--- storage/browser/database/database_quota_client_unittest.cc
+++ storage/browser/database/database_quota_client_unittest.cc
@@ -55,8 +55,7 @@ class MockDatabaseTracker : public DatabaseTracker {
   bool GetAllOriginIdentifiers(
       std::vector<std::string>* origins_identifiers) override {
     std::map<GURL, MockOriginInfo>::const_iterator iter;
-    for (iter = mock_origin_infos_.begin();
-         iter != mock_origin_infos_.end();
+    for (iter = mock_origin_infos_.begin(); iter != mock_origin_infos_.end();
          ++iter) {
       origins_identifiers->push_back(iter->second.GetOriginIdentifier());
     }
@@ -65,8 +64,7 @@ class MockDatabaseTracker : public DatabaseTracker {
 
   bool GetAllOriginsInfo(std::vector<OriginInfo>* origins_info) override {
     std::map<GURL, MockOriginInfo>::const_iterator iter;
-    for (iter = mock_origin_infos_.begin();
-         iter != mock_origin_infos_.end();
+    for (iter = mock_origin_infos_.begin(); iter != mock_origin_infos_.end();
          ++iter) {
       origins_info->push_back(OriginInfo(iter->second));
     }
@@ -89,7 +87,7 @@ class MockDatabaseTracker : public DatabaseTracker {
     callback.Run(net::OK);
   }
 
-  void AddMockDatabase(const GURL& origin,  const char* name, int size) {
+  void AddMockDatabase(const GURL& origin, const char* name, int size) {
     MockOriginInfo& info = mock_origin_infos_[origin];
     info.set_origin(storage::GetIdentifierFromOrigin(origin));
     info.AddMockDatabase(base::ASCIIToUTF16(name), size);
@@ -121,7 +119,6 @@ class MockDatabaseTracker : public DatabaseTracker {
   std::map<GURL, MockOriginInfo> mock_origin_infos_;
 };
 
-
 // Base class for our test fixtures.
 class DatabaseQuotaClientTest : public testing::Test {
  public:
@@ -135,8 +132,7 @@ class DatabaseQuotaClientTest : public testing::Test {
         kOriginOther("http://other"),
         usage_(0),
         mock_tracker_(new MockDatabaseTracker),
-        weak_factory_(this) {
-  }
+        weak_factory_(this) {}
 
   int64_t GetOriginUsage(storage::QuotaClient* client,
                          const GURL& origin,
@@ -154,9 +150,8 @@ class DatabaseQuotaClientTest : public testing::Test {
                                           storage::StorageType type) {
     origins_.clear();
     client->GetOriginsForType(
-        type,
-        base::Bind(&DatabaseQuotaClientTest::OnGetOriginsComplete,
-                   weak_factory_.GetWeakPtr()));
+        type, base::Bind(&DatabaseQuotaClientTest::OnGetOriginsComplete,
+                         weak_factory_.GetWeakPtr()));
     base::RunLoop().RunUntilIdle();
     return origins_;
   }
@@ -187,7 +182,6 @@ class DatabaseQuotaClientTest : public testing::Test {
 
   MockDatabaseTracker* mock_tracker() { return mock_tracker_.get(); }
 
-
  private:
   void OnGetOriginUsageComplete(int64_t usage) { usage_ = usage; }
 
@@ -206,7 +200,6 @@ class DatabaseQuotaClientTest : public testing::Test {
   scoped_refptr<MockDatabaseTracker> mock_tracker_;
   base::WeakPtrFactory<DatabaseQuotaClientTest> weak_factory_;
 };
-
 
 TEST_F(DatabaseQuotaClientTest, GetOriginUsage) {
   DatabaseQuotaClient client(base::ThreadTaskRunnerHandle::Get().get(),
--- storage/browser/database/database_util_unittest.cc
+++ storage/browser/database/database_util_unittest.cc
@@ -3,9 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "storage/browser/database/database_util.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/utf_string_conversions.h"
-#include "storage/browser/database/database_util.h"
 #include "storage/common/database/database_identifier.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -22,8 +22,7 @@ static void TestVfsFilePath(bool expected_result,
   base::string16 sqlite_suffix;
   EXPECT_EQ(expected_result,
             DatabaseUtil::CrackVfsFileName(ASCIIToUTF16(vfs_file_name),
-                                           &origin_identifier,
-                                           &database_name,
+                                           &origin_identifier, &database_name,
                                            &sqlite_suffix));
   EXPECT_EQ(expected_origin_identifier, origin_identifier);
   EXPECT_EQ(ASCIIToUTF16(expected_database_name), database_name);
@@ -35,12 +34,11 @@ namespace content {
 // Test DatabaseUtil::CrackVfsFilePath on various inputs.
 TEST(DatabaseUtilTest, CrackVfsFilePathTest) {
   TestVfsFilePath(true, "http_origin_0/#", "http_origin_0", "", "");
-  TestVfsFilePath(true,
-      "http_origin_0/#suffix", "http_origin_0", "", "suffix");
-  TestVfsFilePath(true,
-      "http_origin_0/db_name#", "http_origin_0", "db_name", "");
-  TestVfsFilePath(true,
-      "http_origin_0/db_name#suffix", "http_origin_0", "db_name", "suffix");
+  TestVfsFilePath(true, "http_origin_0/#suffix", "http_origin_0", "", "suffix");
+  TestVfsFilePath(true, "http_origin_0/db_name#", "http_origin_0", "db_name",
+                  "");
+  TestVfsFilePath(true, "http_origin_0/db_name#suffix", "http_origin_0",
+                  "db_name", "suffix");
   TestVfsFilePath(false, "http_origin_0db_name#");
   TestVfsFilePath(false, "http_origin_0db_name#suffix");
   TestVfsFilePath(false, "http_origin_0/db_name");
@@ -48,6 +46,5 @@ TEST(DatabaseUtilTest, CrackVfsFilePathTest) {
   TestVfsFilePath(false, "/db_name#");
   TestVfsFilePath(false, "/db_name#suffix");
 }
-
 
 }  // namespace content
