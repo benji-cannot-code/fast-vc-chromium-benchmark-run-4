0de2aa20edef31de8a46e75ec5af9b9061ab5abc
awdf <awdf@chromium.org>
2017-03-27 05:54:12 -0700

    Android: Check intent action in broadcast receiver for package replaced
    - no longer need to suppress lint
    
    BUG=635567
    
    Review-Url: https://codereview.chromium.org/2778763002
    Cr-Commit-Position: refs/heads/master@{#459763}

--- chrome/android/java/src/org/chromium/chrome/browser/upgrade/PackageReplacedBroadcastReceiver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/upgrade/PackageReplacedBroadcastReceiver.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.upgrade;
 
-import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -25,11 +24,10 @@ import android.os.Build;
  * - This class immediately cullable by Android as soon as {@link #onReceive} returns. To kick off
  *   longer tasks, you must start a Service.
  */
-// TODO(crbug.com/635567): Fix this properly.
-@SuppressLint("UnsafeProtectedBroadcastReceiver")
 public final class PackageReplacedBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        if (!Intent.ACTION_MY_PACKAGE_REPLACED.equals(intent.getAction())) return;
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.N) return;
         UpgradeIntentService.startMigrationIfNecessary(context);
     }
