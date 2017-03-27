2475b7977fcf783c8f9fa4a8a8421879596a6bf1
alexilin <alexilin@chromium.org>
2017-03-27 08:03:30 -0700

    predictors: Fix incorrect histogram name.
    Add Experimental suffix for FirstMeaningfulPaint histogram.
    histograms.xml already contains correct name.
    
    BUG=680049
    
    Review-Url: https://codereview.chromium.org/2775243002
    Cr-Commit-Position: refs/heads/master@{#459783}

--- chrome/browser/page_load_metrics/observers/resource_prefetch_predictor_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/resource_prefetch_predictor_page_load_metrics_observer.cc
@@ -17,7 +17,7 @@ const char kHistogramResourcePrefetchPredictorFirstContentfulPaint[] =
     "PageLoad.Clients.ResourcePrefetchPredictor.PaintTiming."
     "NavigationToFirstContentfulPaint.Prefetchable";
 const char kHistogramResourcePrefetchPredictorFirstMeaningfulPaint[] =
-    "PageLoad.Clients.ResourcePrefetchPredictor.PaintTiming."
+    "PageLoad.Clients.ResourcePrefetchPredictor.Experimental.PaintTiming."
     "NavigationToFirstMeaningfulPaint.Prefetchable";
 
 }  // namespace internal
