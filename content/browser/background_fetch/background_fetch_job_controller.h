f70f9d975c4763053b686b593635cbf8775d44be
harkness <harkness@chromium.org>
2017-03-27 09:15:09 -0700

    Implement GetJobResponse and merge JobData into DataManager.
    This CL has two major changes. The first is the addition of the
    GetJobResponse call on the DataManager. This Creates a new (and likely
    temporary) BackgroundFetchJobResponseData which is owned by the JobInfo
    and exists to collect the information needed to construct the Response
    that will eventually be sent to Mojo.
    
    The second major change is merging the JobData back into the DataManager.
    The JobData was always intended as a cache layer to assist in fetches
    involving large numbers of files in a batch, but given the desire to
    reduce the memory footprint of Chrome and the relative rarity of these
    operations, the cache layer is being removed in favor of storage accesses.
    
    Minor changes:
    * More attempts to isolate the eventual storage interface.
    * Methods on the JobData which were moved to the DataManager are now virtual
      for testing.
    * DataManager takes a BrowserContext* instead of BackgroundFetchContext*.
    * Much of the batch management which was in JobData is now handled in
      BackgroundFetchJobInfo.
    
    BUG=692621
    
    Review-Url: https://codereview.chromium.org/2767373002
    Cr-Commit-Position: refs/heads/master@{#459793}

--- content/browser/background_fetch/background_fetch_job_controller.h
+++ content/browser/background_fetch/background_fetch_job_controller.h
@@ -18,21 +18,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-class BackgroundFetchJobData;
+class BackgroundFetchDataManager;
 class BackgroundFetchRequestInfo;
 class BrowserContext;
 class StoragePartition;
 
 // The JobController will be responsible for coordinating communication with the
-// DownloadManager. It will get requests from the JobData and dispatch them to
-// the DownloadManager. It lives entirely on the IO thread.
+// DownloadManager. It will get requests from the DataManager and dispatch them
+// to the DownloadManager. It lives entirely on the IO thread.
 class CONTENT_EXPORT BackgroundFetchJobController
     : public DownloadItem::Observer {
  public:
   BackgroundFetchJobController(const std::string& job_guid,
                                BrowserContext* browser_context,
                                StoragePartition* storage_partition,
-                               std::unique_ptr<BackgroundFetchJobData> job_data,
+                               BackgroundFetchDataManager* data_manager,
                                base::OnceClosure completed_closure);
   ~BackgroundFetchJobController() override;
 
@@ -56,6 +56,8 @@ class CONTENT_EXPORT BackgroundFetchJobController
 
   void ProcessRequest(const BackgroundFetchRequestInfo& request);
 
+  std::string job_guid_;
+
   // Pointer to the browser context. The BackgroundFetchJobController is owned
   // by the BrowserContext via the StoragePartition.
   // TODO(harkness): Currently this is only used to lookup the DownloadManager.
@@ -66,8 +68,9 @@ class CONTENT_EXPORT BackgroundFetchJobController
   // (through a sequence of other classes).
   StoragePartition* storage_partition_;
 
-  // The JobData which talks to the DataManager for this job_guid.
-  std::unique_ptr<BackgroundFetchJobData> job_data_;
+  // The DataManager's lifetime is controlled by the BackgroundFetchContext and
+  // will be kept alive until after the JobController is destroyed.
+  BackgroundFetchDataManager* data_manager_;
 
   // Callback for when all fetches have been completed.
   base::OnceClosure completed_closure_;
--- content/browser/background_fetch/background_fetch_job_info.cc
+++ content/browser/background_fetch/background_fetch_job_info.cc
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/guid.h"
 
+#include "content/browser/background_fetch/background_fetch_job_response_data.h"
+#include "content/browser/background_fetch/background_fetch_request_info.h"
+
 namespace content {
 
 BackgroundFetchJobInfo::BackgroundFetchJobInfo(
@@ -19,5 +22,44 @@ BackgroundFetchJobInfo::BackgroundFetchJobInfo(
       service_worker_registration_id_(service_worker_registration_id) {}
 
 BackgroundFetchJobInfo::~BackgroundFetchJobInfo() = default;
+
+void BackgroundFetchJobInfo::set_job_response_data(
+    std::unique_ptr<BackgroundFetchJobResponseData> job_response_data) {
+  job_response_data_ = std::move(job_response_data);
+}
+
+bool BackgroundFetchJobInfo::IsComplete() const {
+  return !HasRequestsRemaining() && active_requests_.empty();
+}
+
+bool BackgroundFetchJobInfo::HasRequestsRemaining() const {
+  return next_request_index_ != num_requests_;
+}
+
+void BackgroundFetchJobInfo::AddActiveRequest(
+    std::unique_ptr<BackgroundFetchRequestInfo> request) {
+  active_requests_.insert(std::move(request));
+  next_request_index_++;
+}
+
+BackgroundFetchRequestInfo* BackgroundFetchJobInfo::GetActiveRequest(
+    const std::string& request_guid) const {
+  for (auto& request : active_requests_) {
+    if (request->guid() == request_guid)
+      return request.get();
+  }
+  return nullptr;
+}
+
+void BackgroundFetchJobInfo::RemoveActiveRequest(
+    const std::string& request_guid) {
+  // active_requests_.erase(request_guid);
+  for (const auto& request : active_requests_) {
+    if (request->guid() == request_guid) {
+      active_requests_.erase(request);
+      return;
+    }
+  }
+}
 
 }  // namespace content
