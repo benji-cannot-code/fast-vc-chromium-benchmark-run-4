af96131bd5e37b1fa632c835b2974c114bfbd372
rune <rune@opera.com>
2017-03-27 02:37:53 -0700

    Update :in-range/:out-of-range when steppable min/max/value changes.
    We called pseudoStateChanged in a lot of cases where it wasn't
    necessary. It should suffice to call it for the mentioned pseudo
    classes when the min and max attributes changes, or the value is
    changed, either as an attribute or as a result of user input.
    
    This should fix the performance regressions in issue 704775.
    
    R=tkent@chromium.org
    BUG=704775
    
    Review-Url: https://codereview.chromium.org/2774723004
    Cr-Commit-Position: refs/heads/master@{#459733}

--- third_party/WebKit/Source/core/html/HTMLInputElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLInputElement.cpp
@@ -727,6 +727,7 @@ void HTMLInputElement::parseAttribute(
     setNeedsValidityCheck();
     m_valueAttributeWasUpdatedAfterParsing = !m_parsingInProgress;
     m_inputType->warnIfValueIsInvalidAndElementIsVisible(value);
+    m_inputType->inRangeChanged();
     m_inputTypeView->valueAttributeChanged();
   } else if (name == checkedAttr) {
     // Another radio button in the same group might be checked by state
@@ -771,11 +772,13 @@ void HTMLInputElement::parseAttribute(
   } else if (name == minAttr) {
     m_inputTypeView->minOrMaxAttributeChanged();
     m_inputType->sanitizeValueInResponseToMinOrMaxAttributeChange();
+    m_inputType->inRangeChanged();
     setNeedsValidityCheck();
     UseCounter::count(document(), UseCounter::MinAttribute);
   } else if (name == maxAttr) {
     m_inputTypeView->minOrMaxAttributeChanged();
     m_inputType->sanitizeValueInResponseToMinOrMaxAttributeChange();
+    m_inputType->inRangeChanged();
     setNeedsValidityCheck();
     UseCounter::count(document(), UseCounter::MaxAttribute);
   } else if (name == multipleAttr) {
@@ -1100,10 +1103,7 @@ void HTMLInputElement::setNonAttributeValue(const String& sanitizedValue) {
   m_nonAttributeValue = sanitizedValue;
   m_hasDirtyValue = true;
   setNeedsValidityCheck();
-  if (m_inputType->isSteppable()) {
-    pseudoStateChanged(CSSSelector::PseudoInRange);
-    pseudoStateChanged(CSSSelector::PseudoOutOfRange);
-  }
+  m_inputType->inRangeChanged();
 }
 
 void HTMLInputElement::setNonDirtyValue(const String& newValue) {
