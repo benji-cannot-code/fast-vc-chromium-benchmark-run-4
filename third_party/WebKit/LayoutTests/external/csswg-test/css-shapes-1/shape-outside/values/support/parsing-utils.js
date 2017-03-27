1303d0237d16d9a4a7cd2ce51c17fb32b6b1f5e7
Blink W3C Test Autoroller <blink-w3c-test-autoroller@chromium.org>
2017-03-27 07:17:55 -0700

    Import csswg-test@f9ba2271263c38d4e2091af0711c95796ec0cca1
    Using wpt-import in Chromium 749433b28d0875dfce9194211f6345f45fa6cea5.
    
    Build: https://build.chromium.org/p/chromium.infra.cron/builders/w3c-test-autoroller/builds/8063
    
    Directory owners for changes in this CL:
    bjonesbe@adobe.com:
      external/csswg-test/css-shapes-1
    cbiesinger@chromium.org:
      external/csswg-test/css-flexbox-1
    rego@igalia.com:
      external/csswg-test/css-ui-3
    
    TBR=qyearsley@chromium.org
    NOEXPORT=true
    
    Change-Id: I8a899c6ae978d10fd69846c6dca9e1eb40e4747d
    Reviewed-on: https://chromium-review.googlesource.com/459041
    Reviewed-by: Blink W3C Test Autoroller <blink-w3c-test-autoroller@chromium.org>
    Commit-Queue: Blink W3C Test Autoroller <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#459786}

--- third_party/WebKit/LayoutTests/external/csswg-test/css-shapes-1/shape-outside/values/support/parsing-utils.js
+++ third_party/WebKit/LayoutTests/external/csswg-test/css-shapes-1/shape-outside/values/support/parsing-utils.js
@@ -117,7 +117,7 @@ function buildTestCases(testCases, testType) {
 
         // expected
         if( type.indexOf('invalid') != -1 ){
-            oneTestCase.push(null)
+            oneTestCase.push("")
         } else if( type == 'inline' ) {
             oneTestCase.push(test['expected_inline']);
         } else if( type == 'computed' ){
@@ -175,7 +175,7 @@ function buildPositionTests(shape, valid, type, units) {
                 testCase = new Array();
                 testCase.push(testValue + ' is invalid');
                 testCase.push(testValue);
-                testCase.push(null);
+                testCase.push("");
                 results.push(testCase);
             });
         }
