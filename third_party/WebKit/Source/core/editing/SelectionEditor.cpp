7738658f2c3dd324e81ed1dee906447b9bffa53b
yosin <yosin@chromium.org>
2017-03-27 18:22:47 -0700

    Split visible selection cache in SelectionEditor into DOM tree and flat tree
    This patch splits visible selection cache in |SelectionEditor| into DOM tree
    version and flat tree version to avoid redundant computation of visible
    selection for *small* performance improvement, e.g. visible selection reference
    in execCommand implementation requires only DOM tree visible selection, or
    selection change by mouse requires only flat tree visible selection.
    
    BUG=n/a
    TEST=no behavior changes
    
    Review-Url: https://codereview.chromium.org/2771423002
    Cr-Commit-Position: refs/heads/master@{#459966}

--- third_party/WebKit/Source/core/editing/SelectionEditor.cpp
+++ third_party/WebKit/Source/core/editing/SelectionEditor.cpp
@@ -55,7 +55,8 @@ void SelectionEditor::clearVisibleSelection() {
   m_selection = SelectionInDOMTree();
   m_cachedVisibleSelectionInDOMTree = VisibleSelection();
   m_cachedVisibleSelectionInFlatTree = VisibleSelectionInFlatTree();
-  m_cacheIsDirty = false;
+  m_cachedVisibleSelectionInDOMTreeIsDirty = false;
+  m_cachedVisibleSelectionInFlatTreeIsDirty = false;
   if (!shouldAlwaysUseDirectionalSelection())
     return;
   m_selection.m_isDirectional = true;
@@ -86,7 +87,7 @@ const VisibleSelectionInFlatTree&
 SelectionEditor::computeVisibleSelectionInFlatTree() const {
   DCHECK_EQ(frame()->document(), document());
   DCHECK_EQ(frame(), document().frame());
-  updateCachedVisibleSelectionIfNeeded();
+  updateCachedVisibleSelectionInFlatTreeIfNeeded();
   if (m_cachedVisibleSelectionInFlatTree.isNone())
     return m_cachedVisibleSelectionInFlatTree;
   DCHECK_EQ(m_cachedVisibleSelectionInFlatTree.base().document(), document());
@@ -111,11 +112,14 @@ bool SelectionEditor::isContentRichlyEditable() const {
 }
 
 void SelectionEditor::markCacheDirty() {
-  if (m_cacheIsDirty)
-    return;
-  m_cachedVisibleSelectionInFlatTree = VisibleSelectionInFlatTree();
-  m_cachedVisibleSelectionInDOMTree = VisibleSelection();
-  m_cacheIsDirty = true;
+  if (!m_cachedVisibleSelectionInDOMTreeIsDirty) {
+    m_cachedVisibleSelectionInDOMTree = VisibleSelection();
+    m_cachedVisibleSelectionInDOMTreeIsDirty = true;
+  }
+  if (!m_cachedVisibleSelectionInFlatTreeIsDirty) {
+    m_cachedVisibleSelectionInFlatTree = VisibleSelectionInFlatTree();
+    m_cachedVisibleSelectionInFlatTreeIsDirty = true;
+  }
 }
 
 void SelectionEditor::setSelection(const SelectionInDOMTree& newSelection) {
@@ -151,18 +155,21 @@ void SelectionEditor::didFinishDOMMutation() {
 void SelectionEditor::documentAttached(Document* document) {
   DCHECK(document);
   DCHECK(!lifecycleContext()) << lifecycleContext();
-  m_styleVersion = static_cast<uint64_t>(-1);
+  m_styleVersionForDOMTree = static_cast<uint64_t>(-1);
+  m_styleVersionForFlatTree = static_cast<uint64_t>(-1);
   clearVisibleSelection();
   setContext(document);
 }
 
 void SelectionEditor::contextDestroyed(Document*) {
   dispose();
-  m_styleVersion = static_cast<uint64_t>(-1);
+  m_styleVersionForDOMTree = static_cast<uint64_t>(-1);
+  m_styleVersionForFlatTree = static_cast<uint64_t>(-1);
   m_selection = SelectionInDOMTree();
   m_cachedVisibleSelectionInDOMTree = VisibleSelection();
   m_cachedVisibleSelectionInFlatTree = VisibleSelectionInFlatTree();
-  m_cacheIsDirty = false;
+  m_cachedVisibleSelectionInDOMTreeIsDirty = false;
+  m_cachedVisibleSelectionInFlatTreeIsDirty = false;
 }
 
 static Position computePositionForChildrenRemoval(const Position& position,
@@ -354,7 +361,8 @@ bool SelectionEditor::shouldAlwaysUseDirectionalSelection() const {
 }
 
 bool SelectionEditor::needsUpdateVisibleSelection() const {
-  return m_cacheIsDirty || m_styleVersion != document().styleVersion();
+  return m_cachedVisibleSelectionInDOMTreeIsDirty ||
+         m_styleVersionForDOMTree != document().styleVersion();
 }
 
 void SelectionEditor::updateCachedVisibleSelectionIfNeeded() const {
@@ -366,14 +374,32 @@ void SelectionEditor::updateCachedVisibleSelectionIfNeeded() const {
   assertSelectionValid();
   if (!needsUpdateVisibleSelection())
     return;
-
-  m_styleVersion = document().styleVersion();
-  m_cacheIsDirty = false;
+  m_styleVersionForDOMTree = document().styleVersion();
+  m_cachedVisibleSelectionInDOMTreeIsDirty = false;
   m_cachedVisibleSelectionInDOMTree = createVisibleSelection(m_selection);
-  if (m_cachedVisibleSelectionInDOMTree.isNone()) {
-    m_cachedVisibleSelectionInFlatTree = VisibleSelectionInFlatTree();
+  if (!m_cachedVisibleSelectionInDOMTree.isNone())
     return;
-  }
+  m_styleVersionForFlatTree = document().styleVersion();
+  m_cachedVisibleSelectionInFlatTreeIsDirty = false;
+  m_cachedVisibleSelectionInFlatTree = VisibleSelectionInFlatTree();
+}
+
+bool SelectionEditor::needsUpdateVisibleSelectionInFlatTree() const {
+  return m_cachedVisibleSelectionInFlatTreeIsDirty ||
+         m_styleVersionForFlatTree != document().styleVersion();
+}
+
+void SelectionEditor::updateCachedVisibleSelectionInFlatTreeIfNeeded() const {
+  // Note: Since we |FrameCaret::updateApperance()| is called from
+  // |FrameView::performPostLayoutTasks()|, we check lifecycle against
+  // |AfterPerformLayout| instead of |LayoutClean|.
+  DCHECK_GE(document().lifecycle().state(),
+            DocumentLifecycle::AfterPerformLayout);
+  assertSelectionValid();
+  if (!needsUpdateVisibleSelectionInFlatTree())
+    return;
+  m_styleVersionForFlatTree = document().styleVersion();
+  m_cachedVisibleSelectionInFlatTreeIsDirty = false;
   m_cachedVisibleSelectionInFlatTree = createVisibleSelection(
       SelectionInFlatTree::Builder()
           .setBaseAndExtent(toPositionInFlatTree(m_selection.base()),
@@ -383,6 +409,11 @@ void SelectionEditor::updateCachedVisibleSelectionIfNeeded() const {
           .setGranularity(m_selection.granularity())
           .setIsDirectional(m_selection.isDirectional())
           .build());
+  if (!m_cachedVisibleSelectionInFlatTree.isNone())
+    return;
+  m_styleVersionForDOMTree = document().styleVersion();
+  m_cachedVisibleSelectionInDOMTreeIsDirty = false;
+  m_cachedVisibleSelectionInDOMTree = VisibleSelection();
 }
 
 void SelectionEditor::cacheRangeOfDocument(Range* range) {
