24e4fe09ee65a5ccc999bb324068ac03a9cad76b
enne <enne@chromium.org>
2017-03-28 07:25:42 -0700

    cc: Make DisplayItemList unittest emit pngs on failure
    It's hard to tell what's going wrong in this test if hypothetically you
    are making these tests fail.  This patch changes them spit out data urls
    to visualize actual and expected results instead of just memcmping.
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2777263002
    Cr-Commit-Position: refs/heads/master@{#460099}

--- cc/paint/display_item_list_unittest.cc
+++ cc/paint/display_item_list_unittest.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/paint/paint_surface.h"
 #include "cc/paint/transform_display_item.h"
 #include "cc/test/geometry_test_utils.h"
+#include "cc/test/pixel_test_utils.h"
 #include "cc/test/skia_common.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -41,6 +42,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace cc {
 
 namespace {
+
+bool CompareN32Pixels(void* actual_pixels,
+                      void* expected_pixels,
+                      int width,
+                      int height) {
+  if (memcmp(actual_pixels, expected_pixels, 4 * width * height) == 0)
+    return true;
+
+  SkImageInfo actual_info = SkImageInfo::MakeN32Premul(width, height);
+  SkBitmap actual_bitmap;
+  actual_bitmap.installPixels(actual_info, actual_pixels,
+                              actual_info.minRowBytes());
+
+  SkImageInfo expected_info = SkImageInfo::MakeN32Premul(width, height);
+  SkBitmap expected_bitmap;
+  expected_bitmap.installPixels(expected_info, expected_pixels,
+                                expected_info.minRowBytes());
+
+  std::string gen_bmp_data_url = GetPNGDataUrl(actual_bitmap);
+  std::string ref_bmp_data_url = GetPNGDataUrl(expected_bitmap);
+
+  LOG(ERROR) << "Pixels do not match!";
+  LOG(ERROR) << "Actual: " << gen_bmp_data_url;
+  LOG(ERROR) << "Expected: " << ref_bmp_data_url;
+  return false;
+}
 
 const gfx::Rect kVisualRect(0, 0, 42, 42);
 
@@ -110,7 +137,7 @@ TEST(DisplayItemListTest, SingleDrawingItem) {
                        75.f + offset.y()),
       blue_flags);
 
-  EXPECT_EQ(0, memcmp(pixels, expected_pixels, 4 * 100 * 100));
+  EXPECT_TRUE(CompareN32Pixels(pixels, expected_pixels, 100, 100));
 }
 
 TEST(DisplayItemListTest, ClipItem) {
@@ -167,7 +194,7 @@ TEST(DisplayItemListTest, ClipItem) {
                        75.f + second_offset.x(), 75.f + second_offset.y()),
       blue_flags);
 
-  EXPECT_EQ(0, memcmp(pixels, expected_pixels, 4 * 100 * 100));
+  EXPECT_TRUE(CompareN32Pixels(pixels, expected_pixels, 100, 100));
 }
 
 TEST(DisplayItemListTest, TransformItem) {
@@ -224,7 +251,7 @@ TEST(DisplayItemListTest, TransformItem) {
                        75.f + second_offset.x(), 75.f + second_offset.y()),
       blue_flags);
 
-  EXPECT_EQ(0, memcmp(pixels, expected_pixels, 4 * 100 * 100));
+  EXPECT_TRUE(CompareN32Pixels(pixels, expected_pixels, 100, 100));
 }
 
 TEST(DisplayItemListTest, FilterItem) {
@@ -288,7 +315,7 @@ TEST(DisplayItemListTest, FilterItem) {
   SkiaPaintCanvas expected_canvas(expected_bitmap);
   expected_canvas.drawRect(RectFToSkRect(filter_bounds), paint);
 
-  EXPECT_EQ(0, memcmp(pixels, expected_pixels, 4 * 100 * 100));
+  EXPECT_TRUE(CompareN32Pixels(pixels, expected_pixels, 100, 100));
 }
 
 TEST(DisplayItemListTest, ApproximateMemoryUsage) {
