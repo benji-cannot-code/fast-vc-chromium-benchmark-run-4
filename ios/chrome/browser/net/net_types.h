320b385364b442c2712362b2fced48d8d9a92a24
pinkerton <pinkerton@chromium.org>
2017-03-28 08:28:07 -0700

    Remove URLRequestInterceptor from BrowserState.
    URLRequestInterceptor is used for the data reduction proxy, no longer
    supported on iOS. Cleaning up code by removing it.
    
    BUG=592012
    TEST=none, code cleanup.
    
    Review-Url: https://codereview.chromium.org/2776333003
    Cr-Commit-Position: refs/heads/master@{#460111}

--- ios/chrome/browser/net/net_types.h
+++ ios/chrome/browser/net/net_types.h
@@ -7,9 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_NET_NET_TYPES_H_
 
 #include <map>
-#include <memory>
 #include <string>
-#include <vector>
 
 #include "base/memory/linked_ptr.h"
 #include "net/url_request/url_request_job_factory.h"
