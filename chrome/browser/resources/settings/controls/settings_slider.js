620ab1a424be9bb139d62b92c36b05e0502efbba
yawano <yawano@chromium.org>
2017-03-27 17:39:11 -0700

    UI change for adjustable large cursor
    - Change the label of the slider.
    - Add small and large labels to the slider.
    - Align the slider to the right side in LTR (left side in RTL).
    - Fix an issue where the slider was not disabled even if large cursor is
      disabled.
    
    BUG=591493
    TEST=Open the accessibility section of MD settings page, and confirm
         that those changes are made.
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2772743002
    Cr-Commit-Position: refs/heads/master@{#459952}

--- chrome/browser/resources/settings/controls/settings_slider.js
+++ chrome/browser/resources/settings/controls/settings_slider.js
@@ -32,9 +32,11 @@ Polymer({
 
     labelMax: String,
 
+    disabled: Boolean,
+
     /** @private */
     disableSlider_: {
-      computed: 'computeDisableSlider_(pref.*)',
+      computed: 'computeDisableSlider_(pref.*, disabled)',
       type: Boolean,
     },
   },
@@ -60,7 +62,7 @@ Polymer({
 
   /** @private */
   computeDisableSlider_: function() {
-    return this.hasAttribute('disabled') || this.isPrefEnforced();
+    return this.disabled || this.isPrefEnforced();
   },
 
   /**
