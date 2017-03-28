1b7728f0c36571e9724da363fec1bba9d5a951a9
sadrul <sadrul@chromium.org>
2017-03-27 21:02:58 -0700

    gpu: Remove libxml dependency.
    None of the gpu code seems to actually use libxml, so remove the
    dependency.
    
    BUG=none
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2778463004
    Cr-Commit-Position: refs/heads/master@{#460008}

--- gpu/config/gpu_info_collector_win.cc
+++ gpu/config/gpu_info_collector_win.cc
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/scoped_com_initializer.h"
 #include "base/win/scoped_comptr.h"
 #include "base/win/windows_version.h"
-#include "third_party/libxml/chromium/libxml_utils.h"
 #include "ui/gl/gl_implementation.h"
 #include "ui/gl/gl_surface_egl.h"
 
