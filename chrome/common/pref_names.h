f808b9a666492b3fa1a445caab4c3491e661bc6f
jlebel <jlebel@chromium.org>
2017-03-28 10:32:20 -0700

    Removing fast user switcher tutorial
    Second part for crrev.com/2709763002
    
    BUG=694285
    
    Review-Url: https://codereview.chromium.org/2765413002
    Cr-Commit-Position: refs/heads/master@{#460157}

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -413,7 +413,6 @@ extern const char kProfileGAIAInfoUpdateTime[];
 extern const char kProfileGAIAInfoPictureURL[];
 
 extern const char kProfileAvatarTutorialShown[];
-extern const char kProfileAvatarRightClickTutorialDismissed[];
 
 extern const char kInvertNotificationShown[];
 
