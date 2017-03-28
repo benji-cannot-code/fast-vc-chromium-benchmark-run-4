da5d38ca7ed35b618fabe762a3439b1718b1e17c
msw <msw@chromium.org>
2017-03-28 13:45:54 -0700

    mash: Pass a ShelfItem copy to ShelfModelObserver::ShelfItemRemoved
    Lets observers check the state of items removed from the shelf model.
    Supports a WIP CL: https://codereview.chromium.org/2769323002
    
    BUG=557406
    TEST=Automated; no shelf item behavior changes.
    R=sky@chromium.org
    
    Review-Url: https://codereview.chromium.org/2784613002
    Cr-Commit-Position: refs/heads/master@{#460213}

--- chrome/browser/ui/ash/launcher/chrome_launcher_controller_impl.h
+++ chrome/browser/ui/ash/launcher/chrome_launcher_controller_impl.h
@@ -245,7 +245,7 @@ class ChromeLauncherControllerImpl
 
   // ash::ShelfModelObserver:
   void ShelfItemAdded(int index) override;
-  void ShelfItemRemoved(int index, ash::ShelfID id) override;
+  void ShelfItemRemoved(int index, const ash::ShelfItem& old_item) override;
   void ShelfItemMoved(int start_index, int target_index) override;
   void ShelfItemChanged(int index, const ash::ShelfItem& old_item) override;
   void OnSetShelfItemDelegate(
