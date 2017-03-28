674edb7c3e74b437149d54f4f20f7ed2537402c9
yhirano <yhirano@chromium.org>
2017-03-28 07:18:25 -0700

    DevTools: expose linkPreload bit on the network request
    This is based on https://codereview.chromium.org/2751043002/.
    
    BUG=701597
    
    Review-Url: https://codereview.chromium.org/2751023005
    Cr-Commit-Position: refs/heads/master@{#460094}

--- third_party/WebKit/Source/core/inspector/InspectorNetworkAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorNetworkAgent.cpp
@@ -638,6 +638,8 @@ void InspectorNetworkAgent::willSendRequestInternal(
       MixedContentChecker::contextTypeForInspector(frame, request)));
 
   requestInfo->setReferrerPolicy(referrerPolicy(request.getReferrerPolicy()));
+  if (initiatorInfo.isLinkPreload)
+    requestInfo->setIsLinkPreload(true);
 
   String resourceType = InspectorPageAgent::resourceTypeJson(type);
   frontend()->requestWillBeSent(
