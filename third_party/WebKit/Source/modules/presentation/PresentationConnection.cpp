55a7dfc498509f2f6e45be89b8d2352d0d9440f0
zhaobin <zhaobin@chromium.org>
2017-03-28 09:56:42 -0700

    [Presentation API] Change PresentationConnection's default binaryType to ArrayBuffer
    To fix http://web-platform.test:8001/presentation-api/controlling-ua/PresentationConnection_onmessage-manual.html:77:17
    
    BUG=705170
    
    Review-Url: https://codereview.chromium.org/2777403002
    Cr-Commit-Position: refs/heads/master@{#460136}

--- third_party/WebKit/Source/modules/presentation/PresentationConnection.cpp
+++ third_party/WebKit/Source/modules/presentation/PresentationConnection.cpp
@@ -153,7 +153,7 @@ PresentationConnection::PresentationConnection(LocalFrame* frame,
       m_id(id),
       m_url(url),
       m_state(WebPresentationConnectionState::Connecting),
-      m_binaryType(BinaryTypeBlob),
+      m_binaryType(BinaryTypeArrayBuffer),
       m_proxy(nullptr) {}
 
 PresentationConnection::~PresentationConnection() {
