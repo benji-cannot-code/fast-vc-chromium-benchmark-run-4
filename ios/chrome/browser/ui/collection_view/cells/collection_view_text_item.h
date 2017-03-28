08b2b0da53a994905f0dc954e186962026611a55
lpromero <lpromero@chromium.org>
2017-03-28 08:19:56 -0700

    Remove mentions of MDCCollectionViewTextCell
    We now use CollectionViewTextCell, our own MDCollectionViewCell
    subclass.
    
    BUG=none
    R=gambard@chromium.org
    
    Review-Url: https://codereview.chromium.org/2782593002
    Cr-Commit-Position: refs/heads/master@{#460106}

--- ios/chrome/browser/ui/collection_view/cells/collection_view_text_item.h
+++ ios/chrome/browser/ui/collection_view/cells/collection_view_text_item.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/collection_view/cells/collection_view_item.h"
 #import "ios/third_party/material_components_ios/src/components/CollectionCells/src/MaterialCollectionCells.h"
 
-// Collection view item to represent and configure an MDCCollectionViewTextCell.
+// Collection view item to represent and configure a CollectionViewTextCell.
 @interface CollectionViewTextItem : CollectionViewItem
 
 // The accessory type for the represented cell.
