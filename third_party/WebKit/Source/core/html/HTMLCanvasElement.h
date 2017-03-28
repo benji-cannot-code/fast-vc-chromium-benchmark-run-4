70a83de5e4c4f7a1c79dd8693e2f3c00a1bda4c9
junov <junov@chromium.org>
2017-03-28 10:47:20 -0700

    Fix unreliable MediaStream capture from WebGL canvases
    When capturing a MediaStrem from a WebGL canvas that
    has the preserveDrawingBuffer option set to false, it is important
    to perform the frame capture before the canvas is cleared by the
    buffer swap mechanism.  This change resolves the issue by performing
    the capture during the paint invalidation phase instead of during
    frame finalization.  For canvases that are not visible on screen,
    there is no paint invalidation, which is a problem. So in that
    case, we fall back to doing the capture during frame finalization,
    which is okay in that case because there is no buffer swap.
    
    BUG=702446
    
    Review-Url: https://codereview.chromium.org/2768683002
    Cr-Commit-Position: refs/heads/master@{#460159}

--- third_party/WebKit/Source/core/html/HTMLCanvasElement.h
+++ third_party/WebKit/Source/core/html/HTMLCanvasElement.h
@@ -322,6 +322,7 @@ class CORE_EXPORT HTMLCanvasElement final
 
   int m_numFramesSinceLastRenderingModeSwitch;
   bool m_pendingRenderingModeSwitch;
+  bool m_didNotifyListenersForCurrentFrame = false;
 };
 
 }  // namespace blink
