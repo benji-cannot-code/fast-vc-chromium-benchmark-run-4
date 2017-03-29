c399fd770a858a7b8f2e90534a24bcefd49e0c09
michaeldo <michaeldo@chromium.org>
2017-03-29 09:48:49 -0700

    Use framework includes in ios/web_view/shell.
    BUG=704946
    
    Review-Url: https://codereview.chromium.org/2772503006
    Cr-Commit-Position: refs/heads/master@{#460424}

--- ios/web_view/shell/shell_app_delegate.m
+++ ios/web_view/shell/shell_app_delegate.m
@@ -5,7 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/web_view/shell/shell_app_delegate.h"
 
-#import "ios/web_view/public/cwv.h"
+#import <ChromeWebView/ChromeWebView.h>
+
 #import "ios/web_view/shell/shell_delegate.h"
 #import "ios/web_view/shell/shell_view_controller.h"
 
--- ios/web_view/shell/shell_view_controller.m
+++ ios/web_view/shell/shell_view_controller.m
@@ -5,14 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/web_view/shell/shell_view_controller.h"
 
+#import <ChromeWebView/ChromeWebView.h>
 #import <MobileCoreServices/MobileCoreServices.h>
 
-#import "ios/web_view/public/cwv.h"
-#import "ios/web_view/public/cwv_html_element.h"
-#import "ios/web_view/public/cwv_navigation_action.h"
-#import "ios/web_view/public/cwv_navigation_delegate.h"
-#import "ios/web_view/public/cwv_ui_delegate.h"
-#import "ios/web_view/public/cwv_web_view.h"
 #import "ios/web_view/shell/translate_controller.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
