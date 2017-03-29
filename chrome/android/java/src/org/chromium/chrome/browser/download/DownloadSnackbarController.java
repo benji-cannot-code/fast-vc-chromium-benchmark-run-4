47940a7392e81283aaa73b83b1fc1026c83feadb
dtrainor <dtrainor@chromium.org>
2017-03-29 11:17:08 -0700

    Properly clean up notification on download open
    Now that downloads is a foreground service with the summary, we need to
    manually clean up the summary in code, which means going through the
    notification service to do it.
    
    BUG=706094
    
    Review-Url: https://codereview.chromium.org/2781873002
    Cr-Commit-Position: refs/heads/master@{#460459}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadSnackbarController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadSnackbarController.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.download;
 
 import android.app.Activity;
-import android.app.NotificationManager;
 import android.content.Context;
 
 import org.chromium.base.ApplicationStatus;
@@ -55,10 +54,8 @@ public class DownloadSnackbarController implements SnackbarManager.SnackbarContr
         DownloadManagerService manager = DownloadManagerService.getDownloadManagerService();
         manager.openDownloadedContent(download.downloadInfo, download.systemDownloadId);
         if (download.notificationId != INVALID_NOTIFICATION_ID) {
-            NotificationManager notificationManager =
-                    (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
-            notificationManager.cancel(
-                    DownloadNotificationService.NOTIFICATION_NAMESPACE, download.notificationId);
+            manager.getDownloadNotifier().removeDownloadNotification(
+                    download.notificationId, download.downloadInfo);
         }
     }
 
