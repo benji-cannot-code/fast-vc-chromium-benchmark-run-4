36c326ed989bfa89c60b818930ded11092d16aea
bsep <bsep@chromium.org>
2017-03-29 13:12:54 -0700

    Update conditional in BrowserNonClientFrameView::DoesIntersectRect.
    As per crrev.com/2283253002#msg37 this patch updates one of the
    conditional branches BrowserNonClientFrameView::DoesIntersectRect to
    return true if a large hit-test rectangle overlaps both the tabstrip and
    the client area.
    
    Review-Url: https://codereview.chromium.org/2775083002
    Cr-Commit-Position: refs/heads/master@{#460511}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_unittest.cc
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_unittest.cc
@@ -8,13 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/test_with_browser_view.h"
+#include "chrome/browser/ui/views/tabs/tab_strip.h"
 #include "ui/base/ui_base_switches.h"
 #include "url/gurl.h"
 
 class BrowserNonClientFrameViewTest : public TestWithBrowserView {
  public:
-  BrowserNonClientFrameViewTest()
-      : TestWithBrowserView(Browser::TYPE_POPUP, false), frame_view_(nullptr) {}
+  explicit BrowserNonClientFrameViewTest(Browser::Type type)
+      : TestWithBrowserView(type, false), frame_view_(nullptr) {}
 
   // TestWithBrowserView override:
   void SetUp() override {
@@ -37,11 +38,45 @@ class BrowserNonClientFrameViewTest : public TestWithBrowserView {
   DISALLOW_COPY_AND_ASSIGN(BrowserNonClientFrameViewTest);
 };
 
-TEST_F(BrowserNonClientFrameViewTest, HitTestTopChrome) {
+class BrowserNonClientFrameViewPopupTest
+    : public BrowserNonClientFrameViewTest {
+ public:
+  BrowserNonClientFrameViewPopupTest()
+      : BrowserNonClientFrameViewTest(Browser::TYPE_POPUP) {}
+};
+
+TEST_F(BrowserNonClientFrameViewPopupTest, HitTestPopupTopChrome) {
   EXPECT_FALSE(frame_view_->HitTestRect(gfx::Rect(-1, 4, 1, 1)));
   EXPECT_FALSE(frame_view_->HitTestRect(gfx::Rect(4, -1, 1, 1)));
   const int top_inset = frame_view_->GetTopInset(false);
   EXPECT_FALSE(frame_view_->HitTestRect(gfx::Rect(4, top_inset, 1, 1)));
   if (top_inset > 0)
     EXPECT_TRUE(frame_view_->HitTestRect(gfx::Rect(4, top_inset - 1, 1, 1)));
+}
+
+class BrowserNonClientFrameViewTabbedTest
+    : public BrowserNonClientFrameViewTest {
+ public:
+  BrowserNonClientFrameViewTabbedTest()
+      : BrowserNonClientFrameViewTest(Browser::TYPE_TABBED) {}
+};
+
+TEST_F(BrowserNonClientFrameViewTabbedTest, HitTestTabstrip) {
+  gfx::Rect tabstrip_bounds =
+      frame_view_->browser_view()->tabstrip()->GetLocalBounds();
+  EXPECT_FALSE(tabstrip_bounds.IsEmpty());
+
+  // Completely outside bounds.
+  EXPECT_FALSE(frame_view_->HitTestRect(
+      gfx::Rect(tabstrip_bounds.x() - 1, tabstrip_bounds.y() + 1, 1, 1)));
+  EXPECT_FALSE(frame_view_->HitTestRect(
+      gfx::Rect(tabstrip_bounds.x() + 1, tabstrip_bounds.y() - 1, 1, 1)));
+
+  // Hits tab strip but not client area.
+  EXPECT_TRUE(frame_view_->HitTestRect(
+      gfx::Rect(tabstrip_bounds.x() + 1, tabstrip_bounds.bottom() - 1, 1, 1)));
+
+  // Hits tab strip and client area.
+  EXPECT_TRUE(frame_view_->HitTestRect(gfx::Rect(
+      tabstrip_bounds.x() + 1, tabstrip_bounds.bottom() - 1, 100, 100)));
 }
