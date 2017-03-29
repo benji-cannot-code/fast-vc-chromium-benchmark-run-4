3dc388fd9b26c083581f2dc698878b7c78c0fc62
karlo <karlo@opera.com>
2017-03-29 13:59:39 -0700

    Fix for performance regression on high dpi devices.
    ComputedStyle::getRoundedInnerBorderFor() could generate negative
    content boxes for boxes with no content and sub pixel borders that
    round up (eg. 1.5px-1.99px).  Negative content boxes are illegal per
    spec, and causes BoxBorderPainter::paintBorderFastPath() to bail,
    and performance to be substantially degraded.
    
    This issue would be evident on the paint-offset-changes perftest when
    device pixel ratio was set to eg. 1.5.  The issue was introduced in
    934becac5daa91ea979fb66e4ae21761ca11ebc9
    
    BUG=692955
    
    Review-Url: https://codereview.chromium.org/2782153002
    Cr-Commit-Position: refs/heads/master@{#460534}

--- third_party/WebKit/Source/core/style/ComputedStyle.cpp
+++ third_party/WebKit/Source/core/style/ComputedStyle.cpp
@@ -1491,7 +1491,9 @@ FloatRoundedRect ComputedStyle::getRoundedInnerBorderFor(
     bool includeLogicalRightEdge) const {
   LayoutRect innerRect(borderRect);
   innerRect.expand(insets);
-  innerRect.size().clampNegativeToZero();
+  LayoutSize innerRectSize = innerRect.size();
+  innerRectSize.clampNegativeToZero();
+  innerRect.setSize(innerRectSize);
 
   FloatRoundedRect roundedRect(pixelSnappedIntRect(innerRect));
 
