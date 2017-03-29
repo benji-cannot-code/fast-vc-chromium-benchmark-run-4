196f4578be14a5c44e64c6d969ddb529b1860af7
peter <peter@chromium.org>
2017-03-29 05:36:28 -0700

    Enable transmitting Fetch Requests over Mojo
    This CL implements support for sending Fetch API requests over Mojo. The
    used type in Blink is WebServiceWorkerRequest, where the browser side
    will use ServiceWorkerFetchRequest.
    
    It's a lot of extra code and definitely more duplication. However, this
    is something that we'll need in the near future to support Onion Soup of
    the other users of these types, too. There also are opportunities to
    remove duplication, notably of the enum values, by adopting the Mojo
    enumerations as opposed to the ones in WebURLRequest.
    
    The introduced type now also supersedes the native struct definition
    included in service_worker_event_dispatcher.mojom.
    
    BUG=692535
    
    Review-Url: https://codereview.chromium.org/2762303002
    Cr-Commit-Position: refs/heads/master@{#460359}

--- content/common/background_fetch/background_fetch_struct_traits.cc
+++ content/common/background_fetch/background_fetch_struct_traits.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/background_fetch/background_fetch_struct_traits.h"
 
 #include "content/common/service_worker/service_worker_event_dispatcher.mojom.h"
+#include "content/common/service_worker/service_worker_fetch_request_struct_traits.h"
 #include "content/common/service_worker/service_worker_messages.h"
 #include "mojo/public/cpp/bindings/array_data_view.h"
 
