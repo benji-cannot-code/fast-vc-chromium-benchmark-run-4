8640474031e95ec7ee1e5f14834b6d2d593305b1
mdjones <mdjones@chromium.org>
2017-03-28 17:20:08 -0700

    Revert of Let ImeAdapterAndroid have the same lifecycle as its Java peer (patchset #12 id:340001 of https://codereview.chromium.org/2752113005/ )
    Reason for revert:
    Suspected breaking Marshmallow 64 bit tester: https://uberchromegw.corp.google.com/i/chromium.android/builders/Marshmallow%2064%20bit%20Tester/builds/10654
    
    Original issue's description:
    > Let ImeAdapterAndroid have the same lifecycle as its Java peer
    >
    > The native ImeAdapterAndroid(IAA) instance is owned by
    > RenderWidgetHostViewAndroid(RWHVA) while its Java peer
    > (ImeAdapter) is owned by CVC. This causes their life
    > cycles to be different. Attach/detach API are used to get them
    > linked before talking to each other.
    >
    > This CL makes this mechanism simpler by having Java ImeAdapter
    > class create the native together, hence gets their lifetime synced.
    > The separate attach/detach mechanism is not necessary since
    > the linking is done as a part of WCVA - RWHVA association job.
    > i.e. IAA is linked to the current RWHVA when the corresponding
    > CVCImpl is set to RWHVA (in the future this will be done separately
    > as RWHVA will be rid of all the references to CVC).
    >
    > The only situation where the linking should be done "manually"
    > is when IAA gets (temporarily) linked to RWHVA for an interstitial
    > page. The link between IAA and the main RWHVA gets restored
    > once the interstitial page is detached.
    >
    > BUG=662908
    >
    > Review-Url: https://codereview.chromium.org/2752113005
    > Cr-Commit-Position: refs/heads/master@{#460028}
    > Committed: https://chromium.googlesource.com/chromium/src/+/153d2811b3cbcf69cda7ab4817cdc35e7c680dee
    
    TBR=aelias@chromium.org,changwan@chromium.org,boliu@chromium.org,tedchoc@chromium.org,jinsukkim@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=662908
    
    Review-Url: https://codereview.chromium.org/2785543003
    Cr-Commit-Position: refs/heads/master@{#460247}

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -495,8 +495,8 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Displa
     }
 
     private ImeAdapter createImeAdapter() {
-        return new ImeAdapter(mWebContents, new InputMethodManagerWrapper(mContext),
-                new ImeAdapter.ImeAdapterDelegate() {
+        return new ImeAdapter(
+                new InputMethodManagerWrapper(mContext), new ImeAdapter.ImeAdapterDelegate() {
                     @Override
                     public void onImeEvent() {
                         mPopupZoomer.hide(true);
@@ -584,9 +584,10 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Displa
 
         initPopupZoomer(mContext);
         mImeAdapter = createImeAdapter();
+        attachImeAdapter();
 
         mSelectionPopupController = new SelectionPopupController(mContext, windowAndroid,
-                webContents, viewDelegate.getContainerView(), mRenderCoordinates);
+                webContents, viewDelegate.getContainerView(), mRenderCoordinates, mImeAdapter);
         mSelectionPopupController.setCallback(ActionModeCallbackHelper.EMPTY_CALLBACK);
         mSelectionPopupController.setContainerView(getContainerView());
 
@@ -1776,14 +1777,16 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Displa
     }
 
     @CalledByNative
-    private void updateImeAdapter(int textInputType, int textInputFlags, int textInputMode,
-            String text, int selectionStart, int selectionEnd, int compositionStart,
-            int compositionEnd, boolean showImeIfNeeded, boolean replyToRequest) {
+    private void updateImeAdapter(long nativeImeAdapterAndroid, int textInputType,
+            int textInputFlags, int textInputMode, String text, int selectionStart,
+            int selectionEnd, int compositionStart, int compositionEnd, boolean showImeIfNeeded,
+            boolean replyToRequest) {
         try {
             TraceEvent.begin("ContentViewCore.updateImeAdapter");
             boolean focusedNodeEditable = (textInputType != TextInputType.NONE);
             boolean focusedNodeIsPassword = (textInputType == TextInputType.PASSWORD);
 
+            mImeAdapter.attach(nativeImeAdapterAndroid);
             mImeAdapter.updateState(textInputType, textInputFlags, textInputMode, showImeIfNeeded,
                     text, selectionStart, selectionEnd, compositionStart, compositionEnd,
                     replyToRequest);
@@ -1798,6 +1801,11 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Displa
         } finally {
             TraceEvent.end("ContentViewCore.updateImeAdapter");
         }
+    }
+
+    @CalledByNative
+    private void forceUpdateImeAdapter(long nativeImeAdapterAndroid) {
+        mImeAdapter.attach(nativeImeAdapterAndroid);
     }
 
     /**
@@ -1910,8 +1918,18 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Displa
     @SuppressWarnings("unused")
     @CalledByNative
     private void onRenderProcessChange() {
+        attachImeAdapter();
         // Immediately sync closed caption settings to the new render process.
         mSystemCaptioningBridge.syncToListener(this);
+    }
+
+    /**
+     * Attaches the native ImeAdapter object to the java ImeAdapter to allow communication via JNI.
+     */
+    public void attachImeAdapter() {
+        if (mImeAdapter != null && mNativeContentViewCore != 0) {
+            mImeAdapter.attach(nativeGetNativeImeAdapter(mNativeContentViewCore));
+        }
     }
 
     /**
@@ -2657,6 +2675,9 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Displa
 
     private native void nativeSelectPopupMenuItems(long nativeContentViewCoreImpl,
             long nativeSelectPopupSourceFrame, int[] indices);
+
+
+    private native long nativeGetNativeImeAdapter(long nativeContentViewCoreImpl);
 
     private native int nativeGetCurrentRenderProcessId(long nativeContentViewCoreImpl);
 
