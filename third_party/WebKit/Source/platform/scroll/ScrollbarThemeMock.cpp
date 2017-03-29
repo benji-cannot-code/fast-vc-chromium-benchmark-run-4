d43e916555a6b0f90d2054634d36de6530babfd2
bokan <bokan@chromium.org>
2017-03-29 06:23:57 -0700

    Reland Change minimum length of Aura overlay scrollbars.
    It turns out Aura (and Android) overlay scrollbars weren't using the minimum
    defined in NativeThemeAura. Instead, the minimum was set from
    ScrollbarTheme::minimumThumbLength which just returns the thumb thickness.
    
    After adding an override for ScrollbarThemeOverlay which correctly uses the
    value form NativeThemeAura, the only consumers of
    ScrollbarTheme::minimumThumbLength are the mock classes so I moved the base
    implementation into the Mock class and made the base pure virtual.
    
    BUG=678595
    
    Review-Url: https://codereview.chromium.org/2734483002
    Cr-Original-Commit-Position: refs/heads/master@{#460199}
    Committed: https://chromium.googlesource.com/chromium/src/+/151a2db45f5fdbca8973988dd51d2bf6c86fbc8c
    Review-Url: https://codereview.chromium.org/2734483002
    Cr-Commit-Position: refs/heads/master@{#460367}

--- third_party/WebKit/Source/platform/scroll/ScrollbarThemeMock.cpp
+++ third_party/WebKit/Source/platform/scroll/ScrollbarThemeMock.cpp
@@ -88,4 +88,9 @@ void ScrollbarThemeMock::paintScrollCorner(GraphicsContext& context,
   context.fillRect(cornerRect, Color::white);
 }
 
+int ScrollbarThemeMock::minimumThumbLength(
+    const ScrollbarThemeClient& scrollbar) {
+  return scrollbarThickness(scrollbar.controlSize());
+}
+
 }  // namespace blink
