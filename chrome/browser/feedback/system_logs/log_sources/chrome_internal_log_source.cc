f18fb08488f3588509748004be3e025ac1767bf9
afakhry <afakhry@chromium.org>
2017-03-29 13:18:11 -0700

    Add info on extensions to detect malicious extensions
    It seems that there are some abusive extensions that act as if they were
    other extensions with fake names. Instead of recording only the extensions
    names in feedback reports, we should also record the ID and the version.
    
    BUG=699266
    TEST=manually
    
    Review-Url: https://codereview.chromium.org/2772403004
    Cr-Commit-Position: refs/heads/master@{#460514}

--- chrome/browser/feedback/system_logs/log_sources/chrome_internal_log_source.cc
+++ chrome/browser/feedback/system_logs/log_sources/chrome_internal_log_source.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/feedback/system_logs/log_sources/chrome_internal_log_source.h"
 
 #include "base/json/json_string_value_serializer.h"
+#include "base/strings/string_util.h"
 #include "base/sys_info.h"
 #include "base/task_scheduler/post_task.h"
 #include "build/build_config.h"
@@ -203,14 +204,18 @@ void ChromeInternalLogSource::PopulateExtensionInfoLogs(
   std::string extensions_list;
   for (const scoped_refptr<const extensions::Extension>& extension :
        extension_registry->enabled_extensions()) {
-    if (extensions_list.empty()) {
-      extensions_list = extension->name();
-    } else {
-      extensions_list += ",\n" + extension->name();
-    }
+    // Format the list as:
+    // "extension_id" : "extension_name" : "extension_version".
+
+    // Work around the anonymizer tool recognizing some versions as IPv4s.
+    // Replaces dots "." by underscores "_".
+    // We shouldn't change the anonymizer tool as it is working as intended; it
+    // must err on the side of safety.
+    std::string version;
+    base::ReplaceChars(extension->VersionString(), ".", "_", &version);
+    extensions_list += extension->id() + " : " + extension->name() +
+                       " : version " + version + "\n";
   }
-  if (!extensions_list.empty())
-    extensions_list += "\n";
 
   if (!extensions_list.empty())
     (*response)[kExtensionsListKey] = extensions_list;
