17636c226ee42d2537b4418849b7ec00ed01d6a9
jinsukkim <jinsukkim@chromium.org>
2017-03-29 16:24:14 -0700

    Respect UTF-8 detection result for local file resources
    https://crbug.com/2697213002 (not guessing UTF8 encoding)
    doesn't have to be applied to local file resources. This
    CL makes such cases an exception to the policy.
    
    BUG=704800
    
    Review-Url: https://codereview.chromium.org/2784483003
    Cr-Commit-Position: refs/heads/master@{#460573}

--- third_party/WebKit/Source/platform/text/TextEncodingDetector.cpp
+++ third_party/WebKit/Source/platform/text/TextEncodingDetector.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "platform/text/TextEncodingDetector.h"
 
+#include "platform/weborigin/KURL.h"
 #include "third_party/ced/src/compact_enc_det/compact_enc_det.h"
 #include "wtf/text/TextEncoding.h"
 
@@ -57,10 +58,12 @@ bool detectTextEncoding(const char* data,
   // Should return false if the detected encoding is UTF8. This helps prevent
   // modern web sites from neglecting proper encoding labelling and simply
   // relying on browser-side encoding detection. Encoding detection is supposed
-  // to work for web sites with legacy encoding only. Detection failure leads
-  // |TextResourceDecoder| to use its default encoding determined from system
-  // locale or TLD.
-  if (encoding == UNKNOWN_ENCODING || encoding == UTF8)
+  // to work for web sites with legacy encoding only (so this doesn't have to
+  // be applied to local file resources).
+  // Detection failure leads |TextResourceDecoder| to use its default encoding
+  // determined from system locale or TLD.
+  String protocol = hintUrl ? KURL(ParsedURLString, hintUrl).protocol() : "";
+  if (encoding == UNKNOWN_ENCODING || (protocol != "file" && encoding == UTF8))
     return false;
 
   *detectedEncoding = WTF::TextEncoding(MimeEncodingName(encoding));
