3583472ac0c8cb0d6dd500a2c3fd3d419cdf91a0
tsunghung <tsunghung@chromium.org>
2017-03-29 15:27:48 -0700

    Fix one gn check failure of cast for Android build
    We moved mojo_media_host to browser process when CMA backend is
    enabled. The gn dependency //media/mojo/services is missing from
    //media/gpu.
    
    BUG=None
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2786473003
    Cr-Commit-Position: refs/heads/master@{#460555}

--- media/gpu/android_video_decode_accelerator.cc
+++ media/gpu/android_video_decode_accelerator.cc
@@ -44,7 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_bindings.h"
 
 #if defined(ENABLE_MOJO_MEDIA_IN_GPU_PROCESS)
-#include "media/mojo/services/mojo_cdm_service.h"
+#include "media/mojo/services/mojo_cdm_service.h"  // nogncheck
 #endif
 
 #define NOTIFY_ERROR(error_code, error_message)      \
