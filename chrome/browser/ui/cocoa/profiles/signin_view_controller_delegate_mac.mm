33187648a05a498d95aa017512213cde8262a95a
msarda <msarda@chromium.org>
2017-03-29 03:49:23 -0700

    Use the same browser instance in the sync confirmation dialog.
    Before this CL, the sync confirmation dialog used the active browser
    when dismissing the dialog. In some cases, the active browser is different
    than the browser that actually presented the sync confirmation dialog
    and this leads to unexpected behavior.
    
    This CL fixes the browser instance used by the sync confirmation dialog
    and ensures that the same instace is used throughout the lifetime of the
    sync confirmation dialog.
    
    TEST=See bug
    BUG=694476
    
    Review-Url: https://codereview.chromium.org/2771113003
    Cr-Commit-Position: refs/heads/master@{#460345}

--- chrome/browser/ui/cocoa/profiles/signin_view_controller_delegate_mac.mm
+++ chrome/browser/ui/cocoa/profiles/signin_view_controller_delegate_mac.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/cocoa/constrained_window/constrained_window_custom_sheet.h"
 #include "chrome/browser/ui/cocoa/constrained_window/constrained_window_custom_window.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/browser/ui/webui/signin/sync_confirmation_ui.h"
 #include "chrome/common/url_constants.h"
 #include "components/signin/core/common/profile_management_switches.h"
 #include "content/public/browser/native_web_keyboard_event.h"
@@ -115,18 +116,21 @@ SigninViewControllerDelegateMac::CreateGaiaWebContents(
 // static
 std::unique_ptr<content::WebContents>
 SigninViewControllerDelegateMac::CreateSyncConfirmationWebContents(
-    Profile* profile) {
+    Browser* browser) {
   std::unique_ptr<content::WebContents> web_contents(
       content::WebContents::Create(
-          content::WebContents::CreateParams(profile)));
+          content::WebContents::CreateParams(browser->profile())));
   web_contents->GetController().LoadURL(
       GURL(chrome::kChromeUISyncConfirmationURL), content::Referrer(),
       ui::PAGE_TRANSITION_AUTO_TOPLEVEL, std::string());
+  SyncConfirmationUI* sync_confirmation_ui = static_cast<SyncConfirmationUI*>(
+      web_contents->GetWebUI()->GetController());
+  sync_confirmation_ui->InitializeMessageHandlerWithBrowser(browser);
 
   NSView* webview = web_contents->GetNativeView();
-  [webview setFrameSize:NSMakeSize(
-                            kModalDialogWidth,
-                            GetSyncConfirmationDialogPreferredHeight(profile))];
+  [webview setFrameSize:NSMakeSize(kModalDialogWidth,
+                                   GetSyncConfirmationDialogPreferredHeight(
+                                       browser->profile()))];
 
   return web_contents;
 }
@@ -247,7 +251,7 @@ SigninViewControllerDelegate::CreateSyncConfirmationDelegate(
   return new SigninViewControllerDelegateMac(
       signin_view_controller,
       SigninViewControllerDelegateMac::CreateSyncConfirmationWebContents(
-          browser->profile()),
+          browser),
       browser,
       NSMakeRect(0, 0, kModalDialogWidth,
                  GetSyncConfirmationDialogPreferredHeight(browser->profile())),
