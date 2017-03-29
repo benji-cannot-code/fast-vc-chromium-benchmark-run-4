7da027532371ef84d95c6b3afc167d6b736aeefe
gchatz <gchatz@chromium.org>
2017-03-28 19:19:50 -0700

    Revert "[Payment Request] EG tests for the generic editor"
    This reverts commit 4ab7b085b90773506d3565cae16c0a1f2bad72a9.
    The CL broke
    SCPaymentsEditorTestCase.testInputAccessoryViewNavigationButtons
    and SCPaymentsEditorTestCase.testInputAccessoryViewCloseButton with
    NoMatchingElementExceptions.
    
    BUG=602666
    TBR=sdefresne
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2782723002
    Cr-Commit-Position: refs/heads/master@{#460261}

--- ios/chrome/browser/payments/payment_request_edit_view_controller.h
+++ ios/chrome/browser/payments/payment_request_edit_view_controller.h
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/collection_view/collection_view_controller.h"
 
-extern NSString* const kWarningMessageAccessibilityID;
-
 @class EditorField;
 @class PaymentRequestEditViewController;
 
--- ios/showcase/payments/sc_payments_editor_coordinator.mm
+++ ios/showcase/payments/sc_payments_editor_coordinator.mm
@@ -28,8 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @synthesize baseViewController = _baseViewController;
 
 - (void)start {
-  [self.baseViewController setNavigationBarHidden:YES animated:YES];
-
   NSArray<EditorField*>* editorFields = [self editorFields];
   _paymentRequestEditViewController = [[PaymentRequestEditViewController alloc]
       initWithEditorFields:editorFields];
@@ -40,23 +38,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                      animated:YES];
 }
 
-- (void)stop {
-  [self.baseViewController popViewControllerAnimated:YES];
-  _paymentRequestEditViewController = nil;
-  [self.baseViewController setNavigationBarHidden:NO animated:YES];
-}
-
 #pragma mark - PaymentRequestEditViewControllerDelegate
 
 - (void)paymentRequestEditViewController:
             (PaymentRequestEditViewController*)controller
                   didFinishEditingFields:(NSArray<EditorField*>*)fields {
-  [self stop];
+  [self.baseViewController popViewControllerAnimated:YES];
 }
 
 - (void)paymentRequestEditViewControllerDidReturn:
     (PaymentRequestEditViewController*)controller {
-  [self stop];
+  [self.baseViewController popViewControllerAnimated:YES];
 }
 
 #pragma mark - Helper methods
