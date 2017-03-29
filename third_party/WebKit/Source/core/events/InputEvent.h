e7acd9b75e401f18ba04b8d44d916438ec7a894a
chongz <chongz@chromium.org>
2017-03-29 09:05:19 -0700

    [InputEvent] Distinguish between soft line and hard line
    This CL adds input types DeleteSoftLineForward/Backward for line boundary and
    DeleteHardLineForward/Backward for paragraph boundary.
    
    BUG=705641
    
    Review-Url: https://codereview.chromium.org/2777863003
    Cr-Commit-Position: refs/heads/master@{#460404}

--- third_party/WebKit/Source/core/events/InputEvent.h
+++ third_party/WebKit/Source/core/events/InputEvent.h
@@ -42,8 +42,10 @@ class InputEvent final : public UIEvent {
     // Deletion.
     DeleteWordBackward,
     DeleteWordForward,
-    DeleteLineBackward,
-    DeleteLineForward,
+    DeleteSoftLineBackward,
+    DeleteSoftLineForward,
+    DeleteHardLineBackward,
+    DeleteHardLineForward,
     DeleteContentBackward,
     DeleteContentForward,
     DeleteByCut,
