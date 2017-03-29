fe946a002b29e0ebe5b96c5059536ebe18c51887
amaralp <amaralp@chromium.org>
2017-03-28 18:45:44 -0700

    Removing mNeedsPrepare
    The field |mNeedsPrepare| was used to prevent recreating the action mode menu if
    the new menu would be the same as the old menu. I think there is no harm in
    recreating the menu even if it there is no visible change. I've tested this locally
    (on floating and legacy action modes) and don't notice a difference.
    
    Review-Url: https://codereview.chromium.org/2777093008
    Cr-Commit-Position: refs/heads/master@{#460259}

--- content/public/android/java/src/org/chromium/content/browser/SelectionPopupController.java
+++ content/public/android/java/src/org/chromium/content/browser/SelectionPopupController.java
@@ -97,9 +97,6 @@ public class SelectionPopupController extends ActionModeCallbackHelper {
     private boolean mIsPasswordType;
     private boolean mIsInsertion;
 
-    // Indicates whether the action mode needs to be redrawn since last invalidation.
-    private boolean mNeedsPrepare;
-
     private boolean mUnselectAllOnDismiss;
     private String mLastSelectedText;
 
@@ -370,7 +367,6 @@ public class SelectionPopupController extends ActionModeCallbackHelper {
 
     @Override
     public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-        if (!mNeedsPrepare) return false;
         menu.clear();
         createActionMenu(mode, menu);
         return true;
@@ -393,7 +389,6 @@ public class SelectionPopupController extends ActionModeCallbackHelper {
     }
 
     private void createActionMenu(ActionMode mode, Menu menu) {
-        mNeedsPrepare = false;
         initializeMenu(mContext, mode, menu);
 
         if (!isSelectionEditable() || !canPaste()) {
@@ -756,7 +751,7 @@ public class SelectionPopupController extends ActionModeCallbackHelper {
 
             case SelectionEventType.INSERTION_HANDLE_SHOWN:
                 mSelectionRect.set(left, top, right, bottom);
-                setIsInsertion(true);
+                mIsInsertion = true;
                 break;
 
             case SelectionEventType.INSERTION_HANDLE_MOVED:
@@ -779,7 +774,7 @@ public class SelectionPopupController extends ActionModeCallbackHelper {
 
             case SelectionEventType.INSERTION_HANDLE_CLEARED:
                 destroyPastePopup();
-                setIsInsertion(false);
+                mIsInsertion = false;
                 mSelectionRect.setEmpty();
                 break;
 
@@ -849,10 +844,7 @@ public class SelectionPopupController extends ActionModeCallbackHelper {
         if (editable != isSelectionEditable() || isPassword != isSelectionPassword()) {
             mEditable = editable;
             mIsPasswordType = isPassword;
-            if (isActionModeValid()) {
-                mNeedsPrepare = true;
-                mActionMode.invalidate();
-            }
+            if (isActionModeValid()) mActionMode.invalidate();
         }
     }
 
@@ -867,11 +859,6 @@ public class SelectionPopupController extends ActionModeCallbackHelper {
     @Override
     public String getSelectedText() {
         return mHasSelection ? mLastSelectedText : "";
-    }
-
-    private void setIsInsertion(boolean insertion) {
-        if (isActionModeValid() && mIsInsertion != insertion) mNeedsPrepare = true;
-        mIsInsertion = insertion;
     }
 
     private boolean isShareAvailable() {
