ecf1c4298268eae88fa1fe554d57dc6ef7672781
rnephew <rnephew@chromium.org>
2017-03-29 13:25:53 -0700

    Revert of [scheduler] Split suspendable tq from unthrottled tq. (patchset #4 id:60001 of https://codereview.chromium.org/2754673002/ )
    Reason for revert:
    Causing failures in perf tests. Reverting to confirm it is the culprit.
    
    crbug.com/706108
    crbug.com/706104
    
    Original issue's description:
    > [scheduler] Add suspendable task queue.
    >
    > Add suspendable task queue to avoid running (and cancelling) callbacks
    > when execution context is suspended.
    >
    > R=skyostil@chromium.org,alexclarke@chromium.org,haraken@chromium.org
    > CC=nhiroki@chromium.org,tzik@chromium.org
    > BUG=700792
    >
    > Review-Url: https://codereview.chromium.org/2754673002
    > Cr-Commit-Position: refs/heads/master@{#459787}
    > Committed: https://chromium.googlesource.com/chromium/src/+/12e16766adec5bb1e5cdd73d3cd8f5016b59ce18
    
    TBR=alexclarke@chromium.org,haraken@chromium.org,skyostil@chromium.org,altimin@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=700792
    
    Review-Url: https://codereview.chromium.org/2780233002
    Cr-Commit-Position: refs/heads/master@{#460519}

--- third_party/WebKit/Source/core/loader/EmptyClients.cpp
+++ third_party/WebKit/Source/core/loader/EmptyClients.cpp
@@ -76,7 +76,6 @@ class EmptyFrameScheduler : public WebFrameScheduler {
   RefPtr<WebTaskRunner> loadingTaskRunner() override;
   RefPtr<WebTaskRunner> timerTaskRunner() override;
   RefPtr<WebTaskRunner> unthrottledTaskRunner() override;
-  RefPtr<WebTaskRunner> suspendableTaskRunner() override;
 };
 
 RefPtr<WebTaskRunner> EmptyFrameScheduler::loadingTaskRunner() {
@@ -88,10 +87,6 @@ RefPtr<WebTaskRunner> EmptyFrameScheduler::timerTaskRunner() {
 }
 
 RefPtr<WebTaskRunner> EmptyFrameScheduler::unthrottledTaskRunner() {
-  return Platform::current()->mainThread()->getWebTaskRunner();
-}
-
-RefPtr<WebTaskRunner> EmptyFrameScheduler::suspendableTaskRunner() {
   return Platform::current()->mainThread()->getWebTaskRunner();
 }
 
