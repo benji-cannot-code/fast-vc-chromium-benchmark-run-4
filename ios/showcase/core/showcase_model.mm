5f78096c6d7cf9487892282b9e2a5e2dd3493b6f
edchin <edchin@chromium.org>
2017-03-29 07:53:51 -0700

    [tab_container] Remove tab_strip container
    BUG=
    
    Review-Url: https://codereview.chromium.org/2711343003
    Cr-Commit-Position: refs/heads/master@{#460385}

--- ios/showcase/core/showcase_model.mm
+++ ios/showcase/core/showcase_model.mm
@@ -43,11 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       showcase::kUseCaseKey : @"Tab grid",
     },
     @{
-      showcase::kClassForDisplayKey : @"TabStripContainerViewController",
-      showcase::kClassForInstantiationKey : @"SCTabStripCoordinator",
-      showcase::kUseCaseKey : @"Tab strip container",
-    },
-    @{
       showcase::kClassForDisplayKey : @"TopToolbarTabViewController",
       showcase::kClassForInstantiationKey : @"SCTopToolbarTabCoordinator",
       showcase::kUseCaseKey : @"Top toolbar tab",
