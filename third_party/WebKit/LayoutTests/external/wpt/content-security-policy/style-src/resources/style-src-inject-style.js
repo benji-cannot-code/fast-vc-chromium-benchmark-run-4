b518ab54c818b30023905024f7531d17b55bcd83
andypaicu <andypaicu@chromium.org>
2017-03-29 04:39:36 -0700

    Fixed wpt content-security-policy/style-src tests
    Fixed existing tests to use testharness
    Ported and deleted relevant tests from contentSecurityPolicy
    
    BUG=694525
    
    Review-Url: https://codereview.chromium.org/2784543003
    Cr-Commit-Position: refs/heads/master@{#460353}

---
+++ third_party/WebKit/LayoutTests/external/wpt/content-security-policy/style-src/resources/style-src-inject-style.js
@@ -0,0 +1,6 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+document.write("<style>#content { margin-left: 2px; }</style>");
+
+var s = document.createElement('style');
+s.innerText = "#content { margin-right: 2px; }";
+document.getElementsByTagName('body')[0].appendChild(s);
