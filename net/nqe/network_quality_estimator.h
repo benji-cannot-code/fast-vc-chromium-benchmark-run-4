8dbfdd2e2fd49e547e1a7fdb8a70e6d51b810c53
tbansal <tbansal@chromium.org>
2017-03-29 16:53:44 -0700

    NQE: Make params a class
    Make network quality estimator (NQE) params a class.
    
    Next CL will move the storing of the params to the Params class
    from the NQE class to make it simpler to query the params again and
    again. This CL does not make any functional change.
    
    BUG=705691
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_cronet_tester
    
    Review-Url: https://codereview.chromium.org/2775223004
    Cr-Commit-Position: refs/heads/master@{#460582}

--- net/nqe/network_quality_estimator.h
+++ net/nqe/network_quality_estimator.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/nqe/external_estimate_provider.h"
 #include "net/nqe/network_id.h"
 #include "net/nqe/network_quality.h"
+#include "net/nqe/network_quality_estimator_params.h"
 #include "net/nqe/network_quality_observation.h"
 #include "net/nqe/network_quality_observation_source.h"
 #include "net/nqe/network_quality_store.h"
@@ -537,8 +538,7 @@ class NET_EXPORT NetworkQualityEstimator
   // the field trial parameters. For each effective connection type, a model
   // (currently composed of a RTT threshold and a downlink throughput threshold)
   // is provided by the field trial.
-  void ObtainOperatingParams(
-      const std::map<std::string, std::string>& variation_params);
+  void ObtainOperatingParams();
 
   // Adds the default median RTT and downstream throughput estimate for the
   // current connection type to the observation buffer.
@@ -650,6 +650,9 @@ class NET_EXPORT NetworkQualityEstimator
       const nqe::internal::CachedNetworkQuality& cached_network_quality);
 
   const char* GetNameForStatistic(int i) const;
+
+  // Params to configure the network quality estimator.
+  const nqe::internal::NetworkQualityEstimatorParams params_;
 
   // Determines if the requests to local host can be used in estimating the
   // network quality. Set to true only for tests.
