b41525c9a8037f1d583fd2f271c0d9476034dd5a
mfoltz <mfoltz@chromium.org>
2017-03-29 11:20:07 -0700

    [Presentation API] Move sandbox check to PresentationRequest constructor.
    Fix PresentationRequest_sandboxing_error.html.
    
    [Patched from https://codereview.chromium.org/2780583003]
    
    BUG=705170
    
    Review-Url: https://codereview.chromium.org/2782663002
    Cr-Commit-Position: refs/heads/master@{#460460}

--- third_party/WebKit/Source/modules/presentation/PresentationRequest.cpp
+++ third_party/WebKit/Source/modules/presentation/PresentationRequest.cpp
@@ -52,13 +52,6 @@ Settings* settings(ExecutionContext* executionContext) {
   return document->settings();
 }
 
-ScriptPromise rejectWithSandBoxException(ScriptState* scriptState) {
-  return ScriptPromise::rejectWithDOMException(
-      scriptState, DOMException::create(SecurityError,
-                                        "The document is sandboxed and lacks "
-                                        "the 'allow-presentation' flag."));
-}
-
 }  // anonymous namespace
 
 // static
@@ -75,6 +68,12 @@ PresentationRequest* PresentationRequest::create(
     ExecutionContext* executionContext,
     const Vector<String>& urls,
     ExceptionState& exceptionState) {
+  if (toDocument(executionContext)->isSandboxed(SandboxPresentation)) {
+    exceptionState.throwSecurityError(
+        "The document is sandboxed and lacks the 'allow-presentation' flag.");
+    return nullptr;
+  }
+
   if (urls.isEmpty()) {
     exceptionState.throwDOMException(NotSupportedError,
                                      "Do not support empty sequence of URLs.");
@@ -141,8 +140,6 @@ ScriptPromise PresentationRequest::start(ScriptState* scriptState) {
             InvalidAccessError,
             "PresentationRequest::start() requires user gesture."));
 
-  if (toDocument(getExecutionContext())->isSandboxed(SandboxPresentation))
-    return rejectWithSandBoxException(scriptState);
 
   WebPresentationClient* client = presentationClient(getExecutionContext());
   if (!client)
@@ -160,9 +157,6 @@ ScriptPromise PresentationRequest::start(ScriptState* scriptState) {
 
 ScriptPromise PresentationRequest::reconnect(ScriptState* scriptState,
                                              const String& id) {
-  if (toDocument(getExecutionContext())->isSandboxed(SandboxPresentation))
-    return rejectWithSandBoxException(scriptState);
-
   WebPresentationClient* client = presentationClient(getExecutionContext());
   if (!client)
     return ScriptPromise::rejectWithDOMException(
@@ -193,9 +187,6 @@ ScriptPromise PresentationRequest::reconnect(ScriptState* scriptState,
 }
 
 ScriptPromise PresentationRequest::getAvailability(ScriptState* scriptState) {
-  if (toDocument(getExecutionContext())->isSandboxed(SandboxPresentation))
-    return rejectWithSandBoxException(scriptState);
-
   WebPresentationClient* client = presentationClient(getExecutionContext());
   if (!client)
     return ScriptPromise::rejectWithDOMException(
