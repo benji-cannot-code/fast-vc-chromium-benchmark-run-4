910a0b62db79e3e2394ad80171f7ef82f830de4f
thomasanderson <thomasanderson@google.com>
2017-03-29 12:34:50 -0700

    Fix base::nix::GetDesktopEnvironment for newer versions of Unity
    This CL fixes some desktop integration issues on Ubuntu 17.04, where
    XDG_CURRENT_DESKTOP has changed from 'Unity' to 'Unity:Unity7' (or
    'Unity:Unity8' if you're of the Mir persuasion).
    
    BUG=706317
    R=thestig@chromium.org
    
    Review-Url: https://codereview.chromium.org/2780863003
    Cr-Commit-Position: refs/heads/master@{#460492}

--- base/nix/xdg_util.cc
+++ base/nix/xdg_util.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/path_service.h"
+#include "base/strings/string_util.h"
 #include "base/third_party/xdg_user_dirs/xdg_user_dir_lookup.h"
 
 namespace {
@@ -58,7 +59,8 @@ DesktopEnvironment GetDesktopEnvironment(Environment* env) {
   std::string xdg_current_desktop;
   if (env->GetVar("XDG_CURRENT_DESKTOP", &xdg_current_desktop)) {
     // Not all desktop environments set this env var as of this writing.
-    if (xdg_current_desktop == "Unity") {
+    if (base::StartsWith(xdg_current_desktop, "Unity",
+                         base::CompareCase::SENSITIVE)) {
       // gnome-fallback sessions set XDG_CURRENT_DESKTOP to Unity
       // DESKTOP_SESSION can be gnome-fallback or gnome-fallback-compiz
       std::string desktop_session;
