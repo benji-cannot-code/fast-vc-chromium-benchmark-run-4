b656fc289b976962993e32f355fae9182aa3ca88
sashab <sashab@chromium.org>
2017-03-28 21:22:26 -0700

    Remove Page::m_frameHost
    Remove Page::m_frameHost and FrameHost() method.
    
    BUG=688614
    
    Review-Url: https://codereview.chromium.org/2771983002
    Cr-Commit-Position: refs/heads/master@{#460275}

--- third_party/WebKit/Source/core/page/Page.cpp
+++ third_party/WebKit/Source/core/page/Page.cpp
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/DOMTimer.h"
 #include "core/frame/EventHandlerRegistry.h"
 #include "core/frame/FrameConsole.h"
-#include "core/frame/FrameHost.h"
 #include "core/frame/FrameView.h"
 #include "core/frame/PageScaleConstraints.h"
 #include "core/frame/PageScaleConstraintsSet.h"
@@ -129,8 +128,7 @@ Page::Page(PageClients& pageClients)
       m_deviceScaleFactor(1),
       m_visibilityState(PageVisibilityStateVisible),
       m_isCursorVisible(true),
-      m_subframeCount(0),
-      m_frameHost(FrameHost::create(*this)) {
+      m_subframeCount(0) {
   ASSERT(m_editorClient);
 
   ASSERT(!allPages().contains(this));
@@ -629,7 +627,6 @@ DEFINE_TRACE(Page) {
   visitor->trace(m_mainFrame);
   visitor->trace(m_validationMessageClient);
   visitor->trace(m_useCounter);
-  visitor->trace(m_frameHost);
   Supplementable<Page>::trace(visitor);
   PageVisibilityNotifier::trace(visitor);
 }
