e2fcfa36ae534d12c9e739ae35357761cc9659a6
ccameron <ccameron@chromium.org>
2017-03-29 16:03:22 -0700

    Add CVPixelBufferRef memory tracking
    Create a separate sequence number for GLImages created from
    CVPixelBufferRefs (previously they had no sequence number, so they
    would alias each other).
    
    Make VTVideoDecodeAccelerator be a dump provider. This dumps through
    the GLImage interface, and so it will correctly link the
    CVPixelBufferRefs that with the command buffer textures.
    
    Fix TextureManager's dump code to not assume that GLImage presence
    and texture state BOUND are related (a texture can have a GLImage but
    not be BOUND because it can't be textured out of directly).
    
    BUG=704792
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2771923006
    Cr-Commit-Position: refs/heads/master@{#460566}

--- media/gpu/vt_video_decode_accelerator_mac.h
+++ media/gpu/vt_video_decode_accelerator_mac.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "base/threading/thread.h"
 #include "base/threading/thread_checker.h"
+#include "base/trace_event/memory_dump_provider.h"
 #include "media/filters/h264_parser.h"
 #include "media/gpu/gpu_video_decode_accelerator_helpers.h"
 #include "media/gpu/media_gpu_export.h"
@@ -37,7 +38,8 @@ MEDIA_GPU_EXPORT bool InitializeVideoToolbox();
 
 // VideoToolbox.framework implementation of the VideoDecodeAccelerator
 // interface for Mac OS X (currently limited to 10.9+).
-class VTVideoDecodeAccelerator : public VideoDecodeAccelerator {
+class VTVideoDecodeAccelerator : public VideoDecodeAccelerator,
+                                 public base::trace_event::MemoryDumpProvider {
  public:
   explicit VTVideoDecodeAccelerator(
       const MakeGLContextCurrentCallback& make_context_current_cb,
@@ -58,6 +60,10 @@ class VTVideoDecodeAccelerator : public VideoDecodeAccelerator {
       const base::WeakPtr<Client>& decode_client,
       const scoped_refptr<base::SingleThreadTaskRunner>& decode_task_runner)
       override;
+
+  // MemoryDumpProvider implementation.
+  bool OnMemoryDump(const base::trace_event::MemoryDumpArgs& args,
+                    base::trace_event::ProcessMemoryDump* pmd) override;
 
   // Called by OutputThunk() when VideoToolbox finishes decoding a frame.
   void Output(void* source_frame_refcon,
@@ -268,6 +274,9 @@ class VTVideoDecodeAccelerator : public VideoDecodeAccelerator {
   bool waiting_for_idr_ = true;
   bool missing_idr_logged_ = false;
   H264POC poc_;
+
+  // Id number for this instance for memory dumps.
+  int memory_dump_id_ = 0;
 
   //
   // Shared state (set up and torn down on GPU thread).
