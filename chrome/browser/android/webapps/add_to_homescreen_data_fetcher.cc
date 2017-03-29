f9557fa8af8a8a44ba34ba7b13290faad5a356bd
zpeng <zpeng@chromium.org>
2017-03-29 09:32:20 -0700

    Use OneShotTimer in AddToHomescreenDataFetcher
    Also stop it when the data is successfully fetched.
    
    BUG=705537
    
    Review-Url: https://codereview.chromium.org/2768083004
    Cr-Commit-Position: refs/heads/master@{#460418}

--- chrome/browser/android/webapps/add_to_homescreen_data_fetcher.cc
+++ chrome/browser/android/webapps/add_to_homescreen_data_fetcher.cc
@@ -37,6 +37,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+// The default number of milliseconds to wait for the data download to complete.
+const int kDataTimeoutInMilliseconds = 4000;
+
 // Looks up the original, online URL of the site requested.  The URL from the
 // WebContents may be a distilled article which is not appropriate for a home
 // screen shortcut.
@@ -82,7 +85,6 @@ AddToHomescreenDataFetcher::AddToHomescreenDataFetcher(
       weak_observer_(observer),
       shortcut_info_(GetShortcutUrl(web_contents->GetBrowserContext(),
                                     web_contents->GetLastCommittedURL())),
-      data_timeout_timer_(false, false),
       ideal_icon_size_in_px_(ideal_icon_size_in_px),
       minimum_icon_size_in_px_(minimum_icon_size_in_px),
       ideal_splash_image_size_in_px_(ideal_splash_image_size_in_px),
@@ -149,7 +151,7 @@ void AddToHomescreenDataFetcher::OnDidGetWebApplicationInfo(
   // Kick off a timeout for downloading data. If we haven't finished within the
   // timeout, fall back to using a dynamically-generated launcher icon.
   data_timeout_timer_.Start(
-      FROM_HERE, base::TimeDelta::FromMilliseconds(4000),
+      FROM_HERE, base::TimeDelta::FromMilliseconds(kDataTimeoutInMilliseconds),
       base::Bind(&AddToHomescreenDataFetcher::OnDataTimedout, this));
 
   manager->GetData(
@@ -192,14 +194,13 @@ void AddToHomescreenDataFetcher::OnDataTimedout() {
     weak_observer_->OnUserTitleAvailable(base::string16());
   }
 
-  if (!is_icon_saved_) {
-    badge_icon_.reset();
-    CreateLauncherIcon(SkBitmap());
-  }
+  badge_icon_.reset();
+  CreateLauncherIcon(SkBitmap());
 }
 
 void AddToHomescreenDataFetcher::OnDidPerformInstallableCheck(
     const InstallableData& data) {
+  data_timeout_timer_.Stop();
   badge_icon_.reset();
 
   if (!web_contents() || !weak_observer_ || is_installable_check_complete_)
