f46ba883e51303b4dc42a6d9a1f58ea9b972b41a
perezju <perezju@chromium.org>
2017-03-29 08:41:11 -0700

    [tools/perf] Disable multitab:misc:typical24
    Story is failing due to large trace size.
    
    TBR=nednguyen@google.com,vovoy@chromium.org
    BUG=704197
    
    Review-Url: https://codereview.chromium.org/2787473002
    Cr-Commit-Position: refs/heads/master@{#460398}

--- tools/perf/page_sets/system_health/multi_tab_stories.py
+++ tools/perf/page_sets/system_health/multi_tab_stories.py
@@ -10,6 +10,8 @@ from page_sets.system_health import system_health_story
 from page_sets.system_health import story_tags
 from page_sets.system_health import platforms
 
+from telemetry import benchmark
+
 
 class _MultiTabStory(system_health_story.SystemHealthStory):
   ABSTRACT_STORY = True
@@ -38,6 +40,7 @@ class _MultiTabStory(system_health_story.SystemHealthStory):
       tab.WaitForFrameToBeDisplayed()
 
 
+@benchmark.Disabled('all')  # crbug.com/704197
 class MultiTabTypical24Story(_MultiTabStory):
   NAME = 'multitab:misc:typical24'
   TAGS = [story_tags.TABS_SWITCHING]
