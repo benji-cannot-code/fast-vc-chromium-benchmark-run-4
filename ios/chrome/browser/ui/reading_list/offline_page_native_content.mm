bed66c08fb974bff7e7182bd2f5a327473ecce8d
olivierrobin <olivierrobin@chromium.org>
2017-03-29 04:41:11 -0700

    Reading List iOS: Adapt loading offline for new reload process
    Loading offline pages relies on being able to reload page after
    changing the URL.
    Adapt to the new reload process which does not allow that.
    Also add EG tests for catching future problem.
    
    BUG=703393, 703206
    
    Review-Url: https://codereview.chromium.org/2762113002
    Cr-Commit-Position: refs/heads/master@{#460354}

--- ios/chrome/browser/ui/reading_list/offline_page_native_content.mm
+++ ios/chrome/browser/ui/reading_list/offline_page_native_content.mm
@@ -101,7 +101,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       _webState->GetNavigationManager()->GetLastCommittedItem();
   DCHECK(item && item->GetVirtualURL() == [self virtualURL]);
   item->SetURL(_entryURL);
-  item->SetVirtualURL(_entryURL);
+  item->SetVirtualURL(GURL::EmptyGURL());
 }
 
 @end
