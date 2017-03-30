8a4a2ec61ebaaeacbdb72045d2625ee55aec844f
fsamuel <fsamuel@chromium.org>
2017-03-29 17:31:07 -0700

    Aura-Mus: Allocate a LocalSurfaceId on size change
    If the window type is EMBED_IN_OWNER, or TOP_LEVEL_IN_WM, then allocate
    a cc::LocalSurfaceId on size change and propagate that new LocalSurfaceId
    down to the WindowTreeHost of the embedded client. The embedded client
    will use that LocalSurfaceId to submit CompositorFrames.
    
    If surface synchronization is enabled (subsequent patch), then the embedder
    can immediately embed the surface ID (if the FrameSinkId is available), and
    submit its own CompositorFrame with that embedding. The parent's CompositorFrame
    will block until the embedded client submits a CompositorFrame or a deadline is
    hit.
    
    BUG=672962
    
    Review-Url: https://codereview.chromium.org/2780043002
    Cr-Commit-Position: refs/heads/master@{#460597}

--- ui/aura/mus/window_mus.h
+++ ui/aura/mus/window_mus.h
@@ -77,7 +77,9 @@ class AURA_EXPORT WindowMus {
   virtual void ReorderFromServer(WindowMus* child,
                                  WindowMus* relative,
                                  ui::mojom::OrderDirection) = 0;
-  virtual void SetBoundsFromServer(const gfx::Rect& bounds) = 0;
+  virtual void SetBoundsFromServer(
+      const gfx::Rect& bounds,
+      const base::Optional<cc::LocalSurfaceId>& local_surface_id) = 0;
   virtual void SetVisibleFromServer(bool visible) = 0;
   virtual void SetOpacityFromServer(float opacity) = 0;
   virtual void SetPredefinedCursorFromServer(ui::mojom::CursorType cursor) = 0;
@@ -85,6 +87,8 @@ class AURA_EXPORT WindowMus {
                                      const std::vector<uint8_t>* data) = 0;
   virtual void SetFrameSinkIdFromServer(
       const cc::FrameSinkId& frame_sink_id) = 0;
+  virtual const cc::LocalSurfaceId& GetOrAllocateLocalSurfaceId(
+      const gfx::Size& new_size) = 0;
   virtual void SetSurfaceInfoFromServer(
       const cc::SurfaceInfo& surface_info) = 0;
   // The window was deleted on the server side. DestroyFromServer() should
@@ -95,6 +99,11 @@ class AURA_EXPORT WindowMus {
   // Called when a window was added/removed as a transient child.
   virtual ChangeSource OnTransientChildAdded(WindowMus* child) = 0;
   virtual ChangeSource OnTransientChildRemoved(WindowMus* child) = 0;
+
+  // Returns the currently used cc::LocalSurfaceId to embed this Window. Local
+  // windows or windows that have not been embedded yet will have an invalid
+  // cc::LocalSurfaceId.
+  virtual const cc::LocalSurfaceId& GetLocalSurfaceId() = 0;
 
   // Called in the rare case when WindowTreeClient needs to change state and
   // can't go through one of the SetFooFromServer() functions above. Generally
