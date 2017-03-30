6eecaafbba5d23d7f823d81fbd64cf83e490dcdb
thestig <thestig@chromium.org>
2017-03-30 00:40:28 -0700

    Switch HeadlessDevToolsManagerDelegate to use base::Bind.
    No functional change. Just making the code more consistent with the rest
    of the code base as there is no need to use function pointers.
    
    Review-Url: https://codereview.chromium.org/2784733003
    Cr-Commit-Position: refs/heads/master@{#460690}

--- headless/lib/browser/headless_devtools_manager_delegate.h
+++ headless/lib/browser/headless_devtools_manager_delegate.h
@@ -6,14 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef HEADLESS_LIB_BROWSER_HEADLESS_DEVTOOLS_MANAGER_DELEGATE_H_
 #define HEADLESS_LIB_BROWSER_HEADLESS_DEVTOOLS_MANAGER_DELEGATE_H_
 
-#include "content/public/browser/devtools_manager_delegate.h"
-
 #include <map>
 #include <memory>
 #include <string>
 
+#include "base/callback_forward.h"
 #include "base/memory/weak_ptr.h"
 #include "base/values.h"
+#include "content/public/browser/devtools_manager_delegate.h"
 
 namespace headless {
 class HeadlessBrowserImpl;
@@ -49,11 +49,11 @@ class HeadlessDevToolsManagerDelegate
 
   base::WeakPtr<HeadlessBrowserImpl> browser_;
 
-  using CommandMemberFnPtr = std::unique_ptr<base::DictionaryValue> (
-      HeadlessDevToolsManagerDelegate::*)(int command_id,
-                                          const base::DictionaryValue* params);
-
-  std::map<std::string, CommandMemberFnPtr> command_map_;
+  using CommandMemberCallback =
+      base::Callback<std::unique_ptr<base::DictionaryValue>(
+          int command_id,
+          const base::DictionaryValue* params)>;
+  std::map<std::string, CommandMemberCallback> command_map_;
 };
 
 }  // namespace headless
