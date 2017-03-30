d60a617edbdc1692f52d3f672a314ecd33a04d12
lazyboy <lazyboy@chromium.org>
2017-03-30 11:43:49 -0700

    Transfer DictionaryValue ownership in SandboxedUnpacker::OnUnpackSuccess()
    This would avoid DeepCopy() in clients that care about retaining the
    DictionaryValue's ownership.
    
    BUG=699528
    Test=None
    
    Review-Url: https://codereview.chromium.org/2788563002
    Cr-Commit-Position: refs/heads/master@{#460835}

--- extensions/browser/sandboxed_unpacker_unittest.cc
+++ extensions/browser/sandboxed_unpacker_unittest.cc
@@ -46,7 +46,7 @@ class MockSandboxedUnpackerClient : public SandboxedUnpackerClient {
 
   void OnUnpackSuccess(const base::FilePath& temp_dir,
                        const base::FilePath& extension_root,
-                       const base::DictionaryValue* original_manifest,
+                       std::unique_ptr<base::DictionaryValue> original_manifest,
                        const Extension* extension,
                        const SkBitmap& install_icon) override {
     temp_dir_ = temp_dir;
