286be766feff209a6529a590413651e3d79c5964
yorkelee <yorkelee@google.com>
2017-03-30 15:22:07 -0700

    Add ability to have active window autohide shelf
    * Actual implementation of ShellSurface.setSystemUiVisibility
    * Add hide_shelf_when_active_ flag to window_state
    which the ShelfLayoutManager uses to determine if
    the currently active window should autohide the shelf.
    * Update flag accordingly from ShellSurface
    
    BUG=705723
    TEST=New unit tests, and also verify that using the
    implementation from Android correctly autohides the shelf
    for the active Android window.
    
    Add set_systemui_visibility to wayland
    
    Review-Url: https://codereview.chromium.org/2782223002
    Cr-Commit-Position: refs/heads/master@{#460907}

--- ash/wm/window_util.cc
+++ ash/wm/window_util.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "ash/common/ash_constants.h"
+#include "ash/common/shelf/wm_shelf.h"
 #include "ash/common/wm/window_state.h"
 #include "ash/common/wm/wm_event.h"
 #include "ash/common/wm/wm_screen_util.h"
@@ -82,6 +83,13 @@ bool IsWindowUserPositionable(aura::Window* window) {
 void PinWindow(aura::Window* window, bool trusted) {
   wm::WMEvent event(trusted ? wm::WM_EVENT_TRUSTED_PIN : wm::WM_EVENT_PIN);
   wm::GetWindowState(window)->OnWMEvent(&event);
+}
+
+void SetAutoHideShelf(aura::Window* window, bool autohide) {
+  wm::GetWindowState(window)->set_autohide_shelf_when_maximized_or_fullscreen(
+      autohide);
+  for (WmWindow* root_window : WmShell::Get()->GetAllRootWindows())
+    WmShelf::ForWindow(root_window)->UpdateVisibilityState();
 }
 
 bool MoveWindowToDisplay(aura::Window* window, int64_t display_id) {
