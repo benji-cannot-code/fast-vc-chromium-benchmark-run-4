1b9521ebdb03ab6773df237ea40c307506c0ef9b
thakis <thakis@chromium.org>
2017-03-30 14:16:17 -0700

    Enable -Wdeprecated-register (except on CrOS).
    No behavior change.
    
    BUG=255186,705812
    
    Review-Url: https://codereview.chromium.org/2780623003
    Cr-Original-Commit-Position: refs/heads/master@{#459973}
    Committed: https://chromium.googlesource.com/chromium/src/+/76ad12fee73e4512dbb9c7a79ffb6cec33fae6f2
    Review-Url: https://codereview.chromium.org/2780623003
    Cr-Commit-Position: refs/heads/master@{#460879}

--- base/third_party/dmg_fp/g_fmt.cc
+++ base/third_party/dmg_fp/g_fmt.cc
@@ -28,10 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace dmg_fp {
 
  char *
-g_fmt(register char *b, double x)
+g_fmt(char *b, double x)
 {
-	register int i, k;
-	register char *s;
+	int i, k;
+	char *s;
 	int decpt, j, sign;
 	char *b0, *s0, *se;
 
