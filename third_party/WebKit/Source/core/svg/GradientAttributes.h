de53e49c4f6965b64a22af6f373d0069ae0adae8
bratell <bratell@opera.com>
2017-03-30 09:38:06 -0700

    Adding missing include in svg/GradientAttributes.h
    If you managed to include GradientAttributes.h before you included
    SVGGradientElement.h you would have no definition of
    SVGSpreadMethod and other types.
    
    R=fs@opera.com
    
    Review-Url: https://codereview.chromium.org/2790623002
    Cr-Commit-Position: refs/heads/master@{#460791}

--- third_party/WebKit/Source/core/svg/GradientAttributes.h
+++ third_party/WebKit/Source/core/svg/GradientAttributes.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef GradientAttributes_h
 #define GradientAttributes_h
 
+#include "core/svg/SVGGradientElement.h"
 #include "core/svg/SVGUnitTypes.h"
 #include "wtf/Allocator.h"
 
