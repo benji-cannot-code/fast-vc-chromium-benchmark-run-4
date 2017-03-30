d31cde5b388022153436d51927849d0e89159347
jlebel <jlebel@chromium.org>
2017-03-30 12:43:25 -0700

    Adding std::ostream& operator<<(std::ostream& out, NSError* error)
    BUG=624811
    
    Review-Url: https://codereview.chromium.org/2745323002
    Cr-Commit-Position: refs/heads/master@{#460854}

--- device/bluetooth/bluetooth_low_energy_device_mac.mm
+++ device/bluetooth/bluetooth_low_energy_device_mac.mm
@@ -202,9 +202,7 @@ void BluetoothLowEnergyDeviceMac::DidDiscoverPrimaryServices(NSError* error) {
     // TODO(http://crbug.com/609320): Need to pass the error.
     // TODO(http://crbug.com/609844): Decide what to do if discover failed
     // a device services.
-    VLOG(1) << *this << ": Can't discover primary services: "
-            << error.localizedDescription.UTF8String << " (" << error.domain
-            << ": " << error.code << ").";
+    VLOG(1) << *this << ": Can't discover primary services: " << error;
     return;
   }
 
@@ -248,9 +246,7 @@ void BluetoothLowEnergyDeviceMac::DidDiscoverCharacteristics(
   if (error) {
     // TODO(http://crbug.com/609320): Need to pass the error.
     // TODO(http://crbug.com/609844): Decide what to do if discover failed
-    VLOG(1) << *this << ": Can't discover characteristics: "
-            << error.localizedDescription.UTF8String << " (" << error.domain
-            << ": " << error.code << ").";
+    VLOG(1) << *this << ": Can't discover characteristics: " << error;
     return;
   }
 
@@ -321,9 +317,7 @@ void BluetoothLowEnergyDeviceMac::DidDiscoverDescriptors(
   if (error) {
     // TODO(http://crbug.com/609320): Need to pass the error.
     // TODO(http://crbug.com/609844): Decide what to do if discover failed
-    VLOG(1) << *this << ": Can't discover descriptors: "
-            << error.localizedDescription.UTF8String << " (" << error.domain
-            << ": " << error.code << ").";
+    VLOG(1) << *this << ": Can't discover descriptors: " << error;
     return;
   }
   if (!IsGattConnected()) {
@@ -409,8 +403,7 @@ BluetoothLowEnergyDeviceMac::GetBluetoothRemoteGattService(
 void BluetoothLowEnergyDeviceMac::DidDisconnectPeripheral(NSError* error) {
   VLOG(1) << *this << ": Disconnected from peripheral.";
   if (error) {
-    VLOG(1) << *this << ": Bluetooth error, domain: " << error.domain.UTF8String
-            << ", error code: " << error.code;
+    VLOG(1) << *this << ": Bluetooth error: " << error;
   }
   SetGattServicesDiscoveryComplete(false);
   // Removing all services at once to ensure that calling GetGattService on
