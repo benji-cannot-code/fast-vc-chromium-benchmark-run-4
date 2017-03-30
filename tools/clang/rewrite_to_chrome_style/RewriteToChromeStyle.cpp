6635fde6a92c6b6f57534a4db770b2eea4268da4
nasko <nasko@chromium.org>
2017-03-30 11:18:21 -0700

    Add selectionInDOMTree to conflicting methods list.
    BUG=582312
    
    Review-Url: https://codereview.chromium.org/2783223002
    Cr-Commit-Position: refs/heads/master@{#460825}

--- tools/clang/rewrite_to_chrome_style/RewriteToChromeStyle.cpp
+++ tools/clang/rewrite_to_chrome_style/RewriteToChromeStyle.cpp
@@ -720,6 +720,7 @@ bool ShouldPrefixFunctionName(const std::string& old_method_name) {
       "screenInfo",
       "screenOrientationController",
       "scrollAnimator",
+      "selectionInDOMTree",
       "selectionInFlatTree",
       "settings",
       "signalingState",
