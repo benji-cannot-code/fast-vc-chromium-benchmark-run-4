ff422e736f11807a6eb669949c1e6d9aedc824b5
estade <estade@chromium.org>
2017-03-29 18:07:34 -0700

    Fix initial focus of Task Manager for ChromeOS.
    The current code appeared to work because a DialogDelegate actually
    defaults to focusing the default button ("End Task") rather than nothing
    (like a default WidgetDelegate). Thus when you press Shift+Escape the
    row for the active tab is selected, and "End Task" is focused. This
    "fixed" the blue line around the table.
    
    But on ChromeOS it's possible to have no windows open and press
    Shift+Escape, and then the Browser row is selected initially. You're not
    allowed to end that task, so the button is disabled.
    Widget::SetInitialFocus has code to advance focus if the initial focus
    View can't actually receive focus, hence the table winds up receiving
    focus.
    
    BUG=702489
    
    Review-Url: https://codereview.chromium.org/2784903002
    Cr-Commit-Position: refs/heads/master@{#460611}

--- chrome/browser/ui/views/task_manager_view.cc
+++ chrome/browser/ui/views/task_manager_view.cc
@@ -162,6 +162,10 @@ bool TaskManagerView::AcceleratorPressed(const ui::Accelerator& accelerator) {
   return true;
 }
 
+views::View* TaskManagerView::GetInitiallyFocusedView() {
+  return nullptr;
+}
+
 bool TaskManagerView::CanResize() const {
   return true;
 }
