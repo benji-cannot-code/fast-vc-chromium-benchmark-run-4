26f1fc0b47e8f8721f3542a3039556a1e0f83e86
gambard <gambard@chromium.org>
2017-03-30 01:35:29 -0700

    Add logic for fetching the Reading List entries
    This CL adds the logic in the ReadingListProvider, allowing it to fetch the
    entries to be displayed.
    
    BUG=702241
    
    Review-Url: https://codereview.chromium.org/2770893003
    Cr-Commit-Position: refs/heads/master@{#460697}

--- components/ntp_snippets/reading_list/reading_list_suggestions_provider.h
+++ components/ntp_snippets/reading_list/reading_list_suggestions_provider.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <string>
 
+#include "base/scoped_observer.h"
 #include "components/ntp_snippets/callbacks.h"
 #include "components/ntp_snippets/category.h"
 #include "components/ntp_snippets/category_info.h"
@@ -50,14 +51,20 @@ class ReadingListSuggestionsProvider : public ContentSuggestionsProvider,
 
   // ReadingListModelObserver implementation.
   void ReadingListModelLoaded(const ReadingListModel* model) override;
+  void ReadingListModelBeingDeleted(const ReadingListModel* model) override;
 
  private:
+  // The actual method to fetch Reading List entries. Must be called after the
+  // model is loaded.
   void FetchReadingListInternal();
 
+  // Updates the |category_status_| and notifies the |observer_|, if necessary.
+  void NotifyStatusChanged(CategoryStatus new_status);
   CategoryStatus category_status_;
   const Category provided_category_;
 
   ReadingListModel* reading_list_model_;
+  ScopedObserver<ReadingListModel, ReadingListModelObserver> scoped_observer_;
 
   DISALLOW_COPY_AND_ASSIGN(ReadingListSuggestionsProvider);
 };
