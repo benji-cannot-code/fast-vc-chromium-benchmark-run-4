1d8f3384423b9e23d21feee0b45eb0acdaf71778
slangley <slangley@chromium.org>
2017-03-30 00:02:48 -0700

    Move registration of LocalFrame mojo interfaces to ModulesInitializer.
    To enable this, added a method to register initilization callbacks for
    LocalFrame initization. This method adds the callbacks to a static vector, and
    the callbacks are executed when a new LocalFrame is initizialized.
    
    This is part of the work to remove the dependency between WebLocalFrameImpl and
    modules/*.
    
    BUG=703405
    
    Review-Url: https://codereview.chromium.org/2783543004
    Cr-Commit-Position: refs/heads/master@{#460684}

--- third_party/WebKit/Source/web/WebLocalFrameImpl.cpp
+++ third_party/WebKit/Source/web/WebLocalFrameImpl.cpp
@@ -159,8 +159,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/style/StyleInheritedData.h"
 #include "core/timing/DOMWindowPerformance.h"
 #include "core/timing/Performance.h"
-#include "modules/app_banner/AppBannerController.h"
-#include "modules/installation/InstallationServiceImpl.h"
 #include "platform/ScriptForbiddenScope.h"
 #include "platform/UserGestureIndicator.h"
 #include "platform/WebFrameScheduler.h"
@@ -183,8 +181,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/weborigin/KURL.h"
 #include "platform/weborigin/SchemeRegistry.h"
 #include "platform/weborigin/SecurityPolicy.h"
-#include "public/platform/InterfaceProvider.h"
-#include "public/platform/InterfaceRegistry.h"
 #include "public/platform/WebDoubleSize.h"
 #include "public/platform/WebFloatPoint.h"
 #include "public/platform/WebFloatRect.h"
@@ -1629,14 +1625,6 @@ void WebLocalFrameImpl::initializeCoreFrame(Page& page,
       frame()->document()->getSecurityOrigin()->grantUniversalAccess();
     }
 
-    // TODO(dominickn): This interface should be document-scoped rather than
-    // frame-scoped, as the resulting banner event is dispatched to
-    // frame()->document().
-    frame()->interfaceRegistry()->addInterface(WTF::bind(
-        &AppBannerController::bindMojoRequest, wrapWeakPersistent(frame())));
-
-    frame()->interfaceRegistry()->addInterface(WTF::bind(
-        &InstallationServiceImpl::create, wrapWeakPersistent(frame())));
     if (!owner) {
       // This trace event is needed to detect the main frame of the
       // renderer in telemetry metrics. See crbug.com/692112#c11.
