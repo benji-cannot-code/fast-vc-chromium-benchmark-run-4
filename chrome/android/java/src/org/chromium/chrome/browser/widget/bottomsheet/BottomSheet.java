2ca6d5ed84007b1af8450b2438ecf41f1cf66e83
mdjones <mdjones@chromium.org>
2017-03-30 09:07:52 -0700

    [Home] Hide toolbar handle and disable swiping when find-in-page shows
    Previously the toolbar handle was visible when the find-in-page
    toolbar was active and could be swiped up. Both have now been disabled
    as they result in a bad user experience for find-in-page.
    
    BUG=706248
    
    Review-Url: https://codereview.chromium.org/2780353002
    Cr-Commit-Position: refs/heads/master@{#460782}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheet.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheet.java
@@ -150,6 +150,9 @@ public class BottomSheet
     /** A placeholder for if there is no content in the bottom sheet. */
     private View mPlaceholder;
 
+    /** A handle to the find-in-page toolbar. */
+    private View mFindInPageView;
+
     /** A handle to the FrameLayout that holds the content of the bottom sheet. */
     private FrameLayout mBottomSheetContentContainer;
 
@@ -953,6 +956,10 @@ public class BottomSheet
         boolean isInOverviewMode = mTabModelSelector != null
                 && (mTabModelSelector.getCurrentTab() == null
                            || mTabModelSelector.getCurrentTab().getActivity().isInOverviewMode());
-        return !isToolbarAndroidViewHidden() && !isInOverviewMode;
+
+        if (mFindInPageView == null) mFindInPageView = findViewById(R.id.find_toolbar);
+        boolean isFindInPageVisible =
+                mFindInPageView != null && mFindInPageView.getVisibility() == View.VISIBLE;
+        return !isToolbarAndroidViewHidden() && !isInOverviewMode && !isFindInPageVisible;
     }
 }
