b04913759b0ceb232e540e298b5eb3af04d424ac
dtrainor <dtrainor@chromium.org>
2017-03-29 20:26:00 -0700

    Don't force the download notifier to unbind
    The DownloadNotificationService tells the SystemDownloadNotifier to
    unbind from it when it has no notifications to show and wants to shut
    itself down.  This isn't needed because the SystemDownloadNotifier will
    unbind when it is a safe time for the service to die.  This patch
    removes the Observer method that tells the SystemDownloadNotifier to
    unbind itself.
    
    BUG=703212
    
    Review-Url: https://codereview.chromium.org/2771353002
    Cr-Commit-Position: refs/heads/master@{#460641}

--- chrome/android/java/src/org/chromium/chrome/browser/download/SystemDownloadNotifier.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/SystemDownloadNotifier.java
@@ -115,11 +115,6 @@ public class SystemDownloadNotifier implements DownloadNotifier, Observer {
     void handlePendingNotifications() {
         if (mPendingNotifications.isEmpty()) return;
         for (int i = 0; i < mPendingNotifications.size(); i++) {
-            // If we lose the service mid-loop retrigger the service load and quit.
-            if (mBoundService == null) {
-                startAndBindToServiceIfNeeded();
-                return;
-            }
             updateDownloadNotification(
                     mPendingNotifications.get(i), i == mPendingNotifications.size() - 1);
         }
@@ -157,11 +152,6 @@ public class SystemDownloadNotifier implements DownloadNotifier, Observer {
     @VisibleForTesting
     void unbindService() {
         mApplicationContext.unbindService(mConnection);
-    }
-
-    @Override
-    public void onServiceShutdownRequested() {
-        unbindServiceIfNeeded();
     }
 
     @Override
