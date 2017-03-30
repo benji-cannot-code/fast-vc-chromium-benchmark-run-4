7b08485dccb9d709cb6bb9f9ce95e8a16b8b0019
xiaochengh <xiaochengh@chromium.org>
2017-03-29 20:52:30 -0700

    Remove useless SpellChecker::clearMisspellingsForMovingParagraphs()
    The above mentioned functions removes markers from nodes that will be
    removed, which is meaningless because node removal removes marker
    eventually.
    
    Hence, this patch removes this function.
    
    BUG=700217
    TEST=n/a; no behavior change
    
    Review-Url: https://codereview.chromium.org/2781213002
    Cr-Commit-Position: refs/heads/master@{#460652}

--- third_party/WebKit/Source/core/editing/commands/CompositeEditCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/CompositeEditCommand.cpp
@@ -1495,8 +1495,6 @@ void CompositeEditCommand::moveParagraphs(
   const SelectionInDOMTree& selectionToDelete =
       SelectionInDOMTree::Builder().collapse(start).extend(end).build();
   setEndingSelection(selectionToDelete);
-  document().frame()->spellChecker().clearMisspellingsForMovingParagraphs(
-      selectionToDelete);
   deleteSelection(editingState, false, false, false);
   if (editingState->isAborted())
     return;
