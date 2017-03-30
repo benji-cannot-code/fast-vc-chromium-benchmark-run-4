98d06c0dd3910c89f95f205a1dfa75120adbdd56
lushnikov <lushnikov@chromium.org>
2017-03-30 15:58:18 -0700

    DevTools: support UISourceCode rename in automapping
    Support file renames in Automapping.
    
    As a drive-by, this patch promisifies UISourceCode.rename()
    and InspectorTest.AutomappintTest.waitForAutomappintStabilized()
    methods. This comes handy with async/await.
    
    R=dgozman
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2780933003
    Cr-Commit-Position: refs/heads/master@{#460913}

--- third_party/WebKit/LayoutTests/http/tests/inspector/persistence/automapping-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/persistence/automapping-test.js
@@ -67,10 +67,11 @@ InspectorTest.AutomappingTest.prototype = {
         }
     },
 
-    waitUntilMappingIsStabilized: function(callback)
+    waitUntilMappingIsStabilized: function()
     {
-        this._stabilizedCallback = callback;
+        var promise = new Promise(x => this._stabilizedCallback = x);
         this._checkStabilized();
+        return promise;
     },
 
     _onSweepHappened: function()
