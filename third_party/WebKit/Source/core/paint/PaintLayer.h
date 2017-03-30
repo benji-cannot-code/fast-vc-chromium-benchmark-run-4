3b8c71ac4fb3229d26063ad29257e08daf6d129e
pdr <pdr@chromium.org>
2017-03-29 17:15:18 -0700

    Start USING_FAST_MALLOC for more paint data structures
    This patch starts using fast malloc (aka partition alloc) for the following
    paint-related data structures:
     PaintInfo
     PaintLayerRareData
     PaintArtifactCompositor
     GeometryMapper
     GeometryMapperClipCache
     GeometryMapperTransformCache
     PropertyTreeState
    
    For more information about USING_FAST_MALLOC, see the Allocator.md docs.
    
    Review-Url: https://codereview.chromium.org/2783913002
    Cr-Commit-Position: refs/heads/master@{#460591}

--- third_party/WebKit/Source/core/paint/PaintLayer.h
+++ third_party/WebKit/Source/core/paint/PaintLayer.h
@@ -95,6 +95,10 @@ class CORE_EXPORT DisableCompositingQueryAsserts {
 };
 
 struct PaintLayerRareData {
+  USING_FAST_MALLOC(PaintLayerRareData);
+  WTF_MAKE_NONCOPYABLE(PaintLayerRareData);
+
+ public:
   PaintLayerRareData();
   ~PaintLayerRareData();
 
--- third_party/WebKit/Source/platform/graphics/paint/GeometryMapperTransformCache.h
+++ third_party/WebKit/Source/platform/graphics/paint/GeometryMapperTransformCache.h
@@ -19,6 +19,8 @@ class TransformPaintPropertyNode;
 // stores cached "transformed rects" (See GeometryMapper.h) from that node in
 // ancestor spaces.
 class PLATFORM_EXPORT GeometryMapperTransformCache {
+  USING_FAST_MALLOC(GeometryMapperTransformCache);
+
  public:
   GeometryMapperTransformCache();
 
