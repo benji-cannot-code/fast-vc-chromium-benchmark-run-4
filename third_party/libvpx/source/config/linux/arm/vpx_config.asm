99eb6d72a90b8cf27f0f9b82ef0ed9d79cb2c53d
marpan <marpan@chromium.org>
2017-03-30 12:30:28 -0700

    Roll src/third_party/libvpx/source/libvpx/ 2fac50fa0..32b3d2f17 (21 commits)
    https://chromium.googlesource.com/webm/libvpx.git/+log/2fac50fa0ee0..32b3d2f174f6
    
    $ git log 2fac50fa0..32b3d2f17 --date=short --no-merges --format='%ad %ae %s'
    2017-03-29 marpan vp9: 1 pass SVC: Modify condition for intra-mode search.
    2017-03-23 johannkoenig vpx_comp_avg_pred: add test
    2017-03-24 marpan vp9: Speed >= 8: avoid chrome check under some condition.
    2017-03-23 marpan vp9: 1 pass: Move source sad computation into encodeframe loop.
    2017-03-20 jzern intrapred: sync highbd_d135_predictor w/d135_
    2017-03-24 jzern intrapred: specialize highbd 4x4 predictors
    2017-03-24 jzern intrapred: rename d63f to d63e
    2017-03-24 jzern remove CONFIG_MISC_FIXES
    2017-03-23 marpan vp9: Fix to condition on using source_sad for 1 pass real-time.
    2017-03-23 jzern rate_hist: add parameter validation
    2017-03-22 marpan vp9: Non-rd partition: avoid unneeded call to chrome_check
    2017-03-22 marpan vp9: Adjust some speed settings for speed 8.
    2017-03-22 johannkoenig vp9 temporal filter: additional test
    2017-03-21 jzern vp9_rdopt: correct size to vpx_sum_squares_2d_i16
    2017-03-20 jzern idct_neon: prefix non-static functions w/'vpx_'
    2017-03-22 johannkoenig vp9 temporal filter: add const to function prototype
    2017-03-22 kaustubh.raste Fix mips msa fwd xform mismatch
    2017-03-14 jianj vp9: Enable adaptive_rd_threshold for row mt for realtime speed 8.
    2017-03-20 jzern .mailmap: add an additional entry for Yaowu Xu
    2017-03-17 jingning Backport "Optimize the use case of token_cost table" to VP9
    (...)
    
    Created with:
      roll-dep src/third_party/libvpx/source/libvpx
    R=johannkoenig@google.com
    
    Review-Url: https://codereview.chromium.org/2784343002
    Cr-Commit-Position: refs/heads/master@{#460849}

--- third_party/libvpx/source/config/linux/arm/vpx_config.asm
+++ third_party/libvpx/source/config/linux/arm/vpx_config.asm
@@ -82,7 +82,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 .equ CONFIG_SPATIAL_SVC ,  0
 .equ CONFIG_FP_MB_STATS ,  0
 .equ CONFIG_EMULATE_HARDWARE ,  0
-.equ CONFIG_MISC_FIXES ,  0
 .equ DECODE_WIDTH_LIMIT ,  16384
 .equ DECODE_HEIGHT_LIMIT ,  16384
 	.section	.note.GNU-stack,"",%progbits
