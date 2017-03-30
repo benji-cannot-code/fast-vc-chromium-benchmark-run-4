2aabc06cecd6e0dfacad72ba472190c5f0f75df8
jbauman <jbauman@chromium.org>
2017-03-29 21:45:58 -0700

    Add UMA histograms for DirectComposition overlays.
    These get information about how often overlays are available, how often
    they're attempted to be used, and how often they're actually used.
    
    BUG=678800
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2779643004
    Cr-Commit-Position: refs/heads/master@{#460666}

--- gpu/ipc/service/image_transport_surface_win.cc
+++ gpu/ipc/service/image_transport_surface_win.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/metrics/histogram_macros.h"
 #include "base/win/windows_version.h"
 #include "gpu/ipc/service/child_window_surface_win.h"
 #include "gpu/ipc/service/direct_composition_surface_win.h"
@@ -50,7 +51,11 @@ scoped_refptr<gl::GLSurface> ImageTransportSurface::CreateNativeSurface(
       vsync_provider.reset(new gl::VSyncProviderWin(surface_handle));
 
     if (gl::GLSurfaceEGL::IsDirectCompositionSupported()) {
-      if (DirectCompositionSurfaceWin::AreOverlaysSupported()) {
+      bool overlays_supported =
+          DirectCompositionSurfaceWin::AreOverlaysSupported();
+      UMA_HISTOGRAM_BOOLEAN("GPU.DirectComposition.OverlaysSupported",
+                            overlays_supported);
+      if (overlays_supported) {
         scoped_refptr<DirectCompositionSurfaceWin> egl_surface =
             make_scoped_refptr(
                 new DirectCompositionSurfaceWin(delegate, surface_handle));
