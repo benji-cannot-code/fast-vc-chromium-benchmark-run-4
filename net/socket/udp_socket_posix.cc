d5063b1677a24a394de7c1c6a21c3529e3d9d5c9
tfarina <tfarina@chromium.org>
2017-03-30 06:20:51 -0700

    net: extract SO_REUSEADDR code into a helper function
    Consolidate this code into a helper function that can be shared by both
    TCP and UDP code.
    
    BUG=None
    TEST=net_unittests
    R=eroman@chromium.org
    
    Review-Url: https://codereview.chromium.org/2775423004
    Cr-Commit-Position: refs/heads/master@{#460739}

--- net/socket/udp_socket_posix.cc
+++ net/socket/udp_socket_posix.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/log/net_log_source.h"
 #include "net/log/net_log_source_type.h"
 #include "net/socket/socket_descriptor.h"
+#include "net/socket/socket_posix.h"
 #include "net/socket/udp_net_log_parameters.h"
 
 #if defined(OS_ANDROID)
@@ -550,10 +551,7 @@ int UDPSocketPosix::AllowAddressReuse() {
   DCHECK_NE(socket_, kInvalidSocket);
   DCHECK(CalledOnValidThread());
   DCHECK(!is_connected());
-  int true_value = 1;
-  int rv = setsockopt(
-      socket_, SOL_SOCKET, SO_REUSEADDR, &true_value, sizeof(true_value));
-  return rv == 0 ? OK : MapSystemError(errno);
+  return SetReuseAddr(socket_, true);
 }
 
 int UDPSocketPosix::SetBroadcast(bool broadcast) {
