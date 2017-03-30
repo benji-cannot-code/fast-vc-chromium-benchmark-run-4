5dafb3584f71a42118fd97d14b9cde763165e2d1
slangley <slangley@chromium.org>
2017-03-29 18:09:10 -0700

    Audit use of m_client in WebFrameWidgetImpl and remove null checks.
    Now that a valid WebWidgetClient must always be passed when creating a
    WebFrameWidget we can remove some of the redundant m_client == nullptr checks.
    
    m_client can now become nullptr if close() is called on the WebFrameWidget,
    but this is generally caught by checking m_layerTreeView.
    
    To be safe if(m_client) checks have been replaced with DCHECK(m_client).
    
    BUG=696895
    
    Review-Url: https://codereview.chromium.org/2775113002
    Cr-Commit-Position: refs/heads/master@{#460612}

--- third_party/WebKit/Source/web/WebFrameWidgetImpl.cpp
+++ third_party/WebKit/Source/web/WebFrameWidgetImpl.cpp
@@ -188,13 +188,12 @@ void WebFrameWidgetImpl::sendResizeEventAndRepaint() {
     m_localRoot->frame()->document()->enqueueResizeEvent();
   }
 
-  if (m_client) {
-    if (isAcceleratedCompositingActive()) {
-      updateLayerTreeViewport();
-    } else {
-      WebRect damagedRect(0, 0, m_size.width, m_size.height);
-      m_client->didInvalidateRect(damagedRect);
-    }
+  DCHECK(m_client);
+  if (isAcceleratedCompositingActive()) {
+    updateLayerTreeViewport();
+  } else {
+    WebRect damagedRect(0, 0, m_size.width, m_size.height);
+    m_client->didInvalidateRect(damagedRect);
   }
 }
 
@@ -340,6 +339,7 @@ WebInputEventResult WebFrameWidgetImpl::handleInputEvent(
   AutoReset<const WebInputEvent*> currentEventChange(&m_currentInputEvent,
                                                      &inputEvent);
 
+  DCHECK(m_client);
   if (m_client->isPointerLocked() &&
       WebInputEvent::isMouseEventType(inputEvent.type())) {
     pointerLockMouseEvent(inputEvent);
@@ -423,8 +423,8 @@ void WebFrameWidgetImpl::scheduleAnimation() {
     m_layerTreeView->setNeedsBeginFrame();
     return;
   }
-  if (m_client)
-    m_client->scheduleAnimation();
+  DCHECK(m_client);
+  m_client->scheduleAnimation();
 }
 
 CompositorMutatorImpl& WebFrameWidgetImpl::mutator() {
@@ -831,6 +831,7 @@ WebInputEventResult WebFrameWidgetImpl::handleMouseWheel(
 
 WebInputEventResult WebFrameWidgetImpl::handleGestureEvent(
     const WebGestureEvent& event) {
+  DCHECK(m_client);
   WebInputEventResult eventResult = WebInputEventResult::NotHandled;
   bool eventCancelled = false;
   switch (event.type()) {
@@ -983,13 +984,12 @@ Element* WebFrameWidgetImpl::focusedElement() const {
 }
 
 void WebFrameWidgetImpl::initializeLayerTreeView() {
-  if (m_client) {
-    DCHECK(!m_mutator);
-    m_layerTreeView = m_client->initializeLayerTreeView();
-    if (m_layerTreeView && m_layerTreeView->compositorAnimationHost()) {
-      m_animationHost = WTF::makeUnique<CompositorAnimationHost>(
-          m_layerTreeView->compositorAnimationHost());
-    }
+  DCHECK(m_client);
+  DCHECK(!m_mutator);
+  m_layerTreeView = m_client->initializeLayerTreeView();
+  if (m_layerTreeView && m_layerTreeView->compositorAnimationHost()) {
+    m_animationHost = WTF::makeUnique<CompositorAnimationHost>(
+        m_layerTreeView->compositorAnimationHost());
   }
 
   if (WebDevToolsAgentImpl* devTools = m_localRoot->devToolsAgentImpl())
@@ -1004,8 +1004,7 @@ void WebFrameWidgetImpl::initializeLayerTreeView() {
   // FIXME: only unittests, click to play, Android priting, and printing (for
   // headers and footers) make this assert necessary. We should make them not
   // hit this code and then delete allowsBrokenNullLayerTreeView.
-  DCHECK(m_layerTreeView || !m_client ||
-         m_client->allowsBrokenNullLayerTreeView());
+  DCHECK(m_layerTreeView || m_client->allowsBrokenNullLayerTreeView());
 }
 
 void WebFrameWidgetImpl::setIsAcceleratedCompositingActive(bool active) {
@@ -1018,9 +1017,6 @@ void WebFrameWidgetImpl::setIsAcceleratedCompositingActive(bool active) {
   DCHECK(!active || m_layerTreeView);
 
   if (m_isAcceleratedCompositingActive == active)
-    return;
-
-  if (!m_client)
     return;
 
   if (active) {
