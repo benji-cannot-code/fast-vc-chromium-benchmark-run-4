180804eda1923c25617c6396d87ad80950ba33ce
dpapad <dpapad@chromium.org>
2017-03-29 18:31:26 -0700

    Roll closure compiler
    Change log:
    https://github.com/google/closure-compiler/compare/b2f2d2f3309c7ee61816e068050651af27bccdfa...f9c738eb53fb5cd1d475871ef16c6d1cca19b815
    chrome_extensions.js: a3f41820d1260b0d7ed13acac526b803ac6c1870 -> 04dad454b5797074b803c0bab27f1aa87e19d562
    
    BUG=699302
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    R=dbeam@chromium.org
    
    Review-Url: https://codereview.chromium.org/2786793002 .
    Cr-Commit-Position: refs/heads/master@{#460617}

--- ui/webui/resources/js/cr/ui/context_menu_handler.js
+++ ui/webui/resources/js/cr/ui/context_menu_handler.js
@@ -16,6 +16,7 @@ cr.define('cr.ui', function() {
    * @implements {EventListener}
    */
   function ContextMenuHandler() {
+    /** @private {!EventTracker} */
     this.showingEvents_ = new EventTracker();
   }
 
@@ -49,7 +50,7 @@ cr.define('cr.ui', function() {
 
       // When the menu is shown we steal a lot of events.
       var doc = menu.ownerDocument;
-      var win = doc.defaultView;
+      var win = /** @type {!Window} */ (doc.defaultView);
       this.showingEvents_.add(doc, 'keydown', this, true);
       this.showingEvents_.add(doc, 'mousedown', this, true);
       this.showingEvents_.add(doc, 'touchstart', this, true);
