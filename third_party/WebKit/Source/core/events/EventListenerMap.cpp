e599caa38ee31808544576ae08c17686cb2ebf8f
bratell <bratell@opera.com>
2017-03-30 13:42:18 -0700

    Removing unnecessary "using" statements.
    In the Chromium code style using statements are not allowed and these
    ones are not even needed/used so let us just drop them.
    
    Review-Url: https://codereview.chromium.org/2788693002
    Cr-Commit-Position: refs/heads/master@{#460866}

--- third_party/WebKit/Source/core/events/EventListenerMap.cpp
+++ third_party/WebKit/Source/core/events/EventListenerMap.cpp
@@ -42,8 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/ThreadingPrimitives.h"
 #endif
 
-using namespace WTF;
-
 namespace blink {
 
 #if DCHECK_IS_ON()
--- third_party/WebKit/Source/core/loader/ProgressTracker.cpp
+++ third_party/WebKit/Source/core/loader/ProgressTracker.cpp
@@ -40,8 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/PtrUtil.h"
 #include "wtf/text/CString.h"
 
-using namespace std;
-
 namespace blink {
 
 // Always start progress at initialProgressValue. This helps provide feedback as
