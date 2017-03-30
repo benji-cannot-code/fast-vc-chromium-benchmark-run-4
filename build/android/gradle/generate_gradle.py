ef11032d26bdfa3a88457d74217469eb59f7e14b
agrieve <agrieve@chromium.org>
2017-03-30 14:14:36 -0700

    generate_gradle.py: Add --extra-target that appends to the default set
    I find this is more often what I want (as opposed to replacing the
    default list of targets).
    
    BUG=620034
    
    Review-Url: https://codereview.chromium.org/2784293002
    Cr-Commit-Position: refs/heads/master@{#460878}

--- build/android/gradle/generate_gradle.py
+++ build/android/gradle/generate_gradle.py
@@ -634,8 +634,13 @@ def main():
   parser.add_argument('--target',
                       dest='targets',
                       action='append',
-                      help='GN target to generate project for. '
-                           'May be repeated.')
+                      help='GN target to generate project for. Replaces set of '
+                           'default targets. May be repeated.')
+  parser.add_argument('--extra-target',
+                      dest='extra_targets',
+                      action='append',
+                      help='GN target to generate project for, in addition to '
+                           'the default ones. May be repeated.')
   parser.add_argument('--project-dir',
                       help='Root of the output project.',
                       default=os.path.join('$CHROMIUM_OUTPUT_DIR', 'gradle'))
@@ -677,6 +682,8 @@ def main():
     targets = _QueryForAllGnTargets(output_dir)
   else:
     targets = args.targets or _DEFAULT_TARGETS
+    if args.extra_targets:
+      targets.extend(args.extra_targets)
     targets = [re.sub(r'_test_apk$', '_test_apk__apk', t) for t in targets]
     # TODO(wnwen): Utilize Gradle's test constructs for our junit tests?
     targets = [re.sub(r'_junit_tests$', '_junit_tests__java_binary', t)
