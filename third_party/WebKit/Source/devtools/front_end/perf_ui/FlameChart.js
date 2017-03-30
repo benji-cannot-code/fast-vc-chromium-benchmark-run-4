18766104c23083b10c5e5c65a9b64be3d9195266
alph <alph@chromium.org>
2017-03-29 17:08:06 -0700

    DevTools: Make EntryHighlighted an event rather than method on data provider
    Review-Url: https://codereview.chromium.org/2782953002
    Cr-Commit-Position: refs/heads/master@{#460588}

--- third_party/WebKit/Source/devtools/front_end/perf_ui/FlameChart.js
+++ third_party/WebKit/Source/devtools/front_end/perf_ui/FlameChart.js
@@ -166,14 +166,14 @@ PerfUI.FlameChart = class extends PerfUI.ChartViewport {
       return;
     this._highlightedEntryIndex = entryIndex;
     this._updateElementPosition(this._highlightElement, this._highlightedEntryIndex);
-    this._dataProvider.highlightEntry(entryIndex);
+    this.dispatchEventToListeners(PerfUI.FlameChart.Events.EntryHighlighted, entryIndex);
   }
 
   hideHighlight() {
     this._entryInfo.removeChildren();
     this._highlightedEntryIndex = -1;
     this._updateElementPosition(this._highlightElement, this._highlightedEntryIndex);
-    this._dataProvider.highlightEntry(-1);
+    this.dispatchEventToListeners(PerfUI.FlameChart.Events.EntryHighlighted, -1);
   }
 
   _resetCanvas() {
@@ -1418,11 +1418,6 @@ PerfUI.FlameChartDataProvider.prototype = {
 
   /**
    * @param {number} entryIndex
-   */
-  highlightEntry(entryIndex) {},
-
-  /**
-   * @param {number} entryIndex
    * @return {boolean}
    */
   forceDecoration(entryIndex) {},
@@ -1466,7 +1461,8 @@ PerfUI.FlameChartMarker.prototype = {
 
 /** @enum {symbol} */
 PerfUI.FlameChart.Events = {
-  EntrySelected: Symbol('EntrySelected')
+  EntrySelected: Symbol('EntrySelected'),
+  EntryHighlighted: Symbol('EntryHighlighted')
 };
 
 /**
