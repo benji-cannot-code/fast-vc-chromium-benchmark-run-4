56a6250ac23ce78f8770158fd15acef4e950adbb
servolk <servolk@chromium.org>
2017-03-30 16:18:24 -0700

    Add IsSupportedAudioConfig in MediaClient
    We already have the IsSupportedVideoConfig function in MediaClient,
    this CL will add a very similar API for audio codec checks on various
    platforms.
    
    BUG=695264
    
    Review-Url: https://codereview.chromium.org/2783963002
    Cr-Commit-Position: refs/heads/master@{#460921}

--- media/base/key_systems_unittest.cc
+++ media/base/key_systems_unittest.cc
@@ -181,6 +181,7 @@ class TestMediaClient : public MediaClient {
   void AddSupportedKeySystems(std::vector<std::unique_ptr<KeySystemProperties>>*
                                   key_systems_properties) override;
   void RecordRapporURL(const std::string& metric, const GURL& url) final;
+  bool IsSupportedAudioConfig(const media::AudioConfig& config) final;
   bool IsSupportedVideoConfig(const media::VideoConfig& config) final;
 
   // Helper function to test the case where IsKeySystemsUpdateNeeded() is true
@@ -230,6 +231,10 @@ void TestMediaClient::AddSupportedKeySystems(
 void TestMediaClient::RecordRapporURL(const std::string& /* metric */,
                                       const GURL& /* url */) {
   NOTIMPLEMENTED();
+}
+
+bool TestMediaClient::IsSupportedAudioConfig(const media::AudioConfig& config) {
+  return true;
 }
 
 bool TestMediaClient::IsSupportedVideoConfig(const media::VideoConfig& config) {
