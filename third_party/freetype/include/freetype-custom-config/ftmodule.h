2bea22346a1fbf201fb4c6f6342f3fc233f53344
drott <drott@chromium.org>
2017-03-30 14:48:07 -0700

    Support for OpenType Font Variations on Windows
    Enable support for variable fonts on Windows through using
    SkFontMgr_Custom_Empty, a FreeType backed font-blob only SkTypeface
    factory (no access to system fonts). We will use a hybrid DirectWrite
    and FreeType font stack on Windows for at least as long as most of
    Windows versions we support with Chrome still do not have native support
    for font variations.
    
    Thanks to Ben Wagner for the help with enabling and prototyping this.
    
    BUG=700926
    
    Review-Url: https://codereview.chromium.org/2780133002
    Cr-Commit-Position: refs/heads/master@{#460895}

--- third_party/freetype/include/freetype-custom-config/ftmodule.h
+++ third_party/freetype/include/freetype-custom-config/ftmodule.h
@@ -17,8 +17,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /***************************************************************************/
 
 FT_USE_MODULE( FT_Module_Class, autofit_module_class )
+FT_USE_MODULE( FT_Driver_ClassRec, t1_driver_class )
+FT_USE_MODULE( FT_Driver_ClassRec, t1cid_driver_class )
 FT_USE_MODULE( FT_Driver_ClassRec, tt_driver_class )
 FT_USE_MODULE( FT_Driver_ClassRec, cff_driver_class )
+FT_USE_MODULE( FT_Module_Class, psaux_module_class )
 FT_USE_MODULE( FT_Module_Class, psnames_module_class )
 FT_USE_MODULE( FT_Module_Class, pshinter_module_class )
 FT_USE_MODULE( FT_Renderer_Class, ft_raster1_renderer_class )
@@ -28,12 +31,9 @@ FT_USE_MODULE( FT_Renderer_Class, ft_smooth_lcd_renderer_class )
 FT_USE_MODULE( FT_Renderer_Class, ft_smooth_lcdv_renderer_class )
 
 /*
-FT_USE_MODULE( FT_Driver_ClassRec, t1_driver_class )
-FT_USE_MODULE( FT_Driver_ClassRec, t1cid_driver_class )
 FT_USE_MODULE( FT_Driver_ClassRec, pfr_driver_class )
 FT_USE_MODULE( FT_Driver_ClassRec, t42_driver_class )
 FT_USE_MODULE( FT_Driver_ClassRec, winfnt_driver_class )
 FT_USE_MODULE( FT_Driver_ClassRec, pcf_driver_class )
 FT_USE_MODULE( FT_Driver_ClassRec, bdf_driver_class )
-FT_USE_MODULE( FT_Module_Class, psaux_module_class )
 */
