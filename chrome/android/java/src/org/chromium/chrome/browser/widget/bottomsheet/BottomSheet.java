7f6e34fe450dcdab6458678063a60bb88f5c8663
twellington <twellington@chromium.org>
2017-03-30 16:25:07 -0700

    [Home] Add some histograms for Chrome Home
    Adds histograms for:
     * The reason Chrome Home was opened
     * The duration Chrome Home is opened
     * The time between app creation and the first CH open
     * The time between the last CH close and the next open
    
    BUG=700592, 700593
    
    Review-Url: https://codereview.chromium.org/2784383002
    Cr-Commit-Position: refs/heads/master@{#460925}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheet.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheet.java
@@ -108,6 +108,9 @@ public class BottomSheet
     /** The minimum distance between half and full states to allow the half state. */
     private final float mMinHalfFullDistance;
 
+    /** The  {@link BottomSheetMetrics} used to record user actions and histograms. */
+    private final BottomSheetMetrics mMetrics;
+
     /** For detecting scroll and fling events on the bottom sheet. */
     private GestureDetector mGestureDetector;
 
@@ -265,9 +268,14 @@ public class BottomSheet
             }
 
             float newOffset = getSheetOffsetFromBottom() + distanceY;
+            boolean wasOpenBeforeSwipe = mIsSheetOpen;
             setSheetOffsetFromBottom(MathUtils.clamp(newOffset, getMinOffset(), getMaxOffset()));
-
             setInternalCurrentState(SHEET_STATE_SCROLLING);
+
+            if (!wasOpenBeforeSwipe && mIsSheetOpen) {
+                mMetrics.recordSheetOpenReason(BottomSheetMetrics.OPENED_BY_SWIPE);
+            }
+
             mIsScrolling = true;
             return true;
         }
@@ -275,6 +283,7 @@ public class BottomSheet
         @Override
         public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
             cancelAnimation();
+            boolean wasOpenBeforeSwipe = mIsSheetOpen;
 
             // Figure out the projected state of the sheet and animate there. Note that a swipe up
             // will have a negative velocity, swipe down will have a positive velocity. Negate this
@@ -284,6 +293,10 @@ public class BottomSheet
                     getSheetOffsetFromBottom() + getFlingDistance(-velocityY), -velocityY);
             setSheetState(targetState, true);
             mIsScrolling = false;
+
+            if (!wasOpenBeforeSwipe && mIsSheetOpen) {
+                mMetrics.recordSheetOpenReason(BottomSheetMetrics.OPENED_BY_SWIPE);
+            }
 
             return true;
         }
@@ -311,7 +324,8 @@ public class BottomSheet
         mGestureDetector = new GestureDetector(context, new BottomSheetSwipeDetector());
         mGestureDetector.setIsLongpressEnabled(false);
 
-        addObserver(new BottomSheetMetrics());
+        mMetrics = new BottomSheetMetrics();
+        addObserver(mMetrics);
     }
 
     @Override
@@ -847,6 +861,11 @@ public class BottomSheet
         return mCurrentState;
     }
 
+    /** @return Whether the sheet is currently open. */
+    public boolean isSheetOpen() {
+        return mIsSheetOpen;
+    }
+
     /**
      * Set the current state of the bottom sheet. This is for internal use to notify observers of
      * state change events.
@@ -872,6 +891,13 @@ public class BottomSheet
     @VisibleForTesting
     public BottomSheetContent getCurrentSheetContent() {
         return mSheetContent;
+    }
+
+    /**
+     * @return The {@link BottomSheetMetrics} used to record user actions and histograms.
+     */
+    public BottomSheetMetrics getBottomSheetMetrics() {
+        return mMetrics;
     }
 
     /**
