ada1c9918fb77abf8482de1aa706586ce5d4085c
szhangcs <szhangcs@google.com>
2017-03-31 14:58:04 -0700

    Always initialize this year and month
    Currently, after click newCardLink for server cards,
    the confirm button is always greyed. This Cl fixes this.
    
    The bug is introduced by:
    
    https://codereview.chromium.org/1964323002/patch/1/10001
    
    When CardUnmaskPrompt is created. mShouldRequestExpirationDate
    is false. So CalendarTask() is never executed:
    
    https://cs.chromium.org/chromium/src/chrome/android/java/src/org/chromium/chrome/browser/autofill/CardUnmaskPrompt.java?l=207
    
    After onNewCardLinkClicked(), mShouldRequestExpirationDate
    is true. That's why getExpirationDateErrorType is
    always returning ERROR_TYPE_NOT_ENOUGH_INFO:
    
    https://cs.chromium.org/chromium/src/chrome/android/java/src/org/chromium/chrome/browser/autofill/CardUnmaskPrompt.java?l=615
    
    We need to execute CalendarTask() from somewhere.
    
    BUG=706143
    
    Review-Url: https://codereview.chromium.org/2784663002
    Cr-Commit-Position: refs/heads/master@{#461241}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/CardUnmaskPrompt.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/CardUnmaskPrompt.java
@@ -298,6 +298,9 @@ public class CardUnmaskPrompt
         mDialog.setTitle(title);
         mInstructions.setText(instructions);
         mShouldRequestExpirationDate = shouldRequestExpirationDate;
+        if (mShouldRequestExpirationDate && (mThisYear == -1 || mThisMonth == -1)) {
+            new CalendarTask().execute();
+        }
         showExpirationDateInputsInputs();
     }
 
