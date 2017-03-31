c2544cb0b7705ddd73afbc1e213125bbd2409471
gangwu <gangwu@chromium.org>
2017-03-31 15:19:19 -0700

    [Sync] TypedUrlSyncableService should filter expired visits from local
    HistoryBackend may not delete expired visits yet, so TypedUrlSyncableService should filter out expired visits to avoid too many visits send to sync.
    
    BUG=702020
    
    Review-Url: https://codereview.chromium.org/2787893003
    Cr-Commit-Position: refs/heads/master@{#461250}

--- chrome/browser/sync/test/integration/typed_urls_helper.cc
+++ chrome/browser/sync/test/integration/typed_urls_helper.cc
@@ -18,9 +18,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync/test/integration/sync_datatype_helper.h"
 #include "chrome/browser/sync/test/integration/sync_test.h"
+#include "components/browser_sync/profile_sync_service.h"
 #include "components/history/core/browser/history_backend.h"
 #include "components/history/core/browser/history_db_task.h"
 #include "components/history/core/browser/history_service.h"
+#include "components/sync/syncable/read_node.h"
+#include "components/sync/syncable/read_transaction.h"
+#include "components/sync/syncable/user_share.h"
 
 using sync_datatype_helper::test;
 
@@ -417,6 +421,14 @@ bool CheckAllProfilesHaveSameURLs() {
       return false;
   }
   return true;
+}
+
+bool CheckSyncDirectoryHasURL(int index, const GURL& url) {
+  syncer::ReadTransaction trans(FROM_HERE,
+                                test()->GetSyncService(index)->GetUserShare());
+  syncer::ReadNode node(&trans);
+  return syncer::BaseNode::INIT_OK ==
+         node.InitByClientTagLookup(syncer::TYPED_URLS, url.spec());
 }
 
 }  // namespace typed_urls_helper
