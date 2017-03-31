5f4d0ef618079d97ac2f63963470c18ec8da3cf7
yoshiki <yoshiki@chromium.org>
2017-03-31 00:23:03 -0700

    Use enum value for notification priority in mojo struct
    This patch changes the type from int32 to enum value for code readability.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2777853003
    Cr-Commit-Position: refs/heads/master@{#461067}

--- ui/arc/notification/arc_notification_item.h
+++ ui/arc/notification/arc_notification_item.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/threading/thread_checker.h"
+#include "components/arc/common/notifications.mojom.h"
 #include "components/signin/core/account_id/account_id.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/arc/notification/arc_notification_manager.h"
@@ -46,7 +47,8 @@ class ArcNotificationItem {
   const std::string& notification_key() const { return notification_key_; }
 
  protected:
-  static int ConvertAndroidPriority(int android_priority);
+  static int ConvertAndroidPriority(
+      mojom::ArcNotificationPriority android_priority);
 
   // Checks whether there is on-going |notification_|.
   bool HasPendingNotification();
