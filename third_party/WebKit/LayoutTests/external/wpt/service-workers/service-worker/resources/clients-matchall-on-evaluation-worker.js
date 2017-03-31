55378156edf92dd03836dbd06e59ae0401da1571
mike <mike@mikepennisi.com>
2017-03-30 19:28:19 -0700

    Upstream service worker `client` tests to WPT
    BUG=688116, 705685
    R=falken@chromium.org
    
    Review-Url: https://codereview.chromium.org/2778903002
    Cr-Commit-Position: refs/heads/master@{#461000}

--- third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/clients-matchall-on-evaluation-worker.js
+++ third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/clients-matchall-on-evaluation-worker.js
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-importScripts('test-helpers.js');
+importScripts('test-helpers.sub.js');
 
-var page_url = normalizeURL('../clients-matchall-on-evaluation.html');
+var page_url = normalizeURL('../clients-matchall-on-evaluation.https.html');
 
 self.clients.matchAll({includeUncontrolled: true})
   .then(function(clients) {
