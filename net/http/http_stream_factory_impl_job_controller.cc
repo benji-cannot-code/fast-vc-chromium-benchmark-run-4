7006b94c683afb31e7c15de14577b9fa2f43fb1c
xunjieli <xunjieli@chromium.org>
2017-03-31 14:22:55 -0700

    Remove one unused field in HttpStreamFactoryImpl
    The |request_map_| is updated but it isn't actually used, because the Job
    ownership model has been refactored into JobController.
    
    This CL also removes JobController::AttachJob().
    
    BUG=475060
    
    Review-Url: https://codereview.chromium.org/2787423002
    Cr-Commit-Position: refs/heads/master@{#461233}

--- net/http/http_stream_factory_impl_job_controller.cc
+++ net/http/http_stream_factory_impl_job_controller.cc
@@ -302,7 +302,6 @@ void HttpStreamFactoryImpl::JobController::OnStreamFailed(
     if (main_job_ && alternative_job_) {
       // Hey, we've got other jobs! Maybe one of them will succeed, let's just
       // ignore this failure.
-      factory_->request_map_.erase(job);
       if (job->job_type() == MAIN) {
         main_job_.reset();
       } else {
@@ -436,7 +435,6 @@ void HttpStreamFactoryImpl::JobController::OnResolveProxyComplete(
       this, ALTERNATIVE, session_, request_info, priority, server_ssl_config,
       proxy_ssl_config, destination, origin_url, alternative_proxy_server,
       enable_ip_based_pooling_, job->net_log().net_log()));
-  AttachJob(alternative_job_.get());
 
   can_start_alternative_proxy_job_ = false;
   main_job_is_blocked_ = true;
@@ -712,7 +710,6 @@ void HttpStreamFactoryImpl::JobController::CreateJobs(
       this, MAIN, session_, request_info, priority, server_ssl_config,
       proxy_ssl_config, destination, origin_url, enable_ip_based_pooling_,
       net_log_.net_log()));
-  AttachJob(main_job_.get());
 
   // Create an alternative job if alternative service is set up for this domain.
   const AlternativeService alternative_service =
@@ -733,7 +730,6 @@ void HttpStreamFactoryImpl::JobController::CreateJobs(
         this, ALTERNATIVE, session_, request_info, priority, server_ssl_config,
         proxy_ssl_config, alternative_destination, origin_url,
         alternative_service, enable_ip_based_pooling_, net_log_.net_log()));
-    AttachJob(alternative_job_.get());
 
     main_job_is_blocked_ = true;
     alternative_job_->Start(request_->stream_type());
@@ -746,11 +742,6 @@ void HttpStreamFactoryImpl::JobController::CreateJobs(
   main_job_->Start(request_->stream_type());
 }
 
-void HttpStreamFactoryImpl::JobController::AttachJob(Job* job) {
-  DCHECK(job);
-  factory_->request_map_[job] = request_;
-}
-
 void HttpStreamFactoryImpl::JobController::BindJob(Job* job) {
   DCHECK(request_);
   DCHECK(job);
@@ -760,7 +751,6 @@ void HttpStreamFactoryImpl::JobController::BindJob(Job* job) {
 
   job_bound_ = true;
   bound_job_ = job;
-  factory_->request_map_.erase(job);
 
   request_->net_log().AddEvent(
       NetLogEventType::HTTP_STREAM_REQUEST_BOUND_TO_JOB,
@@ -777,14 +767,10 @@ void HttpStreamFactoryImpl::JobController::CancelJobs() {
   RemoveRequestFromSpdySessionRequestMap();
   if (job_bound_)
     return;
-  if (alternative_job_) {
-    factory_->request_map_.erase(alternative_job_.get());
+  if (alternative_job_)
     alternative_job_.reset();
-  }
-  if (main_job_) {
-    factory_->request_map_.erase(main_job_.get());
+  if (main_job_)
     main_job_.reset();
-  }
 }
 
 void HttpStreamFactoryImpl::JobController::OrphanUnboundJob() {
@@ -793,11 +779,9 @@ void HttpStreamFactoryImpl::JobController::OrphanUnboundJob() {
 
   DCHECK(bound_job_);
   if (bound_job_->job_type() == MAIN && alternative_job_) {
-    factory_->request_map_.erase(alternative_job_.get());
     alternative_job_->Orphan();
   } else if (bound_job_->job_type() == ALTERNATIVE && main_job_) {
     // Orphan main job.
-    factory_->request_map_.erase(main_job_.get());
     // If ResumeMainJob() is not executed, reset |main_job_|. Otherwise,
     // OnOrphanedJobComplete() will clean up |this| when the job completes.
     // Use |main_job_is_blocked_| and |!main_job_wait_time_.is_zero()| instead
