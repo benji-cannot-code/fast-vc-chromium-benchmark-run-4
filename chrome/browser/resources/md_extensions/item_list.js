9b1f52645adf398cf805ea10b8181729537b4eb6
rdevlin.cronin <rdevlin.cronin@chromium.org>
2017-03-30 19:00:41 -0700

    [MD Extensions] Fix details page styling
    Update the details page sizing to the mocks and center it appropriately.
    Since the details page is no longer as large as the item grid, also
    add a fade out animation to avoid any jarring disappearance of items.
    This involves modifying AnimationHelper to take an array of animations.
    
    BUG=529395
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2780393003
    Cr-Commit-Position: refs/heads/master@{#460992}

--- chrome/browser/resources/md_extensions/item_list.js
+++ chrome/browser/resources/md_extensions/item_list.js
@@ -34,8 +34,8 @@ cr.define('extensions', function() {
     ready: function() {
       /** @type {extensions.AnimationHelper} */
       this.animationHelper = new extensions.AnimationHelper(this, this.$.list);
-      this.animationHelper.setEntryAnimation(extensions.Animation.FADE_IN);
-      this.animationHelper.setExitAnimation(extensions.Animation.HERO);
+      this.animationHelper.setEntryAnimations([extensions.Animation.FADE_IN]);
+      this.animationHelper.setExitAnimations([extensions.Animation.HERO]);
     },
 
     /**
