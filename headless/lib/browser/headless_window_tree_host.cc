f81dc874c341c6b1dd96745b8a28aacdb160c7a9
eseckler <eseckler@chromium.org>
2017-03-31 13:10:46 -0700

    [headless] Use individual aura::WindowTreeHosts per WebContents.
    This way, compositing concerns are handled separately per WebContents,
    which is required for BeginFrameControl.
    
    BUG=646774
    
    Review-Url: https://codereview.chromium.org/2787373002
    Cr-Commit-Position: refs/heads/master@{#461200}

--- headless/lib/browser/headless_window_tree_host.cc
+++ headless/lib/browser/headless_window_tree_host.cc
@@ -4,8 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "headless/lib/browser/headless_window_tree_host.h"
-#include "ui/aura/window.h"
 
+#include "headless/lib/browser/headless_focus_client.h"
+#include "headless/lib/browser/headless_window_parenting_client.h"
+#include "ui/aura/window.h"
 #include "ui/gfx/icc_profile.h"
 
 namespace headless {
@@ -14,6 +16,9 @@ HeadlessWindowTreeHost::HeadlessWindowTreeHost(const gfx::Rect& bounds)
     : bounds_(bounds) {
   CreateCompositor();
   OnAcceleratedWidgetAvailable();
+
+  focus_client_.reset(new HeadlessFocusClient());
+  aura::client::SetFocusClient(window(), focus_client_.get());
 }
 
 HeadlessWindowTreeHost::~HeadlessWindowTreeHost() {
