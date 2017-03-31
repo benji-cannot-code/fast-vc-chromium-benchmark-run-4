56d3c8fe12a51ee1f2c7d73867e1d6ed238cedc3
bshaya <bshaya@google.com>
2017-03-30 20:21:02 -0700

    [Chromecast] Fix cast_alsa_cma_backend_unittests for devices.
    BUG=internal/36703892
    TEST=cast_alsa_cma_backend_unittests
    
    Change-Id: I5d1d8592c923d44a2a18b6b46be5d212b3f0c9fa
    Review-Url: https://codereview.chromium.org/2784193002
    Cr-Commit-Position: refs/heads/master@{#461009}

--- chromecast/media/cma/backend/alsa/filter_group.cc
+++ chromecast/media/cma/backend/alsa/filter_group.cc
@@ -99,5 +99,10 @@ void FilterGroup::ClearActiveInputs() {
   active_inputs_.clear();
 }
 
+void FilterGroup::DisablePostProcessingForTest() {
+  post_processing_pipeline_ =
+      base::MakeUnique<PostProcessingPipeline>(nullptr, channels_);
+}
+
 }  // namespace media
 }  // namespace chromecast
--- chromecast/media/cma/backend/alsa/stream_mixer_alsa.h
+++ chromecast/media/cma/backend/alsa/stream_mixer_alsa.h
@@ -174,6 +174,7 @@ class StreamMixerAlsa {
   void OnFramesQueued();
 
   void SetAlsaWrapperForTest(std::unique_ptr<AlsaWrapper> alsa_wrapper);
+  void DisablePostProcessingForTest();
   void WriteFramesForTest();  // Can be called on any thread.
   void ClearInputsForTest();  // Removes all inputs.
 
