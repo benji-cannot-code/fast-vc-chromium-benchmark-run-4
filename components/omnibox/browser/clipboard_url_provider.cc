ecbe1d5f90d2f35e767f5b74278e99199b181ac3
mpearson <mpearson@chromium.org>
2017-03-31 10:29:51 -0700

    Omnibox Metrics - Record Clipboard URL Impressions
    Not tested, as this provider is only implemented on an iOS device and I don't have one.  I'm pretty sure this is straightforward enough to work right. :-)  [famous last words]
    
    BUG=704715
    
    Review-Url: https://codereview.chromium.org/2782393003
    Cr-Commit-Position: refs/heads/master@{#461161}

--- components/omnibox/browser/clipboard_url_provider.cc
+++ components/omnibox/browser/clipboard_url_provider.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/clipboard_url_provider.h"
 
 #include "base/logging.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/utf_string_conversions.h"
 #include "components/omnibox/browser/autocomplete_input.h"
 #include "components/omnibox/browser/autocomplete_provider_client.h"
@@ -51,6 +52,8 @@ void ClipboardURLProvider::Start(const AutocompleteInput& input,
         client_, input, input.current_url(), history_url_provider_, -1);
     matches_.push_back(verbatim_match);
   }
+  UMA_HISTOGRAM_BOOLEAN("Omnibox.ClipboardSuggestionShownWithCurrentURL",
+                        !matches_.empty());
 
   // Add the clipboard match. The relevance is 800 to beat ZeroSuggest results.
   AutocompleteMatch match(this, 800, false, AutocompleteMatchType::CLIPBOARD);
