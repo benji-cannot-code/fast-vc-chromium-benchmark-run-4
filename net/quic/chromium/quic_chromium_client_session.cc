339ab5224b6400412beb4391ab86e13726f60f6b
rch <rch@chromium.org>
2017-03-30 18:58:07 -0700

    Add a new histogram to track the causes of QUIC write errors post handshake.
    Net.QuicSession.WriteError.HanshakeConfirmed
    
    Review-Url: https://codereview.chromium.org/2787633003
    Cr-Commit-Position: refs/heads/master@{#460991}

--- net/quic/chromium/quic_chromium_client_session.cc
+++ net/quic/chromium/quic_chromium_client_session.cc
@@ -1190,6 +1190,10 @@ void QuicChromiumClientSession::OnNetworkConnected(
 void QuicChromiumClientSession::OnWriteError(int error_code) {
   DCHECK_NE(ERR_IO_PENDING, error_code);
   DCHECK_GT(0, error_code);
+  if (IsCryptoHandshakeConfirmed()) {
+    UMA_HISTOGRAM_SPARSE_SLOWLY("Net.QuicSession.WriteError.HandshakeConfirmed",
+                                -error_code);
+  }
   connection()->OnWriteError(error_code);
 }
 
