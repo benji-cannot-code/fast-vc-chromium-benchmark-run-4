844eaf42f4c9cd70ca019e462e880b9ff9bca50e
fs <fs@opera.com>
2017-03-31 03:38:13 -0700

    Implement Image::maybeAnimated for SVGImage
    This CL renames SVGImage::hasAnimations to maybeAnimated, overriding
    the implementation from the base class (Image.) The old method matches
    the new one in certainty of the reply, and allows SVGImages to be paused
    when being scrolled out of view etc.
    
    BUG=706152
    
    Review-Url: https://codereview.chromium.org/2783133002
    Cr-Commit-Position: refs/heads/master@{#461094}

--- third_party/WebKit/Source/core/svg/graphics/SVGImageTest.cpp
+++ third_party/WebKit/Source/core/svg/graphics/SVGImageTest.cpp
@@ -92,7 +92,7 @@ TEST_F(SVGImageTest, TimelineSuspendAndResume) {
 
   // Simulate a draw. Cause a frame (timer) to be scheduled.
   pumpFrame();
-  EXPECT_TRUE(image().hasAnimations());
+  EXPECT_TRUE(image().maybeAnimated());
   EXPECT_TRUE(timer->isActive());
 
   // Fire the timer/trigger a frame update. Since the observer always returns
@@ -120,7 +120,7 @@ TEST_F(SVGImageTest, ResetAnimation) {
 
   // Simulate a draw. Cause a frame (timer) to be scheduled.
   pumpFrame();
-  EXPECT_TRUE(image().hasAnimations());
+  EXPECT_TRUE(image().maybeAnimated());
   EXPECT_TRUE(timer->isActive());
 
   // Reset the animation. This will suspend the timeline but not cancel the
