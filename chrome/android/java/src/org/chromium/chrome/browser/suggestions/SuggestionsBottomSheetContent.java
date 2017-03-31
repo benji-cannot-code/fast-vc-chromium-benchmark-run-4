f28b81962604cab40f273c8b5a1a91316186e45f
peconn <peconn@chromium.org>
2017-03-31 07:52:46 -0700

    🏡 Refresh suggestions when SuggestionsBottomSheetContent is created.
    The sheet state is updated at the end of an interaction, but the
    SuggestionsBottomSheetContent is created at the start of the first
    pull-up, meaning that the check (now removed) will fail and suggestions
    will be absent on the first bottom sheet pull-up.
    
    Since the SuggestionsBottomSheetContent is created only when needed, I
    believe we won't refresh the suggestions unnecessarily.
    
    BUG=705760
    
    Review-Url: https://codereview.chromium.org/2792523003
    Cr-Commit-Position: refs/heads/master@{#461121}

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsBottomSheetContent.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsBottomSheetContent.java
@@ -81,10 +81,7 @@ public class SuggestionsBottomSheetContent implements BottomSheet.BottomSheetCon
                 adapter.refreshSuggestions();
             }
         });
-
-        if (activity.getBottomSheet().getSheetState() != BottomSheet.SHEET_STATE_PEEK) {
-            adapter.refreshSuggestions();
-        }
+        adapter.refreshSuggestions();
 
         mShadowView = (FadingShadowView) mView.findViewById(R.id.shadow);
         mShadowView.init(
