2585958e325fc494df8d9da633a355081cd64dde
twellington <twellington@chromium.org>
2017-03-31 13:24:51 -0700

    Revert "Revert "[Home] Add some histograms for Chrome Home""
    This reverts commit 826f594306b79439714faec487b06a9fd860c4a2.
    
    The fix is to check if the tab containing the NTP is already showing
    (e.g. from a back navigation or because it's the selected tab on
    startup) and call onNewTabPageShown() if it is rather than relying
    on a call to TabObserver#onShown().
    
    BUG=700592, 700593
    TBR=isherman@chromium.org, mdjones@chromium.org
    
    Review-Url: https://codereview.chromium.org/2788163002
    Cr-Commit-Position: refs/heads/master@{#461208}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/ChromeHomeNewTabPage.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/ChromeHomeNewTabPage.java
@@ -31,6 +31,7 @@ import org.chromium.chrome.browser.tab.TabObserver;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.widget.TintedImageButton;
 import org.chromium.chrome.browser.widget.bottomsheet.BottomSheet;
+import org.chromium.chrome.browser.widget.bottomsheet.BottomSheetMetrics;
 
 /**
  * The new tab page to display when Chrome Home is enabled.
@@ -117,7 +118,11 @@ public class ChromeHomeNewTabPage implements NativePage, TemplateUrlServiceObser
 
         mLogoDelegate = initializeLogoView();
         initializeCloseButton();
-        onNewTabPageShown();
+
+        // If the tab is already showing TabObserver#onShown() won't be called, so we need to call
+        // #onNewTabPageShown() directly.
+        boolean tabAlreadyShowing = mTabModelSelector.getCurrentTab() == mTab;
+        if (tabAlreadyShowing) onNewTabPageShown();
 
         // TODO(twellington): disallow moving the NTP to the other window in Android N+
         //                    multi-window mode.
@@ -192,6 +197,8 @@ public class ChromeHomeNewTabPage implements NativePage, TemplateUrlServiceObser
         if (getLayoutManager() != null && getLayoutManager().overviewVisible()) return;
 
         mBottomSheet.setSheetState(BottomSheet.SHEET_STATE_HALF, true);
+        mBottomSheet.getBottomSheetMetrics().recordSheetOpenReason(
+                BottomSheetMetrics.OPENED_BY_NEW_TAB_CREATION);
     }
 
     private boolean isTabChromeHomeNewTabPage(Tab tab) {
