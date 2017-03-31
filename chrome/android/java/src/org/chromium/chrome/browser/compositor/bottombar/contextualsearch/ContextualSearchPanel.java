731fa5b8c7c07d5ce625e07302c1620bfb1c8059
donnd <donnd@chromium.org>
2017-03-31 16:19:48 -0700

    [TTS] Skip expanded state when swiping down v2.
    The approach taken in CL 2780083002 had some minor problems.
    This reverts that approach and instead checks in getProjectedState
    whether we're doing the MAXIMIZED==>EXPANDED transition and
    projects to PEEKED instead.
    
    Also reenables testSwipeExpand.
    
    BUG=706179
    
    Review-Url: https://codereview.chromium.org/2785163003
    Cr-Commit-Position: refs/heads/master@{#461268}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/contextualsearch/ContextualSearchPanel.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/contextualsearch/ContextualSearchPanel.java
@@ -75,11 +75,6 @@ public class ContextualSearchPanel extends OverlayPanel {
      */
     private ContextualSearchSceneLayer mSceneLayer;
 
-    /**
-     * The velocity of the swipe gesture being handled.
-     */
-    private float mCurrentSwipeVelocity;
-
     // ============================================================================================
     // Constructor
     // ============================================================================================
@@ -225,16 +220,7 @@ public class ContextualSearchPanel extends OverlayPanel {
 
     @Override
     protected boolean isSupportedState(PanelState state) {
-        if (mCurrentSwipeVelocity > 0 && state == PanelState.EXPANDED) return false;
-
         return canDisplayContentInPanel() || state != PanelState.MAXIMIZED;
-    }
-
-    @Override
-    protected float getThresholdToNextState() {
-        if (mCurrentSwipeVelocity > 0) return 0.30f;
-
-        return super.getThresholdToNextState();
     }
 
     @Override
@@ -248,7 +234,6 @@ public class ContextualSearchPanel extends OverlayPanel {
 
     @Override
     protected PanelState getProjectedState(float velocity) {
-        mCurrentSwipeVelocity = velocity;
         PanelState projectedState = super.getProjectedState(velocity);
 
         // Prevent the fling gesture from moving the Panel from PEEKED to MAXIMIZED. This is to
@@ -258,6 +243,12 @@ public class ContextualSearchPanel extends OverlayPanel {
                 && projectedState == PanelState.MAXIMIZED
                 && getPanelState() == PanelState.PEEKED) {
             projectedState = PanelState.EXPANDED;
+        }
+
+        // If we're swiping the panel down from MAXIMIZED skip the EXPANDED state and go all the
+        // way to PEEKED.
+        if (getPanelState() == PanelState.MAXIMIZED && projectedState == PanelState.EXPANDED) {
+            projectedState = PanelState.PEEKED;
         }
 
         return projectedState;
