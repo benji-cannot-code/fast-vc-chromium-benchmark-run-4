08d65ef76ac69a574040ec4b6159f19ad6bb7eae
chrishtr <chrishtr@chromium.org>
2017-03-31 15:51:50 -0700

    [SPv2] Replay DragImages into PropertyTreeState of the enclosing stacking context.
    Previously we were replaying them into the root PropertyTreeState. This is
    incorrect, because it would include any transform, clip or effect nodes that
    are ancestors of node that we want a DragImage from.
    
    BUG=665259
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    .
    
    Review-Url: https://codereview.chromium.org/2784373004
    Cr-Commit-Position: refs/heads/master@{#461261}

--- third_party/WebKit/Source/platform/graphics/paint/PaintRecordBuilder.h
+++ third_party/WebKit/Source/platform/graphics/paint/PaintRecordBuilder.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/geometry/FloatRect.h"
 #include "platform/graphics/paint/DisplayItemClient.h"
 #include "platform/graphics/paint/PaintRecord.h"
+#include "platform/graphics/paint/PropertyTreeState.h"
 #include "third_party/skia/include/core/SkRefCnt.h"
 #include "wtf/Noncopyable.h"
 
@@ -51,7 +52,9 @@ class PLATFORM_EXPORT PaintRecordBuilder final : public DisplayItemClient {
   // Returns a PaintRecord capturing all drawing performed on the builder's
   // context since construction. If SPv2 is on, flattens all paint chunks
   // into PropertyTreeState::root() space.
-  sk_sp<PaintRecord> endRecording();
+  // In SPv2 mode, replays into the ancestor state given by |replayState|.
+  sk_sp<PaintRecord> endRecording(
+      const PropertyTreeState& replayState = PropertyTreeState::root());
 
   // DisplayItemClient methods
   String debugName() const final { return "PaintRecordBuilder"; }
