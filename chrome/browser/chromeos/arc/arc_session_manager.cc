4c2f5d882aae319af811e8d6bb066963ba921c84
khmel <khmel@chromium.org>
2017-03-30 18:45:15 -0700

    arc: Enable UI to send feedback on "Server Communication Error"
    This is to increase reporting rate for this error. Currently users sent
    report for this error vary rare. However number in UMA is quite high.
    
    TEST=Manually
    BUG=707062
    BUG=b/36785117
    
    Review-Url: https://codereview.chromium.org/2787213002
    Cr-Commit-Position: refs/heads/master@{#460986}

--- chrome/browser/chromeos/arc/arc_session_manager.cc
+++ chrome/browser/chromeos/arc/arc_session_manager.cc
@@ -375,7 +375,7 @@ void ArcSessionManager::OnProvisioningFinished(ProvisioningResult result) {
       profile_->GetPrefs()->SetBoolean(prefs::kArcSignedIn, false);
     ShutdownSession();
     if (support_host_)
-      support_host_->ShowError(error, false);
+      support_host_->ShowError(error, true);
     return;
   }
 
@@ -809,7 +809,7 @@ void ArcSessionManager::OnAndroidManagementChecked(
     case policy::AndroidManagementClient::Result::ERROR:
       if (support_host_) {
         support_host_->ShowError(
-            ArcSupportHost::Error::SERVER_COMMUNICATION_ERROR, false);
+            ArcSupportHost::Error::SERVER_COMMUNICATION_ERROR, true);
       }
       UpdateOptInCancelUMA(OptInCancelReason::NETWORK_ERROR);
       break;
