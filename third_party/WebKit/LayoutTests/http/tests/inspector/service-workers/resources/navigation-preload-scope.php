19440120b0b03d011c1adf4a1ca646becbbe432b
horo <horo@chromium.org>
2017-04-01 17:08:31 -0700

    Support redirect responses for NavigationPreload
    BUG=691503
    
    Review-Url: https://codereview.chromium.org/2790433003
    Cr-Commit-Position: refs/heads/master@{#461333}

--- third_party/WebKit/LayoutTests/http/tests/inspector/service-workers/resources/navigation-preload-scope.php
+++ third_party/WebKit/LayoutTests/http/tests/inspector/service-workers/resources/navigation-preload-scope.php
@@ -1,8 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
-if (isset($_GET['RedirectError'])) {
-  header('Location: dummy.html');
+if (isset($_GET['Redirect'])) {
   header('HTTP/1.0 302 Found');
+  header('Location: ./navigation-preload-redirected.html');
 } if (isset($_GET['BrokenChunked'])) {
   header("Content-type: text/html; charset=UTF-8");
   header("Transfer-encoding: chunked");
