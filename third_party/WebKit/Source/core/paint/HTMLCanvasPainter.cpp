01c88af714c4d6f90a6b40fbe1153afc172d54f2
chrishtr <chrishtr@chromium.org>
2017-04-02 16:21:46 -0700

    Some cleanup after https://codereview.chromium.org/2751433002.
    BUG=665259
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2787173005
    Cr-Commit-Position: refs/heads/master@{#461350}

--- third_party/WebKit/Source/core/paint/HTMLCanvasPainter.cpp
+++ third_party/WebKit/Source/core/paint/HTMLCanvasPainter.cpp
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/paint/LayoutObjectDrawingRecorder.h"
 #include "core/paint/PaintInfo.h"
 #include "platform/geometry/LayoutPoint.h"
-#include "platform/graphics/paint/ClipRecorder.h"
 #include "platform/graphics/paint/ForeignLayerDisplayItem.h"
 
 namespace blink {
--- third_party/WebKit/Source/core/paint/SVGRootPainter.cpp
+++ third_party/WebKit/Source/core/paint/SVGRootPainter.cpp
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/paint/PaintInfo.h"
 #include "core/paint/PaintTiming.h"
 #include "core/paint/SVGPaintContext.h"
-#include "core/paint/TransformRecorder.h"
 #include "core/svg/SVGSVGElement.h"
 #include "wtf/Optional.h"
 
