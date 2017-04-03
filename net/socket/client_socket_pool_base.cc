7c4a9636491ee2e34fbe8a4d3e39b2a9ec85b68b
xunjieli <xunjieli@chromium.org>
2017-04-03 09:48:34 -0700

    Fix other invocations of RemoveGroup(GroupMap::iterator it)
    This CL fixes two instances of RemoveGroup(GroupMap::iterator it) to not
    inline postincrement. The attached bug shows that the inlined
    postincrement is not safe.
    
    BUG=700549
    
    Review-Url: https://codereview.chromium.org/2790163002
    Cr-Commit-Position: refs/heads/master@{#461454}

--- net/socket/client_socket_pool_base.cc
+++ net/socket/client_socket_pool_base.cc
@@ -783,14 +783,11 @@ void ClientSocketPoolBaseHelper::CleanupIdleSockets(bool force) {
   // inside the inner loop, since it shouldn't change by any meaningful amount.
   base::TimeTicks now = base::TimeTicks::Now();
 
-  GroupMap::iterator i = group_map_.begin();
-  while (i != group_map_.end()) {
+  for (GroupMap::iterator i = group_map_.begin(); i != group_map_.end();) {
     Group* group = i->second;
     CleanupIdleSocketsInGroup(force, group, now);
     // Delete group if no longer needed.
     if (group->IsEmpty()) {
-      // TODO(xunjieli): Evaluate other RemoveGroup() instances after
-      // crbug.com/700549 is fixed.
       GroupMap::iterator old = i++;
       RemoveGroup(old);
     } else {
@@ -1156,10 +1153,8 @@ void ClientSocketPoolBaseHelper::CancelAllConnectJobs() {
 
     // Delete group if no longer needed.
     if (group->IsEmpty()) {
-      // RemoveGroup() will call .erase() which will invalidate the iterator,
-      // but i will already have been incremented to a valid iterator before
-      // RemoveGroup() is called.
-      RemoveGroup(i++);
+      GroupMap::iterator old = i++;
+      RemoveGroup(old);
     } else {
       ++i;
     }
@@ -1180,10 +1175,8 @@ void ClientSocketPoolBaseHelper::CancelAllRequestsWithError(int error) {
 
     // Delete group if no longer needed.
     if (group->IsEmpty()) {
-      // RemoveGroup() will call .erase() which will invalidate the iterator,
-      // but i will already have been incremented to a valid iterator before
-      // RemoveGroup() is called.
-      RemoveGroup(i++);
+      GroupMap::iterator old = i++;
+      RemoveGroup(old);
     } else {
       ++i;
     }
