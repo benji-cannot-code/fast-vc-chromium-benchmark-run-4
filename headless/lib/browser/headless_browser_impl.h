b40a93d56962b094319b6db3583f1ab56b961451
eseckler <eseckler@chromium.org>
2017-04-03 06:10:36 -0700

    [headless] Use individual aura::WindowTreeHosts per WebContents.
    This way, compositing concerns are handled separately per WebContents,
    which is required for BeginFrameControl.
    
    BUG=646774
    
    Review-Url: https://codereview.chromium.org/2787373002
    Cr-Original-Commit-Position: refs/heads/master@{#461200}
    Committed: https://chromium.googlesource.com/chromium/src/+/56b635d784ad0eacbd06e744e43c115e48993a04
    Review-Url: https://codereview.chromium.org/2787373002
    Cr-Commit-Position: refs/heads/master@{#461414}

--- headless/lib/browser/headless_browser_impl.h
+++ headless/lib/browser/headless_browser_impl.h
@@ -19,16 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "headless/lib/browser/headless_web_contents_impl.h"
 #include "headless/public/headless_export.h"
 
-#if defined(USE_AURA)
-#include "headless/lib/browser/headless_window_tree_host.h"
-
-namespace aura {
-namespace client {
-class FocusClient;
-}
-}
-#endif
-
 namespace headless {
 
 class HeadlessBrowserContextImpl;
@@ -82,17 +72,9 @@ class HEADLESS_EXPORT HeadlessBrowserImpl : public HeadlessBrowser {
   void PlatformInitialize();
   void PlatformCreateWindow();
   void PlatformInitializeWebContents(const gfx::Size& initial_size,
-                                     content::WebContents* web_contents);
+                                     HeadlessWebContentsImpl* web_contents);
 
  protected:
-#if defined(USE_AURA)
-  // TODO(eseckler): Currently one window and one window_tree_host
-  // is used for all web contents. We should probably use one
-  // window per web contents, but additional investigation is needed.
-  std::unique_ptr<HeadlessWindowTreeHost> window_tree_host_;
-  std::unique_ptr<aura::client::FocusClient> focus_client_;
-#endif
-
   base::Callback<void(HeadlessBrowser*)> on_start_callback_;
   HeadlessBrowser::Options options_;
   HeadlessBrowserMainParts* browser_main_parts_;  // Not owned.
