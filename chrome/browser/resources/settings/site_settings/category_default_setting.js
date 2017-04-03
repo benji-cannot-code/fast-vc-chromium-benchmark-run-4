4b43bbf63e94c216e678fd6d347b91d12d6448f2
dschuyler <dschuyler@chromium.org>
2017-04-03 13:59:08 -0700

    [MD settings] clickable toggle labels on category default toggles
    This CL makes top level category default rows react to mouse clicks.
    These are the one or two rows at the top of a content settings category
    (such as "cookies"). The rows are toggles. Before this CL the labels on
    the toggles wouldn't act on mouse clicks when they should.
    
    Also, there are several name changes and tiddying up that could be in a
    separate CL if that is preferred.
    
    BUG=704003
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2788853003
    Cr-Commit-Position: refs/heads/master@{#461533}

--- chrome/browser/resources/settings/site_settings/category_default_setting.js
+++ chrome/browser/resources/settings/site_settings/category_default_setting.js
@@ -14,6 +14,20 @@ Polymer({
   behaviors: [SiteSettingsBehavior, WebUIListenerBehavior],
 
   properties: {
+    /* The second line, shown under the |optionLabel_| line. (optional) */
+    optionDescription: String,
+
+    /* The second line, shown under the |subOptionLabel| line. (optional) */
+    subOptionDescription: String,
+
+    /* The sub-option is a separate toggle. Setting this label will show the
+     * additional toggle. Shown above |subOptionDescription|. (optional) */
+    subOptionLabel: String,
+
+    /* The on/off text for |optionLabel_| below. */
+    toggleOffLabel: String,
+    toggleOnLabel: String,
+
     /** @private {chrome.settingsPrivate.PrefObject} */
     controlParams_: {
       type: Object,
@@ -22,6 +36,13 @@ Polymer({
       },
     },
 
+    /**
+     * The label to be shown next to the toggle (above |optionDescription|).
+     * This will be either toggleOffLabel or toggleOnLabel.
+     * @private
+     */
+    optionLabel_: String,
+
     /** @private {!DefaultContentSetting} */
     priorDefaultContentSetting_: {
       type: Object,
@@ -29,12 +50,6 @@ Polymer({
         return /** @type {DefaultContentSetting} */({});
       },
     },
-
-    /**
-     * The description to be shown next to the slider.
-     * @private
-     */
-    sliderDescription_: String,
 
     /**
      * Cookies and Flash settings have a sub-control that is used to mimic a
@@ -46,16 +61,6 @@ Polymer({
       value: function() {
         return /** @type {chrome.settingsPrivate.PrefObject} */({});
       },
-    },
-
-    /* Labels for the toggle on/off positions. */
-    toggleOffLabel: String,
-    toggleOnLabel: String,
-
-    subOptionLabel: String,
-    subOptionSecondary: {
-      type: String,
-      value: null,  // Needs default value so binding fires upon initialization.
     },
   },
 
@@ -194,7 +199,7 @@ Polymer({
           this.category).then(function(defaultValue) {
             this.updateControlParams_(defaultValue);
 
-            // Flash only shows ALLOW or BLOCK descriptions on the slider.
+            // Flash only shows ALLOW or BLOCK descriptions on the toggle.
             var setting = defaultValue.setting;
             if (this.category == settings.ContentSettingsTypes.PLUGINS &&
                 setting == settings.PermissionValues.IMPORTANT_CONTENT) {
@@ -205,7 +210,7 @@ Polymer({
               setting = settings.PermissionValues.ALLOW;
             }
             var categoryEnabled = setting != settings.PermissionValues.BLOCK;
-            this.sliderDescription_ =
+            this.optionLabel_ =
                 categoryEnabled ? this.toggleOnLabel : this.toggleOffLabel;
           }.bind(this));
   },
@@ -218,14 +223,4 @@ Polymer({
     return this.category == settings.ContentSettingsTypes.POPUPS &&
         loadTimeData.getBoolean('isGuest');
   },
-
-  /**
-   * Returns classname to indicate secondary label exists.
-   * @param {boolean} subOptionLabel
-   * @return {string}
-   * @private
-   */
-  subOptionClass_: function(subOptionLabel) {
-    return subOptionLabel ? 'two-line' : '';
-  }
 });
