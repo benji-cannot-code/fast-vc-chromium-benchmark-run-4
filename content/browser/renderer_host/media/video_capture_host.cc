644cc9deccfbbf9617cb4f3b994bc11a8e4191cd
chfremer <chfremer@chromium.org>
2017-04-03 11:41:53 -0700

    [Mojo Video Capture] Add support to BuildableVideoCaptureDevice for aborting the device start.
    This CL is supposed to be a pure refactoring. There should be no changes to
    existing behavior.
    
    This is part of a series of CLs with the goal of separating the class
    VideoCaptureManager from knowledge of things that are going to be private
    implementation details of the video capture service. The end goal is to have
    VideoCaptureManager talk to an abstraction that can be implemented by either the
    video capture Mojo service or the existing (legacy) in-process implementation.
    
    Changes in this CL:
    * When a request to stop a device comes in while the device is still starting up
      asynchronously, the device needs to be stopped as soon as it has finished
      starting up. This CL moves the logic for this from VideoCaptureManager to
      implementations of BuildableVideoCaptureDevice.
    * Some naming improvements
    
    This CL is part of the Mojo Video Capture work. For the bigger picture,
    see [1] CL17.
    
    BUG=584797
    TEST=
      content_browsertests --gtest_filter="VideoCaptureBrowserTest.*"
      content_unittests --gtest_filter="*Video*"
    
    [1] https://docs.google.com/a/chromium.org/document/d/1Qw7rw1AJy0QHXjha36jZNiEuxsxWslJ_X-zpOhijvI8/edit?usp=sharing
    
    Review-Url: https://codereview.chromium.org/2753073006
    Cr-Commit-Position: refs/heads/master@{#461486}

--- content/browser/renderer_host/media/video_capture_host.cc
+++ content/browser/renderer_host/media/video_capture_host.cc
@@ -40,7 +40,7 @@ VideoCaptureHost::~VideoCaptureHost() {
     const base::WeakPtr<VideoCaptureController>& controller = it->second;
     if (controller) {
       const VideoCaptureControllerID controller_id(it->first);
-      media_stream_manager_->video_capture_manager()->StopCaptureForClient(
+      media_stream_manager_->video_capture_manager()->DisconnectClient(
           controller.get(), controller_id, this, false);
       ++it;
     } else {
@@ -143,7 +143,7 @@ void VideoCaptureHost::Start(int32_t device_id,
   }
 
   controllers_[controller_id] = base::WeakPtr<VideoCaptureController>();
-  media_stream_manager_->video_capture_manager()->StartCaptureForClient(
+  media_stream_manager_->video_capture_manager()->ConnectClient(
       session_id, params, controller_id, this,
       base::Bind(&VideoCaptureHost::OnControllerAdded,
                  weak_factory_.GetWeakPtr(), device_id));
@@ -296,7 +296,7 @@ void VideoCaptureHost::OnControllerAdded(
   auto it = controllers_.find(controller_id);
   if (it == controllers_.end()) {
     if (controller) {
-      media_stream_manager_->video_capture_manager()->StopCaptureForClient(
+      media_stream_manager_->video_capture_manager()->DisconnectClient(
           controller.get(), controller_id, this, false);
     }
     return;
@@ -328,7 +328,7 @@ void VideoCaptureHost::DeleteVideoCaptureController(
   if (!controller)
     return;
 
-  media_stream_manager_->video_capture_manager()->StopCaptureForClient(
+  media_stream_manager_->video_capture_manager()->DisconnectClient(
       controller.get(), controller_id, this, on_error);
 }
 
