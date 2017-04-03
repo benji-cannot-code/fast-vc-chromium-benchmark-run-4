1382988eb74d71f5827e902de77e3451bf9877a4
rch <rch@chromium.org>
2017-04-03 12:44:51 -0700

    Move TestTaskRunner to net/quic/chromium/
    Review-Url: https://codereview.chromium.org/2794783002
    Cr-Commit-Position: refs/heads/master@{#461506}

--- net/quic/chromium/quic_chromium_alarm_factory_test.cc
+++ net/quic/chromium/quic_chromium_alarm_factory_test.cc
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/quic/chromium/quic_chromium_alarm_factory.h"
 
+#include "net/quic/chromium/test_task_runner.h"
 #include "net/quic/test_tools/mock_clock.h"
-#include "net/quic/test_tools/test_task_runner.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace net {
