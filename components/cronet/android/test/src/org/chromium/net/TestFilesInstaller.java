7b0183fd865b3acaefdff7d3e5b3de59315bf03b
pauljensen <pauljensen@chromium.org>
2017-04-03 09:00:12 -0700

    [Cronet] Use org.chromium.base.Log instead of android.util.Log where possible
    It's not possible in:
    -Cronet's API
    -Cronet's Java-only implementation
    -Cronet's shared (between Java-only and native) implementation
    -Cronet's sample app
    -Cronet's smoketest
    As they do not depend on org.chromium.base package, for good reason.
    
    BUG=488547
    R=xunjieli
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_cronet_tester
    
    Review-Url: https://codereview.chromium.org/2794923002
    Cr-Commit-Position: refs/heads/master@{#461441}

--- components/cronet/android/test/src/org/chromium/net/TestFilesInstaller.java
+++ components/cronet/android/test/src/org/chromium/net/TestFilesInstaller.java
@@ -7,9 +7,9 @@ package org.chromium.net;
 
 import android.content.Context;
 import android.content.res.AssetManager;
-import android.util.Log;
 
 import org.chromium.base.FileUtils;
+import org.chromium.base.Log;
 import org.chromium.base.PathUtils;
 import org.chromium.base.annotations.SuppressFBWarnings;
 
