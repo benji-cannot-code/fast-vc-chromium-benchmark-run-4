702c82d360cf7fbcb982511976ce3022bd56be3d
peria <peria@chromium.org>
2017-04-03 10:35:17 -0700

    Move event, state, port1, and port2 from V8HiddenValue to V8PrivateProperty
    BUG=611864
    
    Review-Url: https://codereview.chromium.org/2784473002
    Cr-Commit-Position: refs/heads/master@{#461463}

--- third_party/WebKit/Source/bindings/core/v8/V8AbstractEventListener.cpp
+++ third_party/WebKit/Source/bindings/core/v8/V8AbstractEventListener.cpp
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/V8Event.h"
 #include "bindings/core/v8/V8EventListenerHelper.h"
 #include "bindings/core/v8/V8EventTarget.h"
-#include "bindings/core/v8/V8HiddenValue.h"
+#include "bindings/core/v8/V8PrivateProperty.h"
 #include "core/dom/Document.h"
 #include "core/dom/DocumentParser.h"
 #include "core/events/BeforeUnloadEvent.h"
@@ -128,16 +128,16 @@ void V8AbstractEventListener::invokeEventHandler(ScriptState* scriptState,
     v8::TryCatch tryCatch(isolate());
     tryCatch.SetVerbose(true);
 
+    v8::Local<v8::Object> global = scriptState->context()->Global();
+    V8PrivateProperty::Symbol eventSymbol =
+        V8PrivateProperty::getGlobalEvent(isolate());
     // Save the old 'event' property so we can restore it later.
-    v8::Local<v8::Value> savedEvent = V8HiddenValue::getHiddenValue(
-        scriptState, scriptState->context()->Global(),
-        V8HiddenValue::event(isolate()));
+    v8::Local<v8::Value> savedEvent = eventSymbol.getOrUndefined(global);
     tryCatch.Reset();
 
     // Make the event available in the global object, so LocalDOMWindow can
     // expose it.
-    V8HiddenValue::setHiddenValue(scriptState, scriptState->context()->Global(),
-                                  V8HiddenValue::event(isolate()), jsEvent);
+    eventSymbol.set(global, jsEvent);
     tryCatch.Reset();
 
     returnValue = callListenerFunction(scriptState, jsEvent, event);
@@ -155,14 +155,7 @@ void V8AbstractEventListener::invokeEventHandler(ScriptState* scriptState,
 
     // Restore the old event. This must be done for all exit paths through this
     // method.
-    if (savedEvent.IsEmpty())
-      V8HiddenValue::setHiddenValue(
-          scriptState, scriptState->context()->Global(),
-          V8HiddenValue::event(isolate()), v8::Undefined(isolate()));
-    else
-      V8HiddenValue::setHiddenValue(
-          scriptState, scriptState->context()->Global(),
-          V8HiddenValue::event(isolate()), savedEvent);
+    eventSymbol.set(global, savedEvent);
     tryCatch.Reset();
   }
 
--- third_party/WebKit/Source/bindings/core/v8/custom/V8MessageChannelCustom.cpp
+++ third_party/WebKit/Source/bindings/core/v8/custom/V8MessageChannelCustom.cpp
@@ -32,8 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/V8MessageChannel.h"
 
 #include "bindings/core/v8/V8Binding.h"
-#include "bindings/core/v8/V8HiddenValue.h"
 #include "bindings/core/v8/V8MessagePort.h"
+#include "bindings/core/v8/V8PrivateProperty.h"
 #include "core/dom/MessageChannel.h"
 #include "core/workers/WorkerGlobalScope.h"
 #include "wtf/RefPtr.h"
@@ -42,8 +42,9 @@ namespace blink {
 
 void V8MessageChannel::constructorCustom(
     const v8::FunctionCallbackInfo<v8::Value>& info) {
-  ExecutionContext* context = currentExecutionContext(info.GetIsolate());
+  v8::Isolate* isolate = info.GetIsolate();
 
+  ExecutionContext* context = currentExecutionContext(isolate);
   MessageChannel* channel = MessageChannel::create(context);
 
   v8::Local<v8::Object> wrapper = info.Holder();
@@ -51,17 +52,13 @@ void V8MessageChannel::constructorCustom(
   // Create references from the MessageChannel wrapper to the two
   // MessagePort wrappers to make sure that the MessagePort wrappers
   // stay alive as long as the MessageChannel wrapper is around.
-  ScriptState* scriptState = ScriptState::current(info.GetIsolate());
-  V8HiddenValue::setHiddenValue(
-      scriptState, wrapper, V8HiddenValue::port1(info.GetIsolate()),
-      ToV8(channel->port1(), info.Holder(), info.GetIsolate()));
-  V8HiddenValue::setHiddenValue(
-      scriptState, wrapper, V8HiddenValue::port2(info.GetIsolate()),
-      ToV8(channel->port2(), info.Holder(), info.GetIsolate()));
+  V8PrivateProperty::getMessageChannelPort1(isolate).set(
+      wrapper, ToV8(channel->port1(), wrapper, isolate));
+  V8PrivateProperty::getMessageChannelPort2(isolate).set(
+      wrapper, ToV8(channel->port2(), wrapper, isolate));
 
-  v8SetReturnValue(info,
-                   V8DOMWrapper::associateObjectWithWrapper(
-                       info.GetIsolate(), channel, &wrapperTypeInfo, wrapper));
+  v8SetReturnValue(info, V8DOMWrapper::associateObjectWithWrapper(
+                             isolate, channel, &wrapperTypeInfo, wrapper));
 }
 
 }  // namespace blink
