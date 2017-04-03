0b2c9f3bac546cb64f1093139d6f233233900ad8
meade <meade@chromium.org>
2017-04-03 05:13:39 -0700

    Add a virtual destructor for InterpolationType
    Without this, in some cases we get Illegal Instruction crashes.
    
    BUG=702695
    
    Review-Url: https://codereview.chromium.org/2790083002
    Cr-Commit-Position: refs/heads/master@{#461410}

--- third_party/WebKit/Source/core/animation/InterpolationType.h
+++ third_party/WebKit/Source/core/animation/InterpolationType.h
@@ -32,6 +32,8 @@ class InterpolationType {
   WTF_MAKE_NONCOPYABLE(InterpolationType);
 
  public:
+  virtual ~InterpolationType() {}
+
   PropertyHandle getProperty() const { return m_property; }
 
   // ConversionCheckers are returned from calls to maybeConvertPairwise() and
