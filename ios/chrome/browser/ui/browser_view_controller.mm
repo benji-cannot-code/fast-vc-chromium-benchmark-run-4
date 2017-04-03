0ee7c17f0629d15c3a298fc3753e9a786cbde0af
eugenebut <eugenebut@chromium.org>
2017-04-03 09:04:19 -0700

    Cleaned up unneeded includes of private web headers.
    BUG=620489,620465
    
    Review-Url: https://codereview.chromium.org/2785763003
    Cr-Commit-Position: refs/heads/master@{#461442}

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -172,7 +172,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/public/provider/chrome/browser/voice/voice_search_controller.h"
 #include "ios/public/provider/chrome/browser/voice/voice_search_controller_delegate.h"
 #include "ios/public/provider/chrome/browser/voice/voice_search_provider.h"
-#include "ios/web/navigation/navigation_manager_impl.h"
 #include "ios/web/public/active_state_manager.h"
 #include "ios/web/public/navigation_item.h"
 #import "ios/web/public/navigation_manager.h"
