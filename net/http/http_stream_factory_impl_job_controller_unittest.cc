245599e5bfe35fcd97bdba02de27b6611231436f
rch <rch@chromium.org>
2017-04-03 14:12:04 -0700

    Move QuicStreamFactoryPeer from net/quic/test_tools/ to net/quic/chromium/
    BUG=
    
    Review-Url: https://codereview.chromium.org/2783143004
    Cr-Commit-Position: refs/heads/master@{#461540}

--- net/http/http_stream_factory_impl_job_controller_unittest.cc
+++ net/http/http_stream_factory_impl_job_controller_unittest.cc
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/proxy/proxy_config_service_fixed.h"
 #include "net/proxy/proxy_info.h"
 #include "net/proxy/proxy_service.h"
-#include "net/quic/test_tools/quic_stream_factory_peer.h"
+#include "net/quic/chromium/quic_stream_factory_peer.h"
 #include "net/socket/socket_test_util.h"
 #include "net/spdy/spdy_test_util_common.h"
 #include "testing/gmock/include/gmock/gmock.h"
