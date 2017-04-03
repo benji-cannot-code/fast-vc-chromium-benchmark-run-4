6603bd6243ad482c7f23e12b9276ae73a20dbebe
yosin <yosin@chromium.org>
2017-04-03 00:32:09 -0700

    Make InsertIncrementalTextCommand to handle surrogate pair properly
    This patch makes |InsertIncrementalTextCommand| to handle surrogate pairs
    properly. Before this patch, |InsertIncrementalTextCommand| to attempt to
    replace trailing surrogate pairs code unit with surrogate pair then produces
    surrogate pairs + trailing surrogate pair.
    
    Example:
    Old text: replace D83D DE3A (U+1F63A) by D83D DE38 (U+1F638)
    Current behavior: D83D DE3A DE38
    New behavior: D83D DE38
    
    This is cause by computeCommonPrefixLength(oldString, newString) returns
    1 in above example where middle of surrogate pairs.
    
    BUG=706166
    TEST=run_webkit_unit_tests --gtest_filter=InsertIncrementalTextCommandTest.SurrogatePairs*
    
    Review-Url: https://codereview.chromium.org/2782823002
    Cr-Commit-Position: refs/heads/master@{#461382}

--- third_party/WebKit/Source/core/editing/commands/InsertIncrementalTextCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/InsertIncrementalTextCommand.cpp
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/PlainTextRange.h"
 #include "core/editing/VisibleUnits.h"
 #include "core/editing/iterators/CharacterIterator.h"
+#include "core/editing/state_machines/ForwardCodePointStateMachine.h"
 #include "core/html/HTMLSpanElement.h"
 
 namespace blink {
@@ -21,9 +22,15 @@ namespace {
 
 size_t computeCommonPrefixLength(const String& str1, const String& str2) {
   const size_t maxCommonPrefixLength = std::min(str1.length(), str2.length());
+  ForwardCodePointStateMachine codePointStateMachine;
+  int result = 0;
   for (size_t index = 0; index < maxCommonPrefixLength; ++index) {
     if (str1[index] != str2[index])
-      return index;
+      return result;
+    codePointStateMachine.feedFollowingCodeUnit(str1[index]);
+    if (!codePointStateMachine.atCodePointBoundary())
+      continue;
+    result = index;
   }
   return maxCommonPrefixLength;
 }
--- third_party/WebKit/Source/core/editing/commands/InsertIncrementalTextCommand.h
+++ third_party/WebKit/Source/core/editing/commands/InsertIncrementalTextCommand.h
@@ -10,7 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class InsertIncrementalTextCommand final : public InsertTextCommand {
+class CORE_EXPORT InsertIncrementalTextCommand final
+    : public InsertTextCommand {
  public:
   static InsertIncrementalTextCommand* create(
       Document&,
