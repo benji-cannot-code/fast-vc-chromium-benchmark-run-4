ac31026fbf2e560fc5def1cc9ae8fc63f2900ead
rlanday <rlanday@chromium.org>
2017-04-03 14:27:18 -0700

    Remove unnecessary include from SVGInlineTextBoxPainter.cpp
    This file unnecessarily includes RenderedDocumentMarker.h.
    
    Pointed out in code review for another CL:
    https://codereview.chromium.org/2723663002/diff/140001/third_party/WebKit/Source/core/paint/SVGInlineTextBoxPainter.cpp#oldcode9
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2763603003
    Cr-Commit-Position: refs/heads/master@{#461545}

--- third_party/WebKit/Source/core/paint/SVGInlineTextBoxPainter.cpp
+++ third_party/WebKit/Source/core/paint/SVGInlineTextBoxPainter.cpp
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/editing/Editor.h"
 #include "core/editing/markers/DocumentMarkerController.h"
-#include "core/editing/markers/RenderedDocumentMarker.h"
 #include "core/frame/LocalFrame.h"
 #include "core/layout/LayoutTheme.h"
 #include "core/layout/api/LineLayoutAPIShim.h"
