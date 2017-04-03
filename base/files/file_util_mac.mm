a4f3d68cd3775fce65c5f59412263376d2a1e3b9
iannucci <iannucci@chromium.org>
2017-04-03 13:04:32 -0700

    [base/files] Respect MAC_CHROMIUM_TMPDIR instead of TMPDIR on macOS.
    Some folks are currently setting $TMPDIR to very long values on macOS,
    which causes socket name length issues when using $TMPDIR.
    
    R=mattm@chromium.org
    BUG=698759, 696803
    
    Review-Url: https://codereview.chromium.org/2770753006
    Cr-Commit-Position: refs/heads/master@{#461515}

--- base/files/file_util_mac.mm
+++ base/files/file_util_mac.mm
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/Foundation.h>
 #include <copyfile.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "base/files/file_path.h"
+#include "base/logging.h"
 #include "base/mac/foundation_util.h"
 #include "base/strings/string_util.h"
 #include "base/threading/thread_restrictions.h"
@@ -25,10 +27,14 @@ bool CopyFile(const FilePath& from_path, const FilePath& to_path) {
 }
 
 bool GetTempDir(base::FilePath* path) {
-  // In order to facilitate hermetic runs on macOS, first check $TMPDIR.
-  // NOTE: $TMPDIR is ALMOST ALWAYS set on macOS (unless the user un-set it).
-  const char* env_tmpdir = getenv("TMPDIR");
+  // In order to facilitate hermetic runs on macOS, first check
+  // $MAC_CHROMIUM_TMPDIR. We check this instead of $TMPDIR because external
+  // programs currently set $TMPDIR with no effect, but when we respect it
+  // directly it can cause crashes (like crbug.com/698759).
+  const char* env_tmpdir = getenv("MAC_CHROMIUM_TMPDIR");
   if (env_tmpdir) {
+    DCHECK_LT(strlen(env_tmpdir), 50u)
+        << "too-long TMPDIR causes socket name length issues.";
     *path = base::FilePath(env_tmpdir);
     return true;
   }
