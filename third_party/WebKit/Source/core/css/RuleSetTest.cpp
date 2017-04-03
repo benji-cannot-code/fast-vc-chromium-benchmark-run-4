efc80c76cbec1a5685c765ae320b48ea49bf19d6
rune <rune@opera.com>
2017-04-03 02:05:47 -0700

    Added available bit to m_selectorIndex.
    Increased the max number of simple selectors for one style from 8192 to
    16384 by moving one bit to the second 32-bit part to make space for
    another bit. There is one bit left, but it cannot be utilized since the
    bit field cannot span two 32 bit values. This doesn't really fix the
    issue since it's not unlikely someone will use more than 16000 selectors
    if they use more than 8000 selectors.
    
    R=meade@chromium.org
    BUG=704562
    
    Review-Url: https://codereview.chromium.org/2785103004
    Cr-Commit-Position: refs/heads/master@{#461389}

--- third_party/WebKit/Source/core/css/RuleSetTest.cpp
+++ third_party/WebKit/Source/core/css/RuleSetTest.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/css/CSSTestHelper.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "wtf/text/StringBuilder.h"
 
 namespace blink {
 
@@ -194,6 +195,23 @@ TEST(RuleSetTest, findBestRuleSetAndAdd_HostContextAndClass) {
   RuleSet& ruleSet = helper.ruleSet();
   const HeapVector<RuleData>* rules = ruleSet.shadowHostRules();
   ASSERT_EQ(0u, rules->size());
+}
+
+TEST(RuleSetTest, SelectorIndexLimit) {
+  StringBuilder builder;
+
+  for (unsigned i = 0; i < 16383; i++)
+    builder.append("div,");
+
+  builder.append("b,span {}");
+
+  CSSTestHelper helper;
+  helper.addCSSRules(builder.toString().ascii().data());
+  const RuleSet& ruleSet = helper.ruleSet();
+  const HeapTerminatedArray<RuleData>* rules = ruleSet.tagRules("b");
+  ASSERT_EQ(1u, rules->size());
+  EXPECT_EQ("b", rules->at(0).selector().tagQName().localName());
+  EXPECT_FALSE(ruleSet.tagRules("span"));
 }
 
 }  // namespace blink
