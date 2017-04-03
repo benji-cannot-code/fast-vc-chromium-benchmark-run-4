72e9ec90e49635f84159f807f2461d2865a94c99
rdevlin.cronin <rdevlin.cronin@chromium.org>
2017-04-03 14:09:03 -0700

    [Extensions Bindings] Move safeCallbackApply to exception handler
    Move the safeCallbackApply function to the uncaught exception handler
    module, rather than the sendRequest module.
    Two main reasons:
    - It makes more sense, since the purpose of the function is to run a
      supplied function and notify the exception handler on failure,
    - Moving to native bindings, we should be able to avoid using anything
      from the sendRequest module.
    
    BUG=653596
    
    Review-Url: https://codereview.chromium.org/2794043002
    Cr-Commit-Position: refs/heads/master@{#461538}

--- chrome/renderer/resources/extensions/file_entry_binding_util.js
+++ chrome/renderer/resources/extensions/file_entry_binding_util.js
@@ -5,11 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 var fileSystemNatives = requireNative('file_system_natives');
 var GetIsolatedFileSystem = fileSystemNatives.GetIsolatedFileSystem;
-var sendRequest = require('sendRequest');
 var lastError = require('lastError');
 var GetModuleSystem = requireNative('v8_context').GetModuleSystem;
 // TODO(sammc): Don't require extension. See http://crbug.com/235689.
 var GetExtensionViews = requireNative('runtime').GetExtensionViews;
+var safeCallbackApply = require('uncaught_exception_handler').safeCallbackApply;
 
 // For a given |apiName|, generates object with two elements that are used
 // in file system relayed APIs:
@@ -73,11 +73,10 @@ function getFileBindingsForApi(apiName) {
                 // the callback will not be called with any entries.
                 if (entries.length == response.entries.length) {
                   if (response.multiple) {
-                    sendRequest.safeCallbackApply(
-                        apiName + '.' + functionName, request, callback,
-                        [entries]);
+                    safeCallbackApply(apiName + '.' + functionName, request,
+                                      callback, [entries]);
                   } else {
-                    sendRequest.safeCallbackApply(
+                    safeCallbackApply(
                         apiName + '.' + functionName, request, callback,
                         [entries[0]]);
                   }
--- chrome/renderer/resources/extensions/file_system_custom_bindings.js
+++ chrome/renderer/resources/extensions/file_system_custom_bindings.js
@@ -14,6 +14,7 @@ var fileBindings = getFileBindingsForApi('fileSystem');
 var bindFileEntryCallback = fileBindings.bindFileEntryCallback;
 var entryIdManager = fileBindings.entryIdManager;
 var fileSystemNatives = requireNative('file_system_natives');
+var safeCallbackApply = require('uncaught_exception_handler').safeCallbackApply;
 
 binding.registerCustomHook(function(bindingsAPI) {
   var apiFunctions = bindingsAPI.apiFunctions;
@@ -51,11 +52,7 @@ binding.registerCustomHook(function(bindingsAPI) {
       function(id, callback) {
     var savedEntry = entryIdManager.getEntryById(id);
     if (savedEntry) {
-      sendRequest.safeCallbackApply(
-          'fileSystem.isRestorable',
-          {},
-          callback,
-          [true]);
+      safeCallbackApply('fileSystem.isRestorable', {}, callback, [true]);
     } else {
       sendRequest.sendRequest(
           this.name, [id, callback], this.definition.parameters);
@@ -68,11 +65,7 @@ binding.registerCustomHook(function(bindingsAPI) {
     if (savedEntry) {
       // We already have a file entry for this id so pass it to the callback and
       // send a request to the browser to move it to the back of the LRU.
-      sendRequest.safeCallbackApply(
-          'fileSystem.restoreEntry',
-          {},
-          callback,
-          [savedEntry]);
+      safeCallbackApply('fileSystem.restoreEntry', {}, callback, [savedEntry]);
       return [id, false, null];
     } else {
       // Ask the browser process for a new file entry for this id, to be passed
@@ -88,11 +81,8 @@ binding.registerCustomHook(function(bindingsAPI) {
       fileSystem = fileSystemNatives.GetIsolatedFileSystem(
           response.file_system_id, response.file_system_path);
     }
-    sendRequest.safeCallbackApply(
-        'fileSystem.requestFileSystem',
-        request,
-        callback,
-        [fileSystem]);
+    safeCallbackApply('fileSystem.requestFileSystem', request, callback,
+                      [fileSystem]);
   });
 
   // TODO(benwells): Remove these deprecated versions of the functions.
