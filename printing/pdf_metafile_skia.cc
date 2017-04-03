700f95f1424c4c52209d003f6e3785278572737e
halcanary <halcanary@google.com>
2017-04-03 09:16:13 -0700

    PdfMetafileSkia: folow-up to .detachAsStream() change
    See: https://crrev.com/2747183002 and https://review.skia.org/9682
    Review-Url: https://codereview.chromium.org/2790493005
    Cr-Commit-Position: refs/heads/master@{#461445}

--- printing/pdf_metafile_skia.cc
+++ printing/pdf_metafile_skia.cc
@@ -208,7 +208,7 @@ bool PdfMetafileSkia::FinishDocument() {
   }
   doc->close();
 
-  data_->pdf_data_ = std::unique_ptr<SkStreamAsset>(stream.detachAsStream());
+  data_->pdf_data_ = stream.detachAsStream();
   return true;
 }
 
