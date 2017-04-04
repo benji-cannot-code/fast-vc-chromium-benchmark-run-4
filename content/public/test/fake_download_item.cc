1e1fa25ab9e2ff378aeb479250c540e6b637dc01
vitaliii <vitaliii@chromium.org>
2017-04-04 02:58:11 -0700

    [NTP::Downloads] Use last access time to filter old asset downloads.
    Previously, last_access_time was not provided in DownloadItem. Now it
    is, so in this CL we use it to filter out outdaded asset downloads (same
    way as it was done for offline pages - either creation time or last
    access time (if ever opened) is considered).
    
    BUG=689965
    
    Review-Url: https://codereview.chromium.org/2787303002
    Cr-Commit-Position: refs/heads/master@{#461671}

--- content/public/test/fake_download_item.cc
+++ content/public/test/fake_download_item.cc
@@ -143,6 +143,14 @@ int64_t FakeDownloadItem::GetReceivedBytes() const {
   return received_bytes_;
 }
 
+void FakeDownloadItem::SetLastAccessTime(base::Time time) {
+  last_access_time_ = time;
+}
+
+base::Time FakeDownloadItem::GetLastAccessTime() const {
+  return last_access_time_;
+}
+
 // The methods below are not supported and are not expected to be called.
 void FakeDownloadItem::ValidateDangerousDownload() {
   NOTREACHED();
@@ -371,11 +379,6 @@ bool FakeDownloadItem::GetOpened() const {
   return false;
 }
 
-base::Time FakeDownloadItem::GetLastAccessTime() const {
-  NOTREACHED();
-  return base::Time();
-}
-
 bool FakeDownloadItem::IsTransient() const {
   NOTREACHED();
   return false;
@@ -400,10 +403,6 @@ void FakeDownloadItem::SetOpenWhenComplete(bool open) {
 }
 
 void FakeDownloadItem::SetOpened(bool opened) {
-  NOTREACHED();
-}
-
-void FakeDownloadItem::SetLastAccessTime(base::Time time) {
   NOTREACHED();
 }
 
