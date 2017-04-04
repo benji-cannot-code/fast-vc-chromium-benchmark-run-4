0f8fdf4ca1ce94a9eb5ff9ee141be3aa1aa04ad4
stkhapugin <stkhapugin@chromium.org>
2017-04-04 07:42:00 -0700

    [ObjC ARC] Converts ios/chrome/browser/sessions:test_support to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2794323002
    Cr-Commit-Position: refs/heads/master@{#461708}

--- ios/chrome/browser/sessions/test_session_service.mm
+++ ios/chrome/browser/sessions/test_session_service.mm
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/sessions/test_session_service.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 @implementation TestSessionService
 - (void)saveWindow:(SessionWindowIOS*)window
     forBrowserState:(ios::ChromeBrowserState*)browserState
