3790303bc6e207993c2e47b2956ba8d9c0ac92f5
kouhei <kouhei@chromium.org>
2017-04-04 03:14:11 -0700

    [ES6 modules] ScriptModule::instantiate with resolveModule cb and error handling
    This CL replaces ScriptModule::instantiate implementation to support
    HostResolveModule callback routed to Modulator and proper error handling
    
    BUG=594639
    
    Review-Url: https://codereview.chromium.org/2785983002
    Cr-Commit-Position: refs/heads/master@{#461675}

--- third_party/WebKit/Source/bindings/core/v8/ScriptModule.cpp
+++ third_party/WebKit/Source/bindings/core/v8/ScriptModule.cpp
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/ScriptModule.h"
 
 #include "bindings/core/v8/V8Binding.h"
+#include "core/dom/Modulator.h"
+#include "core/dom/ScriptModuleResolver.h"
 
 namespace blink {
 
@@ -28,18 +30,20 @@ ScriptModule ScriptModule::compile(v8::Isolate* isolate,
   return ScriptModule(isolate, module);
 }
 
-v8::MaybeLocal<v8::Module> dummyCallback(v8::Local<v8::Context> context,
-                                         v8::Local<v8::String> specifier,
-                                         v8::Local<v8::Module> referrer) {
-  return v8::MaybeLocal<v8::Module>();
-}
+ScriptValue ScriptModule::instantiate(ScriptState* scriptState) {
+  v8::Isolate* isolate = scriptState->isolate();
+  v8::TryCatch tryCatch(isolate);
 
-bool ScriptModule::instantiate(ScriptState* scriptState) {
   DCHECK(!isNull());
   v8::Local<v8::Context> context = scriptState->context();
-  // TODO(adamk): pass in a real callback.
-  return m_module->newLocal(scriptState->isolate())
-      ->Instantiate(context, &dummyCallback);
+  bool success = m_module->newLocal(scriptState->isolate())
+                     ->Instantiate(context, &resolveModuleCallback);
+  if (!success) {
+    DCHECK(tryCatch.HasCaught());
+    return ScriptValue(scriptState, tryCatch.Exception());
+  }
+  DCHECK(!tryCatch.HasCaught());
+  return ScriptValue();
 }
 
 void ScriptModule::evaluate(ScriptState* scriptState) {
@@ -52,6 +56,28 @@ void ScriptModule::evaluate(ScriptState* scriptState) {
               result, tryCatch)) {
     // TODO(adamk): report error
   }
+}
+
+v8::MaybeLocal<v8::Module> ScriptModule::resolveModuleCallback(
+    v8::Local<v8::Context> context,
+    v8::Local<v8::String> specifier,
+    v8::Local<v8::Module> referrer) {
+  v8::Isolate* isolate = context->GetIsolate();
+  Modulator* modulator = V8PerContextData::from(context)->modulator();
+  DCHECK(modulator);
+
+  ScriptModule referrerRecord(isolate, referrer);
+  ExceptionState exceptionState(isolate, ExceptionState::ExecutionContext,
+                                "ScriptModule", "resolveModuleCallback");
+  ScriptModule resolved = modulator->scriptModuleResolver()->resolve(
+      toCoreStringWithNullCheck(specifier), referrerRecord, exceptionState);
+  if (resolved.isNull()) {
+    DCHECK(exceptionState.hadException());
+    return v8::MaybeLocal<v8::Module>();
+  }
+
+  DCHECK(!exceptionState.hadException());
+  return v8::MaybeLocal<v8::Module>(resolved.m_module->newLocal(isolate));
 }
 
 }  // namespace blink
