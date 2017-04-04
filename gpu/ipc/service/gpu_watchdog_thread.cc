5f65910ee4063391035540cc15f3d119871749a2
sadrul <sadrul@chromium.org>
2017-04-04 01:11:15 -0700

    gpu: Add a couple of unit-tests for GpuService.
    Add a couple of unit-tests to verify that GpuService can be destroyed
    safely both after incoming GpuServiceRequest have been bound, or while
    they are scheduled to be bound.
    
    BUG=706375, 706357
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2787363003
    Cr-Commit-Position: refs/heads/master@{#461658}

--- gpu/ipc/service/gpu_watchdog_thread.cc
+++ gpu/ipc/service/gpu_watchdog_thread.cc
@@ -27,6 +27,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 #endif
 
+#if defined(USE_X11)
+#include <X11/Xatom.h>
+#include <X11/Xlib.h>
+#endif
+
 namespace gpu {
 namespace {
 
