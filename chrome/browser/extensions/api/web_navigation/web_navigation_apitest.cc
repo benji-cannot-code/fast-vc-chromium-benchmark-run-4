c8f70b999c3feb18708ab624a9777ade9aa5147d
maxmorin <maxmorin@chromium.org>
2017-04-04 06:08:22 -0700

    Disable flaky WebNavigationApiTest.CrossProcessHistory.
    TBR=nasko@chromium.org
    BUG=708139
    
    Review-Url: https://codereview.chromium.org/2790383002
    Cr-Commit-Position: refs/heads/master@{#461690}

--- chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
+++ chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
@@ -750,7 +750,8 @@ IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, CrossProcessFragment) {
       << message_;
 }
 
-IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, CrossProcessHistory) {
+// crbug.com/708139.
+IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, DISABLED_CrossProcessHistory) {
   ASSERT_TRUE(StartEmbeddedTestServer());
 
   // See crossProcessHistory/e.html.
