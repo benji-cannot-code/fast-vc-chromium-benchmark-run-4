699bcd5ce5c4e3084a6c9f462a238802d2b77534
mark <mark@chromium.org>
2017-04-04 10:35:25 -0700

    Revert of Fix chrome://proximity-auth debug WebUI. (patchset #4 id:60001 of https://codereview.chromium.org/2793833002/ )
    Reason for revert:
    Broke https://build.chromium.org/p/chromium.linux/builders/Android%20Builder/builds/81378
    
    Original issue's description:
    > Fix chrome://proximity-auth debug WebUI.
    >
    > The old debug UI had a dependency on Polymer 0.5, which is deprecated in Chrome. This new version removes the Polymer dependency all-together and instead just uses standard JavaScript.
    >
    > Not all features are reimplemented, but this is a good base to re-add those features.
    >
    > Review-Url: https://codereview.chromium.org/2793833002
    > Cr-Commit-Position: refs/heads/master@{#461761}
    > Committed: https://chromium.googlesource.com/chromium/src/+/014f92612d3f78e51af259071c0088b019470030
    
    TBR=sacomoto@chromium.org,tengs@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2798653002
    Cr-Commit-Position: refs/heads/master@{#461768}

--- components/proximity_auth/webui/resources/pollux/logs_controller.js
+++ components/proximity_auth/webui/resources/pollux/logs_controller.js
@@ -58,8 +58,9 @@ LogBufferInterface = {
 };
 
 /**
- * Controller for the logs list element, updating it based on user input and
- * logs received from native code.
+ * Controller for the logs list UI, updating it based on user input and logs
+ * received from native code.
+ *
  */
 class LogsListController {
   constructor() {
@@ -99,6 +100,7 @@ class LogsListController {
     var source = directories[directories.length - 1] + ':' + log.line;
 
     var t = this.itemTemplate_.content;
+    console.log(t.querySelector('.log-item').attributes);
     t.querySelector('.log-item').attributes.severity.value = log.severity;
     t.querySelector('.item-time').textContent = log.time;
     t.querySelector('.item-source').textContent = source;
