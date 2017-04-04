079dbea394418a5ea41b532c97355601edeae1d5
lazyboy <lazyboy@chromium.org>
2017-04-04 15:51:03 -0700

    Make AppWindowCreateFunction UIThreadExtensionFunction.
    AsyncExtensionFunction is deprecated.
    
    Also rename app_window tests to *AppWindowApiTest instead of
    *PlatformAppBrowserTest.
    
    BUG=648275
    Test=Expect no visible changes.
    
    Review-Url: https://codereview.chromium.org/2789413003
    Cr-Commit-Position: refs/heads/master@{#461876}

--- extensions/browser/api/app_window/app_window_api.h
+++ extensions/browser/api/app_window/app_window_api.h
@@ -17,14 +17,14 @@ struct CreateWindowOptions;
 }
 }
 
-class AppWindowCreateFunction : public AsyncExtensionFunction {
+class AppWindowCreateFunction : public UIThreadExtensionFunction {
  public:
   AppWindowCreateFunction();
   DECLARE_EXTENSION_FUNCTION("app.window.create", APP_WINDOW_CREATE)
 
  protected:
   ~AppWindowCreateFunction() override {}
-  bool RunAsync() override;
+  ResponseAction Run() override;
 
  private:
   bool GetBoundsSpec(
@@ -35,7 +35,8 @@ class AppWindowCreateFunction : public AsyncExtensionFunction {
   AppWindow::Frame GetFrameFromString(const std::string& frame_string);
   bool GetFrameOptions(
       const extensions::api::app_window::CreateWindowOptions& options,
-      AppWindow::CreateParams* create_params);
+      AppWindow::CreateParams* create_params,
+      std::string* error);
   void UpdateFrameOptionsForChannel(AppWindow::CreateParams* create_params);
 };
 
