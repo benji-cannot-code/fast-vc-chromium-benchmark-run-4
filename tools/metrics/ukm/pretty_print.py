34c7a0b42b2dc36d978f80640ad63a4fe50e053d
holte <holte@chromium.org>
2017-04-04 11:41:28 -0700

    Create a UKM descriptions XML file
    BUG=678682
    
    Review-Url: https://codereview.chromium.org/2769223005
    Cr-Commit-Position: refs/heads/master@{#461784}

---
+++ tools/metrics/ukm/pretty_print.py
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import sys
+
+import model
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
+import presubmit_util
+
+
+
+def main(argv):
+  presubmit_util.DoPresubmitMain(argv, 'ukm.xml', 'ukm.old.xml',
+                                 'pretty_print.py', model.UpdateXML)
+
+
+if '__main__' == __name__:
+  sys.exit(main(sys.argv))
