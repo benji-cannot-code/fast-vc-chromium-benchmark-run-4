a33563a391c62d7c2e879a8bcdae9c741d7d38e6
dcheng <dcheng@chromium.org>
2017-04-03 18:27:06 -0700

    Remove unnecessary null checks for LocalFileSystemClient.
    BUG=none
    R=kinuko@chromium.org
    
    Review-Url: https://codereview.chromium.org/2789103003
    Cr-Commit-Position: refs/heads/master@{#461601}

--- third_party/WebKit/Source/modules/filesystem/LocalFileSystem.h
+++ third_party/WebKit/Source/modules/filesystem/LocalFileSystem.h
@@ -69,7 +69,7 @@ class LocalFileSystem final : public GarbageCollectedFinalized<LocalFileSystem>,
                          long long size,
                          std::unique_ptr<AsyncFileSystemCallbacks>);
 
-  FileSystemClient* client() const { return m_client.get(); }
+  FileSystemClient& client() const { return *m_client; }
 
   static const char* supplementName();
   static LocalFileSystem* from(ExecutionContext&);
@@ -89,7 +89,7 @@ class LocalFileSystem final : public GarbageCollectedFinalized<LocalFileSystem>,
                                  CallbackWrapper*);
   void resolveURLInternal(ExecutionContext*, const KURL&, CallbackWrapper*);
 
-  std::unique_ptr<FileSystemClient> m_client;
+  const std::unique_ptr<FileSystemClient> m_client;
 };
 
 }  // namespace blink
