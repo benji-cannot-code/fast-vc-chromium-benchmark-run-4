21d7b7a33505fb785d952add2b060619bf2d53be
estade <estade@chromium.org>
2017-04-03 19:17:30 -0700

    Minor pre-MD cleanups in chromeos network UI code (just renames).
    BUG=686251
    
    Review-Url: https://codereview.chromium.org/2796693002
    Cr-Commit-Position: refs/heads/master@{#461614}

--- ash/common/system/chromeos/network/network_state_list_detailed_view.cc
+++ ash/common/system/chromeos/network/network_state_list_detailed_view.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/common/system/chromeos/network/network_icon.h"
 #include "ash/common/system/chromeos/network/network_icon_animation.h"
 #include "ash/common/system/chromeos/network/network_info.h"
-#include "ash/common/system/chromeos/network/network_list_md.h"
+#include "ash/common/system/chromeos/network/network_list.h"
 #include "ash/common/system/chromeos/network/network_list_view_base.h"
 #include "ash/common/system/chromeos/network/tray_network_state_observer.h"
 #include "ash/common/system/chromeos/network/vpn_list_view.h"
@@ -80,9 +80,9 @@ namespace {
 const int kRequestScanDelaySeconds = 10;
 
 // TODO(varkha): Consolidate with a similar method in tray_bluetooth.cc.
-void SetupConnectedItemMd(HoverHighlightView* container,
-                          const base::string16& text,
-                          const gfx::ImageSkia& image) {
+void SetupConnectedItem(HoverHighlightView* container,
+                        const base::string16& text,
+                        const gfx::ImageSkia& image) {
   container->AddIconAndLabels(
       image, text,
       l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_NETWORK_STATUS_CONNECTED));
@@ -92,9 +92,9 @@ void SetupConnectedItemMd(HoverHighlightView* container,
 }
 
 // TODO(varkha): Consolidate with a similar method in tray_bluetooth.cc.
-void SetupConnectingItemMd(HoverHighlightView* container,
-                           const base::string16& text,
-                           const gfx::ImageSkia& image) {
+void SetupConnectingItem(HoverHighlightView* container,
+                         const base::string16& text,
+                         const gfx::ImageSkia& image) {
   container->AddIconAndLabels(
       image, text,
       l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_NETWORK_STATUS_CONNECTING));
@@ -496,9 +496,9 @@ views::View* NetworkStateListDetailedView::CreateViewForNetwork(
     const NetworkInfo& info) {
   HoverHighlightView* container = new HoverHighlightView(this);
   if (info.connected)
-    SetupConnectedItemMd(container, info.label, info.image);
+    SetupConnectedItem(container, info.label, info.image);
   else if (info.connecting)
-    SetupConnectingItemMd(container, info.label, info.image);
+    SetupConnectingItem(container, info.label, info.image);
   else
     container->AddIconAndLabel(info.image, info.label, info.highlight);
   container->set_tooltip(info.tooltip);
@@ -519,9 +519,9 @@ void NetworkStateListDetailedView::UpdateViewForNetwork(
   HoverHighlightView* container = static_cast<HoverHighlightView*>(view);
   DCHECK(!container->has_children());
   if (info.connected)
-    SetupConnectedItemMd(container, info.label, info.image);
+    SetupConnectedItem(container, info.label, info.image);
   else if (info.connecting)
-    SetupConnectingItemMd(container, info.label, info.image);
+    SetupConnectingItem(container, info.label, info.image);
   else
     container->AddIconAndLabel(info.image, info.label, info.highlight);
   views::View* controlled_icon = CreateControlledByExtensionView(info);
