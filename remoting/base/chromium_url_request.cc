c31d1d0a9d775ada64b08240ff385e2321885f0e
rhalavati <rhalavati@chromium.org>
2017-04-04 12:17:41 -0700

    Cookies disabled for ChromiumUrlRequest.
    Following discussion in https://codereview.chromium.org/2729423003,
    cookies are disabled for remoting/base/chromium_url_request.cc
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2792283002
    Cr-Commit-Position: refs/heads/master@{#461797}

--- remoting/base/chromium_url_request.cc
+++ remoting/base/chromium_url_request.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_helpers.h"
 #include "base/memory/ptr_util.h"
+#include "net/base/load_flags.h"
 #include "net/url_request/url_fetcher.h"
 #include "net/url_request/url_request_context_getter.h"
 
@@ -28,6 +29,8 @@ ChromiumUrlRequest::ChromiumUrlRequest(
   url_fetcher_ = net::URLFetcher::Create(GURL(url), request_type, this);
   url_fetcher_->SetRequestContext(url_context.get());
   url_fetcher_->SetReferrer("https://chrome.google.com/remotedesktop");
+  url_fetcher_->SetLoadFlags(net::LOAD_DO_NOT_SAVE_COOKIES |
+                             net::LOAD_DO_NOT_SEND_COOKIES);
 }
 
 ChromiumUrlRequest::~ChromiumUrlRequest() {}
