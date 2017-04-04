4e0e7f31f5a1e9eb5042fbee48067781e6c98d5e
ccameron <ccameron@chromium.org>
2017-04-04 11:16:15 -0700

    color: Clean up dependencies to allow including in cc/paint
    Move color_space.cc and some related functionality into gfx:color_space,
    so that they can be included in cc/paint without creating a dependency
    cycle.
    
    Also shrink some of the struct sizes and add a hash function, as these
    will be helpful in future patches.
    
    BUG=701942
    
    Review-Url: https://codereview.chromium.org/2795093002
    Cr-Commit-Position: refs/heads/master@{#461770}

--- ui/gfx/color_space.h
+++ ui/gfx/color_space.h
@@ -29,7 +29,7 @@ class ICCProfile;
 // between any processes.
 class GFX_EXPORT ColorSpace {
  public:
-  enum class PrimaryID : uint16_t {
+  enum class PrimaryID : uint8_t {
     INVALID,
     BT709,
     BT470M,
@@ -52,7 +52,7 @@ class GFX_EXPORT ColorSpace {
     LAST = ICC_BASED,
   };
 
-  enum class TransferID : uint16_t {
+  enum class TransferID : uint8_t {
     INVALID,
     BT709,
     GAMMA22,
@@ -87,7 +87,7 @@ class GFX_EXPORT ColorSpace {
     LAST = ICC_BASED,
   };
 
-  enum class MatrixID : int16_t {
+  enum class MatrixID : uint8_t {
     INVALID,
     RGB,
     BT709,
@@ -102,7 +102,7 @@ class GFX_EXPORT ColorSpace {
     LAST = YDZDX,
   };
 
-  enum class RangeID : int8_t {
+  enum class RangeID : uint8_t {
     INVALID,
     // Limited Rec. 709 color range with RGB values ranging from 16 to 235.
     LIMITED,
@@ -120,6 +120,8 @@ class GFX_EXPORT ColorSpace {
              MatrixID matrix,
              RangeID full_range);
   ColorSpace(const ColorSpace& other);
+  ColorSpace(ColorSpace&& other);
+  ColorSpace& operator=(const ColorSpace& other);
   ~ColorSpace();
 
   // Create a color space with primary, transfer and matrix values from the
@@ -154,6 +156,7 @@ class GFX_EXPORT ColorSpace {
   bool operator==(const ColorSpace& other) const;
   bool operator!=(const ColorSpace& other) const;
   bool operator<(const ColorSpace& other) const;
+  size_t GetHash() const;
   std::string ToString() const;
 
   // Returns true if the decoded values can be outside of the 0.0-1.0 range.
--- ui/gfx/icc_profile.cc
+++ ui/gfx/icc_profile.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "third_party/skia/include/core/SkColorSpaceXform.h"
 #include "third_party/skia/include/core/SkICC.h"
-#include "ui/gfx/color_transform.h"
 #include "ui/gfx/skia_color_space_util.h"
 
 namespace gfx {
