e333424ade97f8055841cbe848de3f222c278fa0
edchin <edchin@chromium.org>
2017-04-04 16:23:17 -0700

    [ios] Tab strip toolbar UI.
    This CL implements the tab strip toolbar UI.
    
    Screenshots:
    https://drive.google.com/open?id=0BwS6YyQeisH5cHlUSUNqR0NBaDQ
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2793783002
    Cr-Commit-Position: refs/heads/master@{#461890}

--- ios/clean/chrome/browser/ui/tab_collection/tab_collection_view_controller.h
+++ ios/clean/chrome/browser/ui/tab_collection/tab_collection_view_controller.h
@@ -14,7 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Controller for a scrolling view displaying square cells that represent
 // the user's open tabs.
-@interface TabCollectionViewController : UIViewController<TabCollectionConsumer>
+@interface TabCollectionViewController
+    : UIViewController<TabCollectionConsumer, UICollectionViewDataSource>
 // A collection view of tabs.
 @property(nonatomic, weak, readonly) UICollectionView* tabs;
 // Data source for the tabs to be displayed.
--- ios/clean/chrome/browser/ui/tab_strip/tab_strip_view_controller.mm
+++ ios/clean/chrome/browser/ui/tab_strip/tab_strip_view_controller.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/clean/chrome/browser/ui/tab_strip/tab_strip_view_controller.h"
 
 #import "ios/clean/chrome/browser/ui/commands/tab_strip_commands.h"
+#import "ios/clean/chrome/browser/ui/tab_strip/tab_strip_toolbar.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -14,21 +15,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 CGFloat kItemLength = 100.0f;
 CGFloat kSpacing = 10.0f;
+CGFloat kToolbarWidth = 50.0f;
 }
 
 @implementation TabStripViewController
 @synthesize dispatcher = _dispatcher;
 
+#pragma mark - Required subclass override
+
 - (UICollectionViewLayout*)collectionViewLayout {
   UICollectionViewFlowLayout* layout =
       [[UICollectionViewFlowLayout alloc] init];
+  layout.headerReferenceSize = CGSizeMake(kToolbarWidth, 0);
   layout.itemSize = CGSizeMake(kItemLength, kItemLength);
   layout.minimumInteritemSpacing = kSpacing;
-  layout.sectionInset =
-      UIEdgeInsetsMake(kSpacing, kSpacing, kSpacing, kSpacing);
+  layout.sectionInset = UIEdgeInsetsMake(kSpacing, 0, kSpacing, kSpacing);
   layout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
   return layout;
 }
+
+#pragma mark - TabStripCommands
 
 - (void)showTabAtIndex:(int)index {
   [self.dispatcher showTabStripTabAtIndex:index];
@@ -36,6 +42,26 @@ CGFloat kSpacing = 10.0f;
 
 - (void)closeTabAtIndex:(int)index {
   [self.dispatcher closeTabStripTabAtIndex:index];
+}
+
+#pragma mark - View lifecyle
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  [self.tabs registerClass:[TabStripToolbar class]
+      forSupplementaryViewOfKind:UICollectionElementKindSectionHeader
+             withReuseIdentifier:[TabStripToolbar reuseIdentifier]];
+}
+
+#pragma mark - UICollectionViewDataSource
+
+- (UICollectionReusableView*)collectionView:(UICollectionView*)collectionView
+          viewForSupplementaryElementOfKind:(NSString*)kind
+                                atIndexPath:(NSIndexPath*)indexPath {
+  return [collectionView
+      dequeueReusableSupplementaryViewOfKind:kind
+                         withReuseIdentifier:[TabStripToolbar reuseIdentifier]
+                                forIndexPath:indexPath];
 }
 
 @end
