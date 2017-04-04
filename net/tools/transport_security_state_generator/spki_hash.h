3ce6a9d9052f41bb71515ffd2fe25359d3191157
martijn <martijn@martijnc.be>
2017-04-04 16:44:00 -0700

    Add unittests for the certificate and file parsing logic in the transport security state generator.
    BUG=595493
    
    Review-Url: https://codereview.chromium.org/2793823002
    Cr-Commit-Position: refs/heads/master@{#461900}

--- net/tools/transport_security_state_generator/spki_hash.h
+++ net/tools/transport_security_state_generator/spki_hash.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/strings/string_piece.h"
+
 namespace net {
 
 namespace transport_security_state {
@@ -25,7 +27,7 @@ class SPKIHash {
   // SPKI hashes are SHA256. Other algorithms are not supported. Returns true
   // on success and copies the decoded bytes to |data_|. Returns false on
   // failure.
-  bool FromString(const std::string& hash_string);
+  bool FromString(base::StringPiece hash_string);
 
   // Calculates the SHA256 digest over |*input| and copies the result to
   // |data_|.
