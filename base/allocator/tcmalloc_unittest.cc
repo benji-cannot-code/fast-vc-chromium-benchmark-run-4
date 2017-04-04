716b44d6f88e17848d3b54017b4fa2805f64bb09
bcf <bcf@chromium.org>
2017-04-04 12:59:52 -0700

    tcmalloc_unittest: Don't run Realloc test on low memory devices
    BUG=b/36238217
    TEST=Test not run on low memory device.
    
    Review-Url: https://codereview.chromium.org/2790173004
    Cr-Commit-Position: refs/heads/master@{#461805}

--- base/allocator/tcmalloc_unittest.cc
+++ base/allocator/tcmalloc_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/logging.h"
 #include "base/process/process_metrics.h"
+#include "base/sys_info.h"
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -87,6 +88,10 @@ static void TestCalloc(size_t n, size_t s, bool ok) {
   }
 }
 
+bool IsLowMemoryDevice() {
+  return base::SysInfo::AmountOfPhysicalMemory() <= 256LL * 1024 * 1024;
+}
+
 }  // namespace
 
 TEST(TCMallocTest, Malloc) {
@@ -154,6 +159,12 @@ TEST(TCMallocTest, Realloc) {
         free(dst);
     }
   }
+
+  // The logic below tries to allocate kNumEntries * 9000 ~= 130 MB of memory.
+  // This would cause the test to crash on low memory devices with no VM
+  // overcommit (e.g., chromecast).
+  if (IsLowMemoryDevice())
+    return;
 
   // Now make sure realloc works correctly even when we overflow the
   // packed cache, so some entries are evicted from the cache.
