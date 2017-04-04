efb0f1b70c02a89226ea888700d37463c74cc2af
ikilpatrick <ikilpatrick@chromium.org>
2017-04-03 19:51:13 -0700

    [LayoutNG] Refactor NGBlockLayoutAlgorithm::PrepareChildLayout()
    Additionally this only flushes the pending floats if the child is actually going to clear something.
    
    BUG=635619
    
    Review-Url: https://codereview.chromium.org/2761923002
    Cr-Commit-Position: refs/heads/master@{#461617}

--- third_party/WebKit/Source/core/layout/ng/ng_space_utils.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_space_utils.cc
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/layout/ng/ng_space_utils.h"
 
+#include "core/layout/ng/ng_block_node.h"
+#include "core/layout/ng/ng_layout_input_node.h"
+#include "core/layout/ng/ng_writing_mode.h"
+
 namespace blink {
 namespace {
 
@@ -26,8 +30,8 @@ bool IsOutOfFlowPositioned(const EPosition& position) {
 
 }  // namespace
 
-bool IsNewFormattingContextForBlockLevelChild(const NGConstraintSpace& space,
-                                              const ComputedStyle& style) {
+bool IsNewFormattingContextForBlockLevelChild(const ComputedStyle& parent_style,
+                                              const NGLayoutInputNode& node) {
   // TODO(layout-dev): This doesn't capture a few cases which can't be computed
   // directly from style yet:
   //  - The child is a <fieldset>.
@@ -35,6 +39,10 @@ bool IsNewFormattingContextForBlockLevelChild(const NGConstraintSpace& space,
   //    in a multi-col formatting context).
   //    (https://drafts.csswg.org/css-multicol-1/#valdef-column-span-all)
 
+  if (node.IsInline())
+    return false;
+
+  const ComputedStyle& style = node.Style();
   if (style.isFloating() || IsOutOfFlowPositioned(style.position()))
     return true;
 
@@ -50,7 +58,7 @@ bool IsNewFormattingContextForBlockLevelChild(const NGConstraintSpace& space,
       display == EDisplay::kWebkitBox)
     return true;
 
-  if (space.WritingMode() != FromPlatformWritingMode(style.getWritingMode()))
+  if (parent_style.getWritingMode() != style.getWritingMode())
     return true;
 
   return false;
@@ -86,14 +94,13 @@ WTF::Optional<LayoutUnit> GetClearanceOffset(
   return WTF::nullopt;
 }
 
-bool ShouldShrinkToFit(const NGConstraintSpace& parent_space,
+bool ShouldShrinkToFit(const ComputedStyle& parent_style,
                        const ComputedStyle& style) {
-  NGWritingMode child_writing_mode =
-      FromPlatformWritingMode(style.getWritingMode());
   // Whether the child and the containing block are parallel to each other.
   // Example: vertical-rl and vertical-lr
-  bool is_in_parallel_flow =
-      IsParallelWritingMode(parent_space.WritingMode(), child_writing_mode);
+  bool is_in_parallel_flow = IsParallelWritingMode(
+      FromPlatformWritingMode(parent_style.getWritingMode()),
+      FromPlatformWritingMode(style.getWritingMode()));
 
   return style.display() == EDisplay::kInlineBlock || style.isFloating() ||
          !is_in_parallel_flow;
