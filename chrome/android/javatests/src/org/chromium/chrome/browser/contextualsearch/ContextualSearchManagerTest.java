40b75f414a50276b4e9f5930c4cd8a3e9212965e
maxmorin <maxmorin@chromium.org>
2017-04-04 02:11:29 -0700

    Disable flaky ContextualSearchManagerTest#testHidesWhenOmniboxFocused.
    TBR=donnd@chromium.org
    BUG=707529
    
    Review-Url: https://codereview.chromium.org/2794253002
    Cr-Commit-Position: refs/heads/master@{#461666}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
@@ -1094,8 +1094,9 @@ public class ContextualSearchManagerTest extends ChromeActivityTestCaseBase<Chro
     /**
      * Tests whether the contextual search panel hides when omnibox is clicked.
      */
-    @SmallTest
-    @Feature({"ContextualSearch"})
+    //@SmallTest
+    //@Feature({"ContextualSearch"})
+    @DisabledTest
     public void testHidesWhenOmniboxFocused() throws InterruptedException, TimeoutException {
         clickWordNode("intelligence");
 
