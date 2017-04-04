d486dc6148e4f11bdbf7aab408f3b0404824ac5c
lazyboy <lazyboy@chromium.org>
2017-04-04 16:51:32 -0700

    [Ext Registry] Do not try to re-watch registry if we are already watching it.
    Firstly, registry observer/watcher is already set up, then we do not need
    to observe that again. Because we didn't use to call
    ExternalLoader::StartLoading more than once before
    https://crrev.com/2336403002, this wan't an issue when we added registry
    watching code. After we started calling StartLoading > 1nce, we unintentionally
    started retrying to observe windows registry if previous observing
    failed for some reason. This CL also removes this hard-to-explain retry. The
    reasoning is that if we want to retry observing registry, we should be doing
    that explicitly (and periodically?) and not rely on whether additional StartLoading
    was called or not.
    
    Secondly, RegKey::Watcher fails a DCHECK if we want to re-observe using
    same RegKey. This is DCHECK(callback_.is_null()) in
    RegKey::Watcher::StartWatching.
    
    BUG=653045
    Test=On windows, make chrome have a policy forced extension installed.
    Shut down chrome. Manually corrupt the policy installed extension.
    Re-laucnch chrome in debug mode: observe no more DCHECK failures.
    
    Review-Url: https://codereview.chromium.org/2796783002
    Cr-Commit-Position: refs/heads/master@{#461903}

--- chrome/browser/extensions/external_registry_loader_win.cc
+++ chrome/browser/extensions/external_registry_loader_win.cc
@@ -55,6 +55,9 @@ std::string MakePrefName(const std::string& extension_id,
 
 namespace extensions {
 
+ExternalRegistryLoader::ExternalRegistryLoader()
+    : attempted_watching_registry_(false) {}
+
 void ExternalRegistryLoader::StartLoading() {
   CHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   BrowserThread::PostTask(
@@ -201,7 +204,10 @@ void ExternalRegistryLoader::CompleteLoadAndStartWatchingRegistry() {
   CHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   LoadFinished();
 
-  // Start watching registry.
+  // Attempt to watch registry if we haven't already.
+  if (attempted_watching_registry_)
+    return;
+
   LONG result = ERROR_SUCCESS;
   if ((result = hklm_key_.Create(HKEY_LOCAL_MACHINE, kRegistryExtensions,
                                  KEY_NOTIFY | KEY_WOW64_32KEY)) ==
@@ -223,6 +229,8 @@ void ExternalRegistryLoader::CompleteLoadAndStartWatchingRegistry() {
   } else {
     LOG(WARNING) << "Error observing HKCU: " << result;
   }
+
+  attempted_watching_registry_ = true;
 }
 
 void ExternalRegistryLoader::OnRegistryKeyChanged(base::win::RegKey* key) {
