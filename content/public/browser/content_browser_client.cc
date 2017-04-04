ee0fd73c5d3813cbc4c79672588e1d9de086b34b
fdoray <fdoray@chromium.org>
2017-04-04 10:22:20 -0700

    Use TaskScheduler::InitParams to initialize TaskScheduler in browser process.
    Initialization with an arbitrary vector of SchedulerWorkerPoolParams
    is being deprecated.
    
    BUG=690706
    
    Review-Url: https://codereview.chromium.org/2799433002
    Cr-Commit-Position: refs/heads/master@{#461763}

--- content/public/browser/content_browser_client.cc
+++ content/public/browser/content_browser_client.cc
@@ -449,6 +449,11 @@ ContentBrowserClient::GetMemoryCoordinatorDelegate() {
   return nullptr;
 }
 
+std::unique_ptr<base::TaskScheduler::InitParams>
+ContentBrowserClient::GetTaskSchedulerInitParams() {
+  return nullptr;
+}
+
 bool ContentBrowserClient::ShouldRedirectDOMStorageTaskRunner() {
   return false;
 }
