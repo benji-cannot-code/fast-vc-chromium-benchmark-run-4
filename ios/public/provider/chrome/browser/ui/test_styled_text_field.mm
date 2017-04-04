66a4b655cac7a17aa8f38288a5b9f8753c1d8560
stkhapugin <stkhapugin@chromium.org>
2017-04-04 07:48:31 -0700

    [ObjC ARC] Converts ios/public/provider/chrome/browser/ui:test_support to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2794173003
    Cr-Commit-Position: refs/heads/master@{#461712}

--- ios/public/provider/chrome/browser/ui/test_styled_text_field.mm
+++ ios/public/provider/chrome/browser/ui/test_styled_text_field.mm
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/public/provider/chrome/browser/ui/test_styled_text_field.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 @implementation TestStyledTextField
 @synthesize placeholderStyle = _placeholderStyle;
 @synthesize textValidator = _textValidator;
