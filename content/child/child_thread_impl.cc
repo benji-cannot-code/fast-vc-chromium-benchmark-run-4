a4daf663af07aee39db9f386d691f1a4e2b3e481
erikchen <erikchen@chromium.org>
2017-04-04 17:56:16 -0700

    mac: Periodically shim new malloc zones immediately after startup.
    On macOS, libraries lazily make malloc zones. There are no notifications when
    this happens. This CL adds polling with an exponential backoff to check for the
    existence of new malloc zones, stopping after 1 minute.
    
    This CL only has an affect when heap profiling is enabled.
    
    BUG=693237
    
    Review-Url: https://codereview.chromium.org/2713553002
    Cr-Original-Commit-Position: refs/heads/master@{#461836}
    Committed: https://chromium.googlesource.com/chromium/src/+/245f17559f973bb5a207c063d982c0bab0ec1c0b
    Review-Url: https://codereview.chromium.org/2713553002
    Cr-Commit-Position: refs/heads/master@{#461924}

--- content/child/child_thread_impl.cc
+++ content/child/child_thread_impl.cc
@@ -80,6 +80,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_descriptors.h"
 #endif
 
+#if defined(OS_MACOSX)
+#include "base/allocator/allocator_interception_mac.h"
+#endif
+
 using tracked_objects::ThreadData;
 
 namespace content {
@@ -548,6 +552,14 @@ void ChildThreadImpl::Init(const Options& options) {
     if (base::StringToInt(connection_override, &temp))
       connection_timeout = temp;
   }
+
+#if defined(OS_MACOSX)
+  if (base::CommandLine::InitializedForCurrentProcess() &&
+      base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kEnableHeapProfiling)) {
+    base::allocator::PeriodicallyShimNewMallocZones();
+  }
+#endif
 
   message_loop_->task_runner()->PostDelayedTask(
       FROM_HERE, base::Bind(&ChildThreadImpl::EnsureConnected,
