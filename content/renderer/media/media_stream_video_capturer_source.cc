d90d334ebb4293811e043cb5fae0e176b6548f95
guidou <guidou@chromium.org>
2017-04-05 14:29:24 -0700

    Add GetPreferredFormats method to media::VideoCapturerSource.
    GetCurrentSupportedFormats is deprecated and reimplemented on
    top of GetPreferredFormats by default.
    
    This is in anticipation to spec-compliant video constraints processing,
    where device capabilities are queried before the device is selected.
    
    BUG=657733
    
    Review-Url: https://codereview.chromium.org/2787773002
    Cr-Commit-Position: refs/heads/master@{#462223}

--- content/renderer/media/media_stream_video_capturer_source.cc
+++ content/renderer/media/media_stream_video_capturer_source.cc
@@ -210,6 +210,7 @@ class LocalVideoCapturerSource final : public media::VideoCapturerSource {
       int max_requested_height,
       double max_requested_frame_rate,
       const VideoCaptureDeviceFormatsCB& callback) override;
+  media::VideoCaptureFormats GetPreferredFormats() override;
   void StartCapture(const media::VideoCaptureParams& params,
                     const VideoCaptureDeliverFrameCB& new_frame_callback,
                     const RunningCallback& running_callback) override;
@@ -300,6 +301,10 @@ void LocalVideoCapturerSource::GetCurrentSupportedFormats(
       session_id_, media::BindToCurrentLoop(base::Bind(
                        &LocalVideoCapturerSource::OnDeviceFormatsInUseReceived,
                        weak_factory_.GetWeakPtr())));
+}
+
+media::VideoCaptureFormats LocalVideoCapturerSource::GetPreferredFormats() {
+  return media::VideoCaptureFormats();
 }
 
 void LocalVideoCapturerSource::StartCapture(
