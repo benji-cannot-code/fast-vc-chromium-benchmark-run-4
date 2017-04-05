37b5bbb340de4bf8cb45a26bb4b90c0e3a8e3eaf
sdefresne <sdefresne@chromium.org>
2017-04-05 05:49:43 -0700

    [ios] Add free function to (de)serialise WebStateList.
    BUG=546222
    
    Review-Url: https://codereview.chromium.org/2771863002
    Cr-Commit-Position: refs/heads/master@{#462043}

---
+++ ios/shared/chrome/browser/tabs/web_state_list_serialization.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_SHARED_CHROME_BROWSER_TABS_WEB_STATE_LIST_SERIALIZATION_H_
+#define IOS_SHARED_CHROME_BROWSER_TABS_WEB_STATE_LIST_SERIALIZATION_H_
+
+#import <Foundation/Foundation.h>
+
+#include <memory>
+
+#include "base/callback_forward.h"
+
+@class CRWSessionStorage;
+class WebStateList;
+
+namespace web {
+class WebState;
+}
+
+// Factory for creating WebStates.
+using WebStateFactory =
+    base::RepeatingCallback<std::unique_ptr<web::WebState>(CRWSessionStorage*)>;
+
+// Returns an array of serialised sessions.
+NSArray<CRWSessionStorage*>* SerializeWebStateList(
+    WebStateList* web_state_list);
+
+// Restores |sessions| into |web_state_list| using |web_state_factory| for
+// creating the restored WebStates.
+void DeserializeWebStateList(WebStateList* web_state_list,
+                             NSArray<CRWSessionStorage*>* sessions,
+                             const WebStateFactory& web_state_factory);
+
+#endif  // IOS_SHARED_CHROME_BROWSER_TABS_WEB_STATE_LIST_SERIALIZATION_H_
