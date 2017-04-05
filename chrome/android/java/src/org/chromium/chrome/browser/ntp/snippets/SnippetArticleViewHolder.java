abddd8228c75a2c15050db4fbd16619e878cc18a
vitaliii <vitaliii@chromium.org>
2017-04-05 03:28:19 -0700

    [NTP::Downloads] Show file type icons as 36dp.
    UI people agreed to move from 32dp to 36dp.
    
    Reasons:
    1) We have 36dp resources;
    2) The ratio is consistent with Downloads UI.
    
    BUG=693552
    
    Review-Url: https://codereview.chromium.org/2797933002
    Cr-Commit-Position: refs/heads/master@{#462027}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/snippets/SnippetArticleViewHolder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/snippets/SnippetArticleViewHolder.java
@@ -89,7 +89,6 @@ public class SnippetArticleViewHolder extends CardViewHolder implements Impressi
     private final boolean mUseFaviconService;
     private final int mIconBackgroundColor;
     private final ColorStateList mIconForegroundColorList;
-    private final int mFileTypeIconPaddingPx;
 
     private FetchImageCallback mImageCallback;
     private SnippetArticle mArticle;
@@ -125,8 +124,6 @@ public class SnippetArticleViewHolder extends CardViewHolder implements Impressi
 
         mIconBackgroundColor = DownloadUtils.getIconBackgroundColor(parent.getContext());
         mIconForegroundColorList = DownloadUtils.getIconForegroundColorList(parent.getContext());
-        mFileTypeIconPaddingPx = itemView.getResources().getDimensionPixelSize(
-                R.dimen.snippets_thumbnail_file_type_icon_padding);
         mThumbnailProvider = new ThumbnailProviderImpl(
                 Math.min(mThumbnailView.getMaxWidth(), mThumbnailView.getMaxHeight()));
 
@@ -301,7 +298,6 @@ public class SnippetArticleViewHolder extends CardViewHolder implements Impressi
     private void setThumbnailFromBitmap(Bitmap thumbnail) {
         assert thumbnail != null && !thumbnail.isRecycled();
         mThumbnailView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        mThumbnailView.setPadding(0, 0, 0, 0);
         mThumbnailView.setBackground(null);
         mThumbnailView.setImageBitmap(thumbnail);
         mThumbnailView.setTint(null);
@@ -309,8 +305,6 @@ public class SnippetArticleViewHolder extends CardViewHolder implements Impressi
 
     private void setThumbnailFromFileType(int fileType) {
         mThumbnailView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
-        mThumbnailView.setPadding(mFileTypeIconPaddingPx, mFileTypeIconPaddingPx,
-                mFileTypeIconPaddingPx, mFileTypeIconPaddingPx);
         mThumbnailView.setBackgroundColor(mIconBackgroundColor);
         mThumbnailView.setImageResource(
                 DownloadUtils.getIconResId(fileType, DownloadUtils.ICON_SIZE_36_DP));
@@ -358,7 +352,6 @@ public class SnippetArticleViewHolder extends CardViewHolder implements Impressi
         }
 
         // Temporarily set placeholder and then fetch the thumbnail from a provider.
-        mThumbnailView.setPadding(0, 0, 0, 0);
         mThumbnailView.setBackground(null);
         mThumbnailView.setImageResource(R.drawable.ic_snippet_thumbnail_placeholder);
         mThumbnailView.setTint(null);
@@ -403,7 +396,6 @@ public class SnippetArticleViewHolder extends CardViewHolder implements Impressi
                 new BitmapDrawable(mThumbnailView.getResources(), scaledThumbnail)};
         TransitionDrawable transitionDrawable = new TransitionDrawable(layers);
         mThumbnailView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        mThumbnailView.setPadding(0, 0, 0, 0);
         mThumbnailView.setBackground(null);
         mThumbnailView.setImageDrawable(transitionDrawable);
         mThumbnailView.setTint(null);
