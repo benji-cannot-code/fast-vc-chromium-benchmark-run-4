9294a7958b25772e5745518359a3c6df327c7b3f
tzik <tzik@chromium.org>
2017-04-05 14:45:03 -0700

    Replace base::get with std::get
    base::get is no longer needed after the linux sysroot update. The new
    sysroot has libstdc++-4.8, that has rvalue reference support of
    std::tuple.
    
    Review-Url: https://codereview.chromium.org/2797133002
    Cr-Commit-Position: refs/heads/master@{#462229}

--- base/bind_internal.h
+++ base/bind_internal.h
@@ -361,7 +361,7 @@ struct Invoker<StorageType, R(UnboundArgs...)> {
 
     return InvokeHelper<is_weak_call, R>::MakeItSo(
         std::forward<Functor>(functor),
-        Unwrap(base::get<indices>(std::forward<BoundArgsTuple>(bound)))...,
+        Unwrap(std::get<indices>(std::forward<BoundArgsTuple>(bound)))...,
         std::forward<UnboundArgs>(unbound_args)...);
   }
 };
@@ -394,7 +394,7 @@ bool ApplyCancellationTraitsImpl(const Functor& functor,
                                  const BoundArgsTuple& bound_args,
                                  IndexSequence<indices...>) {
   return CallbackCancellationTraits<Functor, BoundArgsTuple>::IsCancelled(
-      functor, base::get<indices>(bound_args)...);
+      functor, std::get<indices>(bound_args)...);
 }
 
 // Relays |base| to corresponding CallbackCancellationTraits<>::Run(). Returns
