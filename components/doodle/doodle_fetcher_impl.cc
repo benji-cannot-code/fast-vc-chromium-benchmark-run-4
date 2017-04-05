96cc811f505efc5ab6315c90a88a4fc5bcf89f2c
rhalavati <rhalavati@chromium.org>
2017-04-05 03:24:27 -0700

    Network traffic annotation added to doodle_fetcher_impl.
    Network traffic annotation is added to network request of
    components/doodle/doodle_fetcher_impl.cc.
    
    BUG=656607
    
    Review-Url: https://codereview.chromium.org/2796913002
    Cr-Commit-Position: refs/heads/master@{#462025}

--- components/doodle/doodle_fetcher_impl.cc
+++ components/doodle/doodle_fetcher_impl.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/google/core/browser/google_util.h"
 #include "net/base/load_flags.h"
 #include "net/http/http_status_code.h"
+#include "net/traffic_annotation/network_traffic_annotation.h"
 #include "net/url_request/url_fetcher.h"
 
 using net::URLFetcher;
@@ -70,9 +71,30 @@ void DoodleFetcherImpl::FetchDoodle(FinishedCallback callback) {
   }
   DCHECK(!fetcher_.get());
   callbacks_.push_back(std::move(callback));
+  net::NetworkTrafficAnnotationTag traffic_annotation =
+      net::DefineNetworkTrafficAnnotation("doodle_fetcher", R"(
+        semantics {
+          sender: "Doodle Fetcher"
+          description:
+            "Retrieves metadata (image URL, clickthrough URL etc) for any "
+            "currently running Doodle."
+          trigger:
+            "Displaying the new tab page on Android."
+          data: "None."
+          destination: GOOGLE_OWNED_SERVICE
+        }
+        policy {
+          cookies_allowed: false
+          setting:
+            "Choosing a non-Google search engine in Chromium settings under "
+            "'Search Engine' will disable this feature."
+          policy_exception_justification:
+            "Not implemented, considered not useful as it does not upload any "
+            "data."
+        })");
   fetcher_ =
       URLFetcher::Create(BuildDoodleURL(GetGoogleBaseUrl(), gray_background_),
-                         URLFetcher::GET, this);
+                         URLFetcher::GET, this, traffic_annotation);
   fetcher_->SetRequestContext(download_context_.get());
   fetcher_->SetLoadFlags(net::LOAD_DO_NOT_SEND_COOKIES |
                          net::LOAD_DO_NOT_SAVE_COOKIES |
