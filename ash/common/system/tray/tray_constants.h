7ad132017378a9ab996b593fb66626b063eb28fe
tdanderson <tdanderson@chromium.org>
2017-04-05 07:30:37 -0700

    [Ash] Remove pre-MD code paths from HoverHighlightView
    This CL removes all mention of ash::MaterialDesignController
    from the HoverHighlightView class and performs some
    preliminary cleanup of dead code. Further refactoring
    and code cleanup for this class requires some additional
    investigation and is tracked by crbug.com/708190.
    
    BUG=686345
    TEST=manual
    
    Review-Url: https://codereview.chromium.org/2795143005
    Cr-Commit-Position: refs/heads/master@{#462064}

--- ash/common/system/tray/tray_constants.h
+++ ash/common/system/tray/tray_constants.h
@@ -77,7 +77,6 @@ extern const int kTrayRoundedBorderRadius;
 extern const int kTrayToggleButtonWidth;
 
 extern const SkColor kBackgroundColor;
-extern const SkColor kHoverBackgroundColor;
 extern const SkColor kPublicAccountUserCardTextColor;
 extern const SkColor kPublicAccountUserCardNameColor;
 
