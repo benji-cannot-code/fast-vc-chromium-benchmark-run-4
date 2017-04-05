98e5d6f49ac7fe87f8edf7acced18afe550140ba
baxley <baxley@chromium.org>
2017-04-05 10:18:49 -0700

    Fix ErrorPageTestCase to not wait for a matcher with grey_nil()
    A negative matcher (WaitForWebViewNotContainingText), should be
    used instead of the positive matcher with grey_nil().
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2798033003
    Cr-Commit-Position: refs/heads/master@{#462128}

--- ios/chrome/browser/ui/error_page_egtest.mm
+++ ios/chrome/browser/ui/error_page_egtest.mm
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using chrome_test_util::OmniboxText;
 using chrome_test_util::TapWebViewElementWithId;
 using chrome_test_util::WebViewContainingText;
+using chrome_test_util::WebViewNotContainingText;
 
 using web::test::HttpServer;
 
@@ -50,8 +51,8 @@ using web::test::HttpServer;
       assertWithMatcher:grey_notNil()];
   const std::string kError =
       l10n_util::GetStringUTF8(IDS_ERRORPAGES_HEADING_NOT_AVAILABLE);
-  [[EarlGrey selectElementWithMatcher:WebViewContainingText(kError)]
-      assertWithMatcher:grey_nil()];
+  [[EarlGrey selectElementWithMatcher:WebViewNotContainingText(kError)]
+      assertWithMatcher:grey_notNil()];
 }
 
 #pragma mark - tests
