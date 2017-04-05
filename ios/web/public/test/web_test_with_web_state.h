c7bcc9c81995712a5c77cb7cbf742076fb6f1786
liaoyuke <liaoyuke@chromium.org>
2017-04-05 16:30:52 -0700

    Reland of Set user agent type of transient item the same as pending item
    The original CL assumes that a pending item should always exists when
    adding a transient item, however this sometimes breaks because as
    described in crbug.com/676129, NavigationManagerImpl or
    CRWSessionController don't create a pending item if its URL is the same
    as the last committed item.
    
    This CL fixes the problem by relaxing the condition to the following:
    if pending item exists, use pending item's user agent type, otherwise
    use last committed non-native item's user agent type.
    
    BUG=676129
    
    Review-Url: https://codereview.chromium.org/2805453002
    Cr-Commit-Position: refs/heads/master@{#462271}

--- ios/web/public/test/web_test_with_web_state.h
+++ ios/web/public/test/web_test_with_web_state.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/ios/block_types.h"
 #include "base/message_loop/message_loop.h"
 #include "ios/web/public/test/web_test.h"
+#include "ui/base/page_transition_types.h"
 #include "url/gurl.h"
 
 namespace web {
@@ -25,6 +26,9 @@ class WebTestWithWebState : public WebTest,
   // WebTest overrides.
   void SetUp() override;
   void TearDown() override;
+
+  // Adds a pending item to the NavigationManager associated with the WebState.
+  void AddPendingItem(const GURL& url, ui::PageTransition transition);
 
   // Loads the specified HTML content with URL into the WebState.
   void LoadHtml(NSString* html, const GURL& url);
