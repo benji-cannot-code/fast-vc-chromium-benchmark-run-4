64576124c1edd451fd3fd169d7f76a48d9dc655f
sebsg <sebsg@chromium.org>
2017-04-04 17:10:50 -0700

    [Payments] Normalize contact info phone# in PaymentResponse on Android.
    BUG=707960
    
    Review-Url: https://codereview.chromium.org/2791973003
    Cr-Commit-Position: refs/heads/master@{#461908}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/PhoneNumberUtil.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/PhoneNumberUtil.java
@@ -28,7 +28,7 @@ public class PhoneNumberUtil {
         public void afterTextChanged(Editable s) {
             if (mSelfChange) return;
 
-            String formattedNumber = format(s.toString());
+            String formattedNumber = formatForDisplay(s.toString());
             mSelfChange = true;
             s.replace(0, s.length(), formattedNumber, 0, formattedNumber.length());
             mSelfChange = false;
@@ -48,10 +48,24 @@ public class PhoneNumberUtil {
      * office will be formatted as "+41 44 668 1800" in INTERNATIONAL format.
      *
      * @param phoneNumber The given phone number.
-     * @return formatted phone number.
+     * @return Formatted phone number.
      */
-    public static String format(String phoneNumber) {
-        return nativeFormat(phoneNumber);
+    public static String formatForDisplay(String phoneNumber) {
+        return nativeFormatForDisplay(phoneNumber);
+    }
+
+    /**
+     * Formats the given phone number in E.164 format as specified in the Payment Request spec
+     * (https://w3c.github.io/browser-payment-api/#paymentrequest-updated-algorithm)
+     * [i18n::phonenumbers::PhoneNumberUtil::PhoneNumberFormat::E164], returning the original number
+     * if no formatting can be made. For example, the number of the Google Zürich office will be
+     * formatted as "+41446681800" in E.164 format.
+     *
+     * @param phoneNumber The given phone number.
+     * @return Formatted phone number.
+     */
+    public static String formatForResponse(String phoneNumber) {
+        return nativeFormatForResponse(phoneNumber);
     }
 
     /**
@@ -66,6 +80,7 @@ public class PhoneNumberUtil {
         return nativeIsValidNumber(phoneNumber);
     }
 
-    private static native String nativeFormat(String phoneNumber);
+    private static native String nativeFormatForDisplay(String phoneNumber);
+    private static native String nativeFormatForResponse(String phoneNumber);
     private static native boolean nativeIsValidNumber(String phoneNumber);
 }
