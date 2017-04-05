baa5f11115d3568477bdb5a2b6dba4b45c64621e
wanchang.ryu <wanchang.ryu@lge.com>
2017-04-05 14:31:41 -0700

    Fix sticky bottom is not applied with both sticky
    The box which has a sticky position should be used instead of
    scroll container block to check whether the box can be
    positioned in constraningsize with sticky both sides offset.
    
    BUG=703853
    TEST=fast/css/sticky/sticky-both-sides*
    
    Review-Url: https://codereview.chromium.org/2788053002
    Cr-Commit-Position: refs/heads/master@{#462224}

--- third_party/WebKit/Source/core/layout/LayoutBoxModelObject.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBoxModelObject.cpp
@@ -1003,8 +1003,7 @@ void LayoutBoxModelObject::updateStickyPositionConstraints() const {
   if (!style()->left().isAuto() && !style()->right().isAuto()) {
     if (horizontalOffsets >
             scrollContainerRelativeContainingBlockRect.width() ||
-        horizontalOffsets + scrollContainerRelativeContainingBlockRect.width() >
-            constrainingSize.width()) {
+        horizontalOffsets + stickyBoxRect.width() > constrainingSize.width()) {
       skipRight = style()->isLeftToRightDirection();
       skipLeft = !skipRight;
     }
@@ -1035,8 +1034,7 @@ void LayoutBoxModelObject::updateStickyPositionConstraints() const {
                             LayoutUnit(constrainingSize.height()));
   if (!style()->top().isAuto() && !style()->bottom().isAuto()) {
     if (verticalOffsets > scrollContainerRelativeContainingBlockRect.height() ||
-        verticalOffsets + scrollContainerRelativeContainingBlockRect.height() >
-            constrainingSize.height()) {
+        verticalOffsets + stickyBoxRect.height() > constrainingSize.height()) {
       skipBottom = true;
     }
   }
