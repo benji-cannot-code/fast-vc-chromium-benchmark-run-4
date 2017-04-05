6eb28536ff5e93123c93a1735a21be397b07c2ba
xunjieli <xunjieli@chromium.org>
2017-04-05 12:43:26 -0700

    [Cronet] Initialize StatisticsRecorder early
    StatisticsRecorder must be initialized before histograms are emitted. Otherwise,
    StatisticsRecorder::FactoryGet() leaks one histogram per call for a given name.
    
    BUG=707836
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_cronet_tester
    
    Review-Url: https://codereview.chromium.org/2790063004
    Cr-Commit-Position: refs/heads/master@{#462185}

--- components/cronet/android/cronet_library_loader.cc
+++ components/cronet/android/cronet_library_loader.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/message_loop/message_loop.h"
+#include "base/metrics/statistics_recorder.h"
 #include "components/cronet/android/cronet_bidirectional_stream_adapter.h"
 #include "components/cronet/android/cronet_upload_data_stream_adapter.h"
 #include "components/cronet/android/cronet_url_request_adapter.h"
@@ -68,6 +69,9 @@ bool NativeInit() {
   if (!base::android::OnJNIOnLoadInit())
     return false;
   url::Initialize();
+  // Initializes the statistics recorder system. This needs to be done before
+  // emitting histograms to prevent memory leaks (crbug.com/707836).
+  base::StatisticsRecorder::Initialize();
   return true;
 }
 
