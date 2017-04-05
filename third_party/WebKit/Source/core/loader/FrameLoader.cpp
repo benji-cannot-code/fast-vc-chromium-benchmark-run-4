90bb9a672b3edcf1fd45d58ea59a855a99753381
japhet <japhet@chromium.org>
2017-04-05 12:09:22 -0700

    When historyEntryRequiresUserGesture is enabled, exempt docs that have been committed for 5 seconds
    Also, add a test for historyEntryRequiresUserGesture flag.
    
    BUG=638198
    TEST=http/tests/history/history-entry-requires-user-gesture.html
    
    Review-Url: https://codereview.chromium.org/2780953002
    Cr-Commit-Position: refs/heads/master@{#462166}

--- third_party/WebKit/Source/core/loader/FrameLoader.cpp
+++ third_party/WebKit/Source/core/loader/FrameLoader.cpp
@@ -717,9 +717,8 @@ FrameLoadType FrameLoader::determineFrameLoadType(
       m_documentLoader->loadType() == FrameLoadTypeReload)
     return FrameLoadTypeReload;
 
-  if (m_frame->settings()->getHistoryEntryRequiresUserGesture() &&
-      request.originDocument() &&
-      !request.originDocument()->frame()->hasReceivedUserGesture())
+  if (request.originDocument() &&
+      !request.originDocument()->canCreateHistoryEntry())
     return FrameLoadTypeReplaceCurrentItem;
 
   if (request.resourceRequest().url().isEmpty() &&
