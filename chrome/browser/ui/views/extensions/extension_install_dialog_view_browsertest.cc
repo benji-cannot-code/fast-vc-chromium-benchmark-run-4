944f59af0dcf9ccdd908ea945a526b5fe4bf8905
ackermanb <ackermanb@chromium.org>
2017-04-04 18:31:42 -0700

    [Extensions UI] Initially disabled OK button for extension install prompts and enable them after a 500 ms time period.
    BUG=394518
    
    Review-Url: https://codereview.chromium.org/2716353003
    Cr-Commit-Position: refs/heads/master@{#461933}

--- chrome/browser/ui/views/extensions/extension_install_dialog_view_browsertest.cc
+++ chrome/browser/ui/views/extensions/extension_install_dialog_view_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/macros.h"
+#include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
 #include "chrome/browser/extensions/extension_icon_manager.h"
@@ -202,4 +203,24 @@ IN_PROC_BROWSER_TEST_F(ExtensionInstallDialogViewTest, NotifyDelegate) {
     // TODO(devlin): Should this be ABORTED?
     EXPECT_EQ(ExtensionInstallPrompt::Result::USER_CANCELED, helper.result());
   }
+}
+
+// Verifies that the "Add extension" button is disabled initally, but re-enabled
+// after a short time delay.
+IN_PROC_BROWSER_TEST_F(ExtensionInstallDialogViewTest, InstallButtonDelay) {
+  ExtensionInstallDialogView::SetInstallButtonDelayForTesting(0);
+  ExtensionInstallPromptTestHelper helper;
+  views::DialogDelegateView* delegate_view = CreateAndShowPrompt(&helper);
+
+  // Check that dialog is visible.
+  EXPECT_TRUE(delegate_view->visible());
+
+  // Check initial button states.
+  EXPECT_FALSE(delegate_view->IsDialogButtonEnabled(ui::DIALOG_BUTTON_OK));
+  EXPECT_TRUE(delegate_view->IsDialogButtonEnabled(ui::DIALOG_BUTTON_CANCEL));
+
+  // Check OK button state after timeout to verify that it is re-enabled.
+  base::RunLoop().RunUntilIdle();
+  EXPECT_TRUE(delegate_view->IsDialogButtonEnabled(ui::DIALOG_BUTTON_OK));
+  delegate_view->Close();
 }
