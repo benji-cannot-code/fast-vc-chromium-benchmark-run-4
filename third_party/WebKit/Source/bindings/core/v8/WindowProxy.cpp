44654f7618cbba58368fd19fdd94006f36433318
yukishiino <yukishiino@chromium.org>
2017-04-06 08:25:08 -0700

    v8binding: Makes reinitialization of WindowProxy clearer.
    This is a follow-up of https://crrev.com/2769803003 .
    Added a little more DCHECK so that the lifecycle of
    WindowProxy will be clearer.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2794873002
    Cr-Commit-Position: refs/heads/master@{#462479}

--- third_party/WebKit/Source/bindings/core/v8/WindowProxy.cpp
+++ third_party/WebKit/Source/bindings/core/v8/WindowProxy.cpp
@@ -80,7 +80,8 @@ v8::Local<v8::Object> WindowProxy::globalProxyIfNotDetached() {
 }
 
 v8::Local<v8::Object> WindowProxy::releaseGlobalProxy() {
-  DCHECK(m_lifecycle != Lifecycle::ContextIsInitialized);
+  DCHECK(m_lifecycle == Lifecycle::ContextIsUninitialized ||
+         m_lifecycle == Lifecycle::GlobalObjectIsDetached);
 
   // Make sure the global object was detached from the proxy by calling
   // clearForNavigation().
@@ -93,6 +94,8 @@ v8::Local<v8::Object> WindowProxy::releaseGlobalProxy() {
 }
 
 void WindowProxy::setGlobalProxy(v8::Local<v8::Object> globalProxy) {
+  DCHECK_EQ(m_lifecycle, Lifecycle::ContextIsUninitialized);
+
   CHECK(m_globalProxy.isEmpty());
   m_globalProxy.set(m_isolate, globalProxy);
 
@@ -102,7 +105,7 @@ void WindowProxy::setGlobalProxy(v8::Local<v8::Object> globalProxy) {
   // Without this, existing script references to a swapped in RemoteDOMWindow
   // would be broken until that RemoteDOMWindow was vended again through an
   // interface like window.frames.
-  initializeIfNeeded();
+  initialize();
 }
 
 // Create a new environment and setup the global object.
