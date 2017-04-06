e5fc4b3cb0aacafe2236bddd4f492af40a592507
dullweber <dullweber@chromium.org>
2017-04-06 02:53:13 -0700

    Change layout and texts for new CBD dialog.
    - Improved spinner layout
    - New spinner texts (1 day -> 24 hours, 1 week -> 7 days)
    - Implement a way to show different counter texts for basic and advanced mode
    - New counter texts for basic mode
    - Clickable myactivity.google.com link in checkbox summary
    - Add instrumentation test to verify that the right string is shown depending on sign in and sync state.
    
    Screenshots for default display size (Nexus 6P):
    https://screenshot.googleplex.com/P1hFk9v8V3k.png
    https://screenshot.googleplex.com/zVjhJJWnXUo.png
    
    Screenshots on largest display size (Nexus 6P):
    https://screenshot.googleplex.com/m9nBDbQ5ra5.png
    https://screenshot.googleplex.com/Gb6vA38rfOH.png
    
    BUG=681523
    
    Review-Url: https://codereview.chromium.org/2730703003
    Cr-Commit-Position: refs/heads/master@{#462406}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/ClearBrowsingDataPreferencesTab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/ClearBrowsingDataPreferencesTab.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.preferences.privacy;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.browsing_data.TimePeriod;
 
 /**
  * The base class for a clear browsing data tab.
@@ -37,6 +39,25 @@ public class ClearBrowsingDataPreferencesTab extends ClearBrowsingDataPreference
     @Override
     protected int getPreferenceXmlId() {
         return R.xml.clear_browsing_data_preferences_tab;
+    }
+
+    @Override
+    protected TimePeriodSpinnerOption[] getTimePeriodSpinnerOptions() {
+        Activity activity = getActivity();
+
+        TimePeriodSpinnerOption[] options = new TimePeriodSpinnerOption[] {
+                new TimePeriodSpinnerOption(TimePeriod.LAST_HOUR,
+                        activity.getString(R.string.clear_browsing_data_tab_period_hour)),
+                new TimePeriodSpinnerOption(TimePeriod.LAST_DAY,
+                        activity.getString(R.string.clear_browsing_data_tab_period_24_hours)),
+                new TimePeriodSpinnerOption(TimePeriod.LAST_WEEK,
+                        activity.getString(R.string.clear_browsing_data_tab_period_7_days)),
+                new TimePeriodSpinnerOption(TimePeriod.FOUR_WEEKS,
+                        activity.getString(R.string.clear_browsing_data_tab_period_four_weeks)),
+                new TimePeriodSpinnerOption(TimePeriod.ALL_TIME,
+                        activity.getString(R.string.clear_browsing_data_tab_period_everything))};
+
+        return options;
     }
 
     @Override
