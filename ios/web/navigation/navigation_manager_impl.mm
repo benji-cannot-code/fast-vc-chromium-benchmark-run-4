2409611ff0c100405c9fc2708700098e0c2f8af6
eugenebut <eugenebut@chromium.org>
2017-04-06 12:08:30 -0700

    Removed NavigationManagerImpl::GetItems.
    This method is unused.
    
    BUG=546365
    
    Review-Url: https://codereview.chromium.org/2799263004
    Cr-Commit-Position: refs/heads/master@{#462571}

--- ios/web/navigation/navigation_manager_impl.mm
+++ ios/web/navigation/navigation_manager_impl.mm
@@ -237,10 +237,6 @@ void NavigationManagerImpl::AddPendingItem(
   }
 }
 
-NavigationItemList NavigationManagerImpl::GetItems() const {
-  return CreateNavigationItemList([session_controller_ items]);
-}
-
 BrowserState* NavigationManagerImpl::GetBrowserState() const {
   return browser_state_;
 }
