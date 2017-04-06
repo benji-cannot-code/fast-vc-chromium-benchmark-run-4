077b97bcf2385f61c0bf135a7bf8ba468c178474
rdevlin.cronin <rdevlin.cronin@chromium.org>
2017-04-05 17:41:32 -0700

    [Extensions Bindings] Split out WebRequestEvent module
    Split the WebRequestEvent JS module out from
    web_request_internal_custom_bindings.js.
    
    BUG=653596
    
    Review-Url: https://codereview.chromium.org/2801473002
    Cr-Commit-Position: refs/heads/master@{#462297}

--- extensions/renderer/resources/web_request_custom_bindings.js
+++ extensions/renderer/resources/web_request_custom_bindings.js
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 var binding = require('binding').Binding.create('webRequest');
 var sendRequest = require('sendRequest').sendRequest;
-var WebRequestEvent = require('webRequestInternal').WebRequestEvent;
+var WebRequestEvent = require('webRequestEvent').WebRequestEvent;
 
 binding.registerCustomHook(function(api) {
   var apiFunctions = api.apiFunctions;
