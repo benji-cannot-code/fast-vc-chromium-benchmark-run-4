a7063db81cbccf29f39609d815e5b29414973468
arthursonzogni <arthursonzogni@chromium.org>
2017-04-06 10:04:12 -0700

    PlzNavigate: add DumpWithoutCrashing for invalid main resource request.
    This CL is an attempt to solve https://crbug.com/705508
    A DumpWithoutCrashing is added on the renderer side to get more
    informations.
    
    BUG=705508
    
    Review-Url: https://codereview.chromium.org/2805693002
    Cr-Commit-Position: refs/heads/master@{#462517}

--- content/child/web_url_loader_impl.cc
+++ content/child/web_url_loader_impl.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/bind.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
@@ -611,6 +612,16 @@ void WebURLLoaderImpl::Context::Start(const WebURLRequest& request,
     DCHECK(!sync_load_response);
     DCHECK_NE(WebURLRequest::FrameTypeNone, request.getFrameType());
     resource_request->resource_body_stream_url = stream_override_->stream_url;
+  }
+
+  // PlzNavigate: Invalid renderer main resource requests are rejected by the
+  // browser. This should not happen.
+  // TODO(arthursonzogni): Remove this when the root cause for
+  // https://crbug.com/705508 is found.
+  if (IsBrowserSideNavigationEnabled() &&
+      IsResourceTypeFrame(resource_request->resource_type) &&
+      !resource_request->resource_body_stream_url.SchemeIs(url::kBlobScheme)) {
+    base::debug::DumpWithoutCrashing();
   }
 
   const RequestExtraData empty_extra_data;
