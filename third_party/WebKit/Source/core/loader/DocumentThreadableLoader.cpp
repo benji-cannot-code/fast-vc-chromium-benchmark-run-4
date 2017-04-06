08f1a7a0a2cb0454557e007754ceb48cd6422fbd
alph <alph@chromium.org>
2017-04-05 17:09:33 -0700

    [instrumentation] Turn inspector override "probes" return values into output parameters.
    This is a temporary measure. They should eventually become regular functions.
    
    BUG=695625
    
    Review-Url: https://codereview.chromium.org/2800853002
    Cr-Commit-Position: refs/heads/master@{#462286}

--- third_party/WebKit/Source/core/loader/DocumentThreadableLoader.cpp
+++ third_party/WebKit/Source/core/loader/DocumentThreadableLoader.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/FrameConsole.h"
 #include "core/frame/LocalFrame.h"
 #include "core/frame/LocalFrameClient.h"
+#include "core/inspector/InspectorNetworkAgent.h"
 #include "core/inspector/InspectorTraceEvents.h"
 #include "core/loader/DocumentThreadableLoaderClient.h"
 #include "core/loader/FrameLoader.h"
@@ -387,8 +388,9 @@ void DocumentThreadableLoader::makeCrossOriginAccessRequest(
     crossOriginRequest.setServiceWorkerMode(
         WebURLRequest::ServiceWorkerMode::None);
 
-    bool shouldForcePreflight = request.isExternalRequest() ||
-                                probe::shouldForceCORSPreflight(document());
+    bool shouldForcePreflight = request.isExternalRequest();
+    if (!shouldForcePreflight)
+      probe::shouldForceCORSPreflight(document(), &shouldForcePreflight);
     bool canSkipPreflight =
         CrossOriginPreflightResultCache::shared().canSkipPreflight(
             getSecurityOrigin()->toString(), crossOriginRequest.url(),
--- third_party/WebKit/Source/core/loader/FrameFetchContext.cpp
+++ third_party/WebKit/Source/core/loader/FrameFetchContext.cpp
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/imports/HTMLImportsController.h"
 #include "core/inspector/ConsoleMessage.h"
 #include "core/inspector/IdentifiersFactory.h"
-#include "core/inspector/InspectorNetworkAgent.h"
 #include "core/inspector/InspectorTraceEvents.h"
 #include "core/loader/DocumentLoader.h"
 #include "core/loader/FrameLoader.h"
@@ -725,7 +724,9 @@ ResourceRequestBlockedReason FrameFetchContext::canRequestInternal(
     SecurityViolationReportingPolicy reportingPolicy,
     FetchRequest::OriginRestriction originRestriction,
     ResourceRequest::RedirectStatus redirectStatus) const {
-  if (probe::shouldBlockRequest(frame(), resourceRequest))
+  bool shouldBlockRequest = false;
+  probe::shouldBlockRequest(frame(), resourceRequest, &shouldBlockRequest);
+  if (shouldBlockRequest)
     return ResourceRequestBlockedReason::Inspector;
 
   SecurityOrigin* securityOrigin = options.securityOrigin.get();
