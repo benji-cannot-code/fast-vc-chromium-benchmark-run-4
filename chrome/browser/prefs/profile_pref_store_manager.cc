c4b1ef44ad824ba847040ae9d40972ed1d6b0c24
tibell <tibell@chromium.org>
2017-04-05 22:31:16 -0700

    Pref service: have Mash and Chrome connect to the right pref stores
    Chrome contains the implementation of all pref stores, except the
    default pref store, in-process and thus only needs to connect to
    one store. Mash needs to connect to all pref stores.
    
    This is achieved by having clients specify which pref stores they
    already know of in-process, rather than the other way
    around. That way e.g. mash doesn't need to know which pref stores
    Chrome exposes to it.
    
    BUG=654988
    
    Review-Url: https://codereview.chromium.org/2791903003
    Cr-Commit-Position: refs/heads/master@{#462364}

--- chrome/browser/prefs/profile_pref_store_manager.cc
+++ chrome/browser/prefs/profile_pref_store_manager.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/sequenced_task_runner.h"
 #include "build/build_config.h"
+#include "chrome/browser/prefs/browser_prefs.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_features.h"
 #include "components/pref_registry/pref_registry_syncable.h"
@@ -100,8 +101,12 @@ PersistentPrefStore* ProfilePrefStoreManager::CreateProfilePrefStore(
                          std::move(validation_delegate), connector);
     prefs::mojom::PrefStoreConnectorPtr pref_connector;
     connector->BindInterface(prefs::mojom::kServiceName, &pref_connector);
+    auto in_process_types_set = chrome::InProcessPrefStores();
+    std::vector<PrefValueStore::PrefStoreType> in_process_types(
+        in_process_types_set.begin(), in_process_types_set.end());
     return new prefs::PersistentPrefStoreClient(std::move(pref_connector),
-                                                std::move(pref_registry));
+                                                std::move(pref_registry),
+                                                std::move(in_process_types));
   }
   if (!kPlatformSupportsPreferenceTracking) {
     return new JsonPrefStore(
--- services/preferences/public/cpp/pref_service_factory.h
+++ services/preferences/public/cpp/pref_service_factory.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/callback.h"
+#include "components/prefs/pref_value_store.h"
 #include "services/preferences/public/interfaces/preferences.mojom.h"
 
 class PrefRegistry;
@@ -36,10 +37,12 @@ using ConnectCallback = base::Callback<void(std::unique_ptr<::PrefService>)>;
 // asynchronous and |callback| will be called when it has been established. All
 // preferences that will be accessed need to be registered in |pref_registry|
 // first.
-void ConnectToPrefService(service_manager::Connector* connector,
-                          scoped_refptr<PrefRegistry> pref_registry,
-                          ConnectCallback callback,
-                          base::StringPiece service_name = mojom::kServiceName);
+void ConnectToPrefService(
+    service_manager::Connector* connector,
+    scoped_refptr<PrefRegistry> pref_registry,
+    const std::vector<PrefValueStore::PrefStoreType>& already_connected_types,
+    ConnectCallback callback,
+    base::StringPiece service_name = mojom::kServiceName);
 
 }  // namespace prefs
 
