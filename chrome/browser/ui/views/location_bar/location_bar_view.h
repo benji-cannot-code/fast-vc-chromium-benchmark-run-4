233a7ae602963e036a1feb12d9c9ad254cb4147d
estade <estade@chromium.org>
2017-04-05 18:42:25 -0700

    Make keyword hint view ("press tab to search foo.com") clickable.
    Since there is no tab button on touch devices (on screen keyboard has
    no tab), the view will need to be clickable. This might as well apply to
    all devices, physical keyboard or no.
    
    As noted on the bug, the view's appearance will also change to
    something that makes more sense when you don't have a tab key.
    
    BUG=698422
    
    Review-Url: https://codereview.chromium.org/2792103005
    Cr-Commit-Position: refs/heads/master@{#462315}

--- chrome/browser/ui/views/location_bar/location_bar_view.h
+++ chrome/browser/ui/views/location_bar/location_bar_view.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/animation/slide_animation.h"
 #include "ui/gfx/font.h"
 #include "ui/gfx/geometry/rect.h"
+#include "ui/views/controls/button/button.h"
 #include "ui/views/drag_controller.h"
 
 class CommandUpdater;
@@ -68,7 +69,8 @@ class LocationBarView : public LocationBar,
                         public ChromeOmniboxEditController,
                         public DropdownBarHostDelegate,
                         public TemplateURLServiceObserver,
-                        public zoom::ZoomEventManagerObserver {
+                        public zoom::ZoomEventManagerObserver,
+                        public views::ButtonListener {
  public:
   class Delegate {
    public:
@@ -236,6 +238,9 @@ class LocationBarView : public LocationBar,
   // ZoomEventManagerObserver:
   // Updates the view for the zoom icon when default zoom levels change.
   void OnDefaultZoomLevelChanged() override;
+
+  // views::ButtonListener:
+  void ButtonPressed(views::Button* sender, const ui::Event& event) override;
 
  private:
   using ContentSettingViews = std::vector<ContentSettingImageView*>;
