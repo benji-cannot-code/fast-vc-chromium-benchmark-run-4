6fbac81d3d36a3d3dce5b4a03f2b0c6f0863e318
shaktisahu <shaktisahu@chromium.org>
2017-04-05 21:47:21 -0700

    Provide component name when opening offline pages from download snack bar
    When component name is not provided, the intent defaults to
    ChromeLauncherActivity which causes the issues in multi-window mode.
    This CL provides ChromeTabbedActivity as the component name for
    launching offline pages, which fixes the bug.
    This also fixes a similar multi-window issue if offline pages is
    launched from notifications.
    
    BUG=689538
    
    Review-Url: https://codereview.chromium.org/2795693007
    Cr-Commit-Position: refs/heads/master@{#462359}

--- chrome/android/java/src/org/chromium/chrome/browser/offlinepages/downloads/OfflinePageDownloadBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/offlinepages/downloads/OfflinePageDownloadBridge.java
@@ -5,12 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.offlinepages.downloads;
 
+import android.app.Activity;
 import android.content.ComponentName;
 import android.support.annotation.Nullable;
 
+import org.chromium.base.ApplicationStatus;
 import org.chromium.base.ObserverList;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
+import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.download.DownloadItem;
 import org.chromium.chrome.browser.download.DownloadServiceDelegate;
 import org.chromium.chrome.browser.download.ui.BackendProvider.OfflinePageDelegate;
@@ -236,10 +239,21 @@ public class OfflinePageDownloadBridge implements DownloadServiceDelegate, Offli
                 new Observer() {
                     @Override
                     public void onItemsLoaded() {
-                        bridge.openItem(id.id, null);
+                        bridge.openItem(id.id, getComponentName());
                         bridge.destroyServiceDelegate();
                     }
                 });
+    }
+
+    private static ComponentName getComponentName() {
+        if (!ApplicationStatus.hasVisibleActivities()) return null;
+
+        Activity activity = ApplicationStatus.getLastTrackedFocusedActivity();
+        if (activity instanceof ChromeTabbedActivity) {
+            return activity.getComponentName();
+        }
+
+        return null;
     }
 
     @CalledByNative
