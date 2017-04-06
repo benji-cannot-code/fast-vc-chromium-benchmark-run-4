1e42ceb8b185417f12d87d37a38e3502745088d2
khorimoto <khorimoto@chromium.org>
2017-04-06 14:54:36 -0700

    [CrOS Tether] Add FakeCryptAuthService, a test double for CryptAuthService.
    BUG=672263
    
    Review-Url: https://codereview.chromium.org/2799683004
    Cr-Commit-Position: refs/heads/master@{#462637}

--- components/cryptauth/cryptauth_service.h
+++ components/cryptauth/cryptauth_service.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_CRYPTAUTH_CRYPTAUTH_SERVICE_H_
 #define COMPONENTS_CRYPTAUTH_CRYPTAUTH_SERVICE_H_
 
+#include <memory>
+
 #include "base/macros.h"
 #include "components/cryptauth/proto/cryptauth_api.pb.h"
 
