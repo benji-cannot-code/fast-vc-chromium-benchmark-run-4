3c8119f10a5a8c5d17bf924052bbefd41ec10596
rhalavati <rhalavati@chromium.org>
2017-04-05 22:48:02 -0700

    Cookies disabled for url_provision_fetcher.
    Following discussion in https://codereview.chromium.org/2717483002, cookies
    are disabled for network request of
    content/browser/media/url_provision_fetcher.cc
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2804563002
    Cr-Commit-Position: refs/heads/master@{#462369}

--- content/browser/media/url_provision_fetcher.cc
+++ content/browser/media/url_provision_fetcher.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "content/public/browser/provision_fetcher_factory.h"
 #include "media/base/bind_to_current_loop.h"
+#include "net/base/load_flags.h"
 #include "net/url_request/url_fetcher.h"
 
 using net::URLFetcher;
@@ -39,6 +40,8 @@ void URLProvisionFetcher::Retrieve(
   request_->SetUploadData("", "");
   request_->AddExtraRequestHeader("User-Agent: Widevine CDM v1.0");
   request_->AddExtraRequestHeader("Content-Type: application/json");
+  request_->SetLoadFlags(net::LOAD_DO_NOT_SAVE_COOKIES |
+                         net::LOAD_DO_NOT_SEND_COOKIES);
 
   DCHECK(context_getter_);
   request_->SetRequestContext(context_getter_);
