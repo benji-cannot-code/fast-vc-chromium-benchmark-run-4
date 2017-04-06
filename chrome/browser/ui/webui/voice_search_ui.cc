0acd668b5f281dc246c86ec89672095597e22181
jbroman <jbroman@chromium.org>
2017-04-06 09:00:59 -0700

    Remove unnecessary v8.h include from chrome/browser/.
    Review-Url: https://codereview.chromium.org/2800533002
    Cr-Commit-Position: refs/heads/master@{#462501}

--- chrome/browser/ui/webui/voice_search_ui.cc
+++ chrome/browser/ui/webui/voice_search_ui.cc
@@ -51,7 +51,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension.h"
 #include "extensions/features/features.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "v8/include/v8.h"
 
 #if defined(OS_WIN)
 #include "base/win/windows_version.h"
