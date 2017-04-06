ccc2e285d5a8928689f8a94665d970a05e0f35e7
fdoray <fdoray@chromium.org>
2017-04-06 08:40:17 -0700

    Use ScopedTaskEnvironment instead of MessageLoop in google_apis unit tests.
    ScopedTaskEnvironment allows usage of ThreadTaskRunnerHandle and
    base/task_scheduler/post_task.h within its scope. It should be
    instantiated in every unit test that uses either of these APIs
    (i.e. no test should instantiate a MessageLoop directly).
    
    Motivation for ScopedTaskEnvironment can be found in:
    https://docs.google.com/document/d/1QabRo8c7D9LsYY3cEcaPQbOCLo8Tu-6VLykYXyl3Pkk/edit
    
    BUG=708584
    R=gab@chromium.org
    TBR=zelidrag@chromium.org
    
    Review-Url: https://codereview.chromium.org/2801643003
    Cr-Commit-Position: refs/heads/master@{#462488}

--- google_apis/gaia/oauth2_access_token_fetcher_impl_unittest.cc
+++ google_apis/gaia/oauth2_access_token_fetcher_impl_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
 #include "google_apis/gaia/gaia_urls.h"
@@ -125,7 +126,7 @@ class OAuth2AccessTokenFetcherImplTest : public testing::Test {
   }
 
  protected:
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
   MockUrlFetcherFactory factory_;
   MockOAuth2AccessTokenConsumer consumer_;
   scoped_refptr<net::TestURLRequestContextGetter> request_context_getter_;
