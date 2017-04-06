c1d7487f00108038827fe8498ee588873e13c8ef
hta <hta@chromium.org>
2017-04-06 10:18:29 -0700

    Move getUserMedia finish to "when audio track configured".
    Previously, getUserMedia() would complete as soon as the sources
    were initialized - which did not do all the initialization.
    This CL will make getUserMedia() return when the track has been
    told of its configuration through OnSetFormat() from upstream.
    
    This will allow GetSettings() to read the audio configuration from
    the track without worrying about whether the configuration is
    propagated or not.
    
    BUG=704918
    
    Review-Url: https://codereview.chromium.org/2777583002
    Cr-Commit-Position: refs/heads/master@{#462523}

--- content/renderer/media/user_media_client_impl.cc
+++ content/renderer/media/user_media_client_impl.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/renderer/render_frame.h"
 #include "content/renderer/media/local_media_stream_audio_source.h"
 #include "content/renderer/media/media_stream.h"
+#include "content/renderer/media/media_stream_audio_track.h"
 #include "content/renderer/media/media_stream_constraints_util.h"
 #include "content/renderer/media/media_stream_constraints_util_video_content.h"
 #include "content/renderer/media/media_stream_constraints_util_video_device.h"
@@ -32,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/media/webrtc_logging.h"
 #include "content/renderer/media/webrtc_uma_histograms.h"
 #include "content/renderer/render_thread_impl.h"
+#include "media/base/bind_to_current_loop.h"
 #include "media/capture/video_capture_types.h"
 #include "third_party/WebKit/public/platform/WebMediaConstraints.h"
 #include "third_party/WebKit/public/platform/WebMediaDeviceInfo.h"
@@ -723,8 +725,6 @@ void UserMediaClientImpl::OnAudioSourceStarted(
     if (result == MEDIA_DEVICE_OK)
       local_sources_.push_back((*it));
     pending_local_sources_.erase(it);
-
-    NotifyCurrentRequestInfoOfAudioSourceStarted(source, result, result_name);
     return;
   }
   NOTREACHED();
@@ -1312,10 +1312,18 @@ void UserMediaClientImpl::UserMediaRequestInfo::StartAudioTrack(
 
   sources_.push_back(track.source());
   bool connected = native_source->ConnectToTrack(track);
-  if (!is_pending) {
-    OnTrackStarted(
-        native_source,
-        connected ? MEDIA_DEVICE_OK : MEDIA_DEVICE_TRACK_START_FAILURE, "");
+  if (!is_pending && !connected) {
+    // It's a failure, and it's final.
+    OnTrackStarted(native_source, MEDIA_DEVICE_TRACK_START_FAILURE, "");
+  } else {
+    // The request may succeed later.
+    // The track will be marked as connected once the configuration has
+    // propagated.
+    MediaStreamAudioTrack::From(track)->SetFormatConfiguredCallback(
+        media::BindToCurrentLoop(base::Bind(
+            &UserMediaClientImpl::UserMediaRequestInfo::OnTrackStarted,
+            AsWeakPtr(), native_source, content::MEDIA_DEVICE_OK,
+            blink::WebString())));
   }
 }
 
@@ -1393,7 +1401,9 @@ void UserMediaClientImpl::UserMediaRequestInfo::OnAudioSourceStarted(
   // ignore the notification.
   auto found = std::find(sources_waiting_for_callback_.begin(),
                          sources_waiting_for_callback_.end(), source);
-  if (found != sources_waiting_for_callback_.end())
+  // If the start failed, inform the request here.
+  if (found != sources_waiting_for_callback_.end() &&
+      result != content::MEDIA_DEVICE_OK)
     OnTrackStarted(source, result, result_name);
 }
 
