e46e92830216a81dc083cf32b874128e13cd1d32
treib <treib@chromium.org>
2017-04-06 06:33:19 -0700

    Don't set a default value for the NTPContentSuggestionsEnabled policy if ChromeHome is enabled
    This is a temporary hack to allow dogfooding of ChromeHome.
    
    BUG=708191
    
    Review-Url: https://codereview.chromium.org/2806453002
    Cr-Commit-Position: refs/heads/master@{#462451}

--- chrome/browser/policy/cloud/user_cloud_policy_manager_factory.cc
+++ chrome/browser/policy/cloud/user_cloud_policy_manager_factory.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/macros.h"
+#include "base/memory/ptr_util.h"
 #include "base/sequenced_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/policy/schema_registry_service.h"
@@ -21,6 +22,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/common/cloud/user_cloud_policy_manager.h"
 #include "components/policy/core/common/cloud/user_cloud_policy_store.h"
 #include "content/public/browser/browser_context.h"
+
+#if defined(OS_ANDROID)
+#include "chrome/browser/android/chrome_feature_list.h"
+#endif
 
 namespace policy {
 
@@ -152,11 +157,16 @@ UserCloudPolicyManagerFactory::CreateManagerForOriginalBrowserContext(
   const base::FilePath component_policy_cache_dir =
       context->GetPath().Append(kPolicy).Append(kComponentsDir);
 
-  std::unique_ptr<UserCloudPolicyManager> manager;
-  manager.reset(new UserCloudPolicyManager(
+  auto manager = base::MakeUnique<UserCloudPolicyManager>(
       std::move(store), component_policy_cache_dir,
       std::unique_ptr<CloudExternalDataManager>(),
-      base::ThreadTaskRunnerHandle::Get(), file_task_runner, io_task_runner));
+      base::ThreadTaskRunnerHandle::Get(), file_task_runner, io_task_runner);
+
+#if defined(OS_ANDROID)
+  // TODO(treib): Remove this again. crbug.com/708191
+  if (base::FeatureList::IsEnabled(chrome::android::kChromeHomeFeature))
+    manager->SetChromeHomeEnabled();
+#endif
   manager->Init(
       SchemaRegistryServiceFactory::GetForContext(context)->registry());
   manager_wrappers_[context] = new ManagerWrapper(manager.get());
--- components/policy/core/common/cloud/user_cloud_policy_manager.h
+++ components/policy/core/common/cloud/user_cloud_policy_manager.h
@@ -49,6 +49,9 @@ class POLICY_EXPORT UserCloudPolicyManager : public CloudPolicyManager {
       const scoped_refptr<base::SequencedTaskRunner>& io_task_runner);
   ~UserCloudPolicyManager() override;
 
+  // TODO(treib): Remove this again. crbug.com/708191
+  void SetChromeHomeEnabled() { chrome_home_enabled_ = true; }
+
   // ConfigurationPolicyProvider overrides:
   void Shutdown() override;
 
@@ -93,6 +96,9 @@ class POLICY_EXPORT UserCloudPolicyManager : public CloudPolicyManager {
 
   // Manages external data referenced by policies.
   std::unique_ptr<CloudExternalDataManager> external_data_manager_;
+
+  // TODO(treib): Remove this again. crbug.com/708191
+  bool chrome_home_enabled_;
 
   DISALLOW_COPY_AND_ASSIGN(UserCloudPolicyManager);
 };
