af94cd95812b17c043d2b3feba43c0021b6f3e00
fdoray <fdoray@chromium.org>
2017-04-06 09:44:43 -0700

    Use ScopedTaskEnvironment instead of MessageLoop in remoting unit tests.
    ScopedTaskEnvironment allows usage of ThreadTaskRunnerHandle and
    base/task_scheduler/post_task.h within its scope. It should be
    instantiated in every unit test that uses either of these APIs
    (i.e. no test should instantiate a MessageLoop directly).
    
    Motivation for ScopedTaskEnvironment can be found in:
    https://docs.google.com/document/d/1QabRo8c7D9LsYY3cEcaPQbOCLo8Tu-6VLykYXyl3Pkk/edit
    
    BUG=708584
    R=gab@chromium.org
    TBR=sergeyu@chromium.org
    
    Review-Url: https://codereview.chromium.org/2800903002
    Cr-Commit-Position: refs/heads/master@{#462507}

--- remoting/host/gcd_rest_client_unittest.cc
+++ remoting/host/gcd_rest_client_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ptr_util.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/test/simple_test_clock.h"
 #include "base/values.h"
 #include "net/url_request/test_url_fetcher_factory.h"
@@ -51,7 +52,7 @@ class GcdRestClientTest : public testing::Test {
   GcdRestClient::Result last_result_ = GcdRestClient::OTHER_ERROR;
 
  private:
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
 };
 
 TEST_F(GcdRestClientTest, NetworkErrorGettingToken) {
