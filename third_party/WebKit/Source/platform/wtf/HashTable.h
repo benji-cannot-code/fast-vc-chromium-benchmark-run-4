c488bfea1ba2096d5dac1a61b587423c626aa625
pilgrim <pilgrim@chromium.org>
2017-04-06 12:44:35 -0700

    Migrate WTF::HashTable::add() to ::insert()
    As discussed on platform-architecture-dev [0], there is interest in
    normalizing the methods of WTF classes to better align with std classes.
    
    There are no functional changes.
    
    [0] https://groups.google.com/a/chromium.org/d/topic/platform-architecture-dev/I7jnz4p1h84/discussion
    
    BUG=662431
    
    Review-Url: https://codereview.chromium.org/2804953004
    Cr-Commit-Position: refs/heads/master@{#462589}

--- third_party/WebKit/Source/platform/wtf/HashTable.h
+++ third_party/WebKit/Source/platform/wtf/HashTable.h
@@ -730,18 +730,18 @@ class HashTable final
   void reserveCapacityForSize(unsigned size);
 
   template <typename IncomingValueType>
-  AddResult add(IncomingValueType&& value) {
-    return add<IdentityTranslatorType>(Extractor::extract(value),
-                                       std::forward<IncomingValueType>(value));
+  AddResult insert(IncomingValueType&& value) {
+    return insert<IdentityTranslatorType>(
+        Extractor::extract(value), std::forward<IncomingValueType>(value));
   }
 
-  // A special version of add() that finds the object by hashing and comparing
-  // with some other type, to avoid the cost of type conversion if the object
-  // is already in the table.
+  // A special version of insert() that finds the object by hashing and
+  // comparing with some other type, to avoid the cost of type conversion if the
+  // object is already in the table.
   template <typename HashTranslator, typename T, typename Extra>
-  AddResult add(T&& key, Extra&&);
+  AddResult insert(T&& key, Extra&&);
   template <typename HashTranslator, typename T, typename Extra>
-  AddResult addPassingHashCode(T&& key, Extra&&);
+  AddResult insertPassingHashCode(T&& key, Extra&&);
 
   iterator find(KeyPeekInType key) { return find<IdentityTranslatorType>(key); }
   const_iterator find(KeyPeekInType key) const {
@@ -1230,7 +1230,7 @@ typename HashTable<Key,
                    KeyTraits,
                    Allocator>::AddResult
 HashTable<Key, Value, Extractor, HashFunctions, Traits, KeyTraits, Allocator>::
-    add(T&& key, Extra&& extra) {
+    insert(T&& key, Extra&& extra) {
   DCHECK(!accessForbidden());
   DCHECK(Allocator::isAllocationAllowed());
   if (!m_table)
@@ -1326,7 +1326,7 @@ typename HashTable<Key,
                    KeyTraits,
                    Allocator>::AddResult
 HashTable<Key, Value, Extractor, HashFunctions, Traits, KeyTraits, Allocator>::
-    addPassingHashCode(T&& key, Extra&& extra) {
+    insertPassingHashCode(T&& key, Extra&& extra) {
   DCHECK(!accessForbidden());
   DCHECK(Allocator::isAllocationAllowed());
   if (!m_table)
@@ -1834,7 +1834,7 @@ HashTable<Key, Value, Extractor, HashFunctions, Traits, KeyTraits, Allocator>::
   // table.  It might be more efficient to copy the table slots, but it's not
   // clear that efficiency is needed.
   for (const auto& element : other)
-    add(element);
+    insert(element);
 }
 
 template <typename Key,
