8770835e05b507a0ce4bcb47faede99fa9f40ca9
reillyg <reillyg@chromium.org>
2017-04-06 08:49:49 -0700

    Fix ODR violation for features::kV8NoTurbo in component builds
    features::kV8NoTurbo is defined in //gin:gin_features. Since this is a
    global and this build target has dependencies from multiple other
    targets it must be a component instead of a source_set to avoid multiple
    definitions. As a component it must then also have its own _export.h
    header.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2799103004
    Cr-Commit-Position: refs/heads/master@{#462494}

--- gin/public/gin_features.h
+++ gin/public/gin_features.h
@@ -10,12 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define GIN_PUBLIC_GIN_FEATURES_H_
 
 #include "base/feature_list.h"
-#include "gin/gin_export.h"
+#include "gin/gin_features_export.h"
 
 namespace features {
 
-GIN_EXPORT extern const base::Feature kV8NoTurbo;
-GIN_EXPORT extern const base::Feature kV8IgnitionLowEnd;
+GIN_FEATURES_EXPORT extern const base::Feature kV8NoTurbo;
+GIN_FEATURES_EXPORT extern const base::Feature kV8IgnitionLowEnd;
 
 }  // namespace features
 
