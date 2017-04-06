3b3784695414f615bfed32837bc19f0f822577fd
bugsnash <bugsnash@chromium.org>
2017-04-06 00:01:30 -0700

    Implemented parseSingleValue in CSSPropertyAPICounterIncrement.
    Part of Project Ribbon, separating the parsing logic for CSS
    properties from the parser into an API.
    
    This patch
    - added parseSingleValue method to the CSSPropertyAPICounterIncrement
      API, with parsing logic from CSSPropertyParser
    - removed CSSPropertyCounterIncrement from the switch statement in
      CSSPropertyParser::parseSingleValue, calling the API instead using
      a CSSPropertyDescriptor
    - added parseSingleValue to counter-increment in CSSProperties.json5
      so it will be added to the generated files for the api
    
    Diff in generated CSSPropertyAPICounterIncrement.h:
    https://gist.github.com/BugsNash/ac16598cbe0677824308a64a52e33213/revisions
    Diff in generated CSSPropertyDescriptor.cpp:
    https://gist.github.com/BugsNash/7156559ba47f4ac929951959fe19cb93/revisions
    
    BUG=668012
    
    Review-Url: https://codereview.chromium.org/2783543002
    Cr-Commit-Position: refs/heads/master@{#462379}

--- third_party/WebKit/Source/core/css/properties/CSSPropertyAPICounterIncrement.cpp
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyAPICounterIncrement.cpp
@@ -5,4 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/css/properties/CSSPropertyAPICounterIncrement.h"
 
-namespace blink {}  // namespace blink
+#include "core/css/properties/CSSPropertyCounterUtils.h"
+
+namespace blink {
+
+const CSSValue* CSSPropertyAPICounterIncrement::parseSingleValue(
+    CSSParserTokenRange& range,
+    const CSSParserContext*) {
+  return CSSPropertyCounterUtils::consumeCounter(
+      range, CSSPropertyCounterUtils::kIncrementDefaultValue);
+}
+
+}  // namespace blink
