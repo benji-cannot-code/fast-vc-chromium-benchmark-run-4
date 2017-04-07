d8868e5b447ff779f2d2ea7d1780536cd2fcae21
stkhapugin <stkhapugin@chromium.org>
2017-04-07 03:26:48 -0700

    [ObjC ARC] Converts ios/chrome/browser/ui:test_support to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2802703002
    Cr-Commit-Position: refs/heads/master@{#462829}

--- ios/chrome/browser/ui/browser_view_controller_testing.mm
+++ ios/chrome/browser/ui/browser_view_controller_testing.mm
@@ -8,6 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/toolbar/web_toolbar_controller.h"
 #import "ios/chrome/browser/ui/toolbar/web_toolbar_controller_private.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 @interface BrowserViewController (Private)
 
 - (WebToolbarController*)toolbarController;
