bd9a46836d1441e380ffdb2a779ce8f05e36b7c3
lukasza <lukasza@chromium.org>
2017-04-07 16:55:52 -0700

    Adding a missing comma between "counterMaps" and "document".
    BUG=582312
    
    Review-Url: https://codereview.chromium.org/2800233002
    Cr-Commit-Position: refs/heads/master@{#463058}

--- tools/clang/rewrite_to_chrome_style/RewriteToChromeStyle.cpp
+++ tools/clang/rewrite_to_chrome_style/RewriteToChromeStyle.cpp
@@ -669,7 +669,7 @@ bool ShouldPrefixFunctionName(const std::string& old_method_name) {
                                               "compositorElementId",
                                               "constructionStack",
                                               "counterDirectives",
-                                              "counterMaps"
+                                              "counterMaps",
                                               "document",
                                               "dragOperation",
                                               "element",
