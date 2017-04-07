8e52fdeaa61a4026e085be9a4fae4a543f4649d3
Nghia Nguyen <NghiaNguyenBH@gmail.com>
2017-04-06 20:16:34 -0700

    Switch 'ignore_swarming_task_failure' to 'ignore_task_failure'
    This is the 2nd CL in our 3-sided patches to rename 'ignore_swarming_task_failure' to 'ignore_task_failure'
    
    BUG=709324
    
    Change-Id: I52efa1bbfd57585e5e28abc6876acbf6ca88af41
    Reviewed-on: https://chromium-review.googlesource.com/471046
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#463017}

--- tools/perf/core/perf_data_generator_unittest.py
+++ tools/perf/core/perf_data_generator_unittest.py
@@ -79,7 +79,7 @@ class PerfDataGeneratorTest(unittest.TestCase):
         'args': ['speedometer', '-v', '--upload-results',
                  '--output-format=chartjson', '--browser=release'],
         'swarming': {
-          'ignore_swarming_task_failure': False,
+          'ignore_task_failure': False,
           'dimension_sets': [{'os': 'SkyNet', 'id': 'T-850', 'pool': 'T-RIP'}],
           'hard_timeout': 7200,
           'can_use_on_swarming_builders': True,
@@ -101,7 +101,7 @@ class PerfDataGeneratorTest(unittest.TestCase):
                  '--output-format=chartjson', '--browser=reference',
                  '--output-trace-tag=_ref'],
         'swarming': {
-          'ignore_swarming_task_failure': True,
+          'ignore_task_failure': True,
           'dimension_sets': [{'os': 'SkyNet', 'id': 'T-850', 'pool': 'T-RIP'}],
           'hard_timeout': 7200,
           'can_use_on_swarming_builders': True,
