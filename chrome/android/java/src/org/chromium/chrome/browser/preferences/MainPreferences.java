3cf34a3ddf07b0bb958b9949afe93c1b480874db
bsazonov <bsazonov@chromium.org>
2017-04-07 07:56:43 -0700

    Clean up code in SignInPreference
    This CL cleans up and structures code in SignInPreference.java.
    1. Three possible states (signin disabled, not signed in & signed in) of
       SignInPreference are now structured and have dedicated setup functions.
    2. Added comment to clarify mViewEnabled magic.
    3. Removed hardcoded Android view ids from onBindView.
    4. Removed code to change enabled state.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2803753002
    Cr-Commit-Position: refs/heads/master@{#462860}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/MainPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/MainPreferences.java
@@ -183,7 +183,6 @@ public class MainPreferences extends PreferenceFragment
     private void setupSignInPref() {
         mSignInPreference = (SignInPreference) findPreference(PREF_SIGN_IN);
         mSignInPreference.registerForUpdates();
-        mSignInPreference.setEnabled(true);
     }
 
     private void clearSignInPref() {
