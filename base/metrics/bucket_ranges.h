7d2b49e684aec0cd8d09278e512667283ce6286f
bcwhite <bcwhite@chromium.org>
2017-04-07 10:02:01 -0700

    De-duplicate ranges information in persistent memory.
    By having BucketRanges keep a reference to where the data can be found
    in persistent memory, the existing de-dup of the BucketRanges object
    also serves to avoid duplicating the data in persistent memory.
    
    Simple testing shows allocated persistent histogram memory in the
    browser is reduced by about 22%.  This should be similar in all
    processes though somewhat less as the number of histograms held goes
    down.
    
    BUG=705342
    
    Review-Url: https://codereview.chromium.org/2794073002
    Cr-Commit-Position: refs/heads/master@{#462897}

--- base/metrics/bucket_ranges.h
+++ base/metrics/bucket_ranges.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <limits.h>
 
+#include "base/atomicops.h"
 #include "base/base_export.h"
 #include "base/macros.h"
 #include "base/metrics/histogram_base.h"
@@ -59,6 +60,17 @@ class BASE_EXPORT BucketRanges {
   // Return true iff |other| object has same ranges_ as |this| object's ranges_.
   bool Equals(const BucketRanges* other) const;
 
+  // Set and get a reference into persistent memory where this bucket data
+  // can be found (and re-used). These calls are internally atomic with no
+  // safety against overwriting an existing value since though it is wasteful
+  // to have multiple identical persistent records, it is still safe.
+  void set_persistent_reference(uint32_t ref) const {
+    subtle::NoBarrier_Store(&persistent_reference_, ref);
+  }
+  uint32_t persistent_reference() const {
+    return subtle::NoBarrier_Load(&persistent_reference_);
+  }
+
  private:
   // A monotonically increasing list of values which determine which bucket to
   // put a sample into.  For each index, show the smallest sample that can be
@@ -71,6 +83,12 @@ class BASE_EXPORT BucketRanges {
   // TODO(kaiwang): Consider change this to uint64_t. Because we see a lot of
   // noise on UMA dashboard.
   uint32_t checksum_;
+
+  // A reference into a global PersistentMemoryAllocator where the ranges
+  // information is stored. This allows for the record to be created once and
+  // re-used simply by having all histograms with the same ranges use the
+  // same reference.
+  mutable subtle::Atomic32 persistent_reference_ = 0;
 
   DISALLOW_COPY_AND_ASSIGN(BucketRanges);
 };
