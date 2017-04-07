2b2f3f693b9fc92d71475dc431c3b8d41f4a7aa1
adithyas <adithyas@chromium.org>
2017-04-07 07:39:44 -0700

    Change Modulator functions to use ScriptState instead of LocalFrame
    Follow up to http://crrev.com/2795593006, changes Modulator::from/setModulator/clearModulator to take ScriptState* as a parameter instead of LocalFrame*
    
    BUG=682322
    
    Review-Url: https://codereview.chromium.org/2803843002
    Cr-Commit-Position: refs/heads/master@{#462856}

--- third_party/WebKit/Source/bindings/core/v8/ScriptModuleTest.cpp
+++ third_party/WebKit/Source/bindings/core/v8/ScriptModuleTest.cpp
@@ -148,7 +148,7 @@ TEST(ScriptModuleTest, instantiateNoDeps) {
   auto modulator = new ScriptModuleTestModulator();
   auto resolver = modulator->testScriptModuleResolver();
 
-  Modulator::setModulator(&scope.frame(), modulator);
+  Modulator::setModulator(scope.getScriptState(), modulator);
 
   ScriptModule module = ScriptModule::compile(
       scope.isolate(), "export const a = 42;", "foo.js", SharableCrossOrigin);
@@ -165,7 +165,7 @@ TEST(ScriptModuleTest, instantiateWithDeps) {
   auto modulator = new ScriptModuleTestModulator();
   auto resolver = modulator->testScriptModuleResolver();
 
-  Modulator::setModulator(&scope.frame(), modulator);
+  Modulator::setModulator(scope.getScriptState(), modulator);
 
   ScriptModule moduleA = ScriptModule::compile(
       scope.isolate(), "export const a = 'a';", "foo.js", SharableCrossOrigin);
--- third_party/WebKit/Source/core/dom/Modulator.h
+++ third_party/WebKit/Source/core/dom/Modulator.h
@@ -16,12 +16,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class LocalFrame;
 class ModuleScript;
 class ModuleScriptFetchRequest;
 class ModuleScriptLoaderClient;
 class ScriptModule;
 class ScriptModuleResolver;
+class ScriptState;
 class SecurityOrigin;
 class WebTaskRunner;
 
@@ -47,12 +47,11 @@ class CORE_EXPORT Modulator : public GarbageCollectedFinalized<Modulator>,
   USING_GARBAGE_COLLECTED_MIXIN(Modulator);
 
  public:
-  static Modulator* from(LocalFrame*);
-  static Modulator* from(V8PerContextData*);
+  static Modulator* from(ScriptState*);
   virtual ~Modulator();
 
-  static void setModulator(LocalFrame*, Modulator*);
-  static void clearModulator(LocalFrame*);
+  static void setModulator(ScriptState*, Modulator*);
+  static void clearModulator(ScriptState*);
 
   DEFINE_INLINE_VIRTUAL_TRACE() {}
 
