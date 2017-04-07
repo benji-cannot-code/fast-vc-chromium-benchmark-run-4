824bf4e1025bcbaeee0f95751208a93f40ecb619
thestig <thestig@chromium.org>
2017-04-07 14:13:03 -0700

    Fix static initializers in RlzValueStoreChromeOS.
    BUG=537099
    
    Review-Url: https://codereview.chromium.org/2800153002
    Cr-Commit-Position: refs/heads/master@{#463004}

--- rlz/chromeos/lib/rlz_value_store_chromeos.h
+++ rlz/chromeos/lib/rlz_value_store_chromeos.h
@@ -14,11 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/macros.h"
 #include "base/threading/non_thread_safe.h"
-#include "base/values.h"
 #include "rlz/lib/rlz_value_store.h"
 
 namespace base {
-class SequencedTaskRunner;
+class DictionaryValue;
 class Value;
 }
 
@@ -28,12 +27,8 @@ namespace rlz_lib {
 class RlzValueStoreChromeOS : public RlzValueStore,
                               public base::NonThreadSafe {
  public:
-  // // Sets the task runner that will be used by ImportantFileWriter for write
-  // // operations.
-  // static void SetFileTaskRunner(base::SequencedTaskRunner* file_task_runner);
-
   // Creates new instance and synchronously reads data from file.
-  RlzValueStoreChromeOS(const base::FilePath& store_path);
+  explicit RlzValueStoreChromeOS(const base::FilePath& store_path);
   ~RlzValueStoreChromeOS() override;
 
   // RlzValueStore overrides:
