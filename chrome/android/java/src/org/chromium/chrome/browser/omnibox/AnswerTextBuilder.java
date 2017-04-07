54013450266ed17c8cf0067d30753ba85a7414aa
jdonnelly <jdonnelly@chromium.org>
2017-04-07 07:41:17 -0700

    Make the answers text color more consistent.
    All answer text (other than the suggestion text and positive/negative
    text) is now the de-emphasizing gray.
    
    BUG=709101
    
    Review-Url: https://codereview.chromium.org/2804143002
    Cr-Commit-Position: refs/heads/master@{#462857}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/AnswerTextBuilder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/AnswerTextBuilder.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.omnibox;
 
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.text.Html;
 import android.text.Spannable;
@@ -65,11 +64,10 @@ class AnswerTextBuilder {
     private static final int ANSWERS_DESCRIPTION_TEXT_POSITIVE_COLOR = 0xFF0B8043;
     private static final int ANSWERS_SUGGESTION_TEXT_COLOR =
             SuggestionView.TITLE_COLOR_STANDARD_FONT_DARK;
-    private static final int ANSWERS_PERSONALIZED_SUGGESTION_TEXT_COLOR = Color.BLACK;
-    private static final int ANSWERS_ANSWER_TEXT_MEDIUM_COLOR =
+    private static final int ANSWERS_PERSONALIZED_SUGGESTION_TEXT_COLOR =
             SuggestionView.TITLE_COLOR_STANDARD_FONT_DARK;
-    private static final int ANSWERS_ANSWER_TEXT_LARGE_COLOR =
-            SuggestionView.TITLE_COLOR_STANDARD_FONT_DARK;
+    private static final int ANSWERS_ANSWER_TEXT_MEDIUM_COLOR = 0xFF8A8A8A;
+    private static final int ANSWERS_ANSWER_TEXT_LARGE_COLOR = 0xFF8A8A8A;
     private static final int ANSWERS_SECONDARY_TEXT_SMALL_COLOR = 0xFF8A8A8A;
     private static final int ANSWERS_SECONDARY_TEXT_MEDIUM_COLOR = 0xFF8A8A8A;
 
