937223236170c8c28943a5cd31af7235ac21cca5
dbeam <dbeam@chromium.org>
2017-04-06 17:35:00 -0700

    MD Downloads: improve early loading shim
    Copy some colors, actually simulate the toolbar.
    Generally make things super fast.
    
    R=esprehn@chromium.org
    BUG=none
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2803653003
    Cr-Commit-Position: refs/heads/master@{#462710}

--- chrome/browser/resources/md_downloads/manager.js
+++ chrome/browser/resources/md_downloads/manager.js
@@ -41,6 +41,7 @@ cr.define('downloads', function() {
     },
 
     hostAttributes: {
+      // TODO(dbeam): this should use a class instead.
       loading: true,
     },
 
@@ -52,6 +53,10 @@ cr.define('downloads', function() {
     observers: [
       'itemsChanged_(items_.*)',
     ],
+
+    attached: function() {
+      document.documentElement.classList.remove('loading');
+    },
 
     /** @private {!PromiseResolver} */
     loaded_: new PromiseResolver,
