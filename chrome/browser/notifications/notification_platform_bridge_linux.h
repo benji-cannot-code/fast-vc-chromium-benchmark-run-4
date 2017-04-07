fddbcaaad3ee73bf72b40603b8c6fae17eadc1b8
thomasanderson <thomasanderson@google.com>
2017-04-07 16:24:08 -0700

    Linux native notifications:  Handle clicks and closes
    This CL adds the initial wiring to GLib and
    NativeNotificationDisplayService to get actions and closes working.
    
    BUG=676220
    R=thestig@chromium.org,peter@chromium.org
    
    Review-Url: https://codereview.chromium.org/2805543005
    Cr-Commit-Position: refs/heads/master@{#463045}

--- chrome/browser/notifications/notification_platform_bridge_linux.h
+++ chrome/browser/notifications/notification_platform_bridge_linux.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "chrome/browser/notifications/notification_platform_bridge.h"
+#include "ui/base/glib/glib_signal.h"
 #include "ui/base/glib/scoped_gobject.h"
 
 class NotificationPlatformBridgeLinux : public NotificationPlatformBridge {
@@ -41,6 +42,9 @@ class NotificationPlatformBridgeLinux : public NotificationPlatformBridge {
 
   ScopedGObject<GDBusProxy> notification_proxy_;
 
+  // Used to disconnect from "g-signal" during destruction.
+  gulong proxy_signal_handler_ = 0;
+
   // A std::set<std::unique_ptr<T>> doesn't work well because
   // eg. std::set::erase(T) would require a std::unique_ptr<T>
   // argument, so the data would get double-destructed.
@@ -60,8 +64,25 @@ class NotificationPlatformBridgeLinux : public NotificationPlatformBridge {
   // Makes the "CloseNotification" call to D-Bus.
   void CloseNow(uint32_t dbus_id);
 
+  void ForwardNotificationOperation(uint32_t dbus_id,
+                                    NotificationCommon::Operation operation,
+                                    int action_index);
+
+  // GSignalReceiver: The function that GLib calls into for
+  // ActionInvoked and NotificationClosed signals.
+  CHROMEG_CALLBACK_3(NotificationPlatformBridgeLinux,
+                     void,
+                     GSignalReceiver,
+                     GDBusProxy*,
+                     const char*,
+                     const char*,
+                     GVariant*);
+
   NotificationData* FindNotificationData(const std::string& notification_id,
-                                         const std::string& profile_id);
+                                         const std::string& profile_id,
+                                         bool is_incognito);
+
+  NotificationData* FindNotificationData(uint32_t dbus_id);
 
   DISALLOW_COPY_AND_ASSIGN(NotificationPlatformBridgeLinux);
 };
