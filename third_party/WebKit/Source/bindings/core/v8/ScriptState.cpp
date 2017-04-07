9991d7e05c9d9400a07a40d8f2b7f623039ab582
adithyas <adithyas@chromium.org>
2017-04-07 14:50:09 -0700

    Remove ScriptState::setExecutionContext
    The function doesn't appear to be used anywhere, and adds an unnecessary dependency to ExecutionContext in ScriptState.
    
    BUG=682322
    
    Review-Url: https://codereview.chromium.org/2800173002
    Cr-Commit-Position: refs/heads/master@{#463013}

--- third_party/WebKit/Source/bindings/core/v8/ScriptState.cpp
+++ third_party/WebKit/Source/bindings/core/v8/ScriptState.cpp
@@ -71,8 +71,4 @@ ExecutionContext* ScriptState::getExecutionContext() const {
   return toExecutionContext(context());
 }
 
-void ScriptState::setExecutionContext(ExecutionContext*) {
-  ASSERT_NOT_REACHED();
-}
-
 }  // namespace blink
