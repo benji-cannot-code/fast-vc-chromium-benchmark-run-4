729439f994304adca5188bae1152b8a0b3ebbf79
mfomitchev <mfomitchev@chromium.org>
2017-04-07 10:13:01 -0700

    Re-adding and fixing StructTraitsTest for events and latency.
    StructTraitsTest for events and latency were removed from the build by
    accident in https://codereview.chromium.org/2783973002.
    
    BUG=686865
    
    Review-Url: https://codereview.chromium.org/2802923002
    Cr-Commit-Position: refs/heads/master@{#462902}

--- ui/events/mojo/struct_traits_unittest.cc
+++ ui/events/mojo/struct_traits_unittest.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/keycodes/dom/dom_code.h"
 #include "ui/events/mojo/event.mojom.h"
 #include "ui/events/mojo/event_struct_traits.h"
+#include "ui/events/mojo/traits_test_service.mojom.h"
+#include "ui/latency/mojo/latency_info_struct_traits.h"
 
 namespace ui {
 
--- ui/latency/mojo/struct_traits_unittest.cc
+++ ui/latency/mojo/struct_traits_unittest.cc
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "ui/events/mojo/latency_info_struct_traits.h"
-#include "ui/events/mojo/traits_test_service.mojom.h"
+#include "ui/latency/mojo/latency_info_struct_traits.h"
+#include "ui/latency/mojo/traits_test_service.mojom.h"
 
 namespace ui {
 
