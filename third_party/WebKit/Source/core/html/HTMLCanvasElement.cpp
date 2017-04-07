140f122bdb0ffd0cde405e5083797f55f0b7c5fa
kbr <kbr@chromium.org>
2017-04-06 22:54:58 -0700

    Revert of Do compositing update when switching from software to accelerated canvas (patchset #1 id:1 of https://codereview.chromium.org/2799033002/ )
    Reason for revert:
    Suspect this caused linux_optional_gpu_tests_rel tryserver to break in http://crbug.com/709342 .
    
    Original issue's description:
    > Do compositing update when switching from software to accelerated canvas
    >
    > Also, re-enables switching from sw to accelerated canvas.
    >
    > BUG=704571
    >
    > Review-Url: https://codereview.chromium.org/2799033002
    > Cr-Commit-Position: refs/heads/master@{#462706}
    > Committed: https://chromium.googlesource.com/chromium/src/+/2f0fa56eb8fc2eee27d437e7b6c8038ba5a52d87
    
    TBR=junov@chromium.org,fserb@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=704571
    
    Review-Url: https://codereview.chromium.org/2799133004
    Cr-Commit-Position: refs/heads/master@{#462783}

--- third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
@@ -1247,7 +1247,6 @@ void HTMLCanvasElement::willDrawImageTo2DContext(CanvasImageSource* source) {
         createAcceleratedImageBufferSurface(opacityMode, &msaaSampleCount);
     if (surface) {
       buffer()->setSurface(std::move(surface));
-      setNeedsCompositingUpdate();
     }
   }
 }
--- third_party/WebKit/Source/platform/graphics/ExpensiveCanvasHeuristicParameters.h
+++ third_party/WebKit/Source/platform/graphics/ExpensiveCanvasHeuristicParameters.h
@@ -99,7 +99,7 @@ enum {
   // acceleration on the destination first. If that does not succeed,
   // we disable acceleration on the source canvas. Either way, future
   // readbacks are prevented.
-  EnableAccelerationToAvoidReadbacks = 1,
+  EnableAccelerationToAvoidReadbacks = 0,
 
 };  // enum
 
