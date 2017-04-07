55dfc780bc1b6f81cb4dbf420de14f2cbcdd9fed
jkrcal <jkrcal@chromium.org>
2017-04-07 09:38:51 -0700

    [ImageFetcher] Return http response headers
    For LargeIconService, we need to access Content-Location header for
    images fetched by ImageFetcher. This CL reveals HttpResponseHeaders* in
    image_fetcher::RequestMetadata.
    
    BUG=644102
    
    Review-Url: https://codereview.chromium.org/2793983002
    Cr-Commit-Position: refs/heads/master@{#462891}

--- components/image_fetcher/core/request_metadata.cc
+++ components/image_fetcher/core/request_metadata.cc
@@ -5,15 +5,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/image_fetcher/core/request_metadata.h"
 
+#include "net/http/http_response_headers.h"
+
 namespace image_fetcher {
 
+namespace {
+
+bool HttpHeadersEqual(const net::HttpResponseHeaders* lhs,
+                      const net::HttpResponseHeaders* rhs) {
+  if (!lhs && !rhs) {
+    return true;  // Equal if both nullptr.
+  }
+  if (!lhs || !rhs) {
+    return false;  // We cannot compare the content if one of them is nullptr.
+  }
+  return lhs->raw_headers() == rhs->raw_headers();
+}
+
+}  // namespace
+
 RequestMetadata::RequestMetadata()
-    : http_response_code(RESPONSE_CODE_INVALID), from_http_cache(false) {}
+    : http_response_code(RESPONSE_CODE_INVALID),
+      from_http_cache(false),
+      http_response_headers(nullptr) {}
 
 bool operator==(const RequestMetadata& lhs, const RequestMetadata& rhs) {
   return lhs.mime_type == rhs.mime_type &&
          lhs.http_response_code == rhs.http_response_code &&
-         lhs.from_http_cache == rhs.from_http_cache;
+         lhs.from_http_cache == rhs.from_http_cache &&
+         HttpHeadersEqual(lhs.http_response_headers, rhs.http_response_headers);
 }
 
 bool operator!=(const RequestMetadata& lhs, const RequestMetadata& rhs) {
--- components/image_fetcher/core/request_metadata_unittest.cc
+++ components/image_fetcher/core/request_metadata_unittest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/image_fetcher/core/request_metadata.h"
 
+#include "base/memory/ref_counted.h"
+#include "net/http/http_response_headers.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace image_fetcher {
