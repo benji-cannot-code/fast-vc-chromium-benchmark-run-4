ebe88fd98128178256f6f4f4a3a63b377e75a0a6
patricialor <patricialor@chromium.org>
2017-04-06 22:30:27 -0700

    Views/Harmony: Remove references to layout constants in c/b/ui/views/sync.
    Remove unnecessary includes for ui/views/layout/layout_constants.h and do some
    refactoring to remove references to layout constants in
    ProfileSigninConfirmationDialogViews under chrome/browser/ui/views/sync/*.
    
    See
    https://drive.google.com/file/d/0BzEa5HU1aAqBOXBDazdRT245Wk0/view?usp=sharing
    for before/after screenshots of ProfileSigninConfirmationDialogViews.
    
    BUG=691897
    
    Review-Url: https://codereview.chromium.org/2799163002
    Cr-Commit-Position: refs/heads/master@{#462771}

--- chrome/browser/ui/views/sync/bubble_sync_promo_view.cc
+++ chrome/browser/ui/views/sync/bubble_sync_promo_view.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/font.h"
 #include "ui/views/controls/styled_label.h"
 #include "ui/views/layout/fill_layout.h"
-#include "ui/views/layout/layout_constants.h"
 
 namespace {
 
