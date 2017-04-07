6a52f77b0c269d2b3ad59b77c9cdb4a4b27284fa
raphael.kubo.da.costa <raphael.kubo.da.costa@intel.com>
2017-04-07 09:36:54 -0700

    bindings: Throw a type error if converting an interface or callback function fails.
    The previous implementation of nativeValue() for IDL interfaces and callback
    functions simply called implTypeWithCheck() (or create() for callback
    functions) and returned its value. This meant it was not possible for
    callers to easily distinguish a nullptr returned here (meaning the
    conversion failed) from nullptr being returned by a nativeValue() call for
    another type (which can just be a nullptr).
    
    BUG=690428
    R=bashi@chromium.org,haraken@chromium.org,yukishiino@chromium.org
    
    Review-Url: https://codereview.chromium.org/2802223002
    Cr-Commit-Position: refs/heads/master@{#462890}

--- third_party/WebKit/Source/bindings/core/v8/NativeValueTraitsImplTest.cpp
+++ third_party/WebKit/Source/bindings/core/v8/NativeValueTraitsImplTest.cpp
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include "bindings/core/v8/ExceptionState.h"
 #include "bindings/core/v8/IDLTypes.h"
+#include "bindings/core/v8/TestSequenceCallback.h"
 #include "bindings/core/v8/ToV8.h"
 #include "bindings/core/v8/V8BindingForTesting.h"
+#include "bindings/core/v8/V8Internals.h"
 #include "platform/wtf/Vector.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -20,6 +22,29 @@ namespace {
 template <typename T>
 v8::Local<v8::Value> ToV8(V8TestingScope* scope, T value) {
   return blink::ToV8(value, scope->context()->Global(), scope->isolate());
+}
+
+TEST(NativeValueTraitsImplTest, IDLInterface) {
+  V8TestingScope scope;
+  {
+    DummyExceptionStateForTesting exceptionState;
+    Internals* internals = NativeValueTraits<Internals>::nativeValue(
+        scope.isolate(), v8::Number::New(scope.isolate(), 42), exceptionState);
+    EXPECT_TRUE(exceptionState.hadException());
+    EXPECT_EQ("Unable to convert value to Internals.",
+              exceptionState.message());
+    EXPECT_EQ(nullptr, internals);
+  }
+  {
+    DummyExceptionStateForTesting exceptionState;
+    TestSequenceCallback* callbackFunction =
+        NativeValueTraits<TestSequenceCallback>::nativeValue(
+            scope.isolate(), v8::Undefined(scope.isolate()), exceptionState);
+    EXPECT_TRUE(exceptionState.hadException());
+    EXPECT_EQ("Unable to convert value to TestSequenceCallback.",
+              exceptionState.message());
+    EXPECT_EQ(nullptr, callbackFunction);
+  }
 }
 
 void ThrowException(v8::Local<v8::Name>,
