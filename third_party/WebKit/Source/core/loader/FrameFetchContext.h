dde901dac13f46eaf633289c0335fab5082f3020
kinuko <kinuko@chromium.org>
2017-04-07 03:24:52 -0700

    Remove FrameFetchContext::dispatchDidReceiveResponseInternal declaration
    The implentation was removed sometime ago, but I forgot to remove
    the declaration.
    
    BUG=695279
    R=horo@chromium.org,japhet@chromium.org
    
    Review-Url: https://codereview.chromium.org/2797423004
    Cr-Commit-Position: refs/heads/master@{#462828}

--- third_party/WebKit/Source/core/loader/FrameFetchContext.h
+++ third_party/WebKit/Source/core/loader/FrameFetchContext.h
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/CoreExport.h"
 #include "core/frame/csp/ContentSecurityPolicy.h"
-#include "core/loader/LinkLoader.h"
 #include "platform/heap/Handle.h"
 #include "platform/loader/fetch/FetchContext.h"
 #include "platform/loader/fetch/FetchRequest.h"
@@ -196,13 +195,6 @@ class CORE_EXPORT FrameFetchContext final : public FetchContext {
       SecurityViolationReportingPolicy,
       FetchRequest::OriginRestriction,
       ResourceRequest::RedirectStatus) const;
-
-  void dispatchDidReceiveResponseInternal(unsigned long identifier,
-                                          const ResourceResponse&,
-                                          WebURLRequest::FrameType,
-                                          WebURLRequest::RequestContext,
-                                          Resource*,
-                                          LinkLoader::CanLoadResources);
 
   void addCSPHeaderIfNecessary(Resource::Type, ResourceRequest&);
 
