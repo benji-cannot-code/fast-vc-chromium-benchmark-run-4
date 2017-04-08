03391057adc360a9a0568211b3040273954f594c
dgozman <dgozman@chromium.org>
2017-04-07 17:53:40 -0700

    [DevTools] Migrate ConsoleMessage from Target to RuntimeModel
    Drive-by: stop passing message to every single method of ConsoleViewMessage.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2809433002
    Cr-Commit-Position: refs/heads/master@{#463083}

--- third_party/WebKit/Source/devtools/front_end/sdk/CPUProfilerModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/CPUProfilerModel.js
@@ -46,6 +46,13 @@ SDK.CPUProfilerModel = class extends SDK.SDKModel {
   }
 
   /**
+   * @return {!SDK.RuntimeModel}
+   */
+  runtimeModel() {
+    return this._debuggerModel.runtimeModel();
+  }
+
+  /**
    * @override
    * @param {string} id
    * @param {!Protocol.Debugger.Location} scriptLocation
