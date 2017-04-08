d261b0b8456a37356ec5c2f93849b3bc740e92c5
derat <derat@chromium.org>
2017-04-07 17:44:58 -0700

    chromeos: Reintroduce proxy resolution service error test.
    Reintroduce an error-reporting test for
    ProxyResolutionServiceProvider. This is a bit trickier to do
    when using a full net::ProxyService.
    
    BUG=706665
    
    Review-Url: https://codereview.chromium.org/2798223002
    Cr-Commit-Position: refs/heads/master@{#463078}

--- chromeos/dbus/services/proxy_resolution_service_provider_unittest.cc
+++ chromeos/dbus/services/proxy_resolution_service_provider_unittest.cc
@@ -62,6 +62,7 @@ class TestProxyResolver : public net::ProxyResolver {
   net::ProxyInfo* mutable_proxy_info() { return &proxy_info_; }
 
   void set_async(bool async) { async_ = async; }
+  void set_result(net::Error result) { result_ = result; }
 
   // net::ProxyResolver:
   int GetProxyForURL(const GURL& url,
@@ -72,10 +73,10 @@ class TestProxyResolver : public net::ProxyResolver {
     CHECK(network_task_runner_->BelongsToCurrentThread());
     results->Use(proxy_info_);
     if (!async_)
-      return net::OK;
+      return result_;
 
     base::ThreadTaskRunnerHandle::Get()->PostTask(
-        FROM_HERE, base::Bind(callback, net::OK));
+        FROM_HERE, base::Bind(callback, result_));
     return net::ERR_IO_PENDING;
   }
 
@@ -87,6 +88,9 @@ class TestProxyResolver : public net::ProxyResolver {
 
   // If true, GetProxyForURL() replies asynchronously rather than synchronously.
   bool async_ = false;
+
+  // Final result for GetProxyForURL() to return.
+  net::Error result_ = net::OK;
 
   DISALLOW_COPY_AND_ASSIGN(TestProxyResolver);
 };
@@ -151,9 +155,12 @@ class TestDelegate : public ProxyResolutionServiceProvider::Delegate {
   void CreateProxyServiceOnNetworkThread() {
     CHECK(context_getter_->GetNetworkTaskRunner()->BelongsToCurrentThread());
 
-    // The config's autodetect property needs to be set in order for
-    // net::ProxyService to send requests to our resolver.
-    net::ProxyConfig config = net::ProxyConfig::CreateAutoDetect();
+    // Setting a mandatory PAC URL makes |proxy_service_| query
+    // |proxy_resolver_| and also lets us generate
+    // net::ERR_MANDATORY_PROXY_CONFIGURATION_FAILED errors.
+    net::ProxyConfig config;
+    config.set_pac_url(GURL("http://www.example.com"));
+    config.set_pac_mandatory(true);
     proxy_service_ = base::MakeUnique<net::ProxyService>(
         base::MakeUnique<net::ProxyConfigServiceFixed>(config),
         base::MakeUnique<TestProxyResolverFactory>(proxy_resolver_),
@@ -378,6 +385,28 @@ TEST_F(ProxyResolutionServiceProviderTest, ResponseAsync) {
   EXPECT_TRUE(reader.PopString(&error));
   EXPECT_EQ(proxy_resolver_->proxy_info().ToPacString(), proxy_info);
   EXPECT_EQ("", error);
+
+  // No signal should've been emitted.
+  EXPECT_FALSE(signal);
+}
+
+TEST_F(ProxyResolutionServiceProviderTest, ResponseError) {
+  const char kSourceURL[] = "http://www.gmail.com/";
+  proxy_resolver_->set_result(net::ERR_FAILED);
+  std::unique_ptr<dbus::Response> response;
+  std::unique_ptr<SignalInfo> signal;
+  CallMethod(kSourceURL, false /* request_signal */, &response, &signal);
+
+  // The response should contain empty proxy info and a "mandatory proxy config
+  // failed" error (which the error from the resolver will be mapped to).
+  ASSERT_TRUE(response);
+  dbus::MessageReader reader(response.get());
+  std::string proxy_info, error;
+  EXPECT_TRUE(reader.PopString(&proxy_info));
+  EXPECT_TRUE(reader.PopString(&error));
+  EXPECT_EQ("DIRECT", proxy_info);
+  EXPECT_EQ(net::ErrorToString(net::ERR_MANDATORY_PROXY_CONFIGURATION_FAILED),
+            error);
 
   // No signal should've been emitted.
   EXPECT_FALSE(signal);
