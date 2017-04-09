dc63955931f9dfa09f7bf6e23a8945a2c0c807cc
Daniel Cheng <dcheng@chromium.org>
2017-04-09 12:37:27 -0700

    Fix sanitizer builds after the Blink rename.
    The ASAN configuration was accidentally omitted from the automated
    tooling runs, so identifiers only used in sanitizer builds were not
    renamed.
    
    BUG=578344
    TBR=haraken@chromium.org
    
    Change-Id: Icebccc698ea70270825f97522d0b582d1748c5d1
    Reviewed-on: https://chromium-review.googlesource.com/472532
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#463141}

--- third_party/WebKit/Source/platform/heap/ThreadState.h
+++ third_party/WebKit/Source/platform/heap/ThreadState.h
@@ -584,8 +584,8 @@ class PLATFORM_EXPORT ThreadState {
   void EagerSweep();
 
 #if defined(ADDRESS_SANITIZER)
-  void poisonEagerArena();
-  void poisonAllHeaps();
+  void PoisonEagerArena();
+  void PoisonAllHeaps();
 #endif
 
   void RemoveAllPages();
@@ -651,7 +651,7 @@ class PLATFORM_EXPORT ThreadState {
   void (*perform_cleanup_)(v8::Isolate*);
 
 #if defined(ADDRESS_SANITIZER)
-  void* m_asanFakeStack;
+  void* asan_fake_stack_;
 #endif
 
   // PersistentNodes that are stored in static references;
