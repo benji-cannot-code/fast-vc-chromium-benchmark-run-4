27ef8e82e433561f2e65306a2e787fc866c6b25a
irisu <irisu@chromium.org>
2017-04-10 04:28:44 -0700

    Add user-agent flag to headless
    BUG=604324
    
    Review-Url: https://codereview.chromium.org/2805793003
    Cr-Commit-Position: refs/heads/master@{#463221}

--- headless/app/headless_shell.cc
+++ headless/app/headless_shell.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/io_buffer.h"
 #include "net/base/ip_address.h"
 #include "net/base/net_errors.h"
+#include "net/http/http_util.h"
 #include "ui/gfx/geometry/size.h"
 
 namespace headless {
@@ -557,6 +558,12 @@ int HeadlessShellMain(int argc, const char** argv) {
   if (command_line.HasSwitch(switches::kHideScrollbars)) {
     builder.SetOverrideWebPreferencesCallback(base::Bind([](
         WebPreferences* preferences) { preferences->hide_scrollbars = true; }));
+  }
+
+  if (command_line.HasSwitch(switches::kUserAgent)) {
+    std::string ua = command_line.GetSwitchValueASCII(switches::kUserAgent);
+    if (net::HttpUtil::IsValidHeaderValue(ua))
+      builder.SetUserAgent(ua);
   }
 
   return HeadlessBrowserMain(
