18e2414f264d9476e31df8aea99cf409a5cbc8c9
weidongg <weidongg@chromium.org>
2017-04-10 10:48:29 -0700

    Add an update warning for downloading over mobile data
    https://screenshot.googleplex.com/sHy71kU48TQ
    
    BUG=691108
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2795603002
    Cr-Commit-Position: refs/heads/master@{#463314}

---
+++ chrome/browser/resources/settings/about_page/update_warning_dialog.js
@@ -0,0 +1,46 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview 'settings-update-warning-dialog' is a component warning the
+ * user about update over mobile data. By clicking 'Continue', the user
+ * agrees to download update using mobile data.
+ */
+Polymer({
+  is: 'settings-update-warning-dialog',
+
+  behaviors: [I18nBehavior],
+
+  /** @override */
+  ready: function() {
+    this.browserProxy_ = settings.AboutPageBrowserProxyImpl.getInstance();
+  },
+
+  /** @override */
+  attached: function() {
+    this.$.dialog.showModal();
+  },
+
+  /** @private */
+  onCancelTap_: function() {
+    // TODO(weidongg): implement the real behaviors here.
+    this.$.dialog.close();
+  },
+
+  /** @private */
+  onContinueTap_: function() {
+    // TODO(weidongg): implement the real behaviors here.
+    this.$.dialog.close();
+  },
+
+  /**
+   * @param {string} updateSizeMb Size of the update in megabytes.
+   * @private
+   */
+  setUpdateWarningMessage: function(updateSizeMb) {
+     this.$$("#update-warning-message").innerHTML =
+         this.i18n("aboutUpdateWarningMessage", updateSizeMb);
+  },
+});
