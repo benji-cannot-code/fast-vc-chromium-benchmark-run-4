8e1c323421b89e66979c9c99bde3b22583f92301
nicholss <nicholss@chromium.org>
2017-04-10 15:45:34 -0700

    Fixing type of returned user info object.
    BUG=652781
    
    Review-Url: https://codereview.chromium.org/2808173004
    Cr-Commit-Position: refs/heads/master@{#463427}

--- remoting/client/ios/domain/user_info.h
+++ remoting/client/ios/domain/user_info.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, copy) NSString* userFullName;
 @property(nonatomic, copy) NSString* userEmail;
 
-+ (User*)parseListFromJSON:(NSMutableData*)data;
++ (UserInfo*)parseListFromJSON:(NSMutableData*)data;
 
 - (NSComparisonResult)compare:(UserInfo*)user;
 
