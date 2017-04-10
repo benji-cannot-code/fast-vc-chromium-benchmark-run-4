34adefae989b17e775d459ba507b1024edcf4a57
raphael.kubo.da.costa <raphael.kubo.da.costa@intel.com>
2017-04-10 08:29:02 -0700

    bindings: Call member.includes_for_type() for container types too.
    Follow-up to commit 8552274ebe ("bindings: Correctly expand all class/header
    dependencies in unions"), which caused member.includes_for_type() not to be
    called when a nested union member is processed.
    
    We now call it in all cases, even for container types such as records,
    sequences and unions, otherwise we risk skipping includes that come from the
    type (e.g. IDLRecord) itself. This does not happen at the moment, but may in
    the future, such as when we start converting sequences via IDLSequence and
    NativeValueTraits::NativeValue().
    
    BUG=690428
    R=bashi@chromium.org,haraken@chromium.org,yukishiino@chromium.org
    
    Review-Url: https://codereview.chromium.org/2806343002
    Cr-Commit-Position: refs/heads/master@{#463268}

--- third_party/WebKit/Source/bindings/tests/results/core/NodeOrLongSequenceOrEventOrXMLHttpRequestOrStringOrStringByteStringOrNodeListRecord.cpp
+++ third_party/WebKit/Source/bindings/tests/results/core/NodeOrLongSequenceOrEventOrXMLHttpRequestOrStringOrStringByteStringOrNodeListRecord.cpp
@@ -18,7 +18,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/ToV8.h"
 #include "bindings/core/v8/V8Event.h"
 #include "bindings/core/v8/V8Node.h"
+#include "bindings/core/v8/V8NodeList.h"
 #include "bindings/core/v8/V8XMLHttpRequest.h"
+#include "core/dom/NameNodeList.h"
+#include "core/dom/NodeList.h"
+#include "core/dom/StaticNodeList.h"
+#include "core/html/LabelsNodeList.h"
 
 namespace blink {
 
