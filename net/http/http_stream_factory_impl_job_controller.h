34e7f2da85fb6e9ce6bfbcc08d7d9f4a79274a36
xunjieli <xunjieli@chromium.org>
2017-04-10 07:20:02 -0700

    Remove unreachable code in HttpStreamFactoryImpl::Job::Orphan
    This CL removes unreachable code that handles WebSockets path in
    HttpStreamFactoryImpl::Job::Orphan().
    
    WebSockets will not have two jobs, so the code is unreachable.
    
    This CL adds DCHECK(!for_websockets()) to make sure that the code
    is indeed not reachable.
    
    BUG=475060
    
    Review-Url: https://codereview.chromium.org/2801993006
    Cr-Commit-Position: refs/heads/master@{#463256}

--- net/http/http_stream_factory_impl_job_controller.h
+++ net/http/http_stream_factory_impl_job_controller.h
@@ -137,9 +137,6 @@ class HttpStreamFactoryImpl::JobController
                              const base::WeakPtr<SpdySession>& spdy_session,
                              bool direct) override;
 
-  // Invoked when the orphaned |job| finishes.
-  void OnOrphanedJobComplete(const Job* job) override;
-
   // Invoked when the |job| finishes pre-connecting sockets.
   void OnPreconnectsComplete(Job* job) override;
 
@@ -167,7 +164,7 @@ class HttpStreamFactoryImpl::JobController
   // Remove session from the SpdySessionRequestMap.
   void RemoveRequestFromSpdySessionRequestMapForJob(Job* job) override;
 
-  const NetLogWithSource* GetNetLog(Job* job) const override;
+  const NetLogWithSource* GetNetLog() const override;
 
   void MaybeSetWaitTimeForMainJob(const base::TimeDelta& delay) override;
 
@@ -208,6 +205,9 @@ class HttpStreamFactoryImpl::JobController
   // ignored by JobController. The unbound job can be canceled or continue until
   // completion.
   void OrphanUnboundJob();
+
+  // Invoked when the orphaned |job| finishes.
+  void OnOrphanedJobComplete(const Job* job);
 
   // Called when a Job succeeds.
   void OnJobSucceeded(Job* job);
