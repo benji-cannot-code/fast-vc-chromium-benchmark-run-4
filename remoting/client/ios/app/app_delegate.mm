9aea234cad362b62fbe6ca08f8863b6f7466ea89
nicholss <nicholss@chromium.org>
2017-04-10 10:20:42 -0700

    Update build files and app delegate for new changes to app support files.
    BUG=652781
    
    Review-Url: https://codereview.chromium.org/2802363002
    Cr-Commit-Position: refs/heads/master@{#463305}

--- remoting/client/ios/app/app_delegate.mm
+++ remoting/client/ios/app/app_delegate.mm
@@ -10,11 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "remoting/client/ios/app/app_delegate.h"
 
 #include "base/logging.h"
-
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
-#import "remoting/client/ios/app/example_view_controller.h"
 
+#import "remoting/client/ios/app/remoting_view_controller.h"
+#import "remoting/client/ios/facade/remoting_service.h"
 
 @implementation AppDelegate
 
@@ -30,13 +30,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (BOOL)application:(UIApplication*)application
     didFinishLaunchingWithOptions:(NSDictionary*)launchOptions {
-
-  ExampleViewController *vc = [[ExampleViewController alloc] init];
-
-  self.window.rootViewController = vc;
-
-  [self.window makeKeyAndVisible];
+  [self launchRemotingViewController];
   return YES;
+}
+
+- (BOOL)application:(UIApplication*)application handleOpenURL:(NSURL*)url {
+  NSMutableDictionary* components = [[NSMutableDictionary alloc] init];
+  NSArray* urlComponents = [[url query] componentsSeparatedByString:@"&"];
+
+  for (NSString* componentPair in urlComponents) {
+    NSArray* pair = [componentPair componentsSeparatedByString:@"="];
+    NSString* key = [[pair firstObject] stringByRemovingPercentEncoding];
+    NSString* value = [[pair lastObject] stringByRemovingPercentEncoding];
+    [components setObject:value forKey:key];
+  }
+  NSString* authorizationCode = [components objectForKey:@"code"];
+
+  [[RemotingService SharedInstance]
+      authenticateWithAuthorizationCode:authorizationCode];
+
+  [self launchRemotingViewController];
+  return YES;
+}
+
+- (void)launchRemotingViewController {
+  RemotingViewController* vc = [[RemotingViewController alloc] init];
+  self.window.rootViewController = vc;
+  [self.window makeKeyAndVisible];
 }
 
 @end
