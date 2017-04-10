b2a896874235b055346230e764a3a90968de758d
benwells <benwells@chromium.org>
2017-04-10 02:42:02 -0700

    Log metrics for the new Default Search Engine geolocation setting.
    Recently a new system was introduced which added a preference for
    whether the default search engine has geolocation access or not. This
    change adds metrics for the state of this setting before the disclosure
    UI is shown and after the disclosure UI is shown for the last time.
    
    BUG=700777
    
    Review-Url: https://codereview.chromium.org/2793253002
    Cr-Commit-Position: refs/heads/master@{#463204}

--- chrome/browser/android/search_geolocation/search_geolocation_disclosure_tab_helper.cc
+++ chrome/browser/android/search_geolocation/search_geolocation_disclosure_tab_helper.cc
@@ -208,6 +208,12 @@ void SearchGeolocationDisclosureTabHelper::RecordPreDisclosureMetrics(
         static_cast<base::HistogramBase::Sample>(status),
         static_cast<base::HistogramBase::Sample>(CONTENT_SETTING_NUM_SETTINGS) +
             1);
+
+    SearchGeolocationService* service =
+        SearchGeolocationService::Factory::GetForBrowserContext(GetProfile());
+    UMA_HISTOGRAM_BOOLEAN("GeolocationDisclosure.PreDisclosureDSESetting",
+                          service->GetDSEGeolocationSetting());
+
     prefs->SetBoolean(prefs::kSearchGeolocationPreDisclosureMetricsRecorded,
                       true);
   }
@@ -227,6 +233,12 @@ void SearchGeolocationDisclosureTabHelper::RecordPostDisclosureMetrics(
         static_cast<base::HistogramBase::Sample>(status),
         static_cast<base::HistogramBase::Sample>(CONTENT_SETTING_NUM_SETTINGS) +
             1);
+
+    SearchGeolocationService* service =
+        SearchGeolocationService::Factory::GetForBrowserContext(GetProfile());
+    UMA_HISTOGRAM_BOOLEAN("GeolocationDisclosure.PostDisclosureDSESetting",
+                          service->GetDSEGeolocationSetting());
+
     prefs->SetBoolean(prefs::kSearchGeolocationPostDisclosureMetricsRecorded,
                       true);
   }
