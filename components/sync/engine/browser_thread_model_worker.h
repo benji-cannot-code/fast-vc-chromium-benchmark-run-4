58855fb79157101e9c989df71ee94769b8b55661
fdoray <fdoray@chromium.org>
2017-04-10 15:05:31 -0700

    [Sync] Refactor ModelSafeWorker::DoWorkAndWaitUntilDone() to avoid code duplication.
    Before this CL, logic to wait for a WorkCallback to run or be
    abandoned was duplicated in multiple
    ModelSafeWorker::DoWorkAndWaitUntilDoneImpl() implementations.
    
    With this CL, ModelSafeWorker::DoWorkAndWaitUntilDoneImpl() is
    replaced with ModelSafeWorker::ScheduleWork(). Implementations
    are merely responsible for scheduling the callback that they
    receive; they don't have to wait for it to run or be abandoned.
    
    The logic to wait for the callback to run or be abandoned is moved
    into ModelSafeWorker::DoWorkAndWaitUntilDone().
    ModelSafeWorker::DoWorkAndWaitUntilDone() calls
    ModelSafeWorker::ScheduleWork(). Then, it waits until either the
    callback that it passed to ModelSafeWorker::ScheduleWork() is deleted
    (may or may not have run) or ModelSafeWorker::RequestStop() is called
    before the callback has started to run. Additionnaly, calling
    ModelSafeWorker::RequestStop() transforms into no-op further
    invocations of callbacks passed to ModelSafeWorker::ScheduleWork().
    
    Unblocking ModelSafeWorker::DoWorkAndWaitUntilDone() when
    ModelSafeWorker::RequestStop() is called is important to
    prevent deadlocks in workers that schedule work on the UI
    thread. Indeed, in Chrome, no tasks are scheduled on the UI
    thread after ModelSafeWorker::RequestStop() is called and
    pending UI tasks are not scheduled until after the sync thread
    is joined.
    
    BUG=663600
    
    Review-Url: https://codereview.chromium.org/2782573002
    Cr-Commit-Position: refs/heads/master@{#463413}

--- components/sync/engine/browser_thread_model_worker.h
+++ components/sync/engine/browser_thread_model_worker.h
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/single_thread_task_runner.h"
-#include "components/sync/base/scoped_event_signal.h"
-#include "components/sync/base/syncer_error.h"
 #include "components/sync/engine/model_safe_worker.h"
 
 namespace syncer {
@@ -29,16 +27,11 @@ class BrowserThreadModelWorker : public ModelSafeWorker {
   ModelSafeGroup GetModelSafeGroup() override;
   bool IsOnModelThread() override;
 
- protected:
+ private:
   ~BrowserThreadModelWorker() override;
 
-  SyncerError DoWorkAndWaitUntilDoneImpl(const WorkCallback& work) override;
+  void ScheduleWork(base::OnceClosure work) override;
 
-  void CallDoWorkAndSignalTask(const WorkCallback& work,
-                               syncer::ScopedEventSignal scoped_event_signal,
-                               SyncerError* error);
-
- private:
   scoped_refptr<base::SingleThreadTaskRunner> runner_;
   ModelSafeGroup group_;
 
--- components/sync/engine_impl/directory_update_handler.cc
+++ components/sync/engine_impl/directory_update_handler.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <utility>
 #include <vector>
 
 #include "base/memory/ptr_util.h"
@@ -128,7 +129,7 @@ void DirectoryUpdateHandler::ApplyUpdates(StatusController* status) {
                    // We wait until the callback is executed.  We can safely use
                    // Unretained.
                    base::Unretained(this), base::Unretained(status));
-    worker_->DoWorkAndWaitUntilDone(c);
+    worker_->DoWorkAndWaitUntilDone(std::move(c));
 
     debug_info_emitter_->EmitUpdateCountersUpdate();
     debug_info_emitter_->EmitStatusCountersUpdate();
