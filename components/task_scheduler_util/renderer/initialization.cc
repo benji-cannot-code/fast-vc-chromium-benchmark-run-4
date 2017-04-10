96e67da64f73bee65ac8815dece5fa7087379c02
fdoray <fdoray@chromium.org>
2017-04-10 06:28:13 -0700

    Remove deprecated functions from components/task_scheduler_util/.
    BUG=690706
    
    Review-Url: https://codereview.chromium.org/2799453002
    Cr-Commit-Position: refs/heads/master@{#463242}

--- components/task_scheduler_util/renderer/initialization.cc
+++ components/task_scheduler_util/renderer/initialization.cc
@@ -6,48 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/task_scheduler_util/renderer/initialization.h"
 
 #include "base/command_line.h"
-#include "base/task_scheduler/task_traits.h"
 #include "components/task_scheduler_util/common/variations_util.h"
 
 namespace task_scheduler_util {
-
-namespace {
-
-enum WorkerPoolType : size_t {
-  BACKGROUND = 0,
-  BACKGROUND_BLOCKING,
-  FOREGROUND,
-  FOREGROUND_BLOCKING,
-  WORKER_POOL_COUNT  // Always last.
-};
-
-}  // namespace
 
 std::unique_ptr<base::TaskScheduler::InitParams>
 GetRendererTaskSchedulerInitParamsFromCommandLine() {
   return GetTaskSchedulerInitParams(
       "Renderer", GetVariationParamsFromCommandLine(
                       *base::CommandLine::ForCurrentProcess()));
-}
-
-std::vector<base::SchedulerWorkerPoolParams> GetRendererWorkerPoolParams() {
-  const auto init_params = GetRendererTaskSchedulerInitParamsFromCommandLine();
-  if (!init_params)
-    return std::vector<base::SchedulerWorkerPoolParams>();
-
-  return std::vector<base::SchedulerWorkerPoolParams>{
-      init_params->background_worker_pool_params,
-      init_params->background_blocking_worker_pool_params,
-      init_params->foreground_worker_pool_params,
-      init_params->foreground_blocking_worker_pool_params};
-}
-
-size_t RendererWorkerPoolIndexForTraits(const base::TaskTraits& traits) {
-  const bool is_background =
-      traits.priority() == base::TaskPriority::BACKGROUND;
-  if (traits.may_block() || traits.with_base_sync_primitives())
-    return is_background ? BACKGROUND_BLOCKING : FOREGROUND_BLOCKING;
-  return is_background ? BACKGROUND : FOREGROUND;
 }
 
 }  // namespace task_scheduler_util
--- components/task_scheduler_util/renderer/initialization.h
+++ components/task_scheduler_util/renderer/initialization.h
@@ -6,17 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_TASK_SCHEDULER_UTIL_RENDERER_INITIALIZATION_H_
 #define COMPONENTS_TASK_SCHEDULER_UTIL_RENDERER_INITIALIZATION_H_
 
-#include <stddef.h>
-
 #include <memory>
-#include <vector>
 
-#include "base/task_scheduler/scheduler_worker_pool_params.h"
 #include "base/task_scheduler/task_scheduler.h"
-
-namespace base {
-class TaskTraits;
-}
 
 namespace task_scheduler_util {
 
@@ -25,20 +17,6 @@ namespace task_scheduler_util {
 // on failure.
 std::unique_ptr<base::TaskScheduler::InitParams>
 GetRendererTaskSchedulerInitParamsFromCommandLine();
-
-// Gets a vector of SchedulerWorkerPoolParams to initialize TaskScheduler in a
-// renderer based off variation params specified on the command line. Returns an
-// empty vector if variation params specified on the command line are incomplete
-// or invalid.
-//
-// Deprecated. https://crbug.com/690706
-std::vector<base::SchedulerWorkerPoolParams> GetRendererWorkerPoolParams();
-
-// Maps |traits| to the index of a renderer worker pool vector provided by
-// GetRendererWorkerPoolParams().
-//
-// Deprecated. https://crbug.com/690706
-size_t RendererWorkerPoolIndexForTraits(const base::TaskTraits& traits);
 
 }  // namespace task_scheduler_util
 
