6e2271394473e62bddb3e208908d9c6baa5c7cc4
fdoray <fdoray@chromium.org>
2017-04-10 07:06:36 -0700

    Include what you use for thread_task_runner_handle.h in components/.
    BUG=
    
    Review-Url: https://codereview.chromium.org/2800133002
    Cr-Commit-Position: refs/heads/master@{#463252}

--- components/ntp_tiles/most_visited_sites_unittest.cc
+++ components/ntp_tiles/most_visited_sites_unittest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/cancelable_task_tracker.h"
 #include "base/test/sequenced_worker_pool_owner.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "components/history/core/browser/top_sites.h"
 #include "components/history/core/browser/top_sites_observer.h"
 #include "components/ntp_tiles/icon_cacher.h"
