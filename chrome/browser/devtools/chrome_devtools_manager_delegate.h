9e50d3ef30116323aba5503c8d8096cbbb0e3ac8
jzfeng <jzfeng@chromium.org>
2017-04-10 13:54:36 -0700

    Revert of add a new set of commands to resize and position windows (patchset #35 id:680001 of https://codereview.chromium.org/2734123004/ )
    Reason for revert:
    It cause flaky browser tests on mac10.10
    
    https://uberchromegw.corp.google.com/i/chromium.mac/builders/Mac10.10%20Tests?numbuilds=100
    
    Original issue's description:
    > add a new set of commands to resize and position windows
    >
    > https://docs.google.com/document/d/1Q0kuHgU1d-sj8gePjEU9nHtlQzz5rNvvGH8fCJ3iBq4/edit?usp=sharing
    >
    > Devtools side, add a new UI domain and methods:
    > method: getWindowForTarget, param: targetID, return: windowID, bounds.
    > method: setWindowBounds, param: windowID, bounds
    > method: getWindowBounds, param: windowID, return: bounds
    >
    > bounds object: left, top, width, height, window_state (minimized, maximized, fullscreen, normal)
    >
    > BUG=604324
    >
    > Review-Url: https://codereview.chromium.org/2734123004
    > Cr-Commit-Position: refs/heads/master@{#463110}
    > Committed: https://chromium.googlesource.com/chromium/src/+/24a8ad434aab59c8c8a08ba02d934ca5fdfdc7dd
    
    TBR=dgozman@chromium.org,pfeldman@chromium.org,samuong@chromium.org
    
    Review-Url: https://codereview.chromium.org/2808923002
    Cr-Commit-Position: refs/heads/master@{#463388}

--- chrome/browser/devtools/chrome_devtools_manager_delegate.h
+++ chrome/browser/devtools/chrome_devtools_manager_delegate.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <memory>
 #include <set>
-#include <string>
 
 #include "base/compiler_specific.h"
 #include "base/macros.h"
@@ -33,7 +32,6 @@ class ChromeDevToolsManagerDelegate :
 
  private:
   class HostData;
-  friend class DevToolsManagerDelegateTest;
   using RemoteLocations = std::set<net::HostPortPair>;
 
   // content::DevToolsManagerDelegate implementation.
@@ -61,20 +59,6 @@ class ChromeDevToolsManagerDelegate :
   std::unique_ptr<base::DictionaryValue> SetRemoteLocations(
       content::DevToolsAgentHost* agent_host,
       int command_id,
-      base::DictionaryValue* params);
-
-  std::unique_ptr<base::DictionaryValue> HandleBrowserCommand(
-      int id,
-      std::string method,
-      base::DictionaryValue* params);
-  static std::unique_ptr<base::DictionaryValue> GetWindowForTarget(
-      int id,
-      base::DictionaryValue* params);
-  static std::unique_ptr<base::DictionaryValue> GetWindowBounds(
-      int id,
-      base::DictionaryValue* params);
-  static std::unique_ptr<base::DictionaryValue> SetWindowBounds(
-      int id,
       base::DictionaryValue* params);
 
   std::unique_ptr<DevToolsNetworkProtocolHandler> network_protocol_handler_;
