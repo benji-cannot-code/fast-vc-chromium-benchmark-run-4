cb85d4495eb123ca520b0618d7d739713d3f70db
jkrcal <jkrcal@chromium.org>
2017-04-10 07:37:20 -0700

    [Image fetcher] Add Content-Location header to image_fetcher::RequestMetadata
    Previous CL (2793983002) added a pointer to net::HttpResponseHeaders
    into image_fetcher::RequestMetadata. This was flawed because the object
    got deleted before the callback to ImageFetcher was called. A solution
    would be to create a copy of the headers object from its raw_headers()
    content. This seems too heavy for a corner-use case needing one header.
    
    As a solution, this CL removes the what has been added by the previous
    CL and adds a new struct field for this particular use case.
    
    BUG=644102
    
    Review-Url: https://codereview.chromium.org/2807143002
    Cr-Commit-Position: refs/heads/master@{#463260}

--- components/image_fetcher/core/image_data_fetcher.cc
+++ components/image_fetcher/core/image_data_fetcher.cc
@@ -17,6 +17,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using data_use_measurement::DataUseUserData;
 
+namespace {
+
+const char kContentLocationHeader[] = "Content-Location";
+
+}  // namespace
+
 namespace image_fetcher {
 
 // An active image URL fetcher request. The struct contains the related requests
@@ -90,9 +96,12 @@ void ImageDataFetcher::OnURLFetchComplete(const net::URLFetcher* source) {
 
   RequestMetadata metadata;
   if (success && source->GetResponseHeaders()) {
-    metadata.http_response_headers = source->GetResponseHeaders();
     source->GetResponseHeaders()->GetMimeType(&metadata.mime_type);
     metadata.http_response_code = source->GetResponseHeaders()->response_code();
+    // Just read the first value-pair for this header (not caring about |iter|).
+    source->GetResponseHeaders()->EnumerateHeader(
+        /*iter=*/nullptr, kContentLocationHeader,
+        &metadata.content_location_header);
     success &= (metadata.http_response_code == net::HTTP_OK);
   }
   metadata.from_http_cache = source->WasCached();
