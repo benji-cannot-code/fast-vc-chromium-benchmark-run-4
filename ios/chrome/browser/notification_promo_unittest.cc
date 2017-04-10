0d49c1daf7b47f274226c444e743eb0a16ba9c29
gchatz <gchatz@chromium.org>
2017-04-10 11:56:37 -0700

    Remove unnecessary promo_type_ field from notification_promo.h/.cc.
    Removes the promo_type_ field from notification_promo.h/.cc
    and uses of it. It is unnecessary as the only promo type in use is the
    MOBILE_NTP_WHATS_NEW_PROMO.
    
    BUG=608525
    
    Review-Url: https://codereview.chromium.org/2805443007
    Cr-Commit-Position: refs/heads/master@{#463344}

--- ios/chrome/browser/notification_promo_unittest.cc
+++ ios/chrome/browser/notification_promo_unittest.cc
@@ -112,7 +112,6 @@ class NotificationPromoTest : public testing::Test {
                                          field_trial_params);
     base::FieldTrialList::CreateFieldTrial("IOSNTPPromotion", "Group1");
 
-    promo_type_ = NotificationPromo::MOBILE_NTP_WHATS_NEW_PROMO;
     promo_text_ = promo_text;
 
     start_ = start;
@@ -134,7 +133,7 @@ class NotificationPromoTest : public testing::Test {
   }
 
   void InitPromoFromJson() {
-    notification_promo_.InitFromJson(*test_json_, promo_type_);
+    notification_promo_.InitFromJson(*test_json_);
 
     // Test the fields.
     TestServerProvidedParameters();
@@ -159,7 +158,7 @@ class NotificationPromoTest : public testing::Test {
     // Initialize promo from saved prefs and server params.
     NotificationPromo first_promo(&local_state_);
     first_promo.InitFromVariations();
-    first_promo.InitFromPrefs(promo_type_);
+    first_promo.InitFromPrefs();
     EXPECT_EQ(first_promo.max_views_ - 2, first_promo.views_);
     EXPECT_TRUE(first_promo.CanShow());
     first_promo.HandleViewed();
@@ -168,14 +167,14 @@ class NotificationPromoTest : public testing::Test {
     // correctly in prefs.
     NotificationPromo second_promo(&local_state_);
     second_promo.InitFromVariations();
-    second_promo.InitFromPrefs(promo_type_);
+    second_promo.InitFromPrefs();
     EXPECT_EQ(second_promo.max_views_ - 1, second_promo.views_);
     EXPECT_TRUE(second_promo.CanShow());
     second_promo.HandleViewed();
 
     NotificationPromo third_promo(&local_state_);
     third_promo.InitFromVariations();
-    third_promo.InitFromPrefs(promo_type_);
+    third_promo.InitFromPrefs();
     EXPECT_EQ(third_promo.max_views_, third_promo.views_);
     EXPECT_FALSE(third_promo.CanShow());
 
@@ -200,7 +199,7 @@ class NotificationPromoTest : public testing::Test {
     // Initialize promo from saved prefs and server params.
     NotificationPromo first_promo(&local_state_);
     first_promo.InitFromVariations();
-    first_promo.InitFromPrefs(promo_type_);
+    first_promo.InitFromPrefs();
     EXPECT_FALSE(first_promo.closed_);
     EXPECT_TRUE(first_promo.CanShow());
     first_promo.HandleClosed();
@@ -209,7 +208,7 @@ class NotificationPromoTest : public testing::Test {
     // recorded correctly in prefs.
     NotificationPromo second_promo(&local_state_);
     second_promo.InitFromVariations();
-    second_promo.InitFromPrefs(promo_type_);
+    second_promo.InitFromPrefs();
     EXPECT_TRUE(second_promo.closed_);
     EXPECT_FALSE(second_promo.CanShow());
 
@@ -277,7 +276,7 @@ class NotificationPromoTest : public testing::Test {
 
     NotificationPromo temp_promo(&local_state_);
     temp_promo.InitFromVariations();
-    temp_promo.InitFromPrefs(promo_type_);
+    temp_promo.InitFromPrefs();
     EXPECT_NE(0, temp_promo.first_view_time_);
 
     notification_promo_.views_ = 0;
@@ -317,7 +316,7 @@ class NotificationPromoTest : public testing::Test {
 
     // Initialize promo and verify that its instance variables match the data
     // saved in the old structure.
-    promo.InitFromPrefs(promo_type_);
+    promo.InitFromPrefs();
     EXPECT_DOUBLE_EQ(first_view_time, promo.first_view_time_);
     EXPECT_EQ(views, promo.views_);
     EXPECT_EQ(closed, promo.closed_);
@@ -344,7 +343,6 @@ class NotificationPromoTest : public testing::Test {
   bool received_notification_;
   std::unique_ptr<base::DictionaryValue> test_json_;
 
-  NotificationPromo::PromoType promo_type_;
   std::string promo_text_;
 
   double start_;
