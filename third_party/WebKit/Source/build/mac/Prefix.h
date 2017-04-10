2dda1147d271cacc33a9974bf47f9222a8ae555e
tapted <tapted@chromium.org>
2017-04-09 22:33:04 -0700

    Remove CoreServices.h from Blink's precompiled headers on Mac
    Its presence is causing compile failures in non-goma builds since the
    Blink identifier renames in r463139. (goma builds disable precompiled
    headers, so this problem isn't appearing on the bots).
    
    Blink Mac code doesn't seem to use anything from it. The framework
    includes deprecated Carbon framework headers such as AVLTree.h which
    pollute the global namespace with `kLeftToRight` which is causing
    ambiguity with WTF::Unicode::kLeftToRight when it is used in
    WebKit/Source/platform/text/BidiResolver.h
    
    Note that Objective C code in blink (there is not much of it) will still
    see symbols from CoreServices.h when it's included via Cocoa.h.
    
    BUG=709850
    TBR=thakis@chromium.org
    
    Review-Url: https://codereview.chromium.org/2800343002
    Cr-Commit-Position: refs/heads/master@{#463173}

--- third_party/WebKit/Source/build/mac/Prefix.h
+++ third_party/WebKit/Source/build/mac/Prefix.h
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sys/time.h>
 #include <sys/resource.h>
 #include <CoreFoundation/CoreFoundation.h>
-#include <CoreServices/CoreServices.h>
 
 #ifdef __OBJC__
 #import <Cocoa/Cocoa.h>
