d74750960d321829240762e258a720b01ee56458
changwan <changwan@chromium.org>
2017-04-10 14:40:32 -0700

    Show keyboard on URL bar whenever it regains window focus
    Currently, we show keyboard on window focus gain only when we previously
    lost window visibility, which we introduced to fix crbug.com/168288.
    
    Extending the current logic to resume keyboard input when regaining
    focus from a pop-up window such as voice input window.
    
    BUG=344235
    
    Review-Url: https://codereview.chromium.org/2806383003
    Cr-Commit-Position: refs/heads/master@{#463405}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/UrlBar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/UrlBar.java
@@ -72,8 +72,6 @@ public class UrlBar extends VerticallyFixedEditText {
     /** The contents of the URL that precede the path/query before formatting. */
     private String mOriginalUrlLocation;
 
-    private boolean mShowKeyboardOnWindowFocus;
-
     private boolean mFirstDrawComplete;
 
     /**
@@ -514,16 +512,11 @@ public class UrlBar extends VerticallyFixedEditText {
     }
 
     @Override
-    protected void onWindowVisibilityChanged(int visibility) {
-        super.onWindowVisibilityChanged(visibility);
-        if (visibility == View.GONE && isFocused()) mShowKeyboardOnWindowFocus = true;
-    }
-
-    @Override
     public void onWindowFocusChanged(boolean hasWindowFocus) {
         super.onWindowFocusChanged(hasWindowFocus);
+        if (DEBUG) Log.i(TAG, "onWindowFocusChanged: " + hasWindowFocus);
         if (hasWindowFocus) {
-            if (mShowKeyboardOnWindowFocus && isFocused()) {
+            if (isFocused()) {
                 // Without the call to post(..), the keyboard was not getting shown when the
                 // window regained focus despite this being the final call in the view system
                 // flow.
@@ -534,7 +527,6 @@ public class UrlBar extends VerticallyFixedEditText {
                     }
                 });
             }
-            mShowKeyboardOnWindowFocus = false;
         }
     }
 
