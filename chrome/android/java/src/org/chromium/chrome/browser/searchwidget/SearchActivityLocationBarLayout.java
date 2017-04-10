73e38b170f5491f34399f18aeec1220dce84fbd3
dfalcantara <dfalcantara@chromium.org>
2017-04-10 12:21:35 -0700

    Revert "🔍 Hide the Google G by default"
    This reverts commit 1204d2395bf8818c777c3af005f046b9d11036b9.
    
    This introduced one bug and exposed another.  Yay!
    
    BUG=709884,709886
    TBR=bauerb,peconn
    
    Review-Url: https://codereview.chromium.org/2806373003
    Cr-Commit-Position: refs/heads/master@{#463356}

--- chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchActivityLocationBarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchActivityLocationBarLayout.java
@@ -7,7 +7,9 @@ package org.chromium.chrome.browser.searchwidget;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 
+import org.chromium.chrome.R;
 import org.chromium.chrome.browser.omnibox.LocationBarLayout;
 
 /** Implementation of the {@link LocationBarLayout} that is displayed for widget searches. */
@@ -25,6 +27,8 @@ public class SearchActivityLocationBarLayout extends LocationBarLayout {
 
     public SearchActivityLocationBarLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+        View gContainer = findViewById(R.id.google_g_container);
+        if (gContainer != null) gContainer.setVisibility(View.GONE);
     }
 
     /** Set the {@link Delegate}. */
