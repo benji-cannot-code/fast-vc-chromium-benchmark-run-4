47055694c7bf9c1c82fd74eb4becdaeedcc9f6a9
rtoy <rtoy@chromium.org>
2017-04-10 14:12:16 -0700

    Convert ChannelMerger tests to new Audit
    Manually converted tests to use new Audit.
    
    BUG=704967
    TEST=
    ChannelMerger/audiochannelmerger-basic.html
    ChannelMerger/audiochannelmerger-cycle.html
    ChannelMerger/audiochannelmerger-disconnect.html
    ChannelMerger/audiochannelmerger-input-non-default.html
    ChannelMerger/audiochannelmerger-input.html
    
    Review-Url: https://codereview.chromium.org/2781123002
    Cr-Commit-Position: refs/heads/master@{#463392}

--- third_party/WebKit/LayoutTests/webaudio/resources/merger-testing.js
+++ third_party/WebKit/LayoutTests/webaudio/resources/merger-testing.js
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This file is for the audiochannelmerger-* layout tests.
 // Requires |audio-testing.js| to work properly.
 
-function testMergerInput(config, done) {
+function testMergerInput(should, config) {
   var context = new OfflineAudioContext(config.numberOfChannels, 128, 44100);
   var merger = context.createChannelMerger(config.numberOfChannels);
   var source = context.createBufferSource();
@@ -16,11 +16,10 @@ function testMergerInput(config, done) {
   merger.connect(context.destination);
   source.start();
 
-  context.startRendering().then(function (buffer) {
+  return context.startRendering().then(function (buffer) {
     let prefix = config.testBufferContent.length + '-channel source: ';
     for (var i = 0; i < config.numberOfChannels; i++)
-      Should(prefix + 'Channel #' + i, buffer.getChannelData(i))
+      should(buffer.getChannelData(i), prefix + 'Channel #' + i)
         .beConstantValueOf(config.expected[i]);
-    done();
   });
 }
