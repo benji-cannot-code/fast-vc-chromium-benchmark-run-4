deaf0a57f6370c94c35c585189a8c0abea643857
yutak <yutak@chromium.org>
2017-04-10 04:24:45 -0700

    Rewrite references to "wtf/" to "platform/wtf/" in platform/scheduler.
    This is essentially the result of the following shell command followed by
    git cl format:
    
        find . -type f -exec sed -i 's/"wtf\//"platform\/wtf\//g' {} \;
    
    DEPS is updated, too, in order to prevent further introduction of "wtf/".
    
    BUG=691465
    
    Review-Url: https://codereview.chromium.org/2796223002
    Cr-Commit-Position: refs/heads/master@{#463220}

--- third_party/WebKit/Source/platform/scheduler/renderer/webthread_impl_for_renderer_scheduler.h
+++ third_party/WebKit/Source/platform/scheduler/renderer/webthread_impl_for_renderer_scheduler.h
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_WEBKIT_SOURCE_PLATFORM_SCHEDULER_RENDERER_WEBTHREAD_IMPL_FOR_RENDERER_SCHEDULER_H_
 #define THIRD_PARTY_WEBKIT_SOURCE_PLATFORM_SCHEDULER_RENDERER_WEBTHREAD_IMPL_FOR_RENDERER_SCHEDULER_H_
 
+#include "platform/wtf/RefPtr.h"
 #include "public/platform/scheduler/child/webthread_base.h"
-#include "wtf/RefPtr.h"
 
 namespace blink {
 class WebScheduler;
--- third_party/WebKit/Source/platform/scheduler/test/fake_web_task_runner.cc
+++ third_party/WebKit/Source/platform/scheduler/test/fake_web_task_runner.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/logging.h"
 #include "base/single_thread_task_runner.h"
-#include "wtf/RefCounted.h"
+#include "platform/wtf/RefCounted.h"
 
 namespace blink {
 namespace scheduler {
