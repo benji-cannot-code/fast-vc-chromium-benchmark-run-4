e9433b1958a57ddb094bebbe757b88f52e8135ae
hbos <hbos@chromium.org>
2017-04-10 02:17:20 -0700

    RTCRtpReceiver.getContributingSources() added.
    Adds RTCRtpReceiver.getContributingSources(),
    https://w3c.github.io/webrtc-pc/#dom-rtcrtpreceiver-getcontributingsources,
    and RTCRtpContributingSource,
    https://w3c.github.io/webrtc-pc/#dom-rtcrtpcontributingsource,
    behind flag "RuntimeEnabled=RTCRtpReceiver".
    
    Contributing sources come in two flavors, SSRCs and CSRCs. This
    interface represents both. For now we ignore SSRCs and only return
    CSRCs. It was recently decided that SSRCs would be returned in a
    separate method to distinguish between the two cases.
    
    A LayoutTest is used to thoroughly test all blink functionality with
    regards to CSRCs and the caching of contributing sources. This uses
    mocking to update contributing sources in a predictable way.
    
    The browsertest verifies that no contributing sources are used in a
    normal peerconnection call. The multiple CSRCs case is currently not
    possible to test in a browsertest so we rely on the LayoutTest (and
    webrtc layer testing of contributing sources).
    
    BUG=703122
    
    Review-Url: https://codereview.chromium.org/2803693002
    Cr-Commit-Position: refs/heads/master@{#463199}

--- content/renderer/media/webrtc/rtc_rtp_receiver.cc
+++ content/renderer/media/webrtc/rtc_rtp_receiver.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/media/webrtc/rtc_rtp_receiver.h"
 
 #include "base/logging.h"
+#include "base/memory/ptr_util.h"
+#include "content/renderer/media/webrtc/rtc_rtp_contributing_source.h"
 #include "third_party/webrtc/base/scoped_ref_ptr.h"
 
 namespace content {
@@ -42,6 +44,17 @@ uintptr_t RTCRtpReceiver::Id() const {
 const blink::WebMediaStreamTrack& RTCRtpReceiver::Track() const {
   DCHECK(web_track_ == webrtc_track());
   return web_track_;
+}
+
+blink::WebVector<std::unique_ptr<blink::WebRTCRtpContributingSource>>
+RTCRtpReceiver::GetSources() {
+  auto webrtc_sources = webrtc_rtp_receiver_->GetSources();
+  blink::WebVector<std::unique_ptr<blink::WebRTCRtpContributingSource>> sources(
+      webrtc_sources.size());
+  for (size_t i = 0; i < webrtc_sources.size(); ++i) {
+    sources[i] = base::MakeUnique<RTCRtpContributingSource>(webrtc_sources[i]);
+  }
+  return sources;
 }
 
 const webrtc::MediaStreamTrackInterface& RTCRtpReceiver::webrtc_track() const {
