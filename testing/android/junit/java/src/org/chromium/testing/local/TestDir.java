607651eab0800fc1ea105b833f5507d69bbbf72a
scottkirkwood <scottkirkwood@chromium.org>
2017-04-10 11:14:34 -0700

    Add a way to get the test data folder from junit.
    BUG=
    
    Review-Url: https://codereview.chromium.org/2788733002
    Cr-Commit-Position: refs/heads/master@{#463328}

---
+++ testing/android/junit/java/src/org/chromium/testing/local/TestDir.java
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.testing.local;
+
+import java.io.File;
+
+/**
+ *  Tools to get the junit test path.
+ */
+public class TestDir {
+    private static final String TEST_FOLDER = "/chrome/test/data/";
+
+    /**
+     * Construct the full path of a test data file.
+     * @param path Pathname relative to chrome/test/data.
+     */
+    public static String getTestFilePath(String path) {
+        String junitRoot = System.getProperty("dir.source.root");
+        if (junitRoot == null) {
+            junitRoot = "";
+        }
+        return new File(junitRoot + TEST_FOLDER + path).getPath();
+    }
+}
