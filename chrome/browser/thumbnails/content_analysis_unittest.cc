23989f9214ab5db56da2e0f68b08566fc8a4f1dd
uzair.jaleel <uzair.jaleel@samsung.com>
2017-04-10 20:27:02 -0700

    Remove deprecated functions from Canvas in favor of the RectF version.
    BUG=553726
    
    Review-Url: https://codereview.chromium.org/2799213003
    Cr-Commit-Position: refs/heads/master@{#463518}

--- chrome/browser/thumbnails/content_analysis_unittest.cc
+++ chrome/browser/thumbnails/content_analysis_unittest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/color_analysis.h"
 #include "ui/gfx/color_utils.h"
 #include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/geometry/rect_f.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/image/image.h"
 
@@ -128,7 +129,7 @@ TEST_F(ThumbnailContentAnalysisTest, ApplyGradientMagnitudeOnFrame) {
   gfx::Canvas canvas(gfx::Size(800, 600), 1.0f, true);
 
   // The image consists of a single white block in the centre.
-  gfx::Rect draw_rect(300, 200, 200, 200);
+  gfx::RectF draw_rect(300, 200, 200, 200);
   canvas.FillRect(gfx::Rect(0, 0, 800, 600), SkColorSetRGB(0, 0, 0));
   canvas.DrawRect(draw_rect, SkColorSetRGB(255, 255, 255));
 
@@ -165,7 +166,7 @@ TEST_F(ThumbnailContentAnalysisTest, ExtractImageProfileInformation) {
   gfx::Canvas canvas(gfx::Size(800, 600), 1.0f, true);
 
   // The image consists of a white frame drawn in the centre.
-  gfx::Rect draw_rect(100, 100, 200, 100);
+  gfx::RectF draw_rect(100, 100, 200, 100);
   gfx::Rect image_rect(0, 0, 800, 600);
   canvas.FillRect(image_rect, SkColorSetRGB(0, 0, 0));
   canvas.DrawRect(draw_rect, SkColorSetRGB(255, 255, 255));
@@ -230,8 +231,8 @@ TEST_F(ThumbnailContentAnalysisTest,
   // single-pixel vertical gap in between.
   gfx::Rect image_rect(0, 0, 800, 600);
   canvas.FillRect(image_rect, SkColorSetRGB(0, 0, 0));
-  canvas.DrawRect(gfx::Rect(300, 250, 99, 100), SkColorSetRGB(255, 255, 255));
-  canvas.DrawRect(gfx::Rect(401, 250, 99, 100), SkColorSetRGB(255, 255, 255));
+  canvas.DrawRect(gfx::RectF(300, 250, 99, 100), SkColorSetRGB(255, 255, 255));
+  canvas.DrawRect(gfx::RectF(401, 250, 99, 100), SkColorSetRGB(255, 255, 255));
 
   SkBitmap source = canvas.GetBitmap();
   SkBitmap reduced_color;
@@ -622,7 +623,7 @@ TEST_F(ThumbnailContentAnalysisTest, CreateRetargetedThumbnailImage) {
     for (int x = footer_rect.x() + fine_print;
          x < footer_rect.right() - fine_print;
          x += 2 * fine_print) {
-      canvas.DrawRect(gfx::Rect(x, y, fine_print, fine_print),  print_color);
+      canvas.DrawRect(gfx::RectF(x, y, fine_print, fine_print), print_color);
     }
   }
 
@@ -650,7 +651,7 @@ TEST_F(ThumbnailContentAnalysisTest, CreateRetargetedThumbnailImage) {
     for (int x = body_rect.x() + fine_print;
          x < body_rect.right() - fine_print;
          x += 2 * fine_print) {
-      canvas.DrawRect(gfx::Rect(x, y, fine_print, fine_print),  print_color);
+      canvas.DrawRect(gfx::RectF(x, y, fine_print, fine_print), print_color);
     }
   }
 
@@ -660,11 +661,10 @@ TEST_F(ThumbnailContentAnalysisTest, CreateRetargetedThumbnailImage) {
         body_rect.height() / 3 * line + margin_vertical;
     const int x = body_rect.x() +
         alignment * body_rect.width() / 2 + margin_vertical;
-    gfx::Rect pict_rect(x, y,
-                        body_rect.width() / 2 - 2 * margin_vertical,
+    gfx::Rect pict_rect(x, y, body_rect.width() / 2 - 2 * margin_vertical,
                         body_rect.height() / 3 - 2 * margin_vertical);
     canvas.FillRect(pict_rect, SkColorSetRGB(255, 255, 255));
-    canvas.DrawRect(pict_rect, SkColorSetRGB(0, 0, 0));
+    canvas.DrawRect(gfx::RectF(pict_rect), SkColorSetRGB(0, 0, 0));
   }
 
   SkBitmap source = canvas.GetBitmap();
