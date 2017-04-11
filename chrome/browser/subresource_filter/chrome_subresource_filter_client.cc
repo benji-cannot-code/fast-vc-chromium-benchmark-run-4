2f61a4066198aada3c3f1d69ee0f54f7f2e37edd
csharrison <csharrison@chromium.org>
2017-04-11 06:13:13 -0700

    Move SubresourceFilterContentSettingsObserver to its own class
    Also rename the observer to manager, as it will soon be the go-to class for
    managing all the content/website settings for subresource filtering.
    
    This CL should have no functional changes
    
    Review-Url: https://codereview.chromium.org/2805983003
    Cr-Commit-Position: refs/heads/master@{#463609}

--- chrome/browser/subresource_filter/chrome_subresource_filter_client.cc
+++ chrome/browser/subresource_filter/chrome_subresource_filter_client.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/content_settings/tab_specific_content_settings.h"
 #include "chrome/browser/infobars/infobar_service.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/subresource_filter/subresource_filter_content_settings_observer_factory.h"
+#include "chrome/browser/subresource_filter/subresource_filter_content_settings_manager_factory.h"
 #include "chrome/browser/ui/android/content_settings/subresource_filter_infobar_delegate.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/content_settings/core/common/content_settings_types.h"
@@ -23,8 +23,8 @@ ChromeSubresourceFilterClient::ChromeSubresourceFilterClient(
     content::WebContents* web_contents)
     : web_contents_(web_contents), shown_for_navigation_(false) {
   DCHECK(web_contents);
-  // Ensure the content settings observer is initialized.
-  SubresourceFilterContentSettingsObserverFactory::EnsureForProfile(
+  // Ensure the content settings manager is initialized.
+  SubresourceFilterContentSettingsManagerFactory::EnsureForProfile(
       Profile::FromBrowserContext(web_contents_->GetBrowserContext()));
 }
 
