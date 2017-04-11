823ff57ee60138ec115e974e2a964d595eb03a27
raphael.kubo.da.costa <raphael.kubo.da.costa@intel.com>
2017-04-11 13:36:43 -0700

    bindings: Use ExceptionMessages when an interface/callback conversion fails.
    https://codereview.chromium.org/2802223002 ("bindings: Throw a type error if
    converting an interface or callback function fails") introduced a 32kb size
    regression on release Android builds (is_official_build=true), as the new
    error strings being thrown as type errors could not be shared and all made
    it into the .rodata section.
    
    Start using ExceptionMessages::FailedToConstruct() in the error messages.
    Not only do we use the existing error infrastructure, but it allows us to
    separate the part that always changes (the callback or interface name) from
    the actual details (the "Unable to convert value" bit).
    
    With commit 07c092298a6243fc2c829e517cf2b19f4caea26c ("Fetch: Make Headers'
    constructor match the current spec IDL") as a baseline, the following data
    can be observed with //tools/binary_size/map2size.py and console.py:
    
    * Reverting the changes from https://codereview.chromium.org/2802223002
      versus this commit:
    
      Section Sizes (Total=-168 bytes):
        .bss: 0 bytes (not included in totals)
        .data: 0 bytes (-0.0%)
        .data.rel.ro: 0 bytes (-0.0%)
        .data.rel.ro.local: 0 bytes (-0.0%)
        .rodata: 256 bytes (-152.4%)
        .text: -424 bytes (252.4%)
      0 symbols added (+), 27 changed (~), 3 removed (-), 313837 unchanged (not shown)
    
      Showing 30 symbols with total size: -160 bytes
      .text=-416 bytes .rodata=256 bytes  other=0 bytes    total=-160 bytes
      Number of object files: 28
    
    * Commit b1bbca356abfa6f8fdec3cd5815f18b164491a45 versus this commit:
    
      Section Sizes (Total=-33,704 bytes):
        .bss: 0 bytes (not included in totals)
        .data: 0 bytes (-0.0%)
        .data.rel.ro: 0 bytes (-0.0%)
        .data.rel.ro.local: 0 bytes (-0.0%)
        .rodata: -33,280 bytes (98.7%)
        .text: -424 bytes (1.3%)
      0 symbols added (+), 27 changed (~), 3 removed (-), 313837 unchanged (not shown)
    
      Showing 30 symbols with total size: -33696 bytes
      .text=-416 bytes .rodata=-32.5kb    other=0 bytes    total=-32.9kb
      Number of object files: 28
    
    BUG=690428,710035
    R=agrieve@chromium.org,bashi@chromium.org,haraken@chromium.org,jbroman@chromium.org,yukishiino@chromium.org
    
    Review-Url: https://codereview.chromium.org/2809003004
    Cr-Commit-Position: refs/heads/master@{#463753}

--- third_party/WebKit/Source/bindings/core/v8/ExceptionMessages.cpp
+++ third_party/WebKit/Source/bindings/core/v8/ExceptionMessages.cpp
@@ -36,6 +36,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+String ExceptionMessages::FailedToConvertJSValue(const char* type) {
+  return String::Format("Failed to convert value to '%s'.", type);
+}
+
 String ExceptionMessages::FailedToConstruct(const char* type,
                                             const String& detail) {
   return "Failed to construct '" + String(type) +
