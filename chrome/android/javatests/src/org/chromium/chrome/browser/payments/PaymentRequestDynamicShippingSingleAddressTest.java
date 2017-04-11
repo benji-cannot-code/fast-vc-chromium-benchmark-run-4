24000ee6e95cef7a2e842db033cc01e21c062479
gogerald <gogerald@chromium.org>
2017-04-10 18:30:55 -0700

    Preview available payment options when no option is selected
    The behavior when there is a selected option in a payment section is the same as before.
    
    When there is no selected option in a payment section:
    a) Display a summary of the options in the section.
    b) The summary is displayed in a single line in grey color with font 14sp.
    c) If there are more than one options in the section, the string pattern is "<Summary of the first option>... and N more".
       N+1 is the total number of options in the section. "* and N more" should never be elided.
       <Summary of the first option> can be shorten to make the entire summary fit the width of the view.
       Do not elide card summary in the middle of the card number, e.g either display "VISA ....1234" or "VISA".
    d) If there is only one option in the section, the string is "<Summary of the first option>".
       Elides at the end of the string if it does not fit the width of the view.
    e) If there is no option in the section, do not show anything.
    
    BUG=703806
    
    Review-Url: https://codereview.chromium.org/2775403002
    Cr-Commit-Position: refs/heads/master@{#463488}

--- chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestDynamicShippingSingleAddressTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestDynamicShippingSingleAddressTest.java
@@ -49,7 +49,7 @@ public class PaymentRequestDynamicShippingSingleAddressTest extends PaymentReque
     public void testAddressNotSelected()
             throws InterruptedException, ExecutionException, TimeoutException {
         triggerUIAndWait(mReadyForInput);
-        assertEquals(PaymentRequestSection.EDIT_BUTTON_SELECT, getSummarySectionButtonState());
+        assertEquals(PaymentRequestSection.EDIT_BUTTON_CHOOSE, getSummarySectionButtonState());
     }
 
     /** Expand the shipping address section, select an address, and click "Pay." */
