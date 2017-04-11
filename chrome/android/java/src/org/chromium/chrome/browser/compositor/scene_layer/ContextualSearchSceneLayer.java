724d36d62785dcbcf93c7f32d23c4a07e7103254
twellington <twellington@chromium.org>
2017-04-11 10:39:53 -0700

    [Contextual Search] Add a handle to the bar when Chrome Home is enabled
    Also increases the height of the bar to match the toolbar.
    
    BUG=706256
    
    Review-Url: https://codereview.chromium.org/2801153003
    Cr-Commit-Position: refs/heads/master@{#463673}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/scene_layer/ContextualSearchSceneLayer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/scene_layer/ContextualSearchSceneLayer.java
@@ -128,6 +128,10 @@ public class ContextualSearchSceneLayer extends SceneOverlayLayer {
         float touchHighlightXOffset = searchBarControl.getTouchHighlightXOffsetPx();
         float touchHighlightWidth = searchBarControl.getTouchHighlightWidthPx();
 
+        int barHandleResId = panel.getBarHandleResourceId();
+        float barHandleOffsetY = panel.getBarHandleOffsetY();
+        float barPaddingBottom = panel.getBarPaddingBottom();
+
         WebContents panelWebContents = panel.getContentViewCore() != null
                 ? panel.getContentViewCore().getWebContents() : null;
 
@@ -153,7 +157,8 @@ public class ContextualSearchSceneLayer extends SceneOverlayLayer {
                 progressBarHeight * mDpToPx, progressBarOpacity, progressBarCompletion,
                 dividerLineVisibilityPercentage, dividerLineWidth, dividerLineHeight,
                 dividerLineColor, dividerLineXOffset, touchHighlightVisible, touchHighlightXOffset,
-                touchHighlightWidth, Profile.getLastUsedProfile());
+                touchHighlightWidth, barHandleResId, barHandleOffsetY, barPaddingBottom,
+                Profile.getLastUsedProfile());
     }
 
     @CalledByNative
@@ -224,5 +229,6 @@ public class ContextualSearchSceneLayer extends SceneOverlayLayer {
             float progressBarHeight, float progressBarOpacity, int progressBarCompletion,
             float dividerLineVisibilityPercentage, float dividerLineWidth, float dividerLineHeight,
             int dividerLineColor, float dividerLineXOffset, boolean touchHighlightVisible,
-            float touchHighlightXOffset, float toucHighlightWidth, Profile profile);
+            float touchHighlightXOffset, float toucHighlightWidth, int barHandleResId,
+            float barHandleOffsetY, float barPaddingBottom, Profile profile);
 }
