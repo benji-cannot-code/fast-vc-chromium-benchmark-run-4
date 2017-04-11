a3a4cf3f8d97f9ac620fec4a7af99d023aaf1472
michaeln <michaeln@chromium.org>
2017-04-10 19:36:20 -0700

    [Quota] Lower quota for ephemeral mode (ie. session only)
    The data for session only origins is ephemeral, it gets deleted at the end of each
    browsing session. This change lowers the storage quota allotted to them.
    
    BUG=619927
    
    Review-Url: https://codereview.chromium.org/2777183010
    Cr-Commit-Position: refs/heads/master@{#463501}

--- storage/browser/quota/quota_settings.cc
+++ storage/browser/quota/quota_settings.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/metrics/histogram_macros.h"
+#include "base/rand_util.h"
 #include "base/sys_info.h"
 
 #define UMA_HISTOGRAM_MBYTES(name, sample)                                     \
@@ -16,16 +17,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace storage {
 
+namespace {
+
+// Skews |value| by +/- |percent|.
+int64_t RandomizeByPercent(int64_t value, int percent) {
+  double random_percent = (base::RandDouble() - 0.5) * percent * 2;
+  return value + (value * (random_percent / 100.0));
+}
+
+}  // anonymous namespace
+
 base::Optional<storage::QuotaSettings> CalculateNominalDynamicSettings(
     const base::FilePath& partition_path,
     bool is_incognito) {
   const int64_t kMBytes = 1024 * 1024;
+  const int kRandomizedPercentage = 10;
 
   if (is_incognito) {
+    // The incognito pool size is a fraction of the amount of system memory,
+    // and the amount is capped to a hard limit.
+    const double kIncognitoPoolSizeRatio = 0.1;  // 10%
+    const int64_t kMaxIncognitoPoolSize = 300 * kMBytes;
+
     storage::QuotaSettings settings;
-    settings.pool_size =
-        std::min(300 * kMBytes, base::SysInfo::AmountOfPhysicalMemory() / 10);
+    settings.pool_size = std::min(
+        RandomizeByPercent(kMaxIncognitoPoolSize, kRandomizedPercentage),
+        static_cast<int64_t>(base::SysInfo::AmountOfPhysicalMemory() *
+                             kIncognitoPoolSizeRatio));
     settings.per_host_quota = settings.pool_size / 3;
+    settings.session_only_per_host_quota = settings.per_host_quota;
     settings.refresh_interval = base::TimeDelta::Max();
     return settings;
   }
@@ -46,6 +66,11 @@ base::Optional<storage::QuotaSettings> CalculateNominalDynamicSettings(
   // Determines the portion of the temp pool that can be
   // utilized by a single host (ie. 5 for 20%).
   const int kPerHostTemporaryPortion = 5;
+
+  // SessionOnly (or ephemeral) origins are allotted a fraction of what
+  // normal origins are provided, and the amount is capped to a hard limit.
+  const double kSessionOnlyHostQuotaRatio = 0.1;  // 10%
+  const int64_t kMaxSessionOnlyHostQuota = 300 * kMBytes;
 
   // os_accomodation is an estimate of how much storage is needed for
   // the os and essential application code outside of the browser.
@@ -82,6 +107,10 @@ base::Optional<storage::QuotaSettings> CalculateNominalDynamicSettings(
   settings.should_remain_available = total * kShouldRemainAvailableRatio;
   settings.must_remain_available = total * kMustRemainAvailableRatio;
   settings.per_host_quota = pool_size / kPerHostTemporaryPortion;
+  settings.session_only_per_host_quota = std::min(
+      RandomizeByPercent(kMaxSessionOnlyHostQuota, kRandomizedPercentage),
+      static_cast<int64_t>(settings.per_host_quota *
+                           kSessionOnlyHostQuotaRatio));
   settings.refresh_interval = base::TimeDelta::FromSeconds(60);
   return settings;
 }
