851113571cf999594e05aedee7273e23a0461ad6
hans <hans@chromium.org>
2017-04-11 13:08:02 -0700

    CSPDirectiveList.cpp: Remove dead code.
    Found by a Clang warning.
    
    BUG=710456
    
    Review-Url: https://codereview.chromium.org/2808263003
    Cr-Commit-Position: refs/heads/master@{#463742}

--- third_party/WebKit/Source/core/frame/csp/CSPDirectiveList.cpp
+++ third_party/WebKit/Source/core/frame/csp/CSPDirectiveList.cpp
@@ -41,11 +41,6 @@ String GetSha256String(const String& content) {
                                   digest.size(), kBase64DoNotInsertLFs);
 }
 
-template <typename CharType>
-inline bool IsASCIIAlphanumericOrHyphen(CharType c) {
-  return IsASCIIAlphanumeric(c) || c == '-';
-}
-
 ContentSecurityPolicyHashAlgorithm ConvertHashAlgorithmToCSPHashAlgorithm(
     HashAlgorithm algorithm) {
   switch (algorithm) {
