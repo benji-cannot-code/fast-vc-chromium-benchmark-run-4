fddad1b9c532c3f0920c2f99a9b41a95c14637f8
hbos <hbos@chromium.org>
2017-04-11 03:05:07 -0700

    Unflag RTCRtpReceiver.
    This exposes RTCRtpReceiver and RTCRtpContributingSource to the web,
    including RTCPeerConnection.getReceivers().
    
    Intent to Ship:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/0aTOUhS0wCY
    
    BUG=703122
    
    Review-Url: https://codereview.chromium.org/2808783002
    Cr-Commit-Position: refs/heads/master@{#463592}

--- chrome/browser/media/webrtc/webrtc_rtp_browsertest.cc
+++ chrome/browser/media/webrtc/webrtc_rtp_browsertest.cc
@@ -20,9 +20,6 @@ class WebRtcRtpBrowserTest : public WebRtcTestBase {
 
   void SetUpCommandLine(base::CommandLine* command_line) override {
     command_line->AppendSwitch(switches::kUseFakeDeviceForMediaStream);
-
-    command_line->AppendSwitchASCII(switches::kEnableBlinkFeatures,
-                                    "RTCRtpReceiver");
   }
 
  protected:
