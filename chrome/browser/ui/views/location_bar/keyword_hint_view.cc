6470e1ce5ed4cc70c65cc5d2be55e17276ed54c3
tapted <tapted@chromium.org>
2017-04-10 22:55:00 -0700

    Guard ui/keyboard include (which is Aura-only) in location_bar/keyword_hint_view.cc
    This regressed in r463089 which added logic to detect when an on-screen
    keyboard is visible.
    
    //ui/keyboard is only added as a c/b/ui dependency with aura.
    
    (macOS does have a native "Keyboard Viewer" window, but it is not a
    functional keyboard replacement.)
    
    BUG=425229
    
    Review-Url: https://codereview.chromium.org/2815523003
    Cr-Commit-Position: refs/heads/master@{#463546}

--- chrome/browser/ui/views/location_bar/keyword_hint_view.cc
+++ chrome/browser/ui/views/location_bar/keyword_hint_view.cc
@@ -18,10 +18,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search_engines/template_url_service.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/color_utils.h"
-#include "ui/keyboard/keyboard_util.h"
 #include "ui/strings/grit/ui_strings.h"
 #include "ui/views/border.h"
 #include "ui/views/controls/label.h"
+
+#if defined(USE_AURA)
+#include "ui/keyboard/keyboard_util.h"
+#endif
 
 namespace {
 
@@ -37,6 +40,14 @@ class KeyboardKeyView : public views::Label {
 
   DISALLOW_COPY_AND_ASSIGN(KeyboardKeyView);
 };
+
+bool IsVirtualKeyboardVisible() {
+#if defined(USE_AURA)
+  return keyboard::IsKeyboardVisible();
+#else
+  return false;
+#endif
+}
 
 KeyboardKeyView::KeyboardKeyView(const gfx::FontList& font_list)
     : views::Label(base::string16(), {font_list}) {
@@ -100,7 +111,7 @@ void KeywordHintView::SetKeyword(const base::string16& keyword) {
   base::string16 short_name(
       url_service->GetKeywordShortName(keyword, &is_extension_keyword));
 
-  if (keyboard::IsKeyboardVisible()) {
+  if (IsVirtualKeyboardVisible()) {
     int message_id = is_extension_keyword
                          ? IDS_OMNIBOX_EXTENSION_KEYWORD_HINT_TOUCH
                          : IDS_OMNIBOX_KEYWORD_HINT_TOUCH;
