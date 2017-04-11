4ab0fc7aa634c055c9da4eac4fc41fe27ab31a2b
reed <reed@google.com>
2017-04-11 07:33:28 -0700

    don't call deprecated copyPixelsTo
    BUG=skia:6465
    
    Review-Url: https://codereview.chromium.org/2812853002
    Cr-Commit-Position: refs/heads/master@{#463622}

--- third_party/WebKit/Source/platform/graphics/ImageFrameGenerator.cpp
+++ third_party/WebKit/Source/platform/graphics/ImageFrameGenerator.cpp
@@ -36,6 +36,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+static void CopyPixels(void* dst_addr,
+                       size_t dst_row_bytes,
+                       const void* src_addr,
+                       size_t src_row_bytes,
+                       const SkImageInfo& info) {
+  size_t row_bytes = info.bytesPerPixel() * info.width();
+  for (int y = 0; y < info.height(); ++y) {
+    memcpy(dst_addr, src_addr, row_bytes);
+    src_addr = static_cast<const char*>(src_addr) + src_row_bytes;
+    dst_addr = static_cast<char*>(dst_addr) + dst_row_bytes;
+  }
+}
+
 static bool CompatibleInfo(const SkImageInfo& src, const SkImageInfo& dst) {
   if (src == dst)
     return true;
@@ -152,7 +165,7 @@ bool ImageFrameGenerator::DecodeAndScale(SegmentReader* data,
   ASSERT(bitmap.height() == scaled_size.height());
   SkAutoLockPixels bitmap_lock(bitmap);
   if (bitmap.getPixels() != pixels)
-    return bitmap.copyPixelsTo(pixels, row_bytes * info.height(), row_bytes);
+    CopyPixels(pixels, row_bytes, bitmap.getPixels(), bitmap.rowBytes(), info);
   return true;
 }
 
