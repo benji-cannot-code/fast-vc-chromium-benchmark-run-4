81a2a7a578217586e4ab3a53ff2e9105e6267124
pkl <pkl@chromium.org>
2017-04-11 02:26:57 -0700

    Removes constant for Google Spaces app
    This is part of the revert of https://chromereviews.googleplex.com/426447013/
    
    This CL depends on https://chromereviews.googleplex.com/578437015 and
    should not be submitted until it has been submitted.
    
    BUG=708607
    TEST=Go to Settings and check that Google Spaces is no longer listed.
    
    Review-Url: https://codereview.chromium.org/2809853002
    Cr-Commit-Position: refs/heads/master@{#463587}

--- ios/chrome/browser/native_app_launcher/ios_appstore_ids.h
+++ ios/chrome/browser/native_app_launcher/ios_appstore_ids.h
@@ -16,7 +16,6 @@ extern const char kIOSAppStoreGooglePlayMusic[];
 extern const char kIOSAppStoreGooglePlus[];
 extern const char kIOSAppStoreGoogleSheets[];
 extern const char kIOSAppStoreGoogleSlides[];
-extern const char kIOSAppStoreSpaces[];
 extern const char kIOSAppStoreTestFlight[];
 extern const char kIOSAppStoreYouTube[];
 
