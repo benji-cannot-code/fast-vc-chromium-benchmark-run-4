5fb189b9e970232fadfe304b47821752683fc96b
chasej <chasej@chromium.org>
2017-04-10 21:03:52 -0700

    Cleanup expected.txt files for layout tests
    Rework the layout tests for origin trials so the expected.txt files are
    no longer needed. This was addressing the harness error about duplicate
    test names. Due to tests being run in multiple workers, the structure
    was reworked to make it easier to create unique test names.
    
    Also, remove the expectation for a flaky test, since the expected.txt
    files were forcing the flaky results (see crbug.com/705953).
    
    This is a precursor so later changes to the layout tests do not require
    changing multiple expected.txt files.
    
    BUG=695123, 705953
    
    Review-Url: https://codereview.chromium.org/2808543002
    Cr-Commit-Position: refs/heads/master@{#463525}

--- third_party/WebKit/LayoutTests/http/tests/origin_trials/resources/disabled-worker.js
+++ third_party/WebKit/LayoutTests/http/tests/origin_trials/resources/disabled-worker.js
@@ -1,25 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-importScripts('/resources/testharness.js');
-
-// Test whether the origin-trial-enabled attributes are *NOT* attached in a
-// worker where the trial is not enabled.
-// This is deliberately just a minimal set of tests to ensure that trials are
-// available in a worker. The full suite of tests are in origin_trials.js.
-test(() => {
-  var testObject = self.internals.originTrialsTest();
-  assert_idl_attribute(testObject, 'throwingAttribute');
-  assert_throws('NotSupportedError', () => {
-    testObject.throwingAttribute;
-  }, 'Accessing attribute should throw error');
-}, 'Accessing attribute should throw error in worker');
-test(() => {
-  var testObject = self.internals.originTrialsTest();
-  assert_not_exists(testObject, 'normalAttribute');
-  assert_equals(testObject.normalAttribute, undefined);
-}, 'Attribute should not exist in worker');
-test(() => {
-  var testObject = self.internals.originTrialsTest();
-  assert_not_exists(testObject, 'CONSTANT');
-  assert_equals(testObject.CONSTANT, undefined);
-}, 'Constant should not exist in worker');
-done();
+importScripts('origintrials-worker.js');
+expect_failure_worker();
