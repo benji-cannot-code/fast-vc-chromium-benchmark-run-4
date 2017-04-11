4291730706925d565a9729b3d969a472f236dae0
jinsukkim <jinsukkim@chromium.org>
2017-04-10 19:56:46 -0700

    Migrate IME state update flow
    Refactored the flow for IME state update so it bypasses CVCImpl
    and go straight from RWHVA -> ImeAdapter native -> Java layer.
    
    Other related changes are:
    
    ImeAdapter provides EventObserver to reduce the dependency on CVC,
    based on the suggestion made in https://goo.gl/pdtQCl. It is used
    by an embedder (Chrome) and CVC to deal with IME notification.
    This replaces IME state update done through CVC. Only the necessary
    info (node editability, password attribute) are passed.
    
    ImeAdapter.ImeAdapterDelegate is to delegate some work upon IME
    events. But it is used by ContentViewCore only, and there is no
    clear benefit of having the interface. It was removed for
    simplification. All the stuff can be (and are now) handled inside
    ImeAdapter.
    
    BUG=662908,626765,620172, 709349
    
    Review-Url: https://codereview.chromium.org/2777223004
    Cr-Original-Commit-Position: refs/heads/master@{#462419}
    Committed: https://chromium.googlesource.com/chromium/src/+/cc9e77e7d9e05da0f6017f1bdea024f0ce669786
    Review-Url: https://codereview.chromium.org/2777223004
    Cr-Commit-Position: refs/heads/master@{#463508}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwContentsGarbageCollectionTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentsGarbageCollectionTest.java
@@ -133,7 +133,9 @@ public class AwContentsGarbageCollectionTest extends AwTestBase {
             resultReceivers[i] = runTestOnUiThreadAndGetResult(new Callable<ResultReceiver>() {
                 @Override
                 public ResultReceiver call() throws Exception {
-                    return containerView.getContentViewCore().getNewShowKeyboardReceiver();
+                    return containerView.getContentViewCore()
+                            .getImeAdapterForTest()
+                            .getNewShowKeyboardReceiver();
                 }
             });
         }
@@ -274,4 +276,3 @@ public class AwContentsGarbageCollectionTest extends AwTestBase {
         assertTrue(criteria.isSatisfied());
     }
 }
-
