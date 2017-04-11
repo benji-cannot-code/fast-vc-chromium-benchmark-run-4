7c1059ed9d77d2bc4d986ec89babd48be0b4a512
bsazonov <bsazonov@chromium.org>
2017-04-11 09:12:22 -0700

    Fix duplicated records of Signin_Impression_FromSettings action
    This CL changes conditions for recording Signin_Impression_FromSettings user
    action.
    Before: user action is recorded on every call to update() if user
    isn't signed in and policy allows sign in.
    After: subsequent calls to update() don't result in new records if preference
    was already in "not signed in" state.
    
    BUG=697572
    
    Review-Url: https://codereview.chromium.org/2800343003
    Cr-Commit-Position: refs/heads/master@{#463645}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/SignInPreference.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/SignInPreference.java
@@ -39,6 +39,7 @@ public class SignInPreference extends Preference
         implements SignInAllowedObserver, ProfileDownloader.Observer,
                    AndroidSyncSettings.AndroidSyncSettingsObserver, SyncStateChangedListener {
     private boolean mViewEnabled;
+    private boolean mShowingPromo;
 
     /**
      * Constructor for inflating from XML.
@@ -85,10 +86,18 @@ public class SignInPreference extends Preference
         String accountName = ChromeSigninController.get().getSignedInAccountName();
         if (SigninManager.get(getContext()).isSigninDisabledByPolicy()) {
             setupSigninDisabled();
+            mShowingPromo = false;
         } else if (accountName == null) {
             setupNotSignedIn();
+
+            if (!mShowingPromo) {
+                // This user action should be recorded when message with sign-in prompt is shown
+                RecordUserAction.record("Signin_Impression_FromSettings");
+            }
+            mShowingPromo = true;
         } else {
             setupSignedIn(accountName);
+            mShowingPromo = false;
         }
 
         setOnPreferenceClickListener(new OnPreferenceClickListener() {
@@ -98,10 +107,6 @@ public class SignInPreference extends Preference
                         getContext(), SigninAccessPoint.SETTINGS);
             }
         });
-
-        if (accountName == null && SigninManager.get(getContext()).isSignInAllowed()) {
-            RecordUserAction.record("Signin_Impression_FromSettings");
-        }
     }
 
     private void setupSigninDisabled() {
