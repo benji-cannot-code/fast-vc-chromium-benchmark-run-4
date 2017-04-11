1c9fc8d06c729d11b9f4910d7bac0c23cab61f3f
raymes <raymes@chromium.org>
2017-04-10 22:16:32 -0700

    Remove c/b/media/webrtc/media_permission.{h,cc}
    This class is now just basically a wrapper around PermissionManager. Remove it
    and inline the calls to PermissionManager.
    
    BUG=596786
    
    Review-Url: https://codereview.chromium.org/2759593002
    Cr-Commit-Position: refs/heads/master@{#463541}

--- chrome/browser/media/webrtc/media_stream_devices_controller.cc
+++ chrome/browser/media/webrtc/media_stream_devices_controller.cc
@@ -16,9 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/content_settings/tab_specific_content_settings.h"
 #include "chrome/browser/media/webrtc/media_capture_devices_dispatcher.h"
-#include "chrome/browser/media/webrtc/media_permission.h"
 #include "chrome/browser/media/webrtc/media_stream_capture_indicator.h"
 #include "chrome/browser/media/webrtc/media_stream_device_permissions.h"
+#include "chrome/browser/permissions/permission_manager.h"
+#include "chrome/browser/permissions/permission_result.h"
 #include "chrome/browser/permissions/permission_uma_util.h"
 #include "chrome/browser/permissions/permission_util.h"
 #include "chrome/browser/profiles/profile.h"
@@ -719,6 +720,7 @@ ContentSetting MediaStreamDevicesController::GetContentSetting(
     content::MediaStreamRequestResult* denial_reason) const {
   DCHECK(content_type == CONTENT_SETTINGS_TYPE_MEDIASTREAM_MIC ||
          content_type == CONTENT_SETTINGS_TYPE_MEDIASTREAM_CAMERA);
+  DCHECK(!request_.security_origin.is_empty());
   DCHECK(content::IsOriginSecure(request_.security_origin) ||
          request_.request_type == content::MEDIA_OPEN_DEVICE_PEPPER_ONLY);
   if (!was_requested) {
@@ -736,10 +738,17 @@ ContentSetting MediaStreamDevicesController::GetContentSetting(
     return CONTENT_SETTING_BLOCK;
   }
 
-  MediaPermission permission(content_type, request.security_origin,
-                             web_contents_->GetLastCommittedURL().GetOrigin(),
-                             profile_, web_contents_);
-  return permission.GetPermissionStatus(denial_reason);
+  PermissionResult result =
+      PermissionManager::Get(profile_)->GetPermissionStatus(
+          content_type, request.security_origin,
+          web_contents_->GetLastCommittedURL().GetOrigin());
+  if (result.content_setting == CONTENT_SETTING_BLOCK) {
+    *denial_reason = (result.source == PermissionStatusSource::KILL_SWITCH)
+                         ? content::MEDIA_DEVICE_KILL_SWITCH_ON
+                         : content::MEDIA_DEVICE_PERMISSION_DENIED;
+  }
+
+  return result.content_setting;
 }
 
 ContentSetting MediaStreamDevicesController::GetNewSetting(
