3a16a62a910098026c8a2bed2e8b17e179e87339
bugsnash <bugsnash@chromium.org>
2017-04-11 00:30:38 -0700

    Removed local RefPtr objects created from PassRefPtr arguments.
    Removed local RefPtr objects created from PassRefPtr arguments and
    converted the PassRefPtr arguments to RefPtr.
    
    This patch deals with one specific corner case of copying PassRefPtr
    objects to RefPtr objects, where PassRefPtr arguments are copied into
    local RefPtr objects to be used throughout the function.
    
    All instances of PassRefPtr objects being copied into RefPtr objects
    need to be wrapped in std::move to avoid introducing ref churn in
    future changes when all PassRefPtr objects will be replaced with RefPtr
    objects. In these cases of creating a local RefPtr just to copy the
    PassRefPtr argument into, a std::move wrap doesn't make sense as it
    would result in a final state where a RefPtr argument is moved into a
    local RefPtr for no reason. So to handle these cases I have converted
    the PassRefPtr arguments to RefPtr in advance, and removed the local
    RefPtr argumen, instead using the passed RefPtr throughout the
    methods. In most cases the RefPtr argument name has been changed to
    match the previousf name of the local RefPtr.
    
    The syntax of the local RefPtr variables have also been changed to
    use std::move wraps instead of calling RefPtr::Release in cases
    where the RefPtr is no longer needed locally after passing.
    Note that std::move wraps have been introduced in some cases where
    RefPtr::Release() was not being called but should have been.
    
    BUG=494719
    
    Review-Url: https://codereview.chromium.org/2807073002
    Cr-Commit-Position: refs/heads/master@{#463565}

--- third_party/WebKit/Source/core/loader/WorkerThreadableLoader.h
+++ third_party/WebKit/Source/core/loader/WorkerThreadableLoader.h
@@ -142,7 +142,7 @@ class WorkerThreadableLoader final : public ThreadableLoader {
 
    public:
     static void CreateAndStart(WorkerThreadableLoader*,
-                               PassRefPtr<WorkerLoaderProxy>,
+                               RefPtr<WorkerLoaderProxy>,
                                WorkerThreadLifecycleContext*,
                                std::unique_ptr<CrossThreadResourceRequestData>,
                                const ThreadableLoaderOptions&,
--- third_party/WebKit/Source/platform/loader/fetch/CrossOriginAccessControl.cpp
+++ third_party/WebKit/Source/platform/loader/fetch/CrossOriginAccessControl.cpp
@@ -473,7 +473,7 @@ void CrossOriginAccessControl::RedirectErrorString(
 }
 
 bool CrossOriginAccessControl::HandleRedirect(
-    PassRefPtr<SecurityOrigin> security_origin,
+    RefPtr<SecurityOrigin> current_security_origin,
     ResourceRequest& new_request,
     const ResourceResponse& redirect_response,
     StoredCredentials with_credentials,
@@ -482,8 +482,6 @@ bool CrossOriginAccessControl::HandleRedirect(
   // http://www.w3.org/TR/cors/#redirect-steps terminology:
   const KURL& last_url = redirect_response.Url();
   const KURL& new_url = new_request.Url();
-
-  RefPtr<SecurityOrigin> current_security_origin = security_origin;
 
   RefPtr<SecurityOrigin> new_security_origin = current_security_origin;
 
