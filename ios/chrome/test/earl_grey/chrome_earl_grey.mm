734af7583adf8a4e2826d2e73cecd1c93694fb1a
liaoyuke <liaoyuke@chromium.org>
2017-04-12 09:06:34 -0700

    [iOS] Re-factoring FormsTestCase.
    This CL re-factors FormsTestCase by doing the following things:
    
    1. Add necessary wait condition after form submission and resend
    warning dialog confirmation to make the tests more robust.
    
    2. Remove private helper methods that can be replaced by public helpers.
    
    3. De-couple redirect test from other tests so that other tests do not
    need to depend on TestFormRedirectResponseProvider.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2807323002
    Cr-Commit-Position: refs/heads/master@{#464044}

--- ios/chrome/test/earl_grey/chrome_earl_grey.mm
+++ ios/chrome/test/earl_grey/chrome_earl_grey.mm
@@ -9,9 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebKit.h>
 
 #include "base/mac/foundation_util.h"
+#include "base/mac/scoped_nsobject.h"
 #include "base/strings/sys_string_conversions.h"
 #import "base/test/ios/wait_util.h"
 #include "components/strings/grit/components_strings.h"
+#import "ios/chrome/browser/ui/commands/generic_chrome_command.h"
+#include "ios/chrome/browser/ui/commands/ios_command_ids.h"
 #import "ios/chrome/browser/ui/static_content/static_html_view_controller.h"
 #import "ios/chrome/test/app/chrome_test_util.h"
 #import "ios/chrome/test/app/history_test_util.h"
@@ -111,6 +114,27 @@ id ExecuteJavaScript(NSString* javascript,
   web::WebState* webState = chrome_test_util::GetCurrentWebState();
   if (webState->ContentIsHTML())
     web::WaitUntilWindowIdInjected(webState);
+}
+
++ (void)reload {
+  base::scoped_nsobject<GenericChromeCommand> reloadCommand(
+      [[GenericChromeCommand alloc] initWithTag:IDC_RELOAD]);
+  chrome_test_util::RunCommandWithActiveViewController(reloadCommand);
+  [ChromeEarlGrey waitForPageToFinishLoading];
+}
+
++ (void)goBack {
+  base::scoped_nsobject<GenericChromeCommand> reloadCommand(
+      [[GenericChromeCommand alloc] initWithTag:IDC_BACK]);
+  chrome_test_util::RunCommandWithActiveViewController(reloadCommand);
+  [ChromeEarlGrey waitForPageToFinishLoading];
+}
+
++ (void)goForward {
+  base::scoped_nsobject<GenericChromeCommand> reloadCommand(
+      [[GenericChromeCommand alloc] initWithTag:IDC_FORWARD]);
+  chrome_test_util::RunCommandWithActiveViewController(reloadCommand);
+  [ChromeEarlGrey waitForPageToFinishLoading];
 }
 
 + (void)waitForPageToFinishLoading {
