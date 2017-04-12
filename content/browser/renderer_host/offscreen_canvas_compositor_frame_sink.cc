fce2851dae20778a3fea65ff461332bc79da0f79
staraz <staraz@chromium.org>
2017-04-11 17:13:53 -0700

    Move Work From CompositorFrameSinkSupport() To Init()
    SurfaceManager::RegisterSurfaceFactoryClient calls
    SurfaceFactoryClient::SetBeginFrameSource() which is a virtual method. Calling
    virtual methods on an object during its construction in the constructor is against the
    style guide.
    
    Call to RegisterFrameSinkId and RegisterSurfaceFactoryClient is moved into
    Init(). Init() is called right after constructing CompositorFrameSinkSupport
    everywhere.
    
    BUG=707105
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2807653003
    Cr-Commit-Position: refs/heads/master@{#463853}

--- content/browser/renderer_host/offscreen_canvas_compositor_frame_sink.cc
+++ content/browser/renderer_host/offscreen_canvas_compositor_frame_sink.cc
@@ -19,12 +19,13 @@ OffscreenCanvasCompositorFrameSink::OffscreenCanvasCompositorFrameSink(
     cc::mojom::MojoCompositorFrameSinkRequest request,
     cc::mojom::MojoCompositorFrameSinkClientPtr client)
     : provider_(provider),
-      support_(this,
-               provider->GetSurfaceManager(),
-               frame_sink_id,
-               false /* is_root */,
-               true /* handles_frame_sink_id_invalidation */,
-               true /* needs_sync_points */),
+      support_(cc::CompositorFrameSinkSupport::Create(
+          this,
+          provider->GetSurfaceManager(),
+          frame_sink_id,
+          false /* is_root */,
+          true /* handles_frame_sink_id_invalidation */,
+          true /* needs_sync_points */)),
       client_(std::move(client)),
       binding_(this, std::move(request)) {
   binding_.set_connection_error_handler(
@@ -33,12 +34,12 @@ OffscreenCanvasCompositorFrameSink::OffscreenCanvasCompositorFrameSink(
 }
 
 OffscreenCanvasCompositorFrameSink::~OffscreenCanvasCompositorFrameSink() {
-  provider_->OnCompositorFrameSinkClientDestroyed(support_.frame_sink_id());
+  provider_->OnCompositorFrameSinkClientDestroyed(support_->frame_sink_id());
 }
 
 void OffscreenCanvasCompositorFrameSink::SetNeedsBeginFrame(
     bool needs_begin_frame) {
-  support_.SetNeedsBeginFrame(needs_begin_frame);
+  support_->SetNeedsBeginFrame(needs_begin_frame);
 }
 
 void OffscreenCanvasCompositorFrameSink::SubmitCompositorFrame(
@@ -46,16 +47,16 @@ void OffscreenCanvasCompositorFrameSink::SubmitCompositorFrame(
     cc::CompositorFrame frame) {
   // TODO(samans): This will need to do something similar to
   // GpuCompositorFrameSink.
-  support_.SubmitCompositorFrame(local_surface_id, std::move(frame));
+  support_->SubmitCompositorFrame(local_surface_id, std::move(frame));
 }
 
 void OffscreenCanvasCompositorFrameSink::BeginFrameDidNotSwap(
     const cc::BeginFrameAck& begin_frame_ack) {
-  support_.BeginFrameDidNotSwap(begin_frame_ack);
+  support_->BeginFrameDidNotSwap(begin_frame_ack);
 }
 
 void OffscreenCanvasCompositorFrameSink::EvictFrame() {
-  support_.EvictFrame();
+  support_->EvictFrame();
 }
 
 void OffscreenCanvasCompositorFrameSink::DidReceiveCompositorFrameAck(
@@ -82,7 +83,7 @@ void OffscreenCanvasCompositorFrameSink::WillDrawSurface(
 
 void OffscreenCanvasCompositorFrameSink::OnClientConnectionLost() {
   provider_->OnCompositorFrameSinkClientConnectionLost(
-      support_.frame_sink_id());
+      support_->frame_sink_id());
 }
 
 }  // namespace content
