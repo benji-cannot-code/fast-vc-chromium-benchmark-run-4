ff8074addcb9e187d422f8080223e1835f12bd1a
liberato <liberato@chromium.org>
2017-04-12 00:30:15 -0700

    Add overlay pointer to AndroidOverlay callbacks.
    AndroidOverlay provides ready / failed / destroyed callbacks to the
    application code.  This CL adds an argument to these callbacks which
    tells the application which overlay is calling back.
    
    BUG=
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2813013002
    Cr-Commit-Position: refs/heads/master@{#463941}

--- media/gpu/android_video_decode_accelerator.cc
+++ media/gpu/android_video_decode_accelerator.cc
@@ -420,7 +420,7 @@ void AndroidVideoDecodeAccelerator::StartSurfaceCreation() {
   InitializePictureBufferManager();
 }
 
-void AndroidVideoDecodeAccelerator::OnOverlayReady() {
+void AndroidVideoDecodeAccelerator::OnOverlayReady(AndroidOverlay* overlay) {
   DCHECK(!defer_surface_creation_);
   DCHECK_EQ(state_, WAITING_FOR_SURFACE);
   DCHECK(incoming_bundle_);
@@ -428,7 +428,7 @@ void AndroidVideoDecodeAccelerator::OnOverlayReady() {
   InitializePictureBufferManager();
 }
 
-void AndroidVideoDecodeAccelerator::OnOverlayFailed() {
+void AndroidVideoDecodeAccelerator::OnOverlayFailed(AndroidOverlay* overlay) {
   NOTIFY_ERROR(PLATFORM_FAILURE, "Surface is not available");
 }
 
@@ -1293,7 +1293,8 @@ AndroidVideoDecodeAccelerator::GetGlDecoder() const {
   return get_gles2_decoder_cb_.Run();
 }
 
-void AndroidVideoDecodeAccelerator::OnSurfaceDestroyed() {
+void AndroidVideoDecodeAccelerator::OnSurfaceDestroyed(
+    AndroidOverlay* overlay) {
   DVLOG(1) << __func__;
   TRACE_EVENT0("media", "AVDA::OnSurfaceDestroyed");
   DCHECK(thread_checker_.CalledOnValidThread());
