9e7bbc2fcd1d66852bddbcd6987b6412e9a308de
yusufo <yusufo@chromium.org>
2017-04-11 17:25:30 -0700

    Unregister from network changes on destroy in ContextualSearchTabHelper
    This causes us to leak ChromeActivity instances and all the views
    associated with them. In general we should always make sure to
    unregister notifiers from application level singletons on destroy.
    
    BUG=710612
    
    Review-Url: https://codereview.chromium.org/2812143002
    Cr-Commit-Position: refs/heads/master@{#463855}

--- chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchTabHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchTabHelper.java
@@ -119,6 +119,9 @@ public class ContextualSearchTabHelper
         if (mTemplateUrlObserver != null) {
             TemplateUrlService.getInstance().removeObserver(mTemplateUrlObserver);
         }
+        if (NetworkChangeNotifier.isInitialized()) {
+            NetworkChangeNotifier.removeConnectionTypeObserver(this);
+        }
         removeContextualSearchHooks(mBaseContentViewCore);
         mBaseContentViewCore = null;
     }
