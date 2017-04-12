f8f496a1d30c320a1a60fa32c4b8f11155ce99c9
sammc <sammc@chromium.org>
2017-04-12 16:47:53 -0700

    Pref service: Batch pref updates.
    BUG=654988
    
    Review-Url: https://codereview.chromium.org/2791893002
    Cr-Commit-Position: refs/heads/master@{#464209}

--- services/preferences/persistent_pref_store_impl.h
+++ services/preferences/persistent_pref_store_impl.h
@@ -15,10 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/preferences/public/interfaces/preferences.mojom.h"
 #include "services/preferences/public/interfaces/tracked_preference_validation_delegate.mojom.h"
 
-namespace base {
-class Value;
-}
-
 namespace prefs {
 
 class PersistentPrefStoreImpl : public PrefStore::Observer {
@@ -41,9 +37,7 @@ class PersistentPrefStoreImpl : public PrefStore::Observer {
  private:
   class Connection;
 
-  void SetValue(const std::string& key,
-                std::unique_ptr<base::Value> value,
-                uint32_t flags);
+  void SetValues(std::vector<mojom::PrefUpdatePtr> updates);
 
   void CommitPendingWrite();
   void SchedulePendingLossyWrites();
