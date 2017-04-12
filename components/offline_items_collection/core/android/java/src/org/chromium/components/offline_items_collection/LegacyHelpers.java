aecb06cf46ce217572771a4a84899e281369ccba
dtrainor <dtrainor@chromium.org>
2017-04-11 20:40:28 -0700

    Fix LegacyHelpers null pointer access
    DownloadNotificationService was calling the LegacyHelpers API with a
    null ContentId.  Fix the call sites so that they only call into the API
    with a valid ContentId.
    
    BUG=707710
    
    Review-Url: https://codereview.chromium.org/2810583004
    Cr-Commit-Position: refs/heads/master@{#463922}

--- components/offline_items_collection/core/android/java/src/org/chromium/components/offline_items_collection/LegacyHelpers.java
+++ components/offline_items_collection/core/android/java/src/org/chromium/components/offline_items_collection/LegacyHelpers.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.offline_items_collection;
 
+import android.support.annotation.Nullable;
 import android.text.TextUtils;
 
 /**
@@ -35,8 +36,8 @@ public class LegacyHelpers {
      * @param id The {@link ContentId} to inspect.
      * @return   Whether or not {@code id} was built for a traditional download.
      */
-    public static boolean isLegacyDownload(ContentId id) {
-        return TextUtils.equals(LEGACY_DOWNLOAD_NAMESPACE, id.namespace);
+    public static boolean isLegacyDownload(@Nullable ContentId id) {
+        return id != null && TextUtils.equals(LEGACY_DOWNLOAD_NAMESPACE, id.namespace);
     }
 
     /**
@@ -46,8 +47,8 @@ public class LegacyHelpers {
      * @param id The {@link ContentId} to inspect.
      * @return   Whether or not {@code id} was built for a traditional offline page.
      */
-    public static boolean isLegacyOfflinePage(ContentId id) {
-        return TextUtils.equals(LEGACY_OFFLINE_PAGE_NAMESPACE, id.namespace);
+    public static boolean isLegacyOfflinePage(@Nullable ContentId id) {
+        return id != null && TextUtils.equals(LEGACY_OFFLINE_PAGE_NAMESPACE, id.namespace);
     }
 
     private LegacyHelpers() {}
