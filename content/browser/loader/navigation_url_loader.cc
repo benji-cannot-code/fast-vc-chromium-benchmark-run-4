8c9331fb740ee39b808b37ae85f0ab0fa6b8490a
scottmg <scottmg@chromium.org>
2017-04-12 09:47:28 -0700

    Beginnings of PlzNavigate navigation using URLLoader in network service
    Instead getting ResourceDispatcherHostImpl to start the request, this is
    the beginning of using the network service's URLLoader to do the navigation
    request.
    
    BUG=598073
    TEST=manual run of content_shell --enable-browser-side-navigation --enable-network-service
    
    Review-Url: https://codereview.chromium.org/2812853004
    Cr-Commit-Position: refs/heads/master@{#464055}

--- content/browser/loader/navigation_url_loader.cc
+++ content/browser/loader/navigation_url_loader.cc
@@ -7,10 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/command_line.h"
 #include "content/browser/frame_host/navigation_request_info.h"
 #include "content/browser/loader/navigation_url_loader_factory.h"
 #include "content/browser/loader/navigation_url_loader_impl.h"
+#include "content/browser/loader/navigation_url_loader_network_service.h"
 #include "content/public/browser/navigation_ui_data.h"
+#include "content/public/common/content_switches.h"
 
 namespace content {
 
@@ -29,10 +32,19 @@ std::unique_ptr<NavigationURLLoader> NavigationURLLoader::Create(
         resource_context, storage_partition, std::move(request_info),
         std::move(navigation_ui_data), service_worker_handle, delegate);
   }
-  return std::unique_ptr<NavigationURLLoader>(new NavigationURLLoaderImpl(
-      resource_context, storage_partition, std::move(request_info),
-      std::move(navigation_ui_data), service_worker_handle, appcache_handle,
-      delegate));
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kEnableNetworkService)) {
+    return std::unique_ptr<NavigationURLLoader>(
+        new NavigationURLLoaderNetworkService(
+            resource_context, storage_partition, std::move(request_info),
+            std::move(navigation_ui_data), service_worker_handle,
+            appcache_handle, delegate));
+  } else {
+    return std::unique_ptr<NavigationURLLoader>(new NavigationURLLoaderImpl(
+        resource_context, storage_partition, std::move(request_info),
+        std::move(navigation_ui_data), service_worker_handle, appcache_handle,
+        delegate));
+  }
 }
 
 void NavigationURLLoader::SetFactoryForTesting(
