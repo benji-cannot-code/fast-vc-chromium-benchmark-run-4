c614600a76a9280af526c242a2891c0b72fc840e
Jeff Carpenter <jeffcarp@chromium.org>
2017-04-12 12:40:31 -0700

    Query exportable Gerrit in-flight CLs and print them out
    This is in preparation for creating PRs for Gerrit CLs.
    
    Bug: 700092
    Change-Id: I6300565986a90e4e9e9e6c834ed91b52864af0fb
    Reviewed-on: https://chromium-review.googlesource.com/475205
    Commit-Queue: Jeff Carpenter <jeffcarp@chromium.org>
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#464513}

---
+++ third_party/WebKit/Tools/Scripts/webkitpy/w3c/gerrit_mock.py
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+class MockGerrit(object):
+
+    def __init__(self, host, user, token):
+        self.host = host
+        self.user = user
+        self.token = token
+
+    def query_open_cls(self):
+        return []
