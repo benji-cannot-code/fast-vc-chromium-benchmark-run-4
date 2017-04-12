42e7f62729dfc4d6ffcda8a4b91d8a627d52d0ea
joelhockey <joelhockey@chromium.org>
2017-04-12 00:58:14 -0700

    Revert of Remove unneeded calls to plugin.SetParentVisible. (patchset #3 id:40001 of https://codereview.chromium.org/2808723002/ )
    Reason for revert:
    Although no tests failed from this change, and it seems that pepper plugins do nothing for visibility changes:
    https://cs.chromium.org/chromium/src/content/renderer/pepper/pepper_webplugin_impl.cc?l=206&rcl=0cc4c62a163318c06636916b7e2b2c6a56f16b74
    
    I think that this is still important for browser_plugin:
    https://cs.chromium.org/chromium/src/content/renderer/browser_plugin/browser_plugin.cc?l=427&rcl=b6dc62f53d4c3b14c0f541897f8d2970be89b84b
    
    Original issue's description:
    > Remove unneeded calls to plugin.SetParentVisible.
    >
    > Plugin (WebPluginContainerImpl) does not keep its own state about
    > visibility of parent, and once it is no longer inheriting from
    > FrameViewBase, it will not implement any ParentVisible methods.
    > If required, it could always call parent_.IsVisible.
    >
    > BUG=637460
    >
    > Review-Url: https://codereview.chromium.org/2808723002
    > Cr-Commit-Position: refs/heads/master@{#463409}
    > Committed: https://chromium.googlesource.com/chromium/src/+/e62eb16ec0d97fe0b59d39e3c5f9c0669e6d9cff
    
    TBR=dcheng@chromium.org,haraken@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=637460
    
    Review-Url: https://codereview.chromium.org/2809913004
    Cr-Commit-Position: refs/heads/master@{#463953}

--- third_party/WebKit/Source/core/frame/FrameView.cpp
+++ third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -4741,6 +4741,9 @@ void FrameView::SetParentVisible(bool visible) {
 
   for (const auto& child : children_)
     child->SetParentVisible(visible);
+
+  for (const auto& plugin : plugins_)
+    plugin->SetParentVisible(visible);
 }
 
 void FrameView::Show() {
@@ -4761,6 +4764,9 @@ void FrameView::Show() {
     if (IsParentVisible()) {
       for (const auto& child : children_)
         child->SetParentVisible(true);
+
+      for (const auto& plugin : plugins_)
+        plugin->SetParentVisible(true);
     }
   }
 
@@ -4772,6 +4778,9 @@ void FrameView::Hide() {
     if (IsParentVisible()) {
       for (const auto& child : children_)
         child->SetParentVisible(false);
+
+      for (const auto& plugin : plugins_)
+        plugin->SetParentVisible(false);
     }
     SetSelfVisible(false);
     if (ScrollingCoordinator* scrolling_coordinator =
