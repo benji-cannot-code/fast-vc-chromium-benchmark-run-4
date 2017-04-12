fd1ccf4ae080f987c29e96d7adf7cfda5aacd4d1
gsennton <gsennton@chromium.org>
2017-04-12 14:00:15 -0700

    [WebView] Ensure app context is initialized in minidump uploader process
    The copying and uploading of WebView minidumps happens in a separate
    process. Currently, the uploading-part of that process initializes the
    application context correctly, but the copying-part does not.
    Ensure that ContextUtils.sApplicationContext is initialized in both the
    copying- and the uploading-part of the minidump uploading code.
    
    BUG=710796
    
    Review-Url: https://codereview.chromium.org/2817693002
    Cr-Commit-Position: refs/heads/master@{#464150}

--- android_webview/java/src/org/chromium/android_webview/crash/CrashReceiverService.java
+++ android_webview/java/src/org/chromium/android_webview/crash/CrashReceiverService.java
@@ -41,6 +41,8 @@ public class CrashReceiverService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ContextUtils.initApplicationContext(this.getApplicationContext());
     }
 
     private final ICrashReceiverService.Stub mBinder = new ICrashReceiverService.Stub() {
