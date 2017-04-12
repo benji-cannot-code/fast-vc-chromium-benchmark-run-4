8d2cc52a2a7e515f0fe438ac1e26b27c9b38027b
pmarko <pmarko@chromium.org>
2017-04-12 16:24:24 -0700

    Login screen IME policy does not apply in user session
    This CL fixes issues with the DeviceLoginScreenInputMethods policy:
    + SigninScreenHandler is now only enforcing input methods on if the
      sign-in screen is visible. This prevents a SigninScreenHandler
      instance which exists during the user session from enforcing input
      methods in the user session.
    + SigninScreenHandler re-enforces input methods on the "addUser" event,
      because if only one user pod is present on the sign-in screen,
      pressing addUser does not de-focus the user pod before switching to
      the gaia screen.
    + Prefer policy-allowed input methods on the gaia screen ("Add Person")
    
    BUG=373324,709313
    TEST=Manual test && browser_tests --gtest_filter=LoginScreen*
    
    Review-Url: https://codereview.chromium.org/2816543004
    Cr-Commit-Position: refs/heads/master@{#464204}

--- chrome/browser/chromeos/login/login_screen_policy_browsertest.cc
+++ chrome/browser/chromeos/login/login_screen_policy_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/chromeos/login/startup_utils.h"
 #include "chrome/browser/chromeos/policy/device_policy_cros_browser_test.h"
 #include "chrome/browser/chromeos/policy/proto/chrome_device_policy.pb.h"
@@ -17,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/chromeos_switches.h"
 #include "chromeos/settings/cros_settings_names.h"
 #include "components/user_manager/user_manager.h"
+#include "content/public/browser/notification_service.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/ime/chromeos/input_method_manager.h"
@@ -76,6 +78,11 @@ IN_PROC_BROWSER_TEST_F(LoginScreenPolicyTest, DisableSupervisedUsers) {
 }
 
 IN_PROC_BROWSER_TEST_F(LoginScreenPolicyTest, RestrictInputMethods) {
+  content::WindowedNotificationObserver(
+      chrome::NOTIFICATION_LOGIN_OR_LOCK_WEBUI_VISIBLE,
+      content::NotificationService::AllSources())
+      .Wait();
+
   input_method::InputMethodManager* imm =
       input_method::InputMethodManager::Get();
   ASSERT_TRUE(imm);
