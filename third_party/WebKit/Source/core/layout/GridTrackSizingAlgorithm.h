d6b68dccc1d94d6aaa4f9c34542e8c2dcaffc38f
rego <rego@igalia.com>
2017-04-12 03:04:26 -0700

    [css-grid] Remove unused method GridTrackSizingAlgorithm::IsTrackSizingOperation()
    Review-Url: https://codereview.chromium.org/2815033003
    Cr-Commit-Position: refs/heads/master@{#463980}

--- third_party/WebKit/Source/core/layout/GridTrackSizingAlgorithm.h
+++ third_party/WebKit/Source/core/layout/GridTrackSizingAlgorithm.h
@@ -111,9 +111,6 @@ class GridTrackSizingAlgorithm final {
 
 #if DCHECK_IS_ON()
   bool TracksAreWiderThanMinTrackBreadth() const;
-  bool IsTrackSizingOperation() const {
-    return sizing_operation_ == kTrackSizing;
-  };
 #endif
 
  private:
