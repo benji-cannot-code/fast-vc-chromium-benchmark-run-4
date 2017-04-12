3e29d7256d796f434c765bc4a9ef386f5686cf0c
lazyboy <lazyboy@chromium.org>
2017-04-12 09:25:43 -0700

    Make all ComplexFeatures with single feature to be SimpleFeature.
    Change DCHECK in ComplexFeature to ensure it has > 1 features.
    
    BUG=710718
    Test=Compile & run chrome.
    
    Review-Url: https://codereview.chromium.org/2813073003
    Cr-Commit-Position: refs/heads/master@{#464051}

--- extensions/common/features/complex_feature.cc
+++ extensions/common/features/complex_feature.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace extensions {
 
 ComplexFeature::ComplexFeature(std::vector<Feature*>* features) {
-  DCHECK_GT(features->size(), 0UL);
+  DCHECK_GT(features->size(), 1UL);
   for (Feature* f : *features)
     features_.push_back(std::unique_ptr<Feature>(f));
   features->clear();
