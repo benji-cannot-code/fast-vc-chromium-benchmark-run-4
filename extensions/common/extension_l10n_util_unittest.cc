fa9518bb7dc582300d5a24b988344798010f7c42
lazyboy <lazyboy@chromium.org>
2017-04-11 19:12:48 -0700

    Fix some implicit int->bool conversion in base::WriteFile usage.
    I've noticed this while writing
    https://codereview.chromium.org/2776873002/ that a lots of test code
    are treating the return value of WriteFile as bool to
    identify whether write succeeded.
    This is incorrect in the case where we ask WriteFile to write 0
    bytes, WriteFile will return 0 and that is not a write failure.
    
    The return value of -1 signifies write failure. So update codebase
    to use that instead.
    
    BUG=None
    Test=None, internal cleanup.
    
    Review-Url: https://codereview.chromium.org/2786813002
    Cr-Commit-Position: refs/heads/master@{#463898}

--- extensions/common/extension_l10n_util_unittest.cc
+++ extensions/common/extension_l10n_util_unittest.cc
@@ -40,7 +40,8 @@ TEST(ExtensionL10nUtil, ValidateLocalesWithBadLocale) {
 
   base::FilePath messages_file = locale.Append(kMessagesFilename);
   std::string data = "{ \"name\":";
-  ASSERT_TRUE(base::WriteFile(messages_file, data.c_str(), data.length()));
+  ASSERT_EQ(static_cast<int>(data.length()),
+            base::WriteFile(messages_file, data.c_str(), data.length()));
 
   base::DictionaryValue manifest;
   manifest.SetString(keys::kDefaultLocale, "en");
@@ -93,8 +94,9 @@ TEST(ExtensionL10nUtil, GetValidLocalesWithUnsupportedLocale) {
   base::FilePath locale_1 = src_path.AppendASCII("sr");
   ASSERT_TRUE(base::CreateDirectory(locale_1));
   std::string data("whatever");
-  ASSERT_TRUE(base::WriteFile(
-      locale_1.Append(kMessagesFilename), data.c_str(), data.length()));
+  ASSERT_EQ(static_cast<int>(data.length()),
+            base::WriteFile(locale_1.Append(kMessagesFilename), data.c_str(),
+                            data.length()));
   // Unsupported locale.
   ASSERT_TRUE(base::CreateDirectory(src_path.AppendASCII("xxx_yyy")));
 
@@ -166,7 +168,8 @@ TEST(ExtensionL10nUtil, LoadMessageCatalogsBadJSONFormat) {
 
   std::string data = "{ \"name\":";
   base::FilePath messages_file = locale.Append(kMessagesFilename);
-  ASSERT_TRUE(base::WriteFile(messages_file, data.c_str(), data.length()));
+  ASSERT_EQ(static_cast<int>(data.length()),
+            base::WriteFile(messages_file, data.c_str(), data.length()));
 
   std::string error;
   EXPECT_TRUE(NULL == extension_l10n_util::LoadMessageCatalogs(
@@ -191,14 +194,16 @@ TEST(ExtensionL10nUtil, LoadMessageCatalogsDuplicateKeys) {
   std::string data =
       "{ \"name\": { \"message\": \"something\" }, "
       "\"name\": { \"message\": \"something else\" } }";
-  ASSERT_TRUE(base::WriteFile(
-      locale_1.Append(kMessagesFilename), data.c_str(), data.length()));
+  ASSERT_EQ(static_cast<int>(data.length()),
+            base::WriteFile(locale_1.Append(kMessagesFilename), data.c_str(),
+                            data.length()));
 
   base::FilePath locale_2 = src_path.AppendASCII("sr");
   ASSERT_TRUE(base::CreateDirectory(locale_2));
 
-  ASSERT_TRUE(base::WriteFile(
-      locale_2.Append(kMessagesFilename), data.c_str(), data.length()));
+  ASSERT_EQ(static_cast<int>(data.length()),
+            base::WriteFile(locale_2.Append(kMessagesFilename), data.c_str(),
+                            data.length()));
 
   std::string error;
   // JSON parser hides duplicates. We are going to get only one key/value
