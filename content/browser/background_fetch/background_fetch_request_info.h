07cd8900cce0781c449e213ba6c0d2a03d447452
peter <peter@chromium.org>
2017-04-12 12:54:55 -0700

    Pass through the response code and headers in Background Fetch
    Following this CL, Background Fetch responses will include the response
    code and headers as provided by the download manager.
    
    We don't yet support the `backgroundfetchfail` event or handle CORS, but
    those can now be implemented in follow-up CLs.
    
    BUG=709489
    
    Review-Url: https://codereview.chromium.org/2814683003
    Cr-Commit-Position: refs/heads/master@{#464119}

--- content/browser/background_fetch/background_fetch_request_info.h
+++ content/browser/background_fetch/background_fetch_request_info.h
@@ -49,6 +49,18 @@ class CONTENT_EXPORT BackgroundFetchRequestInfo
     return fetch_request_;
   }
 
+  // Returns the response code for the download. Available for both successful
+  // and failed requests.
+  int GetResponseCode() const;
+
+  // Returns the response text for the download. Available for all started
+  // items.
+  const std::string& GetResponseText() const;
+
+  // Returns the response headers for the download. Available for both
+  // successful and failed requests.
+  const std::map<std::string, std::string>& GetResponseHeaders() const;
+
   // Returns the URL chain for the response, including redirects.
   const std::vector<GURL>& GetURLChain() const;
 
@@ -60,9 +72,6 @@ class CONTENT_EXPORT BackgroundFetchRequestInfo
 
   // Returns the time at which the response was completed.
   const base::Time& GetResponseTime() const;
-
-  // Returns the mime type describing the response, as indicated by the server.
-  const std::string& GetResponseType() const;
 
  private:
   friend class base::RefCountedThreadSafe<BackgroundFetchRequestInfo>;
@@ -82,6 +91,10 @@ class CONTENT_EXPORT BackgroundFetchRequestInfo
   std::string download_guid_;
   DownloadItem::DownloadState download_state_ = DownloadItem::IN_PROGRESS;
 
+  int response_code_ = 0;
+  std::string response_text_;
+  std::map<std::string, std::string> response_headers_;
+
   // ---- Data associated with the response ------------------------------------
 
   // Indicates whether response data has been populated.
@@ -91,7 +104,6 @@ class CONTENT_EXPORT BackgroundFetchRequestInfo
   base::FilePath file_path_;
   int64_t file_size_ = 0;
   base::Time response_time_;
-  std::string response_type_;
 
   DISALLOW_COPY_AND_ASSIGN(BackgroundFetchRequestInfo);
 };
--- content/browser/background_fetch/background_fetch_test_base.h
+++ content/browser/background_fetch/background_fetch_test_base.h
@@ -19,6 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/origin.h"
 
+namespace net {
+class HttpResponseHeaders;
+}
+
 namespace content {
 
 class BackgroundFetchRegistrationId;
@@ -36,6 +40,36 @@ class BackgroundFetchTestBase : public ::testing::Test {
   void SetUp() override;
   void TearDown() override;
 
+  // Structure encapsulating the data for a injected response. Should only be
+  // created by the builder, which also defines the ownership semantics.
+  struct TestResponse {
+    TestResponse();
+    ~TestResponse();
+
+    scoped_refptr<net::HttpResponseHeaders> headers;
+    std::string data;
+  };
+
+  // Builder for creating a TestResponse object with the given data. The faked
+  // download manager will respond to the corresponding request based on this.
+  class TestResponseBuilder {
+   public:
+    explicit TestResponseBuilder(int response_code);
+    ~TestResponseBuilder();
+
+    TestResponseBuilder& AddResponseHeader(const std::string& name,
+                                           const std::string& value);
+    TestResponseBuilder& SetResponseData(std::string data);
+
+    // Finalizes the builder and invalidates the underlying response.
+    std::unique_ptr<TestResponse> Build();
+
+   private:
+    std::unique_ptr<TestResponse> response_;
+
+    DISALLOW_COPY_AND_ASSIGN(TestResponseBuilder);
+  };
+
   // Creates a Background Fetch registration backed by a Service Worker
   // registration for the testing origin. The resulting registration will be
   // stored in |*registration_id|. Returns whether creation was successful,
@@ -46,13 +80,11 @@ class BackgroundFetchTestBase : public ::testing::Test {
       WARN_UNUSED_RESULT;
 
   // Creates a ServiceWorkerFetchRequest instance for the given details and
-  // provides a faked response with |status_code| and |response_text| to the
-  // download manager, that will resolve the download with that information.
+  // provides a faked |response| with the faked download manager.
   ServiceWorkerFetchRequest CreateRequestWithProvidedResponse(
       const std::string& method,
-      const std::string& url_string,
-      int status_code,
-      const std::string& response_text);
+      const std::string& url,
+      std::unique_ptr<TestResponse> response);
 
   // Returns the embedded worker test helper instance, which can be used to
   // influence the behaviour of the Service Worker events.
