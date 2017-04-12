f478466bd74064aa393eece1767cf9130a04dd30
dgn <dgn@chromium.org>
2017-04-12 07:44:53 -0700

    [Suggestions] Use search suggestions flag to guard the site ones
    BUG=706384
    
    Review-Url: https://codereview.chromium.org/2810913005
    Cr-Commit-Position: refs/heads/master@{#464021}

--- chrome/browser/ntp_snippets/content_suggestions_service_factory.cc
+++ chrome/browser/ntp_snippets/content_suggestions_service_factory.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/translate/language_model_factory.h"
 #include "chrome/common/channel_info.h"
 #include "chrome/common/chrome_features.h"
+#include "chrome/common/pref_names.h"
 #include "components/bookmarks/browser/bookmark_model.h"
 #include "components/browser_sync/profile_sync_service.h"
 #include "components/image_fetcher/core/image_decoder.h"
@@ -215,6 +216,16 @@ void RegisterArticleProvider(SigninManagerBase* signin_manager,
     api_key = is_stable_channel ? google_apis::GetAPIKey()
                                 : google_apis::GetNonStableAPIKey();
   }
+
+  // Pass the pref name associated to the search suggest toggle, and use it to
+  // also guard the remote suggestions feature.
+  // TODO(https://crbug.com/710636): Cleanup this clunky dependency once the
+  // preference design is stable.
+  std::string additional_toggle_pref;
+  if (base::FeatureList::IsEnabled(
+          chrome::android::kContentSuggestionsSettings)) {
+    additional_toggle_pref = prefs::kSearchSuggestEnabled;
+  }
   auto suggestions_fetcher = base::MakeUnique<RemoteSuggestionsFetcher>(
       signin_manager, token_service, request_context, pref_service,
       language_model, base::Bind(&safe_json::SafeJsonParser::Parse),
@@ -226,8 +237,8 @@ void RegisterArticleProvider(SigninManagerBase* signin_manager,
       base::MakeUnique<ImageFetcherImpl>(base::MakeUnique<ImageDecoderImpl>(),
                                          request_context.get()),
       base::MakeUnique<RemoteSuggestionsDatabase>(database_dir, task_runner),
-      base::MakeUnique<RemoteSuggestionsStatusService>(signin_manager,
-                                                       pref_service));
+      base::MakeUnique<RemoteSuggestionsStatusService>(
+          signin_manager, pref_service, additional_toggle_pref));
 
   service->remote_suggestions_scheduler()->SetProvider(provider.get());
   service->set_remote_suggestions_provider(provider.get());
--- components/ntp_snippets/content_suggestions_service.cc
+++ components/ntp_snippets/content_suggestions_service.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/favicon/core/large_icon_service.h"
 #include "components/favicon_base/fallback_icon_style.h"
 #include "components/favicon_base/favicon_types.h"
-#include "components/ntp_snippets/content_suggestions_metrics.h"
 #include "components/ntp_snippets/pref_names.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
@@ -56,7 +55,6 @@ ContentSuggestionsService::ContentSuggestionsService(
   }
 
   RestoreDismissedCategoriesFromPrefs();
-  metrics::RecordRemoteSuggestionsProviderState(AreRemoteSuggestionsEnabled());
 }
 
 ContentSuggestionsService::~ContentSuggestionsService() = default;
