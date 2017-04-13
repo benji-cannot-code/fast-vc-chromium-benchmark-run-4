daedb86c5200ec46cf2c466a5bd1236f0366d2cf
hiroshige <hiroshige@chromium.org>
2017-04-13 16:49:19 -0700

    Rename PendingScript::Url() to UrlForClassicScript()
    Because it is/can be used only for classic scripts.
    
    BUG=686281
    
    Review-Url: https://codereview.chromium.org/2815163005
    Cr-Commit-Position: refs/heads/master@{#464613}

--- third_party/WebKit/Source/core/dom/ClassicPendingScript.cpp
+++ third_party/WebKit/Source/core/dom/ClassicPendingScript.cpp
@@ -230,7 +230,7 @@ bool ClassicPendingScript::WasCanceled() const {
   return GetResource()->WasCanceled();
 }
 
-KURL ClassicPendingScript::Url() const {
+KURL ClassicPendingScript::UrlForClassicScript() const {
   return GetResource()->Url();
 }
 
