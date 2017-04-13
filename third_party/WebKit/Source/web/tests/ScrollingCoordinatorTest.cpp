9f83cd77321174a0e45408c4f1d9fd90a3e5ff46
joelhockey <joelhockey@chromium.org>
2017-04-12 23:51:09 -0700

    Replace LayoutPart::GetFrameViewBase with ChildFrameView
    GetChildFrameView returns FrameView associated with local Node
    if node is HTMLFrameOwnerElement or nullptr.
    
    The method name GetFrameView is already declared in
    LayoutObject which returns Document().View(), so a different
    name was required.
    
    BUG=637460
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2810873007
    Cr-Commit-Position: refs/heads/master@{#464325}

--- third_party/WebKit/Source/web/tests/ScrollingCoordinatorTest.cpp
+++ third_party/WebKit/Source/web/tests/ScrollingCoordinatorTest.cpp
@@ -671,10 +671,10 @@ TEST_P(ScrollingCoordinatorTest, iframeScrolling) {
 
   LayoutPart* layout_part = ToLayoutPart(layout_object);
   ASSERT_TRUE(layout_part);
-  ASSERT_TRUE(layout_part->GetFrameViewBase());
-  ASSERT_TRUE(layout_part->GetFrameViewBase()->IsFrameView());
 
-  FrameView* inner_frame_view = ToFrameView(layout_part->GetFrameViewBase());
+  FrameView* inner_frame_view = layout_part->ChildFrameView();
+  ASSERT_TRUE(inner_frame_view);
+
   LayoutViewItem inner_layout_view_item = inner_frame_view->GetLayoutViewItem();
   ASSERT_FALSE(inner_layout_view_item.IsNull());
 
@@ -723,10 +723,10 @@ TEST_P(ScrollingCoordinatorTest, rtlIframe) {
 
   LayoutPart* layout_part = ToLayoutPart(layout_object);
   ASSERT_TRUE(layout_part);
-  ASSERT_TRUE(layout_part->GetFrameViewBase());
-  ASSERT_TRUE(layout_part->GetFrameViewBase()->IsFrameView());
 
-  FrameView* inner_frame_view = ToFrameView(layout_part->GetFrameViewBase());
+  FrameView* inner_frame_view = layout_part->ChildFrameView();
+  ASSERT_TRUE(inner_frame_view);
+
   LayoutViewItem inner_layout_view_item = inner_frame_view->GetLayoutViewItem();
   ASSERT_FALSE(inner_layout_view_item.IsNull());
 
@@ -901,10 +901,10 @@ TEST_P(ScrollingCoordinatorTest,
 
   LayoutPart* layout_part = ToLayoutPart(layout_object);
   ASSERT_TRUE(layout_part);
-  ASSERT_TRUE(layout_part->GetFrameViewBase());
-  ASSERT_TRUE(layout_part->GetFrameViewBase()->IsFrameView());
 
-  FrameView* inner_frame_view = ToFrameView(layout_part->GetFrameViewBase());
+  FrameView* inner_frame_view = layout_part->ChildFrameView();
+  ASSERT_TRUE(inner_frame_view);
+
   LayoutViewItem inner_layout_view_item = inner_frame_view->GetLayoutViewItem();
   ASSERT_FALSE(inner_layout_view_item.IsNull());
 
