9fa6ee93a894553bdb6cdd87bf1fece43b7a19be
hiroshige <hiroshige@chromium.org>
2017-04-13 12:14:53 -0700

    Split PendingScript into PendingScript and ClassicPendingScript
    This is preparation for introducing ModulePendingScript.
    
    BUG=594639, 686281
    
    Review-Url: https://codereview.chromium.org/2653923008
    Cr-Commit-Position: refs/heads/master@{#464494}

--- third_party/WebKit/Source/bindings/core/v8/ScriptStreamerTest.cpp
+++ third_party/WebKit/Source/bindings/core/v8/ScriptStreamerTest.cpp
@@ -12,9 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/V8Binding.h"
 #include "bindings/core/v8/V8BindingForTesting.h"
 #include "bindings/core/v8/V8ScriptRunner.h"
+#include "core/dom/ClassicPendingScript.h"
 #include "core/dom/ClassicScript.h"
-#include "core/dom/Element.h"
-#include "core/dom/PendingScript.h"
 #include "core/frame/Settings.h"
 #include "platform/heap/Handle.h"
 #include "platform/testing/UnitTestHelpers.h"
@@ -37,9 +36,10 @@ class ScriptStreamingTest : public ::testing::Test {
         settings_(Settings::Create()),
         resource_request_("http://www.streaming-test.com/"),
         resource_(ScriptResource::Create(resource_request_, "UTF-8")),
-        pending_script_(PendingScript::CreateForTesting(resource_.Get())) {
+        pending_script_(
+            ClassicPendingScript::CreateForTesting(resource_.Get())) {
     resource_->SetStatus(ResourceStatus::kPending);
-    pending_script_ = PendingScript::CreateForTesting(resource_.Get());
+    pending_script_ = ClassicPendingScript::CreateForTesting(resource_.Get());
     ScriptStreamer::SetSmallScriptThresholdForTesting(0);
   }
 
@@ -48,7 +48,9 @@ class ScriptStreamingTest : public ::testing::Test {
       pending_script_->Dispose();
   }
 
-  PendingScript* GetPendingScript() const { return pending_script_.Get(); }
+  ClassicPendingScript* GetPendingScript() const {
+    return pending_script_.Get();
+  }
 
  protected:
   void AppendData(const char* data) {
@@ -91,7 +93,7 @@ class ScriptStreamingTest : public ::testing::Test {
   // ScriptResource::appendData.
   ResourceRequest resource_request_;
   Persistent<ScriptResource> resource_;
-  Persistent<PendingScript> pending_script_;
+  Persistent<ClassicPendingScript> pending_script_;
 };
 
 class TestPendingScriptClient
--- third_party/WebKit/Source/core/html/parser/HTMLParserScriptRunner.cpp
+++ third_party/WebKit/Source/core/html/parser/HTMLParserScriptRunner.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/ScriptSourceCode.h"
 #include "bindings/core/v8/V8Binding.h"
 #include "bindings/core/v8/V8PerIsolateData.h"
+#include "core/dom/ClassicPendingScript.h"
 #include "core/dom/ClassicScript.h"
 #include "core/dom/DocumentParserTiming.h"
 #include "core/dom/Element.h"
@@ -49,7 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/WebFrameScheduler.h"
 #include "platform/instrumentation/tracing/TraceEvent.h"
 #include "platform/instrumentation/tracing/TracedValue.h"
-#include "platform/loader/fetch/MemoryCache.h"
 #include "public/platform/Platform.h"
 
 namespace blink {
@@ -336,9 +336,12 @@ void HTMLParserScriptRunner::PossiblyFetchBlockedDocWriteScript(
   if (!script_loader || !script_loader->DisallowedFetchForDocWrittenScript())
     return;
 
+  // We don't allow document.write() and its intervention with module scripts.
+  CHECK_EQ(pending_script->GetScriptType(), ScriptType::kClassic);
+
   if (!pending_script->ErrorOccurred()) {
-    EmitWarningForDocWriteScripts(
-        pending_script->GetResource()->Url().GetString(), *document_);
+    EmitWarningForDocWriteScripts(pending_script->Url().GetString(),
+                                  *document_);
     return;
   }
 
@@ -346,13 +349,12 @@ void HTMLParserScriptRunner::PossiblyFetchBlockedDocWriteScript(
   // ERR_CACHE_MISS but other errors are rare with
   // WebCachePolicy::ReturnCacheDataDontLoad.
 
-  EmitErrorForDocWriteScripts(pending_script->GetResource()->Url().GetString(),
-                              *document_);
+  EmitErrorForDocWriteScripts(pending_script->Url().GetString(), *document_);
   TextPosition starting_position = ParserBlockingScript()->StartingPosition();
   bool is_parser_inserted = script_loader->IsParserInserted();
   // Remove this resource entry from memory cache as the new request
   // should not join onto this existing entry.
-  GetMemoryCache()->Remove(pending_script->GetResource());
+  pending_script->RemoveFromMemoryCache();
   FetchBlockedDocWriteScript(element, is_parser_inserted, starting_position);
 }
 
@@ -364,7 +366,7 @@ void HTMLParserScriptRunner::PendingScriptFinished(
   // script execution to signal an abrupt stop (e.g., window.close().)
   //
   // The parser is unprepared to be told, and doesn't need to be.
-  if (IsExecutingScript() && pending_script->GetResource()->WasCanceled()) {
+  if (IsExecutingScript() && pending_script->WasCanceled()) {
     pending_script->Dispose();
 
     if (pending_script == ParserBlockingScript()) {
@@ -506,7 +508,7 @@ bool HTMLParserScriptRunner::ExecuteScriptsWaitingForParsing() {
   while (!scripts_to_execute_after_parsing_.IsEmpty()) {
     DCHECK(!IsExecutingScript());
     DCHECK(!HasParserBlockingScript());
-    DCHECK(scripts_to_execute_after_parsing_.front()->GetResource());
+    DCHECK(scripts_to_execute_after_parsing_.front()->IsExternal());
 
     // 1. "Spin the event loop until the first script in the list of scripts
     //     that will execute when the document has finished parsing
@@ -552,7 +554,7 @@ void HTMLParserScriptRunner::RequestParsingBlockingScript(Element* element) {
   if (!ParserBlockingScript())
     return;
 
-  DCHECK(ParserBlockingScript()->GetResource());
+  DCHECK(ParserBlockingScript()->IsExternal());
 
   // We only care about a load callback if resource is not already in the cache.
   // Callers will attempt to run the m_parserBlockingScript if possible before
@@ -575,7 +577,7 @@ void HTMLParserScriptRunner::RequestDeferredScript(Element* element) {
                                              ScriptStreamer::kDeferred);
   }
 
-  DCHECK(pending_script->GetResource());
+  DCHECK(pending_script->IsExternal());
 
   // "Add the element to the end of the list of scripts that will execute
   //  when the document has finished parsing associated with the Document
@@ -645,7 +647,7 @@ void HTMLParserScriptRunner::ProcessScriptElementInternal(
         //  (There can only be one such script per Document at a time.)"
         CHECK(!parser_blocking_script_);
         parser_blocking_script_ =
-            PendingScript::Create(element, script_start_position);
+            ClassicPendingScript::Create(element, script_start_position);
       } else {
         // 6th Clause of Step 23.
         // "Immediately execute the script block,
