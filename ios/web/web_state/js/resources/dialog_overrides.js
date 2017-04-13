ab0ce8b6f3e0310e32f85117d835eff7f1f33146
danyao <danyao@chromium.org>
2017-04-13 13:29:00 -0700

    Fully deprecate core.js. Moved last method to error.js.
    This is the last CL to split core.js into better encapsulated parts.
    Also updated all references to web/web_state/js/resources/core.js in all
    comments.
    
    BUG=614092
    
    Review-Url: https://codereview.chromium.org/2817943002
    Cr-Commit-Position: refs/heads/master@{#464528}

--- ios/web/web_state/js/resources/dialog_overrides.js
+++ ios/web/web_state/js/resources/dialog_overrides.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 goog.provide('__crWeb.dialogOverrides');
 
-goog.require('__crWeb.core');
+goog.require('__crWeb.message');
 
 __gCrWeb.dialogOverrides = {};
 
