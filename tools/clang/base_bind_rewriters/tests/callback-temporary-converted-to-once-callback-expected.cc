58e56a9ca5b5367820cbdbcb87cd1056af233c4b
tzik <tzik@chromium.org>
2017-04-12 22:36:09 -0700

    Update BaseBindRewriters to convert base::Bind to base::BindOnce
    The new base_bind_rewriter replaces base::Bind with base::BindOnce where
    the resulting Callback is converted to OnceCallback immediately.
    E.g.:
      PostTask(FROM_HERE, base::Bind([]{}));
      OnceCallback<void()> cb = base::Bind([]{});
    will be replaced to
      PostTask(FROM_HERE, base::BindOnce([]{}));
      OnceCallback<void()> cb = base::BindOnce([]{});
    
    Review-Url: https://codereview.chromium.org/2789153002
    Cr-Commit-Position: refs/heads/master@{#464312}

---
+++ tools/clang/base_bind_rewriters/tests/callback-temporary-converted-to-once-callback-expected.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "callback.h"
+
+void Foo(base::OnceClosure) {}
+
+void Test() {
+  base::OnceClosure cb = base::BindOnce([] {});
+  Foo(base::BindOnce([] {}));
+
+  using namespace base;
+
+  OnceClosure cb2 = BindOnce([] {});
+  Foo(BindOnce([] {}));
+
+  Closure cb3 = base::Bind([] {});
+}
