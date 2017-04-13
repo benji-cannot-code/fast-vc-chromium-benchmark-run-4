4da7c3b7186acd63de76204b7505e4d4c300de2d
mgersh <mgersh@chromium.org>
2017-04-13 13:19:03 -0700

    Change Cronet's "disable_ipv6" to "disable_ipv6_on_wifi"
    The new option is implemented in HostResolverImpl as an assumption that
    IPv6 is unreachable when the current connection is wifi. In order to
    make this testable, the tests that used to override IsIPv6Reachable()
    now override IsGloballyReachable().
    
    BUG=696569
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_cronet_tester
    
    Review-Url: https://codereview.chromium.org/2811183003
    Cr-Commit-Position: refs/heads/master@{#464522}

--- net/base/mock_network_change_notifier.h
+++ net/base/mock_network_change_notifier.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef NET_QUIC_CHROMIUM_MOCK_NETWORK_CHANGE_NOTIFIER_H_
-#define NET_QUIC_CHROMIUM_MOCK_NETWORK_CHANGE_NOTIFIER_H_
+#ifndef NET_BASE_MOCK_NETWORK_CHANGE_NOTIFIER_H_
+#define NET_BASE_MOCK_NETWORK_CHANGE_NOTIFIER_H_
 
 #include "net/base/network_change_notifier.h"
 
@@ -72,4 +72,4 @@ class ScopedMockNetworkChangeNotifier {
 }  // namespace test
 }  // namespace net
 
-#endif  // NET_QUIC_CHROMIUM_MOCK_NETWORK_CHANGE_NOTIFIER_H_
+#endif  // NET_BASE_MOCK_NETWORK_CHANGE_NOTIFIER_H_
--- net/quic/chromium/network_connection_unittest.cc
+++ net/quic/chromium/network_connection_unittest.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/quic/chromium/network_connection.h"
 
 #include "base/run_loop.h"
-#include "net/quic/chromium/mock_network_change_notifier.h"
+#include "net/base/mock_network_change_notifier.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace net {
