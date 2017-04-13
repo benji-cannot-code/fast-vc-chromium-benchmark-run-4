7faca811df7bb204788871c17a3fbb3b3e9ff280
bsazonov <bsazonov@chromium.org>
2017-04-13 02:46:43 -0700

    Removed old workaround for button padding from AccountSigninView
    This CL removes workaround for the bug that was fixed long time ago.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2816713002
    Cr-Commit-Position: refs/heads/master@{#464362}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/AccountSigninView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/AccountSigninView.java
@@ -21,7 +21,6 @@ import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 
-import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.Callback;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.R;
@@ -163,11 +162,6 @@ public class AccountSigninView extends FrameLayout implements ProfileDownloader.
         mPositiveButton = (ButtonCompat) findViewById(R.id.positive_button);
         mNegativeButton = (Button) findViewById(R.id.negative_button);
         mMoreButton = (Button) findViewById(R.id.more_button);
-
-        // A workaround for Android support library ignoring padding set in XML. b/20307607
-        int padding = getResources().getDimensionPixelSize(R.dimen.fre_button_padding);
-        ApiCompatibilityUtils.setPaddingRelative(mPositiveButton, padding, 0, padding, 0);
-        ApiCompatibilityUtils.setPaddingRelative(mNegativeButton, padding, 0, padding, 0);
 
         // TODO(peconn): Ensure this is changed to R.string.cancel when used in Settings > Sign In.
         mCancelButtonTextId = R.string.no_thanks;
