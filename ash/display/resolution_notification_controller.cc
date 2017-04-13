76a87c2517a28473980fcc86d61b07efbfba7473
afakhry <afakhry@chromium.org>
2017-04-12 17:59:57 -0700

    Fix crash when changing display resolution disconnects display
    We used to revert back to an old resolution when display is disconnected
    and removed, by calling SetDisplayMode() which triggers another round of
    configuration changes notifications while we're already inside one
    OnDisplayRemoved(). This reenterance in the case of a removed display
    sometimes leads to a crash depending on timing relations.
    
    BUG=709722
    TEST=ash_unittestts, manually
    
    Review-Url: https://codereview.chromium.org/2810843003
    Cr-Commit-Position: refs/heads/master@{#464224}

--- ash/display/resolution_notification_controller.cc
+++ ash/display/resolution_notification_controller.cc
@@ -81,7 +81,7 @@ void ResolutionChangeNotificationDelegate::ButtonClick(int button_index) {
   if (has_timeout_ && button_index == 0)
     controller_->AcceptResolutionChange(true);
   else
-    controller_->RevertResolutionChange();
+    controller_->RevertResolutionChange(false /* display_was_removed */);
 }
 
 }  // namespace
@@ -181,6 +181,14 @@ void ResolutionNotificationController::PrepareNotification(
     original_resolution = change_info_->old_resolution;
   }
 
+  if (change_info_ && change_info_->display_id != display_id) {
+    // Preparing the notification for a new resolution change of another display
+    // before the previous one was accepted. We decided that it's safer to
+    // revert the previous resolution change since the user didn't explicitly
+    // accept it, and we have no way of knowing for sure that it worked.
+    RevertResolutionChange(false /* display_was_removed */);
+  }
+
   change_info_.reset(new ResolutionChangeInfo(display_id, old_resolution,
                                               new_resolution, accept_callback));
   if (original_resolution && !original_resolution->size().IsEmpty())
@@ -254,7 +262,7 @@ void ResolutionNotificationController::OnTimerTick() {
 
   --change_info_->timeout_count;
   if (change_info_->timeout_count == 0)
-    RevertResolutionChange();
+    RevertResolutionChange(false /* display_was_removed */);
   else
     CreateOrUpdateNotification(false);
 }
@@ -272,7 +280,8 @@ void ResolutionNotificationController::AcceptResolutionChange(
   callback.Run();
 }
 
-void ResolutionNotificationController::RevertResolutionChange() {
+void ResolutionNotificationController::RevertResolutionChange(
+    bool display_was_removed) {
   message_center::MessageCenter::Get()->RemoveNotification(kNotificationId,
                                                            false /* by_user */);
   if (!change_info_)
@@ -281,7 +290,16 @@ void ResolutionNotificationController::RevertResolutionChange() {
   scoped_refptr<display::ManagedDisplayMode> old_resolution =
       change_info_->old_resolution;
   change_info_.reset();
-  Shell::Get()->display_manager()->SetDisplayMode(display_id, old_resolution);
+  if (display_was_removed) {
+    // If display was removed then we are inside the stack of
+    // DisplayManager::UpdateDisplaysWith(), and we need to update the selected
+    // mode of this removed display without reentering again into
+    // UpdateDisplaysWith() because this can cause a crash. crbug.com/709722.
+    Shell::Get()->display_manager()->SetSelectedModeForDisplayId(
+        display_id, old_resolution);
+  } else {
+    Shell::Get()->display_manager()->SetDisplayMode(display_id, old_resolution);
+  }
 }
 
 void ResolutionNotificationController::OnDisplayAdded(
@@ -290,7 +308,7 @@ void ResolutionNotificationController::OnDisplayAdded(
 void ResolutionNotificationController::OnDisplayRemoved(
     const display::Display& old_display) {
   if (change_info_ && change_info_->display_id == old_display.id())
-    RevertResolutionChange();
+    RevertResolutionChange(true /* display_was_removed */);
 }
 
 void ResolutionNotificationController::OnDisplayMetricsChanged(
