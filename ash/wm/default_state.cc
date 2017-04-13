811686a595a0da1cc7b5dcc3e3692c23cb101c61
domlaskowski <domlaskowski@chromium.org>
2017-04-12 19:06:10 -0700

    ash: Do not constrain window bounds if requested
    If a client requests to set window bounds directly, Ash should not
    interfere, e.g. by constraining them to the workspace size.
    
    ARC windows are maximized to the display rather than the workspace.
    However, Ash would constrain the ShellSurface bounds to the workspace
    size. As a result, if the shelf was positioned to the left, the right
    edge of the window would be displaced from that of the display by the
    width of the shelf, so the app was unresponsive to input in that gap.
    
    BUG=709616
    TEST=Print window hierarchy using debug shortcut, and verify that the
         bounds of a maximized ShellSurface match the display bounds.
    
    Review-Url: https://codereview.chromium.org/2806783002
    Cr-Commit-Position: refs/heads/master@{#464242}

--- ash/wm/default_state.cc
+++ ash/wm/default_state.cc
@@ -347,6 +347,7 @@ bool DefaultState::ProcessWorkspaceEvents(WindowState* window_state,
       // If a window is opened as maximized or fullscreen, its bounds may be
       // empty, so update the bounds now before checking empty.
       if (window_state->is_dragged() ||
+          window_state->allow_set_bounds_direct() ||
           SetMaximizedOrFullscreenBounds(window_state)) {
         return true;
       }
@@ -381,6 +382,7 @@ bool DefaultState::ProcessWorkspaceEvents(WindowState* window_state,
     }
     case WM_EVENT_DISPLAY_BOUNDS_CHANGED: {
       if (window_state->is_dragged() ||
+          window_state->allow_set_bounds_direct() ||
           SetMaximizedOrFullscreenBounds(window_state)) {
         return true;
       }
@@ -406,6 +408,7 @@ bool DefaultState::ProcessWorkspaceEvents(WindowState* window_state,
         return true;
 
       if (window_state->is_dragged() ||
+          window_state->allow_set_bounds_direct() ||
           SetMaximizedOrFullscreenBounds(window_state)) {
         return true;
       }
@@ -447,6 +450,7 @@ bool DefaultState::ProcessWorkspaceEvents(WindowState* window_state,
 // static
 bool DefaultState::SetMaximizedOrFullscreenBounds(WindowState* window_state) {
   DCHECK(!window_state->is_dragged());
+  DCHECK(!window_state->allow_set_bounds_direct());
   if (window_state->IsMaximized()) {
     window_state->SetBoundsDirect(
         GetMaximizedWindowBoundsInParent(window_state->window()));
@@ -463,7 +467,7 @@ bool DefaultState::SetMaximizedOrFullscreenBounds(WindowState* window_state) {
 // static
 void DefaultState::SetBounds(WindowState* window_state,
                              const SetBoundsEvent* event) {
-  if (window_state->is_dragged()) {
+  if (window_state->is_dragged() || window_state->allow_set_bounds_direct()) {
     // TODO(oshima|varkha): Is this still needed? crbug.com/485612.
     window_state->SetBoundsDirect(event->requested_bounds());
   } else if (window_state->IsSnapped()) {
@@ -473,8 +477,7 @@ void DefaultState::SetBounds(WindowState* window_state,
     wm::AdjustBoundsSmallerThan(work_area_in_parent.size(), &child_bounds);
     window_state->AdjustSnappedBounds(&child_bounds);
     window_state->SetBoundsDirect(child_bounds);
-  } else if (!SetMaximizedOrFullscreenBounds(window_state) ||
-             window_state->allow_set_bounds_in_maximized()) {
+  } else if (!SetMaximizedOrFullscreenBounds(window_state)) {
     window_state->SetBoundsConstrained(event->requested_bounds());
   }
 }
