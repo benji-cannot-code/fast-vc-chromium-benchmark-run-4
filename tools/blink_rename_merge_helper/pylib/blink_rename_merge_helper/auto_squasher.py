54cd3e2f367de161cfa059d447e6859cea108a19
Daniel Cheng <dcheng@chromium.org>
2017-04-12 19:16:40 -0700

    Rebase helper for Blink rename
    The general workflow is:
    - switch to a branch that needs to be rebased
    - run the script with --prepare
    - run the script with --update
    
    Internally, what the tool does is it runs the clang tool
    across the local branch and records the result. During
    the actual conflict resolution for rebasing across the
    rename commit, it uses the previously recorded result
    to help resolve conflicts.
    
    This is a fairly heavyweight process, and for simpler
    patches, it's likely more efficient to just rebase the
    patch manually.
    
    While this script should be able to help with chained
    branches, usage is tricky: all the dependent branches,
    from root to child, should be prepared with --prepare
    first before running --update from root to child.
    
    Bug: 676588
    Change-Id: I67790467f341ffeb5a696636fe3e43bb7f663f40
    Reviewed-on: https://chromium-review.googlesource.com/472569
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#464471}

---
+++ tools/blink_rename_merge_helper/pylib/blink_rename_merge_helper/auto_squasher.py
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Simple utility to help squash multiple commits into one."""
+
+import sys
+
+
+def main():
+  with open(sys.argv[1], 'r+') as f:
+    lines = f.readlines()
+    for i, line in enumerate(lines):
+      if i:
+        if line.startswith('pick '):
+          lines[i] = line.replace('pick ', 'squash ', 1)
+    f.seek(0)
+    f.truncate()
+    f.write('\n'.join(lines))
+
+
+if __name__ == '__main__':
+  sys.exit(main())
