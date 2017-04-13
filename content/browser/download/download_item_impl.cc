b287889886680617714698846dd36faa4e64e6cd
qinmin <qinmin@chromium.org>
2017-04-12 20:33:44 -0700

    Record the parallel download UMA for fresh new downloads
    Start() can be called on both resumption and fresh new download
    This CL records only the parallel downloads that are new
    
    BUG=644352
    
    Review-Url: https://codereview.chromium.org/2810133003
    Cr-Commit-Position: refs/heads/master@{#464278}

--- content/browser/download/download_item_impl.cc
+++ content/browser/download/download_item_impl.cc
@@ -1225,6 +1225,9 @@ void DownloadItemImpl::Start(
   download_file_ = std::move(file);
   job_ = DownloadJobFactory::CreateJob(this, std::move(req_handle),
                                        new_create_info);
+  if (job_->UsesParallelRequests())
+    RecordParallelDownloadCount(START_COUNT);
+
   deferred_interrupt_reason_ = DOWNLOAD_INTERRUPT_REASON_NONE;
 
   if (state_ == CANCELLED_INTERNAL) {
@@ -1274,6 +1277,8 @@ void DownloadItemImpl::Start(
 
   if (state_ == INITIAL_INTERNAL) {
     RecordDownloadCount(NEW_DOWNLOAD_COUNT);
+    if (job_->UsesParallelRequests())
+      RecordParallelDownloadCount(NEW_DOWNLOAD_COUNT);
     RecordDownloadMimeType(mime_type_);
     if (!GetBrowserContext()->IsOffTheRecord()) {
       RecordDownloadCount(NEW_DOWNLOAD_COUNT_NORMAL_PROFILE);
@@ -1287,9 +1292,6 @@ void DownloadItemImpl::Start(
 
   if (state_ == RESUMING_INTERNAL)
     UpdateValidatorsOnResumption(new_create_info);
-
-  if (state_ == INITIAL_INTERNAL && job_->UsesParallelRequests())
-    RecordParallelDownloadCount(START_COUNT);
 
   TransitionTo(TARGET_PENDING_INTERNAL);
 
