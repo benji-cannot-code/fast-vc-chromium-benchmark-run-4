3c1b512242ce199817db473e9991507ecc8a82f1
acondor <acondor@google.com>
2017-04-13 13:20:44 -0700

    Rendering Insecure WebVR Warnings into a texture with Skia.
    Icons and rounded backgrounds reuse gfx code.
    Text rendering is a temporary solution that cannot handle fallback fonts.
    RTL rendering is not handled either.
    
    BUG=710150
    
    Review-Url: https://codereview.chromium.org/2818553002
    Cr-Commit-Position: refs/heads/master@{#464523}

---
+++ chrome/browser/android/vr_shell/textures/insecure_content_permanent_texture.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ANDROID_VR_SHELL_TEXTURES_INSECURE_CONTENT_PERMANENT_TEXTURE_H_
+#define CHROME_BROWSER_ANDROID_VR_SHELL_TEXTURES_INSECURE_CONTENT_PERMANENT_TEXTURE_H_
+
+#include "chrome/browser/android/vr_shell/textures/ui_texture.h"
+
+namespace vr_shell {
+
+class InsecureContentPermanentTexture : public UITexture {
+ public:
+  InsecureContentPermanentTexture(int texture_handle, int texture_size);
+  ~InsecureContentPermanentTexture() override;
+
+ private:
+  void Draw(gfx::Canvas* canvas) override;
+  void SetSize() override;
+
+  int height_;
+};
+
+}  // namespace vr_shell
+
+#endif  // CHROME_BROWSER_ANDROID_VR_SHELL_TEXTURES_INSECURE_CONTENT_PERMANENT_TEXTURE_H_
