bf3708bdae26d620aaad5b0139435a7a7bccd712
Sam McNally <sammc@chromium.org>
2017-04-13 11:23:25 +1000

    Fix a use-after-free in InterceptablePrefFilter.
    Previously, InterceptablePrefFilter reset |filter_on_load_interceptor_|
    after calling it. However, the callback could result in the object being
    deleted, resulting in the callback being freed before Reset() is called.
    
    This CL fixes this by using ResetAndReturn to move the callback into a
    temporary before calling it.
    
    Bug: 710655
    Change-Id: I841c4157d54e0a31364237f4495384de6664e535
    Reviewed-on: https://chromium-review.googlesource.com/476390
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#464255}

--- services/preferences/tracked/interceptable_pref_filter.cc
+++ services/preferences/tracked/interceptable_pref_filter.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
+#include "base/callback_helpers.h"
 
 InterceptablePrefFilter::InterceptablePrefFilter() {}
 InterceptablePrefFilter::~InterceptablePrefFilter() {}
@@ -27,9 +28,8 @@ void InterceptablePrefFilter::FilterOnLoad(
     const FinalizeFilterOnLoadCallback finalize_filter_on_load(
         base::Bind(&InterceptablePrefFilter::FinalizeFilterOnLoad, AsWeakPtr(),
                    post_filter_on_load_callback));
-    filter_on_load_interceptor_.Run(finalize_filter_on_load,
-                                    std::move(pref_store_contents));
-    filter_on_load_interceptor_.Reset();
+    base::ResetAndReturn(&filter_on_load_interceptor_)
+        .Run(finalize_filter_on_load, std::move(pref_store_contents));
   }
 }
 
