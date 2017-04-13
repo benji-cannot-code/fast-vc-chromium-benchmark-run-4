cf9477664192b86fdc5f6a8711f40b2994797fdd
geofflang <geofflang@chromium.org>
2017-04-13 13:35:26 -0700

    Let DoMapBufferRange write the result integer to signal a GL error.
    TEST=conformance2/buffers/get-buffer-sub-data
    
    BUG=602688
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2822513002
    Cr-Commit-Position: refs/heads/master@{#464530}

--- gpu/command_buffer/service/gles2_cmd_decoder_passthrough_doers.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder_passthrough_doers.cc
@@ -2747,7 +2747,8 @@ error::Error GLES2DecoderPassthroughImpl::DoMapBufferRange(
     GLbitfield access,
     void* ptr,
     int32_t data_shm_id,
-    uint32_t data_shm_offset) {
+    uint32_t data_shm_offset,
+    uint32_t* result) {
   FlushErrors();
 
   GLbitfield filtered_access = access;
@@ -2772,6 +2773,7 @@ error::Error GLES2DecoderPassthroughImpl::DoMapBufferRange(
   void* mapped_ptr = glMapBufferRange(target, offset, size, filtered_access);
   if (FlushErrors() || mapped_ptr == nullptr) {
     // Had an error while mapping, don't copy any data
+    *result = 0;
     return error::kNoError;
   }
 
@@ -2796,6 +2798,7 @@ error::Error GLES2DecoderPassthroughImpl::DoMapBufferRange(
   resources_->mapped_buffer_map.insert(
       std::make_pair(client_buffer, mapped_buffer_info));
 
+  *result = 1;
   return error::kNoError;
 }
 
