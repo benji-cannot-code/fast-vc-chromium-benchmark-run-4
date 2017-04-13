bdc941eb0061eb6cbe880ef17d94c8e128e8ea11
fdoray <fdoray@chromium.org>
2017-04-13 05:04:20 -0700

    Flush TaskScheduler in ~ScopedTaskEnvironment and ~ScopedAsyncTaskScheduler.
    Skipping tasks when destroying ScopedTaskEnvironment or
    ScopedAsyncTaskScheduler means that DeleteSoon() tasks
    can be skipped, resulting in memory leaks.
    
    BUG=708584, 709095
    
    Review-Url: https://codereview.chromium.org/2812293002
    Cr-Commit-Position: refs/heads/master@{#464380}

--- base/test/scoped_task_environment.cc
+++ base/test/scoped_task_environment.cc
@@ -33,6 +33,9 @@ ScopedTaskEnvironment::~ScopedTaskEnvironment() {
   RunLoop().RunUntilIdle();
 
   DCHECK_EQ(TaskScheduler::GetInstance(), task_scheduler_);
+  // Without FlushForTesting(), DeleteSoon() and ReleaseSoon() tasks could be
+  // skipped, resulting in memory leaks.
+  TaskScheduler::GetInstance()->FlushForTesting();
   TaskScheduler::GetInstance()->Shutdown();
   TaskScheduler::GetInstance()->JoinForTesting();
   TaskScheduler::SetInstance(nullptr);
