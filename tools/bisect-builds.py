ea2b8e74fcff13da8a339d2ba6f2b887be71688f
elawrence <elawrence@chromium.org>
2017-04-14 10:18:51 -0700

    Provide instructions if -o or -r is used on public bisect-builds.py
    If the user attempts to use the -o or -r options in the public bisect
    script, they will fail as unknown options. Instead, we should help
    the user get the correct version of the script by pointing to the
    page explaining how to configure their environment.
    
    Review-Url: https://codereview.chromium.org/2742003002
    Cr-Commit-Position: refs/heads/master@{#464743}

--- tools/bisect-builds.py
+++ tools/bisect-builds.py
@@ -1002,6 +1002,11 @@ def PrintChangeLog(min_chromium_rev, max_chromium_rev):
   print ('  ' + CHANGELOG_URL % (GetGitHashFromSVNRevision(min_chromium_rev),
          GetGitHashFromSVNRevision(max_chromium_rev)))
 
+def error_internal_option(option, opt, value, parser):
+   raise optparse.OptionValueError(
+         'The -o and -r options are only\navailable in the internal version of '
+         'this script. Google\nemployees should visit http://go/bisect-builds '
+         'for\nconfiguration instructions.')
 
 def main():
   usage = ('%prog [options] [-- chromium-options]\n'
@@ -1087,6 +1092,8 @@ def main():
                     default=False,
                     help='Test the first and last revisions in the range ' +
                          'before proceeding with the bisect.')
+  parser.add_option("-r", action="callback", callback=error_internal_option)
+  parser.add_option("-o", action="callback", callback=error_internal_option)
 
   (opts, args) = parser.parse_args()
 
