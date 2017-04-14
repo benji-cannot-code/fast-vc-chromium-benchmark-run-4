5d44ee4d6dce1d6f346983dfe091dadd94e2e5a8
hiroshige <hiroshige@chromium.org>
2017-04-14 11:59:21 -0700

    Revert of Rename PendingScript::Url() to UrlForClassicScript() (patchset #1 id:1 of https://codereview.chromium.org/2815163005/ )
    Reason for revert:
    https://codereview.chromium.org/2653923008/ is
    suspected to cause CHECK() failure (crbug.com/711703)
    inside PendingScript's prefinalizer.
    
    BUG=711703
    
    Original issue's description:
    > Rename PendingScript::Url() to UrlForClassicScript()
    >
    > Because it is/can be used only for classic scripts.
    >
    > BUG=686281
    >
    > Review-Url: https://codereview.chromium.org/2815163005
    > Cr-Commit-Position: refs/heads/master@{#464613}
    > Committed: https://chromium.googlesource.com/chromium/src/+/06fcf4b92be26a28fde65c743e6a0d40bbc53dc0
    
    TBR=kouhei@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=686281
    
    Review-Url: https://codereview.chromium.org/2822763002
    Cr-Commit-Position: refs/heads/master@{#464766}

--- third_party/WebKit/Source/core/dom/ClassicPendingScript.cpp
+++ third_party/WebKit/Source/core/dom/ClassicPendingScript.cpp
@@ -230,7 +230,7 @@ bool ClassicPendingScript::WasCanceled() const {
   return GetResource()->WasCanceled();
 }
 
-KURL ClassicPendingScript::UrlForClassicScript() const {
+KURL ClassicPendingScript::Url() const {
   return GetResource()->Url();
 }
 
