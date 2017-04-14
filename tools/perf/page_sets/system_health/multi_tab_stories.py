03f79d30ae61db683b3c24628c38cd982cbdc322
vovoy <vovoy@chromium.org>
2017-04-13 23:06:26 -0700

    Reland of Using multi-tab story in TabSwitching Benchmark (patchset #1 id:1 of https://codereview.chromium.org/2809813003/ )
    Using multi-tab story in TabSwitching Benchmark and rewrite measurement to fit the new multi-tab story
    
    Previous revert is due to tab_swichting mac flaky issue
    (https://bugs.chromium.org/p/chromium/issues/detail?id=634378),
    disable unittest on mac
    
    BUG=689388
    
    Review-Url: https://codereview.chromium.org/2815253002
    Cr-Commit-Position: refs/heads/master@{#464686}

--- tools/perf/page_sets/system_health/multi_tab_stories.py
+++ tools/perf/page_sets/system_health/multi_tab_stories.py
@@ -13,7 +13,7 @@ from page_sets.system_health import platforms
 from telemetry import benchmark
 
 
-class _MultiTabStory(system_health_story.SystemHealthStory):
+class MultiTabStory(system_health_story.SystemHealthStory):
   ABSTRACT_STORY = True
 
   def RunNavigateSteps(self, action_runner):
@@ -41,7 +41,7 @@ class _MultiTabStory(system_health_story.SystemHealthStory):
 
 
 @benchmark.Disabled('all')  # crbug.com/704197
-class MultiTabTypical24Story(_MultiTabStory):
+class MultiTabTypical24Story(MultiTabStory):
   NAME = 'multitab:misc:typical24'
   TAGS = [story_tags.TABS_SWITCHING]
   URL_LIST = [
