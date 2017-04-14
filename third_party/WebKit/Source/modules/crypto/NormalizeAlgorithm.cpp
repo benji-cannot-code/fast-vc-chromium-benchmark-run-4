fbdb034e53725a614f465844ae1402bf49a80818
tkent <tkent@chromium.org>
2017-04-14 00:00:40 -0700

    Remove WTF::String::DeprecatedUpper()
    Except MediaDocument.cpp, input strings are ASCII-only. So changing DeprecatedUpper()
    to UpperASCII() wont't change any behavior.
    
    As for MediaDocument, the input string is a translated text. Changing DeprecatedUpper()
    to UpperUnicode(locale) might improve the result in "tr" and "lt" locale.
    
    BUG=627682
    
    Review-Url: https://codereview.chromium.org/2821633002
    Cr-Commit-Position: refs/heads/master@{#464695}

--- third_party/WebKit/Source/modules/crypto/NormalizeAlgorithm.cpp
+++ third_party/WebKit/Source/modules/crypto/NormalizeAlgorithm.cpp
@@ -141,7 +141,7 @@ bool VerifyAlgorithmNameMappings(const AlgorithmNameMapping* begin,
     String str(it->algorithm_name, it->algorithm_name_length);
     if (!str.ContainsOnlyASCII())
       return false;
-    if (str.DeprecatedUpper() != str)
+    if (str.UpperASCII() != str)
       return false;
   }
 
