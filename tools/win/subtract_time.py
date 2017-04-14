2868889ab817db321ce42c00bbd5f8e688b49e2a
brucedawson <brucedawson@chromium.org>
2017-04-14 15:59:12 -0700

    Add timeit.bat to tools\win
    timeit.bat is a simple batch file that times whatever command you pass
    to it. It uses a helper python script to do the math at the end. There
    is no error checking so don't make mistakes.
    
    R=dcheng@chromium.org
    
    Review-Url: https://codereview.chromium.org/2820843002
    Cr-Commit-Position: refs/heads/master@{#464815}

---
+++ tools/win/subtract_time.py
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright (c) 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""
+This script converts to %time% compatible strings passed to it into seconds,
+subtracts them, and prints the difference. That's it. It's used by timeit.bat.
+"""
+
+import re
+import sys
+
+def ParseTime(time_string):
+  # Time looks like 15:19:30.32
+  match = re.match("(.*):(.*):(.*)\.(.*)", time_string)
+  hours, minutes, seconds, fraction = map(int, match.groups())
+  return hours * 3600 + minutes * 60 + seconds + fraction * .01
+
+print "%1.2f seconds elapsed time" % (ParseTime(sys.argv[1]) -
+                                      ParseTime(sys.argv[2]))
---
+++ tools/win/timeit.bat
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+@ECHO off
+
+REM Copyright (c) 2017 The Chromium Authors. All rights reserved.
+REM Use of this source code is governed by a BSD-style license that can be
+REM found in the LICENSE file.
+
+REM This batch file executes the commands passed to it and prints out the
+REM elapsed run time.
+
+SETLOCAL
+SET starttime=%time%
+CALL %*
+CALL python %~dp0subtract_time.py %time% %starttime%
