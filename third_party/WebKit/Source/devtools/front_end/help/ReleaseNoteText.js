4ae8365969667cc5f7ebc25d40f7fcac9ddcbcf7
chenwilliam <chenwilliam@chromium.org>
2017-04-13 19:44:56 -0700

    DevTools: fix date with version, image for What's New
    * Use a customizable header string
    * Make png smaller
    
    BUG=682879
    
    Review-Url: https://codereview.chromium.org/2813303004
    Cr-Commit-Position: refs/heads/master@{#464662}

--- third_party/WebKit/Source/devtools/front_end/help/ReleaseNoteText.js
+++ third_party/WebKit/Source/devtools/front_end/help/ReleaseNoteText.js
@@ -13,7 +13,7 @@ var commandMenuShortcut = Host.isMac() ? 'Command + Shift + P' : 'Control + Shif
 Help.releaseNoteText = [
   {
     version: 2,
-    date: 'April 2017',
+    header: 'Highlights from Chrome 59 update',
     highlights: [
       {
         title: 'CSS and JS code coverage',
@@ -50,7 +50,7 @@ Help.releaseNoteText = [
   },
   {
     version: 1,
-    date: 'March 2017',
+    header: 'Highlights from Chrome 58 update',
     highlights: [
       {
         title: 'New Performance and Memory panels',
