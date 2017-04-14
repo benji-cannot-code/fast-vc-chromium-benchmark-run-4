69f96f6854b0722d19f79221372d59978174ee30
liaoyuke <liaoyuke@chromium.org>
2017-04-14 16:52:03 -0700

    Replace requirePageReconstruction with setCustomUserAgent
    WKWebView provides a property: customUserAgent that we can use to
    change the user agent associated with the web view, which means that we
    don't have to requirePageConstruction anymore when user agent changes.
    
    After applying this change, I observed that it's becoming significantly
    faster to navigate between desktop and mobile pages during back and
    forward navigations.
    
    BUG=707368
    
    Review-Url: https://codereview.chromium.org/2811073005
    Cr-Commit-Position: refs/heads/master@{#464830}

--- ios/web/web_state/web_view_internal_creation_util.h
+++ ios/web/web_state/web_view_internal_creation_util.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <CoreGraphics/CoreGraphics.h>
 #import <WebKit/WebKit.h>
 
+#include "ios/web/public/user_agent.h"
+
 @protocol CRWContextMenuDelegate;
 
 // This file is a collection of functions that vend web views.
@@ -27,7 +29,7 @@ class BrowserState;
 WKWebView* BuildWKWebView(CGRect frame,
                           WKWebViewConfiguration* configuration,
                           BrowserState* browser_state,
-                          BOOL use_desktop_user_agent,
+                          UserAgentType user_agent_type,
                           id<CRWContextMenuDelegate> context_menu_delegate);
 
 // Creates and returns a new WKWebView for displaying regular web content.
@@ -36,7 +38,7 @@ WKWebView* BuildWKWebView(CGRect frame,
 WKWebView* BuildWKWebView(CGRect frame,
                           WKWebViewConfiguration* configuration,
                           BrowserState* browser_state,
-                          BOOL use_desktop_user_agent);
+                          UserAgentType user_agent_type);
 
 // Creates and returns a new WKWebView for displaying regular web content.
 // The preconditions for the creation of a WKWebView are the same as the
