745d331878ddd7680f6762d75523b11044211188
dschuyler <dschuyler@chromium.org>
2017-04-13 17:54:12 -0700

    [MD settings] hide option for incognito on session only exceptions
    This CL removes the checkbox for 'incognito session only' from the add
    site exception dialog for 'session only' exceptions.
    
    BUG=685027
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2817103002
    Cr-Commit-Position: refs/heads/master@{#464634}

--- chrome/browser/resources/settings/site_settings/add_site_dialog.js
+++ chrome/browser/resources/settings/site_settings/add_site_dialog.js
@@ -27,7 +27,7 @@ Polymer({
     contentSetting: String,
 
     /** @private */
-    isIncognitoActive_: Boolean,
+    showIncognitoSessionOnly_: Boolean,
 
     /**
      * The site to add an exception for.
@@ -49,7 +49,8 @@ Polymer({
    */
   open: function(type) {
     this.addWebUIListener('onIncognitoStatusChanged', function(isActive) {
-      this.isIncognitoActive_ = isActive;
+      this.showIncognitoSessionOnly_ = isActive &&
+          this.contentSetting != settings.PermissionValues.SESSION_ONLY;
     }.bind(this));
     this.browserProxy.updateIncognitoStatus();
     this.$.dialog.showModal();
