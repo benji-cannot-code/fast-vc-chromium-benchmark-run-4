adf16e889a7d9df714d3ad3df1895e911037a8e6
twifkak <twifkak@chromium.org>
2017-04-14 13:48:15 -0700

    Add Precache.Fetch.Begin user action.
    This marks the time (in control or enabled) when precaching begins,
    allowing us to understand ordering with respect to other actions.
    
    We are interested to see if we can draw any conclusions about the
    NavigationToFirstContentfulPaint of the first pageload after a precache, using
    a custom analysis of the logs. Histograms don't include a timestamp, so as an
    approximation, we look at the timestamps of all user actions that occur in the
    same log report as the histogram. This does an okay job of identifying the
    timestamp of a pageload histogram, but as precaches do not accompany any user
    actions, there often aren't any associated timestamps to use. This change adds
    such an action.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2806053002
    Cr-Commit-Position: refs/heads/master@{#464791}

--- components/precache/content/precache_manager.cc
+++ components/precache/content/precache_manager.cc
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/metrics/field_trial.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/metrics/user_metrics.h"
+#include "base/metrics/user_metrics_action.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/time/time.h"
@@ -284,6 +286,8 @@ void PrecacheManager::OnGetUnfinishedWorkDone(
     unfinished_work->set_start_time(base::Time::Now().ToInternalValue());
   unfinished_work_ = std::move(unfinished_work);
   bool needs_top_hosts = unfinished_work_->top_host_size() == 0;
+
+  base::RecordAction(base::UserMetricsAction("Precache.Fetch.Begin"));
 
   if (IsInExperimentGroup()) {
     BrowserThread::PostTask(
