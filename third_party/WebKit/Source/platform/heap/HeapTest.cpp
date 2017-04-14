9414f1072c03df113b7bda90dcacd24f0572bb61
keishi <keishi@chromium.org>
2017-04-14 13:15:30 -0700

    Disable HeapDeathTest.SameThreadCheck* tests on mac
    Disabling HeapDeathTest.SameThreadCheck* because they occasionaly fail on mac bots.
    
    BUG=709069
    
    Review-Url: https://codereview.chromium.org/2819613004
    Cr-Commit-Position: refs/heads/master@{#464784}

--- third_party/WebKit/Source/platform/heap/HeapTest.cpp
+++ third_party/WebKit/Source/platform/heap/HeapTest.cpp
@@ -5541,9 +5541,13 @@ class MemberSameThreadCheckTester {
 };
 
 #if DCHECK_IS_ON()
+// TODO(keishi) This test is flaky on mac_chromium_rel_ng bot.
+// crbug.com/709069
+#if !OS(MACOSX)
 TEST(HeapDeathTest, MemberSameThreadCheck) {
   EXPECT_DEATH(MemberSameThreadCheckTester().Test(), "");
 }
+#endif
 #endif
 
 class PersistentSameThreadCheckTester {
@@ -5580,9 +5584,13 @@ class PersistentSameThreadCheckTester {
 };
 
 #if DCHECK_IS_ON()
+// TODO(keishi) This test is flaky on mac_chromium_rel_ng bot.
+// crbug.com/709069
+#if !OS(MACOSX)
 TEST(HeapDeathTest, PersistentSameThreadCheck) {
   EXPECT_DEATH(PersistentSameThreadCheckTester().Test(), "");
 }
+#endif
 #endif
 
 class MarkingSameThreadCheckTester {
@@ -5630,11 +5638,15 @@ class MarkingSameThreadCheckTester {
 };
 
 #if DCHECK_IS_ON()
+// TODO(keishi) This test is flaky on mac_chromium_rel_ng bot.
+// crbug.com/709069
+#if !OS(MACOSX)
 TEST(HeapDeathTest, MarkingSameThreadCheck) {
   // This will crash during marking, at the DCHECK in Visitor::markHeader() or
   // earlier.
   EXPECT_DEATH(MarkingSameThreadCheckTester().Test(), "");
 }
+#endif
 #endif
 
 static bool AllocateAndReturnBool() {
