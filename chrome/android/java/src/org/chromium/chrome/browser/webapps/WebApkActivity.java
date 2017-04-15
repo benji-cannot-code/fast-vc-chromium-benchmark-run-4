39c9c8adecc26241eabc0f8cc0ef1bccc1327174
pkotwicz <pkotwicz@chromium.org>
2017-04-14 19:52:07 -0700

    [Android WebAPK] Fix regression by https://codereview.chromium.org/2725813004/
    https://codereview.chromium.org/2725813004/ caused
    WebappDataStorage#updateTimeOfLastCheckForUpdatedWebManifest() to be called each
    time that the WebAPK is launched effectively disabling WebAPK updates.
    
    BUG=709226
    
    Review-Url: https://codereview.chromium.org/2801873004
    Cr-Commit-Position: refs/heads/master@{#464854}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkActivity.java
@@ -125,10 +125,6 @@ public class WebApkActivity extends WebappActivity {
     protected void onDeferredStartupWithStorage(WebappDataStorage storage) {
         super.onDeferredStartupWithStorage(storage);
 
-        // Initialize the time of the last is-update-needed check with the registration time. This
-        // prevents checking for updates on the first run.
-        storage.updateTimeOfLastCheckForUpdatedWebManifest();
-
         mUpdateManager = new WebApkUpdateManager(WebApkActivity.this, storage);
         mUpdateManager.updateIfNeeded(getActivityTab(),
                 (WebApkInfo) mWebappInfo);
@@ -145,6 +141,10 @@ public class WebApkActivity extends WebappActivity {
                 mWebappInfo.id(), new WebappRegistry.FetchWebappDataStorageCallback() {
                     @Override
                     public void onWebappDataStorageRetrieved(WebappDataStorage storage) {
+                        // Initialize the time of the last is-update-needed check with the
+                        // registration time. This prevents checking for updates on the first run.
+                        storage.updateTimeOfLastCheckForUpdatedWebManifest();
+
                         onDeferredStartupWithStorage(storage);
                     }
                 });
