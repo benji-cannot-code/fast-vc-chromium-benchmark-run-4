aa5daba82d8ddbf324f801b596c3cc0e07591a89
gogerald <gogerald@chromium.org>
2017-04-15 21:42:40 -0700

    Revert of [Payments] move //components/payments/content/*.mojom files to //components/payments/mojom (patchset #6 id:180001 of https://codereview.chromium.org/2811593009/ )
    Reason for revert:
    broke android build
    
    Original issue's description:
    > move //components/payments/content/*.mojom files to //components/payments/mojom
    >
    > To break dependency of //components/webdata_services on //components/payments/content after
    > landing https://codereview.chromium.org/2801513002/
    >
    > BUG=711952
    >
    > Review-Url: https://codereview.chromium.org/2811593009
    > Cr-Original-Commit-Position: refs/heads/master@{#464877}
    > Committed: https://chromium.googlesource.com/chromium/src/+/b1b326893990633387f90546607ce8271d151a3a
    > Review-Url: https://codereview.chromium.org/2811593009
    > Cr-Commit-Position: refs/heads/master@{#464882}
    > Committed: https://chromium.googlesource.com/chromium/src/+/3e1cedb46b52e6c09243993671a5a0ddb7a4eeed
    
    TBR=rouslan@chromium.org,cpu@chromium.org,dcheng@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=711952
    
    Review-Url: https://codereview.chromium.org/2816303003
    Cr-Commit-Position: refs/heads/master@{#464884}

--- components/payments/content/payment_manifest_parser_host.h
+++ components/payments/content/payment_manifest_parser_host.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_forward.h"
 #include "base/macros.h"
-#include "components/payments/mojom/payment_manifest_parser.mojom.h"
+#include "components/payments/content/payment_manifest_parser.mojom.h"
 #include "url/gurl.h"
 
 namespace content {
--- components/payments/content/payment_response_helper.h
+++ components/payments/content/payment_response_helper.h
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_PAYMENTS_CONTENT_PAYMENT_RESPONSE_HELPER_H_
 
 #include "base/macros.h"
+#include "components/payments/content/payment_request.mojom.h"
 #include "components/payments/core/payment_instrument.h"
-#include "components/payments/mojom/payment_request.mojom.h"
 
 namespace autofill {
 class AutofillProfile;
