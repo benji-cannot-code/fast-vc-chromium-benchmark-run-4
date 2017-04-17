6bc1bcf2ae6dffc0e7985c84da0f1a6126830243
sky <sky@chromium.org>
2017-04-17 15:48:48 -0700

    Makes functions in container_finder take aura::Window
    BUG=687656
    TEST=covered by tests
    R=msw@chromium.org
    
    Review-Url: https://codereview.chromium.org/2826453003
    Cr-Commit-Position: refs/heads/master@{#465049}

--- ash/wm/stacking_controller.cc
+++ ash/wm/stacking_controller.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/stacking_controller.h"
 
 #include "ash/wm/container_finder.h"
-#include "ash/wm_window.h"
+#include "ui/aura/window.h"
 
 namespace ash {
 
@@ -22,8 +22,7 @@ StackingController::~StackingController() {}
 
 aura::Window* StackingController::GetDefaultParent(aura::Window* window,
                                                    const gfx::Rect& bounds) {
-  return WmWindow::GetAuraWindow(
-      wm::GetDefaultParent(WmWindow::Get(window), bounds));
+  return wm::GetDefaultParent(window, bounds);
 }
 
 }  // namespace ash
