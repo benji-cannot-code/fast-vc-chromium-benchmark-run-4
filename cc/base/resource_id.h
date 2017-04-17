a95dab7a5e3f96dfba11d4c988253755863aa067
jbauman <jbauman@chromium.org>
2017-04-17 16:48:36 -0700

    Use flat_set for ResourceIdSet
    For DeclareUsedResourcesFromChild, the caller can accumulate into a
    vector and create a flat_set from that.
    ResourceProvider::wants_promotion_hints_set_ should always be small
    (around 1 entry) so it should be more efficient as a flat_set.
    
    Also remove Child::parent_to_child_map and store the child id directly
    in the Resource, to avoid unnecessary map lookups on resource creation
    and destruction.
    
    This improves the performance of SurfaceAggregatorPerfTest.* by 25-30%
    
    BUG=712295
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2826583002
    Cr-Commit-Position: refs/heads/master@{#465064}

--- cc/base/resource_id.h
+++ cc/base/resource_id.h
@@ -7,12 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CC_BASE_RESOURCE_ID_H_
 
 #include <stdint.h>
-#include <unordered_set>
+
+#include "base/containers/flat_set.h"
 
 namespace cc {
 
 using ResourceId = uint32_t;
-using ResourceIdSet = std::unordered_set<ResourceId>;
+using ResourceIdSet = base::flat_set<ResourceId>;
 
 }  // namespace cc
 
