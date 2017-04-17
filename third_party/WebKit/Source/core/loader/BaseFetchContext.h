3c4aed02f0517b10f0d04a6daaddc1ceb1ee7356
japhet <japhet@chromium.org>
2017-04-17 13:36:34 -0700

    Make BaseFetchContext, move trivial stuff
    BaseFetchContext holds an ExecutionContext. FrameFetchContext therefore
    doesn't need to hold a separate Document, and can downcast the
    ExecutionContext as needed.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2810873011
    Cr-Commit-Position: refs/heads/master@{#465006}

---
+++ third_party/WebKit/Source/core/loader/BaseFetchContext.h
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BaseFetchContext_h
+#define BaseFetchContext_h
+
+#include "core/CoreExport.h"
+#include "core/dom/ExecutionContext.h"
+#include "platform/heap/Handle.h"
+#include "platform/loader/fetch/FetchContext.h"
+#include "platform/loader/fetch/ResourceRequest.h"
+
+namespace blink {
+
+class CORE_EXPORT BaseFetchContext : public FetchContext {
+ public:
+  explicit BaseFetchContext(ExecutionContext*);
+  ~BaseFetchContext() override { execution_context_ = nullptr; }
+
+  void AddAdditionalRequestHeaders(ResourceRequest&,
+                                   FetchResourceType) override;
+  SecurityOrigin* GetSecurityOrigin() const override;
+
+  DECLARE_VIRTUAL_TRACE();
+
+ protected:
+  void PrintAccessDeniedMessage(const KURL&) const;
+  void AddCSPHeaderIfNecessary(Resource::Type, ResourceRequest&);
+
+  // FIXME: Oilpan: Ideally this should just be a traced Member but that will
+  // currently leak because ComputedStyle and its data are not on the heap.
+  // See crbug.com/383860 for details.
+  WeakMember<ExecutionContext> execution_context_;
+};
+
+}  // namespace blink
+
+#endif  // BaseFetchContext_h
--- third_party/WebKit/Source/core/loader/FrameFetchContext.h
+++ third_party/WebKit/Source/core/loader/FrameFetchContext.h
@@ -33,9 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define FrameFetchContext_h
 
 #include "core/CoreExport.h"
-#include "core/frame/csp/ContentSecurityPolicy.h"
+#include "core/loader/BaseFetchContext.h"
 #include "platform/heap/Handle.h"
-#include "platform/loader/fetch/FetchContext.h"
 #include "platform/loader/fetch/FetchParameters.h"
 #include "platform/loader/fetch/ResourceFetcher.h"
 #include "platform/loader/fetch/ResourceRequest.h"
@@ -52,7 +51,7 @@ class LocalFrameClient;
 class ResourceError;
 class ResourceResponse;
 
-class CORE_EXPORT FrameFetchContext final : public FetchContext {
+class CORE_EXPORT FrameFetchContext final : public BaseFetchContext {
  public:
   static ResourceFetcher* CreateFetcherFromDocumentLoader(
       DocumentLoader* loader) {
@@ -62,16 +61,11 @@ class CORE_EXPORT FrameFetchContext final : public FetchContext {
     return ResourceFetcher::Create(new FrameFetchContext(nullptr, document));
   }
 
-  static void ProvideDocumentToContext(FetchContext& context,
-                                       Document* document) {
-    DCHECK(document);
-    CHECK(context.IsLiveContext());
-    static_cast<FrameFetchContext&>(context).document_ = document;
-  }
+  static void ProvideDocumentToContext(FetchContext&, Document*);
 
-  ~FrameFetchContext();
+  ~FrameFetchContext() override;
 
-  bool IsLiveContext() { return true; }
+  bool IsFrameFetchContext() { return true; }
 
   void AddAdditionalRequestHeaders(ResourceRequest&,
                                    FetchResourceType) override;
@@ -147,7 +141,6 @@ class CORE_EXPORT FrameFetchContext final : public FetchContext {
   void SendImagePing(const KURL&) override;
   void AddConsoleMessage(const String&,
                          LogMessageType = kLogErrorMessage) const override;
-  SecurityOrigin* GetSecurityOrigin() const override;
 
   void PopulateResourceRequest(Resource::Type,
                                const ClientHintsPreferences&,
@@ -179,6 +172,7 @@ class CORE_EXPORT FrameFetchContext final : public FetchContext {
   // relevant document loader or frame in either cases without null-checks.
   // TODO(kinuko): Remove constness, these return non-const members.
   DocumentLoader* MasterDocumentLoader() const;
+  Document* GetDocument() const;
   LocalFrame* GetFrame() const;
   LocalFrameClient* GetLocalFrameClient() const;
 
@@ -186,7 +180,6 @@ class CORE_EXPORT FrameFetchContext final : public FetchContext {
 
   LocalFrame* FrameOfImportsController() const;
 
-  void PrintAccessDeniedMessage(const KURL&) const;
   ResourceRequestBlockedReason CanRequestInternal(
       Resource::Type,
       const ResourceRequest&,
@@ -196,12 +189,6 @@ class CORE_EXPORT FrameFetchContext final : public FetchContext {
       FetchParameters::OriginRestriction,
       ResourceRequest::RedirectStatus) const;
 
-  void AddCSPHeaderIfNecessary(Resource::Type, ResourceRequest&);
-
-  // FIXME: Oilpan: Ideally this should just be a traced Member but that will
-  // currently leak because ComputedStyle and its data are not on the heap.
-  // See crbug.com/383860 for details.
-  WeakMember<Document> document_;
   Member<DocumentLoader> document_loader_;
 };
 
