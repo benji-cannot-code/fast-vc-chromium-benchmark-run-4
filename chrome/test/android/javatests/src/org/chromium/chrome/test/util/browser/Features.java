49a25777ce9440b96b44106d00f8a2300a365dcf
mikecase <mikecase@chromium.org>
2017-04-17 15:42:28 -0700

    Move Features.java into support library so can be used downstream.
    Moving some annotations that will be used downstream and possibly
    by both Robolectric and instrumentaion tests into the support
    libraries.
    
    In the future, we should look into create instrumentation_support,
    robolectric_support, and shared_support libraries since not
    everything in base_java_test_support can be used by Robolectric
    tests.
    
    BUG=708199
    
    Review-Url: https://codereview.chromium.org/2806563002
    Cr-Commit-Position: refs/heads/master@{#465047}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/util/browser/Features.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/util/browser/Features.java
@@ -3,9 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser;
+package org.chromium.chrome.test.util.browser;
 
-import org.chromium.testing.local.AnnotationProcessor;
+import org.chromium.base.test.util.AnnotationProcessor;
+import org.chromium.chrome.browser.ChromeFeatureList;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
