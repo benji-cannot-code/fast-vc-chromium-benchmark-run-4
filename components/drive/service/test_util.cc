78ee5acfb71941a856355d1f0a47930d90f9dfd4
yamaguchi <yamaguchi@chromium.org>
2017-04-16 22:56:45 -0700

    Sort some using-declarations by lexicological order.
    BUG=none
    TEST=confirmed all relevant targets build successfully
    
    Review-Url: https://codereview.chromium.org/2823573002
    Cr-Commit-Position: refs/heads/master@{#464898}

--- components/drive/service/test_util.cc
+++ components/drive/service/test_util.cc
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/drive/drive_api_parser.h"
 #include "google_apis/drive/test_util.h"
 
-using google_apis::FileResource;
 using google_apis::DRIVE_OTHER_ERROR;
 using google_apis::DriveApiErrorCode;
+using google_apis::FileResource;
 using google_apis::HTTP_CREATED;
 
 namespace drive {
