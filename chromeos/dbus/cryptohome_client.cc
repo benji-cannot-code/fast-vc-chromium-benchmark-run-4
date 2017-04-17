18dbbac0266cd189d5818597e9b27b9feda5b060
fukino <fukino@chromium.org>
2017-04-17 10:39:09 -0700

    cros: Mount the existing eCryptfs vault to start migration.
    Before we call cryptohome's MigrateToDircrypto method, we need to mount
    existing eCryptfs valut to start migration by calling MountEx method with
    |to_migrate_from_ecryptfs| mount flag.
    
    Additionally, this CL removes |auth| argument from MigrateToDircrypto method,
    since it was removed from DBUS call in the latest cryptohome.
    https://chromium-review.googlesource.com/#/c/472409/6/cryptohome/cryptohome.xml
    
    BUG=711458
    
    Review-Url: https://codereview.chromium.org/2818393002
    Cr-Commit-Position: refs/heads/master@{#464945}

--- chromeos/dbus/cryptohome_client.cc
+++ chromeos/dbus/cryptohome_client.cc
@@ -923,7 +923,6 @@ class CryptohomeClientImpl : public CryptohomeClient {
   }
 
   void MigrateToDircrypto(const cryptohome::Identification& cryptohome_id,
-                          const cryptohome::AuthorizationRequest& auth,
                           const VoidDBusMethodCallback& callback) override {
     dbus::MethodCall method_call(cryptohome::kCryptohomeInterface,
                                  cryptohome::kCryptohomeMigrateToDircrypto);
@@ -933,7 +932,6 @@ class CryptohomeClientImpl : public CryptohomeClient {
 
     dbus::MessageWriter writer(&method_call);
     writer.AppendProtoAsArrayOfBytes(id_proto);
-    writer.AppendProtoAsArrayOfBytes(auth);
 
     // The migration progress takes unpredicatable time depending on the
     // user file size and the number. Setting the time limit to infinite.
