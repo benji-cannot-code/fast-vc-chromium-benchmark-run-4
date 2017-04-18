4d97443eda4160c6ba417e37f75433f7c4360311
rdevlin.cronin <rdevlin.cronin@chromium.org>
2017-04-18 14:03:47 -0700

    [Extensions Bindings] Save custom bindings loaded on the modules object
    The ModuleSystem has a lazy loading system that saves loaded modules on
    as a private on the global object, which serves as caching (if the
    property doesn't exist, it loads the module). The custom binding modules
    loaded through the native bindings system weren't being saved, which
    resulted in reloading them if they were require()d or used via
    ModuleSystem::CallModuleMethod.
    
    Correctly cache these bindings on the object to correct this, and add
    a test for the same.
    
    BUG=653596
    
    Review-Url: https://codereview.chromium.org/2822483002
    Cr-Commit-Position: refs/heads/master@{#465371}

--- chrome/test/data/extensions/api_test/native_bindings/platform_app/test.js
+++ chrome/test/data/extensions/api_test/native_bindings/platform_app/test.js
@@ -3,6 +3,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+var appWindow;
+
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('main.html', {}, function () {});
+  chrome.app.window.create('main.html', {}, function (win) {appWindow = win});
+});
+
+chrome.test.sendMessage('ready', function() {
+  try {
+    // The onClosed event is dispatched by a call to the internal
+    // onAppWindowClosed method in the app.window custom bindings. Getting this
+    // event signals that we can call into methods of custom bindings modules
+    // for APIs.
+    appWindow.onClosed.addListener(function() {
+      chrome.test.sendMessage('success');
+    });
+    appWindow.close();
+  } catch (e) {
+    chrome.test.sendMessage('failure: ' + e);
+  }
 });
