e94b651a739f6158cb7468e4d4bf6e91c8ffb481
catmullings <catmullings@chromium.org>
2017-04-18 12:17:46 -0700

    Update style of md-extensions keyboard settings page
    BUG=
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2791783002
    Cr-Commit-Position: refs/heads/master@{#465320}

--- chrome/test/data/webui/extensions/extension_shortcut_input_test.js
+++ chrome/test/data/webui/extensions/extension_shortcut_input_test.js
@@ -25,8 +25,8 @@ cr.define('extension_shortcut_input_tests', function() {
 
       test(assert(TestNames.Basic), function() {
         var field = input.$['input'];
-        var fieldText = function() { return field.textContent.trim(); };
-        expectEquals('Not set', fieldText());
+        var fieldText = function() { return field.value; };
+        expectEquals('', fieldText());
         var isClearVisible = extension_test_util.isVisible.bind(
                                  null, input, '#clear', false);
         expectFalse(isClearVisible());
@@ -38,7 +38,7 @@ cr.define('extension_shortcut_input_tests', function() {
           MockInteractions.tap(field);
           startCaptureListener.verify();
         }
-        expectEquals('Type a shortcut', fieldText());
+        expectEquals('', fieldText());
         expectTrue(input.capturing_);
         expectFalse(isClearVisible());
 
