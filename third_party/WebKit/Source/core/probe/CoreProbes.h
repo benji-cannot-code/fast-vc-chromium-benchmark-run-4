99b5f3c2675c2212f214e33388999b68bbc138a1
alph <alph@chromium.org>
2017-04-17 17:06:47 -0700

    [instrumentation] Fix probe/BUILD.gn dependencies
    There are two targets. One for generating sources and another for compiling files.
    
    BUG=709568
    
    Review-Url: https://codereview.chromium.org/2807123005
    Cr-Commit-Position: refs/heads/master@{#465070}

--- third_party/WebKit/Source/core/probe/CoreProbes.h
+++ third_party/WebKit/Source/core/probe/CoreProbes.h
@@ -33,9 +33,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CoreProbes_h
 
 #include "core/CoreExport.h"
+#include "core/animation/Animation.h"
 #include "core/dom/Document.h"
-#include "core/dom/Node.h"
+#include "core/dom/PseudoElement.h"
 #include "core/frame/LocalFrame.h"
+#include "core/html/HTMLSlotElement.h"
 #include "core/page/ChromeClient.h"
 #include "platform/probe/PlatformProbes.h"
 
