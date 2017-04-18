101299c14fd8095543864473cd4371456b7de4b5
pkalinnikov <pkalinnikov@chromium.org>
2017-04-18 10:38:44 -0700

    Examine WebSocket schemes in URLPattern tests.
    BUG=
    
    Review-Url: https://codereview.chromium.org/2822013003
    Cr-Commit-Position: refs/heads/master@{#465282}

--- extensions/common/url_pattern_unittest.cc
+++ extensions/common/url_pattern_unittest.cc
@@ -18,13 +18,11 @@ namespace {
 // See url_pattern.h for examples of valid and invalid patterns.
 
 static const int kAllSchemes =
-    URLPattern::SCHEME_HTTP |
-    URLPattern::SCHEME_HTTPS |
-    URLPattern::SCHEME_FILE |
-    URLPattern::SCHEME_FTP |
-    URLPattern::SCHEME_CHROMEUI |
-    URLPattern::SCHEME_EXTENSION |
-    URLPattern::SCHEME_FILESYSTEM;
+    URLPattern::SCHEME_HTTP | URLPattern::SCHEME_HTTPS |
+    URLPattern::SCHEME_FILE | URLPattern::SCHEME_FTP |
+    URLPattern::SCHEME_CHROMEUI | URLPattern::SCHEME_EXTENSION |
+    URLPattern::SCHEME_FILESYSTEM | URLPattern::SCHEME_WS |
+    URLPattern::SCHEME_WSS;
 
 TEST(ExtensionURLPatternTest, ParseInvalid) {
   const struct {
@@ -543,7 +541,7 @@ TEST(ExtensionURLPatternTest, ConvertToExplicitSchemes) {
       URLPattern::SCHEME_FTP,
       "http://google.com/monkey").ConvertToExplicitSchemes());
 
-  ASSERT_EQ(7u, all_urls.size());
+  ASSERT_EQ(9u, all_urls.size());
   ASSERT_EQ(2u, all_schemes.size());
   ASSERT_EQ(1u, monkey.size());
 
@@ -552,6 +550,10 @@ TEST(ExtensionURLPatternTest, ConvertToExplicitSchemes) {
   EXPECT_EQ("file:///*", all_urls[2].GetAsString());
   EXPECT_EQ("ftp://*/*", all_urls[3].GetAsString());
   EXPECT_EQ("chrome://*/*", all_urls[4].GetAsString());
+  EXPECT_EQ("chrome-extension://*/*", all_urls[5].GetAsString());
+  EXPECT_EQ("filesystem://*/*", all_urls[6].GetAsString());
+  EXPECT_EQ("ws://*/*", all_urls[7].GetAsString());
+  EXPECT_EQ("wss://*/*", all_urls[8].GetAsString());
 
   EXPECT_EQ("http://google.com/foo", all_schemes[0].GetAsString());
   EXPECT_EQ("https://google.com/foo", all_schemes[1].GetAsString());
