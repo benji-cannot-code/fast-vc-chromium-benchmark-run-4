493088813032bac0aeb6ff344fc76ac457e6239b
mahmadi <mahmadi@chromium.org>
2017-04-18 11:54:01 -0700

    [Payment Request] Refactors selector view controllers
    Replaces the ShippingAddressSelectionViewController with
    PaymentRequestSelectorViewController.
    
    BUG=602666
    
    Review-Url: https://codereview.chromium.org/2827453004
    Cr-Commit-Position: refs/heads/master@{#465307}

--- ios/chrome/browser/payments/payment_request_selector_view_controller.mm
+++ ios/chrome/browser/payments/payment_request_selector_view_controller.mm
@@ -17,9 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/collection_view/collection_view_model.h"
 #import "ios/chrome/browser/ui/colors/MDCPalette+CrAdditions.h"
 #import "ios/chrome/browser/ui/icons/chrome_icon.h"
-#include "ios/chrome/browser/ui/uikit_ui_util.h"
 #include "ios/chrome/grit/ios_strings.h"
-#include "ios/chrome/grit/ios_theme_resources.h"
+#import "ios/third_party/material_components_ios/src/components/Typography/src/MaterialTypography.h"
 #include "ui/base/l10n/l10n_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -129,6 +128,35 @@ typedef NS_ENUM(NSInteger, ItemType) {
   self.styler.cellStyle = MDCCollectionViewCellStyleCard;
   self.styler.separatorInset =
       UIEdgeInsetsMake(0, kSeparatorEdgeInset, 0, kSeparatorEdgeInset);
+}
+
+#pragma mark UICollectionViewDataSource
+
+- (UICollectionViewCell*)collectionView:(UICollectionView*)collectionView
+                 cellForItemAtIndexPath:(nonnull NSIndexPath*)indexPath {
+  CollectionViewModel* model = self.collectionViewModel;
+
+  UICollectionViewCell* cell =
+      [super collectionView:collectionView cellForItemAtIndexPath:indexPath];
+
+  NSInteger itemType = [model itemTypeForIndexPath:indexPath];
+  switch (itemType) {
+    case ItemTypeHeader: {
+      if ([cell isKindOfClass:[PaymentsTextCell class]]) {
+        PaymentsTextCell* textCell =
+            base::mac::ObjCCastStrict<PaymentsTextCell>(cell);
+        textCell.textLabel.textColor =
+            self.dataSource.state == PaymentRequestSelectorStateError
+                ? [[MDCPalette cr_redPalette] tint600]
+                : [[MDCPalette greyPalette] tint600];
+      }
+      break;
+    }
+    default:
+      break;
+  }
+
+  return cell;
 }
 
 #pragma mark UICollectionViewDelegate
