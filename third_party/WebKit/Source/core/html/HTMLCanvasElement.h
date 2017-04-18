0a01008fccd01bffafddbe4f32890e49b732d149
eseckler <eseckler@chromium.org>
2017-04-18 05:18:40 -0700

    [blink] Request compositing update when canvas hibernates/wakes up.
    Not requesting the update can lead to the canvas not
    being shown, as in the bug referenced below.
    
    BUG=708445
    
    Review-Url: https://codereview.chromium.org/2808493002
    Cr-Commit-Position: refs/heads/master@{#465206}

--- third_party/WebKit/Source/core/html/HTMLCanvasElement.h
+++ third_party/WebKit/Source/core/html/HTMLCanvasElement.h
@@ -205,6 +205,7 @@ class CORE_EXPORT HTMLCanvasElement final
   void NotifySurfaceInvalid() override;
   void DidDisableAcceleration() override;
   void RestoreCanvasMatrixClipStack(PaintCanvas*) const override;
+  void SetNeedsCompositingUpdate() override;
 
   // ImageBitmapSource implementation
   IntSize BitmapSourceSize() const override;
