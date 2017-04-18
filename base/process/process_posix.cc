6dfdf8a8fc4da8f64a71f3d4a8ec0396ddcff63e
raphael.kubo.da.costa <raphael.kubo.da.costa@intel.com>
2017-04-18 07:11:14 -0700

    IWYU: Include <signal.h> in process_posix.cc.
    process_posix.cc references SIGKILL, so stop getting it indirectly by
    explicitly including <signal.h>.
    
    R=dcheng@chromium.org,thakis@chromium.org
    
    Review-Url: https://codereview.chromium.org/2821903005
    Cr-Commit-Position: refs/heads/master@{#465223}

--- base/process/process_posix.cc
+++ base/process/process_posix.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/process.h"
 
 #include <errno.h>
+#include <signal.h>
 #include <stdint.h>
 #include <sys/resource.h>
 #include <sys/wait.h>
