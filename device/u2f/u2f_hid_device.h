b22f91b0cd76b2b2303a78cdb9aea90d3a1f0abf
piperc <piperc@chromium.org>
2017-04-17 17:13:17 -0700

    Add timeout task to U2F HID state machine
    Setup timeout tasks for any calls to HID layer. If a particular
    device hangs or the OS does not respond in a timely manner,
    continue trying other devices.
    
    BUG=697282
    
    Review-Url: https://codereview.chromium.org/2824803003
    Cr-Commit-Position: refs/heads/master@{#465072}

--- device/u2f/u2f_hid_device.h
+++ device/u2f/u2f_hid_device.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <list>
 
+#include "base/cancelable_callback.h"
 #include "device/hid/hid_service.h"
 #include "u2f_device.h"
 
@@ -24,7 +25,7 @@ class HidDeviceInfo;
 class U2fHidDevice : public U2fDevice {
  public:
   U2fHidDevice(scoped_refptr<HidDeviceInfo>);
-  ~U2fHidDevice();
+  ~U2fHidDevice() final;
 
   // Send a U2f command to this device
   void DeviceTransact(std::unique_ptr<U2fApduCommand> command,
@@ -86,8 +87,13 @@ class U2fHidDevice : public U2fDevice {
   void OnWink(const WinkCallback& callback,
               bool success,
               std::unique_ptr<U2fMessage> response);
+  void ArmTimeout(const DeviceCallback& callback);
+  void OnTimeout(const DeviceCallback& callback);
+  void OnDeviceTransact(bool success,
+                        std::unique_ptr<U2fApduResponse> response);
 
   State state_;
+  base::CancelableClosure timeout_callback_;
   std::list<std::pair<std::unique_ptr<U2fApduCommand>, DeviceCallback>>
       pending_transactions_;
   scoped_refptr<HidDeviceInfo> device_info_;
