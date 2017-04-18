cdca477abf37da58e978b4196ca271789f025e86
Jeff Carpenter <jeffcarp@chromium.org>
2017-04-18 15:28:02 -0700

    [WPT Export] Move pull request methods into WPTGitHub
    Also lazy-load pull requests from the GitHub API the first time a
    method that acts upon pull requests is called.
    
    Bug:
    
    Change-Id: I5019eeb21912f82f7666c5874a419fa6410806c2
    Reviewed-on: https://chromium-review.googlesource.com/478539
    Commit-Queue: Jeff Carpenter <jeffcarp@chromium.org>
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#465446}

--- third_party/WebKit/Tools/Scripts/webkitpy/w3c/test_exporter_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/w3c/test_exporter_unittest.py
@@ -37,7 +37,11 @@ class TestExporterTest(unittest.TestCase):
         ]
         test_exporter.run()
 
-        self.assertEqual(test_exporter.wpt_github.calls, ['all_pull_requests'])
+        self.assertEqual(test_exporter.wpt_github.calls, [
+            'pr_with_position',
+            'pr_with_position',
+            'pr_with_position',
+        ])
 
     def test_creates_pull_request_for_all_exportable_commits(self):
         host = MockHost()
@@ -72,9 +76,11 @@ class TestExporterTest(unittest.TestCase):
         test_exporter.run()
 
         self.assertEqual(test_exporter.wpt_github.calls, [
-            'all_pull_requests',
+            'pr_with_position',
             'create_pr',
+            'pr_with_position',
             'create_pr',
+            'pr_with_position',
             'create_pr',
         ])
         self.assertEqual(test_exporter.wpt_github.pull_requests_created, [
@@ -123,10 +129,13 @@ class TestExporterTest(unittest.TestCase):
         ]
         test_exporter.run()
         self.assertEqual(test_exporter.wpt_github.calls, [
-            'all_pull_requests',
+            'pr_with_position',
             'get_pr_branch',
             'merge_pull_request',
+            'pr_with_position',
             'create_pr',
+            'pr_with_position',
+            'pr_with_position',
             'get_pr_branch',
             'merge_pull_request',
             'delete_remote_branch',
