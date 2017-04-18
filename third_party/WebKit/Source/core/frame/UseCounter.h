64f38f250df80824daabab8556765c7d6e64d306
timloh <timloh@chromium.org>
2017-04-17 23:54:49 -0700

    Add comments in UseCounter.h for M58 and M59 branch cutoffs
    M58 and M59 branches were versioned in the following commits:
    58 version increment: 2b7c19d3
    59 version increment: 7ee0c5a1
    
    UseCounter.h can be seen at these versions as follows:
    git show 2b7c19d3:third_party/WebKit/Source/core/frame/UseCounter.h
    git show 7ee0c5a1:third_party/WebKit/Source/core/frame/UseCounter.h
    
    Review-Url: https://codereview.chromium.org/2824943003
    Cr-Commit-Position: refs/heads/master@{#465160}

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -1459,6 +1459,8 @@ class CORE_EXPORT UseCounter {
     kHTMLEmbedElementGetter = 1840,
     kHTMLEmbedElementSetter = 1841,
     kTransformUsesBoxSizeOnSVG = 1842,
+    // The above items are available in M58 branch.
+
     kScrollByKeyboardArrowKeys = 1843,
     kScrollByKeyboardPageUpDownKeys = 1844,
     kScrollByKeyboardHomeEndKeys = 1845,
@@ -1574,6 +1576,8 @@ class CORE_EXPORT UseCounter {
     kMediaStreamConstraintsGoogDAEchoCancellation = 1958,
     kMediaStreamConstraintsGoogNoiseReduction = 1959,
     kMediaStreamConstraintsGoogPowerLineFrequency = 1960,
+    // The above items are available in M59 branch.
+
     kViewportFixedPositionUnderFilter = 1961,
 
     // Add new features immediately above this line. Don't change assigned
