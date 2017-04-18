af8cd04070b1d4c512ea24f722edc312bfc9abd5
reillyg <reillyg@chromium.org>
2017-04-18 12:38:27 -0700

    Ensure tests don't depend on fake devices being added synchronously
    As specified navigator.usb.test.addFakeDevice and removeFakeDevice do
    not synchronously add and remove the fake device however the Mojo-based
    polyfill does. This patch adds a delay and fixes the WebUSB tests so
    that they do not depend on this behaviors.
    
    BUG=705734
    
    Review-Url: https://codereview.chromium.org/2816663002
    Cr-Commit-Position: refs/heads/master@{#465329}

--- third_party/WebKit/LayoutTests/usb/resources/webusb-test.js
+++ third_party/WebKit/LayoutTests/usb/resources/webusb-test.js
@@ -9,14 +9,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // interface USBTest {
 //   attribute EventHandler ondeviceclose;
-//   attribute DOMString? chosenDevice;
+//   attribute FakeUSBDevice? chosenDevice;
 //   attribute FrozenArray<USBDeviceFilter>? lastFilters;
 //
 //   Promise<void> initialize();
 //   Promise<void> attachToWindow(Window window);
-//   DOMString addFakeDevice(FakeUSBDeviceInit deviceInit);
-//   void removeFakeDevice(DOMString);
+//   FakeUSBDevice addFakeDevice(FakeUSBDeviceInit deviceInit);
 //   void reset();
+// };
+//
+// interface FakeUSBDevice {
+//   void disconnect();
 // };
 //
 // dictionary FakeUSBDeviceInit {
@@ -77,6 +80,7 @@ let g_initializePromise = null;
 let g_chooserService = null;
 let g_deviceManager = null;
 let g_closeListener = null;
+let g_nextGuid = 0;
 
 function fakeDeviceInitToDeviceInfo(guid, init) {
   let deviceInfo = {
@@ -348,33 +352,40 @@ class FakeDeviceManager {
     this.bindingSet_ =
         new mojo.bindings.BindingSet(mojo.deviceManager.DeviceManager);
     this.devices_ = new Map();
+    this.devicesByGuid_ = new Map();
     this.client_ = null;
+    this.nextGuid_ = 0;
   }
 
   addBinding(handle) {
     this.bindingSet_.addBinding(this, handle);
   }
 
-  addDevice(info) {
+  addDevice(fakeDevice, info) {
     let device = {
-      guid: this.nextGuid_++ + "",
+      fakeDevice: fakeDevice,
+      guid: (this.nextGuid_++).toString(),
       info: info,
       bindingArray: []
     };
-    this.devices_.set(device.guid, device);
+    this.devices_.set(fakeDevice, device);
+    this.devicesByGuid_.set(device.guid, device);
     if (this.client_)
       this.client_.onDeviceAdded(fakeDeviceInitToDeviceInfo(device.guid, info));
-    return device.guid;
   }
 
-  removeDevice(guid) {
-    let device = this.devices_.get(guid);
+  removeDevice(fakeDevice) {
+    let device = this.devices_.get(fakeDevice);
+    if (!device)
+      throw new Error('Cannot remove unknown device.');
+
     for (var binding of device.bindingArray)
       binding.close();
-    this.devices_.delete(guid);
+    this.devices_.delete(device.fakeDevice);
+    this.devicesByGuid_.delete(device.guid);
     if (this.client_) {
       this.client_.onDeviceRemoved(
-          fakeDeviceInitToDeviceInfo(guid, device.info));
+          fakeDeviceInitToDeviceInfo(device.guid, device.info));
     }
   }
 
@@ -386,6 +397,7 @@ class FakeDeviceManager {
           fakeDeviceInitToDeviceInfo(device.guid, device.info));
     });
     this.devices_.clear();
+    this.devicesByGuid_.clear();
   }
 
   getDevices(options) {
@@ -397,13 +409,13 @@ class FakeDeviceManager {
   }
 
   getDevice(guid, request) {
-    let device = this.devices_.get(guid);
+    let device = this.devicesByGuid_.get(guid);
     if (device) {
       let binding = new mojo.bindings.Binding(
           mojo.device.Device, new FakeDevice(device.info), request);
       binding.setConnectionErrorHandler(() => {
         if (g_closeListener)
-          g_closeListener(guid);
+          g_closeListener(device.fakeDevice);
       });
       device.bindingArray.push(binding);
     } else {
@@ -428,13 +440,13 @@ class FakeChooserService {
     this.bindingSet_.addBinding(this, handle);
   }
 
-  setChosenDevice(guid) {
-    this.chosenDeviceGuid_ = guid;
+  setChosenDevice(fakeDevice) {
+    this.chosenDevice_ = fakeDevice;
   }
 
   getPermission(deviceFilters) {
     this.lastFilters_ = convertMojoDeviceFilters(deviceFilters);
-    let device = g_deviceManager.devices_.get(this.chosenDeviceGuid_);
+    let device = g_deviceManager.devices_.get(this.chosenDevice_);
     if (device) {
       return Promise.resolve({
         result: fakeDeviceInitToDeviceInfo(device.guid, device.info)
@@ -442,6 +454,13 @@ class FakeChooserService {
     } else {
       return Promise.resolve({ result: null });
     }
+  }
+}
+
+// Unlike FakeDevice this class is exported to callers of USBTest.addFakeDevice.
+class FakeUSBDevice {
+  disconnect() {
+    setTimeout(() => g_deviceManager.removeDevice(this), 0);
   }
 }
 
@@ -523,25 +542,23 @@ class USBTest {
     if (!g_deviceManager)
       throw new Error('Call initialize() before addFakeDevice().');
 
-    return g_deviceManager.addDevice(deviceInit);
-  }
-
-  removeFakeDevice(guid) {
-    if (!g_deviceManager)
-      throw new Error('Call initialize() before removeFakeDevice().');
-
-    return g_deviceManager.removeDevice(guid);
+    // |addDevice| and |removeDevice| are called in a setTimeout callback so
+    // that tests do not rely on the device being immediately available which
+    // may not be true for all implementations of this test API.
+    let fakeDevice = new FakeUSBDevice();
+    setTimeout(() => g_deviceManager.addDevice(fakeDevice, deviceInit), 0);
+    return fakeDevice;
   }
 
   set ondeviceclose(func) {
     g_closeListener = func;
   }
 
-  set chosenDevice(guid) {
+  set chosenDevice(fakeDevice) {
     if (!g_chooserService)
       throw new Error('Call initialize() before setting chosenDevice.');
 
-    g_chooserService.setChosenDevice(guid);
+    g_chooserService.setChosenDevice(fakeDevice);
   }
 
   get lastFilters() {
