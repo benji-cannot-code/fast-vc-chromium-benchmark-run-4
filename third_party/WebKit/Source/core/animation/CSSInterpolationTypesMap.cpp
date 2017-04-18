ce08da54389a9ffb38aa345b0254b86c3fed5ed0
alancutter <alancutter@chromium.org>
2017-04-17 19:44:03 -0700

    Support animating <resolution> custom properties
    This patch adds support for animating custom properties registered with
    the <resolution> syntax.
    
    BUG=671904
    
    Review-Url: https://codereview.chromium.org/2812523002
    Cr-Commit-Position: refs/heads/master@{#465115}

--- third_party/WebKit/Source/core/animation/CSSInterpolationTypesMap.cpp
+++ third_party/WebKit/Source/core/animation/CSSInterpolationTypesMap.cpp
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/animation/CSSPathInterpolationType.h"
 #include "core/animation/CSSPositionAxisListInterpolationType.h"
 #include "core/animation/CSSPositionInterpolationType.h"
+#include "core/animation/CSSResolutionInterpolationType.h"
 #include "core/animation/CSSRotateInterpolationType.h"
 #include "core/animation/CSSScaleInterpolationType.h"
 #include "core/animation/CSSShadowListInterpolationType.h"
@@ -357,13 +358,16 @@ CSSInterpolationTypesMap::CreateCSSInterpolationTypesForSyntax(
       case CSSSyntaxType::kNumber:
         result.push_back(WTF::MakeUnique<CSSNumberInterpolationType>(property));
         break;
+      case CSSSyntaxType::kResolution:
+        result.push_back(
+            WTF::MakeUnique<CSSResolutionInterpolationType>(property));
+        break;
       case CSSSyntaxType::kTime:
         result.push_back(WTF::MakeUnique<CSSTimeInterpolationType>(property));
         break;
       case CSSSyntaxType::kImage:
       case CSSSyntaxType::kUrl:
       case CSSSyntaxType::kInteger:
-      case CSSSyntaxType::kResolution:
       case CSSSyntaxType::kTransformFunction:
         // TODO(alancutter): Support smooth interpolation of these types.
         break;
