6f42574e16377f8ad5a0c19cbc8e2bd931863967
ccameron <ccameron@chromium.org>
2017-04-17 17:05:45 -0700

    Make Canvas2DLayerBridge use SkColorSpaceXformCanvas
    This makes legacy mode canvases render into sRGB (transforming images
    as needed).
    
    BUG=709695
    
    Review-Url: https://codereview.chromium.org/2820823004
    Cr-Commit-Position: refs/heads/master@{#465069}

--- third_party/WebKit/Source/platform/graphics/Canvas2DLayerBridge.cpp
+++ third_party/WebKit/Source/platform/graphics/Canvas2DLayerBridge.cpp
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/platform/WebScheduler.h"
 #include "public/platform/WebTraceLocation.h"
 #include "skia/ext/texture_handle.h"
+#include "third_party/skia/include/core/SkColorSpaceXformCanvas.h"
 #include "third_party/skia/include/core/SkData.h"
 #include "third_party/skia/include/core/SkSurface.h"
 #include "third_party/skia/include/gpu/GrContext.h"
@@ -780,8 +781,20 @@ void Canvas2DLayerBridge::FlushRecordingOnly() {
 
   if (have_recorded_draw_commands_ && GetOrCreateSurface()) {
     TRACE_EVENT0("cc", "Canvas2DLayerBridge::flushRecordingOnly");
-    recorder_->finishRecordingAsPicture()->playback(
-        GetOrCreateSurface()->getCanvas());
+
+    // For legacy canvases, transform all input colors and images to the target
+    // space using a SkCreateColorSpaceXformCanvas. This ensures blending will
+    // be done using target space pixel values.
+    SkCanvas* canvas = GetOrCreateSurface()->getCanvas();
+    std::unique_ptr<SkCanvas> color_transform_canvas;
+    if (RuntimeEnabledFeatures::colorCorrectRenderingEnabled() &&
+        !sk_surfaces_use_color_space_) {
+      color_transform_canvas =
+          SkCreateColorSpaceXformCanvas(canvas, color_space_.ToSkColorSpace());
+      canvas = color_transform_canvas.get();
+    }
+
+    recorder_->finishRecordingAsPicture()->playback(canvas);
     if (is_deferral_enabled_)
       StartRecording();
     have_recorded_draw_commands_ = false;
