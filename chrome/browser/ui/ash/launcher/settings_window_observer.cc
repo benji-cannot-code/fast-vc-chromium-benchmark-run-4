2cd352e9e59fac4a6f9f41cc95d4709471412171
estade <estade@chromium.org>
2017-04-18 13:43:25 -0700

    Use MD settings icon in ash shelf unconditionally.
    The shelf is already fully materialized. The icon appears on the shelf.
    No reason to use a different icon for pre-MD settings.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2794333003
    Cr-Commit-Position: refs/heads/master@{#465356}

--- chrome/browser/ui/ash/launcher/settings_window_observer.cc
+++ chrome/browser/ui/ash/launcher/settings_window_observer.cc
@@ -8,12 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shelf_item.h"
 #include "ash/public/cpp/window_properties.h"
 #include "ash/resources/grit/ash_resources.h"
-#include "base/feature_list.h"
 #include "base/memory/ptr_util.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/settings_window_manager.h"
-#include "chrome/common/chrome_features.h"
 #include "components/strings/grit/components_strings.h"
 #include "services/ui/public/interfaces/window_manager.mojom.h"
 #include "ui/aura/client/aura_constants.h"
@@ -57,11 +55,9 @@ void SettingsWindowObserver::OnNewSettingsWindow(Browser* settings_browser) {
   window->SetTitle(l10n_util::GetStringUTF16(IDS_SETTINGS_TITLE));
   window->SetProperty<int>(ash::kShelfItemTypeKey, ash::TYPE_DIALOG);
   ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
-  gfx::ImageSkia* icon =
-      base::FeatureList::IsEnabled(features::kMaterialDesignSettings)
-          ? rb.GetImageSkiaNamed(IDR_ASH_SHELF_ICON_MD_SETTINGS)
-          : rb.GetImageSkiaNamed(IDR_ASH_SHELF_ICON_SETTINGS);
   // The new gfx::ImageSkia instance is owned by the window itself.
-  window->SetProperty(aura::client::kWindowIconKey, new gfx::ImageSkia(*icon));
+  window->SetProperty(
+      aura::client::kWindowIconKey,
+      new gfx::ImageSkia(*rb.GetImageSkiaNamed(IDR_ASH_SHELF_ICON_SETTINGS)));
   aura_window_tracker_->Add(window);
 }
