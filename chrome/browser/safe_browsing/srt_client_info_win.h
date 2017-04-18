ec954b11bbae2967d3ca9be197b87f379297c6f5
ftirelo <ftirelo@chromium.org>
2017-04-18 14:32:15 -0700

    Move shared constants to //components/chrome_cleaner
    BUG=709035
    
    Review-Url: https://codereview.chromium.org/2812223003
    Cr-Commit-Position: refs/heads/master@{#465380}

--- chrome/browser/safe_browsing/srt_client_info_win.h
+++ chrome/browser/safe_browsing/srt_client_info_win.h
@@ -8,11 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace safe_browsing {
 
-extern const char kChromeChannelSwitch[];
-extern const char kChromeVersionSwitch[];
-extern const char kEnableCrashReporting[];
-extern const char kExtendedSafeBrowsingEnabledSwitch[];
-
 // Encodes Chrome's channel as an integer to be passed to the SRT on the command
 // line. The SRT binary expects to receive Chrome's channel encoded as:
 //     0: unknown; 1: canary; 2: dev; 3: beta; 4: stable.
