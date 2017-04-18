51edab55cdc9f48356e704ffc794ebfc5d25d1c9
gambard <gambard@chromium.org>
2017-04-18 05:41:05 -0700

    [ObjC ARC] Converts components/favicon/ios:ios to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2823953003
    Cr-Commit-Position: refs/heads/master@{#465208}

--- components/favicon/ios/web_favicon_driver.mm
+++ components/favicon/ios/web_favicon_driver.mm
@@ -19,6 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/gfx/image/image.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 DEFINE_WEB_STATE_USER_DATA_KEY(favicon::WebFaviconDriver);
 
 // Callback for the download of favicon.
