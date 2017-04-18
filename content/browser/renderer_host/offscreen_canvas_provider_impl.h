475b5c3bc0f1562028fec9d8c8ffb0804d89411e
kylechar <kylechar@chromium.org>
2017-04-18 11:47:33 -0700

    Check renderer client id for offscreen canvas.
    Add a check before creating an OffscreenCanvasSurfaceImpl or
    MojoCompositorFrameSink for offscreen canvas. The renderer should only
    be using FrameSinkIds within it's own namespace.
    
    Without this check, a malicious renderer could learn about SurfaceIds
    from other clients by guessing the FrameSinkId. After creating an
    OffscreenCanvasSurfaceImpl any new SurfaceIds for the FrameSinkId get
    forwarded back to the renderer.
    
    BUG=664547
    TBR=piman@chromium.org
    
    Review-Url: https://codereview.chromium.org/2811413002
    Cr-Commit-Position: refs/heads/master@{#465300}

--- content/browser/renderer_host/offscreen_canvas_provider_impl.h
+++ content/browser/renderer_host/offscreen_canvas_provider_impl.h
@@ -16,7 +16,7 @@ namespace content {
 class OffscreenCanvasProviderImpl
     : public blink::mojom::OffscreenCanvasProvider {
  public:
-  OffscreenCanvasProviderImpl();
+  explicit OffscreenCanvasProviderImpl(uint32_t renderer_client_id);
   ~OffscreenCanvasProviderImpl() override;
 
   void Add(blink::mojom::OffscreenCanvasProviderRequest request);
@@ -33,6 +33,9 @@ class OffscreenCanvasProviderImpl
       cc::mojom::MojoCompositorFrameSinkRequest request) override;
 
  private:
+  // FrameSinkIds for offscreen canvas must use the renderer client id.
+  const uint32_t renderer_client_id_;
+
   mojo::BindingSet<blink::mojom::OffscreenCanvasProvider> bindings_;
 
   DISALLOW_COPY_AND_ASSIGN(OffscreenCanvasProviderImpl);
