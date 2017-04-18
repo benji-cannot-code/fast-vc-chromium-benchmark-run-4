c318dae1536e88747f342d99d66f5ecc4f97b64a
tetsui <tetsui@google.com>
2017-04-17 19:31:42 -0700

    Enable once flaky tests in Chrome OS file manager.
    These tests were previously flaky and there seems to be a shared issue.
    However, they do not reproduce anymore on the latest master branch.
    
    BUG=701022, 701021, 700834, 700502, 700448, 700430
    TEST=out/Release/browser_tests '--gtest_filter=*FileManager*' --gtest_repeat=100
    
    Review-Url: https://codereview.chromium.org/2819373003
    Cr-Commit-Position: refs/heads/master@{#465105}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -73,7 +73,7 @@ IN_PROC_BROWSER_TEST_P(FileManagerBrowserTestWithLegacyEventDispatch, Test) {
 #define MAYBE_FileDisplay FileDisplay
 #endif
 WRAPPED_INSTANTIATE_TEST_CASE_P(
-    DISABLED_FileDisplay,
+    MAYBE_FileDisplay,
     FileManagerBrowserTest,
     ::testing::Values(TestParameter(NOT_IN_GUEST_MODE, "fileDisplayDownloads"),
                       TestParameter(IN_GUEST_MODE, "fileDisplayDownloads"),
@@ -154,7 +154,7 @@ WRAPPED_INSTANTIATE_TEST_CASE_P(
 #define MAYBE_KeyboardOperations KeyboardOperations
 #endif
 WRAPPED_INSTANTIATE_TEST_CASE_P(
-    DISABLED_KeyboardOperations,
+    MAYBE_KeyboardOperations,
     FileManagerBrowserTest,
     ::testing::Values(
         TestParameter(IN_GUEST_MODE, "keyboardDeleteDownloads"),
@@ -273,7 +273,7 @@ WRAPPED_INSTANTIATE_TEST_CASE_P(
 #define MAYBE_Transfer Transfer
 #endif
 WRAPPED_INSTANTIATE_TEST_CASE_P(
-    DISABLED_Transfer,
+    MAYBE_Transfer,
     FileManagerBrowserTest,
     ::testing::Values(
         TestParameter(NOT_IN_GUEST_MODE, "transferFromDriveToDownloads"),
@@ -389,7 +389,7 @@ WRAPPED_INSTANTIATE_TEST_CASE_P(
 #define MAYBE_GenericTask GenericTask
 #endif
 WRAPPED_INSTANTIATE_TEST_CASE_P(
-    DISABLED_GenericTask,
+    MAYBE_GenericTask,
     FileManagerBrowserTest,
     ::testing::Values(
         TestParameter(NOT_IN_GUEST_MODE, "genericTaskIsNotExecuted"),
@@ -561,7 +561,7 @@ WRAPPED_INSTANTIATE_TEST_CASE_P(
 #define MAYBE_GearMenu GearMenu
 #endif
 WRAPPED_INSTANTIATE_TEST_CASE_P(
-    DISABLED_GearMenu,
+    MAYBE_GearMenu,
     FileManagerBrowserTest,
     ::testing::Values(
         TestParameter(NOT_IN_GUEST_MODE, "showHiddenFilesOnDownloads"),
