d62f96f360a5d6ff77261c14647b4840fac6152f
dfalcantara <dfalcantara@chromium.org>
2017-04-18 12:37:14 -0700

    🔍 Add basic search widget test
    * Adds a basic test that confirms that what is displayed by the
      widget accurately reflects SharedPreference state.
    
    * Adds a pair of tests that confirm that clicking on the widget
      either starts a text or voice query.
    
    * Adds a Delegate to the SearchWidgetProvider to control what it
      interacts with and how.
    
    * Fixes the IntentFilters for the SearchWidgetProvider.  Not sure
      they're actually necessary, given that we directly send the
      Intent to the class (it was working while this was wrong).
    
    BUG=708844
    
    Review-Url: https://codereview.chromium.org/2819083002
    Cr-Commit-Position: refs/heads/master@{#465328}

--- chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchActivity.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.IntentHandler;
 import org.chromium.chrome.browser.WebContentsFactory;
@@ -37,6 +38,8 @@ import org.chromium.ui.base.ActivityWindowAndroid;
 public class SearchActivity extends AsyncInitializationActivity
         implements SnackbarManageable, SearchActivityLocationBarLayout.Delegate,
                    View.OnLayoutChangeListener {
+    /** Setting this field causes the Activity to finish itself immediately for tests. */
+    private static boolean sIsDisabledForTest;
 
     /** Main content view. */
     private ViewGroup mContentView;
@@ -53,6 +56,12 @@ public class SearchActivity extends AsyncInitializationActivity
     private SnackbarManager mSnackbarManager;
     private SearchBoxDataProvider mSearchBoxDataProvider;
     private Tab mTab;
+
+    @Override
+    protected boolean isStartedUpCorrectly(Intent intent) {
+        if (sIsDisabledForTest) return false;
+        return true;
+    }
 
     @Override
     public void backKeyPressed() {
@@ -219,5 +228,11 @@ public class SearchActivity extends AsyncInitializationActivity
     private void cancelSearch() {
         finish();
         overridePendingTransition(0, R.anim.activity_close_exit);
+    }
+
+    /** See {@link #sIsDisabledForTest}. */
+    @VisibleForTesting
+    static void disableForTests() {
+        sIsDisabledForTest = true;
     }
 }
