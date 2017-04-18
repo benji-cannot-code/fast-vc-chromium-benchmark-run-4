1d705bf30ec16bbd2ecf84684d2b951121d74a97
megjablon <megjablon@chromium.org>
2017-04-18 11:52:57 -0700

    Record UMA when Data Reduction stats are reset via the settings menu
    BUG=702799
    
    Review-Url: https://codereview.chromium.org/2821593002
    Cr-Commit-Position: refs/heads/master@{#465306}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/datareduction/DataReductionStatsPreference.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/datareduction/DataReductionStatsPreference.java
@@ -182,6 +182,8 @@ public class DataReductionStatsPreference extends Preference {
                     updateReductionStatistics();
                     setDetailText();
                     notifyChanged();
+                    DataReductionProxyUma.dataReductionProxyUIAction(
+                            DataReductionProxyUma.ACTION_STATS_RESET);
                 }
             });
         }
