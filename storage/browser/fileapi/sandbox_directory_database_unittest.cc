d874c937e7981c6d51847a10ca19745bd3604051
pwnall <pwnall@chromium.org>
2017-04-18 20:51:19 -0700

    Move two browser-side File API unittests next to the files they cover.
    BUG=653751
    
    Review-Url: https://codereview.chromium.org/2820113005
    Cr-Commit-Position: refs/heads/master@{#465486}

--- storage/browser/fileapi/sandbox_directory_database_unittest.cc
+++ storage/browser/fileapi/sandbox_directory_database_unittest.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
-#include "content/browser/fileapi/sandbox_database_test_helper.h"
+#include "storage/browser/test/sandbox_database_test_helper.h"
 #include "storage/common/fileapi/file_system_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/leveldatabase/src/include/leveldb/db.h"
