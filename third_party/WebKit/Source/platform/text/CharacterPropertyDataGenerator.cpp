0be1f0a2af5c7c73e6f2a8a6921cb9ea0fa548f8
phajdan.jr <phajdan.jr@chromium.org>
2017-04-19 03:08:10 -0700

    Fix build with system ICU
    This is needed after last big rename in blink.
    
    Also see https://groups.google.com/a/chromium.org/d/msg/blink-dev/Z0qkWrO-af8/Z_-ng4zvBAAJ for some context/followup.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2821433002
    Cr-Commit-Position: refs/heads/master@{#465542}

--- third_party/WebKit/Source/platform/text/CharacterPropertyDataGenerator.cpp
+++ third_party/WebKit/Source/platform/text/CharacterPropertyDataGenerator.cpp
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 #if defined(USING_SYSTEM_ICU)
-static void generate(FILE*) {}
+static void Generate(FILE*) {}
 #else
 
 const UChar32 kMaxCodepoint = 0x10FFFF;
