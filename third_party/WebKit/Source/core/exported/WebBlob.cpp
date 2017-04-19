23a5168a4b7ac6306a4404cc0eca709ed8dd071a
slangley <slangley@chromium.org>
2017-04-19 05:16:51 -0700

    Move WebBlob.cpp from web/ -> core/exported/.
    This cl moves WebBlob.cpp from web/ into core/exported/. As WebBlob.cpp has no
    dependencies in web/ and is expose via public/web/ it makes a good choice to be
    the first class moved into core/exported, so we can ensure that all of the link
    dependencies are correctly setup to build the entire chrome binary.
    
    BUG=712963
    
    Review-Url: https://codereview.chromium.org/2831503002
    Cr-Commit-Position: refs/heads/master@{#465560}

--- third_party/WebKit/Source/core/exported/WebBlob.cpp
+++ third_party/WebKit/Source/core/exported/WebBlob.cpp
@@ -31,12 +31,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "public/web/WebBlob.h"
 
+#include <memory>
+
 #include "bindings/core/v8/V8Binding.h"
 #include "bindings/core/v8/V8Blob.h"
 #include "core/fileapi/Blob.h"
 #include "platform/FileMetadata.h"
 #include "platform/blob/BlobData.h"
-#include <memory>
 
 namespace blink {
 
