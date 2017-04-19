4ba86ffefa1eba166ccd744083770fa49f80d3c4
chengx <chengx@chromium.org>
2017-04-19 12:05:01 -0700

    Various logical fixes for jumplist
    This CL consists of the following three changes:
    
    1) Move DeleteDirectoryContentAndLogResults() so that it runs only if
       jumplist_updater.BeginUpdate() succeeds, otherwise it's pointless.
    
    2) Return early if any AddShellLink call fails. This is suspected to
       cause long execution time for jumplist_updater.
    
    3) Decrease kFileDeleteLimit from 60 to 30. A user without jumplist
       issues should have at most 10 icon files in JumpListIcons folder as
       in crrev.com/2816113002. So changing kFileDeleteLimit to 30 won't
       affect "healthy" users. It'll give a better experience for users
       who have corrupted jumplist folders as the disk IO per jumplist
       update becomes less.
    
    BUG=40407, 179576
    
    Review-Url: https://codereview.chromium.org/2824103003
    Cr-Commit-Position: refs/heads/master@{#465689}

--- chrome/browser/win/jumplist_file_util.h
+++ chrome/browser/win/jumplist_file_util.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 
 // Maximum number of icon files allowed to delete per jumplist update.
-const int kFileDeleteLimit = 60;
+const int kFileDeleteLimit = 30;
 
 // Folder delete status enumeration, used in Delete* methods below.
 // This is used for UMA. Do not delete entries, and keep in sync with
