3198304ba419fe0704b2c73b68a24fd35dd0cdf9
emaxx <emaxx@chromium.org>
2017-04-19 07:37:30 -0700

    Fix wrong inclusion of .cc file in extensions/
    BUG=713138
    
    Review-Url: https://codereview.chromium.org/2823413003
    Cr-Commit-Position: refs/heads/master@{#465600}

--- chrome/browser/extensions/extension_crash_recovery_browsertest.cc
+++ chrome/browser/extensions/extension_crash_recovery_browsertest.cc
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/process_map.h"
 #include "extensions/browser/test_extension_registry_observer.h"
 #include "extensions/common/constants.h"
-#include "extensions/test/background_page_watcher.cc"
+#include "extensions/test/background_page_watcher.h"
 #include "ui/message_center/message_center.h"
 #include "ui/message_center/notification_list.h"
 
