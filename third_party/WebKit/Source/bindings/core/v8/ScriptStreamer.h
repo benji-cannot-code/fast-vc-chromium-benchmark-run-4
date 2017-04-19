a7f47882c6d6253a926e034db28392aef31123c7
hiroshige <hiroshige@chromium.org>
2017-04-18 23:43:19 -0700

    Remove unused methods from ScriptStreamer
    Preparation for https://codereview.chromium.org/2830533002.
    
    BUG=694702
    
    Review-Url: https://codereview.chromium.org/2829453003
    Cr-Commit-Position: refs/heads/master@{#465512}

--- third_party/WebKit/Source/bindings/core/v8/ScriptStreamer.h
+++ third_party/WebKit/Source/bindings/core/v8/ScriptStreamer.h
@@ -73,10 +73,6 @@ class CORE_EXPORT ScriptStreamer final
   void SuppressStreaming();
   bool StreamingSuppressed() const { return streaming_suppressed_; }
 
-  v8::ScriptCompiler::CompileOptions GetCompileOptions() const {
-    return compile_options_;
-  }
-
   // Called by PendingScript when data arrives from the network.
   void NotifyAppendData(ScriptResource*);
   void NotifyFinished(Resource*);
@@ -84,10 +80,6 @@ class CORE_EXPORT ScriptStreamer final
   // Called by ScriptStreamingTask when it has streamed all data to V8 and V8
   // has processed it.
   void StreamingCompleteOnBackgroundThread();
-
-  v8::ScriptCompiler::StreamedSource::Encoding GetEncoding() const {
-    return encoding_;
-  }
 
   const String& ScriptURLString() const { return script_url_string_; }
   unsigned long ScriptResourceIdentifier() const {
@@ -97,8 +89,6 @@ class CORE_EXPORT ScriptStreamer final
   static void SetSmallScriptThresholdForTesting(size_t threshold) {
     small_script_threshold_ = threshold;
   }
-
-  static size_t SmallScriptThreshold() { return small_script_threshold_; }
 
  private:
   // Scripts whose first data chunk is smaller than this constant won't be
