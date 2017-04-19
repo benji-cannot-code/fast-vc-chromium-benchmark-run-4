8c0d98b1111edfa17039c4727f7d7c77ed9d1b21
pkl <pkl@chromium.org>
2017-04-19 06:02:37 -0700

    List Password Manager extensions in a static data structure
    Collected the necessary information for supported Password Manager
    Extensions in a static data structure.
    
    BUG=712194
    TEST=test with 1Password, Lastpass, and Dashlane extensions
    
    Review-Url: https://codereview.chromium.org/2823883002
    Cr-Commit-Position: refs/heads/master@{#465567}

--- ios/chrome/browser/ui/activity_services/activity_service_controller_unittest.mm
+++ ios/chrome/browser/ui/activity_services/activity_service_controller_unittest.mm
@@ -188,7 +188,7 @@ class ActivityServiceControllerTest : public PlatformTest {
     [activityController setShareToDelegateForTesting:(id)shareToDelegateMock];
 
     // Sets up the returned item from a Password Management App Extension.
-    NSString* activityType = activity_services::kAppExtensionLastPass;
+    NSString* activityType = @"com.lastpass.ilastpass.LastPassExt";
     ShareTo::ShareResult result = ShareTo::ShareResult::SHARE_SUCCESS;
     BOOL resetUI =
         [activityController processItemsReturnedFromActivity:activityType
@@ -275,7 +275,7 @@ TEST_F(ActivityServiceControllerTest, ActivityItemsForDataWithPasswordAppEx) {
   // Gets the list of NSExtensionItem objects returned by the array of
   // id<UIActivityItemSource> objects returned by -activityItemsForData:.
   NSArray* extensionItems = FindItemsForActivityType(
-      items, activity_services::kAppExtensionOnePassword);
+      items, @"com.agilebits.onepassword-ios.extension");
   ASSERT_EQ(1U, [extensionItems count]);
   NSExtensionItem* item = extensionItems[0];
   EXPECT_EQ(1U, item.attachments.count);
@@ -314,7 +314,7 @@ TEST_F(ActivityServiceControllerTest, ActivityItemsForDataWithPasswordAppEx) {
       [OCMockObject niceMockForClass:[UIActivityViewController class]];
   NSString* title = [actionSource
       activityViewController:mockActivityViewController
-      subjectForActivityType:activity_services::kAppExtensionOnePassword];
+      subjectForActivityType:@"com.agilebits.onepassword-ios.extension"];
   EXPECT_NSEQ(@"kung fu fighting", title);
 }
 
