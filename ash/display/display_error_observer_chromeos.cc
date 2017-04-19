fb1c4270dd9dc7ff4595ff3a345c2935b7afb354
afakhry <afakhry@chromium.org>
2017-04-19 15:46:50 -0700

    Remove unused virtual display code
    The virtual display code was added 2 years ago for a feature that
    was never complete. Also if this code gets used, it could lead to
    crashes.
    
    BUG=705754,425060
    
    Review-Url: https://codereview.chromium.org/2820323004
    Cr-Commit-Position: refs/heads/master@{#465793}

--- ash/display/display_error_observer_chromeos.cc
+++ ash/display/display_error_observer_chromeos.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/devicetype_utils.h"
 #include "base/strings/string_number_conversions.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/display/types/display_snapshot.h"
 
 namespace ash {
 
--- ui/display/manager/chromeos/display_util.cc
+++ ui/display/manager/chromeos/display_util.cc
@@ -58,7 +58,6 @@ int GetDisplayPower(const std::vector<DisplaySnapshot*>& displays,
   for (size_t i = 0; i < displays.size(); ++i) {
     bool internal = displays[i]->type() == DISPLAY_CONNECTION_TYPE_INTERNAL;
     bool on =
-        displays[i]->type() == DISPLAY_CONNECTION_TYPE_VIRTUAL ||
         state == chromeos::DISPLAY_POWER_ALL_ON ||
         (state == chromeos::DISPLAY_POWER_INTERNAL_OFF_EXTERNAL_ON &&
          !internal) ||
@@ -72,8 +71,7 @@ int GetDisplayPower(const std::vector<DisplaySnapshot*>& displays,
 }
 
 bool IsPhysicalDisplayType(DisplayConnectionType type) {
-  return !(type &
-           (DISPLAY_CONNECTION_TYPE_NETWORK | DISPLAY_CONNECTION_TYPE_VIRTUAL));
+  return !(type & DISPLAY_CONNECTION_TYPE_NETWORK);
 }
 
 }  // namespace display
