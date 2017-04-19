1466cfaf5972a9b6675bb2933cc3f396be763b7a
jdufault <jdufault@chromium.org>
2017-04-19 11:57:18 -0700

    cros Revert DCHECKs added in crbug.com/699798.
    BUG=699798
    
    Review-Url: https://codereview.chromium.org/2820203002
    Cr-Commit-Position: refs/heads/master@{#465684}

--- chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
@@ -1035,8 +1035,6 @@ void SigninScreenHandler::OnUserRemoved(const AccountId& account_id,
 
 void SigninScreenHandler::OnUserImageChanged(const user_manager::User& user) {
   if (page_is_ready()) {
-    // TODO(jdufault): Remove DCHECK after resolving crbug.com/699798
-    DCHECK(is_js_initialized_and_ready());
     CallJSOrDefer("login.AccountPickerScreen.updateUserImage",
                   user.GetAccountId());
   }
@@ -1309,8 +1307,6 @@ void SigninScreenHandler::HandleToggleKioskAutolaunchScreen() {
 
 void SigninScreenHandler::LoadUsers(const base::ListValue& users_list,
                                     bool showGuest) {
-  // TODO(jdufault): Remove DCHECK after resolving crbug.com/699798
-  DCHECK(is_js_initialized_and_ready());
   CallJSOrDefer("login.AccountPickerScreen.loadUsers", users_list,
                 delegate_->IsShowGuest());
 }
