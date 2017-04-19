26aa6ebe668e08c4898338a4e5167c194674d35a
japhet <japhet@chromium.org>
2017-04-19 16:44:00 -0700

    Implement <script nomodule>
    BUG=681050
    TEST=external/wpt/html/semantics/scripting-1/the-script-element/nomodule-*
    
    Review-Url: https://codereview.chromium.org/2824583002
    Cr-Commit-Position: refs/heads/master@{#465811}

--- third_party/WebKit/Source/core/html/HTMLScriptElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLScriptElement.cpp
@@ -153,6 +153,10 @@ String HTMLScriptElement::LanguageAttributeValue() const {
   return getAttribute(languageAttr).GetString();
 }
 
+bool HTMLScriptElement::NomoduleAttributeValue() const {
+  return FastHasAttribute(nomoduleAttr);
+}
+
 String HTMLScriptElement::ForAttributeValue() const {
   return getAttribute(forAttr).GetString();
 }
