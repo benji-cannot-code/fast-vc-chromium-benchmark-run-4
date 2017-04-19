48867eb7ba4e2f9e908d42b929aeb8c7dcf8337f
rdevlin.cronin <rdevlin.cronin@chromium.org>
2017-04-19 08:31:26 -0700

    [Extensions] Remove unnecessary check in app_custom_bindings.js
    app_custom_bindings.js checks for if 'app' is available, but doesn't
    need to - if it's unavailable, the getter will never be added to the
    chrome object, and the custom bindings will never be called.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2829453002
    Cr-Commit-Position: refs/heads/master@{#465615}

--- chrome/renderer/resources/extensions/app_custom_bindings.js
+++ chrome/renderer/resources/extensions/app_custom_bindings.js
@@ -5,13 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Custom binding for the app API.
 
-var GetAvailability = requireNative('v8_context').GetAvailability;
-if (!GetAvailability('app').is_available) {
-  exports.$set('binding', {});
-  exports.$set('onInstallStateResponse', function(){});
-  return;
-}
-
 var appNatives = requireNative('app');
 var process = requireNative('process');
 var extensionId = process.GetExtensionId();
