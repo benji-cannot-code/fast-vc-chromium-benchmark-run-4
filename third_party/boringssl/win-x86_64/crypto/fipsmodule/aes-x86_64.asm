133e65d7c6329f3a8a1b7359df806b9a74e11b25
davidben <davidben@chromium.org>
2017-04-19 14:04:04 -0700

    Roll src/third_party/boringssl/src bc6a76b0e..777fdd644
    https://boringssl.googlesource.com/boringssl/+log/bc6a76b0e0ed252af1688423584f7cd9161f6dee..777fdd6443d5f01420b67137118febdf56a1c8e4
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2829743002
    Cr-Commit-Position: refs/heads/master@{#465744}

--- third_party/boringssl/win-x86_64/crypto/fipsmodule/aes-x86_64.asm
+++ third_party/boringssl/win-x86_64/crypto/fipsmodule/aes-x86_64.asm
@@ -1338,7 +1338,7 @@ $L$SEH_end_asm_AES_set_decrypt_key:
 ALIGN	16
 global	asm_AES_cbc_encrypt
 
-EXTERN	OPENSSL_ia32cap_P
+EXTERN	OPENSSL_ia32cap_addr
 
 asm_AES_cbc_encrypt:
 	mov	QWORD[8+rsp],rdi	;WIN64 prologue
@@ -1372,7 +1372,8 @@ $L$cbc_prologue:
 	cmp	r9,0
 	cmove	r14,r10
 
-	mov	r10d,DWORD[OPENSSL_ia32cap_P]
+	mov	r10,QWORD[OPENSSL_ia32cap_addr]
+	mov	r10d,DWORD[r10]
 	cmp	rdx,512
 	jb	NEAR $L$cbc_slow_prologue
 	test	rdx,15
