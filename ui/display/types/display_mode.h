edeb867a7b2aa55ed0a8cbfac37fda8a4f768868
rjkroege <rjkroege@chromium.org>
2017-04-19 16:05:37 -0700

    Re-order fields for better alignment
    Trivial change to possibly reduce DisplayMode size.
    
    BUG=620927
    
    Review-Url: https://codereview.chromium.org/2809913003
    Cr-Commit-Position: refs/heads/master@{#465802}

--- ui/display/types/display_mode.h
+++ ui/display/types/display_mode.h
@@ -32,8 +32,8 @@ class DISPLAY_TYPES_EXPORT DisplayMode {
 
  private:
   gfx::Size size_;
-  bool is_interlaced_;
   float refresh_rate_;
+  bool is_interlaced_;
 
   DISALLOW_COPY_AND_ASSIGN(DisplayMode);
 };
