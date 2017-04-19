7279653d14f5481f683bf8f35ae8e6b7a725e4ed
mahmadi <mahmadi@chromium.org>
2017-04-19 12:27:14 -0700

    [Payment Request] Sever card summary section in the credit card edit form
    Displays a PaymentMethodItem in the credit card form in order to identify
    the server card being edited.
    
    http://imgur.com/a/CM38f
    
    BUG=602666
    
    Review-Url: https://codereview.chromium.org/2826593005
    Cr-Commit-Position: refs/heads/master@{#465706}

--- ios/chrome/browser/payments/credit_card_edit_view_controller.mm
+++ ios/chrome/browser/payments/credit_card_edit_view_controller.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/autofill/cells/autofill_edit_item.h"
 #import "ios/chrome/browser/ui/collection_view/cells/MDCCollectionViewCell+Chrome.h"
 #import "ios/chrome/browser/ui/collection_view/cells/collection_view_detail_item.h"
+#import "ios/chrome/browser/ui/collection_view/cells/collection_view_item+collection_view_controller.h"
 #import "ios/chrome/browser/ui/collection_view/cells/collection_view_switch_item.h"
 #import "ios/chrome/browser/ui/collection_view/collection_view_model.h"
 #import "ios/chrome/browser/ui/colors/MDCPalette+CrAdditions.h"
@@ -32,12 +33,14 @@ NSString* const kCreditCardEditCollectionViewId =
     @"kCreditCardEditCollectionViewId";
 
 typedef NS_ENUM(NSInteger, SectionIdentifier) {
-  SectionIdentifierBillingAddress = kSectionIdentifierEnumStart,
+  SectionIdentifierCardSummary = kSectionIdentifierEnumStart,
+  SectionIdentifierBillingAddress,
   SectionIdentifierSaveCard,
 };
 
 typedef NS_ENUM(NSInteger, ItemType) {
-  ItemTypeBillingAddress = kItemTypeEnumStart,
+  ItemTypeCardSummary = kItemTypeEnumStart,
+  ItemTypeBillingAddress,
   ItemTypeSaveCard,
 };
 
@@ -156,6 +159,21 @@ typedef NS_ENUM(NSInteger, ItemType) {
   }
 }
 
+- (void)loadHeaderItems {
+  [super loadHeaderItems];
+  CollectionViewModel* model = self.collectionViewModel;
+
+  // Server card summary section.
+  CollectionViewItem* serverCardSummaryItem =
+      [_dataSource serverCardSummaryItem];
+  if (serverCardSummaryItem) {
+    [model addSectionWithIdentifier:SectionIdentifierCardSummary];
+    serverCardSummaryItem.type = ItemTypeCardSummary;
+    [model addItem:serverCardSummaryItem
+        toSectionWithIdentifier:SectionIdentifierCardSummary];
+  }
+}
+
 - (void)loadFooterItems {
   CollectionViewModel* model = self.collectionViewModel;
 
@@ -268,6 +286,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
   CollectionViewItem* item =
       [self.collectionViewModel itemAtIndexPath:indexPath];
   switch (item.type) {
+    case ItemTypeCardSummary:
     case ItemTypeSaveCard:
       break;
     case ItemTypeBillingAddress: {
@@ -288,6 +307,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
   switch (item.type) {
     case ItemTypeBillingAddress:
       return MDCCellDefaultOneLineHeight;
+    case ItemTypeCardSummary:
     case ItemTypeSaveCard:
       return [MDCCollectionViewCell
           cr_preferredHeightForWidth:CGRectGetWidth(collectionView.bounds)
@@ -302,6 +322,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
     hidesInkViewAtIndexPath:(NSIndexPath*)indexPath {
   NSInteger type = [self.collectionViewModel itemTypeForIndexPath:indexPath];
   switch (type) {
+    case ItemTypeCardSummary:
     case ItemTypeSaveCard:
       return YES;
     default:
