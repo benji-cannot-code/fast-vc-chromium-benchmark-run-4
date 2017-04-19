d16e6b2efb344310ef1d75e48f55907d70cc6005
rhalavati <rhalavati@chromium.org>
2017-04-18 23:23:35 -0700

    Network traffic annotation added to variations_service.
    Network traffic annotation is added to network request of
    components/variations/service/variations_service.cc
    
    BUG=656607
    
    Review-Url: https://codereview.chromium.org/2803633002
    Cr-Commit-Position: refs/heads/master@{#465510}

--- components/variations/service/variations_service.cc
+++ components/variations/service/variations_service.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_response_headers.h"
 #include "net/http/http_status_code.h"
 #include "net/http/http_util.h"
+#include "net/traffic_annotation/network_traffic_annotation.h"
 #include "net/url_request/url_fetcher.h"
 #include "net/url_request/url_request_status.h"
 #include "ui/base/device_form_factor.h"
@@ -532,8 +533,27 @@ void VariationsService::DoActualFetch() {
   if (pending_seed_request_)
     return;
 
-  pending_seed_request_ = net::URLFetcher::Create(0, variations_server_url_,
-                                                  net::URLFetcher::GET, this);
+  net::NetworkTrafficAnnotationTag traffic_annotation =
+      net::DefineNetworkTrafficAnnotation("chrome_variations_service", R"(
+        semantics {
+          sender: "Chrome Variations Service"
+          description:
+            "Retrieves the list of Google Chrome's Variations from the server, "
+            "which will apply to the next Chrome session upon a restart."
+          trigger:
+            "Requests are made periodically while Google Chrome is running."
+          data: "The operating system name."
+          destination: GOOGLE_OWNED_SERVICE
+        }
+        policy {
+          cookies_allowed: false
+          setting: "This feature cannot be disabled by settings."
+          policy_exception_justification:
+            "Not implemented, considered not required."
+        })");
+  pending_seed_request_ =
+      net::URLFetcher::Create(0, variations_server_url_, net::URLFetcher::GET,
+                              this, traffic_annotation);
   data_use_measurement::DataUseUserData::AttachToFetcher(
       pending_seed_request_.get(),
       data_use_measurement::DataUseUserData::VARIATIONS);
