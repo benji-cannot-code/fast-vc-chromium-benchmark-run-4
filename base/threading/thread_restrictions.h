a6b7462b3c168ee5bdceecbc79f0f16d034dd549
bcwhite <bcwhite@chromium.org>
2017-04-19 08:30:23 -0700

    Support parallel captures from the StackSamplingProfiler.
    Previously, only one sampling operation could be running and it was
    generally used to profile the startup of the browser.  To make it more
    useful, it can now run against any thread and multiple profilers can
    execute in parallel.
    
    Sampling will continue until the desired number of samples has been
    collected, it is manually stopped, or the controlling object gets
    destructed.
    
    The SamplingThread is a singleton base::Thread that is self-managing.
    - It is started (via GetOrCreateTaskRunnerForAdd) on the calling
      thread when work arrives.
    - It stops (via ShutdownTask) on its own thread when it has been
      idle for 1 minute.
    - DetachFromSequence is called after both of these to allow for
      accessing the API from different threads.
    - thread_execution_state_lock_ is held when doing Thread API calls to
      ensure that access is sequenced.
    
    The sampled thread is expected to live at least as long as the
    thread controlling the sampling.
    
    BUG=671716
    
    Review-Url: https://codereview.chromium.org/2554123002
    Cr-Commit-Position: refs/heads/master@{#465614}

--- base/threading/thread_restrictions.h
+++ base/threading/thread_restrictions.h
@@ -100,6 +100,7 @@ class TaskTracker;
 
 class SequencedWorkerPool;
 class SimpleThread;
+class StackSamplingProfiler;
 class Thread;
 class ThreadTestHelper;
 
@@ -184,6 +185,7 @@ class BASE_EXPORT ThreadRestrictions {
   // BEGIN ALLOWED USAGE.
   friend class android_webview::AwFormDatabaseService;
   friend class android_webview::CookieManager;
+  friend class base::StackSamplingProfiler;
   friend class content::BrowserShutdownProfileDumper;
   friend class content::BrowserSurfaceViewManager;
   friend class content::BrowserTestBase;
