4a4aee4843e5d44b41ec8259a132bfc38d0aa0fc
ananta <ananta@chromium.org>
2017-04-19 13:00:40 -0700

    Ensure that the focus ring in the bookmarks bar does not paint outside the parent view.
    The proposed fix is to notify the parent views when a child enables layering. The parent in
    this case the ScrollView overrides the newly added notification OnChildLayerChanged() and enables
    viewport layering. This ensures that the ring gets clipped.
    
    Longer term it seems like the focus ring should really be a property of the view and should not
    be instantiated by different controls all over the place.
    
    That for a later patchset.
    
    BUG=665412, 656198
    TEST=Covered by test ViewObserverTest.ScrollViewChildAddLayerTest and ViewObserverTest.ChildViewLayerNotificationTest
    
    Review-Url: https://codereview.chromium.org/2813353002
    Cr-Commit-Position: refs/heads/master@{#465720}

--- ui/views/controls/scroll_view.h
+++ ui/views/controls/scroll_view.h
@@ -18,6 +18,8 @@ class ScrollOffset;
 }
 
 namespace views {
+class ViewObserverTest;
+
 namespace test {
 class ScrollViewTestApi;
 }
@@ -106,6 +108,9 @@ class VIEWS_EXPORT ScrollView : public View, public ScrollBarController {
   void OnGestureEvent(ui::GestureEvent* event) override;
   const char* GetClassName() const override;
   void OnNativeThemeChanged(const ui::NativeTheme* theme) override;
+  void ViewHierarchyChanged(
+      const ViewHierarchyChangedDetails& details) override;
+  void OnChildLayerChanged(View* child) override;
 
   // ScrollBarController overrides:
   void ScrollToPosition(ScrollBar* source, int position) override;
@@ -119,6 +124,7 @@ class VIEWS_EXPORT ScrollView : public View, public ScrollBarController {
 
  private:
   friend class test::ScrollViewTestApi;
+  FRIEND_TEST_ALL_PREFIXES(ViewObserverTest, ScrollViewChildAddLayerTest);
 
   class Viewport;
 
@@ -163,6 +169,11 @@ class VIEWS_EXPORT ScrollView : public View, public ScrollBarController {
   void AddBorder();
   void UpdateBorder();
 
+  // Enables view port layering if |child| or any of its descendants has a
+  // layer. Returns true if yes. We short circuit the recursion if we enabled
+  // layering.
+  bool EnableLayeringRecursivelyForChild(View* child);
+
   // The current contents and its viewport. |contents_| is contained in
   // |contents_viewport_|.
   View* contents_;
@@ -204,6 +215,12 @@ class VIEWS_EXPORT ScrollView : public View, public ScrollBarController {
 
   // Focus ring, if one is installed.
   View* focus_ring_ = nullptr;
+
+  // Set to true if we enabled layering for the viewport.
+  bool viewport_layer_enabled_ = false;
+
+  // Set to true if the scroll with layers feature is enabled.
+  const bool scroll_with_layers_enabled_;
 
   DISALLOW_COPY_AND_ASSIGN(ScrollView);
 };
