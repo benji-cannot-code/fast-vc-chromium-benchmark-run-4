c74f800cd263e3bad7c7ea44f621678f350909d0
mmenke <mmenke@chromium.org>
2017-04-19 07:13:59 -0700

    Remove URLRequestJob::GetResponseCode implementations from streams and fileapi code.
    URLRequestJob itself now has a default implementation, which gets
    the response code from the headers returned by GetResponseInfo.
    We eventually want to remove this method, since it's redundant,
    and has weird behavior for just when it is and is not safe to
    call.
    
    BUG=688481
    
    Review-Url: https://codereview.chromium.org/2822593003
    Cr-Commit-Position: refs/heads/master@{#465588}

--- storage/browser/fileapi/file_writer_delegate_unittest.cc
+++ storage/browser/fileapi/file_writer_delegate_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "net/base/io_buffer.h"
 #include "net/base/request_priority.h"
+#include "net/http/http_response_headers.h"
 #include "net/url_request/url_request.h"
 #include "net/url_request/url_request_context.h"
 #include "net/url_request/url_request_job.h"
@@ -202,6 +203,14 @@ class FileWriterDelegateTestJob : public net::URLRequestJob {
   }
 
   int GetResponseCode() const override { return 200; }
+
+  void GetResponseInfo(net::HttpResponseInfo* info) override {
+    const char kStatus[] = "HTTP/1.1 200 OK\0";
+    const size_t kStatusLen = arraysize(kStatus);
+
+    info->headers =
+        new net::HttpResponseHeaders(std::string(kStatus, kStatusLen));
+  }
 
  protected:
   ~FileWriterDelegateTestJob() override {}
