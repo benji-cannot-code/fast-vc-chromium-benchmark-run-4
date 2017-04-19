706a64d702e5afcf3a37234952c77bdfabb394e6
warx <warx@chromium.org>
2017-04-19 15:11:22 -0700

    cros: close dialog when shill detects captive portal network switched to online
    Changes:
    (1) On crrev.com/2756643002, we stop the chrome portal detector attempts when chromebook is successfully behind the captive portal. Thus, the following passive attempts are only triggered by network state change or proxy change, like auth supplied. Currently, the portal dialog auto-close happens if chrome portal detection detects an online result. This used to be robust since we keep doing active chrome detection when we are behind the portal. But now if shill detects a online state, it triggers chrome portal detection but this detection times out (indicating that wifi is bad so that fetching gives no response). To make it robust, we can close the dialog when shill detects captive portal network switched to online state.
    
    (2) SetIgnoreNoNetworkForTesting() is not needed any more since in tests, SetConnected happens before dialog is shown. The dialog wont be auto-closed for the tests.
    
    BUG=705813
    TEST=test that portal dialog can be closed when wifi signal is not that good.
    
    Review-Url: https://codereview.chromium.org/2823323002
    Cr-Commit-Position: refs/heads/master@{#465773}

--- chrome/browser/chromeos/net/network_portal_detector_impl_browsertest.cc
+++ chrome/browser/chromeos/net/network_portal_detector_impl_browsertest.cc
@@ -154,11 +154,6 @@ class NetworkPortalDetectorImplBrowserTest
 
   MessageCenter* message_center() { return MessageCenter::Get(); }
 
-  void SetIgnoreNoNetworkForTesting() {
-    network_portal_detector_->notification_controller_
-        ->SetIgnoreNoNetworkForTesting();
-  }
-
   const NetworkPortalWebDialog* GetDialog() const {
     return network_portal_detector_->notification_controller_
         ->GetDialogForTesting();
@@ -253,8 +248,6 @@ void NetworkPortalDetectorImplBrowserTestIgnoreProxy::TestImpl(
 
   LoginUser(kTestUser);
   content::RunAllPendingInMessageLoop();
-
-  SetIgnoreNoNetworkForTesting();
 
   ProfileManager::GetActiveUserProfile()->GetPrefs()->SetBoolean(
       prefs::kCaptivePortalAuthenticationIgnoresProxy, preference_value);
