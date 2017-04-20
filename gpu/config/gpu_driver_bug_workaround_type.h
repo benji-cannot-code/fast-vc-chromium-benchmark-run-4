e3258eb58f464be9ae19bb21073eb7f183469ec3
qiankun.miao <qiankun.miao@intel.com>
2017-04-19 23:22:07 -0700

    Force cube map texture complete on Linux AMD
    CopyTex{Sub}Image2D reports INVALID_OPERATION error if the destination
    texture is an integer incomplete cube map texture on Linux AMD. Similarly,
    TexSubImage2D reports OUT_OF_MEMORY error if the destination texture is a
    srgb incomplete cube map texture on Linux AMD. This is a bug in AMD Linux
    driver. This CL workarounds this bug by forcing the texture to be cube map
    complete.
    
    BUG=710392, 712117
    TEST=webgl2 conformance test
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2821913002
    Cr-Commit-Position: refs/heads/master@{#465915}

--- gpu/config/gpu_driver_bug_workaround_type.h
+++ gpu/config/gpu_driver_bug_workaround_type.h
@@ -106,6 +106,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
          force_discrete_gpu)                                 \
   GPU_OP(FORCE_INTEGRATED_GPU,                               \
          force_integrated_gpu)                               \
+  GPU_OP(FORCE_INT_OR_SRGB_CUBE_TEXTURE_COMPLETE,            \
+         force_int_or_srgb_cube_texture_complete)            \
   GPU_OP(FORCE_UPDATE_SCISSOR_STATE_WHEN_BINDING_FBO0,       \
          force_update_scissor_state_when_binding_fbo0)       \
   GPU_OP(GET_FRAG_DATA_INFO_BUG,                             \
