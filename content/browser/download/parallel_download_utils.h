4e08e842144930c1c15c924dc28a8d07e613847e
xingliu <xingliu@chromium.org>
2017-04-20 16:14:33 -0700

    Parallel download browser test.
    This CL sets up a browser test fixture for parallel download.
    
    BUG=710576, 644352
    
    Review-Url: https://codereview.chromium.org/2819483002
    Cr-Commit-Position: refs/heads/master@{#466181}

--- content/browser/download/parallel_download_utils.h
+++ content/browser/download/parallel_download_utils.h
@@ -14,6 +14,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+// Finch parameter key value for minimum slice size in bytes to use parallel
+// download.
+constexpr char kMinSliceSizeFinchKey[] = "min_slice_size";
+
+// Finch parameter key value for number of parallel requests in a parallel
+// download, including the original request.
+constexpr char kParallelRequestCountFinchKey[] = "request_count";
+
+// Finch parameter key value for the delay time in milliseconds to send
+// parallel requests after response of the original request is handled.
+constexpr char kParallelRequestDelayFinchKey[] = "parallel_request_delay";
+
+// Finch parameter key value for the remaining time in seconds that is required
+// to send parallel requests.
+constexpr char kParallelRequestRemainingTimeFinchKey[] =
+    "parallel_request_remaining_time";
+
 // Chunks the content that starts from |current_offset|, into at most
 // std::max(|request_count|, 1) smaller slices.
 // Each slice contains at least |min_slice_size| bytes unless |total_length|
