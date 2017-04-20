21ed82e6e999e98a44b46944ac9269e20672794b
rtoy <rtoy@chromium.org>
2017-04-20 14:14:17 -0700

    Slightly reduce memory usage in OscillatorNode
    When an OscillatorNode is created, a PeriodicWave is created for a
    "sine" oscillator.  This is unavoidable when the factory method is
    used.  However, with the constructor, the type of oscillator can be
    specified and there is no need to create both a "sine" PeriodicWave
    and one for the specified type.
    
    Thus, for the constructor, only create the wave table for the
    specified type.
    
    BUG=700175
    TEST=not testable; manually verifed with prints/debugger that sine
    table is not created unless needed.
    
    Review-Url: https://codereview.chromium.org/2767623002
    Cr-Commit-Position: refs/heads/master@{#466134}

--- third_party/WebKit/Source/modules/webaudio/BaseAudioContext.cpp
+++ third_party/WebKit/Source/modules/webaudio/BaseAudioContext.cpp
@@ -520,7 +520,7 @@ OscillatorNode* BaseAudioContext::createOscillator(
     ExceptionState& exception_state) {
   DCHECK(IsMainThread());
 
-  return OscillatorNode::Create(*this, exception_state);
+  return OscillatorNode::Create(*this, "sine", nullptr, exception_state);
 }
 
 PeriodicWave* BaseAudioContext::createPeriodicWave(
