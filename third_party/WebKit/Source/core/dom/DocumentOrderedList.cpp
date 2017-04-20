e7ba60c4389816074650b2cb47c50590d5fa6258
keishi <keishi@chromium.org>
2017-04-20 03:53:18 -0700

    Verify that DocumentOrderedList is not modified during iteration
    Something seems to be going wrong with DocumentOrderedList in DocumentStyleSheetCollection::CollectStyleSheetsFromCandidates() causing a crash.
    
    BUG=699269
    
    Review-Url: https://codereview.chromium.org/2833703002
    Cr-Commit-Position: refs/heads/master@{#465968}

--- third_party/WebKit/Source/core/dom/DocumentOrderedList.cpp
+++ third_party/WebKit/Source/core/dom/DocumentOrderedList.cpp
@@ -35,6 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 void DocumentOrderedList::Add(Node* node) {
+  // TODO(keishi): CHECK() added for crbug.com/699269 diagnosis.
+  CHECK(!mutation_forbidden_);
   if (nodes_.IsEmpty()) {
     nodes_.insert(node);
     return;
@@ -61,6 +63,8 @@ void DocumentOrderedList::Add(Node* node) {
 }
 
 void DocumentOrderedList::Remove(const Node* node) {
+  // TODO(keishi): CHECK() added for crbug.com/699269 diagnosis.
+  CHECK(!mutation_forbidden_);
   nodes_.erase(const_cast<Node*>(node));
 }
 
--- third_party/WebKit/Source/core/dom/DocumentOrderedList.h
+++ third_party/WebKit/Source/core/dom/DocumentOrderedList.h
@@ -43,12 +43,32 @@ class DocumentOrderedList final {
   DISALLOW_NEW();
 
  public:
-  DocumentOrderedList() {}
+  DocumentOrderedList() : mutation_forbidden_(false) {}
+
+  // Forbids changes to DocumentOrderedList. Used to check if we are
+  // unintentionally changing DocumentOrderedList while iterating. Added for
+  // crbug.com/699269 diagnosis.
+  class MutationForbiddenScope final {
+    STACK_ALLOCATED();
+
+   public:
+    explicit MutationForbiddenScope(DocumentOrderedList* list) : list_(list) {
+      list_->mutation_forbidden_ = true;
+    }
+    ~MutationForbiddenScope() { list_->mutation_forbidden_ = false; }
+
+   private:
+    DocumentOrderedList* list_;
+  };
 
   void Add(Node*);
   void Remove(const Node*);
   bool IsEmpty() const { return nodes_.IsEmpty(); }
-  void Clear() { nodes_.clear(); }
+  void Clear() {
+    // TODO(keishi): CHECK() added for crbug.com/699269 diagnosis.
+    CHECK(!mutation_forbidden_);
+    nodes_.clear();
+  }
   size_t size() const { return nodes_.size(); }
 
   using iterator = HeapListHashSet<Member<Node>, 32>::iterator;
@@ -68,6 +88,7 @@ class DocumentOrderedList final {
 
  private:
   HeapListHashSet<Member<Node>, 32> nodes_;
+  bool mutation_forbidden_;
 };
 
 }  // namespace blink
