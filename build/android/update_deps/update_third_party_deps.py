b82bc768cd806d0174eb91a18449312fe5fd779f
zpeng <zpeng@chromium.org>
2017-04-20 07:17:22 -0700

    Reland "Update third_party/checkstyle to 7.6.1"
    Reverted in:
    https://codereview.chromium.org/2807753003/
    
    Reason for reland:
    1. No longer uses Android-related wrapper script for downloading
    checkstyle binary file.
    2. chromium_presubmit bot now supports Java 8.
    
    TBR=klobag@chromium.org
    BUG=703238
    
    Review-Url: https://codereview.chromium.org/2820583002
    Cr-Commit-Position: refs/heads/master@{#465999}

--- build/android/update_deps/update_third_party_deps.py
+++ build/android/update_deps/update_third_party_deps.py
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 """
 Uploads or downloads third party libraries to or from google cloud storage.
+
+This script will only work for Android checkouts.
 """
 
 import argparse
