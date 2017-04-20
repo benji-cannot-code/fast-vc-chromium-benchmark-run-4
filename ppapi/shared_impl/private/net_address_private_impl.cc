7d2f9995e5c73e4257fffb2c220f86d59a15335d
raphael.kubo.da.costa <raphael.kubo.da.costa@intel.com>
2017-04-20 00:39:01 -0700

    ppapi: Include <netinet/in.h> for sockaddr_in{6}.
    IWYU and stop pulling sockaddr_in/sockaddr_in6's definitions indirectly.
    
    R=bbudge@chromium.org,raymes@chromium.org
    
    Review-Url: https://codereview.chromium.org/2822233004
    Cr-Commit-Position: refs/heads/master@{#465937}

--- ppapi/shared_impl/private/net_address_private_impl.cc
+++ ppapi/shared_impl/private/net_address_private_impl.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <ws2tcpip.h>
 #elif defined(OS_POSIX) && !defined(OS_NACL)
 #include <arpa/inet.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #endif
