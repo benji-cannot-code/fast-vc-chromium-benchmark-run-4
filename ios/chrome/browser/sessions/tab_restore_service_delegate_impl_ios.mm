122ef45144d192783e5b6ac03a95694975068e09
sdefresne <sdefresne@chromium.org>
2017-04-20 05:13:13 -0700

    [ios] Convert //ios/chrome/browser/sessions to ARC.
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2812823003
    Cr-Commit-Position: refs/heads/master@{#465977}

--- ios/chrome/browser/sessions/tab_restore_service_delegate_impl_ios.mm
+++ ios/chrome/browser/sessions/tab_restore_service_delegate_impl_ios.mm
@@ -22,6 +22,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/web_state/web_state_impl.h"
 #import "net/base/mac/url_conversions.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 TabRestoreServiceDelegateImplIOS::TabRestoreServiceDelegateImplIOS(
     ios::ChromeBrowserState* browser_state)
     : browser_state_(browser_state) {}
--- ios/chrome/browser/sessions/tab_restore_service_delegate_impl_ios_factory.mm
+++ ios/chrome/browser/sessions/tab_restore_service_delegate_impl_ios_factory.mm
@@ -13,6 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/sessions/tab_restore_service_delegate_impl_ios.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 // static
 TabRestoreServiceDelegateImplIOS*
 TabRestoreServiceDelegateImplIOSFactory::GetForBrowserState(
