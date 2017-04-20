2de266acf861146f300b266982fc4244ae154aab
gambard <gambard@chromium.org>
2017-04-20 10:15:14 -0700

    [ObjC ARC] Converts ios/chrome/browser/ui/icons:icons to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2830033004
    Cr-Commit-Position: refs/heads/master@{#466037}

--- ios/chrome/browser/ui/icons/chrome_icon.mm
+++ ios/chrome/browser/ui/icons/chrome_icon.mm
@@ -8,9 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <CoreGraphics/CoreGraphics.h>
 
 #include "base/logging.h"
-#import "base/mac/scoped_nsobject.h"
 #include "ios/chrome/grit/ios_strings.h"
 #include "ui/base/l10n/l10n_util_mac.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
 
 namespace {
 
@@ -74,14 +77,14 @@ UIImage* ImageFlippedForRightToLeftLayoutDirection(UIImage* image) {
                                             action:(SEL)action {
   UIImage* templateImage =
       [image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
-  base::scoped_nsobject<UIBarButtonItem> barButtonItem([[UIBarButtonItem alloc]
-      initWithImage:templateImage
-              style:UIBarButtonItemStylePlain
-             target:target
-             action:action]);
+  UIBarButtonItem* barButtonItem =
+      [[UIBarButtonItem alloc] initWithImage:templateImage
+                                       style:UIBarButtonItemStylePlain
+                                      target:target
+                                      action:action];
   [barButtonItem setAccessibilityIdentifier:image.accessibilityIdentifier];
   [barButtonItem setAccessibilityLabel:image.accessibilityLabel];
-  return barButtonItem.autorelease();
+  return barButtonItem;
 }
 
 @end
