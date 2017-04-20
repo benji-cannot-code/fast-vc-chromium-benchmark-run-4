92d756e882b429c6e23c43019dd32188cda13e17
jzw <jzw@chromium.org>
2017-04-20 02:19:29 -0700

    Expose way to set google api key through CWVWebView class method.
    BUG=710370
    
    Review-Url: https://codereview.chromium.org/2786033002
    Cr-Commit-Position: refs/heads/master@{#465958}

--- ios/web_view/internal/cwv_web_view.mm
+++ ios/web_view/internal/cwv_web_view.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/ios/weak_nsobject.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/sys_string_conversions.h"
+#include "google_apis/google_api_keys.h"
 #import "ios/web/public/navigation_manager.h"
 #include "ios/web/public/referrer.h"
 #include "ios/web/public/reload_type.h"
@@ -92,6 +93,21 @@ static NSString* gUserAgentProduct = nil;
 
 + (void)setUserAgentProduct:(NSString*)product {
   gUserAgentProduct = [product copy];
+}
+
++ (void)setGoogleAPIKey:(NSString*)googleAPIKey
+               clientID:(NSString*)clientID
+           clientSecret:(NSString*)clientSecret {
+  google_apis::SetAPIKey(base::SysNSStringToUTF8(googleAPIKey));
+
+  std::string clientIDString = base::SysNSStringToUTF8(clientID);
+  std::string clientSecretString = base::SysNSStringToUTF8(clientSecret);
+  for (size_t i = 0; i < google_apis::CLIENT_NUM_ITEMS; ++i) {
+    google_apis::OAuth2Client client =
+        static_cast<google_apis::OAuth2Client>(i);
+    google_apis::SetOAuth2ClientID(client, clientIDString);
+    google_apis::SetOAuth2ClientSecret(client, clientSecretString);
+  }
 }
 
 - (instancetype)initWithFrame:(CGRect)frame
