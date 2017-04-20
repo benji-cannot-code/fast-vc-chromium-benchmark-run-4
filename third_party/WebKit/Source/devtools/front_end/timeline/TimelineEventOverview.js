f873c5d59428c46bc38387c19b9453028fe3c43e
alph <alph@chromium.org>
2017-04-19 18:27:36 -0700

    DevTools: Do not use popover for timeline filmstrip overview.
    Review-Url: https://codereview.chromium.org/2826873002
    Cr-Commit-Position: refs/heads/master@{#465847}

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineEventOverview.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineEventOverview.js
@@ -455,7 +455,7 @@ Timeline.TimelineFilmStripOverview = class extends Timeline.TimelineEventOvervie
    * @param {number} x
    * @return {!Promise<?Element>}
    */
-  popoverElementPromise(x) {
+  overviewInfoPromise(x) {
     if (!this._model || !this._model.filmStripModel().frames().length)
       return Promise.resolve(/** @type {?Element} */ (null));
 
@@ -474,7 +474,6 @@ Timeline.TimelineFilmStripOverview = class extends Timeline.TimelineEventOvervie
     function createFrameElement(image) {
       var element = createElementWithClass('div', 'frame');
       element.createChild('div', 'thumbnail').appendChild(image);
-      UI.appendStyle(element, 'timeline/timelinePanel.css');
       this._lastFrame = frame;
       this._lastElement = element;
       return element;
--- third_party/WebKit/Source/devtools/front_end/ui/Popover.js
+++ third_party/WebKit/Source/devtools/front_end/ui/Popover.js
@@ -53,12 +53,10 @@ UI.PopoverHelper = class {
   /**
    * @param {number} showTimeout
    * @param {number=} hideTimeout
-   * @param {boolean=} hideOnAnchorLeave
    */
-  setTimeout(showTimeout, hideTimeout, hideOnAnchorLeave) {
+  setTimeout(showTimeout, hideTimeout) {
     this._showTimeout = showTimeout;
     this._hideTimeout = typeof hideTimeout === 'number' ? hideTimeout : showTimeout / 2;
-    this._hideOnAnchorLeave = hideOnAnchorLeave;
   }
 
   /**
@@ -242,7 +240,7 @@ UI.PopoverHelper = class {
   }
 
   _stopHidePopoverTimer() {
-    if (!this._hidePopoverTimer || this._hideOnAnchorLeave)
+    if (!this._hidePopoverTimer)
       return;
     clearTimeout(this._hidePopoverTimer);
     delete this._hidePopoverTimer;
