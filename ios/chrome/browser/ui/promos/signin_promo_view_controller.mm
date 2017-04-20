6d66b6f2cda903cfdd68bbda4893d75aaa22eace
gambard <gambard@chromium.org>
2017-04-20 07:06:28 -0700

    [ObjC ARC] Converts ios/chrome/browser/ui/promos:promos to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2827263003
    Cr-Commit-Position: refs/heads/master@{#465995}

--- ios/chrome/browser/ui/promos/signin_promo_view_controller.mm
+++ ios/chrome/browser/ui/promos/signin_promo_view_controller.mm
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/promos/signin_promo_view_controller.h"
 
-#include "base/mac/scoped_nsobject.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/metrics/user_metrics.h"
 #include "base/strings/sys_string_conversions.h"
@@ -20,6 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
 #import "ios/public/provider/chrome/browser/signin/chrome_identity_service.h"
 #include "net/base/network_change_notifier.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
 
 // Key in the UserDefaults to record the version of the application when the
 // SSO Recall promo has been displayed.
@@ -174,9 +177,9 @@ enum PromoAction {
 
 + (UIViewController*)controllerToPresentForBrowserState:
     (ios::ChromeBrowserState*)browserState {
-  base::scoped_nsobject<UIViewController> controller(
-      [[SigninPromoViewController alloc] initWithBrowserState:browserState]);
-  return controller.autorelease();
+  UIViewController* controller =
+      [[SigninPromoViewController alloc] initWithBrowserState:browserState];
+  return controller;
 }
 
 #pragma mark - ChromeSigninViewControllerDelegate
