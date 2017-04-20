fc639c21b8ac607bffe3ed2e49618edf54e0692a
bugsnash <bugsnash@chromium.org>
2017-04-19 17:36:42 -0700

    Added CSSPropertyOffsetPathUtils which holds shared parsing logic.
    This patch
    - Added CSSPropertyOffsetPathUtils.
    - Moved ConsumeOffsetPath, ConsumePathOrNone, and ConsumePath methods
      from CSSPropertyParser to CSSPropertyOffsetPathUtils.
    - Made the ConsumePath method private to the CSSPropertyOffsetPathUtils
      file as it is not used elsewhere.
    
    This is pre work to move ParseSingleValue logic from CSSPropertyParser
    to the property API for CSSPropertyOffsetPath.
    
    BUG=668012
    
    Review-Url: https://codereview.chromium.org/2816903003
    Cr-Commit-Position: refs/heads/master@{#465826}

---
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyOffsetPathUtils.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CSSPropertyOffsetPathUtils_h
+#define CSSPropertyOffsetPathUtils_h
+
+#include "platform/wtf/Allocator.h"
+
+namespace blink {
+
+class CSSParserContext;
+class CSSParserTokenRange;
+class CSSValue;
+
+class CSSPropertyOffsetPathUtils {
+  STATIC_ONLY(CSSPropertyOffsetPathUtils);
+  static CSSValue* ConsumeOffsetPath(CSSParserTokenRange&,
+                                     const CSSParserContext*,
+                                     bool);
+  static CSSValue* ConsumePathOrNone(CSSParserTokenRange&);
+};
+
+}  // namespace blink
+
+#endif  // CSSPropertyOffsetPathUtils_h
