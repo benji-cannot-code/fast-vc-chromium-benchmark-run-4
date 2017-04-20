fd11d3b151175b6e99a06c2fa5a22764d93df211
fdoray <fdoray@chromium.org>
2017-04-20 06:13:11 -0700

    Separate the create and start phases in DelayedTaskManager.
    Tasks added to the DelayedTaskManager before Start() are sent to the
    service thread when Start() is called. Tasks added to the
    DelayedTaskManager after Start() are sent to the service thread as
    they are added. The service thread calls the post task callback of
    a task when it becomes ripe for execution.
    
    BUG=690706
    
    Review-Url: https://codereview.chromium.org/2810873008
    Cr-Commit-Position: refs/heads/master@{#465988}

--- base/task_scheduler/task_scheduler_impl.cc
+++ base/task_scheduler/task_scheduler_impl.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "base/memory/ptr_util.h"
 #include "base/task_scheduler/delayed_task_manager.h"
-#include "base/task_scheduler/scheduler_single_thread_task_runner_manager.h"
 #include "base/task_scheduler/scheduler_worker_pool_params.h"
 #include "base/task_scheduler/sequence_sort_key.h"
 #include "base/task_scheduler/task.h"
@@ -100,7 +99,7 @@ TaskSchedulerImpl::CreateSingleThreadTaskRunnerWithTraits(
   const auto& environment_params =
       kEnvironmentParams[GetEnvironmentIndexForTraits(traits)];
   return single_thread_task_runner_manager_
-      ->CreateSingleThreadTaskRunnerWithTraits(
+      .CreateSingleThreadTaskRunnerWithTraits(
           name_ + environment_params.name_suffix,
           environment_params.priority_hint, traits);
 }
@@ -110,7 +109,7 @@ scoped_refptr<SingleThreadTaskRunner>
 TaskSchedulerImpl::CreateCOMSTATaskRunnerWithTraits(const TaskTraits& traits) {
   const auto& environment_params =
       kEnvironmentParams[GetEnvironmentIndexForTraits(traits)];
-  return single_thread_task_runner_manager_->CreateCOMSTATaskRunnerWithTraits(
+  return single_thread_task_runner_manager_.CreateCOMSTATaskRunnerWithTraits(
       environment_params.name_suffix, environment_params.priority_hint, traits);
 }
 #endif  // defined(OS_WIN)
@@ -141,7 +140,7 @@ void TaskSchedulerImpl::JoinForTesting() {
 #if DCHECK_IS_ON()
   DCHECK(!join_for_testing_returned_.IsSet());
 #endif
-  single_thread_task_runner_manager_->JoinForTesting();
+  single_thread_task_runner_manager_.JoinForTesting();
   for (const auto& worker_pool : worker_pools_)
     worker_pool->DisallowWorkerDetachmentForTesting();
   for (const auto& worker_pool : worker_pools_)
@@ -153,7 +152,10 @@ void TaskSchedulerImpl::JoinForTesting() {
 }
 
 TaskSchedulerImpl::TaskSchedulerImpl(StringPiece name)
-    : name_(name), service_thread_("TaskSchedulerServiceThread") {}
+    : name_(name),
+      service_thread_("TaskSchedulerServiceThread"),
+      single_thread_task_runner_manager_(&task_tracker_,
+                                         &delayed_task_manager_) {}
 
 void TaskSchedulerImpl::Initialize(
     const TaskScheduler::InitParams& init_params) {
@@ -178,13 +180,9 @@ void TaskSchedulerImpl::Initialize(
 #endif
 
   // Needs to happen after starting the service thread to get its task_runner().
-  delayed_task_manager_ =
-      base::MakeUnique<DelayedTaskManager>(service_thread_.task_runner());
+  delayed_task_manager_.Start(service_thread_.task_runner());
 
-  single_thread_task_runner_manager_ =
-      MakeUnique<SchedulerSingleThreadTaskRunnerManager>(
-          &task_tracker_, delayed_task_manager_.get());
-  single_thread_task_runner_manager_->Start();
+  single_thread_task_runner_manager_.Start();
 
   // Callback invoked by workers to re-enqueue a sequence in the appropriate
   // PriorityQueue.
@@ -217,8 +215,7 @@ void TaskSchedulerImpl::Initialize(
     worker_pools_[environment_type] = MakeUnique<SchedulerWorkerPoolImpl>(
         name_ + kEnvironmentParams[environment_type].name_suffix,
         kEnvironmentParams[environment_type].priority_hint,
-        re_enqueue_sequence_callback, &task_tracker_,
-        delayed_task_manager_.get());
+        re_enqueue_sequence_callback, &task_tracker_, &delayed_task_manager_);
     worker_pools_[environment_type]->Start(
         *worker_pool_params[environment_type]);
   }
--- base/task_scheduler/task_scheduler_impl.h
+++ base/task_scheduler/task_scheduler_impl.h
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/strings/string_piece.h"
 #include "base/synchronization/atomic_flag.h"
+#include "base/task_scheduler/delayed_task_manager.h"
+#include "base/task_scheduler/scheduler_single_thread_task_runner_manager.h"
 #include "base/task_scheduler/scheduler_worker_pool_impl.h"
 #include "base/task_scheduler/sequence.h"
 #include "base/task_scheduler/task_scheduler.h"
@@ -33,9 +35,6 @@ namespace base {
 class HistogramBase;
 
 namespace internal {
-
-class DelayedTaskManager;
-class SchedulerSingleThreadTaskRunnerManager;
 
 // Default TaskScheduler implementation. This class is thread-safe.
 class BASE_EXPORT TaskSchedulerImpl : public TaskScheduler {
@@ -95,9 +94,8 @@ class BASE_EXPORT TaskSchedulerImpl : public TaskScheduler {
 #else
   TaskTracker task_tracker_;
 #endif
-  std::unique_ptr<DelayedTaskManager> delayed_task_manager_;
-  std::unique_ptr<SchedulerSingleThreadTaskRunnerManager>
-      single_thread_task_runner_manager_;
+  DelayedTaskManager delayed_task_manager_;
+  SchedulerSingleThreadTaskRunnerManager single_thread_task_runner_manager_;
 
   // There are 4 SchedulerWorkerPoolImpl in this array to match the 4
   // SchedulerWorkerPoolParams in TaskScheduler::InitParams.
