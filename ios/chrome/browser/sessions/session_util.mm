36beba2b99401a9b84f33d059468dd1cfc911b6c
sdefresne <sdefresne@chromium.org>
2017-04-20 03:54:03 -0700

    [ios] Change WebStateList serialization method to use SessionWindowIOS.
    In order to properly save and restore the application state in the new
    architecture, (partially) move the implementation outside of TabModel
    (which is not used in the new architecture).
    
    BUG=710848
    
    Review-Url: https://codereview.chromium.org/2808773004
    Cr-Commit-Position: refs/heads/master@{#465969}

--- ios/chrome/browser/sessions/session_util.mm
+++ ios/chrome/browser/sessions/session_util.mm
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/sessions/session_service_ios.h"
 #include "ios/web/public/browser_state.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace session_util {
 
 // Deletes the file containing the commands for the last session.
--- ios/chrome/browser/web_state_list/web_state_list_serialization.mm
+++ ios/chrome/browser/web_state_list/web_state_list_serialization.mm
@@ -5,12 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/web_state_list/web_state_list_serialization.h"
 
+#include <stdint.h>
+
 #include <memory>
 #include <unordered_map>
 
 #include "base/callback.h"
 #include "base/logging.h"
 #import "base/mac/foundation_util.h"
+#import "ios/chrome/browser/sessions/session_window_ios.h"
 #import "ios/chrome/browser/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/web_state_list/web_state_opener.h"
 #import "ios/web/public/serializable_user_data_manager.h"
@@ -131,8 +134,7 @@ void RestoreRelationship(WebStateList* web_state_list, int old_count) {
 }
 }  // namespace
 
-NSArray<CRWSessionStorage*>* SerializeWebStateList(
-    WebStateList* web_state_list) {
+SessionWindowIOS* SerializeWebStateList(WebStateList* web_state_list) {
   NSMutableArray<CRWSessionStorage*>* serialized_session =
       [NSMutableArray arrayWithCapacity:web_state_list->count()];
 
@@ -159,18 +161,31 @@ NSArray<CRWSessionStorage*>* SerializeWebStateList(
     [serialized_session addObject:web_state->BuildSessionStorage()];
   }
 
-  return [serialized_session copy];
+  NSUInteger selectedIndex =
+      web_state_list->active_index() != WebStateList::kInvalidIndex
+          ? static_cast<NSUInteger>(web_state_list->active_index())
+          : static_cast<NSUInteger>(NSNotFound);
+
+  return [[SessionWindowIOS alloc] initWithSessions:[serialized_session copy]
+                                      selectedIndex:selectedIndex];
 }
 
 void DeserializeWebStateList(WebStateList* web_state_list,
-                             NSArray<CRWSessionStorage*>* sessions,
+                             SessionWindowIOS* session_window,
                              const WebStateFactory& web_state_factory) {
   int old_count = web_state_list->count();
-  for (CRWSessionStorage* session in sessions) {
+  for (CRWSessionStorage* session in session_window.sessions) {
     std::unique_ptr<web::WebState> web_state = web_state_factory.Run(session);
     web_state_list->InsertWebState(web_state_list->count(),
                                    std::move(web_state));
   }
 
   RestoreRelationship(web_state_list, old_count);
+
+  if (session_window.selectedIndex != NSNotFound) {
+    DCHECK_LT(session_window.selectedIndex, session_window.sessions.count);
+    DCHECK_LT(session_window.selectedIndex, static_cast<NSUInteger>(INT_MAX));
+    web_state_list->ActivateWebStateAt(
+        old_count + static_cast<int>(session_window.selectedIndex));
+  }
 }
