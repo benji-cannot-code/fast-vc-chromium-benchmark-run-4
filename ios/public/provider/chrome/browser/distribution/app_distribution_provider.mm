d59bdbac1c9ae0937f759a39296a921258d28145
gambard <gambard@chromium.org>
2017-04-19 23:49:42 -0700

    [ObjC ARC] Converts ios/public/provider/chrome/browser/distribution:distribution to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2828003002
    Cr-Commit-Position: refs/heads/master@{#465925}

--- ios/public/provider/chrome/browser/distribution/app_distribution_provider.mm
+++ ios/public/provider/chrome/browser/distribution/app_distribution_provider.mm
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/public/provider/chrome/browser/distribution/app_distribution_provider.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 AppDistributionProvider::AppDistributionProvider() {}
 
 AppDistributionProvider::~AppDistributionProvider() {}
