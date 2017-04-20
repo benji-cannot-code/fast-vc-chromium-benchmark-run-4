d4200f4ea030bc04bcec13201e3361ffaae9a34c
gambard <gambard@chromium.org>
2017-04-19 23:48:11 -0700

    [ObjC ARC] Converts ios/public/provider/chrome/browser/images:images to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2831633002
    Cr-Commit-Position: refs/heads/master@{#465923}

--- ios/public/provider/chrome/browser/images/branded_image_provider.mm
+++ ios/public/provider/chrome/browser/images/branded_image_provider.mm
@@ -7,6 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 BrandedImageProvider::BrandedImageProvider() {}
 
 BrandedImageProvider::~BrandedImageProvider() {}
