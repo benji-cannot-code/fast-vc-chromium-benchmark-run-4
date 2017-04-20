b8f961f6dff2c72a1eb4b56b04a04d25e42484ed
jam <jam@chromium.org>
2017-04-20 15:45:00 -0700

    Replace SetUpInProcessBrowserTestFixture/TearDownInProcessBrowserTestFixture usage in content with SetUpOnMainThread/TearDownOnMainThread.
    BUG=713847
    
    Review-Url: https://codereview.chromium.org/2832023002
    Cr-Commit-Position: refs/heads/master@{#466171}

--- content/browser/accessibility/cross_platform_accessibility_browsertest.cc
+++ content/browser/accessibility/cross_platform_accessibility_browsertest.cc
@@ -65,8 +65,8 @@ class CrossPlatformAccessibilityBrowserTest : public ContentBrowserTest {
   }
 
   // ContentBrowserTest
-  void SetUpInProcessBrowserTestFixture() override;
-  void TearDownInProcessBrowserTestFixture() override;
+  void SetUpOnMainThread() override;
+  void TearDownOnMainThread() override;
 
  protected:
   std::string GetAttr(const ui::AXNode* node,
@@ -84,15 +84,14 @@ class CrossPlatformAccessibilityBrowserTest : public ContentBrowserTest {
   DISALLOW_COPY_AND_ASSIGN(CrossPlatformAccessibilityBrowserTest);
 };
 
-void CrossPlatformAccessibilityBrowserTest::SetUpInProcessBrowserTestFixture() {
+void CrossPlatformAccessibilityBrowserTest::SetUpOnMainThread() {
 #if defined(OS_WIN)
   ui::win::CreateATLModuleIfNeeded();
   com_initializer_.reset(new base::win::ScopedCOMInitializer());
 #endif
 }
 
-void
-CrossPlatformAccessibilityBrowserTest::TearDownInProcessBrowserTestFixture() {
+void CrossPlatformAccessibilityBrowserTest::TearDownOnMainThread() {
 #if defined(OS_WIN)
   com_initializer_.reset();
 #endif
