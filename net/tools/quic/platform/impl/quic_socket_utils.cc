ac26fca20df655b2c7108657c8bd1240c4a72438
rch <rch@chromium.org>
2017-04-20 15:37:27 -0700

    Landing Recent QUIC changes until Thu Apr 20 2017
    Convert QUIC non-feature flags to the old syntax, for consistency.
    
    Merge internal change: 153720286
    https://codereview.chromium.org/2828163005/
    
    Increases inflight limit for QUIC BBR if the ack rate in the past half-rtt was  lower than half the current bandwidth estimate.
    
    Protected by FLAGS_quic_reloadable_flag_quic_bbr_slow_recent_delivery.
    
    Merge internal change: 153663187
    https://codereview.chromium.org/2833673006/
    
    Include unistd.h in quic_socket_utils.cc
    
    Merge internal change: 153540629
    https://codereview.chromium.org/2835473003/
    
    Remain in packet conservation mode if BBR is in PROBE_BW and not in high-gain mode.  Protected by FLAGS_quic_reloadable_flag_quic_bbr_extra_conservation.
    
    Merge internal change: 153506591
    https://codereview.chromium.org/2832003002/
    
    Review-Url: https://codereview.chromium.org/2834723002
    Cr-Commit-Position: refs/heads/master@{#466169}

--- net/tools/quic/platform/impl/quic_socket_utils.cc
+++ net/tools/quic/platform/impl/quic_socket_utils.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/uio.h>
+#include <unistd.h>
 #include <string>
 
 #include "net/quic/core/quic_packets.h"
