ab179802f376435704540335270581166ed33854
jbroman <jbroman@chromium.org>
2017-04-20 07:25:03 -0700

    Use a two-part data format version in IndexedDB metadata.
    This breaks the 64-bit data format version stored in the database metadata
    into two 32-bit parts (V8 and Blink format versions), and allows them to be
    compared by checking that the database uses supported versions of both.
    
    If either component has reverted, the database is deemed to be from the future
    (i.e. corrupt).
    
    BUG=704293
    
    Review-Url: https://codereview.chromium.org/2773823002
    Cr-Commit-Position: refs/heads/master@{#466002}

---
+++ content/browser/indexed_db/indexed_db_data_format_version.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/indexed_db/indexed_db_data_format_version.h"
+
+#include "third_party/WebKit/public/web/WebSerializedScriptValueVersion.h"
+
+namespace content {
+
+// static
+IndexedDBDataFormatVersion IndexedDBDataFormatVersion::current_(
+    0,
+    blink::kSerializedScriptValueVersion);
+
+}  // namespace content
