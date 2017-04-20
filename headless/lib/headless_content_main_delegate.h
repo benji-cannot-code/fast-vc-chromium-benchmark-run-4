7f7b5ae69148ca6d8850ea74fcd21544662ec50a
dvallet <dvallet@chromium.org>
2017-04-20 15:56:47 -0700

    Launch bug: https://bugs.chromium.org/p/chromium/issues/detail?id=705916
    Add --headless flag to Windows:
    
    Fix browser_tests not compiling properly on windows
    Fix headless/BUILD.gn by:
       - Changing headless_lib to a headless component, following steps in https://chromium.googlesource.com/chromium/src/+/master/docs/component_build.md and fixing the build for linux, since it now compiles as a shared_library when component build is true.
       - Adding neccesary HEADLESS_EXPORT (submitted in https://codereview.chromium.org/2775693003/)
       - Disabling breakpad in windows (will fix in a separate CL)
    
    BUG=686608
    
    Review-Url: https://codereview.chromium.org/2762593002
    Cr-Commit-Position: refs/heads/master@{#466176}

--- headless/lib/headless_content_main_delegate.h
+++ headless/lib/headless_content_main_delegate.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "content/public/app/content_main_delegate.h"
+#include "content/public/browser/content_browser_client.h"
+#include "content/public/renderer/content_renderer_client.h"
 #include "headless/lib/browser/headless_platform_event_source.h"
 #include "headless/lib/headless_content_client.h"
 #include "headless/public/headless_export.h"
@@ -23,8 +25,6 @@ class CommandLine;
 namespace headless {
 
 class HeadlessBrowserImpl;
-class HeadlessContentBrowserClient;
-class HeadlessContentRendererClient;
 
 // Exported for tests.
 class HEADLESS_EXPORT HeadlessContentMainDelegate
@@ -58,8 +58,8 @@ class HEADLESS_EXPORT HeadlessContentMainDelegate
 
   static HeadlessContentMainDelegate* GetInstance();
 
-  std::unique_ptr<HeadlessContentBrowserClient> browser_client_;
-  std::unique_ptr<HeadlessContentRendererClient> renderer_client_;
+  std::unique_ptr<content::ContentRendererClient> renderer_client_;
+  std::unique_ptr<content::ContentBrowserClient> browser_client_;
   HeadlessContentClient content_client_;
   HeadlessPlatformEventSource platform_event_source_;
 
