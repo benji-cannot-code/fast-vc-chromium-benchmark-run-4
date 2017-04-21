dd53eab032581c131d202ea5da1e135cbd83bb2d
mattcary <mattcary@chromium.org>
2017-04-21 01:55:44 -0700

    Revert of GeoLocation: add support for GmsCore location provider (patchset #5 id:260001 of https://codereview.chromium.org/2809813002/ )
    Reason for revert:
    crbug.com/714053
    
    This CL apparently broke WebKit CTS geolocation tests.
    
    Original issue's description:
    > GeoLocation: add support for GmsCore location provider
    >
    > This CL adds LocationProviderGmsCore.java, an implementation
    > of LocationProviderFactory.LocationProvider interface using the
    > Google Play Services (GMS) provided location.
    >
    > The tests content/public/.../LocationProviderTest.java are removed
    > and instead a new pumped up one is added in device/geolocation,
    > doing what the previous one was doing and parameterized to try
    > other location provider APIs (was: android only, now: mock,
    > android and GMS core).
    >
    >
    > BUG=706014
    >
    > Review-Url: https://codereview.chromium.org/2809813002
    > Cr-Commit-Position: refs/heads/master@{#466239}
    > Committed: https://chromium.googlesource.com/chromium/src/+/638d9c4adea26e0f8a9d2e429546a44f5603b99c
    
    TBR=aelias@chromium.org,agrieve@chromium.org,jbudorick@chromium.org,reillyg@chromium.org,timvolodine@chromium.org,mcasas@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=706014
    
    Review-Url: https://codereview.chromium.org/2832983003
    Cr-Commit-Position: refs/heads/master@{#466291}

--- device/geolocation/android/java/src/org/chromium/device/geolocation/LocationProviderAdapter.java
+++ device/geolocation/android/java/src/org/chromium/device/geolocation/LocationProviderAdapter.java
@@ -6,9 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.device.geolocation;
 
 import android.content.Context;
-import android.location.Location;
 
-import org.chromium.base.Log;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.base.annotations.CalledByNative;
@@ -27,8 +25,6 @@ import java.util.concurrent.FutureTask;
 @MainDex
 @VisibleForTesting
 public class LocationProviderAdapter {
-    private static final String TAG = "cr_LocationProvider";
-
     // Delegate handling the real work in the main thread.
     private LocationProviderFactory.LocationProvider mImpl;
 
@@ -80,15 +76,14 @@ public class LocationProviderAdapter {
         return mImpl.isRunning();
     }
 
-    public static void onNewLocationAvailable(Location location) {
-        nativeNewLocationAvailable(location.getLatitude(), location.getLongitude(),
-                location.getTime() / 1000.0, location.hasAltitude(), location.getAltitude(),
-                location.hasAccuracy(), location.getAccuracy(), location.hasBearing(),
-                location.getBearing(), location.hasSpeed(), location.getSpeed());
+    public static void newLocationAvailable(double latitude, double longitude, double timestamp,
+            boolean hasAltitude, double altitude, boolean hasAccuracy, double accuracy,
+            boolean hasHeading, double heading, boolean hasSpeed, double speed) {
+        nativeNewLocationAvailable(latitude, longitude, timestamp, hasAltitude, altitude,
+                hasAccuracy, accuracy, hasHeading, heading, hasSpeed, speed);
     }
 
     public static void newErrorAvailable(String message) {
-        Log.e(TAG, "newErrorAvailable %s", message);
         nativeNewErrorAvailable(message);
     }
 
