ab288d29b7e4daf5c1a0ee97b6f943cb8bcd7b22
xjz <xjz@chromium.org>
2017-04-20 18:42:25 -0700

    RELAND: Media Remoting end to end integration tests.
    This is a re-land of https://codereview.chromium.org/2692593002/.
    Moved tests for media remoting pipeline out of general
    PipelineIntegrationTest.
    
    -------Description of original change follows-------
    
    Media Remoting: End to end integration tests.
    
    Add end to end integration tests for Media Remoting. Refactors
    PipelineIntegrationTest to test both media and media remoting pipeline.
    Re-use current tests. No new tests are added in this CL.
    
    BUG=684065
    
    Review-Url: https://codereview.chromium.org/2808583002
    Cr-Commit-Position: refs/heads/master@{#466216}

--- media/test/pipeline_integration_test_base.h
+++ media/test/pipeline_integration_test_base.h
@@ -26,7 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
-class CdmContext;
+class FakeEncryptedMedia;
+class MockMediaSource;
 
 // Empty MD5 hash string.  Used to verify empty video tracks.
 extern const char kNullVideoHash[];
@@ -44,6 +45,15 @@ class DummyTickClock : public base::TickClock {
 
  private:
   base::TimeTicks now_;
+};
+
+class PipelineTestRendererFactory {
+ public:
+  virtual ~PipelineTestRendererFactory() {}
+  // Creates and returns a Renderer.
+  virtual std::unique_ptr<Renderer> CreateRenderer(
+      CreateVideoDecodersCB prepend_video_decoders_cb,
+      CreateAudioDecodersCB prepend_audio_decoders_cb) = 0;
 };
 
 // Integration tests for Pipeline. Real demuxers, real decoders, and
@@ -127,6 +137,10 @@ class PipelineIntegrationTestBase : public Pipeline::Client {
     encrypted_media_init_data_cb_ = encrypted_media_init_data_cb;
   }
 
+  std::unique_ptr<Renderer> CreateRenderer(
+      CreateVideoDecodersCB prepend_video_decoders_cb,
+      CreateAudioDecodersCB prepend_audio_decoders_cb);
+
  protected:
   MediaLog media_log_;
   base::MessageLoop message_loop_;
@@ -151,6 +165,7 @@ class PipelineIntegrationTestBase : public Pipeline::Client {
   PipelineMetadata metadata_;
   scoped_refptr<VideoFrame> last_frame_;
   base::TimeDelta current_duration_;
+  std::unique_ptr<PipelineTestRendererFactory> renderer_factory_;
 
   PipelineStatus StartInternal(
       std::unique_ptr<DataSource> data_source,
@@ -168,6 +183,15 @@ class PipelineIntegrationTestBase : public Pipeline::Client {
       CreateAudioDecodersCB prepend_audio_decoders_cb =
           CreateAudioDecodersCB());
 
+  PipelineStatus StartPipelineWithMediaSource(MockMediaSource* source);
+  PipelineStatus StartPipelineWithEncryptedMedia(
+      MockMediaSource* source,
+      FakeEncryptedMedia* encrypted_media);
+  PipelineStatus StartPipelineWithMediaSource(
+      MockMediaSource* source,
+      uint8_t test_type,
+      FakeEncryptedMedia* encrypted_media);
+
   void OnSeeked(base::TimeDelta seek_time, PipelineStatus status);
   void OnStatusCallback(PipelineStatus status);
   void DemuxerEncryptedMediaInitDataCB(EmeInitDataType type,
@@ -179,12 +203,6 @@ class PipelineIntegrationTestBase : public Pipeline::Client {
 
   // Creates Demuxer and sets |demuxer_|.
   void CreateDemuxer(std::unique_ptr<DataSource> data_source);
-
-  // Creates and returns a Renderer.
-  virtual std::unique_ptr<Renderer> CreateRenderer(
-      CreateVideoDecodersCB prepend_video_decoders_cb = CreateVideoDecodersCB(),
-      CreateAudioDecodersCB prepend_audio_decoders_cb =
-          CreateAudioDecodersCB());
 
   void OnVideoFramePaint(const scoped_refptr<VideoFrame>& frame);
 
@@ -207,6 +225,9 @@ class PipelineIntegrationTestBase : public Pipeline::Client {
   MOCK_METHOD1(OnVideoNaturalSizeChange, void(const gfx::Size&));
   MOCK_METHOD1(OnVideoOpacityChange, void(bool));
   MOCK_METHOD0(OnVideoAverageKeyframeDistanceUpdate, void());
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(PipelineIntegrationTestBase);
 };
 
 }  // namespace media
