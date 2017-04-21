ba8d22cad6a4ee991ae2ff53cbe994fa379f74e4
lpromero <lpromero@chromium.org>
2017-04-21 08:10:21 -0700

    Remove dealloc that is now unnecessary
    BUG=rohitrao@chromium.org
    
    Review-Url: https://codereview.chromium.org/2836533002
    Cr-Commit-Position: refs/heads/master@{#466339}

--- ios/shared/chrome/browser/ui/coordinators/browser_coordinator.mm
+++ ios/shared/chrome/browser/ui/coordinators/browser_coordinator.mm
@@ -58,6 +58,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 }
 
+- (void)dealloc {
+  for (BrowserCoordinator* child in self.children) {
+    [self removeChildCoordinator:child];
+  }
+}
+
 @end
 
 @implementation BrowserCoordinator (Internal)
