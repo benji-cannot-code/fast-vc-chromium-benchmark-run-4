765d6ff194af42297dfbe85c5e3cd388f458a24a
japhet <japhet@chromium.org>
2017-04-21 15:35:20 -0700

    Move most of FrameLoader::CheckCompleted() to Document
    Split out the the parts that are specific to the committed Document, and
    move those to a new Document::CheckCompleted(). Most current callers of
    FrameLoader::CheckCompleted() will now call Document::CheckCompleted()
    instead. Rename the remained of FrameLoader::CheckCompleted() to
    DidFinishNavigation(), and have it handling the logic that is tied to
    setting Frame::loading_ to false and firing DidStopLoading() callbacks.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2809733003
    Cr-Commit-Position: refs/heads/master@{#466470}

--- third_party/WebKit/Source/core/loader/NavigationScheduler.cpp
+++ third_party/WebKit/Source/core/loader/NavigationScheduler.cpp
@@ -508,6 +508,8 @@ void NavigationScheduler::Schedule(ScheduledNavigation* redirect) {
 
   Cancel();
   redirect_ = redirect;
+  if (redirect_->IsLocationChange())
+    frame_->GetDocument()->SuppressLoadEvent();
   StartTimer();
 }
 
