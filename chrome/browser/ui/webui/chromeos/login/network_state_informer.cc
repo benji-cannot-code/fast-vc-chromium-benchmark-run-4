3bca382e4a4a5b3e60b358b2b9024b077463a521
sammiequon <sammiequon@chromium.org>
2017-04-21 13:57:00 -0700

    tray: Show warning in network tray when VPN or proxy used.
    Let the user know they may be monitored if a VPN or proxy is being used.
    
    TEST=manual
    BUG=646698
    
    Review-Url: https://codereview.chromium.org/2836453003
    Cr-Commit-Position: refs/heads/master@{#466442}

--- chrome/browser/ui/webui/chromeos/login/network_state_informer.cc
+++ chrome/browser/ui/webui/chromeos/login/network_state_informer.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/network/network_state.h"
 #include "chromeos/network/network_state_handler.h"
 #include "chromeos/network/proxy/proxy_config_handler.h"
+#include "chromeos/network/proxy/ui_proxy_config_service.h"
 #include "components/proxy_config/proxy_config_dictionary.h"
 #include "components/proxy_config/proxy_prefs.h"
 #include "net/proxy/proxy_config.h"
@@ -29,20 +30,6 @@ const char kNetworkStateOnline[] = "online";
 const char kNetworkStateCaptivePortal[] = "behind captive portal";
 const char kNetworkStateConnecting[] = "connecting";
 const char kNetworkStateProxyAuthRequired[] = "proxy auth required";
-
-bool HasDefaultNetworkProxyConfigured() {
-  const NetworkState* network =
-      NetworkHandler::Get()->network_state_handler()->DefaultNetwork();
-  if (!network)
-    return false;
-  onc::ONCSource onc_source = onc::ONC_SOURCE_NONE;
-  std::unique_ptr<ProxyConfigDictionary> proxy_dict =
-      proxy_config::GetProxyConfigForNetwork(
-          NULL, g_browser_process->local_state(), *network, &onc_source);
-  ProxyPrefs::ProxyMode mode;
-  return (proxy_dict && proxy_dict->GetMode(&mode) &&
-          mode == ProxyPrefs::MODE_FIXED_SERVERS);
-}
 
 NetworkStateInformer::State GetStateForDefaultNetwork() {
   const NetworkState* network =
@@ -63,13 +50,17 @@ NetworkStateInformer::State GetStateForDefaultNetwork() {
     // NetworkPortalDetector's state of current network is unknown.
     if (status == NetworkPortalDetector::CAPTIVE_PORTAL_STATUS_ONLINE ||
         (status == NetworkPortalDetector::CAPTIVE_PORTAL_STATUS_UNKNOWN &&
-         !HasDefaultNetworkProxyConfigured() &&
+         !NetworkHandler::Get()
+              ->ui_proxy_config_service()
+              ->HasDefaultNetworkProxyConfigured() &&
          network->connection_state() == shill::kStateOnline)) {
       return NetworkStateInformer::ONLINE;
     }
     if (status ==
             NetworkPortalDetector::CAPTIVE_PORTAL_STATUS_PROXY_AUTH_REQUIRED &&
-        HasDefaultNetworkProxyConfigured()) {
+        NetworkHandler::Get()
+            ->ui_proxy_config_service()
+            ->HasDefaultNetworkProxyConfigured()) {
       return NetworkStateInformer::PROXY_AUTH_REQUIRED;
     }
     if (status == NetworkPortalDetector::CAPTIVE_PORTAL_STATUS_PORTAL ||
