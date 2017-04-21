7b47c55df87422c663acdc0b5b2bf7adc9eb14b3
mthiesse <mthiesse@chromium.org>
2017-04-20 19:27:24 -0700

    Fix includes for skia_paint_canvas.h
    BUG=
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2832063002
    Cr-Commit-Position: refs/heads/master@{#466234}

--- cc/paint/skia_paint_canvas.cc
+++ cc/paint/skia_paint_canvas.cc
@@ -3,11 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "cc/paint/paint_canvas.h"
+#include "cc/paint/skia_paint_canvas.h"
 
 #include "base/memory/ptr_util.h"
 #include "cc/paint/display_item_list.h"
-#include "cc/paint/paint_record.h"
 #include "cc/paint/paint_recorder.h"
 #include "third_party/skia/include/core/SkAnnotation.h"
 #include "third_party/skia/include/core/SkMetaData.h"
