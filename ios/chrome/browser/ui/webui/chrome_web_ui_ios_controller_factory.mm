237a637fe453e3169fc8c664fb2b52660eff4b2c
gambard <gambard@chromium.org>
2017-04-21 00:06:22 -0700

    [ObjC ARC] Converts ios/chrome/browser/ui/webui:webui_internal to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2829753004
    Cr-Commit-Position: refs/heads/master@{#466278}

--- ios/chrome/browser/ui/webui/chrome_web_ui_ios_controller_factory.mm
+++ ios/chrome/browser/ui/webui/chrome_web_ui_ios_controller_factory.mm
@@ -24,6 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/ui/webui/version_ui.h"
 #include "url/gurl.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 using web::WebUIIOS;
 using web::WebUIIOSController;
 
