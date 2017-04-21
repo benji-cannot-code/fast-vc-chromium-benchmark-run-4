3cc459287445e20c899a9a7f4d67780eb07c373a
twellington <twellington@chromium.org>
2017-04-21 13:10:32 -0700

    [Home] Don't reopen BottomSheet when switching back to NTP
    Open the BottomSheet when an NTP is created, but do not
    open when switching back to a previously created NTP.
    
    BUG=712576
    
    Review-Url: https://codereview.chromium.org/2831403002
    Cr-Commit-Position: refs/heads/master@{#466425}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/ChromeHomeNewTabPageBase.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/ChromeHomeNewTabPageBase.java
@@ -109,6 +109,8 @@ public abstract class ChromeHomeNewTabPageBase implements NativePage {
         boolean tabAlreadyShowing = mTabModelSelector.getCurrentTab() == mTab;
         if (tabAlreadyShowing) onNewTabPageShown();
 
+        mBottomSheet.setSheetState(BottomSheet.SHEET_STATE_HALF, true);
+
         // TODO(twellington): disallow moving the NTP to the other window in Android N+
         //                    multi-window mode.
     }
@@ -160,7 +162,6 @@ public abstract class ChromeHomeNewTabPageBase implements NativePage {
         // NTPs to remain open after the user leaves them.
         if (getLayoutManager() != null && getLayoutManager().overviewVisible()) return;
 
-        mBottomSheet.setSheetState(BottomSheet.SHEET_STATE_HALF, true);
         mBottomSheet.getBottomSheetMetrics().recordSheetOpenReason(
                 BottomSheetMetrics.OPENED_BY_NEW_TAB_CREATION);
     }
