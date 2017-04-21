80b7519fb118666863ae8d158f001c7c12c1e294
erikchen <erikchen@chromium.org>
2017-04-21 12:06:55 -0700

    Add the UMA metric Memory.Gpu.PhysicalFootprint.MacOS
    This is a temporary metric added to debug Issue 713854. The existing metrics
    don't measure OpenGL memory usage.
    
    BUG=713854
    
    Review-Url: https://codereview.chromium.org/2832933003
    Cr-Commit-Position: refs/heads/master@{#466405}

--- chrome/browser/memory_details_mac.cc
+++ chrome/browser/memory_details_mac.cc
@@ -63,6 +63,7 @@ void CollectProcessDataForChromeProcess(
       base::ProcessMetrics::CreateProcessMetrics(
           pid, content::BrowserChildProcessHost::GetPortProvider());
   metrics->GetCommittedAndWorkingSetKBytes(&info.committed, &info.working_set);
+  info.phys_footprint = metrics->GetPhysicalFootprint();
 
   processes->push_back(info);
 }
