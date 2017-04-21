e3fde99b0e3d1b6e9ff6dbd718ac2e2ccd1ca4c4
ortuno <ortuno@chromium.org>
2017-04-20 19:13:20 -0700

    bluetooth: Add RSSI icon for bluetooth choosers.
    So that users can more easily identify the devices that are near them.
    
    BUG=629689
    
    Review-Url: https://codereview.chromium.org/2821973003
    Cr-Commit-Position: refs/heads/master@{#466230}

--- chrome/android/java/src/org/chromium/chrome/browser/BluetoothChooserDialog.java
+++ chrome/android/java/src/org/chromium/chrome/browser/BluetoothChooserDialog.java
@@ -77,6 +77,8 @@ public class BluetoothChooserDialog
     Drawable mConnectedIcon;
     @VisibleForTesting
     String mConnectedIconDescription;
+    @VisibleForTesting
+    Drawable[] mSignalStrengthLevelIcon;
 
     // A pointer back to the native part of the implementation for this dialog.
     long mNativeBluetoothChooserDialogPtr;
@@ -129,15 +131,16 @@ public class BluetoothChooserDialog
         mNativeBluetoothChooserDialogPtr = nativeBluetoothChooserDialogPtr;
         mAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        Resources res = mActivity.getResources();
-
         // Initialize icons.
-        mConnectedIcon = VectorDrawableCompat.create(
-                res, R.drawable.ic_bluetooth_connected, mActivity.getTheme());
-        DrawableCompat.setTintList(mConnectedIcon,
-                ApiCompatibilityUtils.getColorStateList(res, R.color.item_chooser_row_icon_color));
-
+        mConnectedIcon = getIconWithRowIconColorStateList(R.drawable.ic_bluetooth_connected);
         mConnectedIconDescription = mActivity.getString(R.string.bluetooth_device_connected);
+
+        mSignalStrengthLevelIcon = new Drawable[] {
+                getIconWithRowIconColorStateList(R.drawable.ic_signal_cellular_0_bar),
+                getIconWithRowIconColorStateList(R.drawable.ic_signal_cellular_1_bar),
+                getIconWithRowIconColorStateList(R.drawable.ic_signal_cellular_2_bar),
+                getIconWithRowIconColorStateList(R.drawable.ic_signal_cellular_3_bar),
+                getIconWithRowIconColorStateList(R.drawable.ic_signal_cellular_4_bar)};
 
         if (mAdapter == null) {
             Log.i(TAG, "BluetoothChooserDialog: Default Bluetooth adapter not found.");
@@ -146,6 +149,15 @@ public class BluetoothChooserDialog
                 SpanApplier.applySpans(mActivity.getString(R.string.bluetooth_adapter_off_help),
                         new SpanInfo("<link>", "</link>",
                                 new BluetoothClickableSpan(LinkType.ADAPTER_OFF_HELP, mActivity)));
+    }
+
+    private Drawable getIconWithRowIconColorStateList(int icon) {
+        Resources res = mActivity.getResources();
+
+        Drawable drawable = VectorDrawableCompat.create(res, icon, mActivity.getTheme());
+        DrawableCompat.setTintList(drawable,
+                ApiCompatibilityUtils.getColorStateList(res, R.color.item_chooser_row_icon_color));
+        return drawable;
     }
 
     /**
@@ -374,12 +386,18 @@ public class BluetoothChooserDialog
 
     @VisibleForTesting
     @CalledByNative
-    void addOrUpdateDevice(String deviceId, String deviceName, boolean isGATTConnected) {
+    void addOrUpdateDevice(
+            String deviceId, String deviceName, boolean isGATTConnected, int signalStrengthLevel) {
         Drawable icon = null;
         String iconDescription = null;
         if (isGATTConnected) {
             icon = mConnectedIcon.getConstantState().newDrawable();
             iconDescription = mConnectedIconDescription;
+        } else if (signalStrengthLevel != -1) {
+            icon = mSignalStrengthLevelIcon[signalStrengthLevel].getConstantState().newDrawable();
+            iconDescription = mActivity.getResources().getQuantityString(
+                    R.plurals.signal_strength_level_n_bars, signalStrengthLevel,
+                    signalStrengthLevel);
         }
 
         mItemChooserDialog.addOrUpdateItem(deviceId, deviceName, icon, iconDescription);
