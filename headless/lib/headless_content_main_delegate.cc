46f1ddcd80794f4d16751ee78c99d03557681442
dvallet <dvallet@chromium.org>
2017-04-20 23:40:55 -0700

    Reland Enable crashpad for Mac. -- Add :mac_helpers dependendency to headless_lib
    Original reason for revert: Reason for revert:
    mac_chromium10.10_rel_ng buildbot failing in browsertests
    https://luci-logdog.appspot.com/v/?s=chromium%2Fbb%2Fchromium.mac%2FMac10.10_Tests%2F16541%2F%2B%2Frecipes%2Fsteps%2Fheadless_browsertests_on_Mac-10.10%2F0%2Flogs%2FCrashReporterTest.GenerateMinidump%2F0
    
    This follows the same logic as using breakpad in linux with flags --enable_crash-reporter and --crash-dumps-dir folders.
    
    For headless_shell target, we need to include the Helper crashpad_handler in the build. For --headless, this is already available in the framework bundle.
    
    BUG=687407
    
    Review-Url: https://codereview.chromium.org/2816593006
    Cr-Commit-Position: refs/heads/master@{#464289}
    Committed: https://chromium.googlesource.com/chromium/src/+/ecb2ad24952b077e71e56ba9d3edfc83c522f32a
    
    patch from issue 2816593006 at patchset 60001 (http://crrev.com/2816593006#ps60001)
    
    Review-Url: https://codereview.chromium.org/2820453003
    Cr-Original-Commit-Position: refs/heads/master@{#465869}
    Committed: https://chromium.googlesource.com/chromium/src/+/df51c3e48120bc5f68e0fc2cdc92d91592249e98
    Review-Url: https://codereview.chromium.org/2820453003
    Cr-Commit-Position: refs/heads/master@{#466275}

--- headless/lib/headless_content_main_delegate.cc
+++ headless/lib/headless_content_main_delegate.cc
@@ -37,6 +37,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "headless/lib/renderer/headless_content_renderer_client.h"
 #endif
 
+#if defined(OS_MACOSX)
+#include "components/crash/content/app/crashpad.h"
+#endif
+
 namespace headless {
 namespace {
 // Keep in sync with content/common/content_constants_internal.h.
@@ -163,16 +167,17 @@ void HeadlessContentMainDelegate::InitCrashReporter(
   g_headless_crash_client.Pointer()->set_crash_dumps_dir(
       browser_->options()->crash_dumps_dir);
 
-#if defined(OS_LINUX)
+#if defined(HEADLESS_USE_BREAKPAD)
   if (!browser_->options()->enable_crash_reporter) {
     DCHECK(!breakpad::IsCrashReporterEnabled());
     return;
   }
-#if defined(HEADLESS_USE_BREAKPAD)
   if (process_type != switches::kZygoteProcess)
     breakpad::InitCrashReporter(process_type);
+#elif defined(OS_MACOSX)
+  const bool browser_process = process_type.empty();
+  crash_reporter::InitializeCrashpad(browser_process, process_type);
 #endif  // defined(HEADLESS_USE_BREAKPAD)
-#endif  // !defined(OS_LINUX)
 }
 
 void HeadlessContentMainDelegate::PreSandboxStartup() {
@@ -185,10 +190,8 @@ void HeadlessContentMainDelegate::PreSandboxStartup() {
 #else
   if (command_line.HasSwitch(switches::kEnableLogging))
     InitLogging(command_line);
-#endif
-#if !defined(OS_MACOSX)
+#endif  // defined(OS_WIN)
   InitCrashReporter(command_line);
-#endif
   InitializeResourceBundle();
 }
 
