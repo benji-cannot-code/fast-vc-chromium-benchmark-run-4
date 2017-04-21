93f752b8ad3c75ec2532a929d59c6a3c3baf44ac
vadimt <vadimt@chromium.org>
2017-04-21 16:48:14 -0700

    Correcting the size of the web view once it changes after DidStopLoading.
    In the old model, when I recreated web contents for every request, by the moment of DidStopLoading() callback, I had the final web contents size. So the sequence was like:
    UpdatePreferredSize
    DidStopLoading
    
    After changing to reusing a single instance of web contents (and perhaps some other changes), the sequence looks like this:
    DidStopLoading
    UpdatePreferredSize (size1)
    UpdatePreferredSize (size2 - final)
    
    Changing the code to relayout search_results_page_view_ each time the size changes. Also still need to layout after showing the view since just changing visibility to true doesn’t trigger layout.
    
    In the future, I’ll try optimizing this, i.e. try finding a moment when the size is guaranteed to be final. However, I doubt this is solvable in a general case, with the page running java scripts etc.
    
    Bug=712331
    
    Review-Url: https://codereview.chromium.org/2827193003
    Cr-Commit-Position: refs/heads/master@{#466493}

--- chrome/browser/ui/app_list/search_answer_web_contents_delegate.h
+++ chrome/browser/ui/app_list/search_answer_web_contents_delegate.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "content/public/browser/web_contents_delegate.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "url/gurl.h"
 
@@ -28,7 +29,8 @@ class WebView;
 namespace app_list {
 
 // Manages the web contents for the search answer web view.
-class SearchAnswerWebContentsDelegate : public content::WebContentsObserver {
+class SearchAnswerWebContentsDelegate : public content::WebContentsDelegate,
+                                        public content::WebContentsObserver {
  public:
   SearchAnswerWebContentsDelegate(content::BrowserContext* browser_context,
                                   app_list::AppListModel* model);
@@ -43,6 +45,10 @@ class SearchAnswerWebContentsDelegate : public content::WebContentsObserver {
   // class. The object is owned by this class and has property
   // 'set_owned_by_client()' set.
   views::View* web_view();
+
+  // content::WebContentsDelegate overrides:
+  void UpdatePreferredSize(content::WebContents* web_contents,
+                           const gfx::Size& pref_size) override;
 
   // content::WebContentsObserver overrides:
   void DidFinishNavigation(
