06c780a1cdd31cf78317da5891f03faf23a3e804
awdf <awdf@chromium.org>
2017-04-21 09:02:58 -0700

    [Android O] Only delete legacy notification channels on upgrade
    - Now instead of deleting all channels on upgrade, we only delete
    those we know to be legacy channels.
    
    - This fixes a crash where we tried to delete the default channel
    created by Android causing an IllegalArgumentException.
    
    BUG=710843
    
    Review-Url: https://codereview.chromium.org/2832493002
    Cr-Commit-Position: refs/heads/master@{#466352}

--- chrome/android/java/src/org/chromium/chrome/browser/notifications/ChannelsUpdater.java
+++ chrome/android/java/src/org/chromium/chrome/browser/notifications/ChannelsUpdater.java
@@ -60,8 +60,7 @@ public class ChannelsUpdater {
     public void updateChannels() {
         if (!mIsAtLeastO) return;
         assert mChannelsInitializer != null;
-        // TODO(crbug.com/710843) Iterate through existing channels instead of deleting them all.
-        mChannelsInitializer.deleteAllChannels();
+        mChannelsInitializer.deleteLegacyChannels();
         mChannelsInitializer.initializeStartupChannels();
         storeChannelVersionInPrefs();
     }
