3f19c14dcaf91114c0897d7e447317d80006294b
rego <rego@igalia.com>
2017-04-21 02:47:33 -0700

    [css-grid] Remove unneeded line in GridTrackSizingAlgorithm
    Review-Url: https://codereview.chromium.org/2833023002
    Cr-Commit-Position: refs/heads/master@{#466298}

--- third_party/WebKit/Source/core/layout/GridTrackSizingAlgorithm.h
+++ third_party/WebKit/Source/core/layout/GridTrackSizingAlgorithm.h
@@ -22,8 +22,6 @@ class Grid;
 class GridTrackSizingAlgorithmStrategy;
 class LayoutGrid;
 
-enum MarginDirection;
-
 enum SizingOperation { kTrackSizing, kIntrinsicSizeComputation };
 
 enum TrackSizeComputationPhase {
