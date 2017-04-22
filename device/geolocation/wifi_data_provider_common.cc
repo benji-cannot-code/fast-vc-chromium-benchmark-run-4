10c4aee53d93cdbf51a520935eb710cdeecc2d2f
mcasas <mcasas@chromium.org>
2017-04-21 19:30:50 -0700

    Geolocation: cleanup wifi_data_provider_common_unittest
    Two of the test cases were flakily timing out so this CL rewrites
    the threading model of the test, removing the |run_loop_|
    and expecting MockWlanApi::GetAccessPointData() to
    explicitly finish the tests.
    
    Also this CL:
    - simplifies the logic using GMock mojo
    - moves a few variables to ctor initializer lists ISO SetUp()
    - removes RunNormal() test case bc is a dupe of
     DoAnEmptyScan().
    
    Important: this CL also removes the test case:
    GeolocationWifiDataProviderCommonTest::RegisterUnregister
    
    because it didn't belong to this unit test; incidentally
    it also verifies little since it just register-unregisters a
    test factory.  Will land some specific tests for this class
    very soon.
    
    BUG=405596
    
    Review-Url: https://codereview.chromium.org/2829983002
    Cr-Commit-Position: refs/heads/master@{#466525}

--- device/geolocation/wifi_data_provider_common.cc
+++ device/geolocation/wifi_data_provider_common.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace device {
 
 base::string16 MacAddressAsString16(const uint8_t mac_as_int[6]) {
-  // mac_as_int is big-endian. Write in byte chunks.
+  // |mac_as_int| is big-endian. Write in byte chunks.
   // Format is XX-XX-XX-XX-XX-XX.
   static const char* const kMacFormatString = "%02x-%02x-%02x-%02x-%02x-%02x";
   return base::ASCIIToUTF16(base::StringPrintf(
@@ -28,17 +28,17 @@ WifiDataProviderCommon::WifiDataProviderCommon()
 WifiDataProviderCommon::~WifiDataProviderCommon() {}
 
 void WifiDataProviderCommon::StartDataProvider() {
-  DCHECK(wlan_api_ == NULL);
+  DCHECK(!wlan_api_);
   wlan_api_.reset(NewWlanApi());
-  if (wlan_api_ == NULL) {
+  if (!wlan_api_) {
     // Error! Can't do scans, so don't try and schedule one.
     is_first_scan_complete_ = true;
     return;
   }
 
-  DCHECK(polling_policy_ == NULL);
+  DCHECK(!polling_policy_);
   polling_policy_.reset(NewPollingPolicy());
-  DCHECK(polling_policy_ != NULL);
+  DCHECK(polling_policy_);
 
   // Perform first scan ASAP regardless of the polling policy. If this scan
   // fails we'll retry at a rate in line with the polling policy.
