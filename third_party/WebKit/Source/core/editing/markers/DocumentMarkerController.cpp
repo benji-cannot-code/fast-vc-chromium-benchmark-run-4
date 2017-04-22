079d25781928a2c6ed3b351de3c6edeb9e4cb1d5
rlanday <rlanday@chromium.org>
2017-04-21 17:57:29 -0700

    Refactor DocumentMarkerController::RemoveMarkers(const MarkerRemoverPredicate&)
    MarkerRemoverPredicate seems to have been added with some amount of generality
    in mind, but the way it's written it only serves a single purpose, so I'm
    refactoring the RemoveMarkers() method that takes it to just take a vector of
    words instead.
    
    BUG=707867
    
    Review-Url: https://codereview.chromium.org/2833753004
    Cr-Commit-Position: refs/heads/master@{#466516}

--- third_party/WebKit/Source/core/editing/markers/DocumentMarkerController.cpp
+++ third_party/WebKit/Source/core/editing/markers/DocumentMarkerController.cpp
@@ -46,18 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-MarkerRemoverPredicate::MarkerRemoverPredicate(const Vector<String>& words)
-    : words_(words) {}
-
-bool MarkerRemoverPredicate::operator()(const DocumentMarker& document_marker,
-                                        const Text& text_node) const {
-  unsigned start = document_marker.StartOffset();
-  unsigned length = document_marker.EndOffset() - document_marker.StartOffset();
-
-  String marker_text = text_node.data().Substring(start, length);
-  return words_.Contains(marker_text);
-}
-
 namespace {
 
 DocumentMarker::MarkerTypeIndex MarkerTypeToMarkerIndex(
@@ -499,8 +487,8 @@ void DocumentMarkerController::RemoveMarkers(
     RemoveMarkersFromList(iterator, marker_types);
 }
 
-void DocumentMarkerController::RemoveMarkers(
-    const MarkerRemoverPredicate& should_remove_marker) {
+void DocumentMarkerController::RemoveSpellingMarkersUnderWords(
+    const Vector<String>& words) {
   for (auto& node_markers : markers_) {
     const Node& node = *node_markers.key;
     if (!node.IsTextNode())  // MarkerRemoverPredicate requires a Text node.
@@ -512,8 +500,13 @@ void DocumentMarkerController::RemoveMarkers(
         continue;
       bool removed_markers = false;
       for (size_t j = list->size(); j > 0; --j) {
-        if (should_remove_marker(*list->at(j - 1),
-                                 static_cast<const Text&>(node))) {
+        const DocumentMarker& marker = *list->at(j - 1);
+
+        const unsigned start = marker.StartOffset();
+        const unsigned length = marker.EndOffset() - marker.StartOffset();
+
+        const String marker_text = ToText(node).data().Substring(start, length);
+        if (words.Contains(marker_text)) {
           list->erase(j - 1);
           removed_markers = true;
         }
--- third_party/WebKit/Source/core/editing/spellcheck/SpellChecker.cpp
+++ third_party/WebKit/Source/core/editing/spellcheck/SpellChecker.cpp
@@ -927,11 +927,9 @@ void SpellChecker::RemoveSpellingMarkers() {
 
 void SpellChecker::RemoveSpellingMarkersUnderWords(
     const Vector<String>& words) {
-  MarkerRemoverPredicate remover_predicate(words);
-
   DocumentMarkerController& marker_controller =
       GetFrame().GetDocument()->Markers();
-  marker_controller.RemoveMarkers(remover_predicate);
+  marker_controller.RemoveSpellingMarkersUnderWords(words);
   marker_controller.RepaintMarkers();
 }
 
