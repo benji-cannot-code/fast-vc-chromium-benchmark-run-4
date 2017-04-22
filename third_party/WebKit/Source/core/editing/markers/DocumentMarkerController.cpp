cf26e5abae18ebf64799aa95b779645972d82264
rlanday <rlanday@chromium.org>
2017-04-21 20:39:11 -0700

    Rename DocumentMarkerController::RemoveMarkers() to RemoveMarkersForNode() yosin pointed out that it's confusing to have so many methods named RemoveMarkers(): https://codereview.chromium.org/2820633002#msg30
    This is the fourth of several renaming CLs I'm going to upload
    
    BUG=707867
    
    Review-Url: https://codereview.chromium.org/2830233002
    Cr-Commit-Position: refs/heads/master@{#466534}

--- third_party/WebKit/Source/core/editing/markers/DocumentMarkerController.cpp
+++ third_party/WebKit/Source/core/editing/markers/DocumentMarkerController.cpp
@@ -475,7 +475,7 @@ DEFINE_TRACE(DocumentMarkerController) {
   SynchronousMutationObserver::Trace(visitor);
 }
 
-void DocumentMarkerController::RemoveMarkers(
+void DocumentMarkerController::RemoveMarkersForNode(
     Node* node,
     DocumentMarker::MarkerTypes marker_types) {
   if (!PossiblyHasMarkers(marker_types))
