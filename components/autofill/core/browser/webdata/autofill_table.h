e3f7a7ef5b71003ab4da6a20903505835c00ce43
gangwu <gangwu@chromium.org>
2017-04-23 12:34:52 -0700

    Add UpdateSyncMetadata, ClearSyncMetadata, UpdateModelTypeState, ClearModelTypeState into SyncMetadataStore interface. Let AutofillTable inherit SyncMetadataStore. Also create a SyncMetadataStoreChangeList for autofill. Will do this change for typed url in next CL.
    BUG=708275
    
    Review-Url: https://codereview.chromium.org/2794413002
    Cr-Commit-Position: refs/heads/master@{#466568}

--- components/autofill/core/browser/webdata/autofill_table.h
+++ components/autofill/core/browser/webdata/autofill_table.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "components/sync/base/model_type.h"
 #include "components/sync/model/metadata_batch.h"
+#include "components/sync/model/sync_metadata_store.h"
 #include "components/webdata/common/web_database_table.h"
 
 class WebDatabase;
@@ -250,7 +251,8 @@ struct FormFieldData;
 //
 //   value              The serialized ModelTypeState record.
 
-class AutofillTable : public WebDatabaseTable {
+class AutofillTable : public WebDatabaseTable,
+                      public syncer::SyncMetadataStore {
  public:
   AutofillTable();
   ~AutofillTable() override;
@@ -427,22 +429,16 @@ class AutofillTable : public WebDatabaseTable {
   bool GetAllSyncMetadata(syncer::ModelType model_type,
                           syncer::MetadataBatch* metadata_batch);
 
-  // Update the metadata row for |model_type|, keyed by |storage_key|, to
-  // contain the contents of |metadata|.
+  // syncer::SyncMetadataStore implementation.
   bool UpdateSyncMetadata(syncer::ModelType model_type,
                           const std::string& storage_key,
-                          const sync_pb::EntityMetadata& metadata);
-
-  // Remove the metadata row of type |model_type| keyed by |storage|key|.
+                          const sync_pb::EntityMetadata& metadata) override;
   bool ClearSyncMetadata(syncer::ModelType model_type,
-                         const std::string& storage_key);
-
-  // Update the stored sync state for the |model_type|.
-  bool UpdateModelTypeState(syncer::ModelType model_type,
-                            const sync_pb::ModelTypeState& model_type_state);
-
-  // Clear the stored sync state for |model_type|.
-  bool ClearModelTypeState(syncer::ModelType model_type);
+                         const std::string& storage_key) override;
+  bool UpdateModelTypeState(
+      syncer::ModelType model_type,
+      const sync_pb::ModelTypeState& model_type_state) override;
+  bool ClearModelTypeState(syncer::ModelType model_type) override;
 
   // Table migration functions. NB: These do not and should not rely on other
   // functions in this class. The implementation of a function such as
