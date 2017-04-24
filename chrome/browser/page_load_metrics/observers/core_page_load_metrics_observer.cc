149d03203b8b84d6e59bc3d187ae293cb70c728e
mushan <mushan@chromium.org>
2017-04-24 16:08:55 -0700

    Log absolute foreground duration for page loads before and after paint in from gws observer and core observer.
    BUG=710683
    
    Review-Url: https://codereview.chromium.org/2829363002
    Cr-Commit-Position: refs/heads/master@{#466812}

--- chrome/browser/page_load_metrics/observers/core_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/core_page_load_metrics_observer.cc
@@ -165,6 +165,10 @@ const char kHistogramPageTimingForegroundDurationAfterPaint[] =
     "PageLoad.PageTiming.ForegroundDuration.AfterPaint";
 const char kHistogramPageTimingForegroundDurationNoCommit[] =
     "PageLoad.PageTiming.ForegroundDuration.NoCommit";
+const char kHistogramPageTimingForegroundDurationWithPaint[] =
+    "PageLoad.PageTiming.ForegroundDuration.WithPaint";
+const char kHistogramPageTimingForegroundDurationWithoutPaint[] =
+    "PageLoad.PageTiming.ForegroundDuration.WithoutPaint";
 
 const char kHistogramLoadTypeParseStartReload[] =
     "PageLoad.ParseTiming.NavigationToParseStart.LoadType.Reload";
@@ -742,6 +746,13 @@ void CorePageLoadMetricsObserver::RecordForegroundDurationHistograms(
           internal::kHistogramPageTimingForegroundDurationAfterPaint,
           foreground_duration.value() -
               timing.paint_timing.first_paint.value());
+      PAGE_LOAD_LONG_HISTOGRAM(
+          internal::kHistogramPageTimingForegroundDurationWithPaint,
+          foreground_duration.value());
+    } else {
+      PAGE_LOAD_LONG_HISTOGRAM(
+          internal::kHistogramPageTimingForegroundDurationWithoutPaint,
+          foreground_duration.value());
     }
   } else {
     PAGE_LOAD_LONG_HISTOGRAM(
