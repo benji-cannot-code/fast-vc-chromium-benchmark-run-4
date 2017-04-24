7bfda6ff670b624566482c282878c56c71c66e8a
ekaramad <ekaramad@chromium.org>
2017-04-24 12:26:31 -0700

    Fix a recent regression in Speech->Start Speaking on Mac
    After a recent refactoring, all RenderWidgetHostViews use TextInputManager in
    order to get the value of |selected_text_|. Unfortunately this was not applied
    to SpeakText() call inside RenderWidgetHostViewMac and furthermore, the local
    variable |selected_text_| was not removed from the view.
    
    BUG=713850
    
    Review-Url: https://codereview.chromium.org/2831183002
    Cr-Commit-Position: refs/heads/master@{#466715}

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -1107,7 +1107,7 @@ void RenderWidgetHostViewMac::SpeakSelection() {
     return;
   }
 
-  SpeakText(selected_text_);
+  SpeakText(base::UTF16ToUTF8(selection->selected_text()));
 }
 
 bool RenderWidgetHostViewMac::IsSpeaking() const {
