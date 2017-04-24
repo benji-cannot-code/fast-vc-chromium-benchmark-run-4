b7d4f7cadbff27b62a0686ddf5e7683f1eae20d8
tanvir.rizvi <tanvir.rizvi@samsung.com>
2017-04-24 02:22:48 -0700

    Move ComputeTextRects before Range::BoundingBox() Currently textQuads and ComputeTextRects algorithm is mostly same. The difference of behaviour of these two functions can be handled efficiently by using templates.
    This patch is the first step to achieve above behaviour.
    This patch removes the TODO comment and forward declaration of
    computeTextRects.
    
    BUG=691198
    
    Review-Url: https://codereview.chromium.org/2827603006
    Cr-Commit-Position: refs/heads/master@{#466607}

--- third_party/WebKit/Source/core/dom/Range.cpp
+++ third_party/WebKit/Source/core/dom/Range.cpp
@@ -1441,18 +1441,6 @@ Node* Range::PastLastNode() const {
   return EndPosition().NodeAsRangePastLastNode();
 }
 
-// TODO(hs1217.lee):: we should move this implement to VisibleUnits and then
-// this function will remove.
-static Vector<IntRect> computeTextRects(const EphemeralRange&);
-
-IntRect Range::BoundingBox() const {
-  IntRect result;
-  const Vector<IntRect>& rects = computeTextRects(EphemeralRange(this));
-  for (const IntRect& rect : rects)
-    result.Unite(rect);
-  return result;
-}
-
 static Vector<IntRect> computeTextRects(const EphemeralRange& range) {
   const Position& start_position = range.StartPosition();
   const Position& end_position = range.EndPosition();
@@ -1475,6 +1463,14 @@ static Vector<IntRect> computeTextRects(const EphemeralRange& range) {
     layout_text->AbsoluteRectsForRange(rects, start_offset, end_offset);
   }
   return rects;
+}
+
+IntRect Range::BoundingBox() const {
+  IntRect result;
+  const Vector<IntRect>& rects = computeTextRects(EphemeralRange(this));
+  for (const IntRect& rect : rects)
+    result.Unite(rect);
+  return result;
 }
 
 void Range::TextQuads(Vector<FloatQuad>& quads,
