e068e4f7e6d445b28fb569548ec76cc702f6af3e
jaydasika <jaydasika@chromium.org>
2017-04-24 13:34:01 -0700

    cc : Compute subtree has copy requests before property tree building
    To determine if we can skip creating effect nodes for hidden subtrees
    during property tree building, we need to know if the subtree has any
    copy requests. So, moving this computation to happen before property
    tree building. After this CL, we can stop creating effect nodes for
    hidden subtrees and also stop pushing these layers to compositor thread
    at commit.
    
    BUG=595843
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2822303003
    Cr-Commit-Position: refs/heads/master@{#466745}

--- cc/layers/layer_impl_test_properties.h
+++ cc/layers/layer_impl_test_properties.h
@@ -38,6 +38,7 @@ struct CC_EXPORT LayerImplTestProperties {
   bool should_flatten_transform;
   bool hide_layer_and_subtree;
   bool opacity_can_animate;
+  bool subtree_has_copy_request;
   int sorting_context_id;
   float opacity;
   FilterOperations filters;
--- cc/trees/layer_tree_host_unittest.cc
+++ cc/trees/layer_tree_host_unittest.cc
@@ -6526,7 +6526,7 @@ class LayerTreeHostTestUpdateCopyRequests : public LayerTreeHostTest {
   void WillCommit() override {
     switch (layer_tree_host()->SourceFrameNumber()) {
       case 1:
-        EXPECT_GT(root->num_copy_requests_in_target_subtree(), 0);
+        EXPECT_TRUE(root->has_copy_requests_in_target_subtree());
         break;
     }
   }
@@ -6549,7 +6549,7 @@ class LayerTreeHostTestUpdateCopyRequests : public LayerTreeHostTest {
         child->SetTransform(transform);
         break;
       case 3:
-        EXPECT_EQ(root->num_copy_requests_in_target_subtree(), 0);
+        EXPECT_FALSE(root->has_copy_requests_in_target_subtree());
         EndTest();
         break;
     }
