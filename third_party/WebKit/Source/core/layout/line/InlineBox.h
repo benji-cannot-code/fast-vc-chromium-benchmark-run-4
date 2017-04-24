c2856a95caede90f1eb3bfdc238efca75fc0ddd9
stanisc <stanisc@chromium.org>
2017-04-24 11:51:25 -0700

    Optimize sizeof(InlineBox) by eliminating padding
    Considering that on 64-bit Windows allocations are done with 16 byte
    granularity this reduces the per-instance memory usage of
    InlineBox by 16 bytes - from 80 bytes to 64 bytes.
    
    My estimate is that on a typical page where are 1K to 2K instances of
    InlineBox and derived classes. And on text heavy page like Reddit topic
    discussion I counted 6.7K instances. That means that the estimated memory
    saving is anywhere from 16 KB to 100 KB per Renderer processes.
    
    Class layout before the change:
    
    class blink::InlineBox [sizeof = 72]
     : public blink::DisplayItemClient {
      <padding> (4 bytes)
      [sizeof=4] blink::InlineBox::InlineBoxBitfields bitfields_
      <padding> (4 bytes)
      [sizeof=8] blink::InlineBox* next_
      [sizeof=8] blink::InlineBox* prev_
      [sizeof=8] blink::InlineFlowBox* parent_
      [sizeof=8] blink::LineLayoutItem line_layout_item_
      [sizeof=8] blink::LayoutPoint location_
      [sizeof=4] blink::LayoutUnit logical_width_
      <padding> (4 bytes)
    }
    
    class layout after the change:
    
    class blink::InlineBox [sizeof = 64]
      : public blink::DisplayItemClient {
      <padding> (4 bytes)
      [sizeof=8] blink::InlineBox* next_
      [sizeof=8] blink::InlineBox* prev_
      [sizeof=8] blink::InlineFlowBox* parent_
      [sizeof=8] blink::LineLayoutItem line_layout_item_
      [sizeof=8] blink::LayoutPoint location_
      [sizeof=4] blink::LayoutUnit logical_width_
      [sizeof=4] blink::InlineBox::InlineBoxBitfields bitfields_
    }
    
    BUG=710933
    
    Review-Url: https://codereview.chromium.org/2829683010
    Cr-Commit-Position: refs/heads/master@{#466699}

--- third_party/WebKit/Source/core/layout/line/InlineBox.h
+++ third_party/WebKit/Source/core/layout/line/InlineBox.h
@@ -64,13 +64,13 @@ class CORE_EXPORT InlineBox : public DisplayItemClient {
             InlineBox* next,
             InlineBox* prev,
             InlineFlowBox* parent)
-      : bitfields_(first_line, constructed, dirty, extracted, is_horizontal),
-        next_(next),
+      : next_(next),
         prev_(prev),
         parent_(parent),
         line_layout_item_(item),
         location_(top_left),
-        logical_width_(logical_width) {}
+        logical_width_(logical_width),
+        bitfields_(first_line, constructed, dirty, extracted, is_horizontal) {}
 
   virtual ~InlineBox();
 
@@ -473,8 +473,6 @@ class CORE_EXPORT InlineBox : public DisplayItemClient {
  private:
   void SetLineLayoutItemShouldDoFullPaintInvalidationIfNeeded();
 
-  InlineBoxBitfields bitfields_;
-
   InlineBox* next_;  // The next element on the same line as us.
   InlineBox* prev_;  // The previous element on the same line as us.
 
@@ -521,6 +519,8 @@ class CORE_EXPORT InlineBox : public DisplayItemClient {
   LayoutUnit logical_width_;
 
  private:
+  InlineBoxBitfields bitfields_;
+
 #if DCHECK_IS_ON()
   bool has_bad_parent_ = false;
 #endif
