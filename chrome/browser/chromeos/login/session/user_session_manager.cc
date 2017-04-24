8f2c57fb674002b74ba86e9c5d55162ade24902f
kinaba <kinaba@chromium.org>
2017-04-23 23:37:43 -0700

    arc: Show migration success notification on the next sign-in after migration.
    Referring the tri-state preference, shows the notification when needed.
    The special case is when the profile was newly created on the device.
    In that case, the profile is on the compatible filesystem from the beginning,
    we skip showing the migration and mark the pref as if it has been shown.
    
    BUG=711095
    TEST=Manually tried new/migrated/skipped-migration/guest/supervised accounts.
    
    Review-Url: https://codereview.chromium.org/2828213002
    Cr-Commit-Position: refs/heads/master@{#466592}

--- chrome/browser/chromeos/login/session/user_session_manager.cc
+++ chrome/browser/chromeos/login/session/user_session_manager.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process_platform_part_chromeos.h"
 #include "chrome/browser/browser_shutdown.h"
 #include "chrome/browser/chrome_notification_types.h"
+#include "chrome/browser/chromeos/arc/arc_migration_guide_notification.h"
 #include "chrome/browser/chromeos/arc/arc_service_launcher.h"
 #include "chrome/browser/chromeos/arc/arc_util.h"
 #include "chrome/browser/chromeos/base/locale_util.h"
@@ -1830,6 +1831,10 @@ void UserSessionManager::DoBrowserLaunchInternal(Profile* profile,
   // the message accordingly.
   if (ShouldShowEolNotification(profile))
     CheckEolStatus(profile);
+
+  // Show the one-time notification and update the relevant pref about the
+  // completion of the file system migration necessary for ARC, when needed.
+  arc::ShowArcMigrationSuccessNotificationIfNeeded(profile);
 }
 
 void UserSessionManager::RespectLocalePreferenceWrapper(
