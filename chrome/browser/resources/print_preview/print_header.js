7ee1137ca5ff50da4920759327d6b0a85500c706
thestig <thestig@chromium.org>
2017-04-24 16:18:45 -0700

    Print Preview: Assign to the header summary label only once.
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2835793002
    Cr-Commit-Position: refs/heads/master@{#466816}

--- chrome/browser/resources/print_preview/print_header.js
+++ chrome/browser/resources/print_preview/print_header.js
@@ -160,18 +160,11 @@ cr.define('print_preview', function() {
         return;
       }
 
-      var summaryLabel =
-          loadTimeData.getString('printPreviewSheetsLabelSingular');
-      var pagesLabel = loadTimeData.getString('printPreviewPageLabelPlural');
-
       var saveToPdfOrDrive = this.destinationStore_.selectedDestination &&
           (this.destinationStore_.selectedDestination.id ==
               print_preview.Destination.GooglePromotedId.SAVE_AS_PDF ||
            this.destinationStore_.selectedDestination.id ==
               print_preview.Destination.GooglePromotedId.DOCS);
-      if (saveToPdfOrDrive) {
-        summaryLabel = loadTimeData.getString('printPreviewPageLabelSingular');
-      }
 
       var numPages = this.printTicketStore_.pageRange.getPageNumberSet().size;
       var numSheets = numPages;
@@ -183,9 +176,15 @@ cr.define('print_preview', function() {
       numSheets *= copies;
       numPages *= copies;
 
+      var pagesLabel = loadTimeData.getString('printPreviewPageLabelPlural');
+      var summaryLabel;
       if (numSheets > 1) {
         summaryLabel = saveToPdfOrDrive ? pagesLabel :
             loadTimeData.getString('printPreviewSheetsLabelPlural');
+      } else {
+        summaryLabel = loadTimeData.getString(
+            saveToPdfOrDrive ? 'printPreviewPageLabelSingular' :
+                               'printPreviewSheetsLabelSingular');
       }
 
       var html;
