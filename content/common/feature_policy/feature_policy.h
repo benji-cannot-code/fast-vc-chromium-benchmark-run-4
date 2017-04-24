6e79d8989c20f2e04260fc856208e0fe8005b7a2
Ian Clelland <iclelland@google.com>
2017-04-24 13:25:03 -0400

    Correctly reset FP in RFHI whenever origin changes
    Bug: 713364
    Change-Id: Id8bb923750e20f3db6fc9358b1d44120513ac95f
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Change-Id: Id8bb923750e20f3db6fc9358b1d44120513ac95f
    Reviewed-on: https://chromium-review.googlesource.com/482380
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Charles Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#466778}

--- content/common/feature_policy/feature_policy.h
+++ content/common/feature_policy/feature_policy.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "content/common/content_export.h"
 #include "third_party/WebKit/public/platform/WebFeaturePolicy.h"
@@ -178,6 +179,8 @@ class CONTENT_EXPORT FeaturePolicy : public blink::WebFeaturePolicy {
 
  private:
   friend class FeaturePolicyTest;
+  FRIEND_TEST_ALL_PREFIXES(NavigatorTestWithBrowserSideNavigation,
+                           FeaturePolicyNewChild);
 
   explicit FeaturePolicy(url::Origin origin);
   FeaturePolicy(url::Origin origin, const FeatureList& feature_list);
