3e24ae70226e5f6801d02597e366ee6c4652e80a
kylixrd <kylixrd@chromium.org>
2017-04-24 10:57:35 -0700

    Harmonizing SmartLock dialog
    Including more Smartlock & password dialog tests.
    
    BUG=651682
    TBR=grt@chromium.org
    
    Review-Url: https://codereview.chromium.org/2832913002
    Cr-Commit-Position: refs/heads/master@{#466680}

--- chrome/browser/ui/views/passwords/credentials_item_view.cc
+++ chrome/browser/ui/views/passwords/credentials_item_view.cc
@@ -8,11 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/ui/passwords/manage_passwords_view_utils.h"
+#include "chrome/browser/ui/views/harmony/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/harmony/chrome_typography.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/theme_resources.h"
 #include "components/autofill/core/common/password_form.h"
-#include "ui/base/material_design/material_design_controller.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/path.h"
@@ -23,8 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/layout_constants.h"
 
 namespace {
-// The default spacing between the icon and text.
-const int kSpacing = 12;
 
 gfx::Size GetTextLabelsSize(const views::Label* upper_label,
                             const views::Label* lower_label) {
@@ -95,10 +93,9 @@ CredentialsItemView::CredentialsItemView(
 
   // TODO(tapted): Check these (and the STYLE_ values below) against the spec on
   // http://crbug.com/651681.
-  const int kLabelContext =
-      ui::MaterialDesignController::IsSecondaryUiMaterial()
-          ? CONTEXT_BODY_TEXT_SMALL
-          : CONTEXT_DEPRECATED_SMALL;
+  const int kLabelContext = ChromeLayoutProvider::Get()->IsHarmonyMode()
+                                ? CONTEXT_BODY_TEXT_SMALL
+                                : CONTEXT_DEPRECATED_SMALL;
 
   if (!upper_text.empty()) {
     upper_label_ = new views::Label(upper_text, kLabelContext,
@@ -153,7 +150,9 @@ gfx::Size CredentialsItemView::GetPreferredSize() const {
                              std::max(kAvatarImageSize, labels_size.height()));
   const gfx::Insets insets(GetInsets());
   size.Enlarge(insets.width(), insets.height());
-  size.Enlarge(kSpacing, 0);
+  size.Enlarge(ChromeLayoutProvider::Get()->GetDistanceMetric(
+                   DISTANCE_UNRELATED_CONTROL_HORIZONTAL),
+               0);
 
   // Make the size at least as large as the minimum size needed by the border.
   size.SetToMax(border() ? border()->GetMinimumSize() : gfx::Size());
@@ -180,7 +179,9 @@ void CredentialsItemView::Layout() {
       lower_label_ ? lower_label_->GetPreferredSize() : gfx::Size();
   int y_offset = (child_area.height() -
       (upper_size.height() + lower_size.height())) / 2;
-  gfx::Point label_origin(image_origin.x() + image_size.width() + kSpacing,
+  gfx::Point label_origin(image_origin.x() + image_size.width() +
+                              ChromeLayoutProvider::Get()->GetDistanceMetric(
+                                  DISTANCE_UNRELATED_CONTROL_HORIZONTAL),
                           child_area.origin().y() + y_offset);
   if (upper_label_)
     upper_label_->SetBoundsRect(gfx::Rect(label_origin, upper_size));
--- chrome/browser/ui/views/passwords/manage_passwords_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/passwords/manage_passwords_bubble_view_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/macros.h"
+#include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/ui/passwords/manage_passwords_test.h"
 #include "chrome/browser/ui/test/test_browser_dialog.h"
 #include "ui/base/ui_base_switches.h"
@@ -25,6 +26,16 @@ class ManagePasswordsBubbleDialogViewTest
     } else if (name == "ManagePasswordBubble") {
       SetupManagingPasswords();
       ExecuteManagePasswordsCommand();
+    } else if (name == "AutoSignin") {
+      test_form()->origin = GURL("https://example.com");
+      test_form()->display_name = base::ASCIIToUTF16("Peter");
+      test_form()->username_value = base::ASCIIToUTF16("pet12@gmail.com");
+      std::vector<std::unique_ptr<autofill::PasswordForm>> local_credentials;
+      local_credentials.push_back(
+          base::MakeUnique<autofill::PasswordForm>(*test_form()));
+
+      ManagePasswordsBubbleView::set_auto_signin_toast_timeout(10);
+      SetupAutoSignin(std::move(local_credentials));
     } else {
       ADD_FAILURE() << "Unknown dialog type";
       return;
@@ -54,5 +65,10 @@ IN_PROC_BROWSER_TEST_F(ManagePasswordsBubbleDialogViewTest,
 
 IN_PROC_BROWSER_TEST_F(ManagePasswordsBubbleDialogViewTest,
                        InvokeDialog_ManagePasswordBubble) {
+  RunDialog();
+}
+
+IN_PROC_BROWSER_TEST_F(ManagePasswordsBubbleDialogViewTest,
+                       InvokeDialog_AutoSignin) {
   RunDialog();
 }
