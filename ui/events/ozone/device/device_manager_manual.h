033f05a57c83cdc97c9f8fe70fd95481df66814c
bcf <bcf@chromium.org>
2017-04-24 11:23:47 -0700

    Use FilePathWatcher for DeviceManagerManual
    Previously DeviceManagerManual would only refresh devices when
    ScanDevices was called.
    
    BUG=b/37525463
    TEST=Manual
    
    Review-Url: https://codereview.chromium.org/2832253003
    Cr-Commit-Position: refs/heads/master@{#466692}

--- ui/events/ozone/device/device_manager_manual.h
+++ ui/events/ozone/device/device_manager_manual.h
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_EVENTS_OZONE_DEVICE_DEVICE_MANAGER_MANUAL_H_
 #define UI_EVENTS_OZONE_DEVICE_DEVICE_MANAGER_MANUAL_H_
 
+#include <set>
 #include <vector>
 
+#include "base/files/file_path_watcher.h"
 #include "base/macros.h"
 #include "base/observer_list.h"
 #include "ui/events/ozone/device/device_manager.h"
@@ -29,11 +31,15 @@ class DeviceManagerManual : public DeviceManager {
   void AddObserver(DeviceEventObserver* observer) override;
   void RemoveObserver(DeviceEventObserver* observer) override;
 
+  void StartWatching();
+  void InitiateScanDevices();
   void OnDevicesScanned(std::vector<base::FilePath>* result);
+  void OnWatcherEvent(const base::FilePath& path, bool error);
 
-  bool have_scanned_devices_;
-  std::vector<base::FilePath> devices_;
+  std::set<base::FilePath> devices_;
+  base::FilePathWatcher watcher_;
   base::ObserverList<DeviceEventObserver> observers_;
+  bool is_watching_ = false;
 
   base::WeakPtrFactory<DeviceManagerManual> weak_ptr_factory_;
 
