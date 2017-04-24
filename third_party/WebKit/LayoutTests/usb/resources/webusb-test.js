90c08d7872185f24b25f4e69ffb11ba85ed09361
reillyg <reillyg@chromium.org>
2017-04-24 14:35:01 -0700

    Move the onclose event handler to the FakeUSBDevice object
    This updates the WebUSB Testing API polyfill to put the onclose event
    handler on the FakeUSBDevice object which makes tests a little simpler.
    
    BUG=705734
    
    Review-Url: https://codereview.chromium.org/2831223003
    Cr-Commit-Position: refs/heads/master@{#466774}

--- third_party/WebKit/LayoutTests/usb/resources/webusb-test.js
+++ third_party/WebKit/LayoutTests/usb/resources/webusb-test.js
@@ -15,8 +15,6 @@ let mojo = null;
 let g_initializePromise = null;
 let g_chooserService = null;
 let g_deviceManager = null;
-let g_closeListener = null;
-let g_nextGuid = 0;
 
 function fakeDeviceInitToDeviceInfo(guid, init) {
   let deviceInfo = {
@@ -350,8 +348,8 @@ class FakeDeviceManager {
       let binding = new mojo.bindings.Binding(
           mojo.device.UsbDevice, new FakeDevice(device.info), request);
       binding.setConnectionErrorHandler(() => {
-        if (g_closeListener)
-          g_closeListener(device.fakeDevice);
+        if (device.fakeDevice.onclose)
+          device.fakeDevice.onclose();
       });
       device.bindingArray.push(binding);
     } else {
@@ -395,6 +393,10 @@ class FakeChooserService {
 
 // Unlike FakeDevice this class is exported to callers of USBTest.addFakeDevice.
 class FakeUSBDevice {
+  constructor() {
+    this.onclose = null;
+  }
+
   disconnect() {
     setTimeout(() => g_deviceManager.removeDevice(this), 0);
   }
@@ -486,10 +488,6 @@ class USBTest {
     return fakeDevice;
   }
 
-  set ondeviceclose(func) {
-    g_closeListener = func;
-  }
-
   set chosenDevice(fakeDevice) {
     if (!g_chooserService)
       throw new Error('Call initialize() before setting chosenDevice.');
@@ -510,7 +508,6 @@ class USBTest {
 
     g_deviceManager.removeAllDevices();
     g_chooserService.setChosenDevice(null);
-    g_closeListener = null;
   }
 }
 
