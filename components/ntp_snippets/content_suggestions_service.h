de4f896534afe01cb0c4ff62c8c21deda5d9f8f8
jkrcal <jkrcal@chromium.org>
2017-04-24 09:34:02 -0700

    [Remote suggestions] Get favicon URLs from archive
    Fetching favicons via content suggestions service is currently only
    enabled for remote suggestions. The general API for getting the URL is
    via content_suggestion.h which does not work for archived suggestions
    (i.e. by Fetch More).
    
    This CL introduces a quick fix to loop in remote suggestions provider
    for the favicon URLs of suggestions not known to
    ContentSuggestionsService. A cleaner fix requires more thinking /
    refactoring.
    
    This is a M59 merge candidate.
    
    BUG=714025
    
    Review-Url: https://codereview.chromium.org/2829963005
    Cr-Commit-Position: refs/heads/master@{#466653}

--- components/ntp_snippets/content_suggestions_service.h
+++ components/ntp_snippets/content_suggestions_service.h
@@ -329,6 +329,8 @@ class ContentSuggestionsService : public KeyedService,
   void RestoreDismissedCategoriesFromPrefs();
   void StoreDismissedCategoriesToPrefs();
 
+  // Get the domain of the suggestion suitable for fetching the favicon.
+  GURL GetFaviconDomain(const ContentSuggestion::ID& suggestion_id);
   // Callbacks for fetching favicons.
   void OnGetFaviconFromCacheFinished(
       const GURL& publisher_url,
