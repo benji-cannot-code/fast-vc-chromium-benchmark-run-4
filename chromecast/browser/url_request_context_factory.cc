b655d7ccdd6c56c704f90706968c11e7a798d0ad
fdoray <fdoray@chromium.org>
2017-04-24 13:42:22 -0700

    Use TaskScheduler instead of blocking pool in url_request_context_factory.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=slan@chromium.org
    
    Review-Url: https://codereview.chromium.org/2837123002
    Cr-Commit-Position: refs/heads/master@{#466751}

--- chromecast/browser/url_request_context_factory.cc
+++ chromecast/browser/url_request_context_factory.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
-#include "base/threading/sequenced_worker_pool.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/worker_pool.h"
 #include "chromecast/base/chromecast_switches.h"
 #include "chromecast/browser/cast_browser_process.h"
@@ -289,9 +289,12 @@ void URLRequestContextFactory::InitializeMainContextDependencies(
     set_protocol = job_factory->SetProtocolHandler(
         url::kFileScheme,
         base::MakeUnique<net::FileProtocolHandler>(
-            content::BrowserThread::GetBlockingPool()
-                ->GetTaskRunnerWithShutdownBehavior(
-                    base::SequencedWorkerPool::SKIP_ON_SHUTDOWN)));
+            base::CreateTaskRunnerWithTraits(
+                base::TaskTraits()
+                    .MayBlock()
+                    .WithPriority(base::TaskPriority::BACKGROUND)
+                    .WithShutdownBehavior(
+                        base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN))));
     DCHECK(set_protocol);
   }
 
