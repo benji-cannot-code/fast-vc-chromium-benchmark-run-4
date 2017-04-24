0a9a8e7cc82b1cf454e1b2c9f84bbee20b321cc5
pkalinnikov <pkalinnikov@chromium.org>
2017-04-24 10:16:07 -0700

    Revert of Add U2F request state machines (patchset #5 id:180001 of https://codereview.chromium.org/2821263005/ )
    Reason for revert:
    Reverting because U2fRequestTest.TestBasicMachine fails consistently on MSan bots (Linux MSan, Linux ChromiumOS MSan).
    
    Original issue's description:
    > Add U2F request state machines
    >
    > U2fRequest base class is extended by each type of U2F
    > request. Common functions of enumeration and device iteration
    > are defined in the base class. U2fSign performs an entire
    > sign request on any attached U2F devices, and U2FRegister
    > performs the registration request on the devices.
    >
    > BUG=686310
    >
    > Review-Url: https://codereview.chromium.org/2821263005
    > Cr-Commit-Position: refs/heads/master@{#466529}
    > Committed: https://chromium.googlesource.com/chromium/src/+/892eb34117f811aa62c821c9129dc53110266e9a
    
    TBR=reillyg@chromium.org,kpaulhamus@chromium.org,juanlang@chromium.org,piperc@google.com,piperc@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=686310
    
    Review-Url: https://codereview.chromium.org/2838573002
    Cr-Commit-Position: refs/heads/master@{#466666}

--- device/u2f/u2f_device.h
+++ device/u2f/u2f_device.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "u2f_apdu_response.h"
-#include "u2f_return_code.h"
 
 namespace device {
 
@@ -25,9 +24,15 @@ class U2fDevice {
     U2F_V2,
     UNKNOWN,
   };
+  enum class ReturnCode : uint8_t {
+    SUCCESS,
+    FAILURE,
+    INVALID_PARAMS,
+    CONDITIONS_NOT_SATISFIED,
+  };
 
   using MessageCallback =
-      base::Callback<void(U2fReturnCode, std::vector<uint8_t>)>;
+      base::Callback<void(ReturnCode, std::vector<uint8_t>)>;
   using VersionCallback =
       base::Callback<void(bool success, ProtocolVersion version)>;
   using DeviceCallback =
