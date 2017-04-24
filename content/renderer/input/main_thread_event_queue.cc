df63edf80c1b8965ba630e8e16d067d3e6f4376e
dtapuska <dtapuska@chromium.org>
2017-04-24 09:24:18 -0700

    Get rid of the in_flight_event.
    It was possible that a nested message loop occurs in the processing
    event actually gets destroyed by running an event inside the nested loop.
    
    Put the object on the stack instead since we no longer have the virtual
    callback that we required this member for. Also this might actually be
    the cause to further hung renderers because under the old code the missing
    acks would silently get dropped. After https://codereview.chromium.org/2813683002
    this caused a null-deref.
    
    BUG=714444
    
    Review-Url: https://codereview.chromium.org/2835193002
    Cr-Commit-Position: refs/heads/master@{#466645}

--- content/renderer/input/main_thread_event_queue.cc
+++ content/renderer/input/main_thread_event_queue.cc
@@ -345,13 +345,6 @@ void MainThreadEventQueue::QueueClosure(const base::Closure& closure) {
     PostTaskToMainThread();
 }
 
-void MainThreadEventQueue::DispatchInFlightEvent() {
-  if (in_flight_event_) {
-    in_flight_event_->Dispatch(this);
-    in_flight_event_.reset();
-  }
-}
-
 void MainThreadEventQueue::PossiblyScheduleMainFrame() {
   if (IsRafAlignedInputDisabled())
     return;
@@ -387,15 +380,16 @@ void MainThreadEventQueue::DispatchEvents() {
   }
 
   while (events_to_process--) {
+    std::unique_ptr<MainThreadEventQueueTask> task;
     {
       base::AutoLock lock(shared_state_lock_);
       if (shared_state_.events_.empty())
         return;
-      in_flight_event_ = shared_state_.events_.Pop();
+      task = shared_state_.events_.Pop();
     }
 
     // Dispatching the event is outside of critical section.
-    DispatchInFlightEvent();
+    task->Dispatch(this);
   }
   PossiblyScheduleMainFrame();
 }
@@ -428,6 +422,7 @@ void MainThreadEventQueue::DispatchRafAlignedInput(base::TimeTicks frame_time) {
   }
 
   while (queue_size_at_start--) {
+    std::unique_ptr<MainThreadEventQueueTask> task;
     {
       base::AutoLock lock(shared_state_lock_);
 
@@ -446,11 +441,10 @@ void MainThreadEventQueue::DispatchRafAlignedInput(base::TimeTicks frame_time) {
           shared_state_.last_async_touch_move_timestamp_ = frame_time;
         }
       }
-      in_flight_event_ = shared_state_.events_.Pop();
+      task = shared_state_.events_.Pop();
     }
-
     // Dispatching the event is outside of critical section.
-    DispatchInFlightEvent();
+    task->Dispatch(this);
   }
 
   PossiblyScheduleMainFrame();
