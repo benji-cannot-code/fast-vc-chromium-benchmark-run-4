3f9a0173d550e815e197d6daa18a4d30a58a9bcb
aluo <aluo@chromium.org>
2017-04-24 16:48:21 -0700

    Wait longer for actionmode menu in webview actionmode tests
    BUG=711424
    
    Review-Url: https://codereview.chromium.org/2819563003
    Cr-Commit-Position: refs/heads/master@{#466827}

--- android_webview/tools/automated_ui_tests/javatests/src/org/chromium/webview_ui_test/test/ActionModeTest.java
+++ android_webview/tools/automated_ui_tests/javatests/src/org/chromium/webview_ui_test/test/ActionModeTest.java
@@ -294,6 +294,7 @@ public class ActionModeTest {
     private class ActionBarIdlingResource implements IdlingResource {
         private boolean mActionStarting;
         private ResourceCallback mResourceCallback;
+        private boolean mPreviousActionBarDisplayed;
 
         @Override
         public String getName() {
@@ -303,10 +304,17 @@ public class ActionModeTest {
         @Override
         public boolean isIdleNow() {
             if (!mActionStarting) return true;
-            if (mWebViewActivityRule.isActionBarDisplayed()) {
+            boolean currentActionBarDisplayed = mWebViewActivityRule.isActionBarDisplayed();
+            /* Only transition to idle when action bar is displayed fully for
+             * 2 consecutive checks.  This avoids false transitions
+             * in cases where the action bar was already displayed but is due
+             * to be updated immediately after a previous action
+             */
+            if (mPreviousActionBarDisplayed && currentActionBarDisplayed) {
                 mActionStarting = false;
                 if (mResourceCallback != null) mResourceCallback.onTransitionToIdle();
             }
+            mPreviousActionBarDisplayed = currentActionBarDisplayed;
             return !mActionStarting;
         }
 
@@ -317,6 +325,7 @@ public class ActionModeTest {
 
         public void start() {
             mActionStarting = true;
+            mPreviousActionBarDisplayed = false;
         }
     }
 }
