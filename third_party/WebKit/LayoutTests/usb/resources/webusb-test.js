4308075c0b3015a6a1aa33d6223ba4cbf406c226
reillyg <reillyg@chromium.org>
2017-04-24 12:25:45 -0700

    Add link to WebUSB Test API specification
    Now that it is published online instead of including the WebIDL
    definitions for the WebUSB Test API inline in the polyfill this patch
    replaces them with a link to the editor's draft.
    
    BUG=705734
    
    Review-Url: https://codereview.chromium.org/2829643003
    Cr-Commit-Position: refs/heads/master@{#466714}

--- third_party/WebKit/LayoutTests/usb/resources/webusb-test.js
+++ third_party/WebKit/LayoutTests/usb/resources/webusb-test.js
@@ -1,72 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 'use strict';
 
-// This polyfil library implements the following WebIDL:
-//
-// partial interface USB {
-//   [SameObject] readonly attribute USBTest test;
-// }
-//
-// interface USBTest {
-//   attribute EventHandler ondeviceclose;
-//   attribute FakeUSBDevice? chosenDevice;
-//   attribute FrozenArray<USBDeviceFilter>? lastFilters;
-//
-//   Promise<void> initialize();
-//   Promise<void> attachToWindow(Window window);
-//   FakeUSBDevice addFakeDevice(FakeUSBDeviceInit deviceInit);
-//   void reset();
-// };
-//
-// interface FakeUSBDevice {
-//   void disconnect();
-// };
-//
-// dictionary FakeUSBDeviceInit {
-//   octet usbVersionMajor;
-//   octet usbVersionMinor;
-//   octet usbVersionSubminor;
-//   octet deviceClass;
-//   octet deviceSubclass;
-//   octet deviceProtocol;
-//   unsigned short vendorId;
-//   unsigned short productId;
-//   octet deviceVersionMajor;
-//   octet deviceVersionMinor;
-//   octet deviceVersionSubminor;
-//   DOMString? manufacturerName;
-//   DOMString? productName;
-//   DOMString? serialNumber;
-//   octet activeConfigurationValue = 0;
-//   sequence<FakeUSBConfigurationInit> configurations;
-// };
-//
-// dictionary FakeUSBConfigurationInit {
-//   octet configurationValue;
-//   DOMString? configurationName;
-//   sequence<FakeUSBInterfaceInit> interfaces;
-// };
-//
-// dictionary FakeUSBInterfaceInit {
-//   octet interfaceNumber;
-//   sequence<FakeUSBAlternateInterfaceInit> alternates;
-// };
-//
-// dictionary FakeUSBAlternateInterfaceInit {
-//   octet alternateSetting;
-//   octet interfaceClass;
-//   octet interfaceSubclass;
-//   octet interfaceProtocol;
-//   DOMString? interfaceName;
-//   sequence<FakeUSBEndpointInit> endpoints;
-// };
-//
-// dictionary FakeUSBEndpointInit {
-//   octet endpointNumber;
-//   USBDirection direction;
-//   USBEndpointType type;
-//   unsigned long packetSize;
-// };
+// This polyfill library implements the WebUSB Test API as specified here:
+// https://wicg.github.io/webusb/test/
 
 (() => {
 
