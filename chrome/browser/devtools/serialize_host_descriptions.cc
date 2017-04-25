dba33cb564aec0411df266c261b329901df96c32
vabr <vabr@chromium.org>
2017-04-25 00:17:03 -0700

    SerializeHostDescriptions ignores hosts with duplicated names
    |HostDescriptionNode.name| identifies the node and should not be shared by two
    different nodes. If multiple nodes are passed to SerializeHostDescriptions with
    the same name, currently SerializeHostDescriptions ends up using
    DictionaryValue after performing std::move on it, because of mixing nodes with
    the same name.
    
    This CL replaces some internal use of std::vector with std::unordered_set to
    discard duplicities.
    
    The CL also improves the unit test to be less dependent on the resulting order
    of the serialized descriptions.
    
    The CL also fixes the test name to match its filename and the name of the tested
    function.
    
    BUG=714368
    
    Review-Url: https://codereview.chromium.org/2835823002
    Cr-Commit-Position: refs/heads/master@{#466913}

--- chrome/browser/devtools/serialize_host_descriptions.cc
+++ chrome/browser/devtools/serialize_host_descriptions.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/devtools/serialize_host_descriptions.h"
 
 #include <map>
+#include <unordered_set>
 #include <utility>
 
 #include "base/memory/ptr_util.h"
@@ -41,14 +42,14 @@ base::DictionaryValue Serialize(
 // Takes a vector of host description and converts it into:
 // |children|: a map from a host's representation to representations of its
 //             children,
-// |roots|: a vector of representations of hosts with no parents, and
+// |roots|: a set of representations of hosts with no parents, and
 // |representations|: a vector actually storing all those representations to
 //                    which the rest just points.
 void CreateDictionaryForest(
     std::vector<HostDescriptionNode> hosts,
     std::map<base::DictionaryValue*, std::vector<base::DictionaryValue*>>*
         children,
-    std::vector<base::DictionaryValue*>* roots,
+    std::unordered_set<base::DictionaryValue*>* roots,
     std::vector<base::DictionaryValue>* representations) {
   representations->reserve(hosts.size());
   children->clear();
@@ -60,7 +61,10 @@ void CreateDictionaryForest(
   // First move the representations and map the names to them.
   for (HostDescriptionNode& node : hosts) {
     representations->push_back(std::move(node.representation));
-    name_to_representation[node.name] = &representations->back();
+    // If there are multiple nodes with the same name, subsequent insertions
+    // will be ignored, so only the first node with a given name will be
+    // referenced by |roots| and |children|.
+    name_to_representation.emplace(node.name, &representations->back());
   }
 
   // Now compute children.
@@ -68,12 +72,12 @@ void CreateDictionaryForest(
     base::DictionaryValue* node_rep = name_to_representation[node.name];
     base::StringPiece parent_name = node.parent_name;
     if (parent_name.empty()) {
-      roots->push_back(node_rep);
+      roots->insert(node_rep);
       continue;
     }
     auto node_it = name_to_representation.find(parent_name);
     if (node_it == name_to_representation.end()) {
-      roots->push_back(node_rep);
+      roots->insert(node_rep);
       continue;
     }
     (*children)[name_to_representation[parent_name]].push_back(node_rep);
@@ -90,7 +94,7 @@ base::ListValue SerializeHostDescriptions(
   std::vector<base::DictionaryValue> representations;
   std::map<base::DictionaryValue*, std::vector<base::DictionaryValue*>>
       children;
-  std::vector<base::DictionaryValue*> roots;
+  std::unordered_set<base::DictionaryValue*> roots;
 
   CreateDictionaryForest(std::move(hosts), &children, &roots, &representations);
 
