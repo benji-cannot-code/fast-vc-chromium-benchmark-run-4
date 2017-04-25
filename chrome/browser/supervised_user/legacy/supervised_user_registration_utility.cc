5952c10099afa5f2ce26e55973f3cd2fb727c720
fdoray <fdoray@chromium.org>
2017-04-25 07:21:58 -0700

    Use TaskScheduler instead of blocking pool in supervised_user_registration_utility.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=bauerb@chromium.org
    
    Review-Url: https://codereview.chromium.org/2841543004
    Cr-Commit-Position: refs/heads/master@{#466981}

--- chrome/browser/supervised_user/legacy/supervised_user_registration_utility.cc
+++ chrome/browser/supervised_user/legacy/supervised_user_registration_utility.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/rand_util.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/threading/sequenced_worker_pool.h"
+#include "base/task_scheduler/post_task.h"
 #include "build/build_config.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/chrome_signin_client_factory.h"
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/browser/signin_client.h"
 #include "components/signin/core/browser/signin_manager.h"
 #include "components/sync/base/get_session_name.h"
-#include "content/public/browser/browser_thread.h"
 #include "google_apis/gaia/gaia_urls.h"
 #include "google_apis/gaia/google_service_auth_error.h"
 
@@ -304,9 +303,13 @@ void SupervisedUserRegistrationUtilityImpl::Register(
   }
 
   syncer::GetSessionName(
-      content::BrowserThread::GetBlockingPool()
-          ->GetTaskRunnerWithShutdownBehavior(
-              base::SequencedWorkerPool::CONTINUE_ON_SHUTDOWN).get(),
+      base::CreateTaskRunnerWithTraits(
+          base::TaskTraits()
+              .MayBlock()
+              .WithPriority(base::TaskPriority::BACKGROUND)
+              .WithShutdownBehavior(
+                  base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN))
+          .get(),
       base::Bind(&SupervisedUserRegistrationUtilityImpl::FetchToken,
                  weak_ptr_factory_.GetWeakPtr()));
 }
