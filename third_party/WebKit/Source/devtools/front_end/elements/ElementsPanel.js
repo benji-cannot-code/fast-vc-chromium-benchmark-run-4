bb8c6bcfffb57bbf7d99cf7c5b9afd2b98c33954
dgozman <dgozman@chromium.org>
2017-04-25 14:37:31 -0700

    [DevTools] Move dom breakpoints to DOMDebuggerModel
    DOMBreakpointsSidebarPane just renders dom breakpoints now. Removed
    pane's proxy and moved context menu into it.
    
    The pane should not be referenced outside of components anymore.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2837203002
    Cr-Commit-Position: refs/heads/master@{#467121}

--- third_party/WebKit/Source/devtools/front_end/elements/ElementsPanel.js
+++ third_party/WebKit/Source/devtools/front_end/elements/ElementsPanel.js
@@ -418,7 +418,6 @@ Elements.ElementsPanel = class extends UI.Panel {
     }
 
     this._hasNonDefaultSelectedNode = false;
-    Components.domBreakpointsSidebarPane.restoreBreakpoints(inspectedRootDocument);
 
     if (this._omitDefaultSelection)
       return;
@@ -965,13 +964,6 @@ Elements.ElementsPanel.ContextMenuProvider = class {
     if (!(object instanceof SDK.RemoteObject && (/** @type {!SDK.RemoteObject} */ (object)).isNode()) &&
         !(object instanceof SDK.DOMNode) && !(object instanceof SDK.DeferredDOMNode))
       return;
-
-
-    // Add debbuging-related actions
-    if (object instanceof SDK.DOMNode) {
-      contextMenu.appendSeparator();
-      Components.domBreakpointsSidebarPane.populateNodeContextMenu(object, contextMenu, true);
-    }
 
     // Skip adding "Reveal..." menu item for our own tree outline.
     if (Elements.ElementsPanel.instance().element.isAncestor(/** @type {!Node} */ (event.target)))
