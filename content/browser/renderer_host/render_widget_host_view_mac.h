85859b88add5b2d6e8d243e0e73c43434a37e929
ekaramad <ekaramad@chromium.org>
2017-04-25 15:45:38 -0700

    Making Mac Dictionary better for OOPIFs and <webview>
    There is two ways to invoke Mac dictionary for Chromium: choosing look-up word
    in context menu for highlighted text, or pressing Ctrl + Command + D. Both methods
    might involve sending an IPC through TextInputClientMac to the RenderWidget
    corresponding to the string (i.e., focused RenderWidget or the RenderWidget at
    the position of mouse cursor). In both cases the IPC comes back with a string
    and a position with respect to the local roots view port coordinates. The result
    however is handled on IO thread which leads to complications such as a DCHECK
    firing (at times) due to accessing the RenderWidgetHostViews weak pointer when
    getting the focused RenderWidgetHost.
    
    Furthermore, word lookup through context menu is broken for <webview>s; both
    OOPIF-based and BrowserPlugin-based. The former type of <webview>s don't
    support this feature because RenderWidgetHostViewChildFrame does not implement
    ShowDefinitionForSelection() method. The latter have regressed after implementing
    Mac dictionary support for OOPIFs () where the IPC for getting the string from
    range is only sent to a RenderWidgetHost which is part of the frame tree (which
    will not be that of a guest represented by a BrowserPlugin).
    
    This CL will:
    
    1) Handle dictionary related IPCs on the UI thread to remove the threading issue
    and make transforming the reported points to root coordinates based on compositor
    frames and the right OOPIF way (calling RWHV::TransformPointToRooTCoordSpace which
    can only be done on the UI thread).
    
    2) Implement RenderWidgetHostViewChildFrame::ShowDefinitionForSelection().
    3) Make sure that for BrowserPlugin-based guest the IPC is sent to the
    RenderWidget of the guest renderer.
    
    The CL also adds a test to avoid regressions in word lookup inside guests.
    
    TBR=shuchen@chromium.org
    BUG=643233, 702348, 708183
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2817673003
    Cr-Commit-Position: refs/heads/master@{#467149}

--- content/browser/renderer_host/render_widget_host_view_mac.h
+++ content/browser/renderer_host/render_widget_host_view_mac.h
@@ -485,6 +485,9 @@ class CONTENT_EXPORT RenderWidgetHostViewMac
   // platform view for a guest).
   const TextInputManager::TextSelection* GetTextSelection();
 
+  // Get the focused view that should be used for retrieving the text selection.
+  RenderWidgetHostViewBase* GetFocusedViewForTextSelection();
+
  private:
   friend class RenderWidgetHostViewMacTest;
 
@@ -503,9 +506,6 @@ class CONTENT_EXPORT RenderWidgetHostViewMac
 
   // Dispatches a TTS session.
   void SpeakText(const std::string& text);
-
-  // Get the focused view that should be used for retrieving the text selection.
-  RenderWidgetHostViewBase* GetFocusedViewForTextSelection();
 
   // Adds/Removes frame observer based on state.
   void UpdateNeedsBeginFramesInternal();
