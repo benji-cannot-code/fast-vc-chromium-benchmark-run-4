9ea5ce178cb7f4137bd3f47a8de47cf32c03f2ee
fdoray <fdoray@chromium.org>
2017-04-25 07:26:00 -0700

    Use TaskScheduler instead of blocking pool in profile_io_data.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=bauerb@chromium.org
    
    Review-Url: https://codereview.chromium.org/2837933002
    Cr-Commit-Position: refs/heads/master@{#466984}

--- chrome/browser/profiles/profile_io_data.cc
+++ chrome/browser/profiles/profile_io_data.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/sequenced_worker_pool.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
@@ -231,9 +232,12 @@ class DebugDevToolsInterceptor : public net::URLRequestInterceptor {
     if (IsSupportedDevToolsURL(request->url(), &path))
       return new net::URLRequestFileJob(
           request, network_delegate, path,
-          content::BrowserThread::GetBlockingPool()->
-              GetTaskRunnerWithShutdownBehavior(
-                  base::SequencedWorkerPool::SKIP_ON_SHUTDOWN));
+          base::CreateTaskRunnerWithTraits(
+              base::TaskTraits()
+                  .MayBlock()
+                  .WithPriority(base::TaskPriority::BACKGROUND)
+                  .WithShutdownBehavior(
+                      base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN)));
 
     return NULL;
   }
@@ -1131,9 +1135,12 @@ ProfileIOData::SetUpJobFactoryDefaults(
   bool set_protocol = job_factory->SetProtocolHandler(
       url::kFileScheme,
       base::MakeUnique<net::FileProtocolHandler>(
-          content::BrowserThread::GetBlockingPool()
-              ->GetTaskRunnerWithShutdownBehavior(
-                  base::SequencedWorkerPool::SKIP_ON_SHUTDOWN)));
+          base::CreateTaskRunnerWithTraits(
+              base::TaskTraits()
+                  .MayBlock()
+                  .WithPriority(base::TaskPriority::BACKGROUND)
+                  .WithShutdownBehavior(
+                      base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN))));
   DCHECK(set_protocol);
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
@@ -1161,10 +1168,12 @@ ProfileIOData::SetUpJobFactoryDefaults(
 #if defined(OS_ANDROID)
   set_protocol = job_factory->SetProtocolHandler(
       url::kContentScheme,
-      content::ContentProtocolHandler::Create(
-          content::BrowserThread::GetBlockingPool()
-              ->GetTaskRunnerWithShutdownBehavior(
-                  base::SequencedWorkerPool::SKIP_ON_SHUTDOWN)));
+      content::ContentProtocolHandler::Create(base::CreateTaskRunnerWithTraits(
+          base::TaskTraits()
+              .MayBlock()
+              .WithPriority(base::TaskPriority::BACKGROUND)
+              .WithShutdownBehavior(
+                  base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN))));
 #endif
 
   job_factory->SetProtocolHandler(
