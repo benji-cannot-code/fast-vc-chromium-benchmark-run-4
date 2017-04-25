2df2078fa2be0ecf63d4676032a5595b72115725
samans <samans@chromium.org>
2017-04-25 12:06:09 -0700

    Move validation of BeginFrameAck to StructTraits/ParamTraits
    Currently, some validation on the received BeginFrameAcks is performed
    in RenderWidgetHostImpl. It's better to move the validation code to
    Struct/ParamTraits so that all the receivers of BeginFrameAcks and not
    just RenderWidgetHostImpl can benefit from it and also simplify
    RenderWidgetHostImpl's code.
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2833843003
    Cr-Commit-Position: refs/heads/master@{#467067}

--- cc/ipc/cc_param_traits_unittest.cc
+++ cc/ipc/cc_param_traits_unittest.cc
@@ -457,6 +457,8 @@ TEST_F(CCParamTraitsTest, AllQuads) {
   CompositorFrame frame_in;
   frame_in.render_pass_list.push_back(std::move(child_pass_in));
   frame_in.render_pass_list.push_back(std::move(pass_in));
+  frame_in.metadata.begin_frame_ack.sequence_number =
+      cc::BeginFrameArgs::kStartingFrameNumber;
 
   IPC::ParamTraits<CompositorFrame>::Write(&msg, frame_in);
 
@@ -544,6 +546,8 @@ TEST_F(CCParamTraitsTest, UnusedSharedQuadStates) {
 
   CompositorFrame frame_in;
   frame_in.render_pass_list.push_back(std::move(pass_in));
+  frame_in.metadata.begin_frame_ack.sequence_number =
+      cc::BeginFrameArgs::kStartingFrameNumber;
 
   IPC::Message msg(1, 2, IPC::Message::PRIORITY_NORMAL);
   IPC::ParamTraits<CompositorFrame>::Write(&msg, frame_in);
@@ -621,6 +625,8 @@ TEST_F(CCParamTraitsTest, Resources) {
   frame_in.resource_list.push_back(arbitrary_resource1);
   frame_in.resource_list.push_back(arbitrary_resource2);
   frame_in.render_pass_list.push_back(std::move(renderpass_in));
+  frame_in.metadata.begin_frame_ack.sequence_number =
+      cc::BeginFrameArgs::kStartingFrameNumber;
 
   IPC::ParamTraits<CompositorFrame>::Write(&msg, frame_in);
 
