69bb807369c116b5699c3fd7a695c2bda38c905f
bsheedy <bsheedy@chromium.org>
2017-04-25 07:49:19 -0700

    Fix VR Shell black screen during tests
    Forces VrShellDelegate's constructor to run on the main/UI thread, as
    otherwise the Choreographer instance acquired in the constructor is for
    the instrumentation thread during e2e tests, which breaks VR Shell.
    
    BUG=707275
    
    Review-Url: https://codereview.chromium.org/2838763002
    Cr-Commit-Position: refs/heads/master@{#466990}

--- chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellDelegate.java
@@ -31,6 +31,7 @@ import android.widget.FrameLayout;
 import org.chromium.base.ActivityState;
 import org.chromium.base.ApplicationStatus;
 import org.chromium.base.Log;
+import org.chromium.base.ThreadUtils;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
@@ -271,6 +272,7 @@ public class VrShellDelegate implements ApplicationStatus.ActivityStateListener,
         if (sInstance != null) return sInstance;
         VrClassesWrapper wrapper = getVrClassesWrapper();
         if (wrapper == null) return null;
+        ThreadUtils.assertOnUiThread();
         sInstance = new VrShellDelegate(activity, wrapper);
 
         return sInstance;
