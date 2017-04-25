3eb5449f8d7b6c41c3c71849a6f367953eccee81
nicholss <nicholss@chromium.org>
2017-04-25 09:44:09 -0700

    Update CRD ChromeOS Host build files to be better in line with GN style.
    Review-Url: https://codereview.chromium.org/2438773003
    Cr-Commit-Position: refs/heads/master@{#467017}

---
+++ remoting/host/audio_capturer_chromeos.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/logging.h"
+#include "remoting/host/audio_capturer.h"
+
+namespace remoting {
+
+bool AudioCapturer::IsSupported() {
+  return false;
+}
+
+std::unique_ptr<AudioCapturer> AudioCapturer::Create() {
+  NOTIMPLEMENTED();
+  return nullptr;
+}
+
+}  // namespace remoting
