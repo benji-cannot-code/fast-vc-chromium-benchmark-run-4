be9b89eb28bd55138a1a942dd25d76552350c9b8
carlosk <carlosk@chromium.org>
2017-04-25 14:47:11 -0700

    MHTMLTest::TestMHTMLEncoding checks generated MTHML is well formed.
    After generating MHTML data in memory checks that it is well formed by
    parsing it using MHTMLParser.
    
    BUG=672313
    
    Review-Url: https://codereview.chromium.org/2841483007
    Cr-Commit-Position: refs/heads/master@{#467125}

--- third_party/WebKit/Source/web/tests/MHTMLTest.cpp
+++ third_party/WebKit/Source/web/tests/MHTMLTest.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/SerializedResource.h"
 #include "platform/SharedBuffer.h"
 #include "platform/mhtml/MHTMLArchive.h"
+#include "platform/mhtml/MHTMLParser.h"
 #include "platform/testing/URLTestHelpers.h"
 #include "platform/testing/UnitTestHelpers.h"
 #include "platform/weborigin/KURL.h"
@@ -162,6 +163,15 @@ class MHTMLTest : public ::testing::Test {
     }
     MHTMLArchive::GenerateMHTMLFooterForTesting(boundary,
                                                 *mhtml_data->MutableData());
+
+    // Validate the generated MHTML.
+    MHTMLParser parser(
+        SharedBuffer::Create(mhtml_data->data(), mhtml_data->length()));
+    if (parser.ParseArchive().IsEmpty()) {
+      ADD_FAILURE() << "Invalid MHTML";
+      return RawData::Create();
+    }
+
     return mhtml_data.Release();
   }
 
@@ -207,6 +217,7 @@ TEST_F(MHTMLTest, TestMHTMLEncoding) {
   AddTestResources();
   RefPtr<RawData> data = Serialize("Test Serialization", "text/html",
                                    MHTMLArchive::kUseDefaultEncoding);
+  ASSERT_FALSE(HasFailure());
 
   // Read the MHTML data line per line and do some pseudo-parsing to make sure
   // the right encoding is used for the different sections.
@@ -243,6 +254,8 @@ TEST_F(MHTMLTest, MHTMLFromScheme) {
   AddTestResources();
   RefPtr<RawData> raw_data = Serialize("Test Serialization", "text/html",
                                        MHTMLArchive::kUseDefaultEncoding);
+  ASSERT_FALSE(HasFailure());
+
   RefPtr<SharedBuffer> data =
       SharedBuffer::Create(raw_data->data(), raw_data->length());
   KURL http_url = ToKURL("http://www.example.com");
