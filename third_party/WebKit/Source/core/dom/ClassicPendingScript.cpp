9c87c8a22758cbc5ae708ec2b889985781e6eff7
hiroshige <hiroshige@chromium.org>
2017-04-24 23:56:38 -0700

    Reland of name PendingScript::Url() to UrlForClassicScript() (patchset #1 id:1 of https://codereview.chromium.org/2822763002/ )
    This CL doesn't change the behavior, because this is renaming only.
    
    Reason for revert:
    Issue 711703 is fixed on
    https://codereview.chromium.org/2653923008/.
    
    Original issue's description:
    > Revert of Rename PendingScript::Url() to UrlForClassicScript() (patchset #1 id:1 of https://codereview.chromium.org/2815163005/ )
    >
    > Reason for revert:
    > https://codereview.chromium.org/2653923008/ is
    > suspected to cause CHECK() failure (crbug.com/711703)
    > inside PendingScript's prefinalizer.
    >
    > BUG=711703
    >
    > Original issue's description:
    > > Rename PendingScript::Url() to UrlForClassicScript()
    > >
    > > Because it is/can be used only for classic scripts.
    > >
    > > BUG=686281
    > >
    > > Review-Url: https://codereview.chromium.org/2815163005
    > > Cr-Commit-Position: refs/heads/master@{#464613}
    > > Committed: https://chromium.googlesource.com/chromium/src/+/06fcf4b92be26a28fde65c743e6a0d40bbc53dc0
    >
    > TBR=kouhei@chromium.org
    > # Skipping CQ checks because original CL landed less than 1 days ago.
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=686281
    >
    > Review-Url: https://codereview.chromium.org/2822763002
    > Cr-Commit-Position: refs/heads/master@{#464766}
    > Committed: https://chromium.googlesource.com/chromium/src/+/476b2ca298bce5eae94e680ce84ef56c08e28a2e
    
    TBR=kouhei@chromium.org
    BUG=686281, 711703
    
    Review-Url: https://codereview.chromium.org/2819043002
    Cr-Commit-Position: refs/heads/master@{#466906}

--- third_party/WebKit/Source/core/dom/ClassicPendingScript.cpp
+++ third_party/WebKit/Source/core/dom/ClassicPendingScript.cpp
@@ -227,7 +227,7 @@ bool ClassicPendingScript::WasCanceled() const {
   return GetResource()->WasCanceled();
 }
 
-KURL ClassicPendingScript::Url() const {
+KURL ClassicPendingScript::UrlForClassicScript() const {
   return GetResource()->Url();
 }
 
