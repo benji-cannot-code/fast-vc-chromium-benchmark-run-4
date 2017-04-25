b9b1ef6ab8f14a8a2c7731e1bc808d385bbc968d
fdoray <fdoray@chromium.org>
2017-04-25 06:31:27 -0700

    Use TaskScheduler instead of blocking pool in update_client_config.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=finnur@chromium.org
    
    Review-Url: https://codereview.chromium.org/2842493002
    Cr-Commit-Position: refs/heads/master@{#466965}

--- extensions/browser/updater/update_client_config.cc
+++ extensions/browser/updater/update_client_config.cc
@@ -5,8 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "extensions/browser/updater/update_client_config.h"
 
-#include "base/threading/sequenced_worker_pool.h"
-#include "content/public/browser/browser_thread.h"
+#include "base/task_scheduler/post_task.h"
 
 namespace extensions {
 
@@ -14,10 +13,11 @@ UpdateClientConfig::UpdateClientConfig() {}
 
 scoped_refptr<base::SequencedTaskRunner>
 UpdateClientConfig::GetSequencedTaskRunner() const {
-  return content::BrowserThread::GetBlockingPool()
-      ->GetSequencedTaskRunnerWithShutdownBehavior(
-          base::SequencedWorkerPool::GetSequenceToken(),
-          base::SequencedWorkerPool::SKIP_ON_SHUTDOWN);
+  return base::CreateSequencedTaskRunnerWithTraits(
+      base::TaskTraits()
+          .MayBlock()
+          .WithPriority(base::TaskPriority::BACKGROUND)
+          .WithShutdownBehavior(base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN));
 }
 
 UpdateClientConfig::~UpdateClientConfig() {}
