e596985283752934cc192325efd70e9d46be8356
perezju <perezju@chromium.org>
2017-04-25 04:55:48 -0700

    Revert of 🔍 Don't display the search engine until First Run completes (patchset #9 id:160001 of https://codereview.chromium.org/2834253002/ )
    Reason for revert:
    Appears to have broken FirstRunIntegrationTest#testClickThroughFirstRun
    crbug.com/715054
    
    Original issue's description:
    > 🔍 Don't display the search engine until First Run completes
    >
    > * Adds a check to prevent showing branding if First Run hasn't completed.
    >
    > * Updates the widget so that it is updated when an AsyncInitializationActivity
    >   subclass calls onDeferredStartup().
    >
    > * Changes the resource for the microphone to match the omnibox's, as well as
    >   uses its content description.
    >
    > * Adds tests to confirm that the search engine is updated once First Run
    >   completes.
    >
    > BUG=708844,710952
    >
    > Review-Url: https://codereview.chromium.org/2834253002
    > Cr-Commit-Position: refs/heads/master@{#466839}
    > Committed: https://chromium.googlesource.com/chromium/src/+/ffdac2746296923b2658e9b7fa4a4e6f4ee79184
    
    TBR=tedchoc@chromium.org,yusufo@chromium.org,dfalcantara@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=708844,710952
    
    Review-Url: https://codereview.chromium.org/2839943002
    Cr-Commit-Position: refs/heads/master@{#466946}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunActivity.java
@@ -28,7 +28,6 @@ import org.chromium.chrome.browser.net.spdyproxy.DataReductionProxySettings;
 import org.chromium.chrome.browser.preferences.datareduction.DataReductionPromoUtils;
 import org.chromium.chrome.browser.preferences.datareduction.DataReductionProxyUma;
 import org.chromium.chrome.browser.profiles.Profile;
-import org.chromium.chrome.browser.searchwidget.SearchWidgetProvider;
 import org.chromium.chrome.browser.util.IntentUtils;
 
 import java.lang.ref.WeakReference;
@@ -48,24 +47,6 @@ import java.util.concurrent.Callable;
  * The activity might be run more than once, e.g. 1) for ToS and sign-in, and 2) for intro.
  */
 public class FirstRunActivity extends AsyncInitializationActivity implements FirstRunPageDelegate {
-    /** Alerted about various events when FirstRunActivity performs them. */
-    public interface FirstRunActivityObserver {
-        /** See {@link #onFlowIsKnown}. */
-        void onFlowIsKnown();
-
-        /** See {@link #acceptTermsOfService}. */
-        void onAcceptTermsOfService();
-
-        /** See {@link #jumpToPage}. */
-        void onJumpToPage(int position);
-
-        /** Called when First Run is completed. */
-        void onUpdateCachedEngineName();
-
-        /** See {@link #abortFirstRunExperience}. */
-        void onAbortFirstRunExperience();
-    }
-
     protected static final String TAG = "FirstRunActivity";
 
     // Incoming parameters:
@@ -114,8 +95,6 @@ public class FirstRunActivity extends AsyncInitializationActivity implements Fir
 
     @VisibleForTesting
     static FirstRunGlue sGlue = new FirstRunGlueImpl();
-
-    private static FirstRunActivityObserver sObserver;
 
     private boolean mShowWelcomePage = true;
 
@@ -263,7 +242,6 @@ public class FirstRunActivity extends AsyncInitializationActivity implements Fir
                     skipPagesIfNecessary();
                 }
 
-                if (sObserver != null) sObserver.onFlowIsKnown();
                 recordFreProgressHistogram(mFreProgressStates.get(0));
             }
         };
@@ -414,7 +392,6 @@ public class FirstRunActivity extends AsyncInitializationActivity implements Fir
         finishAllTheActivities(getLocalClassName(), Activity.RESULT_CANCELED, intent);
 
         sendPendingIntentIfNecessary(false);
-        if (sObserver != null) sObserver.onAbortFirstRunExperience();
     }
 
     @Override
@@ -466,10 +443,6 @@ public class FirstRunActivity extends AsyncInitializationActivity implements Fir
         resultData.putExtras(mFreProperties);
         finishAllTheActivities(getLocalClassName(), Activity.RESULT_OK, resultData);
 
-        // Update the search engine name cached by the widget.
-        SearchWidgetProvider.updateCachedEngineName();
-        if (sObserver != null) sObserver.onUpdateCachedEngineName();
-
         sendPendingIntentIfNecessary(true);
     }
 
@@ -492,9 +465,7 @@ public class FirstRunActivity extends AsyncInitializationActivity implements Fir
 
     @Override
     public boolean didAcceptTermsOfService() {
-        boolean result = sGlue.didAcceptTermsOfService(getApplicationContext());
-        if (sObserver != null) sObserver.onAcceptTermsOfService();
-        return result;
+        return sGlue.didAcceptTermsOfService(getApplicationContext());
     }
 
     @Override
@@ -576,8 +547,6 @@ public class FirstRunActivity extends AsyncInitializationActivity implements Fir
      * @param position A page index to transition to.
      */
     private boolean jumpToPage(int position) {
-        if (sObserver != null) sObserver.onJumpToPage(position);
-
         if (mShowWelcomePage && !didAcceptTermsOfService()) {
             return position == 0;
         }
@@ -635,9 +604,9 @@ public class FirstRunActivity extends AsyncInitializationActivity implements Fir
         CustomTabActivity.showInfoPage(this, getString(url));
     }
 
+    /** Returns whether or not First Run is ready for interaction. */
     @VisibleForTesting
-    public static void setObserverForTest(FirstRunActivityObserver observer) {
-        assert sObserver == null;
-        sObserver = observer;
+    public boolean isPostNativePageSequenceCreated() {
+        return mPostNativePageSequenceCreated;
     }
 }
