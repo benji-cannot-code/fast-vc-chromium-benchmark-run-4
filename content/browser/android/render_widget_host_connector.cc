25a07d4d674259204c56d61d04f4e19df910a511
jinsukkim <jinsukkim@chromium.org>
2017-04-24 22:15:36 -0700

    Add a test for RenderWidgetHostConnector
    A new test got added to render_widget_host_connector_browsertest.cc
    which ensures RWHVA gets connected to objects inheriting
    RenderWidgetHostConnector regardless of the order in which they
    are created. Previously this had a bug resulting in RWHVA not
    connected ImeAdapter if RWHVA was created first, hence keeping
    soft keyboard from showing up.
    
    Also removed |WebContentsObserver::RenderViewReady()| override. It was
    overridden to initialize rwvha in the connector after creation. But it
    only covers the following cases:
    
    - connector created -> rwhva created -> RenderViewReady invoked
    - rwhva created -> connector created -> RenderViewReady invoked
    
    Can't cover the following case:
     - rwhva created -> RenderViewReady invoked -> connector created
    
    Now Initialize() covers them all, the method became redundant.
    
    BUG=713924
    
    Review-Url: https://codereview.chromium.org/2840443002
    Cr-Commit-Position: refs/heads/master@{#466877}

--- content/browser/android/render_widget_host_connector.cc
+++ content/browser/android/render_widget_host_connector.cc
@@ -20,7 +20,6 @@ class RenderWidgetHostConnector::Observer
   ~Observer() override;
 
   // WebContentsObserver implementation.
-  void RenderViewReady() override;
   void RenderViewHostChanged(RenderViewHost* old_host,
                              RenderViewHost* new_host) override;
   void DidAttachInterstitialPage() override;
@@ -54,10 +53,6 @@ RenderWidgetHostConnector::Observer::Observer(
 
 RenderWidgetHostConnector::Observer::~Observer() {
   DCHECK(!active_rwhva_);
-}
-
-void RenderWidgetHostConnector::Observer::RenderViewReady() {
-  UpdateRenderWidgetHostView(GetRenderWidgetHostViewAndroid());
 }
 
 void RenderWidgetHostConnector::Observer::RenderViewHostChanged(
