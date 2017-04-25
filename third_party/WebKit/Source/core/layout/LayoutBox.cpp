8f5b7622a722e181f7ecf18abe53764d3773f75c
mstensho <mstensho@opera.com>
2017-04-24 18:08:45 -0700

    Allow forced breaks inside floats.
    BUG=700919
    
    Review-Url: https://codereview.chromium.org/2840443003
    Cr-Commit-Position: refs/heads/master@{#466848}

--- third_party/WebKit/Source/core/layout/LayoutBox.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBox.cpp
@@ -2273,7 +2273,7 @@ bool LayoutBox::IsBreakBetweenControllable(EBreakBetween break_value) const {
       // If this is a flow thread for a multicol container, and we have a break
       // value for paged, we need to keep looking.
     }
-    if (curr->IsFloatingOrOutOfFlowPositioned())
+    if (curr->IsOutOfFlowPositioned())
       return false;
     curr = curr->ContainingBlock();
   }
