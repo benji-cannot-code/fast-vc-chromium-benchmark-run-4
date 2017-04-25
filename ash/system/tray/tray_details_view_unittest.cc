1edb356af3bce06b895833aa1efdb398413d4817
mohsen <mohsen@chromium.org>
2017-04-25 15:43:59 -0700

    Refactor AddScrollListItem() in system menu detailed views
    Most of system menu detailed views have a scrollable list of items with
    very similar layouts. This CL adds a few functions in TrayDetailsView to
    create such items.
    
    Among different detailed views, network and VPN are not using these
    common functions, yet, as they need to be cleaned up first. That will be
    done in follow-up CLs.
    
    BUG=686343
    TEST=TrayDetailsViewTest.ScrollContentsTest and
         *TrayAccessibilityTest.CheckMarksOnDetailMenu* in ash_unittests
    
    Review-Url: https://codereview.chromium.org/2831023003
    Cr-Commit-Position: refs/heads/master@{#467147}

--- ash/system/tray/tray_details_view_unittest.cc
+++ ash/system/tray/tray_details_view_unittest.cc
@@ -44,6 +44,10 @@ class TestDetailsView : public TrayDetailsView {
 
   void CreateScrollerViews() { CreateScrollableList(); }
 
+  views::View* scroll_content() const {
+    return TrayDetailsView::scroll_content();
+  }
+
  private:
   TrayPopupHeaderButton* tray_popup_header_button_;
 
--- ash/system/tray_accessibility.h
+++ ash/system/tray_accessibility.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include "ash/accessibility_delegate.h"
-#include "ash/shell_observer.h"
 #include "ash/system/accessibility_observer.h"
 #include "ash/system/tray/tray_details_view.h"
 #include "ash/system/tray/tray_image_item.h"
@@ -22,10 +21,6 @@ namespace chromeos {
 class TrayAccessibilityTest;
 }
 
-namespace gfx {
-struct VectorIcon;
-}
-
 namespace views {
 class Button;
 class CustomButton;
@@ -34,7 +29,6 @@ class View;
 }
 
 namespace ash {
-class HoverHighlightView;
 class SystemTrayItem;
 
 namespace tray {
@@ -61,8 +55,7 @@ class AccessibilityPopupView : public views::View {
 };
 
 // Create the detailed view of accessibility tray.
-class AccessibilityDetailedView : public TrayDetailsView,
-                                  public ShellObserver {
+class AccessibilityDetailedView : public TrayDetailsView {
  public:
   explicit AccessibilityDetailedView(SystemTrayItem* owner);
   ~AccessibilityDetailedView() override {}
@@ -82,15 +75,6 @@ class AccessibilityDetailedView : public TrayDetailsView,
 
   // Add the accessibility feature list.
   void AppendAccessibilityList();
-
-  // Helper function to create entries in the detailed accessibility view.
-  HoverHighlightView* AddScrollListItem(const base::string16& text,
-                                        bool checked,
-                                        const gfx::VectorIcon& icon);
-  HoverHighlightView* AddScrollListItemWithoutIcon(const base::string16& text,
-                                                   bool checked);
-
-  void AddSubHeader(const base::string16& header_text);
 
   views::View* spoken_feedback_view_ = nullptr;
   views::View* high_contrast_view_ = nullptr;
