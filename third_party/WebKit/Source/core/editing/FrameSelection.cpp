9956c4ffcd9344400d8665888fd14a151beab88f
xiaochengh <xiaochengh@chromium.org>
2017-04-25 00:22:56 -0700

    Prune ComputeVisibleSelectionInDOMTreeDeprecated from FrameSelection::Modify
    This patch prunes the deprecated function from both overloads of
    FrameSelection::Modify to improve code health.
    
    BUG=698633
    TEST=n/a; no behaviorial change
    
    Review-Url: https://codereview.chromium.org/2829043004
    Cr-Commit-Position: refs/heads/master@{#466915}

--- third_party/WebKit/Source/core/editing/FrameSelection.cpp
+++ third_party/WebKit/Source/core/editing/FrameSelection.cpp
@@ -378,15 +378,15 @@ bool FrameSelection::Modify(EAlteration alter,
                             SelectionDirection direction,
                             TextGranularity granularity,
                             EUserTriggered user_triggered) {
-  SelectionModifier selection_modifier(
-      *GetFrame(), ComputeVisibleSelectionInDOMTreeDeprecated(),
-      x_pos_for_vertical_arrow_navigation_);
+  SelectionModifier selection_modifier(*GetFrame(),
+                                       ComputeVisibleSelectionInDOMTree(),
+                                       x_pos_for_vertical_arrow_navigation_);
   const bool modified =
       selection_modifier.Modify(alter, direction, granularity);
   if (user_triggered == kUserTriggered &&
       selection_modifier.Selection().IsRange() &&
-      ComputeVisibleSelectionInDOMTreeDeprecated().IsCaret() &&
-      DispatchSelectStart(ComputeVisibleSelectionInDOMTreeDeprecated()) !=
+      ComputeVisibleSelectionInDOMTree().IsCaret() &&
+      DispatchSelectStart(ComputeVisibleSelectionInDOMTree()) !=
           DispatchEventResult::kNotCanceled) {
     return false;
   }
@@ -421,8 +421,8 @@ bool FrameSelection::Modify(EAlteration alter,
 bool FrameSelection::Modify(EAlteration alter,
                             unsigned vertical_distance,
                             VerticalDirection direction) {
-  SelectionModifier selection_modifier(
-      *GetFrame(), ComputeVisibleSelectionInDOMTreeDeprecated());
+  SelectionModifier selection_modifier(*GetFrame(),
+                                       ComputeVisibleSelectionInDOMTree());
   if (!selection_modifier.ModifyWithPageGranularity(alter, vertical_distance,
                                                     direction)) {
     return false;
