fe7a6ea1072fa74bae685a3b087741c0c9c8a7e2
fdoray <fdoray@chromium.org>
2017-04-25 10:13:29 -0700

    Use TaskScheduler instead of blocking pool in camera_detector.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=stevenjb@chromium.org
    
    Review-Url: https://codereview.chromium.org/2839763002
    Cr-Commit-Position: refs/heads/master@{#467028}

--- chrome/browser/chromeos/camera_detector.cc
+++ chrome/browser/chromeos/camera_detector.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/task_runner_util.h"
-#include "base/threading/sequenced_worker_pool.h"
+#include "base/task_scheduler/post_task.h"
 #include "components/storage_monitor/udev_util_linux.h"
 #include "content/public/browser/browser_thread.h"
 
@@ -48,10 +48,14 @@ void CameraDetector::StartPresenceCheck(const base::Closure& callback) {
   DVLOG(1) << "Starting camera presence check";
   presence_check_in_progress_ = true;
   base::PostTaskAndReplyWithResult(
-      BrowserThread::GetBlockingPool()->GetTaskRunnerWithShutdownBehavior(
-          base::SequencedWorkerPool::SKIP_ON_SHUTDOWN).get(),
-      FROM_HERE,
-      base::Bind(&CameraDetector::CheckPresence),
+      base::CreateTaskRunnerWithTraits(
+          base::TaskTraits()
+              .MayBlock()
+              .WithPriority(base::TaskPriority::BACKGROUND)
+              .WithShutdownBehavior(
+                  base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN))
+          .get(),
+      FROM_HERE, base::Bind(&CameraDetector::CheckPresence),
       base::Bind(&CameraDetector::OnPresenceCheckDone, callback));
 }
 
