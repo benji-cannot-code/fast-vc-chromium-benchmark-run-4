d70e4c8b17bee1fa66ecbded62a7186c150d412b
fdoray <fdoray@chromium.org>
2017-04-26 06:57:07 -0700

    Use TaskScheduler instead of blocking pool in dns_probe_browsertest.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=asanka@chromium.org
    
    Review-Url: https://codereview.chromium.org/2839643003
    Cr-Commit-Position: refs/heads/master@{#467314}

--- chrome/browser/net/dns_probe_browsertest.cc
+++ chrome/browser/net/dns_probe_browsertest.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "base/path_service.h"
 #include "base/run_loop.h"
-#include "base/threading/sequenced_worker_pool.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/thread_restrictions.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/io_thread.h"
@@ -186,8 +186,12 @@ class DelayableURLRequestMockHTTPJob : public URLRequestMockHTTPJob,
             request,
             network_delegate,
             file_path,
-            BrowserThread::GetBlockingPool()->GetTaskRunnerWithShutdownBehavior(
-                base::SequencedWorkerPool::SKIP_ON_SHUTDOWN)),
+            base::CreateTaskRunnerWithTraits(
+                base::TaskTraits()
+                    .MayBlock()
+                    .WithPriority(base::TaskPriority::BACKGROUND)
+                    .WithShutdownBehavior(
+                        base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN))),
         should_delay_(should_delay),
         start_delayed_(false),
         destruction_callback_(destruction_callback) {}
