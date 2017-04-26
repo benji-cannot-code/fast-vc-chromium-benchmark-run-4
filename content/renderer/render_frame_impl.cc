b4b196a6197fb9fad56c38e404919378073ea894
sashab <sashab@chromium.org>
2017-04-25 20:57:26 -0700

    Remove redundant WebLocalFrame* parameter from DidChangeScrollOffset
    Remove redundant WebLocalFrame* parameter from DidChangeScrollOffset in
    WebFrameClient. Also updated relevant tests in WebFrameTest.
    
    BUG=361765
    
    Review-Url: https://codereview.chromium.org/2830003002
    Cr-Commit-Position: refs/heads/master@{#467225}

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -4626,8 +4626,7 @@ void RenderFrameImpl::WillReleaseScriptContext(blink::WebLocalFrame* frame,
     observer.WillReleaseScriptContext(context, world_id);
 }
 
-void RenderFrameImpl::DidChangeScrollOffset(blink::WebLocalFrame* frame) {
-  DCHECK_EQ(frame_, frame);
+void RenderFrameImpl::DidChangeScrollOffset() {
   render_view_->StartNavStateSyncTimerIfNecessary(this);
 
   for (auto& observer : observers_)
