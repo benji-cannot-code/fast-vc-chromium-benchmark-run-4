aee3d0921154fbe96644285a45ea8b6c061f659c
nbishop <nbishop@neverware.com>
2017-04-26 07:56:40 -0700

    logPrivate API: prevent log dump from trying to use a non-native path
    The log dump directory is supposed to be under Downloads, but for a
    logged-in user the download path is a non-native path:
    
        /special/drive-<uid>/...
    
    The logging code uses regular file system calls from base that fail
    when encountering "/special".
    
    There is a similar code path in
    NetInternalsMessageHandler::OnStoreDebugLogs (net_internals_ui.cc)
    which checks for a non-native download path and changes the path to
    GetDefaultDownloadDirectoryForProfile; copying that logic to
    GetLogDumpDirectory fixes the problem.
    
    API reference: https://developer.chrome.com/extensions/logPrivate
    
    BUG=None
    TEST=Build with chromeos=1. Whitelist an extension to allow using the logPrivate API and have the extension call chrome.logPrivate.dumpLogs. Run the extension while logged in (not ephemeral user) and confirm that logs are dumped to Downloads/log_dumps.
    
    Review-Url: https://codereview.chromium.org/2838923004
    Cr-Commit-Position: refs/heads/master@{#467326}

--- chrome/browser/extensions/api/log_private/log_private_api_chromeos.cc
+++ chrome/browser/extensions/api/log_private/log_private_api_chromeos.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/log/net_log_entry.h"
 
 #if defined(OS_CHROMEOS)
+#include "chrome/browser/chromeos/file_manager/filesystem_api_util.h"
 #include "chrome/browser/chromeos/system_logs/debug_log_writer.h"
 #endif
 
@@ -112,7 +113,15 @@ base::FilePath GetAppLogDirectory() {
 // will be stored - /home/chronos/<user_profile_dir>/Downloads/log_dumps
 base::FilePath GetLogDumpDirectory(content::BrowserContext* context) {
   const DownloadPrefs* const prefs = DownloadPrefs::FromBrowserContext(context);
-  return prefs->DownloadPath().Append(kLogDumpsSubdir);
+  base::FilePath path = prefs->DownloadPath();
+
+#if defined(OS_CHROMEOS)
+  Profile* profile = Profile::FromBrowserContext(context);
+  if (file_manager::util::IsUnderNonNativeLocalPath(profile, path))
+    path = prefs->GetDefaultDownloadDirectoryForProfile();
+#endif
+
+  return path.Append(kLogDumpsSubdir);
 }
 
 // Removes direcotry content of |logs_dumps| and |app_logs_dir| (only for the
