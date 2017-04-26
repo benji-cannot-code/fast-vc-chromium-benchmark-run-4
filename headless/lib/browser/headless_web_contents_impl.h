40b45f63167c2af3cdccee4e672b09d5ef3ac68f
alexclarke <alexclarke@chromium.org>
2017-04-26 01:34:28 -0700

    HeadlessWebContents Observers for devtools clients attaching/detatching
    BUG=546953
    
    Review-Url: https://codereview.chromium.org/2843773002
    Cr-Commit-Position: refs/heads/master@{#467268}

--- headless/lib/browser/headless_web_contents_impl.h
+++ headless/lib/browser/headless_web_contents_impl.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <unordered_map>
 
+#include "content/public/browser/devtools_agent_host_observer.h"
 #include "content/public/browser/render_process_host_observer.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "headless/lib/browser/headless_window_tree_host.h"
@@ -36,6 +37,7 @@ class WebContentsObserverAdapter;
 class HEADLESS_EXPORT HeadlessWebContentsImpl
     : public HeadlessWebContents,
       public HeadlessDevToolsTarget,
+      public content::DevToolsAgentHostObserver,
       public content::RenderProcessHostObserver,
       public content::WebContentsObserver {
  public:
@@ -66,7 +68,13 @@ class HEADLESS_EXPORT HeadlessWebContentsImpl
   void DetachClient(HeadlessDevToolsClient* client) override;
   bool IsAttached() override;
 
-  // RenderProcessHostObserver implementation:
+  // content::DevToolsAgentHostObserver implementation:
+  void DevToolsAgentHostAttached(
+      content::DevToolsAgentHost* agent_host) override;
+  void DevToolsAgentHostDetached(
+      content::DevToolsAgentHost* agent_host) override;
+
+  // content::RenderProcessHostObserver implementation:
   void RenderProcessExited(content::RenderProcessHost* host,
                            base::TerminationStatus status,
                            int exit_code) override;
