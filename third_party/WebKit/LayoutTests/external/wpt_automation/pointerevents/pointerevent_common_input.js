e092e78ca5ef433119bbb5582d2a17c3e7319c78
dtapuska <dtapuska@chromium.org>
2017-04-26 15:37:26 -0700

    Fix button event on mouse events.
    Button is supposed to be uninitialized on events that are
    mousemove,leave,enter,over and out.
    
    This matches FF and Edge.
    
    BUG=650704
    
    Review-Url: https://codereview.chromium.org/2840023002
    Cr-Commit-Position: refs/heads/master@{#467486}

--- third_party/WebKit/LayoutTests/external/wpt_automation/pointerevents/pointerevent_common_input.js
+++ third_party/WebKit/LayoutTests/external/wpt_automation/pointerevents/pointerevent_common_input.js
@@ -131,9 +131,12 @@ function mouseClickInTarget(targetSelector, targetFrame) {
   });
 }
 
-function mouseDragInTargets(targetSelectorList) {
+function mouseDragInTargets(targetSelectorList, button) {
   return new Promise(function(resolve, reject) {
     if (window.chrome && chrome.gpuBenchmarking) {
+      if (button === undefined) {
+        button = 'left';
+      }
       scrollPageIfNeeded(targetSelectorList[0], document);
       var target = document.querySelector(targetSelectorList[0]);
       var targetRect = target.getBoundingClientRect();
@@ -143,7 +146,7 @@ function mouseDragInTargets(targetSelectorList) {
       var pointerAction = pointerActions[0];
       pointerAction.actions = [];
       pointerAction.actions.push(
-          {name: 'pointerDown', x: xPosition, y: yPosition});
+          {name: 'pointerDown', x: xPosition, y: yPosition, button: button});
       for (var i = 1; i < targetSelectorList.length; i++) {
         scrollPageIfNeeded(targetSelectorList[i], document);
         target = document.querySelector(targetSelectorList[i]);
@@ -151,9 +154,9 @@ function mouseDragInTargets(targetSelectorList) {
         xPosition = targetRect.left + boundaryOffset;
         yPosition = targetRect.top + boundaryOffset;
         pointerAction.actions.push(
-            {name: 'pointerMove', x: xPosition, y: yPosition});
+            {name: 'pointerMove', x: xPosition, y: yPosition, button: button});
       }
-      pointerAction.actions.push({name: 'pointerUp'});
+      pointerAction.actions.push({name: 'pointerUp', button: button});
       chrome.gpuBenchmarking.pointerActionSequence(pointerActions, resolve);
     } else {
       reject();
