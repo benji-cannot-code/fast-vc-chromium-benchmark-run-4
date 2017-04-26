827e6bba9161565aba59913ee0f5f0ee94fa9f4b
dfalcantara <dfalcantara@chromium.org>
2017-04-26 15:41:27 -0700

    ❄ Introduce header and footer logic to search engine promos
    * Add the ability to PromoDialog to be able to keep a header and footer fixed
      while the contents are scrollable.  Scrollable content is indicated by a never
      disappearing scrollbar indicator.
    
    * Adds the ability to add a InfoBarControlLayout to the PromoDialogLayout.  This
      is used to standardize the buttons that are displayed across dialogs.
    
    * Add the ability to add tags to the InfoBarControlLayout radio buttons.  These
      are needed because IDs aren't set for the radio buttons, and because it is
      safer to keep track of the search engine keywords with the RadioButtons that
      represent them.
    
    Things to do:
    - InfoBarControlLayout needs to move to the widget/ directory and be generalized
      so that they're not so infobar specific.
    
    - Tests are in a separate CL.
    
    Example screenshots:
    https://drive.google.com/corp/drive/u/0/folders/0B7c8ZkXVwskDRmlnZ1g0LXVpMGM
    
    BUG=714223,712836,712833
    
    Review-Url: https://codereview.chromium.org/2839253002
    Cr-Commit-Position: refs/heads/master@{#467488}

--- chrome/android/java/src/org/chromium/chrome/browser/locale/SogouPromoDialog.java
+++ chrome/android/java/src/org/chromium/chrome/browser/locale/SogouPromoDialog.java
@@ -10,6 +10,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.IntDef;
+import android.support.annotation.Nullable;
 import android.text.SpannableString;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
@@ -46,6 +47,9 @@ public class SogouPromoDialog extends PromoDialog {
 
     private static final int CHOICE_ENUM_COUNT = 4;
 
+    /** Run when the dialog is dismissed. */
+    private final Runnable mOnDismissedRunnable;
+
     private final LocaleManager mLocaleManager;
     private final ClickableSpan mSpan = new NoUnderlineClickableSpan() {
         @Override
@@ -64,11 +68,13 @@ public class SogouPromoDialog extends PromoDialog {
     /**
      * Creates an instance of the dialog.
      */
-    public SogouPromoDialog(Context context, LocaleManager localeManager) {
+    public SogouPromoDialog(
+            Context context, LocaleManager localeManager, @Nullable Runnable onDismissed) {
         super(context);
         mLocaleManager = localeManager;
         setOnDismissListener(this);
         setCanceledOnTouchOutside(false);
+        mOnDismissedRunnable = onDismissed;
     }
 
     @Override
@@ -144,5 +150,7 @@ public class SogouPromoDialog extends PromoDialog {
                 .apply();
         RecordHistogram.recordEnumeratedHistogram(
                 "SpecialLocale.PromotionDialog", mChoice, CHOICE_ENUM_COUNT);
+
+        if (mOnDismissedRunnable != null) mOnDismissedRunnable.run();
     }
 }
