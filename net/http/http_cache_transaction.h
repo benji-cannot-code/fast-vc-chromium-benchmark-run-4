9bf98f88e16a124fe7f154152ac2fa2a85f387ba
shivanisha <shivanisha@chromium.org>
2017-04-26 13:09:12 -0700

    This CL is a precursor to allowing shared writing to fix cache lock.
    This CL allows transactions to continue to their validation phase even when another
    transaction is the active reader/writer. After the validation phase, if its a match
    the transaction might wait till the response is written to the cache by the active
    writer. If its not a match the transaction will doom the entry and go to the
    network. In a subsequent CL, the not matching case will create a new entry as well.
    
    BUG=472740
    
    Review-Url: https://codereview.chromium.org/2721933002
    Cr-Commit-Position: refs/heads/master@{#467426}

--- net/http/http_cache_transaction.h
+++ net/http/http_cache_transaction.h
@@ -165,6 +165,10 @@ class HttpCache::Transaction : public HttpTransaction {
   int ResumeNetworkStart() override;
   void GetConnectionAttempts(ConnectionAttempts* out) const override;
 
+  // Invoked when parallel validation cannot proceed due to response failure
+  // and this transaction needs to be restarted.
+  void SetValidatingCannotProceed();
+
   // Returns the estimate of dynamically allocated memory in bytes.
   size_t EstimateMemoryUsage() const;
 
@@ -221,6 +225,9 @@ class HttpCache::Transaction : public HttpTransaction {
     STATE_PARTIAL_HEADERS_RECEIVED,
     STATE_CACHE_READ_METADATA,
     STATE_CACHE_READ_METADATA_COMPLETE,
+    STATE_HEADERS_PHASE_CANNOT_PROCEED,
+    STATE_FINISH_HEADERS,
+    STATE_FINISH_HEADERS_COMPLETE,
 
     // These states are entered from Read/AddTruncatedFlag.
     STATE_NETWORK_READ,
@@ -289,6 +296,9 @@ class HttpCache::Transaction : public HttpTransaction {
   int DoPartialHeadersReceived();
   int DoCacheReadMetadata();
   int DoCacheReadMetadataComplete(int result);
+  int DoHeadersPhaseCannotProceed();
+  int DoFinishHeaders(int result);
+  int DoFinishHeadersComplete(int result);
   int DoNetworkRead();
   int DoNetworkReadComplete(int result);
   int DoCacheReadData();
@@ -431,7 +441,12 @@ class HttpCache::Transaction : public HttpTransaction {
   void SyncCacheEntryStatusToResponse();
   void RecordHistograms();
 
-  // Called to signal completion of asynchronous IO.
+  // Called to signal completion of asynchronous IO. Note that this callback is
+  // used in the conventional sense where one layer calls the callback of the
+  // layer above it e.g. this callback gets called from the network transaction
+  // layer. In addition, it is also used for HttpCache layer to let this
+  // transaction know when it is out of a queued state in ActiveEntry and can
+  // continue its processing.
   void OnIOComplete(int result);
 
   // When in a DoLoop, use this to set the next state as it verifies that the
@@ -457,6 +472,7 @@ class HttpCache::Transaction : public HttpTransaction {
   const HttpResponseInfo* new_response_;
   std::string cache_key_;
   Mode mode_;
+  Mode original_mode_;  // Used when restarting the transaction.
   bool reading_;  // We are already reading. Never reverts to false once set.
   bool invalid_range_;  // We may bypass the cache for this request.
   bool truncated_;  // We don't have all the response data.
--- net/http/http_transaction_test_util.h
+++ net/http/http_transaction_test_util.h
@@ -281,6 +281,8 @@ class MockNetworkTransaction
 
   bool done_reading_called_;
 
+  CompletionCallback callback_;  // used for pause and restart.
+
   base::WeakPtrFactory<MockNetworkTransaction> weak_factory_;
 
 };
