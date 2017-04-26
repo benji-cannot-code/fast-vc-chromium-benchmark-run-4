66c4ad27b6905f8734139cc3b8bf911299ccb563
wangxianzhu <wangxianzhu@chromium.org>
2017-04-26 16:45:55 -0700

    Refactor LayoutTableCell::Has(Start|End)BorderAdjoinjingTable() and add test
    This is a preparation of collapsed border optimization
    (https://codereview.chromium.org/2805103003/)
    and bug fixing (later).
    
    BUG=626748
    
    Review-Url: https://codereview.chromium.org/2842123003
    Cr-Commit-Position: refs/heads/master@{#467516}

--- third_party/WebKit/Source/core/layout/LayoutTableCell.h
+++ third_party/WebKit/Source/core/layout/LayoutTableCell.h
@@ -350,6 +350,8 @@ class CORE_EXPORT LayoutTableCell final : public LayoutBlockFlow {
                             const LayoutRect& container_rect) const override;
 
  private:
+  friend class LayoutTableCellTest;
+
   bool IsOfType(LayoutObjectType type) const override {
     return type == kLayoutObjectTableCell || LayoutBlockFlow::IsOfType(type);
   }
@@ -382,6 +384,8 @@ class CORE_EXPORT LayoutTableCell final : public LayoutBlockFlow {
     SetIntrinsicPaddingAfter(after);
   }
 
+  inline bool IsInStartColumn() const;
+  inline bool IsInEndColumn() const;
   bool HasStartBorderAdjoiningTable() const;
   bool HasEndBorderAdjoiningTable() const;
 
