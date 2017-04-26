db5e1fc0891b3817a7fc6cb10a35f20f7858c88b
brucedawson <brucedawson@chromium.org>
2017-04-25 17:44:25 -0700

    Remove unneeded enum forward declaration
    Forward declaring of enums without specifying a type is illegal. This
    forward declaration only worked because it wasn't needed. It was
    found by VC++'s overzealous C4471 warning.
    
    R=raymes
    
    Review-Url: https://codereview.chromium.org/2836913003
    Cr-Commit-Position: refs/heads/master@{#467182}

--- ppapi/proxy/tcp_socket_resource.h
+++ ppapi/proxy/tcp_socket_resource.h
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ppapi {
 
-enum TCPSocketVersion;
-
 namespace proxy {
 
 class PPAPI_PROXY_EXPORT TCPSocketResource : public thunk::PPB_TCPSocket_API,
