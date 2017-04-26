8c05a54e57a2a3c881ed788aa67bc1c4b09b0834
miguelg <miguelg@chromium.org>
2017-04-26 07:08:39 -0700

    Make Android notifications rely on the native notification flag
    Right now we have specific ifdefs for android as opposed to just relying on ENABLE_NATIVE_NOTIFICATIONS + features::kNativeNotifications like all other platforms.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2836093002
    Cr-Commit-Position: refs/heads/master@{#467316}

--- chrome/browser/notifications/notification_common.cc
+++ chrome/browser/notifications/notification_common.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 void NotificationCommon::OpenNotificationSettings(
     content::BrowserContext* browser_context) {
 #if defined(OS_ANDROID)
+  // Android settings are opened directly from Java
   NOTIMPLEMENTED();
 #else
   Profile* profile = Profile::FromBrowserContext(browser_context);
