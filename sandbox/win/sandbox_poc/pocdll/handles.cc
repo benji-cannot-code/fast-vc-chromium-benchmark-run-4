dd90db1840346559e94dee4ad44202fddd358c30
thakis <thakis@chromium.org>
2017-04-26 10:45:32 -0700

    Remove sandbox/win/tools.
    The files therein look unused, haven't been really touched since 2012, aren't
    referenced in any build files, and have some maintenance cost.
    
    BUG=661774
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2773333002
    Cr-Original-Commit-Position: refs/heads/master@{#459696}
    Committed: https://chromium.googlesource.com/chromium/src/+/6625b2f764ae4efb0637215660500ba721b8a43d
    Review-Url: https://codereview.chromium.org/2773333002
    Cr-Commit-Position: refs/heads/master@{#467366}

--- sandbox/win/sandbox_poc/pocdll/handles.cc
+++ sandbox/win/sandbox_poc/pocdll/handles.cc
@@ -4,8 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "sandbox/win/sandbox_poc/pocdll/exports.h"
+#include "sandbox/win/sandbox_poc/pocdll/ntundoc.h"
 #include "sandbox/win/sandbox_poc/pocdll/utils.h"
-#include "sandbox/win/tools/finder/ntundoc.h"
 
 // This file contains the tests used to verify the security of handles in
 // the process
