4c01140609ffe8d30c70397919082a276b50833c
skyostil <skyostil@chromium.org>
2017-04-26 07:08:02 -0700

    headless: Add support for net logging
    This patch adds support for the --log-net-log command line flag to
    headless to help diagnosing issues with networking.
    
    BUG=706355
    
    Review-Url: https://codereview.chromium.org/2841953002
    Review-Url: https://codereview.chromium.org/2841953002
    Cr-Commit-Position: refs/heads/master@{#467315}

--- headless/lib/browser/headless_browser_main_parts.h
+++ headless/lib/browser/headless_browser_main_parts.h
@@ -8,11 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/files/file_path.h"
 #include "content/public/browser/browser_main_parts.h"
 #include "headless/public/headless_browser.h"
 
 namespace headless {
 
+class HeadlessNetLog;
 class HeadlessBrowserImpl;
 
 class HeadlessBrowserMainParts : public content::BrowserMainParts {
@@ -27,10 +29,13 @@ class HeadlessBrowserMainParts : public content::BrowserMainParts {
   void PreMainMessageLoopStart() override;
 #endif
 
+  HeadlessNetLog* net_log() const { return net_log_.get(); }
+
  private:
   HeadlessBrowserImpl* browser_;  // Not owned.
 
   bool devtools_http_handler_started_;
+  std::unique_ptr<HeadlessNetLog> net_log_;
 
   DISALLOW_COPY_AND_ASSIGN(HeadlessBrowserMainParts);
 };
--- headless/lib/browser/headless_url_request_context_getter.h
+++ headless/lib/browser/headless_url_request_context_getter.h
@@ -36,7 +36,8 @@ class HeadlessURLRequestContextGetter : public net::URLRequestContextGetter {
       content::ProtocolHandlerMap* protocol_handlers,
       ProtocolHandlerMap context_protocol_handlers,
       content::URLRequestInterceptorScopedVector request_interceptors,
-      HeadlessBrowserContextOptions* options);
+      HeadlessBrowserContextOptions* options,
+      net::NetLog* net_log);
 
   // net::URLRequestContextGetter implementation:
   net::URLRequestContext* GetURLRequestContext() override;
@@ -63,6 +64,7 @@ class HeadlessURLRequestContextGetter : public net::URLRequestContextGetter {
   std::unique_ptr<net::URLRequestContext> url_request_context_;
   content::ProtocolHandlerMap protocol_handlers_;
   content::URLRequestInterceptorScopedVector request_interceptors_;
+  net::NetLog* net_log_;  // Not owned.
 
   DISALLOW_COPY_AND_ASSIGN(HeadlessURLRequestContextGetter);
 };
