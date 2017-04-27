2e9c28a36788fcc5ca3ddb495ed3b23df230ac48
vitaliii <vitaliii@chromium.org>
2017-04-27 06:09:03 -0700

    Disable measurements.repaint_unittest.RepaintUnitTest.testRepaint.
    Because flaky.
    
    TBR=nednguyen@google.com
    BUG=715962
    
    Review-Url: https://codereview.chromium.org/2849513002
    Cr-Commit-Position: refs/heads/master@{#467653}

--- tools/perf/measurements/repaint_unittest.py
+++ tools/perf/measurements/repaint_unittest.py
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import unittest
+
 from telemetry import decorators
 from telemetry.page import page as page_module
 from telemetry.testing import options_for_unittests
@@ -35,7 +37,8 @@ class RepaintUnitTest(page_test_test_case.PageTestTestCase):
     self._options = options_for_unittests.GetCopy()
     self._options.browser_options.wpr_mode = wpr_modes.WPR_OFF
 
-  @decorators.Disabled('chromeos')  # crbug.com/483212
+  # Previously this test was disabled on chromeos, see crbug.com/483212.
+  @unittest.skip("flaky") # crbug.com/715962
   def testRepaint(self):
     ps = self.CreateEmptyPageSet()
     ps.AddStory(TestRepaintPage(ps, ps.base_dir))
