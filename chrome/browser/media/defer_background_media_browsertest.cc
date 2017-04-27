b3b281a0862cf65367213a33559768fc8acdb58d
mlamouri <mlamouri@chromium.org>
2017-04-27 08:49:42 -0700

    Rename kDisableGestureRequirementForMediaPlayback and make it a test-only flag.
    The flag should only be set by tests and will allow all autoplay related
    restrictions such as autoplay policy and background playback to be
    disabled.
    
    BUG=714676
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2843913003
    Cr-Commit-Position: refs/heads/master@{#467692}

--- chrome/browser/media/defer_background_media_browsertest.cc
+++ chrome/browser/media/defer_background_media_browsertest.cc
@@ -10,16 +10,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
-#include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test_utils.h"
+#include "media/base/media_switches.h"
 #include "media/base/test_data_util.h"
 
 class DeferredMediaBrowserTest : public InProcessBrowserTest {
  public:
 #if defined(OS_ANDROID)
   void SetUpCommandLine(base::CommandLine* command_line) override {
-    command_line->AppendSwitch(
-        switches::kDisableGestureRequirementForMediaPlayback);
+    command_line->AppendSwitch(switches::kIgnoreAutoplayRestrictionsForTests);
   }
 #endif
 };
