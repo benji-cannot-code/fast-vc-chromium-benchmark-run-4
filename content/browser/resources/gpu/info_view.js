c0775914ff3082e78a3d3b5b502222858f7103ad
Sunny Sachanandani <sunnyps@chromium.org>
2017-04-27 14:58:45 -0700

    gpu: Show full command line in chrome://gpu
    The executable path can have spaces so just splitting by space doesn't
    work. It can be quoted or not depending on platform so using a regex
    doesn't work either.
    
    R=kbr
    BUG=580050
    
    Change-Id: I2707a257c6e89169f5486e2364c5b4045d142d9e
    Reviewed-on: https://chromium-review.googlesource.com/489603
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#467803}

--- content/browser/resources/gpu/info_view.js
+++ content/browser/resources/gpu/info_view.js
@@ -39,10 +39,6 @@ cr.define('gpu', function() {
       if (browserBridge.clientInfo) {
         var clientInfo = browserBridge.clientInfo;
 
-        var commandLineParts = clientInfo.command_line.split(' ');
-        commandLineParts.shift(); // Pop off the exe path
-        var commandLineString = commandLineParts.join(' ')
-
         this.setTable_('client-info', [
           {
             description: 'Data exported',
@@ -73,8 +69,8 @@ cr.define('gpu', function() {
             value: clientInfo.graphics_backend
           },
           {
-            description: 'Command Line Args',
-            value: commandLineString
+            description: 'Command Line',
+            value: clientInfo.command_line
           }]);
       } else {
         this.setText_('client-info', '... loading...');
