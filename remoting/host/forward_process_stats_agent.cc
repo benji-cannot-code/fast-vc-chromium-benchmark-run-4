dd6d7d6e77976547cfba32f6615ede444247b251
zijiehe <zijiehe@chromium.org>
2017-04-27 12:35:43 -0700

    [Chromoting] Retrieve process resource usage (ProcessStats) and its tests
    ProcessStatsStub is an interface to receive process resource usage information
    from a ProcessStats.
    
    ProcessResourceUsage is a structure to store the resource usage of one process.
    AggregatedProcessResourceUsage is a structure to store the resource usgaes of
    several processes.
    
    ProcessStatsAgent is an interface to retrieve resource usage of one process.
    CurrentProcessStatsAgent is a ProcessStatsAgent implementation to retrieve
    resource usage from current process.
    ForwardProcessStatsAgent receives resource usage from a third-party and forwards
    the latest usage as a ProcessStatsAgent.
    
    ProcessStatsSender is a class to regularly report resource usages from various
    ProcessStatsAgent implementations to a ProcessStatsStub. It merges several
    non-empty ProcessResourceUsage into one AggregatedProcessResourceUsage.
    This is typical useful on Windows: we need to calculate the resource usages from
    both network process and desktop process.
    
    R=sergeyu@chromium.org, jamiewalch@chromium.org
    BUG=650926
    
    Review-Url: https://codereview.chromium.org/2775983003
    Cr-Commit-Position: refs/heads/master@{#467753}

---
+++ remoting/host/forward_process_stats_agent.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/host/forward_process_stats_agent.h"
+
+#include "base/logging.h"
+
+namespace remoting {
+
+ForwardProcessStatsAgent::ForwardProcessStatsAgent() = default;
+ForwardProcessStatsAgent::~ForwardProcessStatsAgent() = default;
+
+protocol::ProcessResourceUsage ForwardProcessStatsAgent::GetResourceUsage() {
+  DCHECK(thread_checker_.CalledOnValidThread());
+  return usage_;
+}
+
+void ForwardProcessStatsAgent::OnProcessStats(
+    const protocol::ProcessResourceUsage& usage) {
+  DCHECK(thread_checker_.CalledOnValidThread());
+  usage_ = usage;
+}
+
+}  // namespace remoting
