6c170cddf2b38053f28d70a9d3e0ebdf2a5af217
dfalcantara <dfalcantara@chromium.org>
2017-04-27 13:28:16 -0700

    🔍 Introduce default search engine dialog
    Add a dialog that can be triggered via LocaleManager that allows a user
    to choose their default search engine from a randomized list.
    
    DefaultSearchEnginePromoDialog:
    * Add a basic dialog that forces a user to select a default search engine.  This
      dialog only shows up for the "existing user" case and (currently) randomizes
      the search engines available as defined in the TemplateUrlService.
    
    * Add the ability to show the DefaultSearchEnginePromoDialog from the
      LocaleManager.  It currently never fires because the logic hasn't landed, but
      it is triggered from ChromeTabbedActivity and SearchActivity.
    
    Things to do:
    - Still trying to figure out how to test this.
    
    Screenshots:
    https://drive.google.com/corp/drive/u/0/folders/0B7c8ZkXVwskDRmlnZ1g0LXVpMGM
    
    BUG=714223,712836,712833
    
    Review-Url: https://codereview.chromium.org/2838833002
    Cr-Commit-Position: refs/heads/master@{#467766}

--- chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchWidgetProvider.java
+++ chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchWidgetProvider.java
@@ -29,6 +29,7 @@ import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.IntentHandler;
 import org.chromium.chrome.browser.firstrun.FirstRunFlowSequencer;
+import org.chromium.chrome.browser.omnibox.LocationBarLayout;
 import org.chromium.chrome.browser.search_engines.TemplateUrlService;
 import org.chromium.chrome.browser.search_engines.TemplateUrlService.LoadListener;
 import org.chromium.chrome.browser.search_engines.TemplateUrlService.TemplateUrlServiceObserver;
@@ -123,6 +124,9 @@ public class SearchWidgetProvider extends AppWidgetProvider {
     private static final String TAG = "searchwidget";
     private static final Object DELEGATE_LOCK = new Object();
     private static final Object OBSERVER_LOCK = new Object();
+
+    /** The default search engine's root URL. */
+    private static String sDefaultSearchEngineUrl;
 
     private static SearchWidgetTemplateUrlServiceObserver sObserver;
     private static SearchWidgetProviderDelegate sDelegate;
@@ -292,6 +296,12 @@ public class SearchWidgetProvider extends AppWidgetProvider {
         TemplateUrlService service = TemplateUrlService.getInstance();
         if (!service.isLoaded()) return;
 
+        // Update the URL that we show for zero-suggest.
+        String searchEngineUrl = service.getSearchEngineUrlFromTemplateUrl(
+                service.getDefaultSearchEngineTemplateUrl().getKeyword());
+        sDefaultSearchEngineUrl =
+                LocationBarLayout.splitPathFromUrlDisplayText(searchEngineUrl).first;
+
         updateCachedEngineName(service.getDefaultSearchEngineTemplateUrl().getShortName());
     }
 
@@ -383,5 +393,11 @@ public class SearchWidgetProvider extends AppWidgetProvider {
     static void setDelegateForTest(SearchWidgetProviderDelegate delegate) {
         assert sDelegate == null;
         sDelegate = delegate;
+    }
+
+    /** See {@link #sDefaultSearchEngineUrl}. */
+    static String getDefaultSearchEngineUrl() {
+        // TODO(yusufo): Get rid of this.
+        return sDefaultSearchEngineUrl;
     }
 }
