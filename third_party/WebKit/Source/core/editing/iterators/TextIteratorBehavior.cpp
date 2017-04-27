40174b1810d9dbfd28ff391b4489781af30848b7
timav <timav@chromium.org>
2017-04-26 18:19:08 -0700

    Do not count extra trailing space for the last word in a range
    Sometimes when a text selection ends with the last word on a line
    (for instance when just one word is selected) the generated text
    for the selection range contains an extra trailing space.
    This happens when a text node is split into several InlineTextBoxes
    (e.g. a <span> inside a narrow table) and the last selected word
    happens to be the last one in such text box.
    
    The TextIterator algorithm restores the spaces between the text boxes
    and will add a trailing space for this word. For the SurroundingText,
    however, we need the length that corresponds to the visible selection.
    
    This CL introduces a new text iterator behavior that prevents that
    extra space to be added. This behavior is used in combination with
    DefaultRangeLengthBehavior() for the selection range calculation
    in the SurroundingText.
    
    BUG=708329
    
    Review-Url: https://codereview.chromium.org/2832123002
    Cr-Commit-Position: refs/heads/master@{#467538}

--- third_party/WebKit/Source/core/editing/iterators/TextIteratorBehavior.cpp
+++ third_party/WebKit/Source/core/editing/iterators/TextIteratorBehavior.cpp
@@ -108,6 +108,12 @@ TextIteratorBehavior::Builder::SetStopsOnFormControls(bool value) {
   return *this;
 }
 
+TextIteratorBehavior::Builder&
+TextIteratorBehavior::Builder::SetDoesNotEmitSpaceBeyondRangeEnd(bool value) {
+  behavior_.does_not_emit_space_beyond_range_end_ = value;
+  return *this;
+}
+
 // -
 TextIteratorBehavior::TextIteratorBehavior(const TextIteratorBehavior& other) =
     default;
@@ -127,7 +133,8 @@ TextIteratorBehavior::TextIteratorBehavior()
       for_selection_to_string_(false),
       for_window_find_(false),
       ignores_style_visibility_(false),
-      stops_on_form_controls_(false) {}
+      stops_on_form_controls_(false),
+      does_not_emit_space_beyond_range_end_(false) {}
 
 TextIteratorBehavior::~TextIteratorBehavior() = default;
 
@@ -149,7 +156,9 @@ bool TextIteratorBehavior::operator==(const TextIteratorBehavior& other) const {
          for_selection_to_string_ == other.for_selection_to_string_ ||
          for_window_find_ == other.for_window_find_ ||
          ignores_style_visibility_ == other.ignores_style_visibility_ ||
-         stops_on_form_controls_ == other.stops_on_form_controls_;
+         stops_on_form_controls_ == other.stops_on_form_controls_ ||
+         does_not_emit_space_beyond_range_end_ ==
+             other.does_not_emit_space_beyond_range_end_;
 }
 
 bool TextIteratorBehavior::operator!=(const TextIteratorBehavior& other) const {
@@ -184,6 +193,15 @@ TextIteratorBehavior::AllVisiblePositionsRangeLengthBehavior() {
   return TextIteratorBehavior::Builder()
       .SetEmitsObjectReplacementCharacter(true)
       .SetEmitsCharactersBetweenAllVisiblePositions(true)
+      .Build();
+}
+
+// static
+TextIteratorBehavior
+TextIteratorBehavior::NoTrailingSpaceRangeLengthBehavior() {
+  return TextIteratorBehavior::Builder()
+      .SetEmitsObjectReplacementCharacter(true)
+      .SetDoesNotEmitSpaceBeyondRangeEnd(true)
       .Build();
 }
 
