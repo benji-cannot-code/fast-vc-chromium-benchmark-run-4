cf8feac33415fb0fe577cc09156c9ae11838f4c3
stevenjb <stevenjb@chromium.org>
2017-04-27 13:49:14 -0700

    MD Settings: Fix subpage navigation focus for bluetooth+internet
    This CL:
    * Fixes the subpage arrow for bluetooth and focus it correctly.
    * Provides focus logic for internet-page -> internet-subpage ->
      internet-detail-page.
    * Fixes keyboard navigation to details page from cr-network-list-item
    
    BUG=714350
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2841873004
    Cr-Commit-Position: refs/heads/master@{#467783}

--- chrome/browser/resources/settings/internet_page/internet_page.js
+++ chrome/browser/resources/settings/internet_page/internet_page.js
@@ -89,7 +89,18 @@ Polymer({
         return [];
       }
     },
+
+    /** @private {!Map<string, string>} */
+    focusConfig_: {
+      type: Object,
+      value: function() {
+        return new Map();
+      },
+    },
   },
+
+  /** @private {string} Type of last detail page visited. */
+  detailType_: '',
 
   // Element event listeners
   listeners: {
@@ -111,13 +122,13 @@ Polymer({
 
   /** @override */
   attached: function() {
-    this.onExtensionAddedListener_ = this.onExtensionAddedListener_ ||
-        this.onExtensionAdded_.bind(this);
+    this.onExtensionAddedListener_ =
+        this.onExtensionAddedListener_ || this.onExtensionAdded_.bind(this);
     chrome.management.onInstalled.addListener(this.onExtensionAddedListener_);
     chrome.management.onEnabled.addListener(this.onExtensionAddedListener_);
 
-    this.onExtensionRemovedListener_ = this.onExtensionRemovedListener_ ||
-        this.onExtensionRemoved_.bind(this);
+    this.onExtensionRemovedListener_ =
+        this.onExtensionRemovedListener_ || this.onExtensionRemoved_.bind(this);
     chrome.management.onUninstalled.addListener(
         this.onExtensionRemovedListener_);
 
@@ -147,9 +158,10 @@ Polymer({
   /**
    * settings.RouteObserverBehavior
    * @param {!settings.Route} route
+   * @param {!settings.Route} oldRoute
    * @protected
    */
-  currentRouteChanged: function(route) {
+  currentRouteChanged: function(route, oldRoute) {
     if (route == settings.Route.INTERNET_NETWORKS) {
       // Handle direct navigation to the networks page,
       // e.g. chrome://settings/internet/networks?type=WiFi
@@ -164,7 +176,29 @@ Polymer({
       var type = queryParams.get('type');
       if (type)
         this.knownNetworksType_ = type;
+    } else if (
+        route != settings.Route.INTERNET && route != settings.Route.BASIC) {
+      // If we are navigating to a non internet section, do not set focus.
+      return;
     }
+
+    if (!settings.Route.INTERNET.contains(oldRoute))
+      return;
+
+    // Focus the subpage arrow where appropriate.
+    var selector;
+    if (route == settings.Route.INTERNET_NETWORKS) {
+      // iron-list makes the correct timing to focus an item in the list
+      // very complicated, and the item may not exist, so just focus the
+      // entire list for now.
+      selector = '* /deep/ #networkList';
+    } else if (this.detailType_) {
+      selector = '* /deep/ #' + this.detailType_ + ' /deep/ .subpage-arrow';
+    }
+    if (selector && this.querySelector(selector))
+      this.focusConfig_.set(oldRoute.path, selector);
+    else
+      this.focusConfig_.delete(oldRoute.path);
   },
 
   /**
@@ -185,6 +219,7 @@ Polymer({
    * @private
    */
   onShowDetail_: function(event) {
+    this.detailType_ = event.detail.Type;
     var params = new URLSearchParams;
     params.append('guid', event.detail.GUID);
     params.append('type', event.detail.Type);
@@ -198,6 +233,7 @@ Polymer({
    * @private
    */
   onShowNetworks_: function(event) {
+    this.detailType_ = event.detail.Type;
     var params = new URLSearchParams;
     params.append('type', event.detail.Type);
     this.subpageType_ = event.detail.Type;
@@ -217,6 +253,7 @@ Polymer({
    * @private
    */
   onShowKnownNetworks_: function(event) {
+    this.detailType_ = event.detail.Type;
     var params = new URLSearchParams;
     params.append('type', event.detail.Type);
     this.knownNetworksType_ = event.detail.type;
@@ -330,8 +367,8 @@ Polymer({
    * @private
    */
   deviceIsEnabled_: function(deviceState) {
-    return !!deviceState && deviceState.State ==
-        chrome.networkingPrivate.DeviceStateType.ENABLED;
+    return !!deviceState &&
+        deviceState.State == chrome.networkingPrivate.DeviceStateType.ENABLED;
   },
 
   /**
