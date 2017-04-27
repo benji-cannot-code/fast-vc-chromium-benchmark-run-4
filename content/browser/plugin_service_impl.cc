f579f341b7244d70c9b0e806944f817632cc3d15
fdoray <fdoray@chromium.org>
2017-04-26 23:56:39 -0700

    Use TaskScheduler instead of blocking pool in plugin_service_impl.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=boliu@chromium.org
    
    Review-Url: https://codereview.chromium.org/2837863003
    Cr-Commit-Position: refs/heads/master@{#467611}

--- content/browser/plugin_service_impl.cc
+++ content/browser/plugin_service_impl.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/synchronization/waitable_event.h"
-#include "base/threading/sequenced_worker_pool.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/thread.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
@@ -98,11 +98,11 @@ PluginServiceImpl::~PluginServiceImpl() {
 }
 
 void PluginServiceImpl::Init() {
-  plugin_list_task_runner_ =
-      BrowserThread::GetBlockingPool()
-          ->GetSequencedTaskRunnerWithShutdownBehavior(
-              base::SequencedWorkerPool::GetSequenceToken(),
-              base::SequencedWorkerPool::SKIP_ON_SHUTDOWN);
+  plugin_list_task_runner_ = base::CreateSequencedTaskRunnerWithTraits(
+      base::TaskTraits()
+          .MayBlock()
+          .WithPriority(base::TaskPriority::USER_VISIBLE)
+          .WithShutdownBehavior(base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN));
   PluginList::Singleton()->set_will_load_plugins_callback(
       base::Bind(&WillLoadPluginsCallback, &plugin_list_sequence_checker_));
 
