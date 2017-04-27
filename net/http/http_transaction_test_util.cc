2010a773f8e03277f8ba7a083c4c9fc0f21e662f
warx <warx@chromium.org>
2017-04-26 19:45:58 -0700

    Revert of HttpCache::Transaction layer allowing parallel validation (patchset #33 id:800001 of https://codereview.chromium.org/2721933002/ )
    Reason for revert:
    Breaks tricky-tot-chrome-pfq-informational audio_CrasSanity autotest: https://uberchromegw.corp.google.com/i/chromeos.chrome/builders/tricky-tot-chrome-pfq-informational/builds/4271
    
    Original issue's description:
    > This CL is a precursor to allowing shared writing to fix cache lock.
    >
    > This CL allows transactions to continue to their validation phase even when another
    > transaction is the active reader/writer. After the validation phase, if its a match
    > the transaction might wait till the response is written to the cache by the active
    > writer. If its not a match the transaction will doom the entry and go to the
    > network. In a subsequent CL, the not matching case will create a new entry as well.
    >
    > BUG=472740
    >
    > Review-Url: https://codereview.chromium.org/2721933002
    > Cr-Commit-Position: refs/heads/master@{#467426}
    > Committed: https://chromium.googlesource.com/chromium/src/+/1e2e347f957ef889aaee527bb757849f76e8a808
    
    TBR=asanka@chromium.org,jkarlin@chromium.org,rdsmith@chromium.org,shivanisha@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=472740
    
    Review-Url: https://codereview.chromium.org/2847653002
    Cr-Commit-Position: refs/heads/master@{#467556}

--- net/http/http_transaction_test_util.cc
+++ net/http/http_transaction_test_util.cc
@@ -410,6 +410,13 @@ int MockNetworkTransaction::StartInternal(const HttpRequestInfo* request,
   if (!t)
     return ERR_FAILED;
 
+  if (!before_network_start_callback_.is_null()) {
+    bool defer = false;
+    before_network_start_callback_.Run(&defer);
+    if (defer)
+      return net::ERR_IO_PENDING;
+  }
+
   test_mode_ = t->test_mode;
 
   // Return immediately if we're returning an error.
@@ -460,16 +467,6 @@ int MockNetworkTransaction::StartInternal(const HttpRequestInfo* request,
   if (request_->load_flags & LOAD_PREFETCH)
     response_.unused_since_prefetch = true;
 
-  // Pause and resume.
-  if (!before_network_start_callback_.is_null()) {
-    bool defer = false;
-    before_network_start_callback_.Run(&defer);
-    if (defer) {
-      callback_ = callback;
-      return net::ERR_IO_PENDING;
-    }
-  }
-
   if (test_mode_ & TEST_MODE_SYNC_NET_START)
     return OK;
 
@@ -486,9 +483,8 @@ void MockNetworkTransaction::SetBeforeHeadersSentCallback(
     const BeforeHeadersSentCallback& callback) {}
 
 int MockNetworkTransaction::ResumeNetworkStart() {
-  DCHECK(!callback_.is_null());
-  CallbackLater(callback_, OK);
-  return ERR_IO_PENDING;
+  // Should not get here.
+  return ERR_FAILED;
 }
 
 void MockNetworkTransaction::GetConnectionAttempts(
