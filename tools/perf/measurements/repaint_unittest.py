8a8fb1e6b44e59b2af403371b78ac241fa9881c0
nednguyen <nednguyen@google.com>
2017-04-27 10:52:12 -0700

    Fix the repaint disabling decorator to be consistent with Telemetry disabling style
    BUG=715962
    
    TBR=rnephew@chromium.org
    
    Review-Url: https://codereview.chromium.org/2850483002
    Cr-Commit-Position: refs/heads/master@{#467730}

--- tools/perf/measurements/repaint_unittest.py
+++ tools/perf/measurements/repaint_unittest.py
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import unittest
-
 from telemetry import decorators
 from telemetry.page import page as page_module
 from telemetry.testing import options_for_unittests
@@ -38,7 +36,7 @@ class RepaintUnitTest(page_test_test_case.PageTestTestCase):
     self._options.browser_options.wpr_mode = wpr_modes.WPR_OFF
 
   # Previously this test was disabled on chromeos, see crbug.com/483212.
-  @unittest.skip("flaky") # crbug.com/715962
+  @decorators.Disabled("all") # crbug.com/715962
   def testRepaint(self):
     ps = self.CreateEmptyPageSet()
     ps.AddStory(TestRepaintPage(ps, ps.base_dir))
