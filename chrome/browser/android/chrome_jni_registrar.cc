e3f9525c492d1b7101ac3888a44adc926d4513d6
yusufo <yusufo@chromium.org>
2017-04-27 14:24:17 -0700

    Add Digital Asset Links verification for postMessage API
    Adds DigitalAssetLinks handler which queries digitalassetlinks.googleapis.com/
    for verifying the relationship between an Android app and a web domain.
     This is added in a new component digital_asset_links since this API is
     a generic web API for checking Android-Web relationships. It may be
    useful for iOS and ChromeOS as well. The component currently depends on
     base and net.
    
    Then custom tabs uses this handler for verifying postMessage origin declared by
    the client app when they send requestPostMessageChannel. This enabled
    third party apps to use postMessage related APIs with secure and
    verified origin declaration.
    
    BUG=704975
    
    Review-Url: https://codereview.chromium.org/2767333006
    Cr-Commit-Position: refs/heads/master@{#467791}

--- chrome/browser/android/chrome_jni_registrar.cc
+++ chrome/browser/android/chrome_jni_registrar.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/contextualsearch/contextual_search_tab_helper.h"
 #include "chrome/browser/android/contextualsearch/ctr_suppression.h"
 #include "chrome/browser/android/cookies/cookies_fetcher.h"
+#include "chrome/browser/android/customtabs/origin_verifier.h"
 #include "chrome/browser/android/data_usage/data_use_tab_ui_manager_android.h"
 #include "chrome/browser/android/data_usage/external_data_use_observer_bridge.h"
 #include "chrome/browser/android/devtools_server.h"
@@ -359,6 +360,7 @@ static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
     {"OmniboxUrlEmphasizer",
      OmniboxUrlEmphasizer::RegisterOmniboxUrlEmphasizer},
     {"OmniboxViewUtil", OmniboxViewUtil::RegisterOmniboxViewUtil},
+    {"OriginVerifier", customtabs::RegisterOriginVerifier},
     {"OverlayPanelContent", RegisterOverlayPanelContent},
     {"PartnerBookmarksReader",
      PartnerBookmarksReader::RegisterPartnerBookmarksReader},
