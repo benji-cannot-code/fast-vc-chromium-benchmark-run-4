f724bc41fb1bdba30647b6fd15ee1a2fb7fc975b
nigeltao <nigeltao@chromium.org>
2017-04-26 20:57:36 -0700

    Add a mechanism to scramble Mojo message IDs.
    For example, chrome/common/renderer_configuration.mojom defines:
    
    interface RendererConfiguration {
      SetInitialConfiguration(etc);
      SetContentSettingRules(etc);
      SetFieldTrialGroup(etc);
    };
    
    The generated IDs in
    gen/chrome/common/renderer_configuration.mojom-shared-internal.h, prior
    to this CL, and also after this CL with an empty seed, are:
    
    constexpr uint32_t kRendererConfiguration_SetInitialConfiguration_Name = 0;
    constexpr uint32_t kRendererConfiguration_SetContentSettingRules_Name = 1;
    constexpr uint32_t kRendererConfiguration_SetFieldTrialGroup_Name = 2;
    
    After this CL, with a "foo" seed, the IDs are:
    
    constexpr uint32_t kRendererConfiguration_SetInitialConfiguration_Name = 1257880741;
    constexpr uint32_t kRendererConfiguration_SetContentSettingRules_Name = 631133653;
    constexpr uint32_t kRendererConfiguration_SetFieldTrialGroup_Name = 549336076;
    
    BUG=673417
    
    Review-Url: https://codereview.chromium.org/2794743002
    Cr-Commit-Position: refs/heads/master@{#467573}

--- mojo/public/tools/bindings/mojom_bindings_generator_unittest.py
+++ mojo/public/tools/bindings/mojom_bindings_generator_unittest.py
@@ -6,6 +6,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import unittest
 
 from mojom_bindings_generator import MakeImportStackMessage
+from mojom_bindings_generator import ScrambleMethodOrdinals
+
+
+class FakeIface(object):
+  def __init__( self ):
+    self.name = None
+    self.methods = None
+
+
+class FakeMethod(object):
+  def __init__( self ):
+    self.ordinal = None
+    self.ordinal_comment = None
 
 
 class MojoBindingsGeneratorTest(unittest.TestCase):
@@ -18,6 +31,23 @@ class MojoBindingsGeneratorTest(unittest.TestCase):
         "\n  y was imported by x")
     self.assertEquals(MakeImportStackMessage(["x", "y", "z"]),
         "\n  z was imported by y\n  y was imported by x")
+
+  def testScrambleMethodOrdinals(self):
+    """Tests ScrambleMethodOrdinals()."""
+    interface = FakeIface()
+    interface.name = 'RendererConfiguration'
+    interface.methods = [FakeMethod(), FakeMethod(), FakeMethod()]
+    ScrambleMethodOrdinals([interface], "foo")
+    # These next three values are hard-coded. If the generation algorithm
+    # changes from being based on sha256(seed + interface.name + str(i)) then
+    # these numbers will obviously need to change too.
+    #
+    # Note that hashlib.sha256('fooRendererConfiguration1').digest()[:4] is
+    # '\xa5\xbc\xf9\xca' and that hex(1257880741) = '0x4af9bca5'. The
+    # difference in 0x4a vs 0xca is because we only take 31 bits.
+    self.assertEquals(interface.methods[0].ordinal, 1257880741)
+    self.assertEquals(interface.methods[1].ordinal, 631133653)
+    self.assertEquals(interface.methods[2].ordinal, 549336076)
 
 
 if __name__ == "__main__":
