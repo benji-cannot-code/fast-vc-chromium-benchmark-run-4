94648130dde47aa514573b18cf4a49b785de1f35
fdoray <fdoray@chromium.org>
2017-04-27 06:02:22 -0700

    Switch to TaskPriority::USER_VISIBLE in profile_io_data.cc.
    Protocol handlers are used for navigation.
    
    BUG=667892
    
    Review-Url: https://codereview.chromium.org/2845683002
    Cr-Commit-Position: refs/heads/master@{#467651}

--- chrome/browser/profiles/profile_io_data.cc
+++ chrome/browser/profiles/profile_io_data.cc
@@ -235,7 +235,7 @@ class DebugDevToolsInterceptor : public net::URLRequestInterceptor {
           base::CreateTaskRunnerWithTraits(
               base::TaskTraits()
                   .MayBlock()
-                  .WithPriority(base::TaskPriority::BACKGROUND)
+                  .WithPriority(base::TaskPriority::USER_VISIBLE)
                   .WithShutdownBehavior(
                       base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN)));
 
@@ -1138,7 +1138,7 @@ ProfileIOData::SetUpJobFactoryDefaults(
           base::CreateTaskRunnerWithTraits(
               base::TaskTraits()
                   .MayBlock()
-                  .WithPriority(base::TaskPriority::BACKGROUND)
+                  .WithPriority(base::TaskPriority::USER_VISIBLE)
                   .WithShutdownBehavior(
                       base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN))));
   DCHECK(set_protocol);
@@ -1171,7 +1171,7 @@ ProfileIOData::SetUpJobFactoryDefaults(
       content::ContentProtocolHandler::Create(base::CreateTaskRunnerWithTraits(
           base::TaskTraits()
               .MayBlock()
-              .WithPriority(base::TaskPriority::BACKGROUND)
+              .WithPriority(base::TaskPriority::USER_VISIBLE)
               .WithShutdownBehavior(
                   base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN))));
 #endif
