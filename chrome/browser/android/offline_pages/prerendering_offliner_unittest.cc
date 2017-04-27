3494ef18954078d9653822ace31d13f5945d23c6
petewil <petewil@chromium.org>
2017-04-27 13:27:30 -0700

    Re-enable formerly flaky tests for the PrerenderingOffliner.
    A Sheriff disabled these tests when they were found to be flaky.
    I've fixed two problems, an uninitialized pointer which might have
    been causing a crash (previous changelist), and in this changelist
    a missing call to PumpLoop() causing the test to fail when
    race conditions happened.
    
    BUG=712941
    
    Review-Url: https://codereview.chromium.org/2837873006
    Cr-Commit-Position: refs/heads/master@{#467765}

--- chrome/browser/android/offline_pages/prerendering_offliner_unittest.cc
+++ chrome/browser/android/offline_pages/prerendering_offliner_unittest.cc
@@ -258,7 +258,7 @@ void PrerenderingOfflinerTest::OnCancel(const SavePageRequest& request) {
   cancel_callback_called_ = true;
 }
 
-TEST_F(PrerenderingOfflinerTest, DISABLED_LoadAndSaveBadUrl) {
+TEST_F(PrerenderingOfflinerTest, LoadAndSaveBadUrl) {
   base::Time creation_time = base::Time::Now();
   SavePageRequest request(
       kRequestId, kFileUrl, kClientId, creation_time, kUserRequested);
@@ -267,7 +267,7 @@ TEST_F(PrerenderingOfflinerTest, DISABLED_LoadAndSaveBadUrl) {
   EXPECT_TRUE(loader()->IsIdle());
 }
 
-TEST_F(PrerenderingOfflinerTest, DISABLED_LoadAndSavePrerenderingDisabled) {
+TEST_F(PrerenderingOfflinerTest, LoadAndSavePrerenderingDisabled) {
   base::Time creation_time = base::Time::Now();
   SavePageRequest request(
       kRequestId, kHttpUrl, kClientId, creation_time, kUserRequested);
@@ -278,7 +278,7 @@ TEST_F(PrerenderingOfflinerTest, DISABLED_LoadAndSavePrerenderingDisabled) {
 }
 
 TEST_F(PrerenderingOfflinerTest,
-       DISABLED_LoadAndSaveBlockThirdPartyCookiesForCustomTabs) {
+       LoadAndSaveBlockThirdPartyCookiesForCustomTabs) {
   base::Time creation_time = base::Time::Now();
   ClientId custom_tabs_client_id("custom_tabs", "88");
   SavePageRequest request(kRequestId, kHttpUrl, custom_tabs_client_id,
@@ -290,7 +290,7 @@ TEST_F(PrerenderingOfflinerTest,
 }
 
 TEST_F(PrerenderingOfflinerTest,
-       DISABLED_LoadAndSaveBlockOnDisabledPrerendererForCustomTabs) {
+       LoadAndSaveBlockOnDisabledPrerendererForCustomTabs) {
   base::Time creation_time = base::Time::Now();
   ClientId custom_tabs_client_id("custom_tabs", "88");
   SavePageRequest request(kRequestId, kHttpUrl, custom_tabs_client_id,
@@ -303,7 +303,7 @@ TEST_F(PrerenderingOfflinerTest,
   EXPECT_TRUE(loader()->IsIdle());
 }
 
-TEST_F(PrerenderingOfflinerTest, DISABLED_LoadAndSaveLoadStartedButFails) {
+TEST_F(PrerenderingOfflinerTest, LoadAndSaveLoadStartedButFails) {
   base::Time creation_time = base::Time::Now();
   SavePageRequest request(
       kRequestId, kHttpUrl, kClientId, creation_time, kUserRequested);
@@ -320,7 +320,7 @@ TEST_F(PrerenderingOfflinerTest, DISABLED_LoadAndSaveLoadStartedButFails) {
   EXPECT_FALSE(SaveInProgress());
 }
 
-TEST_F(PrerenderingOfflinerTest, DISABLED_CancelWhenLoading) {
+TEST_F(PrerenderingOfflinerTest, CancelWhenLoading) {
   base::Time creation_time = base::Time::Now();
   SavePageRequest request(
       kRequestId, kHttpUrl, kClientId, creation_time, kUserRequested);
@@ -435,39 +435,46 @@ TEST_F(PrerenderingOfflinerTest, LoadAndSaveLoadedButThenCanceledFromLoader) {
   EXPECT_TRUE(SaveInProgress());
 }
 
-TEST_F(PrerenderingOfflinerTest,
-       DISABLED_ForegroundTransitionCancelsOnLowEndDevice) {
+TEST_F(PrerenderingOfflinerTest, ForegroundTransitionCancelsOnLowEndDevice) {
   offliner()->SetLowEndDeviceForTesting(true);
 
   base::Time creation_time = base::Time::Now();
   SavePageRequest request(
       kRequestId, kHttpUrl, kClientId, creation_time, kUserRequested);
+  // LoadAndSave completes asynchronously, and notifies us via the
+  // completion_callback() when it completes.
   EXPECT_TRUE(offliner()->LoadAndSave(request, completion_callback(),
                                       progress_callback()));
   EXPECT_FALSE(loader()->IsIdle());
 
   offliner()->SetApplicationStateForTesting(
       base::android::APPLICATION_STATE_HAS_RUNNING_ACTIVITIES);
+
+  // Wait until the completion callback arrives before checking postconditions.
+  PumpLoop();
 
   // Loading canceled on low-end device.
   EXPECT_TRUE(loader()->IsIdle());
   EXPECT_EQ(Offliner::RequestStatus::FOREGROUND_CANCELED, request_status());
 }
 
-// TODO(crbug.com/712941): Flaky test.
-TEST_F(PrerenderingOfflinerTest,
-       DISABLED_ForegroundTransitionIgnoredOnHighEndDevice) {
+TEST_F(PrerenderingOfflinerTest, ForegroundTransitionIgnoredOnHighEndDevice) {
   offliner()->SetLowEndDeviceForTesting(false);
 
   base::Time creation_time = base::Time::Now();
   SavePageRequest request(
       kRequestId, kHttpUrl, kClientId, creation_time, kUserRequested);
+  // LoadAndSave completes asynchronously, and notifies us via the
+  // completion_callback() when it completes.
   EXPECT_TRUE(offliner()->LoadAndSave(request, completion_callback(),
                                       progress_callback()));
   EXPECT_FALSE(loader()->IsIdle());
 
   offliner()->SetApplicationStateForTesting(
       base::android::APPLICATION_STATE_HAS_RUNNING_ACTIVITIES);
+
+  // Wait until the completion callback arrives before checking postconditions.
+  PumpLoop();
 
   // Loader still loading since not low-end device.
   EXPECT_FALSE(loader()->IsIdle());
@@ -509,7 +516,7 @@ TEST_F(PrerenderingOfflinerTest,
 }
 
 TEST_F(PrerenderingOfflinerTest,
-       DISABLED_HandleTimeoutCompletedTriesMetWithoutLowbarMet) {
+       HandleTimeoutCompletedTriesMetWithoutLowbarMet) {
   offliner()->SetLowEndDeviceForTesting(false);
 
   base::Time creation_time = base::Time::Now();
@@ -523,8 +530,7 @@ TEST_F(PrerenderingOfflinerTest,
   EXPECT_FALSE(loader()->start_snapshot_called());
 }
 
-TEST_F(PrerenderingOfflinerTest,
-       DISABLED_HandleTimeoutStartedTriesMetWithoutLowbarMet) {
+TEST_F(PrerenderingOfflinerTest, HandleTimeoutStartedTriesMetWithoutLowbarMet) {
   offliner()->SetLowEndDeviceForTesting(false);
 
   base::Time creation_time = base::Time::Now();
@@ -552,7 +558,7 @@ TEST_F(PrerenderingOfflinerTest, HandleTimeoutWithLowbarAndStartedTriesMet) {
   EXPECT_TRUE(loader()->start_snapshot_called());
 }
 
-TEST_F(PrerenderingOfflinerTest, DISABLED_HandleTimeoutWithOnlyLowbarMet) {
+TEST_F(PrerenderingOfflinerTest, HandleTimeoutWithOnlyLowbarMet) {
   offliner()->SetLowEndDeviceForTesting(false);
 
   base::Time creation_time = base::Time::Now();
