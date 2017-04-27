afc05a06e120efc4d120b2f9e617f789c872d342
dcastagna <dcastagna@chromium.org>
2017-04-27 10:50:12 -0700

    cc: Don't subtract transparent overlay from damage.
    After processing overlay candidates we used to remove the overlay rects
    from the damage rect.
    The assumption was that all the overlays would be opaque.
    
    Since we started allowing alpha blended overaly, we should avoid
    subtracting from the damage when blending might be allowed.
    Otherwise any element animating below a transparent overlay might
    not be updated.
    
    We haven't noticed any problem so far since partial update was disabled
    on Mali. crrev.com/2829543003 enables empty swap, surfacing the problem
    when the damage region is a subset of a transparent overlay.
    
    BUG=705290
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2842383003
    Cr-Commit-Position: refs/heads/master@{#467727}

--- cc/output/overlay_candidate.cc
+++ cc/output/overlay_candidate.cc
@@ -177,6 +177,7 @@ OverlayCandidate::OverlayCandidate()
       format(gfx::BufferFormat::RGBA_8888),
       uv_rect(0.f, 0.f, 1.f, 1.f),
       is_clipped(false),
+      is_opaque(false),
       use_output_surface_for_resource(false),
       resource_id(0),
 #if defined(OS_ANDROID)
@@ -212,6 +213,7 @@ bool OverlayCandidate::FromDrawQuad(ResourceProvider* resource_provider,
 
   candidate->clip_rect = quad->shared_quad_state->clip_rect;
   candidate->is_clipped = quad->shared_quad_state->is_clipped;
+  candidate->is_opaque = !quad->ShouldDrawWithBlending();
 
   switch (quad->material) {
     case DrawQuad::TEXTURE_CONTENT:
