1f82a0bb658c8f74e9477408e51e862b8ccc935f
fdoray <fdoray@chromium.org>
2017-04-27 03:21:08 -0700

    Use TaskScheduler instead of blocking pool in external_metrics.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    
    Review-Url: https://codereview.chromium.org/2839973002
    Cr-Commit-Position: refs/heads/master@{#467632}

--- chrome/browser/chromeos/external_metrics.cc
+++ chrome/browser/chromeos/external_metrics.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/sparse_histogram.h"
 #include "base/metrics/statistics_recorder.h"
 #include "base/metrics/user_metrics.h"
-#include "base/threading/sequenced_worker_pool.h"
+#include "base/task_scheduler/post_task.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/metrics/chromeos_metrics_provider.h"
 #include "components/metrics/metrics_service.h"
@@ -49,11 +49,12 @@ bool CheckLinearValues(const std::string& name, int maximum) {
   return CheckValues(name, 1, maximum, maximum + 1);
 }
 
-}  // namespace
+// The interval between external metrics collections.
+constexpr base::TimeDelta kExternalMetricsCollectionInterval =
+    base::TimeDelta::FromSeconds(30);
+constexpr char kEventsFilePath[] = "/var/lib/metrics/uma-events";
 
-// The interval between external metrics collections in seconds
-static const int kExternalMetricsCollectionIntervalSeconds = 30;
-const char kEventsFilePath[] = "/var/lib/metrics/uma-events";
+}  // namespace
 
 ExternalMetrics::ExternalMetrics() : uma_events_file_(kEventsFilePath) {
 }
@@ -172,11 +173,13 @@ void ExternalMetrics::CollectEventsAndReschedule() {
 }
 
 void ExternalMetrics::ScheduleCollector() {
-  bool result = BrowserThread::GetBlockingPool()->PostDelayedWorkerTask(
+  base::PostDelayedTaskWithTraits(
       FROM_HERE,
-      base::Bind(&chromeos::ExternalMetrics::CollectEventsAndReschedule, this),
-      base::TimeDelta::FromSeconds(kExternalMetricsCollectionIntervalSeconds));
-  DCHECK(result);
+      base::TaskTraits().MayBlock().WithPriority(
+          base::TaskPriority::BACKGROUND),
+      base::BindOnce(&chromeos::ExternalMetrics::CollectEventsAndReschedule,
+                     this),
+      kExternalMetricsCollectionInterval);
 }
 
 }  // namespace chromeos
