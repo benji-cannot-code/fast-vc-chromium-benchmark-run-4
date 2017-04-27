d77723a1daf1f1bde8cba2700b6788c0d939c146
karandeepb <karandeepb@chromium.org>
2017-04-26 17:29:09 -0700

    Extensions: Clarify ManifestHandler interface expectations.
    ManifestHandler::Parse is called from within Extension::Create and may or may
    not be called on a thread which supports IO. On the other hand,
    ManifestHandler::ValidateExtension is called through
    file_util::ValidateExtension and hence is always called on a thread supporting
    IO. There are assumptions in the code relying on this as well. This CL documents
    the same in the ManifestHandler header.
    
    BUG=None.
    
    Review-Url: https://codereview.chromium.org/2846553003
    Cr-Commit-Position: refs/heads/master@{#467526}

--- extensions/common/manifest_handler.cc
+++ extensions/common/manifest_handler.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/stl_util.h"
+#include "base/threading/thread_restrictions.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/permissions/manifest_permission.h"
 #include "extensions/common/permissions/manifest_permission_set.h"
@@ -91,6 +92,7 @@ bool ManifestHandler::ParseExtension(Extension* extension,
 bool ManifestHandler::ValidateExtension(const Extension* extension,
                                         std::string* error,
                                         std::vector<InstallWarning>* warnings) {
+  base::ThreadRestrictions::AssertIOAllowed();
   return GetRegistry()->ValidateExtension(extension, error, warnings);
 }
 
