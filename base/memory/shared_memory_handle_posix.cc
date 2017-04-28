67ad9dd2d50d2b665eb951181296466b626a965c
erikchen <erikchen@chromium.org>
2017-04-28 10:19:56 -0700

    Used a single class for SharedMemoryHandle.
    This CL is a refactor with no intended behavior change.
    
    Now that the class is defined on all platforms, make it a single class rather
    than 3 separate classes. This allows the removal of some duplicate code in the
    public interface.
    
    BUG=713763
    
    Review-Url: https://codereview.chromium.org/2846893003
    Cr-Commit-Position: refs/heads/master@{#468039}

--- base/memory/shared_memory_handle_posix.cc
+++ base/memory/shared_memory_handle_posix.cc
@@ -13,6 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 
 SharedMemoryHandle::SharedMemoryHandle() = default;
+SharedMemoryHandle::SharedMemoryHandle(const SharedMemoryHandle& handle) =
+    default;
+SharedMemoryHandle& SharedMemoryHandle::operator=(
+    const SharedMemoryHandle& handle) = default;
 
 SharedMemoryHandle::SharedMemoryHandle(
     const base::FileDescriptor& file_descriptor)
