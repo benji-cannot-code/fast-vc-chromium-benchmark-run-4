e573b749a42c72d285dc7ff74b0025f4e37391ca
tdanderson <tdanderson@chromium.org>
2017-04-27 18:47:47 -0700

    [Ash] Disallow calling HoverHighlightView::SetSubText() with an empty string
    SetSubText() in HoverHighlight view should not be called
    if the parameter string is empty, otherwise multi-line
    rows in the system menu will have the incorrect layout
    of their primary text.
    
    BUG=715615
    TEST=manual
    
    Review-Url: https://codereview.chromium.org/2844933003
    Cr-Commit-Position: refs/heads/master@{#467813}

--- ash/system/tray/hover_highlight_view.cc
+++ ash/system/tray/hover_highlight_view.cc
@@ -58,11 +58,13 @@ void HoverHighlightView::SetRightViewVisible(bool visible) {
 
 void HoverHighlightView::SetSubText(const base::string16& sub_text) {
   DCHECK(text_label_);
+  DCHECK(!sub_text.empty());
 
   if (!sub_text_label_) {
     sub_text_label_ = TrayPopupUtils::CreateDefaultLabel();
     tri_view_->AddView(TriView::Container::CENTER, sub_text_label_);
   }
+
   TrayPopupItemStyle sub_style(TrayPopupItemStyle::FontStyle::CAPTION);
   sub_style.set_color_style(TrayPopupItemStyle::ColorStyle::INACTIVE);
   sub_style.SetupLabel(sub_text_label_);
@@ -118,7 +120,8 @@ void HoverHighlightView::DoAddIconAndLabels(
   style.SetupLabel(text_label_);
   tri_view_->AddView(TriView::Container::CENTER, text_label_);
 
-  SetSubText(sub_text);
+  if (!sub_text.empty())
+    SetSubText(sub_text);
 
   tri_view_->SetContainerVisible(TriView::Container::END, false);
 
