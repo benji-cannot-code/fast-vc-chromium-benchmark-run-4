cce5085d73f10fd34ec8e4a906dfc89080ac04cb
esum <esum@chromium.org>
2017-04-27 18:58:14 -0700

    [Chromecast] Add method to CastCdm for the session expiration updating.
    Allows derived classes to access/run the SessionExpirationUpdateCB that
    gets passed in to Initialize(). This follows the same pattern as the
    other session callbacks in CastCdm (OnSessionClosed(),
    OnSessionKeysChange()).
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2847923002
    Cr-Commit-Position: refs/heads/master@{#467825}

--- chromecast/media/cdm/cast_cdm.h
+++ chromecast/media/cdm/cast_cdm.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/sequenced_task_runner_helpers.h"
 #include "base/threading/thread_checker.h"
+#include "base/time/time.h"
 #include "chromecast/media/base/media_resource_tracker.h"
 #include "chromecast/media/cdm/cast_cdm_context.h"
 #include "chromecast/public/media/cast_key_status.h"
@@ -83,6 +84,8 @@ class CastCdm : public ::media::ContentDecryptionModule {
   void OnSessionKeysChange(const std::string& session_id,
                            bool newly_usable_keys,
                            ::media::CdmKeysInfo keys_info);
+  void OnSessionExpirationUpdate(const std::string& session_id,
+                                 base::Time new_expiry_time);
 
   void KeyIdAndKeyPairsToInfo(const ::media::KeyIdAndKeyPairs& keys,
                               ::media::CdmKeysInfo* key_info);
