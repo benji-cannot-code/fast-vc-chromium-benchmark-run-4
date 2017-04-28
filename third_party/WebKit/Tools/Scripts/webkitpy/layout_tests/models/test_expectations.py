37e27e90aaa1a01d1b27b97eddf05908d2e20bd6
tkent <tkent@chromium.org>
2017-04-27 18:57:48 -0700

    webkitpy: Change paths in lint-test-expectations messages
    In order to void path_from_webkit_base() in test_expectations.py, this CL change
    it to path_from_chromium_base(). lint-expectations messages will be changed from:
    
    LayoutTests/TestExpectations:3320 Duplicate or ambiguous entry lines LayoutTests/TestExpectations:3319 and LayoutTests/TestExpectations:3320. external/wpt/css/css-display-3/display-contents-computed-style.html
    
    to
    
    third_party/WebKit/LayoutTests/TestExpectations:3320 Duplicate or ambiguous entry lines third_party/WebKit/LayoutTests/TestExpectations:3319 and third_party/WebKit/LayoutTests/TestExpectations:3320. external/wpt/css/css-display-3/display-contents-computed-style.html
    
    BUG=710535
    
    Review-Url: https://codereview.chromium.org/2850433002
    Cr-Commit-Position: refs/heads/master@{#467824}

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/models/test_expectations.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/models/test_expectations.py
@@ -1098,9 +1098,8 @@ class TestExpectations(object):
 
     def _shorten_filename(self, filename):
         finder = WebKitFinder(self._port.host.filesystem)
-        # TODO(tkent): Can we use path_from_layout_tests() instead?
-        if filename.startswith(finder.path_from_webkit_base()):
-            return self._port.host.filesystem.relpath(filename, finder.path_from_webkit_base())
+        if filename.startswith(finder.path_from_chromium_base()):
+            return self._port.host.filesystem.relpath(filename, finder.path_from_chromium_base())
         return filename
 
     def _report_warnings(self):
