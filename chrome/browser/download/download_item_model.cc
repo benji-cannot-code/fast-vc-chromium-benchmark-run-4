c4601791c2cb4acd1ca191ee2451c677fbf4f60a
qinmin <qinmin@chromium.org>
2017-04-27 22:23:29 -0700

    interrupt and resume download with CONTENT_LENGTH_MISMATCH errors
    If a download ends with CONTENT_LENGTH_MISMATCH error, chrome treat it as completed.
    However, this could happen if server closes the connection too early.
    And it causes the downloaded file size to be incorrect.
    And with pause/resume functionalities, this could happen more frequently.
    This change treats these download as interrupted if strong validators are present.
    For downloads without strong validators, auto resumption could cause them to enter a restart->interrupt->restart cycle.
    As a result, download is treated as completed if there are no strong validators.
    
    BUG=453357
    
    Review-Url: https://codereview.chromium.org/2832223004
    Cr-Commit-Position: refs/heads/master@{#467897}

--- chrome/browser/download/download_item_model.cc
+++ chrome/browser/download/download_item_model.cc
@@ -182,6 +182,9 @@ base::string16 InterruptReasonStatusMessage(
     case content::DOWNLOAD_INTERRUPT_REASON_SERVER_UNREACHABLE:
       string_id = IDS_DOWNLOAD_INTERRUPTED_STATUS_UNREACHABLE;
       break;
+    case content::DOWNLOAD_INTERRUPT_REASON_SERVER_CONTENT_LENGTH_MISMATCH:
+      string_id = IDS_DOWNLOAD_INTERRUPTED_STATUS_CONTENT_LENGTH_MISMATCH;
+      break;
 
     case content::DOWNLOAD_INTERRUPT_REASON_NONE:
       NOTREACHED();
@@ -269,6 +272,9 @@ base::string16 InterruptReasonMessage(content::DownloadInterruptReason reason) {
       break;
     case content::DOWNLOAD_INTERRUPT_REASON_SERVER_UNREACHABLE:
       string_id = IDS_DOWNLOAD_INTERRUPTED_DESCRIPTION_UNREACHABLE;
+      break;
+    case content::DOWNLOAD_INTERRUPT_REASON_SERVER_CONTENT_LENGTH_MISMATCH:
+      string_id = IDS_DOWNLOAD_INTERRUPTED_DESCRIPTION_CONTENT_LENGTH_MISMATCH;
       break;
     case content::DOWNLOAD_INTERRUPT_REASON_NONE:
       NOTREACHED();
--- content/browser/download/download_request_core.h
+++ content/browser/download/download_request_core.h
@@ -117,7 +117,7 @@ class CONTENT_EXPORT DownloadRequestCore
 
  private:
   static DownloadInterruptReason HandleRequestStatus(
-      const net::URLRequestStatus& status);
+      const net::URLRequestStatus& status, bool has_strong_validators);
 
   static DownloadInterruptReason HandleSuccessfulServerResponse(
       const net::HttpResponseHeaders& http_headers,
