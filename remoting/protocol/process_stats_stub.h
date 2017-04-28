110bc5304cbbf78562b41c308af8198e1e1fd332
zijiehe <zijiehe@chromium.org>
2017-04-27 19:16:05 -0700

    [Chromoting] Retrieve process resource usage (ProcessStats) and its tests
    ProcessStatsStub is an interface to receive process resource usage information
    from a ProcessStats.
    
    ProcessResourceUsage is a structure to store the resource usage of one process.
    AggregatedProcessResourceUsage is a structure to store the resource usgaes of
    several processes.
    
    ProcessStatsAgent is an interface to retrieve resource usage of one process.
    CurrentProcessStatsAgent is a ProcessStatsAgent implementation to retrieve
    resource usage from current process.
    ForwardProcessStatsAgent receives resource usage from a third-party and forwards
    the latest usage as a ProcessStatsAgent.
    
    ProcessStatsSender is a class to regularly report resource usages from various
    ProcessStatsAgent implementations to a ProcessStatsStub. It merges several
    non-empty ProcessResourceUsage into one AggregatedProcessResourceUsage.
    This is typical useful on Windows: we need to calculate the resource usages from
    both network process and desktop process.
    
    R=sergeyu@chromium.org, jamiewalch@chromium.org
    BUG=650926
    
    Review-Url: https://codereview.chromium.org/2775983003
    Cr-Original-Commit-Position: refs/heads/master@{#467753}
    Committed: https://chromium.googlesource.com/chromium/src/+/5928f440f84065ab45f848dcd135e3c2f52ca5bb
    Review-Url: https://codereview.chromium.org/2775983003
    Cr-Commit-Position: refs/heads/master@{#467844}

---
+++ remoting/protocol/process_stats_stub.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_PROTOCOL_PROCESS_STATS_STUB_H_
+#define REMOTING_PROTOCOL_PROCESS_STATS_STUB_H_
+
+#include "remoting/proto/process_stats.pb.h"
+
+namespace remoting {
+namespace protocol {
+
+// An interface to receive process statistic data.
+class ProcessStatsStub {
+ public:
+  ProcessStatsStub() = default;
+  virtual ~ProcessStatsStub() = default;
+
+  virtual void OnProcessStats(
+      const protocol::AggregatedProcessResourceUsage& usage) = 0;
+};
+
+}  // namespace protocol
+}  // namespace remoting
+
+#endif  // REMOTING_PROTOCOL_PROCESS_STATS_STUB_H_
