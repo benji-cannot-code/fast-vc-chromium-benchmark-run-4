358d6e79a19bc5d992def506426073aad7d6d09f
calamity <calamity@chromium.org>
2017-04-27 23:12:29 -0700

    [SiteEngagement WebUI] Replace C++ browser test with JS version.
    BUG=703848
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2811643002
    Cr-Commit-Position: refs/heads/master@{#467916}

--- chrome/browser/resources/engagement/site_engagement.js
+++ chrome/browser/resources/engagement/site_engagement.js
@@ -121,7 +121,7 @@ define('main', [
      */
     function handleBaseScoreChange(origin, e) {
       var baseScoreInput = e.target;
-      uiHandler.setSiteEngagementScoreForUrl(origin, baseScoreInput.value);
+      uiHandler.setSiteEngagementBaseScoreForUrl(origin, baseScoreInput.value);
       baseScoreInput.barCellRef.style.width = (baseScoreInput.value * 4) + 'px';
       baseScoreInput.blur();
       enableAutoupdate();
@@ -196,8 +196,6 @@ define('main', [
 
         engagementTableBody.appendChild(createRow(info));
       });
-
-      resolvePageIsPopulated();
     }
 
     /**
@@ -208,6 +206,7 @@ define('main', [
       uiHandler.getSiteEngagementDetails().then((response) => {
         info = response.info;
         renderTable(info);
+        resolvePageIsPopulated();
       });
     };
 
