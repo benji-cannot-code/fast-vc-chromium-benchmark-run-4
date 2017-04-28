3d8ae238ed7d300ea38c452913e26b950089caf5
estade <estade@chromium.org>
2017-04-28 15:28:06 -0700

    Introduce a type of View background that stays in sync with its host View's native theme.
    This fixes a few bugs where we weren't using the right native theme,
    whether by not updating after the NativeTheme changes or by trying to
    access the NativeTheme before the View is added to a hierarchy (which
    yields the default NativeTheme --- for most platforms, this didn't
    effectively create a bug as there's only one NativeTheme).
    
    Best example is that now the sad tab respects the GTK native theme.
    
    Get rid of Ash's tray_constants::kBackgroundColor in favor of using the
    bubble background color from the NativeTheme.
    
    This is a reland of 097f9cde453ea57eb4aa037f44add782391c5eb9 with fix
    for asan failure.
    
    BUG=711183, 693282
    TBR=stevenjb@chromium.org,tdanderson@chromium.org
    
    Review-Url: https://codereview.chromium.org/2838273002
    Cr-Commit-Position: refs/heads/master@{#468157}

--- ui/views/bubble/tray_bubble_view.h
+++ ui/views/bubble/tray_bubble_view.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
+#include "base/optional.h"
 #include "ui/views/bubble/bubble_dialog_delegate.h"
 #include "ui/views/mouse_watcher.h"
 #include "ui/views/views_export.h"
@@ -84,7 +85,8 @@ class VIEWS_EXPORT TrayBubbleView : public BubbleDialogDelegateView,
     int max_height;
     bool can_activate;
     bool close_on_deactivate;
-    SkColor bg_color;
+    // If not provided, the bg color will be derived from the NativeTheme.
+    base::Optional<SkColor> bg_color;
   };
 
   // Constructs and returns a TrayBubbleView. |init_params| may be modified.
