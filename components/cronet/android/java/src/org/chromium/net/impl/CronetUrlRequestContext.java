d9babf34611472d56b421008fadb470f032645a6
kapishnikov <kapishnikov@chromium.org>
2017-04-28 10:43:38 -0700

    Cronet: fix flaky Http2Test#testHttp2
    BUG=716480
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_cronet_tester
    
    Review-Url: https://codereview.chromium.org/2848903003
    Cr-Commit-Position: refs/heads/master@{#468049}

--- components/cronet/android/java/src/org/chromium/net/impl/CronetUrlRequestContext.java
+++ components/cronet/android/java/src/org/chromium/net/impl/CronetUrlRequestContext.java
@@ -141,7 +141,7 @@ public class CronetUrlRequestContext extends CronetEngineBase {
     /** Holds CertVerifier data. */
     private String mCertVerifierData;
 
-    private ConditionVariable mStopNetLogCompleted;
+    private volatile ConditionVariable mStopNetLogCompleted;
 
     /**
      * True if a NetLog observer is active.
@@ -296,9 +296,9 @@ public class CronetUrlRequestContext extends CronetEngineBase {
                 return;
             }
             checkHaveAdapter();
+            mStopNetLogCompleted = new ConditionVariable();
             nativeStopNetLog(mUrlRequestContextAdapter);
             mIsLogging = false;
-            mStopNetLogCompleted = new ConditionVariable();
         }
         mStopNetLogCompleted.block();
     }
