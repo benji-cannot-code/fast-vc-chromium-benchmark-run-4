693b1974cd63db639dbfe66cc17c1fd83b17c37f
emx <emx@chromium.org>
2017-04-28 11:17:15 -0700

    Fixed some issues with boilerplate.py to make it work on Windows It now support backslashes in file paths and uses LF as EOL (instead of CRLF) even on Windows.
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2846223002
    Cr-Commit-Position: refs/heads/master@{#468063}

--- tools/boilerplate.py
+++ tools/boilerplate.py
@@ -40,7 +40,7 @@ def _GetHeader(filename):
 
 def _CppHeader(filename):
   guard = filename.upper() + '_'
-  for char in '/.+':
+  for char in '/\\.+':
     guard = guard.replace(char, '_')
   return '\n'.join([
     '',
@@ -70,8 +70,13 @@ def _IsIOSFile(filename):
   return False
 
 
+def _FilePathSlashesToCpp(filename):
+  return filename.replace('\\', '/')
+
+
 def _CppImplementation(filename):
-  return '\n#include "' + _RemoveTestSuffix(filename) + '.h"\n'
+  return '\n#include "' + _FilePathSlashesToCpp(_RemoveTestSuffix(filename)) \
+    + '.h"\n'
 
 
 def _ObjCppImplementation(filename):
@@ -95,7 +100,7 @@ def _CreateFile(filename):
   elif filename.endswith('.mm'):
     contents += _ObjCppImplementation(filename)
 
-  fd = open(filename, 'w')
+  fd = open(filename, 'wb')
   fd.write(contents)
   fd.close()
 
