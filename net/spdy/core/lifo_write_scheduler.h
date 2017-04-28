0b94b54db0f31b1fe46f5052b265e43af7f487f5
yasong <yasong@chromium.org>
2017-04-28 05:54:18 -0700

    Enable HTTP/2 to use a LIFO scheduler to schedule write.
    This CL lands server change 152524616 by yasong.
    
    BUG=488484
    
    Review-Url: https://codereview.chromium.org/2844963005
    Cr-Commit-Position: refs/heads/master@{#467971}

--- net/spdy/core/lifo_write_scheduler.h
+++ net/spdy/core/lifo_write_scheduler.h
@@ -70,8 +70,7 @@ class LifoWriteScheduler : public WriteScheduler<StreamIdType> {
   }
 
   bool ShouldYield(StreamIdType stream_id) const override {
-    // stream_id is not necessary to be on the ready list.
-    return stream_id < *ready_streams_.rbegin();
+    return !ready_streams_.empty() && stream_id < *ready_streams_.rbegin();
   }
 
   void MarkStreamReady(StreamIdType stream_id, bool /*add_to_front*/) override;
@@ -107,6 +106,7 @@ void LifoWriteScheduler<StreamIdType>::UnregisterStream(
     return;
   }
   registered_streams_.erase(stream_id);
+  ready_streams_.erase(stream_id);
 }
 
 template <typename StreamIdType>
@@ -162,7 +162,7 @@ void LifoWriteScheduler<StreamIdType>::MarkStreamReady(StreamIdType stream_id,
     return;
   }
   if (ready_streams_.find(stream_id) != ready_streams_.end()) {
-    SPDY_BUG << "Stream already exists in the list";
+    VLOG(1) << "Stream already exists in the list";
     return;
   }
   ready_streams_.insert(stream_id);
@@ -173,7 +173,7 @@ void LifoWriteScheduler<StreamIdType>::MarkStreamNotReady(
     StreamIdType stream_id) {
   auto it = ready_streams_.find(stream_id);
   if (it == ready_streams_.end()) {
-    SPDY_BUG << "Try to remove a stream that is not on list";
+    VLOG(1) << "Try to remove a stream that is not on list";
     return;
   }
   ready_streams_.erase(it);
