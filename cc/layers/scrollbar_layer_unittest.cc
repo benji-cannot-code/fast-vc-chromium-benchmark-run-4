9a38b5835cb7ac194204d0a264f112345cc3ea73
skobes <skobes@chromium.org>
2017-04-27 19:54:17 -0700

    Call SAC::DidScrollUpdate only for compositor-triggered scrolls.
    For everything else we should rely on SAC::DidRequestShowFromMainThread.
    
    Additionally, ensure that show-from-main requests are processed in synchronous
    compositing mode (which commits directly to the active tree).
    
    BUG=606395
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2842553003
    Cr-Commit-Position: refs/heads/master@{#467855}

--- cc/layers/scrollbar_layer_unittest.cc
+++ cc/layers/scrollbar_layer_unittest.cc
@@ -754,6 +754,7 @@ TEST_F(ScrollbarLayerTest, ScrollbarLayerPushProperties) {
       scroll_layer->element_id()));
 
   scroll_layer->SetBounds(gfx::Size(20, 20));
+  scroll_layer->ShowScrollbars();
   scroll_layer->SetForceRenderSurfaceForTesting(true);
   layer_tree_host_->UpdateLayers();
   host_impl->CreatePendingTree();
