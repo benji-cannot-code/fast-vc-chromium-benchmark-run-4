74fe077423cf8353d654e679c91fb1c34fd1b3bf
chfremer <chfremer@chromium.org>
2017-04-28 09:34:12 -0700

    Reland [Mojo Video Capture] Adapt video_capture service to refactored video capture stack
    PatchSet #1 is the state as previously reviewed and reverted.
    PatchSet #2 fixes the reason for the revert.
    
    Description of Fix:
    The test "AccessIsRevokedOnSecondAccess" was flaky because had an incorrect
    expectation on the order in which events from the service arrive. This is fixed
    by removing this expectation.
    
    Original CL description:
    
    This CL is part of the Mojo Video Capture work. For the bigger picture,
    see [1] CL22b.
    
    Note: The video capture service implementation is currently in an incomplete and
    outdated state and its tests are disabled. With the refactoring of the legacy
    video capture stack now being complete, the next goal is to update the service
    implementation and fit it into the refactored stack.
    
    Changes in this CL:
    * Update Mojo interfaces to more closely resemble their native counterparts.
      - video_capture.mojom.Device offers functionality similar to
        VideoCaptureDeviceLauncher + LaunchedVideoCaptureDevice.
      - video_capture.mojom.DeviceFactory offers functionality similar to
        media::VideoCaptureProvider.
      - video_capture.mojom.Receiver offers functionality similar to
        media::VideoFrameReceiver.
    * In the service implementation, use a VideoCaptureSystem instead of a
      VideoCaptureDeviceFactory directly.
    * Add new adapter classes for plumbing between Mojo service and its usage in
      the native code.
    * Re-enable existing video_capture_unittests and add a few more.
    
    BUG=584797
    TEST=
      service_unittests --gtest_filter="*Video*"
      content_unittests --gtest_filter="*Video*"
      content_browsertests --gtest_filter="VideoCaptureBrowserTest.*"
    TBR=emircan@chromium.org,ochang@chromium.org
    
    [1] https://docs.google.com/a/chromium.org/document/d/1Qw7rw1AJy0QHXjha36jZNiEuxsxWslJ_X-zpOhijvI8/edit?usp=sharing
    
    Review-Url: https://codereview.chromium.org/2843333006
    Cr-Commit-Position: refs/heads/master@{#468024}

--- services/video_capture/receiver_mojo_to_media_adapter.cc
+++ services/video_capture/receiver_mojo_to_media_adapter.cc
@@ -5,6 +5,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/video_capture/receiver_mojo_to_media_adapter.h"
 
+#include "mojo/public/cpp/bindings/strong_binding.h"
+
+namespace {
+
+class ScopedAccessPermissionMediaToMojoAdapter
+    : public video_capture::mojom::ScopedAccessPermission {
+ public:
+  ScopedAccessPermissionMediaToMojoAdapter(
+      std::unique_ptr<
+          media::VideoCaptureDevice::Client::Buffer::ScopedAccessPermission>
+          access_permission)
+      : access_permission_(std::move(access_permission)) {}
+
+ private:
+  std::unique_ptr<
+      media::VideoCaptureDevice::Client::Buffer::ScopedAccessPermission>
+      access_permission_;
+};
+
+}  // anonymous namespace
+
 namespace video_capture {
 
 ReceiverMojoToMediaAdapter::ReceiverMojoToMediaAdapter(
@@ -17,7 +38,8 @@ void ReceiverMojoToMediaAdapter::OnNewBufferHandle(
     int buffer_id,
     std::unique_ptr<media::VideoCaptureDevice::Client::Buffer::HandleProvider>
         handle_provider) {
-  NOTIMPLEMENTED();
+  receiver_->OnNewBufferHandle(
+      buffer_id, handle_provider->GetHandleForInterProcessTransit());
 }
 
 void ReceiverMojoToMediaAdapter::OnFrameReadyInBuffer(
@@ -25,9 +47,20 @@ void ReceiverMojoToMediaAdapter::OnFrameReadyInBuffer(
     int frame_feedback_id,
     std::unique_ptr<
         media::VideoCaptureDevice::Client::Buffer::ScopedAccessPermission>
-        buffer_usage_reservation,
+        access_permission,
     media::mojom::VideoFrameInfoPtr frame_info) {
-  NOTIMPLEMENTED();
+  mojom::ScopedAccessPermissionPtr access_permission_proxy;
+  mojo::MakeStrongBinding<mojom::ScopedAccessPermission>(
+      base::MakeUnique<ScopedAccessPermissionMediaToMojoAdapter>(
+          std::move(access_permission)),
+      mojo::MakeRequest(&access_permission_proxy));
+  receiver_->OnFrameReadyInBuffer(buffer_id, frame_feedback_id,
+                                  std::move(access_permission_proxy),
+                                  std::move(frame_info));
+}
+
+void ReceiverMojoToMediaAdapter::OnBufferRetired(int buffer_id) {
+  receiver_->OnBufferRetired(buffer_id);
 }
 
 void ReceiverMojoToMediaAdapter::OnError() {
@@ -43,11 +76,7 @@ void ReceiverMojoToMediaAdapter::OnStarted() {
 }
 
 void ReceiverMojoToMediaAdapter::OnStartedUsingGpuDecode() {
-  NOTIMPLEMENTED();
-}
-
-void ReceiverMojoToMediaAdapter::OnBufferRetired(int buffer_id) {
-  NOTIMPLEMENTED();
+  receiver_->OnStartedUsingGpuDecode();
 }
 
 }  // namespace video_capture
