7ef1b8f3d3866db349b1d7510cc653dae0460cd0
dvadym <dvadym@chromium.org>
2017-04-28 05:53:22 -0700

    Implementation of sync password reuse checking.
    Comparison of hashes of all suffixes of input with the sync password hash is implemented.
    
    BUG=657041
    
    Review-Url: https://codereview.chromium.org/2847743002
    Cr-Commit-Position: refs/heads/master@{#467970}

--- components/password_manager/core/browser/password_reuse_detector.h
+++ components/password_manager/core/browser/password_reuse_detector.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_PASSWORD_REUSE_DETECTOR_H_
 #define COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_PASSWORD_REUSE_DETECTOR_H_
 
+#include <stdint.h>
 #include <map>
 #include <memory>
 #include <set>
@@ -13,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/macros.h"
+#include "base/optional.h"
 #include "base/strings/string16.h"
 #include "components/password_manager/core/browser/password_store_change.h"
 #include "components/password_manager/core/browser/password_store_consumer.h"
@@ -44,7 +46,7 @@ class PasswordReuseDetector : public PasswordStoreConsumer {
   void OnLoginsChanged(const PasswordStoreChangeList& changes);
 
   // Checks that some suffix of |input| equals to a password saved on another
-  // registry controlled domain than |domain|.
+  // registry controlled domain than |domain| or to a sync password.
   // If such suffix is found, |consumer|->OnReuseFound() is called on the same
   // thread on which this method is called.
   // |consumer| should not be null.
@@ -63,6 +65,18 @@ class PasswordReuseDetector : public PasswordStoreConsumer {
   // Add password from |form| to |passwords_|.
   void AddPassword(const autofill::PasswordForm& form);
 
+  // Returns true iff a reuse of a sync password is found. If reuse is found it
+  // is reported to |consumer|.
+  bool CheckSyncPasswordReuse(const base::string16& input,
+                              const std::string& domain,
+                              PasswordReuseDetectorConsumer* consumer);
+
+  // Returns true iff a reuse of a saved password is found. If reuse is found it
+  // is reported to |consumer|.
+  bool CheckSavedPasswordReuse(const base::string16& input,
+                               const std::string& domain,
+                               PasswordReuseDetectorConsumer* consumer);
+
   // Returns the iterator to |passwords_| that corresponds to the longest key in
   // |passwords_| that is a suffix of |input|. Returns passwords_.end() in case
   // when no key in |passwords_| is a prefix of |input|.
@@ -77,6 +91,8 @@ class PasswordReuseDetector : public PasswordStoreConsumer {
   // Number of passwords in |passwords_|, each password is calculated the number
   // of times how many different sites it's saved on.
   int saved_passwords_ = 0;
+
+  base::Optional<uint64_t> sync_password_hash_;
 
   DISALLOW_COPY_AND_ASSIGN(PasswordReuseDetector);
 };
