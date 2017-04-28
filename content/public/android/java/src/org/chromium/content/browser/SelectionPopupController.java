6581d5a7c6ca56adb3948956b9ad4972e97ba138
timav <timav@chromium.org>
2017-04-27 18:43:32 -0700

    Exclude text processing items for insertions and passwords
    The CL http://crrev.com/2801873003 introduced regression: it
    unconditionally added the text processing menu items to the selection
    menu. Originally these items were skipped in case isInsertion() or
    isSelectionPassword() returned true.
    
    This CL restores the old behavior.
    
    BUG=714090
    
    Review-Url: https://codereview.chromium.org/2847793003
    Cr-Commit-Position: refs/heads/master@{#467808}

--- content/public/android/java/src/org/chromium/content/browser/SelectionPopupController.java
+++ content/public/android/java/src/org/chromium/content/browser/SelectionPopupController.java
@@ -456,6 +456,8 @@ public class SelectionPopupController extends ActionModeCallbackHelper {
         mActionMenuDescriptor = createActionMenuDescriptor();
         mActionMenuDescriptor.apply(menu);
 
+        if (isInsertion() || isSelectionPassword()) return;
+
         initializeTextProcessingMenu(menu);
     }
 
