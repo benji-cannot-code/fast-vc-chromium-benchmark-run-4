4296967e8105c7165234648842f0de3f73b20c77
phajdan.jr <phajdan.jr@chromium.org>
2017-04-28 12:20:06 -0700

    Fix a compile error
    std::less requires #include <functional>,
    see e.g. http://en.cppreference.com/w/cpp/utility/functional/less
    
    See the bug for more context.
    
    BUG=716504
    
    Review-Url: https://codereview.chromium.org/2849913002
    Cr-Commit-Position: refs/heads/master@{#468088}

--- base/containers/flat_set.h
+++ base/containers/flat_set.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_CONTAINERS_FLAT_SET_H_
 #define BASE_CONTAINERS_FLAT_SET_H_
 
+#include <functional>
+
 #include "base/containers/flat_tree.h"
 
 namespace base {
