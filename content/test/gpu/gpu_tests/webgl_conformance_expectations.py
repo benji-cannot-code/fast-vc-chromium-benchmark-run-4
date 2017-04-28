8532daa60b75156af894c1c43f2567c59a90d82c
ynovikov <ynovikov@chromium.org>
2017-04-28 10:40:10 -0700

    Update WebGL expectations
    Remove NVIDIA Shield Tablet expectations,
    since they also affect NVIDIA Shield TV.
    
    Mark
    conformance/textures/image_bitmap_from_video/tex-2d-rgb-rgb-unsigned_byte.html
    Flaky on Nexus 5X.
    
    BUG=701929, 716496
    TBR=kbr@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2851773002
    Cr-Commit-Position: refs/heads/master@{#468047}

--- content/test/gpu/gpu_tests/webgl_conformance_expectations.py
+++ content/test/gpu/gpu_tests/webgl_conformance_expectations.py
@@ -624,6 +624,9 @@ class WebGLConformanceExpectations(GpuTestExpectations):
     self.Skip('conformance/glsl/misc/shader-with-non-reserved-words.html',
         ['android', ('qualcomm', 'Adreno (TM) 418'), 'no_passthrough'],
         bug=609883)
+    self.Flaky('conformance/textures/image_bitmap_from_video/' +
+        'tex-2d-rgb-rgb-unsigned_byte.html',
+        ['android', ('qualcomm', 'Adreno (TM) 418')], bug=716496)
     self.Fail('conformance/uniforms/uniform-samplers-test.html',
         ['android', ('qualcomm', 'Adreno (TM) 418'), 'no_passthrough'],
         bug=610951)
@@ -694,16 +697,6 @@ class WebGLConformanceExpectations(GpuTestExpectations):
     self.Fail('conformance/glsl/bugs/constant-precision-qualifier.html',
         ['android', 'android-chromium',
          ('nvidia', 'NVIDIA Tegra')], bug=624621)
-
-    # NVIDIA Shield
-    self.Flaky('conformance/context/' +
-        'context-eviction-with-garbage-collection.html',
-        ['android', ('nvidia', 'NVIDIA Tegra')], bug=701929)
-    self.Flaky('conformance/glsl/misc/glsl-long-variable-names.html',
-        ['android', ('nvidia', 'NVIDIA Tegra')], bug=701929)
-    self.Flaky('conformance/textures/image_bitmap_from_video/' +
-        'tex-2d-rgb-rgb-unsigned_byte.html',
-        ['android', ('nvidia', 'NVIDIA Tegra')], bug=701929)
 
     ############
     # ChromeOS #
