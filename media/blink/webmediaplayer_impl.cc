e74260cbac9a4973c383c0f408d6bd22be2b9fec
tguilbert <tguilbert@chromium.org>
2017-04-27 18:54:43 -0700

    Fix HLS fullscreen gutter color
    On Android, we currently always tell Blink that we will be using
    fullscreen overlays. However, the MediaPlayerRenderer does not support
    them, and does not attempt to use them.
    The result is that Blink avoids drawing any gutter, which causes the
    default background color from the LayerTree to be show instead. When no
    overlay has been created, the default color is white.
    
    This CL updates WMPI::SupportsOverlayFullscreenVideo to only return true
    when we are not using the MediaPlayerRenderer.
    
    BUG=711888
    TEST= manually tested on an HLS and non HLS video.
    
    Review-Url: https://codereview.chromium.org/2840973002
    Cr-Commit-Position: refs/heads/master@{#467821}

--- media/blink/webmediaplayer_impl.cc
+++ media/blink/webmediaplayer_impl.cc
@@ -332,7 +332,7 @@ void WebMediaPlayerImpl::Load(LoadType load_type,
 
 bool WebMediaPlayerImpl::SupportsOverlayFullscreenVideo() {
 #if defined(OS_ANDROID)
-  return true;
+  return !using_media_player_renderer_;
 #else
   return false;
 #endif
