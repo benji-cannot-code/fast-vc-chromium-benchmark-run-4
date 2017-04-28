ce6b495269ba0193e25b8ed9176dac40ddfc41c0
sczs <sczs@chromium.org>
2017-04-28 06:32:39 -0700

    Revert of Follow up from ARC conversion (patchset #2 id:20001 of https://codereview.chromium.org/2851713002/ )
    Reason for revert:
    Revert since its needed to revert https://codereview.chromium.org/2808873002 which broke the roller.
    
    Original issue's description:
    > Follow up from ARC conversion
    >
    > Addressing the comments raised in https://codereview.chromium.org/2808873002/
    >
    > BUG=none
    >
    > Review-Url: https://codereview.chromium.org/2851713002
    > Cr-Commit-Position: refs/heads/master@{#467951}
    > Committed: https://chromium.googlesource.com/chromium/src/+/6273712b527b4e13ec0d854fcd11e0b5e034eb2a
    
    TBR=stkhapugin@chromium.org,sdefresne@chromium.org,gambard@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2849853002
    Cr-Commit-Position: refs/heads/master@{#467978}

--- ios/chrome/browser/ui/toolbar/web_toolbar_controller.mm
+++ ios/chrome/browser/ui/toolbar/web_toolbar_controller.mm
@@ -850,7 +850,7 @@ CGRect RectShiftedDownAndResizedForStatusBar(CGRect rect) {
 - (void)dismissTabHistoryPopup {
   if (!_tabHistoryPopupController)
     return;
-  __block TabHistoryPopupController* tempTHPC = _tabHistoryPopupController;
+  TabHistoryPopupController* tempTHPC = _tabHistoryPopupController;
   [tempTHPC containerView].userInteractionEnabled = NO;
   [tempTHPC dismissAnimatedWithCompletion:^{
     // Unpress the back/forward button by restoring the normal and
@@ -858,7 +858,7 @@ CGRect RectShiftedDownAndResizedForStatusBar(CGRect rect) {
     [self setImagesForNavButton:_backButton withTabHistoryVisible:NO];
     [self setImagesForNavButton:_forwardButton withTabHistoryVisible:NO];
     // Reference tempTHPC so the block retains it.
-    tempTHPC = nil;
+    [tempTHPC self];
   }];
   // reset _tabHistoryPopupController to prevent -applicationDidEnterBackground
   // from posting another kTabHistoryPopupWillHideNotification.
