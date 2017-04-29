4fcc4772b5d52716000a65fdde857687305d9d46
jbudorick <jbudorick@chromium.org>
2017-04-28 20:12:55 -0700

    [Android] Disable CopylessPasteTest.testNoMeta.
    Very flaky on linux_android_rel_ng.
    
    BUG=713895
    TBR=wychen@chromium.org
    
    Review-Url: https://codereview.chromium.org/2850853005
    Cr-Commit-Position: refs/heads/master@{#468219}

--- chrome/android/javatests/src/org/chromium/chrome/browser/CopylessPasteTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/CopylessPasteTest.java
@@ -126,6 +126,7 @@ public class CopylessPasteTest extends ChromeTabbedActivityTestBase {
      */
     @LargeTest
     @Feature({"CopylessPaste"})
+    @DisabledTest(message = "crbug.com/713895")
     public void testNoMeta() throws InterruptedException, TimeoutException {
         loadUrl(mTestServer.getURL(NODATA_PAGE));
         mCallbackHelper.waitForCallback(0);
