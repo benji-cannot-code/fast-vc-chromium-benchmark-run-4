ff31f248b028cdf7ac140bc53e945a7ec83d8019
zhaobin <zhaobin@chromium.org>
2017-04-29 20:29:47 -0700

    [Media Router] Add DialMediaSinkService and DeviceDescriptionService
    Discover process:
      DialMediaSinkService.Start() registers itself with DialRegistry
      DialMediaSinkService.OnDialDeviceEvent() gets invoked when device data comes back, and starts a 3s timer
      Start description fetches for each device
      DeviceDescriptionService.OnDeviceDescriptionFetchComplete() gets invoked when device description comes back.
      Starts an XML parser in utility process to parse device description XML
      DialMediaSinkService.OnDeviceDescriptionAvailable() gets invoked when parsing in utility process finishes
      Create MediaSinkInternal and store it in DialMediaSinkService's sink map
      Invoke DialMediaSinkService.FetchCompleted() when timer expires
    
    Design doc:
    
    https://docs.google.com/document/d/1lJbE4Oc9q1amkWEaD2ZL4mHEdRRlKHvwxUUcJmNC8b8/edit#heading=h.ur85je1upm9u
    
    BUG=687375
    
    Review-Url: https://codereview.chromium.org/2701633002
    Cr-Commit-Position: refs/heads/master@{#468251}

--- chrome/browser/media/router/discovery/dial/dial_device_data.cc
+++ chrome/browser/media/router/discovery/dial/dial_device_data.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/media/router/discovery/dial/dial_device_data.h"
+#include "net/base/ip_address.h"
 
 namespace media_router {
 
@@ -32,7 +33,16 @@ void DialDeviceData::set_device_description_url(const GURL& url) {
 
 // static
 bool DialDeviceData::IsDeviceDescriptionUrl(const GURL& url) {
-  return url.is_valid() && !url.is_empty() && url.SchemeIsHTTPOrHTTPS();
+  if (!url.is_valid() || url.is_empty() || !url.SchemeIsHTTPOrHTTPS())
+    return false;
+
+  net::IPAddress address;
+  if (!net::ParseURLHostnameToAddress(url.host(), &address))
+    return false;
+
+  // TODO(crbug.com/679432): check that this IP address matches the address that
+  // we received the SSDP advertisement from.
+  return address.IsReserved();
 }
 
 bool DialDeviceData::UpdateFrom(const DialDeviceData& new_data) {
--- chrome/common/media_router/discovery/media_sink_service.cc
+++ chrome/common/media_router/discovery/media_sink_service.cc
@@ -3,13 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/media/router/media_sink_service.h"
+#include "chrome/common/media_router/discovery/media_sink_service.h"
 
 namespace media_router {
 
 MediaSinkService::MediaSinkService(
-    const OnSinksDiscoveredCallback& sinks_discovered_callback)
-    : sinks_discovered_callback_(sinks_discovered_callback) {}
+    const OnSinksDiscoveredCallback& sink_discovery_callback)
+    : sink_discovery_callback_(sink_discovery_callback) {}
 
 MediaSinkService::~MediaSinkService() = default;
 
