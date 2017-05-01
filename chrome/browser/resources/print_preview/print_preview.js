08c53271815802d9e49d5c2625582bc455251a14
rbpotter <rbpotter@chromium.org>
2017-05-01 12:08:20 -0700

    Fix Print Preview failure state and add test
    Prevent print preview error state from being unrecoverable by resetting
    the state when the user selects a new printer. Add test to check that
    the state resets and to check that the response to the INVALID_SETTINGS
    event is correct.
    
    BUG=708834
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2849733002
    Cr-Commit-Position: refs/heads/master@{#468379}

--- chrome/browser/resources/print_preview/print_preview.js
+++ chrome/browser/resources/print_preview/print_preview.js
@@ -935,7 +935,6 @@ cr.define('print_preview', function() {
       this.uiState_ = PrintPreview.UiState_.ERROR;
       this.isPreviewGenerationInProgress_ = false;
       this.printHeader_.isPrintButtonEnabled = false;
-      console.error('Invalid settings error reported from native layer');
       this.previewArea_.cancelTimeout();
       this.previewArea_.showCustomMessage(
           loadTimeData.getString('invalidPrinterSettings'));
@@ -1201,6 +1200,10 @@ cr.define('print_preview', function() {
         setIsVisible($('system-dialog-link'),
                      this.shouldShowSystemDialogLink_());
       }
+      // Reset if we had a bad settings fetch since the user selected a new
+      // printer.
+      if (this.uiState_ == PrintPreview.UiState_.ERROR)
+        this.uiState_ = PrintPreview.UiState_.READY;
       if (this.destinationStore_.selectedDestination &&
           this.isInKioskAutoPrintMode_) {
         this.onPrintButtonClick_();
