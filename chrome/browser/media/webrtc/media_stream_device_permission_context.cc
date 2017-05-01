1babfa3ef87e222c650b65378cc4a1409a1267d0
raymes <raymes@chromium.org>
2017-04-30 17:50:41 -0700

    Make media permission requests go through the PermissionManager
    This CL adds a feature flag which, when enabled, causes media requests to call
    into PermissionManager::RequestPermissions in order to display a permission
    prompt (among other things). The feature flag is added because this results in
    the UI changing, and also won't work on Android until we have unified the
    permission request codepaths (see https://crbug.com/606138).
    
    BUG=596786
    
    Review-Url: https://codereview.chromium.org/2829123003
    Cr-Commit-Position: refs/heads/master@{#468265}

--- chrome/browser/media/webrtc/media_stream_device_permission_context.cc
+++ chrome/browser/media/webrtc/media_stream_device_permission_context.cc
@@ -4,8 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/media/webrtc/media_stream_device_permission_context.h"
+#include "base/feature_list.h"
 #include "chrome/browser/media/webrtc/media_stream_device_permissions.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/common/pref_names.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/content_settings/core/common/content_settings.h"
@@ -23,14 +25,18 @@ MediaStreamDevicePermissionContext::MediaStreamDevicePermissionContext(
 
 MediaStreamDevicePermissionContext::~MediaStreamDevicePermissionContext() {}
 
-void MediaStreamDevicePermissionContext::RequestPermission(
+void MediaStreamDevicePermissionContext::DecidePermission(
     content::WebContents* web_contents,
     const PermissionRequestID& id,
-    const GURL& requesting_frame,
+    const GURL& requesting_origin,
+    const GURL& embedding_origin,
     bool user_gesture,
     const BrowserPermissionCallback& callback) {
-  NOTREACHED() << "RequestPermission is not implemented";
-  callback.Run(CONTENT_SETTING_BLOCK);
+  DCHECK(base::FeatureList::IsEnabled(
+      features::kUsePermissionManagerForMediaRequests));
+  PermissionContextBase::DecidePermission(web_contents, id, requesting_origin,
+                                          embedding_origin, user_gesture,
+                                          callback);
 }
 
 ContentSetting MediaStreamDevicePermissionContext::GetPermissionStatusInternal(
