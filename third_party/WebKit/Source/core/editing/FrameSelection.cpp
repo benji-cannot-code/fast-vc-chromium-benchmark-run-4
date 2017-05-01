ff87514498937980fd3a647e6f08b8e3e3b1cf36
xiaochengh <xiaochengh@chromium.org>
2017-04-30 22:42:36 -0700

    Make FrameSelection::SelectAll bail if 'selectstart' detaches frame
    BUG=715387
    TEST=LayoutTests/editing/selection/selectstart_detaches_frame.html
    
    Review-Url: https://codereview.chromium.org/2848993002
    Cr-Commit-Position: refs/heads/master@{#468292}

--- third_party/WebKit/Source/core/editing/FrameSelection.cpp
+++ third_party/WebKit/Source/core/editing/FrameSelection.cpp
@@ -659,6 +659,11 @@ void FrameSelection::SelectAll() {
     if (select_start_target->DispatchEvent(Event::CreateCancelableBubble(
             EventTypeNames::selectstart)) != DispatchEventResult::kNotCanceled)
       return;
+    // The frame may be detached due to selectstart event.
+    if (!IsAvailable()) {
+      // Reached by editing/selection/selectstart_detach_frame.html
+      return;
+    }
     // |root| may be detached due to selectstart event.
     if (!root->isConnected() || expected_document != root->GetDocument())
       return;
