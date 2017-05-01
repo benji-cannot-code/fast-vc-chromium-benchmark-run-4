1f3807c887dbb8ad61b1cd33e6c662397285a307
suzyh <suzyh@chromium.org>
2017-04-30 22:32:29 -0700

    Revert of CSS Motion Path: delete implementation of motion-offset property (patchset #2 id:40001 of https://codereview.chromium.org/2841863005/ )
    Reason for revert:
    Speculatively reverting due to build failure on Mac https://build.chromium.org/p/chromium/builders/Mac/builds/26841
    
    Original issue's description:
    > CSS Motion Path: delete implementation of motion-offset property
    >
    > The 'motion-offset' property has been replaced by 'offset-distance'.
    >
    > 'motion-offset' is among the properties disabled in M58.
    >
    > Intent to Remove: https://groups.google.com/a/chromium.org/d/msg/blink-dev/o1C5NzGf9Q0/sbaLF1MbAgAJ
    >
    > BUG=711937
    >
    > Review-Url: https://codereview.chromium.org/2841863005
    > Cr-Commit-Position: refs/heads/master@{#468287}
    > Committed: https://chromium.googlesource.com/chromium/src/+/612ac411762985a97166c4144ff0d049716a80db
    
    TBR=bugsnash@chromium.org,meade@chromium.org,ericwilligers@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=711937
    
    Review-Url: https://codereview.chromium.org/2856513002
    Cr-Commit-Position: refs/heads/master@{#468291}

--- third_party/WebKit/Source/core/frame/Deprecation.cpp
+++ third_party/WebKit/Source/core/frame/Deprecation.cpp
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 enum Milestone {
+  M58,
   M59,
   M60,
   M61,
@@ -27,6 +28,8 @@ const char* milestoneString(Milestone milestone) {
   // https://www.chromium.org/developers/calendar
 
   switch (milestone) {
+    case M58:
+      return "M58, around April 2017";
     case M59:
       return "M59, around June 2017";
     case M60:
@@ -115,10 +118,13 @@ void Deprecation::WarnOnDeprecatedProperties(
 }
 
 String Deprecation::DeprecationMessage(CSSPropertyID unresolved_property) {
-  // TODO: Add a switch here when there are properties that we intend to
-  // deprecate.
-  // Returning an empty string for now.
-  return g_empty_string;
+  switch (unresolved_property) {
+    case CSSPropertyAliasMotionOffset:
+      return replacedWillBeRemoved("motion-offset", "offset-distance", M58,
+                                   "6390764217040896");
+    default:
+      return g_empty_string;
+  }
 }
 
 void Deprecation::CountDeprecation(const LocalFrame* frame,
