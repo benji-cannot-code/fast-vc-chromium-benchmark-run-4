53b90bf60350e7aff5abf3b08ac432108c7c592d
ramyasharma <ramyasharma@chromium.org>
2017-05-01 00:03:52 -0700

    Introduces snackbar enum, & other snackbar classes on android for translate user actions.
    CL#1 This snackbar will be used to convey to the user the result
    of any action performed on the Translate infobar.
    
    BUG=713514
    TBR=groby@chromium.org
    
    Review-Url: https://codereview.chromium.org/2838833003
    Cr-Commit-Position: refs/heads/master@{#468302}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/infobar/TranslateSnackbarController.java
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.infobar;
+
+import org.chromium.chrome.browser.snackbar.SnackbarManager.SnackbarController;
+
+/**
+ * The controller for translate UI snackbars.
+ */
+class TranslateSnackbarController implements SnackbarController {
+    @Override
+    public void onDismissNoAction(Object actionData) {
+        // No action.
+    }
+
+    @Override
+    public void onAction(Object actionData) {
+        nativeToggleTranslateOption(((Integer) actionData).intValue());
+    }
+
+    private static native void nativeToggleTranslateOption(int type);
+};
