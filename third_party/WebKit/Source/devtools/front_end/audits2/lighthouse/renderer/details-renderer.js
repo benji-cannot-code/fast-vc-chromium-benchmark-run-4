bf81a92b6cf1e1469f40992f3955e87a3fec54ca
paulirish <paulirish@chromium.org>
2017-05-01 15:54:30 -0700

    Roll Lighthouse & add error reporting
    BUG=663085
    
    Review-Url: https://codereview.chromium.org/2856653002
    Cr-Commit-Position: refs/heads/master@{#468468}

--- third_party/WebKit/Source/devtools/front_end/audits2/lighthouse/renderer/details-renderer.js
+++ third_party/WebKit/Source/devtools/front_end/audits2/lighthouse/renderer/details-renderer.js
@@ -16,28 +16,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 'use strict';
 
+/* globals self */
+
 class DetailsRenderer {
   /**
    * @param {!DOM} dom
    */
   constructor(dom) {
+    /** @private {!DOM} */
     this._dom = dom;
   }
 
   /**
-   * @param {(!DetailsRenderer.DetailsJSON|!DetailsRenderer.CardsDetailsJSON)} details
+   * @param {!DetailsRenderer.DetailsJSON} details
    * @return {!Element}
    */
   render(details) {
     switch (details.type) {
       case 'text':
         return this._renderText(details);
-      case 'block':
-        return this._renderBlock(details);
       case 'cards':
         return this._renderCards(/** @type {!DetailsRenderer.CardsDetailsJSON} */ (details));
       case 'list':
-        return this._renderList(details);
+        return this._renderList(/** @type {!DetailsRenderer.ListDetailsJSON} */ (details));
       default:
         throw new Error(`Unknown type: ${details.type}`);
     }
@@ -54,20 +55,7 @@ class DetailsRenderer {
   }
 
   /**
-   * @param {!DetailsRenderer.DetailsJSON} block
-   * @return {!Element}
-   */
-  _renderBlock(block) {
-    const element = this._dom.createElement('div', 'lh-block');
-    const items = block.items || [];
-    for (const item of items) {
-      element.appendChild(this.render(item));
-    }
-    return element;
-  }
-
-  /**
-   * @param {!DetailsRenderer.DetailsJSON} list
+   * @param {!DetailsRenderer.ListDetailsJSON} list
    * @return {!Element}
    */
   _renderList(list) {
@@ -79,8 +67,7 @@ class DetailsRenderer {
     }
 
     const itemsElem = this._dom.createElement('div', 'lh-list__items');
-    const items = list.items || [];
-    for (const item of items) {
+    for (const item of list.items) {
       itemsElem.appendChild(this.render(item));
     }
     element.appendChild(itemsElem);
@@ -120,22 +107,30 @@ class DetailsRenderer {
 
 if (typeof module !== 'undefined' && module.exports) {
   module.exports = DetailsRenderer;
+} else {
+  self.DetailsRenderer = DetailsRenderer;
 }
 
 /**
  * @typedef {{
  *     type: string,
- *     text: (string|undefined),
- *     header: (!DetailsRenderer.DetailsJSON|undefined),
- *     items: (!Array<!DetailsRenderer.DetailsJSON>|undefined)
+ *     text: (string|undefined)
  * }}
  */
 DetailsRenderer.DetailsJSON; // eslint-disable-line no-unused-expressions
 
+/**
+ * @typedef {{
+ *     type: string,
+ *     header: ({text: string}|undefined),
+ *     items: !Array<{type: string, text: (string|undefined)}>
+ * }}
+ */
+DetailsRenderer.ListDetailsJSON; // eslint-disable-line no-unused-expressions
+
 /** @typedef {{
  *     type: string,
- *     text: string,
- *     header: !DetailsRenderer.DetailsJSON,
+ *     header: ({text: string}|undefined),
  *     items: !Array<{title: string, value: string, snippet: (string|undefined), target: string}>
  * }}
  */
