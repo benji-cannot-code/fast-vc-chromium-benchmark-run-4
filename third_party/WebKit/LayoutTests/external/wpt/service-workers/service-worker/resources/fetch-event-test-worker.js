45a68d7eb4952c8011a65a100e0ae0153f5df9ed
mike <mike@mikepennisi.com>
2017-05-01 10:25:21 -0700

    Upstream service worker `fetch` tests to WPT
    Reconcile the remaining Service Worker tests that concern the Fetch
    standard. As noted below, some tests must be persisted in the Chromium
    project due to unresolved timeout issues in the build environment (see
    https://crbug.com/658997).
    
    **fetch-event-async-respond-with**
    
    The Chromium-specific version of this test is almost identical to the
    version provided by the Web Platform Test suite, though it suffers from
    timeout issues in the build environment.
    
    Rename the Chromium-specific version to document its deprecated status
    and improve the upstream version to defer frame removal and omit
    unnecessary resource.
    
    **fetch-event-network-error**
    
    The upstream version of this test is almost identical to the
    Chromium-specific version.
    
    The Chromium-specific version of this test is almost identical to the
    version available upstream. The upstream version is preferable because
    it contains an additional assertion not found in the Chromium version.
    
    Remove the Chromium-specific version of this test, and update the
    upstream version to omit an unnecessary resource.
    
    **fetch-event-respond-with-argument**
    
    Re-locate Chromium-specific test to the Web Platform Test suite
    directory so that it may be included in that project.
    
    **fetch-event-respond-with-stops-propagation**
    
    The Chromium-specific version of this test is almost identical to the
    version provided by the Web Platform Test suite, though it suffers from
    timeout issues in the build environment.
    
    Rename the Chromium-specific version to reflect its deprecated status,
    and improve the upstream version to defer frame removal and omit
    unnecessary resource.
    
    **fetch-event**
    
    Remove sub-tests from the Chromium-specific version that are present in
    the upstream version. Rename the test file to reflect the
    implementation-specific nature of the test.
    
    Extend the upstream version with a relaxed version of Chromium's
    sub-test for request headers.
    
    In both versions, formalize tear down logic to ensure consistent frame
    removal and subsequent client de-registration.
    
    BUG=688116, 658997
    R=mek@chromium.org
    
    Review-Url: https://codereview.chromium.org/2836233002
    Cr-Commit-Position: refs/heads/master@{#468341}

--- third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/fetch-event-test-worker.js
+++ third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/fetch-event-test-worker.js
@@ -114,6 +114,11 @@ function handleIntegrity(event) {
   event.respondWith(new Response(event.request.integrity));
 }
 
+function handleHeaders(event) {
+  const headers = Array.from(event.request.headers);
+  event.respondWith(new Response(JSON.stringify(headers)));
+}
+
 self.addEventListener('fetch', function(event) {
     var url = event.request.url;
     var handlers = [
@@ -133,6 +138,7 @@ self.addEventListener('fetch', function(event) {
       { pattern: '?cache', fn: handleCache },
       { pattern: '?eventsource', fn: handleEventSource },
       { pattern: '?integrity', fn: handleIntegrity },
+      { pattern: '?headers', fn: handleHeaders },
     ];
 
     var handler = null;
