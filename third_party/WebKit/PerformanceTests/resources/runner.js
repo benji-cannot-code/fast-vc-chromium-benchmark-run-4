1d19be08d8deaff5fd3e632cb38dac5047c8e516
nednguyen <nednguyen@google.com>
2017-05-01 09:59:10 -0700

    [blink_perf] Add log messages for tracing metrics in blink_perf
    We add the following log messages:
    * Error message when one of tracingCategories & traceEventsToMeasure is specified
    but not the other.
    * Hint users to use tools/perf/run_benchmark to get tracing based metrics
    
    BUG=701059
    
    Review-Url: https://codereview.chromium.org/2856463003
    Cr-Commit-Position: refs/heads/master@{#468332}

--- third_party/WebKit/PerformanceTests/resources/runner.js
+++ third_party/WebKit/PerformanceTests/resources/runner.js
@@ -151,6 +151,18 @@ if (window.testRunner) {
             return;
         }
         currentTest = test;
+
+        if (test.tracingCategories && !test.traceEventsToMeasure) {
+            PerfTestRunner.logFatalError("test's tracingCategories is " +
+                "specified but test's traceEventsToMeasure is empty");
+            return;
+        }
+
+        if (test.traceEventsToMeasure && !test.tracingCategories) {
+            PerfTestRunner.logFatalError("test's traceEventsToMeasure is " +
+                "specified but test's tracingCategories is empty");
+            return;
+        }
         iterationCount = test.iterationCount || (window.testRunner ? 5 : 20);
         if (test.warmUpCount && test.warmUpCount > 0)
             completedIterations = -test.warmUpCount;
@@ -166,6 +178,13 @@ if (window.testRunner) {
                 scheduleNextRun(scheduler, runner);
             });
         } else if (runner) {
+            if (test.tracingCategories && !(window.testRuner &&
+                  window.testRunner.supportTracing)) {
+                PerfTestRunner.log("Tracing based metrics are specified but " +
+                    "tracing is not supported on this platform. To get those " +
+                    "metrics from this test, you can run the test using " +
+                    "tools/perf/run_benchmarks script.");
+            }
             scheduleNextRun(scheduler, runner);
         }
     }
