7d39ee5770c556e8afb6012bed822ce74fb3b75d
dtapuska <dtapuska@chromium.org>
2017-05-01 15:45:25 -0700

    Add a BeginMainFrame fallback to MainThreadEventQueue.
    I believe we aren't always getting the begin main frame signal leading
    to an increase in hung renderers when rAF mouse aligned input is
    enabled. Test my theory by adding a timer that fires after a
    significantly long time (5s) if we are waiting for a signal but
    haven't received it.
    
    BUG=703344
    
    Review-Url: https://codereview.chromium.org/2846333002
    Cr-Commit-Position: refs/heads/master@{#468464}

--- content/renderer/input/main_thread_event_queue.h
+++ content/renderer/input/main_thread_event_queue.h
@@ -119,6 +119,11 @@ class CONTENT_EXPORT MainThreadEventQueue
   bool IsRafAlignedInputDisabled() const;
   bool IsRafAlignedEvent(
       const std::unique_ptr<MainThreadEventQueueTask>& item) const;
+  void RafFallbackTimerFired();
+
+  void set_use_raf_fallback_timer(bool use_timer) {
+    use_raf_fallback_timer_ = use_timer;
+  }
 
   friend class QueuedWebInputEvent;
   friend class MainThreadEventQueueTest;
@@ -150,6 +155,8 @@ class CONTENT_EXPORT MainThreadEventQueue
 
   scoped_refptr<base::SingleThreadTaskRunner> main_task_runner_;
   blink::scheduler::RendererScheduler* renderer_scheduler_;
+  base::OneShotTimer raf_fallback_timer_;
+  bool use_raf_fallback_timer_;
 
   DISALLOW_COPY_AND_ASSIGN(MainThreadEventQueue);
 };
