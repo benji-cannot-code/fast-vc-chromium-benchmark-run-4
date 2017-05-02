c5c9ec8c655f6e172c5aabcf579767ae37925e8a
erikchen <erikchen@chromium.org>
2017-05-02 12:05:01 -0700

    Replace base::SharedMemory read-only methods with GetReadOnlyHandle.
    This CL is a refactor with no intended behavior change.
    
    The previous methods ShareReadOnlyToProcess and GiveReadOnlyToProcess could be
    used to directly add a handle to the destination process, which is a bad
    practice. All callers already avoid doing this. The new method GetReadOnlyHandle
    returns a read-only handle in the current process which the IPC subsystem will
    transport into the destination process.
    
    BUG=716206
    TBR=brettw@chromium.org
    
    Review-Url: https://codereview.chromium.org/2845113005
    Cr-Commit-Position: refs/heads/master@{#468731}

--- base/memory/shared_memory_mac_unittest.cc
+++ base/memory/shared_memory_mac_unittest.cc
@@ -395,7 +395,7 @@ TEST_F(SharedMemoryMacMultiProcessTest, MachShareToProcess) {
   EXPECT_EQ(active_name_count, GetActiveNameCount());
 }
 
-// Tests that the method ShareReadOnlyToProcess() creates a memory object that
+// Tests that the method GetReadOnlyHandle() creates a memory object that
 // is read only.
 TEST_F(SharedMemoryMacMultiProcessTest, MachShareToProcessReadonly) {
   std::unique_ptr<SharedMemory> shared_memory(
@@ -410,8 +410,7 @@ TEST_F(SharedMemoryMacMultiProcessTest, MachShareToProcessReadonly) {
   ASSERT_EQ(VM_PROT_READ | VM_PROT_WRITE, max_prot);
 
   // Make a new memory object.
-  SharedMemoryHandle shm2;
-  ASSERT_TRUE(shared_memory->ShareReadOnlyToProcess(GetCurrentProcId(), &shm2));
+  SharedMemoryHandle shm2 = shared_memory->GetReadOnlyHandle();
   ASSERT_TRUE(shm2.IsValid());
 
   // Mapping with |readonly| set to |false| should fail.
@@ -436,7 +435,7 @@ TEST_F(SharedMemoryMacMultiProcessTest, MachShareToProcessReadonly) {
   ASSERT_DEATH(memset(shared_memory2.memory(), 'b', s_memory_size), "");
 }
 
-// Tests that the method ShareReadOnlyToProcess() doesn't leak.
+// Tests that the method GetReadOnlyHandle() doesn't leak.
 TEST_F(SharedMemoryMacMultiProcessTest, MachShareToProcessReadonlyLeak) {
   mach_msg_type_number_t active_name_count = GetActiveNameCount();
 
@@ -444,9 +443,7 @@ TEST_F(SharedMemoryMacMultiProcessTest, MachShareToProcessReadonlyLeak) {
     std::unique_ptr<SharedMemory> shared_memory(
         CreateSharedMemory(s_memory_size));
 
-    SharedMemoryHandle shm2;
-    ASSERT_TRUE(
-        shared_memory->ShareReadOnlyToProcess(GetCurrentProcId(), &shm2));
+    SharedMemoryHandle shm2 = shared_memory->GetReadOnlyHandle();
     ASSERT_TRUE(shm2.IsValid());
 
     // Intentionally map with |readonly| set to |false|.
