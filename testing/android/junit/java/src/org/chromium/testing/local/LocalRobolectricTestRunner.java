325267700345d170f462217d4acb237ad0846f24
mikecase <mikecase@chromium.org>
2017-05-02 16:47:35 -0700

    (Reland x2) Expose resources in Robolectric/JUnit tests.
    Will let people use Android resources in Robolectric tests.
    To use, specify package_name GN variable in junit_binary
    targets with your apps package name.
    
    This change will also (basically) require that you use the
    generated wrappers from out_dir/bin/run_<suite name> to run
    the tests since GN will be generating many arguments to
    pass along to the test runner.
    
    BUG=693573
    
    Review-Url: https://codereview.chromium.org/2842603002
    Cr-Commit-Position: refs/heads/master@{#468822}

--- testing/android/junit/java/src/org/chromium/testing/local/LocalRobolectricTestRunner.java
+++ testing/android/junit/java/src/org/chromium/testing/local/LocalRobolectricTestRunner.java
@@ -15,6 +15,7 @@ import org.robolectric.internal.ManifestFactory;
  */
 public class LocalRobolectricTestRunner extends RobolectricTestRunner {
     private static final int DEFAULT_SDK = 25;
+    private static final String DEFAULT_PACKAGE_NAME = "org.robolectric.default";
 
     public LocalRobolectricTestRunner(Class<?> testClass) throws InitializationError {
         super(testClass);
@@ -22,7 +23,10 @@ public class LocalRobolectricTestRunner extends RobolectricTestRunner {
 
     @Override
     protected Config buildGlobalConfig() {
-        return new Config.Builder().setSdk(DEFAULT_SDK).build();
+        String packageName =
+                System.getProperty("chromium.robolectric.package.name", DEFAULT_PACKAGE_NAME);
+
+        return new Config.Builder().setSdk(DEFAULT_SDK).setPackageName(packageName).build();
     }
 
     @Override
