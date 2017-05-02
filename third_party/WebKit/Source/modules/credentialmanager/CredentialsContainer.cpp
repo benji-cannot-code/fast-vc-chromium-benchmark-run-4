bd9fc508ede39132b356bc99543fe1439cd814a9
jdoerrie <jdoerrie@chromium.org>
2017-05-02 02:02:21 -0700

    Speed up CredentialsContainer::get for federations
    This change removes two unnecessary copies in CredentialsContainer::get
    when federations are present in the CredentialRequestOptions.
    
    BUG=400674
    
    Review-Url: https://codereview.chromium.org/2848743003
    Cr-Commit-Position: refs/heads/master@{#468585}

--- third_party/WebKit/Source/modules/credentialmanager/CredentialsContainer.cpp
+++ third_party/WebKit/Source/modules/credentialmanager/CredentialsContainer.cpp
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/credentialmanager/CredentialsContainer.h"
 
 #include <memory>
+#include <utility>
 #include "bindings/core/v8/Dictionary.h"
 #include "bindings/core/v8/ScriptPromise.h"
 #include "bindings/core/v8/ScriptPromiseResolver.h"
@@ -168,17 +169,10 @@ ScriptPromise CredentialsContainer::get(
 
   Vector<KURL> providers;
   if (options.hasFederated() && options.federated().hasProviders()) {
-    // TODO(mkwst): CredentialRequestOptions::federated() needs to return a
-    // reference, not a value.  Because it returns a temporary value now, a for
-    // loop that directly references the value generates code that holds a
-    // reference to a value that no longer exists by the time the loop starts
-    // looping. In order to avoid this crazyness for the moment, we're making a
-    // copy of the vector. https://crbug.com/587088
-    const Vector<String> provider_strings = options.federated().providers();
-    for (const auto& string : provider_strings) {
+    for (const auto& string : options.federated().providers()) {
       KURL url = KURL(KURL(), string);
       if (url.IsValid())
-        providers.push_back(url);
+        providers.push_back(std::move(url));
     }
   }
 
