f0e538748a47460d72b8af1d301d4a03b6319d76
svillar <svillar@igalia.com>
2017-05-02 04:32:22 -0700

    [css-grid] Wipe SizingOperation out
    crrev.com/2823183003 was the first step towards removing the SizingOperation
    enum which was added in the past as a convenient way to modify the track
    sizing algorithm behaviour depending on whether the size was
    indefinite/definite but also on whether we were computing the grid intrinsic
    size or doing a layout. As mentioned in the CL above that was a mistake
    because indefinite sizes do not always correspond to preferred widths
    computations.
    
    Now that we have the algorithm in a separate class with different strategies
    the existence of that enum is no longer required. We can handle the
    remaining code path using SizingOperation just by using the algorithm
    strategies.
    
    No new tests required as this does not imply a change in the behaviour.
    
    Review-Url: https://codereview.chromium.org/2842413003
    Cr-Commit-Position: refs/heads/master@{#468599}

--- third_party/WebKit/Source/core/layout/GridTrackSizingAlgorithm.h
+++ third_party/WebKit/Source/core/layout/GridTrackSizingAlgorithm.h
@@ -22,8 +22,6 @@ class Grid;
 class GridTrackSizingAlgorithmStrategy;
 class LayoutGrid;
 
-enum SizingOperation { kTrackSizing, kIntrinsicSizeComputation };
-
 enum TrackSizeComputationPhase {
   kResolveIntrinsicMinimums,
   kResolveContentBasedMinimums,
@@ -87,7 +85,6 @@ class GridTrackSizingAlgorithm final {
   // the algorithm.
   void Setup(GridTrackSizingDirection,
              size_t num_tracks,
-             SizingOperation,
              Optional<LayoutUnit> available_space,
              Optional<LayoutUnit> free_space);
   void Run();
@@ -191,7 +188,6 @@ class GridTrackSizingAlgorithm final {
   Vector<size_t> flexible_sized_tracks_index_;
 
   GridTrackSizingDirection direction_;
-  SizingOperation sizing_operation_;
 
   Grid& grid_;
 
