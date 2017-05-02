435e8414d47137f7f31702b60f826f6cf0f78cec
fmalita <fmalita@chromium.org>
2017-05-02 10:34:17 -0700

    More missing Skia includes
    TBR=mtklein@chromium.org
    
    Review-Url: https://codereview.chromium.org/2854163002
    Cr-Commit-Position: refs/heads/master@{#468700}

--- skia/tools/filter_fuzz_stub/filter_fuzz_stub.cc
+++ skia/tools/filter_fuzz_stub/filter_fuzz_stub.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/logging.h"
 #include "base/test/test_discardable_memory_allocator.h"
+#include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkFlattenableSerialization.h"
 #include "third_party/skia/include/core/SkImageFilter.h"
