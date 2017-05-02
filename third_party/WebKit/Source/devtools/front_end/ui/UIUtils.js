f48059daf5fb3abd14bb89375c1d5de7e502eadb
alph <alph@chromium.org>
2017-05-02 16:37:08 -0700

    DevTools: Show screenshots on the main flamechart
    The patch puts the screenshots on the frame bar and makes it expandable.
    
    BUG=705054
    
    Review-Url: https://codereview.chromium.org/2852173002
    Cr-Commit-Position: refs/heads/master@{#468819}

--- third_party/WebKit/Source/devtools/front_end/ui/UIUtils.js
+++ third_party/WebKit/Source/devtools/front_end/ui/UIUtils.js
@@ -2015,6 +2015,14 @@ UI.loadImage = function(url) {
   });
 };
 
+/**
+ * @param {?string} data
+ * @return {!Promise<?Image>}
+ */
+UI.loadImageFromData = function(data) {
+  return data ? UI.loadImage('data:image/jpg;base64,' + data) : Promise.resolve(null);
+};
+
 /** @type {!UI.ThemeSupport} */
 UI.themeSupport;
 
