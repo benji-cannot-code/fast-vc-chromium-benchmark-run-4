1c5619629ed0b0c46d767bf62d33bf5c18f67ccf
acondor <acondor@google.com>
2017-05-02 12:28:06 -0700

    Implementing Binary glTF reader for the VR controller model
    The binary glTF format enables us to deliver 3D models on a single file with minimal size.
    
    Binary glTF Specification in: https://github.com/KhronosGroup/glTF/tree/master/extensions/Khronos/KHR_binary_glTF
    
    BUG=644562
    
    Review-Url: https://codereview.chromium.org/2852533004
    Cr-Original-Commit-Position: refs/heads/master@{#468198}
    Committed: https://chromium.googlesource.com/chromium/src/+/b416391e0ae62217afb7ea5ffeb04ab2640a96ed
    Review-Url: https://codereview.chromium.org/2852533004
    Cr-Commit-Position: refs/heads/master@{#468739}

--- chrome/browser/android/vr_shell/gltf_parser_unittest.cc
+++ chrome/browser/android/vr_shell/gltf_parser_unittest.cc
@@ -18,17 +18,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace vr_shell {
 
-class GltfParserTest : public testing::Test {
+class DataDrivenTest : public testing::Test {
  protected:
   static void SetUpTestCase() { test::RegisterPathProvider(); }
 
   void SetUp() override { PathService::Get(test::DIR_TEST_DATA, &data_dir_); }
 
   base::FilePath data_dir_;
+};
 
+class GltfParserTest : public DataDrivenTest {
+ protected:
   std::unique_ptr<base::DictionaryValue> Deserialize(
       const base::FilePath& gltf_path);
 };
+
+class BinaryGltfParserTest : public DataDrivenTest {};
 
 std::unique_ptr<base::DictionaryValue> GltfParserTest::Deserialize(
     const base::FilePath& gltf_path) {
@@ -48,7 +53,7 @@ TEST_F(GltfParserTest, Parse) {
   GltfParser parser;
   std::vector<std::unique_ptr<gltf::Buffer>> buffers;
 
-  auto gltf_model = parser.Parse(*asset, &buffers);
+  std::unique_ptr<gltf::Asset> gltf_model = parser.Parse(*asset, &buffers);
   EXPECT_TRUE(gltf_model);
   EXPECT_EQ(1u, buffers.size());
 
@@ -140,7 +145,7 @@ TEST_F(GltfParserTest, ParseExternal) {
   GltfParser parser;
   std::vector<std::unique_ptr<gltf::Buffer>> buffers;
 
-  auto gltf_model = parser.Parse(gltf_path, &buffers);
+  std::unique_ptr<gltf::Asset> gltf_model = parser.Parse(gltf_path, &buffers);
   EXPECT_NE(nullptr, gltf_model);
   EXPECT_EQ(1u, buffers.size());
   const gltf::Buffer* buffer = buffers[0].get();
@@ -156,6 +161,20 @@ TEST_F(GltfParserTest, ParseExternalNoPath) {
 
   // Parsing fails when no path is provided.
   EXPECT_EQ(nullptr, parser.Parse(*asset, &buffers));
+}
+
+TEST_F(BinaryGltfParserTest, ParseBinary) {
+  std::string data;
+  EXPECT_TRUE(base::ReadFileToString(data_dir_.Append("sample.glb"), &data));
+  base::StringPiece glb_data(data);
+  std::vector<std::unique_ptr<gltf::Buffer>> buffers;
+  std::unique_ptr<gltf::Asset> asset =
+      BinaryGltfParser::Parse(glb_data, &buffers);
+  EXPECT_TRUE(asset);
+  EXPECT_EQ(1u, buffers.size());
+  const gltf::BufferView* buffer_view = asset->GetBufferView(0);
+  EXPECT_TRUE(buffer_view);
+  EXPECT_EQ(0, buffer_view->buffer);
 }
 
 }  // namespace vr_shell
