10cc8b5fbee768b666e6464c40f8b9d1b6a3023c
bashi <bashi@chromium.org>
2017-05-01 22:14:36 -0700

    Don't store ScriptValue in PopStateEvent
    Use TraceWrapperV8Reference instead. This CL doesn't remove
    custom getter of PopStateEvent.state as more work will be
    needed to remove custom bindings.
    
    BUG=501866
    
    Review-Url: https://codereview.chromium.org/2850133002
    Cr-Commit-Position: refs/heads/master@{#468565}

--- third_party/WebKit/Source/core/events/PopStateEvent.h
+++ third_party/WebKit/Source/core/events/PopStateEvent.h
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef PopStateEvent_h
 #define PopStateEvent_h
 
+#include "bindings/core/v8/DOMWrapperWorld.h"
+#include "bindings/core/v8/TraceWrapperV8Reference.h"
 #include "core/events/Event.h"
 #include "core/events/PopStateEventInit.h"
 #include "platform/heap/Handle.h"
@@ -44,9 +46,11 @@ class PopStateEvent final : public Event {
   ~PopStateEvent() override;
   static PopStateEvent* Create();
   static PopStateEvent* Create(PassRefPtr<SerializedScriptValue>, History*);
-  static PopStateEvent* Create(const AtomicString&, const PopStateEventInit&);
+  static PopStateEvent* Create(ScriptState*,
+                               const AtomicString&,
+                               const PopStateEventInit&);
 
-  ScriptValue state() const { return state_; }
+  ScriptValue state(ScriptState*) const;
   SerializedScriptValue* SerializedState() const {
     return serialized_state_.Get();
   }
@@ -60,13 +64,16 @@ class PopStateEvent final : public Event {
 
   DECLARE_VIRTUAL_TRACE();
 
+  DECLARE_VIRTUAL_TRACE_WRAPPERS();
+
  private:
   PopStateEvent();
-  PopStateEvent(const AtomicString&, const PopStateEventInit&);
+  PopStateEvent(ScriptState*, const AtomicString&, const PopStateEventInit&);
   PopStateEvent(PassRefPtr<SerializedScriptValue>, History*);
 
   RefPtr<SerializedScriptValue> serialized_state_;
-  ScriptValue state_;
+  RefPtr<DOMWrapperWorld> world_;
+  TraceWrapperV8Reference<v8::Value> state_;
   Member<History> history_;
 };
 
