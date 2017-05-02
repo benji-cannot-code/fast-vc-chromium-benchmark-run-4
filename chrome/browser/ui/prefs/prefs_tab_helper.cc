9ff69c1b4406739c62923883b4aab7f488984485
ssid <ssid@chromium.org>
2017-05-02 11:47:16 -0700

    Android: disables observing changes for font prefs.
    On Android, there's no exposed way to change font preferences.
    This will save ~200KB of heap.
    This change was done in crrev.com/12529014 but was later reverted in
    crrev.com/444843002 by mistake. So, remove them again.
    
    BUG=716195
    
    Review-Url: https://codereview.chromium.org/2850503006
    Cr-Commit-Position: refs/heads/master@{#468717}

--- chrome/browser/ui/prefs/prefs_tab_helper.cc
+++ chrome/browser/ui/prefs/prefs_tab_helper.cc
@@ -138,7 +138,6 @@ ALL_FONT_SCRIPTS(WEBKIT_WEBPREFS_FONTS_STANDARD)
     }
   }
 }
-#endif  // !defined(OS_ANDROID)
 
 // Registers |obs| to observe per-script font prefs under the path |map_name|.
 // On android, there's no exposed way to change these prefs, so we can save
@@ -156,6 +155,7 @@ void RegisterFontFamilyMapObserver(
     registrar->Add(base::StringPrintf("%s.%s", map_name, script), obs);
   }
 }
+#endif  // !defined(OS_ANDROID)
 
 #if defined(OS_WIN)
 // On Windows with antialising we want to use an alternate fixed font like
@@ -374,6 +374,7 @@ class PrefWatcher : public KeyedService {
       pref_change_registrar_.Add(pref_name, webkit_callback);
     }
 
+#if !defined(OS_ANDROID)
     RegisterFontFamilyMapObserver(&pref_change_registrar_,
                                   prefs::kWebKitStandardFontFamilyMap,
                                   webkit_callback);
@@ -395,6 +396,7 @@ class PrefWatcher : public KeyedService {
     RegisterFontFamilyMapObserver(&pref_change_registrar_,
                                   prefs::kWebKitPictographFontFamilyMap,
                                   webkit_callback);
+#endif  // !defined(OS_ANDROID)
   }
 
   static PrefWatcher* Get(Profile* profile);
