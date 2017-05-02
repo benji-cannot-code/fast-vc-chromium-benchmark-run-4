e4f767597e11b17e995f2275b1924b3d68a66601
stkhapugin <stkhapugin@chromium.org>
2017-05-02 09:07:08 -0700

    [ObjC ARC] Converts ios/public/provider/chrome/browser/voice:voice to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2847863005
    Cr-Commit-Position: refs/heads/master@{#468659}

--- ios/public/provider/chrome/browser/voice/voice_search_language.mm
+++ ios/public/provider/chrome/browser/voice/voice_search_language.mm
@@ -5,11 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/public/provider/chrome/browser/voice/voice_search_language.h"
 
-#include "base/mac/objc_property_releaser.h"
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
 
-@implementation VoiceSearchLanguage {
-  base::mac::ObjCPropertyReleaser _propertyReleaser_VoiceSearchLanguage;
-}
+@implementation VoiceSearchLanguage
 
 @synthesize identifier = _identifier;
 @synthesize displayName = _displayName;
@@ -22,9 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     _identifier = [identifier copy];
     _displayName = [displayName copy];
     _localizationPreference = [localizationPreference copy];
-
-    _propertyReleaser_VoiceSearchLanguage.Init(self,
-                                               [VoiceSearchLanguage class]);
   }
   return self;
 }
--- ios/public/provider/chrome/browser/voice/voice_search_provider.mm
+++ ios/public/provider/chrome/browser/voice/voice_search_provider.mm
@@ -7,6 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/public/provider/chrome/browser/voice/voice_search_controller.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 bool VoiceSearchProvider::IsVoiceSearchEnabled() const {
   return false;
 }
