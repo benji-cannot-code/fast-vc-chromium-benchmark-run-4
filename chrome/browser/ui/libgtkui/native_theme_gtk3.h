54d412bc4a80dc1d7a4ec4c5cd4290c2002f2e17
thomasanderson <thomasanderson@google.com>
2017-05-02 15:49:22 -0700

    Gtk3: Fix black patches on GtkFileChooser dialogs
    This CL fixes parts of GtkFileChoosers not getting rendered on GNOME
    standard themes (Adwaita + HighContrast) causing black areas to
    appear.
    
    BUG=704092
    R=erg@chromium.org
    TBR=sky@chromium.org
    
    Review-Url: https://codereview.chromium.org/2854773002
    Cr-Commit-Position: refs/heads/master@{#468803}

--- chrome/browser/ui/libgtkui/native_theme_gtk3.h
+++ chrome/browser/ui/libgtkui/native_theme_gtk3.h
@@ -8,17 +8,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/optional.h"
+#include "ui/base/glib/glib_signal.h"
+#include "ui/base/glib/scoped_gobject.h"
 #include "ui/native_theme/native_theme_base.h"
 
+typedef struct _GtkCssProvider GtkCssProvider;
+typedef struct _GtkParamSpec GtkParamSpec;
+typedef struct _GtkSettings GtkSettings;
+
 namespace libgtkui {
+
+using ScopedCssProvider = ScopedGObject<GtkCssProvider>;
 
 // A version of NativeTheme that uses GTK3-rendered widgets.
 class NativeThemeGtk3 : public ui::NativeThemeBase {
  public:
   static NativeThemeGtk3* instance();
-
-  // Called when gtk theme changes.
-  void ResetColorCache();
 
   // Overridden from ui::NativeThemeBase:
   SkColor GetSystemColor(ColorId color_id) const override;
@@ -64,7 +69,17 @@ class NativeThemeGtk3 : public ui::NativeThemeBase {
   NativeThemeGtk3();
   ~NativeThemeGtk3() override;
 
+  void SetThemeCssOverride(ScopedCssProvider provider);
+
+  CHROMEG_CALLBACK_1(NativeThemeGtk3,
+                     void,
+                     OnThemeChanged,
+                     GtkSettings*,
+                     GtkParamSpec*);
+
   mutable base::Optional<SkColor> color_cache_[kColorId_NumColors];
+
+  ScopedCssProvider theme_css_override_;
 
   DISALLOW_COPY_AND_ASSIGN(NativeThemeGtk3);
 };
