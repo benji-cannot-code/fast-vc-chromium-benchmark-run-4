de55189e1c8cfdf1ca478865785431d12e06d0cf
renjieliu <renjieliu@chromium.org>
2017-05-01 22:42:42 -0700

    Check network connectivity in onPageTranslated callback.
    When attempting second translation keeping the WiFi/Data OFF, Chrome
    shows "Translated to <desired language>" message though web page remains
    untranslated.
    
    Current fix is to check whether user is online before showing up the
    translate UI in OnPageTranslated callback.
    
    BUG=653499
    
    Review-Url: https://codereview.chromium.org/2822383002
    Cr-Commit-Position: refs/heads/master@{#468569}

--- components/translate/content/browser/content_translate_driver.cc
+++ components/translate/content/browser/content_translate_driver.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/referrer.h"
+#include "net/base/network_change_notifier.h"
 #include "net/http/http_status_code.h"
 #include "url/gurl.h"
 
@@ -259,6 +260,10 @@ void ContentTranslateDriver::OnPageTranslated(
     TranslateErrors::Type error_type) {
   if (cancelled)
     return;
+
+  if (net::NetworkChangeNotifier::IsOffline()) {
+    error_type = TranslateErrors::NETWORK;
+  }
 
   translate_manager_->PageTranslated(
       original_lang, translated_lang, error_type);
