d64aaba0fc155f8468782852a75f6ba0923df11e
tfarina <tfarina@chromium.org>
2017-05-02 12:10:31 -0700

    net: remove unneeded headers from network_interfaces_unittest.cc
    Fixes eroman's TODO.
    
    BUG=488585
    TEST=net_unittests
    R=eroman@chromium.org
    
    Review-Url: https://codereview.chromium.org/2851073002
    Cr-Commit-Position: refs/heads/master@{#468734}

--- net/base/network_interfaces_unittest.cc
+++ net/base/network_interfaces_unittest.cc
@@ -9,16 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <unordered_set>
 
-// TODO(eroman): Remove unneeeded headers.
-#include "base/files/file_path.h"
-#include "base/format_macros.h"
-#include "base/scoped_native_library.h"
-#include "base/strings/string_number_conversions.h"
-#include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/sys_byteorder.h"
-#include "base/time/time.h"
+#include "build/build_config.h"
 #include "net/base/ip_endpoint.h"
+#include "testing/gtest/include/gtest/gtest.h"
 
 #if !defined(OS_NACL) && !defined(OS_WIN)
 #include <net/if.h>
@@ -30,8 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // !OS_IOS
 #endif  // OS_MACOSX
 #endif  // !OS_NACL && !OS_WIN
-#include "testing/gtest/include/gtest/gtest.h"
-#include "url/gurl.h"
 
 #if defined(OS_WIN)
 #include <iphlpapi.h>
