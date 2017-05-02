65f2f01c31585d0a69c88ca72578a4184d5a58e9
mpearson <mpearson@chromium.org>
2017-05-02 07:11:43 -0700

    Clipboard Recent Content - Code Health - Move kAuthorizedScheme into Subclass
    The variable and its related function is only used in one subclass, so it
    shouldn't sit in the base class and possibly mislead folks.
    
    BUG=711576
    
    Review-Url: https://codereview.chromium.org/2842793003
    Cr-Commit-Position: refs/heads/master@{#468626}

--- components/open_from_clipboard/clipboard_recent_content.cc
+++ components/open_from_clipboard/clipboard_recent_content.cc
@@ -13,16 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 ClipboardRecentContent* g_clipboard_recent_content = nullptr;
 
-// Schemes appropriate for suggestion by ClipboardRecentContent.
-const char* kAuthorizedSchemes[] = {
-    url::kAboutScheme, url::kDataScheme, url::kHttpScheme, url::kHttpsScheme,
-    // TODO(mpearson): add support for chrome:// URLs.  Right now the scheme
-    // for that lives in content and is accessible via
-    // GetEmbedderRepresentationOfAboutScheme() or content::kChromeUIScheme
-    // TODO(mpearson): when adding desktop support, add kFileScheme, kFtpScheme,
-    // and kGopherScheme.
-};
-
 }  // namespace
 
 ClipboardRecentContent::ClipboardRecentContent() {}
@@ -40,18 +30,6 @@ void ClipboardRecentContent::SetInstance(
     std::unique_ptr<ClipboardRecentContent> new_instance) {
   delete g_clipboard_recent_content;
   g_clipboard_recent_content = new_instance.release();
-}
-
-// static
-bool ClipboardRecentContent::IsAppropriateSuggestion(const GURL& url) {
-  // Check to make sure it's a scheme we're willing to suggest.
-  for (const auto* authorized_scheme : kAuthorizedSchemes) {
-    if (url.SchemeIs(authorized_scheme))
-      return true;
-  }
-
-  // Not a scheme we're allowed to return.
-  return false;
 }
 
 // static
--- components/open_from_clipboard/clipboard_recent_content_generic.cc
+++ components/open_from_clipboard/clipboard_recent_content_generic.cc
@@ -8,6 +8,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "ui/base/clipboard/clipboard.h"
 
+namespace {
+// Schemes appropriate for suggestion by ClipboardRecentContent.
+const char* kAuthorizedSchemes[] = {
+    url::kAboutScheme, url::kDataScheme, url::kHttpScheme, url::kHttpsScheme,
+    // TODO(mpearson): add support for chrome:// URLs.  Right now the scheme
+    // for that lives in content and is accessible via
+    // GetEmbedderRepresentationOfAboutScheme() or content::kChromeUIScheme
+    // TODO(mpearson): when adding desktop support, add kFileScheme, kFtpScheme,
+    // and kGopherScheme.
+};
+
+}  // namespace
+
 ClipboardRecentContentGeneric::ClipboardRecentContentGeneric() {}
 
 bool ClipboardRecentContentGeneric::GetRecentURLFromClipboard(GURL* url) {
@@ -63,4 +76,16 @@ void ClipboardRecentContentGeneric::SuppressClipboardContent() {
   // omnibox list.  Do this by pretending the current clipboard is ancient,
   // not recent.
   ui::Clipboard::GetForCurrentThread()->ClearLastModifiedTime();
+}
+
+// static
+bool ClipboardRecentContentGeneric::IsAppropriateSuggestion(const GURL& url) {
+  // Check to make sure it's a scheme we're willing to suggest.
+  for (const auto* authorized_scheme : kAuthorizedSchemes) {
+    if (url.SchemeIs(authorized_scheme))
+      return true;
+  }
+
+  // Not a scheme we're allowed to return.
+  return false;
 }
