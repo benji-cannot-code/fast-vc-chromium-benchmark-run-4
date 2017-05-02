a1713c989f6f6c9e033c8c92d3013b57da32b99f
erg <erg@chromium.org>
2017-05-02 11:54:51 -0700

    Remove BitmapArray.
    This changes the last consumer of skia.mojom.BitmapArray to a normal
    array<skia.mojom.Bitmap>, and deletes BitmapArray now that the vector
    mapping code works automatically.
    
    BUG=624509
    
    Review-Url: https://codereview.chromium.org/2850733003
    Cr-Commit-Position: refs/heads/master@{#468725}

--- services/ui/public/interfaces/cursor/cursor_struct_traits.cc
+++ services/ui/public/interfaces/cursor/cursor_struct_traits.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "mojo/common/common_custom_types_struct_traits.h"
 #include "services/ui/public/interfaces/cursor/cursor.mojom.h"
-#include "skia/public/interfaces/bitmap_array_struct_traits.h"
 #include "skia/public/interfaces/bitmap_skbitmap_struct_traits.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/base/cursor/cursor.h"
--- services/ui/public/interfaces/cursor/cursor_struct_traits_unittest.cc
+++ services/ui/public/interfaces/cursor/cursor_struct_traits_unittest.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/common/common_custom_types_struct_traits.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
 #include "services/ui/public/interfaces/cursor/cursor.mojom.h"
-#include "skia/public/interfaces/bitmap_array_struct_traits.h"
 #include "skia/public/interfaces/bitmap_skbitmap_struct_traits.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/cursor/cursor.h"
