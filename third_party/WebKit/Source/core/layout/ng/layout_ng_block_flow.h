61e507c8d93c1cc6e95a77c0e949985ae55a1176
ikilpatrick <ikilpatrick@chromium.org>
2017-05-01 20:20:14 -0700

    [LayoutNG] Move NGInlineNode data to NGInlineNodeData.
    In preparation for making NGInlineNode, NGBlockNode POD-like.
    
    BUG=636993
    
    Review-Url: https://codereview.chromium.org/2836293004
    Cr-Commit-Position: refs/heads/master@{#468547}

--- third_party/WebKit/Source/core/layout/ng/layout_ng_block_flow.h
+++ third_party/WebKit/Source/core/layout/ng/layout_ng_block_flow.h
@@ -6,13 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef LayoutNGBlockFlow_h
 #define LayoutNGBlockFlow_h
 
+#include "core/CoreExport.h"
 #include "core/layout/LayoutBlockFlow.h"
+#include "core/layout/ng/inline/ng_inline_node_data.h"
 #include "core/layout/ng/ng_block_node.h"
 
 namespace blink {
 
 // This overrides the default layout block algorithm to use Layout NG.
-class LayoutNGBlockFlow final : public LayoutBlockFlow {
+class CORE_EXPORT LayoutNGBlockFlow final : public LayoutBlockFlow {
  public:
   explicit LayoutNGBlockFlow(Element*);
   ~LayoutNGBlockFlow() override = default;
@@ -20,9 +22,13 @@ class LayoutNGBlockFlow final : public LayoutBlockFlow {
   void UpdateBlockLayout(bool relayout_children) override;
   NGBlockNode* BoxForTesting() const { return box_.Get(); }
 
+  NGInlineNodeData& GetNGInlineNodeData() const;
+  void ResetNGInlineNodeData();
+
  private:
   bool IsOfType(LayoutObjectType) const override;
 
+  std::unique_ptr<NGInlineNodeData> ng_inline_node_data_;
   Persistent<NGBlockNode> box_;
 };
 
