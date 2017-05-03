e6bc04bf820da8de1893c079cadee2fd44e7cc69
msramek <msramek@chromium.org>
2017-05-03 03:55:36 -0700

    Revert of Roll googletest to 1.8.0. (patchset #3 id:40001 of https://codereview.chromium.org/2852613002/ )
    Reason for revert:
    Seems to have caused all content_browsertests on ChromiumOS to crash.
    
    https://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%28dbg%29%281%29/builds/25582
    
    I'll run the revert through trybots so that it doesn't break anything else.
    
    Original issue's description:
    > Roll googletest to 1.8.0.
    >
    > This is a re-land of http://crrev.com/2779193002 which was reverted due to
    > build errors on Mac.
    >
    > GoogleTest (gtest) and GoogleMock (gmock) are now hosted into the same
    > googletest repository. In order to cope with this, the googletest
    > repository is now sourced at third_party/googletest.
    >
    > The file/directory layout of Google Test is not yet considered stable.
    > To minimize disruption while Google Test stabilizes, Chromium code will
    > be insulated from third_party/googletest.
    >
    > * testing/gtest/include/gtest/ and testing/gmock/include/gmock have
    >   been populated with headers that forward into the appropriate locations of
    >   third_party/googletest
    >
    > * testing/BUILD.gn has been populated with the targets
    >   //testing/gtest(:gtest_main) and //testing/gmock(:gmock_main), which
    >   depend on the appropriate //third_party/googletest targets.
    >
    > All Chromium code should keep depending on the targets and headers
    > in testing/{gtest,gmock} for now.
    >
    > BUG=630705
    > TESTED=ninja -C out/Default/ ced_unittests && ninja -C out/Default
    > TBR=rkc,dpranke,agl
    >
    > Review-Url: https://codereview.chromium.org/2852613002
    > Cr-Commit-Position: refs/heads/master@{#468860}
    > Committed: https://chromium.googlesource.com/chromium/src/+/dbcef9bdc1edd32d62c6de0bd382cdc78ce385da
    
    TBR=dpranke@chromium.org,thakis@chromium.org,rkc@chromium.org,agl@chromium.org,pwnall@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=630705
    
    Review-Url: https://codereview.chromium.org/2856963003
    Cr-Commit-Position: refs/heads/master@{#468934}

--- testing/gtest_mac.h
+++ testing/gtest_mac.h
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef TESTING_GTEST_MAC_H_
 #define TESTING_GTEST_MAC_H_
 
-#include "testing/gtest/include/gtest/gtest.h"
-#include "third_party/googletest/src/googletest/include/gtest/internal/gtest-port.h"
+#include <gtest/internal/gtest-port.h>
+#include <gtest/gtest.h>
 
 #ifdef GTEST_OS_MAC
 
