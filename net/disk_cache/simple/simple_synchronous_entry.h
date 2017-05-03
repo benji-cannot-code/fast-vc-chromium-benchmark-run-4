55f655c4eaf3f9fb0a462d93497443040ab23f34
morlovich <morlovich@chromium.org>
2017-05-03 11:24:13 -0700

    Add histograms for how execution of open + create in SimpleCache is split between queuing delay and synchronous portion. (there is already an end-to-end stat that covers both ops, and one for  sync portion of open only).
    Made the sync portion of open histogram split by cache_type like every
    other SimpleCache statistic.
    
    Also add synchronous portion metrics for Write/Close as both of these
    seem to have opportunities for improvement, so it makes sense to measure them directly, at least short-term.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2853263002
    Cr-Commit-Position: refs/heads/master@{#469038}

--- net/disk_cache/simple/simple_synchronous_entry.h
+++ net/disk_cache/simple/simple_synchronous_entry.h
@@ -119,11 +119,14 @@ class SimpleSynchronousEntry {
   // Opens a disk cache entry on disk. The |key| parameter is optional, if empty
   // the operation may be slower. The |entry_hash| parameter is required.
   // |had_index| is provided only for histograms.
+  // |time_enqueued| is when this operation was added to the I/O thread pool,
+  //  and is provided only for histograms.
   static void OpenEntry(net::CacheType cache_type,
                         const base::FilePath& path,
                         const std::string& key,
                         uint64_t entry_hash,
                         bool had_index,
+                        const base::TimeTicks& time_enqueued,
                         SimpleEntryCreationResults* out_results);
 
   static void CreateEntry(net::CacheType cache_type,
@@ -131,6 +134,7 @@ class SimpleSynchronousEntry {
                           const std::string& key,
                           uint64_t entry_hash,
                           bool had_index,
+                          const base::TimeTicks& time_enqueued,
                           SimpleEntryCreationResults* out_results);
 
   // Deletes an entry from the file system without affecting the state of the
