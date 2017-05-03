0bf9fd419a3c6bbefdc8ddf96e8b36b1a96399de
warx <warx@chromium.org>
2017-05-02 21:53:15 -0700

    cros: Update touchscreen status with backlights forced off state during start
    Changes:
    Sync powerd's backlights forced off state with touchscreen local pref and update touchscreen.
    
    BUG=709209
    TEST=added test coverage
    
    Review-Url: https://codereview.chromium.org/2855033004
    Cr-Commit-Position: refs/heads/master@{#468895}

--- ash/system/power/tablet_power_button_controller.cc
+++ ash/system/power/tablet_power_button_controller.cc
@@ -221,11 +221,7 @@ void TabletPowerButtonController::SetDisplayForcedOff(bool forced_off) {
       ->GetPowerManagerClient()
       ->SetBacklightsForcedOff(forced_off);
   backlights_forced_off_ = forced_off;
-
-  ShellDelegate* delegate = Shell::Get()->shell_delegate();
-  delegate->SetTouchscreenEnabledInPrefs(!forced_off,
-                                         true /* use_local_state */);
-  delegate->UpdateTouchscreenStatusFromPrefs();
+  UpdateTouchscreenStatus();
 
   // Send an a11y alert.
   Shell::Get()->accessibility_delegate()->TriggerAccessibilityAlert(
@@ -243,6 +239,14 @@ void TabletPowerButtonController::GetInitialBacklightsForcedOff() {
 void TabletPowerButtonController::OnGotInitialBacklightsForcedOff(
     bool is_forced_off) {
   backlights_forced_off_ = is_forced_off;
+  UpdateTouchscreenStatus();
+}
+
+void TabletPowerButtonController::UpdateTouchscreenStatus() {
+  ShellDelegate* delegate = Shell::Get()->shell_delegate();
+  delegate->SetTouchscreenEnabledInPrefs(!backlights_forced_off_,
+                                         true /* use_local_state */);
+  delegate->UpdateTouchscreenStatusFromPrefs();
 }
 
 void TabletPowerButtonController::StartShutdownTimer() {
