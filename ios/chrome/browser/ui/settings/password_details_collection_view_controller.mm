1a41ca5eb978e7cbadca225757643b2b0154b799
vabr <vabr@chromium.org>
2017-05-03 03:35:22 -0700

    Separate deletion button in password settings
    The "Delete this password" button gets a separate section. Before this CL it
    shares the section with the password value (separate from username and site),
    but the deletion affects the whole credential, so it should be separate.
    
    Design doc: go/jgdrc, screenshots https://crbug.com/159166#c40
    
    BUG=159166
    
    Review-Url: https://codereview.chromium.org/2722783003
    Cr-Commit-Position: refs/heads/master@{#468931}

--- ios/chrome/browser/ui/settings/password_details_collection_view_controller.mm
+++ ios/chrome/browser/ui/settings/password_details_collection_view_controller.mm
@@ -36,6 +36,7 @@ typedef NS_ENUM(NSInteger, SectionIdentifier) {
   SectionIdentifierSite = kSectionIdentifierEnumZero,
   SectionIdentifierUsername,
   SectionIdentifierPassword,
+  SectionIdentifierDelete,
 };
 
 typedef NS_ENUM(NSInteger, ItemType) {
@@ -176,8 +177,10 @@ reauthenticationModule:(id<ReauthenticationProtocol>)reauthenticationModule
       toSectionWithIdentifier:SectionIdentifierPassword];
   [model addItem:[self passwordCopyButtonItem]
       toSectionWithIdentifier:SectionIdentifierPassword];
+
+  [model addSectionWithIdentifier:SectionIdentifierDelete];
   [model addItem:[self deletePasswordButtonItem]
-      toSectionWithIdentifier:SectionIdentifierPassword];
+      toSectionWithIdentifier:SectionIdentifierDelete];
 }
 
 - (void)dealloc {
--- ios/chrome/browser/ui/settings/password_details_collection_view_controller_unittest.mm
+++ ios/chrome/browser/ui/settings/password_details_collection_view_controller_unittest.mm
@@ -83,7 +83,9 @@ const int kPasswordSection = 2;
 const int kPasswordItem = 0;
 const int kShowHideButtonItem = 1;
 const int kCopyPasswordButtonItem = 2;
-const int kDeleteButtonItem = 3;
+
+const int kDeleteSection = 3;
+const int kDeleteButtonItem = 0;
 
 class PasswordDetailsCollectionViewControllerTest
     : public CollectionViewControllerTest {
@@ -126,7 +128,7 @@ class PasswordDetailsCollectionViewControllerTest
 TEST_F(PasswordDetailsCollectionViewControllerTest, TestInitialization) {
   CreateController();
   CheckController();
-  EXPECT_EQ(3, NumberOfSections());
+  EXPECT_EQ(4, NumberOfSections());
   // Site section
   EXPECT_EQ(2, NumberOfItemsInSection(kUsernameSection));
   CheckSectionHeaderWithId(IDS_IOS_SHOW_PASSWORD_VIEW_SITE, kSiteSection);
@@ -147,7 +149,7 @@ TEST_F(PasswordDetailsCollectionViewControllerTest, TestInitialization) {
   CheckTextCellTitleWithId(IDS_IOS_SETTINGS_USERNAME_COPY_BUTTON,
                            kUsernameSection, kCopyUsernameButtonItem);
   // Password section
-  EXPECT_EQ(4, NumberOfItemsInSection(kPasswordSection));
+  EXPECT_EQ(3, NumberOfItemsInSection(kPasswordSection));
   CheckSectionHeaderWithId(IDS_IOS_SHOW_PASSWORD_VIEW_PASSWORD,
                            kPasswordSection);
   PasswordDetailsItem* passwordItem =
@@ -158,8 +160,10 @@ TEST_F(PasswordDetailsCollectionViewControllerTest, TestInitialization) {
                            kPasswordSection, kShowHideButtonItem);
   CheckTextCellTitleWithId(IDS_IOS_SETTINGS_PASSWORD_COPY_BUTTON,
                            kPasswordSection, kCopyPasswordButtonItem);
+  // Delete section
+  EXPECT_EQ(1, NumberOfItemsInSection(kDeleteSection));
   CheckTextCellTitleWithId(IDS_IOS_SETTINGS_PASSWORD_DELETE_BUTTON,
-                           kPasswordSection, kDeleteButtonItem);
+                           kDeleteSection, kDeleteButtonItem);
 }
 
 struct SimplifyOriginTestData {
@@ -252,7 +256,7 @@ TEST_F(PasswordDetailsCollectionViewControllerTest, DeletePassword) {
   CreateController();
   [controller() collectionView:[controller() collectionView]
       didSelectItemAtIndexPath:[NSIndexPath indexPathForRow:kDeleteButtonItem
-                                                  inSection:kPasswordSection]];
+                                                  inSection:kDeleteSection]];
   EXPECT_EQ(1, delegate_.numberOfCallsToDeletePassword);
 }
 
