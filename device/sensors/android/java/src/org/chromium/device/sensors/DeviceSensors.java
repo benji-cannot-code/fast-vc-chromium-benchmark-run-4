53e781051274540d427e9f8e534dcc2ea2800a13
reillyg <reillyg@chromium.org>
2017-05-02 18:53:00 -0700

    Remove redundant static singleton for DeviceSensors
    The singleton Java DeviceSensors object is owned by a singleton
    SensorManagerAndroid so there is no need to maintain a reference to the
    singleton in both Java and C++. This silences the lint warning about
    keeping references to the Context object in a static field because now
    the static field is in C++ and thus invisible to the linter.
    
    BUG=635567
    
    Review-Url: https://codereview.chromium.org/2860633003
    Cr-Commit-Position: refs/heads/master@{#468863}

--- device/sensors/android/java/src/org/chromium/device/sensors/DeviceSensors.java
+++ device/sensors/android/java/src/org/chromium/device/sensors/DeviceSensors.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.device.sensors;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -67,11 +66,6 @@ class DeviceSensors implements SensorEventListener {
 
     // Lazily initialized when registering for notifications.
     private SensorManagerProxy mSensorManagerProxy;
-
-    // The only instance of that class and its associated lock.
-    @SuppressLint("StaticFieldLeak")
-    private static DeviceSensors sSingleton;
-    private static Object sSingletonLock = new Object();
 
     static final Set<Integer> DEVICE_ORIENTATION_SENSORS_A =
             CollectionUtil.newHashSet(Sensor.TYPE_GAME_ROTATION_VECTOR);
@@ -572,13 +566,8 @@ class DeviceSensors implements SensorEventListener {
     }
 
     @CalledByNative
-    static DeviceSensors getInstance(Context appContext) {
-        synchronized (sSingletonLock) {
-            if (sSingleton == null) {
-                sSingleton = new DeviceSensors(appContext);
-            }
-            return sSingleton;
-        }
+    static DeviceSensors create(Context appContext) {
+        return new DeviceSensors(appContext);
     }
 
     /**
