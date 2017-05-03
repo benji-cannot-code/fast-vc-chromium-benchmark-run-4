8ef18215d96671a6ff2fd53e49150da360341208
Kottackal.George <Kottackal.George@gmail.com>
2017-05-03 12:43:02 -0700

    Emit error events if the loading of an object element failed
    Dispatch onerror/error events in the case of a placeholder being
    instantiated instead of an actual plugin.
    
    BUG=445557
    
    Review-Url: https://codereview.chromium.org/2733083004
    Cr-Commit-Position: refs/heads/master@{#469081}

--- components/plugins/renderer/plugin_placeholder.cc
+++ components/plugins/renderer/plugin_placeholder.cc
@@ -139,6 +139,10 @@ v8::Local<v8::Value> PluginPlaceholder::GetV8Handle(v8::Isolate* isolate) {
   return gin::CreateHandle(isolate, this).ToV8();
 }
 
+bool PluginPlaceholderBase::IsErrorPlaceholder() {
+  return false;
+}
+
 gin::ObjectTemplateBuilder PluginPlaceholder::GetObjectTemplateBuilder(
     v8::Isolate* isolate) {
   return gin::Wrappable<PluginPlaceholder>::GetObjectTemplateBuilder(isolate)
--- third_party/WebKit/Source/core/html/HTMLObjectElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLObjectElement.cpp
@@ -328,6 +328,9 @@ void HTMLObjectElement::UpdatePluginInternal() {
       DispatchErrorEvent();
     if (HasFallbackContent())
       RenderFallbackContent();
+  } else {
+    if (IsErrorplaceholder())
+      DispatchErrorEvent();
   }
 }
 
