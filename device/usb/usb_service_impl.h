84a8151b3f3d767db86f91c17d951f45565cf290
reillyg <reillyg@chromium.org>
2017-05-03 07:45:02 -0700

    Use the task scheduler in the USB service on macOS and Windows
    This updates the libusb-based USB service to use the task scheduler
    which means it will be enabled on macOS and Windows. The new Windows
    USB service will be updated next.
    
    By executing these tasks on a sequence with the CONTINUE_ON_SHUTDOWN
    trait a device or driver bug that causes a request to lock up should
    not prevent Chrome from shutting down.
    
    BUG=652416,703585
    
    Review-Url: https://codereview.chromium.org/2857473002
    Cr-Commit-Position: refs/heads/master@{#468967}

--- device/usb/usb_service_impl.h
+++ device/usb/usb_service_impl.h
@@ -26,10 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 struct libusb_device;
 struct libusb_context;
 
-namespace base {
-class SequencedTaskRunner;
-}
-
 namespace device {
 
 typedef struct libusb_device* PlatformUsbDevice;
@@ -43,8 +39,7 @@ class UsbServiceImpl :
 #endif  // OS_WIN
     public UsbService {
  public:
-  explicit UsbServiceImpl(
-      scoped_refptr<base::SequencedTaskRunner> blocking_task_runner);
+  UsbServiceImpl();
   ~UsbServiceImpl() override;
 
  private:
