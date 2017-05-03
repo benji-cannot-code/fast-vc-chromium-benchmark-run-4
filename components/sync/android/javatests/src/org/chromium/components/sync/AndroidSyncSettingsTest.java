02ee2293ab06ab84ee2705db7aa58024d100a547
johnme <johnme@chromium.org>
2017-05-03 05:46:34 -0700

    [Sync] Disable 2 flaky tests on Android
    Disables AndroidSyncSettingsTest#testAccountInitialization and
    AndroidSyncSettingsTest#testIsSyncableOnSigninAndNotOnSignout
    since they are flaky on Android.
    
    BUG=717960
    SKIPTREECHECKS=true
    TBR=pavely@chromium.org
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2857103004
    Cr-Commit-Position: refs/heads/master@{#468949}

--- components/sync/android/javatests/src/org/chromium/components/sync/AndroidSyncSettingsTest.java
+++ components/sync/android/javatests/src/org/chromium/components/sync/AndroidSyncSettingsTest.java
@@ -14,6 +14,7 @@ import android.test.InstrumentationTestCase;
 import org.chromium.base.Callback;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CallbackHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.components.signin.AccountManagerHelper;
 import org.chromium.components.signin.test.util.AccountHolder;
@@ -153,8 +154,9 @@ public class AndroidSyncSettingsTest extends InstrumentationTestCase {
         });
     }
 
-    @SmallTest
-    @Feature({"Sync"})
+    // @SmallTest
+    // @Feature({"Sync"})
+    @DisabledTest(message = "crbug.com/717960")
     public void testAccountInitialization() throws InterruptedException, TimeoutException {
         // mAccount was set to be syncable and not have periodic syncs.
         assertEquals(1, mSyncContentResolverDelegate.mSetIsSyncableCalls);
@@ -366,8 +368,9 @@ public class AndroidSyncSettingsTest extends InstrumentationTestCase {
                 mSyncSettingsObserver.receivedNotification());
     }
 
-    @SmallTest
-    @Feature({"Sync"})
+    // @SmallTest
+    // @Feature({"Sync"})
+    @DisabledTest(message = "crbug.com/717960")
     public void testIsSyncableOnSigninAndNotOnSignout()
             throws InterruptedException, TimeoutException {
         assertTrue(mSyncContentResolverDelegate.getIsSyncable(mAccount, mAuthority) == 1);
