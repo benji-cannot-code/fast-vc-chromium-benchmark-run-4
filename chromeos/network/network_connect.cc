3317a3b4ac2b8af88ae23ddba505e83173aa31f2
khorimoto <khorimoto@chromium.org>
2017-05-02 17:18:48 -0700

    [CrOS Tether] Show tether dialog when connection is initiated.
    When a connection to a Tether network is initiated but the current device has not yet connected to that tether host before, show the tether connection confirmation dialog.
    
    The dialog is already shown if the connection attempt comes from the settings page (see [1]), but this will show the dialog when attempting a connection from other places (e.g., quick settings or a Tether notification from the message center).
    
    [1] https://codereview.chromium.org/2855813002/
    
    BUG=672263
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2855973002
    Cr-Commit-Position: refs/heads/master@{#468830}

--- chromeos/network/network_connect.cc
+++ chromeos/network/network_connect.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/network/network_profile_handler.h"
 #include "chromeos/network/network_state.h"
 #include "chromeos/network/network_state_handler.h"
+#include "chromeos/network/tether_constants.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
 
 namespace chromeos {
@@ -245,8 +246,9 @@ void NetworkConnectImpl::CallConnectToNetwork(const std::string& network_id,
   }
 
   NetworkHandler::Get()->network_connection_handler()->ConnectToNetwork(
-      network->path(), base::Bind(&NetworkConnectImpl::OnConnectSucceeded,
-                                  weak_factory_.GetWeakPtr(), network_id),
+      network->path(),
+      base::Bind(&NetworkConnectImpl::OnConnectSucceeded,
+                 weak_factory_.GetWeakPtr(), network_id),
       base::Bind(&NetworkConnectImpl::OnConnectFailed,
                  weak_factory_.GetWeakPtr(), network_id),
       check_error_state);
@@ -387,6 +389,10 @@ void NetworkConnectImpl::ConnectToNetworkId(const std::string& network_id) {
       return;
     } else if (network->RequiresActivation()) {
       ActivateCellular(network_id);
+      return;
+    } else if (network->type() == kTypeTether &&
+               !network->tether_has_connected_to_host()) {
+      delegate_->ShowNetworkConfigure(network_id);
       return;
     }
   }
