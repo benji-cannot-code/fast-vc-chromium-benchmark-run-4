25e2addb164034e5b2b1cb79eb543faf42c1cb0c
wychen <wychen@chromium.org>
2017-05-02 18:06:07 -0700

    Only include base/win/windows_version.h on Windows build
    I ran check_gn_headers.py on linux and android full builds and
    manually fixed it.
    
    BUG=661774
    
    Review-Url: https://codereview.chromium.org/2855933002
    Cr-Commit-Position: refs/heads/master@{#468848}

--- base/allocator/partition_allocator/address_space_randomization.cc
+++ base/allocator/partition_allocator/address_space_randomization.cc
@@ -7,11 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/allocator/partition_allocator/page_allocator.h"
 #include "base/allocator/partition_allocator/spin_lock.h"
-#include "base/win/windows_version.h"
 #include "build/build_config.h"
 
 #if defined(OS_WIN)
 #include <windows.h>
+#include "base/win/windows_version.h"
 #else
 #include <sys/time.h>
 #include <unistd.h>
