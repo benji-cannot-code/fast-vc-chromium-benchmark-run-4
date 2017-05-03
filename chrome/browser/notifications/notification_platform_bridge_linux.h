1f54ca7d01adfbf23859724ab4296d2079534f1a
thestig <thestig@chromium.org>
2017-05-03 14:57:43 -0700

    Linux native notifications: Delete notification after sending CloseNotification.
    Also check the "dbus_id" when handling signals.
    
    Fix some nits as well.
    
    BUG=676220
    
    Review-Url: https://codereview.chromium.org/2859483003
    Cr-Commit-Position: refs/heads/master@{#469148}

--- chrome/browser/notifications/notification_platform_bridge_linux.h
+++ chrome/browser/notifications/notification_platform_bridge_linux.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_NOTIFICATIONS_NOTIFICATION_PLATFORM_BRIDGE_LINUX_H_
 #define CHROME_BROWSER_NOTIFICATIONS_NOTIFICATION_PLATFORM_BRIDGE_LINUX_H_
 
+#include <string>
+
 #include "base/callback_forward.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
--- chrome/browser/notifications/notification_platform_bridge_linux_unittest.cc
+++ chrome/browser/notifications/notification_platform_bridge_linux_unittest.cc
@@ -5,11 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/notifications/notification_platform_bridge_linux.h"
 
+#include <memory>
+
 #include "base/callback.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/ref_counted.h"
-#include "base/sequenced_task_runner.h"
 #include "chrome/browser/notifications/notification.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "content/public/test/test_utils.h"
@@ -33,6 +34,7 @@ ACTION_P(RegisterSignalCallback, callback_addr) {
 class NotificationPlatformBridgeLinuxTest : public testing::Test {
  public:
   NotificationPlatformBridgeLinuxTest() = default;
+  ~NotificationPlatformBridgeLinuxTest() override = default;
 
   void SetUp() override {
     mock_bus_ = new dbus::MockBus(dbus::Bus::Options());
@@ -55,8 +57,8 @@ class NotificationPlatformBridgeLinuxTest : public testing::Test {
                                 "NotificationClosed", testing::_, testing::_))
         .WillOnce(RegisterSignalCallback(&notification_closed_callback_));
 
-    notification_bridge_linux_.reset(
-        new NotificationPlatformBridgeLinux(mock_bus_));
+    notification_bridge_linux_ =
+        base::WrapUnique(new NotificationPlatformBridgeLinux(mock_bus_));
   }
 
   void TearDown() override {
