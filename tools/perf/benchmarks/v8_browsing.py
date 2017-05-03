c29b24c14dcdc1a432038c49bdfe9e2fb5527e2f
nednguyen <nednguyen@google.com>
2017-05-03 14:08:07 -0700

    Remove disabling of reference builds on V8 benchmarks
    Now that we ignore the return code of reference benchmarks run, there is no need
    to disabling these benchmarks on reference build.
    
    BUG=707237
    
    Review-Url: https://codereview.chromium.org/2860753002
    Cr-Commit-Position: refs/heads/master@{#469120}

--- tools/perf/benchmarks/v8_browsing.py
+++ tools/perf/benchmarks/v8_browsing.py
@@ -155,7 +155,6 @@ class V8DesktopBrowsingBenchmark(_V8BrowsingBenchmark):
 
 @benchmark.Owner(emails=['ulan@chromium.org'])
 @benchmark.Enabled('android')
-@benchmark.Disabled('reference')  # http://crbug.com/628631
 class V8MobileBrowsingBenchmark(_V8BrowsingBenchmark):
   PLATFORM = 'mobile'
 
@@ -164,7 +163,6 @@ class V8MobileBrowsingBenchmark(_V8BrowsingBenchmark):
     return 'v8.browsing_mobile'
 
 
-@benchmark.Disabled('reference')  # http://crbug.com/700390
 @benchmark.Disabled('android')
 @benchmark.Disabled('all')
 @benchmark.Owner(emails=['mvstaton@chromium.org'])
@@ -182,7 +180,6 @@ class V8DesktopTurboBrowsingBenchmark(_V8BrowsingBenchmark):
 
 
 
-@benchmark.Disabled('reference')  # http://crbug.com/628631
 @benchmark.Enabled('android')
 @benchmark.Disabled('all')
 @benchmark.Owner(emails=['mvstaton@chromium.org'])
@@ -199,7 +196,6 @@ class V8MobileTurboBrowsingBenchmark(_V8BrowsingBenchmark):
     return 'v8.browsing_mobile_turbo'
 
 
-@benchmark.Disabled('reference')  # http://crbug.com/700390
 @benchmark.Disabled('android')
 @benchmark.Owner(emails=['hablich@chromium.org'])
 class V8DesktopClassicBrowsingBenchmark(_V8BrowsingBenchmark):
@@ -215,7 +211,6 @@ class V8DesktopClassicBrowsingBenchmark(_V8BrowsingBenchmark):
     return 'v8.browsing_desktop_classic'
 
 
-@benchmark.Disabled('reference')  # http://crbug.com/628631
 @benchmark.Enabled('android')
 @benchmark.Owner(emails=['hablich@chromium.org'])
 class V8MobileClassicBrowsingBenchmark(_V8BrowsingBenchmark):
@@ -243,7 +238,6 @@ class V8RuntimeStatsDesktopBrowsingBenchmark(
     return 'v8.runtimestats.browsing_desktop'
 
 
-@benchmark.Disabled('reference')  # http://crbug.com/700390
 @benchmark.Disabled('android')
 @benchmark.Disabled('all')
 @benchmark.Owner(emails=['mythria@chromium.org'])
@@ -261,8 +255,7 @@ class V8RuntimeStatsDesktopTurboBrowsingBenchmark(
     return 'v8.runtimestats.browsing_desktop_turbo'
 
 
-@benchmark.Disabled('reference',  # http://crbug.com/700390
-                    'win')        # http://crbug.com/704197
+@benchmark.Disabled('win')  # http://crbug.com/704197
 @benchmark.Disabled('android')
 @benchmark.Owner(emails=['hablich@chromium.org'])
 class V8RuntimeStatsDesktopClassicBrowsingBenchmark(
