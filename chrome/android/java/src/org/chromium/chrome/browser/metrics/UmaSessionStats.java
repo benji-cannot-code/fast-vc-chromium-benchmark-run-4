579efda27c8742ead6d9ed1d93d857b60a8313ed
dtrainor <dtrainor@chromium.org>
2017-05-03 16:56:24 -0700

    Add UMA metrics regarding default browser status
    In order to better understand Chrome usage, add metrics that help us
    figure out if Chrome is the default browser on the system or not.
    
    BUG=704223
    
    Review-Url: https://codereview.chromium.org/2843693002
    Cr-Commit-Position: refs/heads/master@{#469204}

--- chrome/android/java/src/org/chromium/chrome/browser/metrics/UmaSessionStats.java
+++ chrome/android/java/src/org/chromium/chrome/browser/metrics/UmaSessionStats.java
@@ -10,6 +10,7 @@ import android.content.Context;
 import android.content.res.Configuration;
 
 import org.chromium.base.ContextUtils;
+import org.chromium.chrome.browser.DefaultBrowserInfo;
 import org.chromium.chrome.browser.preferences.privacy.PrivacyPreferencesManager;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.TabModel;
@@ -103,6 +104,7 @@ public class UmaSessionStats {
         nativeUmaResumeSession(sNativeUmaSessionStats);
         updatePreferences();
         updateMetricsServiceState();
+        DefaultBrowserInfo.logDefaultBrowserStats();
     }
 
     private static void ensureNativeInitialized() {
