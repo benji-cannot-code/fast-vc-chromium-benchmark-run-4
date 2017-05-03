4f464c740dff0d15f3d8df1ab275861c740e8ef8
finnur <finnur@chromium.org>
2017-05-03 02:06:47 -0700

    Photo Picker Dialog: Use sandboxed utility process for decoding images.
    BUG=656015
    
    Review-Url: https://codereview.chromium.org/2816733002
    Cr-Original-Commit-Position: refs/heads/master@{#468209}
    Committed: https://chromium.googlesource.com/chromium/src/+/83c9f3f4faeb35a3a2607446def65f1a8da7aa49
    Review-Url: https://codereview.chromium.org/2816733002
    Cr-Commit-Position: refs/heads/master@{#468923}

--- chrome/android/java/src/org/chromium/chrome/browser/photo_picker/PickerBitmapView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/photo_picker/PickerBitmapView.java
@@ -190,8 +190,13 @@ public class PickerBitmapView extends SelectableItemView<PickerBitmap> {
 
         mBitmapDetails = bitmapDetails;
         setItem(bitmapDetails);
-        setThumbnailBitmap(thumbnail);
-        mImageLoaded = !placeholder;
+        if (isCameraTile() || isGalleryTile()) {
+            initializeSpecialTile(mBitmapDetails);
+            mImageLoaded = true;
+        } else {
+            setThumbnailBitmap(thumbnail);
+            mImageLoaded = !placeholder;
+        }
 
         updateSelectionState();
     }
@@ -217,8 +222,6 @@ public class PickerBitmapView extends SelectableItemView<PickerBitmap> {
         ApiCompatibilityUtils.setCompoundDrawablesRelativeWithIntrinsicBounds(
                 mSpecialTile, null, image, null, null);
         mSpecialTile.setText(labelStringId);
-
-        initialize(bitmapDetails, null, false);
 
         // Reset visibility, since #initialize() sets mSpecialTile visibility to GONE.
         mSpecialTile.setVisibility(View.VISIBLE);
@@ -267,6 +270,7 @@ public class PickerBitmapView extends SelectableItemView<PickerBitmap> {
      * re-used.
      */
     private void resetTile() {
+        mIconView.setImageBitmap(null);
         mUnselectedView.setVisibility(View.GONE);
         mSelectedView.setVisibility(View.GONE);
         mScrim.setVisibility(View.GONE);
@@ -324,15 +328,14 @@ public class PickerBitmapView extends SelectableItemView<PickerBitmap> {
     }
 
     private boolean isGalleryTile() {
-        // TODO(finnur): Remove the null checks here and below.
-        return mBitmapDetails != null && mBitmapDetails.type() == PickerBitmap.GALLERY;
+        return mBitmapDetails.type() == PickerBitmap.GALLERY;
     }
 
     private boolean isCameraTile() {
-        return mBitmapDetails != null && mBitmapDetails.type() == PickerBitmap.CAMERA;
+        return mBitmapDetails.type() == PickerBitmap.CAMERA;
     }
 
     private boolean isPictureTile() {
-        return mBitmapDetails == null || mBitmapDetails.type() == PickerBitmap.PICTURE;
+        return mBitmapDetails.type() == PickerBitmap.PICTURE;
     }
 }
