1c6a05e051e145c70df1a238ef273d4343817992
c.padhi <c.padhi@samsung.com>
2017-05-03 12:27:32 -0700

    Remove SupportsWeakPtr from VideoCaptureDeviceClient
    VideoCaptureDeviceClient unnecessarily inherits SupportsWeakPtr.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2861603003
    Cr-Commit-Position: refs/heads/master@{#469072}

--- media/capture/video/video_capture_device_client.h
+++ media/capture/video/video_capture_device_client.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
-#include "base/memory/weak_ptr.h"
 #include "base/threading/thread_collision_warner.h"
 #include "media/capture/capture_export.h"
 #include "media/capture/video/video_capture_device.h"
@@ -38,8 +37,7 @@ using VideoCaptureJpegDecoderFactoryCB =
 // The owner is responsible for making sure that the instance outlives these
 // calls.
 class CAPTURE_EXPORT VideoCaptureDeviceClient
-    : public media::VideoCaptureDevice::Client,
-      public base::SupportsWeakPtr<VideoCaptureDeviceClient> {
+    : public media::VideoCaptureDevice::Client {
  public:
   VideoCaptureDeviceClient(
       std::unique_ptr<VideoFrameReceiver> receiver,
