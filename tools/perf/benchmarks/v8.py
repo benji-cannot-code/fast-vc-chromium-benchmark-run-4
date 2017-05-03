6f6e1512b77c774b07aa9747c46d95eb0c00821a
ulan <ulan@chromium.org>
2017-05-03 07:23:13 -0700

    Re-enable v8.infinite_scroll_tbmv2 on Linux and disable it on Android.
    Android runs v8.mobile_infinite_scroll_tbmv2.
    
    BUG=chromium:715716,chromium:716435
    
    Review-Url: https://codereview.chromium.org/2857923003
    Cr-Commit-Position: refs/heads/master@{#468960}

--- tools/perf/benchmarks/v8.py
+++ tools/perf/benchmarks/v8.py
@@ -128,7 +128,7 @@ class _InfiniteScrollBenchmark(perf_benchmark.PerfBenchmark):
     return True
 
 
-@benchmark.Disabled('linux')  # crbug.com/715716
+@benchmark.Disabled('android') # Android runs V8MobileInfiniteScroll.
 @benchmark.Owner(emails=['ulan@chromium.org'])
 class V8InfiniteScroll(_InfiniteScrollBenchmark):
   """Measures V8 GC metrics and memory usage while scrolling the top web pages.
@@ -141,6 +141,7 @@ class V8InfiniteScroll(_InfiniteScrollBenchmark):
     return 'v8.infinite_scroll_tbmv2'
 
 @benchmark.Disabled('all')
+@benchmark.Disabled('android') # Android runs V8MobileInfiniteScroll.
 @benchmark.Owner(emails=['mvstaton@chromium.org'])
 class V8InfiniteScrollTurbo(V8InfiniteScroll):
   """Measures V8 GC metrics using Ignition+TurboFan."""
@@ -155,6 +156,7 @@ class V8InfiniteScrollTurbo(V8InfiniteScroll):
 
 
 @benchmark.Disabled('linux')  # crbug.com/715716
+@benchmark.Disabled('android') # Android runs V8MobileInfiniteScroll.
 @benchmark.Owner(emails=['hablich@chromium.org'])
 class V8InfiniteScrollClassic(V8InfiniteScroll):
   """Measures V8 GC metrics using the Classic pipeline."""
