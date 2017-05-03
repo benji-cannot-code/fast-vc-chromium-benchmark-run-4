250c7206b5d4831d03255cfa2af89315c1bb484b
manzagop <manzagop@chromium.org>
2017-05-03 08:51:52 -0700

    Add stability_debugging_win_unittest.cc to build config
    This fixes an oversight from https://codereview.chromium.org/2344343002/
    
    BUG=620813
    
    Review-Url: https://codereview.chromium.org/2852693003
    Cr-Commit-Position: refs/heads/master@{#468987}

--- components/browser_watcher/stability_debugging_win_unittest.cc
+++ components/browser_watcher/stability_debugging_win_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/browser_watcher/stability_debugging_win.h"
+#include "components/browser_watcher/stability_debugging.h"
 
 #include "base/files/file_enumerator.h"
 #include "base/files/file_path.h"
@@ -37,8 +37,9 @@ TEST_F(StabilityDebuggingWinMultiProcTest, GetStabilityFileForProcessTest) {
   EXPECT_EQ(stability_path, stability_path_two);
 
   // Ensure a different process has a different stability path.
+  base::SpawnChildResult spawn_result = SpawnChild("DummyProcess");
   base::FilePath stability_path_other;
-  ASSERT_TRUE(GetStabilityFileForProcess(SpawnChild("DummyProcess"), empty_path,
+  ASSERT_TRUE(GetStabilityFileForProcess(spawn_result.process, empty_path,
                                          &stability_path_other));
   EXPECT_NE(stability_path, stability_path_other);
 }
@@ -52,7 +53,7 @@ TEST(StabilityDebuggingWinTest,
 
   base::ScopedTempDir temp_dir;
   ASSERT_TRUE(temp_dir.CreateUniqueTempDir());
-  base::FilePath user_data_dir = temp_dir.path();
+  base::FilePath user_data_dir = temp_dir.GetPath();
 
   // Create the stability directory.
   base::FilePath stability_dir = GetStabilityDir(user_data_dir);
