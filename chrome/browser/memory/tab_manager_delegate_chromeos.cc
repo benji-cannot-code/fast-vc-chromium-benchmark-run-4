9418d0b75ab052a85a532aa77a1d7d0ac6462805
yusukes <yusukes@chromium.org>
2017-05-02 18:19:23 -0700

    Revert "Do not kill recently killed ARC processes again"
    This reverts commit b784d0769dce13bd2efc4ae9bf13b0ffe8523a03.
    
    BUG=706048
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2855973003 .
    Cr-Commit-Position: refs/heads/master@{#468854}

--- chrome/browser/memory/tab_manager_delegate_chromeos.cc
+++ chrome/browser/memory/tab_manager_delegate_chromeos.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <map>
+#include <string>
 #include <vector>
 
 #include "ash/shell.h"
@@ -51,7 +52,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using base::ProcessHandle;
 using base::TimeDelta;
-using base::TimeTicks;
 using content::BrowserThread;
 
 namespace memory {
@@ -555,15 +555,6 @@ TabManagerDelegate::GetSortedCandidates(
   return candidates;
 }
 
-bool TabManagerDelegate::IsRecentlyKilledArcProcess(
-    const std::string& process_name,
-    const TimeTicks& now) {
-  const auto it = recently_killed_arc_processes_.find(process_name);
-  if (it == recently_killed_arc_processes_.end())
-    return false;
-  return (now - it->second) <= GetArcRespawnKillDelay();
-}
-
 bool TabManagerDelegate::KillArcProcess(const int nspid) {
   auto* arc_service_manager = arc::ArcServiceManager::Get();
   if (!arc_service_manager)
@@ -600,7 +591,6 @@ void TabManagerDelegate::LowMemoryKillImpl(
       GetSortedCandidates(tab_list, arc_processes);
 
   int target_memory_to_free_kb = mem_stat_->TargetMemoryToFreeKB();
-  const TimeTicks now = TimeTicks::Now();
 
   // Kill processes until the estimated amount of freed memory is sufficient to
   // bring the system memory back to a normal level.
@@ -621,14 +611,9 @@ void TabManagerDelegate::LowMemoryKillImpl(
       continue;
     }
     if (it->app()) {
-      if (IsRecentlyKilledArcProcess(it->app()->process_name(), now)) {
-        MEMORY_LOG(ERROR) << "Avoided killing " << *it << " too often";
-        continue;
-      }
       int estimated_memory_freed_kb =
           mem_stat_->EstimatedMemoryFreedKB(it->app()->pid());
       if (KillArcProcess(it->app()->nspid())) {
-        recently_killed_arc_processes_[it->app()->process_name()] = now;
         target_memory_to_free_kb -= estimated_memory_freed_kb;
         MemoryKillsMonitor::LogLowMemoryKill("APP", estimated_memory_freed_kb);
         MEMORY_LOG(ERROR) << "Killed " << *it << ", estimated "
