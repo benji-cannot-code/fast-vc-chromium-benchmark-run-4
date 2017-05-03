64e402204cf85a7f9c15259134e56083ffbf0747
lgrey <lgrey@chromium.org>
2017-05-03 15:07:57 -0700

    [Mac] Reverse bookmark buttons and menus in RTL (take 2)
    LTR image diffs show this as:
    - Identical to HEAD in retina
    - In non-retina, some bookmarks at the width limit show an additional character vs. HEAD. I think this is more true to the spec and is closer to Views (though it's hard to compare directly since views fades instead of cutting off with an ellipsis)
    
    This is a semi-manual rebase of
    https://codereview.chromium.org/2511973002/
    
    BUG=648561, 648560
    
    Review-Url: https://codereview.chromium.org/2845003003
    Cr-Commit-Position: refs/heads/master@{#469157}

--- chrome/browser/ui/cocoa/bookmarks/bookmark_button_cell_unittest.mm
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_button_cell_unittest.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
+#include "chrome/browser/ui/cocoa/l10n_util.h"
 #include "chrome/browser/ui/cocoa/test/cocoa_profile_test.h"
 #import "chrome/browser/ui/cocoa/test/cocoa_test_helper.h"
 #include "chrome/test/base/testing_profile.h"
@@ -158,7 +159,7 @@ TEST_F(BookmarkButtonCellTest, Awake) {
   base::scoped_nsobject<BookmarkButtonCell> cell(
       [[BookmarkButtonCell alloc] init]);
   [cell awakeFromNib];
-  EXPECT_EQ(NSLeftTextAlignment, [cell alignment]);
+  EXPECT_EQ(NSNaturalTextAlignment, [cell alignment]);
 }
 
 // Subfolder arrow details.
