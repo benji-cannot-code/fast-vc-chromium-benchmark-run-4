91ed24554814181f794db0d710989a105713c78f
mathp <mathp@chromium.org>
2017-05-02 17:48:02 -0700

    [Payments] Desktop: enable the Payment Request integration by default
    The WebPayments feature flag is now ENABLED_BY_DEFAULT, and on
    the renderer side the feature is enabled based on the same feature.
    
    See blink-dev intent to ship update here: https://groups.google.com/a/chromium.org/forum/#!searchin/blink-dev/Payment$20Request%7Csort:relevance/blink-dev/p1DYoxHlkKg/0mP2L5cyBQAJ
    
    BUG=717660
    TEST=existing
    
    Review-Url: https://codereview.chromium.org/2855923002
    Cr-Commit-Position: refs/heads/master@{#468845}

--- chrome/browser/payments/site_per_process_payments_browsertest.cc
+++ chrome/browser/payments/site_per_process_payments_browsertest.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
-#include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_utils.h"
@@ -34,10 +33,7 @@ class SitePerProcessPaymentsBrowserTest : public InProcessBrowserTest {
     // HTTPS server only serves a valid cert for localhost, so this is needed
     // to load pages from other hosts without an error.
     command_line->AppendSwitch(switches::kIgnoreCertificateErrors);
-    command_line->AppendSwitch(
-        switches::kEnableExperimentalWebPlatformFeatures);
-    command_line->AppendSwitchASCII(switches::kEnableFeatures,
-                                    features::kWebPayments.name);
+
     // Append --site-per-process flag.
     content::IsolateAllSitesForTesting(command_line);
   }
