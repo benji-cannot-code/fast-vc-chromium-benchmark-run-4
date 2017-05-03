3594fd2f38958cb837ef73d500daed68b1a81f77
chongz <chongz@chromium.org>
2017-05-03 14:05:33 -0700

    [VSync Queue] Flush input in CommitComplete()
    We should call |DeliverInputForBeginFrame()| in two places:
    1. Low latency mode (begin main frame and commit every frame)
      * Flush input in |WillBeginImplFrame()| and the commit should include all input
    2. High latency mode (begin main frame and commit are late)
      * Flush input in |CommitComplete()| and before |PrepareTiles()|
      * Also make sure it's outside an impl frame
    
    Tested locally and checkerboard issue seems to be fixed.
    
    Graphics-dev discussion:
    https://groups.google.com/a/chromium.org/d/msg/graphics-dev/xOdp3hIxZvg/SQtGediBEAAJ
    
    BUG=704732
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2841263002
    Cr-Commit-Position: refs/heads/master@{#469118}

--- cc/trees/layer_tree_host_impl.h
+++ cc/trees/layer_tree_host_impl.h
@@ -89,6 +89,11 @@ enum class GpuRasterizationStatus {
   MSAA_CONTENT,
 };
 
+enum class ImplThreadPhase {
+  IDLE,
+  INSIDE_IMPL_FRAME,
+};
+
 // LayerTreeHost->Proxy callback interface.
 class LayerTreeHostImplClient {
  public:
@@ -869,6 +874,8 @@ class CC_EXPORT LayerTreeHostImpl
   bool has_scrolled_by_touch_;
 
   bool touchpad_and_wheel_scroll_latching_enabled_;
+
+  ImplThreadPhase impl_thread_phase_;
 
   DISALLOW_COPY_AND_ASSIGN(LayerTreeHostImpl);
 };
