3b8a9f9d29b40b6b32e9d62d4099b479473108db
szager <szager@chromium.org>
2017-05-03 01:57:37 -0700

    Don't update viewport size from UpdateAfterLayout.
    This was a bug introduced in:
    
    https://codereview.chromium.org/2848043002
    
    BUG=717444
    
    Review-Url: https://codereview.chromium.org/2854863002
    Cr-Commit-Position: refs/heads/master@{#468922}

--- third_party/WebKit/Source/web/WebViewImpl.cpp
+++ third_party/WebKit/Source/web/WebViewImpl.cpp
@@ -3692,7 +3692,7 @@ void WebViewImpl::ResizeAfterLayout(WebLocalFrameImpl* webframe) {
   if (GetPageScaleConstraintsSet().ConstraintsDirty())
     RefreshPageScaleFactorAfterLayout();
 
-  UpdateICBAndResizeViewport();
+  ResizeFrameView(webframe);
 }
 
 void WebViewImpl::LayoutUpdated(WebLocalFrameImpl* webframe) {
