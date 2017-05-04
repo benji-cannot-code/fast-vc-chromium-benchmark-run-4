aea20130679239f76aa0a934ec116566f2ac880d
mthiesse <mthiesse@chromium.org>
2017-05-04 16:46:15 -0700

    VR: Don't stay in VR when exiting webVR if the activity doesn't support VR browsing.
    BUG=718610
    
    Review-Url: https://codereview.chromium.org/2862993002
    Cr-Commit-Position: refs/heads/master@{#469525}

--- chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellDelegate.java
@@ -615,7 +615,7 @@ public class VrShellDelegate implements ApplicationStatus.ActivityStateListener,
     private boolean exitWebVRPresent() {
         if (!mInVr) return false;
         mVrShell.setWebVrModeEnabled(false);
-        if (!isVrShellEnabled(mVrSupportLevel)) {
+        if (!isVrShellEnabled(mVrSupportLevel) || !activitySupportsVrBrowsing(mActivity)) {
             shutdownVr(false /* isPausing */, true /* showTransition */);
         }
         return true;
