35e08187aa9d2fd5bd302a062a4c0446e0391f4a
johnme <johnme@chromium.org>
2017-05-04 09:38:04 -0700

    Mark a FirstRunIntegrationTest flaky
    FirstRunIntegrationTest.testDefaultSearchEngine_ShowExisting was
    recently added by https://codereview.chromium.org/2844323003.
    
    However it's been consistently failing on Lollipop Phone Tester, and
    flakily failing on other Android bots, so this patch disables it.
    
    BUG=718461
    TBR=dfalcantara
    NOTRY=true
    SKIPTREECHECKS=true
    
    Review-Url: https://codereview.chromium.org/2863653002
    Cr-Commit-Position: refs/heads/master@{#469356}

--- chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunIntegrationTest.java
@@ -28,6 +28,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.annotations.SuppressFBWarnings;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.customtabs.CustomTabActivity;
@@ -241,8 +242,9 @@ public class FirstRunIntegrationTest {
         runSearchEnginePromptTest(LocaleManager.SEARCH_ENGINE_PROMO_DONT_SHOW);
     }
 
-    @Test
-    @MediumTest
+    // @Test
+    // @MediumTest
+    @DisabledTest // crbug.com/718461
     public void testDefaultSearchEngine_ShowExisting() throws Exception {
         runSearchEnginePromptTest(LocaleManager.SEARCH_ENGINE_PROMO_SHOW_EXISTING);
     }
