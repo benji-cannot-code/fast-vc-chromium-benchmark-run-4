9a5b3e88a70f0d4991dbe7e12b4f3c9d7f32244d
dtrainor <dtrainor@chromium.org>
2017-05-04 10:34:50 -0700

    Add the download component and initial setup
    - Add the BackgroundDownloadService interface and associated files.  Set
    up the test hooks and tie it into the proper Chrome and
    component_unittest targets.
    
    BUG=717180
    
    Review-Url: https://codereview.chromium.org/2851303003
    Cr-Commit-Position: refs/heads/master@{#469371}

---
+++ components/download/internal/download_service_impl.h
@@ -0,0 +1,39 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_DOWNLOAD_INTERNAL_DOWNLOAD_SERVICE_IMPL_H_
+#define COMPONENTS_DOWNLOAD_INTERNAL_DOWNLOAD_SERVICE_IMPL_H_
+
+#include <string>
+
+#include "base/macros.h"
+#include "components/download/public/download_service.h"
+
+namespace download {
+
+struct DownloadParams;
+struct SchedulingParams;
+
+// The internal implementation of the DownloadService.
+class DownloadServiceImpl : public DownloadService {
+ public:
+  DownloadServiceImpl();
+  ~DownloadServiceImpl() override;
+
+  // DownloadService implementation.
+  void StartDownload(const DownloadParams& download_params) override;
+  void PauseDownload(const std::string& guid) override;
+  void ResumeDownload(const std::string& guid) override;
+  void CancelDownload(const std::string& guid) override;
+  void ChangeDownloadCriteria(const std::string& guid,
+                              const SchedulingParams& params) override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(DownloadServiceImpl);
+};
+
+}  // namespace download
+
+#endif  // COMPONENTS_DOWNLOAD_INTERNAL_DOWNLOAD_SERVICE_IMPL_H_
