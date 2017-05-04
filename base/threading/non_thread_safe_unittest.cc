806b061fc7f653c525a749068617970b91dea4d7
rsesek <rsesek@chromium.org>
2017-05-04 10:42:30 -0700

    Run NonThreadSafeDeathTests in the threadsafe gtest mode.
    On Mac, //base/threading uses CoreFoundation, which causes forked child
    processes to violate the framework's requirement that CF may not be used
    after-fork-before-exec.
    
    BUG=717490
    R=mark@chromium.org
    
    Review-Url: https://codereview.chromium.org/2865453002
    Cr-Commit-Position: refs/heads/master@{#469375}

--- base/threading/non_thread_safe_unittest.cc
+++ base/threading/non_thread_safe_unittest.cc
@@ -114,6 +114,7 @@ void NonThreadSafeClass::MethodOnDifferentThreadImpl() {
 
 #if DCHECK_IS_ON()
 TEST(NonThreadSafeDeathTest, MethodNotAllowedOnDifferentThreadInDebug) {
+  ::testing::FLAGS_gtest_death_test_style = "threadsafe";
   ASSERT_DCHECK_DEATH({ NonThreadSafeClass::MethodOnDifferentThreadImpl(); });
 }
 #else
@@ -137,6 +138,7 @@ void NonThreadSafeClass::DestructorOnDifferentThreadImpl() {
 
 #if DCHECK_IS_ON()
 TEST(NonThreadSafeDeathTest, DestructorNotAllowedOnDifferentThreadInDebug) {
+  ::testing::FLAGS_gtest_death_test_style = "threadsafe";
   ASSERT_DCHECK_DEATH(
       { NonThreadSafeClass::DestructorOnDifferentThreadImpl(); });
 }
