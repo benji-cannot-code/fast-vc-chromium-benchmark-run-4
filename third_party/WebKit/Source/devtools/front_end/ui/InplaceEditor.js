5696309e79d1d316c9381d49a73a65e0e449330d
einbinder <einbinder@chromium.org>
2017-05-03 19:39:07 -0700

    DevTools: Migrate from -webkit-user-modify to contenteditable
    BUG=716649
    
    Review-Url: https://codereview.chromium.org/2859623002
    Cr-Commit-Position: refs/heads/master@{#469249}

--- third_party/WebKit/Source/devtools/front_end/ui/InplaceEditor.js
+++ third_party/WebKit/Source/devtools/front_end/ui/InplaceEditor.js
@@ -31,6 +31,7 @@ UI.InplaceEditor = class {
   setUpEditor(editingContext) {
     var element = editingContext.element;
     element.classList.add('editing');
+    element.setAttribute('contenteditable', 'plaintext-only');
 
     var oldTabIndex = element.getAttribute('tabIndex');
     if (typeof oldTabIndex !== 'number' || oldTabIndex < 0)
@@ -42,6 +43,7 @@ UI.InplaceEditor = class {
   closeEditor(editingContext) {
     var element = editingContext.element;
     element.classList.remove('editing');
+    element.removeAttribute('contenteditable');
 
     if (typeof editingContext.oldTabIndex !== 'number')
       element.removeAttribute('tabIndex');
