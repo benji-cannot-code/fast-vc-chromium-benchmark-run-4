eb264c7a077cfd71507fef9349d0c1120c82d14f
jbroman <jbroman@chromium.org>
2017-05-04 12:54:34 -0700

    Use the compile-time constant for v8::ValueSerializer format version.
    BUG=704293
    TBR=jochen@chromium.org
    
    Review-Url: https://codereview.chromium.org/2801053003
    Cr-Commit-Position: refs/heads/master@{#469437}

--- content/browser/indexed_db/indexed_db_data_format_version.cc
+++ content/browser/indexed_db/indexed_db_data_format_version.cc
@@ -6,12 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/indexed_db/indexed_db_data_format_version.h"
 
 #include "third_party/WebKit/public/web/WebSerializedScriptValueVersion.h"
+#include "v8/include/v8-value-serializer-version.h"
 
 namespace content {
 
 // static
 IndexedDBDataFormatVersion IndexedDBDataFormatVersion::current_(
-    0,
+    v8::CurrentValueSerializerFormatVersion(),
     blink::kSerializedScriptValueVersion);
 
 }  // namespace content
