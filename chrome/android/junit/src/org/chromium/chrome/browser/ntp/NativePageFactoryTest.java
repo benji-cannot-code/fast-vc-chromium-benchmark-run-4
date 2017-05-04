6371dfaa9bb42bebcc9200f2902a1f0a33f104c5
twellington <twellington@chromium.org>
2017-05-04 11:41:22 -0700

    [Home] Fix top-offset for native pages
    BUG=706683
    
    Review-Url: https://codereview.chromium.org/2855033002
    Cr-Commit-Position: refs/heads/master@{#469396}

--- chrome/android/junit/src/org/chromium/chrome/browser/ntp/NativePageFactoryTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/ntp/NativePageFactoryTest.java
@@ -8,6 +8,12 @@ package org.chromium.chrome.browser.ntp;
 import android.app.Activity;
 import android.view.View;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.NativePage;
 import org.chromium.chrome.browser.UrlConstants;
@@ -15,11 +21,6 @@ import org.chromium.chrome.browser.ntp.NativePageFactory.NativePageType;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.testing.local.LocalRobolectricTestRunner;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.annotation.Config;
 
 /**
  * Tests public methods in NativePageFactory.
@@ -103,7 +104,7 @@ public class NativePageFactoryTest {
         }
 
         @Override
-        public NativePage buildRecentTabsPage(Activity activity, Tab tab) {
+        public NativePage buildRecentTabsPage(ChromeActivity activity, Tab tab) {
             return new MockNativePage(NativePageType.RECENT_TABS);
         }
     }
