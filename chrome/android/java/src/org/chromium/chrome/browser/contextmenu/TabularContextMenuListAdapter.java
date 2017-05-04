a6e7290f2a8131215368fa03d98b04714ea5af79
ltian <ltian@chromium.org>
2017-05-04 12:11:05 -0700

    [Android] Increase the clickable area of direct share icon
    This CL increases the clickable area of direct share icon:
    1. Change the icon size from 20dp to 24dp.
    2. Add 8dp margin on both top and bottom of the icon.
    3. If direct share icon is shown, remove the 15dp padding on the right
    of the wrapping LinearLayout and add 15dp margin of direct share icon.
    This makes the right area of direct share icon responses to the click
    event of direct share rather than share.
    
    The design spec is here: https://bbergher.googleplex.com/specs/context-menu/.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2858433002
    Cr-Commit-Position: refs/heads/master@{#469415}

--- chrome/android/java/src/org/chromium/chrome/browser/contextmenu/TabularContextMenuListAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextmenu/TabularContextMenuListAdapter.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.Space;
 import android.widget.TextView;
 
 import org.chromium.chrome.R;
@@ -70,6 +71,8 @@ class TabularContextMenuListAdapter extends BaseAdapter {
             viewHolder.mText = (TextView) convertView.findViewById(R.id.context_text);
             viewHolder.mShareIcon =
                     (ImageView) convertView.findViewById(R.id.context_menu_share_icon);
+            viewHolder.mRightPadding =
+                    (Space) convertView.findViewById(R.id.context_menu_right_padding);
 
             convertView.setTag(viewHolder);
         } else {
@@ -95,9 +98,11 @@ class TabularContextMenuListAdapter extends BaseAdapter {
                         mOnDirectShare.run();
                     }
                 });
+                viewHolder.mRightPadding.setVisibility(View.GONE);
             }
         } else {
             viewHolder.mShareIcon.setVisibility(View.GONE);
+            viewHolder.mRightPadding.setVisibility(View.VISIBLE);
         }
 
         return convertView;
@@ -107,5 +112,6 @@ class TabularContextMenuListAdapter extends BaseAdapter {
         ImageView mIcon;
         TextView mText;
         ImageView mShareIcon;
+        Space mRightPadding;
     }
 }
