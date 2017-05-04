8b8dd3c78eea831f7429a216e879eff56066133c
dahollings <dahollings@chromium.org>
2017-05-04 13:57:48 -0700

    Deprecate remove_rewritelength flag.
    This cl merges server change 154325175 by yasong.
    
    BUG=488484
    
    Review-Url: https://codereview.chromium.org/2861893002
    Cr-Commit-Position: refs/heads/master@{#469463}

--- net/spdy/chromium/spdy_flags.h
+++ net/spdy/chromium/spdy_flags.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace net {
 
 NET_EXPORT_PRIVATE extern bool FLAGS_chromium_http2_flag_log_compressed_size;
-NET_EXPORT_PRIVATE extern bool FLAGS_chromium_http2_flag_remove_rewritelength;
 NET_EXPORT_PRIVATE extern bool
     FLAGS_chromium_http2_flag_spdy_use_hpack_decoder3;
 NET_EXPORT_PRIVATE extern bool FLAGS_use_nested_spdy_framer_decoder;
