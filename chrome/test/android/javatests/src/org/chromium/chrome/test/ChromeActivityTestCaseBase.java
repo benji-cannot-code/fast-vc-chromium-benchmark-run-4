1bba14a156a3878fbe0467ab183defac2a18d82c
yolandyan <yolandyan@chromium.org>
2017-05-04 11:13:12 -0700

    Fix ChromeActivityTestCaseBase tearDown mistake
    In the JUnit4 conversion CL (https://codereview.chromium.org/2815453003)
    super.tearDown() call was mistakenly removed.
    
    BUG=718454
    
    Review-Url: https://codereview.chromium.org/2861433006
    Cr-Commit-Position: refs/heads/master@{#469379}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/ChromeActivityTestCaseBase.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/ChromeActivityTestCaseBase.java
@@ -60,6 +60,7 @@ public abstract class ChromeActivityTestCaseBase<T extends ChromeActivity>
     @Override
     protected void tearDown() throws Exception {
         mTestCommon.tearDown();
+        super.tearDown();
     }
 
     /**
