6a97bbba08f7d8a229f5c11ff57d52d290e31fbc
eroman <eroman@chromium.org>
2017-05-04 10:39:32 -0700

    Add tests for PathBuilder when certificates are distrusted.
    Review-Url: https://codereview.chromium.org/2854263004
    Cr-Commit-Position: refs/heads/master@{#469373}

--- net/cert/internal/verify_certificate_chain.cc
+++ net/cert/internal/verify_certificate_chain.cc
@@ -25,6 +25,7 @@ namespace net {
 
 DEFINE_CERT_ERROR_ID(kValidityFailedNotAfter, "Time is after notAfter");
 DEFINE_CERT_ERROR_ID(kValidityFailedNotBefore, "Time is before notBefore");
+DEFINE_CERT_ERROR_ID(kCertIsDistrusted, "Certificate is distrusted");
 
 namespace {
 
@@ -63,7 +64,6 @@ DEFINE_CERT_ERROR_ID(kEkuLacksServerAuth,
                      "The extended key usage does not include server auth");
 DEFINE_CERT_ERROR_ID(kEkuLacksClientAuth,
                      "The extended key usage does not include client auth");
-DEFINE_CERT_ERROR_ID(kCertIsDistrusted, "Certificate is distrusted");
 DEFINE_CERT_ERROR_ID(kCertIsNotTrustAnchor,
                      "Certificate is not a trust anchor");
 
