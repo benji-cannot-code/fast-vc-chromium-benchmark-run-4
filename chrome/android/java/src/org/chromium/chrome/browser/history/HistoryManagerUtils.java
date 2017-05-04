cae27270e3ca857a2723462936c9547228c24130
twellington <twellington@chromium.org>
2017-05-04 09:03:49 -0700

    [Home] Update util methods to open bookmarks/downloads/history
    Downloads, history and bookmarks all have utility methods that open
    their UIs. When Chrome Home is enabled, opening these UIs should
    open the bottom sheet and show the corresponding BottomSheetContent.
    Typed urls (e.g. chrome://history) will still open as native pages
    rather than in the sheet.
    
    Also adds tests for the BottomSheetContentController.
    
    BUG=716250
    
    Review-Url: https://codereview.chromium.org/2861453002
    Cr-Commit-Position: refs/heads/master@{#469349}

--- chrome/android/java/src/org/chromium/chrome/browser/history/HistoryManagerUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/history/HistoryManagerUtils.java
@@ -5,11 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.history;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 
 import org.chromium.base.ContextUtils;
+import org.chromium.chrome.R;
+import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.IntentHandler;
 import org.chromium.chrome.browser.UrlConstants;
@@ -39,16 +40,22 @@ public class HistoryManagerUtils {
     }
 
     /**
-    * Opens the browsing history manager.
-    */
-    public static void showHistoryManager(Activity activity, Tab tab) {
+     * Opens the browsing history manager.
+     *
+     * @param activity The {@link ChromeActivity} that owns the {@link HistoryManager}.
+     * @param tab The {@link Tab} to used to display the native page version of the
+     *            {@link HistoryManager}.
+     */
+    public static void showHistoryManager(ChromeActivity activity, Tab tab) {
         if (!isAndroidHistoryManagerEnabled()) {
             tab.loadUrl(new LoadUrlParams(UrlConstants.HISTORY_URL, PageTransition.AUTO_TOPLEVEL));
             return;
         }
 
         Context appContext = ContextUtils.getApplicationContext();
-        if (DeviceFormFactor.isTablet(appContext)) {
+        if (activity.getBottomSheet() != null) {
+            activity.getBottomSheetContentController().showContentAndOpenSheet(R.id.action_history);
+        } else if (DeviceFormFactor.isTablet(appContext)) {
             // History shows up as a tab on tablets.
             LoadUrlParams params = new LoadUrlParams(UrlConstants.NATIVE_HISTORY_URL);
             tab.loadUrl(params);
--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsNavigationDelegateImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsNavigationDelegateImpl.java
@@ -5,9 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.suggestions;
 
-import android.app.Activity;
-
 import org.chromium.base.metrics.RecordUserAction;
+import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.NativePageHost;
 import org.chromium.chrome.browser.UrlConstants;
 import org.chromium.chrome.browser.bookmarks.BookmarkUtils;
@@ -40,13 +39,13 @@ public class SuggestionsNavigationDelegateImpl implements SuggestionsNavigationD
     private static final String NEW_TAB_URL_HELP =
             "https://support.google.com/chrome/?p=new_tab";
 
-    private final Activity mActivity;
+    private final ChromeActivity mActivity;
     private final Profile mProfile;
 
     private final NativePageHost mHost;
     private final TabModelSelector mTabModelSelector;
 
-    public SuggestionsNavigationDelegateImpl(Activity activity, Profile profile,
+    public SuggestionsNavigationDelegateImpl(ChromeActivity activity, Profile profile,
             NativePageHost host, TabModelSelector tabModelSelector) {
         mActivity = activity;
         mProfile = profile;
