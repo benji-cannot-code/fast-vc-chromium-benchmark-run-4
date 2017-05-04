7b584a6a17454ac66c67f75ac58776459114d597
ntfschr <ntfschr@chromium.org>
2017-05-04 15:13:58 -0700

    WebView: create AwSafeBrowsingBlockingPage
    No change in logic.
    
    This creates an initial implementation of a new WebView-specific
    blocking page. This has no change in logic, but paves the way for
    choosing between two different blocking pages (loud and quiet).
    
    Currently, all this does is specify SBErrorDisplayOptions without
    depending on the defaults set in BaseBlockingPage.
    
    This exposes CreateControllerClient() in BaseBlockingPage as protected
    so that AwSafeBrowsingBlockingPage can take advantage of it.
    
    BUG=718545
    
    Review-Url: https://codereview.chromium.org/2863723002
    Cr-Commit-Position: refs/heads/master@{#469488}

--- android_webview/browser/aw_safe_browsing_ui_manager.cc
+++ android_webview/browser/aw_safe_browsing_ui_manager.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "android_webview/browser/aw_safe_browsing_ui_manager.h"
 
+#include "android_webview/browser/aw_safe_browsing_blocking_page.h"
 #include "content/public/browser/browser_thread.h"
 
 using content::BrowserThread;
@@ -32,6 +33,11 @@ void AwSafeBrowsingUIManager::DisplayBlockingPage(
     return;
   }
   safe_browsing::BaseUIManager::DisplayBlockingPage(resource);
+}
+
+void AwSafeBrowsingUIManager::ShowBlockingPageForResource(
+    const UnsafeResource& resource) {
+  AwSafeBrowsingBlockingPage::ShowBlockingPage(this, resource);
 }
 
 }  // namespace android_webview
