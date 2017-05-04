95e1294ebbd42cb8d7b6a7ac8a47a456c3e4fd04
ericrk <ericrk@chromium.org>
2017-05-04 13:58:37 -0700

    Determine mask UVs based on texture size
    Currently, mask SINGLE_TEXTURE_MASK UVs are determined solely by the size
    of the masked layer. This means that if the mask texture size does not match the
    expected texture size, we will end up squishing the mask when it's applied.
    
    This CL adds a new out param, |resource_uv_size| to GetContentsResourceId. This
    param represents the UV coordinate size of the mask contents within its texture.
    
    This factor is used to adjust the existing calculation, allowing for non-exact mask
    texture sizes.
    
    R=sunxd@chromium.org, enne@chromium.org
    BUG=713872
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2828353003
    Cr-Commit-Position: refs/heads/master@{#469464}

--- cc/test/fake_picture_layer.h
+++ cc/test/fake_picture_layer.h
@@ -43,6 +43,10 @@ class FakePictureLayer : public PictureLayer {
     force_unsuitable_for_gpu_rasterization_ = flag;
   }
 
+  void set_fixed_tile_size(gfx::Size fixed_tile_size) {
+    fixed_tile_size_ = fixed_tile_size;
+  }
+
  private:
   explicit FakePictureLayer(ContentLayerClient* client);
   FakePictureLayer(ContentLayerClient* client,
@@ -53,6 +57,7 @@ class FakePictureLayer : public PictureLayer {
   bool always_update_resources_;
 
   bool force_unsuitable_for_gpu_rasterization_;
+  gfx::Size fixed_tile_size_;
 };
 
 }  // namespace cc
