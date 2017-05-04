025255456fa2ada0b081ef7dba0bec5b4c821cdb
tanvir.rizvi <tanvir.rizvi@samsung.com>
2017-05-04 01:57:38 -0700

    Remove use_selection_height from LayoutText::AbsoluteQuadsForRange
    All calls to LayoutText::AbsoluteQuadsForRange don't use use_selection_height,
    so use_selection_height is removed from LayoutText::AbsoluteQuadsForRange and
    its callee functions.
    
    BUG=691198
    
    Review-Url: https://codereview.chromium.org/2853343003
    Cr-Commit-Position: refs/heads/master@{#469295}

--- third_party/WebKit/Source/core/dom/Range.cpp
+++ third_party/WebKit/Source/core/dom/Range.cpp
@@ -1486,8 +1486,7 @@ IntRect Range::BoundingBox() const {
   return result;
 }
 
-void Range::TextQuads(Vector<FloatQuad>& quads,
-                      bool use_selection_height) const {
+void Range::TextQuads(Vector<FloatQuad>& quads) const {
   Node* start_container = &start_.Container();
   DCHECK(start_container);
   Node* end_container = &end_.Container();
@@ -1504,8 +1503,7 @@ void Range::TextQuads(Vector<FloatQuad>& quads,
     unsigned end_offset = node == end_container
                               ? end_.Offset()
                               : std::numeric_limits<unsigned>::max();
-    layout_text->AbsoluteQuadsForRange(quads, start_offset, end_offset,
-                                       use_selection_height);
+    layout_text->AbsoluteQuadsForRange(quads, start_offset, end_offset);
   }
 }
 
