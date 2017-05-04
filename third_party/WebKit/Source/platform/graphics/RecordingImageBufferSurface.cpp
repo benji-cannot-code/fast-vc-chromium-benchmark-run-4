9b9ff3202545316abd59264e07f377ecb43b2456
Adrienne Walker <enne@chromium.org>
2017-05-04 11:13:46 -0700

    Fix crash when fallback canvas surface is too big
    It's possible for SkSurface creation of a bitmap to fail for various
    reasons, although most commonly because it's too big.
    
    RecordingImageBufferSurface and UnacceleratedImageBufferSurface
    should handle the case when the SkSurface for the fallback
    surface fails to be created.
    
    Bug: 705498
    Change-Id: I348ed2199403053d5690d8c3c0f07a80837f66e3
    Reviewed-on: https://chromium-review.googlesource.com/493775
    Commit-Queue: enne <enne@chromium.org>
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#469444}

--- third_party/WebKit/Source/platform/graphics/RecordingImageBufferSurface.cpp
+++ third_party/WebKit/Source/platform/graphics/RecordingImageBufferSurface.cpp
@@ -60,6 +60,7 @@ void RecordingImageBufferSurface::SetImageBuffer(ImageBuffer* image_buffer) {
     image_buffer_->ResetCanvas(current_frame_->getRecordingCanvas());
   }
   if (fallback_surface_) {
+    DCHECK(fallback_surface_->IsValid());
     fallback_surface_->SetImageBuffer(image_buffer);
   }
 }
@@ -74,6 +75,8 @@ bool RecordingImageBufferSurface::WritePixels(const SkImageInfo& orig_info,
     if (write_rect.Contains(IntRect(IntPoint(), size())))
       WillOverwriteCanvas();
     FallBackToRasterCanvas(kFallbackReasonWritePixels);
+    if (!fallback_surface_->IsValid())
+      return false;
   }
   return fallback_surface_->WritePixels(orig_info, pixels, row_bytes, x, y);
 }
@@ -96,6 +99,10 @@ void RecordingImageBufferSurface::FallBackToRasterCanvas(
 
   fallback_surface_ = WTF::WrapUnique(new UnacceleratedImageBufferSurface(
       size(), GetOpacityMode(), kInitializeImagePixels, color_params()));
+  // If the fallback surface fails to be created, then early out.
+  if (!fallback_surface_->IsValid())
+    return;
+
   fallback_surface_->SetImageBuffer(image_buffer_);
 
   if (previous_frame_) {
@@ -178,12 +185,16 @@ sk_sp<SkImage> RecordingImageBufferSurface::NewImageSnapshot(
     SnapshotReason reason) {
   if (!fallback_surface_)
     FallBackToRasterCanvas(SnapshotReasonToFallbackReason(reason));
+  if (!fallback_surface_->IsValid())
+    return nullptr;
   return fallback_surface_->NewImageSnapshot(hint, reason);
 }
 
 PaintCanvas* RecordingImageBufferSurface::Canvas() {
-  if (fallback_surface_)
+  if (fallback_surface_) {
+    DCHECK(fallback_surface_->IsValid());
     return fallback_surface_->Canvas();
+  }
 
   DCHECK(current_frame_->getRecordingCanvas());
   return current_frame_->getRecordingCanvas();
