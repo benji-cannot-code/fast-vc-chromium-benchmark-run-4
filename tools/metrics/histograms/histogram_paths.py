213d260c989e8631b32eb6d68dafc0c67ea429c7
Steven Holte <holte@google.com>
2017-05-03 17:05:48 -0700

    Split histogram enums to enums.xml
    Instead of picking up a simple copy of histograms.xml, the metrics_metadata target will now by
    merging histograms.xml and enums.xml into single file.
    
    Scripts for enums now update enums.xml, pretty_print.py will enforce that the split is continued,
    and validation is updated to run on the merged result.
    
    To be landed after the server side parsing code is fully migrated to using the archived
    metrics_metadata.zip files.
    
    BUG:699328
    
    TBR = tnagel,bbudge
    
    Change-Id: Ia701423b7f2117991275477a92f77048a542b6db
    Reviewed-on: https://chromium-review.googlesource.com/481901
    Commit-Queue: Steven Holte <holte@chromium.org>
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#469239}

---
+++ tools/metrics/histograms/histogram_paths.py
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Paths to description XML files in this directory."""
+
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
+import path_util
+
+HISTOGRAMS_XML = path_util.GetInputFile(
+    'tools/metrics/histograms/histograms.xml')
+
+ENUMS_XML = path_util.GetInputFile(
+    'tools/metrics/histograms/enums.xml')
+
+ALL_XMLS = [HISTOGRAMS_XML, ENUMS_XML]
