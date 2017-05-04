42a0bbb74c9a0034ed16966846ac6e84d836506b
rhalavati <rhalavati@chromium.org>
2017-05-04 07:25:41 -0700

    Network traffic annotation added to headless.
    Network traffic annotation is added to network request of .
    headless/public/util/http_url_fetcher.cc
    headless/public/util/generic_url_request_job_test.cc
    
    BUG=656607
    
    Review-Url: https://codereview.chromium.org/2857623002
    Cr-Commit-Position: refs/heads/master@{#469326}

--- headless/public/util/generic_url_request_job_test.cc
+++ headless/public/util/generic_url_request_job_test.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/elements_upload_data_stream.h"
 #include "net/base/upload_bytes_element_reader.h"
 #include "net/http/http_response_headers.h"
+#include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
 #include "net/url_request/url_request_job_factory_impl.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -166,7 +167,8 @@ class GenericURLRequestJobTest : public testing::Test {
     json_fetch_reply_map_[url.spec()] = json_reply;
 
     std::unique_ptr<net::URLRequest> request(url_request_context_.CreateRequest(
-        url, net::DEFAULT_PRIORITY, &request_delegate_));
+        url, net::DEFAULT_PRIORITY, &request_delegate_,
+        TRAFFIC_ANNOTATION_FOR_TESTS));
     request->Start();
     base::RunLoop().RunUntilIdle();
     return request;
@@ -179,7 +181,8 @@ class GenericURLRequestJobTest : public testing::Test {
     json_fetch_reply_map_[url.spec()] = json_reply;
 
     std::unique_ptr<net::URLRequest> request(url_request_context_.CreateRequest(
-        url, net::DEFAULT_PRIORITY, &request_delegate_));
+        url, net::DEFAULT_PRIORITY, &request_delegate_,
+        TRAFFIC_ANNOTATION_FOR_TESTS));
     request->set_method("POST");
     request->set_upload(net::ElementsUploadDataStream::CreateWithReader(
         base::MakeUnique<net::UploadBytesElementReader>(post_data.data(),
--- headless/public/util/http_url_fetcher.cc
+++ headless/public/util/http_url_fetcher.cc
@@ -10,8 +10,40 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/upload_bytes_element_reader.h"
 #include "net/cert/cert_status_flags.h"
 #include "net/http/http_response_headers.h"
+#include "net/traffic_annotation/network_traffic_annotation.h"
 #include "net/url_request/url_request.h"
 #include "net/url_request/url_request_context.h"
+
+namespace {
+
+constexpr net::NetworkTrafficAnnotationTag kTrafficAnnotation =
+    net::DefineNetworkTrafficAnnotation("headless_url_request", R"(
+        semantics {
+          sender: "Headless"
+          description:
+            "Headless Chromium allows running Chromium in a headless/server "
+            "environment. Expected use cases include loading web pages, "
+            "extracting metadata (e.g., the DOM) and generating bitmaps "
+            "from page contents, using all the modern web platform features "
+            "provided by Chromium and Blink."
+          trigger:
+            "User running Chrome in headless mode, please refer to https://"
+            "chromium.googlesource.com/chromium/src/+/lkgr/headless/README.md "
+            "for more information."
+          data: "Any data based on given request."
+          destination: OTHER
+        }
+        policy {
+          cookies_allowed: true
+          cookies_store:
+            "Various, but cookies stores are deleted when session ends."
+          setting:
+            "This feature cannot be disabled by settings, but it is used only "
+            "if user enters the Headless mode."
+          policy_exception_justification: "Not implemented."
+        })");
+
+}  // namespace
 
 namespace headless {
 
@@ -68,7 +100,8 @@ HttpURLFetcher::Delegate::Delegate(
       buf_(new net::IOBuffer(kBufSize)),
       request_(url_request_context->CreateRequest(rewritten_url,
                                                   net::DEFAULT_PRIORITY,
-                                                  this)),
+                                                  this,
+                                                  kTrafficAnnotation)),
       result_listener_(result_listener) {
   request_->set_method(method);
 
