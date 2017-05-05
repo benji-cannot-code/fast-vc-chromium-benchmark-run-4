f45ebe27ee95e763678715af9642b457e27914ce
japhet <japhet@chromium.org>
2017-05-05 15:09:11 -0700

    Simplify FrameLoader::Clear(), remove FrameLoader::check_timer_
    Clear() is full of strange, old logic. Several clauses are outright unnecessary, or have better-encapsulated homes.
    
    check_timer_ appears to have two uses, one of which is vestigial, the other of which can be better-situated to not require a timer.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2854853006
    Cr-Commit-Position: refs/heads/master@{#469774}

--- third_party/WebKit/Source/core/html/HTMLFrameOwnerElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLFrameOwnerElement.cpp
@@ -177,6 +177,8 @@ void HTMLFrameOwnerElement::ClearContentFrame() {
 
   for (ContainerNode* node = this; node; node = node->ParentOrShadowHostNode())
     node->DecrementConnectedSubframeCount();
+
+  GetDocument().CheckCompleted();
 }
 
 void HTMLFrameOwnerElement::DisconnectContentFrame() {
--- third_party/WebKit/Source/core/loader/FrameLoader.cpp
+++ third_party/WebKit/Source/core/loader/FrameLoader.cpp
@@ -240,9 +240,6 @@ FrameLoader::FrameLoader(LocalFrame* frame)
     : frame_(frame),
       progress_tracker_(ProgressTracker::Create(frame)),
       in_stop_all_loaders_(false),
-      check_timer_(TaskRunnerHelper::Get(TaskType::kNetworking, frame),
-                   this,
-                   &FrameLoader::CheckTimerFired),
       forced_sandbox_flags_(kSandboxNone),
       dispatching_did_clear_window_object_in_main_world_(false),
       protect_provisional_loader_(false),
@@ -304,10 +301,8 @@ void FrameLoader::SetDefersLoading(bool defers) {
       document->ResumeScheduledTasks();
   }
 
-  if (!defers) {
+  if (!defers)
     frame_->GetNavigationScheduler().StartTimer();
-    ScheduleCheckCompleted();
-  }
 }
 
 void FrameLoader::SaveScrollState() {
@@ -375,16 +370,9 @@ void FrameLoader::Clear() {
     return;
 
   frame_->GetEditor().Clear();
-  frame_->GetDocument()->RemoveFocusedElementOfSubtree(frame_->GetDocument());
   frame_->GetEventHandler().Clear();
   if (frame_->View())
     frame_->View()->Clear();
-
-  frame_->GetScriptController().EnableEval();
-
-  frame_->GetNavigationScheduler().Cancel();
-
-  check_timer_.Stop();
 
   if (state_machine_.IsDisplayingInitialEmptyDocument())
     state_machine_.AdvanceTo(FrameLoaderStateMachine::kCommittedFirstRealLoad);
@@ -489,19 +477,6 @@ void FrameLoader::DidFinishNavigation() {
   Frame* parent = frame_->Tree().Parent();
   if (parent && parent->IsLocalFrame())
     ToLocalFrame(parent)->GetDocument()->CheckCompleted();
-}
-
-void FrameLoader::CheckTimerFired(TimerBase*) {
-  if (Page* page = frame_->GetPage()) {
-    if (page->Suspended())
-      return;
-  }
-  frame_->GetDocument()->CheckCompleted();
-}
-
-void FrameLoader::ScheduleCheckCompleted() {
-  if (!check_timer_.IsActive())
-    check_timer_.StartOneShot(0, BLINK_FROM_HERE);
 }
 
 Frame* FrameLoader::Opener() {
@@ -982,7 +957,6 @@ void FrameLoader::StopAllLoaders() {
   if (!protect_provisional_loader_)
     DetachDocumentLoader(provisional_document_loader_);
 
-  check_timer_.Stop();
   frame_->GetNavigationScheduler().Cancel();
 
   // It's possible that the above actions won't have stopped loading if load
@@ -1210,9 +1184,6 @@ void FrameLoader::Detach() {
   DetachDocumentLoader(document_loader_);
   DetachDocumentLoader(provisional_document_loader_);
 
-  Frame* parent = frame_->Tree().Parent();
-  if (parent && parent->IsLocalFrame())
-    ToLocalFrame(parent)->Loader().ScheduleCheckCompleted();
   if (progress_tracker_) {
     progress_tracker_->Dispose();
     progress_tracker_.Clear();
@@ -1491,7 +1462,6 @@ void FrameLoader::StartLoad(FrameLoadRequest& frame_load_request,
   if (!had_placeholder_client_document_loader ||
       navigation_policy == kNavigationPolicyHandledByClient) {
     frame_->GetNavigationScheduler().Cancel();
-    check_timer_.Stop();
   }
 
   if (frame_load_request.Form())
