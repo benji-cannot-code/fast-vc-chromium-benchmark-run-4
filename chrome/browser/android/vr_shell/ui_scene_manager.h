2f8dc5f128da3a6b61db17d449780a2c00648dc2
mthiesse <mthiesse@chromium.org>
2017-05-05 08:52:39 -0700

    VR: Enable browsing for CCT and make ui_scene_manager aware of whether we're in CCT.
    Note that this is still behind the "Chrome VR" runtime flag.
    
    BUG=718664
    
    Review-Url: https://codereview.chromium.org/2861003003
    Cr-Commit-Position: refs/heads/master@{#469662}

--- chrome/browser/android/vr_shell/ui_scene_manager.h
+++ chrome/browser/android/vr_shell/ui_scene_manager.h
@@ -19,7 +19,7 @@ class VrBrowserInterface;
 
 class UiSceneManager {
  public:
-  UiSceneManager(VrBrowserInterface* browser, UiScene* scene);
+  UiSceneManager(VrBrowserInterface* browser, UiScene* scene, bool in_cct);
   ~UiSceneManager();
 
   base::WeakPtr<UiSceneManager> GetWeakPtr();
@@ -49,6 +49,7 @@ class UiSceneManager {
   bool web_vr_mode_ = false;
   bool secure_origin_ = false;
   bool content_rendering_enabled_ = true;
+  bool in_cct_;
 
   int next_available_id_ = 1;
 
