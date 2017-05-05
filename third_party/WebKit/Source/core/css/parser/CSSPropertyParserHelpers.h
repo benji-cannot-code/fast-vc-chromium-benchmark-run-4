53642b9dd865cab71a98e3a0bec49fb5128d2b9d
bugsnash <bugsnash@chromium.org>
2017-05-04 18:53:12 -0700

    Refactored out property specific logic in animation list parsing.
    Replaced animation list parsing method ConsumeAnimationPropertyList
    with a new more general list parsing method ConsumeCommaSeparatedList.
    This removes the need to pass property information to the list parsing
    method, instead passing a callback function to be called on each of the
    items in the list.
    
    This is pre work to implementing the parseSingleValue method in the
    property APIs for the animation properties that expect a list, as the
    APIs do not take property as an argument.
    
    The new ConsumeCommaSeparatedList method can be used in other parts of
    the code base, which may be done in future patches.
    
    This patch
    - Added ConsumeCommaSeparatedList templated function to
      CSSPropertyParserHelpers
    - Made each animation list property in parseSingleValue use the new
      ConsumeCommaSeparatedList method instead of
      ConsumeAnimationPropertyList
    - Deleted ConsumeAnimationPropertyList method
    
    BUG=668012
    
    Review-Url: https://codereview.chromium.org/2849363002
    Cr-Commit-Position: refs/heads/master@{#469569}

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParserHelpers.h
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParserHelpers.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/CSSCustomIdentValue.h"
 #include "core/css/CSSIdentifierValue.h"
 #include "core/css/CSSPrimitiveValue.h"
+#include "core/css/CSSValueList.h"
 #include "core/css/parser/CSSParserMode.h"
 #include "core/css/parser/CSSParserTokenRange.h"
 #include "platform/Length.h"  // For ValueRange
@@ -120,6 +121,24 @@ CSSIdentifierValue* ConsumeIdent(CSSParserTokenRange& range) {
       !IdentMatches<names...>(range.Peek().Id()))
     return nullptr;
   return CSSIdentifierValue::Create(range.ConsumeIncludingWhitespace().Id());
+}
+
+// ConsumeCommaSeparatedList takes a callback function to call on each item in
+// the list, followed by the arguments to pass to this callback.
+// The first argument to the callback must be the CSSParserTokenRange
+template <typename Func, typename... Args>
+CSSValueList* ConsumeCommaSeparatedList(Func callback,
+                                        CSSParserTokenRange& range,
+                                        Args... args) {
+  CSSValueList* list = CSSValueList::CreateCommaSeparated();
+  do {
+    CSSValue* value = callback(range, args...);
+    if (!value)
+      return nullptr;
+    list->Append(*value);
+  } while (ConsumeCommaIncludingWhitespace(range));
+  DCHECK(list->length());
+  return list;
 }
 
 }  // namespace CSSPropertyParserHelpers
