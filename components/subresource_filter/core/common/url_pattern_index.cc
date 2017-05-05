2d4cb216066fe17c34c3b9f21ede5fc4ef32d3c1
pkalinnikov <pkalinnikov@chromium.org>
2017-05-05 08:15:56 -0700

    Move UrlPatternIndex-specific tests from indexed_ruleset_unittest.
    BUG=713774
    
    Review-Url: https://codereview.chromium.org/2862853002
    Cr-Commit-Position: refs/heads/master@{#469653}

--- components/subresource_filter/core/common/url_pattern_index.cc
+++ components/subresource_filter/core/common/url_pattern_index.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/numerics/safe_conversions.h"
+#include "base/strings/string_piece.h"
 #include "base/strings/string_util.h"
 #include "components/subresource_filter/core/common/ngram_extractor.h"
 #include "components/subresource_filter/core/common/url_pattern.h"
--- components/subresource_filter/core/common/url_pattern_index.h
+++ components/subresource_filter/core/common/url_pattern_index.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/macros.h"
-#include "base/strings/string_piece.h"
+#include "base/strings/string_piece_forward.h"
 #include "components/subresource_filter/core/common/closed_hash_map.h"
 #include "components/subresource_filter/core/common/flat/url_pattern_index_generated.h"
 #include "components/subresource_filter/core/common/proto/rules.pb.h"
