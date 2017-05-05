9ae3790bd11862c4ff26a9bde95df7f5212303fb
staraz <staraz@chromium.org>
2017-05-05 16:38:12 -0700

    Replace ContextFactory::DoesCreateTestContexts with GetRefreshRate()
    ui::Compositor gets refresh rate from ContextFactory directly instead of setting
    it to different values based on the results of
    ContextFactory::DoesCreateTestContexts.
    
    Doing this avoid splitting the work of setting the refresh reate between
    ui::Compositor and the ContextFactory.
    
    BUG=717760
    
    TBR=sadrul@chromium.org
    
    Review-Url: https://codereview.chromium.org/2854403002
    Cr-Commit-Position: refs/heads/master@{#469802}

--- ui/compositor/compositor.cc
+++ ui/compositor/compositor.cc
@@ -47,13 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/icc_profile.h"
 #include "ui/gl/gl_switches.h"
 
-namespace {
-
-constexpr double kDefaultRefreshRate = 60.0;
-constexpr double kTestRefreshRate = 200.0;
-
-}  // namespace
-
 namespace ui {
 
 Compositor::Compositor(const cc::FrameSinkId& frame_sink_id,
@@ -85,8 +78,7 @@ Compositor::Compositor(const cc::FrameSinkId& frame_sink_id,
   // Use occlusion to allow more overlapping windows to take less memory.
   settings.use_occlusion_for_tile_prioritization = true;
   refresh_rate_ = settings.renderer_settings.refresh_rate =
-      context_factory_->DoesCreateTestContexts() ? kTestRefreshRate
-                                                 : kDefaultRefreshRate;
+      context_factory_->GetRefreshRate();
   settings.main_frame_before_activation_enabled = false;
   settings.renderer_settings.partial_swap_enabled =
       !command_line->HasSwitch(switches::kUIDisablePartialSwap);
--- ui/compositor/test/in_process_context_factory.cc
+++ ui/compositor/test/in_process_context_factory.cc
@@ -132,11 +132,9 @@ struct InProcessContextFactory::PerCompositorData {
 };
 
 InProcessContextFactory::InProcessContextFactory(
-    bool context_factory_for_test,
     cc::SurfaceManager* surface_manager)
     : frame_sink_id_allocator_(kDefaultClientId),
       use_test_surface_(true),
-      context_factory_for_test_(context_factory_for_test),
       surface_manager_(surface_manager) {
   DCHECK(surface_manager);
   DCHECK_NE(gl::GetGLImplementation(), gl::kGLImplementationNone)
@@ -151,6 +149,10 @@ InProcessContextFactory::~InProcessContextFactory() {
 void InProcessContextFactory::SendOnLostResources() {
   for (auto& observer : observer_list_)
     observer.OnLostResources();
+}
+
+void InProcessContextFactory::SetUseFastRefreshRateForTests() {
+  refresh_rate_ = 200.0;
 }
 
 void InProcessContextFactory::CreateCompositorFrameSink(
@@ -270,8 +272,8 @@ void InProcessContextFactory::RemoveCompositor(Compositor* compositor) {
   per_compositor_data_.erase(it);
 }
 
-bool InProcessContextFactory::DoesCreateTestContexts() {
-  return context_factory_for_test_;
+double InProcessContextFactory::GetRefreshRate() const {
+  return refresh_rate_;
 }
 
 uint32_t InProcessContextFactory::GetImageTextureTarget(
