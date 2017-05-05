d1903cfce7af973e356bb0174c0ea400ceb01dcf
michaelpg <michaelpg@chromium.org>
2017-05-05 15:54:58 -0700

    Change SettingsLanguagesPageBrowserTest to CrSettingsLanguagesPageTest
    Transforms a browser test to a lighterweight browser test in an attempt
    to avoid test flakes: don't load the settings page (in non-vulcanized
    builds), don't make round trips to the real browser settings, and split
    up the tests into four individual browser tests.
    
    SettingsLanguagesPageBrowserTest is currently a SettingsPageBrowserTest
    test fixture that loads chrome://md-settings with real browser settings
    to test language settings.
    
    This CL changes it into a CrSettingsBrowserTest, which differs by
    loading only the <settings-languages-page> file and its dependencies,
    starting with a blank page, mocking the various settings APIs, and
    testing the element in isolation.
    
    On a vulcanized build, we still load the entire chrome://md-settings
    page. Once it loads, though, we clear that page, mock the various
    settings APIs, and can still test a <settings-languages-page> in
    relative isolation.
    
    The mocha test suite is invoked in four parts, so each browser test has
    to execute fewer mocha tests and should finish in less time.
    
    R=dpapad@chromium.org
    BUG=641400
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2837703002
    Cr-Commit-Position: refs/heads/master@{#469789}

---
+++ chrome/test/data/webui/settings/test_languages_browser_proxy.js
@@ -0,0 +1,52 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+cr.define('settings', function() {
+  /**
+   * @constructor
+   * @implements {settings.LanguagesBrowserProxy}
+   * @extends {settings.TestBrowserProxy}
+   */
+  var TestLanguagesBrowserProxy = function() {
+    var methodNames = [];
+    if (cr.isChromeOS || cr.isWindows)
+      methodNames.push('getProspectiveUILanguage');
+
+    settings.TestBrowserProxy.call(this, methodNames);
+
+    /** @private {!LanguageSettingsPrivate} */
+    this.languageSettingsPrivate_ = new settings.FakeLanguageSettingsPrivate();
+
+    /** @private {!InputMethodPrivate} */
+    this.inputMethodPrivate_ = new settings.FakeInputMethodPrivate();
+  };
+
+  TestLanguagesBrowserProxy.prototype = {
+    __proto__: settings.TestBrowserProxy.prototype,
+
+    /** @override */
+    getLanguageSettingsPrivate: function() {
+      return this.languageSettingsPrivate_;
+    },
+
+    /** @override */
+    getInputMethodPrivate: function() {
+      return this.inputMethodPrivate_;
+    },
+  };
+
+  if (cr.isChromeOS || cr.isWindows) {
+    /** @override */
+    TestLanguagesBrowserProxy.prototype.getProspectiveUILanguage =
+        function() {
+      this.methodCalled('getProspectiveUILanguage');
+      return Promise.resolve('en-US');
+    };
+  }
+
+  return {
+    TestLanguagesBrowserProxy: TestLanguagesBrowserProxy,
+  };
+});
