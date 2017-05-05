b5a5569a9cd41ccd87ed877e7bab9a6e30b741d3
lushnikov <lushnikov@chromium.org>
2017-05-05 15:57:20 -0700

    DevTools: properly handle target suspension/resuming in NetworkProject
    NetworkProject adds UISourceCode in response to DebuggerModel.scriptParsed
    event and removes UISourceCode in response to RuntimeModel.executionContextDestroyed.
    
    In case of target suspension, the runtimeModel is not getting suspended and
    does not destroy execution contexts. But DebuggerModel does get suspended and
    later gets resumed, which results in duplicate scriptParsed events.
    
    This patch starts listening to globalObjectCleared event in NetworkProject
    to clean up scripts on target suspension.
    
    Drive-by: promisify targetManager.suspendAllTargets(), make methods to
    enable/disable debugger private.
    
    R=dgozman
    BUG=670180
    
    Review-Url: https://codereview.chromium.org/2832943002
    Cr-Commit-Position: refs/heads/master@{#469791}

--- third_party/WebKit/LayoutTests/http/tests/inspector/bindings/resources/sourcemap-script.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/bindings/resources/sourcemap-script.js
@@ -1,3 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function doFoo() {
+    console.log('foo');
+}
 window.foo = console.log.bind(console, 'foo');
 //# sourceMappingURL=sourcemap-script.js.map
