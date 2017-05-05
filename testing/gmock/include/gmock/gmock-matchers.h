11b07814a6693785f1486fe9861e0f36a083247f
pwnall <pwnall@chromium.org>
2017-05-04 17:44:27 -0700

    Roll googletest to 1.7.0+.
    This is a (less ambitious) re-land of http://crrev.com/2852613002 which
    was reverted due to content_browsertests crashes on ChromeOS. That CL is
    a re-land of http://crrev.com/2779193002 which was reverted due to build
    errors on Mac.
    
    Instead of rolling Google Test to 1.8.0, this CL uses the last commit
    before a breaking change was introduced. Details about the breakage are
    in https://crbug.com/630705#c27 and https://crbug.com/630705#c28. It is
    expected that rolling Google Test from there to the next usable version
    will be much less disruptive.
    
    GoogleTest (gtest) and GoogleMock (gmock) are now hosted into the same
    googletest repository. In order to cope with this, the googletest
    repository is now sourced at third_party/googletest.
    
    The file/directory layout of Google Test is not yet considered stable.
    To minimize disruption while Google Test stabilizes, Chromium code will
    be insulated from third_party/googletest.
    
    * testing/gtest/include/gtest/ and testing/gmock/include/gmock have
      been populated with headers that forward into the appropriate locations of
      third_party/googletest
    
    * testing/BUILD.gn has been populated with the targets
      //testing/gtest(:gtest_main) and //testing/gmock(:gmock_main), which
      depend on the appropriate //third_party/googletest targets.
    
    All Chromium code should keep depending on the targets and headers
    in testing/{gtest,gmock} for now.
    
    BUG=630705
    TESTED=out/Default/content_browsertests --gtest_filter=MojoTest.Init
    TESTED=ninja -C out/Default/ ced_unittests && ninja -C out/Default
    TBR=rkc, dpranke
    
    Review-Url: https://codereview.chromium.org/2856383002
    Cr-Commit-Position: refs/heads/master@{#469551}

---
+++ testing/gmock/include/gmock/gmock-matchers.h
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// The file/directory layout of Google Test is not yet considered stable. Until
+// it stabilizes, Chromium code will use forwarding headers in testing/gtest
+// and testing/gmock, instead of directly including files in
+// third_party/googletest.
+
+#include "third_party/googletest/src/googlemock/include/gmock/gmock-matchers.h"
--- testing/gtest_mac.mm
+++ testing/gtest_mac.mm
@@ -7,9 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include <gtest/gtest.h>
-#include <gtest/internal/gtest-port.h>
-#include <gtest/internal/gtest-string.h>
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/googletest/src/googletest/include/gtest/internal/gtest-port.h"
+#include "third_party/googletest/src/googletest/include/gtest/internal/gtest-string.h"
 
 #ifdef GTEST_OS_MAC
 
