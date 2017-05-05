0df18cd29dae75aeae676397da061b5b410aa2c2
guidou <guidou@chromium.org>
2017-05-04 22:34:58 -0700

    Check delegate before running Pepper media-device enumeration/monitoring requests.
    This is intended to address a crash that appears to occur because
    the delegate object that performs enumerations is not available.
    
    BUG=716377
    
    Review-Url: https://codereview.chromium.org/2855373003
    Cr-Commit-Position: refs/heads/master@{#469601}

--- content/renderer/pepper/pepper_device_enumeration_host_helper.cc
+++ content/renderer/pepper/pepper_device_enumeration_host_helper.cc
@@ -36,6 +36,9 @@ class PepperDeviceEnumerationHostHelper::ScopedEnumerationRequest
     if (!owner->document_url_.is_valid())
       return;
 
+    if (!owner->delegate_)
+      return;
+
     requested_ = true;
 
     // Note that the callback passed into
@@ -45,7 +48,6 @@ class PepperDeviceEnumerationHostHelper::ScopedEnumerationRequest
     // EnumerateDevicesCallbackBody() to ensure that we always call |callback|
     // asynchronously.
     sync_call_ = true;
-    DCHECK(owner->delegate_);
     owner->delegate_->EnumerateDevices(
         owner->device_type_, owner->document_url_,
         base::Bind(&ScopedEnumerationRequest::EnumerateDevicesCallbackBody,
@@ -92,9 +94,11 @@ class PepperDeviceEnumerationHostHelper::ScopedMonitoringRequest
     if (!owner_->document_url_.is_valid())
       return;
 
+    if (!owner->delegate_)
+      return;
+
     requested_ = true;
 
-    DCHECK(owner_->delegate_);
     // |callback| is never called synchronously by StartMonitoringDevices(),
     // so it is OK to pass it directly, even if |callback| destroys |this|.
     subscription_id_ = owner_->delegate_->StartMonitoringDevices(
