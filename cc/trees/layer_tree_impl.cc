1afbc6fd58fc5a353cb2ef1df9f033cd8bdd9d26
chaopeng <chaopeng@chromium.org>
2017-05-05 13:07:03 -0700

    Revert of Fade out overlay scrollbar after page load (patchset #1 id:1 of https://codereview.chromium.org/2858973002/ )
    Reason for revert:
    Revert for regression
    
    Details: https://bugs.chromium.org/p/chromium/issues/detail?id=718863
    
    Original issue's description:
    > Fade out overlay scrollbar after page load
    >
    > This issue is caused by Aura Overlay Scrollbar is created with
    > opacity>0. We
    > used to call SAC::DidScrollUpdate - LTI::UpdateScrollbars to fade out
    > when RegisterScrollLayer.
    >
    >
    > ```
    > #2 0x7f23d0dfe1ea cc::LayerTreeImpl::UpdateScrollbars()
    > #3 0x7f23d0dfd999 cc::LayerTreeImpl::DidUpdateScrollState()
    > #4 0x7f23d0e07231 cc::LayerTreeImpl::RegisterScrollLayer()
    > ```
    >
    > But we remove ScrollbarAnimationController::DidScrollUpdate call in
    > UpdateScrollbars at
    > https://crrev.com/697a467f819ce09da5209a3df13b8b92f33e35a4.
    > Then Overlay Scrollbar can not fade out until we have real scroll.
    >
    > In this patch, we apply set_needs_show_scrollbars(true) to the layer in
    > LayerTreeImpl::RegisterScrollLayer that will let
    > LTI::HandleScrollbarShowRequestsFromMain pickup and call
    > SAC::DidScrollUpdate.
    >
    > For cc_unittest, remove set_needs_show_scrollbars call in
    > LayerTreeHostImplTestScrollbarAnimation.* and
    > LayerTreeHostImplTest.ScrollbarVisibilityChangeCausesRedrawAndCommit.
    >
    > BUG=717222
    > CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    >
    > Review-Url: https://codereview.chromium.org/2858973002
    > Cr-Commit-Position: refs/heads/master@{#469425}
    > Committed: https://chromium.googlesource.com/chromium/src/+/b14926c905396fbeb0a77bfc6cce569c8a35fa5f
    
    TBR=weiliangc@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=717222
    
    Review-Url: https://codereview.chromium.org/2863153002
    Cr-Commit-Position: refs/heads/master@{#469763}

--- cc/trees/layer_tree_impl.cc
+++ cc/trees/layer_tree_impl.cc
@@ -1700,8 +1700,6 @@ void LayerTreeImpl::RegisterScrollLayer(LayerImpl* layer) {
       std::pair<int, int>(layer->scroll_clip_layer_id(), layer->id()));
 
   DidUpdateScrollState(layer->id());
-
-  layer->set_needs_show_scrollbars(true);
 }
 
 void LayerTreeImpl::UnregisterScrollLayer(LayerImpl* layer) {
