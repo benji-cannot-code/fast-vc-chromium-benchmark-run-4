6ba473db4eb5e134638e5c5fe44f56174b802af1
panicker <panicker@chromium.org>
2017-05-04 21:09:26 -0700

    Use swap-promise to track first* paint up until swap time.
    BUG=657826,657825
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2835763002
    Cr-Commit-Position: refs/heads/master@{#469594}

--- content/renderer/gpu/render_widget_compositor.h
+++ content/renderer/gpu/render_widget_compositor.h
@@ -54,6 +54,8 @@ class CONTENT_EXPORT RenderWidgetCompositor
     : NON_EXPORTED_BASE(public blink::WebLayerTreeView),
       NON_EXPORTED_BASE(public cc::LayerTreeHostClient),
       NON_EXPORTED_BASE(public cc::LayerTreeHostSingleThreadClient) {
+  using ReportTimeCallback = base::Callback<void(bool, double)>;
+
  public:
   // Attempt to construct and initialize a compositor instance for the widget
   // with the given settings. Returns NULL if initialization fails.
@@ -168,6 +170,7 @@ class CONTENT_EXPORT RenderWidgetCompositor
   void SetShowPaintRects(bool show) override;
   void SetShowDebugBorders(bool show) override;
   void SetShowScrollBottleneckRects(bool show) override;
+  void NotifySwapTime(ReportTimeCallback callback) override;
 
   void UpdateBrowserControlsState(blink::WebBrowserControlsState constraints,
                                   blink::WebBrowserControlsState current,
@@ -241,6 +244,8 @@ class CONTENT_EXPORT RenderWidgetCompositor
   cc::FrameSinkId frame_sink_id_;
 
   base::WeakPtrFactory<RenderWidgetCompositor> weak_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(RenderWidgetCompositor);
 };
 
 }  // namespace content
--- third_party/WebKit/Source/core/paint/PaintTiming.cpp
+++ third_party/WebKit/Source/core/paint/PaintTiming.cpp
@@ -10,10 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/LocalDOMWindow.h"
 #include "core/frame/LocalFrame.h"
 #include "core/loader/DocumentLoader.h"
+#include "core/page/ChromeClient.h"
+#include "core/page/Page.h"
 #include "core/timing/DOMWindowPerformance.h"
 #include "core/timing/Performance.h"
 #include "platform/WebFrameScheduler.h"
 #include "platform/instrumentation/tracing/TraceEvent.h"
+#include "public/platform/WebLayerTreeView.h"
 
 namespace blink {
 
@@ -101,6 +104,7 @@ void PaintTiming::SetFirstMeaningfulPaint(double stamp) {
       TraceEvent::ToTraceTimestamp(first_meaningful_paint_), "frame",
       GetFrame());
   NotifyPaintTimingChanged();
+  RegisterNotifySwapTime(PaintEvent::kFirstMeaningfulPaint);
 }
 
 void PaintTiming::NotifyPaint(bool is_first_paint,
@@ -143,6 +147,7 @@ void PaintTiming::SetFirstPaint(double stamp) {
 
   TRACE_EVENT_INSTANT1("loading,rail,devtools.timeline", "firstPaint",
                        TRACE_EVENT_SCOPE_PROCESS, "frame", GetFrame());
+  RegisterNotifySwapTime(PaintEvent::kFirstPaint);
 }
 
 void PaintTiming::SetFirstContentfulPaint(double stamp) {
@@ -153,8 +158,43 @@ void PaintTiming::SetFirstContentfulPaint(double stamp) {
   Performance* performance = GetPerformanceInstance(GetFrame());
   if (performance)
     performance->AddFirstContentfulPaintTiming(first_contentful_paint_);
+
   TRACE_EVENT_INSTANT1("loading,rail,devtools.timeline", "firstContentfulPaint",
                        TRACE_EVENT_SCOPE_PROCESS, "frame", GetFrame());
+  RegisterNotifySwapTime(PaintEvent::kFirstContentfulPaint);
+}
+
+void PaintTiming::RegisterNotifySwapTime(PaintEvent event) {
+  // ReportSwapTime on layerTreeView will queue a swap-promise, the callback is
+  // called when the swap for current render frame completes or fails to happen.
+  if (!GetFrame() || !GetFrame()->GetPage())
+    return;
+  if (WebLayerTreeView* layerTreeView =
+          GetFrame()->GetPage()->GetChromeClient().GetWebLayerTreeView(
+              GetFrame())) {
+    layerTreeView->NotifySwapTime(ConvertToBaseCallback(
+        WTF::Bind(&PaintTiming::ReportSwapTime,
+                  WrapCrossThreadWeakPersistent(this), event)));
+  }
+}
+
+void PaintTiming::ReportSwapTime(PaintEvent event,
+                                 bool did_swap,
+                                 double timestamp) {
+  if (!did_swap)
+    return;
+  switch (event) {
+    case PaintEvent::kFirstPaint:
+      first_paint_swap_ = timestamp;
+      return;
+    case PaintEvent::kFirstContentfulPaint:
+      first_contentful_paint_swap_ = timestamp;
+      return;
+    case PaintEvent::kFirstMeaningfulPaint:
+      first_meaningful_paint_swap_ = timestamp;
+      return;
+  }
+  NOTREACHED();
 }
 
 }  // namespace blink
