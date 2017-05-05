5f11b3b53edee37bf3c0aef84e2368ec406da054
dfalcantara <dfalcantara@chromium.org>
2017-05-04 23:23:39 -0700

    🔍 More SearchActivity tests
    Fixes:
    * Previously, typing in something before native finished loading
      didn't let autocomplete suggestions appear.
    
    * Set OWNERS files with noparent so we're forced to review changes
      that could affect the search engine dialogs.
    
    Tests:
    * Adds a set of tests that monitors delays loading native and acts
      on the SearchActivity.
    
    BUG=708844, 712833, 712836, 717327
    
    Review-Url: https://codereview.chromium.org/2860313002
    Cr-Commit-Position: refs/heads/master@{#469605}

--- chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchActivityLocationBarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchActivityLocationBarLayout.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.searchwidget;
 
 import android.content.Context;
 import android.os.Handler;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -69,6 +70,7 @@ public class SearchActivityLocationBarLayout extends LocationBarLayout {
     void onDeferredStartup(boolean isVoiceSearchIntent) {
         SearchWidgetProvider.updateCachedVoiceSearchAvailability(isVoiceSearchEnabled());
         if (isVoiceSearchIntent && mUrlBar.isFocused()) onUrlFocusChange(true);
+        if (!TextUtils.isEmpty(mUrlBar.getText())) onTextChangedForAutocomplete(false);
     }
 
     /** Begins a new query. */
