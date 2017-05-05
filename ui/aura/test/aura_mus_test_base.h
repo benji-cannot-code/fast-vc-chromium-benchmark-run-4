6961402b2ea902a3b61ea291923b1551057401bf
staraz <staraz@chromium.org>
2017-05-05 11:56:42 -0700

    Install FakeContextFactory for AuraMusWmTestBase
    Override AuraMusWmTestBase::SetUp() and TearDown() to use a FakeContextFactory
    for its tests and restore the original ContextFactory to avoid affecting the
    other tests.
    
    Doing this allows WindowTreeHostMus to set accelerate_widget to a non-zero value
    like it normally would. WindowTreeHostMus doesn't have to have a special case
    for tests anymore.
    
    BUG=717760
    
    Review-Url: https://codereview.chromium.org/2865443002
    Cr-Commit-Position: refs/heads/master@{#469741}

--- ui/aura/test/aura_mus_test_base.h
+++ ui/aura/test/aura_mus_test_base.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "ui/aura/test/aura_test_base.h"
+#include "ui/compositor/test/fake_context_factory.h"
 
 namespace aura {
 namespace test {
@@ -24,8 +25,12 @@ class AuraMusWmTestBase : public AuraTestBase {
 
   // AuraTestBase:
   void SetUp() override;
+  void TearDown() override;
 
  private:
+  ui::FakeContextFactory context_factory_;
+  ui::ContextFactory* context_factory_to_restore_ = nullptr;
+
   DISALLOW_COPY_AND_ASSIGN(AuraMusWmTestBase);
 };
 
@@ -42,8 +47,12 @@ class AuraMusClientTestBase : public AuraTestBase {
 
   // AuraTestBase:
   void SetUp() override;
+  void TearDown() override;
 
  private:
+  ui::FakeContextFactory context_factory_;
+  ui::ContextFactory* context_factory_to_restore_ = nullptr;
+
   DISALLOW_COPY_AND_ASSIGN(AuraMusClientTestBase);
 };
 
