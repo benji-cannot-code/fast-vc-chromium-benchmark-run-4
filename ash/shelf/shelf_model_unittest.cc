9fcffa55c196b6e7131970d66c33ce8f615affe2
khmel <khmel@chromium.org>
2017-05-06 12:20:00 -0700

    Fix crash on openning settings when DCHECK is on.
    Use randomly generated settings app id in format used for app
    and extension identification.
    
    BUG=718987
    Test=Manually
    
    Review-Url: https://codereview.chromium.org/2869473002
    Cr-Commit-Position: refs/heads/master@{#469881}

--- ash/shelf/shelf_model_unittest.cc
+++ ash/shelf/shelf_model_unittest.cc
@@ -16,6 +16,8 @@ namespace ash {
 
 namespace {
 
+constexpr char kAppListId[] = "jlfapfmkapbjlfbpjedlinehodkccjee";
+
 // ShelfModelObserver implementation that tracks what message are invoked.
 class TestShelfModelObserver : public ShelfModelObserver {
  public:
@@ -70,7 +72,7 @@ class ShelfModelTest : public testing::Test {
 
     ShelfItem item;
     item.type = TYPE_APP_LIST;
-    item.id = ShelfID("AppListId");
+    item.id = ShelfID(kAppListId);
     model_->Add(item);
     EXPECT_EQ(1, model_->item_count());
 
