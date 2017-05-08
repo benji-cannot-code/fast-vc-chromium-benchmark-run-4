2ed81858515bf1a2d39f89afe9e7bb0e2212dd7d
lpz <lpz@chromium.org>
2017-05-08 13:12:18 -0700

    Refactoring creation of ThreatDetails out of SafeBrowsingBlockingPage and into TriggerManager.
    This is the first step in sharing ThreatDetails as a DataCollector for future triggers.
    Next step will be to make TriggerManager own ThreatDetails rather than returning them.
    
    Review-Url: https://codereview.chromium.org/2858363002
    Cr-Commit-Position: refs/heads/master@{#470099}

--- components/safe_browsing/triggers/trigger_manager.cc
+++ components/safe_browsing/triggers/trigger_manager.cc
@@ -5,10 +5,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/safe_browsing/triggers/trigger_manager.h"
 
+#include "components/safe_browsing/base_ui_manager.h"
+#include "components/safe_browsing/browser/threat_details.h"
+#include "content/public/browser/web_contents.h"
+#include "net/url_request/url_request_context_getter.h"
+
 namespace safe_browsing {
 
-TriggerManager::TriggerManager() {}
+TriggerManager::TriggerManager(BaseUIManager* ui_manager)
+    : ui_manager_(ui_manager) {}
 
 TriggerManager::~TriggerManager() {}
+
+ThreatDetails* TriggerManager::StartCollectingThreatDetails(
+    content::WebContents* web_contents,
+    const security_interstitials::UnsafeResource& resource,
+    net::URLRequestContextGetter* request_context_getter,
+    history::HistoryService* history_service) {
+  return ThreatDetails::NewThreatDetails(ui_manager_, web_contents, resource,
+                                         request_context_getter,
+                                         history_service);
+}
 
 }  // namespace safe_browsing
