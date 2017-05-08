654eedbb18bb91c1265022519ae4002a69046dff
jam <jam@chromium.org>
2017-05-08 14:21:35 -0700

    Simplify how data scheme is handled by the network service.
    Review-Url: https://codereview.chromium.org/2866973006
    Cr-Commit-Position: refs/heads/master@{#470131}

--- content/network/network_context.cc
+++ content/network/network_context.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/logging.h"
-#include "base/memory/ptr_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "content/network/url_loader_impl.h"
 #include "content/public/common/content_client.h"
@@ -18,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/log/write_to_file_net_log_observer.h"
 #include "net/proxy/proxy_config.h"
 #include "net/proxy/proxy_config_service_fixed.h"
-#include "net/url_request/data_protocol_handler.h"
 #include "net/url_request/url_request_context.h"
 #include "net/url_request/url_request_context_builder.h"
 
@@ -69,9 +67,7 @@ std::unique_ptr<net::URLRequestContext> MakeURLRequestContext() {
 
   builder.EnableHttpCache(cache_params);
   builder.set_file_enabled(true);
-
-  builder.SetProtocolHandler(url::kDataScheme,
-                             base::MakeUnique<net::DataProtocolHandler>());
+  builder.set_data_enabled(true);
 
   if (command_line->HasSwitch(switches::kProxyServer)) {
     net::ProxyConfig config;
