f640602f91e10a859d82f2041d15c5342055fe89
victorhsieh <victorhsieh@chromium.org>
2017-05-08 14:08:33 -0700

    Allow autotest extension to access system.display
    system.display is currently only allowed for a few cases.  Grant
    autotest extension to use the API for test purpose, too.
    
    TEST=autotest works
    BUG=712705
    
    Review-Url: https://codereview.chromium.org/2862223003
    Cr-Commit-Position: refs/heads/master@{#470124}

--- extensions/browser/api/system_display/system_display_api.cc
+++ extensions/browser/api/system_display/system_display_api.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents_observer.h"
 #include "extensions/browser/api/system_display/display_info_provider.h"
 #include "extensions/common/api/system_display.h"
+#include "extensions/common/permissions/permissions_data.h"
 
 #if defined(OS_CHROMEOS)
 #include "extensions/common/manifest_handlers/kiosk_mode_info.h"
@@ -176,6 +177,11 @@ bool SystemDisplayFunction::PreRunValidation(std::string* error) {
 }
 
 bool SystemDisplayFunction::ShouldRestrictToKioskAndWebUI() {
+  // Allow autotest extension to access for Chrome OS testing.
+  if (extension()->permissions_data()->HasAPIPermission(
+      APIPermission::kAutoTestPrivate)) {
+    return false;
+  }
   return true;
 }
 
