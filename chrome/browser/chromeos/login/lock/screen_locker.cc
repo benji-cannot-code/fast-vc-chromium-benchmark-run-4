e9cd40cd8bdc3a3dee834d6d5e8e3ba7580404ba
xiaoyinh <xiaoyinh@chromium.org>
2017-05-08 13:21:31 -0700

    Roll src/third_party/cros_system_api/ c6eab9e4d..6139ae009 + API change
    Roll has 1 commit
    
    https://chromium.googlesource.com/chromiumos/platform/system_api.git/+log/c6eab9e4d0b4..6139ae00925a
    
    $ git log c6eab9e4d..6139ae009 --date=short --no-merges --format='%ad %ae %s'
    2017-04-19 mqg biod: add proto to biod
    
    Created with:
      roll-dep src/third_party/cros_system_api
    
    In addition to roll the cros_system_api, this CL works on the following:
    1. Because the change in cros_system_api moved
    biod::ScanResult from cros_system_api/dbus/service_constants.h
    to cros_system_api/dbus/biod/constants.proto, we need to update
    the files where we use biod::ScanResult
    
    2. The change in cros_system_api added an optional parameter
    percent_complete in EnrollScanDone signal and change the dbus message
    to a protobuff. Update to dbus/biod and fingerprint_chromeos to incorporate this change.
    
    A follow-up CL will modify fingerprint settings to use the
    percent_complete parameter.
    
    BUG=713420, 718076
    
    Review-Url: https://codereview.chromium.org/2858003002
    Cr-Commit-Position: refs/heads/master@{#470102}

--- chrome/browser/chromeos/login/lock/screen_locker.cc
+++ chrome/browser/chromeos/login/lock/screen_locker.cc
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/browser_resources.h"
 #include "chrome/grit/generated_resources.h"
 #include "chromeos/audio/chromeos_sounds.h"
+#include "chromeos/dbus/biod/constants.pb.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/session_manager_client.h"
 #include "chromeos/login/auth/authenticator.h"
