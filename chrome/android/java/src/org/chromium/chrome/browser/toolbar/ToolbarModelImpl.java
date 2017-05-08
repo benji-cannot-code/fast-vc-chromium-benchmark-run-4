e8adff38833647c81be69ead371cdb2c4964f2bc
mdjones <mdjones@chromium.org>
2017-05-08 12:53:54 -0700

    [Home] Disable theme colors in toolbar
    This change disabled theme colors in the toolbar when Chrome Home is
    enabled. The primary mechanism for doing this overriding functionality
    in the ToolbarDataProvider to always return a default theme color.
    Since theme colors are still needed for the status and progress bars,
    the tab still needs to retain knowledge of the web contents' theme.
    In those cases, the tab is queried instead of the data provider (this
    should be cleaned up once a decision is made to keep or remove
    themes).
    
    BUG=718932
    
    Review-Url: https://codereview.chromium.org/2862113003
    Cr-Commit-Position: refs/heads/master@{#470087}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarModelImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarModelImpl.java
@@ -154,8 +154,7 @@ class ToolbarModelImpl extends ToolbarModel implements ToolbarDataProvider, Tool
 
     @Override
     public int getPrimaryColor() {
-        if (mBottomSheet != null && mBottomSheet.isSheetOpen()
-                && mBottomSheet.getTargetSheetState() != BottomSheet.SHEET_STATE_PEEK) {
+        if (mBottomSheet != null) {
             int colorId =
                     isIncognito() ? R.color.incognito_primary_color : R.color.default_primary_color;
             return ApiCompatibilityUtils.getColor(
@@ -166,6 +165,6 @@ class ToolbarModelImpl extends ToolbarModel implements ToolbarDataProvider, Tool
 
     @Override
     public boolean isUsingBrandColor() {
-        return mIsUsingBrandColor;
+        return mIsUsingBrandColor && mBottomSheet == null;
     }
 }
