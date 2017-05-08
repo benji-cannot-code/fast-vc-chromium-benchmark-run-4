b594feb788bb37226eef2cdc5d3b5b7794a41de3
tbarzic <tbarzic@chromium.org>
2017-05-08 13:46:15 -0700

    Introduce ash mojo interface for lock screen action handlers
    This is part of support for note taking apps on lock screen.
    
    This is the interface that will enable ash to observe state of
    apps that can/are allowed to handle actions (currently, only
    "new note" action is supported) on lock screen and update it's
    state accordingly.
    
    For example, this interface will be used for tray icon for launching
    new note. The icon will be exposed only if the action handler
    controller is in the state indicating that there is an app registered
    for handling new note action on lock screen.
    
    Clicking the icon would issue a request to action handler to handle
    the action (thus adding RequestHandleAction interface).
    
    BUG=716081
    
    Review-Url: https://codereview.chromium.org/2848813002
    Cr-Original-Commit-Position: refs/heads/master@{#469870}
    Committed: https://chromium.googlesource.com/chromium/src/+/033f267aec04cedfa513cef1880e9dc81dc83648
    Review-Url: https://codereview.chromium.org/2848813002
    Cr-Commit-Position: refs/heads/master@{#470115}

--- ash/mojo_interface_factory.cc
+++ ash/mojo_interface_factory.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/locale/locale_notification_controller.h"
 #include "ash/system/network/vpn_list.h"
 #include "ash/system/tray/system_tray_controller.h"
+#include "ash/tray_action/tray_action.h"
 #include "ash/wallpaper/wallpaper_controller.h"
 #include "ash/wm/maximize_mode/maximize_mode_controller.h"
 #include "base/bind.h"
@@ -97,6 +98,12 @@ void BindTouchViewRequestOnMainThread(
   Shell::Get()->maximize_mode_controller()->BindRequest(std::move(request));
 }
 
+void BindTrayActionRequestOnMainThread(
+    const service_manager::BindSourceInfo& source_info,
+    mojom::TrayActionRequest request) {
+  Shell::Get()->tray_action()->BindRequest(std::move(request));
+}
+
 void BindVpnListRequestOnMainThread(
     const service_manager::BindSourceInfo& source_info,
     mojom::VpnListRequest request) {
@@ -140,6 +147,8 @@ void RegisterInterfaces(
   registry->AddInterface(base::Bind(&BindSystemTrayRequestOnMainThread),
                          main_thread_task_runner);
   registry->AddInterface(base::Bind(&BindTouchViewRequestOnMainThread),
+                         main_thread_task_runner);
+  registry->AddInterface(base::Bind(&BindTrayActionRequestOnMainThread),
                          main_thread_task_runner);
   registry->AddInterface(base::Bind(&BindVpnListRequestOnMainThread),
                          main_thread_task_runner);
