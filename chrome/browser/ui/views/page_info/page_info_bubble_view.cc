b0cdd7a219626586f05a6aba1124dd84b2d813cd
tapted <tapted@chromium.org>
2017-05-08 00:39:08 -0700

    Harmony: Don't snap the width of the chrome:// page info bubbles.
    Adds views::DialogDelegate::ShouldSnapFrameWidth() -> bool to do this.
    
    BUG=718696, 719274
    
    Review-Url: https://codereview.chromium.org/2863923002
    Cr-Commit-Position: refs/heads/master@{#469914}

--- chrome/browser/ui/views/page_info/page_info_bubble_view.cc
+++ chrome/browser/ui/views/page_info/page_info_bubble_view.cc
@@ -218,6 +218,7 @@ class InternalPageInfoBubbleView : public views::BubbleDialogDelegateView {
   // views::BubbleDialogDelegateView:
   void OnWidgetDestroying(views::Widget* widget) override;
   int GetDialogButtons() const override;
+  bool ShouldSnapFrameWidth() const override;
 
  private:
   friend class PageInfoBubbleView;
@@ -377,6 +378,10 @@ void InternalPageInfoBubbleView::OnWidgetDestroying(views::Widget* widget) {
 
 int InternalPageInfoBubbleView::GetDialogButtons() const {
   return ui::DIALOG_BUTTON_NONE;
+}
+
+bool InternalPageInfoBubbleView::ShouldSnapFrameWidth() const {
+  return false;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
