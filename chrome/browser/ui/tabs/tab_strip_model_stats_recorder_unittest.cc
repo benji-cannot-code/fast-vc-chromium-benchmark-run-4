db0efb5c571f9cbc427de056eead1240ef6ac2a1
kouhei <kouhei@chromium.org>
2017-05-08 09:42:30 -0700

    Deprecate histogram Tabs.StateTransfer.Time_Inactive_*
    BUG=689316
    
    Review-Url: https://codereview.chromium.org/2815483003
    Cr-Commit-Position: refs/heads/master@{#470013}

--- chrome/browser/ui/tabs/tab_strip_model_stats_recorder_unittest.cc
+++ chrome/browser/ui/tabs/tab_strip_model_stats_recorder_unittest.cc
@@ -65,7 +65,6 @@ TEST_F(TabStripModelStatsRecorderTest, BasicTabLifecycle) {
   tester.ExpectUniqueSample(
       "Tabs.StateTransfer.Target_Inactive",
       static_cast<int>(TabStripModelStatsRecorder::TabState::ACTIVE), 1);
-  tester.ExpectTotalCount("Tabs.StateTransfer.Time_Inactive_Active", 1);
   tester.ExpectUniqueSample(
       "Tabs.StateTransfer.NumberOfOtherTabsActivatedBeforeMadeActive", 1, 1);
 
@@ -82,7 +81,6 @@ TEST_F(TabStripModelStatsRecorderTest, BasicTabLifecycle) {
   tester.ExpectBucketCount(
       "Tabs.StateTransfer.Target_Inactive",
       static_cast<int>(TabStripModelStatsRecorder::TabState::CLOSED), 1);
-  tester.ExpectTotalCount("Tabs.StateTransfer.Time_Inactive_Closed", 1);
 
   // Close the active first tab.
   tabstrip.CloseSelectedTabs();
