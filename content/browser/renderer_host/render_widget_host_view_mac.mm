e213eebcf922210fd70b0d9dc5aa5e48173ee7c9
ccameron <ccameron@chromium.org>
2017-05-08 00:55:30 -0700

    mac: Don't set background color from a frame until frame is displayed
    The first frame for dark-themed new tab pages still has a background
    color of white, even though the frame's content is all black tiles.
    
    Before this patch, we would set the background color to white, and then
    later put up the black tiles in front, resulting in a white flash.
    
    This updates all of those parts atomically in the same CATransaction.
    
    BUG=470669
    
    Review-Url: https://codereview.chromium.org/2866983002
    Cr-Commit-Position: refs/heads/master@{#469919}

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -427,6 +427,12 @@ void RenderWidgetHostViewMac::AcceleratedWidgetGetVSyncParameters(
 }
 
 void RenderWidgetHostViewMac::AcceleratedWidgetSwapCompleted() {
+  // Set the background color for the root layer from the frame that just
+  // swapped. See RenderWidgetHostViewAura for more details. Note that this is
+  // done only after the swap has completed, so that the background is not set
+  // before the frame is up.
+  UpdateBackgroundColorFromRenderer(last_frame_root_background_color_);
+
   if (display_link_)
     display_link_->NotifyCurrentTime(base::TimeTicks::Now());
 }
@@ -1424,10 +1430,7 @@ void RenderWidgetHostViewMac::SubmitCompositorFrame(
     cc::CompositorFrame frame) {
   TRACE_EVENT0("browser", "RenderWidgetHostViewMac::OnSwapCompositorFrame");
 
-  // Override the compositor background color. See RenderWidgetHostViewAura
-  // for more details.
-  UpdateBackgroundColorFromRenderer(frame.metadata.root_background_color);
-
+  last_frame_root_background_color_ = frame.metadata.root_background_color;
   last_scroll_offset_ = frame.metadata.root_scroll_offset;
 
   page_at_minimum_scale_ =
