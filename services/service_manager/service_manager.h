0dbf8ee828d720db57633db0a8cdb71868702dce
Sam McNally <sammc@chromium.org>
2017-05-08 18:05:34 +1000

    Only send the necessary parent data from catalog to service manager.
    Only the name is used. The rest of the |ResolveResult| is unused and
    just wastes serialisation and deserialisation time.
    
    This also removes the now-unused "service_manager:instance_per_child"
    capability.
    
    Change-Id: Ie66851d9a105eedb4489415770658f63f840d5bd
    Reviewed-on: https://chromium-review.googlesource.com/497227
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#470153}

--- services/service_manager/service_manager.h
+++ services/service_manager/service_manager.h
@@ -159,7 +159,7 @@ class ServiceManager {
                          bool has_source_instance,
                          base::WeakPtr<Instance> source_instance,
                          mojom::ResolveResultPtr result,
-                         mojom::ResolveResultPtr parent);
+                         const base::Optional<std::string>& parent_name);
 
   base::WeakPtr<ServiceManager> GetWeakPtr();
 
