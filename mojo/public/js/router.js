55cf80e3b44ca392388602e0047c6a2d82eb23a4
wangjimmy <wangjimmy@chromium.org>
2017-05-08 12:00:28 -0700

    Handle error in connector. Close handle and swap with dummy handle when read.result !== core.RESULT_FAILED_PRECONDITION.
    BUG=716959
    
    Review-Url: https://codereview.chromium.org/2842073003
    Cr-Commit-Position: refs/heads/master@{#470069}

--- mojo/public/js/router.js
+++ mojo/public/js/router.js
@@ -154,8 +154,9 @@ define("mojo/public/js/router", [
           var ok = endpoint.client.handleIncomingMessage(message,
               messageValidator);
 
-          if (!ok) {
-            this.handleInvalidIncomingMessage_();
+          // Handle invalid cached incoming message.
+          if (!validator.isTestingMode() && !ok) {
+            this.connector_.handleError(true, true);
           }
         }
       }).bind(this));
@@ -236,10 +237,6 @@ define("mojo/public/js/router", [
         ok = endpoint.client.handleIncomingMessage(message, messageValidator);
       }
     }
-
-    if (!ok) {
-      this.handleInvalidIncomingMessage_();
-    }
     return ok;
   };
 
@@ -253,17 +250,6 @@ define("mojo/public/js/router", [
 
   Router.prototype.waitForNextMessageForTesting = function() {
     this.connector_.waitForNextMessageForTesting();
-  };
-
-  Router.prototype.handleInvalidIncomingMessage_ = function(message) {
-    if (!validator.isTestingMode()) {
-      // TODO(yzshen): Consider notifying the embedder.
-      // TODO(yzshen): This should also trigger connection error handler.
-      // Consider making accept() return a boolean and let the connector deal
-      // with this, as the C++ code does.
-      this.close();
-      return;
-    }
   };
 
   Router.prototype.onPeerAssociatedEndpointClosed = function(interfaceId,
