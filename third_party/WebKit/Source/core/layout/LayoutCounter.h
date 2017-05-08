dfacc47cdea68459f78cdae5b16dbe00422cc5d5
rune <rune@opera.com>
2017-05-08 14:54:23 -0700

    Counters are always generated content and cannot be selected.
    Copying selection across generated ::before/::after with counter() did
    not copy the contents, but the counter content was painted as selected
    as if it was.
    
    LayoutTextFragment was already overriding the implementation of
    CanBeSelectionLeaf() to skip generated content, but LayoutCounter
    inherits from LayoutText which returns always true. Return false for
    LayoutCounter as it is always generated content.
    
    R=mstensho@opera.com
    BUG=703807
    
    Review-Url: https://codereview.chromium.org/2867083003
    Cr-Commit-Position: refs/heads/master@{#470141}

--- third_party/WebKit/Source/core/layout/LayoutCounter.h
+++ third_party/WebKit/Source/core/layout/LayoutCounter.h
@@ -69,6 +69,7 @@ class LayoutCounter final : public LayoutText {
   void UpdateCounter();
 
   const char* GetName() const override { return "LayoutCounter"; }
+  bool CanBeSelectionLeaf() const final { return false; }
 
  protected:
   void WillBeDestroyed() override;
