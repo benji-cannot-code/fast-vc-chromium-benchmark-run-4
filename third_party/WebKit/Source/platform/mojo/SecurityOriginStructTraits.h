8584ecb43c4e9056ab95606ce7cec0031028d7f6
mek <mek@chromium.org>
2017-05-08 13:50:38 -0700

    Revert of Fix passing origins with suborigins over mojo. (patchset #4 id:60001 of https://codereview.chromium.org/2853603002/ )
    Reason for revert:
    Causes crashes because media router code is doing manual mojo origin serialization in javascript, which wasn't updated in this CL.
    
    BUG=719300
    
    Original issue's description:
    > Fix passing origins with suborigins over mojo.
    >
    > Also fixes url::Origin::operator< to take suborigin into account.
    >
    > This fixes the in development mojo localstorage backend to work correctly
    > with suborigins.
    >
    > BUG=586194
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    >
    > Review-Url: https://codereview.chromium.org/2853603002
    > Cr-Commit-Position: refs/heads/master@{#469748}
    > Committed: https://chromium.googlesource.com/chromium/src/+/eff03640eb47562e8a461dd950a42e469a77e842
    
    TBR=jochen@chromium.org,mkwst@chromium.org,tsepez@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=586194
    
    Review-Url: https://codereview.chromium.org/2869883002
    Cr-Commit-Position: refs/heads/master@{#470117}

--- third_party/WebKit/Source/platform/mojo/SecurityOriginStructTraits.h
+++ third_party/WebKit/Source/platform/mojo/SecurityOriginStructTraits.h
@@ -24,10 +24,6 @@ struct StructTraits<url::mojom::blink::Origin::DataView,
   static uint16_t port(const RefPtr<::blink::SecurityOrigin>& origin) {
     return origin->EffectivePort();
   }
-  static WTF::String suborigin(const RefPtr<::blink::SecurityOrigin>& origin) {
-    WTF::String suborigin = origin->GetSuborigin()->GetName();
-    return suborigin.IsNull() ? "" : suborigin;
-  }
   static bool unique(const RefPtr<::blink::SecurityOrigin>& origin) {
     return origin->IsUnique();
   }
@@ -38,13 +34,10 @@ struct StructTraits<url::mojom::blink::Origin::DataView,
     } else {
       WTF::String scheme;
       WTF::String host;
-      WTF::String suborigin;
-      if (!data.ReadScheme(&scheme) || !data.ReadHost(&host) ||
-          !data.ReadSuborigin(&suborigin))
+      if (!data.ReadScheme(&scheme) || !data.ReadHost(&host))
         return false;
 
-      *out =
-          ::blink::SecurityOrigin::Create(scheme, host, data.port(), suborigin);
+      *out = ::blink::SecurityOrigin::Create(scheme, host, data.port());
     }
 
     // If a unique origin was created, but the unique flag wasn't set, then
