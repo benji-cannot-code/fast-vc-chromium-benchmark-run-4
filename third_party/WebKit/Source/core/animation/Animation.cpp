44b2dd50831cc59aafd15a98b8667eeea91a3a35
adithyas <adithyas@chromium.org>
2017-05-09 11:48:04 -0700

    Post task when rejecting Animation promises inside ScriptForbiddenScope
    It is possible for the ready promise to be rejected in a ScriptForbiddenScope. Rejecting a promise with a DOMException results in the constructor for DOMException being executed (due to a ToV8 call), which triggers a RELEASE_ASSERT that detects if script is being executed in a forbidden scope. This patch posts a task to reject promises when inside a forbidden scope to prevent the crash.
    
    BUG=701631
    
    Review-Url: https://codereview.chromium.org/2785303002
    Cr-Commit-Position: refs/heads/master@{#470391}

--- third_party/WebKit/Source/core/animation/Animation.cpp
+++ third_party/WebKit/Source/core/animation/Animation.cpp
@@ -1039,9 +1039,11 @@ Animation::PlayStateUpdateScope::~PlayStateUpdateScope() {
     if (new_play_state == kIdle) {
       if (animation_->ready_promise_->GetState() ==
           AnimationPromise::kPending) {
-        animation_->ready_promise_->Reject(DOMException::Create(kAbortError));
+        animation_->RejectAndResetPromiseMaybeAsync(
+            animation_->ready_promise_.Get());
+      } else {
+        animation_->ready_promise_->Reset();
       }
-      animation_->ready_promise_->Reset();
       animation_->ResolvePromiseMaybeAsync(animation_->ready_promise_.Get());
     } else if (old_play_state == kPending) {
       animation_->ResolvePromiseMaybeAsync(animation_->ready_promise_.Get());
@@ -1056,10 +1058,11 @@ Animation::PlayStateUpdateScope::~PlayStateUpdateScope() {
     if (new_play_state == kIdle) {
       if (animation_->finished_promise_->GetState() ==
           AnimationPromise::kPending) {
-        animation_->finished_promise_->Reject(
-            DOMException::Create(kAbortError));
+        animation_->RejectAndResetPromiseMaybeAsync(
+            animation_->finished_promise_.Get());
+      } else {
+        animation_->finished_promise_->Reset();
       }
-      animation_->finished_promise_->Reset();
     } else if (new_play_state == kFinished) {
       animation_->ResolvePromiseMaybeAsync(animation_->finished_promise_.Get());
     } else if (old_play_state == kFinished) {
@@ -1148,6 +1151,22 @@ void Animation::ResolvePromiseMaybeAsync(AnimationPromise* promise) {
                              WrapPersistent(promise), WrapPersistent(this)));
   } else {
     promise->Resolve(this);
+  }
+}
+
+void Animation::RejectAndResetPromise(AnimationPromise* promise) {
+  promise->Reject(DOMException::Create(kAbortError));
+  promise->Reset();
+}
+
+void Animation::RejectAndResetPromiseMaybeAsync(AnimationPromise* promise) {
+  if (ScriptForbiddenScope::IsScriptForbidden()) {
+    TaskRunnerHelper::Get(TaskType::kDOMManipulation, GetExecutionContext())
+        ->PostTask(BLINK_FROM_HERE,
+                   WTF::Bind(&Animation::RejectAndResetPromise,
+                             WrapPersistent(this), WrapPersistent(promise)));
+  } else {
+    RejectAndResetPromise(promise);
   }
 }
 
