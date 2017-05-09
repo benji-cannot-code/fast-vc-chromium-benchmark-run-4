bad70ac45e45fb5a6e0f0fbe3235c684546e8b30
mike <mike@mikepennisi.com>
2017-05-08 23:26:28 -0700

    Upstream service worker `fetch` test to WPT
    **fetch-request-css-cross-origin-mime-check**
    
    Update resource file paths as appropriate and use HTTPS version of
    absolute URLs.
    
    **fetch-request-fallback**
    
    Both the Chromium version and upstream version of this test cover the
    following conditions:
    
    - same origin XHR should succeed
    - CORS-unsupported other origin XHR should fail
    - CORS-supported other origin XHR should succeed
    - redirected XHR should succeed
    - XHR which is redirected to CORS-unsupported other origin should fail
    - XHR which is redirected to CORS-supported other origin should succeed
    
    However, the Chromium version includes additional tests for image
    requests:
    
    - image request should succeed
    - other origin image request should succeed
    - CORS-unsupported other origin image request should fail
    - CORS-supported other origin image request should succeed
    - redirected image request should succeed
    - image request which is redirected to other origin should succeed
    - image request which is redirected to CORS-unsupported other origin should fail
    - image request which is redirected to CORS-supported other origin should succeed
    
    The two versions differ significantly in test structure, as well. Adopt
    the Chromium version's structure because that implements more granular
    assertion messages.
    
    BUG=688116
    R=mek@chromium.org
    
    Review-Url: https://codereview.chromium.org/2858933003
    Cr-Commit-Position: refs/heads/master@{#470248}

--- third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/fetch-request-css-cross-origin-mime-check-worker.js
+++ third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/fetch-request-css-cross-origin-mime-check-worker.js
@@ -1,16 +1,16 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-importScripts('../../resources/get-host-info.js');
-importScripts('test-helpers.js');
+importScripts('/common/get-host-info.sub.js');
+importScripts('test-helpers.sub.js');
 
 self.addEventListener('fetch', function(event) {
     if (event.request.url.indexOf('cross-origin-css.css') != -1) {
       event.respondWith(fetch(
-          get_host_info()['HTTP_REMOTE_ORIGIN'] + base_path() +
+          get_host_info()['HTTPS_REMOTE_ORIGIN'] + base_path() +
           'fetch-request-css-cross-origin-mime-check-cross.css',
           {mode: 'no-cors'}));
     } else if (event.request.url.indexOf('cross-origin-html.css') != -1) {
       event.respondWith(fetch(
-          get_host_info()['HTTP_REMOTE_ORIGIN'] + base_path() +
+          get_host_info()['HTTPS_REMOTE_ORIGIN'] + base_path() +
           'fetch-request-css-cross-origin-mime-check-cross.html',
           {mode: 'no-cors'}));
     } else if (event.request.url.indexOf('synthetic.css') != -1) {
