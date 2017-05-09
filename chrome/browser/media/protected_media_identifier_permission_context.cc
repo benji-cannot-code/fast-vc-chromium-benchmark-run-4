7348a7beacd7d8ca2bbe6f7530b000b778076429
xhwang <xhwang@chromium.org>
2017-05-08 20:44:59 -0700

    media: Add Permissions.Action.ProtectedMedia UMA on ChromeOS
    BUG=719111
    TEST=Maually tested
    
    Review-Url: https://codereview.chromium.org/2864113002
    Cr-Commit-Position: refs/heads/master@{#470160}

--- chrome/browser/media/protected_media_identifier_permission_context.cc
+++ chrome/browser/media/protected_media_identifier_permission_context.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_CHROMEOS)
 #include <utility>
 
+#include "base/metrics/histogram_macros.h"
 #include "chrome/browser/chromeos/attestation/platform_verification_dialog.h"
 #include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chromeos/chromeos_switches.h"
@@ -190,6 +191,12 @@ bool ProtectedMediaIdentifierPermissionContext::
 }
 
 #if defined(OS_CHROMEOS)
+
+static void ReportPermissionActionUMA(PermissionAction action) {
+  UMA_HISTOGRAM_ENUMERATION("Permissions.Action.ProtectedMedia", action,
+                            PermissionAction::NUM);
+}
+
 void ProtectedMediaIdentifierPermissionContext::
     OnPlatformVerificationConsentResponse(
         content::WebContents* web_contents,
@@ -199,9 +206,13 @@ void ProtectedMediaIdentifierPermissionContext::
         const BrowserPermissionCallback& callback,
         PlatformVerificationDialog::ConsentResponse response) {
   // The request may have been canceled. Drop the callback in that case.
+  // This can happen if the tab is closed.
   PendingRequestMap::iterator request = pending_requests_.find(web_contents);
-  if (request == pending_requests_.end())
+  if (request == pending_requests_.end()) {
+    VLOG(1) << "Platform verification ignored by user.";
+    ReportPermissionActionUMA(PermissionAction::IGNORED);
     return;
+  }
 
   DCHECK(request->second.second == id);
   pending_requests_.erase(request);
@@ -210,7 +221,10 @@ void ProtectedMediaIdentifierPermissionContext::
   bool persist = false; // Whether the ContentSetting should be saved.
   switch (response) {
     case PlatformVerificationDialog::CONSENT_RESPONSE_NONE:
+      // This can happen if user clicked "x", or pressed "Esc", or navigated
+      // away without closing the tab.
       VLOG(1) << "Platform verification dismissed by user.";
+      ReportPermissionActionUMA(PermissionAction::DISMISSED);
       content_setting = CONTENT_SETTING_ASK;
       persist = false;
       break;
@@ -218,6 +232,7 @@ void ProtectedMediaIdentifierPermissionContext::
       VLOG(1) << "Platform verification accepted by user.";
       base::RecordAction(
           base::UserMetricsAction("PlatformVerificationAccepted"));
+      ReportPermissionActionUMA(PermissionAction::GRANTED);
       content_setting = CONTENT_SETTING_ALLOW;
       persist = true;
       break;
@@ -225,6 +240,7 @@ void ProtectedMediaIdentifierPermissionContext::
       VLOG(1) << "Platform verification denied by user.";
       base::RecordAction(
           base::UserMetricsAction("PlatformVerificationRejected"));
+      ReportPermissionActionUMA(PermissionAction::DENIED);
       content_setting = CONTENT_SETTING_BLOCK;
       persist = true;
       break;
