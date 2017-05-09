22237c8692b92cddfba0f06d125d6d9f671d5189
macourteau <macourteau@chromium.org>
2017-05-09 12:15:48 -0700

    [Payment Request] Refactors PaymentRequestCoordinator.
    Refactoring asked for in CL https://codereview.chromium.org/2844783002/.
    
    Moves some logic up into PaymentRequestManager, and other logic into
    PaymentRequestMediator.
    
    There's more to refactor, but this is a start.
    
    BUG=602666
    
    Review-Url: https://codereview.chromium.org/2866793002
    Cr-Commit-Position: refs/heads/master@{#470403}

---
+++ ios/chrome/browser/ui/payments/payment_request_mediator.mm
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/ui/payments/payment_request_mediator.h"
+
+#include "base/strings/sys_string_conversions.h"
+#include "components/signin/core/browser/signin_manager.h"
+#include "ios/chrome/browser/signin/signin_manager_factory.h"
+
+@implementation PaymentRequestMediator {
+  ios::ChromeBrowserState* _browserState;
+}
+
+- (instancetype)initWithBrowserState:(ios::ChromeBrowserState*)browserState {
+  DCHECK(browserState);
+  self = [super init];
+  if (self) {
+    _browserState = browserState;
+  }
+  return self;
+}
+
+- (NSString*)authenticatedAccountName {
+  const SigninManager* signinManager =
+      ios::SigninManagerFactory::GetForBrowserStateIfExists(_browserState);
+  if (signinManager && signinManager->IsAuthenticated()) {
+    return base::SysUTF8ToNSString(
+        signinManager->GetAuthenticatedAccountInfo().email);
+  }
+  return nil;
+}
+
+@end
