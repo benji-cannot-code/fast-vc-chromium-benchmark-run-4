d2722e4ce228bb3e904de7e905a97016d0e31b18
jbudorick <jbudorick@chromium.org>
2017-05-08 20:53:43 -0700

    [Android] Tweak ChromeTabUtils.waitForTabPageLoaded loading state logic.
    ChromeTabUtils.waitForTabPageLoaded was previously using Tab.isLoading()
    to determine whether a tab had previously finished loading, then using
    TabObserver.onPageLoadFinished to determine when a tab that hadn't
    previously finished loading did so. Unfortunately, Tab's loading state
    as tracked by Tab.mIsLoading is modified by the Tab.onLoadStopped
    (which, in turn, calls TabObserver.onLoadStopped), not by
    Tab.didPageLoadFinish (which, in turn, calls
    TabObserver.onPageLoadFinished). The disconnect here means that it's
    possible for ChromeTabUtils.waitForTabPageLoaded to see a Tab that is
    still loading according to mIsLoading but has already fired
    onPageLoadFinished. In such a scenario, waitForTabPageLoaded will time
    out without ever seeing a call to onPageLoadFinished even though the
    page has loaded.
    
    This CL changes the observer used by ChromeTabUtils. Instead of simply
    waiting for onPageLoadFinished, it now will:
     - recognize that the Tab has successfully loaded upon receiving
       onPageLoadFinished (same as now)
     - recognize that the Tab has failed to load upon receiving onCrash
     - note whether onLoadStopped has been called. If a timeout is hit and
       onLoadStopped has been called, the Tab is on the correct page, and
       the Tab hasn't crashed, waitForTabPageLoaded will assume that the
       Tab has successfully loaded.
    
    BUG=709004
    
    Review-Url: https://codereview.chromium.org/2859053004
    Cr-Commit-Position: refs/heads/master@{#470172}

--- base/test/android/javatests/src/org/chromium/base/test/util/CallbackHelper.java
+++ base/test/android/javatests/src/org/chromium/base/test/util/CallbackHelper.java
@@ -7,6 +7,8 @@ package org.chromium.base.test.util;
 
 import static org.chromium.base.test.util.ScalableTimeout.scaleTimeout;
 
+import org.junit.Assert;
+
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
@@ -129,6 +131,7 @@ public class CallbackHelper {
 
     private final Object mLock = new Object();
     private int mCallCount;
+    private String mFailureString;
 
     /**
      * Gets the number of times the callback has been called.
@@ -180,6 +183,11 @@ public class CallbackHelper {
             while (callCountWhenDoneWaiting > mCallCount) {
                 int callCountBeforeWait = mCallCount;
                 mLock.wait(unit.toMillis(timeout));
+                if (mFailureString != null) {
+                    String s = mFailureString;
+                    mFailureString = null;
+                    Assert.fail(s);
+                }
                 if (callCountBeforeWait == mCallCount) {
                     throw new TimeoutException(msg == null ? "waitForCallback timed out!" : msg);
                 }
@@ -226,6 +234,19 @@ public class CallbackHelper {
     public void notifyCalled() {
         synchronized (mLock) {
             mCallCount++;
+            mLock.notifyAll();
+        }
+    }
+
+    /**
+     * Should be called when the callback associated with this helper object wants to
+     * indicate a failure.
+     *
+     * @param s The failure message.
+     */
+    public void notifyFailed(String s) {
+        synchronized (mLock) {
+            mFailureString = s;
             mLock.notifyAll();
         }
     }
