fda0863eec8d5139d7e401902a2d0c85b6acdddf
staraz <staraz@chromium.org>
2017-05-08 20:46:42 -0700

    Install HostSharedBitmapManager to cc::Display created by MusDisplayProvider
    MusDisplayProvider passes the current HostSharedBitmapManager to cc::Display
    when CreateDisplay is called.
    
    MusDisplayProvider should use HostSharedBitmapManager for completeness now that
    HostSharedBitmapManager is componentized.
    
    BUG=712198
    
    Review-Url: https://codereview.chromium.org/2867173002
    Cr-Commit-Position: refs/heads/master@{#470162}

--- services/ui/surfaces/mus_display_provider.cc
+++ services/ui/surfaces/mus_display_provider.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/scheduler/begin_frame_source.h"
 #include "cc/surfaces/display.h"
 #include "cc/surfaces/display_scheduler.h"
+#include "components/display_compositor/host_shared_bitmap_manager.h"
 #include "gpu/command_buffer/client/shared_memory_limits.h"
 #include "gpu/command_buffer/service/image_factory.h"
 #include "services/ui/surfaces/display_output_surface.h"
@@ -86,9 +87,10 @@ std::unique_ptr<cc::Display> MusDisplayProvider::CreateDisplay(
   *begin_frame_source = std::move(synthetic_begin_frame_source);
 
   return base::MakeUnique<cc::Display>(
-      nullptr /* bitmap_manager */, gpu_memory_buffer_manager_.get(), settings,
-      frame_sink_id, begin_frame_source->get(),
-      std::move(display_output_surface), std::move(scheduler),
+      display_compositor::HostSharedBitmapManager::current(),
+      gpu_memory_buffer_manager_.get(), settings, frame_sink_id,
+      begin_frame_source->get(), std::move(display_output_surface),
+      std::move(scheduler),
       base::MakeUnique<cc::TextureMailboxDeleter>(task_runner_.get()));
 }
 
