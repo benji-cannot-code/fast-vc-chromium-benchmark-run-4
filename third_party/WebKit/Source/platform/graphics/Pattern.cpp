f3e46a2e93c8c600fb52b21ce136d6c8bd3cf16e
enne <enne@chromium.org>
2017-05-09 21:51:18 +0000

    Revert "Remove Pattern memory tracking"
    This reverts commit d07ceed2a2ece0c410292c1b39e5151002d23326.
    
    Reason for revert: concerns about OOMs
    
    Original change's description:
    > Remove Pattern memory tracking
    > 
    > schenney asked (in 2013) whether we needed this anymore.  The original
    > bug that added this no longer repros.
    > 
    > Tentatively reverting this awkward v8 memory tracking code.  If another
    > bug appears because of it, there are some other approaches (like
    > not generating bitmaps at record time) that are more complicated that
    > we can try instead.
    > 
    > Bug: 318968,114489
    > Change-Id: I506e44921c78be769aeed807df35a579e394d991
    > Reviewed-on: https://chromium-review.googlesource.com/499154
    > Reviewed-by: Florin Malita <fmalita@chromium.org>
    > Commit-Queue: enne <enne@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#470419}
    
    TBR=fmalita@chromium.org,adamk@chromium.org,enne@chromium.org,haraken@chromium.org,dglazkov@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    Bug: 318968,114489
    
    Change-Id: Ic21ef1cd454454ac6f2826f67697225168a56040
    Reviewed-on: https://chromium-review.googlesource.com/501247
    Reviewed-by: enne <enne@chromium.org>
    Commit-Queue: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#470421}

--- third_party/WebKit/Source/platform/graphics/Pattern.cpp
+++ third_party/WebKit/Source/platform/graphics/Pattern.cpp
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "platform/graphics/Pattern.h"
 
+#include <v8.h>
+
 #include "platform/graphics/ImagePattern.h"
 #include "platform/graphics/PaintRecordPattern.h"
 #include "platform/graphics/paint/PaintFlags.h"
@@ -48,9 +50,13 @@ PassRefPtr<Pattern> Pattern::CreatePaintRecordPattern(sk_sp<PaintRecord> record,
   return PaintRecordPattern::Create(std::move(record), repeat_mode);
 }
 
-Pattern::Pattern(RepeatMode repeat_mode) : repeat_mode_(repeat_mode) {}
+Pattern::Pattern(RepeatMode repeat_mode, int64_t external_memory_allocated)
+    : repeat_mode_(repeat_mode), external_memory_allocated_(0) {
+  AdjustExternalMemoryAllocated(external_memory_allocated);
+}
 
 Pattern::~Pattern() {
+  AdjustExternalMemoryAllocated(-external_memory_allocated_);
 }
 
 void Pattern::ApplyToFlags(PaintFlags& flags, const SkMatrix& local_matrix) {
@@ -62,6 +68,14 @@ void Pattern::ApplyToFlags(PaintFlags& flags, const SkMatrix& local_matrix) {
 
 bool Pattern::IsLocalMatrixChanged(const SkMatrix& local_matrix) const {
   return local_matrix != cached_shader_->getLocalMatrix();
+}
+
+void Pattern::AdjustExternalMemoryAllocated(int64_t delta) {
+  delta = std::max(-external_memory_allocated_, delta);
+
+  v8::Isolate::GetCurrent()->AdjustAmountOfExternalAllocatedMemory(delta);
+
+  external_memory_allocated_ += delta;
 }
 
 }  // namespace blink
