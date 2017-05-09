5ee7aae6e1e050467f4861c9e544918ed1e571ca
mahmadi <mahmadi@chromium.org>
2017-05-09 07:37:03 -0700

    [Payment Request] Fixes the payments selector view controller bugs
    1. Fixes the view controller by styling *all* the expected text labels
    for *every* expected cell type. Otherwise, when cells get reused fonts
    and colors may not have the correct value for the current indexPath.
    
    2. Fixes the PaymentsTextItem by correctly adjusting the spacing in
    absence or presence of an image.
    
    BUG=6026666
    
    Review-Url: https://codereview.chromium.org/2869883003
    Cr-Commit-Position: refs/heads/master@{#470327}

--- ios/chrome/browser/ui/payments/payment_request_selector_view_controller.mm
+++ ios/chrome/browser/ui/payments/payment_request_selector_view_controller.mm
@@ -149,6 +149,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
       if ([cell isKindOfClass:[PaymentsTextCell class]]) {
         PaymentsTextCell* textCell =
             base::mac::ObjCCastStrict<PaymentsTextCell>(cell);
+        textCell.textLabel.font = [MDCTypography body2Font];
         textCell.textLabel.textColor =
             self.dataSource.state == PaymentRequestSelectorStateError
                 ? [[MDCPalette cr_redPalette] tint600]
@@ -160,10 +161,21 @@ typedef NS_ENUM(NSInteger, ItemType) {
       if ([cell isKindOfClass:[PaymentsTextCell class]]) {
         PaymentsTextCell* textCell =
             base::mac::ObjCCastStrict<PaymentsTextCell>(cell);
+        textCell.textLabel.font = [MDCTypography body2Font];
+        textCell.textLabel.textColor = [[MDCPalette greyPalette] tint900];
+        textCell.detailTextLabel.font = [MDCTypography body1Font];
         textCell.detailTextLabel.textColor = [[MDCPalette greyPalette] tint500];
       }
       break;
     }
+    case ItemTypeAddItem:
+      if ([cell isKindOfClass:[PaymentsTextCell class]]) {
+        PaymentsTextCell* textCell =
+            base::mac::ObjCCastStrict<PaymentsTextCell>(cell);
+        textCell.textLabel.font = [MDCTypography body2Font];
+        textCell.textLabel.textColor = [[MDCPalette greyPalette] tint900];
+      }
+      break;
     default:
       break;
   }
