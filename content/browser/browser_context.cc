4652cbc30773c2d4b763f1fd4175ca088ce0ed73
mek <mek@chromium.org>
2017-05-08 20:51:20 -0700

    Fix possible deadlock during shutdown by not using the FILE thread.
    The leveldb service needs to be able to call code in the file service,
    even during browser shutdown. If the file service runs on the file
    thread, this will fail, leading to deadlock/timeout. Instead move
    the file service to a new SingleThreadTaskRunner managed by the task
    scheduler.
    
    Also re-enable tests that hopefully should be fixed by this.
    
    BUG=712872
    BUG=586194
    
    Review-Url: https://codereview.chromium.org/2852333004
    Cr-Commit-Position: refs/heads/master@{#470169}

--- content/browser/browser_context.cc
+++ content/browser/browser_context.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/rand_util.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
 #include "content/browser/blob_storage/chrome_blob_storage_context.h"
@@ -482,10 +483,13 @@ void BrowserContext::Initialize(
     if (base::CommandLine::ForCurrentProcess()->HasSwitch(
             switches::kMojoLocalStorage)) {
       ServiceInfo info;
-      info.factory =
-          base::Bind(&file::CreateFileService,
-                     BrowserThread::GetTaskRunnerForThread(BrowserThread::FILE),
-                     BrowserThread::GetTaskRunnerForThread(BrowserThread::DB));
+      // TODO(mek): Use sequenced task runner rather than single thread task
+      // runner when mojo supports that (http://crbug.com/678155).
+      info.factory = base::Bind(
+          &file::CreateFileService,
+          base::CreateSingleThreadTaskRunnerWithTraits(
+              {base::MayBlock(), base::TaskShutdownBehavior::BLOCK_SHUTDOWN}),
+          BrowserThread::GetTaskRunnerForThread(BrowserThread::DB));
       connection->AddEmbeddedService(file::mojom::kServiceName, info);
     }
 
