36a39dde2cfe3df3402a8108f25d5855af10d172
skym <skym@chromium.org>
2017-05-09 08:00:49 -0700

    [Sync] Switch ModelTypeStore* from blocking pool to post_task.h
    BUG=718974
    
    Review-Url: https://codereview.chromium.org/2863173002
    Cr-Commit-Position: refs/heads/master@{#470334}

--- components/sync/model/model_type_store.h
+++ components/sync/model/model_type_store.h
@@ -18,10 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/model/metadata_change_list.h"
 #include "components/sync/model/model_error.h"
 
-namespace base {
-class SequencedTaskRunner;
-}  // namespace base
-
 namespace syncer {
 
 // ModelTypeStore is leveldb backed store for model type's data, metadata and
@@ -125,24 +121,14 @@ class ModelTypeStore {
       base::Callback<void(base::Optional<ModelError> error,
                           std::unique_ptr<MetadataBatch> metadata_batch)>;
 
-  // CreateStore takes |path| and |blocking_task_runner|. Here is how to get
-  // task runner in production code:
-  //
-  // base::SequencedWorkerPool* worker_pool =
-  //     content::BrowserThread::GetBlockingPool();
-  // scoped_refptr<base::SequencedTaskRunner> blocking_task_runner(
-  //     worker_pool->GetSequencedTaskRunnerWithShutdownBehavior(
-  //         worker_pool->GetNamedSequenceToken(path),
-  //         base::SequencedWorkerPool::SKIP_ON_SHUTDOWN));
-  //
-  // In test get task runner from MessageLoop::task_runner().
+  // CreateStore takes |path|, and will run blocking calls on a task runner
+  // scoped to the given path. Tests likely don't want to use this method.
   static void CreateStore(
       ModelType type,
       const std::string& path,
-      scoped_refptr<base::SequencedTaskRunner> blocking_task_runner,
       const InitCallback& callback);
-  // Creates store object backed by in-memory leveldb database. It is used in
-  // tests.
+  // Creates store object backed by in-memory leveldb database, gets its task
+  // runner from MessageLoop::task_runner(), and should only be used in tests.
   static void CreateInMemoryStoreForTest(ModelType type,
                                          const InitCallback& callback);
 
