34e2ad7f68f8e98391860e672160ade1d31a62ed
slangley <slangley@chromium.org>
2017-05-08 23:48:20 -0700

    Remove unused include WebLocalFrameImpl from WebDOMMessageEvent.
    Note we can't move this file from web/ because it depends on
    WebFrame::ToCoreFrame that is implemented in web/.
    
    BUG=708879
    
    Review-Url: https://codereview.chromium.org/2872863002
    Cr-Commit-Position: refs/heads/master@{#470254}

--- third_party/WebKit/Source/web/WebDOMMessageEvent.cpp
+++ third_party/WebKit/Source/web/WebDOMMessageEvent.cpp
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/web/WebDocument.h"
 #include "public/web/WebFrame.h"
 #include "public/web/WebSerializedScriptValue.h"
-#include "web/WebLocalFrameImpl.h"
 
 namespace blink {
 
