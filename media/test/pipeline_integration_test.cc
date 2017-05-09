2e722cbd1054436f2a7b2ac26ceadcad69a49a47
xhwang <xhwang@chromium.org>
2017-05-08 21:15:23 -0700

    media: Make CdmMessageType an enum class
    See the discussion on enum class at
    https://groups.google.com/a/chromium.org/forum/#!topic/chromium-dev/Q5WmkAImanc
    
    After I make ContentDecryptionModule::MessageType an enum class,
    it becomes too long to refer to an enum value, for example:
    ContentDecryptionModule::MessageType::LICENSE_REQUEST.
    
    Hence move it out of ContentDecryptionModule and rename it to
    CdmMessageType.
    
    TBR=bbudge@chromium.org,dcheng@chromium.org,alokp@chromium.org
    BUG=309237
    TEST=No functionality change.
    
    Review-Url: https://codereview.chromium.org/2864673002
    Cr-Commit-Position: refs/heads/master@{#470191}

--- media/test/pipeline_integration_test.cc
+++ media/test/pipeline_integration_test.cc
@@ -229,14 +229,13 @@ class KeyProvidingApp : public FakeEncryptedMedia::AppBase {
   }
 
   void OnSessionMessage(const std::string& session_id,
-                        ContentDecryptionModule::MessageType message_type,
+                        CdmMessageType message_type,
                         const std::vector<uint8_t>& message,
                         AesDecryptor* decryptor) override {
     EXPECT_FALSE(session_id.empty());
     EXPECT_FALSE(message.empty());
     EXPECT_EQ(current_session_id_, session_id);
-    EXPECT_EQ(ContentDecryptionModule::MessageType::LICENSE_REQUEST,
-              message_type);
+    EXPECT_EQ(CdmMessageType::LICENSE_REQUEST, message_type);
 
     // Extract the key ID from |message|. For Clear Key this is a JSON object
     // containing a set of "kids". There should only be 1 key ID in |message|.
@@ -339,7 +338,7 @@ class RotatingKeyProvidingApp : public KeyProvidingApp {
 class NoResponseApp : public FakeEncryptedMedia::AppBase {
  public:
   void OnSessionMessage(const std::string& session_id,
-                        ContentDecryptionModule::MessageType message_type,
+                        CdmMessageType message_type,
                         const std::vector<uint8_t>& message,
                         AesDecryptor* decryptor) override {
     EXPECT_FALSE(session_id.empty());
