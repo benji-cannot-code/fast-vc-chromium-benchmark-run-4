ced013765a203655008afc82b058d5d5d420e430
dominickn <dominickn@chromium.org>
2017-05-10 16:32:54 -0700

    Remove the initial delay when first entering immersive fullscreen in a PWA.
    On first launch, PWAs open a splash screen. If the PWA is set to open in
    immersive fullscreen via the manifest property display: fullscreen, the
    splash screen currently opens in standalone non-immersive mode before
    the PWA transitions to immersive mode. At the point of transition, the
    splash screen icon "jumps" as the view resizes.
    
    This CL addresses this problem by transitioning immediately to immersive
    fullscreen before loading the splash screen. Now the splash screen opens
    directly into immersive fullscreen, removing any resizing.
    
    BUG=720174
    
    Review-Url: https://codereview.chromium.org/2869283002
    Cr-Commit-Position: refs/heads/master@{#470746}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappActivity.java
@@ -133,9 +133,6 @@ public class WebappActivity extends FullScreenActivity {
         getActivityTab().addObserver(createTabObserver());
         getActivityTab().getTabWebContentsDelegateAndroid().setDisplayMode(
                 mWebappInfo.displayMode());
-        if (mWebappInfo.displayMode() == WebDisplayMode.FULLSCREEN) {
-            enterImmersiveMode();
-        }
     }
 
     @Override
@@ -266,7 +263,7 @@ public class WebappActivity extends FullScreenActivity {
             });
         }
 
-        asyncSetImmersive(ENTER_IMMERSIVE_MODE_DELAY_MILLIS);
+        asyncSetImmersive(0);
     }
 
     /**
@@ -357,6 +354,10 @@ public class WebappActivity extends FullScreenActivity {
     }
 
     private void initializeWebappData() {
+        if (mWebappInfo.displayMode() == WebDisplayMode.FULLSCREEN) {
+            enterImmersiveMode();
+        }
+
         final int backgroundColor = ColorUtils.getOpaqueColor(mWebappInfo.backgroundColor(
                 ApiCompatibilityUtils.getColor(getResources(), R.color.webapp_default_bg)));
 
