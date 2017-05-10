baaa29b094a887fda51a7f0b10aca35aa8a543f6
estark <estark@chromium.org>
2017-05-10 16:43:20 -0700

    Add missing fields to ResourceResponse::DeepCopy
    ResourceResponse::DeepCopy wasn't copying all fields, which, in the case of
    security details, was causing PlzNavigate navigation requests to be missing
    security info when committed.
    
    A test will be added in a follow-up CL.
    
    https: //horo-t.github.io. Open DevTools and go to the Security tab. Reload the
    Review-Url: https://codereview.chromium.org/2871303002
    Cr-Commit-Position: refs/heads/master@{#470752}

--- content/public/common/resource_response_info.h
+++ content/public/common/resource_response_info.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-// Note: when modifying this structure, also update ResourceResponse::DeepCopy
+// NOTE: when modifying this structure, also update ResourceResponse::DeepCopy
 // in resource_response.cc.
 struct CONTENT_EXPORT ResourceResponseInfo {
   ResourceResponseInfo();
@@ -183,6 +183,9 @@ struct CONTENT_EXPORT ResourceResponseInfo {
   // True if service worker navigation preload was performed due to the request
   // for this response.
   bool did_service_worker_navigation_preload;
+
+  // NOTE: When adding or changing fields here, also update
+  // ResourceResponse::DeepCopy in resource_response.cc.
 };
 
 }  // namespace content
