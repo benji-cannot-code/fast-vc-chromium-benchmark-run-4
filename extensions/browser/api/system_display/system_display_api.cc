51e0746d49700c3e36047aea04a187d58240dbeb
afakhry <afakhry@chromium.org>
2017-05-09 21:50:07 -0700

    Revert of Allow autotest extension to access system.display (patchset #1 id:1 of https://codereview.chromium.org/2862223003/ )
    Reason for revert:
    This causes a Chrome crash whenever you attempt to change anything in display settings. It assumes this is used from an extension.
    
    Original issue's description:
    > Allow autotest extension to access system.display
    >
    > system.display is currently only allowed for a few cases.  Grant
    > autotest extension to use the API for test purpose, too.
    >
    > TEST=autotest works
    > BUG=712705
    >
    > Review-Url: https://codereview.chromium.org/2862223003
    > Cr-Commit-Position: refs/heads/master@{#470124}
    > Committed: https://chromium.googlesource.com/chromium/src/+/84b76aa34b7ee89ea7b44af59239f6729aa53ab5
    
    TBR=stevenjb@chromium.org,victorhsieh@google.com,victorhsieh@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=712705,719804
    
    Review-Url: https://codereview.chromium.org/2874633002
    Cr-Commit-Position: refs/heads/master@{#470461}

--- extensions/browser/api/system_display/system_display_api.cc
+++ extensions/browser/api/system_display/system_display_api.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents_observer.h"
 #include "extensions/browser/api/system_display/display_info_provider.h"
 #include "extensions/common/api/system_display.h"
-#include "extensions/common/permissions/permissions_data.h"
 
 #if defined(OS_CHROMEOS)
 #include "extensions/common/manifest_handlers/kiosk_mode_info.h"
@@ -177,11 +176,6 @@ bool SystemDisplayFunction::PreRunValidation(std::string* error) {
 }
 
 bool SystemDisplayFunction::ShouldRestrictToKioskAndWebUI() {
-  // Allow autotest extension to access for Chrome OS testing.
-  if (extension()->permissions_data()->HasAPIPermission(
-      APIPermission::kAutoTestPrivate)) {
-    return false;
-  }
   return true;
 }
 
