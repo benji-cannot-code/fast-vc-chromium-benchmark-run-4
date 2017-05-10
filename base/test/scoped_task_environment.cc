cab7389befd7a7eb32b90f5778ea0a84549524c2
fdoray <fdoray@chromium.org>
2017-05-10 05:24:33 -0700

    Add base::test::ScopedTaskEnvironment::RunUntilIdle().
    This method runs tasks on the main thread until no main thread
    or TaskScheduler undelayed tasks remain.
    
    R=gab@chromium.org,robliao@chromium.org
    BUG=708584
    
    Review-Url: https://codereview.chromium.org/2868093002
    Cr-Commit-Position: refs/heads/master@{#470537}

--- base/test/scoped_task_environment.cc
+++ base/test/scoped_task_environment.cc
@@ -13,6 +13,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 namespace test {
 
+namespace {
+
+class TaskObserver : public MessageLoop::TaskObserver {
+ public:
+  TaskObserver() = default;
+
+  // MessageLoop::TaskObserver:
+  void WillProcessTask(const PendingTask& pending_task) override {}
+  void DidProcessTask(const PendingTask& pending_task) override {
+    ran_task_ = true;
+  }
+
+  bool ran_task() const { return ran_task_; }
+
+ private:
+  bool ran_task_ = false;
+  DISALLOW_COPY_AND_ASSIGN(TaskObserver);
+};
+
+}  // namespace
+
 ScopedTaskEnvironment::ScopedTaskEnvironment(MainThreadType main_thread_type)
     : message_loop_(main_thread_type == MainThreadType::DEFAULT
                         ? MessageLoop::TYPE_DEFAULT
@@ -44,6 +65,20 @@ ScopedTaskEnvironment::~ScopedTaskEnvironment() {
   TaskScheduler::GetInstance()->Shutdown();
   TaskScheduler::GetInstance()->JoinForTesting();
   TaskScheduler::SetInstance(nullptr);
+}
+
+void ScopedTaskEnvironment::RunUntilIdle() {
+  for (;;) {
+    TaskScheduler::GetInstance()->FlushForTesting();
+
+    TaskObserver task_observer;
+    MessageLoop::current()->AddTaskObserver(&task_observer);
+    RunLoop().RunUntilIdle();
+    MessageLoop::current()->RemoveTaskObserver(&task_observer);
+
+    if (!task_observer.ran_task())
+      return;
+  }
 }
 
 }  // namespace test
