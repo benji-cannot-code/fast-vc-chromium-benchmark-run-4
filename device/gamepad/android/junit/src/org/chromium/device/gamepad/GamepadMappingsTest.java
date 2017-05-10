61ea184c659f7c6ad0c4d2b8dece87f2232110b0
mcasas <mcasas@chromium.org>
2017-05-09 21:47:49 -0700

    Device: enable device_junit_tests in bots
    Added device_junit_tests to appropriate locations to have them
    in targets "Android Tests" and "Android Tests (dbg)".
    
    One test is disabled in PS2 (see https://crbug.com/719765).
    
    BUG=719121
    
    Review-Url: https://codereview.chromium.org/2868693002
    Cr-Commit-Position: refs/heads/master@{#470459}

--- device/gamepad/android/junit/src/org/chromium/device/gamepad/GamepadMappingsTest.java
+++ device/gamepad/android/junit/src/org/chromium/device/gamepad/GamepadMappingsTest.java
@@ -10,9 +10,9 @@ import android.view.MotionEvent;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import org.robolectric.annotation.Config;
 
 import org.chromium.base.test.util.Feature;
@@ -258,6 +258,7 @@ public class GamepadMappingsTest {
     }
 
     @Test
+    @Ignore("https://crbug.com/719765")
     @Feature({"Gamepad"})
     public void testPS4GamepadMappings() throws Exception {
         GamepadMappings mappings =
