96764ee198cf518fc79571d30bf6487fd0cdad5f
timav <timav@chromium.org>
2017-05-10 11:08:03 -0700

    Rename ContextSelectionClient into SmartSelectionClient
    Since Smart Text selection has become available in Android O
    Developer Preview we can rename the corresponding classes
    to better reflect their meaning.
    
    BUG=685420
    
    Review-Url: https://codereview.chromium.org/2867093004
    Cr-Commit-Position: refs/heads/master@{#470643}

--- content/browser/android/browser_jni_registrar.cc
+++ content/browser/android/browser_jni_registrar.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/android/content_view_core_impl.h"
 #include "content/browser/android/content_view_render_view.h"
 #include "content/browser/android/content_view_statics.h"
-#include "content/browser/android/context_selection_client.h"
 #include "content/browser/android/date_time_chooser_android.h"
 #include "content/browser/android/dialog_overlay_impl.h"
 #include "content/browser/android/gpu_process_callback.h"
@@ -26,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/android/interstitial_page_delegate_android.h"
 #include "content/browser/android/load_url_params.h"
 #include "content/browser/android/selection_popup_controller.h"
+#include "content/browser/android/smart_selection_client.h"
 #include "content/browser/android/tracing_controller_android.h"
 #include "content/browser/android/web_contents_observer_proxy.h"
 #include "content/browser/child_process_launcher_helper_android.h"
@@ -45,7 +45,6 @@ base::android::RegistrationMethod kContentRegisteredMethods[] = {
     {"ChildProcessLauncher", content::RegisterChildProcessLauncher},
     {"ContentFeatureList", content::android::RegisterContentFeatureListJni},
     {"ContentVideoView", content::ContentVideoView::RegisterContentVideoView},
-    {"ContextSelectionClient", content::RegisterContextSelectionClient},
     {"GpuProcessCallback", content::RegisterGpuProcessCallback},
     {"MemoryMonitorAndroid", content::MemoryMonitorAndroid::Register},
     {"BackgroundSyncNetworkObserverAndroid",
@@ -70,6 +69,7 @@ base::android::RegistrationMethod kContentRegisteredMethods[] = {
     {"RegisterSelectionPopupController",
      content::RegisterSelectionPopupController},
     {"RenderFrameHostAndroid", content::RenderFrameHostAndroid::Register},
+    {"SmartSelectionClient", content::RegisterSmartSelectionClient},
     {"SpeechRecognizerImplAndroid",
      content::SpeechRecognizerImplAndroid::RegisterSpeechRecognizer},
     {"TracingControllerAndroid", content::RegisterTracingControllerAndroid},
