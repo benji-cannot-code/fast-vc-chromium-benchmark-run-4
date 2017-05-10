acff6913d423d90fcee6d9db85a0e68712cb37d5
moshayedi <moshayedi@chromium.org>
2017-05-09 21:55:10 -0700

    Fix Onscreen Keyboard crash in --mus.
    OzonePlatform is instantiated in mus-ws in --mus and we don't have access to it
    in ui::KeyboardController. Trying to access OzonePlatform here failed the
    DCHECK() in ui::OzonePlatform::GetInstance().
    
    BUG=719540
    
    Review-Url: https://codereview.chromium.org/2871673004
    Cr-Commit-Position: refs/heads/master@{#470462}

--- ui/keyboard/keyboard_controller.cc
+++ ui/keyboard/keyboard_controller.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
+#include "ui/aura/env.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_delegate.h"
 #include "ui/aura/window_observer.h"
@@ -97,6 +98,10 @@ class KeyboardWindowDelegate : public aura::WindowDelegate {
 void ToggleTouchEventLogging(bool enable) {
 #if defined(OS_CHROMEOS)
 #if defined(USE_OZONE)
+  // TODO(moshayedi): crbug.com/642863. Revisit when we have mojo interface for
+  // InputController for processes that aren't mus-ws.
+  if (aura::Env::GetInstance()->mode() == aura::Env::Mode::MUS)
+    return;
   ui::InputController* controller =
       ui::OzonePlatform::GetInstance()->GetInputController();
   if (controller)
