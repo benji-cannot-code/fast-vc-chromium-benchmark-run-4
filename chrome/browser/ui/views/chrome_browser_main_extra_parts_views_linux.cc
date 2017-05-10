20dd6dc37582064f3beb7e5faad808042947968e
thomasanderson <thomasanderson@google.com>
2017-05-09 22:30:36 -0700

    Linux Aura: Use incognito profile to theme incognito browser windows
    This CL sets the theme profile of browser windows to the same as the
    actual profile.  The theme profile for children windows will continue
    using the original (ie. non-incognito) profile.
    
    This fixes an issue where incognito windows using the classic Aura
    theme would use NativeThemeAura instead of NativeThemeDarkAura.
    
    BUG=715710
    R=sky@chromium.org
    CC=estade@chromium.org
    
    Review-Url: https://codereview.chromium.org/2862113002
    Cr-Commit-Position: refs/heads/master@{#470478}

--- chrome/browser/ui/views/chrome_browser_main_extra_parts_views_linux.cc
+++ chrome/browser/ui/views/chrome_browser_main_extra_parts_views_linux.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/libgtkui/gtk_ui.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
-#include "chrome/browser/ui/views/theme_profile_key.h"
 #include "chrome/common/pref_names.h"
 #include "components/prefs/pref_service.h"
 #include "ui/aura/env.h"
@@ -35,7 +34,8 @@ ui::NativeTheme* GetNativeThemeForWindow(aura::Window* window) {
   if (!window)
     return nullptr;
 
-  Profile* profile = GetThemeProfileForWindow(window);
+  Profile* profile = reinterpret_cast<Profile*>(
+      window->GetNativeWindowProperty(Profile::kProfileKey));
 
   // If using the system (GTK) theme, don't use an Aura NativeTheme at all.
   // NB: ThemeService::UsingSystemTheme() might lag behind this pref. See
