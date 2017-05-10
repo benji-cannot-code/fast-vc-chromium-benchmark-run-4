2b1bc21ca7c1b0f572e63f8e39f5f52bba8ae7fc
pkl <pkl@chromium.org>
2017-05-10 05:53:12 -0700

    Adds mailto: URL support to app launching.
    Known Mail client apps are managed by a singleton class of
    MailtoURLRewriter. This CL adds support for the default system
    Mail client app and also for Gmail.
    
    BUG=711511
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2852003002
    Cr-Commit-Position: refs/heads/master@{#470544}

--- ios/chrome/browser/web/external_app_launcher.mm
+++ ios/chrome/browser/web/external_app_launcher.mm
@@ -12,10 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/strings/grit/components_strings.h"
 #include "ios/chrome/browser/experimental_flags.h"
 #import "ios/chrome/browser/open_url_util.h"
+#import "ios/chrome/browser/web/mailto_url_rewriter.h"
 #include "ios/chrome/grit/ios_strings.h"
 #import "net/base/mac/url_conversions.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "url/gurl.h"
+#include "url/url_constants.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -53,7 +55,7 @@ void RecordExternalApplicationOpened(bool opened) {
 }
 
 // Returns whether gURL has the scheme of a URL that initiates a call.
-BOOL UrlHasPhoneCallScheme(const GURL& gURL) {
+bool UrlHasPhoneCallScheme(const GURL& gURL) {
   return gURL.SchemeIs("tel") || gURL.SchemeIs("facetime") ||
          gURL.SchemeIs("facetime-audio");
 }
@@ -185,6 +187,18 @@ NSString* PromptActionString(NSString* scheme) {
                  afterDelay:0.0];
       return YES;
     }
+  }
+
+  // Replaces |URL| with a rewritten URL if it is of mailto: scheme.
+  if (!experimental_flags::IsNativeAppLauncherEnabled() &&
+      gURL.SchemeIs(url::kMailToScheme)) {
+    MailtoURLRewriter* rewriter =
+        [[MailtoURLRewriter alloc] initWithStandardHandlers];
+    NSString* launchURL = [rewriter rewriteMailtoURL:gURL];
+    if (launchURL) {
+      URL = [NSURL URLWithString:launchURL];
+    }
+    UMA_HISTOGRAM_BOOLEAN("IOS.MailtoURLRewritten", launchURL != nil);
   }
 
   // If the following call returns YES, an external application is about to be
---
+++ ios/chrome/browser/web/mailto_handler_gmail_unittest.mm
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/web/mailto_handler_gmail.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+#include "testing/gtest_mac.h"
+#include "url/gurl.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+TEST(MailtoHandlerGmailTest, TestConstructor) {
+  MailtoHandlerGmail* handler = [[MailtoHandlerGmail alloc] init];
+  EXPECT_NSEQ(@"Gmail", [handler appName]);
+  EXPECT_NSEQ(@"422689480", [handler appStoreID]);
+  EXPECT_NSEQ(@"googlegmail:/co?", [handler beginningScheme]);
+}
