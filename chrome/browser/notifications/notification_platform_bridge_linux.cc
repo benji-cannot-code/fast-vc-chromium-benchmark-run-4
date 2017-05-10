87e8cad90c948284207505c85254dd4dee7251a2
thestig <thestig@chromium.org>
2017-05-10 05:25:14 -0700

    Linux native notifications: Localize settings button.
    BUG=676220
    
    Review-Url: https://codereview.chromium.org/2873233002
    Cr-Commit-Position: refs/heads/master@{#470538}

--- chrome/browser/notifications/notification_platform_bridge_linux.cc
+++ chrome/browser/notifications/notification_platform_bridge_linux.cc
@@ -30,11 +30,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/notifications/notification_display_service_factory.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/shell_integration_linux.h"
+#include "chrome/grit/generated_resources.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/notification_service.h"
 #include "dbus/bus.h"
 #include "dbus/message.h"
 #include "dbus/object_proxy.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/image/image_skia.h"
 
 namespace {
@@ -468,8 +470,8 @@ class NotificationPlatformBridgeLinuxImpl
       }
       // Always add a settings button.
       actions.push_back(kSettingsButtonId);
-      // TODO(thomasanderson): Localize this string.
-      actions.push_back("Settings");
+      actions.push_back(
+          l10n_util::GetStringUTF8(IDS_NOTIFICATION_BUTTON_SETTINGS));
     }
     writer.AppendArrayOfStrings(actions);
 
