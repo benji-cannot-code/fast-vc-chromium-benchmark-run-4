a6dd7df6b47a91451501cf968cdf629687731543
thestig <thestig@chromium.org>
2017-05-09 22:38:54 -0700

    Remove debugging code from bug 258526.
    Replace the stack trace with just a bool to sanity check.
    
    Review-Url: https://codereview.chromium.org/2867663003
    Cr-Commit-Position: refs/heads/master@{#470484}

--- extensions/renderer/resources/event.js
+++ extensions/renderer/resources/event.js
@@ -204,12 +204,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       this.eventOptions.unmanaged = true;
     }
 
-    // Track whether the event has been destroyed to help track down the cause
-    // of http://crbug.com/258526.
-    // This variable will eventually hold the stack trace of the destroy call.
-    // TODO(kalman): Delete this and replace with more sound logic that catches
-    // when events are used without being *attached*.
-    this.destroyed = null;
+    // Track whether the event has been destroyed as a sanity check.
+    this.destroyed = false;
 
     if (this.eventOptions.unmanaged)
       this.attachmentStrategy = new NullAttachmentStrategy(this);
@@ -347,8 +343,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   EventImpl.prototype.dispatch_ = function(args, listenerIDs) {
     if (this.destroyed) {
-      throw new Error(this.eventName + ' was already destroyed at: ' +
-                      this.destroyed);
+      throw new Error(this.eventName + ' was already destroyed');
     }
     if (!this.eventOptions.supportsListeners)
       throw new Error("This event does not support listeners.");
@@ -403,7 +398,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   EventImpl.prototype.destroy_ = function() {
     this.listeners.length = 0;
     this.detach_();
-    this.destroyed = exceptionHandler.getStackTrace();
+    this.destroyed = true;
   };
 
   EventImpl.prototype.addRules = function(rules, opt_cb) {
