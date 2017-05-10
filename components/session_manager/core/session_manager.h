6dca7acebf7b6697e549638cba8b2a8d8c795278
emaxx <emaxx@chromium.org>
2017-05-10 04:14:56 -0700

    Add CHECK on session start for profile construction
    Fail fast from the profile construction when it's triggered without
    starting a session first.
    
    Crashing the browser is arguably a worth thing to do, as it prevents
    running the browser in unexpected environment (which may lead to, for
    instance, bypassing the managed policies).
    
    Also this CHECK should provide the stack traces which are more likely to
    point to the culprit code (as opposed to crashing at some later point
    asynchronously - like e.g. on the CHECK added at
    https://crrev.com/2801993002).
    
    BUG=719939
    TEST=existing browser tests;
        manual tests: normal login; Guest login; restart-after-crash.
    
    Review-Url: https://codereview.chromium.org/2868043002
    Cr-Commit-Position: refs/heads/master@{#470524}

--- components/session_manager/core/session_manager.h
+++ components/session_manager/core/session_manager.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_SESSION_MANAGER_CORE_SESSION_MANAGER_H_
 #define COMPONENTS_SESSION_MANAGER_CORE_SESSION_MANAGER_H_
 
+#include <string>
 #include <vector>
 
 #include "base/macros.h"
@@ -49,6 +50,9 @@ class SESSION_EXPORT SessionManager {
   // we perform additional initialization after the user is logged in but
   // before the session has been started.
   virtual void SessionStarted();
+
+  // Returns true if the session for the given user was started.
+  bool HasSessionForAccountId(const AccountId& user_account_id) const;
 
   // Convenience wrapps of session state.
   bool IsInSecondaryLoginScreen() const;
