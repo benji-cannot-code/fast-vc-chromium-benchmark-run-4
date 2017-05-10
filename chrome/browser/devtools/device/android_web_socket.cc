05da39bd88d37b5cd851393f6289f5a5fb4bbe9c
eostroukhov <eostroukhov@chromium.org>
2017-05-10 11:26:23 -0700

    [DevTools] Respond with a close frame
    WS spec section 5.5.1:
    
    If an endpoint receives a Close frame and did not previously send a
    Close frame, the endpoint MUST send a Close frame in response.  (When
    sending a Close frame in response, the endpoint typically echos the
    status code it received.)  It SHOULD do so as soon as practical.  An
    endpoint MAY delay sending a Close frame until its current message is
    sent (for instance, if the majority of a fragmented message is
    already sent, an endpoint MAY send the remaining fragments before
    sending a Close frame).  However, there is no guarantee that the
    endpoint that has already sent a Close frame will continue to process
    data.
    
    After both sending and receiving a Close message, an endpoint
    considers the WebSocket connection closed and MUST close the
    underlying TCP connection.  The server MUST close the underlying TCP
    connection immediately; the client SHOULD wait for the server to
    close the connection but MAY close the connection at any time after
    sending and receiving a Close message, e.g., if it has not received a
    TCP Close from the server in a reasonable time period.
    
    BUG=718619
    
    Review-Url: https://codereview.chromium.org/2871573011
    Cr-Commit-Position: refs/heads/master@{#470649}

--- chrome/browser/devtools/device/android_web_socket.cc
+++ chrome/browser/devtools/device/android_web_socket.cc
@@ -24,6 +24,7 @@ using net::WebSocket;
 namespace {
 
 const int kBufferSize = 16 * 1024;
+const char kCloseResponse[] = "\x88\x80\x2D\x0E\x1E\xFA";
 
 }  // namespace
 
@@ -62,9 +63,7 @@ class AndroidDeviceManager::AndroidWebSocket::WebSocketImpl {
     int mask = base::RandInt(0, 0x7FFFFFFF);
     std::string encoded_frame;
     encoder_->EncodeFrame(message, mask, &encoded_frame);
-    request_buffer_ += encoded_frame;
-    if (request_buffer_.length() == encoded_frame.length())
-      SendPendingRequests(0);
+    SendData(encoded_frame);
   }
 
  private:
@@ -103,13 +102,20 @@ class AndroidDeviceManager::AndroidWebSocket::WebSocketImpl {
       parse_result = encoder_->DecodeFrame(
           response_buffer_, &bytes_consumed, &output);
     }
+    if (parse_result == WebSocket::FRAME_CLOSE)
+      SendData(kCloseResponse);
 
-    if (parse_result == WebSocket::FRAME_ERROR ||
-        parse_result == WebSocket::FRAME_CLOSE) {
+    if (parse_result == WebSocket::FRAME_ERROR) {
       Disconnect();
       return;
     }
     Read(io_buffer);
+  }
+
+  void SendData(const std::string& data) {
+    request_buffer_ += data;
+    if (request_buffer_.length() == data.length())
+      SendPendingRequests(0);
   }
 
   void SendPendingRequests(int result) {
