ab1ada5e99c88dd96f09d14a84b6597e1ed0db06
chengx <chengx@chromium.org>
2017-05-10 11:54:58 -0700

    Fix to include correct header files in jumplist.{h, cc}
    This CL fixes jumplist.{h, cc} to include some necessary header files
    according to the iwyu (include-what-you-use) rule, and to remove some
    others which are redundant.
    
    BUG=719752
    
    Review-Url: https://codereview.chromium.org/2861393002
    Cr-Commit-Position: refs/heads/master@{#470659}

--- chrome/browser/win/jumplist.h
+++ chrome/browser/win/jumplist.h
@@ -9,12 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include <list>
+#include <memory>
 #include <string>
 #include <utility>
-#include <vector>
 
 #include "base/files/file_path.h"
 #include "base/macros.h"
+#include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
 #include "base/strings/string16.h"
 #include "base/synchronization/lock.h"
