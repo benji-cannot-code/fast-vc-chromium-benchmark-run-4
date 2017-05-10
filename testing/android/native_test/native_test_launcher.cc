4b338cc2212514badb989a818b8d1549a73c1bf8
mostynb <mostynb@opera.com>
2017-05-10 07:33:14 -0700

    include what you use: errno.h in native_test_launcher.cc
    Add missing errno.h include.
    
    Review-Url: https://codereview.chromium.org/1428653003
    Cr-Commit-Position: refs/heads/master@{#470564}

--- testing/android/native_test/native_test_launcher.cc
+++ testing/android/native_test/native_test_launcher.cc
@@ -11,7 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // It installs signal handlers to detect crashes.
 
 #include <android/log.h>
+#include <errno.h>
 #include <signal.h>
+#include <string.h>
 
 #include "base/android/base_jni_registrar.h"
 #include "base/android/context_utils.h"
