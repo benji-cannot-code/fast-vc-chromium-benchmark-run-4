efdced95b094c3bdfee9c742780138aa30d5eba3
Tommy Nyquist <nyquist@chromium.org>
2017-05-10 11:10:21 -0700

    Change the internal API for the In-Product Help Model
    The current Model for the Feature Engagement Tracker has a method
    GetEvent(...). This model currently returns a const Event&, which
    provides a clean an simple API for callers, knowing that the returned
    value can never be nullptr. However, this also forces the method to
    not be const, since it inserts an empty Event into the internal
    map of all events.
    
    This makes it impossible to pass around a const& to the Model whenever
    someone needs to look up events, which does not feel right.
    
    This CL therefore changes that method declaration to be:
    const Event* GetEvent(const std::string& event_name) const;
    
    It then of course updates all the relevant code to now possibly expect
    nullptr values.
    
    BUG=706309
    
    Change-Id: I54a091fd22c8a3e02aed5f290ff83645b72e1b67
    Reviewed-on: https://chromium-review.googlesource.com/501447
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#470665}

--- components/feature_engagement_tracker/internal/never_condition_validator_unittest.cc
+++ components/feature_engagement_tracker/internal/never_condition_validator_unittest.cc
@@ -45,8 +45,8 @@ class TestModel : public Model {
 
   bool IsCurrentlyShowing() const override { return false; }
 
-  const Event& GetEvent(const std::string& event_name) override {
-    return empty_event_;
+  const Event* GetEvent(const std::string& event_name) const override {
+    return nullptr;
   }
 
   void IncrementEvent(const std::string& event_name) override {}
@@ -55,7 +55,6 @@ class TestModel : public Model {
 
  private:
   FeatureConfig feature_config_;
-  Event empty_event_;
 
   DISALLOW_COPY_AND_ASSIGN(TestModel);
 };
