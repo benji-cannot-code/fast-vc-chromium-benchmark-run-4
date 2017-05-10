91d6421cd40b77aab02b8637a038ecfe6ffd347c
creis <creis@chromium.org>
2017-05-10 12:36:34 -0700

    Remove old diagnostic crash reports.
    We aren't actively investigating these, and they're generating
    crash reports without telling us new information.  Remove the old
    diagnostics.
    
    This basically reverts the following CLs from 575245:
    https://codereview.chromium.org/1613453002/
    https://codereview.chromium.org/1618063003/
    https://codereview.chromium.org/1643673002/
    https://codereview.chromium.org/1656603002/
    
    (I left https://codereview.chromium.org/1581193002/ in place,
    since it seems useful as a safeguard.  I also left
    https://codereview.chromium.org/1624413002/ because it seems
    to be indicative of a problem we might not be catching.)
    
    BUG=575245
    TEST=Fewer crash reports
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2572573003
    Cr-Commit-Position: refs/heads/master@{#470669}

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
 #include "base/debug/alias.h"
-#include "base/debug/crash_logging.h"
 #include "base/feature_list.h"
 #include "base/files/file_path.h"
 #include "base/i18n/rtl.h"
@@ -626,14 +625,6 @@ void RenderViewImpl::Initialize(
 
   if (command_line.HasSwitch(switches::kStatsCollectionController))
     stats_collection_observer_.reset(new StatsCollectionObserver(this));
-
-  // Debug cases of https://crbug.com/575245.
-  base::debug::SetCrashKeyValue("rvinit_view_id",
-                                base::IntToString(GetRoutingID()));
-  base::debug::SetCrashKeyValue("rvinit_proxy_id",
-                                base::IntToString(params.proxy_routing_id));
-  base::debug::SetCrashKeyValue(
-      "rvinit_main_frame_id", base::IntToString(params.main_frame_routing_id));
 
   webview()->SetDisplayMode(display_mode_);
   webview()->GetSettings()->SetPreferCompositingToLCDTextEnabled(
