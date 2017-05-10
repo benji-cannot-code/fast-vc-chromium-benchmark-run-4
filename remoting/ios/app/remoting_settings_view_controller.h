7b9cfd57ad2cdb5153526428af2defc5d8391168
nicholss <nicholss@chromium.org>
2017-05-10 09:39:14 -0700

    Moving the iOS directory to be remoting top level.
    The remoting/client directory is intended for shared client code. But
    the iOS directory is a whole client. Moving it to be more inline with
    Android and webapp.
    
    Review-Url: https://codereview.chromium.org/2871993003
    Cr-Commit-Position: refs/heads/master@{#470603}

---
+++ remoting/ios/app/remoting_settings_view_controller.h
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_IOS_APP_REMOTING_SETTINGS_VIEW_CONTROLLER_H_
+#define REMOTING_IOS_APP_REMOTING_SETTINGS_VIEW_CONTROLLER_H_
+
+#import "ios/third_party/material_components_ios/src/components/Collections/src/MaterialCollections.h"
+
+@interface RemotingSettingsViewController : MDCCollectionViewController
+@end
+
+#endif  // REMOTING_IOS_APP_REMOTING_SETTINGS_VIEW_CONTROLLER_H_
---
+++ remoting/ios/keychain_wrapper.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_IOS_KEYCHAIN_WRAPPER_H_
+#define REMOTING_IOS_KEYCHAIN_WRAPPER_H_
+
+#import <Foundation/Foundation.h>
+
+@class UserInfo;
+
+// Class to abstract the details from how iOS wants to write to the keychain.
+// TODO(nicholss): This will have to be futher refactored when we integrate
+// with the private Google auth.
+@interface KeychainWrapper : NSObject
+
+// Save a refresh token to the keychain.
+- (void)setRefreshToken:(NSString*)refreshToken;
+// Get the refresh token from the keychain, if there is one.
+- (NSString*)refreshToken;
+// Reset the keychain and the cache.
+- (void)resetKeychainItem;
+
+@end
+
+#endif  //  REMOTING_IOS_KEYCHAIN_WRAPPER_H_
