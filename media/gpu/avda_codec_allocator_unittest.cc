f16498d36678bcd3499d13ab2db9909b12b6389f
watk <watk@chromium.org>
2017-05-10 15:34:54 -0700

    media: AVDACodecAllocator now hides the TaskType it uses
    This improves the AVDACodecAllocator by encapsulating TaskType from
    AVDA.
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2863593005
    Cr-Commit-Position: refs/heads/master@{#470734}

--- media/gpu/avda_codec_allocator_unittest.cc
+++ media/gpu/avda_codec_allocator_unittest.cc
@@ -41,7 +41,7 @@ void WaitUntilRestarted(base::WaitableEvent* about_to_wait_event,
 void SignalImmediately(base::WaitableEvent* event) {
   event->Signal();
 }
-}
+}  // namespace
 
 class MockClient : public AVDACodecAllocatorClient {
  public:
@@ -97,7 +97,6 @@ class AVDACodecAllocatorTest : public testing::Test {
   }
 
  protected:
-
   // Start / stop the threads for |avda| on the right thread.
   bool StartThread(AVDACodecAllocatorClient* avda) {
     return PostAndWait(FROM_HERE, base::Bind(
@@ -134,10 +133,12 @@ class AVDACodecAllocatorTest : public testing::Test {
             allocator_, task_type));
   }
 
-  base::Optional<TaskType> TaskTypeForAllocation() {
-    return PostAndWait(FROM_HERE,
-                       base::Bind(&AVDACodecAllocator::TaskTypeForAllocation,
-                                  base::Unretained(allocator_)));
+  base::Optional<TaskType> TaskTypeForAllocation(
+      bool software_codec_forbidden) {
+    return PostAndWait(
+        FROM_HERE,
+        base::Bind(&AVDACodecAllocator::TaskTypeForAllocation,
+                   base::Unretained(allocator_), software_codec_forbidden));
   }
 
   scoped_refptr<base::SingleThreadTaskRunner> TaskRunnerFor(
@@ -202,7 +203,7 @@ TEST_F(AVDACodecAllocatorTest, ThreadsStopAfterAllClientsStop) {
 
 TEST_F(AVDACodecAllocatorTest, TestHangThread) {
   StartThread(avda1_);
-  ASSERT_EQ(AUTO_CODEC, TaskTypeForAllocation().value());
+  ASSERT_EQ(AUTO_CODEC, TaskTypeForAllocation(false));
 
   // Hang the AUTO_CODEC thread.
   base::WaitableEvent about_to_wait_event(
@@ -219,7 +220,7 @@ TEST_F(AVDACodecAllocatorTest, TestHangThread) {
 
   // Verify that we've failed over after a long time has passed.
   tick_clock_.Advance(base::TimeDelta::FromSeconds(1));
-  ASSERT_EQ(SW_CODEC, TaskTypeForAllocation().value());
+  ASSERT_EQ(SW_CODEC, TaskTypeForAllocation(false));
 
   // Un-hang the thread and wait for it to let another task run.  This will
   // notify |allocator_| that the thread is no longer hung.
@@ -233,7 +234,7 @@ TEST_F(AVDACodecAllocatorTest, TestHangThread) {
   done_waiting_event.Wait();
 
   // Verify that we've un-failed over.
-  ASSERT_EQ(AUTO_CODEC, TaskTypeForAllocation().value());
+  ASSERT_EQ(AUTO_CODEC, TaskTypeForAllocation(false));
 }
 
 }  // namespace media
