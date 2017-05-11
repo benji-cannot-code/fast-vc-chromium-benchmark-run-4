0dd2b5286685abac60b37596277c216cbd837c72
amp <amp@chromium.org>
2017-05-10 20:29:23 -0700

    VrShell: Exit fullscreen on app button press.
    BUG=719997
    
    Review-Url: https://codereview.chromium.org/2872143002
    Cr-Commit-Position: refs/heads/master@{#470794}

--- chrome/browser/android/vr_shell/ui_scene_manager_unittest.cc
+++ chrome/browser/android/vr_shell/ui_scene_manager_unittest.cc
@@ -30,6 +30,7 @@ class MockBrowserInterface : public VrBrowserInterface {
   MOCK_METHOD0(AppButtonClicked, void());
   MOCK_METHOD0(ForceExitVr, void());
   MOCK_METHOD0(ExitPresent, void());
+  MOCK_METHOD0(ExitFullscreen, void());
   MOCK_METHOD2(
       RunVRDisplayInfoCallback,
       void(const base::Callback<void(device::mojom::VRDisplayInfoPtr)>&,
@@ -65,11 +66,11 @@ class UiSceneManagerTest : public testing::Test {
   std::unique_ptr<UiSceneManager> manager_;
 };
 
-TEST_F(UiSceneManagerTest, ExitPresentOnAppButtonClick) {
-  InSequence s;
-
+TEST_F(UiSceneManagerTest, ExitPresentAndFullscreenOnAppButtonClick) {
   // Clicking app button should trigger to exit presentation.
   EXPECT_CALL(*browser_, ExitPresent()).Times(1);
+  // And also trigger exit fullscreen.
+  EXPECT_CALL(*browser_, ExitFullscreen()).Times(1);
   manager_->OnAppButtonClicked();
 }
 
