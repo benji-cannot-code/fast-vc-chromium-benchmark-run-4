fb4f7f0e552864f4917b61dc9095e4b5b86aaa29
miguelg <miguelg@chromium.org>
2017-05-11 05:16:07 -0700

    RE-add the ability to delete notification ids unknown by the display service.
    This is a manual revert of 865a357dda206267aafd20d23382b452cec8c084
    
    BUG=700343
    
    Review-Url: https://codereview.chromium.org/2868793003
    Cr-Commit-Position: refs/heads/master@{#470930}

--- content/shell/browser/layout_test/layout_test_permission_manager.cc
+++ content/shell/browser/layout_test/layout_test_permission_manager.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/permission_type.h"
 #include "content/public/browser/web_contents.h"
 #include "content/shell/browser/layout_test/layout_test_content_browser_client.h"
-#include "content/shell/browser/layout_test/layout_test_notification_manager.h"
 
 namespace content {
 
