761e3dbdd8d5da9e91701413427b05412389cc90
ke.he <ke.he@intel.com>
2017-05-10 21:09:10 -0700

    Generalize the API of WakeLockContext mojo interface.
    Generalize the API of WakeLockContext mojo interface, so client can pass
    type, reason and description as they want to create the WakeLockService.
    
    BUG=689410
    
    Review-Url: https://codereview.chromium.org/2867303003
    Cr-Commit-Position: refs/heads/master@{#470801}

--- device/wake_lock/wake_lock_service_context.cc
+++ device/wake_lock/wake_lock_service_context.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "device/power_save_blocker/power_save_blocker.h"
 #include "device/wake_lock/wake_lock_service_impl.h"
 
 namespace device {
@@ -23,14 +22,13 @@ WakeLockServiceContext::WakeLockServiceContext(
 WakeLockServiceContext::~WakeLockServiceContext() {}
 
 void WakeLockServiceContext::GetWakeLock(
+    mojom::WakeLockType type,
+    mojom::WakeLockReason reason,
+    const std::string& description,
     mojom::WakeLockServiceRequest request) {
   // WakeLockServiceImpl owns itself.
-  new WakeLockServiceImpl(std::move(request),
-                          device::PowerSaveBlocker::PowerSaveBlockerType::
-                              kPowerSaveBlockPreventDisplaySleep,
-                          device::PowerSaveBlocker::Reason::kReasonOther,
-                          "Wake Lock API", context_id_, native_view_getter_,
-                          file_task_runner_);
+  new WakeLockServiceImpl(std::move(request), type, reason, description,
+                          context_id_, native_view_getter_, file_task_runner_);
 }
 
 }  // namespace device
--- device/wake_lock/wake_lock_service_impl.h
+++ device/wake_lock/wake_lock_service_impl.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/single_thread_task_runner.h"
 #include "device/power_save_blocker/power_save_blocker.h"
+#include "device/wake_lock/public/interfaces/wake_lock_context.mojom.h"
 #include "device/wake_lock/public/interfaces/wake_lock_service.mojom.h"
 #include "device/wake_lock/wake_lock_service_context.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
@@ -23,8 +24,8 @@ class WakeLockServiceImpl : public mojom::WakeLockService {
  public:
   WakeLockServiceImpl(
       mojom::WakeLockServiceRequest request,
-      device::PowerSaveBlocker::PowerSaveBlockerType type,
-      device::PowerSaveBlocker::Reason reason,
+      mojom::WakeLockType type,
+      mojom::WakeLockReason reason,
       const std::string& description,
       int context_id,
       WakeLockContextCallback native_view_getter,
@@ -44,8 +45,8 @@ class WakeLockServiceImpl : public mojom::WakeLockService {
   void RemoveWakeLock();
   void OnConnectionError();
 
-  device::PowerSaveBlocker::PowerSaveBlockerType type_;
-  device::PowerSaveBlocker::Reason reason_;
+  mojom::WakeLockType type_;
+  mojom::WakeLockReason reason_;
   std::unique_ptr<std::string> description_;
   int num_lock_requests_;
 
