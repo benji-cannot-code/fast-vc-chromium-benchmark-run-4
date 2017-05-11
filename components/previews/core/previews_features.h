9ec3c6b9453d75e08ff10c37d4a8216d6433db06
ryansturm <ryansturm@chromium.org>
2017-05-11 12:07:47 -0700

    Add an about:flag to enable client-side LoFi
    This allows users to turn client-side LoFi on android.
    
    BUG=720920
    
    Review-Url: https://codereview.chromium.org/2870263004
    Cr-Commit-Position: refs/heads/master@{#471030}

--- components/previews/core/previews_features.h
+++ components/previews/core/previews_features.h
@@ -12,6 +12,7 @@ namespace previews {
 namespace features {
 
 extern const base::Feature kOfflinePreviews;
+extern const base::Feature kClientLoFi;
 
 }  // namespace features
 }  // namespace previews
