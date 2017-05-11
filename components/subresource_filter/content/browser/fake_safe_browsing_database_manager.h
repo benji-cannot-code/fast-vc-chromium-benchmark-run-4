12a5cb779e8ff3926236b39f7e6a2261036d31ae
csharrison <csharrison@chromium.org>
2017-05-11 07:22:42 -0700

    [subresource_filter] Make website setting existence imply site activation
    This patch tries its best to make the relationship 1:1 between site activation
    status and the WebsiteSetting metadata being present for that origin.
    
    The setting is added when a site is activated and the UI is shown (to prevent cases
    where no resources are filtered or we are activated but suppressing UI).
    
    The setting is removed when a site is found to not activate for a particular top level
    navigation (% content setting whitelists), or when a url matching the setting's origin
    is deleted from history. Since we don't want to clear this state if the user makes
    some explicit action (like whitelisting), we make sure the whitelisting check is the
    very last one in activation computation and don't take it into account for setting
    removal.
    
    In this way the setting is a best approximation for a cached activation state (minus
    content setting policy) for a given origin. Currently it is only used for the smart UI,
    but in follow up patches we will make other settings UI surfaces dependent on this
     state.
    
    Note: The setting is now used on both Desktop and Android, and is now NOT_LOSSY.
    
    BUG=689487
    
    Review-Url: https://codereview.chromium.org/2859783002
    Cr-Commit-Position: refs/heads/master@{#470945}

--- components/subresource_filter/content/browser/fake_safe_browsing_database_manager.h
+++ components/subresource_filter/content/browser/fake_safe_browsing_database_manager.h
@@ -24,6 +24,7 @@ class FakeSafeBrowsingDatabaseManager
 
   void AddBlacklistedUrl(const GURL& url,
                          safe_browsing::SBThreatType threat_type);
+  void RemoveBlacklistedUrl(const GURL& url);
 
   void SimulateTimeout();
 
