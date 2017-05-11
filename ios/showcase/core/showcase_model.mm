b7fe9a0be28c0ef1692b51bc3136b271fb668922
lod <lod@chromium.org>
2017-05-11 01:49:28 -0700

    Add coordinator for search widget in showcase.
    BUG=622743
    
    Review-Url: https://codereview.chromium.org/2858903004
    Cr-Commit-Position: refs/heads/master@{#470880}

--- ios/showcase/core/showcase_model.mm
+++ ios/showcase/core/showcase_model.mm
@@ -84,7 +84,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     },
     @{
       showcase::kClassForDisplayKey : @"SearchWidgetViewController",
-      showcase::kClassForInstantiationKey : @"SearchWidgetViewController",
+      showcase::kClassForInstantiationKey : @"SCSearchWidgetCoordinator",
       showcase::kUseCaseKey : @"Search Widget",
     },
   ];
