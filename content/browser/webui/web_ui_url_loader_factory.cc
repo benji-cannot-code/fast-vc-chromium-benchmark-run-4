eab4603c566291cf211c76540e9eb706624a3936
jam <jam@chromium.org>
2017-05-11 09:42:46 -0700

    Implement chrome://view-http-cache with network service.
    BUG=717714
    
    Review-Url: https://codereview.chromium.org/2870203002
    Cr-Commit-Position: refs/heads/master@{#470978}

--- content/browser/webui/web_ui_url_loader_factory.cc
+++ content/browser/webui/web_ui_url_loader_factory.cc
@@ -21,12 +21,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/webui/network_error_url_loader.h"
 #include "content/browser/webui/url_data_manager_backend.h"
 #include "content/browser/webui/url_data_source_impl.h"
+#include "content/common/network_service.mojom.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/common/service_manager_connection.h"
+#include "content/public/common/service_names.mojom.h"
 #include "content/public/common/url_constants.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
+#include "services/service_manager/public/cpp/connector.h"
 #include "third_party/zlib/google/compression_utils.h"
 #include "ui/base/template_expressions.h"
 
@@ -242,6 +246,14 @@ class WebUIURLLoaderFactory : public mojom::URLLoaderFactory,
                             const ResourceRequest& request,
                             mojom::URLLoaderClientPtr client) override {
     DCHECK_CURRENTLY_ON(BrowserThread::UI);
+    if (request.url.host_piece() == kChromeUINetworkViewCacheHost) {
+      mojom::NetworkServicePtr network_service;
+      ServiceManagerConnection::GetForProcess()->GetConnector()->BindInterface(
+          mojom::kNetworkServiceName, &network_service);
+      network_service->HandleViewCacheRequest(request, std::move(client));
+      return;
+    }
+
     if (request.url.host_piece() == kChromeUIBlobInternalsHost) {
       BrowserThread::PostTask(
           BrowserThread::IO, FROM_HERE,
@@ -250,11 +262,15 @@ class WebUIURLLoaderFactory : public mojom::URLLoaderFactory,
               base::Unretained(
                   ChromeBlobStorageContext::GetFor(browser_context_))));
       return;
-    } else if (request.url.host_piece() == kChromeUINetworkErrorHost ||
-               request.url.host_piece() == kChromeUIDinoHost) {
+    }
+
+    if (request.url.host_piece() == kChromeUINetworkErrorHost ||
+        request.url.host_piece() == kChromeUIDinoHost) {
       StartNetworkErrorsURLLoader(request, std::move(client));
       return;
-    } else if (request.url.host_piece() == kChromeUIHistogramHost) {
+    }
+
+    if (request.url.host_piece() == kChromeUIHistogramHost) {
       StartHistogramInternalsURLLoader(request, std::move(client));
       return;
     }
--- content/network/network_service.h
+++ content/network/network_service.h
@@ -9,15 +9,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
+#include "content/common/network_service.mojom.h"
 #include "content/common/url_loader_factory.mojom.h"
 #include "content/network/network_context.h"
+#include "mojo/public/cpp/bindings/binding_set.h"
 #include "mojo/public/cpp/bindings/strong_binding_set.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
 #include "services/service_manager/public/cpp/service.h"
 
 namespace content {
 
-class NetworkService : public service_manager::Service {
+class NetworkService : public service_manager::Service,
+                       public mojom::NetworkService {
  public:
   explicit NetworkService(
       std::unique_ptr<service_manager::BinderRegistry> registry);
@@ -29,8 +32,15 @@ class NetworkService : public service_manager::Service {
                        const std::string& interface_name,
                        mojo::ScopedMessagePipeHandle interface_pipe) override;
 
-  void Create(const service_manager::BindSourceInfo& source_info,
-              mojom::URLLoaderFactoryRequest request);
+  void CreateURLLoaderFactory(
+      const service_manager::BindSourceInfo& source_info,
+      mojom::URLLoaderFactoryRequest request);
+  void CreateNetworkService(const service_manager::BindSourceInfo& source_info,
+                            mojom::NetworkServiceRequest request);
+
+  // mojom::NetworkService implementation:
+  void HandleViewCacheRequest(const ResourceRequest& request,
+                              mojom::URLLoaderClientPtr client) override;
 
   std::unique_ptr<service_manager::BinderRegistry> registry_;
 
@@ -39,6 +49,8 @@ class NetworkService : public service_manager::Service {
   // Put it below |context_| so that |context_| outlives all the
   // NetworkServiceURLLoaderFactoryImpl instances.
   mojo::StrongBindingSet<mojom::URLLoaderFactory> loader_factory_bindings_;
+
+  mojo::BindingSet<mojom::NetworkService> network_service_bindings_;
 
   DISALLOW_COPY_AND_ASSIGN(NetworkService);
 };
