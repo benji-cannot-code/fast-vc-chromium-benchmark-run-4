d949b220e5180fec96fbe844beba24e1deddf837
ananta <ananta@chromium.org>
2017-05-10 19:42:48 -0700

    Description: Views/Harmony Remove references to ui/views/layout/layout_constants.h
    Replace references to constants in ui/views/layout/layout_constants.h with their
    equivalents using ChromeLayoutProvider. Removed some stale includes of
    layout_constants.h
    
    BUG=691897
    
    Review-Url: https://codereview.chromium.org/2867253003
    Cr-Commit-Position: refs/heads/master@{#470781}

--- chrome/browser/ui/views/certificate_selector.cc
+++ chrome/browser/ui/views/certificate_selector.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "chrome/browser/certificate_viewer.h"
+#include "chrome/browser/ui/views/harmony/chrome_layout_provider.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/constrained_window/constrained_window_views.h"
 #include "components/guest_view/browser/guest_view_base.h"
@@ -28,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/button/md_text_button.h"
 #include "ui/views/controls/table/table_view.h"
 #include "ui/views/layout/grid_layout.h"
-#include "ui/views/layout/layout_constants.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/window/dialog_client_view.h"
 
@@ -208,7 +208,11 @@ void CertificateSelector::InitWithText(
   layout->StartRow(0, kColumnSetId);
   layout->AddView(text_label.release());
 
-  layout->AddPaddingRow(0, views::kRelatedControlVerticalSpacing);
+  ChromeLayoutProvider* provider = ChromeLayoutProvider::Get();
+  const int vertical_spacing = provider->GetDistanceMetric(
+      views::DISTANCE_RELATED_CONTROL_VERTICAL);
+
+  layout->AddPaddingRow(0, vertical_spacing);
 
   std::vector<ui::TableColumn> columns;
   columns.push_back(ui::TableColumn(IDS_CERT_SELECTOR_SUBJECT_COLUMN,
@@ -229,7 +233,7 @@ void CertificateSelector::InitWithText(
                   views::GridLayout::FILL, views::GridLayout::FILL,
                   kTableViewWidth, kTableViewHeight);
 
-  layout->AddPaddingRow(0, views::kRelatedControlVerticalSpacing);
+  layout->AddPaddingRow(0, vertical_spacing);
 }
 
 ui::TableModel* CertificateSelector::table_model_for_testing() const {
