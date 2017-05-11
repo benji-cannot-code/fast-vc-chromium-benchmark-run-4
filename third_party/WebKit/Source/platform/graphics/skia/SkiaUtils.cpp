f9660740adcba58a7ed3bc3a058e62d60a8be73b
thakis <thakis@chromium.org>
2017-05-11 12:34:56 -0700

    Rewrite three more classes to new blink style.
    No behavior change.
    
    BUG=675877
    
    Review-Url: https://codereview.chromium.org/2875703003
    Cr-Commit-Position: refs/heads/master@{#471044}

--- third_party/WebKit/Source/platform/graphics/skia/SkiaUtils.cpp
+++ third_party/WebKit/Source/platform/graphics/skia/SkiaUtils.cpp
@@ -38,8 +38,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 static const struct CompositOpToXfermodeMode {
-  CompositeOperator m_composit_op;
-  SkBlendMode xfermode_mode_;
+  CompositeOperator composit_op;
+  SkBlendMode xfermode_mode;
 } kGMapCompositOpsToXfermodeModes[] = {
     {kCompositeClear, SkBlendMode::kClear},
     {kCompositeCopy, SkBlendMode::kSrc},
@@ -99,8 +99,8 @@ SkBlendMode WebCoreCompositeToSkiaComposite(CompositeOperator op,
          op));
     return SkBlendMode::kSrcOver;
   }
-  SkASSERT(table[static_cast<uint8_t>(op)].m_composit_op == op);
-  return table[static_cast<uint8_t>(op)].xfermode_mode_;
+  SkASSERT(table[static_cast<uint8_t>(op)].composit_op == op);
+  return table[static_cast<uint8_t>(op)].xfermode_mode;
 }
 
 CompositeOperator CompositeOperatorFromSkia(SkBlendMode xfer_mode) {
