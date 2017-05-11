151f617a695e226563d642e2ebe7712417644c68
yusukes <yusukes@chromium.org>
2017-05-11 15:46:38 -0700

    Refactor ARC OOM handler code
    This CL moves around some code for Chrome OS OOM handling to make
    the code a bit easier to follow:
    
    * Previously, code for selecting "protected" processes was scattered
      around the file. One was in TabManagerDelegate::GetSortedCandidates
      for protecting PERSISTENT processes from *both* Chrome and kernel
      OOM handler, and the other was in LowMemoryKillImpl for protecting
      FOCUSED and VISIBLE processes from *only* Chrome. This CL moves the
      former to AdjustOomPrioritiesImpl to simplify the code.
    * Previously, tab_manager_delegate_chromeos.cc also directly used
      arc::mojom::ProcessState enums. This CL moves the code to
      arc::ArcProcess with unit tests to hide ARC details from the
      TabManagerDelegate class.
    * Previously, AdjustOomPrioritiesImpl had an assumption that the
      processes the function protected from the kernel always had -1000
      OOM adjustment scores. The assumption is fine, but will make it
      difficult for us to e.g. protect arc::mojom::ProcessState::TOP
      processes from the kernel because unlike PERSISTENT ones, TOP
      processes change time to time and may have positive OOM adjustment
      scores. With this CL, AdjustOomPrioritiesImpl explicitly sets
      -1000 for processes the function protects to remove the assumption.
      In the future, we can modify ArcProcess::IsKillableForKernel as we
      like to experiment various OOM scoring policy without any
      artificial restrictions.
    * Add more unit tests for better code coverage.
    
    This CL does NOT change the current Chrome/kernel OOM handling
    behavior at all. This is pure code refactoring. Also, this CL
    does not touch the code for handling Chrome tabs. This is only
    for ARC.
    
    BUG=719537
    TEST=try
    TEST=just in case, manually confirmed PERSISTENT processes still
     have -1000 adjustment.
    
    Review-Url: https://codereview.chromium.org/2874543002
    Cr-Commit-Position: refs/heads/master@{#471099}

--- chrome/browser/chromeos/arc/process/arc_process.cc
+++ chrome/browser/chromeos/arc/process/arc_process.cc
@@ -9,6 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace arc {
 
+namespace {
+
+// A special process on Android side which serves as a dummy "focused" app
+// when the focused window is a Chrome side window (i.e., all Android
+// processes are running in the background). We don't want to kill it anyway.
+constexpr char kArcHomeProcess[] = "org.chromium.arc.home";
+
+}  // namespace
+
 ArcProcess::ArcProcess(base::ProcessId nspid,
                        base::ProcessId pid,
                        const std::string& process_name,
@@ -34,5 +43,18 @@ bool ArcProcess::operator<(const ArcProcess& rhs) const {
 
 ArcProcess::ArcProcess(ArcProcess&& other) = default;
 ArcProcess& ArcProcess::operator=(ArcProcess&& other) = default;
+
+bool ArcProcess::IsImportant() const {
+  return process_state() <= mojom::ProcessState::IMPORTANT_FOREGROUND ||
+         process_name() == kArcHomeProcess;
+}
+
+bool ArcProcess::IsKernelKillable() const {
+  // Protect PERSISTENT, PERSISTENT_UI, and our HOME processes since they should
+  // never be killed even by the kernel. Returning false for them allows their
+  // OOM adjustment scores to remain negative.
+  return process_state() > arc::mojom::ProcessState::PERSISTENT_UI &&
+         process_name() != kArcHomeProcess;
+}
 
 }  // namespace arc
