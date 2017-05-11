fe3ca26e13414fba6a28468eabdbcaea7cd4ca76
pkotwicz <pkotwicz@chromium.org>
2017-05-10 19:26:21 -0700

    Remove unused chrome://touch-icon content::URLDataSource
    This CL:
    - Removes the unused chrome://touch-icon content::URLDataSource
      Usage of chrome://touch-icon was removed in
    https://codereview.chromium.org/1596273003 and
    https://codereview.chromium.org/2494853003
    - Removes unused parameters to getFavicon() JS function
    - Removes restriction on favicon size requested via chrome://favicon/size.
      The restriction is not useful because there is no restriction on the requested
    scale factor.
    
    BUG=None
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    TBR=dbeam@ for chrome/browser/ui/webui/options
    
    Review-Url: https://codereview.chromium.org/2822093002
    Cr-Commit-Position: refs/heads/master@{#470776}

--- chrome/browser/ui/webui/favicon_source.cc
+++ chrome/browser/ui/webui/favicon_source.cc
@@ -46,19 +46,14 @@ FaviconSource::IconRequest::IconRequest(const IconRequest& other) = default;
 FaviconSource::IconRequest::~IconRequest() {
 }
 
-FaviconSource::FaviconSource(Profile* profile, IconType type)
-    : profile_(profile->GetOriginalProfile()),
-      icon_types_(type == FAVICON ? favicon_base::FAVICON
-                                  : favicon_base::TOUCH_PRECOMPOSED_ICON |
-                                        favicon_base::TOUCH_ICON |
-                                        favicon_base::FAVICON) {}
+FaviconSource::FaviconSource(Profile* profile)
+    : profile_(profile->GetOriginalProfile()) {}
 
 FaviconSource::~FaviconSource() {
 }
 
 std::string FaviconSource::GetSource() const {
-  return icon_types_ == favicon_base::FAVICON ? chrome::kChromeUIFaviconHost
-                                              : chrome::kChromeUITouchIconHost;
+  return chrome::kChromeUIFaviconHost;
 }
 
 void FaviconSource::StartDataRequest(
@@ -74,7 +69,7 @@ void FaviconSource::StartDataRequest(
   }
 
   chrome::ParsedFaviconPath parsed;
-  bool success = chrome::ParseFaviconPath(path, icon_types_, &parsed);
+  bool success = chrome::ParseFaviconPath(path, &parsed);
   if (!success) {
     SendDefaultResponse(callback);
     return;
@@ -115,14 +110,11 @@ void FaviconSource::StartDataRequest(
     }
 
     favicon_service->GetRawFaviconForPageURL(
-        url,
-        icon_types_,
-        desired_size_in_pixel,
-        base::Bind(
-            &FaviconSource::OnFaviconDataAvailable,
-            base::Unretained(this),
-            IconRequest(
-                callback, url, parsed.size_in_dip, parsed.device_scale_factor)),
+        url, favicon_base::FAVICON, desired_size_in_pixel,
+        base::Bind(&FaviconSource::OnFaviconDataAvailable,
+                   base::Unretained(this),
+                   IconRequest(callback, url, parsed.size_in_dip,
+                               parsed.device_scale_factor)),
         &cancelable_task_tracker_);
   }
 }
--- components/favicon_base/favicon_url_parser_unittest.cc
+++ components/favicon_base/favicon_url_parser_unittest.cc
@@ -37,11 +37,10 @@ class FaviconUrlParserTest : public testing::Test {
 // Test parsing path with no extra parameters.
 TEST_F(FaviconUrlParserTest, ParsingNoExtraParams) {
   const std::string url("https://www.google.ca/imghp?hl=en&tab=wi");
-  int icon_types = favicon_base::TOUCH_PRECOMPOSED_ICON;
   chrome::ParsedFaviconPath parsed;
 
   const std::string path1 = url;
-  EXPECT_TRUE(chrome::ParseFaviconPath(path1, icon_types, &parsed));
+  EXPECT_TRUE(chrome::ParseFaviconPath(path1, &parsed));
   EXPECT_FALSE(parsed.is_icon_url);
   EXPECT_EQ(url, parsed.url);
   EXPECT_EQ(16, parsed.size_in_dip);
@@ -51,12 +50,11 @@ TEST_F(FaviconUrlParserTest, ParsingNoExtraParams) {
 // Test parsing path with a 'size' parameter.
 TEST_F(FaviconUrlParserTest, ParsingSizeParam) {
   const std::string url("https://www.google.ca/imghp?hl=en&tab=wi");
-  int icon_types = favicon_base::TOUCH_PRECOMPOSED_ICON;
   chrome::ParsedFaviconPath parsed;
 
   // Test that we can still parse the legacy 'size' parameter format.
   const std::string path2 = "size/32/" + url;
-  EXPECT_TRUE(chrome::ParseFaviconPath(path2, icon_types, &parsed));
+  EXPECT_TRUE(chrome::ParseFaviconPath(path2, &parsed));
   EXPECT_FALSE(parsed.is_icon_url);
   EXPECT_EQ(url, parsed.url);
   EXPECT_EQ(32, parsed.size_in_dip);
@@ -64,7 +62,7 @@ TEST_F(FaviconUrlParserTest, ParsingSizeParam) {
 
   // Test parsing current 'size' parameter format.
   const std::string path3 = "size/32@1.4x/" + url;
-  EXPECT_TRUE(chrome::ParseFaviconPath(path3, icon_types, &parsed));
+  EXPECT_TRUE(chrome::ParseFaviconPath(path3, &parsed));
   EXPECT_FALSE(parsed.is_icon_url);
   EXPECT_EQ(url, parsed.url);
   EXPECT_EQ(32, parsed.size_in_dip);
@@ -73,7 +71,7 @@ TEST_F(FaviconUrlParserTest, ParsingSizeParam) {
   // Test that we pick the ui::ScaleFactor which is closest to the passed in
   // scale factor.
   const std::string path4 = "size/16@1.41x/" + url;
-  EXPECT_TRUE(chrome::ParseFaviconPath(path4, icon_types, &parsed));
+  EXPECT_TRUE(chrome::ParseFaviconPath(path4, &parsed));
   EXPECT_FALSE(parsed.is_icon_url);
   EXPECT_EQ(url, parsed.url);
   EXPECT_EQ(16, parsed.size_in_dip);
@@ -81,15 +79,15 @@ TEST_F(FaviconUrlParserTest, ParsingSizeParam) {
 
   // Invalid cases.
   const std::string path5 = "size/" + url;
-  EXPECT_FALSE(chrome::ParseFaviconPath(path5, icon_types, &parsed));
+  EXPECT_FALSE(chrome::ParseFaviconPath(path5, &parsed));
   const std::string path6 = "size/@1x/" + url;
-  EXPECT_FALSE(chrome::ParseFaviconPath(path6, icon_types, &parsed));
+  EXPECT_FALSE(chrome::ParseFaviconPath(path6, &parsed));
   const std::string path7 = "size/abc@1x/" + url;
-  EXPECT_FALSE(chrome::ParseFaviconPath(path7, icon_types, &parsed));
+  EXPECT_FALSE(chrome::ParseFaviconPath(path7, &parsed));
 
   // Part of url looks like 'size' parameter.
   const std::string path8 = "http://www.google.com/size/32@1.4x";
-  EXPECT_TRUE(chrome::ParseFaviconPath(path8, icon_types, &parsed));
+  EXPECT_TRUE(chrome::ParseFaviconPath(path8, &parsed));
   EXPECT_FALSE(parsed.is_icon_url);
   EXPECT_EQ(path8, parsed.url);
   EXPECT_EQ(16, parsed.size_in_dip);
@@ -99,11 +97,10 @@ TEST_F(FaviconUrlParserTest, ParsingSizeParam) {
 // Test parsing path with 'iconurl' parameter.
 TEST_F(FaviconUrlParserTest, ParsingIconUrlParam) {
   const std::string url("https://www.google.ca/imghp?hl=en&tab=wi");
-  int icon_types = favicon_base::TOUCH_PRECOMPOSED_ICON;
   chrome::ParsedFaviconPath parsed;
 
   const std::string path10 = "iconurl/http://www.google.com/favicon.ico";
-  EXPECT_TRUE(chrome::ParseFaviconPath(path10, icon_types, &parsed));
+  EXPECT_TRUE(chrome::ParseFaviconPath(path10, &parsed));
   EXPECT_TRUE(parsed.is_icon_url);
   EXPECT_EQ("http://www.google.com/favicon.ico", parsed.url);
   EXPECT_EQ(16, parsed.size_in_dip);
@@ -114,12 +111,11 @@ TEST_F(FaviconUrlParserTest, ParsingIconUrlParam) {
 // parameter.
 TEST_F(FaviconUrlParserTest, ParsingSizeParamAndUrlModifier) {
   const std::string url("https://www.google.ca/imghp?hl=en&tab=wi");
-  int icon_types = favicon_base::TOUCH_PRECOMPOSED_ICON;
   chrome::ParsedFaviconPath parsed;
 
   const std::string path14 =
       "size/32/iconurl/http://www.google.com/favicon.ico";
-  EXPECT_TRUE(chrome::ParseFaviconPath(path14, icon_types, &parsed));
+  EXPECT_TRUE(chrome::ParseFaviconPath(path14, &parsed));
   EXPECT_TRUE(parsed.is_icon_url);
   EXPECT_EQ("http://www.google.com/favicon.ico", parsed.url);
   EXPECT_EQ(32, parsed.size_in_dip);
