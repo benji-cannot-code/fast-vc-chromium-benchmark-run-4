31b78d6925646045c008e22f55cda1c116ba7000
slangley <slangley@chromium.org>
2017-05-11 03:48:53 -0700

    Move many standalone files from web/ to (core|modules)/exported/.
    This CL starts moving files from web/ that are implementations of classes defined
    in public/web/ that have no other dependencies in web/.
    
    Classes that have some dependency on modules/ move to modules/exported/, otherwise
    classes move to core/exported.
    
    This required setting up the modules/exported directory and BUILD file, and now
    exposing WebKit/Source/modules via the blink_all build rule so that the symbols
    are available.
    
    In some cases this also required adding BLINK_EXPORT to some of the definitions in
    public/web/, but these are all inside of BLINK_IMPLEMENTATION blocks which
    indicates that there should be no dependence on these methods externally.
    
    BUG=712963
    
    Review-Url: https://codereview.chromium.org/2874713002
    Cr-Commit-Position: refs/heads/master@{#470910}

--- third_party/WebKit/public/web/WebDateTimeSuggestion.h
+++ third_party/WebKit/public/web/WebDateTimeSuggestion.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WebDateTimeSuggestion_h
 #define WebDateTimeSuggestion_h
 
+#include "public/platform/WebCommon.h"
 #include "public/platform/WebString.h"
 
 namespace blink {
@@ -41,8 +42,8 @@ struct WebDateTimeSuggestion {
   WebDateTimeSuggestion() {}
 
 #if BLINK_IMPLEMENTATION
-  WebDateTimeSuggestion(const DateTimeSuggestion&);
-  WebDateTimeSuggestion& operator=(const DateTimeSuggestion&);
+  BLINK_EXPORT WebDateTimeSuggestion(const DateTimeSuggestion&);
+  BLINK_EXPORT WebDateTimeSuggestion& operator=(const DateTimeSuggestion&);
 #endif
 };
 
