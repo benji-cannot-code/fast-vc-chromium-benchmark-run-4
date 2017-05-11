7a18b13c040411f81338cb0bf3ddf7e66b9b0154
kaznacheev <kaznacheev@chromium.org>
2017-05-11 13:06:27 -0700

    Add VoiceInteractionFrameworkHost::SetMetalayerEnabled
    Make Metalayer mode visibility dependent on the container side
    setting.
    
    BUG=b:37997351
    TEST=Manual ("Metalayer mode" visible only when screenshots
      are enabled on the container side).
    
    Review-Url: https://codereview.chromium.org/2871583003
    Cr-Commit-Position: refs/heads/master@{#471051}

--- chrome/browser/chromeos/arc/voice_interaction/arc_voice_interaction_framework_service.h
+++ chrome/browser/chromeos/arc/voice_interaction/arc_voice_interaction_framework_service.h
@@ -44,6 +44,7 @@ class ArcVoiceInteractionFrameworkService
       const CaptureFocusedWindowCallback& callback) override;
   void CaptureFullscreen(const CaptureFullscreenCallback& callback) override;
   void OnMetalayerClosed() override;
+  void SetMetalayerEnabled(bool enabled) override;
 
   bool IsMetalayerSupported();
   void ShowMetalayer(const base::Closure& closed);
@@ -57,6 +58,7 @@ class ArcVoiceInteractionFrameworkService
 
   mojo::Binding<mojom::VoiceInteractionFrameworkHost> binding_;
   base::Closure metalayer_closed_callback_;
+  bool metalayer_enabled_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(ArcVoiceInteractionFrameworkService);
 };
