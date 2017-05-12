28e3b380b3d1cefe856b91a7f4c3cbf651bfe667
fdoray <fdoray@chromium.org>
2017-05-12 11:36:17 -0700

    Use TaskScheduler instead of SequencedWorkerPool in local_database_manager.cc.
    SequencedWorkerPool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=jialiul@chromium.org
    
    Review-Url: https://codereview.chromium.org/2882733002
    Cr-Commit-Position: refs/heads/master@{#471375}

--- chrome/browser/safe_browsing/local_database_manager.cc
+++ chrome/browser/safe_browsing/local_database_manager.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/string_util.h"
-#include "base/threading/sequenced_worker_pool.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_notification_types.h"
@@ -690,11 +690,9 @@ void LocalSafeBrowsingDatabaseManager::StartOnIOThread(
   // Only get a new task runner if there isn't one already. If the service has
   // previously been started and stopped, a task runner could already exist.
   if (!safe_browsing_task_runner_) {
-    base::SequencedWorkerPool* pool = BrowserThread::GetBlockingPool();
-    safe_browsing_task_runner_ =
-        pool->GetSequencedTaskRunnerWithShutdownBehavior(
-            pool->GetSequenceToken(),
-            base::SequencedWorkerPool::SKIP_ON_SHUTDOWN);
+    safe_browsing_task_runner_ = base::CreateSequencedTaskRunnerWithTraits(
+        {base::MayBlock(), base::TaskPriority::BACKGROUND,
+         base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN});
   }
 
   enabled_ = true;
