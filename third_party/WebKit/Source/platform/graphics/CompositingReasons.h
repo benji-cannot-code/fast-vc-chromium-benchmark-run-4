02fda15906c367d0ab20c58673c2cbc21bcf69ac
smcgruer <smcgruer@chromium.org>
2017-05-11 21:16:56 -0700

    Composite sticky-positioned elements when they have composited descendants
    In the case of nested sticky elements, if the descendant elements are promoted
    but their ancestor is not we will position them incorrectly. Instead of trying
    to re-compute main-thread positioning on the compositor, we can just promote
    any sticky element that has promoted sticky descendants.
    
    This CL goes slightly further and promotes any sticky element that has
    composited descendants regardless of whether they are sticky - doing so makes
    the code much simpler and the performance impact should be limited by the fact
    that sticky is not common.
    
    BUG=702229
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2754983002
    Cr-Commit-Position: refs/heads/master@{#471217}

--- third_party/WebKit/Source/platform/graphics/CompositingReasons.h
+++ third_party/WebKit/Source/platform/graphics/CompositingReasons.h
@@ -64,8 +64,9 @@ const uint64_t kCompositingReasonReflectionOfCompositedParent = UINT64_C(1)
                                                                 << 28;
 const uint64_t kCompositingReasonIsolateCompositedDescendants = UINT64_C(1)
                                                                 << 29;
-const uint64_t kCompositingReasonPositionFixedWithCompositedDescendants =
-    UINT64_C(1) << 30;
+const uint64_t
+    kCompositingReasonPositionFixedOrStickyWithCompositedDescendants =
+        UINT64_C(1) << 30;
 
 // The root layer is a special case. It may be forced to be a layer, but it also
 // needs to be a layer if anything else in the subtree is composited.
@@ -131,7 +132,7 @@ const uint64_t kCompositingReasonComboCompositedDescendants =
     kCompositingReasonBlendingWithCompositedDescendants |
     kCompositingReasonReflectionWithCompositedDescendants |
     kCompositingReasonClipsCompositingDescendants |
-    kCompositingReasonPositionFixedWithCompositedDescendants;
+    kCompositingReasonPositionFixedOrStickyWithCompositedDescendants;
 
 const uint64_t kCompositingReasonCombo3DDescendants =
     kCompositingReasonPreserve3DWith3DDescendants |
@@ -159,7 +160,7 @@ const uint64_t kCompositingReasonComboReasonsThatRequireOwnBacking =
                                                      // 3d-transformed elements
                                                      // intersect.
     kCompositingReasonBackdropFilter |
-    kCompositingReasonPositionFixedWithCompositedDescendants;
+    kCompositingReasonPositionFixedOrStickyWithCompositedDescendants;
 
 const uint64_t kCompositingReasonComboSquashableReasons =
     kCompositingReasonOverlap | kCompositingReasonAssumedOverlap |
