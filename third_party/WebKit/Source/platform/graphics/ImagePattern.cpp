498f398262fd44aea569c1e02c7c7b4b0a7ae78b
fmalita <fmalita@chromium.org>
2017-05-12 08:31:11 -0700

    Avoid record-time rasterization in ImagePattern::CreateShader()
    For non-repeating patterns, we currently use a workaround involving a
    padded SkImage.  Instead of allocating a raster SkImage, we can use a
    generated (picture-backed) SkImage.
    
    BUG=318968
    R=enne@chromium.org,danakj@chromium.org,reed@google.com
    
    Review-Url: https://codereview.chromium.org/2870203005
    Cr-Commit-Position: refs/heads/master@{#471301}

--- third_party/WebKit/Source/platform/graphics/ImagePattern.cpp
+++ third_party/WebKit/Source/platform/graphics/ImagePattern.cpp
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/graphics/paint/PaintShader.h"
 #include "platform/graphics/skia/SkiaUtils.h"
 #include "third_party/skia/include/core/SkImage.h"
+#include "third_party/skia/include/core/SkPictureRecorder.h"
 #include "third_party/skia/include/core/SkSurface.h"
 
 namespace blink {
@@ -61,24 +62,28 @@ sk_sp<PaintShader> ImagePattern::CreateShader(const SkMatrix& local_matrix) {
 
   // Create a transparent image 2 pixels wider and/or taller than the
   // original, then copy the orignal into the middle of it.
-  // FIXME: Is there a better way to pad (not scale) an image in skia?
-  sk_sp<SkSurface> surface = SkSurface::MakeRasterN32Premul(
-      tile_image_->width() + 2 * border_pixel_x,
-      tile_image_->height() + 2 * border_pixel_y);
-  if (!surface)
-    return WrapSkShader(SkShader::MakeColorShader(SK_ColorTRANSPARENT));
+  const SkISize tile_size =
+      SkISize::Make(tile_image_->width() + 2 * border_pixel_x,
+                    tile_image_->height() + 2 * border_pixel_y);
+  SkPictureRecorder recorder;
+  recorder.beginRecording(tile_size.width(), tile_size.height());
 
   SkPaint paint;
   paint.setBlendMode(SkBlendMode::kSrc);
-  surface->getCanvas()->drawImage(tile_image_, border_pixel_x, border_pixel_y,
-                                  &paint);
+  recorder.getRecordingCanvas()->drawImage(tile_image_, border_pixel_x,
+                                           border_pixel_y, &paint);
+  // Note: we use a picture *image* (as opposed to a picture *shader*) to
+  // lock-in the resolution (for 1px padding in particular).
+  sk_sp<SkImage> tile_image = SkImage::MakeFromPicture(
+      recorder.finishRecordingAsPicture(), tile_size, nullptr, nullptr,
+      SkImage::BitDepth::kU8, SkColorSpace::MakeSRGB());
 
   previous_local_matrix_ = local_matrix;
   SkMatrix adjusted_matrix(local_matrix);
   adjusted_matrix.postTranslate(-border_pixel_x, -border_pixel_y);
 
-  return MakePaintShaderImage(surface->makeImageSnapshot(), tile_mode_x,
-                              tile_mode_y, &adjusted_matrix);
+  return MakePaintShaderImage(std::move(tile_image), tile_mode_x, tile_mode_y,
+                              &adjusted_matrix);
 }
 
 bool ImagePattern::IsTextureBacked() const {
