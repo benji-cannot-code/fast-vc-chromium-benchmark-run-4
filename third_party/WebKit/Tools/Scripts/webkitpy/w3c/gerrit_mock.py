eb69549cee2d55902f8419582e5e372be16d3b02
Jeff Carpenter <jeffcarp@chromium.org>
2017-05-12 10:20:13 -0700

    [WPT Export] Reland of Create GitHub PRs from Gerrit CLs
    Bug: 700092
    Change-Id: Ide415b53d19148f2f01ab5e9df0844091c8bedae
    Reviewed-on: https://chromium-review.googlesource.com/503539
    Commit-Queue: Jeff Carpenter <jeffcarp@chromium.org>
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#471355}

--- third_party/WebKit/Tools/Scripts/webkitpy/w3c/gerrit_mock.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/w3c/gerrit_mock.py
@@ -4,12 +4,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 
-class MockGerrit(object):
+class MockGerritAPI(object):
 
     def __init__(self, host, user, token):
         self.host = host
         self.user = user
         self.token = token
 
-    def query_open_cls(self):
+    def query_exportable_open_cls(self):
         return []
+
+    def get(self, path, raw=False):  # pylint: disable=unused-argument
+        return '' if raw else {}
+
+    def post(self, path, data):  # pylint: disable=unused-argument
+        return {}
