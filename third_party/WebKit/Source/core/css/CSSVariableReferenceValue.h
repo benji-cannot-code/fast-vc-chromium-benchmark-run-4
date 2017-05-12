23c9f9ca62d2d22a04235ff398ba401d0ee2e323
alancutter <alancutter@chromium.org>
2017-05-11 20:18:14 -0700

    Ensure original parser context is used when parsing resolved var() references
    When resolving var() references at style building time we were not
    using the CSSParserContext specific to where the var() came from.
    This resulted in relative url()s not being able to resolve to an appropriate
    absolute URL based on the source base URL.
    
    This change attaches CSSParserContexts to var() references so they can be
    used at style resolution time and appropriately resolve relative URLs.
    
    BUG=700445
    
    Review-Url: https://codereview.chromium.org/2873943002
    Cr-Commit-Position: refs/heads/master@{#471205}

--- third_party/WebKit/Source/core/css/CSSVariableReferenceValue.h
+++ third_party/WebKit/Source/core/css/CSSVariableReferenceValue.h
@@ -8,17 +8,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/css/CSSValue.h"
 #include "core/css/CSSVariableData.h"
+#include "core/css/parser/CSSParserContext.h"
 #include "platform/wtf/RefPtr.h"
 
 namespace blink {
 
 class CSSVariableReferenceValue : public CSSValue {
  public:
-  static CSSVariableReferenceValue* Create(PassRefPtr<CSSVariableData> data) {
-    return new CSSVariableReferenceValue(std::move(data));
+  static CSSVariableReferenceValue* Create(PassRefPtr<CSSVariableData> data,
+                                           const CSSParserContext& context) {
+    return new CSSVariableReferenceValue(std::move(data), context);
   }
 
   CSSVariableData* VariableDataValue() const { return data_.Get(); }
+  const CSSParserContext* ParserContext() const {
+    return parser_context_.Get();
+  }
 
   bool Equals(const CSSVariableReferenceValue& other) const {
     return data_ == other.data_;
@@ -28,10 +33,16 @@ class CSSVariableReferenceValue : public CSSValue {
   DECLARE_TRACE_AFTER_DISPATCH();
 
  private:
-  CSSVariableReferenceValue(PassRefPtr<CSSVariableData> data)
-      : CSSValue(kVariableReferenceClass), data_(std::move(data)) {}
+  CSSVariableReferenceValue(PassRefPtr<CSSVariableData> data,
+                            const CSSParserContext& context)
+      : CSSValue(kVariableReferenceClass),
+        data_(std::move(data)),
+        parser_context_(context) {
+    DCHECK(parser_context_);
+  }
 
   RefPtr<CSSVariableData> data_;
+  Member<const CSSParserContext> parser_context_;
 };
 
 DEFINE_CSS_VALUE_TYPE_CASTS(CSSVariableReferenceValue,
--- third_party/WebKit/Source/core/css/parser/CSSVariableParser.h
+++ third_party/WebKit/Source/core/css/parser/CSSVariableParser.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class CSSCustomPropertyDeclaration;
+class CSSParserContext;
 class CSSVariableReferenceValue;
 
 class CORE_EXPORT CSSVariableParser {
@@ -27,6 +28,7 @@ class CORE_EXPORT CSSVariableParser {
       bool is_animation_tainted);
   static CSSVariableReferenceValue* ParseRegisteredPropertyValue(
       CSSParserTokenRange,
+      const CSSParserContext&,
       bool require_var_reference,
       bool is_animation_tainted);
 
