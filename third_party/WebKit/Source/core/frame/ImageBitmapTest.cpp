c5f2a628d7eb37a2e3bd36e4ab72c26331355483
hiroshige <hiroshige@chromium.org>
2017-05-11 18:56:02 -0700

    Split ImageLoader::SetImage() and set flags consistently in tests
    Previously, ImageLoader::SetImage() is used in three ways:
    1. SetImage(nullptr),
    2. SetImage(non-null) for ImageDocument, and
    3. SetImage(non-null) for unit tests for non-ImageDocument.
    and SetImage() sets has_pending_load_event_ = false and
    image_complete_ = true for all of these.
    
    This flag setting is consistent for 1., but not for 3., and causes
    assertion failures when we apply stronger assertions in [1].
    
    This CL fixes this by splitting SetImage() for separate methods:
    1. ClearImage(),
    2. SetImageForImageDocument(), and
    3. SetImageForTest(),
    and introducing UpdateImageState() that sets:
    - image_
    - has_pending_load_event_
    - image_complete_
    
    This CL
    - Doesn't change non-test behavior, i.e. keeps the behavior for
      Cases 1 and 2.
      Particularly, this leaves the flag values that are apparently
      inconsistent but needed for the current implementation in Case 2 in
      SetImageForImageDocument().
    - Changes the test-only behavior of Case 3, to set
        has_pending_load_event_ = true and
        image_complete_ = false in SetImageForTest().
      This doesn't affect the tested behavior but fixes the assertion
      failures in [1].
    
    [1] https://codereview.chromium.org/2859383002
    
    BUG=624697, 719759
    
    Review-Url: https://codereview.chromium.org/2864253003
    Cr-Commit-Position: refs/heads/master@{#471177}

--- third_party/WebKit/Source/core/frame/ImageBitmapTest.cpp
+++ third_party/WebKit/Source/core/frame/ImageBitmapTest.cpp
@@ -109,7 +109,7 @@ TEST_F(ImageBitmapTest, ImageResourceConsistency) {
       HTMLImageElement::Create(*Document::Create());
   ImageResourceContent* image = ImageResourceContent::CreateLoaded(
       StaticBitmapImage::Create(image_).Get());
-  image_element->SetImageResource(image);
+  image_element->SetImageForTest(image);
 
   Optional<IntRect> crop_rect =
       IntRect(0, 0, image_->width(), image_->height());
@@ -152,7 +152,7 @@ TEST_F(ImageBitmapTest, ImageBitmapSourceChanged) {
   ImageResourceContent* original_image_resource =
       ImageResourceContent::CreateLoaded(
           StaticBitmapImage::Create(image_).Get());
-  image->SetImageResource(original_image_resource);
+  image->SetImageForTest(original_image_resource);
 
   const ImageBitmapOptions default_options;
   Optional<IntRect> crop_rect =
@@ -167,7 +167,7 @@ TEST_F(ImageBitmapTest, ImageBitmapSourceChanged) {
 
   ImageResourceContent* new_image_resource = ImageResourceContent::CreateLoaded(
       StaticBitmapImage::Create(image2_).Get());
-  image->SetImageResource(new_image_resource);
+  image->SetImageForTest(new_image_resource);
 
   {
     ASSERT_NE(image_bitmap->BitmapImage()->ImageForCurrentFrame(),
@@ -245,7 +245,7 @@ TEST_F(ImageBitmapTest, ImageBitmapColorSpaceConversionHTMLImageElement) {
   ImageResourceContent* original_image_resource =
       ImageResourceContent::CreateLoaded(
           StaticBitmapImage::Create(image).Get());
-  image_element->SetImageResource(original_image_resource);
+  image_element->SetImageForTest(original_image_resource);
 
   Optional<IntRect> crop_rect = IntRect(0, 0, image->width(), image->height());
 
@@ -357,7 +357,7 @@ TEST_F(ImageBitmapTest, ImageBitmapColorSpaceConversionImageBitmap) {
   ImageResourceContent* source_image_resource =
       ImageResourceContent::CreateLoaded(
           StaticBitmapImage::Create(image).Get());
-  image_element->SetImageResource(source_image_resource);
+  image_element->SetImageForTest(source_image_resource);
 
   Optional<IntRect> crop_rect = IntRect(0, 0, image->width(), image->height());
   ImageBitmapOptions options =
