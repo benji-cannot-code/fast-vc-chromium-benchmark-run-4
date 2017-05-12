ab0e5a7d8d6dfe250c60fadb0ee2f1a41d89baa6
mathp <mathp@chromium.org>
2017-05-12 06:17:16 -0700

    [Payments] Desktop: Fix the error message dialog view
    The spinner wouldn't stop showing, even over the error message. Now that
    is fixed.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2877903002
    Cr-Commit-Position: refs/heads/master@{#471293}

--- chrome/browser/ui/views/payments/payment_request_dialog_view.cc
+++ chrome/browser/ui/views/payments/payment_request_dialog_view.cc
@@ -142,6 +142,8 @@ void PaymentRequestDialogView::ShowErrorMessage() {
                             request_->spec(), request_->state(), this),
                         &controller_map_),
                     /* animate = */ false);
+  HideProcessingSpinner();
+
   if (observer_for_testing_)
     observer_for_testing_->OnErrorMessageShown();
 }
@@ -348,6 +350,11 @@ void PaymentRequestDialogView::SetupSpinnerOverlay() {
 
   throbber_overlay_.SetLayoutManager(layout.release());
   AddChildView(&throbber_overlay_);
+}
+
+void PaymentRequestDialogView::HideProcessingSpinner() {
+  throbber_.Stop();
+  throbber_overlay_.SetVisible(false);
 }
 
 gfx::Size PaymentRequestDialogView::GetPreferredSize() const {
