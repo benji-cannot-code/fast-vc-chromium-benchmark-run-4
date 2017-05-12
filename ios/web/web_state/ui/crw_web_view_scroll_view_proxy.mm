880c5aca920ed091d353f59c101d3cfaf41666bb
ichikawa <ichikawa@chromium.org>
2017-05-11 18:40:04 -0700

    Add CRWWebViewScrollViewProxy.decelerating.
    It will be used in CWVScrollView.
    
    Review-Url: https://codereview.chromium.org/2868333005
    Cr-Commit-Position: refs/heads/master@{#471168}

--- ios/web/web_state/ui/crw_web_view_scroll_view_proxy.mm
+++ ios/web/web_state/ui/crw_web_view_scroll_view_proxy.mm
@@ -94,6 +94,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [_scrollView setBounces:bounces];
 }
 
+- (BOOL)isDecelerating {
+  return [_scrollView isDecelerating];
+}
+
 - (BOOL)isDragging {
   return [_scrollView isDragging];
 }
