419ba402ffaea6106eb21b754da28461aab9e0bb
fdoray <fdoray@chromium.org>
2017-05-12 11:39:37 -0700

    Use TaskScheduler instead of SequencedWorkerPool in notification_image_reporter.cc.
    SequencedWorkerPool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=jialiul@chromium.org
    
    Review-Url: https://codereview.chromium.org/2875993004
    Cr-Commit-Position: refs/heads/master@{#471379}

--- chrome/browser/safe_browsing/notification_image_reporter.cc
+++ chrome/browser/safe_browsing/notification_image_reporter.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted_memory.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/rand_util.h"
-#include "base/threading/sequenced_worker_pool.h"
+#include "base/task_scheduler/post_task.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/safe_browsing/safe_browsing_service.h"
@@ -151,9 +151,8 @@ void NotificationImageReporter::ReportNotificationImageOnUI(
                        weak_this_on_io));
     return;
   }
-
-  BrowserThread::GetBlockingPool()->PostWorkerTask(
-      FROM_HERE,
+  base::PostTaskWithTraits(
+      FROM_HERE, {base::MayBlock(), base::TaskPriority::BACKGROUND},
       base::BindOnce(
           &NotificationImageReporter::DownscaleNotificationImageOnBlockingPool,
           weak_this_on_io, origin, image));
@@ -164,8 +163,6 @@ void NotificationImageReporter::DownscaleNotificationImageOnBlockingPool(
     const base::WeakPtr<NotificationImageReporter>& weak_this_on_io,
     const GURL& origin,
     const SkBitmap& image) {
-  DCHECK(BrowserThread::GetBlockingPool()->RunsTasksOnCurrentThread());
-
   // Downscale to fit within 512x512. TODO(johnme): Get this from Finch.
   const double MAX_SIZE = 512;
   SkBitmap downscaled_image = image;
