8ad7fb338f3558fefe6c205b70e9114cde1c1441
szager <szager@chromium.org>
2017-05-12 04:45:00 -0700

    Update WebView/FrameView size from LayoutView::UpdateAfterLayout
    Previously, this was set *after* layout had completed, via
    FrameView::AdjustViewSize and WebViewImpl::LayoutUpdated.
    
    The layout size of the full document must be known before the
    WebView and FrameView can be resized, because the document size may
    affect the minimum page scale factor, which will affect the
    FrameView's size.  The earliest that the full document's layout size
    is known is after the LayoutView completes its block layout.
    
    This patch updates the FrameView size in
    LayoutView::UpdateAfterLayout, which is after the LayoutView has
    finished block layout, but still in time for the LayoutView to use
    the new sizes when updating its scrollbars (when root layer
    scrolling is enabled).
    
    The new test expectations are just paint invalidation reasons, which
    are expected; and one case (resize-iframe-text.html) where the existing
    invalidation geometry was too big, and this patch results in a smaller,
    correct set of invalidations.
    
    BUG=701575
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2860433002
    Cr-Commit-Position: refs/heads/master@{#471275}

--- third_party/WebKit/Source/core/layout/LayoutView.cpp
+++ third_party/WebKit/Source/core/layout/LayoutView.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/api/LayoutPartItem.h"
 #include "core/layout/api/LayoutViewItem.h"
 #include "core/layout/compositing/PaintLayerCompositor.h"
+#include "core/page/ChromeClient.h"
 #include "core/page/Page.h"
 #include "core/paint/PaintLayer.h"
 #include "core/paint/ViewPaintInvalidator.h"
@@ -700,6 +701,22 @@ LayoutUnit LayoutView::ViewLogicalHeightForPercentages() const {
 
 float LayoutView::ZoomFactor() const {
   return frame_view_->GetFrame().PageZoomFactor();
+}
+
+void LayoutView::UpdateAfterLayout() {
+  // Unlike every other layer, the root PaintLayer takes its size from the
+  // layout viewport size.  The call to AdjustViewSize() will update the
+  // frame's contents size, which will also update the page's minimum scale
+  // factor.  The call to ResizeAfterLayout() will calculate the layout viewport
+  // size based on the page minimum scale factor, and then update the FrameView
+  // with the new size.
+  if (HasOverflowClip())
+    GetScrollableArea()->ClampScrollOffsetAfterOverflowChange();
+  LocalFrame& frame = GetFrameView()->GetFrame();
+  if (!GetDocument().Printing())
+    GetFrameView()->AdjustViewSize();
+  frame.GetChromeClient().ResizeAfterLayout(&frame);
+  LayoutBlockFlow::UpdateAfterLayout();
 }
 
 void LayoutView::UpdateHitTestResult(HitTestResult& result,
