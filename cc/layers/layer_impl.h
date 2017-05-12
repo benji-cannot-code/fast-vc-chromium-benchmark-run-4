0d77fc74641ffc0ba5ef6c66ad1669b7ba3eead0
wkorman <wkorman@chromium.org>
2017-05-12 11:18:24 -0700

    Remove Layer{Impl}::TransformIsAnimating.
    Transform-focused version of http://crrev.com/2875133003 which does
    same for EffectIsAnimating.
    
    BUG=709137
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2881653002
    Cr-Commit-Position: refs/heads/master@{#471370}

--- cc/layers/layer_impl.h
+++ cc/layers/layer_impl.h
@@ -336,7 +336,6 @@ class CC_EXPORT LayerImpl {
     return touch_event_handler_region_;
   }
 
-  bool TransformIsAnimating() const;
   bool HasPotentiallyRunningTransformAnimation() const;
 
   bool HasFilterAnimationThatInflatesBounds() const;
