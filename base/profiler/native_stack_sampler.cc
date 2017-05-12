913cf6a554f505305dd138719ebc482d09df562f
bcwhite <bcwhite@chromium.org>
2017-05-12 06:28:12 -0700

    Use a common buffer across all instances for stack-copy.
    BUG=671716
    
    Review-Url: https://codereview.chromium.org/2601633002
    Cr-Commit-Position: refs/heads/master@{#471294}

--- base/profiler/native_stack_sampler.cc
+++ base/profiler/native_stack_sampler.cc
@@ -5,11 +5,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/profiler/native_stack_sampler.h"
 
+#include "base/memory/ptr_util.h"
+
 namespace base {
+
+NativeStackSampler::StackBuffer::StackBuffer(size_t buffer_size)
+    : buffer_(new uintptr_t[(buffer_size + sizeof(uintptr_t) - 1) /
+                            sizeof(uintptr_t)]),
+      size_(buffer_size) {}
+
+NativeStackSampler::StackBuffer::~StackBuffer() {}
 
 NativeStackSampler::NativeStackSampler() {}
 
 NativeStackSampler::~NativeStackSampler() {}
+
+std::unique_ptr<NativeStackSampler::StackBuffer>
+NativeStackSampler::CreateStackBuffer() {
+  size_t size = GetStackBufferSize();
+  if (size == 0)
+    return nullptr;
+  return MakeUnique<StackBuffer>(size);
+}
 
 NativeStackSamplerTestDelegate::~NativeStackSamplerTestDelegate() {}
 
