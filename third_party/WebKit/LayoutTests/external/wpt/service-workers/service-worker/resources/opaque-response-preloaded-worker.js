bf62d7c8b3f800534b6ace15408286a50f44c642
mike <mike@mikepennisi.com>
2017-05-12 09:58:31 -0700

    Upstream service worker opaque-response-preloaded.https.html test to WPT
    - Re-locate file for eventual submission to the Web Platform Tests
      project
    - Add "clean up" logic to remove iframe from document
    - Remove implementation-specific details from in-line documentation
    
    BUG=688116, 625575
    R=falken@chromium.org
    
    Review-Url: https://codereview.chromium.org/2877673004
    Cr-Commit-Position: refs/heads/master@{#471341}

---
+++ third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/opaque-response-preloaded-worker.js
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+importScripts('/common/get-host-info.sub.js');
+
+var remoteUrl = get_host_info()['HTTPS_REMOTE_ORIGIN'] +
+  '/service-workers/service-worker/resources/simple.txt'
+
+self.addEventListener('fetch', event => {
+    if (!event.request.url.match(/opaque-response$/)) {
+      return;
+    }
+
+    event.respondWith(fetch(remoteUrl, {mode: 'no-cors'}));
+  });
--- third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/opaque-response-in-memorycache-worker.js
+++
@@ -1,6 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-self.addEventListener('fetch', event => {
-    if (!event.request.url.match(/opaque-response$/))
-      return;
-    event.respondWith(fetch("http://localhost:8000/serviceworker/resources/simple.txt", {mode: 'no-cors'}));
-  });
