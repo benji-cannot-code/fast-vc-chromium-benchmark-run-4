cc1f9ed085e416500d73dccb2a0a9e812fc21cca
Nasko Oskov <nasko@chromium.org>
2017-05-12 09:54:39 -0700

    Revert "Disable ContentVerifierTest.FailOnDone and ContentVerifierTest.FailOnRead on Win 10 with PlzNavigate."
    This reverts commit e3cba8b7bd9d5bf5b045b5cb08434a80cf53b288.
    
    Bug: 699437
    Change-Id: I48c898574d26cca9af8d9def984c084680f71adb
    Reviewed-on: https://chromium-review.googlesource.com/504929
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#471440}

--- chrome/browser/extensions/content_verifier_browsertest.cc
+++ chrome/browser/extensions/content_verifier_browsertest.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "components/policy/core/browser/browser_policy_connector.h"
 #include "components/policy/core/common/mock_configuration_policy_provider.h"
-#include "content/public/common/browser_side_navigation_policy.h"
 #include "content/public/test/test_utils.h"
 #include "extensions/browser/content_verifier.h"
 #include "extensions/browser/content_verify_job.h"
@@ -40,10 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/updater/extension_downloader_test_delegate.h"
 #include "extensions/browser/updater/manifest_fetch_data.h"
 #include "extensions/common/extension_urls.h"
-
-#if defined(OS_WIN)
-#include "base/win/windows_version.h"
-#endif
 
 namespace extensions {
 
@@ -561,13 +556,6 @@ class ContentVerifierTest : public ExtensionBrowserTest {
 };
 
 IN_PROC_BROWSER_TEST_F(ContentVerifierTest, FailOnRead) {
-#if defined(OS_WIN)
-  if (content::IsBrowserSideNavigationEnabled() &&
-      base::win::GetVersion() >= base::win::VERSION_WIN10) {
-    // http://crbug.com/699437
-    return;
-  }
-#endif
   EXPECT_EQ(0, delegate_.bytes_read_failed());
   delegate_.fail_next_read();
   OpenPageAndWaitForUnload();
@@ -575,13 +563,6 @@ IN_PROC_BROWSER_TEST_F(ContentVerifierTest, FailOnRead) {
 }
 
 IN_PROC_BROWSER_TEST_F(ContentVerifierTest, FailOnDone) {
-#if defined(OS_WIN)
-  if (content::IsBrowserSideNavigationEnabled() &&
-      base::win::GetVersion() >= base::win::VERSION_WIN10) {
-    // http://crbug.com/699437
-    return;
-  }
-#endif
   EXPECT_EQ(0, delegate_.done_reading_failed());
   delegate_.fail_next_done();
   OpenPageAndWaitForUnload();
