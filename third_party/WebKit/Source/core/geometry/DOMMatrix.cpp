72ddc4bdadf7248126f784921bc45579d76722a7
fserb <fserb@chromium.org>
2017-05-11 19:04:03 -0700

    Stop accepting string constructor of DOMMatrix on workers
    This conforms to the spec:
    https://drafts.fxtf.org/geometry/#dom-dommatrixreadonly-dommatrixreadonly
    
    And ensures that we don't call SetMatrixValueFromString inside a worker
    
    BUG=703908
    
    Review-Url: https://codereview.chromium.org/2868953002
    Cr-Commit-Position: refs/heads/master@{#471182}

--- third_party/WebKit/Source/core/geometry/DOMMatrix.cpp
+++ third_party/WebKit/Source/core/geometry/DOMMatrix.cpp
@@ -7,13 +7,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-DOMMatrix* DOMMatrix::Create(ExceptionState& exception_state) {
+DOMMatrix* DOMMatrix::Create(ExecutionContext* execution_context,
+                             ExceptionState& exception_state) {
   return new DOMMatrix(TransformationMatrix());
 }
 
-DOMMatrix* DOMMatrix::Create(StringOrUnrestrictedDoubleSequence& init,
+DOMMatrix* DOMMatrix::Create(ExecutionContext* execution_context,
+                             StringOrUnrestrictedDoubleSequence& init,
                              ExceptionState& exception_state) {
   if (init.isString()) {
+    if (!execution_context->IsDocument()) {
+      exception_state.ThrowTypeError(
+          "DOMMatrix can't be constructed with strings on workers.");
+      return nullptr;
+    }
+
     DOMMatrix* matrix = new DOMMatrix(TransformationMatrix());
     matrix->SetMatrixValueFromString(init.getAsString(), exception_state);
     return matrix;
