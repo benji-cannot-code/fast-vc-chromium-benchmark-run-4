0eb636b8dab41d40ed62239241a001c617d8023d
jinsukkim <jinsukkim@chromium.org>
2017-05-12 02:57:04 -0700

    Reland "Refined DCHECK preventing multiple event forwarders in VA tree path"
    Previous CL: https://crrev.com/2861413002
    
    The new test in the CL depends on the presence of DCHECK macro
    but some buildbots failed since they don't have DCHECK enabled
    (Trybot passed because they have deliberately DCHECK on).
    
    Added #if DCHECK_IS_ON() to handle that, and also used a simpler
    macro EXPECT_DCHECK_DEATH over DCHECK_DEATH.
    
    > A ViewAndroid tree path from the top to a leaf node can have only
    > a single EventForwarder instance. Rewrote helper methods used for
    > DCHECK that enforces the requirement to match the actual job with
    > the intention.
    
    BUG=717271
    
    Review-Url: https://codereview.chromium.org/2861413002
    Cr-Commit-Position: refs/heads/master@{#470814}
    Committed: https://chromium.googlesource.com/chromium/src/+/1a116ecfdf829702b653ded84a0e1cdb8bdfd7d6
    
    patch from issue 2861413002 at patchset 200001 (http://crrev.com/2861413002#ps200001)
    
    Review-Url: https://codereview.chromium.org/2879713002
    Cr-Commit-Position: refs/heads/master@{#471262}

--- ui/android/view_android_unittests.cc
+++ ui/android/view_android_unittests.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/test/gtest_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/android/event_forwarder.h"
 #include "ui/android/view_android.h"
@@ -165,6 +166,39 @@ TEST_F(ViewAndroidBoundsTest, MatchesViewsWithOffset) {
   GenerateTouchEventAt(100, 70);
   EXPECT_TRUE(client1_.EventCalled());
   ExpectHit(client3_);
+}
+
+TEST(ViewAndroidTest, ChecksMultipleEventForwarders) {
+  ViewAndroid parent;
+  ViewAndroid child;
+  parent.GetEventForwarder();
+  child.GetEventForwarder();
+  EXPECT_DCHECK_DEATH(parent.AddChild(&child));
+
+  ViewAndroid parent2;
+  ViewAndroid child2;
+  parent2.GetEventForwarder();
+  parent2.AddChild(&child2);
+  EXPECT_DCHECK_DEATH(child2.GetEventForwarder());
+
+  ViewAndroid window;
+  ViewAndroid wcv1, wcv2;
+  ViewAndroid rwhv1a, rwhv1b, rwhv2;
+  wcv1.GetEventForwarder();
+  wcv2.GetEventForwarder();
+
+  window.AddChild(&wcv1);
+  wcv1.AddChild(&rwhv1a);
+  wcv1.AddChild(&rwhv1b);
+
+  wcv2.AddChild(&rwhv2);
+
+  // window should be able to add wcv2 since there's only one event forwarder
+  // in the path window - wcv2* - rwvh2
+  window.AddChild(&wcv2);
+
+  // Additional event forwarder will cause failure.
+  EXPECT_DCHECK_DEATH(rwhv2.GetEventForwarder());
 }
 
 }  // namespace ui
