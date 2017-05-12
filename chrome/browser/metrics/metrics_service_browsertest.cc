64ac7fb99bf207dadfc6fb5f5bc9f75b832c9942
hans <hans@chromium.org>
2017-05-12 11:56:10 -0700

    Disable stack trace during MetricsServiceBrowserTest.OOMRenderers
    DbgHelp is unreliable in low-memory sistuations. After intentionally exhausting
    the heap, the test would hang in DbgHelp's SymFromAddr().
    
    BUG=692564
    
    Review-Url: https://codereview.chromium.org/2879793003
    Cr-Commit-Position: refs/heads/master@{#471392}

--- chrome/browser/metrics/metrics_service_browsertest.cc
+++ chrome/browser/metrics/metrics_service_browsertest.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "content/public/test/browser_test_utils.h"
 #include "net/base/filename_util.h"
+#include "services/service_manager/embedder/switches.h"
 #include "ui/base/window_open_disposition.h"
 #include "url/gurl.h"
 
@@ -196,6 +197,11 @@ IN_PROC_BROWSER_TEST_F(MetricsServiceBrowserTest, MAYBE_CheckCrashRenderers) {
 // OOM code only works on Windows.
 #if defined(OS_WIN) && !defined(ADDRESS_SANITIZER)
 IN_PROC_BROWSER_TEST_F(MetricsServiceBrowserTest, OOMRenderers) {
+  // Disable stack traces during this test since DbgHelp is unreliable in
+  // low-memory conditions (see crbug.com/692564).
+  base::CommandLine::ForCurrentProcess()->AppendSwitch(
+      service_manager::switches::kDisableInProcessStackTraces);
+
   base::HistogramTester histogram_tester;
 
   OpenTabsAndNavigateToCrashyUrl(content::kChromeUIMemoryExhaustURL);
