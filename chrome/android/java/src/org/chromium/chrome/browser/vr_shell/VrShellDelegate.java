97295fd69de2f8b68b5e4fecbd1e1bfeeb1f8c38
mthiesse <mthiesse@chromium.org>
2017-05-12 14:09:42 -0700

    VR: Use an explicit Broadcast intent for returning from VR DON flow.
    Apparently using a PendingIntent created from an activity isn't enough to guarantee the broadcast goes where we want it to, so we need to use an explicit intent.
    
    This also corrects an error case where we create the broadcast PendingIntent without having registered the receiver.
    
    BUG=721547
    
    Review-Url: https://codereview.chromium.org/2875223002
    Cr-Commit-Position: refs/heads/master@{#471443}

--- chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellDelegate.java
@@ -99,6 +99,7 @@ public class VrShellDelegate implements ApplicationStatus.ActivityStateListener,
 
     private static VrShellDelegate sInstance;
     private static VrBroadcastReceiver sVrBroadcastReceiver;
+    private static boolean sRegisteredDaydreamHook = false;
 
     private ChromeActivity mActivity;
 
@@ -127,7 +128,7 @@ public class VrShellDelegate implements ApplicationStatus.ActivityStateListener,
     private boolean mListeningForWebVrActivate;
     private boolean mListeningForWebVrActivateBeforePause;
 
-    private static class VrBroadcastReceiver extends BroadcastReceiver {
+    private static final class VrBroadcastReceiver extends BroadcastReceiver {
         private final WeakReference<ChromeActivity> mTargetActivity;
 
         public VrBroadcastReceiver(ChromeActivity activity) {
@@ -278,7 +279,7 @@ public class VrShellDelegate implements ApplicationStatus.ActivityStateListener,
         // Daydream is not supported on pre-N devices.
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) return;
         if (sInstance != null) return; // Will be handled in onPause.
-        if (!activitySupportsVrBrowsing(activity)) return;
+        if (!sRegisteredDaydreamHook) return;
         VrClassesWrapper wrapper = getVrClassesWrapper();
         if (wrapper == null) return;
         VrDaydreamApi api = wrapper.createVrDaydreamApi(activity);
@@ -341,7 +342,13 @@ public class VrShellDelegate implements ApplicationStatus.ActivityStateListener,
     }
 
     private static PendingIntent getEnterVrPendingIntent(ChromeActivity activity) {
+        if (sVrBroadcastReceiver != null) sVrBroadcastReceiver.unregister();
+        IntentFilter filter = new IntentFilter(VR_ENTRY_RESULT_ACTION);
+        sVrBroadcastReceiver = new VrBroadcastReceiver(activity);
+        activity.registerReceiver(sVrBroadcastReceiver, filter);
+
         Intent vrIntent = new Intent(VR_ENTRY_RESULT_ACTION);
+        vrIntent.setPackage(activity.getPackageName());
         return PendingIntent.getBroadcast(activity, 0, vrIntent,
                 PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_ONE_SHOT);
     }
@@ -351,18 +358,18 @@ public class VrShellDelegate implements ApplicationStatus.ActivityStateListener,
      */
     private static void registerDaydreamIntent(
             final VrDaydreamApi daydreamApi, final ChromeActivity activity) {
-        if (sVrBroadcastReceiver != null) sVrBroadcastReceiver.unregister();
+        if (sRegisteredDaydreamHook) return;
         if (!daydreamApi.registerDaydreamIntent(getEnterVrPendingIntent(activity))) return;
-        IntentFilter filter = new IntentFilter(VR_ENTRY_RESULT_ACTION);
-        sVrBroadcastReceiver = new VrBroadcastReceiver(activity);
-        activity.registerReceiver(sVrBroadcastReceiver, filter);
+        sRegisteredDaydreamHook = true;
     }
 
     /**
      * Unregisters the Intent which registered by this context if any.
      */
     private static void unregisterDaydreamIntent(VrDaydreamApi daydreamApi) {
+        if (!sRegisteredDaydreamHook) return;
         daydreamApi.unregisterDaydreamIntent();
+        sRegisteredDaydreamHook = false;
     }
 
     /**
@@ -671,20 +678,18 @@ public class VrShellDelegate implements ApplicationStatus.ActivityStateListener,
 
     private void pauseVr() {
         mPaused = true;
+        unregisterDaydreamIntent(mVrDaydreamApi);
         if (mVrSupportLevel == VR_NOT_AVAILABLE) return;
 
-        if (mVrSupportLevel == VR_DAYDREAM) {
-            unregisterDaydreamIntent(mVrDaydreamApi);
+        // When the active web page has a vrdisplayactivate event handler,
+        // mListeningForWebVrActivate should be set to true, which means a vrdisplayactive event
+        // should be fired once DON flow finished. However, DON flow will pause our activity,
+        // which makes the active page becomes invisible. And the event fires before the active
+        // page becomes visible again after DON finished. So here we remember the value of
+        // mListeningForWebVrActivity before pause and use this value to decide if
+        // vrdisplayactivate event should be dispatched in enterVRFromIntent.
+        mListeningForWebVrActivateBeforePause = mListeningForWebVrActivate;
 
-            // When the active web page has a vrdisplayactivate event handler,
-            // mListeningForWebVrActivate should be set to true, which means a vrdisplayactive event
-            // should be fired once DON flow finished. However, DON flow will pause our activity,
-            // which makes the active page becomes invisible. And the event fires before the active
-            // page becomes visible again after DON finished. So here we remember the value of
-            // mListeningForWebVrActivity before pause and use this value to decide if
-            // vrdisplayactivate event should be dispatched in enterVRFromIntent.
-            mListeningForWebVrActivateBeforePause = mListeningForWebVrActivate;
-        }
         if (mNativeVrShellDelegate != 0) nativeOnPause(mNativeVrShellDelegate);
 
         // TODO(mthiesse): When VR Shell lives in its own activity, and integrates with Daydream
