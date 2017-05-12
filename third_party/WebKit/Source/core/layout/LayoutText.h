9e7ba196fb619b1af7dc3fa4ff8f97af295004ac
shend <shend@chromium.org>
2017-05-11 23:53:48 -0700

    Make ETextSecurity an enum class.
    This patch makes ETextSecurity an enum class for better type safety.
    
    BUG=684966
    TBR=aboxhall@chromium.org
    
    Review-Url: https://codereview.chromium.org/2868783005
    Cr-Commit-Position: refs/heads/master@{#471239}

--- third_party/WebKit/Source/core/layout/LayoutText.h
+++ third_party/WebKit/Source/core/layout/LayoutText.h
@@ -208,7 +208,9 @@ class CORE_EXPORT LayoutText : public LayoutObject {
 
   bool ContainsReversedText() const { return contains_reversed_text_; }
 
-  bool IsSecure() const { return Style()->TextSecurity() != TSNONE; }
+  bool IsSecure() const {
+    return Style()->TextSecurity() != ETextSecurity::kNone;
+  }
   void MomentarilyRevealLastTypedCharacter(
       unsigned last_typed_character_offset);
 
