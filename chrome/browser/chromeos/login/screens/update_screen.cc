827c5362376ca3b769f2f7712a1ac32ddf9628e3
weidongg <weidongg@chromium.org>
2017-05-13 15:49:35 -0700

    Make update over cellular an option for user
    - What is changed in update engine of Chrome OS?
    A new state NEED_PERMISSION_TO_UPDATE will be broadcasted to Chrome UI
    When update aborts due to cellular connection.
    A new dbus interface SetUpdateOverCellularTarget() is added to set
    update target (version and size) in preferences maintained by update
    engine.
    
    - What is the target in SetUpdateOverCellularTarget()?
    When update engine broadcast NEED_PERMISSION_TO_UPDATE, it also
    broadcasts the metadata of this update such as version and size. We call
    this target. Once user grants the permission to proceed update over
    cellular, we call SetUpdateOverCellularTarget() to send the target back
    to Chrome OS.
    
    - Why do we need to send the target back to update engine?
    There's a corner: When NEED_PERMISSION_TO_UPDATE is broadcasted to
    chrome UI, an update warning dialog will show the update size to user
    and ask whether to proceed. If user just keep the dialog window long
    enough until there's a new update available. Then clicking 'Continue'
    will result in a much larger download as the update engine only download
    the lastest update.
    
    This rarely happens, but it's more user friendly to consider this case.
    
    We send this target back to update engine to make it check with the
    server. If the target matches the latest update on the server, then
    update proceeds. Otherwise, update engine needs to broadcast
    NEED_PERMISSION_TO_UPDATE again with the latest update metadata.
    
    - Design doc: go/cros-cellular-updates
    
    - Related chromium CL: https://codereview.chromium.org/2873193002/
    
    - Dependent on chromeos CL:
    https://chromium-review.googlesource.com/#/c/479467/
    https://chromium-review.googlesource.com/#/c/481102/
    
    BUG=chromium:691108
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2881473002
    Cr-Commit-Position: refs/heads/master@{#471609}

--- chrome/browser/chromeos/login/screens/update_screen.cc
+++ chrome/browser/chromeos/login/screens/update_screen.cc
@@ -198,6 +198,7 @@ void UpdateScreen::ExitUpdate(UpdateScreen::ExitReason reason) {
         case UpdateEngineClient::UPDATE_STATUS_DOWNLOADING:
         case UpdateEngineClient::UPDATE_STATUS_FINALIZING:
         case UpdateEngineClient::UPDATE_STATUS_VERIFYING:
+        case UpdateEngineClient::UPDATE_STATUS_NEED_PERMISSION_TO_UPDATE:
           DCHECK(!HasCriticalUpdate());
         // Noncritical update, just exit screen as if there is no update.
         // no break
@@ -331,6 +332,7 @@ void UpdateScreen::UpdateStatusChanged(
       // else no break
     case UpdateEngineClient::UPDATE_STATUS_ERROR:
     case UpdateEngineClient::UPDATE_STATUS_REPORTING_ERROR_EVENT:
+    case UpdateEngineClient::UPDATE_STATUS_NEED_PERMISSION_TO_UPDATE:
       ExitUpdate(REASON_UPDATE_ENDED);
       break;
     default:
