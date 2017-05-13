5e0912a59749f1161e121619a2d73c95633dc25e
japhet <japhet@chromium.org>
2017-05-12 18:18:58 -0700

    Reland "Simplify FrameLoader::Clear(), remove FrameLoader::check_timer"
    Clear() is full of strange, old logic. Several clauses are outright unnecessary, or have better-encapsulated homes.
    
    check_timer_ appears to have two uses, one of which is vestigial, the other of which can be better-situated to not require a timer.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2878553002
    Cr-Commit-Position: refs/heads/master@{#471526}

--- third_party/WebKit/Source/bindings/core/v8/ScriptController.h
+++ third_party/WebKit/Source/bindings/core/v8/ScriptController.h
@@ -122,7 +122,6 @@ class CORE_EXPORT ScriptController final
   // ignored when evaluating resources injected into the DOM.
   bool ShouldBypassMainWorldCSP();
 
-  void EnableEval();
   void DisableEval(const String& error_message);
 
   TextPosition EventHandlerPosition() const;
@@ -149,6 +148,7 @@ class CORE_EXPORT ScriptController final
   v8::Isolate* GetIsolate() const {
     return window_proxy_manager_->GetIsolate();
   }
+  void EnableEval();
 
   v8::Local<v8::Value> EvaluateScriptInMainWorld(const ScriptSourceCode&,
                                                  AccessControlStatus,
