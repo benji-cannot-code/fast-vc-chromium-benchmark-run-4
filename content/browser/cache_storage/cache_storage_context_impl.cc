93ebf5e1ba11624fd5391404b28aac3ede749e16
fdoray <fdoray@chromium.org>
2017-05-15 13:50:42 -0700

    Use TaskScheduler instead of SequencedWorkerPool in cache_storage_context_impl.cc.
    SequencedWorkerPool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=jkarlin@chromium.org
    
    Review-Url: https://codereview.chromium.org/2875413002
    Cr-Commit-Position: refs/heads/master@{#471892}

--- content/browser/cache_storage/cache_storage_context_impl.cc
+++ content/browser/cache_storage/cache_storage_context_impl.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/files/file_path.h"
-#include "base/threading/sequenced_worker_pool.h"
+#include "base/task_scheduler/post_task.h"
 #include "content/browser/blob_storage/chrome_blob_storage_context.h"
 #include "content/browser/cache_storage/cache_storage_manager.h"
 #include "content/public/browser/browser_context.h"
@@ -33,11 +33,10 @@ void CacheStorageContextImpl::Init(
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
   is_incognito_ = user_data_directory.empty();
-  base::SequencedWorkerPool* pool = BrowserThread::GetBlockingPool();
   scoped_refptr<base::SequencedTaskRunner> cache_task_runner =
-      pool->GetSequencedTaskRunnerWithShutdownBehavior(
-          base::SequencedWorkerPool::GetSequenceToken(),
-          base::SequencedWorkerPool::SKIP_ON_SHUTDOWN);
+      base::CreateSequencedTaskRunnerWithTraits(
+          {base::MayBlock(), base::TaskPriority::BACKGROUND,
+           base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN});
 
   // This thread-hopping antipattern is needed here for some unit tests, where
   // browser threads are collapsed the quota manager is initialized before the
