7da0b588be496bb8cf2bc668dbb3fc1f0fd8212d
hugoh <hugoh@opera.com>
2017-05-15 03:28:59 -0700

    Make context menu aware of hidden selection
    When the frame's selection is hidden, the context menu should
    use the focused element (not the selection) as context.
    
    BUG=715059
    
    Expected: Context menu for <a>.
    Review-Url: https://codereview.chromium.org/2869713003
    Cr-Commit-Position: refs/heads/master@{#471719}

--- third_party/WebKit/Source/core/input/EventHandler.cpp
+++ third_party/WebKit/Source/core/input/EventHandler.cpp
@@ -1827,7 +1827,7 @@ WebInputEventResult EventHandler::SendContextMenuEventForKey(
       selection.ComputeVisibleSelectionInDOMTreeDeprecated().Start();
   VisualViewport& visual_viewport = frame_->GetPage()->GetVisualViewport();
 
-  if (!override_target_element && start.AnchorNode() &&
+  if (!override_target_element && start.AnchorNode() && !selection.IsHidden() &&
       (selection.ComputeVisibleSelectionInDOMTreeDeprecated()
            .RootEditableElement() ||
        selection.ComputeVisibleSelectionInDOMTreeDeprecated().IsRange())) {
