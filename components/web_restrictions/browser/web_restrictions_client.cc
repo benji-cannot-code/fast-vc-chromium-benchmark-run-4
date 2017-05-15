1dab25ada06f0dba811ff305e7b6674579c02ba3
fdoray <fdoray@chromium.org>
2017-05-15 02:54:25 -0700

    Use TaskScheduler instead of SequencedWorkerPool in web_restrictions_client.cc.
    SequencedWorkerPool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=bauerb@chromium.org
    
    Review-Url: https://codereview.chromium.org/2877143002
    Cr-Commit-Position: refs/heads/master@{#471714}

--- components/web_restrictions/browser/web_restrictions_client.cc
+++ components/web_restrictions/browser/web_restrictions_client.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/task_scheduler/post_task.h"
-#include "base/threading/sequenced_worker_pool.h"
 #include "base/threading/thread_restrictions.h"
 #include "content/public/browser/browser_thread.h"
 #include "jni/WebRestrictionsClient_jni.h"
@@ -48,12 +47,9 @@ bool WebRestrictionsClient::Register(JNIEnv* env) {
 
 WebRestrictionsClient::WebRestrictionsClient()
     : initialized_(false), supports_request_(false) {
-  base::SequencedWorkerPool* worker_pool =
-      content::BrowserThread::GetBlockingPool();
-  background_task_runner_ =
-      worker_pool->GetSequencedTaskRunnerWithShutdownBehavior(
-          worker_pool->GetSequenceToken(),
-          base::SequencedWorkerPool::SKIP_ON_SHUTDOWN);
+  background_task_runner_ = base::CreateSequencedTaskRunnerWithTraits(
+      {base::MayBlock(), base::TaskPriority::BACKGROUND,
+       base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN});
 }
 
 WebRestrictionsClient::~WebRestrictionsClient() {
