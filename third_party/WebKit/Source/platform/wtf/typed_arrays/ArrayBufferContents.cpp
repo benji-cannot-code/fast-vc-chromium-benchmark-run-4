be3228c1abb1fd29502109fd452ceac0bfe139cd
jbroman <jbroman@chromium.org>
2017-05-15 12:36:49 -0700

    Keep materialized ArrayBuffer and ImageBitmap objects in SerializedScriptValue.
    This allows a message to be received in multiple worlds, even though the
    ArrayBufferContents can only be consumed once.
    
    BUG=623355
    
    Review-Url: https://codereview.chromium.org/2878053002
    Cr-Commit-Position: refs/heads/master@{#471868}

--- third_party/WebKit/Source/platform/wtf/typed_arrays/ArrayBufferContents.cpp
+++ third_party/WebKit/Source/platform/wtf/typed_arrays/ArrayBufferContents.cpp
@@ -27,10 +27,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "platform/wtf/typed_arrays/ArrayBufferContents.h"
 
+#include <string.h>
 #include "base/allocator/partition_allocator/partition_alloc.h"
 #include "platform/wtf/Assertions.h"
 #include "platform/wtf/allocator/Partitions.h"
-#include <string.h>
 
 namespace WTF {
 
