3266aab55f125e9d2a953ab9cfd2aed05a082e43
shend <shend@chromium.org>
2017-05-14 23:20:28 -0700

    Make ETextModify an enum class.
    This patch makes ETextModify an enum class for better type safety.
    
    BUG=684966
    
    Review-Url: https://codereview.chromium.org/2878333002
    Cr-Commit-Position: refs/heads/master@{#471681}

--- third_party/WebKit/Source/core/css/CSSPrimitiveValueMappings.h
+++ third_party/WebKit/Source/core/css/CSSPrimitiveValueMappings.h
@@ -1390,13 +1390,13 @@ template <>
 inline CSSIdentifierValue::CSSIdentifierValue(EUserModify e)
     : CSSValue(kIdentifierClass) {
   switch (e) {
-    case READ_ONLY:
+    case EUserModify::kReadOnly:
       value_id_ = CSSValueReadOnly;
       break;
-    case READ_WRITE:
+    case EUserModify::kReadWrite:
       value_id_ = CSSValueReadWrite;
       break;
-    case READ_WRITE_PLAINTEXT_ONLY:
+    case EUserModify::kReadWritePlaintextOnly:
       value_id_ = CSSValueReadWritePlaintextOnly;
       break;
   }
@@ -1406,17 +1406,17 @@ template <>
 inline EUserModify CSSIdentifierValue::ConvertTo() const {
   switch (value_id_) {
     case CSSValueReadOnly:
-      return READ_ONLY;
+      return EUserModify::kReadOnly;
     case CSSValueReadWrite:
-      return READ_WRITE;
+      return EUserModify::kReadWrite;
     case CSSValueReadWritePlaintextOnly:
-      return READ_WRITE_PLAINTEXT_ONLY;
+      return EUserModify::kReadWritePlaintextOnly;
     default:
       break;
   }
 
   NOTREACHED();
-  return READ_ONLY;
+  return EUserModify::kReadOnly;
 }
 
 template <>
