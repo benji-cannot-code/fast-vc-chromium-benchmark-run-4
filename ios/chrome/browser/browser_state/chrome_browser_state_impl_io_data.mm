9096acbe1fcd42cde56539d1c1ff7891d78df32c
fdoray <fdoray@chromium.org>
2017-05-15 09:48:49 -0700

    Use TaskScheduler instead of SequencedWorkerPool in chrome_browser_state_impl_io_data.mm.
    SequencedWorkerPool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=sdefresne@chromium.org
    
    Review-Url: https://codereview.chromium.org/2880733003
    Cr-Commit-Position: refs/heads/master@{#471799}

--- ios/chrome/browser/browser_state/chrome_browser_state_impl_io_data.mm
+++ ios/chrome/browser/browser_state/chrome_browser_state_impl_io_data.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/sequenced_task_runner.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/sequenced_worker_pool.h"
 #include "components/cookie_config/cookie_store_util.h"
 #include "components/net_log/chrome_net_log.h"
@@ -87,11 +88,10 @@ void ChromeBrowserStateImplIOData::Handle::Init(
 
   io_data_->InitializeMetricsEnabledStateOnUIThread();
 
-  base::SequencedWorkerPool* pool = web::WebThread::GetBlockingPool();
   scoped_refptr<base::SequencedTaskRunner> db_task_runner =
-      pool->GetSequencedTaskRunnerWithShutdownBehavior(
-          pool->GetSequenceToken(),
-          base::SequencedWorkerPool::SKIP_ON_SHUTDOWN);
+      base::CreateSequencedTaskRunnerWithTraits(
+          {base::MayBlock(), base::TaskPriority::BACKGROUND,
+           base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN});
 }
 
 scoped_refptr<IOSChromeURLRequestContextGetter>
@@ -261,8 +261,8 @@ void ChromeBrowserStateImplIOData::InitializeInternal(
     scoped_refptr<net::SQLiteChannelIDStore> channel_id_db =
         new net::SQLiteChannelIDStore(
             lazy_params_->channel_id_path,
-            web::WebThread::GetBlockingPool()->GetSequencedTaskRunner(
-                web::WebThread::GetBlockingPool()->GetSequenceToken()));
+            base::CreateSequencedTaskRunnerWithTraits(
+                {base::MayBlock(), base::TaskPriority::BACKGROUND}));
     channel_id_service = new net::ChannelIDService(
         new net::DefaultChannelIDStore(channel_id_db.get()));
   }
