d8a69d9d610e2924c8ab870185a4565e99c42e7c
yusukes <yusukes@chromium.org>
2017-05-15 10:48:07 -0700

    Stop special-casing ArcHome
    Because of ag/2248371, ArcHome is now protected by default.
    
    BUG=None
    TEST=try
    TEST=cheets_LowMemoryKill (with CL:*373408) passes
    
    Review-Url: https://codereview.chromium.org/2878773003
    Cr-Commit-Position: refs/heads/master@{#471820}

--- chrome/browser/chromeos/arc/process/arc_process.cc
+++ chrome/browser/chromeos/arc/process/arc_process.cc
@@ -9,15 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace arc {
 
-namespace {
-
-// A special process on Android side which serves as a dummy "focused" app
-// when the focused window is a Chrome side window (i.e., all Android
-// processes are running in the background). We don't want to kill it anyway.
-constexpr char kArcHomeProcess[] = "org.chromium.arc.home";
-
-}  // namespace
-
 ArcProcess::ArcProcess(base::ProcessId nspid,
                        base::ProcessId pid,
                        const std::string& process_name,
@@ -45,16 +36,14 @@ ArcProcess::ArcProcess(ArcProcess&& other) = default;
 ArcProcess& ArcProcess::operator=(ArcProcess&& other) = default;
 
 bool ArcProcess::IsImportant() const {
-  return process_state() <= mojom::ProcessState::IMPORTANT_FOREGROUND ||
-         process_name() == kArcHomeProcess;
+  return process_state() <= mojom::ProcessState::IMPORTANT_FOREGROUND;
 }
 
 bool ArcProcess::IsKernelKillable() const {
   // Protect PERSISTENT, PERSISTENT_UI, and our HOME processes since they should
   // never be killed even by the kernel. Returning false for them allows their
   // OOM adjustment scores to remain negative.
-  return process_state() > arc::mojom::ProcessState::PERSISTENT_UI &&
-         process_name() != kArcHomeProcess;
+  return process_state() > arc::mojom::ProcessState::PERSISTENT_UI;
 }
 
 }  // namespace arc
