fb98c9ec87748baa721f1f06feb864352961ee44
anthonyvd <anthonyvd@chromium.org>
2017-05-15 13:46:11 -0700

    [Web Payments] Add "pencil" edit button to lists.
    BUG=719478
    
    Review-Url: https://codereview.chromium.org/2872623002
    Cr-Commit-Position: refs/heads/master@{#471890}

--- chrome/browser/ui/views/payments/payment_request_item_list_unittest.cc
+++ chrome/browser/ui/views/payments/payment_request_item_list_unittest.cc
@@ -20,7 +20,11 @@ namespace {
 class TestListItem : public PaymentRequestItemList::Item {
  public:
   TestListItem(PaymentRequestItemList* list, bool selected)
-      : PaymentRequestItemList::Item(nullptr, nullptr, list, selected),
+      : PaymentRequestItemList::Item(nullptr,
+                                     nullptr,
+                                     list,
+                                     selected,
+                                     /*show_edit_button=*/false),
         selected_state_changed_calls_count_(0) {}
 
   int selected_state_changed_calls_count() {
@@ -37,6 +41,8 @@ class TestListItem : public PaymentRequestItemList::Item {
   bool CanBeSelected() override { return true; }
 
   void PerformSelectionFallback() override {}
+
+  void EditButtonPressed() override {}
 
   void SelectedStateChanged() override {
     ++selected_state_changed_calls_count_;
