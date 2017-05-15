1428937d115075b91693d03e38454716f6e2fc83
fgorski <fgorski@chromium.org>
2017-05-15 15:15:20 -0700

    [Android] Adding scheduling through GcmNetworkManager
    This patch:
    * Adds ability to schedule using GcmNetworkManager (with junit tests).
    * Adds code to covert from task to TaskParams (GCM)
    * Adds GcmTaskService implementation with a waiter to handle tasks
      taking time to execute (with junit)
    * Adds ability to store tasks in prefs for upgrade scenario (+junit)
    
    BUG=710630
    R=nyquist@chromium.org
    
    Review-Url: https://codereview.chromium.org/2779753002
    Cr-Commit-Position: refs/heads/master@{#471919}

--- components/background_task_scheduler/android/java/src/org/chromium/components/background_task_scheduler/BackgroundTaskScheduler.java
+++ components/background_task_scheduler/android/java/src/org/chromium/components/background_task_scheduler/BackgroundTaskScheduler.java
@@ -76,6 +76,7 @@ public class BackgroundTaskScheduler {
      */
     public boolean schedule(Context context, TaskInfo taskInfo) {
         ThreadUtils.assertOnUiThread();
+        BackgroundTaskSchedulerPrefs.addScheduledTask(taskInfo);
         return mSchedulerDelegate.schedule(context, taskInfo);
     }
 
@@ -87,6 +88,7 @@ public class BackgroundTaskScheduler {
      */
     public void cancel(Context context, int taskId) {
         ThreadUtils.assertOnUiThread();
+        BackgroundTaskSchedulerPrefs.removeScheduledTask(taskId);
         mSchedulerDelegate.cancel(context, taskId);
     }
 }
---
+++ components/background_task_scheduler/android/junit/src/org/chromium/components/background_task_scheduler/TestBackgroundTask.java
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.background_task_scheduler;
+
+import android.content.Context;
+
+/** Dummy implementation of a background task used for testing. */
+class TestBackgroundTask implements BackgroundTask {
+    public TestBackgroundTask() {}
+
+    @Override
+    public boolean onStartTask(
+            Context context, TaskParameters taskParameters, TaskFinishedCallback callback) {
+        return false;
+    }
+
+    @Override
+    public boolean onStopTask(Context context, TaskParameters taskParameters) {
+        return false;
+    }
+}
