b0a222da605bfc156daaf2a4ed2414d64e8d7319
fdoray <fdoray@chromium.org>
2017-05-15 09:46:35 -0700

    Use TaskScheduler instead of SequencedWorkerPool in chrome_browser_state_removal_controller.mm.
    SequencedWorkerPool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=sdefresne@chromium.org
    
    Review-Url: https://codereview.chromium.org/2878763003
    Cr-Commit-Position: refs/heads/master@{#471796}

--- ios/chrome/browser/browser_state/chrome_browser_state_removal_controller.mm
+++ ios/chrome/browser/browser_state/chrome_browser_state_removal_controller.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/mac/foundation_util.h"
 #include "base/strings/sys_string_conversions.h"
+#include "base/task_scheduler/post_task.h"
 #include "components/prefs/pref_service.h"
 #include "google_apis/gaia/gaia_auth_util.h"
 #include "ios/chrome/browser/application_context.h"
@@ -24,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
 #import "ios/public/provider/chrome/browser/signin/chrome_identity.h"
 #include "ios/public/provider/chrome/browser/signin/chrome_identity_service.h"
-#include "ios/web/public/web_thread.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -144,8 +144,9 @@ void ChromeBrowserStateRemovalController::RemoveBrowserStatesIfNecessary() {
 
   if (is_removing_browser_states) {
     SetHasBrowserStateBeenRemoved(true);
-    web::WebThread::PostBlockingPoolTask(
-        FROM_HERE, base::Bind(&NukeBrowserStates, browser_states_to_nuke));
+    base::PostTaskWithTraits(
+        FROM_HERE, {base::MayBlock(), base::TaskPriority::BACKGROUND},
+        base::BindOnce(&NukeBrowserStates, browser_states_to_nuke));
   }
 }
 
