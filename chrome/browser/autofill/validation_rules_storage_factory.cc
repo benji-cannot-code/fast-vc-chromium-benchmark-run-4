0bd1b509fec419698036e9e979b7b514eb9c919b
fdoray <fdoray@chromium.org>
2017-05-15 11:38:17 -0700

    Remove JsonPrefStore::GetTaskRunnerForFile() from validation_rules_storage_factory.cc.
    No explicit SequencedTaskRunner should be provided to the constructor
    of JsonPrefStore unless external tasks need to run on the same
    sequence as JsonPrefStore tasks.
    
    BUG=667892
    
    Review-Url: https://codereview.chromium.org/2875943004
    Cr-Commit-Position: refs/heads/master@{#471842}

--- chrome/browser/autofill/validation_rules_storage_factory.cc
+++ chrome/browser/autofill/validation_rules_storage_factory.cc
@@ -7,11 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/path_service.h"
-#include "base/threading/sequenced_worker_pool.h"
 #include "chrome/common/chrome_paths.h"
 #include "components/prefs/json_pref_store.h"
 #include "components/prefs/pref_filter.h"
-#include "content/public/browser/browser_thread.h"
 #include "third_party/libaddressinput/chromium/chrome_storage_impl.h"
 
 namespace autofill {
@@ -31,15 +29,8 @@ ValidationRulesStorageFactory::ValidationRulesStorageFactory() {
   bool success = PathService::Get(chrome::DIR_USER_DATA, &user_data_dir);
   DCHECK(success);
 
-  base::FilePath cache =
-      user_data_dir.Append(FILE_PATH_LITERAL("Address Validation Rules"));
-
-  scoped_refptr<base::SequencedTaskRunner> task_runner =
-      JsonPrefStore::GetTaskRunnerForFile(
-          cache, content::BrowserThread::GetBlockingPool());
-
-  json_pref_store_ = new JsonPrefStore(cache, task_runner.get(),
-                                       std::unique_ptr<PrefFilter>());
+  json_pref_store_ = new JsonPrefStore(
+      user_data_dir.Append(FILE_PATH_LITERAL("Address Validation Rules")));
   json_pref_store_->ReadPrefsAsync(NULL);
 }
 
