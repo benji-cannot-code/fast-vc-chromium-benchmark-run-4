e1ee92f624e27c2e05a78dde679960fa4c3063dd
fdoray <fdoray@chromium.org>
2017-05-15 10:08:46 -0700

    Use TaskScheduler instead of SequencedWorkerPool in drive_service_bridge.cc.
    SequencedWorkerPool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=tapted@chromium.org
    
    Review-Url: https://codereview.chromium.org/2878723005
    Cr-Commit-Position: refs/heads/master@{#471810}

--- chrome/browser/apps/drive/drive_service_bridge.cc
+++ chrome/browser/apps/drive/drive_service_bridge.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/macros.h"
-#include "base/threading/sequenced_worker_pool.h"
+#include "base/task_scheduler/post_task.h"
 #include "chrome/browser/drive/drive_notification_manager_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/drive/service/drive_api_service.h"
 #include "components/signin/core/browser/profile_oauth2_token_service.h"
 #include "components/signin/core/browser/signin_manager.h"
-#include "content/public/browser/browser_thread.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 
 namespace {
@@ -73,12 +72,10 @@ DriveServiceBridgeImpl::~DriveServiceBridgeImpl() {
 }
 
 void DriveServiceBridgeImpl::Initialize() {
-  scoped_refptr<base::SequencedWorkerPool> worker_pool(
-      content::BrowserThread::GetBlockingPool());
   scoped_refptr<base::SequencedTaskRunner> drive_task_runner(
-      worker_pool->GetSequencedTaskRunnerWithShutdownBehavior(
-          worker_pool->GetSequenceToken(),
-          base::SequencedWorkerPool::SKIP_ON_SHUTDOWN));
+      base::CreateSequencedTaskRunnerWithTraits(
+          {base::MayBlock(), base::TaskPriority::BACKGROUND,
+           base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN}));
 
   ProfileOAuth2TokenService* token_service =
       ProfileOAuth2TokenServiceFactory::GetForProfile(profile_);
