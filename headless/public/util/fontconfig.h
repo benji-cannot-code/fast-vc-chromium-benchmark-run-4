b433d38506968fcd5f0222c369c67abfbdae805a
altimin <altimin@chromium.org>
2017-05-15 09:36:33 -0700

    [headless] Enable headless embedders to specify fonts.
    R=skyostil@chromium.org
    
    Review-Url: https://codereview.chromium.org/2877813002
    Cr-Commit-Position: refs/heads/master@{#471790}

---
+++ headless/public/util/fontconfig.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef HEADLESS_PUBLIC_UTIL_FONTCONFIG_H_
+#define HEADLESS_PUBLIC_UTIL_FONTCONFIG_H_
+
+namespace headless {
+
+// Initialize fontconfig by loading fonts from given path without following
+// symlinks. This is a wrapper around FcInit from libfreetype bundled with
+// Chromium modified to enable headless embedders to deploy in custom
+// environments.
+void InitFonts(const char* font_config_path);
+
+void ReleaseFonts();
+
+}  // namespace headless
+
+#endif  // HEADLESS_PUBLIC_UTIL_FONTCONFIG_H_
