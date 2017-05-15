2760204a7a222df1942782a74cf7db6dcde2c2f8
nainar <nainar@chromium.org>
2017-05-14 20:43:27 -0700

    Revert of Move AppliedTextDecorationList to its own file. (patchset #2 id:20001 of https://codereview.chromium.org/2884553002/ )
    Reason for revert:
    Reverting since the BUILD file wasn't updated. Author is aware
    
    Original issue's description:
    > Move AppliedTextDecorationList to its own file.
    >
    > When we generate external types like Length in ComputedStyle, we specify
    > the path to the type (e.g. "platform/Length"). We then deduce the name
    > of the type from the path ("Length"). This means that the type must be
    > stored in a file with the same name.
    >
    > This patch moves AppliedTextDecorationList to its own file so that it
    > can be generated.
    >
    > BUG=628043
    >
    > Review-Url: https://codereview.chromium.org/2884553002
    > Cr-Commit-Position: refs/heads/master@{#471653}
    > Committed: https://chromium.googlesource.com/chromium/src/+/762e2d87addf15f32ac5bda17b1ad9a8fb25dee2
    
    TBR=suzyh@chromium.org,shend@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=628043
    
    Review-Url: https://codereview.chromium.org/2880963003
    Cr-Commit-Position: refs/heads/master@{#471656}

--- third_party/WebKit/Source/core/style/StyleRareInheritedData.h
+++ third_party/WebKit/Source/core/style/StyleRareInheritedData.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/CoreExport.h"
 #include "core/css/StyleAutoColor.h"
 #include "core/css/StyleColor.h"
-#include "core/style/AppliedTextDecorationList.h"
 #include "core/style/TextSizeAdjust.h"
 #include "platform/Length.h"
 #include "platform/graphics/Color.h"
@@ -42,12 +41,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class AppliedTextDecoration;
 class CursorData;
 class QuotesData;
 class ShadowList;
 class StyleImage;
 class StyleInheritedVariables;
 
+typedef RefVector<AppliedTextDecoration> AppliedTextDecorationList;
 typedef HeapVector<CursorData> CursorList;
 
 // This struct is for rarely used inherited CSS3, CSS2, and WebKit-specific
