5dc69665b1239e7f4cc60c8e37edecb2a4375763
csharrison <csharrison@chromium.org>
2017-05-16 07:05:34 -0700

    NavigationSimulator: add support for GlobalRequestIds
    This adds support for both the PlzNavigate and non-PlzNavigate paths,
    and ensures that TestNavigationURLLoader always uses a non default one.
    
    BUG=680841,711352
    
    Review-Url: https://codereview.chromium.org/2885453003
    Cr-Commit-Position: refs/heads/master@{#472093}

--- content/test/test_navigation_url_loader.cc
+++ content/test/test_navigation_url_loader.cc
@@ -10,8 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/loader/navigation_url_loader_delegate.h"
 #include "content/public/browser/global_request_id.h"
 #include "content/public/browser/navigation_data.h"
+#include "content/public/browser/render_process_host.h"
 #include "content/public/browser/ssl_status.h"
 #include "content/public/browser/stream_handle.h"
+#include "content/public/browser/web_contents.h"
+#include "content/public/common/browser_side_navigation_policy.h"
 #include "content/public/common/resource_response.h"
 #include "net/url_request/redirect_info.h"
 
@@ -23,7 +26,9 @@ TestNavigationURLLoader::TestNavigationURLLoader(
     : request_info_(std::move(request_info)),
       delegate_(delegate),
       redirect_count_(0),
-      response_proceeded_(false) {}
+      response_proceeded_(false) {
+  DCHECK(IsBrowserSideNavigationEnabled());
+}
 
 void TestNavigationURLLoader::FollowRedirect() {
   redirect_count_++;
@@ -57,9 +62,17 @@ void TestNavigationURLLoader::CallOnResponseStarted(
     const scoped_refptr<ResourceResponse>& response,
     std::unique_ptr<StreamHandle> body,
     std::unique_ptr<NavigationData> navigation_data) {
+  // Start the request_ids at 1000 to avoid collisions with request ids from
+  // network resources (it should be rare to compare these in unit tests).
+  static int request_id = 1000;
+  int child_id =
+      WebContents::FromFrameTreeNodeId(request_info_->frame_tree_node_id)
+          ->GetRenderProcessHost()
+          ->GetID();
+  GlobalRequestID global_id(child_id, ++request_id);
   delegate_->OnResponseStarted(
       response, std::move(body), mojo::ScopedDataPipeConsumerHandle(),
-      SSLStatus(), std::move(navigation_data), GlobalRequestID(), false, false);
+      SSLStatus(), std::move(navigation_data), global_id, false, false);
 }
 
 TestNavigationURLLoader::~TestNavigationURLLoader() {}
