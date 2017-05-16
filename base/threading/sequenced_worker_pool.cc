cab2f0965a0dc73d06e41e47ca1a9a168dc5be9f
fdoray <fdoray@chromium.org>
2017-05-16 12:47:21 -0700

    Remove SequencedWorkerPool::PostDelayedWorkerTask().
    There are no more callers of this method.
    
    BUG=667892
    
    Review-Url: https://codereview.chromium.org/2882523002
    Cr-Commit-Position: refs/heads/master@{#472194}

--- base/threading/sequenced_worker_pool.cc
+++ base/threading/sequenced_worker_pool.cc
@@ -174,7 +174,7 @@ bool SequencedWorkerPoolTaskRunner::PostDelayedTask(
     return pool_->PostWorkerTaskWithShutdownBehavior(from_here, std::move(task),
                                                      shutdown_behavior_);
   }
-  return pool_->PostDelayedWorkerTask(from_here, std::move(task), delay);
+  return pool_->PostDelayedTask(from_here, std::move(task), delay);
 }
 
 bool SequencedWorkerPoolTaskRunner::RunsTasksInCurrentSequence() const {
@@ -1553,16 +1553,6 @@ bool SequencedWorkerPool::PostWorkerTask(
                           std::move(task), TimeDelta());
 }
 
-bool SequencedWorkerPool::PostDelayedWorkerTask(
-    const tracked_objects::Location& from_here,
-    OnceClosure task,
-    TimeDelta delay) {
-  WorkerShutdown shutdown_behavior =
-      delay.is_zero() ? BLOCK_SHUTDOWN : SKIP_ON_SHUTDOWN;
-  return inner_->PostTask(NULL, SequenceToken(), shutdown_behavior, from_here,
-                          std::move(task), delay);
-}
-
 bool SequencedWorkerPool::PostWorkerTaskWithShutdownBehavior(
     const tracked_objects::Location& from_here,
     OnceClosure task,
@@ -1612,7 +1602,10 @@ bool SequencedWorkerPool::PostDelayedTask(
     const tracked_objects::Location& from_here,
     OnceClosure task,
     TimeDelta delay) {
-  return PostDelayedWorkerTask(from_here, std::move(task), delay);
+  WorkerShutdown shutdown_behavior =
+      delay.is_zero() ? BLOCK_SHUTDOWN : SKIP_ON_SHUTDOWN;
+  return inner_->PostTask(nullptr, SequenceToken(), shutdown_behavior,
+                          from_here, std::move(task), delay);
 }
 
 bool SequencedWorkerPool::RunsTasksInCurrentSequence() const {
