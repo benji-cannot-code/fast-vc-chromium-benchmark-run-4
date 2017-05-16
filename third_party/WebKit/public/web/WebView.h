17dc1b235003d451dea2d0ce176b69b14f3ab3a6
Daniel Cheng <dcheng@chromium.org>
2017-05-16 08:07:02 -0700

    Remove unused function WebView::CompositedDisplayList.
    Change-Id: I76afae213d8dfbaa61c76b33384b083cd5d3c4bc
    Reviewed-on: https://chromium-review.googlesource.com/506464
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#472263}

--- third_party/WebKit/public/web/WebView.h
+++ third_party/WebKit/public/web/WebView.h
@@ -48,7 +48,6 @@ class ICCProfile;
 namespace blink {
 
 class WebAXObject;
-class WebCompositedDisplayList;
 class WebCredentialManagerClient;
 class WebFrame;
 class WebHitTestResult;
@@ -462,10 +461,6 @@ class WebView : protected WebWidget {
   // Sets the visibility of the WebView.
   virtual void SetVisibilityState(WebPageVisibilityState visibility_state,
                                   bool is_initial_state) {}
-
-  // Graphics -------------------------------------------------------------
-
-  virtual WebCompositedDisplayList* CompositedDisplayList() { return nullptr; }
 
   // PageOverlay ----------------------------------------------------------
 
