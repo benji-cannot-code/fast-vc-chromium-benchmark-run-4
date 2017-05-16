73ba93d1e4c6fc5fd41c8f567e82181181168375
Ahmed Fakhry <afakhry@google.com>
2017-05-15 19:22:56 -0700

    Revert "[Memory-UMA] Implement basic working prototype."
    Manual revet due to too long file.
    
    Reason for revert: Caused ProcessMemoryMetricsEmitterTest.FetchDuringTrace
    failure on the Linux ChromiumOS Tests builder.
    BUG=722673,703184
    TBR=rkaplow@chromium.org,avi@chrmium.org,erikchen@chromium.org,dcheng@chromium.org,primiano@chromium.org
    
    This reverts commit 18ea873dd28c6b2f49b529e57c55898f5df1b4d2.
    
    Review-Url: https://codereview.chromium.org/2884143002 .
    Cr-Commit-Position: refs/heads/master@{#471985}

--- services/resource_coordinator/memory/coordinator/coordinator_impl.h
+++ services/resource_coordinator/memory/coordinator/coordinator_impl.h
@@ -15,13 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/memory_dump_request_args.h"
 #include "mojo/public/cpp/bindings/binding.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
-#include "services/resource_coordinator/memory/coordinator/process_map.h"
 #include "services/resource_coordinator/public/cpp/memory/coordinator.h"
 #include "services/resource_coordinator/public/interfaces/memory/memory_instrumentation.mojom.h"
-
-namespace service_manager {
-class Connector;
-}
 
 namespace memory_instrumentation {
 
@@ -30,8 +25,7 @@ class CoordinatorImpl : public Coordinator, public mojom::Coordinator {
   // The getter of the unique instance.
   static CoordinatorImpl* GetInstance();
 
-  CoordinatorImpl(bool initialize_memory_dump_manager,
-                  service_manager::Connector* connector);
+  explicit CoordinatorImpl(bool initialize_memory_dump_manager);
 
   // Coordinator
   void BindCoordinatorRequest(
@@ -41,11 +35,6 @@ class CoordinatorImpl : public Coordinator, public mojom::Coordinator {
   bool initialize_memory_dump_manager() const {
     return initialize_memory_dump_manager_;
   }
-
- protected:
-  // virtual for testing.
-  virtual service_manager::Identity GetDispatchContext() const;
-  ~CoordinatorImpl() override;
 
  private:
   friend std::default_delete<CoordinatorImpl>;  // For testing
@@ -59,12 +48,10 @@ class CoordinatorImpl : public Coordinator, public mojom::Coordinator {
     const RequestGlobalMemoryDumpCallback callback;
 
     // Collects the data received from OnProcessMemoryDumpResponse().
-    std::vector<std::pair<base::ProcessId, mojom::ProcessMemoryDumpPtr>>
-        process_memory_dumps;
+    std::vector<mojom::ProcessMemoryDumpPtr> process_memory_dumps;
   };
 
-  using ProcessLocalDumpManagerEntry =
-      std::pair<mojom::ProcessLocalDumpManagerPtr, service_manager::Identity>;
+  ~CoordinatorImpl() override;
 
   // mojom::Coordinator
   void RegisterProcessLocalDumpManager(
@@ -91,10 +78,10 @@ class CoordinatorImpl : public Coordinator, public mojom::Coordinator {
   void PerformNextQueuedGlobalMemoryDump();
   void FinalizeGlobalMemoryDumpIfAllManagersReplied();
 
-  mojo::BindingSet<mojom::Coordinator, service_manager::Identity> bindings_;
+  mojo::BindingSet<mojom::Coordinator> bindings_;
 
   // Registered ProcessLocalDumpManagers.
-  std::map<mojom::ProcessLocalDumpManager*, ProcessLocalDumpManagerEntry>
+  std::map<mojom::ProcessLocalDumpManager*, mojom::ProcessLocalDumpManagerPtr>
       process_managers_;
 
   // Pending process managers for RequestGlobalMemoryDump.
@@ -105,8 +92,6 @@ class CoordinatorImpl : public Coordinator, public mojom::Coordinator {
   const bool initialize_memory_dump_manager_;
 
   base::ThreadChecker thread_checker_;
-
-  std::unique_ptr<ProcessMap> process_map_;
 
   DISALLOW_COPY_AND_ASSIGN(CoordinatorImpl);
 };
