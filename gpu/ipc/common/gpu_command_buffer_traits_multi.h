efd581143f832d77e414d17ad7ed882a005b3520
Antoine Labour <piman@chromium.org>
2017-05-16 14:23:42 -0700

    Make SetGetBuffer async
    This removes one sync IPC at initialization time, and after we release the command buffer.
    We make sure we handle the possibility of having more than one SetGetBuffer in flight by
    counting them on client side and service side, making sure we only ever compare |get| values
    for the same buffer.
    
    Bug: 484375
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ife91dd5846e9d82f03e5bfe5d022a06284389756
    Reviewed-on: https://chromium-review.googlesource.com/502016
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#472312}

--- gpu/ipc/common/gpu_command_buffer_traits_multi.h
+++ gpu/ipc/common/gpu_command_buffer_traits_multi.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Multiply-included message file, hence no include guard here.
 
 #include "gpu/command_buffer/common/capabilities.h"
+#include "gpu/command_buffer/common/command_buffer.h"
 #include "gpu/command_buffer/common/constants.h"
 #include "gpu/command_buffer/common/gles2_cmd_utils.h"
 #include "gpu/gpu_export.h"
@@ -18,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IPC_MESSAGE_EXPORT GPU_EXPORT
 
 IPC_ENUM_TRAITS_MAX_VALUE(gpu::error::Error, gpu::error::kErrorLast)
+IPC_ENUM_TRAITS_MAX_VALUE(gpu::error::ContextLostReason,
+                          gpu::error::kContextLostReasonLast)
 IPC_ENUM_TRAITS_MIN_MAX_VALUE(
     gpu::CommandBufferNamespace,
     gpu::CommandBufferNamespace::INVALID,
@@ -136,6 +139,16 @@ IPC_STRUCT_TRAITS_BEGIN(gpu::Capabilities)
 
   IPC_STRUCT_TRAITS_MEMBER(major_version)
   IPC_STRUCT_TRAITS_MEMBER(minor_version)
+IPC_STRUCT_TRAITS_END()
+
+IPC_STRUCT_TRAITS_BEGIN(gpu::CommandBuffer::State)
+  IPC_STRUCT_TRAITS_MEMBER(get_offset)
+  IPC_STRUCT_TRAITS_MEMBER(token)
+  IPC_STRUCT_TRAITS_MEMBER(release_count)
+  IPC_STRUCT_TRAITS_MEMBER(error)
+  IPC_STRUCT_TRAITS_MEMBER(context_lost_reason)
+  IPC_STRUCT_TRAITS_MEMBER(generation)
+  IPC_STRUCT_TRAITS_MEMBER(set_get_buffer_count)
 IPC_STRUCT_TRAITS_END()
 
 IPC_STRUCT_TRAITS_BEGIN(gpu::gles2::ContextCreationAttribHelper)
