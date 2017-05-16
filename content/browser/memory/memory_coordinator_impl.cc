b7603064893eee0ea422c5a86c8a71f30f0b3ab8
hajimehoshi <hajimehoshi@chromium.org>
2017-05-16 08:48:29 -0700

    Refactoring: Use a static variable for singleton of MemoryCoordinatorImpl
    As C++11's static variables are thread safe, let's use a static variable
    for singleton like FileDescriptorStore::GetInstance().
    
    BUG=n/a
    TEST=n/a
    
    Review-Url: https://codereview.chromium.org/2882413002
    Cr-Commit-Position: refs/heads/master@{#472115}

--- content/browser/memory/memory_coordinator_impl.cc
+++ content/browser/memory/memory_coordinator_impl.cc
@@ -113,16 +113,6 @@ void MemoryCoordinatorHandleImpl::AddChild(
   coordinator_->OnChildAdded(render_process_id_);
 }
 
-// SingletonTraits for MemoryCoordinator. Returns MemoryCoordinatorImpl
-// as an actual instance.
-struct MemoryCoordinatorImplSingletonTraits
-    : public base::LeakySingletonTraits<MemoryCoordinatorImpl> {
-  static MemoryCoordinatorImpl* New() {
-    return new MemoryCoordinatorImpl(base::ThreadTaskRunnerHandle::Get(),
-                                     CreateMemoryMonitor());
-  }
-};
-
 // static
 MemoryCoordinator* MemoryCoordinator::GetInstance() {
   return MemoryCoordinatorImpl::GetInstance();
@@ -132,8 +122,9 @@ MemoryCoordinator* MemoryCoordinator::GetInstance() {
 MemoryCoordinatorImpl* MemoryCoordinatorImpl::GetInstance() {
   if (!base::FeatureList::IsEnabled(features::kMemoryCoordinator))
     return nullptr;
-  return base::Singleton<MemoryCoordinatorImpl,
-                         MemoryCoordinatorImplSingletonTraits>::get();
+  static MemoryCoordinatorImpl* instance = new MemoryCoordinatorImpl(
+      base::ThreadTaskRunnerHandle::Get(), CreateMemoryMonitor());
+  return instance;
 }
 
 MemoryCoordinatorImpl::MemoryCoordinatorImpl(
--- content/browser/memory/memory_coordinator_impl.h
+++ content/browser/memory/memory_coordinator_impl.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/memory_coordinator_client.h"
 #include "base/memory/memory_coordinator_proxy.h"
 #include "base/memory/memory_pressure_monitor.h"
-#include "base/memory/singleton.h"
 #include "base/single_thread_task_runner.h"
 #include "base/threading/non_thread_safe.h"
 #include "base/time/tick_clock.h"
@@ -32,7 +31,6 @@ class MemoryCoordinatorHandleImpl;
 class MemoryCoordinatorImplTest;
 class MemoryMonitor;
 class RenderProcessHost;
-struct MemoryCoordinatorSingletonTraits;
 
 using MemoryState = base::MemoryState;
 
@@ -205,7 +203,6 @@ class CONTENT_EXPORT MemoryCoordinatorImpl : public base::MemoryCoordinator,
   FRIEND_TEST_ALL_PREFIXES(MemoryCoordinatorImplTest, OnWarningCondition);
   FRIEND_TEST_ALL_PREFIXES(MemoryCoordinatorImplTest, OnCriticalCondition);
 
-  friend struct MemoryCoordinatorSingletonTraits;
   friend class MemoryCoordinatorHandleImpl;
 
   // Called when ChildMemoryCoordinator calls AddChild().
