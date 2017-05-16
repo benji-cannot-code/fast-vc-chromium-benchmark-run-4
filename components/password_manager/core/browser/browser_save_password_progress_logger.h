1baf4e743529797e881a9fe35c43059450cbafb3
dvadym <dvadym@chromium.org>
2017-05-16 06:24:12 -0700

    Adding logging password form submission Indicator event.
    On CL https://codereview.chromium.org/2859243004/ UMA for password form submission event was added. This CL adds this information to chrome://password-manager-internals
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2884913003
    Cr-Commit-Position: refs/heads/master@{#472078}

--- components/password_manager/core/browser/browser_save_password_progress_logger.h
+++ components/password_manager/core/browser/browser_save_password_progress_logger.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/macros.h"
+#include "components/autofill/core/common/password_form.h"
 #include "components/autofill/core/common/save_password_progress_logger.h"
 #include "url/gurl.h"
 
@@ -45,6 +46,10 @@ class BrowserSavePasswordProgressLogger
   // Browser-specific addition to the base class' Log* methods. The input is
   // passed to SendLog for display.
   void LogString(StringID label, const std::string& s);
+
+  // Log a password successful submission event.
+  void LogSuccessfulSubmissionIndicatorEvent(
+      autofill::PasswordForm::SubmissionIndicatorEvent event);
 
  protected:
   // autofill::SavePasswordProgressLogger:
