dac73774bc896cbd8ad7e13273c5d37b1c24e745
reed <reed@google.com>
2017-05-16 08:34:17 -0700

    SkPictureRecorder is changing (including less), so IWYU
    pending skia cl: https://skia-review.googlesource.com/c/17031/
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2888563002
    Cr-Commit-Position: refs/heads/master@{#472110}

--- skia/ext/analysis_canvas_unittest.cc
+++ skia/ext/analysis_canvas_unittest.cc
@@ -3,13 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "skia/ext/analysis_canvas.h"
 #include "base/compiler_specific.h"
 #include "base/macros.h"
-#include "skia/ext/analysis_canvas.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/skia/include/core/SkPath.h"
 #include "third_party/skia/include/core/SkPicture.h"
 #include "third_party/skia/include/core/SkPictureRecorder.h"
-#include "third_party/skia/include/core/SkRefCnt.h"
+#include "third_party/skia/include/core/SkRRect.h"
+#include "third_party/skia/include/core/SkRegion.h"
 #include "third_party/skia/include/core/SkShader.h"
 #include "third_party/skia/include/effects/SkOffsetImageFilter.h"
 
