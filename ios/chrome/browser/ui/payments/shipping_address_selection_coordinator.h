2dccdd284c40992e695e0f0cfe4fbfb6c069c010
mahmadi <mahmadi@chromium.org>
2017-05-16 07:08:26 -0700

    [Payment Request] Address edit view controller (Part 1)
    This CL introduces an address edit view controller, an empty mediator,
    and an empty coordinator that sets up the view controller and the
    mediator and handles the navigation.
    The address edit form can be launched from the shipping address selection
    view. Saving the form reaches a DCHECK at this moment.
    
    BUG=602666
    
    Review-Url: https://codereview.chromium.org/2871313004
    Cr-Commit-Position: refs/heads/master@{#472095}

--- ios/chrome/browser/ui/payments/shipping_address_selection_coordinator.h
+++ ios/chrome/browser/ui/payments/shipping_address_selection_coordinator.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #import "ios/chrome/browser/chrome_coordinator.h"
+#import "ios/chrome/browser/ui/payments/address_edit_coordinator.h"
 #import "ios/chrome/browser/ui/payments/payment_request_selector_view_controller.h"
 
 class PaymentRequest;
@@ -40,7 +41,8 @@ class AutofillProfile;
 // selection view controller. This view controller will be presented by the view
 // controller provided in the initializer.
 @interface ShippingAddressSelectionCoordinator
-    : ChromeCoordinator<PaymentRequestSelectorViewControllerDelegate>
+    : ChromeCoordinator<PaymentRequestSelectorViewControllerDelegate,
+                        AddressEditCoordinatorDelegate>
 
 // The PaymentRequest object having a copy of web::PaymentRequest as provided by
 // the page invoking the Payment Request API. This pointer is not owned by this
