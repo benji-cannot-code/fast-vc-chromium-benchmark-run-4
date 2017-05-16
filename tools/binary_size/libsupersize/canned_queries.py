ba78122304f7fd1d5d8111430c40a235f7934cd9
agrieve <agrieve@chromium.org>
2017-05-16 11:11:19 -0700

    supersize: Move [clone] from name -> flags & rewrite Cluster()
    SizeInfo.symbols is now Clustered() by default.
    SizeInfo.raw_symbols is now a SymbolGroup rather than a list.
    
    Clustered() now uses GroupedBy() and cluster_symbols.py is no more.
    
    BUG=681694
    
    Review-Url: https://codereview.chromium.org/2885073002
    Cr-Commit-Position: refs/heads/master@{#472162}

--- tools/binary_size/libsupersize/canned_queries.py
+++ tools/binary_size/libsupersize/canned_queries.py
@@ -145,4 +145,4 @@ class CannedQueries(object):
     """Lists C++ templates grouped by name."""
     symbols = self._SymbolsArg(symbols)
     # TODO(agrieve): Might be nice to recursively GroupedByName() on these.
-    return symbols.WhereIsTemplate().GroupedByName(depth).Clustered().Sorted()
+    return symbols.WhereIsTemplate().GroupedByName(depth).Sorted()
