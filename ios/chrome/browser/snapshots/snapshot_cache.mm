e6c95b7bed23ba4295469680aeb4f28fb3bb6f79
kolos <kolos@chromium.org>
2017-05-16 05:20:15 -0700

    Revert of Use TaskScheduler instead of SequencedWorkerPool in snapshot_cache.mm. (patchset #2 id:20001 of https://codereview.chromium.org/2881873002/ )
    Reason for revert:
    Probably this CL causes many failures of iOS bots. For example, https://build.chromium.org/p/chromium.mac/builders/ios-simulator/builds/17573
    
    Original issue's description:
    > Use TaskScheduler instead of SequencedWorkerPool in snapshot_cache.mm.
    >
    > SequencedWorkerPool is being deprecated in favor of TaskScheduler.
    >
    > BUG=667892
    > R=jif@chromium.org
    >
    > Review-Url: https://codereview.chromium.org/2881873002
    > Cr-Commit-Position: refs/heads/master@{#472052}
    > Committed: https://chromium.googlesource.com/chromium/src/+/aa5299ae23a57ca5dce00a11da3ac8e73849351f
    
    TBR=jif@chromium.org,fdoray@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=667892
    
    Review-Url: https://codereview.chromium.org/2889553002
    Cr-Commit-Position: refs/heads/master@{#472065}

--- ios/chrome/browser/snapshots/snapshot_cache.mm
+++ ios/chrome/browser/snapshots/snapshot_cache.mm
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/scoped_nsobject.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/task_runner_util.h"
-#include "base/task_scheduler/post_task.h"
 #include "base/threading/thread_restrictions.h"
 #include "ios/chrome/browser/experimental_flags.h"
 #import "ios/chrome/browser/snapshots/lru_cache.h"
@@ -576,10 +575,9 @@ void ConvertAndSaveGreyImage(
     }
   }
 
-  base::PostTaskWithTraits(
-      FROM_HERE, {base::MayBlock(), base::TaskPriority::USER_VISIBLE},
-      base::BindOnce(&ConvertAndSaveGreyImage, colorImagePath, greyImagePath,
-                     backgroundingColorImage_));
+  web::WebThread::PostBlockingPoolTask(
+      FROM_HERE, base::Bind(&ConvertAndSaveGreyImage, colorImagePath,
+                            greyImagePath, backgroundingColorImage_));
 }
 
 @end
