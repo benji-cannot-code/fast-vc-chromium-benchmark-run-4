42aa54de53b80382ee1ec74b4a43c06b95f66b69
fdoray <fdoray@chromium.org>
2017-05-16 06:45:14 -0700

    Use TaskScheduler instead of SequencedWorkerPool in ios_most_visited_sites_factory.cc.
    SequencedWorkerPool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=bauerb@chromium.org
    TBR=rohitrao@chromium.org
    
    Review-Url: https://codereview.chromium.org/2884433003
    Cr-Commit-Position: refs/heads/master@{#472090}

--- ios/chrome/browser/ntp_tiles/ios_most_visited_sites_factory.cc
+++ ios/chrome/browser/ntp_tiles/ios_most_visited_sites_factory.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/ntp_tiles/ios_most_visited_sites_factory.h"
 
 #include "base/memory/ptr_util.h"
-#include "base/threading/sequenced_worker_pool.h"
+#include "base/task_scheduler/post_task.h"
 #include "components/history/core/browser/top_sites.h"
 #include "components/image_fetcher/core/image_fetcher_impl.h"
 #include "components/image_fetcher/ios/ios_image_decoder_impl.h"
@@ -19,16 +19,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/history/top_sites_factory.h"
 #include "ios/chrome/browser/ntp_tiles/ios_popular_sites_factory.h"
 #include "ios/chrome/browser/suggestions/suggestions_service_factory.h"
-#include "ios/web/public/web_thread.h"
 
 std::unique_ptr<ntp_tiles::MostVisitedSites>
 IOSMostVisitedSitesFactory::NewForBrowserState(
     ios::ChromeBrowserState* browser_state) {
   scoped_refptr<base::SequencedTaskRunner> task_runner =
-      web::WebThread::GetBlockingPool()
-          ->GetSequencedTaskRunnerWithShutdownBehavior(
-              base::SequencedWorkerPool::GetSequenceToken(),
-              base::SequencedWorkerPool::CONTINUE_ON_SHUTDOWN);
+      base::CreateSequencedTaskRunnerWithTraits(
+          {base::MayBlock(), base::TaskPriority::BACKGROUND,
+           base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN});
   return base::MakeUnique<ntp_tiles::MostVisitedSites>(
       browser_state->GetPrefs(),
       ios::TopSitesFactory::GetForBrowserState(browser_state),
