a79ed0f8b19fb235d4fac8f40f5e70858270bf43
Rouslan Solomakhin <rouslan@chromium.org>
2017-05-16 11:51:52 -0400

    Add a use counter for "visa", "amex", etc payment methods.
    Bug: 719526
    Change-Id: I6c25fd781b7aaa7df9209cc925a4c15146557bdb
    Reviewed-on: https://chromium-review.googlesource.com/506748
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#472446}

--- third_party/WebKit/Source/modules/payments/PaymentRequest.cpp
+++ third_party/WebKit/Source/modules/payments/PaymentRequest.cpp
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/events/Event.h"
 #include "core/events/EventQueue.h"
 #include "core/frame/FrameOwner.h"
+#include "core/frame/UseCounter.h"
 #include "core/html/HTMLIFrameElement.h"
 #include "core/inspector/ConsoleMessage.h"
 #include "core/inspector/ConsoleTypes.h"
@@ -129,6 +130,20 @@ struct TypeConverter<PaymentOptionsPtr, blink::PaymentOptions> {
 
 namespace blink {
 namespace {
+
+using ::payments::mojom::blink::BasicCardNetwork;
+
+const struct {
+  const BasicCardNetwork code;
+  const char* const name;
+} kBasicCardNetworks[] = {{BasicCardNetwork::AMEX, "amex"},
+                          {BasicCardNetwork::DINERS, "diners"},
+                          {BasicCardNetwork::DISCOVER, "discover"},
+                          {BasicCardNetwork::JCB, "jcb"},
+                          {BasicCardNetwork::MASTERCARD, "mastercard"},
+                          {BasicCardNetwork::MIR, "mir"},
+                          {BasicCardNetwork::UNIONPAY, "unionpay"},
+                          {BasicCardNetwork::VISA, "visa"}};
 
 // If the website does not call complete() 60 seconds after show() has been
 // resolved, then behave as if the website called complete("fail").
@@ -351,20 +366,6 @@ void SetBasicCardMethodData(const ScriptValue& input,
     return;
 
   if (basic_card.hasSupportedNetworks()) {
-    using ::payments::mojom::blink::BasicCardNetwork;
-
-    const struct {
-      const BasicCardNetwork code;
-      const char* const name;
-    } kBasicCardNetworks[] = {{BasicCardNetwork::AMEX, "amex"},
-                              {BasicCardNetwork::DINERS, "diners"},
-                              {BasicCardNetwork::DISCOVER, "discover"},
-                              {BasicCardNetwork::JCB, "jcb"},
-                              {BasicCardNetwork::MASTERCARD, "mastercard"},
-                              {BasicCardNetwork::MIR, "mir"},
-                              {BasicCardNetwork::UNIONPAY, "unionpay"},
-                              {BasicCardNetwork::VISA, "visa"}};
-
     for (const String& network : basic_card.supportedNetworks()) {
       for (size_t i = 0; i < arraysize(kBasicCardNetworks); ++i) {
         if (network == kBasicCardNetworks[i].name) {
@@ -435,6 +436,15 @@ void StringifyAndParseMethodSpecificData(
     SetBasicCardMethodData(input, output, execution_context, exception_state);
     if (exception_state.HadException())
       exception_state.ClearException();
+  }
+
+  for (size_t i = 0; i < arraysize(kBasicCardNetworks); ++i) {
+    if (supported_methods.Contains(kBasicCardNetworks[i].name)) {
+      UseCounter::Count(
+          &execution_context,
+          UseCounter::kPaymentRequestNetworkNameInSupportedMethods);
+      break;
+    }
   }
 }
 
