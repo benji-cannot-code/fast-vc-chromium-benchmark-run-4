504697fe5b00d0183a996e555c217c6506dcefde
treib <treib@chromium.org>
2017-05-16 10:09:25 -0700

    Thumbnails: Add some perf UMA
    BUG=718413
    
    Review-Url: https://codereview.chromium.org/2882183002
    Cr-Commit-Position: refs/heads/master@{#472138}

--- chrome/browser/thumbnails/thumbnail_tab_helper.h
+++ chrome/browser/thumbnails/thumbnail_tab_helper.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "base/time/time.h"
 #include "chrome/browser/thumbnails/thumbnailing_context.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
@@ -72,9 +73,13 @@ class ThumbnailTabHelper
   const bool capture_on_load_finished_;
 
   content::NotificationRegistrar registrar_;
-  scoped_refptr<thumbnails::ThumbnailingContext> thumbnailing_context_;
 
   bool load_interrupted_;
+
+  scoped_refptr<thumbnails::ThumbnailingContext> thumbnailing_context_;
+
+  base::TimeTicks copy_from_surface_start_time_;
+  base::TimeTicks process_bitmap_start_time_;
 
   base::WeakPtrFactory<ThumbnailTabHelper> weak_factory_;
 
