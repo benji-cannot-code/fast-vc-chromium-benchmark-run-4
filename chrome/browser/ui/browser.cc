820fb88659291d8e70cb2e3b0ef3c3311444703e
thestig <thestig@chromium.org>
2017-05-16 12:35:41 -0700

    Add blink::WebInputEvent::IsPinchGestureEventType().
    Review-Url: https://codereview.chromium.org/2861373004
    Cr-Commit-Position: refs/heads/master@{#472185}

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -565,7 +565,6 @@ Browser::~Browser() {
   // away so they don't try and call back to us.
   if (select_file_dialog_.get())
     select_file_dialog_->ListenerDestroyed();
-
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -1313,10 +1312,7 @@ bool Browser::PreHandleGestureEvent(content::WebContents* source,
                                     const blink::WebGestureEvent& event) {
   // Disable pinch zooming in undocked dev tools window due to poor UX.
   if (app_name() == DevToolsWindow::kDevToolsApp)
-    return event.GetType() == blink::WebGestureEvent::kGesturePinchBegin ||
-           event.GetType() == blink::WebGestureEvent::kGesturePinchUpdate ||
-           event.GetType() == blink::WebGestureEvent::kGesturePinchEnd;
-
+    return blink::WebInputEvent::IsPinchGestureEventType(event.GetType());
   return false;
 }
 
