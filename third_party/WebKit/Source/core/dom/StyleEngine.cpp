abf008292001397c834a9f34c69c547d5af1d2c7
rune <rune@opera.com>
2017-05-16 01:06:34 -0700

    Don't try to set the empty string as the preferred set.
    Noticed during debugging that we did an extra MarkDocumentDirty() for
    active stylesheet update trying to change the preferred set from empty
    string to empty string in a document where no sheets had a title
    attribute.
    
    Guard the call sites which didn't check for emptiness and add a DCHECK
    in the SetPreferredStylesheetSetNameIfNotSet implementation.
    
    Review-Url: https://codereview.chromium.org/2879223002
    Cr-Commit-Position: refs/heads/master@{#472039}

--- third_party/WebKit/Source/core/dom/StyleEngine.cpp
+++ third_party/WebKit/Source/core/dom/StyleEngine.cpp
@@ -561,8 +561,11 @@ CSSStyleSheet* StyleEngine::CreateSheet(Element& element,
 
   DCHECK(style_sheet);
   if (!element.IsInShadowTree()) {
-    style_sheet->SetTitle(element.title());
-    SetPreferredStylesheetSetNameIfNotSet(element.title());
+    String title = element.title();
+    if (!title.IsEmpty()) {
+      style_sheet->SetTitle(title);
+      SetPreferredStylesheetSetNameIfNotSet(title);
+    }
   }
   return style_sheet;
 }
@@ -925,6 +928,7 @@ void StyleEngine::SetStatsEnabled(bool enabled) {
 }
 
 void StyleEngine::SetPreferredStylesheetSetNameIfNotSet(const String& name) {
+  DCHECK(!name.IsEmpty());
   if (!preferred_stylesheet_set_name_.IsEmpty())
     return;
   preferred_stylesheet_set_name_ = name;
@@ -947,7 +951,8 @@ void StyleEngine::SetSelectedStylesheetSetName(const String& name) {
 }
 
 void StyleEngine::SetHttpDefaultStyle(const String& content) {
-  SetPreferredStylesheetSetNameIfNotSet(content);
+  if (!content.IsEmpty())
+    SetPreferredStylesheetSetNameIfNotSet(content);
 }
 
 void StyleEngine::EnsureUAStyleForFullscreen() {
