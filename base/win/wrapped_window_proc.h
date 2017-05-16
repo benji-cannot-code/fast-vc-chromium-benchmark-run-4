4ebc573ba9f8f7dcdb552cdd85f357baf0ffe070
siggi <siggi@chromium.org>
2017-05-16 10:15:15 -0700

    Make window message processing tally in the task profiler.
    As-is the processing and memory allocations taking place during window
    message processing is invisible to the task profiler.
    
    BUG=644385
    
    Review-Url: https://codereview.chromium.org/2881943002
    Cr-Commit-Position: refs/heads/master@{#472142}

--- base/win/wrapped_window_proc.h
+++ base/win/wrapped_window_proc.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 
 #include "base/base_export.h"
+#include "base/profiler/scoped_profile.h"
 #include "base/strings/string16.h"
 
 namespace base {
@@ -70,10 +71,13 @@ BASE_EXPORT void InitializeWindowClass(
 //   CreateWindowW(class_name, window_name, ...
 //
 template <WNDPROC proc>
-LRESULT CALLBACK WrappedWindowProc(HWND hwnd, UINT message,
-                                   WPARAM wparam, LPARAM lparam) {
+LRESULT CALLBACK
+WrappedWindowProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam) {
   LRESULT rv = 0;
   __try {
+    tracked_objects::ScopedProfile scoped_profile(
+        FROM_HERE, tracked_objects::ScopedProfile::ENABLED);
+
     rv = proc(hwnd, message, wparam, lparam);
   } __except(CallExceptionFilter(GetExceptionInformation())) {
   }
