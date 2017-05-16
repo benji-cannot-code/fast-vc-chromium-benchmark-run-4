2156b0f21ec5e6ead22ac7fd73229d85ea63e8b1
fdoray <fdoray@chromium.org>
2017-05-16 06:37:54 -0700

    Use TaskScheduler instead of SequencedWorkerPool in external_file_remover.mm.
    SequencedWorkerPool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=marq@chromium.org
    
    Review-Url: https://codereview.chromium.org/2876033003
    Cr-Commit-Position: refs/heads/master@{#472082}

--- ios/chrome/browser/ui/external_file_remover.mm
+++ ios/chrome/browser/ui/external_file_remover.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #import "base/mac/bind_objc_block.h"
+#include "base/task_scheduler/post_task.h"
 #include "components/sessions/core/tab_restore_service.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/sessions/ios_chrome_tab_restore_service_factory.h"
@@ -72,8 +73,9 @@ void ExternalFileRemover::RemoveFiles(bool all_files,
   if (callback_wrapper.is_null()) {
     callback_wrapper = base::Bind(&base::DoNothing);
   }
-  web::WebThread::PostBlockingPoolTaskAndReply(
-      FROM_HERE, base::BindBlockArc(^{
+  base::PostTaskWithTraitsAndReply(
+      FROM_HERE, {base::MayBlock(), base::TaskPriority::BACKGROUND},
+      base::BindBlockArc(^{
         [ExternalFileController removeFilesExcluding:referencedFiles
                                            olderThan:ageInDays];
       }),
