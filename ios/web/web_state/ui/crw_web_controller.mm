d6b7c96b5e43f97e322a8478fc16abb6cb411f7b
eugenebut <eugenebut@chromium.org>
2017-05-17 11:59:20 -0700

    Update NavigationContext headers right after setting headers to WebState
    There are 2 reasons for the change:
     1.) Structurally makes sense
     2.) Will allow to diagnose the root cause for crbug.com/676458
         (if context dereferencing will crash then
          didStartProvisionalNavigation: did not call registerLoadRequestForURL:).
    
    BUG=676458
    
    Review-Url: https://codereview.chromium.org/2886153002
    Cr-Commit-Position: refs/heads/master@{#472515}

--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -4523,7 +4523,12 @@ registerLoadRequestForURL:(const GURL&)requestURL
          (!_lastRegisteredRequestURL.is_valid() &&     // invalid URL load
           _documentURL.spec() == url::kAboutBlankURL));
 
-  self.webStateImpl->UpdateHttpResponseHeaders(_documentURL);
+  // Update HTTP response headers.
+  _webStateImpl->UpdateHttpResponseHeaders(_documentURL);
+  web::NavigationContextImpl* context =
+      [_navigationStates contextForNavigation:navigation];
+  context->SetResponseHeaders(_webStateImpl->GetHttpResponseHeaders());
+
   [self commitPendingNavigationInfo];
   if ([self currentBackForwardListItemHolder]->navigation_type() ==
       WKNavigationTypeBackForward) {
@@ -4566,9 +4571,6 @@ registerLoadRequestForURL:(const GURL&)requestURL
     }
   }
 
-  web::NavigationContextImpl* context =
-      [_navigationStates contextForNavigation:navigation];
-  context->SetResponseHeaders(_webStateImpl->GetHttpResponseHeaders());
   self.webStateImpl->OnNavigationFinished(context);
 
   [self updateSSLStatusForCurrentNavigationItem];
