2a8bbbf8084bc5798c7fd8621eec7ecb53fc1156
xidachen <xidachen@chromium.org>
2017-05-17 03:57:00 -0700

    Remove a dead class WebExternalBitmap
    BUG=NONE
    
    Review-Url: https://codereview.chromium.org/2886753002
    Cr-Commit-Position: refs/heads/master@{#472417}

--- third_party/WebKit/Source/platform/graphics/Canvas2DLayerBridgeTest.cpp
+++ third_party/WebKit/Source/platform/graphics/Canvas2DLayerBridgeTest.cpp
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/wtf/PtrUtil.h"
 #include "platform/wtf/RefPtr.h"
 #include "public/platform/Platform.h"
-#include "public/platform/WebExternalBitmap.h"
 #include "public/platform/WebThread.h"
 #include "public/platform/WebTraceLocation.h"
 #include "skia/ext/texture_handle.h"
--- third_party/WebKit/Source/platform/graphics/gpu/DrawingBuffer.cpp
+++ third_party/WebKit/Source/platform/graphics/gpu/DrawingBuffer.cpp
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/wtf/typed_arrays/ArrayBufferContents.h"
 #include "public/platform/Platform.h"
 #include "public/platform/WebCompositorSupport.h"
-#include "public/platform/WebExternalBitmap.h"
 #include "public/platform/WebExternalTextureLayer.h"
 #include "skia/ext/texture_handle.h"
 #include "third_party/skia/include/core/SkSurface.h"
