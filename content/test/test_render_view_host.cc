a8d76c6affcb0059b5df2a336724aa928201df18
bsep <bsep@chromium.org>
2017-05-17 13:26:19 -0700

    Backfill some UI tests.
    * Added a test for DialogClientView's button layout.
    * Added a test for non client hit results in BubbleFrameView.
    * Added a test for ValidationMessageBubbleView's anchor rect at HighDPI.
    
    BUG=702196,680019
    
    Review-Url: https://codereview.chromium.org/2861533003
    Cr-Commit-Position: refs/heads/master@{#472550}

--- content/test/test_render_view_host.cc
+++ content/test/test_render_view_host.cc
@@ -31,7 +31,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/video_frame.h"
 #include "ui/aura/env.h"
 #include "ui/compositor/compositor.h"
+#include "ui/compositor/layer_type.h"
 #include "ui/gfx/geometry/rect.h"
+
+#if defined(USE_AURA)
+#include "ui/aura/test/test_window_delegate.h"
+#endif
 
 namespace content {
 
@@ -74,6 +79,13 @@ TestRenderWidgetHostView::TestRenderWidgetHostView(RenderWidgetHost* rwh)
 #endif
 
   rwh_->SetView(this);
+
+#if defined(USE_AURA)
+  window_.reset(new aura::Window(
+      aura::test::TestWindowDelegate::CreateSelfDestroyingDelegate()));
+  window_->set_owned_by_parent(false);
+  window_->Init(ui::LayerType::LAYER_NOT_DRAWN);
+#endif
 }
 
 TestRenderWidgetHostView::~TestRenderWidgetHostView() {
@@ -92,7 +104,11 @@ gfx::Vector2dF TestRenderWidgetHostView::GetLastScrollOffset() const {
 }
 
 gfx::NativeView TestRenderWidgetHostView::GetNativeView() const {
+#if defined(USE_AURA)
+  return window_.get();
+#else
   return nullptr;
+#endif
 }
 
 gfx::NativeViewAccessible TestRenderWidgetHostView::GetNativeViewAccessible() {
