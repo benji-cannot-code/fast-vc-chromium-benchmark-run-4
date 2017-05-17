190066d251c5515ca143758a411473dc68ffdff7
parastoog <parastoog@google.com>
2017-05-17 14:18:35 -0700

    Improvements to the Payment Address Form UI
    Vertically align a text field and a dropdown field on a single line,
    in the payment request form.
    
    Also, according to the UX designer, locality and dependent-locality
    fields (city, .. ) need a full line, on the payment address form.
    
    BUG=707860
    
    Review-Url: https://codereview.chromium.org/2868313002
    Cr-Commit-Position: refs/heads/master@{#472573}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/ui/EditorDialog.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ui/EditorDialog.java
@@ -80,6 +80,7 @@ public class EditorDialog
     private final Handler mHandler;
     private final TextView.OnEditorActionListener mEditorActionListener;
     private final int mHalfRowMargin;
+    private final int mDropdownTopPadding;
     private final List<EditorFieldView> mFieldViews;
     private final List<EditText> mEditableTextFields;
     private final List<Spinner> mDropdownFields;
@@ -132,6 +133,8 @@ public class EditorDialog
 
         mHalfRowMargin = activity.getResources().getDimensionPixelSize(
                 R.dimen.payments_section_large_spacing);
+        mDropdownTopPadding = activity.getResources().getDimensionPixelSize(
+                R.dimen.payments_section_dropdown_top_padding);
         mFieldViews = new ArrayList<>();
         mEditableTextFields = new ArrayList<>();
         mDropdownFields = new ArrayList<>();
@@ -361,6 +364,16 @@ public class EditorDialog
                 ApiCompatibilityUtils.setMarginEnd(firstParams, mHalfRowMargin);
                 lastParams.width = 0;
                 lastParams.weight = 1;
+
+                // Align the text field and the dropdown field.
+                if ((fieldModel.isTextField() && nextFieldModel.isDropdownField())
+                        || (nextFieldModel.isTextField() && fieldModel.isDropdownField())) {
+                    LinearLayout.LayoutParams dropdownParams =
+                            fieldModel.isDropdownField() ? firstParams : lastParams;
+                    dropdownParams.topMargin = mDropdownTopPadding;
+                    dropdownParams.bottomMargin = 0;
+                }
+
                 i = i + 1;
             }
         }
