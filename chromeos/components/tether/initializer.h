8a16fa0f9119b3165ad35a5f8a6d6c5a3366d1f7
hansberry <hansberry@chromium.org>
2017-05-17 10:16:43 -0700

    Tether: only scan when asked to by NetworkStateHandler.
    Tether will not necessarily always scan when asked to by NetworkStateHandler; if a scan is active or has recently finished, the request is a no-op.
    
    BUG=672263
    
    Review-Url: https://codereview.chromium.org/2870393003
    Cr-Commit-Position: refs/heads/master@{#472481}

--- chromeos/components/tether/initializer.h
+++ chromeos/components/tether/initializer.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
+#include "base/time/default_clock.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/signin/core/browser/profile_oauth2_token_service.h"
 #include "device/bluetooth/bluetooth_adapter.h"
@@ -40,6 +41,7 @@ class NetworkConnectionHandlerTetherDelegate;
 class DeviceIdTetherNetworkGuidMap;
 class HostScanCache;
 class HostScanner;
+class HostScanScheduler;
 class HostScanDevicePrioritizer;
 class LocalDeviceDataProvider;
 class NetworkConfigurationRemover;
@@ -131,7 +133,9 @@ class Initializer : public OAuth2TokenService::Observer {
   std::unique_ptr<TetherNetworkDisconnectionHandler>
       tether_network_disconnection_handler_;
   std::unique_ptr<HostScanCache> host_scan_cache_;
+  std::unique_ptr<base::DefaultClock> clock_;
   std::unique_ptr<HostScanner> host_scanner_;
+  std::unique_ptr<HostScanScheduler> host_scan_scheduler_;
 
   base::WeakPtrFactory<Initializer> weak_ptr_factory_;
 
