667340a794fa08c127a9be9570ae524b57c89c62
vmpstr <vmpstr@chromium.org>
2017-05-16 17:19:35 -0700

    cc: Renamed approximate{BytesUsed,OpCount} in paint op buffer.
    This patch changes the names of functions as follows:
    PaintOpBuffer::approximateBytesUsed -> bytes_used
    PaintOpBuffer::approximateOpCount -> size
    
    Also changes related functions in other classes where appropriate.
    Also changes the return type of new size function to be size_t.
    
    R=enne@chromium.org, danakj@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2884563004
    Cr-Commit-Position: refs/heads/master@{#472259}

--- cc/paint/compositing_display_item.h
+++ cc/paint/compositing_display_item.h
@@ -30,7 +30,7 @@ class CC_PAINT_EXPORT CompositingDisplayItem : public DisplayItem {
     // TODO(pdr): Include color_filter's memory here.
     return 0;
   }
-  int ApproximateOpCount() const { return 1; }
+  int OpCount() const { return 1; }
 
   const uint8_t alpha;
   const SkBlendMode xfermode;
@@ -45,7 +45,7 @@ class CC_PAINT_EXPORT EndCompositingDisplayItem : public DisplayItem {
   EndCompositingDisplayItem();
   ~EndCompositingDisplayItem() override;
 
-  int ApproximateOpCount() const { return 0; }
+  int OpCount() const { return 0; }
 };
 
 }  // namespace cc
--- cc/paint/filter_display_item.h
+++ cc/paint/filter_display_item.h
@@ -26,7 +26,7 @@ class CC_PAINT_EXPORT FilterDisplayItem : public DisplayItem {
     // enough.
     return filters.size() * sizeof(filters.at(0));
   }
-  int ApproximateOpCount() const { return 1; }
+  int OpCount() const { return 1; }
 
   const FilterOperations filters;
   const gfx::RectF bounds;
@@ -38,7 +38,7 @@ class CC_PAINT_EXPORT EndFilterDisplayItem : public DisplayItem {
   EndFilterDisplayItem();
   ~EndFilterDisplayItem() override;
 
-  int ApproximateOpCount() const { return 0; }
+  int OpCount() const { return 0; }
 };
 
 }  // namespace cc
