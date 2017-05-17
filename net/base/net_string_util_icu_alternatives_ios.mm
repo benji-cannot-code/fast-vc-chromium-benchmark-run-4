d52db350e743710720a28099b588875327519ba9
mattm <mattm@chromium.org>
2017-05-17 14:17:58 -0700

    Add missing include in net/base/net_string_util_icu_alternatives_ios.mm.
    Fixes ios cronet build.
    
    BUG=671420
    
    Review-Url: https://codereview.chromium.org/2887203002
    Cr-Commit-Position: refs/heads/master@{#472572}

--- net/base/net_string_util_icu_alternatives_ios.mm
+++ net/base/net_string_util_icu_alternatives_ios.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/mac/scoped_cftyperef.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/strings/string16.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/sys_string_conversions.h"
