bc76004cbeb34e9b387f95501334e577165671e9
glebl <glebl@chromium.org>
2017-05-16 21:25:58 -0700

    Account for border/padding in NG Inline layout algorithm.
    BUG=635619
    TESTS=79 tests
    
    Review-Url: https://codereview.chromium.org/2886453005
    Cr-Commit-Position: refs/heads/master@{#472323}

--- third_party/WebKit/Source/core/layout/ng/inline/ng_inline_layout_algorithm.h
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_inline_layout_algorithm.h
@@ -89,6 +89,7 @@ class CORE_EXPORT NGInlineLayoutAlgorithm final
   unsigned disallow_first_line_rules_ : 1;
 
   NGConstraintSpaceBuilder space_builder_;
+  NGBoxStrut border_and_padding_;
 };
 
 }  // namespace blink
