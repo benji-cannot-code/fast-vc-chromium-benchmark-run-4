dbb073329d94fb9d2fce076b4d94466bd78d84e0
ossu <ossu@chromium.org>
2017-05-17 02:06:03 -0700

    Add builtin_audio_encoder_factory.h to webrtc_transport.cc
    It's been included by peerconnectioninterface.h, but that include is going away soon.
    
    BUG=webrtc:5806
    
    Review-Url: https://codereview.chromium.org/2877343005
    Cr-Commit-Position: refs/heads/master@{#472392}

--- remoting/protocol/webrtc_transport.cc
+++ remoting/protocol/webrtc_transport.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/protocol/webrtc_dummy_video_encoder.h"
 #include "third_party/libjingle_xmpp/xmllite/xmlelement.h"
 #include "third_party/webrtc/api/audio_codecs/builtin_audio_decoder_factory.h"
+#include "third_party/webrtc/api/audio_codecs/builtin_audio_encoder_factory.h"
 #include "third_party/webrtc/api/test/fakeconstraints.h"
 
 using buzz::QName;
