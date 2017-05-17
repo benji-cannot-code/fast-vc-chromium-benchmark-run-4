7cf77b96d8575d1cf4e5665df5ac250c687bb459
baxley <baxley@chromium.org>
2017-05-16 21:13:49 -0700

    [ObjC ARC] Converts ios/chrome/browser/autofill:eg_tests to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2884723002
    Cr-Commit-Position: refs/heads/master@{#472318}

--- ios/chrome/browser/autofill/form_input_egtest.mm
+++ ios/chrome/browser/autofill/form_input_egtest.mm
@@ -21,6 +21,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/test/http_server.h"
 #include "ios/web/public/test/http_server_util.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace {
 
 const char kFormElementId1[] = "username";
@@ -32,7 +36,7 @@ NSString* GetFocusedElementId() {
   NSString* js = @"(function() {"
                   "  return document.activeElement.id;"
                   "})();";
-  NSError* error = nil;
+  __unsafe_unretained NSError* error = nil;
   NSString* result = chrome_test_util::ExecuteJavaScript(js, &error);
   GREYAssertTrue(!error, @"Unexpected error when executing JavaScript.");
   return result;
