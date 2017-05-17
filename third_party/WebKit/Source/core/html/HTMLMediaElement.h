11beed9aee48595c0bc7b41154f18c8e77355cea
hubbe <hubbe@chromium.org>
2017-05-16 21:56:42 -0700

    Autoplay time metric
    We're going to run an experiment to make the "canplaythrough" event fire according
    to spec instead of at the same time as "canplay" this will sometimes delay autoplay,
    and we want to know how much, so let's add a metric for that.
    
    BUG=73609
    
    Review-Url: https://codereview.chromium.org/2856783002
    Cr-Commit-Position: refs/heads/master@{#472331}

--- third_party/WebKit/Source/core/html/HTMLMediaElement.h
+++ third_party/WebKit/Source/core/html/HTMLMediaElement.h
@@ -332,6 +332,8 @@ class CORE_EXPORT HTMLMediaElement
 
   const AutoplayPolicy& GetAutoplayPolicy() const { return *autoplay_policy_; }
 
+  WebMediaPlayer::LoadType GetLoadType() const;
+
  protected:
   HTMLMediaElement(const QualifiedName&, Document&);
   ~HTMLMediaElement() override;
@@ -444,7 +446,6 @@ class CORE_EXPORT HTMLMediaElement
   void LoadResource(const WebMediaPlayerSource&, const String& content_type);
   void StartPlayerLoad();
   void SetPlayerPreload();
-  WebMediaPlayer::LoadType GetLoadType() const;
   void ScheduleNextSourceChild();
   void LoadSourceFromObject();
   void LoadSourceFromAttribute();
