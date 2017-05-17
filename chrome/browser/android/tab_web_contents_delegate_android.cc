251f232e35629a27db344af53e8a9ddf562f3920
asimjour <asimjour@chromium.org>
2017-05-17 15:12:27 -0700

    Use VrTabHelper to disable media permission and bluetooth picker
    Disable media permission and bluetooth picker when in VR mode.
    
    BUG=715613
    
    Review-Url: https://codereview.chromium.org/2886163003
    Cr-Commit-Position: refs/heads/master@{#472584}

--- chrome/browser/android/tab_web_contents_delegate_android.cc
+++ chrome/browser/android/tab_web_contents_delegate_android.cc
@@ -128,9 +128,8 @@ void TabWebContentsDelegateAndroid::RunFileChooser(
     const FileChooserParams& params) {
 #if BUILDFLAG(ENABLE_VR)
   if (vr_shell::VrTabHelper::IsInVr(
-          WebContents::FromRenderFrameHost(render_frame_host))) {
+          WebContents::FromRenderFrameHost(render_frame_host)))
     return;
-  }
 #endif
   FileSelectHelper::RunFileChooser(render_frame_host, params);
 }
@@ -139,6 +138,10 @@ std::unique_ptr<BluetoothChooser>
 TabWebContentsDelegateAndroid::RunBluetoothChooser(
     content::RenderFrameHost* frame,
     const BluetoothChooser::EventHandler& event_handler) {
+#if BUILDFLAG(ENABLE_VR)
+  if (vr_shell::VrTabHelper::IsInVr(WebContents::FromRenderFrameHost(frame)))
+    return nullptr;
+#endif
   return base::MakeUnique<BluetoothChooserAndroid>(frame, event_handler);
 }
 
@@ -280,6 +283,10 @@ void TabWebContentsDelegateAndroid::RequestMediaAccessPermission(
     content::WebContents* web_contents,
     const content::MediaStreamRequest& request,
     const content::MediaResponseCallback& callback) {
+#if BUILDFLAG(ENABLE_VR)
+  if (vr_shell::VrTabHelper::IsInVr(web_contents))
+    return;
+#endif
   MediaCaptureDevicesDispatcher::GetInstance()->ProcessMediaAccessRequest(
       web_contents, request, callback, nullptr);
 }
