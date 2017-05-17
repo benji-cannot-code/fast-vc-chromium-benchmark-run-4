62064d356582bb4d852ab1881f9e65f97f9330d7
liaoyuke <liaoyuke@chromium.org>
2017-05-17 05:39:21 -0700

    [ObjC ARC] Converts ios/chrome/test/earl_grey:ios_chrome_device_check_egtests to ARC.
    The automatic arc script doesn't work for this target because it simply
    adds configs += [ "//build/config/compiler:enable_arc" ] to
    chrome_ios_eg_test("ios_chrome_device_check_egtests"), however, the
    body of a  template invocation doesn't take configs.
    
    This CL manually moves "device_check_egtest.mm" to a separate
    source_set and adds configs += [ "//build/config/compiler:enable_arc" ]
    to that source_set.
    
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2882253002
    Cr-Commit-Position: refs/heads/master@{#472430}

--- ios/chrome/test/earl_grey/device_check_egtest.mm
+++ ios/chrome/test/earl_grey/device_check_egtest.mm
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
 #include "url/gurl.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 using chrome_test_util::WebViewContainingText;
 
 // Test suite to verify Internet connectivity.
