cbc0954654f74b0544275e6ebbeeb72ee7bf996d
Paweł Hajdan, Jr <phajdan.jr@chromium.org>
2017-05-17 18:28:38 +0200

    Upstream Gentoo Linux patch for using system ffmpeg
    This upstreams https://gitweb.gentoo.org/repo/gentoo.git/tree/www-client/chromium/files/chromium-system-ffmpeg-r6.patch?id=c387b221c37fe243cf51a6e6d1493fd738f633ac
    
    This is intended to be used together with https://cs.chromium.org/chromium/src/build/linux/unbundle/ffmpeg.gn
    
    build/linux/unbundle is a separate directory tree, so that regular
    Chromium developers don't need to worry about this configuration.
    
    Please see https://cs.chromium.org/chromium/src/build/linux/unbundle/README?dr
    for more context.
    
    BUG=none
    R=dalecurtis@chromium.org
    
    Review-Url: https://codereview.chromium.org/2885543003 .
    Cr-Commit-Position: refs/heads/master@{#472472}

--- media/ffmpeg/ffmpeg_common.h
+++ media/ffmpeg/ffmpeg_common.h
@@ -24,10 +24,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Include FFmpeg header files.
 extern "C" {
+#if !defined(USE_SYSTEM_FFMPEG)
 // Disable deprecated features which result in spammy compile warnings.  This
 // list of defines must mirror those in the 'defines' section of FFmpeg's
 // BUILD.gn file or the headers below will generate different structures!
 #define FF_API_CONVERGENCE_DURATION 0
+#endif  // !defined(USE_SYSTEM_FFMPEG)
 // Upstream libavcodec/utils.c still uses the deprecated
 // av_dup_packet(), causing deprecation warnings.
 // The normal fix for such things is to disable the feature as below,
@@ -41,7 +43,9 @@ extern "C" {
 MSVC_PUSH_DISABLE_WARNING(4244);
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
+#if !defined(USE_SYSTEM_FFMPEG)
 #include <libavformat/internal.h>
+#endif  // !defined(USE_SYSTEM_FFMPEG)
 #include <libavformat/avio.h>
 #include <libavutil/avutil.h>
 #include <libavutil/imgutils.h>
