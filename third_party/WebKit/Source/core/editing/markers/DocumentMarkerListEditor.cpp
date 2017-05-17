c2550c586fb05db8efb71ba1ab98d5d1164673c0
rlanday <rlanday@chromium.org>
2017-05-17 02:25:47 -0700

    Move DocumentMarkerList::RemoveMarkersUnderWords() to SpellCheckMarkerListImpl
    Currently this is a method on the general DocumentMarkerList interface. This CL
    moves it to only be on SpellCheckMarkerListImpl and adds a typecast at the
    callsite in DocumentMarkerController to enable this.
    
    I am leaving the method implementation in DocumentMarkerListEditor.cpp in this CL
    to make this CL smaller; I will move it to SpellCheckMarkerListImpl.cpp in a future
    CL.
    
    BUG=707867
    
    Review-Url: https://codereview.chromium.org/2871823002
    Cr-Commit-Position: refs/heads/master@{#472395}

--- third_party/WebKit/Source/core/editing/markers/DocumentMarkerListEditor.cpp
+++ third_party/WebKit/Source/core/editing/markers/DocumentMarkerListEditor.cpp
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/markers/DocumentMarkerListEditor.h"
 
 #include "core/editing/markers/RenderedDocumentMarker.h"
+#include "core/editing/markers/SpellCheckMarkerListImpl.h"
 
 namespace blink {
 
@@ -83,18 +84,18 @@ bool DocumentMarkerListEditor::RemoveMarkers(MarkerList* list,
   return doc_dirty;
 }
 
-bool DocumentMarkerListEditor::RemoveMarkersUnderWords(
-    MarkerList* list,
+// TODO(rlanday): move to SpellCheckMarkerListImpl.cpp
+bool SpellCheckMarkerListImpl::RemoveMarkersUnderWords(
     const String& node_text,
     const Vector<String>& words) {
   bool removed_markers = false;
-  for (size_t j = list->size(); j > 0; --j) {
-    const DocumentMarker& marker = *(*list)[j - 1];
+  for (size_t j = markers_.size(); j > 0; --j) {
+    const DocumentMarker& marker = *markers_[j - 1];
     const unsigned start = marker.StartOffset();
     const unsigned length = marker.EndOffset() - marker.StartOffset();
     const String& marker_text = node_text.Substring(start, length);
     if (words.Contains(marker_text)) {
-      list->erase(j - 1);
+      markers_.erase(j - 1);
       removed_markers = true;
     }
   }
