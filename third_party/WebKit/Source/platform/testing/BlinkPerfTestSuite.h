78df0a5afc1a818f7b0d7bc6c604ea9509371bec
Sami Kyostila <skyostil@chromium.org>
2017-05-17 14:26:02 +0100

    scheduler: Fix blink perf test crash
    Fix perf test crash by making sure WTF is initialized.
    
    BUG=718894
    
    Change-Id: I4b653e63bb5e3b8c285484435686691eb754e694
    Reviewed-on: https://chromium-review.googlesource.com/506727
    Commit-Queue: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#472912}

---
+++ third_party/WebKit/Source/platform/testing/BlinkPerfTestSuite.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BlinkPerfTestSuite_h
+#define BlinkPerfTestSuite_h
+
+#include "base/test/test_suite.h"
+
+namespace blink {
+
+class BlinkPerfTestSuite : public base::TestSuite {
+ public:
+  BlinkPerfTestSuite(int argc, char** argv);
+
+  void Initialize() override;
+  void Shutdown() override;
+};
+
+}  // namespace blink
+
+#endif  // BlinkPerfTestSuite_h
---
+++ third_party/WebKit/Source/platform/testing/RunAllPerfTests.cpp
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/testing/BlinkPerfTestSuite.h"
+
+int main(int argc, char** argv) {
+  return blink::BlinkPerfTestSuite(argc, argv).Run();
+}
