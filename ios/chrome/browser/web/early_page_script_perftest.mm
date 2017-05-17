a14fc2d788d5e4fa60bcb381efc1a181dfc9bfb2
liaoyuke <liaoyuke@chromium.org>
2017-05-17 13:53:58 -0700

    [ObjC ARC] Converts ios/chrome/browser/web:perf_tests to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2886843003
    Cr-Commit-Position: refs/heads/master@{#472559}

--- ios/chrome/browser/web/early_page_script_perftest.mm
+++ ios/chrome/browser/web/early_page_script_perftest.mm
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/mac/bundle_locations.h"
-#include "base/mac/scoped_nsobject.h"
 #include "base/timer/elapsed_timer.h"
 #include "ios/chrome/test/base/perf_test_ios.h"
 #include "ios/web/public/test/fakes/test_browser_state.h"
@@ -15,6 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/Foundation.h>
 #import <WebKit/WebKit.h>
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace {
 
 // Class for testing early page script injection into WKWebView.
@@ -22,7 +25,7 @@ namespace {
 class EarlyPageScriptPerfTest : public PerfTest {
  protected:
   EarlyPageScriptPerfTest() : PerfTest("Early Page Script for WKWebView") {
-    web_view_.reset([web::BuildWKWebView(CGRectZero, &browser_state_) retain]);
+    web_view_ = web::BuildWKWebView(CGRectZero, &browser_state_);
   }
 
   // Injects early script into WKWebView.
@@ -33,7 +36,7 @@ class EarlyPageScriptPerfTest : public PerfTest {
   // BrowserState required for web view creation.
   web::TestBrowserState browser_state_;
   // WKWebView to test scripts injections.
-  base::scoped_nsobject<WKWebView> web_view_;
+  WKWebView* web_view_;
 };
 
 // Tests script loading time.
