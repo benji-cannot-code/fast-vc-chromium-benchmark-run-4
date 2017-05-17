57bace27c2d53a6e244de21d7364114344f1d2a9
kolos <kolos@chromium.org>
2017-05-17 02:59:15 -0700

    Revert of media: Shutdown AudioManager in TestMojoMediaClient (patchset #3 id:40001 of https://codereview.chromium.org/2886683003/ )
    Reason for revert:
    Layout tests failure ; mojo/module-loading(-manual-deps-loading).html
    
    BUG=723461
    
    Original issue's description:
    > media: Shutdown AudioManager in TestMojoMediaClient
    >
    > By AudioManager API, Shutdown() must be called before destruction. This
    > isn't caught by tests because media_service_unittests is only run on
    > release builds.
    >
    > Review-Url: https://codereview.chromium.org/2886683003
    > Cr-Commit-Position: refs/heads/master@{#472225}
    > Committed: https://chromium.googlesource.com/chromium/src/+/6e0f47b9cc33450f93d02d238cd4e60dffa89421
    
    TBR=alokp@chromium.org,xhwang@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2890733004
    Cr-Commit-Position: refs/heads/master@{#472404}

--- media/mojo/services/test_mojo_media_client.cc
+++ media/mojo/services/test_mojo_media_client.cc
@@ -28,11 +28,11 @@ TestMojoMediaClient::TestMojoMediaClient() {}
 
 TestMojoMediaClient::~TestMojoMediaClient() {
   DVLOG(1) << __func__;
-
-  if (audio_manager_) {
-    audio_manager_->Shutdown();
-    audio_manager_.reset();
-  }
+  // AudioManager destructor requires MessageLoop.
+  // Destroy it before the message loop goes away.
+  audio_manager_.reset();
+  // Flush the message loop to ensure that the audio manager is destroyed.
+  base::RunLoop().RunUntilIdle();
 }
 
 void TestMojoMediaClient::Initialize(
@@ -45,6 +45,7 @@ void TestMojoMediaClient::Initialize(
   if (!audio_manager) {
     audio_manager_ = media::AudioManager::CreateForTesting(
         base::MakeUnique<AudioThreadImpl>());
+    audio_manager = audio_manager_.get();
     // Flush the message loop to ensure that the audio manager is initialized.
     base::RunLoop().RunUntilIdle();
   }
