c8bc7fd3289563ffae766c7d13544d67380da15a
xhwang <xhwang@chromium.org>
2017-05-17 09:22:30 -0700

    Reland of media: Shutdown AudioManager in TestMojoMediaClient (patchset #1 id:1 of https://codereview.chromium.org/2890733004/ )
    Reason for revert:
    This was not the cause of the failure, which has been fixed by reverting other CLs. See BUG for details.
    
    Original issue's description:
    > Revert of media: Shutdown AudioManager in TestMojoMediaClient (patchset #3 id:40001 of https://codereview.chromium.org/2886683003/ )
    >
    > Reason for revert:
    > Layout tests failure ; mojo/module-loading(-manual-deps-loading).html
    >
    > BUG=723461
    >
    > Original issue's description:
    > > media: Shutdown AudioManager in TestMojoMediaClient
    > >
    > > By AudioManager API, Shutdown() must be called before destruction. This
    > > isn't caught by tests because media_service_unittests is only run on
    > > release builds.
    > >
    > > Review-Url: https://codereview.chromium.org/2886683003
    > > Cr-Commit-Position: refs/heads/master@{#472225}
    > > Committed: https://chromium.googlesource.com/chromium/src/+/6e0f47b9cc33450f93d02d238cd4e60dffa89421
    >
    > TBR=alokp@chromium.org,xhwang@chromium.org
    > # Skipping CQ checks because original CL landed less than 1 days ago.
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    >
    > Review-Url: https://codereview.chromium.org/2890733004
    > Cr-Commit-Position: refs/heads/master@{#472404}
    > Committed: https://chromium.googlesource.com/chromium/src/+/36e3810d369c449a7f6659addd6697f7e81132a4
    
    TBR=alokp@chromium.org,kolos@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=723461
    
    Review-Url: https://codereview.chromium.org/2886193002
    Cr-Commit-Position: refs/heads/master@{#472470}

--- media/mojo/services/test_mojo_media_client.cc
+++ media/mojo/services/test_mojo_media_client.cc
@@ -28,11 +28,11 @@ TestMojoMediaClient::TestMojoMediaClient() {}
 
 TestMojoMediaClient::~TestMojoMediaClient() {
   DVLOG(1) << __func__;
-  // AudioManager destructor requires MessageLoop.
-  // Destroy it before the message loop goes away.
-  audio_manager_.reset();
-  // Flush the message loop to ensure that the audio manager is destroyed.
-  base::RunLoop().RunUntilIdle();
+
+  if (audio_manager_) {
+    audio_manager_->Shutdown();
+    audio_manager_.reset();
+  }
 }
 
 void TestMojoMediaClient::Initialize(
@@ -45,7 +45,6 @@ void TestMojoMediaClient::Initialize(
   if (!audio_manager) {
     audio_manager_ = media::AudioManager::CreateForTesting(
         base::MakeUnique<AudioThreadImpl>());
-    audio_manager = audio_manager_.get();
     // Flush the message loop to ensure that the audio manager is initialized.
     base::RunLoop().RunUntilIdle();
   }
