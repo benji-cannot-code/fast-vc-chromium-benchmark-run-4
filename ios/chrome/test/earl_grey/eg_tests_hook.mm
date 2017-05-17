cab80d6e67cced62370a3853f8a9d55aef58dc84
liaoyuke <liaoyuke@chromium.org>
2017-05-17 12:44:01 -0700

    [ObjC ARC] Converts ios/chrome/test/earl_grey:hooks to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2885453004
    Cr-Commit-Position: refs/heads/master@{#472538}

--- ios/chrome/test/earl_grey/eg_tests_hook.mm
+++ ios/chrome/test/earl_grey/eg_tests_hook.mm
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ios/chrome/app/tests_hook.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace tests_hook {
 
 bool DisableContextualSearch() {
