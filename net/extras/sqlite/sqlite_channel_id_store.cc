ba004f69f4cf3f525016afea0ca615ecb96bd8ad
Eric Stevenson <estevenson@chromium.org>
2017-05-17 22:25:00 +0000

    Revert "Add android application status listener to //net SQLite stores"
    This reverts commit 83325fee9ee045b846a4965e2773c3bf26bcdba0.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > Add android application status listener to //net SQLite stores
    > 
    > This listener listens for APPLICATION_STATE_HAS_STOPPED_ACTIVITIES and commits
    > any pending changes to the sqlite database.
    > 
    > Bug: 719726
    > Change-Id: Iddb25fc7c34b5c9e87395c92eecb23becae1c163
    > Reviewed-on: https://chromium-review.googlesource.com/503516
    > Reviewed-by: Matt Mueller <mattm@chromium.org>
    > Commit-Queue: Nick Harper <nharper@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#472509}
    
    TBR=pasko@chromium.org,mattm@chromium.org,nharper@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    Bug: 719726
    
    Change-Id: I460bd2c2151d74469eb6a0da95520557fee39b79
    Reviewed-on: https://chromium-review.googlesource.com/508190
    Reviewed-by: Eric Stevenson <estevenson@chromium.org>
    Commit-Queue: Eric Stevenson <estevenson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#472589}

--- net/extras/sqlite/sqlite_channel_id_store.cc
+++ net/extras/sqlite/sqlite_channel_id_store.cc
@@ -31,10 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sql/transaction.h"
 #include "url/gurl.h"
 
-#if defined(OS_ANDROID)
-#include "base/android/application_status_listener.h"
-#endif
-
 namespace {
 
 // Version number of the database.
@@ -159,13 +155,6 @@ class SQLiteChannelIDStore::Backend
   void DatabaseErrorCallback(int error, sql::Statement* stmt);
   void KillDatabase();
 
-#if defined(OS_ANDROID)
-  void OnApplicationStateChange(base::android::ApplicationState state);
-
-  std::unique_ptr<base::android::ApplicationStatusListener>
-      app_status_listener_;
-#endif
-
   const base::FilePath path_;
   std::unique_ptr<sql::Connection> db_;
   sql::MetaTable meta_table_;
@@ -207,11 +196,6 @@ void SQLiteChannelIDStore::Backend::LoadInBackground(
     std::vector<std::unique_ptr<DefaultChannelIDStore::ChannelID>>*
         channel_ids) {
   DCHECK(background_task_runner_->RunsTasksOnCurrentThread());
-
-#if defined(OS_ANDROID)
-  app_status_listener_.reset(new base::android::ApplicationStatusListener(
-      base::Bind(&Backend::OnApplicationStateChange, base::Unretained(this))));
-#endif
 
   // This method should be called only once per instance.
   DCHECK(!db_.get());
@@ -554,15 +538,6 @@ void SQLiteChannelIDStore::Backend::PrunePendingOperationsForDeletes(
     }
   }
 }
-
-#if defined(OS_ANDROID)
-void SQLiteChannelIDStore::Backend::OnApplicationStateChange(
-    base::android::ApplicationState state) {
-  if (state != base::android::APPLICATION_STATE_HAS_STOPPED_ACTIVITIES)
-    return;
-  Commit();
-}
-#endif
 
 void SQLiteChannelIDStore::Backend::Commit() {
   DCHECK(background_task_runner_->RunsTasksOnCurrentThread());
