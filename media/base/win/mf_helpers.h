5d8678929b4aa1161561cd980d861b9634920bad
robliao <robliao@chromium.org>
2017-05-17 10:17:20 -0700

    Add Missing Includes to mf_helpers.h
    mf_helpers.h was previously relying on scoped_comptr.h for selected
    definitions. scoped_comptr.h in the future will not include
    base/logging.h and base/macros.h, breaking this file.
    
    BUG=638327
    TBR=dalecurtis@chromium.org
    Trivial change.
    
    Review-Url: https://codereview.chromium.org/2892533002
    Cr-Commit-Position: refs/heads/master@{#472482}

--- media/base/win/mf_helpers.h
+++ media/base/win/mf_helpers.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <mfapi.h>
 #include <stdint.h>
 
+#include "base/logging.h"
+#include "base/macros.h"
 #include "base/win/scoped_comptr.h"
 #include "media/base/win/mf_initializer_export.h"
 
