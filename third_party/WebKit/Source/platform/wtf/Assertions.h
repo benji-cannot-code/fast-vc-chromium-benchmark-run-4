01eb82801141608b89ec66bca4c5a2df8a23064e
pilgrim <pilgrim@chromium.org>
2017-05-16 17:58:19 -0700

    Remove obsolete RELEASE_ASSERT macro definition
    BUG=596760
    
    Review-Url: https://codereview.chromium.org/2885003003
    Cr-Commit-Position: refs/heads/master@{#472274}

--- third_party/WebKit/Source/platform/wtf/Assertions.h
+++ third_party/WebKit/Source/platform/wtf/Assertions.h
@@ -30,9 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // This file uses some GCC extensions, but it should be compatible with C++ and
 // Objective C++.
-//
-// For non-debug builds, everything is disabled by default, except for the
-// RELEASE_ASSERT family of macros.
 
 #include <stdarg.h>
 #include "base/allocator/partition_allocator/oom.h"
@@ -168,17 +165,6 @@ class WTF_EXPORT ScopedLogger {
 #else
 #define SECURITY_DCHECK(condition) ((void)0)
 #define SECURITY_CHECK(condition) CHECK(condition)
-#endif
-
-// RELEASE_ASSERT
-// Use in places where failure of an assertion indicates a definite security
-// vulnerability from which execution must not continue even in a release build.
-// Please sure to file bugs for these failures using the security template:
-//    http://code.google.com/p/chromium/issues/entry?template=Security%20Bug
-#if defined(ADDRESS_SANITIZER)
-#define RELEASE_ASSERT(condition) SECURITY_CHECK(condition)
-#else
-#define RELEASE_ASSERT(condition) CHECK(condition)
 #endif
 
 // DEFINE_COMPARISON_OPERATORS_WITH_REFERENCES
