8fa95afbf3d2d980296d8cd82e76cd7e6ddb7ba3
gambard <gambard@chromium.org>
2017-05-18 08:28:32 -0700

    Limit the number of Most Visited lines to two
    The Most Visited tiles are not longer displayed on more than two lines.
    
    BUG=707754
    
    Review-Url: https://codereview.chromium.org/2888403002
    Cr-Commit-Position: refs/heads/master@{#472820}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_updater.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_updater.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_footer_item.h"
 #import "ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_text_item.h"
 #import "ios/chrome/browser/ui/content_suggestions/cells/suggested_content.h"
+#import "ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_utils.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_data_sink.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_data_source.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_image_fetcher.h"
@@ -102,6 +103,8 @@ SectionIdentifier SectionIdentifierForInfo(
   }
 }
 
+const CGFloat kNumberOfMostVisitedLines = 2;
+
 }  // namespace
 
 @interface ContentSuggestionsCollectionUpdater ()<ContentSuggestionsDataSink,
@@ -111,6 +114,8 @@ SectionIdentifier SectionIdentifierForInfo(
 @property(nonatomic, strong)
     NSMutableDictionary<NSNumber*, ContentSuggestionsSectionInformation*>*
         sectionInfoBySectionIdentifier;
+// Width of the collection. Upon size change, it reflects the new size.
+@property(nonatomic, assign) CGFloat collectionWidth;
 
 @end
 
@@ -119,6 +124,7 @@ SectionIdentifier SectionIdentifierForInfo(
 @synthesize collectionViewController = _collectionViewController;
 @synthesize dataSource = _dataSource;
 @synthesize sectionInfoBySectionIdentifier = _sectionInfoBySectionIdentifier;
+@synthesize collectionWidth = _collectionWidth;
 
 - (instancetype)initWithDataSource:
     (id<ContentSuggestionsDataSource>)dataSource {
@@ -135,6 +141,8 @@ SectionIdentifier SectionIdentifierForInfo(
 - (void)setCollectionViewController:
     (ContentSuggestionsViewController*)collectionViewController {
   _collectionViewController = collectionViewController;
+  self.collectionWidth =
+      collectionViewController.collectionView.bounds.size.width;
 
   [self reloadAllData];
 }
@@ -271,6 +279,12 @@ addSuggestionsToModel:(NSArray<CSCollectionViewItem*>*)suggestions
 
   [suggestions enumerateObjectsUsingBlock:^(CSCollectionViewItem* item,
                                             NSUInteger index, BOOL* stop) {
+    NSInteger section = [model sectionForSectionIdentifier:sectionIdentifier];
+    if ([self isMostVisitedSection:section] &&
+        [model numberOfItemsInSection:section] >=
+            [self mostVisitedPlaceCount]) {
+      return;
+    }
     ItemType type = ItemTypeForInfo(sectionInfo);
     item.type = type;
     NSIndexPath* addedIndexPath =
@@ -347,6 +361,47 @@ addSuggestionsToModel:(NSArray<CSCollectionViewItem*>*)suggestions
   return
       [self.collectionViewController.collectionViewModel
           sectionIdentifierForSection:section] == SectionIdentifierMostVisited;
+}
+
+- (void)updateMostVisitedForSize:(CGSize)size {
+  self.collectionWidth = size.width;
+
+  CSCollectionViewModel* model =
+      self.collectionViewController.collectionViewModel;
+  if (![model hasSectionForSectionIdentifier:SectionIdentifierMostVisited])
+    return;
+
+  NSInteger mostVisitedSection =
+      [model sectionForSectionIdentifier:SectionIdentifierMostVisited];
+  ContentSuggestionsSectionInformation* mostVisitedSectionInfo =
+      self.sectionInfoBySectionIdentifier[@(SectionIdentifierMostVisited)];
+  NSArray<CSCollectionViewItem*>* mostVisited =
+      [self.dataSource itemsForSectionInfo:mostVisitedSectionInfo];
+  NSInteger newCount = MIN([self mostVisitedPlaceCount],
+                           static_cast<NSInteger>(mostVisited.count));
+  NSInteger currentCount = [model numberOfItemsInSection:mostVisitedSection];
+
+  if (currentCount == newCount)
+    return;
+
+  // If the animations are enabled, the items are added then the rotation
+  // animation is triggered, creating a weird sequenced animation.
+  [UIView setAnimationsEnabled:NO];
+  if (currentCount > newCount) {
+    for (NSInteger i = newCount; i < currentCount; i++) {
+      NSIndexPath* itemToRemove =
+          [NSIndexPath indexPathForItem:newCount inSection:mostVisitedSection];
+      [self.collectionViewController dismissEntryAtIndexPath:itemToRemove];
+    }
+  } else {
+    NSMutableArray* itemsToBeAdded = [NSMutableArray array];
+    for (NSInteger i = currentCount; i < newCount; i++) {
+      [itemsToBeAdded addObject:mostVisited[i]];
+    }
+    [self.collectionViewController addSuggestions:itemsToBeAdded
+                                    toSectionInfo:mostVisitedSectionInfo];
+  }
+  [UIView setAnimationsEnabled:YES];
 }
 
 #pragma mark - SuggestedContentDelegate
@@ -532,6 +587,13 @@ addSuggestionsToModel:(NSArray<CSCollectionViewItem*>*)suggestions
   [model addItem:item toSectionWithIdentifier:sectionIdentifier];
 
   return [NSIndexPath indexPathForItem:itemNumber inSection:section];
+}
+
+// Returns the maximum number of Most Visited tiles to be displayed in the
+// collection.
+- (NSInteger)mostVisitedPlaceCount {
+  return content_suggestions::numberOfTilesForWidth(self.collectionWidth) *
+         kNumberOfMostVisitedLines;
 }
 
 @end
