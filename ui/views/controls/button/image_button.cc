d41cf1581048edcb0ff6bbbf474da6697520ea52
estade <estade@chromium.org>
2017-05-17 20:45:12 -0700

    Apply View::set_preferred_size in some more places.
    Also fix a botched attempt to simplify SystemMenuButton.
    
    BUG=723080
    
    Review-Url: https://codereview.chromium.org/2889623002
    Cr-Commit-Position: refs/heads/master@{#472646}

--- ui/views/controls/button/image_button.cc
+++ ui/views/controls/button/image_button.cc
@@ -99,20 +99,6 @@ void ImageButton::SetMinimumImageSize(const gfx::Size& size) {
 ////////////////////////////////////////////////////////////////////////////////
 // ImageButton, View overrides:
 
-gfx::Size ImageButton::GetPreferredSize() const {
-  gfx::Size size(kDefaultWidth, kDefaultHeight);
-  if (!images_[STATE_NORMAL].isNull()) {
-    size = gfx::Size(images_[STATE_NORMAL].width(),
-                     images_[STATE_NORMAL].height());
-  }
-
-  size.SetToMax(minimum_image_size_);
-
-  gfx::Insets insets = GetInsets();
-  size.Enlarge(insets.width(), insets.height());
-  return size;
-}
-
 const char* ImageButton::GetClassName() const {
   return kViewClassName;
 }
@@ -141,6 +127,20 @@ void ImageButton::OnPaint(gfx::Canvas* canvas) {
   }
 
   Painter::PaintFocusPainter(this, canvas, focus_painter());
+}
+
+gfx::Size ImageButton::CalculatePreferredSize() const {
+  gfx::Size size(kDefaultWidth, kDefaultHeight);
+  if (!images_[STATE_NORMAL].isNull()) {
+    size = gfx::Size(images_[STATE_NORMAL].width(),
+                     images_[STATE_NORMAL].height());
+  }
+
+  size.SetToMax(minimum_image_size_);
+
+  gfx::Insets insets = GetInsets();
+  size.Enlarge(insets.width(), insets.height());
+  return size;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
--- ui/views/controls/button/image_button.h
+++ ui/views/controls/button/image_button.h
@@ -75,9 +75,9 @@ class VIEWS_EXPORT ImageButton : public CustomButton {
   }
 
   // Overridden from View:
-  gfx::Size GetPreferredSize() const override;
   const char* GetClassName() const override;
   void OnPaint(gfx::Canvas* canvas) override;
+  gfx::Size CalculatePreferredSize() const override;
 
  protected:
   // Overridden from View:
