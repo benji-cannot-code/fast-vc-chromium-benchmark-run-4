79bb0aeb0fb26653d8ed2806d1c39ce44af708cc
rtoy <rtoy@chromium.org>
2017-05-18 09:50:57 -0700

    Compute the tail time for an IIRFilter from its coefficients
    For an IIRFilterNode the tail time is currently set to infinity, which is
    mathmatically true, but impractical because that keeps the IIRFilter
    alive even if the output is very small.
    
    Instead, compute a tail time based on the coefficients of the IIR
    filter. The tail time is the estimated time where the impulse response
    is sufficiently small that we can say that output is essentially
    zero.
    
    By doing so, an IIRFilterNode can disconnect itself from downstreams
    nodes, reducing complexity of the graph.  Without this, downstream
    nodes stay alive processing silence.
    
    BUG=612215
    TEST=iirfilter.html
    
    Review-Url: https://codereview.chromium.org/2851873003
    Cr-Commit-Position: refs/heads/master@{#472845}

--- third_party/WebKit/Source/modules/webaudio/IIRDSPKernel.cpp
+++ third_party/WebKit/Source/modules/webaudio/IIRDSPKernel.cpp
@@ -9,6 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+IIRDSPKernel::IIRDSPKernel(IIRProcessor* processor)
+    : AudioDSPKernel(processor),
+      iir_(processor->Feedforward(), processor->Feedback()) {
+  tail_time_ = iir_.TailTime(processor->SampleRate());
+}
+
 void IIRDSPKernel::Process(const float* source,
                            float* destination,
                            size_t frames_to_process) {
@@ -42,14 +48,7 @@ void IIRDSPKernel::GetFrequencyResponse(int n_frequencies,
 }
 
 double IIRDSPKernel::TailTime() const {
-  // TODO(rtoy): This is true mathematically (infinite impulse response), but
-  // perhaps it should be limited to a smaller value, possibly based on the
-  // actual filter coefficients.  To do that, we would probably need to find the
-  // pole, r, with largest magnitude and select some threshold, eps, such that
-  // |r|^n < eps for all n >= N.  N is then the tailTime for the filter.  If the
-  // the magnitude of r is greater than or equal to 1, the infinity is the right
-  // answer. (There is no constraint on the IIR filter that it be stable.)
-  return std::numeric_limits<double>::infinity();
+  return tail_time_;
 }
 
 double IIRDSPKernel::LatencyTime() const {
--- third_party/WebKit/Source/modules/webaudio/IIRDSPKernel.h
+++ third_party/WebKit/Source/modules/webaudio/IIRDSPKernel.h
@@ -16,9 +16,7 @@ class IIRProcessor;
 
 class IIRDSPKernel final : public AudioDSPKernel {
  public:
-  explicit IIRDSPKernel(IIRProcessor* processor)
-      : AudioDSPKernel(processor),
-        iir_(processor->Feedforward(), processor->Feedback()) {}
+  explicit IIRDSPKernel(IIRProcessor*);
 
   // AudioDSPKernel
   void Process(const float* source,
@@ -38,6 +36,9 @@ class IIRDSPKernel final : public AudioDSPKernel {
 
  protected:
   IIRFilter iir_;
+
+ private:
+  double tail_time_;
 };
 
 }  // namespace blink
