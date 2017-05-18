96d4747e4cdacea179c7394c96a3ef66bcce04d3
mek <mek@chromium.org>
2017-05-18 09:49:40 -0700

    Add UMA histograms to mojo leveldb env
    This adds more or less the same UMA histograms that are logged by the non-mojo
    leveldb env to the mojo leveldb env.
    
    BUG=586194
    
    Review-Url: https://codereview.chromium.org/2752963002
    Cr-Commit-Position: refs/heads/master@{#472844}

--- components/leveldb/env_mojo.h
+++ components/leveldb/env_mojo.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/filesystem/public/interfaces/directory.mojom.h"
 #include "components/leveldb/leveldb_mojo_proxy.h"
+#include "third_party/leveldatabase/env_chromium.h"
 #include "third_party/leveldatabase/src/include/leveldb/env.h"
 
 namespace leveldb {
@@ -17,7 +18,7 @@ namespace leveldb {
 // synchronous and block on responses from the filesystem service. That's fine
 // since, for the most part, they merely open files or check for a file's
 // existence.
-class MojoEnv : public leveldb::Env {
+class MojoEnv : public Env, public leveldb_env::UMALogger {
  public:
   MojoEnv(scoped_refptr<LevelDBMojoProxy> file_thread,
           LevelDBMojoProxy::OpaqueDir* dir);
@@ -51,6 +52,13 @@ class MojoEnv : public leveldb::Env {
   void StartThread(void (*function)(void* arg), void* arg) override;
 
  private:
+  void RecordErrorAt(leveldb_env::MethodID method) const override;
+  void RecordOSError(leveldb_env::MethodID method,
+                     base::File::Error error) const override;
+
+  void RecordFileError(leveldb_env::MethodID method,
+                       filesystem::mojom::FileError error) const;
+
   scoped_refptr<LevelDBMojoProxy> thread_;
   LevelDBMojoProxy::OpaqueDir* dir_;
 
