9ea731aa82facd0e82a2c8b1c90849368d75491d
xiaochengh <xiaochengh@chromium.org>
2017-05-17 20:29:05 -0700

    Make EnabledRangeInEditableText return false for user-triggered command and unfocused selection
    Certain editing commands shouldn't be enabled when they are user-triggered and
    the selection doesn't have focus. This patch adds checking of
    FrameSelection::SelectionHasFocus() in EnabledRangeInEditableText() to fix
    the behavior of the RemoveFormat command.
    
    BUG=722925
    TEST=editing/execCommand/remove_format_with_unfocused_selection.html
    
    Review-Url: https://codereview.chromium.org/2893713003
    Cr-Commit-Position: refs/heads/master@{#472635}

--- third_party/WebKit/Source/core/editing/commands/EditorCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/EditorCommand.cpp
@@ -2107,8 +2107,11 @@ static bool EnabledPaste(LocalFrame& frame,
 
 static bool EnabledRangeInEditableText(LocalFrame& frame,
                                        Event*,
-                                       EditorCommandSource) {
+                                       EditorCommandSource source) {
   frame.GetDocument()->UpdateStyleAndLayoutIgnorePendingStylesheets();
+  if (source == kCommandFromMenuOrKeyBinding &&
+      !frame.Selection().SelectionHasFocus())
+    return false;
   return frame.Selection()
              .ComputeVisibleSelectionInDOMTreeDeprecated()
              .IsRange() &&
