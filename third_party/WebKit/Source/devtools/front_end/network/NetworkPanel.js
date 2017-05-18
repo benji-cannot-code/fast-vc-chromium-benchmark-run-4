8a166dee379a27a57149205d0a8217f3c857eb80
allada <allada@chromium.org>
2017-05-17 20:49:11 -0700

    [Devtools][Regression] Fixed network film strip sometimes errored
    Fixes bug where if the overviewPane was not shown the filmStrip would
    never show and throw an exception in network panel.
    
    Offending patch: https://codereview.chromium.org/2857833002/
    
    R=pfeldman
    BUG=723659
    
    Review-Url: https://codereview.chromium.org/2890943003
    Cr-Commit-Position: refs/heads/master@{#472651}

--- third_party/WebKit/Source/devtools/front_end/network/NetworkPanel.js
+++ third_party/WebKit/Source/devtools/front_end/network/NetworkPanel.js
@@ -52,6 +52,8 @@ Network.NetworkPanel = class extends UI.Panel {
     this._filterBar = new UI.FilterBar('networkPanel', true);
     this._filterBar.show(this.element);
 
+    this._filmStripPlaceholderElement = this.element.createChild('div');
+
     // Create top overview component.
     this._overviewPane = new PerfUI.TimelineOverviewPane('network');
     this._overviewPane.addEventListener(
@@ -59,6 +61,8 @@ Network.NetworkPanel = class extends UI.Panel {
     this._overviewPane.element.id = 'network-overview-panel';
     this._networkOverview = new Network.NetworkOverview();
     this._overviewPane.setOverviewControls([this._networkOverview]);
+    this._overviewPlaceholderElement = this.element.createChild('div');
+
     this._calculator = new Network.NetworkTransferTimeCalculator();
 
     this._splitWidget = new UI.SplitWidget(true, false, 'networkPanelSplitViewState');
@@ -297,7 +301,7 @@ Network.NetworkPanel = class extends UI.Panel {
   _toggleShowOverview() {
     var toggled = this._networkLogShowOverviewSetting.get();
     if (toggled)
-      this._overviewPane.show(this.element, this._splitWidget.element);
+      this._overviewPane.show(this._overviewPlaceholderElement);
     else
       this._overviewPane.detach();
     this.doResize();
@@ -311,7 +315,7 @@ Network.NetworkPanel = class extends UI.Panel {
       this._filmStripView.element.classList.add('network-film-strip');
       this._filmStripRecorder =
           new Network.NetworkPanel.FilmStripRecorder(this._networkLogView.timeCalculator(), this._filmStripView);
-      this._filmStripView.show(this.element, this._overviewPane.element);
+      this._filmStripView.show(this._filmStripPlaceholderElement);
       this._filmStripView.addEventListener(PerfUI.FilmStripView.Events.FrameSelected, this._onFilmFrameSelected, this);
       this._filmStripView.addEventListener(PerfUI.FilmStripView.Events.FrameEnter, this._onFilmFrameEnter, this);
       this._filmStripView.addEventListener(PerfUI.FilmStripView.Events.FrameExit, this._onFilmFrameExit, this);
