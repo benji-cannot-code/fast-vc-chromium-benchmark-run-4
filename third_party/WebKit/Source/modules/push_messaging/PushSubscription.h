5dfe1b651148b411d7d7e4a865ed0a041b018d0a
peter <peter@chromium.org>
2017-05-18 11:43:15 -0700

    Implement and ship PushSubscription.expirationTime
    This property tells developers the time after which the subscription
    will be deactivated, if any. Since we don't support refreshing
    subscriptions yet, it always returns NULL.
    
    Specification:
    https://w3c.github.io/push-api/#dom-pushsubscription-expirationtime
    
    Intent to Implement and Ship:
    https://groups.google.com/a/chromium.org/d/topic/blink-dev/xCTqjIDrOm4/discussion
    
    BUG=718837
    
    Review-Url: https://codereview.chromium.org/2864603003
    Cr-Commit-Position: refs/heads/master@{#472886}

--- third_party/WebKit/Source/modules/push_messaging/PushSubscription.h
+++ third_party/WebKit/Source/modules/push_messaging/PushSubscription.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/ScriptPromise.h"
 #include "bindings/core/v8/ScriptValue.h"
 #include "core/dom/DOMArrayBuffer.h"
+#include "core/dom/DOMTimeStamp.h"
 #include "platform/bindings/ScriptWrappable.h"
 #include "platform/heap/Handle.h"
 #include "platform/weborigin/KURL.h"
@@ -38,6 +39,7 @@ class PushSubscription final
   virtual ~PushSubscription();
 
   KURL endpoint() const { return endpoint_; }
+  DOMTimeStamp expirationTime(bool& out_is_null) const;
 
   PushSubscriptionOptions* options() const { return options_.Get(); }
 
