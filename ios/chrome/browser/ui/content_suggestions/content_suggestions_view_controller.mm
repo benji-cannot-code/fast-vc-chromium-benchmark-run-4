008efa0322d5640a966daca5f44f773f16fe1264
gambard <gambard@chromium.org>
2017-05-17 23:41:34 -0700

    Layout Most Visited for ContentSuggestions
    Layout the Most Visited tiles to have them displayed as wanted in the
    ContentSuggestions view.
    It displays them as individual cells, maximum 4 by rows.
    
    BUG=707754
    
    Review-Url: https://codereview.chromium.org/2887073002
    Cr-Commit-Position: refs/heads/master@{#472712}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.mm
@@ -5,10 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.h"
 
+#include "base/mac/foundation_util.h"
 #import "ios/chrome/browser/ui/collection_view/cells/MDCCollectionViewCell+Chrome.h"
 #import "ios/chrome/browser/ui/collection_view/cells/collection_view_item.h"
 #import "ios/chrome/browser/ui/collection_view/collection_view_model.h"
+#import "ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_most_visited_cell.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_updater.h"
+#import "ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_utils.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_commands.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -141,6 +144,44 @@ using CSCollectionViewItem = CollectionViewItem<SuggestedContent>;
     case ContentSuggestionTypeEmpty:
       break;
   }
+}
+
+#pragma mark - UICollectionViewDelegateFlowLayout
+
+- (CGSize)collectionView:(UICollectionView*)collectionView
+                    layout:(UICollectionViewLayout*)collectionViewLayout
+    sizeForItemAtIndexPath:(NSIndexPath*)indexPath {
+  if ([self.collectionUpdater isMostVisitedSection:indexPath.section]) {
+    return [ContentSuggestionsMostVisitedCell defaultSize];
+  }
+  return [super collectionView:collectionView
+                        layout:collectionViewLayout
+        sizeForItemAtIndexPath:indexPath];
+}
+
+- (UIEdgeInsets)collectionView:(UICollectionView*)collectionView
+                        layout:(UICollectionViewLayout*)collectionViewLayout
+        insetForSectionAtIndex:(NSInteger)section {
+  if ([self.collectionUpdater isMostVisitedSection:section]) {
+    CGFloat margin = content_suggestions::centeredTilesMarginForWidth(
+        collectionView.frame.size.width);
+    return UIEdgeInsetsMake(0, margin, 0, margin);
+  }
+  return [super collectionView:collectionView
+                        layout:collectionViewLayout
+        insetForSectionAtIndex:section];
+}
+
+- (CGFloat)collectionView:(UICollectionView*)collectionView
+                                 layout:(UICollectionViewLayout*)
+                                            collectionViewLayout
+    minimumLineSpacingForSectionAtIndex:(NSInteger)section {
+  if ([self.collectionUpdater isMostVisitedSection:section]) {
+    return content_suggestions::spacingBetweenTiles();
+  }
+  return [super collectionView:collectionView
+                                   layout:collectionViewLayout
+      minimumLineSpacingForSectionAtIndex:section];
 }
 
 #pragma mark - MDCCollectionViewStylingDelegate
