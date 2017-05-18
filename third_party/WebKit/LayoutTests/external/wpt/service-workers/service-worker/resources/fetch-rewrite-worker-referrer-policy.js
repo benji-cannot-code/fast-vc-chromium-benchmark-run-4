1533b042018d5dd27971c0d8510bd9a39abb4c79
mike <mike@mikepennisi.com>
2017-05-18 12:35:35 -0700

    Upstream service worker "referer" tests to WPT
    **referer**
    
    This test exists in both WPT and the Chromium project tree. They differ
    only in implementation details of the testing infrastructure.  However,
    the upstream version suffers from timeout issues in the build
    environment.
    
    Rename the Chromium-specific version to reflect its deprecated status,
    and re-enable the upstream version as an expected timeout.
    
    **referrer-policy-header**
    
    Move test file for automated contribution to the Web Platform Tests
    project. Update URLs to use contextual values. Express asynchronous
    sub-test using the `promise_test` function. Consistently remove iframe
    regardless of sub-test result. Specify necessary HTTP headers in a
    format that is supported by the Web Platform Tests project.
    
    BUG=688116, 658997
    R=falken@chromium.org
    
    Review-Url: https://codereview.chromium.org/2883373002
    Cr-Commit-Position: refs/heads/master@{#472902}

---
+++ third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/fetch-rewrite-worker-referrer-policy.js
@@ -0,0 +1,5 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// This script is intended to be served with the `Referrer-Policy` header as
+// defined in the corresponding `.headers` file.
+
+importScripts('fetch-rewrite-worker.js');
--- third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/fetch-rewrite-worker.php
+++
@@ -1,6 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<?
-header('Content-Type: application/javascript');
-header('Referrer-Policy: origin');
-?>
-importScripts('fetch-rewrite-worker.js');
