0fca59d5eb475e249f2df8e18fd8517029d1a728
liaoyuke <liaoyuke@chromium.org>
2017-05-18 09:25:47 -0700

    [ObjC ARC] Converts ios/chrome/test/base:perf_test_support to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2881273002
    Cr-Commit-Position: refs/heads/master@{#472837}

--- ios/chrome/test/base/perf_test_ios.mm
+++ ios/chrome/test/base/perf_test_ios.mm
@@ -8,6 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/web/chrome_web_client.h"
 #import "ios/chrome/test/base/perf_test_ios.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 PerfTest::PerfTest(std::string testGroup)
     : BlockCleanupTest(),
       testGroup_(testGroup),
