d7fec4b2193ea2b210d1a350babb5a75c46d9d07
timav <timav@chromium.org>
2017-05-17 20:42:22 -0700

    [SmartText selection] Implement TextClassifier getter and setter
    Pass the text classifier object by the chain
      AwContents -> ContentViewCore -> SelectionPopupController ->
      SmartSelectionClient -> SmartSelectionProvider.
    
    The classifier is supposed to be owned by the implementation of
    SmartSelectionProvider.
    
    Move the classifier object to the new popup contents in case of
    popup creation.
    
    BUG=710503
    
    Review-Url: https://codereview.chromium.org/2882583004
    Cr-Commit-Position: refs/heads/master@{#472643}

--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -1040,6 +1040,10 @@ public class AwContents implements SmartClipProvider {
      * ^^^^^^^^^  See the native class declaration for more details on relative object lifetimes.
      */
     private void setNewAwContents(long newAwContentsPtr) {
+        // Move the text classifier to the new ContentViewCore.
+        Object textClassifier =
+                mContentViewCore == null ? null : mContentViewCore.getCustomTextClassifier();
+
         if (mNativeAwContents != 0) {
             destroyNatives();
             mContentViewCore = null;
@@ -1071,6 +1075,8 @@ public class AwContents implements SmartClipProvider {
         mNavigationController = mWebContents.getNavigationController();
         installWebContentsObserver();
         mSettings.setWebContents(webContents);
+
+        if (textClassifier != null) mContentViewCore.setTextClassifier(textClassifier);
 
         final float dipScale = mWindowAndroid.getWindowAndroid().getDisplay().getDipScale();
         mDisplayObserver.onDIPScaleChanged(dipScale);
@@ -2713,6 +2719,19 @@ public class AwContents implements SmartClipProvider {
         nativeSetRendererPriorityPolicy(
                 mNativeAwContents, rendererRequestedPriority, waivedWhenNotVisible);
     }
+
+    // TODO(timav): Use |TextClassifier| instead of |Object| after we switch to Android SDK 26.
+    public void setTextClassifier(Object textClassifier) {
+        assert mContentViewCore != null;
+        mContentViewCore.setTextClassifier(textClassifier);
+    }
+
+    // TODO(timav): Use |TextClassifier| instead of |Object| after we switch to Android SDK 26.
+    public Object getTextClassifier() {
+        assert mContentViewCore != null;
+        return mContentViewCore.getTextClassifier();
+    }
+
     //--------------------------------------------------------------------------------------------
     //  Methods called from native via JNI
     //--------------------------------------------------------------------------------------------
