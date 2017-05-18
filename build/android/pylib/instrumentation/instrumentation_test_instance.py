965030bdb784f8471415fb7d6fa26868717e11b2
mikecase <mikecase@chromium.org>
2017-05-17 21:57:46 -0700

    Add --render-results-dir arg to store render results locally.
    If --render-results-dir is specified, the render result images
    will get pulled from the device and stored there locally.
    
    BUG=722779
    
    Review-Url: https://codereview.chromium.org/2889663003
    Cr-Commit-Position: refs/heads/master@{#472670}

--- build/android/pylib/instrumentation/instrumentation_test_instance.py
+++ build/android/pylib/instrumentation/instrumentation_test_instance.py
@@ -490,6 +490,8 @@ class InstrumentationTestInstance(test_instance.TestInstance):
     self._driver_name = None
     self._initializeDriverAttributes()
 
+    self._render_results_dir = None
+    self._screenshot_dir = None
     self._timeout_scale = None
     self._initializeTestControlAttributes(args)
 
@@ -667,6 +669,7 @@ class InstrumentationTestInstance(test_instance.TestInstance):
       self._driver_apk = None
 
   def _initializeTestControlAttributes(self, args):
+    self._render_results_dir = args.render_results_dir
     self._screenshot_dir = args.screenshot_dir
     self._timeout_scale = args.timeout_scale or 1
 
@@ -753,6 +756,10 @@ class InstrumentationTestInstance(test_instance.TestInstance):
   @property
   def package_info(self):
     return self._package_info
+
+  @property
+  def render_results_dir(self):
+    return self._render_results_dir
 
   @property
   def screenshot_dir(self):
