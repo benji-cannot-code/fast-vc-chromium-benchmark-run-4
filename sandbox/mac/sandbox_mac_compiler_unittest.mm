374e2bf7bd70adf87c90b91b5d71a8dd6e6998d1
kerrnel <kerrnel@chromium.org>
2017-05-17 21:17:33 -0700

    Add the SeatbeltExec classes to facilitate the V2 sandbox.
    This adds the SeatbeltExec classes and unit tests. These classes are
    used to pipe data, such as the sandbox profile and parameters, from the
    browser process to the helper process which launces the sandboxed
    renderers.
    
    BUG=689306
    
    Review-Url: https://codereview.chromium.org/2869203003
    Cr-Commit-Position: refs/heads/master@{#472658}

--- sandbox/mac/sandbox_mac_compiler_unittest.mm
+++ sandbox/mac/sandbox_mac_compiler_unittest.mm
@@ -22,6 +22,7 @@ class SandboxMacCompilerTest : public base::MultiProcessTest {};
 MULTIPROCESS_TEST_MAIN(BasicProfileProcess) {
   std::string profile =
       "(version 1)"
+      "(deny default (with no-log))"
       "(allow file-read* file-write* (literal \"/\"))";
 
   SandboxCompiler compiler(profile);
@@ -44,6 +45,7 @@ TEST_F(SandboxMacCompilerTest, BasicProfileTest) {
 MULTIPROCESS_TEST_MAIN(BasicProfileWithParamProcess) {
   std::string profile =
       "(version 1)"
+      "(deny default (with no-log))"
       "(allow file-read* file-write* (literal (param \"DIR\")))";
 
   SandboxCompiler compiler(profile);
@@ -68,7 +70,7 @@ TEST_F(SandboxMacCompilerTest, BasicProfileTestWithParam) {
 MULTIPROCESS_TEST_MAIN(ProfileFunctionalProcess) {
   std::string profile =
       "(version 1)"
-      "(debug deny)"
+      "(deny default (with no-log))"
       "(allow file-read-data file-read-metadata (literal \"/dev/urandom\"))";
 
   SandboxCompiler compiler(profile);
@@ -99,7 +101,7 @@ TEST_F(SandboxMacCompilerTest, ProfileFunctionalityTest) {
 MULTIPROCESS_TEST_MAIN(ProfileFunctionalTestWithParamsProcess) {
   std::string profile =
       "(version 1)"
-      "(debug deny)"
+      "(deny default (with no-log))"
       "(if (string=? (param \"ALLOW_FILE\") \"TRUE\")"
       "    (allow file-read-data file-read-metadata (literal (param "
       "\"URANDOM\"))))";
