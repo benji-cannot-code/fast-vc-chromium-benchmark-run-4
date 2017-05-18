e70800778140f451a76b4844ef861b0a2978f8eb
mike <mike@mikepennisi.com>
2017-05-18 08:29:18 -0700

    Upstream service wrkr "registration" tests to WPT
    **registration**
    
    This test file was upstreamed to the Web Platform Tests project in
    Chromium commit e46007715a723c2219f49db6199f1af09cbbc7db, but the
    associated "expectations" file was mistakenly persisted. Remove the
    expectations file.
    
    **registration-end-to-end**
    
    The version of this test in the Chromium source tree is equivalent to
    the version available in the Web Platform Tests project. Remove the
    Chromium version.
    
    **registration-events**
    
    The version of this test in the Chromium source tree is equivalent to
    the version available in the Web Platform Tests project. Remove the
    Chromium version.
    
    **registration-service-worker-attributes**
    
    The version defined by the Chromium project is more strict in that it
    does not allow workers to transition between states before Promise
    resolution microtasks have been executed. Because any such transition
    would be in violation of the service workers specification, the
    additional strictness is preferable.
    
    Remove the logic that relaxes the WPT version of this test, and remove
    the Chromium test file entirely.
    
    BUG=688116
    R=falken@chromium.org
    
    Review-Url: https://codereview.chromium.org/2893743003
    Cr-Commit-Position: refs/heads/master@{#472821}

--- third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/end-to-end-worker.js
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-onmessage = function(e) {
-  var message = e.data;
-  if (typeof message === 'object' && 'port' in message) {
-    var response = 'Ack for: ' + message.from;
-    message.port.postMessage(response);
-  }
-};
