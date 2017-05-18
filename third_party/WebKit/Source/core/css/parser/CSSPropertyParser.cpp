e0c37fa90264e2d8f337aaebd7d0db662269ab38
bugsnash <bugsnash@chromium.org>
2017-05-18 15:05:43 -0700

    Refactored out need to pass unresolved property id to ConsumePerspective
    BUG=668012
    
    Review-Url: https://codereview.chromium.org/2892483004
    Cr-Commit-Position: refs/heads/master@{#472946}

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
@@ -762,13 +762,12 @@ static CSSValue* ConsumeNoneOrURI(CSSParserTokenRange& range,
 
 static CSSValue* ConsumePerspective(CSSParserTokenRange& range,
                                     const CSSParserContext* context,
-                                    CSSPropertyID unresolved_property) {
+                                    bool use_legacy_parsing) {
   if (range.Peek().Id() == CSSValueNone)
     return ConsumeIdent(range);
   CSSPrimitiveValue* parsed_value =
       ConsumeLength(range, context->Mode(), kValueRangeAll);
-  if (!parsed_value &&
-      (unresolved_property == CSSPropertyAliasWebkitPerspective)) {
+  if (!parsed_value && use_legacy_parsing) {
     double perspective;
     if (!ConsumeNumberRaw(range, perspective))
       return nullptr;
@@ -1766,7 +1765,9 @@ const CSSValue* CSSPropertyParser::ParseSingleValue(
       return ConsumeLengthOrPercent(range_, kSVGAttributeMode, kValueRangeAll,
                                     UnitlessQuirk::kForbid);
     case CSSPropertyPerspective:
-      return ConsumePerspective(range_, context_, unresolved_property);
+      return ConsumePerspective(
+          range_, context_,
+          unresolved_property == CSSPropertyAliasWebkitPerspective);
     case CSSPropertyScrollSnapPointsX:
     case CSSPropertyScrollSnapPointsY:
       return ConsumeScrollSnapPoints(range_, context_->Mode());
