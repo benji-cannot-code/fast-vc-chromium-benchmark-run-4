3932bbc1b0268c555caca574cacdc7be95e988b0
shend <shend@chromium.org>
2017-05-18 05:22:03 -0700

    Remove PassRefPtr on setters in ComputedStyle.
    As we are going to remove PassRefPtr, this patch replaces uses of
    PassRefPtr with RefPtr in ComputedStyle setters.
    
    BUG=494719
    
    Review-Url: https://codereview.chromium.org/2881143004
    Cr-Commit-Position: refs/heads/master@{#472778}

--- third_party/WebKit/Source/core/style/ComputedStyle.cpp
+++ third_party/WebKit/Source/core/style/ComputedStyle.cpp
@@ -415,7 +415,7 @@ ComputedStyle* ComputedStyle::GetCachedPseudoStyle(PseudoId pid) const {
 }
 
 ComputedStyle* ComputedStyle::AddCachedPseudoStyle(
-    PassRefPtr<ComputedStyle> pseudo) {
+    RefPtr<ComputedStyle> pseudo) {
   if (!pseudo)
     return 0;
 
@@ -1092,7 +1092,7 @@ void ComputedStyle::SetCursorList(CursorList* other) {
   rare_inherited_data_.Access()->cursor_data_ = other;
 }
 
-void ComputedStyle::SetQuotes(PassRefPtr<QuotesData> q) {
+void ComputedStyle::SetQuotes(RefPtr<QuotesData> q) {
   rare_inherited_data_.Access()->quotes_ = std::move(q);
 }
 
@@ -1372,7 +1372,7 @@ void ComputedStyle::ApplyMotionPathTransform(
     transform.Translate(-origin_shift_x, -origin_shift_y);
 }
 
-void ComputedStyle::SetTextShadow(PassRefPtr<ShadowList> s) {
+void ComputedStyle::SetTextShadow(RefPtr<ShadowList> s) {
   rare_inherited_data_.Access()->text_shadow_ = std::move(s);
 }
 
@@ -1380,7 +1380,7 @@ bool ComputedStyle::TextShadowDataEquivalent(const ComputedStyle& other) const {
   return DataEquivalent(TextShadow(), other.TextShadow());
 }
 
-void ComputedStyle::SetBoxShadow(PassRefPtr<ShadowList> s) {
+void ComputedStyle::SetBoxShadow(RefPtr<ShadowList> s) {
   rare_non_inherited_data_.Access()->box_shadow_ = std::move(s);
 }
 
@@ -1767,29 +1767,28 @@ StyleNonInheritedVariables& ComputedStyle::MutableNonInheritedVariables() {
 
 void ComputedStyle::SetUnresolvedInheritedVariable(
     const AtomicString& name,
-    PassRefPtr<CSSVariableData> value) {
+    RefPtr<CSSVariableData> value) {
   DCHECK(value && value->NeedsVariableResolution());
   MutableInheritedVariables().SetVariable(name, std::move(value));
 }
 
 void ComputedStyle::SetUnresolvedNonInheritedVariable(
     const AtomicString& name,
-    PassRefPtr<CSSVariableData> value) {
+    RefPtr<CSSVariableData> value) {
   DCHECK(value && value->NeedsVariableResolution());
   MutableNonInheritedVariables().SetVariable(name, std::move(value));
 }
 
 void ComputedStyle::SetResolvedUnregisteredVariable(
     const AtomicString& name,
-    PassRefPtr<CSSVariableData> value) {
+    RefPtr<CSSVariableData> value) {
   DCHECK(value && !value->NeedsVariableResolution());
   MutableInheritedVariables().SetVariable(name, std::move(value));
 }
 
-void ComputedStyle::SetResolvedInheritedVariable(
-    const AtomicString& name,
-    PassRefPtr<CSSVariableData> value,
-    const CSSValue* parsed_value) {
+void ComputedStyle::SetResolvedInheritedVariable(const AtomicString& name,
+                                                 RefPtr<CSSVariableData> value,
+                                                 const CSSValue* parsed_value) {
   DCHECK(!!value == !!parsed_value);
   DCHECK(!(value && value->NeedsVariableResolution()));
 
@@ -1800,7 +1799,7 @@ void ComputedStyle::SetResolvedInheritedVariable(
 
 void ComputedStyle::SetResolvedNonInheritedVariable(
     const AtomicString& name,
-    PassRefPtr<CSSVariableData> value,
+    RefPtr<CSSVariableData> value,
     const CSSValue* parsed_value) {
   DCHECK(!!value == !!parsed_value);
   DCHECK(!(value && value->NeedsVariableResolution()));
@@ -2310,7 +2309,7 @@ void ComputedStyle::SetMarginEnd(const Length& margin) {
   }
 }
 
-void ComputedStyle::SetOffsetPath(PassRefPtr<BasicShape> path) {
+void ComputedStyle::SetOffsetPath(RefPtr<BasicShape> path) {
   rare_non_inherited_data_.Access()->transform_.Access()->motion_.path_ =
       std::move(path);
 }
