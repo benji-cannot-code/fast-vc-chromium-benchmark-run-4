8fe8007d242e761aa17ac3835732fa7ebb7b5229
qyearsley <qyearsley@chromium.org>
2017-05-19 14:56:00 -0700

    Refactoring: Extract some logic from RebaselineCL.execute.
    Specifically, this CL extracts the methods:
    
      _log_pending_builds
      _make_test_baseline_set_for_tests
    
    And modifies trigger_builds.
    
    The purpose of this change is to simplify the main execute() method and move some details into helper methods.
    
    Review-Url: https://codereview.chromium.org/2890373003
    Cr-Commit-Position: refs/heads/master@{#473346}

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/commands/rebaseline_cl_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/commands/rebaseline_cl_unittest.py
@@ -243,7 +243,8 @@ class RebaselineCLTest(BaseTestCase, LoggingTestCase):
         self.assertLog([
             'INFO: Triggering try jobs for:\n',
             'INFO:   MOCK Try Linux\n',
-            'INFO: Please re-run webkit-patch rebaseline-cl once all pending try jobs have finished.\n',
+            'INFO: Once all pending try jobs have finished, please re-run\n'
+            'webkit-patch rebaseline-cl to fetch new baselines.\n',
         ])
         self.assertEqual(
             self.tool.executive.calls,
@@ -305,6 +306,8 @@ class RebaselineCLTest(BaseTestCase, LoggingTestCase):
             'INFO: Triggering try jobs for:\n',
             'INFO:   MOCK Try Linux\n',
             'INFO:   MOCK Try Win\n',
+            'INFO: Once all pending try jobs have finished, please re-run\n'
+            'webkit-patch rebaseline-cl to fetch new baselines.\n',
         ])
 
     def test_builders_with_pending_builds(self):
