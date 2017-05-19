564275dca8f52cd95d75680d8fa8379eeeae7d5f
shend <shend@chromium.org>
2017-05-18 18:23:29 -0700

    Generate getters/setters for VisitedLinkColor.
    Currently, the VisitedLinkColor property is a 'storage_only' field, so
    it has no generated public getters/setters. This patch changes it to an
    'external' field so that their getters/setters can be generated as well.
    
    Diff of generated files:
    https://gist.github.com/darrnshn/66f19ed3bd09d0744ffc292f3210b479/revisions
    
    BUG=628043
    
    Review-Url: https://codereview.chromium.org/2844253004
    Cr-Commit-Position: refs/heads/master@{#473013}

--- third_party/WebKit/Source/core/style/ComputedStyle.h
+++ third_party/WebKit/Source/core/style/ComputedStyle.h
@@ -3448,7 +3448,6 @@ class CORE_EXPORT ComputedStyle : public ComputedStyleBase<ComputedStyle>,
                               ApplyMotionPath) const;
 
  private:
-  void SetVisitedLinkColor(const Color&);
   void SetVisitedLinkBackgroundColor(const StyleColor& v) {
     SET_VAR(rare_non_inherited_data_, visited_link_background_color_, v);
   }
@@ -3603,7 +3602,6 @@ class CORE_EXPORT ComputedStyle : public ComputedStyleBase<ComputedStyle>,
       return StyleAutoColor::AutoColor();
     return StyleAutoColor(rare_inherited_data_->visited_link_caret_color_);
   }
-  Color VisitedLinkColor() const;
   StyleColor VisitedLinkBackgroundColor() const {
     return rare_non_inherited_data_->visited_link_background_color_;
   }
