a02b27b0642e3cbb69b3e579c1876a15b24e8dd1
bnc <bnc@chromium.org>
2017-05-19 10:56:15 -0700

    Return Request as unique_ptr from JobController::Start().
    Also return HttpStreamRequest as unique_ptr from RequestStream(),
    RequestWebSocketHandshakeStream(), RequestBidirectionalStreamImpl(), and
    RequestStreamInternal().
    
    BUG=475060
    
    Review-Url: https://codereview.chromium.org/2895613002
    Cr-Commit-Position: refs/heads/master@{#473241}

--- net/http/http_stream_factory_impl.h
+++ net/http/http_stream_factory_impl.h
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include <map>
+#include <memory>
 #include <set>
+#include <string>
 #include <vector>
 
 #include "base/gtest_prod_util.h"
@@ -45,16 +47,17 @@ class NET_EXPORT_PRIVATE HttpStreamFactoryImpl : public HttpStreamFactory {
   ~HttpStreamFactoryImpl() override;
 
   // HttpStreamFactory interface
-  HttpStreamRequest* RequestStream(const HttpRequestInfo& info,
-                                   RequestPriority priority,
-                                   const SSLConfig& server_ssl_config,
-                                   const SSLConfig& proxy_ssl_config,
-                                   HttpStreamRequest::Delegate* delegate,
-                                   bool enable_ip_based_pooling,
-                                   bool enable_alternative_services,
-                                   const NetLogWithSource& net_log) override;
+  std::unique_ptr<HttpStreamRequest> RequestStream(
+      const HttpRequestInfo& info,
+      RequestPriority priority,
+      const SSLConfig& server_ssl_config,
+      const SSLConfig& proxy_ssl_config,
+      HttpStreamRequest::Delegate* delegate,
+      bool enable_ip_based_pooling,
+      bool enable_alternative_services,
+      const NetLogWithSource& net_log) override;
 
-  HttpStreamRequest* RequestWebSocketHandshakeStream(
+  std::unique_ptr<HttpStreamRequest> RequestWebSocketHandshakeStream(
       const HttpRequestInfo& info,
       RequestPriority priority,
       const SSLConfig& server_ssl_config,
@@ -65,7 +68,7 @@ class NET_EXPORT_PRIVATE HttpStreamFactoryImpl : public HttpStreamFactory {
       bool enable_alternative_services,
       const NetLogWithSource& net_log) override;
 
-  HttpStreamRequest* RequestBidirectionalStreamImpl(
+  std::unique_ptr<HttpStreamRequest> RequestBidirectionalStreamImpl(
       const HttpRequestInfo& info,
       RequestPriority priority,
       const SSLConfig& server_ssl_config,
@@ -121,7 +124,7 @@ class NET_EXPORT_PRIVATE HttpStreamFactoryImpl : public HttpStreamFactory {
     MAX_ALTERNATIVE_SERVICE_TYPE
   };
 
-  HttpStreamRequest* RequestStreamInternal(
+  std::unique_ptr<HttpStreamRequest> RequestStreamInternal(
       const HttpRequestInfo& info,
       RequestPriority priority,
       const SSLConfig& server_ssl_config,
