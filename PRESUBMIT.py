6fbd136a7280059d11abbcad06f07f7a8738ddd9
ricea <ricea@chromium.org>
2017-05-19 05:39:24 -0700

    Change PRESUBMIT warning text for obsolete predeclaration
    When a patch removes the code that required a predeclaration the PRESUBMIT would
    print a warning with a message like "ResourceRequestInfo forward declaration is
    becoming useless". I didn't understand the meaning of the message.
    
    Change the message to "ResourceRequestInfo forward declaration is no longer
    needed" which hopefully will be easier to understand.
    
    BUG=662195
    
    Review-Url: https://codereview.chromium.org/2890413002
    Cr-Commit-Position: refs/heads/master@{#473169}

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -1733,7 +1733,7 @@ def _CheckUselessForwardDeclarations(input_api, output_api):
         for decl in useless_fwd_decls:
           if input_api.re.search(r'\b%s\b' % decl, line[1:]):
             results.append(output_api.PresubmitPromptWarning(
-              '%s: %s forward declaration is becoming useless' %
+              '%s: %s forward declaration is no longer needed' %
               (f.LocalPath(), decl)))
             useless_fwd_decls.remove(decl)
 
