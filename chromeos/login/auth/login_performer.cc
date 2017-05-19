b43b9a8b05ea462d150c1461941a57848cbf716f
peary2 <peary2@gmail.com>
2017-05-18 17:38:52 -0700

    Rename TaskRunner::RunsTasksOnCurrentThread() in //dbus, //chromeos, //ppapi
    renamed TaskRunner::RunsTasksOnCurrentThread() to
    TaskRunner::RunsTasksInCurrentSequence() in //dbus, //chromeos, //ppapi
    
    BUG=665062
    
    Review-Url: https://codereview.chromium.org/2886913005
    Cr-Commit-Position: refs/heads/master@{#472997}

--- chromeos/login/auth/login_performer.cc
+++ chromeos/login/auth/login_performer.cc
@@ -54,7 +54,7 @@ LoginPerformer::~LoginPerformer() {
 // LoginPerformer, AuthStatusConsumer implementation:
 
 void LoginPerformer::OnAuthFailure(const AuthFailure& failure) {
-  DCHECK(task_runner_->RunsTasksOnCurrentThread());
+  DCHECK(task_runner_->RunsTasksInCurrentSequence());
   base::RecordAction(UserMetricsAction("Login_Failure"));
 
   UMA_HISTOGRAM_ENUMERATION("Login.FailureReason",
@@ -78,7 +78,7 @@ void LoginPerformer::OnAuthFailure(const AuthFailure& failure) {
 }
 
 void LoginPerformer::OnAuthSuccess(const UserContext& user_context) {
-  DCHECK(task_runner_->RunsTasksOnCurrentThread());
+  DCHECK(task_runner_->RunsTasksInCurrentSequence());
   base::RecordAction(UserMetricsAction("Login_Success"));
 
   // Do not distinguish between offline and online success.
@@ -94,7 +94,7 @@ void LoginPerformer::OnAuthSuccess(const UserContext& user_context) {
 }
 
 void LoginPerformer::OnOffTheRecordAuthSuccess() {
-  DCHECK(task_runner_->RunsTasksOnCurrentThread());
+  DCHECK(task_runner_->RunsTasksInCurrentSequence());
   base::RecordAction(UserMetricsAction("Login_GuestLoginSuccess"));
 
   if (delegate_)
@@ -115,7 +115,7 @@ void LoginPerformer::OnPasswordChangeDetected() {
 
 void LoginPerformer::OnOldEncryptionDetected(const UserContext& user_context,
                                              bool has_incomplete_migration) {
-  DCHECK(task_runner_->RunsTasksOnCurrentThread());
+  DCHECK(task_runner_->RunsTasksInCurrentSequence());
 
   if (delegate_)
     delegate_->OnOldEncryptionDetected(user_context, has_incomplete_migration);
