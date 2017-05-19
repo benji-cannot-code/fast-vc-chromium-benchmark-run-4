8e605a26c021d0f15280a18b18f93357e094b0d3
wzang <wzang@chromium.org>
2017-05-18 23:50:58 -0700

    Login screen redesign (CL 2)
    Make the lock screen PIN keyboard look the same with the mock here:
    https://gallery.googleplex.com/projects/MCHbtQVoQ2HCZdwfnbqy2eSi/files/MCEk7MHTx6J1ur9AuP2SqZvcwMSJbxAVNpw
    
    It does not change the underlying implementation of PIN keyboard at all.
    
    BUG=718174
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2881243002
    Cr-Commit-Position: refs/heads/master@{#473110}

--- chrome/browser/resources/chromeos/login/md_lock.js
+++ chrome/browser/resources/chromeos/login/md_lock.js
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview Login UI based on a stripped down OOBE controller.
  */
 
-// <include src="login_shared.js">
+// <include src="md_login_shared.js">
 
 /**
  * Ensures that the pin keyboard is loaded.
