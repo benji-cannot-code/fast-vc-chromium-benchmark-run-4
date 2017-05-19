2402aacb3c72a7940877465eb88298491da16516
pkl <pkl@chromium.org>
2017-05-19 14:20:50 -0700

    IWYU: Removed some unnecessary includes (mailto_handler*)
    Review-Url: https://codereview.chromium.org/2898483002
    Cr-Commit-Position: refs/heads/master@{#473322}

--- ios/chrome/browser/web/mailto_handler_gmail_unittest.mm
+++ ios/chrome/browser/web/mailto_handler_gmail_unittest.mm
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/gtest_mac.h"
-#include "url/gurl.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
