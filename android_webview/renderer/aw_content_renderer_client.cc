f4af166d6b0f95ef0be429c6d7520b47358325e0
ctzsm <ctzsm@chromium.org>
2017-05-19 13:36:01 -0700

    [WebView] Merge AwMetricsServiceClient and move few files to new place
    This CL:
    
    1) Move files around to let them make more sense.
    browser/net_init_native_callback.cc -> browser/net/init_native_callback.cc
    browser/aw_resource.cc -> common/aw_resource.cc
    
    2) Removed aw_metrics_service_client.{h,cc} and
    use aw_metrics_service_client_impl.{h,cc} to replace them.
    
    Hitting the same banned function usage:
    Use base/task_scheduler/post_task.h instead of the blocking pool.
    
    Hence requires no presubmit check.
    
    NOPRESUBMIT=true
    BUG=716604
    
    Review-Url: https://codereview.chromium.org/2886283005
    Cr-Commit-Position: refs/heads/master@{#473299}

--- android_webview/renderer/aw_content_renderer_client.cc
+++ android_webview/renderer/aw_content_renderer_client.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
-#include "android_webview/common/aw_resource.h"
 #include "android_webview/common/aw_switches.h"
 #include "android_webview/common/render_view_messages.h"
 #include "android_webview/common/url_constants.h"
