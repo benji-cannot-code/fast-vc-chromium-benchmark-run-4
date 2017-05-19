9ff59c34858323a21f2c9bd50beea2094b48ceac
Marc Treib <treib@chromium.org>
2017-05-19 09:47:34 +0000

    Revert "Import wpt@edf1d4d14d8ee7ff5f0f306ed83dfbb1325d0bd7 (manual)"
    This reverts commit 398df0990bd74c82c88e43172e572ff64d72667c.
    
    Reason for revert: external/wpt/resource-timing/resource-timing.html is failing on WebKit Linux Trusty:
    https://uberchromegw.corp.google.com/i/chromium.webkit/builders/WebKit%20Linux%20Trusty%20%28dbg%29
    
    (3 out of 3 runs failed right now)
    
    Original change's description:
    > Import wpt@edf1d4d14d8ee7ff5f0f306ed83dfbb1325d0bd7 (manual)
    > 
    > Using wpt-import in Chromium e6bdad2acc6f745d504782ca9e3655d2570393e7.
    > 
    > NOEXPORT=true
    > 
    > Change-Id: I7a9023eaaadc6b7543c6adad30b807cabab212ff
    > Reviewed-on: https://chromium-review.googlesource.com/508160
    > Reviewed-by: Jeff Carpenter <jeffcarp@chromium.org>
    > Commit-Queue: Quinten Yearsley <qyearsley@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#472944}
    
    TBR=qyearsley@chromium.org,jeffcarp@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Change-Id: I0c2fedffbfcdad48ca86bfb7b34b2bc8cdf3b4fc
    Reviewed-on: https://chromium-review.googlesource.com/509250
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#473142}

--- third_party/WebKit/LayoutTests/external/wpt/html/browsers/the-window-object/historical.window.js
+++
@@ -1,5 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-test(() => {
-  assert_false("showModalDialog" in window)
-  assert_false("showModalDialog" in Window.prototype)
-}, "showModalDialog() has been removed from the platform")
--- third_party/WebKit/LayoutTests/external/wpt/workers/support/shared-name.js
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-"use strict";
-
-self.counter = 0;
-
-self.onconnect = e => {
-  ++self.counter;
-  e.source.postMessage({ counter: self.counter, name: self.name });
-};
