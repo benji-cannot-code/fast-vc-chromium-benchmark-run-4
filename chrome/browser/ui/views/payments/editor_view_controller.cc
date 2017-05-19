88020134c200f5541c6cb415b921b3195cbbd199
mad <mad@chromium.org>
2017-05-19 07:45:09 -0700

    Change text and disable empty combobox for payment request shipping address
    BUG=721500
    
    Review-Url: https://codereview.chromium.org/2888733004
    Cr-Commit-Position: refs/heads/master@{#473186}

--- chrome/browser/ui/views/payments/editor_view_controller.cc
+++ chrome/browser/ui/views/payments/editor_view_controller.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/payments/editor_view_controller.h"
 
+#include <algorithm>
 #include <map>
 #include <memory>
 #include <utility>
