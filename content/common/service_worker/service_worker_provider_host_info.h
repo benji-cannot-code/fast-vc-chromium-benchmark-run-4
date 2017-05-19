8e7cec497c7b65ea9b6292a2646ba85ddc3147ab
shimazu <shimazu@chromium.org>
2017-05-19 05:19:19 -0700

    ServiceWorker: Keep info as a member of ServiceWorkerProviderHost
    This patch is to use SWProviderHostInfo as an argument of ctor of
    SWProviderHost. SWProvider directly keeps the passed SWProviderHostInfo as a
    member.
    
    This patch split off from https://crrev.com/2653493009.
    
    BUG=N/A
    
    Review-Url: https://codereview.chromium.org/2891903002
    Cr-Commit-Position: refs/heads/master@{#473166}

--- content/common/service_worker/service_worker_provider_host_info.h
+++ content/common/service_worker/service_worker_provider_host_info.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+// Container object to create a ServiceWorkerProviderHost.
 struct CONTENT_EXPORT ServiceWorkerProviderHostInfo {
   ServiceWorkerProviderHostInfo();
   ServiceWorkerProviderHostInfo(ServiceWorkerProviderHostInfo&& other);
