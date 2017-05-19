e20e066f2d7c054d5e711196be7762e51f974ef8
Tsuyoshi Horo <horo@chromium.org>
2017-05-19 13:31:16 +0900

    Set the requester context info to the requests for off-main-thread-fetch.
    This CL pipes the IsOriginSecure state to WorkerFetchContext and set it to
    the RequestExtraData at WorkerFetchContextImpl::WillSendRequest().
    And WorkerFetchContext::PrepareRequest() sets the RequestorOrigin of requests.
    
    After this cl, foreign fetch tests will pass.
    
    BUG=443374
    R=falken@chromium.org, kinuko@chromium.org
    
    Review-Url: https://codereview.chromium.org/2890723002 .
    Cr-Commit-Position: refs/heads/master@{#473086}

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -3038,6 +3038,8 @@ RenderFrameImpl::CreateWorkerFetchContext() {
   worker_fetch_context->set_parent_frame_id(routing_id_);
   worker_fetch_context->set_first_party_for_cookies(
       frame_->GetDocument().FirstPartyForCookies());
+  worker_fetch_context->set_is_secure_context(
+      frame_->GetDocument().IsSecureContext());
   blink::WebServiceWorkerNetworkProvider* web_provider =
       frame_->DataSource()->GetServiceWorkerNetworkProvider();
   if (web_provider) {
