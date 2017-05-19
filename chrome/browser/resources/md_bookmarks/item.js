4951088d0be5989b3d9f92a0c77dfc29fccf90bd
tsergeant <tsergeant@chromium.org>
2017-05-19 07:12:34 -0700

    MD Bookmarks: Add 'Open' command, to open in either the BMM or in new tabs
    This command is triggered by double clicking selected items, or by
    pressing Enter (non-Mac) or Meta-Down (Mac).
    
    If a single folder is selected, the command will open that folder in the
    bookmark manager. Otherwise, it will open all selected bookmarks in
    foreground tabs.
    
    BUG=692837
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2872163002
    Cr-Commit-Position: refs/heads/master@{#473180}

--- chrome/browser/resources/md_bookmarks/item.js
+++ chrome/browser/resources/md_bookmarks/item.js
@@ -38,6 +38,9 @@ Polymer({
 
     /** @private */
     isFolder_: Boolean,
+
+    /** @private */
+    openItemUrl_: String,
   },
 
   observers: [
@@ -91,6 +94,7 @@ Polymer({
    */
   onMenuButtonClick_: function(e) {
     e.stopPropagation();
+    e.preventDefault();
     this.dispatch(bookmarks.actions.selectItem(
         this.itemId, false, false, this.getState()));
     this.fire('open-item-menu', {
@@ -117,6 +121,10 @@ Polymer({
   /** @private */
   onItemChanged_: function() {
     this.isFolder_ = !this.item_.url;
+    if (this.item_.url)
+      this.openItemUrl_ = this.item_.url;
+    else
+      this.openItemUrl_ = 'chrome://bookmarks/?id=' + this.itemId;
   },
 
   /**
@@ -134,26 +142,25 @@ Polymer({
   },
 
   /**
-   * @param {Event} e
+   * @param {MouseEvent} e
    * @private
    */
   onClick_: function(e) {
     this.dispatch(bookmarks.actions.selectItem(
         this.itemId, e.ctrlKey, e.shiftKey, this.getState()));
     e.stopPropagation();
+    e.preventDefault();
   },
 
   /**
-   * @param {Event} e
+   * @param {MouseEvent} e
    * @private
    */
   onDblClick_: function(e) {
-    if (!this.item_.url) {
-      this.dispatch(
-          bookmarks.actions.selectFolder(this.item_.id, this.getState().nodes));
-    } else {
-      chrome.tabs.create({url: this.item_.url});
-    }
+    var commandManager = bookmarks.CommandManager.getInstance();
+    var itemSet = this.getState().selection.items;
+    if (commandManager.canExecute(Command.OPEN, itemSet))
+      commandManager.handle(Command.OPEN, itemSet);
   },
 
   /**
