fca10b24634c5a98fdf46faab3bee2aeb0b7cfdd
Daniel Cheng <dcheng@chromium.org>
2017-05-19 01:36:13 -0700

    Remove RenderViewObserver::DidFinishLoad
    One "browser test" uses this, so switch it over to FrameLoadWaiter.
    FrameLoadWaiter also has a few minor changes to make it easier to use
    correctly:
    - it allows nested tasks while waiting for the load event, to support
      single-process tests.
    - it exits Wait() immediately in case a load somehow completes
      synchronously.
    
    Bug: 304341
    Change-Id: I47add64a0782bdefd248837e393a2315703c3490
    Reviewed-on: https://chromium-review.googlesource.com/508064
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#473143}

--- content/public/test/frame_load_waiter.h
+++ content/public/test/frame_load_waiter.h
@@ -18,6 +18,10 @@ namespace content {
 class FrameLoadWaiter : public RenderFrameObserver {
  public:
   explicit FrameLoadWaiter(RenderFrame* frame);
+
+  // Note: single-process browser tests need to enable nestable tasks by
+  // instantiating a base::MessageLoop::ScopedNestableTaskAllower or this method
+  // will never return.
   void Wait();
 
  private:
@@ -26,6 +30,7 @@ class FrameLoadWaiter : public RenderFrameObserver {
   void OnDestruct() override;
 
   base::RunLoop run_loop_;
+  bool did_load_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(FrameLoadWaiter);
 };
--- content/renderer/dom_serializer_browsertest.cc
+++ content/renderer/dom_serializer_browsertest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/hash_tables.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
+#include "base/message_loop/message_loop.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/thread_restrictions.h"
@@ -19,10 +20,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_switches.h"
+#include "content/public/renderer/render_frame.h"
 #include "content/public/renderer/render_view.h"
-#include "content/public/renderer/render_view_observer.h"
 #include "content/public/test/content_browser_test.h"
 #include "content/public/test/content_browser_test_utils.h"
+#include "content/public/test/frame_load_waiter.h"
 #include "content/public/test/test_utils.h"
 #include "content/renderer/savable_resources.h"
 #include "content/shell/browser/shell.h"
@@ -65,24 +67,6 @@ bool HasDocType(const WebDocument& doc) {
   return doc.FirstChild().IsDocumentTypeNode();
 }
 
-class LoadObserver : public RenderViewObserver {
- public:
-  LoadObserver(RenderView* render_view, const base::Closure& quit_closure)
-      : RenderViewObserver(render_view),
-        quit_closure_(quit_closure) {}
-
-  void DidFinishLoad(blink::WebLocalFrame* frame) override {
-    if (frame == render_view()->GetWebView()->MainFrame())
-      quit_closure_.Run();
-  }
-
- private:
-  // RenderViewObserver implementation.
-  void OnDestruct() override { delete this; }
-
-  base::Closure quit_closure_;
-};
-
 class DomSerializerTests : public ContentBrowserTest,
                            public WebFrameSerializerClient {
  public:
@@ -123,7 +107,9 @@ class DomSerializerTests : public ContentBrowserTest,
     return GetRenderView()->GetWebView();
   }
 
-  WebFrame* GetMainFrame() { return GetWebView()->MainFrame(); }
+  WebLocalFrame* GetMainFrame() {
+    return GetRenderView()->GetMainRenderFrame()->GetWebFrame();
+  }
 
   WebFrame* FindSubFrameByURL(const GURL& url) {
     for (WebFrame* frame = GetWebView()->MainFrame(); frame;
@@ -139,27 +125,19 @@ class DomSerializerTests : public ContentBrowserTest,
   void LoadContents(const std::string& contents,
                     const GURL& base_url,
                     const WebString encoding_info) {
-    scoped_refptr<MessageLoopRunner> runner = new MessageLoopRunner;
-    LoadObserver observer(GetRenderView(), runner->QuitClosure());
-
+    FrameLoadWaiter waiter(GetRenderView()->GetMainRenderFrame());
     // If input encoding is empty, use UTF-8 as default encoding.
     if (encoding_info.IsEmpty()) {
       GetMainFrame()->LoadHTMLString(contents, base_url);
     } else {
-      WebData data(contents.data(), contents.length());
-
       // Do not use WebFrame.LoadHTMLString because it assumes that input
       // html contents use UTF-8 encoding.
-      // TODO(darin): This should use WebFrame::loadData.
-      WebFrame* web_frame = GetMainFrame();
-
-      ASSERT_TRUE(web_frame != NULL);
-
-      web_frame->ToWebLocalFrame()->LoadData(data, "text/html", encoding_info,
-                                             base_url);
+      WebData data(contents.data(), contents.length());
+      GetMainFrame()->LoadData(data, "text/html", encoding_info, base_url);
     }
-
-    runner->Run();
+    base::MessageLoop::ScopedNestableTaskAllower allow(
+        base::MessageLoop::current());
+    waiter.Wait();
   }
 
   class SingleLinkRewritingDelegate
