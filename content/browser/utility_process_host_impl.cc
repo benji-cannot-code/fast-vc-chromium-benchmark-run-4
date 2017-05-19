1bbb227e9e6e1c5438661e3b8a70e9214d463cad
chfremer <chfremer@chromium.org>
2017-05-19 15:18:16 -0700

    [Mojo Video Capture] Hook up video capture service behind a feature flag
    This CL is part of the Mojo Video Capture work. For the bigger picture,
    see [1] CL25.
    
    After this CL, the video capture service can be enabled by starting Chrome with
    command-line flag --enable-features=MojoVideoCapture.
    
    Changes in this CL:
    * Add a base::Feature kMojoVideoCapture to allow switching the service on and
      off.
    * Add a class VideoCaptureProviderSwitcher that can route device capture
      requests to the service's VideoCaptureProvider while routing screen capture
      requests to the InProcessVideoCaptureProvider.
    * Hook up the VideoCaptureProviderSwitcher in the factory code in
      MediaStreamManager.
    * Register video capture service in the service manager context for the Browser
      process.
    * Remove build flag "enable_mojo_video_capture", since we want the service to
      be testable in all builds.
    * Forward switches for using fake video capture devices to utility processes.
    * Add test cases for exercising the service to VideoCaptureBrowserTest.
    
    BUG=584797
    TEST=
      content_browsertests --gtest_filter="VideoCaptureBrowserTest.*"
    
    [1] https://docs.google.com/a/chromium.org/document/d/1Qw7rw1AJy0QHXjha36jZNiEuxsxWslJ_X-zpOhijvI8/edit?usp=sharing
    
    Review-Url: https://codereview.chromium.org/2867213004
    Cr-Commit-Position: refs/heads/master@{#473358}

--- content/browser/utility_process_host_impl.cc
+++ content/browser/utility_process_host_impl.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/sandboxed_process_launcher_delegate.h"
 #include "content/public/common/service_manager_connection.h"
 #include "content/public/common/service_names.mojom.h"
+#include "media/base/media_switches.h"
 #include "mojo/edk/embedder/embedder.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
 #include "ui/base/ui_base_switches.h"
@@ -313,6 +314,8 @@ bool UtilityProcessHostImpl::StartProcess() {
 #if defined(OS_MACOSX)
       switches::kEnableSandboxLogging,
 #endif
+      switches::kUseFakeDeviceForMediaStream,
+      switches::kUseFileForFakeVideoCapture,
     };
     cmd_line->CopySwitchesFrom(browser_command_line, kSwitchNames,
                                arraysize(kSwitchNames));
