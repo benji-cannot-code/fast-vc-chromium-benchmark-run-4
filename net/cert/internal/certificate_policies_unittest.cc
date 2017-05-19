a963ee837cee8a28d68386108bdda8297c7d0291
eroman <eroman@chromium.org>
2017-05-18 20:06:29 -0700

    Add parsing for RFC 5280's InhibitAnyPolicy.
    Review-Url: https://codereview.chromium.org/2870323002
    Cr-Commit-Position: refs/heads/master@{#473053}

--- net/cert/internal/certificate_policies_unittest.cc
+++ net/cert/internal/certificate_policies_unittest.cc
@@ -151,4 +151,7 @@ TEST(ParseCertificatePoliciesTest, TwoPoliciesWithQualifiers) {
 // NOTE: The tests for ParseCertificatePolicies() are part of
 // parsed_certificate_unittest.cc
 
+// NOTE: The tests for ParseInhibitAnyPolicy() are part of
+// parsed_certificate_unittest.cc
+
 }  // namespace net
