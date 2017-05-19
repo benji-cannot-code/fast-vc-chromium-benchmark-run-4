e12bd83bb903461e6c408e27e3f18c538951908f
lasoren <lasoren@chromium.org>
2017-05-19 14:42:00 -0700

    MediaPerceptionPrivate IDL and skeleton.
    This private API is for process management of and communicating with a
    media analytics process running outside of Chrome. The analytics process
    uses computer vision technology to send metadata about image frames over
    D-Bus, and MediaPerception events are fired on the frontend.
    
    BUG=709180, 720495
    
    Review-Url: https://codereview.chromium.org/2860203002
    Cr-Commit-Position: refs/heads/master@{#473337}

--- extensions/common/permissions/extensions_api_permissions.cc
+++ extensions/common/permissions/extensions_api_permissions.cc
@@ -64,6 +64,7 @@ ExtensionsAPIPermissions::GetAllPermissions() const {
       {APIPermission::kOverrideEscFullscreen,
        "app.window.fullscreen.overrideEsc"},
       {APIPermission::kIdle, "idle"},
+      {APIPermission::kMediaPerceptionPrivate, "mediaPerceptionPrivate"},
       {APIPermission::kMetricsPrivate, "metricsPrivate",
        APIPermissionInfo::kFlagCannotBeOptional},
       {APIPermission::kNetworkingConfig, "networking.config"},
