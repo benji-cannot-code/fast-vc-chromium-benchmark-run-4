851bc6632714b859c18a82e1bdd1549bed818c59
jdoerrie <jdoerrie@chromium.org>
2017-05-19 08:14:22 -0700

    Get and return String by const-ref if used as rvalue
    Generated bindings return String attributes by value instead of const reference.
    This leads to uneccesary copies and can lead to confusing runtime behavior when
    a getter is used as a setter by accident. This change addresses this by changing
    'String' to 'const String&' where appropriate, which addresses both of these
    issues.
    
    BUG=NONE
    
    Review-Url: https://codereview.chromium.org/2897603003
    Cr-Commit-Position: refs/heads/master@{#473192}

--- third_party/WebKit/Source/bindings/tests/results/core/StringOrArrayBufferOrArrayBufferView.h
+++ third_party/WebKit/Source/bindings/tests/results/core/StringOrArrayBufferOrArrayBufferView.h
@@ -44,9 +44,9 @@ class CORE_EXPORT StringOrArrayBufferOrArrayBufferView final {
   static StringOrArrayBufferOrArrayBufferView fromArrayBufferView(NotShared<TestArrayBufferView>);
 
   bool isString() const { return m_type == SpecificTypeString; }
-  String getAsString() const;
-  void setString(String);
-  static StringOrArrayBufferOrArrayBufferView fromString(String);
+  const String& getAsString() const;
+  void setString(const String&);
+  static StringOrArrayBufferOrArrayBufferView fromString(const String&);
 
   StringOrArrayBufferOrArrayBufferView(const StringOrArrayBufferOrArrayBufferView&);
   ~StringOrArrayBufferOrArrayBufferView();
