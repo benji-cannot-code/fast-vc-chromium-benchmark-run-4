5cf5eb5a200ae9b80161f451e71c756e2d473f9f
pasko <pasko@chromium.org>
2017-05-19 05:23:05 -0700

    Revert of DevTools: Roll Lighthouse binary to 2.0.0-alpha.7 (patchset #5 id:40001 of https://codereview.chromium.org/2888263002/ )
    Reason for revert:
    webview perfbot failing with:
    
    TraceImportError: No clock sync markers exist pairing clock domain "LINUX_FTRACE_GLOBAL" with target clock domain "TELEMETRY".
    
    http://crbug.com/724423
    
    Original issue's description:
    > DevTools: Roll Lighthouse binary to 2.0.0-alpha.7
    > Hash: aebc9bdf53754ffd360f7c54c752b046383c2d1b
    >
    > BUG=663085
    > R=pfeldman@chromium.org, phulce@chromium.org
    >
    > Review-Url: https://codereview.chromium.org/2888263002 .
    > Cr-Original-Commit-Position: refs/heads/master@{#472680}
    > Committed: https://chromium.googlesource.com/chromium/src/+/5b84a98141df91643a05bcb8e81c9f0398e6ba81
    > Review-Url: https://codereview.chromium.org/2888263002 .
    > Cr-Commit-Position: refs/heads/master@{#472913}
    > Committed: https://chromium.googlesource.com/chromium/src/+/a9b28ebe7c619f7025e9b9c20671967eb3a440f3
    
    TBR=pfeldman@chromium.org,phulce@chromium.org,paulirish@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=663085
    
    Review-Url: https://codereview.chromium.org/2894113002
    Cr-Commit-Position: refs/heads/master@{#473167}

--- third_party/WebKit/Source/devtools/front_end/audits2/lighthouse/renderer/report-renderer.js
+++ third_party/WebKit/Source/devtools/front_end/audits2/lighthouse/renderer/report-renderer.js
@@ -177,7 +177,6 @@ if (typeof module !== 'undefined' && module.exports) {
  *       rawValue: (number|undefined),
  *       description: string,
  *       informative: boolean,
- *       manual: boolean,
  *       debugString: string,
  *       displayValue: string,
  *       helpText: string,
