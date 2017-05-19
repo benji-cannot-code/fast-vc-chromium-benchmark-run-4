3bb9c706b061228be9243d1d8c30bb913b3a13d1
ntfschr <ntfschr@chromium.org>
2017-05-19 12:15:30 -0700

    SafeBrowsing: allow WebView to customize the Help Center URL
    This adds a 'plink' field to SBErrorDisplayOptions, allowing WebView
    to specify its own plink (the p= URL query parameter) when visiting the
    Help Center. This will allow the Help Center to differentiate between
    Chrome and WebView and show appropriate information for each.
    
    SBErrorDisplayOptions.plink is set to nullptr for Chrome, to indicate
    that we should use a default value (p=cpn_safe_browsing).
    
    Right now, the _wv suffix in the plink is ignored by the help center,
    and is treated just like "cpn_safe_browsing" would be. So there should
    be no change in high-level behavior, the URL will just vary slightly
    between WebView and Chrome.
    
    BUG=723125
    
    Review-Url: https://codereview.chromium.org/2890703002
    Cr-Commit-Position: refs/heads/master@{#473271}

--- chrome/browser/safe_browsing/safe_browsing_blocking_page_unittest.cc
+++ chrome/browser/safe_browsing/safe_browsing_blocking_page_unittest.cc
@@ -94,7 +94,8 @@ class TestSafeBrowsingBlockingPageFactory
         web_contents->GetBrowserContext()->IsOffTheRecord(),
         IsExtendedReportingEnabled(*prefs), IsScout(*prefs),
         is_proceed_anyway_disabled,
-        BaseBlockingPage::IsMainPageLoadBlocked(unsafe_resources));
+        BaseBlockingPage::IsMainPageLoadBlocked(unsafe_resources),
+        "cpn_safe_browsing" /* help_center_article_link */);
     return new TestSafeBrowsingBlockingPage(manager, web_contents,
                                             main_frame_url, unsafe_resources,
                                             display_options);
@@ -175,7 +176,8 @@ class TestSafeBrowsingBlockingQuietPageFactory
         web_contents->GetBrowserContext()->IsOffTheRecord(),
         IsExtendedReportingEnabled(*prefs), IsScout(*prefs),
         is_proceed_anyway_disabled,
-        BaseBlockingPage::IsMainPageLoadBlocked(unsafe_resources));
+        BaseBlockingPage::IsMainPageLoadBlocked(unsafe_resources),
+        "cpn_safe_browsing" /* help_center_article_link */);
     return new TestSafeBrowsingBlockingPageQuiet(
         manager, web_contents, main_frame_url, unsafe_resources,
         display_options);
