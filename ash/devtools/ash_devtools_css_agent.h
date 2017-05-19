4c84abd2b4dfe2fa8ecc440efdd8e22e9908f2b7
thanhph <thanhph@chromium.org>
2017-05-19 14:07:03 -0700

    Create a unified UIElement interface for Widget, View and Window.
    The unified interface will enable css_agent to set/access many
    properties of widget, view and window without the need of knowing the
    actual object type. A UIElement tree where each node can be either
    view, widget and window will be kept in sync with tree structures of
    window, widget and view objects.
    
    BUG=700024
    
    Review-Url: https://codereview.chromium.org/2776543002
    Cr-Commit-Position: refs/heads/master@{#473315}

--- ash/devtools/ash_devtools_css_agent.h
+++ ash/devtools/ash_devtools_css_agent.h
@@ -6,9 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_DEVTOOLS_ASH_DEVTOOLS_CSS_AGENT_H_
 #define ASH_DEVTOOLS_ASH_DEVTOOLS_CSS_AGENT_H_
 
+#include "ash/ash_export.h"
 #include "ash/devtools/ash_devtools_dom_agent.h"
+#include "base/macros.h"
 #include "components/ui_devtools/CSS.h"
-#include "ui/views/widget/widget_observer.h"
 
 namespace ash {
 namespace devtools {
@@ -21,7 +22,7 @@ class ASH_EXPORT AshDevToolsCSSAgent
   explicit AshDevToolsCSSAgent(AshDevToolsDOMAgent* dom_agent);
   ~AshDevToolsCSSAgent() override;
 
-  // CSS::Backend
+  // CSS::Backend:
   ui::devtools::protocol::Response enable() override;
   ui::devtools::protocol::Response disable() override;
   ui::devtools::protocol::Response getMatchedStylesForNode(
@@ -35,10 +36,8 @@ class ASH_EXPORT AshDevToolsCSSAgent
           ui::devtools::protocol::Array<ui::devtools::protocol::CSS::CSSStyle>>*
           result) override;
 
-  // AshDevToolsDOMAgentObserver
-  void OnWindowBoundsChanged(aura::Window* window) override;
-  void OnWidgetBoundsChanged(views::Widget* widget) override;
-  void OnViewBoundsChanged(views::View* view) override;
+  // AshDevToolsDOMAgentObserver:
+  void OnNodeBoundsChanged(int node_id) override;
 
  private:
   std::unique_ptr<ui::devtools::protocol::CSS::CSSStyle> GetStylesForNode(
