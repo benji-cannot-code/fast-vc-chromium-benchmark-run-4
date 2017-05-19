3981dfe05827642fc254be7225ee529fa57fdf31
benwells <benwells@chromium.org>
2017-05-19 13:34:59 -0700

    Manually revert only showing search geo disclosure after EULA accepted
    Now that the disclosure is only shown when the X-Geo header is sent,
    or a page uses the geolocation API, this isn't needed.
    
    The change which prevented the disclosure being shown until after EULA
    was accepted also apparently caused a perf regression. It's hard to
    see how it would have, but removingt the change will determine if
    it really was that change or something else.
    
    BUG=714863
    
    Review-Url: https://codereview.chromium.org/2891263003
    Cr-Commit-Position: refs/heads/master@{#473298}

--- chrome/browser/android/search_geolocation/search_geolocation_service.cc
+++ chrome/browser/android/search_geolocation/search_geolocation_service.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/values.h"
 #include "chrome/browser/android/search_geolocation/search_geolocation_disclosure_tab_helper.h"
-#include "chrome/browser/browser_process.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
@@ -25,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search_engines/template_url.h"
 #include "components/search_engines/template_url_service.h"
 #include "components/search_engines/template_url_service_observer.h"
-#include "components/web_resource/web_resource_pref_names.h"
 #include "url/gurl.h"
 #include "url/url_constants.h"
 
@@ -160,15 +158,6 @@ bool SearchGeolocationService::UseDSEGeolocationSetting(
     const url::Origin& requesting_origin) {
   if (!UseConsistentSearchGeolocation())
     return false;
-
-  // Don't use the DSE setting until the EULA has been acceptd. This prevents
-  // the disclosure being shown on the privacy policy before accepting the UELA,
-  // and would also stop the privacy policy ever getting geolocation access by
-  // default.
-  if (g_browser_process && g_browser_process->local_state() &&
-      !g_browser_process->local_state()->GetBoolean(prefs::kEulaAccepted)) {
-    return false;
-  }
 
   if (requesting_origin.scheme() != url::kHttpsScheme)
     return false;
