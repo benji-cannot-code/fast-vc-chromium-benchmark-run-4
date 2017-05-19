35289a9f771a2137dca1f21ad5adfdc85db7258d
ltian <ltian@chromium.org>
2017-05-18 18:44:09 -0700

    [Android] Prevent displaying context menu is item list is empty
    If the item list of a context menu is empty, Chrome should prevent
    displaying the context menu and and call onMenuClosed to notify native
    code to free resources.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2889083003
    Cr-Commit-Position: refs/heads/master@{#473024}

--- chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ContextMenuHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ContextMenuHelper.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.view.View.OnCreateContextMenuListener;
 
 import org.chromium.base.Callback;
+import org.chromium.base.ThreadUtils;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.metrics.RecordHistogram;
@@ -115,6 +116,10 @@ public class ContextMenuHelper implements OnCreateContextMenuListener {
         if (ChromeFeatureList.isEnabled(ChromeFeatureList.CUSTOM_CONTEXT_MENU)) {
             List<Pair<Integer, List<ContextMenuItem>>> items =
                     mPopulator.buildContextMenu(null, mActivity, mCurrentContextMenuParams);
+            if (items.isEmpty()) {
+                ThreadUtils.postOnUiThread(mOnMenuClosed);
+                return;
+            }
 
             final ContextMenuUi menuUi = new TabularContextMenuUi(new Runnable() {
                 @Override
@@ -219,6 +224,10 @@ public class ContextMenuHelper implements OnCreateContextMenuListener {
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
         List<Pair<Integer, List<ContextMenuItem>>> items =
                 mPopulator.buildContextMenu(menu, v.getContext(), mCurrentContextMenuParams);
+        if (items.isEmpty()) {
+            ThreadUtils.postOnUiThread(mOnMenuClosed);
+            return;
+        }
         ContextMenuUi menuUi = new PlatformContextMenuUi(menu);
         menuUi.displayMenu(mActivity, mCurrentContextMenuParams, items, mCallback, mOnMenuShown,
                 mOnMenuClosed);
