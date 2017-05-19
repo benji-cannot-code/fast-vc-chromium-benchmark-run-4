a433e4cc9d00693fb3380898fd1855550f17bcb9
edcourtney <edcourtney@chromium.org>
2017-05-18 22:15:08 -0700

    [Notifications] Fix swipe to close for ARC notifications.
    Change ArcCustomNotificationView::EventForwarder to not directly invoke On*Event methods of ArcCustomNotificationView (which then call up to the parent). Instead, re-dispatch them to the containing widget, which should handle the event propagation, letting SlideOutController get events as it should. A consequence of this is that I had to add a delegate method to SlideOutController to update ArcCustomNotificationView::SlideHelper. This is because ArcCustomNotificationView will no longer get the gesture events, since it is no longer being called directly from EventForwarder.
    
    BUG=719407
    
    Review-Url: https://codereview.chromium.org/2873553002
    Cr-Commit-Position: refs/heads/master@{#473094}

--- ui/message_center/views/slide_out_controller.cc
+++ ui/message_center/views/slide_out_controller.cc
@@ -73,6 +73,7 @@ void SlideOutController::OnGestureEvent(ui::GestureEvent* event) {
     RestoreVisualState();
   }
 
+  delegate_->OnSlideChanged();
   event->SetHandled();
 }
 
@@ -101,6 +102,7 @@ void SlideOutController::SlideOutAndClose(int direction) {
   transform.Translate(direction < 0 ? -width : width, 0.0);
   layer->SetTransform(transform);
   layer->SetOpacity(0.f);
+  delegate_->OnSlideChanged();
 }
 
 void SlideOutController::OnImplicitAnimationsCompleted() {
