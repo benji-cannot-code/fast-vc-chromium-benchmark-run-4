bc5f03e154ff3cc31bfcd0e875e946ca322259af
mike <mike@mikepennisi.com>
2017-05-19 08:59:57 -0700

    Upstream service worker "timing" test to WPT
    The version of this test in the Chromium source tree is equivalent to
    the version available in the Web Platform Tests project. The WPT version
    was previously marked as an expected failure because it constructed URLs
    in terms of the HTTP protocol. This is correct within the Chromium
    infrastructure (where the "secure context" requirement of the service
    worker specification is satisfied by the use of the origin `127.0.0.1`),
    but it is inappropriate for use within the WPT testing infrastructure
    (where HTTPS is used to provide a secure context).
    
    Correct the upstream version of the test, remove Chromium's "expected
    failure" file, and remove the Chromium version of the test.
    
    BUG=688116, 678905
    R=falken@chromium.org
    
    Review-Url: https://codereview.chromium.org/2895473002
    Cr-Commit-Position: refs/heads/master@{#473206}

--- third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/resource-timing-worker.js
+++
@@ -1,6 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-self.addEventListener('fetch', function(event) {
-    if (event.request.url.indexOf('dummy.js') != -1) {
-      event.respondWith(new Response());
-    }
-  });
