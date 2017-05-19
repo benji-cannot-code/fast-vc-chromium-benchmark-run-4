277b052f8d4f0d3b9438507dc3f5524950ec39b9
mike <mike@mikepennisi.com>
2017-05-19 10:34:37 -0700

    Upstream service wrkr "respond with" tests to WPT
    **(fetch-event-)respond-with-readable-stream**
    
    **(fetch-event-)respond-with-response-body-with-invalid-chunk**
    
    Re-locate test files to Web Platform Test directory for eventual
    automated upstreaming. Modify file names according to convention set by
    related tests in that project. Update resource URLs to suitable values
    for that project. Schedule frame removal to occur following test
    completion. Introduce a "use strict" directive. Simplify Promise
    creation.
    
    BUG=688116
    R=falken@chromium.org
    
    Review-Url: https://codereview.chromium.org/2893793004
    Cr-Commit-Position: refs/heads/master@{#473232}

--- third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/fetch-event-respond-with-response-body-with-invalid-chunk-worker.js
+++ third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/fetch-event-respond-with-response-body-with-invalid-chunk-worker.js
@@ -1,4 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+'use strict';
+
 self.addEventListener('fetch', event => {
     if (!event.request.url.match(/body-stream-with-invalid-chunk$/))
       return;
