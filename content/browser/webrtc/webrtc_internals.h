76cbd146a3bdd304e8ea3d50d41462e0624603ce
ke.he <ke.he@intel.com>
2017-05-19 17:34:52 -0700

    Convert //content/browser/webrtc to be clients of WakeLock mojo service.
    Wake Lock is a Mojo interface that wraps PowerSaveBlocker. As part of the
    creation of standalone Device Service, all browser-side clients of
    PowerSaveBlocker should be converted to be clients of the Wake Lock Mojo
    interface instead.
    
    This CL converts clients of PowerSaveBlocker in //content/browser/webrtc
    
    BUG=689413
    
    Review-Url: https://codereview.chromium.org/2886603003
    Cr-Commit-Position: refs/heads/master@{#473395}

--- content/browser/webrtc/webrtc_internals.h
+++ content/browser/webrtc/webrtc_internals.h
@@ -20,12 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/render_process_host_observer.h"
+#include "device/wake_lock/public/interfaces/wake_lock_service.mojom.h"
 #include "media/media_features.h"
 #include "ui/shell_dialogs/select_file_dialog.h"
-
-namespace device {
-class PowerSaveBlocker;
-}  // namespace device
 
 namespace content {
 
@@ -112,7 +109,6 @@ class CONTENT_EXPORT WebRTCInternals : public RenderProcessHostObserver,
   const base::FilePath& GetEventLogFilePath() const;
 
   int num_open_connections() const { return num_open_connections_; }
-  bool IsPowerSavingBlocked() const { return !!power_save_blocker_; }
 
  protected:
   // Constructor/Destructor are protected to allow tests to derive from the
@@ -122,6 +118,8 @@ class CONTENT_EXPORT WebRTCInternals : public RenderProcessHostObserver,
   WebRTCInternals();
   WebRTCInternals(int aggregate_updates_ms, bool should_block_power_saving);
   ~WebRTCInternals() override;
+
+  device::mojom::WakeLockServicePtr wake_lock_service_;
 
  private:
   friend struct base::LazyInstanceTraitsBase<WebRTCInternals>;
@@ -166,9 +164,11 @@ class CONTENT_EXPORT WebRTCInternals : public RenderProcessHostObserver,
   void MaybeClosePeerConnection(base::DictionaryValue* record);
 
   // Called whenever a PeerConnection is created or stopped in order to
-  // impose/release a block on suspending the current application for power
+  // request/cancel a wake lock on suspending the current application for power
   // saving.
-  void CreateOrReleasePowerSaveBlocker();
+  void UpdateWakeLock();
+
+  device::mojom::WakeLockService* GetWakeLockService();
 
   // Called on a timer to deliver updates to javascript.
   // We throttle and bulk together updates to avoid DOS like scenarios where
@@ -219,11 +219,9 @@ class CONTENT_EXPORT WebRTCInternals : public RenderProcessHostObserver,
   bool selecting_event_log_;
   base::FilePath event_log_recordings_file_path_;
 
-  // While |num_open_connections_| is greater than zero, hold an instance of
-  // PowerSaveBlocker.  This prevents the application from being suspended while
-  // remoting.
+  // While |num_open_connections_| is greater than zero, request a wake lock
+  // service. This prevents the application from being suspended while remoting.
   int num_open_connections_;
-  std::unique_ptr<device::PowerSaveBlocker> power_save_blocker_;
   const bool should_block_power_saving_;
 
   // Set of render process hosts that |this| is registered as an observer on.
