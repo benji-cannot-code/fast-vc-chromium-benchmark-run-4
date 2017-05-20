861eab3268b4435cdcb693d03d052bd54aeee6fc
huayinz <huayinz@chromium.org>
2017-05-19 21:41:20 -0700

    Remove unused histograms
    Removed Omnibox.ProgressBarUpdateCount and
    Omnibox.ProgressBarBreakPointUpdateCount
    since collected data is not used anymore
    
    BUG=719801
    R=mdjones@chromium.org
    
    Review-Url: https://codereview.chromium.org/2885013004
    Cr-Commit-Position: refs/heads/master@{#473413}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/ClipDrawableProgressBar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/ClipDrawableProgressBar.java
@@ -40,7 +40,6 @@ public class ClipDrawableProgressBar extends ImageView {
     private final ColorDrawable mForegroundDrawable;
     private int mBackgroundColor = Color.TRANSPARENT;
     private float mProgress;
-    private int mProgressUpdateCount;
     private int mDesiredVisibility;
 
     /**
@@ -85,7 +84,6 @@ public class ClipDrawableProgressBar extends ImageView {
         if (mProgress == progress) return;
 
         mProgress = progress;
-        mProgressUpdateCount += 1;
         getDrawable().setLevel(Math.round(progress * CLIP_DRAWABLE_MAX));
     }
 
@@ -129,20 +127,6 @@ public class ClipDrawableProgressBar extends ImageView {
                     drawingInfoOut.progressBarRect.left,
                     getBottom());
         }
-    }
-
-    /**
-     * Resets progress update count to 0.
-     */
-    public void resetProgressUpdateCount() {
-        mProgressUpdateCount = 0;
-    }
-
-    /**
-     * @return Progress update count since reset.
-     */
-    public int getProgressUpdateCount() {
-        return mProgressUpdateCount;
     }
 
     private void updateInternalVisibility() {
