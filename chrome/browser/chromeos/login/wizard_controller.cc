f8cf0c79989f515b3ddec58fb963ba748b548e3d
kumarniranjan <kumarniranjan@google.com>
2017-05-19 17:00:34 -0700

    Make Hands-Off Zero-Touch Enrollment compatibile with tests
    Specifically, the automatic nature of Hands-Off Enrollment leads to
    the DUT performing an update check after reboot which interferes
    with autoupdate_EndToEndTest. This CL fixes that issue.
    It does so by instructing the UpdateScreen to skip checking for updates
    if both of the following conditions hold:
    1. The previous update check initiated by the UpdateScreen did not result
    in an update.
    2. This previous update check occurred during the last hour.
    
    BUG=chromium:710716
    TEST=unit test
    
    Review-Url: https://codereview.chromium.org/2894783003
    Cr-Commit-Position: refs/heads/master@{#473388}

--- chrome/browser/chromeos/login/wizard_controller.cc
+++ chrome/browser/chromeos/login/wizard_controller.cc
@@ -1375,6 +1375,13 @@ void WizardController::SkipPostLoginScreensForTesting() {
   skip_post_login_screens_ = true;
 }
 
+// static
+bool WizardController::UsingHandsOffEnrollment() {
+  return policy::DeviceCloudPolicyManagerChromeOS::
+             GetZeroTouchEnrollmentMode() ==
+         policy::ZeroTouchEnrollmentMode::HANDS_OFF;
+}
+
 void WizardController::OnLocalStateInitialized(bool /* succeeded */) {
   if (GetLocalState()->GetInitializationStatus() !=
       PrefService::INITIALIZATION_STATUS_ERROR) {
