9e0f05b93145f9118905dd930994ce6ad8b81ec1
tapted <tapted@chromium.org>
2017-05-20 20:10:15 -0700

    Support finer grained font weights on Mac.
    We want to explore a MEDIUM weight for button text under Harmony. This
    is consistent with MD and helps compensate for buttons not using solid
    black for the font color.
    
    The San Francisco font in 10.11 and 10.12 has good support for finer
    grained weights and is the font Chrome UI is mostly concerned with, so
    focus testing on that but survey expectations for all supported OS
    versions.
    
    BUG=691891
    
    Review-Url: https://codereview.chromium.org/2869803005
    Cr-Commit-Position: refs/heads/master@{#473457}

--- ui/views/examples/example_combobox_model.cc
+++ ui/views/examples/example_combobox_model.cc
@@ -10,9 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace views {
 namespace examples {
 
-ExampleComboboxModel::ExampleComboboxModel(const char** strings, int count)
-    : strings_(strings), count_(count) {
-}
+ExampleComboboxModel::ExampleComboboxModel(const char* const* strings,
+                                           int count)
+    : strings_(strings), count_(count) {}
 
 ExampleComboboxModel::~ExampleComboboxModel() {
 }
