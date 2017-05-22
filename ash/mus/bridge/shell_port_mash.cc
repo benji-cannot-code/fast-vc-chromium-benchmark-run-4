f42c96d996d4972f75f6b039e4482db8ae6c75a7
sky <sky@chromium.org>
2017-05-22 12:17:33 -0700

    chromeos: adds TouchDeviceServer and wires up in mushrome
    TouchDeviceServer is necessary for hooking up AshTouchTransformController in mus/mash.
    
    BUG=705686
    TEST=covered by tests
    R=erg@chromium.org,rockot@chromium.org,tsepez@chromium.org
    
    Review-Url: https://codereview.chromium.org/2901563002
    Cr-Commit-Position: refs/heads/master@{#473651}

--- ash/mus/bridge/shell_port_mash.cc
+++ ash/mus/bridge/shell_port_mash.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/mus/drag_window_resizer.h"
 #include "ash/mus/keyboard_ui_mus.h"
 #include "ash/mus/screen_mus.h"
+#include "ash/mus/touch_transform_setter_mus.h"
 #include "ash/mus/window_manager.h"
 #include "ash/public/cpp/config.h"
 #include "ash/public/cpp/shell_window_ids.h"
@@ -267,6 +268,13 @@ void ShellPortMash::SetDisplayWorkAreaInsets(WmWindow* window,
     return;
   }
   window_manager_->screen()->SetWorkAreaInsets(window->aura_window(), insets);
+}
+
+std::unique_ptr<display::TouchTransformSetter>
+ShellPortMash::CreateTouchTransformDelegate() {
+  std::unique_ptr<TouchTransformSetterMus> delegate =
+      base::MakeUnique<TouchTransformSetterMus>(window_manager_->connector());
+  return delegate;
 }
 
 void ShellPortMash::LockCursor() {
