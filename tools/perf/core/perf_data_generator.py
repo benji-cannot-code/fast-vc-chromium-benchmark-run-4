da813ecd0f7ca1d14a469cd869f672484c831fb6
Ned Nguyen <nednguyen@google.com>
2017-05-22 22:06:12 +0000

    Revert "Disable performance_browser_tests on failing bots"
    This reverts commit 107bc42153f7df4157a1beea973ca03bd34404d8.
    
    Reason for revert: reenable the benchmark to see which tests are failing.
    
    Original change's description:
    > Disable performance_browser_tests on failing bots
    > 
    > The failing bots are:
    > 
    > Mac Air 10.11 Perf 
    > Mac Pro 10.11 Perf
    > Mac Retina Perf
    > Win 7 ATI GPU Perf
    > Win 7 Nvidia GPU Perf 
    > Win 7 Perf
    > 
    > Bug:722367
    > Change-Id: Iffb4d077312a69c3fb2386603183c3737ecb6540
    > 
    > NOTRY=true (since this doesn't affect any CQ test)
    > TBR=sullivan@chromium.org
    > 
    > Change-Id: Iffb4d077312a69c3fb2386603183c3737ecb6540
    > Reviewed-on: https://chromium-review.googlesource.com/509989
    > Commit-Queue: Ned Nguyen <nednguyen@google.com>
    > Reviewed-by: Ned Nguyen <nednguyen@google.com>
    > Cr-Commit-Position: refs/heads/master@{#473326}
    
    TBR=nednguyen@google.com,martiniss@chromium.org
    # Not skipping CQ checks because original CL landed > 1 day ago.
    Bug:722367
    
    Change-Id: Ife4777108c4318b808d7bf1f8b9ffe2a9dda1ec1
    Reviewed-on: https://chromium-review.googlesource.com/511462
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#473786}

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -382,7 +382,7 @@ def get_waterfall_config():
           ],
        'perf_tests': [
          ('load_library_perf_tests', 'build187-m1'),
-         #  ('performance_browser_tests', 'build187-m1'),  # crbug.com/722367
+         ('performance_browser_tests', 'build187-m1'),
          ('media_perftests', 'build188-m1')]
       }
     ])
@@ -418,7 +418,7 @@ def get_waterfall_config():
        'perf_tests': [
          ('angle_perftests', 'build103-m1'),
          ('load_library_perf_tests', 'build103-m1'),
-         # ('performance_browser_tests', 'build103-m1'),  # crbug.com/722367
+         ('performance_browser_tests', 'build103-m1'),
          ('media_perftests', 'build104-m1')]
       }
     ])
@@ -455,7 +455,7 @@ def get_waterfall_config():
        'perf_tests': [
          ('angle_perftests', 'build94-m1'),
          ('load_library_perf_tests', 'build94-m1'),
-         # ('performance_browser_tests', 'build94-m1'),  # crbug.com/722367
+         ('performance_browser_tests', 'build94-m1'),
          ('media_perftests', 'build95-m1')]
       }
     ])
@@ -501,7 +501,7 @@ def get_waterfall_config():
            'build4-b1', 'build5-b1', 'build6-b1', 'build7-b1', 'build8-b1'
           ],
        'perf_tests': [
-         # ('performance_browser_tests', 'build8-b1')  # crbug.com/722367
+         ('performance_browser_tests', 'build8-b1')
        ]
       }
     ])
@@ -518,7 +518,7 @@ def get_waterfall_config():
            'build130-b1', 'build131-b1', 'build132-b1'
           ],
        'perf_tests': [
-         # ('performance_browser_tests', 'build132-b1')  # crbug.com/722367
+         ('performance_browser_tests', 'build132-b1')
        ]
       }
     ])
@@ -535,7 +535,7 @@ def get_waterfall_config():
            'build125-b1', 'build126-b1', 'build127-b1'
           ],
        'perf_tests': [
-         # ('performance_browser_tests', 'build126-b1')  # crbug.com/722367
+         ('performance_browser_tests', 'build126-b1')
        ]
       }
     ])
