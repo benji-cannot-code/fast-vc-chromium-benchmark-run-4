c1f878786a6e6e1b27bbb65b5522e19a513c3a2b
fdoray <fdoray@chromium.org>
2017-05-22 14:08:54 -0700

    Use TaskScheduler instead of SequencedWorkerPool in chrome_cookie_store_ios_client.mm.
    SequencedWorkerPool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=droger@chromium.org
    
    Review-Url: https://codereview.chromium.org/2877783006
    Cr-Commit-Position: refs/heads/master@{#473691}

--- ios/chrome/browser/net/chrome_cookie_store_ios_client.mm
+++ ios/chrome/browser/net/chrome_cookie_store_ios_client.mm
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/net/chrome_cookie_store_ios_client.h"
 
 #include "base/logging.h"
-#include "base/threading/sequenced_worker_pool.h"
+#include "base/task_scheduler/post_task.h"
 #import "ios/chrome/browser/browsing_data/browsing_data_change_listening.h"
 #include "ios/web/public/web_thread.h"
 
@@ -30,7 +30,6 @@ void ChromeCookieStoreIOSClient::DidChangeCookieStorage() const {
 scoped_refptr<base::SequencedTaskRunner>
 ChromeCookieStoreIOSClient::GetTaskRunner() const {
   DCHECK(thread_checker_.CalledOnValidThread());
-
-  base::SequencedWorkerPool* pool = web::WebThread::GetBlockingPool();
-  return pool->GetSequencedTaskRunner(pool->GetSequenceToken()).get();
+  return base::CreateSequencedTaskRunnerWithTraits(
+      {base::MayBlock(), base::TaskPriority::BACKGROUND});
 }
