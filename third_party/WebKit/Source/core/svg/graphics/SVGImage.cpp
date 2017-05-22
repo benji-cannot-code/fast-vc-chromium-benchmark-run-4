e7205ef916a002f30536beaf9f4ba5584c4ac95b
hiroshige <hiroshige@chromium.org>
2017-05-22 09:24:08 -0700

    Make ImageObserver::AsyncLoadCompleted() called asynchronously
    - To avoid complex things (i.e. ImageNotifyFinished() calls)
      during Document::ImplicitClose() to prevent timing bugs, and
    - To make LoadEventFinished() of SVG's document true when
      ImageNotifyFinished() is called, and make the CHECK() in
      https://codereview.chromium.org/2888953004/ hold.
    
    BUG=382170, 690480, 667641
    
    Review-Url: https://codereview.chromium.org/2897533002
    Cr-Commit-Position: refs/heads/master@{#473593}

--- third_party/WebKit/Source/core/svg/graphics/SVGImage.cpp
+++ third_party/WebKit/Source/core/svg/graphics/SVGImage.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/animation/DocumentAnimations.h"
 #include "core/animation/DocumentTimeline.h"
 #include "core/dom/NodeTraversal.h"
+#include "core/dom/TaskRunnerHelper.h"
 #include "core/dom/shadow/FlatTreeTraversal.h"
 #include "core/frame/FrameView.h"
 #include "core/frame/LocalFrame.h"
@@ -641,8 +642,16 @@ void SVGImage::LoadCompleted() {
 
     case kWaitingForAsyncLoadCompletion:
       load_state_ = kLoadCompleted;
-      if (GetImageObserver())
-        GetImageObserver()->AsyncLoadCompleted(this);
+
+      // Because LoadCompleted() is called synchronously from
+      // Document::ImplicitClose(), we defer AsyncLoadCompleted() to avoid
+      // potential bugs and timing dependencies around ImplicitClose() and
+      // to make LoadEventFinished() true when AsyncLoadCompleted() is called.
+      TaskRunnerHelper::Get(TaskType::kUnspecedLoading,
+                            ToLocalFrame(page_->MainFrame()))
+          ->PostTask(BLINK_FROM_HERE,
+                     WTF::Bind(&SVGImage::NotifyAsyncLoadCompleted,
+                               RefPtr<SVGImage>(this)));
       break;
 
     case kDataChangedNotStarted:
@@ -650,6 +659,11 @@ void SVGImage::LoadCompleted() {
       CHECK(false);
       break;
   }
+}
+
+void SVGImage::NotifyAsyncLoadCompleted() {
+  if (GetImageObserver())
+    GetImageObserver()->AsyncLoadCompleted(this);
 }
 
 Image::SizeAvailability SVGImage::DataChanged(bool all_data_received) {
