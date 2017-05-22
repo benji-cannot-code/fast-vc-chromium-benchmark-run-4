1d1dff4e0c2041d237fb03591d9feb10b9170215
thildebr <thildebr@chromium.org>
2017-05-22 09:42:52 -0700

    Allow filtering of instant apps in InstalledAppProvider.
    Adds logic for returning instant apps from InstalledAppProviderImpl's
    filterInstalledApps.
    
    Adds the function getRelatedApplicationForInstantAppUrl to InstantAppsHandler
    with a base implementation that currently just returns null, in a similar
    fashion to the base implemention of getInstantAppIntentForUrl.
    
    A unit test that mocks the return of getRelatedApplicationForInstantAppUrl
    is now in InstalledAppProviderTest.
    
    BUG=703474
    
    Review-Url: https://codereview.chromium.org/2889653004
    Cr-Commit-Position: refs/heads/master@{#473598}

--- chrome/android/java/src/org/chromium/chrome/browser/installedapp/InstalledAppProviderFactory.java
+++ chrome/android/java/src/org/chromium/chrome/browser/installedapp/InstalledAppProviderFactory.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.installedapp;
 
 import org.chromium.base.ContextUtils;
+import org.chromium.chrome.browser.instantapps.InstantAppsHandler;
 import org.chromium.content.browser.framehost.RenderFrameHostImpl;
 import org.chromium.content_public.browser.RenderFrameHost;
 import org.chromium.installedapp.mojom.InstalledAppProvider;
@@ -50,7 +51,7 @@ public class InstalledAppProviderFactory implements InterfaceFactory<InstalledAp
 
     @Override
     public InstalledAppProvider createImpl() {
-        return new InstalledAppProviderImpl(
-                mFrameUrlDelegate, ContextUtils.getApplicationContext());
+        return new InstalledAppProviderImpl(mFrameUrlDelegate, ContextUtils.getApplicationContext(),
+                InstantAppsHandler.getInstance());
     }
 }
