35b933694f9985123a092934ff9c26e244389b50
rhalavati <rhalavati@chromium.org>
2017-05-22 10:33:05 -0700

    Network traffic annotation added to DataReductionProxy unittests.
    Network traffic annotation is added to network requests of:
    components/data_reduction_proxy/core/browser/data_reduction_proxy_metrics_unittest.cc
    components/data_reduction_proxy/core/browser/data_reduction_proxy_network_delegate_unittest.cc
    
    BUG=656607
    
    Review-Url: https://codereview.chromium.org/2899673002
    Cr-Commit-Position: refs/heads/master@{#473612}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_metrics_unittest.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_metrics_unittest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/proxy/proxy_server.h"
 #include "net/proxy/proxy_service.h"
 #include "net/socket/socket_test_util.h"
+#include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
 #include "net/url_request/url_request.h"
 #include "net/url_request/url_request_test_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -178,8 +179,8 @@ TEST(ChromeNetworkDailyDataSavingMetricsTest,
     mock_socket_factory.AddSocketDataProvider(&socket_data_provider);
 
     net::TestDelegate delegate;
-    std::unique_ptr<net::URLRequest> request =
-        context.CreateRequest(test_case.url, net::IDLE, &delegate);
+    std::unique_ptr<net::URLRequest> request = context.CreateRequest(
+        test_case.url, net::IDLE, &delegate, TRAFFIC_ANNOTATION_FOR_TESTS);
     request->SetLoadFlags(test_case.load_flags);
     request->Start();
     test_context->RunUntilIdle();
