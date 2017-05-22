6d68d845007ab9c401618b3f84b9f724a3797697
erg <erg@chromium.org>
2017-05-22 13:19:43 -0700

    Move more mus tests to service_unittest.
    This moves mus_ime_unittests, ui_struct_traits_unittests,
    input_device_unittests, mus_public_unittests, mus_demo_unittests to
    service_unittest.
    
    BUG=722546
    
    Review-Url: https://codereview.chromium.org/2885193002
    Cr-Commit-Position: refs/heads/master@{#473675}

--- services/ui/ime/ime_unittest.cc
+++ services/ui/ime/ime_unittest.cc
@@ -50,7 +50,7 @@ class TestTextInputClient : public ui::mojom::TextInputClient {
 
 class IMEAppTest : public service_manager::test::ServiceTest {
  public:
-  IMEAppTest() : ServiceTest("mus_ime_unittests") {}
+  IMEAppTest() : ServiceTest("ime_unittests") {}
   ~IMEAppTest() override {}
 
   // service_manager::test::ServiceTest:
