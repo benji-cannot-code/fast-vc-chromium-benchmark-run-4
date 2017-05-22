e89c9d2befafe1279ed1a6c7fbb821430dffab31
liaoyuke <liaoyuke@chromium.org>
2017-05-22 09:38:34 -0700

    [ObjC ARC] Converts ios/chrome/test/earl_grey:ios_chrome_smoke_egtests to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2893763002
    Cr-Commit-Position: refs/heads/master@{#473597}

--- ios/chrome/test/earl_grey/smoke_egtest.mm
+++ ios/chrome/test/earl_grey/smoke_egtest.mm
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 // Test case to verify that EarlGrey tests can be launched and perform basic UI
 // interactions.
 @interface SmokeTestCase : ChromeTestCase
