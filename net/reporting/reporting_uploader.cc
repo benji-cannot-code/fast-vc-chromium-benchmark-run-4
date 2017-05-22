d1658c8981fed33e987123725ef97fee530fe0a6
rhalavati <rhalavati@chromium.org>
2017-05-22 03:12:41 -0700

    Network traffic annotation added to reporting_uploader
    Network traffic annotation is added to network request of
    net/reporting/reporting_uploader.cc.
    
    BUG=656607
    
    Review-Url: https://codereview.chromium.org/2844063004
    Cr-Commit-Position: refs/heads/master@{#473539}

--- net/reporting/reporting_uploader.cc
+++ net/reporting/reporting_uploader.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/load_flags.h"
 #include "net/base/upload_bytes_element_reader.h"
 #include "net/http/http_response_headers.h"
+#include "net/traffic_annotation/network_traffic_annotation.h"
 #include "net/url_request/redirect_info.h"
 #include "net/url_request/url_request_context.h"
 #include "url/gurl.h"
@@ -49,8 +50,28 @@ class ReportingUploaderImpl : public ReportingUploader, URLRequest::Delegate {
   void StartUpload(const GURL& url,
                    const std::string& json,
                    const Callback& callback) override {
+    net::NetworkTrafficAnnotationTag traffic_annotation =
+        net::DefineNetworkTrafficAnnotation("reporting", R"(
+        semantics {
+          sender: "Reporting API"
+          description:
+            "The Reporting API reports various issues back to website owners "
+            "to help them detect and fix problems."
+          trigger:
+            "Encountering issues. Examples of these issues are Content "
+            "Security Policy violations and Interventions/Deprecations "
+            "encountered. See draft of reporting spec here: "
+            "https://wicg.github.io/reporting."
+          data: "Details of the issue, depending on issue type."
+          destination: OTHER
+        }
+        policy {
+          cookies_allowed: false
+          setting: "This feature cannot be disabled by settings."
+          policy_exception_justification: "Not implemented."
+        })");
     std::unique_ptr<URLRequest> request =
-        context_->CreateRequest(url, IDLE, this);
+        context_->CreateRequest(url, IDLE, this, traffic_annotation);
 
     request->set_method("POST");
 
