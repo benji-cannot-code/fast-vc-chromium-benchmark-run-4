79b7c9e0c4bafb0a7aa6fe0b9e79709c3fb8818f
dvallet <dvallet@chromium.org>
2017-05-21 19:36:10 -0700

    Add specific headless_shell_win.cc and headless_content_main_delegate_win.cc For Windows component builds
    BUG=686608
    
    Review-Url: https://codereview.chromium.org/2887033003
    Cr-Commit-Position: refs/heads/master@{#473494}

--- headless/app/headless_shell.cc
+++ headless/app/headless_shell.cc
@@ -72,10 +72,8 @@ HeadlessShell::HeadlessShell()
 
 HeadlessShell::~HeadlessShell() {}
 
-void HeadlessShell::OnStart(HeadlessBrowser* browser) {
-// TODO(dvallet): Consider making a Windows specific class to make specific
-// child builds clearer.
 #if !defined(CHROME_MULTIPLE_DLL_CHILD)
+void HeadlessShell::OnStart(HeadlessBrowser* browser) {
   browser_ = browser;
 
   HeadlessBrowserContext::Builder context_builder =
@@ -139,11 +137,9 @@ void HeadlessShell::OnStart(HeadlessBrowser* browser) {
       web_contents_->AddObserver(this);
     }
   }
-#endif  // !defined(CHROME_MULTIPLE_DLL_CHILD)
 }
 
 void HeadlessShell::Shutdown() {
-#if !defined(CHROME_MULTIPLE_DLL_CHILD)
   if (!web_contents_)
     return;
   if (!RemoteDebuggingEnabled()) {
@@ -158,11 +154,9 @@ void HeadlessShell::Shutdown() {
   web_contents_ = nullptr;
   browser_context_->Close();
   browser_->Shutdown();
-#endif  // !defined(CHROME_MULTIPLE_DLL_CHILD)
 }
 
 void HeadlessShell::DevToolsTargetReady() {
-#if !defined(CHROME_MULTIPLE_DLL_CHILD)
   web_contents_->GetDevToolsTarget()->AttachClient(devtools_client_.get());
   devtools_client_->GetInspector()->GetExperimental()->AddObserver(this);
   devtools_client_->GetPage()->GetExperimental()->AddObserver(this);
@@ -234,8 +228,8 @@ void HeadlessShell::DevToolsTargetReady() {
   }
 
   // TODO(skyostil): Implement more features to demonstrate the devtools API.
-#endif  // !defined(CHROME_MULTIPLE_DLL_CHILD)
 }
+#endif  // !defined(CHROME_MULTIPLE_DLL_CHILD)
 
 void HeadlessShell::FetchTimeout() {
   LOG(INFO) << "Timeout.";
@@ -662,8 +656,10 @@ int HeadlessShellMain(int argc, const char** argv) {
   }
 
   if (command_line.HasSwitch(switches::kHideScrollbars)) {
-    builder.SetOverrideWebPreferencesCallback(base::Bind([](
-        WebPreferences* preferences) { preferences->hide_scrollbars = true; }));
+    builder.SetOverrideWebPreferencesCallback(
+        base::Bind([](WebPreferences* preferences) {
+          preferences->hide_scrollbars = true;
+        }));
   }
 
   if (command_line.HasSwitch(switches::kUserAgent)) {
