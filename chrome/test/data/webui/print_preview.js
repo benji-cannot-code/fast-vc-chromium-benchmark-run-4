624d839b540adba8ba40a2f4b5e670577eb28d23
rbpotter <rbpotter@chromium.org>
2017-05-22 10:52:25 -0700

    Print Preview tests: Remove unnecessary C++ class definition
    It is no longer necessary to manually define a C++ class for
    PrintPreviewWebUITest as it can be autogenerated, so
    chrome/test/data/webui/print_preview.cc and .h are no longer needed.
    Remove these extra files and references to them.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2894243004
    Cr-Commit-Position: refs/heads/master@{#473616}

--- chrome/test/data/webui/print_preview.js
+++ chrome/test/data/webui/print_preview.js
@@ -3,9 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-GEN('#include "base/feature_list.h"');
-GEN('#include "chrome/common/chrome_features.h"');
-
 /**
  * Test fixture for print preview WebUI testing.
  * @constructor
@@ -225,13 +222,6 @@ PrintPreviewWebUITest.prototype = {
   },
 
   /**
-   * Generate a real C++ class; don't typedef.
-   * @type {?string}
-   * @override
-   */
-  typedefCppFixture: null,
-
-  /**
    * @this {PrintPreviewWebUITest}
    * @override
    */
@@ -268,8 +258,6 @@ PrintPreviewWebUITest.prototype = {
         '#page-settings > .right-column > *');
   }
 };
-
-GEN('#include "chrome/test/data/webui/print_preview.h"');
 
 // Test some basic assumptions about the print preview WebUI.
 TEST_F('PrintPreviewWebUITest', 'TestPrinterList', function() {
