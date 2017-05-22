af8e702fd85868a4be40ec9ffbf03524e3f0b625
liaoyuke <liaoyuke@chromium.org>
2017-05-22 06:12:56 -0700

    [ObjC ARC] Converts ios/chrome/browser:browser_internal to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2885163004
    Cr-Commit-Position: refs/heads/master@{#473563}

--- ios/chrome/browser/callback_counter.mm
+++ ios/chrome/browser/callback_counter.mm
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ios/chrome/browser/callback_counter.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 CallbackCounter::CallbackCounter(const FinalCallback& final_callback)
     : callback_count_(0U), final_callback_(final_callback) {
   DCHECK(!final_callback.is_null());
