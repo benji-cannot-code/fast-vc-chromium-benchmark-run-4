027d7841021e1b6ac1b0adece00f0abbab51e00a
dgn <dgn@chromium.org>
2017-05-22 14:17:42 -0700

    [Suggestions] Add visibility change triggers for bottom sheet content
    Add SheetContentStateChangeObserver, which emits events specialised
    for the visibility of a BottomSheetContent, and use it to record
    user actions for the Suggestions bottom sheet
    
    BUG=693593,717704
    
    Review-Url: https://codereview.chromium.org/2862893002
    Cr-Commit-Position: refs/heads/master@{#473699}

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsMetrics.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsMetrics.java
@@ -27,6 +27,14 @@ public abstract class SuggestionsMetrics {
         RecordUserAction.record("Suggestions.SurfaceVisible");
     }
 
+    public static void recordSurfaceHalfVisible() {
+        RecordUserAction.record("Suggestions.SurfaceHalfVisible");
+    }
+
+    public static void recordSurfaceFullyVisible() {
+        RecordUserAction.record("Suggestions.SurfaceFullyVisible");
+    }
+
     public static void recordSurfaceHidden() {
         RecordUserAction.record("Suggestions.SurfaceHidden");
     }
