2467dbecccf9b8dbf7ac89814131ab29d47a99b4
hs1217.lee <hs1217.lee@samsung.com>
2017-05-22 02:43:58 -0700

    Rewrite UpdateMarkerRenderedRect() to use EphemeralRange.
    this patch is last step to replace Range with EphemeralRange in
    updateMarkerRenderedRect().
    
    we are progressing three steps.
    [1/3] Make RenderedRectsForMarkers() to ignore disconnected nodes.
     (https://codereview.chromium.org/2776103002)
    [2/3] In-place change of Range::textRects() and boundingBox()
     to EphemeralRange version in "Range.cpp"
     (https://codereview.chromium.org/2810313003)
    [3/3] Rewrite UpdateMarkerRenderedRect() to use EphemeralRange
     instead of Range (this patch)
    
    BUG=691202
    
    Review-Url: https://codereview.chromium.org/2885103004
    Cr-Commit-Position: refs/heads/master@{#473534}

--- third_party/WebKit/Source/core/editing/markers/DocumentMarkerController.cpp
+++ third_party/WebKit/Source/core/editing/markers/DocumentMarkerController.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/NodeTraversal.h"
 #include "core/dom/Range.h"
 #include "core/dom/Text.h"
+#include "core/editing/VisibleUnits.h"
 #include "core/editing/iterators/TextIterator.h"
 #include "core/editing/markers/CompositionMarkerListImpl.h"
 #include "core/editing/markers/DocumentMarkerListEditor.h"
@@ -202,23 +203,10 @@ void DocumentMarkerController::RemoveMarkersInRange(
 
 static void UpdateMarkerRenderedRect(const Node& node,
                                      RenderedDocumentMarker& marker) {
-  Range* range = Range::Create(node.GetDocument());
-  // The offsets of the marker may be out-dated, so check for exceptions.
-  DummyExceptionStateForTesting exception_state;
-  range->setStart(&const_cast<Node&>(node), marker.StartOffset(),
-                  exception_state);
-  if (!exception_state.HadException()) {
-    range->setEnd(&const_cast<Node&>(node), marker.EndOffset(),
-                  IGNORE_EXCEPTION_FOR_TESTING);
-  }
-  if (!exception_state.HadException()) {
-    // TODO(yosin): Once we have a |EphemeralRange| version of |boundingBox()|,
-    // we should use it instead of |Range| version.
-    marker.SetRenderedRect(LayoutRect(range->BoundingBox()));
-  } else {
-    marker.NullifyRenderedRect();
-  }
-  range->Dispose();
+  const Position startPosition(&const_cast<Node&>(node), marker.StartOffset());
+  const Position endPostion(&const_cast<Node&>(node), marker.EndOffset());
+  EphemeralRange range(startPosition, endPostion);
+  marker.SetRenderedRect(LayoutRect(ComputeTextRect(range)));
 }
 
 // Markers are stored in order sorted by their start offset.
