5f048a741e33f20e884182232f89d306ec58471b
rhalavati <rhalavati@chromium.org>
2017-05-22 21:30:25 -0700

    Network traffic annotation added to Service Worker unittests.
    Network traffic annotation is added to network requests of:
    content/browser/service_worker/service_worker_request_handler_unittest.cc
    content/browser/service_worker/service_worker_url_request_job_unittest.cc
    
    BUG=656607
    
    Review-Url: https://codereview.chromium.org/2896793002
    Cr-Commit-Position: refs/heads/master@{#473808}

--- content/browser/service_worker/service_worker_request_handler_unittest.cc
+++ content/browser/service_worker/service_worker_request_handler_unittest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/request_context_type.h"
 #include "content/public/common/resource_type.h"
 #include "content/public/test/test_browser_thread_bundle.h"
+#include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
 #include "net/url_request/redirect_info.h"
 #include "net/url_request/url_request_context.h"
 #include "net/url_request/url_request_job.h"
@@ -64,7 +65,8 @@ class ServiceWorkerRequestHandlerTest : public testing::Test {
                                                  const std::string& method) {
     std::unique_ptr<net::URLRequest> request =
         url_request_context_.CreateRequest(GURL(url), net::DEFAULT_PRIORITY,
-                                           &url_request_delegate_);
+                                           &url_request_delegate_,
+                                           TRAFFIC_ANNOTATION_FOR_TESTS);
     request->set_method(method);
     return request;
   }
