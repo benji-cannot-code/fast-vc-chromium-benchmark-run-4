2c2714d95c1b3a73aae80c17506782865f824355
qiangchen <qiangchen@chromium.org>
2017-05-23 16:50:59 -0700

    Dynamically Adjust Screen Item Size According To Item Count
    When Mac has two screens, and we try to do a screen share,
    the second screen looks missing. It is actually there, but
    it is in the second row, and the second row is just out of
    the visual region, so one needs to scroll the list to see it,
    and thus appears missing.
    
    This CL shrinks the item size when Mac has multiple screens,
    so we can display two items in a row, and the second row is
    partially visible, and thus no confusion for the end user.
    
    BUG=721387
    
    Review-Url: https://codereview.chromium.org/2890233003
    Cr-Commit-Position: refs/heads/master@{#474110}

--- chrome/browser/ui/cocoa/media_picker/desktop_media_picker_controller.mm
+++ chrome/browser/ui/cocoa/media_picker/desktop_media_picker_controller.mm
@@ -41,6 +41,8 @@ const CGFloat kThumbnailWidth = 150;
 const CGFloat kThumbnailHeight = 150;
 const CGFloat kSingleScreenWidth = 300;
 const CGFloat kSingleScreenHeight = 300;
+const CGFloat kMultipleScreenWidth = 220;
+const CGFloat kMultipleScreenHeight = 220;
 const CGFloat kFramePadding = 20;
 const CGFloat kControlSpacing = 10;
 const CGFloat kExcessButtonPadding = 6;
@@ -280,9 +282,12 @@ NSString* const kTitleId = @"title";
 
 - (void)createSourceViewsAtOrigin:(NSPoint)origin {
   if (screenList_) {
-    screenBrowser_.reset([[self
-        createImageBrowserWithSize:NSMakeSize(kSingleScreenWidth,
-                                              kSingleScreenHeight)] retain]);
+    const bool is_single = screenList_->GetSourceCount() <= 1;
+    const CGFloat width = is_single ? kSingleScreenWidth : kMultipleScreenWidth;
+    const CGFloat height =
+        is_single ? kSingleScreenHeight : kMultipleScreenHeight;
+    screenBrowser_.reset(
+        [[self createImageBrowserWithSize:NSMakeSize(width, height)] retain]);
   }
 
   if (windowList_) {
@@ -603,7 +608,6 @@ NSString* const kTitleId = @"title";
   // "Entire Screen", because it is redundant with tab label "Your Entire
   // Screen".
   [item setTitleHidden:browser == screenBrowser_ && [items count] == 1];
-
   return item;
 }
 
@@ -694,10 +698,16 @@ NSString* const kTitleId = @"title";
   if (sourceType == DesktopMediaID::TYPE_WEB_CONTENTS) {
     // Memorizing selection.
     [self setTabBrowserIndex:selectedIndex];
-  } else if (sourceType == DesktopMediaID::TYPE_SCREEN && [items count] == 1) {
-    // Preselect the first screen source.
-    [browser setSelectionIndexes:[NSIndexSet indexSetWithIndex:0]
-            byExtendingSelection:NO];
+  } else if (sourceType == DesktopMediaID::TYPE_SCREEN) {
+    if ([items count] == 1) {
+      // Preselect the first screen source.
+      [browser setSelectionIndexes:[NSIndexSet indexSetWithIndex:0]
+              byExtendingSelection:NO];
+    } else if ([items count] == 2) {
+      // Switch to multiple sources mode.
+      [browser
+          setCellSize:NSMakeSize(kMultipleScreenWidth, kMultipleScreenHeight)];
+    }
   }
 
   NSString* autoselectSource = base::SysUTF8ToNSString(
@@ -732,6 +742,8 @@ NSString* const kTitleId = @"title";
     [browser setSelectionIndexes:[NSIndexSet indexSet] byExtendingSelection:NO];
   }
   [items removeObjectAtIndex:index];
+  if (sourceType == DesktopMediaID::TYPE_SCREEN && [items count] == 1)
+    [browser setCellSize:NSMakeSize(kSingleScreenWidth, kSingleScreenHeight)];
   [browser reloadData];
 }
 
