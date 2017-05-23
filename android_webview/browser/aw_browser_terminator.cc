067824e29c4cd01497ae76fa8e52e8f745464ed0
gsennton <gsennton@chromium.org>
2017-05-23 05:59:25 -0700

    [Android WebView] Trigger minidump uploading on renderer crash.
    In MultiProcess mode the WebView renderer process can crash without
    bringing down the browser process.
    Such a crash causes a minidump to be generated - with this CL that
    minidump is handled (copied, later to be uploaded) quickly after the
    renderer process crash instead of during the next WebView startup.
    
    BUG=674145
    
    Review-Url: https://codereview.chromium.org/2833893002
    Cr-Commit-Position: refs/heads/master@{#473871}

--- android_webview/browser/aw_browser_terminator.cc
+++ android_webview/browser/aw_browser_terminator.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <unistd.h>
 
+#include "android_webview/browser/aw_browser_process.h"
 #include "android_webview/browser/aw_render_process_gone_delegate.h"
 #include "android_webview/common/aw_descriptors.h"
 #include "android_webview/common/crash_reporter/aw_microdump_crash_reporter.h"
@@ -86,6 +87,13 @@ void OnRenderProcessGoneDetail(int child_process_id,
       }
     }
   }
+
+  // By this point we have moved the minidump to the crash directory, so it can
+  // now be copied and uploaded. This is guaranteed by the order in which we
+  // register breakpad::CrashDumpManager and AwBrowserTerminator as
+  // breakpad::CrashDumpObserver clients over in AwBrowserMainParts
+  // (CrashDumpManager is registered first).
+  TriggerMinidumpUploading();
 }
 
 }  // namespace
