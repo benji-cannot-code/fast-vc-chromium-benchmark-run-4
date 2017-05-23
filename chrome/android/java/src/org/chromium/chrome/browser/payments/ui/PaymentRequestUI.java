496f39467051d637a14439e947f49fd35b50b4bd
wuandy <wuandy@chromium.org>
2017-05-23 13:11:48 -0700

    enable modifiers for native apps
    1. set web-payments-modifiers to enabled by default
    2. implement the serialization of modifiers to native apps through intent
    3. integration test for app modifiers
    
    BUG=719666
    
    Review-Url: https://codereview.chromium.org/2885443003
    Cr-Commit-Position: refs/heads/master@{#474022}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/ui/PaymentRequestUI.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ui/PaymentRequestUI.java
@@ -1361,6 +1361,11 @@ public class PaymentRequestUI implements DialogInterface.OnDismissListener, View
     }
 
     @VisibleForTesting
+    public TextView getOrderSummaryTotalTextViewForTest() {
+        return mOrderSummarySection.getSummaryRightTextView();
+    }
+
+    @VisibleForTesting
     public ViewGroup getShippingAddressSectionForTest() {
         return mShippingAddressSection;
     }
