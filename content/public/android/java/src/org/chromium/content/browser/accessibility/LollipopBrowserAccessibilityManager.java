51fac059790aeab18d368195182347e955d05a87
dmazzoni <dmazzoni@chromium.org>
2017-05-23 11:24:14 -0700

    Expose the language to Android accessibility services using LocaleSpans
    TalkBack and other accessibility services on Android now pay attention to
    LocaleSpans to determine what language to use when speaking text. Wrap
    text in AccessibilityNodeInfo in a LocaleSpan indicating the correct
    language, but for efficiency, only do this when it's different than the
    system language.
    
    BUG=725134
    
    Review-Url: https://codereview.chromium.org/2902533002
    Cr-Commit-Position: refs/heads/master@{#473989}

--- content/public/android/java/src/org/chromium/content/browser/accessibility/LollipopBrowserAccessibilityManager.java
+++ content/public/android/java/src/org/chromium/content/browser/accessibility/LollipopBrowserAccessibilityManager.java
@@ -6,7 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.content.browser.accessibility;
 
 import android.annotation.TargetApi;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Build;
+import android.text.SpannableString;
+import android.text.style.LocaleSpan;
 import android.util.SparseArray;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
@@ -14,6 +20,8 @@ import android.view.accessibility.AccessibilityNodeInfo.AccessibilityAction;
 
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.content.browser.ContentViewCore;
+
+import java.util.Locale;
 
 /**
  * Subclass of BrowserAccessibilityManager for Lollipop.
@@ -23,10 +31,21 @@ import org.chromium.content.browser.ContentViewCore;
 public class LollipopBrowserAccessibilityManager extends KitKatBrowserAccessibilityManager {
     private static SparseArray<AccessibilityAction> sAccessibilityActionMap =
             new SparseArray<AccessibilityAction>();
+    private String mSystemLanguageTag;
 
     LollipopBrowserAccessibilityManager(long nativeBrowserAccessibilityManagerAndroid,
             ContentViewCore contentViewCore) {
         super(nativeBrowserAccessibilityManagerAndroid, contentViewCore);
+
+        // Cache the system language and set up a listener for when it changes.
+        IntentFilter filter = new IntentFilter(Intent.ACTION_LOCALE_CHANGED);
+        mContentViewCore.getContext().registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                mSystemLanguageTag = Locale.getDefault().toLanguageTag();
+            }
+        }, filter);
+        mSystemLanguageTag = Locale.getDefault().toLanguageTag();
     }
 
     @Override
@@ -123,5 +142,22 @@ public class LollipopBrowserAccessibilityManager extends KitKatBrowserAccessibil
             sAccessibilityActionMap.put(actionId, action);
         }
         node.addAction(action);
+    }
+
+    @Override
+    protected CharSequence computeText(String text, boolean annotateAsLink, String language) {
+        CharSequence charSequence = super.computeText(text, annotateAsLink, language);
+        if (!language.isEmpty() && !language.equals(mSystemLanguageTag)) {
+            SpannableString spannable;
+            if (charSequence instanceof SpannableString) {
+                spannable = (SpannableString) charSequence;
+            } else {
+                spannable = new SpannableString(charSequence);
+            }
+            Locale locale = Locale.forLanguageTag(language);
+            spannable.setSpan(new LocaleSpan(locale), 0, spannable.length(), 0);
+            return spannable;
+        }
+        return charSequence;
     }
 }
