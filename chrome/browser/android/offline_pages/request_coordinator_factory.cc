5a66e3c45eb92a317637b71b2691e373ae41cd46
dewittj <dewittj@chromium.org>
2017-05-23 11:35:23 -0700

    [Offline Pages] Add an observer for requests in the CCT namespace.
    This will allow us to track when requests finish.  Adds a hook so that
    internal code can have different behavior.  See bug for details.
    
    BUG=720782
    
    Review-Url: https://codereview.chromium.org/2888273003
    Cr-Commit-Position: refs/heads/master@{#473994}

--- chrome/browser/android/offline_pages/request_coordinator_factory.cc
+++ chrome/browser/android/offline_pages/request_coordinator_factory.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequenced_task_runner.h"
 #include "base/task_scheduler/post_task.h"
 #include "chrome/browser/android/offline_pages/background_scheduler_bridge.h"
+#include "chrome/browser/android/offline_pages/cct_request_observer.h"
 #include "chrome/browser/android/offline_pages/downloads/offline_page_notification_bridge.h"
 #include "chrome/browser/android/offline_pages/offline_page_model_factory.h"
 #include "chrome/browser/android/offline_pages/prerendering_offliner.h"
@@ -89,6 +90,8 @@ KeyedService* RequestCoordinatorFactory::BuildServiceInstanceFor(
   DownloadNotifyingObserver::CreateAndStartObserving(
       request_coordinator,
       base::MakeUnique<android::OfflinePageNotificationBridge>());
+
+  CCTRequestObserver::AttachToRequestCoordinator(request_coordinator);
 
   return request_coordinator;
 }
