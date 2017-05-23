a031fa7c854b9981a943cb2932c1c8e8099a4cb2
estark <estark@chromium.org>
2017-05-23 10:35:34 -0700

    Do not require Expect-CT report-uris to be quoted
    Requiring quoted URLs was cargo-culted from the HPKP implementation. The HPKP
    spec does not actually say that report-uris must be quoted -- it's simply that
    all the examples quote them. So it's possibly a bug that Chrome's HPKP implementation
    requires quoted report-uris. The Expect-CT spec doesn't say anything about quoting
    report-uris nor do I see a reason that it should, so Chrome's implementation shouldn't
    require them.
    
    BUG=679012
    
    Review-Url: https://codereview.chromium.org/2895373002
    Cr-Commit-Position: refs/heads/master@{#473959}

--- net/http/http_security_headers.cc
+++ net/http/http_security_headers.cc
@@ -369,7 +369,7 @@ bool ParseHPKPReportOnlyHeader(const std::string& value,
 // "Expect-CT" ":"
 //     "max-age" "=" delta-seconds
 //     [ "," "enforce" ]
-//     [ "," "report-uri" "=" uri-reference ]
+//     [ "," "report-uri" "=" absolute-URI ]
 bool ParseExpectCTHeader(const std::string& value,
                          base::TimeDelta* max_age,
                          bool* enforce,
@@ -413,9 +413,6 @@ bool ParseExpectCTHeader(const std::string& value,
       // "A given directive MUST NOT appear more than once in a given header
       // field."
       if (has_report_uri)
-        return false;
-      // report-uris are always quoted.
-      if (!name_value_pairs.value_is_quoted())
         return false;
 
       has_report_uri = true;
