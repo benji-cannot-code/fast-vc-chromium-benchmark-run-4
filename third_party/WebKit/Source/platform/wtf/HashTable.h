de8482751a4a88a2b527c1d79e1961ccaef4d074
Yuta Kitamura <yutak@chromium.org>
2017-05-23 14:47:20 +0900

    WTF::HashTable: Reduce const_cast usage.
    It seems like the original author did not consider constness seriously,
    and there are a number of bogus const_casts in the code. This patch cleans
    them up.
    
    One const_cast is newly introduced in DeleteBucket(), where the table fills
    a "deleted value" to the slot that's about to get deleted. I think this usage
    makes sense and is probably better than the current situation (i.e.
    const_cast in each erase() overload).
    
    Bug: None
    Change-Id: I9cca5065f49fa6d1c6ed47c7ecbce956afb0637d
    Reviewed-on: https://chromium-review.googlesource.com/509334
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Reviewed-by: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#473832}

--- third_party/WebKit/Source/platform/wtf/HashTable.h
+++ third_party/WebKit/Source/platform/wtf/HashTable.h
@@ -777,6 +777,9 @@ class HashTable final
   ValueType* Lookup(KeyPeekInType key) {
     return Lookup<IdentityTranslatorType, KeyPeekInType>(key);
   }
+  const ValueType* Lookup(KeyPeekInType key) const {
+    return Lookup<IdentityTranslatorType, KeyPeekInType>(key);
+  }
   template <typename HashTranslator, typename T>
   ValueType* Lookup(const T&);
   template <typename HashTranslator, typename T>
@@ -824,7 +827,7 @@ class HashTable final
   template <typename HashTranslator, typename T>
   LookupType LookupForWriting(const T&);
 
-  void erase(ValueType*);
+  void erase(const ValueType*);
 
   bool ShouldExpand() const {
     return (key_count_ + deleted_count_) * kMaxLoad >= table_size_;
@@ -851,9 +854,10 @@ class HashTable final
   ValueType* Reinsert(ValueType&&);
 
   static void InitializeBucket(ValueType& bucket);
-  static void DeleteBucket(ValueType& bucket) {
+  static void DeleteBucket(const ValueType& bucket) {
     bucket.~ValueType();
-    Traits::ConstructDeletedValue(bucket, Allocator::kIsGarbageCollected);
+    Traits::ConstructDeletedValue(const_cast<ValueType&>(bucket),
+                                  Allocator::kIsGarbageCollected);
   }
 
   FullLookupType MakeLookupResult(ValueType* position,
@@ -865,13 +869,13 @@ class HashTable final
   iterator MakeIterator(ValueType* pos) {
     return iterator(pos, table_ + table_size_, this);
   }
-  const_iterator MakeConstIterator(ValueType* pos) const {
+  const_iterator MakeConstIterator(const ValueType* pos) const {
     return const_iterator(pos, table_ + table_size_, this);
   }
   iterator MakeKnownGoodIterator(ValueType* pos) {
     return iterator(pos, table_ + table_size_, this, kHashItemKnownGood);
   }
-  const_iterator MakeKnownGoodConstIterator(ValueType* pos) const {
+  const_iterator MakeKnownGoodConstIterator(const ValueType* pos) const {
     return const_iterator(pos, table_ + table_size_, this, kHashItemKnownGood);
   }
 
@@ -1010,6 +1014,7 @@ template <typename HashTranslator, typename T>
 inline Value*
 HashTable<Key, Value, Extractor, HashFunctions, Traits, KeyTraits, Allocator>::
     Lookup(const T& key) {
+  // Call the const version of Lookup<HashTranslator, T>().
   return const_cast<Value*>(
       const_cast<const HashTable*>(this)->Lookup<HashTranslator>(key));
 }
@@ -1430,7 +1435,7 @@ inline typename HashTable<Key,
                           Allocator>::const_iterator
 HashTable<Key, Value, Extractor, HashFunctions, Traits, KeyTraits, Allocator>::
     Find(const T& key) const {
-  ValueType* entry = const_cast<HashTable*>(this)->Lookup<HashTranslator>(key);
+  const ValueType* entry = Lookup<HashTranslator>(key);
   if (!entry)
     return end();
 
@@ -1452,7 +1457,7 @@ bool HashTable<Key,
                Traits,
                KeyTraits,
                Allocator>::Contains(const T& key) const {
-  return const_cast<HashTable*>(this)->Lookup<HashTranslator>(key);
+  return Lookup<HashTranslator>(key);
 }
 
 template <typename Key,
@@ -1468,7 +1473,7 @@ void HashTable<Key,
                HashFunctions,
                Traits,
                KeyTraits,
-               Allocator>::erase(ValueType* pos) {
+               Allocator>::erase(const ValueType* pos) {
   RegisterModification();
 #if DUMP_HASHTABLE_STATS
   atomicIncrement(&HashTableStats::instance().numRemoves);
@@ -1499,7 +1504,7 @@ HashTable<Key, Value, Extractor, HashFunctions, Traits, KeyTraits, Allocator>::
     erase(iterator it) {
   if (it == end())
     return;
-  erase(const_cast<ValueType*>(it.iterator_.position_));
+  erase(it.iterator_.position_);
 }
 
 template <typename Key,
@@ -1514,7 +1519,7 @@ HashTable<Key, Value, Extractor, HashFunctions, Traits, KeyTraits, Allocator>::
     erase(const_iterator it) {
   if (it == end())
     return;
-  erase(const_cast<ValueType*>(it.position_));
+  erase(it.position_);
 }
 
 template <typename Key,
