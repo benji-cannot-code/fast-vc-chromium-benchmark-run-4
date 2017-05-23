17d46cd1196a2a0884e0ebfd2e4a2506db0e3027
jamescook <jamescook@chromium.org>
2017-05-23 10:11:14 -0700

    chromeos: Initialize StatisticsRecorder in AshTestSuite
    This fixes flake in ash_unittests in tests that record and read back
    histograms, in particular PointerMetricsRecorderTest.
    
    This is similar to the initialization in ComponentsTestSuite,
    ContentTestSuiteBase, etc.
    
    BUG=725287
    TEST=run ash_unittests locally, PointerMetricsRecorderTest doesn't retry
    
    Review-Url: https://codereview.chromium.org/2901703002
    Cr-Commit-Position: refs/heads/master@{#473952}

--- ash/test/ash_test_suite.cc
+++ ash/test/ash_test_suite.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/i18n/rtl.h"
+#include "base/metrics/statistics_recorder.h"
 #include "base/path_service.h"
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -29,6 +30,8 @@ AshTestSuite::~AshTestSuite() {}
 
 void AshTestSuite::Initialize() {
   base::TestSuite::Initialize();
+  // Ensure histograms hit during tests are registered properly.
+  base::StatisticsRecorder::Initialize();
   gl::GLSurfaceTestSupport::InitializeOneOff();
 
   gfx::RegisterPathProvider();
