bef5cd89959d09917ab328a078eb7fd1f05bd7f5
pfeldman <pfeldman@chromium.org>
2017-05-23 14:41:39 -0700

    DevTools: disable input event processing while auditing with lighthouse.
    Review-Url: https://codereview.chromium.org/2898793002
    Cr-Commit-Position: refs/heads/master@{#474068}

--- content/browser/devtools/protocol/input_handler.h
+++ content/browser/devtools/protocol/input_handler.h
@@ -76,6 +76,8 @@ class InputHandler : public DevToolsDomainHandler,
                                       Maybe<int> modifiers,
                                       Maybe<int> click_count) override;
 
+  Response SetIgnoreInputEvents(bool ignore) override;
+
   void SynthesizePinchGesture(
       int x,
       int y,
@@ -142,6 +144,7 @@ class InputHandler : public DevToolsDomainHandler,
   float page_scale_factor_;
   gfx::SizeF scrollable_viewport_size_;
   int last_id_;
+  bool ignore_input_events_ = false;
   base::WeakPtrFactory<InputHandler> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(InputHandler);
