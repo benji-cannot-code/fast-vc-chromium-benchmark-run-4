6d520f0395f06bcdc907c35209ef30624a84fc0e
Mohsen Izadi <mohsen@chromium.org>
2017-05-23 00:10:12 -0400

    Remove spinner from connectivity rows in system menu
    According to MD specs for system menu detailed views, there is no
    spinner for network/vpn/bluetooth rows that are trying to connect.
    
    BUG=724680
    TEST=none
    
    Change-Id: If610554c95b1c35ffbcc32bd6ad9f51927bf558d
    Reviewed-on: https://chromium-review.googlesource.com/509231
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#473817}

--- ash/system/bluetooth/tray_bluetooth.cc
+++ ash/system/bluetooth/tray_bluetooth.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/system_tray.h"
 #include "ash/system/tray/system_tray_controller.h"
 #include "ash/system/tray/system_tray_notifier.h"
-#include "ash/system/tray/throbber_view.h"
 #include "ash/system/tray/tray_constants.h"
 #include "ash/system/tray/tray_details_view.h"
 #include "ash/system/tray/tray_item_more.h"
@@ -351,9 +350,6 @@ class BluetoothDetailedView : public TrayDetailsView {
   void SetupConnectingItem(HoverHighlightView* container) {
     container->SetSubText(l10n_util::GetStringUTF16(
         IDS_ASH_STATUS_TRAY_NETWORK_STATUS_CONNECTING));
-    ThrobberView* throbber = new ThrobberView;
-    throbber->Start();
-    container->AddRightView(throbber);
   }
 
   // Returns true if the device with |device_id| is found in |device_list|.
--- ash/system/network/vpn_list_view.cc
+++ ash/system/network/vpn_list_view.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/hover_highlight_view.h"
 #include "ash/system/tray/system_menu_button.h"
 #include "ash/system/tray/system_tray_controller.h"
-#include "ash/system/tray/throbber_view.h"
 #include "ash/system/tray/tray_constants.h"
 #include "ash/system/tray/tray_popup_utils.h"
 #include "ash/system/tray/tri_view.h"
@@ -229,9 +228,6 @@ void VPNListNetworkEntry::SetupConnectingItem(const base::string16& text,
   AddIconAndLabels(
       image, text,
       l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_NETWORK_STATUS_CONNECTING));
-  ThrobberView* throbber = new ThrobberView;
-  throbber->Start();
-  AddRightView(throbber);
 }
 
 }  // namespace
