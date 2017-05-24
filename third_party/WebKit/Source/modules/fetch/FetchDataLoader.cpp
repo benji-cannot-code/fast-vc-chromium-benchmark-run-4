792f1e66949cdce61f2a72d61707a82ddaafb96d
eero.hakkinen <eero.hakkinen@intel.com>
2017-05-23 23:53:32 -0700

    Use ParsedContentDisposition for Content-Disposition parsing
    BUG=455103
    
    Review-Url: https://codereview.chromium.org/2901983002
    Cr-Commit-Position: refs/heads/master@{#474188}

--- third_party/WebKit/Source/modules/fetch/FetchDataLoader.cpp
+++ third_party/WebKit/Source/modules/fetch/FetchDataLoader.cpp
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/fetch/MultipartParser.h"
 #include "mojo/public/cpp/system/simple_watcher.h"
 #include "platform/HTTPNames.h"
-#include "platform/network/ParsedContentType.h"
+#include "platform/network/ParsedContentDisposition.h"
 #include "platform/wtf/Functional.h"
 #include "platform/wtf/PtrUtil.h"
 #include "platform/wtf/text/StringBuilder.h"
@@ -329,17 +329,10 @@ class FetchDataLoaderAsFormData final : public FetchDataLoader,
   class Entry {
    public:
     bool Initialize(const HTTPHeaderMap& header_fields) {
-      // TODO(e_hakkinen): Use a proper Content-Disposition parser instead of
-      // converting content disposition to a faked content type and parsing
-      // that.
-      const String fakePrefix = "x-fake/";
-      const ParsedContentType disposition(
-          fakePrefix + header_fields.Get(HTTPNames::Content_Disposition));
-      // The faked MIME type without the faked prefix is a proper disposition
-      // type.
-      const String disposition_type =
-          disposition.MimeType().Substring(fakePrefix.length());
-      filename_ = disposition.ParameterValueForName("filename");
+      const ParsedContentDisposition disposition(
+          header_fields.Get(HTTPNames::Content_Disposition));
+      const String disposition_type = disposition.Type();
+      filename_ = disposition.Filename();
       name_ = disposition.ParameterValueForName("name");
       blob_data_.reset();
       string_builder_.reset();
