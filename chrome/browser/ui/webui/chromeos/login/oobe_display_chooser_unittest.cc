71c9d3fd647252b9dba880985fef194796af2758
felixe <felixe@chromium.org>
2017-05-24 00:27:22 -0700

    Avoid changing primary display in DisplayObserver call
    BUG=668449,722564
    
    Review-Url: https://codereview.chromium.org/2885153004
    Cr-Commit-Position: refs/heads/master@{#474196}

--- chrome/browser/ui/webui/chromeos/login/oobe_display_chooser_unittest.cc
+++ chrome/browser/ui/webui/chromeos/login/oobe_display_chooser_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/display/display_configuration_controller.h"
 #include "ash/shell.h"
 #include "ash/test/ash_test_base.h"
+#include "base/run_loop.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/display/display.h"
 #include "ui/display/display_observer.h"
@@ -64,6 +65,7 @@ TEST_F(OobeDisplayChooserTest, PreferTouchAsPrimary) {
 
   EXPECT_EQ(ids[0], GetPrimaryDisplay());
   display_chooser.TryToPlaceUiOnTouchDisplay();
+  base::RunLoop().RunUntilIdle();
 
   EXPECT_EQ(ids[1], GetPrimaryDisplay());
 }
@@ -78,6 +80,7 @@ TEST_F(OobeDisplayChooserTest, AddingSecondTouchDisplayShouldbeNOP) {
 
   EXPECT_EQ(ids[0], GetPrimaryDisplay());
   display_chooser.TryToPlaceUiOnTouchDisplay();
+  base::RunLoop().RunUntilIdle();
 
   EXPECT_EQ(ids[0], GetPrimaryDisplay());
 }
