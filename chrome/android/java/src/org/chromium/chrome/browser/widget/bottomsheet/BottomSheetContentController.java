6d20647e6ed293ac7b642655bbcd6c834746b52b
mdjones <mdjones@chromium.org>
2017-05-24 11:31:07 -0700

    [Home] Show bottom nav above keyboard
    This change allows the bottom navigation to be shown above the
    keyboard when the bottom sheet is open. An onLayoutChange event is
    triggered when the keyboard is shown or hidden. The height of the
    window and keyboard are determined and sent as a new event,
    "onSheetLayout" to any BottomSheetObservers.
    
    When the keyboard is focused while viewing any of the bottom sheet
    contents, extra padding is added to the content container. This
    shrinks the view so it remains visible while allowing the bottom
    sheet to retain it's original height. The result of this is seeing
    the default tab theme color behind the keyboard when it appears
    instead of seeing black flashes.
    
    Since the navigation is now visible when the omnibox is focused from
    the peeking state (thus showing the placeholder sheet content) the
    bottom nav shows none of the tabs as selected. This allows users to
    view suggestions from this state if they wish to.
    
    BUG=706249
    
    Review-Url: https://codereview.chromium.org/2892953005
    Cr-Commit-Position: refs/heads/master@{#474363}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheetContentController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheetContentController.java
@@ -77,9 +77,18 @@ public class BottomSheetContentController extends BottomNavigationView
     private final BottomSheetObserver mBottomSheetObserver = new EmptyBottomSheetObserver() {
         @Override
         public void onSheetOffsetChanged(float heightFraction) {
-            float offsetY = (mBottomSheet.getMinOffset() - mBottomSheet.getSheetOffsetFromBottom())
-                    + mDistanceBelowToolbarPx;
-            setTranslationY((int) Math.max(offsetY, 0f));
+            // If the omnibox is not focused, allow the navigation bar to set its Y translation.
+            if (!mOmniboxHasFocus) {
+                float offsetY =
+                        (mBottomSheet.getMinOffset() - mBottomSheet.getSheetOffsetFromBottom())
+                        + mDistanceBelowToolbarPx;
+                setTranslationY(Math.max(offsetY, 0f));
+
+                if (mBottomSheet.getTargetSheetState() != BottomSheet.SHEET_STATE_PEEK
+                        && mSelectedItemId == PLACEHOLDER_ID) {
+                    showBottomSheetContent(R.id.action_home);
+                }
+            }
             setVisibility(MathUtils.areFloatsEqual(heightFraction, 0f) ? View.GONE : View.VISIBLE);
 
             mSnackbarManager.dismissAllSnackbars();
@@ -125,6 +134,11 @@ public class BottomSheetContentController extends BottomNavigationView
                 mBottomSheet.setSheetState(BottomSheet.SHEET_STATE_FULL, true);
             }
         }
+
+        @Override
+        public void onSheetLayout(int windowHeight, int containerHeight) {
+            setTranslationY(containerHeight - windowHeight);
+        }
     };
 
     private BottomSheet mBottomSheet;
@@ -136,6 +150,7 @@ public class BottomSheetContentController extends BottomNavigationView
     private ChromeActivity mActivity;
     private boolean mShouldOpenSheetOnNextContentChange;
     private PlaceholderSheetContent mPlaceholderContent;
+    private boolean mOmniboxHasFocus;
 
     public BottomSheetContentController(Context context, AttributeSet atts) {
         super(context, atts);
@@ -219,11 +234,15 @@ public class BottomSheetContentController extends BottomNavigationView
      * @param hasFocus Whether or not the omnibox has focus.
      */
     public void onOmniboxFocusChange(boolean hasFocus) {
+        mOmniboxHasFocus = hasFocus;
+
         // If the omnibox is being focused, show the placeholder.
         if (hasFocus && mBottomSheet.getSheetState() != BottomSheet.SHEET_STATE_HALF
                 && mBottomSheet.getSheetState() != BottomSheet.SHEET_STATE_FULL) {
             mBottomSheet.showContent(mPlaceholderContent);
             mBottomSheet.endTransitionAnimations();
+            if (mSelectedItemId > 0) getMenu().findItem(mSelectedItemId).setChecked(false);
+            mSelectedItemId = PLACEHOLDER_ID;
         }
 
         if (!hasFocus && mBottomSheet.getCurrentSheetContent() == mPlaceholderContent) {
@@ -234,6 +253,8 @@ public class BottomSheetContentController extends BottomNavigationView
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         if (mSelectedItemId == item.getItemId()) return false;
+
+        mBottomSheet.defocusOmnibox();
 
         mSnackbarManager.dismissAllSnackbars();
         showBottomSheetContent(item.getItemId());
@@ -291,7 +312,7 @@ public class BottomSheetContentController extends BottomNavigationView
         // There are some bugs related to programatically selecting menu items that are fixed in
         // newer support library versions.
         // TODO(twellington): remove this after the support library is rolled.
-        if (mSelectedItemId != 0) getMenu().findItem(mSelectedItemId).setChecked(false);
+        if (mSelectedItemId > 0) getMenu().findItem(mSelectedItemId).setChecked(false);
         mSelectedItemId = navItemId;
         getMenu().findItem(mSelectedItemId).setChecked(true);
 
