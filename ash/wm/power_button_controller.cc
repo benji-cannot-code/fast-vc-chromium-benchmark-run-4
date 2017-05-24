ecce32f221860b61042b18d23bbe24aa9fd82c86
warx <warx@chromium.org>
2017-05-24 11:03:58 -0700

    cros: Make tablet power button behavior depend on having seen accelerometer data
    Changes:
    Making tablet power button behavior enabled only when device has seen accelerometer data update, otherwise the power button should have clamshell-behavior.
    
    BUG=b/38153788
    TEST=tested on cyan that tablet power button still works properly;
    added test coverage.
    
    Review-Url: https://codereview.chromium.org/2904603006
    Cr-Commit-Position: refs/heads/master@{#474353}

--- ash/wm/power_button_controller.cc
+++ ash/wm/power_button_controller.cc
@@ -26,23 +26,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 PowerButtonController::PowerButtonController(LockStateController* controller)
-    : power_button_down_(false),
-      lock_button_down_(false),
-      volume_down_pressed_(false),
-      volume_percent_before_screenshot_(0),
-      brightness_is_zero_(false),
-      internal_display_off_and_external_display_on_(false),
-      has_legacy_power_button_(
+    : has_legacy_power_button_(
           base::CommandLine::ForCurrentProcess()->HasSwitch(
               switches::kAuraLegacyPowerButton)),
       lock_state_controller_(controller) {
   chromeos::DBusThreadManager::Get()->GetPowerManagerClient()->AddObserver(
       this);
-  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kAshEnableTouchView)) {
-    tablet_controller_.reset(
-        new TabletPowerButtonController(lock_state_controller_));
-  }
+  chromeos::AccelerometerReader::GetInstance()->AddObserver(this);
   Shell::Get()->display_configurator()->AddObserver(this);
   Shell::Get()->PrependPreTargetHandler(this);
 }
@@ -50,9 +40,10 @@ PowerButtonController::PowerButtonController(LockStateController* controller)
 PowerButtonController::~PowerButtonController() {
   Shell::Get()->RemovePreTargetHandler(this);
   Shell::Get()->display_configurator()->RemoveObserver(this);
-  tablet_controller_.reset();
+  chromeos::AccelerometerReader::GetInstance()->RemoveObserver(this);
   chromeos::DBusThreadManager::Get()->GetPowerManagerClient()->RemoveObserver(
       this);
+  tablet_controller_.reset();
 }
 
 void PowerButtonController::OnScreenBrightnessChanged(double percent) {
@@ -197,9 +188,16 @@ void PowerButtonController::PowerButtonEventReceived(
   OnPowerButtonEvent(down, timestamp);
 }
 
-void PowerButtonController::ResetTabletPowerButtonControllerForTest() {
+void PowerButtonController::OnAccelerometerUpdated(
+    scoped_refptr<const chromeos::AccelerometerUpdate> update) {
+  if (tablet_controller_)
+    return;
   tablet_controller_.reset(
       new TabletPowerButtonController(lock_state_controller_));
+}
+
+void PowerButtonController::ResetTabletPowerButtonControllerForTest() {
+  tablet_controller_.reset();
 }
 
 }  // namespace ash
