6ffe60f0ea5da86b73282cd142ad47e37bfd90ac
Chris Watkins <watk@chromium.org>
2017-05-24 16:03:57 -0700

    media: Delete unused decoder_ member from AVDACodecImage
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ib5949f315aeffd9a7415d64ad1879a438efa58c5
    Reviewed-on: https://chromium-review.googlesource.com/514485
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Chris Watkins <watk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#474695}

--- media/gpu/avda_codec_image.cc
+++ media/gpu/avda_codec_image.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "gpu/command_buffer/service/gles2_cmd_decoder.h"
 #include "gpu/command_buffer/service/texture_manager.h"
 #include "media/base/android/media_codec_bridge_impl.h"
 #include "media/gpu/avda_shared_state.h"
@@ -21,12 +20,10 @@ namespace media {
 
 AVDACodecImage::AVDACodecImage(
     const scoped_refptr<AVDASharedState>& shared_state,
-    MediaCodecBridge* codec,
-    const base::WeakPtr<gpu::gles2::GLES2Decoder>& decoder)
+    MediaCodecBridge* codec)
     : shared_state_(shared_state),
       codec_buffer_index_(kInvalidCodecBufferIndex),
       media_codec_(codec),
-      decoder_(decoder),
       has_surface_texture_(false),
       texture_(0) {}
 
