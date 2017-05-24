df8220b99baeb4ddea1d6a46cf573a8c45834041
lpromero <lpromero@chromium.org>
2017-05-24 04:45:02 -0700

    Clean up: use arraysize macro.
    BUG=675356
    R=sdefresne@chromium.org
    
    Review-Url: https://codereview.chromium.org/2902113003
    Cr-Commit-Position: refs/heads/master@{#474251}

--- ios/chrome/browser/ui/stack_view/stack_view_controller_perftest.mm
+++ ios/chrome/browser/ui/stack_view/stack_view_controller_perftest.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #import "base/mac/bind_objc_block.h"
 #include "base/mac/foundation_util.h"
+#include "base/macros.h"
 #include "base/strings/sys_string_conversions.h"
 #import "base/test/ios/wait_util.h"
 #import "ios/chrome/browser/snapshots/snapshot_manager.h"
@@ -129,10 +130,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-#define ARRAYSIZE(a)            \
-  ((sizeof(a) / sizeof(*(a))) / \
-   static_cast<size_t>(!(sizeof(a) % sizeof(*(a)))))
-
 // Use multiple URLs in the test in case the complexity of a website has an
 // effect on the performance of UIWebView -renderInContext.
 static const char* url_list[] = {
@@ -248,7 +245,7 @@ void StackViewControllerPerfTest::LoadNextURL() {
 
 const GURL StackViewControllerPerfTest::NextURL() {
   current_url_index_++;
-  if (static_cast<unsigned long>(current_url_index_) >= ARRAYSIZE(url_list))
+  if (static_cast<unsigned long>(current_url_index_) >= arraysize(url_list))
     current_url_index_ = 0;
   return GURL(url_list[current_url_index_]);
 }
