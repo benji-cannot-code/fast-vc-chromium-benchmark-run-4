04fcb4f52333f02efd5b9ca6d3b3c9bae02deea7
Matthew Jones <mdjones@chromium.org>
2017-05-24 14:17:40 -0700

    [Home] Fix progress bar theming
    This change overrides onPrimaryColor changed in BottomToolbarPhone
    to only update the progress bar's theme color and skip logic that
    would otherwise block the update.
    
    ToolbarManager has also been changed to cache the primary color
    locally due to some Chrome Home specific logic in the toolbar model.
    
    BUG=726084
    
    Change-Id: I12fb43852825a2b1847920ac12004fd39f90af0b
    Reviewed-on: https://chromium-review.googlesource.com/514482
    Reviewed-by: Theresa Wellington <twellington@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#474507}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -173,6 +173,7 @@ public class ToolbarManager implements ToolbarTabController, UrlFocusChangeListe
 
     private boolean mShouldUpdateTabCount = true;
     private boolean mShouldUpdateToolbarPrimaryColor = true;
+    private int mCurrentThemeColor;
 
     /**
      * Creates a ToolbarManager object.
@@ -1031,9 +1032,10 @@ public class ToolbarManager implements ToolbarTabController, UrlFocusChangeListe
     public void updatePrimaryColor(int color, boolean shouldAnimate) {
         if (!mShouldUpdateToolbarPrimaryColor) return;
 
-        boolean colorChanged = mToolbarModel.getPrimaryColor() != color;
+        boolean colorChanged = mCurrentThemeColor != color;
         if (!colorChanged) return;
 
+        mCurrentThemeColor = color;
         mToolbarModel.setPrimaryColor(color);
         mToolbar.onPrimaryColorChanged(shouldAnimate);
     }
