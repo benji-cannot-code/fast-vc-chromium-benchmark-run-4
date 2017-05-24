9ce37057db197a2416cfc51fd482f83eafbb9dea
amp <amp@chromium.org>
2017-05-23 22:34:44 -0700

    Update fullscreen ux, make floor and ceiling visible.
    BUG=643434,661762
    
    Review-Url: https://codereview.chromium.org/2887773008
    Cr-Commit-Position: refs/heads/master@{#474172}

--- chrome/browser/android/vr_shell/ui_elements/ui_element_debug_id.h
+++ chrome/browser/android/vr_shell/ui_elements/ui_element_debug_id.h
@@ -14,7 +14,19 @@ enum UiElementDebugId {
   kNone = 0,
   kWebVrPermanentHttpSecurityWarning,
   kWebVrTransientHttpSecurityWarning,
+  kContentQuad,
+  kBackplane,
+  kCeiling,
+  kFloor,
+  kFloorGrid,
+  kUrlBar,
+  kLoadingIndicator,
+  kAudioCaptureIndicator,
+  kVideoCaptureIndicator,
+  kScreenCaptureIndicator,
   kCloseButton,
+  kScreenDimmer,
+  kExitWarning,
 };
 
 }  // namespace vr_shell
--- chrome/browser/android/vr_shell/ui_scene_manager.h
+++ chrome/browser/android/vr_shell/ui_scene_manager.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/timer.h"
 #include "base/values.h"
 #include "chrome/browser/android/vr_shell/ui_interface.h"
+#include "device/vr/vr_types.h"
 
 class GURL;
 
@@ -60,6 +61,8 @@ class UiSceneManager {
   void CreateExitWarning();
 
   void ConfigureScene();
+  void ConfigureBackgroundColor(vr::Colorf center_color,
+                                vr::Colorf horizon_color);
   void ConfigureSecurityWarnings();
   void OnSecurityWarningTimer();
   void OnBackButtonClicked();
@@ -78,6 +81,9 @@ class UiSceneManager {
   UiElement* video_capture_indicator_ = nullptr;
   UiElement* screen_capture_indicator_ = nullptr;
   UiElement* screen_dimmer_ = nullptr;
+  UiElement* ceiling_ = nullptr;
+  UiElement* floor_ = nullptr;
+  UiElement* floor_grid_ = nullptr;
   UrlBar* url_bar_ = nullptr;
   LoadingIndicator* loading_indicator_ = nullptr;
 
