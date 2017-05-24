a22e785574c28dc112e195a6c2945b6606019442
Yutaka Hirano <yhirano@chromium.org>
2017-05-24 18:25:58 +0900

    Have SendBeacon timeout settable via finch
    This CL adds kFetchKeepaliveKeepaliveSetting feature, whose "timeout_in_sec"
    controls the timeout duration for SendBeacon (and keepalive specified fetch in
    the future).
    
    We currently have 30sec timeout for SendBeacon, and we are going to run a finch
    experiment about the timeout value.
    
    Bug: 717424
    Change-Id: I66d13d98ac35be70250758fe9b5dd10eb36d8273
    Reviewed-on: https://chromium-review.googlesource.com/513762
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Takeshi Yoshino <tyoshino@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#474308}

--- content/public/common/content_features.h
+++ content/public/common/content_features.h
@@ -29,6 +29,7 @@ CONTENT_EXPORT extern const base::Feature kCompositeOpaqueScrollers;
 CONTENT_EXPORT extern const base::Feature kDocumentWriteEvaluator;
 CONTENT_EXPORT extern const base::Feature kExpensiveBackgroundTimerThrottling;
 CONTENT_EXPORT extern const base::Feature kFeaturePolicy;
+CONTENT_EXPORT extern const base::Feature kFetchKeepaliveTimeoutSetting;
 CONTENT_EXPORT extern const base::Feature kFilterSameOriginTinyPlugin;
 CONTENT_EXPORT extern const base::Feature kFontCacheScaling;
 CONTENT_EXPORT extern const base::Feature
