31d6c388cd1b936c9ae4f2156fcf0694cbbf2a00
klausw <klausw@chromium.org>
2017-05-23 17:32:24 -0700

    Implement EGL context priority if supported, use in VrShell
    On devices where the EGL context priority extension is supported,
    make this available when creating GL contexts, and use it for VrShell's
    VR compositing. The extension is documented here:
    
    https://www.khronos.org/registry/EGL/extensions/IMG/EGL_IMG_context_priority.txt
    
    If the extension is not supported, ignore the requested priority.
    
    As a special case, assume the extension is present if other
    VR-specific extensions are available. It was erroneously not reported
    as present on Daydream devices even though it was implemented, see
    https://github.com/googlevr/gvr-android-sdk/issues/330 .
    
    (This functionality was previously implemented in http://crrev.com/2586803003
    but was reverted due to synchronization issues with non-virtualized contexts.
    In this new version, only VrShell's private native rendering context uses this
    option, so WebGL can continue using virtualized contexts as before.)
    
    BUG=725684
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2902863002
    Cr-Commit-Position: refs/heads/master@{#474122}

--- ui/gl/gl_surface_egl.h
+++ ui/gl/gl_surface_egl.h
@@ -73,6 +73,7 @@ class GL_EXPORT GLSurfaceEGL : public GLSurface {
   static bool IsCreateContextBindGeneratesResourceSupported();
   static bool IsCreateContextWebGLCompatabilitySupported();
   static bool IsEGLSurfacelessContextSupported();
+  static bool IsEGLContextPrioritySupported();
   static bool IsDirectCompositionSupported();
 
  protected:
