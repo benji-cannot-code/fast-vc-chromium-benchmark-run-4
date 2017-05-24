e6b7f74527e37d2ffb551afef5982f7fb9b9c93b
ehmaldonado <ehmaldonado@chromium.org>
2017-05-24 00:39:44 -0700

    [Telemetry] Disable webrtc.multiple_peerconnections story.
    It is failing on all platforms.
    The previous CL (https://codereview.chromium.org/2899123002) caused builders to break.
    
    BUG=725502,725796
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    TBR=rnephew@chromium.org, nednguyen@google.com, vitaliii@chromium.org
    
    Review-Url: https://codereview.chromium.org/2896253004
    Cr-Commit-Position: refs/heads/master@{#474198}

--- tools/perf/benchmarks/webrtc.py
+++ tools/perf/benchmarks/webrtc.py
@@ -22,9 +22,6 @@ class WebrtcPerfBenchmark(perf_benchmark.PerfBenchmark):
   def Name(cls):
     return 'webrtc'
 
-  def GetExpectations(self):
-    return page_sets.WebrtcExpectations()
-
   def CreateTimelineBasedMeasurementOptions(self):
     categories = [
         # Disable all categories by default.
