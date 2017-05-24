bbf95b4fd865eb210bc9767f430df574f927c1c1
Chong Jiang <chongjiang@chromium.org>
2017-05-24 15:39:25 -0700

    PerfProvider: Collect cache-misses on x86-64
    BUG=726108
    
    Change-Id: I9eab31007e6a00fd8c960481bac1e829556d4e90
    Reviewed-on: https://chromium-review.googlesource.com/514328
    Reviewed-by: Simon Que <sque@chromium.org>
    Commit-Queue: Chong Jiang <chongjiang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#474491}

--- chrome/browser/metrics/perf/perf_provider_chromeos_unittest.cc
+++ chrome/browser/metrics/perf/perf_provider_chromeos_unittest.cc
@@ -36,6 +36,8 @@ const char kPerfRecordCallgraphCmd[] =
   "perf record -a -e cycles -g -c 4000037";
 const char kPerfRecordLBRCmd[] =
   "perf record -a -e r20c4 -b -c 200011";
+const char kPerfRecordCacheMissesCmd[] =
+  "perf record -a -e cache-misses -c 4001";
 const char kPerfStatMemoryBandwidthCmd[] =
   "perf stat -a -e cycles -e instructions "
   "-e uncore_imc/data_reads/ -e uncore_imc/data_writes/ "
@@ -583,6 +585,12 @@ TEST_F(PerfProviderTest, DefaultCommandsBasedOnUarch_IvyBridge) {
         return cmd.value == kPerfRecordLBRCmd;
       });
   EXPECT_NE(cmds.end(), found);
+  found = std::find_if(
+      cmds.begin(), cmds.end(),
+      [](const RandomSelector::WeightAndValue& cmd) -> bool {
+        return cmd.value == kPerfRecordCacheMissesCmd;
+      });
+  EXPECT_NE(cmds.end(), found);
 }
 
 TEST_F(PerfProviderTest, DefaultCommandsBasedOnUarch_SandyBridge) {
@@ -609,6 +617,12 @@ TEST_F(PerfProviderTest, DefaultCommandsBasedOnUarch_SandyBridge) {
         return cmd.value == kPerfRecordLBRCmd;
       });
   EXPECT_NE(cmds.end(), found);
+  found = std::find_if(
+      cmds.begin(), cmds.end(),
+      [](const RandomSelector::WeightAndValue& cmd) -> bool {
+        return cmd.value == kPerfRecordCacheMissesCmd;
+      });
+  EXPECT_NE(cmds.end(), found);
 }
 
 TEST_F(PerfProviderTest, DefaultCommandsBasedOnArch_Arm) {
@@ -623,10 +637,16 @@ TEST_F(PerfProviderTest, DefaultCommandsBasedOnArch_Arm) {
   ASSERT_GE(cmds.size(), 2UL);
   EXPECT_EQ(cmds[0].value, kPerfRecordCyclesCmd);
   EXPECT_EQ(cmds[1].value, kPerfRecordCallgraphCmd);
-  const auto found = std::find_if(
+  auto found = std::find_if(
       cmds.begin(), cmds.end(),
       [](const RandomSelector::WeightAndValue& cmd) -> bool {
         return cmd.value == kPerfRecordLBRCmd;
+      });
+  EXPECT_EQ(cmds.end(), found) << "ARM does not support this command";
+  found = std::find_if(
+      cmds.begin(), cmds.end(),
+      [](const RandomSelector::WeightAndValue& cmd) -> bool {
+        return cmd.value == kPerfRecordCacheMissesCmd;
       });
   EXPECT_EQ(cmds.end(), found) << "ARM does not support this command";
 }
@@ -653,6 +673,12 @@ TEST_F(PerfProviderTest, DefaultCommandsBasedOnArch_x86_32) {
       cmds.begin(), cmds.end(),
       [](const RandomSelector::WeightAndValue& cmd) -> bool {
         return cmd.value == kPerfRecordLBRCmd;
+      });
+  EXPECT_EQ(cmds.end(), found) << "x86_32 does not support this command";
+  found = std::find_if(
+      cmds.begin(), cmds.end(),
+      [](const RandomSelector::WeightAndValue& cmd) -> bool {
+        return cmd.value == kPerfRecordCacheMissesCmd;
       });
   EXPECT_EQ(cmds.end(), found) << "x86_32 does not support this command";
 }
