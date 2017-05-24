79d2a27f81f86ffd1e5ddd9e6a60a7f7bdb62ef2
tedchoc <tedchoc@chromium.org>
2017-05-23 17:18:19 -0700

    Disable learning text suggestions if in incognito (via Omnibox).
    BUG=725543
    
    Review-Url: https://codereview.chromium.org/2901023002
    Cr-Commit-Position: refs/heads/master@{#474118}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
@@ -2250,13 +2250,16 @@ public class LocationBarLayout extends FrameLayout
         updateSecurityIcon(getSecurityLevel());
     }
 
-    /**
-     * @return The Tab currently showing.
-     */
     @Override
     public Tab getCurrentTab() {
         if (mToolbarDataProvider == null) return null;
         return mToolbarDataProvider.getTab();
+    }
+
+    @Override
+    public boolean allowKeyboardLearning() {
+        if (mToolbarDataProvider == null) return false;
+        return !mToolbarDataProvider.isIncognito();
     }
 
     private void initOmniboxResultsContainer() {
