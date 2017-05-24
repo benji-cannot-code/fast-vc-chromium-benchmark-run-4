e014b4d4ebfbbedeb3d0ed83ffc7c83f42147f8e
sebsg <sebsg@chromium.org>
2017-05-24 12:27:20 -0700

    [Payments] Fix metric log.
    BUG=725960
    
    Review-Url: https://codereview.chromium.org/2899083005
    Cr-Commit-Position: refs/heads/master@{#474387}

--- chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestCanMakePaymentMetricsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestCanMakePaymentMetricsTest.java
@@ -61,7 +61,7 @@ public class PaymentRequestCanMakePaymentMetricsTest implements MainActivityStar
     @Test
     @MediumTest
     @Feature({"Payments"})
-    public void testCannotMakePayment_Abort()
+    public void testCannotMakePayment_UserAbort()
             throws InterruptedException, ExecutionException, TimeoutException {
         // Initiate a payment request.
         mPaymentRequestTestRule.triggerUIAndWait(
@@ -152,13 +152,13 @@ public class PaymentRequestCanMakePaymentMetricsTest implements MainActivityStar
 
     /**
      * Tests that the CanMakePayment metrics are correctly logged for the case of a merchant
-     * calling it, receiving yeas as a response, showing the Payment Request and the user aborts the
-     * flow.
+     * calling it, receiving yes as a response, showing the Payment Request and the merchant aborts
+     * the flow.
      */
     @Test
     @MediumTest
     @Feature({"Payments"})
-    public void testCanMakePayment_Abort()
+    public void testCanMakePayment_MerchantAbort()
             throws InterruptedException, ExecutionException, TimeoutException {
         // Install the app so CanMakePayment returns true.
         mPaymentRequestTestRule.installPaymentApp(HAVE_INSTRUMENTS, IMMEDIATE_RESPONSE);
@@ -167,16 +167,9 @@ public class PaymentRequestCanMakePaymentMetricsTest implements MainActivityStar
         mPaymentRequestTestRule.triggerUIAndWait(
                 "queryShow", mPaymentRequestTestRule.getReadyForInput());
 
-        // Press the back button.
-        int callCount = mPaymentRequestTestRule.getDismissed().getCallCount();
-        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
-            @Override
-            public void run() {
-                mPaymentRequestTestRule.getPaymentRequestUI().getDialogForTest().onBackPressed();
-            }
-        });
-        mPaymentRequestTestRule.getDismissed().waitForCallback(callCount);
-        mPaymentRequestTestRule.expectResultContains(new String[] {"Request cancelled"});
+        // Simulate an abort by the merchant.
+        mPaymentRequestTestRule.clickNodeAndWait("abort", mPaymentRequestTestRule.getDismissed());
+        mPaymentRequestTestRule.expectResultContains(new String[] {"Abort"});
 
         // CanMakePayment was queried.
         Assert.assertEquals(1,
@@ -196,12 +189,12 @@ public class PaymentRequestCanMakePaymentMetricsTest implements MainActivityStar
         Assert.assertEquals(1,
                 RecordHistogram.getHistogramValueCountForTesting(
                         "PaymentRequest.CanMakePayment.Used.TrueWithShowEffectOnCompletion",
-                        JourneyLogger.COMPLETION_STATUS_USER_ABORTED));
+                        JourneyLogger.COMPLETION_STATUS_OTHER_ABORTED));
     }
 
     /**
      * Tests that the CanMakePayment metrics are correctly logged for the case of a merchant
-     * calling it, receiving yeas as a response, showing the Payment Request and the user completes
+     * calling it, receiving yes as a response, showing the Payment Request and the user completes
      * the flow.
      */
     @Test
@@ -246,7 +239,7 @@ public class PaymentRequestCanMakePaymentMetricsTest implements MainActivityStar
     @Test
     @MediumTest
     @Feature({"Payments"})
-    public void testNoQuery_Abort()
+    public void testNoQuery_UserAbort()
             throws InterruptedException, ExecutionException, TimeoutException {
         // Initiate a payment request.
         mPaymentRequestTestRule.triggerUIAndWait(
@@ -284,7 +277,7 @@ public class PaymentRequestCanMakePaymentMetricsTest implements MainActivityStar
     @Test
     @MediumTest
     @Feature({"Payments"})
-    public void testNoQuery_Completes()
+    public void testNoQuery_Complete()
             throws InterruptedException, ExecutionException, TimeoutException {
         // Install the app so the user can complete the Payment Request.
         mPaymentRequestTestRule.installPaymentApp(HAVE_INSTRUMENTS, IMMEDIATE_RESPONSE);
