df40ac2546ec7aaf7cf877597eaa9139bc499033
eero.hakkinen <eero.hakkinen@intel.com>
2017-05-23 17:11:39 -0700

    Implement a parser for Content-Disposition header fields
    This allows disposition types and parameters to be extracted from
    Content-Disposition header fields.
    
    BUG=455103
    
    Review-Url: https://codereview.chromium.org/2880543003
    Cr-Commit-Position: refs/heads/master@{#474116}

---
+++ third_party/WebKit/Source/platform/network/ParsedContentDisposition.cpp
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/network/ParsedContentDisposition.h"
+
+#include "platform/network/HeaderFieldTokenizer.h"
+
+namespace blink {
+
+ParsedContentDisposition::ParsedContentDisposition(
+    const String& content_disposition,
+    Mode mode) {
+  HeaderFieldTokenizer tokenizer(content_disposition);
+
+  StringView type;
+  if (!tokenizer.ConsumeToken(Mode::kNormal, type)) {
+    DVLOG(1) << "Failed to find `type' in '" << content_disposition << "'";
+    return;
+  }
+  type_ = type.ToString();
+
+  parameters_.ParseParameters(std::move(tokenizer), mode);
+}
+
+String ParsedContentDisposition::Filename() const {
+  return ParameterValueForName("filename");
+}
+
+}  // namespace blink
