bba81c1710bd53a4cae952aea2dd00a49caba6a3
Dominic Cooney <dominicc@chromium.org>
2017-05-25 21:09:41 +0900

    Remove custom element definition recursion check.
    This check used to be important but now the spec disallows any
    recursive registration so we do not need this constructor-specific
    check any more.
    
    Bug: 710184
    Change-Id: I056997e7608e3327f9eb01d4e548eade062991ac
    Reviewed-on: https://chromium-review.googlesource.com/515244
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Dominic Cooney <dominicc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#475415}

--- third_party/WebKit/Source/bindings/core/v8/ScriptCustomElementDefinitionBuilder.cpp
+++ third_party/WebKit/Source/bindings/core/v8/ScriptCustomElementDefinitionBuilder.cpp
@@ -18,25 +18,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-ScriptCustomElementDefinitionBuilder*
-    ScriptCustomElementDefinitionBuilder::stack_ = nullptr;
-
 ScriptCustomElementDefinitionBuilder::ScriptCustomElementDefinitionBuilder(
     ScriptState* script_state,
     CustomElementRegistry* registry,
     const ScriptValue& constructor,
     ExceptionState& exception_state)
-    : prev_(stack_),
-      script_state_(script_state),
+    : script_state_(script_state),
       registry_(registry),
       constructor_value_(constructor.V8Value()),
-      exception_state_(exception_state) {
-  stack_ = this;
-}
-
-ScriptCustomElementDefinitionBuilder::~ScriptCustomElementDefinitionBuilder() {
-  stack_ = prev_;
-}
+      exception_state_(exception_state) {}
 
 bool ScriptCustomElementDefinitionBuilder::CheckConstructorIntrinsics() {
   DCHECK(script_state_->World().IsMainWorld());
@@ -55,26 +45,15 @@ bool ScriptCustomElementDefinitionBuilder::CheckConstructorIntrinsics() {
 }
 
 bool ScriptCustomElementDefinitionBuilder::CheckConstructorNotRegistered() {
-  if (ScriptCustomElementDefinition::ForConstructor(script_state_.Get(),
-                                                    registry_, constructor_)) {
-    // Constructor is already registered.
-    exception_state_.ThrowDOMException(
-        kNotSupportedError,
-        "this constructor has already been used with this registry");
-    return false;
-  }
-  for (auto builder = prev_; builder; builder = builder->prev_) {
-    CHECK(!builder->constructor_.IsEmpty());
-    if (registry_ != builder->registry_ ||
-        constructor_ != builder->constructor_) {
-      continue;
-    }
-    exception_state_.ThrowDOMException(
-        kNotSupportedError,
-        "this constructor is already being defined in this registry");
-    return false;
-  }
-  return true;
+  if (!ScriptCustomElementDefinition::ForConstructor(script_state_.Get(),
+                                                     registry_, constructor_))
+    return true;
+
+  // Constructor is already registered.
+  exception_state_.ThrowDOMException(
+      kNotSupportedError,
+      "this constructor has already been used with this registry");
+  return false;
 }
 
 bool ScriptCustomElementDefinitionBuilder::ValueForName(
--- third_party/WebKit/Source/bindings/core/v8/ScriptCustomElementDefinitionBuilder.h
+++ third_party/WebKit/Source/bindings/core/v8/ScriptCustomElementDefinitionBuilder.h
@@ -36,7 +36,7 @@ class CORE_EXPORT ScriptCustomElementDefinitionBuilder
       CustomElementRegistry*,
       const ScriptValue& constructor_script_value,
       ExceptionState&);
-  ~ScriptCustomElementDefinitionBuilder();
+  ~ScriptCustomElementDefinitionBuilder() = default;
 
   bool CheckConstructorIntrinsics() override;
   bool CheckConstructorNotRegistered() override;
@@ -47,7 +47,6 @@ class CORE_EXPORT ScriptCustomElementDefinitionBuilder
  private:
   static ScriptCustomElementDefinitionBuilder* stack_;
 
-  ScriptCustomElementDefinitionBuilder* prev_;
   RefPtr<ScriptState> script_state_;
   Member<CustomElementRegistry> registry_;
   v8::Local<v8::Value> constructor_value_;
