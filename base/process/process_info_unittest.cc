05fb6f5f1d5e971507736723b22438afbb118008
scottmg <scottmg@chromium.org>
2017-05-25 16:55:06 -0700

    fuchsia: Don't reference CurrentProcessInfo::CreationTime()
    (I thought maybe I could thread time and subtract it from current time,
    but thread time is only how long it's been scheduled for, not total wall
    time, so it's not really that close:
    https://codereview.chromium.org/2909513003/ ).
    
    BUG=706592,726484
    
    Review-Url: https://codereview.chromium.org/2903323002
    Cr-Commit-Position: refs/heads/master@{#474846}

--- base/process/process_info_unittest.cc
+++ base/process/process_info_unittest.cc
@@ -11,11 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-#if !defined(OS_IOS)
+// See https://crbug.com/726484 for Fuchsia.
+#if !defined(OS_IOS) && !defined(OS_FUCHSIA)
 TEST(ProcessInfoTest, CreationTime) {
   Time creation_time = CurrentProcessInfo::CreationTime();
   ASSERT_FALSE(creation_time.is_null());
 }
-#endif  // !defined(OS_IOS)
+#endif  // !defined(OS_IOS) && !defined(OS_FUCHSIA)
 
 }  // namespace base
