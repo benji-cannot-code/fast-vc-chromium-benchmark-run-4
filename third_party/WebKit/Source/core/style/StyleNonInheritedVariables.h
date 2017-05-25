1cce0197c4b6b446fa6faef95bbd682b5e79c651
shend <shend@chromium.org>
2017-05-25 01:53:22 -0700

    Rename StyleNonInheritedVariables::Copy to Clone.
    This patch renames StyleNonInheritedVariables::Copy to Clone to match
    convention.
    
    BUG=628043
    
    Review-Url: https://codereview.chromium.org/2905493003
    Cr-Commit-Position: refs/heads/master@{#474610}

--- third_party/WebKit/Source/core/style/StyleNonInheritedVariables.h
+++ third_party/WebKit/Source/core/style/StyleNonInheritedVariables.h
@@ -20,7 +20,7 @@ class StyleNonInheritedVariables {
     return WTF::WrapUnique(new StyleNonInheritedVariables);
   }
 
-  std::unique_ptr<StyleNonInheritedVariables> Copy() {
+  std::unique_ptr<StyleNonInheritedVariables> Clone() {
     return WTF::WrapUnique(new StyleNonInheritedVariables(*this));
   }
 
