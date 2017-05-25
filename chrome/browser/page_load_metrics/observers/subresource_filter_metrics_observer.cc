af4479f69dba86df478add7c752ce614dc124af5
csharrison <csharrison@chromium.org>
2017-05-25 07:25:11 -0700

    [subresource_filter] Move activation computation to the SB throttle
    This CL moves computation of the ActivationDecision / ActivationOptions
    from the driver factory to the SafeBrowsing throttle. Page activation is
    then forwarded to the driver factory, which forwards it to the observer
    manager.
    
    This is in preparation to remove the driver factory entirely.
    
    BUG=708181
    
    Review-Url: https://codereview.chromium.org/2894023002
    Cr-Commit-Position: refs/heads/master@{#474644}

--- chrome/browser/page_load_metrics/observers/subresource_filter_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/subresource_filter_metrics_observer.cc
@@ -7,11 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/page_load_metrics/page_load_metrics_util.h"
 #include "components/subresource_filter/content/browser/content_subresource_filter_driver_factory.h"
+#include "components/subresource_filter/content/browser/subresource_filter_safe_browsing_activation_throttle.h"
 #include "components/subresource_filter/core/common/activation_decision.h"
 #include "third_party/WebKit/public/platform/WebLoadingBehaviorFlag.h"
 
 using subresource_filter::ContentSubresourceFilterDriverFactory;
-
+using subresource_filter::SubresourceFilterSafeBrowsingActivationThrottle;
 using ActivationDecision = subresource_filter::ActivationDecision;
 
 namespace internal {
@@ -124,9 +125,8 @@ void LogActivationDecisionMetrics(content::NavigationHandle* navigation_handle,
       static_cast<int>(decision),
       static_cast<int>(ActivationDecision::ACTIVATION_DECISION_MAX));
 
-  if (ContentSubresourceFilterDriverFactory::NavigationIsPageReload(
-          navigation_handle->GetURL(), navigation_handle->GetReferrer(),
-          navigation_handle->GetPageTransition())) {
+  if (SubresourceFilterSafeBrowsingActivationThrottle::NavigationIsPageReload(
+          navigation_handle)) {
     UMA_HISTOGRAM_ENUMERATION(
         internal::kHistogramSubresourceFilterActivationDecisionReload,
         static_cast<int>(decision),
