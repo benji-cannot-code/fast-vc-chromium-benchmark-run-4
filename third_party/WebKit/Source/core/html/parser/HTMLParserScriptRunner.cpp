cdefe10952b88423d271165eaaafcf1acee3f09f
mrunal.kapade <mrunal.kapade@intel.com>
2017-05-25 02:23:39 -0700

    Remove WTFLogAlways() usages from HTMLParserScriptRunner.cpp
    BUG=638849
    
    Review-Url: https://codereview.chromium.org/2906603002
    Cr-Commit-Position: refs/heads/master@{#474615}

--- third_party/WebKit/Source/core/html/parser/HTMLParserScriptRunner.cpp
+++ third_party/WebKit/Source/core/html/parser/HTMLParserScriptRunner.cpp
@@ -302,7 +302,6 @@ void EmitWarningForDocWriteScripts(const String& url, Document& document) {
       "connectivity.";
   document.AddConsoleMessage(
       ConsoleMessage::Create(kJSMessageSource, kWarningMessageLevel, message));
-  WTFLogAlways("%s", message.Utf8().data());
 }
 
 void EmitErrorForDocWriteScripts(const String& url, Document& document) {
@@ -314,7 +313,6 @@ void EmitErrorForDocWriteScripts(const String& url, Document& document) {
       "network connectivity. ";
   document.AddConsoleMessage(
       ConsoleMessage::Create(kJSMessageSource, kErrorMessageLevel, message));
-  WTFLogAlways("%s", message.Utf8().data());
 }
 
 void HTMLParserScriptRunner::PossiblyFetchBlockedDocWriteScript(
