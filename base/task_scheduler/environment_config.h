9ee2987f9d4b32a4e34fb1bfe34c24d5471f4ad1
robliao <robliao@chromium.org>
2017-05-25 14:33:43 -0700

    Implement Shared SingleThreadTaskRunners in the Task Scheduler
    This change provides one shared SingleThreadTaskRunner per Trait+COM
    combination by reusing dedicated SingleThreadTaskRunners.
    
    In production, these SingleThreadTaskRunners will never be reclaimed.
    In tests, JoinForTesting() cleans these up.
    
    BUG=720058
    
    Review-Url: https://codereview.chromium.org/2902753003
    Cr-Commit-Position: refs/heads/master@{#474813}

--- base/task_scheduler/environment_config.h
+++ base/task_scheduler/environment_config.h
@@ -38,7 +38,7 @@ constexpr struct {
     {"ForegroundBlocking", base::ThreadPriority::NORMAL},
 };
 
-size_t GetEnvironmentIndexForTraits(const TaskTraits& traits);
+size_t BASE_EXPORT GetEnvironmentIndexForTraits(const TaskTraits& traits);
 
 }  // namespace internal
 }  // namespace base
