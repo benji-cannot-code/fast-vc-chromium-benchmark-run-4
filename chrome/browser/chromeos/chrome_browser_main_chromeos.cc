775580d59e4a7771e1f022873a79e774a5475a7a
sky <sky@chromium.org>
2017-05-25 13:04:21 -0700

    Revert of Move RedirectChromeLogging to src/chromeos. (patchset #2 id:20001 of https://codereview.chromium.org/2901173002/ )
    Reason for revert:
    This is causing IO failures. Specifically the SetUpSymlinkIfNeeded causes an IO assertion to hit and login to crash.
    
    Original issue's description:
    > Move RedirectChromeLogging to src/chromeos.
    >
    > BUG=chromium:724273
    >
    > Review-Url: https://codereview.chromium.org/2901173002
    > Cr-Commit-Position: refs/heads/master@{#474725}
    > Committed: https://chromium.googlesource.com/chromium/src/+/cb9bde9860d070b022407e3d988ba3239e00f35a
    
    TBR=xiyuan@chromium.org,achuith@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=chromium:724273
    
    Review-Url: https://codereview.chromium.org/2909483003
    Cr-Commit-Position: refs/heads/master@{#474770}

--- chrome/browser/chromeos/chrome_browser_main_chromeos.cc
+++ chrome/browser/chromeos/chrome_browser_main_chromeos.cc
@@ -58,7 +58,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/input_method/input_method_util.h"
 #include "chrome/browser/chromeos/language_preferences.h"
 #include "chrome/browser/chromeos/lock_screen_apps/state_controller.h"
-#include "chrome/browser/chromeos/logging.h"
 #include "chrome/browser/chromeos/login/helper.h"
 #include "chrome/browser/chromeos/login/lock/screen_locker.h"
 #include "chrome/browser/chromeos/login/login_wizard.h"
