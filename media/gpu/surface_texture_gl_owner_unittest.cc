88d3acde25dc9812e43e1b70a0e6f8ce2d4b3e6b
Chris Watkins <watk@chromium.org>
2017-05-25 10:18:18 -0700

    media: Move WaitForFrameAvailable logic from AVDASharedState to SurfaceTextureGLOwner
    Conceptually it makes sense for SurfaceTexture to keep track of incoming
    buffers and expose a way to wait for them to be available. This will
    also let us share the logic between AVDA and MCVD.
    
    Bug: 660942
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: If08e0236a308c465a52557b69bd31e421405dbd6
    Reviewed-on: https://chromium-review.googlesource.com/514210
    Commit-Queue: Chris Watkins <watk@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#474721}

--- media/gpu/surface_texture_gl_owner_unittest.cc
+++ media/gpu/surface_texture_gl_owner_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/logging.h"
+#include "base/message_loop/message_loop.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gl/gl_bindings.h"
@@ -66,6 +67,7 @@ class SurfaceTextureGLOwnerTest : public testing::Test {
   scoped_refptr<gl::GLContext> context_;
   scoped_refptr<gl::GLShareGroup> share_group_;
   scoped_refptr<gl::GLSurface> surface_;
+  base::MessageLoop message_loop_;
 };
 
 // Verify that SurfaceTextureGLOwner creates a bindable GL texture, and deletes
