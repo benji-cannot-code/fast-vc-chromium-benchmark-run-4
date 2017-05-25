c5719a673e1fd3824ecdc2583475ad8d7efe6885
shivanisha <shivanisha@chromium.org>
2017-05-25 09:12:55 -0700

    [subresource_filter] Display dev tools console message for subresource blocking
    This CL displays a dev tools message when a sub resource is blocked, subsequent CLs
    will include messages for subframes and main frame navigations.
    Note that the string being displayed in this CL is not the final string. This CL is
    to ensure that a place holder and tests are in place for the final string.
    
    BUG=724549
    
    Review-Url: https://codereview.chromium.org/2895643002
    Cr-Commit-Position: refs/heads/master@{#474670}

--- content/shell/test_runner/mock_web_document_subresource_filter.cc
+++ content/shell/test_runner/mock_web_document_subresource_filter.cc
@@ -48,7 +48,7 @@ MockWebDocumentSubresourceFilter::getLoadPolicyImpl(const blink::WebURL& url) {
 void MockWebDocumentSubresourceFilter::ReportDisallowedLoad() {}
 
 bool MockWebDocumentSubresourceFilter::ShouldLogToConsole() {
-  return false;
+  return true;
 }
 
 }  // namespace test_runner
