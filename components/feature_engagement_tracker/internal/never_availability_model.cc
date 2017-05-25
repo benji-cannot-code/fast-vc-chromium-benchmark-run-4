7172d7544ad634247087cf4dd28af2736cdb8849
Tommy Nyquist <nyquist@chromium.org>
2017-05-25 10:53:54 -0700

    Add AvailabilityModel for feature engagement tracker.
    The FeatureEngagementTracker is missing functionality to track when a
    particular feature has been made available to the end user.
    
    This CL adds a model to track that, which uses the underlying
    AvailabilityStore for a one-time load during initialization.
    
    To help with testing and demo mode, this CL also adds a
    NeverAvailabililtyModel, which can be used whenever a real model is
    unnecessary.
    
    BUG=723007, 706309
    
    Change-Id: I0f39acf1979c88532e0321a252d1567f1cb77cc0
    Reviewed-on: https://chromium-review.googlesource.com/515142
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#474754}

---
+++ components/feature_engagement_tracker/internal/never_availability_model.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/feature_engagement_tracker/internal/never_availability_model.h"
+
+#include "base/callback.h"
+#include "base/optional.h"
+
+namespace feature_engagement_tracker {
+
+NeverAvailabilityModel::NeverAvailabilityModel() = default;
+
+NeverAvailabilityModel::~NeverAvailabilityModel() = default;
+
+void NeverAvailabilityModel::Initialize(OnInitializedCallback callback,
+                                        uint32_t current_day) {}
+
+bool NeverAvailabilityModel::IsReady() const {
+  return false;
+}
+
+base::Optional<uint32_t> NeverAvailabilityModel::GetAvailability(
+    const base::Feature& feature) const {
+  return base::nullopt;
+}
+
+}  // namespace feature_engagement_tracker
