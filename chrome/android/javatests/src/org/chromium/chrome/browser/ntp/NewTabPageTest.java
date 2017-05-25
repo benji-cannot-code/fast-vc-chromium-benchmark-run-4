49380d13e75a9607d991011baf9dcd4b8bd9d0c4
cblume <cblume@chromium.org>
2017-05-25 16:06:22 -0700

    Revert of Add NTPCondensedLayout to test config. (patchset #1 id:1 of https://codereview.chromium.org/2905873002/ )
    Reason for revert:
    Failing
    org.chromium.chrome.browser.ntp.NewTabPageTest#testRender
    on
    Lollipop Phone Tester
    https://uberchromegw.corp.google.com/i/chromium.android/builders/Lollipop%20Phone%20Tester
    
    Original issue's description:
    > Add NTPCondensedLayout to test config.
    >
    > BUG=688291
    >
    > Review-Url: https://codereview.chromium.org/2905873002
    > Cr-Commit-Position: refs/heads/master@{#474710}
    > Committed: https://chromium.googlesource.com/chromium/src/+/0dffaefa4b9bbf9d3ea7e97063d1936c07db3a06
    
    TBR=bauerb@chromium.org,asvitkine@chromium.org,mvanouwerkerk@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=726518
    
    Review-Url: https://codereview.chromium.org/2910563002
    Cr-Commit-Position: refs/heads/master@{#474838}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
@@ -391,7 +391,6 @@ public class NewTabPageTest {
     @Test
     @SmallTest
     @Feature({"NewTabPage"})
-    @CommandLineFlags.Add("disable-features=NTPCondensedLayout")
     public void testSetSearchProviderHasLogo() throws Throwable {
         mActivityTestRule.runOnUiThread(new Runnable() {
             @Override
@@ -436,7 +435,6 @@ public class NewTabPageTest {
     @Test
     @SmallTest
     @Feature({"NewTabPage"})
-    @CommandLineFlags.Add("disable-features=NTPCondensedLayout")
     public void testPlaceholder() {
         final NewTabPageView ntpView = mNtp.getNewTabPageView();
         final View logoView = ntpView.findViewById(R.id.search_provider_logo);
