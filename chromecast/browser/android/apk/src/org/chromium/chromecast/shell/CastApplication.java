6167f049befa3df59ab93e4277590677ad0a93a6
Bryan Henry <bryanhenry@google.com>
2017-05-25 14:15:21 -0700

    [Chromecast] Update CastApplication to use CommandLineInitUtil
    Enables developers to apply command-line flags on user builds of Android
    by setting the Debug App setting appropriately in Developer Options.
    Matches the usage in ChromeApplication.
    
    /data/local/tmp/castshell-command-line on a public, user build of
    Android (7.1.2 NHG67N on Pixel) iff ADB is enabled and DEBUG_APP is set
    to "com.google.android.apps.mediashell".
    
    Bug: internal b/62042278
    Test: Verified that command-line flags were properly loaded from
    Change-Id: Iabb6ed29bee8812bd8a9eb9a9815ac8f68192187
    Reviewed-on: https://chromium-review.googlesource.com/516243
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Le-Chun Wu <lcwu@chromium.org>
    Commit-Queue: Bryan Henry <bryanhenry@google.com>
    Cr-Commit-Position: refs/heads/master@{#474905}

--- chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastApplication.java
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastApplication.java
@@ -6,11 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chromecast.shell;
 
 import android.content.Context;
-import android.os.Build;
 
-import org.chromium.base.CommandLine;
+import org.chromium.base.CommandLineInitUtil;
 import org.chromium.base.ContextUtils;
-import org.chromium.base.Log;
 import org.chromium.base.PathUtils;
 import org.chromium.content.app.ContentApplication;
 
@@ -26,7 +24,7 @@ public class CastApplication extends ContentApplication {
     private static final String TAG = "CastApplication";
 
     private static final String PRIVATE_DATA_DIRECTORY_SUFFIX = "cast_shell";
-    private static final String COMMAND_LINE_FILE = "/data/local/tmp/castshell-command-line";
+    private static final String COMMAND_LINE_FILE = "castshell-command-line";
 
     @Override
     protected void attachBaseContext(Context base) {
@@ -42,15 +40,6 @@ public class CastApplication extends ContentApplication {
 
     @Override
     public void initCommandLine() {
-        if (allowCommandLineImport()) {
-            Log.d(TAG, "Initializing command line from " + COMMAND_LINE_FILE);
-            CommandLine.initFromFile(COMMAND_LINE_FILE);
-        } else {
-            CommandLine.init(null);
-        }
-    }
-
-    private static boolean allowCommandLineImport() {
-        return !Build.TYPE.equals("user");
+        CommandLineInitUtil.initCommandLine(this, COMMAND_LINE_FILE);
     }
 }
