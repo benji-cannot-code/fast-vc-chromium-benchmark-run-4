cdbbeab844680bbc54806f9ac2f3509644a8f6d8
klausw <klausw@chromium.org>
2017-05-24 21:33:01 -0700

    Add SlidingAverage to FPSMeter, use for WebVR prediction time
    Refactor FPSMeter to extract the "sliding window sum" code
    into a separate class, use it in FPSMeter, and separately
    use it to implement a SlidingAverage class.
    
    This revealed a pre-existing bug where current_index_ was
    not being initialized, leading to wrong FPS calculations.
    
    Use SlidingAverage to calculate WebVR Javascript and rendering
    times, and use those to calculate the prediction time offset
    for GVR poses instead of hardcoding 50ms for this.
    
    BUG=723962
    
    Review-Url: https://codereview.chromium.org/2901213002
    Cr-Commit-Position: refs/heads/master@{#474549}

--- chrome/browser/android/vr_shell/fps_meter.h
+++ chrome/browser/android/vr_shell/fps_meter.h
@@ -13,10 +13,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace vr_shell {
 
+class SampleQueue {
+ public:
+  explicit SampleQueue(size_t window_size);
+  ~SampleQueue();
+
+  int64_t GetSum() const { return sum_; }
+
+  void AddSample(int64_t value);
+
+  size_t GetCount() const { return samples_.size(); }
+
+  // Get sliding window size for tests.
+  size_t GetWindowSize() const { return window_size_; }
+
+ private:
+  int64_t sum_ = 0;
+  size_t current_index_ = 0;
+  size_t window_size_;
+  std::vector<int64_t> samples_;
+};
+
 // Computes fps based on submitted frame times.
 class FPSMeter {
  public:
   FPSMeter();
+  explicit FPSMeter(size_t window_size);
   ~FPSMeter();
 
   void AddFrame(const base::TimeTicks& time_stamp);
@@ -29,11 +51,22 @@ class FPSMeter {
   size_t GetNumFrameTimes();
 
  private:
-  size_t current_index_;
-  int64_t total_time_us_;
+  SampleQueue frame_times_;
   base::TimeTicks last_time_stamp_;
-  std::vector<base::TimeDelta> frame_times_;
   DISALLOW_COPY_AND_ASSIGN(FPSMeter);
+};
+
+class SlidingAverage {
+ public:
+  explicit SlidingAverage(size_t window_size);
+  ~SlidingAverage();
+
+  void AddSample(int64_t value);
+  int64_t GetAverageOrDefault(int64_t default_value) const;
+  int64_t GetAverage() const { return GetAverageOrDefault(0); }
+
+ private:
+  SampleQueue values_;
 };
 
 }  // namespace vr_shell
