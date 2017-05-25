d648591ea56792271e130afbef624dba652429de
rdevlin.cronin <rdevlin.cronin@chromium.org>
2017-05-25 07:58:19 -0700

    [Extensions] Remove extension_install_ui_util.*
    extension_install_ui_util.* seem to have been added for in
    crrev.com/453ac12c99c79c6490342cac6469f24bd2454591 for ephemeral apps,
    which no longer exist. Remove them.
    
    BUG=517735
    
    Review-Url: https://codereview.chromium.org/2897143004
    Cr-Commit-Position: refs/heads/master@{#474651}

--- chrome/browser/extensions/api/webstore_private/webstore_private_api.cc
+++ chrome/browser/extensions/api/webstore_private/webstore_private_api.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/version.h"
 #include "chrome/browser/bitmap_fetcher/bitmap_fetcher.h"
 #include "chrome/browser/extensions/crx_installer.h"
-#include "chrome/browser/extensions/extension_install_ui_util.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_util.h"
 #include "chrome/browser/extensions/install_tracker.h"
--- chrome/browser/extensions/webstore_standalone_installer.cc
+++ chrome/browser/extensions/webstore_standalone_installer.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/version.h"
 #include "chrome/browser/extensions/crx_installer.h"
 #include "chrome/browser/extensions/extension_install_prompt.h"
-#include "chrome/browser/extensions/extension_install_ui_util.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/install_tracker.h"
 #include "chrome/browser/extensions/webstore_data_fetcher.h"
