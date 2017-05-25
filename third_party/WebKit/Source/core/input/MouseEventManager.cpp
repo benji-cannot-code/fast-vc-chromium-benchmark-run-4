e6ff26129464272357a383464e001676add8893e
nzolghadr <nzolghadr@chromium.org>
2017-05-25 16:28:08 -0700

    Gather UMA for click retarget due to DOM changes
    If the mouse down element is removed from DOM
    and gets added back we do not send a click event.
    This change adds the UMA metric to see how often
    this happens. Also enables sending that click
    event behind flag to see the effect in the wild.
    
    BUG=716694
    
    Review-Url: https://codereview.chromium.org/2894963002
    Cr-Commit-Position: refs/heads/master@{#474841}

--- third_party/WebKit/Source/core/input/MouseEventManager.cpp
+++ third_party/WebKit/Source/core/input/MouseEventManager.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/page/FocusController.h"
 #include "core/paint/PaintLayer.h"
 #include "core/svg/SVGDocumentExtensions.h"
+#include "platform/Histogram.h"
 #include "platform/geometry/FloatQuad.h"
 
 namespace blink {
@@ -96,6 +97,7 @@ void MouseEventManager::Clear() {
   mouse_pressed_ = false;
   click_count_ = 0;
   click_element_ = nullptr;
+  mouse_down_element_ = nullptr;
   mouse_down_pos_ = IntPoint();
   mouse_down_timestamp_ = TimeTicks();
   mouse_down_ = WebMouseEvent();
@@ -113,6 +115,7 @@ DEFINE_TRACE(MouseEventManager) {
   visitor->Trace(node_under_mouse_);
   visitor->Trace(mouse_press_node_);
   visitor->Trace(click_element_);
+  visitor->Trace(mouse_down_element_);
   SynchronousMutationObserver::Trace(visitor);
 }
 
@@ -250,9 +253,10 @@ WebInputEventResult MouseEventManager::DispatchMouseClickIfNeeded(
 #endif
 
   const bool should_dispatch_click_event =
-      click_count_ > 0 && !context_menu_event && click_element_ &&
+      click_count_ > 0 && !context_menu_event && mouse_down_element_ &&
       mouse_release_target.CanParticipateInFlatTree() &&
-      click_element_->CanParticipateInFlatTree() &&
+      mouse_down_element_->CanParticipateInFlatTree() &&
+      mouse_down_element_->isConnected() &&
       !(frame_->GetEventHandler()
             .GetSelectionController()
             .HasExtendedSelection() &&
@@ -261,27 +265,45 @@ WebInputEventResult MouseEventManager::DispatchMouseClickIfNeeded(
     return WebInputEventResult::kNotHandled;
 
   Node* click_target_node = nullptr;
-  if (click_element_ == mouse_release_target) {
-    click_target_node = click_element_;
-  } else if (click_element_->GetDocument() ==
+  if (mouse_down_element_ == mouse_release_target) {
+    click_target_node = mouse_down_element_;
+  } else if (mouse_down_element_->GetDocument() ==
              mouse_release_target.GetDocument()) {
     // Updates distribution because a 'mouseup' event listener can make the
     // tree dirty at dispatchMouseEvent() invocation above.
     // Unless distribution is updated, commonAncestor would hit ASSERT.
-    click_element_->UpdateDistribution();
+    mouse_down_element_->UpdateDistribution();
     mouse_release_target.UpdateDistribution();
     click_target_node = mouse_release_target.CommonAncestor(
-        *click_element_, EventHandlingUtil::ParentForClickEvent);
+        *mouse_down_element_, EventHandlingUtil::ParentForClickEvent);
   }
   if (!click_target_node)
     return WebInputEventResult::kNotHandled;
-  return DispatchMouseEvent(
-      click_target_node,
-      !RuntimeEnabledFeatures::auxclickEnabled() ||
-              (mev.Event().button == WebPointerProperties::Button::kLeft)
-          ? EventTypeNames::click
-          : EventTypeNames::auxclick,
-      mev.Event(), mev.CanvasRegionId(), nullptr);
+
+  DEFINE_STATIC_LOCAL(BooleanHistogram, histogram,
+                      ("Event.ClickNotFiredDueToDomManipulation"));
+
+  if (click_element_ && click_element_->CanParticipateInFlatTree() &&
+      click_element_->isConnected()) {
+    DCHECK(click_element_ == mouse_down_element_);
+    histogram.Count(false);
+  } else {
+    histogram.Count(true);
+  }
+
+  if ((click_element_ && click_element_->CanParticipateInFlatTree() &&
+       click_element_->isConnected()) ||
+      RuntimeEnabledFeatures::clickRetargettingEnabled()) {
+    return DispatchMouseEvent(
+        click_target_node,
+        !RuntimeEnabledFeatures::auxclickEnabled() ||
+                (mev.Event().button == WebPointerProperties::Button::kLeft)
+            ? EventTypeNames::click
+            : EventTypeNames::auxclick,
+        mev.Event(), mev.CanvasRegionId(), nullptr);
+  }
+
+  return WebInputEventResult::kNotHandled;
 }
 
 void MouseEventManager::FakeMouseMoveEventTimerFired(TimerBase* timer) {
@@ -387,6 +409,9 @@ void MouseEventManager::NodeChildrenWillBeRemoved(ContainerNode& container) {
   if (!container.IsShadowIncludingInclusiveAncestorOf(click_element_.Get()))
     return;
   click_element_ = nullptr;
+
+  // TODO(crbug.com/716694): Do not reset mouse_down_element_ for the purpose of
+  // gathering data.
 }
 
 void MouseEventManager::NodeWillBeRemoved(Node& node_to_be_removed) {
@@ -395,6 +420,9 @@ void MouseEventManager::NodeWillBeRemoved(Node& node_to_be_removed) {
     // We don't dispatch click events if the mousedown node is removed
     // before a mouseup event. It is compatible with IE and Firefox.
     click_element_ = nullptr;
+
+    // TODO(crbug.com/716694): Do not reset mouse_down_element_ for the purpose
+    // of gathering data.
   }
 }
 
@@ -1021,6 +1049,7 @@ bool MouseEventManager::HandleSvgPanIfNeeded(bool is_release_event) {
 void MouseEventManager::InvalidateClick() {
   click_count_ = 0;
   click_element_ = nullptr;
+  mouse_down_element_ = nullptr;
 }
 
 bool MouseEventManager::MousePressed() {
@@ -1050,6 +1079,7 @@ void MouseEventManager::SetMousePressNode(Node* node) {
 void MouseEventManager::SetClickElement(Element* element) {
   SetContext(element ? element->ownerDocument() : nullptr);
   click_element_ = element;
+  mouse_down_element_ = element;
 }
 
 void MouseEventManager::SetClickCount(int click_count) {
