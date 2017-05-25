7a689485ef489271e22acfb3fd930dbffa9a73ad
leon.han <leon.han@intel.com>
2017-05-24 19:25:01 -0700

    [ServiceWorker] Fix ExtendableMessageEvent idl binding
    Spec:
    https://w3c.github.io/ServiceWorker/#extendablemessageevent-interface
    
    ExtendableMessageEventInit.ports should NOT allow null,
    ExtendableMessageEvent.ports should be empty array by default.
    
    BUG=702352, 702361
    TEST=blink_tests
    external/wpt/service-workers/service-worker/ServiceWorkerGlobalScope/extendable-message-event-constructor.https.html
    
    Review-Url: https://codereview.chromium.org/2904563004
    Cr-Commit-Position: refs/heads/master@{#474528}

--- third_party/WebKit/Source/modules/serviceworkers/ExtendableMessageEvent.h
+++ third_party/WebKit/Source/modules/serviceworkers/ExtendableMessageEvent.h
@@ -47,7 +47,6 @@ class MODULES_EXPORT ExtendableMessageEvent final : public ExtendableEvent {
   }
   const String& origin() const { return origin_; }
   const String& lastEventId() const { return last_event_id_; }
-  MessagePortArray ports(bool& is_null) const;
   MessagePortArray ports() const;
   void source(ClientOrServiceWorkerOrMessagePort& result) const;
 
