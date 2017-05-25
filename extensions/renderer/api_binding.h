b01c28aa588799c81293df4db87ecff3984fe479
rdevlin.cronin <rdevlin.cronin@chromium.org>
2017-05-24 19:06:03 -0700

    [Extensions Bindings] Handle updating context permissions
    Extensions can update permissions during a context's lifetime (by
    requesting or removing optional permissions). In this case, we need to
    update the bindings for that context. This includes two pieces:
    - Ensuring APIs from new permissions are present
    - Ensuring APIs that require removed permissions no longer work
    
    Note that we don't actually *remove* APIs that required old permissions;
    we just disable them. This is partially for simplicity and partially
    because removing the API from the chrome object wouldn't really matter,
    since scripts could cache the API object somewhere.
    
    BUG=653596
    
    Review-Url: https://codereview.chromium.org/2891123002
    Cr-Commit-Position: refs/heads/master@{#474523}

--- extensions/renderer/api_binding.h
+++ extensions/renderer/api_binding.h
@@ -48,9 +48,10 @@ class APIBinding {
       const std::string& property_name,
       const base::ListValue* property_values)>;
 
-  // The callback for determining if a given API method (specified by |name|)
-  // is available.
-  using AvailabilityCallback = base::Callback<bool(const std::string& name)>;
+  // The callback for determining if a given API feature (specified by |name|)
+  // is available in the given context.
+  using AvailabilityCallback =
+      base::Callback<bool(v8::Local<v8::Context>, const std::string& name)>;
 
   // The callback type for handling an API call.
   using HandlerCallback = base::Callback<void(gin::Arguments*)>;
@@ -64,6 +65,7 @@ class APIBinding {
              const base::ListValue* event_definitions,
              const base::DictionaryValue* property_definitions,
              const CreateCustomType& create_custom_type,
+             const AvailabilityCallback& is_available,
              std::unique_ptr<APIBindingHooks> binding_hooks,
              APITypeReferenceMap* type_refs,
              APIRequestHandler* request_handler,
@@ -71,9 +73,7 @@ class APIBinding {
   ~APIBinding();
 
   // Returns a new v8::Object for the API this APIBinding represents.
-  v8::Local<v8::Object> CreateInstance(
-      v8::Local<v8::Context> context,
-      const AvailabilityCallback& is_available);
+  v8::Local<v8::Object> CreateInstance(v8::Local<v8::Context> context);
 
   APIBindingHooks* hooks() { return binding_hooks_.get(); }
 
@@ -130,6 +130,9 @@ class APIBinding {
 
   // The callback for constructing a custom type.
   CreateCustomType create_custom_type_;
+
+  // The callback for checking availability of an API feature.
+  AvailabilityCallback is_available_;
 
   // The registered hooks for this API.
   std::unique_ptr<APIBindingHooks> binding_hooks_;
--- extensions/renderer/api_bindings_system.h
+++ extensions/renderer/api_bindings_system.h
@@ -45,6 +45,7 @@ class APIBindingsSystem {
   APIBindingsSystem(const binding::RunJSFunction& call_js,
                     const binding::RunJSFunctionSync& call_js_sync,
                     const GetAPISchemaMethod& get_api_schema,
+                    const APIBinding::AvailabilityCallback& is_available,
                     const APIRequestHandler::SendRequestMethod& send_request,
                     const APIEventHandler::EventListenersChangedMethod&
                         event_listeners_changed,
@@ -55,7 +56,6 @@ class APIBindingsSystem {
   v8::Local<v8::Object> CreateAPIInstance(
       const std::string& api_name,
       v8::Local<v8::Context> context,
-      const APIBinding::AvailabilityCallback& is_available,
       APIBindingHooks** hooks_out);
 
   // Responds to the request with the given |request_id|, calling the callback
@@ -135,6 +135,8 @@ class APIBindingsSystem {
   // The method to retrieve the DictionaryValue describing a given extension
   // API. Curried in for testing purposes so we can use fake APIs.
   GetAPISchemaMethod get_api_schema_;
+
+  APIBinding::AvailabilityCallback is_available_;
 
   DISALLOW_COPY_AND_ASSIGN(APIBindingsSystem);
 };
