f6ce3ccf8b7a3d48f927fc2b54e2bd00fbef9333
Kenichi Ishibashi <bashi@chromium.org>
2017-05-25 15:22:45 +0900

    bindings: Fix missing forward declarations in generated callback functions
    If a callback function has references to interfaces, generated header file of
    the callback function needs to have forward declarations for all
    interfaces. Before this CL, the code generator failed to add forward
    declarations when there are indirect references (e.g. sequence<T>).
    Find interface names with a recursive function to handle indirect
    references.
    
    BUG=569301
    
    Change-Id: I977eda19616b7d8103a739e61d697b4c1e6b6f92
    Reviewed-on: https://chromium-review.googlesource.com/514923
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#474612}

--- third_party/WebKit/Source/bindings/scripts/v8_callback_function.py
+++ third_party/WebKit/Source/bindings/scripts/v8_callback_function.py
@@ -33,11 +33,10 @@ def callback_function_context(callback_function):
     includes.update(CALLBACK_FUNCTION_CPP_INCLUDES)
     idl_type = callback_function.idl_type
     idl_type_str = str(idl_type)
-    forward_declarations = []
+
     for argument in callback_function.arguments:
-        if argument.idl_type.is_interface_type:
-            forward_declarations.append(argument.idl_type)
-        argument.idl_type.add_includes_for_type(callback_function.extended_attributes)
+        argument.idl_type.add_includes_for_type(
+            callback_function.extended_attributes)
 
     context = {
         # While both |callback_function_name| and |cpp_class| are identical at
@@ -47,7 +46,7 @@ def callback_function_context(callback_function):
         'callback_function_name': callback_function.name,
         'cpp_class': callback_function.name,
         'cpp_includes': sorted(includes),
-        'forward_declarations': sorted(forward_declarations),
+        'forward_declarations': sorted(forward_declarations(callback_function)),
         'header_includes': sorted(CALLBACK_FUNCTION_H_INCLUDES),
         'idl_type': idl_type_str,
     }
@@ -64,6 +63,22 @@ def callback_function_context(callback_function):
 
     context.update(arguments_context(callback_function.arguments, context.get('return_cpp_type')))
     return context
+
+
+def forward_declarations(callback_function):
+    def find_interface_name(idl_type):
+        if idl_type.is_interface_type:
+            return idl_type.implemented_as
+        elif idl_type.is_array_or_sequence_type:
+            return find_interface_name(idl_type.element_type)
+        return None
+
+    declarations = []
+    for argument in callback_function.arguments:
+        name = find_interface_name(argument.idl_type)
+        if name:
+            declarations.append(name)
+    return declarations
 
 
 def arguments_context(arguments, return_cpp_type):
