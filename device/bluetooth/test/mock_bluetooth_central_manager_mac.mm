4b9ae0cb736cac058117b3b74857127b0008f1d1
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2017-05-25 18:06:03 -0700

    bluetooth: Flush pending writes without response callbacks if we disconnected
    The problem:
    1. Device connects.
    2. Write to a characteristic without response. (Which ends up posting a task to run DidWriteValue)
    3. Immediately disconnect. (which causes macOS to mark the device as disconnected)
    4. Crash. (DidWriteValue is ran which CHECKs that the device is connected, but since it is it crashes).
    
    Flushes all pending writes when DidWriteValue is called and the device is disconnected.
    
    Bug: 724300
    Change-Id: I6cb0fd640e313733297ae093f45bdaa8dfe71a67
    Reviewed-on: https://chromium-review.googlesource.com/516487
    Reviewed-by: Vincent Scheib <scheib@chromium.org>
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#475008}

--- device/bluetooth/test/mock_bluetooth_central_manager_mac.mm
+++ device/bluetooth/test/mock_bluetooth_central_manager_mac.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "device/bluetooth/test/mock_bluetooth_central_manager_mac.h"
 
+#import "base/mac/foundation_util.h"
 #import "base/mac/scoped_nsobject.h"
 #import "device/bluetooth/test/bluetooth_test_mac.h"
 #import "device/bluetooth/test/mock_bluetooth_cbperipheral_mac.h"
@@ -69,6 +70,12 @@ using base::scoped_nsobject;
   if (_bluetoothTestMac) {
     _bluetoothTestMac->OnFakeBluetoothGattDisconnect();
   }
+
+  // When cancelPeripheralConnection is called macOS marks the device as
+  // disconnected.
+  MockCBPeripheral* mock_peripheral =
+      base::mac::ObjCCastStrict<MockCBPeripheral>(peripheral);
+  [mock_peripheral setState:CBPeripheralStateDisconnected];
 }
 
 - (NSArray*)retrieveConnectedPeripheralServiceUUIDs {
