e677ac3c504bcfcb826997cea2b4a01065341d2d
jiameng <jiameng@chromium.org>
2017-05-25 20:28:27 -0700

    Create a new local context class.
    CSSParserLocalContext represents local context for each property.
    Currently it only has one boolean field (use_alias_parsing), but it
    will be later extended to contain other info, such as whether the
    property is a longhand of a shorthand.
    
    BUG=668012
    
    Review-Url: https://codereview.chromium.org/2901393002
    Cr-Commit-Position: refs/heads/master@{#474897}

---
+++ third_party/WebKit/Source/core/css/parser/CSSParserLocalContext.h
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CSSParserLocalContext_h
+#define CSSParserLocalContext_h
+
+#include "platform/wtf/Allocator.h"
+
+namespace blink {
+
+// A wrapper class containing all local context when parsing a property.
+// TODO(jiameng): add info for shorthand properties into this class.
+
+class CSSParserLocalContext {
+  STACK_ALLOCATED();
+
+ public:
+  CSSParserLocalContext();
+  explicit CSSParserLocalContext(bool use_alias_parsing);
+  bool GetUseAliasParsing();
+
+ private:
+  bool use_alias_parsing_;
+};
+
+}  // namespace blink
+
+#endif  // CSSParserLocalContext_h
