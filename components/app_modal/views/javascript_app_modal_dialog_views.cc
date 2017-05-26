898c8543015bf22903484e39b4809c28cecfdbc1
thestig <thestig@chromium.org>
2017-05-26 10:42:49 -0700

    components/app_modal does not depend on constrained_window.
    Review-Url: https://codereview.chromium.org/2907793002
    Cr-Commit-Position: refs/heads/master@{#475052}

--- components/app_modal/views/javascript_app_modal_dialog_views.cc
+++ components/app_modal/views/javascript_app_modal_dialog_views.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/utf_string_conversions.h"
 #include "components/app_modal/javascript_app_modal_dialog.h"
-#include "components/constrained_window/constrained_window_views.h"
 #include "components/strings/grit/components_strings.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/events/keycodes/keyboard_codes.h"
