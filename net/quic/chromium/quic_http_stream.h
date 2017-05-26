52831ffc5daaf87d2cdc8034c9aab4efc235d05c
rch <rch@chromium.org>
2017-05-26 15:54:54 -0700

    Add an async ReadBody method to QuicChromiumClientStream::Handle
    This does not change OnClose or OnError and the callback is not yet invoked
    on errors. That will happen after all the upcalls are replaced by async
    methods.
    
    BUG=716563
    
    Review-Url: https://codereview.chromium.org/2877063002
    Cr-Commit-Position: refs/heads/master@{#475172}

--- net/quic/chromium/quic_http_stream.h
+++ net/quic/chromium/quic_http_stream.h
@@ -70,7 +70,6 @@ class NET_EXPORT_PRIVATE QuicHttpStream
   void SetPriority(RequestPriority priority) override;
 
   // QuicChromiumClientStream::Delegate implementation
-  void OnDataAvailable() override;
   void OnTrailingHeadersAvailable(const SpdyHeaderBlock& headers,
                                   size_t frame_len) override;
   void OnClose() override;
@@ -126,7 +125,9 @@ class NET_EXPORT_PRIVATE QuicHttpStream
   void OnReadResponseHeadersComplete(int rv);
   int ProcessResponseHeaders(const SpdyHeaderBlock& headers);
 
-  int ReadAvailableData(IOBuffer* buf, int buf_len);
+  void OnReadBodyComplete(int rv);
+  int HandleReadComplete(int rv);
+
   void EnterStateSendHeaders();
 
   void ResetStream();
