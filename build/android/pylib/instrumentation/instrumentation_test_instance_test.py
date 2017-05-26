f3bfc065882e4e7f97bf0d2d513d0cf34c38f05c
peconn <peconn@chromium.org>
2017-05-26 03:25:44 -0700

    📷 Create RenderTestRule which fails after test method finishes.
    This CL:
    - Removes RenderUtils.
    - Creates RenderTestRule with most of the RenderUtils functionality.
    - Updates render tests to RenderTestRule.
    - Removes the 'regenerate-goldens' flag. This is not needed because the
      RenderTestRule will throw an Exception once the test method has
      finished, so all image renders and diffs will be saved anyway.
    - Simplifies the rendering logic.
    
    BUG=614305
    
    Review-Url: https://codereview.chromium.org/2901053003
    Cr-Commit-Position: refs/heads/master@{#474978}

--- build/android/pylib/instrumentation/instrumentation_test_instance_test.py
+++ build/android/pylib/instrumentation/instrumentation_test_instance_test.py
@@ -47,15 +47,13 @@ class InstrumentationTestInstanceTest(unittest.TestCase):
         'command_line_flags',
         'device_flags_file',
         'strict_mode',
-        'regenerate_goldens',
       ])
 
   def createFlagAttributesArgs(
       self, command_line_flags=None, device_flags_file=None,
-      strict_mode=None, regenerate_goldens=None):
+      strict_mode=None):
     return self._FlagAttributesArgs(
-        command_line_flags, device_flags_file, strict_mode,
-        regenerate_goldens)
+        command_line_flags, device_flags_file, strict_mode)
 
   def test_initializeFlagAttributes_commandLineFlags(self):
     o = self.createTestInstance()
@@ -84,13 +82,6 @@ class InstrumentationTestInstanceTest(unittest.TestCase):
     args = self.createFlagAttributesArgs(strict_mode='off')
     o._initializeFlagAttributes(args)
     self.assertEquals(o._flags, ['--enable-test-intents'])
-
-  def test_initializeFlagAttributes_regenerateGoldens(self):
-    o = self.createTestInstance()
-    args = self.createFlagAttributesArgs(regenerate_goldens=True)
-    o._initializeFlagAttributes(args)
-    self.assertEquals(
-        o._flags, ['--enable-test-intents', '--regenerate-goldens'])
 
   def testGetTests_noFilter(self):
     o = self.createTestInstance()
--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
@@ -45,7 +45,7 @@ import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.util.ChromeTabUtils;
 import org.chromium.chrome.test.util.NewTabPageTestUtils;
 import org.chromium.chrome.test.util.OmniboxTestUtils;
-import org.chromium.chrome.test.util.RenderUtils.ViewRenderer;
+import org.chromium.chrome.test.util.RenderTestRule;
 import org.chromium.content.browser.test.util.Criteria;
 import org.chromium.content.browser.test.util.CriteriaHelper;
 import org.chromium.content.browser.test.util.KeyUtils;
@@ -74,6 +74,9 @@ import java.util.concurrent.TimeUnit;
 public class NewTabPageTest {
     @Rule
     public ChromeTabbedActivityTestRule mActivityTestRule = new ChromeTabbedActivityTestRule();
+    @Rule
+    public RenderTestRule mRenderTestRule =
+            new RenderTestRule("chrome/test/data/android/render_tests");
 
     private static final String TEST_PAGE = "/chrome/test/data/android/navigate/simple.html";
 
@@ -120,11 +123,9 @@ public class NewTabPageTest {
     @MediumTest
     @Feature({"NewTabPage", "RenderTest"})
     public void testRender() throws IOException {
-        ViewRenderer viewRenderer = new ViewRenderer(mActivityTestRule.getActivity(),
-                "chrome/test/data/android/render_tests", "NewTabPageTest");
-        viewRenderer.renderAndCompare(mTileGridLayout, "most_visited");
-        viewRenderer.renderAndCompare(mFakebox, "fakebox");
-        viewRenderer.renderAndCompare(mNtp.getView().getRootView(), "new_tab_page");
+        mRenderTestRule.render(mTileGridLayout, "most_visited");
+        mRenderTestRule.render(mFakebox, "fakebox");
+        mRenderTestRule.render(mNtp.getView().getRootView(), "new_tab_page");
 
         // Scroll to search bar
         final NewTabPageRecyclerView recyclerView = mNtp.getNewTabPageView().getRecyclerView();
@@ -143,7 +144,7 @@ public class NewTabPageTest {
             }
         });
 
-        viewRenderer.renderAndCompare(mNtp.getView().getRootView(), "new_tab_page_scrolled");
+        mRenderTestRule.render(mNtp.getView().getRootView(), "new_tab_page_scrolled");
     }
 
     @Test
