ecc794bd2e0228055d9463204068fc18e87c701c
Ken Rockot <rockot@chromium.org>
2017-05-26 00:13:28 -0700

    Enable Blink native Mojo bindings in WebUI
    Adds the concept of ContextEnabled features as an extended attribute in
    Web IDL, exposing a way for embedders to turn specific API features on
    or off at context creation time.
    
    Affixes this extended attribute to Mojo, MojoHandle, and MojoWatcher
    IDLs to allow their exposure to be controlled per-context separately
    from the MojoJS RuntimeEnabled state.
    
    Finally, this also flips on the MojoJS ContextEnabled feature for all
    RenderFrame contexts in which WebUI bindings are enabled.
    
    BUG=699569,723923
    
    Change-Id: Ie0fd6383ae86c133685b3e7b31eeca769bf83e06
    Reviewed-on: https://chromium-review.googlesource.com/509390
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#474973}

--- content/test/data/web_ui_mojo_native.js
+++ content/test/data/web_ui_mojo_native.js
@@ -2,12 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+'use strict';
 
-[
-    ActiveScriptWrappable,
-    DependentLifetime,
-    ContextEnabled=MojoJS,
-    RuntimeEnabled=MojoJS,
-] interface MojoWatcher {
-    MojoResult cancel();
+window.isNativeMojoAvailable = () => {
+  return 'Mojo' in self && 'MojoHandle' in self && 'MojoWatcher' in self;
 };
