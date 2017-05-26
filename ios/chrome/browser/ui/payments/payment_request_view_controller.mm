f1625778348365660bc2b1a58a0ed9a9ed0edfb3
macourteau <macourteau@chromium.org>
2017-05-26 10:17:08 -0700

    Revert of Fixes formatting of the signed-in setting string on iOS. (patchset #4 id:100001 of https://codereview.chromium.org/2903273002/ )
    Reason for revert:
    Seems like this broke ios-simulator tests on the waterfall. Reverting and will re-land once fixed.
    
    Original issue's description:
    > Fixes formatting of the signed-in setting string on iOS.
    >
    > Also adds a test to ensure that that specific string gets formatted
    > properly.
    >
    > R=mahmadi@chromium.org, rouslan@chromium.org
    > BUG=602666
    >
    > Review-Url: https://codereview.chromium.org/2903273002 .
    > Cr-Commit-Position: refs/heads/master@{#474991}
    > Committed: https://chromium.googlesource.com/chromium/src/+/b2c5e5f71d6b747d62709c6f9c190c1380ce8976
    
    TBR=mahmadi@chromium.org,rouslan@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=602666
    
    Review-Url: https://codereview.chromium.org/2907883002
    Cr-Commit-Position: refs/heads/master@{#475039}

--- ios/chrome/browser/ui/payments/payment_request_view_controller.mm
+++ ios/chrome/browser/ui/payments/payment_request_view_controller.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/mac/foundation_util.h"
 
+#include "base/strings/stringprintf.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "components/autofill/core/browser/autofill_data_util.h"
@@ -377,10 +378,12 @@ typedef NS_ENUM(NSInteger, ItemType) {
     footer.text =
         l10n_util::GetNSString(IDS_PAYMENTS_CARD_AND_ADDRESS_SETTINGS);
   } else if ([[_dataSource authenticatedAccountName] length]) {
-    const base::string16 accountName =
-        base::SysNSStringToUTF16([_dataSource authenticatedAccountName]);
-    const std::string formattedString = l10n_util::GetStringFUTF8(
-        IDS_PAYMENTS_CARD_AND_ADDRESS_SETTINGS_SIGNED_IN, accountName);
+    const std::string unformattedString = l10n_util::GetStringUTF8(
+        IDS_PAYMENTS_CARD_AND_ADDRESS_SETTINGS_SIGNED_IN);
+    const std::string accountName =
+        base::SysNSStringToUTF8([_dataSource authenticatedAccountName]);
+    const std::string formattedString =
+        base::StringPrintf(unformattedString.c_str(), accountName.c_str());
     footer.text = base::SysUTF8ToNSString(formattedString);
   } else {
     footer.text = l10n_util::GetNSString(
