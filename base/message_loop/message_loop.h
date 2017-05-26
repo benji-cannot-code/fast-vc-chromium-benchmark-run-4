dac8ce5c3ed948a0856487db48978b0f512f149a
Scott Graham <scottmg@chromium.org>
2017-05-25 19:02:10 -0700

    fuchsia: boilerplate for MessageLoop and FilePathWatcher
    Add boilerplate based on libevent and/or posix versions of these. This
    is just a mechanical change so that subsequent CLs will be more able to
    focus on the meat of the implementations of these classes without
    getting excessively large.
    
    Bug: 706592
    Change-Id: Ia9e1356a863576acdf5ba7892f3fe853386e928c
    Reviewed-on: https://chromium-review.googlesource.com/516486
    Commit-Queue: Scott Graham <scottmg@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#474938}

--- base/message_loop/message_loop.h
+++ base/message_loop/message_loop.h
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // TODO(sky): these includes should not be necessary. Nuke them.
 #if defined(OS_WIN)
 #include "base/message_loop/message_pump_win.h"
+#elif defined(OS_FUCHSIA)
+#include "base/message_loop/message_pump_fuchsia.h"
 #elif defined(OS_IOS)
 #include "base/message_loop/message_pump_io_ios.h"
 #elif defined(OS_POSIX)
@@ -551,6 +553,13 @@ class BASE_EXPORT MessageLoopForIO : public MessageLoop {
 #if defined(OS_WIN)
   typedef MessagePumpForIO::IOHandler IOHandler;
   typedef MessagePumpForIO::IOContext IOContext;
+#elif defined(OS_FUCHSIA)
+  typedef MessagePumpFuchsia::Watcher Watcher;
+  typedef MessagePumpFuchsia::FileDescriptorWatcher FileDescriptorWatcher;
+
+  enum Mode{WATCH_READ = MessagePumpFuchsia::WATCH_READ,
+            WATCH_WRITE = MessagePumpFuchsia::WATCH_WRITE,
+            WATCH_READ_WRITE = MessagePumpFuchsia::WATCH_READ_WRITE};
 #elif defined(OS_IOS)
   typedef MessagePumpIOSForIO::Watcher Watcher;
   typedef MessagePumpIOSForIO::FileDescriptorWatcher
---
+++ base/message_loop/message_pump_fuchsia.cc
@@ -0,0 +1,55 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/message_loop/message_pump_fuchsia.h"
+
+#include "base/logging.h"
+
+namespace base {
+
+MessagePumpFuchsia::FileDescriptorWatcher::FileDescriptorWatcher(
+    const tracked_objects::Location& from_here)
+    : created_from_location_(from_here) {
+  NOTIMPLEMENTED();
+}
+
+MessagePumpFuchsia::FileDescriptorWatcher::~FileDescriptorWatcher() {}
+
+bool MessagePumpFuchsia::FileDescriptorWatcher::StopWatchingFileDescriptor() {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+MessagePumpFuchsia::MessagePumpFuchsia() {}
+
+MessagePumpFuchsia::~MessagePumpFuchsia() {}
+
+bool MessagePumpFuchsia::WatchFileDescriptor(int fd,
+                                             bool persistent,
+                                             int mode,
+                                             FileDescriptorWatcher* controller,
+                                             Watcher* delegate) {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+void MessagePumpFuchsia::Run(Delegate* delegate) {
+  NOTIMPLEMENTED();
+}
+
+void MessagePumpFuchsia::Quit() {
+  NOTIMPLEMENTED();
+}
+
+void MessagePumpFuchsia::ScheduleWork() {
+  NOTIMPLEMENTED();
+}
+
+void MessagePumpFuchsia::ScheduleDelayedWork(
+    const TimeTicks& delayed_work_time) {
+  NOTIMPLEMENTED();
+}
+
+}  // namespace base
