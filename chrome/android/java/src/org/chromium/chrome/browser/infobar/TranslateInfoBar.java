c1e475449931b6aeb43df9ed4bed376915ce15fc
ramyasharma <ramyasharma@chromium.org>
2017-05-26 01:11:26 -0700

    Adds translate infobar impression in the old Translate UI on Android
    This is added so that we can compare the impressions in old UI and
    the new one.
    
    BUG=720231
    
    Review-Url: https://codereview.chromium.org/2897233002
    Cr-Commit-Position: refs/heads/master@{#474961}

--- chrome/android/java/src/org/chromium/chrome/browser/infobar/TranslateInfoBar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/infobar/TranslateInfoBar.java
@@ -15,6 +15,7 @@ import android.text.style.ClickableSpan;
 import android.view.View;
 
 import org.chromium.base.annotations.CalledByNative;
+import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.R;
 import org.chromium.ui.base.DeviceFormFactor;
 
@@ -42,6 +43,8 @@ public class TranslateInfoBar extends InfoBar implements SubPanelListener {
     private int mOptionsPanelViewType;
     private TranslateSubPanel mSubPanel;
     private final boolean mShouldShowNeverBar;
+
+    private static final String INFOBAR_IMPRESSION_HISTOGRAM = "Translate.InfobarShown";
 
     @CalledByNative
     private static InfoBar show(int translateBarType, String sourceLanguageCode,
@@ -174,6 +177,16 @@ public class TranslateInfoBar extends InfoBar implements SubPanelListener {
 
     @Override
     public void createContent(InfoBarLayout layout) {
+        // Log infobar impression only when the infobar shown is either in step: before translate
+        // or in step: translating and always enabled is true. This is to remove duplicate
+        // impressions.
+        int infoBarType = getInfoBarType();
+        if (infoBarType == BEFORE_TRANSLATE_INFOBAR
+                || (mOptions.alwaysTranslateLanguageState()
+                           && infoBarType == TRANSLATING_INFOBAR)) {
+            RecordHistogram.recordBooleanHistogram(INFOBAR_IMPRESSION_HISTOGRAM, true);
+        }
+
         if (mOptionsPanelViewType == NO_PANEL) {
             mSubPanel = null;
         } else {
