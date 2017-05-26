956f9c6b80a230aaf7e109d28d709102ac39a550
erickung <erickung@chromium.org>
2017-05-26 12:23:40 -0700

    Enable MonoMixer in Filter Group
    - Add a flag --audio-output-channels to indicates number of audio
      output channel being sent to alsa and create loopback audio.
    - Enable MonoMixer if necessary (with above flag being set) inside
      filter group so that mono mixing won't depend on external post
      processing component such as waves.
    - Adjust log format to include process and thread ID
    
    Bug: internal b/38134177
    Review-Url: https://codereview.chromium.org/2909463002
    Cr-Commit-Position: refs/heads/master@{#475085}

--- chromecast/media/cma/backend/alsa/filter_group.cc
+++ chromecast/media/cma/backend/alsa/filter_group.cc
@@ -18,17 +18,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromecast {
 namespace media {
 FilterGroup::FilterGroup(int num_channels,
+                         bool mix_to_mono,
                          const std::string& name,
                          const base::ListValue* filter_list,
                          const std::unordered_set<std::string>& device_ids,
                          const std::vector<FilterGroup*>& mixed_inputs)
     : num_channels_(num_channels),
+      mix_to_mono_(mix_to_mono),
       name_(name),
       device_ids_(device_ids),
       mixed_inputs_(mixed_inputs),
       output_samples_per_second_(0),
       post_processing_pipeline_(
-          PostProcessingPipeline::Create(name_, filter_list, num_channels_)) {}
+          PostProcessingPipeline::Create(name_, filter_list, num_channels_)) {
+  for (auto* const m : mixed_inputs)
+    DCHECK_EQ(m->GetOutputChannelCount(), num_channels);
+  // Don't need mono mixer if input is single channel.
+  if (num_channels == 1)
+    mix_to_mono_ = false;
+}
 
 FilterGroup::~FilterGroup() = default;
 
@@ -109,6 +117,17 @@ float FilterGroup::MixAndFilter(int chunk_size) {
 
   delay_frames_ = post_processing_pipeline_->ProcessFrames(
       interleaved(), chunk_size, last_volume_, is_silence);
+
+  // Mono mixing after all processing if needed.
+  if (mix_to_mono_) {
+    for (int frame = 0; frame < chunk_size; ++frame) {
+      float sum = 0;
+      for (int c = 0; c < num_channels_; ++c)
+        sum += interleaved()[frame * num_channels_ + c];
+      interleaved()[frame] = sum / num_channels_;
+    }
+  }
+
   return last_volume_;
 }
 
@@ -119,6 +138,10 @@ int64_t FilterGroup::GetRenderingDelayMicroseconds() {
 
 void FilterGroup::ClearActiveInputs() {
   active_inputs_.clear();
+}
+
+int FilterGroup::GetOutputChannelCount() const {
+  return mix_to_mono_ ? 1 : num_channels_;
 }
 
 void FilterGroup::ResizeBuffersIfNecessary(int chunk_size) {
