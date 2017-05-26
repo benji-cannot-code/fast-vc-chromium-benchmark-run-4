510d93228f500dc417cfde828d8fbe241e4718e0
Maksim Ivanov <emaxx@chromium.org>
2017-05-26 13:11:41 +0200

    Skip Drive apps integration when extensions are disabled
    The Drive apps integration (mainly provided by DriveAppProvider) should
    be disabled when extensions are completely disabled - e.g. when the
    --disable-extensions command line flag is specified.
    
    Not doing this would result in unexpected attempts to fetch and install
    Drive apps, which will eventually fail and hit some DCHECK's in the
    extension system.
    
    TEST=Manual: run the browser built in Debug mode with
         target_os="chromeos" with the --disable-extensions flag specified
    
    Bug: chromium:709602
    Change-Id: I34e3b0850affea8f45a50cba54e7a671f7f4b720
    Reviewed-on: https://chromium-review.googlesource.com/517045
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#475027}

--- chrome/common/chrome_switches.cc
+++ chrome/common/chrome_switches.cc
@@ -1162,6 +1162,10 @@ bool ExtensionsDisabled(const base::CommandLine& command_line) {
          command_line.HasSwitch(switches::kDisableExtensionsExcept);
 }
 
+bool ExtensionsDisabled() {
+  return ExtensionsDisabled(*base::CommandLine::ForCurrentProcess());
+}
+
 bool MdFeedbackEnabled() {
   return base::CommandLine::ForCurrentProcess()->HasSwitch(
       ::switches::kEnableMaterialDesignFeedback);
