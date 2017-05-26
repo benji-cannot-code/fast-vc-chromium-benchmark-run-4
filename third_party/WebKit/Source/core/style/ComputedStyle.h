ac663a07d5ff6b342c9cc3338ac0482825f8307a
shend <shend@chromium.org>
2017-05-26 00:49:47 -0700

    Move RareNonInheritedData::*DataEquivalent to ComputedStyle.
    When we generate RareNonInheritedData, we would no longer generate
    helper methods like ReflectionDataEquivalent. These need to be
    handwritten in ComputedStyle. This patch moves these helper methods
    from RareNonInheritedData to ComputedStyle.
    
    This is preparation for generating RareNonInheritedData.
    
    BUG=628043
    
    Review-Url: https://codereview.chromium.org/2907713002
    Cr-Commit-Position: refs/heads/master@{#474958}

--- third_party/WebKit/Source/core/style/ComputedStyle.h
+++ third_party/WebKit/Source/core/style/ComputedStyle.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/style/AppliedTextDecorationList.h"
 #include "core/style/BorderValue.h"
 #include "core/style/ComputedStyleConstants.h"
+#include "core/style/ContentData.h"
 #include "core/style/CounterDirectives.h"
 #include "core/style/CursorData.h"
 #include "core/style/CursorList.h"
@@ -598,6 +599,9 @@ class CORE_EXPORT ComputedStyle : public ComputedStyleBase<ComputedStyle>,
     return rare_non_inherited_data_->box_shadow_.Get();
   }
   void SetBoxShadow(RefPtr<ShadowList>);
+  bool BoxShadowDataEquivalent(const ComputedStyle& other) const {
+    return DataEquivalent(BoxShadow(), other.BoxShadow());
+  }
 
   // clip
   static LengthBox InitialClip() { return LengthBox(); }
@@ -1421,6 +1425,9 @@ class CORE_EXPORT ComputedStyle : public ComputedStyleBase<ComputedStyle>,
       return;
     rare_non_inherited_data_.Access()->shape_outside_ = value;
   }
+  bool ShapeOutsideDataEquivalent(const ComputedStyle& other) const {
+    return DataEquivalent(ShapeOutside(), other.ShapeOutside());
+  }
 
   // size
   const FloatSize& PageSize() const {
@@ -1593,6 +1600,9 @@ class CORE_EXPORT ComputedStyle : public ComputedStyleBase<ComputedStyle>,
   void SetClipPath(RefPtr<ClipPathOperation> operation) {
     if (rare_non_inherited_data_->clip_path_ != operation)
       rare_non_inherited_data_.Access()->clip_path_ = std::move(operation);
+  }
+  bool ClipPathDataEquivalent(const ComputedStyle& other) const {
+    return DataEquivalent(ClipPath(), other.ClipPath());
   }
 
   // Mask properties.
@@ -2329,8 +2339,7 @@ class CORE_EXPORT ComputedStyle : public ComputedStyleBase<ComputedStyle>,
   }
   bool HasBoxReflect() const { return BoxReflect(); }
   bool ReflectionDataEquivalent(const ComputedStyle& other) const {
-    return rare_non_inherited_data_->ReflectionDataEquivalent(
-        *other.rare_non_inherited_data_);
+    return DataEquivalent(BoxReflect(), other.BoxReflect());
   }
 
   // Mask utility functions.
@@ -3110,8 +3119,7 @@ class CORE_EXPORT ComputedStyle : public ComputedStyleBase<ComputedStyle>,
 
   // Content utility functions.
   bool ContentDataEquivalent(const ComputedStyle& other) const {
-    return rare_non_inherited_data_->ContentDataEquivalent(
-        *other.rare_non_inherited_data_);
+    return DataEquivalent(GetContentData(), other.GetContentData());
   }
 
   // Contain utility functions.
