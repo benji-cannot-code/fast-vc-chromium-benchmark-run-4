85368743b629a1511389191464b176203b1487da
ccameron <ccameron@chromium.org>
2017-05-26 01:34:31 -0700

    color: Draw blink::DragImage to sRGB
    When click-dragging an image, color convert the image to sRGB.
    
    Prior to this patch, we would convert the image to the output color
    space, but that is not well defined.
    
    BUG=672316
    
    Review-Url: https://codereview.chromium.org/2911453002
    Cr-Commit-Position: refs/heads/master@{#474969}

--- third_party/WebKit/Source/platform/DragImage.cpp
+++ third_party/WebKit/Source/platform/DragImage.cpp
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/wtf/text/WTFString.h"
 #include "skia/ext/image_operations.h"
 #include "third_party/skia/include/core/SkCanvas.h"
+#include "third_party/skia/include/core/SkColorSpaceXformCanvas.h"
 #include "third_party/skia/include/core/SkImage.h"
 #include "third_party/skia/include/core/SkMatrix.h"
 #include "third_party/skia/include/core/SkSurface.h"
@@ -114,6 +115,12 @@ PaintImage DragImage::ResizeAndOrientImage(
                              : kHigh_SkFilterQuality);
 
   SkCanvas* canvas = surface->getCanvas();
+  std::unique_ptr<SkCanvas> color_transform_canvas;
+  if (RuntimeEnabledFeatures::colorCorrectRenderingEnabled()) {
+    color_transform_canvas =
+        SkCreateColorSpaceXformCanvas(canvas, SkColorSpace::MakeSRGB());
+    canvas = color_transform_canvas.get();
+  }
   canvas->concat(AffineTransformToSkMatrix(transform));
   canvas->drawImage(image.sk_image(), 0, 0, &paint);
 
