6c8f9aeab5056747236e270fc06387d51bba75dc
ericwilligers <ericwilligers@chromium.org>
2017-05-25 19:20:48 -0700

    CSS Motion Path: Web Platform tests for longhand parsing
    We introduce web platform tests for parsing of
    - offset-position
    - offset-path
    - offset-distance
    - offset-rotate
    - offset-anchor
    
    The unshipped properties offset-position and offset-anchor currently
    fail a test because we have not yet updated position parsing for
    a recent CSS Values spec change (see http://crbug.com/717833).
    
    offset-rotate currently fails a test because we parse 0 as an angle.
    This was correct (but leads to shorthand parsing ambiguities) until
    https://github.com/w3c/csswg-drafts/issues/1162
    
    offset-path fails some tests because we don't yet support url or
    basic-shape || geometry-box shapes.
    
    BUG=722757
    
    Review-Url: https://codereview.chromium.org/2886703002
    Cr-Commit-Position: refs/heads/master@{#474881}

---
+++ third_party/WebKit/LayoutTests/external/wpt/css/motion-1/parsing/resources/parsing-testcommon.js
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+'use strict';
+
+function test_valid_value(property, value, serializedValue) {
+    if (arguments.length < 3)
+        serializedValue = value;
+
+    var stringifiedValue = JSON.stringify(value);
+
+    test(function(){
+        var div = document.createElement('div');
+        div.style[property] = value;
+        assert_not_equals(div.style[property], "");
+    }, "e.style['" + property + "'] = " + stringifiedValue + " should set the property value");
+
+    test(function(){
+        var div = document.createElement('div');
+        div.style[property] = value;
+        var readValue = div.style[property];
+        assert_equals(readValue, serializedValue);
+        div.style[property] = readValue;
+        assert_equals(div.style[property], readValue);
+    }, "Serialization should round-trip after setting e.style['" + property + "'] = " + stringifiedValue);
+}
+
+function test_invalid_value(property, value) {
+    var stringifiedValue = JSON.stringify(value);
+
+    test(function(){
+        var div = document.createElement('div');
+        div.style[property] = value;
+        assert_equals(div.style[property], "");
+    }, "e.style['" + property + "'] = " + stringifiedValue + " should not set the property value");
+}
