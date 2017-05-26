7fc9ba908b5a2cdf5220904488092888476b2be1
shend <shend@chromium.org>
2017-05-25 22:38:25 -0700

    Make EImageRendering an enum class.
    This patch makes EImageRendering an enum class for better type safety.
    
    BUG=684966
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2899633002
    Cr-Commit-Position: refs/heads/master@{#474926}

--- third_party/WebKit/Source/core/page/DragController.cpp
+++ third_party/WebKit/Source/core/page/DragController.cpp
@@ -1019,7 +1019,7 @@ static std::unique_ptr<DragImage> DragImageForImage(
 
   InterpolationQuality interpolation_quality =
       element->EnsureComputedStyle()->ImageRendering() ==
-              kImageRenderingPixelated
+              EImageRendering::kPixelated
           ? kInterpolationNone
           : kInterpolationHigh;
   RespectImageOrientationEnum should_respect_image_orientation =
