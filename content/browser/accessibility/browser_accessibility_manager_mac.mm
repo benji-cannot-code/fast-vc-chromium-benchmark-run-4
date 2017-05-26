518e52db434f1e400a0d0a8052bb0ac3b34ed6d7
dmazzoni <dmazzoni@chromium.org>
2017-05-26 02:03:10 -0700

    Fix DumpAccessibilityEvents tests on Mac and improve test coverage
    Fix some minor issues that caused DumpAccessibilityEvents tests to fail
    or behave flakily. Re-enable several tests and add many more with a
    goal of covering all possible Mac notifications with tests.
    
    This is part of a larger effort to fire fewer events from Blink
    and fire them in platform-specific code instead. Increasing
    test coverage will help with this.
    
    BUG=719030,699438
    
    Review-Url: https://codereview.chromium.org/2897943003
    Cr-Commit-Position: refs/heads/master@{#474971}

--- content/browser/accessibility/browser_accessibility_manager_mac.mm
+++ content/browser/accessibility/browser_accessibility_manager_mac.mm
@@ -223,8 +223,6 @@ void BrowserAccessibilityManagerMac::NotifyAccessibilityEvent(
       BrowserAccessibility* focus = GetFocus();
       if (!focus)
         break;  // Just fire a notification on the root.
-      NSAccessibilityPostNotification(ToBrowserAccessibilityCocoa(focus),
-                                      mac_notification);
 
       if (base::mac::IsAtLeastOS10_11()) {
         // |NSAccessibilityPostNotificationWithUserInfo| should be used on OS X
@@ -243,6 +241,9 @@ void BrowserAccessibilityManagerMac::NotifyAccessibilityEvent(
         NSAccessibilityPostNotificationWithUserInfo(
             ToBrowserAccessibilityCocoa(root), mac_notification, user_info);
         return;
+      } else {
+        NSAccessibilityPostNotification(ToBrowserAccessibilityCocoa(focus),
+                                        mac_notification);
       }
       break;
     }
@@ -289,6 +290,13 @@ void BrowserAccessibilityManagerMac::NotifyAccessibilityEvent(
       // Voiceover seems to drop live region changed notifications if they come
       // too soon after a live region created notification.
       // TODO(nektar): Limit the number of changed notifications as well.
+
+      if (never_suppress_or_delay_events_for_testing_) {
+        NSAccessibilityPostNotification(
+            native_node, NSAccessibilityLiveRegionChangedNotification);
+        return;
+      }
+
       base::scoped_nsobject<BrowserAccessibilityCocoa> retained_node(
           [native_node retain]);
       BrowserThread::PostDelayedTask(
