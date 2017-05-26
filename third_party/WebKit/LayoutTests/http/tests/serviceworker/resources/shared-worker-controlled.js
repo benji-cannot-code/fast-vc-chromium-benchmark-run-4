7645a88111b49149ec6857b126a0932d33bea134
mike <mike@mikepennisi.com>
2017-05-26 11:28:25 -0700

    Remove duplicate service wrkr "shared worker" test
    This test is identical to the version available in the Web Platform
    Tests project and may therefore be removed without impacting coverage.
    
    BUG=688116
    R=falken@chromium.org
    
    Review-Url: https://codereview.chromium.org/2897113002
    Cr-Commit-Position: refs/heads/master@{#475066}

--- third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/shared-worker-controlled.js
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-onconnect = function(e) {
-  var port = e.ports[0];
-  var xhr = new XMLHttpRequest();
-  xhr.onload = function() { port.postMessage(this.responseText); };
-  xhr.onerror = function(e) { port.postMessage(e); };
-  xhr.open('GET', 'dummy.txt?simple', true);
-  xhr.send();
-};
