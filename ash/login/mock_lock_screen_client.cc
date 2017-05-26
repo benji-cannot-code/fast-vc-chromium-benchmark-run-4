a0ac8779434ac110fe7cd233d2725e6b0b246290
jdufault <jdufault@chromium.org>
2017-05-26 14:25:55 -0700

    cros: Pull TestLockScreenClient into a separate MockLockScreenClient file.
    - converted the class to a mock.
    - made it easy for tests to create an instance by defining a helper method.
    
    BUG=719015
    
    Review-Url: https://codereview.chromium.org/2894193002
    Cr-Commit-Position: refs/heads/master@{#475135}

---
+++ ash/login/mock_lock_screen_client.cc
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/login/mock_lock_screen_client.h"
+
+#include "ash/login/lock_screen_controller.h"
+#include "ash/shell.h"
+
+namespace ash {
+
+MockLockScreenClient::MockLockScreenClient() : binding_(this) {}
+
+MockLockScreenClient::~MockLockScreenClient() = default;
+
+mojom::LockScreenClientPtr MockLockScreenClient::CreateInterfacePtrAndBind() {
+  return binding_.CreateInterfacePtrAndBind();
+}
+
+std::unique_ptr<MockLockScreenClient> BindMockLockScreenClient() {
+  LockScreenController* lock_screen_controller =
+      Shell::Get()->lock_screen_controller();
+  auto lock_screen_client = base::MakeUnique<MockLockScreenClient>();
+  lock_screen_controller->SetClient(
+      lock_screen_client->CreateInterfacePtrAndBind());
+  return lock_screen_client;
+}
+
+}  // namespace ash
