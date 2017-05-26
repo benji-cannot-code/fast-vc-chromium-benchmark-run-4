bd8449ffae228096a308c92264b11eeb40f81034
Thiago Farina <tfarina@chromium.org>
2017-05-26 17:27:30 -0300

    remove --memory-metrics switch
    --memory-metrics enable the sampling of two histograms: Memory.BrowserUsed
    and Memory.RendererUsed. Those are currently reported in telemetry but:
     - nobody seems to care / be aware of those.
     - there are no perf alerts associated to them.
    
    As memory benchmarks are being superseded by the ones based on memory-infra,
    this code can be removed now.
    
    BUG=581365
    
    Change-Id: I19c2091d7eae643a37f051739a9d68b1bcbc6e71
    Reviewed-on: https://chromium-review.googlesource.com/517482
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Thiago Farina <tfarina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#476261}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -2150,7 +2150,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kMainFrameResizesAreOrientationChanges,
     switches::kMaxUntiledLayerWidth,
     switches::kMaxUntiledLayerHeight,
-    switches::kMemoryMetrics,
     switches::kMojoLocalStorage,
     switches::kMSEAudioBufferSizeLimit,
     switches::kMSEVideoBufferSizeLimit,
