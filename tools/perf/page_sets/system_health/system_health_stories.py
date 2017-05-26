6566eadd7eeecb1ecb748ed1cc767653e7548cbb
Juan A. Navarro Perez <perezju@chromium.org>
2017-05-26 14:01:51 +0100

    [tools/perf] Clean up some memory benchmarks
    Remove system_health.webview_startup_multiprocess (no longer needed),
    and move memory.dual_browser_test to contrib.
    
    Also fix a bug where the story set for system_health.webview_startup
    was not working correctly.
    
    Bug: 724519,720257,726639
    Change-Id: I042aa62ac36e91b6182fc00f6587aad62b935eff
    Reviewed-on: https://chromium-review.googlesource.com/514044
    Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Reviewed-by: Selim Gurun <sgurun@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#474998}

--- tools/perf/page_sets/system_health/system_health_stories.py
+++ tools/perf/page_sets/system_health/system_health_stories.py
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import os
 
+from page_sets.system_health import chrome_stories
 from page_sets.system_health import platforms
 from page_sets.system_health import system_health_story
 
@@ -30,6 +31,14 @@ class SystemHealthStorySet(story.StorySet):
           case and not story_class.NAME.startswith(case + ':')):
         continue
       self.AddStory(story_class(self, take_memory_measurement))
+
+
+class SystemHealthBlankStorySet(story.StorySet):
+  """A story set containing the chrome:blank story only."""
+  def __init__(self, take_memory_measurement=False):
+    super(SystemHealthBlankStorySet, self).__init__()
+    self.AddStory(
+        chrome_stories.BlankAboutBlankStory(self, take_memory_measurement))
 
 
 class DesktopSystemHealthStorySet(SystemHealthStorySet):
