fc7466260c1a7ec4fd0a78b867a1e5cd9455e2ae
patricialor <patricialor@chromium.org>
2017-05-25 18:25:32 -0700

    a11y: Exclude children of nested keyboard accessible controls from a11y tree.
    Currently, children of keyboard accessible controls, such as the Labels inside
    LabelButtons, are not hidden from the accessibility tree. This is not useful
    because the user only cares about the element that has focus, not any children
    inside it, which are implementation details. Set any children of keyboard
    accessible controls to have ignored roles, which indicates to the OS (works for
    Windows, Mac, and Linux) that the ignored element should be excluded from the
    accessibility tree.
    
    BUG=610589,723574
    TEST=On Mac: Open the XCode Accessibility Inspector. On Windows: Open the
    Windows SDK 'Inspect.exe' and make sure the icon for 'Watch Focus' is selected.
    Then (on Mac, hover over; on Windows, focus) a views::LabelButton (e.g. on the
    HTTP Authentication dialog). It should report an empty set of children and its
    role ("Type"/"accessibilityRole" on Mac; "Role"/"ControlType" on Windows) should
    say it is a button.
    
    Review-Url: https://codereview.chromium.org/2119413004
    Cr-Commit-Position: refs/heads/master@{#474867}

--- ui/views/accessibility/native_view_accessibility_base.cc
+++ ui/views/accessibility/native_view_accessibility_base.cc
@@ -15,6 +15,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace views {
 
+namespace {
+
+bool IsAccessibilityFocusableWhenEnabled(View* view) {
+  return view->focus_behavior() != View::FocusBehavior::NEVER &&
+         view->IsDrawn();
+}
+
+// Used to determine if a View should be ignored by accessibility clients by
+// being a non-keyboard-focusable child of a keyboard-focusable ancestor. E.g.,
+// LabelButtons contain Labels, but a11y should just show that there's a button.
+bool IsViewUnfocusableChildOfFocusableAncestor(View* view) {
+  if (IsAccessibilityFocusableWhenEnabled(view))
+    return false;
+
+  while (view->parent()) {
+    view = view->parent();
+    if (IsAccessibilityFocusableWhenEnabled(view))
+      return true;
+  }
+  return false;
+}
+
+}  // namespace
+
 NativeViewAccessibilityBase::NativeViewAccessibilityBase(View* view)
     : view_(view),
       parent_widget_(nullptr),
@@ -66,6 +90,11 @@ const ui::AXNodeData& NativeViewAccessibilityBase::GetData() const {
   if (!view_->IsDrawn())
     data_.AddState(ui::AX_STATE_INVISIBLE);
 
+  // Make sure this element is excluded from the a11y tree if there's a
+  // focusable parent. All keyboard focusable elements should be leaf nodes.
+  // Exceptions to this rule will themselves be accessibility focusable.
+  if (IsViewUnfocusableChildOfFocusableAncestor(view_))
+    data_.role = ui::AX_ROLE_IGNORED;
   return data_;
 }
 
