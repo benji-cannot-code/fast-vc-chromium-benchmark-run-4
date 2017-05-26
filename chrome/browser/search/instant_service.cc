95fad155be662000de262d6a03125105cbceda45
pkotwicz <pkotwicz@chromium.org>
2017-05-26 12:28:05 -0700

    Delete unused chrome://fallback-icon/ handling
    BUG=712475
    R=huangs,jkrcal,pkasting
    TBR=treib for removal of stray includes in chrome/browser/search/
    
    Review-Url: https://codereview.chromium.org/2883053003
    Cr-Commit-Position: refs/heads/master@{#475087}

--- chrome/browser/search/instant_service.cc
+++ chrome/browser/search/instant_service.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/theme_source.h"
 #include "chrome/common/render_messages.h"
 #include "chrome/grit/theme_resources.h"
-#include "components/favicon/core/fallback_icon_service.h"
 #include "components/history/core/browser/top_sites.h"
 #include "components/image_fetcher/core/image_fetcher_impl.h"
 #include "components/keyed_service/core/service_access_type.h"
