b6b7072b8de20fa33f9913a1060b7e47946a3cd7
jkwang <jkwang@google.com>
2017-05-26 13:35:24 -0700

    ozone: evdev: Use correct integer width for gamepad axis values
    Gamepad event value should be int instead of uint16_t. This patch fixes
    the bug.
    
    BUG=717246
    TEST=Add/Fix and run unittest.
    
    Review-Url: https://codereview.chromium.org/2893073003
    Cr-Commit-Position: refs/heads/master@{#475110}

--- ui/events/ozone/evdev/gamepad_event_converter_evdev.h
+++ ui/events/ozone/evdev/gamepad_event_converter_evdev.h
@@ -47,12 +47,12 @@ class EVENTS_OZONE_EVDEV_EXPORT GamepadEventConverterEvdev
  private:
   // This function processes EV_KEY event from gamepad device.
   void ProcessEvdevKey(uint16_t code,
-                       uint16_t value,
+                       int value,
                        const base::TimeTicks& timestamp);
 
   // This function processes EV_ABS event from gamepad device.
   void ProcessEvdevAbs(uint16_t code,
-                       uint16_t value,
+                       int value,
                        const base::TimeTicks& timestamp);
 
   // This function releases all the keys and resets all the axises.
@@ -83,7 +83,7 @@ class EVENTS_OZONE_EVDEV_EXPORT GamepadEventConverterEvdev
          GamepadEventType mapped_type,
          uint16_t mapped_code);
 
-    bool MapValue(uint16_t value, double* mapped_value);
+    bool MapValue(int value, double* mapped_value);
 
     GamepadEventType mapped_type();
 
