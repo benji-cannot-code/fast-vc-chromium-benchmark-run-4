ae0028a3809de92528eb50a579fd9bde66d11093
mmenke <mmenke@chromium.org>
2017-05-26 13:36:11 -0700

    Make HttpNetworkSession::host_mapping_rules no longer a pointer.
    TBR=dimich@chromium.org
    BUG=725653
    
    Review-Url: https://codereview.chromium.org/2906463002
    Cr-Commit-Position: refs/heads/master@{#475111}

--- net/http/http_network_session.h
+++ net/http/http_network_session.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
 #include "base/threading/non_thread_safe.h"
+#include "net/base/host_mapping_rules.h"
 #include "net/base/host_port_pair.h"
 #include "net/base/net_export.h"
 #include "net/dns/host_resolver.h"
@@ -96,7 +97,7 @@ class NET_EXPORT HttpNetworkSession
     HttpAuthHandlerFactory* http_auth_handler_factory;
     HttpServerProperties* http_server_properties;
     NetLog* net_log;
-    HostMappingRules* host_mapping_rules;
+    HostMappingRules host_mapping_rules;
     SocketPerformanceWatcherFactory* socket_performance_watcher_factory;
     bool ignore_certificate_errors;
     uint16_t testing_fixed_http_port;
