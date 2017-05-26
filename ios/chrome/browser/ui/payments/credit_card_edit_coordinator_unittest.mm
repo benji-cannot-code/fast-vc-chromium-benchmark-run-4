7984d1dc22953f3e0a31449351baf4c0931cfb95
mahmadi <mahmadi@chromium.org>
2017-05-26 06:46:24 -0700

    [Payment Request] Improved validation and tests for the credit card editor
    BUG=602666
    
    Review-Url: https://codereview.chromium.org/2902423003
    Cr-Commit-Position: refs/heads/master@{#474997}

--- ios/chrome/browser/ui/payments/credit_card_edit_coordinator_unittest.mm
+++ ios/chrome/browser/ui/payments/credit_card_edit_coordinator_unittest.mm
@@ -32,6 +32,7 @@ class MockTestPersonalDataManager : public autofill::TestPersonalDataManager {
  public:
   MockTestPersonalDataManager() : TestPersonalDataManager() {}
   MOCK_METHOD1(AddCreditCard, void(const autofill::CreditCard&));
+  MOCK_METHOD1(UpdateCreditCard, void(const autofill::CreditCard&));
 };
 
 class MockPaymentRequest : public PaymentRequest {
@@ -174,6 +175,8 @@ TEST_F(PaymentRequestCreditCardEditCoordinatorTest, DidFinishCreatingWithSave) {
               AddCreditCard(CreditCardMatches("4111111111111111", "John Doe",
                                               "12", "2090", "12345")))
       .Times(1);
+  // No credit card should get updated in the PersonalDataManager.
+  EXPECT_CALL(personal_data_manager_, UpdateCreditCard(_)).Times(0);
 
   // Call the controller delegate method.
   CreditCardEditViewController* view_controller =
@@ -224,6 +227,8 @@ TEST_F(PaymentRequestCreditCardEditCoordinatorTest, DidFinishCreatingNoSave) {
       .Times(1);
   // No credit card should get added to the PersonalDataManager.
   EXPECT_CALL(personal_data_manager_, AddCreditCard(_)).Times(0);
+  // No credit card should get updated in the PersonalDataManager.
+  EXPECT_CALL(personal_data_manager_, UpdateCreditCard(_)).Times(0);
 
   // Call the controller delegate method.
   CreditCardEditViewController* view_controller =
@@ -274,6 +279,11 @@ TEST_F(PaymentRequestCreditCardEditCoordinatorTest, DidFinishEditing) {
   EXPECT_CALL(*payment_request_, AddCreditCard(_)).Times(0);
   // No credit card should get added to the PersonalDataManager.
   EXPECT_CALL(personal_data_manager_, AddCreditCard(_)).Times(0);
+  // Expect a credit card to be updated in the PersonalDataManager.
+  EXPECT_CALL(personal_data_manager_,
+              UpdateCreditCard(CreditCardMatches("4111111111111111", "John Doe",
+                                                 "12", "2090", "12345")))
+      .Times(1);
 
   // Call the controller delegate method.
   CreditCardEditViewController* view_controller =
