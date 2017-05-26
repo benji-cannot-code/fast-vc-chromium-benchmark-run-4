98b4edf17927c79d6230c70a577c0fe7600a55a4
Rune Lillesveen <rune@opera.com>
2017-05-26 16:24:28 +0200

    Use LowerASCII instead of DeprecatedLower in css/
    Incorrectly folded upper-case non-ascii characters into ascii for type,
    attribute, id, and class selectors causing non-matching selectors to
    match.
    
    Also fixed for media types and features.
    
    BUG=726176
    
    Change-Id: I5a6f813b2722ee4efcff2ab933f5ad075faadcbb
    Reviewed-on: https://chromium-review.googlesource.com/517105
    Commit-Queue: Rune Lillesveen <rune@opera.com>
    Reviewed-by: Morten Stenshorne <mstensho@opera.com>
    Cr-Commit-Position: refs/heads/master@{#475021}

--- third_party/WebKit/Source/core/css/parser/CSSParserImpl.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSParserImpl.cpp
@@ -283,7 +283,7 @@ CSSSelectorList CSSParserImpl::ParsePageSelector(
     selector = CSSParserSelector::Create();
     if (!pseudo.IsNull()) {
       selector->SetMatch(CSSSelector::kPagePseudoClass);
-      selector->UpdatePseudoPage(pseudo.DeprecatedLower());
+      selector->UpdatePseudoPage(pseudo.LowerASCII());
       if (selector->GetPseudoType() == CSSSelector::kPseudoUnknown)
         return CSSSelectorList();
     }
