1a34708119becf9be0d97db4f24f51b92361abd1
sigbjornf <sigbjornf@opera.com>
2017-05-26 01:23:56 -0700

    Enable heap compaction on all 'container' arenas.
    Reconcile indexing for compactable arenas to always be wrt
    the arena index -- it was miscued on setting, using an
    offset instead, thereby preventing vector[1-4] arenas from
    being considered for compaction.
    
    R=haraken
    BUG=726335
    
    Review-Url: https://codereview.chromium.org/2908463002
    Cr-Commit-Position: refs/heads/master@{#474966}

--- third_party/WebKit/Source/platform/heap/HeapCompactTest.cpp
+++ third_party/WebKit/Source/platform/heap/HeapCompactTest.cpp
@@ -17,13 +17,38 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+enum VerifyArenaCompaction {
+  NoVerify,
+  VectorsAreCompacted,
+  HashTablesAreCompacted,
+};
+
 class IntWrapper : public blink::GarbageCollectedFinalized<IntWrapper> {
  public:
-  static IntWrapper* Create(int x) { return new IntWrapper(x); }
+  static IntWrapper* Create(int x, VerifyArenaCompaction verify = NoVerify) {
+    return new IntWrapper(x, verify);
+  }
 
   virtual ~IntWrapper() {}
 
-  DEFINE_INLINE_TRACE() {}
+  DEFINE_INLINE_TRACE() {
+    // Verify if compaction is indeed activated.
+    //
+    // What arenas end up being compacted is dependent on residency,
+    // so approximate the arena checks to fit.
+    blink::HeapCompact* compaction = visitor->Heap().Compaction();
+    switch (verify_) {
+      case NoVerify:
+        return;
+      case HashTablesAreCompacted:
+        CHECK(compaction->IsCompactingArena(
+            blink::BlinkGC::kHashTableArenaIndex));
+        return;
+      case VectorsAreCompacted:
+        CHECK(compaction->IsCompactingVectorArenas());
+        return;
+    }
+  }
 
   int Value() const { return x_; }
 
@@ -33,11 +58,13 @@ class IntWrapper : public blink::GarbageCollectedFinalized<IntWrapper> {
 
   unsigned GetHash() { return IntHash<int>::GetHash(x_); }
 
-  IntWrapper(int x) : x_(x) {}
+  IntWrapper(int x, VerifyArenaCompaction verify) : x_(x), verify_(verify) {}
 
  private:
   IntWrapper();
+
   int x_;
+  VerifyArenaCompaction verify_;
 };
 static_assert(WTF::IsTraceable<IntWrapper>::value,
               "IsTraceable<> template failed to recognize trace method.");
@@ -218,7 +245,7 @@ static void ClearOutOldGarbage() {
 TEST(HeapCompactTest, CompactVector) {
   ClearOutOldGarbage();
 
-  IntWrapper* val = IntWrapper::Create(1);
+  IntWrapper* val = IntWrapper::Create(1, VectorsAreCompacted);
   Persistent<IntVector> vector = new IntVector(10, val);
   EXPECT_EQ(10u, vector->size());
 
@@ -236,7 +263,7 @@ TEST(HeapCompactTest, CompactHashMap) {
 
   Persistent<IntMap> int_map = new IntMap();
   for (size_t i = 0; i < 100; ++i) {
-    IntWrapper* val = IntWrapper::Create(i);
+    IntWrapper* val = IntWrapper::Create(i, HashTablesAreCompacted);
     int_map->insert(val, 100 - i);
   }
 
@@ -259,7 +286,7 @@ TEST(HeapCompactTest, CompactVectorPartHashMap) {
   for (size_t i = 0; i < 10; ++i) {
     IntMap map;
     for (size_t j = 0; j < 10; ++j) {
-      IntWrapper* val = IntWrapper::Create(j);
+      IntWrapper* val = IntWrapper::Create(j, VectorsAreCompacted);
       map.insert(val, 10 - j);
     }
     int_map_vector->push_back(map);
@@ -293,7 +320,7 @@ TEST(HeapCompactTest, CompactHashPartVector) {
   for (size_t i = 0; i < 10; ++i) {
     IntVector vector;
     for (size_t j = 0; j < 10; ++j) {
-      vector.push_back(IntWrapper::Create(j));
+      vector.push_back(IntWrapper::Create(j, HashTablesAreCompacted));
     }
     int_vector_map->insert(1 + i, vector);
   }
@@ -320,7 +347,7 @@ TEST(HeapCompactTest, CompactHashPartVector) {
 TEST(HeapCompactTest, CompactDeques) {
   Persistent<IntDeque> deque = new IntDeque;
   for (int i = 0; i < 8; ++i) {
-    deque->push_front(IntWrapper::Create(i));
+    deque->push_front(IntWrapper::Create(i, VectorsAreCompacted));
   }
   EXPECT_EQ(8u, deque->size());
 
@@ -336,7 +363,7 @@ TEST(HeapCompactTest, CompactDeques) {
 TEST(HeapCompactTest, CompactDequeVectors) {
   Persistent<HeapDeque<IntVector>> deque = new HeapDeque<IntVector>;
   for (int i = 0; i < 8; ++i) {
-    IntWrapper* value = IntWrapper::Create(i);
+    IntWrapper* value = IntWrapper::Create(i, VectorsAreCompacted);
     IntVector vector = IntVector(8, value);
     deque->push_front(vector);
   }
@@ -355,7 +382,7 @@ TEST(HeapCompactTest, CompactLinkedHashSet) {
   using OrderedHashSet = HeapLinkedHashSet<Member<IntWrapper>>;
   Persistent<OrderedHashSet> set = new OrderedHashSet;
   for (int i = 0; i < 13; ++i) {
-    IntWrapper* value = IntWrapper::Create(i);
+    IntWrapper* value = IntWrapper::Create(i, HashTablesAreCompacted);
     set->insert(value);
   }
   EXPECT_EQ(13u, set->size());
