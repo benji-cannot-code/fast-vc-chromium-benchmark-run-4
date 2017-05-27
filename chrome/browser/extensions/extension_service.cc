d982a18c5d3abd1f642d901044b46637efecc499
rdevlin.cronin <rdevlin.cronin@chromium.org>
2017-05-26 18:27:59 -0700

    [Extensions] Simplify OnLoadedInstalledExtensions calls
    InstalledLoader calls ExtensionService::OnLoadedInstalledExtensions()
    at the completion of LoadAllExtensions(), but since
    LoadAllExtensions() is synchronous, we can just call
    OnLoadedInstalledExtensions() directly from ExtensionService. This
    avoids a weird branch (where we don't call LoadAllExtensions()) and lets
    us make OnLoadedInstalledExtensions private.
    
    Also de-virtual OnLoadedInstalledExtensions(), since it was never
    overridden.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2905003002
    Cr-Commit-Position: refs/heads/master@{#475204}

--- chrome/browser/extensions/extension_service.cc
+++ chrome/browser/extensions/extension_service.cc
@@ -452,16 +452,11 @@ void ExtensionService::Init() {
     load_command_line_extensions = false;
   }
 #endif
-  if (load_saved_extensions) {
+  if (load_saved_extensions)
     extensions::InstalledLoader(this).LoadAllExtensions();
-  } else {
-    // InstalledLoader::LoadAllExtensions normally calls
-    // OnLoadedInstalledExtensions itself, but here we circumvent that path.
-    // Call OnLoadedInstalledExtensions directly.
-    // TODO(devlin): LoadInstalledExtensions() is synchronous - we can simplify
-    // this.
-    OnLoadedInstalledExtensions();
-  }
+
+  OnInstalledExtensionsLoaded();
+
   LoadExtensionsFromCommandLineFlag(switches::kDisableExtensionsExcept);
   if (load_command_line_extensions)
     LoadExtensionsFromCommandLineFlag(switches::kLoadExtension);
@@ -1456,6 +1451,7 @@ void ExtensionService::ReloadExtensionsForTest() {
   UnloadAllExtensionsInternal();
   component_loader_->LoadAll();
   extensions::InstalledLoader(this).LoadAllExtensions();
+  OnInstalledExtensionsLoaded();
   // Don't call SetReadyAndNotifyListeners() since tests call this multiple
   // times.
 }
@@ -1471,25 +1467,6 @@ void ExtensionService::SetReadyAndNotifyListeners() {
       extensions::NOTIFICATION_EXTENSIONS_READY_DEPRECATED,
       content::Source<Profile>(profile_),
       content::NotificationService::NoDetails());
-}
-
-void ExtensionService::OnLoadedInstalledExtensions() {
-  if (updater_)
-    updater_->Start();
-
-  // Enable any Shared Modules that incorrectly got disabled previously.
-  // This is temporary code to fix incorrect behavior from previous versions of
-  // Chrome and can be removed after several releases (perhaps M60).
-  extensions::ExtensionList to_enable;
-  for (const auto& extension : registry_->disabled_extensions()) {
-    if (SharedModuleInfo::IsSharedModule(extension.get()))
-      to_enable.push_back(extension);
-  }
-  for (const auto& extension : to_enable) {
-    EnableExtension(extension->id());
-  }
-
-  OnBlacklistUpdated();
 }
 
 void ExtensionService::AddExtension(const Extension* extension) {
@@ -2561,4 +2538,23 @@ void ExtensionService::OnProfileDestructionStarted() {
        ++it) {
     UnloadExtension(*it, UnloadedExtensionReason::PROFILE_SHUTDOWN);
   }
+}
+
+void ExtensionService::OnInstalledExtensionsLoaded() {
+  if (updater_)
+    updater_->Start();
+
+  // Enable any Shared Modules that incorrectly got disabled previously.
+  // This is temporary code to fix incorrect behavior from previous versions of
+  // Chrome and can be removed after several releases (perhaps M60).
+  extensions::ExtensionList to_enable;
+  for (const auto& extension : registry_->disabled_extensions()) {
+    if (SharedModuleInfo::IsSharedModule(extension.get()))
+      to_enable.push_back(extension);
+  }
+  for (const auto& extension : to_enable) {
+    EnableExtension(extension->id());
+  }
+
+  OnBlacklistUpdated();
 }
