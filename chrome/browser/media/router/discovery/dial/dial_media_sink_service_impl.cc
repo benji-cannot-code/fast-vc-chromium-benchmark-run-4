3d043d196bc5de3374aeaf2fabdb959fd69ef825
zhaobin <zhaobin@chromium.org>
2017-05-26 20:12:45 -0700

    [Media Router] Use DialMediaSinkServiceDelegate in MediaRouterMojoImpl
    - Added DialMediaSinkServiceDelegate to handle thread hopping between IO/UI thread
    - Added MediaRouterMojoImpl::StartDiscovery()
    
    TODO:
    - Call StartDiscovery() in MediaRouterMojoImpl::RegisterMediaRouteProvider()
    
    BUG=687375
    
    Review-Url: https://codereview.chromium.org/2837363002
    Cr-Commit-Position: refs/heads/master@{#475230}

--- chrome/browser/media/router/discovery/dial/dial_media_sink_service_impl.cc
+++ chrome/browser/media/router/discovery/dial/dial_media_sink_service_impl.cc
@@ -32,9 +32,11 @@ void DialMediaSinkServiceImpl::Start() {
   if (dial_registry_)
     return;
 
-  dial_registry_ = DialRegistry::GetInstance();
+  dial_registry_ =
+      test_dial_registry_ ? test_dial_registry_ : DialRegistry::GetInstance();
   dial_registry_->RegisterObserver(this);
   dial_registry_->OnListenerAdded();
+  MediaSinkServiceBase::StartTimer();
 }
 
 void DialMediaSinkServiceImpl::Stop() {
@@ -45,6 +47,7 @@ void DialMediaSinkServiceImpl::Stop() {
   dial_registry_->OnListenerRemoved();
   dial_registry_->UnregisterObserver(this);
   dial_registry_ = nullptr;
+  MediaSinkServiceBase::StopTimer();
 }
 
 DeviceDescriptionService* DialMediaSinkServiceImpl::GetDescriptionService() {
@@ -61,12 +64,8 @@ DeviceDescriptionService* DialMediaSinkServiceImpl::GetDescriptionService() {
 
 void DialMediaSinkServiceImpl::SetDialRegistryForTest(
     DialRegistry* dial_registry) {
-  DCHECK(!dial_registry_);
-  dial_registry_ = dial_registry;
-
-  DCHECK(dial_registry);
-  dial_registry_->RegisterObserver(this);
-  dial_registry_->OnListenerAdded();
+  DCHECK(!test_dial_registry_);
+  test_dial_registry_ = dial_registry;
 }
 
 void DialMediaSinkServiceImpl::SetDescriptionServiceForTest(
@@ -80,11 +79,6 @@ void DialMediaSinkServiceImpl::OnDialDeviceEvent(
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
   DVLOG(2) << "DialMediaSinkServiceImpl::OnDialDeviceEvent found "
            << devices.size() << " devices";
-
-  // Timer starts in |OnDialDeviceEvent()|, and expires 3 seconds later. If
-  // |OnDeviceDescriptionAvailable()| is called after |finish_timer_| expires,
-  // |finish_timer_| is restarted.
-  StartTimer();
 
   current_sinks_.clear();
   current_devices_ = devices;
@@ -126,8 +120,7 @@ void DialMediaSinkServiceImpl::OnDeviceDescriptionAvailable(
 
   // Start fetch timer again if device description comes back after
   // |finish_timer_| fires.
-  if (!finish_timer_->IsRunning())
-    StartTimer();
+  MediaSinkServiceBase::RestartTimer();
 }
 
 void DialMediaSinkServiceImpl::OnDeviceDescriptionError(
--- chrome/browser/media/router/discovery/media_sink_service_base.cc
+++ chrome/browser/media/router/discovery/media_sink_service_base.cc
@@ -52,4 +52,14 @@ void MediaSinkServiceBase::StartTimer() {
                                   base::Unretained(this)));
 }
 
+void MediaSinkServiceBase::StopTimer() {
+  finish_timer_.reset();
+}
+
+void MediaSinkServiceBase::RestartTimer() {
+  if (!finish_timer_ || finish_timer_->IsRunning())
+    return;
+  StartTimer();
+}
+
 }  // namespace media_router
