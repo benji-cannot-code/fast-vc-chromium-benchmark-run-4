3ef3d9db607e6b70f546868c7331890c72347f83
luoe <luoe@chromium.org>
2017-05-26 21:55:58 -0700

    DevTools: migrate EventListenersSidebarPane to shadow
    BUG=726972
    
    Review-Url: https://codereview.chromium.org/2902553002
    Cr-Commit-Position: refs/heads/master@{#475236}

--- third_party/WebKit/Source/devtools/front_end/sources/EventListenerBreakpointsSidebarPane.js
+++ third_party/WebKit/Source/devtools/front_end/sources/EventListenerBreakpointsSidebarPane.js
@@ -7,14 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 Sources.EventListenerBreakpointsSidebarPane = class extends UI.VBox {
   constructor() {
-    super();
+    super(true);
     this.registerRequiredCSS('components/breakpointsList.css');
 
     this._categoriesTreeOutline = new UI.TreeOutlineInShadow();
     this._categoriesTreeOutline.element.tabIndex = 0;
-    this._categoriesTreeOutline.element.classList.add('event-listener-breakpoints');
     this._categoriesTreeOutline.registerRequiredCSS('sources/eventListenerBreakpoints.css');
-    this.element.appendChild(this._categoriesTreeOutline.element);
+    this.contentElement.appendChild(this._categoriesTreeOutline.element);
 
     /** @type {!Map<string, !Sources.EventListenerBreakpointsSidebarPane.Item>} */
     this._categories = new Map();
