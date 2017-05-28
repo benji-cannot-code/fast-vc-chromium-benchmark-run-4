541baf176d5e969a32857d42e61d50e072fd63dd
Chris Watkins <watk@chromium.org>
2017-05-28 09:01:12 -0700

    media: Set texture parameters in SurfaceTextureGLOwner::Create
    Previously we separately created a SurfaceTextureGLOwner and set
    the parameters on its texture. Now they're both done together
    in Create().
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I5ab8a5fa4995c5e784172565377ceaa9cef885d0
    Reviewed-on: https://chromium-review.googlesource.com/517378
    Commit-Queue: Chris Watkins <watk@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#475265}

--- media/gpu/surface_texture_gl_owner.cc
+++ media/gpu/surface_texture_gl_owner.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/synchronization/waitable_event.h"
+#include "ui/gl/scoped_binders.h"
 #include "ui/gl/scoped_make_current.h"
 
 namespace media {
@@ -34,6 +35,15 @@ scoped_refptr<SurfaceTextureGLOwner> SurfaceTextureGLOwner::Create() {
   glGenTextures(1, &texture_id);
   if (!texture_id)
     return nullptr;
+
+  // Set the parameters on the texture.
+  gl::ScopedActiveTexture active_texture(GL_TEXTURE0);
+  gl::ScopedTextureBinder texture_binder(GL_TEXTURE_EXTERNAL_OES, texture_id);
+  glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  DCHECK_EQ(static_cast<GLenum>(GL_NO_ERROR), glGetError());
 
   return new SurfaceTextureGLOwner(texture_id);
 }
