d4b708e4b086232f0da0ac541a54bbb321cb7bb2
bugsnash <bugsnash@chromium.org>
2017-05-28 19:26:30 -0700

    Replaced usage of RefPtr::Release with std::move wraps in Source/modules
    Replaces many of the calls to RefPtr::Release with a std::move wrap,
    which returns a RefPtr instead of a PassRefPtr. As the code currently
    stands, most methods still expect a PassRefPtr and so the RefPtr is
    implicitly converted to a PassRefPtr.
    
    Future patches will change the types passed from PassRefPtr to RefPtr.
    
    Does not handle RefPtr::Release calls in return statements that
    default to move. This will be handled in a future patch.
    
    BUG=494719
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2906483005
    Cr-Commit-Position: refs/heads/master@{#475278}

--- third_party/WebKit/Source/modules/canvas2d/BaseRenderingContext2D.cpp
+++ third_party/WebKit/Source/modules/canvas2d/BaseRenderingContext2D.cpp
@@ -1460,7 +1460,7 @@ CanvasPattern* BaseRenderingContext2D::createPattern(
   bool origin_clean =
       !WouldTaintOrigin(image_source, ExecutionContext::From(script_state));
 
-  return CanvasPattern::Create(image_for_rendering.Release(), repeat_mode,
+  return CanvasPattern::Create(std::move(image_for_rendering), repeat_mode,
                                origin_clean);
 }
 
--- third_party/WebKit/Source/modules/webaudio/AudioNode.cpp
+++ third_party/WebKit/Source/modules/webaudio/AudioNode.cpp
@@ -551,9 +551,10 @@ void AudioNode::Dispose() {
 #endif
   BaseAudioContext::AutoLocker locker(context());
   Handler().Dispose();
-  if (context()->ContextState() == BaseAudioContext::kRunning)
+  if (context()->ContextState() == BaseAudioContext::kRunning) {
     context()->GetDeferredTaskHandler().AddRenderingOrphanHandler(
-        handler_.Release());
+        std::move(handler_));
+  }
 }
 
 void AudioNode::SetHandler(PassRefPtr<AudioHandler> handler) {
