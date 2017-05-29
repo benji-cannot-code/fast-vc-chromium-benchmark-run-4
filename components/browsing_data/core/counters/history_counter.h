dc943b3097aa9f2cffb93a0f821bfcb976dff8ba
Christian Dullweber <dullweber@chromium.org>
2017-05-29 14:24:52 +0200

    Replace history and password statistics with counters
    Remove history and password related code from 
    PasswordStatisticsAggregator replace it with BrowsingDataCounters.
    
    Bug: 716267
    Change-Id: I35b3f4e312732306dcb4ff2a9906976f3f62223e
    Reviewed-on: https://chromium-review.googlesource.com/503232
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#475860}

--- components/browsing_data/core/counters/history_counter.h
+++ components/browsing_data/core/counters/history_counter.h
@@ -59,6 +59,8 @@ class HistoryCounter : public browsing_data::BrowsingDataCounter,
   void OnWebHistoryTimeout();
   void MergeResults();
 
+  history::WebHistoryService* GetWebHistoryService();
+
   // SyncServiceObserver implementation.
   void OnStateChanged(syncer::SyncService* sync) override;
 
