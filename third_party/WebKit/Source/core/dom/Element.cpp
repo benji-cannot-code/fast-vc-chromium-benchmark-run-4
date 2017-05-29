20553b26ffff5412b6131bf8a2e4c56a215b18c1
nainar <nainar@chromium.org>
2017-05-29 02:07:31 -0700

    Disable style sharing outside tests.
    This disables style sharing for most users in Canary, but leaves the code and
    leaves it enabled in the layout tests. Once this bakes for a little while we
    can then remove the code and update the tests.
    
    Per the style-dev@ discussion and design doc:
    https://groups.google.com/a/chromium.org/forum/#!topic/style-dev/8BV0QYubjos
    
    Pateched in from esprehn's patch: http://crrev.com/2858423002
    
    BUG=721517
    
    Review-Url: https://codereview.chromium.org/2887593002
    Cr-Commit-Position: refs/heads/master@{#475317}

--- third_party/WebKit/Source/core/dom/Element.cpp
+++ third_party/WebKit/Source/core/dom/Element.cpp
@@ -4247,6 +4247,8 @@ void Element::AddPropertyToPresentationAttributeStyle(
 }
 
 bool Element::SupportsStyleSharing() const {
+  if (!RuntimeEnabledFeatures::styleSharingEnabled())
+    return false;
   if (!IsStyledElement() || !ParentOrShadowHostElement())
     return false;
   // If the element has inline style it is probably unique.
