ca98ccac1efa697c4da088c54b773bb093cd7b8b
Scott Violet <sky@chromium.org>
2017-05-29 09:05:23 -0700

    chromeos: makes window_finder honor transform set on window
    BUG=726865
    TEST=covered by tests
    
    Change-Id: I37b6fa81a870fc1e38904cdddffb9c194e6ff6be
    Reviewed-on: https://chromium-review.googlesource.com/517240
    Reviewed-by: Ria Jiang <riajiang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#475376}

--- services/ui/ws/window_finder.cc
+++ services/ui/ws/window_finder.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/ui/ws/server_window_delegate.h"
 #include "services/ui/ws/window_coordinate_conversions.h"
 #include "ui/gfx/geometry/point.h"
+#include "ui/gfx/geometry/point3_f.h"
 #include "ui/gfx/geometry/point_f.h"
 #include "ui/gfx/transform.h"
 
@@ -33,6 +34,23 @@ bool IsLocationInNonclientArea(const ServerWindow* target,
   }
 
   return true;
+}
+
+gfx::Point ConvertPointFromParentToChild(const ServerWindow* child,
+                                         const gfx::Point& location_in_parent) {
+  if (child->transform().IsIdentity()) {
+    return gfx::Point(location_in_parent.x() - child->bounds().x(),
+                      location_in_parent.y() - child->bounds().y());
+  }
+
+  gfx::Transform transform = child->transform();
+  gfx::Transform translation;
+  translation.Translate(static_cast<float>(child->bounds().x()),
+                        static_cast<float>(child->bounds().y()));
+  transform.ConcatTransform(translation);
+  gfx::Point3F location_in_child3(gfx::PointF{location_in_parent});
+  transform.TransformPointReverse(&location_in_child3);
+  return gfx::ToFlooredPoint(location_in_child3.AsPointF());
 }
 
 bool FindDeepestVisibleWindowForEventsImpl(ServerWindow* window,
@@ -60,9 +78,8 @@ bool FindDeepestVisibleWindowForEventsImpl(ServerWindow* window,
       if (!child->visible())
         continue;
 
-      // TODO(sky): support transform.
-      gfx::Point location_in_child(location.x() - child->bounds().x(),
-                                   location.y() - child->bounds().y());
+      gfx::Point location_in_child =
+          ConvertPointFromParentToChild(child, location);
       gfx::Rect child_bounds(child->bounds().size());
       child_bounds.Inset(-child->extended_hit_test_region().left(),
                          -child->extended_hit_test_region().top(),
