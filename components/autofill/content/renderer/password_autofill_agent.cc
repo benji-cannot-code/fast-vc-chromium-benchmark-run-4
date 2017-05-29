a30db781dde41f3bd32fdb64f0046820d84d551f
pkalinnikov <pkalinnikov@chromium.org>
2017-05-29 03:13:04 -0700

    Autofill username when the user interacts with the password field.
    Previously, when the user clicked on a password field, only this field
    was autocompleted with their password. This CL ensures that the username
    field is autocompleted as well.
    
    If the user interacts with a password field then the username is filled
    only if it was empty or *autofilled*, i.e. the user-supplied data is
    never overridden.
    
    Note that this works only if the username field was found.
    
    BUG=710374,708605,708602
    
    Review-Url: https://codereview.chromium.org/2902113004
    Cr-Commit-Position: refs/heads/master@{#475329}

--- components/autofill/content/renderer/password_autofill_agent.cc
+++ components/autofill/content/renderer/password_autofill_agent.cc
@@ -282,6 +282,20 @@ bool IsElementAutocompletable(const blink::WebInputElement& element) {
   return IsElementEditable(element);
 }
 
+// Returns whether the |username_element| is allowed to be autofilled.
+//
+// Note that if the user interacts with the |password_field| and the
+// |username_element| is user-defined (i.e., non-empty and non-autofilled), then
+// this function returns false. This is a precaution, to not override the field
+// if it has been classified as username by accident.
+bool IsUsernameAmendable(const blink::WebInputElement& username_element,
+                         bool is_password_field_selected) {
+  return !username_element.IsNull() &&
+         IsElementAutocompletable(username_element) &&
+         (!is_password_field_selected || username_element.IsAutofilled() ||
+          username_element.Value().IsEmpty());
+}
+
 // Return true if either password_value or new_password_value is not empty and
 // not default.
 bool FormContainsNonDefaultPasswordValue(const PasswordForm& password_form) {
@@ -774,8 +788,9 @@ bool PasswordAutofillAgent::FillSuggestion(
   if (element->IsPasswordField()) {
     password_info->password_field_suggestion_was_accepted = true;
     password_info->password_field = password_element;
-  } else if (!username_element.IsNull() &&
-             IsElementAutocompletable(username_element)) {
+  }
+
+  if (IsUsernameAmendable(username_element, element->IsPasswordField())) {
     username_element.SetAutofillValue(blink::WebString::FromUTF16(username));
     username_element.SetAutofilled(true);
     UpdateFieldValueAndPropertiesMaskMap(username_element, &username,
@@ -815,8 +830,7 @@ bool PasswordAutofillAgent::PreviewSuggestion(
     return false;
   }
 
-  if (!element->IsPasswordField() && !username_element.IsNull() &&
-      IsElementAutocompletable(username_element)) {
+  if (IsUsernameAmendable(username_element, element->IsPasswordField())) {
     if (username_query_prefix_.empty())
       username_query_prefix_ = username_element.Value().Utf16();
 
@@ -844,8 +858,9 @@ bool PasswordAutofillAgent::DidClearAutofillSelection(
   PasswordInfo* password_info;
 
   if (!FindPasswordInfoForElement(*element, &username_element,
-                                  &password_element, &password_info))
+                                  &password_element, &password_info)) {
     return false;
+  }
 
   ClearPreview(&username_element, &password_element);
   return true;
