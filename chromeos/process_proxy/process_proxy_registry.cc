d3af48b3a1fe6b3fefdd6122c0984c278dc4e359
gab <gab@chromium.org>
2017-05-30 13:59:48 -0700

    Deprecate NonThreadSafe in chromeos/process_proxy in favor of SequenceChecker.
    Note to crash team: This CL is a refactor and has no intended behavior change.
    
    This change was scripted by https://crbug.com/676387#c8.
    
    Note-worthy for the reviewer:
     * SequenceChecker enforces thread-safety but not thread-affinity!
       If the classes that were updated are thread-affine (use thread local
       storage or a third-party API that does) they should be migrated to
       ThreadChecker instead.
     * ~NonThreadSafe() used to implcitly check in its destructor
       ~Sequence/ThreadChecker() doesn't by design. To keep this CL a
       no-op, an explicit check was added to the destructor of migrated
       classes.
     * NonThreadSafe used to provide access to subclasses, as such
       the |sequence_checker_| member was made protected rather than
       private where necessary.
    
    BUG=676387
    This CL was uploaded by git cl split.
    
    R=tbarzic@chromium.org
    
    Review-Url: https://codereview.chromium.org/2909113003
    Cr-Commit-Position: refs/heads/master@{#475668}

--- chromeos/process_proxy/process_proxy_registry.cc
+++ chromeos/process_proxy/process_proxy_registry.cc
@@ -51,7 +51,7 @@ ProcessProxyRegistry::ProcessProxyRegistry() {
 ProcessProxyRegistry::~ProcessProxyRegistry() {
   // TODO(tbarzic): Fix issue with ProcessProxyRegistry being destroyed
   // on a different thread (it's a LazyInstance).
-  DetachFromThread();
+  // DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   ShutDown();
 }
@@ -74,7 +74,7 @@ ProcessProxyRegistry* ProcessProxyRegistry::Get() {
 
 int ProcessProxyRegistry::OpenProcess(const std::string& command,
                                       const OutputCallback& output_callback) {
-  DCHECK(CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   if (!EnsureWatcherThreadStarted())
     return -1;
@@ -105,7 +105,7 @@ int ProcessProxyRegistry::OpenProcess(const std::string& command,
 }
 
 bool ProcessProxyRegistry::SendInput(int id, const std::string& data) {
-  DCHECK(CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   std::map<int, ProcessProxyInfo>::iterator it = proxy_map_.find(id);
   if (it == proxy_map_.end())
@@ -114,7 +114,7 @@ bool ProcessProxyRegistry::SendInput(int id, const std::string& data) {
 }
 
 bool ProcessProxyRegistry::CloseProcess(int id) {
-  DCHECK(CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   std::map<int, ProcessProxyInfo>::iterator it = proxy_map_.find(id);
   if (it == proxy_map_.end())
@@ -126,7 +126,7 @@ bool ProcessProxyRegistry::CloseProcess(int id) {
 }
 
 bool ProcessProxyRegistry::OnTerminalResize(int id, int width, int height) {
-  DCHECK(CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   std::map<int, ProcessProxyInfo>::iterator it = proxy_map_.find(id);
   if (it == proxy_map_.end())
@@ -136,7 +136,7 @@ bool ProcessProxyRegistry::OnTerminalResize(int id, int width, int height) {
 }
 
 void ProcessProxyRegistry::AckOutput(int id) {
-  DCHECK(CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   std::map<int, ProcessProxyInfo>::iterator it = proxy_map_.find(id);
   if (it == proxy_map_.end())
@@ -148,7 +148,7 @@ void ProcessProxyRegistry::AckOutput(int id) {
 void ProcessProxyRegistry::OnProcessOutput(int id,
                                            ProcessOutputType type,
                                            const std::string& data) {
-  DCHECK(CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   const char* type_str = ProcessOutputTypeToString(type);
   DCHECK(type_str);
