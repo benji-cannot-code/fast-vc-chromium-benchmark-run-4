2073b542b8f72529887c52c7a3feacd54cf35d38
shend <shend@chromium.org>
2017-05-30 02:35:23 -0700

    Make PageSizeType an enum class.
    This patch makes PageSizeType an enum class for better type safety.
    
    BUG=684966
    
    Review-Url: https://codereview.chromium.org/2908933002
    Cr-Commit-Position: refs/heads/master@{#475479}

--- third_party/WebKit/Source/web/WebLocalFrameImpl.cpp
+++ third_party/WebKit/Source/web/WebLocalFrameImpl.cpp
@@ -1451,7 +1451,7 @@ bool WebLocalFrameImpl::HasCustomPageSizeStyle(int page_index) {
   return GetFrame()
              ->GetDocument()
              ->StyleForPage(page_index)
-             ->GetPageSizeType() != PAGE_SIZE_AUTO;
+             ->GetPageSizeType() != PageSizeType::kAuto;
 }
 
 bool WebLocalFrameImpl::IsPageBoxVisible(int page_index) {
