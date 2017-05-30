c598e86b60fe89735435c4c9369d9585347d84b0
spqchan <spqchan@chromium.org>
2017-05-30 10:20:12 -0700

    [Mac] Hide the page info when navigating
    Bug: 696136
    Change-Id: I2f6e51444eeafe70b491f560f48cf47e524822dd
    Reviewed-on: https://chromium-review.googlesource.com/517420
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Sarah Chan <spqchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#475675}

--- chrome/browser/ui/cocoa/page_info/page_info_bubble_controller.mm
+++ chrome/browser/ui/cocoa/page_info/page_info_bubble_controller.mm
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/theme_resources.h"
 #include "components/strings/grit/components_chromium_strings.h"
 #include "components/strings/grit/components_strings.h"
+#include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/page_navigator.h"
 #include "content/public/browser/ssl_host_state_delegate.h"
 #include "content/public/browser/web_contents.h"
@@ -1336,4 +1337,14 @@ void PageInfoUIBridge::SetPermissionInfo(
     ChosenObjectInfoList chosen_object_info_list) {
   [bubble_controller_ setPermissionInfo:permission_info_list
                        andChosenObjects:std::move(chosen_object_info_list)];
+}
+
+void PageInfoUIBridge::DidFinishNavigation(
+    content::NavigationHandle* navigation_handle) {
+  if (!navigation_handle->IsInMainFrame() ||
+      !navigation_handle->HasCommitted()) {
+    return;
+  }
+  // If the browser navigates to another page, close the bubble.
+  [bubble_controller_ close];
 }
