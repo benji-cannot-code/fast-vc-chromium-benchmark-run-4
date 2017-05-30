6cfa0d8de3686a405fb0eac8d68966a3ca090fa7
yosin <yosin@chromium.org>
2017-05-30 02:11:47 -0700

    Get rid of redundant include directives from TextIterator.cpp
    This patch gets rid of redundant include directives from "TextIterator.cpp"
    for improving code health.
    
    BUG=n/a
    TEST=n/a; no behavior changes
    
    Review-Url: https://codereview.chromium.org/2909263002
    Cr-Commit-Position: refs/heads/master@{#475476}

--- third_party/WebKit/Source/core/editing/iterators/TextIterator.cpp
+++ third_party/WebKit/Source/core/editing/iterators/TextIterator.cpp
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/iterators/TextIterator.h"
 
 #include <unicode/utf16.h>
-#include "bindings/core/v8/ExceptionState.h"
+//#include "bindings/core/v8/ExceptionState.h"
 #include "core/HTMLNames.h"
 #include "core/InputTypeNames.h"
 #include "core/dom/Document.h"
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/Position.h"
 #include "core/editing/VisiblePosition.h"
 #include "core/editing/VisibleUnits.h"
-#include "core/editing/iterators/CharacterIterator.h"
 #include "core/frame/LocalFrameView.h"
 #include "core/frame/UseCounter.h"
 #include "core/html/HTMLElement.h"
@@ -49,7 +48,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/LayoutTableCell.h"
 #include "core/layout/LayoutTableRow.h"
 #include "platform/fonts/Font.h"
-#include "platform/wtf/text/CString.h"
 #include "platform/wtf/text/StringBuilder.h"
 
 namespace blink {
