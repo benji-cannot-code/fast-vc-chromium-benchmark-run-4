83dee9baead2f0b6baf49ec1d446094529c8061f
jiameng <jiameng@chromium.org>
2017-05-29 21:24:05 -0700

    Implements CSSPropertyAPI for the animation-name property.
    This patch:
    - Adds CSSPropertyAPIAnimationName.cpp with parseSingleValue method.
    - Moves parsing logic for animation-name from CSSPropertyParser.cpp to
      CSSPropertyAPIAnimationName::parseSingleValue method.
    - Moves shared function ConsumeCommaSeparatedList to
      CSSPropertyAnimationNameUtils.h
    - Modifies BUILD and json5 files.
    
    Diff: https://gist.github.com/jm318/8998e767a695314c7e1ee3e0c809d2d0/revisions
    Review-Url: https://codereview.chromium.org/2896543002
    Cr-Commit-Position: refs/heads/master@{#475423}

---
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyAPIAnimationName.cpp
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/properties/CSSPropertyAPIAnimationName.h"
+
+#include "core/css/parser/CSSParserLocalContext.h"
+#include "core/css/parser/CSSPropertyParserHelpers.h"
+#include "core/css/properties/CSSPropertyAnimationNameUtils.h"
+
+class CSSParserContext;
+
+namespace blink {
+
+const CSSValue* CSSPropertyAPIAnimationName::parseSingleValue(
+    CSSParserTokenRange& range,
+    const CSSParserContext& context,
+    const CSSParserLocalContext& local_context) {
+  // Allow quoted name if this is an alias property.
+  return CSSPropertyParserHelpers::ConsumeCommaSeparatedList(
+      CSSPropertyAnimationNameUtils::ConsumeAnimationName, range, &context,
+      local_context.GetUseAliasParsing());
+}
+
+}  // namespace blink
