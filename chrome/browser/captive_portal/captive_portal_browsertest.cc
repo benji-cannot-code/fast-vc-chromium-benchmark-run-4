472a73b5e18f004ee4ef9c71454f5ee213c8cf67
gab <gab@chromium.org>
2017-05-30 09:32:55 -0700

    Deprecate NonThreadSafe in chrome/browser/captive_portal in favor of SequenceChecker.
    Note to crash team: This CL is a refactor and has no intended behavior change.
    
    This change was scripted by https://crbug.com/676387#c8.
    
    Note-worthy for the reviewer:
     * SequenceChecker enforces thread-safety but not thread-affinity!
       If the classes that were updated are thread-affine (use thread local
       storage or a third-party API that does) they should be migrated to
       ThreadChecker instead.
     * ~NonThreadSafe() used to implcitly check in its destructor
       ~Sequence/ThreadChecker() doesn't by design. To keep this CL a
       no-op, an explicit check was added to the destructor of migrated
       classes.
     * NonThreadSafe used to provide access to subclasses, as such
       the |sequence_checker_| member was made protected rather than
       private where necessary.
    
    BUG=676387
    This CL was uploaded by git cl split.
    
    R=mmenke@chromium.org
    
    Review-Url: https://codereview.chromium.org/2909943002
    Cr-Commit-Position: refs/heads/master@{#475552}

--- chrome/browser/captive_portal/captive_portal_browsertest.cc
+++ chrome/browser/captive_portal/captive_portal_browsertest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/message_loop/message_loop.h"
 #include "base/path_service.h"
+#include "base/sequence_checker.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task_scheduler/post_task.h"
 #include "base/values.h"
@@ -134,8 +135,7 @@ const char* const kInternetConnectedTitle = "Title Of Awesomeness";
 // A URL request job that hangs until FailJobs() is called.  Started jobs
 // are stored in a static class variable containing a linked list so that
 // FailJobs() can locate them.
-class URLRequestTimeoutOnDemandJob : public net::URLRequestJob,
-                                     public base::NonThreadSafe {
+class URLRequestTimeoutOnDemandJob : public net::URLRequestJob {
  public:
   // net::URLRequestJob:
   void Start() override;
@@ -212,6 +212,8 @@ class URLRequestTimeoutOnDemandJob : public net::URLRequestJob,
   // The next job that had been started but not yet timed out.
   URLRequestTimeoutOnDemandJob* next_job_;
 
+  SEQUENCE_CHECKER(sequence_checker_);
+
   DISALLOW_COPY_AND_ASSIGN(URLRequestTimeoutOnDemandJob);
 };
 
@@ -221,7 +223,7 @@ int URLRequestTimeoutOnDemandJob::num_jobs_started_ = 0;
 URLRequestTimeoutOnDemandJob* URLRequestTimeoutOnDemandJob::job_list_ = NULL;
 
 void URLRequestTimeoutOnDemandJob::Start() {
-  EXPECT_TRUE(CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   // Insert at start of the list.
   next_job_ = job_list_;
@@ -277,6 +279,7 @@ URLRequestTimeoutOnDemandJob::URLRequestTimeoutOnDemandJob(
 }
 
 URLRequestTimeoutOnDemandJob::~URLRequestTimeoutOnDemandJob() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   // All hanging jobs should have failed or been abandoned before being
   // destroyed.
   EXPECT_FALSE(RemoveFromList());
