b1e24ac5e9e2b1e164eaa3f703ac75233745d845
eugenebut <eugenebut@chromium.org>
2017-05-30 16:54:28 -0700

    Made renderer-initiated requests registration more reliable.
    Old code relied on URLs comparisson which may give false negatives
    because WKWebView allows multiple pending navigations.
    
    This CL uses NavigationContextImpl to check if the load has been
    registered previously which should always work as long as
    didStartProvisionalNavigation: is guaranteed to be called.
    
    If didStartProvisionalNavigation: is always called, then it should fix
    crbug.com/725958 crash and if it's not, then didFinishNavigation: will
    have to register loading request as well (which will be done in a
    separate CL if needed).
    
    BUG=727289
    
    Review-Url: https://codereview.chromium.org/2901293003
    Cr-Commit-Position: refs/heads/master@{#475698}

--- ios/web/navigation/navigation_manager_util.mm
+++ ios/web/navigation/navigation_manager_util.mm
@@ -14,6 +14,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace web {
 
+NavigationItem* GetItemWithUniqueID(NavigationManager* navigation_manager,
+                                    int unique_id) {
+  NavigationItem* transient_item = navigation_manager->GetTransientItem();
+  if (transient_item && transient_item->GetUniqueID() == unique_id)
+    return transient_item;
+
+  NavigationItem* pending_item = navigation_manager->GetPendingItem();
+  if (pending_item && pending_item->GetUniqueID() == unique_id)
+    return pending_item;
+
+  return GetCommittedItemWithUniqueID(navigation_manager, unique_id);
+}
+
 NavigationItem* GetCommittedItemWithUniqueID(
     NavigationManager* navigation_manager,
     int unique_id) {
