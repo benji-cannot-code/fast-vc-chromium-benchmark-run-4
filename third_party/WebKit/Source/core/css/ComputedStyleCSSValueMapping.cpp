bb2b0620ad2f1474f6278923f70a20fc90b70672
nainar <nainar@chromium.org>
2017-05-30 04:29:29 -0700

    Generate enum/getters/setters/mappings for text-align-last.
    Currently, the text-align-last property is a 'storage_only' field,
    so it has no generated public getters/setters. This patch changes it to
    a 'keyword' field so that its getters/setters can be generated as well.
    We also generate the ETextAlignLast enum and use the generated
    CSSValueID <-> ETextAlignLast mappings.
    
    Diff: https://gist.github.com/7ebcc52951f3abdb9e1fd8f23bdb3e71/revisions
    
    BUG=628043
    
    Review-Url: https://codereview.chromium.org/2915543002
    Cr-Commit-Position: refs/heads/master@{#475493}

--- third_party/WebKit/Source/core/css/ComputedStyleCSSValueMapping.cpp
+++ third_party/WebKit/Source/core/css/ComputedStyleCSSValueMapping.cpp
@@ -2824,7 +2824,7 @@ const CSSValue* ComputedStyleCSSValueMapping::Get(
     case CSSPropertyTextAlign:
       return CSSIdentifierValue::Create(style.GetTextAlign());
     case CSSPropertyTextAlignLast:
-      return CSSIdentifierValue::Create(style.GetTextAlignLast());
+      return CSSIdentifierValue::Create(style.TextAlignLast());
     case CSSPropertyTextDecoration:
       if (RuntimeEnabledFeatures::css3TextDecorationsEnabled())
         return ValuesForShorthandProperty(textDecorationShorthand(), style,
