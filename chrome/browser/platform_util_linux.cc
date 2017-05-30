e484816bee4dd06f6c5f8bdff46bdd1677f50ddc
fdoray <fdoray@chromium.org>
2017-05-30 11:00:11 -0700

    Use TaskScheduler instead of blocking pool in platform_util_linux.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=jochen@chromium.org
    
    Review-Url: https://codereview.chromium.org/2674873003
    Cr-Commit-Position: refs/heads/master@{#475593}

--- chrome/browser/platform_util_linux.cc
+++ chrome/browser/platform_util_linux.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/launch.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/threading/sequenced_worker_pool.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/version.h"
 #include "chrome/browser/platform_util_internal.h"
 #include "content/public/browser/browser_thread.h"
@@ -161,10 +161,12 @@ void PlatformOpenVerifiedItem(const base::FilePath& path, OpenItemType type) {
 
 void ShowItemInFolder(Profile* profile, const base::FilePath& full_path) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  base::PostTaskAndReplyWithResult(content::BrowserThread::GetBlockingPool(),
-                                   FROM_HERE,
-                                   base::Bind(&CheckNautilusIsDefault),
-                                   base::Bind(&ShowItem, profile, full_path));
+  base::PostTaskWithTraitsAndReplyWithResult(
+      FROM_HERE,
+      {base::WithBaseSyncPrimitives(), base::MayBlock(),
+       base::TaskPriority::USER_BLOCKING},
+      base::BindOnce(&CheckNautilusIsDefault),
+      base::BindOnce(&ShowItem, profile, full_path));
 }
 
 void OpenExternal(Profile* profile, const GURL& url) {
