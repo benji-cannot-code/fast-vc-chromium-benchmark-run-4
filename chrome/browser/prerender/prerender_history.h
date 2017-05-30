f355ec6345050c5ad3eb3e1ec56b7b55026a4140
gab <gab@chromium.org>
2017-05-30 09:45:45 -0700

    Replace deprecated base::NonThreadSafe in chrome/browser/prerender in favor of SequenceChecker.
    Note to crash team: This CL is a refactor and has no intended behavior change.
    
    This change was scripted by https://crbug.com/676387#c8.
    
    Note-worthy for the reviewer:
     * SequenceChecker enforces thread-safety but not thread-affinity!
       If the classes that were updated are thread-affine (use thread local
       storage or a third-party API that does) they should be migrated to
       ThreadChecker instead.
     * ~NonThreadSafe() used to implcitly check in its destructor
       ~Sequence/ThreadChecker() doesn't by design. To keep this CL a
       no-op, an explicit check was added to the destructor of migrated
       classes.
     * NonThreadSafe used to provide access to subclasses, as such
       the |sequence_checker_| member was made protected rather than
       private where necessary.
    
    BUG=676387
    This CL was uploaded by git cl split.
    
    R=davidben@chromium.org
    
    Review-Url: https://codereview.chromium.org/2909193002
    Cr-Commit-Position: refs/heads/master@{#475556}

--- chrome/browser/prerender/prerender_history.h
+++ chrome/browser/prerender/prerender_history.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <list>
 
 #include "base/macros.h"
-#include "base/threading/non_thread_safe.h"
+#include "base/sequence_checker.h"
 #include "base/time/time.h"
 #include "chrome/browser/prerender/prerender_final_status.h"
 #include "chrome/browser/prerender/prerender_origin.h"
@@ -26,7 +26,7 @@ namespace prerender {
 // PrerenderHistory maintains a per-session history of prerendered pages
 // and their final dispositions. It has a fixed maximum capacity, and old
 // items in history will be removed when the capacity is reached.
-class PrerenderHistory : public base::NonThreadSafe {
+class PrerenderHistory {
  public:
   // Entry is an individual entry in the history list. It corresponds to a
   // specific prerendered page.
@@ -74,6 +74,8 @@ class PrerenderHistory : public base::NonThreadSafe {
  private:
   std::list<Entry> entries_;
   size_t max_items_;
+
+  SEQUENCE_CHECKER(sequence_checker_);
 
   DISALLOW_COPY_AND_ASSIGN(PrerenderHistory);
 };
