d2ec4e4ed23b7e0e1b5318bc5a5b8dd12fa71ef3
yosin <yosin@chromium.org>
2017-05-29 23:36:36 -0700

    Get rid of unused class WordAwareIterator
    This patch gets rid of unused class |WordAwareIterator| for improving code
    health.
    
    BUG=n/a
    TEST=n/a; no behavior changes
    
    Review-Url: https://codereview.chromium.org/2909003003
    Cr-Commit-Position: refs/heads/master@{#475444}

--- third_party/WebKit/Source/core/editing/iterators/TextIterator.cpp
+++ third_party/WebKit/Source/core/editing/iterators/TextIterator.cpp
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/VisiblePosition.h"
 #include "core/editing/VisibleUnits.h"
 #include "core/editing/iterators/CharacterIterator.h"
-#include "core/editing/iterators/WordAwareIterator.h"
 #include "core/frame/LocalFrameView.h"
 #include "core/frame/UseCounter.h"
 #include "core/html/HTMLElement.h"
