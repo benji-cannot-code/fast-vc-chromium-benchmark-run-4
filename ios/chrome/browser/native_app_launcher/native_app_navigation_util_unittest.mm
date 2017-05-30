309280535ea5ea75ed8ff9c4a5a59ced77ac3130
lindsayw <lindsayw@chromium.org>
2017-05-30 12:13:43 -0700

    [ObjC ARC] Converts ios/chrome/browser/native_app_launcher:unit_tests_internal to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2891133002
    Cr-Commit-Position: refs/heads/master@{#475623}

--- ios/chrome/browser/native_app_launcher/native_app_navigation_util_unittest.mm
+++ ios/chrome/browser/native_app_launcher/native_app_navigation_util_unittest.mm
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/page_transition_types.h"
 #include "url/gurl.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 // Tests the implementation of IsLinkNavigation(). The function being tested
 // uses public NavigationManager interfaces and can be tested by using
 // TestNavigationManager that implements the same interface.
