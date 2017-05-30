14212389ba24775b1d3e153e7b3cee5c431868a3
finnur <finnur@chromium.org>
2017-05-30 02:05:27 -0700

    Photo Picker Dialog: Make sure Back button fires the Cancel action.
    BUG=656015
    
    Review-Url: https://codereview.chromium.org/2908163002
    Cr-Commit-Position: refs/heads/master@{#475473}

--- chrome/android/java/src/org/chromium/chrome/browser/photo_picker/PickerCategoryView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/photo_picker/PickerCategoryView.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.photo_picker;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.Rect;
@@ -191,6 +192,13 @@ public class PickerCategoryView extends RelativeLayout
         mDialog = dialog;
         mMultiSelectionAllowed = multiSelectionAllowed;
         mListener = listener;
+
+        mDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                mListener.onPickerUserAction(PhotoPickerListener.Action.CANCEL, null);
+            }
+        });
     }
 
     // FileEnumWorkerTask.FilesEnumeratedCallback:
