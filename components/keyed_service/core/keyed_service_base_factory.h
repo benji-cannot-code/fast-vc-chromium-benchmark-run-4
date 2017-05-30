2ea6049450be7b79415755d67091f56cba51e8e4
gab <gab@chromium.org>
2017-05-30 08:30:58 -0700

    Replace deprecated base::NonThreadSafe in components/keyed_service in favor of SequenceChecker.
    Note to crash team: This CL is a refactor and has no intended behavior change.
    
    This change was scripted by https://crbug.com/676387#c8.
    
    Note-worthy for the reviewer:
     * SequenceChecker enforces thread-safety but not thread-affinity!
       If the classes that were updated are thread-affine (use thread local
       storage or a third-party API that does) they should be migrated to
       ThreadChecker instead.
     * ~NonThreadSafe() used to implicitly check in its destructor
       ~Sequence/ThreadChecker() doesn't by design. To keep this CL a
       no-op, an explicit check was added to the destructor of migrated
       classes.
     * NonThreadSafe used to provide access to subclasses, as such
       the |sequence_checker_| member was made protected rather than
       private where necessary.
    
    BUG=676387
    This CL was uploaded by git cl split.
    
    R=phajdan.jr@chromium.org
    
    Review-Url: https://codereview.chromium.org/2910053002
    Cr-Commit-Position: refs/heads/master@{#475536}

--- components/keyed_service/core/keyed_service_base_factory.h
+++ components/keyed_service/core/keyed_service_base_factory.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <set>
 
-#include "base/threading/non_thread_safe.h"
+#include "base/sequence_checker.h"
 #include "components/keyed_service/core/dependency_node.h"
 #include "components/keyed_service/core/keyed_service_export.h"
 
@@ -30,8 +30,7 @@ class PrefRegistrySyncable;
 // This object describes general dependency management between factories while
 // direct subclasses react to lifecycle events and implement memory management.
 class KEYED_SERVICE_EXPORT KeyedServiceBaseFactory
-    : public base::NonThreadSafe,
-      NON_EXPORTED_BASE(public DependencyNode) {
+    : NON_EXPORTED_BASE(public DependencyNode) {
  public:
 #ifndef NDEBUG
   // Returns our name. We don't keep track of this in release mode.
@@ -112,6 +111,8 @@ class KEYED_SERVICE_EXPORT KeyedServiceBaseFactory
 
   // Mark context has having preferences registered.
   void MarkPreferencesSetOn(base::SupportsUserData* context);
+
+  SEQUENCE_CHECKER(sequence_checker_);
 
  private:
   friend class DependencyManager;
