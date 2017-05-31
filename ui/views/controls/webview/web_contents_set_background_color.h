4f9462105ab677383bec0629b6868d764b8418f3
vadimt <vadimt@chromium.org>
2017-05-31 14:38:17 -0700

    Making answer card to behave like other results.
    Now it lives in a search result container, which can be selected,
    highlighted, navigated with keyboard etc.
    
    Opening the result (i.e. what happens upon clicking on it) is not yet
    implemented.
    
    Unit test for the new result container isn't yet implemented since both
    result opening and accessibility behavior are not yet implemented.
    
    Bug=712331
    
    Review-Url: https://codereview.chromium.org/2905523004
    Cr-Original-Commit-Position: refs/heads/master@{#475782}
    Committed: https://chromium.googlesource.com/chromium/src/+/2420a92712ee82be175bea6750774bec8006c5ce
    Review-Url: https://codereview.chromium.org/2905523004
    Cr-Commit-Position: refs/heads/master@{#476039}

--- ui/views/controls/webview/web_contents_set_background_color.h
+++ ui/views/controls/webview/web_contents_set_background_color.h
@@ -3,16 +3,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_CHROMEOS_LOGIN_UI_WEB_CONTENTS_SET_BACKGROUND_COLOR_H_
-#define CHROME_BROWSER_CHROMEOS_LOGIN_UI_WEB_CONTENTS_SET_BACKGROUND_COLOR_H_
+#ifndef UI_VIEWS_CONTROLS_WEBVIEW_WEB_CONTENTS_SET_BACKGROUND_COLOR_H_
+#define UI_VIEWS_CONTROLS_WEBVIEW_WEB_CONTENTS_SET_BACKGROUND_COLOR_H_
 
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
+#include "ui/views/controls/webview/webview_export.h"
 
 // Defined in SkColor.h (32-bit ARGB color).
 using SkColor = unsigned int;
 
-namespace chromeos {
+namespace views {
 
 // Ensures that the background color of a given WebContents instance is always
 // set to a given color value.
@@ -20,8 +21,9 @@ class WebContentsSetBackgroundColor
     : public content::WebContentsObserver,
       public content::WebContentsUserData<WebContentsSetBackgroundColor> {
  public:
-  static void CreateForWebContentsWithColor(content::WebContents* web_contents,
-                                            SkColor color);
+  WEBVIEW_EXPORT static void CreateForWebContentsWithColor(
+      content::WebContents* web_contents,
+      SkColor color);
 
   ~WebContentsSetBackgroundColor() override;
 
@@ -40,6 +42,6 @@ class WebContentsSetBackgroundColor
   DISALLOW_COPY_AND_ASSIGN(WebContentsSetBackgroundColor);
 };
 
-}  // namespace chromeos
+}  // namespace views
 
-#endif  // CHROME_BROWSER_CHROMEOS_LOGIN_UI_WEB_CONTENTS_SET_BACKGROUND_COLOR_H_
+#endif  // UI_VIEWS_CONTROLS_WEBVIEW_WEB_CONTENTS_SET_BACKGROUND_COLOR_H_
