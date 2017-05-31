ad23b01ae4c86a83ac9eea2b23033eba8020847b
yosin <yosin@chromium.org>
2017-05-31 03:06:45 -0700

    Limit visibility {Left,Right}BoundaryOfLine() to SelectionModifier only
    This patch moves |{Left,Right}BoundaryOfLine()| to "SelectionModifier.cpp" as
    file local functions since these functions are used in |SelectionModifier| class
    only, for improving code health.
    
    BUG=707656
    TEST=n/a; no behavior changes
    
    Review-Url: https://codereview.chromium.org/2912303002
    Cr-Commit-Position: refs/heads/master@{#475863}

--- third_party/WebKit/Source/core/editing/SelectionModifier.cpp
+++ third_party/WebKit/Source/core/editing/SelectionModifier.cpp
@@ -38,6 +38,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+namespace {
+
+VisiblePosition LeftBoundaryOfLine(const VisiblePosition& c,
+                                   TextDirection direction) {
+  DCHECK(c.IsValid()) << c;
+  return direction == TextDirection::kLtr ? LogicalStartOfLine(c)
+                                          : LogicalEndOfLine(c);
+}
+
+VisiblePosition RightBoundaryOfLine(const VisiblePosition& c,
+                                    TextDirection direction) {
+  DCHECK(c.IsValid()) << c;
+  return direction == TextDirection::kLtr ? LogicalEndOfLine(c)
+                                          : LogicalStartOfLine(c);
+}
+
+}  // namespace
+
 LayoutUnit NoXPosForVerticalArrowNavigation() {
   return LayoutUnit::Min();
 }
