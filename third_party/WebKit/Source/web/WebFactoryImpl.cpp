583bcbd4b9b41dbcc4df86f03ba1f9a572c1bad5
sashab <sashab@chromium.org>
2017-05-31 15:11:44 -0700

    Made WebFactory::SetInstance private
    Made WebFactory::SetInstance private and only allowed WebFactoryImpl
    to call it, through a new Initialize() method.
    
    BUG=712963
    
    Review-Url: https://codereview.chromium.org/2913613003
    Cr-Commit-Position: refs/heads/master@{#476055}

--- third_party/WebKit/Source/web/WebFactoryImpl.cpp
+++ third_party/WebKit/Source/web/WebFactoryImpl.cpp
@@ -9,6 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+void WebFactoryImpl::Initialize() {
+  WebFactory::SetInstance(*(new WebFactoryImpl()));
+}
+
 ChromeClient* WebFactoryImpl::CreateChromeClient(WebViewBase* view) const {
   return ChromeClientImpl::Create(view);
 }
