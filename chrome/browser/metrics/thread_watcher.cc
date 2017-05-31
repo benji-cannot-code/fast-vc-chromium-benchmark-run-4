e743df371af118965ad2771a1ccfcb7e9202eb0c
Daniel Cheng <dcheng@chromium.org>
2017-05-31 11:42:26 -0700

    Use task cancellation status in base::MessageLoop.
    MessageLoop now sweeps cancelled tasks from the front of the work queue
    before dispatching or scheduling work. Previously, MessageLoop did not
    use the task cancellation status. It often scheduled wakeups for
    cancelled tasks or dispatched cancelled tasks, relying on base::Callback
    to turn it into a no-op.
    
    This was problematic for several reasons:
    - Scheduling wakeups based on cancelled tasks means the wakeup is
      spurious.
    - MessageLoop only processes one non-delayed and one delayed task per
      tick, so having many cancelled tasks results in spinning the message
      loop for awhile to drain them.
    
    Bug: 653394
    Change-Id: I7287d5d430c1791535f7274c56c40f1e3f8669a5
    Reviewed-on: https://chromium-review.googlesource.com/505274
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#476033}

--- chrome/browser/metrics/thread_watcher.cc
+++ chrome/browser/metrics/thread_watcher.cc
@@ -36,20 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using content::BrowserThread;
 
-namespace {
-
-base::StackSamplingProfiler::SamplingParams GetJankTimeBombSamplingParams() {
-  base::StackSamplingProfiler::SamplingParams params;
-  params.initial_delay = base::TimeDelta::FromMilliseconds(0);
-  params.bursts = 1;
-  // 5 seconds at 10Hz.
-  params.samples_per_burst = 50;
-  params.sampling_interval = base::TimeDelta::FromMilliseconds(100);
-  return params;
-}
-
-}  // namespace
-
 // ThreadWatcher methods and members.
 ThreadWatcher::ThreadWatcher(const WatchingParams& params)
     : thread_id_(params.thread_id),
@@ -928,47 +914,6 @@ void StartupTimeBomb::DisarmStartupTimeBomb() {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   if (g_startup_timebomb_)
     g_startup_timebomb_->Disarm();
-}
-
-// JankTimeBomb methods and members.
-//
-JankTimeBomb::JankTimeBomb(base::TimeDelta duration,
-                           metrics::CallStackProfileParams::Thread thread)
-    : thread_(thread), weak_ptr_factory_(this) {
-  if (IsEnabled()) {
-    WatchDogThread::PostDelayedTask(
-        FROM_HERE,
-        base::Bind(&JankTimeBomb::Alarm,
-                   weak_ptr_factory_.GetWeakPtr(),
-                   base::PlatformThread::CurrentId()),
-        duration);
-  }
-}
-
-JankTimeBomb::~JankTimeBomb() {
-}
-
-bool JankTimeBomb::IsEnabled() const {
-  version_info::Channel channel = chrome::GetChannel();
-  return channel == version_info::Channel::UNKNOWN ||
-      channel == version_info::Channel::CANARY ||
-      channel == version_info::Channel::DEV;
-}
-
-void JankTimeBomb::Alarm(base::PlatformThreadId thread_id) {
-  DCHECK(WatchDogThread::CurrentlyOnWatchDogThread());
-  sampling_profiler_.reset(new base::StackSamplingProfiler(
-      thread_id,
-      GetJankTimeBombSamplingParams(),
-      metrics::CallStackProfileMetricsProvider::GetProfilerCallback(
-          metrics::CallStackProfileParams(
-              metrics::CallStackProfileParams::BROWSER_PROCESS,
-              thread_,
-              metrics::CallStackProfileParams::JANKY_TASK,
-              metrics::CallStackProfileParams::PRESERVE_ORDER))));
-  // Use synchronous profiler. It will automatically stop collection when
-  // destroyed.
-  sampling_profiler_->Start();
 }
 
 // ShutdownWatcherHelper methods and members.
