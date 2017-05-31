dd7ded9da9d7cbb017ecb9954eea54bd845c610c
John Budorick <jbudorick@chromium.org>
2017-05-31 10:51:46 -0700

    Disable SelectFileDialogTest.testPhotoPickerLaunchAndMimeTypes.
    TBR=finnur@chromium.org,tedchoc@chromium.org
    
    Bug: 728247
    Change-Id: I2e6be118b9bf8039067fafedf696c6ab75ff7fee
    Reviewed-on: https://chromium-review.googlesource.com/519647
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#475990}

--- chrome/android/javatests/src/org/chromium/chrome/browser/SelectFileDialogTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/SelectFileDialogTest.java
@@ -211,6 +211,7 @@ public class SelectFileDialogTest {
 
     @Test
     @MediumTest
+    @DisabledTest
     public void testPhotoPickerLaunchAndMimeTypes() throws Throwable {
         assertEquals("", SelectFileDialog.ensureMimeType(""));
         assertEquals("image/jpeg", SelectFileDialog.ensureMimeType(".jpg"));
