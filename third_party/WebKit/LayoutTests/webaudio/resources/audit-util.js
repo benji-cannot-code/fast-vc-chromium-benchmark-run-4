c67294f67a4a6fccc1ca193a50aa0d108b4d3411
tyoshino <tyoshino@chromium.org>
2017-05-30 23:05:35 -0700

    Revert of Compute tail time from Biquad coefficients (patchset #7 id:120001 of https://codereview.chromium.org/2862373002/ )
    Reason for revert:
    The added layout tests are failing on "WebKit Linux Trusty MSAN" bot.
    https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux%20Trusty%20MSAN/builds/1242
    
    Original issue's description:
    > Compute tail time from Biquad coefficients
    >
    > Instead of using a fixed tail time of 200 ms for all biquad filters,
    > we compute the actual tail time from filter coefficients.  Many
    > typical filters in applications have a tail time of 10 ms or less.
    > This allows biquads and downstream nodes to stop processing much sooner
    > than what currently happens, reducing CPU usage and allowing GC to
    > collect nodes sooner.
    >
    > BUG=612215
    > TEST=
    >
    > Review-Url: https://codereview.chromium.org/2862373002
    > Cr-Commit-Position: refs/heads/master@{#475632}
    > Committed: https://chromium.googlesource.com/chromium/src/+/0be695dcb495cade0ed5c4b70aba95eb3dce49bd
    
    TBR=hongchan@chromium.org,rtoy@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=612215
    
    Review-Url: https://codereview.chromium.org/2912293002
    Cr-Commit-Position: refs/heads/master@{#475822}

--- third_party/WebKit/LayoutTests/webaudio/resources/audit-util.js
+++ third_party/WebKit/LayoutTests/webaudio/resources/audit-util.js
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *                test.
  */
 
-// How many frames in a WebAudio render quantum.
-let RENDER_QUANTUM_FRAMES = 128;
 
 function writeString(s, a, offset) {
   for (let i = 0; i < s.length; ++i) {
