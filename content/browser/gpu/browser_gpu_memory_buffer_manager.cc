e0dbc20b0ddd2599e65636712c9336f79c456ac1
hajimehoshi <hajimehoshi@chromium.org>
2017-05-30 21:19:33 -0700

    Refactoring: Remove unused BrowserGpuMemoryBufferManager::BufferInfo constructor
    BUG=n/a
    TEST=n/a
    
    Review-Url: https://codereview.chromium.org/2907333002
    Cr-Commit-Position: refs/heads/master@{#475794}

--- content/browser/gpu/browser_gpu_memory_buffer_manager.cc
+++ content/browser/gpu/browser_gpu_memory_buffer_manager.cc
@@ -458,8 +458,6 @@ uint64_t BrowserGpuMemoryBufferManager::ClientIdToTracingProcessId(
       client_id);
 }
 
-BrowserGpuMemoryBufferManager::BufferInfo::BufferInfo() = default;
-
 BrowserGpuMemoryBufferManager::BufferInfo::BufferInfo(
     const gfx::Size& size,
     gfx::GpuMemoryBufferType type,
