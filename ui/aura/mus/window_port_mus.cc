eeb9cba52e5bbd243cc697d7ea4ae58e5be67e86
fsamuel <fsamuel@chromium.org>
2017-05-30 17:58:12 -0700

    Aura-Mus: Fix high-DPI gutter
    Previously, gutter did not take into account high-DPIness and didn't
    convert from DIP to pixels and back correctly. This CL corrects this.
    
    BUG=672962
    
    Review-Url: https://codereview.chromium.org/2873473002
    Cr-Commit-Position: refs/heads/master@{#475712}

--- ui/aura/mus/window_port_mus.cc
+++ ui/aura/mus/window_port_mus.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/class_property.h"
 #include "ui/display/display.h"
 #include "ui/display/screen.h"
+#include "ui/gfx/geometry/dip_util.h"
 
 namespace aura {
 
@@ -231,7 +232,9 @@ void WindowPortMus::SetBoundsFromServer(
   ServerChangeData data;
   data.bounds_in_dip = bounds;
   ScopedServerChange change(this, ServerChangeType::BOUNDS, data);
-  last_surface_size_ = bounds.size();
+  float device_scale_factor = ScaleFactorForDisplay(window_);
+  last_surface_size_in_pixels_ =
+      gfx::ConvertSizeToPixel(device_scale_factor, bounds.size());
   if (local_surface_id)
     local_surface_id_ = *local_surface_id;
   else
@@ -285,12 +288,14 @@ void WindowPortMus::SetFrameSinkIdFromServer(
 }
 
 const cc::LocalSurfaceId& WindowPortMus::GetOrAllocateLocalSurfaceId(
-    const gfx::Size& surface_size) {
-  if (last_surface_size_ == surface_size && local_surface_id_.is_valid())
+    const gfx::Size& surface_size_in_pixels) {
+  if (last_surface_size_in_pixels_ == surface_size_in_pixels &&
+      local_surface_id_.is_valid()) {
     return local_surface_id_;
+  }
 
   local_surface_id_ = local_surface_id_allocator_.GenerateId();
-  last_surface_size_ = surface_size;
+  last_surface_size_in_pixels_ = surface_size_in_pixels;
 
   // If the FrameSinkId is available, then immediately embed the SurfaceId.
   // The newly generated frame by the embedder will block in the display
@@ -534,9 +539,9 @@ void WindowPortMus::UpdatePrimarySurfaceInfo() {
   if (!frame_sink_id_.is_valid() || !local_surface_id_.is_valid())
     return;
 
-  SetPrimarySurfaceInfo(
-      cc::SurfaceInfo(cc::SurfaceId(frame_sink_id_, local_surface_id_),
-                      ScaleFactorForDisplay(window_), last_surface_size_));
+  SetPrimarySurfaceInfo(cc::SurfaceInfo(
+      cc::SurfaceId(frame_sink_id_, local_surface_id_),
+      ScaleFactorForDisplay(window_), last_surface_size_in_pixels_));
 }
 
 void WindowPortMus::UpdateClientSurfaceEmbedder() {
