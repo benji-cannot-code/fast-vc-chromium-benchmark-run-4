95f59368e40a5b3adcb58b1bfc10720b669e64de
dspaid <dspaid@chromium.org>
2017-05-31 16:37:09 -0700

    cros: Prevent shut down on lid-close during migration.
    In the login screen, the system will shut down on lid close.
    Although we ask users not to close the lid during the migration, it
    should be better to change the lid-close action to SUSPEND during
    migration.
    
    BUG=726229
    TEST=out/Release/chromeos_unittests --gtest_filter=PowerPolicyControllerTest.*
    
    Review-Url: https://codereview.chromium.org/2912763003
    Cr-Commit-Position: refs/heads/master@{#476091}

--- chrome/browser/ui/webui/chromeos/login/encryption_migration_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/encryption_migration_screen_handler.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/power_manager/power_supply_properties.pb.h"
 #include "chromeos/dbus/power_manager_client.h"
+#include "chromeos/dbus/power_policy_controller.h"
 #include "components/login/localized_values_builder.h"
 #include "components/user_manager/user_manager.h"
 #include "content/public/browser/browser_thread.h"
@@ -283,11 +284,14 @@ void EncryptionMigrationScreenHandler::UpdateUIState(UIState state) {
   if (state == UIState::READY)
     DBusThreadManager::Get()->GetPowerManagerClient()->RequestStatusUpdate();
 
-  // We should block power save during migration.
-  if (state == UIState::MIGRATING)
+  // We should block power save and not shut down on lid close during migration.
+  if (state == UIState::MIGRATING) {
     StartBlockingPowerSave();
-  else
+    PowerPolicyController::Get()->SetEncryptionMigrationActive(true);
+  } else {
     StopBlockingPowerSave();
+    PowerPolicyController::Get()->SetEncryptionMigrationActive(false);
+  }
 }
 
 void EncryptionMigrationScreenHandler::CheckAvailableStorage() {
