4681bb449cb0e33d7d5f2ddd5daa8fbde5e30949
kristipark <kristipark@chromium.org>
2017-05-30 19:03:02 -0700

    Added a refresh database button on the IndexedDB view.
    BUG=723871
    
    Review-Url: https://codereview.chromium.org/2902673003
    Cr-Commit-Position: refs/heads/master@{#475739}

--- third_party/WebKit/Source/devtools/front_end/resources/IndexedDBViews.js
+++ third_party/WebKit/Source/devtools/front_end/resources/IndexedDBViews.js
@@ -54,6 +54,11 @@ Resources.IDBDatabaseView = class extends UI.VBox {
         Common.UIString('Delete database'), () => this._deleteDatabase(), Common.UIString('Delete database'));
     footer.appendChild(this._clearButton);
 
+    this._refreshButton = UI.createTextButton(
+        Common.UIString('Refresh database'), () => this._refreshDatabaseButtonClicked(),
+        Common.UIString('Refresh database'));
+    footer.appendChild(this._refreshButton);
+
     this.update(database);
   }
 
@@ -62,12 +67,21 @@ Resources.IDBDatabaseView = class extends UI.VBox {
     this._versionElement.textContent = this._database.version;
   }
 
+  _refreshDatabaseButtonClicked() {
+    this._model.refreshDatabase(this._database.databaseId);
+  }
+
   /**
    * @param {!Resources.IndexedDBModel.Database} database
    */
   update(database) {
     this._database = database;
     this._refreshDatabase();
+    this._updatedForTests();
+  }
+
+  _updatedForTests() {
+    // Sniffed in tests.
   }
 
   _deleteDatabase() {
@@ -290,6 +304,7 @@ Resources.IDBDataView = class extends UI.SimpleView {
 
       this._pageBackButton.setEnabled(!!skipCount);
       this._pageForwardButton.setEnabled(hasMore);
+      this._updatedDataForTests();
     }
 
     var idbKeyRange = key ? window.IDBKeyRange.lowerBound(key) : null;
@@ -301,6 +316,10 @@ Resources.IDBDataView = class extends UI.SimpleView {
       this._model.loadObjectStoreData(
           this._databaseId, this._objectStore.name, idbKeyRange, skipCount, pageSize, callback.bind(this));
     }
+  }
+
+  _updatedDataForTests() {
+    // Sniffed in tests.
   }
 
   /**
