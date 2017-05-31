5d7b6a68de131f8a003f56a0795a8bb190fd965f
xiaochengh <xiaochengh@chromium.org>
2017-05-31 01:20:26 -0700

    Remove a leftover include from TextIterator.cpp
    BUG=n/a
    TEST=n/a; no behavioral change
    
    Review-Url: https://codereview.chromium.org/2913953002
    Cr-Commit-Position: refs/heads/master@{#475844}

--- third_party/WebKit/Source/core/editing/iterators/TextIterator.cpp
+++ third_party/WebKit/Source/core/editing/iterators/TextIterator.cpp
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/iterators/TextIterator.h"
 
 #include <unicode/utf16.h>
-//#include "bindings/core/v8/ExceptionState.h"
 #include "core/HTMLNames.h"
 #include "core/InputTypeNames.h"
 #include "core/dom/Document.h"
