e7c0ccb6894d7052329812316a04e8bb2c5ec4c7
Ken Rockot <rockot@chromium.org>
2017-05-31 04:46:57 +0000

    Revert "Revert "Mojo EDK: Refactor layered message serialization""
    This reverts commit 9a9457b324f2f57511d6a1cd9a00a86b7f96837c.
    
    Reason for revert: Relanding this change as the broken test was invalid anyway and has since been removed. Sorry for the churn!
    
    Original change's description:
    > Revert "Mojo EDK: Refactor layered message serialization"
    > 
    > This reverts commit 77c59a87000dc29ebe28488b9302590081463f6e.
    > 
    > Reason for revert: Broke build. See the comment on the original review.
    > 
    > Original change's description:
    > > Mojo EDK: Refactor layered message serialization
    > > 
    > > Consolidates and simplifies user message serialization within the EDK.
    > > Namely:
    > > 
    > >   * The ports layer no longer operates under the assumption that all
    > >   events exist exclusively in serialized form. This eliminates the
    > >   complexity of layered message allocation at the cost of a small amount
    > >   of extra copying in some cases.
    > > 
    > >   * ports::Message is now more aptly named ports::Event, and different
    > >   event types are subclasses.
    > > 
    > >   * Introduces a ports::UserMessage base class for ports embedder
    > >   message types. These can be attached to ports::UserMessageEvent
    > >   objects.
    > > 
    > >   * The MessageForTransit and PortsMessage types have been flattened
    > >   into a single UserMessageImpl type, a subclass of ports::UserMessage.
    > > 
    > >   * Nearly all user message serialization and deserialization logic has
    > >   been consolidated into UserMessageImpl. A few small pieces remain
    > >   isolated in node_controller.cc and will eventually move elsewhere
    > >   after additional refactoring.
    > > 
    > > All of this work is a precursor to supporting lazy serialization.
    > > 
    > > BUG=725321
    > > 
    > > Change-Id: Ib9e66cd0d5b88ac0e6dc898f248c958039078023
    > > Reviewed-on: https://chromium-review.googlesource.com/516402
    > > Commit-Queue: Ken Rockot <rockot@chromium.org>
    > > Reviewed-by: Jay Civelli <jcivelli@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#475731}
    > 
    > TBR=jcivelli@chromium.org,rockot@chromium.org
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > BUG=725321
    > 
    > Change-Id: I84f950290e6c45dc830f2bc3177af09ae955f432
    > Reviewed-on: https://chromium-review.googlesource.com/518742
    > Reviewed-by: Takeshi Yoshino <tyoshino@chromium.org>
    > Commit-Queue: Takeshi Yoshino <tyoshino@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#475805}
    
    TBR=abarth@chromium.org,jcivelli@chromium.org,aa@chromium.org,rockot@chromium.org,tyoshino@chromium.org,darin@chromium.org,chromium-reviews@chromium.org,findit-for-me@appspot.gserviceaccount.com
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    BUG=725321
    
    Change-Id: I5a2dbe4abf2e1f576bcf3f7f651f3557583d9033
    Reviewed-on: https://chromium-review.googlesource.com/518427
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#475825}

--- mojo/edk/system/ports/node_delegate.h
+++ mojo/edk/system/ports/node_delegate.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
-#include "mojo/edk/system/ports/message.h"
+#include "mojo/edk/system/ports/event.h"
 #include "mojo/edk/system/ports/name.h"
 #include "mojo/edk/system/ports/port_ref.h"
 
@@ -23,18 +23,12 @@ class NodeDelegate {
   // Port names should be difficult to guess.
   virtual void GenerateRandomPortName(PortName* port_name) = 0;
 
-  // Allocate a message, including a header that can be used by the Node
-  // implementation. |num_header_bytes| will be aligned. The newly allocated
-  // memory need not be zero-filled.
-  virtual void AllocMessage(size_t num_header_bytes,
-                            ScopedMessage* message) = 0;
+  // Forward an event asynchronously to the specified node. This method MUST NOT
+  // synchronously call any methods on Node.
+  virtual void ForwardEvent(const NodeName& node, ScopedEvent event) = 0;
 
-  // Forward a message asynchronously to the specified node. This method MUST
-  // NOT synchronously call any methods on Node.
-  virtual void ForwardMessage(const NodeName& node, ScopedMessage message) = 0;
-
-  // Broadcast a message to all nodes.
-  virtual void BroadcastMessage(ScopedMessage message) = 0;
+  // Broadcast an event to all nodes.
+  virtual void BroadcastEvent(ScopedEvent event) = 0;
 
   // Indicates that the port's status has changed recently. Use Node::GetStatus
   // to query the latest status of the port. Note, this event could be spurious
