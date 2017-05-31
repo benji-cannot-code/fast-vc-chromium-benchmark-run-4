61a3bbdead55b1e85bb9c0303c0eb7a4a8c3f68f
Alexander Timin <altimin@chromium.org>
2017-05-31 13:04:30 +0100

    [scheduler] Foreground & background task duration.
    Report task duration split by queue type and renderer state (foreground
    vs background).
    
    R=skyostil@chromium.org,haraken@chromium.org,rkaplow@chromium.org
    BUG=702314
    
    Change-Id: Ie6ef61356bdb5eacc778a5a4475c6a59cb60ecf6
    Reviewed-on: https://chromium-review.googlesource.com/515463
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#475914}

--- third_party/WebKit/Source/platform/scheduler/renderer/renderer_scheduler_impl.h
+++ third_party/WebKit/Source/platform/scheduler/renderer/renderer_scheduler_impl.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/scheduler/renderer/idle_time_estimator.h"
 #include "platform/scheduler/renderer/render_widget_signals.h"
 #include "platform/scheduler/renderer/task_cost_estimator.h"
+#include "platform/scheduler/renderer/task_duration_metric_reporter.h"
 #include "platform/scheduler/renderer/user_model.h"
 #include "platform/scheduler/renderer/web_view_scheduler_impl.h"
 #include "public/platform/scheduler/renderer/renderer_scheduler.h"
@@ -33,7 +34,6 @@ namespace base {
 namespace trace_event {
 class ConvertableToTraceFormat;
 }
-class HistogramBase;
 }
 
 namespace blink {
@@ -304,6 +304,8 @@ class PLATFORM_EXPORT RendererSchedulerImpl
     DISALLOW_COPY_AND_ASSIGN(PollableNeedsUpdateFlag);
   };
 
+  class TaskDurationMetricTracker;
+
   // IdleHelper::Delegate implementation:
   bool CanEnterLongIdlePeriod(
       base::TimeTicks now,
@@ -412,9 +414,6 @@ class PLATFORM_EXPORT RendererSchedulerImpl
                          base::TimeTicks start_time,
                          base::TimeTicks end_time);
 
-  void RecordTaskDurationPerQueueType(TaskQueue::QueueType queue_type,
-                                      base::TimeDelta duration);
-
   SchedulerHelper helper_;
   IdleHelper idle_helper_;
   IdleCanceledDelayedTaskSweeper idle_canceled_delayed_task_sweeper_;
@@ -497,10 +496,9 @@ class PLATFORM_EXPORT RendererSchedulerImpl
     std::set<WebViewSchedulerImpl*> web_view_schedulers;  // Not owned.
     RAILModeObserver* rail_mode_observer;                 // Not owned.
     WakeUpBudgetPool* wake_up_budget_pool;                // Not owned.
-    std::array<base::TimeDelta,
-               static_cast<size_t>(TaskQueue::QueueType::COUNT)>
-        unreported_task_duration;
-    base::HistogramBase* task_duration_per_queue_type_histogram;
+    TaskDurationMetricReporter task_duration_reporter;
+    TaskDurationMetricReporter foreground_task_duration_reporter;
+    TaskDurationMetricReporter background_task_duration_reporter;
   };
 
   struct AnyThread {
