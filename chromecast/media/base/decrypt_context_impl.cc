36763a3147d1496edc950941978ad296b18e078a
Stephen Lanham <slan@google.com>
2017-05-31 11:29:52 -0700

    [Chromecast] Deprecate base::Callback etc in chromecast/media/base.
    BUG=714018, b/38263595
    
    Change-Id: Iaface3064a72965504e363d8b61f71bb7aa2d808
    Reviewed-on: https://chromium-review.googlesource.com/519605
    Commit-Queue: Stephen Lanham <slan@chromium.org>
    Reviewed-by: Stephen Lanham <slan@chromium.org>
    Reviewed-by: Alok Priyadarshi <alokp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#475989}

--- chromecast/media/base/decrypt_context_impl.cc
+++ chromecast/media/base/decrypt_context_impl.cc
@@ -38,7 +38,7 @@ bool DecryptContextImpl::Decrypt(CastDecoderBuffer* buffer,
   bool called = false;
   bool success = false;
   DecryptAsync(buffer, output, data_offset,
-               base::Bind(&BufferDecryptCB, &called, &success));
+               base::BindOnce(&BufferDecryptCB, &called, &success));
   CHECK(called) << "Sync Decrypt isn't supported";
 
   return success;
@@ -47,8 +47,8 @@ bool DecryptContextImpl::Decrypt(CastDecoderBuffer* buffer,
 void DecryptContextImpl::DecryptAsync(CastDecoderBuffer* buffer,
                                       uint8_t* output,
                                       size_t data_offset,
-                                      const DecryptCB& decrypt_cb) {
-  decrypt_cb.Run(false);
+                                      DecryptCB decrypt_cb) {
+  std::move(decrypt_cb).Run(false);
 }
 
 bool DecryptContextImpl::CanDecryptToBuffer() const {
