cae6c78becdfb091a8f17232e8bea388cec2f093
einbinder <einbinder@chromium.org>
2017-05-30 23:05:02 -0700

    DevTools: Let the drawer tabs be reordered
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2867133002
    Cr-Commit-Position: refs/heads/master@{#475821}

--- third_party/WebKit/Source/devtools/front_end/ui/View.js
+++ third_party/WebKit/Source/devtools/front_end/ui/View.js
@@ -713,6 +713,16 @@ UI.ViewManager._TabbedLocation = class extends UI.ViewManager._Location {
       }
     }
     this._appendTab(view, index);
+
+    if (view.isCloseable()) {
+      var tabs = this._closeableTabSetting.get();
+      var tabId = view.viewId();
+      if (!tabs[tabId]) {
+        tabs[tabId] = true;
+        this._closeableTabSetting.set(tabs);
+      }
+    }
+    this._persistTabOrder();
   }
 
   /**
@@ -751,17 +761,6 @@ UI.ViewManager._TabbedLocation = class extends UI.ViewManager._Location {
     var tabId = /** @type {string} */ (event.data.tabId);
     if (this._lastSelectedTabSetting && event.data['isUserGesture'])
       this._lastSelectedTabSetting.set(tabId);
-    var view = this._views.get(tabId);
-    if (!view)
-      return;
-
-    if (view.isCloseable()) {
-      var tabs = this._closeableTabSetting.get();
-      if (!tabs[tabId]) {
-        tabs[tabId] = true;
-        this._closeableTabSetting.set(tabs);
-      }
-    }
   }
 
   /**
@@ -776,10 +775,7 @@ UI.ViewManager._TabbedLocation = class extends UI.ViewManager._Location {
     }
   }
 
-  /**
-   * @param {!Common.Event} event
-   */
-  _persistTabOrder(event) {
+  _persistTabOrder() {
     var tabIds = this._tabbedPane.tabIds();
     var tabOrders = {};
     for (var i = 0; i < tabIds.length; i++)
