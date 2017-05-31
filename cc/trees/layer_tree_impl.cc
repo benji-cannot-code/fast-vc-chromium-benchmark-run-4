f5c6f40b7a8bf1e58efe8e6008e41fb9cb036a00
Philip Rogers <pdr@chromium.org>
2017-05-30 19:14:36 -0700

    Do not mark active LayerImpls as needing to push layer properties.
    Active LayerImpl properties are never pushed. This patch enforces this
    by making LayerImpl::SetNeedsPushProperties only work on the pending tree.
    
    The existing VerifyLayerChangesAreTrackedProperly test has been modified
    to work on the pending tree instead of the active tree. The existing
    mask_to_bounds tests have been updated as they were intended for
    another era when SetBounds did depend on mask_to_bounds.
    
    An additional test has been added for active layer change tracking.
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I711109df86e6c644333a8e14a5540a4b22675894
    Reviewed-on: https://chromium-review.googlesource.com/516826
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#475779}

--- cc/trees/layer_tree_impl.cc
+++ cc/trees/layer_tree_impl.cc
@@ -1248,6 +1248,7 @@ void LayerTreeImpl::ClearSurfaceLayerIds() {
 }
 
 void LayerTreeImpl::AddLayerShouldPushProperties(LayerImpl* layer) {
+  DCHECK(!IsActiveTree()) << "The active tree does not push layer properties";
   layers_that_should_push_properties_.insert(layer);
 }
 
