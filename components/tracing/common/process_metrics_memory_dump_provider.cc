cd88b8b7ca7f9e5e299cd7998b7d43a45cc7746f
etienneb <etienneb@chromium.org>
2017-05-31 09:01:56 -0700

    Adding PE TimeStamp to chrome trace to ease retrieving debug information
    On windows, symbols servers can be queried to retrieve debug information.
    The code identifier is used as a key to retrieve the exact executable.
    
    Current chrome memory dump contains the loaded module and their loaded addresses.
    example:
      {"bs":{"pc":"0","pd":"0","pss":"0","sc":"0","sd":"0","sw":"0"},"mf":"C:\\Windows\\system32\\powrprof.dll","pf":0,"sa":"7ffdae7a0000","sz":"4b000"},
    
    But to uniquely identify them and being able to retrieve debug information from
    symbols servers, the TimeStamp present in PE header is required.
    
    This patch is adding the "ts" (TimeStamp) field.
    
    {"bs":{"pc":"0","pd":"0","pss":"0","sc":"0","sd":"0","sw":"0"},"mf":"C:\\Windows\\SYSTEM32\\ntdll.dll","pf":0,"sa":"7ffdb2170000","sz":"1c1000","ts":"580ee321"},
    
    From these information one can query MS/Google symbols servers.
      https://msdl.microsoft.com/download/symbols/ntdll.dll/580EE3211C1000/ntdll.dl_
                                                  basename / code-ident   /dll
    
    The code-identifier is built from the timestamp AND the size of the executable
    
    CC=chrisha@chromium.org, erikchen@chromium.org
    R=primiano@chromium.org
    
    BUG=719969
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    
    Review-Url: https://codereview.chromium.org/2869073002
    Cr-Commit-Position: refs/heads/master@{#475930}

--- components/tracing/common/process_metrics_memory_dump_provider.cc
+++ components/tracing/common/process_metrics_memory_dump_provider.cc
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 
 #include <base/strings/sys_string_conversions.h>
+#include <base/win/pe_image.h>
 #include <base/win/win_util.h>
 #endif  // defined(OS_WIN)
 
@@ -274,6 +275,12 @@ bool ProcessMetricsMemoryDumpProvider::DumpProcessMemoryMaps(
     region.size_in_bytes = module_info.SizeOfImage;
     region.mapped_file = base::SysWideToNativeMB(module_name);
     region.start_address = reinterpret_cast<uint64_t>(module_info.lpBaseOfDll);
+
+    // The PE header field |TimeDateStamp| is required to build the PE code
+    // identifier which is used as a key to query symbols servers.
+    base::win::PEImage pe_image(module_info.lpBaseOfDll);
+    region.module_timestamp = pe_image.GetNTHeaders()->FileHeader.TimeDateStamp;
+
     pmd->process_mmaps()->AddVMRegion(region);
   }
   if (!pmd->process_mmaps()->vm_regions().empty())
