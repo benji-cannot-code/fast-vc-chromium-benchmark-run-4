336eb74c2a67f9500a65c9799d951d5308fb4f2d
robliao <robliao@chromium.org>
2017-05-30 21:37:49 -0700

    Followup to https://codereview.chromium.org/2902753003/
    BUG=720058
    
    Review-Url: https://codereview.chromium.org/2917433002
    Cr-Commit-Position: refs/heads/master@{#475804}

--- base/task_scheduler/environment_config.h
+++ base/task_scheduler/environment_config.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
+#include "base/base_export.h"
 #include "base/task_scheduler/task_traits.h"
 #include "base/threading/thread.h"
 
