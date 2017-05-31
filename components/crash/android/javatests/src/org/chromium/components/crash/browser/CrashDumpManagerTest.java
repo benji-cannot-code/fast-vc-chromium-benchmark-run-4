46a98babc569d269a71a5e7a7db288e98e513535
pkasting <pkasting@chromium.org>
2017-05-31 13:02:39 -0700

    Revert of Fix some flaky tests: Use a dedicated temp dir. (patchset #1 id:1 of https://codereview.chromium.org/2911983008/ )
    Reason for revert:
    Looks to be causing consistent failures on Android, e.g.:
    
    https://luci-milo.appspot.com/buildbot/chromium.linux/Android%20Tests/42292
    
    Original issue's description:
    > Fix some flaky tests: Use a dedicated temp dir.
    >
    > BUG=725379
    > TEST=CrashDumpManagerTest.*
    > R=gsennton@chromium.org
    >
    > Review-Url: https://codereview.chromium.org/2911983008
    > Cr-Commit-Position: refs/heads/master@{#475924}
    > Committed: https://chromium.googlesource.com/chromium/src/+/6b0e73063c6d65e7172cd247e05006078a60cf23
    
    TBR=gsennton@chromium.org,isherman@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=725379
    
    Review-Url: https://codereview.chromium.org/2915883003
    Cr-Commit-Position: refs/heads/master@{#475991}

--- components/crash/android/javatests/src/org/chromium/components/crash/browser/CrashDumpManagerTest.java
+++ components/crash/android/javatests/src/org/chromium/components/crash/browser/CrashDumpManagerTest.java
@@ -9,9 +9,10 @@ import android.support.test.filters.SmallTest;
 import android.test.InstrumentationTestCase;
 
 import org.chromium.base.ContextUtils;
-import org.chromium.base.FileUtils;
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.TestFileUtil;
 
 import java.io.File;
 import java.io.IOException;
@@ -25,24 +26,26 @@ public class CrashDumpManagerTest extends InstrumentationTestCase {
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-
         ContextUtils.initApplicationContextForTests(
                 getInstrumentation().getTargetContext().getApplicationContext());
-        mTempDir = new File(ContextUtils.getApplicationContext().getCacheDir(), "crash-dump-test");
-        if (mTempDir.exists()) {
-            FileUtils.recursivelyDeleteFile(mTempDir);
-        }
-        assert mTempDir.mkdirs();
+        mTempDir = ContextUtils.getApplicationContext().getCacheDir();
+        assert mTempDir.exists();
     }
 
     @Override
     protected void tearDown() throws Exception {
-        FileUtils.recursivelyDeleteFile(mTempDir);
         super.tearDown();
+        File[] files = mTempDir.listFiles();
+        if (files == null) return;
+
+        for (File file : files) {
+            TestFileUtil.deleteFile(file);
+        }
     }
 
     @SmallTest
     @Feature({"Android-AppBase"})
+    @DisabledTest // Flaky, crbug.com/725379.
     public void testUploadMinidump_NoCallback() throws IOException {
         File minidump = new File(mTempDir, "mini.dmp");
         assertTrue(minidump.createNewFile());
@@ -52,6 +55,7 @@ public class CrashDumpManagerTest extends InstrumentationTestCase {
 
     @SmallTest
     @Feature({"Android-AppBase"})
+    @DisabledTest // Flaky, crbug.com/725379.
     public void testUploadMinidump_NullMinidumpPath() {
         registerUploadCallback(new CrashDumpManager.UploadMinidumpCallback() {
             @Override
@@ -63,8 +67,9 @@ public class CrashDumpManagerTest extends InstrumentationTestCase {
         CrashDumpManager.tryToUploadMinidump(null);
     }
 
-    @SmallTest
-    @Feature({"Android-AppBase"})
+    // @SmallTest
+    // @Feature({"Android-AppBase"})
+    @DisabledTest // Flaky, crbug.com/726976.
     public void testUploadMinidump_FileDoesntExist() {
         registerUploadCallback(new CrashDumpManager.UploadMinidumpCallback() {
             @Override
