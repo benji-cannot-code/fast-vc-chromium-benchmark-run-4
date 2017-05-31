58fce747def87eb1cdad669158aaf3fb0b49d64d
Miguel Casas-Sanchez <mcasas@chromium.org>
2017-05-30 17:59:25 -0700

    Image Capture: resolve applyConstraints() with the passed MediaTrackConstraints argument
    MediaStreamTrack::applyConstraints() currently resolves with
    PhotoCapabilities bc Caps+Settings are retrieved after applying
    those constraints.
    
    This CL changes this to resolve applyConstraints() with the
    constraints that were applied. 
    
    LayoutTests are updated and slightly cleaned up by factoring
    out a service method into resources/imagecapture-helpers.js
    
    Bug: 723251
    Change-Id: I0db8671c75154fbeff1ef03f385ed277cf5ce17e
    Reviewed-on: https://chromium-review.googlesource.com/517280
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#475808}

--- third_party/WebKit/Source/modules/imagecapture/ImageCapture.cpp
+++ third_party/WebKit/Source/modules/imagecapture/ImageCapture.cpp
@@ -129,7 +129,7 @@ ScriptPromise ImageCapture::getPhotoCapabilities(ScriptState* script_state) {
     resolver->Reject(DOMException::Create(kNotFoundError, kNoServiceError));
     return promise;
   }
-  service_requests_.insert(resolver);
+  service_requests_.insert(resolver, HeapVector<MediaTrackConstraintSet>());
 
   // m_streamTrack->component()->source()->id() is the renderer "name" of the
   // camera;
@@ -159,7 +159,7 @@ ScriptPromise ImageCapture::setOptions(ScriptState* script_state,
     resolver->Reject(DOMException::Create(kNotFoundError, kNoServiceError));
     return promise;
   }
-  service_requests_.insert(resolver);
+  service_requests_.insert(resolver, HeapVector<MediaTrackConstraintSet>());
 
   // TODO(mcasas): should be using a mojo::StructTraits instead.
   auto settings = media::mojom::blink::PhotoSettings::New();
@@ -234,7 +234,7 @@ ScriptPromise ImageCapture::takePhoto(ScriptState* script_state) {
     return promise;
   }
 
-  service_requests_.insert(resolver);
+  service_requests_.insert(resolver, HeapVector<MediaTrackConstraintSet>());
 
   // m_streamTrack->component()->source()->id() is the renderer "name" of the
   // camera;
@@ -295,10 +295,8 @@ void ImageCapture::SetMediaTrackConstraints(
     resolver->Reject(DOMException::Create(kNotFoundError, kNoServiceError));
     return;
   }
-  service_requests_.insert(resolver);
-
   // TODO(mcasas): add support more than one single advanced constraint.
-  const auto constraints = constraints_vector[0];
+  auto constraints = constraints_vector[0];
 
   if ((constraints.hasWhiteBalanceMode() &&
        !capabilities_.hasWhiteBalanceMode()) ||
@@ -502,6 +500,8 @@ void ImageCapture::SetMediaTrackConstraints(
 
   current_constraints_ = temp_constraints;
 
+  service_requests_.insert(resolver, constraints_vector);
+
   service_->SetOptions(
       stream_track_->Component()->Source()->Id(), std::move(settings),
       ConvertToBaseCallback(
@@ -638,7 +638,19 @@ void ImageCapture::OnMojoPhotoCapabilities(
     return;
   }
 
-  resolver->Resolve(photo_capabilities_);
+  // If this is a response to a SetMediaTrackConstraints() request, it will have
+  // the original constraints to apply: Resolve() with those; Resolve() with the
+  // |photo_capabilities_| otherwise.
+  const HeapVector<MediaTrackConstraintSet>& originalConstraints =
+      service_requests_.at(resolver);
+  if (originalConstraints.IsEmpty()) {
+    resolver->Resolve(photo_capabilities_);
+  } else {
+    MediaTrackConstraints constraints;
+    constraints.setAdvanced(originalConstraints);
+    resolver->Resolve(constraints);
+  }
+
   service_requests_.erase(resolver);
 }
 
@@ -779,8 +791,8 @@ void ImageCapture::UpdateMediaTrackCapabilities(
 
 void ImageCapture::OnServiceConnectionError() {
   service_.reset();
-  for (ScriptPromiseResolver* resolver : service_requests_)
-    resolver->Reject(DOMException::Create(kNotFoundError, kNoServiceError));
+  for (const auto& resolver : service_requests_)
+    resolver.key->Reject(DOMException::Create(kNotFoundError, kNoServiceError));
   service_requests_.clear();
 }
 
