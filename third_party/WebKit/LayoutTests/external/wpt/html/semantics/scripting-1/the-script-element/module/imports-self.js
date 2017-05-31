754f7be5f439f93ebbf5f3d0b1a673a3827645e6
Georg Neis <neis@chromium.org>
2017-05-31 11:29:02 +0200

    [LayoutTests/wpt] Fix bogus tests of module cycles.
    See https://github.com/w3c/web-platform-tests/issues/5239 for an explanation.
    
    R=domenic@chromium.org, kouhei@chromium.org
    
    Bug: 
    Change-Id: I7312e4d774eea1083d2283d8ccbad361bd974d0b
    Reviewed-on: https://chromium-review.googlesource.com/519242
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#476227}

--- third_party/WebKit/LayoutTests/external/wpt/html/semantics/scripting-1/the-script-element/module/imports-self.js
+++ third_party/WebKit/LayoutTests/external/wpt/html/semantics/scripting-1/the-script-element/module/imports-self.js
@@ -2,5 +2,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import { SelfInner } from "./imports-self-inner.js";
 
 test_importSelf.step(function () {
-    assert_unreached("This module should not have loaded!");
+    assert_equals(SelfInner, "SelfInner");
+    test_importSelf.done();
 });
