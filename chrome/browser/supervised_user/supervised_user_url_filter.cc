18eb27b705c7f74fb1369f0e0ea093a00cf421c6
gab <gab@chromium.org>
2017-05-31 10:47:57 -0700

    Replace deprecated base::NonThreadSafe in chrome/browser/supervised_user in favor of SequenceChecker.
    Note to crash team: This CL is a refactor and has no intended behavior change.
    
    This change was scripted by https://crbug.com/676387#c8.
    
    Note-worthy for the reviewer:
     * SequenceChecker enforces thread-safety but not thread-affinity!
       If the classes that were updated are thread-affine (use thread local
       storage or a third-party API that does) they should be migrated to
       ThreadChecker instead.
     * ~NonThreadSafe() used to implcitly check in its destructor
       ~Sequence/ThreadChecker() doesn't by design. To keep this CL a
       no-op, an explicit check was added to the destructor of migrated
       classes.
     * NonThreadSafe used to provide access to subclasses, as such
       the |sequence_checker_| member was made protected rather than
       private where necessary.
    
    BUG=676387
    This CL was uploaded by git cl split.
    
    R=bauerb@chromium.org
    
    Review-Url: https://codereview.chromium.org/2911113004
    Cr-Commit-Position: refs/heads/master@{#475958}

--- chrome/browser/supervised_user/supervised_user_url_filter.cc
+++ chrome/browser/supervised_user/supervised_user_url_filter.cc
@@ -244,12 +244,11 @@ SupervisedUserURLFilter::SupervisedUserURLFilter()
   DCHECK(amp_cache_path_regex_.ok());
   DCHECK(google_amp_viewer_path_regex_.ok());
   DCHECK(google_web_cache_query_regex_.ok());
-  // Detach from the current thread so we can be constructed on a different
-  // thread than the one where we're used.
-  DetachFromThread();
 }
 
-SupervisedUserURLFilter::~SupervisedUserURLFilter() {}
+SupervisedUserURLFilter::~SupervisedUserURLFilter() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+}
 
 // static
 SupervisedUserURLFilter::FilteringBehavior
@@ -338,7 +337,7 @@ SupervisedUserURLFilter::GetFilteringBehaviorForURL(
     const GURL& url,
     bool manual_only,
     supervised_user_error_page::FilteringBehaviorReason* reason) const {
-  DCHECK(CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   GURL effective_url = GetEmbeddedURL(url);
   if (!effective_url.is_valid())
@@ -464,7 +463,7 @@ SupervisedUserURLFilter::GetMatchingWhitelistTitles(const GURL& url) const {
 
 void SupervisedUserURLFilter::SetDefaultFilteringBehavior(
     FilteringBehavior behavior) {
-  DCHECK(CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   default_behavior_ = behavior;
 }
 
@@ -475,7 +474,7 @@ SupervisedUserURLFilter::GetDefaultFilteringBehavior() const {
 
 void SupervisedUserURLFilter::LoadWhitelists(
     const std::vector<scoped_refptr<SupervisedUserSiteList>>& site_lists) {
-  DCHECK(CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   base::PostTaskAndReplyWithResult(
       blocking_task_runner_.get(), FROM_HERE,
@@ -495,7 +494,7 @@ bool SupervisedUserURLFilter::HasBlacklist() const {
 
 void SupervisedUserURLFilter::SetFromPatternsForTesting(
     const std::vector<std::string>& patterns) {
-  DCHECK(CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   base::PostTaskAndReplyWithResult(
       blocking_task_runner_.get(), FROM_HERE,
@@ -506,7 +505,7 @@ void SupervisedUserURLFilter::SetFromPatternsForTesting(
 
 void SupervisedUserURLFilter::SetFromSiteListsForTesting(
     const std::vector<scoped_refptr<SupervisedUserSiteList>>& site_lists) {
-  DCHECK(CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   base::PostTaskAndReplyWithResult(
       blocking_task_runner_.get(), FROM_HERE,
@@ -517,12 +516,12 @@ void SupervisedUserURLFilter::SetFromSiteListsForTesting(
 
 void SupervisedUserURLFilter::SetManualHosts(
     std::map<std::string, bool> host_map) {
-  DCHECK(CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   host_map_ = std::move(host_map);
 }
 
 void SupervisedUserURLFilter::SetManualURLs(std::map<GURL, bool> url_map) {
-  DCHECK(CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   url_map_ = std::move(url_map);
 }
 
@@ -662,7 +661,7 @@ GURL SupervisedUserURLFilter::GetEmbeddedURL(const GURL& url) const {
 }
 
 void SupervisedUserURLFilter::SetContents(std::unique_ptr<Contents> contents) {
-  DCHECK(CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   contents_ = std::move(contents);
   for (Observer& observer : observers_)
     observer.OnSiteListUpdated();
--- chrome/browser/supervised_user/supervised_user_url_filter.h
+++ chrome/browser/supervised_user/supervised_user_url_filter.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_forward.h"
 #include "base/macros.h"
 #include "base/observer_list.h"
-#include "base/threading/non_thread_safe.h"
+#include "base/sequence_checker.h"
 #include "base/values.h"
 #include "chrome/browser/safe_search_api/safe_search_url_checker.h"
 #include "chrome/browser/supervised_user/supervised_user_site_list.h"
@@ -42,7 +42,7 @@ class URLRequestContextGetter;
 //   * User-specified manual overrides (allow or block) for either sites
 //     (hostnames) or exact URLs, which take precedence over the previous
 //     sources.
-class SupervisedUserURLFilter : public base::NonThreadSafe {
+class SupervisedUserURLFilter {
  public:
   enum FilteringBehavior {
     ALLOW,
@@ -219,6 +219,8 @@ class SupervisedUserURLFilter : public base::NonThreadSafe {
   re2::RE2 google_web_cache_query_regex_;
 
   scoped_refptr<base::TaskRunner> blocking_task_runner_;
+
+  SEQUENCE_CHECKER(sequence_checker_);
 
   base::WeakPtrFactory<SupervisedUserURLFilter> weak_ptr_factory_;
 
