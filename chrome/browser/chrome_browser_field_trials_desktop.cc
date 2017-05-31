fb3b7cabbd3ae95a907f6dd9e602d49428123466
bcwhite <bcwhite@chromium.org>
2017-05-31 14:57:51 -0700

    Store the system profile in the breadcrumbs file.
    BUG=620813
    
    Review-Url: https://codereview.chromium.org/2908073005
    Cr-Commit-Position: refs/heads/master@{#476051}

--- chrome/browser/chrome_browser_field_trials_desktop.cc
+++ chrome/browser/chrome_browser_field_trials_desktop.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "components/browser_watcher/features.h"
 #include "components/browser_watcher/stability_paths.h"
+#include "components/metrics/persistent_system_profile.h"
 #include "components/variations/variations_associated_data.h"
 #include "content/public/common/content_switches.h"
 #include "media/media_features.h"
@@ -192,6 +193,12 @@ void SetupStabilityDebugging() {
         browser_watcher::kInitFlushParam, false);
     if (should_flush)
       ::FlushViewOfFile(global_tracker->allocator()->data(), 0U);
+
+    // Store a copy of the system profile in this allocator. There will be some
+    // delay before this gets populated, perhaps as much as a minute. Because
+    // of this, there is no need to flush it here.
+    metrics::GlobalPersistentSystemProfile::GetInstance()
+        ->RegisterPersistentAllocator(global_tracker->allocator());
   }
 }
 #endif  // defined(OS_WIN)
