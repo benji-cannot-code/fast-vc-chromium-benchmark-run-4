a16526bd11885e46f7beb24ce56630def097f1e0
timloh <timloh@chromium.org>
2017-05-30 21:08:15 -0700

    Wire up Android system permission prompts in PermissionRequestManager codepath
    This patch fixes the PermissionRequestManager codepath to show Android
    system permission prompts where appropriate, by implementing
    PermissionRequestImpl::GetContentSettingsType() so we know what
    permission to request.
    
    BUG=606138
    
    Review-Url: https://codereview.chromium.org/2909223003
    Cr-Commit-Position: refs/heads/master@{#475790}

--- chrome/browser/permissions/permission_request_impl.cc
+++ chrome/browser/permissions/permission_request_impl.cc
@@ -173,3 +173,7 @@ PermissionRequestGestureType PermissionRequestImpl::GetGestureType()
     const {
   return PermissionUtil::GetGestureType(has_gesture_);
 }
+
+ContentSettingsType PermissionRequestImpl::GetContentSettingsType() const {
+  return content_settings_type_;
+}
