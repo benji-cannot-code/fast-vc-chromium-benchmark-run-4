1dd7054fc0edae8359340d2692600b39a9451bf5
renjieliu <renjieliu@chromium.org>
2017-05-31 18:35:59 -0700

    Refactoring ConstructLanguageDetectionEvent.
    Refactoring the code so it can also be used by the iOS client.
    Also add a unit test.
    
    BUG=722679
    
    Review-Url: https://codereview.chromium.org/2912843003
    Cr-Commit-Position: refs/heads/master@{#476135}

---
+++ components/translate/core/common/language_detection_logging_helper.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_TRANSLATE_CORE_COMMON_LANGUAGE_DETECTION_LOGGING_HELPER_H_
+#define COMPONENTS_TRANSLATE_CORE_COMMON_LANGUAGE_DETECTION_LOGGING_HELPER_H_
+
+#include <memory>
+
+namespace sync_pb {
+class UserEventSpecifics;
+}
+
+namespace translate {
+
+struct LanguageDetectionDetails;
+
+std::unique_ptr<sync_pb::UserEventSpecifics> ConstructLanguageDetectionEvent(
+    const LanguageDetectionDetails& details);
+
+}  // namespace translate
+
+#endif  // COMPONENTS_TRANSLATE_CORE_COMMON_LANGUAGE_DETECTION_LOGGING_HELPER_H_
