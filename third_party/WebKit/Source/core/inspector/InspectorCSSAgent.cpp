a922692dfd009a53cf6e02f9b1926bfcf99b39ff
pfeldman <pfeldman@chromium.org>
2017-05-31 18:19:32 -0700

    DevTools: require enabling CSS domain before running CSS.* commands.
    BUG=727795
    
    Review-Url: https://codereview.chromium.org/2914663002
    Cr-Commit-Position: refs/heads/master@{#476130}

--- third_party/WebKit/Source/core/inspector/InspectorCSSAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorCSSAgent.cpp
@@ -758,6 +758,7 @@ void InspectorCSSAgent::WasEnabled() {
   HeapVector<Member<Document>> documents = dom_agent_->Documents();
   for (Document* document : documents)
     UpdateActiveStyleSheets(document);
+  was_enabled_ = true;
 }
 
 Response InspectorCSSAgent::disable() {
@@ -765,6 +766,7 @@ Response InspectorCSSAgent::disable() {
   dom_agent_->SetDOMListener(nullptr);
   instrumenting_agents_->removeInspectorCSSAgent(this);
   state_->setBoolean(CSSAgentState::kCssAgentEnabled, false);
+  was_enabled_ = false;
   resource_content_loader_->Cancel(resource_content_loader_client_id_);
   state_->setBoolean(CSSAgentState::kRuleRecordingEnabled, false);
   SetCoverageEnabled(false);
@@ -916,8 +918,12 @@ Response InspectorCSSAgent::getMatchedStylesForNode(
         inherited_entries,
     Maybe<protocol::Array<protocol::CSS::CSSKeyframesRule>>*
         css_keyframes_rules) {
+  Response response = AssertEnabled();
+  if (!response.isSuccess())
+    return response;
+
   Element* element = nullptr;
-  Response response = dom_agent_->AssertElement(node_id, element);
+  response = dom_agent_->AssertElement(node_id, element);
   if (!response.isSuccess())
     return response;
 
@@ -1093,8 +1099,11 @@ Response InspectorCSSAgent::getInlineStylesForNode(
     int node_id,
     Maybe<protocol::CSS::CSSStyle>* inline_style,
     Maybe<protocol::CSS::CSSStyle>* attributes_style) {
+  Response response = AssertEnabled();
+  if (!response.isSuccess())
+    return response;
   Element* element = nullptr;
-  Response response = dom_agent_->AssertElement(node_id, element);
+  response = dom_agent_->AssertElement(node_id, element);
   if (!response.isSuccess())
     return response;
 
@@ -1112,8 +1121,11 @@ Response InspectorCSSAgent::getComputedStyleForNode(
     int node_id,
     std::unique_ptr<protocol::Array<protocol::CSS::CSSComputedStyleProperty>>*
         style) {
+  Response response = AssertEnabled();
+  if (!response.isSuccess())
+    return response;
   Node* node = nullptr;
-  Response response = dom_agent_->AssertNode(node_id, node);
+  response = dom_agent_->AssertNode(node_id, node);
   if (!response.isSuccess())
     return response;
 
@@ -1179,8 +1191,11 @@ Response InspectorCSSAgent::getPlatformFontsForNode(
     int node_id,
     std::unique_ptr<protocol::Array<protocol::CSS::PlatformFontUsage>>*
         platform_fonts) {
+  Response response = AssertEnabled();
+  if (!response.isSuccess())
+    return response;
   Node* node = nullptr;
-  Response response = dom_agent_->AssertNode(node_id, node);
+  response = dom_agent_->AssertNode(node_id, node);
   if (!response.isSuccess())
     return response;
 
@@ -1566,8 +1581,11 @@ Response InspectorCSSAgent::addRule(
 Response InspectorCSSAgent::forcePseudoState(
     int node_id,
     std::unique_ptr<protocol::Array<String>> forced_pseudo_classes) {
+  Response response = AssertEnabled();
+  if (!response.isSuccess())
+    return response;
   Element* element = nullptr;
-  Response response = dom_agent_->AssertElement(node_id, element);
+  response = dom_agent_->AssertElement(node_id, element);
   if (!response.isSuccess())
     return response;
 
@@ -1892,9 +1910,17 @@ InspectorStyleSheet* InspectorCSSAgent::ViaInspectorStyleSheet(
   return css_style_sheet_to_inspector_style_sheet_.at(&inspector_sheet);
 }
 
+Response InspectorCSSAgent::AssertEnabled() {
+  return was_enabled_ ? Response::OK()
+                      : Response::Error("CSS agent was not enabled");
+}
+
 Response InspectorCSSAgent::AssertInspectorStyleSheetForId(
     const String& style_sheet_id,
     InspectorStyleSheet*& result) {
+  Response response = AssertEnabled();
+  if (!response.isSuccess())
+    return response;
   IdToInspectorStyleSheet::iterator it =
       id_to_inspector_style_sheet_.find(style_sheet_id);
   if (it == id_to_inspector_style_sheet_.end())
