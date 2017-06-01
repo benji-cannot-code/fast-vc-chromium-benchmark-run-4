70c8f824cb0abf4762aeaf0b12d077dc53720ed5
asvitkine <asvitkine@chromium.org>
2017-06-01 13:14:27 -0700

    Default to new metrics service URL for non-Cast clients.
    M59-M60 are controlled by the feature, while M61+ will use the
    new URL exclusively, except for Cast.
    
    BUG=703835
    
    Review-Url: https://codereview.chromium.org/2922493002
    Cr-Commit-Position: refs/heads/master@{#476407}

--- components/metrics/metrics_service_client.cc
+++ components/metrics/metrics_service_client.cc
@@ -5,22 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/metrics/metrics_service_client.h"
 
-#include "base/feature_list.h"
 #include "components/metrics/url_constants.h"
 
 namespace metrics {
-
-namespace {
-
-#if defined(OS_ANDROID) || defined(OS_IOS)
-const base::Feature kNewMetricsUrlFeature{"NewMetricsUrl",
-                                          base::FEATURE_ENABLED_BY_DEFAULT};
-#else
-const base::Feature kNewMetricsUrlFeature{"NewMetricsUrl",
-                                          base::FEATURE_DISABLED_BY_DEFAULT};
-#endif
-
-}  // namespace
 
 MetricsServiceClient::MetricsServiceClient() : update_running_services_() {}
 
@@ -47,9 +34,7 @@ bool MetricsServiceClient::IsUMACellularUploadLogicEnabled() {
 }
 
 std::string MetricsServiceClient::GetMetricsServerUrl() {
-  return base::FeatureList::IsEnabled(kNewMetricsUrlFeature)
-             ? kNewMetricsServerUrl
-             : kOldMetricsServerUrl;
+  return kNewMetricsServerUrl;
 }
 
 bool MetricsServiceClient::IsHistorySyncEnabledOnAllProfiles() {
