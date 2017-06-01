0d1b13b9381bd6d058f0ceedde08e32f0cd7c3a8
Ramin Halavati <rhalavati@chromium.org>
2017-06-01 06:53:45 +0200

    Network traffic annotation moved to callers of LargeIconService.
    LargeIconService was creating required network traffic annotation for all its
    different callers, now annotations are moved to callers' level to be more
    precise and closer to the real point of origin.
    
    Bug: 656607
    Change-Id: I809f127f526f4ea331be10e268b379b7c1a5bfe6
    Reviewed-on: https://chromium-review.googlesource.com/518863
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Helen Li <xunjieli@chromium.org>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#476641}

--- components/favicon/core/large_icon_service.h
+++ components/favicon/core/large_icon_service.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/favicon_base/favicon_callback.h"
 #include "components/image_fetcher/core/image_fetcher.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "net/traffic_annotation/network_traffic_annotation.h"
 
 class GURL;
 
@@ -95,6 +96,7 @@ class LargeIconService : public KeyedService {
       int min_source_size_in_pixel,
       int desired_size_in_pixel,
       bool may_page_url_be_private,
+      const net::NetworkTrafficAnnotationTag& traffic_annotation,
       const base::Callback<void(bool success)>& callback);
 
  private:
--- components/favicon/core/large_icon_service_unittest.cc
+++ components/favicon/core/large_icon_service_unittest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/image_fetcher/core/image_fetcher.h"
 #include "components/image_fetcher/core/request_metadata.h"
 #include "components/variations/variations_params_manager.h"
+#include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/skia/include/core/SkBitmap.h"
@@ -165,7 +166,7 @@ TEST_F(LargeIconServiceTest, ShouldGetFromGoogleServer) {
       .GetLargeIconOrFallbackStyleFromGoogleServerSkippingLocalCache(
           GURL(kDummyUrl), /*min_source_size_in_pixel=*/42,
           /*desired_size_in_pixel=*/61, /*may_page_url_be_private=*/true,
-          callback.Get());
+          TRAFFIC_ANNOTATION_FOR_TESTS, callback.Get());
 
   EXPECT_CALL(callback, Run(true));
   base::RunLoop().RunUntilIdle();
@@ -202,7 +203,7 @@ TEST_F(LargeIconServiceTest, ShouldGetFromGoogleServerWithCustomUrl) {
       .GetLargeIconOrFallbackStyleFromGoogleServerSkippingLocalCache(
           GURL(kDummyUrl), /*min_source_size_in_pixel=*/42,
           /*desired_size_in_pixel=*/61, /*may_page_url_be_private=*/true,
-          callback.Get());
+          TRAFFIC_ANNOTATION_FOR_TESTS, callback.Get());
 
   EXPECT_CALL(callback, Run(true));
   base::RunLoop().RunUntilIdle();
@@ -233,7 +234,7 @@ TEST_F(LargeIconServiceTest, ShouldGetFromGoogleServerWithOriginalUrl) {
       .GetLargeIconOrFallbackStyleFromGoogleServerSkippingLocalCache(
           GURL(kDummyUrl), /*min_source_size_in_pixel=*/42,
           /*desired_size_in_pixel=*/61, /*may_page_url_be_private=*/true,
-          callback.Get());
+          TRAFFIC_ANNOTATION_FOR_TESTS, callback.Get());
 
   EXPECT_CALL(callback, Run(true));
   base::RunLoop().RunUntilIdle();
@@ -258,7 +259,7 @@ TEST_F(LargeIconServiceTest, ShouldTrimQueryParametersForGoogleServer) {
       .GetLargeIconOrFallbackStyleFromGoogleServerSkippingLocalCache(
           GURL(kDummyUrlWithQuery), /*min_source_size_in_pixel=*/42,
           /*desired_size_in_pixel=*/61, /*may_page_url_be_private=*/true,
-          base::Callback<void(bool success)>());
+          TRAFFIC_ANNOTATION_FOR_TESTS, base::Callback<void(bool success)>());
 
   base::RunLoop().RunUntilIdle();
 }
@@ -277,7 +278,7 @@ TEST_F(LargeIconServiceTest, ShouldNotCheckOnPublicUrls) {
       .GetLargeIconOrFallbackStyleFromGoogleServerSkippingLocalCache(
           GURL(kDummyUrl), /*min_source_size_in_pixel=*/42,
           /*desired_size_in_pixel=*/61, /*may_page_url_be_private=*/false,
-          callback.Get());
+          TRAFFIC_ANNOTATION_FOR_TESTS, callback.Get());
 
   EXPECT_CALL(callback, Run(false));
   base::RunLoop().RunUntilIdle();
@@ -295,7 +296,7 @@ TEST_F(LargeIconServiceTest, ShouldNotQueryGoogleServerIfInvalidScheme) {
       .GetLargeIconOrFallbackStyleFromGoogleServerSkippingLocalCache(
           GURL(kDummyFtpUrl), /*min_source_size_in_pixel=*/42,
           /*desired_size_in_pixel=*/61, /*may_page_url_be_private=*/true,
-          callback.Get());
+          TRAFFIC_ANNOTATION_FOR_TESTS, callback.Get());
 
   EXPECT_CALL(callback, Run(false));
   base::RunLoop().RunUntilIdle();
@@ -325,7 +326,7 @@ TEST_F(LargeIconServiceTest, ShouldReportUnavailableIfFetchFromServerFails) {
       .GetLargeIconOrFallbackStyleFromGoogleServerSkippingLocalCache(
           kDummyUrlWithQuery, /*min_source_size_in_pixel=*/42,
           /*desired_size_in_pixel=*/61, /*may_page_url_be_private=*/true,
-          callback.Get());
+          TRAFFIC_ANNOTATION_FOR_TESTS, callback.Get());
 
   EXPECT_CALL(callback, Run(false));
   base::RunLoop().RunUntilIdle();
@@ -354,7 +355,7 @@ TEST_F(LargeIconServiceTest, ShouldNotGetFromGoogleServerIfUnavailable) {
       .GetLargeIconOrFallbackStyleFromGoogleServerSkippingLocalCache(
           GURL(kDummyUrl), /*min_source_size_in_pixel=*/42,
           /*desired_size_in_pixel=*/61, /*may_page_url_be_private=*/true,
-          callback.Get());
+          TRAFFIC_ANNOTATION_FOR_TESTS, callback.Get());
 
   EXPECT_CALL(callback, Run(false));
   base::RunLoop().RunUntilIdle();
