9b1adc5692281e8b4d48995300961717632d692b
Florin Malita <fmalita@chromium.org>
2017-06-01 15:49:46 -0400

    Prefer the sk_sp-based SkRWBuffer API
    s/newRBufferSnapshot/makeROBufferSnapshot
    
    Change-Id: I0c37f776cd196bac3b7c2a0ff5bc0f9d4e27249c
    Reviewed-on: https://chromium-review.googlesource.com/521382
    Reviewed-by: Leon Scroggins <scroggo@chromium.org>
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#476667}

--- third_party/WebKit/Source/platform/image-decoders/FastSharedBufferReaderTest.cpp
+++ third_party/WebKit/Source/platform/image-decoders/FastSharedBufferReaderTest.cpp
@@ -54,8 +54,7 @@ PassRefPtr<SegmentReader> CopyToROBufferSegmentReader(
     rw_buffer.append(segment, length);
     position += length;
   }
-  return SegmentReader::CreateFromSkROBuffer(
-      sk_sp<SkROBuffer>(rw_buffer.newRBufferSnapshot()));
+  return SegmentReader::CreateFromSkROBuffer(rw_buffer.makeROBufferSnapshot());
 }
 
 PassRefPtr<SegmentReader> CopyToDataSegmentReader(
@@ -220,8 +219,8 @@ TEST(SegmentReaderTest, variableSegments) {
     rw_buffer.append(reference_data + 3 * SharedBuffer::kSegmentSize,
                      .5 * SharedBuffer::kSegmentSize);
 
-    segment_reader = SegmentReader::CreateFromSkROBuffer(
-        sk_sp<SkROBuffer>(rw_buffer.newRBufferSnapshot()));
+    segment_reader =
+        SegmentReader::CreateFromSkROBuffer(rw_buffer.makeROBufferSnapshot());
   }
 
   const char* segment;
