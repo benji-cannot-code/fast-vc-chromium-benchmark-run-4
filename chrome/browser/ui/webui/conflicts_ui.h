06856e1ebbbe133039c48baaef75e7f8f21535e0
Patrick Monette <pmonette@chromium.org>
2017-06-01 19:28:06 -0400

    Split ConflictsHandler into its own file.
    This paves the way for a new handler that uses the ModuleDatabase as a
    backend.
    
    BUG=717696
    
    Change-Id: I7d5fdf5729e460fc13c181d62c73d7586d4e8f7f
    Reviewed-on: https://chromium-review.googlesource.com/521736
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#476805}

--- chrome/browser/ui/webui/conflicts_ui.h
+++ chrome/browser/ui/webui/conflicts_ui.h
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_ui_controller.h"
 #include "ui/base/layout.h"
 
-#if defined(OS_WIN)
-
 namespace base {
 class RefCountedMemory;
 }
@@ -28,7 +26,5 @@ class ConflictsUI : public content::WebUIController {
  private:
   DISALLOW_COPY_AND_ASSIGN(ConflictsUI);
 };
-
-#endif
 
 #endif  // CHROME_BROWSER_UI_WEBUI_CONFLICTS_UI_H_
