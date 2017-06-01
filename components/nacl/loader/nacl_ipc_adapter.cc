c570c31d670c1b3ee0076e387aebd067c5888745
tzik <tzik@chromium.org>
2017-05-31 22:13:10 -0700

    Close NaCl IPC channel when the NaClDesc is released
    NaClIPCAdapter has a ref count and a NaClDesc retains the reference to
    it, and an IPC::Channel has a raw pointer to the NaClIPCAdapter as a
    IPC::Listener without retaining a reference.
    
    When NaClDesc releases NaClIPCAdapter and it's being destroyed, it
    schedules a task to destroy the channel. However, there's a gap between
    the NaClIPCAdapter destruction and the channel destruction, and a chance
    for the channel to call OnChannelError on NaClIPCAdapter, that causes
    an UAF.
    
    After this CL, NaClDesc closes the channel before it releases NaClIPCAdapter.
    
    BUG=719942
    TBR=dschuff@chromium.org
    
    Review-Url: https://codereview.chromium.org/2876493002
    Cr-Commit-Position: refs/heads/master@{#476198}

--- components/nacl/loader/nacl_ipc_adapter.cc
+++ components/nacl/loader/nacl_ipc_adapter.cc
@@ -68,7 +68,12 @@ struct DescThunker {
   explicit DescThunker(NaClIPCAdapter* adapter_arg)
       : adapter(adapter_arg) {
   }
+
+  ~DescThunker() { adapter->CloseChannel(); }
+
   scoped_refptr<NaClIPCAdapter> adapter;
+
+  DISALLOW_COPY_AND_ASSIGN(DescThunker);
 };
 
 NaClIPCAdapter* ToAdapter(void* handle) {
