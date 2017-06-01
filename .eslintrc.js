03bcd2a8c2b2b81bfbbdfd7b777ea5b723af8bed
dpapad <dpapad@chromium.org>
2017-05-31 18:06:42 -0700

    js_checker.py: Replace custom WrapperTypeCheck with ESLint's no-new-wrappers.
    BUG=720034
    
    Review-Url: https://codereview.chromium.org/2913453002
    Cr-Commit-Position: refs/heads/master@{#476121}

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
   'rules': {
     // Enabled checks.
     'no-extra-semi': 'error',
+    'no-new-wrappers': 'error',
     'no-restricted-properties': ['error', {
       'object': 'document',
       'property': 'getElementById',
