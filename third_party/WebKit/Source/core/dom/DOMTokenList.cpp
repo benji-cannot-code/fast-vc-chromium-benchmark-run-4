b29a07621b214d0a26fabc7e1c9421b541d7a3c9
tkent <tkent@chromium.org>
2017-05-31 19:09:53 -0700

    DOMTokenList: Rename add() and remove() with single string argument.
    add() -> Add()
    remove() -> Remove()
    
    because they are not web-exposed. Also, this CL removes ExceptionState arguments
    and adds ASSERT_NO_EXCEPTION in the functions.
    
    This CL has no behavior changes.
    
    BUG=726214
    
    Review-Url: https://codereview.chromium.org/2910143002
    Cr-Commit-Position: refs/heads/master@{#476153}

--- third_party/WebKit/Source/core/dom/DOMTokenList.cpp
+++ third_party/WebKit/Source/core/dom/DOMTokenList.cpp
@@ -87,11 +87,8 @@ bool DOMTokenList::contains(const AtomicString& token) const {
   return tokens_.Contains(token);
 }
 
-void DOMTokenList::add(const AtomicString& token,
-                       ExceptionState& exception_state) {
-  Vector<String> tokens;
-  tokens.push_back(token.GetString());
-  add(tokens, exception_state);
+void DOMTokenList::Add(const AtomicString& token) {
+  add(Vector<String>({token}), ASSERT_NO_EXCEPTION);
 }
 
 // https://dom.spec.whatwg.org/#dom-domtokenlist-add
@@ -105,11 +102,8 @@ void DOMTokenList::add(const Vector<String>& tokens,
   AddTokens(tokens);
 }
 
-void DOMTokenList::remove(const AtomicString& token,
-                          ExceptionState& exception_state) {
-  Vector<String> tokens;
-  tokens.push_back(token.GetString());
-  remove(tokens, exception_state);
+void DOMTokenList::Remove(const AtomicString& token) {
+  remove(Vector<String>({token}), ASSERT_NO_EXCEPTION);
 }
 
 // https://dom.spec.whatwg.org/#dom-domtokenlist-remove
