45db6b0ea0437d7f44c48d1935f22214e5cadcbd
pauljensen <pauljensen@chromium.org>
2017-06-01 08:49:48 -0700

    [Cronet] Remove a remaining part of data reduction proxy
    This was added back in crrev.com/329674 and appears unused.
    
    R=mgersh@chromium.org
    B=716108
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_cronet_tester
    
    Review-Url: https://codereview.chromium.org/2918663005
    Cr-Commit-Position: refs/heads/master@{#476296}

--- components/cronet/android/cronet_library_loader.cc
+++ components/cronet/android/cronet_library_loader.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_registrar.h"
 #include "base/android/jni_utils.h"
 #include "base/android/library_loader/library_loader_hooks.h"
-#include "base/command_line.h"
 #include "base/feature_list.h"
 #include "base/logging.h"
 #include "base/macros.h"
@@ -103,9 +102,6 @@ void CronetInitOnInitThread(JNIEnv* env, const JavaParamRef<jclass>& jcaller) {
 #endif
 
   base::FeatureList::InitializeInstance(std::string(), std::string());
-  // TODO(bengr): Remove once Data Reduction Proxy no longer needs this for
-  // configuration information.
-  base::CommandLine::Init(0, nullptr);
   DCHECK(!base::MessageLoop::current());
   DCHECK(!g_init_message_loop);
   g_init_message_loop =
