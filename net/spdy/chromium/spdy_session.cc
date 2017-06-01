dc10517260d7ffc353ef37e86ff12d8023921494
zhongyi <zhongyi@chromium.org>
2017-05-31 19:00:03 -0700

    Use correct error code: REFUSED_STREAM to reject server push from the server in SPDY.
    BUG=726725
    
    Review-Url: https://codereview.chromium.org/2914943002
    Cr-Commit-Position: refs/heads/master@{#476147}

--- net/spdy/chromium/spdy_session.cc
+++ net/spdy/chromium/spdy_session.cc
@@ -1592,7 +1592,7 @@ void SpdySession::TryCreatePushStream(SpdyStreamId stream_id,
   GURL gurl = GetUrlFromHeaderBlock(headers);
   if (!gurl.is_valid()) {
     EnqueueResetStreamFrame(
-        stream_id, request_priority, ERROR_CODE_PROTOCOL_ERROR,
+        stream_id, request_priority, ERROR_CODE_REFUSED_STREAM,
         "Pushed stream url was invalid: " + gurl.possibly_invalid_spec());
     return;
   }
@@ -1661,7 +1661,7 @@ void SpdySession::TryCreatePushStream(SpdyStreamId stream_id,
   if (pushed_it != unclaimed_pushed_streams_.end() &&
       pushed_it->first == gurl) {
     EnqueueResetStreamFrame(
-        stream_id, request_priority, ERROR_CODE_PROTOCOL_ERROR,
+        stream_id, request_priority, ERROR_CODE_REFUSED_STREAM,
         "Received duplicate pushed stream with url: " + gurl.spec());
     return;
   }
