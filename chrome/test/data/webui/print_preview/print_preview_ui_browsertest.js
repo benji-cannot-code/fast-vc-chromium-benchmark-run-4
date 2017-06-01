2c417db3f8a88dffeadcd3f7c87b46bfa65239fe
dpapad <dpapad@chromium.org>
2017-05-31 20:57:01 -0700

    Print Preview: Migrate remaining JS tests to use Mocha.
    Migrating all remaining tests:
    
    DuplexSettingsTrue
    DuplexSettingsFalse
    PrinterChangeUpdatesPreview
    NoPDFPluginErrorMessage
    CustomPaperNames
    AdvancedSettings1Option
    AdvancedSettings2Options
    InitIssuesOneRequest
    InvalidSettingsError
    GenerateDraft
    
    BUG=726536
    
    Review-Url: https://codereview.chromium.org/2920593002
    Cr-Commit-Position: refs/heads/master@{#476182}

--- chrome/test/data/webui/print_preview/print_preview_ui_browsertest.js
+++ chrome/test/data/webui/print_preview/print_preview_ui_browsertest.js
@@ -26,7 +26,7 @@ PrintPreviewUIBrowserTest.prototype = {
   runAccessibilityChecks: true,
 
   /** @override */
-  accessibilityIssuesAreErrors: false,
+  accessibilityIssuesAreErrors: true,
 
   /** @override */
   isAsync: true,
@@ -83,15 +83,35 @@ PrintPreviewUIBrowserTest.prototype = {
   'ZeroTopAndBottomMarginsHideHeaderFooter',
   'ZeroTopAndNonZeroBottomMarginShowHeaderFooter',
   'SmallPaperSizeHeaderFooter',
-  'TestColorSettingsMonochrome',
-  'TestColorSettingsCustomMonochrome',
-  'TestColorSettingsColor',
-  'TestColorSettingsCustomColor',
-  'TestColorSettingsBothStandardDefaultColor',
-  'TestColorSettingsBothStandardDefaultMonochrome',
-  'TestColorSettingsBothCustomDefaultColor',
+  'ColorSettingsMonochrome',
+  'ColorSettingsCustomMonochrome',
+  'ColorSettingsColor',
+  'ColorSettingsCustomColor',
+  'ColorSettingsBothStandardDefaultColor',
+  'ColorSettingsBothStandardDefaultMonochrome',
+  'ColorSettingsBothCustomDefaultColor',
+  'DuplexSettingsTrue',
+  'DuplexSettingsFalse',
+  'PrinterChangeUpdatesPreview',
+  'NoPDFPluginErrorMessage',
+  'CustomPaperNames',
+  'InitIssuesOneRequest',
+  'InvalidSettingsError',
+  'GenerateDraft',
 ].forEach(function(testName) {
   TEST_F('PrintPreviewUIBrowserTest', testName, function() {
+    mocha.grep(new RegExp(testName + '\\b')).run();
+  });
+});
+
+
+// Disable accessibility errors for some tests.
+[
+  'AdvancedSettings1Option',
+  'AdvancedSettings2Options',
+].forEach(function(testName) {
+  TEST_F('PrintPreviewUIBrowserTest', testName, function() {
+    this.accessibilityIssuesAreErrors = false;
     mocha.grep(new RegExp(testName + '\\b')).run();
   });
 });
