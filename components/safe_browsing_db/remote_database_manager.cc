be8ffe7c869b7deae89d79bd4499b9b628c72e22
nparker <nparker@chromium.org>
2017-06-01 11:53:40 -0700

    Add the ability to check the CSD Whitelist asynchronously,for PhishGuard.
    This adds V4LocalDatabaseManager::CheckCsdWhitelistUrl() and tests for
    it. A later CL will switch the three callers of MatchCsdWhitelistUrl()
    to this new function, and remove the Match method.
    
    BUG=714300
    
    Review-Url: https://codereview.chromium.org/2890293004
    Cr-Commit-Position: refs/heads/master@{#476372}

--- components/safe_browsing_db/remote_database_manager.cc
+++ components/safe_browsing_db/remote_database_manager.cc
@@ -259,6 +259,13 @@ bool RemoteSafeBrowsingDatabaseManager::CheckUrlForSubresourceFilter(
   return false;
 }
 
+AsyncMatch RemoteSafeBrowsingDatabaseManager::CheckCsdWhitelistUrl(
+    const GURL& url,
+    Client* client) {
+  NOTREACHED();
+  return AsyncMatch::MATCH;
+}
+
 bool RemoteSafeBrowsingDatabaseManager::MatchCsdWhitelistUrl(const GURL& url) {
   NOTREACHED();
   return true;
