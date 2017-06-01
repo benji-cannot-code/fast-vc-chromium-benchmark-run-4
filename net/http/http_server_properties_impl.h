0c7f1716f436f2d28606c55c806aff8351bd7820
wangyix <wangyix@chromium.org>
2017-06-01 16:14:16 -0700

    Fix and refactor HttpServerPropertiesImpl's alternative services brokenness expiration behavior.
    One of HttpServerPropertiesImpl's responsibilities is to store HTTP alternative services that have failed and keeps track of their time-until-retry (i.e. when their brokenness expires). The broken alt-svcs and their respective expiration times are kept in a list ordered by expiration time. When the expiration task runs, it drops items from the front of the list until the expiration time of the head alt-svc is no longer in the past. Turns out this list was kept in insertion order as opposed to expiration-sorted order. This change keeps this queue in expiration-sorted order, as expected.
    The additional logic added to implement the above change led to a refactor of HttpServerPropertiesImpl: its broken alt-svc logic is put in its own class, BrokenAlternativeServices.
    
    BUG=724302
    
    Review-Url: https://codereview.chromium.org/2898983006
    Cr-Commit-Position: refs/heads/master@{#476475}

--- net/http/http_server_properties_impl.h
+++ net/http/http_server_properties_impl.h
@@ -18,11 +18,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/threading/non_thread_safe.h"
+#include "base/time/default_tick_clock.h"
 #include "base/values.h"
 #include "net/base/host_port_pair.h"
 #include "net/base/ip_address.h"
 #include "net/base/linked_hash_map.h"
 #include "net/base/net_export.h"
+#include "net/http/broken_alternative_services.h"
 #include "net/http/http_server_properties.h"
 
 namespace base {
@@ -31,18 +33,15 @@ class ListValue;
 
 namespace net {
 
-struct AlternativeServiceHash {
-  size_t operator()(const net::AlternativeService& entry) const {
-    return entry.protocol ^ std::hash<std::string>()(entry.host) ^ entry.port;
-  }
-};
-
 // The implementation for setting/retrieving the HTTP server properties.
 class NET_EXPORT HttpServerPropertiesImpl
     : public HttpServerProperties,
+      public BrokenAlternativeServices::Delegate,
       NON_EXPORTED_BASE(public base::NonThreadSafe) {
  public:
   HttpServerPropertiesImpl();
+  explicit HttpServerPropertiesImpl(
+      base::TickClock* broken_alternative_services_clock);
   ~HttpServerPropertiesImpl() override;
 
   // Sets |spdy_servers_map_| with the servers (host/port) from
@@ -123,7 +122,13 @@ class NET_EXPORT HttpServerPropertiesImpl
       size_t max_server_configs_stored_in_properties) override;
   bool IsInitialized() const override;
 
+  // BrokenAlternativeServices::Delegate method.
+  void OnExpireBrokenAlternativeService(
+      const AlternativeService& expired_alternative_service) override;
+
  private:
+  // TODO (wangyix): modify HttpServerPropertiesImpl unit tests so this
+  // friendness is no longer required.
   friend class HttpServerPropertiesImplPeer;
 
   // |spdy_servers_map_| has flattened representation of servers
@@ -132,14 +137,6 @@ class NET_EXPORT HttpServerPropertiesImpl
   typedef std::map<url::SchemeHostPort, url::SchemeHostPort> CanonicalHostMap;
   typedef std::vector<std::string> CanonicalSufficList;
   typedef std::set<HostPortPair> Http11ServerHostPortSet;
-
-  // Linked hash map from AlternativeService to expiration time.  This container
-  // is a queue with O(1) enqueue and dequeue, and a hash_map with O(1) lookup
-  // at the same time.
-  typedef linked_hash_map<AlternativeService,
-                          base::TimeTicks,
-                          AlternativeServiceHash>
-      BrokenAlternativeServices;
 
   // Return the iterator for |server|, or for its canonical host, or end.
   AlternativeServiceMap::const_iterator GetAlternateProtocolIterator(
@@ -151,17 +148,14 @@ class NET_EXPORT HttpServerPropertiesImpl
 
   // Remove the cononical host for |server|.
   void RemoveCanonicalHost(const url::SchemeHostPort& server);
-  void ExpireBrokenAlternateProtocolMappings();
-  void ScheduleBrokenAlternateProtocolMappingsExpiration();
+
+  base::DefaultTickClock broken_alternative_services_clock_;
+  BrokenAlternativeServices broken_alternative_services_;
 
   SpdyServersMap spdy_servers_map_;
   Http11ServerHostPortSet http11_servers_;
 
   AlternativeServiceMap alternative_service_map_;
-  BrokenAlternativeServices broken_alternative_services_;
-  // Class invariant:  Every alternative service in broken_alternative_services_
-  // must also be in recently_broken_alternative_services_.
-  RecentlyBrokenAlternativeServices recently_broken_alternative_services_;
 
   IPAddress last_quic_address_;
   ServerNetworkStatsMap server_network_stats_map_;
@@ -175,8 +169,6 @@ class NET_EXPORT HttpServerPropertiesImpl
 
   QuicServerInfoMap quic_server_info_map_;
   size_t max_server_configs_stored_in_properties_;
-
-  base::WeakPtrFactory<HttpServerPropertiesImpl> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(HttpServerPropertiesImpl);
 };
