3fb738f9d74f2c045ecf9843ee3f572be888b521
Philip Rogers <pdr@chromium.org>
2017-05-31 18:56:25 -0700

    Fix blink_perf tests after FrameView rename
    FrameView was renamed LocalFrameView which broke our perf tests.
    This patch updates the trace names to use LocalFrameView. The
    indentation in this patch follows the javascript style guide.
    
    The perf unittests (benchmarks.blink_perf_unittest.BlinkPerfTest) had
    a bug where GetRepresentativeNumber was missing parens which is how
    this got through.
    
    BUG: 728011
    Change-Id: I48ec4efd38ec82338143f931364e7d203a0c771c
    Reviewed-on: https://chromium-review.googlesource.com/520711
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#476294}

--- third_party/WebKit/PerformanceTests/Paint/resources/paint.js
+++ third_party/WebKit/PerformanceTests/Paint/resources/paint.js
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 function measurePaint(test) {
   test.tracingCategories = 'blink';
-  test.traceEventsToMeasure = ['FrameView::prePaint', 'FrameView::paintTree'];
+  test.traceEventsToMeasure = [
+    'LocalFrameView::prePaint',
+    'LocalFrameView::paintTree'
+  ];
   PerfTestRunner.measureFrameTime(test);
 }
