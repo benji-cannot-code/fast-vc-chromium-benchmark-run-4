09ee0acb4d583903daa87648643ea233d9aeaf79
allada <allada@chromium.org>
2017-06-01 15:52:34 -0700

    [Devtools] Move to canReplayRequest in NetworkRequest
    This patch is in preporation to break target dependency in
    NetworkRequest. This specific patch is because we will be adding more
    conditions in canReplayRequest().
    
    R=dgozman,pfeldman
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2906843002
    Cr-Commit-Position: refs/heads/master@{#476466}

--- third_party/WebKit/Source/devtools/front_end/network/NetworkLogView.js
+++ third_party/WebKit/Source/devtools/front_end/network/NetworkLogView.js
@@ -1162,6 +1162,12 @@ Network.NetworkLogView = class extends UI.VBox {
             Common.UIString.capitalize('Unblock ' + croppedDomain), removeBlockedURL.bind(null, domain));
       }
 
+      if (SDK.NetworkManager.canReplayRequest(request)) {
+        contextMenu.appendSeparator();
+        contextMenu.appendItem(Common.UIString('Replay XHR'), SDK.NetworkManager.replayRequest.bind(null, request));
+        contextMenu.appendSeparator();
+      }
+
       /**
        * @param {string} url
        */
@@ -1180,12 +1186,6 @@ Network.NetworkLogView = class extends UI.VBox {
         manager.setBlockedPatterns(patterns);
         UI.viewManager.showView('network.blocked-urls');
       }
-    }
-
-    if (request && request.resourceType() === Common.resourceTypes.XHR) {
-      contextMenu.appendSeparator();
-      contextMenu.appendItem(Common.UIString('Replay XHR'), request.replayXHR.bind(request));
-      contextMenu.appendSeparator();
     }
   }
 
--- third_party/WebKit/Source/devtools/front_end/sdk/NetworkRequest.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/NetworkRequest.js
@@ -1101,10 +1101,6 @@ SDK.NetworkRequest = class extends Common.Object {
     this.dispatchEventToListeners(SDK.NetworkRequest.Events.EventSourceMessageAdded, message);
   }
 
-  replayXHR() {
-    this._networkManager.target().networkAgent().replayXHR(this._requestId);
-  }
-
   /**
    * @return {!SDK.NetworkManager}
    */
