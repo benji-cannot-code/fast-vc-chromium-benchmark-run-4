4a93a8768079fff6a661dab16352b93485f95bd5
Sonny Sasaka <sonnysasaka@chromium.org>
2017-06-01 12:08:09 -0700

    Roll src/third_party/cros_system_api/ 6b09df23f..ff270ca21 (4 commits)
    https://chromium.googlesource.com/chromiumos/platform/system_api.git/+log/6b09df23f8a9..ff270ca217a8
    
    $ git log 6b09df23f..ff270ca21 --date=short --no-merges --format='%ad %ae %s'
    2017-05-23 sonnysasaka New DBus constant for BlueZ API ResetAdvertising.
    2017-05-19 tengs CHROMIUM: Add SetLEConnectionParameters to DBUS service constants.
    2017-04-27 ljusten Move authpolicy's ErrorType to a proto
    2017-05-15 yunlian system_api: use '-fPIC' for libsystem_api-chaps-protos.a
    
    Created with:
      roll-dep src/third_party/cros_system_api
    
    Adjustments need to be made to include headers of some of chrome/browser/ui/webui/chromeos/login/*
    due to https://chromium-review.googlesource.com/c/488881/
    
    BUG=636142
    TEST=gclient sync and confirmed that Chrome compiled fine.
    
    Change-Id: Iabae04950d6c5c54c04a13c1a6d45544aa116726
    Reviewed-on: https://chromium-review.googlesource.com/517309
    Commit-Queue: Sonny Sasaka <sonnysasaka@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Miao-chen Chou <mcchou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#476431}

--- chrome/browser/ui/webui/chromeos/login/enrollment_screen_handler.h
+++ chrome/browser/ui/webui/chromeos/login/enrollment_screen_handler.h
@@ -16,8 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/policy/enrollment_config.h"
 #include "chrome/browser/ui/webui/chromeos/login/base_screen_handler.h"
 #include "chrome/browser/ui/webui/chromeos/login/network_state_informer.h"
+#include "chromeos/dbus/auth_policy_client.h"
 #include "net/base/net_errors.h"
-#include "third_party/cros_system_api/dbus/service_constants.h"
 
 namespace chromeos {
 
--- chrome/browser/ui/webui/chromeos/login/gaia_screen_handler.h
+++ chrome/browser/ui/webui/chromeos/login/gaia_screen_handler.h
@@ -15,9 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/screens/gaia_view.h"
 #include "chrome/browser/ui/webui/chromeos/login/base_screen_handler.h"
 #include "chrome/browser/ui/webui/chromeos/login/network_state_informer.h"
+#include "chromeos/dbus/auth_policy_client.h"
 #include "chromeos/network/portal_detector/network_portal_detector.h"
 #include "net/base/net_errors.h"
-#include "third_party/cros_system_api/dbus/service_constants.h"
 
 class AccountId;
 
