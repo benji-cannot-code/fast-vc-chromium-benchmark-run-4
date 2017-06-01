ba58f359a82a2ce732cfdabbaf7b5fa0d5b7930f
rtoy <rtoy@chromium.org>
2017-05-31 17:41:16 -0700

    Compute tail time from Biquad coefficients
    Instead of using a fixed tail time of 200 ms for all biquad filters,
    we compute the actual tail time from filter coefficients.  Many
    typical filters in applications have a tail time of 10 ms or less.
    This allows biquads and downstream nodes to stop processing much sooner
    than what currently happens, reducing CPU usage and allowing GC to
    collect nodes sooner.
    
    BUG=612215
    TEST=
    
    Review-Url: https://codereview.chromium.org/2862373002
    Cr-Original-Commit-Position: refs/heads/master@{#475632}
    Committed: https://chromium.googlesource.com/chromium/src/+/0be695dcb495cade0ed5c4b70aba95eb3dce49bd
    Review-Url: https://codereview.chromium.org/2862373002
    Cr-Commit-Position: refs/heads/master@{#476113}

--- third_party/WebKit/Source/modules/webaudio/BiquadDSPKernel.h
+++ third_party/WebKit/Source/modules/webaudio/BiquadDSPKernel.h
@@ -41,7 +41,8 @@ class BiquadProcessor;
 class BiquadDSPKernel final : public AudioDSPKernel {
  public:
   explicit BiquadDSPKernel(BiquadProcessor* processor)
-      : AudioDSPKernel(processor) {}
+      : AudioDSPKernel(processor),
+        tail_time_(std::numeric_limits<double>::infinity()) {}
 
   // AudioDSPKernel
   void Process(const float* source,
@@ -76,8 +77,15 @@ class BiquadDSPKernel final : public AudioDSPKernel {
                           const float* detune);
 
  private:
+  // Compute the tail time using the BiquadFilter coefficients at
+  // index |coef_index|.
+  void UpdateTailTime(int coef_index);
+
   // Synchronize process() with getting and setting the filter coefficients.
   mutable Mutex process_lock_;
+
+  // The current tail time for biquad filter.
+  double tail_time_;
 };
 
 }  // namespace blink
