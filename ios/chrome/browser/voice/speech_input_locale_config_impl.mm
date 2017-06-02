b4b8274a83bec20625fe3d5c0c86cced540e4c06
lindsayw <lindsayw@chromium.org>
2017-06-02 12:12:14 -0700

    [ObjC ARC] Converts ios/chrome/browser/voice:voice to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2894623004
    Cr-Commit-Position: refs/heads/master@{#476745}

--- ios/chrome/browser/voice/speech_input_locale_config_impl.mm
+++ ios/chrome/browser/voice/speech_input_locale_config_impl.mm
@@ -17,6 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/public/provider/chrome/browser/voice/voice_search_language.h"
 #include "ios/public/provider/chrome/browser/voice/voice_search_provider.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace {
 
 // Returns the language portion of |locale_code|.
