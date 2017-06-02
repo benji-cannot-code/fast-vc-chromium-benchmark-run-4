7dc4694151cf597a13b1ff95a17d665904c88b7a
khmel <khmel@google.com>
2017-06-02 16:57:59 -0700

    arc: Provide metric to monitor Play Store window is shown time.
    This adds UMA to track time between initial ARC OptIn and moment
    when the first Play Store window is shown.
    
    BUG=728880
    TEST=Manually for managed/unmanaged accounts. Confirmed that
         metric is updted only for initial ARC boot.
    
    Change-Id: I2b988f039ec709a4d9c369a27ccd01777b688028
    Reviewed-on: https://chromium-review.googlesource.com/522442
    Commit-Queue: Yury Khmel <khmel@google.com>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#476874}

--- chrome/browser/chromeos/arc/arc_optin_uma.cc
+++ chrome/browser/chromeos/arc/arc_optin_uma.cc
@@ -12,6 +12,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace arc {
 
+namespace {
+
+std::string GetHistogramName(const std::string& base_name, bool managed) {
+  return base_name + (managed ? "Managed" : "Unmanaged");
+}
+
+}  // namespace
+
 void UpdateOptInActionUMA(OptInActionType type) {
   UMA_HISTOGRAM_ENUMERATION("Arc.OptInAction", static_cast<int>(type),
                             static_cast<int>(OptInActionType::SIZE));
@@ -33,10 +41,9 @@ void UpdateOptInFlowResultUMA(OptInFlowResult result) {
 
 void UpdateProvisioningResultUMA(ProvisioningResult result, bool managed) {
   DCHECK_NE(result, ProvisioningResult::CHROME_SERVER_COMMUNICATION_ERROR);
-  std::string histogram_name = "Arc.Provisioning.Result.";
-  histogram_name += managed ? "Managed" : "Unmanaged";
   base::LinearHistogram::FactoryGet(
-      histogram_name, 0, static_cast<int>(ProvisioningResult::SIZE),
+      GetHistogramName("Arc.Provisioning.Result.", managed), 0,
+      static_cast<int>(ProvisioningResult::SIZE),
       static_cast<int>(ProvisioningResult::SIZE) + 1,
       base::HistogramBase::kUmaTargetedHistogramFlag)
       ->Add(static_cast<int>(result));
@@ -47,15 +54,19 @@ void UpdateProvisioningTiming(const base::TimeDelta& elapsed_time,
                               bool managed) {
   std::string histogram_name = "Arc.Provisioning.TimeDelta.";
   histogram_name += success ? "Success." : "Failure.";
-  histogram_name += managed ? "Managed" : "Unmanaged";
   // The macro UMA_HISTOGRAM_CUSTOM_TIMES expects a constant string, but since
   // this measurement happens very infrequently, we do not need to use a macro
   // here.
-  base::Histogram::FactoryTimeGet(
-      histogram_name, base::TimeDelta::FromSeconds(1),
-      base::TimeDelta::FromMinutes(6), 50,
-      base::HistogramBase::kUmaTargetedHistogramFlag)
-      ->AddTime(elapsed_time);
+  base::UmaHistogramCustomTimes(GetHistogramName(histogram_name, managed),
+                                elapsed_time, base::TimeDelta::FromSeconds(1),
+                                base::TimeDelta::FromMinutes(6), 50);
+}
+
+void UpdatePlayStoreShowTime(const base::TimeDelta& elapsed_time,
+                             bool managed) {
+  base::UmaHistogramCustomTimes(
+      GetHistogramName("Arc.PlayStoreShown.TimeDelta.", managed), elapsed_time,
+      base::TimeDelta::FromSeconds(1), base::TimeDelta::FromMinutes(10), 50);
 }
 
 void UpdateAuthTiming(const char* histogram_name,
