394767dcb9d7709cb50ed81bc5f0605b045e28fc
malaykeshav <malaykeshav@chromium.org>
2017-06-02 11:43:26 -0700

    Sets the rotation source as USER when using the system display API
    The rotation preference is not persistent across reboot since we only
    store the preference if the rotation source is 'USER'. With the move to
    MD settings, we now use the System Display API to update any display
    settings, which still uses the source of rotation as ACTIVE.
    
    This change updates the source of rotation to USER. We persist the
    setting even if the change has been made by an application. (This is
    consistent with the other settings that are being set via this API.)
    
    BUG=716110
    COMPONENT=Display Info Provider, Chrome Settings
    
    Review-Url: https://codereview.chromium.org/2920703003
    Cr-Commit-Position: refs/heads/master@{#476734}

--- chrome/browser/extensions/display_info_provider_chromeos.cc
+++ chrome/browser/extensions/display_info_provider_chromeos.cc
@@ -525,7 +525,7 @@ bool DisplayInfoProviderChromeOS::SetInfo(
     } else {
       display_configuration_controller->SetDisplayRotation(
           display_id, DegreesToRotation(*info.rotation),
-          display::Display::ROTATION_SOURCE_ACTIVE);
+          display::Display::ROTATION_SOURCE_USER);
     }
   }
 
