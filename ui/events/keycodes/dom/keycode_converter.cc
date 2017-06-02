9f574fb4bd05a4c4a5700ce15bc6f91d54844069
zijiehe <zijiehe@chromium.org>
2017-06-01 20:14:47 -0700

    [System-Keyboard-Lock] Add KeycodeConverter::CodeStringToNativeKeycode() function
    Native keycode is used in system keyboard lock feature to understand the input
    requested keys from Javascript API.
    
    Also for consistency, CodeToUsbKeycode() has been renamed to
    CodeStringToUsbKeycode().
    
    You will find the usage of the newly added function in,
    https://codereview.chromium.org/2879033002/diff/510001/components/keyboard_lock/keyboard_lock_host.cc
    function KeyboardLockHost::SetReservedKeyCodes().
    
    For detail, please refer to the design doc at,
    https://docs.google.com/document/d/1T9gJHYdA1VGZ6QHQeOu0hOacWWWJ7yNEt1VDbLju4bs/edit#heading=h.cgwemqs2j4ta
    
    W3C Working Draft: https://garykac.github.io/system-keyboard-lock/
    Intent to implement: https://groups.google.com/a/chromium.org/forum/#!msg/blink-dev/9pauQUAvrcw/lfbG7eunCAAJ
    
    A prototype can be found at,
    https://codereview.chromium.org/2879033002
    
    BUG=680809
    
    Review-Url: https://codereview.chromium.org/2910873002
    Cr-Commit-Position: refs/heads/master@{#476544}

--- ui/events/keycodes/dom/keycode_converter.cc
+++ ui/events/keycodes/dom/keycode_converter.cc
@@ -76,6 +76,9 @@ int KeycodeConverter::InvalidNativeKeycode() {
   return usb_keycode_map[0].native_keycode;
 }
 
+// TODO(zijiehe): Most of the following functions can be optimized by using
+// either multiple arrays or unordered_map.
+
 // static
 DomCode KeycodeConverter::NativeKeycodeToDomCode(int native_keycode) {
   for (size_t i = 0; i < kKeycodeMapEntries; ++i) {
@@ -298,7 +301,7 @@ uint32_t KeycodeConverter::DomCodeToUsbKeycode(DomCode dom_code) {
 }
 
 // static
-uint32_t KeycodeConverter::CodeToUsbKeycode(const std::string& code) {
+uint32_t KeycodeConverter::CodeStringToUsbKeycode(const std::string& code) {
   if (code.empty())
     return InvalidUsbKeycode();
 
@@ -308,6 +311,11 @@ uint32_t KeycodeConverter::CodeToUsbKeycode(const std::string& code) {
     }
   }
   return InvalidUsbKeycode();
+}
+
+// static
+int KeycodeConverter::CodeStringToNativeKeycode(const std::string& code) {
+  return UsbKeycodeToNativeKeycode(CodeStringToUsbKeycode(code));
 }
 
 }  // namespace ui
--- ui/events/keycodes/dom/keycode_converter.h
+++ ui/events/keycodes/dom/keycode_converter.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef UI_EVENTS_KEYCODES_DOM4_KEYCODE_CONVERTER_H_
-#define UI_EVENTS_KEYCODES_DOM4_KEYCODE_CONVERTER_H_
+#ifndef UI_EVENTS_KEYCODES_DOM_KEYCODE_CONVERTER_H_
+#define UI_EVENTS_KEYCODES_DOM_KEYCODE_CONVERTER_H_
 
 #include <stddef.h>
 #include <stdint.h>
@@ -100,8 +100,11 @@ class KeycodeConverter {
   // Convert a DomCode into a USB keycode.
   static uint32_t DomCodeToUsbKeycode(DomCode dom_code);
 
-  // Convert a DOM3 Event |code| string into a USB keycode value.
-  static uint32_t CodeToUsbKeycode(const std::string& code);
+  // Convert a UI Event |code| string into a USB keycode value.
+  static uint32_t CodeStringToUsbKeycode(const std::string& code);
+
+  // Convert a UI Event |code| string into a native keycode.
+  static int CodeStringToNativeKeycode(const std::string& code);
 
   // Static methods to support testing.
   static size_t NumKeycodeMapEntriesForTest();
@@ -114,4 +117,4 @@ class KeycodeConverter {
 
 }  // namespace ui
 
-#endif  // UI_EVENTS_KEYCODES_DOM4_KEYCODE_CONVERTER_H_
+#endif  // UI_EVENTS_KEYCODES_DOM_KEYCODE_CONVERTER_H_
