fb8901b70580686da990cd591239175380f71daa
Majid Valipour <majidvp@chromium.org>
2017-06-02 13:30:31 -0400

    Make WorkletGlobalScope ActiveScriptWrappable
    This is a necessary pre-cursor to enable wrapper tracing for worklet global
    scopes. The worklet global scope wrapper is kept alive until its execution
    context is destroyed which happens during worklet shutdown.
    
    Bug: 726457
    Change-Id: Icd8c1a1be8fd3ba9fd37d0abb58b6297fd1a3b50
    Reviewed-on: https://chromium-review.googlesource.com/522169
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Majid Valipour <majidvp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477099}

--- third_party/WebKit/Source/core/workers/WorkletGlobalScope.h
+++ third_party/WebKit/Source/core/workers/WorkletGlobalScope.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/SecurityContext.h"
 #include "core/inspector/ConsoleMessage.h"
 #include "core/workers/WorkerOrWorkletGlobalScope.h"
+#include "platform/bindings/ActiveScriptWrappable.h"
 #include "platform/bindings/ScriptWrappable.h"
 #include "platform/heap/Handle.h"
 
@@ -23,7 +24,8 @@ class CORE_EXPORT WorkletGlobalScope
     : public GarbageCollectedFinalized<WorkletGlobalScope>,
       public SecurityContext,
       public WorkerOrWorkletGlobalScope,
-      public ScriptWrappable {
+      public ScriptWrappable,
+      public ActiveScriptWrappable<WorkletGlobalScope> {
   DEFINE_WRAPPERTYPEINFO();
   USING_GARBAGE_COLLECTED_MIXIN(WorkletGlobalScope);
 
@@ -48,6 +50,9 @@ class CORE_EXPORT WorkletGlobalScope
       v8::Isolate*,
       const WrapperTypeInfo*,
       v8::Local<v8::Object> wrapper) final;
+  bool HasPendingActivity() const override;
+
+  ExecutionContext* GetExecutionContext() const;
 
   // ExecutionContext
   String UserAgent() const final { return user_agent_; }
