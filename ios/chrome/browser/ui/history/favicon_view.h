345f99581edabf1d9a0255315717e7ac8d46883c
helenlyang <helenlyang@google.com>
2017-06-02 01:39:58 -0700

    Added a comment to the FaviconView class
    Review-Url: https://codereview.chromium.org/2914183004
    Cr-Commit-Position: refs/heads/master@{#476604}

--- ios/chrome/browser/ui/history/favicon_view.h
+++ ios/chrome/browser/ui/history/favicon_view.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
+// View for displaying the favicon of a history entry.
 @interface FaviconView : UIView
 
 // Size for the favicon.
