255f3c743e0d469a35a0219de18adc8d355b9f62
Fernando Serboncini <fserb@chromium.org>
2017-06-02 14:36:43 -0400

    Remove CSSTextCache from CSSPrimitiveValue
    CSSTextCache isn't ever cleared. Its values are stored keyed by
    elements not by value, so no reuse is possible. It really works as a
    very leaky way of caching CustomCSSText on the same object.
    
    There's no real world use case of requesting the same CSS value
    multiple times, while not caching gives us a small performance gain
    (due to not having to cache it) on single requests - which are 
    overwhelmingly more common -, plus we get a nice memory gain of
    not having those dangling strings forever there.
    
    Local performance test of dromaeo.jslibstylejquery (that gets/sets 
    CSS values on DOM objects) were on average 3% faster (most within 1
    stddev). The best tests was 15% faster (4 stddev), the 2nd best 8%
    faster (2 stddev). The worst test was 0.5 stddev below baseline.
    
    Bug: 724565
    Change-Id: I5c13566763d36bb40d37808ae38355b937baec9d
    Reviewed-on: https://chromium-review.googlesource.com/522107
    Commit-Queue: Fernando Serboncini <fserb@chromium.org>
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Reviewed-by: Eddy Mead <meade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477380}

--- third_party/WebKit/Source/core/css/CSSValue.h
+++ third_party/WebKit/Source/core/css/CSSValue.h
@@ -248,7 +248,6 @@ class CORE_EXPORT CSSValue : public GarbageCollectedFinalized<CSSValue> {
 
   explicit CSSValue(ClassType class_type)
       : primitive_unit_type_(0),
-        has_cached_css_text_(false),
         value_list_separator_(kSpaceSeparator),
         class_type_(class_type) {}
 
@@ -261,7 +260,6 @@ class CORE_EXPORT CSSValue : public GarbageCollectedFinalized<CSSValue> {
 
   // CSSPrimitiveValue bits:
   unsigned primitive_unit_type_ : 7;  // CSSPrimitiveValue::UnitType
-  mutable unsigned has_cached_css_text_ : 1;
 
   unsigned value_list_separator_ : kValueListSeparatorBits;
 
