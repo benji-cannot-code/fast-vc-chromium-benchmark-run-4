24c0ceda3d6e67e6214070f57b7443819cad9d08
Reza.Zakerinasab <zakerinasab@chromium.org>
2017-06-02 10:47:43 -0400

    Disable failing ImageBitmap unit tests on Android [Fix]
    A minor fix for disabling failing ImageBitmap unit tests on Android
    (follow up for chromium-review.googlesource.com/c/519582/).
    
    Bug: 721819
    Change-Id: I1766ffe135802d8a25565c90bc5eb032d62413a1
    Reviewed-on: https://chromium-review.googlesource.com/522625
    Reviewed-by: Mohammad Reza Zakerinasab <zakerinasab@chromium.org>
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#476679}

--- third_party/WebKit/Source/core/frame/ImageBitmapTest.cpp
+++ third_party/WebKit/Source/core/frame/ImageBitmapTest.cpp
@@ -357,7 +357,7 @@ TEST_F(ImageBitmapTest, MAYBE_ImageBitmapColorSpaceConversionHTMLImageElement) {
   ImageBitmapColorSpaceConversionImageBitmap
 #endif
 
-TEST_F(ImageBitmapTest, ImageBitmapColorSpaceConversionImageBitmap) {
+TEST_F(ImageBitmapTest, MAYBE_ImageBitmapColorSpaceConversionImageBitmap) {
   HTMLImageElement* image_element =
       HTMLImageElement::Create(*Document::Create());
 
@@ -476,7 +476,8 @@ TEST_F(ImageBitmapTest, ImageBitmapColorSpaceConversionImageBitmap) {
   ImageBitmapColorSpaceConversionStaticBitmapImage
 #endif
 
-TEST_F(ImageBitmapTest, ImageBitmapColorSpaceConversionStaticBitmapImage) {
+TEST_F(ImageBitmapTest,
+       MAYBE_ImageBitmapColorSpaceConversionStaticBitmapImage) {
   SkPaint p;
   p.setColor(SK_ColorRED);
   sk_sp<SkColorSpace> src_rgb_color_space = SkColorSpace::MakeSRGB();
