fa1d8ba4008a97ff5f329b7603116617b9576cb0
xiaochengh <xiaochengh@chromium.org>
2017-06-02 12:45:52 -0700

    Manage DOM and string offsets separately in TextIteratorTextState
    TextIteratorTextState stores only one offset range as both DOM offset and
    string offset, which is incorrect (e.g., when with text-transform).
    
    This patch changes TextIteratorTextState to store DOM and string offsets
    separately, so that the above issue no longer exist in the class (it still
    widely exists in many other components, though).
    
    This patch also makes it possible to stop using LayoutText in the class, so
    that TextIteratorTextState can also work with Layout NG.
    
    Note: The class originally has a member |text_start_offet_| as a hack when
    there is :first-letter, which is removed by the patch. A new member for
    string offset is added back, which has the same name by pure coincidence.
    
    BUG=721957
    TEST=n/a; no behavioral change
    
    Review-Url: https://codereview.chromium.org/2914313002
    Cr-Commit-Position: refs/heads/master@{#476759}

--- third_party/WebKit/Source/core/editing/iterators/TextIterator.cpp
+++ third_party/WebKit/Source/core/editing/iterators/TextIterator.cpp
@@ -876,7 +876,7 @@ template <typename Strategy>
 int TextIteratorAlgorithm<Strategy>::StartOffsetInCurrentContainer() const {
   if (text_state_->PositionNode()) {
     text_state_->FlushPositionOffsets();
-    return text_state_->PositionStartOffset() + text_state_->TextStartOffset();
+    return text_state_->PositionStartOffset();
   }
   DCHECK(end_container_);
   return end_offset_;
@@ -886,7 +886,7 @@ template <typename Strategy>
 int TextIteratorAlgorithm<Strategy>::EndOffsetInCurrentContainer() const {
   if (text_state_->PositionNode()) {
     text_state_->FlushPositionOffsets();
-    return text_state_->PositionEndOffset() + text_state_->TextStartOffset();
+    return text_state_->PositionEndOffset();
   }
   DCHECK(end_container_);
   return end_offset_;
