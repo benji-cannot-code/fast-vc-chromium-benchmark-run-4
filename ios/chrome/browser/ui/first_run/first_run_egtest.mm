6c809616ce515abc4482806fca85cc84324c2fdb
jlebel <jlebel@chromium.org>
2017-06-02 09:40:46 -0700

    Making the gradient click-through in ChromeSigninViewController
    "Settings" button is partially below the gradient while using small screen
    like iPhone SE. This is probably the reason why testSignInOpenSettings
    and testSignInAndTapSettingsLink can be flaky.
    
    Also, updating those 2 tests to make it more robust to animation by
    adding calls to WaitForMatcher(), to wait for "Settings" to be displayed.
    
    BUG=718023
    
    Review-Url: https://codereview.chromium.org/2906313004
    Cr-Commit-Position: refs/heads/master@{#476691}

--- ios/chrome/browser/ui/first_run/first_run_egtest.mm
+++ ios/chrome/browser/ui/first_run/first_run_egtest.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <XCTest/XCTest.h>
 
 #include "base/strings/sys_string_conversions.h"
+#import "base/test/ios/wait_util.h"
 #include "components/metrics/metrics_pref_names.h"
 #include "components/metrics/metrics_reporting_default_state.h"
 #include "components/prefs/pref_member.h"
@@ -29,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
 #import "ios/public/provider/chrome/browser/signin/fake_chrome_identity.h"
 #import "ios/public/provider/chrome/browser/signin/fake_chrome_identity_service.h"
+#import "ios/testing/wait_util.h"
 #include "ui/base/l10n/l10n_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -86,6 +88,19 @@ void AssertAuthenticatedIdentityInActiveProfile(ChromeIdentity* identity) {
                   @"Unexpected Gaia ID of the signed in user [expected = "
                   @"\"%@\", actual = \"%s\"]",
                   identity.gaiaID, info.gaia.c_str());
+}
+
+// Wait until |matcher| is accessible (not nil)
+void WaitForMatcher(id<GREYMatcher> matcher) {
+  ConditionBlock condition = ^{
+    NSError* error = nil;
+    [[EarlGrey selectElementWithMatcher:matcher] assertWithMatcher:grey_notNil()
+                                                             error:&error];
+    return error == nil;
+  };
+  GREYAssert(testing::WaitUntilConditionOrTimeout(
+                 testing::kWaitForUIElementTimeout, condition),
+             @"Waiting for matcher %@ failed.", matcher);
 }
 }
 
@@ -217,8 +232,7 @@ void AssertAuthenticatedIdentityInActiveProfile(ChromeIdentity* identity) {
 }
 
 // Signs in to an account and then taps the Advanced link to go to settings.
-// TODO(crbug.com/718023): Re-enable test.
-- (void)DISABLED_testSignInAndTapSettingsLink {
+- (void)testSignInAndTapSettingsLink {
   ChromeIdentity* identity = GetFakeIdentity();
   ios::FakeChromeIdentityService::GetInstanceFromChromeProvider()->AddIdentity(
       identity);
@@ -236,6 +250,7 @@ void AssertAuthenticatedIdentityInActiveProfile(ChromeIdentity* identity) {
   // Tap Settings link.
   id<GREYMatcher> settings_link_matcher = grey_allOf(
       grey_accessibilityLabel(@"Settings"), grey_sufficientlyVisible(), nil);
+  WaitForMatcher(settings_link_matcher);
   [[EarlGrey selectElementWithMatcher:settings_link_matcher]
       performAction:grey_tap()];
 
