cb0b6e5a8ac36533baef67b4a1dadf29b0016faa
pmonette <pmonette@chromium.org>
2017-06-01 19:28:01 -0700

    Pass the path mapping via RefCountedData in ModuleInspector
    The mapping was getting copied every invocation of InspectModule, which
    is pretty inefficient.
    
    Review-Url: https://codereview.chromium.org/2916663002
    Cr-Commit-Position: refs/heads/master@{#476525}

--- chrome/browser/conflicts/module_inspector_win.h
+++ chrome/browser/conflicts/module_inspector_win.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/macros.h"
+#include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
 #include "base/task_scheduler/task_traits.h"
 #include "base/threading/thread_checker.h"
@@ -68,7 +69,7 @@ class ModuleInspector {
   // The vector of paths to %env_var%, used to account for differences in
   // localization and where people keep their files.
   // e.g. c:\windows vs d:\windows
-  StringMapping path_mapping_;
+  scoped_refptr<base::RefCountedData<StringMapping>> path_mapping_;
 
   base::ThreadChecker thread_checker_;
 
