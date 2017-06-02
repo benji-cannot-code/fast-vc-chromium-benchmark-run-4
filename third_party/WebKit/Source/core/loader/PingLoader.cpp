6c417c506c388cf62d70698db792a7b706fc9874
Takeshi Yoshino <tyoshino@chromium.org>
2017-06-02 15:51:02 +0900

    Change the CHECK in PingLoaderImpl destructor to DCHECK
    Bug: 662769
    Change-Id: I273e82bc46447a407f5129b759b1ed5936a2cab5
    Reviewed-on: https://chromium-review.googlesource.com/517549
    Commit-Queue: Takeshi Yoshino <tyoshino@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#476606}

--- third_party/WebKit/Source/core/loader/PingLoader.cpp
+++ third_party/WebKit/Source/core/loader/PingLoader.cpp
@@ -283,8 +283,7 @@ PingLoaderImpl::PingLoaderImpl(LocalFrame* frame,
 }
 
 PingLoaderImpl::~PingLoaderImpl() {
-  // TODO(kinuko): Turn this to DCHECK once crbug.com/662769 is resolved.
-  CHECK(!loader_);
+  DCHECK(!loader_);
 }
 
 void PingLoaderImpl::Dispose() {
