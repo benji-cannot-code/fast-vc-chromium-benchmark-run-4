340e7b2380d4f061f7c718e60c8908844a1de26d
tbansal <tbansal@chromium.org>
2017-06-02 00:00:38 -0700

    NetInfo network quality extension: Add callbacks and Layout tests
    "change" event is fired from NetworkInformation.cpp when there
    is a change in the network quality.
    
    Also, layout tests have been added for the network quality extension
    to the NetInfo API.
    
    BUG=725282, 726762
    
    Review-Url: https://codereview.chromium.org/2903493002
    Cr-Commit-Position: refs/heads/master@{#476595}

--- third_party/WebKit/LayoutTests/netinfo/resources/web-worker.js
+++ third_party/WebKit/LayoutTests/netinfo/resources/web-worker.js
@@ -1,8 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 addEventListener('message', function(e) {
-    self.postMessage(navigator.connection.type + ',' + navigator.connection.downlinkMax);
+    self.postMessage(navigator.connection.type + ',' + navigator.connection.downlinkMax + ',' + navigator.connection.effectiveType + ',' + navigator.connection.rtt + ',' + navigator.connection.downlink);
 }, false);
 
 navigator.connection.addEventListener('change', function() {
-    self.postMessage(navigator.connection.type + ',' + navigator.connection.downlinkMax);
+    self.postMessage(navigator.connection.type + ',' + navigator.connection.downlinkMax + ',' + navigator.connection.effectiveType + ',' + navigator.connection.rtt + ',' + navigator.connection.downlink);
 }, false);
--- third_party/WebKit/Source/core/html/HTMLMediaElementTest.cpp
+++ third_party/WebKit/Source/core/html/HTMLMediaElementTest.cpp
@@ -126,7 +126,7 @@ TEST_P(HTMLMediaElementTest, preloadType) {
     Media()->GetDocument().GetSettings()->SetForcePreloadNoneForMediaElements(
         data.force_preload_none_for_media_elements);
     if (data.is_cellular) {
-      GetNetworkStateNotifier().SetOverride(
+      GetNetworkStateNotifier().SetNetworkConnectionInfoOverride(
           true, WebConnectionType::kWebConnectionTypeCellular3G, 2.0);
     } else {
       GetNetworkStateNotifier().ClearOverride();
