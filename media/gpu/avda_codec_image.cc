dc9274a7519239066431595805ee7e284c4b0a66
tguilbert <tguilbert@chromium.org>
2017-06-01 19:18:12 -0700

    Reset |most_recent_bounds_| on new Overlay
    When overlays are enabled, on Nexus 5 devices, upon exiting and
    re-entering fullscreen, nothing is displayed on screen.
    
    This is caused by the fact that AVDACodecImage never resets
    |most_recent_bounds_| when a new overlay is received. This means
    AndroidOverlay::ScheduleLayout() is never called in
    AVDACodecImage::ScheduleOverlayPlane(), and new overlays retains their
    initial bounds of [0,0,1,1].
    
    This CL fixes the issue by reseting the bounds when a new shared state
    (and therefore potential new Overlay) is set.
    
    BUG=728802
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2916193002
    Cr-Commit-Position: refs/heads/master@{#476524}

--- media/gpu/avda_codec_image.cc
+++ media/gpu/avda_codec_image.cc
@@ -149,6 +149,7 @@ bool AVDACodecImage::SetSharedState(
   if (shared_state == shared_state_)
     return false;
   shared_state_ = shared_state;
+  most_recent_bounds_ = gfx::Rect();
   return true;
 }
 
