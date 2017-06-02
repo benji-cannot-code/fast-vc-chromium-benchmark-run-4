d39d7724de2151e5c4d8b3a46f865e490ef14e14
Alexei Filippov <alph@chromium.org>
2017-06-01 20:59:06 -0700

    DevTools: Promisify IndexedDB protocol domain
    Bug: 718063
    Change-Id: I1ea52c362e58cfc57727062fadd9d184c545f858
    Reviewed-on: https://chromium-review.googlesource.com/520665
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#476584}

--- third_party/WebKit/Source/devtools/front_end/resources/ApplicationPanelSidebar.js
+++ third_party/WebKit/Source/devtools/front_end/resources/ApplicationPanelSidebar.js
@@ -1300,14 +1300,9 @@ Resources.IDBObjectStoreTreeElement = class extends Resources.BaseStorageTreeEle
     contextMenu.show();
   }
 
-  _clearObjectStore() {
-    /**
-     * @this {Resources.IDBObjectStoreTreeElement}
-     */
-    function callback() {
-      this.update(this._objectStore);
-    }
-    this._model.clearObjectStore(this._databaseId, this._objectStore.name, callback.bind(this));
+  async _clearObjectStore() {
+    await this._model.clearObjectStore(this._databaseId, this._objectStore.name);
+    this.update(this._objectStore);
   }
 
   /**
