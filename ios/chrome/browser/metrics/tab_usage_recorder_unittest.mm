fd18ece0f47de3116b948417bc8278acde3585fc
kkhorimoto <kkhorimoto@chromium.org>
2017-06-01 19:36:35 -0700

    Remove usage of Tab's |url| property from TabUsageRecorder.
    BUG=546208
    
    Review-Url: https://codereview.chromium.org/2820763002
    Cr-Commit-Position: refs/heads/master@{#476528}

--- ios/chrome/browser/metrics/tab_usage_recorder_unittest.mm
+++ ios/chrome/browser/metrics/tab_usage_recorder_unittest.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/memory/ptr_util.h"
 #include "base/message_loop/message_loop.h"
 #include "base/metrics/histogram_samples.h"
 #include "base/stl_util.h"
@@ -14,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/metrics/tab_usage_recorder_delegate.h"
 #import "ios/chrome/browser/tabs/tab.h"
 #include "ios/testing/ocmock_complex_type_helper.h"
+#import "ios/web/public/test/fakes/test_navigation_manager.h"
+#import "ios/web/public/test/fakes/test_web_state.h"
 #include "ios/web/public/test/test_web_thread.h"
 #import "ios/web/web_state/ui/crw_web_controller.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -26,9 +29,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface TURTestTabMock : OCMockComplexTypeHelper {
   GURL _url;
+  GURL _lastCommittedURL;
+  GURL _visibleURL;
+  web::TestWebState _webState;
 }
 
 @property(nonatomic, assign) const GURL& url;
+@property(nonatomic, assign) const GURL& lastCommittedURL;
+@property(nonatomic, assign) const GURL& visibleURL;
+@property(nonatomic, readonly) web::WebState* webState;
+
 @end
 
 @implementation TURTestTabMock
@@ -37,6 +47,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 - (void)setUrl:(const GURL&)url {
   _url = url;
+}
+- (const GURL&)lastCommittedURL {
+  return _lastCommittedURL;
+}
+- (void)setLastCommittedURL:(const GURL&)lastCommittedURL {
+  _lastCommittedURL = lastCommittedURL;
+}
+- (const GURL&)visibleURL {
+  return _visibleURL;
+}
+- (void)setVisibleURL:(const GURL&)visibleURL {
+  _visibleURL = visibleURL;
+}
+- (web::WebState*)webState {
+  if (!_webState.GetNavigationManager()) {
+    _webState.SetNavigationManager(
+        base::MakeUnique<web::TestNavigationManager>());
+  }
+  return &_webState;
 }
 @end
 
@@ -107,6 +136,8 @@ class TabUsageRecorderTest : public PlatformTest {
     [[[tab_mock stub] andReturn:web_controller_mock] webController];
     [[[tab_mock stub] andReturnBool:false] isPrerenderTab];
     [tab_mock setUrl:webUrl_];
+    [tab_mock setLastCommittedURL:webUrl_];
+    [tab_mock setVisibleURL:webUrl_];
     [[[web_controller_mock stub] andReturnBool:inMemory] isViewAlive];
     [[web_controller_mock stub] removeObserver:OCMOCK_ANY];
     return tab_mock;
@@ -174,7 +205,11 @@ TEST_F(TabUsageRecorderTest, CountNativePageLoadsBeforeEvictedTab) {
   id tab_mock_a = MockTab(true);
   id tab_mock_b = MockTab(false);
   [tab_mock_a setUrl:nativeUrl_];
+  [tab_mock_a setLastCommittedURL:nativeUrl_];
+  [tab_mock_a setVisibleURL:nativeUrl_];
   [tab_mock_b setUrl:nativeUrl_];
+  [tab_mock_b setLastCommittedURL:nativeUrl_];
+  [tab_mock_b setVisibleURL:nativeUrl_];
 
   // Call reload an arbitrary number of times.
   const int kNumReloads = 4;
