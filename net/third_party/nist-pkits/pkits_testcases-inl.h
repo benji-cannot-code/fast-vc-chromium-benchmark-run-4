ff5f3f82f05c969a3bb74a43b4b7634f5b54b6bc
yoichio <yoichio@chromium.org>
2017-06-01 21:29:42 -0700

    Revert of Add policies support to VerifyCertificateChain(). (patchset #9 id:160001 of https://codereview.chromium.org/2903283002/ )
    Reason for revert:
    This patch causes VerifyCertificateChain test failure on linux:
    https://uberchromegw.corp.google.com/i/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%28dbg%29%281%29/builds/26805
    
    Original issue's description:
    > Add policies support to VerifyCertificateChain().
    >
    > Support is compliant with RFC 5280 and supports all the policy
    > extensions specified therein:
    >
    >  * Inhibit Any Policy
    >  * Policy Constraints
    >  * Policies
    >  * Policy Mappings
    >
    > Testing is done solely using the PKITS test suite, which has fairly good
    > coverage of these extensions:
    >
    >   4.8 (Certificate Policies)
    >   4.9 (Require Explicit Policy)
    >   4.10 (Policy Mappings)
    >   4.11 (Inhibit Policy Mapping)
    >   4.12 (Inhibit Any Policy)
    >
    > BUG=634456,634453,634452
    >
    > Review-Url: https://codereview.chromium.org/2903283002
    > Cr-Commit-Position: refs/heads/master@{#476513}
    > Committed: https://chromium.googlesource.com/chromium/src/+/bcca0368ac116828cecc6c68a381bfc5147f8c98
    
    TBR=mattm@chromium.org,eroman@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=634456,634453,634452
    
    Review-Url: https://codereview.chromium.org/2918063002
    Cr-Commit-Position: refs/heads/master@{#476562}

--- net/third_party/nist-pkits/pkits_testcases-inl.h
+++ net/third_party/nist-pkits/pkits_testcases-inl.h
@@ -2114,7 +2114,6 @@ WRAPPED_TYPED_TEST_P(PkitsTest10PolicyMappings,
                               "MappingFromanyPolicyCACRL"};
   PkitsTestInfo info;
   info.should_validate = false;
-  info.SetUserConstrainedPolicySet("");
 
   this->RunTest(certs, crls, info);
 }
