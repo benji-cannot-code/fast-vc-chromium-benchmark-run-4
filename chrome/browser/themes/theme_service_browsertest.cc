88fa9c86da00a6e6293947752ae4f6e92fa82da7
estade <estade@chromium.org>
2017-06-01 17:59:42 -0700

    Unpack theme data from extensions off of UI thread.
    Even on a beefy workstation, resizing theme images takes a long time
    (>1s). This causes a painful hiccup in the UI. This patch addresses that
    by moving the operation to a helper thread. The actual application of
    the theme won't be any faster but it won't block interaction.
    
    As a future area of investigation it might be worth changing the "theme
    installed" infobar to show earlier with a message like "installing
    theme..." until this operation is completed, since otherwise it can feel
    like nothing is happening.
    
    BUG=316070
    
    Review-Url: https://codereview.chromium.org/2799003002
    Cr-Commit-Position: refs/heads/master@{#476505}

--- chrome/browser/themes/theme_service_browsertest.cc
+++ chrome/browser/themes/theme_service_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/threading/sequenced_worker_pool.h"
 #include "base/threading/thread_restrictions.h"
+#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/extensions/component_loader.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
 #include "chrome/browser/profiles/profile.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/common/pref_names.h"
 #include "components/prefs/pref_service.h"
+#include "content/public/test/test_utils.h"
 
 namespace {
 
@@ -60,6 +62,14 @@ IN_PROC_BROWSER_TEST_F(ThemeServiceBrowserTest, PRE_ThemeDataPackInvalid) {
             profile->GetPrefs()->GetFilePath(prefs::kCurrentThemePackFilename));
 
   InstallExtension(test_data_dir_.AppendASCII("theme"), 1);
+  // The theme isn't installed synchronously.
+  EXPECT_FALSE(UsingCustomTheme(*theme_service));
+
+  // Wait for the theme to be loaded.
+  content::WindowedNotificationObserver theme_change_observer(
+      chrome::NOTIFICATION_BROWSER_THEME_CHANGED,
+      content::Source<ThemeService>(theme_service));
+  theme_change_observer.Wait();
 
   // Check that the theme was installed.
   EXPECT_TRUE(UsingCustomTheme(*theme_service));
