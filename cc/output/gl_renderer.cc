0328470e894a65e89ed09c05730a4cc267b2ca57
aelias <aelias@chromium.org>
2017-06-02 11:55:54 -0700

    Call glReleaseShaderCompiler when GLRenderer loses visibility.
    This call is a hint that the driver should release RAM caches that it
    may have built to optimize shader compilation from strings.  We should
    call it to save RAM when we expect that we will not be compiling any
    shaders anytime soon.  The browser going into background is a simple,
    deterministic time where we can establish that.
    
    Notes:
    - On Android, visibility loss results in GLRenderer (and
    Display) destruction instead of a SetVisibility call, so also add a
    call in CleanupSharedObjects().
    - I plan to hook up ANGLE ShaderTranslatorCache clearing to this signal
    in a later patch, as mentioned in the TODO in shader_translator_cache.h.
    
    BUG=725306,725303
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2916263002
    Cr-Commit-Position: refs/heads/master@{#476740}

--- cc/output/gl_renderer.cc
+++ cc/output/gl_renderer.cc
@@ -440,6 +440,7 @@ void GLRenderer::DidChangeVisibility() {
     TRACE_EVENT0("cc", "GLRenderer::DidChangeVisibility dropping resources");
     ReleaseRenderPassTextures();
     output_surface_->DiscardBackbuffer();
+    gl_->ReleaseShaderCompiler();
   }
 
   PrepareGeometry(NO_BINDING);
@@ -3097,6 +3098,7 @@ const gfx::ColorTransform* GLRenderer::GetColorTransform(
 void GLRenderer::CleanupSharedObjects() {
   shared_geometry_ = nullptr;
 
+  gl_->ReleaseShaderCompiler();
   for (auto& iter : program_cache_)
     iter.second->Cleanup(gl_);
   program_cache_.clear();
