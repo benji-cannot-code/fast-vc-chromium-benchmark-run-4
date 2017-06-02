7fa19a5ae34fe19eb015ba337cb0785c4d9a4de3
michaelbai <michaelbai@chromium.org>
2017-06-02 11:13:49 -0700

    Revert "Track GVR version for each headset type using UMA."
    This reverts commit 92c0c2fb929a969c0ca9b7626e302360ece09f83.
    
    BUG=706436
    
    TBR=tiborg, billorr, amp, tedchoc, rkaplow, ddorwin, mthiesse
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2917113002
    Cr-Commit-Position: refs/heads/master@{#476725}

--- chrome/browser/android/vr_shell/vr_shell_delegate.cc
+++ chrome/browser/android/vr_shell/vr_shell_delegate.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_android.h"
 #include "base/callback_helpers.h"
 #include "chrome/browser/android/vr_shell/non_presenting_gvr_delegate.h"
-#include "chrome/browser/android/vr_shell/vr_metrics_util.h"
 #include "device/vr/android/gvr/gvr_delegate.h"
 #include "device/vr/android/gvr/gvr_device.h"
 #include "device/vr/android/gvr/gvr_device_provider.h"
@@ -18,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using base::android::JavaParamRef;
 using base::android::AttachCurrentThread;
-using base::android::ScopedJavaLocalRef;
 
 namespace vr_shell {
 
@@ -41,7 +39,8 @@ VrShellDelegate::~VrShellDelegate() {
 
 device::GvrDelegateProvider* VrShellDelegate::CreateVrShellDelegate() {
   JNIEnv* env = AttachCurrentThread();
-  ScopedJavaLocalRef<jobject> jdelegate = Java_VrShellDelegate_getInstance(env);
+  base::android::ScopedJavaLocalRef<jobject> jdelegate =
+      Java_VrShellDelegate_getInstance(env);
   if (!jdelegate.is_null())
     return GetNativeVrShellDelegate(env, jdelegate.obj());
   return nullptr;
@@ -58,9 +57,9 @@ void VrShellDelegate::SetPresentingDelegate(
     gvr_context* context) {
   presenting_delegate_ = delegate;
   // Clean up the non-presenting delegate.
-  JNIEnv* env = AttachCurrentThread();
   if (presenting_delegate_ && non_presenting_delegate_) {
     non_presenting_delegate_ = nullptr;
+    JNIEnv* env = AttachCurrentThread();
     Java_VrShellDelegate_shutdownNonPresentingNativeContext(
         env, j_vr_shell_delegate_.obj());
   }
@@ -76,9 +75,6 @@ void VrShellDelegate::SetPresentingDelegate(
     base::ResetAndReturn(&present_callback_).Run(true);
     pending_successful_present_request_ = false;
   }
-
-  std::unique_ptr<VrCoreInfo> vr_core_info = MakeVrCoreInfo(env);
-  VrMetricsUtil::LogGvrVersionForVrViewerType(context, *vr_core_info);
 }
 
 void VrShellDelegate::RemoveDelegate() {
@@ -210,11 +206,6 @@ void VrShellDelegate::ExitWebVRPresent() {
   }
 }
 
-std::unique_ptr<VrCoreInfo> VrShellDelegate::MakeVrCoreInfo(JNIEnv* env) {
-  return base::WrapUnique(reinterpret_cast<VrCoreInfo*>(
-      Java_VrShellDelegate_getVrCoreInfo(env, j_vr_shell_delegate_.obj())));
-}
-
 void VrShellDelegate::CreateNonPresentingDelegate() {
   JNIEnv* env = AttachCurrentThread();
   gvr_context* context = reinterpret_cast<gvr_context*>(
@@ -224,8 +215,6 @@ void VrShellDelegate::CreateNonPresentingDelegate() {
       base::MakeUnique<NonPresentingGvrDelegate>(context);
   non_presenting_delegate_->UpdateVSyncInterval(timebase_nanos_,
                                                 interval_seconds_);
-  std::unique_ptr<VrCoreInfo> vr_core_info = MakeVrCoreInfo(env);
-  VrMetricsUtil::LogGvrVersionForVrViewerType(context, *vr_core_info);
 }
 
 void VrShellDelegate::OnActivateDisplayHandled(bool will_not_present) {
