b6b0e37efd3fed651044346ef52768373afc71ae
Mathieu Perreault <mathp@chromium.org>
2017-06-01 20:06:04 -0400

    [Payments] Link to Autofill settings from Payment Request UI
    Bug: 728873
    Test: Manual
    Change-Id: Iaad231a4765de170d9ac287ab89e08a3833b8c45
    Reviewed-on: https://chromium-review.googlesource.com/521910
    Reviewed-by: Anthony Vallee-Dubois <anthonyvd@chromium.org>
    Commit-Queue: Anthony Vallee-Dubois <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#476629}

--- chrome/browser/ui/views/payments/payment_sheet_view_controller.cc
+++ chrome/browser/ui/views/payments/payment_sheet_view_controller.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/payments/payment_request_dialog_view_ids.h"
 #include "chrome/browser/ui/views/payments/payment_request_row_view.h"
 #include "chrome/browser/ui/views/payments/payment_request_views_util.h"
+#include "chrome/common/url_constants.h"
 #include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/browser/personal_data_manager.h"
 #include "components/payments/content/payment_request_spec.h"
@@ -514,7 +515,8 @@ void PaymentSheetViewController::StyledLabelLinkClicked(
     int event_flags) {
   // The only thing that can trigger this is the user clicking on the "settings"
   // link in the data attribution text.
-  chrome::ShowSettingsSubPageForProfile(dialog()->GetProfile(), "");
+  chrome::ShowSettingsSubPageForProfile(dialog()->GetProfile(),
+                                        chrome::kAutofillSubPage);
 }
 
 void PaymentSheetViewController::UpdatePayButtonState(bool enabled) {
