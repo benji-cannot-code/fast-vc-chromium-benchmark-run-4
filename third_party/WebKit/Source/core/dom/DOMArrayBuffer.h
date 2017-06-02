53e5331ddc0129330c03c05a79039205017219d7
Florin Malita <fmalita@chromium.org>
2017-06-02 15:39:10 -0400

    Add a DOMArrayBuffer factory for segmented SharedBuffers
    Convert existing clients to use the new factory and avoid flattening
    the SharedBuffer data.
    
    BUG=728627
    
    Change-Id: If39410b9a70c1a60a760e7dfbf2246a6cf8bf45b
    Reviewed-on: https://chromium-review.googlesource.com/523082
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#476930}

--- third_party/WebKit/Source/core/dom/DOMArrayBuffer.h
+++ third_party/WebKit/Source/core/dom/DOMArrayBuffer.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class SharedBuffer;
+
 class CORE_EXPORT DOMArrayBuffer final : public DOMArrayBufferBase {
   DEFINE_WRAPPERTYPEINFO();
 
@@ -29,6 +31,7 @@ class CORE_EXPORT DOMArrayBuffer final : public DOMArrayBufferBase {
   static DOMArrayBuffer* Create(WTF::ArrayBufferContents& contents) {
     return Create(WTF::ArrayBuffer::Create(contents));
   }
+  static DOMArrayBuffer* Create(PassRefPtr<SharedBuffer>);
 
   // Only for use by XMLHttpRequest::responseArrayBuffer and
   // Internals::serializeObject.
