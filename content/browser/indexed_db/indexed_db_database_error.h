f9d1145607c70d6a5226f0efc6c079b193b16925
Daniel Murphy <dmurph@chromium.org>
2017-06-02 12:00:30 -0700

    [IndexedDB] Fixed indexeddb user abort reporting
    Change-Id: I7a7b47c7dc10664bd96202c3f44a6fdbaeed03d7
    Reviewed-on: https://chromium-review.googlesource.com/522362
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#476800}

--- content/browser/indexed_db/indexed_db_database_error.h
+++ content/browser/indexed_db/indexed_db_database_error.h
@@ -10,10 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/string16.h"
 #include "base/strings/utf_string_conversions.h"
+#include "content/common/content_export.h"
 
 namespace content {
 
-class IndexedDBDatabaseError {
+class CONTENT_EXPORT IndexedDBDatabaseError {
  public:
   IndexedDBDatabaseError();
   explicit IndexedDBDatabaseError(uint16_t code);
