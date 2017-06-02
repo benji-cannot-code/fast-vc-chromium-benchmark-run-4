8bd94fd959ab70e8223c882c00d9dbf42e9ccc4b
Chris Watkins <watk@chromium.org>
2017-06-02 12:15:33 -0700

    media: Add CodecImage; a GLImage based on AVDACodecImage
    CodecImage is the AVDACodecImage equivalent for MediaCodecVideoDecoder.
    The main way in which it's different is that it won't be reused to back
    different video frames. This lets us simplify it by relying on two
    things: the CodecOutputBuffer backing it won't change, and the surface
    backing it won't change.
    
    It also comes with unit tests.
    
    Bug: 660942
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ie04ac81298bf1d2784a17a3f6a4db5ef69a97268
    Reviewed-on: https://chromium-review.googlesource.com/522230
    Commit-Queue: Chris Watkins <watk@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#476818}

--- media/gpu/mock_surface_texture_gl_owner.h
+++ media/gpu/mock_surface_texture_gl_owner.h
@@ -9,12 +9,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/surface_texture_gl_owner.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/gl/gl_bindings.h"
+#include "ui/gl/gl_context.h"
+#include "ui/gl/gl_surface.h"
 
 namespace media {
 
+// This is a mock with a small amount of fake functionality too.
 class MockSurfaceTextureGLOwner : public SurfaceTextureGLOwner {
  public:
-  MockSurfaceTextureGLOwner();
+  MockSurfaceTextureGLOwner(GLuint fake_texture_id,
+                            gl::GLContext* fake_context,
+                            gl::GLSurface* fake_surface);
+
   MOCK_CONST_METHOD0(GetTextureId, GLuint());
   MOCK_CONST_METHOD0(GetContext, gl::GLContext*());
   MOCK_CONST_METHOD0(GetSurface, gl::GLSurface*());
@@ -26,6 +33,17 @@ class MockSurfaceTextureGLOwner : public SurfaceTextureGLOwner {
   MOCK_METHOD0(IgnorePendingRelease, void());
   MOCK_METHOD0(IsExpectingFrameAvailable, bool());
   MOCK_METHOD0(WaitForFrameAvailable, void());
+
+  // Fake implementations that the mocks will call by default.
+  void FakeSetReleaseTimeToNow() { expecting_frame_available = true; }
+  void FakeIgnorePendingRelease() { expecting_frame_available = false; }
+  bool FakeIsExpectingFrameAvailable() { return expecting_frame_available; }
+  void FakeWaitForFrameAvailable() { expecting_frame_available = false; }
+
+  GLuint fake_texture_id;
+  gl::GLContext* fake_context;
+  gl::GLSurface* fake_surface;
+  bool expecting_frame_available;
 
  protected:
   ~MockSurfaceTextureGLOwner();
