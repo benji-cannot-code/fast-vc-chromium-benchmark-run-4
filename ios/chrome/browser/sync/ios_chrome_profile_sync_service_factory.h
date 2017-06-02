f2c8a68f2fc33ec8c0064cb8052f1a40af2bd835
skym <skym@chromium.org>
2017-06-01 18:31:39 -0700

    [Sync] Create iOS factory for UserEventService.
    This is essentially a clone of UserEventServiceFactory that uses
    web::BrowserState to be iOS compatible.
    
    BUG=701032
    
    Review-Url: https://codereview.chromium.org/2916873003
    Cr-Commit-Position: refs/heads/master@{#476515}

--- ios/chrome/browser/sync/ios_chrome_profile_sync_service_factory.h
+++ ios/chrome/browser/sync/ios_chrome_profile_sync_service_factory.h
@@ -44,7 +44,7 @@ class IOSChromeProfileSyncServiceFactory
   IOSChromeProfileSyncServiceFactory();
   ~IOSChromeProfileSyncServiceFactory() override;
 
-  // BrowserContextKeyedServiceFactory:
+  // BrowserStateKeyedServiceFactory implementation.
   std::unique_ptr<KeyedService> BuildServiceInstanceFor(
       web::BrowserState* context) const override;
 };
