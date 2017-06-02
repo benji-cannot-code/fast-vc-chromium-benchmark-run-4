22f83f31bb1b382ba2999959789f94d9d91f15bd
Lucas Furukawa Gadani <lfg@chromium.org>
2017-06-02 16:13:09 -0400

    Add DCHECKS to prevent incorrect construction of WebMouseEvent.
    Bug: 718079
    Change-Id: I2f315038d0fb748d5c770daad9ed335b3cb1b364
    Reviewed-on: https://chromium-review.googlesource.com/517730
    Commit-Queue: Lucas Furukawa Gadani <lfg@chromium.org>
    Reviewed-by: David Tapuska <dtapuska@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#476813}

--- third_party/WebKit/public/platform/WebMouseEvent.h
+++ third_party/WebKit/public/platform/WebMouseEvent.h
@@ -42,7 +42,10 @@ class WebMouseEvent : public WebInputEvent, public WebPointerProperties {
                              PointerType::kUnknown,
                              Button::kNoButton,
                              WebFloatPoint(x_param, y_param),
-                             WebFloatPoint(global_x_param, global_y_param)) {}
+                             WebFloatPoint(global_x_param, global_y_param)) {
+    DCHECK_GE(type_param, kMouseTypeFirst);
+    DCHECK_LE(type_param, kMouseTypeLast);
+  }
 
   WebMouseEvent(Type type_param,
                 WebFloatPoint position,
@@ -62,7 +65,10 @@ class WebMouseEvent : public WebInputEvent, public WebPointerProperties {
             button_param,
             WebFloatPoint(floor(position.x), floor(position.y)),
             WebFloatPoint(floor(global_position.x), floor(global_position.y))),
-        click_count(click_count_param) {}
+        click_count(click_count_param) {
+    DCHECK_GE(type_param, kMouseTypeFirst);
+    DCHECK_LE(type_param, kMouseTypeLast);
+  }
 
   WebMouseEvent(Type type_param,
                 int modifiers_param,
