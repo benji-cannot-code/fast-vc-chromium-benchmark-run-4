2465591acf2b573c98a2ad2867d249d6b1dd0097
gab <gab@chromium.org>
2017-06-02 14:12:18 -0700

    Replace deprecated base::NonThreadSafe in /content/browser/loader in favor of SequenceChecker.
    Note to crash team: This CL is a refactor and has no intended behavior change.
    
    This change was scripted by https://crbug.com/676387#c84.
    
    If you're wondering why there's a second wave of this, the first version of the
    script handled "NON_EXPORTED_BASE(public base::NonThreadSafe)" but not
    "public NON_EXPORTED_BASE(base::NonThreadSafe)"...
    
    Note-worthy for the reviewer:
     * SequenceChecker enforces thread-safety but not thread-affinity!
       If the classes that were updated are thread-affine (use thread local
       storage or a third-party API that does) they should be migrated to
       ThreadChecker instead.
     * ~NonThreadSafe() used to implicitly check in its destructor
       ~Sequence/ThreadChecker() doesn't by design. To keep this CL a
       no-op, an explicit check was added to the destructor of migrated
       classes.
     * NonThreadSafe used to provide access to subclasses, as such
       the |sequence_checker_| member was made protected rather than
       private where necessary.
    
    BUG=676387
    This CL was uploaded by git cl split.
    
    Review-Url: https://codereview.chromium.org/2915313002
    Cr-Commit-Position: refs/heads/master@{#476791}

--- content/browser/loader/resource_handler.cc
+++ content/browser/loader/resource_handler.cc
@@ -17,7 +17,9 @@ void ResourceHandler::SetDelegate(Delegate* delegate) {
   delegate_ = delegate;
 }
 
-ResourceHandler::~ResourceHandler() {}
+ResourceHandler::~ResourceHandler() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+}
 
 ResourceHandler::ResourceHandler(net::URLRequest* request)
     : request_(request) {}
--- content/browser/loader/resource_handler.h
+++ content/browser/loader/resource_handler.h
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
-#include "base/threading/non_thread_safe.h"
+#include "base/sequence_checker.h"
 #include "content/browser/loader/resource_controller.h"
 #include "content/common/content_export.h"
 
@@ -44,8 +44,7 @@ struct ResourceResponse;
 // No ResourceHandler method other than OnWillRead will ever be called
 // synchronously when it calls into the ResourceController passed in to it,
 // either to resume or cancel the request.
-class CONTENT_EXPORT ResourceHandler
-    : public NON_EXPORTED_BASE(base::NonThreadSafe) {
+class CONTENT_EXPORT ResourceHandler {
  public:
   virtual ~ResourceHandler();
 
@@ -176,6 +175,8 @@ class CONTENT_EXPORT ResourceHandler
   net::URLRequest* request_;
   Delegate* delegate_;
   std::unique_ptr<ResourceController> controller_;
+
+  SEQUENCE_CHECKER(sequence_checker_);
 
   DISALLOW_COPY_AND_ASSIGN(ResourceHandler);
 };
