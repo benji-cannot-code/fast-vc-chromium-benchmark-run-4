f4b51b6bc6948082df9236cfaee0530bacd69874
gab <gab@chromium.org>
2017-06-02 12:05:41 -0700

    ThreadChecker instead of SequenceChecker in classes that use TLS.
    A few incorrect SequenceChecker slipped through reviews in mass migration
    of base::NonThreadSafe. As documented on those CLs, usage of ThreadLocalStorage
    requires thread-affinity (ThreadChecker instead of SequenceChecker).
    
    BUG=676387
    
    Review-Url: https://codereview.chromium.org/2916303002
    Cr-Commit-Position: refs/heads/master@{#476742}

--- content/child/fileapi/webfilesystem_impl.h
+++ content/child/fileapi/webfilesystem_impl.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
-#include "base/sequence_checker.h"
+#include "base/threading/thread_checker.h"
 #include "content/public/child/worker_thread.h"
 #include "third_party/WebKit/public/platform/WebFileSystem.h"
 
@@ -107,7 +107,8 @@ class WebFileSystemImpl : public blink::WebFileSystem,
   int next_callbacks_id_;
   WaitableCallbackResultsMap waitable_results_;
 
-  SEQUENCE_CHECKER(sequence_checker_);
+  // Thread-affine per use of TLS in impl.
+  THREAD_CHECKER(thread_checker_);
 
   DISALLOW_COPY_AND_ASSIGN(WebFileSystemImpl);
 };
