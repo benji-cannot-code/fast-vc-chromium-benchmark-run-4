21849cbb82371f670e8c1998294db64cc5a0fe2d
moshayedi <moshayedi@chromium.org>
2017-06-02 18:33:24 -0700

    Don't rewrite events twice.
    Rewriters are first tried in AshWindowTreeHostPlatform::DispatchEvent(). We
    also rewrite them in WindowTreeHost::DispatchKeyEventPostIME().
    
    This for example causes problems when ARC++ is focused while spoken feedback
    is active. So this CL removes the 2nd rewrite.
    
    If Spoken Feedback is active but ARC++ is focused, this is the path for
    super+right-arrow:
     1. AshWindowTreeHostPlatform::DispatchEvent() tries rewriters before sending
        event to sink.
     2. SpokenFeedbackEventRewriter sees super key is down, so sends the event to
        the Spoken Feedback event extension and tells ash to discard the event.
     3. The Spoken Feedback extension doesn't handle the event, and the event is
        resent to ash from WebContentsImpl::HandleKeyboardEvent(), which sends it
        to SpokenFeedbackEventRewriterDelegate::HandleKeyboardEvent(), which sends
        it to event sink (skipping rewriters).
     4. Event is sent to InputMethod, it is not handled by InputMethod, so it is
        sent to WindowTreeHost::DispatchKeyEventPostIME().
    
    Now, if at WTH::DispatchKeyEventPostIME() we try to rewrite the event again,
    steps 2 to 4 will be tried infinitely and event won't be dispatched to ARC++.
    
    This is what was happening before this CL. This CL skips rewriters at
    WTH::DispatchKeyEventPostIME().
    
    BUG=727179
    
    Review-Url: https://codereview.chromium.org/2916673002
    Cr-Commit-Position: refs/heads/master@{#476848}

--- ui/aura/window_tree_host_unittest.cc
+++ ui/aura/window_tree_host_unittest.cc
@@ -6,7 +6,41 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/test/aura_test_base.h"
 #include "ui/aura/test/test_screen.h"
 #include "ui/aura/window.h"
+#include "ui/base/ime/input_method.h"
 #include "ui/compositor/layer.h"
+#include "ui/events/event_rewriter.h"
+
+namespace {
+
+// Counts number of events observed.
+class CounterEventRewriter : public ui::EventRewriter {
+ public:
+  CounterEventRewriter() : events_seen_(0) {}
+  ~CounterEventRewriter() override {}
+
+  int events_seen() const { return events_seen_; }
+
+ private:
+  // ui::EventRewriter:
+  ui::EventRewriteStatus RewriteEvent(
+      const ui::Event& event,
+      std::unique_ptr<ui::Event>* new_event) override {
+    events_seen_++;
+    return ui::EVENT_REWRITE_CONTINUE;
+  }
+
+  ui::EventRewriteStatus NextDispatchEvent(
+      const ui::Event& last_event,
+      std::unique_ptr<ui::Event>* new_event) override {
+    return ui::EVENT_REWRITE_CONTINUE;
+  }
+
+  int events_seen_;
+
+  DISALLOW_COPY_AND_ASSIGN(CounterEventRewriter);
+};
+
+}  // namespace
 
 namespace aura {
 
@@ -38,6 +72,17 @@ TEST_F(WindowTreeHostTest, DPIWindowSize) {
   EXPECT_EQ(gfx::Rect(1, 1, 534, 401), root_window()->bounds());
   EXPECT_EQ(gfx::Vector2dF(0, 0),
             host()->compositor()->root_layer()->subpixel_position_offset());
+}
+
+TEST_F(WindowTreeHostTest, NoRewritesPostIME) {
+  CounterEventRewriter event_rewriter;
+  host()->AddEventRewriter(&event_rewriter);
+
+  ui::KeyEvent key_event('A', ui::VKEY_A, 0);
+  host()->GetInputMethod()->DispatchKeyEvent(&key_event);
+  EXPECT_EQ(0, event_rewriter.events_seen());
+
+  host()->RemoveEventRewriter(&event_rewriter);
 }
 
 }  // namespace aura
