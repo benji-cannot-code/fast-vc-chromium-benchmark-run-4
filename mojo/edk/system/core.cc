7c3e706ed045c39dd59f2654bb1be4aa30e0e128
erikchen <erikchen@chromium.org>
2017-06-02 19:40:57 -0700

    Add a Mojo MemoryDumpProvider.
    The MemoryDumpProvider counts Dispatchers and segments them by type.
    
    BUG=705785
    
    Review-Url: https://codereview.chromium.org/2915013006
    Cr-Commit-Position: refs/heads/master@{#476870}

--- mojo/edk/system/core.cc
+++ mojo/edk/system/core.cc
@@ -154,7 +154,7 @@ NodeController* Core::GetNodeController() {
 }
 
 scoped_refptr<Dispatcher> Core::GetDispatcher(MojoHandle handle) {
-  base::AutoLock lock(handles_lock_);
+  base::AutoLock lock(handles_.GetLock());
   return handles_.GetDispatcher(handle);
 }
 
@@ -215,7 +215,7 @@ void Core::SetMachPortProvider(base::PortProvider* port_provider) {
 }
 
 MojoHandle Core::AddDispatcher(scoped_refptr<Dispatcher> dispatcher) {
-  base::AutoLock lock(handles_lock_);
+  base::AutoLock lock(handles_.GetLock());
   return handles_.AddDispatcher(dispatcher);
 }
 
@@ -224,7 +224,7 @@ bool Core::AddDispatchersFromTransit(
     MojoHandle* handles) {
   bool failed = false;
   {
-    base::AutoLock lock(handles_lock_);
+    base::AutoLock lock(handles_.GetLock());
     if (!handles_.AddDispatchersFromTransit(dispatchers, handles))
       failed = true;
   }
@@ -250,7 +250,7 @@ MojoResult Core::CreatePlatformHandleWrapper(
 MojoResult Core::PassWrappedPlatformHandle(
     MojoHandle wrapper_handle,
     ScopedPlatformHandle* platform_handle) {
-  base::AutoLock lock(handles_lock_);
+  base::AutoLock lock(handles_.GetLock());
   scoped_refptr<Dispatcher> d;
   MojoResult result = handles_.GetAndRemoveDispatcher(wrapper_handle, &d);
   if (result != MOJO_RESULT_OK)
@@ -301,7 +301,7 @@ MojoResult Core::PassSharedMemoryHandle(
   scoped_refptr<Dispatcher> dispatcher;
   MojoResult result = MOJO_RESULT_OK;
   {
-    base::AutoLock lock(handles_lock_);
+    base::AutoLock lock(handles_.GetLock());
     // Get the dispatcher and check it before removing it from the handle table
     // to ensure that the dispatcher is of the correct type. This ensures we
     // don't close and remove the wrong type of dispatcher.
@@ -367,7 +367,7 @@ MojoResult Core::Close(MojoHandle handle) {
   RequestContext request_context;
   scoped_refptr<Dispatcher> dispatcher;
   {
-    base::AutoLock lock(handles_lock_);
+    base::AutoLock lock(handles_.GetLock());
     MojoResult rv = handles_.GetAndRemoveDispatcher(handle, &dispatcher);
     if (rv != MOJO_RESULT_OK)
       return rv;
@@ -464,7 +464,7 @@ MojoResult Core::AllocMessage(uint32_t num_bytes,
 
   std::vector<Dispatcher::DispatcherInTransit> dispatchers;
   {
-    base::AutoLock lock(handles_lock_);
+    base::AutoLock lock(handles_.GetLock());
     MojoResult rv = handles_.BeginTransit(handles, num_handles, &dispatchers);
     if (rv != MOJO_RESULT_OK) {
       handles_.CancelTransit(dispatchers);
@@ -478,7 +478,7 @@ MojoResult Core::AllocMessage(uint32_t num_bytes,
       num_bytes, dispatchers.data(), num_handles, &message);
 
   {
-    base::AutoLock lock(handles_lock_);
+    base::AutoLock lock(handles_.GetLock());
     if (rv == MOJO_RESULT_OK) {
       handles_.CompleteTransitAndClose(dispatchers);
       *message_handle = reinterpret_cast<MojoMessageHandle>(message.release());
@@ -548,7 +548,7 @@ MojoResult Core::CreateMessagePipe(
     scoped_refptr<Dispatcher> unused;
     unused->Close();
 
-    base::AutoLock lock(handles_lock_);
+    base::AutoLock lock(handles_.GetLock());
     handles_.GetAndRemoveDispatcher(*message_pipe_handle0, &unused);
     return MOJO_RESULT_RESOURCE_EXHAUSTED;
   }
@@ -658,7 +658,7 @@ MojoResult Core::FuseMessagePipes(MojoHandle handle0, MojoHandle handle1) {
 
   bool valid_handles = true;
   {
-    base::AutoLock lock(handles_lock_);
+    base::AutoLock lock(handles_.GetLock());
     MojoResult result0 = handles_.GetAndRemoveDispatcher(handle0, &dispatcher0);
     MojoResult result1 = handles_.GetAndRemoveDispatcher(handle1, &dispatcher1);
     if (result0 != MOJO_RESULT_OK || result1 != MOJO_RESULT_OK ||
@@ -755,7 +755,7 @@ MojoResult Core::CreateDataPipe(
       *data_pipe_consumer_handle == MOJO_HANDLE_INVALID) {
     if (*data_pipe_producer_handle != MOJO_HANDLE_INVALID) {
       scoped_refptr<Dispatcher> unused;
-      base::AutoLock lock(handles_lock_);
+      base::AutoLock lock(handles_.GetLock());
       handles_.GetAndRemoveDispatcher(*data_pipe_producer_handle, &unused);
     }
     producer->Close();
@@ -994,7 +994,7 @@ MojoResult Core::UnwrapPlatformSharedBufferHandle(
   scoped_refptr<Dispatcher> dispatcher;
   MojoResult result = MOJO_RESULT_OK;
   {
-    base::AutoLock lock(handles_lock_);
+    base::AutoLock lock(handles_.GetLock());
     result = handles_.GetAndRemoveDispatcher(mojo_handle, &dispatcher);
     if (result != MOJO_RESULT_OK)
       return result;
@@ -1025,7 +1025,7 @@ MojoResult Core::UnwrapPlatformSharedBufferHandle(
 }
 
 void Core::GetActiveHandlesForTest(std::vector<MojoHandle>* handles) {
-  base::AutoLock lock(handles_lock_);
+  base::AutoLock lock(handles_.GetLock());
   handles_.GetActiveHandlesForTest(handles);
 }
 
--- mojo/edk/system/handle_table.h
+++ mojo/edk/system/handle_table.h
@@ -12,16 +12,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/hash_tables.h"
 #include "base/macros.h"
+#include "base/synchronization/lock.h"
+#include "base/trace_event/memory_dump_provider.h"
 #include "mojo/edk/system/dispatcher.h"
 #include "mojo/public/c/system/types.h"
 
 namespace mojo {
 namespace edk {
 
-class HandleTable {
+class HandleTable : public base::trace_event::MemoryDumpProvider {
  public:
   HandleTable();
-  ~HandleTable();
+  ~HandleTable() override;
+
+  // HandleTable is thread-hostile. All access should be gated by GetLock().
+  base::Lock& GetLock();
 
   MojoHandle AddDispatcher(scoped_refptr<Dispatcher> dispatcher);
 
@@ -51,6 +56,10 @@ class HandleTable {
   void GetActiveHandlesForTest(std::vector<MojoHandle> *handles);
 
  private:
+  // MemoryDumpProvider implementation.
+  bool OnMemoryDump(const base::trace_event::MemoryDumpArgs& args,
+                    base::trace_event::ProcessMemoryDump* pmd) override;
+
   struct Entry {
    Entry();
    explicit Entry(scoped_refptr<Dispatcher> dispatcher);
@@ -64,6 +73,7 @@ class HandleTable {
   using HandleMap = base::hash_map<MojoHandle, Entry>;
 
   HandleMap handles_;
+  base::Lock lock_;
 
   uint32_t next_available_handle_ = 1;
 
