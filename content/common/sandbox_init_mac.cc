9aac5fb9bb64ebecb334b715972214775ae981eb
kerrnel <kerrnel@chromium.org>
2017-06-02 19:23:52 -0700

    Add flags for v2 sandbox to Chrome and Helper executable.
    This adds the flags for the v2 sandbox to Chrome and the Helper
    executable. The helper executable has its own declarations of the flags
    to minimize the static linking size.
    
    BUG=689306
    
    Review-Url: https://codereview.chromium.org/2921733002
    Cr-Commit-Position: refs/heads/master@{#476865}

--- content/common/sandbox_init_mac.cc
+++ content/common/sandbox_init_mac.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/sandbox_mac.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/sandbox_init.h"
+#include "sandbox/mac/seatbelt.h"
 
 namespace content {
 
@@ -37,6 +38,8 @@ bool GetSandboxTypeFromCommandLine(int* sandbox_type,
       *base::CommandLine::ForCurrentProcess();
   if (command_line.HasSwitch(switches::kNoSandbox))
     return false;
+  if (command_line.HasSwitch(switches::kV2SandboxedEnabled))
+    CHECK(sandbox::Seatbelt::IsSandboxed());
 
   std::string process_type =
       command_line.GetSwitchValueASCII(switches::kProcessType);
