7c89147f7de0f6b13673be0df1caf4f8a2104618
Dirk Pranke <dpranke@chromium.org>
2017-06-05 19:23:37 +0000

    Revert "Refactored IPH demo mode to be run cross-platform."
    This reverts commit 31093e7189d7a055b362bc8f325ef96099ee1b20.
    
    Reason for revert: This change adds static initializers to the build, which is not allowed. It broke the `sizes` step as a result:
    
    https://luci-milo.appspot.com/buildbot/chromium/Linux%20x64/41189
    https://luci-logdog.appspot.com/v/?s=chromium%2Fbb%2Fchromium%2FLinux_x64%2F41189%2F%2B%2Frecipes%2Fsteps%2Fsizes%2F0%2Fstdout
    
    Original change's description:
    > Refactored IPH demo mode to be run cross-platform.
    > 
    > Current code only enables IPH demo mode on Android, restricting testing
    > on other platforms.
    > 
    > This feature needs to be cross-platform to be used with deferred onboarding
    > (new tab) on Desktop Chrome.
    > 
    > Things that were changed in this CL:
    > * Makes the whole feature engagement tracker available to all platforms
    > * Updated documentation to reflect this change
    > * Enables demo mode in chrome://flags to be cross-platform
    > * Change in feature_engagement_tracker_impl.cc was necessary to make it
    >   work for Windows' pathnames.
    > 
    > Bug: 729138
    > Change-Id: I76b382e004ea9b73bd7fb2df26aa83ff7856422e
    > Reviewed-on: https://chromium-review.googlesource.com/518933
    > Commit-Queue: Catherine Chung <catherinechung@google.com>
    > Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Robert Liao <robliao@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#477045}
    
    TBR=sky@chromium.org,nyquist@chromium.org,robliao@chromium.org,erikchen@chromium.org,catherinechung@google.com
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=729138
    
    Change-Id: Ia6b18d2b797eacea75ec16349f9f42d32ea3f0e8
    Reviewed-on: https://chromium-review.googlesource.com/524289
    Commit-Queue: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477106}

--- components/feature_engagement_tracker/internal/feature_engagement_tracker_impl.cc
+++ components/feature_engagement_tracker/internal/feature_engagement_tracker_impl.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/feature_list.h"
-#include "base/files/file_path.h"
 #include "base/memory/ptr_util.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/metrics/user_metrics.h"
@@ -37,10 +36,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace feature_engagement_tracker {
 
 namespace {
-const base::FilePath::CharType kEventDBStorageDir[] =
-    FILE_PATH_LITERAL("EventDB");
-const base::FilePath::CharType kAvailabilityDBStorageDir[] =
-    FILE_PATH_LITERAL("AvailabilityDB");
+const char kEventDBStorageDir[] = "EventDB";
+const char kAvailabilityDBStorageDir[] = "AvailabilityDB";
 
 // Creates a FeatureEngagementTrackerImpl that is usable for a demo mode.
 std::unique_ptr<FeatureEngagementTracker>
