f5a3866883a2108c30761cd9d7e79c76153321ef
rkaplow <rkaplow@chromium.org>
2017-06-05 09:13:19 -0700

    Change the logic for discarding all non-histogram logs because of actions/omnibox events to truncating.
    I also expanded the limit to 5k events per type (omnibox, user actions).
    
    BUG=683934
    
    Review-Url: https://codereview.chromium.org/2910023003
    Cr-Commit-Position: refs/heads/master@{#476992}

--- ios/chrome/today_extension/today_metrics_logger.mm
+++ ios/chrome/today_extension/today_metrics_logger.mm
@@ -47,9 +47,6 @@ NSString* const kTodayExtensionMetricsSessionID = @"MetricsSessionID";
 // restart, this log can be written to disk for upload.
 NSString* const kTodayExtensionMetricsCurrentLog = @"MetricsCurrentLog";
 
-// Maximum number of event in a log.
-const int kMaxEventsPerLog = 1000;
-
 // Maximum age of a log.
 const int kMaxLogLifeTimeInSeconds = 86400;
 
@@ -208,9 +205,9 @@ void TodayMetricsLogger::PersistLogs() {
   [[NSUserDefaults standardUserDefaults]
       setObject:ns_encoded_log
          forKey:kTodayExtensionMetricsCurrentLog];
-  if (log_->num_events() >= kMaxEventsPerLog ||
-      (base::TimeTicks::Now() - log_->creation_time()).InSeconds() >=
-          kMaxLogLifeTimeInSeconds) {
+  log_->TruncateEvents();
+  if ((base::TimeTicks::Now() - log_->creation_time()).InSeconds() >=
+      kMaxLogLifeTimeInSeconds) {
     CreateNewLog();
   }
 }
