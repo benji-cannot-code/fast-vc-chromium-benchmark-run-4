4d05c2fbdc7ba870ff39f9f88b19ab73b8a6e38b
lesliewatkins <lesliewatkins@chromium.org>
2017-06-05 13:32:57 -0700

    Add Tether to Mobile NetworkTypePattern.
    BUG=672263
    
    Review-Url: https://codereview.chromium.org/2913103002
    Cr-Commit-Position: refs/heads/master@{#477073}

--- ash/system/network/network_list.cc
+++ ash/system/network/network_list.cc
@@ -524,7 +524,7 @@ NetworkListView::UpdateNetworkListEntries() {
   }
 
   // Cellular initializing.
-  int cellular_message_id = network_icon::GetCellularUninitializedMsg();
+  int cellular_message_id = network_icon::GetMobileUninitializedMsg();
   if (!cellular_message_id &&
       handler->IsTechnologyEnabled(NetworkTypePattern::Mobile()) &&
       !handler->FirstNetworkByType(NetworkTypePattern::Mobile())) {
