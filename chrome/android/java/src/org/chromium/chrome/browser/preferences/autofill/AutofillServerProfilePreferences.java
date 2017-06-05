c9d3d6f0dab0678a9d3b5f08266ab7a78acd1881
Peter Wen <wnwen@google.com>
2017-06-05 10:51:20 -0400

    Android: Remove non-translateable strings in clank
    Despite not being translated, translateable="false" strings are still
    copied into every language pack. Since they are not translateable, there
    is no benefit to bloating size and keeping them as @strings rather than
    string literals which proguard can inline.
    
    Estimated size savings: 11kb
    
    Bug: 703134
    Change-Id: I02d47eb221f50c734fcc7e0eac5833f18e3233d4
    Reviewed-on: https://chromium-review.googlesource.com/521282
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477007}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/autofill/AutofillServerProfilePreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/autofill/AutofillServerProfilePreferences.java
@@ -12,6 +12,7 @@ import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceFragment;
 
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.ChromeStringConstants;
 import org.chromium.chrome.browser.autofill.PersonalDataManager;
 import org.chromium.chrome.browser.autofill.PersonalDataManager.AutofillProfile;
 import org.chromium.chrome.browser.customtabs.CustomTabActivity;
@@ -59,7 +60,7 @@ public class AutofillServerProfilePreferences
         assert preference.getKey().equals(PREF_SERVER_PROFILE_EDIT_LINK);
         Context context = preference.getContext();
         CustomTabActivity.showInfoPage(
-                context, context.getString(R.string.autofill_manage_wallet_addresses_url));
+                context, ChromeStringConstants.AUTOFILL_MANAGE_WALLET_ADDRESSES_URL);
         return true;
     }
 }
