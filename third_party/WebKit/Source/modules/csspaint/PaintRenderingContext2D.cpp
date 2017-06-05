71cce52f5ac6806443b8dc6e5808ae75df822a92
Xida Chen <xidachen@chromium.org>
2017-06-05 15:12:37 -0400

    Support filter operations in PaintRenderingContext2D
    This CL makes PaintRenderingContext2D support filter operations. The infra for
    the filter operations is already built in the BaseRenderingContext2D. What we
    really need to do is just to expose that in the IDL file. In addition, this CL
    adds a layout test to verify the correctness. The layout test is the 
    same as the layout test for OffscreenCanvas's filter operations.
    
    Bug: 728600
    Change-Id: I0e8f26bc4afd5e679807dcb527149167535952e4
    Reviewed-on: https://chromium-review.googlesource.com/522445
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477127}

--- third_party/WebKit/Source/modules/csspaint/PaintRenderingContext2D.cpp
+++ third_party/WebKit/Source/modules/csspaint/PaintRenderingContext2D.cpp
@@ -77,4 +77,12 @@ void PaintRenderingContext2D::ValidateStateStack() const {
 #endif
 }
 
+bool PaintRenderingContext2D::StateHasFilter() {
+  return GetState().HasFilterForOffscreenCanvas(IntSize(Width(), Height()));
+}
+
+sk_sp<SkImageFilter> PaintRenderingContext2D::StateGetFilter() {
+  return GetState().GetFilterForOffscreenCanvas(IntSize(Width(), Height()));
+}
+
 }  // namespace blink
