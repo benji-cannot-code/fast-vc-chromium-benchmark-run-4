b2bf8687ae8552705ee0ca0c29da9f6b5e7a8c6a
Francois Doray <fdoray@chromium.org>
2017-06-05 08:39:36 -0400

    [reland] Use TaskScheduler instead of WorkerPool in x11_hotplug_event_handler.cc.
    Previously landed as  https://codereview.chromium.org/2860703002
    https://chromium-review.googlesource.com/c/523803/ should fix the
    issue that caused the revert.
    
    The following traits are used:
    
    Priority: Inherited (default)
      The priority is inherited from the calling context (i.e. TaskTraits
      are initialized with the priority of the current task).
    
    Shutdown behavior: CONTINUE_ON_SHUTDOWN
      Tasks posted with this mode which have not started executing before
      shutdown is initiated will never run. Tasks with this mode running at
      shutdown will be ignored (the worker will not be joined).
    
      Note: Tasks that were previously posted to base::WorkerPool should
      use this shutdown behavior because this is how base::WorkerPool
      handles all its tasks.
    
    May Block:
      Tasks posted with MayBlock() may block. This includes but is not
      limited to tasks that wait on synchronous file I/O operations:
      read or write a file from disk, interact with a pipe or a socket,
      rename or delete a file, enumerate files in a directory, etc. This
      trait isn't required for the mere use of locks.
    
    BUG=659191, 725505
    TBR=danakj@chromium.org
    
    Change-Id: Ie4177b0e7cb7d554ea9ef02dc26afd22942e59e7
    Reviewed-on: https://chromium-review.googlesource.com/524023
    Reviewed-by: Francois Doray <fdoray@chromium.org>
    Commit-Queue: Francois Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479685}

--- ui/compositor/test/test_suite.cc
+++ ui/compositor/test/test_suite.cc
@@ -6,7 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/test/test_suite.h"
 
 #include "base/command_line.h"
-#include "base/message_loop/message_loop.h"
+#include "base/memory/ptr_util.h"
+#include "base/test/scoped_task_environment.h"
 #include "build/build_config.h"
 #include "ui/compositor/compositor.h"
 #include "ui/compositor/compositor_switches.h"
@@ -44,11 +45,13 @@ void CompositorTestSuite::Initialize() {
   display::win::SetDefaultDeviceScaleFactor(1.0f);
 #endif
 
-  message_loop_.reset(new base::MessageLoopForUI);
+  scoped_task_environment_ =
+      base::MakeUnique<base::test::ScopedTaskEnvironment>(
+          base::test::ScopedTaskEnvironment::MainThreadType::UI);
 }
 
 void CompositorTestSuite::Shutdown() {
-  message_loop_.reset();
+  scoped_task_environment_.reset();
 
   base::TestSuite::Shutdown();
 }
--- ui/compositor/test/test_suite.h
+++ ui/compositor/test/test_suite.h
@@ -13,7 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_suite.h"
 
 namespace base {
-class MessageLoop;
+namespace test {
+class ScopedTaskEnvironment;
+}
 }
 
 namespace ui {
@@ -30,7 +32,7 @@ class CompositorTestSuite : public base::TestSuite {
   void Shutdown() override;
 
  private:
-  std::unique_ptr<base::MessageLoop> message_loop_;
+  std::unique_ptr<base::test::ScopedTaskEnvironment> scoped_task_environment_;
 
   DISALLOW_COPY_AND_ASSIGN(CompositorTestSuite);
 };
