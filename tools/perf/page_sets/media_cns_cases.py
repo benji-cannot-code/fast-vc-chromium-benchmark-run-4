ebc57399ef93a98940c4b2445ad88f8b501d722d
ashleymarie <ashleymarie@chromium.org>
2017-06-05 10:58:07 -0700

    Explicitly setting names for media story sets
    BUG=chromium:720002
    
    Review-Url: https://codereview.chromium.org/2923543002
    Cr-Commit-Position: refs/heads/master@{#477022}

--- tools/perf/page_sets/media_cns_cases.py
+++ tools/perf/page_sets/media_cns_cases.py
@@ -47,7 +47,7 @@ class MediaCnsCasesPageSet(story.StorySet):
   """ Media benchmark on network constrained conditions. """
 
   def __init__(self):
-    super(MediaCnsCasesPageSet, self).__init__()
+    super(MediaCnsCasesPageSet, self).__init__(verify_names=True)
 
     urls_list = [
       # pylint: disable=line-too-long
@@ -95,7 +95,7 @@ class MediaCnsCasesPageSet(story.StorySet):
     ]
 
     for url in urls_list:
-      self.AddStory(BasicPlayPage(url, self))
+      self.AddStory(BasicPlayPage(url, self, url.split('/')[-1]))
 
     urls_list2 = [
       # pylint: disable=line-too-long
@@ -118,5 +118,5 @@ class MediaCnsCasesPageSet(story.StorySet):
       if url in urls_list:
         name = 'seek_' + url
       else:
-        name = ''
+        name = url.split('/')[-1]
       self.AddStory(SeekBeforeAndAfterPlayheadPage(url, self, name=name))
