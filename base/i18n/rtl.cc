fac4caefb574d066fd9ac11d40820173cac990a0
mbjorge <mbjorge@chromium.org>
2017-06-05 13:12:55 -0700

    Revert of [TSAN] Fix TSAN error in i18n RTL. (patchset #3 id:40001 of https://codereview.chromium.org/2877983004/ )
    Reason for revert:
    Appears to break the Force UI Direction / Force Text Direction / FOrce RTL flags on mac.
    
    BUG=727229
    
    Original issue's description:
    > [TSAN] Fix TSAN error in i18n RTL.
    >
    > Use an atomic to protect the global g_icu_text_direction cached value
    > from multiple readers/writers.
    >
    > Also reduce acccess to g_icu_text_diretion so it only has a single write
    > location (in SetICUDefaultLocal) and a single read location (ICUIsRTL),
    > to simplify the race-y-ness.
    >
    > Outside of test code, SetICUDefaultLocal is only used in two locations:
    > ppapi_plugin_main.cc and l10n_util.cc, whereas IsRTL is used in >100
    > places outside of test code, so it seemed reasonable to move the heavy
    > logic (updating g_icu_text_direction when it changes) into what I imagine to be
    > a function that is called rarely and let the one that is used a lot
    > (IsRTL/ICUIsRTL) just do a simple read.
    >
    > This still still leaves a race condition in that IsRTL may give in
    > inaccurate result if SetICUDefaultLocale is executed at the same time.
    > In particular, IsRTL may return the value for the previously set default
    > locale after the default locale has been updated.
    >
    > BUG=695929
    > TEST=cast_shell_browsertests under TSAN before/after
    >
    > Review-Url: https://codereview.chromium.org/2877983004
    > Cr-Commit-Position: refs/heads/master@{#472890}
    > Committed: https://chromium.googlesource.com/chromium/src/+/89c24d4a46c1f0e7ad03891a659e6d23dc776c36
    
    TBR=jshin@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=695929
    
    Review-Url: https://codereview.chromium.org/2917523002
    Cr-Commit-Position: refs/heads/master@{#477064}

--- base/i18n/rtl.cc
+++ base/i18n/rtl.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
-#include "base/atomicops.h"
 #include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/i18n/base_i18n_switches.h"
@@ -115,8 +114,7 @@ namespace base {
 namespace i18n {
 
 // Represents the locale-specific ICU text direction.
-static subtle::Atomic32 g_icu_text_direction =
-    static_cast<subtle::Atomic32>(UNKNOWN_DIRECTION);
+static TextDirection g_icu_text_direction = UNKNOWN_DIRECTION;
 
 // Convert the ICU default locale to a string.
 std::string GetConfiguredLocale() {
@@ -165,10 +163,7 @@ void SetICUDefaultLocale(const std::string& locale_string) {
   // presence of actual locale data). However,
   // it does not hurt to have it as a sanity check.
   DCHECK(U_SUCCESS(error_code));
-  subtle::Release_Store(
-      &g_icu_text_direction,
-      static_cast<subtle::Atomic32>(
-          GetTextDirectionForLocaleInStartUp(locale.getName())));
+  g_icu_text_direction = UNKNOWN_DIRECTION;
 }
 
 bool IsRTL() {
@@ -176,11 +171,11 @@ bool IsRTL() {
 }
 
 bool ICUIsRTL() {
-  // Note: There is still  a race if this is executed between the
-  // icu::Locale::setDefault and the g_icu_text_direction store
-  // that happens in SetICUDefaultLocale.
-  return static_cast<TextDirection>(
-             subtle::Acquire_Load(&g_icu_text_direction)) == RIGHT_TO_LEFT;
+  if (g_icu_text_direction == UNKNOWN_DIRECTION) {
+    const icu::Locale& locale = icu::Locale::getDefault();
+    g_icu_text_direction = GetTextDirectionForLocaleInStartUp(locale.getName());
+  }
+  return g_icu_text_direction == RIGHT_TO_LEFT;
 }
 
 TextDirection GetTextDirectionForLocaleInStartUp(const char* locale_name) {
