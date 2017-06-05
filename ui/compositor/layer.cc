1e55c92cd98c2baf915be907d8feab4025da961d
afakhry <afakhry@chromium.org>
2017-06-05 11:09:48 -0700

    Cloning a ui::Layer should also clone its target temperature
    BUG=729203
    TEST=compositor_unittests --gtest_filter=LayerWithDelegateTest.Cloning
    
    Review-Url: https://codereview.chromium.org/2919183002
    Cr-Commit-Position: refs/heads/master@{#477030}

--- ui/compositor/layer.cc
+++ ui/compositor/layer.cc
@@ -170,6 +170,7 @@ std::unique_ptr<Layer> Layer::Clone() const {
   clone->SetBackgroundZoom(zoom_, zoom_inset_);
 
   // Filters.
+  clone->SetLayerTemperature(GetTargetTemperature());
   clone->SetLayerSaturation(layer_saturation_);
   clone->SetLayerBrightness(GetTargetBrightness());
   clone->SetLayerGrayscale(GetTargetGrayscale());
@@ -381,6 +382,14 @@ float Layer::GetCombinedOpacity() const {
 
 void Layer::SetLayerTemperature(float value) {
   GetAnimator()->SetTemperature(value);
+}
+
+float Layer::GetTargetTemperature() const {
+  if (animator_ &&
+      animator_->IsAnimatingProperty(LayerAnimationElement::TEMPERATURE)) {
+    return animator_->GetTargetTemperature();
+  }
+  return layer_temperature();
 }
 
 void Layer::SetBackgroundBlur(int blur_radius) {
