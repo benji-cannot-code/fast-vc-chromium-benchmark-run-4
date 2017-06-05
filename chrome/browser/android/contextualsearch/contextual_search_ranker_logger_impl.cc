f573578c80970b4683fe2f3ebc67487801a08923
donnd <donnd@chromium.org>
2017-06-05 11:59:09 -0700

    [TTS] Fix a crash using a null UkmRecorder.
    Adds checks for whether the UkmRecorder is a nullptr befor use.
    ContextualSearchRankerLoggerImpl now will check the UkmRecorder
    and Builder before each access.
    
    BUG=729504
    
    Review-Url: https://codereview.chromium.org/2919383002
    Cr-Commit-Position: refs/heads/master@{#477044}

--- chrome/browser/android/contextualsearch/contextual_search_ranker_logger_impl.cc
+++ chrome/browser/android/contextualsearch/contextual_search_ranker_logger_impl.cc
@@ -14,9 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/ukm/public/ukm_recorder.h"
 #include "jni/ContextualSearchRankerLoggerImpl_jni.h"
 
-ContextualSearchRankerLoggerImpl::ContextualSearchRankerLoggerImpl(
-    JNIEnv* env,
-    jobject obj) {
+ContextualSearchRankerLoggerImpl::ContextualSearchRankerLoggerImpl(JNIEnv* env,
+                                                                   jobject obj)
+    : ukm_recorder_(nullptr), builder_(nullptr) {
   java_object_.Reset(env, obj);
 }
 
@@ -38,6 +38,11 @@ void ContextualSearchRankerLoggerImpl::SetupLoggingAndRanker(
 void ContextualSearchRankerLoggerImpl::SetUkmRecorder(
     ukm::UkmRecorder* ukm_recorder,
     const GURL& page_url) {
+  if (!ukm_recorder) {
+    builder_.reset();
+    return;
+  }
+
   ukm_recorder_ = ukm_recorder;
   source_id_ = ukm_recorder_->GetNewSourceID();
   ukm_recorder_->UpdateSourceURL(source_id_, page_url);
@@ -49,12 +54,18 @@ void ContextualSearchRankerLoggerImpl::LogLong(
     jobject obj,
     const base::android::JavaParamRef<jstring>& j_feature,
     jlong j_long) {
+  if (!builder_)
+    return;
+
   std::string feature = base::android::ConvertJavaStringToUTF8(env, j_feature);
   builder_->AddMetric(feature.c_str(), j_long);
 }
 
 void ContextualSearchRankerLoggerImpl::WriteLogAndReset(JNIEnv* env,
                                                         jobject obj) {
+  if (!ukm_recorder_)
+    return;
+
   // Set up another builder for the next record (in case it's needed).
   builder_ = ukm_recorder_->GetEntryBuilder(source_id_, "ContextualSearch");
 }
