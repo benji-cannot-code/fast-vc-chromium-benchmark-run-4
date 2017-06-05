48484032a1d67f26120c734e87e8a790e5e2cb42
Scott Graham <scottmg@chromium.org>
2017-06-05 13:01:45 -0700

    fuchsia: Few more stubs to get base_unittests linking in is_debug=true
    Bug: 706592
    Change-Id: I811cb9d3660bd960184649a02525a1da1326e9f5
    Reviewed-on: https://chromium-review.googlesource.com/523676
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Scott Graham <scottmg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477421}

--- base/process/memory_fuchsia.cc
+++ base/process/memory_fuchsia.cc
@@ -5,10 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/process/memory.h"
 
+#include <stdlib.h>
+
 namespace base {
 
 void EnableTerminationOnHeapCorruption() {
   // Nothing to be done here.
+}
+
+bool UncheckedMalloc(size_t size, void** result) {
+  *result = malloc(size);
+  return *result != nullptr;
 }
 
 }  // namespace base
