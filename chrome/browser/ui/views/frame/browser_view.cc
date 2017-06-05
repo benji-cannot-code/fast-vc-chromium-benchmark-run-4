41af7466512248dfe5d7027ddcc2a50b82e9ebf5
Scott Violet <sky@chromium.org>
2017-06-05 08:37:24 -0700

    views: convert ViewStorage to ViewTracker
    BrowserView didn't need it at all.
    
    BUG=727934
    TEST=covered by tests
    R=msw@chromium.org
    
    Change-Id: Ia5d3a44a0d0e9f6ea7d152a0c42cabd253636712
    Reviewed-on: https://chromium-review.googlesource.com/522243
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#476999}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -138,7 +138,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/controls/webview/webview.h"
 #include "ui/views/focus/external_focus_tracker.h"
-#include "ui/views/focus/view_storage.h"
 #include "ui/views/layout/grid_layout.h"
 #include "ui/views/widget/native_widget.h"
 #include "ui/views/widget/root_view.h"
@@ -397,8 +396,6 @@ const char BrowserView::kViewClassName[] = "BrowserView";
 
 BrowserView::BrowserView()
     : views::ClientView(nullptr, nullptr),
-      last_focused_view_storage_id_(
-          views::ViewStorage::GetInstance()->CreateStorageID()),
       frame_(nullptr),
       top_container_(nullptr),
       tabstrip_(nullptr),
