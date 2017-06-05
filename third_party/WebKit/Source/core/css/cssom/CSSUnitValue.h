b5faa3199f2604a7e110592d99241442cba70c42
meade <meade@chromium.org>
2017-06-04 20:03:53 -0700

    Restructure type tracking in StyleValues to work better with new numeric types.
    The enum CSSStyleValue::StyleValueType is now used to track the
    "strong type"
    (https://drafts.css-houdini.org/css-typed-om/#cssnumericvalue-strong-type)
    of each style value.
    
    Reviewers, please add thoughts on putting "ContainsPercent" on
    CSSStyleValue. I couldn't think of a good way to express
    "CSSStyleValues can be <a non-percent type> *and* a percent at
    the same time (e.g. calc(3px + 2%)). However this is only used
    on CSSNumericValue subclasses.
    
    Additional notes:
    - User border-*-width for testing setting CSSUnitValues as lengths.
    - border-*-width was previously marked as supports_percent, but it
    actually does not
    (https://developer.mozilla.org/en/docs/Web/CSS/border-bottom-width)
    - There is now a list of valid values for typedom_types in
    CSSProperties.json5
    - supports_percentage is now implied by including "Percent" in
    typedom_types
    
    BUG=545318
    
    Review-Url: https://codereview.chromium.org/2903413002
    Cr-Commit-Position: refs/heads/master@{#476929}

--- third_party/WebKit/Source/core/css/cssom/CSSUnitValue.h
+++ third_party/WebKit/Source/core/css/cssom/CSSUnitValue.h
@@ -11,30 +11,40 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+// Represents numeric values that can be expressed as a single number plus a
+// unit (or a naked number or percentage).
+// See CSSUnitValue.idl for more information about this class.
 class CORE_EXPORT CSSUnitValue final : public CSSNumericValue {
   WTF_MAKE_NONCOPYABLE(CSSUnitValue);
   DEFINE_WRAPPERTYPEINFO();
 
  public:
-  static CSSPrimitiveValue::UnitType UnitFromName(const String& name);
+  // The constructor defined in the IDL.
   static CSSUnitValue* Create(double value,
                               const String& unit,
                               ExceptionState&);
+  // Blink-internal ways of creating CSSUnitValues.
   static CSSUnitValue* Create(double value, CSSPrimitiveValue::UnitType);
   static CSSUnitValue* FromCSSValue(const CSSPrimitiveValue&);
 
+  // Setters and getters for attributes defined in the IDL.
   void setValue(double new_value) { value_ = new_value; }
   double value() const { return value_; }
   void setUnit(const String& new_unit, ExceptionState&);
   String unit() const;
-
   String type() const;
 
-  StyleValueType GetType() const override { return StyleValueType::kUnitType; }
-
+  // Gets the Typed OM category, e.g. length, angle, etc.
+  StyleValueType GetType() const override;
+  bool ContainsPercent() const override {
+    return unit_ == CSSPrimitiveValue::UnitType::kPercentage;
+  }
+  // Gets the representation that can be applied to an inline style.
   const CSSValue* ToCSSValue() const override;
 
  private:
+  static CSSPrimitiveValue::UnitType UnitFromName(const String& name);
+
   CSSUnitValue(double value, CSSPrimitiveValue::UnitType unit)
       : CSSNumericValue(), value_(value), unit_(unit) {}
 
