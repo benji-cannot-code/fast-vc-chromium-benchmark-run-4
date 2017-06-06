50279574b7e8bcd4f9f1058a01c1f510e7d1a6da
updowndota <updowndota@chromium.org>
2017-06-06 13:43:45 -0700

    Adding ArcBridge Interface for Volume Update Requests
    In order to enable volume adjustment from Android side, this CL provide
    ArcBridge interface to ArcAudioService on Android side.
    
    Android could send volume update request by calling
    OnVolumeUpdateRequest(float volume) with volume in range [0,100].
    
    BUG=b/36129286
    
    Review-Url: https://codereview.chromium.org/2813113002
    Cr-Commit-Position: refs/heads/master@{#477399}

--- components/arc/audio/arc_audio_bridge.h
+++ components/arc/audio/arc_audio_bridge.h
@@ -32,6 +32,7 @@ class ArcAudioBridge : public ArcService,
 
   // mojom::AudioHost overrides.
   void ShowVolumeControls() override;
+  void OnSystemVolumeUpdateRequest(int32_t percent) override;
 
  private:
   mojo::Binding<mojom::AudioHost> binding_;
