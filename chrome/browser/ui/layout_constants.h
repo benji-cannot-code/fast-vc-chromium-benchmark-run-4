1c52063731be7fda849d755dec4edade43510640
Evan Stade <estade@chromium.org>
2017-06-06 10:42:49 -0600

    Fix omnibox keyword hint bubble for touch-hybrid mode.
    The bubble itself should have 4dip of padding above and below regardless
    of whether it's touch hybrid or normal mode (previously it was 6dip for
    touch hybrid and 4dip for normal).
    
    The padding interior to the bubble is also adjusted by 1dip to make the
    text slightly bigger in hybrid mode.
    
    Bug: 729719
    Change-Id: I494e63aa181cb4a403b6d2f71c216bb8d0b7acfd
    Reviewed-on: https://chromium-review.googlesource.com/525996
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477672}

--- chrome/browser/ui/layout_constants.h
+++ chrome/browser/ui/layout_constants.h
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/size.h"
 
 enum LayoutConstant {
+  // The vertical padding (additional to TOOLBAR_ELEMENT_PADDING) above and
+  // below location bar bubbles.
+  LOCATION_BAR_BUBBLE_VERTICAL_PADDING,
+
   // The vertical padding between the edge of a location bar bubble and its
   // contained text.
   LOCATION_BAR_BUBBLE_FONT_VERTICAL_PADDING,
--- chrome/browser/ui/views/location_bar/location_bar_view.h
+++ chrome/browser/ui/views/location_bar/location_bar_view.h
@@ -107,9 +107,6 @@ class LocationBarView : public LocationBar,
   // each icon view should be kIconWidth + 2 * kIconInteriorPadding.
   static constexpr int kIconInteriorPadding = 4;
 
-  // The additional vertical padding of a bubble.
-  static constexpr int kBubbleVerticalPadding = 3;
-
   // The location bar view's class name.
   static const char kViewClassName[];
 
