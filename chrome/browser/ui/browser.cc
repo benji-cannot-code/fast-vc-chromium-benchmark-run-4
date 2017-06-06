b3a43c67dd47f5a0173a2a6c3c0792e44e11e41c
lukasza <lukasza@chromium.org>
2017-06-05 20:10:20 -0700

    Propagate opener to BackgroundsContents.
    Before this CL, the opener would be lost:
    1. When creating the BackgroundContents:
        1.1. The call from WebContentsImpl::CreateNewWindow to
             Browser::ShouldCreateWebContents didn't propagate |opener|
        1.2. WebContents::CreateWithSessionStorage ignored opener-related
             fields in WebContents::CreateParams.
    2. When inspecting the opener - WebContents::GetOpener used to return
       WebContents rather than RenderFrameHost (misrepresenting an opener
       that is a subframe).
    
    This CL fixes this by:
    1. Adding an |opener| parameter to WebContentsDelegate::ShouldCreateWebContents
    2. Modifying constructor of BackgroundContents to take a new |opener|
       parameter and to pass it to WebContents::CreateParams.
    3. Modifying WebContents::CreateWithSessionStorage so that it sets
       the opener on the newly created contents.
    4. Changing WebContents::GetOpener and WebContents::GetOriginalOpener
       methods to return RenderFrameHost.
    
    The accurate opener was originally needed for a planned but ultimately
    abandoned UMA (see https://crbug.com/718516), but having an accurate
    opener is desirable in general.
    
    This CL re-enables AppBackgroundPageApiTest.OpenThenClose which was
    disabled due to flakiness 4 years ago.  This test is where the
    verification of the opener has to go.  I cannot repro the flakiness when
    trying the test locally (i.e. running the test 30 times individually,
    and 20 times as part of the whole AppBackgroundPageApiTest suite) or on
    the trybots (I've tried 3 x win_chromium_rel_ng, linux_chromium_rel_ng
    and mac_chromium_rel_ng).
    
    BUG=165644, 724280
    
    Review-Url: https://codereview.chromium.org/2882513005
    Cr-Commit-Position: refs/heads/master@{#477169}

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -1672,6 +1672,7 @@ void Browser::ShowRepostFormWarningDialog(WebContents* source) {
 
 bool Browser::ShouldCreateWebContents(
     content::WebContents* web_contents,
+    content::RenderFrameHost* opener,
     content::SiteInstance* source_site_instance,
     int32_t route_id,
     int32_t main_frame_route_id,
@@ -1686,7 +1687,7 @@ bool Browser::ShouldCreateWebContents(
       content::mojom::WindowContainerType::BACKGROUND) {
     // If a BackgroundContents is created, suppress the normal WebContents.
     return !MaybeCreateBackgroundContents(
-        source_site_instance, opener_url, route_id, main_frame_route_id,
+        source_site_instance, opener, opener_url, route_id, main_frame_route_id,
         main_frame_widget_route_id, frame_name, target_url, partition_id,
         session_storage_namespace);
   }
@@ -2550,6 +2551,7 @@ bool Browser::ShouldStartShutdown() const {
 
 bool Browser::MaybeCreateBackgroundContents(
     content::SiteInstance* source_site_instance,
+    content::RenderFrameHost* opener,
     const GURL& opener_url,
     int32_t route_id,
     int32_t main_frame_route_id,
@@ -2607,7 +2609,7 @@ bool Browser::MaybeCreateBackgroundContents(
   BackgroundContents* contents = nullptr;
   if (allow_js_access) {
     contents = service->CreateBackgroundContents(
-        source_site_instance, route_id, main_frame_route_id,
+        source_site_instance, opener, route_id, main_frame_route_id,
         main_frame_widget_route_id, profile_, frame_name,
         base::ASCIIToUTF16(extension->id()), partition_id,
         session_storage_namespace);
@@ -2619,7 +2621,7 @@ bool Browser::MaybeCreateBackgroundContents(
     contents = service->CreateBackgroundContents(
         content::SiteInstance::Create(
             source_site_instance->GetBrowserContext()),
-        MSG_ROUTING_NONE, MSG_ROUTING_NONE, MSG_ROUTING_NONE, profile_,
+        nullptr, MSG_ROUTING_NONE, MSG_ROUTING_NONE, MSG_ROUTING_NONE, profile_,
         frame_name, base::ASCIIToUTF16(extension->id()), partition_id,
         session_storage_namespace);
 
--- content/browser/web_contents/web_contents_impl.h
+++ content/browser/web_contents/web_contents_impl.h
@@ -432,9 +432,9 @@ class CONTENT_EXPORT WebContentsImpl
   gfx::Size GetPreferredSize() const override;
   bool GotResponseToLockMouseRequest(bool allowed) override;
   bool HasOpener() const override;
-  WebContentsImpl* GetOpener() const override;
+  RenderFrameHostImpl* GetOpener() const override;
   bool HasOriginalOpener() const override;
-  WebContents* GetOriginalOpener() const override;
+  RenderFrameHostImpl* GetOriginalOpener() const override;
   void DidChooseColorInColorChooser(SkColor color) override;
   void DidEndColorChooser() override;
   int DownloadImage(const GURL& url,
@@ -1187,6 +1187,8 @@ class CONTENT_EXPORT WebContentsImpl
 
   // Sends a Page message IPC.
   void SendPageMessage(IPC::Message* msg);
+
+  void SetOpenerForNewContents(FrameTreeNode* opener, bool opener_suppressed);
 
   // Tracking loading progress -------------------------------------------------
 
