5f98c21dcab1ff2dc0a5be4e49ad90fbb4cca3b0
kojii <kojii@chromium.org>
2017-06-06 06:25:01 -0700

    [LayoutNG] Handle empty inlines and border edges
    After we supported inline margin/border/padding[1] and adding box
    fragments when needed[2], this patch adds a few finishing touches:
    
    1. Creates empty box fragments when needed. [2] creates box fragments
       for inline boxes, but does not create when it's empty.
       Whether to create empty box fragments or not affects layout, since
       such inline boxes affects line height. The behavior isn't well
       defined, but there are some CSS2 tests. NG passes them now.
    2. Improve "Edges" support. When an inline box was broken by line wrap
       or inline continuation, the broken edge should not have margins/
       borders/paddings. This patch supports suppressing borders on edges.
       Note, computing positions for edges was done in [1].
    3. Add margins/borders/paddings to NGInlineItemResult to re-use in
       later phases.
    4. Some minor fixes found in wpt/css/CSS2 test suites.
    
    #2 was needed because it was working magically in common cases, but #1
    broke the magic. Implementing it properly also helps NG paint work.
    
    [1] https://codereview.chromium.org/2865903002
    [2] https://codereview.chromium.org/2898413002
    
    BUG=636993
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    
    Review-Url: https://codereview.chromium.org/2910133002
    Cr-Commit-Position: refs/heads/master@{#477273}

--- third_party/WebKit/Source/core/layout/ng/ng_physical_fragment.h
+++ third_party/WebKit/Source/core/layout/ng/ng_physical_fragment.h
@@ -55,10 +55,8 @@ class CORE_EXPORT NGPhysicalFragment : public RefCounted<NGPhysicalFragment> {
   // Returns the border-box size.
   NGPhysicalSize Size() const { return size_; }
 
-  // Bitmask for border edges, see NGPaintBorderEdge.
-  NGBorderEdges::Physical BorderEdges() const {
-    return static_cast<NGBorderEdges::Physical>(border_edge_);
-  }
+  // Bitmask for border edges, see NGBorderEdges::Physical.
+  unsigned BorderEdges() const { return border_edge_; }
   NGPixelSnappedPhysicalBoxStrut BorderWidths() const;
 
   // Returns the offset relative to the parent fragment's content-box.
@@ -105,7 +103,7 @@ class CORE_EXPORT NGPhysicalFragment : public RefCounted<NGPhysicalFragment> {
 
   unsigned type_ : 2;  // NGFragmentType
   unsigned is_placed_ : 1;
-  unsigned border_edge_ : 4;  // NGPhysicalBorderEdges
+  unsigned border_edge_ : 4;  // NGBorderEdges::Physical
 
  private:
   void Destroy() const;
