415bcaeda4de92b5e22b50e6511dcac8990bfbec
btolsch <btolsch@chromium.org>
2017-06-06 12:48:19 -0700

    Add DiscoveryNetworkMonitor implementation
    This changes adds a singleton class DiscoveryNetworkMonitor which
    provides general information to services doing device discovery on the
    local network.
    
    BUG=698943
    
    Review-Url: https://codereview.chromium.org/2750453002
    Cr-Commit-Position: refs/heads/master@{#477378}

---
+++ chrome/browser/media/router/discovery/discovery_network_info.cc
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/media/router/discovery/discovery_network_info.h"
+
+DiscoveryNetworkInfo::DiscoveryNetworkInfo() {}
+
+DiscoveryNetworkInfo::DiscoveryNetworkInfo(const std::string& name,
+                                           const std::string& network_id)
+    : name(name), network_id(network_id) {}
+
+DiscoveryNetworkInfo::~DiscoveryNetworkInfo() {}
+
+DiscoveryNetworkInfo::DiscoveryNetworkInfo(const DiscoveryNetworkInfo&) =
+    default;
+
+DiscoveryNetworkInfo& DiscoveryNetworkInfo::operator=(
+    const DiscoveryNetworkInfo&) = default;
+
+bool DiscoveryNetworkInfo::operator==(const DiscoveryNetworkInfo& other) const {
+  return name == other.name && network_id == other.network_id;
+}
+
+bool DiscoveryNetworkInfo::operator!=(const DiscoveryNetworkInfo& o) const {
+  return !(*this == o);
+}
