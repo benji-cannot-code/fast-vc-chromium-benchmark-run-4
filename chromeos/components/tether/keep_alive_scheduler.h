d279d17456988a89f2c3e94cd9092823d957a354
khorimoto <khorimoto@chromium.org>
2017-06-06 12:13:35 -0700

    [CrOS Tether] Update the KeepAliveTickle code to receive DeviceStatus updates.
    This CL consists of three parts:
    (1) Update KeepAliveOperation to receive a KeepAliveTickleResponse message after sending a KeepAliveTickle message.
    (2) Update KeepAliveScheduler to handle a received message and record the updated information in HostScanCache.
    (3) Integrate KeepAliveScheduler into the app (i.e., initialize it in Initializer).
    
    BUG=672263
    
    Review-Url: https://codereview.chromium.org/2918863002
    Cr-Commit-Position: refs/heads/master@{#477362}

--- chromeos/components/tether/keep_alive_scheduler.h
+++ chromeos/components/tether/keep_alive_scheduler.h
@@ -12,11 +12,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/timer/timer.h"
 #include "chromeos/components/tether/active_host.h"
+#include "chromeos/components/tether/device_status_util.h"
 #include "chromeos/components/tether/keep_alive_operation.h"
 
 namespace chromeos {
 
 namespace tether {
+
+class HostScanCache;
+class DeviceIdTetherNetworkGuidMap;
 
 // Schedules keep-alive messages to be sent when this device is connected to a
 // remote device's tether hotspot. When a device connects, a keep-alive message
@@ -25,8 +29,11 @@ namespace tether {
 class KeepAliveScheduler : public ActiveHost::Observer,
                            public KeepAliveOperation::Observer {
  public:
-  KeepAliveScheduler(ActiveHost* active_host,
-                     BleConnectionManager* connection_manager);
+  KeepAliveScheduler(
+      ActiveHost* active_host,
+      BleConnectionManager* connection_manager,
+      HostScanCache* host_scan_cache,
+      DeviceIdTetherNetworkGuidMap* device_id_tether_network_guid_map);
   virtual ~KeepAliveScheduler();
 
   // ActiveHost::Observer:
@@ -34,14 +41,19 @@ class KeepAliveScheduler : public ActiveHost::Observer,
       const ActiveHost::ActiveHostChangeInfo& change_info) override;
 
   // KeepAliveOperation::Observer:
-  void OnOperationFinished() override;
+  void OnOperationFinished(
+      const cryptauth::RemoteDevice& remote_device,
+      std::unique_ptr<DeviceStatus> device_status) override;
 
  private:
   friend class KeepAliveSchedulerTest;
 
-  KeepAliveScheduler(ActiveHost* active_host,
-                     BleConnectionManager* connection_manager,
-                     std::unique_ptr<base::Timer> timer);
+  KeepAliveScheduler(
+      ActiveHost* active_host,
+      BleConnectionManager* connection_manager,
+      HostScanCache* host_scan_cache,
+      DeviceIdTetherNetworkGuidMap* device_id_tether_network_guid_map,
+      std::unique_ptr<base::Timer> timer);
 
   void SendKeepAliveTickle();
 
@@ -49,6 +61,8 @@ class KeepAliveScheduler : public ActiveHost::Observer,
 
   ActiveHost* active_host_;
   BleConnectionManager* connection_manager_;
+  HostScanCache* host_scan_cache_;
+  DeviceIdTetherNetworkGuidMap* device_id_tether_network_guid_map_;
 
   std::unique_ptr<base::Timer> timer_;
   std::shared_ptr<cryptauth::RemoteDevice> active_host_device_;
