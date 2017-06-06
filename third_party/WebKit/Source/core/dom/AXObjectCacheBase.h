ff703526d0831be03dd91ef75e83b01b816b44cd
Nicholas Verne <nverne@chromium.org>
2017-06-06 10:38:26 +1000

    Allows WebAXObject creation from a const WebNode.
    To avoid const_cast-ing the FromWebNode() argument, AXObjectCacheBase's
    getter changed from Get(Node*) to Get(const Node*), and the underlying 
    WebHashMap in AXObjectCacheImpl now uses Member<const Node> as its key
    type. 
    
    Bug: 715382
    Change-Id: I1fbc9cd5de32dfd6c6b38286388e22e28e66f0cf
    Reviewed-on: https://chromium-review.googlesource.com/523925
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Stuart Langley <slangley@chromium.org>
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477189}

--- third_party/WebKit/Source/core/dom/AXObjectCacheBase.h
+++ third_party/WebKit/Source/core/dom/AXObjectCacheBase.h
@@ -26,7 +26,7 @@ class CORE_EXPORT AXObjectCacheBase : public AXObjectCache {
  public:
   virtual ~AXObjectCacheBase();
 
-  virtual AXObjectImpl* Get(Node*) = 0;
+  virtual AXObjectImpl* Get(const Node*) = 0;
 
  protected:
   AXObjectCacheBase();
