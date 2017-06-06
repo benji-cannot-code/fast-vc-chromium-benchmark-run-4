581f2e63f1174c3321097451159b20c819f64127
victorhsieh <victorhsieh@chromium.org>
2017-06-06 12:42:58 -0700

    Turn ArcSupportHost from Observer model to Delegate
    ArcSupportHost's subscribers have some overlap of responsibility, and
    there are several places to skip the work in case it is supposed to be
    done in the other subscriber.
    
    Instead, this change turns the observer model into several delegates
    for better responsibility split.
    
    TEST=unit_test --gtest_filter=Arc*
    TEST=browser_test --gtest_filer=Arc*
    TEST=(disable silent sign-in) accept ToS, click sign-in and unplug
         ethernet cable.  See feedback button.  Click retry, click sign-in,
         See Play Store.
    TEST=(disable silent sign-in) accept ToS, force logout from Chrome OS.
         Re-login.  See the window pops up with sign-in button.
    TEST=git cl try
    BUG=698418
    
    Review-Url: https://codereview.chromium.org/2844383006
    Cr-Commit-Position: refs/heads/master@{#477376}

--- chrome/browser/chromeos/arc/optin/arc_terms_of_service_default_negotiator.cc
+++ chrome/browser/chromeos/arc/optin/arc_terms_of_service_default_negotiator.cc
@@ -21,7 +21,7 @@ ArcTermsOfServiceDefaultNegotiator::ArcTermsOfServiceDefaultNegotiator(
 }
 
 ArcTermsOfServiceDefaultNegotiator::~ArcTermsOfServiceDefaultNegotiator() {
-  support_host_->RemoveObserver(this);
+  support_host_->SetTermsOfServiceDelegate(nullptr);
 }
 
 void ArcTermsOfServiceDefaultNegotiator::StartNegotiationImpl() {
@@ -31,15 +31,15 @@ void ArcTermsOfServiceDefaultNegotiator::StartNegotiationImpl() {
   // This automatically updates all preferences.
   preference_handler_->Start();
 
-  support_host_->AddObserver(this);
+  support_host_->SetTermsOfServiceDelegate(this);
   support_host_->ShowTermsOfService();
 }
 
-void ArcTermsOfServiceDefaultNegotiator::OnWindowClosed() {
+void ArcTermsOfServiceDefaultNegotiator::OnTermsRejected() {
   // User cancels terms-of-service agreement UI by clicking "Cancel" button
   // or closing the window directly.
   DCHECK(preference_handler_);
-  support_host_->RemoveObserver(this);
+  support_host_->SetTermsOfServiceDelegate(nullptr);
   preference_handler_.reset();
 
   ReportResult(false);
@@ -50,7 +50,7 @@ void ArcTermsOfServiceDefaultNegotiator::OnTermsAgreed(
     bool is_backup_and_restore_enabled,
     bool is_location_service_enabled) {
   DCHECK(preference_handler_);
-  support_host_->RemoveObserver(this);
+  support_host_->SetTermsOfServiceDelegate(nullptr);
 
   // Update the preferences with the value passed from UI.
   preference_handler_->EnableMetrics(is_metrics_enabled);
@@ -61,17 +61,8 @@ void ArcTermsOfServiceDefaultNegotiator::OnTermsAgreed(
   ReportResult(true);
 }
 
-void ArcTermsOfServiceDefaultNegotiator::OnAuthSucceeded(
-    const std::string& auth_code) {
-  NOTREACHED();
-}
-
-void ArcTermsOfServiceDefaultNegotiator::OnRetryClicked() {
+void ArcTermsOfServiceDefaultNegotiator::OnTermsRetryClicked() {
   support_host_->ShowTermsOfService();
-}
-
-void ArcTermsOfServiceDefaultNegotiator::OnSendFeedbackClicked() {
-  NOTREACHED();
 }
 
 void ArcTermsOfServiceDefaultNegotiator::OnMetricsModeChanged(bool enabled,
