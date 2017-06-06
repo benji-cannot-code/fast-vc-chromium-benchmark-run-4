c7d7781c63653e5754dd0b597b9dd712f7a3cbcb
Quinten Yearsley <qyearsley@google.com>
2017-06-06 10:46:38 -0700

    In webkitpy unit tests, use "with self.assertRaises".
    This is a mechanical change that changes
    
      self.assertRaises(ExceptionClass, function,
                        arg1, ...)
    
    to
    
      with self.assertRaises(ExceptionClass):
          function(arg1, ...)
    
    The rationale is that in general I find this more readable, because it
    shows clearly how a function invocation that causes an exception looks.
    
    Change-Id: Idac75f22d1a3b08024e6b0b447f5c59694e3b807
    Reviewed-on: https://chromium-review.googlesource.com/525936
    Reviewed-by: Tim 'mithro' Ansell <tansell@chromium.org>
    Commit-Queue: Quinten Yearsley <qyearsley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477524}

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/models/test_expectations_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/models/test_expectations_unittest.py
@@ -92,7 +92,8 @@ Bug(test) failures/expected/image.html [ Crash Mac ]
         self.assert_exp_list(test, [result])
 
     def assert_bad_expectations(self, expectations, overrides=None):
-        self.assertRaises(ParseError, self.parse_exp, expectations, is_lint_mode=True, overrides=overrides)
+        with self.assertRaises(ParseError):
+            self.parse_exp(expectations, is_lint_mode=True, overrides=overrides)
 
 
 class BasicTests(Base):
@@ -171,8 +172,8 @@ class MiscTests(Base):
         self.parse_exp(exp_str)
         test_name = 'failures/expected/unknown-test.html'
         unknown_test = test_name
-        self.assertRaises(KeyError, self._exp.get_expectations,
-                          unknown_test)
+        with self.assertRaises(KeyError):
+            self._exp.get_expectations(unknown_test)
         self.assert_exp_list('failures/expected/crash.html', [PASS])
 
     def test_get_expectations_string(self):
@@ -182,8 +183,8 @@ class MiscTests(Base):
     def test_expectation_to_string(self):
         # Normal cases are handled by other tests.
         self.parse_exp(self.get_basic_expectations())
-        self.assertRaises(ValueError, self._exp.expectation_to_string,
-                          -1)
+        with self.assertRaises(ValueError):
+            self._exp.expectation_to_string(-1)
 
     def test_get_test_set(self):
         # Handle some corner cases for this routine not covered by other tests.
@@ -289,21 +290,19 @@ Bug(user) reftests/failures/expected/needsmanualrebaseline_with_txt.html [ Needs
 
     def test_error_on_different_platform(self):
         # parse_exp uses a Windows port. Assert errors on Mac show up in lint mode.
-        self.assertRaises(
-            ParseError,
-            self.parse_exp,
-            ('Bug(test) [ Mac ] failures/expected/text.html [ Failure ]\n'
-             'Bug(test) [ Mac ] failures/expected/text.html [ Failure ]'),
-            is_lint_mode=True)
+        with self.assertRaises(ParseError):
+            self.parse_exp(
+                'Bug(test) [ Mac ] failures/expected/text.html [ Failure ]\n'
+                'Bug(test) [ Mac ] failures/expected/text.html [ Failure ]',
+                is_lint_mode=True)
 
     def test_error_on_different_build_type(self):
         # parse_exp uses a Release port. Assert errors on DEBUG show up in lint mode.
-        self.assertRaises(
-            ParseError,
-            self.parse_exp,
-            ('Bug(test) [ Debug ] failures/expected/text.html [ Failure ]\n'
-             'Bug(test) [ Debug ] failures/expected/text.html [ Failure ]'),
-            is_lint_mode=True)
+        with self.assertRaises(ParseError):
+            self.parse_exp(
+                'Bug(test) [ Debug ] failures/expected/text.html [ Failure ]\n'
+                'Bug(test) [ Debug ] failures/expected/text.html [ Failure ]',
+                is_lint_mode=True)
 
     def test_overrides(self):
         self.parse_exp('Bug(exp) failures/expected/text.html [ Failure ]',
@@ -418,8 +417,10 @@ class SkippedTests(Base):
         self.check(expectations='', overrides=None, skips=['failures/expected/text.html'], expected_results=[WONTFIX, SKIP])
 
     def test_duplicate_skipped_test_fails_lint(self):
-        self.assertRaises(ParseError, self.check, expectations='Bug(x) failures/expected/text.html [ Failure ]\n',
-                          overrides=None, skips=['failures/expected/text.html'], lint=True)
+        with self.assertRaises(ParseError):
+            self.check(
+                expectations='Bug(x) failures/expected/text.html [ Failure ]\n',
+                overrides=None, skips=['failures/expected/text.html'], lint=True)
 
     def test_skipped_file_overrides_expectations(self):
         self.check(expectations='Bug(x) failures/expected/text.html [ Failure ]\n', overrides=None,
@@ -539,7 +540,8 @@ class ExpectationSyntaxTests(Base):
 class SemanticTests(Base):
 
     def test_bug_format(self):
-        self.assertRaises(ParseError, self.parse_exp, 'BUG1234 failures/expected/text.html [ Failure ]', is_lint_mode=True)
+        with self.assertRaises(ParseError):
+            self.parse_exp('BUG1234 failures/expected/text.html [ Failure ]', is_lint_mode=True)
 
     def test_bad_bugid(self):
         try:
@@ -571,17 +573,18 @@ class SemanticTests(Base):
 
     def test_rebaseline(self):
         # Can't lint a file w/ 'REBASELINE' in it.
-        self.assertRaises(ParseError, self.parse_exp,
-                          'Bug(test) failures/expected/text.html [ Failure Rebaseline ]',
-                          is_lint_mode=True)
+        with self.assertRaises(ParseError):
+            self.parse_exp(
+                'Bug(test) failures/expected/text.html [ Failure Rebaseline ]',
+                is_lint_mode=True)
 
     def test_duplicates(self):
         self.assertRaises(ParseError, self.parse_exp, """
 Bug(exp) failures/expected/text.html [ Failure ]
 Bug(exp) failures/expected/text.html [ Timeout ]""", is_lint_mode=True)
-
-        self.assertRaises(ParseError, self.parse_exp,
-                          self.get_basic_expectations(), overrides="""
+        with self.assertRaises(ParseError):
+            self.parse_exp(
+                self.get_basic_expectations(), overrides="""
 Bug(override) failures/expected/text.html [ Failure ]
 Bug(override) failures/expected/text.html [ Timeout ]""", is_lint_mode=True)
 
