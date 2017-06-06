fda2a2945f63994be9b755b8668add1bf6af8ffd
donnd <donnd@chromium.org>
2017-06-06 10:27:17 -0700

    [TTS] Default-enable Ranker logging.
    Changes the Field-trial param from a boolean to enable to a
    boolean that can be used to disable in case of unforseen issues.
    
    Removes the CS section from fieldtrial_testing_config since it's
    no longer needed.
    
    BUG=676108
    
    Review-Url: https://codereview.chromium.org/2919003004
    Cr-Commit-Position: refs/heads/master@{#477323}

--- chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchRankerLoggerImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchRankerLoggerImpl.java
@@ -152,7 +152,7 @@ public class ContextualSearchRankerLoggerImpl implements ContextualSearchRankerL
 
     /** Whether actually writing data is enabled.  If not, we may do nothing, or just print. */
     private boolean isEnabled() {
-        return ContextualSearchFieldTrial.isRankerLoggingEnabled();
+        return !ContextualSearchFieldTrial.isRankerLoggingDisabled();
     }
 
     /**
