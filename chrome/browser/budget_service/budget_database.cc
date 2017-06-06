2d02a04a1c9d0eae83c6bb4016ee55829c022502
peter <peter@chromium.org>
2017-06-05 18:33:56 -0700

    Always use milliseconds in the Budget API implementation
    BUG=729656
    
    Review-Url: https://codereview.chromium.org/2922203002
    Cr-Commit-Position: refs/heads/master@{#477147}

--- chrome/browser/budget_service/budget_database.cc
+++ chrome/browser/budget_service/budget_database.cc
@@ -157,7 +157,7 @@ void BudgetDatabase::GetBudgetAfterSync(const url::Origin& origin,
   // Always add one entry at the front of the list for the total budget now.
   blink::mojom::BudgetStatePtr prediction(blink::mojom::BudgetState::New());
   prediction->budget_at = total;
-  prediction->time = clock_->Now().ToDoubleT();
+  prediction->time = clock_->Now().ToJsTime();
   predictions.push_back(std::move(prediction));
 
   // Starting with the soonest expiring chunks, add entries for the
@@ -167,7 +167,7 @@ void BudgetDatabase::GetBudgetAfterSync(const url::Origin& origin,
     blink::mojom::BudgetStatePtr prediction(blink::mojom::BudgetState::New());
     total -= chunk.amount;
     prediction->budget_at = total;
-    prediction->time = chunk.expiration.ToDoubleT();
+    prediction->time = chunk.expiration.ToJsTime();
     predictions.push_back(std::move(prediction));
   }
 
