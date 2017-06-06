72c073ff65626d70aa2602e37d5ee90b2871640a
hajimehoshi <hajimehoshi@chromium.org>
2017-06-05 21:47:03 -0700

    Revert "Bug fix: TranferBufferManager's SharedMemory memory usage reporting was wrong"
    This reverts commit 6c8897721e2df9a0ec3677d0a13d677c457e5e1e.
    
    The previous change removes 'shared' global dumps when the backend is
    not base::SharedMemory, but actually the 'shared' global dumps are
    needed regardless of the backend. 'Shared' in this context means not only
    base::SharedMemory but also wider things.
    
    BUG=604726
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2912723002
    Cr-Commit-Position: refs/heads/master@{#477197}

--- gpu/command_buffer/common/buffer.cc
+++ gpu/command_buffer/common/buffer.cc
@@ -15,10 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gpu {
 
-bool BufferBacking::is_shared() const {
-  return false;
-}
-
 base::SharedMemoryHandle BufferBacking::shared_memory_handle() const {
   return base::SharedMemoryHandle();
 }
@@ -29,10 +25,6 @@ SharedMemoryBufferBacking::SharedMemoryBufferBacking(
     : shared_memory_(std::move(shared_memory)), size_(size) {}
 
 SharedMemoryBufferBacking::~SharedMemoryBufferBacking() {}
-
-bool SharedMemoryBufferBacking::is_shared() const {
-  return true;
-}
 
 base::SharedMemoryHandle SharedMemoryBufferBacking::shared_memory_handle()
     const {
