d5290353f96013a1e9cf869495cceed070e1d251
Mounir Lamouri <mlamouri@chromium.org>
2017-06-06 13:53:16 +0100

    Report watch time for native controls (and non-native).
    This is reported for Audio and AudioVideo. There is no distinction
    based on the type of media (MSE, EME, SRC, ...).
    
    Bug: 686805
    Change-Id: I8b2fd443e5e53d1edea347c315b1415a17cb71d5
    Reviewed-on: https://chromium-review.googlesource.com/518007
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477347}

--- media/blink/webmediaplayer_impl.h
+++ media/blink/webmediaplayer_impl.h
@@ -194,6 +194,7 @@ class MEDIA_BLINK_EXPORT WebMediaPlayerImpl
   void ExitedFullscreen() override;
   void BecameDominantVisibleContent(bool isDominant) override;
   void SetIsEffectivelyFullscreen(bool isEffectivelyFullscreen) override;
+  void OnHasNativeControlsChanged(bool) override;
 
   // WebMediaPlayerDelegate::Observer implementation.
   void OnFrameHidden() override;
--- third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
@@ -3681,10 +3681,14 @@ void HTMLMediaElement::UpdateControlsVisibility() {
   // fullscreen/compositor-touch-hit-rects-fullscreen-video-controls.html
   GetMediaControls()->Reset();
 
-  if (ShouldShowControls(RecordMetricsBehavior::kDoRecord))
+  bool native_controls = ShouldShowControls(RecordMetricsBehavior::kDoRecord);
+  if (native_controls)
     GetMediaControls()->MaybeShow();
   else
     GetMediaControls()->Hide();
+
+  if (web_media_player_)
+    web_media_player_->OnHasNativeControlsChanged(native_controls);
 }
 
 CueTimeline& HTMLMediaElement::GetCueTimeline() {
@@ -4044,6 +4048,10 @@ void HTMLMediaElement::ActivateViewportIntersectionMonitoring(bool activate) {
   } else if (!activate) {
     check_viewport_intersection_timer_.Stop();
   }
+}
+
+bool HTMLMediaElement::HasNativeControls() {
+  return ShouldShowControls(RecordMetricsBehavior::kDoRecord);
 }
 
 void HTMLMediaElement::CheckViewportIntersectionTimerFired(TimerBase*) {
