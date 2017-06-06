89ccf0c46272a5ca230754e8e3a0460e6dbcb110
yhanada <yhanada@chromium.org>
2017-06-06 18:15:33 +0900

    Implment ArcCustomNotificationView::GetAccessibleNodeData().
    After making CustomNotificationView pass the focus to
    ArcCustomNotificationView,
    ArcCustomNotificationView::GetAccessibleNodeData is used instead
    of MessageView::GetAccessibleNodeData to get the accessible content
    by ChromeVox. However,
    ArcCustomNotificationView::GetAccessibleNodeData is not implemented,
    so ChromeVox can't read content of any Android notification.
    
    BUG=727288
    
    Change-Id: Ib370c7be8dfbbf1bbbe55c4f3163fa7904e8d939
    Reviewed-on: https://chromium-review.googlesource.com/517448
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477248}

--- ui/arc/notification/arc_notification_content_view.cc
+++ ui/arc/notification/arc_notification_content_view.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/notification_surface.h"
 #include "components/exo/surface.h"
 #include "ui/accessibility/ax_action_data.h"
+#include "ui/accessibility/ax_node_data.h"
 #include "ui/arc/notification/arc_notification_view.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
@@ -253,6 +254,7 @@ ArcNotificationContentView::ArcNotificationContentView(
   // Create a layer as an anchor to insert surface copy during a slide.
   SetPaintToLayer();
   UpdatePreferredSize();
+  UpdateAccessibleName();
 }
 
 ArcNotificationContentView::~ArcNotificationContentView() {
@@ -486,6 +488,14 @@ bool ArcNotificationContentView::ShouldUpdateControlButtonsColor() const {
   return false;
 }
 
+void ArcNotificationContentView::UpdateAccessibleName() {
+  // Don't update the accessible name when we are about to be destroyed.
+  if (!item_)
+    return;
+
+  accessible_name_ = item_->GetAccessibleName();
+}
+
 void ArcNotificationContentView::ViewHierarchyChanged(
     const views::View::ViewHierarchyChangedDetails& details) {
   views::Widget* widget = GetWidget();
@@ -626,6 +636,12 @@ bool ArcNotificationContentView::HandleAccessibleAction(
   return false;
 }
 
+void ArcNotificationContentView::GetAccessibleNodeData(
+    ui::AXNodeData* node_data) {
+  node_data->role = ui::AX_ROLE_BUTTON;
+  node_data->SetName(accessible_name_);
+}
+
 void ArcNotificationContentView::ButtonPressed(views::Button* sender,
                                                const ui::Event& event) {
   if (item_ && !item_->GetPinned() && sender == close_button_.get()) {
@@ -662,6 +678,7 @@ void ArcNotificationContentView::OnItemDestroying() {
 }
 
 void ArcNotificationContentView::OnItemUpdated() {
+  UpdateAccessibleName();
   UpdatePinnedState();
   UpdateSnapshot();
   if (ShouldUpdateControlButtonsColor())
