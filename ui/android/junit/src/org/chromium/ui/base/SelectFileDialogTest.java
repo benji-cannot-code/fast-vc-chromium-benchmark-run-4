583a5493a67f1e2ed3dffa1b9ff564b86a956285
finnur <finnur@chromium.org>
2017-06-06 13:48:45 -0700

    Photo Picker dialog: Convert disabled test to Roboelectric.
    BUG=728247, 656015
    
    TBR=tedchoc
    
    Review-Url: https://codereview.chromium.org/2915283002
    Cr-Commit-Position: refs/heads/master@{#477401}

--- ui/android/junit/src/org/chromium/ui/base/SelectFileDialogTest.java
+++ ui/android/junit/src/org/chromium/ui/base/SelectFileDialogTest.java
@@ -6,10 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.ui.base;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import android.webkit.MimeTypeMap;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowMimeTypeMap;
 
 import org.chromium.testing.local.LocalRobolectricTestRunner;
 
@@ -87,5 +93,30 @@ public class SelectFileDialogTest {
                 scopeForFileTypes("image/jpeg", "video/ogg"));
         assertEquals(SelectFileDialog.SELECT_FILE_DIALOG_SCOPE_GENERIC,
                 scopeForFileTypes("video/*", "image/*", "text/plain"));
+    }
+
+    @Test
+    public void testPhotoPickerLaunchAndMimeTypes() throws Throwable {
+        ShadowMimeTypeMap shadowMimeTypeMap = Shadows.shadowOf(MimeTypeMap.getSingleton());
+        shadowMimeTypeMap.addExtensionMimeTypMapping("jpg", "image/jpeg");
+        shadowMimeTypeMap.addExtensionMimeTypMapping("gif", "image/gif");
+        shadowMimeTypeMap.addExtensionMimeTypMapping("txt", "text/plain");
+        shadowMimeTypeMap.addExtensionMimeTypMapping("mpg", "audio/mpeg");
+
+        assertEquals("", SelectFileDialog.ensureMimeType(""));
+        assertEquals("image/jpeg", SelectFileDialog.ensureMimeType(".jpg"));
+        assertEquals("image/jpeg", SelectFileDialog.ensureMimeType("image/jpeg"));
+        // Unknown extension, expect default response:
+        assertEquals("application/octet-stream", SelectFileDialog.ensureMimeType(".flv"));
+
+        assertFalse(SelectFileDialog.usePhotoPicker(Arrays.asList("")));
+        assertTrue(SelectFileDialog.usePhotoPicker(Arrays.asList(".jpg")));
+        assertTrue(SelectFileDialog.usePhotoPicker(Arrays.asList("image/jpeg")));
+        assertTrue(SelectFileDialog.usePhotoPicker(Arrays.asList(".jpg", "image/jpeg")));
+        assertTrue(SelectFileDialog.usePhotoPicker(Arrays.asList(".gif", "image/jpeg")));
+        // Returns false because generic picker is required (due to addition of .txt file).
+        assertFalse(SelectFileDialog.usePhotoPicker(Arrays.asList(".txt", ".jpg", "image/jpeg")));
+        // Returns false because video file is included.
+        assertFalse(SelectFileDialog.usePhotoPicker(Arrays.asList(".jpg", "image/jpeg", ".mpg")));
     }
 }
