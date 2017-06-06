55556baacf4929ade472f67a04d8e32d7e6fc9ef
lukasza <lukasza@chromium.org>
2017-06-06 12:58:30 -0700

    Move LoadHTMLString method from WebFrame to WebLocalFrame.
    Most callers were already calling through WebLocalFrame (tests had to be
    changed to use MainFrameImpl accessor to realize that).  One test caller
    (ParameterizedWebFrameTest.GetContentAsPlainText) cannot guarantee at
    compile time that it has a local frame, but in practice the new
    ToWebLocalFrame() passes all tests.
    
    No intended behavior change.
    
    BUG=416660
    
    Review-Url: https://codereview.chromium.org/2917343004
    Cr-Commit-Position: refs/heads/master@{#477381}

--- third_party/WebKit/Source/web/tests/RootScrollerTest.cpp
+++ third_party/WebKit/Source/web/tests/RootScrollerTest.cpp
@@ -846,7 +846,7 @@ TEST_F(RootScrollerTest, TopControlsAdjustmentAppliedToRootScroller) {
   Initialize();
 
   WebURL base_url = URLTestHelpers::ToKURL("http://www.test.com/");
-  FrameTestHelpers::LoadHTMLString(GetWebView()->MainFrame(),
+  FrameTestHelpers::LoadHTMLString(GetWebView()->MainFrameImpl(),
                                    "<!DOCTYPE html>"
                                    "<style>"
                                    "  body, html {"
@@ -1123,7 +1123,7 @@ TEST_F(RootScrollerHitTest, HitTestInAreaRevealedByURLBarSameLayer) {
   // that we can hit this target.
   Initialize();
   WebURL baseURL = URLTestHelpers::ToKURL("http://www.test.com/");
-  FrameTestHelpers::LoadHTMLString(GetWebView()->MainFrame(),
+  FrameTestHelpers::LoadHTMLString(GetWebView()->MainFrameImpl(),
                                    "<!DOCTYPE html>"
                                    "<style>"
                                    "  body, html {"
@@ -1172,7 +1172,7 @@ TEST_F(RootScrollerHitTest, HitTestInAreaRevealedByURLBarDifferentLayer) {
   // that we can hit this target.
   Initialize();
   WebURL baseURL = URLTestHelpers::ToKURL("http://www.test.com/");
-  FrameTestHelpers::LoadHTMLString(GetWebView()->MainFrame(),
+  FrameTestHelpers::LoadHTMLString(GetWebView()->MainFrameImpl(),
                                    "<!DOCTYPE html>"
                                    "<style>"
                                    "  body, html {"
--- third_party/WebKit/Source/web/tests/ScrollingCoordinatorTest.cpp
+++ third_party/WebKit/Source/web/tests/ScrollingCoordinatorTest.cpp
@@ -92,7 +92,7 @@ class ScrollingCoordinatorTest : public ::testing::Test,
   }
 
   void LoadHTML(const std::string& html) {
-    FrameTestHelpers::LoadHTMLString(GetWebView()->MainFrame(), html,
+    FrameTestHelpers::LoadHTMLString(GetWebView()->MainFrameImpl(), html,
                                      URLTestHelpers::ToKURL("about:blank"));
   }
 
