c8ee7d3990c7b2318d23b148d902aba597d4caf1
fsamuel <fsamuel@chromium.org>
2017-06-05 20:03:47 -0700

    cc: De-special-case SurfaceLayer code
    Currently, there's a bunch of special casing for surface synchronization
    in cc::SurfaceLayer. This makes the code hard to read and reason about.
    This CL fixes the issue by unifying the two code paths.
    
    A Surface ID referenced as a fallback is always placed in |referenced_surfaces|,
    and a surface ID referenced as a primary surface is added to
    |activation_dependencies| if the fallback is valid and does not match the
    primary.
    
    This CL allows a single code path by setting both primary and fallback
    SurfaceInfos in all callsites.
    
    BUG=672962
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2919373002
    Cr-Commit-Position: refs/heads/master@{#477166}

--- third_party/WebKit/Source/platform/graphics/CanvasSurfaceLayerBridge.cpp
+++ third_party/WebKit/Source/platform/graphics/CanvasSurfaceLayerBridge.cpp
@@ -102,6 +102,7 @@ void CanvasSurfaceLayerBridge::OnSurfaceCreated(
     scoped_refptr<cc::SurfaceLayer> surface_layer =
         cc::SurfaceLayer::Create(ref_factory_);
     surface_layer->SetPrimarySurfaceInfo(surface_info);
+    surface_layer->SetFallbackSurfaceInfo(surface_info);
     surface_layer->SetStretchContentToFillBounds(true);
     cc_layer_ = surface_layer;
 
@@ -116,6 +117,7 @@ void CanvasSurfaceLayerBridge::OnSurfaceCreated(
     cc::SurfaceLayer* surface_layer =
         static_cast<cc::SurfaceLayer*>(cc_layer_.get());
     surface_layer->SetPrimarySurfaceInfo(surface_info);
+    surface_layer->SetFallbackSurfaceInfo(surface_info);
   }
 
   observer_->OnWebLayerReplaced();
