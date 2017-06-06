8f502aa9986fae926943b133e892836f5e98923c
vmpstr <vmpstr@chromium.org>
2017-06-05 20:07:18 -0700

    Add frame count to paint image.
    This patch adds the number of frames in the image to PaintImage.
    
    R=chrishtr@chromium.org
    BUG=693968
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2883593002
    Cr-Commit-Position: refs/heads/master@{#477168}

--- cc/paint/paint_image.cc
+++ cc/paint/paint_image.cc
@@ -15,11 +15,13 @@ PaintImage::PaintImage() = default;
 PaintImage::PaintImage(Id id,
                        sk_sp<SkImage> sk_image,
                        AnimationType animation_type,
-                       CompletionState completion_state)
+                       CompletionState completion_state,
+                       size_t frame_count)
     : id_(id),
       sk_image_(std::move(sk_image)),
       animation_type_(animation_type),
-      completion_state_(completion_state) {}
+      completion_state_(completion_state),
+      frame_count_(frame_count) {}
 PaintImage::PaintImage(const PaintImage& other) = default;
 PaintImage::PaintImage(PaintImage&& other) = default;
 PaintImage::~PaintImage() = default;
@@ -30,7 +32,8 @@ PaintImage& PaintImage::operator=(PaintImage&& other) = default;
 bool PaintImage::operator==(const PaintImage& other) const {
   return id_ == other.id_ && sk_image_ == other.sk_image_ &&
          animation_type_ == other.animation_type_ &&
-         completion_state_ == other.completion_state_;
+         completion_state_ == other.completion_state_ &&
+         frame_count_ == other.frame_count_;
 }
 
 PaintImage::Id PaintImage::GetNextId() {
--- third_party/WebKit/Source/platform/graphics/BitmapImage.h
+++ third_party/WebKit/Source/platform/graphics/BitmapImage.h
@@ -89,6 +89,7 @@ class PLATFORM_EXPORT BitmapImage final : public Image {
   bool CurrentFrameKnownToBeOpaque(MetadataMode = kUseCurrentMetadata) override;
   bool CurrentFrameIsComplete() override;
   bool CurrentFrameIsLazyDecoded() override;
+  size_t FrameCount() override;
 
   ImageOrientation CurrentFrameOrientation();
 
@@ -119,7 +120,6 @@ class PLATFORM_EXPORT BitmapImage final : public Image {
             ImageClampingMode) override;
 
   size_t CurrentFrame() const { return current_frame_; }
-  size_t FrameCount();
 
   sk_sp<SkImage> FrameAtIndex(size_t);
 
