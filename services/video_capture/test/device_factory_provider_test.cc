e605fe81d67cf5ff73ea140a6c8dc219ec63574b
Christian Fremerey <chfremer@chromium.org>
2017-06-06 13:25:51 -0700

    [Mojo Video Capture] Remove API for connecting to fake device factory
    This CL is part of the Mojo Video Capture work. For the bigger picture,
    see [1] CL26.2.
    
    The video capture service exposes an API for connecting to a fake device factory
    instance. This API is redundant with the command-line switch
    kUseFakeDeviceForMediaStream which is widely used across our integration tests
    to switch out the real factory with a fake one.
    
    This CL removes this redundant API. It also removes service-level integration
    tests FakeVideoCaptureDeviceTest.ReceiveFramesFromFakeCaptureDevice, which have
    become redundant with content_browser level intergration tests
    VideoCaptureBrowserTest.ReceiveFramesFromFakeCaptureDevice.
    
      content_browsertests --gtest_filter="*Video*"
    
    BUG: 584797
    TEST: 
    Change-Id: Ifb8bae50fda0329b60a2e88812016448a64d01cc
    Reviewed-on: https://chromium-review.googlesource.com/526358
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479129}

--- services/video_capture/test/device_factory_provider_test.cc
+++ services/video_capture/test/device_factory_provider_test.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/video_capture/test/device_factory_provider_test.h"
 
+#include "base/command_line.h"
+#include "media/base/media_switches.h"
 #include "services/service_manager/public/interfaces/constants.mojom.h"
 #include "services/service_manager/public/interfaces/service_manager.mojom.h"
 #include "services/video_capture/public/interfaces/constants.mojom.h"
@@ -24,6 +26,9 @@ DeviceFactoryProviderTest::DeviceFactoryProviderTest()
 DeviceFactoryProviderTest::~DeviceFactoryProviderTest() = default;
 
 void DeviceFactoryProviderTest::SetUp() {
+  base::CommandLine::ForCurrentProcess()->AppendSwitch(
+      switches::kUseFakeDeviceForMediaStream);
+
   service_manager::test::ServiceTest::SetUp();
 
   service_manager::mojom::ServiceManagerPtr service_manager;
@@ -38,7 +43,7 @@ void DeviceFactoryProviderTest::SetUp() {
 
   connector()->BindInterface(mojom::kServiceName, &factory_provider_);
   factory_provider_->SetShutdownDelayInSeconds(0.0f);
-  factory_provider_->ConnectToFakeDeviceFactory(mojo::MakeRequest(&factory_));
+  factory_provider_->ConnectToDeviceFactory(mojo::MakeRequest(&factory_));
 }
 
 }  // namespace video_capture
