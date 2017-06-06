09bd946889357378064723d73d9cd3784fcfa096
jam <jam@chromium.org>
2017-06-05 22:45:54 -0700

    Fix network service hanging with empty bodies.
    This fixes WebContentsBindingSetBrowserTest.CloseOnFrameDeletion with --enable-network-service.
    
    Also add a ScopedAllowSyncCall for the host resolver sync IPC. And update the filter with a new failing test.
    
    BUG=729848
    
    Review-Url: https://codereview.chromium.org/2919413003
    Cr-Commit-Position: refs/heads/master@{#477205}

--- content/public/test/browser_test_base.cc
+++ content/public/test/browser_test_base.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/test_launcher.h"
 #include "content/public/test/test_utils.h"
 #include "content/test/content_browser_sanity_checker.h"
+#include "mojo/public/cpp/bindings/sync_call_restrictions.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "services/service_manager/public/cpp/connector.h"
@@ -409,6 +410,7 @@ void BrowserTestBase::InitializeNetworkProcess() {
   mojom::NetworkServiceTestPtr network_service_test;
   ServiceManagerConnection::GetForProcess()->GetConnector()->BindInterface(
       mojom::kNetworkServiceName, &network_service_test);
+  mojo::SyncCallRestrictions::ScopedAllowSyncCall allow_sync_call;
   network_service_test->AddRules(std::move(mojo_rules));
 }
 
