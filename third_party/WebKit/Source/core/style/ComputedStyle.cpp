28c19274f90b69958ee98b43ce2b84038f390cf0
Darren Shen <shend@chromium.org>
2017-06-06 17:45:45 +1000

    Rename StyleWillChangeData members.
    When we generate fields in ComputedStyleBase, the name of the fields
    must be distinct. Sometimes, existing handwritten data members share
    the same name because they are stored on different groups. This may not
    be true when they become generated. Hence, we have to rename these
    members with a distinct name.
    
    This patch adds the "will_change" prefix to the names of all the
    StyleWillChange data members.
    
    There are no changes in behaviour.
    
    Bug=628043
    
    Change-Id: I5f7d484a2afedb4aa64e33dd1ea2665a2dc44611
    Reviewed-on: https://chromium-review.googlesource.com/525252
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Eddy Mead <meade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477230}

--- third_party/WebKit/Source/core/style/ComputedStyle.cpp
+++ third_party/WebKit/Source/core/style/ComputedStyle.cpp
@@ -1010,10 +1010,11 @@ void ComputedStyle::SetContent(ContentData* content_data) {
 }
 
 bool ComputedStyle::HasWillChangeCompositingHint() const {
-  for (size_t i = 0;
-       i < rare_non_inherited_data_->will_change_data_->properties_.size();
+  for (size_t i = 0; i < rare_non_inherited_data_->will_change_data_
+                             ->will_change_properties_.size();
        ++i) {
-    switch (rare_non_inherited_data_->will_change_data_->properties_[i]) {
+    switch (rare_non_inherited_data_->will_change_data_
+                ->will_change_properties_[i]) {
       case CSSPropertyOpacity:
       case CSSPropertyTransform:
       case CSSPropertyAliasWebkitTransform:
@@ -1031,7 +1032,7 @@ bool ComputedStyle::HasWillChangeCompositingHint() const {
 
 bool ComputedStyle::HasWillChangeTransformHint() const {
   for (const auto& property :
-       rare_non_inherited_data_->will_change_data_->properties_) {
+       rare_non_inherited_data_->will_change_data_->will_change_properties_) {
     switch (property) {
       case CSSPropertyTransform:
       case CSSPropertyAliasWebkitTransform:
