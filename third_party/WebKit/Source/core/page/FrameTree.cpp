eabf2dbbedda18510f6c0a9d771689d525a7aa20
Andy Paicu <andypaicu@chromium.org>
2017-06-06 11:57:49 +0200

    Added another counter for cross origin name that will ignore no-ops
    Bug: 706350
    Change-Id: Ib91d511d5053a8504cf2d071ff5fc51b81009b81
    Reviewed-on: https://chromium-review.googlesource.com/522584
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477266}

--- third_party/WebKit/Source/core/page/FrameTree.cpp
+++ third_party/WebKit/Source/core/page/FrameTree.cpp
@@ -59,6 +59,10 @@ const AtomicString& FrameTree::GetName() const {
     if (frame) {
       UseCounter::Count(frame,
                         UseCounter::kCrossOriginMainFrameNulledNameAccessed);
+      if (!name_.IsEmpty()) {
+        UseCounter::Count(
+            frame, UseCounter::kCrossOriginMainFrameNulledNonEmptyNameAccessed);
+      }
     }
   }
   return name_;
