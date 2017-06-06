5facc1108cfcaac56c7ee9f3d9f18eb4ee3eb5ff
yuweih <yuweih@chromium.org>
2017-06-06 16:35:57 -0700

    [CRD iOS] Finish up the host list UI
    This CL finishes up matching the host list UX mocks. It:
    
    * Changes the menu and refresh icon
    * Adds title for the app bar
    * Adds the "Remote devices" section header
    
    Screenshots:
    https://drive.google.com/file/d/0BytzIZKeM8nBZ255YmxxeTBPajQ/view?usp=sharing
    https://drive.google.com/file/d/0BytzIZKeM8nBd0g0cjRWSWlJSlE/view?usp=sharing
    
    BUG=728280
    
    Review-Url: https://codereview.chromium.org/2921413002
    Cr-Commit-Position: refs/heads/master@{#477462}

---
+++ remoting/ios/app/host_collection_header_view.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_IOS_APP_HOST_COLLECTION_HEADER_VIEW_H_
+#define REMOTING_IOS_APP_HOST_COLLECTION_HEADER_VIEW_H_
+
+#import <UIKit/UIKit.h>
+
+// A simple view that displays a white text label on the collection view.
+@interface HostCollectionHeaderView : UICollectionReusableView
+@property(nonatomic) NSString* text;
+@end
+
+#endif  // REMOTING_IOS_APP_HOST_COLLECTION_HEADER_VIEW_H_
