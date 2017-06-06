5f49614f811865bbff510c70d10996dd78e75a34
helenlyang <helenlyang@google.com>
2017-06-06 15:44:15 -0700

    Added a comment to the AboutChromeCollectionViewController class. BUG=none
    Review-Url: https://codereview.chromium.org/2917913003
    Cr-Commit-Position: refs/heads/master@{#477446}

--- ios/chrome/browser/ui/settings/about_chrome_collection_view_controller.h
+++ ios/chrome/browser/ui/settings/about_chrome_collection_view_controller.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/settings/settings_root_collection_view_controller.h"
 
+// Controller for the About Google Chrome collection view, which allows users to
+// view open source licenses, terms of service, etc.
 @interface AboutChromeCollectionViewController
     : SettingsRootCollectionViewController
 
