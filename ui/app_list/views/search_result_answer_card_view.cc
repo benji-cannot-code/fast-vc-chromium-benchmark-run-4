a9ee14e496eb0cff9511a4a84ca5792963456db5
vadimt <vadimt@chromium.org>
2017-06-06 13:27:31 -0700

    Implementing answer card hover highlight.
    Catching WebContents mouse-hover notifications and passing them to
    SearchAnswerContainerView to that it knows that the mouse is above the
    card.
    
    Bug=712331
    
    Review-Url: https://codereview.chromium.org/2920393002
    Cr-Commit-Position: refs/heads/master@{#477389}

--- ui/app_list/views/search_result_answer_card_view.cc
+++ ui/app_list/views/search_result_answer_card_view.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/app_list/app_list_constants.h"
 #include "ui/app_list/app_list_features.h"
 #include "ui/app_list/app_list_view_delegate.h"
+#include "ui/app_list/search_result_observer.h"
 #include "ui/views/background.h"
 #include "ui/views/controls/button/custom_button.h"
 #include "ui/views/layout/box_layout.h"
@@ -18,7 +19,8 @@ namespace app_list {
 // Container of the search answer view.
 class SearchResultAnswerCardView::SearchAnswerContainerView
     : public views::CustomButton,
-      public views::ButtonListener {
+      public views::ButtonListener,
+      public SearchResultObserver {
  public:
   explicit SearchAnswerContainerView(AppListViewDelegate* view_delegate)
       : CustomButton(this), view_delegate_(view_delegate) {
@@ -50,7 +52,11 @@ class SearchResultAnswerCardView::SearchAnswerContainerView
         AddChildView(new_result_view);
     }
 
+    if (search_result_)
+      search_result_->RemoveObserver(this);
     search_result_ = search_result ? search_result->Duplicate() : nullptr;
+    if (search_result_)
+      search_result_->AddObserver(this);
 
     SetVisible(new_result_view != nullptr);
   }
@@ -70,12 +76,19 @@ class SearchResultAnswerCardView::SearchAnswerContainerView
                                      event.flags());
   }
 
+  // SearchResultObserver overrides:
+  void OnViewHoverStateChanged() override { UpdateBackgroundColor(); }
+
  private:
   void UpdateBackgroundColor() {
-    if (selected_)
+    if (selected_) {
       SetBackground(views::CreateSolidBackground(kSelectedColor));
-    else if (state() == STATE_HOVERED || state() == STATE_PRESSED)
+    } else if (state() == STATE_HOVERED || state() == STATE_PRESSED ||
+               (search_result_ && search_result_->is_mouse_in_view())) {
       SetBackground(views::CreateSolidBackground(kHighlightedColor));
+    } else {
+      SetBackground(nullptr);
+    }
 
     SchedulePaint();
   }
