1323d0c699014e48e995ab2ba1716efbfaecd7b1
yoshiki <yoshiki@chromium.org>
2017-06-06 06:24:22 -0700

    Fix the terminology of notification key in exosphere code
    We have distinguished "notification key" with "notification id".
    - Notification Key: Identifier for Android notifications
    - Notification ID: Identifier for Chrome notifications
    
    This patch clarifies the difference of them.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2923503002
    Cr-Commit-Position: refs/heads/master@{#477272}

--- ui/arc/notification/arc_notification_content_view.cc
+++ ui/arc/notification/arc_notification_content_view.cc
@@ -687,7 +687,7 @@ void ArcNotificationContentView::OnItemUpdated() {
 
 void ArcNotificationContentView::OnNotificationSurfaceAdded(
     exo::NotificationSurface* surface) {
-  if (surface->notification_id() != notification_key_)
+  if (surface->notification_key() != notification_key_)
     return;
 
   SetSurface(surface);
@@ -695,7 +695,7 @@ void ArcNotificationContentView::OnNotificationSurfaceAdded(
 
 void ArcNotificationContentView::OnNotificationSurfaceRemoved(
     exo::NotificationSurface* surface) {
-  if (surface->notification_id() != notification_key_)
+  if (surface->notification_key() != notification_key_)
     return;
 
   SetSurface(nullptr);
