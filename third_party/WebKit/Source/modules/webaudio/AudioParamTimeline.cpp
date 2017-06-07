ca61ed919cc622cafc243f274bd1f336eb97425b
rtoy <rtoy@chromium.org>
2017-06-07 16:55:18 -0700

    Throw RangeError instead of InvalidAccessError for invalid times
    Automation methods are supposed to throw a RangeError if the start time
    or time constant are invalid.  Currently they throw
    InvalidAccessError.
    
    See spec issue: https://github.com/WebAudio/web-audio-api/issues/1182
    
    BUG=685657, 726793
    TEST=dom-exceptions.html,
    AudioParam/audioparam-exceptional-values.html,
    AudioParam/audioparam-method-channing.html
    
    Review-Url: https://codereview.chromium.org/2656993002
    Cr-Commit-Position: refs/heads/master@{#477812}

--- third_party/WebKit/Source/modules/webaudio/AudioParamTimeline.cpp
+++ third_party/WebKit/Source/modules/webaudio/AudioParamTimeline.cpp
@@ -62,9 +62,9 @@ static bool IsNonNegativeAudioParamTime(double time,
   if (time >= 0)
     return true;
 
-  exception_state.ThrowDOMException(
-      kInvalidAccessError, message + " must be a finite non-negative number: " +
-                               String::Number(time));
+  exception_state.ThrowRangeError(
+      message +
+      " must be a finite non-negative number: " + String::Number(time));
   return false;
 }
 
@@ -74,8 +74,7 @@ static bool IsPositiveAudioParamTime(double time,
   if (time > 0)
     return true;
 
-  exception_state.ThrowDOMException(
-      kInvalidAccessError,
+  exception_state.ThrowRangeError(
       message + " must be a finite positive number: " + String::Number(time));
   return false;
 }
@@ -445,12 +444,11 @@ void AudioParamTimeline::ExponentialRampToValueAtTime(
     return;
 
   if (!value) {
-    exception_state.ThrowDOMException(
-        kInvalidAccessError,
+    exception_state.ThrowRangeError(
         "The float target value provided (" + String::Number(value) +
-            ") should not be in the range (" +
-            String::Number(-std::numeric_limits<float>::denorm_min()) + ", " +
-            String::Number(std::numeric_limits<float>::denorm_min()) + ").");
+        ") should not be in the range (" +
+        String::Number(-std::numeric_limits<float>::denorm_min()) + ", " +
+        String::Number(std::numeric_limits<float>::denorm_min()) + ").");
     return;
   }
 
