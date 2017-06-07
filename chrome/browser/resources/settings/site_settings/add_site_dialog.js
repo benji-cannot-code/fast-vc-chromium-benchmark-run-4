85bb91231c5422d3e18c9c05062769d183ed10ea
stevenjb <stevenjb@chromium.org>
2017-06-06 19:56:32 -0700

    Settings: Cookies: Avoid duplicate 'incognito' exceptions in CrOS Guest mode
    BUG=725425
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2921103002
    Cr-Commit-Position: refs/heads/master@{#477531}

--- chrome/browser/resources/settings/site_settings/add_site_dialog.js
+++ chrome/browser/resources/settings/site_settings/add_site_dialog.js
@@ -47,6 +47,7 @@ Polymer({
     this.addWebUIListener('onIncognitoStatusChanged', function(hasIncognito) {
       this.$.incognito.checked = false;
       this.showIncognitoSessionOnly_ = hasIncognito &&
+          !loadTimeData.getBoolean('isGuest') &&
           this.contentSetting != settings.PermissionValues.SESSION_ONLY;
     }.bind(this));
     this.browserProxy.updateIncognitoStatus();
