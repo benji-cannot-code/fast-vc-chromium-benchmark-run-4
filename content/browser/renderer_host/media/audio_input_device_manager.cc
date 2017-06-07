1bc141fb60a3a1158b7a4b90ae15c496d85a2412
Max Morin <maxmorin@chromium.org>
2017-06-07 14:49:54 +0200

    Convert some audio input code to OnceCallback.
    I'll be doing some work around AudioInputController, so I figured I
    would convert it and some surrounding code to OnceCallback in a separate
    CL first to avoid noise in future CLs.
    
    BUG=653861
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I211ddb2016995b3303d88cb7da8b50e2797e5b51
    Reviewed-on: https://chromium-review.googlesource.com/527074
    Commit-Queue: Max Morin <maxmorin@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480015}

--- content/browser/renderer_host/media/audio_input_device_manager.cc
+++ content/browser/renderer_host/media/audio_input_device_manager.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/media/audio_input_device_manager.h"
 
 #include <memory>
+#include <utility>
 
 #include "base/bind.h"
 #include "base/command_line.h"
@@ -116,29 +117,27 @@ void AudioInputDeviceManager::Close(int session_id) {
 
   // Post a callback through the listener on IO thread since
   // MediaStreamManager is expecting the callback asynchronously.
-  BrowserThread::PostTask(BrowserThread::IO,
-                          FROM_HERE,
-                          base::Bind(&AudioInputDeviceManager::ClosedOnIOThread,
-                                     this, stream_type, session_id));
+  BrowserThread::PostTask(
+      BrowserThread::IO, FROM_HERE,
+      base::BindOnce(&AudioInputDeviceManager::ClosedOnIOThread, this,
+                     stream_type, session_id));
 }
 
 #if defined(OS_CHROMEOS)
 void AudioInputDeviceManager::RegisterKeyboardMicStream(
-    const base::Closure& callback) {
+    base::OnceClosure callback) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
 
   ++keyboard_mic_streams_count_;
   if (keyboard_mic_streams_count_ == 1) {
     BrowserThread::PostTaskAndReply(
-        BrowserThread::UI,
-        FROM_HERE,
-        base::Bind(
+        BrowserThread::UI, FROM_HERE,
+        base::BindOnce(
             &AudioInputDeviceManager::SetKeyboardMicStreamActiveOnUIThread,
-            this,
-            true),
-        callback);
+            this, true),
+        std::move(callback));
   } else {
-    callback.Run();
+    std::move(callback).Run();
   }
 }
 
@@ -149,12 +148,10 @@ void AudioInputDeviceManager::UnregisterKeyboardMicStream() {
   DCHECK_GE(keyboard_mic_streams_count_, 0);
   if (keyboard_mic_streams_count_ == 0) {
     BrowserThread::PostTask(
-        BrowserThread::UI,
-        FROM_HERE,
-        base::Bind(
+        BrowserThread::UI, FROM_HERE,
+        base::BindOnce(
             &AudioInputDeviceManager::SetKeyboardMicStreamActiveOnUIThread,
-            this,
-            false));
+            this, false));
   }
 }
 #endif
--- media/audio/test_audio_input_controller_factory.cc
+++ media/audio/test_audio_input_controller_factory.cc
@@ -4,6 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "media/audio/test_audio_input_controller_factory.h"
+
+#include <utility>
+
 #include "media/audio/audio_io.h"
 
 namespace media {
@@ -37,8 +40,8 @@ void TestAudioInputController::Record() {
     factory_->delegate_->TestAudioControllerOpened(this);
 }
 
-void TestAudioInputController::Close(const base::Closure& closed_task) {
-  GetTaskRunnerForTesting()->PostTask(FROM_HERE, closed_task);
+void TestAudioInputController::Close(base::OnceClosure closed_task) {
+  GetTaskRunnerForTesting()->PostTask(FROM_HERE, std::move(closed_task));
   if (factory_->delegate_)
     factory_->delegate_->TestAudioControllerClosed(this);
 }
