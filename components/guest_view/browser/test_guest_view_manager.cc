807860b12ca8bb450f11ce2b01046614f298ebcc
Lucas Furukawa Gadani <lfg@chromium.org>
2017-06-07 18:17:40 -0400

    Fix ProcessManagerBrowserTest.NestedURLNavigationsToAppBlocked on OOPIF-based guests.
    This CL forces the test to wait until the guest is attached before
    proceeding.
    
    Bug: 730041
    Change-Id: Ia024392f02c159c9acd046381e9936c2340ebb92
    Reviewed-on: https://chromium-review.googlesource.com/526097
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Lucas Furukawa Gadani <lfg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478007}

--- components/guest_view/browser/test_guest_view_manager.cc
+++ components/guest_view/browser/test_guest_view_manager.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/memory/ptr_util.h"
+#include "components/guest_view/browser/guest_view_base.h"
 #include "components/guest_view/browser/guest_view_manager_delegate.h"
 
 namespace guest_view {
@@ -20,7 +21,8 @@ TestGuestViewManager::TestGuestViewManager(
       num_guests_created_(0),
       expected_num_guests_created_(0),
       num_views_garbage_collected_(0),
-      waiting_for_guests_created_(false) {}
+      waiting_for_guests_created_(false),
+      waiting_for_attach_(nullptr) {}
 
 TestGuestViewManager::~TestGuestViewManager() {
 }
@@ -83,6 +85,19 @@ void TestGuestViewManager::WaitForNumGuestsCreated(size_t count) {
   num_created_message_loop_runner_->Run();
 }
 
+void TestGuestViewManager::WaitUntilAttached(
+    content::WebContents* guest_web_contents) {
+  GuestViewBase* guest = GuestViewBase::FromWebContents(guest_web_contents);
+
+  if (guest->attached())
+    return;
+
+  waiting_for_attach_ = guest;
+
+  attached_message_loop_runner_ = new content::MessageLoopRunner;
+  attached_message_loop_runner_->Run();
+}
+
 void TestGuestViewManager::WaitForViewGarbageCollected() {
   gc_message_loop_runner_ = new content::MessageLoopRunner;
   gc_message_loop_runner_->Run();
@@ -112,6 +127,22 @@ void TestGuestViewManager::AddGuest(int guest_instance_id,
 
   if (num_created_message_loop_runner_.get())
     num_created_message_loop_runner_->Quit();
+}
+
+void TestGuestViewManager::AttachGuest(
+    int embedder_process_id,
+    int element_instance_id,
+    int guest_instance_id,
+    const base::DictionaryValue& attach_params) {
+  GuestViewManager::AttachGuest(embedder_process_id, element_instance_id,
+                                guest_instance_id, attach_params);
+
+  if (waiting_for_attach_ &&
+      (waiting_for_attach_ ==
+       GuestViewBase::From(embedder_process_id, guest_instance_id))) {
+    attached_message_loop_runner_->Quit();
+    waiting_for_attach_ = nullptr;
+  }
 }
 
 void TestGuestViewManager::GetGuestWebContentsList(
