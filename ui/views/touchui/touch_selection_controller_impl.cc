0783b5bbd48acc0bf8e66057bc848841e617c468
sammiequon <sammiequon@chromium.org>
2017-06-07 12:00:32 -0700

    touch ui: Do not hide text selection handles when screen is rotated.
    The text selection handles currently disappear when the device is rotated in tablet mode.
    
    TEST=manual
    BUG=476320
    
    Review-Url: https://codereview.chromium.org/2924893002
    Cr-Commit-Position: refs/heads/master@{#477716}

--- ui/views/touchui/touch_selection_controller_impl.cc
+++ ui/views/touchui/touch_selection_controller_impl.cc
@@ -447,7 +447,6 @@ TouchSelectionControllerImpl::TouchSelectionControllerImpl(
       dragging_handle_(nullptr) {
   selection_start_time_ = base::TimeTicks::Now();
   aura::Window* client_window = client_view_->GetNativeView();
-  client_window->AddObserver(this);
   client_widget_ = Widget::GetTopLevelWidgetForNativeView(client_window);
   if (client_widget_)
     client_widget_->AddObserver(this);
@@ -461,7 +460,6 @@ TouchSelectionControllerImpl::~TouchSelectionControllerImpl() {
   aura::Env::GetInstance()->RemovePreTargetHandler(this);
   if (client_widget_)
     client_widget_->RemoveObserver(this);
-  client_view_->GetNativeView()->RemoveObserver(this);
 }
 
 void TouchSelectionControllerImpl::SelectionChanged() {
@@ -642,12 +640,6 @@ void TouchSelectionControllerImpl::RunContextMenu() {
   const gfx::Rect rect = GetQuickMenuAnchorRect();
   const gfx::Point anchor(rect.CenterPoint().x(), rect.y());
   client_view_->OpenContextMenu(anchor);
-}
-
-void TouchSelectionControllerImpl::OnAncestorWindowTransformed(
-    aura::Window* window,
-    aura::Window* ancestor) {
-  client_view_->DestroyTouchSelection();
 }
 
 void TouchSelectionControllerImpl::OnWidgetClosing(Widget* widget) {
--- ui/views/touchui/touch_selection_controller_impl.h
+++ ui/views/touchui/touch_selection_controller_impl.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/timer/timer.h"
-#include "ui/aura/window_observer.h"
 #include "ui/base/touch/touch_editing_controller.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/selection_bound.h"
@@ -30,7 +29,6 @@ class WidgetTestInteractive;
 class VIEWS_EXPORT TouchSelectionControllerImpl
     : public ui::TouchEditingControllerDeprecated,
       public ui::TouchSelectionMenuClient,
-      public aura::WindowObserver,
       public WidgetObserver,
       public ui::EventHandler {
  public:
@@ -77,10 +75,6 @@ class VIEWS_EXPORT TouchSelectionControllerImpl
   bool IsCommandIdEnabled(int command_id) const override;
   void ExecuteCommand(int command_id, int event_flags) override;
   void RunContextMenu() override;
-
-  // Overriden from aura::WindowObserver.
-  void OnAncestorWindowTransformed(aura::Window* source,
-                                   aura::Window* window) override;
 
   // Overridden from WidgetObserver. We will observe the widget backing the
   // |client_view_| so that when its moved/resized, we can update the selection
