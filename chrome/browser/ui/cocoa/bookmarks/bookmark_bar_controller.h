3b43a807b58f0c539202ba2931b1cdad3ff6dc29
Robert Sesek <rsesek@chromium.org>
2017-06-07 13:36:45 -0400

    Remove old and unneeded base/mac/cocoa_protocols.h.
    Change-Id: I7f19f5115575726365db24f156c9806e8e103dc5
    Reviewed-on: https://chromium-review.googlesource.com/527453
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477769}

--- chrome/browser/ui/cocoa/bookmarks/bookmark_bar_controller.h
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_bar_controller.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <unordered_map>
 
-#import "base/mac/cocoa_protocols.h"
 #include "base/mac/scoped_nsobject.h"
 #include "chrome/browser/ui/cocoa/bookmarks/bookmark_bar_bridge.h"
 #import "chrome/browser/ui/cocoa/bookmarks/bookmark_bar_constants.h"
--- components/storage_monitor/image_capture_device.h
+++ components/storage_monitor/image_capture_device.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file.h"
 #include "base/files/file_path.h"
-#include "base/mac/cocoa_protocols.h"
 #include "base/mac/foundation_util.h"
 #include "base/mac/scoped_nsobject.h"
 #include "base/memory/ref_counted.h"
