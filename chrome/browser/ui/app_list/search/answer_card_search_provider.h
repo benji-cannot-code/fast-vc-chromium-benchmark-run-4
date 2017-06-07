b89e5c885396a16f431eac4dfc5147e9d78ab8bd
vadimt <vadimt@chromium.org>
2017-06-07 16:25:47 -0700

    Adding accessibility description for the answer card.
    It arrives as a header from the server, gets stored as title in
    SearchAnswer, and ends up as accessible description of the button that
    hosts the answer web view.
    
    Bug=712331
    
    Review-Url: https://codereview.chromium.org/2931733002
    Cr-Commit-Position: refs/heads/master@{#477779}

--- chrome/browser/ui/app_list/search/answer_card_search_provider.h
+++ chrome/browser/ui/app_list/search/answer_card_search_provider.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_APP_LIST_SEARCH_ANSWER_WEB_CONTENTS_DELEGATE_H_
-#define CHROME_BROWSER_UI_APP_LIST_SEARCH_ANSWER_WEB_CONTENTS_DELEGATE_H_
+#ifndef CHROME_BROWSER_UI_APP_LIST_SEARCH_ANSWER_CARD_SEARCH_PROVIDER_H_
+#define CHROME_BROWSER_UI_APP_LIST_SEARCH_ANSWER_CARD_SEARCH_PROVIDER_H_
 
 #include <memory>
 #include <string>
@@ -107,9 +107,12 @@ class AnswerCardSearchProvider : public SearchProvider,
   // Url to open when the user clicks at the result.
   std::string result_url_;
 
+  // Title of the result.
+  std::string result_title_;
+
   DISALLOW_COPY_AND_ASSIGN(AnswerCardSearchProvider);
 };
 
 }  // namespace app_list
 
-#endif  // CHROME_BROWSER_UI_APP_LIST_SEARCH_ANSWER_WEB_CONTENTS_DELEGATE_H_
+#endif  // CHROME_BROWSER_UI_APP_LIST_SEARCH_ANSWER_CARD_SEARCH_PROVIDER_H_
