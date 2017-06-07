38ee2f830454500ee70425d1779f09ed41253d19
Nicholas Verne <nverne@chromium.org>
2017-06-07 10:30:50 +1000

    Pre-work for TextFinder to move out of web/
    AXObjectImpl's complete type is no longer needed for
    TextFinder to post accessiblity events. Next, we can
    change usage of WebAXObject to AXObject and eliminate 
    explicit use of AXObjectImpl. After this it should be
    possible to move TextFinder to core/.
    
    Bug: 715382
    Change-Id: Ib6dcede044552ab3ac9ef628345ebdec2c7a5ce0
    Reviewed-on: https://chromium-review.googlesource.com/526554
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Stuart Langley <slangley@chromium.org>
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477577}

--- third_party/WebKit/Source/web/TextFinder.cpp
+++ third_party/WebKit/Source/web/TextFinder.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "web/TextFinder.h"
 
+#include "core/dom/AXObjectCacheBase.h"
 #include "core/dom/Range.h"
 #include "core/dom/TaskRunnerHelper.h"
 #include "core/dom/shadow/ShadowRoot.h"
@@ -46,8 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/LayoutObject.h"
 #include "core/layout/TextAutosizer.h"
 #include "core/page/Page.h"
-#include "modules/accessibility/AXObjectCacheImpl.h"
-#include "modules/accessibility/AXObjectImpl.h"
 #include "platform/RuntimeEnabledFeatures.h"
 #include "platform/Timer.h"
 #include "platform/wtf/CurrentTime.h"
@@ -58,6 +57,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/web/WebViewClient.h"
 
 namespace blink {
+
+class AXObjectImpl;
 
 TextFinder::FindMatch::FindMatch(Range* range, int ordinal)
     : range_(range), ordinal_(ordinal) {}
@@ -248,26 +249,18 @@ void TextFinder::ReportFindInPageResultToAccessibility(int identifier) {
   if (!active_match_)
     return;
 
-  AXObjectCacheImpl* ax_object_cache = ToAXObjectCacheImpl(
+  AXObjectCacheBase* ax_object_cache = ToAXObjectCacheBase(
       OwnerFrame().GetFrame()->GetDocument()->ExistingAXObjectCache());
   if (!ax_object_cache)
     return;
 
-  AXObjectImpl* start_object =
-      ax_object_cache->Get(active_match_->startContainer());
-  AXObjectImpl* end_object =
-      ax_object_cache->Get(active_match_->endContainer());
-  if (!start_object || !end_object)
-    return;
-
-  // Notify the client of new text marker data.
-  ax_object_cache->PostNotification(
-      start_object, AXObjectCache::AXNotification::kAXChildrenChanged);
-  if (start_object != end_object)
-    ax_object_cache->PostNotification(
-        end_object, AXObjectCache::AXNotification::kAXChildrenChanged);
+  Node* start_node = active_match_->startContainer();
+  Node* end_node = active_match_->endContainer();
+  ax_object_cache->HandleTextMarkerDataAdded(start_node, end_node);
 
   if (OwnerFrame().Client()) {
+    AXObjectImpl* start_object = ax_object_cache->Get(start_node);
+    AXObjectImpl* end_object = ax_object_cache->Get(end_node);
     OwnerFrame().Client()->HandleAccessibilityFindInPageResult(
         identifier, active_match_index_ + 1, WebAXObject(start_object),
         active_match_->startOffset(), WebAXObject(end_object),
