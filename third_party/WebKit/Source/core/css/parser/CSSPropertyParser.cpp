6bafc17d5dff1f7bb96e036ae55fba7664d8384c
jiameng <jiameng@chromium.org>
2017-06-06 21:40:29 -0700

    Deprecate CSSPropertyAPI as the base class of other property classes.
    Each property class will be a standalone class instead of a subclass of
    CSSPropertyAPI. CSSPropertyAPI is not being used and can be removed.
    Hence this cl contains the following changes
    - Removed CSSPropertyAPI from class generation templates.
    - Modified class generator script.
    - Removed CSSPropertyAPI.h.tmpl.
    
    Diff: https://gist.github.com/jm318/f689435e315f8bbd1551361ceb305f24/revisions
    Review-Url: https://codereview.chromium.org/2925753002
    Cr-Commit-Position: refs/heads/master@{#477550}

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/parser/CSSVariableParser.h"
 #include "core/css/parser/FontVariantLigaturesParser.h"
 #include "core/css/parser/FontVariantNumericParser.h"
-#include "core/css/properties/CSSPropertyAPI.h"
 #include "core/css/properties/CSSPropertyAlignmentUtils.h"
 #include "core/css/properties/CSSPropertyAnimationNameUtils.h"
 #include "core/css/properties/CSSPropertyColumnUtils.h"
