b80f822b4ffdb85f10ab9403f742989c512007fc
shend <shend@chromium.org>
2017-06-07 01:21:04 -0700

    Remove template parameter on ComputedStyleBase.
    In a previous patch, we made ComputedStyleBase a template class. This
    allowed ComputedStyle to pass itself as a template parameter so that
    ComputedStyleBase can access methods on ComputedStyle.
    
    However, this meant that all the ComputedStyleBase code were in a
    single header file. Whenever it calls a non-inlined function declared
    without CORE_EXPORT, it would cause a linker error. This is because
    ComputedStyleBase functions were being inlined in different translation
    units that did not have the necessary symbols.
    
    Whilst we could make an explicit instantiation of ComputedStyleBase
    with ComputedStyle, it is much easier to go back to the original .h/cpp
    split. To make this work, we forward declare ComputedStyle in
    ComputedStyleBase.h. Whenever we call methods on ComputedStyle, we do
    that in ComputedStyleBase.cpp. We also have to make diffing functions
    static functions that took two ComputedStyles instead of an "other"
    ComputedStyleBase.
    
    This patch does not change behaviour.
    
    Diff of generated files:
    https://gist.github.com/darrnshn/18645fc5c36cc74e00f2724be2079f0a/revisions
    
    BUG=628043
    
    Review-Url: https://codereview.chromium.org/2919343002
    Cr-Commit-Position: refs/heads/master@{#477580}

--- third_party/WebKit/Source/core/style/ComputedStyle.cpp
+++ third_party/WebKit/Source/core/style/ComputedStyle.cpp
@@ -82,11 +82,6 @@ struct SameSizeAsComputedStyle : public RefCounted<SameSizeAsComputedStyle> {
   void* data_ref_svg_style;
 };
 
-// If this fails, the packing algorithm in make_computed_style_base.py has
-// failed to produce the optimal packed size. To fix, update the algorithm to
-// ensure that the buckets are placed so that each takes up at most 1 word.
-ASSERT_SIZE(ComputedStyleBase<ComputedStyle>, SameSizeAsComputedStyleBase);
-
 // If this assert fails, it means that size of ComputedStyle has changed. Please
 // check that you really *do* what to increase the size of ComputedStyle, then
 // update the SameSizeAsComputedStyle struct to match the updated storage of
@@ -537,7 +532,7 @@ StyleDifference ComputedStyle::VisualInvalidationDiff(
 bool ComputedStyle::ScrollAnchorDisablingPropertyChanged(
     const ComputedStyle& other,
     const StyleDifference& diff) const {
-  if (ComputedStyleBase::ScrollAnchorDisablingPropertyChanged(other))
+  if (ComputedStyleBase::ScrollAnchorDisablingPropertyChanged(*this, other))
     return true;
 
   if (diff.TransformChanged())
@@ -555,7 +550,7 @@ bool ComputedStyle::DiffNeedsFullLayoutAndPaintInvalidation(
   // - or the layoutObject knows how to exactly invalidate paints caused by the
   //   layout change instead of forced full paint invalidation.
 
-  if (ComputedStyleBase::DiffNeedsFullLayoutAndPaintInvalidation(other))
+  if (ComputedStyleBase::DiffNeedsFullLayoutAndPaintInvalidation(*this, other))
     return true;
 
   if (rare_non_inherited_data_.Get() != other.rare_non_inherited_data_.Get()) {
@@ -635,7 +630,8 @@ bool ComputedStyle::DiffNeedsFullLayoutAndPaintInvalidation(
 
   if (IsDisplayTableType(Display())) {
     if (ComputedStyleBase::
-            DiffNeedsFullLayoutAndPaintInvalidationDisplayTableType(other))
+            DiffNeedsFullLayoutAndPaintInvalidationDisplayTableType(*this,
+                                                                    other))
       return true;
 
     // In the collapsing border model, 'hidden' suppresses other borders, while
@@ -660,7 +656,8 @@ bool ComputedStyle::DiffNeedsFullLayoutAndPaintInvalidation(
       return true;
   } else if (Display() == EDisplay::kListItem) {
     if (ComputedStyleBase::
-            DiffNeedsFullLayoutAndPaintInvalidationDisplayListItem(other))
+            DiffNeedsFullLayoutAndPaintInvalidationDisplayListItem(*this,
+                                                                   other))
       return true;
   }
 
@@ -675,7 +672,7 @@ bool ComputedStyle::DiffNeedsFullLayoutAndPaintInvalidation(
 }
 
 bool ComputedStyle::DiffNeedsFullLayout(const ComputedStyle& other) const {
-  if (ComputedStyleBase::DiffNeedsFullLayout(other))
+  if (ComputedStyleBase::DiffNeedsFullLayout(*this, other))
     return true;
 
   if (box_data_.Get() != other.box_data_.Get()) {
@@ -730,7 +727,7 @@ bool ComputedStyle::DiffNeedsPaintInvalidationSubtree(
 
 bool ComputedStyle::DiffNeedsPaintInvalidationObject(
     const ComputedStyle& other) const {
-  if (ComputedStyleBase::DiffNeedsPaintInvalidationObject(other))
+  if (ComputedStyleBase::DiffNeedsPaintInvalidationObject(*this, other))
     return true;
 
   if (!BorderVisuallyEqual(other) || !RadiiEqual(other) ||
@@ -814,7 +811,7 @@ bool ComputedStyle::DiffNeedsPaintInvalidationObjectForPaintImage(
 bool ComputedStyle::DiffNeedsVisualRectUpdate(
     const ComputedStyle& other) const {
   // Visual rect is empty if visibility is hidden.
-  if (ComputedStyleBase::DiffNeedsVisualRectUpdate(other))
+  if (ComputedStyleBase::DiffNeedsVisualRectUpdate(*this, other))
     return true;
 
   // Need to update visual rect of the resizer.
@@ -890,7 +887,8 @@ void ComputedStyle::UpdatePropertySpecificDifferences(
               other.rare_non_inherited_data_
                   ->visited_link_text_decoration_color_)) ||
         ComputedStyleBase::
-            UpdatePropertySpecificDifferencesTextDecorationOrColor(other)) {
+            UpdatePropertySpecificDifferencesTextDecorationOrColor(*this,
+                                                                   other)) {
       diff.SetTextDecorationOrColorChanged();
     }
   }
