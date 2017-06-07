b607b7e366350ed516f9b155dabbf0a4258111c2
Michael Giuffrida <michaelpg@chromium.org>
2017-06-07 10:54:50 -0700

    Move path_util to //extensions
    R=rdevlin.cronin@chromium.org
    
    Bug: 729713
    Change-Id: Iee0ecb436217d769933002dcc2befbd82dc95c79
    Reviewed-on: https://chromium-review.googlesource.com/524989
    Commit-Queue: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477895}

--- extensions/browser/path_util.cc
+++ extensions/browser/path_util.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/extensions/path_util.h"
+#include "extensions/browser/path_util.h"
 
 #include "base/path_service.h"
 #include "base/strings/sys_string_conversions.h"
@@ -12,10 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_MACOSX)
 #include <CoreFoundation/CoreFoundation.h>
 #include "base/mac/foundation_util.h"
-#endif
-
-#if defined(OS_CHROMEOS)
-#include "chrome/browser/chromeos/file_manager/filesystem_api_util.h"
 #endif
 
 namespace extensions {
--- extensions/browser/path_util_unittest.cc
+++ extensions/browser/path_util_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/extensions/path_util.h"
+#include "extensions/browser/path_util.h"
 
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
@@ -13,8 +13,7 @@ using base::FilePath;
 
 namespace extensions {
 
-// Basic unittest for path_util::PrettifyPath in
-// chrome/browser/extensions/path_util.cc.
+// Basic unittest for path_util::PrettifyPath.
 // For legacy reasons, it's tested more in
 // FileSystemApiTest.FileSystemApiGetDisplayPathPrettify.
 TEST(ExtensionPathUtilTest, BasicPrettifyPathTest) {
