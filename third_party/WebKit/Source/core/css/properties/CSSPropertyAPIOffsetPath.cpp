eb8f64d47e59c6ef8e8425efd7df0dfee7b0a9a0
bugsnash <bugsnash@chromium.org>
2017-06-07 02:34:20 -0700

    Added null safety to CSSParserContext args in CSSPropertyOffsetPathUtils
    Changed CSSParserContext argument from * to & for null safety and
    readability.
    
    BUG=668012
    
    Review-Url: https://codereview.chromium.org/2918283002
    Cr-Commit-Position: refs/heads/master@{#477597}

--- third_party/WebKit/Source/core/css/properties/CSSPropertyAPIOffsetPath.cpp
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyAPIOffsetPath.cpp
@@ -14,7 +14,7 @@ const CSSValue* CSSPropertyAPIOffsetPath::parseSingleValue(
     CSSParserTokenRange& range,
     const CSSParserContext& context,
     const CSSParserLocalContext&) {
-  return CSSPropertyOffsetPathUtils::ConsumeOffsetPath(range, &context);
+  return CSSPropertyOffsetPathUtils::ConsumeOffsetPath(range, context);
 }
 
 }  // namespace blink
