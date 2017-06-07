7b2e7211cb46a75ea42680449be5eeeb3c875a21
rlanday <rlanday@chromium.org>
2017-06-06 17:21:39 -0700

    Handle ActiveSuggestionMarkers for accessibility
    I'm adding a new type of DocumentMarker, ActiveSuggestionMarker, in
    https://codereview.chromium.org/2923033002. This MarkerType will be used to mark
    the region of text that an open spellcheck/suggestion menu pertains to. This
    seems potentially useful to expose for accessibility, so I'm doing that in this
    CL (I didn't want to add more complexity to the original CL).
    
    BUG=715365
    
    Review-Url: https://codereview.chromium.org/2931473002
    Cr-Commit-Position: refs/heads/master@{#477481}

--- third_party/WebKit/Source/web/AssertMatchingEnums.cpp
+++ third_party/WebKit/Source/web/AssertMatchingEnums.cpp
@@ -396,6 +396,8 @@ STATIC_ASSERT_ENUM(kWebAXInvalidStateOther, kInvalidStateOther);
 STATIC_ASSERT_ENUM(kWebAXMarkerTypeSpelling, DocumentMarker::kSpelling);
 STATIC_ASSERT_ENUM(kWebAXMarkerTypeGrammar, DocumentMarker::kGrammar);
 STATIC_ASSERT_ENUM(kWebAXMarkerTypeTextMatch, DocumentMarker::kTextMatch);
+STATIC_ASSERT_ENUM(kWebAXMarkerTypeActiveSuggestion,
+                   DocumentMarker::kActiveSuggestion);
 
 STATIC_ASSERT_ENUM(kWebAXTextStyleNone, kTextStyleNone);
 STATIC_ASSERT_ENUM(kWebAXTextStyleBold, kTextStyleBold);
