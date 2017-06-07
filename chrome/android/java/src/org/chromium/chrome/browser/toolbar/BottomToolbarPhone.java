f17015965474f3d633ff66626e62d860286dd8b2
Matthew Jones <mdjones@chromium.org>
2017-06-07 12:02:12 -0700

    [Home] Correctly position expand button for screenshot
    The canvas that is being rendered to when we get a screenshot of
    the toolbar needs to be translated for the expand button now that
    it is in a different location.
    
    This change also improves the animations for the button since it
    is no longer covered by the location bar.
    
    BUG=730574
    
    Change-Id: I4f1f2bf35791772a4aca819a67e527d5ed1d44d7
    Reviewed-on: https://chromium-review.googlesource.com/527238
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477821}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/BottomToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/BottomToolbarPhone.java
@@ -220,6 +220,13 @@ public class BottomToolbarPhone extends ToolbarPhone {
     }
 
     @Override
+    protected void setTabSwitcherMode(boolean inTabSwitcherMode, boolean showToolbar,
+            boolean delayAnimation, boolean animate) {
+        super.setTabSwitcherMode(inTabSwitcherMode, showToolbar, delayAnimation, animate);
+        if (!mUseToolbarHandle) mExpandButton.setClickable(!inTabSwitcherMode);
+    }
+
+    @Override
     protected int getProgressBarColor() {
         int color = super.getProgressBarColor();
         if (getToolbarDataProvider().getTab() != null) {
@@ -372,8 +379,7 @@ public class BottomToolbarPhone extends ToolbarPhone {
     public void updateButtonVisibility() {
         super.updateButtonVisibility();
         if (!mUseToolbarHandle) {
-            mExpandButton.setVisibility(
-                    urlHasFocus() || isTabSwitcherAnimationRunning() ? INVISIBLE : VISIBLE);
+            mExpandButton.setVisibility(urlHasFocus() ? INVISIBLE : VISIBLE);
         }
     }
 
@@ -463,12 +469,13 @@ public class BottomToolbarPhone extends ToolbarPhone {
         mExpandButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mBottomSheet != null) mBottomSheet.onExpandButtonPressed();
+                if (mBottomSheet != null && mTabSwitcherState == STATIC_TAB) {
+                    mBottomSheet.onExpandButtonPressed();
+                }
             }
         });
 
         mExpandButton.setVisibility(View.VISIBLE);
-        mBrowsingModeViews.add(mExpandButton);
 
         updateToolbarTopMargin();
     }
@@ -489,7 +496,7 @@ public class BottomToolbarPhone extends ToolbarPhone {
         if (mUseToolbarHandle) {
             mToolbarHandleView.setImageDrawable(isLightTheme() ? mHandleDark : mHandleLight);
         } else {
-            ColorStateList tint = mUseLightToolbarDrawables ? mLightModeTint : mDarkModeTint;
+            ColorStateList tint = isIncognito() ? mLightModeTint : mDarkModeTint;
             mExpandButton.setTint(tint);
         }
     }
@@ -522,7 +529,11 @@ public class BottomToolbarPhone extends ToolbarPhone {
                         ? View.INVISIBLE
                         : View.VISIBLE);
 
-        if (mUseToolbarHandle) mToolbarHandleView.setAlpha(1f - progress);
+        if (mUseToolbarHandle) {
+            mToolbarHandleView.setAlpha(1f - progress);
+        } else {
+            mExpandButton.setAlpha(1f - progress);
+        }
 
         int tabSwitcherThemeColor = getToolbarColorForVisualState(VisualState.TAB_SWITCHER_NORMAL);
 
@@ -550,7 +561,10 @@ public class BottomToolbarPhone extends ToolbarPhone {
         if (mTextureCaptureMode) {
             super.drawTabSwitcherAnimationOverlay(canvas, 0f);
             if (!mUseToolbarHandle && mExpandButton.getVisibility() != View.GONE) {
+                canvas.save();
+                translateCanvasToView(this, mToolbarButtonsContainer, canvas);
                 drawChild(canvas, mExpandButton, SystemClock.uptimeMillis());
+                canvas.restore();
             }
         }
     }
