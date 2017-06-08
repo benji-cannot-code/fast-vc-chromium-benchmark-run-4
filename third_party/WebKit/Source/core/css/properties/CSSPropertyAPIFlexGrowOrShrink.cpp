684306c2affbb71631dc2138818a902c3393c1bf
jiameng <jiameng@chromium.org>
2017-06-08 01:13:40 -0700

    Implement parseSingleValue API for flex-grow and flex-shrink properties.
    I will shortly implement API for "flex" shorthand, which has "flex-grow" and "flex-shrink" as longhands. I'm implementing these two longhands API in this cl to complete longhands impl first (and make it possible to call longhands API from shorthand API if needed).
    
    Diff: https://gist.github.com/jm318/e3036cbd8ca67a96901fe91085cebf9d/revisions
    Review-Url: https://codereview.chromium.org/2923413002
    Cr-Commit-Position: refs/heads/master@{#477923}

--- third_party/WebKit/Source/core/css/properties/CSSPropertyAPIFlexGrowOrShrink.cpp
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyAPIFlexGrowOrShrink.cpp
@@ -5,4 +5,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/css/properties/CSSPropertyAPIFlexGrowOrShrink.h"
 
-namespace blink {}  // namespace blink
+#include "core/css/parser/CSSPropertyParserHelpers.h"
+#include "platform/Length.h"
+
+namespace blink {
+
+class CSSParserContext;
+class CSSParserLocalContext;
+
+const CSSValue* CSSPropertyAPIFlexGrowOrShrink::parseSingleValue(
+    CSSParserTokenRange& range,
+    const CSSParserContext&,
+    const CSSParserLocalContext&) {
+  return CSSPropertyParserHelpers::ConsumeNumber(range, kValueRangeNonNegative);
+}
+
+}  // namespace blink
