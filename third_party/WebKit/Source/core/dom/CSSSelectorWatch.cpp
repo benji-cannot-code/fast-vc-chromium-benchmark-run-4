ae53aed2417858b025986479889fe4b2ad62aa70
shend <shend@chromium.org>
2017-06-08 00:54:57 -0700

    Remove unused include in CSSSelectorWatch.
    StyleRareNonInheritedData.h will no longer exist once we generate it.
    This removes an unused include of StyleRareNonInheritedData.h.
    
    BUG=628043
    
    Review-Url: https://codereview.chromium.org/2926243002
    Cr-Commit-Position: refs/heads/master@{#477921}

--- third_party/WebKit/Source/core/dom/CSSSelectorWatch.cpp
+++ third_party/WebKit/Source/core/dom/CSSSelectorWatch.cpp
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/TaskRunnerHelper.h"
 #include "core/frame/LocalFrame.h"
 #include "core/frame/LocalFrameClient.h"
-#include "core/style/StyleRareNonInheritedData.h"
 
 namespace blink {
 
