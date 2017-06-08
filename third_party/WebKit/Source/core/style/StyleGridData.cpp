e85cfd3d1e237301c211c1fdea755d01270ecae8
Darren Shen <shend@chromium.org>
2017-06-09 09:30:04 +1000

    Pack enums in StyleGridItemData as bit fields.
    This patch changes StyleGridItemData::auto_repeat_columns_type_ and
    auto_repeat_rows_type_ into bit fields. It also moves the declaration of
    grid_auto_flow_ to be with the other bit fields so that they can be
    packed together.
    
    This reduces the size of StyleGridItemData by 8 bytes.
    
    Bug: 628043
    Change-Id: Ic1fad688f72c15278c240ebdb0eacac2d42c9e66
    Reviewed-on: https://chromium-review.googlesource.com/527738
    Reviewed-by: Eddy Mead <meade@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478184}

--- third_party/WebKit/Source/core/style/StyleGridData.cpp
+++ third_party/WebKit/Source/core/style/StyleGridData.cpp
@@ -46,7 +46,6 @@ StyleGridData::StyleGridData()
           ComputedStyle::InitialOrderedNamedGridColumnLines()),
       auto_repeat_ordered_named_grid_row_lines_(
           ComputedStyle::InitialOrderedNamedGridRowLines()),
-      grid_auto_flow_(ComputedStyle::InitialGridAutoFlow()),
       grid_auto_rows_(ComputedStyle::InitialGridAutoRows()),
       grid_auto_columns_(ComputedStyle::InitialGridAutoColumns()),
       named_grid_area_(ComputedStyle::InitialNamedGridArea()),
@@ -60,6 +59,7 @@ StyleGridData::StyleGridData()
           ComputedStyle::InitialGridAutoRepeatInsertionPoint()),
       auto_repeat_rows_insertion_point_(
           ComputedStyle::InitialGridAutoRepeatInsertionPoint()),
+      grid_auto_flow_(ComputedStyle::InitialGridAutoFlow()),
       auto_repeat_columns_type_(ComputedStyle::InitialGridAutoRepeatType()),
       auto_repeat_rows_type_(ComputedStyle::InitialGridAutoRepeatType()) {}
 
@@ -78,7 +78,6 @@ StyleGridData::StyleGridData(const StyleGridData& o)
           o.auto_repeat_ordered_named_grid_column_lines_),
       auto_repeat_ordered_named_grid_row_lines_(
           o.auto_repeat_ordered_named_grid_row_lines_),
-      grid_auto_flow_(o.grid_auto_flow_),
       grid_auto_rows_(o.grid_auto_rows_),
       grid_auto_columns_(o.grid_auto_columns_),
       named_grid_area_(o.named_grid_area_),
@@ -91,6 +90,7 @@ StyleGridData::StyleGridData(const StyleGridData& o)
       auto_repeat_columns_insertion_point_(
           o.auto_repeat_columns_insertion_point_),
       auto_repeat_rows_insertion_point_(o.auto_repeat_rows_insertion_point_),
+      grid_auto_flow_(o.grid_auto_flow_),
       auto_repeat_columns_type_(o.auto_repeat_columns_type_),
       auto_repeat_rows_type_(o.auto_repeat_rows_type_) {}
 
