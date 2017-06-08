86ab3268367c595fa84d1830fc6df2a65eeea73c
sclittle <sclittle@chromium.org>
2017-06-08 14:48:05 -0700

    Stylize PlaceholderImages with icons.
    This CL stylizes the appearance of PlaceholderImages by adding an icon.
    See the spec here:
    https://docs.google.com/document/d/1BHeA1azbgCdZgCnr16VN2g7A9MHPQ_dwKn5szh8evMQ/edit
    
    BUG=605351
    
    Review-Url: https://codereview.chromium.org/2915353002
    Cr-Commit-Position: refs/heads/master@{#478098}

--- third_party/WebKit/Source/platform/graphics/PlaceholderImage.h
+++ third_party/WebKit/Source/platform/graphics/PlaceholderImage.h
@@ -16,7 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class FloatPoint;
 class FloatRect;
+class FloatSize;
+class GraphicsContext;
 class ImageObserver;
 
 // A generated placeholder image that shows a translucent gray rectangle.
@@ -43,8 +46,7 @@ class PLATFORM_EXPORT PlaceholderImage final : public Image {
   void DestroyDecodedData() override;
 
  private:
-  PlaceholderImage(ImageObserver* observer, const IntSize& size)
-      : Image(observer), size_(size) {}
+  PlaceholderImage(ImageObserver*, const IntSize&);
 
   bool CurrentFrameHasSingleSecurityOrigin() const override { return true; }
 
@@ -54,7 +56,16 @@ class PLATFORM_EXPORT PlaceholderImage final : public Image {
     return false;
   }
 
-  IntSize size_;
+  void DrawPattern(GraphicsContext&,
+                   const FloatRect& src_rect,
+                   const FloatSize& scale,
+                   const FloatPoint& phase,
+                   SkBlendMode,
+                   const FloatRect& dest_rect,
+                   const FloatSize& repeat_spacing = FloatSize()) override;
+
+  const IntSize size_;
+
   // Lazily initialized.
   sk_sp<SkImage> image_for_current_frame_;
 };
