4c855dd97816426b6f13d49a3b16bc02ef4dfa89
billorr <billorr@chromium.org>
2017-06-07 23:01:44 -0700

    Fix build break when building with enable_openvr=true on Windows
    BUG=730436
    
    Review-Url: https://codereview.chromium.org/2929733002
    Cr-Commit-Position: refs/heads/master@{#477907}

--- device/vr/openvr/openvr_device.cc
+++ device/vr/openvr/openvr_device.cc
@@ -297,7 +297,7 @@ void OpenVRDevice::OpenVRRenderLoop::UnregisterPollingEventCallback() {
 }
 
 void OpenVRDevice::OpenVRRenderLoop::GetVSync(
-    const mojom::VRVSyncProvider::GetVSyncCallback& callback) {
+    mojom::VRVSyncProvider::GetVSyncCallback callback) {
   static int16_t next_frame = 0;
   int16_t frame = next_frame++;
 
@@ -314,8 +314,8 @@ void OpenVRDevice::OpenVRRenderLoop::GetVSync(
   device::mojom::VRPosePtr pose = getPose();
   Sleep(11);  // TODO (billorr): Use real vsync timing instead of a sleep (this
               // sleep just throttles vsyncs so we don't fill message queues).
-  callback.Run(std::move(pose), time, frame,
-               device::mojom::VRVSyncProvider::Status::SUCCESS);
+  std::move(callback).Run(std::move(pose), time, frame,
+                          device::mojom::VRVSyncProvider::Status::SUCCESS);
 }
 
 }  // namespace device
