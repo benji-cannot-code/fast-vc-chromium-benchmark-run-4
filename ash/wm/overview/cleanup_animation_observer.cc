d6a2fcf1d0fc7d027242726df241cab8acf4c4b5
wutao <wutao@chromium.org>
2017-06-08 10:25:29 -0700

    Fix CleanupAnimationObserver constructor.
    When removing a display in overview mode, there is a circular deletion of |OverviewModeShield| and its window, initialized by ~Window(). In the constructor of CleanupAnimationObserver, mark the window as not owned by the parent to ensure that OnImplicitAnimationsCompleted() is not called from ~Window() and the window is not deleted there.
    
    BUG=720589
    R=sky@chromium.org, oshima@chromium.org, varkha@chromium.org
    TEST=WindowSelectorTest.RemoveDisplayWithAnimation
    
    Review-Url: https://codereview.chromium.org/2884373002
    Cr-Commit-Position: refs/heads/master@{#478010}

--- ash/wm/overview/cleanup_animation_observer.cc
+++ ash/wm/overview/cleanup_animation_observer.cc
@@ -5,14 +5,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/wm/overview/cleanup_animation_observer.h"
 
+#include "ui/aura/window.h"
 #include "ui/views/widget/widget.h"
 
 namespace ash {
 
+// Widget must own NativeWidget.
+// |widget_| will delete the NativeWidget and NativeWindow.
+// Mark the window as not owned by the parent to ensure that
+// OnImplicitAnimationsCompleted() is not called from ~Window() and the window
+// is not deleted there.
 CleanupAnimationObserver::CleanupAnimationObserver(
     std::unique_ptr<views::Widget> widget)
     : widget_(std::move(widget)), owner_(nullptr) {
   DCHECK(widget_);
+  widget_->GetNativeWindow()->set_owned_by_parent(false);
 }
 
 CleanupAnimationObserver::~CleanupAnimationObserver() {}
