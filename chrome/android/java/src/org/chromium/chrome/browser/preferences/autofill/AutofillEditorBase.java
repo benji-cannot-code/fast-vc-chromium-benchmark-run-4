eea91f14172c2ac9a7e2a3f5780deeb24ad8494b
Rouslan Solomakhin <rouslan@chromium.org>
2017-06-08 14:49:52 -0400

    Avoid crash when tapping help icon in autofill settings.
    Bug: 730865
    Change-Id: Ic1c3fa4357eb5d3881a588d748f0a3daf76088ba
    Reviewed-on: https://chromium-review.googlesource.com/527407
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Ganggui Tang <gogerald@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478056}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/autofill/AutofillEditorBase.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/autofill/AutofillEditorBase.java
@@ -85,7 +85,7 @@ public abstract class AutofillEditorBase
             getActivity().finish();
             return true;
         } else if (item.getItemId() == R.id.help_menu_id) {
-            EditorDialog.launchAutofillHelpPage(mContext);
+            EditorDialog.launchAutofillHelpPage(getActivity());
             return true;
         }
 
