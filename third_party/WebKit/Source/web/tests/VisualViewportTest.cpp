e943111dcc08568dc938732787052c14d5eedca5
David Bokan <bokan@chromium.org>
2017-06-08 16:02:51 -0400

    Remove InertTopControls RuntimeEnabledFeature
    This feature has been in stable since M56 so it's safe to remove the
    flag now.
    
    Bug: 428132
    Change-Id: I11341acde13034262899e072941b59667847c70d
    Reviewed-on: https://chromium-review.googlesource.com/528225
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478325}

--- third_party/WebKit/Source/web/tests/VisualViewportTest.cpp
+++ third_party/WebKit/Source/web/tests/VisualViewportTest.cpp
@@ -2112,10 +2112,6 @@ static void configureAndroidCompositing(WebSettings* settings) {
 // Make sure a composited background-attachment:fixed background gets resized
 // when using inert (non-layout affecting) browser controls.
 TEST_P(VisualViewportTest, ResizeCompositedAndFixedBackground) {
-  bool originalInertTopControls =
-      RuntimeEnabledFeatures::InertTopControlsEnabled();
-  RuntimeEnabledFeatures::SetInertTopControlsEnabled(true);
-
   std::unique_ptr<FrameTestHelpers::TestWebViewClient>
       fake_compositing_web_view_client =
           WTF::MakeUnique<FrameTestHelpers::TestWebViewClient>();
@@ -2182,8 +2178,6 @@ TEST_P(VisualViewportTest, ResizeCompositedAndFixedBackground) {
   EXPECT_EQ(page_width, compositor->FixedRootBackgroundLayer()->Size().Width());
   EXPECT_EQ(page_height,
             compositor->FixedRootBackgroundLayer()->Size().Height());
-
-  RuntimeEnabledFeatures::SetInertTopControlsEnabled(originalInertTopControls);
 }
 
 static void configureAndroidNonCompositing(WebSettings* settings) {
@@ -2198,10 +2192,6 @@ static void configureAndroidNonCompositing(WebSettings* settings) {
 // Make sure a non-composited background-attachment:fixed background gets
 // resized when using inert (non-layout affecting) browser controls.
 TEST_P(VisualViewportTest, ResizeNonCompositedAndFixedBackground) {
-  bool originalInertTopControls =
-      RuntimeEnabledFeatures::InertTopControlsEnabled();
-  RuntimeEnabledFeatures::SetInertTopControlsEnabled(true);
-
   FrameTestHelpers::WebViewHelper web_view_helper;
   WebViewBase* web_view_impl = web_view_helper.Initialize(
       true, nullptr, nullptr, nullptr, &configureAndroidNonCompositing);
@@ -2296,16 +2286,11 @@ TEST_P(VisualViewportTest, ResizeNonCompositedAndFixedBackground) {
             (*raster_invalidations)[0].rect);
 
   document->View()->SetTracksPaintInvalidations(false);
-  RuntimeEnabledFeatures::SetInertTopControlsEnabled(originalInertTopControls);
 }
 
 // Make sure a browser control resize with background-attachment:not-fixed
 // background doesn't cause invalidation or layout.
 TEST_P(VisualViewportTest, ResizeNonFixedBackgroundNoLayoutOrInvalidation) {
-  bool originalInertTopControls =
-      RuntimeEnabledFeatures::InertTopControlsEnabled();
-  RuntimeEnabledFeatures::SetInertTopControlsEnabled(true);
-
   std::unique_ptr<FrameTestHelpers::TestWebViewClient>
       fake_compositing_web_view_client =
           WTF::MakeUnique<FrameTestHelpers::TestWebViewClient>();
@@ -2382,14 +2367,9 @@ TEST_P(VisualViewportTest, ResizeNonFixedBackgroundNoLayoutOrInvalidation) {
     EXPECT_FALSE(invalidation_tracking);
 
   document->View()->SetTracksPaintInvalidations(false);
-  RuntimeEnabledFeatures::SetInertTopControlsEnabled(originalInertTopControls);
 }
 
 TEST_P(VisualViewportTest, InvalidateLayoutViewWhenDocumentSmallerThanView) {
-  bool originalInertTopControls =
-      RuntimeEnabledFeatures::InertTopControlsEnabled();
-  RuntimeEnabledFeatures::SetInertTopControlsEnabled(true);
-
   std::unique_ptr<FrameTestHelpers::TestWebViewClient>
       fake_compositing_web_view_client =
           WTF::MakeUnique<FrameTestHelpers::TestWebViewClient>();
@@ -2436,7 +2416,6 @@ TEST_P(VisualViewportTest, InvalidateLayoutViewWhenDocumentSmallerThanView) {
   }
 
   document->View()->SetTracksPaintInvalidations(false);
-  RuntimeEnabledFeatures::SetInertTopControlsEnabled(originalInertTopControls);
 }
 
 // Make sure we don't crash when the visual viewport's height is 0. This can
