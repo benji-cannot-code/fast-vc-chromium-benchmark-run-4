fd5873e783b327b0270b63ad4e93b878fb0828ea
pkotwicz <pkotwicz@chromium.org>
2017-06-08 12:34:11 -0700

    [Android WebAPK] Add UMA histogram to record error code when install fails pt 2/3
    This CL adds a new UMA histogram WebApk.Install.GooglePlayInstallErrorCode to
    record the error code when a WebAPK install fails.
    
    The metric is temporary. Once we know which error codes are of interest, we will
    add new enum entries to WebApkGooglePlayInstallResult.
    
    BUG=727893
    R=hanxi,dominickn,isherman
    TBR=yfriedman for WebApkUma.java
    
    Review-Url: https://codereview.chromium.org/2910313002
    Cr-Commit-Position: refs/heads/master@{#478054}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/GooglePlayWebApkInstallDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/GooglePlayWebApkInstallDelegate.java
@@ -5,33 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.webapps;
 
-import android.support.annotation.IntDef;
-
 import org.chromium.base.Callback;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 
 /**
  * Defines an interface for installing WebAPKs via Google Play.
  */
 public interface GooglePlayWebApkInstallDelegate {
-    /**
-     * The app state transitions provided by Google Play during download and installation process.
-     */
-    @Retention(RetentionPolicy.SOURCE)
-    @IntDef({INVALID, DOWNLOAD_PENDING, DOWNLOADING, DOWNLOAD_CANCELLED, DOWNLOAD_ERROR,
-             INSTALLING, INSTALL_ERROR, INSTALLED})
-    public @interface InstallerPackageEvent {}
-    public static final int INVALID = -1;
-    public static final int DOWNLOAD_PENDING = 0;
-    public static final int DOWNLOADING = 1;
-    public static final int DOWNLOAD_CANCELLED = 2;
-    public static final int DOWNLOAD_ERROR = 3;
-    public static final int INSTALLING = 4;
-    public static final int INSTALL_ERROR = 5;
-    public static final int INSTALLED = 6;
-
     /**
      * Uses Google Play to install WebAPK asynchronously.
      * @param packageName The package name of WebAPK to install.
@@ -55,10 +34,4 @@ public interface GooglePlayWebApkInstallDelegate {
      */
     void updateAsync(String packageName, int version, String title, String token, String url,
             Callback<Integer> callback);
-    /**
-     * Calls the callback once the installation either succeeded or failed.
-     * @param packageName The package name of WebAPK for the installation.
-     * @param event The result of the install.
-     */
-    void onGotInstallEvent(String packageName, @InstallerPackageEvent int event);
 }
