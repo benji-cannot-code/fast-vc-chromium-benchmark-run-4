d43b45a8ef7cbeea64d638b6b85c6a39c55072bd
kinuko <kinuko@chromium.org>
2017-06-07 21:41:50 -0700

    Use independent URLLoader in browser-side code
    BUG=729503
    
    Review-Url: https://codereview.chromium.org/2931623002
    Cr-Commit-Position: refs/heads/master@{#477890}

--- content/browser/service_worker/service_worker_url_job_wrapper.cc
+++ content/browser/service_worker/service_worker_url_job_wrapper.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/resource_request_info.h"
 #include "content/public/common/browser_side_navigation_policy.h"
 #include "content/public/common/content_switches.h"
-#include "mojo/public/cpp/bindings/strong_associated_binding.h"
+#include "mojo/public/cpp/bindings/strong_binding.h"
 #include "net/base/io_buffer.h"
 #include "storage/browser/blob/blob_storage_context.h"
 
@@ -27,13 +27,12 @@ class URLLoaderImpl : public mojom::URLLoader {
       const ServiceWorkerResponse& response,
       blink::mojom::ServiceWorkerStreamHandlePtr body_as_stream,
       base::WeakPtr<storage::BlobStorageContext> blob_storage_context,
-      mojom::URLLoaderAssociatedRequest request,
+      mojom::URLLoaderRequest request,
       mojom::URLLoaderClientPtr client) {
-    mojo::MakeStrongAssociatedBinding(
-        base::MakeUnique<URLLoaderImpl>(response, std::move(body_as_stream),
-                                        blob_storage_context,
-                                        std::move(client)),
-        std::move(request));
+    mojo::MakeStrongBinding(base::MakeUnique<URLLoaderImpl>(
+                                response, std::move(body_as_stream),
+                                blob_storage_context, std::move(client)),
+                            std::move(request));
   }
 
   URLLoaderImpl(const ServiceWorkerResponse& response,
