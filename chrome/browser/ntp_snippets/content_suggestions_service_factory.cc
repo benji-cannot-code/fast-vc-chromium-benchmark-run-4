ca3f9a2bc9d688598b85c38eebe1c28bd2890190
Chris Pickel <sfiera@chromium.org>
2017-06-08 10:43:19 +0200

    ntp_snippets: disable local categories by feature
    If kContentSuggestionsCategories is enabled, then we only want to
    display categories served by the server, so in that case, disable local
    categories.
    
    BUG=725887
    
    Change-Id: Ibe67824088cb6ca342635054e416a8315c769d79
    Reviewed-on: https://chromium-review.googlesource.com/521163
    Commit-Queue: Chris Pickel <sfiera@chromium.org>
    Reviewed-by: Markus Heintz <markusheintz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477979}

--- chrome/browser/ntp_snippets/content_suggestions_service_factory.cc
+++ chrome/browser/ntp_snippets/content_suggestions_service_factory.cc
@@ -142,10 +142,20 @@ bool IsChromeHomeEnabled() {
 #endif
 }
 
+bool AreLocalCategoriesEnabled() {
+#if defined(OS_ANDROID)
+  return !base::FeatureList::IsEnabled(
+      chrome::android::kContentSuggestionsCategories);
+#else
+  return false;
+#endif
+}
+
 #if BUILDFLAG(ENABLE_OFFLINE_PAGES)
 
 bool IsRecentTabProviderEnabled() {
-  return base::FeatureList::IsEnabled(
+  return AreLocalCategoriesEnabled() &&
+         base::FeatureList::IsEnabled(
              ntp_snippets::kRecentOfflineTabSuggestionsFeature) &&
          base::FeatureList::IsEnabled(
              offline_pages::kOffliningRecentPagesFeature);
@@ -185,7 +195,8 @@ bool AreOfflinePageDownloadsEnabled() {
 }
 
 bool IsDownloadsProviderEnabled() {
-  return AreAssetDownloadsEnabled() || AreOfflinePageDownloadsEnabled();
+  return AreLocalCategoriesEnabled() &&
+         (AreAssetDownloadsEnabled() || AreOfflinePageDownloadsEnabled());
 }
 
 void RegisterDownloadsProviderIfEnabled(ContentSuggestionsService* service,
@@ -237,7 +248,8 @@ void RegisterBookmarkProviderIfEnabled(ContentSuggestionsService* service,
 #if defined(OS_ANDROID)
 
 bool IsPhysicalWebPageProviderEnabled() {
-  return base::FeatureList::IsEnabled(
+  return AreLocalCategoriesEnabled() &&
+         base::FeatureList::IsEnabled(
              ntp_snippets::kPhysicalWebPageSuggestionsFeature) &&
          base::FeatureList::IsEnabled(chrome::android::kPhysicalWebFeature);
 }
@@ -324,8 +336,9 @@ void RegisterArticleProviderIfEnabled(ContentSuggestionsService* service,
 }
 
 bool IsForeignSessionsProviderEnabled() {
-  return base::FeatureList::IsEnabled(
-      ntp_snippets::kForeignSessionsSuggestionsFeature);
+  return AreLocalCategoriesEnabled() &&
+         base::FeatureList::IsEnabled(
+             ntp_snippets::kForeignSessionsSuggestionsFeature);
 }
 
 void RegisterForeignSessionsProviderIfEnabled(
