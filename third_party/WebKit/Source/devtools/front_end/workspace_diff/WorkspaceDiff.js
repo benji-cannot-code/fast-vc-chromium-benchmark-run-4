50b27266b70fd4b03b2696d64dbeb9480b308add
einbinder <einbinder@chromium.org>
2017-06-08 11:15:40 -0700

    DevTools: Avoid a race in WorkspaceDiff when UISourceCodes are removed
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2925023003
    Cr-Commit-Position: refs/heads/master@{#478025}

--- third_party/WebKit/Source/devtools/front_end/workspace_diff/WorkspaceDiff.js
+++ third_party/WebKit/Source/devtools/front_end/workspace_diff/WorkspaceDiff.js
@@ -90,8 +90,7 @@ WorkspaceDiff.WorkspaceDiff = class extends Common.Object {
    */
   _uiSourceCodeRemoved(event) {
     var uiSourceCode = /** @type {!Workspace.UISourceCode} */ (event.data);
-    this._loadingUISourceCodes.delete(uiSourceCode);
-    this._markAsUnmodified(uiSourceCode);
+    this._removeUISourceCode(uiSourceCode);
   }
 
   /**
@@ -99,10 +98,19 @@ WorkspaceDiff.WorkspaceDiff = class extends Common.Object {
    */
   _projectRemoved(event) {
     var project = /** @type {!Workspace.Project} */ (event.data);
-    for (var uiSourceCode of project.uiSourceCodes()) {
-      this._loadingUISourceCodes.delete(uiSourceCode);
-      this._markAsUnmodified(uiSourceCode);
-    }
+    for (var uiSourceCode of project.uiSourceCodes())
+      this._removeUISourceCode(uiSourceCode);
+  }
+
+  /**
+   * @param {!Workspace.UISourceCode} uiSourceCode
+   */
+  _removeUISourceCode(uiSourceCode) {
+    this._loadingUISourceCodes.delete(uiSourceCode);
+    var uiSourceCodeDiff = this._uiSourceCodeDiffs.get(uiSourceCode);
+    if (uiSourceCodeDiff)
+      uiSourceCodeDiff._dispose = true;
+    this._markAsUnmodified(uiSourceCode);
   }
 
   /**
@@ -172,6 +180,7 @@ WorkspaceDiff.WorkspaceDiff.UISourceCodeDiff = class extends Common.Object {
     uiSourceCode.addEventListener(Workspace.UISourceCode.Events.WorkingCopyCommitted, this._uiSourceCodeChanged, this);
     this._requestDiffPromise = null;
     this._pendingChanges = null;
+    this._dispose = false;
   }
 
   _uiSourceCodeChanged() {
@@ -189,6 +198,8 @@ WorkspaceDiff.WorkspaceDiff.UISourceCodeDiff = class extends Common.Object {
      * @this {WorkspaceDiff.WorkspaceDiff.UISourceCodeDiff}
      */
     function emitDiffChanged() {
+      if (this._dispose)
+        return;
       this.dispatchEventToListeners(WorkspaceDiff.Events.DiffChanged);
       this._pendingChanges = null;
     }
@@ -207,10 +218,21 @@ WorkspaceDiff.WorkspaceDiff.UISourceCodeDiff = class extends Common.Object {
    * @return {!Promise<?Diff.Diff.DiffArray>}
    */
   async _innerRequestDiff() {
+    if (this._dispose)
+      return null;
+
     var current = this._uiSourceCode.workingCopy();
     if (!current && !this._uiSourceCode.contentLoaded())
       current = await this._uiSourceCode.requestContent();
+    // ------------ ASYNC ------------
+    if (this._dispose)
+      return null;
+
     var baseline = await this._uiSourceCode.requestOriginalContent();
+    // ------------ ASYNC ------------
+    if (this._dispose)
+      return null;
+
     if (current === null || baseline === null)
       return null;
     return Diff.Diff.lineDiff(baseline.split('\n'), current.split('\n'));
