05d7afe53a89a5e45f2e5cc0603ec589d19accd8
Yipeng Wang <yipengw@chromium.org>
2017-06-08 13:59:44 -0400

    Make monochrome use the same copy of licenses as chrome
    1. Compare chrome and webview licenses, and add the Chromium project license;
    2. Get the Brotli decompression function and put it in component/about_ui/;
    3. make monochrome call the function via JNI.
    
    Bug: 688077
    Change-Id: I125dce46f85d3b268d9cee6d19249905f79f0209
    Reviewed-on: https://chromium-review.googlesource.com/497870
    Commit-Queue: Yipeng Wang <yipengw@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478052}

---
+++ components/about_ui/android/java/src/org/chromium/components/aboutui/CreditUtils.java
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.aboutui;
+
+import org.chromium.base.annotations.JNINamespace;
+
+/** Credits-related utilities. */
+@JNINamespace("about_ui")
+public class CreditUtils {
+    private CreditUtils() {}
+
+    /** Returns a string containing the content of about_credits.html. */
+    public static native byte[] nativeGetJavaWrapperCredits();
+}
---
+++ components/about_ui/credit_utils.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_ABOUT_UI_CREDIT_UTILS_H_
+#define COMPONENTS_ABOUT_UI_CREDIT_UTILS_H_
+
+#include <string>
+
+namespace about_ui {
+
+// Decode a Brotli compressed HTML license file and attach .js files.
+std::string GetCredits(bool include_scripts);
+
+}  // namespace about_ui
+
+#endif  // COMPONENTS_ABOUT_UI_CREDIT_UTILS_H_
