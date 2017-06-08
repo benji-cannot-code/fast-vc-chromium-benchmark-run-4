01ce4b150f26d7354874d1043e560c8f4bd8f7a7
Pavel Feldman <pfeldman@chromium.org>
2017-06-08 16:35:51 -0700

    DevTools: remove stray error messages while devtools on devtools.
    Bug: 
    Change-Id: Iee80bacd32c4d3aa561e7187396c801fa2ce4eec
    Reviewed-on: https://chromium-review.googlesource.com/527505
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478139}

--- chrome/browser/devtools/devtools_ui_bindings.cc
+++ chrome/browser/devtools/devtools_ui_bindings.cc
@@ -1355,6 +1355,8 @@ void DevToolsUIBindings::AttachTo(
 
 void DevToolsUIBindings::Reload() {
   reloading_ = true;
+  if (agent_host_)
+    agent_host_->DetachClient(this);
   web_contents_->GetController().Reload(content::ReloadType::NORMAL, false);
 }
 
@@ -1398,10 +1400,8 @@ void DevToolsUIBindings::DocumentAvailableInMainFrame() {
   if (!reloading_)
     return;
   reloading_ = false;
-  if (agent_host_.get()) {
-    agent_host_->DetachClient(this);
+  if (agent_host_.get())
     agent_host_->AttachClient(this);
-  }
 }
 
 void DevToolsUIBindings::DocumentOnLoadCompletedInMainFrame() {
