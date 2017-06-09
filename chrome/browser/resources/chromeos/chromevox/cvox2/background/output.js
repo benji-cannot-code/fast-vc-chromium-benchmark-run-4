70d5d5cb7b20b31f89e82f31216f0eccebba28d2
dtseng <dtseng@chromium.org>
2017-06-08 22:46:48 -0700

    Rich editable text implementation using spannables CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    Review-Url: https://codereview.chromium.org/2903973002
    Cr-Commit-Position: refs/heads/master@{#478211}

--- chrome/browser/resources/chromeos/chromevox/cvox2/background/output.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/background/output.js
@@ -770,7 +770,7 @@ Output.prototype = {
    */
   get hasSpeech() {
     for (var i = 0; i < this.speechBuffer_.length; i++) {
-      if (this.speechBuffer_[i].trim().length)
+      if (this.speechBuffer_[i].length)
         return true;
     }
     return false;
