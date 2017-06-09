784275cf17910c85262510610a3935560e12ed21
Raymond Toy <rtoy@chromium.org>
2017-06-09 10:39:17 -0700

    Throw RangeError for invalid start/stop times
    Calling start() or stop() with negative time values should throw
    RangeError, not InvalidAccessError.  This also includes the offset and
    duration parameters for AudioBufferSourceNode.start().
    
    constant-source-basic.html,
    oscillator-basic.html
    
    Bug: 731242
    Test: audiobuffersource-basic.html,
    Change-Id: Ide02d22a66712ba46aa1f0007c71ae24e78b88fe
    Reviewed-on: https://chromium-review.googlesource.com/528479
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Raymond Toy <rtoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478776}

--- third_party/WebKit/Source/modules/webaudio/AudioScheduledSourceNode.cpp
+++ third_party/WebKit/Source/modules/webaudio/AudioScheduledSourceNode.cpp
@@ -169,8 +169,7 @@ void AudioScheduledSourceHandler::Start(double when,
   }
 
   if (when < 0) {
-    exception_state.ThrowDOMException(
-        kInvalidAccessError,
+    exception_state.ThrowRangeError(
         ExceptionMessages::IndexExceedsMinimumBound("start time", when, 0.0));
     return;
   }
@@ -202,8 +201,7 @@ void AudioScheduledSourceHandler::Stop(double when,
   }
 
   if (when < 0) {
-    exception_state.ThrowDOMException(
-        kInvalidAccessError,
+    exception_state.ThrowRangeError(
         ExceptionMessages::IndexExceedsMinimumBound("stop time", when, 0.0));
     return;
   }
