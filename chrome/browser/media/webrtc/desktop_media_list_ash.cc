b7195caef5054515341918ee314cef30910f5e0f
Sergey Ulanov <sergeyu@chromium.org>
2017-06-09 11:08:52 -0700

    Update DesktopMediaListAsh to show only windows or only screens
    Windows and Screens are shown in separate views in the desktop capture
    picker UI, so we no longer need both types of sources in a single list.
    
    Bug: 728866
    Change-Id: I96df72f2920b0b5b9ebc3bbc6adef1ef939a1a64
    Reviewed-on: https://chromium-review.googlesource.com/527772
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Zijie He <zijiehe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478385}

--- chrome/browser/media/webrtc/desktop_media_list_ash.cc
+++ chrome/browser/media/webrtc/desktop_media_list_ash.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/snapshot/snapshot.h"
 
 using content::BrowserThread;
+using content::DesktopMediaID;
 
 namespace {
 
@@ -24,12 +25,15 @@ const int kDefaultUpdatePeriod = 500;
 
 }  // namespace
 
-DesktopMediaListAsh::DesktopMediaListAsh(int source_types)
+DesktopMediaListAsh::DesktopMediaListAsh(content::DesktopMediaID::Type type)
     : DesktopMediaListBase(
           base::TimeDelta::FromMilliseconds(kDefaultUpdatePeriod)),
-      source_types_(source_types),
+      type_(type),
       pending_window_capture_requests_(0),
-      weak_factory_(this) {}
+      weak_factory_(this) {
+  DCHECK(type == content::DesktopMediaID::TYPE_SCREEN ||
+         type == content::DesktopMediaID::TYPE_WINDOW);
+}
 
 DesktopMediaListAsh::~DesktopMediaListAsh() {}
 
@@ -71,7 +75,7 @@ void DesktopMediaListAsh::EnumerateSources(
   aura::Window::Windows root_windows = ash::Shell::GetAllRootWindows();
 
   for (size_t i = 0; i < root_windows.size(); ++i) {
-    if (source_types_ & SCREENS) {
+    if (type_ == content::DesktopMediaID::TYPE_SCREEN) {
       SourceDescription screen_source(
           content::DesktopMediaID::RegisterAuraWindow(
               content::DesktopMediaID::TYPE_SCREEN, root_windows[i]),
@@ -99,9 +103,7 @@ void DesktopMediaListAsh::EnumerateSources(
       }
 
       CaptureThumbnail(screen_source.id, root_windows[i]);
-    }
-
-    if (source_types_ & WINDOWS) {
+    } else {
       EnumerateWindowsForRoot(
           sources, root_windows[i], ash::kShellWindowId_DefaultContainer);
       EnumerateWindowsForRoot(
--- chrome/browser/media/webrtc/desktop_media_list_ash.h
+++ chrome/browser/media/webrtc/desktop_media_list_ash.h
@@ -22,12 +22,7 @@ class Image;
 // native windows.
 class DesktopMediaListAsh : public DesktopMediaListBase {
  public:
-  enum SourceTypes {
-    SCREENS = 1,
-    WINDOWS = 2,
-  };
-
-  explicit DesktopMediaListAsh(int source_types);
+  explicit DesktopMediaListAsh(content::DesktopMediaID::Type type);
   ~DesktopMediaListAsh() override;
 
  private:
@@ -43,7 +38,7 @@ class DesktopMediaListAsh : public DesktopMediaListBase {
   void OnThumbnailCaptured(content::DesktopMediaID id,
                            const gfx::Image& image);
 
-  int source_types_;
+  const content::DesktopMediaID::Type type_;
 
   int pending_window_capture_requests_;
 
