4734783f48bcbcf6908e6c88821779d69ad730c2
erikchen <erikchen@chromium.org>
2017-06-09 14:56:03 -0700

    Correctly initialize MemoryDumpArgs in the MojoHandleTable test.
    BUG=705785
    
    Review-Url: https://codereview.chromium.org/2930103002
    Cr-Commit-Position: refs/heads/master@{#478430}

--- mojo/edk/system/handle_table_unittest.cc
+++ mojo/edk/system/handle_table_unittest.cc
@@ -65,7 +65,8 @@ TEST(HandleTableTest, OnMemoryDump) {
     ht.AddDispatcher(dispatcher);
   }
 
-  base::trace_event::MemoryDumpArgs args;
+  base::trace_event::MemoryDumpArgs args = {
+      base::trace_event::MemoryDumpLevelOfDetail::DETAILED};
   base::trace_event::ProcessMemoryDump pmd(nullptr, args);
   ht.OnMemoryDump(args, &pmd);
 
