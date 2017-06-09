403343722cb6b2ab98f16ac35e75b068a536fbfd
Xida Chen <xidachen@chromium.org>
2017-06-09 11:24:22 -0400

    Move csspaint/ layout tests to external/wpt/
    Originally I had a pull request here:
    https://github.com/w3c/web-platform-tests/pull/6163
    
    Later on, as pointed out by foolip@, we could just do it in one Chromium
    CL, so here it is. Once this lands, I will close the above pull request.
    
    In the final patch, I add the tests to external/wpt/ first. In a follow up
    CL, I will try to remove corresponding tests in http/tests/csspaint/.
    The reason of breaking into two patches is that doing it in one patch broke
    some unrelated tests. Please refer to the trybot results in PS#4 and PS#5.
    
    Bug: 728593
    Change-Id: Iec3affaa20d09e48a73e777d77e550c10a9c0d54
    Reviewed-on: https://chromium-review.googlesource.com/527352
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478315}

---
+++ third_party/WebKit/LayoutTests/external/wpt/common/css-paint-tests.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// To make sure that we take the snapshot at the right time, we do double
+// requestAnimationFrame. In the second frame, we take a screenshot, that makes
+// sure that we already have a full frame.
+function importPaintWorkletAndTerminateTestAfterAsyncPaint(code) {
+    if (typeof paintWorklet == "undefined") {
+        takeScreenshot();
+    } else {
+        var blob = new Blob([code], {type: 'text/javascript'});
+        paintWorklet.addModule(URL.createObjectURL(blob)).then(function() {
+            requestAnimationFrame(function() {
+                requestAnimationFrame(function() {
+                    takeScreenshot();
+                });
+            });
+        });
+    }
+}
+
