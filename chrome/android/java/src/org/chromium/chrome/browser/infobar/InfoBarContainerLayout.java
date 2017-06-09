f7cbad3e0cb432f06938a9799b7caeb4bc39dbc8
Matthew Jones <mdjones@chromium.org>
2017-06-09 11:30:28 -0700

    Fix infobar animation-end race
    This change allows individual infobar animations to clean up after
    themselves before another animation is allowed to start. This is
    controlled by setting the current animation to null only after
    onAnimationEnd is called for the InfoBarAnimation.
    
    BUG=729575
    
    Change-Id: I86a513435083c99d5de70407306309af99931817
    Reviewed-on: https://chromium-review.googlesource.com/529685
    Reviewed-by: Dan Alcantara <dfalcantara@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478462}

--- chrome/android/java/src/org/chromium/chrome/browser/infobar/InfoBarContainerLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/infobar/InfoBarContainerLayout.java
@@ -219,8 +219,8 @@ public class InfoBarContainerLayout extends FrameLayout {
             Animator.AnimatorListener listener = new AnimatorListenerAdapter() {
                 @Override
                 public void onAnimationEnd(Animator animation) {
-                    mAnimation = null;
                     InfoBarAnimation.this.onAnimationEnd();
+                    mAnimation = null;
                     for (InfoBarAnimationListener listener : mAnimationListeners) {
                         listener.notifyAnimationFinished(getAnimationType());
                     }
@@ -449,19 +449,19 @@ public class InfoBarContainerLayout extends FrameLayout {
                 public void onAnimationStart(Animator animation) {
                     setHierarchyClipsChildren(false);
                 }
-
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mAppearingWrapper.setRestrictHeightForAnimation(false);
-                    mAppearingWrapper.removeView(mAppearingWrapper.getItem().getView());
-                    setHierarchyClipsChildren(true);
-                }
             });
 
             set.playSequentially(animators);
             set.setDuration(DURATION_PEEK_MS);
 
             return set;
+        }
+
+        @Override
+        public void onAnimationEnd() {
+            mAppearingWrapper.setRestrictHeightForAnimation(false);
+            mAppearingWrapper.removeView(mAppearingWrapper.getItem().getView());
+            setHierarchyClipsChildren(true);
         }
 
         @Override
