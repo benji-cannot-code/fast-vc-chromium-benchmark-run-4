ce7c0f58135a7b0a56df1e4180ef841254180782
David Reveman <reveman@chromium.org>
2017-06-09 15:48:19 -0400

    ppapi: Add support for single buffer contexts.
    BUG=732473
    
    Change-Id: Ib89549a861016f0927bb6dfac2586be6e0ba1932
    Reviewed-on: https://chromium-review.googlesource.com/529925
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479023}

--- ppapi/c/pp_graphics_3d.h
+++ ppapi/c/pp_graphics_3d.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-/* From pp_graphics_3d.idl modified Wed Oct  3 15:55:01 2012. */
+/* From pp_graphics_3d.idl modified Fri Jun  9 15:44:15 2017. */
 
 #ifndef PPAPI_C_PP_GRAPHICS_3D_H_
 #define PPAPI_C_PP_GRAPHICS_3D_H_
@@ -93,7 +93,12 @@ typedef enum {
   /**
    * The context may be high-power and may be created on a discrete gpu.
    */
-  PP_GRAPHICS3DATTRIB_GPU_PREFERENCE_PERFORMANCE = 0x11002
+  PP_GRAPHICS3DATTRIB_GPU_PREFERENCE_PERFORMANCE = 0x11002,
+  /**
+   * Whether or not offscreen color buffers exist in front/back pairs that
+   * can be swapped.
+   */
+  PP_GRAPHICS3DATTRIB_SINGLE_BUFFER = 0x3085
 } PP_Graphics3DAttrib;
 PP_COMPILE_ASSERT_SIZE_IN_BYTES(PP_Graphics3DAttrib, 4);
 /**
