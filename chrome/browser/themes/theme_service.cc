1ccd4a3ae9bc43bec4432fb0bc1b1340be863153
Evan Stade <estade@chromium.org>
2017-06-09 14:17:14 -0600

    Don't show theme uninstall infobar for synced themes.
    Bug: 731688
    Change-Id: I4c42adc0e5a11dae0486824fc7c0754413f601c8
    Reviewed-on: https://chromium-review.googlesource.com/530066
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478667}

--- chrome/browser/themes/theme_service.cc
+++ chrome/browser/themes/theme_service.cc
@@ -285,7 +285,7 @@ void ThemeService::Observe(int type,
 }
 
 void ThemeService::SetTheme(const Extension* extension) {
-  DoSetTheme(extension, false);
+  DoSetTheme(extension, true);
 }
 
 void ThemeService::RevertToTheme(const Extension* extension) {
