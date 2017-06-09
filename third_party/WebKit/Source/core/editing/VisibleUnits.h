4fd20bb98fde6aaf03872a9d0755424f7f71a500
yosin <yosin@chromium.org>
2017-06-08 18:11:01 -0700

    Move InSameBlock() to InsertParagraphSeparatorCommand.cpp
    This patch moves |InSameBlock()| to "InsertParagraphSeparatorCommand.cpp" since
    it is used only in there for improving code health.
    
    BUG=n/a
    TEST=n/a; no behavior changes
    
    Review-Url: https://codereview.chromium.org/2932643003
    Cr-Commit-Position: refs/heads/master@{#478150}

--- third_party/WebKit/Source/core/editing/VisibleUnits.h
+++ third_party/WebKit/Source/core/editing/VisibleUnits.h
@@ -285,7 +285,6 @@ VisiblePosition StartOfBlock(
 VisiblePosition EndOfBlock(
     const VisiblePosition&,
     EditingBoundaryCrossingRule = kCannotCrossEditingBoundary);
-bool InSameBlock(const VisiblePosition&, const VisiblePosition&);
 bool IsStartOfBlock(const VisiblePosition&);
 bool IsEndOfBlock(const VisiblePosition&);
 
