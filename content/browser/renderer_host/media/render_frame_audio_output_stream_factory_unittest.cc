d64487a83055f21687daf9980db5edd7a8f74537
Max Morin <maxmorin@chromium.org>
2017-06-09 14:31:40 +0200

    Remove unused GetController mock methods.
    These were left over from some previous cleanup.
    
    BUG = None
    
    Change-Id: I6588e2b7606d34a032d1ec892ec272f98ca3abb5
    Reviewed-on: https://chromium-review.googlesource.com/529184
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Max Morin <maxmorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478570}

--- content/browser/renderer_host/media/render_frame_audio_output_stream_factory_unittest.cc
+++ content/browser/renderer_host/media/render_frame_audio_output_stream_factory_unittest.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/test/test_browser_context.h"
 #include "content/public/test/test_browser_thread_bundle.h"
-#include "media/audio/audio_output_controller.h"
 #include "media/base/audio_parameters.h"
 #include "mojo/edk/embedder/embedder.h"
 #include "mojo/public/cpp/bindings/binding.h"
@@ -73,8 +72,6 @@ class MockAudioOutputDelegate : public media::AudioOutputDelegate {
       std::move(on_destruction_).Run();
   }
 
-  MOCK_CONST_METHOD0(GetController,
-                     scoped_refptr<media::AudioOutputController>());
   MOCK_CONST_METHOD0(GetStreamId, int());
   MOCK_METHOD0(OnPlayStream, void());
   MOCK_METHOD0(OnPauseStream, void());
--- media/mojo/services/mojo_audio_output_stream_unittest.cc
+++ media/mojo/services/mojo_audio_output_stream_unittest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/sync_socket.h"
 #include "media/audio/audio_output_controller.h"
-#include "media/mojo/interfaces/audio_output_stream.mojom.h"
 #include "mojo/public/cpp/system/platform_handle.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -61,7 +60,6 @@ class MockDelegate : NON_EXPORTED_BASE(public AudioOutputDelegate) {
   MockDelegate() {}
   ~MockDelegate() {}
 
-  MOCK_CONST_METHOD0(GetController, scoped_refptr<AudioOutputController>());
   MOCK_CONST_METHOD0(GetStreamId, int());
   MOCK_METHOD0(OnPlayStream, void());
   MOCK_METHOD0(OnPauseStream, void());
