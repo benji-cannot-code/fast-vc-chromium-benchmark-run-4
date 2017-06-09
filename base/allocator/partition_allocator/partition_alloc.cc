24c7f02e13381802ac50b7370bc37ebe4395a957
tasak <tasak@google.com>
2017-06-09 00:20:39 -0700

    Need to update last_slot when unprovisioned_bytes && discard.
    last_slot points the last node of PartitionFreelistEntry freelist.
    However, when truncating slots, the freelist is reordered.
    So last_slot calculated previously is not valid.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2921283002
    Cr-Commit-Position: refs/heads/master@{#478222}

--- base/allocator/partition_allocator/partition_alloc.cc
+++ base/allocator/partition_allocator/partition_alloc.cc
@@ -1163,6 +1163,7 @@ static size_t PartitionPurgePage(PartitionPage* page, bool discard) {
       *entry_ptr = PartitionFreelistMask(entry);
       entry_ptr = reinterpret_cast<PartitionFreelistEntry**>(entry);
       num_new_entries++;
+      last_slot = slotIndex;
     }
     // Terminate the freelist chain.
     *entry_ptr = nullptr;
