46f8d49a045d92c5d2765826b10e0267e1a467a9
Alex Zhang <staraz@chromium.org>
2017-06-09 14:34:47 -0400

    Wrap GpuHost in an Interface
    This CL extracts the public methods from GpuHost to form a GpuHost interface.
    The existing GpuHost is renamed to DefaultGpuHost and implements the GpuHost
    interface.
    
    The DefaultGpuHost sets up connection from clients to the real service
    implementation in the GPU process. With the GpuHost interface, WS tests can
    inject a test implementation of GpuHost to WindowServer and we can have unit
    tests without any mojo connections.
    
    Test implementations will be added in a separate CL.
    
    Bug: 698026
    Change-Id: I5e283b6230dd0508386fc4ae74fdfaeca3361b4d
    Reviewed-on: https://chromium-review.googlesource.com/526077
    Commit-Queue: Xingyu Zhang <staraz@chromium.org>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478381}

--- services/ui/ws/test_utils.cc
+++ services/ui/ws/test_utils.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/ui/ws/display_creation_config.h"
 #include "services/ui/ws/display_manager.h"
 #include "services/ui/ws/frame_sink_manager_client_binding.h"
+#include "services/ui/ws/gpu_host.h"
 #include "services/ui/ws/window_manager_access_policy.h"
 #include "services/ui/ws/window_manager_window_tree_factory.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -554,6 +555,11 @@ WindowServerTestHelper::WindowServerTestHelper()
     message_loop_ = base::MakeUnique<base::MessageLoop>();
   PlatformDisplay::set_factory_for_testing(&platform_display_factory_);
   window_server_ = base::MakeUnique<WindowServer>(&window_server_delegate_);
+  // TODO(staraz): Replace DefaultGpuHost and FrameSinkManagerClientBinding with
+  // test implementations.
+  std::unique_ptr<GpuHost> gpu_host =
+      base::MakeUnique<DefaultGpuHost>(window_server_.get());
+  window_server_->SetGpuHost(std::move(gpu_host));
   std::unique_ptr<FrameSinkManagerClientBinding> frame_sink_manager =
       base::MakeUnique<FrameSinkManagerClientBinding>(
           window_server_.get(), window_server_->gpu_host());
