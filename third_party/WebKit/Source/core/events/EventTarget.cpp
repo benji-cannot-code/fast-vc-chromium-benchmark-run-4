5121e37355e8efb3ba97c83e18ff3a143ffa010b
Dave Tapuska <dtapuska@chromium.org>
2017-06-09 12:12:15 -0400

    Enable Smooth Scroll.js intervention.
    Add a console message indicating the library is no longer necessary and
    it is being treated as a passive listener.
    
    Approved intent to ship: https://groups.google.com/a/chromium.org/d/msg/blink-dev/2iOz5-fgD8Y/GO7qLkg4BwAJ
    
    BUG=501568
    
    Change-Id: I07e48572d046d6e47d65fd38a12a7ba2f2a68d74
    Reviewed-on: https://chromium-review.googlesource.com/529424
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478343}

--- third_party/WebKit/Source/core/events/EventTarget.cpp
+++ third_party/WebKit/Source/core/events/EventTarget.cpp
@@ -44,10 +44,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/events/Event.h"
 #include "core/events/EventUtil.h"
 #include "core/events/PointerEvent.h"
+#include "core/frame/FrameConsole.h"
 #include "core/frame/LocalDOMWindow.h"
 #include "core/frame/PerformanceMonitor.h"
 #include "core/frame/Settings.h"
 #include "core/frame/UseCounter.h"
+#include "core/inspector/ConsoleMessage.h"
 #include "core/probe/CoreProbes.h"
 #include "platform/EventDispatchForbiddenScope.h"
 #include "platform/Histogram.h"
@@ -242,7 +244,16 @@ void EventTarget::SetDefaultAddEventListenerOptions(
         if (executing_window) {
           UseCounter::Count(executing_window->document(),
                             WebFeature::kSmoothScrollJSInterventionActivated);
+
+          executing_window->GetFrame()->Console().AddMessage(
+              ConsoleMessage::Create(
+                  kInterventionMessageSource, kWarningMessageLevel,
+                  "Registering mousewheel event as passive due to "
+                  "smoothscroll.js usage. The smoothscroll.js library is "
+                  "buggy, no longer necessary and degrades performance. See "
+                  "https://www.chromestatus.com/feature/5749447073988608"));
         }
+
         return;
       }
     }
