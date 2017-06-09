a341966965ddafc3130f9f6360ab94431ce0fa16
gambard <gambard@chromium.org>
2017-06-09 15:17:32 +0200

    Use generic item in the ReadingListCollectionVC
    The ReadingListCollectionViewController now use only CollectionViewItem.
    This allows the ReadingListCollectionVC and the ReadingListVC to move to the
    UI target, giving a layer separation.
    
    BUG=721758
    
    Change-Id: I43690c74d493ecd6f6c2ec653035da9f60674497
    Reviewed-on: https://chromium-review.googlesource.com/529126
    Reviewed-by: Louis Romero <lpromero@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478274}

--- ios/chrome/browser/ui/reading_list/reading_list_data_source.h
+++ ios/chrome/browser/ui/reading_list/reading_list_data_source.h
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 @class CollectionViewItem;
-@class ReadingListCollectionViewItem;
 @protocol ReadingListDataSink;
+@protocol ReadingListCollectionViewItemAccessibilityDelegate;
 
 // Data Source for the Reading List UI, providing the data sink with the data to
 // be displayed. Handle the interactions with the model.
@@ -38,14 +38,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Fills the |readArray| and |unreadArray| with the corresponding items from the
 // model. The items are sorted most recent first.
-- (void)fillReadItems:
-            (nullable NSMutableArray<ReadingListCollectionViewItem*>*)readArray
-          unreadItems:(nullable NSMutableArray<ReadingListCollectionViewItem*>*)
-                          unreadArray;
+- (void)fillReadItems:(nullable NSMutableArray<CollectionViewItem*>*)readArray
+          unreadItems:(nullable NSMutableArray<CollectionViewItem*>*)unreadArray
+         withDelegate:
+             (nullable id<ReadingListCollectionViewItemAccessibilityDelegate>)
+                 delegate;
 
 // Fetches the |faviconURL| of this |item|, notifies the data sink when
 // receiving the favicon.
-- (void)fetchFaviconForItem:(nonnull ReadingListCollectionViewItem*)item;
+- (void)fetchFaviconForItem:(nonnull CollectionViewItem*)item;
 
 // Prepares the data source for batch updates. The UI is not notified for the
 // updates happenning between |-beginBatchUpdates| and |-endBatchUpdates|.
--- ios/chrome/browser/ui/reading_list/reading_list_mediator_unittest.mm
+++ ios/chrome/browser/ui/reading_list/reading_list_mediator_unittest.mm
@@ -15,10 +15,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/url_formatter/url_formatter.h"
 #include "ios/chrome/browser/favicon/ios_chrome_large_icon_service_factory.h"
 #import "ios/chrome/browser/ui/reading_list/reading_list_collection_view_item.h"
+#import "ios/chrome/browser/ui/reading_list/reading_list_collection_view_item_accessibility_delegate.h"
 #include "ios/web/public/test/test_web_thread_bundle.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
+#import "third_party/ocmock/OCMock/OCMock.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -80,20 +82,29 @@ class ReadingListMediatorTest : public PlatformTest {
 
 TEST_F(ReadingListMediatorTest, fillItems) {
   // Setup.
-  NSMutableArray<ReadingListCollectionViewItem*>* readArray =
-      [NSMutableArray array];
-  NSMutableArray<ReadingListCollectionViewItem*>* unreadArray =
-      [NSMutableArray array];
+  NSMutableArray<CollectionViewItem*>* readArray = [NSMutableArray array];
+  NSMutableArray<CollectionViewItem*>* unreadArray = [NSMutableArray array];
+  id mockDelegate = OCMProtocolMock(
+      @protocol(ReadingListCollectionViewItemAccessibilityDelegate));
 
   // Action.
-  [mediator_ fillReadItems:readArray unreadItems:unreadArray];
+  [mediator_ fillReadItems:readArray
+               unreadItems:unreadArray
+              withDelegate:mockDelegate];
 
   // Tests.
   EXPECT_EQ(3U, [unreadArray count]);
   EXPECT_EQ(2U, [readArray count]);
-  EXPECT_TRUE([unreadArray[0].title
+  NSArray<ReadingListCollectionViewItem*>* rlReadArray = [readArray copy];
+  NSArray<ReadingListCollectionViewItem*>* rlUneadArray = [unreadArray copy];
+  EXPECT_TRUE([rlUneadArray[0].title
       isEqualToString:base::SysUTF16ToNSString(url_formatter::FormatUrl(
                           no_title_entry_url_.GetOrigin()))]);
-  EXPECT_TRUE([readArray[0].title isEqualToString:@"read2"]);
-  EXPECT_TRUE([readArray[1].title isEqualToString:@"read1"]);
+  EXPECT_TRUE([rlReadArray[0].title isEqualToString:@"read2"]);
+  EXPECT_TRUE([rlReadArray[1].title isEqualToString:@"read1"]);
+  EXPECT_EQ(mockDelegate, rlReadArray[0].accessibilityDelegate);
+  EXPECT_EQ(mockDelegate, rlReadArray[1].accessibilityDelegate);
+  EXPECT_EQ(mockDelegate, rlUneadArray[0].accessibilityDelegate);
+  EXPECT_EQ(mockDelegate, rlUneadArray[1].accessibilityDelegate);
+  EXPECT_EQ(mockDelegate, rlUneadArray[2].accessibilityDelegate);
 }
