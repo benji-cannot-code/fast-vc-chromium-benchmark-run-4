8d458247171a9701ff62cab6668d42010b4915ba
suzyh <suzyh@chromium.org>
2017-06-08 23:24:16 -0700

    Implement Animation constructor
    This patch changes the Animation object from NoInterfaceObject to having
    a constructor, and implements two corresponding Animation::create
    functions. The implementations are only exposed behind the
    --enable-experimental-web-platform-features flag in the Window context.
    
    Test expectations are updated accordingly.
    
    BUG=674513
    
    Review-Url: https://codereview.chromium.org/2806623003
    Cr-Commit-Position: refs/heads/master@{#478215}

--- third_party/WebKit/Source/core/animation/Animation.cpp
+++ third_party/WebKit/Source/core/animation/Animation.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/animation/AnimationTimeline.h"
 #include "core/animation/CompositorPendingAnimations.h"
+#include "core/animation/DocumentTimeline.h"
 #include "core/animation/KeyframeEffectReadOnly.h"
 #include "core/animation/css/CSSAnimations.h"
 #include "core/dom/DOMNodeIds.h"
@@ -71,6 +72,7 @@ Animation* Animation::Create(AnimationEffectReadOnly* effect,
                              AnimationTimeline* timeline) {
   if (!timeline) {
     // FIXME: Support creating animations without a timeline.
+    NOTREACHED();
     return nullptr;
   }
 
@@ -83,6 +85,28 @@ Animation* Animation::Create(AnimationEffectReadOnly* effect,
   }
 
   return animation;
+}
+
+Animation* Animation::Create(ExecutionContext* execution_context,
+                             AnimationEffectReadOnly* effect,
+                             ExceptionState& exception_state) {
+  DCHECK(RuntimeEnabledFeatures::WebAnimationsAPIEnabled());
+
+  Document* document = ToDocument(execution_context);
+  return Create(effect, &document->Timeline());
+}
+
+Animation* Animation::Create(ExecutionContext* execution_context,
+                             AnimationEffectReadOnly* effect,
+                             AnimationTimeline* timeline,
+                             ExceptionState& exception_state) {
+  DCHECK(RuntimeEnabledFeatures::WebAnimationsAPIEnabled());
+
+  if (!timeline) {
+    return Create(execution_context, effect, exception_state);
+  }
+
+  return Create(effect, timeline);
 }
 
 Animation::Animation(ExecutionContext* execution_context,
--- third_party/WebKit/Source/core/animation/Animation.h
+++ third_party/WebKit/Source/core/animation/Animation.h
@@ -76,6 +76,16 @@ class CORE_EXPORT Animation final : public EventTargetWithInlineData,
   };
 
   static Animation* Create(AnimationEffectReadOnly*, AnimationTimeline*);
+
+  // Web Animations API IDL constructors.
+  static Animation* Create(ExecutionContext*,
+                           AnimationEffectReadOnly*,
+                           ExceptionState&);
+  static Animation* Create(ExecutionContext*,
+                           AnimationEffectReadOnly*,
+                           AnimationTimeline*,
+                           ExceptionState&);
+
   ~Animation();
   void Dispose();
 
