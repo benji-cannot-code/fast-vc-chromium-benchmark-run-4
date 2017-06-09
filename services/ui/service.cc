9fb659ac5ddaff792a8feb6e11b758e9804b4607
Scott Violet <sky@chromium.org>
2017-06-09 08:13:57 -0700

    chromeos: adds InputDeviceController mojo interface
    This interface will be used by Chrome to set touchpad/mouse related
    preferences, and by ash to disable touchpad (in certain scenarios),
    among other things.
    
    BUG=642863
    TEST=none
    
    Change-Id: I3bb3b62e166bcd17817c928af4cdf37e27b83895
    Reviewed-on: https://chromium-review.googlesource.com/526734
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478312}

--- services/ui/service.cc
+++ services/ui/service.cc
@@ -61,6 +61,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/public/ozone_platform.h"
 #endif
 
+#if defined(OS_CHROMEOS) && defined(USE_OZONE)
+#include "services/ui/public/cpp/input_devices/input_device_controller.h"
+#endif
+
 using mojo::InterfaceRequest;
 using ui::mojom::WindowServerTest;
 using ui::mojom::WindowTreeHostFactory;
@@ -99,6 +103,11 @@ Service::~Service() {
   window_server_.reset();
 
 #if defined(USE_OZONE)
+#if defined(OS_CHROMEOS)
+  // InputDeviceController uses ozone.
+  input_device_controller_.reset();
+#endif
+
   OzonePlatform::Shutdown();
 #endif
 }
@@ -179,7 +188,7 @@ void Service::OnStart() {
   params.single_process = false;
   ui::OzonePlatform::InitializeForUI(params);
 
-  // TODO(kylechar): We might not always want a US keyboard layout.
+  // Assume a client will change the layout to an appropriate configuration.
   ui::KeyboardLayoutEngineManager::GetKeyboardLayoutEngine()
       ->SetCurrentLayoutByName("us");
   client_native_pixmap_factory_ = ui::CreateClientNativePixmapFactoryOzone();
@@ -187,6 +196,11 @@ void Service::OnStart() {
       client_native_pixmap_factory_.get());
 
   DCHECK(gfx::ClientNativePixmapFactory::GetInstance());
+
+#if defined(OS_CHROMEOS)
+  input_device_controller_ = base::MakeUnique<InputDeviceController>();
+  input_device_controller_->AddInterface(&registry_);
+#endif
 #endif
 
 // TODO(rjkroege): Enter sandbox here before we start threads in GpuState
