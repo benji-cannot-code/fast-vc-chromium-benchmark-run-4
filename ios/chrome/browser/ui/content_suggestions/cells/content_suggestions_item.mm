5e696663bd11e2f85486860e3cadca6df7cbf911
gambard <gambard@chromium.org>
2017-06-09 09:27:09 +0200

    Add accessibility label for Content Suggestions
    Add an accessibility label for the suggestion cells.
    
    BUG=722750
    
    Change-Id: I25a0f903452e01c25d03f757d93b2bfa3c61f0f6
    Reviewed-on: https://chromium-review.googlesource.com/528093
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Jungshik Shin <jshin@chromium.org>
    Reviewed-by: Louis Romero <lpromero@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478232}

--- ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_item.mm
+++ ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_item.mm
@@ -80,6 +80,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [cell setAdditionalInformationWithPublisherName:self.publisher
                                              date:[self relativeDate]
                               offlineAvailability:self.availableOffline];
+  cell.isAccessibilityElement = YES;
+  cell.accessibilityLabel = [self accessibilityLabel];
 }
 
 - (void)setImage:(UIImage*)image {
@@ -107,6 +109,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   return relativeDate;
+}
+
+// Returns the accessibility label.
+- (NSString*)accessibilityLabel {
+  NSString* offlineAvailability = @"";
+  if (self.availableOffline) {
+    offlineAvailability = l10n_util::GetNSString(
+        IDS_IOS_CONTENT_SUGGESTIONS_ACCESSIBILITY_AVAILABLE_OFFLINE);
+  }
+
+  return l10n_util::GetNSStringF(
+      IDS_IOS_CONTENT_SUGGESTIONS_ACCESSIBILITY_LABEL_SUGGESTION,
+      base::SysNSStringToUTF16(self.title),
+      base::SysNSStringToUTF16(self.publisher),
+      base::SysNSStringToUTF16([self relativeDate]),
+      base::SysNSStringToUTF16(offlineAvailability),
+      base::SysNSStringToUTF16(self.subtitle));
 }
 
 @end
