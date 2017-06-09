61aa90dd86adebf2564db4c0e3904c1df7b050e1
thomasanderson <thomasanderson@chromium.org>
2017-06-09 13:34:49 -0700

    Fix ubsan vptr error in HttpServerPropertiesImpl()
    CL [1] introduced a constructor for HttpServerPropertiesImpl that uses
    a delgated constructor and passes the address of a member as an
    argument to it.  When doing this, the member is not yet intialized, so
    ubsan will complain about "use of an object whose vptr indicates
    that... its lifetime has not begun..." [2].
    
    This specific case appears to be okay since only the pointer is passed
    and nothing is done with the value.  This CL is just to fix the
    failures on linux_chromium_ubsan_rel_ng [3].
    
    [1] https://codereview.chromium.org/2898983006
    [2] https://clang.llvm.org/docs/UndefinedBehaviorSanitizer.html
    [3] https://build.chromium.org/p/tryserver.chromium.linux/builders/linux_chromium_ubsan_rel_ng
    
    R=asanka@chromium.org
    CC=wangyix@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_chromium_ubsan_rel_ng
    
    Review-Url: https://codereview.chromium.org/2935433002
    Cr-Commit-Position: refs/heads/master@{#478400}

--- net/http/http_server_properties_impl.cc
+++ net/http/http_server_properties_impl.cc
@@ -22,11 +22,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace net {
 
 HttpServerPropertiesImpl::HttpServerPropertiesImpl()
-    : HttpServerPropertiesImpl(&broken_alternative_services_clock_) {}
+    : HttpServerPropertiesImpl(nullptr) {}
 
 HttpServerPropertiesImpl::HttpServerPropertiesImpl(
     base::TickClock* broken_alternative_services_clock)
-    : broken_alternative_services_(this, broken_alternative_services_clock),
+    : broken_alternative_services_(this,
+                                   broken_alternative_services_clock
+                                       ? broken_alternative_services_clock
+                                       : &broken_alternative_services_clock_),
       spdy_servers_map_(SpdyServersMap::NO_AUTO_EVICT),
       alternative_service_map_(AlternativeServiceMap::NO_AUTO_EVICT),
       server_network_stats_map_(ServerNetworkStatsMap::NO_AUTO_EVICT),
