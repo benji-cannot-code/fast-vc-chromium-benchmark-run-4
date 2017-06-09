5e4559d8e7a86731f416c335df75af869fbc6926
shanmuga.m <shanmuga.m@samsung.com>
2017-06-09 00:13:45 -0700

    Add support for Element.getAttributeNames()
    Spec:
    https://dom.spec.whatwg.org/#dom-element-getattributenames
    
    Intent to implement and ship:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/s0pPWgyGexg
    
    Webkit patch:
    https://trac.webkit.org/changeset/203852/webkit
    
    BUG=727433
    
    Review-Url: https://codereview.chromium.org/2912383002
    Cr-Commit-Position: refs/heads/master@{#478220}

--- third_party/WebKit/Source/core/dom/Element.h
+++ third_party/WebKit/Source/core/dom/Element.h
@@ -373,6 +373,7 @@ class CORE_EXPORT Element : public ContainerNode {
 
   // For exposing to DOM only.
   NamedNodeMap* attributesForBindings() const;
+  Vector<AtomicString> getAttributeNames() const;
 
   enum class AttributeModificationReason { kDirectly, kByParser, kByCloning };
   struct AttributeModificationParams {
