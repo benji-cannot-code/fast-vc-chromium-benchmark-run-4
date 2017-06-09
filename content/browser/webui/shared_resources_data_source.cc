11e5f640c18e4836185224768749d8e7b0c1f41f
yzshen <yzshen@chromium.org>
2017-06-09 01:11:33 -0700

    Mojo JS bindings: specify compress="gzip" for bindings JS files in .grd.
    Numbers measured using a Release Android build:
    =====================================
    Before:
    Analyzing pak files in out/ARelease/apks/MonochromePublic.apk...
    Total pak files: 99
    Total compressed size: 5.10m
    Total uncompressed size: 9.93m
    ...
    ====================================
    After:
    Analyzing pak files in out/ARelease/apks/MonochromePublic.apk...
    Total pak files: 99
    Total compressed size: 5.01m
    Total uncompressed size: 9.84m
    ...
    ===================================
    
    BUG=731243
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2931983002
    Cr-Commit-Position: refs/heads/master@{#478233}

--- content/browser/webui/shared_resources_data_source.cc
+++ content/browser/webui/shared_resources_data_source.cc
@@ -197,4 +197,8 @@ SharedResourcesDataSource::GetAccessControlAllowOriginForOrigin(
   return origin;
 }
 
+bool SharedResourcesDataSource::IsGzipped(const std::string& path) const {
+  return path == "js/mojo_bindings.js";
+}
+
 }  // namespace content
