df9e325f1ce6cb1dfdc3dfdf360af7099763ff32
ssid <ssid@chromium.org>
2017-06-08 20:42:05 -0700

    [memory-infra] Add API to ProcessMemoryDump to create ownership edges for base::SharedMemory
    This cl is adapted from hajimehoshi's
    chromium-review.googlesource.com/c/523684
    
    This CL adds a new method to ProcessMemoryDump to create proper
    ownership edges for base::SharedMemory. It handles both the cases
    where the global dumps are created by clients and global dumps are
    created by SharedMemoryTracker.
    
    BUG=604726
    
    Review-Url: https://codereview.chromium.org/2923123004
    Cr-Commit-Position: refs/heads/master@{#478187}

--- base/trace_event/process_memory_dump.h
+++ base/trace_event/process_memory_dump.h
@@ -31,6 +31,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace base {
+
+class UnguessableToken;
+
 namespace trace_event {
 
 class HeapProfilerSerializationState;
@@ -151,6 +154,32 @@ class BASE_EXPORT ProcessMemoryDump {
                                    const MemoryAllocatorDumpGuid& target,
                                    int importance);
 
+  // Creates ownership edges for memory backed by base::SharedMemory. Handles
+  // the case of cross process sharing and importnace of ownership for the case
+  // with and without the base::SharedMemory dump provider. The new version
+  // should just use global dumps created by SharedMemoryTracker and this
+  // function handles the transition until we get SharedMemory IDs through mojo
+  // channel crbug.com/713763. The weak version creates a weak global dump.
+  // |client_local_dump_guid| The guid of the local dump created by the client
+  // of base::SharedMemory.
+  // |client_global_dump_guid| The global guid given by the clients to create
+  // ownership edges of their own. These global dumps will no longer be required
+  // after the transition.
+  // |shared_memory_guid| The ID of the base::SharedMemory that is assigned
+  // globally, used to create global dump edges in the new model.
+  // |importance| Importance of the global dump edges to say if the current
+  // process owns the memory segment.
+  void CreateSharedMemoryOwnershipEdge(
+      const MemoryAllocatorDumpGuid& client_local_dump_guid,
+      const MemoryAllocatorDumpGuid& client_global_dump_guid,
+      const UnguessableToken& shared_memory_guid,
+      int importance);
+  void CreateWeakSharedMemoryOwnershipEdge(
+      const MemoryAllocatorDumpGuid& client_local_dump_guid,
+      const MemoryAllocatorDumpGuid& client_global_dump_guid,
+      const UnguessableToken& shared_memory_guid,
+      int importance);
+
   const AllocatorDumpEdgesMap& allocator_dumps_edges_for_testing() const {
     return allocator_dumps_edges_;
   }
@@ -201,6 +230,13 @@ class BASE_EXPORT ProcessMemoryDump {
 
   MemoryAllocatorDump* AddAllocatorDumpInternal(
       std::unique_ptr<MemoryAllocatorDump> mad);
+
+  void CreateSharedMemoryOwnershipEdgeInternal(
+      const MemoryAllocatorDumpGuid& client_local_dump_guid,
+      const MemoryAllocatorDumpGuid& client_global_dump_guid,
+      const UnguessableToken& shared_memory_guid,
+      int importance,
+      bool is_weak);
 
   MemoryAllocatorDump* GetBlackHoleMad();
 
