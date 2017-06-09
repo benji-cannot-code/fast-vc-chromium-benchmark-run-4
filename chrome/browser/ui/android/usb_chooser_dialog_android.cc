b58349db7c9ffaf635364be51b5d4f79bda2c02f
asimjour <asimjour@chromium.org>
2017-06-09 09:37:45 -0700

    Disable USB chooser dialog while in VR
    This change disabled display of the USB chooser dialog while in VR.
    
    BUG=730850
    
    Review-Url: https://codereview.chromium.org/2929843003
    Cr-Commit-Position: refs/heads/master@{#478300}

--- chrome/browser/ui/android/usb_chooser_dialog_android.cc
+++ chrome/browser/ui/android/usb_chooser_dialog_android.cc
@@ -30,9 +30,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/usb/usb_device.h"
 #include "device/usb/usb_device_filter.h"
 #include "device/usb/webusb_descriptors.h"
+#include "device/vr/features/features.h"
 #include "jni/UsbChooserDialog_jni.h"
 #include "ui/android/window_android.h"
 #include "url/gurl.h"
+
+#if BUILDFLAG(ENABLE_VR)
+#include "chrome/browser/android/vr_shell/vr_tab_helper.h"
+#endif  // BUILDFLAG(ENABLE_VR)
 
 using device::UsbDevice;
 using device::UsbDeviceFilter;
@@ -60,6 +65,16 @@ UsbChooserDialogAndroid::UsbChooserDialogAndroid(
       usb_service_observer_(this),
       filters_(filters),
       weak_factory_(this) {
+  content::WebContents* web_contents =
+      content::WebContents::FromRenderFrameHost(render_frame_host_);
+#if BUILDFLAG(ENABLE_VR)
+  if (vr_shell::VrTabHelper::IsInVr(web_contents)) {
+    DCHECK(!callback_.is_null());
+    callback_.Run(nullptr);
+    callback_.Reset();  // Reset |callback_| so that it is only run once.
+    return;
+  }
+#endif
   device::UsbService* usb_service =
       device::DeviceClient::Get()->GetUsbService();
   if (!usb_service)
@@ -69,8 +84,6 @@ UsbChooserDialogAndroid::UsbChooserDialogAndroid(
     usb_service_observer_.Add(usb_service);
 
   // Create (and show) the UsbChooser dialog.
-  content::WebContents* web_contents =
-      content::WebContents::FromRenderFrameHost(render_frame_host_);
   base::android::ScopedJavaLocalRef<jobject> window_android =
       content::ContentViewCore::FromWebContents(web_contents)
           ->GetWindowAndroid()
