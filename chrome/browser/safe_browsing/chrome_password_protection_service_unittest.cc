9047284dd39d45b6536f7c5e68c323420bf66196
Jialiu Lin <jialiul@chromium.org>
2017-06-09 20:09:03 -0700

    Revert "Cache protected password entry and password on focus ping separately."
    This reverts commit 999a40e458bbcfbfad9bb4498c0e2799261a9ee3.
    
    TBR=raymes@chromium.org, isherman@chromium.org
    
    Bug: 727942
    Change-Id: Ie7afca85a33b2167d56b42dfcc919b656a7b3615
    Reviewed-on: https://chromium-review.googlesource.com/530487
    Commit-Queue: Jialiu Lin <jialiul@chromium.org>
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478509}

--- chrome/browser/safe_browsing/chrome_password_protection_service_unittest.cc
+++ chrome/browser/safe_browsing/chrome_password_protection_service_unittest.cc
@@ -76,7 +76,6 @@ class MockChromePasswordProtectionService
   }
 
   void CacheVerdict(const GURL& url,
-                    LoginReputationClientRequest::TriggerType trigger_type,
                     LoginReputationClientResponse* verdict,
                     const base::Time& receive_time) override {}
 
