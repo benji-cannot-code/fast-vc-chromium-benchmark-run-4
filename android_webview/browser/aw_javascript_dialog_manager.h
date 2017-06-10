58f007e7d996cc49d994aa4f325a80ace83aec42
fanjinsong <fanjinsong@sogou-inc.com>
2017-06-10 16:42:21 -0700

    Fix wrong explicit marked for zero-parameter constructor
    Remove unnecessary explicit for zero-parameter constructor in
    AwContentBrowserClient and AwJavaScriptDialogManager.
    
    BUG=
    TEST=
    R=sgurun@chromium.org
    
    Review-Url: https://codereview.chromium.org/2935533002
    Cr-Commit-Position: refs/heads/master@{#478523}

--- android_webview/browser/aw_javascript_dialog_manager.h
+++ android_webview/browser/aw_javascript_dialog_manager.h
@@ -13,7 +13,7 @@ namespace android_webview {
 
 class AwJavaScriptDialogManager : public content::JavaScriptDialogManager {
  public:
-  explicit AwJavaScriptDialogManager();
+  AwJavaScriptDialogManager();
   ~AwJavaScriptDialogManager() override;
 
   // Overridden from content::JavaScriptDialogManager:
