075e021dc689629fd69ac11fb56334a257f3ef47
Philip Rogers <pdr@chromium.org>
2017-06-11 10:01:10 -0700

    Remove unused ScrollbarLayerInterface functions
    This patch removes orientation() and scroll_element_id() from
    ScrollbarLayerInterface as these are not used. The corresponding
    getters/setters on ScrollbarLayerImplBase are needed but the Layer
    interface is only used to pass these values along.
    
    BUG: 723263
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I4631dc469b1f6776f3e6d4246bed39d94316822f
    Reviewed-on: https://chromium-review.googlesource.com/530490
    Reviewed-by: Chris harrelson <chrishtr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478818}

--- cc/layers/painted_overlay_scrollbar_layer.cc
+++ cc/layers/painted_overlay_scrollbar_layer.cc
@@ -50,10 +50,6 @@ PaintedOverlayScrollbarLayer::PaintedOverlayScrollbarLayer(
 
 PaintedOverlayScrollbarLayer::~PaintedOverlayScrollbarLayer() {}
 
-ElementId PaintedOverlayScrollbarLayer::scroll_element_id() const {
-  return scroll_element_id_;
-}
-
 void PaintedOverlayScrollbarLayer::SetScrollElementId(ElementId element_id) {
   if (element_id == scroll_element_id_)
     return;
@@ -66,10 +62,6 @@ bool PaintedOverlayScrollbarLayer::OpacityCanAnimateOnImplThread() const {
   return scrollbar_->IsOverlay();
 }
 
-ScrollbarOrientation PaintedOverlayScrollbarLayer::orientation() const {
-  return scrollbar_->Orientation();
-}
-
 void PaintedOverlayScrollbarLayer::PushPropertiesTo(LayerImpl* layer) {
   Layer::PushPropertiesTo(layer);
 
@@ -80,7 +72,7 @@ void PaintedOverlayScrollbarLayer::PushPropertiesTo(LayerImpl* layer) {
 
   scrollbar_layer->SetThumbThickness(thumb_thickness_);
   scrollbar_layer->SetThumbLength(thumb_length_);
-  if (orientation() == HORIZONTAL) {
+  if (scrollbar_->Orientation() == HORIZONTAL) {
     scrollbar_layer->SetTrackStart(track_rect_.x() - location_.x());
     scrollbar_layer->SetTrackLength(track_rect_.width());
   } else {
--- cc/layers/painted_overlay_scrollbar_layer.h
+++ cc/layers/painted_overlay_scrollbar_layer.h
@@ -29,9 +29,7 @@ class CC_EXPORT PaintedOverlayScrollbarLayer : public ScrollbarLayerInterface,
   ScrollbarLayerInterface* ToScrollbarLayer() override;
 
   // ScrollbarLayerInterface
-  ElementId scroll_element_id() const override;
   void SetScrollElementId(ElementId element_id) override;
-  ScrollbarOrientation orientation() const override;
 
   // Layer interface
   bool Update() override;
