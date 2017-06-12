a683c608eecb9d846c23b55029ae1dccd39029ae
jbauman <jbauman@chromium.org>
2017-06-12 15:36:51 -0700

    Split DirectCompositionSurfaceWin into two parts.
    DirectCompositionSurfaceWin remains as the host of the DCLayerTree,
    while DirectCompositionChildSurfaceWin handles drawing to the
    IDCompositionSurface itself. This will allow us to have more Surfaces
    in the layer tree in the future.
    
    BUG=726957
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2926763002
    Cr-Commit-Position: refs/heads/master@{#478794}

--- ui/gl/scoped_make_current.h
+++ ui/gl/scoped_make_current.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
+#include "base/optional.h"
 #include "ui/gl/gl_export.h"
 
 namespace gl {
@@ -32,6 +33,20 @@ class GL_EXPORT ScopedMakeCurrent {
   bool succeeded_;
 
   DISALLOW_COPY_AND_ASSIGN(ScopedMakeCurrent);
+};
+
+// This class is used to make sure a specified surface isn't current, and upon
+// destruction it will make the surface current again if it had been before.
+class GL_EXPORT ScopedReleaseCurrent {
+ public:
+  explicit ScopedReleaseCurrent(gl::GLSurface* this_surface);
+
+  ~ScopedReleaseCurrent();
+
+ private:
+  base::Optional<ScopedMakeCurrent> make_current_;
+
+  DISALLOW_COPY_AND_ASSIGN(ScopedReleaseCurrent);
 };
 
 }  // namespace ui
