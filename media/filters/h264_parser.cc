7f7c2a445a7d74b5d7b7583390be45981978d803
Pawel Osciak <posciak@chromium.org>
2017-06-12 18:49:27 +0900

    H264Parser: reset stream state after returning kEOStream.
    Reset stream_ and bytes_left_ to avoid reusing them in any subsequent
    AdvanceToNextNALU() calls if the client fails to SetStream() beforehand.
    
    BUG=632169
    TEST=vdatest,H264ParserTest
    
    Change-Id: I5ff611989c28f0f5f1c470189b3c2dd442e1d294
    Reviewed-on: https://chromium-review.googlesource.com/530908
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Pawel Osciak <posciak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478842}

--- media/filters/h264_parser.cc
+++ media/filters/h264_parser.cc
@@ -479,6 +479,8 @@ H264Parser::Result H264Parser::AdvanceToNextNALU(H264NALU* nalu) {
   if (!LocateNALU(&nalu_size_with_start_code, &start_code_size)) {
     DVLOG(4) << "Could not find next NALU, bytes left in stream: "
              << bytes_left_;
+    stream_ = nullptr;
+    bytes_left_ = 0;
     return kEOStream;
   }
 
@@ -487,8 +489,11 @@ H264Parser::Result H264Parser::AdvanceToNextNALU(H264NALU* nalu) {
   DVLOG(4) << "NALU found: size=" << nalu_size_with_start_code;
 
   // Initialize bit reader at the start of found NALU.
-  if (!br_.Initialize(nalu->data, nalu->size))
+  if (!br_.Initialize(nalu->data, nalu->size)) {
+    stream_ = nullptr;
+    bytes_left_ = 0;
     return kEOStream;
+  }
 
   // Move parser state to after this NALU, so next time AdvanceToNextNALU
   // is called, we will effectively be skipping it;
