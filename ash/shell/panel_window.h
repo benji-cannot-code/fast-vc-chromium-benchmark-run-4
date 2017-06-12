31059daca99ffade1dcc1ef3df3fcfce7d2b2a21
Thiago Farina <tfarina@chromium.org>
2017-06-12 15:44:52 -0300

    ui/aura: list aura_export.h in the sources list of aura target
    Header files should be listed in the sources list, so gn analyze
    can work.
    
    https://docs.google.com/spreadsheets/d/15az3FMl-jAS0mx4E9XVSBVHVpmEzo-9EAGY0ywe7bZs/edit#gid=0
    
    https://groups.google.com/a/chromium.org/d/topic/chromium-dev/1kK45mVemBc/discussion
    
    BUG=661774
    TBR=jamescook@chromium.org
    
    Change-Id: I68020668044858be898ce6d18392ee8610cf22f4
    Reviewed-on: https://chromium-review.googlesource.com/531030
    Commit-Queue: Thiago Farina <tfarina@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478819}

--- ash/shell/panel_window.h
+++ ash/shell/panel_window.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_SHELL_PANEL_WINDOW_H_
 
 #include "base/macros.h"
-#include "ui/aura/aura_export.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/widget/widget_delegate.h"
 
