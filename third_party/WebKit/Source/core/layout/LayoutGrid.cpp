64fc36f3c84f2390b4130de77740cce950d9b111
Javier Fernandez <jfernandez@igalia.com>
2017-06-12 16:33:44 +0200

    [css-grid] Control only style changes that imply shrinking or stretching
    We are monitoring grid container's style change to detect cases where
    some grid items may be shrunk or stretched. In those cases we are
    marking the item for layout, which implies a considerable impact on
    performance.
    
    Current code assumes, wrongly, that if grid container's old style had
    'stretch' as value for its align-items property it may affect some of
    its children's Self Alignment properties. This assumption led to an
    unnecessary overhead of evaluating every child's align-self property
    to detect stretching or shrinking behaviors.
    
    It's obvious that if grid container's Default Alignment change didn't
    imply stretching or shrinking, we don't need to evaluate its children
    at all.
    
    
    Bug: 729236
    Change-Id: I0b21f09b1d1c16e06374c2d20d150fd2829711f0
    Reviewed-on: https://chromium-review.googlesource.com/524043
    Commit-Queue: Javier Fernandez <jfernandez@igalia.com>
    Reviewed-by: Sergio Villar <svillar@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#478722}

--- third_party/WebKit/Source/core/layout/LayoutGrid.cpp
+++ third_party/WebKit/Source/core/layout/LayoutGrid.cpp
@@ -106,6 +106,14 @@ StyleSelfAlignmentData LayoutGrid::SelfAlignmentForChild(
                               : AlignSelfForChild(child, style);
 }
 
+StyleSelfAlignmentData LayoutGrid::DefaultAlignmentForChild(
+    GridAxis axis,
+    const ComputedStyle& style) const {
+  return axis == kGridRowAxis
+             ? style.ResolvedJustifyItems(SelfAlignmentNormalBehavior(this))
+             : style.ResolvedAlignItems(SelfAlignmentNormalBehavior(this));
+}
+
 bool LayoutGrid::SelfAlignmentChangedToStretch(GridAxis axis,
                                                const ComputedStyle& old_style,
                                                const ComputedStyle& new_style,
@@ -126,6 +134,26 @@ bool LayoutGrid::SelfAlignmentChangedFromStretch(GridAxis axis,
              kItemPositionStretch;
 }
 
+bool LayoutGrid::DefaultAlignmentChangedFromStretch(
+    GridAxis axis,
+    const ComputedStyle& old_style,
+    const ComputedStyle& new_style) const {
+  return DefaultAlignmentForChild(axis, old_style).GetPosition() ==
+             kItemPositionStretch &&
+         DefaultAlignmentForChild(axis, new_style).GetPosition() !=
+             kItemPositionStretch;
+}
+
+bool LayoutGrid::DefaultAlignmentChangedToStretch(
+    GridAxis axis,
+    const ComputedStyle& old_style,
+    const ComputedStyle& new_style) const {
+  return DefaultAlignmentForChild(axis, old_style).GetPosition() !=
+             kItemPositionStretch &&
+         DefaultAlignmentForChild(axis, new_style).GetPosition() ==
+             kItemPositionStretch;
+}
+
 void LayoutGrid::StyleDidChange(StyleDifference diff,
                                 const ComputedStyle* old_style) {
   LayoutBlock::StyleDidChange(diff, old_style);
@@ -133,10 +161,14 @@ void LayoutGrid::StyleDidChange(StyleDifference diff,
     return;
 
   const ComputedStyle& new_style = StyleRef();
-  if (old_style &&
-      old_style->ResolvedAlignItems(SelfAlignmentNormalBehavior(this))
-              .GetPosition() == kItemPositionStretch &&
-      diff.NeedsFullLayout()) {
+  if (diff.NeedsFullLayout() &&
+      (DefaultAlignmentChangedToStretch(kGridRowAxis, *old_style, new_style) ||
+       DefaultAlignmentChangedToStretch(kGridColumnAxis, *old_style,
+                                        new_style) ||
+       DefaultAlignmentChangedFromStretch(kGridRowAxis, *old_style,
+                                          new_style) ||
+       DefaultAlignmentChangedFromStretch(kGridColumnAxis, *old_style,
+                                          new_style))) {
     // Style changes on the grid container implying stretching (to-stretch) or
     // shrinking (from-stretch) require the affected items to be laid out again.
     // These logic only applies to 'stretch' since the rest of the alignment
--- third_party/WebKit/Source/core/layout/LayoutGrid.h
+++ third_party/WebKit/Source/core/layout/LayoutGrid.h
@@ -137,6 +137,12 @@ class LayoutGrid final : public LayoutBlock {
                                        const ComputedStyle& old_style,
                                        const ComputedStyle& new_style,
                                        const LayoutBox&) const;
+  bool DefaultAlignmentChangedToStretch(GridAxis,
+                                        const ComputedStyle& old_style,
+                                        const ComputedStyle& new_style) const;
+  bool DefaultAlignmentChangedFromStretch(GridAxis,
+                                          const ComputedStyle& old_style,
+                                          const ComputedStyle& new_style) const;
   void StyleDidChange(StyleDifference, const ComputedStyle*) override;
 
   Optional<LayoutUnit> AvailableSpaceForGutters(GridTrackSizingDirection) const;
@@ -231,6 +237,8 @@ class LayoutGrid final : public LayoutBlock {
       GridAxis,
       const LayoutBox& child,
       const ComputedStyle* = nullptr) const;
+  StyleSelfAlignmentData DefaultAlignmentForChild(GridAxis,
+                                                  const ComputedStyle&) const;
   void ApplyStretchAlignmentToChildIfNeeded(LayoutBox&);
   bool HasAutoSizeInColumnAxis(const LayoutBox& child) const {
     return IsHorizontalWritingMode() ? child.StyleRef().Height().IsAuto()
