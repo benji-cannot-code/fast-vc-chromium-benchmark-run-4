a32c11d4157147ae33ec2591b4d54d4914869a5e
Theresa Wellington <twellington@google.com>
2017-06-12 09:28:40 -0700

    [Home] Finish showing the NTP immediately if bottom sheet is opened
    When toggling accessibility mode, if the overview mode is showing we
    hide it and then open a new tab (see
    ChromeTabbedActivity#onAccessibilityModeChanged()). In Chrome Home, if
    the NTP UI is showing then the overview mode is technically showing.
    
    Prior to this patch, when ChromeTabbedActivity tried to open a new tab, we
    would "show" the bottom sheet new tab UI but since the bottom sheet was
    already opened we never finished showing the new UI (which currently
    consists of showing the regular toolbar). This patch updates the logic in
    BottomSheetNewTabController to finishing showing the NTP UI immediately
    if the bottom sheet is already open when
    BottomSheetNewTabController#displayNewTabUi() is called.
    
    BUG=731147
    
    Change-Id: Ief2d8c738f2c96f34f293ea29e41d8a0ab69b275
    Reviewed-on: https://chromium-review.googlesource.com/530077
    Commit-Queue: Theresa <twellington@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478670}

--- chrome/android/javatests/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheetNewTabControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheetNewTabControllerTest.java
@@ -76,11 +76,12 @@ public class BottomSheetNewTabControllerTest {
         mActivity.getTabModelSelector().getModel(false).addObserver(mTabModelObserver);
         mActivity.getTabModelSelector().getModel(true).addObserver(mTabModelObserver);
         mFadingBackgroundView = mActivity.getFadingBackgroundView();
+        mBottomSheetTestRule.setSheetState(BottomSheet.SHEET_STATE_PEEK, false);
     }
 
     @Test
     @SmallTest
-    public void testNTPOverTabSwitcher_Normal() throws InterruptedException, TimeoutException {
+    public void testNTPOverTabSwitcher_Normal() {
         assertEquals(1, mActivity.getTabModelSelector().getTotalTabCount());
         assertFalse("Overview mode should not be showing",
                 mActivity.getLayoutManager().overviewVisible());
@@ -115,7 +116,7 @@ public class BottomSheetNewTabControllerTest {
 
     @Test
     @SmallTest
-    public void testNTPOverTabSwitcher_Incognito() throws InterruptedException, TimeoutException {
+    public void testNTPOverTabSwitcher_Incognito() {
         assertEquals(1, mActivity.getTabModelSelector().getTotalTabCount());
         assertFalse("Overview mode should not be showing",
                 mActivity.getLayoutManager().overviewVisible());
@@ -158,7 +159,7 @@ public class BottomSheetNewTabControllerTest {
 
     @Test
     @SmallTest
-    public void testNTPOverTabSwitcher_NoTabs() throws InterruptedException, TimeoutException {
+    public void testNTPOverTabSwitcher_NoTabs() throws InterruptedException {
         // Close all tabs.
         ChromeTabUtils.closeAllTabs(InstrumentationRegistry.getInstrumentation(), mActivity);
         assertEquals(0, mActivity.getTabModelSelector().getTotalTabCount());
@@ -209,7 +210,7 @@ public class BottomSheetNewTabControllerTest {
         InstrumentationRegistry.getInstrumentation().waitForIdleSync();
     }
 
-    private void createNewBlankTab(final boolean incognito) throws InterruptedException {
+    private void createNewBlankTab(final boolean incognito) {
         MenuUtils.invokeCustomMenuActionSync(InstrumentationRegistry.getInstrumentation(),
                 mActivity, incognito ? R.id.new_incognito_tab_menu_id : R.id.new_tab_menu_id);
 
@@ -251,7 +252,7 @@ public class BottomSheetNewTabControllerTest {
 
         if (chromeHomeTabPageSelected) {
             assertFalse(mFadingBackgroundView.isEnabled());
-            assertEquals(0f, mFadingBackgroundView.getAlpha());
+            assertEquals(0f, mFadingBackgroundView.getAlpha(), 0);
         } else {
             assertTrue(mFadingBackgroundView.isEnabled());
         }
