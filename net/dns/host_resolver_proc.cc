46c1cfeb261274003a7a9fadc703c5ce1922367a
palmer <palmer@chromium.org>
2017-06-12 14:05:30 -0700

    Track how often we successfully resolve non-'compliant' domain names.
    Add a counter to see how often we successfully resolve hostnames, tracking
    whether or not |net::IsValidHostLabelCharacter| returns true for all characters
    in all labels in the name.
    
    BUG=695474
    
    Review-Url: https://codereview.chromium.org/2921553002
    Cr-Commit-Position: refs/heads/master@{#478761}

--- net/dns/host_resolver_proc.cc
+++ net/dns/host_resolver_proc.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 
 #include "base/logging.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/sys_byteorder.h"
 #include "net/base/address_list.h"
 #include "net/base/net_errors.h"
@@ -126,10 +127,14 @@ int SystemHostResolverCall(const std::string& host,
                            HostResolverFlags host_resolver_flags,
                            AddressList* addrlist,
                            int* os_error) {
-  // Make sure |host| is properly formed.
+  // Make sure |host| is properly formed. Save the validity check result so that
+  // we can count how often invalid names (see |net::IsValidHostLabelCharacter|)
+  // successfully resolve, if ever. TODO(crbug.com/695474): Remove this when we
+  // no longer need the UMA metrics.
+  bool valid_hostname = false;
   {
     std::string out_ignored;
-    if (!DNSDomainFromDot(host, &out_ignored))
+    if (!DNSDomainFromDotWithValidityCheck(host, &out_ignored, &valid_hostname))
       return ERR_NAME_NOT_RESOLVED;
   }
 
@@ -254,6 +259,8 @@ int SystemHostResolverCall(const std::string& host,
 
   *addrlist = AddressList::CreateFromAddrinfo(ai);
   freeaddrinfo(ai);
+  UMA_HISTOGRAM_BOOLEAN("Net.SuccessfulResolutionWithValidDNSName",
+                        valid_hostname);
   return OK;
 }
 
