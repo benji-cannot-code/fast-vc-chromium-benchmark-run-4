404e2be8ce58146bed19b3f4bda8919a63c0110a
dgozman <dgozman@chromium.org>
2017-06-12 14:39:05 -0700

    [DevTools] Rework RFDTAH navigation tracking with browser side navigation
    This patch introduces a separate code path for browser side navigation. Instead of
    using pending and current, we keep track of a single RenderFrameHostImpl instance via
    standard WebContentsObserver notifications.
    
    While any navigations are active (determined by the navigation_handles_ set), we put messages
    to suspended_messages_ instead of sending them to the renderer, resuming once navigations
    commit to a specific RenderFrameHostImpl instance.
    
    While navigation is not active, we send messages to renderer and put them to
    waiting_for_response_messages_, which are exactly the messages received after last
    state cookie update (see DevToolsMessageChunkProcessor).
    
    BUG=727517
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2927983004
    Cr-Commit-Position: refs/heads/master@{#478774}

--- content/browser/devtools/devtools_manager_unittest.cc
+++ content/browser/devtools/devtools_manager_unittest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/devtools_external_agent_proxy.h"
 #include "content/public/browser/devtools_external_agent_proxy_delegate.h"
 #include "content/public/browser/web_contents_delegate.h"
+#include "content/public/common/browser_side_navigation_policy.h"
 #include "content/public/test/test_utils.h"
 #include "content/test/test_content_browser_client.h"
 #include "content/test/test_render_view_host.h"
@@ -174,6 +175,9 @@ TEST_F(DevToolsManagerTest, NoUnresponsiveDialogInInspectedContents) {
 }
 
 TEST_F(DevToolsManagerTest, ReattachOnCancelPendingNavigation) {
+  // This test triggers incorrect notifications with PlzNavigate.
+  if (IsBrowserSideNavigationEnabled())
+    return;
   // Navigate to URL.  First URL should use first RenderViewHost.
   const GURL url("http://www.google.com");
   controller().LoadURL(
