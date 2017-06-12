dceb2c0ccb04ce2a137972a53903e526755ba740
tommycli <tommycli@chromium.org>
2017-06-12 15:53:40 -0700

    Omnibox UI: Create right-margin on Cocoa dropdown
    BUG=729197
    TBR=groby@chromium.org
    
    Review-Url: https://codereview.chromium.org/2927193002
    Cr-Commit-Position: refs/heads/master@{#478801}

--- chrome/browser/ui/cocoa/omnibox/omnibox_popup_cell.h
+++ chrome/browser/ui/cocoa/omnibox/omnibox_popup_cell.h
@@ -67,7 +67,7 @@ class OmniboxPopupViewMac;
 
 + (NSAttributedString*)createSeparatorStringForDarkTheme:(BOOL)isDarkTheme;
 
-+ (CGFloat)getContentAreaWidth:(NSRect)cellFrame;
++ (CGFloat)getTextContentAreaWidth:(CGFloat)cellContentMaxWidth;
 
 + (CGFloat)getContentTextHeightForDoubleLine:(BOOL)isDoubleLine;
 
