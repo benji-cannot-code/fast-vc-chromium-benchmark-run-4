b3dd596f30da4881c64145fd7e1798e9457da720
horo <horo@chromium.org>
2017-06-12 09:30:21 -0700

    Move Google search related util methods to page_load_metrics_util
    BUG=
    
    Review-Url: https://codereview.chromium.org/2936543002
    Cr-Commit-Position: refs/heads/master@{#478654}

--- chrome/browser/page_load_metrics/observers/service_worker_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/service_worker_page_load_metrics_observer.cc
@@ -119,7 +119,7 @@ void ServiceWorkerPageLoadMetricsObserver::OnFirstContentfulPaintInPage(
   if (!IsServiceWorkerControlled(info)) {
     if (!WasStartedInForegroundOptionalEventInForeground(
             timing.paint_timing->first_contentful_paint, info) ||
-        !FromGWSPageLoadMetricsLogger::IsGoogleSearchResultUrl(info.url)) {
+        !page_load_metrics::IsGoogleSearchResultUrl(info.url)) {
       return;
     }
     PAGE_LOAD_HISTOGRAM(
@@ -154,7 +154,7 @@ void ServiceWorkerPageLoadMetricsObserver::OnFirstContentfulPaintInPage(
         internal::kHistogramServiceWorkerParseStartToFirstContentfulPaintInbox,
         timing.paint_timing->first_contentful_paint.value() -
             timing.parse_timing->parse_start.value());
-  } else if (FromGWSPageLoadMetricsLogger::IsGoogleSearchResultUrl(info.url)) {
+  } else if (page_load_metrics::IsGoogleSearchResultUrl(info.url)) {
     PAGE_LOAD_HISTOGRAM(
         internal::kHistogramServiceWorkerFirstContentfulPaintSearch,
         timing.paint_timing->first_contentful_paint.value());
@@ -174,7 +174,7 @@ void ServiceWorkerPageLoadMetricsObserver::
     return;
   }
   if (!IsServiceWorkerControlled(info)) {
-    if (!FromGWSPageLoadMetricsLogger::IsGoogleSearchResultUrl(info.url))
+    if (!page_load_metrics::IsGoogleSearchResultUrl(info.url))
       return;
     PAGE_LOAD_HISTOGRAM(
         internal::kHistogramNoServiceWorkerFirstMeaningfulPaintSearch,
@@ -201,7 +201,7 @@ void ServiceWorkerPageLoadMetricsObserver::
         internal::kHistogramServiceWorkerParseStartToFirstMeaningfulPaintInbox,
         timing.paint_timing->first_meaningful_paint.value() -
             timing.parse_timing->parse_start.value());
-  } else if (FromGWSPageLoadMetricsLogger::IsGoogleSearchResultUrl(info.url)) {
+  } else if (page_load_metrics::IsGoogleSearchResultUrl(info.url)) {
     PAGE_LOAD_HISTOGRAM(
         internal::kHistogramServiceWorkerFirstMeaningfulPaintSearch,
         timing.paint_timing->first_meaningful_paint.value());
@@ -220,7 +220,7 @@ void ServiceWorkerPageLoadMetricsObserver::OnDomContentLoadedEventStart(
     return;
   }
   if (!IsServiceWorkerControlled(info)) {
-    if (!FromGWSPageLoadMetricsLogger::IsGoogleSearchResultUrl(info.url))
+    if (!page_load_metrics::IsGoogleSearchResultUrl(info.url))
       return;
     PAGE_LOAD_HISTOGRAM(
         internal::kHistogramNoServiceWorkerDomContentLoadedSearch,
@@ -234,7 +234,7 @@ void ServiceWorkerPageLoadMetricsObserver::OnDomContentLoadedEventStart(
     PAGE_LOAD_HISTOGRAM(
         internal::kHistogramServiceWorkerDomContentLoadedInbox,
         timing.document_timing->dom_content_loaded_event_start.value());
-  } else if (FromGWSPageLoadMetricsLogger::IsGoogleSearchResultUrl(info.url)) {
+  } else if (page_load_metrics::IsGoogleSearchResultUrl(info.url)) {
     PAGE_LOAD_HISTOGRAM(
         internal::kHistogramServiceWorkerDomContentLoadedSearch,
         timing.document_timing->dom_content_loaded_event_start.value());
@@ -248,7 +248,7 @@ void ServiceWorkerPageLoadMetricsObserver::OnLoadEventStart(
           timing.document_timing->load_event_start, info))
     return;
   if (!IsServiceWorkerControlled(info)) {
-    if (!FromGWSPageLoadMetricsLogger::IsGoogleSearchResultUrl(info.url))
+    if (!page_load_metrics::IsGoogleSearchResultUrl(info.url))
       return;
     PAGE_LOAD_HISTOGRAM(internal::kHistogramNoServiceWorkerLoadSearch,
                         timing.document_timing->load_event_start.value());
@@ -259,7 +259,7 @@ void ServiceWorkerPageLoadMetricsObserver::OnLoadEventStart(
   if (IsInboxSite(info.url)) {
     PAGE_LOAD_HISTOGRAM(internal::kHistogramServiceWorkerLoadInbox,
                         timing.document_timing->load_event_start.value());
-  } else if (FromGWSPageLoadMetricsLogger::IsGoogleSearchResultUrl(info.url)) {
+  } else if (page_load_metrics::IsGoogleSearchResultUrl(info.url)) {
     PAGE_LOAD_HISTOGRAM(internal::kHistogramServiceWorkerLoadSearch,
                         timing.document_timing->load_event_start.value());
   }
