8540036fe07638cffef9f35c920e5a2addba8fa7
kojii <kojii@chromium.org>
2017-06-12 12:46:21 -0700

    [LayoutNG] Initial letter-spacing and word-spacing support
    This patch adds initial support for letter-spacing and word-spacing.
    
    ShapeResultSpacing is templatized to share the code with TextRun.
    
    This patch does not support ShapingLineBreaker yet. When it reshapes,
    we need to apply spacing to reshaped parts. I'll work on it in following
    patches.
    
    Justification in LayoutNG is not supported yet either. It will be
    following work too.
    
    BUG=636993
    
    Review-Url: https://codereview.chromium.org/2924263003
    Cr-Commit-Position: refs/heads/master@{#478724}

--- third_party/WebKit/Source/platform/fonts/shaping/ShapeResult.h
+++ third_party/WebKit/Source/platform/fonts/shaping/ShapeResult.h
@@ -47,6 +47,7 @@ struct hb_buffer_t;
 namespace blink {
 
 class Font;
+template <typename TextContainerType>
 class ShapeResultSpacing;
 class SimpleFontData;
 class TextRun;
@@ -91,7 +92,10 @@ class PLATFORM_EXPORT ShapeResult : public RefCounted<ShapeResult> {
     return LayoutUnit::FromFloatCeil(PositionForOffset(offset));
   }
 
-  PassRefPtr<ShapeResult> ApplySpacingToCopy(ShapeResultSpacing&,
+  void ApplySpacing(ShapeResultSpacing<StringView>&,
+                    const StringView&,
+                    TextDirection);
+  PassRefPtr<ShapeResult> ApplySpacingToCopy(ShapeResultSpacing<TextRun>&,
                                              const TextRun&) const;
 
   void CopyRange(unsigned start, unsigned end, ShapeResult*) const;
@@ -106,7 +110,10 @@ class PLATFORM_EXPORT ShapeResult : public RefCounted<ShapeResult> {
     return AdoptRef(new ShapeResult(other));
   }
 
-  void ApplySpacing(ShapeResultSpacing&, const TextRun&);
+  template <typename TextContainerType>
+  void ApplySpacing(ShapeResultSpacing<TextContainerType>&,
+                    const TextContainerType&,
+                    bool is_rtl);
   void InsertRun(std::unique_ptr<ShapeResult::RunInfo>,
                  unsigned start_glyph,
                  unsigned num_glyphs,
