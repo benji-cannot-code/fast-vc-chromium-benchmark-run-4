77e5af2410c100cfbb0e4c313adb5f5929f7f9b4
kristipark <kristipark@chromium.org>
2017-06-12 15:42:23 -0700

    [CacheStorage] [DevTools] Added "Time Received" column to cache storage data grid
    Screenshot of the change in the bug.
    
    Since the entry response time is given as an internal UTC format, I added wrapper methods to Time.h to convert it to standard UTC format.
    
    BUG=727922
    
    Review-Url: https://codereview.chromium.org/2928833002
    Cr-Commit-Position: refs/heads/master@{#478797}

--- third_party/WebKit/Source/modules/cachestorage/InspectorCacheStorageAgent.cpp
+++ third_party/WebKit/Source/modules/cachestorage/InspectorCacheStorageAgent.cpp
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/wtf/PtrUtil.h"
 #include "platform/wtf/RefCounted.h"
 #include "platform/wtf/RefPtr.h"
+#include "platform/wtf/Time.h"
 #include "platform/wtf/Vector.h"
 #include "platform/wtf/text/StringBuilder.h"
 #include "public/platform/Platform.h"
@@ -166,6 +167,7 @@ struct RequestResponse {
       : request(request), response(response) {}
   String request;
   String response;
+  double response_time;
 };
 
 class ResponsesAccumulator : public RefCounted<ResponsesAccumulator> {
@@ -187,6 +189,8 @@ class ResponsesAccumulator : public RefCounted<ResponsesAccumulator> {
         responses_.at(responses_.size() - num_responses_left_);
     request_response.request = request.Url().GetString();
     request_response.response = response.StatusText();
+    request_response.response_time =
+        WTF::Time::FromInternalValue(response.ResponseTime()).ToDoubleT();
 
     if (--num_responses_left_ != 0)
       return;
@@ -209,6 +213,7 @@ class ResponsesAccumulator : public RefCounted<ResponsesAccumulator> {
           DataEntry::create()
               .setRequest(request_response.request)
               .setResponse(request_response.response)
+              .setResponseTime(request_response.response_time)
               .build();
       array->addItem(std::move(entry));
     }
