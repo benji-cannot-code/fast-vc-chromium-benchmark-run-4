a90d19d79a68d65b45fc0cb2129b38c55047db53
lod <lod@chromium.org>
2017-06-12 06:09:46 -0700

    Add a showcase entry for the content widget.
    BUG=622743
    
    Review-Url: https://codereview.chromium.org/2924013005
    Cr-Commit-Position: refs/heads/master@{#478599}

--- ios/showcase/core/showcase_model.mm
+++ ios/showcase/core/showcase_model.mm
@@ -87,6 +87,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       showcase::kClassForInstantiationKey : @"SCSearchWidgetCoordinator",
       showcase::kUseCaseKey : @"Search Widget",
     },
+    @{
+      showcase::kClassForDisplayKey : @"ContentWidgetViewController",
+      showcase::kClassForInstantiationKey : @"SCContentWidgetCoordinator",
+      showcase::kUseCaseKey : @"Content Widget",
+    },
   ];
 }
 
