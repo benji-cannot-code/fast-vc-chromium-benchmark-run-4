b7733b6cefa4c3abcfd9705a19565b6330517091
dpapad <dpapad@chromium.org>
2017-06-12 11:49:26 -0700

    MD Settings, cleanup: Remove obsolete references to paper-dialog, paper-item.
    BUG=None
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2932913003
    Cr-Commit-Position: refs/heads/master@{#478699}

--- chrome/browser/resources/settings/languages_page/edit_dictionary_page.js
+++ chrome/browser/resources/settings/languages_page/edit_dictionary_page.js
@@ -112,7 +112,7 @@ Polymer({
   },
 
   /**
-   * Handles tapping on a paper-item's Remove Word icon button.
+   * Handles tapping on a "Remove word" icon button.
    * @param {!{model: !{item: string}}} e
    */
   onRemoveWordTap_: function(e) {
