4af68edd673cb3ba70fa5bdb0c7134032d9241d7
lod <lod@chromium.org>
2017-06-12 03:14:54 -0700

    Add the new content widget including localised title.
    This is inspired by (aka basically copied from) the search widget. The
    few bits of existing implementation are those that could be reused from
    the search widget (the app opening code). In order to add unit tests I
    had to move resources from being a dependency of the code to being a
    dependency of the full appex (see build.gn files) as both widgets are
    in the same unit test target and caused resource file name collisions.
    
    BUG=622743
    
    Review-Url: https://codereview.chromium.org/2927803002
    Cr-Commit-Position: refs/heads/master@{#478577}

---
+++ ios/chrome/content_widget_extension/content_widget_view_controller.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_CONTENT_WIDGET_EXTENSION_CONTENT_WIDGET_VIEW_CONTROLLER_H_
+#define IOS_CHROME_CONTENT_WIDGET_EXTENSION_CONTENT_WIDGET_VIEW_CONTROLLER_H_
+
+#import <NotificationCenter/NotificationCenter.h>
+#import <UIKit/UIKit.h>
+
+@interface ContentWidgetViewController : UIViewController<NCWidgetProviding>
+
+@end
+
+#endif  // IOS_CHROME_CONTENT_WIDGET_EXTENSION_CONTENT_WIDGET_VIEW_CONTROLLER_H_
