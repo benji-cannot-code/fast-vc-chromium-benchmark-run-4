e123cb7bed1c2fb2f785f87ae96752e58e8765b4
vabr <vabr@chromium.org>
2017-06-12 04:03:09 -0700

    Don't move raw pointers in HTMLCollection.h
    Moving raw pointers does the same job as copying, but is more verbose and also
    more confusing: e.g., is the supposed pointer meant to be a smart one?
    
    This instance was flagged by the tool from
    https://codereview.chromium.org/2919243002/.
    
    BUG=729393
    
    Review-Url: https://codereview.chromium.org/2927233003
    Cr-Commit-Position: refs/heads/master@{#478589}

--- third_party/WebKit/Source/core/html/HTMLCollection.h
+++ third_party/WebKit/Source/core/html/HTMLCollection.h
@@ -181,7 +181,7 @@ class CORE_EXPORT HTMLCollection
     // Do not repeat registration for the same invalidation type.
     if (InvalidationType() != kInvalidateOnIdNameAttrChange)
       GetDocument().RegisterNodeListWithIdNameCache(this);
-    named_item_cache_ = std::move(cache);
+    named_item_cache_ = cache;
   }
 
   NamedItemCache& GetNamedItemCache() const {
