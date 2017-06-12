828ed6c385a9051ee14c5f7c27a7b92b72bef7b4
Mohamed Amir Yosef <mamir@chromium.org>
2017-06-12 15:34:39 +0200

    [NTP::Push] Extract JSON-to-Snippet in Helper
    Bug: 732161
    Change-Id: I57dd0dd8029fd83fabdab063040fc5163059afc9
    Reviewed-on: https://chromium-review.googlesource.com/530371
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: vitaliii <vitaliii@chromium.org>
    Reviewed-by: Chris Pickel <sfiera@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478626}

--- components/ntp_snippets/remote/remote_suggestions_provider_impl.h
+++ components/ntp_snippets/remote/remote_suggestions_provider_impl.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/ntp_snippets/category_status.h"
 #include "components/ntp_snippets/content_suggestion.h"
 #include "components/ntp_snippets/content_suggestions_provider.h"
+#include "components/ntp_snippets/remote/json_to_categories.h"
 #include "components/ntp_snippets/remote/remote_suggestion.h"
 #include "components/ntp_snippets/remote/remote_suggestions_fetcher.h"
 #include "components/ntp_snippets/remote/remote_suggestions_provider.h"
