0a9e0c386c1ae0c8b3e941e8a4df7bbf1d257c71
Luke Zielinski <lpz@chromium.org>
2017-06-12 10:54:03 -0400

    Make TriggerManager own ThreatDetails objects.
    TriggerManager will now store ThreatDetails that it creates in a map keyed by the WebContents that 
    data collection is happening on. In the future this map will be multi-level, so other types of 
    data collectors can run on a given tab.
    
    Also added unit tests for trigger manager
    
    BUG=730646
    
    Change-Id: Iec6ef3c59fcd7b838f929fc7fe43e99c3c745cc7
    Reviewed-on: https://chromium-review.googlesource.com/525854
    Commit-Queue: Luke Z <lpz@chromium.org>
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Reviewed-by: Paweł Hajdan Jr. <phajdan.jr@chromium.org>
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478649}

--- components/safe_browsing/triggers/trigger_manager.cc
+++ components/safe_browsing/triggers/trigger_manager.cc
@@ -7,7 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/safe_browsing/base_ui_manager.h"
 #include "components/safe_browsing/browser/threat_details.h"
-#include "content/public/browser/web_contents.h"
+#include "components/security_interstitials/content/unsafe_resource.h"
+#include "content/public/browser/browser_thread.h"
 #include "net/url_request/url_request_context_getter.h"
 
 namespace safe_browsing {
@@ -17,14 +18,48 @@ TriggerManager::TriggerManager(BaseUIManager* ui_manager)
 
 TriggerManager::~TriggerManager() {}
 
-ThreatDetails* TriggerManager::StartCollectingThreatDetails(
+bool TriggerManager::StartCollectingThreatDetails(
     content::WebContents* web_contents,
     const security_interstitials::UnsafeResource& resource,
     net::URLRequestContextGetter* request_context_getter,
     history::HistoryService* history_service) {
-  return ThreatDetails::NewThreatDetails(ui_manager_, web_contents, resource,
-                                         request_context_getter,
-                                         history_service);
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+
+  // Ensure we're not already collecting data on this tab.
+  if (base::ContainsKey(data_collectors_map_, web_contents))
+    return false;
+
+  data_collectors_map_[web_contents] = scoped_refptr<ThreatDetails>(
+      ThreatDetails::NewThreatDetails(ui_manager_, web_contents, resource,
+                                      request_context_getter, history_service));
+  return true;
+}
+
+bool TriggerManager::FinishCollectingThreatDetails(
+    content::WebContents* web_contents,
+    const base::TimeDelta& delay,
+    bool did_proceed,
+    int num_visits) {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+
+  // Make sure there's a data collector running on this tab.
+  if (!base::ContainsKey(data_collectors_map_, web_contents))
+    return false;
+
+  // Find the data collector and tell it to finish collecting data, and then
+  // remove it from our map. We release ownership of the data collector here but
+  // it will live until the end of the FinishCollection call because it
+  // implements RefCountedThreadSafe.
+  scoped_refptr<ThreatDetails> threat_details =
+      data_collectors_map_[web_contents];
+  content::BrowserThread::PostDelayedTask(
+      content::BrowserThread::IO, FROM_HERE,
+      base::BindOnce(&ThreatDetails::FinishCollection, threat_details,
+                     did_proceed, num_visits),
+      delay);
+  data_collectors_map_.erase(web_contents);
+
+  return true;
 }
 
 }  // namespace safe_browsing
