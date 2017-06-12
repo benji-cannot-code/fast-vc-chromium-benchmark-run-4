9b0a54a4c4b1a4d00eab934e31d981c9c892709d
rdevlin.cronin <rdevlin.cronin@chromium.org>
2017-06-12 14:54:11 -0700

    Revert of Move ObjectContentType entirely to HTMLPlugInElement (patchset #2 id:20001 of https://codereview.chromium.org/2927703003/ )
    Reason for revert:
    Looks like schenney's unavailable at the moment; speculatively reverting for layout test virtual/new-remote-playback-pipeline/media/controls/controls-cast-overlay-slow-fade.html failures (e.g. https://build.chromium.org/p/chromium.linux/builders/Linux%20Tests/builds/57955)
    
    Original issue's description:
    > Move ObjectContentType entirely to HTMLPlugInElement
    >
    > There's no need to go to LocalFrameClientImpl to determine how to load
    > a plugin element, that was a purely pre-blink necessity.
    >
    > Simplify a bunch of logic around HTMLPlugInElement to assume that url_
    > and service_type_ are always set and correct by the time the
    > ObjectContentType is being determined.
    >
    > BUG=
    >
    > Review-Url: https://codereview.chromium.org/2927703003
    > Cr-Commit-Position: refs/heads/master@{#478683}
    > Committed: https://chromium.googlesource.com/chromium/src/+/2718186cc8ac74b38b9a7304764642f05c76bb81
    
    TBR=schenney@chromium.org,japhet@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=
    
    Review-Url: https://codereview.chromium.org/2933213002
    Cr-Commit-Position: refs/heads/master@{#478781}

--- third_party/WebKit/Source/core/loader/FrameLoaderTypes.h
+++ third_party/WebKit/Source/core/loader/FrameLoaderTypes.h
@@ -52,6 +52,13 @@ enum NavigationType {
   kNavigationTypeOther
 };
 
+enum ObjectContentType {
+  kObjectContentNone,
+  kObjectContentImage,
+  kObjectContentFrame,
+  kObjectContentNetscapePlugin,
+};
+
 enum ShouldSendReferrer { kMaybeSendReferrer, kNeverSendReferrer };
 
 enum ShouldSetOpener { kMaybeSetOpener, kNeverSetOpener };
