aa4bca07ca6d3ed625aad596f8997d9bf5556e3e
updowndota <updowndota@chromium.org>
2017-06-12 14:09:49 -0700

    Add value proposition entrypoint from Voice Interaction shortcut
    - Voice interaction keyboard shortcut should bring up voice interaction
      value prop screen if voice interaction is disabled.
    - If playstore is already set up, start voice interaction setup wizard.
    - Otherwise show playstore terms, and start voice interaction setup
      wizard afterwards if user accept.
    
    BUG=b/38263948
    
    Review-Url: https://codereview.chromium.org/2929963002
    Cr-Commit-Position: refs/heads/master@{#478763}

--- chrome/browser/chromeos/login/wizard_controller.cc
+++ chrome/browser/chromeos/login/wizard_controller.cc
@@ -96,6 +96,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/pairing/shark_connection_listener.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
+#include "components/session_manager/core/session_manager.h"
 #include "components/user_manager/user_manager.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/notification_types.h"
@@ -256,6 +257,9 @@ WizardController::WizardController(LoginDisplayHost* host, OobeUI* oobe_ui)
   DCHECK(default_controller_ == nullptr);
   default_controller_ = this;
   screen_manager_ = base::MakeUnique<ScreenManager>(this);
+  // In session OOBE was initiated from voice interaction keyboard shortcuts.
+  is_in_session_oobe_ =
+      session_manager::SessionManager::Get()->IsSessionStarted();
   if (!ash_util::IsRunningInMash()) {
     AccessibilityManager* accessibility_manager = AccessibilityManager::Get();
     CHECK(accessibility_manager);
@@ -829,6 +833,13 @@ void WizardController::OnTermsOfServiceAccepted() {
 }
 
 void WizardController::OnArcTermsOfServiceFinished() {
+  const Profile* profile = ProfileManager::GetActiveUserProfile();
+  if (is_in_session_oobe_) {
+    if (profile->GetPrefs()->GetBoolean(prefs::kArcTermsAccepted))
+      StartVoiceInteractionSetupWizard();
+    OnOobeFlowFinished();
+    return;
+  }
   // If the user finished with the PlayStore Terms of Service, advance to the
   // user image screen.
   ShowUserImageScreen();
@@ -839,12 +850,12 @@ void WizardController::OnVoiceInteractionValuePropSkipped() {
 }
 
 void WizardController::OnVoiceInteractionValuePropAccepted() {
-  // Start voice interaction setup wizard in container
-  arc::ArcVoiceInteractionFrameworkService* service =
-      arc::ArcServiceManager::Get()
-          ->GetService<arc::ArcVoiceInteractionFrameworkService>();
-  if (service)
-    service->StartVoiceInteractionSetupWizard();
+  const Profile* profile = ProfileManager::GetActiveUserProfile();
+  if (is_in_session_oobe_ && !arc::IsArcPlayStoreEnabledForProfile(profile)) {
+    ShowArcTermsOfServiceScreen();
+    return;
+  }
+  StartVoiceInteractionSetupWizard();
   OnOobeFlowFinished();
 }
 
@@ -863,6 +874,13 @@ void WizardController::OnAutoEnrollmentCheckCompleted() {
 }
 
 void WizardController::OnOobeFlowFinished() {
+  if (is_in_session_oobe_) {
+    host_->SetStatusAreaVisible(true);
+    host_->Finalize(base::OnceClosure());
+    host_ = nullptr;
+    return;
+  }
+
   if (!time_oobe_started_.is_null()) {
     base::TimeDelta delta = base::Time::Now() - time_oobe_started_;
     UMA_HISTOGRAM_CUSTOM_TIMES("OOBE.BootToSignInCompleted", delta,
@@ -1545,8 +1563,10 @@ bool WizardController::ShouldShowArcTerms() const {
 }
 
 bool WizardController::ShouldShowVoiceInteractionValueProp() const {
-  if (!arc::IsArcPlayStoreEnabledForProfile(
-          ProfileManager::GetActiveUserProfile())) {
+  // If the OOBE flow was initiated from voice interaction shortcut, we will
+  // show Arc terms later.
+  if (!is_in_session_oobe_ && !arc::IsArcPlayStoreEnabledForProfile(
+                                  ProfileManager::GetActiveUserProfile())) {
     VLOG(1) << "Skip Voice Interaction Value Prop screen because Arc Terms is "
             << "skipped.";
     return false;
@@ -1557,6 +1577,14 @@ bool WizardController::ShouldShowVoiceInteractionValueProp() const {
     return false;
   }
   return true;
+}
+
+void WizardController::StartVoiceInteractionSetupWizard() {
+  arc::ArcVoiceInteractionFrameworkService* service =
+      arc::ArcServiceManager::Get()
+          ->GetService<arc::ArcVoiceInteractionFrameworkService>();
+  if (service)
+    service->StartVoiceInteractionSetupWizard();
 }
 
 void WizardController::MaybeStartListeningForSharkConnection() {
