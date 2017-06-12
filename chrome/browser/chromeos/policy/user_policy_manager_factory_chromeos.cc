3056251a2cd853b53bdd89ed3810b0328d3a4ccf
tnagel <tnagel@chromium.org>
2017-06-12 06:06:24 -0700

    UserPolicyManagerFactoryChromeOS: Elaborate comment
    BUG=722374
    
    Review-Url: https://codereview.chromium.org/2891933002
    Cr-Commit-Position: refs/heads/master@{#478598}

--- chrome/browser/chromeos/policy/user_policy_manager_factory_chromeos.cc
+++ chrome/browser/chromeos/policy/user_policy_manager_factory_chromeos.cc
@@ -179,7 +179,7 @@ UserPolicyManagerFactoryChromeOS::CreateManagerForProfile(
     case AccountType::UNKNOWN:
     case AccountType::GOOGLE:
       // TODO(tnagel): Return nullptr for unknown accounts once AccountId
-      // migration is finished.
+      // migration is finished.  (KioskAppManager still needs to be migrated.)
       if (!user->HasGaiaAccount()) {
         return {};
       }
