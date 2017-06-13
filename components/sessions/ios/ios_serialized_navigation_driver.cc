063bd6b232f11bf58f77e3be0e563a77ce7c56ef
zea <zea@chromium.org>
2017-06-13 12:04:26 -0700

    [Sync] Ensure referrer is recorded regardless of policy conversion
    Previously recording the referrer was gated on logic checking if the referrer
    policy could be mapped to an old value. Whether the policy maps or not should
    not affect whether the referrer is set or not, just whether the obsolete
    value is set or not.
    
    BUG=729159
    
    Review-Url: https://codereview.chromium.org/2921883003
    Cr-Commit-Position: refs/heads/master@{#479088}

--- components/sessions/ios/ios_serialized_navigation_driver.cc
+++ components/sessions/ios/ios_serialized_navigation_driver.cc
@@ -11,13 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace sessions {
 
-namespace {
-const int kObsoleteReferrerPolicyAlways = 0;
-const int kObsoleteReferrerPolicyDefault = 1;
-const int kObsoleteReferrerPolicyNever = 2;
-const int kObsoleteReferrerPolicyOrigin = 3;
-}  // namespace
-
 // static
 SerializedNavigationDriver* SerializedNavigationDriver::Get() {
   return IOSSerializedNavigationDriver::GetInstance();
@@ -39,45 +32,6 @@ IOSSerializedNavigationDriver::~IOSSerializedNavigationDriver() {
 
 int IOSSerializedNavigationDriver::GetDefaultReferrerPolicy() const {
   return web::ReferrerPolicyDefault;
-}
-
-bool IOSSerializedNavigationDriver::MapReferrerPolicyToOldValues(
-    int referrer_policy,
-    int* mapped_referrer_policy) const {
-  switch (referrer_policy) {
-    case web::ReferrerPolicyAlways:
-    case web::ReferrerPolicyDefault:
-      // "always" and "default" are the same value in all versions.
-      *mapped_referrer_policy = referrer_policy;
-      return true;
-
-    case web::ReferrerPolicyOrigin:
-      // "origin" exists in the old encoding.
-      *mapped_referrer_policy = kObsoleteReferrerPolicyOrigin;
-      return true;
-
-    default:
-      // Everything else is mapped to never.
-      *mapped_referrer_policy = kObsoleteReferrerPolicyNever;
-      return false;
-  }
-}
-
-bool IOSSerializedNavigationDriver::MapReferrerPolicyToNewValues(
-    int referrer_policy,
-    int* mapped_referrer_policy) const {
-  switch (referrer_policy) {
-    case kObsoleteReferrerPolicyAlways:
-    case kObsoleteReferrerPolicyDefault:
-      // "always" and "default" are the same value in all versions.
-      *mapped_referrer_policy = referrer_policy;
-      return true;
-
-    default:
-      // Since we don't know what encoding was used, we map the rest to "never".
-      *mapped_referrer_policy = web::ReferrerPolicyNever;
-      return false;
-  }
 }
 
 std::string
