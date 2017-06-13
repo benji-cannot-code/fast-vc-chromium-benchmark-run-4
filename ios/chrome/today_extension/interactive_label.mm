038bf1e5e14c8e43c05b878dfaba74c3ff327f26
marq <marq@chromium.org>
2017-06-13 01:17:16 -0700

    [ObjC ARC] Converts ios/chrome/today_extension:today_extension to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2938433002
    Cr-Commit-Position: refs/heads/master@{#478931}

--- ios/chrome/today_extension/interactive_label.mm
+++ ios/chrome/today_extension/interactive_label.mm
@@ -35,8 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   base::scoped_nsobject<NSString> _labelString;
   base::scoped_nsobject<NSString> _buttonString;
   base::scoped_nsobject<UITextView> _label;
-  base::mac::ScopedBlock<ProceduralBlock> _linkBlock;
-  base::mac::ScopedBlock<ProceduralBlock> _buttonBlock;
+  ProceduralBlock _linkBlock;
+  ProceduralBlock _buttonBlock;
   base::scoped_nsobject<TransparentButton> _activationButton;
   NSRange _buttonRange;
   base::scoped_nsobject<NSMutableAttributedString> _attributedText;
@@ -68,7 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     NSString* prefixedString =
         [NSString stringWithFormat:@"\u200B%@", labelString];
     _labelString.reset([prefixedString copy]);
-    _linkBlock.reset(linkBlock, base::scoped_policy::RETAIN);
+    _linkBlock = [linkBlock copy];
 
     _label.reset([[NoSelectionUITextView alloc] initWithFrame:CGRectZero]);
     [_label setTranslatesAutoresizingMaskIntoConstraints:NO];
@@ -100,7 +100,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     BOOL showButton = buttonString && buttonBlock;
     if (showButton) {
       _buttonString.reset([buttonString copy]);
-      _buttonBlock.reset(buttonBlock, base::scoped_policy::RETAIN);
+      _buttonBlock = [buttonBlock copy];
       _activationButton.reset(
           [[TransparentButton alloc] initWithFrame:CGRectZero]);
       [_activationButton addTarget:self
@@ -113,14 +113,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       [_activationButton setBorderColor:ui_util::BorderColor()];
       [self addSubview:_activationButton];
       [self bringSubviewToFront:_activationButton];
-      _buttonTopConstraint.reset([[[_activationButton topAnchor]
-          constraintEqualToAnchor:[self topAnchor]] retain]);
-      _buttonLeftConstraint.reset([[[_activationButton leftAnchor]
-          constraintEqualToAnchor:[self leftAnchor]] retain]);
-      _buttonWidthConstraint.reset([[[_activationButton widthAnchor]
-          constraintEqualToConstant:0] retain]);
-      _buttonHeightConstraint.reset([[[_activationButton heightAnchor]
-          constraintEqualToConstant:0] retain]);
+      _buttonTopConstraint.reset([[_activationButton topAnchor]
+          constraintEqualToAnchor:[self topAnchor]]);
+      _buttonLeftConstraint.reset([[_activationButton leftAnchor]
+          constraintEqualToAnchor:[self leftAnchor]]);
+      _buttonWidthConstraint.reset(
+          [[_activationButton widthAnchor] constraintEqualToConstant:0]);
+      _buttonHeightConstraint.reset(
+          [[_activationButton heightAnchor] constraintEqualToConstant:0]);
       [NSLayoutConstraint activateConstraints:@[
         _buttonTopConstraint, _buttonLeftConstraint, _buttonWidthConstraint,
         _buttonHeightConstraint
@@ -234,12 +234,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (BOOL)textView:(UITextView*)textView
     shouldInteractWithURL:(NSURL*)URL
                   inRange:(NSRange)characterRange {
-  _linkBlock.get()();
+  if (_linkBlock)
+    _linkBlock();
   return NO;
 }
 
 - (void)buttonPressed:(id)sender {
-  _buttonBlock.get()();
+  if (_buttonBlock)
+    _buttonBlock();
 }
 
 - (CGSize)intrinsicContentSize {
