344cf08466ab56be7322459ae689c0bdf575c59d
ltian <ltian@chromium.org>
2017-06-12 20:11:13 -0700

    [Android] Wrap all share parameters into the ShareParams class
    Functions in ShareHelper take too many parameters for sharing. Wrap them
    into a new ShareParams class to redunce the number of parameters passed
    to the functions.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2917703004
    Cr-Commit-Position: refs/heads/master@{#478878}

--- chrome/android/java/src/org/chromium/chrome/browser/webshare/ShareServiceImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webshare/ShareServiceImpl.java
@@ -11,6 +11,7 @@ import android.support.annotation.Nullable;
 
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.browser.share.ShareHelper;
+import org.chromium.chrome.browser.share.ShareParams;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.mojo.system.MojoException;
 import org.chromium.ui.base.WindowAndroid;
@@ -77,7 +78,11 @@ public class ShareServiceImpl implements ShareService {
             }
         };
 
-        ShareHelper.share(false, false, mActivity, title, text, url.url, null, null, innerCallback);
+        ShareParams params = new ShareParams.Builder(mActivity, title, url.url)
+                                     .setText(text)
+                                     .setCallback(innerCallback)
+                                     .build();
+        ShareHelper.share(params);
     }
 
     @Nullable
