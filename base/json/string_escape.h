26abc8c3e7fddaad42afb615f57fd0cdbcadc937
sabbakumov <sabbakumov@yandex-team.ru>
2017-06-12 21:28:13 -0700

    Escape invalid code points
    Make base::EscapeJSONString() to escape strings like "\xF3\xBF\xBF\xBF"
    (containing invalid Unicode code points) to be escaped as "\xEF\xBF\xBD".
    
    BUG=725778
    
    Review-Url: https://codereview.chromium.org/2903773003
    Cr-Commit-Position: refs/heads/master@{#478900}

--- base/json/string_escape.h
+++ base/json/string_escape.h
@@ -15,11 +15,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-// Appends to |dest| an escaped version of |str|. Valid UTF-8 code units will
-// pass through from the input to the output. Invalid code units will be
-// replaced with the U+FFFD replacement character. This function returns true
-// if no replacement was necessary and false if there was a lossy replacement.
-// On return, |dest| will contain a valid UTF-8 JSON string.
+// Appends to |dest| an escaped version of |str|. Valid UTF-8 code units and
+// characters will pass through from the input to the output. Invalid code
+// units and characters will be replaced with the U+FFFD replacement character.
+// This function returns true if no replacement was necessary and false if
+// there was a lossy replacement. On return, |dest| will contain a valid UTF-8
+// JSON string.
 //
 // Non-printing control characters will be escaped as \uXXXX sequences for
 // readability.
