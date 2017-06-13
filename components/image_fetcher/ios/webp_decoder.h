53ecda6b1f2e96925eae635c239aab1605f1679b
James Zern <jzern@google.com>
2017-06-12 20:07:13 -0700

    libwebp: move sources to src/
    this more closely matches upstream, adds a bit of separation between
    chromium code and will make the addition of examples/ and imageio/ a
    bit simpler.
    
    BUG=732565
    
    Change-Id: I06cb8285c04bada5fbbecf30f2fa410321c3e8aa
    Reviewed-on: https://chromium-review.googlesource.com/532573
    Commit-Queue: Urvang Joshi <urvang@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Urvang Joshi <urvang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479060}

--- components/image_fetcher/ios/webp_decoder.h
+++ components/image_fetcher/ios/webp_decoder.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/mac/scoped_nsobject.h"
 #include "base/memory/ref_counted.h"
-#include "third_party/libwebp/webp/decode.h"
+#include "third_party/libwebp/src/webp/decode.h"
 
 @class NSData;
 
