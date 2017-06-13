d127a40041ce4d02cc04d2e9c9a9005e66ac6164
marq <marq@chromium.org>
2017-06-13 06:14:31 -0700

    [ObjC ARC] Converts components/signin/ios/browser:unit_tests to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2930273003
    Cr-Commit-Position: refs/heads/master@{#478992}

--- components/signin/ios/browser/profile_oauth2_token_service_ios_delegate_unittest.mm
+++ components/signin/ios/browser/profile_oauth2_token_service_ios_delegate_unittest.mm
@@ -21,6 +21,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/test_url_fetcher_factory.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 typedef ProfileOAuth2TokenServiceIOSProvider::AccountInfo ProviderAccount;
 
 class ProfileOAuth2TokenServiceIOSDelegateTest
