93910e1d044b0b0c81d74bd5b38bf9a73ff528f1
suzyh <suzyh@chromium.org>
2017-06-13 00:29:35 -0700

    Add comments for Interpolation and subclasses.
    This patch adds class-level documentation for the Interpolation class
    and its three subclasses, InvalidatableInterpolation,
    LegacyStyleInterpolation and TransitionInterpolation.
    
    Review-Url: https://codereview.chromium.org/2885363002
    Cr-Commit-Position: refs/heads/master@{#478923}

--- third_party/WebKit/Source/core/animation/InvalidatableInterpolation.h
+++ third_party/WebKit/Source/core/animation/InvalidatableInterpolation.h
@@ -15,9 +15,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-// TODO(alancutter): This class will replace *StyleInterpolation and
-// Interpolation. For now it needs to distinguish itself during the refactor and
-// temporarily has an ugly name.
+// See the documentation of Interpolation for general information about this
+// class hierarchy.
+//
+// The InvalidatableInterpolation subclass stores the start and end keyframes as
+// PropertySpecificKeyframe objects.
+//
+// InvalidatableInterpolation uses conversion checkers and the interpolation
+// environment to respond to changes to the underlying property value during
+// interpolation.
+//
+// InvalidatableInterpolation is used to implement additive animations. During
+// the effect application phase of animation computation, the current animated
+// value of the property is applied to the element by calling the static
+// ApplyStack function with an ordered list of InvalidatableInterpolation
+// objects.
 class CORE_EXPORT InvalidatableInterpolation : public Interpolation {
  public:
   static PassRefPtr<InvalidatableInterpolation> Create(
