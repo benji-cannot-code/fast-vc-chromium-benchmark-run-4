47cd091b89239a9562cd8c11d1f5a72372ab3a81
mmenke <mmenke@chromium.org>
2017-06-13 12:51:56 -0700

    Make URLRequestContextBuilder use url's scheme constants.
    It had been using inlined strings.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2936903002
    Cr-Commit-Position: refs/heads/master@{#479109}

--- net/url_request/url_request_context_builder.cc
+++ net/url_request/url_request_context_builder.cc
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_interceptor.h"
 #include "net/url_request/url_request_job_factory_impl.h"
 #include "net/url_request/url_request_throttler_manager.h"
+#include "url/url_constants.h"
 
 #if !BUILDFLAG(DISABLE_FILE_SUPPORT)
 #include "net/url_request/file_protocol_handler.h"  // nogncheck
@@ -480,13 +481,13 @@ std::unique_ptr<URLRequestContext> URLRequestContextBuilder::Build() {
   protocol_handlers_.clear();
 
   if (data_enabled_)
-    job_factory->SetProtocolHandler("data",
+    job_factory->SetProtocolHandler(url::kDataScheme,
                                     base::WrapUnique(new DataProtocolHandler));
 
 #if !BUILDFLAG(DISABLE_FILE_SUPPORT)
   if (file_enabled_) {
     job_factory->SetProtocolHandler(
-        "file",
+        url::kFileScheme,
         base::MakeUnique<FileProtocolHandler>(context->GetFileTaskRunner()));
   }
 #endif  // !BUILDFLAG(DISABLE_FILE_SUPPORT)
@@ -494,7 +495,7 @@ std::unique_ptr<URLRequestContext> URLRequestContextBuilder::Build() {
 #if !BUILDFLAG(DISABLE_FTP_SUPPORT)
   if (ftp_enabled_) {
     job_factory->SetProtocolHandler(
-        "ftp", FtpProtocolHandler::Create(context->host_resolver()));
+        url::kFtpScheme, FtpProtocolHandler::Create(context->host_resolver()));
   }
 #endif  // !BUILDFLAG(DISABLE_FTP_SUPPORT)
 
