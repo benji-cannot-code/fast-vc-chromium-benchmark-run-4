a91eb3aabbcb57d860e31b10f967cb99df4e1d32
Pavel Feldman <pfeldman@chromium.org>
2017-06-13 12:15:51 -0700

    LayoutTests, devtools: allow creating and testing OOPIFs.
    Bug: 706169
    Change-Id: Ie2ae166d6f57ab4f8aacf154bd183a4d2d029683
    Reviewed-on: https://chromium-review.googlesource.com/533897
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479157}

--- content/shell/browser/layout_test/layout_test_content_browser_client.cc
+++ content/shell/browser/layout_test/layout_test_content_browser_client.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ptr_util.h"
 #include "base/single_thread_task_runner.h"
+#include "base/strings/pattern.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_process_host.h"
@@ -148,6 +149,16 @@ void LayoutTestContentBrowserClient::GetQuotaSettings(
   // The 1GB limit is intended to give a large headroom to tests that need to
   // build up a large data set and issue many concurrent reads or writes.
   callback.Run(storage::GetHardCodedSettings(1024 * 1024 * 1024));
+}
+
+bool LayoutTestContentBrowserClient::DoesSiteRequireDedicatedProcess(
+    BrowserContext* browser_context,
+    const GURL& effective_site_url) {
+  if (ShellContentBrowserClient::DoesSiteRequireDedicatedProcess(
+          browser_context, effective_site_url))
+    return true;
+  url::Origin origin(effective_site_url);
+  return base::MatchPattern(origin.Serialize(), "*oopif.test");
 }
 
 PlatformNotificationService*
