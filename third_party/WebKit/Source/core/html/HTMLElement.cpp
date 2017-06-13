43737c2b193cb007a5d0ccac8a1173da51de8264
Kent Tamura <tkent@chromium.org>
2017-06-13 17:45:40 +0900

    Remove leftover of the contextmenu feature.
    This CL removes:
    
    * onshow global event handler
    * blink::WebMenuItemInfo::icon
    
    They should have been removed in crrev.com/476230.
    
    onshow was accidentally shipped in Stable channel. This CL removes it without
    intent-to-remove.  The removal should be safe because it worked only with
    Experimental Web Platform features flag.
    
    Bug: 87553
    Change-Id: I94c48f2dd9db2c6c52e12f3419ba357211a6c88a
    Reviewed-on: https://chromium-review.googlesource.com/532497
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479292}

--- third_party/WebKit/Source/core/html/HTMLElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLElement.cpp
@@ -397,7 +397,6 @@ const AtomicString& HTMLElement::EventNameForAttributeName(
         {onseekingAttr, EventTypeNames::seeking},
         {onselectAttr, EventTypeNames::select},
         {onselectstartAttr, EventTypeNames::selectstart},
-        {onshowAttr, EventTypeNames::show},
         {onstalledAttr, EventTypeNames::stalled},
         {onsubmitAttr, EventTypeNames::submit},
         {onsuspendAttr, EventTypeNames::suspend},
--- third_party/WebKit/public/web/WebMenuItemInfo.h
+++ third_party/WebKit/public/web/WebMenuItemInfo.h
@@ -51,8 +51,6 @@ struct WebMenuItemInfo {
         checked(false) {}
 
   WebString label;
-  // "icon" provides absolute URL of an image that repesents the command.
-  WebString icon;
   WebString tool_tip;
   Type type;
   unsigned action;
