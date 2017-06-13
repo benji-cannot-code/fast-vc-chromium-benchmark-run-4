d744405e00e62785cb53ce2c2759df52382fb3cd
Mohamed Amir Yosef <mamir@chromium.org>
2017-06-13 10:50:28 +0200

    [NTP::Push] Parse pushed content suggestions
    Parse the JSON content suggestions pushed via GCM.
    
    Bug: 732161
    Change-Id: Ic12cd8a2687983dc5e698a5a93e20ad6b27b5c43
    Reviewed-on: https://chromium-review.googlesource.com/530305
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: Chris Pickel <sfiera@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478994}

--- components/ntp_snippets/breaking_news/breaking_news_suggestions_provider.cc
+++ components/ntp_snippets/breaking_news/breaking_news_suggestions_provider.cc
@@ -34,7 +34,7 @@ void BreakingNewsSuggestionsProvider::Start() {
 }
 
 void BreakingNewsSuggestionsProvider::OnNewContentSuggestion(
-    const base::Value& content) {}
+    std::unique_ptr<base::Value> content) {}
 
 ////////////////////////////////////////////////////////////////////////////////
 // Private methods
