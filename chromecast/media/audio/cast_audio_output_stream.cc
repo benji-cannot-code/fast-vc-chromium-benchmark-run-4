b3d16c599e94ece1c73d1e43af42dd2d991f5647
Alok Priyadarshi <alokp@chromium.org>
2017-06-12 17:17:37 -0700

    [chromecast] Do not push buffers after AudioDecoder reports error.
    Bug: internal b/62541375
    Change-Id: Idcf22b815a2a575846f4de6db079465e6ea53946
    Reviewed-on: https://chromium-review.googlesource.com/532056
    Reviewed-by: Stephen Lanham <slan@chromium.org>
    Commit-Queue: Alok Priyadarshi <alokp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478906}

--- chromecast/media/audio/cast_audio_output_stream.cc
+++ chromecast/media/audio/cast_audio_output_stream.cc
@@ -53,6 +53,7 @@ class CastAudioOutputStream::Backend
         buffer_duration_(audio_params.GetBufferDuration()),
         first_start_(true),
         push_in_progress_(false),
+        encountered_error_(false),
         decoder_(nullptr),
         source_callback_(nullptr),
         weak_factory_(this) {
@@ -151,7 +152,7 @@ class CastAudioOutputStream::Backend
     DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
     DCHECK(push_in_progress_);
 
-    if (!source_callback_) {
+    if (!source_callback_ || encountered_error_) {
       push_in_progress_ = false;
       return;
     }
@@ -188,7 +189,7 @@ class CastAudioOutputStream::Backend
     DCHECK(push_in_progress_);
     push_in_progress_ = false;
 
-    if (!source_callback_)
+    if (!source_callback_ || encountered_error_)
       return;
 
     if (status != MediaPipelineBackend::kBufferSuccess) {
@@ -216,6 +217,8 @@ class CastAudioOutputStream::Backend
   void OnDecoderError() override {
     VLOG(1) << this << ": " << __func__;
     DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+
+    encountered_error_ = true;
     if (source_callback_)
       source_callback_->OnError();
   }
@@ -233,6 +236,7 @@ class CastAudioOutputStream::Backend
   const base::TimeDelta buffer_duration_;
   bool first_start_;
   bool push_in_progress_;
+  bool encountered_error_;
   base::TimeTicks next_push_time_;
   std::unique_ptr<TaskRunnerImpl> backend_task_runner_;
   std::unique_ptr<MediaPipelineBackend> backend_;
