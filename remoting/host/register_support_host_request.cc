43cffba587e4d3cb097bd7b0d63cc820613cd508
kelvinp <kelvinp@chromium.org>
2017-06-12 23:04:21 -0700

    Report signling errors for register-support-host requests.
    Summary of changes:
    Report an error message if:
    1. SignalStrategy->SendStanza() fails.
    2. There are no responses from the Bot 10 seconds after the request is
       sent.
    
    BUG=729854
    
    Review-Url: https://codereview.chromium.org/2925733003
    Cr-Commit-Position: refs/heads/master@{#478912}

--- remoting/host/register_support_host_request.cc
+++ remoting/host/register_support_host_request.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/base/constants.h"
 #include "remoting/host/host_config.h"
 #include "remoting/host/host_details.h"
+#include "remoting/protocol/errors.h"
 #include "remoting/signaling/iq_sender.h"
 #include "remoting/signaling/jid_util.h"
 #include "remoting/signaling/signal_strategy.h"
@@ -43,6 +44,9 @@ const char kHostOsVersionTag[] = "host-os-version";
 const char kRegisterQueryResultTag[] = "register-support-host-result";
 const char kSupportIdTag[] = "support-id";
 const char kSupportIdLifetimeTag[] = "support-id-lifetime";
+
+// The signaling timeout for register support host requests.
+constexpr int kRegisterRequestTimeoutInSeconds = 10;
 }
 
 RegisterSupportHostRequest::RegisterSupportHostRequest(
@@ -69,12 +73,22 @@ void RegisterSupportHostRequest::OnSignalStrategyStateChange(
     SignalStrategy::State state) {
   if (state == SignalStrategy::CONNECTED) {
     DCHECK(!callback_.is_null());
-
     request_ = iq_sender_->SendIq(
         buzz::STR_SET, directory_bot_jid_,
         CreateRegistrationRequest(signal_strategy_->GetLocalAddress().jid()),
         base::Bind(&RegisterSupportHostRequest::ProcessResponse,
                    base::Unretained(this)));
+    if (!request_) {
+      std::string error_message =
+          "Error sending the register-support-host request.";
+      LOG(ERROR) << error_message;
+      CallCallback(std::string(), base::TimeDelta(), error_message);
+      return;
+    }
+
+    request_->SetTimeout(
+        base::TimeDelta::FromSeconds(kRegisterRequestTimeoutInSeconds));
+
   } else if (state == SignalStrategy::DISCONNECTED) {
     // We will reach here if signaling fails to connect.
     std::string error_message = "Signal strategy disconnected.";
@@ -144,11 +158,15 @@ void RegisterSupportHostRequest::ParseResponse(const XmlElement* response,
                                                std::string* error_message) {
   std::ostringstream error;
 
+  if (!response) {
+    *error_message = "register-support-host request timed out.";
+    return;
+  }
+
   std::string type = response->Attr(buzz::QN_TYPE);
   if (type == buzz::STR_ERROR) {
     error << "Received error in response to heartbeat: " << response->Str();
     *error_message = error.str();
-    LOG(ERROR) << *error_message;
     return;
   }
 
@@ -156,7 +174,6 @@ void RegisterSupportHostRequest::ParseResponse(const XmlElement* response,
   if (type != buzz::STR_RESULT) {
     error << "Received unexpect stanza of type \"" << type << "\"";
     *error_message = error.str();
-    LOG(ERROR) << *error_message;
     return;
   }
 
@@ -167,7 +184,6 @@ void RegisterSupportHostRequest::ParseResponse(const XmlElement* response,
           << "> is missing in the host registration response: "
           << response->Str();
     *error_message = error.str();
-    LOG(ERROR) << *error_message;
     return;
   }
 
@@ -178,7 +194,6 @@ void RegisterSupportHostRequest::ParseResponse(const XmlElement* response,
           << "> is missing in the host registration response: "
           << response->Str();
     *error_message = error.str();
-    LOG(ERROR) << *error_message;
     return;
   }
 
@@ -190,7 +205,6 @@ void RegisterSupportHostRequest::ParseResponse(const XmlElement* response,
           << "> is missing in the host registration response: "
           << response->Str();
     *error_message = error.str();
-    LOG(ERROR) << *error_message;
     return;
   }
 
@@ -201,7 +215,6 @@ void RegisterSupportHostRequest::ParseResponse(const XmlElement* response,
           << "> is malformed in the host registration response: "
           << response->Str();
     *error_message = error.str();
-    LOG(ERROR) << *error_message;
     return;
   }
 
@@ -216,6 +229,9 @@ void RegisterSupportHostRequest::ProcessResponse(IqRequest* request,
   base::TimeDelta lifetime;
   std::string error_message;
   ParseResponse(response, &support_id, &lifetime, &error_message);
+  if (!error_message.empty()) {
+    LOG(ERROR) << error_message;
+  }
   CallCallback(support_id, lifetime, error_message);
 }
 
--- remoting/host/register_support_host_request_unittest.cc
+++ remoting/host/register_support_host_request_unittest.cc
@@ -11,10 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/message_loop/message_loop.h"
 #include "base/observer_list.h"
-#include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/stringize_macros.h"
 #include "base/test/mock_callback.h"
+#include "base/test/scoped_mock_time_message_loop_task_runner.h"
 #include "remoting/base/constants.h"
 #include "remoting/base/rsa_key_pair.h"
 #include "remoting/base/test_rsa_key_pair.h"
@@ -35,6 +35,7 @@ using testing::Invoke;
 using testing::NotNull;
 using testing::Return;
 using testing::SaveArg;
+using testing::DeleteArg;
 
 namespace remoting {
 
@@ -72,11 +73,29 @@ class RegisterSupportHostRequestTest : public testing::Test {
   }
 
   base::MessageLoop message_loop_;
+  base::ScopedMockTimeMessageLoopTaskRunner mock_time_task_runner_;
   MockSignalStrategy signal_strategy_;
   base::ObserverList<SignalStrategy::Listener, true> signal_strategy_listeners_;
   scoped_refptr<RsaKeyPair> key_pair_;
   base::MockCallback<RegisterSupportHostRequest::RegisterCallback> callback_;
 };
+
+TEST_F(RegisterSupportHostRequestTest, Timeout) {
+  std::unique_ptr<RegisterSupportHostRequest> request(
+      new RegisterSupportHostRequest(&signal_strategy_, key_pair_, kTestBotJid,
+                                     callback_.Get()));
+  EXPECT_CALL(signal_strategy_, GetNextId()).WillOnce(Return(kStanzaId));
+  EXPECT_CALL(signal_strategy_, SendStanzaPtr(NotNull()))
+      .WillOnce(DoAll(DeleteArg<0>(), Return(true)));
+
+  request->OnSignalStrategyStateChange(SignalStrategy::CONNECTED);
+
+  // Generate response and verify that callback is called.
+  EXPECT_CALL(callback_, Run("", base::TimeDelta::FromSeconds(0),
+                             "register-support-host request timed out."));
+
+  mock_time_task_runner_->FastForwardBy(base::TimeDelta::FromSeconds(15));
+}
 
 TEST_F(RegisterSupportHostRequestTest, Send) {
   // |iq_request| is freed by RegisterSupportHostRequest.
@@ -93,7 +112,7 @@ TEST_F(RegisterSupportHostRequestTest, Send) {
       .WillOnce(DoAll(SaveArg<0>(&sent_iq), Return(true)));
 
   request->OnSignalStrategyStateChange(SignalStrategy::CONNECTED);
-  base::RunLoop().RunUntilIdle();
+  mock_time_task_runner_->RunUntilIdle();
 
   // Verify format of the query.
   std::unique_ptr<XmlElement> stanza(sent_iq);
@@ -168,7 +187,7 @@ TEST_F(RegisterSupportHostRequestTest, Send) {
   }
   EXPECT_EQ(1, consumed);
 
-  base::RunLoop().RunUntilIdle();
+  mock_time_task_runner_->RunUntilIdle();
 }
 
 }  // namespace remoting
