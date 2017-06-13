5ad90e6db7e390f7b367259c7f9300560eb9a345
Marc Treib <treib@chromium.org>
2017-06-13 10:03:00 +0200

    [Doodle] Cleanup: Remove a bunch of unused fields
    Bug: none
    Change-Id: Ib44c387a2c38f7adbe37175b80b9926b691c3e80
    Reviewed-on: https://chromium-review.googlesource.com/531046
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478945}

--- components/doodle/doodle_types.h
+++ components/doodle/doodle_types.h
@@ -35,7 +35,7 @@ enum class DoodleType {
   SLIDESHOW,
 };
 
-// Information about a Doodle image. By default the dimensions are 0.
+// Information about a Doodle image.
 struct DoodleImage {
   DoodleImage(const GURL& url);
   ~DoodleImage();
@@ -50,10 +50,6 @@ struct DoodleImage {
   bool operator!=(const DoodleImage& other) const;
 
   GURL url;
-  int height;
-  int width;
-  bool is_animated_gif;
-  bool is_cta;
 
   // Copying and assignment allowed.
 };
@@ -76,13 +72,11 @@ struct DoodleConfig {
 
   DoodleType doodle_type;
   std::string alt_text;
-  std::string interactive_html;
 
   GURL target_url;
 
   DoodleImage large_image;
   base::Optional<DoodleImage> large_cta_image;
-  base::Optional<DoodleImage> transparent_large_image;
 
   // Copying and assignment allowed.
 };
