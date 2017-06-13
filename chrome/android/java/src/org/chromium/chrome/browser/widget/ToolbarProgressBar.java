18103fa208f1840df4661a15f70428870b990f0a
Matthew Jones <mdjones@chromium.org>
2017-06-13 14:18:44 -0700

    Remove old progress bar logic
    This change removes the fast-start and linear progress bar animation
    logic as they are no longer needed.
    
    BUG=
    
    Change-Id: I332be5d99f1750ea7a40e2d616c6c1abf500fda2
    Reviewed-on: https://chromium-review.googlesource.com/534495
    Reviewed-by: Dan Alcantara <dfalcantara@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479826}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/ToolbarProgressBar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/ToolbarProgressBar.java
@@ -235,10 +235,6 @@ public class ToolbarProgressBar extends ClipDrawableProgressBar {
                 setForegroundColor(getForegroundColor());
             }
             UiUtils.insertAfter(mProgressBarContainer, mAnimatingView, this);
-        } else if (TextUtils.equals(animation, "fast-start")) {
-            mAnimationLogic = new ProgressAnimationFastStart();
-        } else if (TextUtils.equals(animation, "linear")) {
-            mAnimationLogic = new ProgressAnimationLinear();
         }
     }
 
