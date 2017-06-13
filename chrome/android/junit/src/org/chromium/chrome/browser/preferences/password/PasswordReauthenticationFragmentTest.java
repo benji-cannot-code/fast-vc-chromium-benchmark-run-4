519243ba499c58361d65d72c3b269b536f72734f
Gustav Sennton <gsennton@google.com>
2017-06-13 13:17:34 +0100

    Declare the non-use of manifest in PasswordReauthenticationFragmentTest
    Some bots are failing to run chrome_junit_tests because
    org.chromium.chrome.browser.preferences.password.PasswordReauthenticationFragmentTest
    doesn't declare that it's not using a manifest file.
    
    No-Presubmit: true
    Bug: 732763
    
    Change-Id: If0badfbcb68e46f241178d50b8a7f62d40d250bf
    Reviewed-on: https://chromium-review.googlesource.com/532976
    Commit-Queue: Gustav Sennton <gsennton@chromium.org>
    Reviewed-by: Gustav Sennton <gsennton@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478986}

--- chrome/android/junit/src/org/chromium/chrome/browser/preferences/password/PasswordReauthenticationFragmentTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/preferences/password/PasswordReauthenticationFragmentTest.java
@@ -16,6 +16,7 @@ import android.content.Intent;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
+import org.robolectric.annotation.Config;
 
 import org.chromium.testing.local.LocalRobolectricTestRunner;
 
@@ -23,6 +24,7 @@ import org.chromium.testing.local.LocalRobolectricTestRunner;
  * Tests for the "Save Passwords" settings screen.
  */
 @RunWith(LocalRobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
 public class PasswordReauthenticationFragmentTest {
     /**
      * Ensure that upon reauthentication PasswordReauthenticationFragment is popped from
