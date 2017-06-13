e96e5724cc0a44015044edb37a15740eb62c3cf2
tzik <tzik@chromium.org>
2017-06-12 20:16:20 -0700

    Revert of Moves mojo_js_integration_tests into blink. (patchset #1 id:1 of https://codereview.chromium.org/2920383004/ )
    Reason for revert:
    This CL seems to break webkit_unittests.
    The error log is:
    https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Mac10.10/builds/34695
    
    The tests below were timed out:
    JsToCppTest.BackPointer
    JsToCppTest.BitFlip
    JsToCppTest.Echo
    JsToCppTest.Ping
    
    Original issue's description:
    > Reland of Moves mojo_js_integration_tests into blink. (patchset #1 id:1 of https://codereview.chromium.org/2926143002/ )
    >
    > Reason for revert:
    > Fixed failing tests.
    >
    > Original issue's description:
    > > Revert of Moves mojo_js_integration_tests into blink. (patchset #11 id:190001 of https://codereview.chromium.org/2734783008/ )
    > >
    > > Reason for revert:
    > > Seems related to webkit failures of the JsToCpp tests. E.x. https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Mac10.10/builds/34530.
    > >
    > > Original issue's description:
    > > > Moves mojo_js_integration_tests into blink.
    > > >
    > > > BUG=699569
    > > >
    > > > Review-Url: https://codereview.chromium.org/2734783008
    > > > Cr-Commit-Position: refs/heads/master@{#477549}
    > > > Committed: https://chromium.googlesource.com/chromium/src/+/dd57fa764813de09d159a232eeef1bb21b0ccc1e
    > >
    > > TBR=jbroman@chromium.org,dpranke@chromium.org,yzshen@chromium.org,alokp@chromium.org
    > > # Skipping CQ checks because original CL landed less than 1 days ago.
    > > NOPRESUBMIT=true
    > > NOTREECHECKS=true
    > > NOTRY=true
    > > BUG=699569
    > >
    > > Review-Url: https://codereview.chromium.org/2926143002
    > > Cr-Commit-Position: refs/heads/master@{#477653}
    > > Committed: https://chromium.googlesource.com/chromium/src/+/6536a56d25c245bd109e32ed49240bfc04f8890f
    >
    > TBR=jbroman@chromium.org,dpranke@chromium.org,yzshen@chromium.org,jwd@chromium.org
    > # Skipping CQ checks because original CL landed less than 1 days ago.
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=699569
    >
    > Review-Url: https://codereview.chromium.org/2920383004
    > Cr-Commit-Position: refs/heads/master@{#478820}
    > Committed: https://chromium.googlesource.com/chromium/src/+/0fac10b6b0a132a25eedf971c8ccffaa6e8a4961
    
    TBR=jbroman@chromium.org,dpranke@chromium.org,yzshen@chromium.org,jwd@chromium.org,alokp@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=699569
    
    Review-Url: https://codereview.chromium.org/2939663002
    Cr-Commit-Position: refs/heads/master@{#478881}

--- mojo/edk/js/tests/js_to_cpp_tests.js
+++ mojo/edk/js/tests/js_to_cpp_tests.js
@@ -3,18 +3,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-(function () {
+define('mojo/edk/js/tests/js_to_cpp_tests', [
+  'console',
+  'mojo/edk/js/tests/js_to_cpp.mojom',
+  'mojo/public/js/bindings',
+  'mojo/public/js/connector',
+  'mojo/public/js/core',
+], function (console, jsToCpp, bindings, connector, core) {
   var retainedJsSide;
+  var retainedJsSideStub;
   var sampleData;
   var sampleMessage;
   var BAD_VALUE = 13;
   var DATA_PIPE_PARAMS = {
+    flags: core.CREATE_DATA_PIPE_OPTIONS_FLAG_NONE,
     elementNumBytes: 1,
     capacityNumBytes: 64
   };
 
   function JsSideConnection() {
-    this.binding = new mojo.Binding(jsToCpp.JsSide, this);
+    this.binding = new bindings.Binding(jsToCpp.JsSide, this);
   }
 
   JsSideConnection.prototype.setCppSide = function(cppSide) {
@@ -48,13 +56,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       arg.si8 = BAD_VALUE;
 
     for (i = 0; i < numIterations; ++i) {
-      dataPipe1 = Mojo.createDataPipe(DATA_PIPE_PARAMS);
-      dataPipe2 = Mojo.createDataPipe(DATA_PIPE_PARAMS);
-      messagePipe1 = Mojo.createMessagePipe();
-      messagePipe2 = Mojo.createMessagePipe();
+      dataPipe1 = core.createDataPipe(DATA_PIPE_PARAMS);
+      dataPipe2 = core.createDataPipe(DATA_PIPE_PARAMS);
+      messagePipe1 = core.createMessagePipe();
+      messagePipe2 = core.createMessagePipe();
 
-      arg.dataHandle = dataPipe1.consumer;
-      arg.messageHandle = messagePipe1.handle1;
+      arg.data_handle = dataPipe1.consumerHandle;
+      arg.message_handle = messagePipe1.handle1;
 
       specialArg = new jsToCpp.EchoArgs();
       specialArg.si64 = -1;
@@ -62,19 +70,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       specialArg.si16 = -1;
       specialArg.si8 = -1;
       specialArg.name = 'going';
-      specialArg.dataHandle = dataPipe2.consumer;
-      specialArg.messageHandle = messagePipe2.handle1;
+      specialArg.data_handle = dataPipe2.consumerHandle;
+      specialArg.message_handle = messagePipe2.handle1;
 
       writeDataPipe(dataPipe1, sampleData);
       writeDataPipe(dataPipe2, sampleData);
       writeMessagePipe(messagePipe1, sampleMessage);
       writeMessagePipe(messagePipe2, sampleMessage);
+
       this.cppSide_.echoResponse(createEchoArgsList(specialArg, arg));
 
-      dataPipe1.producer.close();
-      dataPipe2.producer.close();
-      messagePipe1.handle0.close();
-      messagePipe2.handle0.close();
+      core.close(dataPipe1.producerHandle);
+      core.close(dataPipe2.producerHandle);
+      core.close(messagePipe1.handle0);
+      core.close(messagePipe2.handle0);
     }
     this.cppSide_.testFinished();
   };
@@ -83,7 +92,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     var iteration = 0;
     var dataPipe;
     var messagePipe;
-    var proto = mojo.internal.Connector.prototype;
+    var proto = connector.Connector.prototype;
     var stopSignalled = false;
 
     proto.realAccept = proto.accept;
@@ -102,13 +111,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     };
 
     while (!stopSignalled) {
-      messagePipe = Mojo.createMessagePipe();
+      messagePipe = core.createMessagePipe();
       writeMessagePipe(messagePipe, sampleMessage);
-      arg.messageHandle = messagePipe.handle1;
+      arg.message_handle = messagePipe.handle1;
 
       this.cppSide_.bitFlipResponse(createEchoArgsList(arg), null);
 
-      messagePipe.handle0.close();
+      core.close(messagePipe.handle0);
       iteration += 1;
     }
 
@@ -121,7 +130,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     var iteration = 0;
     var dataPipe;
     var messagePipe;
-    var proto = mojo.internal.Connector.prototype;
+    var proto = connector.Connector.prototype;
     var stopSignalled = false;
 
     proto.realAccept = proto.accept;
@@ -138,13 +147,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     };
 
     while (!stopSignalled) {
-      messagePipe = Mojo.createMessagePipe();
+      messagePipe = core.createMessagePipe();
       writeMessagePipe(messagePipe, sampleMessage);
-      arg.messageHandle = messagePipe.handle1;
+      arg.message_handle = messagePipe.handle1;
 
       this.cppSide_.backPointerResponse(createEchoArgsList(arg));
 
-      messagePipe.handle0.close();
+      core.close(messagePipe.handle0);
       iteration += 1;
     }
 
@@ -154,9 +163,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   };
 
   function writeDataPipe(pipe, data) {
-    var writeResult = pipe.producer.writeData(data);
+    var writeResult = core.writeData(
+      pipe.producerHandle, data, core.WRITE_DATA_FLAG_ALL_OR_NONE);
 
-    if (writeResult.result != Mojo.RESULT_OK) {
+    if (writeResult.result != core.RESULT_OK) {
       console.log('ERROR: Data pipe write result was ' + writeResult.result);
       return false;
     }
@@ -168,8 +178,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   function writeMessagePipe(pipe, arrayBuffer) {
-    var result = pipe.handle0.writeMessage(arrayBuffer, []);
-    if (result != Mojo.RESULT_OK) {
+    var result = core.writeMessage(pipe.handle0, arrayBuffer, [], 0);
+    if (result != core.RESULT_OK) {
       console.log('ERROR: Message pipe write result was ' + result);
       return false;
     }
@@ -203,4 +213,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     retainedJsSide = new JsSideConnection;
     retainedJsSide.binding.bind(jsSideRequestHandle);
   };
-})();
+});
