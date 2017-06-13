6296cd84da1a781a2cac27e37db67c3acb262a02
Xiaohan Wang <xhwang@chromium.org>
2017-06-13 10:29:42 -0700

    media: Enable Widevine L1 on Clank behind a feature
    When AnroidOverlay is enabled and used by the media player, enable
    hardware secure codec support which will also enable L1 on Clank.
    
    Whether AndroidOverlay is enabled will be checked at run-time.
    Whether media player uses AndroidOverlay is current controlled by
    a feature and will be enabled by default in the future. To enable
    this feature for testing, use
    
    --enable-features=use-android_overlay
    
    Cast on Android still has kForceVideoOverlays command line switch and
    L1 will always be supported (even without AndroidOverlay support). In
    a future Cast on Android will also switch to the AndroidOverlay model.
    
    AndroidOverlay is not supported on WebView yet. This will be addressed
    separately in the future.
    
    BUG=459414
    
    Change-Id: I8b20f460f7c2835874a01da1b068125a2fab88ac
    Reviewed-on: https://chromium-review.googlesource.com/531738
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479285}

--- components/cdm/browser/cdm_message_filter_android.cc
+++ components/cdm/browser/cdm_message_filter_android.cc
@@ -10,12 +10,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/feature_list.h"
 #include "base/macros.h"
 #include "components/cdm/common/cdm_messages_android.h"
+#include "content/public/browser/android/android_overlay_provider.h"
+#include "content/public/browser/browser_thread.h"
 #include "ipc/ipc_message_macros.h"
 #include "media/base/android/media_codec_util.h"
 #include "media/base/android/media_drm_bridge.h"
 #include "media/base/audio_codecs.h"
+#include "media/base/media_switches.h"
 #include "media/base/video_codecs.h"
 #include "media/media_features.h"
 
@@ -92,7 +96,7 @@ static SupportedCodecs GetSupportedCodecs(
 
 CdmMessageFilterAndroid::CdmMessageFilterAndroid(bool can_use_secure_codecs)
     : BrowserMessageFilter(EncryptedMediaMsgStart),
-      can_use_secure_codecs_(can_use_secure_codecs) {}
+      force_to_support_secure_codecs_(can_use_secure_codecs) {}
 
 CdmMessageFilterAndroid::~CdmMessageFilterAndroid() {}
 
@@ -134,8 +138,16 @@ void CdmMessageFilterAndroid::OnQueryKeySystemSupport(
   DCHECK(request.codecs & media::EME_CODEC_ALL) << "unrecognized codec";
   response->key_system = request.key_system;
   response->non_secure_codecs = GetSupportedCodecs(request, false);
-  if (can_use_secure_codecs_)
+
+  bool are_overlay_supported =
+      content::AndroidOverlayProvider::GetInstance()->AreOverlaysSupported();
+  bool use_android_overlay =
+      base::FeatureList::IsEnabled(media::kUseAndroidOverlay);
+  if (force_to_support_secure_codecs_ ||
+      (are_overlay_supported && use_android_overlay)) {
+    DVLOG(1) << "Rendering the output of secure codecs is supported!";
     response->secure_codecs = GetSupportedCodecs(request, true);
+  }
 
   response->is_persistent_license_supported =
       MediaDrmBridge::IsPersistentLicenseTypeSupported(request.key_system);
