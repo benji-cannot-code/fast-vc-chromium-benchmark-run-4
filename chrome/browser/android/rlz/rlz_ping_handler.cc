d949996d88361de1decbbbd60554fe3386104400
yusufo <yusufo@chromium.org>
2017-06-13 00:59:24 -0700

    Add rep=1 to rlz ping from mobile
    This was a missed rlz param from the initial CL
    
    BUG=732614
    
    Review-Url: https://codereview.chromium.org/2937513003
    Cr-Commit-Position: refs/heads/master@{#478927}

--- chrome/browser/android/rlz/rlz_ping_handler.cc
+++ chrome/browser/android/rlz/rlz_ping_handler.cc
@@ -25,6 +25,10 @@ using base::android::JavaParamRef;
 
 constexpr int kMaxRetries = 10;
 
+namespace {
+const char kProtocolCgiVariable[] = "rep";
+}
+
 namespace chrome {
 namespace android {
 
@@ -64,6 +68,8 @@ void RlzPingHandler::Ping(
       "https://%s%s?", rlz_lib::kFinancialServer, rlz_lib::kFinancialPingPath));
   request_url = net::AppendQueryParameter(
       request_url, rlz_lib::kProductSignatureCgiVariable, "chrome");
+  request_url =
+      net::AppendQueryParameter(request_url, kProtocolCgiVariable, "1");
   request_url = net::AppendQueryParameter(
       request_url, rlz_lib::kProductBrandCgiVariable, brand);
   request_url = net::AppendQueryParameter(
