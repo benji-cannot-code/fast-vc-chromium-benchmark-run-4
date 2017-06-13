a5fb9a850b828fc509b849a493eaa423a28e2b0d
Kouhei Ueno <kouhei@chromium.org>
2017-06-13 09:10:53 +0900

    Do not restore ScrollPosition if the frame is being detached.
    This is a speculative crash fix for scroll position restore during
    LocalFrame::Detach.
    
    Bug: 715078
    Change-Id: I4b4f5038a9948b57624f38c9c7e05c210ea97e19
    Reviewed-on: https://chromium-review.googlesource.com/532393
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478889}

--- third_party/WebKit/Source/core/loader/FrameLoader.cpp
+++ third_party/WebKit/Source/core/loader/FrameLoader.cpp
@@ -1063,7 +1063,8 @@ void FrameLoader::RestoreScrollPositionAndViewStateForLoadType(
     FrameLoadType load_type) {
   LocalFrameView* view = frame_->View();
   if (!view || !view->LayoutViewportScrollableArea() ||
-      !state_machine_.CommittedFirstRealDocumentLoad()) {
+      !state_machine_.CommittedFirstRealDocumentLoad() ||
+      !frame_->IsAttached()) {
     return;
   }
   if (!NeedsHistoryItemRestore(load_type))
