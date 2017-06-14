39bff38ce1e5aaa6a247eb281187b8415618872e
Sasha Bermeister <sashab@chromium.org>
2017-06-14 14:18:11 +1000

    Merge PopupMenu with PopupMenuImpl
    The PopupMenu parent class is an artifact left over from when PopupMenu
    was in platform/ and PopupMenuImpl was in core/. Now that they are both
    in core/, there's no need to have these as two separate classes.
    
    Bug: 731490
    Change-Id: I673e4f18b67b03e3f04c29517219ddda500d2680
    Reviewed-on: https://chromium-review.googlesource.com/527721
    Commit-Queue: Sasha Morrissey <sashab@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479287}

--- third_party/WebKit/Source/core/loader/EmptyClients.cpp
+++ third_party/WebKit/Source/core/loader/EmptyClients.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/forms/ColorChooser.h"
 #include "core/html/forms/DateTimeChooser.h"
 #include "core/loader/DocumentLoader.h"
+#include "core/page/PopupMenu.h"
 #include "platform/FileChooser.h"
 #include "platform/wtf/PtrUtil.h"
 #include "public/platform/Platform.h"
@@ -63,6 +64,8 @@ void FillWithEmptyClients(Page::PageClients& page_clients) {
 
 class EmptyPopupMenu : public PopupMenu {
  public:
+  EmptyPopupMenu(ChromeClient* chrome_client, HTMLSelectElement& owner_element)
+      : PopupMenu(chrome_client, owner_element) {}
   void Show() override {}
   void Hide() override {}
   void UpdateFromElement(UpdateReason) override {}
@@ -100,8 +103,9 @@ RefPtr<WebTaskRunner> EmptyFrameScheduler::UnthrottledButBlockableTaskRunner() {
   return Platform::Current()->MainThread()->GetWebTaskRunner();
 }
 
-PopupMenu* EmptyChromeClient::OpenPopupMenu(LocalFrame&, HTMLSelectElement&) {
-  return new EmptyPopupMenu();
+PopupMenu* EmptyChromeClient::OpenPopupMenu(LocalFrame& frame,
+                                            HTMLSelectElement& element) {
+  return new EmptyPopupMenu(&frame.GetChromeClient(), element);
 }
 
 ColorChooser* EmptyChromeClient::OpenColorChooser(LocalFrame*,
