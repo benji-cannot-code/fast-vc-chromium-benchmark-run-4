b7eee7ded3f27ce8cbaa4e299be0f250e7f17bf1
Sasha Bermeister <sashab@chromium.org>
2017-06-14 14:05:51 +1000

    Move PopupMenu from platform/ to core/page
    PopupMenu will be merged with PopupMenuImpl once it moves to core/page
    as well.
    
    Bug: 712963
    Change-Id: Ib9e8b0da67dfbcf5bc86d3cefcd1d168c603eb62
    Reviewed-on: https://chromium-review.googlesource.com/527015
    Commit-Queue: Sasha Morrissey <sashab@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479279}

--- third_party/WebKit/Source/core/html/HTMLSelectElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLSelectElement.cpp
@@ -69,8 +69,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/page/AutoscrollController.h"
 #include "core/page/ChromeClient.h"
 #include "core/page/Page.h"
+#include "core/page/PopupMenu.h"
 #include "core/page/SpatialNavigation.h"
-#include "platform/PopupMenu.h"
 #include "platform/instrumentation/tracing/TraceEvent.h"
 #include "platform/text/PlatformLocale.h"
 
--- third_party/WebKit/Source/core/page/PopupMenuImpl.h
+++ third_party/WebKit/Source/core/page/PopupMenuImpl.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/CoreExport.h"
 #include "core/page/PagePopupClient.h"
-#include "platform/PopupMenu.h"
+#include "core/page/PopupMenu.h"
 
 namespace blink {
 
