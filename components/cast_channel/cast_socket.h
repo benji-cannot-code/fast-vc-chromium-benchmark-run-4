e67f7bbb5bd7f93d230715a8d1642907c84616fe
Dale Curtis <dalecurtis@chromium.org>
2017-06-14 16:24:58 +0000

    Revert "[chrome.cast.channel] Remove logging.proto from Cast channel."
    This reverts commit 0a656de983cf9717acef57c64a999a90b96d531d.
    
    Reason for revert: Broke the tree,
    https://build.chromium.org/p/chromium/builders/Mac/builds/28833
    https://build.chromium.org/p/chromium.chrome/builders/Google%20Chrome%20ChromeOS/builds/35155
    
    Original change's description:
    > [chrome.cast.channel] Remove logging.proto from Cast channel.
    > 
    > This protocol message is no longer necessary as we are no longer
    > serializing logging events. This patch also converts the enums still
    > in use to scoped enums, consolidates redundant enums elsewhere, and
    > makes the following renamings:
    > 
    > LastErrors => LastError
    > EventType => ChannelEvent
    > ChallengeAuthErrorType => ChallengeAuthError
    > 
    > Bug: 513801
    > Change-Id: If6e690c94854739f9861219871a048164924126b
    > Reviewed-on: https://chromium-review.googlesource.com/533683
    > Commit-Queue: mark a. foltz <mfoltz@chromium.org>
    > Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#479405}
    
    TBR=mfoltz@chromium.org,kmarshall@chromium.org,marshallk@google.com,zhaobin@chromium.org
    
    Change-Id: Icd83d69ddf8b24f87d8aac08e66646456b5a6f8f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 513801
    Reviewed-on: https://chromium-review.googlesource.com/534903
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479408}

--- components/cast_channel/cast_socket.h
+++ components/cast_channel/cast_socket.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/cast_channel/cast_channel_enum.h"
 #include "components/cast_channel/cast_socket.h"
 #include "components/cast_channel/cast_transport.h"
+#include "components/cast_channel/proto/logging.pb.h"
 #include "net/base/completion_callback.h"
 #include "net/base/io_buffer.h"
 #include "net/base/ip_endpoint.h"
@@ -41,7 +42,7 @@ class X509Certificate;
 namespace cast_channel {
 class CastMessage;
 class Logger;
-struct LastError;
+struct LastErrors;
 
 // Cast device capabilities.
 enum CastDeviceCapability {
@@ -190,7 +191,7 @@ class CastSocketImpl : public CastSocket {
     ChannelError error_state() const;
 
     // Gets recorded error details.
-    LastError last_error() const;
+    LastErrors last_errors() const;
 
     // CastTransport::Delegate interface.
     void OnError(ChannelError error_state) override;
@@ -200,7 +201,7 @@ class CastSocketImpl : public CastSocket {
    private:
     CastSocketImpl* socket_;
     ChannelError error_state_;
-    LastError last_error_;
+    LastErrors last_errors_;
   };
 
   // Replaces the internally-constructed transport object with one provided
@@ -287,7 +288,7 @@ class CastSocketImpl : public CastSocket {
 
   virtual base::Timer* GetTimer();
 
-  void SetConnectState(ConnectionState connect_state);
+  void SetConnectState(proto::ConnectionState connect_state);
   void SetReadyState(ReadyState ready_state);
 
   THREAD_CHECKER(thread_checker_);
@@ -356,13 +357,13 @@ class CastSocketImpl : public CastSocket {
   bool audio_only_;
 
   // Connection flow state machine state.
-  ConnectionState connect_state_;
+  proto::ConnectionState connect_state_;
 
   // Write flow state machine state.
-  WriteState write_state_;
+  proto::WriteState write_state_;
 
   // Read flow state machine state.
-  ReadState read_state_;
+  proto::ReadState read_state_;
 
   // The last error encountered by the channel.
   ChannelError error_state_;
--- components/cast_channel/cast_transport_unittest.cc
+++ components/cast_channel/cast_transport_unittest.cc
@@ -251,10 +251,9 @@ TEST_F(CastTransportTest, TestWriteFailureAsync) {
   RunPendingTasks();
   socket_cbs.Pop(net::ERR_CONNECTION_RESET);
   RunPendingTasks();
-  EXPECT_EQ(ChannelEvent::SOCKET_WRITE,
-            logger_->GetLastError(kChannelId).channel_event);
+  EXPECT_EQ(proto::SOCKET_WRITE, logger_->GetLastErrors(kChannelId).event_type);
   EXPECT_EQ(net::ERR_CONNECTION_RESET,
-            logger_->GetLastError(kChannelId).net_return_value);
+            logger_->GetLastErrors(kChannelId).net_return_value);
 }
 
 // ----------------------------------------------------------------------------
@@ -312,10 +311,9 @@ TEST_F(CastTransportTest, TestWriteFailureSync) {
       message,
       base::Bind(&CompleteHandler::Complete, base::Unretained(&write_handler)));
   RunPendingTasks();
-  EXPECT_EQ(ChannelEvent::SOCKET_WRITE,
-            logger_->GetLastError(kChannelId).channel_event);
+  EXPECT_EQ(proto::SOCKET_WRITE, logger_->GetLastErrors(kChannelId).event_type);
   EXPECT_EQ(net::ERR_CONNECTION_RESET,
-            logger_->GetLastError(kChannelId).net_return_value);
+            logger_->GetLastErrors(kChannelId).net_return_value);
 }
 
 // ----------------------------------------------------------------------------
@@ -431,10 +429,9 @@ TEST_F(CastTransportTest, TestReadErrorInHeaderAsync) {
   transport_->Start();
   // Header read failure.
   socket_cbs.Pop(net::ERR_CONNECTION_RESET);
-  EXPECT_EQ(ChannelEvent::SOCKET_READ,
-            logger_->GetLastError(kChannelId).channel_event);
+  EXPECT_EQ(proto::SOCKET_READ, logger_->GetLastErrors(kChannelId).event_type);
   EXPECT_EQ(net::ERR_CONNECTION_RESET,
-            logger_->GetLastError(kChannelId).net_return_value);
+            logger_->GetLastErrors(kChannelId).net_return_value);
 }
 
 TEST_F(CastTransportTest, TestReadErrorInBodyAsync) {
@@ -472,10 +469,9 @@ TEST_F(CastTransportTest, TestReadErrorInBodyAsync) {
   socket_cbs.Pop(MessageFramer::MessageHeader::header_size());
   // Body read fails.
   socket_cbs.Pop(net::ERR_CONNECTION_RESET);
-  EXPECT_EQ(ChannelEvent::SOCKET_READ,
-            logger_->GetLastError(kChannelId).channel_event);
+  EXPECT_EQ(proto::SOCKET_READ, logger_->GetLastErrors(kChannelId).event_type);
   EXPECT_EQ(net::ERR_CONNECTION_RESET,
-            logger_->GetLastError(kChannelId).net_return_value);
+            logger_->GetLastErrors(kChannelId).net_return_value);
 }
 
 TEST_F(CastTransportTest, TestReadCorruptedMessageAsync) {
@@ -643,10 +639,9 @@ TEST_F(CastTransportTest, TestReadErrorInBodySync) {
       .RetiresOnSaturation();
   EXPECT_CALL(*delegate_, OnError(ChannelError::CAST_SOCKET_ERROR));
   transport_->Start();
-  EXPECT_EQ(ChannelEvent::SOCKET_READ,
-            logger_->GetLastError(kChannelId).channel_event);
+  EXPECT_EQ(proto::SOCKET_READ, logger_->GetLastErrors(kChannelId).event_type);
   EXPECT_EQ(net::ERR_CONNECTION_RESET,
-            logger_->GetLastError(kChannelId).net_return_value);
+            logger_->GetLastErrors(kChannelId).net_return_value);
 }
 
 TEST_F(CastTransportTest, TestReadCorruptedMessageSync) {
