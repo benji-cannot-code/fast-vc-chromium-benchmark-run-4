a45ad0fb5cc94a02a6b942e93ded406cd9d30d81
marcheu <marcheu@google.com>
2017-06-13 22:03:30 -0700

    Revert "Workaround for Qualcomm renderbuffer resize bug"
    This reverts commit 10b29d38bbd2eb90d7d93fb07214d468f7af7dde.
    
    This breaks WebGL on ARM Chromebooks.
    
    BUG=chromium:732616
    TEST=webgl works again on Chrome OS
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2936973003
    Cr-Commit-Position: refs/heads/master@{#479289}

--- gpu/command_buffer/service/gles2_cmd_decoder_unittest_framebuffers.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder_unittest_framebuffers.cc
@@ -2306,9 +2306,13 @@ TEST_P(GLES2DecoderManualInitTest, RenderbufferStorageMultisampleCHROMIUM) {
   DoBindRenderbuffer(
       GL_RENDERBUFFER, client_renderbuffer_id_, kServiceRenderbufferId);
   InSequence sequence;
-  DoRenderbufferStorageMultisampleCHROMIUM(
-      GL_RENDERBUFFER, TestHelper::kMaxSamples, GL_RGBA4, GL_RGBA,
-      TestHelper::kMaxRenderbufferSize, 1, false);
+  EnsureRenderbufferBound(false);
+  DoRenderbufferStorageMultisampleCHROMIUM(GL_RENDERBUFFER,
+                                           TestHelper::kMaxSamples,
+                                           GL_RGBA4,
+                                           GL_RGBA,
+                                           TestHelper::kMaxRenderbufferSize,
+                                           1);
 }
 
 TEST_P(GLES2DecoderManualInitTest,
@@ -2320,9 +2324,13 @@ TEST_P(GLES2DecoderManualInitTest,
       GL_RENDERBUFFER, client_renderbuffer_id_, kServiceRenderbufferId);
   RestoreRenderbufferBindings();
   InSequence sequence;
-  DoRenderbufferStorageMultisampleCHROMIUM(
-      GL_RENDERBUFFER, TestHelper::kMaxSamples, GL_RGBA4, GL_RGBA,
-      TestHelper::kMaxRenderbufferSize, 1, true);
+  EnsureRenderbufferBound(true);
+  DoRenderbufferStorageMultisampleCHROMIUM(GL_RENDERBUFFER,
+                                           TestHelper::kMaxSamples,
+                                           GL_RGBA4,
+                                           GL_RGBA,
+                                           TestHelper::kMaxRenderbufferSize,
+                                           1);
 }
 
 TEST_P(GLES2DecoderManualInitTest,
@@ -2364,16 +2372,16 @@ class GLES2DecoderMultisampledRenderToTextureTest
     DoBindRenderbuffer(
         GL_RENDERBUFFER, client_renderbuffer_id_, kServiceRenderbufferId);
     InSequence sequence;
-
-    EXPECT_CALL(*gl_, GetError())
-        .WillOnce(Return(GL_NO_ERROR))
-        .RetiresOnSaturation();
     if (rb_rebind) {
       RestoreRenderbufferBindings();
       EnsureRenderbufferBound(true);
     } else {
       EnsureRenderbufferBound(false);
     }
+
+    EXPECT_CALL(*gl_, GetError())
+        .WillOnce(Return(GL_NO_ERROR))
+        .RetiresOnSaturation();
     if (strstr(extension, "GL_IMG_multisampled_render_to_texture")) {
       EXPECT_CALL(
           *gl_,
