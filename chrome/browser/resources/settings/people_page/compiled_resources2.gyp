8b8fb52270f9f4de40986101ee8425ab6274a564
scottchen <scottchen@chromium.org>
2017-06-14 13:05:17 -0700

    MD Settings: fix auto-focus on sync page.
    BUG=730954
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2941433002
    Cr-Commit-Position: refs/heads/master@{#479480}

--- chrome/browser/resources/settings/people_page/compiled_resources2.gyp
+++ chrome/browser/resources/settings/people_page/compiled_resources2.gyp
@@ -206,6 +206,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         '<(DEPTH)/ui/webui/resources/js/compiled_resources2.gyp:assert',
         '<(DEPTH)/ui/webui/resources/js/compiled_resources2.gyp:load_time_data',
         '<(DEPTH)/ui/webui/resources/js/compiled_resources2.gyp:web_ui_listener_behavior',
+        '<(DEPTH)/ui/webui/resources/js/compiled_resources2.gyp:util',
         'sync_browser_proxy',
       ],
       'includes': ['../../../../../third_party/closure_compiler/compile_js2.gypi'],
---
+++ chrome/test/data/webui/settings/people_page_sync_page_interactive_test.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+suite('sync-page-test', function() {
+  /** @type {SyncPageElement} */ var testElement;
+
+  setup(function() {
+    PolymerTest.clearBody();
+
+    testElement = document.createElement('settings-sync-page');
+    document.body.appendChild(testElement);
+  });
+
+  test('autofocus correctly after container is shown', function() {
+    cr.webUIListenerCallback('sync-prefs-changed', {passphraseRequired: true});
+    Polymer.dom.flush();
+
+    var input = testElement.$$('#existingPassphraseInput');
+
+    var focused = false;
+    input.addEventListener('focus', function() {
+      focused = true;
+    });
+
+    // Simulate event normally fired by main_page_behavior after subpage
+    // animation ends.
+    testElement.fire('show-container');
+    assertTrue(focused);
+  });
+});
