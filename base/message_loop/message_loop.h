4e4b59b97b73d38ad57e8f4e580e302e4392ccf4
Jeffrey He <jeffreyhe@google.com>
2017-06-14 13:16:48 -0400

    Integrate SequenceLocalStorageMap into MessageLoop
    MessageLoop::BindToCurrentThread() now sets up a SequenceLocalStorageMap
    on the thread running the MessageLoop. This allows SequenceLocalStorage
    to be used by tasks running on a MessageLoop as well as by code within
    the scope of a MessageLoop.
    
    Bug: 695727
    Change-Id: If0d1eb06cd936e1d032d8bb70bbdb994f080a3fe
    Reviewed-on: https://chromium-review.googlesource.com/533394
    Commit-Queue: Jeffrey He <jeffreyhe@google.com>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Francois Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479458}

--- base/message_loop/message_loop.h
+++ base/message_loop/message_loop.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/pending_task.h"
 #include "base/run_loop.h"
 #include "base/synchronization/lock.h"
+#include "base/threading/sequence_local_storage_map.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
 
@@ -460,6 +461,14 @@ class BASE_EXPORT MessageLoop : public MessagePump::Delegate,
 
   // An interface back to RunLoop state accessible by this RunLoop::Delegate.
   RunLoop::Delegate::Client* run_loop_client_ = nullptr;
+
+  // Holds data stored through the SequenceLocalStorageSlot API.
+  internal::SequenceLocalStorageMap sequence_local_storage_map_;
+
+  // Enables the SequenceLocalStorageSlot API within its scope.
+  // Instantiated in BindToCurrentThread().
+  std::unique_ptr<internal::ScopedSetSequenceLocalStorageMapForCurrentThread>
+      scoped_set_sequence_local_storage_map_for_current_thread_;
 
   DISALLOW_COPY_AND_ASSIGN(MessageLoop);
 };
