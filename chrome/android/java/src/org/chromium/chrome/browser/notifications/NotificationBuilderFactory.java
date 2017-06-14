0c59b59b2b106aa8f9e00f8d3d8a7c47dbea21a3
Anita Woodruff <awdf@chromium.org>
2017-06-14 14:59:06 +0100

    [Android Notification Channels] Basic implementation: Java side
    This change completes a basic implementation of site channels, when the
    SiteNotificationChannels feature flag is enabled:
    
    - When notification permission is granted to a site, a channel is
    created for that site.
    
    - When site notifications are posted, they are assigned to the site's
    notification channel, instead of the generic Sites channel.
    
    - When notification permission is cleared in Chrome's Site Settings,
    the channel is deleted.
    
    - Still need to make the Allow/Block toggle in Chrome's Site Settings
    link through to the system channel settings.
    
    Bug: 700377
    Change-Id: I51b09670bc351fe1f6a89abe33b6f860dbb468b6
    Reviewed-on: https://chromium-review.googlesource.com/531165
    Commit-Queue: Anita Woodruff <awdf@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479394}

--- chrome/android/java/src/org/chromium/chrome/browser/notifications/NotificationBuilderFactory.java
+++ chrome/android/java/src/org/chromium/chrome/browser/notifications/NotificationBuilderFactory.java
@@ -11,7 +11,6 @@ import android.content.Context;
 
 import org.chromium.base.BuildInfo;
 import org.chromium.base.ContextUtils;
-import org.chromium.chrome.browser.notifications.channels.ChannelDefinitions;
 import org.chromium.chrome.browser.notifications.channels.ChannelsInitializer;
 
 /**
@@ -34,7 +33,7 @@ public class NotificationBuilderFactory {
      *                  {@link ChannelsInitializer#ensureInitialized(String)}.
      */
     public static ChromeNotificationBuilder createChromeNotificationBuilder(
-            boolean preferCompat, @ChannelDefinitions.ChannelId String channelId) {
+            boolean preferCompat, String channelId) {
         Context context = ContextUtils.getApplicationContext();
         if (BuildInfo.isAtLeastO()) {
             return createNotificationBuilderForO(channelId, context);
@@ -45,7 +44,7 @@ public class NotificationBuilderFactory {
 
     @SuppressLint("NewApi") // for Context.getSystemService(Class)
     private static ChromeNotificationBuilder createNotificationBuilderForO(
-            @ChannelDefinitions.ChannelId String channelId, Context context) {
+            String channelId, Context context) {
         return new NotificationBuilderForO(context, channelId,
                 new ChannelsInitializer(new NotificationManagerProxyImpl(context.getSystemService(
                                                 NotificationManager.class)),
