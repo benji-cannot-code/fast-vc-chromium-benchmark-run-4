04c947113f8ac8ae7653633202927420e0af3486
Xianzhu Wang <wangxianzhu@chromium.org>
2017-06-14 15:31:39 -0700

    Fix crash when a stacked layer is removed from a non-stack composited layer
    Previously we missed invalidatation of the compositing container
    of the removed stacked layer.
    
    Now invalidate the compositing container chain at the beginning of
    PaintLayer::RemoveChild().
    
    BUG=732597
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I0bdebc096e459319bdd0d9010141e588dd1f6cb4
    Reviewed-on: https://chromium-review.googlesource.com/536133
    Reviewed-by: Chris harrelson <chrishtr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480219}

--- third_party/WebKit/Source/core/paint/PaintLayer.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayer.cpp
@@ -1318,6 +1318,8 @@ void PaintLayer::AddChild(PaintLayer* child, PaintLayer* before_child) {
 }
 
 PaintLayer* PaintLayer::RemoveChild(PaintLayer* old_child) {
+  old_child->MarkCompositingContainerChainForNeedsRepaint();
+
   if (old_child->PreviousSibling())
     old_child->PreviousSibling()->SetNextSibling(old_child->NextSibling());
   if (old_child->NextSibling())
@@ -1361,8 +1363,6 @@ PaintLayer* PaintLayer::RemoveChild(PaintLayer* old_child) {
 
   if (old_child->EnclosingPaginationLayer())
     old_child->ClearPaginationRecursive();
-
-  SetNeedsRepaint();
 
   return old_child;
 }
