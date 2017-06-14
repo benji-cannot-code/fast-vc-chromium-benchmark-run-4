3a32bc23ebd390fe8c3beaa9683ef9c643b9a7bc
liaoyuke <liaoyuke@chromium.org>
2017-06-14 14:11:23 -0700

    [ObjC ARC] Converts ios/testing:ocmock_support_unittest to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2891863002
    Cr-Commit-Position: refs/heads/master@{#479505}

--- ios/testing/ocmock_complex_type_helper_unittest.mm
+++ ios/testing/ocmock_complex_type_helper_unittest.mm
@@ -6,10 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/testing/ocmock_complex_type_helper.h"
 
 #include "base/logging.h"
-#import "base/mac/scoped_nsobject.h"
 #include "testing/platform_test.h"
 #import "third_party/ocmock/OCMock/OCMock.h"
 #include "third_party/ocmock/gtest_support.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
 
 // A complex type to test with..
 struct SampleComplexType {
@@ -50,9 +53,9 @@ class OCMockComplexTypeHelperTest : public PlatformTest {
  protected:
   void SetUp() override {
     PlatformTest::SetUp();
-    helped_mock_.reset([[MockClass alloc]
-        initWithRepresentedObject:
-            [OCMockObject mockForProtocol:@protocol(TestedProtocol)]]);
+    OCMockObject* protocol_mock =
+        [OCMockObject mockForProtocol:@protocol(TestedProtocol)];
+    helped_mock_ = [[MockClass alloc] initWithRepresentedObject:protocol_mock];
   }
 
   void TearDown() override {
@@ -60,7 +63,7 @@ class OCMockComplexTypeHelperTest : public PlatformTest {
     PlatformTest::TearDown();
   }
 
-  base::scoped_nsobject<id> helped_mock_;
+  id helped_mock_;
 };
 
 TEST_F(OCMockComplexTypeHelperTest, nilObjectStillWorks) {
@@ -69,7 +72,7 @@ TEST_F(OCMockComplexTypeHelperTest, nilObjectStillWorks) {
 }
 
 TEST_F(OCMockComplexTypeHelperTest, anyObjectStillWorks) {
-  base::scoped_nsobject<id> someObject([[NSObject alloc] init]);
+  id someObject = [[NSObject alloc] init];
   [[helped_mock_ expect] passObject:OCMOCK_ANY];
   [helped_mock_ passObject:someObject];
 }
