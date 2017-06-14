b3069d0adef7e55eff414f5d0ac136bb0254937b
rohitrao <rohitrao@chromium.org>
2017-06-14 07:10:00 -0700

    [ios] Removes the setChipText method from OmniboxTextFieldIOS.
    This function is safe to remove after verifying that no callers were relying on
    the call to updateLeftView as a side effect of calling setChipText.  See the
    comments in the codereview for specific details.
    
    BUG=527473
    TEST=The page/lock icon should update properly when pressing Go in the omnibox.
    
    Review-Url: https://codereview.chromium.org/2938733002
    Cr-Commit-Position: refs/heads/master@{#479373}

--- ios/chrome/browser/ui/omnibox/omnibox_view_ios.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_view_ios.mm
@@ -393,7 +393,6 @@ void OmniboxViewIOS::OnDidBeginEditing() {
 void OmniboxViewIOS::OnDidEndEditing() {
   CloseOmniboxPopup();
   [field_ enableLeftViewButton:YES];
-  [field_ setChipText:@""];
   model()->OnWillKillFocus();
   model()->OnKillFocus();
   if ([field_ isPreEditing])
@@ -724,9 +723,6 @@ void OmniboxViewIOS::UpdateAppearance() {
     RevertAll();
   } else if (!model()->has_focus() &&
              !ShouldIgnoreUserInputDueToPendingVoiceSearch()) {
-    // Only update the chip text if the omnibox is not currently focused.
-    [field_ setChipText:@""];
-
     // Even if the change wasn't "user visible" to the model, it still may be
     // necessary to re-color to the URL string.  Only do this if the omnibox is
     // not currently focused.
@@ -787,7 +783,6 @@ void OmniboxViewIOS::ClearText() {
 }
 
 void OmniboxViewIOS::RemoveQueryRefinementChip() {
-  [field_ setChipText:nil];
   controller_->OnChanged();
 }
 
