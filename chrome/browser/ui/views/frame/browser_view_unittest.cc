fe57639b4883f324e678af177010bac56f5d77ee
yiyix <yiyix@chromium.org>
2017-06-13 21:55:15 -0400

    Fix: Icons are chopped from bottom on bookmark bar
    In cl "reducing overdraw on bookmark bar", https://codereview.chromium.org/2899133004, 
    the height of bookmark bar is reduced by 1 px for both detached mode and attached mode 
    after removing the 1 px overlap between toolbar and bookmark bar. However, the tool 
    bar and bookmark bar only overlaps when bookmark bar is in detached mode. The height 
    of bookmark bar in attached mode should remain unchanged. 
    
    TEST=BrowserView.BrowserViewLayout
    
    Bug: 732062
    Change-Id: Id363f9cf9d3ef5a606eb3f94723b28c5b1db09cb
    Reviewed-on: https://chromium-review.googlesource.com/532055
    Commit-Queue: Yi Xu <yiyix@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479482}

--- chrome/browser/ui/views/frame/browser_view_unittest.cc
+++ chrome/browser/ui/views/frame/browser_view_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "chrome/app/chrome_command_ids.h"
+#include "chrome/browser/ui/bookmarks/bookmark_bar_constants.h"
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/views/bookmarks/bookmark_bar_view.h"
 #include "chrome/browser/ui/views/frame/browser_view_layout.h"
@@ -115,6 +116,9 @@ TEST_F(BrowserViewTest, BrowserViewLayout) {
   BookmarkBarView* bookmark_bar = browser_view()->GetBookmarkBarView();
   EXPECT_FALSE(bookmark_bar->visible());
   EXPECT_FALSE(bookmark_bar->IsDetached());
+  EXPECT_EQ(devtools_web_view->y(), bookmark_bar->height());
+  EXPECT_EQ(chrome::kMinimumBookmarkBarHeight,
+            bookmark_bar->GetMinimumSize().height());
   chrome::ExecuteCommand(browser, IDC_SHOW_BOOKMARK_BAR);
   EXPECT_TRUE(bookmark_bar->visible());
   EXPECT_FALSE(bookmark_bar->IsDetached());
