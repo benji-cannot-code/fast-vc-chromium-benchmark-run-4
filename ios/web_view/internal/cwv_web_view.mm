65adbc98f76676269174726ec967109a9a34564f
eugenebut <eugenebut@chromium.org>
2017-06-13 18:01:06 -0700

    Correctly update CWVWebView properties on state restoration.
    This has regressed when properties were made KVO compiant.
    
    BUG=704038
    
    Review-Url: https://codereview.chromium.org/2935923002
    Cr-Commit-Position: refs/heads/master@{#479235}

--- ios/web_view/internal/cwv_web_view.mm
+++ ios/web_view/internal/cwv_web_view.mm
@@ -88,6 +88,8 @@ NSString* const kSessionStorageKey = @"sessionStorage";
 - (void)updateNavigationAvailability;
 // Updates the URLs exposed through |lastCommittedURL| and |visibleURL|.
 - (void)updateCurrentURLs;
+// Updates |title| property.
+- (void)updateTitle;
 
 @end
 
@@ -240,7 +242,7 @@ static NSString* gUserAgentProduct = nil;
 }
 
 - (void)webStateDidChangeTitle:(web::WebState*)webState {
-  self.title = base::SysUTF16ToNSString(_webState->GetTitle());
+  [self updateTitle];
 }
 
 - (void)renderProcessGoneForWebState:(web::WebState*)webState {
@@ -374,6 +376,12 @@ static NSString* gUserAgentProduct = nil;
   _translationController.webState = _webState.get();
 
   [self addInternalWebViewAsSubview];
+
+  [self updateNavigationAvailability];
+  [self updateCurrentURLs];
+  [self updateTitle];
+  self.loading = NO;
+  self.estimatedProgress = 0.0;
 }
 
 // Adds the web view provided by |_webState| as a subview unless it has already.
@@ -397,6 +405,10 @@ static NSString* gUserAgentProduct = nil;
 - (void)updateCurrentURLs {
   self.lastCommittedURL = net::NSURLWithGURL(_webState->GetLastCommittedURL());
   self.visibleURL = net::NSURLWithGURL(_webState->GetVisibleURL());
+}
+
+- (void)updateTitle {
+  self.title = base::SysUTF16ToNSString(_webState->GetTitle());
 }
 
 @end
