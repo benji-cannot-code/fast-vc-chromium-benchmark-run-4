828c1ac8d1d8ecba931d773c56d57f6a7ec74ac0
tnagel <tnagel@chromium.org>
2017-06-14 08:10:58 -0700

    Fix scoping of ScopedStubInstallAttributes
    The existing code got lucky and return value optimization prevented the
    destructor from being called prematurely.  This CL disallows copy and
    assign so that we don't have to rely on luck anymore.
    
    BUG=639295, 638713
    TEST=existing tests still pass
    
    Review-Url: https://codereview.chromium.org/2937703002
    Cr-Commit-Position: refs/heads/master@{#479385}

--- chrome/browser/chromeos/settings/stub_install_attributes.cc
+++ chrome/browser/chromeos/settings/stub_install_attributes.cc
@@ -88,11 +88,9 @@ ScopedStubInstallAttributes::CreateActiveDirectoryManaged(
   return ScopedStubInstallAttributes();
 }
 
-ScopedStubInstallAttributes::ScopedStubInstallAttributes() {
-}
-
 ScopedStubInstallAttributes::~ScopedStubInstallAttributes() {
-  policy::BrowserPolicyConnectorChromeOS::RemoveInstallAttributesForTesting();
+  if (cleanup_)
+    policy::BrowserPolicyConnectorChromeOS::RemoveInstallAttributesForTesting();
 }
 
 }  // namespace chromeos
