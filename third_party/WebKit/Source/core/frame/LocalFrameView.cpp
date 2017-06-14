fb8f8ba8bdbfa4cd52b8c1d201de2304e26891f3
Joel Hockey <joelhockey@chromium.org>
2017-06-13 21:09:58 -0700

    Add CHECKS to see that FrameView is detached before Frame
    Bug: 729196
    Change-Id: I91d7faec0620cb7d3a774e8bf01dfd5010cf5e2d
    Reviewed-on: https://chromium-review.googlesource.com/535293
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479512}

--- third_party/WebKit/Source/core/frame/LocalFrameView.cpp
+++ third_party/WebKit/Source/core/frame/LocalFrameView.cpp
@@ -242,6 +242,7 @@ LocalFrameView::~LocalFrameView() {
 
 DEFINE_TRACE(LocalFrameView) {
   visitor->Trace(frame_);
+  visitor->Trace(parent_);
   visitor->Trace(fragment_anchor_);
   visitor->Trace(scrollable_areas_);
   visitor->Trace(animating_scrollable_areas_);
@@ -3885,9 +3886,17 @@ void LocalFrameView::RemoveAnimatingScrollableArea(
 }
 
 void LocalFrameView::AttachToLayout() {
-  DCHECK(!is_attached_);
+  CHECK(!is_attached_);
   is_attached_ = true;
-  if (ParentFrameView()->IsVisible())
+  parent_ = ParentFrameView();
+  if (!parent_) {
+    Frame* parent_frame = frame_->Tree().Parent();
+    CHECK(parent_frame);
+    CHECK(parent_frame->IsLocalFrame());
+    CHECK(parent_frame->View());
+  }
+  CHECK(parent_);
+  if (parent_->IsVisible())
     SetParentVisible(true);
   UpdateParentScrollableAreaSet();
   SetupRenderThrottling();
@@ -3895,9 +3904,18 @@ void LocalFrameView::AttachToLayout() {
 }
 
 void LocalFrameView::DetachFromLayout() {
-  DCHECK(is_attached_);
+  // TODO(crbug.com/729196): Trace why LocalFrameView::DetachFromLayout crashes.
+  CHECK(is_attached_);
+  LocalFrameView* parent = ParentFrameView();
+  if (!parent) {
+    Frame* parent_frame = frame_->Tree().Parent();
+    CHECK(parent_frame);
+    CHECK(parent_frame->IsLocalFrame());
+    CHECK(parent_frame->View());
+  }
+  CHECK(parent == parent_);
   if (!RuntimeEnabledFeatures::RootLayerScrollingEnabled())
-    ParentFrameView()->RemoveScrollableArea(this);
+    parent->RemoveScrollableArea(this);
   SetParentVisible(false);
   is_attached_ = false;
 }
