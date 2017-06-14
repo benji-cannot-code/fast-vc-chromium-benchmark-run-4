83d4c12822d1d1e37b5346efbfa5ba026c14d86f
bcwhite <bcwhite@chromium.org>
2017-06-14 14:45:03 -0700

    Persist core system profile during startup.
    Almost 40% of persistent metrics files are missing the embedded system
    profile which is stored about 1 minute into the process lifetime when
    the initial metrics upload is sent.
    
    This distinguishes between basic and complete system profiles allowing
    the core profile to be stored when the Log is created and be replaced by
    a more complete version later.  Thus basic profile will thus be available
    even if the browser crashes almost immediately.
    
    BUG=695880
    
    Review-Url: https://codereview.chromium.org/2938013002
    Cr-Commit-Position: refs/heads/master@{#479513}

--- components/metrics/file_metrics_provider_unittest.cc
+++ components/metrics/file_metrics_provider_unittest.cc
@@ -497,7 +497,7 @@ TEST_P(FileMetricsProviderTest, AccessEmbeddedProfileMetricsWithProfile) {
         PersistentSystemProfile persistent_profile;
         persistent_profile.RegisterPersistentAllocator(
             allocator->memory_allocator());
-        persistent_profile.SetSystemProfile(profile_proto);
+        persistent_profile.SetSystemProfile(profile_proto, true);
       });
 
   // Register the file and allow the "checker" task to run.
@@ -568,7 +568,7 @@ TEST_P(FileMetricsProviderTest, AccessEmbeddedFallbackMetricsWithProfile) {
         PersistentSystemProfile persistent_profile;
         persistent_profile.RegisterPersistentAllocator(
             allocator->memory_allocator());
-        persistent_profile.SetSystemProfile(profile_proto);
+        persistent_profile.SetSystemProfile(profile_proto, true);
       });
 
   // Register the file and allow the "checker" task to run.
