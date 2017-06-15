b727821f21d8f7ae582e7d166120a4a18871913c
Pavel Yatsuk <pavely@chromium.org>
2017-06-15 10:56:56 -0700

    [Sync] FaviconCache: Remove entries from page_favicon_map_ when synced favicon is freed
    The problem is that entries are never removed from page_favicon_map_ and
    therefore accumulate in memory. page_favicon_map_is used to map page url to
    favicon url before accessing favicon in synced_favicons_. It is not really
    needed if there is no entry in synced_favicons_.
    
    In this change I'm erasing entries from page_favicon_map_ whose value matches
    favicon url.
    
    BUG=732969
    R=zea@chromium.org
    
    Change-Id: I9d9deb74d083b07c026ba2e1a4fa86ef3137ef92
    Reviewed-on: https://chromium-review.googlesource.com/537114
    Reviewed-by: Nicolas Zea <zea@chromium.org>
    Commit-Queue: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480681}

--- components/sync_sessions/favicon_cache.cc
+++ components/sync_sessions/favicon_cache.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/single_thread_task_runner.h"
+#include "base/stl_util.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "components/favicon/core/favicon_service.h"
 #include "components/history/core/browser/history_service.h"
@@ -962,7 +963,11 @@ void FaviconCache::DeleteSyncedFavicon(
 
 void FaviconCache::DropSyncedFavicon(FaviconMap::iterator favicon_iter) {
   DVLOG(1) << "Dropping favicon " << favicon_iter->second.get()->favicon_url;
+  const GURL& url = favicon_iter->first;
   recent_favicons_.erase(favicon_iter->second);
+  base::EraseIf(page_favicon_map_, [url](const PageFaviconMap::value_type& kv) {
+    return kv.second == url;
+  });
   synced_favicons_.erase(favicon_iter);
 }
 
