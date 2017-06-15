22530dd7a964c503e011324423fd4ff436620ecb
Darren Shen <shend@chromium.org>
2017-06-15 14:34:09 +1000

    Generate GridItemData.
    This patch generates all GridItemData fields as storage_only.
    
    Diff of generated files:
    https://gist.github.com/5cb077833e916d7c9cbc7b828f2870d9/revisions
    
    Bug: 628043
    Change-Id: I9ad401c3961f6e762c7ba6969cbe99390264d471
    Reviewed-on: https://chromium-review.googlesource.com/535239
    Reviewed-by: nainar <nainar@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479867}

--- third_party/WebKit/Source/core/style/ComputedStyle.h
+++ third_party/WebKit/Source/core/style/ComputedStyle.h
@@ -53,7 +53,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/style/StyleDifference.h"
 #include "core/style/StyleFilterData.h"
 #include "core/style/StyleGridData.h"
-#include "core/style/StyleGridItemData.h"
 #include "core/style/StyleImage.h"
 #include "core/style/StyleInheritedVariables.h"
 #include "core/style/StyleOffsetRotation.h"
