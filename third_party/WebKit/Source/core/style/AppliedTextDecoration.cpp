d03d5780f6a9599f8a8ce2e01f490490eb948b98
Naina Raisinghani <nainar@chromium.org>
2017-06-15 13:00:53 +1000

    Make TextDecorationStyle an enum class and change name to ETextDecorationStyle.
    This patch makes TextDecorationStyle an enum class for type safety and changes the
    name to EResize to match the other enums
    
    BUG=684966
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ia3757a254b1fc44dcf1881dab0c46047dd7cd5ec
    Reviewed-on: https://chromium-review.googlesource.com/536713
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Commit-Queue: nainar <nainar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480007}

--- third_party/WebKit/Source/core/style/AppliedTextDecoration.cpp
+++ third_party/WebKit/Source/core/style/AppliedTextDecoration.cpp
@@ -8,9 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 AppliedTextDecoration::AppliedTextDecoration(TextDecoration line,
-                                             TextDecorationStyle style,
+                                             ETextDecorationStyle style,
                                              Color color)
-    : lines_(static_cast<unsigned>(line)), style_(style), color_(color) {}
+    : lines_(static_cast<unsigned>(line)),
+      style_(static_cast<unsigned>(style)),
+      color_(color) {}
 
 bool AppliedTextDecoration::operator==(const AppliedTextDecoration& o) const {
   return color_ == o.color_ && lines_ == o.lines_ && style_ == o.style_;
