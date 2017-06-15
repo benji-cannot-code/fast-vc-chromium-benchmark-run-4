a457ff7bab32141c30faf16b6e6d257677305d98
marq <marq@chromium.org>
2017-06-15 06:58:57 -0700

    [clean ios] Fix TabCollectionMediator disconnect.
    TabCollectionMediator -disconnect wasn't removing observers, and was
    spuriously resetting the observer (the scoped observer takes care of
    that).
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2936563002
    Cr-Commit-Position: refs/heads/master@{#479689}

--- ios/clean/chrome/browser/ui/tab_collection/tab_collection_mediator.mm
+++ ios/clean/chrome/browser/ui/tab_collection/tab_collection_mediator.mm
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)disconnect {
   _webStateList = nullptr;
   _webStateObserver.reset();
+  _scopedWebStateListObserver->RemoveAll();
 }
 
 #pragma mark - Properties
