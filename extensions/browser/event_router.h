7d1f671ad849a3de2cb96c346820f8290bebc1b6
lazyboy <lazyboy@chromium.org>
2017-06-15 14:17:27 -0700

    Move lazy event dispatching code out of EventRouter.
    Separate out lazy background page logic from EventRouter to
    LazyEventDispatcher. This will facilitate introducing service worker
    (another "lazy context") lazy events to extensions.
    
    Also introduce LazyContextId to identify a lazy context. This is
    currently used only for identifying event page context, but in future
    will also identify extension service worker context.
    
    BUG=721147, 723754
    
    Review-Url: https://codereview.chromium.org/2940883007
    Cr-Commit-Position: refs/heads/master@{#479833}

--- extensions/browser/event_router.h
+++ extensions/browser/event_router.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/linked_ptr.h"
 #include "base/memory/ref_counted.h"
+#include "base/memory/weak_ptr.h"
 #include "base/scoped_observer.h"
 #include "base/values.h"
 #include "components/keyed_service/core/keyed_service.h"
@@ -102,6 +103,12 @@ class EventRouter : public KeyedService,
                                     std::unique_ptr<base::ListValue> event_args,
                                     UserGestureState user_gesture,
                                     const EventFilteringInfo& info);
+
+  // Returns false when the event is scoped to a context and the listening
+  // extension does not have access to events from that context.
+  static bool CanDispatchEventToBrowserContext(content::BrowserContext* context,
+                                               const Extension* extension,
+                                               const Event& event);
 
   // An EventRouter is shared between |browser_context| and its associated
   // incognito context. |extension_prefs| may be NULL in tests.
@@ -300,14 +307,6 @@ class EventRouter : public KeyedService,
                               const base::DictionaryValue* listener_filter,
                               bool did_enqueue);
 
-  // Returns false when the event is scoped to a context and the listening
-  // extension does not have access to events from that context. Also fills
-  // |event_args| with the proper arguments to send, which may differ if
-  // the event crosses the incognito boundary.
-  bool CanDispatchEventToBrowserContext(content::BrowserContext* context,
-                                        const Extension* extension,
-                                        const linked_ptr<Event>& event);
-
   // Possibly loads given extension's background page in preparation to
   // dispatch an event.  Returns true if the event was queued for subsequent
   // dispatch, false otherwise.
@@ -378,6 +377,8 @@ class EventRouter : public KeyedService,
   std::set<content::RenderProcessHost*> observed_process_set_;
 
   LazyEventDispatchUtil lazy_event_dispatch_util_;
+
+  base::WeakPtrFactory<EventRouter> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(EventRouter);
 };
