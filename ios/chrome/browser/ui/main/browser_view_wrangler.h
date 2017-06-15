54bbd2774a477451855468c9df4b5609f03779fa
Sylvain Defresne <sdefresne@chromium.org>
2017-06-15 12:25:20 +0200

    [ios] Ensure proper shutdown of Chrome on iOS.
    As ARC uses -autorelease (or at least the method used has similar
    effect), the -dealloc method call may be delayed. This does not
    mix well with C++ memory management.
    
    Add -shutdown method to ensure that Objective-C objects that need
    to deterministically deregister themselves from C++ objects can
    do it.
    
    BUG=722879
    
    Change-Id: I2eebf29a510421ca0518a92535782cc61056ffd4
    Reviewed-on: https://chromium-review.googlesource.com/532874
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479681}

--- ios/chrome/browser/ui/main/browser_view_wrangler.h
+++ ios/chrome/browser/ui/main/browser_view_wrangler.h
@@ -58,6 +58,9 @@ class ChromeBrowserState;
 - (void)deleteIncognitoTabModelState:
     (BrowsingDataRemovalController*)removalController;
 
+// Called before the instance is deallocated.
+- (void)shutdown;
+
 @end
 
 @interface BrowserViewWrangler (Testing)
