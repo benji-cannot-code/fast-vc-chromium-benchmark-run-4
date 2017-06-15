39572baad473a510481773645e61e479820c7199
Naina Raisinghani <nainar@chromium.org>
2017-06-15 19:52:34 +1000

    Generate enum/getters/setters for transform-style.
    Currently, the transform-style (named TransformStyle3D internally)
    property is a 'storage_only' field, so it has no generated public
    getters/setters.
    
    This patch changes it to a 'keyword' field so that its getters/setters
    can be generated as well. We also generate the ETransformStyle3D enum.
    Although there are generated CSSValueID <-> ETransformStyle3D mappings,
    they are not used because the handwritten code don't have them.
    
    Diff: https://gist.github.com/nainar/56e680b491980fcb82332c616ab47c65/revisions
    
    Bug: 628043
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I95ffd60c4edcc155b1b58a87f8f9380a22bc8b18
    Reviewed-on: https://chromium-review.googlesource.com/536222
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Commit-Queue: nainar <nainar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479984}

--- third_party/WebKit/Source/core/style/ComputedStyleConstants.h
+++ third_party/WebKit/Source/core/style/ComputedStyleConstants.h
@@ -219,8 +219,6 @@ inline TextDecorationSkip& operator|=(TextDecorationSkip& a,
   return a = a | b;
 }
 
-enum class ETransformStyle3D { kFlat, kPreserve3D };
-
 enum OffsetRotationType { kOffsetRotationAuto, kOffsetRotationFixed };
 
 enum EBackfaceVisibility {
