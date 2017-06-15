af6ed39500eb2de8df49228490d3a918abca35ba
Charles Harrison <csharrison@chromium.org>
2017-06-15 16:40:02 -0400

    [subresource_filter] Replace the infobar with the experimental infobar
    This CL replaces the subresource filter infobar with an "AdsBlockedInfobar"
    and updates strings accordingly. This infobar will be used on master for
    filtering on pages just after a safe browsing interstitial. Other uses
    (e.g. for blocking more generally on sites which show intrusive ads)
    is still behind a flag
    
    Bug: 689992
    Change-Id: I62bf99e3f67e4a32fec8820f5bdad0b93da3d17e
    Reviewed-on: https://chromium-review.googlesource.com/526092
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Dan Alcantara <dfalcantara@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479864}

--- chrome/browser/subresource_filter/chrome_subresource_filter_client.cc
+++ chrome/browser/subresource_filter/chrome_subresource_filter_client.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/subresource_filter/subresource_filter_content_settings_manager.h"
 #include "chrome/browser/subresource_filter/subresource_filter_profile_context.h"
 #include "chrome/browser/subresource_filter/subresource_filter_profile_context_factory.h"
-#include "chrome/browser/ui/android/content_settings/subresource_filter_infobar_delegate.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/content_settings/core/common/content_settings_types.h"
 #include "components/safe_browsing_db/database_manager.h"
@@ -33,6 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/subresource_filter/core/common/activation_state.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_handle.h"
+
+#if defined(OS_ANDROID)
+#include "chrome/browser/ui/android/content_settings/ads_blocked_infobar_delegate.h"
+#endif
 
 DEFINE_WEB_CONTENTS_USER_DATA_KEY(ChromeSubresourceFilterClient);
 
@@ -122,7 +125,7 @@ void ChromeSubresourceFilterClient::ToggleNotificationVisibility(
 #if defined(OS_ANDROID)
     InfoBarService* infobar_service =
         InfoBarService::FromWebContents(web_contents_);
-    SubresourceFilterInfobarDelegate::Create(infobar_service);
+    AdsBlockedInfobarDelegate::Create(infobar_service);
 #endif
     TabSpecificContentSettings* content_settings =
         TabSpecificContentSettings::FromWebContents(web_contents_);
