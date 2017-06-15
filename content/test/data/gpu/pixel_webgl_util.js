df197006dbeb2a4ff62198c2c883a3caac1c0edd
Justin Novosad <junov@chromium.org>
2017-06-15 17:04:12 -0400

    Removing 'webkit' prefix from rAF callsites
    Some test logs are polluted with deprecation warnings due to calls
    to webkitRequestAnimationFrame. Not anymore...
    
    Bug: 733814
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I7ad29702cd44d7afd428a8a7d50fa7b63e6db1a7
    Reviewed-on: https://chromium-review.googlesource.com/537083
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480058}

--- content/test/data/gpu/pixel_webgl_util.js
+++ content/test/data/gpu/pixel_webgl_util.js
@@ -122,6 +122,6 @@ function drawSomeFramesUtil()
   } else {
     g_swapsBeforeAckUtil--;
     drawTriangle(g_glUtil);
-    window.webkitRequestAnimationFrame(drawSomeFramesUtil);
+    window.requestAnimationFrame(drawSomeFramesUtil);
   }
 }
