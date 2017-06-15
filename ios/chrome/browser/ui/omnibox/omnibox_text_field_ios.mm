5e0bca24fdd5985b866a086f51f9e7365cc65caf
justincohen <justincohen@chromium.org>
2017-06-15 08:16:04 -0700

    [ios] Fix omnibox baseWritingDirection and dismissal iOS11 bugs.
    BUG=730461, 733650
    
    Review-Url: https://codereview.chromium.org/2933963002
    Cr-Commit-Position: refs/heads/master@{#479708}

--- ios/chrome/browser/ui/omnibox/omnibox_text_field_ios.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_text_field_ios.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <CoreText/CoreText.h>
 
 #include "base/command_line.h"
+#include "base/ios/ios_util.h"
 #include "base/logging.h"
 #include "base/mac/foundation_util.h"
 
@@ -555,15 +556,19 @@ NSString* const kOmniboxFadeAnimationKey = @"OmniboxFadeAnimation";
 
   NSTextAlignment alignment = [self bestTextAlignment];
   [self setTextAlignment:alignment];
-  UITextWritingDirection writingDirection =
-      alignment == NSTextAlignmentLeft ? UITextWritingDirectionLeftToRight
-                                       : UITextWritingDirectionRightToLeft;
-  [self
-      setBaseWritingDirection:writingDirection
-                     forRange:[self
-                                  textRangeFromPosition:[self
-                                                            beginningOfDocument]
-                                             toPosition:[self endOfDocument]]];
+  if (!base::ios::IsRunningOnIOS11OrLater()) {
+    // TODO(crbug.com/730461): Remove this entire block once it's been tested
+    // on trunk.
+    UITextWritingDirection writingDirection =
+        alignment == NSTextAlignmentLeft ? UITextWritingDirectionLeftToRight
+                                         : UITextWritingDirectionRightToLeft;
+    [self
+        setBaseWritingDirection:writingDirection
+                       forRange:
+                           [self
+                               textRangeFromPosition:[self beginningOfDocument]
+                                          toPosition:[self endOfDocument]]];
+  }
 }
 
 - (void)setPlaceholder:(NSString*)placeholder {
