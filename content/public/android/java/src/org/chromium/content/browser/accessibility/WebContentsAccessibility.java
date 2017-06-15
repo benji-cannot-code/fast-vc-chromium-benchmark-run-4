13820768b8cf5e4ae462c3b5dd86164f1ab7c2fe
Jinsuk Kim <jinsukkim@chromium.org>
2017-06-15 15:14:00 +0200

    Fix crash in WebContentsAccessibility
    WebContentsAccessibility.isEnabled() can be invoked by Android framework
    via ContentViewCore.getAccessibilityNodeProvider() after the native
    accessibility is disabled. The call down to native should be safe-guarded
    by null check against the native pointer.
    
    Bug: 733605
    Change-Id: I23ec359c0e0ac7c4ebb0ce0783418e481be34ffe
    Reviewed-on: https://chromium-review.googlesource.com/536936
    Reviewed-by: Paul Miller <paulmiller@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479950}

--- content/public/android/java/src/org/chromium/content/browser/accessibility/WebContentsAccessibility.java
+++ content/public/android/java/src/org/chromium/content/browser/accessibility/WebContentsAccessibility.java
@@ -141,7 +141,7 @@ public class WebContentsAccessibility {
     }
 
     public boolean isEnabled() {
-        return nativeIsEnabled(mNativeObj);
+        return mNativeObj != 0 ? nativeIsEnabled(mNativeObj) : false;
     }
 
     public void enable() {
