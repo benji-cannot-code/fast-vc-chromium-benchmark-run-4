82eb4b5a75777f35295a4ff507b5808c8eb143d4
Olga Sharonova <olka@chromium.org>
2017-06-15 10:30:26 +0000

    Revert "[Mojo Video Capture] Add support for ImageCapture API to video_capture service"
    This reverts commit dd68266fcef32bcb88046c2e795689ee95faf386.
    
    Reason for revert: Speculative revert, possible caused https://bugs.chromium.org/p/chromium/issues/detail?id=733582
    
    Original change's description:
    > [Mojo Video Capture] Add support for ImageCapture API to video_capture service
    > 
    > This CL is part of the Mojo Video Capture work. For the bigger picture,
    > see [1] CL26.1.
    > 
    > Adds methods to video_capture service's Device interface to bring it on 
    > par with interface media::VideoCaptureDevice. Besides three methods
    >   RequestRefreshFrame()
    >   MaybeSuspend()
    >   Resume()
    > which simply forward an event, this consists of methods for the 
    > ImageCapture API.
    > 
    > Use existing integration test for the ImageCaptureAPI.
    > Make the tests run both with and without the Mojo service.
    > 
    > [1] https://docs.google.com/a/chromium.org/document/d/1Qw7rw1AJy0QHXjha36jZNiEuxsxWslJ_X-zpOhijvI8/edit?usp=sharing
    > 
    >   content_browsertests --gtest_filter="WebRtcImageCapture*"
    > 
    > BUG: 584797
    > TEST: 
    > Change-Id: I8ab26be74bc0b8f666dcf9b4b20559403e1e0120
    > Reviewed-on: https://chromium-review.googlesource.com/526254
    > Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    > Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    > Reviewed-by: Miguel Casas <mcasas@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#479554}
    
    TBR=meacer@chromium.org,mcasas@chromium.org,chfremer@chromium.org
    
    Change-Id: Iad148237905b52c2a97c73be5c7c2245170e3da8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/536913
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479663}

--- media/capture/video/fake_video_capture_device_factory.h
+++ media/capture/video/fake_video_capture_device_factory.h
@@ -19,7 +19,6 @@ struct CAPTURE_EXPORT FakeVideoCaptureDeviceSettings {
   std::string device_id;
   FakeVideoCaptureDevice::DeliveryMode delivery_mode;
   VideoCaptureFormats supported_formats;
-  FakePhotoDeviceConfig photo_device_config;
 };
 
 // Implementation of VideoCaptureDeviceFactory that creates fake devices
@@ -32,18 +31,12 @@ struct CAPTURE_EXPORT FakeVideoCaptureDeviceSettings {
 class CAPTURE_EXPORT FakeVideoCaptureDeviceFactory
     : public VideoCaptureDeviceFactory {
  public:
-  static constexpr const char kDeviceConfigForGetPhotoStateFails[] =
-      "GetPhotoStateFails";
-  static constexpr const char kDeviceConfigForSetPhotoOptionsFails[] =
-      "SetPhotoOptionsFails";
-  static constexpr const char kDeviceConfigForTakePhotoFails[] =
-      "TakePhotoFails";
-
   FakeVideoCaptureDeviceFactory();
   ~FakeVideoCaptureDeviceFactory() override;
 
-  static std::unique_ptr<VideoCaptureDevice> CreateDeviceWithSettings(
-      const FakeVideoCaptureDeviceSettings& settings);
+  static std::unique_ptr<VideoCaptureDevice> CreateDeviceWithSupportedFormats(
+      FakeVideoCaptureDevice::DeliveryMode delivery_mode,
+      const VideoCaptureFormats& formats);
 
   static std::unique_ptr<VideoCaptureDevice> CreateDeviceWithDefaultResolutions(
       VideoPixelFormat pixel_format,
