801736cc09c851382120bf47a3a7ee98c05c6c75
Ricky Liang <jcliang@chromium.org>
2017-06-15 20:14:50 +0800

    RELAND: media: add video capture device for ARC++ camera HAL v3
    This is a re-land of https://codereview.chromium.org/2837273004/ which
    failed debug build on buildbot due to uninitialized virtual functions.
    
    Original CL description ------------------------------------------------
    
    media: add video capture device for ARC++ camera HAL v3
    
    This CL adds VideoCaptureDevice and VideoCaptureDeviceFactory
    for the ARC++ camera HAL v3. The VCD and VCD factory talk to
    the HAL adapter process on Chrome OS throgh Mojo IPC to access
    the camera service.
    
    BUG=b:32690003
    TEST=Make sure camera preview works in hangout.
    TEST=unit tests
    TBR=wuchengli@chromium.org,chfremer@chromium.org,posciak@chromium.org
    
    Change-Id: I302d9cd8104846d89a4a1ce4c57837513c06d85c
    Reviewed-on: https://chromium-review.googlesource.com/536584
    Commit-Queue: Ricky Liang <jcliang@chromium.org>
    Reviewed-by: Wu-cheng Li <wuchengli@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479697}

---
+++ media/capture/video/chromeos/pixel_format_utils.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MEDIA_CAPTURE_VIDEO_CHROMEOS_PIXEL_FORMAT_UTILS_H_
+#define MEDIA_CAPTURE_VIDEO_CHROMEOS_PIXEL_FORMAT_UTILS_H_
+
+#include "media/capture/video/chromeos/mojo/arc_camera3.mojom.h"
+#include "media/capture/video_capture_types.h"
+
+namespace media {
+
+// Converts the HAL pixel format |from| to Chromium pixel format.  Returns
+// PIXEL_FORMAT_UNKNOWN if |from| is not supported.
+VideoPixelFormat PixFormatHalToChromium(arc::mojom::HalPixelFormat from);
+
+// Converts the Chromium pixel format |from| to DRM pixel format.  Returns 0
+// if |from| is not supported.
+uint32_t PixFormatChromiumToDrm(VideoPixelFormat from);
+
+}  // namespace media
+
+#endif  // MEDIA_CAPTURE_VIDEO_CHROMEOS_PIXEL_FORMAT_UTILS_H_
