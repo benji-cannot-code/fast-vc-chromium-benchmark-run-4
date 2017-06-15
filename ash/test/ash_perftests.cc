7ecbbc68d250fc7dc8366b1c99628b49ac912092
wutao <wutao@chromium.org>
2017-06-15 12:50:03 -0700

    Add ash perftest on background blur filter.
    Add an ash perftest to evaluate the background blur filter efficiency.
    
    BUG=730841,646062,682082
    TEST=perftest
    
    Review-Url: https://codereview.chromium.org/2932503003
    Cr-Commit-Position: refs/heads/master@{#479797}

---
+++ ash/test/ash_perftests.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/test/ash_test_suite.h"
+#include "base/bind.h"
+#include "base/command_line.h"
+#include "base/test/launcher/unit_test_launcher.h"
+#include "mojo/edk/embedder/embedder.h"
+#include "ui/gl/gl_switches.h"
+
+namespace {
+
+int RunHelper(ash::test::AshTestSuite* test_suite) {
+  // In ash_perftests we want to use hardware GL. By adding this switch we can
+  // set |use_software_gl| to false in gl_surface_test_support.cc.
+  base::CommandLine::ForCurrentProcess()->AppendSwitch(
+      switches::kUseGpuInTests);
+  return test_suite->Run();
+}
+
+}  // namespace
+
+int main(int argc, char** argv) {
+  ash::test::AshTestSuite test_suite(argc, argv);
+
+  mojo::edk::Init();
+  return base::LaunchUnitTestsSerially(
+      argc, argv, base::Bind(&RunHelper, base::Unretained(&test_suite)));
+}
