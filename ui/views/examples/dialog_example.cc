95cb501f5d794b2653f8c4ec1ce1f57460c0f40d
ananta <ananta@chromium.org>
2017-06-14 23:03:14 -0700

    Remove references to ui/views/layout/layout_constants.h
    This patch removes layout_constants.h and inlines them in layout_provider.cc and
    chrome_layout_provider.cc. Last step of the set of changes which were around replacing
    these constants with their equivalents from the layout provider.
    
    BUG=691897
    TBR=sky
    
    Review-Url: https://codereview.chromium.org/2942453002
    Cr-Commit-Position: refs/heads/master@{#479616}

--- ui/views/examples/dialog_example.cc
+++ ui/views/examples/dialog_example.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/layout/grid_layout.h"
-#include "ui/views/layout/layout_constants.h"
 #include "ui/views/layout/layout_provider.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/window/dialog_client_view.h"
--- ui/views/layout/layout_provider.cc
+++ ui/views/layout/layout_provider.cc
@@ -8,14 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "ui/base/material_design/material_design_controller.h"
-#include "ui/views/layout/layout_constants.h"
 #include "ui/views/views_delegate.h"
 
 namespace views {
 
 namespace {
+
 LayoutProvider* g_layout_delegate = nullptr;
-}
+
+}  // namespace
 
 LayoutProvider::LayoutProvider() {
   g_layout_delegate = this;
@@ -63,7 +64,7 @@ gfx::Insets LayoutProvider::GetInsetsMetric(int metric) const {
                          dialog_contents.right());
     }
     case InsetsMetric::INSETS_VECTOR_IMAGE_BUTTON:
-      return gfx::Insets(kVectorButtonExtraTouchSize);
+      return gfx::Insets(4);
   }
   NOTREACHED();
   return gfx::Insets();
@@ -73,31 +74,31 @@ int LayoutProvider::GetDistanceMetric(int metric) const {
   DCHECK_GE(metric, VIEWS_INSETS_MAX);
   switch (metric) {
     case DISTANCE_BUBBLE_CONTENTS_HORIZONTAL_MARGIN:
-      return kPanelHorizMargin;
+      return 13;
     case DISTANCE_BUBBLE_CONTENTS_VERTICAL_MARGIN:
     case DISTANCE_DIALOG_CONTENTS_VERTICAL_MARGIN:
-      return kPanelVertMargin;
+      return 13;
     case DistanceMetric::DISTANCE_BUTTON_HORIZONTAL_PADDING:
-      return kButtonHorizontalPadding;
+      return 16;
     case DistanceMetric::DISTANCE_BUTTON_MAX_LINKABLE_WIDTH:
       return 0;
     case DistanceMetric::DISTANCE_CLOSE_BUTTON_MARGIN:
-      return kCloseButtonMargin;
+      return 7;
     case DistanceMetric::DISTANCE_RELATED_BUTTON_HORIZONTAL:
-      return kRelatedButtonHSpacing;
+      return 6;
     case DistanceMetric::DISTANCE_RELATED_CONTROL_HORIZONTAL:
-      return kRelatedControlHorizontalSpacing;
+      return 8;
     case DistanceMetric::DISTANCE_BUBBLE_BUTTON_TOP_MARGIN:
     case DistanceMetric::DISTANCE_RELATED_CONTROL_VERTICAL:
-      return kRelatedControlVerticalSpacing;
+      return 8;
     case DISTANCE_DIALOG_BUTTON_BOTTOM_MARGIN:
-      return views::kButtonVEdgeMarginNew;
+      return 20;
     case DistanceMetric::DISTANCE_DIALOG_BUTTON_MINIMUM_WIDTH:
-      return kDialogMinimumButtonWidth;
+      return 75;
     case DISTANCE_DIALOG_CONTENTS_HORIZONTAL_MARGIN:
-      return kButtonHEdgeMarginNew;
+      return 20;
     case DISTANCE_UNRELATED_CONTROL_VERTICAL:
-      return kUnrelatedControlVerticalSpacing;
+      return 20;
   }
   NOTREACHED();
   return 0;
