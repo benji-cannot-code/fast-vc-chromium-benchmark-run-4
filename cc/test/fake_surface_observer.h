b086c95cbc064e17bcd473fa91222260d5e0a78d
gklassen <gklassen@chromium.org>
2017-06-15 15:22:40 -0700

    Add SurfaceWillDraw notification
    Adds a SurfaceWillDraw notification to allow observers to monitor as
    surfaces are added to the next display frame.
    
    Viz will implement a hit-test component that will aggregate hit-test
    information published along with compositor frames.  The aggregated
    data will be made available for hit testing in a shared memory object.
    
    In order to ensure that the hit-test information matches the current
    information for the frame it needs to know which surfaces have been
    added to the current DisplayFrame.
    
    A method has been added to Surface observer so that observers will be
    notified as surfaces are added to the Display frame.  The notification
    is invoked during Surface Aggregation at the same place and following
    the same convention as the existing RunWillDrawCallback - the key
    difference being that this implementation allows one observer to
    monitor all surfaces without creating observers for each surface
    instance.
    
    BUG=732398
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2938833002
    Cr-Commit-Position: refs/heads/master@{#479865}

--- cc/test/fake_surface_observer.h
+++ cc/test/fake_surface_observer.h
@@ -25,6 +25,8 @@ class FakeSurfaceObserver : public SurfaceObserver {
 
   bool IsSurfaceDamaged(const SurfaceId& surface_id) const;
 
+  bool SurfaceWillDrawCalled(const SurfaceId& surface_id) const;
+
   const SurfaceId& last_created_surface_id() const {
     return last_created_surface_id_;
   }
@@ -42,10 +44,12 @@ class FakeSurfaceObserver : public SurfaceObserver {
   void OnSurfaceDestroyed(const SurfaceId& surface_id) override {}
   void OnSurfaceDamageExpected(const SurfaceId& surface_id,
                                const BeginFrameArgs& args) override {}
+  void OnSurfaceWillDraw(const SurfaceId& surface_id) override;
 
   bool damage_display_;
   BeginFrameAck last_ack_;
   base::flat_set<SurfaceId> damaged_surfaces_;
+  base::flat_set<SurfaceId> will_draw_surfaces_;
   SurfaceId last_created_surface_id_;
   SurfaceInfo last_surface_info_;
 };
