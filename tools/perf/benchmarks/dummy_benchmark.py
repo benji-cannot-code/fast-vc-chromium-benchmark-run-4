801bdf6a550ea7d537127b0a4f7d3a3d162eb29c
rnephew <rnephew@chromium.org>
2017-06-15 16:55:46 -0700

    [Telemetry] Move dummy benchmarks to story expectations.
    TBR=charliea@chromium.org
    
    Bug: 713222
    Change-Id: I85026fcef1e6d7675c5f68aa4c8ecd51893e6b08
    Reviewed-on: https://chromium-review.googlesource.com/538055
    Reviewed-by: rnephew <rnephew@chromium.org>
    Commit-Queue: rnephew <rnephew@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479936}

--- tools/perf/benchmarks/dummy_benchmark.py
+++ tools/perf/benchmarks/dummy_benchmark.py
@@ -19,7 +19,6 @@ from telemetry.page import legacy_page_test
 
 from page_sets import dummy_story_set
 
-
 class _DummyTest(legacy_page_test.LegacyPageTest):
 
   def __init__(self, avg, std):
@@ -52,6 +51,9 @@ class DummyBenchmarkOne(_DummyBenchmark):
   def Name(cls):
     return 'dummy_benchmark.stable_benchmark_1'
 
+  def GetExpectations(self):
+    return dummy_story_set.DummyStoryExpectations()
+
 
 @benchmark.Owner(emails=['nednguyen@google.com'])
 class DummyBenchmarkTwo(_DummyBenchmark):
@@ -63,3 +65,6 @@ class DummyBenchmarkTwo(_DummyBenchmark):
   @classmethod
   def Name(cls):
     return 'dummy_benchmark.noisy_benchmark_1'
+
+  def GetExpectations(self):
+    return dummy_story_set.DummyStoryExpectations()
