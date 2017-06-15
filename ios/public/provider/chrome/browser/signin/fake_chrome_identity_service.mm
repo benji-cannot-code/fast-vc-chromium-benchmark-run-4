61b530c3bee603b18809a34a975c3ce7212993e9
stkhapugin <stkhapugin@chromium.org>
2017-06-15 07:07:33 -0700

    Removes usage of signin APIs that return scoped_nsobjects.
    Returning scoped_nsobjects is against the philosophy of autoreleased
    objects being passed around. Refactoring methods and functions vending
    scoped_nsobjects allows for a cleaner transition to ARC later.
    
    This is CL 3/5.
    
    BUG=None
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2920853006
    Cr-Commit-Position: refs/heads/master@{#479691}

--- ios/public/provider/chrome/browser/signin/fake_chrome_identity_service.mm
+++ ios/public/provider/chrome/browser/signin/fake_chrome_identity_service.mm
@@ -143,8 +143,8 @@ FakeChromeIdentityService::GetInstanceFromChromeProvider() {
       ios::GetChromeBrowserProvider()->GetChromeIdentityService());
 }
 
-base::scoped_nsobject<UINavigationController>
-FakeChromeIdentityService::NewAccountDetails(
+UINavigationController*
+FakeChromeIdentityService::CreateAccountDetailsController(
     ChromeIdentity* identity,
     id<ChromeIdentityBrowserOpener> browser_opener) {
   base::scoped_nsobject<UIViewController> accountDetailsViewController(
@@ -155,13 +155,13 @@ FakeChromeIdentityService::NewAccountDetails(
   return navigationController;
 }
 
-base::scoped_nsobject<ChromeIdentityInteractionManager>
-FakeChromeIdentityService::NewChromeIdentityInteractionManager(
+ChromeIdentityInteractionManager*
+FakeChromeIdentityService::CreateChromeIdentityInteractionManager(
     ios::ChromeBrowserState* browser_state,
     id<ChromeIdentityInteractionManagerDelegate> delegate) const {
-  base::scoped_nsobject<ChromeIdentityInteractionManager> manager(
-      [[FakeChromeIdentityInteractionManager alloc] init]);
-  manager.get().delegate = delegate;
+  ChromeIdentityInteractionManager* manager =
+      [[[FakeChromeIdentityInteractionManager alloc] init] autorelease];
+  manager.delegate = delegate;
   return manager;
 }
 
