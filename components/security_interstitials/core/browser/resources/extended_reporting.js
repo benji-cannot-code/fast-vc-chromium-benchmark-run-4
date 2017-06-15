a88114ef2ddcf0f336b65343170bc7774103c190
Nate Fischer <ntfschr@chromium.org>
2017-06-15 12:26:28 -0700

    Interstitials: fix CMD_OPEN_WHITEPAPER
    Clicking the "system information and page content" link in interstitials
    was broken by a refactoring error in f9dfdb6d9d. CMD_* variables were
    moved to be properties on SecurityInterstitialCommandId, but only
    CMD_OPEN_WHITEPAPER was not updated correctly.
    
    Bug: 733277
    Test: browser_tests --gtest_filter=SafeBrowsingBlockingPageBrowserTest*
    Change-Id: I1b07f2843b7b8c367105a2280eebcae583fb9bf4
    Reviewed-on: https://chromium-review.googlesource.com/536435
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480091}

--- components/security_interstitials/core/browser/resources/extended_reporting.js
+++ components/security_interstitials/core/browser/resources/extended_reporting.js
@@ -36,7 +36,7 @@ function setupExtendedReportingCheckbox() {
 
   if ($('whitepaper-link')) {
     $('whitepaper-link').addEventListener('click', function(event) {
-      sendCommand(CMD_OPEN_WHITEPAPER);
+      sendCommand(SecurityInterstitialCommandId.CMD_OPEN_WHITEPAPER);
     });
   }
 
