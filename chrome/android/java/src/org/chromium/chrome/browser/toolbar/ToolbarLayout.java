d53c97a498f4fe26129dd77639ef36259b307565
Angela S <angelashao@google.com>
2017-06-15 10:02:12 -0700

    chrome/android: Fix CustomTabToolbar crash with Chrome home and data saver
    When Chrome home and In-Product Help Demo Mode "Enabled
    IPH_DataSaverDetail" are both enabled, crash happens when loading a
    custom tab from a custom tab client because data saver footer is called
    to show. This patch checks that Chrome home is not enabled before
    showing data saver footer and tracking the event that the footer is
    shown when the menu button opens.
    
    This patch also adds an early return in
    ToolbarLayout#setMenuButtonHighlightDrawable if inflation didn't complete.
    When inflation completes, it calls that method to set it as it would have
    if inflation finished before it was called.
    
    
    BUG=732338
    
    Change-Id: I1b0b667339e4f47a513cfb6cc93429bbeff5fec7
    Reviewed-on: https://chromium-review.googlesource.com/534896
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Angela Shao <angelashao@google.com>
    Cr-Commit-Position: refs/heads/master@{#479802}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarLayout.java
@@ -187,6 +187,10 @@ public abstract class ToolbarLayout extends FrameLayout implements Toolbar {
                 return false;
             }
         };
+
+        // Set menu button background in case it was previously called before inflation
+        // finished (i.e. mMenuButtonWrapper == null)
+        setMenuButtonHighlightDrawable(mHighlightingMenu);
     }
 
     /**
@@ -804,6 +808,9 @@ public abstract class ToolbarLayout extends FrameLayout implements Toolbar {
      * @param highlighting Whether or not the menu button should be highlighted.
      */
     protected void setMenuButtonHighlightDrawable(boolean highlighting) {
+        // Return if onFinishInflate didn't finish
+        if (mMenuButtonWrapper == null) return;
+
         if (highlighting) {
             if (mHighlightDrawable == null) {
                 mHighlightDrawable = PulseDrawable.createCircle(getContext());
