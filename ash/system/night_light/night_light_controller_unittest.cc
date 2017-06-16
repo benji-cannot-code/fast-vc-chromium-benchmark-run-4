418e4609449a205e554e6a25f4783b54b428f634
afakhry <afakhry@chromium.org>
2017-06-16 13:22:07 -0700

    [Night Light] CL7: Flip the Night Light flag
    Make Night Light enabled by default.
    
    BUG=705816
    
    Review-Url: https://codereview.chromium.org/2942063002
    Cr-Commit-Position: refs/heads/master@{#480149}

--- ash/system/night_light/night_light_controller_unittest.cc
+++ ash/system/night_light/night_light_controller_unittest.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cmath>
 #include <limits>
 
-#include "ash/ash_switches.h"
 #include "ash/public/cpp/ash_pref_names.h"
 #include "ash/public/cpp/config.h"
 #include "ash/public/cpp/session_types.h"
@@ -19,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/test/test_shell_delegate.h"
 #include "base/bind.h"
 #include "base/callback_forward.h"
-#include "base/command_line.h"
 #include "base/macros.h"
 #include "components/prefs/testing_pref_service.h"
 #include "ui/compositor/layer.h"
@@ -128,10 +126,6 @@ class NightLightTest : public test::AshTestBase {
 
   // ash::test::AshTestBase:
   void SetUp() override {
-    // Explicitly enable the NightLight feature for the tests.
-    base::CommandLine::ForCurrentProcess()->AppendSwitch(
-        ash::switches::kAshEnableNightLight);
-
     test::AshTestBase::SetUp();
     CreateTestUserSessions();
     Shell::RegisterPrefs(user1_pref_service_.registry());
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -140,6 +140,9 @@ extern const char kDevtoolsExperimentsDescription[];
 extern const char kDisableAudioForDesktopShareName[];
 extern const char kDisableAudioForDesktopShareDescription[];
 
+extern const char kDisableNightLightName[];
+extern const char kDisableNightLightDescription[];
+
 extern const char kDisableTabForDesktopShareName[];
 extern const char kDisableTabForDesktopShareDescription[];
 
@@ -236,9 +239,6 @@ extern const char kEnableMidiManagerDynamicInstantiationDescription[];
 
 extern const char kEnableNavigationTracingName[];
 extern const char kEnableNavigationTracingDescription[];
-
-extern const char kEnableNightLightName[];
-extern const char kEnableNightLightDescription[];
 
 extern const char kEnablePictureInPictureName[];
 extern const char kEnablePictureInPictureDescription[];
