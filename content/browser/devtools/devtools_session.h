2576959b379fec721a4230b56ab5aaa88a5e1015
dgozman <dgozman@chromium.org>
2017-06-16 14:12:41 -0700

    [DevTools] Make DevToolsSession own it's DevToolsMessageChunkProcessor
    Session now handles the state cookie and messages waiting for response.
    This will also help with multiple sessions.
    
    BUG=590878
    
    Review-Url: https://codereview.chromium.org/2933243002
    Cr-Commit-Position: refs/heads/master@{#480170}

--- content/browser/devtools/devtools_session.h
+++ content/browser/devtools/devtools_session.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_DEVTOOLS_DEVTOOLS_SESSION_H_
 #define CONTENT_BROWSER_DEVTOOLS_DEVTOOLS_SESSION_H_
 
+#include <map>
+
 #include "base/containers/flat_map.h"
 #include "base/memory/weak_ptr.h"
 #include "base/values.h"
@@ -30,10 +32,19 @@ class DevToolsSession : public protocol::FrontendChannel {
   void SetRenderFrameHost(RenderFrameHostImpl* host);
   void SetFallThroughForNotFound(bool value);
 
+  struct Message {
+    std::string method;
+    std::string message;
+  };
+  using MessageByCallId = std::map<int, Message>;
+  MessageByCallId& waiting_messages() { return waiting_for_response_messages_; }
+  const std::string& state_cookie() { return chunk_processor_.state_cookie(); }
+
   protocol::Response::Status Dispatch(
       const std::string& message,
       int* call_id,
       std::string* method);
+  bool ReceiveMessageChunk(const DevToolsMessageChunk& chunk);
 
   // Only used by DevToolsAgentHostImpl.
   DevToolsAgentHostClient* client() const { return client_; }
@@ -54,7 +65,9 @@ class DevToolsSession : public protocol::FrontendChannel {
   }
 
  private:
-  void sendResponse(std::unique_ptr<base::DictionaryValue> response);
+  void SendMessageToClient(int session_id, const std::string& message);
+  void SendResponse(std::unique_ptr<base::DictionaryValue> response);
+
   // protocol::FrontendChannel implementation.
   void sendProtocolResponse(
       int call_id,
@@ -70,6 +83,10 @@ class DevToolsSession : public protocol::FrontendChannel {
       handlers_;
   RenderFrameHostImpl* host_;
   std::unique_ptr<protocol::UberDispatcher> dispatcher_;
+  // Chunk processor's state cookie always corresponds to a state before
+  // any of the waiting for response messages have been handled.
+  DevToolsMessageChunkProcessor chunk_processor_;
+  MessageByCallId waiting_for_response_messages_;
 
   base::WeakPtrFactory<DevToolsSession> weak_factory_;
 };
