8c229a58c6794009455492e86e43349294a7a3ad
Hiroki Nakagawa <nhiroki@chromium.org>
2017-06-16 10:37:34 +0900

    Worklet: Merge ThreadedWorklet into Worklet
    As a cleanup, this CL merges ThreadedWorklet into Worklet.
    
    Bonus: This enables Worklet to transparently handle messaging proxies to
    MainThreadWorkletGlobalScope and ThreadedWorkletGlobalScope. This would be
    useful when we support ThreadedPaintWorklet and MainThreadAnimationWorklet
    later.
    
    Bug: 727194
    Change-Id: I090597616b4e7dd7bea82b4111de5910857da306
    Reviewed-on: https://chromium-review.googlesource.com/518643
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479988}

--- third_party/WebKit/Source/modules/csspaint/PaintWorkletGlobalScopeProxy.cpp
+++ third_party/WebKit/Source/modules/csspaint/PaintWorkletGlobalScopeProxy.cpp
@@ -41,6 +41,11 @@ void PaintWorkletGlobalScopeProxy::FetchAndInvokeScript(
                                       pending_tasks);
 }
 
+void PaintWorkletGlobalScopeProxy::WorkletObjectDestroyed() {
+  DCHECK(IsMainThread());
+  // Do nothing.
+}
+
 void PaintWorkletGlobalScopeProxy::TerminateWorkletGlobalScope() {
   DCHECK(IsMainThread());
   global_scope_->Terminate();
--- third_party/WebKit/Source/modules/webaudio/AudioWorklet.h
+++ third_party/WebKit/Source/modules/webaudio/AudioWorklet.h
@@ -6,37 +6,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef AudioWorklet_h
 #define AudioWorklet_h
 
-#include "core/workers/ThreadedWorklet.h"
+#include "core/workers/Worklet.h"
 #include "modules/ModulesExport.h"
 #include "platform/heap/Handle.h"
 
 namespace blink {
 
 class LocalFrame;
-class ThreadedWorkletMessagingProxy;
-class WorkletGlobalScopeProxy;
 
-class MODULES_EXPORT AudioWorklet final : public ThreadedWorklet {
-  // Eager finalization is needed to notify parent object destruction of the
-  // GC-managed messaging proxy and to initiate worklet termination.
-  EAGERLY_FINALIZE();
+class MODULES_EXPORT AudioWorklet final : public Worklet {
   WTF_MAKE_NONCOPYABLE(AudioWorklet);
 
  public:
   static AudioWorklet* Create(LocalFrame*);
   ~AudioWorklet() override;
 
-  void Initialize() final;
-  bool IsInitialized() const final;
-
-  WorkletGlobalScopeProxy* GetWorkletGlobalScopeProxy() const final;
-
   DECLARE_VIRTUAL_TRACE();
 
  private:
   explicit AudioWorklet(LocalFrame*);
 
-  Member<ThreadedWorkletMessagingProxy> worklet_messaging_proxy_;
+  // Implements Worklet.
+  bool NeedsToCreateGlobalScope() final;
+  WorkletGlobalScopeProxy* CreateGlobalScope() final;
 };
 
 }  // namespace blink
