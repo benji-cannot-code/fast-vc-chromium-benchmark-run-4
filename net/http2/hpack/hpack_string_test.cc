f8df5236e4a7dc8cef128796d08f5cbc275af480
Dianna Hu <diannahu@chromium.org>
2017-06-16 16:50:39 -0400

    Avoid implicit conversion operator to StringPiece.
    This CL lands server change 158769050 by rogeeff.
    
    BUG=488484
    
    Change-Id: I4085be6dbf5152bee00ae5d2978d8b01437f39b0
    Reviewed-on: https://chromium-review.googlesource.com/538934
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Reviewed-by: Bence Béky <bnc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480434}

--- net/http2/hpack/hpack_string_test.cc
+++ net/http2/hpack/hpack_string_test.cc
@@ -36,7 +36,7 @@ class HpackStringTest : public ::testing::Test {
     VERIFY_NE(*actual, not_expected_ptr);
     VERIFY_NE(*actual, not_expected_sp);
     VERIFY_NE(*actual, not_expected_str);
-    VERIFY_NE(static_cast<StringPiece>(*actual), not_expected_sp);
+    VERIFY_NE(actual->ToStringPiece(), not_expected_sp);
 
     if (!(not_expected_ptr != *actual)) {
       return AssertionFailure();
@@ -47,7 +47,7 @@ class HpackStringTest : public ::testing::Test {
     if (!(not_expected_str != *actual)) {
       return AssertionFailure();
     }
-    if (!(not_expected_sp != static_cast<StringPiece>(*actual))) {
+    if (!(not_expected_sp != actual->ToStringPiece())) {
       return AssertionFailure();
     }
 
@@ -63,7 +63,7 @@ class HpackStringTest : public ::testing::Test {
     VERIFY_EQ(*actual, expected_ptr);
     VERIFY_EQ(*actual, expected_sp);
     VERIFY_EQ(*actual, expected_str);
-    VERIFY_EQ(static_cast<StringPiece>(*actual), expected_sp);
+    VERIFY_EQ(actual->ToStringPiece(), expected_sp);
 
     if (!(expected_sp == *actual)) {
       return AssertionFailure();
@@ -74,7 +74,7 @@ class HpackStringTest : public ::testing::Test {
     if (!(expected_str == *actual)) {
       return AssertionFailure();
     }
-    if (!(expected_sp == static_cast<StringPiece>(*actual))) {
+    if (!(expected_sp == actual->ToStringPiece())) {
       return AssertionFailure();
     }
 
