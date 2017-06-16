559ee5861f4bbe84559a24fc99609489ce473b43
findit-for-me <findit-for-me@appspot.gserviceaccount.com>
2017-06-15 17:06:23 -0700

    Revert of Added HALF, FULLSCREEN_ALL_APPS, and FULLSCREEN_SEARCH. (patchset #8 id:330001 of https://codereview.chromium.org/2939693004/ )
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 479851 as the
    culprit for failures in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2ExMjAxY2ViMTJiZmVkZjUxY2IxYzBlMDE4NzliZDM1MjFiYTg4YTcM
    
    Original issue's description:
    > Added HALF, FULLSCREEN_ALL_APPS, and FULLSCREEN_SEARCH.
    >
    > |HALF| - Half App List. This mode is
    > entered when the user types into the search box from
    > peeking mode.
    >
    > |FULLSCREEN_ALL_APPS| - Fullscreen app list that
    > shows all apps. Entered by default in maximize and
    > side shelf modes. Also entered with an upward swipe
    > from |PEEKING|.
    >
    > |FULLSCREEN_SEARCH| -  Fullscreen app list that
    > shows search results. Entered from
    > FULLSCREEN_ALL_APPS after text is entered in the
    > search box, or from upward swipe from |HALF|.
    >
    > Please note: all Mash support will come post 61 as
    > per crbug.com/726838
    >
    > BUG=731892
    >
    > Review-Url: https://codereview.chromium.org/2939693004
    > Cr-Commit-Position: refs/heads/master@{#479851}
    > Committed: https://chromium.googlesource.com/chromium/src/+/a1201ceb12bfedf51cb1c0e01879bd3521ba88a7
    
    TBR=vadimt@chromium.org,xiyuan@chromium.org,oshima@chromium.org,newcomer@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=731892
    
    Review-Url: https://codereview.chromium.org/2938203004
    Cr-Commit-Position: refs/heads/master@{#479883}

--- ui/app_list/views/search_box_view.cc
+++ ui/app_list/views/search_box_view.cc
@@ -55,6 +55,7 @@ constexpr SkColor kHintTextColor = SkColorSetARGBMacro(0xFF, 0xA0, 0xA0, 0xA0);
 
 constexpr int kBackgroundBorderCornerRadius = 2;
 constexpr int kBackgroundBorderCornerRadiusFullscreen = 24;
+
 constexpr int kGoogleIconSize = 24;
 constexpr int kMicIconSize = 24;
 
@@ -368,8 +369,11 @@ void SearchBoxView::ContentsChanged(views::Textfield* sender,
   UpdateModel();
   view_delegate_->AutoLaunchCanceled();
   NotifyQueryChanged();
-  if (is_fullscreen_app_list_enabled_)
-    app_list_view_->SetStateFromSearchBoxView(search_box_->text().empty());
+
+  if (is_fullscreen_app_list_enabled_ && !app_list_view_->is_fullscreen()) {
+    // If the app list is in the peeking state, switch it to fullscreen.
+    app_list_view_->SetState(AppListView::FULLSCREEN);
+  }
 }
 
 bool SearchBoxView::HandleKeyEvent(views::Textfield* sender,
