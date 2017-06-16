bae916c167f64831cadf9f1034ffea7add021454
Max Morin <maxmorin@chromium.org>
2017-06-17 00:19:26 +0200

    Reduce AudioDeviceThread priority on Chrome OS.
    Reduce the priority of AudioDeviceThread on two-core Chrome OS machines to avoid priority inversion issues.
    
    BUG=710245
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ia8bff7fa57faa68f43294cc583cfec38a355b49f
    Reviewed-on: https://chromium-review.googlesource.com/535560
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Max Morin <maxmorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480398}

--- media/audio/audio_device_thread.cc
+++ media/audio/audio_device_thread.cc
@@ -8,6 +8,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <limits>
 
 #include "base/logging.h"
+#include "base/sys_info.h"
+
+namespace {
+
+base::ThreadPriority GetAudioThreadPriority() {
+#if defined(OS_CHROMEOS)
+  // On Chrome OS, there are priority inversion issues with having realtime
+  // threads on systems with only two cores, see crbug.com/710245.
+  return base::SysInfo::NumberOfProcessors() > 2
+             ? base::ThreadPriority::REALTIME_AUDIO
+             : base::ThreadPriority::NORMAL;
+#else
+  return base::ThreadPriority::REALTIME_AUDIO;
+#endif
+}
+
+}  // namespace
 
 namespace media {
 
@@ -48,8 +65,8 @@ AudioDeviceThread::AudioDeviceThread(Callback* callback,
                                      base::SyncSocket::Handle socket,
                                      const char* thread_name)
     : callback_(callback), thread_name_(thread_name), socket_(socket) {
-  CHECK(base::PlatformThread::CreateWithPriority(
-      0, this, &thread_handle_, base::ThreadPriority::REALTIME_AUDIO));
+  CHECK(base::PlatformThread::CreateWithPriority(0, this, &thread_handle_,
+                                                 GetAudioThreadPriority()));
   DCHECK(!thread_handle_.is_null());
 }
 
