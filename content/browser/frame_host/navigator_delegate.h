b81e820eef5eaee0d21f6f520a57bd32f67cc5cd
creis <creis@chromium.org>
2017-06-16 12:12:11 -0700

    Don't show current RenderWidgetHostView while interstitial is showing.
    Also moves interstitial page tracking from RenderFrameHostManager to
    WebContents, since interstitial pages are not frame-specific. This was
    necessary for subframes to detect if an interstitial page is showing.
    
    BUG=729105
    TEST=See comment 13 of bug for repro steps
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2938313002
    Cr-Commit-Position: refs/heads/master@{#480117}

--- content/browser/frame_host/navigator_delegate.h
+++ content/browser/frame_host/navigator_delegate.h
@@ -128,6 +128,10 @@ class CONTENT_EXPORT NavigatorDelegate {
   // embedder and |nullptr| is returned.
   virtual std::unique_ptr<NavigationUIData> GetNavigationUIData(
       NavigationHandle* navigation_handle);
+
+  // Whether the delegate is displaying an interstitial page over the current
+  // page.
+  virtual bool ShowingInterstitialPage() const = 0;
 };
 
 }  // namspace content
