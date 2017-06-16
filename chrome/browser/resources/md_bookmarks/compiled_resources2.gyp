fd75b81af3a16f8dc4425396cce17f3531b22fda
tsergeant <tsergeant@chromium.org>
2017-06-15 20:44:35 -0700

    MD Bookmarks: Fix issue where keyboard shortcuts could fire incorrectly
    MD Bookmarks did not correctly distinguish between a keypress of 'Enter'
    vs 'Shift-Enter', meaning that someone trying to 'Open in new window'
    could instead 'Edit' or 'Open', depending on platform.
    
    This fixes the issue by moving shortcut handling from iron-a11y-keys
    to using the standard WebUI Command handlers.
    
    BUG=731531
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2939873004
    Cr-Commit-Position: refs/heads/master@{#479940}

--- chrome/browser/resources/md_bookmarks/compiled_resources2.gyp
+++ chrome/browser/resources/md_bookmarks/compiled_resources2.gyp
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         '<(DEPTH)/ui/webui/resources/cr_elements/cr_action_menu/compiled_resources2.gyp:cr_action_menu',
         '<(DEPTH)/ui/webui/resources/js/compiled_resources2.gyp:cr',
         '<(DEPTH)/ui/webui/resources/js/compiled_resources2.gyp:load_time_data',
+        '<(DEPTH)/ui/webui/resources/js/cr/ui/compiled_resources2.gyp:command',
         '<(EXTERNS_GYP):bookmark_manager_private',
         'edit_dialog',
         'store_client',
--- ui/webui/resources/js/cr/ui/command.js
+++ ui/webui/resources/js/cr/ui/command.js
@@ -66,6 +66,32 @@ cr.define('cr.ui', function() {
   };
 
   /**
+   * A list of keyboard shortcuts which all perform one command.
+   * @param {string} shortcuts Text-based representation of one or more keyboard
+   *     shortcuts, separated by spaces.
+   * @constructor
+   */
+  function KeyboardShortcutList(shortcuts) {
+    this.shortcuts_ = shortcuts.split(/\s+/).map(function(shortcut) {
+      return new KeyboardShortcut(shortcut);
+    });
+  }
+
+  KeyboardShortcutList.prototype = {
+    /**
+     * Returns true if any of the keyboard shortcuts in the list matches a
+     * keyboard event.
+     * @param {!Event} e
+     * @return {boolean}
+     */
+    matchesEvent: function(e) {
+      return this.shortcuts_.some(function(keyboardShortcut) {
+        return keyboardShortcut.matchesEvent(e);
+      });
+    },
+  };
+
+  /**
    * Creates a new command element.
    * @constructor
    * @extends {HTMLElement}
@@ -136,9 +162,7 @@ cr.define('cr.ui', function() {
     set shortcut(shortcut) {
       var oldShortcut = this.shortcut_;
       if (shortcut !== oldShortcut) {
-        this.keyboardShortcuts_ = shortcut.split(/\s+/).map(function(shortcut) {
-          return new KeyboardShortcut(shortcut);
-        });
+        this.keyboardShortcuts_ = new KeyboardShortcutList(shortcut);
 
         // Set this after the keyboardShortcuts_ since that might throw.
         this.shortcut_ = shortcut;
@@ -155,10 +179,7 @@ cr.define('cr.ui', function() {
     matchesEvent: function(e) {
       if (!this.keyboardShortcuts_)
         return false;
-
-      return this.keyboardShortcuts_.some(function(keyboardShortcut) {
-        return keyboardShortcut.matchesEvent(e);
-      });
+      return this.keyboardShortcuts_.matchesEvent(e);
     },
   };
 
@@ -324,5 +345,9 @@ cr.define('cr.ui', function() {
   };
 
   // Export
-  return {Command: Command, CanExecuteEvent: CanExecuteEvent};
+  return {
+    Command: Command,
+    CanExecuteEvent: CanExecuteEvent,
+    KeyboardShortcutList: KeyboardShortcutList,
+  };
 });
