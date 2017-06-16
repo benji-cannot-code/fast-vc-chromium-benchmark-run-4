4832c7131778907a15ea33cdc7007b403b7b3d48
raymes <raymes@chromium.org>
2017-06-16 04:10:15 -0700

    Only enable kUsePermissionManagerForMediaRequests when the PermissionRequestManager is used
    This adds a check which ensures that the PermissionManager is used to
    request permission for Media requests only when the
    PermissionRequestManager is enabled. If it's not enabled, the grouped
    request won't work correctly.
    
    BUG=596786
    
    Review-Url: https://codereview.chromium.org/2941943002
    Cr-Commit-Position: refs/heads/master@{#480017}

--- chrome/browser/media/webrtc/media_stream_devices_controller.cc
+++ chrome/browser/media/webrtc/media_stream_devices_controller.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/media/webrtc/media_stream_capture_indicator.h"
 #include "chrome/browser/media/webrtc/media_stream_device_permissions.h"
 #include "chrome/browser/permissions/permission_manager.h"
+#include "chrome/browser/permissions/permission_request_manager.h"
 #include "chrome/browser/permissions/permission_result.h"
 #include "chrome/browser/permissions/permission_uma_util.h"
 #include "chrome/browser/permissions/permission_util.h"
@@ -54,7 +55,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/android/content_view_core.h"
 #include "ui/android/window_android.h"
 #else  // !defined(OS_ANDROID)
-#include "chrome/browser/permissions/permission_request_manager.h"
 #include "ui/vector_icons/vector_icons.h"
 #endif
 
@@ -466,7 +466,8 @@ void MediaStreamDevicesController::RequestPermissionsWithDelegate(
     Profile* profile =
         Profile::FromBrowserContext(web_contents->GetBrowserContext());
     if (base::FeatureList::IsEnabled(
-            features::kUsePermissionManagerForMediaRequests)) {
+            features::kUsePermissionManagerForMediaRequests) &&
+        PermissionRequestManager::IsEnabled()) {
       std::vector<ContentSettingsType> content_settings_types;
 
       if (is_asking_for_audio)
