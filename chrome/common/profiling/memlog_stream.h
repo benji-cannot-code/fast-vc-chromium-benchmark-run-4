dfb566f51b7b4f5ebd295f617d5137e2096b7299
brettw <brettw@chromium.org>
2017-06-15 19:30:53 -0700

    Add pipe implementation for OOP memory logging
    These pipes are not currently hooked up.
    
    The sender pipe just connects to a known Windows named pipe and has a send capability. The receiver has a server that waits on Windows I/O completion ports for connections, then associates those connections with a receiver pipe.
    
    We will investigate converting this to a Mojo EDK pipe in a second phase.
    
    The pipes will need very different implementations per-platform, but we won't need to switch them. As a result, I put the Windows implementation in _win.* files, but didn't do any virtual base classes or anything and just named the classes without the Win suffix. In my previous implementation I did some more separation that I didn't find gave any more flexibility.
    
    BUG=733336
    
    Review-Url: https://codereview.chromium.org/2940003002
    Cr-Commit-Position: refs/heads/master@{#479927}

---
+++ chrome/common/profiling/memlog_stream.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_COMMON_PROFILING_MEMLOG_STREAM_H_
+#define CHROME_COMMON_PROFILING_MEMLOG_STREAM_H_
+
+#include "build/build_config.h"
+
+namespace profiling {
+
+#if defined(OS_WIN)
+// Prefix for pipe name for communicating between chrome processes and the
+// memlog process. The pipe ID is appended to this to get the pipe name.
+extern const wchar_t kWindowsPipePrefix[];
+#endif  // OS_WIN
+
+}  // namespace profiling
+
+#endif  // CHROME_COMMON_PROFILING_MEMLOG_STREAM_H_
