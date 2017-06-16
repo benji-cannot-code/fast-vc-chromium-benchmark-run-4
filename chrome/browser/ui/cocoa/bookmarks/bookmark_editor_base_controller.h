d731f7914b2436a529207c994625d4190c359cb0
spqchan <spqchan@chromium.org>
2017-06-16 14:36:24 -0700

    [Mac] Fix for Dialog Textfields
    Currently, the custom field editors in the
    BookmarkBubbleController and BookmarkEditorController
    are causing incorrect behaviors with the dialog
    textfields. Also, a zombie crash is still happening.
    
    To fix the behavior, don't shift the custom field
    editors to the autorelease pool and force the editors to
    resign the first responder.
    
    Bug: 731284
    Change-Id: I23ab024c9b5de2d8ddb6d654de676d3039c7f406
    Reviewed-on: https://chromium-review.googlesource.com/538954
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Sarah Chan <spqchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480512}

--- chrome/browser/ui/cocoa/bookmarks/bookmark_editor_base_controller.h
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_editor_base_controller.h
@@ -70,6 +70,8 @@ class BookmarkModel;
                      title:(const base::string16&)title
              configuration:(BookmarkEditor::Configuration)configuration;
 
+- (void)windowWillClose:(NSNotification*)notification;
+
 // Override to customize the touch bar.
 - (NSTouchBar*)makeTouchBar;
 
