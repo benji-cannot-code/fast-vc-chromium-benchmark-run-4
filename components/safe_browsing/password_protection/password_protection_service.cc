fbbbafe408c7487ba34cafab9c4905e29f542d45
Nathan Parker <nparker@chromium.org>
2017-06-16 17:29:57 -0700

    Switch to async whitelist checking in PasswordProtectionRequest
    This adds a new WhitelistCheckerClient class that lets the caller
    provide a callback rather than extending
    SafeBrowsingDatabaseManager::Client, and makes ownership a bit simpler.
    
    Bug: 714300
    Change-Id: I1005debdbd9afe6314962537b48ce5f25c521614
    Reviewed-on: https://chromium-review.googlesource.com/524694
    Commit-Queue: Nathan Parker <nparker@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480271}

--- components/safe_browsing/password_protection/password_protection_service.cc
+++ components/safe_browsing/password_protection/password_protection_service.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/password_protection/password_protection_request.h"
 #include "components/safe_browsing_db/database_manager.h"
 #include "components/safe_browsing_db/v4_protocol_manager_util.h"
+#include "components/safe_browsing_db/whitelist_checker_client.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/web_contents.h"
 #include "google_apis/google_api_keys.h"
@@ -104,14 +105,6 @@ PasswordProtectionService::~PasswordProtectionService() {
   CancelPendingRequests();
   history_service_observer_.RemoveAll();
   weak_factory_.InvalidateWeakPtrs();
-}
-
-void PasswordProtectionService::CheckCsdWhitelistOnIOThread(
-    const GURL& url,
-    bool* check_result) {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
-  *check_result =
-      url.is_valid() ? database_manager_->MatchCsdWhitelistUrl(url) : true;
 }
 
 bool PasswordProtectionService::CanGetReputationOfURL(const GURL& url) {
