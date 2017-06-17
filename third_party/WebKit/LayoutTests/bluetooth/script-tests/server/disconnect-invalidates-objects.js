5e93151a94d87e7df0cff1454f16b01a7b53d8ba
Daniel Cheng <dcheng@chromium.org>
2017-06-17 07:28:08 +0000

    Revert "bluetooth: Refactor bluetooth-helpers and tests"
    This reverts commit c0181afc21c3be53b827da7104eef59a0ed9c4e4.
    
    Reason for revert:
    https://chromium-review.googlesource.com/c/527719/ needs to be
    reverted to fix MSAN bots, and this CL depends on it =(
    
    Original change's description:
    > bluetooth: Refactor bluetooth-helpers and tests
    > 
    > 1. Changes method to return an object instead of an array. This will make it
    >    easier to grab the desired fake attributes from helper methods e.g.:
    > 
    >    function getTwoBatteryLevelCharacteristicsDevice() {
    >      return getHealthThermometerDevice()
    >        .then(({device, fake_peripheral, fake_health_thermometer}) => {
    >          return fake_health_thermometer.addFakeCharacteristic(...);
    >        });
    >     }
    > 
    > 2. Introduce getConnectedHealthThermometerDevice(). Similar to
    >    getHealthThermometerDevice except the GATT discovery
    >    response has not been set yet so more attributes can still be added.
    > 
    > 3. Introduce getTwoHealthThermometerServicesDevice(). There is no order
    >    guarantee when calling getPrimaryServices so test that need to
    >    interact with a specific service might break.
    > 
    > Bug: 
    > Change-Id: I7ea23cf370521cf374652c473010dea48da47eee
    > Reviewed-on: https://chromium-review.googlesource.com/536379
    > Reviewed-by: Vincent Scheib <scheib@chromium.org>
    > Commit-Queue: Vincent Scheib <scheib@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#480137}
    
    TBR=scheib@chromium.org,ortuno@chromium.org
    
    Change-Id: If40b85b173f204aa367ec8cbfc71fdb3ef7cc993
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: Change-Id: I7ea23cf370521cf374652c473010dea48da47eee
    Reviewed-on: https://chromium-review.googlesource.com/539095
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480279}

--- third_party/WebKit/LayoutTests/bluetooth/script-tests/server/disconnect-invalidates-objects.js
+++ third_party/WebKit/LayoutTests/bluetooth/script-tests/server/disconnect-invalidates-objects.js
@@ -4,7 +4,7 @@ promise_test(() => {
   let promise;
   return getHealthThermometerDevice({
       filters: [{services: ['health_thermometer']}]})
-    .then(({device}) => {
+    .then(([device]) => {
       return device.gatt.CALLS([
         getPrimaryService('health_thermometer')|
         getPrimaryServices()|
