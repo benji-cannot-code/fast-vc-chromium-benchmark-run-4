1cbbf28b6b80cea1bd4b32682ea6bc2604a5e1a3
chanpatorikku <chanpatorikku@gmail.com>
2017-06-16 17:48:10 -0700

    [NaCl SDK] Add comments in the implementation code of GoogleDriveFs
    PNaCl is on a path to deprecation, and GoogleDriveFs is compiled from PNaCl,
    so comments are added to the implementation code to inform that GoogleDriveFs
    is not going to be further developed.
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_nacl_sdk;master.tryserver.chromium.mac:mac_nacl_sdk;master.tryserver.chromium.win:win_nacl_sdk
    
    Review-Url: https://codereview.chromium.org/2937083003
    Cr-Commit-Position: refs/heads/master@{#480250}

--- native_client_sdk/src/libraries/nacl_io/googledrivefs/googledrivefs.cc
+++ native_client_sdk/src/libraries/nacl_io/googledrivefs/googledrivefs.cc
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace nacl_io {
 
+// This is not further implemented.
+// PNaCl is on a path to deprecation, and WebAssembly is
+// the focused technology.
+
 GoogleDriveFs::GoogleDriveFs() {}
 
 Error GoogleDriveFs::Init(const FsInitArgs& args) {
--- native_client_sdk/src/libraries/nacl_io/googledrivefs/googledrivefs_node.h
+++ native_client_sdk/src/libraries/nacl_io/googledrivefs/googledrivefs_node.h
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace nacl_io {
 
+// This is not further implemented.
+// PNaCl is on a path to deprecation, and WebAssembly is
+// the focused technology.
+
 class GoogleDriveFsNode : public Node {
  public:
   GoogleDriveFsNode(GoogleDriveFs* googledrivefs);
