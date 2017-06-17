2f007a391788dd9b5eef327d1286074651e65799
Ken Rockot <rockot@chromium.org>
2017-06-17 10:15:24 -0700

    Mojo C++ Bindings: Fix Buffer allocation failure logic
    Per discussion on https://chromium-review.googlesource.com/c/539097/
    
    BUG=None
    R=yzshen@chromium.org
    
    Change-Id: I0fb75a60dddaa4afd67fea9fdd7600b0399b3255
    Reviewed-on: https://chromium-review.googlesource.com/539076
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480310}

--- mojo/public/cpp/bindings/lib/buffer.cc
+++ mojo/public/cpp/bindings/lib/buffer.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/lib/buffer.h"
 
 #include "base/logging.h"
-#include "base/numerics/safe_math.h"
 #include "mojo/public/cpp/bindings/lib/bindings_internal.h"
 
 namespace mojo {
@@ -34,12 +33,10 @@ Buffer& Buffer::operator=(Buffer&& other) {
 
 void* Buffer::Allocate(size_t num_bytes) {
   const size_t block_start = cursor_;
-  auto safe_result = base::CheckedNumeric<uintptr_t>(cursor_);
-  safe_result += Align(num_bytes);
-  cursor_ = base::checked_cast<size_t>(safe_result.ValueOrDie());
-  if (cursor_ > size_) {
+  cursor_ += Align(num_bytes);
+  if (cursor_ > size_ || cursor_ < block_start) {
     NOTREACHED();
-    cursor_ -= base::checked_cast<size_t>(safe_result.ValueOrDie());
+    cursor_ = block_start;
     return nullptr;
   }
   DCHECK_LE(cursor_, size_);
