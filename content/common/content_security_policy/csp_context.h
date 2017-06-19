9950263843713171c8348fe223d0735c9a0a56e6
arthursonzogni <arthursonzogni@chromium.org>
2017-06-19 07:01:46 -0700

    CSP, PlzNavigate: make clear what happens with unique origins.
    This CL makes the decision that when the current origin is unique, no
    url can match 'self'. That was already the case before this CL, but now
    it is explicitly stated.
    
    It removes several methods and merges several attributes of CSPContext.
    The goal is to make it less easy to shoot ourself in the foot.
    
    A few tests are added too.
    
    BUG=692449, 694959
    
    Review-Url: https://codereview.chromium.org/2937503002
    Cr-Commit-Position: refs/heads/master@{#480427}

--- content/common/content_security_policy/csp_context.h
+++ content/common/content_security_policy/csp_context.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/optional.h"
 #include "content/common/content_export.h"
 #include "content/common/content_security_policy/content_security_policy.h"
 #include "content/common/content_security_policy_header.h"
@@ -60,14 +61,17 @@ class CONTENT_EXPORT CSPContext {
                                     GURL* new_url);
 
   void SetSelf(const url::Origin origin);
-  bool AllowSelf(const GURL& url);
-  bool ProtocolIsSelf(const GURL& url);
-  const std::string& GetSelfScheme();
+
+  // When a CSPSourceList contains 'self', the url is allowed when it match the
+  // CSPSource returned by this function.
+  // Sometimes there is no 'self' source. It means that the current origin is
+  // unique and no urls will match 'self' whatever they are.
+  // Note: When there is a 'self' source, its scheme is guaranteed to be
+  // non-empty.
+  const base::Optional<CSPSource>& self_source() { return self_source_; }
 
   virtual void ReportContentSecurityPolicyViolation(
       const CSPViolationParams& violation_params);
-
-  bool SelfSchemeShouldBypassCsp();
 
   void ResetContentSecurityPolicies() { policies_.clear(); }
   void AddContentSecurityPolicy(const ContentSecurityPolicy& policy) {
@@ -91,10 +95,7 @@ class CONTENT_EXPORT CSPContext {
       SourceLocation* source_location) const;
 
  private:
-  bool has_self_ = false;
-  std::string self_scheme_;
-  CSPSource self_source_;
-
+  base::Optional<CSPSource> self_source_;
   std::vector<ContentSecurityPolicy> policies_;
 
   DISALLOW_COPY_AND_ASSIGN(CSPContext);
