e7fa53f3ec975d1c0a254b3c0eb02c83b766a33f
Stuart Langley <slangley@chromium.org>
2017-06-20 06:59:17 +1000

    Move WebElementTest and WebDragDataTest from web/ -> core/exported/.
    Bug: 712963
    Change-Id: Ie305c85b440ef83a51f13c5c1d26ceae00158f38
    Reviewed-on: https://chromium-review.googlesource.com/540495
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Stuart Langley <slangley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480642}

--- third_party/WebKit/Source/core/exported/WebElementTest.cpp
+++ third_party/WebKit/Source/core/exported/WebElementTest.cpp
@@ -5,12 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "public/web/WebElement.h"
 
+#include <memory>
 #include "core/dom/Document.h"
 #include "core/dom/Element.h"
 #include "core/dom/shadow/ShadowRoot.h"
 #include "core/testing/DummyPageHolder.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include <memory>
 
 namespace blink {
 
