d7485b19367d344e8f48f83389921d94b52d42bb
jif <jif@chromium.org>
2017-06-19 03:25:40 -0700

    [iOS] Iteration on the new experimental keyboard accessory view.
    This CL iterates over the new keyboard accessory view:
    -Final assets were added.
    -The design was updated slightly (no buttons are centered aligned).
    -Limit the new keyboard accessory view to iPhones.
    
    BUG=642702
    
    Review-Url: https://codereview.chromium.org/2939093002
    Cr-Commit-Position: refs/heads/master@{#480388}

--- ios/chrome/browser/ui/toolbar/new_keyboard_accessory_view.h
+++ ios/chrome/browser/ui/toolbar/new_keyboard_accessory_view.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Accessory View above the keyboard.
 // Shows keys that are shortcuts to commonly used characters or strings,
 // and buttons to start Voice Search or a Camera Search.
+// Must only be used on iPhones as iPads have a different pattern regarding
+// keyboard shortcuts.
 @interface NewKeyboardAccessoryView
     : UIInputView<KeyboardAccessoryViewProtocol, UIInputViewAudioFeedback>
 
