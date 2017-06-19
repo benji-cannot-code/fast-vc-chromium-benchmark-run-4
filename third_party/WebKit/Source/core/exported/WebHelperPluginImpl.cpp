148d03167464814c09cdfb4ef9eb16497e189b81
Stuart Langley <slangley@chromium.org>
2017-06-20 06:42:09 +1000

    Move WebHelperPluginImpl.* from web/ to core/exported/.
    Bug: 712963
    Change-Id: I0c132097626890ef4746ba86150f34dc9eb69ac5
    Reviewed-on: https://chromium-review.googlesource.com/540455
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Stuart Langley <slangley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480621}

--- third_party/WebKit/Source/core/exported/WebHelperPluginImpl.cpp
+++ third_party/WebKit/Source/core/exported/WebHelperPluginImpl.cpp
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "web/WebHelperPluginImpl.h"
+#include "core/exported/WebHelperPluginImpl.h"
 
 #include "core/exported/WebPluginContainerBase.h"
 #include "core/frame/LocalFrameClient.h"
