9e07ecf517129290482ac2ce3865315d3590e791
marq <marq@chromium.org>
2017-06-19 04:23:18 -0700

    [ObjC ARC] Converts ios/web/test:test_support to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2940303002
    Cr-Commit-Position: refs/heads/master@{#480403}

--- ios/web/test/crw_fake_web_controller_observer.mm
+++ ios/web/test/crw_fake_web_controller_observer.mm
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/web/test/crw_fake_web_controller_observer.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 @implementation CRWFakeWebControllerObserver
 
 @synthesize pageLoaded = _pageLoaded;
