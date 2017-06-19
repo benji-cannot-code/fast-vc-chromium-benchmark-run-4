d6eece01025735b98fa81e457fd6a10dd89ac9b9
moshayedi <moshayedi@chromium.org>
2017-06-19 12:38:13 -0700

    Remove InputMethodGlue.
    We no longer need this since we have moved event dispatching to input method to
    WindowEventDispatcher.
    
    BUG=NONE
    
    Review-Url: https://codereview.chromium.org/2948573002
    Cr-Commit-Position: refs/heads/master@{#480533}

--- ui/wm/test/wm_test_helper.h
+++ ui/wm/test/wm_test_helper.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window_tree_host.h"
 
 namespace aura {
-class InputMethodGlue;
 class Window;
 class WindowTreeHost;
 namespace client {
@@ -55,7 +54,6 @@ class WMTestHelper : public aura::client::WindowParentingClient {
  private:
   std::unique_ptr<WMState> wm_state_;
   std::unique_ptr<aura::WindowTreeHost> host_;
-  std::unique_ptr<aura::InputMethodGlue> input_method_glue_;
   std::unique_ptr<wm::CompoundEventFilter> root_window_event_filter_;
   std::unique_ptr<aura::client::DefaultCaptureClient> capture_client_;
   std::unique_ptr<aura::client::FocusClient> focus_client_;
