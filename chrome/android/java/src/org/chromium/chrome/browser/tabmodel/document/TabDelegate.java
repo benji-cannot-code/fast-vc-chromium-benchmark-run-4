c7e1003630bf939100975f823a1d5a8a29856ffe
johnme <johnme@chromium.org>
2017-06-19 03:46:21 -0700

    Revert "Redirects _blank and window.open() off-origin navigation from PWA to CCT."
    This reverts commit 4d9776c156c177107053872a437a9639d95a7160.
    
    Reason for revert: This caused each of the following tests:
    
    WebappNavigationTest#testNewTabLinkOpensInCct,
    WebappNavigationTest#testRegularLinkOffOriginInCctNoWebappThemeColor,
    WebappNavigationTest#testWindowOpenInCct and
    WebappNavigationTest#testWindowTopLocationOffOriginInCctAndWebappThemeColor
    
    to fail consistently on one or more of the following bots:
    
    http://build.chromium.org/p/chromium.android/builders/Lollipop%20Phone%20Tester
    http://build.chromium.org/p/chromium.android/builders/Lollipop%20Tablet%20Tester
    http://build.chromium.org/p/chromium.android/builders/Marshmallow%2064%20bit%20Tester
    http://build.chromium.org/p/chromium.swarm/builders/Android%20N5X%20Swarm
    
    and the first test also fails flakily on Lollipop Phone Tester and
    Marshmallow 64 bit Tester.
    
    Original change's description:
    > Redirects _blank and window.open() off-origin navigation from PWA to CCT.
    >
    > Custom Tab opened from a _blank link or a window.open() call is opened in
    > a separate Android task to mimic a new window/blank context of a web browser.
    > This is done in order to improve UX and bring more app-like feel to PWAs.
    > Transition to CCT for regular navigation is already done
    > (codereview.chromium.org/2829943002), this patch brings the same to _blank
    > and window.open().
    >
    > This requires some changes to Custom Tabs, as WebContents for the new window
    > is already created when we get to Java layer. I replicated the trick from
    > ChromeTabCreator with remembering WebContents on AsyncTabParamsManager
    > during the transition.
    >
    > Tests asserting that _blank and window.open() are opened in TabbedChrome has
    > been removed from WebappModeTest. Tests for transition to CCT has been added
    > to WebappNavigationTest.
    >
    > On Lollipop and above we maintain at most one CCT task per installed Webapp.
    > Below Lollipop there is no API I could use to enforce that I think, let me
    > know if you know the way.
    >
    > BUG=709889
    > Review-Url: https://codereview.chromium.org/2898373002
    > Cr-Commit-Position: refs/heads/master@{#480286}
    
    BUG=709889
    TBR=piotrs@chromium.org,yusufo@chromium.org
    NOTRY=true
    NOTREECHECKS=true
    
    Review-Url: https://codereview.chromium.org/2950483002
    Cr-Commit-Position: refs/heads/master@{#480393}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/document/TabDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/document/TabDelegate.java
@@ -141,13 +141,6 @@ public class TabDelegate extends TabCreator {
             intent.setComponent(componentName);
         }
 
-        addAsyncTabExtras(asyncParams, parentId, isChromeUI, assignedTabId, intent);
-
-        return intent;
-    }
-
-    protected final void addAsyncTabExtras(AsyncTabCreationParams asyncParams, int parentId,
-            boolean isChromeUI, int assignedTabId, Intent intent) {
         Map<String, String> extraHeaders = asyncParams.getLoadUrlParams().getExtraHeaders();
         if (extraHeaders != null && !extraHeaders.isEmpty()) {
             Bundle bundle = new Bundle();
@@ -179,6 +172,7 @@ public class TabDelegate extends TabCreator {
         }
 
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        return intent;
     }
 
     /**
