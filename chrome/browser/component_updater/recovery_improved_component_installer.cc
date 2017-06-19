17393d0fde0cd010c31c1e23d82a6268a6c5d311
Sorin Jianu <sorin@chromium.org>
2017-06-19 16:00:31 -0700

    Register the improved recovery component for per-user installs only.
    Bug: 687231
    Change-Id: I18a3dfd7828e309784e50f3bce64c87fe82ff142
    Reviewed-on: https://chromium-review.googlesource.com/540576
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480833}

--- chrome/browser/component_updater/recovery_improved_component_installer.cc
+++ chrome/browser/component_updater/recovery_improved_component_installer.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "build/build_config.h"
+#include "chrome/browser/component_updater/component_updater_utils.h"
 
 // This component is behind a Finch experiment. To enable the registration of
 // the component, run Chrome with --enable-features=ImprovedRecoveryComponent.
@@ -84,6 +85,12 @@ void RegisterRecoveryImprovedComponent(ComponentUpdateService* cus,
                                        PrefService* prefs) {
 #if defined(GOOGLE_CHROME_BUILD)
 #if defined(OS_WIN) || defined(OS_MACOSX)
+  // The improved recovery components requires elevation in the case where
+  // Chrome is installed per-machine. The elevation mechanism is not implemented
+  // yet; therefore, the component is not registered in this case.
+  if (!IsPerUserInstall())
+    return;
+
   DVLOG(1) << "Registering RecoveryImproved component.";
 
   std::unique_ptr<ComponentInstallerTraits> traits(
