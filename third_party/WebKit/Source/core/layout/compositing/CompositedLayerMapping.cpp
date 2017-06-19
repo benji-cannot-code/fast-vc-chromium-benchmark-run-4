0595bc74fa183de6a033cfa93196f7598df3f566
Hayley Ferr <hayleyferr@google.com>
2017-06-19 14:55:46 -0400

    Solution to incorrect hit testing of composited transparent overlay
    Previously, scrollers that were covered by a transparent overlay were
    being permitted to be scrolled with the mouse wheel. By adding the
    ShouldHitTest bit, blink now directly informs cc of whether the
    layers should be hit tested. MainGraphicsLayer and
    ScrollingContentsLayer result in the ShouldHitTest bit being set to 1.
    
    Bug: 724301
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ib087e247f749125a349f8e6c0f7174713dc98f2b
    Reviewed-on: https://chromium-review.googlesource.com/529426
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Hayley Ferr <hayleyferr@google.com>
    Cr-Commit-Position: refs/heads/master@{#480803}

--- third_party/WebKit/Source/core/layout/compositing/CompositedLayerMapping.cpp
+++ third_party/WebKit/Source/core/layout/compositing/CompositedLayerMapping.cpp
@@ -227,6 +227,7 @@ void CompositedLayerMapping::CreatePrimaryGraphicsLayer() {
       CreateGraphicsLayer(owning_layer_.GetCompositingReasons(),
                           owning_layer_.GetSquashingDisallowedReasons());
 
+  UpdateShouldHitTest(true);
   UpdateOpacity(GetLayoutObject().StyleRef());
   UpdateTransform(GetLayoutObject().StyleRef());
   UpdateFilters(GetLayoutObject().StyleRef());
@@ -251,6 +252,10 @@ void CompositedLayerMapping::DestroyGraphicsLayers() {
 
   scrolling_layer_ = nullptr;
   scrolling_contents_layer_ = nullptr;
+}
+
+void CompositedLayerMapping::UpdateShouldHitTest(const bool& should_hit_test) {
+  graphics_layer_->SetShouldHitTest(should_hit_test);
 }
 
 void CompositedLayerMapping::UpdateOpacity(const ComputedStyle& style) {
@@ -2381,6 +2386,7 @@ bool CompositedLayerMapping::UpdateScrollingLayers(
       // Inner layer which renders the content that scrolls.
       scrolling_contents_layer_ =
           CreateGraphicsLayer(kCompositingReasonLayerForScrollingContents);
+      scrolling_contents_layer_->SetShouldHitTest(true);
 
       AnimatingData data;
       GetAnimatingData(owning_layer_, data);
