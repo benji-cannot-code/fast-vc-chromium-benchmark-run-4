7d16d4c1e79a5ff7df1ee990c9fc75f864ab08ba
rnephew <rnephew@chromium.org>
2017-06-19 10:23:00 -0700

    [Telemetry] Move memory benchmarks to StoryExpectations.
    TBR=charliea@chromium.org
    
    Bug: 713222
    Change-Id: I08457ceba85b7449c7809bf06983b1c3da5965eb
    Reviewed-on: https://chromium-review.googlesource.com/539819
    Reviewed-by: rnephew <rnephew@chromium.org>
    Commit-Queue: rnephew <rnephew@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480542}

--- tools/perf/page_sets/trivial_sites.py
+++ tools/perf/page_sets/trivial_sites.py
@@ -159,3 +159,9 @@ class TrivialSitesStorySet(story.StorySet):
 class TrivialStoryExpectations(story.expectations.StoryExpectations):
   def SetExpectations(self):
     pass # No tests are disabled.
+
+
+class TrivialSitesMemoryStoryExpectations(story.expectations.StoryExpectations):
+  def SetExpectations(self):
+    pass # No tests are disabled.
+
