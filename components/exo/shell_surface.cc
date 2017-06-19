9d172daadce7f416e29709f48f9f389ce828c1bb
Peng Huang <penghuang@google.com>
2017-06-19 16:32:00 -0400

    exo: Use content_size() to replace window_->layer()->size() in surface
    In future, we may remove aura::Window in exo::Surface, so we should
    not get the size from layer of the aura::Window. Instead we should
    use exo::Surface::content_size().
    
    Bug: 731742
    Change-Id: I467ebfb8e6eb3120188f7ef487c9f44251259dd0
    Reviewed-on: https://chromium-review.googlesource.com/540435
    Reviewed-by: David Reveman <reveman@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480678}

--- components/exo/shell_surface.cc
+++ components/exo/shell_surface.cc
@@ -905,7 +905,7 @@ gfx::Size ShellSurface::CalculatePreferredSize() const {
   if (!geometry_.IsEmpty())
     return geometry_.size();
 
-  return surface_ ? surface_->window()->layer()->size() : gfx::Size();
+  return surface_ ? surface_->content_size() : gfx::Size();
 }
 
 gfx::Size ShellSurface::GetMinimumSize() const {
@@ -1414,8 +1414,7 @@ bool ShellSurface::IsResizing() const {
 
 gfx::Rect ShellSurface::GetVisibleBounds() const {
   // Use |geometry_| if set, otherwise use the visual bounds of the surface.
-  return geometry_.IsEmpty() ? gfx::Rect(surface_->window()->layer()->size())
-                             : geometry_;
+  return geometry_.IsEmpty() ? gfx::Rect(surface_->content_size()) : geometry_;
 }
 
 gfx::Point ShellSurface::GetSurfaceOrigin() const {
@@ -1540,7 +1539,7 @@ void ShellSurface::UpdateSurfaceBounds() {
 
   surface_->window()->SetBounds(
       gfx::Rect(GetSurfaceOrigin() + client_view_bounds.OffsetFromOrigin(),
-                surface_->window()->layer()->size()));
+                surface_->content_size()));
 }
 
 void ShellSurface::UpdateShadow() {
