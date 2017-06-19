6c206c88eb08f3ff4b9d828b27f565ce8b47d98c
Naina Raisinghani <nainar@chromium.org>
2017-06-19 11:53:03 +1000

    Make Box{Alignment,Pack,Orient,Lines} an enum class.
    This patch makes Box{Alignment,Pack,Orient,Lines} an enum class for type
    safety.
    Also renames them to have a prefix E to match other generated enums if
    they weren't already.
    
    BUG=684966
    
    Change-Id: I88422a720249bc84d1d6dee78fe8de5954a7f402
    Reviewed-on: https://chromium-review.googlesource.com/538441
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Commit-Queue: nainar <nainar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480363}

--- third_party/WebKit/Source/core/css/BasicShapeFunctions.cpp
+++ third_party/WebKit/Source/core/css/BasicShapeFunctions.cpp
@@ -85,7 +85,7 @@ static CSSValue* ValueForCenterCoordinate(
     return CSSValue::Create(center.length(), style.EffectiveZoom());
 
   CSSValueID keyword =
-      orientation == HORIZONTAL ? CSSValueRight : CSSValueBottom;
+      orientation == EBoxOrient::kHorizontal ? CSSValueRight : CSSValueBottom;
 
   return CSSValuePair::Create(
       CSSIdentifierValue::Create(keyword),
@@ -137,10 +137,10 @@ CSSValue* ValueForBasicShape(const ComputedStyle& style,
       CSSBasicShapeCircleValue* circle_value =
           CSSBasicShapeCircleValue::Create();
 
-      circle_value->SetCenterX(
-          ValueForCenterCoordinate(style, circle->CenterX(), HORIZONTAL));
-      circle_value->SetCenterY(
-          ValueForCenterCoordinate(style, circle->CenterY(), VERTICAL));
+      circle_value->SetCenterX(ValueForCenterCoordinate(
+          style, circle->CenterX(), EBoxOrient::kHorizontal));
+      circle_value->SetCenterY(ValueForCenterCoordinate(
+          style, circle->CenterY(), EBoxOrient::kVertical));
       circle_value->SetRadius(
           BasicShapeRadiusToCSSValue(style, circle->Radius()));
       return circle_value;
@@ -150,10 +150,10 @@ CSSValue* ValueForBasicShape(const ComputedStyle& style,
       CSSBasicShapeEllipseValue* ellipse_value =
           CSSBasicShapeEllipseValue::Create();
 
-      ellipse_value->SetCenterX(
-          ValueForCenterCoordinate(style, ellipse->CenterX(), HORIZONTAL));
-      ellipse_value->SetCenterY(
-          ValueForCenterCoordinate(style, ellipse->CenterY(), VERTICAL));
+      ellipse_value->SetCenterX(ValueForCenterCoordinate(
+          style, ellipse->CenterX(), EBoxOrient::kHorizontal));
+      ellipse_value->SetCenterY(ValueForCenterCoordinate(
+          style, ellipse->CenterY(), EBoxOrient::kVertical));
       ellipse_value->SetRadiusX(
           BasicShapeRadiusToCSSValue(style, ellipse->RadiusX()));
       ellipse_value->SetRadiusY(
--- third_party/WebKit/Source/core/layout/LayoutDeprecatedFlexibleBox.cpp
+++ third_party/WebKit/Source/core/layout/LayoutDeprecatedFlexibleBox.cpp
@@ -42,7 +42,7 @@ class FlexBoxIterator {
  public:
   FlexBoxIterator(LayoutDeprecatedFlexibleBox* parent)
       : box_(parent), largest_ordinal_(1) {
-    if (box_->Style()->BoxOrient() == HORIZONTAL &&
+    if (box_->Style()->BoxOrient() == EBoxOrient::kHorizontal &&
         !box_->Style()->IsLeftToRightDirection())
       forward_ = box_->Style()->BoxDirection() != EBoxDirection::kNormal;
     else
@@ -381,8 +381,8 @@ void LayoutDeprecatedFlexibleBox::UpdateBlockLayout(bool relayout_children) {
 
     if (previous_size != Size() ||
         (Parent()->IsDeprecatedFlexibleBox() &&
-         Parent()->Style()->BoxOrient() == HORIZONTAL &&
-         Parent()->Style()->BoxAlign() == BSTRETCH))
+         Parent()->Style()->BoxOrient() == EBoxOrient::kHorizontal &&
+         Parent()->Style()->BoxAlign() == EBoxAlignment::kStretch))
       relayout_children = true;
 
     SetHeight(LayoutUnit());
@@ -494,7 +494,7 @@ void LayoutDeprecatedFlexibleBox::LayoutHorizontalBox(bool relayout_children) {
       child->LayoutIfNeeded();
 
       // Update our height and overflow height.
-      if (Style()->BoxAlign() == BBASELINE) {
+      if (Style()->BoxAlign() == EBoxAlignment::kBaseline) {
         LayoutUnit ascent(child->FirstLineBoxBaseline());
         if (ascent == -1)
           ascent = child->Size().Height() + child->MarginBottom();
@@ -536,7 +536,7 @@ void LayoutDeprecatedFlexibleBox::LayoutHorizontalBox(bool relayout_children) {
       height_specified = true;
 
     // Now that our height is actually known, we can place our boxes.
-    stretching_children_ = (Style()->BoxAlign() == BSTRETCH);
+    stretching_children_ = (Style()->BoxAlign() == EBoxAlignment::kStretch);
     for (LayoutBox* child = iterator.First(); child; child = iterator.Next()) {
       if (child->IsOutOfFlowPositioned()) {
         child->ContainingBlock()->InsertPositionedObject(child);
@@ -582,14 +582,14 @@ void LayoutDeprecatedFlexibleBox::LayoutHorizontalBox(bool relayout_children) {
       x_pos += child->MarginLeft();
       LayoutUnit child_y = y_pos;
       switch (Style()->BoxAlign()) {
-        case BCENTER:
+        case EBoxAlignment::kCenter:
           child_y += child->MarginTop() +
                      ((ContentHeight() -
                        (child->Size().Height() + child->MarginHeight())) /
                       2)
                          .ClampNegativeToZero();
           break;
-        case BBASELINE: {
+        case EBoxAlignment::kBaseline: {
           LayoutUnit ascent(child->FirstLineBoxBaseline());
           if (ascent == -1)
             ascent = child->Size().Height() + child->MarginBottom();
@@ -597,7 +597,7 @@ void LayoutDeprecatedFlexibleBox::LayoutHorizontalBox(bool relayout_children) {
           child_y += child->MarginTop() + (max_ascent - ascent);
           break;
         }
-        case BEND:
+        case EBoxAlignment::kBend:
           child_y +=
               ContentHeight() - child->MarginBottom() - child->Size().Height();
           break;
@@ -725,12 +725,12 @@ void LayoutDeprecatedFlexibleBox::LayoutHorizontalBox(bool relayout_children) {
   } while (have_flex);
 
   if (remaining_space > 0 && ((Style()->IsLeftToRightDirection() &&
-                               Style()->BoxPack() != kBoxPackStart) ||
+                               Style()->BoxPack() != EBoxPack::kStart) ||
                               (!Style()->IsLeftToRightDirection() &&
-                               Style()->BoxPack() != kBoxPackEnd))) {
+                               Style()->BoxPack() != EBoxPack::kEnd))) {
     // Children must be repositioned.
     LayoutUnit offset;
-    if (Style()->BoxPack() == kBoxPackJustify) {
+    if (Style()->BoxPack() == EBoxPack::kJustify) {
       // Determine the total number of children.
       int total_children = 0;
       for (LayoutBox* child = iterator.First(); child;
@@ -764,7 +764,7 @@ void LayoutDeprecatedFlexibleBox::LayoutHorizontalBox(bool relayout_children) {
         }
       }
     } else {
-      if (Style()->BoxPack() == kBoxPackCenter)
+      if (Style()->BoxPack() == EBoxPack::kCenter)
         offset += remaining_space / 2;
       else  // END for LTR, START for RTL
         offset += remaining_space;
@@ -862,15 +862,16 @@ void LayoutDeprecatedFlexibleBox::LayoutVerticalBox(bool relayout_children) {
       // We can place the child now, using our value of box-align.
       LayoutUnit child_x = BorderLeft() + PaddingLeft();
       switch (Style()->BoxAlign()) {
-        case BCENTER:
-        case BBASELINE:  // Baseline just maps to center for vertical boxes
+        case EBoxAlignment::kCenter:
+        case EBoxAlignment::kBaseline:  // Baseline just maps to center for
+                                        // vertical boxes
           child_x += child->MarginLeft() +
                      ((ContentWidth() -
                        (child->Size().Width() + child->MarginWidth())) /
                       2)
                          .ClampNegativeToZero();
           break;
-        case BEND:
+        case EBoxAlignment::kBend:
           if (!Style()->IsLeftToRightDirection()) {
             child_x += child->MarginLeft();
           } else {
@@ -1031,10 +1032,10 @@ void LayoutDeprecatedFlexibleBox::LayoutVerticalBox(bool relayout_children) {
     }
   } while (have_flex);
 
-  if (Style()->BoxPack() != kBoxPackStart && remaining_space > 0) {
+  if (Style()->BoxPack() != EBoxPack::kStart && remaining_space > 0) {
     // Children must be repositioned.
     LayoutUnit offset;
-    if (Style()->BoxPack() == kBoxPackJustify) {
+    if (Style()->BoxPack() == EBoxPack::kJustify) {
       // Determine the total number of children.
       int total_children = 0;
       for (LayoutBox* child = iterator.First(); child;
@@ -1068,7 +1069,7 @@ void LayoutDeprecatedFlexibleBox::LayoutVerticalBox(bool relayout_children) {
         }
       }
     } else {
-      if (Style()->BoxPack() == kBoxPackCenter)
+      if (Style()->BoxPack() == EBoxPack::kCenter)
         offset += remaining_space / 2;
       else  // END
         offset += remaining_space;
