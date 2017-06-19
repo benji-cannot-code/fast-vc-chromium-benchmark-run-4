d266ecc38e967fe63015e11600b2396882651fcb
tbansal <tbansal@chromium.org>
2017-06-19 14:49:30 -0700

    NQE: use application layer throughput instead of transport layer
    BUG=733881
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_cronet_tester
    
    Review-Url: https://codereview.chromium.org/2946733002
    Cr-Commit-Position: refs/heads/master@{#480594}

--- net/nqe/throughput_analyzer.cc
+++ net/nqe/throughput_analyzer.cc
@@ -17,10 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request.h"
 #include "net/url_request/url_request_context.h"
 
-#if defined(OS_ANDROID)
-#include "net/android/traffic_stats.h"
-#endif  // OS_ANDROID
-
 namespace net {
 
 class HostResolver;
@@ -260,12 +256,6 @@ void ThroughputAnalyzer::SetUseSmallResponsesForTesting(
 
 int64_t ThroughputAnalyzer::GetBitsReceived() const {
   DCHECK(thread_checker_.CalledOnValidThread());
-
-#if defined(OS_ANDROID)
-  int64_t rx_bytes;
-  if (android::traffic_stats::GetCurrentUidRxBytes(&rx_bytes))
-    return static_cast<uint64_t>(rx_bytes * 8);
-#endif
   return NetworkActivityMonitor::GetInstance()->GetBytesReceived() * 8;
 }
 
