5650117a2f61b1cef4f4ed0c4cb7f4fcec6c305e
japhet <japhet@chromium.org>
2017-06-19 14:00:49 -0700

    Simplify core<->web layering for frame creation
    BUG=
    
    Review-Url: https://codereview.chromium.org/2932943002
    Cr-Commit-Position: refs/heads/master@{#480573}

--- third_party/WebKit/Source/core/layout/LayoutTestHelper.cpp
+++ third_party/WebKit/Source/core/layout/LayoutTestHelper.cpp
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 LocalFrame* SingleChildLocalFrameClient::CreateFrame(
-    const FrameLoadRequest&,
     const AtomicString& name,
     HTMLFrameOwnerElement* owner_element) {
   DCHECK(!child_) << "This test helper only supports one child frame.";
--- third_party/WebKit/Source/web/WebLocalFrameImpl.h
+++ third_party/WebKit/Source/web/WebLocalFrameImpl.h
@@ -76,7 +76,6 @@ class WebSpellCheckPanelHostClient;
 class WebView;
 class WebViewBase;
 enum class WebFrameLoadType;
-struct FrameLoadRequest;
 struct WebContentSecurityPolicyViolation;
 struct WebPrintParams;
 
@@ -348,8 +347,7 @@ class WEB_EXPORT WebLocalFrameImpl final
 
   ~WebLocalFrameImpl() override;
 
-  LocalFrame* CreateChildFrame(const FrameLoadRequest&,
-                               const AtomicString& name,
+  LocalFrame* CreateChildFrame(const AtomicString& name,
                                HTMLFrameOwnerElement*) override;
 
   void DidChangeContentsSize(const IntSize&);
