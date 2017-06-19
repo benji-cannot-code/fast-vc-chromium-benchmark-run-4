d44d06ddd5c0a746c061dc8891c39e6ecc6eb4dc
cjgrant <cjgrant@chromium.org>
2017-06-19 16:33:40 -0700

    VR: Support security chip text on URL bar.
    - Add rendering of "Secure", "Not secure" or "Dangerous" next to the
      security icon in the URL bar.
    - If present, include the text in the PageInfo clickable region.
    - Plumb malware status through to the URL bar to properly differentiate
      "Not secure" from "Dangerous", as is done by ToolbarModel.
    - Fix the broken HTTPS strike through from diagonal to horizontal, as
      a UX study determined is ideal.
    
    BUG=734206
    
    Review-Url: https://codereview.chromium.org/2946523002
    Cr-Commit-Position: refs/heads/master@{#480635}

--- chrome/browser/android/vr_shell/textures/url_bar_texture.h
+++ chrome/browser/android/vr_shell/textures/url_bar_texture.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/vr_shell/textures/ui_texture.h"
 #include "chrome/browser/android/vr_shell/ui_unsupported_mode.h"
 #include "components/security_state/core/security_state.h"
+#include "ui/gfx/geometry/rect_f.h"
 #include "url/gurl.h"
 
 namespace gfx {
@@ -42,11 +43,11 @@ class UrlBarTexture : public UiTexture {
 
   void SetURL(const GURL& gurl);
   void SetHistoryButtonsEnabled(bool can_go_back);
-  void SetSecurityLevel(security_state::SecurityLevel level);
+  void SetSecurityInfo(security_state::SecurityLevel level, bool malware);
 
   bool HitsBackButton(const gfx::PointF& position) const;
   bool HitsUrlBar(const gfx::PointF& position) const;
-  bool HitsSecurityIcon(const gfx::PointF& position) const;
+  bool HitsSecurityRegion(const gfx::PointF& position) const;
 
   void SetBackButtonHovered(bool hovered);
   void SetBackButtonPressed(bool pressed);
@@ -61,11 +62,10 @@ class UrlBarTexture : public UiTexture {
  private:
   void Draw(SkCanvas* canvas, const gfx::Size& texture_size) override;
   float ToPixels(float meters) const;
+  float ToMeters(float pixels) const;
   bool HitsTransparentRegion(const gfx::PointF& meters, bool left) const;
   void RenderUrl(const gfx::Size& texture_size, const gfx::Rect& bounds);
   void OnSetMode() override;
-  gfx::PointF SecurityIconPositionMeters() const;
-  gfx::PointF UrlBarPositionMeters() const;
   SkColor GetLeftCornerColor() const;
 
   gfx::SizeF size_;
@@ -74,13 +74,18 @@ class UrlBarTexture : public UiTexture {
   bool can_go_back_ = false;
 
   GURL gurl_;
-  security_state::SecurityLevel security_level_;
+  security_state::SecurityLevel security_level_ =
+      security_state::SecurityLevel::NONE;
+  bool malware_ = false;
 
   std::unique_ptr<gfx::RenderText> url_render_text_;
   GURL last_drawn_gurl_;
   bool has_back_button_ = true;
+  bool has_security_chip_ = true;
   security_state::SecurityLevel last_drawn_security_level_;
   base::Callback<void(UiUnsupportedMode)> failure_callback_;
+  gfx::RectF security_hit_region_ = gfx::RectF(0, 0, 0, 0);
+  gfx::RectF back_button_hit_region_ = gfx::RectF(0, 0, 0, 0);
 
   DISALLOW_COPY_AND_ASSIGN(UrlBarTexture);
 };
--- chrome/browser/android/vr_shell/ui_elements/url_bar.h
+++ chrome/browser/android/vr_shell/ui_elements/url_bar.h
@@ -40,7 +40,7 @@ class UrlBar : public TexturedElement {
 
   void SetHistoryButtonsEnabled(bool can_go_back);
   void SetURL(const GURL& gurl);
-  void SetSecurityLevel(security_state::SecurityLevel level);
+  void SetSecurityInfo(security_state::SecurityLevel level, bool malware);
 
  private:
   void UpdateTexture() override;
@@ -53,7 +53,7 @@ class UrlBar : public TexturedElement {
   bool enabled_ = false;
   bool can_go_back_ = false;
   bool down_ = false;
-  bool security_icon_down_ = false;
+  bool security_region_down_ = false;
   base::TimeTicks last_begin_frame_time_;
   base::TimeTicks last_update_time_;
 
