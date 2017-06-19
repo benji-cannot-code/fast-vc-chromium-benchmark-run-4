fa163a6b8f9399a3a3428d66aed2bdb90ae3e5cf
findit-for-me <findit-for-me@appspot.gserviceaccount.com>
2017-06-19 11:52:04 -0700

    Revert of Move the files related to Android<->Web credentials to a separate folder. (patchset #2 id:20001 of https://codereview.chromium.org/2949573002/ )
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 480510 as the
    culprit for failures in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2UyNWJmODA1ZGEyNTk5MDA1YWY2M2Y4YmZmYjk5MGNmOGE2OTNiZWUM
    
    Original issue's description:
    > Move the files related to Android<->Web credentials to a separate folder.
    >
    > The files create confusion with separate site<->site affiliation.
    >
    > BUG=630555
    > TBR=stevenjb@chromium.org,sdefresne@chromium.org
    >
    > Review-Url: https://codereview.chromium.org/2949573002
    > Cr-Commit-Position: refs/heads/master@{#480510}
    > Committed: https://chromium.googlesource.com/chromium/src/+/e25bf805da2599005af63f8bffb990cf8a693bee
    
    TBR=engedy@chromium.org,vabr@chromium.org,vasilii@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=630555
    
    Review-Url: https://codereview.chromium.org/2947683002
    Cr-Commit-Position: refs/heads/master@{#480516}

--- components/password_manager/core/browser/login_database.cc
+++ components/password_manager/core/browser/login_database.cc
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "components/autofill/core/common/password_form.h"
-#include "components/password_manager/core/browser/android_affiliation/affiliation_utils.h"
+#include "components/password_manager/core/browser/affiliation_utils.h"
 #include "components/password_manager/core/browser/password_manager_client.h"
 #include "components/password_manager/core/browser/password_manager_metrics_util.h"
 #include "components/password_manager/core/browser/password_manager_util.h"
