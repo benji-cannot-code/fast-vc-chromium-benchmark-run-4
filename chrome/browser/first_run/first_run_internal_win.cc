be5cb9dff0f56309817c968955d0070e54534d2d
Greg Thompson <grt@chromium.org>
2017-06-19 09:00:36 +0200

    Remove use of BlockingPool in first_run.
    BUG=667892
    R=fdoray@chromium.org
    
    Change-Id: I1e224442d01104af055a6c62c3116422a1172e28
    Reviewed-on: https://chromium-review.googlesource.com/538714
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: Francois Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480383}

--- chrome/browser/first_run/first_run_internal_win.cc
+++ chrome/browser/first_run/first_run_internal_win.cc
@@ -18,7 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/kill.h"
 #include "base/process/launch.h"
 #include "base/process/process.h"
-#include "base/threading/sequenced_worker_pool.h"
+#include "base/task_scheduler/post_task.h"
+#include "base/task_scheduler/task_traits.h"
 #include "base/time/time.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_paths.h"
@@ -108,15 +109,13 @@ namespace internal {
 void DoPostImportPlatformSpecificTasks(Profile* /* profile */) {
   // Trigger the Active Setup command for system-level Chromes to finish
   // configuring this user's install (e.g. per-user shortcuts).
-  // Delay the task slightly to give Chrome launch I/O priority while also
-  // making sure shortcuts are created promptly to avoid annoying the user by
-  // re-creating shortcuts they previously deleted.
-  static const int64_t kTiggerActiveSetupDelaySeconds = 5;
   if (!InstallUtil::IsPerUserInstall()) {
-    content::BrowserThread::GetBlockingPool()->PostDelayedTask(
+    content::BrowserThread::PostAfterStartupTask(
         FROM_HERE,
-        base::Bind(&InstallUtil::TriggerActiveSetupCommand),
-        base::TimeDelta::FromSeconds(kTiggerActiveSetupDelaySeconds));
+        base::CreateTaskRunnerWithTraits(
+            {base::MayBlock(), base::TaskPriority::BACKGROUND,
+             base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN}),
+        base::BindOnce(&InstallUtil::TriggerActiveSetupCommand));
   }
 }
 
