ea5beae809f7b4890bdd76f55ccc473b6d8a3423
vadimt <vadimt@chromium.org>
2017-06-19 15:50:23 -0700

    Making data source for AnswerCardSearchProvider mockable.
    This will enable unit-testing of AnswerCardSearchProvider, however the
    test itself will come in a separate CL.
    
    This separates the part dealing with WebView and WebContents in a
    AnswerCardWebContents class. It has an abstract parent
    AnswerCardContents, pointer to which is passed to
    AnswerCardSearchProvider constructor. The future test will use some mock
    implementation of AnswerCardContents that won’t depend on behavior of
    web loading and rendering, and can produce events for the search provider
    to test their various sequences.
    
    Bug=712331
    
    Review-Url: https://codereview.chromium.org/2947543002
    Cr-Commit-Position: refs/heads/master@{#480615}

--- chrome/browser/ui/app_list/search/answer_card/answer_card_search_provider.h
+++ chrome/browser/ui/app_list/search/answer_card/answer_card_search_provider.h
@@ -3,16 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_APP_LIST_SEARCH_ANSWER_CARD_SEARCH_PROVIDER_H_
-#define CHROME_BROWSER_UI_APP_LIST_SEARCH_ANSWER_CARD_SEARCH_PROVIDER_H_
+#ifndef CHROME_BROWSER_UI_APP_LIST_SEARCH_ANSWER_CARD_ANSWER_CARD_SEARCH_PROVIDER_H_
+#define CHROME_BROWSER_UI_APP_LIST_SEARCH_ANSWER_CARD_ANSWER_CARD_SEARCH_PROVIDER_H_
 
 #include <memory>
 #include <string>
 
 #include "base/time/time.h"
-#include "content/public/browser/web_contents_delegate.h"
-#include "content/public/browser/web_contents_observer.h"
+#include "chrome/browser/ui/app_list/search/answer_card/answer_card_contents.h"
 #include "ui/app_list/search_provider.h"
+#include "ui/gfx/geometry/size.h"
 #include "url/gurl.h"
 
 class AppListControllerDelegate;
@@ -26,48 +26,30 @@ namespace net {
 class HttpResponseHeaders;
 }
 
-namespace views {
-class View;
-class WebView;
-}
-
 namespace app_list {
 
 // Search provider for the answer card.
 class AnswerCardSearchProvider : public SearchProvider,
-                                 public content::WebContentsDelegate,
-                                 public content::WebContentsObserver {
+                                 public AnswerCardContents::Delegate {
  public:
   AnswerCardSearchProvider(Profile* profile,
                            app_list::AppListModel* model,
-                           AppListControllerDelegate* list_controller);
+                           AppListControllerDelegate* list_controller,
+                           std::unique_ptr<AnswerCardContents> contents);
 
   ~AnswerCardSearchProvider() override;
-
-  // Returns a pointer to the web view for the web contents managed by this
-  // class. The object is owned by this class and has property
-  // 'set_owned_by_client()' set.
-  views::View* web_view();
 
   // SearchProvider overrides:
   void Start(bool is_voice_query, const base::string16& query) override;
   void Stop() override {}
 
-  // content::WebContentsDelegate overrides:
-  void UpdatePreferredSize(content::WebContents* web_contents,
-                           const gfx::Size& pref_size) override;
-
+  // AnswerCardContents::Delegate overrides:
+  void UpdatePreferredSize(const gfx::Size& pref_size) override;
   content::WebContents* OpenURLFromTab(
-      content::WebContents* source,
       const content::OpenURLParams& params) override;
-
-  bool HandleContextMenu(const content::ContextMenuParams& params) override;
-
-  // content::WebContentsObserver overrides:
   void DidFinishNavigation(
       content::NavigationHandle* navigation_handle) override;
   void DidStopLoading() override;
-  void DidGetUserInteraction(const blink::WebInputEvent::Type type) override;
 
  private:
   bool IsCardSizeOk() const;
@@ -84,15 +66,12 @@ class AnswerCardSearchProvider : public SearchProvider,
   // Unowned pointer to app list controller.
   AppListControllerDelegate* const list_controller_;
 
-  // Web view for the web contents managed by this class.
-  const std::unique_ptr<views::WebView> web_view_;
+  // The source of answer card contents.
+  const std::unique_ptr<AnswerCardContents> contents_;
 
   // Whether have received a server response for the current query string, and
   // the response contains an answer.
   bool received_answer_ = false;
-
-  // Web contents managed by this class.
-  const std::unique_ptr<content::WebContents> web_contents_;
 
   // If valid, URL of the answer server. Otherwise, search answers are disabled.
   GURL answer_server_url_;
@@ -116,9 +95,12 @@ class AnswerCardSearchProvider : public SearchProvider,
   // Title of the result.
   std::string result_title_;
 
+  // Current preferred size of the contents.
+  gfx::Size preferred_size_;
+
   DISALLOW_COPY_AND_ASSIGN(AnswerCardSearchProvider);
 };
 
 }  // namespace app_list
 
-#endif  // CHROME_BROWSER_UI_APP_LIST_SEARCH_ANSWER_CARD_SEARCH_PROVIDER_H_
+#endif  // CHROME_BROWSER_UI_APP_LIST_SEARCH_ANSWER_CARD_ANSWER_CARD_SEARCH_PROVIDER_H_
