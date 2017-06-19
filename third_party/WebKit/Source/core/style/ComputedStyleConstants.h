1ce65f2e16572da5b763bd645abe449827aa168f
shend <shend@chromium.org>
2017-06-18 21:33:39 -0700

    Make EFlexDirection an enum class.
    This patch makes EFlexDirection an enum class for type safety.
    
    Bug: 684966
    Review-Url: https://codereview.chromium.org/2949453002
    Cr-Commit-Position: refs/heads/master@{#480348}

--- third_party/WebKit/Source/core/style/ComputedStyleConstants.h
+++ third_party/WebKit/Source/core/style/ComputedStyleConstants.h
@@ -158,12 +158,7 @@ enum EBoxLines { SINGLE, MULTIPLE };
 
 // CSS3 Flexbox Properties
 
-enum EFlexDirection {
-  kFlowRow,
-  kFlowRowReverse,
-  kFlowColumn,
-  kFlowColumnReverse
-};
+enum class EFlexDirection { kRow, kRowReverse, kColumn, kColumnReverse };
 enum EFlexWrap { kFlexNoWrap, kFlexWrap, kFlexWrapReverse };
 
 // CSS3 Image Values
