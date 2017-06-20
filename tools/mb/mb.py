28bbb257aedfad3067ebde9a533d690f256f28f3
pcc <pcc@chromium.org>
2017-06-20 15:11:41 -0700

    Make the linux_chromium_cfi_rel_ng trybot more useful.
    This change causes the trybot to:
    - use ThinLTO
    - produce detailed diagnostics
    - produce symbolized stack traces.
    The goal is to reduce cycle times on the bot, as well as provide more
    details when it fails so that its results are more actionable.
    
    Tested by patching a bad cast into one of the base_unittests and
    sending a swarming job as described here:
    http://dev.chromium.org/developers/testing/isolated-testing/for-swes#TOC-Run-a-test-built-locally-on-Swarming
    
    BUG=732652
    R=dpranke@chromium.org
    
    Review-Url: https://codereview.chromium.org/2938383004
    Cr-Commit-Position: refs/heads/master@{#480983}

--- tools/mb/mb.py
+++ tools/mb/mb.py
@@ -1063,6 +1063,7 @@ class MetaBuildWrapper(object):
     asan = 'is_asan=true' in vals['gn_args']
     msan = 'is_msan=true' in vals['gn_args']
     tsan = 'is_tsan=true' in vals['gn_args']
+    cfi_diag = 'use_cfi_diag=true' in vals['gn_args']
 
     test_type = isolate_map[target]['type']
 
@@ -1097,6 +1098,7 @@ class MetaBuildWrapper(object):
         '--asan=%d' % asan,
         '--msan=%d' % msan,
         '--tsan=%d' % tsan,
+        '--cfi-diag=%d' % cfi_diag,
       ]
     elif test_type in ('windowed_test_launcher', 'console_test_launcher'):
       extra_files = [
@@ -1110,6 +1112,7 @@ class MetaBuildWrapper(object):
           '--asan=%d' % asan,
           '--msan=%d' % msan,
           '--tsan=%d' % tsan,
+          '--cfi-diag=%d' % cfi_diag,
       ]
     elif test_type == 'script':
       extra_files = [
