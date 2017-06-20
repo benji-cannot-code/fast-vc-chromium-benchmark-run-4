530df09529ecb8a28fedb9dd02988bcf5b4841a1
ricea <ricea@chromium.org>
2017-06-20 05:48:50 -0700

    cpplint fixes for //net/websockets
    After this change
    
    > cpplint.py *.h *.cc
    
    in //net/websockets finds 0 errors.
    
    No functional changes.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2944833002
    Cr-Commit-Position: refs/heads/master@{#480796}

--- net/websockets/websocket_channel_test.cc
+++ net/websockets/websocket_channel_test.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <string.h>
 
+#include <algorithm>
 #include <iostream>
 #include <iterator>
 #include <memory>
@@ -380,7 +381,7 @@ template <size_t N>
 class EqualsFramesMatcher : public ::testing::MatcherInterface<
                                 std::vector<std::unique_ptr<WebSocketFrame>>*> {
  public:
-  EqualsFramesMatcher(const InitFrame (*expect_frames)[N])
+  explicit EqualsFramesMatcher(const InitFrame (*expect_frames)[N])
       : expect_frames_(expect_frames) {}
 
   virtual bool MatchAndExplain(
