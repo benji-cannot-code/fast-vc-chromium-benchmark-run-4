d8fd180f292ff72b036c1e6a05bda5ef2044b5ca
vasilii <vasilii@chromium.org>
2017-06-20 10:10:22 -0700

    Reland: Move the files related to Android <-> Web credentials to a separate folder.
    Reland https://codereview.chromium.org/2949573002/
    
    BUG=630555
    TBR=engedy@chromium.org
    
    Review-Url: https://codereview.chromium.org/2949793002
    Cr-Commit-Position: refs/heads/master@{#480867}

--- ios/chrome/browser/ui/settings/save_passwords_collection_view_controller.mm
+++ ios/chrome/browser/ui/settings/save_passwords_collection_view_controller.mm
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/password_form.h"
 #include "components/google/core/browser/google_util.h"
 #include "components/keyed_service/core/service_access_type.h"
-#include "components/password_manager/core/browser/affiliation_utils.h"
+#include "components/password_manager/core/browser/android_affiliation/affiliation_utils.h"
 #include "components/password_manager/core/browser/password_manager_constants.h"
 #include "components/password_manager/core/browser/password_store.h"
 #include "components/password_manager/core/browser/password_store_consumer.h"
