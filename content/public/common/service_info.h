330460134542bae2ecd403eeb09d2590779b8eda
mfomitchev <mfomitchev@chromium.org>
2017-06-20 10:53:12 -0700

    Add support for specifying message loop type and thread priority for content mojo services
    BUG=722527
    
    Review-Url: https://codereview.chromium.org/2952603002
    Cr-Commit-Position: refs/heads/master@{#480876}

--- content/public/common/service_info.h
+++ content/public/common/service_info.h
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/memory/ref_counted.h"
+#include "base/message_loop/message_loop.h"
 #include "base/single_thread_task_runner.h"
+#include "base/threading/platform_thread.h"
 #include "content/common/content_export.h"
 
 namespace service_manager {
@@ -44,6 +46,14 @@ struct CONTENT_EXPORT ServiceInfo {
   //
   // If |task_runner| is not null, this value is ignored.
   bool use_own_thread = false;
+
+  // If the service uses its own thread, this determines the type of the message
+  // loop used by the thread.
+  base::MessageLoop::Type message_loop_type = base::MessageLoop::TYPE_DEFAULT;
+
+  // If the service uses its own thread, this determines the priority of the
+  // thread.
+  base::ThreadPriority thread_priority = base::ThreadPriority::NORMAL;
 };
 
 }  // namespace content
