1162e16040a523fa462937e4b9c3e3ec02351499
Johan Tibell <tibell@chromium.org>
2017-06-20 14:53:11 +1000

    Pref service: remove obsolete PrefObserverStore todo
    Bug: 
    Change-Id: Iaf0b2eb29748ede22cfd2038f1cd894f571fa20e
    Reviewed-on: https://chromium-review.googlesource.com/541235
    Commit-Queue: Johan Tibell <tibell@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480748}

--- services/preferences/public/cpp/pref_store_client.h
+++ services/preferences/public/cpp/pref_store_client.h
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace prefs {
 
-// TODO(tibell): Make PrefObserverStore use PrefStoreClient as a base class.
-
 // An implementation of PrefStore which uses prefs::mojom::PrefStore as
 // the backing store of the preferences.
 //
