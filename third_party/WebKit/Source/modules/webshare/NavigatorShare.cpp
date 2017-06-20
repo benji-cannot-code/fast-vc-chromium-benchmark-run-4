38bdfa27f407a1eec3fbf9bed885325059812700
Matt Giuca <mgiuca@chromium.org>
2017-06-20 14:36:27 +1000

    navigator.share: Reject with TypeError if invalid url.
    This is a spec compliance issue. Previously just succeeded with an empty
    URL.
    
    Bug: 734486
    Change-Id: I8df989646227e9a75d8aaf807bf818ff971b878d
    Reviewed-on: https://chromium-review.googlesource.com/538385
    Commit-Queue: Matt Giuca <mgiuca@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480756}

--- third_party/WebKit/Source/modules/webshare/NavigatorShare.cpp
+++ third_party/WebKit/Source/modules/webshare/NavigatorShare.cpp
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/LocalFrame.h"
 #include "core/frame/Navigator.h"
 #include "modules/webshare/ShareData.h"
+#include "platform/bindings/V8ThrowException.h"
 #include "platform/mojo/MojoHelper.h"
 #include "public/platform/InterfaceProvider.h"
 #include "public/platform/Platform.h"
@@ -70,7 +71,6 @@ void NavigatorShare::ShareClientImpl::Callback(mojom::blink::ShareError error) {
   if (error == mojom::blink::ShareError::OK) {
     resolver_->Resolve();
   } else {
-    // TODO(mgiuca): Work out which error type to use.
     resolver_->Reject(DOMException::Create(kAbortError, ErrorToString(error)));
   }
 }
@@ -110,6 +110,16 @@ ScriptPromise NavigatorShare::share(ScriptState* script_state,
     DOMException* error = DOMException::Create(kSecurityError, error_message);
     return ScriptPromise::RejectWithDOMException(script_state, error);
   }
+
+  Document* doc = ToDocument(ExecutionContext::From(script_state));
+  DCHECK(doc);
+  KURL full_url = doc->CompleteURL(share_data.url());
+  if (!full_url.IsNull() && !full_url.IsValid()) {
+    v8::Local<v8::Value> error = V8ThrowException::CreateTypeError(
+        script_state->GetIsolate(), "Invalid URL");
+    return ScriptPromise::Reject(script_state, error);
+  }
+
   if (!UserGestureIndicator::ProcessingUserGesture()) {
     DOMException* error = DOMException::Create(
         kSecurityError,
@@ -117,8 +127,6 @@ ScriptPromise NavigatorShare::share(ScriptState* script_state,
     return ScriptPromise::RejectWithDOMException(script_state, error);
   }
 
-  Document* doc = ToDocument(ExecutionContext::From(script_state));
-  DCHECK(doc);
   if (!service_) {
     LocalFrame* frame = doc->GetFrame();
     DCHECK(frame);
@@ -135,7 +143,7 @@ ScriptPromise NavigatorShare::share(ScriptState* script_state,
 
   service_->Share(share_data.hasTitle() ? share_data.title() : g_empty_string,
                   share_data.hasText() ? share_data.text() : g_empty_string,
-                  doc->CompleteURL(share_data.url()),
+                  full_url,
                   ConvertToBaseCallback(WTF::Bind(&ShareClientImpl::Callback,
                                                   WrapPersistent(client))));
 
