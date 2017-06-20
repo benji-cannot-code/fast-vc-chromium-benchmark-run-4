2dc595ff63069db90490b1ff57d1d0de251748ee
xiyuan <xiyuan@chromium.org>
2017-06-20 06:02:35 -0700

    Fix missing base:: in a couple of macros
    LAZY_SINGLE_THREAD_TASK_RUNNER_INITIALIZER and
    LAZY_COM_STA_TASK_RUNNER_INITIALIZER should have base:: for
    SingleThreadTaskRunnerThreadMode. Otherwise, it would end
    up with compiler errors when expanded outside of base namespace.
    
    BUG=730170
    
    Review-Url: https://codereview.chromium.org/2946823002
    Cr-Commit-Position: refs/heads/master@{#480798}

--- base/task_scheduler/lazy_task_runner.h
+++ base/task_scheduler/lazy_task_runner.h
@@ -106,7 +106,7 @@ using LazyCOMSTATaskRunner =
   ALLOW_UNUSED_TYPE constexpr base::TaskTraits                            \
       LAZY_TASK_RUNNER_CONCATENATE_INTERNAL(kVerifyTraitsAreConstexpr,    \
                                             __LINE__) = traits;           \
-  ALLOW_UNUSED_TYPE constexpr SingleThreadTaskRunnerThreadMode            \
+  ALLOW_UNUSED_TYPE constexpr base::SingleThreadTaskRunnerThreadMode      \
       LAZY_TASK_RUNNER_CONCATENATE_INTERNAL(kVerifyThreadModeIsConstexpr, \
                                             __LINE__) = thread_mode
 
@@ -119,7 +119,7 @@ using LazyCOMSTATaskRunner =
   ALLOW_UNUSED_TYPE constexpr base::TaskTraits                            \
       LAZY_TASK_RUNNER_CONCATENATE_INTERNAL(kVerifyTraitsAreConstexpr,    \
                                             __LINE__) = traits;           \
-  ALLOW_UNUSED_TYPE constexpr SingleThreadTaskRunnerThreadMode            \
+  ALLOW_UNUSED_TYPE constexpr base::SingleThreadTaskRunnerThreadMode      \
       LAZY_TASK_RUNNER_CONCATENATE_INTERNAL(kVerifyThreadModeIsConstexpr, \
                                             __LINE__) = thread_mode
 
