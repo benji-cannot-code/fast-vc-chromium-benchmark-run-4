38c2c10250c890cafc37538711fc77350fb594ac
Scott Graham <scottmg@chromium.org>
2017-06-20 16:49:44 -0700

    fuchsia: define SYSTEM_NATIVE_UTF8
    Fixes RTLTest.WrapPathWithLTRFormatting.
    
    Bug: 706592
    Change-Id: I36f629ef8cfb512e78b1a060168d9edc412b7056
    Reviewed-on: https://chromium-review.googlesource.com/541997
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Scott Graham <scottmg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481089}

--- base/strings/sys_string_conversions_posix.cc
+++ base/strings/sys_string_conversions_posix.cc
@@ -158,6 +158,6 @@ std::wstring SysNativeMBToWide(const StringPiece& native_mb) {
   return out;
 }
 
-#endif  // OS_CHROMEOS
+#endif  // defined(SYSTEM_NATIVE_UTF8) || defined(OS_ANDROID)
 
 }  // namespace base
