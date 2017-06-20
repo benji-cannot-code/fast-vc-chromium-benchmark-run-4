3451b617209096a915020c81bf8984659fa26838
rbpotter <rbpotter@chromium.org>
2017-06-20 12:41:47 -0700

    Add policy to use system default printer
    Add a policy/command line flag to use the system default printer as the
    default destination in Print Preview instead of the most recently used
    destination.
    
    BUG=375238
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2885363003
    Cr-Commit-Position: refs/heads/master@{#480908}

--- chrome/browser/ui/webui/print_preview/print_preview_ui.cc
+++ chrome/browser/ui/webui/print_preview/print_preview_ui.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/command_line.h"
 #include "base/feature_list.h"
 #include "base/id_map.h"
 #include "base/lazy_instance.h"
@@ -32,10 +33,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/print_preview/print_preview_handler.h"
 #include "chrome/browser/ui/webui/theme_source.h"
 #include "chrome/common/chrome_features.h"
+#include "chrome/common/chrome_switches.h"
+#include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/browser_resources.h"
 #include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
+#include "components/prefs/pref_service.h"
 #include "components/printing/common/print_messages.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/url_data_source.h"
@@ -49,11 +53,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect.h"
 #include "ui/web_dialogs/web_dialog_delegate.h"
 #include "ui/web_dialogs/web_dialog_ui.h"
-
-#if defined(OS_CHROMEOS)
-#include "base/command_line.h"
-#include "chrome/common/chrome_switches.h"
-#endif
 
 using content::WebContents;
 using printing::PageSizeMargins;
@@ -166,7 +165,7 @@ bool HandleRequestCallback(
   return true;
 }
 
-content::WebUIDataSource* CreatePrintPreviewUISource() {
+content::WebUIDataSource* CreatePrintPreviewUISource(Profile* profile) {
   content::WebUIDataSource* source =
       content::WebUIDataSource::Create(chrome::kChromeUIPrintHost);
 #if defined(OS_CHROMEOS)
@@ -426,8 +425,12 @@ content::WebUIDataSource* CreatePrintPreviewUISource() {
       !base::CommandLine::ForCurrentProcess()->HasSwitch(
           ::switches::kDisableNativeCups);
   source->AddBoolean("showLocalManageButton", cups_and_md_settings_enabled);
+  source->AddBoolean("useSystemDefaultPrinter", false);
 #else
   source->AddBoolean("showLocalManageButton", true);
+  bool system_default_printer = profile->GetPrefs()->GetBoolean(
+      prefs::kPrintPreviewUseSystemDefaultPrinter);
+  source->AddBoolean("useSystemDefaultPrinter", system_default_printer);
 #endif
   return source;
 }
@@ -445,7 +448,7 @@ PrintPreviewUI::PrintPreviewUI(content::WebUI* web_ui)
       dialog_closed_(false) {
   // Set up the chrome://print/ data source.
   Profile* profile = Profile::FromWebUI(web_ui);
-  content::WebUIDataSource::Add(profile, CreatePrintPreviewUISource());
+  content::WebUIDataSource::Add(profile, CreatePrintPreviewUISource(profile));
 
   // Set up the chrome://theme/ source.
   content::URLDataSource::Add(profile, new ThemeSource(profile));
