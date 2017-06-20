5556efb2d21ef772f58c37672b707c41771c690e
Vitalii Iarko <vitaliii@chromium.org>
2017-06-20 13:59:55 +0200

    [NTP::Prefetched] Add flag.
    This CL adds a flag, which controls extended lifetime of prefetched
    suggestions.
    
    Bug: 734584
    Change-Id: I467046510ad67c461677a2ab151b5bdbab14a875
    Reviewed-on: https://chromium-review.googlesource.com/541341
    Commit-Queue: vitaliii <vitaliii@chromium.org>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480802}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1000,6 +1000,9 @@ extern const char kHerbPrototypeChoicesName[];
 extern const char kHerbPrototypeChoicesDescription[];
 extern const char kHerbPrototypeFlavorElderberry[];
 
+extern const char kKeepPrefetchedContentSuggestionsName[];
+extern const char kKeepPrefetchedContentSuggestionsDescription[];
+
 extern const char kLsdPermissionPromptName[];
 extern const char kLsdPermissionPromptDescription[];
 
--- components/ntp_snippets/features.cc
+++ components/ntp_snippets/features.cc
@@ -22,6 +22,7 @@ const base::Feature*(kAllFeatures[]) = {&kArticleSuggestionsFeature,
                                         &kContentSuggestionsPushFeature,
                                         &kForeignSessionsSuggestionsFeature,
                                         &kIncreasedVisibility,
+                                        &kKeepPrefetchedContentSuggestions,
                                         &kNotificationsFeature,
                                         &kPhysicalWebPageSuggestionsFeature,
                                         &kPublisherFaviconsFromNewServerFeature,
@@ -145,5 +146,8 @@ const char kNotificationsKeepWhenFrontmostParam[] =
 const char kNotificationsOpenToNTPParam[] = "open_to_ntp";
 const char kNotificationsDailyLimit[] = "daily_limit";
 const char kNotificationsIgnoredLimitParam[] = "ignored_limit";
+
+const base::Feature kKeepPrefetchedContentSuggestions{
+    "KeepPrefetchedContentSuggestions", base::FEATURE_DISABLED_BY_DEFAULT};
 
 }  // namespace ntp_snippets
