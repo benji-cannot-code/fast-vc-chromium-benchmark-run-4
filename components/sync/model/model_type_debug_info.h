b0190995228278df30b5e5408216e056b61849cc
pavely <pavely@chromium.org>
2017-06-20 15:20:04 -0700

    [Sync] Record sync memory usage in histogram broken by datatypes
    Memory estimate is collected from Directory for directory based datatypes and
    from SharedModelTypeProcessor for USS datatypes. It is triggered right after
    configuration when all sync data is loaded into memory.
    
    BUG=706214
    R=pnoland@chromium.org
    
    Review-Url: https://codereview.chromium.org/2950583003
    Cr-Commit-Position: refs/heads/master@{#480992}

--- components/sync/model/model_type_debug_info.h
+++ components/sync/model/model_type_debug_info.h
@@ -36,6 +36,10 @@ class ModelTypeDebugInfo {
           callback,
       ModelTypeSyncBridge* bridge);
 
+  // Queries |bridge| for estimate of memory usage and records it in a
+  // histogram.
+  static void RecordMemoryUsageHistogram(ModelTypeSyncBridge* bridge);
+
  private:
   ModelTypeDebugInfo();
 
