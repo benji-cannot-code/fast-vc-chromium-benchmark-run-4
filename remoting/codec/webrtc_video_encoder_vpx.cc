67b0fbf2f5ba81fd407407dfc4fc28c265540a07
Sergey Ulanov <sergeyu@chromium.org>
2017-06-20 14:29:11 -0700

    Make WebrtcVideoEncoder asynchronous.
    Previously WebrtcVideoEncoder interface was synchronous, so it was
    incompatible with HW encoders. Refactored it to make it asynchronous.
    
    Change-Id: I1b0f3ce6dda657ecddb4ac84cf8132a22b473308
    Reviewed-on: https://chromium-review.googlesource.com/541122
    Reviewed-by: Scott Nichols <nicholss@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481297}

--- remoting/codec/webrtc_video_encoder_vpx.cc
+++ remoting/codec/webrtc_video_encoder_vpx.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
+#include "base/callback.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/sys_info.h"
@@ -274,9 +275,9 @@ void WebrtcVideoEncoderVpx::SetLosslessColor(bool want_lossless) {
   }
 }
 
-std::unique_ptr<WebrtcVideoEncoder::EncodedFrame> WebrtcVideoEncoderVpx::Encode(
-    const webrtc::DesktopFrame* frame,
-    const FrameParams& params) {
+void WebrtcVideoEncoderVpx::Encode(std::unique_ptr<webrtc::DesktopFrame> frame,
+                                   const FrameParams& params,
+                                   EncodeCallback done) {
   webrtc::DesktopSize previous_frame_size =
       image_ ? webrtc::DesktopSize(image_->w, image_->h)
              : webrtc::DesktopSize();
@@ -285,7 +286,8 @@ std::unique_ptr<WebrtcVideoEncoder::EncodedFrame> WebrtcVideoEncoderVpx::Encode(
 
   // Don't need to send anything until we get the first non-null frame.
   if (frame_size.is_empty()) {
-    return nullptr;
+    std::move(done).Run(nullptr);
+    return;
   }
 
   DCHECK_GE(frame_size.width(), 32);
@@ -305,7 +307,7 @@ std::unique_ptr<WebrtcVideoEncoder::EncodedFrame> WebrtcVideoEncoderVpx::Encode(
 
   webrtc::DesktopRegion updated_region;
   // Convert the updated capture data ready for encode.
-  PrepareImage(frame, &updated_region);
+  PrepareImage(frame.get(), &updated_region);
 
   // Update active map based on updated region.
   if (params.clear_active_map)
@@ -374,7 +376,7 @@ std::unique_ptr<WebrtcVideoEncoder::EncodedFrame> WebrtcVideoEncoderVpx::Encode(
     }
   }
 
-  return encoded_frame;
+  std::move(done).Run(std::move(encoded_frame));
 }
 
 WebrtcVideoEncoderVpx::WebrtcVideoEncoderVpx(bool use_vp9)
