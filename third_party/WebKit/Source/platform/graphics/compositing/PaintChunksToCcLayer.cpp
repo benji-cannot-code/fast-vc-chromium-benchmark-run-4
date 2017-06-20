4330512bc96a0423a51d29559bcf42e4bc69a8ea
Tien-Ren Chen <trchen@chromium.org>
2017-06-20 14:38:38 -0700

    [SPv2] Rework GeometryMapper transform computation
    When computing the conversion matrix between two spaces, there are two
    types of conversion that are commonly used, but with slightly different
    semantics:
    
    m1 = destination_to_screen ^ -1 * source_to_screen
    m2 = flatten(destination_to_screen) ^ -1 * flatten(source_to_screen)
    
    The former computes the space conversion, which is useful for mapping
    3D points from one space to 3D points in another space. The latter
    computes the "projection through screen", which is useful for mapping
    2D points from a plane to a backing that is expected to be drawn onto
    the screen.
    
    To illustrate the difference, see the following example:
    <div style="transform:rotateY(45deg)">A</div>
    <div style="transform:rotateY(-45deg)">B</div>
    
    The space conversion from A to B is equal to rotateY(90deg), but the
    projection between the two planes is identity.
    
    Prior to this CL, GeometryMapper implemented a mixed semantics. This is
    okay because all existing code only make queries that are co-planar, in
    which case, either version returns identical result. This is because we
    only make intra-compositing-layer queries, and 3D transform makes a
    compositing boundary.
    
    This CL changed the implementation to strictly return the "projection
    through screen". The algorithm runs in amortized O(1) by caching the
    matrix between a node and its nearest coplanar ancestor that has a
    invertible projection.
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ia526dc6feb60c902f27589d73e78dfea235c6a93
    Reviewed-on: https://chromium-review.googlesource.com/534899
    Commit-Queue: Tien-Ren Chen <trchen@chromium.org>
    Reviewed-by: Chris harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481049}

--- third_party/WebKit/Source/platform/graphics/compositing/PaintChunksToCcLayer.cpp
+++ third_party/WebKit/Source/platform/graphics/compositing/PaintChunksToCcLayer.cpp
@@ -32,8 +32,8 @@ static void ApplyClipsBetweenStates(const PropertyTreeState& local_state,
       local_state.Clip()->LocalTransformSpace();
   if (transform_node != ancestor_state.Transform()) {
     const TransformationMatrix& local_to_ancestor_matrix =
-        GeometryMapper::LocalToAncestorMatrix(transform_node,
-                                              ancestor_state.Transform());
+        GeometryMapper::SourceToDestinationProjection(
+            transform_node, ancestor_state.Transform());
     // Clips are only in descendant spaces that are transformed by one
     // or more scrolls.
     DCHECK(local_to_ancestor_matrix.IsIdentityOrTranslation());
@@ -122,8 +122,8 @@ static void RecordPairedBeginDisplayItems(
           const TransformPaintPropertyNode* transform_node =
               paired_state->Effect()->LocalTransformSpace();
           const TransformationMatrix& local_to_ancestor_matrix =
-              GeometryMapper::LocalToAncestorMatrix(transform_node,
-                                                    paired_state->Transform());
+              GeometryMapper::SourceToDestinationProjection(
+                  transform_node, paired_state->Transform());
           // Effects are only in descendant spaces that are transformed by one
           // or more scrolls.
           DCHECK(local_to_ancestor_matrix.IsIdentityOrTranslation());
