c8876f401b2b8d29d03553170061ce16e9fc3e0e
Tommy Martino <tmartino@chromium.org>
2017-06-20 21:52:41 +0000

    Revert "Remove Remoting API Key"
    This reverts commit e8f9d07dc81bad7e5fdd7d2c28a3ef14334b5d1c.
    
    Reason for revert: Causing breakages -- https://build.chromium.org/p/chromium/builders/Android/builds/73527
    
    Original change's description:
    > Remove Remoting API Key
    > 
    > Chromoting host no longer needs API Key, so the key can be removed.
    > 
    > Change-Id: I0960660ce2ddd5f4352ab691468d7c3881530d8d
    > Reviewed-on: https://chromium-review.googlesource.com/540035
    > Reviewed-by: Roger Tawa <rogerta@chromium.org>
    > Reviewed-by: Joe Downing <joedow@chromium.org>
    > Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#480940}
    
    TBR=rogerta@chromium.org,sergeyu@chromium.org,joedow@chromium.org
    
    Change-Id: Ibc4df105f77ad41a859663cbef76b73baa208a63
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/541763
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480970}

--- google_apis/google_api_keys.cc
+++ google_apis/google_api_keys.cc
@@ -35,6 +35,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define GOOGLE_API_KEY DUMMY_API_TOKEN
 #endif
 
+#if !defined(GOOGLE_API_KEY_REMOTING)
+#define GOOGLE_API_KEY_REMOTING DUMMY_API_TOKEN
+#endif
+
 #if !defined(GOOGLE_CLIENT_ID_MAIN)
 #define GOOGLE_CLIENT_ID_MAIN DUMMY_API_TOKEN
 #endif
@@ -111,6 +115,14 @@ class APIKeyCache {
 #else
     api_key_non_stable_ = api_key_;
 #endif
+
+    api_key_remoting_ =
+        CalculateKeyValue(GOOGLE_API_KEY_REMOTING,
+                          STRINGIZE_NO_EXPANSION(GOOGLE_API_KEY_REMOTING),
+                          NULL,
+                          std::string(),
+                          environment.get(),
+                          command_line);
 
     std::string default_client_id =
         CalculateKeyValue(GOOGLE_DEFAULT_CLIENT_ID,
@@ -199,6 +211,7 @@ class APIKeyCache {
   void set_api_key(const std::string& api_key) { api_key_ = api_key; }
 #endif
   std::string api_key_non_stable() const { return api_key_non_stable_; }
+  std::string api_key_remoting() const { return api_key_remoting_; }
 
   std::string GetClientID(OAuth2Client client) const {
     DCHECK_LT(client, CLIENT_NUM_ITEMS);
@@ -294,6 +307,7 @@ class APIKeyCache {
 
   std::string api_key_;
   std::string api_key_non_stable_;
+  std::string api_key_remoting_;
   std::string client_ids_[CLIENT_NUM_ITEMS];
   std::string client_secrets_[CLIENT_NUM_ITEMS];
 };
@@ -322,6 +336,10 @@ std::string GetAPIKey() {
 
 std::string GetNonStableAPIKey() {
   return g_api_key_cache.Get().api_key_non_stable();
+}
+
+std::string GetRemotingAPIKey() {
+  return g_api_key_cache.Get().api_key_remoting();
 }
 
 #if defined(OS_IOS)
