7d9072ad31a563bc1c69bdc116c6571c5a7429b8
Sorin Jianu <sorin@chromium.org>
2017-06-20 14:30:56 -0700

    New run action test file.
    This file contains a ChromeRecovery.crx3, which contains a
    ChromeRecovery.exe.
    
    Bug: 687231
    Change-Id: Ia9789b1c5c4cd8884d56c25e0765045dda7a749a
    Reviewed-on: https://chromium-review.googlesource.com/541101
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481010}

--- components/update_client/action_runner_win.cc
+++ components/update_client/action_runner_win.cc
@@ -17,9 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// TODO(sorin): rename to "ChromeRecovery.exe".
 const base::FilePath::CharType kRecoveryFileName[] =
-    FILE_PATH_LITERAL("recovery.exe");
+    FILE_PATH_LITERAL("ChromeRecovery.exe");
 
 }  // namespace
 
