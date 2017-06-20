2180d51ba00a8055e12343c78d89793a31194c75
Chris Mumford <cmumford@chromium.org>
2017-06-20 10:15:14 -0700

    Deleted redundant SymmetricKey::GetRawKey().
    This was functionally equivalent to SymmetricKey::key(), and also
    returned a always true status code. Switching to key() allowed for
    the simplification of downstream functions.
    
    R=alemate@chromium.org, alokp@chromium.org, davidben@chromium.org, rogerta@chromium.org, stanisc@chromium.org
    
    Review-Url: https://codereview.chromium.org/2934893003 .
    Cr-Commit-Position: refs/heads/master@{#480868}

--- components/sync/base/nigori_unittest.cc
+++ components/sync/base/nigori_unittest.cc
@@ -130,7 +130,7 @@ TEST(SyncNigoriTest, ExportImport) {
   std::string user_key;
   std::string encryption_key;
   std::string mac_key;
-  EXPECT_TRUE(nigori1.ExportKeys(&user_key, &encryption_key, &mac_key));
+  nigori1.ExportKeys(&user_key, &encryption_key, &mac_key);
 
   Nigori nigori2;
   EXPECT_TRUE(nigori2.InitByImport(user_key, encryption_key, mac_key));
@@ -160,7 +160,7 @@ TEST(SyncNigoriTest, InitByDerivationSetsUserKey) {
   std::string user_key = "";
   std::string encryption_key;
   std::string mac_key;
-  EXPECT_TRUE(nigori.ExportKeys(&user_key, &encryption_key, &mac_key));
+  nigori.ExportKeys(&user_key, &encryption_key, &mac_key);
 
   EXPECT_NE(user_key, "");
 }
@@ -172,7 +172,7 @@ TEST(SyncNigoriTest, ToleratesEmptyUserKey) {
   std::string user_key;
   std::string encryption_key;
   std::string mac_key;
-  EXPECT_TRUE(nigori1.ExportKeys(&user_key, &encryption_key, &mac_key));
+  nigori1.ExportKeys(&user_key, &encryption_key, &mac_key);
   EXPECT_FALSE(user_key.empty());
   EXPECT_FALSE(encryption_key.empty());
   EXPECT_FALSE(mac_key.empty());
@@ -181,7 +181,7 @@ TEST(SyncNigoriTest, ToleratesEmptyUserKey) {
   EXPECT_TRUE(nigori2.InitByImport("", encryption_key, mac_key));
 
   user_key = "non-empty-value";
-  EXPECT_TRUE(nigori2.ExportKeys(&user_key, &encryption_key, &mac_key));
+  nigori2.ExportKeys(&user_key, &encryption_key, &mac_key);
   EXPECT_TRUE(user_key.empty());
   EXPECT_FALSE(encryption_key.empty());
   EXPECT_FALSE(mac_key.empty());
