cf35b929b3bfe07851ca9694e0226c19e5e3cb4f
rnephew <rnephew@chromium.org>
2017-06-20 15:32:52 -0700

    [Telemetry] Move oopif, tab_switching, tracing, v8, and cluster telemetery benchmarks to story expectations TBR=charliea@chromium.org
    Bug: 713222
    Change-Id: I6b4a71f0212b12cb31f0f53632aca36bef9e6c2d
    Reviewed-on: https://chromium-review.googlesource.com/541587
    Reviewed-by: rnephew <rnephew@chromium.org>
    Commit-Queue: rnephew <rnephew@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481063}

--- tools/perf/benchmarks/tracing.py
+++ tools/perf/benchmarks/tracing.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from core import perf_benchmark
 
 from telemetry import benchmark
+from telemetry import story
 from telemetry.timeline import chrome_trace_category_filter
 from telemetry.timeline import chrome_trace_config
 from telemetry.web_perf import timeline_based_measurement
@@ -29,6 +30,12 @@ class TracingWithDebugOverhead(perf_benchmark.PerfBenchmark):
   @classmethod
   def Name(cls):
     return 'tracing.tracing_with_debug_overhead'
+
+  def GetExpectations(self):
+    class StoryExpectations(story.expectations.StoryExpectations):
+      def SetExpectations(self):
+        pass # No tests disabled.
+    return StoryExpectations()
 
 
 # TODO(ssid): Enable on reference builds once stable browser starts supporting
@@ -54,3 +61,9 @@ class TracingWithBackgroundMemoryInfra(perf_benchmark.PerfBenchmark):
   @classmethod
   def Name(cls):
     return 'tracing.tracing_with_background_memory_infra'
+
+  def GetExpectations(self):
+    class StoryExpectations(story.expectations.StoryExpectations):
+      def SetExpectations(self):
+        pass # No tests disabled.
+    return StoryExpectations()
