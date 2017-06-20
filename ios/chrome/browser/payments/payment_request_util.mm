b31285a595ac31495a500d44514f63db281b8c81
mahmadi <mahmadi@chromium.org>
2017-06-20 08:31:18 -0700

    [Payment Request] Updates name, phone number, and email labels
    BUG=602666
    
    Review-Url: https://codereview.chromium.org/2952503002
    Cr-Commit-Position: refs/heads/master@{#480827}

--- ios/chrome/browser/payments/payment_request_util.mm
+++ ios/chrome/browser/payments/payment_request_util.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/autofill_profile.h"
 #include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/browser/personal_data_manager.h"
+#include "components/payments/core/payment_request_data_util.h"
 #include "components/payments/core/strings_util.h"
 #include "components/strings/grit/components_strings.h"
 #include "ios/chrome/browser/application_context.h"
@@ -27,9 +28,10 @@ namespace payment_request_util {
 
 NSString* GetNameLabelFromAutofillProfile(
     const autofill::AutofillProfile& profile) {
-  return base::SysUTF16ToNSString(
+  base::string16 label =
       profile.GetInfo(autofill::AutofillType(autofill::NAME_FULL),
-                      GetApplicationContext()->GetApplicationLocale()));
+                      GetApplicationContext()->GetApplicationLocale());
+  return !label.empty() ? base::SysUTF16ToNSString(label) : nil;
 }
 
 NSString* GetShippingAddressLabelFromAutofillProfile(
@@ -48,13 +50,16 @@ NSString* GetBillingAddressLabelFromAutofillProfile(
 
 NSString* GetPhoneNumberLabelFromAutofillProfile(
     const autofill::AutofillProfile& profile) {
-  base::string16 label = profile.GetRawInfo(autofill::PHONE_HOME_WHOLE_NUMBER);
+  base::string16 label = payments::data_util::GetFormattedPhoneNumberForDisplay(
+      profile, GetApplicationContext()->GetApplicationLocale());
   return !label.empty() ? base::SysUTF16ToNSString(label) : nil;
 }
 
 NSString* GetEmailLabelFromAutofillProfile(
     const autofill::AutofillProfile& profile) {
-  base::string16 label = profile.GetRawInfo(autofill::EMAIL_ADDRESS);
+  base::string16 label =
+      profile.GetInfo(autofill::AutofillType(autofill::EMAIL_ADDRESS),
+                      GetApplicationContext()->GetApplicationLocale());
   return !label.empty() ? base::SysUTF16ToNSString(label) : nil;
 }
 
