53c545930688818e4b9449fba826fdfce9d86d42
tanvir.rizvi <tanvir.rizvi@samsung.com>
2017-06-20 18:41:55 -0700

    Move drag related functions present in LocalFrame.cpp to correct files.
    Move class DraggedNodeImageBuilder, DataTransfer::NodeImage(),
    DataTransfer::DeviceSpaceBounds(), DataTransfer::CreateDragImageForFrame()
    to DataTransfer.cpp.
    Move DragController::DragImageForSelection() to DragController.cpp.
    
    BUG=727981
    
    Review-Url: https://codereview.chromium.org/2950793002
    Cr-Commit-Position: refs/heads/master@{#481067}

--- third_party/WebKit/Source/core/page/DragController.cpp
+++ third_party/WebKit/Source/core/page/DragController.cpp
@@ -77,6 +77,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/graphics/BitmapImage.h"
 #include "platform/graphics/Image.h"
 #include "platform/graphics/ImageOrientation.h"
+#include "platform/graphics/paint/PaintRecordBuilder.h"
 #include "platform/loader/fetch/ResourceFetcher.h"
 #include "platform/loader/fetch/ResourceRequest.h"
 #include "platform/weborigin/SecurityOrigin.h"
@@ -1079,6 +1080,29 @@ static std::unique_ptr<DragImage> DragImageForLink(
                       mouse_dragged_point.Y() + drag_image_offset.Y());
 
   return drag_image;
+}
+
+// static
+std::unique_ptr<DragImage> DragController::DragImageForSelection(
+    const LocalFrame& frame,
+    float opacity) {
+  if (!frame.Selection().ComputeVisibleSelectionInDOMTreeDeprecated().IsRange())
+    return nullptr;
+
+  frame.View()->UpdateAllLifecyclePhasesExceptPaint();
+  DCHECK(frame.GetDocument()->IsActive());
+
+  FloatRect painting_rect = FloatRect(frame.Selection().Bounds());
+  GlobalPaintFlags paint_flags =
+      kGlobalPaintSelectionOnly | kGlobalPaintFlattenCompositingLayers;
+
+  PaintRecordBuilder builder(
+      DataTransfer::DeviceSpaceBounds(painting_rect, frame));
+  frame.View()->PaintContents(builder.Context(), paint_flags,
+                              EnclosingIntRect(painting_rect));
+  return DataTransfer::CreateDragImageForFrame(
+      frame, opacity, kDoNotRespectImageOrientation, painting_rect, builder,
+      PropertyTreeState::Root());
 }
 
 bool DragController::StartDrag(LocalFrame* src,
