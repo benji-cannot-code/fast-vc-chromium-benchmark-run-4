36a9e8b5403d1e2798c02a17844486910fa5154d
hans <hans@chromium.org>
2017-06-20 19:34:17 -0700

    Make LockImpl::Unlock available for inlining
    This function is hit frequently and we'd like it to available
    for inlining even in non-LTO builds (see bug).
    
    BUG=728324
    
    Review-Url: https://codereview.chromium.org/2949953002
    Cr-Commit-Position: refs/heads/master@{#481090}

--- base/synchronization/lock_impl_win.cc
+++ base/synchronization/lock_impl_win.cc
@@ -23,9 +23,5 @@ void LockImpl::Lock() {
   ::AcquireSRWLockExclusive(&native_handle_);
 }
 
-void LockImpl::Unlock() {
-  ::ReleaseSRWLockExclusive(&native_handle_);
-}
-
 }  // namespace internal
 }  // namespace base
