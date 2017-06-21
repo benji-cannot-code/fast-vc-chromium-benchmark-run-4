d414c121aa1271961e1aadf3b06be84d2a0b8ed4
allada <allada@chromium.org>
2017-06-20 17:33:06 -0700

    [Devtools] Removed NetworkRequest dependency on NetworkManager
    This patch fully removes the dependency from NetworkRequest to
    NetworkManager.
    
    R=dgozman,pfeldman
    BUG=374557
    
    Review-Url: https://codereview.chromium.org/2921963002
    Cr-Commit-Position: refs/heads/master@{#481044}

--- third_party/WebKit/Source/devtools/front_end/sdk/NetworkRequest.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/NetworkRequest.js
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 SDK.NetworkRequest = class extends Common.Object {
   /**
-   * @param {!SDK.NetworkManager} networkManager
    * @param {!Protocol.Network.RequestId} requestId
    * @param {string} url
    * @param {string} documentURL
@@ -42,10 +41,9 @@ SDK.NetworkRequest = class extends Common.Object {
    * @param {!Protocol.Network.LoaderId} loaderId
    * @param {?Protocol.Network.Initiator} initiator
    */
-  constructor(networkManager, requestId, url, documentURL, frameId, loaderId, initiator) {
+  constructor(requestId, url, documentURL, frameId, loaderId, initiator) {
     super();
 
-    this._networkManager = networkManager;
     this._requestId = requestId;
     this.setUrl(url);
     this._documentURL = documentURL;
@@ -532,7 +530,8 @@ SDK.NetworkRequest = class extends Common.Object {
     } else {
       this._path = this._parsedURL.host + this._parsedURL.folderPathComponents;
 
-      var inspectedURL = this._networkManager.target().inspectedURL().asParsedURL();
+      var networkManager = SDK.NetworkManager.forRequest(this);
+      var inspectedURL = networkManager ? networkManager.target().inspectedURL().asParsedURL() : null;
       this._path = this._path.trimURL(inspectedURL ? inspectedURL.host : '');
       if (this._parsedURL.lastPathComponent || this._parsedURL.queryParams) {
         this._name =
@@ -1088,13 +1087,6 @@ SDK.NetworkRequest = class extends Common.Object {
     var message = {time: this.pseudoWallTime(time), eventName: eventName, eventId: eventId, data: data};
     this._eventSourceMessages.push(message);
     this.dispatchEventToListeners(SDK.NetworkRequest.Events.EventSourceMessageAdded, message);
-  }
-
-  /**
-   * @return {!SDK.NetworkManager}
-   */
-  networkManager() {
-    return this._networkManager;
   }
 };
 
