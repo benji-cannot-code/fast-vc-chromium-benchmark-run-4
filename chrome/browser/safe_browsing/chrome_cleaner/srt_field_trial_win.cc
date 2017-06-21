e2931d68d6f4d1496340ef149ba3280d3a67a165
veranika <veranika@chromium.org>
2017-06-21 08:42:06 -0700

    Do not tie cleanup tool download to SRTPromptFieldTrial field trial
    BUG=733358
    R=csharp@chromium.org
    
    Review-Url: https://codereview.chromium.org/2949633002
    Cr-Commit-Position: refs/heads/master@{#481214}

--- chrome/browser/safe_browsing/chrome_cleaner/srt_field_trial_win.cc
+++ chrome/browser/safe_browsing/chrome_cleaner/srt_field_trial_win.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/safe_browsing/chrome_cleaner/srt_field_trial_win.h"
 
 #include "base/metrics/field_trial.h"
+#include "base/metrics/field_trial_params.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/string_util.h"
 #include "base/win/windows_version.h"
@@ -44,6 +45,9 @@ namespace safe_browsing {
 const base::Feature kInBrowserCleanerUIFeature{
     "InBrowserCleanerUI", base::FEATURE_DISABLED_BY_DEFAULT};
 
+const base::Feature kCleanerDownloadFeature{"DownloadCleanupToolByBitness",
+                                            base::FEATURE_DISABLED_BY_DEFAULT};
+
 bool IsInSRTPromptFieldTrialGroups() {
   return !base::StartsWith(base::FieldTrialList::FindFullName(kSRTPromptTrial),
                            kSRTPromptOffGroup, base::CompareCase::SENSITIVE);
@@ -71,8 +75,8 @@ GURL GetLegacyDownloadURL() {
 
 GURL GetSRTDownloadURL() {
   constexpr char kDownloadGroupParam[] = "download_group";
-  const std::string download_group =
-      variations::GetVariationParamValue(kSRTPromptTrial, kDownloadGroupParam);
+  const std::string download_group = base::GetFieldTrialParamValueByFeature(
+      kCleanerDownloadFeature, kDownloadGroupParam);
   if (download_group.empty())
     return GetLegacyDownloadURL();
 
