ca6c50526a2821a404b82d7c43acaf643932bc8c
Scott Violet <sky@chromium.org>
2017-06-21 15:25:20 -0700

    chromeos: make mus use ash's DragDropController
    This way we get all the features in ash.
    
    BUG=none
    TEST=none
    
    Change-Id: Ie350cc537f5529f9d914b7f2aeaf2c1f8c892d7a
    Reviewed-on: https://chromium-review.googlesource.com/543921
    Reviewed-by: Elliot Glaysher <erg@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481377}

--- ui/aura/mus/window_tree_client.h
+++ ui/aura/mus/window_tree_client.h
@@ -120,6 +120,8 @@ class AURA_EXPORT WindowTreeClient
   // See mojom for details on |automatically_create_display_roots|.
   void ConnectAsWindowManager(bool automatically_create_display_roots = true);
 
+  void DisableDragDropClient() { install_drag_drop_client_ = false; }
+
   service_manager::Connector* connector() { return connector_; }
   ui::Gpu* gpu() { return gpu_.get(); }
   CaptureSynchronizer* capture_synchronizer() {
@@ -652,6 +654,10 @@ class AURA_EXPORT WindowTreeClient
   gfx::Insets normal_client_area_insets_;
 
   bool in_shutdown_ = false;
+
+  // Temporary while we have mushrome, once we switch to mash this can be
+  // removed.
+  bool install_drag_drop_client_ = true;
 
   base::WeakPtrFactory<WindowTreeClient> weak_factory_;
 
