3def643033dc0330e49448b008d92818ad473801
zhaobin <zhaobin@chromium.org>
2017-06-21 15:35:21 -0700

    [cast_channel] Make CastMessageHandler a CastSocket::Observer instead of CastTransport::Delegate
    We are going to create one CastSocket for one IP endpoint and share it between browser and different extensions. To handle message and errors, it seems more reasonable to have one CastTransport::Delegate and multiple observers per CastSocket.
    
    BUG=687377
    
    Review-Url: https://codereview.chromium.org/2942993003
    Cr-Commit-Position: refs/heads/master@{#481334}

--- components/cast_channel/cast_test_util.cc
+++ components/cast_channel/cast_test_util.cc
@@ -27,6 +27,9 @@ void MockCastTransport::SetReadDelegate(
 MockCastTransportDelegate::MockCastTransportDelegate() {}
 MockCastTransportDelegate::~MockCastTransportDelegate() {}
 
+MockCastSocketObserver::MockCastSocketObserver() {}
+MockCastSocketObserver::~MockCastSocketObserver() {}
+
 MockCastSocket::MockCastSocket() : mock_transport_(new MockCastTransport()) {}
 MockCastSocket::~MockCastSocket() {}
 
