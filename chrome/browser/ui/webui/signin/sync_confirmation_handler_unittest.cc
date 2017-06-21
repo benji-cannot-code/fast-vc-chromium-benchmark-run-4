0237077fd23b377ddc344cb494834d0af8725773
Boris Sazonov <bsazonov@chromium.org>
2017-06-21 18:39:00 +0200

    Add no_silhouette option to GetImageURLWithThumbnailSize
    This CL makes several changes to GetImageURLWithThumbnailSize to add support
    for no_silhouette option:
    1. Rename GetImageURLWithThumbnailSize to GetImageURLWithOptions.
    2. Add no_silhouette option and change all callers to pass false to keep the old
       behavior for now.
    3. Use URL path components instead of pattern matching to replace exisiting
       options. This also fixes the cases when initial URL specifies thumbnail size
       that is not 64.
    4. Add tests for no_silhouette and different initial thumbnail size.
    
    Bug: 733306
    Change-Id: I77967daf398fdd2a5df7f7717e6a9ca2a5313044
    Reviewed-on: https://chromium-review.googlesource.com/536776
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481264}

--- chrome/browser/ui/webui/signin/sync_confirmation_handler_unittest.cc
+++ chrome/browser/ui/webui/signin/sync_confirmation_handler_unittest.cc
@@ -210,10 +210,9 @@ TEST_F(SyncConfirmationHandlerTest, TestSetImageIfPrimaryAccountReady) {
   std::string original_picture_url =
       AccountTrackerServiceFactory::GetForProfile(profile())->
           GetAccountInfo("gaia").picture_url;
-  GURL picture_url_with_size;
-  EXPECT_TRUE(profiles::GetImageURLWithThumbnailSize(GURL(original_picture_url),
-                                                     kExpectedProfileImageSize,
-                                                     &picture_url_with_size));
+  GURL picture_url_with_size = profiles::GetImageURLWithOptions(
+      GURL(original_picture_url), kExpectedProfileImageSize,
+      false /* no_silhouette */);
   EXPECT_EQ(picture_url_with_size.spec(), passed_picture_url);
 }
 
@@ -261,10 +260,9 @@ TEST_F(SyncConfirmationHandlerTest, TestSetImageIfPrimaryAccountReadyLater) {
   std::string original_picture_url =
       AccountTrackerServiceFactory::GetForProfile(profile())->
           GetAccountInfo("gaia").picture_url;
-  GURL picture_url_with_size;
-  EXPECT_TRUE(profiles::GetImageURLWithThumbnailSize(GURL(original_picture_url),
-                                                     kExpectedProfileImageSize,
-                                                     &picture_url_with_size));
+  GURL picture_url_with_size = profiles::GetImageURLWithOptions(
+      GURL(original_picture_url), kExpectedProfileImageSize,
+      false /* no_silhouette */);
   EXPECT_EQ(picture_url_with_size.spec(), passed_picture_url);
 }
 
