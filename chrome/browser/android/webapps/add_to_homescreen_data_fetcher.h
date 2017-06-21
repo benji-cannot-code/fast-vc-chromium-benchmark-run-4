59eff80d4a1951ca104e88bca1044a56d0df9528
dominickn <dominickn@chromium.org>
2017-06-21 16:46:39 -0700

    Migrate AddToHomescreenDataFetcher to use TaskScheduler.
    BUG=667892
    
    Review-Url: https://codereview.chromium.org/2949863003
    Cr-Commit-Position: refs/heads/master@{#481359}

--- chrome/browser/android/webapps/add_to_homescreen_data_fetcher.h
+++ chrome/browser/android/webapps/add_to_homescreen_data_fetcher.h
@@ -8,26 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
+#include "base/strings/string16.h"
 #include "base/task/cancelable_task_tracker.h"
 #include "base/timer/timer.h"
 #include "chrome/browser/android/shortcut_info.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 
-namespace base {
-class TaskRunner;
-}
-
-namespace content {
-class WebContents;
-}
-
 namespace favicon_base {
 struct FaviconRawBitmapResult;
-}
-
-namespace IPC {
-class Message;
 }
 
 class GURL;
@@ -123,8 +112,6 @@ class AddToHomescreenDataFetcher
 
   // Notifies the observer that the shortcut data is all available.
   void NotifyObserver(const SkBitmap& icon);
-
-  scoped_refptr<base::TaskRunner> background_task_runner_;
 
   Observer* weak_observer_;
 
