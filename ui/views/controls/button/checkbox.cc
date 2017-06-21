1100a7930e6dfe4c9fd75b0ac3a6d3a2d249917d
bsep <bsep@chromium.org>
2017-06-20 18:44:37 -0700

    Revert of Fix Harmony checkbox having an ink drop highlight on click. (patchset #1 id:1 of https://codereview.chromium.org/2939043005/ )
    Reason for revert:
    Breaks Chrome OS x86.
    
    Original issue's description:
    > Fix Harmony checkbox having an ink drop highlight on click.
    >
    > Follow-up to crrev.com/2911573002. There was a regression with a later
    > refactor of the ink drop code, and this fixes that.
    >
    > BUG=652510
    >
    > Review-Url: https://codereview.chromium.org/2939043005
    > Cr-Commit-Position: refs/heads/master@{#480958}
    > Committed: https://chromium.googlesource.com/chromium/src/+/6be568e4976c957d72d685a18b395bb60842273f
    
    TBR=sky@chromium.org,bruthig@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=652510
    
    Review-Url: https://codereview.chromium.org/2948753003
    Cr-Commit-Position: refs/heads/master@{#481074}

--- ui/views/controls/button/checkbox.cc
+++ ui/views/controls/button/checkbox.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/color_utils.h"
 #include "ui/gfx/paint_vector_icon.h"
-#include "ui/views/animation/ink_drop_impl.h"
+#include "ui/views/animation/ink_drop_highlight.h"
 #include "ui/views/animation/ink_drop_ripple.h"
 #include "ui/views/animation/square_ink_drop_ripple.h"
 #include "ui/views/controls/button/label_button_border.h"
@@ -144,10 +144,8 @@ void Checkbox::OnNativeThemeChanged(const ui::NativeTheme* theme) {
 }
 
 std::unique_ptr<InkDrop> Checkbox::CreateInkDrop() {
-  // Completely removes the highlight.
-  std::unique_ptr<InkDropImpl> ink_drop = CreateDefaultInkDropImpl();
+  std::unique_ptr<InkDrop> ink_drop = LabelButton::CreateInkDrop();
   ink_drop->SetShowHighlightOnHover(false);
-  ink_drop->SetAutoHighlightMode(views::InkDropImpl::AutoHighlightMode::NONE);
   return ink_drop;
 }
 
