1fa66607b32ed5f4b78cdbd174afa49479d80fd9
Xiyuan Xia <xiyuan@chromium.org>
2017-06-21 15:53:58 -0700

    cros: Replace BrowserThread::FILE in FreezerCgroupProcessManager
    BrowserThread::FILE is deprecated in favor of the post_task API.
    
    BUG=689520
    
    Change-Id: If72d1d07d683efd66e921af3e15cabfbba2543b4
    Reviewed-on: https://chromium-review.googlesource.com/544103
    Reviewed-by: Dan Erat <derat@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481534}

--- chrome/browser/chromeos/power/freezer_cgroup_process_manager.cc
+++ chrome/browser/chromeos/power/freezer_cgroup_process_manager.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/sequenced_task_runner.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/task_scheduler/post_task.h"
 #include "content/public/browser/browser_thread.h"
 
 namespace chromeos {
@@ -147,8 +148,8 @@ class FreezerCgroupProcessManager::FileWorker {
 };
 
 FreezerCgroupProcessManager::FreezerCgroupProcessManager()
-    : file_thread_(content::BrowserThread::GetTaskRunnerForThread(
-          content::BrowserThread::FILE)),
+    : file_thread_(base::CreateSequencedTaskRunnerWithTraits(
+          {base::TaskPriority::BACKGROUND, base::MayBlock()})),
       file_worker_(new FileWorker(file_thread_)) {
   file_thread_->PostTask(FROM_HERE,
                          base::Bind(&FileWorker::Start,
