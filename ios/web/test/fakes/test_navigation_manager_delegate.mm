23c2d09cdcafe1f5420943ae296ad9feb1eb6a2f
kkhorimoto <kkhorimoto@chromium.org>
2017-06-21 15:40:23 -0700

    Fix failing NavigationManagerUtil test that wasn't being run.
    This test was added to a suite that was not properly connected to the
    waterfall, and has been failing silently.
    
    BUG=733658
    
    Review-Url: https://codereview.chromium.org/2942103002
    Cr-Commit-Position: refs/heads/master@{#481336}

---
+++ ios/web/test/fakes/test_navigation_manager_delegate.mm
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/web/test/fakes/test_navigation_manager_delegate.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+namespace web {
+
+void TestNavigationManagerDelegate::GoToIndex(int index) {}
+void TestNavigationManagerDelegate::LoadURLWithParams(
+    const NavigationManager::WebLoadParams&) {}
+void TestNavigationManagerDelegate::Reload() {}
+void TestNavigationManagerDelegate::OnNavigationItemsPruned(
+    size_t pruned_item_count) {}
+void TestNavigationManagerDelegate::OnNavigationItemChanged() {}
+void TestNavigationManagerDelegate::OnNavigationItemCommitted(
+    const LoadCommittedDetails& load_details) {}
+WebState* TestNavigationManagerDelegate::GetWebState() {
+  return nullptr;
+}
+
+}  // namespace web
