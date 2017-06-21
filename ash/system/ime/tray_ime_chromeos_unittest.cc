333fc4ddc0bd613076445d3f24f7d6d92fb819b5
James Cook <jamescook@chromium.org>
2017-06-20 17:38:28 -0700

    chromeos: Send current IME to ash by ID rather than struct
    The current IME is always in the list of available IMEs, so just send
    its ID.
    
    Also get rid of ash::ImeInfo.selected since that can be inferred by
    comparing the ID of an IME to the current ID.
    
    Also cleanup some test init code in ImeControllerClientTest.
    
    Bug: 724305
    Test: ash_unittests and chrome unit_tests
    Change-Id: Id830048980728e30c6ae39acd833e26beb83ff64
    Reviewed-on: https://chromium-review.googlesource.com/540665
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481069}

--- ash/system/ime/tray_ime_chromeos_unittest.cc
+++ ash/system/ime/tray_ime_chromeos_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/ime_menu/ime_list_view.h"
 #include "ash/system/tray/system_tray_notifier.h"
 #include "ash/test/ash_test_base.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "ui/events/devices/device_data_manager.h"
 #include "ui/keyboard/keyboard_util.h"
@@ -85,6 +86,8 @@ void TrayIMETest::SetAccessibilityKeyboardEnabled(bool enabled) {
 
 void TrayIMETest::SetActiveImeCount(int count) {
   available_imes_.resize(count);
+  for (int i = 0; i < count; ++i)
+    available_imes_[i].id = base::IntToString(i);
   RefreshImeController();
 }
 
@@ -145,8 +148,6 @@ void TrayIMETest::SetUp() {
 }
 
 void TrayIMETest::RefreshImeController() {
-  mojom::ImeInfoPtr current_ime_ptr = current_ime_.Clone();
-
   std::vector<mojom::ImeInfoPtr> available_ime_ptrs;
   for (const auto& ime : available_imes_)
     available_ime_ptrs.push_back(ime.Clone());
@@ -155,7 +156,7 @@ void TrayIMETest::RefreshImeController() {
   for (const auto& item : menu_items_)
     menu_item_ptrs.push_back(item.Clone());
 
-  Shell::Get()->ime_controller()->RefreshIme(std::move(current_ime_ptr),
+  Shell::Get()->ime_controller()->RefreshIme(current_ime_.id,
                                              std::move(available_ime_ptrs),
                                              std::move(menu_item_ptrs));
 }
