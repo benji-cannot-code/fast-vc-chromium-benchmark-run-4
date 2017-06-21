c5d042c024132f7ddaae3c03cde5bf47a08fe0bf
liberato@chromium.org <liberato@chromium.org>
2017-06-21 14:16:52 -0700

    Use fullscreen hint from WMPI in AVDA.
    Previously, WMPI would send / revoke the overlay factory during
    fullscreen transitions.  AVDA simply chose an overlay based on
    whether it had a CVV surface id (or AndroidOverlay routing token).
    
    That doesn't allow AVDA to make smarter decisions based on local
    information, such as whether the overlay is currently supported
    by the browser compositor.
    
    This CL retains that behavior for ContentVideoView.  For AO,
    it provides the routing token immediately, and sends signals about
    the current fullscreen state to AVDA.  AndroidVideoSurfaceChooser
    now uses these signals to choose between SurfaceTexture and overlay.
    
    This CL also adds is_secure to the chooser, and makes AVDA set it.
    
    Bug: 732866
    Test: AndroidVideoSurfaceChooser
    Change-Id: I307679865ce8e6623a2f7a3e6709c808f5faf806
    Reviewed-on: https://chromium-review.googlesource.com/540182
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Chris Watkins <watk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481367}

--- media/gpu/android_video_surface_chooser.h
+++ media/gpu/android_video_surface_chooser.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "base/optional.h"
 #include "media/base/android/android_overlay.h"
 #include "media/gpu/media_gpu_export.h"
 
@@ -17,6 +18,16 @@ namespace media {
 // Manage details of which surface to use for video playback.
 class MEDIA_GPU_EXPORT AndroidVideoSurfaceChooser {
  public:
+  // Input state used for choosing the surface type.
+  struct State {
+    bool is_fullscreen = false;
+
+    // Does playback require a secure surface?
+    bool is_secure = false;
+
+    // TODO(liberato): add compositor feedback.
+  };
+
   // Notify the client that |overlay| is ready for use.  The client may get
   // the surface immediately.
   using UseOverlayCB =
@@ -36,11 +47,15 @@ class MEDIA_GPU_EXPORT AndroidVideoSurfaceChooser {
   // an empty callback to indicate "no factory".
   virtual void Initialize(UseOverlayCB use_overlay_cb,
                           UseSurfaceTextureCB use_surface_texture_cb,
-                          AndroidOverlayFactoryCB initial_factory) = 0;
+                          AndroidOverlayFactoryCB initial_factory,
+                          const State& initial_state) = 0;
 
-  // Notify us that a new factory has arrived.  May be is_null() to indicate
-  // that the most recent factory has been revoked.
-  virtual void ReplaceOverlayFactory(AndroidOverlayFactoryCB factory) = 0;
+  // Notify us that a new factory has arrived and / or other state is available.
+  // |*new_factory| may be an is_null() callback to indicate that the most
+  // recent factory has been revoked.  |!new_factory.has_value()| results in no
+  // change to the factory.
+  virtual void UpdateState(base::Optional<AndroidOverlayFactoryCB> new_factory,
+                           const State& new_state) = 0;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(AndroidVideoSurfaceChooser);
