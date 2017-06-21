40168f7cf3433ef7cdfa1778d87fb8e82137dab1
Min Qin <qinmin@chromium.org>
2017-06-20 20:18:57 -0700

    Add new UMA to record image download issues
    User are reporting that they cannot download images.
    This CL adds some UMA to record some metrics about the failure
    The UMA includes duplicate infobar, storage permission and context menu option.
    
    Bug=734194
    
    Change-Id: Idb43dc03cd14c99488d253a8a76bd7685ce53cb6
    Reviewed-on: https://chromium-review.googlesource.com/541684
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481113}

--- chrome/browser/android/download/chrome_duplicate_download_infobar_delegate.cc
+++ chrome/browser/android/download/chrome_duplicate_download_infobar_delegate.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/path_utils.h"
 #include "base/memory/ptr_util.h"
+#include "base/metrics/histogram_macros.h"
 #include "chrome/browser/android/download/download_controller.h"
 #include "chrome/browser/download/download_path_reservation_tracker.h"
 #include "chrome/browser/infobars/infobar_service.h"
@@ -68,6 +69,7 @@ ChromeDuplicateDownloadInfoBarDelegate::ChromeDuplicateDownloadInfoBarDelegate(
       is_off_the_record_(download_item->GetBrowserContext()->IsOffTheRecord()),
       file_selected_callback_(file_selected_callback) {
   download_item_->AddObserver(this);
+  RecordDuplicateInfobarType(INFOBAR_SHOWN);
 }
 
 infobars::InfoBarDelegate::InfoBarIdentifier
@@ -76,12 +78,16 @@ ChromeDuplicateDownloadInfoBarDelegate::GetIdentifier() const {
 }
 
 bool ChromeDuplicateDownloadInfoBarDelegate::Accept() {
-  if (!download_item_)
+  if (!download_item_) {
+    RecordDuplicateInfobarType(INFOBAR_DOWNLOAD_CANCELED);
     return true;
+  }
 
   base::FilePath download_dir;
-  if (!base::android::GetDownloadsDirectory(&download_dir))
+  if (!base::android::GetDownloadsDirectory(&download_dir)) {
+    RecordDuplicateInfobarType(INFOBAR_NO_DOWNLOAD_DIR);
     return true;
+  }
 
   DownloadPathReservationTracker::GetReservedPath(
       download_item_,
@@ -90,6 +96,7 @@ bool ChromeDuplicateDownloadInfoBarDelegate::Accept() {
       true,
       DownloadPathReservationTracker::UNIQUIFY,
       base::Bind(&CreateNewFileDone, file_selected_callback_));
+  RecordDuplicateInfobarType(INFOBAR_CREATE_NEW_FILE);
   return true;
 }
 
@@ -115,6 +122,12 @@ void ChromeDuplicateDownloadInfoBarDelegate::InfoBarDismissed() {
 
 bool ChromeDuplicateDownloadInfoBarDelegate::IsOffTheRecord() const {
   return is_off_the_record_;
+}
+
+void ChromeDuplicateDownloadInfoBarDelegate::RecordDuplicateInfobarType(
+    DuplicateInfobarType type) {
+  UMA_HISTOGRAM_ENUMERATION("MobileDownload.DuplicateInfobar", type,
+                            INFOBAR_MAX);
 }
 
 }  // namespace android
