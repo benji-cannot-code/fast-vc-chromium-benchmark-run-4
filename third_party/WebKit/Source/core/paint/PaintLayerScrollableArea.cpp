adb3f4788fa46ff6e7dea0c03af0a90968720e99
Stefan Zager <szager@chromium.org>
2017-06-21 15:43:34 -0700

    [RootLayerScrolls] Fix user-input-scrollable logic
    The logic for preventing viewport scrolling when there's a fullscreen element needs
    to run even if the root layer doesn't have a composited scrolling layer.
    
    BUG=490942
    R=skobes@chromium.
    TEST=WebFrameTest with RLS enabled
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I8a399eb52756f4516c1154a8fab2ce2c75ceac76
    Reviewed-on: https://chromium-review.googlesource.com/526857
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481393}

--- third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.cpp
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/PseudoStyleRequest.h"
 #include "core/dom/AXObjectCache.h"
 #include "core/dom/DOMNodeIds.h"
+#include "core/dom/Fullscreen.h"
 #include "core/dom/Node.h"
 #include "core/dom/TaskRunnerHelper.h"
 #include "core/dom/shadow/ShadowRoot.h"
@@ -679,13 +680,17 @@ bool PaintLayerScrollableArea::UserInputScrollable(
     return true;
 
   if (Box().IsLayoutView()) {
+    Document& document = Box().GetDocument();
+    Element* fullscreen_element = Fullscreen::FullscreenElementFrom(document);
+    if (fullscreen_element && fullscreen_element != document.documentElement())
+      return false;
+
     ScrollbarMode h_mode;
     ScrollbarMode v_mode;
     ToLayoutView(Box()).CalculateScrollbarModes(h_mode, v_mode);
-    if (orientation == kHorizontalScrollbar && h_mode == kScrollbarAlwaysOff)
-      return false;
-    if (orientation == kVerticalScrollbar && v_mode == kScrollbarAlwaysOff)
-      return false;
+    ScrollbarMode mode =
+        (orientation == kHorizontalScrollbar) ? h_mode : v_mode;
+    return mode == kScrollbarAuto || mode == kScrollbarAlwaysOn;
   }
 
   EOverflow overflow_style = (orientation == kHorizontalScrollbar)
