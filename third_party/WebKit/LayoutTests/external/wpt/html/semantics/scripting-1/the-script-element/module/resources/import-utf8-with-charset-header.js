481a89236f768c8ee1d3d671be6da8466903191b
Hiroshige Hayashizaki <hiroshige@chromium.org>
2017-06-21 12:47:51 -0700

    Enforce UTF-8 decoding for module scripts
    Bug: 725654
    Change-Id: I51495ca34d641d02bc2f73384900a5bd6b8543a3
    Reviewed-on: https://chromium-review.googlesource.com/534456
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481416}

---
+++ third_party/WebKit/LayoutTests/external/wpt/html/semantics/scripting-1/the-script-element/module/resources/import-utf8-with-charset-header.js
@@ -0,0 +1,2 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import "../../serve-with-content-type.py?fn=external-script-utf8.js&ct=text/javascript%3Bcharset=windows-1250&dummy=6";
