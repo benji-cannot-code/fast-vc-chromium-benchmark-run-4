97b6c392965a937e06d1b5e8203a3f88a6afabee
yosin <yosin@chromium.org>
2017-06-21 01:59:09 -0700

    Refactor AdjustInlineBoxPositionForPrimaryDirection()
    This patch utilizes const-variable for improving readability.
    
    BUG=n/a
    TEST=n/a; no behavior changes
    
    Review-Url: https://codereview.chromium.org/2953543002
    Cr-Commit-Position: refs/heads/master@{#481151}

--- third_party/WebKit/Source/core/editing/VisibleUnits.cpp
+++ third_party/WebKit/Source/core/editing/VisibleUnits.cpp
@@ -767,9 +767,10 @@ static InlineBoxPosition AdjustInlineBoxPositionForPrimaryDirection(
       return InlineBoxPosition(inline_box, caret_offset);
 
     // For example, abc 123 ^ CBA
-    inline_box = InlineBoxTraversal::FindRightBoundaryOfEntireBidiRun(
-        *inline_box, level);
-    return InlineBoxPosition(inline_box, inline_box->CaretRightmostOffset());
+    InlineBox* const result_box =
+        InlineBoxTraversal::FindRightBoundaryOfEntireBidiRun(*inline_box,
+                                                             level);
+    return InlineBoxPosition(result_box, result_box->CaretRightmostOffset());
   }
 
   if (IsStartOfDifferentDirection(inline_box))
@@ -782,9 +783,9 @@ static InlineBoxPosition AdjustInlineBoxPositionForPrimaryDirection(
   if (next_box && next_box->BidiLevel() == level)
     return InlineBoxPosition(inline_box, caret_offset);
 
-  inline_box =
+  InlineBox* const result_box =
       InlineBoxTraversal::FindLeftBoundaryOfEntireBidiRun(*inline_box, level);
-  return InlineBoxPosition(inline_box, inline_box->CaretLeftmostOffset());
+  return InlineBoxPosition(result_box, result_box->CaretLeftmostOffset());
 }
 
 static InlineBoxPosition AdjustInlineBoxPositionForTextDirection(
