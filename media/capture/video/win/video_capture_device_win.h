47ee6ec8d64a91213068011c46383ebbd3bc9d5f
Sami Kalliomäki <sakal@chromium.org>
2017-06-21 11:11:10 +0000

    Revert "[Video/Image Capture] Put Windows image capture controls behind feature flag"
    This reverts commit f21fba7db4b89356077c052c49b165f3e48c9305.
    
    Reason for revert: Speculatively reverting, might have caused
    chromium.webrtc tests to start failing. Failing tests:
    https://build.chromium.org/p/chromium.webrtc/builders/Win8%20Tester/builds/35162
    https://build.chromium.org/p/chromium.webrtc/builders/Win10%20Tester/builds/17320
    
    Original change's description:
    > [Video/Image Capture] Put Windows image capture controls behind feature flag
    > 
    > The drivers for several devices on Windows appear to not properly handle
    > querying for controls used in the context of Image Capture. These cases lead to
    > video capture being broken and users seeing only a blank image.
    > 
    > This CL adds a feature flag kImageCaptureControls Image Capture on Windows. 
    > For now, we disable this feature by default. The feature can be enabled by 
    > adding --enable-features=ImageCaptureControls to the command-line.
    > 
    > Bug: 722038
    > Test: Manual tested on Windows using iSpy virtual device.
    > Change-Id: I654ff72772af10c2d5356d5131ed7ed98e300c1f
    > Reviewed-on: https://chromium-review.googlesource.com/530076
    > Reviewed-by: Dan Sanders <sandersd@chromium.org>
    > Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    > Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#480999}
    
    TBR=mcasas@chromium.org,sandersd@chromium.org,emircan@chromium.org,chfremer@chromium.org
    
    Change-Id: I12d976ca1c81c55a685360bb23817dca605085ad
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 722038, 735379
    Reviewed-on: https://chromium-review.googlesource.com/543137
    Reviewed-by: Henrik Kjellander <kjellander@chromium.org>
    Commit-Queue: Sami Kalliomäki <sakal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481173}

--- media/capture/video/win/video_capture_device_win.h
+++ media/capture/video/win/video_capture_device_win.h
@@ -66,8 +66,8 @@ class VideoCaptureDeviceWin : public VideoCaptureDevice,
   static VideoPixelFormat TranslateMediaSubtypeToPixelFormat(
       const GUID& sub_type);
 
-  VideoCaptureDeviceWin(const VideoCaptureDeviceDescriptor& device_descriptor,
-                        bool allow_image_capture_controls);
+  explicit VideoCaptureDeviceWin(
+      const VideoCaptureDeviceDescriptor& device_descriptor);
   ~VideoCaptureDeviceWin() override;
   // Opens the device driver for this device.
   bool Init();
@@ -90,8 +90,6 @@ class VideoCaptureDeviceWin : public VideoCaptureDevice,
                  // User needs to recover by destroying the object.
   };
 
-  void InitializeVideoAndCameraControls();
-
   // Implements SinkFilterObserver.
   void FrameReceived(const uint8_t* buffer,
                      int length,
@@ -105,7 +103,6 @@ class VideoCaptureDeviceWin : public VideoCaptureDevice,
                      HRESULT hr);
 
   const VideoCaptureDeviceDescriptor device_descriptor_;
-  const bool allow_image_capture_controls_;
   InternalState state_;
   std::unique_ptr<VideoCaptureDevice::Client> client_;
 
