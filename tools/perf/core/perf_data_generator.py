d07fc2b6695d114a1d4f9e14bb6e01742af05b4c
Charlie Andrews <charliea@google.com>
2017-06-21 15:02:30 -0700

    Disable performance_browser_tests on Win 7 and Win 7 Nvidia GPU
    TBR=nednguyen@google.com,miu@chromium.org
    
    Bug: 735679
    Change-Id: I17f2519c250e2b4e9da5b1ff05e0e5d4ca117b39
    Reviewed-on: https://chromium-review.googlesource.com/544235
    Commit-Queue: Charlie Andrews <charliea@chromium.org>
    Reviewed-by: Charlie Andrews <charliea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481372}

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -416,7 +416,8 @@ def get_waterfall_config():
           ],
        'perf_tests': [
          ('load_library_perf_tests', 'build187-m1'),
-         ('performance_browser_tests', 'build187-m1'),
+         # crbug.com/735679
+         # ('performance_browser_tests', 'build187-m1'),
          ('media_perftests', 'build188-m1')]
       }
     ])
@@ -489,7 +490,8 @@ def get_waterfall_config():
        'perf_tests': [
          ('angle_perftests', 'build94-m1'),
          ('load_library_perf_tests', 'build94-m1'),
-         ('performance_browser_tests', 'build94-m1'),
+         # crbug.com/735679
+         # ('performance_browser_tests', 'build94-m1'),
          ('media_perftests', 'build95-m1')]
       }
     ])
