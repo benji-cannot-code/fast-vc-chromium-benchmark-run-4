c3a7453d2f5c82445025349e2ce2ce213adf00f6
Matthew Jones <mdjones@chromium.org>
2017-06-22 12:01:29 -0700

    [Home] Implement new tab creation animation from tab switcher
    This change introduces the animation for creating a new tab from the
    tab switcher. A previously unused code path through StackLayout has
    been replaced by the logic for this animation. The primary additions
    are as follows:
    
    - New tab slides up in 300ms.
    - Existing tabs in stack move to top of screen.
      - Landscape mode excludes this animation.
    
    BUG=731294
    
    Change-Id: Iedb542f08eb75bbb50f33eee4bf5e113d4d05c56
    Reviewed-on: https://chromium-review.googlesource.com/541917
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481636}

--- chrome/android/javatests/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheetNewTabControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheetNewTabControllerTest.java
@@ -218,6 +218,7 @@ public class BottomSheetNewTabControllerTest {
             @Override
             public void run() {
                 mBottomSheet.loadUrl(new LoadUrlParams("about:blank"), incognito);
+                mActivity.getLayoutManager().getActiveLayout().finishAnimationsForTests();
             }
         });
     }
