01235190a39b946e0a39d7e6c9b7f508ba18cb89
rbpotter <rbpotter@chromium.org>
2017-06-21 22:35:17 -0700

    Change getAccessToken and getExtensionPrinterAccess to sendWithPromise
    Change getAccessToken and getExtensionPrinterAccess to sendWithPromise
    in print preview.
    
    BUG=717296
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2938073003
    Cr-Commit-Position: refs/heads/master@{#481451}

--- chrome/browser/resources/print_preview/data/destination.js
+++ chrome/browser/resources/print_preview/data/destination.js
@@ -23,7 +23,6 @@ print_preview.DestinationType = {
 print_preview.DestinationOrigin = {
   LOCAL: 'local',
   COOKIES: 'cookies',
-  PROFILE: 'profile',
   DEVICE: 'device',
   PRIVET: 'privet',
   EXTENSION: 'extension',
