cc36accd77e6f3ddb626433c40bf85463ddcd684
Takeshi Yoshino <tyoshino@chromium.org>
2017-06-22 20:48:14 +0900

    Don't store RequestContext in WorkerScriptLoader
    WorkerScriptLoader doesn't need to store it. The setter is always called
    right before kicking the loader. We should just pass it via the
    argument of the Load.* method.
    
    As a bonus, inlined CreateResourceRequest(). It reduces copy and makes
    the code simpler given that after removing SetRequestContext() call from
    it, it's small enough to be inlined.
    
    Bug: 
    Change-Id: I8fe423921cad2d2a40f213e7310e8e991ae52825
    Reviewed-on: https://chromium-review.googlesource.com/544418
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Takeshi Yoshino <tyoshino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481823}

--- third_party/WebKit/Source/core/workers/WorkerScriptLoader.cpp
+++ third_party/WebKit/Source/core/workers/WorkerScriptLoader.cpp
@@ -52,7 +52,6 @@ namespace blink {
 WorkerScriptLoader::WorkerScriptLoader()
     : response_callback_(nullptr),
       finished_callback_(nullptr),
-      request_context_(WebURLRequest::kRequestContextWorker),
       response_address_space_(kWebAddressSpacePublic) {}
 
 WorkerScriptLoader::~WorkerScriptLoader() {
@@ -67,11 +66,16 @@ WorkerScriptLoader::~WorkerScriptLoader() {
 void WorkerScriptLoader::LoadSynchronously(
     ExecutionContext& execution_context,
     const KURL& url,
+    WebURLRequest::RequestContext request_context,
     WebAddressSpace creation_address_space) {
   url_ = url;
   execution_context_ = &execution_context;
 
-  ResourceRequest request(CreateResourceRequest(creation_address_space));
+  ResourceRequest request(url);
+  request.SetHTTPMethod(HTTPNames::GET);
+  request.SetExternalRequestStateFromRequestorAddressSpace(
+      creation_address_space);
+  request.SetRequestContext(request_context);
   request.SetFetchCredentialsMode(WebURLRequest::kFetchCredentialsModeInclude);
 
   SECURITY_DCHECK(execution_context.IsWorkerGlobalScope());
@@ -92,6 +96,7 @@ void WorkerScriptLoader::LoadSynchronously(
 void WorkerScriptLoader::LoadAsynchronously(
     ExecutionContext& execution_context,
     const KURL& url,
+    WebURLRequest::RequestContext request_context,
     WebURLRequest::FetchRequestMode fetch_request_mode,
     WebURLRequest::FetchCredentialsMode fetch_credentials_mode,
     WebAddressSpace creation_address_space,
@@ -103,7 +108,11 @@ void WorkerScriptLoader::LoadAsynchronously(
   url_ = url;
   execution_context_ = &execution_context;
 
-  ResourceRequest request(CreateResourceRequest(creation_address_space));
+  ResourceRequest request(url);
+  request.SetHTTPMethod(HTTPNames::GET);
+  request.SetExternalRequestStateFromRequestorAddressSpace(
+      creation_address_space);
+  request.SetRequestContext(request_context);
   request.SetFetchCredentialsMode(fetch_credentials_mode);
 
   ThreadableLoaderOptions options;
@@ -127,16 +136,6 @@ void WorkerScriptLoader::LoadAsynchronously(
 const KURL& WorkerScriptLoader::ResponseURL() const {
   DCHECK(!Failed());
   return response_url_;
-}
-
-ResourceRequest WorkerScriptLoader::CreateResourceRequest(
-    WebAddressSpace creation_address_space) {
-  ResourceRequest request(url_);
-  request.SetHTTPMethod(HTTPNames::GET);
-  request.SetRequestContext(request_context_);
-  request.SetExternalRequestStateFromRequestorAddressSpace(
-      creation_address_space);
-  return request;
 }
 
 void WorkerScriptLoader::DidReceiveResponse(
