6a4f6a7e61446ba5a8c815ddf092f27a64684a72
eugenebut <eugenebut@chromium.org>
2017-06-22 10:39:13 -0700

    Gracefully handle didCommitNavigation: call after didFinishNavigation:
    It is possible to get WKNavigationDelegate callbacks in the following
    order: didFinishNavigation, didCommitNavigation:.
    
    In this case didFinishNavigation should not remove navigation from
    CRWWKNavigationStates storage, because that navigation will be used in
    didCommitNavigation.
    
    Also didCommitNavigation should remove navigation and signal
    didFinishNavigation: which is expected to be called only after
    navigation is committed.
    
    BUG=727289
    
    Review-Url: https://codereview.chromium.org/2950853002
    Cr-Commit-Position: refs/heads/master@{#481584}

--- ios/web/web_state/ui/crw_wk_navigation_states.mm
+++ ios/web/web_state/ui/crw_wk_navigation_states.mm
@@ -123,12 +123,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         [[CRWWKNavigationsStateRecord alloc] initWithState:state
                                                      index:++_lastStateIndex];
   } else {
-    DCHECK(
-        record.state < state ||
-        (record.state == state && state == web::WKNavigationState::REDIRECTED));
+    DCHECK(record.state < state ||
+           // Redirect can be called multiple times.
+           (record.state == state &&
+            state == web::WKNavigationState::REDIRECTED) ||
+           // didFinishNavigation can be called before didCommitNvigation.
+           (record.state == web::WKNavigationState::FINISHED &&
+            state == web::WKNavigationState::COMMITTED));
     record.state = state;
   }
   [_records setObject:record forKey:key];
+}
+
+- (web::WKNavigationState)stateForNavigation:(WKNavigation*)navigation {
+  id key = [self keyForNavigation:navigation];
+  CRWWKNavigationsStateRecord* record = [_records objectForKey:key];
+  return record ? record.state : web::WKNavigationState::NONE;
 }
 
 - (void)removeNavigation:(WKNavigation*)navigation {
