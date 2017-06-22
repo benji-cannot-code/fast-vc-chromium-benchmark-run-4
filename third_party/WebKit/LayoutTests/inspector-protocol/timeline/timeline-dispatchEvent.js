818ed19ec152daf0194c969953ba070b64e5e6a6
dgozman <dgozman@chromium.org>
2017-06-22 16:52:40 -0700

    [DevTools] Migrate inspector-protocol/{timeline,worker} tests to new harness
    BUG=734762
    
    Review-Url: https://codereview.chromium.org/2953663003
    Cr-Commit-Position: refs/heads/master@{#481726}

---
+++ third_party/WebKit/LayoutTests/inspector-protocol/timeline/timeline-dispatchEvent.js
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  let {page, session, dp} = await testRunner.startHTML(`
+    <div id='my-div'></div>
+  `, '');
+
+  function performAction() {
+    var div = document.querySelector('#my-div');
+    div.addEventListener('click', function(e) {  }, false);
+    div.click();
+
+    var iframe = document.createElement('iframe');
+    div.appendChild(iframe);
+    return new Promise(resolve => {
+      iframe.onload = resolve;
+      iframe.src = 'blank.html';
+    });
+  }
+
+  var TracingHelper = await testRunner.loadScript('../resources/tracing-test.js');
+  var tracingHelper = new TracingHelper(testRunner, session);
+  await tracingHelper.invokeAsyncWithTracing(performAction);
+
+  var windowEventNames = [ 'click', 'beforeunload', 'unload', 'load' ];
+  for (var eventName of windowEventNames) {
+    var events = tracingHelper.filterEvents(e => e.name === 'EventDispatch' && e.args.data.type === eventName);
+    if (events.length >= 1)
+      testRunner.log('SUCCESS: found ' + eventName + ' event');
+    else
+      testRunner.log('FAIL: ' + eventName + ' event is missing; devtools.timeline events: ' + tracingHelper.formattedEvents());
+  }
+
+  testRunner.completeTest();
+})
