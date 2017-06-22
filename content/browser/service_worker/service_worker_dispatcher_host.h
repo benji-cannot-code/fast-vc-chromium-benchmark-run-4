c19fdf2d0e9baef8532db104ccaf359273746480
kinuko <kinuko@chromium.org>
2017-06-22 01:02:16 -0700

    Implement dumb URLLoader{Factory} for ServiceWorker script loading
    Currently it only proxies request/response to/from the network factory,
    plus does necessary HttpResponseInfo passing.
    
    The plan is to use this URLLoader for script loading/caching during
    installation. (Currently this always sets-up the URLLoader{Factory}
    if servicification is enabled because we need it for correctly setting
    HttpResponseInfo of the main resource of ServiceWorker)
    
    (Patch on top of https://codereview.chromium.org/2779763004/
    Try: https://codereview.chromium.org/2936623002/)
    
    BUG=715640
    
    Review-Url: https://codereview.chromium.org/2943463002
    Cr-Commit-Position: refs/heads/master@{#481477}

--- content/browser/service_worker/service_worker_dispatcher_host.h
+++ content/browser/service_worker/service_worker_dispatcher_host.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/service_worker/service_worker_types.h"
 #include "content/public/browser/browser_message_filter.h"
 #include "mojo/public/cpp/bindings/associated_binding_set.h"
+#include "mojo/public/cpp/bindings/strong_associated_binding_set.h"
 
 class GURL;
 
@@ -55,6 +56,8 @@ class CONTENT_EXPORT ServiceWorkerDispatcherHost
       int render_process_id,
       ResourceContext* resource_context);
 
+  // |blob_storage_context| and |loader_factory_getter| are used only
+  // if IsServicificationEnabled is true.
   void Init(ServiceWorkerContextWrapper* context_wrapper);
 
   // BrowserMessageFilter implementation
--- content/browser/service_worker/service_worker_provider_host.h
+++ content/browser/service_worker/service_worker_provider_host.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/request_context_type.h"
 #include "content/public/common/resource_type.h"
 #include "mojo/public/cpp/bindings/associated_binding.h"
+#include "mojo/public/cpp/bindings/strong_associated_binding_set.h"
 
 namespace storage {
 class BlobStorageContext;
