f85468bab9bdb420b9e94ecc56dffe8fa341ecc2
timloh <timloh@chromium.org>
2017-06-21 20:53:20 -0700

    Permission prompts shouldn't cause browser to be focused
    BUG=729495
    
    Review-Url: https://codereview.chromium.org/2925773002
    Cr-Commit-Position: refs/heads/master@{#481430}

--- chrome/browser/ui/views/permission_bubble/permission_prompt_impl.cc
+++ chrome/browser/ui/views/permission_bubble/permission_prompt_impl.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/platform_util.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_dialogs.h"
+#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/layout_constants.h"
 #include "chrome/browser/ui/views/exclusive_access_bubble_views.h"
@@ -279,7 +280,15 @@ void PermissionPromptImpl::Show() {
   bubble_delegate_->set_anchor_view_insets(gfx::Insets(
       GetLayoutConstant(LOCATION_BAR_BUBBLE_ANCHOR_VERTICAL_INSET), 0));
 
-  views::BubbleDialogDelegateView::CreateBubble(bubble_delegate_)->Show();
+  views::Widget* widget =
+      views::BubbleDialogDelegateView::CreateBubble(bubble_delegate_);
+  // If a browser window (or popup) other than the bubble parent has focus,
+  // don't take focus.
+  if (browser_->window()->IsActive())
+    widget->Show();
+  else
+    widget->ShowInactive();
+
   bubble_delegate_->SizeToContents();
 
   bubble_delegate_->UpdateAnchor(GetAnchorView(),
