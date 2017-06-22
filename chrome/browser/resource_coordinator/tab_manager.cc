73130b272f3446063c337599a233f8b772975a20
bashi <bashi@chromium.org>
2017-06-22 14:47:42 -0700

    Don't disable MemoryPressureListener when MemoryCoordinator is enabled
    Memory coordinator is going to focus on how/when to request tab
    discarding and won't replace existing MemoryPressureListeners except for
    TabManager. We shouldn't disable MemoryPressureListeners.
    
    BUG=696844
    
    Review-Url: https://codereview.chromium.org/2943953002
    Cr-Commit-Position: refs/heads/master@{#481674}

--- chrome/browser/resource_coordinator/tab_manager.cc
+++ chrome/browser/resource_coordinator/tab_manager.cc
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/common/content_features.h"
 #include "content/public/common/page_importance_signals.h"
 
 #if defined(OS_CHROMEOS)
@@ -191,9 +192,11 @@ void TabManager::Start() {
         this, &TabManager::RecordRecentTabDiscard);
   }
   start_time_ = NowTicks();
-  // Create a |MemoryPressureListener| to listen for memory events.
+  // Create a |MemoryPressureListener| to listen for memory events when
+  // MemoryCoordinator is disabled. When MemoryCoordinator is enabled
+  // it asks TabManager to do tab discarding.
   base::MemoryPressureMonitor* monitor = base::MemoryPressureMonitor::Get();
-  if (monitor) {
+  if (monitor && !base::FeatureList::IsEnabled(features::kMemoryCoordinator)) {
     memory_pressure_listener_.reset(new base::MemoryPressureListener(
         base::Bind(&TabManager::OnMemoryPressure, base::Unretained(this))));
     base::MemoryPressureListener::MemoryPressureLevel level =
