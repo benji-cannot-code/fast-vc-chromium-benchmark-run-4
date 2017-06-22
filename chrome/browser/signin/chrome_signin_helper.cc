2d483fbf190054a3dec3e683c5ca86781a7c3402
droger <droger@chromium.org>
2017-06-22 06:51:27 -0700

    [signin] Generate OAuth token on Dice Signin responses
    Introduces the DiceResponseHandler class to handle the Dice responses, which
    is a new profile keyed service.
    In this CL, only the SIGNIN response is implemented, and triggers a OAuth2
    token request.
    
    BUG=730589
    
    Review-Url: https://codereview.chromium.org/2942193002
    Cr-Commit-Position: refs/heads/master@{#481518}

--- chrome/browser/signin/chrome_signin_helper.cc
+++ chrome/browser/signin/chrome_signin_helper.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_io_data.h"
 #include "chrome/browser/signin/account_reconcilor_factory.h"
 #include "chrome/browser/signin/chrome_signin_client.h"
+#include "chrome/browser/signin/chrome_signin_client_factory.h"
+#include "chrome/browser/signin/dice_response_handler.h"
 #include "chrome/browser/tab_contents/tab_util.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/common/url_constants.h"
@@ -106,6 +108,29 @@ void ProcessMirrorHeaderUIThread(
 #endif  // !defined(OS_ANDROID)
 }
 
+#if !defined(OS_ANDROID)
+void ProcessDiceHeaderUIThread(
+    const DiceResponseParams& dice_params,
+    const content::ResourceRequestInfo::WebContentsGetter&
+        web_contents_getter) {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  DCHECK_EQ(switches::AccountConsistencyMethod::kDice,
+            switches::GetAccountConsistencyMethod());
+
+  content::WebContents* web_contents = web_contents_getter.Run();
+  if (!web_contents)
+    return;
+
+  Profile* profile =
+      Profile::FromBrowserContext(web_contents->GetBrowserContext());
+  DCHECK(!profile->IsOffTheRecord());
+
+  DiceResponseHandler* dice_response_handler =
+      DiceResponseHandler::GetForProfile(profile);
+  dice_response_handler->ProcessDiceHeader(dice_params);
+}
+#endif  // !defined(OS_ANDROID)
+
 // Looks for the X-Chrome-Manage-Accounts response header, and if found,
 // tries to show the avatar bubble in the browser identified by the
 // child/route id. Must be called on IO thread.
@@ -159,8 +184,11 @@ void ProcessMirrorResponseHeaderIfExists(
 }
 
 #if !defined(OS_ANDROID)
-void ProcessDiceResponseHeaderIfExists(net::URLRequest* request,
-                                       ProfileIOData* io_data) {
+void ProcessDiceResponseHeaderIfExists(
+    net::URLRequest* request,
+    ProfileIOData* io_data,
+    const content::ResourceRequestInfo::WebContentsGetter&
+        web_contents_getter) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
 
   if (io_data->IsOffTheRecord())
@@ -195,9 +223,9 @@ void ProcessDiceResponseHeaderIfExists(net::URLRequest* request,
   if (params.user_intention == DiceAction::NONE)
     return;
 
-  // TODO(droger): Process the Dice header: on sign-in, exchange the
-  // authorization code for a refresh token, on sign-out just follow the
-  // sign-out URL.
+  content::BrowserThread::PostTask(
+      content::BrowserThread::UI, FROM_HERE,
+      base::Bind(ProcessDiceHeaderUIThread, params, web_contents_getter));
 }
 #endif  // !defined(OS_ANDROID)
 
@@ -253,15 +281,14 @@ void ProcessAccountConsistencyResponseHeaders(
     // See if the response contains the X-Chrome-Manage-Accounts header. If so
     // show the profile avatar bubble so that user can complete signin/out
     // action the native UI.
-    signin::ProcessMirrorResponseHeaderIfExists(request, io_data,
-                                                web_contents_getter);
+    ProcessMirrorResponseHeaderIfExists(request, io_data, web_contents_getter);
   } else {
-// This is a redirect.
+    // This is a redirect.
 
 #if !defined(OS_ANDROID)
     // Process the Dice header: on sign-in, exchange the authorization code for
     // a refresh token, on sign-out just follow the sign-out URL.
-    signin::ProcessDiceResponseHeaderIfExists(request, io_data);
+    ProcessDiceResponseHeaderIfExists(request, io_data, web_contents_getter);
 #endif
   }
 }
