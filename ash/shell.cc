d6c3ababad7b6db99af0dc597d933db3faa7993a
thanhph <thanhph@chromium.org>
2017-06-22 13:32:13 -0700

    Create DomAgent and CSSAgent in Chrome. When DevTools is moved out of ash to Chrome, this will enables using DevTools in Window and Linux platform.
    BUG=711343
    
    Review-Url: https://codereview.chromium.org/2887003003
    Cr-Commit-Position: refs/heads/master@{#481653}

--- ash/shell.cc
+++ ash/shell.cc
@@ -133,9 +133,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/system/devicemode.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
-#include "components/ui_devtools/devtools_server.h"
-#include "components/ui_devtools/views/ui_devtools_css_agent.h"
-#include "components/ui_devtools/views/ui_devtools_dom_agent.h"
 #include "services/preferences/public/cpp/pref_service_factory.h"
 #include "services/preferences/public/interfaces/preferences.mojom.h"
 #include "services/service_manager/public/cpp/connector.h"
@@ -851,19 +848,6 @@ void Shell::Init(const ShellInitParams& init_params) {
   views::FocusManagerFactory::Install(new AshFocusManagerFactory);
 
   wallpaper_controller_ = base::MakeUnique<WallpaperController>();
-
-  // Start devtools server
-  devtools_server_ = ui_devtools::UiDevToolsServer::Create(nullptr);
-  if (devtools_server_) {
-    auto dom_backend = base::MakeUnique<ui_devtools::UIDevToolsDOMAgent>();
-    auto css_backend =
-        base::MakeUnique<ui_devtools::UIDevToolsCSSAgent>(dom_backend.get());
-    auto devtools_client = base::MakeUnique<ui_devtools::UiDevToolsClient>(
-        "Ash", devtools_server_.get());
-    devtools_client->AddAgent(std::move(dom_backend));
-    devtools_client->AddAgent(std::move(css_backend));
-    devtools_server_->AttachClient(std::move(devtools_client));
-  }
 
   if (config == Config::MASH)
     app_list_delegate_impl_ = base::MakeUnique<AppListDelegateImpl>();
--- chrome/browser/ui/views/chrome_browser_main_extra_parts_views.h
+++ chrome/browser/ui/views/chrome_browser_main_extra_parts_views.h
@@ -21,6 +21,9 @@ class ViewsDelegate;
 }
 
 #if defined(USE_AURA)
+namespace ui_devtools {
+class UiDevToolsServer;
+}
 namespace views {
 class MusClient;
 }
@@ -46,6 +49,9 @@ class ChromeBrowserMainExtraPartsViews : public ChromeBrowserMainExtraParts {
   std::unique_ptr<views::LayoutProvider> layout_provider_;
 
 #if defined(USE_AURA)
+  // Only used when running in --enable-ui-devtools.
+  std::unique_ptr<ui_devtools::UiDevToolsServer> devtools_server_;
+
   // Not created when running in ash::Config::MUS.
   std::unique_ptr<wm::WMState> wm_state_;
 
