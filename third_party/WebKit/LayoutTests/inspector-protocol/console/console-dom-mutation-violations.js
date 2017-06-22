e20be58cd64692d127a4425cdd075a96bfae42d6
dgozman <dgozman@chromium.org>
2017-06-22 16:28:17 -0700

    [DevTools] Migrate inspector-protocol/{console,cpu-profiler} to new harness
    BUG=734762
    
    Review-Url: https://codereview.chromium.org/2952023003
    Cr-Commit-Position: refs/heads/master@{#481718}

---
+++ third_party/WebKit/LayoutTests/inspector-protocol/console/console-dom-mutation-violations.js
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  let {page, session, dp} = await testRunner.startBlank('');
+  dp.Log.onEntryAdded(testRunner.logMessage.bind(testRunner));
+  dp.Log.enable();
+  dp.Log.startViolationsReport({config: [{name: 'discouragedAPIUse', threshold: -1}]});
+  await session.evaluate(`document.body.addEventListener('DOMSubtreeModified', () => {})`);
+  testRunner.completeTest();
+})
