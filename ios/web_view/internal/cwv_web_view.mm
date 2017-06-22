0de69508576fabdd89504917478c02c51b37a077
michaeldo <michaeldo@chromium.org>
2017-06-21 19:19:25 -0700

    Ensure that global web_view state is setup when setting GoogleAPIKey.
    Setting the googleAPIKey: on CWVWebView relies on global ios_web_view
    state being configured. Ensure the state is configured even if the
    API keys are set before CWVWebViewConfiguration is used.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2937423002
    Cr-Commit-Position: refs/heads/master@{#481407}

--- ios/web_view/internal/cwv_web_view.mm
+++ ios/web_view/internal/cwv_web_view.mm
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web_view/internal/translate/cwv_translation_controller_internal.h"
 #import "ios/web_view/internal/translate/web_view_translate_client.h"
 #include "ios/web_view/internal/web_view_browser_state.h"
+#include "ios/web_view/internal/web_view_global_state_util.h"
 #import "ios/web_view/internal/web_view_java_script_dialog_presenter.h"
 #import "ios/web_view/internal/web_view_web_state_policy_decider.h"
 #import "ios/web_view/public/cwv_navigation_delegate.h"
@@ -109,6 +110,14 @@ static NSString* gUserAgentProduct = nil;
 @synthesize UIDelegate = _UIDelegate;
 @synthesize scrollView = _scrollView;
 @synthesize visibleURL = _visibleURL;
+
++ (void)initialize {
+  if (self != [CWVWebView class]) {
+    return;
+  }
+
+  ios_web_view::InitializeGlobalState();
+}
 
 + (NSString*)userAgentProduct {
   return gUserAgentProduct;
