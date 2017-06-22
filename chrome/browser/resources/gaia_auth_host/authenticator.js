ee0cf1e10a06699e62332f16553468a8bdb47efe
alemate <alemate@chromium.org>
2017-06-21 17:06:43 -0700

    ChromeOS: Add support for loading GAIA API v2
    This CL adds ability to load GAIA API v2 endpoint.
    
    BUG=709244
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2946023002
    Cr-Commit-Position: refs/heads/master@{#481365}

--- chrome/browser/resources/gaia_auth_host/authenticator.js
+++ chrome/browser/resources/gaia_auth_host/authenticator.js
@@ -32,6 +32,7 @@ cr.define('cr.login', function() {
   var GAPS_COOKIE = 'GAPS';
   var SERVICE_ID = 'chromeoslogin';
   var EMBEDDED_SETUP_CHROMEOS_ENDPOINT = 'embedded/setup/chromeos';
+  var EMBEDDED_SETUP_CHROMEOS_ENDPOINT_V2 = 'embedded/setup/v2/chromeos';
   var SAML_REDIRECTION_PATH = 'samlredirect';
   var BLANK_PAGE_URL = 'about:blank';
 
@@ -77,15 +78,16 @@ cr.define('cr.login', function() {
                      // not called before dispatching |authCopleted|.
                      // Default is |true|.
     'flow',          // One of 'default', 'enterprise', or 'theftprotection'.
-    'enterpriseDomain',  // Domain in which hosting device is (or should be)
-                         // enrolled.
-    'emailDomain',       // Value used to prefill domain for email.
-    'chromeType',        // Type of Chrome OS device, e.g. "chromebox".
-    'clientVersion',     // Version of the Chrome build.
-    'platformVersion',   // Version of the OS build.
-    'releaseChannel',    // Installation channel.
-    'endpointGen',       // Current endpoint generation.
-    'gapsCookie',        // GAPS cookie
+    'enterpriseDomain',    // Domain in which hosting device is (or should be)
+                           // enrolled.
+    'emailDomain',         // Value used to prefill domain for email.
+    'chromeType',          // Type of Chrome OS device, e.g. "chromebox".
+    'clientVersion',       // Version of the Chrome build.
+    'platformVersion',     // Version of the OS build.
+    'releaseChannel',      // Installation channel.
+    'endpointGen',         // Current endpoint generation.
+    'gapsCookie',          // GAPS cookie
+    'chromeOSApiVersion',  // GAIA Chrome OS API version
 
     // The email fields allow for the following possibilities:
     //
@@ -232,6 +234,7 @@ cr.define('cr.login', function() {
     this.gapsCookieSent_ = false;
     this.newGapsCookie_ = null;
     this.dontResizeNonEmbeddedPages = data.dontResizeNonEmbeddedPages;
+    this.chromeOSApiVersion_ = data.chromeOSApiVersion;
 
     this.initialFrameUrl_ = this.constructInitialFrameUrl_(data);
     this.reloadUrl_ = data.frameUrl || this.initialFrameUrl_;
@@ -248,7 +251,7 @@ cr.define('cr.login', function() {
 
       if (!this.onBeforeSetHeadersSet_) {
         this.onBeforeSetHeadersSet_ = true;
-        var filterPrefix = this.idpOrigin_ + EMBEDDED_SETUP_CHROMEOS_ENDPOINT;
+        var filterPrefix = this.constructChromeOSAPIUrl_();
         // This depends on gaiaUrl parameter, that is why it is here.
         this.webview_.request.onBeforeSendHeaders.addListener(
             this.onBeforeSendHeaders_.bind(this),
@@ -259,6 +262,13 @@ cr.define('cr.login', function() {
 
     this.webview_.src = this.reloadUrl_;
     this.isLoaded_ = true;
+  };
+
+  Authenticator.prototype.constructChromeOSAPIUrl_ = function() {
+    if (this.chromeOSApiVersion_ && this.chromeOSApiVersion_ == 2)
+      return this.idpOrigin_ + EMBEDDED_SETUP_CHROMEOS_ENDPOINT_V2;
+
+    return this.idpOrigin_ + EMBEDDED_SETUP_CHROMEOS_ENDPOINT;
   };
 
   /**
@@ -284,12 +294,13 @@ cr.define('cr.login', function() {
       return url;
     }
 
-    var path = data.gaiaPath;
-    if (!path && this.isNewGaiaFlow)
-      path = EMBEDDED_SETUP_CHROMEOS_ENDPOINT;
-    if (!path)
-      path = IDP_PATH;
-    var url = this.idpOrigin_ + path;
+    var url;
+    if (data.gaiaPath)
+      url = this.idpOrigin_ + data.gaiaPath;
+    else if (this.isNewGaiaFlow)
+      url = this.constructChromeOSAPIUrl_();
+    else
+      url = this.idpOrigin_ + IDP_PATH;
 
     if (this.isNewGaiaFlow) {
       if (data.chromeType)
