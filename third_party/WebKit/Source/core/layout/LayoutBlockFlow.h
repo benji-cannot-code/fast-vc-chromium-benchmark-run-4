b057d9e04bcff9f331b139def11a0dfc12d26c36
Robert Hogan <robhogan@gmail.com>
2017-06-22 21:51:17 +0100

    Hide atomic inline elements after an ellipsis
    "For the ellipsis value implementations must hide characters and atomic inline-level elements
    at the end edge of the line as necessary to fit the ellipsis, and place the ellipsis
    immediately adjacent to then end edge of the remaining inline content."
    - https://drafts.csswg.org/css-ui/#text-overflow
    
    Bug: 534798
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I39e39e17bcb8fbafd862822f9633faa8adb33d88
    Reviewed-on: https://chromium-review.googlesource.com/539460
    Commit-Queue: Robert Hogan <robhogan@gmail.com>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481714}

--- third_party/WebKit/Source/core/layout/LayoutBlockFlow.h
+++ third_party/WebKit/Source/core/layout/LayoutBlockFlow.h
@@ -967,7 +967,8 @@ class CORE_EXPORT LayoutBlockFlow : public LayoutBlock {
                                          LayoutUnit block_right_edge,
                                          LayoutUnit block_left_edge,
                                          LayoutUnit width,
-                                         const AtomicString&);
+                                         const AtomicString&,
+                                         bool found_box);
   void MarkLinesDirtyInBlockRange(LayoutUnit logical_top,
                                   LayoutUnit logical_bottom,
                                   RootInlineBox* highest = nullptr);
