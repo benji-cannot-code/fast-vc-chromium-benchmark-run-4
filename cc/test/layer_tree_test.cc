9bb4a464d05d867a4767403f6894327b515515a4
khushalsagar <khushalsagar@chromium.org>
2017-06-23 16:03:46 -0700

    cc: Move pre-decoding for checkerable images to image worker.
    Currently images for pre-decoded but not pre-painted tiles are decoded
    on the background tile worker, even for checkerable images. This has
    the following downsides:
    
    1) It creates cases where decoding of images for lower priority
    pre-decode tiles starts pre-empting a higher priority decode running on
    the image worker.
    
    2) Since the checker image tracking is not aware of pre-decoded images,
    we still checker them when these tiles are first rasterized, even while
    the image could be currently locked in the decode cache.
    
    3) If the image is in both pre-paint and pre-decode region, then we
    could be decoding them on both, the raster worker and image worker.
    
    This change moves decoding of checkerable images for these tiles to the
    image worker, similar to the rasterized tiles. While invalidating is
    not neccesary for these images, we use the same mechanism in order to
    generate tasks for uploading these images in the GPU cache.
    
    BUG=728796
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2924233002
    Cr-Commit-Position: refs/heads/master@{#482062}

--- cc/test/layer_tree_test.cc
+++ cc/test/layer_tree_test.cc
@@ -231,6 +231,7 @@ class LayerTreeHostImplForTesting : public LayerTreeHostImpl {
     block_impl_side_invalidation_ = block;
     if (!block_impl_side_invalidation_ && impl_side_invalidation_was_blocked_) {
       RequestImplSideInvalidation();
+      impl_side_invalidation_was_blocked_ = false;
     }
   }
 
@@ -282,6 +283,7 @@ class LayerTreeHostImplForTesting : public LayerTreeHostImpl {
   }
 
   void RequestImplSideInvalidation() override {
+    test_hooks_->DidReceiveImplSideInvalidationRequest(this);
     if (block_impl_side_invalidation_) {
       impl_side_invalidation_was_blocked_ = true;
       return;
