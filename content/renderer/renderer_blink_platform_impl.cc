12b844ea7e6be7885679169f6614368537bde2a8
Khushal <khushalsagar@chromium.org>
2017-06-23 03:31:00 +0000

    Revert "Use GpuMemoryBuffers for OffscreenCanvas"
    This reverts commit 46742f7890323f88e6d7b0d23dc058ec7736d572.
    
    Reason for revert: Pixel_OffscreenCanvasUnaccelerated2DGPUCompositing[Worker] failing on Android bots. See crbug.com/736022.
    
    Original change's description:
    > Use GpuMemoryBuffers for OffscreenCanvas
    > 
    > This CL Makes use of GpuMemoryBuffers in cases where the canvas context
    > is not accelerated but the compositor is. This includes WebGL
    > contexts that are rendered using swiftshader.
    > 
    > GpuMemoryBuffers will also be used for GPU-accelerated canvases on
    > platforms that support gfx::BufferUsage::SCANOUT (i.e. MacOS and
    > Ozone).
    > 
    > Bug: 646022
    > Change-Id: If00a828b769e989a58919b7b3133c0eea3faecd5
    > Reviewed-on: https://chromium-review.googlesource.com/533699
    > Commit-Queue: Justin Novosad <junov@chromium.org>
    > Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    > Reviewed-by: Jesse Doherty <jwd@chromium.org>
    > Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#481355}
    
    TBR=reveman@chromium.org,jwd@chromium.org,junov@chromium.org,fserb@chromium.org,piman@chromium.org,jochen@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 646022
    Change-Id: I19e7c9ed068a13cc0aa2b3c50fa6fdd0b00655d6
    Reviewed-on: https://chromium-review.googlesource.com/545185
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481963}

--- content/renderer/renderer_blink_platform_impl.cc
+++ content/renderer/renderer_blink_platform_impl.cc
@@ -278,12 +278,9 @@ RendererBlinkPlatformImpl::RendererBlinkPlatformImpl(
         RenderThreadImpl::current()->GetIOTaskRunner().get()));
     web_database_observer_impl_.reset(
         new WebDatabaseObserverImpl(sync_message_filter_.get()));
-    gpu_memory_buffer_manager_ =
-        RenderThreadImpl::current()->GetGpuMemoryBufferManager();
   } else {
     service_manager::mojom::ConnectorRequest request;
     connector_ = service_manager::Connector::Create(&request);
-    gpu_memory_buffer_manager_ = nullptr;
   }
 
   top_level_blame_context_.Initialize();
@@ -1085,8 +1082,8 @@ RendererBlinkPlatformImpl::CreateSharedOffscreenGraphicsContext3DProvider() {
 
 gpu::GpuMemoryBufferManager*
 RendererBlinkPlatformImpl::GetGpuMemoryBufferManager() {
-  DCHECK(gpu_memory_buffer_manager_);  // Does not work in unit tests
-  return gpu_memory_buffer_manager_;
+  RenderThreadImpl* thread = RenderThreadImpl::current();
+  return thread ? thread->GetGpuMemoryBufferManager() : nullptr;
 }
 
 //------------------------------------------------------------------------------
