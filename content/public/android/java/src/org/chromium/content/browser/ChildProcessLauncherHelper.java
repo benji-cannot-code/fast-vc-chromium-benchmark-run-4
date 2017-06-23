c846bf5d2feb63749409f4714c86bcd127c6b6e9
Jay Civelli <jcivelli@google.com>
2017-06-23 10:52:53 -0700

    Moving allocator clean-up to a listener.
    Moving the clean-up of ChildConnectionAllocator from freeConnection
    to a specific listener, in preparation of moving clean-up of
    connections from ChildProcessLauncher to the allocator itself.
    
    Bug: 702316
    Change-Id: I90755e475a94b34840868d3e788d7e7c1cfe2f79
    Reviewed-on: https://chromium-review.googlesource.com/544085
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Jay Civelli <jcivelli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481975}

--- content/public/android/java/src/org/chromium/content/browser/ChildProcessLauncherHelper.java
+++ content/public/android/java/src/org/chromium/content/browser/ChildProcessLauncherHelper.java
@@ -297,7 +297,8 @@ public class ChildProcessLauncherHelper {
     static ChildConnectionAllocator getConnectionAllocator(
             Context context, ChildProcessCreationParams creationParams, boolean sandboxed) {
         assert LauncherThread.runningOnLauncherThread();
-        String packageName = getPackageNameFromCreationParams(context, creationParams, sandboxed);
+        final String packageName =
+                getPackageNameFromCreationParams(context, creationParams, sandboxed);
         boolean bindAsExternalService =
                 isServiceExternalFromCreationParams(creationParams, sandboxed);
 
@@ -334,6 +335,21 @@ public class ChildProcessLauncherHelper {
                         sSandboxedServiceFactoryForTesting);
             }
             sSandboxedChildConnectionAllocatorMap.put(packageName, connectionAllocator);
+            final ChildConnectionAllocator finalConnectionAllocator = connectionAllocator;
+            // Tracks connections removal so the allocator can be freed when no longer used.
+            connectionAllocator.addListener(new ChildConnectionAllocator.Listener() {
+                @Override
+                public void onConnectionFreed(
+                        ChildConnectionAllocator allocator, ChildProcessConnection connection) {
+                    assert allocator == finalConnectionAllocator;
+                    if (!allocator.anyConnectionAllocated()) {
+                        allocator.removeListener(this);
+                        ChildConnectionAllocator removedAllocator =
+                                sSandboxedChildConnectionAllocatorMap.remove(packageName);
+                        assert removedAllocator == allocator;
+                    }
+                }
+            });
         }
         return sSandboxedChildConnectionAllocatorMap.get(packageName);
     }
@@ -514,12 +530,6 @@ public class ChildProcessLauncherHelper {
             public void run() {
                 assert connectionAllocator != null;
                 connectionAllocator.free(connection);
-                String packageName = connectionAllocator.getPackageName();
-                if (!connectionAllocator.anyConnectionAllocated()
-                        && sSandboxedChildConnectionAllocatorMap.get(packageName)
-                                == connectionAllocator) {
-                    sSandboxedChildConnectionAllocatorMap.remove(packageName);
-                }
             }
         }, FREE_CONNECTION_DELAY_MILLIS);
     }
