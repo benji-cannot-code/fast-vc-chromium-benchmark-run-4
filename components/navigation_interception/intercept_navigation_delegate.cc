e02d1abc4fa8546c25921de999604298afd07727
Jinsuk Kim <jinsukkim@chromium.org>
2017-06-23 17:10:12 +0200

    Revert "PlzNavigate: Run InterceptnavigationThrottle in synchronous mode"
    Reason for revert: Other part in navigation flow still depends on
    the asynchronous behavior of InterceptNavigationThrottle. Reverting
    to avoid the reported crashes. Will revisit this with fixes.
    
    This reverts commit d5545cdaf40989792dd8cc2483304c613ac9f54c.
    
    BUG=735017
    TBR=jam
    
    Change-Id: I8e20adf373dc9c460d4326e9158be0e62f8a1055
    Reviewed-on: https://chromium-review.googlesource.com/545915
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482293}

--- components/navigation_interception/intercept_navigation_delegate.cc
+++ components/navigation_interception/intercept_navigation_delegate.cc
@@ -90,7 +90,7 @@ std::unique_ptr<content::NavigationThrottle>
 InterceptNavigationDelegate::CreateThrottleFor(
     content::NavigationHandle* handle) {
   return base::MakeUnique<InterceptNavigationThrottle>(
-      handle, base::Bind(&CheckIfShouldIgnoreNavigationOnUIThread));
+      handle, base::Bind(&CheckIfShouldIgnoreNavigationOnUIThread), false);
 }
 
 // static
