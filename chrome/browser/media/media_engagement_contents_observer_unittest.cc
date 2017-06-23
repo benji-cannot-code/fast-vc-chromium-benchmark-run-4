ed718da4d91405984bc3002e1f68c57255bd7964
Becca Hughes <beccahughes@chromium.org>
2017-06-23 23:55:41 +0100

    Media Engagement: Factor in audio muted state into the Media Engagement Index
    Monitor muted state changes and store them in the
    MediaEngagementContentsObserver. Then filter out media that is muted.
    
    BUG=729998
    
    Change-Id: I914537214f0cb6247a04e27ce291588cd5c38468
    Reviewed-on: https://chromium-review.googlesource.com/527435
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482080}

--- chrome/browser/media/media_engagement_contents_observer_unittest.cc
+++ chrome/browser/media/media_engagement_contents_observer_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/media/media_engagement_service_factory.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "chrome/test/base/testing_profile.h"
+#include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -29,6 +30,8 @@ class MediaEngagementContentsObserverTest
 
     playback_timer_ = new base::MockTimer(true, false);
     contents_observer_->SetTimerForTest(base::WrapUnique(playback_timer_));
+
+    ASSERT_FALSE(GetStoredPlayerStatesCount());
   }
 
   bool IsTimerRunning() const { return playback_timer_->IsRunning(); }
@@ -39,6 +42,17 @@ class MediaEngagementContentsObserverTest
 
   size_t GetSignificantActivePlayersCount() const {
     return contents_observer_->significant_players_.size();
+  }
+
+  size_t GetStoredPlayerStatesCount() const {
+    return contents_observer_->player_states_.size();
+  }
+
+  void Navigate(GURL url) {
+    std::unique_ptr<content::NavigationHandle> test_handle =
+        content::NavigationHandle::CreateNavigationHandleForTesting(
+            url, main_rfh(), true /** committed */);
+    contents_observer_->DidFinishNavigation(test_handle.get());
   }
 
   void SimulatePlaybackStarted(int id) {
@@ -52,6 +66,7 @@ class MediaEngagementContentsObserverTest
     content::WebContentsObserver::MediaPlayerId player_id =
         std::make_pair(nullptr /* RenderFrameHost */, id);
     contents_observer_->MediaStartedPlaying(player_info, player_id);
+    SimulateMutedStateChange(id, false);
   }
 
   void SimulatePlaybackStopped(int id) {
@@ -59,6 +74,12 @@ class MediaEngagementContentsObserverTest
     content::WebContentsObserver::MediaPlayerId player_id =
         std::make_pair(nullptr /* RenderFrameHost */, id);
     contents_observer_->MediaStoppedPlaying(player_info, player_id);
+  }
+
+  void SimulateMutedStateChange(int id, bool muted_state) {
+    content::WebContentsObserver::MediaPlayerId player_id =
+        std::make_pair(nullptr /* RenderFrameHost */, id);
+    contents_observer_->MediaMutedStateChanged(player_id, muted_state);
   }
 
   void SimulateIsVisible() { contents_observer_->WasShown(); }
@@ -130,6 +151,22 @@ TEST_F(MediaEngagementContentsObserverTest, AreConditionsMet) {
 
   SimulatePlaybackStarted(0);
   EXPECT_TRUE(AreConditionsMet());
+
+  SimulateMutedStateChange(0, true);
+  EXPECT_FALSE(AreConditionsMet());
+
+  SimulatePlaybackStarted(1);
+  EXPECT_TRUE(AreConditionsMet());
+}
+
+TEST_F(MediaEngagementContentsObserverTest, EnsureCleanupAfterNavigation) {
+  EXPECT_FALSE(GetStoredPlayerStatesCount());
+
+  SimulateMutedStateChange(0, true);
+  EXPECT_TRUE(GetStoredPlayerStatesCount());
+
+  Navigate(GURL("https://example.com"));
+  EXPECT_FALSE(GetStoredPlayerStatesCount());
 }
 
 TEST_F(MediaEngagementContentsObserverTest, TimerRunsDependingOnConditions) {
@@ -152,6 +189,12 @@ TEST_F(MediaEngagementContentsObserverTest, TimerRunsDependingOnConditions) {
   EXPECT_FALSE(IsTimerRunning());
 
   SimulatePlaybackStarted(0);
+  EXPECT_TRUE(IsTimerRunning());
+
+  SimulateMutedStateChange(0, true);
+  EXPECT_FALSE(IsTimerRunning());
+
+  SimulatePlaybackStarted(1);
   EXPECT_TRUE(IsTimerRunning());
 }
 
