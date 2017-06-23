6712c97c0e751c4769c0e5043bcb02dbd9225645
Robert Liao <robliao@chromium.org>
2017-06-23 12:04:16 -0700

    Add ComInitCheckHook for Eligible Builds to the Task Scheduler
    This will allow for COM initialization checking as code is refactored
    to use the task scheduler.
    
    As a side effect, this will also validate other COM call sites too.
    
    BUG=708303
    
    Change-Id: I0b38ea7d3e9a543c023aaada46a238f2421e97fd
    Reviewed-on: https://chromium-review.googlesource.com/546157
    Reviewed-by: Francois Doray <fdoray@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482385}

--- base/task_scheduler/task_scheduler_impl.h
+++ base/task_scheduler/task_scheduler_impl.h
@@ -31,6 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task_scheduler/task_tracker_posix.h"
 #endif
 
+#if defined(OS_WIN)
+#include "base/win/com_init_check_hook.h"
+#endif
+
 namespace base {
 
 class HistogramBase;
@@ -98,6 +102,15 @@ class BASE_EXPORT TaskSchedulerImpl : public TaskScheduler {
 #if DCHECK_IS_ON()
   // Set once JoinForTesting() has returned.
   AtomicFlag join_for_testing_returned_;
+#endif
+
+#if defined(OS_WIN)
+// The check below cannot be &&'ed with defined(OS_WIN) since the preprocessor
+// will find that the macro is undefined even if defined(OS_WIN) is not true.
+#if COM_INIT_CHECK_HOOK_ENABLED()
+  // Provides COM initialization verification for supported builds.
+  base::win::ComInitCheckHook com_init_check_hook_;
+#endif
 #endif
 
   DISALLOW_COPY_AND_ASSIGN(TaskSchedulerImpl);
