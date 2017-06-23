f4d0a8f24f86304508f4dcaf9eaa9b1e078b45c2
Joseph Arhar <jarhar@google.com>
2017-06-23 12:59:12 -0700

    Move test echo logic to host extension
    Bug: 606809
    Change-Id: Ifb8ce14df492bebf98a07b5f77fe99c413292a2b
    Reviewed-on: https://chromium-review.googlesource.com/546315
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Joseph Arhar <jarhar@google.com>
    Cr-Commit-Position: refs/heads/master@{#482052}

--- remoting/host/remoting_me2me_host.cc
+++ remoting/host/remoting_me2me_host.cc
@@ -85,6 +85,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/signaling_connector.h"
 #include "remoting/host/single_window_desktop_environment.h"
 #include "remoting/host/switches.h"
+#include "remoting/host/test_echo_extension.h"
 #include "remoting/host/third_party_auth_config.h"
 #include "remoting/host/token_validator_factory_impl.h"
 #include "remoting/host/usage_stats_consent.h"
@@ -1492,6 +1493,8 @@ void HostProcess::StartHost() {
     host_->AddExtension(
         base::MakeUnique<SecurityKeyExtension>(context_->file_task_runner()));
   }
+
+  host_->AddExtension(base::MakeUnique<TestEchoExtension>());
 
   // TODO(simonmorris): Get the maximum session duration from a policy.
 #if defined(OS_LINUX)
---
+++ remoting/host/test_echo_extension.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_HOST_TEST_ECHO_EXTENSION_H_
+#define REMOTING_HOST_TEST_ECHO_EXTENSION_H_
+
+#include "base/macros.h"
+#include "remoting/host/host_extension.h"
+
+namespace remoting {
+
+class TestEchoExtension : public HostExtension {
+ public:
+  TestEchoExtension();
+  ~TestEchoExtension() override;
+
+  // HostExtension interface.
+  std::string capability() const override;
+  std::unique_ptr<HostExtensionSession> CreateExtensionSession(
+      ClientSessionDetails* client_session_details,
+      protocol::ClientStub* client_stub) override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(TestEchoExtension);
+};
+
+}  // namespace remoting
+
+#endif  // REMOTING_HOST_TEST_ECHO_EXTENSION_H_
