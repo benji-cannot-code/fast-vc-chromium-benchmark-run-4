c2e83235526fa5a4f40d465bbfbb7dc999eea315
Sasha Bermeister <sashab@chromium.org>
2017-06-23 15:03:59 +1000

    Moved KeyboardTest and LinkSelectionTest to core/editing
    This is part of a larger effort to move unit tests out of web/tests.
    
    KeyboardTest was introduced in 2010 by eric@webkit.org:
    https://chromium.googlesource.com/chromium/src/+/ca0e3e27917c674326f9ab59746ed894b5818e24
    
    LinkSelectionTest was introduced in 2016 by kotenkov@yandex-team.ru:
    https://chromium.googlesource.com/chromium/src/+/9e80a9e5b5b71a48119225a614b7c0b8369badf2
    
    Bug: 353585
    Change-Id: I86bce295200c68877b0e71e144933afe7323eed6
    Reviewed-on: https://chromium-review.googlesource.com/544747
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Ojan Vafai <ojan@chromium.org>
    Commit-Queue: Ojan Vafai <ojan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482121}

--- third_party/WebKit/Source/core/editing/KeyboardTest.cpp
+++ third_party/WebKit/Source/core/editing/KeyboardTest.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <memory>
 #include "core/editing/EditingBehavior.h"
 #include "core/editing/Editor.h"
 #include "core/events/EventTarget.h"
@@ -38,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/platform/Platform.h"
 #include "public/platform/WebInputEvent.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include <memory>
 
 namespace blink {
 
