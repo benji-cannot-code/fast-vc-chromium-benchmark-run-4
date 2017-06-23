04567bc0c44680f1b1d445d2157a1beb150945fc
karandeepb <karandeepb@chromium.org>
2017-06-23 11:44:09 -0700

    Extensions Cleanup: Simplify how code for IPC::ParamTraits<Manifest::Location> is generated.
    Currently, code for IPC::ParamTraits<Manifest::Location> is generated using a
    separate generator file. Simplify this by generating this as part of
    extension_message_generator.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2951383003
    Cr-Commit-Position: refs/heads/master@{#481968}

--- extensions/common/manifest_location_param_traits.h
+++ extensions/common/manifest_location_param_traits.h
@@ -3,8 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef EXTENSIONS_COMMON_MANIFEST_LOCATION_PARAM_TRAITS_H_
-#define EXTENSIONS_COMMON_MANIFEST_LOCATION_PARAM_TRAITS_H_
+// Multiply-included file, hence no include guard.
 
 #include "extensions/common/manifest.h"
 #include "ipc/ipc_message_macros.h"
@@ -13,5 +12,3 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 IPC_ENUM_TRAITS_MIN_MAX_VALUE(extensions::Manifest::Location,
                               extensions::Manifest::INVALID_LOCATION,
                               extensions::Manifest::NUM_LOCATIONS - 1)
-
-#endif  // EXTENSIONS_COMMON_MANIFEST_LOCATION_PARAM_TRAITS_H_
