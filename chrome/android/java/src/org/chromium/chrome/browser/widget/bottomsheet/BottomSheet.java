496fdc5670b7b41a376fadc6a122f26452b17f02
Theresa Wellington <twellington@google.com>
2017-06-23 13:33:43 -0700

    [Home] Disable sheet touches before browser process started
    The sheet contents require a Profile which requires the browser process
    to be completely initialized. Ignore touch events on the sheet before
    the browser is initialized.
    
    BUG=736017
    
    Change-Id: I44b812d73b0c7e2f6b88481e892506415be850c8
    Reviewed-on: https://chromium-review.googlesource.com/546700
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482035}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheet.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheet.java
@@ -31,6 +31,7 @@ import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.ObserverList;
 import org.chromium.base.VisibleForTesting;
+import org.chromium.base.library_loader.LibraryProcessType;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.ChromeFeatureList;
@@ -53,6 +54,7 @@ import org.chromium.chrome.browser.util.ViewUtils;
 import org.chromium.chrome.browser.widget.FadingBackgroundView;
 import org.chromium.chrome.browser.widget.bottomsheet.BottomSheetContentController.ContentType;
 import org.chromium.chrome.browser.widget.textbubble.ViewAnchoredTextBubble;
+import org.chromium.content.browser.BrowserStartupController;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.ui.UiUtils;
 
@@ -223,7 +225,7 @@ public class BottomSheet
     private BottomToolbarPhone mDefaultToolbarView;
 
     /** Whether the {@link BottomSheet} and its children should react to touch events. */
-    private boolean mIsTouchEnabled = true;
+    private boolean mIsTouchEnabled;
 
     /** Whether the sheet is currently open. */
     private boolean mIsSheetOpen;
@@ -463,6 +465,17 @@ public class BottomSheet
 
         mMetrics = new BottomSheetMetrics();
         addObserver(mMetrics);
+
+        BrowserStartupController.get(LibraryProcessType.PROCESS_BROWSER)
+                .addStartupCompletedObserver(new BrowserStartupController.StartupCallback() {
+                    @Override
+                    public void onSuccess(boolean alreadyStarted) {
+                        mIsTouchEnabled = true;
+                    }
+
+                    @Override
+                    public void onFailure() {}
+                });
     }
 
     /**
