8ba5a14861e86ad9d9af82b9e5198cb905e4467c
cjgrant <cjgrant@chromium.org>
2017-06-23 15:45:00 -0700

    VR: Enforce LTR directionality on rendered URL text.
    Do as the Views-based omnibox does, and enforce LTR on URL text.  This
    ensures that malicious URLs can't visually spoof a hostname.  Also, add a unit test to ensure that the LTR directionality holds up against such a URL.
    
    TBR=estark@chromium.org
    BUG=735960
    
    Review-Url: https://codereview.chromium.org/2945173006
    Cr-Commit-Position: refs/heads/master@{#482054}

--- chrome/browser/android/vr_shell/textures/url_bar_texture.cc
+++ chrome/browser/android/vr_shell/textures/url_bar_texture.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/android/vr_shell/textures/url_bar_texture.h"
 
-#include "base/i18n/rtl.h"
 #include "base/strings/utf_string_conversions.h"
 #include "cc/paint/skia_paint_canvas.h"
 #include "chrome/browser/android/vr_shell/color_scheme.h"
@@ -354,9 +353,6 @@ void UrlBarTexture::RenderUrl(const gfx::Size& texture_size,
       gurl_, url_formatter::kFormatUrlOmitAll, net::UnescapeRule::NORMAL,
       &parsed, nullptr, nullptr);
 
-  if (base::i18n::StringContainsStrongRTLChars(text))
-    failure_callback_.Run(UiUnsupportedMode::kURLWithStrongRTLChars);
-
   int pixel_font_height = texture_size.height() * kFontHeight / kHeight;
 
   gfx::FontList font_list;
@@ -369,6 +365,7 @@ void UrlBarTexture::RenderUrl(const gfx::Size& texture_size,
   render_text->SetColor(SK_ColorBLACK);
   render_text->SetHorizontalAlignment(gfx::ALIGN_LEFT);
   render_text->SetElideBehavior(gfx::ELIDE_TAIL);
+  render_text->SetDirectionalityMode(gfx::DIRECTIONALITY_FORCE_LTR);
   render_text->SetText(text);
   render_text->SetDisplayRect(bounds);
 
@@ -392,6 +389,7 @@ void UrlBarTexture::RenderUrl(const gfx::Size& texture_size,
                   color_scheme());
 
   url_render_text_ = std::move(render_text);
+  url_text_ = text;
 }
 
 // This method replicates behavior in OmniboxView::UpdateTextStyle(), and
