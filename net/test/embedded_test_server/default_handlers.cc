3c5e25bd31dd7ee44416e89232a904fbda3de546
mmenke <mmenke@chromium.org>
2017-06-22 18:14:39 -0700

    NetworkService:  Destroy URLLoaders when a NetworkContext is destroyed.
    URLRequests can't outlast the URLRequestContext that spawned them, so
    the current code can cause crashes on NetworkContext destruction.
    
    BUG=735995
    
    Review-Url: https://codereview.chromium.org/2951293002
    Cr-Commit-Position: refs/heads/master@{#481760}

--- net/test/embedded_test_server/default_handlers.cc
+++ net/test/embedded_test_server/default_handlers.cc
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/base64.h"
+#include "base/bind.h"
+#include "base/bind_helpers.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/format_macros.h"
@@ -601,7 +603,7 @@ std::unique_ptr<HttpResponse> HandleSlowServer(const HttpRequest& request) {
 }
 
 // Never returns a response.
-class HungHttpResponse : public BasicHttpResponse {
+class HungHttpResponse : public HttpResponse {
  public:
   HungHttpResponse() {}
 
@@ -616,6 +618,27 @@ class HungHttpResponse : public BasicHttpResponse {
 // Never returns a response.
 std::unique_ptr<HttpResponse> HandleHungResponse(const HttpRequest& request) {
   return base::MakeUnique<HungHttpResponse>();
+}
+
+// Return headers, then hangs.
+class HungAfterHeadersHttpResponse : public HttpResponse {
+ public:
+  HungAfterHeadersHttpResponse() {}
+
+  void SendResponse(const SendBytesCallback& send,
+                    const SendCompleteCallback& done) override {
+    send.Run("HTTP/1.1 OK\r\n\r\n", base::Bind(&base::DoNothing));
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(HungAfterHeadersHttpResponse);
+};
+
+// /hung-after-headers
+// Never returns a response.
+std::unique_ptr<HttpResponse> HandleHungAfterHeadersResponse(
+    const HttpRequest& request) {
+  return base::MakeUnique<HungAfterHeadersHttpResponse>();
 }
 
 }  // anonymous namespace
@@ -663,6 +686,8 @@ void RegisterDefaultHandlers(EmbeddedTestServer* server) {
   server->RegisterDefaultHandler(PREFIXED_HANDLER("/slow", &HandleSlowServer));
   server->RegisterDefaultHandler(
       PREFIXED_HANDLER("/hung", &HandleHungResponse));
+  server->RegisterDefaultHandler(
+      PREFIXED_HANDLER("/hung-after-headers", &HandleHungAfterHeadersResponse));
 
   // TODO(svaldez): HandleDownload
   // TODO(svaldez): HandleDownloadFinish
