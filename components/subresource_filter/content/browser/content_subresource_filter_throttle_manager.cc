16d6b062d5da442ef1fcf757b7e2cc25661dd096
Charles Harrison <csharrison@chromium.org>
2017-06-23 15:47:39 -0400

    [subresource_filter] Add additional CHECKs in throttle_manager to debug crash
    Bug: 731714
    Change-Id: Iceff92f05317d19ba1f3be32c8e94b0984559aa8
    Reviewed-on: https://chromium-review.googlesource.com/538356
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482028}

--- components/subresource_filter/content/browser/content_subresource_filter_throttle_manager.cc
+++ components/subresource_filter/content/browser/content_subresource_filter_throttle_manager.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/stl_util.h"
 #include "base/trace_event/trace_event.h"
 #include "base/trace_event/trace_event_argument.h"
 #include "components/subresource_filter/content/browser/activation_state_computing_navigation_throttle.h"
@@ -73,13 +74,18 @@ void ContentSubresourceFilterThrottleManager::ReadyToCommitNavigation(
   if (navigation_handle->GetNetErrorCode() != net::OK)
     return;
 
-  auto throttle = ongoing_activation_throttles_.find(navigation_handle);
-  if (throttle == ongoing_activation_throttles_.end())
+  auto it = ongoing_activation_throttles_.find(navigation_handle);
+  if (it == ongoing_activation_throttles_.end())
     return;
+
+  // TODO(crbug.com/736249): Remove CHECKs in this file when the root cause of
+  // the crash is found.
+  ActivationStateComputingNavigationThrottle* throttle = it->second;
+  CHECK_EQ(navigation_handle, throttle->navigation_handle());
 
   // Main frame throttles with disabled page-level activation will not have
   // associated filters.
-  AsyncDocumentSubresourceFilter* filter = throttle->second->filter();
+  AsyncDocumentSubresourceFilter* filter = throttle->filter();
   if (!filter)
     return;
 
@@ -93,7 +99,7 @@ void ContentSubresourceFilterThrottleManager::ReadyToCommitNavigation(
       "ContentSubresourceFilterThrottleManager::ReadyToCommitNavigation",
       "activation_state", filter->activation_state().ToTracedValue());
 
-  throttle->second->WillSendActivationToRenderer();
+  throttle->WillSendActivationToRenderer();
 
   content::RenderFrameHost* frame_host =
       navigation_handle->GetRenderFrameHost();
@@ -114,6 +120,7 @@ void ContentSubresourceFilterThrottleManager::DidFinishNavigation(
   auto throttle = ongoing_activation_throttles_.find(navigation_handle);
   std::unique_ptr<AsyncDocumentSubresourceFilter> filter;
   if (throttle != ongoing_activation_throttles_.end()) {
+    CHECK_EQ(navigation_handle, throttle->second->navigation_handle());
     filter = throttle->second->ReleaseFilter();
     ongoing_activation_throttles_.erase(throttle);
   }
@@ -207,6 +214,7 @@ void ContentSubresourceFilterThrottleManager::MaybeAppendNavigationThrottles(
   }
   if (auto activation_throttle =
           MaybeCreateActivationStateComputingThrottle(navigation_handle)) {
+    CHECK(!base::ContainsKey(ongoing_activation_throttles_, navigation_handle));
     ongoing_activation_throttles_[navigation_handle] =
         activation_throttle.get();
     throttles->push_back(std::move(activation_throttle));
