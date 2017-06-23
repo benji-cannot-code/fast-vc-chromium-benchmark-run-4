e4b4833c53b3958d4d3930cd4c84320b3a59b54d
eugenebut <eugenebut@chromium.org>
2017-06-22 17:59:35 -0700

    ChromeWebView integration tests for Incognito.
    These tests verify that browsing data (cookie, localStorage) does not
    leak between incognito and non-incognito web views.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2945563002
    Cr-Commit-Position: refs/heads/master@{#481755}

--- ios/web_view/test/web_view_test_util.h
+++ ios/web_view/test/web_view_test_util.h
@@ -11,9 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 
 @class CWVWebView;
+@class CWVWebViewConfiguration;
 
 namespace ios_web_view {
 namespace test {
+
+// Creates web view with given |configuration| and frame equal to screen bounds.
+CWVWebView* CreateWebView(CWVWebViewConfiguration* configuration)
+    WARN_UNUSED_RESULT;
 
 // Creates web view with default configuration and frame equal to screen bounds.
 CWVWebView* CreateWebView() WARN_UNUSED_RESULT;
