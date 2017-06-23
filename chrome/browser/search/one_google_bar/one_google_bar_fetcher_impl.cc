be5185caea674fda026d8128d0db8dd3bbcf6b68
Marc Treib <treib@chromium.org>
2017-06-23 14:06:04 +0200

    Local NTP OneGoogleBar: In case of network errors, keep the cached data
    Before, we'd clear the cached data when the refresh request failed due
    to e.g. missing network connectivity. Now, we keep the cache in that
    case. We still clear the cache if the server responds with an HTTP
    error, or when it returns invalid/unexpected data.
    
    Bug: 583292
    Change-Id: I974ec774591d77bb11417a7cafdb511934250fcb
    Reviewed-on: https://chromium-review.googlesource.com/541357
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481866}

--- chrome/browser/search/one_google_bar/one_google_bar_fetcher_impl.cc
+++ chrome/browser/search/one_google_bar/one_google_bar_fetcher_impl.cc
@@ -354,7 +354,7 @@ void OneGoogleBarFetcherImpl::FetchDone(const net::URLFetcher* source) {
     // response).
     DLOG(WARNING) << "Request failed with error: " << request_status.error()
                   << ": " << net::ErrorToString(request_status.error());
-    Respond(base::nullopt);
+    Respond(Status::TRANSIENT_ERROR, base::nullopt);
     return;
   }
 
@@ -364,7 +364,7 @@ void OneGoogleBarFetcherImpl::FetchDone(const net::URLFetcher* source) {
     std::string response;
     source->GetResponseAsString(&response);
     DLOG(WARNING) << "Response: " << response;
-    Respond(base::nullopt);
+    Respond(Status::FATAL_ERROR, base::nullopt);
     return;
   }
 
@@ -387,18 +387,20 @@ void OneGoogleBarFetcherImpl::FetchDone(const net::URLFetcher* source) {
 }
 
 void OneGoogleBarFetcherImpl::JsonParsed(std::unique_ptr<base::Value> value) {
-  Respond(JsonToOGBData(*value));
+  base::Optional<OneGoogleBarData> result = JsonToOGBData(*value);
+  Respond(result.has_value() ? Status::OK : Status::FATAL_ERROR, result);
 }
 
 void OneGoogleBarFetcherImpl::JsonParseFailed(const std::string& message) {
   DLOG(WARNING) << "Parsing JSON failed: " << message;
-  Respond(base::nullopt);
+  Respond(Status::FATAL_ERROR, base::nullopt);
 }
 
 void OneGoogleBarFetcherImpl::Respond(
+    Status status,
     const base::Optional<OneGoogleBarData>& data) {
   for (auto& callback : callbacks_) {
-    std::move(callback).Run(data);
+    std::move(callback).Run(status, data);
   }
   callbacks_.clear();
 }
