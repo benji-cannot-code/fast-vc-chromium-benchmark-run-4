877d2c7eaa90f47f12d45013251dc8cd9f3ad1da
Vladimir Levin <vmpstr@chromium.org>
2017-06-23 10:42:01 -0700

    cc: Add PaintOpTypeToString in order to aid in debugging things.
    This patch adds a type to string function for PaintOpTypes.
    
    R=enne@chromium.org
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I31dbff609c94a13a24757a76238421824cbcfa86
    Reviewed-on: https://chromium-review.googlesource.com/546595
    Reviewed-by: enne <enne@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481992}

--- cc/paint/paint_op_buffer.h
+++ cc/paint/paint_op_buffer.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <string>
+
 #include "base/logging.h"
 #include "base/memory/aligned_memory.h"
 #include "cc/base/math_util.h"
@@ -72,6 +74,8 @@ enum class PaintOpType : uint8_t {
   Translate,
   LastPaintOpType = Translate,
 };
+
+std::string PaintOpTypeToString(PaintOpType type);
 
 struct CC_PAINT_EXPORT PaintOp {
   uint32_t type : 8;
