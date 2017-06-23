eccab41db7b4ff4329bcfcb3cf460a4b6b4a7107
khorimoto <khorimoto@chromium.org>
2017-06-22 19:03:31 -0700

    [CrOS Tether] Network detail settings page: Do not show irrelevant fields for Tether networks.
    Previously, the Advanced, Networks, and Proxy fields were shown, which are not related to Tether networks.
    
    BUG=672263,730246
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2949363002
    Cr-Commit-Position: refs/heads/master@{#481782}

--- chrome/browser/resources/settings/internet_page/internet_detail_page.js
+++ chrome/browser/resources/settings/internet_page/internet_detail_page.js
@@ -888,8 +888,9 @@ Polymer({
    */
   getAdvancedFields_: function() {
     /** @type {!Array<string>} */ var fields = [];
-    fields.push('MacAddress');
     var type = this.networkProperties.Type;
+    if (type != CrOnc.Type.TETHER)
+      fields.push('MacAddress');
     if (type == CrOnc.Type.CELLULAR && !!this.networkProperties.Cellular) {
       fields.push(
           'Cellular.Carrier', 'Cellular.Family', 'Cellular.NetworkTechnology',
@@ -929,6 +930,11 @@ Polymer({
    * @private
    */
   showAdvanced_: function(networkProperties) {
+    if (networkProperties.Type == CrOnc.Type.TETHER) {
+      // These settings apply to the underlying WiFi network, not the Tether
+      // network.
+      return false;
+    }
     return this.hasAdvancedFields_() || this.hasDeviceFields_() ||
         this.isRememberedOrConnected_(networkProperties);
   },
@@ -963,6 +969,11 @@ Polymer({
    * @private
    */
   hasNetworkSection_: function(networkProperties) {
+    if (networkProperties.Type == CrOnc.Type.TETHER) {
+      // These settings apply to the underlying WiFi network, not the Tether
+      // network.
+      return false;
+    }
     if (networkProperties.Type == CrOnc.Type.VPN)
       return false;
     if (networkProperties.Type == CrOnc.Type.CELLULAR)
