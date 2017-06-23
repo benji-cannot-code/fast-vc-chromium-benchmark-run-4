d6097beb53e187560769c5da3bf549f4ce9d7880
wzang <wzang@chromium.org>
2017-06-23 13:38:31 -0700

    Fix undefined small pods container error
    Previously ignored that this.smallPodsContainer is not defined in the
    particular namespace.
    
    BUG=735724
    
    Review-Url: https://codereview.chromium.org/2950113004
    Cr-Commit-Position: refs/heads/master@{#482014}

--- ui/login/account_picker/md_user_pod_row.js
+++ ui/login/account_picker/md_user_pod_row.js
@@ -3170,7 +3170,7 @@ cr.define('login', function() {
         this.smallPodsContainer.classList.remove('images-loading');
         this.topMask.classList.remove('images-loading');
         this.bottomMask.classList.remove('images-loading');
-      }, POD_ROW_IMAGES_LOAD_TIMEOUT_MS);
+      }.bind(this), POD_ROW_IMAGES_LOAD_TIMEOUT_MS);
 
       var isAccountPicker = $('login-header-bar').signinUIState ==
           SIGNIN_UI_STATE.ACCOUNT_PICKER;
