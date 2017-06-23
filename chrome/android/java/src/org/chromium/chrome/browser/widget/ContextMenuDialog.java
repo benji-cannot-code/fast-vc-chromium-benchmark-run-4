deefdc4fe543593ea43387a0ae9d74920fe253c8
ltian <ltian@chromium.org>
2017-06-23 12:08:56 -0700

    [Android] Fix context menu animation errors for Browser Actions
    This CL fixes two issues about animation of Chrome context menu for
    Browser Actions:
    1. NullPointerException for getContentOffsetYPix because Browser Actions
    does not have ContentViewCore. To fix it, add a check if null, return 0.
    2. Browser Actions request does not provide the touch event coordinates
    and sets them to 0 when displaying the context menu. This forces context
    menu to be displayed from top left. The ideal performance should display
    the context menu from center. To fix it, if the Activity is a
    BrowserActionActivity, set ScaleAnimation to animate from center.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2949153002
    Cr-Commit-Position: refs/heads/master@{#481983}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/ContextMenuDialog.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/ContextMenuDialog.java
@@ -23,6 +23,8 @@ import android.view.animation.ScaleAnimation;
 import org.chromium.chrome.browser.contextmenu.TabularContextMenuViewPager;
 import org.chromium.content.browser.RenderCoordinates;
 
+import javax.annotation.Nullable;
+
 /**
  * ContextMenuDialog is a subclass of AlwaysDismissedDialog that ensures that the proper scale
  * animation is played upon calling {@link #show()} and {@link #dismiss()}.
@@ -50,10 +52,11 @@ public class ContextMenuDialog extends AlwaysDismissedDialog {
      * @param touchPointXPx The x-coordinate of the touch that triggered the context menu.
      * @param touchPointYPx The y-coordinate of the touch that triggered the context menu.
      * @param contentView The The {@link TabularContextMenuViewPager} to display on the dialog.
-     * @param renderCoordinates The render coordinates to get the y offset of the window.
+     * @param renderCoordinates The render coordinates to get the y offset of the window. This could
+     *                          be null if ContentViewCore is not available.
      */
     public ContextMenuDialog(Activity ownerActivity, int theme, float touchPointXPx,
-            float touchPointYPx, View contentView, RenderCoordinates renderCoordinates) {
+            float touchPointYPx, View contentView, @Nullable RenderCoordinates renderCoordinates) {
         super(ownerActivity, theme);
         mActivity = ownerActivity;
         mTouchPointXPx = touchPointXPx;
@@ -96,7 +99,9 @@ public class ContextMenuDialog extends AlwaysDismissedDialog {
         window.getDecorView().getWindowVisibleDisplayFrame(rectangle);
 
         float xOffsetPx = rectangle.left;
-        float yOffsetPx = rectangle.top + mRenderCoordinates.getContentOffsetYPix();
+        float contentOffsetYPx =
+                mRenderCoordinates != null ? mRenderCoordinates.getContentOffsetYPix() : 0;
+        float yOffsetPx = rectangle.top + contentOffsetYPx;
 
         int[] currentLocationOnScreenPx = new int[2];
         mContentView.getLocationOnScreen(currentLocationOnScreenPx);
@@ -160,8 +165,14 @@ public class ContextMenuDialog extends AlwaysDismissedDialog {
         float fromY = fromX;
         float toY = toX;
 
-        ScaleAnimation animation = new ScaleAnimation(
-                fromX, toX, fromY, toY, Animation.ABSOLUTE, pivotX, Animation.ABSOLUTE, pivotY);
+        ScaleAnimation animation;
+        if (mRenderCoordinates == null) {
+            animation = new ScaleAnimation(fromX, toX, fromY, toY, Animation.RELATIVE_TO_SELF, 0.5f,
+                    Animation.RELATIVE_TO_SELF, 0.5f);
+        } else {
+            animation = new ScaleAnimation(
+                    fromX, toX, fromY, toY, Animation.ABSOLUTE, pivotX, Animation.ABSOLUTE, pivotY);
+        }
 
         long duration = isEnterAnimation ? ENTER_ANIMATION_DURATION_MS : EXIT_ANIMATION_DURATION_MS;
 
