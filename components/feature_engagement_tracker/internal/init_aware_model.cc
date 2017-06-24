74b7c41c251168338704251f7d62663357077f76
Tommy Nyquist <nyquist@chromium.org>
2017-06-23 18:35:09 -0700

    Do not queue up events forever when initialization fails
    The FeatureEngagementTracker can end up queueing events up forever
    in memory. It will stay there until the browser is killed. This can
    happen in two ways: For events that are notified before initialization
    is completed, they will stay in memory.
    
    In addition, events that are notified after init has completed (but
    failed), they will also be kept in memory.
    
    This CL adds a test that identifies both these scenarios, and also
    adds the code to fix the issue.
    
    BUG=736550
    
    Change-Id: I201c97839a9246cbbebf9b866d0131ea42643823
    Reviewed-on: https://chromium-review.googlesource.com/546959
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482147}

--- components/feature_engagement_tracker/internal/init_aware_model.cc
+++ components/feature_engagement_tracker/internal/init_aware_model.cc
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace feature_engagement_tracker {
 
 InitAwareModel::InitAwareModel(std::unique_ptr<Model> model)
-    : model_(std::move(model)), weak_ptr_factory_(this) {
+    : model_(std::move(model)),
+      initialization_complete_(false),
+      weak_ptr_factory_(this) {
   DCHECK(model_);
 }
 
@@ -38,19 +40,27 @@ void InitAwareModel::IncrementEvent(const std::string& event_name,
     return;
   }
 
+  if (initialization_complete_)
+    return;
+
   queued_events_.push_back(std::tie(event_name, current_day));
 }
 
 void InitAwareModel::OnInitializeComplete(
     const OnModelInitializationFinished& callback,
     bool success) {
+  initialization_complete_ = true;
   if (success) {
     for (auto& event : queued_events_)
       model_->IncrementEvent(std::get<0>(event), std::get<1>(event));
-    queued_events_.clear();
   }
+  queued_events_.clear();
 
   callback.Run(success);
+}
+
+size_t InitAwareModel::GetQueuedEventCountForTesting() {
+  return queued_events_.size();
 }
 
 }  // namespace feature_engagement_tracker
