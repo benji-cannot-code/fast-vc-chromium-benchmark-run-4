830f30b8b70278f83de8a8fa05251468843167d3
rdevlin.cronin <rdevlin.cronin@chromium.org>
2017-06-23 19:12:03 -0700

    [Extensions] Don't require() a module for calling a method
    The ModuleSystem has an API to call a method of a given module, which
    many callers use as a way to call into JS.  We've updated the JS call
    itself to accept asynchronous JS execution, but if the module isn't
    already active, the Require() call can result in JS execution.
    
    Modify CallModuleMethodSafe() so that it won't Require() a module that
    doesn't exist.  I think this should be okay, since I can't find any
    cases of calling into a module where we'd still want to if the module
    wasn't required.  The main time we would attempt to call into a module
    that hasn't been required is to dispatch an event in JS bindings, but
    if the Event module has never been instantiated, it's impossible that
    any events have been registered.
    
    Gracefully handle the case of the module not being required, and just
    don't dispatch the method.
    
    BUG=629431
    
    Review-Url: https://codereview.chromium.org/2936213002
    Cr-Commit-Position: refs/heads/master@{#482119}

--- extensions/renderer/utils_unittest.cc
+++ extensions/renderer/utils_unittest.cc
@@ -12,7 +12,7 @@ namespace extensions {
 namespace {
 
 class UtilsUnittest : public ModuleSystemTest {
- private:
+ public:
   void SetUp() override {
     ModuleSystemTest::SetUp();
 
@@ -30,6 +30,26 @@ class UtilsUnittest : public ModuleSystemTest {
         gin::Dictionary::CreateEmpty(env()->isolate()));
     chrome.Set("runtime", chrome_runtime);
   }
+
+  void RunTest(const std::string& test_name) { RunTestImpl(test_name, false); }
+
+  void RunTestWithPromises(const std::string& test_name) {
+    RunTestImpl(test_name, true);
+  }
+
+ private:
+  void RunTestImpl(const std::string& test_name, bool run_promises) {
+    ModuleSystem::NativesEnabledScope natives_enabled_scope(
+        env()->module_system());
+    ASSERT_FALSE(env()
+                     ->module_system()
+                     ->Require("utils_unittest")
+                     .ToLocalChecked()
+                     .IsEmpty());
+    env()->module_system()->CallModuleMethodSafe("utils_unittest", test_name);
+    if (run_promises)
+      RunResolvedPromises();
+  }
 };
 
 TEST_F(UtilsUnittest, TestNothing) {
@@ -37,42 +57,23 @@ TEST_F(UtilsUnittest, TestNothing) {
 }
 
 TEST_F(UtilsUnittest, SuperClass) {
-  ModuleSystem::NativesEnabledScope natives_enabled_scope(
-      env()->module_system());
-  env()->module_system()->CallModuleMethodSafe("utils_unittest",
-                                               "testSuperClass");
+  RunTest("testSuperClass");
 }
 
 TEST_F(UtilsUnittest, PromiseNoResult) {
-  ModuleSystem::NativesEnabledScope natives_enabled_scope(
-      env()->module_system());
-  env()->module_system()->CallModuleMethodSafe("utils_unittest",
-                                               "testPromiseNoResult");
-  RunResolvedPromises();
+  RunTestWithPromises("testPromiseNoResult");
 }
 
 TEST_F(UtilsUnittest, PromiseOneResult) {
-  ModuleSystem::NativesEnabledScope natives_enabled_scope(
-      env()->module_system());
-  env()->module_system()->CallModuleMethodSafe("utils_unittest",
-                                               "testPromiseOneResult");
-  RunResolvedPromises();
+  RunTestWithPromises("testPromiseOneResult");
 }
 
 TEST_F(UtilsUnittest, PromiseTwoResults) {
-  ModuleSystem::NativesEnabledScope natives_enabled_scope(
-      env()->module_system());
-  env()->module_system()->CallModuleMethodSafe("utils_unittest",
-                                               "testPromiseTwoResults");
-  RunResolvedPromises();
+  RunTestWithPromises("testPromiseTwoResults");
 }
 
 TEST_F(UtilsUnittest, PromiseError) {
-  ModuleSystem::NativesEnabledScope natives_enabled_scope(
-      env()->module_system());
-  env()->module_system()->CallModuleMethodSafe("utils_unittest",
-                                               "testPromiseError");
-  RunResolvedPromises();
+  RunTestWithPromises("testPromiseError");
 }
 
 }  // namespace
