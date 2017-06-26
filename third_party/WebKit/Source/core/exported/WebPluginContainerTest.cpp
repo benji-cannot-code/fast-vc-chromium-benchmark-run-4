b290ae3040cab205ab19c60b228c744ed9682f20
Sasha Bermeister <sashab@chromium.org>
2017-06-27 09:33:42 +1000

    Moved FakeWebPlugin and dependent tests to core/exported
    Bug: 353585
    Change-Id: I3f3436ea890432edcc2923e4400744f7357732e2
    Reviewed-on: https://chromium-review.googlesource.com/544739
    Commit-Queue: Sasha Morrissey <sashab@chromium.org>
    Reviewed-by: Stuart Langley <slangley@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482478}

--- third_party/WebKit/Source/core/exported/WebPluginContainerTest.cpp
+++ third_party/WebKit/Source/core/exported/WebPluginContainerTest.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include "core/dom/Element.h"
 #include "core/events/KeyboardEvent.h"
+#include "core/exported/FakeWebPlugin.h"
 #include "core/exported/WebPluginContainerBase.h"
 #include "core/exported/WebViewBase.h"
 #include "core/frame/EventHandlerRegistry.h"
@@ -67,7 +68,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/web/WebSettings.h"
 #include "public/web/WebView.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "web/tests/FakeWebPlugin.h"
 
 using blink::testing::RunPendingTasks;
 
