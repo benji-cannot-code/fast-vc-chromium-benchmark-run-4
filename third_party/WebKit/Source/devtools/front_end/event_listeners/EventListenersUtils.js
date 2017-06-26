6bb80fad7147097bf190f681a5c0e7ee91430b5e
einbinder <einbinder@chromium.org>
2017-06-26 14:25:38 -0700

    DevTools: Type namespaces with @const
    Closure recommends typing namespaces with @const. This fixes a lot of
    missing types in DevTools, such as flavors.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2948923002
    Cr-Commit-Position: refs/heads/master@{#482407}

--- third_party/WebKit/Source/devtools/front_end/event_listeners/EventListenersUtils.js
+++ third_party/WebKit/Source/devtools/front_end/event_listeners/EventListenersUtils.js
@@ -221,7 +221,7 @@ EventListeners.frameworkEventListeners = function(object) {
    * @param {!SDK.RemoteObject} errorString
    */
   function printErrorString(errorString) {
-    Common.console.error(errorString.value);
+    Common.console.error(String(errorString.value));
   }
 
   /**
