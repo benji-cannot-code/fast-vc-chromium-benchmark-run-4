39a4a9e1b43c1bf96de10c9a446bdd0f0e94960d
Kenneth Russell <kbr@chromium.org>
2017-06-26 22:32:17 +0000

    Revert "Try running context_lost tests on Linux TSAN GPU bot again."
    This reverts commit a2c0592a18bd438cedc03892bdaaf54132f18898.
    
    Reason for revert: browser still doesn't start under TSAN; see https://luci-milo.appspot.com/buildbot/chromium.gpu.fyi/Linux%20GPU%20TSAN%20Release/4419 .
    
    Original change's description:
    > Try running context_lost tests on Linux TSAN GPU bot again.
    > 
    > Suppressions were added a while back; let's see if they've helped.
    > 
    > BUG=691029
    > TBR=zmo@chromium.org
    > NOTRY=true
    > 
    > Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    > Change-Id: I9aa633145871c3e279b2967e8942831562336cea
    > Reviewed-on: https://chromium-review.googlesource.com/548958
    > Commit-Queue: Kenneth Russell <kbr@chromium.org>
    > Reviewed-by: Kenneth Russell <kbr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#482324}
    
    TBR=zmo@chromium.org,kbr@chromium.org
    
    Change-Id: I655d27f702a005f5f111f3c351b06af3ce30cef0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 691029
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Reviewed-on: https://chromium-review.googlesource.com/549069
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482439}

--- content/test/gpu/generate_buildbot_json.py
+++ content/test/gpu/generate_buildbot_json.py
@@ -1585,6 +1585,7 @@ TELEMETRY_GPU_INTEGRATION_TESTS = {
     'tester_configs': [
       {
         'predicate': Predicates.DEFAULT_PLUS_V8,
+        'disabled_instrumentation_types': ['tsan'],
       },
     ],
     'disabled_tester_configs': [
