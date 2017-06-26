9e0a24a2d0bcaf8784dd044e4980182f7a72868e
mef <mef@chromium.org>
2017-06-26 14:37:37 -0700

    [Cronet] Export NSError constants instead of defining them in Cronet.h
    BUG=670689
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_cronet_tester
    
    Review-Url: https://codereview.chromium.org/2956973002
    Cr-Commit-Position: refs/heads/master@{#482418}

--- components/cronet/ios/Cronet.mm
+++ components/cronet/ios/Cronet.mm
@@ -23,6 +23,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cert/cert_verifier.h"
 #include "net/url_request/url_request_context_getter.h"
 
+// Cronet NSError constants.
+NSString* const CRNCronetErrorDomain = @"CRNCronetErrorDomain";
+NSString* const CRNInvalidArgumentKey = @"CRNInvalidArgumentKey";
+
 namespace {
 
 class CronetHttpProtocolHandlerDelegate;
