8e7f5c015d93f74ceba773a03f23c94f224c300e
stkhapugin <stkhapugin@chromium.org>
2017-06-26 04:29:53 -0700

    [ObjC ARC] Converts ios/chrome/app:main to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2959473002
    Cr-Commit-Position: refs/heads/master@{#482240}

--- ios/chrome/app/chrome_exe_main.mm
+++ ios/chrome/app/chrome_exe_main.mm
@@ -13,6 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/crash_report/crash_keys.h"
 #include "ios/chrome/common/channel_info.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace {
 
 NSString* const kUIApplicationDelegateInfoKey = @"UIApplicationDelegate";
