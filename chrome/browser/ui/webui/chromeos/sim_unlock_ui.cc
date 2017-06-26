4e4ba3aecb1c8c92b24fbc430425d9ae3ca43553
tzik <tzik@chromium.org>
2017-06-26 18:25:54 +0900

    Rewrite base::Bind to base::BindOnce in chrome/browser/ui/webui/chromeos
    This is a generated CL to rewrite base::Bind to base::BindOnce where
    the result of base::Bind is immediately converted to OnceCallback.
    E.g.:
      base::PostTask(FROM_HERE, base::Bind(&Foo));
    is rewritten to:
      base::PostTask(FROM_HERE, base::BindOnce(&Foo));
    
    An explainer of OnceCallback is available here goo.gl/YZiUL1.
    
    The rewriting tool is in //tools/clang/base_bind_rewriters. To build and
    run locally, follow //docs/clang_tool_refactoring.md.
    
    This conversion makes almost no behavior change except that a bound
    objects are passed-by-move to the target function.
    
    Bug: 554299, 714018
    Change-Id: I4f484f65e7fb5edae0b8244ddbd3b8538b465344
    Reviewed-on: https://chromium-review.googlesource.com/547124
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482899}

--- chrome/browser/ui/webui/chromeos/sim_unlock_ui.cc
+++ chrome/browser/ui/webui/chromeos/sim_unlock_ui.cc
@@ -588,8 +588,9 @@ void SimUnlockHandler::HandleChangePinCode(const base::ListValue* args) {
 void SimUnlockHandler::HandleEnterCode(SimUnlockCode code_type,
                                        const std::string& code) {
   scoped_refptr<TaskProxy> task = new TaskProxy(AsWeakPtr(), code, code_type);
-  BrowserThread::PostTask(BrowserThread::UI, FROM_HERE,
-      base::Bind(&TaskProxy::HandleEnterCode, task.get()));
+  BrowserThread::PostTask(
+      BrowserThread::UI, FROM_HERE,
+      base::BindOnce(&TaskProxy::HandleEnterCode, task.get()));
 }
 
 void SimUnlockHandler::HandleEnterPinCode(const base::ListValue* args) {
@@ -623,8 +624,9 @@ void SimUnlockHandler::HandleProceedToPukInput(const base::ListValue* args) {
     return;
   }
   scoped_refptr<TaskProxy> task = new TaskProxy(AsWeakPtr());
-  BrowserThread::PostTask(BrowserThread::UI, FROM_HERE,
-      base::Bind(&TaskProxy::HandleProceedToPukInput, task.get()));
+  BrowserThread::PostTask(
+      BrowserThread::UI, FROM_HERE,
+      base::BindOnce(&TaskProxy::HandleProceedToPukInput, task.get()));
 }
 
 void SimUnlockHandler::HandleSimStatusInitialize(const base::ListValue* args) {
@@ -638,8 +640,9 @@ void SimUnlockHandler::HandleSimStatusInitialize(const base::ListValue* args) {
   dialog_mode_ = static_cast<SimDialogDelegate::SimDialogMode>(mode);
   VLOG(1) << "Initializing SIM dialog in mode: " << dialog_mode_;
   scoped_refptr<TaskProxy> task = new TaskProxy(AsWeakPtr());
-  BrowserThread::PostTask(BrowserThread::UI, FROM_HERE,
-      base::Bind(&TaskProxy::HandleInitialize, task.get()));
+  BrowserThread::PostTask(
+      BrowserThread::UI, FROM_HERE,
+      base::BindOnce(&TaskProxy::HandleInitialize, task.get()));
 }
 
 void SimUnlockHandler::InitializeSimStatus() {
