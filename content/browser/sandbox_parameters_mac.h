1078f573b20b0bc8e6c5de97fe1ce86ead3174ec
kerrnel <kerrnel@chromium.org>
2017-06-26 16:40:58 -0700

    Implement the V2 sandbox in the process launcher.
    Implements the V2 sandbox in the process launcher, passing the
    parameters and flags to Chrome Helper executable. The V2 sandbox is
    currently a disabled by default feature.
    
    BUG=689306
    CQ-DEPEND=2916323004
    
    Review-Url: https://codereview.chromium.org/2931173003
    Cr-Commit-Position: refs/heads/master@{#482462}

---
+++ content/browser/sandbox_parameters_mac.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_SANDBOX_PARAMETERS_MAC_H_
+#define CONTENT_BROWSER_SANDBOX_PARAMETERS_MAC_H_
+
+namespace sandbox {
+class SeatbeltExecClient;
+}
+
+namespace content {
+
+// Populates the |client| with the parameters that the sandbox needs to
+// resolve information that cannot be known at build time, such as
+// the user's home directory.
+void SetupRendererSandboxParameters(sandbox::SeatbeltExecClient* client);
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_SANDBOX_PARAMETERS_MAC_H_
