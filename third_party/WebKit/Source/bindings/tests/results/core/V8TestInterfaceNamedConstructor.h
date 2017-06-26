1cb24398982728bc0b96eb89650936b52c86d08d
Hitoshi Yoshida <peria@chromium.org>
2017-06-26 14:43:11 +0900

    Bindings: Generate installers of runtime enabled features for templates
    This enables to install runtime enabled features on interface templates.
    
    
    Bug: 617892
    Change-Id: I06d11711941950e2df8568f735590da4f5df41d1
    Reviewed-on: https://chromium-review.googlesource.com/544743
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Hitoshi Yoshida <peria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482214}

--- third_party/WebKit/Source/bindings/tests/results/core/V8TestInterfaceNamedConstructor.h
+++ third_party/WebKit/Source/bindings/tests/results/core/V8TestInterfaceNamedConstructor.h
@@ -54,6 +54,11 @@ class V8TestInterfaceNamedConstructor {
   static const int internalFieldCount = kV8DefaultWrapperInternalFieldCount + 0;
 
   // Callback functions
+
+  static void InstallRuntimeEnabledFeaturesOnTemplate(
+      v8::Isolate*,
+      const DOMWrapperWorld&,
+      v8::Local<v8::FunctionTemplate> interface_template);
 };
 
 template <>
