5d1673edd4e76881f39c00e1613002643c6bdc09
hejq <hejq@chromium.org>
2017-06-26 10:50:41 -0700

    Add the Play Store app search to the launcher.
    Bug=736552
    
    Review-Url: https://codereview.chromium.org/2929273002
    Cr-Original-Commit-Position: refs/heads/master@{#482130}
    Committed: https://chromium.googlesource.com/chromium/src/+/fa1bfdf6d5fb0952a524f4ca048ff104fdbdb2dd
    Review-Url: https://codereview.chromium.org/2929273002
    Cr-Commit-Position: refs/heads/master@{#482312}

--- ui/app_list/views/search_box_view.cc
+++ ui/app_list/views/search_box_view.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/app_list/resources/grit/app_list_resources.h"
 #include "ui/app_list/search_box_model.h"
 #include "ui/app_list/speech_ui_model.h"
-#include "ui/app_list/vector_icons.h"
+#include "ui/app_list/vector_icons/vector_icons.h"
 #include "ui/app_list/views/app_list_view.h"
 #include "ui/app_list/views/contents_view.h"
 #include "ui/app_list/views/search_box_view_delegate.h"
