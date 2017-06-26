a60ee026d02132cfb74f770b3486f3be389f1ece
cjgrant <cjgrant@chromium.org>
2017-06-26 09:39:26 -0700

    VR: Add support for time-aware unit tests.
    Add an initial unit test for the transient WebVR warning. This CL is a
    lead-in to pulling common transient code out of scene manager and
    specific elements.
    
    Note that this development seemed to expose a bug in the mock task
    runner, in conjunction with timers. This bug is now blocked on that
    bug.
    
    BUG=735163
    
    Review-Url: https://codereview.chromium.org/2960533002
    Cr-Commit-Position: refs/heads/master@{#482294}

--- chrome/browser/android/vr_shell/ui_scene_manager_unittest.cc
+++ chrome/browser/android/vr_shell/ui_scene_manager_unittest.cc
@@ -7,7 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
-#include "base/test/scoped_task_environment.h"
+#include "base/message_loop/message_loop.h"
+#include "base/test/scoped_mock_time_message_loop_task_runner.h"
 #include "chrome/browser/android/vr_shell/ui_browser_interface.h"
 #include "chrome/browser/android/vr_shell/ui_elements/ui_element.h"
 #include "chrome/browser/android/vr_shell/ui_elements/ui_element_debug_id.h"
@@ -103,7 +104,7 @@ class UiSceneManagerTest : public testing::Test {
     return true;
   }
 
-  base::test::ScopedTaskEnvironment scoped_task_environment_;
+  base::MessageLoop message_loop_;
   std::unique_ptr<MockBrowserInterface> browser_;
   std::unique_ptr<UiScene> scene_;
   std::unique_ptr<UiSceneManager> manager_;
@@ -147,6 +148,21 @@ TEST_F(UiSceneManagerTest, WebVrWarningsDoNotShowWhenInitiallyOutsideWebVr) {
   manager_->SetWebVrMode(true, false, false);
   EXPECT_TRUE(IsVisible(kWebVrPermanentHttpSecurityWarning));
   EXPECT_TRUE(IsVisible(kWebVrTransientHttpSecurityWarning));
+}
+
+TEST_F(UiSceneManagerTest, WebVrTransientWarningTimesOut) {
+  base::ScopedMockTimeMessageLoopTaskRunner task_runner_;
+
+  MakeManager(kNotInCct, kInWebVr);
+  EXPECT_TRUE(IsVisible(kWebVrTransientHttpSecurityWarning));
+
+  // Note: Fast-forwarding appears broken in conjunction with restarting timers.
+  // In this test, we can fast-forward by the appropriate time interval, but in
+  // other cases (until the bug is addressed), we could work around the problem
+  // by using FastForwardUntilNoTasksRemain() instead.
+  // See http://crbug.com/736558.
+  task_runner_->FastForwardBy(base::TimeDelta::FromSeconds(31));
+  EXPECT_FALSE(IsVisible(kWebVrTransientHttpSecurityWarning));
 }
 
 TEST_F(UiSceneManagerTest, ToastVisibility) {
