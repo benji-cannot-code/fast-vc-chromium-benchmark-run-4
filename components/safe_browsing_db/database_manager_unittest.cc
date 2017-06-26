38955ce04f338bf14a77fbabf42298a096c7407a
Derek Cheng <imcheng@chromium.org>
2017-06-26 23:13:47 +0000

    Revert "Fix thread safety issues with safe browsing DB."
    This reverts commit 0ac8d89ee822c431ea66e0a7da762bb8c28b1877.
    
    Reason for revert: Seems to have broken RemoteDatabaseManagerTest on android - crbug.com/736924
    
    Original change's description:
    > Fix thread safety issues with safe browsing DB.
    > 
    > Fix thread safety issues with safe browsing DB.
    > The Safe Browsing DB has some sequence consistency issues, which
    > is caught by the stricter WeakPtr semantics detailed on CL 2908073007.
    > 
    > * Delete SafeBrowsingDatabaseManager on IO thread.
    > * Remove illegal dereferencing of IO-thread WeakPtr from DB thread in
    >   V4Database::VerifyChecksumOnTaskRunner
    > * Remove non-threadsafe accesses of |io_thread_| resident members from
    >   the DB thread in V4LocalDatabaseManager.
    > * Add IO-thread runloops to unit tests to handle database teardown.
    > 
    > 
    > R=​nparker@chromium.org
    > CC=wez@chromium.org
    > 
    > Bug: 729716
    > Change-Id: I1bc620d42aca2f1cc99e482b7776a628d783d390
    > Reviewed-on: https://chromium-review.googlesource.com/523983
    > Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    > Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    > Reviewed-by: Greg Thompson <grt@chromium.org>
    > Reviewed-by: Varun Khaneja <vakh@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#481735}
    
    TBR=kmarshall@chromium.org,nparker@chromium.org,csharrison@chromium.org,vakh@chromium.org,grt@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 729716
    Change-Id: Ia10145f404619bdde8f0348d5ab764fd29a59fbc
    Reviewed-on: https://chromium-review.googlesource.com/549073
    Reviewed-by: Derek Cheng <imcheng@chromium.org>
    Commit-Queue: Derek Cheng <imcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482489}

--- components/safe_browsing_db/database_manager_unittest.cc
+++ components/safe_browsing_db/database_manager_unittest.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
-#include "base/synchronization/waitable_event.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "components/safe_browsing_db/test_database_manager.h"
 #include "components/safe_browsing_db/v4_protocol_manager_util.h"
@@ -70,9 +69,8 @@ class SafeBrowsingDatabaseManagerTest : public testing::Test {
   }
 
   void TearDown() override {
-    db_manager_->StopOnIOThread(false);
-    db_manager_ = nullptr;
     base::RunLoop().RunUntilIdle();
+    db_manager_->StopOnIOThread(false);
   }
 
   std::string GetStockV4GetHashResponse() {
