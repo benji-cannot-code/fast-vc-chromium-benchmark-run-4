ad96751cb3e16889aebf671546e8ba7f175bd68f
mgersh <mgersh@chromium.org>
2017-06-26 12:20:24 -0700

    Fix test failure on Cronet Asan bot
    HostCachePersistenceManager didn't properly initialize writing_pref_,
    which caused it to miss the initial read from prefs when asan is
    enabled.
    
    BUG=736803
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_cronet_tester
    
    Review-Url: https://codereview.chromium.org/2954393002
    Cr-Commit-Position: refs/heads/master@{#482349}

--- components/cronet/host_cache_persistence_manager.cc
+++ components/cronet/host_cache_persistence_manager.cc
@@ -21,6 +21,7 @@ HostCachePersistenceManager::HostCachePersistenceManager(
     : cache_(cache),
       pref_service_(pref_service),
       pref_name_(pref_name),
+      writing_pref_(false),
       delay_(delay),
       weak_factory_(this) {
   DCHECK(cache_);
