8884793d5cbe8f707b6fd2e43e2c459625e1c0fc
Sasha Bermeister <sashab@chromium.org>
2017-06-26 11:59:53 +1000

    Moved WebFormElementObserverImpl to core/exported
    Bug: 712963
    Change-Id: I3f9a301bedf73038b6c5bdb67419599f76f2be5a
    Reviewed-on: https://chromium-review.googlesource.com/541179
    Commit-Queue: Sasha Morrissey <sashab@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482192}

--- third_party/WebKit/Source/core/exported/WebFormElementObserverImpl.h
+++ third_party/WebKit/Source/core/exported/WebFormElementObserverImpl.h
@@ -6,19 +6,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WebFormElementObserverImpl_h
 #define WebFormElementObserverImpl_h
 
+#include "core/CoreExport.h"
 #include "platform/heap/HeapAllocator.h"
 #include "platform/heap/Member.h"
 #include "platform/heap/SelfKeepAlive.h"
 #include "platform/wtf/Compiler.h"
 #include "public/web/modules/password_manager/WebFormElementObserver.h"
-#include "web/WebExport.h"
 
 namespace blink {
 
 class HTMLElement;
 class WebFormElementObserverCallback;
 
-class WEB_EXPORT WebFormElementObserverImpl final
+class CORE_EXPORT WebFormElementObserverImpl final
     : public GarbageCollectedFinalized<WebFormElementObserverImpl>,
       NON_EXPORTED_BASE(public WebFormElementObserver) {
   WTF_MAKE_NONCOPYABLE(WebFormElementObserverImpl);
