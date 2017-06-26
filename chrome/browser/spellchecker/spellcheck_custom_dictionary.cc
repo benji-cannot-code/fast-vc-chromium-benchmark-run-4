239ce60efcf2cfa6011cec6aff98d2538f2d35d5
tzik <tzik@chromium.org>
2017-06-26 18:31:47 +0900

    Rewrite base::Bind to base::BindOnce in chrome/browser/spellchecker
    This is a generated CL to rewrite base::Bind to base::BindOnce where
    the result of base::Bind is immediately converted to OnceCallback.
    E.g.:
      base::PostTask(FROM_HERE, base::Bind(&Foo));
    is rewritten to:
      base::PostTask(FROM_HERE, base::BindOnce(&Foo));
    
    An explainer of OnceCallback is available here goo.gl/YZiUL1.
    
    The rewriting tool is in //tools/clang/base_bind_rewriters. To build and
    run locally, follow //docs/clang_tool_refactoring.md.
    
    This conversion makes almost no behavior change except that a bound
    objects are passed-by-move to the target function.
    
    Bug: 554299, 714018
    Change-Id: I650d279475adc7c55285e6f2b0f898c092350b50
    Reviewed-on: https://chromium-review.googlesource.com/547125
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482513}

--- chrome/browser/spellchecker/spellcheck_custom_dictionary.cc
+++ chrome/browser/spellchecker/spellcheck_custom_dictionary.cc
@@ -454,8 +454,8 @@ void SpellcheckCustomDictionary::FixInvalidFile(
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   BrowserThread::PostTask(
       BrowserThread::FILE, FROM_HERE,
-      base::Bind(&SavePassedWordsToDictionaryFileReliably,
-                 custom_dictionary_path_, base::Passed(&load_file_result)));
+      base::BindOnce(&SavePassedWordsToDictionaryFileReliably,
+                     custom_dictionary_path_, base::Passed(&load_file_result)));
 }
 
 void SpellcheckCustomDictionary::Save(
@@ -464,8 +464,9 @@ void SpellcheckCustomDictionary::Save(
   fix_invalid_file_.Cancel();
   BrowserThread::PostTask(
       BrowserThread::FILE, FROM_HERE,
-      base::Bind(&SpellcheckCustomDictionary::UpdateDictionaryFile,
-                 base::Passed(&dictionary_change), custom_dictionary_path_));
+      base::BindOnce(&SpellcheckCustomDictionary::UpdateDictionaryFile,
+                     base::Passed(&dictionary_change),
+                     custom_dictionary_path_));
 }
 
 syncer::SyncError SpellcheckCustomDictionary::Sync(
