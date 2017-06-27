7f9c00d962c18180a64fd431a6f8f152a1fe59de
liberato@chromium.org <liberato@chromium.org>
2017-06-27 10:30:11 -0700

    Make AVDA drop overlays when its frame is hidden.
    When the media player's frame is hidden, we want to give up any
    overlay that's used by it.  Even though the pipeline will suspend on
    Android, the VideoFrames might survive for much longer.
    
    This CL signals AVDA that the frame is hidden, so that it can drop
    the overlays.  Note that AVDA::Destroy isn't enough; suspending the
    player while the frames are still visible shouldn't do this.
    
    Bug: 737155
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I958fb07fe2a5fb6e7dedd75417875e23fd98e62a
    Reviewed-on: https://chromium-review.googlesource.com/550400
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482734}

--- media/blink/webmediaplayer_impl.cc
+++ media/blink/webmediaplayer_impl.cc
@@ -1563,6 +1563,9 @@ void WebMediaPlayerImpl::OnFrameHidden() {
   if (IsHidden())
     video_locked_when_paused_when_hidden_ = true;
 
+  overlay_info_.is_frame_hidden = true;
+  MaybeSendOverlayInfoToDecoder();
+
   if (watch_time_reporter_)
     watch_time_reporter_->OnHidden();
 
@@ -1576,6 +1579,11 @@ void WebMediaPlayerImpl::OnFrameHidden() {
 
 void WebMediaPlayerImpl::OnFrameClosed() {
   DCHECK(main_task_runner_->BelongsToCurrentThread());
+
+  // Re-use |is_hidden| since nothing cares about the difference anyway.
+  overlay_info_.is_frame_hidden = true;
+  MaybeSendOverlayInfoToDecoder();
+
   UpdatePlayState();
 }
 
@@ -1585,6 +1593,9 @@ void WebMediaPlayerImpl::OnFrameShown() {
 
   // Foreground videos don't require user gesture to continue playback.
   video_locked_when_paused_when_hidden_ = false;
+
+  overlay_info_.is_frame_hidden = false;
+  MaybeSendOverlayInfoToDecoder();
 
   if (watch_time_reporter_)
     watch_time_reporter_->OnShown();
--- media/gpu/avda_shared_state.cc
+++ media/gpu/avda_shared_state.cc
@@ -28,7 +28,7 @@ AVDASharedState::AVDASharedState(
   // overlay's surface is destroyed.
   if (overlay()) {
     overlay()->AddSurfaceDestroyedCallback(base::Bind(
-        &AVDASharedState::OnSurfaceDestroyed, weak_this_factory_.GetWeakPtr()));
+        &AVDASharedState::ClearOverlay, weak_this_factory_.GetWeakPtr()));
   }
 }
 
@@ -62,7 +62,7 @@ void AVDASharedState::ClearReleaseTime() {
     surface_texture()->IgnorePendingRelease();
 }
 
-void AVDASharedState::OnSurfaceDestroyed(AndroidOverlay* overlay_raw) {
+void AVDASharedState::ClearOverlay(AndroidOverlay* overlay_raw) {
   if (surface_bundle_ && overlay() == overlay_raw)
     surface_bundle_ = nullptr;
 }
