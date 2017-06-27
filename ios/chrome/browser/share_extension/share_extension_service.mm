8f3512fc8b9f3e1cbac1b49894e84cad7af33d00
Sylvain Defresne <sdefresne@chromium.org>
2017-06-27 15:50:59 +0200

    Remove FILE thread dependency in share_extension_item_receiver.mm.
    The named thread API is deprecated in favor of the post_task.h API,
    instead use a local SequencedTaskRunner with required traits.
    
    Change ShareExtensionItemReceiver to no longer be a singleton as the
    SequencedTaskRunner returned by the post_task.h API reference the
    TaskSequencer and becomes invalid after each unit tests. As only a
    single object was referencing ShareExtensionItemReceiver, it did not
    really need to be a singleton.
    
    Change the posted blocks to use __weak pointer to self as they will
    be cancelled when the object is destroyed anyway (since the object
    owns the SequencedTaskRunner) so there is no need to extend lifetime
    by capturing a reference to self.
    
    Add missing #include for IWYU (include what you use) and change the
    factory from a .cc to an .mm file as share_extension_service.h needs
    to forward-declare an Objective-C class.
    
    Test:
    1.  launch another application and share a link to Chrome,
    2.  select either "Read Later" or "Bookmark", then switch to Chrome,
    3.  check that the entry has been added to "Read Later" list or to
        the list of bookmarks.
    
    Bug: 689520
    Change-Id: I50bd4c510d8750a38e2678f20051638672548707
    Reviewed-on: https://chromium-review.googlesource.com/550055
    Reviewed-by: Elodie Banel <lod@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482970}

--- ios/chrome/browser/share_extension/share_extension_service.mm
+++ ios/chrome/browser/share_extension/share_extension_service.mm
@@ -20,7 +20,8 @@ ShareExtensionService::ShareExtensionService(
     : reading_list_model_(reading_list_model),
       reading_list_model_loaded_(false),
       bookmark_model_(bookmark_model),
-      bookmark_model_loaded_(false) {
+      bookmark_model_loaded_(false),
+      receiver_(nil) {
   DCHECK(bookmark_model);
   DCHECK(reading_list_model);
 }
@@ -37,11 +38,12 @@ void ShareExtensionService::Initialize() {
 }
 
 void ShareExtensionService::Shutdown() {
-  [[ShareExtensionItemReceiver sharedInstance] shutdown];
   reading_list_model_->RemoveObserver(this);
   reading_list_model_loaded_ = false;
   bookmark_model_->RemoveObserver(this);
   bookmark_model_loaded_ = false;
+  [receiver_ shutdown];
+  receiver_ = nil;
 }
 
 void ShareExtensionService::ReadingListModelLoaded(
@@ -56,10 +58,13 @@ void ShareExtensionService::BookmarkModelLoaded(bookmarks::BookmarkModel* model,
   this->AnyModelLoaded();
 }
 
+void ShareExtensionService::BookmarkModelChanged() {}
+
 void ShareExtensionService::AnyModelLoaded() {
   if (reading_list_model_loaded_ && bookmark_model_loaded_) {
-    [[ShareExtensionItemReceiver sharedInstance]
-        setBookmarkModel:bookmark_model_
-        readingListModel:reading_list_model_];
+    DCHECK(!receiver_);
+    receiver_ = [[ShareExtensionItemReceiver alloc]
+        initWithBookmarkModel:bookmark_model_
+             readingListModel:reading_list_model_];
   }
 }
