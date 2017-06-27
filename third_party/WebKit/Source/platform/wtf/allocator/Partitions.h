f46e1bc4f62458332da5377fd06bcb5444105de5
palmer <palmer@chromium.org>
2017-06-27 10:51:39 -0700

    Fix another FastMalloc call site with potential integer overflow.
    BUG=669751
    
    Review-Url: https://codereview.chromium.org/2957933002
    Cr-Commit-Position: refs/heads/master@{#482676}

--- third_party/WebKit/Source/platform/wtf/allocator/Partitions.h
+++ third_party/WebKit/Source/platform/wtf/allocator/Partitions.h
@@ -174,6 +174,8 @@ using base::PartitionMemoryStats;
 using base::PartitionBucketMemoryStats;
 using base::PartitionAllocHooks;
 
+using CheckedSizeT = base::CheckedNumeric<size_t>;
+
 }  // namespace WTF
 
 #endif  // Partitions_h
