8c0175c915d70047d06a6991127cd69870735d88
ossu <ossu@chromium.org>
2017-06-27 06:02:17 -0700

    Propagate muted state from MediaStreamAudioSource into JS.
    BUG=chromium:729002
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2922733002
    Cr-Commit-Position: refs/heads/master@{#482609}

--- content/test/data/media/webrtc_test_utilities.js
+++ content/test/data/media/webrtc_test_utilities.js
@@ -19,6 +19,8 @@ var gNumberOfEvents = 0;
 
 var gAllEventsOccured = function () {};
 
+var gPendingTimeout;
+
 // Use this function to set a function that will be called once all expected
 // events has occurred.
 function setAllEventsOccuredHandler(handler) {
@@ -40,6 +42,20 @@ function sendValueToTest(value) {
 function failTest(reason) {
   var error = new Error(reason);
   window.domAutomationController.send(error.stack);
+}
+
+// Fail a test on the C++ side after a timeout. Will cancel any pending timeout.
+function failTestAfterTimeout(reason, timeout_ms) {
+  cancelTestTimeout();
+  gPendingTimeout = setTimeout(function() {
+    failTest(reason);
+  }, timeout_ms);
+}
+
+// Cancels the current test timeout.
+function cancelTestTimeout() {
+  clearTimeout(gPendingTimeout);
+  gPendingTimeout = null;
 }
 
 // Called if getUserMedia fails.
@@ -263,4 +279,3 @@ function assertTrue(booleanExpression, description) {
     failTest(description);
   }
 }
-
