df80c7310f1ea799ef15de5993e3986ac23b9f55
kylechar <kylechar@chromium.org>
2017-06-27 17:33:11 -0400

    Change SurfaceReferenceFactory usage in SurfaceManager.
    Move the StubSurfaceReferenceFactory implementation to //cc/surfaces and
    use it in SurfaceManager when surface references are enabled. Also
    update the comment on SurfaceReferencyFactory to reflect that it's only
    used with SurfaceSequences and will be deleted eventually, since the
    name is misleading at this point.
    
    Bug: 676384
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I20e3a43186c67ec3b0e0812bbbc0636b8bc60880
    Reviewed-on: https://chromium-review.googlesource.com/550537
    Reviewed-by: John Bauman <jbauman@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482986}

--- cc/surfaces/surface_manager.cc
+++ cc/surfaces/surface_manager.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/surfaces/compositor_frame_sink_support.h"
 #include "cc/surfaces/direct_surface_reference_factory.h"
 #include "cc/surfaces/local_surface_id_allocator.h"
+#include "cc/surfaces/stub_surface_reference_factory.h"
 #include "cc/surfaces/surface.h"
 #include "cc/surfaces/surface_info.h"
 
@@ -35,8 +36,12 @@ SurfaceManager::SurfaceManager(LifetimeType lifetime_type)
                        LocalSurfaceId(1u, base::UnguessableToken::Create())),
       weak_factory_(this) {
   thread_checker_.DetachFromThread();
-  reference_factory_ =
-      new DirectSurfaceReferenceFactory(weak_factory_.GetWeakPtr());
+  if (using_surface_references()) {
+    reference_factory_ = new StubSurfaceReferenceFactory();
+  } else {
+    reference_factory_ =
+        new DirectSurfaceReferenceFactory(weak_factory_.GetWeakPtr());
+  }
 }
 
 SurfaceManager::~SurfaceManager() {
--- cc/surfaces/surface_reference_factory.h
+++ cc/surfaces/surface_reference_factory.h
@@ -13,8 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc {
 
-// Creates surface references. The referenced surface will be kept alive as
-// long as there is a reference to it.
+// Confusingly, SurfaceReferenceFactory is only used to create SurfaceSequences.
+// TODO(kylechar): Delete all usage of SurfaceReferenceFactory when surface
+// references are enabled by default.
 class SurfaceReferenceFactory
     : public base::RefCountedThreadSafe<SurfaceReferenceFactory> {
  public:
