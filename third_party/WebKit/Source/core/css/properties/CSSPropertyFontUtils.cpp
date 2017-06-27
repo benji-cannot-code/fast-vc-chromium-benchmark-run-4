c06da5180437cf0966ae6536dbb563ef20abe5ea
Alan Cutter <alancutter@chromium.org>
2017-06-27 11:16:16 +1000

    Don't treat "default" as a CSS-wide keyword
    The keyword "default" was defined as a CSS-wide keyword in
    https://codereview.chromium.org/1331233003 for a parser helper function.
    This is no longer matching spec or actual CSSPropertyParser behaviour.
    
    This patch updates the helper function to match spec and behaviour.
    
    Change-Id: Id07de5320bfcd66771ed9431012185578e39a6a2
    Reviewed-on: https://chromium-review.googlesource.com/544411
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: nainar <nainar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482539}

--- third_party/WebKit/Source/core/css/properties/CSSPropertyFontUtils.cpp
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyFontUtils.cpp
@@ -89,8 +89,10 @@ String CSSPropertyFontUtils::ConcatenateFamilyName(CSSParserTokenRange& range) {
     builder.Append(range.ConsumeIncludingWhitespace().Value());
   }
   if (!added_space &&
-      CSSPropertyParserHelpers::IsCSSWideKeyword(first_token.Value()))
+      (CSSPropertyParserHelpers::IsCSSWideKeyword(first_token.Value()) ||
+       EqualIgnoringASCIICase(first_token.Value(), "default"))) {
     return String();
+  }
   return builder.ToString();
 }
 
