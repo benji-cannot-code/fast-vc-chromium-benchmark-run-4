22d5c565a6e657404888d66f01cd06c5b9c68858
dgozman <dgozman@chromium.org>
2017-06-26 17:51:47 -0700

    [DevTools] Migrate inspector-protocol/runtime tests to new harness
    BUG=734762
    
    Review-Url: https://codereview.chromium.org/2954093003
    Cr-Commit-Position: refs/heads/master@{#482493}

---
+++ third_party/WebKit/LayoutTests/inspector-protocol/runtime/runtime-evaluate-in-default-context.js
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  let {page, session, dp} = await testRunner.startBlank(`Tests that default execution context is selected correctly.`);
+
+  await session.evaluateAsync(`
+    var frame = document.createElement('iframe');
+    frame.src = '${testRunner.url('../resources/runtime-events-iframe.html')}';
+    document.body.appendChild(frame);
+    new Promise(f => frame.onload = f)
+  `);
+
+  var result = await dp.Runtime.evaluate({expression: 'window.a' });
+  testRunner.logMessage(result);
+  testRunner.completeTest();
+})
---
+++ third_party/WebKit/LayoutTests/inspector-protocol/runtime/runtime-evaluate-without-enabling.js
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  let {page, session, dp} = await testRunner.startBlank(`Tests that default execution context accessed without enabling Runtime domain gets properly cleaned up on reload.`);
+  await session.evaluate('window.dummyObject = { a : 1 };');
+  var result = await dp.Runtime.evaluate({expression: 'window.dummyObject' });
+  await dp.Page.reload();
+  testRunner.logMessage(await dp.Runtime.getProperties({ objectId: result.result.result.objectId, ownProperties: true }));
+  testRunner.completeTest();
+})
