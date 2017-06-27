02352d462523c050b35aa68b840d965a36b1ce84
dgozman <dgozman@chromium.org>
2017-06-27 14:44:09 -0700

    [DevTools] Migrate inspector-protocol/{input,network} tests to new harness
    BUG=734762
    
    Review-Url: https://codereview.chromium.org/2961493002
    Cr-Commit-Position: refs/heads/master@{#482756}

---
+++ third_party/WebKit/LayoutTests/inspector-protocol/input/dispatchMouseEvent.js
@@ -0,0 +1,70 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  let {page, session, dp} = await testRunner.startBlank(``);
+
+  await session.evaluate(`
+    var logs = [];
+    function log(text) {
+      logs.push(text);
+    }
+
+    function logEvent(event) {
+      log('-----Event-----');
+      log('type: ' + event.type);
+      log('button: ' + event.button);
+      if (event.shiftKey)
+        log('shiftKey');
+      log('x: ' + event.x);
+      log('y: ' + event.y);
+      event.preventDefault();
+    }
+
+    window.addEventListener('mousedown', logEvent);
+    window.addEventListener('mouseup', logEvent);
+    window.addEventListener('mousemove', logEvent);
+    window.addEventListener('contextmenu', logEvent);
+  `);
+
+  function dumpError(message) {
+    if (message.error)
+      testRunner.log('Error: ' + message.error.message);
+  }
+
+  dumpError(await dp.Input.dispatchMouseEvent({
+    type: 'mousePressed',
+    button: 'left',
+    clickCount: 1,
+    x: 100,
+    y: 200
+  }));
+  dumpError(await dp.Input.dispatchMouseEvent({
+    type: 'mouseReleased',
+    button: 'left',
+    clickCount: 1,
+    x: 100,
+    y: 200
+  }));
+  dumpError(await dp.Input.dispatchMouseEvent({
+    type: 'mouseMoved',
+    modifiers: 8, // shift
+    x: 50,
+    y: 150
+  }));
+  dumpError(await dp.Input.dispatchMouseEvent({
+    type: 'mousePressed',
+    button: 'right',
+    clickCount: 1,
+    x: 100,
+    y: 200
+  }));
+  dumpError(await dp.Input.dispatchMouseEvent({
+    type: 'mouseReleased',
+    button: 'right',
+    clickCount: 1,
+    x: 100,
+    y: 200
+  }));
+
+  testRunner.log(await session.evaluate(`window.logs.join('\\n')`));
+  testRunner.completeTest();
+})
