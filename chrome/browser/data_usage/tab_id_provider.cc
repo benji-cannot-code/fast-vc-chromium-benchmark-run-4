830fd4faeba3d558bd5226a4bbda919a900a582b
rajendrant <rajendrant@chromium.org>
2017-06-26 19:12:45 -0700

    Populate global request ID for mainframe request in DataUse
    With PlzNavigate, Tab ID will be -1 for mainframe requests. This CL
    populates global request ID for mainframe request in DataUse. This will
    be used in subsequent CL to match with its mainframe navigation which
    could happen after mainframe request is destroyed, and associate with
    its tab ID.
    
    BUG=718598
    
    Review-Url: https://codereview.chromium.org/2954523003
    Cr-Commit-Position: refs/heads/master@{#482515}

--- chrome/browser/data_usage/tab_id_provider.cc
+++ chrome/browser/data_usage/tab_id_provider.cc
@@ -22,8 +22,8 @@ namespace chrome_browser_data_usage {
 namespace {
 
 // Convenience typedefs for clarity.
-typedef base::Callback<int32_t(void)> TabIdGetter;
-typedef base::Callback<void(int32_t)> TabIdCallback;
+typedef base::Callback<TabIdProvider::URLRequestTabInfo(void)> TabIdGetter;
+typedef base::Callback<void(TabIdProvider::URLRequestTabInfo)> TabIdCallback;
 
 }  // namespace
 
@@ -37,7 +37,7 @@ class TabIdProvider::CallbackRunner {
   ~CallbackRunner() {
     // Ensure that no callbacks are abandoned without being run.
     if (!is_done_)
-      RunAll(-1);
+      RunAll(URLRequestTabInfo(-1, content::GlobalRequestID()));
   }
 
   // Adds a new callback to be run later. New callbacks must not be added after
@@ -50,7 +50,7 @@ class TabIdProvider::CallbackRunner {
 
   // Runs all the callbacks in the order that they were added. This method must
   // not be called more than once.
-  void RunAll(int32_t tab_id) {
+  void RunAll(TabIdProvider::URLRequestTabInfo tab_info) {
     DCHECK(thread_checker_.CalledOnValidThread());
     DCHECK(!is_done_);
     is_done_ = true;
@@ -58,7 +58,7 @@ class TabIdProvider::CallbackRunner {
     // |callbacks_| can no longer be modified at this point, so it's impossible
     // for one of the |callbacks_| to add more callbacks to this CallbackRunner.
     for (const auto& callback : callbacks_)
-      callback.Run(tab_id);
+      callback.Run(tab_info);
     callbacks_.clear();
   }
 
@@ -79,7 +79,9 @@ class TabIdProvider::CallbackRunner {
 TabIdProvider::TabIdProvider(base::TaskRunner* task_runner,
                              const tracked_objects::Location& from_here,
                              const TabIdGetter& tab_id_getter)
-    : is_tab_id_ready_(false), tab_id_(-1), weak_ptr_factory_(this) {
+    : is_tab_info_ready_(false),
+      tab_info_(-1, content::GlobalRequestID()),
+      weak_ptr_factory_(this) {
   std::unique_ptr<CallbackRunner> callback_runner(new CallbackRunner());
   weak_callback_runner_ = callback_runner->GetWeakPtr();
   callback_runner->AddCallback(
@@ -99,8 +101,8 @@ TabIdProvider::~TabIdProvider() {}
 
 void TabIdProvider::ProvideTabId(const TabIdCallback& callback) {
   DCHECK(thread_checker_.CalledOnValidThread());
-  if (is_tab_id_ready_) {
-    callback.Run(tab_id_);
+  if (is_tab_info_ready_) {
+    callback.Run(tab_info_);
     return;
   }
   if (weak_callback_runner_) {
@@ -110,7 +112,7 @@ void TabIdProvider::ProvideTabId(const TabIdCallback& callback) {
   // If no cached tab ID is available and |weak_callback_runner_| has been
   // destroyed, pass a tab ID of -1 to the callback indicating that no tab was
   // found.
-  callback.Run(-1);
+  callback.Run(URLRequestTabInfo(-1, content::GlobalRequestID()));
 }
 
 base::WeakPtr<TabIdProvider> TabIdProvider::GetWeakPtr() {
@@ -122,11 +124,11 @@ base::WeakPtr<TabIdProvider> TabIdProvider::GetWeakPtr() {
 const void* TabIdProvider::kUserDataKey =
     static_cast<void*>(&TabIdProvider::kUserDataKey);
 
-void TabIdProvider::OnTabIdReady(int32_t tab_id) {
+void TabIdProvider::OnTabIdReady(URLRequestTabInfo tab_info) {
   DCHECK(thread_checker_.CalledOnValidThread());
-  DCHECK(!is_tab_id_ready_);
-  tab_id_ = tab_id;
-  is_tab_id_ready_ = true;
+  DCHECK(!is_tab_info_ready_);
+  tab_info_ = tab_info;
+  is_tab_info_ready_ = true;
 }
 
 }  // namespace chrome_browser_data_usage
