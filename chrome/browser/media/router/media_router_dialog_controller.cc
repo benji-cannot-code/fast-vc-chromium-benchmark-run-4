c48188b8e580ea2d7d82ab473b5791e05eb5d6e2
Adam Parker <amp@chromium.org>
2017-06-27 16:50:15 +0000

    Revert "[VrShell] Temporarily disable Media Router dialog when in VR."
    This reverts commit 7acdd748b1ef6a8a2d957c9f8a14e6873f537d25.
    
    Reason for revert: Doesn't disable the dialog (and isn't the correct place to do so).
    
    Original change's description:
    > [VrShell] Temporarily disable Media Router dialog when in VR.
    > 
    > This will be re-enabled when the dialog can be displayed properly in binocular vr.
    > 
    > Bug: 733410
    > Change-Id: Ia40339a0538b8c4631c21e6e6e9d87fbaa67dc5a
    > Reviewed-on: https://chromium-review.googlesource.com/547076
    > Commit-Queue: Adam Parker <amp@chromium.org>
    > Reviewed-by: Jennifer Apacible <apacible@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#482387}
    
    TBR=cjgrant@chromium.org,asimjour@chromium.org,apacible@chromium.org,amp@chromium.org
    
    Change-Id: I4a2614b6c6859dd1163b9dff65d76de95d33c234
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 733410
    Reviewed-on: https://chromium-review.googlesource.com/550575
    Reviewed-by: Amirhossein Simjour <asimjour@chromium.org>
    Commit-Queue: Adam Parker <amp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482666}

--- chrome/browser/media/router/media_router_dialog_controller.cc
+++ chrome/browser/media/router/media_router_dialog_controller.cc
@@ -11,13 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_delegate.h"
-#include "device/vr/features/features.h"
 
 #if defined(OS_ANDROID)
 #include "chrome/browser/media/android/router/media_router_dialog_controller_android.h"
-#if BUILDFLAG(ENABLE_VR)
-#include "chrome/browser/android/vr_shell/vr_tab_helper.h"
-#endif  // BUILDFLAG(ENABLE_VR)
 #else
 #include "chrome/browser/ui/webui/media_router/media_router_dialog_controller_impl.h"
 #endif
@@ -29,11 +25,6 @@ MediaRouterDialogController*
 MediaRouterDialogController::GetOrCreateForWebContents(
     content::WebContents* contents) {
 #if defined(OS_ANDROID)
-#if BUILDFLAG(ENABLE_VR)
-  if (vr_shell::VrTabHelper::IsInVr(contents)) {
-    return nullptr;
-  }
-#endif
   return MediaRouterDialogControllerAndroid::GetOrCreateForWebContents(
       contents);
 #else
