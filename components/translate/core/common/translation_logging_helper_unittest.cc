f5f91687f10d8c6aba6d581220e1484c0a5c93f4
skym <skym@chromium.org>
2017-06-27 07:35:51 -0700

    [Sync] Make children messages of UserEventSpecifics' consistent use event suffix.
    Inside of the UserEventSpecifics proto each potential field of the
    oneof should use _event as a suffix of the field name, but none of the
    message classes that are held by these _event fields should have Event
    as a suffix of the message name.
    
    BUG=701032
    
    Review-Url: https://codereview.chromium.org/2942703002
    Cr-Commit-Position: refs/heads/master@{#482622}

--- components/translate/core/common/translation_logging_helper_unittest.cc
+++ components/translate/core/common/translation_logging_helper_unittest.cc
@@ -43,7 +43,8 @@ TEST(TranslationLoggingHelperTest, ConstructUserEventSpecifics) {
       navigation_id, translation_event, &user_specifics);
   EXPECT_TRUE(needs_logging);
   EXPECT_EQ(user_specifics.navigation_id(), navigation_id);
-  EqualTranslationProto(user_translation_event, user_specifics.translation());
+  EqualTranslationProto(user_translation_event,
+                        user_specifics.translation_event());
 }
 
 // Tests that if user change the target language, the event is MANUAL.
@@ -66,7 +67,8 @@ TEST(TranslationLoggingHelperTest, UserManualEvent) {
       navigation_id, translation_event, &user_specifics);
   EXPECT_TRUE(needs_logging);
   EXPECT_EQ(user_specifics.navigation_id(), navigation_id);
-  EqualTranslationProto(user_translation_event, user_specifics.translation());
+  EqualTranslationProto(user_translation_event,
+                        user_specifics.translation_event());
 }
 
 // Tests that we don't build unnecessary events.
