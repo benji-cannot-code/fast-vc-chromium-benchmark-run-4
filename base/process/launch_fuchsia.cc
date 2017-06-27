489fd7ee48cc721ace07fbfee681659c202021b9
Scott Graham <scottmg@chromium.org>
2017-06-27 11:35:51 -0700

    Update Fuchsia SDK to 17e51e32c9a81d3f957afe533ac99ef1d565378c
    Follows https://bugs.chromium.org/p/chromium/issues/detail?id=707030#c4.
    
    Notable changes include:
    - LP_CLONE_MXIO_ROOT -> LP_CLONE_MXIO_NAMESPACE
    - removal of epoll (not used in currently-compiling code)
    
    Bug: 707030
    Change-Id: I8c29958fce0fb04a3513265c1485dec35c771b9d
    Reviewed-on: https://chromium-review.googlesource.com/550403
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Scott Graham <scottmg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482717}

--- base/process/launch_fuchsia.cc
+++ base/process/launch_fuchsia.cc
@@ -32,7 +32,7 @@ bool GetAppOutputInternal(const std::vector<std::string>& argv,
   launchpad_create(MX_HANDLE_INVALID, argv_cstr[0], &lp);
   launchpad_load_from_file(lp, argv_cstr[0]);
   launchpad_set_args(lp, argv.size(), argv_cstr.data());
-  launchpad_clone(lp, LP_CLONE_MXIO_ROOT | LP_CLONE_MXIO_CWD |
+  launchpad_clone(lp, LP_CLONE_MXIO_NAMESPACE | LP_CLONE_MXIO_CWD |
                           LP_CLONE_DEFAULT_JOB | LP_CLONE_ENVIRON);
   launchpad_clone_fd(lp, STDIN_FILENO, STDIN_FILENO);
   int pipe_fd;
@@ -94,7 +94,7 @@ Process LaunchProcess(const std::vector<std::string>& argv,
   launchpad_load_from_file(lp, argv_cstr[0]);
   launchpad_set_args(lp, argv.size(), argv_cstr.data());
 
-  uint32_t to_clone = LP_CLONE_MXIO_ROOT | LP_CLONE_DEFAULT_JOB;
+  uint32_t to_clone = LP_CLONE_MXIO_NAMESPACE | LP_CLONE_DEFAULT_JOB;
 
   std::unique_ptr<char* []> new_environ;
   char* const empty_environ = nullptr;
