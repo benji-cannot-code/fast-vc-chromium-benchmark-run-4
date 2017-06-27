64c4185b7800ee89d61f12f67d976266d208e97b
Shimi Zhang <ctzsm@chromium.org>
2017-06-26 18:36:04 -0700

    [Android] Reorder file closing sequence
    There is a race condition in test `testNormalPrintingFlow`,
    which will cause failures in `FileInputStream`. Generally,
    when `FileInputStream` created, the same file descriptor used
    previously will be reused, but in the instrumentation thread,
    after `onWriteFinished`, it is trying to close the same file
    descriptor, so sometimes it will cause crashes.
    
    This CL:
    
    1) Removes unnecessary fileDescriptor.close() in
    the override `onWriteFinished`.
    
    2) Place `closeFileDescriptor(mFileDescriptor)` before calls
    `onWrite*` callbacks to make the testing code wait the whole
    procedure finished, this removes the race condition.
    
    Bug: 732698
    Change-Id: Ifdc1fa9f00cb77f64f46f6d09df5890b1bb12061
    Reviewed-on: https://chromium-review.googlesource.com/549227
    Reviewed-by: Selim Gurun <sgurun@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483023}

--- chrome/android/javatests/src/org/chromium/chrome/browser/printing/PrintingControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/printing/PrintingControllerTest.java
@@ -42,7 +42,6 @@ import org.chromium.printing.PrintingControllerImpl;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
@@ -291,13 +290,7 @@ public class PrintingControllerTest {
                     new WriteResultCallbackWrapperMock() {
                         @Override
                         public void onWriteFinished(PageRange[] pages) {
-                            try {
-                                descriptor.close();
-                                // Result is ready, signal to continue.
-                                result.run();
-                            } catch (IOException ex) {
-                                Assert.fail("Failed file operation: " + ex.toString());
-                            }
+                            result.run();
                         }
                     }
             );
