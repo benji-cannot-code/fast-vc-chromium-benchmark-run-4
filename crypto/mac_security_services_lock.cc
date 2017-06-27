649c53b8b7567c019081783ffe4d6cb5430e44b5
David Benjamin <davidben@chromium.org>
2017-06-27 15:05:38 -0400

    Remove unused crypto/cssm_init.h.
    This appears to be completely unused now.
    
    Bug: 
    Change-Id: Idf670f32cbc853be951e8cf711296876a09d79f4
    Reviewed-on: https://chromium-review.googlesource.com/550637
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483007}

--- crypto/mac_security_services_lock.cc
+++ crypto/mac_security_services_lock.cc
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// This singleton pertains to Apple's wrappers over their own CSSM handles,
-// as opposed to our own CSSM_CSP_HANDLE in cssm_init.cc.
 class SecurityServicesSingleton {
  public:
   static SecurityServicesSingleton* GetInstance() {
--- net/cert/x509_certificate_mac.cc
+++ net/cert/x509_certificate_mac.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_piece.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/synchronization/lock.h"
-#include "crypto/cssm_init.h"
 #include "crypto/mac_security_services_lock.h"
 #include "net/cert/x509_util_mac.h"
 
