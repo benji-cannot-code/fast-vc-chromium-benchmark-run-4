744902f4bd5c2bdab20f897a207d8ec2c28cf4e3
dgozman <dgozman@chromium.org>
2017-06-28 11:22:40 -0700

    [DevTools] Migrate inspector-protocol/emulation tests to new harness
    BUG=734762
    
    Review-Url: https://codereview.chromium.org/2960023003
    Cr-Commit-Position: refs/heads/master@{#483067}

---
+++ third_party/WebKit/LayoutTests/inspector-protocol/emulation/device-emulation-980-2x.js
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  let {page, session, dp} = await testRunner.startBlank('Tests that device emulation affects media rules, viewport meta tag, body dimensions and window.screen.');
+
+  var DeviceEmulator = await testRunner.loadScript('../resources/device-emulator.js');
+  var deviceEmulator = new DeviceEmulator(testRunner, session);
+  await deviceEmulator.emulate(1200, 1000, 2);
+
+  var viewport = 'w=980';
+  testRunner.log(`Loading page with viewport=${viewport}`);
+  await session.navigate('../resources/device-emulation.html?' + viewport);
+
+  testRunner.log(await session.evaluate(`dumpMetrics(true)`));
+  testRunner.completeTest();
+})
---
+++ third_party/WebKit/LayoutTests/inspector-protocol/emulation/device-emulation-initial-scale.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  let {page, session, dp} = await testRunner.startBlank(`
+    Tests that turning on device emulation with a non-1 device pixel ratio sets the
+    appropriate initial scale. Page scale is reflected by the innerWidth and
+    innerHeight properties. Since the layout width is set to 980 (in the viewport
+    meta tag), and the emulated viewport width is 490 px, the initial scale should
+    be 490/980 = 0.5. i.e. innerWidth=980 innerHeight=640.`);
+
+  var DeviceEmulator = await testRunner.loadScript('../resources/device-emulator.js');
+  var deviceEmulator = new DeviceEmulator(testRunner, session);
+  // 980px viewport loaded into a 490x320 screen should load at 0.5 scale.
+  await deviceEmulator.emulate(490, 320, 3);
+
+  var viewport = 'w=980';
+  testRunner.log(`Loading page with viewport=${viewport}`);
+  await session.navigate('../resources/device-emulation.html?' + viewport);
+
+  testRunner.log(await session.evaluate(`dumpMetrics(true)`));
+  testRunner.completeTest();
+})
