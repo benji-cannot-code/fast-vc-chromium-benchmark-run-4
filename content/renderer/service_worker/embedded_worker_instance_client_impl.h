c5ecac3b2a70138c440bb9eb8eab2ff4c231072d
Matt Falkenhagen <falken@chromium.org>
2017-06-28 17:33:23 +0900

    service worker: Add UMA for start worker IPC latency.
    The motivation is to get a bound on how much optimization can be done before
    the start worker message is received. This includes work to startup a renderer
    faster and off-main-thread start worker.
    
    In order to try to distinguish between time spent in process creation/setup vs
    main thread contention, I tried to estimate when the renderer is launched
    and ready to receive messages via RenderThreadImpl::Init(). However
    it looks like on browser startup, the renderer setup finishes quickly
    (before start worker is sent) but there a huge message latency (500 ms).
    I suspect this can never be super accurate anyway because the renderer
    may have been initialized long ago, was suspended, and we need to wait
    for it to resume.
    
    The CL also sets the path for sending timing information from the renderer to
    browser, so we can record the timing directly in the renderer and remove IPCs
    we currently use to keep track of the start sequence on the browser-side.
    Because these IPCs are affected by IO thread contention on the browser side,
    they can be causing inaccurate timing information logged to UMA.
    
    Bug: 561209
    Change-Id: I4d000ba2a66c6a0576bb6c0eee21b35590db12d4
    Reviewed-on: https://chromium-review.googlesource.com/544718
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483027}

--- content/renderer/service_worker/embedded_worker_instance_client_impl.h
+++ content/renderer/service_worker/embedded_worker_instance_client_impl.h
@@ -34,7 +34,8 @@ class ServiceWorkerContextClient;
 class EmbeddedWorkerInstanceClientImpl
     : public mojom::EmbeddedWorkerInstanceClient {
  public:
-  static void Create(const service_manager::BindSourceInfo& source_info,
+  static void Create(base::TimeTicks blink_initialized_time,
+                     const service_manager::BindSourceInfo& source_info,
                      mojom::EmbeddedWorkerInstanceClientRequest request);
 
   ~EmbeddedWorkerInstanceClientImpl() override;
@@ -94,6 +95,9 @@ class EmbeddedWorkerInstanceClientImpl
 
   // nullptr means the worker is not running.
   std::unique_ptr<WorkerWrapper> wrapper_;
+
+  // For UMA.
+  base::TimeTicks blink_initialized_time_;
 
   DISALLOW_COPY_AND_ASSIGN(EmbeddedWorkerInstanceClientImpl);
 };
