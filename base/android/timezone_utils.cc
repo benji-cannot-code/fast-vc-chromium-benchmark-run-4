a977bed81c79d2657905727ee01b6314329bacec
Jungshik Shin <jshin@chromium.org>
2017-06-28 15:07:30 -0700

    Detect the OS timezone using JNI
    ICU timezone detection on Android does not work properly for most
    timezones. [1]
    
    Use JNI to detect the OS timezone and set the ICU default timezone
    accordingly both on ICU initialization and on timezone change.
    
    This is to reland https://chromium-review.googlesource.com/c/512282
    with a temporary suppression of StrictMode check for disk read for
    devices that skipped the set-up wizard.
    
    [1] http://bugs.icu-project.org/trac/ticket/13208
    
    TBR=mark@chromium.org,yfriedman@chromium.org,torne@chromium.org
    BUG=chromium:722821, chromium:737475
    TEST=Change the timezone on Android to a few different zones (e.g.
    Australia/Sydney, Asia/Seoul, Europe/Amstredam). At
    match what's chosen in Android settings.
    
    http: //jungshik.github.io/cr/bugs/722821.html , local tz should
    Change-Id: Ibc183295b46344d29f383851cf62697032e18777
    Reviewed-on: https://chromium-review.googlesource.com/553717
    Reviewed-by: Jungshik Shin <jshin@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Jungshik Shin <jshin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483276}

---
+++ base/android/timezone_utils.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/android/timezone_utils.h"
+
+#include "base/android/jni_android.h"
+#include "base/android/jni_string.h"
+#include "base/strings/string16.h"
+#include "jni/TimezoneUtils_jni.h"
+
+namespace base {
+namespace android {
+
+base::string16 GetDefaultTimeZoneId() {
+  JNIEnv* env = base::android::AttachCurrentThread();
+  ScopedJavaLocalRef<jstring> timezone_id =
+      Java_TimezoneUtils_getDefaultTimeZoneId(env);
+  return ConvertJavaStringToUTF16(timezone_id);
+}
+
+}  // namespace android
+}  // namespace base
