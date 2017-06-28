4305e392827a8078c2e6bf144af2e100432df928
jbauman <jbauman@chromium.org>
2017-06-28 12:11:35 -0700

    Log UMA histogram of error line on DXVAVDA failures.
    There's a large number of possible errors from the DXVA video decode
    accelerator, though most of them should be extremely rare. Logging a
    histogram of the line number when an error occurs should make it
    possible to determine if any errors are unexpectedly common. The line
    number won't be stable across versions, but it should be enough for
    determining the precise cause of an error within a single version.
    
    Review-Url: https://codereview.chromium.org/2962783002
    Cr-Commit-Position: refs/heads/master@{#483092}

--- media/base/win/mf_helpers.cc
+++ media/base/win/mf_helpers.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/base/win/mf_helpers.h"
 
+#include "base/metrics/histogram_macros.h"
+
 namespace media {
 
 namespace mf {
@@ -12,6 +14,7 @@ namespace mf {
 void LogDXVAError(int line) {
   LOG(ERROR) << "Error in dxva_video_decode_accelerator_win.cc on line "
              << line;
+  UMA_HISTOGRAM_SPARSE_SLOWLY("Media.DXVAVDA.ErrorLine", line);
 }
 
 base::win::ScopedComPtr<IMFSample> CreateEmptySampleWithBuffer(
