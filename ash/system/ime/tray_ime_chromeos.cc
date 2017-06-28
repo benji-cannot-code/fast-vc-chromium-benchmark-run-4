62f94276d23065c838315707c937b16d099de641
James Cook <jamescook@chromium.org>
2017-06-28 15:55:08 -0700

    chromeos: Remove IME values from the ash UserMetricsAction enum
    Ash doesn't need to use UserMetricsRecorder anymore -- I think it dates
    back to when metrics had to go through the content module. The code
    can directly call base::RecordAction() now.
    
    Bug: 678998
    Test: ash_unittests, manually switch IMEs
    Change-Id: I53ce99037972d6f580b7130b64609afbc1f9146d
    Reviewed-on: https://chromium-review.googlesource.com/553461
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483368}

--- ash/system/ime/tray_ime_chromeos.cc
+++ ash/system/ime/tray_ime_chromeos.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "ash/ime/ime_controller.h"
-#include "ash/metrics/user_metrics_recorder.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
@@ -26,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/tri_view.h"
 #include "ash/system/tray_accessibility.h"
 #include "base/logging.h"
+#include "base/metrics/user_metrics.h"
 #include "base/strings/utf_string_conversions.h"
 #include "ui/accessibility/ax_enums.h"
 #include "ui/accessibility/ax_node_data.h"
@@ -129,8 +129,7 @@ class IMEDetailedView : public ImeListView {
   }
 
   void ShowSettings() {
-    Shell::Get()->metrics()->RecordUserMetricsAction(
-        UMA_STATUS_AREA_IME_SHOW_DETAILED);
+    base::RecordAction(base::UserMetricsAction("StatusArea_IME_Detailed"));
     Shell::Get()->system_tray_controller()->ShowIMESettings();
     if (owner()->system_tray())
       owner()->system_tray()->CloseSystemBubble();
--- ash/system/ime_menu/ime_menu_tray.cc
+++ ash/system/ime_menu/ime_menu_tray.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/accessibility_delegate.h"
 #include "ash/ash_constants.h"
 #include "ash/ime/ime_controller.h"
-#include "ash/metrics/user_metrics_recorder.h"
 #include "ash/root_window_controller.h"
 #include "ash/session/session_controller.h"
 #include "ash/shelf/shelf.h"
@@ -25,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/tray_popup_utils.h"
 #include "ash/system/tray/tray_utils.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/metrics/user_metrics.h"
 #include "base/strings/utf_string_conversions.h"
 #include "components/session_manager/session_manager_types.h"
 #include "ui/base/ime/chromeos/input_method_manager.h"
@@ -58,8 +58,7 @@ gfx::Range GetImeListViewRange() {
 
 // Shows language and input settings page.
 void ShowIMESettings() {
-  Shell::Get()->metrics()->RecordUserMetricsAction(
-      UMA_STATUS_AREA_IME_SHOW_DETAILED);
+  base::RecordAction(base::UserMetricsAction("StatusArea_IME_Detailed"));
   Shell::Get()->system_tray_controller()->ShowIMESettings();
 }
 
