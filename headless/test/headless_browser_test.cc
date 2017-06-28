316550a359c06719cd272eb965f933355b530441
Rouslan Solomakhin <rouslan@chromium.org>
2017-06-28 19:00:46 +0000

    Revert "Headless: Support multiple TabSockets"
    This reverts commit c5b3efa9a0ea0b0d2e4aa28784e6d7029b7f1289.
    
    Reason for revert: the newly added test HeadlessNoDevToolsTabSocketTest is flaky on Mac.
    https://build.chromium.org/p/chromium.mac/builders/Mac10.9%20Tests%20%28dbg%29
    
    Original change's description:
    > Headless: Support multiple TabSockets
    > 
    > TabSockets are intended to either be installed into the main world or
    > a isolated world created by Page.createIsolatedWorld.  Previously we
    > only supported a single TabSocket per WebContents but that's unfortunate
    > with OOPIF.  This patch lets us support multiple TabSockets up to one
    > per world per frame.
    > 
    > The mojoms have changed a bit with
    > TabSocket::AwaitNextMessageFromEmbedder moving to 
    > HeadlessRenderFrameController::SendMessageToTabSocket which simplifies
    > recieveing a message from the embedder since a hanging IPC is no
    > longer needed.
    > 
    > Bug: 546953
    > Change-Id: I835ffcdf3bf9acd5be687074977346fb19f69d12
    > Reviewed-on: https://chromium-review.googlesource.com/543160
    > Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    > Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    > Reviewed-by: Mike West <mkwst@chromium.org>
    > Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    > Reviewed-by: Eric Seckler <eseckler@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#482990}
    
    TBR=skyostil@chromium.org,alexclarke@chromium.org,eseckler@chromium.org,jochen@chromium.org,mkwst@chromium.org
    
    Change-Id: If1c988e89b2594d88304a673c20f15888233630b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 546953
    Reviewed-on: https://chromium-review.googlesource.com/553237
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483089}

--- headless/test/headless_browser_test.cc
+++ headless/test/headless_browser_test.cc
@@ -236,7 +236,7 @@ void HeadlessAsyncDevTooledBrowserTest::RunTest() {
   HeadlessBrowserContext::Builder builder =
       browser()->CreateBrowserContextBuilder();
   builder.SetProtocolHandlers(GetProtocolHandlers());
-  if (GetAllowTabSockets()) {
+  if (GetTabSocketType() != HeadlessWebContents::Builder::TabSocketType::NONE) {
     builder.EnableUnsafeNetworkAccessWithMojoBindings(true);
     builder.AddTabSocketMojoBindings();
   }
@@ -246,7 +246,7 @@ void HeadlessAsyncDevTooledBrowserTest::RunTest() {
   browser()->GetDevToolsTarget()->AttachClient(browser_devtools_client_.get());
 
   web_contents_ = browser_context_->CreateWebContentsBuilder()
-                      .SetAllowTabSockets(GetAllowTabSockets())
+                      .SetTabSocketType(GetTabSocketType())
                       .Build();
   web_contents_->AddObserver(this);
 
@@ -266,8 +266,9 @@ ProtocolHandlerMap HeadlessAsyncDevTooledBrowserTest::GetProtocolHandlers() {
   return ProtocolHandlerMap();
 }
 
-bool HeadlessAsyncDevTooledBrowserTest::GetAllowTabSockets() {
-  return false;
+HeadlessWebContents::Builder::TabSocketType
+HeadlessAsyncDevTooledBrowserTest::GetTabSocketType() {
+  return HeadlessWebContents::Builder::TabSocketType::NONE;
 }
 
 bool HeadlessAsyncDevTooledBrowserTest::
