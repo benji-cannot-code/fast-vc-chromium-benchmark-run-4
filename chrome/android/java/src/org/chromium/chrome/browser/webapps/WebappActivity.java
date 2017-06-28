ab1c7c91114e2d39e88acfe65c947f13b310aaba
hanxi <hanxi@chromium.org>
2017-06-28 11:08:41 -0700

    Add an offline dialog when launching the WebAPK needs network connection.
    When launching WebAPK for the first time or user cleans Chrome's data, the
    WebAPK can't be properly launched when the device is offline. This CL adds a
    offline dialog to tell the user to connect to the internet if they want to use
    the WebAPK.
    Once the network connection is back, the offline dialog will disappear and the
    WebAPK loads.
    
    Records a video:
    https://drive.google.com/a/google.com/file/d/0B7zEF5GgyYmpaTRRdWFoRkNhdnc/view?usp=sharing
    Steps:
    1) launch the WebAPK when the device is offline, and a dialog is shown.
    2) turn off the air plane mode.
    Observe:
    The dialog disappears, and the url is loaded in the WebAPK.
    
    BUG=733241
    
    Review-Url: https://codereview.chromium.org/2937973002
    Cr-Commit-Position: refs/heads/master@{#483061}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappActivity.java
@@ -87,7 +87,11 @@ public class WebappActivity extends FullScreenActivity {
     public WebappActivity() {
         mWebappInfo = createWebappInfo(null);
         mDirectoryManager = new WebappDirectoryManager();
-        mSplashController = new WebappSplashScreenController();
+        mSplashController = createWebappSplashScreenController();
+    }
+
+    protected WebappSplashScreenController createWebappSplashScreenController() {
+        return new WebappSplashScreenController();
     }
 
     @Override
