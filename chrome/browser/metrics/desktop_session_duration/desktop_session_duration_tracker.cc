d93fab543d1e698b7cfc2c6b4d8c60f0afe5f404
Catherine Chung <catherinechung@google.com>
2017-06-28 15:38:54 -0700

    Added observer to DesktopSessionDurationTracker.
    Currently, the DesktopSessionDurationTracker only keeps track of session
    time for a browsing instance, not an overall Chrome session. Adding
    an observer for the class will allow the tracker to tell observers
    when a session ends.
    
    The first client for this observer is the NewTabFeatureEngagementTracker,
    which uses the signals to determine when new users have accumulated
    2 hours of usage in order to show them in-product assistance. The
    observer is tested in its unittests.
    https://chromium-review.googlesource.com/c/549057
    
    Bug: 736438
    Change-Id: Id51b48e160c25f80e9557d94ce1becfdb9dd2f2e
    Reviewed-on: https://chromium-review.googlesource.com/546540
    Commit-Queue: Catherine Chung <catherinechung@google.com>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483215}

--- chrome/browser/metrics/desktop_session_duration/desktop_session_duration_tracker.cc
+++ chrome/browser/metrics/desktop_session_duration/desktop_session_duration_tracker.cc
@@ -56,6 +56,14 @@ void DesktopSessionDurationTracker::OnVisibilityChanged(
   }
 }
 
+void DesktopSessionDurationTracker::AddObserver(Observer* observer) {
+  observer_list_.AddObserver(observer);
+}
+
+void DesktopSessionDurationTracker::RemoveObserver(Observer* observer) {
+  observer_list_.RemoveObserver(observer);
+}
+
 void DesktopSessionDurationTracker::OnUserEvent() {
   if (!is_visible_)
     return;
@@ -118,6 +126,9 @@ void DesktopSessionDurationTracker::StartSession() {
   is_first_session_ = false;
   session_start_ = base::TimeTicks::Now();
   StartTimer(inactivity_timeout_);
+
+  for (Observer& observer : observer_list_)
+    observer.OnSessionStarted(session_start_);
 }
 
 void DesktopSessionDurationTracker::EndSession(
@@ -131,6 +142,9 @@ void DesktopSessionDurationTracker::EndSession(
   delta -= time_to_discount;
   if (delta < kZeroTime)
     delta = kZeroTime;
+
+  for (Observer& observer : observer_list_)
+    observer.OnSessionEnded(delta);
 
   DVLOG(4) << "Logging session length of " << delta.InSeconds() << " seconds.";
 
