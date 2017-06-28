cbda856c85df6b8837ee89458d785c8440b65594
keishi <keishi@chromium.org>
2017-06-27 23:14:47 -0700

    Stop using WeakPtr for IntersectionObserverController which is GarbageCollected
    Right now, IntersectionObserverController maybe already scheduled for sweep when DeliverIntersectionObservations is called.
    Because WeakPtr is valid until IntersectionObserverController is actually swept.
    
    BUG=737020
    
    Review-Url: https://codereview.chromium.org/2956203002
    Cr-Commit-Position: refs/heads/master@{#482901}

--- third_party/WebKit/Source/core/dom/IntersectionObserverController.h
+++ third_party/WebKit/Source/core/dom/IntersectionObserverController.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/SuspendableObject.h"
 #include "platform/heap/Handle.h"
 #include "platform/wtf/HashSet.h"
-#include "platform/wtf/WeakPtr.h"
 
 // Design doc for IntersectionObserver implementation:
 //   https://docs.google.com/a/google.com/document/d/1hLK0eyT5_BzyNS4OkjsnoqqFQDYCbKfyBinj94OnLiQ
@@ -48,7 +47,6 @@ class IntersectionObserverController
   // IntersectionObservers for which this is the execution context of the
   // callback.
   HeapHashSet<Member<IntersectionObserver>> pending_intersection_observers_;
-  WTF::WeakPtrFactory<IntersectionObserverController> weak_ptr_factory_;
 
   bool callback_fired_while_suspended_;
 };
