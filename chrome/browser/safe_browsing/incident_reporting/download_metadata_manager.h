408c5f80a72569ffd2ebf0f0b086fd06489da7f5
Greg Thompson <grt@chromium.org>
2017-06-28 22:49:16 +0200

    Migrate DownloadMetadataManager to the new task scheduler API.
    BUG=667892
    R=fdoray@chromium.org
    
    Change-Id: I5a3863bf11d2e2cb41a59d62829c84115625f98a
    Reviewed-on: https://chromium-review.googlesource.com/552149
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: Francois Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483186}

--- chrome/browser/safe_browsing/incident_reporting/download_metadata_manager.h
+++ chrome/browser/safe_browsing/incident_reporting/download_metadata_manager.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 class SequencedTaskRunner;
-class SequencedWorkerPool;
 }
 
 namespace content {
@@ -40,15 +39,7 @@ class DownloadMetadataManager : public content::DownloadManager::Observer {
       std::unique_ptr<ClientIncidentReport_DownloadDetails>)>
       GetDownloadDetailsCallback;
 
-  // Constructs a new instance for which disk IO operations will take place in
-  // |worker_pool|.
-  explicit DownloadMetadataManager(
-      const scoped_refptr<base::SequencedWorkerPool>& worker_pool);
-
-  // Constructor that allows tests to provide a specific runner for
-  // asynchronous tasks.
-  explicit DownloadMetadataManager(
-      const scoped_refptr<base::SequencedTaskRunner>& task_runner);
+  DownloadMetadataManager();
   ~DownloadMetadataManager() override;
 
   // Adds |download_manager| to the set observed by the metadata manager.
@@ -83,11 +74,8 @@ class DownloadMetadataManager : public content::DownloadManager::Observer {
   typedef std::map<content::DownloadManager*, ManagerContext*>
       ManagerToContextMap;
 
-  // A task runner to which read tasks are posted.
-  scoped_refptr<base::SequencedTaskRunner> read_runner_;
-
-  // A task runner to which write tasks are posted.
-  scoped_refptr<base::SequencedTaskRunner> write_runner_;
+  // A task runner to which IO tasks are posted.
+  const scoped_refptr<base::SequencedTaskRunner> task_runner_;
 
   // Contexts for each DownloadManager that has been added and has not yet
   // "gone down".
--- chrome/browser/safe_browsing/incident_reporting/incident_reporting_service.cc
+++ chrome/browser/safe_browsing/incident_reporting/incident_reporting_service.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/task_scheduler/post_task.h"
 #include "base/task_scheduler/task_traits.h"
-#include "base/threading/sequenced_worker_pool.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
 #include "chrome/browser/chrome_notification_types.h"
@@ -335,7 +334,6 @@ IncidentReportingService::IncidentReportingService(
       delayed_analysis_callbacks_(
           base::TimeDelta::FromMilliseconds(kDefaultCallbackIntervalMs),
           GetBackgroundTaskRunner()),
-      download_metadata_manager_(content::BrowserThread::GetBlockingPool()),
       receiver_weak_ptr_factory_(this),
       weak_ptr_factory_(this) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
@@ -423,7 +421,6 @@ IncidentReportingService::IncidentReportingService(
                        this,
                        &IncidentReportingService::OnCollationTimeout),
       delayed_analysis_callbacks_(delayed_task_interval, delayed_task_runner),
-      download_metadata_manager_(content::BrowserThread::GetBlockingPool()),
       receiver_weak_ptr_factory_(this),
       weak_ptr_factory_(this) {
   notification_registrar_.Add(this,
