4c226687ebf88d723a1601f16ed8b50b45af4542
rdevlin.cronin <rdevlin.cronin@chromium.org>
2017-06-28 10:55:37 -0700

    [Extensions Bindings] Support chrome.extension.lastError
    For legacy reasons, we surface errors through both
    chrome.runtime.lastError and chrome.extension.lastError (though the
    latter is both less fancy [it doesn't check for access] and also not
    available in all contexts). Allow for providing a "secondary parent"
    object to the APILastError object to support this.
    
    Add unittests and end-to-end tests for the same.
    
    BUG=653596
    
    Review-Url: https://codereview.chromium.org/2953133002
    Cr-Commit-Position: refs/heads/master@{#483055}

--- extensions/renderer/bindings/api_request_handler_unittest.cc
+++ extensions/renderer/bindings/api_request_handler_unittest.cc
@@ -374,7 +374,8 @@ TEST_F(APIRequestHandlerTest, SettingLastError) {
   v8::Local<v8::Context> context = MainContext();
 
   base::Optional<std::string> logged_error;
-  auto get_parent = [](v8::Local<v8::Context> context) {
+  auto get_parent = [](v8::Local<v8::Context> context,
+                       v8::Local<v8::Object>* secondary_parent) {
     return context->Global();
   };
 
