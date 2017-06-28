923e468ce8deb1884debd414aacdf8d83cf921d2
hkamila <hkamila@google.com>
2017-06-28 12:47:02 -0700

    Added the chrome://safe-browsing/ page as part of the chrome://about
    Created the chrome://safe-browsing/ WebUI interface, and added it as
    part of the list, in the chrome://about page.
    
    Reference: I followed the steps in
    https://www.chromium.org/developers/webui to create the interface.
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2940963002
    Cr-Commit-Position: refs/heads/master@{#483104}

--- chrome/common/url_constants.cc
+++ chrome/common/url_constants.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "build/build_config.h"
 #include "chrome/common/features.h"
+#include "components/safe_browsing/web_ui/constants.h"
 #include "content/public/common/url_constants.h"
 #include "extensions/features/features.h"
 #include "media/media_features.h"
@@ -670,6 +671,7 @@ const char* const kChromeHostURLs[] = {
     kChromeUISignInInternalsHost,
     kChromeUISiteEngagementHost,
     kChromeUINTPTilesInternalsHost,
+    safe_browsing::kChromeUISafeBrowsingHost,
     kChromeUISuggestionsHost,
     kChromeUISupervisedUserInternalsHost,
     kChromeUISyncInternalsHost,
