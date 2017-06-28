a66d641e4ad9ea095c70d580611728b1d340f7f9
ashleymarie <ashleymarie@chromium.org>
2017-06-28 06:19:26 -0700

    Revert of Rerecorded browse:news:toi (patchset #2 id:20001 of https://codereview.chromium.org/2959743003/ )
    Reason for revert:
    Looks like this recording didn't fix the bug
    It's running and failing here: https://build.chromium.org/p/chromium.perf/builders/Android%20Nexus5X%20Perf/builds/227
    
    Original issue's description:
    > Rerecorded browse:news:toi
    >
    > BUG=chromium:728081
    >
    > Review-Url: https://codereview.chromium.org/2959743003
    > Cr-Commit-Position: refs/heads/master@{#482663}
    > Committed: https://chromium.googlesource.com/chromium/src/+/7c12598ed00f30cc125b9af98a7826edcbf47a39
    
    TBR=nednguyen@google.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=chromium:728081
    
    Review-Url: https://codereview.chromium.org/2961013003
    Cr-Commit-Position: refs/heads/master@{#482963}

--- tools/perf/page_sets/system_health/expectations.py
+++ tools/perf/page_sets/system_health/expectations.py
@@ -83,6 +83,8 @@ class SystemHealthMobileCommonExpectations(expectations.StoryExpectations):
                       [expectations.ALL_ANDROID], 'crbug.com/726301')
     self.DisableStory('long_running:tools:gmail-foreground',
                       [expectations.ALL_ANDROID], 'crbug.com/726301')
+    self.DisableStory('browse:news:toi', [expectations.ALL_ANDROID],
+                      'crbug.com/728081')
     self.DisableStory('browse:social:facebook_infinite_scroll',
                       [expectations.ALL], 'crbug.com/728152')
     self.DisableStory('browse:media:flickr_infinite_scroll',
@@ -117,6 +119,8 @@ class SystemHealthMobileMemoryExpectations(expectations.StoryExpectations):
                       [expectations.ALL_ANDROID], 'crbug.com/726301')
     self.DisableStory('long_running:tools:gmail-foreground',
                       [expectations.ALL_ANDROID], 'crbug.com/726301')
+    self.DisableStory('browse:news:toi', [expectations.ALL_ANDROID],
+                      'crbug.com/728081')
     self.DisableStory('browse:social:facebook_infinite_scroll',
                       [expectations.ALL], 'crbug.com/728152')
     self.DisableStory('browse:media:flickr_infinite_scroll',
@@ -196,6 +200,8 @@ class V8BrowsingMobileExpecations(expectations.StoryExpectations):
                       'crbug.com/708300')
     self.DisableStory('browse:news:globo', [expectations.ALL_ANDROID],
                       'crbug.com/714650')
+    self.DisableStory('browse:news:toi', [expectations.ALL_ANDROID],
+                      'crbug.com/728081')
     self.DisableStory('browse:social:facebook_infinite_scroll',
                       [expectations.ALL], 'crbug.com/728152')
     self.DisableStory('browse:media:flickr_infinite_scroll',
