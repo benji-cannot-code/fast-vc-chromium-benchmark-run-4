c003b111f287134d641a1b5ac6f877da0a861591
Theresa Wellington <twellington@google.com>
2017-06-28 08:43:03 -0700

    [Home] Restore active tab when opening Chrome with NTP intent
    If Chrome is opened with an intent for the NTP (e.g. via launcher
    shortcut), restore the active tab so that the correct tab is selected
    when the bottom sheet NTP UI is closed. Also disallow toolbar texture
    capture when the NTP UI is showing.
    
    BUG=736411
    
    Change-Id: If8caf4d7b939a792f238fdb80579a1f0f6184a2b
    Reviewed-on: https://chromium-review.googlesource.com/549010
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483015}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/BottomToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/BottomToolbarPhone.java
@@ -217,6 +217,11 @@ public class BottomToolbarPhone extends ToolbarPhone {
         return mBottomSheet.isSheetOpen() || super.shouldDrawShadow();
     }
 
+    @Override
+    public boolean isReadyForTextureCapture() {
+        return super.isReadyForTextureCapture() && !mBottomSheet.isShowingNewTab();
+    }
+
     /** Shows the tab switcher toolbar. */
     public void showTabSwitcherToolbar() {
         setTabSwitcherMode(true, true, false);
