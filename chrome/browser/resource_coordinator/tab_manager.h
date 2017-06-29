93c68019b064e97261fef713f5a34af8c3f4ac6f
shaseley <shaseley@google.com>
2017-06-29 15:45:49 -0700

    [Tab Metrics] Added TabManager.SessionRestore.SwitchToTab UMA.
    This is the first of two metrics recorded in the TabManager when switching to a
    different tab. If there is currently a session restore in progress and the user
    switches tabs, we emit an UMA histogram recording the tab loading state. As a
    follow-up, we'll add a separate UMA if the TabManager is in a background tab
    loading state, once the tab manager has that functionality.
    
    BUG=732837
    
    Review-Url: https://codereview.chromium.org/2962773002
    Cr-Commit-Position: refs/heads/master@{#483525}

--- chrome/browser/resource_coordinator/tab_manager.h
+++ chrome/browser/resource_coordinator/tab_manager.h
@@ -184,6 +184,7 @@ class TabManager : public TabStripModelObserver {
   FRIEND_TEST_ALL_PREFIXES(TabManagerTest, FastShutdownSingleTabProcess);
   FRIEND_TEST_ALL_PREFIXES(TabManagerTest,
                            GetUnsortedTabStatsIsInVisibleWindow);
+  FRIEND_TEST_ALL_PREFIXES(TabManagerTest, HistogramsSessionRestoreSwitchToTab);
 
   // Information about a Browser.
   struct BrowserInfo {
@@ -321,6 +322,10 @@ class TabManager : public TabStripModelObserver {
 
   void OnSessionRestoreStartedLoadingTabs();
   void OnSessionRestoreFinishedLoadingTabs();
+
+  // Records UMA histograms for the tab state when switching to a different tab
+  // during session restore.
+  void RecordSwitchToTab(content::WebContents* contents) const;
 
   // Timer to periodically update the stats of the renderers.
   base::RepeatingTimer update_timer_;
