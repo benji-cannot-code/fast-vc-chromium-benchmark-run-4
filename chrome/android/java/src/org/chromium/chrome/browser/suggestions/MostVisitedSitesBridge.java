31114c7a8cec4da6e307d618300037961cd1e3e5
Nicolas Dossou-gbete <dgn@chromium.org>
2017-06-29 17:28:23 +0000

    [Suggestions] Use standard ChromeHome feature check
    The most used way to check if ChromeHome is enabled is by calling
    FeatureUtilities#isChromeHomeEnabled, which checks nore than only
    the state of the feature flag.
    This should ensure a consistent way of checking for the state of
    the feature across Chrome
    
    Bug: 677672
    Change-Id: Iac96b7f5d05315cfe2bedf26fa4269f849acf8fd
    Reviewed-on: https://chromium-review.googlesource.com/552546
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483408}

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/MostVisitedSitesBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/MostVisitedSitesBridge.java
@@ -7,10 +7,10 @@ package org.chromium.chrome.browser.suggestions;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.annotations.JNIAdditionalImport;
-import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.ntp.NewTabPage;
 import org.chromium.chrome.browser.partnercustomizations.HomepageManager;
 import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.browser.util.FeatureUtilities;
 
 /**
  * Methods to bridge into native history to provide most recent urls, titles and thumbnails.
@@ -29,7 +29,7 @@ public class MostVisitedSitesBridge
         mNativeMostVisitedSitesBridge = nativeInit(profile);
         // The first tile replaces the home page button (only) in Chrome Home. To support that,
         // provide information about the home page.
-        if (ChromeFeatureList.isEnabled(ChromeFeatureList.CHROME_HOME)) {
+        if (FeatureUtilities.isChromeHomeEnabled()) {
             nativeSetHomePageClient(mNativeMostVisitedSitesBridge, new HomePageClient() {
                 @Override
                 public boolean isHomePageEnabled() {
