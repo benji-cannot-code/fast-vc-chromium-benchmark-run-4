a0c25eba62be492234a67e968ed38ec1e95d30c9
Takeshi Yoshino <tyoshino@chromium.org>
2017-06-29 13:20:23 +0900

    Add web platform tests about what credentials mode is used for ES6 module fetching for various configurations
    Bug: 717525
    Change-Id: I4f399ae51e8211274485e3d29dbf8f48ddf18dc4
    Reviewed-on: https://chromium-review.googlesource.com/551519
    Commit-Queue: Takeshi Yoshino <tyoshino@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483307}

---
+++ third_party/WebKit/LayoutTests/external/wpt/html/semantics/scripting-1/the-script-element/module/resources/check-cookie.py
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+def main(request, response):
+    headers = [
+        ("Content-Type", "text/javascript"),
+        ("Access-Control-Allow-Origin", request.headers.get("Origin")),
+        ("Access-Control-Allow-Credentials", "true")
+    ]
+    identifier = request.GET.first("id")
+    cookie_name = request.GET.first("cookieName")
+    cookie = request.cookies.first(cookie_name, None)
+    if identifier is None or cookie_name is None:
+        return headers, ""
+
+    if cookie is None:
+        result = "not found"
+    elif cookie.value == "1":
+        result = "found"
+    else:
+        result = "different value: " + cookie.value
+
+    return headers, "window." + identifier + " = '" + result + "';"
