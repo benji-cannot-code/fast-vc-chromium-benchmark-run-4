8b83fb9d5013ce5d9b12118176c0d5c13ec22141
thestig <thestig@chromium.org>
2017-06-28 20:06:07 -0700

    Change Linux sysroot creator scripts to use XZ compression.
    BUG=623785
    
    Review-Url: https://codereview.chromium.org/2957423003
    Cr-Commit-Position: refs/heads/master@{#483274}

--- build/linux/sysroot_scripts/sysroot-creator.sh
+++ build/linux/sysroot_scripts/sysroot-creator.sh
@@ -156,7 +156,7 @@ SanityCheck() {
 
   # This is where the staging sysroot is.
   INSTALL_ROOT="${BUILD_DIR}/${DIST}_${ARCH_LOWER}_staging"
-  TARBALL="${BUILD_DIR}/${DISTRO}_${DIST}_${ARCH_LOWER}_sysroot.tgz"
+  TARBALL="${BUILD_DIR}/${DISTRO}_${DIST}_${ARCH_LOWER}_sysroot.tar.xz"
 
   if ! mkdir -p "${INSTALL_ROOT}" ; then
     echo "ERROR: ${INSTALL_ROOT} can't be created."
@@ -179,7 +179,7 @@ ClearInstallDir() {
 
 CreateTarBall() {
   Banner "Creating tarball ${TARBALL}"
-  tar zcf ${TARBALL} -C ${INSTALL_ROOT} .
+  tar Jcf ${TARBALL} -C ${INSTALL_ROOT} .
 }
 
 ExtractPackageGz() {
