fbaa22744b8a9f81e03a08b3a3c55373e9b960b8
tzik <tzik@chromium.org>
2017-06-29 10:52:49 +0900

    Notify RendererScheduler of starting LOADING phase on DidStartProvisionalLoad
    Blink Scheduler has been considering the renderer is the loading phase
    after OnNavigate, DidCommitProvisionalLoad event. However on the renderer
    initiated navigation, OnNavigate doesn't happen and DidCommitProvisionalLoad
    happens after the main resource load, so the renderer is not in the loading
    phase while a main resource request is in-flight. That may cause a throttled
    loading task queue, and delay the main resource load.
    
    This CL adds DidStartProvisionalLoad as a trigger of the loading phase,
    so that the main resource is always loaded in an unthrottled task queue.
    
    Bug: 732738
    Change-Id: Ic04331e79eefa450da41cf2e1332583a1224880b
    Reviewed-on: https://chromium-review.googlesource.com/547377
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483295}

--- third_party/WebKit/Source/core/loader/FrameLoader.cpp
+++ third_party/WebKit/Source/core/loader/FrameLoader.cpp
@@ -1458,7 +1458,8 @@ void FrameLoader::StartLoad(FrameLoadRequest& frame_load_request,
     provisional_document_loader_->SetItemForHistoryNavigation(history_item);
   }
 
-  frame_->FrameScheduler()->DidStartProvisionalLoad();
+  DCHECK(!frame_load_request.GetResourceRequest().IsSameDocumentNavigation());
+  frame_->FrameScheduler()->DidStartProvisionalLoad(frame_->IsMainFrame());
 
   // TODO(ananta):
   // We should get rid of the dependency on the DocumentLoader in consumers of
