fd5cfb7550fb18b853e58316278d4b0385787cb4
Kevin Bailey <krb@chromium.org>
2017-06-29 06:15:36 -0700

    [omnibox] Select all when we revert all
    If you look at all the times we call RevertAll(), they either also call
    SelectAll(true), or wouldn't mind if we did. To simplify both
    RevertAll() and the calling code, we now make this call unconditionally.
    It happens to fix an issue also, where a stale cursor is produced when
    tabbing into the Omnibox.
    
    Bug: 735802
    Change-Id: Ib70f3ffd88555ef6aa600e17f111f22442e94eb0
    Reviewed-on: https://chromium-review.googlesource.com/548985
    Commit-Queue: Kevin Bailey <krb@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483362}

--- components/omnibox/browser/omnibox_edit_model.cc
+++ components/omnibox/browser/omnibox_edit_model.cc
@@ -150,7 +150,6 @@ const OmniboxEditModel::State OmniboxEditModel::GetStateForTabSwitch() {
     if (MaybePrependKeyword(display_text).empty()) {
       base::AutoReset<bool> tmp(&in_revert_, true);
       view_->RevertAll();
-      view_->SelectAll(true);
     } else {
       InternalSetUserText(display_text);
     }
@@ -371,13 +370,8 @@ void OmniboxEditModel::Revert() {
   keyword_.clear();
   is_keyword_hint_ = false;
   has_temporary_text_ = false;
-  size_t start, end;
-  view_->GetSelectionBounds(&start, &end);
-  // First home the cursor, so view of text is scrolled to left, then correct
-  // it. |SetCaretPos()| doesn't scroll the text, so doing that first wouldn't
-  // accomplish anything.
   view_->SetWindowTextAndCaretPos(permanent_text_, 0, false, true);
-  view_->SetCaretPos(std::min(permanent_text_.length(), start));
+  view_->SelectAll(true);
   client_->OnRevert();
 }
 
@@ -660,9 +654,6 @@ void OmniboxEditModel::OpenMatch(AutocompleteMatch match,
                               SEARCH_ENGINE_MAX);
   }
 
-  // Get the current text before we call RevertAll() which will clear it.
-  base::string16 current_text = view_->GetText();
-
   if (disposition != WindowOpenDisposition::NEW_BACKGROUND_TAB) {
     base::AutoReset<bool> tmp(&in_revert_, true);
     view_->RevertAll();  // Revert the box to its unedited state.
@@ -938,7 +929,6 @@ bool OmniboxEditModel::OnEscapeKeyPressed() {
   // for ease of replacement, and matches other browsers.
   bool user_input_was_in_progress = user_input_in_progress_;
   view_->RevertAll();
-  view_->SelectAll(true);
 
   // If the user was in the midst of editing, don't cancel any underlying page
   // load.  This doesn't match IE or Firefox, but seems more correct.  Note that
