0467d833f3436b6e6a60e53c27771f4cfcb5d04b
varkha <varkha@chromium.org>
2017-06-28 19:57:05 -0700

    [ui] Adds a browser_tests hook to show ZoomBubbleView
    To show the dialog use:
    browser_tests --gtest_filter=BrowserDialogTest.Invoke --interactive
     --dialog=ZoomBubbleDialogTest.InvokeDialog_default
    
    BUG=635173
    
    Review-Url: https://codereview.chromium.org/2963893002
    Cr-Commit-Position: refs/heads/master@{#483270}

--- chrome/browser/ui/views/location_bar/zoom_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/location_bar/zoom_bubble_view_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/exclusive_access/fullscreen_controller.h"
 #include "chrome/browser/ui/exclusive_access/fullscreen_controller_test.h"
+#include "chrome/browser/ui/test/test_browser_dialog.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/immersive_mode_controller.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -140,3 +141,26 @@ IN_PROC_BROWSER_TEST_F(ZoomBubbleBrowserTest, ImmersiveFullscreen) {
   }
 }
 #endif  // OS_CHROMEOS
+
+class ZoomBubbleDialogTest : public DialogBrowserTest {
+ public:
+  ZoomBubbleDialogTest() {}
+
+  // DialogBrowserTest:
+  void ShowDialog(const std::string& name) override {
+    BrowserView* browser_view = static_cast<BrowserView*>(browser()->window());
+    content::WebContents* web_contents = browser_view->GetActiveWebContents();
+    ZoomBubbleView::ShowBubble(web_contents, gfx::Point(),
+                               ZoomBubbleView::USER_GESTURE);
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(ZoomBubbleDialogTest);
+};
+
+// Test that calls ShowDialog("default"). Interactive when run via
+// browser_tests --gtest_filter=BrowserDialogTest.Invoke --interactive
+// --dialog=ZoomBubbleDialogTest.InvokeDialog_default
+IN_PROC_BROWSER_TEST_F(ZoomBubbleDialogTest, InvokeDialog_default) {
+  RunDialog();
+}
