26a42a23f102c3a53c852698eae37be39e926c9e
mahmadi <mahmadi@chromium.org>
2017-06-28 20:17:49 -0700

    [Payment Request] Simplifies VC's logic to decide if an item is selectable
    * The decision is made in the coordinator instead by return a boolean from
      -paymentRequestSelectorViewController:didSelectItemAtIndex: indicating
      whether selection can actually be made.
    * Also fixes a bug where editing an item from the selector screen, in edit
      mode, without any previosuly selected items was causing a crash.
    
    BUG=602666
    
    Review-Url: https://codereview.chromium.org/2956323002
    Cr-Commit-Position: refs/heads/master@{#483277}

--- ios/chrome/browser/ui/payments/payment_request_selector_view_controller.h
+++ ios/chrome/browser/ui/payments/payment_request_selector_view_controller.h
@@ -17,7 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @protocol PaymentRequestSelectorViewControllerDelegate<NSObject>
 
 // Notifies the delegate that the user has selected an item at the given index.
-- (void)paymentRequestSelectorViewController:
+// Returns whether the selection can actually be made.
+- (BOOL)paymentRequestSelectorViewController:
             (PaymentRequestSelectorViewController*)controller
                         didSelectItemAtIndex:(NSUInteger)index;
 
