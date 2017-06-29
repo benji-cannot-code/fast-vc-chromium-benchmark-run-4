58c6dcfd4ad69adf89a8f5dc7c6ecfb7e65c7945
Quinten Yearsley <qyearsley@google.com>
2017-06-29 20:51:17 +0000

    Import wpt@ba43a2e9a4430b5e2ea3a22b6468529b835c3fad (manual)
    Using wpt-import in Chromium ac24b8c6e0c3056bd54167d029238e28953210ff.
    
    Reason for manual import:
    There is a stuck PR for exportable commit:
    https://github.com/w3c/web-platform-tests/pull/6359
    
    Main spec directories affected by this CL:
      css/css-tables-3
      custom-elements
      html
      intersection-observer
      service-workers
      web-animations
    
    TBR=jeffcarp
    NOEXPORT=true
    
    Change-Id: I38b343c927ace3ff2ebcba11419e371e51e2350d
    Reviewed-on: https://chromium-review.googlesource.com/555835
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Commit-Queue: Quinten Yearsley <qyearsley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483481}

---
+++ third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/bytecheck-worker-imported-script.py
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import time
+
+def main(request, response):
+    headers = [('Content-Type', 'application/javascript'),
+               ('Cache-Control', 'max-age=0')]
+
+    imported_content_type = ''
+    if 'imported' in request.GET:
+        imported_content_type = request.GET['imported']
+
+    imported_content = 'default'
+    if imported_content_type == 'time':
+        imported_content = time.time()
+
+    body = '''
+    // %s
+    ''' % (imported_content)
+
+    return headers, body
