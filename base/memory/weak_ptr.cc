23026b0930eda09b1edd216da2b194c214f5af9f
hans <hans@chromium.org>
2017-06-29 11:22:36 -0700

    Make base::WeakPtr::Get() fast
    It was previously calling WeakReference::is_valid() which was an out-of-line
    method. That means code like
    
      if (my_weak_ptr)
        my_weak_ptr->foo();
    
    would do two calls to is_valid(), plus tests and branching.
    
    The is_valid() method showed up as one of the most frequently called non-inline
    functions during browser start-up on Windows (about 1M calls).
    
    is_valid() was also inefficient because it had to do a null-pointer check on
    flag_, as well as checking if the flag was marked valid.
    
    This patch removes the null-pointer check by using a sentinel object instead of
    a nullptr. And instead of storing a bool in the flag, it stores a pointer-sized
    bitmask 0 or ~0, so it can be AND'ed with the pointer value and conveniently
    setting EFLAGS so that the code above just becomes a few loads, AND, branch,
    and call. (The size of Flag is unchanged; it grows into the padding only.)
    
    This is expected to reduce the binary size by ~48KB on Android, and increase it
    by 79KB on x64 Linux -- something that's paid for by the split-out refactorings
    to WeakPtr and WeakPtrFactory.
    
    Exposing the SequenceChecker calls in inline functions caused the
    MessageLoopTestType*.Nesting test to overflow on Win64 dcheck release builds,
    which is why this patch also lowers the recursion depth there.
    
    BUG=728324
    
    Review-Url: https://codereview.chromium.org/2963623002
    Cr-Commit-Position: refs/heads/master@{#483427}

--- base/memory/weak_ptr.cc
+++ base/memory/weak_ptr.cc
@@ -5,50 +5,55 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/weak_ptr.h"
 
+#include "base/debug/leak_annotations.h"
+
 namespace base {
 namespace internal {
 
-WeakReference::Flag::Flag() : is_valid_(true) {
+static constexpr uintptr_t kTrueMask = ~static_cast<uintptr_t>(0);
+
+WeakReference::Flag::Flag() : is_valid_(kTrueMask) {
+#if DCHECK_IS_ON()
   // Flags only become bound when checked for validity, or invalidated,
   // so that we can check that later validity/invalidation operations on
   // the same Flag take place on the same sequenced thread.
   sequence_checker_.DetachFromSequence();
+#endif
 }
 
-void WeakReference::Flag::Invalidate() {
-  // The flag being invalidated with a single ref implies that there are no
-  // weak pointers in existence. Allow deletion on other thread in this case.
-  DCHECK(sequence_checker_.CalledOnValidSequence() || HasOneRef())
-      << "WeakPtrs must be invalidated on the same sequenced thread.";
-  is_valid_ = false;
+WeakReference::Flag::Flag(WeakReference::Flag::NullFlagTag) : is_valid_(false) {
+  // There is no need for sequence_checker_.DetachFromSequence() because the
+  // null flag doesn't participate in the sequence checks. See DCHECK in
+  // Invalidate() and IsValid().
+
+  // Keep the object alive perpetually, even when there are no references to it.
+  AddRef();
 }
 
-bool WeakReference::Flag::IsValid() const {
-  DCHECK(sequence_checker_.CalledOnValidSequence())
-      << "WeakPtrs must be checked on the same sequenced thread.";
-  return is_valid_;
+WeakReference::Flag* WeakReference::Flag::NullFlag() {
+  ANNOTATE_SCOPED_MEMORY_LEAK;
+  static Flag* g_null_flag = new Flag(kNullFlagTag);
+  return g_null_flag;
 }
 
-WeakReference::Flag::~Flag() {
-}
+WeakReference::Flag::~Flag() {}
 
-WeakReference::WeakReference() {
-}
-
-WeakReference::WeakReference(const Flag* flag) : flag_(flag) {
-}
+WeakReference::WeakReference() : flag_(Flag::NullFlag()) {}
 
 WeakReference::~WeakReference() {
 }
 
-WeakReference::WeakReference(WeakReference&& other) = default;
+WeakReference::WeakReference(const Flag* flag) : flag_(flag) {}
+
+WeakReference::WeakReference(WeakReference&& other)
+    : flag_(std::move(other.flag_)) {
+  other.flag_ = Flag::NullFlag();
+}
 
 WeakReference::WeakReference(const WeakReference& other) = default;
 
-bool WeakReference::is_valid() const { return flag_.get() && flag_->IsValid(); }
-
-WeakReferenceOwner::WeakReferenceOwner() {
-}
+WeakReferenceOwner::WeakReferenceOwner()
+    : flag_(WeakReference::Flag::NullFlag()) {}
 
 WeakReferenceOwner::~WeakReferenceOwner() {
   Invalidate();
@@ -63,10 +68,8 @@ WeakReference WeakReferenceOwner::GetRef() const {
 }
 
 void WeakReferenceOwner::Invalidate() {
-  if (flag_.get()) {
-    flag_->Invalidate();
-    flag_ = NULL;
-  }
+  flag_->Invalidate();
+  flag_ = WeakReference::Flag::NullFlag();
 }
 
 WeakPtrBase::WeakPtrBase() {
