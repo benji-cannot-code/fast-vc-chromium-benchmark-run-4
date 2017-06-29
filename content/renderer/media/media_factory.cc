ac9c427e1ee5fe7b6a10788dec46ad8a049a0175
Sergey Volk <servolk@google.com>
2017-06-28 17:26:41 -0700

    Include cdm_factory.h in content/renderer/media/media_factory.cc
    type, but there is only a forward declaration for it in the .h file,
    so compiling media_factory.cc fails without ENABLE_MOJO_CDM flag due
    to using incomplete type media::CdmFactory.
    
    content: :MediaFactory has cdm_factory_, which uses media::CdmFactory
    Change-Id: I19963499fcea69d233240ff8dcc47cd76dc3f266
    Reviewed-on: https://chromium-review.googlesource.com/553980
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Sergey Volk <servolk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483266}

--- content/renderer/media/media_factory.cc
+++ content/renderer/media/media_factory.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/render_frame_impl.h"
 #include "content/renderer/render_thread_impl.h"
 #include "content/renderer/render_view_impl.h"
+#include "media/base/cdm_factory.h"
 #include "media/base/decoder_factory.h"
 #include "media/base/media_switches.h"
 #include "media/base/renderer_factory_selector.h"
