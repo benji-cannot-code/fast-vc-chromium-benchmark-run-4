af28ecdd64f8ea956540117c93286bef2e211fb4
James Cook <jamescook@chromium.org>
2017-06-29 23:13:36 +0000

    chromeos: Fix crash when showing network list under --mash
    There is no UIProxyConfigService available under mash because the prefs
    service does not yet support local state prefs. Just skip the proxy
    check for now.
    
    Bug: 717645
    Test: ash_unittests --mash, also manually open menu when running chrome --mash
    Change-Id: I163b8edb71cbee477d80480abc47bb1e0337becb
    Reviewed-on: https://chromium-review.googlesource.com/556269
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483535}

--- ash/system/network/network_list.cc
+++ ash/system/network/network_list.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "ash/metrics/user_metrics_recorder.h"
+#include "ash/public/cpp/config.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
@@ -438,13 +439,15 @@ NetworkListView::UpdateNetworkListEntries() {
   const bool using_vpn =
       !!NetworkHandler::Get()->network_state_handler()->ConnectedNetworkByType(
           NetworkTypePattern::VPN());
-  // TODO(jamescook): Eliminate the null check by creating UIProxyConfigService
-  // under mash. This will require the mojo pref service to work with prefs in
-  // Local State. http://crbug.com/718072
-  const bool using_proxy = NetworkHandler::Get()->ui_proxy_config_service() &&
-                           NetworkHandler::Get()
-                               ->ui_proxy_config_service()
-                               ->HasDefaultNetworkProxyConfigured();
+  bool using_proxy = false;
+  // TODO(jamescook): Create UIProxyConfigService under mash. This will require
+  // the mojo pref service to work with prefs in Local State.
+  // http://crbug.com/718072
+  if (Shell::GetAshConfig() != Config::MASH) {
+    using_proxy = NetworkHandler::Get()
+                      ->ui_proxy_config_service()
+                      ->HasDefaultNetworkProxyConfigured();
+  }
   if (using_vpn || using_proxy) {
     if (!connection_warning_)
       connection_warning_ = CreateConnectionWarning();
