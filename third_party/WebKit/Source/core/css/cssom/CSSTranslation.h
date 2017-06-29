eb8df424959b28d00f218bf03b842c982c11bcab
meade <meade@chromium.org>
2017-06-29 04:14:10 -0700

    [CSS Typed OM] Make remaining CSSTranslation attributes mutable
    Spec: https://drafts.css-houdini.org/css-typed-om/#csstranslation
    
    BUG=545318
    
    Review-Url: https://codereview.chromium.org/2936363002
    Cr-Commit-Position: refs/heads/master@{#483321}

--- third_party/WebKit/Source/core/css/cssom/CSSTranslation.h
+++ third_party/WebKit/Source/core/css/cssom/CSSTranslation.h
@@ -6,7 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CSSTranslation_h
 #define CSSTranslation_h
 
+#include "core/css/CSSPrimitiveValue.h"
+#include "core/css/cssom/CSSStyleValue.h"
 #include "core/css/cssom/CSSTransformComponent.h"
+#include "core/css/cssom/CSSUnitValue.h"
 
 namespace blink {
 
@@ -42,14 +45,17 @@ class CORE_EXPORT CSSTranslation final : public CSSTransformComponent {
   CSSNumericValue* x() const { return x_; }
   CSSNumericValue* y() const { return y_; }
   CSSNumericValue* z() const { return z_; }
+  void setX(CSSNumericValue* x, ExceptionState&);
+  void setY(CSSNumericValue* y, ExceptionState&);
+  void setZ(CSSNumericValue* z, ExceptionState&);
 
   // Internal methods - from CSSTransformComponent.
-  TransformComponentType GetType() const override {
+  TransformComponentType GetType() const final {
     return Is2D() ? kTranslationType : kTranslation3DType;
   }
   // TODO: Implement AsMatrix for CSSTranslation.
-  DOMMatrix* AsMatrix() const override { return nullptr; }
-  CSSFunctionValue* ToCSSValue() const override;
+  DOMMatrix* AsMatrix() const final { return nullptr; }
+  CSSFunctionValue* ToCSSValue() const final;
 
   DEFINE_INLINE_VIRTUAL_TRACE() {
     visitor->Trace(x_);
