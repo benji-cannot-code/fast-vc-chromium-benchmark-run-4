7ffc47ac5a34bf5f399d535d1c59fc0faddf0ccb
hanxi <hanxi@chromium.org>
2017-06-29 16:43:49 -0700

    Update WebAPK strings.
    Update WebAPKs strings shown on the ChooseHostBrowserDialog and the
    InstallHostBrowserDialog. See (go/webapk-install-strings).
    
    BUG=737685
    
    Review-Url: https://codereview.chromium.org/2956363002
    Cr-Commit-Position: refs/heads/master@{#483548}

--- chrome/android/webapk/shell_apk/src/org/chromium/webapk/shell_apk/MainActivity.java
+++ chrome/android/webapk/shell_apk/src/org/chromium/webapk/shell_apk/MainActivity.java
@@ -21,8 +21,6 @@ import org.chromium.webapk.lib.common.WebApkConstants;
 import org.chromium.webapk.lib.common.WebApkMetaDataKeys;
 
 import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.List;
 
 /**
@@ -112,20 +110,11 @@ public class MainActivity extends Activity {
             return;
         }
 
-        String hostUrl = "";
-        try {
-            hostUrl = new URL(mStartUrl).getHost();
-        } catch (MalformedURLException e) {
-            Log.e(TAG, "Invalid URL of the WebApk.");
-            finish();
-            return;
-        }
-
         List<ResolveInfo> infos = WebApkUtils.getInstalledBrowserResolveInfos(getPackageManager());
         if (hasBrowserSupportingWebApks(infos)) {
-            showChooseHostBrowserDialog(infos, hostUrl);
+            showChooseHostBrowserDialog(infos);
         } else {
-            showInstallHostBrowserDialog(hostUrl, metadata);
+            showInstallHostBrowserDialog(metadata);
         }
     }
 
@@ -228,7 +217,7 @@ public class MainActivity extends Activity {
     }
 
     /** Shows a dialog to choose the host browser. */
-    private void showChooseHostBrowserDialog(List<ResolveInfo> infos, String hostUrl) {
+    private void showChooseHostBrowserDialog(List<ResolveInfo> infos) {
         ChooseHostBrowserDialog.DialogListener listener =
                 new ChooseHostBrowserDialog.DialogListener() {
                     @Override
@@ -243,11 +232,11 @@ public class MainActivity extends Activity {
                         finish();
                     }
                 };
-        ChooseHostBrowserDialog.show(this, listener, infos, hostUrl);
+        ChooseHostBrowserDialog.show(this, listener, infos, getString(R.string.name));
     }
 
     /** Shows a dialog to install the host browser. */
-    private void showInstallHostBrowserDialog(String hostUrl, Bundle metadata) {
+    private void showInstallHostBrowserDialog(Bundle metadata) {
         String lastResortHostBrowserPackageName =
                 metadata.getString(WebApkMetaDataKeys.RUNTIME_HOST);
         String lastResortHostBrowserApplicationName =
@@ -274,7 +263,8 @@ public class MainActivity extends Activity {
                     }
                 };
 
-        InstallHostBrowserDialog.show(this, listener, hostUrl, lastResortHostBrowserPackageName,
-                lastResortHostBrowserApplicationName, R.drawable.last_resort_runtime_host_logo);
+        InstallHostBrowserDialog.show(this, listener, getString(R.string.name),
+                lastResortHostBrowserPackageName, lastResortHostBrowserApplicationName,
+                R.drawable.last_resort_runtime_host_logo);
     }
 }
