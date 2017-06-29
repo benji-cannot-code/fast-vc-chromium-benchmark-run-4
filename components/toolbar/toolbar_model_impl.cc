74fdd21bf5cd9d9c1273658e2232cbd3cc12bfbc
cjgrant <cjgrant@chromium.org>
2017-06-29 15:39:30 -0700

    Toolbar: Do not include vector icons if on Android with no VR.
    A previous change rolled components/toolbar into Android for use with
    VR. But, to respect svelte devices, we should probably continue to not
    include these icons if not needed for VR.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2964513002
    Cr-Commit-Position: refs/heads/master@{#483523}

--- components/toolbar/toolbar_model_impl.cc
+++ components/toolbar/toolbar_model_impl.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/security_state/core/security_state.h"
 #include "components/strings/grit/components_strings.h"
+#include "components/toolbar/features.h"
 #include "components/toolbar/toolbar_model_delegate.h"
 #include "components/url_formatter/elide_url.h"
 #include "components/url_formatter/url_formatter.h"
@@ -22,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/text_elider.h"
 #include "ui/gfx/vector_icon_types.h"
 
-#if !defined(OS_IOS)
+#if (!defined(OS_ANDROID) || BUILDFLAG(ENABLE_VR)) && !defined(OS_IOS)
 #include "components/toolbar/vector_icons.h"  // nogncheck
 #include "ui/vector_icons/vector_icons.h"     // nogncheck
 #endif
@@ -74,7 +75,7 @@ security_state::SecurityLevel ToolbarModelImpl::GetSecurityLevel(
 }
 
 const gfx::VectorIcon& ToolbarModelImpl::GetVectorIcon() const {
-#if !defined(OS_IOS)
+#if (!defined(OS_ANDROID) || BUILDFLAG(ENABLE_VR)) && !defined(OS_IOS)
   auto* const icon_override = delegate_->GetVectorIconOverride();
   if (icon_override)
     return *icon_override;
