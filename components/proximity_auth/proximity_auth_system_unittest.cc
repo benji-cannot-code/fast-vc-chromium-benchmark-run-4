1a400b6e09457a80721bbc6513b1b826c7fdfc51
tengs <tengs@chromium.org>
2017-06-29 18:47:22 -0700

    [EasyUnlock] Disable password reauth until specifications are defined.
    We may not need this feature if there is no enterprise demand.
    
    Review-Url: https://codereview.chromium.org/2953163002
    Cr-Commit-Position: refs/heads/master@{#483586}

--- components/proximity_auth/proximity_auth_system_unittest.cc
+++ components/proximity_auth/proximity_auth_system_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/proximity_auth/proximity_auth_system.h"
 
+#include "base/command_line.h"
 #include "base/test/simple_test_clock.h"
 #include "base/test/test_simple_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
@@ -14,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/proximity_auth/logging/logging.h"
 #include "components/proximity_auth/mock_proximity_auth_client.h"
 #include "components/proximity_auth/proximity_auth_pref_manager.h"
+#include "components/proximity_auth/switches.h"
 #include "components/proximity_auth/unlock_manager.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -428,6 +430,8 @@ TEST_F(ProximityAuthSystemTest, Suspend_RegisteredUserFocused) {
 }
 
 TEST_F(ProximityAuthSystemTest, ForcePasswordReauth) {
+  base::CommandLine::ForCurrentProcess()->AppendSwitch(
+      proximity_auth::switches::kEnableForcePasswordReauth);
   ON_CALL(*pref_manager_, GetLastPasswordEntryTimestampMs())
       .WillByDefault(Return(kTimestampAfterReauthMs));
   EXPECT_CALL(proximity_auth_client_,
