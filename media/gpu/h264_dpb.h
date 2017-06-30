aaf04076785e42715e95ec8c132bb213b3837c91
owenlin <owenlin@chromium.org>
2017-06-29 20:41:17 -0700

    h264_decoder_unittests: Initial Change.
    BUG=b/35934865
    TEST=Run the test on CrOS devices.
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2760513002
    Cr-Commit-Position: refs/heads/master@{#483623}

--- media/gpu/h264_dpb.h
+++ media/gpu/h264_dpb.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "media/filters/h264_parser.h"
+#include "media/gpu/media_gpu_export.h"
 #include "ui/gfx/geometry/rect.h"
 
 namespace media {
@@ -25,7 +26,7 @@ class VaapiH264Picture;
 
 // A picture (a frame or a field) in the H.264 spec sense.
 // See spec at http://www.itu.int/rec/T-REC-H.264
-class H264Picture : public base::RefCounted<H264Picture> {
+class MEDIA_GPU_EXPORT H264Picture : public base::RefCounted<H264Picture> {
  public:
   using Vector = std::vector<scoped_refptr<H264Picture>>;
 
