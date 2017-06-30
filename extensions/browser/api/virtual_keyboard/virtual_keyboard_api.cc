a55422128aa2ddb96f9cda1d2aa2621d929c589a
azurewei <azurewei@chromium.org>
2017-06-30 08:04:18 -0700

    Hide handwriting and voice buttons when keyboard is in restricted state.
    When the virtual keyboard is in restricted state, some advanced features
    like handwriting and voice are disabled. We should also hide the
    handwriting and voice buttons on the opt-in IME menu.
    
    Add InputMethodManager::SetImeMenuFeatureEnabled() method to enable or
    disable of each feature.
    ImeMenuTray use InputMethodManager::GetImeMenuFeatureEnabled() to
    make decision whether to show each button when showing tray view.
    
    BUG=710242
    TEST=Unit test
    
    Review-Url: https://codereview.chromium.org/2953033002
    Cr-Commit-Position: refs/heads/master@{#483706}

--- extensions/browser/api/virtual_keyboard/virtual_keyboard_api.cc
+++ extensions/browser/api/virtual_keyboard/virtual_keyboard_api.cc
@@ -11,6 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/api/virtual_keyboard_private/virtual_keyboard_private_api.h"
 #include "extensions/common/api/virtual_keyboard.h"
 
+#if defined(OS_CHROMEOS)
+#include "ui/base/ime/chromeos/input_method_manager.h"
+using chromeos::input_method::InputMethodManager;
+#endif
+
 namespace extensions {
 
 VirtualKeyboardRestrictFeaturesFunction::
@@ -33,6 +38,18 @@ VirtualKeyboardRestrictFeaturesFunction::Run() {
   VirtualKeyboardAPI* api =
       BrowserContextKeyedAPIFactory<VirtualKeyboardAPI>::Get(browser_context());
   api->delegate()->SetKeyboardRestricted(!features_enabled);
+
+#if defined(OS_CHROMEOS)
+  InputMethodManager* input_method_manager = InputMethodManager::Get();
+  if (input_method_manager) {
+    input_method_manager->SetImeMenuFeatureEnabled(
+        InputMethodManager::FEATURE_VOICE,
+        params->restrictions.voice_input_enabled);
+    input_method_manager->SetImeMenuFeatureEnabled(
+        InputMethodManager::FEATURE_HANDWRITING,
+        params->restrictions.handwriting_enabled);
+  }
+#endif
   return RespondNow(NoArguments());
 }
 
