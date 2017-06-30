fe85fd5489f972e9c9b5a456a28986975d90b2ca
thestig <thestig@chromium.org>
2017-06-30 15:29:41 -0700

    Move more printing code off of the FILE thread.
    Do some trivial cleanups while we are here.
    
    BUG=689520
    
    Review-Url: https://codereview.chromium.org/2965803002
    Cr-Commit-Position: refs/heads/master@{#483845}

--- chrome/browser/printing/printer_manager_dialog_linux.cc
+++ chrome/browser/printing/printer_manager_dialog_linux.cc
@@ -10,15 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/environment.h"
 #include "base/files/file_util.h"
-#include "base/message_loop/message_loop.h"
 #include "base/nix/xdg_util.h"
 #include "base/process/kill.h"
 #include "base/process/launch.h"
-#include "base/strings/string_split.h"
-#include "content/public/browser/browser_thread.h"
-
-using base::Environment;
-using content::BrowserThread;
+#include "base/task_scheduler/post_task.h"
+#include "base/threading/thread_restrictions.h"
 
 namespace {
 
@@ -50,8 +46,8 @@ bool OpenPrinterConfigDialog(const char* const* command) {
 // Detect the command based on the deskop environment and open the printer
 // manager dialog.
 void DetectAndOpenPrinterConfigDialog() {
-  DCHECK_CURRENTLY_ON(BrowserThread::FILE);
-  std::unique_ptr<Environment> env(Environment::Create());
+  base::ThreadRestrictions::AssertIOAllowed();
+  std::unique_ptr<base::Environment> env(base::Environment::Create());
 
   bool opened = false;
   switch (base::nix::GetDesktopEnvironment(env.get())) {
@@ -74,13 +70,14 @@ void DetectAndOpenPrinterConfigDialog() {
   LOG_IF(ERROR, !opened) << "Failed to open printer manager dialog ";
 }
 
-}  // anonymous namespace
+}  // namespace
 
 namespace printing {
 
 void PrinterManagerDialog::ShowPrinterManagerDialog() {
-  BrowserThread::PostTask(BrowserThread::FILE, FROM_HERE,
-                          base::BindOnce(&DetectAndOpenPrinterConfigDialog));
+  base::PostTaskWithTraits(
+      FROM_HERE, {base::MayBlock(), base::TaskPriority::USER_BLOCKING},
+      base::BindOnce(&DetectAndOpenPrinterConfigDialog));
 }
 
 }  // namespace printing
