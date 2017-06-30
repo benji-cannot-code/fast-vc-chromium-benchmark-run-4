ac4275fd784786e3da4916b6a69b43db9112a628
Angela S <angelashao@google.com>
2017-06-30 03:33:47 +0000

    chrome/android: Add screenshot detector for download in-product help.
    We need to detect screenshots that are taken when Chrome is in the
    foreground in order to trigger the download page in-product help in
    ChromeTabbedActivity. This patch creates a ScreenshotMonitor class to
    do that.
    
    The SCREENSHOT_TAKEN_CHROME_IN_FOREGROUND event is tracked when a
    screenshot is detected.
    
    The DOWNLOAD_PAGE_SCREENSHOT_FEATURE feature has the same IPH UI as 
    DOWNLOAD_PAGE_FEATURE but is tied to the 
    SCREENSHOT_TAKEN_CHROME_IN_FOREGROUND event so we can better control
    the event (screenshot or offline) that triggers the download page IPH.
    
    BUG=723011
    
    Change-Id: I7828686bbd34aad7a3081df70d78c910284ae19b
    Reviewed-on: https://chromium-review.googlesource.com/531895
    Commit-Queue: Angela Shao <angelashao@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483618}

--- components/feature_engagement_tracker/public/feature_list.cc
+++ components/feature_engagement_tracker/public/feature_list.cc
@@ -20,6 +20,7 @@ const base::Feature* kAllFeatures[] = {
     &kIPHDataSaverPreviewFeature,
     &kIPHDownloadHomeFeature,
     &kIPHDownloadPageFeature,
+    &kIPHDownloadPageScreenshotFeature,
 #endif  // defined(OS_ANDROID)
 #if defined(OS_WIN) || defined(OS_LINUX)
     &kIPHIncognitoWindowFeature,
