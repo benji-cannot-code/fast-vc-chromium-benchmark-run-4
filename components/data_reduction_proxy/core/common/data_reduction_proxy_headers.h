212bef78b908dcfe4c2efbafd5646c00ab633ad7
dougarnett <dougarnett@chromium.org>
2017-06-30 12:07:21 -0700

    Adds Server Previews protocol UMA plus UMA for not accepting previews
    BUG=731895
    
    patch from issue 2945423002 at patchset 80001 (http://crrev.com/2945423002#ps80001)
    
    Review-Url: https://codereview.chromium.org/2956703002
    Cr-Commit-Position: refs/heads/master@{#483782}

--- components/data_reduction_proxy/core/common/data_reduction_proxy_headers.h
+++ components/data_reduction_proxy/core/common/data_reduction_proxy_headers.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/strings/string_piece.h"
 #include "base/time/time.h"
+#include "net/http/http_request_headers.h"
+#include "net/http/http_response_headers.h"
 #include "net/proxy/proxy_service.h"
 
 class GURL;
@@ -21,6 +23,15 @@ class HttpResponseHeaders;
 }  // namespace net
 
 namespace data_reduction_proxy {
+
+// Transform directives that may be parsed out of http headers.
+enum TransformDirective {
+  TRANSFORM_NONE,
+  TRANSFORM_LITE_PAGE,
+  TRANSFORM_EMPTY_IMAGE,
+  TRANSFORM_COMPRESSED_VIDEO,
+  TRANSFORM_PAGE_POLICIES_EMPTY_IMAGE,
+};
 
 // Values of the UMA DataReductionProxy.BypassType{Primary|Fallback} and
 // DataReductionProxy.BlockType{Primary|Fallback} histograms. This enum must
@@ -117,6 +128,18 @@ const char* if_heavy_qualifier();
 // Returns true if the Chrome-Proxy-Content-Transform response header indicates
 // that an empty image has been provided.
 bool IsEmptyImagePreview(const net::HttpResponseHeaders& headers);
+
+// Retrieves the accepted transform type, if any, from |headers|.
+TransformDirective ParseRequestTransform(
+    const net::HttpRequestHeaders& headers);
+
+// Retrieves the transform directive (whether applied or a page policy), if any,
+// from |headers|.
+// Note if the response headers contains both an applied content transform and
+// a page policies directive, only the applied content transform type will
+// be returned.
+TransformDirective ParseResponseTransform(
+    const net::HttpResponseHeaders& headers);
 
 // Returns true if the provided value of the Chrome-Proxy-Content-Transform
 // response header that is provided in |content_transform_value| indicates that
