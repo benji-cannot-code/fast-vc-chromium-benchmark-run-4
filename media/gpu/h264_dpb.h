e9fb46f6bed9f685c5b97b308fb244109f8d885b
timloh <timloh@chromium.org>
2017-06-29 22:25:23 -0700

    Revert of h264_decoder_unittests: Initial Change. (patchset #11 id:240001 of https://codereview.chromium.org/2760513002/ )
    Reason for revert:
    Broke some bots:
    
    "CommandFailedError: Failed to list tests on any device"
    https://build.chromium.org/p/chromium.linux/builders/Android%20Tests
    https://build.chromium.org/p/chromium.android/builders/Android%20N5X%20Swarm%20Builder
    
    Added tests all fail:
    https://build.chromium.org/p/chromium.memory/builders/Linux%20MSan%20Tests
    
    Original issue's description:
    > h264_decoder_unittests: Initial Change.
    >
    > BUG=b/35934865
    > TEST=Run the test on CrOS devices.
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    >
    > Review-Url: https://codereview.chromium.org/2760513002
    > Cr-Commit-Position: refs/heads/master@{#483623}
    > Committed: https://chromium.googlesource.com/chromium/src/+/0fc85fd05e9607d8d1ca242b831abee0e4f4c318
    
    TBR=dalecurtis@chromium.org,johnylin@chromium.org,kcwu@chromium.org,posciak@chromium.org,owenlin@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=b/35934865
    
    Review-Url: https://codereview.chromium.org/2964193002
    Cr-Commit-Position: refs/heads/master@{#483639}

--- media/gpu/h264_dpb.h
+++ media/gpu/h264_dpb.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "media/filters/h264_parser.h"
-#include "media/gpu/media_gpu_export.h"
 #include "ui/gfx/geometry/rect.h"
 
 namespace media {
@@ -26,7 +25,7 @@ class VaapiH264Picture;
 
 // A picture (a frame or a field) in the H.264 spec sense.
 // See spec at http://www.itu.int/rec/T-REC-H.264
-class MEDIA_GPU_EXPORT H264Picture : public base::RefCounted<H264Picture> {
+class H264Picture : public base::RefCounted<H264Picture> {
  public:
   using Vector = std::vector<scoped_refptr<H264Picture>>;
 
