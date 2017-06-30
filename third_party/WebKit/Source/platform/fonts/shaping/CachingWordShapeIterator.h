0f0084c012e87f950276ff553b90be360f259f9f
kojii <kojii@chromium.org>
2017-06-29 18:11:58 -0700

    Add comments when letter-/word-spacing is disabled
    From review comment in:
    https://codereview.chromium.org/2924263003/#msg24
    
    TBR=eae@chromium.org
    
    Review-Url: https://codereview.chromium.org/2967493003
    Cr-Commit-Position: refs/heads/master@{#483572}

--- third_party/WebKit/Source/platform/fonts/shaping/CachingWordShapeIterator.h
+++ third_party/WebKit/Source/platform/fonts/shaping/CachingWordShapeIterator.h
@@ -58,6 +58,7 @@ class PLATFORM_EXPORT CachingWordShapeIterator final {
     // fall back on shaping the entire run.
     shape_by_word_ = font_->CanShapeWordByWord();
 
+    // SVG sets SpacingDisabled because it handles spacing by themselves.
     if (!run.SpacingDisabled())
       spacing_.SetSpacingAndExpansion(font->GetFontDescription());
   }
