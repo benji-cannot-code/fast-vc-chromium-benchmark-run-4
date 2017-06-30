bc815633046a11c31d4945a5853aac19504af80e
eroman <eroman@chromium.org>
2017-06-29 17:02:37 -0700

    Check the return value of base::StringToInt() in ProxyConfigServiceLinux.
    The code was assuming it would set the result to 0 on failure, however
    this is not part of the API contract (the actual contract is weirder).
    
    This wouldn't have caused any real bugs, other than some
    different interpretations of invalid KDE proxy configurations.
    
    The CL changes some of those edge cases in arbitrary ways (i.e. overflowed integer is now 0 rather than max_int).
    
    BUG=596573
    
    Review-Url: https://codereview.chromium.org/2968573002
    Cr-Commit-Position: refs/heads/master@{#483553}

--- net/proxy/proxy_config_service_linux_unittest.cc
+++ net/proxy/proxy_config_service_linux_unittest.cc
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
 
+// TODO(eroman): Convert these to parameterized tests using TEST_P().
+
 namespace net {
 namespace {
 
@@ -1158,6 +1160,33 @@ TEST_F(ProxyConfigServiceLinuxTest, KDEConfigParser) {
           GURL(),  // pac_url
           ProxyRulesExpectation::Empty(),
       },
+      {
+          TEST_DESC("Invalid proxy type (ProxyType=-3)"),
+
+          // Input.
+          "[Proxy Settings]\nProxyType=-3\n",
+          {},  // env_values
+
+          // Expected result.
+          ProxyConfigService::CONFIG_VALID,
+          false,   // auto_detect
+          GURL(),  // pac_url
+          ProxyRulesExpectation::Empty(),
+      },
+
+      {
+          TEST_DESC("Invalid proxy type (ProxyType=AB-)"),
+
+          // Input.
+          "[Proxy Settings]\nProxyType=AB-\n",
+          {},  // env_values
+
+          // Expected result.
+          ProxyConfigService::CONFIG_VALID,
+          false,   // auto_detect
+          GURL(),  // pac_url
+          ProxyRulesExpectation::Empty(),
+      },
 
       {
           TEST_DESC("Auto detect"),
@@ -1314,7 +1343,7 @@ TEST_F(ProxyConfigServiceLinuxTest, KDEConfigParser) {
       },
 
       {
-          TEST_DESC("Correctly parse bypass list with ReversedException"),
+          TEST_DESC("Correctly parse bypass list with ReversedException=true"),
 
           // Input.
           "[Proxy Settings]\nProxyType=1\nhttpProxy=www.google.com\n"
@@ -1330,6 +1359,79 @@ TEST_F(ProxyConfigServiceLinuxTest, KDEConfigParser) {
               "",                   // https
               "",                   // ftp
               "*.google.com"),      // bypass rules
+      },
+
+      {
+          TEST_DESC("Correctly parse bypass list with ReversedException=false"),
+
+          // Input.
+          "[Proxy Settings]\nProxyType=1\nhttpProxy=www.google.com\n"
+          "NoProxyFor=.google.com\nReversedException=false\n",
+          {},  // env_values
+
+          // Expected result.
+          ProxyConfigService::CONFIG_VALID,
+          false,                                                 // auto_detect
+          GURL(),                                                // pac_url
+          ProxyRulesExpectation::PerScheme("www.google.com:80",  // http
+                                           "",                   // https
+                                           "",                   // ftp
+                                           "*.google.com"),      // bypass rules
+      },
+
+      {
+          TEST_DESC("Correctly parse bypass list with ReversedException=1"),
+
+          // Input.
+          "[Proxy Settings]\nProxyType=1\nhttpProxy=www.google.com\n"
+          "NoProxyFor=.google.com\nReversedException=1\n",
+          {},  // env_values
+
+          // Expected result.
+          ProxyConfigService::CONFIG_VALID,
+          false,   // auto_detect
+          GURL(),  // pac_url
+          ProxyRulesExpectation::PerSchemeWithBypassReversed(
+              "www.google.com:80",  // http
+              "",                   // https
+              "",                   // ftp
+              "*.google.com"),      // bypass rules
+      },
+
+      {
+          TEST_DESC("Overflow: ReversedException=18446744073709551617"),
+
+          // Input.
+          "[Proxy Settings]\nProxyType=1\nhttpProxy=www.google.com\n"
+          "NoProxyFor=.google.com\nReversedException=18446744073709551617\n",
+          {},  // env_values
+
+          // Expected result.
+          ProxyConfigService::CONFIG_VALID,
+          false,                                                 // auto_detect
+          GURL(),                                                // pac_url
+          ProxyRulesExpectation::PerScheme("www.google.com:80",  // http
+                                           "",                   // https
+                                           "",                   // ftp
+                                           "*.google.com"),      // bypass rules
+      },
+
+      {
+          TEST_DESC("Not a number: ReversedException=noitpecxE"),
+
+          // Input.
+          "[Proxy Settings]\nProxyType=1\nhttpProxy=www.google.com\n"
+          "NoProxyFor=.google.com\nReversedException=noitpecxE\n",
+          {},  // env_values
+
+          // Expected result.
+          ProxyConfigService::CONFIG_VALID,
+          false,                                                 // auto_detect
+          GURL(),                                                // pac_url
+          ProxyRulesExpectation::PerScheme("www.google.com:80",  // http
+                                           "",                   // https
+                                           "",                   // ftp
+                                           "*.google.com"),      // bypass rules
       },
 
       {
