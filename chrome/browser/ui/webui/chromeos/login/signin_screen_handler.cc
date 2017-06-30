38b1e4ae2d90150855aad0853319cc12c8f0db46
tbarzic <tbarzic@chromium.org>
2017-06-30 16:14:51 -0700

    Have signin screen handler always observe lock screen apps state
    Previously, signin screen handler would observe lock screen apps
    state only if it was created while screen lock was active - this does
    not work well with lock screen preload, as the lock screen UI might get
    created while screen locker is unset. This cl switches the check for
    screen lock existence to the lock screen apps observer implementation.
    State changes are forwarded to the UI only in lock screen UI.
    
    BUG=716081
    
    Review-Url: https://codereview.chromium.org/2955233003
    Cr-Commit-Position: refs/heads/master@{#483857}

--- chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
@@ -285,10 +285,8 @@ SigninScreenHandler::SigninScreenHandler(
   ash::mojom::TouchViewObserverPtr observer;
   touch_view_binding_.Bind(mojo::MakeRequest(&observer));
   touch_view_manager_ptr_->AddObserver(std::move(observer));
-  if (ScreenLocker::default_screen_locker() &&
-      lock_screen_apps::StateController::IsEnabled()) {
+  if (lock_screen_apps::StateController::IsEnabled())
     lock_screen_apps_observer_.Add(lock_screen_apps::StateController::Get());
-  }
   if (WallpaperManager::HasInstance())
     WallpaperManager::Get()->AddObserver(this);
 }
@@ -1089,6 +1087,9 @@ void SigninScreenHandler::OnTouchViewToggled(bool enabled) {
 
 void SigninScreenHandler::OnLockScreenNoteStateChanged(
     ash::mojom::TrayActionState state) {
+  if (!ScreenLocker::default_screen_locker())
+    return;
+
   std::string lock_screen_apps_state;
   switch (state) {
     case ash::mojom::TrayActionState::kActive:
@@ -1287,8 +1288,7 @@ void SigninScreenHandler::HandleAccountPickerReady() {
 
   is_account_picker_showing_first_time_ = true;
 
-  if (ScreenLocker::default_screen_locker() &&
-      lock_screen_apps::StateController::IsEnabled()) {
+  if (lock_screen_apps::StateController::IsEnabled()) {
     OnLockScreenNoteStateChanged(
         lock_screen_apps::StateController::Get()->GetLockScreenNoteState());
   }
