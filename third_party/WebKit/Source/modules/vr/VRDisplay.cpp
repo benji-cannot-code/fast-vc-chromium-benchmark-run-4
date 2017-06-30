187aec8dceb6e23f4d899692f127739d8ce1cdb6
Michael Thiessen <mthiesse@chromium.org>
2017-06-30 21:18:44 +0000

    VR: Delay setting present result until DON flow completes.
    This CL does two main things:
    1. It delays telling the webVR page the presentation result until
    the DON flow is completed.
    2. It keeps Chrome in VR when we're going through the DON flow for
    both sanity and smoothness reasons (we now resume ~instantly after
    the DON flow).
    
    This CL inadvertently exposed a focus issue around presentation,
    which is fixed by making sure the VrDisplay isn't blurred due to
    focus while presenting. It also drive-by fixes some possible races
    in starting/ending presentation, making sure bindings are closed
    appropriately.
    
    Bug: 737259, 737656
    Change-Id: I97892617a75a597effd70dbadbf53547a2578825
    Reviewed-on: https://chromium-review.googlesource.com/553558
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483815}

--- third_party/WebKit/Source/modules/vr/VRDisplay.cpp
+++ third_party/WebKit/Source/modules/vr/VRDisplay.cpp
@@ -184,9 +184,7 @@ void VRDisplay::RequestVSync() {
         doc->RequestAnimationFrame(new VRDisplayFrameRequestCallback(this));
     return;
   }
-
-  if (!pending_vrdisplay_raf_)
-    return;
+  DCHECK(vr_presentation_provider_.is_bound());
 
   // The logic here is a bit subtle. We get called from one of the following
   // four contexts:
@@ -496,7 +494,11 @@ void VRDisplay::BeginPresent() {
     Platform::Current()->RecordRapporURL("VR.WebVR.PresentSuccess",
                                          WebURL(doc->Url()));
   }
-
+  if (!FocusedOrPresenting() && display_blurred_) {
+    // Presentation doesn't care about focus, so if we're blurred because of
+    // focus, then unblur.
+    OnFocus();
+  }
   is_presenting_ = true;
   // Call RequestVSync to switch from the (internal) document rAF to the
   // VrPresentationProvider VSync.
@@ -966,11 +968,9 @@ bool VRDisplay::HasPendingActivity() const {
 
 void VRDisplay::FocusChanged() {
   DVLOG(1) << __FUNCTION__;
-  if (is_presenting_)
-    return;
   if (navigator_vr_->IsFocused()) {
     OnFocus();
-  } else {
+  } else if (!is_presenting_) {
     OnBlur();
   }
 }
