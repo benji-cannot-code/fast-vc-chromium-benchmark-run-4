306b8b8cbac0bc052e3e9a2842784157d2d1d5fe
Dale Curtis <dalecurtis@chromium.org>
2017-06-30 01:01:54 +0000

    Switch to intrinsics for vector_math primitives in clang on Linux.
    The recent enablement of ThinLTO on Linux has broken the
    auto vectorizer in clang, so switch back to using our intrinsics
    based version for now.
    
    BUG=738085
    TEST=none
    
    Change-Id: I9f4b182adf20c090e4933bbf671de3b1b5043147
    Reviewed-on: https://chromium-review.googlesource.com/557059
    Reviewed-by: Peter Collingbourne <pcc@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483567}

--- media/base/vector_math.cc
+++ media/base/vector_math.cc
@@ -16,7 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <xmmintrin.h>
 // Don't use custom SSE versions where the auto-vectorized C version performs
 // better, which is anywhere clang is used.
-#if !defined(__clang__)
+// TODO(pcc): Linux currently uses ThinLTO which has broken auto-vectorization
+// in clang, so use our intrinsic version for now. http://crbug.com/738085
+#if !defined(__clang__) || defined(OS_LINUX)
 #define FMAC_FUNC FMAC_SSE
 #define FMUL_FUNC FMUL_SSE
 #else
