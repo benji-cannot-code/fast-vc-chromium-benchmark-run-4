d53b3af60f8f050afde96faa044b636d44cff5a2
Robert Flack <flackr@chromium.org>
2017-06-30 16:42:01 +0000

    Update google_login helper and gmail recordings.
    Support old and new google login fields. Update the gmail recordings used for
    smoothness metrics. Removes the old custom login behavior in GmailSmoothPage.
    
    Bug: 590766,663155
    Change-Id: I7c6d22b48e7cb16501dc393564d7e4df097490db
    Reviewed-on: https://chromium-review.googlesource.com/546615
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483732}

--- tools/perf/page_sets/top_pages.py
+++ tools/perf/page_sets/top_pages.py
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from telemetry.page import page as page_module
 from telemetry.page import shared_page_state
 
+from page_sets.login_helpers import google_login
+
 
 class TopPages(page_module.Page):
 
@@ -57,10 +59,11 @@ class GmailPage(TopPages):
     super(GmailPage, self).__init__(
         url='https://mail.google.com/mail/',
         page_set=page_set,
-        credentials='google',
         shared_page_state_class=shared_page_state_class)
 
   def RunNavigateSteps(self, action_runner):
+    google_login.LoginGoogleAccount(action_runner, 'googletest',
+                                    self.credentials_path)
     super(GmailPage, self).RunNavigateSteps(action_runner)
     action_runner.WaitForJavaScriptCondition(
         'window.gmonkey !== undefined &&'
