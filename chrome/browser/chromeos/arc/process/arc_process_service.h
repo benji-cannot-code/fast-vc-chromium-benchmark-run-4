3a9ed143ddfbe6fea326ad62dc985db0d4285387
Francois Doray <fdoray@chromium.org>
2017-06-30 19:11:25 +0000

    Pass vector of ArcProcesses by value to RequestProcessListCallback.
    Pass the vector of ArcProcesses by value rather than by const& to
    the RequestProcessListCallback. The vector is moved from the source
    to the callback, so this CL does not incur additional copies
    (as proved by the DISALLOW_COPY_AND_ASSIGN in the definition of
    ArcProcess).
    
    This change allows the RequestProcessListCallback to move the
    vector it receives to another variable.
    
    TBR=chrisha@chromium.org
    
    Bug: 731145
    Change-Id: If91b4107c3b723de38ba80d281f19dafcdada54b
    Reviewed-on: https://chromium-review.googlesource.com/557909
    Commit-Queue: Francois Doray <fdoray@chromium.org>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483784}

--- chrome/browser/chromeos/arc/process/arc_process_service.h
+++ chrome/browser/chromeos/arc/process/arc_process_service.h
@@ -51,7 +51,7 @@ class ArcProcessService
       public InstanceHolder<mojom::ProcessInstance>::Observer {
  public:
   using RequestProcessListCallback =
-      base::Callback<void(const std::vector<ArcProcess>&)>;
+      base::Callback<void(std::vector<ArcProcess>)>;
 
   explicit ArcProcessService(ArcBridgeService* bridge_service);
   ~ArcProcessService() override;
--- chrome/browser/resource_coordinator/tab_manager_delegate_chromeos_unittest.cc
+++ chrome/browser/resource_coordinator/tab_manager_delegate_chromeos_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 #include <string>
+#include <utility>
 #include <vector>
 
 #include "base/process/process_handle.h"
@@ -249,7 +250,8 @@ TEST_F(TabManagerDelegateTest, SetOomScoreAdj) {
   // tab1                pid: 11
   // tab5                pid: 12
   // tab2                pid: 11
-  tab_manager_delegate.AdjustOomPrioritiesImpl(tab_list, arc_processes);
+  tab_manager_delegate.AdjustOomPrioritiesImpl(tab_list,
+                                               std::move(arc_processes));
   auto& oom_score_map = tab_manager_delegate.oom_score_map_;
 
   // 6 PIDs for apps + 2 PIDs for tabs.
@@ -336,7 +338,7 @@ TEST_F(TabManagerDelegateTest, DoNotKillRecentlyKilledArcProcesses) {
   memory_stat->SetTargetMemoryToFreeKB(250000);
   memory_stat->SetProcessPss(30, 10000);
   TabStatsList tab_list;
-  tab_manager_delegate.LowMemoryKillImpl(tab_list, arc_processes);
+  tab_manager_delegate.LowMemoryKillImpl(tab_list, std::move(arc_processes));
 
   auto killed_arc_processes = tab_manager_delegate.GetKilledArcProcesses();
   EXPECT_EQ(0U, killed_arc_processes.size());
@@ -413,7 +415,7 @@ TEST_F(TabManagerDelegateTest, KillMultipleProcesses) {
   memory_stat->SetProcessPss(20, 30000);
   memory_stat->SetProcessPss(10, 100000);
 
-  tab_manager_delegate.LowMemoryKillImpl(tab_list, arc_processes);
+  tab_manager_delegate.LowMemoryKillImpl(tab_list, std::move(arc_processes));
 
   auto killed_arc_processes = tab_manager_delegate.GetKilledArcProcesses();
   auto killed_tabs = tab_manager_delegate.GetKilledTabs();
