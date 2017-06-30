79f82bb6cdcf7b815aec26e94ade068dd5035f44
Becky Zhou <huayinz@chromium.org>
2017-06-30 02:55:47 +0000

    Dismiss floating action bar when chrome home is opened
    + Change text control visibility when bottom sheet is opened/closed
    Before: action bar show on top of bottom sheet
    After: action bar and text selection are dismissed and won't
               reactive again if bottom sheet is closed
    
    BUG=718056
    
    Change-Id: I496cebf1c2c5c343deb35430f7dbc3fc6c331e16
    Reviewed-on: https://chromium-review.googlesource.com/548438
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Becky Zhou <huayinz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483606}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheet.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheet.java
@@ -57,6 +57,7 @@ import org.chromium.chrome.browser.widget.FadingBackgroundView;
 import org.chromium.chrome.browser.widget.bottomsheet.BottomSheetContentController.ContentType;
 import org.chromium.chrome.browser.widget.textbubble.ViewAnchoredTextBubble;
 import org.chromium.content.browser.BrowserStartupController;
+import org.chromium.content.browser.ContentViewCore;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.ui.UiUtils;
 
@@ -960,6 +961,7 @@ public class BottomSheet
         if (mIsSheetOpen) return;
 
         mIsSheetOpen = true;
+        dismissSelectedText();
         for (BottomSheetObserver o : mObservers) o.onSheetOpened();
         announceForAccessibility(getResources().getString(R.string.bottom_sheet_opened));
         mActivity.addViewObscuringAllTabs(this);
@@ -1102,6 +1104,18 @@ public class BottomSheet
 
         setTranslationY(mContainerHeight - offset);
         sendOffsetChangeEvents();
+    }
+
+    /**
+     * Deselects any text in the active tab's web contents and dismisses the text controls.
+     */
+    private void dismissSelectedText() {
+        Tab activeTab = getActiveTab();
+        if (activeTab == null) return;
+
+        ContentViewCore contentViewCore = activeTab.getContentViewCore();
+        if (contentViewCore == null) return;
+        contentViewCore.clearSelection();
     }
 
     /**
