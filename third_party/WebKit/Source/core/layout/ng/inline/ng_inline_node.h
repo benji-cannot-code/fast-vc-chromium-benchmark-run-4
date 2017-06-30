695d42d7b5293e3ba26ba3d7959f0d56995f42b9
xiaochengh <xiaochengh@chromium.org>
2017-06-29 18:46:34 -0700

    Refactor NGInlineNode::CollectInlines (both overloads)
    This patch does the following refactoring:
    
    1. Remove the parameter NGInlineNode::CollectInlines(LayoutBlockFlow*) because
    it is always equal to GetLayoutBlockFlow().
    
    2. Change NGInlineNode::CollectInlines(LayoutBlockFlow*, NGInlineItemsBuilder*)
    to a local function of the .cc file, because it does not access any private
    member of NGInlineNode.
    
    3. Move call sites of NGInlineItemsBuilder::EnterBlock and ExitBlock into the
    above local function, so that it is easier to reuse this function to build
    offset mapping.
    
    Step 3 is a preparation for crrev.com/2963093002
    
    BUG=699017
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    
    Review-Url: https://codereview.chromium.org/2965573003
    Cr-Commit-Position: refs/heads/master@{#483585}

--- third_party/WebKit/Source/core/layout/ng/inline/ng_inline_node.h
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_inline_node.h
@@ -22,7 +22,6 @@ class NGInlineItemsBuilderTemplate;
 class EmptyOffsetMappingBuilder;
 class LayoutBlockFlow;
 class LayoutNGBlockFlow;
-class LayoutObject;
 struct MinMaxContentSize;
 class NGConstraintSpace;
 class NGInlineItem;
@@ -81,8 +80,7 @@ class CORE_EXPORT NGInlineNode : public NGLayoutInputNode {
   void PrepareLayout();
   bool IsPrepareLayoutFinished() const { return !Text().IsNull(); }
 
-  void CollectInlines(LayoutBlockFlow*);
-  LayoutObject* CollectInlines(LayoutBlockFlow*, NGInlineItemsBuilder*);
+  void CollectInlines();
   void SegmentText();
   void ShapeText();
 
