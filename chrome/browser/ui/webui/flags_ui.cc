e9e5b88229389a85ed982a547e9942228530dfa3
Dave Schuyler <dschuyler@chromium.org>
2017-06-30 21:18:59 +0000

    [i18n] remove strings.js
    Follow-up for CL https://codereview.chromium.org/2944743002 to stop
    sending the strings.js (which is no longer used).
    
    Bug: 677338
    Change-Id: I3edcabcf01db9ad7dc794bd4bc8fa05f6146d4ac
    Reviewed-on: https://chromium-review.googlesource.com/557966
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Dave Schuyler <dschuyler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483816}

--- chrome/browser/ui/webui/flags_ui.cc
+++ chrome/browser/ui/webui/flags_ui.cc
@@ -102,7 +102,6 @@ content::WebUIDataSource* CreateFlagsUIHTMLSource() {
   }
 #endif
 
-  source->SetJsonPath("strings.js");
   source->AddResourcePath(flags_ui::kFlagsJS, IDR_FLAGS_UI_FLAGS_JS);
   source->SetDefaultResource(IDR_FLAGS_UI_FLAGS_HTML);
   return source;
@@ -176,7 +175,7 @@ void FlagsDOMHandler::Init(flags_ui::FlagsStorage* flags_storage,
   access_ = access;
 
   if (experimental_features_requested_)
-    HandleRequestExperimentalFeatures(NULL);
+    HandleRequestExperimentalFeatures(nullptr);
 }
 
 void FlagsDOMHandler::HandleRequestExperimentalFeatures(
