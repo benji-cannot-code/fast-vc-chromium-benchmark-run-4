293f2709d8b857a022fc37173e40d08729197c82
Kent Tamura <tkent@chromium.org>
2017-06-30 08:30:55 +0000

    Remove WTF_CPU_32BIT and WTF_CPU_64BIT.
    Use defined(ARCH_CPU_xx_BITS) instead.
    This CL removes Bswapuintptrt() because it's not used.
    
    Bug: 738331
    Change-Id: I8307d23adfa2e02e3bfe4b9b6e01b74ca2db7995
    Reviewed-on: https://chromium-review.googlesource.com/557584
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483656}

--- third_party/WebKit/Source/platform/wtf/BitwiseOperations.h
+++ third_party/WebKit/Source/platform/wtf/BitwiseOperations.h
@@ -37,14 +37,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WTF_BitwiseOperations_h
 
 #include "base/bits.h"
-#include "platform/wtf/CPU.h"
+#include "build/build_config.h"
 
 namespace WTF {
 
 using base::bits::CountLeadingZeroBits32;
 using base::bits::CountLeadingZeroBitsSizeT;
 
-#if CPU(64BIT)
+#if defined(ARCH_CPU_64_BITS)
 using base::bits::CountLeadingZeroBits64;
 #endif
 
