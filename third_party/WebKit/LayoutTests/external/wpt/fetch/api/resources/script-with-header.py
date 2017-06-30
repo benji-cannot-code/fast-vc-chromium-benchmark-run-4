9d8476dad318b99e185f903e1f8a826f20aa92ea
Hiroshige Hayashizaki <hiroshige@chromium.org>
2017-06-30 02:46:45 +0000

    Remove Script::IsEmpty() check in ScriptLoader::DoExecuteScript()
    As preparation for DoExecuteScript() refactoring, this CL removes
    an early-exit condition for empty scripts, and let empty scripts to
    be handled by the general-case path below.
    
    Behavior changes:
    Previously some CSP/MIME type checks are skipped for empty scripts,
    and this causes load events are fired for empty scripts that would
    fail CSP/MIME checks.
    This CL makes these checks applied and causes error events are fired
    instead of load events.
    The modified layout test adds cases for MIME checks of empty scripts,
    which would have failed previously.
    
    Bug: 686281
    Change-Id: I21b460e395c952fa4a6289fb82e027cecd47335e
    Reviewed-on: https://chromium-review.googlesource.com/553559
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483601}

--- third_party/WebKit/LayoutTests/external/wpt/fetch/api/resources/script-with-header.py
+++ third_party/WebKit/LayoutTests/external/wpt/fetch/api/resources/script-with-header.py
@@ -1,5 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 def main(request, response):
     headers = [("Content-type", request.GET.first("mime"))]
-    content = "console.log('Script loaded')"
+    if "content" in request.GET and request.GET.first("content") == "empty":
+        content = ''
+    else:
+        content = "console.log('Script loaded')"
     return 200, headers, content
