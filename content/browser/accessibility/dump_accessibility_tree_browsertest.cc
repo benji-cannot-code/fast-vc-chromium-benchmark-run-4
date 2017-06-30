1192524ff25e61cb586ce733f294b7309f24928b
maniscalco <maniscalco@chromium.org>
2017-06-30 11:18:44 -0700

    Disable DumpAccessibilityTreeTest.AccessibilityModalDialogInIframeClosed
    Test seems to be flaky.
    
    BUG=738497
    
    TBR=dmazzoni
    
    Review-Url: https://codereview.chromium.org/2964223002
    Cr-Commit-Position: refs/heads/master@{#483762}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -1406,8 +1406,9 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
   RunHtmlTest(FILE_PATH_LITERAL("modal-dialog-opened.html"));
 }
 
+// http://crbug.com/738497
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
-                       AccessibilityModalDialogInIframeClosed) {
+                       DISABLED_AccessibilityModalDialogInIframeClosed) {
   RunHtmlTest(FILE_PATH_LITERAL("modal-dialog-in-iframe-closed.html"));
 }
 
