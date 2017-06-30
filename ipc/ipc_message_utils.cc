01b12c4d9cde4996361252533d68153576177eea
Scott Graham <scottmg@chromium.org>
2017-06-30 01:07:10 +0000

    fuchsia: get ipc_tests compiling again
    The broke when the message loop file descriptor stuff was removed, and
    some mojo things were refactored.
    
    handle_fuchsia.* and handle_attachment_fuchsia.* are just the
    _win versions with s/HANDLE/mx_handle_t/.
    
    Adds .filter file for 54/80 tests that pass, so this implementation
    undoubtedly needs work, but can be sharded better once this lands.
    
    Bug: 734791
    Change-Id: If9c506972e1d70119f9d20a66108902ee2fbfd16
    Reviewed-on: https://chromium-review.googlesource.com/557100
    Commit-Queue: Scott Graham <scottmg@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483568}

--- ipc/ipc_message_utils.cc
+++ ipc/ipc_message_utils.cc
@@ -39,6 +39,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/ipc_platform_file.h"
 #endif
 
+#if defined(OS_FUCHSIA)
+#include "ipc/handle_fuchsia.h"
+#endif
+
 namespace IPC {
 
 namespace {
@@ -754,6 +758,10 @@ bool ParamTraits<base::SharedMemoryHandle>::Read(const base::Pickle* m,
   HandleWin handle_win;
   if (!ReadParam(m, iter, &handle_win))
     return false;
+#elif defined(OS_FUCHSIA)
+  HandleFuchsia handle_fuchsia;
+  if (!ReadParam(m, iter, &handle_fuchsia))
+    return false;
 #else
   scoped_refptr<base::Pickle::Attachment> attachment;
   if (!m->ReadAttachment(iter, &attachment))
@@ -776,6 +784,9 @@ bool ParamTraits<base::SharedMemoryHandle>::Read(const base::Pickle* m,
                                 static_cast<size_t>(size), guid);
 #elif defined(OS_WIN)
   *r = base::SharedMemoryHandle(handle_win.get_handle(),
+                                static_cast<size_t>(size), guid);
+#elif defined(OS_FUCHSIA)
+  *r = base::SharedMemoryHandle(handle_fuchsia.get_handle(),
                                 static_cast<size_t>(size), guid);
 #else
   *r = base::SharedMemoryHandle(
