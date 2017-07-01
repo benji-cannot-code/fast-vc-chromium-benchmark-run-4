b7e3ef5797d1259c4789c87699eb9a2cd2af82b1
Xianzhu Wang <wangxianzhu@chromium.org>
2017-07-01 04:17:55 +0000

    [SPv2] Force new chunk at the end of a subsequence
    This is a follow-up of https://chromium-review.googlesource.com/c/525139/
    which forced new chunk at the beginning of a subsequence.
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I8c2f750f1bc08ef89c728d1413a51d8af03ec03a
    Reviewed-on: https://chromium-review.googlesource.com/556265
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483901}

--- third_party/WebKit/Source/platform/graphics/paint/PaintChunker.h
+++ third_party/WebKit/Source/platform/graphics/paint/PaintChunker.h
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-enum NewChunkForceState { kDontForceNewChunk, kForceNewChunk };
-
 // Accepts information about changes to |PaintChunkProperties| as drawings are
 // accumulated, and produces a series of paint chunks: contiguous ranges of the
 // display list with identical |PaintChunkProperties|.
@@ -36,15 +34,13 @@ class PLATFORM_EXPORT PaintChunker final {
   const PaintChunkProperties& CurrentPaintChunkProperties() const {
     return current_properties_;
   }
-  void UpdateCurrentPaintChunkProperties(
-      const PaintChunk::Id*,
-      const PaintChunkProperties&,
-      NewChunkForceState force_new_chunk = kDontForceNewChunk);
+  void UpdateCurrentPaintChunkProperties(const PaintChunk::Id*,
+                                         const PaintChunkProperties&);
+
+  void ForceNewChunk() { force_new_chunk_ = true; }
 
   // Returns true if a new chunk is created.
   bool IncrementDisplayItemIndex(const DisplayItem&);
-
-  void DecrementDisplayItemIndex();
 
   PaintChunk& PaintChunkAt(size_t i) { return chunks_[i]; }
   size_t LastChunkIndex() const {
@@ -65,19 +61,10 @@ class PLATFORM_EXPORT PaintChunker final {
   Vector<PaintChunk> ReleasePaintChunks();
 
  private:
-  enum ItemBehavior {
-    // Can be combined with adjacent items when building chunks.
-    kDefaultBehavior = 0,
-
-    // Item requires its own paint chunk.
-    kRequiresSeparateChunk,
-  };
-
   Vector<PaintChunk> chunks_;
-  Vector<ItemBehavior> chunk_behavior_;
   Optional<PaintChunk::Id> current_chunk_id_;
   PaintChunkProperties current_properties_;
-  NewChunkForceState force_new_chunk_;
+  bool force_new_chunk_;
 };
 
 }  // namespace blink
