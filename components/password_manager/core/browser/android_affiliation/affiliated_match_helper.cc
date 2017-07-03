192a80762fff3ee3c715c06501cb96f67d6faf73
vabr <vabr@chromium.org>
2017-07-03 07:16:35 -0700

    Remove facet-independent cache trimming in AffiliationService
    This is dead code.
    
    BUG=437865, 479507
    
    Review-Url: https://codereview.chromium.org/2971543002
    Cr-Commit-Position: refs/heads/master@{#484002}

--- components/password_manager/core/browser/android_affiliation/affiliated_match_helper.cc
+++ components/password_manager/core/browser/android_affiliation/affiliated_match_helper.cc
@@ -126,10 +126,6 @@ void AffiliatedMatchHelper::CompleteInjectAffiliatedWebRealm(
   barrier_closure.Run();
 }
 
-void AffiliatedMatchHelper::TrimAffiliationCache() {
-  affiliation_service_->TrimCache();
-}
-
 // static
 bool AffiliatedMatchHelper::IsValidAndroidCredential(
     const PasswordStore::FormDigest& form) {
