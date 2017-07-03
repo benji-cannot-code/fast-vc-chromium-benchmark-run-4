7bd936e64a86d7042a6f4bc122d1228cd91dc14d
ajith.v <ajith.v@samsung.com>
2017-07-03 10:05:26 -0700

    Relanding [Android] Adding Smart GO/NEXT feature in Chrome
    Smart Go/Next brings better user experience to the user during form submitting applications.
    For navigating between form elements, user can use NEXT/PREVIOUS button from IME
    without touching on individual fields. This will avoid unnecessary form submissions before
    filling or visiting all fields in the form.
    
    Additionally it will save user time and avoid redundant network requests before actually
    filling/attending entire fields in the form
    
    Design Document: https://docs.google.com/document/d/1h0diigZ8LUi7A3UKJ_zwNUbvNQoe-Nwr55_p6ivSPNg/edit?usp=sharing
    
    Initial patch is reviewed @ https://codereview.chromium.org/2839993002/ and
    https://codereview.chromium.org/2948593002/
    
    Scrolling performance is getting effected due to call to
    NextFocusableElementInForm() from InputMethodController. hence splitting it into multiple patches.
    This is the initial landing without enabling functionality.
    In next patch functionality will be enabled with necessary test coverage.
    
    BUG=410785, 648986, 733222
    
    Review-Url: https://codereview.chromium.org/2967493002
    Cr-Commit-Position: refs/heads/master@{#484020}

--- third_party/WebKit/public/web/WebLocalFrame.h
+++ third_party/WebKit/public/web/WebLocalFrame.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebFrameLoadType.h"
 #include "WebHistoryItem.h"
 #include "public/platform/WebCachePolicy.h"
+#include "public/platform/WebFocusType.h"
 #include "public/platform/WebSize.h"
 #include "public/platform/WebURLError.h"
 #include "public/platform/WebURLRequest.h"
@@ -764,6 +765,13 @@ class WebLocalFrame : public WebFrame {
   // plugin whose content indicates that printed output should not be scaled,
   // return true, otherwise return false.
   virtual bool IsPrintScalingDisabledForPlugin(const WebNode& = WebNode()) = 0;
+
+  // Advance the focus of the WebView to next text input element from current
+  // input field wrt sequential navigation with TAB or Shift + TAB
+  // WebFocusTypeForward simulates TAB and WebFocusTypeBackward simulates
+  // Shift + TAB. (Will be extended to other form controls like select element,
+  // checkbox, radio etc.)
+  virtual void AdvanceFocusInForm(WebFocusType) = 0;
 
   // Testing ------------------------------------------------------------------
 
