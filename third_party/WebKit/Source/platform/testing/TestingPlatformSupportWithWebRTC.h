80fba87a48c0e96a8eb1069ec5d3cad58103774a
Joel Hockey <joelhockey@chromium.org>
2017-07-03 02:23:19 +0000

    Move web/tests/scheduler tests to core/testing
    These are end-to-end tests for platform/scheduler that were
    previously in web/ since they had dependencies on web.  Now
    they have dependencies on core/ and I have chosen
    core/testing/scheduler.
    
    First moved
    modules/peerconnection/testing/TestingPlatformSupportWithRTC
    to platform/testing/ so as to remove the dependency on modules.
    
    With the modules dependency gone, the 3 tests in
    web/tests/scheduler can be moved to core/testing.
    
    Note that the presubmit gives an error of:
    ** Presubmit ERRORS **
    Do not use Chromium class from namespace cc inside Blink core:
    third_party/WebKit/Source/core/testing/scheduler/FrameThrottlingTest.cpp:50
    
    I'm guessing that it is ok to ignore the error for this test which
    is mocking a method which takes a type involving cc:PaintRecord.
    
    
    Bug: 712963
    Change-Id: Ifcbbdf57888af1760197676d4c99561b8161612f
    Reviewed-on: https://chromium-review.googlesource.com/557700
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483926}

--- third_party/WebKit/Source/platform/testing/TestingPlatformSupportWithWebRTC.h
+++ third_party/WebKit/Source/platform/testing/TestingPlatformSupportWithWebRTC.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef TestingPlatformSupportWithWebRTC_h
 #define TestingPlatformSupportWithWebRTC_h
 
-#include "bindings/core/v8/V8BindingForTesting.h"
 #include "platform/testing/TestingPlatformSupport.h"
 #include "public/platform/WebRTCPeerConnectionHandler.h"
 
