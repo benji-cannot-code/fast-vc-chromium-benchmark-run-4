e6f8d3731948292666213cdad34d8cbe6d2c2247
Kouhei Ueno <kouhei@chromium.org>
2017-07-03 07:27:41 +0000

    Remove ScriptIntegrityDisposition
    Not used anywhere
    
    Bug: None
    Change-Id: I8be8c8be0b98fb7ce3a80d950dc60f1776fb5ec5
    Reviewed-on: https://chromium-review.googlesource.com/558726
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483956}

--- third_party/WebKit/Source/core/loader/resource/ScriptResource.h
+++ third_party/WebKit/Source/core/loader/resource/ScriptResource.h
@@ -37,8 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-enum class ScriptIntegrityDisposition { kNotChecked = 0, kFailed, kPassed };
-
 class FetchParameters;
 class KURL;
 class ResourceFetcher;
