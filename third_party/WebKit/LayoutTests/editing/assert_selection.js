b64014df53d10939dabf6b18b314cac9fcbaeb9c
yosin <yosin@chromium.org>
2017-07-02 20:15:34 -0700

    Introduce Selection#compute{Left,Top} in assert_selection()
    This patch Introduces |selection#compute{Left,Top}| in |assert_selection()|
    to provide left and top position for mouse click testing.
    
    This patch is a preparation of the patch[1].
    
    [1] http://crrev.com/2966473002: Convert editing/selection/shift-click.html to use
    assert_selection()
    
    BUG=679977
    TEST=n/a; no behavior changes
    
    Review-Url: https://codereview.chromium.org/2963113002
    Cr-Commit-Position: refs/heads/master@{#483929}

--- third_party/WebKit/LayoutTests/editing/assert_selection.js
+++ third_party/WebKit/LayoutTests/editing/assert_selection.js
@@ -61,6 +61,10 @@ const DumpAs = {
   FLAT_TREE: 'flattree',
 };
 
+// border-size of IFRAME which hosts sample HTML. This value comes from
+// "core/css/html.css".
+const kIFrameBorderSize = 2;
+
 /** @const @type {string} */
 const kTextArea = 'TEXTAREA';
 
@@ -665,6 +669,28 @@ class Serializer {
 }
 
 /**
+ * @param {!HTMLElement} element
+ * @return {number}
+ */
+function computeLeft(element) {
+  let left = kIFrameBorderSize + element.ownerDocument.offsetLeft;
+  for (let runner = element; runner; runner = runner.offsetParent)
+    left += runner.offsetLeft;
+  return left;
+}
+
+/**
+ * @param {!HTMLElement} element
+ * @return {number}
+ */
+function computeTop(element) {
+  let top = kIFrameBorderSize + element.ownerDocument.offsetTop;
+  for (let runner = element; runner; runner = runner.offsetParent)
+    top += runner.offsetTop;
+  return top;
+}
+
+/**
  * @this {!DOMSelection}
  * @param {string} html
  * @param {string=} opt_text
@@ -713,6 +739,8 @@ class Sample {
     this.selection_.document.offsetLeft = this.iframe_.offsetLeft;
     this.selection_.document.offsetTop = this.iframe_.offsetTop;
     this.selection_.setClipboardData = setClipboardData;
+    this.selection_.computeLeft = computeLeft;
+    this.selection_.computeTop = computeTop;
     this.load(sampleText);
   }
 
