e78ab53a20333cab8c5ecab32a634408a85eca8a
ricea <ricea@chromium.org>
2017-07-03 01:21:43 -0700

    Add GURL::HostNoBracketsPiece()
    In several places GURL::HostNoBrackets() is used as an argument to a
    function that only requires a StringPiece. To avoid this unnecessary
    string copy, add a HostNoBracketsPiece() variant in line with the
    existing host_piece() variant.
    
    This CL also changes calls to HostNoBrackets() to HostNoBracketsPiece()
    where it is simple and useful.
    
    BUG=
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2956643002
    Cr-Commit-Position: refs/heads/master@{#483962}

--- content/common/origin_util.cc
+++ content/common/origin_util.cc
@@ -37,8 +37,7 @@ bool IsOriginSecure(const GURL& url) {
     return true;
   }
 
-  std::string hostname = url.HostNoBrackets();
-  if (net::IsLocalhost(hostname))
+  if (net::IsLocalhost(url.HostNoBracketsPiece()))
     return true;
 
   if (base::ContainsValue(url::GetSecureSchemes(), url.scheme()))
@@ -80,7 +79,7 @@ bool IsPotentiallyTrustworthyOrigin(const url::Origin& origin) {
 
   if (base::ContainsValue(url::GetSecureSchemes(), origin.scheme()) ||
       base::ContainsValue(url::GetLocalSchemes(), origin.scheme()) ||
-      net::IsLocalhost(origin.GetURL().HostNoBrackets())) {
+      net::IsLocalhost(origin.GetURL().HostNoBracketsPiece())) {
     return true;
   }
 
