9a40cba423e2bd63b44279a04c55adad24614274
tapted <tapted@chromium.org>
2017-07-02 21:16:16 -0700

    MacViews a11y: Support the "Show menu" action in Textfield and Combobox.
    Currently Combobox and Textfield show no actions available to
    accessibility.
    
    For Mac, fix by mapping the default action (a click) to
    NSAccessibilityPressAction and map AX_ACTION_SHOW_CONTEXT_MENU to
    NSAccessibilityShowMenuAction. For comboboxes, both
    NSAccessibilityPressAction and NSAccessibilityShowMenuAction show a
    menu. For textfields, just the latter.
    
    On Mac, automatically add and handle NSAccessibilityShowMenuAction for
    controls that show a menu on press and don't have a separate context
    menu.
    
    For Textfields (and other controls using context menus), automatically
    add ui::AX_ACTION_SHOW_CONTEXT_MENU for views that have a context menu
    controller.
    
    Comboboxes are trickier since the View that usually handles clicks is a
    "TransparentButton". So handle these actions explicitly.
    
    For menus triggered by a11y actions, there may be no mouse event. Cocoa
    needs one to position the menu, so generate a dummy event for this.
    
    BUG=679247, 663536, 732655
    
    Review-Url: https://codereview.chromium.org/2944083004
    Cr-Commit-Position: refs/heads/master@{#483939}

--- ui/views/controls/combobox/combobox.cc
+++ ui/views/controls/combobox/combobox.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/macros.h"
 #include "build/build_config.h"
+#include "ui/accessibility/ax_action_data.h"
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/base/default_style.h"
 #include "ui/base/ime/input_method.h"
@@ -765,6 +766,20 @@ void Combobox::GetAccessibleNodeData(ui::AXNodeData* node_data) {
   }
   node_data->AddIntAttribute(ui::AX_ATTR_POS_IN_SET, selected_index_);
   node_data->AddIntAttribute(ui::AX_ATTR_SET_SIZE, model_->GetItemCount());
+}
+
+bool Combobox::HandleAccessibleAction(const ui::AXActionData& action_data) {
+  // The action handling in View would generate a mouse event and send it to
+  // |this|. However, mouse events for Combobox are handled by |arrow_button_|,
+  // which is hidden from the a11y tree (so can't expose actions). Rather than
+  // forwarding AX_ACTION_DO_DEFAULT to View and then forwarding the mouse event
+  // it generates to |arrow_button_| to have it forward back to |this| (as its
+  // ButtonListener), just handle the action explicitly here and bypass View.
+  if (enabled() && action_data.action == ui::AX_ACTION_DO_DEFAULT) {
+    ShowDropDownMenu(ui::MENU_SOURCE_KEYBOARD);
+    return true;
+  }
+  return View::HandleAccessibleAction(action_data);
 }
 
 void Combobox::ButtonPressed(Button* sender, const ui::Event& event) {
