ace568a6f547318b05ebf308361eb8a3a65571de
Joel Hockey <joelhockey@chromium.org>
2017-07-03 11:25:07 +0000

    Move mhtml files from web/tests/data to core/testing/data
    These files include CRLF line endings by design.
    I was having trouble getting these to move with all other files
    in https://chromium-review.googlesource.com/c/558570/.
    
    Bug: 353585
    Change-Id: Ieabeb6b5beecef62c0528e749975a1160551460b
    Reviewed-on: https://chromium-review.googlesource.com/558589
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483988}

--- third_party/WebKit/Source/core/frame/MHTMLTest.cpp
+++ third_party/WebKit/Source/core/frame/MHTMLTest.cpp
@@ -87,7 +87,7 @@ class LineReader {
 
 class MHTMLTest : public ::testing::Test {
  public:
-  MHTMLTest() { file_path_ = testing::WebTestDataPath("mhtml/"); }
+  MHTMLTest() { file_path_ = testing::CoreTestDataPath("mhtml/"); }
 
  protected:
   void SetUp() override { helper_.Initialize(); }
@@ -102,7 +102,7 @@ class MHTMLTest : public ::testing::Test {
                              const std::string& file_name) {
     URLTestHelpers::RegisterMockedURLLoad(
         ToKURL(url),
-        testing::WebTestDataPath(WebString::FromUTF8("mhtml/" + file_name)),
+        testing::CoreTestDataPath(WebString::FromUTF8("mhtml/" + file_name)),
         WebString::FromUTF8("multipart/related"));
   }
 
