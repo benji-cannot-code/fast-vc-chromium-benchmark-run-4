6c6614ba322ce6085b59c92acf35dac335b2d531
Vaclav Brozek <vabr@chromium.org>
2017-07-03 13:36:41 +0000

    Allow "view-passwords" Feature to control iOS as well
    Viewing and copying passwords on iOS is behind an experimental flag. For the
    Android version of the same feature, there is already a base::Feature:
    "view-passwords". This CL allows viewing and copying passwords on iOS to be
    enabled by that Feature as well.
    
    The main benefit is the integration with Chrome Variations.
    
    The impact of combining allowing through the experimental flag's
    NSUserDefaults and base::Feature is that splitting UMA metrics by Finch
    experiments is not meaningful (because the feature being disabled by Finch
    does not imply it is indeed off). However, the experimental flags are only
    accessible in Canary and custom builds, so as long as we ignore Canary, the
    data will still be useful.
    
    There is no added configuration to enable this feature on trybots. Because the
    feature only affects the settings page, it is unlikely to influence other
    tests, except those for settings. Here the feature is already covered by its
    own EG tests, which force it to be on.
    
    R=sdefresne@chromium.org
    BUG=159166
    
    Change-Id: I5674c28ae3b855528a08d1ccd56f873d1c02634e
    Reviewed-on: https://chromium-review.googlesource.com/558416
    Commit-Queue: Vaclav Brozek <vabr@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483998}

--- ios/chrome/browser/experimental_flags.mm
+++ ios/chrome/browser/experimental_flags.mm
@@ -14,10 +14,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/metrics/field_trial.h"
 #include "base/strings/string_util.h"
 #include "base/strings/sys_string_conversions.h"
 #include "components/autofill/core/common/autofill_switches.h"
+#include "components/password_manager/core/common/password_manager_features.h"
 #include "components/signin/core/common/signin_switches.h"
 #include "components/variations/variations_associated_data.h"
 #include "ios/chrome/browser/chrome_switches.h"
@@ -234,7 +236,8 @@ bool IsViewCopyPasswordsEnabled() {
       objectForKey:kEnableViewCopyPasswords];
   if ([viewCopyPasswordFlag isEqualToString:@"Enabled"])
     return true;
-  return false;
+  return base::FeatureList::IsEnabled(
+      password_manager::features::kViewPasswords);
 }
 
 bool UseOnlyLocalHeuristicsForPasswordGeneration() {
