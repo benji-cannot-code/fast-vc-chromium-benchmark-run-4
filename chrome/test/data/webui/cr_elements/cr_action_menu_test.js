9cccd20bb27382facf3027590a9009cdcef579b6
calamity <calamity@chromium.org>
2017-07-02 20:59:49 -0700

    [cr-action-menu] Constrain menu to viewport.
    This CL forces the action menu to align to the viewport after choosing
    an anchor direction.
    
    BUG=734984
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2948973002
    Cr-Commit-Position: refs/heads/master@{#483938}

--- chrome/test/data/webui/cr_elements/cr_action_menu_test.js
+++ chrome/test/data/webui/cr_elements/cr_action_menu_test.js
@@ -216,8 +216,6 @@ suite('CrActionMenu', function() {
       anchorAlignmentX: AnchorAlignment.CENTER,
       anchorAlignmentY: AnchorAlignment.CENTER,
     }));
-    menuWidth = menu.offsetWidth;
-    menuHeight = menu.offsetHeight;
     assertEquals(`${120 - menuWidth / 2}px`, menu.style.left);
     assertEquals(`${255 - menuHeight / 2}px`, menu.style.top);
     menu.close();
@@ -227,8 +225,6 @@ suite('CrActionMenu', function() {
       anchorAlignmentX: AnchorAlignment.BEFORE_END,
       anchorAlignmentY: AnchorAlignment.BEFORE_END,
     }));
-    menuWidth = menu.offsetWidth;
-    menuHeight = menu.offsetHeight;
     assertEquals(`${140 - menuWidth}px`, menu.style.left);
     assertEquals(`${260 - menuHeight}px`, menu.style.top);
     menu.close();
@@ -240,8 +236,6 @@ suite('CrActionMenu', function() {
       left: 0,
       top: 0,
     }));
-    menuWidth = menu.offsetWidth;
-    menuHeight = menu.offsetHeight;
     assertEquals(`0px`, menu.style.left);
     assertEquals(`0px`, menu.style.top);
     menu.close();
@@ -254,17 +248,25 @@ suite('CrActionMenu', function() {
       maxX: 1000,
       maxY: 2000,
     });
-    menuWidth = menu.offsetWidth;
-    menuHeight = menu.offsetHeight;
     assertEquals(`${1000 - menuWidth}px`, menu.style.left);
     assertEquals(`${2000 - menuHeight}px`, menu.style.top);
+    menu.close();
+
+    // If the viewport can't fit the menu, align the menu to the viewport.
+    menu.showAtPosition({
+      left: menuWidth - 5,
+      top: 0,
+      width: 0,
+      height: 0,
+      maxX: menuWidth * 2 - 10,
+    });
+    assertEquals(`${menuWidth  - 10}px`, menu.style.left);
+    assertEquals(`0px`, menu.style.top);
     menu.close();
 
     // Alignment is reversed in RTL.
     document.body.style.direction = 'rtl';
     menu.showAtPosition(config);
-    menuWidth = menu.offsetWidth;
-    menuHeight = menu.offsetHeight;
     assertTrue(menu.open);
     assertEquals(140 - menuWidth, menu.offsetLeft);
     assertEquals('250px', menu.style.top);
