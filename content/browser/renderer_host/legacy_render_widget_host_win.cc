f758660a3269dca8527fe0ec28dff15e30080a5d
nektar <nektar@chromium.org>
2017-07-04 16:57:39 -0700

    Added a system caret used for accessibility on Windows.
    Screen magnifiers need to know where the caret is located so that they can show a magnified caret allowing the user to more easily track it.
    Existing screen magnifiers, such as the built-in Windows Magnifier, require a system caret to be present in order to track it. However, Chrome doesn't use the system caret.
    This is an object that implements the same accessibility interface that would have been returned by the system if the system caret had been used.
    R=dmazzoni@chromium.org, shuchen@chromium.org, ellyjones@chromium.org, ananta@chromium.org, sky@chromium.org, sadrul@chromium.org
    
    Review-Url: https://codereview.chromium.org/2852763002
    Cr-Original-Original-Commit-Position: refs/heads/master@{#483558}
    Committed: https://chromium.googlesource.com/chromium/src/+/3fc0df1e01ab7e17318ce50724890f939bc00fa4
    Review-Url: https://codereview.chromium.org/2852763002
    Cr-Original-Commit-Position: refs/heads/master@{#483825}
    Committed: https://chromium.googlesource.com/chromium/src/+/99936ad737f80a465b479d6bc0abacd22767deaf
    Review-Url: https://codereview.chromium.org/2852763002
    Cr-Commit-Position: refs/heads/master@{#484167}

--- content/browser/renderer_host/legacy_render_widget_host_win.cc
+++ content/browser/renderer_host/legacy_render_widget_host_win.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/render_widget_host_impl.h"
 #include "content/browser/renderer_host/render_widget_host_view_aura.h"
 #include "content/public/common/content_switches.h"
-#include "ui/accessibility/platform/ax_fake_caret_win.h"
+#include "ui/accessibility/platform/ax_system_caret_win.h"
 #include "ui/base/view_prop.h"
 #include "ui/base/win/internal_constants.h"
 #include "ui/base/win/window_event_target.h"
@@ -90,8 +90,8 @@ void LegacyRenderWidgetHostHWND::SetBounds(const gfx::Rect& bounds) {
 }
 
 void LegacyRenderWidgetHostHWND::MoveCaretTo(const gfx::Rect& bounds) {
-  DCHECK(ax_fake_caret_);
-  ax_fake_caret_->MoveCaretTo(bounds);
+  DCHECK(ax_system_caret_);
+  ax_system_caret_->MoveCaretTo(bounds);
 }
 
 void LegacyRenderWidgetHostHWND::OnFinalMessage(HWND hwnd) {
@@ -112,10 +112,10 @@ LegacyRenderWidgetHostHWND::LegacyRenderWidgetHostHWND(HWND parent)
   Base::Create(parent, rect, L"Chrome Legacy Window",
                WS_CHILDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS,
                WS_EX_TRANSPARENT);
-  // We create the fake caret regardless of accessibility mode since not all
-  // assistive software that makes use of a fake caret is classified as a screen
+  // We create a system caret regardless of accessibility mode since not all
+  // assistive software that makes use of a caret is classified as a screen
   // reader, e.g. the built-in Windows Magnifier.
-  ax_fake_caret_ = std::make_unique<ui::AXFakeCaretWin>(hwnd());
+  ax_system_caret_ = std::make_unique<ui::AXSystemCaretWin>(hwnd());
 }
 
 LegacyRenderWidgetHostHWND::~LegacyRenderWidgetHostHWND() {
@@ -202,11 +202,11 @@ LRESULT LegacyRenderWidgetHostHWND::OnGetObject(UINT message,
   }
 
   if (static_cast<DWORD>(OBJID_CARET) == obj_id && host_->HasFocus()) {
-    DCHECK(ax_fake_caret_);
-    base::win::ScopedComPtr<IAccessible> fake_caret_accessible =
-        ax_fake_caret_->GetCaret();
+    DCHECK(ax_system_caret_);
+    base::win::ScopedComPtr<IAccessible> ax_system_caret_accessible =
+        ax_system_caret_->GetCaret();
     return LresultFromObject(IID_IAccessible, w_param,
-                             fake_caret_accessible.Detach());
+                             ax_system_caret_accessible.Detach());
   }
 
   return static_cast<LRESULT>(0L);
