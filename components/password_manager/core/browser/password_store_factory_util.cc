5cbf515fc8ba0973a8b81776a2b454dafe36d222
vabr <vabr@chromium.org>
2017-07-04 03:38:34 -0700

    Revert of Switch AffiliationService from DB thread to a TaskRunner (patchset #5 id:80001 of https://codereview.chromium.org/2968903002/ )
    Reason for revert:
    Throws DCHECKS.
    
    Original issue's description:
    > Switch AffiliationService from DB thread to a TaskRunner
    >
    > This CL does these significant changes:
    > (1) Changes a SingleThreadTaskRunner to SequencedThreadTaskRunner in
    >     AffiliationService and related backend code.
    > (2) Pushes obtaining of the TaskRunner to the callsite of the
    >     AffiliationService constructor.
    > (3) Replaces the choice of BrowserThread::DB thread task runner with a generic
    >     one which has the lowest priority and sequential execution guarantee.
    >
    > I could not find a reason why single thread (thread affinity) was required by
    > the affiliation code, so my hope is that (1) is safe. The tasks to be posted
    > on the task runner are potentially blocking and are not part of user
    > interaction, so setting the BLOCKING trait (currently the lowest priority)
    > seems appropriate, explaining (3).
    >
    > As for (2), the general guidance [1] is that task runner is obtained down in
    > the component by the "end user" of it. This is not easily done, however,
    > because the unittests for AffiliationService inject a different task runner,
    > which enables time-forwarding simulation for testing. Still, at least now
    > the task runner is obtained in the component.
    >
    > [1]
    > https://chromium.googlesource.com/chromium/src/+/master/docs/task_scheduler_migration.md#BrowserThreads,
    > look for "passed down"
    >
    > R=engedy@chromium.org
    > BUG=689520
    >
    > Review-Url: https://codereview.chromium.org/2968903002
    > Cr-Commit-Position: refs/heads/master@{#484017}
    > Committed: https://chromium.googlesource.com/chromium/src/+/4cb77ce419b39c1fd06975f16374b6cee0f06224
    
    TBR=engedy@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=689520
    
    Review-Url: https://codereview.chromium.org/2970693003
    Cr-Commit-Position: refs/heads/master@{#484083}

--- components/password_manager/core/browser/password_store_factory_util.cc
+++ components/password_manager/core/browser/password_store_factory_util.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/memory/ptr_util.h"
-#include "base/task_scheduler/post_task.h"
 #include "components/password_manager/core/browser/android_affiliation/affiliated_match_helper.h"
 #include "components/password_manager/core/browser/android_affiliation/affiliation_service.h"
 #include "components/password_manager/core/browser/android_affiliation/affiliation_utils.h"
@@ -30,17 +29,13 @@ bool ShouldAffiliationBasedMatchingBeActive(syncer::SyncService* sync_service) {
 void ActivateAffiliationBasedMatching(
     PasswordStore* password_store,
     net::URLRequestContextGetter* request_context_getter,
-    const base::FilePath& db_path) {
+    const base::FilePath& db_path,
+    scoped_refptr<base::SingleThreadTaskRunner> db_thread_runner) {
   // The PasswordStore is so far the only consumer of the AffiliationService,
   // therefore the service is owned by the AffiliatedMatchHelper, which in
   // turn is owned by the PasswordStore.
-  // Task priority is USER_VISIBLE, because AffiliationService-related tasks
-  // block obtaining credentials from PasswordStore, which matches the
-  // USER_VISIBLE example: "Loading data that might be shown in the UI after a
-  // future user interaction."
   std::unique_ptr<AffiliationService> affiliation_service(
-      new AffiliationService(base::CreateSequencedTaskRunnerWithTraits(
-          {base::MayBlock(), base::TaskPriority::USER_VISIBLE})));
+      new AffiliationService(db_thread_runner));
   affiliation_service->Initialize(request_context_getter, db_path);
   std::unique_ptr<AffiliatedMatchHelper> affiliated_match_helper(
       new AffiliatedMatchHelper(password_store,
@@ -62,7 +57,8 @@ void ToggleAffiliationBasedMatchingBasedOnPasswordSyncedState(
     PasswordStore* password_store,
     syncer::SyncService* sync_service,
     net::URLRequestContextGetter* request_context_getter,
-    const base::FilePath& profile_path) {
+    const base::FilePath& profile_path,
+    scoped_refptr<base::SingleThreadTaskRunner> db_thread_runner) {
   DCHECK(password_store);
 
   const bool matching_should_be_active =
@@ -72,7 +68,8 @@ void ToggleAffiliationBasedMatchingBasedOnPasswordSyncedState(
 
   if (matching_should_be_active && !matching_is_active) {
     ActivateAffiliationBasedMatching(password_store, request_context_getter,
-                                     GetAffiliationDatabasePath(profile_path));
+                                     GetAffiliationDatabasePath(profile_path),
+                                     db_thread_runner);
   } else if (!matching_should_be_active && matching_is_active) {
     password_store->SetAffiliatedMatchHelper(
         base::WrapUnique<AffiliatedMatchHelper>(nullptr));
