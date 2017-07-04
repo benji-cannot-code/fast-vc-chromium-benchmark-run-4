cdf89abe816d2f2a1318b4f5740f23da0e4d1910
erikchen <erikchen@chromium.org>
2017-07-04 00:54:42 -0700

    macOS: Partition alloc should use memory tag 254.
    This allows vmmap to more easily distinguish regions allocated by partition
    alloc.
    
    BUG=738926
    
    Review-Url: https://codereview.chromium.org/2971603002
    Cr-Commit-Position: refs/heads/master@{#484062}

--- base/allocator/partition_allocator/page_allocator.cc
+++ base/allocator/partition_allocator/page_allocator.cc
@@ -14,6 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "build/build_config.h"
 
+#if defined(OS_MACOSX)
+#include <mach/mach.h>
+#endif
+
 #if defined(OS_POSIX)
 
 #include <errno.h>
@@ -62,10 +66,19 @@ static void* SystemAllocPages(
   if (!ret)
     s_allocPageErrorCode = GetLastError();
 #else
+
+#if defined(OS_MACOSX)
+  // Use a custom tag to make it easier to distinguish partition alloc regions
+  // in vmmap.
+  int fd = VM_MAKE_TAG(254);
+#else
+  int fd = -1;
+#endif
+
   int access_flag = page_accessibility == PageAccessible
                         ? (PROT_READ | PROT_WRITE)
                         : PROT_NONE;
-  ret = mmap(hint, length, access_flag, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+  ret = mmap(hint, length, access_flag, MAP_ANONYMOUS | MAP_PRIVATE, fd, 0);
   if (ret == MAP_FAILED) {
     s_allocPageErrorCode = errno;
     ret = 0;
