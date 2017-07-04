bc8de3c41090b6b2e654900459ef9f448a754e5a
mahmadi <mahmadi@chromium.org>
2017-07-04 09:14:10 -0700

    [Payment Request] Populates country field if country code/name is valid.
    Compares autofill profile's country against available country codes and
    country names. This is similar to what is being done for regions and should
    make selecting the value for the country field more resilient to changes in
    autofill's backend.
    
    BUG=602666
    
    Review-Url: https://codereview.chromium.org/2964333002
    Cr-Commit-Position: refs/heads/master@{#484125}

--- ios/chrome/browser/ui/payments/address_edit_mediator.mm
+++ ios/chrome/browser/ui/payments/address_edit_mediator.mm
@@ -154,7 +154,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     if ([regions objectForKey:region]) {
       self.regionField.value = region;
     } else if ([[regions allKeysForObject:region] count]) {
-      DCHECK(1 == [[regions allKeysForObject:region] count]);
+      DCHECK_EQ(1U, [[regions allKeysForObject:region] count]);
       self.regionField.value = [regions allKeysForObject:region][0];
     }
   }
@@ -190,16 +190,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
   _countries = countries;
 
-  // If an address is being edited and it has a valid country code, the selected
-  // country code is set to that value. Otherwise, it is set to the default
-  // country code.
-  NSString* countryCode =
+  // If an address is being edited and it has a valid country code or a valid
+  // country name for the autofill::ADDRESS_HOME_COUNTRY field, the selected
+  // country code is set to the respective country code. Otherwise, the selected
+  // country code is set to the default country code.
+  NSString* country =
       [self fieldValueFromProfile:_address
                         fieldType:autofill::ADDRESS_HOME_COUNTRY];
-  _selectedCountryCode =
-      countryCode && [_countries objectForKey:countryCode]
-          ? countryCode
-          : base::SysUTF8ToNSString(countryModel.GetDefaultCountryCode());
+
+  if ([countries objectForKey:country]) {
+    _selectedCountryCode = country;
+  } else if ([[countries allKeysForObject:country] count]) {
+    DCHECK_EQ(1U, [[countries allKeysForObject:country] count]);
+    _selectedCountryCode = [countries allKeysForObject:country][0];
+  } else {
+    _selectedCountryCode =
+        base::SysUTF8ToNSString(countryModel.GetDefaultCountryCode());
+  }
 }
 
 // Queries the region names based on the selected country code.
