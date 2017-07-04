2a9391d17daa9793160cce8cef42fd3e42a5fe6f
Vaclav Brozek <vabr@chromium.org>
2017-07-04 20:51:27 +0000

    Add histograms for showing passwords in settings
    This CL adds histograms to count how passwords are shown in Chrome's settings,
    as well as how reauthentication is used.
    
    It adds it for iOS and desktop. On Android, those should be added later, as
    building the viewing feature there progresses.
    
    BUG=159166
    R=rkaplow@chromium.org, melandory@chromium.org
    
    Change-Id: I995408dc16bc389ea3893612b834a482c3b11356
    Reviewed-on: https://chromium-review.googlesource.com/558422
    Reviewed-by: Tatiana Gornak <melandory@chromium.org>
    Reviewed-by: Louis Romero <lpromero@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Vaclav Brozek <vabr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484155}

--- ios/chrome/browser/ui/settings/reauthentication_module.mm
+++ ios/chrome/browser/ui/settings/reauthentication_module.mm
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <LocalAuthentication/LocalAuthentication.h>
 
 #import "base/logging.h"
+#include "base/metrics/histogram_macros.h"
+#include "components/password_manager/core/browser/password_manager_metrics_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -43,6 +45,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                  handler:(void (^)(BOOL success))handler {
   if ([self isPreviousAuthValid]) {
     handler(YES);
+    UMA_HISTOGRAM_ENUMERATION(
+        "PasswordManager.ReauthToAccessPasswordInSettings",
+        password_manager::metrics_util::REAUTH_SKIPPED,
+        password_manager::metrics_util::REAUTH_COUNT);
     return;
   }
 
@@ -61,6 +67,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         [strongSelf->_successfulReauthTimeAccessor updateSuccessfulReauthTime];
       }
       handler(success);
+      UMA_HISTOGRAM_ENUMERATION(
+          "PasswordManager.ReauthToAccessPasswordInSettings",
+          success ? password_manager::metrics_util::REAUTH_SUCCESS
+                  : password_manager::metrics_util::REAUTH_FAILURE,
+          password_manager::metrics_util::REAUTH_COUNT);
     });
   };
 
