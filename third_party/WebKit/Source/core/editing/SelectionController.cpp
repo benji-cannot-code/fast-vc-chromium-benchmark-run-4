8a410d6d228bf842a81789801bd5f77bd55e9571
yosin <yosin@chromium.org>
2017-07-04 01:29:23 -0700

    Make VisibleSelection::AppendTrailingWhitespace() as const function
    This patch makes |VisibleSelection::AppendTrailingWhitespace()| as const
    function to make |VisibleSelection| as *virtual* immutable for improving
    code health.
    
    BUG=660320
    TEST=n/a; no behavior changes
    
    Review-Url: https://codereview.chromium.org/2964893002
    Cr-Commit-Position: refs/heads/master@{#484066}

--- third_party/WebKit/Source/core/editing/SelectionController.cpp
+++ third_party/WebKit/Source/core/editing/SelectionController.cpp
@@ -541,7 +541,7 @@ bool SelectionController::SelectClosestWordFromHitTestResult(
   }
 
   if (append_trailing_whitespace == AppendTrailingWhitespace::kShouldAppend)
-    new_selection.AppendTrailingWhitespace();
+    new_selection = new_selection.AppendTrailingWhitespace();
 
   return UpdateSelectionForMouseDownDispatchingSelectStart(
       inner_node,
@@ -585,7 +585,7 @@ void SelectionController::SelectClosestMisspellingFromHitTestResult(
       SelectionInFlatTree::Builder().Collapse(start).Extend(end).Build());
 
   if (append_trailing_whitespace == AppendTrailingWhitespace::kShouldAppend)
-    new_selection.AppendTrailingWhitespace();
+    new_selection = new_selection.AppendTrailingWhitespace();
 
   UpdateSelectionForMouseDownDispatchingSelectStart(
       inner_node,
