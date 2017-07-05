b4db6c47b575aaf500650578498e10cdf571560c
tetsui <tetsui@chromium.org>
2017-07-05 01:54:03 -0700

    Implement time stamp in new-style notification.
    New-style notification should show the notification timestamp relative
    to now on the header.
    The format should be consistent with Android's one, so we have to
    implement our own version of formatter.
    
    BUG=737003
    TEST=manual
    
    Review-Url: https://codereview.chromium.org/2972493002
    Cr-Commit-Position: refs/heads/master@{#484220}

--- ui/message_center/views/notification_header_view.h
+++ ui/message_center/views/notification_header_view.h
@@ -25,6 +25,7 @@ class NotificationHeaderView : public views::CustomButton {
   void SetAppName(const base::string16& name);
   void SetProgress(int progress);
   void SetOverflowIndicator(int count);
+  void SetTimestamp(base::Time past);
   void SetExpandButtonEnabled(bool enabled);
   void SetExpanded(bool expanded);
   void SetSettingsButtonEnabled(bool enabled);
@@ -32,6 +33,7 @@ class NotificationHeaderView : public views::CustomButton {
   void SetControlButtonsVisible(bool visible);
   void ClearProgress();
   void ClearOverflowIndicator();
+  void ClearTimestamp();
   bool IsExpandButtonEnabled();
   bool IsSettingsButtonEnabled();
   bool IsCloseButtonEnabled();
@@ -49,11 +51,14 @@ class NotificationHeaderView : public views::CustomButton {
 
  private:
   void UpdateControlButtonsVisibility();
+  // Update visibility for both |summary_text_view_| and |timestamp_view_|.
   void UpdateSummaryTextVisibility();
 
   views::Label* app_name_view_ = nullptr;
   views::Label* summary_text_divider_ = nullptr;
   views::Label* summary_text_view_ = nullptr;
+  views::Label* timestamp_divider_ = nullptr;
+  views::Label* timestamp_view_ = nullptr;
   views::ImageView* app_icon_view_ = nullptr;
   views::ImageView* expand_button_ = nullptr;
   PaddedButton* settings_button_ = nullptr;
@@ -64,6 +69,7 @@ class NotificationHeaderView : public views::CustomButton {
   bool is_control_buttons_visible_ = false;
   bool has_progress_ = false;
   bool has_overflow_indicator_ = false;
+  bool has_timestamp_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(NotificationHeaderView);
 };
