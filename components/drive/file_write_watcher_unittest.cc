361d74e6b2e065ded928dace0e00e34335219dbc
satorux <satorux@chromium.org>
2017-07-04 22:26:37 -0700

    drive: Get rid of use of BrowserThread::FILE part 2
    This should have been removed in crrev.com/483658 but forgot
    to do it.
    
    BUG=689520
    TEST=unit_tests pass as before
    
    Review-Url: https://codereview.chromium.org/2972523003
    Cr-Commit-Position: refs/heads/master@{#484192}

--- components/drive/file_write_watcher_unittest.cc
+++ components/drive/file_write_watcher_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_temp_dir.h"
 #include "base/macros.h"
 #include "base/run_loop.h"
+#include "base/task_scheduler/post_task.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -90,10 +91,9 @@ TEST_F(FileWriteWatcherTest, WatchThreeFiles) {
   TestObserver observer(expected, loop.QuitClosure());
 
   // Set up the watcher.
-  scoped_refptr<base::SingleThreadTaskRunner> file_task_runner =
-      content::BrowserThread::GetTaskRunnerForThread(
-          content::BrowserThread::FILE);
-  FileWriteWatcher watcher(file_task_runner.get());
+  scoped_refptr<base::SequencedTaskRunner> task_runner =
+      base::CreateSequencedTaskRunnerWithTraits({base::MayBlock()});
+  FileWriteWatcher watcher(task_runner.get());
   watcher.DisableDelayForTesting();
 
   // Start watching and running.
