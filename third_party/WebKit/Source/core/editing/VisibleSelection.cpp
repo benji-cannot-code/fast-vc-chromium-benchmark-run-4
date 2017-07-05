9f2c99473b77f0c5007b419c5089bf813244f3d0
yosin <yosin@chromium.org>
2017-07-05 03:15:53 -0700

    Expose Compute{Start,End}RespectingGranularity()
    This patch exposes flat tree version of |ComputeStartRespectingGranularity()|
    and |ComputeEndRespectingGranularity()| used in |VisibleSelection::Validate()|
    as a preparation of getting rid of |granularity_| member variable from
    |VisibleSelectionTemplate|.
    
    The patch[1] contains usages of |Compute{Start,End}RespectingGranularity()|
    in "SelectionCotroller.cpp".
    
    [1] http://crrev.com/2959153002: Get rid of SelectionTemplate::granularity_
    
    BUG=692923
    TEST=n/a; no behavior changes
    
    Review-Url: https://codereview.chromium.org/2967113002
    Cr-Commit-Position: refs/heads/master@{#484234}

--- third_party/WebKit/Source/core/editing/VisibleSelection.cpp
+++ third_party/WebKit/Source/core/editing/VisibleSelection.cpp
@@ -218,7 +218,7 @@ void VisibleSelectionTemplate<Strategy>::SetBaseAndExtentToDeepEquivalents() {
 }
 
 template <typename Strategy>
-static PositionTemplate<Strategy> ComputeStartRespectingGranularity(
+static PositionTemplate<Strategy> ComputeStartRespectingGranularityAlgorithm(
     const PositionWithAffinityTemplate<Strategy>& passed_start,
     TextGranularity granularity) {
   DCHECK(passed_start.IsNotNull());
@@ -277,8 +277,20 @@ static PositionTemplate<Strategy> ComputeStartRespectingGranularity(
   return passed_start.GetPosition();
 }
 
+static Position ComputeStartRespectingGranularity(
+    const PositionWithAffinity& start,
+    TextGranularity granularity) {
+  return ComputeStartRespectingGranularityAlgorithm(start, granularity);
+}
+
+PositionInFlatTree ComputeStartRespectingGranularity(
+    const PositionInFlatTreeWithAffinity& start,
+    TextGranularity granularity) {
+  return ComputeStartRespectingGranularityAlgorithm(start, granularity);
+}
+
 template <typename Strategy>
-static PositionTemplate<Strategy> ComputeEndRespectingGranularity(
+static PositionTemplate<Strategy> ComputeEndRespectingGranularityAlgorithm(
     const PositionTemplate<Strategy>& start,
     const PositionWithAffinityTemplate<Strategy>& passed_end,
     TextGranularity granularity) {
@@ -391,6 +403,19 @@ static PositionTemplate<Strategy> ComputeEndRespectingGranularity(
   }
   NOTREACHED();
   return passed_end.GetPosition();
+}
+
+static Position ComputeEndRespectingGranularity(const Position& start,
+                                                const PositionWithAffinity& end,
+                                                TextGranularity granularity) {
+  return ComputeEndRespectingGranularityAlgorithm(start, end, granularity);
+}
+
+PositionInFlatTree ComputeEndRespectingGranularity(
+    const PositionInFlatTree& start,
+    const PositionInFlatTreeWithAffinity& end,
+    TextGranularity granularity) {
+  return ComputeEndRespectingGranularityAlgorithm(start, end, granularity);
 }
 
 template <typename Strategy>
