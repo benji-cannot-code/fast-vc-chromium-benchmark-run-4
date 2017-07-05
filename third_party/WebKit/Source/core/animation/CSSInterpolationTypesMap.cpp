12e8e63b6a5868267f0f6006c8fc635850c97419
Alan Cutter <alancutter@chromium.org>
2017-07-05 10:48:40 +0000

    Handle cyclic var() references in animation keyframes
    This change adds a special InterpolationType for when var() cycles
    are detected when animating registered custom properties.
    If either keyframe contains a var() reference that leads to a
    cyclic dependency the property gets unset.
    
    Bug: 671904
    Change-Id: Ifbdfa5c3d708d39365f1e7375b5f0e115d693dc4
    Reviewed-on: https://chromium-review.googlesource.com/542058
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Suzy Howlett <suzyh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484237}

--- third_party/WebKit/Source/core/animation/CSSInterpolationTypesMap.cpp
+++ third_party/WebKit/Source/core/animation/CSSInterpolationTypesMap.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/animation/CSSTransformInterpolationType.h"
 #include "core/animation/CSSTransformOriginInterpolationType.h"
 #include "core/animation/CSSTranslateInterpolationType.h"
+#include "core/animation/CSSVarCycleInterpolationType.h"
 #include "core/animation/CSSVisibilityInterpolationType.h"
 #include "core/css/CSSPropertyMetadata.h"
 #include "core/css/CSSSyntaxDescriptor.h"
@@ -343,6 +344,11 @@ CSSInterpolationTypesMap::CreateInterpolationTypesForCSSSyntax(
     const PropertyRegistration& registration) {
   PropertyHandle property(property_name);
   InterpolationTypes result;
+
+  // All custom properties may encounter var() dependency cycles.
+  result.push_back(
+      WTF::MakeUnique<CSSVarCycleInterpolationType>(property, registration));
+
   for (const CSSSyntaxComponent& component : descriptor.Components()) {
     if (component.repeatable_) {
       // TODO(alancutter): Support animation of repeatable types.
