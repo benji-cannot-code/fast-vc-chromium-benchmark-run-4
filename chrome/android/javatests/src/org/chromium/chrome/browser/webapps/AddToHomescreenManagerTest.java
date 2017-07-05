32bb37c2dedbd8d931e1149bdbdf4198f0653a42
pkotwicz <pkotwicz@chromium.org>
2017-07-05 10:10:54 -0700

    [Android Webapps] Remove unnecessary if() in add_to_homescreen_manager.cc
    The [Enable] dialog button is disabled unless the add-to-homescreen dialog text
    field is non-empty.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2970583002
    Cr-Commit-Position: refs/heads/master@{#484294}

--- chrome/android/javatests/src/org/chromium/chrome/browser/webapps/AddToHomescreenManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webapps/AddToHomescreenManagerTest.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.SmallTest;
+import android.text.TextUtils;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -153,7 +154,11 @@ public class AddToHomescreenManagerTest {
         public void showDialog() {
             AddToHomescreenManager.Observer observer = new AddToHomescreenManager.Observer() {
                 @Override
-                public void onUserTitleAvailable(String title) {}
+                public void onUserTitleAvailable(String title) {
+                    if (TextUtils.isEmpty(mTitle)) {
+                        mTitle = title;
+                    }
+                }
 
                 @Override
                 public void onReadyToAdd(Bitmap icon) {
