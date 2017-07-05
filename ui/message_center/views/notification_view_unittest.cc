b2bddcf4bcb371acff6e0ef3a79a0e0d517c8e34
yoshiki <yoshiki@chromium.org>
2017-07-05 06:46:16 -0700

    Reland of Use shared NotificationControlButtonsView for non-arc notification buttons (patchset #1 id:1 of https://codereview.chromium.org/2970953002/ )
    Reason for revert:
    Let me reland this CL. This should break the ASAN but the fix is ready and I'll commit it soon after this.
    
    Fix CL: https://codereview.chromium.org/2967133002/
    
    Original issue's description:
    > Revert of Use shared NotificationControlButtonsView for non-arc notification buttons (patchset #3 id:60001 of https://codereview.chromium.org/2964973002/ )
    >
    > Reason for revert:
    > Reverting, as this patch caused numerous leaks (see https://bugs.chromium.org/p/chromium/issues/detail?id=739356)
    >
    > Original issue's description:
    > > Use shared NotificationControlButtonsView for non-arc notification buttons
    > >
    > > We recently introduced NotificationControlButtonsView for displaying control buttons. ARC notification already uses it. This CL makes non-ARC notifications use it.
    > >
    > > BUG=717455
    > > TEST=unittest passes
    > >
    > > Review-Url: https://codereview.chromium.org/2964973002
    > > Cr-Commit-Position: refs/heads/master@{#484219}
    > > Committed: https://chromium.googlesource.com/chromium/src/+/12cad72cd90108551c79ebde78ed564832478707
    >
    > TBR=yhanada@chromium.org,yoshiki@chromium.org
    > # Skipping CQ checks because original CL landed less than 1 days ago.
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=717455
    >
    > Review-Url: https://codereview.chromium.org/2970953002
    > Cr-Commit-Position: refs/heads/master@{#484255}
    > Committed: https://chromium.googlesource.com/chromium/src/+/5f296638b446e73d14fb90901448cf117d03669c
    
    TBR=yhanada@chromium.org,glider@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=717455
    
    Review-Url: https://codereview.chromium.org/2966063003
    Cr-Commit-Position: refs/heads/master@{#484261}

--- ui/message_center/views/notification_view_unittest.cc
+++ ui/message_center/views/notification_view_unittest.cc
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/message_center/views/message_center_controller.h"
 #include "ui/message_center/views/message_view_factory.h"
 #include "ui/message_center/views/notification_button.h"
+#include "ui/message_center/views/notification_control_buttons_view.h"
+#include "ui/message_center/views/padded_button.h"
 #include "ui/message_center/views/proportional_image_view.h"
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/layout/fill_layout.h"
@@ -161,8 +163,14 @@ class NotificationViewTest : public views::ViewsTestBase,
     }
   }
 
-  views::ImageButton* GetCloseButton() {
-    return notification_view()->close_button();
+  PaddedButton* GetCloseButton() {
+    return notification_view()
+        ->control_buttons_view_->close_button_for_testing();
+  }
+
+  PaddedButton* GetSettingsButton() {
+    return notification_view()
+        ->control_buttons_view_->settings_button_for_testing();
   }
 
   void UpdateNotificationViews() {
@@ -309,11 +317,16 @@ TEST_F(NotificationViewTest, CreateOrUpdateTest) {
   notification()->set_message(base::ASCIIToUTF16(""));
   notification()->set_icon(gfx::Image());
 
-  notification_view()->CreateOrUpdateViews(*notification());
+  notification_view()->UpdateWithNotification(*notification());
   EXPECT_TRUE(NULL == notification_view()->title_view_);
   EXPECT_TRUE(NULL == notification_view()->message_view_);
   EXPECT_TRUE(NULL == notification_view()->image_view_);
-  EXPECT_TRUE(NULL == notification_view()->settings_button_view_);
+  // Notification must have a control buttons view.
+  EXPECT_TRUE(NULL != notification_view()->control_buttons_view_);
+  // Notification is not pinned and have a close button by default.
+  EXPECT_TRUE(NULL != GetCloseButton());
+  // Notification doesn't have a settings button by default.
+  EXPECT_TRUE(NULL == GetSettingsButton());
   // We still expect an icon view for all layouts.
   EXPECT_TRUE(NULL != notification_view()->icon_view_);
 }
@@ -329,11 +342,12 @@ TEST_F(NotificationViewTest, CreateOrUpdateTestSettingsButton) {
                     NotifierId(NotifierId::APPLICATION, "extension_id"),
                     *data(), delegate.get());
 
-  notification_view()->CreateOrUpdateViews(notf);
+  notification_view()->UpdateWithNotification(notf);
   EXPECT_TRUE(NULL != notification_view()->title_view_);
   EXPECT_TRUE(NULL != notification_view()->message_view_);
   EXPECT_TRUE(NULL != notification_view()->context_message_view_);
-  EXPECT_TRUE(NULL != notification_view()->settings_button_view_);
+  EXPECT_TRUE(NULL != GetCloseButton());
+  EXPECT_TRUE(NULL != GetSettingsButton());
   EXPECT_TRUE(NULL != notification_view()->icon_view_);
 
   EXPECT_TRUE(NULL == notification_view()->image_view_);
@@ -342,21 +356,21 @@ TEST_F(NotificationViewTest, CreateOrUpdateTestSettingsButton) {
 TEST_F(NotificationViewTest, TestLineLimits) {
   notification()->set_image(CreateTestImage(0, 0));
   notification()->set_context_message(base::ASCIIToUTF16(""));
-  notification_view()->CreateOrUpdateViews(*notification());
+  notification_view()->UpdateWithNotification(*notification());
 
   EXPECT_EQ(5, notification_view()->GetMessageLineLimit(0, 360));
   EXPECT_EQ(5, notification_view()->GetMessageLineLimit(1, 360));
   EXPECT_EQ(3, notification_view()->GetMessageLineLimit(2, 360));
 
   notification()->set_image(CreateTestImage(2, 2));
-  notification_view()->CreateOrUpdateViews(*notification());
+  notification_view()->UpdateWithNotification(*notification());
 
   EXPECT_EQ(2, notification_view()->GetMessageLineLimit(0, 360));
   EXPECT_EQ(2, notification_view()->GetMessageLineLimit(1, 360));
   EXPECT_EQ(1, notification_view()->GetMessageLineLimit(2, 360));
 
   notification()->set_context_message(base::ASCIIToUTF16("foo"));
-  notification_view()->CreateOrUpdateViews(*notification());
+  notification_view()->UpdateWithNotification(*notification());
 
   EXPECT_TRUE(notification_view()->context_message_view_ != NULL);
 
@@ -449,7 +463,7 @@ TEST_F(NotificationViewTest, TestImageSizing) {
 
 TEST_F(NotificationViewTest, UpdateButtonsStateTest) {
   notification()->set_buttons(CreateButtons(2));
-  notification_view()->CreateOrUpdateViews(*notification());
+  notification_view()->UpdateWithNotification(*notification());
   widget()->Show();
 
   EXPECT_EQ(views::CustomButton::STATE_NORMAL,
@@ -467,7 +481,7 @@ TEST_F(NotificationViewTest, UpdateButtonsStateTest) {
   EXPECT_EQ(views::CustomButton::STATE_HOVERED,
             notification_view()->action_buttons_[0]->state());
 
-  notification_view()->CreateOrUpdateViews(*notification());
+  notification_view()->UpdateWithNotification(*notification());
 
   EXPECT_EQ(views::CustomButton::STATE_HOVERED,
             notification_view()->action_buttons_[0]->state());
@@ -485,7 +499,7 @@ TEST_F(NotificationViewTest, UpdateButtonsStateTest) {
 
 TEST_F(NotificationViewTest, UpdateButtonCountTest) {
   notification()->set_buttons(CreateButtons(2));
-  notification_view()->CreateOrUpdateViews(*notification());
+  notification_view()->UpdateWithNotification(*notification());
   widget()->Show();
 
   EXPECT_EQ(views::CustomButton::STATE_NORMAL,
@@ -510,7 +524,7 @@ TEST_F(NotificationViewTest, UpdateButtonCountTest) {
             notification_view()->action_buttons_[1]->state());
 
   notification()->set_buttons(CreateButtons(1));
-  notification_view()->CreateOrUpdateViews(*notification());
+  notification_view()->UpdateWithNotification(*notification());
 
   EXPECT_EQ(views::CustomButton::STATE_HOVERED,
             notification_view()->action_buttons_[0]->state());
@@ -537,19 +551,16 @@ TEST_F(NotificationViewTest, SettingsButtonTest) {
                     GURL("https://hello.com"),
                     NotifierId(NotifierId::APPLICATION, "extension_id"),
                     *data(), delegate.get());
-  notification_view()->CreateOrUpdateViews(notf);
+  notification_view()->UpdateWithNotification(notf);
   widget()->Show();
-  notification_view()->Layout();
 
-  EXPECT_TRUE(NULL != notification_view()->settings_button_view_);
-  EXPECT_EQ(views::CustomButton::STATE_NORMAL,
-            notification_view()->settings_button_view_->state());
+  EXPECT_TRUE(NULL != GetSettingsButton());
+  EXPECT_EQ(views::CustomButton::STATE_NORMAL, GetSettingsButton()->state());
 
   // Now construct a mouse move event 1 pixel inside the boundary of the action
   // button.
   gfx::Point cursor_location(1, 1);
-  views::View::ConvertPointToScreen(notification_view()->settings_button_view_,
-                                    &cursor_location);
+  views::View::ConvertPointToScreen(GetSettingsButton(), &cursor_location);
   ui::MouseEvent move(ui::ET_MOUSE_MOVED, cursor_location, cursor_location,
                       ui::EventTimeForNow(), ui::EF_NONE, ui::EF_NONE);
   widget()->OnMouseEvent(&move);
@@ -557,8 +568,7 @@ TEST_F(NotificationViewTest, SettingsButtonTest) {
       views::test::WidgetTest::GetEventSink(widget())->OnEventFromSource(&move);
   EXPECT_FALSE(details.dispatcher_destroyed);
 
-  EXPECT_EQ(views::CustomButton::STATE_HOVERED,
-            notification_view()->settings_button_view_->state());
+  EXPECT_EQ(views::CustomButton::STATE_HOVERED, GetSettingsButton()->state());
 
   // Now construct a mouse move event 1 pixel outside the boundary of the
   // widget.
@@ -567,8 +577,7 @@ TEST_F(NotificationViewTest, SettingsButtonTest) {
                         ui::EventTimeForNow(), ui::EF_NONE, ui::EF_NONE);
   widget()->OnMouseEvent(&move);
 
-  EXPECT_EQ(views::CustomButton::STATE_NORMAL,
-            notification_view()->settings_button_view_->state());
+  EXPECT_EQ(views::CustomButton::STATE_NORMAL, GetSettingsButton()->state());
 }
 
 TEST_F(NotificationViewTest, ViewOrderingTest) {
