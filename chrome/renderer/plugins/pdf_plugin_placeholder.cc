4b38f9bf9c315005b5a516f289258702da34f005
amberwon <amberwon@google.com>
2017-07-05 16:35:06 -0700

    Add DownloadPDF Callback function.
    Added stub for callback function. Will be called in pdf_plugin.html to
    start downloading the PDF. Was tested manually by adding a link that
    calls the function.
    
    BUG=737787
    
    Review-Url: https://codereview.chromium.org/2971753003
    Cr-Commit-Position: refs/heads/master@{#484369}

--- chrome/renderer/plugins/pdf_plugin_placeholder.cc
+++ chrome/renderer/plugins/pdf_plugin_placeholder.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/renderer/plugins/pdf_plugin_placeholder.h"
 
 #include "chrome/grit/renderer_resources.h"
+#include "gin/object_template_builder.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/webui/jstemplate_builder.h"
 
@@ -31,4 +32,15 @@ PDFPluginPlaceholder* PDFPluginPlaceholder::CreatePDFPlaceholder(
 
 v8::Local<v8::Value> PDFPluginPlaceholder::GetV8Handle(v8::Isolate* isolate) {
   return gin::CreateHandle(isolate, this).ToV8();
+}
+
+gin::ObjectTemplateBuilder PDFPluginPlaceholder::GetObjectTemplateBuilder(
+    v8::Isolate* isolate) {
+  return gin::Wrappable<PDFPluginPlaceholder>::GetObjectTemplateBuilder(isolate)
+      .SetMethod<void (PDFPluginPlaceholder::*)()>(
+          "downloadPDF", &PDFPluginPlaceholder::DownloadPDFCallback);
+}
+
+void PDFPluginPlaceholder::DownloadPDFCallback() {
+  // TODO(amberwon): Implement starting PDF download.
 }
