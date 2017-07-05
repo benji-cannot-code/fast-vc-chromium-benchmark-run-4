a3fa859c41d64854285571905cb0f14d2f50e734
gogerald <gogerald@chromium.org>
2017-07-04 22:28:49 -0700

    Implement web payment app manifest
    The web page registering web payment app needs to add
    <link rel="manifest" href="manifest.json">
    in the page to provide the web app manifest.
    
    BUG=735063
    
    Review-Url: https://codereview.chromium.org/2958333002
    Cr-Commit-Position: refs/heads/master@{#484193}

--- content/browser/payments/payment_app_database.h
+++ content/browser/payments/payment_app_database.h
@@ -12,12 +12,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "content/browser/payments/payment_app_info_fetcher.h"
 #include "content/browser/payments/payment_instrument_icon_fetcher.h"
 #include "content/browser/service_worker/service_worker_context_wrapper.h"
 #include "content/browser/service_worker/service_worker_registration.h"
 #include "content/common/content_export.h"
 #include "content/common/service_worker/service_worker_status_code.h"
-#include "content/public/browser/stored_payment_instrument.h"
+#include "content/public/browser/stored_payment_app.h"
 #include "mojo/public/cpp/bindings/binding.h"
 #include "third_party/WebKit/public/platform/modules/payments/payment_app.mojom.h"
 
@@ -27,8 +28,7 @@ class ServiceWorkerRegistration;
 
 class CONTENT_EXPORT PaymentAppDatabase {
  public:
-  using Instruments = std::vector<std::unique_ptr<StoredPaymentInstrument>>;
-  using PaymentApps = std::map<GURL, Instruments>;
+  using PaymentApps = std::map<GURL, std::unique_ptr<StoredPaymentApp>>;
   using ReadAllPaymentAppsCallback = base::OnceCallback<void(PaymentApps)>;
 
   using DeletePaymentInstrumentCallback =
@@ -42,6 +42,8 @@ class CONTENT_EXPORT PaymentAppDatabase {
   using HasPaymentInstrumentCallback =
       base::OnceCallback<void(payments::mojom::PaymentHandlerStatus)>;
   using WritePaymentInstrumentCallback =
+      base::OnceCallback<void(payments::mojom::PaymentHandlerStatus)>;
+  using FetchAndWritePaymentAppInfoCallback =
       base::OnceCallback<void(payments::mojom::PaymentHandlerStatus)>;
   using ClearPaymentInstrumentsCallback =
       base::OnceCallback<void(payments::mojom::PaymentHandlerStatus)>;
@@ -67,12 +69,21 @@ class CONTENT_EXPORT PaymentAppDatabase {
                               const std::string& instrument_key,
                               payments::mojom::PaymentInstrumentPtr instrument,
                               WritePaymentInstrumentCallback callback);
+  void FetchAndWritePaymentAppInfo(
+      const GURL& context,
+      const GURL& scope,
+      FetchAndWritePaymentAppInfoCallback callback);
   void ClearPaymentInstruments(const GURL& scope,
                                ClearPaymentInstrumentsCallback callback);
 
  private:
   // ReadAllPaymentApps callbacks
   void DidReadAllPaymentApps(
+      ReadAllPaymentAppsCallback callback,
+      const std::vector<std::pair<int64_t, std::string>>& raw_data,
+      ServiceWorkerStatusCode status);
+  void DidReadAllPaymentInstruments(
+      PaymentApps apps,
       ReadAllPaymentAppsCallback callback,
       const std::vector<std::pair<int64_t, std::string>>& raw_data,
       ServiceWorkerStatusCode status);
@@ -131,6 +142,21 @@ class CONTENT_EXPORT PaymentAppDatabase {
   void DidWritePaymentInstrument(WritePaymentInstrumentCallback callback,
                                  ServiceWorkerStatusCode status);
 
+  // FetchAndWritePaymentAppInfo callbacks.
+  void FetchPaymentAppInfoCallback(const GURL& scope,
+                                   FetchAndWritePaymentAppInfoCallback callback,
+                                   const std::string& name,
+                                   const std::string& icon);
+  void DidFindRegistrationToWritePaymentAppInfo(
+      FetchAndWritePaymentAppInfoCallback callback,
+      const std::string& name,
+      const std::string& icon,
+      ServiceWorkerStatusCode status,
+      scoped_refptr<ServiceWorkerRegistration> registration);
+  void DidWritePaymentApp(FetchAndWritePaymentAppInfoCallback callback,
+                          bool fetch_app_info_failed,
+                          ServiceWorkerStatusCode status);
+
   // PaymentInstrumentIconFetcherCallback.
   void DidFetchedPaymentInstrumentIcon(
       const GURL& scope,
@@ -146,13 +172,14 @@ class CONTENT_EXPORT PaymentAppDatabase {
       ServiceWorkerStatusCode status,
       scoped_refptr<ServiceWorkerRegistration> registration);
   void DidGetKeysToClearPaymentInstruments(
-      int64_t registration_id,
+      scoped_refptr<ServiceWorkerRegistration> registration,
       ClearPaymentInstrumentsCallback callback,
       const std::vector<std::string>& keys,
       payments::mojom::PaymentHandlerStatus status);
   void DidClearPaymentInstruments(ClearPaymentInstrumentsCallback callback,
                                   ServiceWorkerStatusCode status);
 
+  scoped_refptr<PaymentAppInfoFetcher> payment_app_info_fetcher_;
   scoped_refptr<PaymentInstrumentIconFetcher> instrument_icon_fetcher_;
   scoped_refptr<ServiceWorkerContextWrapper> service_worker_context_;
   base::WeakPtrFactory<PaymentAppDatabase> weak_ptr_factory_;
--- content/public/browser/payment_app_provider.h
+++ content/public/browser/payment_app_provider.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_forward.h"
 #include "content/common/content_export.h"
-#include "content/public/browser/stored_payment_instrument.h"
+#include "content/public/browser/stored_payment_app.h"
 #include "third_party/WebKit/public/platform/modules/payments/payment_app.mojom.h"
 
 namespace content {
@@ -33,9 +33,7 @@ class CONTENT_EXPORT PaymentAppProvider {
   // Please see: content/browser/payments/payment_app_provider_impl.cc
   static PaymentAppProvider* GetInstance();
 
-  using Instruments = std::vector<std::unique_ptr<StoredPaymentInstrument>>;
-  using PaymentApps = std::map<GURL, Instruments>;
-
+  using PaymentApps = std::map<GURL, std::unique_ptr<StoredPaymentApp>>;
   using GetAllPaymentAppsCallback = base::OnceCallback<void(PaymentApps)>;
   using InvokePaymentAppCallback =
       base::Callback<void(payments::mojom::PaymentAppResponsePtr)>;
