485ed3317239d6d2027e77c2c29598aeacfec9bf
tommycli <tommycli@chromium.org>
2017-07-06 11:55:12 -0700

    Omnibox UI Experiments: Strip trivial subdomains
    This revised patch strips the trivial subdomains while preserving the
    registry and domain. It also correctly handles the wwww.foo.com (4 w's),
    which the previous version did not.
    
    Previous version: https://codereview.chromium.org/2939423003/
    
    BUG=732582
    
    Review-Url: https://codereview.chromium.org/2966233002
    Cr-Commit-Position: refs/heads/master@{#484695}

--- components/omnibox/browser/autocomplete_match.cc
+++ components/omnibox/browser/autocomplete_match.cc
@@ -503,6 +503,12 @@ url_formatter::FormatUrlTypes AutocompleteMatch::GetFormatTypes(
           omnibox::kUIExperimentElideSuggestionUrlAfterHost)) {
     format_types |= url_formatter::kFormatUrlExperimentalElideAfterHost;
   }
+
+  if (base::FeatureList::IsEnabled(
+          omnibox::kUIExperimentHideSuggestionUrlTrivialSubdomains)) {
+    format_types |= url_formatter::kFormatUrlExperimentalOmitTrivialSubdomains;
+  }
+
   return format_types;
 }
 
