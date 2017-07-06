f8887fcd8036eb25ab4d9c0fb6f0d3e2c9baa97b
Allen Bauer <kylixrd@chromium.org>
2017-07-06 18:19:59 +0000

    Remove close (X) from javascript unload and reload dialogs.
    Bug: 652017
    Bug: 652015
    Change-Id: I61970a2755ac95d9b19cf648422dce5585ec427c
    Reviewed-on: https://chromium-review.googlesource.com/557941
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484675}

--- components/app_modal/views/javascript_app_modal_dialog_views.cc
+++ components/app_modal/views/javascript_app_modal_dialog_views.cc
@@ -146,6 +146,10 @@ views::View* JavaScriptAppModalDialogViews::GetInitiallyFocusedView() {
   return views::DialogDelegate::GetInitiallyFocusedView();
 }
 
+bool JavaScriptAppModalDialogViews::ShouldShowCloseButton() const {
+  return false;
+}
+
 void JavaScriptAppModalDialogViews::WindowClosing() {
   parent_->OnClose();
 }
