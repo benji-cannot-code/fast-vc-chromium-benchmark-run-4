1a3557bb33333f339b9a32ee32e32bba7bea442d
kbr <kbr@chromium.org>
2017-07-05 21:50:25 -0700

    Revert of Make OS audio buffer size limits visible. (patchset #13 id:240001 of https://codereview.chromium.org/2908073002/ )
    Reason for revert:
    The reason for reverting is: New test is asserting on Linux GPU bots (physical
    machines); see http://crbug.com/708917 .
    
    Original issue's description:
    > Make OS audio buffer size limits visible.
    >
    > Make platform-specific audio buffer size limits visible in limits.h and update AudioLatency::GetExactBufferSize() to allow requesting buffer sizes down to the minimum. Update OSX and CRAS to allow audio buffer sizes below their previous minimums when using GetExactBufferSize(), for use in Web Audio AudioContext creation with a latencyHint.
    >
    > BUG=708917
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    >
    > Review-Url: https://codereview.chromium.org/2908073002
    > Cr-Commit-Position: refs/heads/master@{#484231}
    > Committed: https://chromium.googlesource.com/chromium/src/+/3fe2409358437193a07496c2d0a0b559ef399760
    
    TBR=mkwst@chromium.org,dalecurtis@chromium.org,hongchan@chromium.org,olka@chromium.org,peter@chromium.org,rtoy@chromium.org,andrew.macpherson@soundtrap.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=708917
    
    Review-Url: https://codereview.chromium.org/2973773002
    Cr-Commit-Position: refs/heads/master@{#484467}

--- media/audio/pulse/audio_manager_pulse.cc
+++ media/audio/pulse/audio_manager_pulse.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/audio/pulse/pulse_util.h"
 #include "media/base/audio_parameters.h"
 #include "media/base/channel_layout.h"
-#include "media/base/limits.h"
 
 namespace media {
 
@@ -28,6 +27,10 @@ using pulse::WaitForOperationCompletion;
 
 // Maximum number of output streams that can be open simultaneously.
 static const int kMaxOutputStreams = 50;
+
+// Define bounds for the output buffer size.
+static const int kMinimumOutputBufferSize = 512;
+static const int kMaximumOutputBufferSize = 8192;
 
 // Default input buffer size.
 static const int kDefaultInputBufferSize = 1024;
@@ -162,7 +165,7 @@ AudioParameters AudioManagerPulse::GetPreferredOutputStreamParameters(
   // TODO(tommi): Support |output_device_id|.
   VLOG_IF(0, !output_device_id.empty()) << "Not implemented!";
 
-  int buffer_size = limits::kMinAudioBufferSize;
+  int buffer_size = kMinimumOutputBufferSize;
   int bits_per_sample = 16;
 
   // Query native parameters where applicable; Pulse does not require these to
@@ -175,7 +178,7 @@ AudioParameters AudioManagerPulse::GetPreferredOutputStreamParameters(
     bits_per_sample = input_params.bits_per_sample();
     channel_layout = input_params.channel_layout();
     buffer_size =
-        std::min(static_cast<int>(limits::kMaxAudioBufferSize),
+        std::min(kMaximumOutputBufferSize,
                  std::max(buffer_size, input_params.frames_per_buffer()));
   }
 
