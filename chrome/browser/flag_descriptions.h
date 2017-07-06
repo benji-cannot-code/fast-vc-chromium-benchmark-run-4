ea3e8cbe6ea96f5e0562195a297a2f6bbd856ea4
Cathy Li <chili@chromium.org>
2017-07-06 03:27:08 +0000

    [Offline pages] Feature flags for custom tabs V2 API and page origin
    Bug: 734753
    Change-Id: Ida756ec7f3daf4d325dfcce1e0d1ea4d48f8d77e
    Reviewed-on: https://chromium-review.googlesource.com/553077
    Commit-Queue: Cathy Li <chili@chromium.org>
    Reviewed-by: Dmitry Titov <dimich@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484454}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1068,6 +1068,9 @@ extern const char kOfflinePagesAsyncDownloadDescription[];
 extern const char kOfflinePagesCtName[];
 extern const char kOfflinePagesCtDescription[];
 
+extern const char kOfflinePagesCtV2Name[];
+extern const char kOfflinePagesCtV2Description[];
+
 extern const char kOfflinePagesLoadSignalCollectingName[];
 extern const char kOfflinePagesLoadSignalCollectingDescription[];
 
