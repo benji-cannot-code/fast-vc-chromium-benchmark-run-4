574f66a0967f30ddc8c22401ea1daba295550f5c
irmakk <irmakk@google.com>
2017-07-06 09:45:01 -0700

    Edit button makes username editable in the password manager bubble.
    When user clicks on the edit button in the password manager bubble;
     - Username becomes editable
     - Focus is on editable field
     - Edit button becomes disabled
    
    While username is editable, if user hits tab/enter/esc keys;
     - Username field becomes a label again
     - Focus is on save button
     - Edit button is enabled again
    
    Known issue:
     Since this cl doesn't contain any communication with the username in the background and currently only has the ui part of the project, edited username will be restored to its original state after editing is done. This problem will be solved with the next cl sending the information to back-end and communication with the manager itself.
    
    BUG=734965
    
    Review-Url: https://codereview.chromium.org/2960843002
    Cr-Commit-Position: refs/heads/master@{#484635}

--- chrome/browser/ui/views/passwords/manage_password_items_view.cc
+++ chrome/browser/ui/views/passwords/manage_password_items_view.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/harmony/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/harmony/chrome_typography.h"
 #include "chrome/grit/generated_resources.h"
+#include "components/password_manager/core/common/password_manager_ui.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/resources/grit/ui_resources.h"
@@ -23,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/label.h"
 #include "ui/views/controls/link.h"
 #include "ui/views/controls/link_listener.h"
+#include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/layout/grid_layout.h"
 
@@ -71,29 +73,6 @@ void BuildColumnSetIfNeeded(views::GridLayout* layout, int column_set_id) {
   }
 }
 
-std::unique_ptr<views::Label> GenerateUsernameLabel(
-    const autofill::PasswordForm& form) {
-  auto label = base::MakeUnique<views::Label>(GetDisplayUsername(form),
-                                              CONTEXT_DEPRECATED_SMALL);
-  label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
-  return label;
-}
-
-std::unique_ptr<views::Label> GeneratePasswordLabel(
-    const autofill::PasswordForm& form) {
-  base::string16 text =
-      form.federation_origin.unique()
-          ? form.password_value
-          : l10n_util::GetStringFUTF16(
-                IDS_PASSWORDS_VIA_FEDERATION,
-                base::UTF8ToUTF16(form.federation_origin.host()));
-  auto label = base::MakeUnique<views::Label>(text, CONTEXT_DEPRECATED_SMALL);
-  label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
-  if (form.federation_origin.unique())
-    label->SetObscured(true);
-  return label;
-}
-
 std::unique_ptr<views::ImageButton> GenerateDeleteButton(
     views::ButtonListener* listener) {
   ui::ResourceBundle* rb = &ui::ResourceBundle::GetSharedInstance();
@@ -130,6 +109,36 @@ std::unique_ptr<views::Link> GenerateUndoLink(views::LinkListener* listener) {
 }
 
 }  // namespace
+
+std::unique_ptr<views::Label> GenerateUsernameLabel(
+    const autofill::PasswordForm& form) {
+  auto label = base::MakeUnique<views::Label>(GetDisplayUsername(form),
+                                              CONTEXT_DEPRECATED_SMALL);
+  label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
+  return label;
+}
+
+std::unique_ptr<views::Textfield> GenerateUsernameEditable(
+    const autofill::PasswordForm& form) {
+  auto editable = base::MakeUnique<views::Textfield>();
+  editable->SetText(form.username_value);
+  return editable;
+}
+
+std::unique_ptr<views::Label> GeneratePasswordLabel(
+    const autofill::PasswordForm& form) {
+  base::string16 text =
+      form.federation_origin.unique()
+          ? form.password_value
+          : l10n_util::GetStringFUTF16(
+                IDS_PASSWORDS_VIA_FEDERATION,
+                base::UTF8ToUTF16(form.federation_origin.host()));
+  auto label = base::MakeUnique<views::Label>(text, CONTEXT_DEPRECATED_SMALL);
+  label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
+  if (form.federation_origin.unique())
+    label->SetObscured(true);
+  return label;
+}
 
 // Manage credentials: stores credentials state and adds proper row to layout
 // based on credential state.
