f39e2a22c74848cfb04fd52208abea9d4911e459
Andrey Kosyakov <caseq@chromium.org>
2017-07-06 17:38:19 +0000

    Fix DCHECKs and a crash in DevToolsFileSystemIndexer
    - make FileSystemIndexingJob RefCountedThreadSafe
    - add missing static on the LazyTaskRunner;
    - post task rather than call when proceeding to next file;
    - add tests.
    
    Bug: 737748
    Change-Id: I28a63693629c087781fd6dc031ae202c236fd975
    Reviewed-on: https://chromium-review.googlesource.com/553958
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484654}

--- chrome/browser/devtools/devtools_file_system_indexer.cc
+++ chrome/browser/devtools/devtools_file_system_indexer.cc
@@ -44,9 +44,9 @@ namespace {
 base::SequencedTaskRunner* impl_task_runner() {
   constexpr base::TaskTraits kBlockingTraits = {base::MayBlock(),
                                                 base::TaskPriority::BACKGROUND};
-  base::LazySequencedTaskRunner g_sequenced_task_task_runner =
+  static base::LazySequencedTaskRunner s_sequenced_task_task_runner =
       LAZY_SEQUENCED_TASK_RUNNER_INITIALIZER(kBlockingTraits);
-  return g_sequenced_task_task_runner.Get().get();
+  return s_sequenced_task_task_runner.Get().get();
 }
 
 typedef int32_t Trigram;
@@ -383,7 +383,8 @@ void DevToolsFileSystemIndexer::FileSystemIndexingJob::FinishFileIndexing(
   }
   ReportWorked();
   ++indexing_it_;
-  IndexFiles();
+  impl_task_runner()->PostTask(
+      FROM_HERE, base::BindOnce(&FileSystemIndexingJob::IndexFiles, this));
 }
 
 void DevToolsFileSystemIndexer::FileSystemIndexingJob::CloseFile() {
