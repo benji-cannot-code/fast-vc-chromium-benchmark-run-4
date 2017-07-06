88d64f8e76c08b87a424ede4cbbb98a1c7992551
Allen Bauer <kylixrd@chromium.org>
2017-07-06 19:20:08 +0000

    Use the LayoutProvider to supply the default checkbox/radiobutton borders.
    Bug: 652017
    Bug: 652015
    Change-Id: I40188d8827bd32df8eb152861ebf9f422234f803
    Reviewed-on: https://chromium-review.googlesource.com/555834
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484705}

--- ui/views/controls/button/checkbox.cc
+++ ui/views/controls/button/checkbox.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/animation/ink_drop_impl.h"
 #include "ui/views/animation/ink_drop_ripple.h"
 #include "ui/views/controls/button/label_button_border.h"
+#include "ui/views/layout/layout_provider.h"
 #include "ui/views/painter.h"
 #include "ui/views/resources/grit/views_resources.h"
 #include "ui/views/style/platform_style.h"
@@ -192,6 +193,14 @@ gfx::ImageSkia Checkbox::GetImage(ButtonState for_state) const {
       images_[checked_index][focused_index][for_state].isNull())
     return images_[checked_index][focused_index][STATE_NORMAL];
   return images_[checked_index][focused_index][for_state];
+}
+
+std::unique_ptr<LabelButtonBorder> Checkbox::CreateDefaultBorder() const {
+  std::unique_ptr<LabelButtonBorder> border =
+      LabelButton::CreateDefaultBorder();
+  border->set_insets(
+      LayoutProvider::Get()->GetInsetsMetric(INSETS_CHECKBOX_RADIO_BUTTON));
+  return border;
 }
 
 void Checkbox::SetCustomImage(bool checked,
