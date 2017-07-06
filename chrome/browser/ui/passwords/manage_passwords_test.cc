e0bdd9fd4e49e54acd51b378d5caff0c7559b529
Vaclav Brozek <vabr@chromium.org>
2017-07-06 22:50:28 +0000

    Reland: Separate PasswordFormManager::metrics_recorder_ init from constructor
    Why the original failed on debug builds:
    There were two more places where Init was forgotten after a PasswordFormManager
    creation. Those were only discovered by debug builds, which are not run on the
    pre-submitting trybots.
    
    Patch set 1 is the original, patch set 2 is the fix.
    
    Original description:
    -----------------------
    Until now, PasswordFormManager created its metrics recorder in its constructor.
    However, soon we will need to be able to inject a reference to an existing
    recorder instead.
    
    There are at least two ways to do that:
      (1) Inject it through a constructor argument, or
      (2) Inject it through a separate method
    
    Case (1) allows to emulate the current state completely, but causes a lot of
    churn in updating all the PasswordFormManager construction sites (mostly in
    tests).
    
    Case (2) makes it impossible to record events happening in the constructor, but
    causes just a handful of changes through the code.
    
    Because there are no events logged during construction, and it is unlikely that
    there will be need to have them in the future, (2) is implemented by this CL.
    
    Note that no "dummy" recorder is constructed, because it would upload confusing
    metrics. Therefore, the PasswordFormManager code needs to handle the null
    recorder gracefully.
    
    An Init() method is added, it takes a scoped_refptr to a metrics recorder. If
    the pointer is null, it creates a fresh recorder, otherwise starts sharing
    ownership of the passed recorder. The Init() method also does some other work
    previously done by the constructor, because (1) that work might result in
    needing the recorder, and (2) it is good practice not to do heavy lifting
    during construction, when the object may not be fully working.
    -----------------------
    
    TBR=battre@chromium.org
    
    Bug: 725883
    Change-Id: I92ef32a9ee312774dfba64611ddf00d06a77c6ca
    Reviewed-on: https://chromium-review.googlesource.com/562477
    Reviewed-by: Vaclav Brozek <vabr@chromium.org>
    Commit-Queue: Vaclav Brozek <vabr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484759}

--- chrome/browser/ui/passwords/manage_passwords_test.cc
+++ chrome/browser/ui/passwords/manage_passwords_test.cc
@@ -72,6 +72,7 @@ void ManagePasswordsTest::SetupPendingPassword() {
       new password_manager::PasswordFormManager(
           nullptr, &client_, driver_.AsWeakPtr(), *test_form(),
           base::WrapUnique(new password_manager::StubFormSaver), &fetcher_));
+  test_form_manager->Init(nullptr);
   fetcher_.SetNonFederated(std::vector<const autofill::PasswordForm*>(), 0u);
   GetController()->OnPasswordSubmitted(std::move(test_form_manager));
 }
@@ -81,6 +82,7 @@ void ManagePasswordsTest::SetupAutomaticPassword() {
       new password_manager::PasswordFormManager(
           nullptr, &client_, driver_.AsWeakPtr(), *test_form(),
           base::WrapUnique(new password_manager::StubFormSaver), &fetcher_));
+  test_form_manager->Init(nullptr);
   fetcher_.SetNonFederated(std::vector<const autofill::PasswordForm*>(), 0u);
   GetController()->OnAutomaticPasswordSave(std::move(test_form_manager));
 }
