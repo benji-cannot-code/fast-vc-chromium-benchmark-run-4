497c6bfaa9e5e4c194b17cd94eba81e7a2813871
Alexei Filippov <alph@chromium.org>
2017-07-06 21:21:42 +0000

    DevTools: Fix race in StackTraceTopFrameLocation
    There was a race between dispose being called while an update promise is in flight
    causing a failure.
    
    Change-Id: I33300d0915a9accfb9a7df4bfdc7d968dc7584ff
    Reviewed-on: https://chromium-review.googlesource.com/562222
    Reviewed-by: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484743}

--- third_party/WebKit/Source/devtools/front_end/bindings/DebuggerWorkspaceBinding.js
+++ third_party/WebKit/Source/devtools/front_end/bindings/DebuggerWorkspaceBinding.js
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 /**
  * @unrestricted
  * @implements {SDK.SDKModelObserver<!SDK.DebuggerModel>}
@@ -382,9 +383,6 @@ Bindings.DebuggerWorkspaceBinding.Location = class extends Bindings.LiveLocation
   }
 };
 
-/**
- * @unrestricted
- */
 Bindings.DebuggerWorkspaceBinding.StackTraceTopFrameLocation = class extends Bindings.LiveLocationWithPool {
   /**
    * @param {!Array<!SDK.DebuggerModel.Location>} rawLocations
@@ -394,12 +392,10 @@ Bindings.DebuggerWorkspaceBinding.StackTraceTopFrameLocation = class extends Bin
    */
   constructor(rawLocations, binding, updateDelegate, locationPool) {
     super(updateDelegate, locationPool);
-
     this._updateScheduled = true;
-    /** @type {!Set<!Bindings.LiveLocation>} */
-    this._locations = new Set();
-    for (var location of rawLocations)
-      this._locations.add(binding.createLiveLocation(location, this._scheduleUpdate.bind(this), locationPool));
+    this._current = null;
+    this._locations = rawLocations.map(
+        location => binding.createLiveLocation(location, this._scheduleUpdate.bind(this), locationPool));
     this._updateLocation();
   }
 
@@ -426,24 +422,22 @@ Bindings.DebuggerWorkspaceBinding.StackTraceTopFrameLocation = class extends Bin
     super.dispose();
     for (var location of this._locations)
       location.dispose();
+    this._locations = null;
+    this._current = null;
   }
 
   _scheduleUpdate() {
-    if (!this._updateScheduled) {
-      this._updateScheduled = true;
-      setImmediate(this._updateLocation.bind(this));
-    }
+    if (this._updateScheduled)
+      return;
+    this._updateScheduled = true;
+    setImmediate(this._updateLocation.bind(this));
   }
 
   _updateLocation() {
     this._updateScheduled = false;
-    this._current = this._locations.values().next().value;
-    for (var current of this._locations) {
-      if (!current.isBlackboxed()) {
-        this._current = current;
-        break;
-      }
-    }
+    if (!this._locations)
+      return;
+    this._current = this._locations.find(location => !location.isBlackboxed()) || this._locations[0];
     this.update();
   }
 };
