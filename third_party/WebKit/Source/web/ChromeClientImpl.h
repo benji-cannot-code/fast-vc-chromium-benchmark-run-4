e0db6c8a3fddf0e56f018c6b9410c65faf9d55c6
David Bokan <bokan@chromium.org>
2017-07-06 21:30:17 +0000

    Allow testing browser controls with rootScroller
    This patch is split off from https://crrev.com/c/516554/. It prepares
    the testing infrastructure for follow up patches and should make it
    cleaner to test browser control behavior from root scroller tests.
    
    All rootScroller related tests have been moved into their own directory
    and we add a new virtual test suite using the Android set of flags for
    the rootScroller tests.
    
    I've also added an internal method for controlling the state of
    browser controls as far as Blink can tell. This will be used from
    followup patches to test behavior with the browser controls in various
    states.
    
    Bug: 505516
    Change-Id: Iaed2d03c0e8aae4800644649bc4d8e7fce691cdc
    Reviewed-on: https://chromium-review.googlesource.com/548975
    Commit-Queue: David Bokan <bokan@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484745}

--- third_party/WebKit/Source/web/ChromeClientImpl.h
+++ third_party/WebKit/Source/web/ChromeClientImpl.h
@@ -184,6 +184,8 @@ class WEB_EXPORT ChromeClientImpl final : public ChromeClient {
   void ClosePagePopup(PagePopup*) override;
   DOMWindow* PagePopupWindowForTesting() const override;
 
+  void SetBrowserControlsState(float height, bool shrinks_layout) override;
+
   bool ShouldOpenModalDialogDuringPageDismissal(
       LocalFrame&,
       DialogType,
