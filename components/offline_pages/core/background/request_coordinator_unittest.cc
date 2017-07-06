9c687aceedc8e9214bc99fe9e5d431599ff82b20
Cathy Li <chili@chromium.org>
2017-07-06 20:37:14 +0000

    [Offline pages] Propagate request_origin to SavePageParams; Add request_origin to SavePageLaterParams
    Bug: 734753
    Change-Id: I1ff0cff1d11248dc048c18111519eef24aa7ed3c
    Reviewed-on: https://chromium-review.googlesource.com/553757
    Commit-Queue: Cathy Li <chili@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484733}

--- components/offline_pages/core/background/request_coordinator_unittest.cc
+++ components/offline_pages/core/background/request_coordinator_unittest.cc
@@ -53,6 +53,7 @@ const int kMaxCompletedTries = 3;
 const bool kPowerRequired = true;
 const bool kUserRequested = true;
 const int kAttemptCount = 1;
+const std::string kRequestOrigin("abc.xyz");
 }  // namespace
 
 class ObserverStub : public RequestCoordinator::Observer {
@@ -267,6 +268,7 @@ class RequestCoordinatorTest : public testing::Test {
     params.url = kUrl1;
     params.client_id = kClientId1;
     params.user_requested = kUserRequested;
+    params.request_origin = kRequestOrigin;
     return coordinator()->SavePageLater(params);
   }
 
@@ -277,6 +279,7 @@ class RequestCoordinatorTest : public testing::Test {
     params.client_id = kClientId1;
     params.user_requested = kUserRequested;
     params.availability = availability;
+    params.request_origin = kRequestOrigin;
     return coordinator()->SavePageLater(params);
   }
 
@@ -565,6 +568,7 @@ TEST_F(RequestCoordinatorTest, SavePageLater) {
   params.url = kUrl1;
   params.client_id = kClientId1;
   params.original_url = kUrl2;
+  params.request_origin = kRequestOrigin;
   EXPECT_NE(0, coordinator()->SavePageLater(params));
 
   // Expect that a request got placed on the queue.
@@ -584,6 +588,7 @@ TEST_F(RequestCoordinatorTest, SavePageLater) {
   EXPECT_EQ(kClientId1, last_requests().at(0)->client_id());
   EXPECT_TRUE(last_requests().at(0)->user_requested());
   EXPECT_EQ(kUrl2, last_requests().at(0)->original_url());
+  EXPECT_EQ(kRequestOrigin, last_requests().at(0)->request_origin());
 
   // Expect that the scheduler got notified.
   SchedulerStub* scheduler_stub =
