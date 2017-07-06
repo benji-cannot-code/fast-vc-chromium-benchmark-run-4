d873ceb1967edd682a0dbe9973e701a8a75a6dfb
Jeremy Roman <jbroman@chromium.org>
2017-07-06 19:09:36 +0000

    Replace base::AtomicRefCount functions with member functions.
    Also, the use of AtomicRefCount to check for cross-thread use of non-atomic
    RefCounted is suspect (the guarantees provided by AtomicRefCount aren't
    obviously strong enough for this use). Since this is only used for DCHECKs,
    it seems simplest to have it use sequentially-consistent atomics and not
    think about it further.
    
    Bug: 736037
    Tbr: sky@chromium.org
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I1613e5f39fa814ceef3cec5bd25806179b65c978
    Reviewed-on: https://chromium-review.googlesource.com/556014
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484700}

--- base/memory/ref_counted.cc
+++ base/memory/ref_counted.cc
@@ -11,7 +11,7 @@ namespace base {
 namespace {
 
 #if DCHECK_IS_ON()
-AtomicRefCount g_cross_thread_ref_count_access_allow_count(0);
+std::atomic_int g_cross_thread_ref_count_access_allow_count(0);
 #endif
 
 }  // namespace
@@ -19,7 +19,7 @@ AtomicRefCount g_cross_thread_ref_count_access_allow_count(0);
 namespace subtle {
 
 bool RefCountedThreadSafeBase::HasOneRef() const {
-  return AtomicRefCountIsOne(&ref_count_);
+  return ref_count_.IsOne();
 }
 
 RefCountedThreadSafeBase::~RefCountedThreadSafeBase() {
@@ -41,7 +41,7 @@ void RefCountedThreadSafeBase::AddRef() const {
 #if DCHECK_IS_ON()
 bool RefCountedBase::CalledOnValidSequence() const {
   return sequence_checker_.CalledOnValidSequence() ||
-         !AtomicRefCountIsZero(&g_cross_thread_ref_count_access_allow_count);
+         g_cross_thread_ref_count_access_allow_count.load() != 0;
 }
 #endif
 
@@ -49,11 +49,11 @@ bool RefCountedBase::CalledOnValidSequence() const {
 
 #if DCHECK_IS_ON()
 ScopedAllowCrossThreadRefCountAccess::ScopedAllowCrossThreadRefCountAccess() {
-  AtomicRefCountInc(&g_cross_thread_ref_count_access_allow_count);
+  ++g_cross_thread_ref_count_access_allow_count;
 }
 
 ScopedAllowCrossThreadRefCountAccess::~ScopedAllowCrossThreadRefCountAccess() {
-  AtomicRefCountDec(&g_cross_thread_ref_count_access_allow_count);
+  --g_cross_thread_ref_count_access_allow_count;
 }
 #endif
 
