1ef27ce7b10538f55df7e857edc4187058a18e7c
yiyix <yiyix@chromium.org>
2017-07-06 00:36:40 +0000

    Fix bug: Cursor overlaps with text in omnibox when window is dragged
    The cursor location used to get updated after the dragging event is over, 
    so the cursor shows in the previous location for some time before jumping 
    to the new location as shown in the bug. In this fix, Textfield::OnBoundsChanged 
    will trigger location updating for text cursor as well. 
    
    TEXT=TextfieldTest.TextfieldBoundsChangeTest
    
    Bug: 734988
    Change-Id: Idbc05e59b8b97f76ec9d70b5c99b887e69798fda
    Reviewed-on: https://chromium-review.googlesource.com/559591
    Commit-Queue: Yi Xu <yiyix@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484422}

--- ui/views/controls/textfield/textfield.cc
+++ ui/views/controls/textfield/textfield.cc
@@ -971,6 +971,7 @@ void Textfield::OnBoundsChanged(const gfx::Rect& previous_bounds) {
   bounds.Inset(gfx::Insets(0, kTextPadding, 0, kTextPadding));
   GetRenderText()->SetDisplayRect(bounds);
   OnCaretBoundsChanged();
+  UpdateCursorViewPosition();
 }
 
 bool Textfield::GetNeedsNotificationWhenVisibleBoundsChange() const {
@@ -2116,8 +2117,8 @@ void Textfield::StopBlinkingCursor() {
 
 void Textfield::OnCursorBlinkTimerFired() {
   DCHECK(ShouldBlinkCursor());
-  cursor_view_.SetVisible(!cursor_view_.visible());
   UpdateCursorViewPosition();
+  cursor_view_.SetVisible(!cursor_view_.visible());
 }
 
 }  // namespace views
