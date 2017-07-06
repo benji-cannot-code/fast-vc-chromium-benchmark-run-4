2a1e07736c1d8bf7743d475d51affa80ffcad5b1
takumif <takumif@chromium.org>
2017-07-06 11:37:46 -0700

    [Media Router] Factor extension-related logic out of MediaRouterMojoImpl
    We take the component-extension-waking and request-queueing logic out of
    MediaRouterMojoImpl and put it in its own class, EventPageRequestManager. In a
    future patch the part of MediaRouterMojoImpl that interfaces with the request
    manager will be further refactored into its own class, as described in the
    design doc below.
    
    Currently MediaRouterMojoImpl is the only client of the request manager, but the
    MediaRouteController will also use it in the future (see design doc).
    
    Design doc:
    https://docs.google.com/document/d/1PEcNLc9TTaRbeC1jZwVX7E3Rm3bJrqV8049cKXQ7Yho/edit
    
    BUG=737320
    BUG=739516
    
    Review-Url: https://codereview.chromium.org/2949933002
    Cr-Commit-Position: refs/heads/master@{#484688}

--- chrome/browser/media/router/media_router_factory.cc
+++ chrome/browser/media/router/media_router_factory.cc
@@ -13,9 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_ANDROID)
 #include "chrome/browser/media/android/router/media_router_android.h"
 #else
+#include "chrome/browser/media/router/event_page_request_manager_factory.h"
 #include "chrome/browser/media/router/mojo/media_router_mojo_impl.h"
-#include "extensions/browser/process_manager.h"
-#include "extensions/browser/process_manager_factory.h"
 #endif
 
 using content::BrowserContext;
@@ -60,8 +59,7 @@ MediaRouterFactory::MediaRouterFactory()
           "MediaRouter",
           BrowserContextDependencyManager::GetInstance()) {
 #if !defined(OS_ANDROID)
-  // On desktop platforms, MediaRouter depends on ProcessManager.
-  DependsOn(extensions::ProcessManagerFactory::GetInstance());
+  DependsOn(EventPageRequestManagerFactory::GetInstance());
 #endif
 }
 
@@ -79,8 +77,7 @@ KeyedService* MediaRouterFactory::BuildServiceInstanceFor(
 #if defined(OS_ANDROID)
   media_router = new MediaRouterAndroid(context);
 #else
-  media_router = new MediaRouterMojoImpl(
-      extensions::ProcessManager::Get(context), context);
+  media_router = new MediaRouterMojoImpl(context);
 #endif
   media_router->Initialize();
   return media_router;
