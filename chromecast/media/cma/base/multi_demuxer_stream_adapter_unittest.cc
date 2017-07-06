129ed25e95cb9a51579cfec11f47ee13f40e172e
avi <avi@chromium.org>
2017-07-06 14:04:30 -0700

    Remove ScopedVector from chromecast/.
    BUG=554289
    
    Review-Url: https://codereview.chromium.org/2976433002
    Cr-Commit-Position: refs/heads/master@{#484740}

--- chromecast/media/cma/base/multi_demuxer_stream_adapter_unittest.cc
+++ chromecast/media/cma/base/multi_demuxer_stream_adapter_unittest.cc
@@ -4,12 +4,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include <memory>
+#include <vector>
 
 #include "base/bind.h"
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/ref_counted.h"
-#include "base/memory/scoped_vector.h"
 #include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
@@ -59,9 +59,9 @@ class MultiDemuxerStreamAdaptersTest : public testing::Test {
   // List of expected frame indices with decoder config changes.
   std::list<int> config_idx_;
 
-  ScopedVector<DemuxerStreamForTest> demuxer_streams_;
+  std::vector<std::unique_ptr<DemuxerStreamForTest>> demuxer_streams_;
 
-  ScopedVector<CodedFrameProvider> coded_frame_providers_;
+  std::vector<std::unique_ptr<CodedFrameProvider>> coded_frame_providers_;
 
  private:
   // exit if all of the streams end
@@ -93,22 +93,22 @@ void MultiDemuxerStreamAdaptersTest::Start() {
   coded_frame_providers_.clear();
   frame_received_count_ = 0;
 
-  for (auto* stream : demuxer_streams_) {
+  for (const auto& stream : demuxer_streams_) {
     coded_frame_providers_.push_back(base::MakeUnique<DemuxerStreamAdapter>(
         base::ThreadTaskRunnerHandle::Get(), media_task_runner_factory_,
-        stream));
+        stream.get()));
   }
   running_stream_count_ = coded_frame_providers_.size();
 
   // read each stream
-  for (auto* code_frame_provider : coded_frame_providers_) {
-    auto read_cb = base::Bind(&MultiDemuxerStreamAdaptersTest::OnNewFrame,
-                              base::Unretained(this),
-                              code_frame_provider);
+  for (const auto& code_frame_provider : coded_frame_providers_) {
+    auto read_cb =
+        base::Bind(&MultiDemuxerStreamAdaptersTest::OnNewFrame,
+                   base::Unretained(this), code_frame_provider.get());
 
-    base::Closure task = base::Bind(&CodedFrameProvider::Read,
-                                    base::Unretained(code_frame_provider),
-                                    read_cb);
+    base::Closure task =
+        base::Bind(&CodedFrameProvider::Read,
+                   base::Unretained(code_frame_provider.get()), read_cb);
 
     base::ThreadTaskRunnerHandle::Get()->PostTask(FROM_HERE, task);
   }
@@ -155,10 +155,10 @@ TEST_F(MultiDemuxerStreamAdaptersTest, EarlyEos) {
       frame_count_short +
       kMaxPtsDiffMs / DemuxerStreamForTest::kDemuxerStreamForTestFrameDuration +
       100;
-  demuxer_streams_.push_back(std::unique_ptr<DemuxerStreamForTest>(
-      new DemuxerStreamForTest(frame_count_short, 2, 0, config_idx_)));
-  demuxer_streams_.push_back(std::unique_ptr<DemuxerStreamForTest>(
-      new DemuxerStreamForTest(frame_count_long, 10, 0, config_idx_)));
+  demuxer_streams_.push_back(base::MakeUnique<DemuxerStreamForTest>(
+      frame_count_short, 2, 0, config_idx_));
+  demuxer_streams_.push_back(base::MakeUnique<DemuxerStreamForTest>(
+      frame_count_long, 10, 0, config_idx_));
 
   total_expected_frames_ = frame_count_short + frame_count_long;
 
