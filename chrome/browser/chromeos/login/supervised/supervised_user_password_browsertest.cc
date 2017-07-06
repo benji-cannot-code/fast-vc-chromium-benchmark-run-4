588eb6383974f42faec3ee774905b412b9b8d932
Markus Heintz <markusheintz@chromium.org>
2017-07-06 13:55:27 +0000

    Move DCHECK for verifying that Supervised Users creation is enabled to the beginning of the SupervisedUserRegistrationUtility::Create method.
    Bug: 736042
    Change-Id: I1c363f21f6e78a92e1eb7276470565f622d0d20f
    Reviewed-on: https://chromium-review.googlesource.com/558355
    Commit-Queue: Markus Heintz <markusheintz@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484572}

--- chrome/browser/chromeos/login/supervised/supervised_user_password_browsertest.cc
+++ chrome/browser/chromeos/login/supervised/supervised_user_password_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/threading/sequenced_worker_pool.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/chromeos/login/login_manager_test.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/net/network_portal_detector_test_impl.h"
 #include "chrome/browser/chromeos/settings/stub_cros_settings_provider.h"
 #include "chrome/browser/supervised_user/supervised_user_constants.h"
+#include "chrome/common/chrome_features.h"
 #include "chromeos/cryptohome/mock_async_method_caller.h"
 #include "chromeos/cryptohome/mock_homedir_methods.h"
 #include "components/sync/model/attachments/attachment_service_proxy_for_test.h"
@@ -44,7 +46,15 @@ class SupervisedUserPasswordTest : public SupervisedUserTestBase {
  public:
   SupervisedUserPasswordTest() : SupervisedUserTestBase() {}
 
+  void SetUpInProcessBrowserTestFixture() override {
+    SupervisedUserTestBase::SetUpInProcessBrowserTestFixture();
+    scoped_feature_list_.InitAndEnableFeature(
+        features::kSupervisedUserCreation);
+  }
+
  private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+
   DISALLOW_COPY_AND_ASSIGN(SupervisedUserPasswordTest);
 };
 
