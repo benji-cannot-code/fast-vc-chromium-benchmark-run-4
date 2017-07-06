bba081ee534f32cd068c202500d151430e1b4c07
Anton Vayvod <avayvod@google.com>
2017-07-06 20:54:15 +0000

    Disable failing PersonalDataManager tests
    Bug:739575
    Change-Id: Ia0c4d2f84ac21ecca179a8f40a05d70761b1d735
    
    NOTREECHECKS=1
    NOTRY=1
    TBR=ellyjones
    
    Change-Id: Ia0c4d2f84ac21ecca179a8f40a05d70761b1d735
    Reviewed-on: https://chromium-review.googlesource.com/562198
    Commit-Queue: Anton Vayvod <avayvod@chromium.org>
    Reviewed-by: Anton Vayvod <avayvod@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484738}

--- chrome/android/javatests/src/org/chromium/chrome/browser/autofill/PersonalDataManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/autofill/PersonalDataManagerTest.java
@@ -15,6 +15,7 @@ import org.junit.rules.RuleChain;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.browser.autofill.PersonalDataManager.AutofillProfile;
@@ -121,9 +122,12 @@ public class PersonalDataManagerTest {
     }
 
     @Test
+    /*
     @SmallTest
     @Feature({"Autofill"})
     @RetryOnFailure
+    */
+    @DisabledTest(message = "crbug.com/739575")
     public void testAddAndDeleteProfile()
             throws InterruptedException, ExecutionException, TimeoutException {
         String profileOneGUID = mHelper.setProfile(createTestProfile());
@@ -258,8 +262,11 @@ public class PersonalDataManagerTest {
     }
 
     @Test
+    /*
     @SmallTest
     @Feature({"Autofill"})
+    */
+    @DisabledTest(message = "crbug.com/739575")
     public void testLabels() throws InterruptedException, ExecutionException, TimeoutException {
         AutofillProfile profile1 = new AutofillProfile(
                  "" /* guid */, "https://www.example.com" /* origin */,
@@ -311,8 +318,11 @@ public class PersonalDataManagerTest {
     }
 
     @Test
+    /*
     @SmallTest
     @Feature({"Autofill"})
+    */
+    @DisabledTest(message = "crbug.com/739575")
     public void testProfilesFrecency()
             throws InterruptedException, ExecutionException, TimeoutException {
         // Create 3 profiles.
@@ -438,9 +448,12 @@ public class PersonalDataManagerTest {
     }
 
     @Test
+    /*
     @SmallTest
     @Feature({"Autofill"})
     @RetryOnFailure
+    */
+    @DisabledTest(message = "crbug.com/739575")
     public void testCreditCardUseStatsSettingAndGetting()
             throws InterruptedException, ExecutionException, TimeoutException {
         String guid = mHelper.setCreditCard(
@@ -488,9 +501,12 @@ public class PersonalDataManagerTest {
     }
 
     @Test
+    /*
     @SmallTest
     @Feature({"Autofill"})
     @RetryOnFailure
+    */
+    @DisabledTest(message = "crbug.com/739575")
     public void testRecordAndLogCreditCardUse()
             throws InterruptedException, ExecutionException, TimeoutException {
         String guid = mHelper.setCreditCard(
