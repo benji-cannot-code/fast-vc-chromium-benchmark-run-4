077153d542f4fa6348931d3649acfd9580cae6cc
Jeff Carpenter <jeffcarp@chromium.org>
2017-07-06 22:09:50 +0000

    [WPT Export] Remove provisional PR label when attempting to merge
    Bug: 739391
    Change-Id: Id7d86dda511d7e434009afcd084521a965038919
    Reviewed-on: https://chromium-review.googlesource.com/559837
    Commit-Queue: Jeff Carpenter <jeffcarp@chromium.org>
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484750}

--- third_party/WebKit/Tools/Scripts/webkitpy/w3c/test_exporter_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/w3c/test_exporter_unittest.py
@@ -84,15 +84,15 @@ class TestExporterTest(unittest.TestCase):
             'pr_with_position',
             'pr_with_change_id',
             'create_pr',
-            'add_label "default"',
+            'add_label "chromium-export"',
             'pr_with_position',
             'pr_with_change_id',
             'create_pr',
-            'add_label "default"',
+            'add_label "chromium-export"',
             'pr_with_position',
             'pr_with_change_id',
             'create_pr',
-            'add_label "default"',
+            'add_label "chromium-export"',
         ])
         self.assertEqual(test_exporter.wpt_github.pull_requests_created, [
             ('chromium-export-c881563d73', 'older fake text', 'older fake text'),
@@ -141,13 +141,15 @@ class TestExporterTest(unittest.TestCase):
         test_exporter.run()
         self.assertEqual(test_exporter.wpt_github.calls, [
             'pr_with_position',
+            'remove_label "do not merge yet"',
             'get_pr_branch',
             'merge_pull_request',
             'pr_with_position',
             'create_pr',
-            'add_label "default"',
+            'add_label "chromium-export"',
             'pr_with_position',
             'pr_with_position',
+            'remove_label "do not merge yet"',
             'get_pr_branch',
             'merge_pull_request',
             'delete_remote_branch',
@@ -183,7 +185,7 @@ class TestExporterTest(unittest.TestCase):
         self.assertEqual(test_exporter.wpt_github.calls, [
             'pr_with_change_id',
             'create_pr',
-            'add_label "default"',
+            'add_label "chromium-export"',
             'add_label "do not merge yet"',
         ])
         self.assertEqual(test_exporter.wpt_github.pull_requests_created, [
@@ -288,6 +290,7 @@ class TestExporterTest(unittest.TestCase):
         self.assertEqual(test_exporter.wpt_github.calls, [
             'pr_with_position',
             'pr_with_change_id',
+            'remove_label "do not merge yet"',
             'get_pr_branch',
             'merge_pull_request',
             'delete_remote_branch',
