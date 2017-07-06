a0c6b2df4bf9c6b7f63a1f5a7b6d2ddfbda70b13
Bernhard Bauer <bauerb@chromium.org>
2017-07-06 16:30:20 +0000

    Don't set empty keyword when parsing search engines from OpenSearch description files.
    Bug: 728558
    Change-Id: I721b053dac06bdec283c7bf00780019cd26ec8d0
    Reviewed-on: https://chromium-review.googlesource.com/561140
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484625}

--- components/search_engines/template_url_parser.cc
+++ components/search_engines/template_url_parser.cc
@@ -281,8 +281,10 @@ void TemplateURLParsingContext::EndElementImpl(void* ctx, const xmlChar* name) {
       break;
     }
     case TemplateURLParsingContext::ALIAS: {
-      context->data_.SetKeyword(context->string_);
-      context->has_custom_keyword_ = true;
+      if (!context->string_.empty()) {
+        context->data_.SetKeyword(context->string_);
+        context->has_custom_keyword_ = true;
+      }
       break;
     }
     default:
