aa96d3fa1ea2a91872565dbf00995f6e841187d0
Brett Wilson <brettw@chromium.org>
2017-07-07 22:28:47 +0000

    Sort some items in flag_descriptions.cc
    This brings the first part of the .cc file in line with the header, as promised
    in the patch that ordered the header.
    
    Remove 2 obsolete items in the header that had no definition, and re-sort one
    thing added in the wrong place.
    
    TBR=pkasing@chromium.org
    
    Change-Id: I6427c182d11cd164d3a0ac42fc0280d8bd806730
    Reviewed-on: https://chromium-review.googlesource.com/562646
    Commit-Queue: Brett Wilson <brettw@chromium.org>
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485075}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -128,9 +128,6 @@ extern const char kDefaultTileWidthVenti[];
 extern const char kDebugShortcutsName[];
 extern const char kDebugShortcutsDescription[];
 
-extern const char kDelayNavigationName[];
-extern const char kDelayNavigationDescription[];
-
 extern const char kDeviceDiscoveryNotificationsName[];
 extern const char kDeviceDiscoveryNotificationsDescription[];
 
@@ -169,9 +166,6 @@ extern const char kEasyUnlockProximityDetectionDescription[];
 
 extern const char kEmbeddedExtensionOptionsName[];
 extern const char kEmbeddedExtensionOptionsDescription[];
-
-extern const char kEnableAdjustableLargeCursorName[];
-extern const char kEnableAdjustableLargeCursorDescription[];
 
 extern const char kEnableAsmWasmName[];
 extern const char kEnableAsmWasmDescription[];
@@ -305,6 +299,9 @@ extern const char kExperimentalCanvasFeaturesDescription[];
 extern const char kExperimentalExtensionApisName[];
 extern const char kExperimentalExtensionApisDescription[];
 
+extern const char kExperimentalFullscreenExitUIName[];
+extern const char kExperimentalFullscreenExitUIDescription[];
+
 extern const char kExperimentalHotwordHardwareName[];
 extern const char kExperimentalHotwordHardwareDescription[];
 
@@ -346,9 +343,6 @@ extern const char kEffectiveConnectionTypeSlow2GDescription[];
 extern const char kEffectiveConnectionType2GDescription[];
 extern const char kEffectiveConnectionType3GDescription[];
 extern const char kEffectiveConnectionType4GDescription[];
-
-extern const char kExperimentalFullscreenExitUIName[];
-extern const char kExperimentalFullscreenExitUIDescription[];
 
 extern const char kFillOnAccountSelectName[];
 extern const char kFillOnAccountSelectDescription[];
