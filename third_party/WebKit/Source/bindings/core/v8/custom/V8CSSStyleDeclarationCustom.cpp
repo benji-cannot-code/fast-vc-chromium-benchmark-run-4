41296b89236a17c1b82fd6fb8698a7b4fb20b5b7
bratell <bratell@opera.com>
2017-07-07 08:42:47 -0700

    Drop a "using namespace WTF" in V8CSSStyleDeclarationCustom.cpp
    global "using namespace WTF" causes compiler warnings in jumbo builds
    and in general extend the global namespace a lot which increases the
    risk of symbol conflicts.
    
    This patch changes a "using namespace WTF" to an explicit
    WTF::CopePointCompareLessThan.
    Review-Url: https://codereview.chromium.org/2975603002
    Cr-Commit-Position: refs/heads/master@{#484938}

--- third_party/WebKit/Source/bindings/core/v8/custom/V8CSSStyleDeclarationCustom.cpp
+++ third_party/WebKit/Source/bindings/core/v8/custom/V8CSSStyleDeclarationCustom.cpp
@@ -50,8 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/wtf/text/StringBuilder.h"
 #include "platform/wtf/text/StringConcatenate.h"
 
-using namespace WTF;
-
 namespace blink {
 
 // Check for a CSS prefix.
@@ -162,7 +160,7 @@ void V8CSSStyleDeclaration::namedPropertyEnumeratorCustom(
         property_names.push_back(getJSPropertyName(property_id));
     }
     std::sort(property_names.begin(), property_names.end(),
-              CodePointCompareLessThan);
+              WTF::CodePointCompareLessThan);
     property_names_length = property_names.size();
   }
 
