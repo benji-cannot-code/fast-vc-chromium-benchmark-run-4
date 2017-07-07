3957c457440a9eeb444cf51f644e89f0e5354c6c
Mathieu Perreault <mathp@chromium.org>
2017-07-07 15:04:47 +0000

    [Autofill] Add scrollbar to Autofill popup (views impl).
    Currently long lists will clip if for example 100 elements are added
    to the autofill popup through datalist. This adds a ScrollView.
    
    Bug: 375637
    Change-Id: Ifa7d753369fea147db9cbcd5cc2f3562bc7557ab
    Reviewed-on: https://chromium-review.googlesource.com/552324
    Commit-Queue: Mathieu Perreault <mathp@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484925}

--- chrome/browser/ui/autofill/popup_constants.h
+++ chrome/browser/ui/autofill/popup_constants.h
@@ -6,10 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_AUTOFILL_POPUP_CONSTANTS_H_
 #define CHROME_BROWSER_UI_AUTOFILL_POPUP_CONSTANTS_H_
 
+#include "build/build_config.h"
+
 namespace autofill {
 
+#if defined(TOOLKIT_VIEWS)
+// In views, the implementation takes care of the border itself.
+const int kPopupBorderThickness = 0;
+#else
 // TODO(crbug.com/676221): Change this to pixels
 const int kPopupBorderThickness = 1;
+#endif
 
 }  // namespace autofill
 
