cf3452349b9e51014bdac083790a1ef90e4f42fc
skau <skau@chromium.org>
2017-07-06 19:38:48 -0700

    Separate manufacturer and model fields from PPD info
    Current representation of manufacturer and model impedes merger into a
    single make_and_model field.
    
    BUG=730242
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2962413002
    Cr-Commit-Position: refs/heads/master@{#484808}

--- chrome/browser/resources/settings/printing_page/cups_add_printer_dialog.js
+++ chrome/browser/resources/settings/printing_page/cups_add_printer_dialog.js
@@ -40,6 +40,8 @@ var kPrinterListFullHeight = 350;
  */
 function getEmptyPrinter_() {
   return {
+    ppdManufacturer: '',
+    ppdModel: '',
     printerAddress: '',
     printerAutoconf: false,
     printerDescription: '',
@@ -139,8 +141,8 @@ Polymer({
     // If we're switching to the manufacturer/model dialog, clear the existing
     // data we have about the PPD (if any), as we're dropping that in favor of
     // user selections.
-    this.selectedPrinter.printerManufacturer = '';
-    this.selectedPrinter.printerModel = '';
+    this.selectedPrinter.ppdManufacturer = '';
+    this.selectedPrinter.ppdModel = '';
     this.selectedPrinter.printerPPDPath = '';
     this.$$('add-printer-dialog').close();
     this.fire('open-manufacturer-model-dialog');
@@ -219,7 +221,7 @@ Polymer({
   },
 
   observers: [
-    'selectedManufacturerChanged_(newPrinter.printerManufacturer)',
+    'selectedManufacturerChanged_(newPrinter.ppdManufacturer)',
   ],
 
   /** @override */
@@ -236,7 +238,7 @@ Polymer({
    */
   selectedManufacturerChanged_: function(manufacturer) {
     // Reset model if manufacturer is changed.
-    this.set('newPrinter.printerModel', '');
+    this.set('newPrinter.ppdModel', '');
     if (manufacturer) {
       settings.CupsPrintersBrowserProxyImpl.getInstance()
           .getCupsPrinterModelsList(manufacturer)
@@ -299,14 +301,14 @@ Polymer({
   },
 
   /**
-   * @param {string} printerManufacturer
-   * @param {string} printerModel
+   * @param {string} ppdManufacturer
+   * @param {string} ppdModel
    * @param {string} printerPPDPath
    * @return {boolean} Whether we have enough information to set up the printer
    * @private
    */
-  canAddPrinter_: function(printerManufacturer, printerModel, printerPPDPath) {
-    return !!((printerManufacturer && printerModel) || printerPPDPath);
+  canAddPrinter_: function(ppdManufacturer, ppdModel, printerPPDPath) {
+    return !!((ppdManufacturer && ppdModel) || printerPPDPath);
   },
 });
 
