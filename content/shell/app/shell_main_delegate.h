725207af076da10dc827c273c98491ac771b1346
Han Leon <leon.han@intel.com>
2017-07-07 02:54:57 +0000

    Reland 1: [DeviceService] Add PowerMonitor browsertest against gpu process
    The original CL https://chromium-review.googlesource.com/c/547672/
    was reverted because of timeout of PowerMonitorTest.TestGpuProcess on
    https://build.chromium.org/p/chromium.mac/builders/Mac10.11%20Tests/builds/14487
    
    Original CL description:
    This CL is to confirm that in a real browser environment the gpu
    process CAN succeed in connecting to Device Service to bind PowerMonitor
    interface, and, via which gpu process CAN get power state change
    correctly.
    
    BUG=715985
    TEST=content_browsertests
    TBR=jam@chromium.org
    
    Change-Id: Ib9f4bd0ad7b779f61d13eb7fbee9b2ab56d3d354
    Reviewed-on: https://chromium-review.googlesource.com/554339
    Commit-Queue: Han Leon <leon.han@intel.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484812}

--- content/shell/app/shell_main_delegate.h
+++ content/shell/app/shell_main_delegate.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 class ContentClient;
 class ShellContentBrowserClient;
+class ShellContentGpuClient;
 class ShellContentRendererClient;
 class ShellContentUtilityClient;
 
@@ -37,6 +38,7 @@ class ShellMainDelegate : public ContentMainDelegate {
   void ZygoteForked() override;
 #endif
   ContentBrowserClient* CreateContentBrowserClient() override;
+  ContentGpuClient* CreateContentGpuClient() override;
   ContentRendererClient* CreateContentRendererClient() override;
   ContentUtilityClient* CreateContentUtilityClient() override;
 
@@ -44,6 +46,7 @@ class ShellMainDelegate : public ContentMainDelegate {
 
  private:
   std::unique_ptr<ShellContentBrowserClient> browser_client_;
+  std::unique_ptr<ShellContentGpuClient> gpu_client_;
   std::unique_ptr<ShellContentRendererClient> renderer_client_;
   std::unique_ptr<ShellContentUtilityClient> utility_client_;
   std::unique_ptr<ContentClient> content_client_;
---
+++ content/shell/gpu/shell_content_gpu_client.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_SHELL_GPU_SHELL_CONTENT_GPU_CLIENT_H_
+#define CONTENT_SHELL_GPU_SHELL_CONTENT_GPU_CLIENT_H_
+
+#include <memory>
+
+#include "base/macros.h"
+#include "content/public/common/network_service_test.mojom.h"
+#include "content/public/gpu/content_gpu_client.h"
+
+namespace content {
+
+class ShellContentGpuClient : public ContentGpuClient {
+ public:
+  ShellContentGpuClient();
+  ~ShellContentGpuClient() override;
+
+  // ContentGpuClient:
+  void Initialize(base::FieldTrialList::Observer* observer,
+                  service_manager::BinderRegistry* registry) override;
+
+  DISALLOW_COPY_AND_ASSIGN(ShellContentGpuClient);
+};
+
+}  // namespace content
+
+#endif  // CONTENT_SHELL_GPU_SHELL_CONTENT_GPU_CLIENT_H_
