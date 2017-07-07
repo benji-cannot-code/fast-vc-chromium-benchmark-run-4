19ac69416828baaf0ad7aebad822ffbf51329a30
Hiroshige Hayashizaki <hiroshige@chromium.org>
2017-07-07 22:12:49 +0000

    Use ExecuteScriptBlock() in ScriptLoader::PrepareScript()
    Follow-up of https://chromium-review.googlesource.com/c/554098.
    This shouldn't change the behavior.
    
    Bug: 686281
    Change-Id: Ie5bc4d5f6974c00efa13899998c6b0b225fd941c
    Reviewed-on: https://chromium-review.googlesource.com/554119
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485069}

--- third_party/WebKit/Source/core/dom/ClassicPendingScript.cpp
+++ third_party/WebKit/Source/core/dom/ClassicPendingScript.cpp
@@ -211,6 +211,8 @@ void ClassicPendingScript::StartStreamingIfPossible(
 }
 
 bool ClassicPendingScript::WasCanceled() const {
+  if (!GetResource())
+    return false;
   return GetResource()->WasCanceled();
 }
 
