9a25956304b3e8a13d420da952a4fef98902ad6e
msramek <msramek@chromium.org>
2017-07-07 04:46:01 -0700

    Enable Clear-Site-Data by default.
    BUG=607897
    
    Review-Url: https://codereview.chromium.org/2972083002
    Cr-Commit-Position: refs/heads/master@{#484890}

--- content/browser/browsing_data/clear_site_data_throttle_browsertest.cc
+++ content/browser/browsing_data/clear_site_data_throttle_browsertest.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/browser/storage_partition.h"
 #include "content/public/browser/web_contents.h"
-#include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/cache_test_util.h"
 #include "content/public/test/content_browser_test.h"
@@ -154,8 +153,6 @@ class ClearSiteDataThrottleBrowserTest : public ContentBrowserTest {
  public:
   void SetUpCommandLine(base::CommandLine* command_line) override {
     ContentBrowserTest::SetUpCommandLine(command_line);
-    command_line->AppendSwitch(
-        switches::kEnableExperimentalWebPlatformFeatures);
 
     // We're redirecting all hosts to localhost even on HTTPS, so we'll get
     // certificate errors.
