92328468ee9167852e4e2e6b1aba08e62d7281a2
xjz <xjz@chromium.org>
2017-07-07 13:58:19 -0700

    Media Remoting: Add mojo interfaces between browser and extension.
    Currently media remoting communicates with extension by text strings
    through Media Router mojo API. This CL adds mojo interfaces to allow
    CastRemotingConnector in browser talk directly to MediaRemoter in
    extension after connected through the Media Router mojo API. These
    changes also enable passing the receiver's capablities to remoting
    control logic in renderer, which will be done in a follow up CL.
    
    BUG=734672
    
    Review-Url: https://codereview.chromium.org/2951523002
    Cr-Commit-Position: refs/heads/master@{#485043}

--- media/remoting/renderer_controller.cc
+++ media/remoting/renderer_controller.cc
@@ -119,6 +119,7 @@ void RendererController::OnBecameDominantVisibleContent(bool is_dominant) {
 }
 
 void RendererController::OnSetCdm(CdmContext* cdm_context) {
+  VLOG(2) << __func__;
   DCHECK(thread_checker_.CalledOnValidThread());
 
   auto* remoting_cdm_context = RemotingCdmContext::From(cdm_context);
@@ -132,6 +133,7 @@ void RendererController::OnSetCdm(CdmContext* cdm_context) {
 }
 
 void RendererController::OnRemotePlaybackDisabled(bool disabled) {
+  VLOG(2) << __func__ << ": disabled = " << disabled;
   DCHECK(thread_checker_.CalledOnValidThread());
 
   is_remote_playback_disabled_ = disabled;
@@ -291,6 +293,10 @@ bool RendererController::ShouldBeRemoting() {
     case mojom::RemotingSinkCapabilities::RENDERING_ONLY:
     case mojom::RemotingSinkCapabilities::CONTENT_DECRYPTION_AND_RENDERING:
       break;  // The sink is capable of remote rendering.
+    default:
+      // TODO(xjz): Will be changed in a coming CL that passes the receiver's
+      // capabilities.
+      NOTREACHED();
   }
 
   if ((!has_audio() && !has_video()) ||
@@ -353,6 +359,7 @@ void RendererController::UpdateAndMaybeSwitch(StartTrigger start_trigger,
 }
 
 void RendererController::OnRendererFatalError(StopTrigger stop_trigger) {
+  VLOG(2) << __func__ << ": stop_trigger= " << stop_trigger;
   DCHECK(thread_checker_.CalledOnValidThread());
 
   // Do not act on errors caused by things like Mojo pipes being closed during
