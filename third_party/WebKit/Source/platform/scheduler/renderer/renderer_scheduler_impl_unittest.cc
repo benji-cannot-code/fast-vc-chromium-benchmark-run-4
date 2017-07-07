57634f32d6fa16a6a0a254ee395cbc06f9b0e148
Dan Elphick <delphick@chromium.org>
2017-07-07 08:57:32 +0000

    Increase priority of compositor TQ in NONE Usecase
    This allows animated GIFs to keep updating even in the presence of timer
    storms.
    
    Reland change partially reverted in
    WebRtc and CrSettings tests have been landed.
    
    https: //chromium-review.googlesource.com/c/508792/ now that fixes to
    Bug: 703608
    Change-Id: Id014fda26b7168658c679a7bf74359485c5262e5
    Reviewed-on: https://chromium-review.googlesource.com/559345
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Dan Elphick <delphick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484863}

--- third_party/WebKit/Source/platform/scheduler/renderer/renderer_scheduler_impl_unittest.cc
+++ third_party/WebKit/Source/platform/scheduler/renderer/renderer_scheduler_impl_unittest.cc
@@ -855,9 +855,9 @@ TEST_F(RendererSchedulerImplTest, TestDefaultPolicy) {
   EnableIdleTasks();
   RunUntilIdle();
   EXPECT_THAT(run_order,
-              testing::ElementsAre(std::string("L1"), std::string("D1"),
-                                   std::string("C1"), std::string("D2"),
-                                   std::string("C2"), std::string("I1")));
+              testing::ElementsAre(std::string("C1"), std::string("C2"),
+                                   std::string("L1"), std::string("D1"),
+                                   std::string("D2"), std::string("I1")));
   EXPECT_EQ(RendererSchedulerImpl::UseCase::NONE, CurrentUseCase());
 }
 
@@ -2428,7 +2428,7 @@ TEST_F(RendererSchedulerImplTest, SuspendRenderer) {
   EnableIdleTasks();
   RunUntilIdle();
   EXPECT_THAT(run_order,
-              testing::ElementsAre(std::string("D1"), std::string("C1"),
+              testing::ElementsAre(std::string("C1"), std::string("D1"),
                                    std::string("I1")));
 
   // The rest queued tasks fire when the tab goes foregrounded.
@@ -2460,7 +2460,7 @@ TEST_F(RendererSchedulerImplTest, ResumeRenderer) {
   EnableIdleTasks();
   RunUntilIdle();
   EXPECT_THAT(run_order,
-              testing::ElementsAre(std::string("D1"), std::string("C1"),
+              testing::ElementsAre(std::string("C1"), std::string("D1"),
                                    std::string("I1")));
 
   // The rest queued tasks fire when the renderer is resumed.
@@ -2478,7 +2478,7 @@ TEST_F(RendererSchedulerImplTest, ResumeRenderer) {
   EnableIdleTasks();
   RunUntilIdle();
   EXPECT_THAT(run_order,
-              testing::ElementsAre(std::string("D2"), std::string("C2"),
+              testing::ElementsAre(std::string("C2"), std::string("D2"),
                                    std::string("I2")));
 
   // The rest queued tasks fire when the renderer is resumed.
