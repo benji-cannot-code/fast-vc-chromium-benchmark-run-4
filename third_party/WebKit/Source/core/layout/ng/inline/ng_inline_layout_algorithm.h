d932d6bcce08e844b3916de70de7b28794633713
kojii <kojii@chromium.org>
2017-07-06 19:30:43 -0700

    [LayoutNG] Fix borders/paddings for blocks with inline children
    A block with inline children generates fragment tree as follows:
    - A box fragment created by NGBlockNode
      - An anonymous wrapper box fragment created by NGInlineNode
        - Line box fragments.
    
    When such block has borders and/or paddings, they were added both to the
    anonymous wrapper box and to line boxes. The rendering was correct
    though, because NGInlineNode::CopyFragmentDataToLayoutBox() ignored the
    offset of the anonymous wrapper box.
    
    This patch fixes this by placing line boxes relative to the anonymous
    wrapper box. Also fixes CopyFragmentDataToLayoutBox() to convert
    positions relative to the parent LayoutBlockFlow.
    
    BUG=636993
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    
    Review-Url: https://codereview.chromium.org/2969383002
    Cr-Commit-Position: refs/heads/master@{#484803}

--- third_party/WebKit/Source/core/layout/ng/inline/ng_inline_layout_algorithm.h
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_inline_layout_algorithm.h
@@ -79,7 +79,6 @@ class CORE_EXPORT NGInlineLayoutAlgorithm final
   unsigned is_horizontal_writing_mode_ : 1;
 
   NGConstraintSpaceBuilder space_builder_;
-  NGBoxStrut border_and_padding_;
 };
 
 }  // namespace blink
