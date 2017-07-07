8084ae5f99f296b746ee57e118610ffc18264466
Kenneth Russell <kbr@chromium.org>
2017-07-07 17:40:59 +0000

    Run audio_unittests on optional GPU tryservers.
    These will now run on win_optional_gpu_tests_rel,
    linux_optional_gpu_tests_rel, and mac_optional_gpu_tests_rel, on
    physical hardware. Previously they'd only been running on the
    chromium.gpu.fyi waterfall.
    
    BUG=708917
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Change-Id: Ifc4e6ab582324d7217793f0137d5df57cd7f1a11
    Reviewed-on: https://chromium-review.googlesource.com/563077
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484969}

--- content/test/gpu/generate_buildbot_json.py
+++ content/test/gpu/generate_buildbot_json.py
@@ -1350,7 +1350,7 @@ COMMON_GTESTS = {
   'audio_unittests': {
     'tester_configs': [
       {
-        'predicate': Predicates.FYI_ONLY,
+        'predicate': Predicates.FYI_AND_OPTIONAL,
       }
     ],
     # Don't run these tests on Android.
