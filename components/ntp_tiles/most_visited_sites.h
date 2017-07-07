f74fbdf016f56dd51322a676bb51c4168a2720ab
Friedrich Horschig <fhorschig@chromium.org>
2017-07-07 11:25:13 +0000

    Retrieve title for Homepage tile on Chrome Home from history
    In order to replace the address with the title of the page after having
    visited it for the first time, check the history service.
    As this is going to be a field provided by partner customizations,
    add it to the HomePageClient.
    
    BUG=730825
    
    Change-Id: Idace5c8a4f9d52a52b212bd65fbd0c545e82a7ee
    Reviewed-on: https://chromium-review.googlesource.com/559147
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484886}

--- components/ntp_tiles/most_visited_sites.h
+++ components/ntp_tiles/most_visited_sites.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/callback_forward.h"
 #include "base/compiler_specific.h"
 #include "base/files/file_path.h"
 #include "base/macros.h"
@@ -96,10 +97,14 @@ class MostVisitedSites : public history::TopSitesObserver,
   // platform-specific implementation.
   class HomePageClient {
    public:
+    using TitleCallback =
+        base::OnceCallback<void(const base::Optional<base::string16>& title)>;
+
     virtual ~HomePageClient() = default;
     virtual bool IsHomePageEnabled() const = 0;
     virtual bool IsNewTabPageUsedAsHomePage() const = 0;
-    virtual GURL GetHomepageUrl() const = 0;
+    virtual GURL GetHomePageUrl() const = 0;
+    virtual void QueryHomePageTitle(TitleCallback title_callback) = 0;
   };
 
   // Construct a MostVisitedSites instance.
@@ -194,10 +199,14 @@ class MostVisitedSites : public history::TopSitesObserver,
       const std::set<std::string>& used_hosts,
       size_t num_actual_tiles);
 
+  // Initiates a query for the home page tile if needed and calls
+  // |SaveTilesAndNotify| in the end.
+  void InitiateNotificationForNewTiles(NTPTilesVector new_tiles);
+
   // Takes the personal tiles, creates and merges in whitelist and popular tiles
   // if appropriate, and saves the new tiles. Notifies the observer if the tiles
   // were actually changed.
-  void SaveNewTilesAndNotify(NTPTilesVector personal_tiles);
+  void SaveTilesAndNotify(NTPTilesVector personal_tiles);
 
   void OnPopularSitesDownloaded(bool success);
 
@@ -212,7 +221,11 @@ class MostVisitedSites : public history::TopSitesObserver,
   // Adds the home page as first tile to |tiles| and returns them as new vector.
   // Drops existing tiles with the same host as the home page and tiles that
   // would exceed the maximum.
-  NTPTilesVector CreatePersonalTilesWithHomeTile(NTPTilesVector tiles) const;
+  NTPTilesVector InsertHomeTile(NTPTilesVector tiles,
+                                const base::string16& title) const;
+
+  void OnHomePageTitleDetermined(NTPTilesVector tiles,
+                                 const base::Optional<base::string16>& title);
 
   // Returns true if there is a valid home page that can be pinned as tile.
   bool ShouldAddHomeTile() const;
