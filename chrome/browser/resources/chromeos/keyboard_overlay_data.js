d269dd676365c0abc2f5116e65177f8849fdf0fb
afakhry <afakhry@chromium.org>
2017-07-06 19:18:34 -0700

    Change Toggle High Contrast accelerator
    Due to accidental triggering of High Contrast accelerator, we are
    implemnting the following plan:
    
    1: search+shift+h should stop working and display a notification
       that the shortcut has changed.
    2: ctrl+search+h should be the new combo
    3: Keyboard overlay should update with the change.
    
    BUG=738214
    TEST=covered by tests
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2971773003
    Cr-Commit-Position: refs/heads/master@{#484799}

--- chrome/browser/resources/chromeos/keyboard_overlay_data.js
+++ chrome/browser/resources/chromeos/keyboard_overlay_data.js
@@ -4224,7 +4224,7 @@ var keyboardOverlayData = {
     'g<>CTRL': 'keyboardOverlayFindTextAgain',
     'g<>CTRL<>SHIFT': 'keyboardOverlayFindPreviousText',
     'h<>CTRL': 'keyboardOverlayHistory',
-    'h<>SEARCH<>SHIFT': 'keyboardOverlayToggleHighContrastMode',
+    'h<>CTRL<>SEARCH': 'keyboardOverlayToggleHighContrastMode',
     'i<>ALT<>CTRL': 'keyboardOverlayTouchHudModeChange',
     'i<>ALT<>SHIFT': 'keyboardOverlayReportIssue',
     'i<>CTRL<>SHIFT': 'keyboardOverlayDeveloperTools',
