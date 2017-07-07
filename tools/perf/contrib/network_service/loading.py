8e643a4ae2462375e4e5d8f582a82e897b628cd4
yzshen <yzshen@chromium.org>
2017-07-07 12:26:33 -0700

    Enable loading.desktop benchmark with network service enabled on perf fyi bot.
    BUG=717738
    
    Review-Url: https://codereview.chromium.org/2973733002
    Cr-Commit-Position: refs/heads/master@{#485007}

---
+++ tools/perf/contrib/network_service/loading.py
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from benchmarks import loading
+
+from telemetry import benchmark
+
+
+@benchmark.Owner(emails=['yzshen@chromium.org'])
+class LoadingDesktopNetworkService(loading.LoadingDesktop):
+  """Measures loading performance of desktop sites, with the network service
+  enabled.
+  """
+  @classmethod
+  def Name(cls):
+    return 'loading.desktop.network_service'
+
+  def SetExtraBrowserOptions(self, options):
+    enable_features_arg = '--enable-features=NetworkService'
+
+    # If an "--enable-features" argument has been specified, append to the value
+    # list of that argument.
+    for arg in options.extra_browser_args:
+      if arg.startswith('--enable-features='):
+        options.extra_browser_args.remove(arg)
+        enable_features_arg = arg + ',NetworkService'
+        break
+
+    options.AppendExtraBrowserArgs([ enable_features_arg, '--incognito' ])
