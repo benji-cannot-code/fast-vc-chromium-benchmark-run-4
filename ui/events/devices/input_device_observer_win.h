348b8ca42e44b41a4d0e1388762e28d81ba539be
Alexis Menard <alexis.menard@intel.com>
2017-07-07 18:06:09 +0000

    Reland "Make Interaction Media Features MQ dynamic on Android"
    This is a reland of 1bdb883378a5a40e6341d288e3927b637d337501 without
    changes. 
    
    Original CL was reverted due to a bug in how chromium 
    generates JNI bindings with native functions. It is fixed by 
    https://codereview.chromium.org/2966163006/.
    
    Original change's description:
    > Make Interaction Media Features MQ dynamic on Android
    > 
    > With detachable form factors such as Asus Transformer
    > it is important to make sure the interaction media
    > features are updated whenever the keyboard/trackpad combo is
    > detached. This will allow content author to react to
    > media query changes to adapt the user interface to
    > better suit the new interaction method (often touch vs
    > trackpad). The patch also handles any input changes e.g.
    > USB connected mouse.
    > 
    > The patch uses the facility landed in previous OSes and
    > add a bit of Java code to listen the changes of inputs
    > through the InputDeviceListener mechanism.
    > 
    > BUG=442418
    > 
    > Change-Id: I5c18ce4df8e6da927eede5a24f40aac10ecf1974
    > Reviewed-on: https://chromium-review.googlesource.com/553457
    > Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Bo Liu <boliu@chromium.org>
    > Commit-Queue: Alexis Menard <alexis.menard@intel.com>
    > Cr-Commit-Position: refs/heads/master@{#484641}
    
    
    TBR=agrieve@chromium.org, alexis.menard@intel.com, boliu@chromium.org, sadrul@chromium.org
    
    Bug: 442418
    Change-Id: I5dca047c566625fa2f976d3311899f9a3a23f44e
    Reviewed-on: https://chromium-review.googlesource.com/563478
    Reviewed-by: Alexis Menard <alexis.menard@intel.com>
    Commit-Queue: Alexis Menard <alexis.menard@intel.com>
    Cr-Commit-Position: refs/heads/master@{#484981}

--- ui/events/devices/input_device_observer_win.h
+++ ui/events/devices/input_device_observer_win.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_BROWSER_RENDERER_HOST_INPUT_INPUT_DEVICE_OBSERVER_WIN_H_
-#define CONTENT_BROWSER_RENDERER_HOST_INPUT_INPUT_DEVICE_OBSERVER_WIN_H_
+#ifndef UI_EVENTS_DEVICES_INPUT_DEVICE_OBSERVER_WIN_H_
+#define UI_EVENTS_DEVICES_INPUT_DEVICE_OBSERVER_WIN_H_
 
 #include "base/observer_list.h"
 #include "base/win/registry.h"
@@ -46,4 +46,4 @@ class EVENTS_DEVICES_EXPORT InputDeviceObserverWin {
 
 }  // namespace ui
 
-#endif  // CONTENT_BROWSER_RENDERER_HOST_INPUT_INPUT_DEVICE_OBSERVER_WIN_H_
+#endif  // UI_EVENTS_DEVICES_INPUT_DEVICE_OBSERVER_WIN_H_
