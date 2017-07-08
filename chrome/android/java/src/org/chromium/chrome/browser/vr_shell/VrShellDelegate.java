3a36d9d9227ecbe45048f28aec19ffef437fd2cf
Michael Thiessen <mthiesse@chromium.org>
2017-07-08 02:10:15 +0000

    VR: Fix double-queueing of Choreographer callbacks when skipping VR entry screens
    Moves pausing of the vsync estimator into onPause rather than onStop.
    
    Bug: 740161
    Change-Id: I4a8c3b9ecfd60bc43ed3499761cc6b0391f47082
    Reviewed-on: https://chromium-review.googlesource.com/563858
    Reviewed-by: Biao She <bshe@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485129}

--- chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellDelegate.java
@@ -981,6 +981,8 @@ public class VrShellDelegate implements ApplicationStatus.ActivityStateListener,
         unregisterDaydreamIntent(mVrDaydreamApi);
         if (mVrSupportLevel == VR_NOT_AVAILABLE) return;
 
+        if (mInVr) mVSyncEstimator.pause();
+
         cancelPendingVrEntry();
 
         // When the active web page has a vrdisplayactivate event handler,
@@ -1001,10 +1003,7 @@ public class VrShellDelegate implements ApplicationStatus.ActivityStateListener,
         cancelPendingVrEntry();
         // We defer pausing of VrShell until the app is stopped to keep head tracking working for
         // as long as possible while going to daydream home.
-        if (mInVr) {
-            mVrShell.pause();
-            mVSyncEstimator.pause();
-        }
+        if (mInVr) mVrShell.pause();
         if (mShowingDaydreamDoff || mProbablyInDon) return;
 
         // TODO(mthiesse): When the user resumes Chrome in a 2D context, we don't want to tear down
