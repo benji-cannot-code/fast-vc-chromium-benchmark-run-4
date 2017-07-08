43b8992b3b25ca437a56143de25518ef1645fca0
zhaobin <zhaobin@chromium.org>
2017-07-07 17:22:29 -0700

    [Media Router] Add CastMediaSinkService
    NOTE: those extensions header files and extensions::api:: namespace will go away when https://codereview.chromium.org/2913033003/ lands
    
    Discover process is similar to DialMediaSinkService:
    - CastMediaSinkService::Start() registers itself with DnsRegistry
    - CastMediaSinkService::OnDnsSdEvent() gets invoked when Dns services comes back and starts a 3s timer
    - Start opening channel for each service on IO thread
    - CastMediaSinkService::OnChannelOpenedOnIOThread() gets invoked when channel open finishes
    - Post task back to UI thread via CastMediaSinkService::OnChannelOpenOnUIThread()
    - Create MediaSinkInternal and store it in CastSinkService's sink map
    - Invoke MediaSinkBase::FetchCompleted() and send sinks to MRP when timer expires
    
    BUG=687377
    
    Review-Url: https://codereview.chromium.org/2927833002
    Cr-Original-Commit-Position: refs/heads/master@{#484979}
    Committed: https://chromium.googlesource.com/chromium/src/+/b9a2a9b60034d3ee2138c4815bc3c66b13cee3b5
    Review-Url: https://codereview.chromium.org/2927833002
    Cr-Commit-Position: refs/heads/master@{#485110}

--- chrome/browser/media/router/discovery/mdns/mock_dns_sd_registry.cc
+++ chrome/browser/media/router/discovery/mdns/mock_dns_sd_registry.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media_router {
 
 MockDnsSdRegistry::MockDnsSdRegistry(DnsSdRegistry::DnsSdObserver* observer)
-    : observer_(observer) {}
+    : DnsSdRegistry(nullptr), observer_(observer) {}
 
 MockDnsSdRegistry::~MockDnsSdRegistry() {}
 
