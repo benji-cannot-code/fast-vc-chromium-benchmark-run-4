7b5f8c5629db71d0efdbdc8090cf138f053033fc
Qiang Xu <warx@chromium.org>
2017-07-10 21:55:47 +0000

    app_list: create a single Env instance for app_list_unittests
    Bug: 740321
    TEST: covered by tests and the issue in crbug.com/740321 can be solved.
    Change-Id: I9ff64cde700bc280efda6f27fdf8dc6acac4faa7
    Reviewed-on: https://chromium-review.googlesource.com/564317
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Qiang(Joe) Xu <warx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485413}

--- ui/app_list/test/run_all_unittests.cc
+++ ui/app_list/test/run_all_unittests.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "mojo/edk/embedder/embedder.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/aura/env.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/ui_base_paths.h"
 #include "ui/gl/test/gl_surface_test_support.h"
@@ -33,8 +34,9 @@ class AppListTestSuite : public base::TestSuite {
     base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
     command_line->AppendSwitchASCII(kTestType, "applist");
 
-    gl::GLSurfaceTestSupport::InitializeOneOff();
     base::TestSuite::Initialize();
+    gl::GLSurfaceTestSupport::InitializeOneOff();
+    env_ = aura::Env::CreateInstance();
     ui::RegisterPathProvider();
 
     base::FilePath ui_test_pak_path;
@@ -46,11 +48,13 @@ class AppListTestSuite : public base::TestSuite {
   }
 
   void Shutdown() override {
+    env_.reset();
     ui::ResourceBundle::CleanupSharedInstance();
     base::TestSuite::Shutdown();
   }
 
  private:
+  std::unique_ptr<aura::Env> env_;
   base::TestDiscardableMemoryAllocator discardable_memory_allocator_;
 
   DISALLOW_COPY_AND_ASSIGN(AppListTestSuite);
