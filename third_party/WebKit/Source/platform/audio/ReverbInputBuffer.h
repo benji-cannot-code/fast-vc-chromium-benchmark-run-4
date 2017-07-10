78075e5dc07812dba139157824a51e2876d8303f
Raymond Toy <rtoy@chromium.org>
2017-07-10 20:01:45 +0000

    Fix data race in accessing write_index in ReverbInputBuffer
    Use atomics to protect access to write_index_.  This can be read from
    multiple threads (performing the convolution stages) and also written
    by the audio thread when new data needs to be buffered for
    convolution.
    
    Ran the test from the bug to verify the race is gone. (The race was
    100% reproducible with that test).
    
    BUG=732918
    TEST=none
    
    Change-Id: Iefa3cf1e1fc182e714ad4e330287cc83be75c36a
    Reviewed-on: https://chromium-review.googlesource.com/546095
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Raymond Toy <rtoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485356}

--- third_party/WebKit/Source/platform/audio/ReverbInputBuffer.h
+++ third_party/WebKit/Source/platform/audio/ReverbInputBuffer.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/PlatformExport.h"
 #include "platform/audio/AudioArray.h"
 #include "platform/wtf/Allocator.h"
+#include "platform/wtf/Atomics.h"
 #include "platform/wtf/Noncopyable.h"
 
 namespace blink {
@@ -53,7 +54,10 @@ class PLATFORM_EXPORT ReverbInputBuffer {
   void Write(const float* source_p, size_t number_of_frames);
 
   // Background threads can call this to check if there's anything to read...
-  size_t WriteIndex() const { return write_index_; }
+  size_t WriteIndex() const { return AcquireLoad(&write_index_); }
+  void SetWriteIndex(size_t new_index) {
+    ReleaseStore(&write_index_, new_index);
+  }
 
   // The individual background threads read here (and hope that they can keep up
   // with the buffer writing).
@@ -67,6 +71,10 @@ class PLATFORM_EXPORT ReverbInputBuffer {
 
  private:
   AudioFloatArray buffer_;
+
+  // |write_index_| can be accessed from several threads.  Only use
+  // the getter and setter to access it atomically.  Don't access
+  // directly!
   size_t write_index_;
 };
 
