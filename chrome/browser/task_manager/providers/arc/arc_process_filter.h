163efdf028a2e4b2eac899055191d914a2ed01d2
Eliot Courtney <edcourtney@chromium.org>
2017-07-10 04:36:00 +0000

    [Task Manager] Implicitly white list more process states.
    If you minimise Gmail, it disappears from the task manager, so allow the
    LAST_ACTIVITY process state to be displayed for ARC processes.
    
    Additionally allow FOREGROUND_SERVICE since foreground services are user
    visible.
    
    BUG=738651
    
    Change-Id: I403b9946fbbb29b115fe1877f63bb64e3fe3b2c7
    Reviewed-on: https://chromium-review.googlesource.com/562906
    Reviewed-by: Nick Carter <nick@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485183}

--- chrome/browser/task_manager/providers/arc/arc_process_filter.h
+++ chrome/browser/task_manager/providers/arc/arc_process_filter.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <string>
 
+#include "base/macros.h"
 #include "chrome/browser/chromeos/arc/process/arc_process.h"
 
 namespace task_manager {
