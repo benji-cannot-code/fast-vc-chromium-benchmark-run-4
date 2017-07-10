214d2ede9ac14cdb6c0bcce014316925ca1e5219
tzik <tzik@chromium.org>
2017-07-10 09:08:21 +0000

    Use OnceCallback on Mojo interfaces in //media/mojo/interfaces
    This CL flips `use_once_callback` flag on the Mojo code generator, and
    fixes all compile errors after it. After this CL, Mojo interfaces in
    //media/mojo/interfaces starts using base::OnceCallback instead of
    base::Callback on its return value handling.
    
    The migration recipe was:
     - Convert pass-by-ref callback objects to pass-by-value.
     - Use std::move() to forward it to other consumer, or to invoke it
       with Callback::Run().
     - Handle wherever copies are required manually.
     - Check if the conversion doesn't change the semantics. As the transfer
       and invocation clobber the callback object, care about use-after-move.
       It's considered safe to consume almost scoped-out callback.
    
    TBR=satorux@chromium.org
    
    Bug: 714018
    Change-Id: Ic0cad5638bd8e9b05ddb62c028839bb4d0b955b2
    Reviewed-on: https://chromium-review.googlesource.com/536384
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485211}

--- components/cdm/browser/media_drm_storage_impl.cc
+++ components/cdm/browser/media_drm_storage_impl.cc
@@ -124,13 +124,13 @@ void MediaDrmStorageImpl::Initialize(const url::Origin& origin) {
   initialized_ = true;
 }
 
-void MediaDrmStorageImpl::OnProvisioned(const OnProvisionedCallback& callback) {
+void MediaDrmStorageImpl::OnProvisioned(OnProvisionedCallback callback) {
   DVLOG(1) << __func__;
   DCHECK(thread_checker_.CalledOnValidThread());
 
   if (!initialized_) {
     DVLOG(1) << __func__ << ": Not initialized.";
-    callback.Run(false);
+    std::move(callback).Run(false);
     return;
   }
 
@@ -145,19 +145,19 @@ void MediaDrmStorageImpl::OnProvisioned(const OnProvisionedCallback& callback) {
 
   storage_dict->SetWithoutPathExpansion(origin_string_,
                                         CreateOriginDictionary());
-  callback.Run(true);
+  std::move(callback).Run(true);
 }
 
 void MediaDrmStorageImpl::SavePersistentSession(
     const std::string& session_id,
     media::mojom::SessionDataPtr session_data,
-    const SavePersistentSessionCallback& callback) {
+    SavePersistentSessionCallback callback) {
   DVLOG(2) << __func__;
   DCHECK(thread_checker_.CalledOnValidThread());
 
   if (!initialized_) {
     DVLOG(1) << __func__ << ": Not initialized.";
-    callback.Run(false);
+    std::move(callback).Run(false);
     return;
   }
 
@@ -197,18 +197,18 @@ void MediaDrmStorageImpl::SavePersistentSession(
       session_id, CreateSessionDictionary(session_data->key_set_id,
                                           session_data->mime_type));
 
-  callback.Run(true);
+  std::move(callback).Run(true);
 }
 
 void MediaDrmStorageImpl::LoadPersistentSession(
     const std::string& session_id,
-    const LoadPersistentSessionCallback& callback) {
+    LoadPersistentSessionCallback callback) {
   DVLOG(2) << __func__;
   DCHECK(thread_checker_.CalledOnValidThread());
 
   if (!initialized_) {
     DVLOG(1) << __func__ << ": Not initialized.";
-    callback.Run(nullptr);
+    std::move(callback).Run(nullptr);
     return;
   }
 
@@ -222,14 +222,14 @@ void MediaDrmStorageImpl::LoadPersistentSession(
     DVLOG(1) << __func__
              << ": Failed to save persistent session data; entry for origin "
              << origin_ << " does not exist.";
-    callback.Run(nullptr);
+    std::move(callback).Run(nullptr);
     return;
   }
 
   const base::DictionaryValue* sessions_dict = nullptr;
   if (!origin_dict->GetDictionary(kSessions, &sessions_dict)) {
     DVLOG(2) << __func__ << ": Sessions dictionary does not exist.";
-    callback.Run(nullptr);
+    std::move(callback).Run(nullptr);
     return;
   }
 
@@ -237,7 +237,7 @@ void MediaDrmStorageImpl::LoadPersistentSession(
   if (!sessions_dict->GetDictionaryWithoutPathExpansion(session_id,
                                                         &session_dict)) {
     DVLOG(2) << __func__ << ": Session dictionary does not exist.";
-    callback.Run(nullptr);
+    std::move(callback).Run(nullptr);
     return;
   }
 
@@ -245,22 +245,23 @@ void MediaDrmStorageImpl::LoadPersistentSession(
   std::string mime_type;
   if (!GetSessionData(session_dict, &key_set_id, &mime_type)) {
     DVLOG(2) << __func__ << ": Failed to read session data.";
-    callback.Run(nullptr);
+    std::move(callback).Run(nullptr);
     return;
   }
 
-  callback.Run(media::mojom::SessionData::New(key_set_id, mime_type));
+  std::move(callback).Run(
+      media::mojom::SessionData::New(key_set_id, mime_type));
 }
 
 void MediaDrmStorageImpl::RemovePersistentSession(
     const std::string& session_id,
-    const RemovePersistentSessionCallback& callback) {
+    RemovePersistentSessionCallback callback) {
   DVLOG(2) << __func__;
   DCHECK(thread_checker_.CalledOnValidThread());
 
   if (!initialized_) {
     DVLOG(1) << __func__ << ": Not initialized.";
-    callback.Run(false);
+    std::move(callback).Run(false);
     return;
   }
 
@@ -274,19 +275,19 @@ void MediaDrmStorageImpl::RemovePersistentSession(
   if (!origin_dict) {
     DVLOG(1) << __func__ << ": Entry for rigin " << origin_string_
              << " does not exist.";
-    callback.Run(true);
+    std::move(callback).Run(true);
     return;
   }
 
   base::DictionaryValue* sessions_dict = nullptr;
   if (!origin_dict->GetDictionary(kSessions, &sessions_dict)) {
     DVLOG(2) << __func__ << ": Sessions dictionary does not exist.";
-    callback.Run(true);
+    std::move(callback).Run(true);
     return;
   }
 
   sessions_dict->RemoveWithoutPathExpansion(session_id, nullptr);
-  callback.Run(true);
+  std::move(callback).Run(true);
 }
 
 void MediaDrmStorageImpl::RenderFrameDeleted(
