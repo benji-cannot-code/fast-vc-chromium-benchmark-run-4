5d85c0d2a25116b79a4fd6f587dc3b500df57929
W. James MacLean <wjmaclean@chromium.org>
2017-07-10 15:52:30 +0000

    Introduce TouchSelection Editing Interfaces for PDF
    This CL introduces the interfaces TouchSelectionControllerClient and
    TouchSelectionMenuClient, and the basic logic to support (initially)
    copy on touch-selected content. It is not functional until the
    required plumbing to export selection updates from PDFium lands in a
    separate CL.
    
    Bug: 737585
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: Iaa5ed68a0590d3e2e27e5d2ae3d7b86ab5342e92
    Reviewed-on: https://chromium-review.googlesource.com/558771
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Dan Sinclair <dsinclair@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485281}

--- components/pdf/browser/pdf_web_contents_helper.h
+++ components/pdf/browser/pdf_web_contents_helper.h
@@ -12,9 +12,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/macros.h"
 #include "components/pdf/common/pdf.mojom.h"
+#include "content/public/browser/touch_selection_controller_client_manager.h"
 #include "content/public/browser/web_contents_binding_set.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
+#include "ui/touch_selection/selection_event_type.h"
+#include "ui/touch_selection/touch_selection_controller.h"
+#include "ui/touch_selection/touch_selection_menu_runner.h"
 
 namespace content {
 class WebContents;
@@ -28,7 +32,10 @@ class PDFWebContentsHelperClient;
 class PDFWebContentsHelper
     : public content::WebContentsObserver,
       public content::WebContentsUserData<PDFWebContentsHelper>,
-      public mojom::PdfService {
+      public mojom::PdfService,
+      public ui::TouchSelectionControllerClient,
+      public ui::TouchSelectionMenuClient,
+      public content::TouchSelectionControllerClientManager::Observer {
  public:
   ~PDFWebContentsHelper() override;
 
@@ -36,9 +43,35 @@ class PDFWebContentsHelper
       content::WebContents* contents,
       std::unique_ptr<PDFWebContentsHelperClient> client);
 
+  void SelectionChanged(const gfx::Point& left,
+                        int32_t left_height,
+                        const gfx::Point& right,
+                        int32_t right_height);
+
+  // ui::TouchSelectionControllerClient :
+  bool SupportsAnimation() const override;
+  void SetNeedsAnimate() override {}
+  void MoveCaret(const gfx::PointF& position) override;
+  void MoveRangeSelectionExtent(const gfx::PointF& extent) override;
+  void SelectBetweenCoordinates(const gfx::PointF& base,
+                                const gfx::PointF& extent) override;
+  void OnSelectionEvent(ui::SelectionEventType event) override;
+  std::unique_ptr<ui::TouchHandleDrawable> CreateDrawable() override;
+
+  // ui::TouchSelectionMenuRunner:
+  bool IsCommandIdEnabled(int command_id) const override;
+  void ExecuteCommand(int command_id, int event_flags) override;
+  void RunContextMenu() override;
+
+  // ui::TouchSelectionControllerClientManager::Observer:
+  void OnManagerWillDestroy(
+      content::TouchSelectionControllerClientManager* manager) override;
+
  private:
   PDFWebContentsHelper(content::WebContents* web_contents,
                        std::unique_ptr<PDFWebContentsHelperClient> client);
+
+  void InitTouchSelectionClientManager();
 
   // mojom::PdfService:
   void HasUnsupportedFeature() override;
@@ -47,6 +80,9 @@ class PDFWebContentsHelper
 
   content::WebContentsFrameBindingSet<mojom::PdfService> pdf_service_bindings_;
   std::unique_ptr<PDFWebContentsHelperClient> client_;
+  content::TouchSelectionControllerClientManager*
+      touch_selection_controller_client_manager_;
+  bool has_selection_;
 
   DISALLOW_COPY_AND_ASSIGN(PDFWebContentsHelper);
 };
