da391acc5c56aa70d59f7251581c78781190d47a
jzw <jzw@chromium.org>
2017-07-10 14:13:54 -0700

    Expose method to reset translate settings.
    Had to refactor browser states to be global instances and force off the record
    browser state to use an in-memory store instead of a json file persisted on disk.
    
    BUG=739987
    
    Review-Url: https://codereview.chromium.org/2965303002
    Cr-Commit-Position: refs/heads/master@{#485392}

--- ios/web_view/internal/cwv_web_view_configuration.mm
+++ ios/web_view/internal/cwv_web_view_configuration.mm
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ptr_util.h"
 #include "base/threading/thread_restrictions.h"
-#include "components/translate/core/browser/translate_download_manager.h"
 #include "ios/web_view/internal/app/application_context.h"
 #import "ios/web_view/internal/cwv_user_content_controller_internal.h"
 #include "ios/web_view/internal/web_view_browser_state.h"
@@ -33,14 +32,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @synthesize userContentController = _userContentController;
 
 + (instancetype)defaultConfiguration {
-  auto browserState =
-      base::MakeUnique<ios_web_view::WebViewBrowserState>(false);
-  return [[self alloc] initWithBrowserState:std::move(browserState)];
+  static CWVWebViewConfiguration* defaultConfiguration;
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    auto browserState =
+        base::MakeUnique<ios_web_view::WebViewBrowserState>(false);
+    defaultConfiguration =
+        [[self alloc] initWithBrowserState:std::move(browserState)];
+  });
+  return defaultConfiguration;
 }
 
 + (instancetype)incognitoConfiguration {
-  auto browserState = base::MakeUnique<ios_web_view::WebViewBrowserState>(true);
-  return [[self alloc] initWithBrowserState:std::move(browserState)];
+  static CWVWebViewConfiguration* incognitoConfiguration;
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    auto browserState =
+        base::MakeUnique<ios_web_view::WebViewBrowserState>(true);
+    incognitoConfiguration =
+        [[self alloc] initWithBrowserState:std::move(browserState)];
+  });
+  return incognitoConfiguration;
 }
 
 + (void)initialize {
@@ -63,9 +75,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return self;
 }
 
+#pragma mark - Public Methods
+
 - (BOOL)isPersistent {
   return !_browserState->IsOffTheRecord();
 }
+
+#pragma mark - Private Methods
 
 - (ios_web_view::WebViewBrowserState*)browserState {
   return _browserState.get();
