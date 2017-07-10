fdee344757a7a447fe6972ccce19f57d073d3f86
Friedrich Horschig <fhorschig@chromium.org>
2017-07-10 17:50:57 +0000

    [Respin] Reenable baked-in popular sites
    This cl complements https://chromium-review.googlesource.com/c/564614/
    in that it reenables the baked-in popular sites for iOS.
    The reason for not reverting the old CL is the includes that were needed
    to make the presubmit happy.
    
    Bug: 737607
    Change-Id: I24020f6949aff679bc80a918f61610d32056ceda
    Reviewed-on: https://chromium-review.googlesource.com/565565
    Reviewed-by: Chris Pickel <sfiera@chromium.org>
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485309}

--- components/ntp_tiles/popular_sites_impl.cc
+++ components/ntp_tiles/popular_sites_impl.cc
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_status_code.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 
-#if defined(OS_ANDROID)
+#if defined(OS_ANDROID) || defined(OS_IOS)
 #include "base/json/json_reader.h"
 #include "components/grit/components_resources.h"
 #include "ui/base/resource/resource_bundle.h"
@@ -133,7 +133,7 @@ PopularSites::SitesVector ParseSiteList(const base::ListValue& list) {
   return sites;
 }
 
-#if defined(GOOGLE_CHROME_BUILD) && defined(OS_ANDROID)
+#if defined(GOOGLE_CHROME_BUILD) && (defined(OS_ANDROID) || defined(OS_IOS))
 void SetDefaultResourceForSite(int index,
                                int resource_id,
                                base::ListValue* sites) {
@@ -147,7 +147,7 @@ void SetDefaultResourceForSite(int index,
 
 // Creates the list of popular sites based on a snapshot available for mobile.
 std::unique_ptr<base::ListValue> DefaultPopularSites() {
-#if !defined(OS_ANDROID)
+#if !defined(OS_ANDROID) && !defined(OS_IOS)
   return base::MakeUnique<base::ListValue>();
 #else
   if (!base::FeatureList::IsEnabled(kPopularSitesBakedInContentFeature)) {
