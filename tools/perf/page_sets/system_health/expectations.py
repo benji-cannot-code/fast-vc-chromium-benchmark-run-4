ac514c17750f8bef0498993dd063a23d56c50376
nednguyen <nednguyen@google.com>
2017-07-10 15:49:34 +0000

    [system_health] Reenable facebook_infinite_scroll
    TBR=perezju@chromium.org, ulan@chromium.org
    
    Bug: 728152
    Change-Id: I5b71e3b844c11a51dedc6be291885762ad373a0a
    Reviewed-on: https://chromium-review.googlesource.com/565184
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#485279}

--- tools/perf/page_sets/system_health/expectations.py
+++ tools/perf/page_sets/system_health/expectations.py
@@ -25,8 +25,6 @@ class SystemHealthDesktopCommonExpectations(expectations.StoryExpectations):
     self.DisableStory('browse:tech:discourse_infinite_scroll',
                       [expectations.ALL_WIN, expectations.ALL_LINUX],
                       'crbug.com/728152')
-    self.DisableStory('browse:social:facebook_infinite_scroll',
-                      [expectations.ALL_WIN], 'crbug.com/728152')
     self.DisableStory('browse:news:cnn',
                       [expectations.ALL_MAC], 'crbug.com/728576')
 
@@ -53,8 +51,6 @@ class SystemHealthDesktopMemoryExpectations(expectations.StoryExpectations):
     self.DisableStory('browse:tech:discourse_infinite_scroll',
                       [expectations.ALL_WIN, expectations.ALL_LINUX],
                       'crbug.com/728152')
-    self.DisableStory('browse:social:facebook_infinite_scroll',
-                      [expectations.ALL_WIN], 'crbug.com/728152')
     self.DisableStory('browse:news:cnn',
                       [expectations.ALL_MAC], 'crbug.com/728576')
     self.DisableStory('browse:social:twitter_infinite_scroll',
@@ -77,8 +73,6 @@ class SystemHealthMobileCommonExpectations(expectations.StoryExpectations):
                       [expectations.ALL_ANDROID], 'crbug.com/726301')
     self.DisableStory('browse:news:toi', [expectations.ALL_ANDROID],
                       'crbug.com/728081')
-    self.DisableStory('browse:social:facebook_infinite_scroll',
-                      [expectations.ALL], 'crbug.com/728152')
     self.DisableStory(
         'load:tools:drive',
         [expectations.ANDROID_NEXUS5X, expectations.ANDROID_WEBVIEW],
@@ -115,8 +109,6 @@ class SystemHealthMobileMemoryExpectations(expectations.StoryExpectations):
                       [expectations.ALL_ANDROID], 'crbug.com/726301')
     self.DisableStory('browse:news:toi', [expectations.ALL_ANDROID],
                       'crbug.com/728081')
-    self.DisableStory('browse:social:facebook_infinite_scroll',
-                      [expectations.ALL], 'crbug.com/728152')
     self.DisableStory(
         'load:tools:drive',
         [expectations.ANDROID_NEXUS5X, expectations.ANDROID_WEBVIEW],
@@ -166,8 +158,6 @@ class V8BrowsingMobileExpecations(expectations.StoryExpectations):
                       'crbug.com/714650')
     self.DisableStory('browse:news:toi', [expectations.ALL_ANDROID],
                       'crbug.com/728081')
-    self.DisableStory('browse:social:facebook_infinite_scroll',
-                      [expectations.ALL], 'crbug.com/728152')
     # TODO(rnephew): This disabling should move to CanRunOnBrowser.
     self.DisableStory('browse:chrome:omnibox',
                       [expectations.ANDROID_WEBVIEW],
