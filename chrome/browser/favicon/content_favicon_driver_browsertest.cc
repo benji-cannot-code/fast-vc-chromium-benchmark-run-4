5e2629441cbef0c4d768cd6f741fa6172baed512
Reilly Grant <reillyg@chromium.org>
2017-07-10 01:49:07 +0000

    Remove ShouldBeginRequest override from ContentFaviconDriverTest
    The ShouldBeginRequest override in TestResourceDispatcherHostDelegate
    is not necessary as it does not change the default behavior of
    
    content: :ResourceDispatcherHostDelegate.
    Change-Id: I4678bd494a5aa94698fffe2e1c8bafcabf1a8b71
    Reviewed-on: https://chromium-review.googlesource.com/563524
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485170}

--- chrome/browser/favicon/content_favicon_driver_browsertest.cc
+++ chrome/browser/favicon/content_favicon_driver_browsertest.cc
@@ -55,13 +55,6 @@ class TestResourceDispatcherHostDelegate
 
  private:
   // content::ResourceDispatcherHostDelegate:
-  bool ShouldBeginRequest(const std::string& method,
-                          const GURL& url,
-                          content::ResourceType resource_type,
-                          content::ResourceContext* resource_context) override {
-    return true;
-  }
-
   void RequestBeginning(net::URLRequest* request,
                         content::ResourceContext* resource_context,
                         content::AppCacheService* appcache_service,
