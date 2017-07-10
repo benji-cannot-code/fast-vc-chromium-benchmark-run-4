9370bd243a56350ec7bb038da44f699902adb6c6
Yoshifumi Inoue <yosin@chromium.org>
2017-07-10 08:33:57 +0000

    Make LineDirectionPointForBlockDirectionNavigation() to take const Position& instead of EPositionType
    This patch makes |LineDirectionPointForBlockDirectionNavigation()| to take
    |const Position&| and get rid of |EPositionType| enum to simplify source code
    for improving code health.
    
    Change-Id: I640c0391783ac4f8141742ee9495545fe2889fd9
    Reviewed-on: https://chromium-review.googlesource.com/564873
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485206}

--- third_party/WebKit/Source/core/editing/SelectionModifier.h
+++ third_party/WebKit/Source/core/editing/SelectionModifier.h
@@ -59,11 +59,6 @@ class SelectionModifier {
                                  VerticalDirection);
 
  private:
-  // TODO(yosin): We should move |EPositionType| to "SelectionModifier.cpp",
-  // it is only used for implementing |modify()|.
-  // TODO(yosin) We should use capitalized name for |EPositionType|.
-  enum EPositionType { START, END, BASE, EXTENT };  // NOLINT
-
   LocalFrame* GetFrame() const { return frame_; }
 
   static bool ShouldAlwaysUseDirectionalSelection(LocalFrame*);
@@ -72,7 +67,7 @@ class SelectionModifier {
   VisiblePosition PositionForPlatform(bool is_get_start) const;
   VisiblePosition StartForPlatform() const;
   VisiblePosition EndForPlatform() const;
-  LayoutUnit LineDirectionPointForBlockDirectionNavigation(EPositionType);
+  LayoutUnit LineDirectionPointForBlockDirectionNavigation(const Position&);
   VisiblePosition ModifyExtendingRight(TextGranularity);
   VisiblePosition ModifyExtendingForward(TextGranularity);
   VisiblePosition ModifyMovingRight(TextGranularity);
