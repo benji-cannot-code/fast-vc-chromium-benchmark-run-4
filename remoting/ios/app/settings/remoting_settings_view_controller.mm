7aac7434a97979b14ba96c00b1fb996ec228ddfa
Yuwei Huang <yuweih@chromium.org>
2017-07-10 18:59:09 +0000

    [CRD iOS] Use white status bar color in settings menu
    Use white status bar color to match the color of other texts.
    
    Change-Id: I56ff20106c139b9dbacdbeaad4d1b1f122bfb76c
    Reviewed-on: https://chromium-review.googlesource.com/564181
    Reviewed-by: Scott Nichols <nicholss@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485331}

--- remoting/ios/app/settings/remoting_settings_view_controller.mm
+++ remoting/ios/app/settings/remoting_settings_view_controller.mm
@@ -88,6 +88,10 @@ static NSString* const kFeedbackContext = @"InSessionFeedbackContext";
   [self loadContent];
 }
 
+- (UIStatusBarStyle)preferredStatusBarStyle {
+  return UIStatusBarStyleLightContent;
+}
+
 #pragma mark - UICollectionViewDataSource
 
 - (NSInteger)numberOfSectionsInCollectionView:
