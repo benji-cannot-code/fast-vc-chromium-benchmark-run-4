9d0c68a04975833903babc6c68bee35c67cf74df
Alex Clarke <alexclarke@chromium.org>
2017-07-10 14:32:42 +0000

    Headless: Wire up a basic NetLog if we're not logging to disk
    DevTools relies on a NetLogObserver (which needs a NetLog) to return the
    raw response headers, e.g. Set-Cookie headers.
    
    Bug: 692090
    Change-Id: I9faae71198b7c45d1dc05175b606452012d7ec83
    Reviewed-on: https://chromium-review.googlesource.com/565139
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485265}

--- headless/lib/browser/headless_browser_main_parts.h
+++ headless/lib/browser/headless_browser_main_parts.h
@@ -12,9 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_main_parts.h"
 #include "headless/public/headless_browser.h"
 
+namespace net {
+class NetLog;
+}  // namespace net
+
 namespace headless {
 
-class HeadlessNetLog;
 class HeadlessBrowserImpl;
 
 class HeadlessBrowserMainParts : public content::BrowserMainParts {
@@ -29,13 +32,13 @@ class HeadlessBrowserMainParts : public content::BrowserMainParts {
   void PreMainMessageLoopStart() override;
 #endif
 
-  HeadlessNetLog* net_log() const { return net_log_.get(); }
+  net::NetLog* net_log() const { return net_log_.get(); }
 
  private:
   HeadlessBrowserImpl* browser_;  // Not owned.
 
   bool devtools_http_handler_started_;
-  std::unique_ptr<HeadlessNetLog> net_log_;
+  std::unique_ptr<net::NetLog> net_log_;
 
   DISALLOW_COPY_AND_ASSIGN(HeadlessBrowserMainParts);
 };
--- headless/lib/browser/headless_content_browser_client.cc
+++ headless/lib/browser/headless_content_browser_client.cc
@@ -240,4 +240,8 @@ void HeadlessContentBrowserClient::ResourceDispatcherHostCreated() {
       resource_dispatcher_host_delegate_.get());
 }
 
+net::NetLog* HeadlessContentBrowserClient::GetNetLog() {
+  return browser_->browser_main_parts()->net_log();
+}
+
 }  // namespace headless
