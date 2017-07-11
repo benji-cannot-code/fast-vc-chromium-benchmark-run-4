6e803df1ecc521ec85c55cc857dc17641c70183a
Thanh Pham <thanhph@chromium.org>
2017-07-11 04:23:22 +0000

    Use ui::layer for highlighting instead of widget.
    Currently Devtools uses widget to highlight elements, i.e.,
    windows, widgets, and views. We instead can ui::layer for highlighting
    and to support future drawing/animation.
    
    Bug: 730828
    Change-Id: Ic84a79c382e2e3a2bcf0b9cba421d2cb9a8e72bc
    Reviewed-on: https://chromium-review.googlesource.com/564036
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485519}

--- components/ui_devtools/views/ui_devtools_unittest.cc
+++ components/ui_devtools/views/ui_devtools_unittest.cc
@@ -140,11 +140,10 @@ int GetPropertyByName(const std::string& name,
   return -1;
 }
 
-aura::Window* GetHighlightingWindow(aura::Window* root_window) {
-  const aura::Window::Windows& overlay_windows = root_window->children();
-  for (aura::Window* window : overlay_windows) {
-    if (window->GetName() == "HighlightingWidget")
-      return window;
+ui::Layer* GetHighlightingLayer(aura::Window* highlight_window) {
+  for (auto* layer : highlight_window->layer()->children()) {
+    if (layer->name() == "HighlightingLayer")
+      return layer;
   }
   NOTREACHED();
   return nullptr;
@@ -168,15 +167,12 @@ std::unique_ptr<DOM::HighlightConfig> CreateHighlightConfig(
       .build();
 }
 
-void ExpectHighlighted(const gfx::Rect& bounds, aura::Window* root_window) {
-  aura::Window* highlighting_window = GetHighlightingWindow(root_window);
-  EXPECT_TRUE(highlighting_window->IsVisible());
-  EXPECT_EQ(bounds, highlighting_window->GetBoundsInScreen());
-  EXPECT_EQ(kBackgroundColor,
-            views::Widget::GetWidgetForNativeView(highlighting_window)
-                ->GetRootView()
-                ->background()
-                ->get_color());
+void ExpectHighlighted(const gfx::Rect& bounds,
+                       aura::Window* highlight_window) {
+  ui::Layer* highlighting_layer = GetHighlightingLayer(highlight_window);
+  EXPECT_TRUE(highlighting_layer->visible());
+  EXPECT_EQ(bounds, highlighting_layer->bounds());
+  EXPECT_EQ(kBackgroundColor, highlighting_layer->GetTargetColor());
 }
 
 }  // namespace
@@ -315,8 +311,8 @@ class UIDevToolsTest : public views::ViewsTestBase {
     DCHECK_LE(root_window_index,
               static_cast<int>(dom_agent()->root_windows().size()));
     ASSERT_FALSE(
-        GetHighlightingWindow(dom_agent()->root_windows()[root_window_index])
-            ->IsVisible());
+        GetHighlightingLayer(dom_agent()->root_windows()[root_window_index])
+            ->visible());
   }
 
   FakeFrontendChannel* frontend_channel() {
@@ -727,7 +723,7 @@ TEST_F(UIDevToolsTest, WindowWidgetViewHighlight) {
 
   // Highlight non-existent node
   HighlightNode(10000);
-  EXPECT_FALSE(GetHighlightingWindow(GetPrimaryRootWindow())->IsVisible());
+  EXPECT_FALSE(GetHighlightingLayer(GetPrimaryRootWindow())->visible());
 }
 
 int GetNodeIdFromWindow(ui_devtools::UIElement* ui_element,
