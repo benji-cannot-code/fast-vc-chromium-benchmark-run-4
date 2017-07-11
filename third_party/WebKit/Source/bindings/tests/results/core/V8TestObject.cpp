ec0b137bf9358c24db37bf5f71960feb9331a32b
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2017-07-11 06:59:38 +0000

    Remove the Transferable typedef from Window.idl.
    The typedef was added back in 2014 in commit 38b7b40abe5848dc ("Improve the
    declared IDL type for postMessage()'s transferables"), when the HTML Living
    Standard defined a Transferable typedef like this:
    
        typedef (ArrayBuffer or CanvasProxy or MessagePort) Transferable;
    
    since the bindings layer did not support unions at the time and all uses of
    Transferable in our IDLs amounted to MessagePort objects, the current
    "typedef MessagePort Transferable" was introduced for compliance.
    
    https://github.com/whatwg/html/commit/bfb960c938580c95e77365e614218b952f96375b ("Write
    structured clone algorithm in terms of ECMAScript") got rid of the typedef
    in the spec altogether though, so we were again not compliant in our IDLs.
    
    This CL makes the switch by using object instead of Transferable in the
    IDLs.
    
    In practice, this does not make any difference, as |transfer| is always
    parsed via SerializedScriptValue::ExtractTransferables(), which does all the
    argument parsing on its own.
    
    While here, also make a few other IDL updates that do not change the
    generated bindings code, in some cases due to the fact that the
    postMessage() bindings code is more customized than usual:
    - Add a "= []" to the "optional sequence<object>" postMessage() arguments.
    - Change |message|'s type from "SerializedScriptValue" to "any" in some
      postMessage() declarations to match the spec(s).
    - Update the spec link in ServiceWorker.idl.
    
    Bug: 701482
    Change-Id: I0a5c0dc7fed8f3498ccd072b4422fb269e5def97
    Reviewed-on: https://chromium-review.googlesource.com/561459
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Raphael Kubo da Costa (rakuco) <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/master@{#485542}

--- third_party/WebKit/Source/bindings/tests/results/core/V8TestObject.cpp
+++ third_party/WebKit/Source/bindings/tests/results/core/V8TestObject.cpp
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/V8HTMLElement.h"
 #include "bindings/core/v8/V8Int32Array.h"
 #include "bindings/core/v8/V8Iterator.h"
-#include "bindings/core/v8/V8MessagePort.h"
 #include "bindings/core/v8/V8Node.h"
 #include "bindings/core/v8/V8NodeFilterCondition.h"
 #include "bindings/core/v8/V8ShadowRoot.h"
