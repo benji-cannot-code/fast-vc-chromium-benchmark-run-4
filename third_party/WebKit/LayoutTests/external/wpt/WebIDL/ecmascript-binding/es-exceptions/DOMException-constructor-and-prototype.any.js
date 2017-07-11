d9ea25f150a912e149e2f9636cf280b2a1727b24
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2017-07-11 15:51:03 +0000

    Import wpt@683fb5ad785084f35cac8ed9740a18ceaf3fca37
    Using wpt-import in Chromium 90fd8c3e191c33de5a342c77d059e9ecdbd80bb4.
    
    Build: https://build.chromium.org/p/chromium.infra.cron/builders/wpt-importer/builds/297
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    blink-network-dev@chromium.org:
      external/wpt/url
    dom-dev@chromium.org:
      external/wpt/dom
    hta@chromium.org:
      external/wpt/webrtc
    igrigorik@chromium.org, panicker@chromium.org:
      external/wpt/performance-timeline
    jsbell@chromium.org:
      external/wpt/hr-time
      external/wpt/WebIDL
    ksakamoto@chromium.org:
      external/wpt/resource-timing
    panicker@chromium.org:
      external/wpt/page-visibility
    rouslan@chromium.org, mathp@chromium.org:
      external/wpt/payment-request
    sunjian@chromium.org, panicker@chromium.org:
      external/wpt/navigation-timing
    
    TBR: qyearsley@chromium.org
    No-Export: true
    Change-Id: Id9d70fe81ecd1d34a10b7a8af4f7d909a51ba164
    Reviewed-on: https://chromium-review.googlesource.com/566218
    Commit-Queue: Quinten Yearsley <qyearsley@chromium.org>
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485638}

---
+++ third_party/WebKit/LayoutTests/external/wpt/WebIDL/ecmascript-binding/es-exceptions/DOMException-constructor-and-prototype.any.js
@@ -0,0 +1,33 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+test(function() {
+    assert_own_property(self, "DOMException", "property of global");
+
+    var desc = Object.getOwnPropertyDescriptor(self, "DOMException");
+    assert_false("get" in desc, "get");
+    assert_false("set" in desc, "set");
+    assert_true(desc.writable, "writable");
+    assert_false(desc.enumerable, "enumerable");
+    assert_true(desc.configurable, "configurable");
+}, "existence and property descriptor of DOMException");
+
+test(function() {
+    assert_own_property(self.DOMException, "prototype", "prototype property");
+
+    var desc = Object.getOwnPropertyDescriptor(self.DOMException, "prototype");
+    assert_false("get" in desc, "get");
+    assert_false("set" in desc, "set");
+    assert_false(desc.writable, "writable");
+    assert_false(desc.enumerable, "enumerable");
+    assert_false(desc.configurable, "configurable");
+}, "existence and property descriptor of DOMException.prototype");
+
+test(function() {
+    assert_own_property(self.DOMException.prototype, "constructor", "property of prototype");
+    var desc = Object.getOwnPropertyDescriptor(self.DOMException.prototype, "constructor");
+    assert_false("get" in desc, "get");
+    assert_false("set" in desc, "set");
+    assert_true(desc.writable, "writable");
+    assert_false(desc.enumerable, "enumerable");
+    assert_true(desc.configurable, "configurable");
+    assert_equals(self.DOMException.prototype.constructor, self.DOMException, "equality with actual constructor");
+}, "existence and property descriptor of DOMException.prototype.constructor");
