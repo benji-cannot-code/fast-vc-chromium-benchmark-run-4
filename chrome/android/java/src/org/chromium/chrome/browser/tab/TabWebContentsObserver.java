07bbe803b2c498db0ee68d3f601b7d166a86db4a
wychen <wychen@chromium.org>
2017-07-10 17:49:41 -0700

    Skip TabObserver#onUrlUpdated when not in main frame
    onUrlUpdated() should be called when the URL is updated. It doesn't
    make sense to call onUrlUpdated() at didFinishNavigation() of the
    iframes, since the main URL is not updated.
    
    onUrlUpdated() triggers GoogleApiIcingClientImpl#reportContext(), and
    excessive calls to onUrlUpdated() translates to excessive Icing calls.
    This CL lowers reporting per navigation to <5 roughly.
    
    In UMA, total count and Success bucket count of
    Search.IcingContextReportingStatus per navigation should decrease.
    
    BUG=739570
    
    Review-Url: https://codereview.chromium.org/2971953002
    Cr-Commit-Position: refs/heads/master@{#485468}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabWebContentsObserver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabWebContentsObserver.java
@@ -232,11 +232,11 @@ public class TabWebContentsObserver extends WebContentsObserver {
             mTab.updateTitle();
             mTab.handleDidFinishNavigation(url, pageTransition);
             mTab.setIsShowingErrorPage(isErrorPage);
-        }
 
-        observers.rewind();
-        while (observers.hasNext()) {
-            observers.next().onUrlUpdated(mTab);
+            observers.rewind();
+            while (observers.hasNext()) {
+                observers.next().onUrlUpdated(mTab);
+            }
         }
 
         FullscreenManager fullscreenManager = mTab.getFullscreenManager();
