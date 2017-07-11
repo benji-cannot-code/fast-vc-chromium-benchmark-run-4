016b9c8483b4ead3d5ea45b20adff82cfe508d9e
gambard <gambard@chromium.org>
2017-07-11 11:34:10 +0000

    Change the color of ContentSuggestions titles
    The section titles are not visible enough in ContentSuggestions.
    Changing the color to match the one used for other titles should help.
    
    BUG=740863
    
    Change-Id: I9dd640ec6ec6cb0df734200987c06fd13ce39f50
    Reviewed-on: https://chromium-review.googlesource.com/566823
    Reviewed-by: Elodie Banel <lod@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485589}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_updater.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_updater.mm
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller_audience.h"
 #import "ios/chrome/browser/ui/content_suggestions/identifier/content_suggestion_identifier.h"
 #import "ios/chrome/browser/ui/content_suggestions/identifier/content_suggestions_section_information.h"
+#import "ios/third_party/material_components_ios/src/components/Palettes/src/MaterialPalettes.h"
 #include "ui/base/l10n/l10n_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -527,6 +528,7 @@ addSuggestionsToModel:(NSArray<CSCollectionViewItem*>*)suggestions
     CollectionViewTextItem* header =
         [[CollectionViewTextItem alloc] initWithType:ItemTypeHeader];
     header.text = sectionInfo.title;
+    header.textColor = [[MDCPalette greyPalette] tint500];
     [self.collectionViewController.collectionViewModel
                        setHeader:header
         forSectionWithIdentifier:sectionIdentifier];
