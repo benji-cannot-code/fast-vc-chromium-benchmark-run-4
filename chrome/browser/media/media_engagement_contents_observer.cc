6678bc97817f411306ece2ce5b4994141b2b9781
Becca Hughes <beccahughes@chromium.org>
2017-07-11 12:08:01 +0000

    Media Engagement: Fix muted status propagation and timer bug.
    The playbacks were not be recoreded correctly because muted state
    was not propagating through to the WebContentsObserver correctly.
    There was also a bug where the timer didn't have the time-delta
    set correctly so it was never being called.
    
    BUG=731750
    
    Change-Id: I2944679adbb88b9f37c1a88efa97652ff032d9b1
    Reviewed-on: https://chromium-review.googlesource.com/563668
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485599}

--- chrome/browser/media/media_engagement_contents_observer.cc
+++ chrome/browser/media/media_engagement_contents_observer.cc
@@ -9,8 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents.h"
 
-constexpr base::TimeDelta
-    MediaEngagementContentsObserver::kSignificantMediaPlaybackTime;
+namespace {
+
+constexpr base::TimeDelta kSignificantMediaPlaybackTime =
+    base::TimeDelta::FromSeconds(7);
+
+}  // namespace.
 
 // This is the minimum size (in px) of each dimension that a media
 // element has to be in order to be determined significant.
@@ -87,8 +91,6 @@ MediaEngagementContentsObserver::GetPlayerState(const MediaPlayerId& id) {
 void MediaEngagementContentsObserver::MediaStartedPlaying(
     const MediaPlayerInfo& media_player_info,
     const MediaPlayerId& media_player_id) {
-  // TODO(mlamouri): check if:
-  // - the playback has the minimum size requirements;
   if (!media_player_info.has_audio)
     return;
 
@@ -97,12 +99,12 @@ void MediaEngagementContentsObserver::MediaStartedPlaying(
   UpdateTimer();
 }
 
-void MediaEngagementContentsObserver::MediaMutedStateChanged(
+void MediaEngagementContentsObserver::MediaMutedStatusChanged(
     const MediaPlayerId& id,
-    bool muted_state) {
-  GetPlayerState(id)->muted = muted_state;
+    bool muted) {
+  GetPlayerState(id)->muted = muted;
 
-  if (muted_state)
+  if (muted)
     MaybeRemoveSignificantPlayer(id);
   else
     MaybeInsertSignificantPlayer(id);
@@ -189,6 +191,7 @@ void MediaEngagementContentsObserver::UpdateTimer() {
   if (AreConditionsMet()) {
     if (playback_timer_->IsRunning())
       return;
+
     playback_timer_->Start(
         FROM_HERE, kSignificantMediaPlaybackTime,
         base::Bind(
