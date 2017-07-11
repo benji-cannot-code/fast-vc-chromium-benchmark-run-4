3ec553311b37bdfcff0ad8545221632cbe2d691e
Theresa Wellington <twellington@google.com>
2017-07-11 16:14:18 +0000

    Mark Android bookmark tests @RetryOnFailure
    This reverts commit 5b5eabade0bbcd861198599ff65c33a9b8843b2f
    and adds @RetryOnFailure to the bookmarks tests. These tests are
    timing out on loading the bookmark model. No culprit has been
    identified.
    
    TBR=twellington@chromium.org
    BUG=740786,739581
    
    Change-Id: If677168ca35b955a2a23ee5e81d1f8cc8338033a
    Reviewed-on: https://chromium-review.googlesource.com/566890
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485645}

--- chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkBridgeTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkBridgeTest.java
@@ -18,8 +18,8 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.browser.bookmarks.BookmarkBridge.BookmarkItem;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.test.ChromeBrowserTestRule;
@@ -34,6 +34,7 @@ import java.util.List;
 /**
  * Tests for bookmark bridge
  */
+@RetryOnFailure(message = "crbug.com/740786")
 @RunWith(BaseJUnit4ClassRunner.class)
 public class BookmarkBridgeTest {
     @Rule
@@ -140,7 +141,6 @@ public class BookmarkBridgeTest {
     @Test
     @SmallTest
     @UiThreadTest
-    @DisabledTest // Timing out on the try bot. http://crbug.com/740786
     @Feature({"Bookmark"})
     public void testGetMoveDestinations() throws Throwable {
         BookmarkId folderA = mBookmarkBridge.addFolder(mMobileNode, 0, "a");
