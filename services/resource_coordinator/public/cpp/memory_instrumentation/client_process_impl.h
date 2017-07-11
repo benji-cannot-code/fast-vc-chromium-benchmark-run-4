d24983ef2aa97de44918393b248b56320f98e82b
Siddhartha <ssid@chromium.org>
2017-07-11 01:13:10 +0000

    [memory-infra] Move the tests related to tracing observer into service/
    Move the tests related to tracing observer, peak detector and periodic
    schduler into client_process_impl_unittests.
    Removed test CallbackCalledOnFailure since it is covered by
    DumpWithoutInitializingTracing
    
    Bug: 728199
    
    Change-Id: Idb2fb0ed566e54ab6f6be825f844e2ffab9b4d16
    Reviewed-on: https://chromium-review.googlesource.com/553585
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485477}

--- services/resource_coordinator/public/cpp/memory_instrumentation/client_process_impl.h
+++ services/resource_coordinator/public/cpp/memory_instrumentation/client_process_impl.h
@@ -38,6 +38,7 @@ class SERVICES_RESOURCE_COORDINATOR_PUBLIC_CPP_EXPORT ClientProcessImpl
     ~Config();
 
     service_manager::Connector* const connector;
+    Coordinator* coordinator_for_testing;
     const std::string service_name;
     const mojom::ProcessType process_type;
   };
@@ -46,7 +47,7 @@ class SERVICES_RESOURCE_COORDINATOR_PUBLIC_CPP_EXPORT ClientProcessImpl
 
  private:
   friend std::default_delete<ClientProcessImpl>;  // For testing
-  friend class ClientProcessImplTest;
+  friend class MemoryTracingIntegrationTest;
 
   ClientProcessImpl(const Config& config);
   ~ClientProcessImpl() override;
