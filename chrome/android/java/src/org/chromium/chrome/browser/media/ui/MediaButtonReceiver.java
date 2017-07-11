27af564b776c9f76060032382db828f344c4ddd1
Mounir Lamouri <mlamouri@chromium.org>
2017-07-11 16:56:08 +0000

    Use AppHooks to start foreground service from MediaButtonReceiver.
    Bug: 732227
    Change-Id: Ib38b9a99a4c08bdb973f0a98ec941ffc04e6d238
    Reviewed-on: https://chromium-review.googlesource.com/565500
    Reviewed-by: Anton Vayvod <avayvod@chromium.org>
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485653}

--- chrome/android/java/src/org/chromium/chrome/browser/media/ui/MediaButtonReceiver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/ui/MediaButtonReceiver.java
@@ -9,6 +9,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import org.chromium.chrome.browser.AppHooks;
+
 /**
  * MediaButtonReceiver is a basic BroadcastReceiver class that receives
  * ACTION_MEDIA_BUTTON from a MediaSessionCompat. It then forward these intents
@@ -21,6 +23,6 @@ public abstract class MediaButtonReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         intent.setClass(context, getServiceClass());
-        context.startService(intent);
+        AppHooks.get().startForegroundService(intent);
     }
 }
