a7d95d8d4991c3caeae1357126584fd9c6c83f00
rbpotter <rbpotter@chromium.org>
2017-07-10 17:44:40 -0700

    Print Preview: Fix followup issues for removing global JS functions
    Some followup changes from https://codereview.chromium.org/2969383003/
    
    BUG=717296
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2975663003
    Cr-Commit-Position: refs/heads/master@{#485466}

--- chrome/browser/resources/print_preview/print_preview.js
+++ chrome/browser/resources/print_preview/print_preview.js
@@ -85,7 +85,8 @@ cr.define('print_preview', function() {
      * @private
      */
     this.destinationStore_ = new print_preview.DestinationStore(
-        this.nativeLayer_, this.userInfo_, this.appState_);
+        this.nativeLayer_, this.userInfo_, this.appState_,
+        this.listenerTracker);
 
     /**
      * Data store which holds printer sharing invitations.
@@ -340,7 +341,6 @@ cr.define('print_preview', function() {
       print_preview.PrintPreviewFocusManager.getInstance().initialize();
       cr.ui.FocusOutlineManager.forDocument(document);
       this.listenerTracker.add('print-failed', this.onPrintFailed_.bind(this));
-      this.destinationStore_.addWebUIEventListeners(this.listenerTracker);
       this.listenerTracker.add(
           'use-cloud-print', this.onCloudPrintEnable_.bind(this));
       this.listenerTracker.add(
