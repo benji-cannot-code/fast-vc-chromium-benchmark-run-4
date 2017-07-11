177d04abd845fc20a81e65f5e571c42628cb236c
Takashi Toyoshima <toyoshim@chromium.org>
2017-07-11 08:22:29 +0000

    Loading Dispatcher: plumb field trial parameters
    This patch introduces a field trial for the ResourceLoadScheduler
    and makes the feature configurable through chrome://flags and
    field trial parameters.
    
    Bug: 729953
    Change-Id: Id8619ad8979669748e94a9988c0a46ef6a318726
    Reviewed-on: https://chromium-review.googlesource.com/558902
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485552}

--- content/public/common/content_features.h
+++ content/public/common/content_features.h
@@ -66,6 +66,7 @@ CONTENT_EXPORT extern const base::Feature kRafAlignedTouchInputEvents;
 CONTENT_EXPORT extern const base::Feature kRenderingPipelineThrottling;
 CONTENT_EXPORT extern const base::Feature
     kRequireSecureOriginsForPepperMediaRequests;
+CONTENT_EXPORT extern const base::Feature kResourceLoadScheduler;
 CONTENT_EXPORT extern const base::Feature kScrollAnchoring;
 CONTENT_EXPORT extern const base::Feature kServiceWorkerNavigationPreload;
 CONTENT_EXPORT extern const base::Feature kServiceWorkerScriptStreaming;
