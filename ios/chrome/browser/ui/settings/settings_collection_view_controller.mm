9f84707c3c081a6a1c606b9d925ec94b9c975b9a
Jérôme Lebel <jlebel@chromium.org>
2017-07-11 10:02:07 +0000

    Fixing impression count for settings.
    The "ImpressionsTilDismiss" should be sent only when the setting view
    is closed (it was sent when going into a submenu of the settings).
    The display count should only be increased when opening the settings.
    Before it was increased for each data reload of the collection view.
    
    Bug: 709286
    Change-Id: I301f96241f7704aa01695fc41e841364fca1f80a
    Reviewed-on: https://chromium-review.googlesource.com/563389
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Louis Romero <lpromero@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485574}

--- ios/chrome/browser/ui/settings/settings_collection_view_controller.mm
+++ ios/chrome/browser/ui/settings/settings_collection_view_controller.mm
@@ -293,6 +293,14 @@ void SigninObserverBridge::GoogleSignedOut(const std::string& account_id,
 
 - (void)dealloc {
   [self stopBrowserStateServiceObservers];
+  if (!_signinStarted && _signinPromoViewMediator) {
+    PrefService* prefs = _browserState->GetPrefs();
+    int displayedCount =
+        prefs->GetInteger(prefs::kIosSettingsSigninPromoDisplayedCount);
+    UMA_HISTOGRAM_COUNTS_100(
+        "MobileSignInPromo.SettingsManager.ImpressionsTilDismiss",
+        displayedCount);
+  }
 }
 
 - (void)stopBrowserStateServiceObservers {
@@ -314,18 +322,6 @@ void SigninObserverBridge::GoogleSignedOut(const std::string& account_id,
 - (void)viewWillAppear:(BOOL)animated {
   [super viewWillAppear:animated];
   [self updateSearchCell];
-}
-
-- (void)viewDidDisappear:(BOOL)animated {
-  [super viewDidDisappear:animated];
-  if (!_signinStarted && _signinPromoViewMediator) {
-    PrefService* prefs = _browserState->GetPrefs();
-    int displayedCount =
-        prefs->GetInteger(prefs::kIosSettingsSigninPromoDisplayedCount);
-    UMA_HISTOGRAM_COUNTS_100(
-        "MobileSignInPromo.SettingsManager.ImpressionsTilDismiss",
-        displayedCount);
-  }
 }
 
 #pragma mark SettingsRootCollectionViewController
@@ -352,11 +348,13 @@ void SigninObserverBridge::GoogleSignedOut(const std::string& account_id,
         prefs->GetInteger(prefs::kIosSettingsSigninPromoDisplayedCount);
     if (experimental_flags::IsSigninPromoEnabled() &&
         displayedCount < kAutomaticSigninPromoViewDismissCount) {
-      _signinPromoViewMediator =
-          [[SigninPromoViewMediator alloc] initWithBrowserState:_browserState];
-      _signinPromoViewMediator.consumer = self;
-      prefs->SetInteger(prefs::kIosSettingsSigninPromoDisplayedCount,
-                        displayedCount + 1);
+      if (!_signinPromoViewMediator) {
+        _signinPromoViewMediator = [[SigninPromoViewMediator alloc]
+            initWithBrowserState:_browserState];
+        _signinPromoViewMediator.consumer = self;
+        prefs->SetInteger(prefs::kIosSettingsSigninPromoDisplayedCount,
+                          displayedCount + 1);
+      }
     }
     [model addItem:[self signInTextItem]
         toSectionWithIdentifier:SectionIdentifierSignIn];
