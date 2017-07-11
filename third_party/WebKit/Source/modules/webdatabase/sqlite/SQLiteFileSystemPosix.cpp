f7c45057009815e15802b88669703d9bfce186ad
Daniel Bratell <bratell@opera.com>
2017-07-11 08:35:18 +0000

    Use the same sqlite header configuration in all of webdatabase
    Some of third_party/WebKit/Source/webdatabase uses sqlite withq
    internals exposed and some uses it with internals not exposed. That
    mismatch causes issues in jumbo builds since it compiles several files
    together and need headers to say the same thing.
    
    This changes webdatabase so that it all compile with the
    CHROMIUM_SQLITE_INTERNALS define set.
    
    R=michaeln@chromium.org
    
    Bug: 740542
    Change-Id: I0b5c66fde7d191afd7a7143ef32c9c6f1b21b9e2
    Reviewed-on: https://chromium-review.googlesource.com/565502
    Reviewed-by: Michael Nordman <michaeln@chromium.org>
    Commit-Queue: bratell at Opera <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#485558}

--- third_party/WebKit/Source/modules/webdatabase/sqlite/SQLiteFileSystemPosix.cpp
+++ third_party/WebKit/Source/modules/webdatabase/sqlite/SQLiteFileSystemPosix.cpp
@@ -29,9 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-// Expose chromium_sqlite3_* functions from Chromium's patched SQLite.
-#define CHROMIUM_SQLITE_INTERNALS
-
 #include "modules/webdatabase/sqlite/SQLiteFileSystem.h"
 
 #include "public/platform/Platform.h"
