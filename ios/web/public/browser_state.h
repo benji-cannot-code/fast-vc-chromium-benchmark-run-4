4e3e2aa6a995d4b375be56ea8afea0ec09f247d7
Colin Blundell <blundell@chromium.org>
2017-07-11 07:24:12 +0000

    [iOS] Bring up support for per-user services on iOS
    This CL augments the existing ServiceManager embedding on iOS with
    support for per-user embedded services. A "per-user" service is one
    that has one instance per user, rather than a shared instance for all
    users. Concretely, each BrowserState has a unique UserID, and there is
    one instance of a per-user service per UserID. Essentially, one can
    think of per-user services as the equivalent of profile-keyed services
    in the services world.
    
    The support added in this CL closely follows that of //content:
    
    - Public APIs are identical
    - Implementation in BrowserState follows that of BrowserContext with minor
      changes:
      - iOS doesn't use File Service at this time
      - No client of //ios/web needs to override how a user ID is generated
    
    This CL adds an Earl Grey test of web_shell that a per-user service can be
    embedded and if connected to from a given BrowserState, has its user ID
    properly associated with that of the BrowserState.
    
    Finally, this added support requires that all BrowserState subclasses call
    BrowserState::Initialize(). This is also done in this CL.
    
    Bug: 731588
    Change-Id: Ie3bb79d4f754aecddd9c35c72576262dfc87cb55
    Reviewed-on: https://chromium-review.googlesource.com/559529
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485547}

--- ios/web/public/browser_state.h
+++ ios/web/public/browser_state.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_WEB_PUBLIC_BROWSER_STATE_H_
 
 #include "base/supports_user_data.h"
+#include "services/service_manager/embedder/embedded_service_info.h"
 
 namespace base {
 class FilePath;
@@ -16,9 +17,14 @@ namespace net {
 class URLRequestContextGetter;
 }
 
+namespace service_manager {
+class Connector;
+}
+
 namespace web {
 class ActiveStateManager;
 class CertificatePolicyCache;
+class ServiceManagerConnection;
 class URLDataManagerIOS;
 class URLDataManagerIOSBackend;
 class URLRequestChromeJob;
@@ -61,8 +67,36 @@ class BrowserState : public base::SupportsUserData {
   static BrowserState* FromSupportsUserData(
       base::SupportsUserData* supports_user_data);
 
+  // Returns a Service User ID associated with this BrowserState. This ID is
+  // not persistent across runs. See
+  // services/service_manager/public/interfaces/connector.mojom. By default,
+  // this user id is randomly generated when Initialize() is called.
+  static const std::string& GetServiceUserIdFor(BrowserState* browser_state);
+
+  // Returns a Connector associated with this BrowserState, which can be used
+  // to connect to service instances bound as this user.
+  static service_manager::Connector* GetConnectorFor(
+      BrowserState* browser_state);
+
+  // Returns a ServiceManagerConnection associated with this BrowserState,
+  // which can be used to connect to service instances bound as this user.
+  static ServiceManagerConnection* GetServiceManagerConnectionFor(
+      BrowserState* browser_state);
+
+  using StaticServiceMap =
+      std::map<std::string, service_manager::EmbeddedServiceInfo>;
+
+  // Registers per-browser-state services to be loaded by the Service Manager.
+  virtual void RegisterServices(StaticServiceMap* services) {}
+
  protected:
   BrowserState();
+
+  // Makes the Service Manager aware of this BrowserState, and assigns a user
+  // ID number to it. Must be called for each BrowserState created. |path|
+  // should be the same path that would be returned by GetStatePath().
+  static void Initialize(BrowserState* browser_state,
+                         const base::FilePath& path);
 
  private:
   friend class URLDataManagerIOS;
