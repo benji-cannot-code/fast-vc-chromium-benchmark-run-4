6dfecd6afaf5b09986f03910ea966fbd06981274
Yoshifumi Inoue <yosin@chromium.org>
2017-07-11 11:23:26 +0000

    Get rid of useless member variable VisibleSelection::granularity_
    This patch gets rid of useless member variable |granularity_| from
    |VisibleSelectionTemplate| for improving code health.
    
    Bug: 692923
    Change-Id: I0ed67a6076e4c34496cf8c6eaf39f497572f614d
    Reviewed-on: https://chromium-review.googlesource.com/566200
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485583}

--- third_party/WebKit/Source/core/editing/VisibleSelection.cpp
+++ third_party/WebKit/Source/core/editing/VisibleSelection.cpp
@@ -46,8 +46,7 @@ VisibleSelectionTemplate<Strategy>::VisibleSelectionTemplate()
     : affinity_(TextAffinity::kDownstream),
       selection_type_(kNoSelection),
       base_is_first_(true),
-      is_directional_(false),
-      granularity_(TextGranularity::kCharacter) {}
+      is_directional_(false) {}
 
 template <typename Strategy>
 VisibleSelectionTemplate<Strategy>::VisibleSelectionTemplate(
@@ -57,9 +56,8 @@ VisibleSelectionTemplate<Strategy>::VisibleSelectionTemplate(
       extent_(selection.Extent()),
       affinity_(selection.Affinity()),
       selection_type_(kNoSelection),
-      is_directional_(selection.IsDirectional()),
-      granularity_(granularity) {
-  Validate(granularity_);
+      is_directional_(selection.IsDirectional()) {
+  Validate(granularity);
 }
 
 template <typename Strategy>
@@ -124,8 +122,7 @@ VisibleSelectionTemplate<Strategy>::VisibleSelectionTemplate(
       affinity_(other.affinity_),
       selection_type_(other.selection_type_),
       base_is_first_(other.base_is_first_),
-      is_directional_(other.is_directional_),
-      granularity_(other.granularity_) {}
+      is_directional_(other.is_directional_) {}
 
 template <typename Strategy>
 VisibleSelectionTemplate<Strategy>& VisibleSelectionTemplate<Strategy>::
@@ -138,7 +135,6 @@ operator=(const VisibleSelectionTemplate<Strategy>& other) {
   selection_type_ = other.selection_type_;
   base_is_first_ = other.base_is_first_;
   is_directional_ = other.is_directional_;
-  granularity_ = other.granularity_;
   return *this;
 }
 
@@ -201,7 +197,6 @@ VisibleSelectionTemplate<Strategy>
 VisibleSelectionTemplate<Strategy>::AppendTrailingWhitespace() const {
   if (IsNone())
     return *this;
-  DCHECK_EQ(granularity_, TextGranularity::kWord);
   if (!IsRange())
     return *this;
   const PositionTemplate<Strategy>& new_end = SkipWhitespace(end_);
