f8d77d21dd6b44d3926d872074817f02e5041b86
jdoerrie <jdoerrie@chromium.org>
2017-07-11 12:28:21 +0000

    Set correct signon_realm during import of Android credentials
    This change fixes the PasswordCSVReader to properly set the signon_realm
    of Android credentials when importing them.
    
    Bug: 683963
    Change-Id: I447d1393c1aa179f07737382e03ede2f8f056139
    Reviewed-on: https://chromium-review.googlesource.com/566799
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Vaclav Brozek <vabr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485605}

--- components/password_manager/core/browser/import/password_csv_reader_unittest.cc
+++ components/password_manager/core/browser/import/password_csv_reader_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/utf_string_conversions.h"
 #include "components/autofill/core/common/password_form.h"
+#include "components/password_manager/core/browser/android_affiliation/affiliation_utils.h"
 #include "components/password_manager/core/browser/import/password_importer.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -35,6 +36,25 @@ TEST(PasswordCSVReaderTest, DeserializePasswords_SingleValid) {
   EXPECT_EQ(expected_origin.GetOrigin().spec(), passwords[0].signon_realm);
   EXPECT_EQ(base::UTF8ToUTF16("test@gmail.com"), passwords[0].username_value);
   EXPECT_EQ(base::UTF8ToUTF16("test1"), passwords[0].password_value);
+}
+
+TEST(PasswordCSVReaderTest, DeserializePasswords_SingleAndroid) {
+  constexpr char kCSVInput[] =
+      "Url,Username,Password\n"
+      "android://hash@com.example.android,test@gmail.com,test1\n";
+  std::vector<autofill::PasswordForm> passwords;
+  PasswordCSVReader reader;
+  EXPECT_EQ(PasswordImporter::SUCCESS,
+            reader.DeserializePasswords(kCSVInput, &passwords));
+  EXPECT_EQ(1u, passwords.size());
+  const GURL expected_origin("android://hash@com.example.android");
+
+  const autofill::PasswordForm& password = passwords.front();
+  EXPECT_EQ(expected_origin, password.origin);
+  EXPECT_EQ(expected_origin.spec(), password.signon_realm);
+  EXPECT_TRUE(IsValidAndroidFacetURI(password.signon_realm));
+  EXPECT_EQ(base::UTF8ToUTF16("test@gmail.com"), password.username_value);
+  EXPECT_EQ(base::UTF8ToUTF16("test1"), password.password_value);
 }
 
 TEST(PasswordCSVReaderTest, DeserializePasswords_TwoValid) {
