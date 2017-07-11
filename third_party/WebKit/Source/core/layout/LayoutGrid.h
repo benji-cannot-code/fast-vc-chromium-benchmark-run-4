c00773f597e87922caeec5e84e77d33ad6e23e1e
jfernandez <jfernandez@igalia.com>
2017-07-10 20:49:10 -0700

    [css-grid] Check if baseline alignment affects grid areas sizing
    The bug this CL intend to fix has a very specific root cause; we were
    wrongly assuming that we need to repeat the track sizing algorithm only
    if baseline alignment may change the grid container's intrinsic size.
    
    Actually, the spec states that we should run a new iteration of the
    sizing algorithm if any of the grid items has changed its min-content
    contribution. This affects to the grid item's container, hence the
    grid area, but not necessarily to the grid container.
    
    Since we were running the baseline alignment logic after the first run
    of the tracks sizing algorithm, we were relying on the extra run to
    adjust the grid areas' size based on the resulting baseline alignment
    offsets.
    
    This patch fixes the bug by comparing the grid areas's size with the
    baseline extent of their items, triggering the extra tracks sizing cycle
    when necessary.
    
    BUG=719445
    
    Review-Url: https://codereview.chromium.org/2868283002
    Cr-Commit-Position: refs/heads/master@{#485518}

--- third_party/WebKit/Source/core/layout/LayoutGrid.h
+++ third_party/WebKit/Source/core/layout/LayoutGrid.h
@@ -270,8 +270,7 @@ class LayoutGrid final : public LayoutBlock {
                              LayoutUnit ascent,
                              GridAxis) const;
 
-  bool BaselineMayAffectIntrinsicWidth() const;
-  bool BaselineMayAffectIntrinsicHeight() const;
+  bool BaselineMayAffectIntrinsicSize(GridTrackSizingDirection) const;
   void ComputeBaselineAlignmentContext();
   void UpdateBaselineAlignmentContextIfNeeded(LayoutBox&, GridAxis);
 
