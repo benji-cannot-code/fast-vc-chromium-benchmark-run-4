6fa784ec19f99f2dafa93410e2da907a06c5ee73
Yuwei Huang <yuweih@chromium.org>
2017-07-11 23:56:32 +0000

    [CRD iOS] Refactor account switching toast logic
    Previously RemotingVC listens to the userDidUpdate event and shows the toast,
    which is painful to handle when navigating back and forth between VCs.
    
    This CL moves the present status logic into a standalone class so that it
    doesn't depend on the lifecycle of RemotingVC and simplifies some logic.
    
    Change-Id: I43526d367ccf6c6ff957fd7b7a126b8857c653eb
    Reviewed-on: https://chromium-review.googlesource.com/565661
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Scott Nichols <nicholss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485731}

--- remoting/ios/app/app_delegate.mm
+++ remoting/ios/app/app_delegate.mm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "remoting/ios/app/app_view_controller.h"
 #import "remoting/ios/app/help_and_feedback.h"
 #import "remoting/ios/app/remoting_view_controller.h"
+#import "remoting/ios/app/user_status_presenter.h"
 #import "remoting/ios/facade/remoting_oauth_authentication.h"
 
 @interface AppDelegate () {
@@ -109,6 +110,7 @@ static NSString* const kFAQsUrl =
       [[AppViewController alloc] initWithMainViewController:navController];
   self.window.rootViewController = _appViewController;
   [self.window makeKeyAndVisible];
+  [UserStatusPresenter.instance start];
 }
 
 #pragma mark - AppDelegate
