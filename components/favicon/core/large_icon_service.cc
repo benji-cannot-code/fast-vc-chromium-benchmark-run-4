986dfd3129802d0514602337eeb7e3543491baa8
Jan Krcal <jkrcal@chromium.org>
2017-07-11 13:03:00 +0000

    [LargeIconService] Touch icons from google server upon reading
    This CL marks on-demand favicons from the Google server as accessed upon
    reading from them favicon database. In order to do so:
     - it adds LargeIconService::TouchIconFromGoogleServer() so that the clients
       of this service do not have to keep the handle to FaviconService as well;
     - it reveals icon_url info into the LargeIconImageResult struct.
    
    This is the last CL in the series for removing old on-demand favicons.
    
    Bug: 709471
    Change-Id: I92d51be4b1bed14b6e6e06636e9aa7996b1fc060
    Reviewed-on: https://chromium-review.googlesource.com/563402
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485612}

--- components/favicon/core/large_icon_service.cc
+++ components/favicon/core/large_icon_service.cc
@@ -130,6 +130,7 @@ void ProcessIconOnBackgroundThread(
     int desired_size,
     favicon_base::FaviconRawBitmapResult* raw_result,
     SkBitmap* bitmap,
+    GURL* icon_url,
     favicon_base::FallbackIconStyle* fallback_icon_style) {
   if (IsDbResultAdequate(db_result, min_source_size)) {
     gfx::Image image;
@@ -144,6 +145,9 @@ void ProcessIconOnBackgroundThread(
       }
       if (bitmap) {
         *bitmap = image.AsBitmap();
+      }
+      if (icon_url) {
+        *icon_url = db_result.icon_url;
       }
       return;
     }
@@ -206,6 +210,7 @@ class LargeIconWorker : public base::RefCountedThreadSafe<LargeIconWorker> {
 
   favicon_base::FaviconRawBitmapResult raw_bitmap_result_;
   SkBitmap bitmap_result_;
+  GURL icon_url_;
   std::unique_ptr<favicon_base::FallbackIconStyle> fallback_icon_style_;
 
   DISALLOW_COPY_AND_ASSIGN(LargeIconWorker);
@@ -238,6 +243,7 @@ void LargeIconWorker::OnIconLookupComplete(
                  min_source_size_in_pixel_, desired_size_in_pixel_,
                  raw_bitmap_callback_ ? &raw_bitmap_result_ : nullptr,
                  image_callback_ ? &bitmap_result_ : nullptr,
+                 image_callback_ ? &icon_url_ : nullptr,
                  fallback_icon_style_.get()),
       base::Bind(&LargeIconWorker::OnIconProcessingComplete, this));
 }
@@ -257,7 +263,7 @@ void LargeIconWorker::OnIconProcessingComplete() {
 
   if (!bitmap_result_.isNull()) {
     image_callback_.Run(favicon_base::LargeIconImageResult(
-        gfx::Image::CreateFrom1xBitmap(bitmap_result_)));
+        gfx::Image::CreateFrom1xBitmap(bitmap_result_), icon_url_));
     return;
   }
   image_callback_.Run(
@@ -400,6 +406,10 @@ void LargeIconService::
       base::Bind(&OnFetchIconFromGoogleServerComplete, favicon_service_,
                  page_url, callback),
       traffic_annotation);
+}
+
+void LargeIconService::TouchIconFromGoogleServer(const GURL& icon_url) {
+  favicon_service_->TouchOnDemandFavicon(icon_url);
 }
 
 base::CancelableTaskTracker::TaskId
