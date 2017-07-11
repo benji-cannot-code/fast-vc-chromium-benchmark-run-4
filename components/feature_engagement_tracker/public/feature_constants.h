39f2ce1c8315a8097e8cd6127d2d3b359e139d13
Cooper Knaak <cooperknaak@google.com>
2017-07-11 18:01:08 +0000

    [iOS] FeatureEngagementTracker Feature Constants
    Added base::Feature constants for FeatureEngagementTracker IPH events.
    
    Bug: 738164
    Change-Id: I26120c392dc721451d13ecfd9dc9652ad87b5b8d
    Reviewed-on: https://chromium-review.googlesource.com/556355
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Gregory Chatzinoff <gchatz@chromium.org>
    Reviewed-by: Ed Chin <edchin@chromium.org>
    Commit-Queue: Cooper Knaak <cooperknaak@google.com>
    Cr-Commit-Position: refs/heads/master@{#485676}

--- components/feature_engagement_tracker/public/feature_constants.h
+++ components/feature_engagement_tracker/public/feature_constants.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace feature_engagement_tracker {
 
-// A feature for enabling a demonstration mode for In-Product Help.
+// A feature for enabling a demonstration mode for In-Product Help (IPH).
 extern const base::Feature kIPHDemoMode;
 
 // A feature to ensure all arrays can contain at least one feature.
@@ -32,6 +32,12 @@ extern const base::Feature kIPHDownloadPageScreenshotFeature;
 extern const base::Feature kIPHIncognitoWindowFeature;
 extern const base::Feature kIPHNewTabFeature;
 #endif  // defined(OS_WIN) || defined(OS_LINUX)
+
+#if defined(OS_IOS)
+extern const base::Feature kIPHNewTabTipFeature;
+extern const base::Feature kIPHNewIncognitoTabTipFeature;
+extern const base::Feature kIPHBadgedReadingListFeature;
+#endif  // defined(OS_IOS)
 
 }  // namespace feature_engagement_tracker
 
