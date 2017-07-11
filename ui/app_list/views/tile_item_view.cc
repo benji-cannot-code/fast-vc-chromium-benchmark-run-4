0774222a81c2c057826af27a9b1dfadecce9510d
hejq <hejq@chromium.org>
2017-07-10 17:11:56 -0700

    Use EnablePlayStoreAppSearch feature flag.
    We apply EnablePlayStoreAppSearch to decide whether to:
    
    - search for uninstalled apps from Play Store;
    - show a proper badge in the Play Store app result;
    - show the review score and price of every searched app.
    
    Currently using the IsFullscreenAppListEnabled flag is incorrect.
    
    BUG=736552
    
    Review-Url: https://codereview.chromium.org/2961923002
    Cr-Commit-Position: refs/heads/master@{#485454}

--- ui/app_list/views/tile_item_view.cc
+++ ui/app_list/views/tile_item_view.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/app_list/views/tile_item_view.h"
 
+#include <utility>
+
 #include "ui/app_list/app_list_constants.h"
 #include "ui/app_list/app_list_features.h"
 #include "ui/app_list/views/app_list_main_view.h"
@@ -67,7 +69,7 @@ TileItemView::TileItemView()
   title_->SetHandlesTooltips(false);
 
   AddChildView(icon_);
-  if (features::IsFullscreenAppListEnabled()) {
+  if (features::IsPlayStoreAppSearchEnabled()) {
     badge_ = new views::ImageView();
     badge_->set_can_process_events_within_subtree(false);
     badge_->SetVerticalAlignment(views::ImageView::LEADING);
--- ui/app_list/views/tile_item_view.h
+++ ui/app_list/views/tile_item_view.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_APP_LIST_VIEWS_TILE_ITEM_VIEW_H_
 #define UI_APP_LIST_VIEWS_TILE_ITEM_VIEW_H_
 
+#include <memory>
+
 #include "base/macros.h"
 #include "base/strings/string16.h"
 #include "third_party/skia/include/core/SkColor.h"
