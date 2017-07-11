63b8609f34a0e127a9f80c84147982b0d183cc39
ikilpatrick <ikilpatrick@chromium.org>
2017-07-11 09:56:39 -0700

    [LayoutNG] Move decision if a inline node is empty to CollectInlines.
    This is to simplify bfc resolving "abort" code. Inside NGLineBreaker we
    should be able to always have a resolved BFC offset with this change.
    
    In a followup i'll move the unpositioned floats behaviour to a separate
    walk if needed.
    
    BUG=635619
    
    Review-Url: https://codereview.chromium.org/2970343002
    Cr-Commit-Position: refs/heads/master@{#485655}

--- third_party/WebKit/Source/core/layout/ng/inline/ng_line_breaker_test.cc
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_line_breaker_test.cc
@@ -39,6 +39,7 @@ class NGLineBreakerTest : public NGBaseLayoutAlgorithmTest {
 
     NGFragmentBuilder container_builder(
         NGPhysicalFragment::NGFragmentType::kFragmentBox, node);
+    container_builder.SetBfcOffset(NGLogicalOffset{LayoutUnit(), LayoutUnit()});
 
     NGLineBreaker line_breaker(node, space.Get(), &container_builder);
     Vector<NGInlineItemResults> lines;
