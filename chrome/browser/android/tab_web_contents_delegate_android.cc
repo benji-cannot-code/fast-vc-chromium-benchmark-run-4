f2638949281f21fde809ff6dfecb511f6432a87a
Amirhossein Simjour <asimjour@chromium.org>
2017-07-12 02:21:56 +0000

    VR: Permission request in VR is not count as DENIED
    When in VR, RequestMediaAccessPermission should call the callback by
    MEDIA_DEVICE_NOT_SUPPORTED. As a result user can ask for the permission
    outside of VR mode later.
    
    BUG=737826
    
    Change-Id: I86675bdbf832ac7bb65a74fd88f54254e0bfd931
    Reviewed-on: https://chromium-review.googlesource.com/567482
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485800}

--- chrome/browser/android/tab_web_contents_delegate_android.cc
+++ chrome/browser/android/tab_web_contents_delegate_android.cc
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/file_chooser_params.h"
+#include "content/public/common/media_stream_request.h"
 #include "jni/TabWebContentsDelegateAndroid_jni.h"
 #include "ppapi/features/features.h"
 #include "ui/gfx/geometry/rect.h"
@@ -284,8 +285,11 @@ void TabWebContentsDelegateAndroid::RequestMediaAccessPermission(
     const content::MediaStreamRequest& request,
     const content::MediaResponseCallback& callback) {
 #if BUILDFLAG(ENABLE_VR)
-  if (vr_shell::VrTabHelper::IsInVr(web_contents))
+  if (vr_shell::VrTabHelper::IsInVr(web_contents)) {
+    callback.Run(content::MediaStreamDevices(),
+                 content::MEDIA_DEVICE_NOT_SUPPORTED, nullptr);
     return;
+  }
 #endif
   MediaCaptureDevicesDispatcher::GetInstance()->ProcessMediaAccessRequest(
       web_contents, request, callback, nullptr);
