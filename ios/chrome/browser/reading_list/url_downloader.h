ac2fb3aa6cd0eccd618c1c0c813d103260198474
Sylvain Defresne <sdefresne@chromium.org>
2017-07-12 14:32:55 +0000

    Remove FILE thread dependency in url_downloader.cc.
    The named thread API is deprecated in favor of the post_task.h API.
    
    Bug: 689520
    Change-Id: I8068157d94c0d3dd2fb385dcf74d4fc28444e64d
    Reviewed-on: https://chromium-review.googlesource.com/548558
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485945}

--- ios/chrome/browser/reading_list/url_downloader.h
+++ ios/chrome/browser/reading_list/url_downloader.h
@@ -17,6 +17,7 @@ class PrefService;
 class GURL;
 namespace base {
 class FilePath;
+class SequencedTaskRunner;
 }
 
 namespace net {
@@ -193,6 +194,7 @@ class URLDownloader : public net::URLFetcherDelegate,
   // URLRequestContextGetter needed for the URLFetcher.
   scoped_refptr<net::URLRequestContextGetter> url_request_context_getter_;
   std::unique_ptr<dom_distiller::DistillerViewerInterface> distiller_;
+  scoped_refptr<base::SequencedTaskRunner> task_runner_;
   base::CancelableTaskTracker task_tracker_;
 
   DISALLOW_COPY_AND_ASSIGN(URLDownloader);
