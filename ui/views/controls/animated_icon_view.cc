0f8c0c772701903ba9c9f4df3f6181e18ee76621
spqchan <spqchan@chromium.org>
2017-07-12 01:49:51 +0000

    [Views] Fix Color Issue With App Menu Icon
    When the icon's color is updated, the static image should also be
    updated.
    
    Bug: 704786
    Change-Id: I123da5c43b57d0ec4343a2497a0ab0c711e46803
    Reviewed-on: https://chromium-review.googlesource.com/566025
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Sarah Chan <spqchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485785}

--- ui/views/controls/animated_icon_view.cc
+++ ui/views/controls/animated_icon_view.cc
@@ -22,6 +22,13 @@ AnimatedIconView::AnimatedIconView(const gfx::VectorIcon& icon)
 
 AnimatedIconView::~AnimatedIconView() {}
 
+void AnimatedIconView::SetColor(SkColor color) {
+  if (color_ != color) {
+    color_ = color;
+    UpdateStaticImage();
+  }
+}
+
 void AnimatedIconView::Animate(State target) {
   SetState(target);
   if (!IsAnimating())
