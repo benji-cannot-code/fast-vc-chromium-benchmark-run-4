2ba75a75b84c2ea5246f099c18bc8286cb07de15
lukasza <lukasza@chromium.org>
2017-07-11 18:25:19 -0700

    Remove all calls to domAutomationController.setAutomationId.
    These calls are no longer needed after r485461
    (https://crrev.com/2478803003) which has made the
    domAutomationController.setAutomationId call a no-op.
    
    BUG=662543
    This CL was uploaded by git cl split.
    
    R=boliu@chromium.org
    
    Review-Url: https://codereview.chromium.org/2982473002
    Cr-Commit-Position: refs/heads/master@{#485764}

--- content/browser/zoom_browsertest.cc
+++ content/browser/zoom_browsertest.cc
@@ -135,7 +135,6 @@ struct FrameResizeObserver {
                              const std::string& label) {
     const char kOnResizeCallbackSetup[] =
         "document.body.onresize = function(){"
-        "  window.domAutomationController.setAutomationId(0);"
         "  window.domAutomationController.send('%s ' + window.innerWidth);"
         "};";
     EXPECT_TRUE(ExecuteScript(
@@ -169,7 +168,6 @@ struct ResizeObserver {
   void SetupOnResizeCallback(const ToRenderFrameHost& adapter) {
     const char kOnResizeCallbackSetup[] =
         "document.body.onresize = function(){"
-        "  window.domAutomationController.setAutomationId(0);"
         "  window.domAutomationController.send('Resized');"
         "};";
     EXPECT_TRUE(ExecuteScript(
