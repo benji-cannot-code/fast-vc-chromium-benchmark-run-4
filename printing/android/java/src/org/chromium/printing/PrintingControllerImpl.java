bee45809037199f38326cd2a0027c49e08087ff8
Shimi Zhang <ctzsm@chromium.org>
2017-07-12 19:47:02 +0000

    [Android] Print only iframe contents for iframe window.print()
    We are using GetFrameToPrint() in print_view_manager_common.cc
    on Android to get RenderFrameHost to print, but the
    functionality of GetFrameToPrint() is only related to focused
    RenderFrameHost. In most cases, focused RenderFrameHost is
    not necessarily the one triggered window.print().
    
    This CL records |render_process_id| and |render_frame_id| to
    PrintingControllerImpl.java so that TabAndroid could
    use them to determine the correct RenderFrameHost to
    do the printing job.
    
    Letting PrintingControllerImpl manage life cycle of
    |render_process_id| and |render_frame_id| makes it easier to
    handle crashes and failures.
    
    Bug: 561438
    Change-Id: Ie3e740d0b00a39a4975ba7d5b8ed496cacd79171
    Reviewed-on: https://chromium-review.googlesource.com/560656
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486063}

--- printing/android/java/src/org/chromium/printing/PrintingControllerImpl.java
+++ printing/android/java/src/org/chromium/printing/PrintingControllerImpl.java
@@ -55,6 +55,8 @@ public class PrintingControllerImpl implements PrintingController, PdfGenerator 
      * with javascript.
      */
     private PrintingContextInterface mContextFromScriptInitiation;
+    private int mRenderProcessId;
+    private int mRenderFrameId;
 
     /** The file descriptor into which the PDF will be written.  Provided by the framework. */
     private int mFileDescriptor;
@@ -91,8 +93,8 @@ public class PrintingControllerImpl implements PrintingController, PdfGenerator 
 
     private PrintManagerDelegate mPrintManager;
 
-    private PrintingControllerImpl(PrintDocumentAdapterWrapper printDocumentAdapterWrapper,
-                                   String errorText) {
+    private PrintingControllerImpl(
+            PrintDocumentAdapterWrapper printDocumentAdapterWrapper, String errorText) {
         mErrorMessage = errorText;
         mPrintDocumentAdapterWrapper = printDocumentAdapterWrapper;
         mPrintDocumentAdapterWrapper.setPdfGenerator(this);
@@ -173,13 +175,16 @@ public class PrintingControllerImpl implements PrintingController, PdfGenerator 
     }
 
     @Override
-    public void setPendingPrint(final Printable printable, PrintManagerDelegate printManager) {
+    public void setPendingPrint(final Printable printable, PrintManagerDelegate printManager,
+            int renderProcessId, int renderFrameId) {
         if (mIsBusy) {
             Log.d(TAG, "Pending print can't be set. PrintingController is busy.");
             return;
         }
         mPrintable = printable;
         mPrintManager = printManager;
+        mRenderProcessId = renderProcessId;
+        mRenderFrameId = renderFrameId;
     }
 
     @Override
@@ -200,7 +205,7 @@ public class PrintingControllerImpl implements PrintingController, PdfGenerator 
     @Override
     public void startPrint(final Printable printable, PrintManagerDelegate printManager) {
         if (mIsBusy) return;
-        setPendingPrint(printable, printManager);
+        setPendingPrint(printable, printManager, mRenderProcessId, mRenderFrameId);
         startPendingPrint(null);
     }
 
@@ -271,7 +276,9 @@ public class PrintingControllerImpl implements PrintingController, PdfGenerator 
         mFileDescriptor = destination.getFd();
         mPages = convertPageRangesToIntegerArray(ranges);
 
-        if (mPrintable.print()) {
+        // mRenderProcessId and mRenderFrameId could be invalid values, in this case we are going to
+        // print the main frame.
+        if (mPrintable.print(mRenderProcessId, mRenderFrameId)) {
             mPrintingState = PRINTING_STATE_STARTED_FROM_ONWRITE;
         } else {
             mOnWriteCallback.onWriteFailed(mErrorMessage);
@@ -303,6 +310,8 @@ public class PrintingControllerImpl implements PrintingController, PdfGenerator 
             mContextFromScriptInitiation.showSystemDialogDone();
             mContextFromScriptInitiation = null;
         }
+        mRenderProcessId = -1;
+        mRenderFrameId = -1;
 
         mPrintingState = PRINTING_STATE_FINISHED;
 
