01ba5184656519b77bb2c363556c8abdd81c7229
Vadym Doroshenko <dvadym@chromium.org>
2017-07-12 11:16:39 +0000

    UMA metrics for saved sync hash passwords.
    Bug:657041
    
    Change-Id: Idafd23621f458dfd73757899bca952f470f31767
    Reviewed-on: https://chromium-review.googlesource.com/563299
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485924}

--- components/password_manager/core/browser/hash_password_manager.h
+++ components/password_manager/core/browser/hash_password_manager.h
@@ -34,6 +34,8 @@ class HashPasswordManager {
   // Returns empty if no hash is available.
   base::Optional<SyncPasswordData> RetrievePasswordHash();
 
+  void ReportIsSyncPasswordHashSavedMetric();
+
   void set_prefs(PrefService* prefs) { prefs_ = prefs; }
 
  private:
