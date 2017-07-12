12ee18d46ea09db5634f9541b9476d4595ad27f1
Sunny Sachanandani <sunnyps@chromium.org>
2017-07-12 02:27:18 +0000

    cc: Add crash key for debugging BeginMainFrame renderer hang.
    This CL adds a crash key that contains compositor state useful for
    debugging the long standing BeginMainFrame renderer hang. The plan is
    to start with a small set of values and expand that when needed.
    
    R=rsesek,halliwell,brianderson
    TBR=ananta
    BUG=622080
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ic6fd3d9f7b7b942026604b3c01ea2733535c87d4
    Reviewed-on: https://chromium-review.googlesource.com/563176
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Reviewed-by: Brian Anderson <brianderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485802}

--- cc/scheduler/scheduler.cc
+++ cc/scheduler/scheduler.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/auto_reset.h"
+#include "base/debug/crash_logging.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/profiler/scoped_tracker.h"
@@ -26,7 +27,12 @@ namespace {
 // for message latency and kernel scheduling variability.
 const base::TimeDelta kDeadlineFudgeFactor =
     base::TimeDelta::FromMicroseconds(1000);
-}
+
+// TEMPORARY: Compositor state for debugging BeginMainFrame renderer hang.
+// TODO(sunnyps): Remove after fixing https://crbug.com/622080
+const char kBeginMainFrameHangCompositorState[] =
+    "begin-main-frame-hang-compositor-state";
+}  // namespace
 
 Scheduler::Scheduler(
     SchedulerClient* client,
@@ -702,6 +708,12 @@ void Scheduler::ProcessScheduledActions() {
 
   ScheduleBeginImplFrameDeadlineIfNeeded();
   SetupNextBeginFrameIfNeeded();
+
+  // TEMPORARY: Compositor state for debugging BeginMainFrame renderer hang.
+  // TODO(sunnyps): Remove after fixing https://crbug.com/622080
+  base::debug::SetCrashKeyValue(
+      kBeginMainFrameHangCompositorState,
+      state_machine_.CrashKeyValueForBeginMainFrameHang());
 }
 
 std::unique_ptr<base::trace_event::ConvertableToTraceFormat>
