9ba6929a4df5578b58ad4fc43468d16f344682b6
hkamila <hkamila@google.com>
2017-07-11 19:50:45 -0700

    -Added the experimental features section as part of the chrome://safe-browsing. For the experiments not running at 100%.
    -Exported the list of SafeBrowsing features (base::Feature) to have them in the future in a central location.
    
    -Fixed the message dialogue for the WebUI.
    
    -Added some extra css.
    
    Note: Tags for the experiments are not added in this issue.
    Review-Url: https://codereview.chromium.org/2964693003
    Cr-Commit-Position: refs/heads/master@{#485380}
    Committed: https://chromium.googlesource.com/chromium/src/+/fdacb19a2600b6ca47c7d58752a97bc075fd89f5
    
    BUG=740955,734667
    
    Review-Url: https://codereview.chromium.org/2964693003
    Cr-Commit-Position: refs/heads/master@{#485821}

---
+++ components/safe_browsing/web_ui/resources/safe_browsing.js
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/* Copyright 2017 The Chromium Authors. All rights reserved.
+* Use of this source code is governed by a BSD-style license that can be
+* found in the LICENSE file. */
+
+   cr.define('safe_browsing', function() {
+     'use strict';
+
+    function initialize() {
+     chrome.send('expParamList',[]);
+    }
+
+   function addExperiment(result) {
+   var resLength = result.length;
+   var experimentsListFormatted="";
+
+for (var i = 0; i < resLength; i+=2) {
+    experimentsListFormatted += "<div>" + result[i+1] + " --- " + result [i] +
+    "</div>";}
+     $('experimentsList').innerHTML = experimentsListFormatted;
+  }
+
+    return {
+     addExperiment: addExperiment,
+      initialize: initialize,
+    };
+
+});
+
+document.addEventListener('DOMContentLoaded', safe_browsing.initialize);
