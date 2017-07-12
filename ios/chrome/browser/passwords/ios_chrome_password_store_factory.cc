e34bd2fdbe0a5a2a87be1e2ba0faec676f8b394b
Vaclav Brozek <vabr@chromium.org>
2017-07-12 17:09:50 +0000

    No DB thread in IOSChromePasswordStoreFactory any more
    This CL removes the DB thread reference from IOSChromePasswordStoreFactory and
    replaces it with an appropriate CreateSequencedTaskRunnerWithTraits call.
    
    Bug: 689520
    Change-Id: I19c7a6473f7d26ce192005e25b297c878716bee5
    Reviewed-on: https://chromium-review.googlesource.com/567984
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Vaclav Brozek <vabr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486001}

--- ios/chrome/browser/passwords/ios_chrome_password_store_factory.cc
+++ ios/chrome/browser/passwords/ios_chrome_password_store_factory.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/memory/singleton.h"
 #include "base/sequenced_task_runner.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "components/browser_sync/profile_sync_service.h"
 #include "components/keyed_service/core/service_access_type.h"
@@ -27,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/sync/glue/sync_start_util.h"
 #include "ios/chrome/browser/sync/ios_chrome_profile_sync_service_factory.h"
 #include "ios/chrome/browser/web_data_service_factory.h"
-#include "ios/web/public/web_thread.h"
 
 // static
 scoped_refptr<password_manager::PasswordStore>
@@ -81,8 +81,14 @@ IOSChromePasswordStoreFactory::BuildServiceInstanceFor(
 
   scoped_refptr<base::SequencedTaskRunner> main_thread_runner(
       base::SequencedTaskRunnerHandle::Get());
+  // USER_VISIBLE priority is chosen for the background task runner, because
+  // the passwords obtained through tasks on the background runner influence
+  // what the user sees.
+  // TODO(crbug.com/741660): Create the task runner inside password_manager
+  // component instead.
   scoped_refptr<base::SequencedTaskRunner> db_thread_runner(
-      web::WebThread::GetTaskRunnerForThread(web::WebThread::DB));
+      base::CreateSequencedTaskRunnerWithTraits(
+          {base::MayBlock(), base::TaskPriority::USER_VISIBLE}));
 
   scoped_refptr<password_manager::PasswordStore> store =
       new password_manager::PasswordStoreDefault(
