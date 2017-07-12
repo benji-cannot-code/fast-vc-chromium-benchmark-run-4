f197260b40b00b8dd8c3c15c20823937d5b003a0
lizeb <lizeb@chromium.org>
2017-07-12 02:54:37 -0700

    customtabs: Remove unnecessary Context plumbing.
    Also removes Context -> Application downcasts in callers.
    
    Review-Url: https://codereview.chromium.org/2966193002
    Cr-Commit-Position: refs/heads/master@{#485909}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunActivity.java
@@ -586,8 +586,8 @@ public class FirstRunActivity extends AsyncInitializationActivity implements Fir
                 public void onSendFinished(PendingIntent pendingIntent, Intent intent,
                         int resultCode, String resultData, Bundle resultExtras) {
                     if (ChromeLauncherActivity.isCustomTabIntent(intent)) {
-                        CustomTabsConnection.getInstance(
-                                getApplication()).sendFirstRunCallbackIfNecessary(intent, complete);
+                        CustomTabsConnection.getInstance().sendFirstRunCallbackIfNecessary(
+                                intent, complete);
                     }
                 }
             };
