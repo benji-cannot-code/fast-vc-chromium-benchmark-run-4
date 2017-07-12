cd2980861126af0a6977657bd663f4ba491c5c16
Hiroshige Hayashizaki <hiroshige@chromium.org>
2017-07-12 01:41:20 +0000

    Fire error event asynchronously for ScriptRunner-controlled scripts
    Previously, error scripts that are controlled by ScriptRunner
    fell into a special path in ScriptLoader::PendingScriptFinished(),
    and the error event is dispatched synchronously there.
    
    In order to centralize the code for script execution and error event
    dispatching to ScriptLoader::ExecuteScriptBlock(), this CL removes
    the special path, lets error scripts to be processed by
    ScriptRunner::NotifyScriptReady() (that eventually dispatches an error
    event asynchronously in ScriptLoader::ExecuteScriptBlock()),
    just like successful scripts.
    
    local-JavaScript-from-remote.html is fixed because it assumed the
    error event for a file:// script is fired synchronously.
    
    Bug: 686281
    Change-Id: Iebc0f5f178d9af97a2dfb6ba47374798da382451
    Reviewed-on: https://chromium-review.googlesource.com/557938
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485779}

--- third_party/WebKit/Source/core/dom/ScriptRunner.h
+++ third_party/WebKit/Source/core/dom/ScriptRunner.h
@@ -62,7 +62,6 @@ class CORE_EXPORT ScriptRunner final
   void Suspend();
   void Resume();
   void NotifyScriptReady(ScriptLoader*, AsyncExecutionType);
-  void NotifyScriptLoadError(ScriptLoader*, AsyncExecutionType);
 
   static void MovePendingScript(Document&, Document&, ScriptLoader*);
 
