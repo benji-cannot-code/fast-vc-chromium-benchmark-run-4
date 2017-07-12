222433bb83efdd1762641867c2ccb9c5022f4fea
Peter Conn <peconn@chromium.org>
2017-07-12 12:01:53 +0000

    Revert "📺 Use video aspect ratio to determine PiP params."
    This reverts commit 164f881f06faea76c87b246e2cdd17ab531416e5.
    
    Reason for revert: Caused a failure https://build.chromium.org/p/chromium/builders/Android/builds/74079
    
    Original change's description:
    > 📺 Use video aspect ratio to determine PiP params.
    > 
    > Use the bounds of the fullscreen video to determine the starting
    > bounds for the PiP transformation and the aspect ratio of the PiP
    > window.
    > 
    > Change-Id: Ia2227c9d277b5ae2a1dedddb899082e2947137fe
    > Reviewed-on: https://chromium-review.googlesource.com/536974
    > Commit-Queue: Peter Conn <peconn@chromium.org>
    > Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    > Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    > Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#485916}
    
    TBR=bauerb@chromium.org,mlamouri@chromium.org,peconn@chromium.org,jochen@chromium.org
    
    Change-Id: I91b3e716c02bd5c41cf93d3fde04e0874d26d3e6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/566865
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485929}

--- content/browser/web_contents/web_contents_impl.h
+++ content/browser/web_contents/web_contents_impl.h
@@ -846,7 +846,7 @@ class CONTENT_EXPORT WebContentsImpl
                     const WebContentsObserver::MediaPlayerId& id);
 
   int GetCurrentlyPlayingVideoCount() override;
-  base::Optional<gfx::Size> GetFullscreenVideoSize() override;
+  const VideoSizeMap& GetCurrentlyPlayingVideoSizes() override;
   bool IsFullscreen() override;
 
   MediaWebContentsObserver* media_web_contents_observer() {
