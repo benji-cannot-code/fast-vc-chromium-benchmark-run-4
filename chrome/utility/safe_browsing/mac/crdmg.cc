bf0d373fc3e22b651d119814212ebddd64e91ee4
Peter Collingbourne <pcc@chromium.org>
2017-07-12 01:45:55 +0000

    crdmg: Use __builtin_available/availability instead of -mmacosx-version-min.
    We can't use @available in this file because that is only available
    in Objective C.
    
    Bug: 735328
    Change-Id: I09ed1b2bcc46fceafc4bfb84800d18177fd4f4a3
    Reviewed-on: https://chromium-review.googlesource.com/566956
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Reviewed-by: Sidney San Martin <sdy@chromium.org>
    Commit-Queue: Peter Collingbourne <pcc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485783}

--- chrome/utility/safe_browsing/mac/crdmg.cc
+++ chrome/utility/safe_browsing/mac/crdmg.cc
@@ -54,7 +54,7 @@ class SafeDMG {
   bool EnableSandbox();
 
   // Performs the actual DMG operation.
-  bool ParseDMG();
+  __attribute__((availability(macos, introduced = 10.10))) bool ParseDMG();
 
   base::File dmg_file_;
 
@@ -92,8 +92,13 @@ int SafeDMG::Main(int argc, const char* argv[]) {
   if (!EnableSandbox())
     return EXIT_FAILURE;
 
-  if (!ParseDMG())
+  if (__builtin_available(macOS 10.10, *)) {
+    if (!ParseDMG())
+      return EXIT_FAILURE;
+  } else {
+    LOG(ERROR) << "Requires 10.10 or higher";
     return EXIT_FAILURE;
+  }
 
   return EXIT_SUCCESS;
 }
