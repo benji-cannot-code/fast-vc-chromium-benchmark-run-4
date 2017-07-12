fc4816ecd599022d983c3e8a5fce22de34a8cd11
Michael Thiessen <mthiesse@chromium.org>
2017-07-12 18:27:42 +0000

    VR: Don't set webVR pages to a different size than normal pages.
    This was a legacy workaround from when we used the normal compositor and
    fullscreened the layer to render webVR. With the mailbox path this is
    long dead and we don't need to set the page size differently for webVR.
    
    Bug: 728772
    Change-Id: I0fb22859f2250f1192bcbf8d52dd6366c78217ea
    Reviewed-on: https://chromium-review.googlesource.com/567497
    Reviewed-by: Yash Malik <ymalik@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486031}

--- chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellImpl.java
@@ -73,8 +73,6 @@ public class VrShellImpl
 
     // Increasing DPR any more than this doesn't appear to increase text quality.
     private static final float DEFAULT_DPR = 1.4f;
-    // For WebVR we just create a DPR 1.0 display that matches the physical display size.
-    private static final float WEBVR_DPR = 1.0f;
     // Fairly arbitrary values that put a good amount of content on the screen without making the
     // text too small to read.
     @VisibleForTesting
@@ -362,7 +360,7 @@ public class VrShellImpl
         setSplashScreenIcon();
 
         // Set the UI and content sizes before we load the UI.
-        updateWebVrDisplaySize(forWebVr);
+        setContentCssSize(DEFAULT_CONTENT_WIDTH, DEFAULT_CONTENT_HEIGHT, DEFAULT_DPR);
 
         reparentAllTabs(mContentVrWindowAndroid);
         swapToForegroundTab();
@@ -587,17 +585,6 @@ public class VrShellImpl
     public void setWebVrModeEnabled(boolean enabled, boolean showToast) {
         mContentVrWindowAndroid.setVSyncPaused(enabled);
         nativeSetWebVrMode(mNativeVrShell, enabled, showToast);
-        updateWebVrDisplaySize(enabled);
-    }
-
-    private void updateWebVrDisplaySize(boolean inWebVr) {
-        if (inWebVr) {
-            DisplayAndroid primaryDisplay = DisplayAndroid.getNonMultiDisplay(mActivity);
-            setContentCssSize(
-                    primaryDisplay.getDisplayWidth(), primaryDisplay.getDisplayHeight(), WEBVR_DPR);
-        } else {
-            setContentCssSize(DEFAULT_CONTENT_WIDTH, DEFAULT_CONTENT_HEIGHT, DEFAULT_DPR);
-        }
     }
 
     @Override
