b0271b9a8800f88bbe6f40a8a50868877c1ca82c
helenlyang <helenlyang@google.com>
2017-07-11 17:06:30 -0700

    [ios] BubbleViewController and BubbleView stubs.
    Added bubble_view_controller interface and implementation stubs,
    bubble_view interface and implementation stubs, and unit test
    stubs.
    
    These files live in the bubble_promo directory. For context, they are
    being used for the User Education project.
    
    BUG=740145.
    
    Review-Url: https://codereview.chromium.org/2964233002
    Cr-Commit-Position: refs/heads/master@{#485735}

---
+++ ios/chrome/browser/ui/bubble/bubble_view_controller_unittest.mm
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/bubble/bubble_view_controller.h"
+
+#include "base/logging.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "testing/platform_test.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+// Fixture to test BubbleViewController.
+class BubbleViewControllerTest : public PlatformTest {};
+
+// Tests the bubble's intrinsic content size given short text.
+TEST_F(BubbleViewControllerTest, BubbleSizeShortText) {}
+
+// Tests that the bubble accommodates text that exceeds the maximum bubble width
+// by displaying the text on multiple lines.
+TEST_F(BubbleViewControllerTest, BubbleSizeMultipleLineText) {}
+
+// Tests that the bubble attaches to the UI element when the layout is flipped
+// for RTL languages.
+TEST_F(BubbleViewControllerTest, RTL) {}
+
+// Tests that the accessibility label matches the display text.
+TEST_F(BubbleViewControllerTest, Accessibility) {}
