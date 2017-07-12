da1e9bb7d86466ad470b3783ddf4b9bb92190ba0
tzik <tzik@chromium.org>
2017-07-12 07:09:50 +0000

    Use media::BindToCurrentLoop on device_media_to_mojo_adapter
    media::BindToCurrentLoop used to be unable to handle move only type, so
    its user needed to make custom ones, which are no longer needed since
    m::BTCL can handle move-only types after http://crrev.com/2b817313110d5178.
    
    Change-Id: I6287a3ac8f29a15d9d915a2b3b74e6da6de49f9b
    Reviewed-on: https://chromium-review.googlesource.com/567614
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485885}

--- services/video_capture/device_media_to_mojo_adapter.cc
+++ services/video_capture/device_media_to_mojo_adapter.cc
@@ -20,13 +20,6 @@ namespace {
 // those frames get dropped.
 static const int kMaxBufferCount = 3;
 
-void RunSuccessfulGetPhotoStateCallback(
-    video_capture::mojom::Device::GetPhotoStateCallback callback,
-    scoped_refptr<base::SingleThreadTaskRunner> task_runner,
-    media::mojom::PhotoStatePtr result) {
-  task_runner->PostTask(FROM_HERE, base::Bind(callback, base::Passed(&result)));
-}
-
 void RunFailedGetPhotoStateCallback(
     base::Callback<void(media::mojom::PhotoStatePtr)> cb) {
   cb.Run(nullptr);
@@ -34,13 +27,6 @@ void RunFailedGetPhotoStateCallback(
 
 void RunFailedSetOptionsCallback(base::Callback<void(bool)> cb) {
   cb.Run(false);
-}
-
-void RunSuccessfulTakePhotoCallback(
-    video_capture::mojom::Device::TakePhotoCallback callback,
-    scoped_refptr<base::SingleThreadTaskRunner> task_runner,
-    media::mojom::BlobPtr blob) {
-  task_runner->PostTask(FROM_HERE, base::Bind(callback, base::Passed(&blob)));
 }
 
 void RunFailedTakePhotoCallback(
@@ -130,10 +116,7 @@ void DeviceMediaToMojoAdapter::Resume() {
 void DeviceMediaToMojoAdapter::GetPhotoState(
     const GetPhotoStateCallback& callback) {
   media::VideoCaptureDevice::GetPhotoStateCallback scoped_callback(
-      // Cannot use BindToCurrentLoop() here, because it does not support
-      // callbacks with unbound move-only parameters.
-      base::Bind(&RunSuccessfulGetPhotoStateCallback, std::move(callback),
-                 base::ThreadTaskRunnerHandle::Get()),
+      media::BindToCurrentLoop(callback),
       media::BindToCurrentLoop(base::Bind(&RunFailedGetPhotoStateCallback)));
   device_->GetPhotoState(std::move(scoped_callback));
 }
@@ -151,10 +134,7 @@ void DeviceMediaToMojoAdapter::SetPhotoOptions(
 void DeviceMediaToMojoAdapter::TakePhoto(const TakePhotoCallback& callback) {
   media::ScopedResultCallback<media::mojom::ImageCapture::TakePhotoCallback>
       scoped_callback(
-          // Cannot use BindToCurrentLoop() here, because it does not support
-          // callbacks with unbound move-only parameters.
-          base::Bind(&RunSuccessfulTakePhotoCallback, std::move(callback),
-                     base::ThreadTaskRunnerHandle::Get()),
+          media::BindToCurrentLoop(callback),
           media::BindToCurrentLoop(base::Bind(&RunFailedTakePhotoCallback)));
   device_->TakePhoto(std::move(scoped_callback));
 }
