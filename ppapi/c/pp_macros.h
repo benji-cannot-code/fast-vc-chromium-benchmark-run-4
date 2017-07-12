1248d9ee156a92f41dd0babe1442a28ea27a89b2
John Rummell <jrummell@chromium.org>
2017-07-12 00:24:56 +0000

    Update Platform Verification Pepper API to request Storage ID
    In order to enable offline licenses, the CDM needs a Storage ID that will be
    used to encrypt the license files saved. The CL updates the Platform
    Verification Pepper API to add the messages to retrieve the Storage ID from
    the browser process.
    
    Actual implementation of the Storage ID value in progress.
    
    BUG=478960
    TEST=new browser_test passes
    
    Change-Id: I27250418b298a3bd071c7b8c3f6dc2d150d35b77
    Reviewed-on: https://chromium-review.googlesource.com/531915
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485745}

--- ppapi/c/pp_macros.h
+++ ppapi/c/pp_macros.h
@@ -4,13 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-/* From pp_macros.idl modified Tue Apr  4 15:29:47 2017. */
+/* From pp_macros.idl modified Fri Oct  9 11:08:46 2015. */
 
 #ifndef PPAPI_C_PP_MACROS_H_
 #define PPAPI_C_PP_MACROS_H_
 
 
-#define PPAPI_RELEASE 60
+#define PPAPI_RELEASE 61
 
 /**
  * @file
