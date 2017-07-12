a8ad9ef82b6fe21d4ab3094485f1f4ce9fc429e0
piotrs <piotrs@chromium.org>
2017-07-11 21:06:11 -0700

    Fixes the redirect to external apps when navigating from PWA.
    This is done with avoiding explicit chrome package name (using
    component name instead) and using
    EXTRA_SEND_TO_EXTERNAL_DEFAULT_HANDLER on the intent.
    
    With this patch the redirect works, however it takes a long time for CCT to
    consult ExternalNavigationHandler. This results in suboptimal UX. Same redirect
    in ChromeTabbedActivity is almost instantaneous. Result is that
    for _blank links CCT shows up briefly before redirecting to an external app.
    In a follow up it should be investigated why it is the case and if it cannot
    be improved, we might need to bypass CCT in such case, which would diverge
    from existing navigation paths for _blank links in Clank.
    
    BUG=740402
    
    Review-Url: https://codereview.chromium.org/2969143002
    Cr-Commit-Position: refs/heads/master@{#485831}

--- chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebApkIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebApkIntegrationTest.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.webapps;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.LargeTest;
@@ -16,11 +17,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.chromium.base.ApplicationStatus;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.ScalableTimeout;
-import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.DeferredStartupHandler;
 import org.chromium.chrome.browser.ShortcutHelper;
@@ -45,9 +46,6 @@ public class WebApkIntegrationTest {
 
     @Rule
     public final NativeLibraryTestRule mNativeLibraryTestRule = new NativeLibraryTestRule();
-
-    @Rule
-    public final TopActivityListener activityListener = new TopActivityListener();
 
     private static final long STARTUP_TIMEOUT = ScalableTimeout.scaleTimeout(10000);
 
@@ -127,11 +125,14 @@ public class WebApkIntegrationTest {
         CriteriaHelper.pollUiThread(new Criteria() {
             @Override
             public boolean isSatisfied() {
-                ChromeActivity activity = (ChromeActivity) activityListener.getMostRecentActivity();
-                return activity instanceof CustomTabActivity
-                        && activity.getActivityTab() != null
+                Activity activity = ApplicationStatus.getLastTrackedFocusedActivity();
+                if (!(activity instanceof CustomTabActivity)) {
+                    return false;
+                }
+                CustomTabActivity customTab = (CustomTabActivity) activity;
+                return customTab.getActivityTab() != null
                         // Dropping the TLD as Google can redirect to a local site.
-                        && activity.getActivityTab().getUrl().startsWith("https://www.google.");
+                        && customTab.getActivityTab().getUrl().startsWith("https://www.google.");
             }
         });
     }
