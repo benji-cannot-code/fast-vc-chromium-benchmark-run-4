729b04940e02d30f65f8119b6369e80a9fc50bda
Khushal <khushalsagar@chromium.org>
2017-07-12 19:53:49 +0000

    chrome: Add CheckerImaging options to about:flags.
    Change-Id: Ib8ba6e80d484b10d44dd4376622501fc6e695232
    Reviewed-on: https://chromium-review.googlesource.com/567641
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486066}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -47,6 +47,9 @@ extern const char kAllowNaclSocketApiDescription[];
 extern const char kAppBannersName[];
 extern const char kAppBannersDescription[];
 
+extern const char kAsyncImageDecodingName[];
+extern const char kAsyncImageDecodingDescription[];
+
 extern const char kAutoplayPolicyName[];
 extern const char kAutoplayPolicyDescription[];
 
