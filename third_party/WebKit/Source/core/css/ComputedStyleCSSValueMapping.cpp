750735f129cab0c09b259721c8afad6f00a0df28
Quinten Yearsley <qyearsley@google.com>
2017-07-12 16:24:57 +0000

    Disable value "after-white-space" for the unprefixed line-break property.
    Bug: 738593
    Change-Id: Iafbe191294f825b9d1446c3a51c3f7ce527b9831
    Reviewed-on: https://chromium-review.googlesource.com/564279
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Quinten Yearsley <qyearsley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485988}

--- third_party/WebKit/Source/core/css/ComputedStyleCSSValueMapping.cpp
+++ third_party/WebKit/Source/core/css/ComputedStyleCSSValueMapping.cpp
@@ -3003,6 +3003,7 @@ const CSSValue* ComputedStyleCSSValueMapping::Get(
       return ZoomAdjustedPixelValue(style.WordSpacing(), style);
     case CSSPropertyWordWrap:
       return CSSIdentifierValue::Create(style.OverflowWrap());
+    case CSSPropertyLineBreak:
     case CSSPropertyWebkitLineBreak:
       return CSSIdentifierValue::Create(style.GetLineBreak());
     case CSSPropertyResize:
