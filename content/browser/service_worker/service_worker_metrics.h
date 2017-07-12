5fe8da08f617dc3dc50a00aeeb853b7e36298610
Tsuyoshi Horo <horo@chromium.org>
2017-07-12 06:15:01 +0000

    Revive ServiceWorker.NavigationHintPrecision UMA as StartHintPrecision
    I removed the UMA (ServiceWorker.NavigationHintPrecision) when we stopped
    "Speculatively start Service Workers on UI-events" experiment (crbug.com/616502).
    But we will start "Warm-up service worker on omnibox signal" experiment
    (crbug.com/732681).
    So this CL revive the UMA as ServiceWorker.StartHintPrecision.
    
    Bug: 740102
    Change-Id: I84034f9a3c8edb9ff25e5644d6b94c624ae00f1c
    Reviewed-on: https://chromium-review.googlesource.com/566138
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485876}

--- content/browser/service_worker/service_worker_metrics.h
+++ content/browser/service_worker/service_worker_metrics.h
@@ -205,7 +205,7 @@ class ServiceWorkerMetrics {
 
   class ScopedEventRecorder {
    public:
-    explicit ScopedEventRecorder();
+    explicit ScopedEventRecorder(EventType start_worker_purpose);
     ~ScopedEventRecorder();
 
     void RecordEventHandledStatus(EventType event, bool handled);
@@ -222,6 +222,7 @@ class ServiceWorkerMetrics {
                                         size_t fired_events);
 
     std::map<EventType, EventStat> event_stats_;
+    const EventType start_worker_purpose_;
 
     DISALLOW_COPY_AND_ASSIGN(ScopedEventRecorder);
   };
--- content/browser/service_worker/service_worker_version.cc
+++ content/browser/service_worker/service_worker_version.cc
@@ -1434,6 +1434,8 @@ void ServiceWorkerVersion::DidEnsureLiveRegistrationForStartWorker(
             "ServiceWorker", "ServiceWorkerVersion::StartWorker", trace_id,
             "Script", script_url_.spec(), "Purpose",
             ServiceWorkerMetrics::EventTypeToString(purpose));
+        DCHECK(!start_worker_first_purpose_);
+        start_worker_first_purpose_ = purpose;
         start_callbacks_.push_back(
             base::Bind(&ServiceWorkerVersion::RecordStartWorkerResult,
                        weak_factory_.GetWeakPtr(), purpose, prestart_status,
@@ -1453,12 +1455,17 @@ void ServiceWorkerVersion::DidEnsureLiveRegistrationForStartWorker(
 
 void ServiceWorkerVersion::StartWorkerInternal() {
   DCHECK_EQ(EmbeddedWorkerStatus::STOPPED, running_status());
+  DCHECK(start_worker_first_purpose_);
 
   if (!ServiceWorkerMetrics::ShouldExcludeSiteFromHistogram(site_for_uma_)) {
     DCHECK(!event_recorder_);
     event_recorder_ =
-        base::MakeUnique<ServiceWorkerMetrics::ScopedEventRecorder>();
+        base::MakeUnique<ServiceWorkerMetrics::ScopedEventRecorder>(
+            start_worker_first_purpose_.value());
   }
+  // We don't clear |start_worker_first_purpose_| here but clear in
+  // FinishStartWorker. This is because StartWorkerInternal may be called
+  // again from OnStoppedInternal if StopWorker is called before OnStarted.
 
   StartTimeoutTimer();
 
@@ -1853,6 +1860,7 @@ void ServiceWorkerVersion::OnStoppedInternal(EmbeddedWorkerStatus old_status) {
 }
 
 void ServiceWorkerVersion::FinishStartWorker(ServiceWorkerStatusCode status) {
+  start_worker_first_purpose_ = base::nullopt;
   RunCallbacks(this, &start_callbacks_, status);
 }
 
