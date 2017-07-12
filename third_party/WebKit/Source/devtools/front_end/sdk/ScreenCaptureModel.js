8537059d6e10c3261dee662ce1bf426c8dc062d8
Pavel Feldman <pfeldman@chromium.org>
2017-07-12 13:26:40 +0000

    DevTools: introduce clip into Page.captureScreenshot.
    Change-Id: I4f926faa964b9eded1ec4ea9a19058c30de05de0
    Reviewed-on: https://chromium-review.googlesource.com/564303
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485931}

--- third_party/WebKit/Source/devtools/front_end/sdk/ScreenCaptureModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/ScreenCaptureModel.js
@@ -47,7 +47,7 @@ SDK.ScreenCaptureModel = class extends SDK.SDKModel {
    * @return {!Promise<?string>}
    */
   captureScreenshot(format, quality) {
-    return this._agent.captureScreenshot(format, quality, true);
+    return this._agent.captureScreenshot(format, quality, undefined, true);
   }
 
   /**
