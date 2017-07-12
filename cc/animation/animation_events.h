59316f32db26e95d64f8c46d0dfe169b912c0592
Ian Vollick <vollick@chromium.org>
2017-07-12 15:16:18 +0000

    [cc] Remove "property update" animation event
    This is now dead code.
    
    Bug: 738644
    Change-Id: I4f199067216fa09d94d1ba59d954f2f120d57f78
    Reviewed-on: https://chromium-review.googlesource.com/567705
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485952}

--- cc/animation/animation_events.h
+++ cc/animation/animation_events.h
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace cc {
 
 struct CC_ANIMATION_EXPORT AnimationEvent {
-  enum Type { STARTED, FINISHED, ABORTED, PROPERTY_UPDATE, TAKEOVER };
+  enum Type { STARTED, FINISHED, ABORTED, TAKEOVER };
 
   AnimationEvent(Type type,
                  ElementId element_id,
