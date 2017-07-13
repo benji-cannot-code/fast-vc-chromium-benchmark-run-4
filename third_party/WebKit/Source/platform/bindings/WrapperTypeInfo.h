323e6e3b1a567f917883d37789d22ccaca3504e9
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2017-07-13 10:35:19 +0000

    bindings: Remove WrapperTypeInfo::kWrapperTypeExceptionPrototype.
    https://chromium-review.googlesource.com/c/558904 ("Remove toString()
    override from DOMException") changed the way DOMException's inheritance
    chain was set up, and its function object now inherits from %ErrorPrototype%
    in V8DOMException::installV8DOMExceptionTemplate().
    
    Since DOMException was the only user of kWrapperTypeExceptionPrototype, we
    can remove it from WrapperTypeInfo::WrapperTypePrototype and clean up the
    code a little.
    
    Bug: 737497
    Change-Id: I532431eb57839a7c7e0df35e250ada2426be5b75
    Reviewed-on: https://chromium-review.googlesource.com/569700
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Raphael Kubo da Costa (rakuco) <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/master@{#486334}

--- third_party/WebKit/Source/platform/bindings/WrapperTypeInfo.h
+++ third_party/WebKit/Source/platform/bindings/WrapperTypeInfo.h
@@ -83,7 +83,6 @@ struct WrapperTypeInfo {
 
   enum WrapperTypePrototype {
     kWrapperTypeObjectPrototype,
-    kWrapperTypeExceptionPrototype,
     kWrapperTypeNoPrototype,  // For legacy callback interface
   };
 
