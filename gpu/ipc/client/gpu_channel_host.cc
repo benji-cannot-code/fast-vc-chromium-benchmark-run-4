0290fa0bff308de7a860c2a702e6464688db945d
tzik <tzik@chromium.org>
2017-07-13 05:07:41 +0000

    Replace base::StaticAtomicSequenceNumber with base::AtomicSequenceNumber in //gpu
    after a refactoring at http://crrev.com/ad6d8a9bbf9bd5e2.
    As a follow-up of the refactoring, this CL inlines the alias to its users
    in //gpu
    
    base: :StaticAtomicSequenceNumber is an alias of base::AtomicSequenceNumber
    Change-Id: I19b104842fd7805c5af9c65c98cc53956c35b84e
    Reviewed-on: https://chromium-review.googlesource.com/568065
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486274}

--- gpu/ipc/client/gpu_channel_host.cc
+++ gpu/ipc/client/gpu_channel_host.cc
@@ -26,7 +26,7 @@ namespace gpu {
 namespace {
 
 // Global atomic to generate unique transfer buffer IDs.
-base::StaticAtomicSequenceNumber g_next_transfer_buffer_id;
+base::AtomicSequenceNumber g_next_transfer_buffer_id;
 
 }  // namespace
 
