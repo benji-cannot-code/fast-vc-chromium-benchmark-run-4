2b712f1af8897bbe176152a565cd21f5aa75e4b8
Tommi <tommi@chromium.org>
2017-07-13 13:05:22 +0000

    Migrate TtsPlatformImplLinux away from using BrowserThread::FILE.
    BUG=689520
    
    Change-Id: I157f86906ee45c7a8a64d58cdc0df3a7ba01ee00
    Reviewed-on: https://chromium-review.googlesource.com/570022
    Reviewed-by: Henrik Grunell <grunell@chromium.org>
    Commit-Queue: Tommi <tommi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486358}

--- chrome/browser/speech/tts_linux.cc
+++ chrome/browser/speech/tts_linux.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/singleton.h"
 #include "base/synchronization/lock.h"
+#include "base/task_scheduler/post_task.h"
 #include "chrome/browser/speech/tts_platform.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/common/content_switches.h"
@@ -101,10 +102,9 @@ TtsPlatformImplLinux::TtsPlatformImplLinux()
   if (!command_line.HasSwitch(switches::kEnableSpeechDispatcher))
     return;
 
-  BrowserThread::PostTask(BrowserThread::FILE,
-                          FROM_HERE,
-                          base::Bind(&TtsPlatformImplLinux::Initialize,
-                                     base::Unretained(this)));
+  base::PostTaskWithTraits(
+      FROM_HERE, {base::MayBlock(), base::TaskPriority::BACKGROUND},
+      base::Bind(&TtsPlatformImplLinux::Initialize, base::Unretained(this)));
 }
 
 void TtsPlatformImplLinux::Initialize() {
