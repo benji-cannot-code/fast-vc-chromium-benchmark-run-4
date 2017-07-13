42d73c8bae36956f073d97686d01c618ba7d154c
Eric Roman <eroman@chromium.org>
2017-07-13 03:40:58 +0000

    Delete WriteToFileNetLogObserver.
    It is no longer needed as consumers of WriteToFileNetLogObserver have
    been switched over to FileNetLogObserver.
    
    Bug: 716570
    Change-Id: Ia70e04aebc8560f2799a84c8c691538c5beb5db5
    Reviewed-on: https://chromium-review.googlesource.com/565450
    Commit-Queue: Eric Roman <eroman@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486259}

--- chrome/browser/ui/webui/net_internals/net_internals_ui.cc
+++ chrome/browser/ui/webui/net_internals/net_internals_ui.cc
@@ -73,7 +73,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/log/net_log_capture_mode.h"
 #include "net/log/net_log_entry.h"
 #include "net/log/net_log_util.h"
-#include "net/log/write_to_file_net_log_observer.h"
 #include "net/proxy/proxy_service.h"
 #include "net/url_request/url_request_context.h"
 #include "net/url_request/url_request_context_getter.h"
