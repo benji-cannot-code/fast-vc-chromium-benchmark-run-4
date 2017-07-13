4826ed529e7d900db24f3f7e21a639c2cfe17f51
mattcary <mattcary@chromium.org>
2017-07-13 07:26:31 -0700

    Enable spare RenderProcessHost to be preinitialized.
    Enables a preinitialization of an unbound RenderProcessHost, and refactors
    RenderProcessHost creation to allow that to be used where appropriate.
    
    BUG=730587
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2929113002
    Cr-Commit-Position: refs/heads/master@{#486372}

--- chrome/browser/android/warmup_manager.cc
+++ chrome/browser/android/warmup_manager.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/net/predictor.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_android.h"
+#include "content/public/browser/render_process_host.h"
 #include "jni/WarmupManager_jni.h"
 #include "url/gurl.h"
 
@@ -28,6 +29,14 @@ static void PreconnectUrlAndSubresources(JNIEnv* env,
   }
 }
 
+static void WarmupSpareRenderer(JNIEnv* env,
+                                const JavaParamRef<jclass>& clazz,
+                                const JavaParamRef<jobject>& jprofile) {
+  Profile* profile = ProfileAndroid::FromProfileAndroid(jprofile);
+  if (profile) {
+    content::RenderProcessHost::WarmupSpareRenderProcessHost(profile);
+  }
+}
 
 // Register native methods.
 bool RegisterWarmupManager(JNIEnv* env) {
