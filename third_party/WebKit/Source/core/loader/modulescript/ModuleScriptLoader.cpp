682a1ca3768b1913d38e613816b4b9bb7f9abc3c
Kouhei Ueno <kouhei@chromium.org>
2017-07-13 10:05:03 +0000

    [ES6 modules] Log to console if module script load failed due to invalid MIME type.
    Before this CL, module script load of invalid mime type has silently failed.
    This CL aims to help web developer by notifying this tricky failure to DevTools console.
    
    Bug: 594639
    Change-Id: I9533fdb325fcecccedbf24b9819e65f116a17c11
    Reviewed-on: https://chromium-review.googlesource.com/566746
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486331}

--- third_party/WebKit/Source/core/loader/modulescript/ModuleScriptLoader.cpp
+++ third_party/WebKit/Source/core/loader/modulescript/ModuleScriptLoader.cpp
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/loader/modulescript/ModuleScriptLoader.h"
 
+#include "core/dom/ExecutionContext.h"
 #include "core/dom/Modulator.h"
 #include "core/dom/ModuleScript.h"
+#include "core/inspector/ConsoleMessage.h"
 #include "core/loader/modulescript/ModuleScriptLoaderClient.h"
 #include "core/loader/modulescript/ModuleScriptLoaderRegistry.h"
 #include "platform/loader/fetch/FetchUtils.h"
@@ -162,7 +164,10 @@ void ModuleScriptLoader::Fetch(const ModuleScriptFetchRequest& module_request,
   SetResource(resource);
 }
 
-bool ModuleScriptLoader::WasModuleLoadSuccessful(Resource* resource) {
+namespace {
+
+bool WasModuleLoadSuccessful(Resource* resource,
+                             ConsoleMessage** error_message = nullptr) {
   // Implements conditions in Step 7 of
   // https://html.spec.whatwg.org/#fetch-a-single-module-script
 
@@ -185,11 +190,25 @@ bool ModuleScriptLoader::WasModuleLoadSuccessful(Resource* resource) {
   // We use ResourceResponse::httpContentType() instead of mimeType(), as
   // mimeType() may be rewritten by mime sniffer.
   if (!MIMETypeRegistry::IsSupportedJavaScriptMIMEType(
-          response.HttpContentType()))
+          response.HttpContentType())) {
+    if (error_message) {
+      String message =
+          "Failed to load module script: The server responded with a "
+          "non-JavaScript MIME type of \"" +
+          response.HttpContentType() +
+          "\". Strict MIME type checking is enforced for module scripts per "
+          "HTML spec.";
+      *error_message = ConsoleMessage::CreateForRequest(
+          kJSMessageSource, kErrorMessageLevel, message,
+          response.Url().GetString(), resource->Identifier());
+    }
     return false;
+  }
 
   return true;
 }
+
+}  // namespace
 
 // ScriptResourceClient callback handler
 void ModuleScriptLoader::NotifyFinished(Resource*) {
@@ -198,7 +217,13 @@ void ModuleScriptLoader::NotifyFinished(Resource*) {
   // Step 7. If any of the following conditions are met, set moduleMap[url] to
   // null, asynchronously complete this algorithm with null, and abort these
   // steps.
-  if (!WasModuleLoadSuccessful(GetResource())) {
+  ConsoleMessage* error_message = nullptr;
+  if (!WasModuleLoadSuccessful(GetResource(), &error_message)) {
+    if (error_message) {
+      ExecutionContext::From(modulator_->GetScriptState())
+          ->AddConsoleMessage(error_message);
+    }
+
     AdvanceState(State::kFinished);
     return;
   }
