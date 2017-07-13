6215ce9a34e1dca13442884821058d1d3a3b87bf
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2017-07-13 00:23:27 +0000

    Import wpt@3b43715d596e37808ac839dece7a01abe4ea828a
    Using wpt-import in Chromium 383b95ed4509e05eaf33a19b057af37d04b1d937.
    
    Build: https://build.chromium.org/p/chromium.infra.cron/builders/wpt-importer/builds/306
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    chcunningham@chromium.org, wolenetz@chromium.org:
      external/wpt/media-source
    dom-dev@chromium.org:
      external/wpt/html
    hta@chromium.org:
      external/wpt/webrtc
    
    TBR: qyearsley@chromium.org
    No-Export: true
    Change-Id: I36f7fa640ecf332e2595472d1715f0da4432212c
    Reviewed-on: https://chromium-review.googlesource.com/568373
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Commit-Queue: Quinten Yearsley <qyearsley@chromium.org>
    Commit-Queue: Blink W3C Test Autoroller <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486181}

---
+++ third_party/WebKit/LayoutTests/external/wpt/webrtc/RTCConfiguration-helper.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+'use strict';
+
+// Run a test function as two test cases.
+// The first test case test the configuration by passing a given config
+// to the constructor.
+// The second test case create an RTCPeerConnection object with default
+// configuration, then call setConfiguration with the provided config.
+// The test function is given a constructor function to create
+// a new instance of RTCPeerConnection with given config,
+// either directly as constructor parameter or through setConfiguration.
+function config_test(test_func, desc) {
+  test(() => {
+    test_func(config => new RTCPeerConnection(config));
+  }, `new RTCPeerConnection(config) - ${desc}`);
+
+  test(() => {
+    test_func(config => {
+      const pc = new RTCPeerConnection();
+      assert_own_property(pc, 'setConfiguration');
+      pc.setConfiguration(config);
+      return pc;
+    })
+  }, `setConfiguration(config) - ${desc}`);
+}
