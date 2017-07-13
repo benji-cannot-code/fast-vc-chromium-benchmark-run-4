181fd759f1ed450fddf61b20618c53c0c345f88e
Robert Hogan <robhogan@gmail.com>
2017-07-13 22:50:25 +0000

    Place ellipsis correctly when inline has relative offset
    When an inline has relative position we need to apply that to the
    ellipsis as well. 
    
    We also need to be able to detect cases where
    relative position moves the inline so that it requires ellipsis in
    the first place. This means that we need to look at the layout
    overflow rather than the linebox edge when deciding if ellipsis is
    required - this is because the dimensions of the root inline box
    won't tell us if relative positioning makes it exceed the horizontal
    bounds.
    
    When flexbox applies line clamp it depends on the ellipsis logic
    to apply an ellipsis even when there's adequate space for the line
    box. We now make it force that rather than apply it in all cases,
    because where we're looking at a normal ellipsis we want to ensure
    we account for relative positioning on the line boxes before deciding
    to apply an ellipsis on a line box that would otherwise already fit
    on the line.
    
    The final complication to deal with here is cases where relative
    positioning changes a line box from having an overflow (e.g. text
    overflowing the left hand side of the box in an rtl flow) to not
    having an overflow. InlineFlowBox::ComputeOverflow needs to notice
    when this is happening, so we change it so that the dimensions of the
    root inline box are not used to initialize its layout overflow because shifting the box
    subsequently for relative positioning doesn't subtract the overflow
    they initially create.
    
    Bug: 737837
    Change-Id: I66c848299a93b2b796f2f831d9237f41aa50e962
    Reviewed-on: https://chromium-review.googlesource.com/563625
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486504}

--- third_party/WebKit/Source/core/layout/line/RootInlineBox.h
+++ third_party/WebKit/Source/core/layout/line/RootInlineBox.h
@@ -37,6 +37,8 @@ class LineLayoutBlockFlow;
 
 struct BidiStatus;
 
+enum ForceEllipsisOnLine { DoNotForceEllipsis, ForceEllipsis };
+
 class RootInlineBox : public InlineFlowBox {
  public:
   explicit RootInlineBox(LineLayoutItem);
@@ -115,7 +117,8 @@ class RootInlineBox : public InlineFlowBox {
                            LayoutUnit block_right_edge,
                            LayoutUnit ellipsis_width,
                            LayoutUnit logical_left_offset,
-                           bool found_box);
+                           bool found_box,
+                           ForceEllipsisOnLine = DoNotForceEllipsis);
   // Return the position of the EllipsisBox or -1.
   LayoutUnit PlaceEllipsisBox(bool ltr,
                               LayoutUnit block_left_edge,
