3c8f4c84f4302277b4f2d27f9e21233ded423a3f
Marijn Kruisselbrink <mek@chromium.org>
2017-07-13 18:03:30 +0000

    Remove unused blink::Stream class.
    After ac68cf90fa046d3e9c1418aee7cd14b8ec38210 there is no code left that
    still uses blink::Stream, so remove the class and its IPCs.
    
    Bug: 690795
    Change-Id: I1a60a65b232bed39aa1d16896ba611d484324c8e
    Reviewed-on: https://chromium-review.googlesource.com/568689
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Takeshi Yoshino <tyoshino@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486437}

--- third_party/WebKit/Source/modules/fetch/FetchDataLoader.h
+++ third_party/WebKit/Source/modules/fetch/FetchDataLoader.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define FetchDataLoader_h
 
 #include "core/dom/DOMArrayBuffer.h"
-#include "core/streams/Stream.h"
 #include "modules/ModulesExport.h"
 #include "mojo/public/cpp/system/data_pipe.h"
 #include "platform/blob/BlobData.h"
