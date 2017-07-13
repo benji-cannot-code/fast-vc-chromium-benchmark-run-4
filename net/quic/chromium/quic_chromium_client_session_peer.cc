6ceebbd06b69c6db1c1defb7724b38a711df25ac
Nick Harper <nharper@chromium.org>
2017-07-13 00:34:58 +0000

    Start refactor of QuicCryptoStream
    QuicCryptoClientStream and QuicCryptoServerStream are refactored to delegate
    their implementations to a CryptoClientStreamDelegate or
    CryptoServerStreamDelegate, and the existing implementations are moved to new
    subclasses of the new delegates.
    
    The major work left to do in this refactor is for QuicCryptoStream to no longer
    implement CryptoFramerVisitorInterface and to remove the OnHandshakeMessage
    method from CryptoClientStreamDelegate and CryptoServerStreamDelegate
    interfaces.
    
    Merge internal change: 161549072
    
    Change-Id: I3919d067028cdacd939c64be4362d4645d7e7e65
    Reviewed-on: https://chromium-review.googlesource.com/565978
    Commit-Queue: Nick Harper <nharper@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486185}

--- net/quic/chromium/quic_chromium_client_session_peer.cc
+++ net/quic/chromium/quic_chromium_client_session_peer.cc
@@ -19,13 +19,6 @@ void QuicChromiumClientSessionPeer::SetMaxOpenStreams(
 }
 
 // static
-void QuicChromiumClientSessionPeer::SetChannelIDSent(
-    QuicChromiumClientSession* session,
-    bool channel_id_sent) {
-  session->crypto_stream_->channel_id_sent_ = channel_id_sent;
-}
-
-// static
 void QuicChromiumClientSessionPeer::SetHostname(
     QuicChromiumClientSession* session,
     const std::string& hostname) {
