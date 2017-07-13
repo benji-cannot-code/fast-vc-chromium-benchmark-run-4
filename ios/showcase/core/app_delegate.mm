1e9748e77b5d3e85769feb5a3f612fe21f6d77fd
sczs <sczs@chromium.org>
2017-07-13 03:24:28 +0000

    [ios showcase] Removes ios/web dependency re-land.
    Showcase is intended to be a standalone UI app and as such should not reference 
    //ios/web or //ios/chrome. This CL eliminates the Showcase app creating an IOSChromeMain 
    instance and thus implicitly being an //ios/web embedder, instead directly inlining the 
    minimal parts of that startup that are needed (initializing base::CommandLine and loading resources).
     
    As part of that change, this CL eliminates Showcase bundling //ios/web resources.
    
    Bug: 738880
    Change-Id: Idcd7849927e1d56fa1958de34e145beb362f4c3a
    Reviewed-on: https://chromium-review.googlesource.com/568243
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Louis Romero <lpromero@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486253}

--- ios/showcase/core/app_delegate.mm
+++ ios/showcase/core/app_delegate.mm
@@ -5,14 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/showcase/core/app_delegate.h"
 
+#include "base/command_line.h"
+#include "base/i18n/icu_util.h"
 #include "base/memory/ptr_util.h"
 #include "base/path_service.h"
-#include "ios/chrome/app/startup/ios_chrome_main.h"
 #import "ios/showcase/core/showcase_model.h"
 #import "ios/showcase/core/showcase_view_controller.h"
 #import "ios/third_party/material_components_ios/src/components/Typography/src/MaterialTypography.h"
-#import "ios/web/public/app/web_main_parts.h"
-#import "ios/web/public/web_client.h"
 #include "ui/base/resource/resource_bundle.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -21,42 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation AppDelegate
 @synthesize window = _window;
-
-namespace {
-
-class ShowcaseWebMainParts : public web::WebMainParts {
-  void PreMainMessageLoopStart() override {
-    ResourceBundle::InitSharedInstanceWithLocale(
-        std::string(), nullptr, ResourceBundle::LOAD_COMMON_RESOURCES);
-
-    base::FilePath pak_path;
-    PathService::Get(base::DIR_MODULE, &pak_path);
-    ResourceBundle::GetSharedInstance().AddDataPackFromPath(
-        pak_path.AppendASCII("showcase_resources.pak"), ui::SCALE_FACTOR_100P);
-  }
-};
-
-class ShowcaseWebClient : public web::WebClient {
- public:
-  ShowcaseWebClient() {}
-  ~ShowcaseWebClient() override {}
-
-  // WebClient implementation.
-  std::unique_ptr<web::WebMainParts> CreateWebMainParts() override {
-    return base::MakeUnique<ShowcaseWebMainParts>();
-  }
-  base::StringPiece GetDataResource(
-      int resource_id,
-      ui::ScaleFactor scale_factor) const override {
-    return ResourceBundle::GetSharedInstance().GetRawDataResourceForScale(
-        resource_id, scale_factor);
-  }
-  base::RefCountedMemory* GetDataResourceBytes(int resource_id) const override {
-    return ResourceBundle::GetSharedInstance().LoadDataResourceBytes(
-        resource_id);
-  }
-};
-}
 
 - (void)setupUI {
   ShowcaseViewController* viewController =
@@ -70,11 +33,10 @@ class ShowcaseWebClient : public web::WebClient {
 
 - (BOOL)application:(UIApplication*)application
     didFinishLaunchingWithOptions:(NSDictionary*)launchOptions {
-  // TODO(crbug.com/738880): Showcase ideally shouldn't be an embedder of
-  // //ios/web, in which case it wouldn't have to do this. This almost
-  // certainly means not creating IOSChromeMain.
-  web::SetWebClient(new ShowcaseWebClient());
-  base::MakeUnique<IOSChromeMain>();
+  base::CommandLine::Init(0, nullptr);
+  ResourceBundle::InitSharedInstanceWithLocale(
+      std::string(), nullptr, ResourceBundle::LOAD_COMMON_RESOURCES);
+  CHECK(base::i18n::InitializeICU());
 
   self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
   [self setupUI];
