00eda7a0ea06b95d66dd53ae96eaf7e73dea8ba4
Pavel Feldman <pfeldman@chromium.org>
2017-07-13 01:18:35 +0000

    DevTools: accept backendNodeId and object as a node identifier in selected DOM domain methods.
    Change-Id: I73a4bca08180684a447a1e41c4e268e322a8492d
    Reviewed-on: https://chromium-review.googlesource.com/568714
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486198}

--- third_party/WebKit/Source/core/inspector/InspectorDOMAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorDOMAgent.cpp
@@ -350,6 +350,27 @@ Response InspectorDOMAgent::AssertNode(int node_id, Node*& node) {
   return Response::OK();
 }
 
+Response InspectorDOMAgent::AssertNode(
+    const protocol::Maybe<int>& node_id,
+    const protocol::Maybe<int>& backend_node_id,
+    const protocol::Maybe<String>& object_id,
+    Node*& node) {
+  if (node_id.isJust())
+    return AssertNode(node_id.fromJust(), node);
+
+  if (backend_node_id.isJust()) {
+    node = DOMNodeIds::NodeForId(backend_node_id.fromJust());
+    return !node ? Response::Error("No node found for given backend id")
+                 : Response::OK();
+  }
+
+  if (object_id.isJust())
+    return NodeForRemoteObjectId(object_id.fromJust(), node);
+
+  return Response::Error(
+      "Either nodeId, backendNodeId or objectId must be specified");
+}
+
 Response InspectorDOMAgent::AssertElement(int node_id, Element*& element) {
   Node* node = nullptr;
   Response response = AssertNode(node_id, node);
@@ -1213,12 +1234,16 @@ Response InspectorDOMAgent::markUndoableState() {
   return Response::OK();
 }
 
-Response InspectorDOMAgent::focus(int node_id) {
-  Element* element = nullptr;
-  Response response = AssertElement(node_id, element);
+Response InspectorDOMAgent::focus(Maybe<int> node_id,
+                                  Maybe<int> backend_node_id,
+                                  Maybe<String> object_id) {
+  Node* node = nullptr;
+  Response response = AssertNode(node_id, backend_node_id, object_id, node);
   if (!response.isSuccess())
     return response;
-
+  if (!node->IsElementNode())
+    return Response::Error("Node is not an Element");
+  Element* element = ToElement(node);
   element->GetDocument().UpdateStyleAndLayoutIgnorePendingStylesheets();
   if (!element->IsFocusable())
     return Response::Error("Element is not focusable");
@@ -1227,10 +1252,12 @@ Response InspectorDOMAgent::focus(int node_id) {
 }
 
 Response InspectorDOMAgent::setFileInputFiles(
-    int node_id,
-    std::unique_ptr<protocol::Array<String>> files) {
+    std::unique_ptr<protocol::Array<String>> files,
+    Maybe<int> node_id,
+    Maybe<int> backend_node_id,
+    Maybe<String> object_id) {
   Node* node = nullptr;
-  Response response = AssertNode(node_id, node);
+  Response response = AssertNode(node_id, backend_node_id, object_id, node);
   if (!response.isSuccess())
     return response;
   if (!isHTMLInputElement(*node) ||
@@ -1245,10 +1272,12 @@ Response InspectorDOMAgent::setFileInputFiles(
 }
 
 Response InspectorDOMAgent::getBoxModel(
-    int node_id,
+    Maybe<int> node_id,
+    Maybe<int> backend_node_id,
+    Maybe<String> object_id,
     std::unique_ptr<protocol::DOM::BoxModel>* model) {
   Node* node = nullptr;
-  Response response = AssertNode(node_id, node);
+  Response response = AssertNode(node_id, backend_node_id, object_id, node);
   if (!response.isSuccess())
     return response;
 
