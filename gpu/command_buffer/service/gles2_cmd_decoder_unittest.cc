828c6c06a684b730896fd7bc5d373442cb39f27b
Antoine Labour <piman@chromium.org>
2017-07-13 00:12:01 +0000

    Have queries keep a reference to their shared memory buffer
    This allows 2 things:
    1- it makes the validation logic a lot simpler: we can just test the shared
       memory id/size/offset in BeginQuery and QueryCounter commands, and if it
       validates we can ensure it stays valid throughout the query lifetime, so
       it removes a lot of failure paths
    2- it ensures we don't need to wait for the query to be completed before freeing
       its memory on the client side, giving us the potential to reduce round trips
    
    Bug: 741215
    Change-Id: I63a76594c2d8299c4dead5b297d304a9aa204948
    Reviewed-on: https://chromium-review.googlesource.com/567721
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486172}

--- gpu/command_buffer/service/gles2_cmd_decoder_unittest.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder_unittest.cc
@@ -640,7 +640,7 @@ static error::Error ExecuteQueryCounterCmd(GLES2DecoderTestBase* test,
   return test->ExecuteCmd(query_counter_cmd);
 }
 
-static bool ProcessQuery(GLES2DecoderTestBase* test,
+static void ProcessQuery(GLES2DecoderTestBase* test,
                          ::gl::MockGLInterface* gl,
                          GLenum target,
                          GLuint service_id) {
@@ -660,16 +660,12 @@ static bool ProcessQuery(GLES2DecoderTestBase* test,
   }
 
   QueryManager* query_manager = test->GetDecoder()->GetQueryManager();
-  EXPECT_TRUE(nullptr != query_manager);
-  if (!query_manager)
-    return false;
-
-  return query_manager->ProcessPendingQueries(false);
+  ASSERT_TRUE(nullptr != query_manager);
+  query_manager->ProcessPendingQueries(false);
 }
 
 static void CheckBeginEndQueryBadMemoryFails(GLES2DecoderTestBase* test,
                                              GLuint client_id,
-                                             GLuint service_id,
                                              const QueryType& query_type,
                                              int32_t shm_id,
                                              uint32_t shm_offset) {
@@ -684,43 +680,29 @@ static void CheckBeginEndQueryBadMemoryFails(GLES2DecoderTestBase* test,
   init.request_alpha = true;
   init.bind_generates_resource = true;
   test->InitDecoder(init);
-  ::testing::StrictMock<::gl::MockGLInterface>* gl = test->GetGLMock();
-  ::gl::GPUTimingFake gpu_timing_queries;
 
-  ExecuteGenerateQueryCmd(test, gl, query_type.type,
-                          client_id, service_id);
+  test->GenHelper<GenQueriesEXTImmediate>(client_id);
 
   // Test bad shared memory fails
-  error::Error error1 = error::kNoError;
-  error::Error error2 = error::kNoError;
+  error::Error error = error::kNoError;
   if (query_type.is_counter) {
-    error1 =
-        ExecuteQueryCounterCmd(test, gl, &gpu_timing_queries, query_type.type,
-                               client_id, service_id, shm_id, shm_offset, 1);
+    QueryCounterEXT query_counter_cmd;
+    query_counter_cmd.Init(client_id, query_type.type, shm_id, shm_offset, 1);
+    error = test->ExecuteCmd(query_counter_cmd);
   } else {
-    error1 = ExecuteBeginQueryCmd(test, gl, &gpu_timing_queries,
-                                  query_type.type,
-                                  client_id, service_id,
-                                  shm_id, shm_offset);
-    error2 = ExecuteEndQueryCmd(test, gl, query_type.type, 1);
+    BeginQueryEXT begin_cmd;
+    begin_cmd.Init(query_type.type, client_id, shm_id, shm_offset);
+    error = test->ExecuteCmd(begin_cmd);
   }
 
-  bool process_success = ProcessQuery(test, gl, query_type.type, service_id);
+  EXPECT_TRUE(error != error::kNoError);
 
-  EXPECT_TRUE(error1 != error::kNoError || error2 != error::kNoError ||
-              !process_success);
-
-  if (GL_ANY_SAMPLES_PASSED_EXT == query_type.type)
-    EXPECT_CALL(*gl, DeleteQueries(1, _)).Times(1).RetiresOnSaturation();
   test->ResetDecoder();
 }
 
 TEST_P(GLES2DecoderManualInitTest, BeginEndQueryEXTBadMemoryIdFails) {
   for (size_t i = 0; i < arraysize(kQueryTypes); ++i) {
-    CheckBeginEndQueryBadMemoryFails(this,
-                                     kNewClientId,
-                                     kNewServiceId,
-                                     kQueryTypes[i],
+    CheckBeginEndQueryBadMemoryFails(this, kNewClientId, kQueryTypes[i],
                                      kInvalidSharedMemoryId,
                                      kSharedMemoryOffset);
   }
@@ -729,13 +711,12 @@ TEST_P(GLES2DecoderManualInitTest, BeginEndQueryEXTBadMemoryIdFails) {
 TEST_P(GLES2DecoderManualInitTest, BeginEndQueryEXTBadMemoryOffsetFails) {
   for (size_t i = 0; i < arraysize(kQueryTypes); ++i) {
     // Out-of-bounds.
-    CheckBeginEndQueryBadMemoryFails(this, kNewClientId, kNewServiceId,
-                                     kQueryTypes[i], shared_memory_id_,
+    CheckBeginEndQueryBadMemoryFails(this, kNewClientId, kQueryTypes[i],
+                                     shared_memory_id_,
                                      kInvalidSharedMemoryOffset);
     // Overflow.
-    CheckBeginEndQueryBadMemoryFails(this, kNewClientId, kNewServiceId,
-                                     kQueryTypes[i], shared_memory_id_,
-                                     0xfffffffcu);
+    CheckBeginEndQueryBadMemoryFails(this, kNewClientId, kQueryTypes[i],
+                                     shared_memory_id_, 0xfffffffcu);
   }
 }
 
@@ -775,7 +756,7 @@ TEST_P(GLES2DecoderManualInitTest, QueryReuseTest) {
                                                     query_type.type, 1));
     }
 
-    EXPECT_TRUE(ProcessQuery(this, gl, query_type.type, kNewServiceId));
+    ProcessQuery(this, gl, query_type.type, kNewServiceId);
 
     // Reuse query.
     if (query_type.is_counter) {
@@ -794,7 +775,7 @@ TEST_P(GLES2DecoderManualInitTest, QueryReuseTest) {
                                                     query_type.type, 2));
     }
 
-    EXPECT_TRUE(ProcessQuery(this, gl, query_type.type, kNewServiceId));
+    ProcessQuery(this, gl, query_type.type, kNewServiceId);
 
     if (GL_ANY_SAMPLES_PASSED_EXT == query_type.type)
       EXPECT_CALL(*gl, DeleteQueries(1, _)).Times(1).RetiresOnSaturation();
@@ -926,9 +907,8 @@ TEST_P(GLES2DecoderManualInitTest, BeginEndQueryEXTCommandsCompletedCHROMIUM) {
   EXPECT_CALL(*gl_, ClientWaitSync(kGlSync, _, _))
       .WillOnce(Return(GL_TIMEOUT_EXPIRED))
       .RetiresOnSaturation();
-  bool process_success = query_manager->ProcessPendingQueries(false);
+  query_manager->ProcessPendingQueries(false);
 
-  EXPECT_TRUE(process_success);
   EXPECT_TRUE(query->IsPending());
 
 #if DCHECK_IS_ON()
@@ -939,9 +919,8 @@ TEST_P(GLES2DecoderManualInitTest, BeginEndQueryEXTCommandsCompletedCHROMIUM) {
   EXPECT_CALL(*gl_, ClientWaitSync(kGlSync, _, _))
       .WillOnce(Return(GL_ALREADY_SIGNALED))
       .RetiresOnSaturation();
-  process_success = query_manager->ProcessPendingQueries(false);
+  query_manager->ProcessPendingQueries(false);
 
-  EXPECT_TRUE(process_success);
   EXPECT_FALSE(query->IsPending());
 
 #if DCHECK_IS_ON()
