5b156e5c3f2338d20dbe94945832a14836d0a95b
Brian Salomon <bsalomon@google.com>
2017-07-13 02:38:41 +0000

    Use GrBackendTexture rather than GrBackendTextureDesc in cc
    GrBackendTextureDesc is being phased out.
    
    Change-Id: I0c2d00b0a80c63037ecc937021ef8bf978f4c359
    Reviewed-on: https://chromium-review.googlesource.com/563119
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Commit-Queue: Brian Salomon <bsalomon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486231}

--- cc/output/gl_renderer.cc
+++ cc/output/gl_renderer.cc
@@ -60,6 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkColorFilter.h"
 #include "third_party/skia/include/core/SkImage.h"
 #include "third_party/skia/include/core/SkSurface.h"
+#include "third_party/skia/include/gpu/GrBackendSurface.h"
 #include "third_party/skia/include/gpu/GrContext.h"
 #include "third_party/skia/include/gpu/gl/GrGLInterface.h"
 #include "third_party/skia/include/gpu/gl/GrGLTypes.h"
@@ -639,20 +640,17 @@ static sk_sp<SkImage> WrapTexture(
     const ResourceProvider::ScopedReadLockGL& lock,
     GrContext* context,
     bool flip_texture) {
-  // Wrap a given texture in a Ganesh platform texture.
-  GrBackendTextureDesc backend_texture_description;
+  // Wrap a given texture in a Ganesh backend texture.
   GrGLTextureInfo texture_info;
   texture_info.fTarget = lock.target();
   texture_info.fID = lock.texture_id();
-  backend_texture_description.fWidth = lock.size().width();
-  backend_texture_description.fHeight = lock.size().height();
-  backend_texture_description.fConfig = kSkia8888_GrPixelConfig;
-  backend_texture_description.fTextureHandle =
-      skia::GrGLTextureInfoToGrBackendObject(texture_info);
-  backend_texture_description.fOrigin =
+  GrBackendTexture backend_texture(lock.size().width(), lock.size().height(),
+                                   kSkia8888_GrPixelConfig, texture_info);
+  GrSurfaceOrigin origin =
       flip_texture ? kBottomLeft_GrSurfaceOrigin : kTopLeft_GrSurfaceOrigin;
 
-  return SkImage::MakeFromTexture(context, backend_texture_description);
+  return SkImage::MakeFromTexture(context, backend_texture, origin,
+                                  kPremul_SkAlphaType, nullptr);
 }
 
 static sk_sp<SkImage> ApplyImageFilter(
