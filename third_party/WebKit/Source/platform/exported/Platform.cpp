ed70a2e56c24e818f7be369bfb95b090b47018b7
Fernando Serboncini <fserb@chromium.org>
2017-07-13 22:13:35 +0000

    Initialize FontFamilyNames also on Platform.
    This is needed because PPAPI uses Platform without Core, so font names
    aren't initialized properly there.
    
    Bug: 742311
    Change-Id: Ia0c89ffa9391759b54a73ec0d020ff437343e7a3
    Reviewed-on: https://chromium-review.googlesource.com/570661
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Commit-Queue: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486488}

--- third_party/WebKit/Source/platform/exported/Platform.cpp
+++ third_party/WebKit/Source/platform/exported/Platform.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/trace_event/memory_dump_manager.h"
+#include "platform/FontFamilyNames.h"
 #include "platform/Histogram.h"
 #include "platform/Language.h"
 #include "platform/MemoryCoordinator.h"
@@ -124,6 +125,11 @@ void Platform::Initialize(Platform* platform) {
         base::ThreadTaskRunnerHandle::Get());
 
   ThreadState::AttachMainThread();
+
+  // FontFamilyNames are used by platform/fonts and are initialized by core.
+  // In case core is not available (like on PPAPI plugins), we need to init
+  // them here.
+  FontFamilyNames::init();
   InitializePlatformLanguage();
 
   // TODO(ssid): remove this check after fixing crbug.com/486782.
