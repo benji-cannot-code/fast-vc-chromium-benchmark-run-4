78a982e21502c2e614d0f782c88a00c6f1e21ba8
arthursonzogni <arthursonzogni@chromium.org>
2017-07-13 15:18:16 +0000

    Reland of: Block redirects to renderer-debug urls.
    This is a reland of https://codereview.chromium.org/2973433003/
    It was reverted for compilation error.
    
    Original CL description:
    > Redirects toward renderer-debug urls (chrome://crash, javascript:[...],
    > ...) were not expected to happen in the NavigationHandleImpl. It caused
    > WebContentsObserver::DidFinishNavigation not being called and
    > consequently, bugs in some WebContentsObservers.
    >
    > This CL blocks any redirects to renderer-debug urls. One test is added.
    >
    > The blocking of the URL was moved (and partially removed) in:
    > https://crrev.com/2436253002
    > This CL tries to restore the previous behavior.
    
    BUG=736658,704892
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation;master.tryserver.chromium.linux:linux_chromium_browser_side_navigation_rel
    
    Change-Id: I1e82511b88acfabcb29e4fad102a5efa32ddca9a
    Reviewed-on: https://chromium-review.googlesource.com/569878
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486384}

--- content/browser/frame_host/navigation_handle_impl.cc
+++ content/browser/frame_host/navigation_handle_impl.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <iterator>
 
-#include "base/debug/dump_without_crashing.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
 #include "content/browser/appcache/appcache_navigation_handle.h"
@@ -125,7 +124,6 @@ NavigationHandleImpl::NavigationHandleImpl(
       is_form_submission_(is_form_submission),
       expected_render_process_host_id_(ChildProcessHost::kInvalidUniqueID),
       weak_factory_(this) {
-  is_in_constructor = true;
   TRACE_EVENT_ASYNC_BEGIN2("navigation", "NavigationHandle", this,
                            "frame_tree_node",
                            frame_tree_node_->frame_tree_node_id(), "url",
@@ -179,15 +177,9 @@ NavigationHandleImpl::NavigationHandleImpl(
     TRACE_EVENT_ASYNC_STEP_INTO0("navigation", "NavigationHandle", this,
                                  "Same document");
   }
-  is_in_constructor = false;
 }
 
 NavigationHandleImpl::~NavigationHandleImpl() {
-  // TODO(arthursonzogni): Remove this when we understand the root cause behind
-  // crbug.com/704892.
-  if (is_in_constructor)
-    base::debug::DumpWithoutCrashing();
-
   // Inform the RenderProcessHost to no longer expect a navigation.
   if (expected_render_process_host_id_ != ChildProcessHost::kInvalidUniqueID) {
     RenderProcessHost* process =
@@ -654,10 +646,13 @@ void NavigationHandleImpl::WillRedirectRequest(
                                "WillRedirectRequest", "url",
                                new_url.possibly_invalid_spec());
 
-  // TODO(arthursonzogni): Remove this when we understand the root cause behind
-  // crbug.com/704892 and crbug.com/736658
-  if (IsRendererDebugURL(new_url))
-    base::debug::DumpWithoutCrashing();
+  // |new_url| is not expected to be a "renderer debug" url. It should be
+  // blocked in NavigationRequest::OnRequestRedirected or in
+  // ResourceLoader::OnReceivedRedirect. If it is not the case,
+  // DidFinishNavigation will not be called. It could confuse some
+  // WebContentsObserver because DidStartNavigation was called.
+  // See https://crbug.com/728398.
+  CHECK(!IsRendererDebugURL(new_url));
 
   // Update the navigation parameters.
   url_ = new_url;
