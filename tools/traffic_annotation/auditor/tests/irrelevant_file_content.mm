5e507e677059e0c55eef3da320b9a9c8403b313b
Ramin Halavati <rhalavati@chromium.org>
2017-07-13 05:00:34 +0000

    More tests added to network traffic annotation auditor.
    traffic_annotation_auditor_unittest expanded to include more tests.
    
    Bug: 656607
    Bug: 690323
    Change-Id: I1cb0bf46a7742c1bbef27cf7e2de3f70e22d48c6
    Reviewed-on: https://chromium-review.googlesource.com/566818
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486272}

---
+++ tools/traffic_annotation/auditor/tests/irrelevant_file_content.mm
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This is a sample file that has relevant file extension but does not includes
+// any keywords relevant to network traffic annotation should not be included
+// in tests.
+
+#include "net/traffic_annotation/network_traffic.h"
+
+// Some other content including network and traffic and annotation!
