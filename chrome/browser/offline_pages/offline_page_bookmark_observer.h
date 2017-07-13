7a5c90c1ac2f9eb253d72bc71b5eacf052b94cbc
Dmitry Titov <dimich@chromium.org>
2017-07-13 01:25:49 +0000

    [Offline Pages] Move files without dependency on Android to os-independent folder.
    This is ongoing refactoring of offline pages, in order to remove/isolate
    Android-specific dependdencies. In this case, it's just a result
    of:
    tools/git/move_source_file.py chrome/browser/android/offline_pages/offline_page_bookmark_observer.* chrome/browser/offline_pages/
    
    Bug: 722467
    Change-Id: I35ecde79bee6b6fba7d7c27d75c5fb7be1799c00
    Reviewed-on: https://chromium-review.googlesource.com/568565
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Yafei Duan <romax@chromium.org>
    Commit-Queue: Dmitry Titov <dimich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486202}

--- chrome/browser/offline_pages/offline_page_bookmark_observer.h
+++ chrome/browser/offline_pages/offline_page_bookmark_observer.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_ANDROID_OFFLINE_PAGES_OFFLINE_PAGE_BOOKMARK_OBSERVER_H_
-#define CHROME_BROWSER_ANDROID_OFFLINE_PAGES_OFFLINE_PAGE_BOOKMARK_OBSERVER_H_
+#ifndef CHROME_BROWSER_OFFLINE_PAGES_OFFLINE_PAGE_BOOKMARK_OBSERVER_H_
+#define CHROME_BROWSER_OFFLINE_PAGES_OFFLINE_PAGE_BOOKMARK_OBSERVER_H_
 
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/offline_pages/offline_page_model_factory.h"
@@ -53,4 +53,4 @@ class OfflinePageBookmarkObserver
 };
 }  // namespace offline_pages
 
-#endif  // CHROME_BROWSER_ANDROID_OFFLINE_PAGES_OFFLINE_PAGE_BOOKMARK_OBSERVER_H_
+#endif  // CHROME_BROWSER_OFFLINE_PAGES_OFFLINE_PAGE_BOOKMARK_OBSERVER_H_
