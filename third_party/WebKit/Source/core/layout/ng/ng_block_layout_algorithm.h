3e87ef7b941f5ded73ad2d770b913cc5f3422325
ikilpatrick <ikilpatrick@chromium.org>
2017-07-13 11:10:10 -0700

    [LayoutNG] Fix IsEmptyBlock check within block_layout_algorithm.
    Previoiusly we were just checking fragment.BlockSize() which was error
    prone as:
    <div style="height: 0px;">text</div>
    ... would have been considered an empty block.
    
    Now the check uses the presence of the BfcOffset on the child's layout
    result. If it isn't present, the child must be empty.
    
    BUG=635619
    
    Review-Url: https://codereview.chromium.org/2982643002
    Cr-Commit-Position: refs/heads/master@{#486440}

--- third_party/WebKit/Source/core/layout/ng/ng_block_layout_algorithm.h
+++ third_party/WebKit/Source/core/layout/ng/ng_block_layout_algorithm.h
@@ -81,6 +81,7 @@ class CORE_EXPORT NGBlockLayoutAlgorithm
 
   NGPreviousInflowPosition ComputeInflowPosition(
       const NGPreviousInflowPosition& previous_inflow_position,
+      const NGLayoutInputNode child,
       const NGInflowChildData& child_data,
       const WTF::Optional<NGLogicalOffset>& child_bfc_offset,
       const NGLogicalOffset& logical_offset,
