00ddef739ed10ea3c74fdeb57c551d5f36c71cef
Valery Arkhangorodsky <varkha@chromium.org>
2017-07-13 20:08:06 +0000

    Makes it easier to resize transient children of top level windows
    Extends the use of EasyResizeWindowTargeter to transient children of
    top level windows that currently use EasyResizeWindowTargeter - as long
    as those children can themselves be resized.
    
    Bug: 741013
    Test: added ToplevelWindowEventHandlerTest.EasyResizerUsed
    Change-Id: I5e9db48e80210a0ee108dcbd8f02dd33b1c62d78
    Reviewed-on: https://chromium-review.googlesource.com/567266
    Commit-Queue: Valery Arkhangorodsky <varkha@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486466}

--- ash/wm/window_modality_controller_unittest.cc
+++ ash/wm/window_modality_controller_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/test/ash_test_base.h"
 #include "ash/test/child_modal_window.h"
 #include "ash/wm/window_util.h"
+#include "services/ui/public/interfaces/window_manager.mojom.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/client/capture_client.h"
 #include "ui/aura/test/test_window_delegate.h"
@@ -413,6 +414,11 @@ TEST_F(WindowModalityControllerTest, TouchEvent) {
   TouchTrackerWindowDelegate d11;
   std::unique_ptr<aura::Window> w11(CreateTestWindowInShellWithDelegate(
       &d11, -11, gfx::Rect(20, 20, 50, 50)));
+  // Make |w11| non-resizable to avoid touch events inside its transient parent
+  // |w1| from going to |w11| because of EasyResizeWindowTargeter.
+  w11->SetProperty(aura::client::kResizeBehaviorKey,
+                   ui::mojom::kResizeBehaviorCanMaximize |
+                       ui::mojom::kResizeBehaviorCanMinimize);
   ui::test::EventGenerator generator(Shell::GetPrimaryRootWindow(),
                                      gfx::Point(10, 10));
 
