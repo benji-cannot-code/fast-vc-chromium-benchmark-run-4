fd1ae4ca1d0cce46abfbb2f7c7382143486c9227
Nico Weber <thakis@chromium.org>
2017-07-13 16:57:40 +0000

    Revert "Remove unsupported perm, symlink calls for Fuchsia."
    This reverts commit 52118eee387b24fe8c3001a7e31d11f588a8b183.
    
    Reason for revert:
    Breaks build: https://luci-logdog.appspot.com/v/?s=chromium%2Fbb%2Fchromium.fyi%2FFuchsia%2F6583%2F%2B%2Frecipes%2Fsteps%2Fcompile%2F0%2Fstdout
    
    Original change's description:
    > Remove unsupported perm, symlink calls for Fuchsia.
    > 
    > Remove unsupported perm, symlink calls for Fuchsia.
    > Fuchsia's POSIX support does not extend to file permission bit or
    > symbolic links. This CL conditionally removes the relevant calls from
    > file_util and disables affected tests under Fuchsia builds.
    > 
    > R: thakis@chromium.org
    > Bug: 706592
    > Change-Id: Id6b68fd9a440f37debe50feb7ce3f457bbee3e4f
    > Reviewed-on: https://chromium-review.googlesource.com/562901
    > Reviewed-by: Nico Weber <thakis@chromium.org>
    > Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#486236}
    
    TBR=thakis@chromium.org,kmarshall@chromium.org
    
    Change-Id: Icdd32651551e1a77adf9322fb531d4af69b56b38
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 706592
    Reviewed-on: https://chromium-review.googlesource.com/570499
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486414}

--- base/files/file_util_posix.cc
+++ base/files/file_util_posix.cc
@@ -439,8 +439,6 @@ bool ReadFromFD(int fd, char* buffer, size_t bytes) {
 }
 
 #if !defined(OS_NACL_NONSFI)
-
-#if !defined(OS_FUCHSIA)
 bool CreateSymbolicLink(const FilePath& target_path,
                         const FilePath& symlink_path) {
   DCHECK(!symlink_path.empty());
@@ -516,8 +514,6 @@ bool ExecutableExistsInPath(Environment* env,
   }
   return false;
 }
-
-#endif  // !OS_FUCHSIA
 
 #if !defined(OS_MACOSX)
 // This is implemented in file_util_mac.mm for Mac.
