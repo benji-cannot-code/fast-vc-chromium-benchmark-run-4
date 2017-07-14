95e920d1640e2c42ee31bfc1a80a34b9146227ec
thomasanderson <thomasanderson@chromium.org>
2017-07-13 18:34:31 -0700

    [Protobuf] Add <map> include to workaround a gcc bug
    GCC gets a bit confused by libc++'s map headers, and including <map>
    after <set> can cause problems under certain conditions [1].
    
    [1] https://gcc.gnu.org/bugzilla/show_bug.cgi?id=81347
    
    BUG=webrtc:7922
    R=pkasting@chromium.org,dpranke@chromium.org
    
    Review-Url: https://codereview.chromium.org/2974603002
    Cr-Commit-Position: refs/heads/master@{#486608}

--- third_party/protobuf/src/google/protobuf/descriptor.h
+++ third_party/protobuf/src/google/protobuf/descriptor.h
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef GOOGLE_PROTOBUF_DESCRIPTOR_H__
 #define GOOGLE_PROTOBUF_DESCRIPTOR_H__
 
+#include <map>
 #include <memory>
 #ifndef _SHARED_PTR_H
 #include <google/protobuf/stubs/shared_ptr.h>
