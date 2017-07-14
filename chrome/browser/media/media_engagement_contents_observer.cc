dd7f9fcbf936b365cc41a52bc129b24ffb59a46b
Becca Hughes <beccahughes@chromium.org>
2017-07-14 13:28:55 +0000

    Media Engagement: Record score at playback.
    Record the Media Engagement score at playback to a histogram
    so we can see if the playback would have been blocked.
    
    BUG=740106
    
    Change-Id: I7ed9659e64fd9ffcae3eafaccdf32e6755165ccb
    Reviewed-on: https://chromium-review.googlesource.com/566862
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486747}

--- chrome/browser/media/media_engagement_contents_observer.cc
+++ chrome/browser/media/media_engagement_contents_observer.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/media/media_engagement_contents_observer.h"
 
+#include "base/metrics/histogram_macros.h"
 #include "build/build_config.h"
 #include "chrome/browser/media/media_engagement_service.h"
 #include "content/public/browser/navigation_handle.h"
@@ -21,6 +22,9 @@ constexpr base::TimeDelta kSignificantMediaPlaybackTime =
 // element has to be in order to be determined significant.
 const gfx::Size MediaEngagementContentsObserver::kSignificantSize =
     gfx::Size(200, 140);
+
+const char* MediaEngagementContentsObserver::kHistogramScoreAtPlaybackName =
+    "Media.Engagement.ScoreAtPlayback";
 
 MediaEngagementContentsObserver::MediaEngagementContentsObserver(
     content::WebContents* web_contents,
@@ -93,12 +97,31 @@ MediaEngagementContentsObserver::GetPlayerState(const MediaPlayerId& id) {
 void MediaEngagementContentsObserver::MediaStartedPlaying(
     const MediaPlayerInfo& media_player_info,
     const MediaPlayerId& media_player_id) {
-  if (!media_player_info.has_audio)
-    return;
+  PlayerState* state = GetPlayerState(media_player_id);
+  state->playing = true;
+  state->has_audio = media_player_info.has_audio;
 
-  GetPlayerState(media_player_id)->playing = true;
   MaybeInsertSignificantPlayer(media_player_id);
   UpdateTimer();
+  RecordEngagementScoreToHistogramAtPlayback(media_player_id);
+}
+
+void MediaEngagementContentsObserver::
+    RecordEngagementScoreToHistogramAtPlayback(const MediaPlayerId& id) {
+  GURL url = committed_origin_.GetURL();
+  if (!service_->ShouldRecordEngagement(url))
+    return;
+
+  PlayerState* state = GetPlayerState(id);
+  if (!state->playing || state->muted || !state->has_audio ||
+      state->score_recorded)
+    return;
+
+  int percentage = round(service_->GetEngagementScore(url) * 100);
+  UMA_HISTOGRAM_PERCENTAGE(
+      MediaEngagementContentsObserver::kHistogramScoreAtPlaybackName,
+      percentage);
+  state->score_recorded = true;
 }
 
 void MediaEngagementContentsObserver::MediaMutedStatusChanged(
@@ -112,6 +135,7 @@ void MediaEngagementContentsObserver::MediaMutedStatusChanged(
     MaybeInsertSignificantPlayer(id);
 
   UpdateTimer();
+  RecordEngagementScoreToHistogramAtPlayback(id);
 }
 
 void MediaEngagementContentsObserver::MediaResized(const gfx::Size& size,
