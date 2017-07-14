e5db762609891b196b80105b3ed03908c2f85e40
Juan A. Navarro Perez <perezju@chromium.org>
2017-07-14 11:20:12 +0000

    [tools/perf] Use correct browser for try jobs on webview bots
    Use --browser=android-webview on trybot command when selected bots
    include webview.
    
    NOTRY=True
    
    Bug: 740845
    Change-Id: Ib1d7f436a6d290a9cb63a3f7aaebfb92581e07d8
    Reviewed-on: https://chromium-review.googlesource.com/567933
    Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Reviewed-by: Simon Hatch <simonhatch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486734}

--- tools/perf/core/trybot_command_unittest.py
+++ tools/perf/core/trybot_command_unittest.py
@@ -167,6 +167,7 @@ class TrybotCommandTest(unittest.TestCase):
         'android_nexus4_perf_bisect': 'stuff',
         'mac_10_9_perf_bisect': 'otherstuff',
         'win_perf_bisect_builder': 'not a trybot',
+        'android_fyi_perf_bisect': 'not meant for actual perf testing'
     })
     expected_trybots_list = [
         'all',
@@ -208,6 +209,7 @@ class TrybotCommandTest(unittest.TestCase):
     self._MockTryserverJson({
         'android_nexus4_perf_bisect': 'stuff',
         'android_nexus5_perf_bisect': 'stuff2',
+        'android_webview_nexus6_aosp_perf_bisect': 'stuff3',
         'mac_10_9_perf_bisect': 'otherstuff',
         'mac_perf_bisect': 'otherstuff1',
         'win_perf_bisect': 'otherstuff2',
@@ -218,11 +220,14 @@ class TrybotCommandTest(unittest.TestCase):
     command = trybot_command.Trybot()
     command._InitializeBuilderNames('all')
     self.assertEquals(
-        ['android', 'linux', 'mac', 'win', 'win-x64'],
+        ['android', 'android-webview', 'linux', 'mac', 'win', 'win-x64'],
         sorted(command._builder_names))
     self.assertEquals(
         ['android_nexus4_perf_bisect', 'android_nexus5_perf_bisect'],
         sorted(command._builder_names.get('android')))
+    self.assertEquals(
+        ['android_webview_nexus6_aosp_perf_bisect'],
+        sorted(command._builder_names.get('android-webview')))
     self.assertEquals(
         ['mac_10_9_perf_bisect', 'mac_perf_bisect'],
         sorted(command._builder_names.get('mac')))
@@ -266,6 +271,7 @@ class TrybotCommandTest(unittest.TestCase):
     self._MockTryserverJson({
         'android_nexus4_perf_bisect': 'stuff',
         'android_nexus5_perf_bisect': 'stuff2',
+        'android_webview_nexus6_aosp_perf_bisect': 'stuff3',
         'win_8_perf_bisect': 'otherstuff',
         'win_perf_bisect': 'otherstuff2',
         'linux_perf_bisect': 'otherstuff3',
@@ -277,6 +283,9 @@ class TrybotCommandTest(unittest.TestCase):
     self.assertEquals(
         ['android_nexus4_perf_bisect', 'android_nexus5_perf_bisect'],
         sorted(command._builder_names.get('android')))
+    self.assertEquals(
+        ['android_webview_nexus6_aosp_perf_bisect'],
+        sorted(command._builder_names.get('android-webview')))
 
   def testConstructorTrybotAllMac(self):
     self._MockTryserverJson({
@@ -391,6 +400,18 @@ class TrybotCommandTest(unittest.TestCase):
     self.assertEquals(
         {'command': ('src/tools/perf/run_benchmark '
                      '--browser=android-chromium sunspider --verbose'),
+         'max_time_minutes': '120',
+         'repeat_count': '1',
+         'target_arch': 'ia32',
+         'truncate_percent': '0'
+        }, config)
+
+  def testConfigAndroidWebview(self):
+    config, _ = self._GetConfigForTrybot(
+        'android-webview-nexus6-aosp', 'android-webview')
+    self.assertEquals(
+        {'command': ('src/tools/perf/run_benchmark '
+                     '--browser=android-webview sunspider --verbose'),
          'max_time_minutes': '120',
          'repeat_count': '1',
          'target_arch': 'ia32',
