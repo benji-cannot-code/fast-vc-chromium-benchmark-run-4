c9b12b1d2c9bb5d21016be96f51c1e052a65ec1b
Jia <jiameng@chromium.org>
2017-07-14 08:16:45 +0000

    Implement parseShorthand for scroll-padding-inline and scroll-snap-margin-[block|inline].
    All these shorthand properties call the same helper function
    ConsumeShorthandVia2LonghandAPIs. Hence these properties are grouped in
    one cl.
    
    Bug: 668012
    Change-Id: I308be0165a6e7962874053e6cbefd831cac6f410
    Diff: https://gist.github.com/06821a4b93df191366b1b8a2ac9d01ad/revisions
    Reviewed-on: https://chromium-review.googlesource.com/571338
    Commit-Queue: Jia Meng <jiameng@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486707}

---
+++ third_party/WebKit/Source/core/css/properties/CSSShorthandPropertyAPIScrollSnapMarginInline.cpp
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/properties/CSSShorthandPropertyAPIScrollSnapMarginInline.h"
+
+#include "core/StylePropertyShorthand.h"
+#include "core/css/parser/CSSPropertyParserHelpers.h"
+
+namespace blink {
+
+bool CSSShorthandPropertyAPIScrollSnapMarginInline::parseShorthand(
+    bool important,
+    CSSParserTokenRange& range,
+    const CSSParserContext& context,
+    bool,
+    HeapVector<CSSProperty, 256>& properties) {
+  return CSSPropertyParserHelpers::ConsumeShorthandVia2LonghandAPIs(
+      scrollSnapMarginInlineShorthand(), important, context, range, properties);
+}
+}  // namespace blink
