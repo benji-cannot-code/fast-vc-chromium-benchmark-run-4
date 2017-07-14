dc59d1184cc5b7af0d696cfad1d1deaf85cfd1e7
Yoshifumi Inoue <yosin@chromium.org>
2017-07-14 06:52:50 +0000

    Move FrameSelection::Modify() for page granularity to EditorComamand.cpp
    2017-07-14T11:14:28
    
    2017-07-14T11: 14:22
    Bug: 
    Change-Id: I39e2a7d0906be5f8883d7467d2414babf6a84362
    Reviewed-on: https://chromium-review.googlesource.com/571559
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486694}

--- third_party/WebKit/Source/core/editing/commands/EditorCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/EditorCommand.cpp
@@ -1225,6 +1225,29 @@ static bool ExecuteMoveLeftAndModifySelection(LocalFrame& frame,
   return true;
 }
 
+// Returns true if selection is modified.
+bool ModifySelectionyWithPageGranularity(
+    LocalFrame& frame,
+    FrameSelection::EAlteration alter,
+    unsigned vertical_distance,
+    FrameSelection::VerticalDirection direction) {
+  SelectionModifier selection_modifier(
+      frame, frame.Selection().ComputeVisibleSelectionInDOMTree());
+  if (!selection_modifier.ModifyWithPageGranularity(alter, vertical_distance,
+                                                    direction)) {
+    return false;
+  }
+
+  frame.Selection().SetSelection(selection_modifier.Selection().AsSelection(),
+                                 FrameSelection::kCloseTyping |
+                                     FrameSelection::kClearTypingStyle |
+                                     kUserTriggered,
+                                 alter == FrameSelection::kAlterationMove
+                                     ? CursorAlignOnScroll::kAlways
+                                     : CursorAlignOnScroll::kIfNeeded);
+  return true;
+}
+
 static bool ExecuteMovePageDown(LocalFrame& frame,
                                 Event*,
                                 EditorCommandSource,
@@ -1232,8 +1255,9 @@ static bool ExecuteMovePageDown(LocalFrame& frame,
   unsigned distance = VerticalScrollDistance(frame);
   if (!distance)
     return false;
-  return frame.Selection().Modify(FrameSelection::kAlterationMove, distance,
-                                  FrameSelection::kDirectionDown);
+  return ModifySelectionyWithPageGranularity(
+      frame, FrameSelection::kAlterationMove, distance,
+      FrameSelection::kDirectionDown);
 }
 
 static bool ExecuteMovePageDownAndModifySelection(LocalFrame& frame,
@@ -1243,8 +1267,9 @@ static bool ExecuteMovePageDownAndModifySelection(LocalFrame& frame,
   unsigned distance = VerticalScrollDistance(frame);
   if (!distance)
     return false;
-  return frame.Selection().Modify(FrameSelection::kAlterationExtend, distance,
-                                  FrameSelection::kDirectionDown);
+  return ModifySelectionyWithPageGranularity(
+      frame, FrameSelection::kAlterationExtend, distance,
+      FrameSelection::kDirectionDown);
 }
 
 static bool ExecuteMovePageUp(LocalFrame& frame,
@@ -1254,8 +1279,9 @@ static bool ExecuteMovePageUp(LocalFrame& frame,
   unsigned distance = VerticalScrollDistance(frame);
   if (!distance)
     return false;
-  return frame.Selection().Modify(FrameSelection::kAlterationMove, distance,
-                                  FrameSelection::kDirectionUp);
+  return ModifySelectionyWithPageGranularity(
+      frame, FrameSelection::kAlterationMove, distance,
+      FrameSelection::kDirectionUp);
 }
 
 static bool ExecuteMovePageUpAndModifySelection(LocalFrame& frame,
@@ -1265,8 +1291,9 @@ static bool ExecuteMovePageUpAndModifySelection(LocalFrame& frame,
   unsigned distance = VerticalScrollDistance(frame);
   if (!distance)
     return false;
-  return frame.Selection().Modify(FrameSelection::kAlterationExtend, distance,
-                                  FrameSelection::kDirectionUp);
+  return ModifySelectionyWithPageGranularity(
+      frame, FrameSelection::kAlterationExtend, distance,
+      FrameSelection::kDirectionUp);
 }
 
 static bool ExecuteMoveRight(LocalFrame& frame,
