3203c78dc8b4a00523f02d9a95de916ba947506e
Randall Raymond <rayraymond@google.com>
2017-07-14 01:30:45 +0000

    [Payment Request] Adds functionality for parsing payent apps from Payment Method Data
    Bug: 602666
    Change-Id: I7d21288a1ec457267d213aad8c0b0c0d001203e5
    Reviewed-on: https://chromium-review.googlesource.com/567438
    Reviewed-by: mahmadi <mahmadi@chromium.org>
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Commit-Queue: Randall Raymond <rayraymond@google.com>
    Cr-Commit-Position: refs/heads/master@{#486605}

--- components/payments/core/payment_request_data_util.cc
+++ components/payments/core/payment_request_data_util.cc
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/payments/core/payment_address.h"
 #include "components/payments/core/payment_method_data.h"
 #include "third_party/libphonenumber/phonenumber_api.h"
+#include "url/gurl.h"
+#include "url/url_constants.h"
 
 namespace payments {
 namespace data_util {
@@ -92,12 +94,14 @@ BasicCardResponse GetBasicCardResponseFromAutofillCreditCard(
   return response;
 }
 
-void ParseBasicCardSupportedNetworks(
+void ParseSupportedMethods(
     const std::vector<PaymentMethodData>& method_data,
     std::vector<std::string>* out_supported_networks,
-    std::set<std::string>* out_basic_card_specified_networks) {
+    std::set<std::string>* out_basic_card_specified_networks,
+    std::vector<std::string>* out_url_payment_method_identifiers) {
   DCHECK(out_supported_networks->empty());
   DCHECK(out_basic_card_specified_networks->empty());
+  DCHECK(out_url_payment_method_identifiers->empty());
 
   const std::set<std::string> kBasicCardNetworks{
       "amex",       "diners", "discover", "jcb",
@@ -150,6 +154,13 @@ void ParseBasicCardSupportedNetworks(
             }
           }
         }
+      } else {
+        // Here |method| could be a repeated deprecated supported network (e.g.,
+        // "visa"), some invalid string or a URL Payment Method Identifier.
+        // Capture this last category if the URL is https.
+        GURL url(method);
+        if (url.is_valid() && url.SchemeIs(url::kHttpsScheme))
+          out_url_payment_method_identifiers->push_back(method);
       }
     }
   }
