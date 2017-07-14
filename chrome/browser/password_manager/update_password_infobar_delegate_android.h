e46115dd2241a94763a7ab64cac96e1f8a592dcc
Dominic Battre <battre@chromium.org>
2017-07-14 07:42:53 +0000

    UKMs for password save/update bubbles
    This CL introduces measuring the events that save/update bubbles are triggered
    and reasons why save/update bubbles are dismissed.
    
    NOTRY=true
    
    Bug: 732846
    Change-Id: I885985e0ffcd610af8c2f0a1d55824d63330b43f
    Reviewed-on: https://chromium-review.googlesource.com/565286
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Tatiana Gornak <melandory@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486699}

--- chrome/browser/password_manager/update_password_infobar_delegate_android.h
+++ chrome/browser/password_manager/update_password_infobar_delegate_android.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/password_manager/password_manager_infobar_delegate_android.h"
 #include "chrome/browser/ui/passwords/manage_passwords_state.h"
 #include "components/password_manager/core/browser/password_form_manager.h"
+#include "components/password_manager/core/browser/password_manager_metrics_util.h"
 
 namespace content {
 class WebContents;
@@ -58,11 +59,16 @@ class UpdatePasswordInfoBarDelegate : public PasswordManagerInfoBarDelegate {
       std::unique_ptr<password_manager::PasswordFormManager> form_to_update,
       bool is_smartlock_branding_enabled);
 
+  // Used to track the results we get from the info bar.
+  password_manager::metrics_util::UIDismissalReason infobar_response_;
+
   // ConfirmInfoBarDelegate:
   infobars::InfoBarDelegate::InfoBarIdentifier GetIdentifier() const override;
   int GetButtons() const override;
   base::string16 GetButtonLabel(InfoBarButton button) const override;
   bool Accept() override;
+  void InfoBarDismissed() override;
+  bool Cancel() override;
 
   ManagePasswordsState passwords_state_;
   base::string16 branding_;
--- components/password_manager/core/browser/password_form_manager.h
+++ components/password_manager/core/browser/password_form_manager.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/password_form_metrics_recorder.h"
 #include "components/password_manager/core/browser/password_form_user_action.h"
 #include "components/password_manager/core/browser/password_manager_driver.h"
+#include "components/password_manager/core/browser/password_manager_metrics_util.h"
 #include "components/password_manager/core/browser/password_store.h"
 
 using autofill::FormData;
@@ -276,12 +277,21 @@ class PasswordFormManager : public FormFetcher::Consumer {
   // adds itself as a consumer of the new one.
   void GrabFetcher(std::unique_ptr<FormFetcher> fetcher);
 
+  PasswordFormMetricsRecorder* metrics_recorder() {
+    return metrics_recorder_.get();
+  }
+
   // Create a copy of |*this| which can be passed to the code handling
   // save-password related UI. This omits some parts of the internal data, so
   // the result is not identical to the original.
   // TODO(crbug.com/739366): Replace with translating one appropriate class into
   // another one.
   std::unique_ptr<PasswordFormManager> Clone();
+
+  // Returns who created this PasswordFormManager. The Credential Management API
+  // uses a derived class of the PasswordFormManager that can indicate its
+  // origin.
+  virtual metrics_util::CredentialSourceType GetCredentialSource();
 
  protected:
   // FormFetcher::Consumer:
