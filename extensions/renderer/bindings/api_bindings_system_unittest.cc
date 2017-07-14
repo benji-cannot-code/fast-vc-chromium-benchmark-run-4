55f506860d4c62cc46ebedeaf72d0ba3afaaba7b
Devlin Cronin <rdevlin.cronin@chromium.org>
2017-07-14 04:02:22 +0000

    [Extensions Bindings] Add JS hooks for the exception handler
    There are two JS hooks we used in handling exceptions in extensions
    bindings:
    - There is an exposed "handleException" method, which handles exceptions
      encountered in running untrusted code.
    - The test API allows us to set custom handlers for uncaught exceptions
      (so we can fail a test or verify expected errors).
    
    Implement each of these in the utilities exposed to the JS bindings, and
    add unittests for the same.
    
    Bug: 653596
    Change-Id: I246308c78b0760f5176949c2b1d88da07dca337a
    Reviewed-on: https://chromium-review.googlesource.com/568692
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486667}

--- extensions/renderer/bindings/api_bindings_system_unittest.cc
+++ extensions/renderer/bindings/api_bindings_system_unittest.cc
@@ -116,6 +116,8 @@ void APIBindingsSystemTest::SetUp() {
     api_schemas_[api.name] = std::move(api_schema);
   }
 
+  binding::AddConsoleError add_console_error(base::Bind(
+      &APIBindingsSystemTest::AddConsoleError, base::Unretained(this)));
   bindings_system_ = base::MakeUnique<APIBindingsSystem>(
       base::Bind(&RunFunctionOnGlobalAndIgnoreResult),
       base::Bind(&RunFunctionOnGlobalAndReturnHandle),
@@ -124,10 +126,10 @@ void APIBindingsSystemTest::SetUp() {
       base::Bind(&APIBindingsSystemTest::OnAPIRequest, base::Unretained(this)),
       base::Bind(&APIBindingsSystemTest::OnEventListenersChanged,
                  base::Unretained(this)),
-      base::Bind(&DoNothingWithSilentRequest), binding::AddConsoleError(),
+      base::Bind(&DoNothingWithSilentRequest), add_console_error,
       APILastError(base::Bind(&APIBindingsSystemTest::GetLastErrorParent,
                               base::Unretained(this)),
-                   binding::AddConsoleError()));
+                   add_console_error));
 }
 
 void APIBindingsSystemTest::TearDown() {
