70caa45b5c956438ac207f61357d87f8abfc2ba9
Brian White <bcwhite@chromium.org>
2017-07-14 13:37:42 +0000

    Validate histogram fields before upload.
    This is temporary to try to discover the source of the memory corruption
    and possible deduce where it is happening.
    
    Bug: 736675
    Change-Id: I08f3b8cbfe1ae47b6872694300a7eb87c14fa7f0
    Reviewed-on: https://chromium-review.googlesource.com/570458
    Reviewed-by: Alexei Svitkine (slow) <asvitkine@chromium.org>
    Commit-Queue: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486749}

--- base/metrics/histogram_snapshot_manager.cc
+++ base/metrics/histogram_snapshot_manager.cc
@@ -46,11 +46,13 @@ HistogramSnapshotManager::~HistogramSnapshotManager() {
 }
 
 void HistogramSnapshotManager::PrepareDelta(HistogramBase* histogram) {
+  histogram->ValidateHistogramContents();
   PrepareSamples(histogram, histogram->SnapshotDelta());
 }
 
 void HistogramSnapshotManager::PrepareFinalDelta(
     const HistogramBase* histogram) {
+  histogram->ValidateHistogramContents();
   PrepareSamples(histogram, histogram->SnapshotFinalDelta());
 }
 
