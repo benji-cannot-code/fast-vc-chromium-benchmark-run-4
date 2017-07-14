5ade23a5098256f08cb4a55103e99bce4deb3edc
Duc Bui <ducbui@google.com>
2017-07-14 18:15:08 +0000

    Fix started_in_foreground for OnStart() in session restore.
    started_in_foreground in PageLoadMetricsObserver::OnStart() is false for
    foreground tabs in session restore.
    
    This patches fixes 2 things:
    1. Correct the in_foreground variable in MetricsWebContentsObserver constructor.
    2. Pass the correct visibility status of tabs in session_restore.cc.
    
    TBR=xiyuan@chromium.org
    BUG=740252
    
    Change-Id: Ic8ab868bc850b20070813ec659d64c60345cc160
    Reviewed-on: https://chromium-review.googlesource.com/569578
    Commit-Queue: Duc Bui <ducbui@google.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Reviewed-by: Fadi Meawad <fmeawad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486809}

--- chrome/browser/page_load_metrics/page_load_tracker.h
+++ chrome/browser/page_load_metrics/page_load_tracker.h
@@ -45,6 +45,7 @@ extern const char kAbortChainSizeNewNavigation[];
 extern const char kAbortChainSizeNoCommit[];
 extern const char kAbortChainSizeSameURL[];
 extern const char kPageLoadCompletedAfterAppBackground[];
+extern const char kPageLoadStartedInForeground[];
 
 }  // namespace internal
 
