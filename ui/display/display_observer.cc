7609d6e1133e90b1619f45c0a7af2e726ed02496
Scott Violet <sky@chromium.org>
2017-07-14 19:30:14 +0000

    chromeos: gets reusing of WindowTreeHosts working for mus
    WindowTreeHostManager may reuse a WindowTreeHost for a different
    display. This happens if all displays are removed, and then a new
    display is added. Ash now handles this by way of calling through to
    SetDisplayRoot() and SetDisplayRoot() on mus has been updated to allow
    this to happen.
    
    In addition this changes ash's DisplaySynchronizer to only notify mus
    of the current display layout after all changes have been
    processed. This is necessary as DisplayManager notifies observers while
    internally the list of displays contains both added and removed
    entries. This means when FocusSynchronizer could notify of state
    display state, which mus didn't like and caused ash to crash (hit a
    CHECK).
    
    BUG=740589
    TEST=covered by tests
    
    Change-Id: Ie0a2a05a1b592645669355f39997f9e3a5e77c3f
    Reviewed-on: https://chromium-review.googlesource.com/571399
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486845}

--- ui/display/display_observer.cc
+++ ui/display/display_observer.cc
@@ -9,6 +9,10 @@ namespace display {
 
 DisplayObserver::~DisplayObserver() {}
 
+void DisplayObserver::OnWillProcessDisplayChanges() {}
+
+void DisplayObserver::OnDidProcessDisplayChanges() {}
+
 void DisplayObserver::OnDisplayAdded(const Display& new_display) {}
 
 void DisplayObserver::OnDisplayRemoved(const Display& old_display) {}
