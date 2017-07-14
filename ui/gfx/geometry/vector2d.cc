b71579340d57139e416e322b1eb7399a8e5260c1
Justin Schuh <jschuh@chromium.org>
2017-07-14 22:12:07 +0000

    Replace Saturated*() calls with Clamp*() templates
    This replaces the calls but leaves the saturated_arithmetic headers
    where they are. Those will be moved in the next CL.
    
    Bug: 672489
    Change-Id: If901e49240b2e8d1f85155b9b395d9bb584300cf
    Reviewed-on: https://chromium-review.googlesource.com/571317
    Commit-Queue: Justin Schuh <jschuh@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486905}

--- ui/gfx/geometry/vector2d.cc
+++ ui/gfx/geometry/vector2d.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <cmath>
 
-#include "base/numerics/saturated_arithmetic.h"
+#include "base/numerics/clamped_math.h"
 #include "base/strings/stringprintf.h"
 
 namespace gfx {
@@ -17,13 +17,13 @@ bool Vector2d::IsZero() const {
 }
 
 void Vector2d::Add(const Vector2d& other) {
-  x_ = base::SaturatedAddition(other.x_, x_);
-  y_ = base::SaturatedAddition(other.y_, y_);
+  x_ = base::ClampAdd(other.x_, x_);
+  y_ = base::ClampAdd(other.y_, y_);
 }
 
 void Vector2d::Subtract(const Vector2d& other) {
-  x_ = base::SaturatedSubtraction(x_, other.x_);
-  y_ = base::SaturatedSubtraction(y_, other.y_);
+  x_ = base::ClampSub(x_, other.x_);
+  y_ = base::ClampSub(y_, other.y_);
 }
 
 int64_t Vector2d::LengthSquared() const {
