6f2027c9a09f94d4034f1f92c54ef69826eda77b
Bernhard Bauer <bauerb@chromium.org>
2017-07-14 13:40:37 +0000

    Grab ThreadTaskRunnerHandle in JsonSanitizerAndroid before calling into Java.
    This works around an issue where in official builds of unit_tests the
    ThreadTaskRunnerHandle is null when calling into native code from Java.
    
    Bug: 739510
    Change-Id: Ide93e67076f905d03a6a9271a812e49553dcf0d8
    Reviewed-on: https://chromium-review.googlesource.com/570251
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486750}

--- components/safe_json/json_sanitizer_android.cc
+++ components/safe_json/json_sanitizer_android.cc
@@ -52,6 +52,11 @@ class JsonSanitizerAndroid : public JsonSanitizer {
   StringCallback success_callback_;
   StringCallback error_callback_;
 
+  // Runs the callbacks. Stored as a member because grabbing it when posting
+  // the callbacks (being called from Java) fails under certain circumstances
+  // (https://crbug.com/739510).
+  scoped_refptr<base::SequencedTaskRunner> task_runner_;
+
   DISALLOW_COPY_AND_ASSIGN(JsonSanitizerAndroid);
 };
 
@@ -59,7 +64,8 @@ JsonSanitizerAndroid::JsonSanitizerAndroid(
     const StringCallback& success_callback,
     const StringCallback& error_callback)
     : success_callback_(success_callback),
-      error_callback_(error_callback) {}
+      error_callback_(error_callback),
+      task_runner_(base::SequencedTaskRunnerHandle::Get()) {}
 
 void JsonSanitizerAndroid::Sanitize(const std::string& unsafe_json) {
   // The JSON parser only accepts wellformed UTF-8.
@@ -78,13 +84,11 @@ void JsonSanitizerAndroid::Sanitize(const std::string& unsafe_json) {
 }
 
 void JsonSanitizerAndroid::OnSuccess(const std::string& json) {
-  base::SequencedTaskRunnerHandle::Get()->PostTask(
-      FROM_HERE, base::Bind(success_callback_, json));
+  task_runner_->PostTask(FROM_HERE, base::Bind(success_callback_, json));
 }
 
 void JsonSanitizerAndroid::OnError(const std::string& error) {
-  base::SequencedTaskRunnerHandle::Get()->PostTask(
-      FROM_HERE, base::Bind(error_callback_, error));
+  task_runner_->PostTask(FROM_HERE, base::Bind(error_callback_, error));
 }
 
 }  // namespace
@@ -112,7 +116,7 @@ void JsonSanitizer::Sanitize(const std::string& unsafe_json,
                              const StringCallback& success_callback,
                              const StringCallback& error_callback) {
   // JsonSanitizerAndroid does all its work synchronously, but posts any
-  // callbacks to the current message loop. This means it can be destroyed at
+  // callbacks to the current sequence. This means it can be destroyed at
   // the end of this method.
   JsonSanitizerAndroid sanitizer(success_callback, error_callback);
   sanitizer.Sanitize(unsafe_json);
