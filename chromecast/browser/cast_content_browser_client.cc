97d18e062aedd97cf5e6b7c72522ae55f9dba33e
Jenny Wong <jyw@chromium.org>
2017-07-14 20:36:19 +0000

    [Chromecast] Let CastAudioManager provide ALSA AudioInputStreams.
    CastAudioManager should only be used on platforms that use ALSA.
    
    BUG=internal 35197382
    TEST=internal CQ
    
    Change-Id: I259dc5a5798c5a7144641a09ae6aede18125c0a6
    Reviewed-on: https://chromium-review.googlesource.com/565625
    Commit-Queue: Jenny Wong <jyw@chromium.org>
    Reviewed-by: Alok Priyadarshi <alokp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486872}

--- chromecast/browser/cast_content_browser_client.cc
+++ chromecast/browser/cast_content_browser_client.cc
@@ -83,6 +83,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/media/cdm/cast_cdm_factory.h"
 #endif  // BUILDFLAG(IS_CAST_USING_CMA_BACKEND)
 
+#if defined(USE_ALSA)
+#include "chromecast/media/audio/cast_audio_manager_alsa.h"  // nogncheck
+#endif  // defined(USE_ALSA)
+
 namespace chromecast {
 namespace shell {
 
@@ -203,11 +207,19 @@ CastContentBrowserClient::CreateAudioManager(
   // because we already have a mixer in the audio pipeline downstream of
   // CastAudioManager.
   bool use_mixer = true;
+#if defined(USE_ALSA)
+  return base::MakeUnique<media::CastAudioManagerAlsa>(
+      base::MakeUnique<::media::AudioThreadImpl>(), audio_log_factory,
+      base::MakeUnique<media::MediaPipelineBackendFactoryImpl>(
+          media_pipeline_backend_manager()),
+      GetMediaTaskRunner(), use_mixer);
+#else
   return base::MakeUnique<media::CastAudioManager>(
       base::MakeUnique<::media::AudioThreadImpl>(), audio_log_factory,
       base::MakeUnique<media::MediaPipelineBackendFactoryImpl>(
           media_pipeline_backend_manager()),
       GetMediaTaskRunner(), use_mixer);
+#endif  // defined(USE_ALSA)
 }
 
 std::unique_ptr<::media::CdmFactory>
