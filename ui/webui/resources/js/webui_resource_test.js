beade379aea1f4639ca927159f9dc714357379e4
lukasza <lukasza@chromium.org>
2017-07-13 19:18:45 -0700

    Remove all calls to domAutomationController.setAutomationId.
    These calls are no longer needed after r485461
    (https://crrev.com/2478803003) which has made the
    domAutomationController.setAutomationId call a no-op.
    
    BUG=662543
    This CL was uploaded by git cl split.
    
    R=michaelpg@chromium.org
    
    Review-Url: https://codereview.chromium.org/2974323003
    Cr-Commit-Position: refs/heads/master@{#486638}

--- ui/webui/resources/js/webui_resource_test.js
+++ ui/webui/resources/js/webui_resource_test.js
@@ -196,7 +196,6 @@ function continueTesting(opt_asyncTestFailure) {
     endTests(cleanTestRun);
   }
   if (!done) {
-    domAutomationController.setAutomationId(1);
     domAutomationController.send('PENDING');
   }
 }
@@ -209,7 +208,6 @@ exports.runTests = runTests;
  * @param {boolean} success Indicates if the test completed successfully.
  */
 function endTests(success) {
-  domAutomationController.setAutomationId(1);
   domAutomationController.send(success ? 'SUCCESS' : 'FAILURE');
 }
 
