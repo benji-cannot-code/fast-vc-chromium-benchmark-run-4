0c048436ec635ae6b18b4f9cf1ac3ce3b859a2fa
Karan Bhatia <karandeepb@chromium.org>
2017-07-14 01:35:48 +0000

    [Extensions] Update ExtensionUserScriptLoaderTest test thread usage
    This CL removes the usage of MessageLoop and TestBrowserThread in
    ExtensionUserScriptLoaderTest, both of which should not be used directly in
    tests, and replaces them with a TestBrowserThreadBundle instance. This also
    gives (implicitly) ExtensionUserScriptLoaderTest a TaskScheduler instance.
    
    BUG=272091
    
    Change-Id: I7fc45c0d6bf30b25aa4b5029abfec013f29e4c0b
    Reviewed-on: https://chromium-review.googlesource.com/571182
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486610}

--- chrome/browser/extensions/extension_user_script_loader_unittest.cc
+++ chrome/browser/extensions/extension_user_script_loader_unittest.cc
@@ -14,21 +14,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/location.h"
+#include "base/macros.h"
 #include "base/memory/ptr_util.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
-#include "base/single_thread_task_runner.h"
 #include "base/strings/string_util.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/test/base/testing_profile.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/notification_service.h"
-#include "content/public/test/test_browser_thread.h"
+#include "content/public/test/test_browser_thread_bundle.h"
 #include "extensions/common/host_id.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-using content::BrowserThread;
 using extensions::URLPatternSet;
 
 namespace {
@@ -43,7 +41,6 @@ namespace extensions {
 
 // Test bringing up a script loader on a specific directory, putting a script
 // in there, etc.
-
 class ExtensionUserScriptLoaderTest : public testing::Test,
                                       public content::NotificationObserver {
  public:
@@ -56,18 +53,6 @@ class ExtensionUserScriptLoaderTest : public testing::Test,
     registrar_.Add(this,
                    extensions::NOTIFICATION_USER_SCRIPTS_UPDATED,
                    content::NotificationService::AllSources());
-
-    // ExtensionUserScriptLoader posts tasks to the file thread so make the
-    // current thread look like one.
-    file_thread_.reset(new content::TestBrowserThread(
-        BrowserThread::FILE, base::MessageLoop::current()));
-    ui_thread_.reset(new content::TestBrowserThread(
-        BrowserThread::UI, base::MessageLoop::current()));
-  }
-
-  void TearDown() override {
-    file_thread_.reset();
-    ui_thread_.reset();
   }
 
   void Observe(int type,
@@ -76,23 +61,19 @@ class ExtensionUserScriptLoaderTest : public testing::Test,
     DCHECK(type == extensions::NOTIFICATION_USER_SCRIPTS_UPDATED);
 
     shared_memory_ = content::Details<base::SharedMemory>(details).ptr();
-    if (base::MessageLoop::current() == &message_loop_)
-      base::MessageLoop::current()->QuitWhenIdle();
   }
 
   // Directory containing user scripts.
   base::ScopedTempDir temp_dir_;
 
-  content::NotificationRegistrar registrar_;
-
-  // MessageLoop used in tests.
-  base::MessageLoopForUI message_loop_;
-
-  std::unique_ptr<content::TestBrowserThread> file_thread_;
-  std::unique_ptr<content::TestBrowserThread> ui_thread_;
-
   // Updated to the script shared memory when we get notified.
   base::SharedMemory* shared_memory_;
+
+ private:
+  content::TestBrowserThreadBundle thread_bundle_;
+  content::NotificationRegistrar registrar_;
+
+  DISALLOW_COPY_AND_ASSIGN(ExtensionUserScriptLoaderTest);
 };
 
 // Test that we get notified even when there are no scripts.
