cfbb7829358d041e6b8a3ae5896894de04665c62
David Trainor <dtrainor@chromium.org>
2017-07-14 03:00:35 +0000

    Add recovery pipeline for DownloadDriver
    Add the infrastructure for a DownloadDriver recovery flow.  This CL
    doesn't actually perform any cleanup or recovery of a DownloadDriver,
    but it just returns success.  Implementing the actual recovery code will
    be included in a follow up, but this allows us to implement the
    DownloadService comprehensive recovery code without blocking on the
    implementation of this component in particular.
    
    BUG=736222
    
    Change-Id: Ibbbb8d64b5114593404339d514bafb8b7d984a9b
    Reviewed-on: https://chromium-review.googlesource.com/569073
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486654}

--- components/download/content/internal/download_driver_impl_unittest.cc
+++ components/download/content/internal/download_driver_impl_unittest.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/guid.h"
 #include "base/memory/ptr_util.h"
+#include "base/test/test_simple_task_runner.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "content/public/test/fake_download_item.h"
 #include "content/public/test/mock_download_manager.h"
 #include "net/http/http_response_headers.h"
@@ -45,6 +47,7 @@ MATCHER_P(DriverEntryEqual, entry, "") {
 class MockDriverClient : public DownloadDriver::Client {
  public:
   MOCK_METHOD1(OnDriverReady, void(bool));
+  MOCK_METHOD1(OnDriverHardRecoverComplete, void(bool));
   MOCK_METHOD1(OnDownloadCreated, void(const DriverEntry&));
   MOCK_METHOD2(OnDownloadFailed, void(const DriverEntry&, FailureType));
   MOCK_METHOD1(OnDownloadSucceeded, void(const DriverEntry&));
@@ -53,7 +56,9 @@ class MockDriverClient : public DownloadDriver::Client {
 
 class DownloadDriverImplTest : public testing::Test {
  public:
-  DownloadDriverImplTest() = default;
+  DownloadDriverImplTest()
+      : task_runner_(new base::TestSimpleTaskRunner), handle_(task_runner_) {}
+
   ~DownloadDriverImplTest() override = default;
 
   void SetUp() override {
@@ -64,6 +69,10 @@ class DownloadDriverImplTest : public testing::Test {
   NiceMock<content::MockDownloadManager> mock_manager_;
   MockDriverClient mock_client_;
   std::unique_ptr<DownloadDriverImpl> driver_;
+
+ protected:
+  scoped_refptr<base::TestSimpleTaskRunner> task_runner_;
+  base::ThreadTaskRunnerHandle handle_;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(DownloadDriverImplTest);
@@ -79,6 +88,17 @@ TEST_F(DownloadDriverImplTest, ManagerLateInitialization) {
   EXPECT_CALL(mock_client_, OnDriverReady(true));
   static_cast<content::DownloadManager::Observer*>(driver_.get())
       ->OnManagerInitialized();
+}
+
+TEST_F(DownloadDriverImplTest, TestHardRecover) {
+  EXPECT_CALL(mock_manager_, IsManagerInitialized())
+      .Times(1)
+      .WillOnce(Return(false));
+  driver_->Initialize(&mock_client_);
+
+  EXPECT_CALL(mock_client_, OnDriverHardRecoverComplete(true)).Times(1);
+  driver_->HardRecover();
+  task_runner_->RunUntilIdle();
 }
 
 // Ensures download updates from download items are propagated correctly.
--- components/download/internal/test/test_download_driver.h
+++ components/download/internal/test/test_download_driver.h
@@ -25,6 +25,7 @@ class TestDownloadDriver : public DownloadDriver {
   // Marks download driver as ready, used to test logic that depends on
   // data initialization.
   void MakeReady();
+  void TriggerHardRecoverComplete(bool success);
 
   // Adds driver entries data that will be returned
   void AddTestData(const std::vector<DriverEntry>& entries);
@@ -36,6 +37,7 @@ class TestDownloadDriver : public DownloadDriver {
 
   // DownloadDriver implementation.
   void Initialize(DownloadDriver::Client* client) override;
+  void HardRecover() override;
   bool IsReady() const override;
   void Start(
       const RequestParams& params,
