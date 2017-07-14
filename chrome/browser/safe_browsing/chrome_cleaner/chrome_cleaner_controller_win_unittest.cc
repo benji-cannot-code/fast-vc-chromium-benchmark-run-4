d48aa90e36a1fde3378cbd33a932ad0c449010d3
proberge <proberge@chromium.org>
2017-07-14 17:02:21 +0000

    Chrome Cleanup: Do not use SBER/Scout for Cleaner logs
    Now that Chrome Cleanup has its own opt-in, the SBER/Scout state should
    not be passed to the Cleaner's command-line to make sure it isn't
    accidentally used.
    
    Bug: 690020
    Change-Id: I6f1ecb60f2a9d0bdb5c3f4bbab728621082a2879
    Reviewed-on: https://chromium-review.googlesource.com/570349
    Reviewed-by: Chris Sharp <csharp@chromium.org>
    Commit-Queue: proberge <proberge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486784}

--- chrome/browser/safe_browsing/chrome_cleaner/chrome_cleaner_controller_win_unittest.cc
+++ chrome/browser/safe_browsing/chrome_cleaner/chrome_cleaner_controller_win_unittest.cc
@@ -85,11 +85,6 @@ enum class MetricsStatus {
   kDisabled,
 };
 
-enum class ScoutStatus {
-  kEnabled,
-  kDisabled,
-};
-
 // Simple test fixture that passes an invalid process handle back to the
 // ChromeCleanerRunner class and is intended for testing simple things like
 // command line flags that Chrome sends to the Chrome Cleaner process.
@@ -97,10 +92,8 @@ enum class ScoutStatus {
 // Parameters:
 // - metrics_status (MetricsStatus): whether Chrome metrics reporting is
 //   enabled.
-// - scout_status (ScoutStatus): whether logs upload in the Cleaner process
-//   should be enabled.
 class ChromeCleanerControllerSimpleTest
-    : public testing::TestWithParam<std::tuple<MetricsStatus, ScoutStatus>>,
+    : public testing::TestWithParam<MetricsStatus>,
       public ChromeCleanerRunnerTestDelegate,
       public ChromeCleanerControllerDelegate {
  public:
@@ -110,12 +103,9 @@ class ChromeCleanerControllerSimpleTest
   ~ChromeCleanerControllerSimpleTest() override {}
 
   void SetUp() override {
-    MetricsStatus metrics_status;
-    ScoutStatus scout_status;
-    std::tie(metrics_status, scout_status) = GetParam();
+    MetricsStatus metrics_status = GetParam();
 
     metrics_enabled_ = metrics_status == MetricsStatus::kEnabled;
-    scout_enabled_ = scout_status == ScoutStatus::kEnabled;
 
     SetChromeCleanerRunnerTestDelegateForTesting(this);
     ChromeCleanerController::ResetInstanceForTesting();
@@ -135,10 +125,6 @@ class ChromeCleanerControllerSimpleTest
     // executable succeeds.
     std::move(fetched_callback)
         .Run(base::FilePath(FILE_PATH_LITERAL("chrome_cleaner.exe")));
-  }
-
-  bool SafeBrowsingExtendedReportingScoutEnabled() override {
-    return scout_enabled_;
   }
 
   bool IsMetricsAndCrashReportingEnabled() override { return metrics_enabled_; }
@@ -175,7 +161,6 @@ class ChromeCleanerControllerSimpleTest
   base::test::ScopedFeatureList scoped_feature_list_;
 
   bool metrics_enabled_;
-  bool scout_enabled_;
   base::CommandLine command_line_;
 
   StrictMock<MockChromeCleanerControllerObserver> mock_observer_;
@@ -213,17 +198,14 @@ TEST_P(ChromeCleanerControllerSimpleTest, FlagsPassedToCleanerProcess) {
             command_line_.HasSwitch(chrome_cleaner::kUmaUserSwitch));
   EXPECT_EQ(metrics_enabled_, command_line_.HasSwitch(
                                   chrome_cleaner::kEnableCrashReportingSwitch));
-  EXPECT_EQ(scout_enabled_, command_line_.HasSwitch(
-                                chrome_cleaner::kEnableCleanerLoggingSwitch));
 
   controller->RemoveObserver(&mock_observer_);
 }
 
-INSTANTIATE_TEST_CASE_P(
-    All,
-    ChromeCleanerControllerSimpleTest,
-    Combine(Values(MetricsStatus::kDisabled, MetricsStatus::kEnabled),
-            Values(ScoutStatus::kDisabled, ScoutStatus::kEnabled)));
+INSTANTIATE_TEST_CASE_P(All,
+                        ChromeCleanerControllerSimpleTest,
+                        Values(MetricsStatus::kDisabled,
+                               MetricsStatus::kEnabled));
 
 enum class CleanerProcessStatus {
   kFetchFailure,
@@ -289,12 +271,6 @@ class ChromeCleanerControllerTest
             process_status_ != CleanerProcessStatus::kFetchFailure
                 ? base::FilePath(FILE_PATH_LITERAL("chrome_cleaner.exe"))
                 : base::FilePath()));
-  }
-
-  bool SafeBrowsingExtendedReportingScoutEnabled() override {
-    // Returning an arbitrary value since this is not being tested in this
-    // fixture.
-    return false;
   }
 
   bool IsMetricsAndCrashReportingEnabled() override {
