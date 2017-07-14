aab5804387923ca67e5027dca794558989fab39e
Jérôme Lebel <jlebel@chromium.org>
2017-07-14 23:15:47 +0000

    Fixing tab switcher access user action metrics
    The sign-in promo view in the tab switcher should use ACCESS_POINT_TAB_SWITCHER
    access point (instead of ACCESS_POINT_RECENT_TABS. Also adding histograms
    for the tab switcher (instead of using the one for recent tabs).
    
    Bug introduced with crrev.com/2885943002
    Histograms added with crrev.com/2871753002
    
    Bug: 742331
    Change-Id: I9ac3864ba692081115df7f368417a82673bf1b62
    Reviewed-on: https://chromium-review.googlesource.com/570958
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486927}

--- ios/chrome/browser/ui/authentication/signin_promo_view_mediator.mm
+++ ios/chrome/browser/ui/authentication/signin_promo_view_mediator.mm
@@ -44,6 +44,10 @@ void RecordSigninUserActionForAccessPoint(
       base::RecordAction(
           base::UserMetricsAction("Signin_Signin_FromRecentTabs"));
       break;
+    case signin_metrics::AccessPoint::ACCESS_POINT_TAB_SWITCHER:
+      base::RecordAction(
+          base::UserMetricsAction("Signin_Signin_FromTabSwitcher"));
+      break;
     default:
       NOTREACHED() << "Unexpected value for access point "
                    << static_cast<int>(access_point);
@@ -61,6 +65,10 @@ void RecordSigninDefaultUserActionForAccessPoint(
     case signin_metrics::AccessPoint::ACCESS_POINT_RECENT_TABS:
       base::RecordAction(
           base::UserMetricsAction("Signin_SigninWithDefault_FromRecentTabs"));
+      break;
+    case signin_metrics::AccessPoint::ACCESS_POINT_TAB_SWITCHER:
+      base::RecordAction(
+          base::UserMetricsAction("Signin_SigninWithDefault_FromTabSwitcher"));
       break;
     default:
       NOTREACHED() << "Unexpected value for access point "
@@ -80,6 +88,10 @@ void RecordSigninNotDefaultUserActionForAccessPoint(
       base::RecordAction(
           base::UserMetricsAction("Signin_SigninNotDefault_FromRecentTabs"));
       break;
+    case signin_metrics::AccessPoint::ACCESS_POINT_TAB_SWITCHER:
+      base::RecordAction(
+          base::UserMetricsAction("Signin_SigninNotDefault_FromTabSwitcher"));
+      break;
     default:
       NOTREACHED() << "Unexpected value for access point "
                    << static_cast<int>(access_point);
@@ -97,6 +109,10 @@ void RecordSigninNewAccountUserActionForAccessPoint(
     case signin_metrics::AccessPoint::ACCESS_POINT_RECENT_TABS:
       base::RecordAction(
           base::UserMetricsAction("Signin_SigninNewAccount_FromRecentTabs"));
+      break;
+    case signin_metrics::AccessPoint::ACCESS_POINT_TAB_SWITCHER:
+      base::RecordAction(
+          base::UserMetricsAction("Signin_SigninNewAccount_FromTabSwitcher"));
       break;
     default:
       NOTREACHED() << "Unexpected value for access point "
