fde3a295ce289017232d49559dc9e15514a53b0d
Tsuyoshi Horo <horo@chromium.org>
2017-07-14 04:02:48 +0000

    Record the result of starting service worker for a navigation hint.
    Bug: 740102
    Change-Id: I250fde1868f3c244c8db51a2fcf54d30c946f470
    Reviewed-on: https://chromium-review.googlesource.com/564958
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486668}

--- content/browser/service_worker/service_worker_metrics.h
+++ content/browser/service_worker/service_worker_metrics.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/service_worker/service_worker_database.h"
 #include "content/common/service_worker/embedded_worker.mojom.h"
 #include "content/common/service_worker/service_worker_types.h"
+#include "content/public/browser/service_worker_context.h"
 #include "content/public/common/service_worker_modes.h"
 #include "third_party/WebKit/public/platform/modules/serviceworker/WebServiceWorkerResponseError.h"
 #include "ui/base/page_transition_types.h"
@@ -395,6 +396,10 @@ class ServiceWorkerMetrics {
   // previously installed.
   // TODO(falken): Remove after this is deprecated. https://crbug.com/737044
   static void RecordUninstalledScriptImport(const GURL& url);
+
+  // Records the result of starting service worker for a navigation hint.
+  static void RecordStartServiceWorkerForNavigationHintResult(
+      StartServiceWorkerForNavigationHintResult result);
 
  private:
   DISALLOW_IMPLICIT_CONSTRUCTORS(ServiceWorkerMetrics);
