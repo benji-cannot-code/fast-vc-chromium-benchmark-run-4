9c0d714f2aad03d7fd22d42c422cddc370408dd8
Fady Samuel <fsamuel@chromium.org>
2017-07-14 18:52:36 +0000

    viz: surface_sequence_generator_unittest.cc => components/viz/common/surfaces
    I forgot to move this file in the last CL.
    
    Bug: 722935
    Change-Id: Ic7e3872fe876b23b993b9d0a3f53d998e6b43ea6
    Reviewed-on: https://chromium-review.googlesource.com/571265
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Fady Samuel <fsamuel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486820}

--- components/viz/common/surfaces/surface_sequence_generator_unittest.cc
+++ components/viz/common/surfaces/surface_sequence_generator_unittest.cc
@@ -8,18 +8,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/surfaces/surface_sequence.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-namespace cc {
+namespace viz {
 namespace {
 
-static constexpr viz::FrameSinkId kArbitraryFrameSinkId(1, 1);
+static constexpr FrameSinkId kArbitraryFrameSinkId(1, 1);
 
 TEST(SurfaceSequenceGeneratorTest, Basic) {
-  viz::SurfaceSequenceGenerator generator;
+  SurfaceSequenceGenerator generator;
   generator.set_frame_sink_id(kArbitraryFrameSinkId);
-  viz::SurfaceSequence sequence1 = generator.CreateSurfaceSequence();
-  viz::SurfaceSequence sequence2 = generator.CreateSurfaceSequence();
+  SurfaceSequence sequence1 = generator.CreateSurfaceSequence();
+  SurfaceSequence sequence2 = generator.CreateSurfaceSequence();
   EXPECT_NE(sequence1, sequence2);
 }
 
 }  // namespace
-}  // namespace cc
+}  // namespace viz
