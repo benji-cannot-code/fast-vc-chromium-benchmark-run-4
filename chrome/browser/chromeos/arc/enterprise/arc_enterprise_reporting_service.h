b6dd78fd698edd8c3e9d2712fa5837ac0b2e467c
Hidehiko Abe <hidehiko@chromium.org>
2017-07-15 12:52:40 +0000

    Migrate ArcService to BrowserContextKeyedService part 8.
    This CL migrates ArcEnterpriseReportingService.
    
    BUG=672829
    TEST=Ran try.
    
    Change-Id: Ib86168f0cf52096025366c4a52ec0030a1c09f55
    Reviewed-on: https://chromium-review.googlesource.com/571116
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486990}

--- chrome/browser/chromeos/arc/enterprise/arc_enterprise_reporting_service.h
+++ chrome/browser/chromeos/arc/enterprise/arc_enterprise_reporting_service.h
@@ -11,10 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/threading/thread_checker.h"
-#include "components/arc/arc_service.h"
 #include "components/arc/common/enterprise_reporting.mojom.h"
 #include "components/arc/instance_holder.h"
+#include "components/keyed_service/core/keyed_service.h"
 #include "mojo/public/cpp/bindings/binding.h"
+
+namespace content {
+class BrowserContext;
+}  // namespace content
 
 namespace arc {
 
@@ -22,11 +26,17 @@ class ArcBridgeService;
 
 // This class controls the ARC enterprise reporting.
 class ArcEnterpriseReportingService
-    : public ArcService,
+    : public KeyedService,
       public InstanceHolder<mojom::EnterpriseReportingInstance>::Observer,
       public mojom::EnterpriseReportingHost {
  public:
-  explicit ArcEnterpriseReportingService(ArcBridgeService* arc_bridge_service);
+  // Returns singleton instance for the given BrowserContext,
+  // or nullptr if the browser |context| is not allowed to use ARC.
+  static ArcEnterpriseReportingService* GetForBrowserContext(
+      content::BrowserContext* context);
+
+  ArcEnterpriseReportingService(content::BrowserContext* context,
+                                ArcBridgeService* arc_bridge_service);
   ~ArcEnterpriseReportingService() override;
 
   // InstanceHolder<mojom::EnterpriseReportingInstance>::Observer overrides:
@@ -37,6 +47,8 @@ class ArcEnterpriseReportingService
 
  private:
   THREAD_CHECKER(thread_checker_);
+
+  ArcBridgeService* const arc_bridge_service_;  // Owned by ArcServiceManager.
 
   mojo::Binding<mojom::EnterpriseReportingHost> binding_;
 
