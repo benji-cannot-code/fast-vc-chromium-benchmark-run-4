61e720dfcb751160c8d163660e768950184e42cd
Fady Samuel <fsamuel@chromium.org>
2017-07-15 04:11:51 +0000

    viz: SurfaceResourceHolder => components/viz/service/frame_sinks
    This CL moves SurfaceResourceHolder{Client} to
    components/viz/service/frame_sinks, removes an invalid include from cc::Surface
    to SurfaceResourceHolderClient, deletes an unused
    FakeSurfaceResourceHolderClient and updates all includes,
    forward declarations and namespaces appropriately.
    
    Bug: 722935
    Change-Id: Ib0194d17a16555bbf70899ff67b8c7bca7ec9e04
    Reviewed-on: https://chromium-review.googlesource.com/571267
    Commit-Queue: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486977}

--- cc/surfaces/surface.cc
+++ cc/surfaces/surface.cc
@@ -12,9 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/stl_util.h"
 #include "cc/output/copy_output_request.h"
+#include "cc/resources/returned_resource.h"
+#include "cc/resources/transferable_resource.h"
 #include "cc/surfaces/surface_client.h"
 #include "cc/surfaces/surface_manager.h"
-#include "cc/surfaces/surface_resource_holder_client.h"
 #include "components/viz/common/surfaces/local_surface_id_allocator.h"
 
 namespace cc {
--- components/viz/service/frame_sinks/surface_resource_holder_client.h
+++ components/viz/service/frame_sinks/surface_resource_holder_client.h
@@ -2,24 +2,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-#ifndef CC_SURFACES_SURFACE_RESOURCE_HOLDER_CLIENT_H_
-#define CC_SURFACES_SURFACE_RESOURCE_HOLDER_CLIENT_H_
+
+#ifndef COMPONENTS_VIZ_SERVICE_FRAME_SINKS_SURFACE_RESOURCE_HOLDER_CLIENT_H_
+#define COMPONENTS_VIZ_SERVICE_FRAME_SINKS_SURFACE_RESOURCE_HOLDER_CLIENT_H_
 
 #include "cc/resources/returned_resource.h"
-#include "cc/surfaces/surfaces_export.h"
+#include "components/viz/service/viz_service_export.h"
 
-namespace cc {
+namespace viz {
 
-class CC_SURFACES_EXPORT SurfaceResourceHolderClient {
+class VIZ_SERVICE_EXPORT SurfaceResourceHolderClient {
  public:
   virtual ~SurfaceResourceHolderClient() = default;
 
   // ReturnResources gets called when the display compositor is done using the
   // resources so that the client can use them.
   virtual void ReturnResources(
-      const std::vector<ReturnedResource>& resources) = 0;
+      const std::vector<cc::ReturnedResource>& resources) = 0;
 };
 
-}  // namespace cc
+}  // namespace viz
 
-#endif  // CC_SURFACES_SURFACE_RESOURCE_HOLDER_CLIENT_H_
+#endif  // COMPONENTS_VIZ_SERVICE_FRAME_SINKS_SURFACE_RESOURCE_HOLDER_CLIENT_H_
