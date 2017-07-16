85b6b1caf1cfa0f3d4b46cbf8b2ae1c8a586ac7e
dskiba <dskiba@chromium.org>
2017-07-16 10:45:45 -0700

    [tracing] Optimize StackFrameDeduplicator.
    Profiling StackFrameDeduplicator with new heap format revealed that:
    
    1. At least 60% of all insertions are hits, i.e. they insert backtraces
       that already exist.
    2. There can be as many as 500K frames.
    3. The average child count at each level is ~1.2.
    
    This CL optimizes StackFrameDeduplicator based on that data:
    
    1. Adds a hash-based backtrace lookup to cut down number of lookups
       in FrameNode::children maps.
    2. Changes FrameNode storage from std::vector to std::deque, avoiding
       costly vector reallocations.
    3. Changes FrameNode::children storage from std::map to std::flat_map.
    
    These changes improve HeapProfilerPerfTest.DeduplicateStackFrames perftest
    ~1.6x on macOS (1040ms -> 620ms), but real-world impact should be higher
    because the perftest is pessimistic.
    
    BUG=739378
    
    Review-Url: https://codereview.chromium.org/2977783002
    Cr-Commit-Position: refs/heads/master@{#487015}

--- base/trace_event/memory_usage_estimator.h
+++ base/trace_event/memory_usage_estimator.h
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/base_export.h"
+#include "base/containers/flat_map.h"
+#include "base/containers/flat_set.h"
 #include "base/containers/linked_list.h"
 #include "base/strings/string16.h"
 
@@ -150,6 +152,12 @@ size_t EstimateMemoryUsage(const std::priority_queue<T, C>& queue);
 
 template <class T, class C>
 size_t EstimateMemoryUsage(const std::stack<T, C>& stack);
+
+template <class T, class C>
+size_t EstimateMemoryUsage(const base::flat_set<T, C>& set);
+
+template <class K, class V, class C>
+size_t EstimateMemoryUsage(const base::flat_map<K, V, C>& map);
 
 // TODO(dskiba):
 //   std::forward_list
@@ -541,6 +549,20 @@ size_t EstimateMemoryUsage(const std::priority_queue<T, C>& queue) {
 template <class T, class C>
 size_t EstimateMemoryUsage(const std::stack<T, C>& stack) {
   return EstimateMemoryUsage(internal::GetUnderlyingContainer(stack));
+}
+
+// Flat containers
+
+template <class T, class C>
+size_t EstimateMemoryUsage(const base::flat_set<T, C>& set) {
+  using value_type = typename base::flat_set<T, C>::value_type;
+  return sizeof(value_type) * set.capacity() + EstimateIterableMemoryUsage(set);
+}
+
+template <class K, class V, class C>
+size_t EstimateMemoryUsage(const base::flat_map<K, V, C>& map) {
+  using value_type = typename base::flat_map<K, V, C>::value_type;
+  return sizeof(value_type) * map.capacity() + EstimateIterableMemoryUsage(map);
 }
 
 }  // namespace trace_event
