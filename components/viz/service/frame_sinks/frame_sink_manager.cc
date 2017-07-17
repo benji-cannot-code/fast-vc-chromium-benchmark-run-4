b61af8e473b5a6283540881d6c1e0bd2bd9b67cc
Fady Samuel <fsamuel@chromium.org>
2017-07-17 17:49:37 +0000

    viz: Remove SurfaceManager::GetValidFrameSinkIds
    The set of valid FrameSinkIds is a concept used for SurfaceSequences
    which are deprecated. We cannot remove SurfaceSequence code just yet
    but GetValidFrameSinkIds is a publicly exposed method that is used
    by FrameSinkManager for some DCHECKs. These DCHECKs are not really
    necessary and removing them allows us to delete a public method
    in SurfaceManager, simplifying its interface.
    
    Bug: 722935
    TBR: piman@chromium.org
    Change-Id: I220a591d26e5bcd366eaa9a24404179f7439f49c
    Reviewed-on: https://chromium-review.googlesource.com/574575
    Commit-Queue: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: Saman Sami <samans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487149}

--- components/viz/service/frame_sinks/frame_sink_manager.cc
+++ components/viz/service/frame_sinks/frame_sink_manager.cc
@@ -47,7 +47,6 @@ void FrameSinkManager::RegisterFrameSinkManagerClient(
     const FrameSinkId& frame_sink_id,
     FrameSinkManagerClient* client) {
   DCHECK(client);
-  DCHECK_EQ(surface_manager_.GetValidFrameSinkIds().count(frame_sink_id), 1u);
 
   clients_[frame_sink_id] = client;
 
@@ -60,7 +59,6 @@ void FrameSinkManager::RegisterFrameSinkManagerClient(
 
 void FrameSinkManager::UnregisterFrameSinkManagerClient(
     const FrameSinkId& frame_sink_id) {
-  DCHECK_EQ(surface_manager_.GetValidFrameSinkIds().count(frame_sink_id), 1u);
   auto client_iter = clients_.find(frame_sink_id);
   DCHECK(client_iter != clients_.end());
 
@@ -77,7 +75,6 @@ void FrameSinkManager::RegisterBeginFrameSource(
     const FrameSinkId& frame_sink_id) {
   DCHECK(source);
   DCHECK_EQ(registered_sources_.count(source), 0u);
-  DCHECK_EQ(surface_manager_.GetValidFrameSinkIds().count(frame_sink_id), 1u);
 
   registered_sources_[source] = frame_sink_id;
   RecursivelyAttachBeginFrameSource(frame_sink_id, source);
