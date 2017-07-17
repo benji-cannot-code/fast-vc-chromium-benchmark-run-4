bdf6225353d7d6c7ada7d0eb61ca428d1c22023a
Simon Que <sque@chromium.org>
2017-07-17 19:58:22 +0000

    Start reading Atrus logs from a particular timestamp
    Previously, this was only done for /var/log/messages.
    
    BUG=715263
    R=afakhry@chromium.org
    
    Change-Id: I1ece80c22d9a74f2ab28eab6c63fd3b6d5772ba7
    Reviewed-on: https://chromium-review.googlesource.com/572582
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Simon Que <sque@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487207}

--- chrome/browser/chromeos/system_logs/single_log_file_log_source.cc
+++ chrome/browser/chromeos/system_logs/single_log_file_log_source.cc
@@ -18,6 +18,8 @@ namespace system_logs {
 
 namespace {
 
+using SupportedSource = SingleLogFileLogSource::SupportedSource;
+
 constexpr char kDefaultSystemLogDirPath[] = "/var/log";
 constexpr int kMaxNumAllowedLogRotationsDuringFileRead = 3;
 
@@ -37,11 +39,11 @@ const base::Time* g_chrome_start_time_for_test = nullptr;
 base::FilePath GetLogFileSourceRelativeFilePath(
     SingleLogFileLogSource::SupportedSource source) {
   switch (source) {
-    case SingleLogFileLogSource::SupportedSource::kMessages:
+    case SupportedSource::kMessages:
       return base::FilePath("messages");
-    case SingleLogFileLogSource::SupportedSource::kUiLatest:
+    case SupportedSource::kUiLatest:
       return base::FilePath("ui/ui.LATEST");
-    case SingleLogFileLogSource::SupportedSource::kAtrusLog:
+    case SupportedSource::kAtrusLog:
       return base::FilePath("atrus.log");
   }
   NOTREACHED();
@@ -76,6 +78,20 @@ base::Time GetChromeStartTime() {
   if (g_chrome_start_time_for_test)
     return *g_chrome_start_time_for_test;
   return base::CurrentProcessInfo::CreationTime();
+}
+
+// Returns true if |source_type| is a log source that should be read starting
+// from a particular timestamp rather than from the beginning of the file.
+bool ShouldReadFromTimestampBasedOffset(SupportedSource source_type) {
+  switch (source_type) {
+    case SupportedSource::kMessages:
+    case SupportedSource::kAtrusLog:
+      return true;
+    case SupportedSource::kUiLatest:
+      return false;
+  }
+  NOTREACHED();
+  return false;
 }
 
 // Returns the file offset into |path| of the first line that starts with a
@@ -179,7 +195,7 @@ void SingleLogFileLogSource::ReadFile(size_t num_rotations_allowed,
       return;
 
     // Determine actual offset from which to start reading.
-    if (source_type_ == SupportedSource::kMessages) {
+    if (ShouldReadFromTimestampBasedOffset(source_type_)) {
       const base::Time earliest_log_time =
           GetChromeStartTime() - kLogCutoffTimeBeforeChromeStart;
 
