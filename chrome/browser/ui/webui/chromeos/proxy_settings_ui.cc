7ab0e9c6a19002621ba7462cf70025fdb648ffcf
Dave Schuyler <dschuyler@chromium.org>
2017-07-17 19:01:41 +0000

    [i18n] network strings in chromeos proxy UI
    This CL adds the internet_options_handler_strings to the i18n set when
    loading the proxy_settings_ui.
    
    Bug: 743097
    Change-Id: Ic5930f90d185a4f09545380de9f5af623aad626a
    Reviewed-on: https://chromium-review.googlesource.com/572265
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Dave Schuyler <dschuyler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487182}

--- chrome/browser/ui/webui/chromeos/proxy_settings_ui.cc
+++ chrome/browser/ui/webui/chromeos/proxy_settings_ui.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/chromeos/proxy_settings_ui.h"
 
 #include <memory>
+#include <string>
+#include <utility>
 
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
@@ -15,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/system/input_device_settings.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/options/chromeos/core_chromeos_options_handler.h"
+#include "chrome/browser/ui/webui/options/chromeos/internet_options_handler_strings.h"
 #include "chrome/browser/ui/webui/options/chromeos/proxy_handler.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/url_constants.h"
@@ -77,7 +80,6 @@ void ProxySettingsHTMLSource::StartDataRequest(
     const content::URLDataSource::GotDataCallback& callback) {
   const std::string& app_locale = g_browser_process->GetApplicationLocale();
   webui::SetLoadTimeDataDefaults(app_locale, localized_strings_.get());
-
   static const base::StringPiece html(
       ResourceBundle::GetSharedInstance().GetRawDataResource(
           IDR_PROXY_SETTINGS_HTML));
@@ -107,6 +109,7 @@ ProxySettingsUI::ProxySettingsUI(content::WebUI* web_ui)
   web_ui->AddMessageHandler(std::move(proxy_handler));
   proxy_handler_->GetLocalizedValues(localized_strings);
 
+  internet_options_strings::RegisterLocalizedStrings(localized_strings);
   bool keyboard_driven_oobe =
       system::InputDeviceSettings::Get()->ForceKeyboardDrivenUINavigation();
   localized_strings->SetString("highlightStrength",
