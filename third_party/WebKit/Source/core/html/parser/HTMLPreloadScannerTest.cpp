dcb59d2fcca857bb8818590272dde8589dcfb0bc
Daniel Bratell <bratell@opera.com>
2017-07-17 21:02:45 +0000

    Renaming unit_test class MockHTMLResourcePreloader to not collide.
    CSSPreloadScannerTest and HTMLPreloadScannerTest
    both use a local MockHTMLResourcePreloader class for
    testing. That is normally no big deal but in jumbo builds they
    can be compiled in the same translation unit and then the classes
    will collide. This patch gives the classes unique prefixes.
    
    R=fs@opera.com
    
    Change-Id: I29cd360e919c6b708a6021b6930b3347e5ae8194
    Reviewed-on: https://chromium-review.googlesource.com/574707
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487242}

--- third_party/WebKit/Source/core/html/parser/HTMLPreloadScannerTest.cpp
+++ third_party/WebKit/Source/core/html/parser/HTMLPreloadScannerTest.cpp
@@ -67,7 +67,7 @@ struct ContextTestCase {
   bool is_image_set;
 };
 
-class MockHTMLResourcePreloader : public ResourcePreloader {
+class HTMLMockHTMLResourcePreloader : public ResourcePreloader {
  public:
   void PreloadRequestVerification(Resource::Type type,
                                   const char* url,
@@ -213,7 +213,7 @@ class HTMLPreloadScannerTest : public ::testing::Test {
   void SetUp() override { RunSetUp(kViewportEnabled); }
 
   void Test(PreloadScannerTestCase test_case) {
-    MockHTMLResourcePreloader preloader;
+    HTMLMockHTMLResourcePreloader preloader;
     KURL base_url(kParsedURLString, test_case.base_url);
     scanner_->AppendToEnd(String(test_case.input_html));
     PreloadRequestStream requests = scanner_->Scan(base_url, nullptr);
@@ -225,7 +225,7 @@ class HTMLPreloadScannerTest : public ::testing::Test {
   }
 
   void Test(HTMLPreconnectTestCase test_case) {
-    MockHTMLResourcePreloader preloader;
+    HTMLMockHTMLResourcePreloader preloader;
     KURL base_url(kParsedURLString, test_case.base_url);
     scanner_->AppendToEnd(String(test_case.input_html));
     PreloadRequestStream requests = scanner_->Scan(base_url, nullptr);
@@ -235,7 +235,7 @@ class HTMLPreloadScannerTest : public ::testing::Test {
   }
 
   void Test(ReferrerPolicyTestCase test_case) {
-    MockHTMLResourcePreloader preloader;
+    HTMLMockHTMLResourcePreloader preloader;
     KURL base_url(kParsedURLString, test_case.base_url);
     scanner_->AppendToEnd(String(test_case.input_html));
     PreloadRequestStream requests = scanner_->Scan(base_url, nullptr);
@@ -254,7 +254,7 @@ class HTMLPreloadScannerTest : public ::testing::Test {
   }
 
   void Test(NonceTestCase test_case) {
-    MockHTMLResourcePreloader preloader;
+    HTMLMockHTMLResourcePreloader preloader;
     KURL base_url(kParsedURLString, test_case.base_url);
     scanner_->AppendToEnd(String(test_case.input_html));
     PreloadRequestStream requests = scanner_->Scan(base_url, nullptr);
@@ -264,7 +264,7 @@ class HTMLPreloadScannerTest : public ::testing::Test {
   }
 
   void Test(ContextTestCase test_case) {
-    MockHTMLResourcePreloader preloader;
+    HTMLMockHTMLResourcePreloader preloader;
     KURL base_url(kParsedURLString, test_case.base_url);
     scanner_->AppendToEnd(String(test_case.input_html));
     PreloadRequestStream requests = scanner_->Scan(base_url, nullptr);
