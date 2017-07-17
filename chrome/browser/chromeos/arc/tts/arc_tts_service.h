844b376c2eafe9df6ec42af2e1b28654644c1080
Hidehiko Abe <hidehiko@chromium.org>
2017-07-17 20:27:17 +0000

    Migrate ArcService to BrowserContextKeyedService part 22.
    This CL migrates ArcTtsService.
    
    BUG=672829
    TEST=Ran try.
    
    Change-Id: Ia635f234c7bd03ce19a2ffd2f78bc20bbe9ecc19
    Reviewed-on: https://chromium-review.googlesource.com/572473
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487224}

--- chrome/browser/chromeos/arc/tts/arc_tts_service.h
+++ chrome/browser/chromeos/arc/tts/arc_tts_service.h
@@ -9,10 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/macros.h"
-#include "components/arc/arc_service.h"
 #include "components/arc/common/tts.mojom.h"
 #include "components/arc/instance_holder.h"
+#include "components/keyed_service/core/keyed_service.h"
 #include "mojo/public/cpp/bindings/binding.h"
+
+namespace content {
+class BrowserContext;
+}  // namespace content
 
 namespace arc {
 
@@ -20,11 +24,16 @@ class ArcBridgeService;
 
 // Provides text to speech services and events to Chrome OS via Android's text
 // to speech API.
-class ArcTtsService : public ArcService,
+class ArcTtsService : public KeyedService,
                       public InstanceHolder<mojom::TtsInstance>::Observer,
                       public mojom::TtsHost {
  public:
-  explicit ArcTtsService(ArcBridgeService* bridge_service);
+  // Returns singleton instance for the given BrowserContext,
+  // or nullptr if the browser |context| is not allowed to use ARC.
+  static ArcTtsService* GetForBrowserContext(content::BrowserContext* context);
+
+  ArcTtsService(content::BrowserContext* context,
+                ArcBridgeService* bridge_service);
   ~ArcTtsService() override;
 
   // InstanceHolder<mojom::TtsInstance>::Observer overrides:
@@ -37,6 +46,8 @@ class ArcTtsService : public ArcService,
                   const std::string& error_msg) override;
 
  private:
+  ArcBridgeService* const arc_bridge_service_;  // Owned by ArcServiceManager.
+
   mojo::Binding<mojom::TtsHost> binding_;
 
   DISALLOW_COPY_AND_ASSIGN(ArcTtsService);
