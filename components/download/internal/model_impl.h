7a4d7e9710c06ae01c5211e9d5a5e45ee1289e16
David Trainor <dtrainor@chromium.org>
2017-07-17 18:14:39 +0000

    Add recovery method to the Model
    Add the way for the DownloadService Model to attempt a hard recovery.
    This will be used along with other component CLs to allow the
    DownloadService to attempt to reset itself.
    
    The recovery method for the Model happens via the following steps:
    1. Clears the Model of all Entries.
    2. Has the Store destroy itself.
    3. The Store attempts to re-initialize.
    4. Return the result back to the Model::Client.
    
    BUG=736222
    
    Change-Id: I4e0a256d0b241ef627d457d9c2276691d61da8ce
    Reviewed-on: https://chromium-review.googlesource.com/568712
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487160}

--- components/download/internal/model_impl.h
+++ components/download/internal/model_impl.h
@@ -29,6 +29,7 @@ class ModelImpl : public Model {
 
   // Model implementation.
   void Initialize(Client* client) override;
+  void HardRecover() override;
   void Add(const Entry& entry) override;
   void Update(const Entry& entry) override;
   void Remove(const std::string& guid) override;
@@ -40,6 +41,7 @@ class ModelImpl : public Model {
 
   void OnInitializedFinished(bool success,
                              std::unique_ptr<std::vector<Entry>> entries);
+  void OnHardRecoverFinished(bool success);
   void OnAddFinished(DownloadClient client,
                      const std::string& guid,
                      bool success);
