a422115d3d2e960b40d283505dbdd1e3cdb69bc8
Hidehiko Abe <hidehiko@chromium.org>
2017-07-17 15:32:48 +0000

    Migrate ArcService to BrowserContextKeyedService part 7.
    This CL migrates ArcDownloadsWatcherService.
    
    BUG=672829
    TEST=Ran try.
    
    Change-Id: I1004dfa57616778463f36e86c3cc46295d8c9627
    Reviewed-on: https://chromium-review.googlesource.com/571567
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487087}

--- chrome/browser/chromeos/arc/downloads_watcher/arc_downloads_watcher_service.h
+++ chrome/browser/chromeos/arc/downloads_watcher/arc_downloads_watcher_service.h
@@ -13,14 +13,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
-#include "components/arc/arc_service.h"
 #include "components/arc/common/file_system.mojom.h"
 #include "components/arc/instance_holder.h"
+#include "components/keyed_service/core/keyed_service.h"
 
 namespace base {
 class FilePath;
 class SequencedTaskRunner;
 }  // namespace base
+
+namespace content {
+class BrowserContext;
+}  // namespace content
 
 namespace arc {
 
@@ -32,10 +36,16 @@ bool HasAndroidSupportedMediaExtension(const base::FilePath& path);
 // Watches Downloads directory and registers newly created media files to
 // Android MediaProvider.
 class ArcDownloadsWatcherService
-    : public ArcService,
+    : public KeyedService,
       public InstanceHolder<mojom::FileSystemInstance>::Observer {
  public:
-  explicit ArcDownloadsWatcherService(ArcBridgeService* bridge_service);
+  // Returns singleton instance for the given BrowserContext,
+  // or nullptr if the browser |context| is not allowed to use ARC.
+  static ArcDownloadsWatcherService* GetForBrowserContext(
+      content::BrowserContext* context);
+
+  ArcDownloadsWatcherService(content::BrowserContext* context,
+                             ArcBridgeService* bridge_service);
   ~ArcDownloadsWatcherService() override;
 
   // InstanceHolder<mojom::FileSystemInstance>::Observer
@@ -49,6 +59,9 @@ class ArcDownloadsWatcherService
   void StopWatchingDownloads();
 
   void OnDownloadsChanged(const std::vector<std::string>& paths);
+
+  content::BrowserContext* const context_;
+  ArcBridgeService* const arc_bridge_service_;  // Owned by ArcServiceManager.
 
   std::unique_ptr<DownloadsWatcher> watcher_;
 
