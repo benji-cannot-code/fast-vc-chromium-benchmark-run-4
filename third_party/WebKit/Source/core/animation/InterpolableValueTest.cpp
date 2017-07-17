0d8fb7b180bf8db913f3fef0adf45f06802e9416
Daniel Bratell <bratell@opera.com>
2017-07-17 13:53:37 +0000

    Deduplicate helper class SampleInterpolation in unit tests
    Both InterpolableValueTest and InterpolationEffectTest use an
    identical class for value testing. These classes collided in jumbo
    builds where they were compiled in the same translation unit.
    
    This patch moves the class to AnimationTestHelper.h and changes its
    name to be more descriptive now when it isn't as close to where it
    is used anymore.
    
    Also changing the name of the kDuration constant that interfered
    with blink::kDuration.
    
    R=fs@opera.com
    
    Change-Id: Ic90cec2972f5464d7932b2ba91994273968e87c4
    Reviewed-on: https://chromium-review.googlesource.com/574489
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#487067}

--- third_party/WebKit/Source/core/animation/InterpolableValueTest.cpp
+++ third_party/WebKit/Source/core/animation/InterpolableValueTest.cpp
@@ -3,33 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <memory>
+#include "core/animation/AnimationTestHelper.h"
 #include "core/animation/InterpolableValue.h"
-
 #include "core/animation/LegacyStyleInterpolation.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include <memory>
 
 namespace blink {
-
-namespace {
-
-class SampleInterpolation : public LegacyStyleInterpolation {
- public:
-  static PassRefPtr<LegacyStyleInterpolation> Create(
-      std::unique_ptr<InterpolableValue> start,
-      std::unique_ptr<InterpolableValue> end) {
-    return AdoptRef(new SampleInterpolation(std::move(start), std::move(end)));
-  }
-
- private:
-  SampleInterpolation(std::unique_ptr<InterpolableValue> start,
-                      std::unique_ptr<InterpolableValue> end)
-      : LegacyStyleInterpolation(std::move(start),
-                                 std::move(end),
-                                 CSSPropertyBackgroundColor) {}
-};
-
-}  // namespace
 
 class AnimationInterpolableValueTest : public ::testing::Test {
  protected:
@@ -39,7 +19,7 @@ class AnimationInterpolableValueTest : public ::testing::Test {
   }
 
   double InterpolateNumbers(double a, double b, double progress) {
-    RefPtr<LegacyStyleInterpolation> i = SampleInterpolation::Create(
+    RefPtr<LegacyStyleInterpolation> i = SampleTestInterpolation::Create(
         InterpolableNumber::Create(a), InterpolableNumber::Create(b));
     i->Interpolate(0, progress);
     return ToInterpolableNumber(InterpolationValue(*i.Get()))->Value();
@@ -56,7 +36,7 @@ class AnimationInterpolableValueTest : public ::testing::Test {
       std::unique_ptr<InterpolableList> list_b,
       double progress) {
     RefPtr<LegacyStyleInterpolation> i =
-        SampleInterpolation::Create(std::move(list_a), std::move(list_b));
+        SampleTestInterpolation::Create(std::move(list_a), std::move(list_b));
     i->Interpolate(0, progress);
     return i;
   }
