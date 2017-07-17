5e54fc9e042c982f88d59ad51fa13e917f7553ba
Mohamed Amir Yosef <mamir@chromium.org>
2017-07-17 10:43:52 +0000

    [NTP::Push] Add API key to the subscription request.
    Bug: 735465
    Change-Id: I4810c94f4c5a0f9fd61915affd2a2fd8c7f7e29c
    Reviewed-on: https://chromium-review.googlesource.com/570638
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: vitaliii <vitaliii@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487039}

--- components/ntp_snippets/breaking_news/subscription_manager.cc
+++ components/ntp_snippets/breaking_news/subscription_manager.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/signin/core/browser/access_token_fetcher.h"
 #include "components/signin/core/browser/signin_manager_base.h"
+#include "net/base/url_util.h"
 
 namespace ntp_snippets {
 
@@ -29,6 +30,7 @@ const char kPushSubscriptionBackendParam[] = "push_subscription_backend";
 // Variation parameter for chrome-push-unsubscription backend.
 const char kPushUnsubscriptionBackendParam[] = "push_unsubscription_backend";
 
+const char kApiKeyParamName[] = "key";
 const char kAuthorizationRequestHeaderFormat[] = "Bearer %s";
 
 }  // namespace
@@ -65,6 +67,7 @@ SubscriptionManager::SubscriptionManager(
     PrefService* pref_service,
     SigninManagerBase* signin_manager,
     OAuth2TokenService* access_token_service,
+    const std::string& api_key,
     const GURL& subscribe_url,
     const GURL& unsubscribe_url)
     : url_request_context_getter_(std::move(url_request_context_getter)),
@@ -75,6 +78,7 @@ SubscriptionManager::SubscriptionManager(
           base::Bind(&SubscriptionManager::SigninStatusChanged,
                      base::Unretained(this)))),
       access_token_service_(access_token_service),
+      api_key_(api_key),
       subscribe_url_(subscribe_url),
       unsubscribe_url_(unsubscribe_url) {}
 
@@ -97,11 +101,16 @@ void SubscriptionManager::SubscribeInternal(
     const std::string& access_token) {
   SubscriptionJsonRequest::Builder builder;
   builder.SetToken(subscription_token)
-      .SetUrlRequestContextGetter(url_request_context_getter_)
-      .SetUrl(subscribe_url_);
+      .SetUrlRequestContextGetter(url_request_context_getter_);
+
   if (!access_token.empty()) {
+    builder.SetUrl(subscribe_url_);
     builder.SetAuthenticationHeader(base::StringPrintf(
         kAuthorizationRequestHeaderFormat, access_token.c_str()));
+  } else {
+    // When not providing OAuth token, we need to pass the Google API key.
+    builder.SetUrl(
+        net::AppendQueryParameter(subscribe_url_, kApiKeyParamName, api_key_));
   }
 
   request_ = builder.Build();
@@ -181,9 +190,10 @@ void SubscriptionManager::ResubscribeInternal(const std::string& old_token,
   }
 
   SubscriptionJsonRequest::Builder builder;
-  builder.SetToken(old_token)
-      .SetUrlRequestContextGetter(url_request_context_getter_)
-      .SetUrl(unsubscribe_url_);
+  builder.SetToken(old_token).SetUrlRequestContextGetter(
+      url_request_context_getter_);
+  builder.SetUrl(
+      net::AppendQueryParameter(unsubscribe_url_, kApiKeyParamName, api_key_));
 
   request_ = builder.Build();
   request_->Start(base::BindOnce(&SubscriptionManager::DidUnsubscribe,
