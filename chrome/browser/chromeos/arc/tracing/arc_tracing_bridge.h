c04d9a63c42c6217fadc6f26e042f1e2aab0ae42
Hidehiko Abe <hidehiko@chromium.org>
2017-07-17 11:29:35 +0000

    Migrate ArcService to BrowserContextKeyedService part 21.
    This CL migrates ArcTracingBridge.
    
    BUG=672829
    TEST=Ran try.
    
    Change-Id: Ibc52186e24f7e042dfc86541cecf7fc18d2dea50
    Reviewed-on: https://chromium-review.googlesource.com/572887
    Reviewed-by: Yusuke Sato (in China Mon-Thurs, may be offline) <yusukes@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487043}

--- chrome/browser/chromeos/arc/tracing/arc_tracing_bridge.h
+++ chrome/browser/chromeos/arc/tracing/arc_tracing_bridge.h
@@ -13,10 +13,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/trace_event/trace_event.h"
-#include "components/arc/arc_service.h"
 #include "components/arc/common/tracing.mojom.h"
 #include "components/arc/instance_holder.h"
+#include "components/keyed_service/core/keyed_service.h"
 #include "content/public/browser/arc_tracing_agent.h"
+
+namespace content {
+class BrowserContext;
+}  // namespace content
 
 namespace arc {
 
@@ -24,11 +28,17 @@ class ArcBridgeService;
 
 // This class provides the interface to trigger tracing in the container.
 class ArcTracingBridge
-    : public ArcService,
+    : public KeyedService,
       public content::ArcTracingAgent::Delegate,
       public InstanceHolder<mojom::TracingInstance>::Observer {
  public:
-  explicit ArcTracingBridge(ArcBridgeService* bridge_service);
+  // Returns singleton instance for the given BrowserContext,
+  // or nullptr if the browser |context| is not allowed to use ARC.
+  static ArcTracingBridge* GetForBrowserContext(
+      content::BrowserContext* context);
+
+  ArcTracingBridge(content::BrowserContext* context,
+                   ArcBridgeService* bridge_service);
   ~ArcTracingBridge() override;
 
   // InstanceHolder<mojom::TracingInstance>::Observer overrides:
@@ -45,6 +55,8 @@ class ArcTracingBridge
 
   // Callback for QueryAvailableCategories.
   void OnCategoriesReady(const std::vector<std::string>& categories);
+
+  ArcBridgeService* const arc_bridge_service_;  // Owned by ArcServiceManager.
 
   // List of available categories.
   std::vector<Category> categories_;
