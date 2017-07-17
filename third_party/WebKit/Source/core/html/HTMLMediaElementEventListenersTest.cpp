ed70c7343492eb650539c97ba786bb9ae2660e3f
Daniel Bratell <bratell@opera.com>
2017-07-17 16:57:45 +0000

    Renaming unit_test class StubLocalFrameClient to not collide.
    ElementVisibilityObserverTest, HTMLMediaElementEventListenersTest and
    VideoPainterTest all use a local StubLocalFrameClient class for
    testing. That is normally no big deal but in jumbo builds they
    can be compiled in the same translation unit and then the classes
    will collide. This patch gives the classes unique prefixes.
    
    R=fs@opera.com
    
    Change-Id: I47c7088486456e22ca776419a836f4e5d18be39c
    Reviewed-on: https://chromium-review.googlesource.com/574605
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#487122}

--- third_party/WebKit/Source/core/html/HTMLMediaElementEventListenersTest.cpp
+++ third_party/WebKit/Source/core/html/HTMLMediaElementEventListenersTest.cpp
@@ -27,9 +27,11 @@ class MockWebMediaPlayer final : public EmptyWebMediaPlayer {
   MOCK_METHOD1(SetIsEffectivelyFullscreen, void(bool));
 };
 
-class StubLocalFrameClient : public EmptyLocalFrameClient {
+class MediaStubLocalFrameClient : public EmptyLocalFrameClient {
  public:
-  static StubLocalFrameClient* Create() { return new StubLocalFrameClient; }
+  static MediaStubLocalFrameClient* Create() {
+    return new MediaStubLocalFrameClient;
+  }
 
   std::unique_ptr<WebMediaPlayer> CreateWebMediaPlayer(
       HTMLMediaElement&,
@@ -48,7 +50,7 @@ class HTMLMediaElementEventListenersTest : public ::testing::Test {
  protected:
   void SetUp() override {
     page_holder_ = DummyPageHolder::Create(IntSize(800, 600), nullptr,
-                                           StubLocalFrameClient::Create());
+                                           MediaStubLocalFrameClient::Create());
   }
 
   Document& GetDocument() { return page_holder_->GetDocument(); }
