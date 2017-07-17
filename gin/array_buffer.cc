38b17d604007269c091a9a841a7dd4a6554c2900
Eric Holk <eholk@chromium.org>
2017-07-17 18:51:36 +0000

    Reland "Implement updated array buffer allocator in gin"
    This is a reland of 714060db89bbb6dc78229cd0b582bd51df8f1bd4
    Original change's description:
    > Implement updated array buffer allocator in gin
    > 
    > Bug: chromium:720302
    > Change-Id: I127daca6ee9954774b8ff23382ba38cb23da7318
    > Reviewed-on: https://chromium-review.googlesource.com/543670
    > Commit-Queue: Eric Holk <eholk@chromium.org>
    > Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#481959}
    
    Bug: chromium:720302
    Change-Id: I880773c83d51c8ec0d3d55dfa8913e6b3a16ddec
    Reviewed-on: https://chromium-review.googlesource.com/570978
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Eric Holk <eholk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487179}

--- gin/array_buffer.cc
+++ gin/array_buffer.cc
@@ -6,9 +6,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdlib.h>
 
+#include "base/allocator/partition_allocator/page_allocator.h"
 #include "base/logging.h"
+#include "build/build_config.h"
 #include "gin/array_buffer.h"
 #include "gin/per_isolate_data.h"
+
+#if defined(OS_POSIX)
+#include <sys/mman.h>
+
+#ifndef MAP_ANONYMOUS
+#define MAP_ANONYMOUS MAP_ANON
+#endif
+#endif
 
 namespace gin {
 
@@ -31,8 +41,72 @@ void* ArrayBufferAllocator::AllocateUninitialized(size_t length) {
   return malloc(length);
 }
 
+void* ArrayBufferAllocator::Reserve(size_t length) {
+  void* const hint = nullptr;
+#if defined(OS_POSIX)
+  int const access_flag = PROT_NONE;
+  void* const ret =
+      mmap(hint, length, access_flag, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+  if (ret == MAP_FAILED) {
+    return nullptr;
+  }
+  return ret;
+#else
+  DWORD const access_flag = PAGE_NOACCESS;
+  return VirtualAlloc(hint, length, MEM_RESERVE, access_flag);
+#endif
+}
+
 void ArrayBufferAllocator::Free(void* data, size_t length) {
   free(data);
+}
+
+void ArrayBufferAllocator::Free(void* data,
+                                size_t length,
+                                AllocationMode mode) {
+  switch (mode) {
+    case AllocationMode::kNormal:
+      Free(data, length);
+      return;
+    case AllocationMode::kReservation: {
+#if defined(OS_POSIX)
+      int const ret = munmap(data, length);
+      CHECK(!ret);
+#else
+      BOOL const ret = VirtualFree(data, 0, MEM_RELEASE);
+      CHECK(ret);
+#endif
+      return;
+    }
+    default:
+      NOTREACHED();
+  }
+}
+
+void ArrayBufferAllocator::SetProtection(void* data,
+                                         size_t length,
+                                         Protection protection) {
+  switch (protection) {
+    case Protection::kNoAccess: {
+#if defined(OS_POSIX)
+      int ret = mprotect(data, length, PROT_NONE);
+      CHECK(!ret);
+#else
+      BOOL ret = VirtualFree(data, length, MEM_DECOMMIT);
+      CHECK(ret);
+#endif
+      break;
+    }
+    case Protection::kReadWrite:
+#if defined(OS_POSIX)
+      mprotect(data, length, PROT_READ | PROT_WRITE);
+#else
+      VirtualAlloc(data, length, MEM_COMMIT, PAGE_READWRITE);
+#endif
+      break;
+    default:
+      NOTREACHED();
+  }
 }
 
 ArrayBufferAllocator* ArrayBufferAllocator::SharedInstance() {
@@ -83,6 +157,9 @@ class ArrayBuffer::Private : public base::RefCounted<ArrayBuffer::Private> {
   v8::Isolate* isolate_;
   void* buffer_;
   size_t length_;
+  void* allocation_base_;
+  size_t allocation_length_;
+  v8::ArrayBuffer::Allocator::AllocationMode allocation_mode_;
 };
 
 scoped_refptr<ArrayBuffer::Private> ArrayBuffer::Private::From(
@@ -105,6 +182,14 @@ ArrayBuffer::Private::Private(v8::Isolate* isolate,
   v8::ArrayBuffer::Contents contents = array->Externalize();
   buffer_ = contents.Data();
   length_ = contents.ByteLength();
+  allocation_base_ = contents.AllocationBase();
+  allocation_length_ = contents.AllocationLength();
+  allocation_mode_ = contents.AllocationMode();
+
+  DCHECK(reinterpret_cast<uintptr_t>(allocation_base_) <=
+         reinterpret_cast<uintptr_t>(buffer_));
+  DCHECK(reinterpret_cast<uintptr_t>(buffer_) + length_ <=
+         reinterpret_cast<uintptr_t>(allocation_base_) + allocation_length_);
 
   array->SetAlignedPointerInInternalField(kWrapperInfoIndex,
                                           &g_array_buffer_wrapper_info);
@@ -116,7 +201,8 @@ ArrayBuffer::Private::Private(v8::Isolate* isolate,
 }
 
 ArrayBuffer::Private::~Private() {
-  PerIsolateData::From(isolate_)->allocator()->Free(buffer_, length_);
+  PerIsolateData::From(isolate_)->allocator()->Free(
+      allocation_base_, allocation_length_, allocation_mode_);
 }
 
 void ArrayBuffer::Private::FirstWeakCallback(
