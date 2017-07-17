5236f9396856ef7cc3ccd62784f128f121c7b055
Joe Downing <joedow@google.com>
2017-07-17 19:44:04 +0000

    Replacing BrowserThread::FILE in ChromeOS It2Me Host
    This change is part of the work to replace BrowserThread::FILE instances
    with TaskScheduler friendly constructs.
    
    In our case, we will now provide a single threaded task runner which
    allows blocking I/O to the ChromotingHostContext passed to the
    Native Message Host.  Many of the classes in Chromoting expect thread
    affinity so I did not want to use a sequenced task runner for this
    change to reduce its potential impact.
    
    Bug: 
    Change-Id: I93aa30b2fb35165200aed0f6e926583ec077e635
    Reviewed-on: https://chromium-review.googlesource.com/572340
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487200}

--- chrome/browser/extensions/api/messaging/native_message_host_chromeos.cc
+++ chrome/browser/extensions/api/messaging/native_message_host_chromeos.cc
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/macros.h"
 #include "base/single_thread_task_runner.h"
+#include "base/task_scheduler/post_task.h"
+#include "base/task_scheduler/task_traits.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/values.h"
 #include "chrome/browser/browser_process.h"
@@ -105,8 +107,8 @@ std::unique_ptr<NativeMessageHost> CreateIt2MeHost() {
               content::BrowserThread::IO),
           content::BrowserThread::GetTaskRunnerForThread(
               content::BrowserThread::UI),
-          content::BrowserThread::GetTaskRunnerForThread(
-              content::BrowserThread::FILE));
+          base::CreateSingleThreadTaskRunnerWithTraits(
+              {base::MayBlock(), base::TaskPriority::BACKGROUND}));
   std::unique_ptr<remoting::PolicyWatcher> policy_watcher =
       remoting::PolicyWatcher::Create(g_browser_process->policy_service(),
                                       context->file_task_runner());
