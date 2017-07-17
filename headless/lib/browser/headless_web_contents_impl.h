cd4e543e4bbec992d42fd1ccb3cdfd16fbcedbf4
Eric Seckler <eseckler@chromium.org>
2017-07-17 15:52:22 +0000

    [headless] Resize new WebContents to initial size when they are added.
    Bug: 696439
    Change-Id: Ib57bd3c95ce734d8a26c798f43fbfa94fcf3a67a
    Reviewed-on: https://chromium-review.googlesource.com/574531
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487095}

--- headless/lib/browser/headless_web_contents_impl.h
+++ headless/lib/browser/headless_web_contents_impl.h
@@ -30,6 +30,7 @@ class Rect;
 }
 
 namespace headless {
+class HeadlessBrowser;
 class HeadlessBrowserImpl;
 class HeadlessTabSocketImpl;
 
@@ -44,6 +45,8 @@ class HEADLESS_EXPORT HeadlessWebContentsImpl
   ~HeadlessWebContentsImpl() override;
 
   static HeadlessWebContentsImpl* From(HeadlessWebContents* web_contents);
+  static HeadlessWebContentsImpl* From(HeadlessBrowser* browser,
+                                       content::WebContents* contents);
 
   static std::unique_ptr<HeadlessWebContentsImpl> Create(
       HeadlessWebContents::Builder* builder);
