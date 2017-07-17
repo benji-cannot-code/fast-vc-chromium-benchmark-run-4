cbae3a07142d304cbe1ff333fb4f8e2d55034a06
Hector Dearman <hjd@google.com>
2017-07-17 12:28:20 +0000

    memory-infra: Fix crash in UnregisterClientProcess
    This use after free crash was caused when a client with a pending
    response unregisters (via UnregisterClientProcess) and that pending
    response is the last pending response for the current request.
    
    UnregisterClientProcess iterates over request->pending_responses
    calling OnProcessMemoryDumpResponse or OnOSMemoryDumpResponse when it
    finds a pending response which matches the client.
    OnOSMemoryDumpResponse/OnProcessMemoryDumpResponse removes the pending
    response and calls FinalizeGlobalMemoryDumpIfAllManagersReplied which
    (if there are no more pending responses) destroys the request we're
    iterating over.
    
    We had a test which attempted to test this but for subtle reasons
    it didn't actually cover this case.
    
    This CL fixes the bug by not-calling
    OnOSMemoryDumpResponse/OnProcessMemoryDumpResponse from
    UnregisterClientProcess and adds a test to cover this case.
    
    Bug: 742265
    Change-Id: I96504edf98ea1b0e530e0daf3f4adbfaa509bc15
    Reviewed-on: https://chromium-review.googlesource.com/570247
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Commit-Queue: Hector Dearman <hjd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487052}

--- services/resource_coordinator/memory_instrumentation/coordinator_impl.h
+++ services/resource_coordinator/memory_instrumentation/coordinator_impl.h
@@ -130,6 +130,8 @@ class CoordinatorImpl : public Coordinator, public mojom::Coordinator {
   void OnOSMemoryDumpResponse(mojom::ClientProcess*,
                               bool success,
                               const OSMemDumpMap&);
+  void RemovePendingResponse(mojom::ClientProcess*,
+                             QueuedMemoryDumpRequest::PendingResponse::Type);
 
   void PerformNextQueuedGlobalMemoryDump();
   void FinalizeGlobalMemoryDumpIfAllManagersReplied();
