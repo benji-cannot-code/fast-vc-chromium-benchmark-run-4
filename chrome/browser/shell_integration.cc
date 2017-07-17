682bb1a1ab9526f04ce79f9111df5388f3aa5190
Robert Liao <robliao@chromium.org>
2017-07-17 19:58:32 +0000

    Move DefaultWebClientWorker to use the COM STA Task Runner
    BUG=706512
    
    Change-Id: I51f6817dd9267ce842d77e8a85c5c2e15dacba22
    Reviewed-on: https://chromium-review.googlesource.com/574436
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487208}

--- chrome/browser/shell_integration.cc
+++ chrome/browser/shell_integration.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task_scheduler/post_task.h"
+#include "base/task_scheduler/task_traits.h"
 #include "base/threading/thread_restrictions.h"
 #include "build/build_config.h"
 #include "chrome/browser/policy/policy_path_parser.h"
@@ -181,9 +182,18 @@ DefaultWebClientWorker::GetTaskRunner() {
   // singleton.
   static scoped_refptr<base::SequencedTaskRunner>* task_runner = nullptr;
 
+  constexpr base::TaskTraits traits = {base::MayBlock()};
+
   if (!task_runner) {
+#if defined(OS_WIN)
+    // Shell integration calls like shell_integration::GetDefaultBrowser require
+    // the thread to have COM initialized.
     task_runner = new scoped_refptr<base::SequencedTaskRunner>(
-        base::CreateSequencedTaskRunnerWithTraits({base::MayBlock()}));
+        base::CreateCOMSTATaskRunnerWithTraits(traits));
+#else
+    task_runner = new scoped_refptr<base::SequencedTaskRunner>(
+        base::CreateSequencedTaskRunnerWithTraits(traits));
+#endif
   }
 
   return *task_runner;
