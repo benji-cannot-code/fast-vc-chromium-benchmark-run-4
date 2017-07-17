8f6dbe45e379601c0afe135c52a4fe2a117c1796
Daniel Bratell <bratell@opera.com>
2017-07-17 12:47:30 +0000

    Renaming unit_test class TestParam to not collide.
    HTMLMediaElementTest and MediaCustomControlsFullscreenDetectorTest
    both use a local TestParam struct/enum for testing.
    That is normally no big deal but in jumbo builds they
    can be compiled in the same translation unit and then the classes
    will collide. This patch gives the classes unique prefixes.
    
    R=fs@opera.com
    
    Change-Id: I17a4d3a25e38bc0789071fcec31b066a385d34a0
    Reviewed-on: https://chromium-review.googlesource.com/574178
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#487057}

--- third_party/WebKit/Source/core/html/HTMLMediaElementTest.cpp
+++ third_party/WebKit/Source/core/html/HTMLMediaElementTest.cpp
@@ -14,14 +14,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-enum class TestParam { kAudio, kVideo };
+enum class MediaTestParam { kAudio, kVideo };
 
-class HTMLMediaElementTest : public ::testing::TestWithParam<TestParam> {
+class HTMLMediaElementTest : public ::testing::TestWithParam<MediaTestParam> {
  protected:
   void SetUp() {
     dummy_page_holder_ = DummyPageHolder::Create();
 
-    if (GetParam() == TestParam::kAudio)
+    if (GetParam() == MediaTestParam::kAudio)
       media_ = HTMLAudioElement::Create(dummy_page_holder_->GetDocument());
     else
       media_ = HTMLVideoElement::Create(dummy_page_holder_->GetDocument());
@@ -40,10 +40,10 @@ class HTMLMediaElementTest : public ::testing::TestWithParam<TestParam> {
 
 INSTANTIATE_TEST_CASE_P(Audio,
                         HTMLMediaElementTest,
-                        ::testing::Values(TestParam::kAudio));
+                        ::testing::Values(MediaTestParam::kAudio));
 INSTANTIATE_TEST_CASE_P(Video,
                         HTMLMediaElementTest,
-                        ::testing::Values(TestParam::kVideo));
+                        ::testing::Values(MediaTestParam::kVideo));
 
 TEST_P(HTMLMediaElementTest, effectiveMediaVolume) {
   struct TestData {
