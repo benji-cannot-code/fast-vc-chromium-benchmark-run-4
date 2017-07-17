b0fc20257b5b0262201fd29860d162aeabd1012c
Zijie He <zijiehe@google.com>
2017-07-17 20:37:55 +0000

    Add GetAppOutputWithExitCode for Windows
    GetAppOutputWithExitCode() is implemented for Windows, but not exposed.
    
    Bug: 741926
    Change-Id: Ib0518a8c6b28bc0946e6e820756e264d13028312
    Reviewed-on: https://chromium-review.googlesource.com/571239
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Zijie He <zijiehe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487230}

---
+++ base/process/launch_unittest_win.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/process/launch.h"
+
+#include "base/command_line.h"
+#include "base/files/file_path.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace base {
+
+TEST(LaunchWinTest, GetAppOutputWithExitCodeShouldReturnExitCode) {
+  CommandLine cl(FilePath(FILE_PATH_LITERAL("cmd")));
+  cl.AppendArg("/c");
+  cl.AppendArg("this-is-not-an-application");
+  std::string output;
+  int exit_code;
+  ASSERT_TRUE(GetAppOutputWithExitCode(cl, &output, &exit_code));
+  ASSERT_TRUE(output.empty());
+  ASSERT_EQ(1, exit_code);
+}
+
+}  // namespace
