0327a6717da597dd485395ebca0b5f1584517d97
amberwon <amberwon@google.com>
2017-07-17 13:47:53 -0700

    Click to Open PDF: Open PDF once View PDF is clicked.
    pdf_plugin.html shows the file name and a View PDF link. When View PDF is
    clicked, the callback function sends a message. When this message is
    handled in PluginObserver, the URL for the PDF file is opened in a new
    tab. However, in guest view, when the message is handled, nothing is
    done.
    
    BUG=737787
    
    Review-Url: https://codereview.chromium.org/2972123002
    Cr-Commit-Position: refs/heads/master@{#487235}

--- chrome/browser/guest_view/web_view/chrome_web_view_permission_helper_delegate.cc
+++ chrome/browser/guest_view/web_view/chrome_web_view_permission_helper_delegate.cc
@@ -63,6 +63,7 @@ bool ChromeWebViewPermissionHelperDelegate::OnMessageReceived(
                         OnCouldNotLoadPlugin)
     IPC_MESSAGE_HANDLER(ChromeViewHostMsg_RemovePluginPlaceholderHost,
                         OnRemovePluginPlaceholderHost)
+    IPC_MESSAGE_HANDLER(ChromeViewHostMsg_OpenPDF, OnOpenPDF)
     IPC_MESSAGE_UNHANDLED(return false)
   IPC_END_MESSAGE_MAP()
 
@@ -113,6 +114,10 @@ void ChromeWebViewPermissionHelperDelegate::OnPermissionResponse(
 }
 
 #endif  // BUILDFLAG(ENABLE_PLUGINS)
+
+void ChromeWebViewPermissionHelperDelegate::OnOpenPDF(const GURL& url) {
+  // Intentionally blank since guest views should never trigger PDF downloads.
+}
 
 void ChromeWebViewPermissionHelperDelegate::CanDownload(
     const GURL& url,
