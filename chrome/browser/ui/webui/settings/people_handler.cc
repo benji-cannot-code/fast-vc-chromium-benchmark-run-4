203d5250b8e3f81c886f208975ceaaf660cb7965
pnoland <pnoland@chromium.org>
2017-07-17 13:48:51 -0700

    [sync] Prevent flash of error icon while loading syncSetup page
    Profile sync service relies on the lifetime of
    SyncSetupInProgressHandles to determine if setup is in progress.
    Currently, none of these handles are alive in the period between the
    closure of the sync confirmation dialog and the loading of the sync
    setup page. This causes profile sync service to think that setup is both
    incomplete and not in progress, which surfaces an error icon on the
    avatar.
    
    This CL causes PeopleHandler to acquire its SyncSetupInProgressHandle
    earlier if it detects that it's navigating directly to the syncSetup
    page.
    
    R=
    BUG=738789
    
    Review-Url: https://codereview.chromium.org/2971353002
    Cr-Commit-Position: refs/heads/master@{#487236}

--- chrome/browser/ui/webui/settings/people_handler.cc
+++ chrome/browser/ui/webui/settings/people_handler.cc
@@ -30,12 +30,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/sync_ui_util.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window.h"
+#include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/singleton_tabs.h"
 #include "chrome/browser/ui/user_manager.h"
 #include "chrome/browser/ui/webui/profile_helper.h"
 #include "chrome/browser/ui/webui/signin/login_ui_service.h"
 #include "chrome/browser/ui/webui/signin/login_ui_service_factory.h"
 #include "chrome/common/chrome_switches.h"
+#include "chrome/common/url_constants.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/autofill/core/common/autofill_constants.h"
 #include "components/autofill/core/common/autofill_pref_names.h"
@@ -197,6 +199,7 @@ PeopleHandler::~PeopleHandler() {
 }
 
 void PeopleHandler::RegisterMessages() {
+  InitializeSyncBlocker();
   web_ui()->RegisterMessageCallback(
       "SyncSetupDidClosePage",
       base::Bind(&PeopleHandler::OnDidClosePage, base::Unretained(this)));
@@ -626,7 +629,7 @@ void PeopleHandler::OpenSyncSetup() {
   GetLoginUIService()->SetLoginUI(this);
 
   ProfileSyncService* service = GetSyncService();
-  if (service)
+  if (service && !sync_blocker_)
     sync_blocker_ = service->GetSetupInProgressHandle();
 
   // There are several different UI flows that can bring the user here:
@@ -685,6 +688,20 @@ void PeopleHandler::OpenSyncSetup() {
   // via the "Advanced..." button or through One-Click signin (cases 4-6), or
   // they are re-enabling sync after having disabled it (case 7).
   PushSyncPrefs();
+}
+
+void PeopleHandler::InitializeSyncBlocker() {
+  if (!web_ui())
+    return;
+  WebContents* web_contents = web_ui()->GetWebContents();
+  if (web_contents) {
+    ProfileSyncService* service = GetSyncService();
+    const GURL current_url = web_contents->GetVisibleURL();
+    if (service &&
+        current_url == chrome::GetSettingsUrl(chrome::kSyncSetupSubPage)) {
+      sync_blocker_ = service->GetSetupInProgressHandle();
+    }
+  }
 }
 
 void PeopleHandler::FocusUI() {
