62c6cf408e2f9768daf12d42ced0d8675a902dca
Alexandr Ilin <alexilin@chromium.org>
2017-07-17 18:26:50 +0000

    predictors: Remove unused pointer from ResourcePrefetchPredictor
    ResourcePrefetchPredictor doesn't use LoadingStatsCollector since the CL
    https://crrev.com/2937623007 was landed.
    
    Bug: 715525
    Change-Id: Ic16227b20fe2f3dfbd2b28e662a28f9339cc816e
    Reviewed-on: https://chromium-review.googlesource.com/574606
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Commit-Queue: Alexandr Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487167}

--- chrome/browser/predictors/resource_prefetch_predictor.cc
+++ chrome/browser/predictors/resource_prefetch_predictor.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/predictors/loading_data_collector.h"
-#include "chrome/browser/predictors/loading_stats_collector.h"
 #include "chrome/browser/predictors/predictor_database.h"
 #include "chrome/browser/predictors/predictor_database_factory.h"
 #include "chrome/browser/profiles/profile.h"
@@ -194,7 +193,6 @@ ResourcePrefetchPredictor::ResourcePrefetchPredictor(
     Profile* profile)
     : profile_(profile),
       observer_(nullptr),
-      stats_collector_(nullptr),
       config_(config),
       initialization_state_(NOT_INITIALIZED),
       tables_(PredictorDatabaseFactory::GetForProfile(profile)
@@ -259,11 +257,6 @@ bool ResourcePrefetchPredictor::IsResourcePrefetchable(
 
 void ResourcePrefetchPredictor::SetObserverForTesting(TestObserver* observer) {
   observer_ = observer;
-}
-
-void ResourcePrefetchPredictor::SetStatsCollector(
-    LoadingStatsCollector* stats_collector) {
-  stats_collector_ = stats_collector;
 }
 
 void ResourcePrefetchPredictor::Shutdown() {
--- chrome/browser/predictors/resource_prefetch_predictor.h
+++ chrome/browser/predictors/resource_prefetch_predictor.h
@@ -60,7 +60,6 @@ struct LastVisitTimeCompare {
 
 class TestObserver;
 class ResourcePrefetcherManager;
-class LoadingStatsCollector;
 
 struct PreconnectPrediction {
   PreconnectPrediction();
@@ -150,9 +149,6 @@ class ResourcePrefetchPredictor : public history::HistoryServiceObserver {
   // data changes. Previously registered observer will be discarded. Call
   // this with nullptr parameter to de-register observer.
   void SetObserverForTesting(TestObserver* observer);
-
-  // Sets the |stats_collector| which is used to report histograms.
-  void SetStatsCollector(LoadingStatsCollector* stats_collector);
 
   // Returns true iff there is PrefetchData that can be used for a
   // |main_frame_url| and fills |prediction| with resources that need to be
@@ -294,7 +290,6 @@ class ResourcePrefetchPredictor : public history::HistoryServiceObserver {
 
   Profile* const profile_;
   TestObserver* observer_;
-  LoadingStatsCollector* stats_collector_;
   const LoadingPredictorConfig config_;
   InitializationState initialization_state_;
   scoped_refptr<ResourcePrefetchPredictorTables> tables_;
