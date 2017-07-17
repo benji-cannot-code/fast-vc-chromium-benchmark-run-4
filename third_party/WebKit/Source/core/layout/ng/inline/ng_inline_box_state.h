e0110ad33467a9acf634500ba679b5e3a11335ba
Koji Ishii <kojii@chromium.org>
2017-07-17 16:37:27 +0000

    [LayoutNG] Implement line height from font metrics when elements wraps
    When elements wraps, 'line-height: normal' should take used fonts of
    each line into account, but it was talking used fonts of the element
    instead. This patch implements this case using the ShapeResult from
    ShapingLineBreaker.
    
    This patch also fixes a case where ShapeResult was not set.
    
    BUG=636993
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    
    Change-Id: Idf11e7b4676ec0639aa3ffa94890619502064070
    Reviewed-on: https://chromium-review.googlesource.com/572603
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487110}

--- third_party/WebKit/Source/core/layout/ng/inline/ng_inline_box_state.h
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_inline_box_state.h
@@ -19,6 +19,7 @@ namespace blink {
 class NGInlineItem;
 struct NGInlineItemResult;
 class NGLineBoxFragmentBuilder;
+class ShapeResult;
 
 // Fragments that require the layout position/size of ancestor are packed in
 // this struct.
@@ -66,10 +67,7 @@ struct NGInlineBoxState {
 
   // Compute text metrics for a box. All text in a box share the same metrics.
   void ComputeTextMetrics(const ComputedStyle& style, FontBaseline);
-  void AccumulateUsedFonts(const NGInlineItem&,
-                           unsigned start,
-                           unsigned end,
-                           FontBaseline);
+  void AccumulateUsedFonts(const ShapeResult*, FontBaseline);
 
   // Create a box fragment for this box.
   void SetNeedsBoxFragment(bool when_empty);
