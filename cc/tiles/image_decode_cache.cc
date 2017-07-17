c0617785d466d344cf1fb9d06532ca2acf64ebdb
Khushal <khushalsagar@chromium.org>
2017-07-17 20:22:13 +0000

    cc: Record mip levels used for downscaling images in the renderer.
    Bug: 729183
    Change-Id: I4e5b3251048d57d592bb82e933d23adab146a31d
    Reviewed-on: https://chromium-review.googlesource.com/568662
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487221}

---
+++ cc/tiles/image_decode_cache.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "cc/tiles/image_decode_cache.h"
+
+#include "base/metrics/histogram_macros.h"
+
+namespace cc {
+
+void ImageDecodeCache::RecordImageMipLevelUMA(int mip_level) {
+  DCHECK_GE(mip_level, 0);
+  DCHECK_LT(mip_level, 32);
+  UMA_HISTOGRAM_EXACT_LINEAR("Renderer4.ImageDecodeMipLevel", mip_level + 1,
+                             33);
+}
+
+}  // namespace cc
