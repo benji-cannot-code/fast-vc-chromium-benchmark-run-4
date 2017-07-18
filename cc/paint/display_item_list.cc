d9425635e711f02424ae48baa433725568aeeee9
Vladimir Levin <vmpstr@chromium.org>
2017-07-18 19:03:59 +0000

    cc: Refactor discardable image gathering to reduce plumbing.
    This patch consolidates a few discardable image related work into fewer
    classes in cc. Specifically, it moves all of the generation code into
    discardable image map. It also removes display item list functionality
    and just exposes discardable image map from there, since most of those
    functions were pass-through.
    
    R=enne@chromium.org, khushalsagar@chromium.org
    
    Bug: 741775
    Change-Id: I483d4544f35356465c09f9084d9a0ec3ae0a9029
    Reviewed-on: https://chromium-review.googlesource.com/575347
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487546}

--- cc/paint/display_item_list.cc
+++ cc/paint/display_item_list.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/base/math_util.h"
 #include "cc/base/render_surface_filters.h"
 #include "cc/debug/picture_debug_util.h"
-#include "cc/paint/discardable_image_store.h"
 #include "cc/paint/solid_color_analyzer.h"
 #include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkPictureRecorder.h"
@@ -142,28 +141,7 @@ DisplayItemList::CreateTracedValue(bool include_items) const {
 }
 
 void DisplayItemList::GenerateDiscardableImagesMetadata() {
-  // This should be only called once.
-  DCHECK(image_map_.empty());
-  if (!paint_op_buffer_.HasDiscardableImages())
-    return;
-
-  gfx::Rect bounds = rtree_.GetBounds();
-  DiscardableImageMap::ScopedMetadataGenerator generator(
-      &image_map_, gfx::Size(bounds.right(), bounds.bottom()));
-  generator.image_store()->GatherDiscardableImages(&paint_op_buffer_);
-}
-
-void DisplayItemList::GetDiscardableImagesInRect(
-    const gfx::Rect& rect,
-    float contents_scale,
-    const gfx::ColorSpace& target_color_space,
-    std::vector<DrawImage>* images) {
-  image_map_.GetDiscardableImagesInRect(rect, contents_scale,
-                                        target_color_space, images);
-}
-
-gfx::Rect DisplayItemList::GetRectForImage(PaintImage::Id image_id) const {
-  return image_map_.GetRectForImage(image_id);
+  image_map_.Generate(&paint_op_buffer_, rtree_.GetBounds());
 }
 
 void DisplayItemList::Reset() {
