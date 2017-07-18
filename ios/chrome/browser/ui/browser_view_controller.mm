331b493712eb44b59e58f7fe9964605190baca1b
Mark Cogan <marq@google.com>
2017-07-18 10:40:44 +0000

    [ios] Remove unused and unneeded BVC methods.
    This CL removes -resetAllWebviews, which no longer has any call sites.
    
    This CL also removes -ensureViewCreated, which is just our implementation
    of [UIViewController loadViewIfNeeded], which has been available since
    iOS9. 
    
    Bug: 
    Change-Id: Ia2adab95a7a5726114a7e81ce784aaa4e40aa676
    Reviewed-on: https://chromium-review.googlesource.com/570019
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487434}

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -1048,7 +1048,7 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint {
     // tab if the caller is about to create one) ends up on screen completely.
     Tab* currentTab = [_model currentTab];
     // Force loading the view in case it was not loaded yet.
-    [self ensureViewCreated];
+    [self loadViewIfNeeded];
     if (_expectingForegroundTab)
       [currentTab.webController setOverlayPreviewMode:YES];
     if (currentTab)
@@ -1731,10 +1731,6 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint {
   }
 }
 
-- (void)ensureViewCreated {
-  ignore_result([self view]);
-}
-
 - (void)browserStateDestroyed {
   [self setActive:NO];
   // Reset the toolbar opacity in case it was changed for contextual search.
@@ -1935,12 +1931,8 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint {
 
 - (void)displayTab:(Tab*)tab isNewSelection:(BOOL)newSelection {
   DCHECK(tab);
-  // Ensure that self.view is loaded to avoid errors that can otherwise occur
-  // when accessing |_contentArea| below.
-  if (!_contentArea)
-    [self ensureViewCreated];
+  [self loadViewIfNeeded];
 
-  DCHECK(_contentArea);
   if (!self.inNewTabAnimation) {
     // Hide findbar.  |updateToolbar| will restore the findbar later.
     [self hideFindBarWithAnimation:NO];
@@ -4334,11 +4326,6 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint {
                   referrer:web::Referrer()
               inBackground:NO
                   appendTo:kCurrentTab];
-}
-
-- (void)resetAllWebViews {
-  [_dialogPresenter cancelAllDialogs];
-  [_model resetAllWebViews];
 }
 
 #pragma mark - Find Bar
--- ios/chrome/browser/ui/tab_switcher/tab_switcher_transition_context.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_switcher_transition_context.mm
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   transitionContextContent.initialTabID = bvc.tabModel.currentTab.tabId;
 
   if (![bvc isViewLoaded]) {
-    [bvc ensureViewCreated];
+    [bvc loadViewIfNeeded];
     [bvc.view setFrame:[[UIScreen mainScreen] bounds]];
   }
 
