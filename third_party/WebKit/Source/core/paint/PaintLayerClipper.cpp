42faa4336007ccc4ade8096be1277270a4c32975
Chris Harrelson <chrishtr@chromium.org>
2017-07-18 20:07:15 +0000

    Plumb subpixel accumulation to software ancestor-clips of software transforms.
    Bug: 740273
    Change-Id: I30bed13de22c3edece852c4bae30870e41c535c6
    Reviewed-on: https://chromium-review.googlesource.com/575547
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487572}

--- third_party/WebKit/Source/core/paint/PaintLayerClipper.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayerClipper.cpp
@@ -286,7 +286,6 @@ void PaintLayerClipper::CalculateRectsWithGeometryMapper(
     foreground_rect = ClipRect(LayoutRect(LayoutRect::InfiniteIntRect()));
   } else {
     CalculateBackgroundClipRectWithGeometryMapper(context, background_rect);
-    background_rect.Move(context.sub_pixel_accumulation);
     background_rect.Intersect(paint_dirty_rect);
 
     foreground_rect = background_rect;
@@ -470,6 +469,7 @@ void PaintLayerClipper::CalculateBackgroundClipRectWithGeometryMapper(
   }
 
   output.MoveBy(-context.root_layer->GetLayoutObject().PaintOffset());
+  output.Move(context.sub_pixel_accumulation);
 }
 
 void PaintLayerClipper::InitializeCommonClipRectState(
