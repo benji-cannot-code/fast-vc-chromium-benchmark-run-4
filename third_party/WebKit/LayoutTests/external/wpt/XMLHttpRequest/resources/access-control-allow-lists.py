d625ebeb8cc37fabd7b49c464d348698246f2fc4
Quinten Yearsley <qyearsley@google.com>
2017-07-18 23:13:57 +0000

    Import wpt@40e04eba01c2e35800a70261059495ebbde33309 (manual)
    Using wpt-import in Chromium fcfcaa9965c21f1652f8616cc51ec1caa4603961.
    
    Reason for manual import:
     1. Trying to see why wpt-update-expectations didn't update expectations
        for tests that failed on Windows in recent jobs.
     2. Also avoiding clobbering https://github.com/w3c/web-platform-tests/pull/6504
    
    No-Export: true
    Bug: 745099
    Change-Id: I48628d41aeaa7d452425330643020b1a2157b05a
    Reviewed-on: https://chromium-review.googlesource.com/576267
    Reviewed-by: Jeff Carpenter <jeffcarp@chromium.org>
    Commit-Queue: Quinten Yearsley <qyearsley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487655}

---
+++ third_party/WebKit/LayoutTests/external/wpt/XMLHttpRequest/resources/access-control-allow-lists.py
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import json
+
+def main(request, response):
+    if "origin" in request.GET:
+        response.headers.set("Access-Control-Allow-Origin", request.GET["origin"])
+    elif "origins" in request.GET:
+        for origin in request.GET["origins"].split(','):
+            response.headers.set("Access-Control-Allow-Origin", request.GET["origin"])
+
+    if "headers" in request.GET:
+        response.headers.set("Access-Control-Allow-Headers", '{'+request.GET["headers"]+'}')
+    if "methods" in request.GET:
+        response.headers.set("Access-Control-Allow-Methods", '{'+request.GET["methods"]+'}')
+
+    headers = dict(request.headers)
+
+    for header in headers:
+        headers[header] = headers[header][0]
+
+    headers["get_value"] = "" if "get_value" not in request.GET else request.GET["get_value"]
+
+    return json.dumps(headers)
