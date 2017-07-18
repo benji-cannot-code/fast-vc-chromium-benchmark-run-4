945fac645f64bc51b5e2b9daf4a27d65a561b6e3
xdai <xdai@chromium.org>
2017-07-18 11:57:56 -0700

    Cros Tablet Window management - Split Screen part II
    Wire up the SplitViewController with the overview mode. It's a following
    up CL of https://codereview.chromium.org/2960843004/.
    
    Changes in this CL:
    1. The split view mode can be initiated by dragging the window in
    overview mode to the side of the screen
    2. After one window is snapped to one side of the screen, all the other
    overview windows will display in the other side of the screen, allowing
    the user to select another window to snap. When two windows are snapped
    to both sides of the screen, the overview mode will be ended.
    3. The first snapped window will remain snapped until the user
    explicitly exit the split view mode. All the other windows (incluing
    newly created window) will be open in the other side of the screen.
    Clicking/Tapping on the overview button also open the overview windows
    in the other side of the screen.
    
    Not covered in this CL:
    1. The highlighted region showing where split will occur is not
       implemented in this CL.
    2. The split divider is not implemented in this CL.
    
    BUG=725683
    
    Review-Url: https://codereview.chromium.org/2955203002
    Cr-Commit-Position: refs/heads/master@{#487542}

--- ash/wm/overview/scoped_transform_overview_window.h
+++ ash/wm/overview/scoped_transform_overview_window.h
@@ -35,6 +35,7 @@ class Widget;
 namespace ash {
 
 class ScopedOverviewAnimationSettings;
+class WindowSelectorItem;
 
 // Manages a window, and its transient children, in the overview mode. This
 // class allows transforming the windows with a helper to determine the best
@@ -66,7 +67,8 @@ class ASH_EXPORT ScopedTransformOverviewWindow : public ui::EventHandler {
   static gfx::Transform GetTransformForRect(const gfx::Rect& src_rect,
                                             const gfx::Rect& dst_rect);
 
-  explicit ScopedTransformOverviewWindow(aura::Window* window);
+  ScopedTransformOverviewWindow(WindowSelectorItem* selector_item,
+                                aura::Window* window);
   ~ScopedTransformOverviewWindow() override;
 
   // Starts an animation sequence which will use animation settings specified by
@@ -165,6 +167,9 @@ class ASH_EXPORT ScopedTransformOverviewWindow : public ui::EventHandler {
 
   // Makes Close() execute synchronously when used in tests.
   static void SetImmediateCloseForTests();
+
+  // A weak pointer to the window selector item that owns the transform window.
+  WindowSelectorItem* selector_item_;
 
   // A weak pointer to the real window in the overview.
   aura::Window* window_;
