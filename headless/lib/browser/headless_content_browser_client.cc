c0ede0cbcf739e421d0491bc02952faaccfde367
Eric Seckler <eseckler@chromium.org>
2017-07-18 20:47:56 +0000

    [headless] Fix crashes when using storage quota APIs.
    Adds a HeadlessQuotaPermissionContext.
    
    When the ContentBrowserClient did not return a valid QPC, nullptr
    crashes were possible.
    
    Bug: 733569
    Change-Id: I082e3953580620b84802dc3330dbae3c113301c4
    Reviewed-on: https://chromium-review.googlesource.com/574545
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Reviewed-by: Michael Nordman <michaeln@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487596}

--- headless/lib/browser/headless_content_browser_client.cc
+++ headless/lib/browser/headless_content_browser_client.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "headless/lib/browser/headless_browser_impl.h"
 #include "headless/lib/browser/headless_browser_main_parts.h"
 #include "headless/lib/browser/headless_devtools_manager_delegate.h"
+#include "headless/lib/browser/headless_quota_permission_context.h"
 #include "headless/lib/headless_macros.h"
 #include "storage/browser/quota/quota_settings.h"
 #include "ui/base/resource/resource_bundle.h"
@@ -177,6 +178,11 @@ HeadlessContentBrowserClient::GetRendererServiceManifestOverlay() {
       ui::ResourceBundle::GetSharedInstance().GetRawDataResource(
           IDR_HEADLESS_RENDERER_MANIFEST_OVERLAY);
   return base::JSONReader::Read(manifest_template);
+}
+
+content::QuotaPermissionContext*
+HeadlessContentBrowserClient::CreateQuotaPermissionContext() {
+  return new HeadlessQuotaPermissionContext();
 }
 
 void HeadlessContentBrowserClient::GetQuotaSettings(
---
+++ headless/lib/browser/headless_quota_permission_context.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef HEADLESS_LIB_BROWSER_HEADLESS_QUOTA_PERMISSION_CONTEXT_H_
+#define HEADLESS_LIB_BROWSER_HEADLESS_QUOTA_PERMISSION_CONTEXT_H_
+
+#include "base/macros.h"
+#include "content/public/browser/quota_permission_context.h"
+
+namespace headless {
+
+class HeadlessQuotaPermissionContext : public content::QuotaPermissionContext {
+ public:
+  HeadlessQuotaPermissionContext();
+
+  // The callback will be dispatched on the IO thread.
+  void RequestQuotaPermission(const content::StorageQuotaParams& params,
+                              int render_process_id,
+                              const PermissionCallback& callback) override;
+
+ private:
+  ~HeadlessQuotaPermissionContext() override;
+
+  DISALLOW_COPY_AND_ASSIGN(HeadlessQuotaPermissionContext);
+};
+
+}  // namespace headless
+
+#endif  // HEADLESS_LIB_BROWSER_HEADLESS_QUOTA_PERMISSION_CONTEXT_H_
