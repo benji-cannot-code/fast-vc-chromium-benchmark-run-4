3e301a7ecccb14f711ee7f7b1ec9347c1de18bc4
Jérôme Lebel <jlebel@chromium.org>
2017-07-18 13:19:34 +0000

    Simplyfing SigninPromoViewMediator API
    Removing 3 properties for metrics, all related to the access point
    value.
    
    Bug: 661794
    Change-Id: I4f97831e1bf775d9c660d6658e6dfa3bf857260d
    Reviewed-on: https://chromium-review.googlesource.com/575137
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487452}

--- ios/chrome/browser/ui/authentication/signin_promo_view_mediator.h
+++ ios/chrome/browser/ui/authentication/signin_promo_view_mediator.h
@@ -18,14 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ios {
 class ChromeBrowserState;
 
-// Enums to choose which histograms is used to record the user actions.
-enum class SigninPromoViewHistograms {
-  // No histograms.
-  None,
-  // Histograms: MobileSignInPromo.BookmarkManager.*.
-  Bookmarks,
-};
-
 // Enums for the sign-in promo view state.
 enum class SigninPromoViewState {
   // None of the buttons has been used yet.
@@ -54,17 +46,6 @@ enum class SigninPromoViewState {
 // contains nil.
 @property(nonatomic, readonly, strong) ChromeIdentity* defaultIdentity;
 
-// Access point used to send user action metrics.
-@property(nonatomic) signin_metrics::AccessPoint accessPoint;
-
-// Preference key to count how many time the sign-in promo view is seen. The
-// value should point to static storage.
-@property(nonatomic) const char* displayedCountPreferenceKey;
-// Preference key, set to true when the sign-in promo view is seen too much. The
-// value should point to static storage.
-@property(nonatomic) const char* alreadySeenSigninViewPreferenceKey;
-// Histograms to use for the user actions.
-@property(nonatomic) ios::SigninPromoViewHistograms histograms;
 // Sign-in promo view state.
 @property(nonatomic) ios::SigninPromoViewState signinPromoViewState;
 
@@ -72,7 +53,11 @@ enum class SigninPromoViewState {
 - (instancetype)init NS_UNAVAILABLE;
 
 // Initialises with browser state.
+// * |browserState| is the current browser state.
+// * |accessPoint| only ACCESS_POINT_SETTINGS, ACCESS_POINT_BOOKMARK_MANAGER,
+// ACCESS_POINT_RECENT_TABS, ACCESS_POINT_TAB_SWITCHER are supported.
 - (instancetype)initWithBrowserState:(ios::ChromeBrowserState*)browserState
+                         accessPoint:(signin_metrics::AccessPoint)accessPoint
     NS_DESIGNATED_INITIALIZER;
 
 - (SigninPromoViewConfigurator*)createConfigurator;
