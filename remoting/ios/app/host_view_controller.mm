1e0272162c45984539b896312e33c14587442bb6
Scott Nichols <nicholss@chromium.org>
2017-07-18 00:12:27 +0000

    Working on first impl of persistent host settings.
    + adds a wrapper around defaults and makes this a singleton.
    + Creates a new settings object to save settings in.
    + Serializes the settings to and from defaults.
    
    Change-Id: I8e4aaed4c38fd02ca8a71b410c6d441e17da72b6
    Reviewed-on: https://chromium-review.googlesource.com/574888
    Commit-Queue: Scott Nichols <nicholss@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487308}

--- remoting/ios/app/host_view_controller.mm
+++ remoting/ios/app/host_view_controller.mm
@@ -17,7 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "remoting/ios/client_gestures.h"
 #import "remoting/ios/client_keyboard.h"
 #import "remoting/ios/display/eagl_view.h"
+#import "remoting/ios/domain/host_info.h"
+#import "remoting/ios/domain/host_settings.h"
 #import "remoting/ios/mdc/MDCActionImageView.h"
+#import "remoting/ios/persistence/remoting_preferences.h"
 #import "remoting/ios/session/remoting_client.h"
 
 #include "base/strings/sys_string_conversions.h"
@@ -38,6 +41,7 @@ static const CGFloat kKeyboardAnimationTime = 0.3;
   ClientKeyboard* _clientKeyboard;
   CGSize _keyboardSize;
   BOOL _surfaceCreated;
+  HostSettings* _settings;
 }
 @end
 
@@ -49,6 +53,8 @@ static const CGFloat kKeyboardAnimationTime = 0.3;
     _client = client;
     _keyboardSize = CGSizeZero;
     _surfaceCreated = NO;
+    _settings =
+        [[RemotingPreferences instance] settingsForHost:client.hostInfo.hostId];
   }
   return self;
 }
@@ -82,9 +88,7 @@ static const CGFloat kKeyboardAnimationTime = 0.3;
   [_floatingButton addSubview:_actionImageView];
   [self.view addSubview:_floatingButton];
 
-  // TODO(yuweih): This should be loaded from and stored into user defaults.
-  _client.gestureInterpreter->SetInputMode(
-      remoting::GestureInterpreter::DIRECT_INPUT_MODE);
+  [self applyInputMode];
 }
 
 - (void)viewDidUnload {
@@ -135,6 +139,9 @@ static const CGFloat kKeyboardAnimationTime = 0.3;
 
 - (void)viewWillDisappear:(BOOL)animated {
   [super viewWillDisappear:animated];
+
+  [[RemotingPreferences instance] setSettings:_settings
+                                      forHost:_client.hostInfo.hostId];
 
   [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
@@ -243,15 +250,13 @@ static const CGFloat kKeyboardAnimationTime = 0.3;
 }
 
 - (void)useDirectInputMode {
-  // TODO(nicholss): Store this as a preference.
-  _client.gestureInterpreter->SetInputMode(
-      remoting::GestureInterpreter::DIRECT_INPUT_MODE);
+  _settings.inputMode = ClientInputModeDirect;
+  [self applyInputMode];
 }
 
 - (void)useTrackpadInputMode {
-  // TODO(nicholss): Store this as a preference.
-  _client.gestureInterpreter->SetInputMode(
-      remoting::GestureInterpreter::TRACKPAD_INPUT_MODE);
+  _settings.inputMode = ClientInputModeTrackpad;
+  [self applyInputMode];
 }
 
 - (void)sendCtrAltDel {
@@ -263,6 +268,19 @@ static const CGFloat kKeyboardAnimationTime = 0.3;
 }
 
 #pragma mark - Private
+
+- (void)applyInputMode {
+  switch (_settings.inputMode) {
+    case ClientInputModeTrackpad:
+      _client.gestureInterpreter->SetInputMode(
+          remoting::GestureInterpreter::TRACKPAD_INPUT_MODE);
+      break;
+    case ClientInputModeDirect:  // Fall-through.
+    default:
+      _client.gestureInterpreter->SetInputMode(
+          remoting::GestureInterpreter::DIRECT_INPUT_MODE);
+  }
+}
 
 - (void)didTap:(id)sender {
   // TODO(nicholss): The FAB is being used to launch an alert window with
@@ -299,10 +317,15 @@ static const CGFloat kKeyboardAnimationTime = 0.3;
           ? @"Trackpad Mode"
           : @"Touch Mode";
   void (^switchInputModeHandler)(UIAlertAction*) = ^(UIAlertAction*) {
-    _client.gestureInterpreter->SetInputMode(
-        currentInputMode == remoting::GestureInterpreter::DIRECT_INPUT_MODE
-            ? remoting::GestureInterpreter::TRACKPAD_INPUT_MODE
-            : remoting::GestureInterpreter::DIRECT_INPUT_MODE);
+    switch (currentInputMode) {
+      case remoting::GestureInterpreter::DIRECT_INPUT_MODE:
+        [self useTrackpadInputMode];
+        break;
+      case remoting::GestureInterpreter::TRACKPAD_INPUT_MODE:  // Fall-through.
+      default:
+        [self useDirectInputMode];
+        break;
+    }
     [_actionImageView setActive:NO animated:YES];
   };
   [alert addAction:[UIAlertAction actionWithTitle:switchInputModeTitle
---
+++ remoting/ios/persistence/remoting_preferences.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_IOS_PERSISTENCE_REMOTING_PREFERENCES_H_
+#define REMOTING_IOS_PERSISTENCE_REMOTING_PREFERENCES_H_
+
+#import <Foundation/Foundation.h>
+
+@class HostSettings;
+
+// |RemotingPreferences| is the centralized place to ask for information about
+// defaults and prefrences.
+@interface RemotingPreferences : NSObject
+
+- (HostSettings*)settingsForHost:(NSString*)hostId;
+- (void)setSettings:(HostSettings*)settings forHost:(NSString*)hostId;
+
+// Access to the singleton shared instance from this property.
+@property(nonatomic, readonly, class) RemotingPreferences* instance;
+
+@property(nonatomic) NSString* activeUserKey;
+
+@end
+
+#endif  // REMOTING_IOS_PERSISTENCE_REMOTING_PREFERENCES_H_
