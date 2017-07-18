6210cdd375945877382e92bc09f7169566bef426
Marti Wong <martiw@chromium.org>
2017-07-18 03:37:31 +0000

    Show loading spinner on bookmark panel when syncing (iOS)
    [Demo videos]
    - account with bookmarks:
    https://drive.google.com/open?id=0B1O0Z7eoZMuGeTBjVXdla0RPeFE
    - account with no bookmarks:
    https://drive.google.com/open?id=0B1O0Z7eoZMuGR0pGb2tYUHcyOWM
    
    TEST=Make sure it works when signed in user has no bookmarks or there is any sync error...
    
    Bug: 696893
    Change-Id: I30a29eed423253651ff2466943c8ecdd6974e0b0
    Reviewed-on: https://chromium-review.googlesource.com/569538
    Commit-Queue: Marti Wong <martiw@chromium.org>
    Reviewed-by: Jean-François Geyelin <jif@chromium.org>
    Reviewed-by: Eric Noyau <noyau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487374}

--- ios/chrome/browser/ui/sync/synced_sessions_bridge.mm
+++ ios/chrome/browser/ui/sync/synced_sessions_bridge.mm
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/signin/signin_manager_factory.h"
 #include "ios/chrome/browser/sync/ios_chrome_profile_sync_service_factory.h"
+#include "ios/chrome/browser/sync/sync_setup_service.h"
+#include "ios/chrome/browser/sync/sync_setup_service_factory.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -31,9 +33,11 @@ SyncedSessionsObserverBridge::SyncedSessionsObserverBridge(
           ios::SigninManagerFactory::GetForBrowserState(browserState)),
       sync_service_(
           IOSChromeProfileSyncServiceFactory::GetForBrowserState(browserState)),
+      browser_state_(browserState),
       signin_manager_observer_(this),
       first_sync_cycle_is_completed_(false) {
   signin_manager_observer_.Add(signin_manager_);
+  CheckIfFirstSyncIsCompleted();
 }
 
 SyncedSessionsObserverBridge::~SyncedSessionsObserverBridge() {}
@@ -41,15 +45,14 @@ SyncedSessionsObserverBridge::~SyncedSessionsObserverBridge() {}
 #pragma mark - SyncObserverBridge
 
 void SyncedSessionsObserverBridge::OnStateChanged(syncer::SyncService* sync) {
-  if (!signin_manager_->IsAuthenticated())
+  if (!IsSignedIn())
     first_sync_cycle_is_completed_ = false;
   [owner_ onSyncStateChanged];
 }
 
 void SyncedSessionsObserverBridge::OnSyncCycleCompleted(
     syncer::SyncService* sync) {
-  if (sync_service_->GetActiveDataTypes().Has(syncer::SESSIONS))
-    first_sync_cycle_is_completed_ = true;
+  CheckIfFirstSyncIsCompleted();
   [owner_ onSyncStateChanged];
 }
 
@@ -67,6 +70,11 @@ bool SyncedSessionsObserverBridge::IsFirstSyncCycleCompleted() {
   return first_sync_cycle_is_completed_;
 }
 
+void SyncedSessionsObserverBridge::CheckIfFirstSyncIsCompleted() {
+  first_sync_cycle_is_completed_ =
+      sync_service_->GetActiveDataTypes().Has(syncer::SESSIONS);
+}
+
 #pragma mark - SigninManagerBase::Observer
 
 void SyncedSessionsObserverBridge::GoogleSignedOut(
@@ -74,6 +82,26 @@ void SyncedSessionsObserverBridge::GoogleSignedOut(
     const std::string& username) {
   first_sync_cycle_is_completed_ = false;
   [owner_ reloadSessions];
+}
+
+#pragma mark - Signin and syncing status
+
+bool SyncedSessionsObserverBridge::IsSignedIn() {
+  return signin_manager_->IsAuthenticated();
+}
+
+bool SyncedSessionsObserverBridge::IsSyncing() {
+  if (!IsSignedIn())
+    return false;
+
+  SyncSetupService* sync_setup_service =
+      SyncSetupServiceFactory::GetForBrowserState(browser_state_);
+
+  bool sync_enabled = sync_setup_service->IsSyncEnabled();
+  bool no_sync_error = (sync_setup_service->GetSyncServiceState() ==
+                        SyncSetupService::kNoSyncServiceError);
+
+  return sync_enabled && no_sync_error && !IsFirstSyncCycleCompleted();
 }
 
 }  // namespace synced_sessions
