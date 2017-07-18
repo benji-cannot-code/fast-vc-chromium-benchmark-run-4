527a7cb6237b36a04b9b320bd9fb1ed5de950d0c
Yutaka Hirano <yhirano@chromium.org>
2017-07-18 06:51:25 +0000

    Use net::ERR_CACHE_MISS for Resource::CacheMissError
    kErrorDomainBlinkInternal is now not needed because we can use the net
    error directly.
    
    Bug: None
    Change-Id: I2a1cce8c2802a03bc56a39c899d9a4cf066fd96c
    Reviewed-on: https://chromium-review.googlesource.com/575732
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487403}

--- third_party/WebKit/Source/platform/loader/fetch/ResourceError.cpp
+++ third_party/WebKit/Source/platform/loader/fetch/ResourceError.cpp
@@ -71,9 +71,8 @@ ResourceError ResourceError::CancelledDueToAccessCheckError(
 }
 
 ResourceError ResourceError::CacheMissError(const String& failing_url) {
-  ResourceError error(kErrorDomainBlinkInternal, 0, failing_url, String());
-  error.SetIsCacheMiss(true);
-  return error;
+  return WebURLError(KURL(kParsedURLString, failing_url), false,
+                     net::ERR_CACHE_MISS);
 }
 
 ResourceError ResourceError::Copy() const {
@@ -86,9 +85,7 @@ ResourceError ResourceError::Copy() const {
   error_copy.is_cancellation_ = is_cancellation_;
   error_copy.is_access_check_ = is_access_check_;
   error_copy.is_timeout_ = is_timeout_;
-  error_copy.stale_copy_in_cache_ = stale_copy_in_cache_;
   error_copy.was_ignored_by_handler_ = was_ignored_by_handler_;
-  error_copy.is_cache_miss_ = is_cache_miss_;
   return error_copy;
 }
 
@@ -126,9 +123,6 @@ bool ResourceError::Compare(const ResourceError& a, const ResourceError& b) {
   if (a.WasIgnoredByHandler() != b.WasIgnoredByHandler())
     return false;
 
-  if (a.IsCacheMiss() != b.IsCacheMiss())
-    return false;
-
   return true;
 }
 
@@ -142,8 +136,6 @@ void ResourceError::InitializeWebURLError(WebURLError* error,
   error->unreachable_url = url;
   if (reason == net::ERR_ABORTED) {
     error->is_cancellation = true;
-  } else if (reason == net::ERR_CACHE_MISS) {
-    error->is_cache_miss = true;
   } else if (reason == net::ERR_TEMPORARILY_THROTTLED) {
     error->localized_description =
         WebString::FromASCII(kThrottledErrorDescription);
@@ -151,6 +143,11 @@ void ResourceError::InitializeWebURLError(WebURLError* error,
     error->localized_description =
         WebString::FromASCII(net::ErrorToString(reason));
   }
+}
+
+bool ResourceError::IsCacheMiss() const {
+  return domain_ == String(net::kErrorDomain) &&
+         error_code_ == net::ERR_CACHE_MISS;
 }
 
 }  // namespace blink
