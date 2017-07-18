cb72c07537cff675b9a60ddd442c5bda18b21df1
ananta <ananta@chromium.org>
2017-07-17 17:19:20 -0700

    Harmonize the find in page dialog.
    This patch changes the insets and control spacing and the match count text
    
    We space the controls in the find bar using margins to ensure that the
    distances are computed to the vector image glyphs and not to the border.
    
    Next steps would be to tackle the rest of the items in this dialog for harmony
    
    BUG=651643
    TBR=blundell
    
    Review-Url: https://codereview.chromium.org/2968713003
    Cr-Commit-Position: refs/heads/master@{#487316}

--- chrome/browser/ui/views/harmony/harmony_layout_provider.cc
+++ chrome/browser/ui/views/harmony/harmony_layout_provider.cc
@@ -19,6 +19,8 @@ gfx::Insets HarmonyLayoutProvider::GetInsetsMetric(int metric) const {
     }
     case views::INSETS_VECTOR_IMAGE_BUTTON:
       return gfx::Insets(kHarmonyLayoutUnit / 4);
+    case INSETS_TOAST:
+      return gfx::Insets(0, kHarmonyLayoutUnit);
     default:
       return ChromeLayoutProvider::GetInsetsMetric(metric);
   }
@@ -74,9 +76,9 @@ int HarmonyLayoutProvider::GetDistanceMetric(int metric) const {
       return kHarmonyLayoutUnit;
     case DISTANCE_UNRELATED_CONTROL_VERTICAL_LARGE:
       return kHarmonyLayoutUnit;
+    default:
+      return ChromeLayoutProvider::GetDistanceMetric(metric);
   }
-  NOTREACHED();
-  return 0;
 }
 
 views::GridLayout::Alignment
