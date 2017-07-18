3f62e7b1341f867b6ea4997cc3b94240377c2e72
dpapad <dpapad@chromium.org>
2017-07-18 20:19:57 +0000

    WebUI: Remove remaining usage of obsolete iron-flex-layout.html
    Also remove the deprecated files from polymer_resources.grdp since
    they are not needed anymore.
    
    Bug: 744902
    Change-Id: I35ab3bbbb2e9d42758430672aba4736393e6056d
    Reviewed-on: https://chromium-review.googlesource.com/575388
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487583}

--- third_party/polymer/v1_0/components-chromium/iron-flex-layout/classes/iron-shadow-flex-layout-extracted.js
+++
@@ -1,2 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-console.warn('This file is deprecated. Please use `iron-flex-layout/iron-flex-layout-classes.html`, and one of the specific dom-modules instead');
