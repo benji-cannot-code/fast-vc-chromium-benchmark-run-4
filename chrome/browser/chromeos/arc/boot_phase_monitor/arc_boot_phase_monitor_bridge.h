2f30f386df429c5b20eb897db0bec42ea7a635ab
Hidehiko Abe <hidehiko@chromium.org>
2017-07-18 09:35:38 +0000

    Migrate ArcService to BrowserContextKeyedService part 28.
    This CL migrates ArcBootPhaseMonitorBridge.
    
    BUG=672829
    TEST=Ran try.
    
    Change-Id: I2a6988e5867092de9b8a981a253f7b1993a9eebc
    Reviewed-on: https://chromium-review.googlesource.com/575287
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Reviewed-by: Yusuke Sato (in China Mon-Thurs, may be offline) <yusukes@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487425}

--- chrome/browser/chromeos/arc/boot_phase_monitor/arc_boot_phase_monitor_bridge.h
+++ chrome/browser/chromeos/arc/boot_phase_monitor/arc_boot_phase_monitor_bridge.h
@@ -10,11 +10,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/threading/thread_checker.h"
-#include "components/arc/arc_service.h"
 #include "components/arc/common/boot_phase_monitor.mojom.h"
 #include "components/arc/instance_holder.h"
+#include "components/keyed_service/core/keyed_service.h"
 #include "components/signin/core/account_id/account_id.h"
 #include "mojo/public/cpp/bindings/binding.h"
+
+namespace content {
+class BrowserContext;
+}  // namespace content
 
 namespace arc {
 
@@ -23,12 +27,17 @@ class ArcInstanceThrottle;
 
 // Receives boot phase notifications from ARC.
 class ArcBootPhaseMonitorBridge
-    : public ArcService,
+    : public KeyedService,
       public InstanceHolder<mojom::BootPhaseMonitorInstance>::Observer,
       public mojom::BootPhaseMonitorHost {
  public:
-  ArcBootPhaseMonitorBridge(ArcBridgeService* bridge_service,
-                            const AccountId& account_id);
+  // Returns singleton instance for the given BrowserContext,
+  // or nullptr if the browser |context| is not allowed to use ARC.
+  static ArcBootPhaseMonitorBridge* GetForBrowserContext(
+      content::BrowserContext* context);
+
+  ArcBootPhaseMonitorBridge(content::BrowserContext* context,
+                            ArcBridgeService* bridge_service);
   ~ArcBootPhaseMonitorBridge() override;
 
   // InstanceHolder<mojom::BootPhaseMonitorInstance>::Observer
@@ -41,6 +50,7 @@ class ArcBootPhaseMonitorBridge
  private:
   THREAD_CHECKER(thread_checker_);
 
+  ArcBridgeService* const arc_bridge_service_;  // Owned by ArcServiceManager.
   const AccountId account_id_;
   mojo::Binding<mojom::BootPhaseMonitorHost> binding_;
   std::unique_ptr<ArcInstanceThrottle> throttle_;
