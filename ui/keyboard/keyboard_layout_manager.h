8f724bbeed17664fa9c83f2cf419e5f335a83a26
oka@chromium.org <oka@chromium.org>
2017-07-18 07:12:23 +0000

    Rename keyboard to contents if it indicates the contents window.
    Bug: 739069
    Change-Id: I358d67e6842d5fd4dc4d51098736647107f9e113
    Reviewed-on: https://chromium-review.googlesource.com/570118
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487407}

--- ui/keyboard/keyboard_layout_manager.h
+++ ui/keyboard/keyboard_layout_manager.h
@@ -20,8 +20,7 @@ class KeyboardController;
 class KeyboardLayoutManager : public aura::LayoutManager {
  public:
   explicit KeyboardLayoutManager(KeyboardController* controller)
-      : controller_(controller), keyboard_(NULL) {
-  }
+      : controller_(controller), contents_window_(NULL) {}
 
   // Overridden from aura::LayoutManager
   void OnWindowResized() override;
@@ -35,7 +34,7 @@ class KeyboardLayoutManager : public aura::LayoutManager {
 
  private:
   KeyboardController* controller_;
-  aura::Window* keyboard_;
+  aura::Window* contents_window_;
 
   DISALLOW_COPY_AND_ASSIGN(KeyboardLayoutManager);
 };
