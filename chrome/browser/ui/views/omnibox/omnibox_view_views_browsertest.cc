73f9a2757852697be64c041077fb00cca271bb6e
Matt Giuca <mgiuca@chromium.org>
2017-07-18 04:34:44 +0000

    Views Omnibox: Fixed text being selected even after blur event.
    This regressed in r473830 (blurring the Omnibox then mouse-overing
    resulted in an arrow cursor, not an I-beam). Now it properly deselects,
    so the cursor shows as an I-beam again.
    
    Bug: 740008
    Change-Id: Ice2b4fdb21acf642bdf928c07c40beec5639274f
    Reviewed-on: https://chromium-review.googlesource.com/563143
    Commit-Queue: Matt Giuca <mgiuca@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487386}

--- chrome/browser/ui/views/omnibox/omnibox_view_views_browsertest.cc
+++ chrome/browser/ui/views/omnibox/omnibox_view_views_browsertest.cc
@@ -150,9 +150,10 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewViewsTest, SelectAllOnClick) {
   EXPECT_TRUE(ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX));
   EXPECT_TRUE(omnibox_view->IsSelectAll());
 
-  // Clicking in another view should clear focus.
+  // Clicking in another view should clear focus and the selection.
   ASSERT_NO_FATAL_FAILURE(ClickBrowserWindowCenter());
   EXPECT_FALSE(ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX));
+  EXPECT_FALSE(omnibox_view->IsSelectAll());
 
   // Clicking in the omnibox again should take focus and select all text again.
   ASSERT_NO_FATAL_FAILURE(Click(ui_controls::LEFT,
@@ -245,6 +246,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewViewsTest, SelectAllOnTap) {
   // Take the focus away from the omnibox.
   ASSERT_NO_FATAL_FAILURE(TapBrowserWindowCenter());
   EXPECT_FALSE(ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX));
+  EXPECT_FALSE(omnibox_view->IsSelectAll());
 
   // Tapping in the omnibox should take focus and select all text.
   const gfx::Rect omnibox_bounds = BrowserView::GetBrowserViewForBrowser(
@@ -254,9 +256,10 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewViewsTest, SelectAllOnTap) {
   EXPECT_TRUE(ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX));
   EXPECT_TRUE(omnibox_view->IsSelectAll());
 
-  // Tapping in another view should clear focus.
+  // Tapping in another view should clear focus and the selection.
   ASSERT_NO_FATAL_FAILURE(TapBrowserWindowCenter());
   EXPECT_FALSE(ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX));
+  EXPECT_FALSE(omnibox_view->IsSelectAll());
 
   // Tapping in the omnibox again should take focus and select all text again.
   ASSERT_NO_FATAL_FAILURE(Tap(tap_location, tap_location));
