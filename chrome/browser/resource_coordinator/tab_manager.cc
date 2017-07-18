cd109372425a676f50dd767cc0e56351079969f2
Scott Haseley <shaseley@google.com>
2017-07-18 00:15:06 +0000

    [Tab Metrics] Adding TabManagerStatsCollector to TabManager
    This CL creates a separate class that is in charge of recording UMAs on behalf
    of TabManager for tab and system-related properties and events during session
    restore and background tab loading. The RecordSwitchTo metric method has been
    moved out of TabManager and into TabManagerStatsCollector.
    
    In this implementation, the TabManagerStatsCollector is scoped to the lifetime
    and owned by the TabManager, and the TabManagerStatsCollector holds a pointer
    to the TabManager.
    
    Bug: 740625
    Change-Id: Ic3c77d5d99563ceefedab3d417721a7624d02ffe
    Reviewed-on: https://chromium-review.googlesource.com/565353
    Reviewed-by: Fadi Meawad <fmeawad@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Zhen Wang <zhenw@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@google.com>
    Cr-Commit-Position: refs/heads/master@{#487311}

--- chrome/browser/resource_coordinator/tab_manager.cc
+++ chrome/browser/resource_coordinator/tab_manager.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/resource_coordinator/resource_coordinator_web_contents_observer.h"
 #include "chrome/browser/resource_coordinator/tab_manager_grc_tab_signal_observer.h"
 #include "chrome/browser/resource_coordinator/tab_manager_observer.h"
+#include "chrome/browser/resource_coordinator/tab_manager_stats_collector.h"
 #include "chrome/browser/resource_coordinator/tab_manager_web_contents_data.h"
 #include "chrome/browser/sessions/session_restore.h"
 #include "chrome/browser/ui/browser.h"
@@ -156,6 +157,7 @@ TabManager::TabManager()
   if (resource_coordinator::IsResourceCoordinatorEnabled()) {
     grc_tab_signal_observer_.reset(new GRCTabSignalObserver());
   }
+  tab_manager_stats_collector_.reset(new TabManagerStatsCollector(this));
 }
 
 TabManager::~TabManager() {
@@ -826,6 +828,11 @@ void TabManager::ActiveTabChanged(content::WebContents* old_contents,
                                   content::WebContents* new_contents,
                                   int index,
                                   int reason) {
+  // An active tab is not purged.
+  // Calling GetWebContentsData() early ensures that the WebContentsData is
+  // created for |new_contents|, which |tab_manager_stats_collector_| expects.
+  GetWebContentsData(new_contents)->set_is_purged(false);
+
   // If |old_contents| is set, that tab has switched from being active to
   // inactive, so record the time of that transition.
   if (old_contents) {
@@ -836,11 +843,8 @@ void TabManager::ActiveTabChanged(content::WebContents* old_contents,
             GetTimeToPurge(min_time_to_purge_, max_time_to_purge_));
     // Only record switch-to-tab metrics when a switch happens, i.e.
     // |old_contents| is set.
-    RecordSwitchToTab(new_contents);
+    tab_manager_stats_collector_->RecordSwitchToTab(new_contents);
   }
-
-  // An active tab is not purged.
-  GetWebContentsData(new_contents)->set_is_purged(false);
 
   // Reload |web_contents| if it is in an active browser and discarded.
   if (IsActiveWebContentsInActiveBrowser(new_contents)) {
@@ -998,14 +1002,6 @@ std::vector<TabManager::BrowserInfo> TabManager::GetBrowserInfoList() const {
   }
 
   return browser_info_list;
-}
-
-void TabManager::RecordSwitchToTab(content::WebContents* contents) const {
-  if (is_session_restore_loading_tabs_) {
-    UMA_HISTOGRAM_ENUMERATION("TabManager.SessionRestore.SwitchToTab",
-                              GetWebContentsData(contents)->tab_loading_state(),
-                              TAB_LOADING_STATE_MAX);
-  }
 }
 
 content::NavigationThrottle::ThrottleCheckResult
