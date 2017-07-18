f026dbc8424c0d946acc6abde0d0459f46682f5e
Chris Harrelson <chrishtr@chromium.org>
2017-07-18 20:16:58 +0000

    Force an intersection observation after calling observe().
    Bug: 742413
    Change-Id: I43129261e2c69d32b4e2b31a66d6d4f3bef52a36
    Reviewed-on: https://chromium-review.googlesource.com/571417
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487579}

--- third_party/WebKit/Source/core/frame/LocalFrameView.cpp
+++ third_party/WebKit/Source/core/frame/LocalFrameView.cpp
@@ -216,6 +216,7 @@ LocalFrameView::LocalFrameView(LocalFrame& frame, IntRect frame_rect)
       suppress_adjust_view_size_(false),
       allows_layout_invalidation_after_layout_clean_(true),
       forcing_layout_parent_view_(false),
+      needs_intersection_observation_(false),
       main_thread_scrolling_reasons_(0) {
   Init();
 }
@@ -4966,6 +4967,7 @@ void LocalFrameView::UpdateViewportIntersectionsForSubtree(
        child = child->Tree().NextSibling()) {
     child->View()->UpdateViewportIntersectionsForSubtree(target_state);
   }
+  needs_intersection_observation_ = false;
 }
 
 void LocalFrameView::UpdateRenderThrottlingStatusForTesting() {
@@ -5126,9 +5128,15 @@ void LocalFrameView::RecordDeferredLoadingStats() {
                                    total_screens_away));
 }
 
+void LocalFrameView::SetNeedsIntersectionObservation() {
+  needs_intersection_observation_ = true;
+  if (LocalFrameView* parent = ParentFrameView())
+    parent->SetNeedsIntersectionObservation();
+}
+
 bool LocalFrameView::ShouldThrottleRendering() const {
-  return CanThrottleRendering() && frame_->GetDocument() &&
-         Lifecycle().ThrottlingAllowed();
+  return CanThrottleRendering() && !needs_intersection_observation_ &&
+         frame_->GetDocument() && Lifecycle().ThrottlingAllowed();
 }
 
 bool LocalFrameView::CanThrottleRendering() const {
