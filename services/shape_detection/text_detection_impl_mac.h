73c4fe9a2eb493d2bf4772e548a2f741879c546b
erikchen <erikchen@chromium.org>
2017-07-18 00:12:48 +0000

    Update availability annotations for macOS 10.12 SDK [part 2].
    Due to a clang bug, adding the API_AVAILABLE annotation also changes the
    visibility to be "default". This causes an unintentional leak of
    global-visibility symbols. Explicitly re-specify "hidden" visibility to work
    around this.
    
    https://bugs.llvm.org/show_bug.cgi?id=33796
    
    Bug: 669240
    Change-Id: I1118de5ddb6035402f8b787cece33e0bb040ff15
    TBR: reillyg@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/574683
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487309}

--- services/shape_detection/text_detection_impl_mac.h
+++ services/shape_detection/text_detection_impl_mac.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_SHAPE_DETECTION_TEXT_DETECTION_IMPL_MAC_H_
 #define SERVICES_SHAPE_DETECTION_TEXT_DETECTION_IMPL_MAC_H_
 
+#include "base/mac/availability.h"
 #include "base/mac/scoped_nsobject.h"
 #include "services/shape_detection/public/interfaces/textdetection.mojom.h"
 
@@ -13,7 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace shape_detection {
 
-class TextDetectionImplMac : public mojom::TextDetection {
+// The __attribute__ visibility annotation is necessary to work around a clang
+// bug: https://bugs.llvm.org/show_bug.cgi?id=33796.
+class API_AVAILABLE(macosx(10.11))
+    __attribute__((visibility("hidden"))) TextDetectionImplMac
+    : public mojom::TextDetection {
  public:
   TextDetectionImplMac();
   ~TextDetectionImplMac() override;
--- ui/base/cocoa/touch_bar_util.h
+++ ui/base/cocoa/touch_bar_util.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Cocoa/Cocoa.h>
 
+#include "base/mac/availability.h"
 #include "ui/base/ui_base_export.h"
 
 namespace ui {
@@ -20,6 +21,15 @@ UI_BASE_EXPORT Class NSCustomTouchBarItem();
 
 // Returns a stylized blue button for the touch bar. The button performs
 // |action| from the |target|.
+// The __attribute__ visibility annotation is necessary to work around a clang
+// bug: https://bugs.llvm.org/show_bug.cgi?id=33796.
+#if defined(UI_BASE_IMPLEMENTATION) && defined(COMPONENT_BUILD)
+// UI_BASE_EXPORT specifies "default" visibility.
+API_AVAILABLE(macosx(10.12.2))
+#else
+API_AVAILABLE(macosx(10.12.2))
+__attribute__((visibility("hidden")))
+#endif
 UI_BASE_EXPORT NSButton* GetBlueTouchBarButton(NSString* title,
                                                id target,
                                                SEL action);
