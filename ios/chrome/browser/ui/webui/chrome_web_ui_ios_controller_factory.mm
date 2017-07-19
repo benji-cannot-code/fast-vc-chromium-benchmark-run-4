efdab00a5ee4390c18923808da00ffdd0e1dde51
noyau <noyau@chromium.org>
2017-07-19 07:39:52 -0700

    Removing ui-alternatives
    BUG=None
    TEST=Making sure chrome://ui-alternatives is actually gone, check chrome://flags still works.
    
    Review-Url: https://codereview.chromium.org/2970863002
    Cr-Commit-Position: refs/heads/master@{#487851}

--- ios/chrome/browser/ui/webui/chrome_web_ui_ios_controller_factory.mm
+++ ios/chrome/browser/ui/webui/chrome_web_ui_ios_controller_factory.mm
@@ -90,8 +90,6 @@ WebUIIOSFactoryFunction GetWebUIIOSFactoryFunction(WebUIIOS* web_ui,
     return &NewWebUIIOS<VersionUI>;
   if (url_host == kChromeUIFlagsHost)
     return &NewWebUIIOS<FlagsUI>;
-  if (url_host == kChromeUIAppleFlagsHost)
-    return &NewWebUIIOS<AppleFlagsUI>;
 
   return nullptr;
 }
