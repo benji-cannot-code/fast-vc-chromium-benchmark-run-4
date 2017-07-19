949d63eb7d2affb863e44b4d8361bd1c85a6d48b
Jun Cai <juncai@chromium.org>
2017-07-19 01:25:41 +0000

    Fix ChooserBubbleUi issue when browser entering/exiting fullscreen on Mac
    If the WebUSB/WebBluetooth chooser is open and the browser enters/exits
    fullscreen, the chooser will be closed, and the
    |chooser_bubble_ui_view_delegate_| in the ChooserBubbleUi class becomes
    nullptr. So ChooserBubbleUi::UpdateAnchorPosition() needs to check if
    |chooser_bubble_ui_view_delegate_| is nullptr before using it.
    
    Bug: 745304
    Change-Id: I5f460e46dbcff976dac75aeaecfe665cfbb91b71
    Reviewed-on: https://chromium-review.googlesource.com/576549
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487709}

--- chrome/browser/ui/views/permission_bubble/chooser_bubble_ui.cc
+++ chrome/browser/ui/views/permission_bubble/chooser_bubble_ui.cc
@@ -212,9 +212,10 @@ void ChooserBubbleUi::Close() {
 }
 
 void ChooserBubbleUi::UpdateAnchorPosition() {
-  DCHECK(chooser_bubble_ui_view_delegate_);
-  chooser_bubble_ui_view_delegate_->UpdateAnchor(GetAnchorView(),
-                                                 GetAnchorArrow());
+  if (chooser_bubble_ui_view_delegate_) {
+    chooser_bubble_ui_view_delegate_->UpdateAnchor(GetAnchorView(),
+                                                   GetAnchorArrow());
+  }
 }
 
 void ChooserBubbleUi::OnWidgetClosing(views::Widget* widget) {
