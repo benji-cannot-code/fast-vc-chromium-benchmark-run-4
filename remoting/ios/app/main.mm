4512b8e0deec6dca50977af90927e112ba06d7cd
Yuwei Huang <yuweih@chromium.org>
2017-07-19 22:13:26 +0000

    [CRD iOS] Adding an AppInitializer
    This CL introduces an AppInitializer class for initializing the app
    differently in different builds before launching the app.
    
    Previously we had two different main.mm for the Chromium and internal
    build. It's a pain to keep them in sync and introduce some unnecessary
    dependencies (e.g. base:i18n and ui/base) to the internal build target.
    This is especially painful when I try to add initialization code for
    i18n stuff.
    
    This refactoring should help reducing redundant code and simplify the
    dependency map of the internal build a bit.
    
    This should be landed after internal CL:
    https://chromereviews.googleplex.com/595547013/
    
    Bug: 746105
    Change-Id: I4e80ef92cb3f9f68a0f811a6e9d749c36b06b145
    Reviewed-on: https://chromium-review.googlesource.com/575260
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Scott Nichols <nicholss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488011}

--- remoting/ios/app/main.mm
+++ remoting/ios/app/main.mm
@@ -9,13 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
+#import "remoting/ios/app/app_delegate.h"
+#import "remoting/ios/app/app_initializer.h"
+
 #include "base/at_exit.h"
 #include "base/command_line.h"
 #include "base/i18n/icu_util.h"
-#import "remoting/ios/app/app_delegate.h"
-#import "remoting/ios/app/help_and_feedback.h"
-#import "remoting/ios/facade/remoting_oauth_authentication.h"
-#import "remoting/ios/facade/remoting_service.h"
 
 int main(int argc, char* argv[]) {
   // This class is designed to fulfill the dependents needs when it goes out of
@@ -35,9 +34,7 @@ int main(int argc, char* argv[]) {
 #endif
 
   @autoreleasepool {
-    RemotingService.instance.authentication =
-        [[RemotingOAuthAuthentication alloc] init];
-    HelpAndFeedback.instance = [[HelpAndFeedback alloc] init];
+    [AppInitializer initializeApp];
     return UIApplicationMain(
         argc, argv, nil, NSStringFromClass([AppDelegate class]));
   }
