3aa0b2c69cc2a68635d50cb0a4198eda4e166600
Peter K. Lee <pkl@chromium.org>
2017-07-19 01:32:31 +0000

    Removed ios/public NativeAppMetadata and *WhitelistManager protocols
    Also removed faked implementations for testing and cleaned up #include
    lines for removed files.
    
    Note that this cannot be submitted until
    https://chromereviews.googleplex.com/598167013/ is submitted.
    
    Bug: 742617
    Change-Id: Ie6702584337c1d340519762ddff60345bf750dd6
    Reviewed-on: https://chromium-review.googlesource.com/575527
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Peter Lee <pkl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487711}

--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -134,7 +134,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/net/crn_http_protocol_handler.h"
 #include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
 #include "ios/public/provider/chrome/browser/distribution/app_distribution_provider.h"
-#import "ios/public/provider/chrome/browser/native_app_launcher/native_app_whitelist_manager.h"
 #include "ios/public/provider/chrome/browser/signin/chrome_identity_service.h"
 #import "ios/public/provider/chrome/browser/user_feedback/user_feedback_provider.h"
 #import "ios/third_party/material_components_ios/src/components/Typography/src/MaterialTypography.h"
