2921544d9ea5ce89757aeb55ffe347c864dfa2cb
Scott Chen <scottchen@chromium.org>
2017-07-19 01:42:24 +0000

    MD Extension: unify the way delegates are attached to elements.
    Currently the singleton delegate instance is attached to different elements in
    4 different ways. This CL makes it so all elements get the reference to the
    delegate via extension Manager.
    
    Bug: None
    Change-Id: Ifbeae9741933cd4b8c470ac8b1373c871f7c99ba
    Reviewed-on: https://chromium-review.googlesource.com/575023
    Commit-Queue: Scott Chen <scottchen@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487714}

--- chrome/browser/resources/md_extensions/toolbar.js
+++ chrome/browser/resources/md_extensions/toolbar.js
@@ -29,26 +29,23 @@ cr.define('extensions', function() {
     behaviors: [I18nBehavior],
 
     properties: {
+      /** @type {extensions.ToolbarDelegate} */
+      delegate: Object,
+
       inDevMode: {
         type: Boolean,
         value: false,
       },
     },
 
-    /** @param {extensions.ToolbarDelegate} delegate */
-    setDelegate: function(delegate) {
-      /** @private {extensions.ToolbarDelegate} */
-      this.delegate_ = delegate;
-    },
-
     /** @private */
     onDevModeChange_: function() {
-      this.delegate_.setProfileInDevMode(this.$['dev-mode'].checked);
+      this.delegate.setProfileInDevMode(this.$['dev-mode'].checked);
     },
 
     /** @private */
     onLoadUnpackedTap_: function() {
-      this.delegate_.loadUnpacked();
+      this.delegate.loadUnpacked();
     },
 
     /** @private */
@@ -58,7 +55,7 @@ cr.define('extensions', function() {
 
     /** @private */
     onUpdateNowTap_: function() {
-      this.delegate_.updateAllExtensions();
+      this.delegate.updateAllExtensions();
     },
   });
 
--- chrome/test/data/webui/extensions/extension_toolbar_test.js
+++ chrome/test/data/webui/extensions/extension_toolbar_test.js
@@ -43,7 +43,7 @@ cr.define('extension_toolbar_tests', function() {
       setup(function() {
         toolbar = document.querySelector('extensions-manager').toolbar;
         mockDelegate = new MockDelegate();
-        toolbar.setDelegate(mockDelegate);
+        toolbar.set('delegate', mockDelegate);
       });
 
       test(assert(TestNames.Layout), function() {
