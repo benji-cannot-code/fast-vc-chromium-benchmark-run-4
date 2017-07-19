da5fd4647ae8ff77a73a5c40ef9258beaa6b5484
rbpotter <rbpotter@chromium.org>
2017-07-19 21:10:06 +0000

    Cleanup chrome.send in print preview
    Move all chrome.send calls to the native layer
    Rename native layer functions to match names of messages being sent.
    
    Bug: 717296
    Change-Id: Ie69391d107183c4fd0e97339d4014ea89f1181e3
    Reviewed-on: https://chromium-review.googlesource.com/575374
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487979}

--- chrome/test/data/webui/print_preview/print_preview_tests.js
+++ chrome/test/data/webui/print_preview/print_preview_tests.js
@@ -1346,7 +1346,7 @@ cr.define('print_preview_test', function() {
             expectEquals(
                 mediaDefault.height_microns,
                 printTicketStore.mediaSize.getValue().height_microns);
-            return nativeLayer.whenCalled('startHideDialog');
+            return nativeLayer.whenCalled('hidePreview');
           });
     });
 
@@ -1448,7 +1448,7 @@ cr.define('print_preview_test', function() {
                  */
                 function(args) {
                   expectTrue(args.openPdfInPreview);
-                  return nativeLayer.whenCalled('startHideDialog');
+                  return nativeLayer.whenCalled('hidePreview');
                 });
       });
     }
@@ -1481,7 +1481,7 @@ cr.define('print_preview_test', function() {
                  */
                 function(args) {
                   expectTrue(args.showSystemDialog);
-                  return nativeLayer.whenCalled('startHideDialog');
+                  return nativeLayer.whenCalled('hidePreview');
                 });
       });
     }
