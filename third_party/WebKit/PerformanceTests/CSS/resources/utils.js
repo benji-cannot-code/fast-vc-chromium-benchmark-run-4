db7973aacd5e2444486c591060edc51c4ad8d8c7
Naina Raisinghani <nainar@chromium.org>
2017-07-19 03:05:00 +0000

    Add testcases to PerformanceTests/CSS to test changing style in trees of different depth
    This CL adds a utils file that allows us to create a DOM tree of
    varying depth and branch factor.
    
    We use this file to create trees with:
    1. Shallow tree (depth=2) with high branching factor(10)
    2. Deep tree (depth=10) with small branching factor(2)
    
    We then change the style of all elements in the tree and benchmark the
    number of runs done in a second. This allows us to set up a benchmark
    for style manipulation as a benchmark is not currently present.
    
    Bug: 741970
    Change-Id: I90bf5d16ceefb6d79c7bbbc3657d8202ca49b89d
    Reviewed-on: https://chromium-review.googlesource.com/569421
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: nainar <nainar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487731}

---
+++ third_party/WebKit/PerformanceTests/CSS/resources/utils.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function createDOMTree(node, siblings, depth) {
+    if (!depth)
+        return;
+    for (var i=0; i<siblings; i++) {
+        var div = document.createElement("div");
+        node.appendChild(div);
+        createDOMTree(div, siblings, depth-1);
+    }
+}
+
+function createDeepDOMTree() {
+        createDOMTree(document.body, 2, 10);
+}
+
+function createShallowDOMTree() {
+        createDOMTree(document.body, 10, 2);
+}
