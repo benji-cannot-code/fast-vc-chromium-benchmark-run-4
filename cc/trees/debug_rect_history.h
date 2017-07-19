d5493115707814fce47ad4ea226f5bc91cac1988
Hayley Ferr <hayleyferr@chromium.org>
2017-07-19 20:32:48 +0000

    Iterate each TouchAction in DebugRectHistory::SaveTouchEventHandlerRectsCallback
    So we don't lose data in the debugging visualization in
    DebugRectHistory::SaveTouchEventHandlerRectsCallback, we now
    iterate through the rects independently from each touch action 
    as opposed to using the union of these values. DebugRect
    stores a TouchAction bit that is populated within
    SaveTouchEventHandlerRectsCallback.
    
    In HeadsUpDisplayLayerImpl::DrawDebugRects, the
    |debug_rects|'s touch action is added to the
    HeadsUpDisplayLayerImpl::DrawDebugRect's
    label in the case |debug_rects|'s type is
    TOUCH_EVENT_HANDLER_RECT_TYPE.
    
    Bug: 
    Change-Id: I7af79e490481b485b11601f7c44cf290d5707e46
    Reviewed-on: https://chromium-review.googlesource.com/571032
    Commit-Queue: Hayley Ferr <hayleyferr@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487959}

--- cc/trees/debug_rect_history.h
+++ cc/trees/debug_rect_history.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/macros.h"
+#include "cc/input/touch_action.h"
 #include "cc/layers/layer_collections.h"
 #include "ui/gfx/geometry/rect.h"
 
@@ -47,11 +48,21 @@ enum DebugRectType {
 };
 
 struct DebugRect {
+  DebugRect(DebugRectType new_type,
+            const gfx::Rect& new_rect,
+            TouchAction new_touch_action)
+      : type(new_type), rect(new_rect), touch_action(new_touch_action) {
+    if (type != TOUCH_EVENT_HANDLER_RECT_TYPE)
+      DCHECK_EQ(touch_action, kTouchActionNone);
+  }
   DebugRect(DebugRectType new_type, const gfx::Rect& new_rect)
-      : type(new_type), rect(new_rect) {}
+      : DebugRect(new_type, new_rect, kTouchActionNone) {}
 
   DebugRectType type;
   gfx::Rect rect;
+  // Valid when |type| is |TOUCH_EVENT_HANDLER_RECT_TYPE|, otherwise default to
+  // |kTouchActionNone|.
+  TouchAction touch_action;
 };
 
 // This class maintains a history of rects of various types that can be used
