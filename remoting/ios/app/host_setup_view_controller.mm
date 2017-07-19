dfa2dc702675ba8dab28b997c3d2c6a386116256
Yuwei Huang <yuweih@chromium.org>
2017-07-19 18:05:48 +0000

    [CRD iOS] Hide the send email button
    Since we are not going to implement the send email feature soon, this CL
    hides the send email button in the host instruction page.
    
    This CL replaces the HostSetupFooterView with an empty view.
    
    Bug: 736932
    Change-Id: Id3ce3faea6f2352667904eb48d4e811587254d82
    Reviewed-on: https://chromium-review.googlesource.com/575302
    Reviewed-by: Scott Nichols <nicholss@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487913}

--- remoting/ios/app/host_setup_view_controller.mm
+++ remoting/ios/app/host_setup_view_controller.mm
@@ -35,7 +35,7 @@ static NSArray<NSString*>* const kSetupSteps = @[
 #pragma clang diagnostic pop
 
 static const CGFloat kHeaderHeight = 80.f;
-static const CGFloat kFooterHeight = 80.f;
+static const CGFloat kFooterHeight = 15.f;  // 80.0f for HostSetupFooterView
 
 @implementation HostSetupViewController
 
@@ -52,7 +52,9 @@ static const CGFloat kFooterHeight = 80.f;
             forSupplementaryViewOfKind:UICollectionElementKindSectionHeader
                    withReuseIdentifier:UICollectionElementKindSectionHeader];
 
-    [self.collectionView registerClass:[HostSetupFooterView class]
+    // TODO(yuweih): Use HostSetupFooterView once we have the email instructions
+    // feature.
+    [self.collectionView registerClass:[UICollectionReusableView class]
             forSupplementaryViewOfKind:UICollectionElementKindSectionFooter
                    withReuseIdentifier:UICollectionElementKindSectionFooter];
   }
@@ -87,9 +89,15 @@ static const CGFloat kFooterHeight = 80.f;
 - (UICollectionReusableView*)collectionView:(UICollectionView*)collectionView
           viewForSupplementaryElementOfKind:(NSString*)kind
                                 atIndexPath:(NSIndexPath*)indexPath {
-  return [collectionView dequeueReusableSupplementaryViewOfKind:kind
-                                            withReuseIdentifier:kind
-                                                   forIndexPath:indexPath];
+  UICollectionReusableView* view =
+      [collectionView dequeueReusableSupplementaryViewOfKind:kind
+                                         withReuseIdentifier:kind
+                                                forIndexPath:indexPath];
+  if (kind == UICollectionElementKindSectionFooter) {
+    // TODO(yuweih): No longer necessary once we use HostSetupFooterView.
+    view.backgroundColor = RemotingTheme.setupListBackgroundColor;
+  }
+  return view;
 }
 
 #pragma mark - MDCCollectionViewStylingDelegate
