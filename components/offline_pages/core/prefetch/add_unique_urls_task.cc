bd86607be11ae25674838f1a6f26f6ebf22982eb
Carlos Knippschild <carlosk@chromium.org>
2017-07-19 00:06:15 +0000

    Offline Prefetch: readme update and TODO updated.
    Added a new section to the Offline Prefetch readme file and updated the
    NWake scheduling TODO when Zine URL suggestions are added to the
    prefetch store.
    
    Bug: 701939
    Change-Id: Ibe7fb76ab8a7c67fa7e5953d7d35a7cb24bdd82a
    Reviewed-on: https://chromium-review.googlesource.com/574906
    Commit-Queue: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487677}

--- components/offline_pages/core/prefetch/add_unique_urls_task.cc
+++ components/offline_pages/core/prefetch/add_unique_urls_task.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback.h"
+#include "base/logging.h"
 #include "base/time/time.h"
 #include "components/offline_pages/core/prefetch/prefetch_types.h"
 #include "components/offline_pages/core/prefetch/store/prefetch_store.h"
@@ -134,8 +135,11 @@ void AddUniqueUrlsTask::Run() {
 }
 
 void AddUniqueUrlsTask::OnUrlsAdded(Result result) {
-  // TODO(carlosk): schedule NWake here if at least one new entry was added to
-  // the store.
+  if (result == Result::URLS_ADDED) {
+    // TODO(carlosk): schedule NWake here if at least one new entry was added to
+    // the store.
+    NOTIMPLEMENTED();
+  }
   TaskComplete();
 }
 
