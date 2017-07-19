3d317eefeacb73cd0dbfe68dfaf60864c9cec107
Matt Mueller <mattm@chromium.org>
2017-07-19 21:37:09 +0000

    Remove unused keychain variable in PasswordStoreFactory::BuildServiceInstanceFor
    The variable is unused since https://chromium.googlesource.com/chromium/src/+/efe5baba2d156238707d6d8303b2055ddb246fe2
    
    Bug: none
    Change-Id: I11901f5def30d0058f3c1cf10e1ba4e5be9f84d3
    Reviewed-on: https://chromium-review.googlesource.com/576993
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487996}

--- chrome/browser/password_manager/password_store_factory.cc
+++ chrome/browser/password_manager/password_store_factory.cc
@@ -44,8 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/webdata/password_web_data_service_win.h"
 #elif defined(OS_MACOSX)
 #include "chrome/browser/password_manager/password_store_mac.h"
-#include "crypto/apple_keychain.h"
-#include "crypto/mock_apple_keychain.h"
 #elif defined(OS_CHROMEOS) || defined(OS_ANDROID)
 // Don't do anything. We're going to use the default store.
 #elif defined(USE_X11)
@@ -171,11 +169,6 @@ PasswordStoreFactory::BuildServiceInstanceFor(
                             WebDataServiceFactory::GetPasswordWebDataForProfile(
                                 profile, ServiceAccessType::EXPLICIT_ACCESS));
 #elif defined(OS_MACOSX)
-  std::unique_ptr<crypto::AppleKeychain> keychain(
-      base::CommandLine::ForCurrentProcess()->HasSwitch(
-          os_crypt::switches::kUseMockKeychain)
-          ? new crypto::MockAppleKeychain()
-          : new crypto::AppleKeychain());
   ps = new PasswordStoreMac(main_thread_runner, std::move(login_db),
                             profile->GetPrefs());
 #elif defined(OS_CHROMEOS) || defined(OS_ANDROID)
