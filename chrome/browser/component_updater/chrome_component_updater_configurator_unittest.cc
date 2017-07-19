f0a3b2a6bebf18cf9018feaaf8df3c4e0667e754
Joshua Pawlicki <waffles@google.com>
2017-07-19 18:08:58 +0000

    Switch component updater to use new URL by default. (Launching.)
    Bug: 685897
    Change-Id: Ib08627d9b1f99baa7f50d97e30b7f6ba271d3495
    Reviewed-on: https://chromium-review.googlesource.com/576707
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487915}

--- chrome/browser/component_updater/chrome_component_updater_configurator_unittest.cc
+++ chrome/browser/component_updater/chrome_component_updater_configurator_unittest.cc
@@ -125,8 +125,8 @@ TEST_F(ChromeComponentUpdaterConfiguratorTest, TestUseEncryption) {
 
   const auto urls = config->UpdateUrl();
   ASSERT_EQ(2u, urls.size());
-  ASSERT_STREQ(kUpdaterDefaultUrl, urls[0].spec().c_str());
-  ASSERT_STREQ(kUpdaterFallbackUrl, urls[1].spec().c_str());
+  ASSERT_STREQ(kUpdaterDefaultUrlAlt, urls[0].spec().c_str());
+  ASSERT_STREQ(kUpdaterFallbackUrlAlt, urls[1].spec().c_str());
 
   ASSERT_EQ(config->UpdateUrl(), config->PingUrl());
 
@@ -136,7 +136,7 @@ TEST_F(ChromeComponentUpdaterConfiguratorTest, TestUseEncryption) {
     const ConfiguratorImpl config(cmdline, nullptr, true);
     const auto urls = config.UpdateUrl();
     ASSERT_EQ(1u, urls.size());
-    ASSERT_STREQ(kUpdaterDefaultUrl, urls[0].spec().c_str());
+    ASSERT_STREQ(kUpdaterDefaultUrlAlt, urls[0].spec().c_str());
     ASSERT_EQ(config.UpdateUrl(), config.PingUrl());
   }
 
@@ -144,8 +144,8 @@ TEST_F(ChromeComponentUpdaterConfiguratorTest, TestUseEncryption) {
     const ConfiguratorImpl config(cmdline, nullptr, false);
     const auto urls = config.UpdateUrl();
     ASSERT_EQ(2u, urls.size());
-    ASSERT_STREQ(kUpdaterDefaultUrl, urls[0].spec().c_str());
-    ASSERT_STREQ(kUpdaterFallbackUrl, urls[1].spec().c_str());
+    ASSERT_STREQ(kUpdaterDefaultUrlAlt, urls[0].spec().c_str());
+    ASSERT_STREQ(kUpdaterFallbackUrlAlt, urls[1].spec().c_str());
     ASSERT_EQ(config.UpdateUrl(), config.PingUrl());
   }
 }
