e83b92ece3e0fed825d3965582ad94e76b60ab28
Olivier Robin <olivierrobin@chromium.org>
2017-07-19 13:15:45 +0000

    Revert "Show loading spinner on bookmark panel when syncing (iOS)"
    This reverts commit cbd742dd1e63e553a4a33b428b81620f7fa7cedb.
    
    Reason for revert: Eg tests time out waiting for this animation.
    Testing the EGtests bots without this CL.
    
    Original change's description:
    > Show loading spinner on bookmark panel when syncing (iOS)
    > 
    > [Demo videos]
    > - account with bookmarks:
    > https://drive.google.com/open?id=0B1O0Z7eoZMuGeTBjVXdla0RPeFE
    > - account with no bookmarks:
    > https://drive.google.com/open?id=0B1O0Z7eoZMuGR0pGb2tYUHcyOWM
    > 
    > TEST=Make sure it works when signed in user has no bookmarks or there is any sync error...
    > 
    > Bug: 696893
    > Change-Id: I30a29eed423253651ff2466943c8ecdd6974e0b0
    > Reviewed-on: https://chromium-review.googlesource.com/569538
    > Commit-Queue: Marti Wong <martiw@chromium.org>
    > Reviewed-by: Jean-François Geyelin <jif@chromium.org>
    > Reviewed-by: Eric Noyau <noyau@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#487374}
    
    TBR=jif@chromium.org,noyau@chromium.org,martiw@google.com,ramyasharma@chromium.org,martiw@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 696893
    Change-Id: I89336f1cb8f0ed6213fe09d344aba37529cf7dab
    Reviewed-on: https://chromium-review.googlesource.com/577547
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Eric Noyau <noyau@chromium.org>
    Reviewed-by: Jean-François Geyelin <jif@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487839}

--- ios/chrome/browser/ui/ntp/recent_tabs/recent_tabs_panel_controller.mm
+++ ios/chrome/browser/ui/ntp/recent_tabs/recent_tabs_panel_controller.mm
@@ -9,10 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/browser_sync/profile_sync_service.h"
 #include "components/sessions/core/tab_restore_service.h"
+#include "components/signin/core/browser/signin_manager.h"
 #include "components/sync_sessions/open_tabs_ui_delegate.h"
 #include "components/sync_sessions/synced_session.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/sessions/ios_chrome_tab_restore_service_factory.h"
+#include "ios/chrome/browser/signin/signin_manager_factory.h"
 #include "ios/chrome/browser/sync/ios_chrome_profile_sync_service_factory.h"
 #include "ios/chrome/browser/sync/sync_setup_service.h"
 #include "ios/chrome/browser/sync/sync_setup_service_factory.h"
@@ -186,7 +188,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - Private
 
 - (BOOL)isSignedIn {
-  return _syncedSessionsObserver->IsSignedIn();
+  SigninManager* signin_manager =
+      ios::SigninManagerFactory::GetForBrowserState(_browserState);
+  return signin_manager->IsAuthenticated();
 }
 
 - (BOOL)isSyncTabsEnabled {
