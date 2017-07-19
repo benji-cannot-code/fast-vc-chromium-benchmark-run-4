c4162b43968a2c0fe290b87979d37e3a98d7583e
Mohamad Ahmadi <mahmadi@chromium.org>
2017-07-19 17:51:54 +0000

    [Payment Request] PaymentRequestCache, a map of WebState to PaymentRequests
    - Creates a PaymentRequestCache class as KeyedService along with its
      factory to ensure only one instance of it exists per BrowserState.
    - PaymentRequestCache maintains a map of web::WebState to a list of
      payments::PaymentRequest instances maintained for that WebState.
    - The PaymentRequestCache instance can be accessed in browser tests via
      the instance of BrowserState in order to verify the correct creation
      and state of payments::PaymentRequest objects.
    
    BUG=602666
    
    Change-Id: I4aef5be49ab93261bcbebd6f742378bcca59938e
    Reviewed-on: https://chromium-review.googlesource.com/577379
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: mahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487909}

---
+++ ios/chrome/browser/payments/ios_payment_request_cache_factory.mm
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/payments/ios_payment_request_cache_factory.h"
+
+#include "base/memory/ptr_util.h"
+#include "base/memory/singleton.h"
+#include "components/keyed_service/ios/browser_state_dependency_manager.h"
+#include "ios/chrome/browser/browser_state/chrome_browser_state.h"
+#include "ios/chrome/browser/payments/payment_request_cache.h"
+#include "ios/web/public/browser_state.h"
+
+namespace payments {
+
+// static
+payments::PaymentRequestCache*
+IOSPaymentRequestCacheFactory::GetForBrowserState(
+    ios::ChromeBrowserState* browser_state) {
+  return static_cast<payments::PaymentRequestCache*>(
+      GetInstance()->GetServiceForBrowserState(browser_state, true));
+}
+
+// static
+IOSPaymentRequestCacheFactory* IOSPaymentRequestCacheFactory::GetInstance() {
+  return base::Singleton<IOSPaymentRequestCacheFactory>::get();
+}
+
+IOSPaymentRequestCacheFactory::IOSPaymentRequestCacheFactory()
+    : BrowserStateKeyedServiceFactory(
+          "PaymentRequestCache",
+          BrowserStateDependencyManager::GetInstance()) {}
+
+IOSPaymentRequestCacheFactory::~IOSPaymentRequestCacheFactory() {}
+
+std::unique_ptr<KeyedService>
+IOSPaymentRequestCacheFactory::BuildServiceInstanceFor(
+    web::BrowserState* context) const {
+  return base::WrapUnique(new payments::PaymentRequestCache);
+}
+
+}  // namespace payments
