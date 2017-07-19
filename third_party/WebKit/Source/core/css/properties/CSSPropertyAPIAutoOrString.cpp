f5b23b02a2f72bc0a7f1c6fbf8998284cbcefb87
Rob Buis <rob.buis@samsung.com>
2017-07-19 04:47:52 +0000

    Implements CSSPropertyAPI for -webkit-locale/-webkit-hyphenate-character
    As part of Project Ribbon, separate the parsing logic for CSS
    properties -webkit-locale and -webkit-hyphenate-character from
    the parser into an API.
    
    Bug: 668012
    
    Signed-off-by: Rob Buis <rob.buis@samsung.com>
    Change-Id: I6a1e7cf419d2f425ef2ca736f0153c48025caa78
    Reviewed-on: https://chromium-review.googlesource.com/576234
    Reviewed-by: Bugs Nash <bugsnash@chromium.org>
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487753}

---
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyAPIAutoOrString.cpp
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/properties/CSSPropertyAPIAutoOrString.h"
+
+#include "core/css/CSSStringValue.h"
+#include "core/css/parser/CSSPropertyParserHelpers.h"
+
+namespace blink {
+class CSSParserLocalContext;
+
+const CSSValue* CSSPropertyAPIAutoOrString::parseSingleValue(
+    CSSParserTokenRange& range,
+    const CSSParserContext&,
+    const CSSParserLocalContext&) {
+  if (range.Peek().Id() == CSSValueAuto)
+    return CSSPropertyParserHelpers::ConsumeIdent(range);
+  return CSSPropertyParserHelpers::ConsumeString(range);
+}
+
+}  // namespace blink
