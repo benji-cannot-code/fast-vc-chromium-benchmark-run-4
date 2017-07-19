1f7e0bb42ae01d0f0c9d87d8cbfce521d6b48df5
Scott Violet <sky@chromium.org>
2017-07-19 20:21:52 +0000

    chromeos: gets swapping displays working for mus
    WindowTreeHostManager has a special code path when the primary display
    changes to an existing display. This gets that code path working for
    mus.
    
    BUG=none
    TEST=covered by tests
    
    Change-Id: I5558a93303eeb9ca6a8f3c6769661d71d92d97fe
    Reviewed-on: https://chromium-review.googlesource.com/576399
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Elliot Glaysher <erg@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487949}

--- ash/mus/display_synchronizer.cc
+++ ash/mus/display_synchronizer.cc
@@ -14,6 +14,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/manager/managed_display_info.h"
 
 namespace ash {
+namespace {
+
+aura::WindowTreeHostMus* ToWindowTreeHostMus(AshWindowTreeHost* ash_host) {
+  return static_cast<aura::WindowTreeHostMus*>(ash_host->AsWindowTreeHost());
+}
+
+}  // namespace
 
 DisplaySynchronizer::DisplaySynchronizer(
     aura::WindowManagerClient* window_manager_client)
@@ -69,8 +76,7 @@ void DisplaySynchronizer::OnWindowTreeHostReusedForDisplay(
     AshWindowTreeHost* window_tree_host,
     const display::Display& display) {
   aura::WindowTreeHostMus* window_tree_host_mus =
-      static_cast<aura::WindowTreeHostMus*>(
-          window_tree_host->AsWindowTreeHost());
+      ToWindowTreeHostMus(window_tree_host);
   if (window_tree_host_mus->display_id() == display.id())
     return;
   const display::ManagedDisplayInfo& display_info =
@@ -81,9 +87,17 @@ void DisplaySynchronizer::OnWindowTreeHostReusedForDisplay(
   viewport_metrics->device_scale_factor = display.device_scale_factor();
   viewport_metrics->ui_scale_factor = display_info.configured_ui_scale();
   window_manager_client_->AddDisplayReusingWindowTreeHost(
-      static_cast<aura::WindowTreeHostMus*>(
-          window_tree_host->AsWindowTreeHost()),
-      display, std::move(viewport_metrics));
+      ToWindowTreeHostMus(window_tree_host), display,
+      std::move(viewport_metrics));
+}
+
+void DisplaySynchronizer::OnWindowTreeHostsSwappedDisplays(
+    AshWindowTreeHost* host1,
+    AshWindowTreeHost* host2) {
+  DCHECK(host1);
+  DCHECK(host2);
+  window_manager_client_->SwapDisplayRoots(ToWindowTreeHostMus(host1),
+                                           ToWindowTreeHostMus(host2));
 }
 
 void DisplaySynchronizer::OnWillProcessDisplayChanges() {
