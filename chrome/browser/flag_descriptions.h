5ec89544d32d7ce2255fab6655c289b0f481b1bc
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2017-07-19 21:55:52 +0000

    Add flag to disable touch-specific interactions of the Files app.
    This change also includes a change to hide the context menu button
    added by https://chromium-review.googlesource.com/c/566768/ when
    the flag is set.
    
    Bug: 745879
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I1f1a75f55eab76e9ec3300a73820937357a37679
    Reviewed-on: https://chromium-review.googlesource.com/574902
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Reviewed-by: Naoki Fukino <fukino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488004}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1313,6 +1313,9 @@ extern const char kEnableLockScreenAppsDescription[];
 extern const char kEnableTouchSupportForScreenMagnifierName[];
 extern const char kEnableTouchSupportForScreenMagnifierDescription[];
 
+extern const char kFileManagerTouchModeName[];
+extern const char kFileManagerTouchModeDescription[];
+
 extern const char kEnableZipArchiverOnFileManagerName[];
 extern const char kEnableZipArchiverOnFileManagerDescription[];
 
