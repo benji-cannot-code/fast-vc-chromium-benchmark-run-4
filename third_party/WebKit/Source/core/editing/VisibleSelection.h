d00f6f87c7aadddb325263a5d85b3820b8d20510
Yoshifumi Inoue <yosin@chromium.org>
2017-07-19 04:34:50 +0000

    Revise shadow boundary adjustment in VisibleSelectionTemplate::Validate()
    This patch changes |VisibleSelectionTemplate::Validate()| to adjust start/end
    position not to avoid crossing shadow boundaries by using pure function instead
    of modifying |VisibleSelectionTemplate| object itself for improving code health.
    
    Change details:
    - VisibleSelectionTemplate::AdjustSelectionToAvoidCrossingShadowBoundaries()
      We don't need to check null selection since before doing adjustment we
      check null-position after granularity expansion.
    - SelectionAdjuster::AdjustSelectionToAvoidCrossingShadowBoundaries()
     * Changes to two pure-functions returning start or end.
     * Move checking shadow boundary crossing into functions to share code between
       functions for computing start/end.
    
    Bug: 230267
    Change-Id: I34cb3bd6ca65e5d25370028b98cd69c9d2dceb2c
    Reviewed-on: https://chromium-review.googlesource.com/569898
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487746}

--- third_party/WebKit/Source/core/editing/VisibleSelection.h
+++ third_party/WebKit/Source/core/editing/VisibleSelection.h
@@ -156,7 +156,6 @@ class CORE_TEMPLATE_CLASS_EXPORT VisibleSelectionTemplate {
   void Validate(const SelectionTemplate<Strategy>&, TextGranularity);
 
   // Support methods for Validate()
-  void AdjustSelectionToAvoidCrossingShadowBoundaries();
   void AdjustSelectionToAvoidCrossingEditingBoundaries();
   void UpdateSelectionType();
 
