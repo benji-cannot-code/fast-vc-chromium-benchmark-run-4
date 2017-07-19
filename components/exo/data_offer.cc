6519a7550bfa2390a2d83fe0fc819f8d31c334bd
Daichi Hirono <hirono@chromium.org>
2017-07-19 07:14:55 +0000

    Add wl_data_device and relatives' stubs.
    The CL adds stubs of wl_data_device, wl_data_source, and wl_data_offer to
    exo.
    
    BUG=b:31988797, b:28779679
    TEST=No new test since it just adds stubs.
    
    Change-Id: I6e9ae74d00dbfb8306f2c8285176965feaf3d7cc
    Reviewed-on: https://chromium-review.googlesource.com/569322
    Commit-Queue: Daichi Hirono <hirono@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487776}

---
+++ components/exo/data_offer.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/exo/data_offer.h"
+
+#include "components/exo/data_offer_delegate.h"
+
+namespace exo {
+
+DataOffer::DataOffer(DataOfferDelegate* delegate) : delegate_(delegate) {}
+
+DataOffer::~DataOffer() {
+  delegate_->OnDataOfferDestroying(this);
+}
+
+void DataOffer::Accept(const std::string& mime_type) {
+  NOTIMPLEMENTED();
+}
+void DataOffer::Receive(const std::string& mime_type, base::ScopedFD fd) {
+  NOTIMPLEMENTED();
+}
+void DataOffer::Finish() {
+  NOTIMPLEMENTED();
+}
+void DataOffer::SetActions(const base::flat_set<DndAction>& dnd_actions,
+                           DndAction preferred_action) {
+  NOTIMPLEMENTED();
+}
+
+}  // namespace exo
---
+++ components/exo/data_source.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/exo/data_source.h"
+
+#include "components/exo/data_source_delegate.h"
+
+namespace exo {
+
+DataSource::DataSource(DataSourceDelegate* delegate) : delegate_(delegate) {}
+
+DataSource::~DataSource() {
+  delegate_->OnDataSourceDestroying(this);
+}
+
+void DataSource::Offer(const std::string& mime_type) {
+  NOTIMPLEMENTED();
+}
+
+void DataSource::SetActions(const base::flat_set<DndAction>& dnd_actions) {
+  NOTIMPLEMENTED();
+}
+
+}  // namespace exo
