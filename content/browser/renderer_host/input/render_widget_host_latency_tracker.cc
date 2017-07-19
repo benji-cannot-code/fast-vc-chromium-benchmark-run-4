f28a506942c1f4dffd913af0fbadf3af55391d90
nzolghadr <nzolghadr@chromium.org>
2017-07-19 10:35:10 -0700

    Add a sampling scheme to ScrollUpdate.Touch UKM
    There was a lot of data being reported as part of
    the Event.ScrollUpdate.Touch UKM. For now we decided
    to reduce the volume of the data by a simple sampling
    scheme until we find a more permanent solution.
    
    BUG=742363
    
    Review-Url: https://codereview.chromium.org/2976113002
    Cr-Commit-Position: refs/heads/master@{#487904}

--- content/browser/renderer_host/input/render_widget_host_latency_tracker.cc
+++ content/browser/renderer_host/input/render_widget_host_latency_tracker.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/rand_util.h"
 #include "build/build_config.h"
 #include "components/rappor/public/rappor_utils.h"
 #include "content/browser/renderer_host/render_widget_host_delegate.h"
@@ -29,6 +30,8 @@ using ui::LatencyInfo;
 
 namespace content {
 namespace {
+
+constexpr int kSamplingInterval = 10;
 
 std::string WebInputEventTypeToInputModalityString(WebInputEvent::Type type) {
   if (type == blink::WebInputEvent::kMouseWheel) {
@@ -139,7 +142,8 @@ void RecordEQTAccuracy(base::TimeDelta queueing_time,
 
 }  // namespace
 
-RenderWidgetHostLatencyTracker::RenderWidgetHostLatencyTracker()
+RenderWidgetHostLatencyTracker::RenderWidgetHostLatencyTracker(
+    bool metric_sampling)
     : ukm_source_id_(-1),
       last_event_id_(0),
       latency_component_id_(0),
@@ -147,7 +151,13 @@ RenderWidgetHostLatencyTracker::RenderWidgetHostLatencyTracker()
       has_seen_first_gesture_scroll_update_(false),
       active_multi_finger_gesture_(false),
       touch_start_default_prevented_(false),
-      render_widget_host_delegate_(nullptr) {}
+      metric_sampling_(metric_sampling),
+      metric_sampling_events_since_last_sample_(-1),
+      render_widget_host_delegate_(nullptr) {
+  if (metric_sampling)
+    metric_sampling_events_since_last_sample_ =
+        base::RandUint64() % kSamplingInterval;
+}
 
 RenderWidgetHostLatencyTracker::~RenderWidgetHostLatencyTracker() {}
 
@@ -414,6 +424,14 @@ void RenderWidgetHostLatencyTracker::ReportUkmScrollLatency(
     const LatencyInfo::LatencyComponent& start_component,
     const LatencyInfo::LatencyComponent& end_component) {
   CONFIRM_VALID_TIMING(start_component, end_component)
+
+  // Only report a subset of this metric as the volume is too high.
+  if (event_name == "Event.ScrollUpdate.Touch") {
+    metric_sampling_events_since_last_sample_++;
+    metric_sampling_events_since_last_sample_ %= kSamplingInterval;
+    if (metric_sampling_ && metric_sampling_events_since_last_sample_)
+      return;
+  }
 
   ukm::SourceId ukm_source_id = GetUkmSourceId();
   ukm::UkmRecorder* ukm_recorder = ukm::UkmRecorder::Get();
