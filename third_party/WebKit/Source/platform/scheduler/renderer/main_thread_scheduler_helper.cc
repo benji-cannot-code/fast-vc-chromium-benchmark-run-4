c816d292ab2f7488be5fc4df98aa3be281a190f6
Alexander Timin <altimin@chromium.org>
2017-07-19 16:36:47 +0000

    [scheduler] Do not run observers on default queue.
    Do not run observers on main thread default queue and always run
    OnTaskCompleted regardless of ShouldNotifyObservers setting, in order
    to record task duration metrics for all queues.
    
    R=alexclarke@chromium.org
    
    Change-Id: I5223a9d704af98d02088557f27a5c32733c8b577
    Reviewed-on: https://chromium-review.googlesource.com/577535
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487886}

--- third_party/WebKit/Source/platform/scheduler/renderer/main_thread_scheduler_helper.cc
+++ third_party/WebKit/Source/platform/scheduler/renderer/main_thread_scheduler_helper.cc
@@ -23,7 +23,7 @@ MainThreadSchedulerHelper::MainThreadSchedulerHelper(
       control_task_queue_(
           NewTaskQueue(MainThreadTaskQueue::QueueCreationParams(
                            MainThreadTaskQueue::QueueType::CONTROL)
-                           .SetShouldNotifyObservers(true))) {
+                           .SetShouldNotifyObservers(false))) {
   InitDefaultQueues(default_task_queue_, control_task_queue_);
 }
 
