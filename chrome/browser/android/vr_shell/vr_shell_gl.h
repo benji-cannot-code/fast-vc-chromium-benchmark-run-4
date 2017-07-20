5e2c6f00389f365f73d37cde94a165006b5a2b58
Christopher Grant <cjgrant@chromium.org>
2017-07-20 16:34:14 +0000

    VR: Remove inappropriate state from the Scene class.
    The Scene is meant to hold state supplied by the SceneManager, and
    consumed by the rendering subsystem.  Scene shouldn't know about modes,
    prompts, etc.  This CL tries to banish any such state.
    
    BUG=
    
    Change-Id: I5cab334aac5150b725ab6408625367d0554bdabf
    Reviewed-on: https://chromium-review.googlesource.com/576471
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: Yash Malik <ymalik@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488267}

--- chrome/browser/android/vr_shell/vr_shell_gl.h
+++ chrome/browser/android/vr_shell/vr_shell_gl.h
@@ -115,6 +115,8 @@ class VrShellGl : public device::mojom::VRPresentationProvider,
       device::mojom::VRSubmitFrameClientPtrInfo submit_client_info,
       device::mojom::VRPresentationProviderRequest request);
 
+  void set_is_exiting(bool exiting) { is_exiting_ = exiting; }
+
  private:
   void GvrInit(gvr_context* gvr_api);
   void InitializeRenderer();
@@ -237,6 +239,7 @@ class VrShellGl : public device::mojom::VRPresentationProvider,
   bool ready_to_draw_ = false;
   bool surfaceless_rendering_;
   bool daydream_support_;
+  bool is_exiting_ = false;
 
   std::unique_ptr<VrController> controller_;
 
