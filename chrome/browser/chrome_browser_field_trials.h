df8721b7b5be39201cacb91a49d0a21d4bd16f6b
Kyle Milka <kmilka@google.com>
2017-07-20 20:24:57 +0000

    Add the PlatformFieldTrials Class
    This is part 4 of 6 for FieldTrial refactoring for WebView
    
    Part 1: https://chromium-review.googlesource.com/c/562098/
    Part 2: https://chromium-review.googlesource.com/c/561920/
    Part 3: https://chromium-review.googlesource.com/c/561922/
    Part 4: https://chromium-review.googlesource.com/c/561980/
    Part 5: https://chromium-review.googlesource.com/c/562417/
    Part 6: https://chromium-review.googlesource.com/c/562021/
    
    This CL adds a new class, PlatformFieldTrials, that the 
    Chrome specific ChromeBrowserFieldTrials is now
    derived from. Its implementation is currently just stubs
    as they shouldn't be necessary for WebView.
    
    BUG=678288
    
    Change-Id: I5d7b1b77f5af39594977a5bd7ed66bf51cb63628
    Reviewed-on: https://chromium-review.googlesource.com/561980
    Commit-Queue: Kyle Milka <kmilka@google.com>
    Reviewed-by: Alexei Svitkine (slow) <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488383}

--- chrome/browser/chrome_browser_field_trials.h
+++ chrome/browser/chrome_browser_field_trials.h
@@ -7,26 +7,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROME_BROWSER_FIELD_TRIALS_H_
 
 #include "base/macros.h"
+#include "components/variations/platform_field_trials.h"
 
 namespace base {
 class FeatureList;
 }
 
-class ChromeBrowserFieldTrials {
+class ChromeBrowserFieldTrials : public variations::PlatformFieldTrials {
  public:
   ChromeBrowserFieldTrials();
-  ~ChromeBrowserFieldTrials();
+  ~ChromeBrowserFieldTrials() override;
 
-  void SetupFieldTrials();
-
-  // Create field trials that will control feature list features. This should be
-  // called during the same timing window as
-  // FeatureList::AssociateReportingFieldTrial. |has_seed| indicates that the
-  // variations service used a seed to create field trials. This can be used to
-  // prevent associating a field trial with a feature that you expect to be
-  // controlled by the variations seed.
-  void SetupFeatureControllingFieldTrials(bool has_seed,
-                                          base::FeatureList* feature_list);
+  // variations::PlatformFieldTrials:
+  void SetupFieldTrials() override;
+  void SetupFeatureControllingFieldTrials(
+      bool has_seed,
+      base::FeatureList* feature_list) override;
 
  private:
   // Instantiates dynamic trials by querying their state, to ensure they get
