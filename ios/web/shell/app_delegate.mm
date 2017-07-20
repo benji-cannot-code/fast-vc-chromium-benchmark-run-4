a345a63d1b06329e8612b5ff38bbf1a31f7ec15b
Mike Dougherty <michaeldo@chromium.org>
2017-07-20 21:26:50 +0000

    Modernize layout of toolbar items for ios web shell
    The manual layout of the toolbar items for ios_web_shell is
    outdated and no longer works with iOS 11. Update the UIButtons to
    UIBarButtonItems and automatically lay them out with setItems: instead
    of adding items as subviews to the UIToolbar.
    
    Bug: None
    Change-Id: Id04d7bd7b630c28b0f76f3dc0f78510bca650ffd
    Reviewed-on: https://chromium-review.googlesource.com/580148
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488411}

--- ios/web/shell/app_delegate.mm
+++ ios/web/shell/app_delegate.mm
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
   [self.window makeKeyAndVisible];
   self.window.backgroundColor = [UIColor whiteColor];
+  self.window.tintColor = [UIColor darkGrayColor];
 
   _delegate.reset(new web::ShellMainDelegate());
 
