90120e01e2183051c6c079fab0ffcb47611c7257
Justin Carlson <justincarlson@chromium.org>
2017-07-20 17:02:10 +0000

    Refactor SyncedPrintersManager.
    This change brings SyncedPrintersManager closer to PrinterDetector
    style interfaces, and aligns function names/styles with the design doc
    for CUPS printers management.  It also removes a bunch of unique_ptr
    wrapping of printers that are passed around, since these are not at
    all performance critical and the unique_ptr stuff hampers readability
    (and leads to subtle bugs when unique_ptrs are both used in a bind and
    passed as a parameter at the same time).
    
    Also convert it to the factory-function pattern while I'm mucking with
    stuff and clean up the header.
    
    
    Bug: 742487
    Change-Id: Ibc9dd0a2f94dad8cb7a1f4adb1897668800311bf
    Reviewed-on: https://chromium-review.googlesource.com/575289
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Justin Carlson <justincarlson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488276}

--- chrome/browser/chromeos/printing/synced_printers_manager_factory.cc
+++ chrome/browser/chromeos/printing/synced_printers_manager_factory.cc
@@ -65,7 +65,8 @@ SyncedPrintersManager* SyncedPrintersManagerFactory::BuildServiceInstanceFor(
           store_factory, base::BindRepeating(base::IgnoreResult(
                              &base::debug::DumpWithoutCrashing)));
 
-  return new SyncedPrintersManager(profile, std::move(sync_bridge));
+  return SyncedPrintersManager::Create(profile, std::move(sync_bridge))
+      .release();
 }
 
 }  // namespace chromeos
