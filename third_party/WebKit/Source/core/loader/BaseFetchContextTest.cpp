5258aecd8e0ebbff88843fcbe586101a38c4a9ba
Takeshi Yoshino <tyoshino@chromium.org>
2017-07-20 10:11:23 +0000

    Rename ResourceRequestBlockedReason::CSP to kCSP to follow the style guide
    Bug: 
    Change-Id: I72bb8b8b547980e3097c23f1f64fc1da37929774
    Reviewed-on: https://chromium-review.googlesource.com/578698
    Commit-Queue: Takeshi Yoshino <tyoshino@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488185}

--- third_party/WebKit/Source/core/loader/BaseFetchContextTest.cpp
+++ third_party/WebKit/Source/core/loader/BaseFetchContextTest.cpp
@@ -272,7 +272,7 @@ TEST_F(BaseFetchContextTest, RedirectChecksReportedAndEnforcedCSP) {
 
   ResourceLoaderOptions options;
 
-  EXPECT_EQ(ResourceRequestBlockedReason::CSP,
+  EXPECT_EQ(ResourceRequestBlockedReason::kCSP,
             fetch_context_->CanFollowRedirect(
                 Resource::kScript, resource_request, url, options,
                 SecurityViolationReportingPolicy::kReport,
@@ -299,7 +299,7 @@ TEST_F(BaseFetchContextTest, AllowResponseChecksReportedAndEnforcedCSP) {
 
   ResourceLoaderOptions options;
 
-  EXPECT_EQ(ResourceRequestBlockedReason::CSP,
+  EXPECT_EQ(ResourceRequestBlockedReason::kCSP,
             fetch_context_->AllowResponse(Resource::kScript, resource_request,
                                           url, options));
   EXPECT_EQ(2u, policy->violation_reports_sent_.size());
