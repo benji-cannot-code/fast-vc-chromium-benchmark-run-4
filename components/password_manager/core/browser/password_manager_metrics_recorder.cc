046e60f706866a33f9b61fa51df32229ebef3de6
Dominic Battre <battre@chromium.org>
2017-07-20 09:22:01 +0000

    Record UKMs for failing to provisionally save a password form
    Bug: 732846
    Change-Id: I011b4805a15501df0fb32ac3fd6002e44a56d9c2
    Reviewed-on: https://chromium-review.googlesource.com/575242
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488173}

--- components/password_manager/core/browser/password_manager_metrics_recorder.cc
+++ components/password_manager/core/browser/password_manager_metrics_recorder.cc
@@ -16,6 +16,10 @@ typedef autofill::SavePasswordProgressLogger Logger;
 
 namespace password_manager {
 
+// URL Keyed Metrics.
+const char kUkmUserModifiedPasswordField[] = "UserModifiedPasswordField";
+const char kUkmProvisionalSaveFailure[] = "ProvisionalSaveFailure";
+
 PasswordManagerMetricsRecorder::PasswordManagerMetricsRecorder(
     std::unique_ptr<ukm::UkmEntryBuilder> ukm_entry_builder)
     : ukm_entry_builder_(std::move(ukm_entry_builder)) {}
@@ -25,7 +29,7 @@ PasswordManagerMetricsRecorder::PasswordManagerMetricsRecorder(
 
 PasswordManagerMetricsRecorder::~PasswordManagerMetricsRecorder() {
   if (user_modified_password_field_)
-    RecordUkmMetric("UserModifiedPasswordField", 1);
+    RecordUkmMetric(kUkmUserModifiedPasswordField, 1);
 }
 
 PasswordManagerMetricsRecorder& PasswordManagerMetricsRecorder::operator=(
@@ -52,6 +56,7 @@ void PasswordManagerMetricsRecorder::RecordProvisionalSaveFailure(
     BrowserSavePasswordProgressLogger* logger) {
   UMA_HISTOGRAM_ENUMERATION("PasswordManager.ProvisionalSaveFailure", failure,
                             MAX_FAILURE_VALUE);
+  RecordUkmMetric(kUkmProvisionalSaveFailure, static_cast<int64_t>(failure));
 
   if (logger) {
     switch (failure) {
