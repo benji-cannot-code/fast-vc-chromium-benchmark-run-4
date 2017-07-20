5bcb5978169e2cdbf1b09d2e96bc8735cb3f7410
Dmitry Gozman <dgozman@chromium.org>
2017-07-20 20:47:14 +0000

    [DevTools] Support multiple sessions in Target domain
    BUG=590878
    
    Change-Id: Idcebf0336623322813b4cdfac66183d37c28f2bd
    Reviewed-on: https://chromium-review.googlesource.com/573411
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488395}

--- content/browser/devtools/protocol/devtools_protocol_browsertest.cc
+++ content/browser/devtools/protocol/devtools_protocol_browsertest.cc
@@ -1658,6 +1658,8 @@ IN_PROC_BROWSER_TEST_F(DevToolsProtocolTest, TargetDiscovery) {
   EXPECT_TRUE(params->GetBoolean("targetInfo.attached", &is_attached));
   EXPECT_TRUE(is_attached);
   params = WaitForNotification("Target.attachedToTarget", true);
+  std::string session_id;
+  EXPECT_TRUE(params->GetString("sessionId", &session_id));
   EXPECT_TRUE(params->GetString("targetInfo.targetId", &temp));
   EXPECT_EQ(attached_id, temp);
   EXPECT_TRUE(notifications_.empty());
@@ -1668,9 +1670,11 @@ IN_PROC_BROWSER_TEST_F(DevToolsProtocolTest, TargetDiscovery) {
   EXPECT_TRUE(notifications_.empty());
 
   command_params.reset(new base::DictionaryValue());
-  command_params->SetString("targetId", attached_id);
+  command_params->SetString("sessionId", session_id);
   SendCommand("Target.detachFromTarget", std::move(command_params), true);
   params = WaitForNotification("Target.detachedFromTarget", true);
+  EXPECT_TRUE(params->GetString("sessionId", &temp));
+  EXPECT_EQ(session_id, temp);
   EXPECT_TRUE(params->GetString("targetId", &temp));
   EXPECT_EQ(attached_id, temp);
   EXPECT_TRUE(notifications_.empty());
@@ -1738,6 +1742,8 @@ IN_PROC_BROWSER_TEST_F(SitePerProcessDevToolsProtocolTest, TargetNoDiscovery) {
   command_params->SetBoolean("value", true);
   SendCommand("Target.setAttachToFrames", std::move(command_params), false);
   params = WaitForNotification("Target.attachedToTarget", true);
+  std::string session_id;
+  EXPECT_TRUE(params->GetString("sessionId", &session_id));
   EXPECT_TRUE(params->GetString("targetInfo.targetId", &target_id));
   EXPECT_TRUE(params->GetString("targetInfo.type", &temp));
   EXPECT_EQ("iframe", temp);
@@ -1749,10 +1755,13 @@ IN_PROC_BROWSER_TEST_F(SitePerProcessDevToolsProtocolTest, TargetNoDiscovery) {
   params = WaitForNotification("Target.detachedFromTarget", true);
   EXPECT_TRUE(params->GetString("targetId", &temp));
   EXPECT_EQ(target_id, temp);
+  EXPECT_TRUE(params->GetString("sessionId", &temp));
+  EXPECT_EQ(session_id, temp);
 
   // Navigate back to cross-site iframe.
   NavigateFrameToURL(root->child_at(0), cross_site_url);
   params = WaitForNotification("Target.attachedToTarget", true);
+  EXPECT_TRUE(params->GetString("sessionId", &session_id));
   EXPECT_TRUE(params->GetString("targetInfo.targetId", &target_id));
   EXPECT_TRUE(params->GetString("targetInfo.type", &temp));
   EXPECT_EQ("iframe", temp);
@@ -1765,6 +1774,8 @@ IN_PROC_BROWSER_TEST_F(SitePerProcessDevToolsProtocolTest, TargetNoDiscovery) {
   params = WaitForNotification("Target.detachedFromTarget", true);
   EXPECT_TRUE(params->GetString("targetId", &temp));
   EXPECT_EQ(target_id, temp);
+  EXPECT_TRUE(params->GetString("sessionId", &temp));
+  EXPECT_EQ(session_id, temp);
 }
 
 IN_PROC_BROWSER_TEST_F(DevToolsProtocolTest, SetAndGetCookies) {
