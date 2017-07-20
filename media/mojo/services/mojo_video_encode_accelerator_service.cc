2154d1b27e91d76dfe3a9da88fd4425b80552f41
Miguel Casas-Sanchez <mcasas@chromium.org>
2017-07-20 04:16:33 +0000

    VEA mojification: client-side implementation w/unittest
    This CL lands the client-side implementation of the mojom::VEA
    and unit tests for it. It doesn't connect it yet, but the 
    final result can be found in https://crrev.com/c/558846.
    
    Bug: 736517
    Change-Id: I24f29c8893c59e1f16560026e0ed94b611b0651a
    
    TBR=tsepez@ for adding [Sync] to the mojom interfaces
    (that anyway reflects the current IPC).
    
    Change-Id: I24f29c8893c59e1f16560026e0ed94b611b0651a
    Reviewed-on: https://chromium-review.googlesource.com/566145
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488135}

--- media/mojo/services/mojo_video_encode_accelerator_service.cc
+++ media/mojo/services/mojo_video_encode_accelerator_service.cc
@@ -49,7 +49,8 @@ void MojoVideoEncodeAcceleratorService::Initialize(
     const gfx::Size& input_visible_size,
     VideoCodecProfile output_profile,
     uint32_t initial_bitrate,
-    mojom::VideoEncodeAcceleratorClientPtr client) {
+    mojom::VideoEncodeAcceleratorClientPtr client,
+    InitializeCallback success_callback) {
   DVLOG(1) << __func__
            << " input_format=" << VideoPixelFormatToString(input_format)
            << ", input_visible_size=" << input_visible_size.ToString()
@@ -61,6 +62,7 @@ void MojoVideoEncodeAcceleratorService::Initialize(
 
   if (!client) {
     DLOG(ERROR) << __func__ << "null |client|";
+    std::move(success_callback).Run(false);
     return;
   }
   vea_client_ = std::move(client);
@@ -71,6 +73,7 @@ void MojoVideoEncodeAcceleratorService::Initialize(
     DLOG(ERROR) << __func__ << "too large input_visible_size "
                 << input_visible_size.ToString();
     NotifyError(::media::VideoEncodeAccelerator::kInvalidArgumentError);
+    std::move(success_callback).Run(false);
     return;
   }
 
@@ -80,6 +83,7 @@ void MojoVideoEncodeAcceleratorService::Initialize(
   if (!encoder_) {
     DLOG(ERROR) << __func__ << " Error creating or initializing VEA";
     NotifyError(::media::VideoEncodeAccelerator::kPlatformFailureError);
+    std::move(success_callback).Run(false);
     return;
   }
 
@@ -87,6 +91,7 @@ void MojoVideoEncodeAcceleratorService::Initialize(
   // ad-hoc background worker thread, but for the time being this doesn't seem
   // necessary since we're already on a background thread.
 
+  std::move(success_callback).Run(true);
   return;
 }
 
@@ -104,6 +109,7 @@ void MojoVideoEncodeAcceleratorService::Encode(
                 << input_coded_size_.ToString() << ", got "
                 << frame->coded_size().ToString();
     NotifyError(::media::VideoEncodeAccelerator::kInvalidArgumentError);
+    std::move(callback).Run();
     return;
   }
 
