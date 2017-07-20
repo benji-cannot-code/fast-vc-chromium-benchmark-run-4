1c5406cfa8e8a29fe2e909ce7ae53e792c23ea99
Ian Vollick <vollick@chromium.org>
2017-07-20 19:54:40 +0000

    [vr] Introduce linear layout
    With this CL, we add a LinearLayout element whose responsibility is the
    positioning of its child elements.
    
    Bug: None
    Change-Id: Ied896a84a1f5615b3a22e1857469766a21883cad
    Reviewed-on: https://chromium-review.googlesource.com/577193
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488367}

---
+++ chrome/browser/vr/elements/linear_layout.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_VR_ELEMENTS_LINEAR_LAYOUT_H_
+#define CHROME_BROWSER_VR_ELEMENTS_LINEAR_LAYOUT_H_
+
+#include "chrome/browser/vr/elements/ui_element.h"
+
+namespace vr {
+
+class LinearLayout : public UiElement {
+ public:
+  enum Direction { kVertical, kHorizontal };
+
+  explicit LinearLayout(Direction direction);
+  ~LinearLayout() override;
+
+  void set_margin(float margin) { margin_ = margin; }
+
+  void LayOutChildren() override;
+
+ private:
+  Direction direction_;
+  float margin_ = 0.0f;
+};
+
+}  // namespace vr
+
+#endif  // CHROME_BROWSER_VR_ELEMENTS_LINEAR_LAYOUT_H_
