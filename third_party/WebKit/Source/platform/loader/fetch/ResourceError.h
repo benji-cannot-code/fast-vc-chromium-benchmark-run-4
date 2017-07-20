4297f7a5ecfcfc2c4f979ca74b4c2c66865a5add
Yutaka Hirano <yhirano@chromium.org>
2017-07-20 04:09:44 +0000

    Remove WebURLError::is_cancellation
    Now it is equivalent to domain == "net" && reason == ERROR_ABORTED.
    
    Bug: None
    Change-Id: Ia39d123a8a15d79a3efbcc86fd1c70f225c51439
    Reviewed-on: https://chromium-review.googlesource.com/575112
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Takeshi Yoshino <tyoshino@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488132}

--- third_party/WebKit/Source/platform/loader/fetch/ResourceError.h
+++ third_party/WebKit/Source/platform/loader/fetch/ResourceError.h
@@ -67,7 +67,6 @@ class PLATFORM_EXPORT ResourceError final {
   ResourceError()
       : error_code_(0),
         is_null_(true),
-        is_cancellation_(false),
         is_access_check_(false),
         is_timeout_(false),
         stale_copy_in_cache_(false),
@@ -83,7 +82,6 @@ class PLATFORM_EXPORT ResourceError final {
         failing_url_(failing_url),
         localized_description_(localized_description),
         is_null_(false),
-        is_cancellation_(false),
         is_access_check_(false),
         is_timeout_(false),
         stale_copy_in_cache_(false),
@@ -101,10 +99,7 @@ class PLATFORM_EXPORT ResourceError final {
   const String& FailingURL() const { return failing_url_; }
   const String& LocalizedDescription() const { return localized_description_; }
 
-  void SetIsCancellation(bool is_cancellation) {
-    is_cancellation_ = is_cancellation;
-  }
-  bool IsCancellation() const { return is_cancellation_; }
+  bool IsCancellation() const;
 
   void SetIsAccessCheck(bool is_access_check) {
     is_access_check_ = is_access_check;
@@ -146,7 +141,6 @@ class PLATFORM_EXPORT ResourceError final {
   String failing_url_;
   String localized_description_;
   bool is_null_;
-  bool is_cancellation_;
   bool is_access_check_;
   bool is_timeout_;
   bool stale_copy_in_cache_;
