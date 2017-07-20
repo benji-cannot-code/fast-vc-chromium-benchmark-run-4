4e2b6564e8bee01aa08b8d970329dae7ce99f6b6
Tim Sergeant <tsergeant@chromium.org>
2017-07-20 03:07:17 +0000

    MD Bookmarks: Disable keyboard shortcuts when a dialog is open
    Even though our dialogs are modal, it was still possible to execute
    keyboard commands on the background content by clicking on the dialog
    background, which refocuses the document body and allowed commands
    to execute.
    
    Bug: 742972
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I01f9cb3060fd6a91a6fa5a832578f3f245712e0c
    Reviewed-on: https://chromium-review.googlesource.com/575252
    Reviewed-by: calamity <calamity@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488116}

--- chrome/test/data/webui/md_bookmarks/command_manager_test.js
+++ chrome/test/data/webui/md_bookmarks/command_manager_test.js
@@ -59,6 +59,7 @@ suite('<bookmarks-command-manager>', function() {
     replaceBody(commandManager);
     document.body.appendChild(
         document.createElement('bookmarks-toast-manager'));
+    bookmarks.DialogFocusManager.instance_ = null;
   });
 
   test('Copy URL is only active for single URL items', function() {
@@ -165,7 +166,7 @@ suite('<bookmarks-command-manager>', function() {
     commandManager.assertLastCommand('redo');
   });
 
-  test.only('Show In Folder is only available during search', function() {
+  test('Show In Folder is only available during search', function() {
     store.data.selection.items = new Set(['12']);
     store.notifyObservers();
 
@@ -343,6 +344,21 @@ suite('<bookmarks-command-manager>', function() {
       commandItem[element.getAttribute('command')] = element;
     });
     MockInteractions.tap(commandItem[Command.EDIT]);
+    commandManager.assertLastCommand(null);
+  });
+
+  test('keyboard shortcuts are disabled while a dialog is open', function() {
+    assertFalse(bookmarks.DialogFocusManager.getInstance().hasOpenDialog());
+    items = new Set(['12']);
+    store.data.selection.items = items;
+    store.notifyObservers();
+
+    var editKey = cr.isMac ? 'Enter' : 'F2';
+    MockInteractions.pressAndReleaseKeyOn(document.body, '', '', editKey);
+    commandManager.assertLastCommand(Command.EDIT);
+    assertTrue(bookmarks.DialogFocusManager.getInstance().hasOpenDialog());
+
+    MockInteractions.pressAndReleaseKeyOn(document.body, '', '', 'Delete');
     commandManager.assertLastCommand(null);
   });
 });
