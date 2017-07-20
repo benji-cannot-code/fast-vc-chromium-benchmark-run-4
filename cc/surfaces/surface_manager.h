abf41082e2468e6ad2eb8bc19b6cf11acdd3d437
Fady Samuel <fsamuel@chromium.org>
2017-07-20 22:39:44 +0000

    viz; Cleanup SurfaceManager::RequestSurfaceResolution
    SurfaceManager::RequestSurfaceResolution and
    SurfaceDependencyTracker::RequestSurfaceResolution take in an unused
    SurfaceDependencyDeadline parameter. This CL removes it.
    
    Bug: none
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I914cd1f45423ef231f76a5b58a29a178a8bbb941
    Reviewed-on: https://chromium-review.googlesource.com/580528
    Reviewed-by: John Bauman <jbauman@chromium.org>
    Commit-Queue: Fady Samuel <fsamuel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488452}

--- cc/surfaces/surface_manager.h
+++ cc/surfaces/surface_manager.h
@@ -63,8 +63,7 @@ class CC_SURFACES_EXPORT SurfaceManager {
   std::string SurfaceReferencesToString();
 #endif
 
-  void RequestSurfaceResolution(Surface* surface,
-                                SurfaceDependencyDeadline* deadline);
+  void RequestSurfaceResolution(Surface* surface);
 
   // Creates a Surface for the given SurfaceClient. The surface will be
   // destroyed when DestroySurface is called, all of its destruction
