cd0a4bebcde728a023c3e39022203f5cf4975f47
Koji Ishii <kojii@chromium.org>
2017-07-20 08:36:58 +0000

    [LayoutNG] Implement break-after-space style line breaking
    This patch implements break-after-space style line breaking. This style
    is needed for 'white-space:pre-wrap', and when in editing.
    
    The current layout engine handles this in BreakingContext. This patch
    implements the style in LazyTextBreakIterator so that ShapingLineBreaker
    can implement the behavior without additional code.
    
    The current layout engine supports 3 modes:
    1. break-before-space (most cases)
    2. break-after-space (white-space:pre-wrap)
    3. break-after-space + hanging space (editing)
    
    This patch does not support 3rd mode yet, and that editing behavior is
    slightly different from the current layout engine, closer to Edge. The
    editing test failure is caused by this difference. I'll evaluate the
    impact and the needs of the 3rd mode later.
    
    There are more tests that actually pass, but they fail for other
    reasons, such as the lack of quirks mode support.
    
    BUG=636993
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: Iec1b684da4dcf45f10eaa408889ebf77d387e668
    Reviewed-on: https://chromium-review.googlesource.com/574903
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488169}

--- third_party/WebKit/Source/platform/text/TextBreakIteratorTest.cpp
+++ third_party/WebKit/Source/platform/text/TextBreakIteratorTest.cpp
@@ -19,6 +19,7 @@ class TextBreakIteratorTest : public ::testing::Test {
 
   // The expected break positions must be specified UTF-16 character boundaries.
   void MatchLineBreaks(LineBreakType line_break_type,
+                       bool break_after_space,
                        const Vector<int> expected_break_positions) {
     if (test_string_.Is8Bit()) {
       test_string_ = String::Make16BitFrom8BitSource(test_string_.Characters8(),
@@ -26,6 +27,7 @@ class TextBreakIteratorTest : public ::testing::Test {
     }
     LazyLineBreakIterator lazy_break_iterator(test_string_);
     lazy_break_iterator.SetBreakType(line_break_type);
+    lazy_break_iterator.SetBreakAfterSpace(break_after_space);
     TestIsBreakable(expected_break_positions, lazy_break_iterator);
     TestNextBreakOpportunity(expected_break_positions, lazy_break_iterator);
   }
@@ -70,15 +72,22 @@ class TextBreakIteratorTest : public ::testing::Test {
   Vector<int> breaks;                                \
   breaks.Append(kBreaksArray, sizeof(kBreaksArray) / sizeof(*kBreaksArray));
 
-#define MATCH_LINE_BREAKS(LINEBREAKTYPE, ...) \
-  {                                           \
-    DECLARE_BREAKSVECTOR(__VA_ARGS__);        \
-    MatchLineBreaks(LINEBREAKTYPE, breaks);   \
+#define MATCH_LINE_BREAKS(LINEBREAKTYPE, ...)      \
+  {                                                \
+    DECLARE_BREAKSVECTOR(__VA_ARGS__);             \
+    MatchLineBreaks(LINEBREAKTYPE, false, breaks); \
+  }
+
+#define MATCH_BREAK_AFTER_SPACE(LINEBREAKTYPE, ...) \
+  {                                                 \
+    DECLARE_BREAKSVECTOR(__VA_ARGS__);              \
+    MatchLineBreaks(LINEBREAKTYPE, true, breaks);   \
   }
 
 TEST_F(TextBreakIteratorTest, Basic) {
-  SetTestString("a b c");
-  MATCH_LINE_BREAKS(LineBreakType::kNormal, {1, 3, 5});
+  SetTestString("a b  c");
+  MATCH_LINE_BREAKS(LineBreakType::kNormal, {1, 3, 4, 6});
+  MATCH_BREAK_AFTER_SPACE(LineBreakType::kNormal, {2, 5, 6});
 }
 
 TEST_F(TextBreakIteratorTest, LatinPunctuation) {
@@ -87,6 +96,8 @@ TEST_F(TextBreakIteratorTest, LatinPunctuation) {
   MATCH_LINE_BREAKS(LineBreakType::kBreakAll, {2, 4, 6, 8});
   MATCH_LINE_BREAKS(LineBreakType::kBreakCharacter, {1, 2, 3, 4, 5, 6, 7, 8});
   MATCH_LINE_BREAKS(LineBreakType::kKeepAll, {4, 8});
+  MATCH_BREAK_AFTER_SPACE(LineBreakType::kNormal, {5, 8});
+  MATCH_BREAK_AFTER_SPACE(LineBreakType::kBreakAll, {2, 5, 6, 8});
 }
 
 TEST_F(TextBreakIteratorTest, Chinese) {
@@ -95,6 +106,8 @@ TEST_F(TextBreakIteratorTest, Chinese) {
   MATCH_LINE_BREAKS(LineBreakType::kBreakAll, {1, 2, 3, 4, 5});
   MATCH_LINE_BREAKS(LineBreakType::kBreakCharacter, {1, 2, 3, 4, 5});
   MATCH_LINE_BREAKS(LineBreakType::kKeepAll, {5});
+  MATCH_BREAK_AFTER_SPACE(LineBreakType::kNormal, {1, 2, 3, 4, 5});
+  MATCH_BREAK_AFTER_SPACE(LineBreakType::kBreakAll, {1, 2, 3, 4, 5});
 }
 
 TEST_F(TextBreakIteratorTest, ChineseMixed) {
@@ -104,6 +117,19 @@ TEST_F(TextBreakIteratorTest, ChineseMixed) {
   MATCH_LINE_BREAKS(LineBreakType::kBreakCharacter,
                     {1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
   MATCH_LINE_BREAKS(LineBreakType::kKeepAll, {1, 4, 9, 10});
+  MATCH_BREAK_AFTER_SPACE(LineBreakType::kNormal, {1, 4, 5, 7, 9, 10});
+  MATCH_BREAK_AFTER_SPACE(LineBreakType::kBreakAll, {1, 4, 5, 6, 7, 9, 10});
+}
+
+TEST_F(TextBreakIteratorTest, ChineseSpaces) {
+  SetTestString("標  萬  a  國");
+  MATCH_LINE_BREAKS(LineBreakType::kNormal, {1, 2, 4, 5, 7, 8, 10});
+  MATCH_LINE_BREAKS(LineBreakType::kBreakAll, {1, 2, 4, 5, 7, 8, 10});
+  MATCH_LINE_BREAKS(LineBreakType::kBreakCharacter,
+                    {1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+  MATCH_LINE_BREAKS(LineBreakType::kKeepAll, {1, 2, 4, 5, 7, 8, 10});
+  MATCH_BREAK_AFTER_SPACE(LineBreakType::kNormal, {3, 6, 9, 10});
+  MATCH_BREAK_AFTER_SPACE(LineBreakType::kBreakAll, {3, 6, 9, 10});
 }
 
 TEST_F(TextBreakIteratorTest, KeepEmojiZWJFamilyIsolate) {
@@ -112,6 +138,8 @@ TEST_F(TextBreakIteratorTest, KeepEmojiZWJFamilyIsolate) {
   MATCH_LINE_BREAKS(LineBreakType::kBreakAll, {11});
   MATCH_LINE_BREAKS(LineBreakType::kBreakCharacter, {11});
   MATCH_LINE_BREAKS(LineBreakType::kKeepAll, {11});
+  MATCH_BREAK_AFTER_SPACE(LineBreakType::kNormal, {11});
+  MATCH_BREAK_AFTER_SPACE(LineBreakType::kBreakAll, {11});
 }
 
 TEST_F(TextBreakIteratorTest, KeepEmojiModifierSequenceIsolate) {
@@ -120,6 +148,8 @@ TEST_F(TextBreakIteratorTest, KeepEmojiModifierSequenceIsolate) {
   MATCH_LINE_BREAKS(LineBreakType::kBreakAll, {3});
   MATCH_LINE_BREAKS(LineBreakType::kBreakCharacter, {3});
   MATCH_LINE_BREAKS(LineBreakType::kKeepAll, {3});
+  MATCH_BREAK_AFTER_SPACE(LineBreakType::kNormal, {3});
+  MATCH_BREAK_AFTER_SPACE(LineBreakType::kBreakAll, {3});
 }
 
 TEST_F(TextBreakIteratorTest, KeepEmojiZWJSequence) {
@@ -130,6 +160,8 @@ TEST_F(TextBreakIteratorTest, KeepEmojiZWJSequence) {
   MATCH_LINE_BREAKS(LineBreakType::kBreakCharacter,
                     {1, 2, 3, 4, 15, 16, 17, 18, 19});
   MATCH_LINE_BREAKS(LineBreakType::kKeepAll, {3, 15, 19});
+  MATCH_BREAK_AFTER_SPACE(LineBreakType::kNormal, {4, 16, 19});
+  MATCH_BREAK_AFTER_SPACE(LineBreakType::kBreakAll, {1, 2, 4, 16, 17, 18, 19});
 }
 
 TEST_F(TextBreakIteratorTest, KeepEmojiModifierSequence) {
@@ -139,6 +171,8 @@ TEST_F(TextBreakIteratorTest, KeepEmojiModifierSequence) {
   MATCH_LINE_BREAKS(LineBreakType::kBreakCharacter,
                     {1, 2, 3, 4, 7, 8, 9, 10, 11});
   MATCH_LINE_BREAKS(LineBreakType::kKeepAll, {3, 7, 11});
+  MATCH_BREAK_AFTER_SPACE(LineBreakType::kNormal, {4, 8, 11});
+  MATCH_BREAK_AFTER_SPACE(LineBreakType::kBreakAll, {1, 2, 4, 8, 9, 10, 11});
 }
 
 }  // namespace blink
