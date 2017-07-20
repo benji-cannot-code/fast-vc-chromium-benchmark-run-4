c6cc49f13f39cb571e1dd351330cb66b6080fb6b
Wenzhao Zang <wzang@chromium.org>
2017-07-20 19:49:21 +0000

    Fix several accessibility issues on login screen
    (1) Remove extra focus on the disabled dropdown menu button.
    
    (2) Make password field focused only once instead of three times.
    
    (3) Assign the user name as ARIA label for the entire pod.
    
    (4) Skip image until more descriptive texts are available.
    
    Bug: 746568
    Change-Id: I4ed9918947a3a7f03fe84ebbf64ab94be7e88475
    Reviewed-on: https://chromium-review.googlesource.com/578459
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488363}

--- ui/login/account_picker/md_user_pod_row.js
+++ ui/login/account_picker/md_user_pod_row.js
@@ -1161,14 +1161,14 @@ cr.define('login', function() {
      */
     get statsMapElements() {
       return {
-          'BrowsingHistory':
-              this.querySelector('.action-box-remove-user-warning-history'),
-          'Passwords':
-              this.querySelector('.action-box-remove-user-warning-passwords'),
-          'Bookmarks':
-              this.querySelector('.action-box-remove-user-warning-bookmarks'),
-          'Autofill':
-              this.querySelector('.action-box-remove-user-warning-autofill')
+        'BrowsingHistory':
+            this.querySelector('.action-box-remove-user-warning-history'),
+            'Passwords':
+                this.querySelector('.action-box-remove-user-warning-passwords'),
+            'Bookmarks':
+                this.querySelector('.action-box-remove-user-warning-bookmarks'),
+            'Autofill':
+                this.querySelector('.action-box-remove-user-warning-autofill')
       }
     },
 
@@ -1244,11 +1244,8 @@ cr.define('login', function() {
         this.passwordLabelElement.textContent = this.authValue;
 
       this.updateActionBoxArea();
-
-      this.passwordElement.setAttribute('aria-label', loadTimeData.getStringF(
-        'passwordFieldAccessibleName', this.user_.emailAddress));
-
       this.customizeUserPodPerUserType();
+      this.updateAriaLabels_();
     },
 
     updateActionBoxArea: function() {
@@ -1259,12 +1256,6 @@ cr.define('login', function() {
 
       this.actionBoxMenuRemoveElement.hidden = !this.user_.canRemove;
 
-      this.actionBoxAreaElement.setAttribute(
-          'aria-label', loadTimeData.getStringF(
-              'podMenuButtonAccessibleName', this.user_.emailAddress));
-      this.actionBoxMenuRemoveElement.setAttribute(
-          'aria-label', loadTimeData.getString(
-               'podMenuRemoveItemAccessibleName'));
       this.actionBoxMenuTitleNameElement.textContent = this.user_.isOwner ?
           loadTimeData.getStringF('ownerUserPattern', this.user_.displayName) :
           this.user_.displayName;
@@ -1298,6 +1289,27 @@ cr.define('login', function() {
       } else if (this.user_.isApp) {
         this.setUserPodIconType('app');
       }
+    },
+
+    /**
+     * Updates ARIA labels and sets hidden states. All updates related to ARIA
+     * should go here.
+     * @private
+     */
+    updateAriaLabels_: function() {
+      this.setAttribute('aria-label', this.user_.displayName);
+      this.querySelector('.password-container')
+          .setAttribute(
+              'aria-label',
+              loadTimeData.getStringF(
+                  'passwordFieldAccessibleName', this.user_.emailAddress));
+      this.actionBoxAreaElement.setAttribute(
+          'aria-label',
+          loadTimeData.getStringF(
+              'podMenuButtonAccessibleName', this.user_.emailAddress));
+      this.actionBoxMenuRemoveElement.setAttribute(
+          'aria-label',
+          loadTimeData.getString('podMenuRemoveItemAccessibleName'));
     },
 
     isPinReady: function() {
