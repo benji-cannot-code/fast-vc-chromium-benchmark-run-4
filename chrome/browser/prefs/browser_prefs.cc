fdafda71aa13c4439e3788dd690ce8019471dcd5
Catherine Chung <catherinechung@google.com>
2017-07-20 17:38:23 +0000

    Add new tab feature engagement tracker
    To use the FeatureEngagementTracker to observe the New Tab and related
    actions, the NewTabFeatureEngagementTracker class was created.
    
    Things that were changed in this CL:
    * Added the NewTabInProductHelp trial to the fieldtrial json
    * Added a pref to track the user's total active session time
    * Created a NewTabFeatureEngagementTracker class, which contains
    methods to notify the IPH server when events happen and if the help
    UI should be triggered.
    
    Related CLs:
    * The Observer for DesktopSessionDurationTracker
      http://crrev.com/d69292f
    * Event constants that are used in NewTabFeatureEngagementTracker
      http://crrev.com/bdf5c97
    * Using the NewTabFeatureEngagementTracker
      https://chromium-review.googlesource.com/c/549074/
    * Addition to fieldtrial_testing.json
      https://chromium-review.googlesource.com/c/566335/
    
    Bug: 734132
    Change-Id: I8654ef001f3fb50fbd8bb8c4f215dffcfe403762
    Reviewed-on: https://chromium-review.googlesource.com/549057
    Commit-Queue: Catherine Chung <catherinechung@google.com>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Pam Greene <pam@chromium.org>
    Reviewed-by: Anthony Vallee-Dubois <anthonyvd@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488293}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -263,6 +263,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/desktop_ios_promotion/desktop_ios_promotion_util.h"
 #endif
 
+#if !defined(OS_ANDROID) && !defined(OS_CHROMEOS) && !defined(OS_MACOSX)
+#include "chrome/browser/feature_engagement_tracker/new_tab/new_tab_tracker.h"
+#endif
+
 #if !defined(OS_ANDROID) && !defined(OS_CHROMEOS)
 #include "chrome/browser/ui/startup/default_browser_prompt.h"
 #endif
@@ -581,6 +585,10 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
 
 #if !defined(OS_ANDROID) && !defined(OS_CHROMEOS)
   default_apps::RegisterProfilePrefs(registry);
+#endif
+
+#if !defined(OS_ANDROID) && !defined(OS_CHROMEOS) && !defined(OS_MACOSX)
+  feature_engagement_tracker::NewTabTracker::RegisterProfilePrefs(registry);
 #endif
 
 #if defined(OS_CHROMEOS)
