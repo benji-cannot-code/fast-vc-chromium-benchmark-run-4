3ecd699885ae277a2e825cef6f75976dfaa602cb
Ian Kilpatrick <ikilpatrick@chromium.org>
2017-07-20 22:37:14 +0000

    [LayoutNG] Fix margins on html element.
    Existing layout is bizarre. Margins for a child are calculated inside the
    childs layout. This includes the auto margins calculation as far as we
    can tell.
    
    This just does the "simple" margin calculation (no auto etc) as auto margins
    etc only occur within block layout, which we now control.
    
    We may have to refine this to include more behaviour if we discover other
    cases we don't cover.
    
    Bug: 635619
    Change-Id: I944ae3435074441f06e118c59f8d2ad317227b32
    Reviewed-on: https://chromium-review.googlesource.com/580291
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488451}

--- third_party/WebKit/Source/core/layout/ng/layout_ng_block_flow.cc
+++ third_party/WebKit/Source/core/layout/ng/layout_ng_block_flow.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/ng/ng_constraint_space.h"
 #include "core/layout/ng/ng_fragment.h"
 #include "core/layout/ng/ng_layout_result.h"
+#include "core/layout/ng/ng_length_utils.h"
 
 namespace blink {
 
@@ -42,9 +43,28 @@ void LayoutNGBlockFlow::UpdateBlockLayout(bool relayout_children) {
     SetLogicalTop(computed_values.position_);
   }
 
+  // We need to update our margins as these are calculated once and stored in
+  // LayoutBox::margin_box_outsets_. Typically this happens within
+  // UpdateLogicalWidth and UpdateLogicalHeight.
+  //
+  // This primarily fixes cases where we are embedded inside another layout,
+  // for example LayoutView, LayoutFlexibleBox, etc.
+  UpdateMargins(*constraint_space);
+
   for (NGOutOfFlowPositionedDescendant descendant :
        result->OutOfFlowPositionedDescendants())
     descendant.node.UseOldOutOfFlowPositioning();
+}
+
+void LayoutNGBlockFlow::UpdateMargins(
+    const NGConstraintSpace& constraint_space) {
+  NGBoxStrut margins =
+      ComputeMargins(constraint_space, StyleRef(),
+                     constraint_space.WritingMode(), StyleRef().Direction());
+  SetMarginBefore(margins.block_start);
+  SetMarginAfter(margins.block_end);
+  SetMarginStart(margins.inline_start);
+  SetMarginEnd(margins.inline_end);
 }
 
 NGInlineNodeData& LayoutNGBlockFlow::GetNGInlineNodeData() const {
