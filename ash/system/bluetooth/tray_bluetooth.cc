7a6f589520e18aa5de110e2e427966538235bb5e
Leslie Watkins <lesliewatkins@chromium.org>
2017-07-20 16:20:50 +0000

    Add a row in the network tray to inform users to turn Bluetooth on to enable Tether.
    Rename TrayDetailsView::InfoLabel to TrayInfoLabel and moved it to its own file. It also now subclasses ActionableView instead of View, so it can be clickable. TrayInfoLabel::Delegate keeps track of whether or not the label is clickable, and handles clicks.
    
    Reland of https://codereview.chromium.org/2957043002/
    
    This reverts commit fc8ab64a4bd1d37a7eee1d50629750e79ba6553e.
    
    Bug: 672263, 735642, 740671
    Change-Id: I7957bc934e01e226ee588939a21d1fc4afc0bdb3
    Reviewed-on: https://chromium-review.googlesource.com/578142
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488263}

--- ash/system/bluetooth/tray_bluetooth.cc
+++ ash/system/bluetooth/tray_bluetooth.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/system_tray_notifier.h"
 #include "ash/system/tray/tray_constants.h"
 #include "ash/system/tray/tray_details_view.h"
+#include "ash/system/tray/tray_info_label.h"
 #include "ash/system/tray/tray_item_more.h"
 #include "ash/system/tray/tray_popup_item_style.h"
 #include "ash/system/tray/tray_popup_utils.h"
@@ -315,8 +316,8 @@ class BluetoothDetailedView : public TrayDetailsView {
 
     // Show user Bluetooth state if there is no bluetooth devices in list.
     if (device_map_.size() == 0 && bluetooth_available && bluetooth_enabled) {
-      scroll_content()->AddChildView(
-          new InfoLabel(IDS_ASH_STATUS_TRAY_BLUETOOTH_DISCOVERING));
+      scroll_content()->AddChildView(new TrayInfoLabel(
+          nullptr /* delegate */, IDS_ASH_STATUS_TRAY_BLUETOOTH_DISCOVERING));
     }
 
     // Focus the device which was focused before the device-list update.
