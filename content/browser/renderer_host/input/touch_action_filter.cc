d84e48b72b1a0e16479d83bae663e3f5bb977c1b
Hayley Ferr <hayleyferr@chromium.org>
2017-07-20 17:47:00 +0000

    Histogram for gesture events being dropped based on allowed touch action.
    Added an UMA_HISTOGRAM_BOOLEAN to report how often a gesture event is
    or is not dropped based on the current touch action state not
    matching the gesture event.
    
    Bug: 745718
    Change-Id: Ib1c9f91cfcaa846f3e8bba9a7b1c87f698b2a962
    Reviewed-on: https://chromium-review.googlesource.com/575071
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488296}

--- content/browser/renderer_host/input/touch_action_filter.cc
+++ content/browser/renderer_host/input/touch_action_filter.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <math.h>
 
 #include "base/logging.h"
+#include "base/metrics/histogram_macros.h"
 #include "third_party/WebKit/public/platform/WebGestureEvent.h"
 
 using blink::WebInputEvent;
@@ -24,6 +25,12 @@ bool IsYAxisActionDisallowed(cc::TouchAction action) {
 
 bool IsXAxisActionDisallowed(cc::TouchAction action) {
   return (action & cc::kTouchActionPanY) && !(action & cc::kTouchActionPanX);
+}
+
+// Report how often the gesture event is or is not dropped due to the current
+// allowed touch action state not matching the gesture event.
+void ReportGestureEventFiltered(bool event_filtered) {
+  UMA_HISTOGRAM_BOOLEAN("TouchAction.GestureEventFiltered", event_filtered);
 }
 
 }  // namespace
@@ -70,6 +77,7 @@ bool TouchActionFilter::FilterGestureEvent(WebGestureEvent* gesture_event) {
       break;
 
     case WebInputEvent::kGestureFlingStart:
+      ReportGestureEventFiltered(suppress_manipulation_events_);
       // Touchscreen flings should always have non-zero velocity.
       DCHECK(gesture_event->data.fling_start.velocity_x ||
              gesture_event->data.fling_start.velocity_y);
@@ -90,11 +98,13 @@ bool TouchActionFilter::FilterGestureEvent(WebGestureEvent* gesture_event) {
       return FilterManipulationEventAndResetState();
 
     case WebInputEvent::kGestureScrollEnd:
+      ReportGestureEventFiltered(suppress_manipulation_events_);
       return FilterManipulationEventAndResetState();
 
     case WebInputEvent::kGesturePinchBegin:
     case WebInputEvent::kGesturePinchUpdate:
     case WebInputEvent::kGesturePinchEnd:
+      ReportGestureEventFiltered(suppress_manipulation_events_);
       return suppress_manipulation_events_;
 
     // The double tap gesture is a tap ending event. If a double tap gesture is
