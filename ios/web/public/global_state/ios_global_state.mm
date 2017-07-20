f9b8c626790741cae17fb0e5e0d33a67c4fefd3a
Sylvain Defresne <sdefresne@chromium.org>
2017-07-20 08:10:29 +0000

    Cleanup object destruction order during application shutdown.
    The NetworkChangeNotifier owns an object that checks it is destroyed
    on the correct thread (HistogramWatcher). This requires the object
    to be destroyed before the thread is stopped, so add helper function
    to control when the NetworkChangeNotifier is destroyed.
    
    For the same reason, do it also for MessageLoopForUI.
    
    Call the new function from WebMainLoop destructor. This fixes a new
    regression that was introduced during the refactoring of this code
    that moved the object destruction till after the "main" function
    returns (as they are owned by "static" globals).
    
    To ensure the destruction happens in the same order as before the
    refactoring in https://chromium-review.googlesource.com/558268,
    use base::ScopedClosureRunner to register in which order the helper
    functions are called.
    
    As CroNet code was leaking those objects, do not add a call to the
    destroy function there.
    
    Bug: 746478
    Change-Id: If2e5d1dea15c73bccd973222f9811660a5687b77
    Reviewed-on: https://chromium-review.googlesource.com/577851
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488164}

--- ios/web/public/global_state/ios_global_state.mm
+++ ios/web/public/global_state/ios_global_state.mm
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 base::AtExitManager* g_exit_manager = nullptr;
+base::MessageLoopForUI* g_message_loop = nullptr;
+net::NetworkChangeNotifier* g_network_change_notifer = nullptr;
 
 base::TaskScheduler::InitParams GetDefaultTaskSchedulerInitParams() {
   using StandbyThreadPolicy =
@@ -65,26 +67,32 @@ void Create(const CreateParams& create_params) {
 }
 
 void BuildMessageLoop() {
-  static std::unique_ptr<base::MessageLoopForUI> main_message_loop;
-
   static dispatch_once_t once_token;
   dispatch_once(&once_token, ^{
     // Create a MessageLoop if one does not already exist for the current
     // thread.
     if (!base::MessageLoop::current()) {
-      main_message_loop = base::MakeUnique<base::MessageLoopForUI>();
+      g_message_loop = new base::MessageLoopForUI();
     }
     base::MessageLoopForUI::current()->Attach();
   });
 }
 
-void CreateNetworkChangeNotifier() {
-  static std::unique_ptr<net::NetworkChangeNotifier> network_change_notifier;
+void DestroyMessageLoop() {
+  delete g_message_loop;
+  g_message_loop = nullptr;
+}
 
+void CreateNetworkChangeNotifier() {
   static dispatch_once_t once_token;
   dispatch_once(&once_token, ^{
-    network_change_notifier.reset(net::NetworkChangeNotifier::Create());
+    g_network_change_notifer = net::NetworkChangeNotifier::Create();
   });
+}
+
+void DestroyNetworkChangeNotifier() {
+  delete g_network_change_notifer;
+  g_network_change_notifer = nullptr;
 }
 
 void StartTaskScheduler(base::TaskScheduler::InitParams* params) {
