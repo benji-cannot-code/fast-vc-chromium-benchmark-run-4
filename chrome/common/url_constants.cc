a2617aa47418a9e217cb48cbe1886e6c28dd511d
Brett Wilson <brettw@chromium.org>
2017-07-20 21:06:16 +0000

    Add a skeleton WebUI for saving memory profiles.
    This is only populated with dummy data for now, but provides a place
    to hook up the process list and dumping function in the future.
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I24934d3052dd8e36ad4a7b1372a5ef557ce716d4
    Reviewed-on: https://chromium-review.googlesource.com/578559
    Commit-Queue: Brett Wilson <brettw@chromium.org>
    Reviewed-by: Albert J. Wong <ajwong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488401}

--- chrome/common/url_constants.cc
+++ chrome/common/url_constants.cc
@@ -153,6 +153,10 @@ const char kChromeUITabModalConfirmDialogURL[] =
     "chrome://tab-modal-confirm-dialog/";
 #endif
 
+#if BUILDFLAG(ENABLE_OOP_HEAP_PROFILING)
+extern const char kChromeUIMemoryInternalsURL[] = "chrome://memory-internals/";
+#endif
+
 #if BUILDFLAG(ENABLE_WEBRTC)
 const char kChromeUIWebRtcLogsURL[] = "chrome://webrtc-logs/";
 #endif
@@ -347,6 +351,10 @@ const char kChromeUIMetroFlowHost[] = "make-metro";
 
 #if (defined(OS_LINUX) && defined(TOOLKIT_VIEWS)) || defined(USE_AURA)
 const char kChromeUITabModalConfirmDialogHost[] = "tab-modal-confirm-dialog";
+#endif
+
+#if BUILDFLAG(ENABLE_OOP_HEAP_PROFILING)
+extern const char kChromeUIMemoryInternalsHost[] = "memory-internals";
 #endif
 
 #if BUILDFLAG(ENABLE_WEBRTC)
