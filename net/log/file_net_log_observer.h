e5b0fd5f7b1f385d1b933669b32fcd6adf46a0fa
Eric Roman <eroman@chromium.org>
2017-07-20 20:06:03 +0000

    Enable bounded mode logging on chrome://net-export.
    There is an option to configure the maximum size of the NetLog. The
    default is 100MiB.
    
    This is a re-land that was originally reviewed at https://chromium-review.googlesource.com/c/571514/
    
    TBR=xunjieli@chromium.org
    
    Bug: 679030
    Change-Id: Ibf4fd80bca9ecd2d7899bc7039a930bbfc70ffa0
    Reviewed-on: https://chromium-review.googlesource.com/580030
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Commit-Queue: Eric Roman <eroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488371}

--- net/log/file_net_log_observer.h
+++ net/log/file_net_log_observer.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_LOG_FILE_NET_LOG_OBSERVER_H_
 #define NET_LOG_FILE_NET_LOG_OBSERVER_H_
 
+#include <limits>
 #include <memory>
 
 #include "base/macros.h"
@@ -37,7 +38,7 @@ class NetLogCaptureMode;
 class NET_EXPORT FileNetLogObserver : public NetLog::ThreadSafeObserver {
  public:
   // Special value meaning "can use an unlimited number of bytes".
-  static const size_t kNoLimit;
+  static constexpr size_t kNoLimit = std::numeric_limits<size_t>::max();
 
   // Creates an instance of FileNetLogObserver that writes observed netlog
   // events to |log_path|.
