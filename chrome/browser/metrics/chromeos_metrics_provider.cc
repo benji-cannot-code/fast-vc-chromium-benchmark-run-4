2906c12d403d23a7fd11ee190f8418e4d75b4079
Ilya Sherman <isherman@chromium.org>
2017-07-20 20:16:23 +0000

    [Task Scheduler] Allow the ChromeOSMetricsProvider to wait.
    The InitTaskGetHardwareClassOnBackgroundThread() code calls into
    StatisticsProvider::GetMachineStatistic(), which calls
    WaitForStatisticsLoaded(), which uses a WaitableEvent.
    
    Bug: 743056
    Change-Id: I3da6e975b1d774f963ca4cdb9242878357bd2b78
    Reviewed-on: https://chromium-review.googlesource.com/572740
    Reviewed-by: Alexei Svitkine (slow) <asvitkine@chromium.org>
    Commit-Queue: Alexei Svitkine (slow) <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488378}

--- chrome/browser/metrics/chromeos_metrics_provider.cc
+++ chrome/browser/metrics/chromeos_metrics_provider.cc
@@ -174,7 +174,8 @@ void ChromeOSMetricsProvider::InitTaskGetHardwareClass(
   // the UI thread.
   base::PostTaskWithTraitsAndReply(
       FROM_HERE,
-      {base::MayBlock(), base::TaskPriority::BACKGROUND,
+      {base::MayBlock(), base::WithBaseSyncPrimitives(),
+       base::TaskPriority::BACKGROUND,
        base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN},
       base::BindOnce(
           &ChromeOSMetricsProvider::InitTaskGetHardwareClassOnBackgroundThread,
