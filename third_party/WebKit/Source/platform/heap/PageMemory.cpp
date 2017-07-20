f27861ccf7357caab395294a1681592e6c59726f
Kent Tamura <tkent@chromium.org>
2017-07-20 06:31:29 +0000

    Remove unnecessary #includes from Assertions.h
    Change-Id: I9d9a0ff0c56a10923e393d4eef7ccde7b33c4a2b
    Reviewed-on: https://chromium-review.googlesource.com/578532
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488149}

--- third_party/WebKit/Source/platform/heap/PageMemory.cpp
+++ third_party/WebKit/Source/platform/heap/PageMemory.cpp
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "platform/heap/PageMemory.h"
 
+#include "base/allocator/partition_allocator/oom.h"
 #include "platform/heap/Heap.h"
 #include "platform/wtf/AddressSanitizer.h"
 #include "platform/wtf/Assertions.h"
--- third_party/WebKit/Source/platform/wtf/allocator/Partitions.cpp
+++ third_party/WebKit/Source/platform/wtf/allocator/Partitions.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "platform/wtf/allocator/Partitions.h"
 
+#include "base/allocator/partition_allocator/oom.h"
 #include "base/allocator/partition_allocator/page_allocator.h"
 #include "base/debug/alias.h"
 #include "platform/wtf/allocator/PartitionAllocator.h"
