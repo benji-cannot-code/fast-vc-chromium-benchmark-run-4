bc63cf56a1f14abf18bdb958fd4fcd51f51ea45a
Colin Blundell <blundell@chromium.org>
2017-07-20 17:00:09 +0000

    [iOS] Add chrome://suggestions WebUI
    This WebUI is already present on other platforms. It will be useful for
    testing upcoming refactoring of the suggestions component.
    
    Change-Id: I303b02c04b0f90c34dd72bf0157199a60a26eb97
    Reviewed-on: https://chromium-review.googlesource.com/574592
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488274}

--- ios/chrome/browser/ui/webui/chrome_web_ui_ios_controller_factory.mm
+++ ios/chrome/browser/ui/webui/chrome_web_ui_ios_controller_factory.mm
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/ui/webui/omaha_ui.h"
 #include "ios/chrome/browser/ui/webui/physical_web_ui.h"
 #include "ios/chrome/browser/ui/webui/signin_internals_ui_ios.h"
+#include "ios/chrome/browser/ui/webui/suggestions_ui.h"
 #include "ios/chrome/browser/ui/webui/sync_internals/sync_internals_ui.h"
 #include "ios/chrome/browser/ui/webui/terms_ui.h"
 #include "ios/chrome/browser/ui/webui/version_ui.h"
@@ -82,6 +83,8 @@ WebUIIOSFactoryFunction GetWebUIIOSFactoryFunction(WebUIIOS* web_ui,
   }
   if (url_host == kChromeUISignInInternalsHost)
     return &NewWebUIIOS<SignInInternalsUIIOS>;
+  if (url.host_piece() == kChromeUISuggestionsHost)
+    return &NewWebUIIOS<suggestions::SuggestionsUI>;
   if (url_host == kChromeUISyncInternalsHost)
     return &NewWebUIIOS<SyncInternalsUI>;
   if (url_host == kChromeUITermsHost)
