30fa32041413522874727493f39a0f686cacfdb3
Sammie Quon <sammiequon@google.com>
2017-07-21 00:01:15 +0000

    cros: Add separator between help and settings on palette menu.
    As per UI review request to look the seperator for the tiles in the system tray bubble.
    
    Test: manual
    Bug: 747148
    Change-Id: I2259bc498fb571d1c44d7e42a9970168c5872436
    Reviewed-on: https://chromium-review.googlesource.com/580636
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488499}

--- ash/system/palette/palette_tray.cc
+++ ash/system/palette/palette_tray.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/tray_container.h"
 #include "ash/system/tray/tray_popup_header_button.h"
 #include "ash/system/tray/tray_popup_item_style.h"
+#include "ash/system/tray/tray_popup_utils.h"
 #include "base/metrics/histogram_macros.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
@@ -86,6 +87,8 @@ class TitleView : public views::View, public views::ButtonListener {
     // TODO(tdanderson|jdufault): Use TriView to handle the layout of the title.
     // See crbug.com/614453.
     auto* box_layout = new views::BoxLayout(views::BoxLayout::kHorizontal);
+    box_layout->set_cross_axis_alignment(
+        views::BoxLayout::CROSS_AXIS_ALIGNMENT_CENTER);
     SetLayoutManager(box_layout);
 
     auto* title_label =
@@ -103,6 +106,7 @@ class TitleView : public views::View, public views::ButtonListener {
                              kSystemMenuSettingsIcon, IDS_ASH_PALETTE_SETTINGS);
 
     AddChildView(help_button_);
+    AddChildView(TrayPopupUtils::CreateVerticalSeparator());
     AddChildView(settings_button_);
   }
 
