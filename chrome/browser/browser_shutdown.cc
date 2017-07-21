e25fbf3d34dbeff17d4e8914be07dd6e2444ec32
Gabriel Charette <gab@chromium.org>
2017-07-21 15:24:15 +0000

    Migrate browser_shutdown.cc off the FILE thread.
    R=thakis@chromium.org
    
    Bug: 689520
    Change-Id: I5ea04e1c89cf5694071aa1c7d0d6ff5caaca4f88
    Reviewed-on: https://chromium-review.googlesource.com/580505
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488659}

--- chrome/browser/browser_shutdown.cc
+++ chrome/browser/browser_shutdown.cc
@@ -18,7 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/path_service.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/thread.h"
+#include "base/threading/thread_restrictions.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "chrome/browser/about_flags.h"
@@ -35,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
 #include "components/tracing/common/tracing_switches.h"
-#include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/tracing_controller.h"
 #include "printing/features/features.h"
@@ -69,7 +70,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using base::Time;
 using base::TimeDelta;
-using content::BrowserThread;
 
 namespace browser_shutdown {
 namespace {
@@ -82,7 +82,13 @@ ShutdownType g_shutdown_type = NOT_VALID;
 int g_shutdown_num_processes;
 int g_shutdown_num_processes_slow;
 
-const char kShutdownMsFile[] = "chrome_shutdown_ms.txt";
+constexpr char kShutdownMsFile[] = "chrome_shutdown_ms.txt";
+
+base::FilePath GetShutdownMsPath() {
+  base::FilePath shutdown_ms_file;
+  PathService::Get(chrome::DIR_USER_DATA, &shutdown_ms_file);
+  return shutdown_ms_file.AppendASCII(kShutdownMsFile);
+}
 
 const char* ToShutdownTypeString(ShutdownType type) {
   switch (type) {
@@ -143,12 +149,6 @@ void OnShutdownStarting(ShutdownType type) {
     if (!i.GetCurrentValue()->FastShutdownIfPossible())
       ++g_shutdown_num_processes_slow;
   }
-}
-
-base::FilePath GetShutdownMsPath() {
-  base::FilePath shutdown_ms_file;
-  PathService::Get(chrome::DIR_USER_DATA, &shutdown_ms_file);
-  return shutdown_ms_file.AppendASCII(kShutdownMsFile);
 }
 
 #if !defined(OS_ANDROID)
@@ -274,6 +274,10 @@ void ShutdownPostThreadsStop(int shutdown_flags) {
         base::Int64ToString(shutdown_delta.InMilliseconds());
     int len = static_cast<int>(shutdown_ms.length()) + 1;
     base::FilePath shutdown_ms_file = GetShutdownMsPath();
+    // Note: ReadLastShutdownFile() is done as a BLOCK_SHUTDOWN task so there's
+    // an implicit sequencing between it and this write which happens after
+    // threads have been stopped (and thus TaskScheduler::Shutdown() is
+    // complete).
     base::WriteFile(shutdown_ms_file, shutdown_ms.c_str(), len);
   }
 
@@ -286,7 +290,7 @@ void ShutdownPostThreadsStop(int shutdown_flags) {
 void ReadLastShutdownFile(ShutdownType type,
                           int num_procs,
                           int num_procs_slow) {
-  DCHECK_CURRENTLY_ON(BrowserThread::FILE);
+  base::ThreadRestrictions::AssertIOAllowed();
 
   base::FilePath shutdown_ms_file = GetShutdownMsPath();
   std::string shutdown_ms_str;
@@ -333,9 +337,10 @@ void ReadLastShutdownInfo() {
 
   UMA_HISTOGRAM_ENUMERATION("Shutdown.ShutdownType", type, kNumShutdownTypes);
 
-  // Read and delete the file on the file thread.
-  BrowserThread::PostTask(
-      BrowserThread::FILE, FROM_HERE,
+  base::PostTaskWithTraits(
+      FROM_HERE,
+      {base::MayBlock(), base::TaskPriority::BACKGROUND,
+       base::TaskShutdownBehavior::BLOCK_SHUTDOWN},
       base::BindOnce(&ReadLastShutdownFile, type, num_procs, num_procs_slow));
 }
 
