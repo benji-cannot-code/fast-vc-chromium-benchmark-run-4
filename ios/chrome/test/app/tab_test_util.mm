dd0a59aab359781c31a60a0b6844a70352ba1c13
Eugene But <eugenebut@google.com>
2017-07-21 21:36:38 +0000

    Removed -[CRWWebController handleLowMemory] call.
    Disabling and enabling web usage has the same effect of evicting tabs.
    
    Bug: 732525
    Change-Id: I2ecafe2ddaf419aaeea81500f94d8b5893dee95e
    Reviewed-on: https://chromium-review.googlesource.com/581848
    Reviewed-by: Mike Baxley <baxley@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488755}

--- ios/chrome/test/app/tab_test_util.mm
+++ ios/chrome/test/app/tab_test_util.mm
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/tabs/tab_strip_controller_private.h"
 #import "ios/chrome/test/app/chrome_test_util.h"
 #import "ios/testing/wait_util.h"
-#import "ios/web/web_state/ui/crw_web_controller.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -150,11 +149,9 @@ void EvictOtherTabModelTabs() {
       IsIncognitoMode()
           ? [GetMainController().browserViewInformation mainTabModel]
           : [GetMainController().browserViewInformation otrTabModel];
-  NSUInteger count = otherTabModel.count;
-  for (NSUInteger i = 0; i < count; i++) {
-    Tab* tab = [otherTabModel tabAtIndex:i];
-    [tab.webController handleLowMemory];
-  }
+  // Disabling and enabling web usage will evict all web views.
+  otherTabModel.webUsageEnabled = NO;
+  otherTabModel.webUsageEnabled = YES;
 }
 
 void CloseAllIncognitoTabs() {
