d68405a19cd455e19f54664375e7fe1563a1952c
Alexis Hetu <sugoi@google.com>
2017-07-21 23:47:13 +0000

    Added texture precision extension
    SwiftShader's linear texture filtering precision was insufficient to
    pass some of the layout tests. In order to have the option to switch
    between high precision and high performance, a new chromium extension
    was added and implemented in SwiftShader. That extension is now added
    in this cl, along with the appropriate usage which would trigger high
    precision filtering within SwiftShader when running layout tests.
    
    Bug: 726075
    Change-Id: I69406e460595225b2436bb393f64144ff3ad7a1a
    Reviewed-on: https://chromium-review.googlesource.com/579787
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Reviewed-by: John Bauman <jbauman@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488808}

--- ui/gl/gl_bindings.h
+++ ui/gl/gl_bindings.h
@@ -164,6 +164,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define GL_SUBSCRIBED_VALUES_BUFFER_CHROMIUM             0x924B
 #define GL_MOUSE_POSITION_CHROMIUM                       0x924C
 
+// GL_CHROMIUM_texture_filtering_hint
+#define GL_TEXTURE_FILTERING_HINT_CHROMIUM               0x8AF0
+
 // GL_OES_texure_3D
 #define GL_SAMPLER_3D_OES                                0x8B5F
 
