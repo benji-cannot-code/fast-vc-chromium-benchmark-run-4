e9874ace64c4464fbd32a744e252cef670ab2f4b
Collin Baker <collinbaker@google.com>
2017-07-21 17:40:12 +0000

    [Offline pages] Change SnapshotController interface for renovations.
    Adds methods to SnapshotController and SnapshotController::Client for
    the renovation framework. Also adds basic implementations to all
    SnapshotController::Client implementors. This doesn't change the
    behavior of the SnapshotController yet.
    
    Design doc:
    https://docs.google.com/document/d/1WuwRJaxBKAwVpeAHG0xhq5uRxMTB-j4LFleT4QEqANU
    
    Bug: 736933
    Change-Id: Iee15c54fa4524b1c09a87f6e6c43a8df8ea630ee
    Reviewed-on: https://chromium-review.googlesource.com/580692
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Reviewed-by: Dmitry Titov <dimich@chromium.org>
    Reviewed-by: Yafei Duan <romax@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@google.com>
    Cr-Commit-Position: refs/heads/master@{#488696}

--- components/offline_pages/core/snapshot_controller_unittest.cc
+++ components/offline_pages/core/snapshot_controller_unittest.cc
@@ -30,6 +30,7 @@ class SnapshotControllerTest : public testing::Test,
 
   // SnapshotController::Client
   void StartSnapshot() override;
+  void RunRenovations() override;
 
   // Utility methods.
   // Runs until all of the tasks that are not delayed are gone from the task
@@ -65,6 +66,10 @@ void SnapshotControllerTest::TearDown() {
 
 void SnapshotControllerTest::StartSnapshot() {
   snapshot_count_++;
+}
+
+void SnapshotControllerTest::RunRenovations() {
+  controller_->RenovationsCompleted();
 }
 
 void SnapshotControllerTest::PumpLoop() {
