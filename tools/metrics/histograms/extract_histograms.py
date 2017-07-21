45639a252c0b6d81bf47b90c91f31107de436bfa
Ira Burak <iburak@google.com>
2017-07-21 15:54:18 +0000

    Fix a typo error. Store expiry date as string.
    Bug: 744672
    Change-Id: I83f9548ebe6a3a1e004d48476a5eed020d37b817
    Reviewed-on: https://chromium-review.googlesource.com/578438
    Commit-Queue: Ira Burak <iburak@google.com>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488668}

--- tools/metrics/histograms/extract_histograms.py
+++ tools/metrics/histograms/extract_histograms.py
@@ -302,7 +302,9 @@ def _ExtractHistogramsFromXmlTree(tree, enums):
             ' found %s.',
             name, EXPIRY_DATE_PATTERN, expiry_date_str)
         have_errors = True
-      histograms[expiry_date] = expiry_date
+      else:
+        histogram_entry['expiry_date'] = expiry_date.strftime(
+            EXPIRY_DATE_PATTERN)
 
     # Find <owner> tag.
     owners = _ExtractOwners(histogram)
