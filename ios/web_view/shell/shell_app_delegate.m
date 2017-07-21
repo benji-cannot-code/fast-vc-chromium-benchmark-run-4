97d6ab527f9fac4b2d139943dbf8ce9631ffd441
Mike Dougherty <michaeldo@chromium.org>
2017-07-21 06:59:13 +0000

    Modernize layout of toolbar items for ChromeWebView shell
    The manual layout of the toolbar items for ios_web_view_shell is
    outdated and no longer works with iOS 11. Update the UIButtons to
    UIBarButtonItems and automatically lay them out with setItems: instead
    of adding items as subviews to the UIToolbar.
    
    Bug: None
    Change-Id: I800ade863253ac244cb8705a4afec0b09a0a2198
    Reviewed-on: https://chromium-review.googlesource.com/578239
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488607}

--- ios/web_view/shell/shell_app_delegate.m
+++ ios/web_view/shell/shell_app_delegate.m
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
   self.window.backgroundColor = [UIColor whiteColor];
+  self.window.tintColor = [UIColor darkGrayColor];
 
   ShellViewController* controller = [[ShellViewController alloc] init];
   // Gives a restoration identifier so that state restoration works.
