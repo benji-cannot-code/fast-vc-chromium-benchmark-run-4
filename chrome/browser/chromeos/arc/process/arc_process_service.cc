f6607ba70f113fa8f7045bf1cd644bf78a18456b
Yusuke Sato <yusukes@chromium.org>
2017-07-21 03:33:00 +0000

    Use task_scheduler in c/b/chromeos/arc/process/arc_process_service.cc
    BUG=chromium:667892
    TEST=try, start ARC, press Search+ESC to show the UI
    
    Change-Id: I96b9cefaddbe26dc78edcb5217cae58d3c4a6a81
    Reviewed-on: https://chromium-review.googlesource.com/580012
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488569}

--- chrome/browser/chromeos/arc/process/arc_process_service.cc
+++ chrome/browser/chromeos/arc/process/arc_process_service.cc
@@ -23,7 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/process.h"
 #include "base/process/process_iterator.h"
 #include "base/task_runner_util.h"
-#include "base/threading/sequenced_worker_pool.h"
+#include "base/task_scheduler/post_task.h"
+#include "base/task_scheduler/task_traits.h"
 #include "base/trace_event/trace_event.h"
 #include "components/arc/arc_bridge_service.h"
 #include "components/arc/arc_browser_context_keyed_service_factory_base.h"
@@ -234,10 +235,9 @@ ArcProcessService::ArcProcessService(content::BrowserContext* context,
       nspid_to_pid_(new NSPidToPidMap()),
       weak_ptr_factory_(this) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  auto* blocking_pool = content::BrowserThread::GetBlockingPool();
-  task_runner_ = blocking_pool->GetSequencedTaskRunnerWithShutdownBehavior(
-      blocking_pool->GetSequenceToken(),
-      base::SequencedWorkerPool::SKIP_ON_SHUTDOWN);
+  task_runner_ = base::CreateSequencedTaskRunnerWithTraits(
+      {base::MayBlock(), base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN,
+       base::TaskPriority::USER_VISIBLE});
   arc_bridge_service_->process()->AddObserver(this);
 }
 
