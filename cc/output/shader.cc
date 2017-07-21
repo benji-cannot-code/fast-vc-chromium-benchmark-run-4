a2fb2ffbfc4ad56de0f6a6a0a2de72c982c5b4f5
Dongseong Hwang <dongseong.hwang@intel.com>
2017-07-21 00:10:04 +0000

    Reland "Fix HTML5 video blurry"
    Original CL: https://codereview.chromium.org/2418173002/
    
    Revert CL: https://codereview.chromium.org/2463103002/
    https://codereview.chromium.org/2474073004
    https://codereview.chromium.org/2544893006
    
    Since it was reverted long time ago, reimplement it from the scratch.
    
    HW decoded video is blurry, because crbug.com/575587 shinked visual rect by 1
    pixel, if coded rect is larger than visual rect. This fixed bleeding texture,
    but caused blurry issue.
    
    Meanwhile, crbug.com/429640 fixed bleeding texture for software YUV video in
    the elegant way. It clamps all texture coordinates to this maximum value in the
    fragment shader.
    
    This CL removes the 1 pixel hack and applies the way to TextureDrawQuad and
    StreamVideoDrawQuad.
    
    In addition, Video overlay requires this fix because there is noticible
    transition between GPU composition and hardware overlay.
    
    TEST=cc_unittests PrecisionSamplerShadersCompile/PrecisionSamplerShaderPixelTest.ShadersCompile
    chrome on http://browsertests.herokuapp.com/media/sharpness.html using amd64-generic
    BUG=615325, 683347
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I63c8736a365f0024513bfe1e999bc7beb7321da5
    Reviewed-on: https://chromium-review.googlesource.com/567809
    Commit-Queue: Dongseong Hwang <dongseong.hwang@intel.com>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Dongseong Hwang <dongseong.hwang@intel.com>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488504}

--- cc/output/shader.cc
+++ cc/output/shader.cc
@@ -258,7 +258,7 @@ std::string VertexShader::GetShaderString() const {
   }
 
   // Read the index variables.
-  if (use_uniform_arrays_ ||
+  if (use_uniform_arrays_ || has_vertex_opacity_ ||
       position_source_ == POSITION_SOURCE_ATTRIBUTE_INDEXED_UNIFORM) {
     HDR("attribute float a_index;");
     SRC("// Compute indices for uniform arrays.");
@@ -371,9 +371,12 @@ std::string VertexShader::GetShaderString() const {
 
   // Write varying vertex opacity.
   if (has_vertex_opacity_) {
-    DCHECK(use_uniform_arrays_);
-    HDR("uniform float opacity[NUM_QUADS * 4];");
     HDR("varying float v_alpha;");
+    if (use_uniform_arrays_) {
+      HDR("uniform float opacity[NUM_QUADS * 4];");
+    } else {
+      HDR("uniform float opacity[4];");
+    }
     SRC("v_alpha = opacity[vertex_index];");
   }
 
@@ -423,6 +426,8 @@ void FragmentShader::Init(GLES2Interface* context,
     uniforms.push_back("alpha");
   if (has_background_color_)
     uniforms.push_back("background_color");
+  if (has_tex_clamp_rect_)
+    uniforms.push_back("tex_clamp_rect");
   switch (input_color_type_) {
     case INPUT_COLOR_SOURCE_RGBA_TEXTURE:
       uniforms.push_back("s_texture");
@@ -477,6 +482,8 @@ void FragmentShader::Init(GLES2Interface* context,
     alpha_location_ = locations[index++];
   if (has_background_color_)
     background_color_location_ = locations[index++];
+  if (has_tex_clamp_rect_)
+    tex_clamp_rect_location_ = locations[index++];
   switch (input_color_type_) {
     case INPUT_COLOR_SOURCE_RGBA_TEXTURE:
       sampler_location_ = locations[index++];
@@ -832,15 +839,25 @@ std::string FragmentShader::GetShaderSource() const {
         SRC("   fragmentTexTransform.xy;");
         SRC("vec4 texColor = TextureLookup(s_texture, texCoord);");
         DCHECK(!ignore_sampler_type_);
+        DCHECK(!has_tex_clamp_rect_);
       } else {
         SRC("// Texture lookup");
-        if (ignore_sampler_type_)
+        if (ignore_sampler_type_) {
           SRC("vec4 texColor = texture2D(s_texture, v_texCoord);");
-        else
-          SRC("vec4 texColor = TextureLookup(s_texture, v_texCoord);");
+          DCHECK(!has_tex_clamp_rect_);
+        } else {
+          SRC("TexCoordPrecision vec2 texCoord = v_texCoord;");
+          if (has_tex_clamp_rect_) {
+            HDR("uniform vec4 tex_clamp_rect;");
+            SRC("texCoord = max(tex_clamp_rect.xy,");
+            SRC("    min(tex_clamp_rect.zw, texCoord));");
+          }
+          SRC("vec4 texColor = TextureLookup(s_texture, texCoord);");
+        }
       }
       break;
     case INPUT_COLOR_SOURCE_YUV_TEXTURES:
+      DCHECK(!has_tex_clamp_rect_);
       // Compute the clamped texture coordinates for the YA and UV textures.
       HDR("uniform SamplerType y_texture;");
       SRC("// YUV texture lookup and conversion to RGB.");
@@ -876,6 +893,7 @@ std::string FragmentShader::GetShaderSource() const {
     case INPUT_COLOR_SOURCE_UNIFORM:
       DCHECK(!ignore_sampler_type_);
       DCHECK(!has_rgba_fragment_tex_transform_);
+      DCHECK(!has_tex_clamp_rect_);
       HDR("uniform vec4 color;");
       SRC("// Uniform color");
       SRC("vec4 texColor = color;");
