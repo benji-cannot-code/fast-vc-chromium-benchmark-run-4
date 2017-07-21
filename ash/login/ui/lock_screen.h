7e2fb6177c81ae4f21dc2511246b4e2a3a269e7c
wutao <wutao@chromium.org>
2017-07-21 08:12:45 +0000

    CrOS: Use foreground blur for lock screen.
    Using foreground blur make it possible to cache the render surface and
    provide good performance.
    
    Bug: 738185
    Test: tested locally.
    Change-Id: I838c072ecd4deeb9e181e08644d215877616f0ef
    Reviewed-on: https://chromium-review.googlesource.com/578713
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488618}

--- ash/login/ui/lock_screen.h
+++ ash/login/ui/lock_screen.h
@@ -6,10 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_LOGIN_UI_LOCK_SCREEN_H_
 #define ASH_LOGIN_UI_LOCK_SCREEN_H_
 
+#include <unordered_map>
+
 #include "ash/ash_export.h"
 #include "base/macros.h"
 
 class AccountId;
+
+namespace ui {
+class Layer;
+}
 
 namespace ash {
 
@@ -40,6 +46,9 @@ class LockScreen {
 
   // Unowned pointer to the window which hosts the lock screen.
   LockWindow* window_ = nullptr;
+
+  // The wallpaper bluriness before entering lock_screen.
+  std::unordered_map<ui::Layer*, float> initial_blur_;
 
   DISALLOW_COPY_AND_ASSIGN(LockScreen);
 };
