cc3ca1a4d6e589f87377dbb8546abdfc3edf7187
Michael Thiessen <mthiesse@chromium.org>
2017-07-21 21:38:07 +0000

    VR: Make sure VR is fully entered before proceeding in tests.
    Bug: 746504
    Change-Id: Ia7296889916aac759edfef72c34beb01770d79ca
    Reviewed-on: https://chromium-review.googlesource.com/581851
    Reviewed-by: Yash Malik <ymalik@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488757}

--- chrome/android/javatests/src/org/chromium/chrome/browser/vr_shell/util/VrTransitionUtils.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr_shell/util/VrTransitionUtils.java
@@ -68,7 +68,7 @@ public class VrTransitionUtils {
         CriteriaHelper.pollUiThread(Criteria.equals(true, new Callable<Boolean>() {
             @Override
             public Boolean call() {
-                return VrShellDelegate.isInVr();
+                return VrShellDelegate.getInstanceForTesting().isVrEntryComplete();
             }
         }), timeout, POLL_CHECK_INTERVAL_SHORT_MS);
     }
@@ -112,8 +112,8 @@ public class VrTransitionUtils {
      */
     public static void enterPresentationOrFail(ContentViewCore cvc) {
         enterPresentation(cvc);
-        VrTestRule.pollJavaScriptBoolean(
-                "vrDisplay.isPresenting", POLL_TIMEOUT_LONG_MS, cvc.getWebContents());
+        Assert.assertTrue(VrTestRule.pollJavaScriptBoolean(
+                "vrDisplay.isPresenting", POLL_TIMEOUT_LONG_MS, cvc.getWebContents()));
         Assert.assertTrue(VrShellDelegate.getVrShellForTesting().getWebVrModeEnabled());
     }
 
