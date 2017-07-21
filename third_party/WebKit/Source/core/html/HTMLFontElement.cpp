632419f90ac10065b3c78013934fbeb7cea5c60e
Kent Tamura <tkent@chromium.org>
2017-07-21 04:37:52 +0000

    Optimize integer parsing in HTMLFontElement and HTMLParserIdioms.
    We don't need to create new string with StringBuilder to collect digits.
    Also, use ParsingUtilities functions to simplify the code.
    
    Change-Id: Id98ff2298db5c447f53e13a21c1d9e597e971098
    Reviewed-on: https://chromium-review.googlesource.com/580211
    Reviewed-by: Takayoshi Kochi <kochi@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488584}

--- third_party/WebKit/Source/core/html/HTMLFontElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLFontElement.cpp
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/StylePropertySet.h"
 #include "core/css/parser/CSSParser.h"
 #include "core/html/parser/HTMLParserIdioms.h"
-#include "platform/wtf/text/StringBuilder.h"
+#include "platform/wtf/text/ParsingUtilities.h"
 #include "platform/wtf/text/StringToNumber.h"
 
 namespace blink {
@@ -55,11 +55,7 @@ static bool ParseFontSize(const CharacterType* characters,
   const CharacterType* end = characters + length;
 
   // Step 3
-  while (position < end) {
-    if (!IsHTMLSpace<CharacterType>(*position))
-      break;
-    ++position;
-  }
+  skipWhile<CharacterType, IsHTMLSpace<CharacterType>>(position, end);
 
   // Step 4
   if (position == end)
@@ -84,28 +80,16 @@ static bool ParseFontSize(const CharacterType* characters,
   }
 
   // Step 6
-  StringBuilder digits;
-  digits.ReserveCapacity(16);
-  while (position < end) {
-    if (!IsASCIIDigit(*position))
-      break;
-    digits.Append(*position++);
-  }
+  const CharacterType* digits_start = position;
+  skipWhile<CharacterType, IsASCIIDigit>(position, end);
 
   // Step 7
-  if (digits.IsEmpty())
+  if (digits_start == position)
     return false;
 
   // Step 8
-  int value;
-
-  if (digits.Is8Bit()) {
-    value =
-        CharactersToIntStrict(digits.Characters8(), digits.length(), nullptr);
-  } else {
-    value =
-        CharactersToIntStrict(digits.Characters16(), digits.length(), nullptr);
-  }
+  int value =
+      CharactersToIntStrict(digits_start, position - digits_start, nullptr);
 
   // Step 9
   if (mode == kRelativePlus)
