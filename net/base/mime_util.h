dac1a355c8b25371b417aa325161bff1f21e8c59
Sergey Ulanov <sergeyu@google.com>
2017-07-21 02:34:05 +0000

    Use Hardcoded mime types to implement GetPreferredExtensionForMimeType().
    Updated GetPreferredExtensionForMimeType() implementation so it
    searches uses the same MIME type tables that are used for
    GetExtensionsForMimeType() and GetWellKnownMimeTypeFromExtension()
    
    Previously platform_mime_util_linux.cc and mime_extension_chromeos.cc
    contained some hardcoded tables to map between mime types and filename
    extensions. There is no reason these need to be separate from the maps
    in mime_util.cc. Merged them with the tables in mime_util.cc
    
    Bug: 731302
    Change-Id: Icdaad51c70cffbf62c404297ad615ac5c6dd33e9
    Reviewed-on: https://chromium-review.googlesource.com/578649
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488556}

--- net/base/mime_util.h
+++ net/base/mime_util.h
@@ -28,12 +28,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 
-// Get the mime type (if any) that is associated with the given file extension.
+// Gets the mime type (if any) that is associated with the given file extension.
 // Returns true if a corresponding mime type exists.
 NET_EXPORT bool GetMimeTypeFromExtension(const base::FilePath::StringType& ext,
                                          std::string* mime_type);
 
-// Get the mime type (if any) that is associated with the given file extension.
+// Gets the mime type (if any) that is associated with the given file extension.
 // Returns true if a corresponding mime type exists. In this method,
 // the search for a mime type is constrained to a limited set of
 // types known to the net library, the OS/registry is not consulted.
@@ -41,12 +41,12 @@ NET_EXPORT bool GetWellKnownMimeTypeFromExtension(
     const base::FilePath::StringType& ext,
     std::string* mime_type);
 
-// Get the mime type (if any) that is associated with the given file.  Returns
+// Gets the mime type (if any) that is associated with the given file.  Returns
 // true if a corresponding mime type exists.
 NET_EXPORT bool GetMimeTypeFromFile(const base::FilePath& file_path,
                                     std::string* mime_type);
 
-// Get the preferred extension (if any) associated with the given mime type.
+// Gets the preferred extension (if any) associated with the given mime type.
 // Returns true if a corresponding file extension exists.  The extension is
 // returned without a prefixed dot, ex "html".
 NET_EXPORT bool GetPreferredExtensionForMimeType(
@@ -105,16 +105,6 @@ NET_EXPORT void AddMultipartValueForUpload(const std::string& value_name,
 NET_EXPORT void AddMultipartFinalDelimiterForUpload(
     const std::string& mime_boundary,
     std::string* post_data);
-
-struct MimeInfo {
-  const char* const mime_type;
-  const char* const extensions;  // comma separated list
-};
-
-// Finds mime type of |ext| from |mappings|.
-const char* FindMimeType(const MimeInfo* mappings,
-                         size_t mappings_len,
-                         const std::string& ext);
 
 }  // namespace net
 
--- net/base/platform_mime_util_fuchsia.cc
+++ net/base/platform_mime_util_fuchsia.cc
@@ -13,27 +13,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace net {
 
 bool PlatformMimeUtil::GetPlatformMimeTypeFromExtension(
-    const base::FilePath::StringType& ext,
+    const base::FilePath::StringType& extension,
     std::string* result) const {
-  // TODO(fuchsia): Is there MIME DB on Fuchsia? Do we need it?
-  NOTIMPLEMENTED();
+  // TODO(fuchsia): Integrate with MIME DB when Fuchsia provides an API.
   return false;
 }
 
-bool PlatformMimeUtil::GetPreferredExtensionForMimeType(
+bool PlatformMimeUtil::GetPlatformPreferredExtensionForMimeType(
     const std::string& mime_type,
-    base::FilePath::StringType* ext) const {
-  // TODO(fuchsia): Is there MIME DB on Fuchsia? Do we need it?
-  NOTIMPLEMENTED();
+    base::FilePath::StringType* extension) const {
+  // TODO(fuchsia): Integrate with MIME DB when Fuchsia provides an API.
   return false;
 }
 
 void PlatformMimeUtil::GetPlatformExtensionsForMimeType(
     const std::string& mime_type,
     std::unordered_set<base::FilePath::StringType>* extensions) const {
-  base::FilePath::StringType ext;
-  if (GetPreferredExtensionForMimeType(mime_type, &ext))
-    extensions->insert(ext);
+  // TODO(fuchsia): Integrate with MIME DB when Fuchsia provides an API.
 }
 
 }  // namespace net
