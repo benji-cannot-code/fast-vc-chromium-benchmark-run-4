d3fdcb557413b892df6ecdac2bd6bf3bfe33d5ad
Daniel Vogelheim <vogelheim@chromium.org>
2017-07-21 09:20:13 +0000

    Introduce Loading Control Task Queue.
    This introduces a separate task queue for 'control' loading tasks. That is,
    very short task used to coordinate work between different loading tasks.
    These are scheduled separately as these tasks would typically unblock other,
    larger tasks.
    
    Bug: 557466
    Change-Id: Id2de6b73f24e550fe00efa19f4c898866ed0a1cb
    Reviewed-on: https://chromium-review.googlesource.com/500147
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488623}

--- third_party/WebKit/Source/core/loader/EmptyClients.cpp
+++ third_party/WebKit/Source/core/loader/EmptyClients.cpp
@@ -76,6 +76,7 @@ class EmptyFrameScheduler : public WebFrameScheduler {
   void RemoveThrottlingObserver(ObserverType, Observer*) override {}
   void SetFrameVisible(bool) override {}
   RefPtr<WebTaskRunner> LoadingTaskRunner() override;
+  RefPtr<WebTaskRunner> LoadingControlTaskRunner() override;
   RefPtr<WebTaskRunner> TimerTaskRunner() override;
   RefPtr<WebTaskRunner> UnthrottledTaskRunner() override;
   RefPtr<WebTaskRunner> SuspendableTaskRunner() override;
@@ -83,6 +84,10 @@ class EmptyFrameScheduler : public WebFrameScheduler {
 };
 
 RefPtr<WebTaskRunner> EmptyFrameScheduler::LoadingTaskRunner() {
+  return Platform::Current()->MainThread()->GetWebTaskRunner();
+}
+
+RefPtr<WebTaskRunner> EmptyFrameScheduler::LoadingControlTaskRunner() {
   return Platform::Current()->MainThread()->GetWebTaskRunner();
 }
 
