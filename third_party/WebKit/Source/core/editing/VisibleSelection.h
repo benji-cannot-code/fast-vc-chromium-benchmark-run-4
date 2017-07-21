c8a2b9423bd410f15562ac7140009d09b4532070
Yoshifumi Inoue <yosin@chromium.org>
2017-07-21 05:29:20 +0000

    Make VisibleSelectionTemplate::AdjustSelectionToAvoidCrossingEditingBoundaries() as static function
    This patch makes |AdjustSelectionToAvoidCrossingEditingBoundaries()| in
    |VisibleSelectionTemplate| as static function to avoid modifying member
    variables, |start_| and |end_|, of |VisibleSelectionTemplate| for improve
    readability and a preparation of removing |start_| and |end_|.
    
    Bug: 230267
    Change-Id: I1f5824f71ba115d3a11a3c534cf2f5024e715bbc
    Reviewed-on: https://chromium-review.googlesource.com/578908
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488591}

--- third_party/WebKit/Source/core/editing/VisibleSelection.h
+++ third_party/WebKit/Source/core/editing/VisibleSelection.h
@@ -156,7 +156,6 @@ class CORE_TEMPLATE_CLASS_EXPORT VisibleSelectionTemplate {
   void Validate(const SelectionTemplate<Strategy>&, TextGranularity);
 
   // Support methods for Validate()
-  void AdjustSelectionToAvoidCrossingEditingBoundaries();
   void UpdateSelectionType();
 
   // We need to store these as Positions because VisibleSelection is
