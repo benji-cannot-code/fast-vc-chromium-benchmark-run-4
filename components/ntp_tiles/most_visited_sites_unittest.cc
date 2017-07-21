db62e97dd8b44113505275e1b3bf8162aef47018
Mikel Astiz <mastiz@chromium.org>
2017-07-21 13:52:51 +0000

    Fix InstantService not refreshing TopSites when kNtpTilesFeature enabled
    InstantService is the only caller of MostVisitedSites::Refresh() and it
    is obvious from the caller's side that TopSites should be refreshed too,
    when an NTP is opened. Prior to this patch, TopSites would be updated
    time-based, leading to more stale NTP tiles when the feature
    kNtpTilesFeature is enabled (non-default case).
    
    The bug is believed to make little impact.
    
    Bug: 741431
    Change-Id: Iccdc4f89d3ab430eafd9a2faf1b1564f363ff29f
    Reviewed-on: https://chromium-review.googlesource.com/581147
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488647}

--- components/ntp_tiles/most_visited_sites_unittest.cc
+++ components/ntp_tiles/most_visited_sites_unittest.cc
@@ -445,6 +445,12 @@ TEST_P(MostVisitedSitesTest, ShouldStartNoCallInConstructor) {
   base::RunLoop().RunUntilIdle();
 }
 
+TEST_P(MostVisitedSitesTest, ShouldRefreshBothBackends) {
+  EXPECT_CALL(*mock_top_sites_, SyncWithHistory());
+  EXPECT_CALL(mock_suggestions_service_, FetchSuggestionsData());
+  most_visited_sites_->Refresh();
+}
+
 TEST_P(MostVisitedSitesTest, ShouldIncludeTileForHomePage) {
   FakeHomePageClient* home_page_client = RegisterNewHomePageClient();
   home_page_client->SetHomePageEnabled(true);
@@ -782,7 +788,6 @@ TEST_P(MostVisitedSitesTest, ShouldHandleTopSitesCacheHit) {
           MostVisitedURLList{MakeMostVisitedURL("Site 1", "http://site1/")}));
 
   InSequence seq;
-  EXPECT_CALL(*mock_top_sites_, SyncWithHistory());
   EXPECT_CALL(mock_suggestions_service_, AddCallback(_))
       .WillOnce(Invoke(&suggestions_service_callbacks_,
                        &SuggestionsService::ResponseCallbackList::Add));
@@ -802,6 +807,7 @@ TEST_P(MostVisitedSitesTest, ShouldHandleTopSitesCacheHit) {
                 OnMostVisitedURLsAvailable(ElementsAre(MatchesTile(
                     "Site 1", "http://site1/", TileSource::TOP_SITES))));
   }
+  EXPECT_CALL(*mock_top_sites_, SyncWithHistory());
   EXPECT_CALL(mock_suggestions_service_, FetchSuggestionsData())
       .WillOnce(Return(true));
 
@@ -835,7 +841,6 @@ class MostVisitedSitesWithCacheHitTest : public MostVisitedSitesTest {
   // service has cached results when the observer is registered.
   MostVisitedSitesWithCacheHitTest() {
     InSequence seq;
-    EXPECT_CALL(*mock_top_sites_, SyncWithHistory());
     EXPECT_CALL(mock_suggestions_service_, AddCallback(_))
         .WillOnce(Invoke(&suggestions_service_callbacks_,
                          &SuggestionsService::ResponseCallbackList::Add));
@@ -866,6 +871,7 @@ class MostVisitedSitesWithCacheHitTest : public MostVisitedSitesTest {
                       MatchesTile("Site 3", "http://site3/",
                                   TileSource::SUGGESTIONS_SERVICE))));
     }
+    EXPECT_CALL(*mock_top_sites_, SyncWithHistory());
     EXPECT_CALL(mock_suggestions_service_, FetchSuggestionsData())
         .WillOnce(Return(true));
 
@@ -979,7 +985,6 @@ class MostVisitedSitesWithEmptyCacheTest : public MostVisitedSitesTest {
   // service doesn't have cached results when the observer is registered.
   MostVisitedSitesWithEmptyCacheTest() {
     InSequence seq;
-    EXPECT_CALL(*mock_top_sites_, SyncWithHistory());
     EXPECT_CALL(mock_suggestions_service_, AddCallback(_))
         .WillOnce(Invoke(&suggestions_service_callbacks_,
                          &SuggestionsService::ResponseCallbackList::Add));
@@ -987,6 +992,7 @@ class MostVisitedSitesWithEmptyCacheTest : public MostVisitedSitesTest {
         .WillOnce(Return(SuggestionsProfile()));  // Empty cache.
     EXPECT_CALL(*mock_top_sites_, GetMostVisitedURLs(_, false))
         .WillOnce(Invoke(&top_sites_callbacks_, &TopSitesCallbackList::Add));
+    EXPECT_CALL(*mock_top_sites_, SyncWithHistory());
     EXPECT_CALL(mock_suggestions_service_, FetchSuggestionsData())
         .WillOnce(Return(true));
 
