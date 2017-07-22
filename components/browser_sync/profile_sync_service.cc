3d17444fd72f4823131da2afc2acd21442264667
skym <skym@chromium.org>
2017-07-21 18:32:46 -0700

    [Sync] Maintain a global_id mapping to update UserEvents that references navigations.
    UserEvents are created and reference sessions data by global_id. It is
    important that they travel over the wire in this format, as this is how
    the sessions data is keyed into Footprints, and the sync server cannot
    feasibly perform a persistent read before writing new UserEvents.
    Because global_id is forced to be part of the wire interface and
    specifics, it is also conveniently part of the interface to the
    UserEventService.
    
    However, while this global_id is seemingly sacred to sync, it comes
    from simply the timestmap of the navigation entry. And if a page is
    reloaded, this value can change. A problem arises when a UserEvent
    references a navigation by a global_id that was updated and the old
    value is never going to reach the sync server.
    
    To fix we maintain two sets of mappings, one that allows us to map old
    global_ids to new global_id, and one that allows us to efficiently look
    up uncommitted UserEvents by global_id, so that if we get either a new
    global_id or a new UserEvent we can quickly fix the global_id.
    
    BUG=747621
    
    Review-Url: https://codereview.chromium.org/2958303002
    Cr-Commit-Position: refs/heads/master@{#488826}

--- components/browser_sync/profile_sync_service.cc
+++ components/browser_sync/profile_sync_service.cc
@@ -2134,6 +2134,10 @@ void ProfileSyncService::GetAllNodes(
   }
 }
 
+syncer::GlobalIdMapper* ProfileSyncService::GetGlobalIdMapper() const {
+  return sessions_sync_manager_.get();
+}
+
 base::WeakPtr<syncer::JsController> ProfileSyncService::GetJsController() {
   DCHECK(thread_checker_.CalledOnValidThread());
   return sync_js_controller_.AsWeakPtr();
