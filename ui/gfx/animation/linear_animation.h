de01491fe3f7595c0b2584e14d81dbbe165951a2
Mohsen Izadi <mohsen@chromium.org>
2017-07-22 02:30:32 +0000

    Add elasticity to simple gesture nav
    This CL adds elasticity to the simple gesture nav UI. After it reaches
    the activation threshold, the affordance would be in its final shape
    (full opacity, centered arrow, full ripple size), however it would still
    move with further drags with diminishing speed until it completely
    stops. Further drags would be ignored.
    
    BUG=668296
    TEST=manual
    
    Change-Id: I7423e441bf90c04b84fda2cf9ddde44745bc945e
    Reviewed-on: https://chromium-review.googlesource.com/559592
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488840}

--- ui/gfx/animation/linear_animation.h
+++ ui/gfx/animation/linear_animation.h
@@ -19,7 +19,7 @@ class AnimationDelegate;
 class ANIMATION_EXPORT LinearAnimation : public Animation {
  public:
   // Default frame rate (hz).
-  static const int kDefaultFrameRate = 60;
+  static const int kDefaultFrameRate;
 
   // Initializes everything except the duration.
   //
