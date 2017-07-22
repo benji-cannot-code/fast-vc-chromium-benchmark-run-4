b4d6999b68af9c9cb9ada544753aa16fa7c7abbf
Daniel Bratell <bratell@opera.com>
2017-07-22 07:22:46 +0000

    Make sure CSSPropertyIdMap knows the CSSPropertyID traits.
    CSSPropertyID has some custom traits and without knowing them the
    HashTable will not be compiled as intended. This was noticed through
    a warning in jumbo builds about the traits being instantiated implicitly
    before they were instantiated explicitly (in a specialization).
    
    R=haraken@chromium.org
    
    Bug: 747408
    Change-Id: I2dea24bec17c8d071cbe60e443834d440eebfb2a
    Reviewed-on: https://chromium-review.googlesource.com/581447
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#488861}

--- third_party/WebKit/Source/bindings/core/v8/custom/V8CSSStyleDeclarationCustom.cpp
+++ third_party/WebKit/Source/bindings/core/v8/custom/V8CSSStyleDeclarationCustom.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/V8BindingForCore.h"
 #include "core/CSSPropertyNames.h"
 #include "core/css/CSSPrimitiveValue.h"
+#include "core/css/CSSPropertyIDTemplates.h"
 #include "core/css/CSSPropertyMetadata.h"
 #include "core/css/CSSStyleDeclaration.h"
 #include "core/css/CSSValue.h"
