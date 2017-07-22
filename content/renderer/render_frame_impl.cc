53efa6f0e8d8e2dd6441cad56b5425146d7c9d51
tzik <tzik@chromium.org>
2017-07-22 07:20:15 +0000

    Remove RendererScheduler::OnNavigate
    RendererScheduler::OnNavigate notifies RendererScheduler of the start of
    a navigation, so that RendererScheduler can switch its scheduling policy
    for resource loading. Similar notifications are needed for all non-same-
    document navigations, and RendererScheduler::OnNavigate covers browser
    initiated navigations.
    
    Though, after wiring up RendererScheduler::DidStartProvisionalLoad in
    http://crrev.com/211d6bc6aacfeed4, it should cover all non-same-document
    navigations, and RS::OnNavigate is no longer needed.
    
    Bug: 732738
    Change-Id: I9169f4cdf5d80d351bbca6d5a2aa88c7f522217f
    Reviewed-on: https://chromium-review.googlesource.com/558170
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488860}

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -192,7 +192,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/platform/WebVector.h"
 #include "third_party/WebKit/public/platform/modules/permissions/permission.mojom.h"
 #include "third_party/WebKit/public/platform/modules/serviceworker/WebServiceWorkerNetworkProvider.h"
-#include "third_party/WebKit/public/platform/scheduler/renderer/renderer_scheduler.h"
 #include "third_party/WebKit/public/web/WebColorSuggestion.h"
 #include "third_party/WebKit/public/web/WebConsoleMessage.h"
 #include "third_party/WebKit/public/web/WebDocument.h"
@@ -1712,10 +1711,6 @@ void RenderFrameImpl::OnNavigate(
     const CommonNavigationParams& common_params,
     const StartNavigationParams& start_params,
     const RequestNavigationParams& request_params) {
-  RenderThreadImpl* render_thread_impl = RenderThreadImpl::current();
-  // Can be NULL in tests.
-  if (render_thread_impl)
-    render_thread_impl->GetRendererScheduler()->OnNavigate();
   DCHECK(!IsBrowserSideNavigationEnabled());
   TRACE_EVENT2("navigation,rail", "RenderFrameImpl::OnNavigate", "id",
                routing_id_, "url", common_params.url.possibly_invalid_spec());
@@ -5147,12 +5142,6 @@ void RenderFrameImpl::OnCommitNavigation(
     const CommonNavigationParams& common_params,
     const RequestNavigationParams& request_params) {
   CHECK(IsBrowserSideNavigationEnabled());
-
-  RenderThreadImpl* render_thread_impl = RenderThreadImpl::current();
-  // Can be NULL in tests.
-  if (render_thread_impl &&
-      !FrameMsg_Navigate_Type::IsSameDocument(common_params.navigation_type))
-    render_thread_impl->GetRendererScheduler()->OnNavigate();
 
   // This will override the url requested by the WebURLLoader, as well as
   // provide it with the response to the request.
--- third_party/WebKit/Source/platform/scheduler/test/fake_renderer_scheduler.cc
+++ third_party/WebKit/Source/platform/scheduler/test/fake_renderer_scheduler.cc
@@ -89,8 +89,6 @@ void FakeRendererScheduler::AddPendingNavigation(NavigatingFrameType type) {}
 
 void FakeRendererScheduler::RemovePendingNavigation(NavigatingFrameType type) {}
 
-void FakeRendererScheduler::OnNavigate() {}
-
 bool FakeRendererScheduler::ShouldYieldForHighPriorityWork() {
   return false;
 }
