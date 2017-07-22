a9d200f46e848488e6dd830322e391521840c5de
Ian Vollick <vollick@chromium.org>
2017-07-22 00:38:05 +0000

    Revert "[vr] Restore opacity when transient elements are shown again."
    This reverts commit c140538983d4575003bb31ee4f522da635e1a3c9.
    
    Reason for revert: trying a different approach.
    
    Original change's description:
    > [vr] Restore opacity when transient elements are shown again.
    > 
    > We'd previously set opacity to 0.0 when hiding elements in the
    > transience manager which makes it tricky to see them when they come
    > back. With this change, we inform the manager of the desired "opacity
    > when visible" and it will restore elements to that opacity when
    > shown again.
    > 
    > Bug: None
    > Change-Id: I1a941c3032c35adab5afed17a8afc60bd41230f5
    > Reviewed-on: https://chromium-review.googlesource.com/582069
    > Commit-Queue: Ian Vollick <vollick@chromium.org>
    > Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#488788}
    
    TBR=vollick@chromium.org,cjgrant@chromium.org,ymalik@chromium.org
    
    Change-Id: Ic3ec7b20a980b42c1048c0d074cff6136c3997c2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: None
    Reviewed-on: https://chromium-review.googlesource.com/582527
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488822}

--- chrome/browser/vr/elements/transience_manager.cc
+++ chrome/browser/vr/elements/transience_manager.cc
@@ -8,11 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace vr {
 
 TransienceManager::TransienceManager(UiElement* element,
-                                     float opacity_when_enabled,
                                      const base::TimeDelta& timeout)
-    : element_(element),
-      opacity_when_enabled_(opacity_when_enabled),
-      timeout_(timeout) {
+    : element_(element), timeout_(timeout) {
   element_->SetVisible(false);
 }
 
@@ -21,24 +18,24 @@ void TransienceManager::SetEnabled(bool enabled) {
     return;
   enabled_ = enabled;
   if (enabled) {
-    Show();
+    element_->SetVisible(true);
     StartTimer();
   } else {
-    Hide();
+    element_->SetVisible(false);
     visibility_timer_.Stop();
   }
 }
 
 void TransienceManager::KickVisibilityIfEnabled() {
   if (enabled_) {
-    Show();
+    element_->SetVisible(true);
     StartTimer();
   }
 }
 
 void TransienceManager::EndVisibilityIfEnabled() {
   if (enabled_) {
-    Hide();
+    element_->SetVisible(false);
     visibility_timer_.Stop();
   }
 }
@@ -50,15 +47,6 @@ void TransienceManager::StartTimer() {
 }
 
 void TransienceManager::OnTimeout() {
-  Hide();
-}
-
-void TransienceManager::Show() {
-  element_->SetVisible(true);
-  element_->SetOpacity(opacity_when_enabled_);
-}
-
-void TransienceManager::Hide() {
   element_->SetVisible(false);
   element_->SetOpacity(0);
 }
