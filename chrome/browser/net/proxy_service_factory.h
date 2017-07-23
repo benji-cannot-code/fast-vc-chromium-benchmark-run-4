132694caf9913983fca2bb9ebc3dfc3c27f8ba08
mmenke <mmenke@chromium.org>
2017-07-23 10:06:35 -0700

    Reland of Make ProfileIOData use URLRequestContextBuilder (patchset #1 id:1 of https://codereview.chromium.org/2986623002/ )
    Reason for revert:
    We're now after branch, re-landing this (Since I'll need it for a number of CLs).  I'll fix devtools in a followup CL, and merge the revert if needed.
    
    Original issue's description:
    > Revert of Make ProfileIOData use URLRequestContextBuilder (patchset #12 id:330001 of https://codereview.chromium.org/2978443002/ )
    >
    > Reason for revert:
    > Wasn't thinking when I landed this just before branch.
    >
    > Original issue's description:
    > > Make ProfileIOData use URLRequestContextBuilder
    > >
    > > BUG=734199
    > >
    > > Review-Url: https://codereview.chromium.org/2978443002
    > > Cr-Commit-Position: refs/heads/master@{#487590}
    > > Committed: https://chromium.googlesource.com/chromium/src/+/189ddf60b2f4034a2ab6efb16e22ea389b2b779f
    >
    > TBR=rdsmith@chromium.org
    > # Not skipping CQ checks because original CL landed more than 1 days ago.
    > BUG=734199
    >
    > Review-Url: https://codereview.chromium.org/2986623002
    > Cr-Commit-Position: refs/heads/master@{#488562}
    > Committed: https://chromium.googlesource.com/chromium/src/+/8636cbd6b9449dd374d6879b6fad0e8ed7bf61de
    
    TBR=rdsmith@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=734199
    
    Review-Url: https://codereview.chromium.org/2986733002
    Cr-Commit-Position: refs/heads/master@{#488887}

--- chrome/browser/net/proxy_service_factory.h
+++ chrome/browser/net/proxy_service_factory.h
@@ -13,16 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class PrefProxyConfigTracker;
 class PrefService;
 
-namespace base {
-class CommandLine;
-}
-
 namespace net {
-class NetLog;
-class NetworkDelegate;
 class ProxyConfigService;
-class ProxyService;
-class URLRequestContext;
 }
 
 class ProxyServiceFactory {
@@ -45,16 +37,6 @@ class ProxyServiceFactory {
   // (ChromeOS only).
   static PrefProxyConfigTracker* CreatePrefProxyConfigTrackerOfLocalState(
       PrefService* local_state_prefs);
-
-  // Create a proxy service according to the options on command line.
-  static std::unique_ptr<net::ProxyService> CreateProxyService(
-      net::NetLog* net_log,
-      net::URLRequestContext* context,
-      net::NetworkDelegate* network_delegate,
-      std::unique_ptr<net::ProxyConfigService> proxy_config_service,
-      const base::CommandLine& command_line,
-      bool quick_check_enabled,
-      bool pac_https_url_stripping_enabled);
 
  private:
   DISALLOW_IMPLICIT_CONSTRUCTORS(ProxyServiceFactory);
