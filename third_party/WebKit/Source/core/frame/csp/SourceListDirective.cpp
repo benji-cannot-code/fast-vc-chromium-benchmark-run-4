8e9bcc574507b6d0855c162a1d89a303c7f0b866
Kent Tamura <tkent@chromium.org>
2017-07-24 08:42:44 +0000

    Merge CharactersToFooStrict and CharactersToFoo by adding NumberParsingOptions argument.
    Bug: 746157
    Change-Id: I0bf9c2a5e2799ff0fc0ed701c7ec9b656ed7538e
    Reviewed-on: https://chromium-review.googlesource.com/579898
    Reviewed-by: Takayoshi Kochi <kochi@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488930}

--- third_party/WebKit/Source/core/frame/csp/SourceListDirective.cpp
+++ third_party/WebKit/Source/core/frame/csp/SourceListDirective.cpp
@@ -539,7 +539,8 @@ bool SourceListDirective::ParsePort(
     return false;
 
   bool ok;
-  port = CharactersToIntStrict(begin, end - begin, &ok);
+  port = CharactersToInt(begin, end - begin, WTF::NumberParsingOptions::kNone,
+                         &ok);
   return ok;
 }
 
