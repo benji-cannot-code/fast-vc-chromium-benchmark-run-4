8085c9e9dc11dbbf0e5d9cbf3bf3914b7344d7ac
Bugs Nash <bugsnash@chromium.org>
2017-07-24 06:07:29 +0000

    Implemented parsing logic for grid-template-areas in property API
    This patch
    - implemented the parsing logic for the property grid-template-areas in
      the property API
    - Removed the legacy parsing logic for this property from
      CSSPropertyParser.cpp
    - updated CSSProperties.json5 so that the relevant generated files are
      updated.
    - added #include StringHash.h in GridArea.h because GridArea.h
      defines a HashMap with a String key (NamedGridAreaMap). Calling
      .find() on a NamedGridAreaMap without knowledge of StringHash
      results in a compile error.
    
    Diff in generated files:
    https://gist.github.com/BugsNash/a48d571d91d90992e4afb50fa4ba7efe/revisions
    
    Bug: 668012
    Change-Id: I992c3351610de3382f6b7ae2741b63d44d95266a
    Reviewed-on: https://chromium-review.googlesource.com/578538
    Reviewed-by: nainar <nainar@chromium.org>
    Commit-Queue: Bugs Nash <bugsnash@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488923}

--- third_party/WebKit/Source/core/style/GridArea.h
+++ third_party/WebKit/Source/core/style/GridArea.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/wtf/Allocator.h"
 #include "platform/wtf/HashMap.h"
 #include "platform/wtf/MathExtras.h"
+#include "platform/wtf/text/StringHash.h"
 #include "platform/wtf/text/WTFString.h"
 
 namespace blink {
