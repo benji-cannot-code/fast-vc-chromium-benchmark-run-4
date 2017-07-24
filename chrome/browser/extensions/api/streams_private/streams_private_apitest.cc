06967313f5f067070c336779874f7c53b5ce5a4d
Robert Flack <flackr@chromium.org>
2017-07-24 16:16:58 +0000

    Disable flaky StreamsPrivateApiTests on Linux as well as ChromeOS
    These tests are flaky on Linux as well. See recent failures:
    StreamsPrivateApiTest.DirectDownload:
    https://build.chromium.org/p/chromium.linux/builders/Linux%20Tests/builds/59858
    StreamsPrivateApiTest.NavigateToAnAttachment:
    https://build.chromium.org/p/chromium.linux/builders/Linux%20Tests/builds/59861
    
    They are failing about 8.5% and 7.5% of linux browser_tests runs respectively.
    
    TBR=zork@chromium.org
    
    Bug: 746526
    Change-Id: I6a8fa5c7fb7e13706664601401642408ce9821bd
    Reviewed-on: https://chromium-review.googlesource.com/582973
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488991}

--- chrome/browser/extensions/api/streams_private/streams_private_apitest.cc
+++ chrome/browser/extensions/api/streams_private/streams_private_apitest.cc
@@ -323,8 +323,8 @@ IN_PROC_BROWSER_TEST_F(StreamsPrivateApiTest, NavigateCrossSite) {
   EXPECT_TRUE(catcher.GetNextResult());
 }
 
-// Flaky on ChromeOS: http://crbug.com/746526.
-#if defined(OS_CHROMEOS)
+// Flaky on Linux and ChromeOS: http://crbug.com/746526.
+#if defined(OS_LINUX)
 #define MAYBE_NavigateToAnAttachment DISABLED_NavigateToAnAttachment
 #else
 #define MAYBE_NavigateToAnAttachment NavigateToAnAttachment
@@ -368,8 +368,8 @@ IN_PROC_BROWSER_TEST_F(StreamsPrivateApiTest, MAYBE_NavigateToAnAttachment) {
   EXPECT_TRUE(catcher.GetNextResult());
 }
 
-// Flaky on ChromeOS: http://crbug.com/746526.
-#if defined(OS_CHROMEOS)
+// Flaky on Linux and ChromeOS: http://crbug.com/746526.
+#if defined(OS_LINUX)
 #define MAYBE_DirectDownload DISABLED_DirectDownload
 #else
 #define MAYBE_DirectDownload DirectDownload
