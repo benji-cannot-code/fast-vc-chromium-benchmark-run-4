d63cc484d2ba91ba70cbea6a02b52c697f0cbcd0
Jamie Madill <jmadill@chromium.org>
2017-07-24 16:05:48 +0000

    gpu: Implement passthrough decoder program cache.
    This hooks up ANGLE to talk to the disk cache, and instruments the
    necessary controls over the cache to handle sizing and memory
    pressure.
    
    Uses an unretained callback to avoid the layering violation of
    calling from ui/gl into the command buffer.
    
    BUG=738513
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I9cb3ad9cbdd756d7b31248e788bb7f9e93aae90a
    Reviewed-on: https://chromium-review.googlesource.com/568671
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Jamie Madill <jmadill@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488984}

--- gpu/command_buffer/service/gles2_cmd_decoder_passthrough.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder_passthrough.cc
@@ -5,12 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "gpu/command_buffer/service/gles2_cmd_decoder_passthrough.h"
 
+#include "base/callback.h"
 #include "base/strings/string_split.h"
 #include "gpu/command_buffer/service/command_buffer_service.h"
 #include "gpu/command_buffer/service/feature_info.h"
 #include "gpu/command_buffer/service/gl_utils.h"
 #include "gpu/command_buffer/service/gpu_tracer.h"
 #include "ui/gl/gl_version_info.h"
+
+#if defined(USE_EGL)
+#include "ui/gl/angle_platform_impl.h"
+#endif  // defined(USE_EGL)
 
 namespace gpu {
 namespace gles2 {
@@ -342,6 +347,13 @@ void GLES2DecoderPassthroughImpl::Destroy(bool have_context) {
   surface_ = nullptr;
 
   if (group_) {
+#if defined(USE_EGL)
+    // Clear the program binary caching callback.
+    if (group_->has_program_cache()) {
+      angle::ResetCacheProgramCallback();
+    }
+#endif  // defined(USE_EGL)
+
     group_->Destroy(this, have_context);
     group_ = nullptr;
   }
@@ -404,6 +416,15 @@ bool GLES2DecoderPassthroughImpl::MakeCurrent() {
     group_->LoseContexts(error::kUnknown);
     return false;
   }
+
+#if defined(USE_EGL)
+  // Establish the program binary caching callback.
+  if (group_->has_program_cache()) {
+    auto program_callback = base::BindRepeating(
+        &GLES2DecoderClient::CacheShader, base::Unretained(client_));
+    angle::SetCacheProgramCallback(program_callback);
+  }
+#endif  // defined(USE_EGL)
 
   return true;
 }
