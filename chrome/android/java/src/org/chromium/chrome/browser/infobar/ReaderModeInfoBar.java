8f06ca0966b9f6625a10a331cda84be74487a198
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2017-07-24 19:48:11 +0000

    Make Reader Mode infobar icon not selectable in TalkBack
    Currently the Reader Mode infobar icon can be selected in TalkBack. It
    does not have accessibility description, and its click listener is the
    same as the main text in the infobar. Therefore, selecting it is not
    informative, and is redundant.
    
    Bug: None
    Change-Id: I3cac8ccaabb2ece006f4fc76dd5bfc02bf833280
    Reviewed-on: https://chromium-review.googlesource.com/583589
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489051}

--- chrome/android/java/src/org/chromium/chrome/browser/infobar/ReaderModeInfoBar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/infobar/ReaderModeInfoBar.java
@@ -5,9 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.infobar;
 
+import static android.view.View.IMPORTANT_FOR_ACCESSIBILITY_NO;
+
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.chromium.base.ApiCompatibilityUtils;
@@ -56,7 +59,9 @@ public class ReaderModeInfoBar extends InfoBar {
         prompt.setGravity(Gravity.CENTER_VERTICAL);
         prompt.setOnClickListener(mNavigateListener);
 
-        layout.findViewById(R.id.infobar_icon).setOnClickListener(mNavigateListener);
+        ImageView iconView = layout.findViewById(R.id.infobar_icon);
+        iconView.setOnClickListener(mNavigateListener);
+        iconView.setImportantForAccessibility(IMPORTANT_FOR_ACCESSIBILITY_NO);
         final int messagePadding = getContext().getResources().getDimensionPixelOffset(
                 R.dimen.reader_mode_infobar_text_padding);
         prompt.setPadding(0, messagePadding, 0, messagePadding);
