94e01c1ffc1436a078e66b26b3962d84f36aa418
Christopher Grant <cjgrant@chromium.org>
2017-07-24 17:43:52 +0000

    VR: Add offline mode URL rendering unit test.
    Check that:
    - The security verbose text is present in offline mode oly.
    - The URL moves when security verbose text is present.
    
    BUG=
    
    VR: Ensure URL and offline security text do not overlap.
    
    BUG=747590
    
    Change-Id: I2220ba6a2657a78a7fab1cb03fd23e6b02024bf9
    Reviewed-on: https://chromium-review.googlesource.com/582934
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489020}

--- chrome/browser/vr/elements/url_bar_texture.cc
+++ chrome/browser/vr/elements/url_bar_texture.cc
@@ -174,6 +174,11 @@ void UrlBarTexture::Draw(SkCanvas* canvas, const gfx::Size& texture_size) {
   size_.set_height(texture_size.height());
   size_.set_width(texture_size.width());
 
+  rendered_url_text_ = base::string16();
+  rendered_url_text_rect_ = gfx::Rect();
+  rendered_security_text_ = base::string16();
+  rendered_security_text_rect_ = gfx::Rect();
+
   canvas->save();
   canvas->scale(size_.width() / kWidth, size_.width() / kWidth);
 
@@ -283,6 +288,8 @@ void UrlBarTexture::Draw(SkCanvas* canvas, const gfx::Size& texture_size) {
     render_text->SetText(chip_text);
     render_text->SetDisplayRect(text_bounds);
     render_text->Draw(&gfx_canvas);
+    rendered_security_text_ = render_text->text();
+    rendered_security_text_rect_ = render_text->display_rect();
 
     // Capture the rendered text region for future hit testing.
     gfx::Size string_size = render_text->GetStringSize();
@@ -317,6 +324,8 @@ void UrlBarTexture::Draw(SkCanvas* canvas, const gfx::Size& texture_size) {
       last_drawn_url_x_position_ = url_x;
     }
     url_render_text_->Draw(&gfx_canvas);
+    rendered_url_text_ = url_render_text_->text();
+    rendered_url_text_rect_ = url_render_text_->display_rect();
   }
 }
 
@@ -364,7 +373,6 @@ void UrlBarTexture::RenderUrl(const gfx::Size& texture_size,
                   color_scheme());
 
   url_render_text_ = std::move(render_text);
-  url_text_ = text;
 }
 
 // This method replicates behavior in OmniboxView::UpdateTextStyle(), and
