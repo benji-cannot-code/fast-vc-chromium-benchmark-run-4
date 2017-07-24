19b3ebcbf5d73ba10bed8891dc86cc1e6ab52024
Bruce Dawson <brucedawson@chromium.org>
2017-07-24 21:35:21 +0000

    Remove obsolete use_legacy_launch_ flag/code
    use_legacy_launch_ was added to support creating processes on Windows
    XP. We no longer support this OS so this change deletes the flags,
    supporting code, and tests.
    
    Change-Id: Iedc900cffa66abf620e1ca1ecba7efaadd2bf371
    Reviewed-on: https://chromium-review.googlesource.com/569318
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Pierre-Antoine Manzagol (departed) <manzagop@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489080}

--- components/browser_watcher/watcher_client_win.cc
+++ components/browser_watcher/watcher_client_win.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/logging.h"
 #include "base/process/launch.h"
-#include "base/win/windows_version.h"
 
 namespace browser_watcher {
 
@@ -26,10 +25,8 @@ base::Process OpenOwnProcessInheritable() {
 }  // namespace
 
 WatcherClient::WatcherClient(const CommandLineGenerator& command_line_generator)
-    : use_legacy_launch_(base::win::GetVersion() < base::win::VERSION_VISTA),
-      command_line_generator_(command_line_generator),
-      process_(base::kNullProcessHandle) {
-}
+    : command_line_generator_(command_line_generator),
+      process_(base::kNullProcessHandle) {}
 
 WatcherClient::~WatcherClient() {
 }
@@ -45,17 +42,10 @@ void WatcherClient::LaunchWatcher() {
   base::HandlesToInheritVector to_inherit;
   base::LaunchOptions options;
   options.start_hidden = true;
-  if (use_legacy_launch_) {
-    // Launch the child process inheriting all handles on XP.
-    options.inherit_handles = true;
-  } else {
-    // Launch the child process inheriting only |self| on
-    // Vista and better.
-    to_inherit.push_back(self.Handle());
-    to_inherit.insert(to_inherit.end(), inherited_handles_.begin(),
-                      inherited_handles_.end());
-    options.handles_to_inherit = &to_inherit;
-  }
+  to_inherit.push_back(self.Handle());
+  to_inherit.insert(to_inherit.end(), inherited_handles_.begin(),
+                    inherited_handles_.end());
+  options.handles_to_inherit = &to_inherit;
 
   process_ = base::LaunchProcess(cmd_line, options);
   if (!process_.IsValid())
