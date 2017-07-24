78fcb978057ed0627969e22cbae73d55ca1d404a
Hiroki Nakagawa <nhiroki@chromium.org>
2017-07-24 06:06:30 +0000

    Worklet: Fix TaskRunnerHelper::Get() for MainThreadWorkletGlobalScope
    Before this CL, TaskRunnerHelper::Get() with MainThreadWorkletGlobalScope hits
    an assertion failure (NOTREACHED) in MainThreadWorkletGlobalScope::GetThread().
    This is because MainThreadWorkletGlobalScope lives on the main thread and a
    WorkerThread instance doesn't exist for that.
    
    After this CL, TaskRunnerHelper::Get() with MainThreadWorkletGlobalScope
    retrieves a task runner from an associated frame.
    
    Bug: 726576
    Change-Id: Iabf0ee81b6998f2173615cadbed53729a85b9a93
    Reviewed-on: https://chromium-review.googlesource.com/582055
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488922}

--- third_party/WebKit/Source/core/workers/MainThreadWorkletTest.cpp
+++ third_party/WebKit/Source/core/workers/MainThreadWorkletTest.cpp
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "bindings/core/v8/V8BindingForCore.h"
+#include "core/dom/TaskRunnerHelper.h"
 #include "core/frame/UseCounter.h"
 #include "core/testing/DummyPageHolder.h"
 #include "core/workers/MainThreadWorkletGlobalScope.h"
@@ -92,6 +93,12 @@ TEST_F(MainThreadWorkletTest, UseCounter) {
   // API use should be reported to the Document only one time. See comments in
   // MainThreadWorkletGlobalScopeForTest::ReportDeprecation.
   Deprecation::CountDeprecation(global_scope_, kFeature2);
+}
+
+TEST_F(MainThreadWorkletTest, TaskRunner) {
+  RefPtr<WebTaskRunner> task_runner =
+      TaskRunnerHelper::Get(TaskType::kUnthrottled, global_scope_);
+  EXPECT_TRUE(task_runner->RunsTasksInCurrentSequence());
 }
 
 }  // namespace blink
