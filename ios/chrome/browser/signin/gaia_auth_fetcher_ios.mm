8e04d7f4b3c92e80a74e78096fd83aab3db968cf
Jérôme Lebel <jlebel@chromium.org>
2017-07-24 11:43:37 +0000

    No more javascript hack to load a request with body
    WKWebView was not supporting to load a POST request with a body. The
    work around was to load it with javascript. This has been fixed with iOS
    11: https://bugs.webkit.org/show_bug.cgi?id=167131
    
    Bug: 746882
    Change-Id: Ia5c2bf49f2d7e8da0993bc33c95c4c8447a83eac
    Reviewed-on: https://chromium-review.googlesource.com/579189
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Mihai Sardarescu (OOO) <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488948}

--- ios/chrome/browser/signin/gaia_auth_fetcher_ios.mm
+++ ios/chrome/browser/signin/gaia_auth_fetcher_ios.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <WebKit/WebKit.h>
 
+#include "base/ios/ios_util.h"
 #include "base/json/string_escape.h"
 #include "base/logging.h"
 #import "base/mac/foundation_util.h"
@@ -115,10 +116,13 @@ NSString* EscapeAndQuoteToNSString(const std::string& value) {
 // This is needed because WKWebView ignores the HTTPBody in a POST request.
 // See
 // https://bugs.webkit.org/show_bug.cgi?id=145410
+// TODO(crbug.com/740987): Remove this function workaround once iOS 10 is
+// dropped.
 void DoPostRequest(WKWebView* web_view,
                    const std::string& body,
                    const std::string& headers,
                    const GURL& url) {
+  DCHECK(!base::ios::IsRunningOnIOS11OrLater());
   NSMutableString* header_data = [NSMutableString string];
   net::HttpRequestHeaders request_headers;
   request_headers.AddHeadersFromString(headers);
@@ -263,12 +267,12 @@ void GaiaAuthFetcherIOSBridge::URLFetchFailure(bool is_cancelled) {
 void GaiaAuthFetcherIOSBridge::FetchPendingRequest() {
   if (!request_.pending)
     return;
-  if (!request_.body.empty()) {
+  if (!request_.body.empty() && !base::ios::IsRunningOnIOS11OrLater()) {
     DoPostRequest(GetWKWebView(), request_.body, request_.headers,
                   request_.url);
   } else {
-  [GetWKWebView()
-      loadRequest:GetRequest(request_.body, request_.headers, request_.url)];
+    [GetWKWebView()
+        loadRequest:GetRequest(request_.body, request_.headers, request_.url)];
   }
 }
 
