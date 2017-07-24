ba44ebea498bb3f4ee69e1e8b9bf47aafa2af4ee
Morten Stenshorne <mstensho@opera.com>
2017-07-24 19:43:13 +0000

    Explicit namespace ::testing, to not confuse it with blink::testing
    This fixes a merge mistake at
    https://chromium-review.googlesource.com/c/519168/25/third_party/WebKit/Source/core/dom/ScriptRunnerTest.cpp
    
    That CL overwrote parts of https://codereview.chromium.org/2970833002 ,
    which broke Jumbo compilation.
    
    BUG=739121
    
    Change-Id: I486a2c49032ffeef2bd81d5e230a9f9cf81b646f
    Reviewed-on: https://chromium-review.googlesource.com/582611
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@opera.com>
    Cr-Commit-Position: refs/heads/master@{#489050}

--- third_party/WebKit/Source/core/dom/ScriptRunnerTest.cpp
+++ third_party/WebKit/Source/core/dom/ScriptRunnerTest.cpp
@@ -123,7 +123,7 @@ MockScriptLoader* MockScriptLoader::SetupForNonStreaming() {
   return this;
 }
 
-class ScriptRunnerTest : public testing::Test {
+class ScriptRunnerTest : public ::testing::Test {
  public:
   ScriptRunnerTest() : document_(Document::Create()) {}
 
