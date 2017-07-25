b06194d50b9146fc9edfbc698efe4473fbcee728
Reza.Zakerinasab <zakerinasab@chromium.org>
2017-07-25 14:45:56 +0000

    Disable flaky tests on Win7
    This CL disables the following unit tests on Win7:
    
    ActivationLevelTest/SubresourceFilterSafeBrowsingActivationThrottleParamTest.ListMatchedOnStart_NoDelay
    ActivationLevelTest/SubresourceFilterSafeBrowsingActivationThrottleParamTest.ListMatchedOnRedirect_NoDelay
    
    TBR=rhalavati@chromium.org
    NOTRY=true
    
    Bug: 748524
    Change-Id: I58b0a1754463389a3ab01a7c53b826da10e0178a
    Reviewed-on: https://chromium-review.googlesource.com/585069
    Commit-Queue: Mohammad Reza Zakerinasab <zakerinasab@chromium.org>
    Reviewed-by: Mohammad Reza Zakerinasab <zakerinasab@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489294}

--- components/subresource_filter/content/browser/subresource_filter_safe_browsing_activation_throttle_unittest.cc
+++ components/subresource_filter/content/browser/subresource_filter_safe_browsing_activation_throttle_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/test/histogram_tester.h"
 #include "base/test/test_mock_time_task_runner.h"
+#include "build/build_config.h"
 #include "components/safe_browsing_db/test_database_manager.h"
 #include "components/subresource_filter/content/browser/content_subresource_filter_driver_factory.h"
 #include "components/subresource_filter/content/browser/fake_safe_browsing_database_manager.h"
@@ -947,8 +948,14 @@ TEST_P(SubresourceFilterSafeBrowsingActivationThrottleParamTest,
   tester().ExpectTotalCount(kSafeBrowsingCheckTime, 1);
 }
 
+// Flaky on Win only.  http://crbug.com/748524
+#if defined(OS_WIN)
+#define MAYBE_ListMatchedOnStart_NoDelay DISABLED_ListMatchedOnStart_NoDelay
+#else
+#define MAYBE_ListMatchedOnStart_NoDelay ListMatchedOnStart_NoDelay
+#endif
 TEST_P(SubresourceFilterSafeBrowsingActivationThrottleParamTest,
-       ListMatchedOnStart_NoDelay) {
+       MAYBE_ListMatchedOnStart_NoDelay) {
   const ActivationListTestData& test_data = GetParam();
   const GURL url(kURL);
   ConfigureForMatchParam(url);
@@ -971,8 +978,15 @@ TEST_P(SubresourceFilterSafeBrowsingActivationThrottleParamTest,
   tester().ExpectTotalCount(kSafeBrowsingNavigationDelayNoSpeculation, 1);
 }
 
+// Flaky on Win only.  http://crbug.com/748524
+#if defined(OS_WIN)
+#define MAYBE_ListMatchedOnRedirect_NoDelay \
+  DISABLED_ListMatchedOnRedirect_NoDelay
+#else
+#define MAYBE_ListMatchedOnRedirect_NoDelay ListMatchedOnRedirect_NoDelay
+#endif
 TEST_P(SubresourceFilterSafeBrowsingActivationThrottleParamTest,
-       ListMatchedOnRedirect_NoDelay) {
+       MAYBE_ListMatchedOnRedirect_NoDelay) {
   const ActivationListTestData& test_data = GetParam();
   const GURL url(kURL);
   const GURL redirect_url(kRedirectURL);
