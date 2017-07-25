14551f82a59e710ed0ae6194498deba55b6b5f3d
Ella Ge <eirage@chromium.org>
2017-07-25 16:59:47 +0000

    Revert change in MediaRemotingElements
    Revert change MouseEvent to Pointer in MediaRemotingElements
    
    https: //chromium-review.googlesource.com/c/567131/
    Bug: 748332
    Change-Id: I7de6a11a763c133edda629a4e213ca05fc78b2e1
    Reviewed-on: https://chromium-review.googlesource.com/584812
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489344}

--- third_party/WebKit/Source/core/html/media/MediaRemotingElements.cpp
+++ third_party/WebKit/Source/core/html/media/MediaRemotingElements.cpp
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/media/MediaRemotingElements.h"
 
 #include "core/dom/ShadowRoot.h"
-#include "core/events/PointerEvent.h"
+#include "core/events/MouseEvent.h"
 #include "core/geometry/DOMRect.h"
 #include "core/html/HTMLVideoElement.h"
 #include "core/input/EventHandler.h"
@@ -19,10 +19,10 @@ using namespace HTMLNames;
 
 // ----------------------------
 
-class MediaRemotingExitButtonElement::PointerEventsListener final
+class MediaRemotingExitButtonElement::MouseEventsListener final
     : public EventListener {
  public:
-  explicit PointerEventsListener(MediaRemotingExitButtonElement& element)
+  explicit MouseEventsListener(MediaRemotingExitButtonElement& element)
       : EventListener(kCPPEventListenerType), element_(element) {}
 
   bool operator==(const EventListener& other) const override {
@@ -38,10 +38,10 @@ class MediaRemotingExitButtonElement::PointerEventsListener final
   void handleEvent(ExecutionContext* context, Event* event) override {
     DCHECK_EQ(event->type(), EventTypeNames::click);
 
-    PointerEvent* pointer_event = ToPointerEvent(event);
+    MouseEvent* mouse_event = ToMouseEvent(event);
     DOMRect* client_rect = element_->getBoundingClientRect();
-    const double x = pointer_event->x();
-    const double y = pointer_event->y();
+    const double x = mouse_event->x();
+    const double y = mouse_event->y();
     if (x < client_rect->left() || x > client_rect->right() ||
         y < client_rect->top() || y > client_rect->bottom())
       return;
@@ -57,7 +57,7 @@ class MediaRemotingExitButtonElement::PointerEventsListener final
 MediaRemotingExitButtonElement::MediaRemotingExitButtonElement(
     MediaRemotingInterstitial& interstitial)
     : HTMLDivElement(interstitial.GetDocument()), interstitial_(interstitial) {
-  listener_ = new PointerEventsListener(*this);
+  listener_ = new MouseEventsListener(*this);
   SetShadowPseudoId(AtomicString("-internal-media-remoting-disable-button"));
   setInnerText(interstitial.GetVideoElement().GetLocale().QueryString(
                    WebLocalizedString::kMediaRemotingDisableText),
