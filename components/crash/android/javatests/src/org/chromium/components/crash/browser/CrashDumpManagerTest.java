35cb8ed19e351235ecc810ae2db51ee15da7f641
Yoland Yan <yolandyan@chromium.org>
2017-07-25 02:37:16 +0000

    Reland: Convert components/ java tests to JUnit4
    Fixed cronet runner instrumentation issue for last CL:
    https://chromium-review.googlesource.com/c/567627/
    
    For more on JUnit4 migration, please check src/testing/android/docs/junit4.md
    
    Bug: 640116
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester
    Change-Id: Ide1b9363d6cde3d988c92eadef2547ff0115702f
    Reviewed-on: https://chromium-review.googlesource.com/582500
    Commit-Queue: Yoland Yan <yolandyan@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489194}

--- components/crash/android/javatests/src/org/chromium/components/crash/browser/CrashDumpManagerTest.java
+++ components/crash/android/javatests/src/org/chromium/components/crash/browser/CrashDumpManagerTest.java
@@ -5,11 +5,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.crash.browser;
 
+import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.SmallTest;
-import android.test.InstrumentationTestCase;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.TestFileUtil;
@@ -20,21 +27,21 @@ import java.io.IOException;
 /**
  * Unittests for {@link CrashDumpManager}.
  */
-public class CrashDumpManagerTest extends InstrumentationTestCase {
+@RunWith(BaseJUnit4ClassRunner.class)
+public class CrashDumpManagerTest {
     File mTempDir;
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-        ContextUtils.initApplicationContextForTests(
-                getInstrumentation().getTargetContext().getApplicationContext());
+    @Before
+    public void setUp() throws Exception {
+        ContextUtils.initApplicationContextForTests(InstrumentationRegistry.getInstrumentation()
+                                                            .getTargetContext()
+                                                            .getApplicationContext());
         mTempDir = ContextUtils.getApplicationContext().getCacheDir();
         assert mTempDir.exists();
     }
 
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
+    @After
+    public void tearDown() throws Exception {
         File[] files = mTempDir.listFiles();
         if (files == null) return;
 
@@ -43,16 +50,18 @@ public class CrashDumpManagerTest extends InstrumentationTestCase {
         }
     }
 
+    @Test
     @SmallTest
     @Feature({"Android-AppBase"})
     @DisabledTest // Flaky, crbug.com/725379.
     public void testUploadMinidump_NoCallback() throws IOException {
         File minidump = new File(mTempDir, "mini.dmp");
-        assertTrue(minidump.createNewFile());
+        Assert.assertTrue(minidump.createNewFile());
 
         CrashDumpManager.tryToUploadMinidump(minidump.getAbsolutePath());
     }
 
+    @Test
     @SmallTest
     @Feature({"Android-AppBase"})
     @DisabledTest // Flaky, crbug.com/725379.
@@ -60,7 +69,7 @@ public class CrashDumpManagerTest extends InstrumentationTestCase {
         registerUploadCallback(new CrashDumpManager.UploadMinidumpCallback() {
             @Override
             public void tryToUploadMinidump(File minidump) {
-                fail("The callback should not be called when the minidump path is null.");
+                Assert.fail("The callback should not be called when the minidump path is null.");
             }
         });
 
@@ -69,12 +78,14 @@ public class CrashDumpManagerTest extends InstrumentationTestCase {
 
     // @SmallTest
     // @Feature({"Android-AppBase"})
+    @Test
     @DisabledTest // Flaky, crbug.com/725379.
     public void testUploadMinidump_FileDoesntExist() {
         registerUploadCallback(new CrashDumpManager.UploadMinidumpCallback() {
             @Override
             public void tryToUploadMinidump(File minidump) {
-                fail("The callback should not be called when the minidump file doesn't exist.");
+                Assert.fail(
+                        "The callback should not be called when the minidump file doesn't exist.");
             }
         });
 
@@ -82,34 +93,37 @@ public class CrashDumpManagerTest extends InstrumentationTestCase {
                 mTempDir.getAbsolutePath() + "/some/file/that/doesnt/exist");
     }
 
+    @Test
     @SmallTest
     @Feature({"Android-AppBase"})
     @DisabledTest // Flaky, crbug.com/725379.
     public void testUploadMinidump_MinidumpPathIsDirectory() throws IOException {
         File directory = new File(mTempDir, "a_directory");
-        assertTrue(directory.mkdir());
+        Assert.assertTrue(directory.mkdir());
 
         registerUploadCallback(new CrashDumpManager.UploadMinidumpCallback() {
             @Override
             public void tryToUploadMinidump(File minidump) {
-                fail("The callback should not be called when the minidump path is not a file.");
+                Assert.fail(
+                        "The callback should not be called when the minidump path is not a file.");
             }
         });
 
         CrashDumpManager.tryToUploadMinidump(directory.getAbsolutePath());
     }
 
+    @Test
     @SmallTest
     @Feature({"Android-AppBase"})
     @DisabledTest // Flaky, crbug.com/725379.
     public void testUploadMinidump_MinidumpPathIsValid() throws IOException {
         final File minidump = new File(mTempDir, "mini.dmp");
-        assertTrue(minidump.createNewFile());
+        Assert.assertTrue(minidump.createNewFile());
 
         registerUploadCallback(new CrashDumpManager.UploadMinidumpCallback() {
             @Override
             public void tryToUploadMinidump(File actualMinidump) {
-                assertEquals("Should call the callback with the correct filename.", minidump,
+                Assert.assertEquals("Should call the callback with the correct filename.", minidump,
                         actualMinidump);
             }
         });
--- components/sync/android/javatests/src/org/chromium/components/sync/notifier/InvalidationClientNameProviderTest.java
+++ components/sync/android/javatests/src/org/chromium/components/sync/notifier/InvalidationClientNameProviderTest.java
@@ -6,21 +6,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.components.sync.notifier;
 
 import android.support.test.filters.SmallTest;
-import android.test.InstrumentationTestCase;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.Feature;
 
 import java.util.Arrays;
 
 /** Tests for the {@link InvalidationClientNameProvider} */
-public class InvalidationClientNameProviderTest extends InstrumentationTestCase {
+@RunWith(BaseJUnit4ClassRunner.class)
+public class InvalidationClientNameProviderTest {
     private InvalidationClientNameProvider mProvider;
 
-    @Override
-    protected void setUp() {
+    @Before
+    public void setUp() {
         mProvider = new InvalidationClientNameProvider();
     }
 
+    @Test
     @SmallTest
     @Feature({"Sync"})
     public void testFallbackClientId() {
@@ -30,7 +37,7 @@ public class InvalidationClientNameProviderTest extends InstrumentationTestCase 
         byte[] id2 = mProvider.getInvalidatorClientName();
 
         // We expect the returned IDs to be consistent in every call.
-        assertTrue("Expected returned IDs to be consistent", Arrays.equals(id1, id2));
+        Assert.assertTrue("Expected returned IDs to be consistent", Arrays.equals(id1, id2));
 
         // Even if initialize the generator late, the ID will remain consistent.
         registerHardCodedGenerator(mProvider);
@@ -40,10 +47,11 @@ public class InvalidationClientNameProviderTest extends InstrumentationTestCase 
         // getInvalidatorClientName() and never change afterwards.  We test this anyway to make sure
         // nothing will blow up if someone accidentally violates that constraint.)
         byte[] id3 = mProvider.getInvalidatorClientName();
-        assertTrue("Changing generators should not affect returned ID consistency",
+        Assert.assertTrue("Changing generators should not affect returned ID consistency",
                 Arrays.equals(id2, id3));
     }
 
+    @Test
     @SmallTest
     @Feature({"Sync"})
     public void testPreRegisteredGenerator() {
@@ -53,7 +61,7 @@ public class InvalidationClientNameProviderTest extends InstrumentationTestCase 
         byte[] id2 = mProvider.getInvalidatorClientName();
 
         // Expect that consistent IDs are maintained when using a custom generator, too.
-        assertTrue("Custom generators should return consistent IDs", Arrays.equals(id, id2));
+        Assert.assertTrue("Custom generators should return consistent IDs", Arrays.equals(id, id2));
     }
 
     private static void registerHardCodedGenerator(InvalidationClientNameProvider provider) {
