965f8bdf9fc16597f10d0eb7766bbb5f15ac4bc5
Yoshifumi Inoue <yosin@chromium.org>
2017-07-25 01:13:47 +0000

    Make set_selection_by parameter of FrameSelection::Modify() as required parameter
    This patch changes |FrameSelection::Modify()| to take |set_selection_by| as
    required parameter as required parameter as a preparation of moving declaration
    of enum class |SelectionBy| to another file[1].
    
    This patch is similar to the patch[2].
    
    
    [1] http://crrev.com/c/569662: ALL-IN-ONE Introduce SetSelectionData for
    [2] http://crrev.com/c/580175: Introduce no parameter variant of
    
    FrameSelection: :SetSelection()
    FrameSelection: :SelectAll() to avoid default parameter
    Change-Id: Ic0bc5dc0eb532a062a6cd2122030ece71be875b8
    Reviewed-on: https://chromium-review.googlesource.com/582256
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489176}

--- third_party/WebKit/Source/core/editing/FrameSelection.h
+++ third_party/WebKit/Source/core/editing/FrameSelection.h
@@ -149,7 +149,7 @@ class CORE_EXPORT FrameSelection final
   bool Modify(SelectionModifyAlteration,
               SelectionModifyDirection,
               TextGranularity,
-              SetSelectionBy = SetSelectionBy::kSystem);
+              SetSelectionBy);
 
   // Moves the selection extent based on the selection granularity strategy.
   // This function does not allow the selection to collapse. If the new
