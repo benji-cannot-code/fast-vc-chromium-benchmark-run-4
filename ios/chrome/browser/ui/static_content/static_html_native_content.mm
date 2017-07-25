a3d56aeb5ac2743c7bb00de0a4b03ce9a54cb445
Olivier Robin <olivierrobin@chromium.org>
2017-07-25 16:38:14 +0000

    Fix ExternalURLTabUsageRecorderTestCase.testEvictedTabReloadFailure
    Resetting _staticHTMLViewController will deallog the WKWebView.
    The scroll view must be detached before that.
    
    Bug: 748404
    Change-Id: I89ce9c8f1e8b23ab72184617510635b24b9bb077
    Reviewed-on: https://chromium-review.googlesource.com/584467
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489333}

--- ios/chrome/browser/ui/static_content/static_html_native_content.mm
+++ ios/chrome/browser/ui/static_content/static_html_native_content.mm
@@ -157,6 +157,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
   _webUsageEnabled = webUsageEnabled;
   if (!_webUsageEnabled) {
+    [_overscrollActionsController invalidate];
+    [[self scrollView] setDelegate:nil];
     _staticHTMLViewController = nil;
   }
 }
