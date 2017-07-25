92f7930cd490fbcd885e7ff9544001b47d5457e8
Ted Choc <tedchoc@google.com>
2017-07-25 21:08:59 +0000

    Fix NPE in first run if the activity has been destroyed.
    BUG=748161
    
    Change-Id: Ic188d622ed44982d0c9cb138711e2e907b8d49aa
    Reviewed-on: https://chromium-review.googlesource.com/583710
    Reviewed-by: Alexei Svitkine (slow) <asvitkine@chromium.org>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489430}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunActivity.java
@@ -293,6 +293,8 @@ public class FirstRunActivity extends AsyncInitializationActivity implements Fir
         Runnable onNativeFinished = new Runnable() {
             @Override
             public void run() {
+                if (isActivityDestroyed()) return;
+
                 onNativeDependenciesFullyInitialized();
             }
         };
