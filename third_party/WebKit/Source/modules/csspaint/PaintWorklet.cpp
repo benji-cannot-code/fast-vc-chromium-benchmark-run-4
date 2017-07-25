8f8f92a05b50f0484e93a7d89a5e043e51e26d7e
Bruce Dawson <brucedawson@chromium.org>
2017-07-25 08:11:09 +0000

    Only declare one kInvalidDocumentDefinition object
    In crrev.com/c/562197 two translation units both defined
    kInvalidDocumentDefinition. Since the two definitions were static this
    is legal, but confusing. One of the objects was tagged as const* and the
    other one wasn't, leading to more potential confusion.
    
    Both missed the opportunity to tag the pointer itself as const.
    
    Most importantly, these duplicate declarations break jumbo builds so
    this CL defines this variable once and declares it in a header file, as
    a const pointer to a non-const DocumentPaintDefinition.
    
    TBR=ikilpatrick@chromium.org
    
    Change-Id: Ief20cc3e9e355ba046dabda41fcdea0a0a227ca3
    Reviewed-on: https://chromium-review.googlesource.com/583900
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489253}

--- third_party/WebKit/Source/modules/csspaint/PaintWorklet.cpp
+++ third_party/WebKit/Source/modules/csspaint/PaintWorklet.cpp
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-static const DocumentPaintDefinition* kInvalidDocumentDefinition = nullptr;
+DocumentPaintDefinition* const kInvalidDocumentDefinition = nullptr;
 
 // static
 PaintWorklet* PaintWorklet::Create(LocalFrame* frame) {
--- third_party/WebKit/Source/modules/csspaint/PaintWorkletGlobalScope.cpp
+++ third_party/WebKit/Source/modules/csspaint/PaintWorkletGlobalScope.cpp
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-static DocumentPaintDefinition* kInvalidDocumentDefinition = nullptr;
-
 // static
 PaintWorkletGlobalScope* PaintWorkletGlobalScope::Create(
     LocalFrame* frame,
