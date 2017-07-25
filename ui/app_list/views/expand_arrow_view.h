92148db7a048b9529fea58c0a4a75b5913ccf457
Weidong Guo <weidongg@chromium.org>
2017-07-25 03:16:25 +0000

    Fix search box issue when expand arrow has focus
    Changes:
    1. Override OnKeyPressed function in ExpandArrowView to prevent SPACE
    key being handled by parent view.
    2. Avoid returning true for unhandled key event in
    StartPageView::OnKeyPressed.
    
    BUG=747146
    
    Change-Id: Ie3a195b133ba57040299deb1bbe542ed80462e1c
    Reviewed-on: https://chromium-review.googlesource.com/582235
    Commit-Queue: Weidong Guo <weidongg@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489207}

--- ui/app_list/views/expand_arrow_view.h
+++ ui/app_list/views/expand_arrow_view.h
@@ -41,6 +41,7 @@ class ExpandArrowView : public views::CustomButton,
   // Overridden from views::View:
   gfx::Size CalculatePreferredSize() const override;
   void Layout() override;
+  bool OnKeyPressed(const ui::KeyEvent& event) override;
 
   // Overridden from views::InkDropHost:
   std::unique_ptr<views::InkDrop> CreateInkDrop() override;
@@ -50,6 +51,8 @@ class ExpandArrowView : public views::CustomButton,
       const override;
 
  private:
+  void TransitToFullscreenAllAppsState();
+
   ContentsView* const contents_view_;
   AppListView* const app_list_view_;  // Owned by the views hierarchy.
 
