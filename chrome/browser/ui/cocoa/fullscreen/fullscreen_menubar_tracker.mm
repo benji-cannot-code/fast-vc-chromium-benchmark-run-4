92ef5ec4ffecdbebaf3d4992c53b1a291fb0be96
Sidney San Martín <sdy@chromium.org>
2017-07-25 17:46:09 +0000

    Call +[CATransaction flush] for the toolbar to move with the menu bar.
    Bug: 672254
    Change-Id: I1f6a8a6e0cc5437a67703650732b3ed12c785351
    Reviewed-on: https://chromium-review.googlesource.com/581859
    Commit-Queue: Sidney San Martin <sdy@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Reviewed-by: Sarah Chan <spqchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489358}

--- chrome/browser/ui/cocoa/fullscreen/fullscreen_menubar_tracker.mm
+++ chrome/browser/ui/cocoa/fullscreen/fullscreen_menubar_tracker.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/fullscreen/fullscreen_menubar_tracker.h"
 
 #include <Carbon/Carbon.h>
+#include <QuartzCore/QuartzCore.h>
 
 #include "base/mac/mac_util.h"
 #include "base/macros.h"
@@ -142,21 +143,9 @@ OSStatus MenuBarRevealHandler(EventHandlerCallRef handler,
   menubarFraction_ = progress;
   [owner_ updateToolbarLayout];
 
-  // In 10.12. the toolbar to be janky since the UI doesn't update until the
-  // menubar finished revealing itself. To smooth things out, animate the
-  // toolbar in/out by locking/releasing its visibility instead of relying on
-  // the menubar fraction.
-  // TODO(spqchan): Figure out why it's not updating and make the toolbar drop
-  // down in sync with the menubar. See crbug.com/672254.
-  if (base::mac::IsOS10_12()) {
-    if (state_ == FullscreenMenubarState::SHOWING) {
-      [[owner_ visibilityLockController] lockToolbarVisibilityForOwner:self
-                                                         withAnimation:YES];
-    } else if (state_ == FullscreenMenubarState::HIDING) {
-      [[owner_ visibilityLockController] releaseToolbarVisibilityForOwner:self
-                                                            withAnimation:YES];
-    }
-  }
+  // AppKit drives the menu bar animation from a nested run loop. Flush
+  // explicitly so that Chrome's UI updates during the animation.
+  [CATransaction flush];
 }
 
 - (BOOL)isMouseOnScreen {
