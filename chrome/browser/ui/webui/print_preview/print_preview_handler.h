d4575206efc1735bc3f19ebe37447047515b1115
rbpotter <rbpotter@chromium.org>
2017-07-25 23:55:14 +0000

    Fix chrome://print crash
    Unlike normal print preview dialogs, chrome://print can be reloaded,
    which results in a call to DisallowJavascript() and reloading of the
    print preview web contents without destroying the PrintPreviewHandler.
    This means capabilities fetch may return after the print preview has
    beent torn down but before it has been re-initialized, and the handler
    will incorrectly try to resolve a javascript promise.
    
    Bug: 748356
    Change-Id: I857fbcd8ce5bb69b88383d961b680c1dd7c41275
    Reviewed-on: https://chromium-review.googlesource.com/585548
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489486}

--- chrome/browser/ui/webui/print_preview/print_preview_handler.h
+++ chrome/browser/ui/webui/print_preview/print_preview_handler.h
@@ -64,6 +64,8 @@ class PrintPreviewHandler
   // WebUIMessageHandler implementation.
   void RegisterMessages() override;
 
+  void OnJavascriptDisallowed() override;
+
   // SelectFileDialog::Listener implementation.
   void FileSelected(const base::FilePath& path,
                     int index,
