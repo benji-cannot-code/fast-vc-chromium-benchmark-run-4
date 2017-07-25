7b13a3d992f387a8451dc588c06b31841f18d77c
Francois Doray <fdoray@chromium.org>
2017-07-25 15:56:38 +0000

    Initialize TaskScheduler in views examples.
    A TaskScheduler is required for
    ui::X11HotplugEventHandler::OnHotplugEvent().
    
    Bug: 735971
    Change-Id: I05979686cdcf2f5b3fe59f6cd2929f69e9c9c62a
    Reviewed-on: https://chromium-review.googlesource.com/544779
    Commit-Queue: Francois Doray <fdoray@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489317}

--- ui/views/examples/examples_main.cc
+++ ui/views/examples/examples_main.cc
@@ -10,11 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/i18n/icu_util.h"
 #include "base/memory/ptr_util.h"
-#include "base/message_loop/message_loop.h"
 #include "base/path_service.h"
 #include "base/power_monitor/power_monitor.h"
 #include "base/power_monitor/power_monitor_device_source.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/test/test_discardable_memory_allocator.h"
 #include "build/build_config.h"
 #include "components/viz/host/host_frame_sink_manager.h"
@@ -74,7 +74,8 @@ int main(int argc, char** argv) {
       &host_frame_sink_manager_, &frame_sink_manager_);
   context_factory->set_use_test_surface(false);
 
-  base::MessageLoopForUI message_loop;
+  base::test::ScopedTaskEnvironment scoped_task_environment(
+      base::test::ScopedTaskEnvironment::MainThreadType::UI);
 
   base::i18n::InitializeICU();
 
