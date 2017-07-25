398ff82c593ec39016de169d7c750f0f7bf4d380
Pavel Kalinnikov <pkalinnikov@chromium.org>
2017-07-25 10:15:14 +0000

    Revert "Add touch selection for PDF files."
    This reverts commit 9218acc1309c18e11e255dd99cfb282140cc5e62.
    
    Reason for revert: The PDFResourceTest.SelectionChanged test introduced by this CL fails consistently on "Linux MSan Tests" and "Linux Chromium MSan Tests" bots.
    
    Original change's description:
    > Add touch selection for PDF files.
    > 
    > This CL plumbs through the need methods to allow touch selection with
    > the Copy action in PDF files.
    > 
    > Bug: chromium:490184, chromium:737585
    > Change-Id: I4dab62833dac94b6c8ed59cbcc7db8e9387a7fde
    > Reviewed-on: https://chromium-review.googlesource.com/565899
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Reviewed-by: Tom Sepez <tsepez@chromium.org>
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Reviewed-by: Raymes Khoury <raymes@chromium.org>
    > Reviewed-by: James MacLean <wjmaclean@chromium.org>
    > Commit-Queue: Dan Sinclair <dsinclair@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#489071}
    
    TBR=raymes@chromium.org,thestig@chromium.org,dsinclair@chromium.org,jam@chromium.org,tsepez@chromium.org,wjmaclean@chromium.org
    
    Change-Id: I66c58582a3a14ad93ecf29e743de1071c44d19f0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:490184, chromium:737585
    Reviewed-on: https://chromium-review.googlesource.com/584613
    Reviewed-by: Pavel Kalinnikov <pkalinnikov@chromium.org>
    Commit-Queue: Pavel Kalinnikov <pkalinnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489270}

--- components/pdf/renderer/pepper_pdf_host.h
+++ components/pdf/renderer/pepper_pdf_host.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "components/pdf/common/pdf.mojom.h"
 #include "ipc/ipc_platform_file.h"
-#include "mojo/public/cpp/bindings/binding.h"
 #include "ppapi/c/ppb_image_data.h"
 #include "ppapi/c/private/ppb_pdf.h"
 #include "ppapi/host/resource_host.h"
@@ -38,8 +37,7 @@ namespace pdf {
 
 class PdfAccessibilityTree;
 
-class PepperPDFHost : public ppapi::host::ResourceHost,
-                      public mojom::PdfListener {
+class PepperPDFHost : public ppapi::host::ResourceHost {
  public:
   class PrintClient {
    public:
@@ -74,12 +72,6 @@ class PepperPDFHost : public ppapi::host::ResourceHost,
       const IPC::Message& msg,
       ppapi::host::HostMessageContext* context) override;
 
-  // mojom::PdfListener
-  void SetCaretPosition(const gfx::PointF& position) override;
-  void MoveRangeSelectionExtent(const gfx::PointF& extent) override;
-  void SetSelectionBounds(const gfx::PointF& base,
-                          const gfx::PointF& extent) override;
-
  private:
   int32_t OnHostMsgDidStartLoading(ppapi::host::HostMessageContext* context);
   int32_t OnHostMsgDidStopLoading(ppapi::host::HostMessageContext* context);
@@ -108,11 +100,6 @@ class PepperPDFHost : public ppapi::host::ResourceHost,
       const PP_PrivateAccessibilityPageInfo& page_info,
       const std::vector<PP_PrivateAccessibilityTextRunInfo>& text_runs,
       const std::vector<PP_PrivateAccessibilityCharInfo>& chars);
-  int32_t OnHostMsgSelectionChanged(ppapi::host::HostMessageContext* context,
-                                    const PP_FloatPoint& left,
-                                    int32_t left_height,
-                                    const PP_FloatPoint& right,
-                                    int32_t right_height);
 
   void CreatePdfAccessibilityTreeIfNeeded();
 
@@ -123,7 +110,6 @@ class PepperPDFHost : public ppapi::host::ResourceHost,
   std::unique_ptr<PdfAccessibilityTree> pdf_accessibility_tree_;
   content::RendererPpapiHost* const host_;
   mojom::PdfServiceAssociatedPtr remote_pdf_service_;
-  mojo::Binding<mojom::PdfListener> binding_;
 
   DISALLOW_COPY_AND_ASSIGN(PepperPDFHost);
 };
