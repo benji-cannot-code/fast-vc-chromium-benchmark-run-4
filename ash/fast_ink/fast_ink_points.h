8e79908952bdfc948fccadedc00494437094ccc7
Vladislav Kaznacheev <kaznacheev@google.com>
2017-07-25 21:04:42 +0000

    Reland: Extract prediction logic from LaserPointerView
    (Added a missing include that MSAN bots seem to require)
    
    Renamed LaserPointerPoints to FastInkPoints and
    move it under ash/fast_ink.
    
    Moved prediction logic into FastInkPoints.
    
    Added a unit test for prediction.
    
    This should allow other tools such as highlighter
    to use the common prediction logic.
    
    TBR=oshima@chromium.org
    
    Bug: 743083
    Change-Id: Iaa626a00c01188fd4aae468b47c0e44e8f29235b
    Reviewed-on: https://chromium-review.googlesource.com/585267
    Commit-Queue: Vladislav Kaznacheev <kaznacheev@chromium.org>
    Reviewed-by: Vladislav Kaznacheev <kaznacheev@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489428}

--- ash/fast_ink/fast_ink_points.h
+++ ash/fast_ink/fast_ink_points.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_LASER_LASER_POINTER_POINTS_H_
-#define ASH_LASER_LASER_POINTER_POINTS_H_
+#ifndef ASH_FAST_INK_FAST_INK_POINTS_H_
+#define ASH_FAST_INK_FAST_INK_POINTS_H_
 
 #include <deque>
 #include <memory>
@@ -17,26 +17,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-// LaserPointerPoints is a helper class used for displaying the palette tool
-// laser pointer. It keeps track of the points needed to render the laser
-// pointer and its tail.
-class ASH_EXPORT LaserPointerPoints {
+// FastInkPoints is a helper class used for displaying low-latency palette tools
+// It keeps track of the points needed to render the image.
+class ASH_EXPORT FastInkPoints {
  public:
   // Struct to describe each point.
-  struct LaserPoint {
+  struct FastInkPoint {
     gfx::PointF location;
     base::TimeTicks time;
-    // age is a value between [0,1] where 0 means the point was just added and 1
-    // means that the point is just about to be removed.
-    float age = 0.0f;
   };
 
   // Constructor with a parameter to choose the fade out time of the points in
-  // the collection.
-  explicit LaserPointerPoints(base::TimeDelta life_duration);
-  ~LaserPointerPoints();
+  // the collection. Zero means no fadeout.
+  explicit FastInkPoints(base::TimeDelta life_duration);
+  ~FastInkPoints();
 
-  // Adds a point. Automatically clears points that are too old.
+  // Adds a point.
   void AddPoint(const gfx::PointF& point, const base::TimeTicks& time);
   // Updates the collection latest time. Automatically clears points that are
   // too old.
@@ -44,30 +40,37 @@ class ASH_EXPORT LaserPointerPoints {
   // Removes all points.
   void Clear();
   // Gets the bounding box of the points.
-  gfx::Rect GetBoundingBox();
+  gfx::Rect GetBoundingBox() const;
   // Returns the oldest point in the collection.
-  LaserPoint GetOldest() const;
+  FastInkPoint GetOldest() const;
   // Returns the newest point in the collection.
-  LaserPoint GetNewest() const;
+  FastInkPoint GetNewest() const;
   // Returns the number of points in the collection.
   int GetNumberOfPoints() const;
   // Whether there are any points or not.
   bool IsEmpty() const;
   // Expose the collection so callers can work with the points.
-  const std::deque<LaserPoint>& laser_points() const;
+  const std::deque<FastInkPoint>& points() const;
+  // Returns the fadeout factor for a point. This is a value between 0.0 and
+  // 1.0, where 0.0 corresponds to a recently  added point, and 1.0 to a point
+  // that is about to expire. Do not call this method if |life_duration_| is 0.
+  float GetFadeoutFactor(int index) const;
+  // Fills the container with predicted points based on |real_points|.
+  void Predict(const FastInkPoints& real_points,
+               const base::TimeTicks& current_time,
+               base::TimeDelta prediction_duration,
+               const gfx::Size& screen_size);
 
  private:
-  friend class LaserPointerPointsTestApi;
-
-  base::TimeDelta life_duration_;
-  std::deque<LaserPoint> points_;
+  const base::TimeDelta life_duration_;
+  std::deque<FastInkPoint> points_;
   // The latest time of the collection of points. This gets updated when new
-  // points are added or when MoveForwardInTime is called.
+  // points are added or when MoveForwardToTime is called.
   base::TimeTicks collection_latest_time_;
 
-  DISALLOW_COPY_AND_ASSIGN(LaserPointerPoints);
+  DISALLOW_COPY_AND_ASSIGN(FastInkPoints);
 };
 
 }  // namespace ash
 
-#endif  // ASH_LASER_LASER_POINTER_POINTS_H_
+#endif  // ASH_FAST_INK_FAST_INK_POINTS_H_
