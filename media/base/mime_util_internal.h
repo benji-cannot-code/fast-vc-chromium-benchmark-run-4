09ed5f9b112fdffb04c54e59770a81de832147cb
Dale Curtis <dalecurtis@chromium.org>
2017-07-25 03:49:51 +0000

    Switch to flat containers and initializer lists for MimeUtil.
    There's been a todo to do this for a while. These are small enough
    that there's no perf diff and only memory wins for switching to
    flat containers.
    
    BUG=none
    TEST=all existing tests pass.
    
    Change-Id: Ibd9add8333d7e32cfc50ef68d8135736c6426349
    Reviewed-on: https://chromium-review.googlesource.com/584003
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489220}

--- media/base/mime_util_internal.h
+++ media/base/mime_util_internal.h
@@ -6,11 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_BASE_MIME_UTIL_INTERNAL_H_
 #define MEDIA_BASE_MIME_UTIL_INTERNAL_H_
 
-#include <map>
 #include <string>
 #include <vector>
 
-#include "base/containers/hash_tables.h"
+#include "base/containers/flat_map.h"
+#include "base/containers/flat_set.h"
 #include "base/macros.h"
 #include "media/base/media_export.h"
 #include "media/base/mime_util.h"
@@ -99,8 +99,8 @@ class MEDIA_EXPORT MimeUtil {
                                         const PlatformInfo& platform_info);
 
  private:
-  typedef base::hash_set<int> CodecSet;
-  typedef std::map<std::string, CodecSet> MediaFormatMappings;
+  typedef base::flat_set<int> CodecSet;
+  typedef base::flat_map<std::string, CodecSet> MediaFormatMappings;
 
   // Initializes the supported media types into hash sets for faster lookup.
   void InitializeMimeTypeMaps();
