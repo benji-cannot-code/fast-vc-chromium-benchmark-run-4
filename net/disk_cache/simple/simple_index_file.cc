38f5079c80ebaa4bbd87f0999aba3bb4bfab5c16
Maks Orlovich <morlovich@chromium.org>
2017-07-25 18:09:31 +0000

    SimpleCache: Factor out crc calcs, to avoid clutter from hideous cast
    Change-Id: I7cd224f982777e7200b8e0fd693a1d75e20d8471
    Reviewed-on: https://chromium-review.googlesource.com/581803
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489370}

--- net/disk_cache/simple/simple_index_file.cc
+++ net/disk_cache/simple/simple_index_file.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/disk_cache/simple/simple_index.h"
 #include "net/disk_cache/simple/simple_synchronous_entry.h"
 #include "net/disk_cache/simple/simple_util.h"
-#include "third_party/zlib/zlib.h"
 
 using base::File;
 
@@ -44,9 +43,7 @@ const int64_t kMaxIndexFileSizeBytes =
     kMaxEntriesInIndex * (8 + EntryMetadata::kOnDiskSizeBytes);
 
 uint32_t CalculatePickleCRC(const base::Pickle& pickle) {
-  return crc32(crc32(0, Z_NULL, 0),
-               reinterpret_cast<const Bytef*>(pickle.payload()),
-               pickle.payload_size());
+  return simple_util::Crc32(pickle.payload(), pickle.payload_size());
 }
 
 // Used in histograms. Please only add new values at the end.
--- net/disk_cache/simple/simple_util.cc
+++ net/disk_cache/simple/simple_util.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_restrictions.h"
 #include "base/time/time.h"
 #include "net/disk_cache/simple/simple_entry_format.h"
+#include "third_party/zlib/zlib.h"
 
 namespace {
 
@@ -101,6 +102,17 @@ bool GetMTime(const base::FilePath& path, base::Time* out_mtime) {
     return false;
   *out_mtime = file_info.last_modified;
   return true;
+}
+
+uint32_t Crc32(const char* data, int length) {
+  uint32_t empty_crc = crc32(0, Z_NULL, 0);
+  if (length == 0)
+    return empty_crc;
+  return crc32(empty_crc, reinterpret_cast<const Bytef*>(data), length);
+}
+
+uint32_t IncrementalCrc32(uint32_t previous_crc, const char* data, int length) {
+  return crc32(previous_crc, reinterpret_cast<const Bytef*>(data), length);
 }
 
 }  // namespace simple_util
