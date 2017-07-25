1c527da714218d1235ac76e3d12037c342c39b33
Robert Liao <robliao@chromium.org>
2017-07-25 18:15:18 +0000

    ScopedComInitializer Cleanup
    * Removed custom thread ID checking in favor of THREAD_CHECKER.
    * Removed LOG(ERROR) when COM is already initialized to the requested
      apartment. While undesirable, it's technically not an error to
      initialize COM multiple times to the same apartment. If we don't
      want Chrome code to do this, we can consider making this a DCHECK.
    
    BUG=
    
    Change-Id: Ib251aed188d860d4972ec1b42664d140526ee96c
    Reviewed-on: https://chromium-review.googlesource.com/583933
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489373}

--- base/win/scoped_com_initializer.h
+++ base/win/scoped_com_initializer.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/macros.h"
+#include "base/threading/thread_checker.h"
 #include "build/build_config.h"
 
 namespace base {
@@ -38,10 +39,7 @@ class ScopedCOMInitializer {
   }
 
   ~ScopedCOMInitializer() {
-#ifndef NDEBUG
-    // Using the windows API directly to avoid dependency on platform_thread.
-    DCHECK_EQ(GetCurrentThreadId(), thread_id_);
-#endif
+    DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
     if (succeeded())
       CoUninitialize();
   }
@@ -50,26 +48,13 @@ class ScopedCOMInitializer {
 
  private:
   void Initialize(COINIT init) {
-#ifndef NDEBUG
-    thread_id_ = GetCurrentThreadId();
-#endif
+    DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
     hr_ = CoInitializeEx(NULL, init);
-#ifndef NDEBUG
-    if (hr_ == S_FALSE)
-      LOG(ERROR) << "Multiple CoInitialize() calls for thread " << thread_id_;
-    else
-      DCHECK_NE(RPC_E_CHANGED_MODE, hr_) << "Invalid COM thread model change";
-#endif
+    DCHECK_NE(RPC_E_CHANGED_MODE, hr_) << "Invalid COM thread model change";
   }
 
   HRESULT hr_;
-#ifndef NDEBUG
-  // In debug builds we use this variable to catch a potential bug where a
-  // ScopedCOMInitializer instance is deleted on a different thread than it
-  // was initially created on.  If that ever happens it can have bad
-  // consequences and the cause can be tricky to track down.
-  DWORD thread_id_;
-#endif
+  THREAD_CHECKER(thread_checker_);
 
   DISALLOW_COPY_AND_ASSIGN(ScopedCOMInitializer);
 };
