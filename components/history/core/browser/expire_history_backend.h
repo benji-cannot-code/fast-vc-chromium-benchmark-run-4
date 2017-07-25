4940ecf5cb0248c19b1f7c4130a6ddb3e8e16d5a
jkrcal <jkrcal@chromium.org>
2017-07-25 08:23:07 -0700

    [Thumbnails DB] Add functionality to clear unused on-demand favicons.
    The preceding CL 2856873002 adds a concept of on-demand favicons into
    ThumbnailsDatabase that should get evicted based on last_requested
    timestamp.
    
    This CL adds the functionality to clear on-demand favicons that have not
    been used for long.
    
    BUG=709471
    
    Review-Url: https://codereview.chromium.org/2903573002
    Cr-Commit-Position: refs/heads/master@{#489307}

--- components/history/core/browser/expire_history_backend.h
+++ components/history/core/browser/expire_history_backend.h
@@ -21,6 +21,7 @@ class GURL;
 class TestingProfile;
 
 namespace base {
+struct Feature;
 class SequencedTaskRunner;
 }
 
@@ -42,6 +43,11 @@ class ExpiringVisitsReader {
 };
 
 typedef std::vector<const ExpiringVisitsReader*> ExpiringVisitsReaders;
+
+namespace internal {
+// Feature that enables clearing old on-demand favicons.
+extern const base::Feature kClearOldOnDemandFavicons;
+}  // namespace internal
 
 // Helper component to HistoryBackend that manages expiration and deleting of
 // history.
@@ -103,6 +109,16 @@ class ExpireHistoryBackend {
   FRIEND_TEST_ALL_PREFIXES(ExpireHistoryTest, ExpireSomeOldHistory);
   FRIEND_TEST_ALL_PREFIXES(ExpireHistoryTest, ExpiringVisitsReader);
   FRIEND_TEST_ALL_PREFIXES(ExpireHistoryTest, ExpireSomeOldHistoryWithSource);
+  FRIEND_TEST_ALL_PREFIXES(ExpireHistoryTest,
+                           ClearOldOnDemandFaviconsDoesNotDeleteStarred);
+  FRIEND_TEST_ALL_PREFIXES(ExpireHistoryTest,
+                           ClearOldOnDemandFaviconsDoesDeleteUnstarred);
+  FRIEND_TEST_ALL_PREFIXES(ExpireHistoryTest,
+                           ClearOldOnDemandFaviconsDoesDeleteAfterLongDelay);
+  FRIEND_TEST_ALL_PREFIXES(
+      ExpireHistoryTest,
+      ClearOldOnDemandFaviconsDoesNotDeleteAfterShortDelay);
+
   friend class ::TestingProfile;
 
   struct DeleteEffects {
@@ -207,6 +223,9 @@ class ExpireHistoryBackend {
   // future.
   void DoExpireIteration();
 
+  // Clears all old on-demand favicons from thumbnail database.
+  void ClearOldOnDemandFavicons(base::Time expiration_threshold);
+
   // Tries to expire the oldest |max_visits| visits from history that are older
   // than |time_threshold|. The return value indicates if we think there might
   // be more history to expire with the current time threshold (it does not
@@ -240,6 +259,9 @@ class ExpireHistoryBackend {
 
   // The threshold for "old" history where we will automatically delete it.
   base::TimeDelta expiration_threshold_;
+
+  // The lastly used threshold for "old" on-demand favicons.
+  base::Time last_on_demand_expiration_threshold_;
 
   // List of all distinct types of readers. This list is used to populate the
   // work queue.
