2277e5eb0d56312eaba0f76bc58d12c34795733a
kylechar <kylechar@chromium.org>
2017-07-25 00:10:59 +0000

    Register frame sink hierarchy through HostFrameSinkManager.
    This CL changes calls to (R|Unr)egisterFrameSinkHierarchy() to go
    through HostFrameSinkManager instead of FrameSinkManagerImpl. By
    default, HostFrameSinkManager just calls into FrameSinkManagerImpl so
    the change should be a no-op change.
    
    Change the internal workings of HostFrameSinkManager to accommodate
    this.  Originally, the frame sink hierarchy unregisters itself on
    destruction of a mojom::CompositorFrameSink. This isn't compatible with
    how CompositorFrameSinkSupport works so remove that code. There
    shouldn't be any ordering requirements for the order of
    creating/destroying the CompositorFrameSink/CompositorFrameSinkSupport
    vs registering frame sink hierarchy.
    
    Also improve the two tests in HostFrameSinkManagerTests.
    
    Bug: 742374
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I537f36305d9060223975ce6e54b33bf760b844d4
    Reviewed-on: https://chromium-review.googlesource.com/583630
    Commit-Queue: kylechar <kylechar@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489153}

--- content/browser/frame_host/render_widget_host_view_child_frame.cc
+++ content/browser/frame_host/render_widget_host_view_child_frame.cc
@@ -112,8 +112,8 @@ void RenderWidgetHostViewChildFrame::SetCrossProcessFrameConnector(
   if (frame_connector_) {
     if (parent_frame_sink_id_.is_valid() &&
         !service_manager::ServiceManagerIsRemote()) {
-      GetFrameSinkManager()->UnregisterFrameSinkHierarchy(parent_frame_sink_id_,
-                                                          frame_sink_id_);
+      GetHostFrameSinkManager()->UnregisterFrameSinkHierarchy(
+          parent_frame_sink_id_, frame_sink_id_);
     }
     parent_frame_sink_id_ = viz::FrameSinkId();
     local_surface_id_ = viz::LocalSurfaceId();
@@ -130,8 +130,8 @@ void RenderWidgetHostViewChildFrame::SetCrossProcessFrameConnector(
       parent_frame_sink_id_ = parent_view->GetFrameSinkId();
       DCHECK(parent_frame_sink_id_.is_valid());
       if (!service_manager::ServiceManagerIsRemote()) {
-        GetFrameSinkManager()->RegisterFrameSinkHierarchy(parent_frame_sink_id_,
-                                                          frame_sink_id_);
+        GetHostFrameSinkManager()->RegisterFrameSinkHierarchy(
+            parent_frame_sink_id_, frame_sink_id_);
       }
     }
 
@@ -845,8 +845,8 @@ void RenderWidgetHostViewChildFrame::CreateCompositorFrameSinkSupport() {
       this, frame_sink_id_, is_root, handles_frame_sink_id_invalidation,
       needs_sync_points);
   if (parent_frame_sink_id_.is_valid()) {
-    GetFrameSinkManager()->RegisterFrameSinkHierarchy(parent_frame_sink_id_,
-                                                      frame_sink_id_);
+    GetHostFrameSinkManager()->RegisterFrameSinkHierarchy(parent_frame_sink_id_,
+                                                          frame_sink_id_);
   }
   if (host_->needs_begin_frames())
     support_->SetNeedsBeginFrame(true);
@@ -856,8 +856,8 @@ void RenderWidgetHostViewChildFrame::ResetCompositorFrameSinkSupport() {
   if (!support_)
     return;
   if (parent_frame_sink_id_.is_valid()) {
-    GetFrameSinkManager()->UnregisterFrameSinkHierarchy(parent_frame_sink_id_,
-                                                        frame_sink_id_);
+    GetHostFrameSinkManager()->UnregisterFrameSinkHierarchy(
+        parent_frame_sink_id_, frame_sink_id_);
   }
   support_.reset();
 }
--- content/browser/renderer_host/compositor_impl_android.cc
+++ content/browser/renderer_host/compositor_impl_android.cc
@@ -917,8 +917,8 @@ viz::FrameSinkId CompositorImpl::GetFrameSinkId() {
 
 void CompositorImpl::AddChildFrameSink(const viz::FrameSinkId& frame_sink_id) {
   if (has_layer_tree_frame_sink_) {
-    GetFrameSinkManager()->RegisterFrameSinkHierarchy(frame_sink_id_,
-                                                      frame_sink_id);
+    GetHostFrameSinkManager()->RegisterFrameSinkHierarchy(frame_sink_id_,
+                                                          frame_sink_id);
   } else {
     pending_child_frame_sink_ids_.insert(frame_sink_id);
   }
@@ -931,8 +931,8 @@ void CompositorImpl::RemoveChildFrameSink(
     pending_child_frame_sink_ids_.erase(it);
     return;
   }
-  GetFrameSinkManager()->UnregisterFrameSinkHierarchy(frame_sink_id_,
-                                                      frame_sink_id);
+  GetHostFrameSinkManager()->UnregisterFrameSinkHierarchy(frame_sink_id_,
+                                                          frame_sink_id);
 }
 
 bool CompositorImpl::HavePendingReadbacks() {
