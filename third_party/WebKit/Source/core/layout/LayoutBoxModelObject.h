a68635c5cc75246b833963e86aa535eb0ff2538e
Yi Gu <yigu@chromium.org>
2017-07-25 06:32:04 +0000

    Remove unused variable in LayoutBoxModelObjectRareData
    Bug: 
    Change-Id: I05d702a929d0f95c90434795e895640f402a88b0
    Reviewed-on: https://chromium-review.googlesource.com/578493
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489243}

--- third_party/WebKit/Source/core/layout/LayoutBoxModelObject.h
+++ third_party/WebKit/Source/core/layout/LayoutBoxModelObject.h
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/BackgroundBleedAvoidance.h"
 #include "core/layout/ContentChangeType.h"
 #include "core/layout/LayoutObject.h"
-#include "core/page/scrolling/StickyPositionScrollingConstraints.h"
 #include "platform/geometry/LayoutRect.h"
 #include "platform/wtf/PtrUtil.h"
 
@@ -60,16 +59,6 @@ enum LinePositionMode {
   kPositionOfInteriorLineBoxes
 };
 enum LineDirectionMode { kHorizontalLine, kVerticalLine };
-
-struct LayoutBoxModelObjectRareData {
-  WTF_MAKE_NONCOPYABLE(LayoutBoxModelObjectRareData);
-  USING_FAST_MALLOC(LayoutBoxModelObjectRareData);
-
- public:
-  LayoutBoxModelObjectRareData() {}
-
-  StickyPositionScrollingConstraints sticky_position_scrolling_constraints_;
-};
 
 // This class is the base class for all CSS objects.
 //
@@ -553,14 +542,6 @@ class CORE_EXPORT LayoutBoxModelObject : public LayoutObject {
 
   LayoutUnit ComputedCSSPadding(const Length&) const;
   bool IsBoxModelObject() const final { return true; }
-
-  LayoutBoxModelObjectRareData& EnsureRareData() {
-    if (!rare_data_)
-      rare_data_ = WTF::MakeUnique<LayoutBoxModelObjectRareData>();
-    return *rare_data_.get();
-  }
-
-  std::unique_ptr<LayoutBoxModelObjectRareData> rare_data_;
 };
 
 DEFINE_LAYOUT_OBJECT_TYPE_CASTS(LayoutBoxModelObject, IsBoxModelObject());
