4a0e45d94610d49581112f84bc345af2160f60b6
Jun Cai <juncai@chromium.org>
2017-07-25 19:03:43 +0000

    Implement RELATIVE_ORIENTATION_QUATERNION using euler angles on macOS
    This CL implements the RELATIVE_ORIENTATION_QUATERNION sensor type
    using data from RELATIVE_ORIENTATION_EULER_ANGLES on macOS. It just
    needs a conversion from euler angles to quaternion.
    
    Bug: 733707
    Change-Id: I621c6fe208ced6a0c904965985f3b8519eb24f50
    Reviewed-on: https://chromium-review.googlesource.com/583484
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489393}

---
+++ services/device/generic_sensor/orientation_quaternion_fusion_algorithm_using_euler_angles.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_DEVICE_ORIENTATION_QUATERNION_FUSION_ALGORITHM_USING_EULER_ANGLES_H_
+#define SERVICES_DEVICE_ORIENTATION_QUATERNION_FUSION_ALGORITHM_USING_EULER_ANGLES_H_
+
+#include "base/macros.h"
+#include "services/device/generic_sensor/platform_sensor_fusion_algorithm.h"
+
+namespace device {
+
+// Sensor fusion algorithm for implementing *_ORIENTATION_QUATERNION
+// using *_ORIENTATION_EULER_ANGLES.
+class OrientationQuaternionFusionAlgorithmUsingEulerAngles
+    : public PlatformSensorFusionAlgorithm {
+ public:
+  OrientationQuaternionFusionAlgorithmUsingEulerAngles();
+  ~OrientationQuaternionFusionAlgorithmUsingEulerAngles() override;
+
+  void GetFusedData(const std::vector<SensorReading>& readings,
+                    SensorReading* fused_reading) override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(
+      OrientationQuaternionFusionAlgorithmUsingEulerAngles);
+};
+
+}  // namespace device
+
+#endif  // SERVICES_DEVICE_ORIENTATION_QUATERNION_FUSION_ALGORITHM_USING_EULER_ANGLES_H_
--- services/device/generic_sensor/platform_sensor_provider_mac.cc
+++ services/device/generic_sensor/platform_sensor_provider_mac.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ptr_util.h"
 #include "base/memory/singleton.h"
+#include "services/device/generic_sensor/orientation_quaternion_fusion_algorithm_using_euler_angles.h"
 #include "services/device/generic_sensor/platform_sensor_accelerometer_mac.h"
 #include "services/device/generic_sensor/platform_sensor_ambient_light_mac.h"
 #include "services/device/generic_sensor/platform_sensor_fusion.h"
@@ -48,11 +49,28 @@ void PlatformSensorProviderMac::CreateSensorInternal(
       auto relative_orientation_euler_angles_fusion_algorithm_using_accelerometer =
           base::MakeUnique<
               RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometer>();
+      // If a PlatformSensorFusion object is created successfully, the caller
+      // of this function holds the reference to the object.
       base::MakeRefCounted<PlatformSensorFusion>(
           std::move(mapping), this, callback, source_sensor_types,
           mojom::SensorType::RELATIVE_ORIENTATION_EULER_ANGLES,
           std::move(
               relative_orientation_euler_angles_fusion_algorithm_using_accelerometer));
+      break;
+    }
+    case mojom::SensorType::RELATIVE_ORIENTATION_QUATERNION: {
+      std::vector<mojom::SensorType> source_sensor_types = {
+          mojom::SensorType::RELATIVE_ORIENTATION_EULER_ANGLES};
+      auto orientation_quaternion_fusion_algorithm_using_euler_angles =
+          base::MakeUnique<
+              OrientationQuaternionFusionAlgorithmUsingEulerAngles>();
+      // If a PlatformSensorFusion object is created successfully, the caller
+      // of this function holds the reference to the object.
+      base::MakeRefCounted<PlatformSensorFusion>(
+          std::move(mapping), this, callback, source_sensor_types,
+          mojom::SensorType::RELATIVE_ORIENTATION_QUATERNION,
+          std::move(
+              orientation_quaternion_fusion_algorithm_using_euler_angles));
       break;
     }
     default:
