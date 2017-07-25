ca8e9f52924573d63cff182b8c29ca10e363186d
Nasko Oskov <nasko@chromium.org>
2017-07-25 02:45:42 +0000

    Revert "No more filtering out of IPC messages in swapped-out state."
    This reverts commit 00043bdefebe1d854b0cffbd1d35d90c0091057c.
    
    Reason for revert: Removing the swapped out IPC filtering resulted in some layout tests to fail with Site Isolation. The ShellViewHostMsg_TextDump and ShellViewHostMsg_TestFinished IPCs are no longer filtered, which results in different test expectations.
    The failing tests are:
    http/tests/security/mixedContent/insecure-prefetch-in-main-frame.html
    http/tests/security/mixedContent/blob-url-in-iframe.html
    http/tests/inspector/change-iframe-src.html
    
    Original change's description:
    > No more filtering out of IPC messages in swapped-out state.
    > 
    > Bug: 745091
    > Change-Id: I869a71ea38b0bd7e8631ef7b78cdb75a8468520e
    > Reviewed-on: https://chromium-review.googlesource.com/568740
    > Commit-Queue: Nasko Oskov <nasko@chromium.org>
    > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#488859}
    
    TBR=nasko@chromium.org,lukasza@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 745091
    Change-Id: Iaa7affcc1439ea0c6cf1c56f2a99bf82aa612d11
    Reviewed-on: https://chromium-review.googlesource.com/584034
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489197}

--- content/renderer/render_widget.cc
+++ content/renderer/render_widget.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/input/synthetic_gesture_packet.h"
 #include "content/common/input_messages.h"
 #include "content/common/render_message_filter.mojom.h"
+#include "content/common/swapped_out_messages.h"
 #include "content/common/text_input_state.h"
 #include "content/common/view_messages.h"
 #include "content/public/common/content_client.h"
@@ -665,8 +666,11 @@ bool RenderWidget::OnMessageReceived(const IPC::Message& message) {
 }
 
 bool RenderWidget::Send(IPC::Message* message) {
-  // Don't send any messages after the browser has told us to close.
-  if (closing_) {
+  // Don't send any messages after the browser has told us to close, and filter
+  // most outgoing messages while swapped out.
+  if ((is_swapped_out_ &&
+       !SwappedOutMessages::CanSendWhileSwappedOut(message)) ||
+      closing_) {
     delete message;
     return false;
   }
@@ -759,7 +763,7 @@ void RenderWidget::OnResize(const ResizeParams& params) {
 }
 
 void RenderWidget::OnEnableDeviceEmulation(
-    const blink::WebDeviceEmulationParams& params) {
+   const blink::WebDeviceEmulationParams& params) {
   if (!screen_metrics_emulator_) {
     ResizeParams resize_params;
     resize_params.screen_info = screen_info_;
@@ -1990,9 +1994,8 @@ void RenderWidget::SetHidden(bool hidden) {
   if (is_hidden_) {
     RenderThreadImpl::current()->WidgetHidden();
     time_to_first_active_paint_recorded_ = false;
-  } else {
+  } else
     RenderThreadImpl::current()->WidgetRestored();
-  }
 
   if (render_widget_scheduling_state_)
     render_widget_scheduling_state_->SetHidden(hidden);
