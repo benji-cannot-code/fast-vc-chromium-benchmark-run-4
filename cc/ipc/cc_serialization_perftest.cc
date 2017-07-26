677d94e9cb660cc088788f3bd79707e05bcebfb0
Pavel Kalinnikov <pkalinnikov@chromium.org>
2017-07-26 09:48:54 +0000

    Revert "Mojo bindings: API to serialize struct -> Message"
    This reverts commit 8a0f44c5d3ccc6f345472ffd9591c9d748b90618.
    
    Reason for revert: Might be the cause of StructTest.Serialization_PublicAPI failing on "Win7 Tests (dbg)": https://uberchromegw.corp.google.com/i/chromium.win/builders/Win7%20Tests%20%28dbg%29%281%29/builds/61850
    
    Original change's description:
    > Mojo bindings: API to serialize struct -> Message
    > 
    > This allows callers to serialize a mojom struct directly into a Message
    > object, ensuring that the struct's on-demand serialization logic can
    > mirror what is used in production bindings code when serializing full
    > messages.
    > 
    > Updates cc_perftests to use this API in its serialization perf tests,
    > eliminating a large amount of redundant allocation, writing, and copying
    > that is not done by the analogous ParamTraits test cases.
    > 
    > This does not resolve the Linux perf regression introduced by switching
    > to libc++, but it does bring the work done by StructTraits test cases in
    > line with the work done by ParamTraits test cases, revealing the actual
    > regression to be quite a bit smaller (~2x rather than ~3.5x).
    > 
    > BUG=747525
    > 
    > Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    > Change-Id: Ia0cd5cdaa9d92e210ffbc9a3b031431fff781fb5
    > Reviewed-on: https://chromium-review.googlesource.com/584004
    > Reviewed-by: danakj <danakj@chromium.org>
    > Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    > Commit-Queue: Ken Rockot <rockot@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#489497}
    
    TBR=danakj@chromium.org,rockot@chromium.org,yzshen@chromium.org
    
    Change-Id: Ic006cf30fc186e720b334fcbe7280e075027a9ec
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 747525
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Reviewed-on: https://chromium-review.googlesource.com/586267
    Reviewed-by: Pavel Kalinnikov <pkalinnikov@chromium.org>
    Commit-Queue: Pavel Kalinnikov <pkalinnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489593}

--- cc/ipc/cc_serialization_perftest.cc
+++ cc/ipc/cc_serialization_perftest.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/ipc/common/mailbox_struct_traits.h"
 #include "gpu/ipc/common/sync_token_struct_traits.h"
 #include "ipc/ipc_message.h"
-#include "mojo/public/cpp/bindings/message.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/perf/perf_test.h"
 #include "third_party/skia/include/effects/SkBlurImageFilter.h"
@@ -142,11 +141,11 @@ class CCSerializationPerfTest : public testing::Test {
       const std::string& test_name,
       const CompositorFrame& frame,
       UseSingleSharedQuadState single_sqs) {
-    mojo::Message message = mojom::CompositorFrame::SerializeAsMessage(&frame);
+    auto data = mojom::CompositorFrame::Serialize(&frame);
+    DCHECK_GT(data.size(), 0u);
     for (int i = 0; i < kNumWarmupRuns; ++i) {
       CompositorFrame compositor_frame;
-      mojom::CompositorFrame::Deserialize(
-          message.payload(), message.payload_num_bytes(), &compositor_frame);
+      mojom::CompositorFrame::Deserialize(data, &compositor_frame);
     }
 
     base::TimeTicks start = base::TimeTicks::Now();
@@ -158,8 +157,7 @@ class CCSerializationPerfTest : public testing::Test {
     while (start < end) {
       for (int i = 0; i < kTimeCheckInterval; ++i) {
         CompositorFrame compositor_frame;
-        mojom::CompositorFrame::Deserialize(
-            message.payload(), message.payload_num_bytes(), &compositor_frame);
+        mojom::CompositorFrame::Deserialize(data, &compositor_frame);
         now = base::TimeTicks::Now();
         // We don't count iterations after the end time.
         if (now < end)
@@ -191,8 +189,8 @@ class CCSerializationPerfTest : public testing::Test {
       const CompositorFrame& frame,
       UseSingleSharedQuadState single_sqs) {
     for (int i = 0; i < kNumWarmupRuns; ++i) {
-      mojo::Message message =
-          mojom::CompositorFrame::SerializeAsMessage(&frame);
+      auto data = mojom::CompositorFrame::Serialize(&frame);
+      DCHECK_GT(data.size(), 0u);
     }
 
     base::TimeTicks start = base::TimeTicks::Now();
@@ -203,8 +201,8 @@ class CCSerializationPerfTest : public testing::Test {
     size_t count = 0;
     while (start < end) {
       for (int i = 0; i < kTimeCheckInterval; ++i) {
-        mojo::Message message =
-            mojom::CompositorFrame::SerializeAsMessage(&frame);
+        auto data = mojom::CompositorFrame::Serialize(&frame);
+        DCHECK_GT(data.size(), 0u);
         now = base::TimeTicks::Now();
         // We don't count iterations after the end time.
         if (now < end)
