0d94dffb6c0f1b05cceb636d635d9fc9ea7fc1f5
Kent Tamura <tkent@chromium.org>
2017-07-26 05:31:43 +0000

    macOS: Fix 'ButtonFace' CSS color, and update the CSS button rendering
    * This CL fixes 'ButtonFace' CSS system color
     It was a fixed gray, and now it returns a real button color retrieved
     from OS. It's white nowadays.
    
    * Update CSS-rendered default button to match macOS 10.12.
     - Has border-radius
     - Update border-color and border-style
     The CSS-rendered button is used instead of OS-native rendering in
     non-100% zoom level, or with additional CSS properties. It should be
     similar to OS-native buttons.
    
    This CL affects only macOS, and doesn't change the default size of
    buttons.
    
    Implementation:
    
    * themeMac.css, LayoutThemeMac.mm:
     The main change. Update ButtonFace definition and button default style.
    
    * colorSuggestionPicker.css, PaintPropertyTreeBuilderTest.cpp, and many
    tests:
      Set border-radius:0 to keep existing behavior.
    
    * auto-click.js, feature-policy-vibrate.html,
    iframe-that-performs-parent-navigation.html,
    iframe-that-performs-parent-navigation-async.html, and
    vibrate-from-iframe.html:
      Update mouse-click position for border-radius.
    
    * Other tests:
     - Update rendering result due to the rendering change, or
     - Update only rendering tree dump because native button rendering
      is used.
    
    Bug: 546679
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I0ef2ae135a6ac0677040d48c435fedc3af379481
    Reviewed-on: https://chromium-review.googlesource.com/584149
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489558}

--- third_party/WebKit/Source/core/paint/PaintPropertyTreeBuilderTest.cpp
+++ third_party/WebKit/Source/core/paint/PaintPropertyTreeBuilderTest.cpp
@@ -1069,6 +1069,7 @@ TEST_P(PaintPropertyTreeBuilderTest, ControlClip) {
       "    margin: 0;"
       "  }"
       "  input {"
+      "    border-radius: 0;"
       "    border-width: 5px;"
       "    padding: 0;"
       "  }"
