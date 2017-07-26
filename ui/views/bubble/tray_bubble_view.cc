1561458be8603f6f40eb31925d595a71b80f8268
Valery Arkhangorodsky <varkha@chromium.org>
2017-07-26 02:19:50 +0000

    [mus+ash] Refactors BubbleWindowTargeter, SearchBoxWindowTargeter and TouchHandleWindowTargeter
    Original idea was to convert MaskedWindowTargeter subclasses to be subclasses of the
    enhanced WindowTargeter instead. However the such subclasses are not really needed anymore:
    BubbleWindowTargeter was used for app-list and system tray menu bubble when they could be anchored
    to the shelf with a visible arrow tip. This functionality is no longer necessary.
      
    SearchBoxWindowTargeter was used on a search box in app-list preventing clicks on a shadow from
    reaching the search box. The shadow is small enough and doesn't seem to warrant special treatment.
    Also the UI elements are no longer positioned directly next to the search box making this
    special case unnecessary.
    
    TouchHandleWindowTargeter conversion relies on the observation that
    GetWidgetHitTestMask() returns the same bounds as the widget bounds when
    offset by the cursor height and a small padding (kSelectionHandleVerticalVisualOffset).
    
    This CL follows a plan in https://docs.google.com/document/d/1ruwo-mqyh2bPG1MGnbXHqyF7lLj5yKPrBdqVaU0y9F4/edit#heading=h.ido2dtmpweg5
    
    Bug: 732395
    Change-Id: I28992032573b4f607bc87504917691ca3f5dd047
    Reviewed-on: https://chromium-review.googlesource.com/567486
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Valery Arkhangorodsky <varkha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489531}

--- ui/views/bubble/tray_bubble_view.cc
+++ ui/views/bubble/tray_bubble_view.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/path.h"
 #include "ui/gfx/skia_util.h"
 #include "ui/views/bubble/bubble_frame_view.h"
-#include "ui/views/bubble/bubble_window_targeter.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/painter.h"
 #include "ui/views/widget/widget.h"
@@ -207,8 +206,6 @@ void TrayBubbleView::InitializeAndShowBubble() {
   layer()->parent()->SetMaskLayer(bubble_content_mask_->layer());
 
   GetWidget()->Show();
-  GetWidget()->GetNativeWindow()->SetEventTargeter(
-      std::unique_ptr<ui::EventTargeter>(new BubbleWindowTargeter(this)));
   UpdateBubble();
 
   ++g_current_tray_bubble_showing_count_;
