ef873aa89593cced7f4e4aa0cc93353c1d6e6db8
Steven Bennetts <stevenjb@chromium.org>
2017-07-26 16:52:25 +0000

    Remove chrome://settings-frame on CrOS
    This removes all remaining references to chrome://settings-frame
    and removes the URL.
    
    It does not remove the options code yet, see the list of issues
    blocking crbug.com/728353
    
    This also removes quick_unlock_configure_overlay.js  since it
    included a reference to chrome://settings-frame and is only
    used by the now fully deprecated options UI.
    
    Bug: 748164
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I1b44f8a9c4b4eeefb972ae4b29eec95a8969220c
    Reviewed-on: https://chromium-review.googlesource.com/583935
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489672}

--- chrome/common/url_constants.h
+++ chrome/common/url_constants.h
@@ -79,8 +79,6 @@ extern const char kChromeUIQuitURL[];
 extern const char kChromeUIRestartURL[];
 extern const char kChromeUISettingsURL[];
 extern const char kChromeUIContentSettingsURL[];
-// TODO(dbeam): remove settings-frame.
-extern const char kChromeUISettingsFrameURL[];
 extern const char kChromeUISigninEmailConfirmationURL[];
 extern const char kChromeUISigninErrorURL[];
 extern const char kChromeUISiteDetailsPrefixURL[];
@@ -231,7 +229,6 @@ extern const char kChromeUIQuotaInternalsHost[];
 extern const char kChromeUIQuitHost[];
 extern const char kChromeUIRestartHost[];
 extern const char kChromeUISettingsHost[];
-extern const char kChromeUISettingsFrameHost[];
 extern const char kChromeUIShorthangHost[];
 extern const char kChromeUISigninEmailConfirmationHost[];
 extern const char kChromeUISigninErrorHost[];
