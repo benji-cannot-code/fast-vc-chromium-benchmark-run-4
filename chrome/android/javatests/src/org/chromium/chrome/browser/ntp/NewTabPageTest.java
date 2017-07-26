c1feb2aebeab22c63272af64c037646ff5c66733
Peter E Conn <peconn@chromium.org>
2017-07-26 10:15:30 +0000

    📷 Enable Render Tests and encode Android API level in goldens.
    This makes goldens specific to both the device model and Android SDK
    version and enables render tests on the Nexus 5 Kitkat. It also removes
    the golden images for Nexus 5Xs since chrome_public_test_apk is not
    run on Nexus 5Xs on the CQ.
    
    Bug: 641973
    Change-Id: I46dd428489820f814355d51d266d256fcf189c4c
    Reviewed-on: https://chromium-review.googlesource.com/583067
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489597}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
@@ -34,6 +34,7 @@ import org.chromium.chrome.browser.UrlConstants;
 import org.chromium.chrome.browser.ntp.cards.NewTabPageRecyclerView;
 import org.chromium.chrome.browser.omnibox.LocationBarLayout;
 import org.chromium.chrome.browser.omnibox.UrlBar;
+import org.chromium.chrome.browser.preferences.ChromePreferenceManager;
 import org.chromium.chrome.browser.suggestions.FakeMostVisitedSites;
 import org.chromium.chrome.browser.suggestions.TileSource;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
@@ -101,6 +102,9 @@ public class NewTabPageTest {
 
     @Before
     public void setUp() throws Exception {
+        // Disable peeking card animation.
+        ChromePreferenceManager.getInstance().setNewTabPageFirstCardAnimationRunCount(100);
+
         mTestServer = EmbeddedTestServer.createAndStartServer(
                 InstrumentationRegistry.getInstrumentation().getContext());
 
@@ -134,6 +138,7 @@ public class NewTabPageTest {
     @MediumTest
     @Feature({"NewTabPage", "RenderTest"})
     public void testRender() throws IOException {
+        mActivityTestRule.getInstrumentation().waitForIdleSync();
         mRenderTestRule.render(mTileGridLayout, "most_visited");
         mRenderTestRule.render(mFakebox, "fakebox");
         mRenderTestRule.render(mNtp.getView().getRootView(), "new_tab_page");
