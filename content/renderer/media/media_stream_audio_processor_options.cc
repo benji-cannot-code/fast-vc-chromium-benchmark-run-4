b3165341d1de4156d1e2460c69085f43c56e9dcc
Alex Loiko <aleloi@chromium.org>
2017-07-26 09:51:11 +0000

    AecDump recording in Chrome to new interface
    The functionality to record acoustic echo cancellation dumps from the
    Audio Processing module has been rewritten. It's now possible to make
    the file operations on a task queue instead of on the audio threads.
    
    This CL makes the recording in Chrome happen on a rtc::TaskQueue by
    using the new interface. A single TaskQueue is optionally created in
    media_stream_audio_processor when recording is started. A TaskQueue
    always starts a new thread when created. To keep the number of threads
    down, we only create the TQ when needed, and delete in when logging
    has ended.
    
    Caveat: An rtc::TaskQueue has to be started and stopped from the same
    thread. MediaStreamAudioProssor provides a guarantee that this is the
    main thread.
    
    Bug: webrtc:7404, webrtc:5928
    
    Change-Id: I000e38ba009e1b24eee6ee6fce5dc308de3a6b11
    Reviewed-on: https://chromium-review.googlesource.com/576431
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Alex Loiko <aleloi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489594}

--- content/renderer/media/media_stream_audio_processor_options.cc
+++ content/renderer/media/media_stream_audio_processor_options.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/media/media_stream_constraints_util.h"
 #include "content/renderer/media/media_stream_source.h"
 #include "media/base/audio_parameters.h"
+#include "third_party/webrtc/modules/audio_processing/aec_dump/aec_dump_factory.h"
 #include "third_party/webrtc/modules/audio_processing/include/audio_processing.h"
 #include "third_party/webrtc/modules/audio_processing/typing_detection.h"
 
@@ -469,22 +470,27 @@ void EnableTypingDetection(AudioProcessing* audio_processing,
 }
 
 void StartEchoCancellationDump(AudioProcessing* audio_processing,
-                               base::File aec_dump_file) {
+                               base::File aec_dump_file,
+                               rtc::TaskQueue* worker_queue) {
   DCHECK(aec_dump_file.IsValid());
 
   FILE* stream = base::FileToFILE(std::move(aec_dump_file), "w");
   if (!stream) {
-    LOG(ERROR) << "Failed to open AEC dump file";
+    LOG(DFATAL) << "Failed to open AEC dump file";
     return;
   }
 
-  if (audio_processing->StartDebugRecording(stream))
-    DLOG(ERROR) << "Fail to start AEC debug recording";
+  auto aec_dump = webrtc::AecDumpFactory::Create(
+      stream, -1 /* max_log_size_bytes */, worker_queue);
+  if (!aec_dump) {
+    LOG(ERROR) << "Failed to start AEC debug recording";
+    return;
+  }
+  audio_processing->AttachAecDump(std::move(aec_dump));
 }
 
 void StopEchoCancellationDump(AudioProcessing* audio_processing) {
-  if (audio_processing->StopDebugRecording())
-    DLOG(ERROR) << "Fail to stop AEC debug recording";
+  audio_processing->DetachAecDump();
 }
 
 void EnableAutomaticGainControl(AudioProcessing* audio_processing) {
--- content/renderer/media/media_stream_audio_processor_options.h
+++ content/renderer/media/media_stream_audio_processor_options.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/webrtc/api/mediastreaminterface.h"
 #include "third_party/webrtc/media/base/mediachannel.h"
 #include "third_party/webrtc/modules/audio_processing/include/audio_processing.h"
+#include "third_party/webrtc_overrides/webrtc/rtc_base/task_queue.h"
 
 namespace webrtc {
 
@@ -189,9 +190,13 @@ void EnableNoiseSuppression(AudioProcessing* audio_processing,
 void EnableTypingDetection(AudioProcessing* audio_processing,
                            webrtc::TypingDetection* typing_detector);
 
-// Starts the echo cancellation dump in |audio_processing|.
+// Starts the echo cancellation dump in
+// |audio_processing|. |worker_queue| must be kept alive until either
+// |audio_processing| is destroyed, or
+// StopEchoCancellationDump(audio_processing) is called.
 void StartEchoCancellationDump(AudioProcessing* audio_processing,
-                               base::File aec_dump_file);
+                               base::File aec_dump_file,
+                               rtc::TaskQueue* worker_queue);
 
 // Stops the echo cancellation dump in |audio_processing|.
 // This method has no impact if echo cancellation dump has not been started on
