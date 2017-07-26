ccbd40d886b61e4f99f218156b5d601c3e5bdeb2
Yaron Friedman <yfriedman@chromium.org>
2017-07-26 18:40:39 +0000

    Revert breakage from  Issue 2960843003: Remove UMA.Debug.EnableCrashUpload.*
    We need to early-out *before* calling onDeferredStartup so that we don't
    unnecessarily invoke subclasses' implementation of onDeferredStartup
    BUG=749134
    
    Change-Id: I9b9b6aeda41c21e1b3c7166866dbf31102f716e1
    Reviewed-on: https://chromium-review.googlesource.com/587035
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489709}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -976,9 +976,6 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
      * super depending on whether the tasks should run before or after these ones.
      */
     protected void onDeferredStartup() {
-        if (mDeferredStartupPosted) return;
-
-        mDeferredStartupPosted = true;
         initDeferredStartupForActivity();
         ProcessInitializationHandler.getInstance().initializeDeferredStartupTasks();
         DeferredStartupHandler.getInstance().queueDeferredTasksOnIdleHandler();
@@ -2140,7 +2137,11 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
             return;
         }
         mDeferredStartupQueued = false;
-        onDeferredStartup();
+
+        if (!mDeferredStartupPosted) {
+            mDeferredStartupPosted = true;
+            onDeferredStartup();
+        }
     }
 
     @Override
