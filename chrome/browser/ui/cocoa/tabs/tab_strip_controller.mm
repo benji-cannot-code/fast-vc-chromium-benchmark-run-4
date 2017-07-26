aede95b2ab1d5fba9f3eb7e51110158fc80258ef
Avi Drissman <avi@chromium.org>
2017-07-26 16:38:44 +0000

    Use ObjCCast in TabStripController.
    This is just clean-up; no functional change intended.
    
    BUG=747117
    
    Change-Id: Icf562942ae45efddb721e90d1b3c81d2967e1034
    Reviewed-on: https://chromium-review.googlesource.com/586643
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489666}

--- chrome/browser/ui/cocoa/tabs/tab_strip_controller.mm
+++ chrome/browser/ui/cocoa/tabs/tab_strip_controller.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/command_line.h"
+#include "base/mac/foundation_util.h"
 #include "base/mac/mac_util.h"
 #include "base/mac/scoped_nsautorelease_pool.h"
 #include "base/mac/sdk_forward_declarations.h"
@@ -1690,8 +1691,8 @@ NSRect FlipRectInView(NSView* view, NSRect rect) {
   [tabContentsArray_ insertObject:movedTabContentsController.get()
                           atIndex:to];
   base::scoped_nsobject<TabController> movedTabController(
-      [[tabArray_ objectAtIndex:from] retain]);
-  DCHECK([movedTabController isKindOfClass:[TabController class]]);
+      base::mac::ObjCCastStrict<TabController>(
+          [[tabArray_ objectAtIndex:from] retain]));
   [tabArray_ removeObjectAtIndex:from];
   [tabArray_ insertObject:movedTabController.get() atIndex:to];
 
@@ -1708,8 +1709,8 @@ NSRect FlipRectInView(NSView* view, NSRect rect) {
   // Take closing tabs into account.
   NSInteger index = [self indexFromModelIndex:modelIndex];
 
-  TabController* tabController = [tabArray_ objectAtIndex:index];
-  DCHECK([tabController isKindOfClass:[TabController class]]);
+  TabController* tabController =
+      base::mac::ObjCCastStrict<TabController>([tabArray_ objectAtIndex:index]);
 
   // Don't do anything if the change was already picked up by the move event.
   if (tabStripModel_->IsTabPinned(modelIndex) == [tabController pinned])
@@ -1867,7 +1868,7 @@ NSRect FlipRectInView(NSView* view, NSRect rect) {
 }
 
 - (void)mouseMoved:(NSEvent*)event {
-  // We don't want the draggged tab to repeatedly redraw its glow unnecessarily.
+  // We don't want the dragged tab to repeatedly redraw its glow unnecessarily.
   // We also want the dragged tab to keep the glow even when it slides behind
   // another tab.
   if ([dragController_ draggedTab])
@@ -1880,14 +1881,9 @@ NSRect FlipRectInView(NSView* view, NSRect rect) {
   BOOL shouldShowHoverImage = [targetView isKindOfClass:[NewTabButton class]];
   [self setNewTabButtonHoverState:shouldShowHoverImage];
 
-  TabView* tabView = (TabView*)targetView;
-  if (![tabView isKindOfClass:[TabView class]]) {
-    if ([[tabView superview] isKindOfClass:[TabView class]]) {
-      tabView = (TabView*)[targetView superview];
-    } else {
-      tabView = nil;
-    }
-  }
+  TabView* tabView = base::mac::ObjCCast<TabView>(targetView);
+  if (!tabView)
+    tabView = base::mac::ObjCCast<TabView>([targetView superview]);
 
   if (hoveredTab_ != tabView) {
     [self setHoveredTab:tabView];
@@ -2052,8 +2048,7 @@ NSRect FlipRectInView(NSView* view, NSRect rect) {
   NSInteger i = 0;
   BOOL isRTL = cocoa_l10n_util::ShouldDoExperimentalRTLLayout();
   for (TabController* tab in tabArray_.get()) {
-    NSView* view = [tab view];
-    DCHECK([view isKindOfClass:[TabView class]]);
+    TabView* view = base::mac::ObjCCastStrict<TabView>([tab view]);
 
     // Recall that |-[NSView frame]| is in its superview's coordinates, so a
     // |TabView|'s frame is in the coordinates of the |TabStripView| (which
