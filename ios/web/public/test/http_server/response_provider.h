ce67c9f31e7426bca281c894b8497dab70174b59
Eugene But <eugenebut@google.com>
2017-07-26 03:57:07 +0000

    Removed gcdwebserver.
    TBR=baxley@chromium.org
    
    Bug: None
    Change-Id: Iacf5e1303fff013f75bb83a787c624a702029f52
    Reviewed-on: https://chromium-review.googlesource.com/582202
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489547}

--- ios/web/public/test/http_server/response_provider.h
+++ ios/web/public/test/http_server/response_provider.h
@@ -17,8 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/test/embedded_test_server/http_response.h"
 #include "url/gurl.h"
 
-@class GCDWebServerResponse;
-
 namespace web {
 
 // An abstract class for a provider that services a request and returns a
