bcd6dcadccbd3e3fb849a8d333ec4347c8e06ba1
gambard <gambard@chromium.org>
2017-07-26 09:41:13 +0000

    Pull-to-refresh reloads the suggestions
    When the pull-to-refresh overscroll action is used, the suggestions
    should be reloaded. If the suggestions are stale, the gesture will
    update the content.
    
    Bug: 740932,700375
    Change-Id: I4c811c2d57d2b8ba78c3f1cad33e9466eda4a135
    Reviewed-on: https://chromium-review.googlesource.com/585010
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489581}

--- ios/chrome/browser/content_suggestions/content_suggestions_coordinator.mm
+++ ios/chrome/browser/content_suggestions/content_suggestions_coordinator.mm
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_most_visited_item.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_utils.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_commands.h"
+#import "ios/chrome/browser/ui/content_suggestions/content_suggestions_data_sink.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_header_synchronizer.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller_audience.h"
@@ -411,7 +412,7 @@ const char kNTPHelpURL[] = "https://support.google.com/chrome/?p=new_tab";
 }
 
 - (void)reload {
-  // TODO(crbug.com/700375): implement this.
+  [self.contentSuggestionsMediator.dataSink reloadAllData];
 }
 
 - (void)wasShown {
