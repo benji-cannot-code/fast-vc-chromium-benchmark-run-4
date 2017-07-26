7a8f82238d2052b5e2df7f027f98020ba4a14255
Mike Bjorge <mbjorge@chromium.org>
2017-07-26 23:13:09 +0000

    [Chromecast] Init Mojo in cast_graphics_unittests
    https://chromium-review.googlesource.com/c/549118/ added a requirement
    that test targets using ui::InitializeContextFactoryForTests()
    initialize the mojo::edk.
    
    Bug: 657959, 749144
    Test: cast_graphics_unittests
    Change-Id: Ibdd7ff732eed4c74a9e5f2160b1b42b34cae75d6
    Reviewed-on: https://chromium-review.googlesource.com/587127
    Reviewed-by: Alok Priyadarshi <alokp@chromium.org>
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Mike Bjorge <mbjorge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489792}

--- chromecast/graphics/run_all_unittests.cc
+++ chromecast/graphics/run_all_unittests.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/test/launcher/unit_test_launcher.h"
 #include "base/test/test_suite.h"
+#include "mojo/edk/embedder/embedder.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/aura/env.h"
 #include "ui/base/resource/resource_bundle.h"
@@ -45,6 +46,8 @@ class ChromecastGraphicsTestSuite : public base::TestSuite {
 
 int main(int argc, char** argv) {
   ChromecastGraphicsTestSuite test_suite(argc, argv);
+
+  mojo::edk::Init();
 
   return base::LaunchUnitTests(argc, argv,
                                base::Bind(&ChromecastGraphicsTestSuite::Run,
