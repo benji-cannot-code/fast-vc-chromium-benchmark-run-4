a5836720c73723022c25968664dfd1bdd05c6e5b
Diana Gage <drgage@google.com>
2017-07-26 04:25:25 +0000

    Add CanCut() method to check if plugin text can be cut.
    CanCut() can be utilized by the web plugin container to ask plugins if
    they can cut text. With respect to the PDF plugin, this method returns
    true if focus is within a user-editable form text area and returns
    false otherwise.
    
    BUG=59266
    
    Change-Id: Iefcd8314f231c7fad5d555c9981d7b38ce064441
    Reviewed-on: https://chromium-review.googlesource.com/572402
    Commit-Queue: Diana Gage <drgage@google.com>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Alexandre Elias <aelias@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Dan Sinclair <dsinclair@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489551}

--- pdf/pdfium/pdfium_engine.h
+++ pdf/pdfium/pdfium_engine.h
@@ -73,6 +73,7 @@ class PDFiumEngine : public PDFEngine,
   void RotateClockwise() override;
   void RotateCounterclockwise() override;
   std::string GetSelectedText() override;
+  bool CanCut() override;
   std::string GetLinkAtPosition(const pp::Point& point) override;
   bool HasPermission(DocumentPermission permission) const override;
   void SelectAll() override;
@@ -676,6 +677,10 @@ class PDFiumEngine : public PDFEngine,
 
   // True if focus is in form text field or form combobox text field.
   bool in_form_text_area_;
+
+  // True if the form text area currently in focus is not read only, and is a
+  // form text field or user-editable form combobox text field.
+  bool editable_form_text_area_;
 
   // True if left mouse button is currently being held down.
   bool mouse_left_button_down_;
