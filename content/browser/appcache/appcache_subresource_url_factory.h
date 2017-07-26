b64df05de250394fe8d751274165c902476bdce9
ananta <ananta@chromium.org>
2017-07-26 11:44:35 -0700

    The Appcache subresource URL factory needs to inform the URLLoaderClient if there is a failure.
    Failure cases include, an invalid AppCacheHost instance, failure to create a request handler, etc.
    We already notify the client about the latter case above. Need to do this for an invalid host as well.
    This caused the w3schools AppCache test page to spin indefinitely if we pass in an invalid URL.
    
    Additionally on the renderer side we need to reset the cached URLLoaderFactory pointer if a null factory
    is passed in the CommitNavigation IPC. This ensures that the request hits the default network loader.
    
    BUG=715632
    TEST=Covered by browser test. AppCacheNetworkServiceBrowserTest.VerifySubresourceFactoryClearedOnNewNavigation
    
    Review-Url: https://codereview.chromium.org/2991443002
    Cr-Commit-Position: refs/heads/master@{#489710}

--- content/browser/appcache/appcache_subresource_url_factory.h
+++ content/browser/appcache/appcache_subresource_url_factory.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
+#include "content/common/content_export.h"
 #include "content/public/common/url_loader_factory.mojom.h"
 #include "mojo/public/cpp/bindings/binding.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
@@ -21,7 +22,8 @@ class AppCacheServiceImpl;
 class URLLoaderFactoryGetter;
 
 // Implements the URLLoaderFactory mojom for AppCache subresource requests.
-class AppCacheSubresourceURLFactory : public mojom::URLLoaderFactory {
+class CONTENT_EXPORT AppCacheSubresourceURLFactory
+    : public mojom::URLLoaderFactory {
  public:
   ~AppCacheSubresourceURLFactory() override;
 
@@ -53,11 +55,17 @@ class AppCacheSubresourceURLFactory : public mojom::URLLoaderFactory {
                 SyncLoadCallback callback) override;
 
  private:
+  friend class AppCacheNetworkServiceBrowserTest;
+
   AppCacheSubresourceURLFactory(mojom::URLLoaderFactoryRequest request,
                                 URLLoaderFactoryGetter* factory_getter,
                                 base::WeakPtr<AppCacheHost> host);
 
   void OnConnectionError();
+
+  // Notifies the |client| if there is a failure. The |error_code| contains the
+  // actual error.
+  void NotifyError(mojom::URLLoaderClientPtr client, int error_code);
 
   // Mojo binding.
   mojo::Binding<mojom::URLLoaderFactory> binding_;
