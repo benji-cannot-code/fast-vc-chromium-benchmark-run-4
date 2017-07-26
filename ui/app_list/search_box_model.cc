b6f23182bb3ddb1b983fdab8549202d4f2c9f6b7
Weidong Guo <weidongg@chromium.org>
2017-07-26 23:12:58 +0000

    Remove mic icon in new launcher
    Some background on the removal of the Mic button. There has been a chain
    going on about removing it until we figure out what our voice search
    strategy is with the new assistant. Since, we were using the existing
    voice search UI and a new experience adapted to our re-design was not
    specd, the decision to remove it was made. The existing voice search
    behaviour is broken and doesn't fit with our current re-design.
    
    BUG=748737
    
    Change-Id: Icedc79677badbe013ac11671c5be3d372b174685
    Reviewed-on: https://chromium-review.googlesource.com/585838
    Commit-Queue: Weidong Guo <weidongg@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489791}

--- ui/app_list/search_box_model.cc
+++ ui/app_list/search_box_model.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/metrics/histogram_macros.h"
+#include "ui/app_list/app_list_features.h"
 #include "ui/app_list/search_box_model_observer.h"
 
 namespace app_list {
@@ -35,6 +36,8 @@ SearchBoxModel::~SearchBoxModel() {
 
 void SearchBoxModel::SetSpeechRecognitionButton(
     std::unique_ptr<SearchBoxModel::SpeechButtonProperty> speech_button) {
+  if (features::IsFullscreenAppListEnabled())
+    return;
   speech_button_ = std::move(speech_button);
   for (auto& observer : observers_)
     observer.SpeechRecognitionButtonPropChanged();
