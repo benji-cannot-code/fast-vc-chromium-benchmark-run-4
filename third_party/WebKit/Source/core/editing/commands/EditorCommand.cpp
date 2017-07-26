a5d23ad128b4884f18d29a40c44e04eee5e371a1
Yoshifumi Inoue <yosin@chromium.org>
2017-07-26 01:37:59 +0000

    Move Editor::Transpose() out of Editor class
    This patch moves |Editor::Transpose()| out of |Editor| class as preparation of
    expanding it into |ExecutTranspose()| in "EditorCommand.cpp" to make |Editor|
    class simpler for improving code health.
    
    Following patch will expand |Transpose()| into |ExecutTranspose()|.
    
    Bug: 672405
    Change-Id: Icde253623f31813d2b4517c4da7d4798bd5fadf6
    Reviewed-on: https://chromium-review.googlesource.com/583880
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489518}

--- third_party/WebKit/Source/core/editing/commands/EditorCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/EditorCommand.cpp
@@ -1955,7 +1955,7 @@ static bool ExecuteTranspose(LocalFrame& frame,
                              Event*,
                              EditorCommandSource,
                              const String&) {
-  frame.GetEditor().Transpose();
+  Transpose(frame);
   return true;
 }
 
