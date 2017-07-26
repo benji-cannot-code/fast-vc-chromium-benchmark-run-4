a7ed9a84117dfe031a117f1fc297e71a6fd748eb
Jeffrey He <jeffreyhe@google.com>
2017-07-26 22:44:45 +0000

    Remove StandbyThreadPolicy.
    Since the TaskScheduler is now actively being used, StandbyThreadPolicy::LAZY is no 
    longer useful as a thread will at some point always be created.
    
    Bug: 738104
    Change-Id: Ib18779420d867fcbce091c6bd1161b9fbb863c0e
    Reviewed-on: https://chromium-review.googlesource.com/562017
    Commit-Queue: Jeffrey He <jeffreyhe@google.com>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Scott Byer <scottbyer@chromium.org>
    Reviewed-by: Francois Doray <fdoray@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489782}

--- base/task_scheduler/scheduler_worker_pool_params.cc
+++ base/task_scheduler/scheduler_worker_pool_params.cc
@@ -8,12 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 
 SchedulerWorkerPoolParams::SchedulerWorkerPoolParams(
-    StandbyThreadPolicy standby_thread_policy,
     int max_threads,
     TimeDelta suggested_reclaim_time,
     SchedulerBackwardCompatibility backward_compatibility)
-    : standby_thread_policy_(standby_thread_policy),
-      max_threads_(max_threads),
+    : max_threads_(max_threads),
       suggested_reclaim_time_(suggested_reclaim_time),
       backward_compatibility_(backward_compatibility) {}
 
--- base/task_scheduler/task_scheduler_impl_unittest.cc
+++ base/task_scheduler/task_scheduler_impl_unittest.cc
@@ -193,8 +193,6 @@ class TaskSchedulerImplTest
   TaskSchedulerImplTest() : scheduler_("Test") {}
 
   void StartTaskScheduler() {
-    using StandbyThreadPolicy = SchedulerWorkerPoolParams::StandbyThreadPolicy;
-
     constexpr TimeDelta kSuggestedReclaimTime = TimeDelta::FromSeconds(30);
     constexpr int kMaxNumBackgroundThreads = 1;
     constexpr int kMaxNumBackgroundBlockingThreads = 3;
@@ -202,14 +200,10 @@ class TaskSchedulerImplTest
     constexpr int kMaxNumForegroundBlockingThreads = 12;
 
     scheduler_.Start(
-        {{StandbyThreadPolicy::LAZY, kMaxNumBackgroundThreads,
-          kSuggestedReclaimTime},
-         {StandbyThreadPolicy::LAZY, kMaxNumBackgroundBlockingThreads,
-          kSuggestedReclaimTime},
-         {StandbyThreadPolicy::LAZY, kMaxNumForegroundThreads,
-          kSuggestedReclaimTime},
-         {StandbyThreadPolicy::LAZY, kMaxNumForegroundBlockingThreads,
-          kSuggestedReclaimTime}});
+        {{kMaxNumBackgroundThreads, kSuggestedReclaimTime},
+         {kMaxNumBackgroundBlockingThreads, kSuggestedReclaimTime},
+         {kMaxNumForegroundThreads, kSuggestedReclaimTime},
+         {kMaxNumForegroundBlockingThreads, kSuggestedReclaimTime}});
   }
 
   void TearDown() override { scheduler_.JoinForTesting(); }
