04f9e5560f4d4835832b8f8c55394b348f5c4b95
Ken Rockot <rockot@chromium.org>
2017-07-26 22:34:10 +0000

    Mojo: Update cached payload pointers on buffer relocation
    Message payload buffers may be reallocated and thus relocated when
    the payload is extended. Channel::Message was at construction time
    internally caching and never updating pointers into the payload on
    Windows and Mac. Not good!
    
    This CL ensures that such cached pointers are updated in the event
    of buffer reallocation.
    
    BUG=742369
    
    Change-Id: I1d5add44a72feccfeeaeabfc5cab910c58487c42
    Reviewed-on: https://chromium-review.googlesource.com/587297
    Reviewed-by: Jay Civelli <jcivelli@chromium.org>
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489778}

--- mojo/edk/system/channel.cc
+++ mojo/edk/system/channel.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/aligned_memory.h"
 #include "base/numerics/safe_math.h"
 #include "base/process/process_handle.h"
+#include "build/build_config.h"
 #include "mojo/edk/embedder/embedder_internal.h"
 #include "mojo/edk/embedder/platform_handle.h"
 #include "mojo/edk/system/configuration.h"
@@ -270,6 +271,17 @@ void Channel::Message::ExtendPayload(size_t new_payload_size) {
     base::AlignedFree(data_);
     data_ = static_cast<char*>(new_data);
     capacity_ = new_capacity;
+
+    if (max_handles_ > 0) {
+// We also need to update the cached extra header addresses in case the
+// payload buffer has been relocated.
+#if defined(OS_WIN)
+      handles_ = reinterpret_cast<HandleEntry*>(mutable_extra_header());
+#elif defined(OS_MACOSX) && !defined(OS_IOS)
+      mach_ports_header_ =
+          reinterpret_cast<MachPortsExtraHeader*>(mutable_extra_header());
+#endif
+    }
   }
   size_ = header_size + new_payload_size;
   DCHECK(base::IsValueInRangeForNumericType<uint32_t>(size_));
