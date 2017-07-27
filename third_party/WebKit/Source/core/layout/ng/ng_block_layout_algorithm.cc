9d47f8bb1c946caad4ca637dee35ef48521af2af
Morten Stenshorne <mstensho@opera.com>
2017-07-27 17:09:44 +0000

    [LayoutNG] Reduce variable span, for increased readability.
    Also changed the child loop from "while" to "for".
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: Iabca756c82281525818f1d5f3f31613679bae211
    Reviewed-on: https://chromium-review.googlesource.com/584755
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@opera.com>
    Cr-Commit-Position: refs/heads/master@{#489991}

--- third_party/WebKit/Source/core/layout/ng/ng_block_layout_algorithm.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_block_layout_algorithm.cc
@@ -228,11 +228,6 @@ RefPtr<NGLayoutResult> NGBlockLayoutAlgorithm::Layout() {
   if (abort_when_bfc_resolved_)
     DCHECK(!constraint_space_->FloatsBfcOffset());
 
-  NGBlockChildIterator child_iterator(Node().FirstChild(), BreakToken());
-  NGBlockChildIterator::Entry entry = child_iterator.NextChild();
-  NGLayoutInputNode child = entry.node;
-  NGBreakToken* child_break_token = entry.token;
-
   // If we are resuming from a break token our start border and padding is
   // within a previous fragment.
   content_size_ =
@@ -278,7 +273,12 @@ RefPtr<NGLayoutResult> NGBlockLayoutAlgorithm::Layout() {
       NGPreviousInflowPosition{input_bfc_block_offset, content_size_,
                                input_margin_strut};
 
-  while (child) {
+  NGBlockChildIterator child_iterator(Node().FirstChild(), BreakToken());
+  for (auto entry = child_iterator.NextChild();
+       NGLayoutInputNode child = entry.node;
+       entry = child_iterator.NextChild()) {
+    NGBreakToken* child_break_token = entry.token;
+
     if (child.IsOutOfFlowPositioned()) {
       DCHECK(!child_break_token);
       HandleOutOfFlowPositioned(previous_inflow_position.value(),
@@ -297,10 +297,6 @@ RefPtr<NGLayoutResult> NGBlockLayoutAlgorithm::Layout() {
         return container_builder_.Abort(NGLayoutResult::kBfcOffsetResolved);
       }
     }
-
-    entry = child_iterator.NextChild();
-    child = entry.node;
-    child_break_token = entry.token;
 
     if (IsOutOfSpace(ConstraintSpace(), content_size_))
       break;
