a4a2d2d9de2280a95c7c102c6f58bd10ce44e033
khmel <khmel@google.com>
2017-07-27 17:09:36 +0000

    arc: Make icon invalidation working with packages updated offline.
    Before we had functionality that invalidated icon in the package app
    list refresh. This worked well when update happened in runtime mode.
    However, it could be the case when a package was updated offline (or
    before ArcAppLauncher started) or in case package list updated was not
    sent due to crash, for example. In last case icons for updated package
    is left outdated. Reliable way is to check the package version. We have
    a callback when the package is updated in runtime. Moreover, we sent
    list of packages on each ARC container boot. So we have guaranteed
    package updates. Once version code is changed then we invalidate
    existing icons, and request icon update.
    
    arc: Make icon update working with packages updated offline.
    
    Test: Manually (together with fix on Android side ag/2572656) + for
          the runtime and offline update. Unit tests updated.
    Bug: 749683
    Bug: b/63839124
    TBR=xiyuan@chromium.org
    
    Change-Id: I6e2223054a5429a8af4e3e0574fa2a4e7e4351ab
    Reviewed-on: https://chromium-review.googlesource.com/580689
    Commit-Queue: Yury Khmel <khmel@google.com>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489990}

--- chrome/browser/ui/app_list/arc/arc_app_unittest.cc
+++ chrome/browser/ui/app_list/arc/arc_app_unittest.cc
@@ -1410,6 +1410,13 @@ TEST_P(ArcAppModelBuilderRecreate, IconInvalidation) {
   ui::test::ScopedSetSupportedScaleFactors scoped_supported_scale_factors(
       supported_scale_factors);
 
+  arc::mojom::ArcPackageInfo package;
+  package.package_name = fake_apps()[0].package_name;
+  package.package_version = 1;
+  package.last_backup_android_id = 1;
+  package.last_backup_time = 1;
+  package.sync = true;
+
   ASSERT_FALSE(fake_apps().empty());
   std::vector<arc::mojom::AppInfo> apps = std::vector<arc::mojom::AppInfo>(
       fake_apps().begin(), fake_apps().begin() + 1);
@@ -1422,6 +1429,7 @@ TEST_P(ArcAppModelBuilderRecreate, IconInvalidation) {
 
   app_instance()->RefreshAppList();
   app_instance()->SendRefreshAppList(apps);
+  AddPackage(package);
 
   prefs->MaybeRequestIcon(app_id, ui::SCALE_FACTOR_100P);
 
@@ -1437,12 +1445,15 @@ TEST_P(ArcAppModelBuilderRecreate, IconInvalidation) {
   ASSERT_NE(nullptr, prefs);
   app_instance()->RefreshAppList();
   app_instance()->SendRefreshAppList(apps);
+  app_instance()->SendPackageModified(package);
 
   // No icon update requests on restart. Icons were not invalidated.
   EXPECT_TRUE(app_instance()->icon_requests().empty());
 
-  // Send new apps for the package. This should invalidate app icons.
+  // Send new apps for the package. This should invalidate package icons.
+  package.package_version = 2;
   app_instance()->SendPackageAppListRefreshed(apps[0].package_name, apps);
+  app_instance()->SendPackageModified(package);
   base::RunLoop().RunUntilIdle();
 
   // Requests to reload icons are issued for all supported scales.
@@ -1467,6 +1478,7 @@ TEST_P(ArcAppModelBuilderRecreate, IconInvalidation) {
   ASSERT_NE(nullptr, prefs);
   app_instance()->RefreshAppList();
   app_instance()->SendRefreshAppList(apps);
+  app_instance()->SendPackageModified(package);
 
   // No new icon update requests on restart. Icons were invalidated and updated.
   EXPECT_TRUE(app_instance()->icon_requests().empty());
