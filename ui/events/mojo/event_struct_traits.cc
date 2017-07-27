71cc086a90ded139077ab480a3f3be4c9ab4ba96
Yuri Wiitala <miu@chromium.org>
2017-07-27 23:12:19 +0000

    Fix plumbing of time_stamp in ui::PointerEvent struct traits.
    Prior code used ui::EventTimeForNow(), which is the "current time" and
    not the time at which the event occurred. This change makes the
    
    ui: :PointerEvent struct traits adapter code do the same thing as for
    ui: :KeyEvents.
    Change-Id: I63b765d4bdaf90e5737d444c95bd7ef661ce1c94
    Reviewed-on: https://chromium-review.googlesource.com/588032
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490073}

--- ui/events/mojo/event_struct_traits.cc
+++ ui/events/mojo/event_struct_traits.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/mojo/event_struct_traits.h"
 
 #include "ui/events/event.h"
-#include "ui/events/event_utils.h"
 #include "ui/events/keycodes/dom/keycode_converter.h"
 #include "ui/events/mojo/event_constants.mojom.h"
 #include "ui/latency/mojo/latency_info_struct_traits.h"
@@ -244,14 +243,15 @@ bool StructTraits<ui::mojom::EventDataView, EventUniquePtr>::Read(
     EventUniquePtr* out) {
   DCHECK(!out->get());
 
+  base::TimeTicks time_stamp;
+  if (!event.ReadTimeStamp(&time_stamp))
+    return false;
+
   switch (event.action()) {
     case ui::mojom::EventType::KEY_PRESSED:
     case ui::mojom::EventType::KEY_RELEASED: {
       ui::mojom::KeyDataPtr key_data;
       if (!event.ReadKeyData<ui::mojom::KeyDataPtr>(&key_data))
-        return false;
-      base::TimeTicks time_stamp;
-      if (!event.ReadTimeStamp(&time_stamp))
         return false;
 
       if (key_data->is_char) {
@@ -259,7 +259,6 @@ bool StructTraits<ui::mojom::EventDataView, EventUniquePtr>::Read(
             new ui::KeyEvent(static_cast<base::char16>(key_data->character),
                              static_cast<ui::KeyboardCode>(key_data->key_code),
                              event.flags(), time_stamp));
-
       } else {
         out->reset(
             new ui::KeyEvent(event.action() == ui::mojom::EventType::KEY_PRESSED
@@ -303,7 +302,7 @@ bool StructTraits<ui::mojom::EventDataView, EventUniquePtr>::Read(
                         ui::MouseEvent::kMousePointerId)
                   : ui::PointerDetails(ui::EventPointerType::POINTER_TYPE_MOUSE,
                                        ui::MouseEvent::kMousePointerId),
-              ui::EventTimeForNow()));
+              time_stamp));
           break;
         }
         case ui::mojom::PointerKind::TOUCH: {
@@ -318,7 +317,7 @@ bool StructTraits<ui::mojom::EventDataView, EventUniquePtr>::Read(
                                  pointer_data->brush_data->pressure,
                                  pointer_data->brush_data->tilt_x,
                                  pointer_data->brush_data->tilt_y),
-              ui::EventTimeForNow()));
+              time_stamp));
           break;
         }
         case ui::mojom::PointerKind::PEN:
