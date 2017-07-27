379e4cf4339525e21eb627eeae6cc423d0edd80c
Michael Giuffrida <michaelpg@chromium.org>
2017-07-27 23:21:58 +0000

    Remove GetBlockingPool call from shell_prefs.cc
    These files are only accessed through the JsonPrefStore created here,
    so we don't need to worry about getting a unique SequencedWorkerPool.
    
    Bug: 689520
    Change-Id: I4c609c8b76ff07bf6c43c6d67efa3edf65c374f4
    Reviewed-on: https://chromium-review.googlesource.com/582587
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490088}

--- extensions/shell/browser/shell_prefs.cc
+++ extensions/shell/browser/shell_prefs.cc
@@ -5,7 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "extensions/shell/browser/shell_prefs.h"
 
-#include "base/sequenced_task_runner.h"
+#include "base/files/file_path.h"
+#include "base/memory/ref_counted.h"
 #include "build/build_config.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/json_pref_store.h"
@@ -15,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service_factory.h"
 #include "components/user_prefs/user_prefs.h"
 #include "content/public/browser/browser_context.h"
-#include "content/public/browser/browser_thread.h"
 #include "extensions/browser/api/audio/audio_api.h"
 #include "extensions/browser/extension_prefs.h"
 
@@ -38,11 +38,8 @@ void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
 // Creates a JsonPrefStore from a file at |filepath| and synchronously loads
 // the preferences.
 scoped_refptr<JsonPrefStore> CreateAndLoadPrefStore(const FilePath& filepath) {
-  scoped_refptr<base::SequencedTaskRunner> task_runner =
-      JsonPrefStore::GetTaskRunnerForFile(
-          filepath, content::BrowserThread::GetBlockingPool());
   scoped_refptr<JsonPrefStore> pref_store =
-      new JsonPrefStore(filepath, task_runner, std::unique_ptr<PrefFilter>());
+      base::MakeRefCounted<JsonPrefStore>(filepath);
   pref_store->ReadPrefs();  // Synchronous.
   return pref_store;
 }
