9f0c820d54d764e57a4d291da7110269fac763f2
Collin Baker <collinbaker@google.com>
2017-07-27 05:57:23 +0000

    [Offline pages] Manage renovations from SnapshotController
    Implements changes to the SnapshotController for running renovations
    and waiting for them to complete. This new functionality is enabled
    with a boolean flag passed in at construction. When disabled, the
    SnapshotController will function just like before.
    
    Design doc:
    https://docs.google.com/document/d/1WuwRJaxBKAwVpeAHG0xhq5uRxMTB-j4LFleT4QEqANU
    
    Bug: 736933
    Change-Id: I44f3be47f8610876b0506c8811d13c04ac9642a3
    Reviewed-on: https://chromium-review.googlesource.com/584650
    Reviewed-by: Dmitry Titov <dimich@chromium.org>
    Reviewed-by: Yafei Duan <romax@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@google.com>
    Cr-Commit-Position: refs/heads/master@{#489868}

--- chrome/browser/offline_pages/background_loader_offliner_unittest.cc
+++ chrome/browser/offline_pages/background_loader_offliner_unittest.cc
@@ -205,7 +205,9 @@ class BackgroundLoaderOfflinerTest : public testing::Test {
                                offliner_.get(),
                                0L /* DelayAfterDocumentAvailable */,
                                0L /* DelayAfterDocumentOnLoad */,
-                               false /* DocumentAvailableTriggersSnapshot */));
+                               0L /* DelayAfterRenovationsCompleted */,
+                               false /* DocumentAvailableTriggersSnapshot */,
+                               false /* RenovationsEnabled */));
     offliner_->SetSnapshotControllerForTest(std::move(snapshot_controller));
     // Call complete loading.
     offliner()->DocumentOnLoadCompletedInMainFrame();
