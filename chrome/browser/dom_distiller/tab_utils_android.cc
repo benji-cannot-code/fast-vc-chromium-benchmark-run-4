bad7c4668d9517c479327882201cb9cf4e458d87
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2017-07-27 10:26:15 +0000

    Implement ReaderModeInCCT feature
    When feature ReaderModeInCCT is turned on, Mobile-friendly View
    would be shown in a Chrome Custom Tab on high-end devices.
    Otherwise, in-tab navigate to the distilled view.
    
    This is mostly from: https://codereview.chromium.org/1837243002/
    Notable difference is that the distillation is now done in the
    WebContents in the original tab, and we no longer need to load
    the original content in an invisible tab.
    
    Bug: 726375
    Change-Id: I1d3343f62c73b1abd6d17810c2b89e14b1e95fdc
    Reviewed-on: https://chromium-review.googlesource.com/578385
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Maria Khomenko <mariakhomenko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489892}

--- chrome/browser/dom_distiller/tab_utils_android.cc
+++ chrome/browser/dom_distiller/tab_utils_android.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "components/dom_distiller/core/experiments.h"
+#include "components/navigation_interception/intercept_navigation_delegate.h"
 #include "components/url_formatter/url_formatter.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_constants.h"
@@ -27,6 +28,14 @@ void DistillCurrentPageAndView(JNIEnv* env,
   content::WebContents* web_contents =
       content::WebContents::FromJavaWebContents(j_web_contents);
   ::DistillCurrentPageAndView(web_contents);
+}
+
+void DistillCurrentPage(JNIEnv* env,
+                        const JavaParamRef<jclass>& clazz,
+                        const JavaParamRef<jobject>& j_source_web_contents) {
+  content::WebContents* source_web_contents =
+      content::WebContents::FromJavaWebContents(j_source_web_contents);
+  ::DistillCurrentPage(source_web_contents);
 }
 
 void DistillAndView(JNIEnv* env,
@@ -72,6 +81,20 @@ jboolean IsHeuristicAlwaysTrue(JNIEnv* env,
                                const JavaParamRef<jclass>& clazz) {
   return dom_distiller::GetDistillerHeuristicsType()
       == dom_distiller::DistillerHeuristicsType::ALWAYS_TRUE;
+}
+
+void SetInterceptNavigationDelegate(
+    JNIEnv* env,
+    const JavaParamRef<jclass>& clazz,
+    const JavaParamRef<jobject>& delegate,
+    const JavaParamRef<jobject>& j_web_contents) {
+  content::WebContents* web_contents =
+      content::WebContents::FromJavaWebContents(j_web_contents);
+  DCHECK(web_contents);
+  navigation_interception::InterceptNavigationDelegate::Associate(
+      web_contents,
+      base::MakeUnique<navigation_interception::InterceptNavigationDelegate>(
+          env, delegate));
 }
 
 }  // namespace android
