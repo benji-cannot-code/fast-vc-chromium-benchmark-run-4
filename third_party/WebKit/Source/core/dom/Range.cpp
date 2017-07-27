ec32f9f609c13d901b2271fd678a3ce46e9e9361
Yoshifumi Inoue <yosin@chromium.org>
2017-07-27 01:26:31 +0000

    Utilize SetSelectionData in Range.cpp
    This patch changes call sites of FrameSelection::SetSelection() in
    "Rang.cpp" to use |SetSelectionData| for a preparation of the patch[1].
    
    [1] http://crrev.com/c/569662 Get rid of FrameSelection::SetSelectionOptions
    
    Bug: 742093
    Change-Id: Ib4e96ff5b8d38bc36ccd7f228f457c003e2f0500
    Reviewed-on: https://chromium-review.googlesource.com/585341
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489829}

--- third_party/WebKit/Source/core/dom/Range.cpp
+++ third_party/WebKit/Source/core/dom/Range.cpp
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/EditingUtilities.h"
 #include "core/editing/EphemeralRange.h"
 #include "core/editing/FrameSelection.h"
+#include "core/editing/SetSelectionData.h"
 #include "core/editing/VisiblePosition.h"
 #include "core/editing/VisibleUnits.h"
 #include "core/editing/iterators/TextIterator.h"
@@ -1691,9 +1692,11 @@ void Range::UpdateSelectionIfAddedToSelection() {
                              .Collapse(StartPosition())
                              .Extend(EndPosition())
                              .Build(),
-                         FrameSelection::kCloseTyping |
-                             FrameSelection::kClearTypingStyle |
-                             FrameSelection::kDoNotSetFocus);
+                         SetSelectionData::Builder()
+                             .SetShouldCloseTyping(true)
+                             .SetShouldClearTypingStyle(true)
+                             .SetDoNotSetFocus(true)
+                             .Build());
   selection.CacheRangeOfDocument(this);
 }
 
