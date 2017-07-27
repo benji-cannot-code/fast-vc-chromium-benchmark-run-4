9e342dafa8c18fb578af57c1c5f6782d38ebd7e3
Yoshifumi Inoue <yosin@chromium.org>
2017-07-27 03:46:07 +0000

    Utilize SetSelectionData in TextControlElement.cpp
    This patch changes call sites of FrameSelection::SetSelection() in
    "TextControlElement.cpp" to use |SetSelectionData| for a preparation of the
    patch[1].
    
    [1] http://crrev.com/c/569662 Get rid of FrameSelection::SetSelectionOptions
    
    Bug: 742093
    Change-Id: I976090d4d6b0a1fe35e4935de8e68e18478da106
    Reviewed-on: https://chromium-review.googlesource.com/586071
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489848}

--- third_party/WebKit/Source/core/html/TextControlElement.cpp
+++ third_party/WebKit/Source/core/html/TextControlElement.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/EditingUtilities.h"
 #include "core/editing/Editor.h"
 #include "core/editing/FrameSelection.h"
+#include "core/editing/SetSelectionData.h"
 #include "core/editing/iterators/CharacterIterator.h"
 #include "core/editing/iterators/TextIterator.h"
 #include "core/editing/serializers/Serialization.h"
@@ -440,8 +441,11 @@ bool TextControlElement::SetSelectionRange(
                                                               : end_position)
           .SetIsDirectional(direction != kSelectionHasNoDirection)
           .Build(),
-      FrameSelection::kCloseTyping | FrameSelection::kClearTypingStyle |
-          FrameSelection::kDoNotSetFocus);
+      SetSelectionData::Builder()
+          .SetShouldCloseTyping(true)
+          .SetShouldClearTypingStyle(true)
+          .SetDoNotSetFocus(true)
+          .Build());
   return did_change;
 }
 
