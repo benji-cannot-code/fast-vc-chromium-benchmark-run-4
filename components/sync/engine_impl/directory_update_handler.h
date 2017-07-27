85c8caa8cd160c8a472625a0ed48c0ec6eb70f26
Gang Wu <gangwu@chromium.org>
2017-07-27 16:58:17 +0000

    [USS] Implement garbage collection by age and verion for USS.
    Similar to directory, for age out, we only do it once a day
    or server changed age_out_days, for saving resource.
    
    Bug: 347253, 702020
    Change-Id: I5275f407dbf0a06fe2594c99064c3f2be6525e1f
    Reviewed-on: https://chromium-review.googlesource.com/581767
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489984}

--- components/sync/engine_impl/directory_update_handler.h
+++ components/sync/engine_impl/directory_update_handler.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace sync_pb {
 class DataTypeProgressMarker;
-class GarbageCollectionDirective;
 }
 
 namespace syncer {
