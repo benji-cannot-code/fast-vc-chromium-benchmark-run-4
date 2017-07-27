975a2b9da8d974bd34a08991791b64f09d66f94b
Gabriel Charette <gab@chromium.org>
2017-07-27 23:51:33 +0000

    Migrate away from deprecated MessageLoop::QuitWhenIdle() in java_handler_thread.cc
    This is one of the very last calls in the codebase.
    
    base::RunLoop::QuitCurrentWhenIdleDeprecated() is equivalent to what
    base::MessageLoop::QuitWhenIdle() used to do.
    
    Bug: 748715
    Change-Id: I2ca9626d771e62b3ba8cbf6164d93ae13a4e05ef
    Reviewed-on: https://chromium-review.googlesource.com/590100
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490136}

--- base/android/java_handler_thread.cc
+++ base/android/java_handler_thread.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
 #include "base/message_loop/message_loop.h"
+#include "base/run_loop.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/threading/thread_restrictions.h"
 #include "jni/JavaHandlerThread_jni.h"
@@ -82,7 +83,7 @@ void JavaHandlerThread::StartMessageLoop() {
 }
 
 void JavaHandlerThread::StopMessageLoop() {
-  static_cast<MessageLoopForUI*>(message_loop_.get())->QuitWhenIdle();
+  base::RunLoop::QuitCurrentWhenIdleDeprecated();
 }
 
 } // namespace android
