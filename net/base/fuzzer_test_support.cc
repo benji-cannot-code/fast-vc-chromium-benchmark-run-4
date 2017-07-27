40512412733d14fe11333feb8d73aa9cc53dc1bc
Matt Menke <mmenke@chromium.org>
2017-07-27 23:32:28 +0000

    Initialize command line in net fuzzers.
    Bug: 749799
    Change-Id: Iaff60e9b20c38c59e94b89786cf924fc6d84e98f
    Reviewed-on: https://chromium-review.googlesource.com/590708
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490105}

--- net/base/fuzzer_test_support.cc
+++ net/base/fuzzer_test_support.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/at_exit.h"
+#include "base/command_line.h"
 #include "base/i18n/icu_util.h"
 #include "base/logging.h"
 #include "base/metrics/statistics_recorder.h"
@@ -19,6 +20,8 @@ struct InitGlobals {
   InitGlobals()
       : scoped_task_environment(
             base::test::ScopedTaskEnvironment::MainThreadType::IO) {
+    base::CommandLine::Init(0, nullptr);
+
     // Set up ICU. ICU is used internally by GURL, which is used throughout the
     // //net code. Initializing ICU is important to prevent fuzztests from
     // asserting when handling non-ASCII urls.
