24ba32a16dfaf47cbf0b9df3cb6bde0086ca9f61
Sebastien Marchand <sebmarchand@chromium.org>
2017-07-27 13:47:26 +0000

    Base for the tab stats tracker.
    This is the base for the design described in this document: https://docs.google.com/a/chromium.org/document/d/1qZkPV_IG5naCgdlwmzlb8D-iAcFnJhnMqmFX8BiJ15Q/edit?usp=sharing
    
    Bug: 720131
    Change-Id: I7fedf3e40a971f25f64b1fee420bfe27d5a0fbe7
    Reviewed-on: https://chromium-review.googlesource.com/571176
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Alexei Svitkine (OOO July28-Aug6) <asvitkine@chromium.org>
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489910}

--- chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
+++ chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if !defined(OS_ANDROID)
 #include "chrome/browser/metrics/first_web_contents_profiler.h"
+#include "chrome/browser/metrics/tab_stats_tracker.h"
 #include "chrome/browser/metrics/tab_usage_recorder.h"
 #endif  // !defined(OS_ANDROID)
 
@@ -562,6 +563,7 @@ void ChromeBrowserMainExtraPartsMetrics::PostBrowserStart() {
 #if !defined(OS_ANDROID)
   metrics::BeginFirstWebContentsProfiling();
   metrics::TabUsageRecorder::InitializeIfNeeded();
+  metrics::TabStatsTracker::Initialize();
 #endif  // !defined(OS_ANDROID)
 }
 
