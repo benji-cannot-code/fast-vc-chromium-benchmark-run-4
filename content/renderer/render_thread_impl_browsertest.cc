6656dd3b86770db8ead819d334392093feee8a1b
Gabriel Charette <gab@chromium.org>
2017-07-27 23:09:31 +0000

    Manually migrate away from deprecated MessageLoop::Quit*() methods in /content/renderer.
    This follows an automated pass for simpler use cases @
    https://chromium-review.googlesource.com/c/585732/.
    
    Minimal cleanup was made when trivial.
    
    (some calls were even removed as they merely added a redundant QuitWhenIdle()
    to a RunLoop().RunUntilIdle() call)
    
    When too complex I defaulted to the lesser bad alternative of
    RunLoop::QuitCurrent*Deprecated() methods.
    
    I will not engage in further cleanups as part of this massive
    change -- owners are welcome to follow suit with better usage
    of RunLoop if unsatisfied with this).
    
    This CL was uploaded by git cl split.
    
    R=dglazkov@chromium.org
    
    Bug: 748715
    Change-Id: I6191937397a05bdd7daaac21ec46a6a77ae773c2
    Reviewed-on: https://chromium-review.googlesource.com/586908
    Reviewed-by: Dimitri Glazkov <dglazkov@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490068}

--- content/renderer/render_thread_impl_browsertest.cc
+++ content/renderer/render_thread_impl_browsertest.cc
@@ -14,11 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/debug/leak_annotations.h"
 #include "base/location.h"
 #include "base/memory/discardable_memory.h"
-#include "base/message_loop/message_loop.h"
 #include "base/metrics/field_trial.h"
 #include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/threading/sequenced_task_runner_handle.h"
 #include "base/threading/sequenced_worker_pool.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "components/viz/common/resources/buffer_to_texture_target_map.h"
@@ -137,19 +137,15 @@ class RenderThreadImplForTest : public RenderThreadImpl {
 #pragma warning(pop)
 #endif
 
-void QuitTask(base::MessageLoop* message_loop) {
-  message_loop->QuitWhenIdle();
-}
-
 class QuitOnTestMsgFilter : public IPC::MessageFilter {
  public:
-  explicit QuitOnTestMsgFilter(base::MessageLoop* message_loop)
-      : message_loop_(message_loop) {}
+  explicit QuitOnTestMsgFilter(base::OnceClosure quit_closure)
+      : origin_task_runner_(base::SequencedTaskRunnerHandle::Get()),
+        quit_closure_(std::move(quit_closure)) {}
 
   // IPC::MessageFilter overrides:
   bool OnMessageReceived(const IPC::Message& message) override {
-    message_loop_->task_runner()->PostTask(
-        FROM_HERE, base::Bind(&QuitTask, message_loop_));
+    origin_task_runner_->PostTask(FROM_HERE, std::move(quit_closure_));
     return true;
   }
 
@@ -162,7 +158,8 @@ class QuitOnTestMsgFilter : public IPC::MessageFilter {
  private:
   ~QuitOnTestMsgFilter() override {}
 
-  base::MessageLoop* message_loop_;
+  scoped_refptr<base::SequencedTaskRunner> origin_task_runner_;
+  base::OnceClosure quit_closure_;
 };
 
 class RenderThreadImplBrowserTest : public testing::Test {
@@ -232,8 +229,9 @@ class RenderThreadImplBrowserTest : public testing::Test {
         std::move(renderer_scheduler), test_task_counter);
     cmd->InitFromArgv(old_argv);
 
+    run_loop_ = base::MakeUnique<base::RunLoop>();
     test_msg_filter_ = make_scoped_refptr(
-        new QuitOnTestMsgFilter(base::MessageLoop::current()));
+        new QuitOnTestMsgFilter(run_loop_->QuitWhenIdleClosure()));
     thread_->AddFilter(test_msg_filter_.get());
   }
 
@@ -264,6 +262,8 @@ class RenderThreadImplBrowserTest : public testing::Test {
   RenderThreadImplForTest* thread_;  // Owned by mock_process_.
 
   base::FieldTrialList field_trial_list_;
+
+  std::unique_ptr<base::RunLoop> run_loop_;
 };
 
 void CheckRenderThreadInputHandlerManager(RenderThreadImpl* thread) {
@@ -295,7 +295,7 @@ TEST_F(RenderThreadImplBrowserTest,
   sender()->Send(new ResourceHostMsg_FollowRedirect(0));
   sender()->Send(new TestMsg_QuitRunLoop());
 
-  base::RunLoop().Run();
+  run_loop_->Run();
   EXPECT_EQ(1, test_task_counter_->NumTasksPosted());
 }
 
@@ -307,7 +307,7 @@ TEST_F(RenderThreadImplBrowserTest,
 
   sender()->Send(new TestMsg_QuitRunLoop());
 
-  base::RunLoop().Run();
+  run_loop_->Run();
 
   EXPECT_EQ(0, test_task_counter_->NumTasksPosted());
 }
