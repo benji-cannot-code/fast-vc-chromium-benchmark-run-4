5956dc5b77e87e0a3c5242fa86a909f9080f9d20
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2017-07-27 16:25:32 +0000

    Import wpt@9e9969659c5bb38686c8474dafd1605fa3c18089
    Using wpt-import in Chromium 7b48b3e4878624375427d69bf077f1179d17dcd5.
    
    Build: https://build.chromium.org/p/chromium.infra.cron/builders/wpt-importer/builds/383
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    :
      external/wpt/html/semantics
    blink-network-dev@chromium.org:
      external/wpt/XMLHttpRequest
      external/wpt/XMLHttpRequest
      external/wpt/url
      external/wpt/url
      external/wpt/url
      external/wpt/url
      external/wpt/url
    dom-dev@chromium.org:
      external/wpt/dom
      external/wpt/dom
      external/wpt/dom
      external/wpt/dom
      external/wpt/html
    dpranke@chromium.org:
      external
      external
      external
      external
      external
      external
      external
      external
      external
      external
      external
      external
    hta@chromium.org:
      external/wpt/webrtc
      external/wpt/webrtc
      external/wpt/webrtc
      external/wpt/webrtc
      external/wpt/webrtc
      external/wpt/webrtc
      external/wpt/webrtc
      external/wpt/webrtc
      external/wpt/webrtc
      external/wpt/webrtc
      external/wpt/webrtc
      external/wpt/webrtc
      external/wpt/webrtc
      external/wpt/webrtc
      external/wpt/webrtc
      external/wpt/webrtc
      external/wpt/webrtc
      external/wpt/webrtc
    jsbell@chromium.org:
      external/wpt/WebIDL
      external/wpt/WebIDL
      external/wpt/user-timing
      external/wpt/user-timing
    panicker@chromium.org:
      external/wpt/page-visibility
      external/wpt/page-visibility
      external/wpt/page-visibility
    smcgruer@chromium.org:
      external/wpt/css/css-position-3
    style-dev@chromium.org:
      external/wpt/cssom-view
      external/wpt/cssom-view
      external/wpt/cssom-view
      external/wpt/cssom-view
      external/wpt/cssom-view
      external/wpt/cssom-view
      external/wpt/cssom-view
      external/wpt/cssom-view
      external/wpt/cssom-view
    tyoshino@chromium.org, yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
      external/wpt/fetch
      external/wpt/fetch
      external/wpt/fetch
      external/wpt/fetch
      external/wpt/fetch
      external/wpt/fetch
      external/wpt/fetch
      external/wpt/fetch
      external/wpt/fetch
    worker-dev@chromium.org:
      external/wpt/workers
      external/wpt/workers
      external/wpt/workers
      external/wpt/workers
      external/wpt/workers
      external/wpt/workers
    
    TBR=qyearsley@chromium.org
    
    No-Export: true
    Change-Id: I3e06d008046f7c9c533f8275f2a1e2ec8edb2aae
    Reviewed-on: https://chromium-review.googlesource.com/589229
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489962}

--- third_party/WebKit/LayoutTests/external/wpt/webrtc/RTCPeerConnection-helper.js
+++ third_party/WebKit/LayoutTests/external/wpt/webrtc/RTCPeerConnection-helper.js
@@ -252,10 +252,11 @@ function doSignalingHandshake(localPc, remotePc) {
 // It does the heavy lifting of performing signaling handshake,
 // ICE candidate exchange, and waiting for data channel at two
 // end points to open.
-function createDataChannelPair() {
-  const pc1 = new RTCPeerConnection();
-  const pc2 = new RTCPeerConnection();
-  const channel1 = pc1.createDataChannel('test');
+function createDataChannelPair(
+  pc1=new RTCPeerConnection(),
+  pc2=new RTCPeerConnection())
+{
+  const channel1 = pc1.createDataChannel('');
 
   exchangeIceCandidates(pc1, pc2);
 
@@ -279,7 +280,7 @@ function createDataChannelPair() {
     }
 
     function onDataChannel(event) {
-      channel2 = event.channel
+      channel2 = event.channel;
       channel2.addEventListener('error', reject);
       const { readyState } = channel2;
 
