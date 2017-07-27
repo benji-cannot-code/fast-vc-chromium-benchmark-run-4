bd07fcc76209962822a1767b4ebed5b08e9b7d90
Takeshi Yoshino <tyoshino@chromium.org>
2017-07-27 23:13:27 +0000

    Build a ResourceRequest with net::RedirectInfo applied in ResourceLoader::WillFollowRedirect()
    Currently, the WebURLLoaderImpl builds a WebURLRequest, applies
    net::RedirectInfo and passes it to the ResourceLoader and holds the
    built WebURLRequest also in it. This back and forth data passing is
    confusing and misleading. People often think that data modified on the
    WebURLRequest in ResourceLoader are propagated to net/ which is not
    true.
    
    The WebURLLoaderImpl::Context also holds the initial WebURLRequest
    passed to Start() in |request_|. It's wasteful as the Resource also
    holds copies of ResourceRequests for the whole redirect chain including
    the initial one.
    
    Bug: 665766
    Change-Id: Ie13776f03c5f64ad577e20f7b32c68ceb73f94d2
    Reviewed-on: https://chromium-review.googlesource.com/583107
    Commit-Queue: Takeshi Yoshino <tyoshino@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490075}

--- third_party/WebKit/Source/platform/testing/weburl_loader_mock.h
+++ third_party/WebKit/Source/platform/testing/weburl_loader_mock.h
@@ -41,8 +41,8 @@ class WebURLLoaderMock : public WebURLLoader {
                                 const WebURLError& error);
 
   // Simulates the redirect being served.
-  WebURLRequest ServeRedirect(const WebURLRequest& request,
-                              const WebURLResponse& redirect_response);
+  WebURL ServeRedirect(const WebURLRequest& request,
+                       const WebURLResponse& redirect_response);
 
   // WebURLLoader methods:
   void LoadSynchronously(const WebURLRequest& request,
--- third_party/WebKit/Source/platform/testing/weburl_loader_mock_factory_impl.h
+++ third_party/WebKit/Source/platform/testing/weburl_loader_mock_factory_impl.h
@@ -81,7 +81,7 @@ class WebURLLoaderMockFactoryImpl : public WebURLLoaderMockFactory {
 
   // Loads the specified request and populates the response, error and data
   // accordingly.
-  void LoadRequest(const WebURLRequest& request,
+  void LoadRequest(const WebURL& url,
                    WebURLResponse* response,
                    WebURLError* error,
                    WebData* data);
