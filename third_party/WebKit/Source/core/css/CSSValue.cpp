e2f8744aeeb37bbae61ea1a226162a9998519b3e
Dominik Röttsches <drott@chromium.org>
2017-07-27 23:29:40 +0000

    Reland: Add range-matching capabilities to FontFaceCache
    Reland of b45665298c860afc0b5042ceac5b4131e8d55cac, reverted in
    b45665298c860afc0b5042ceac5b4131e8d55cac. Original issue fixed in parent
    commit. This CL itself got reverted because it was dependent on the
    parent.
    
    As step 2 of issue 733154, add range matching capabilities to FontFace
    and FontFaceCache, tested in range matching tests of
    FontFaceCacheTest. This functionality conforms to the font style
    matching algorithm in section 5.2 of CSS Fonts Level 4.
    
    This CL does not yet introduce changes to the CSS parser and does not
    expose the extended matching behavior to CSS. In order to keep CLs
    small, this is an interim step, keeping the interpretation of
    CSSIdentifierValue's in FontFace.cpp until those are removed in
    subsequent CLs, see issue 733154 for more details.
    
    [1] https://drafts.csswg.org/css-fonts-4/#font-style-matching
    
    TBR'ing since original CL has been reviewed in
    https://chromium-review.googlesource.com/c/568150/
    
    Bug: 733154
    Change-Id: Ifd2e6d43a0b673c96b147d2398038148d5c337bf
    TBR: eae, meade
    Reviewed-on: https://chromium-review.googlesource.com/589567
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490100}

--- third_party/WebKit/Source/core/css/CSSValue.cpp
+++ third_party/WebKit/Source/core/css/CSSValue.cpp
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/CSSFontFaceSrcValue.h"
 #include "core/css/CSSFontFamilyValue.h"
 #include "core/css/CSSFontFeatureValue.h"
+#include "core/css/CSSFontStyleRangeValue.h"
 #include "core/css/CSSFontVariationValue.h"
 #include "core/css/CSSFunctionValue.h"
 #include "core/css/CSSGradientValue.h"
@@ -170,6 +171,8 @@ bool CSSValue::operator==(const CSSValue& other) const {
         return CompareCSSValues<CSSFontFamilyValue>(*this, other);
       case kFontFeatureClass:
         return CompareCSSValues<CSSFontFeatureValue>(*this, other);
+      case kFontStyleRangeClass:
+        return CompareCSSValues<CSSFontStyleRangeValue>(*this, other);
       case kFontVariationClass:
         return CompareCSSValues<CSSFontVariationValue>(*this, other);
       case kFunctionClass:
@@ -272,6 +275,8 @@ String CSSValue::CssText() const {
       return ToCSSFontFamilyValue(this)->CustomCSSText();
     case kFontFeatureClass:
       return ToCSSFontFeatureValue(this)->CustomCSSText();
+    case kFontStyleRangeClass:
+      return ToCSSFontStyleRangeValue(this)->CustomCSSText();
     case kFontVariationClass:
       return ToCSSFontVariationValue(this)->CustomCSSText();
     case kFunctionClass:
@@ -381,6 +386,9 @@ void CSSValue::FinalizeGarbageCollectedObject() {
       return;
     case kFontFeatureClass:
       ToCSSFontFeatureValue(this)->~CSSFontFeatureValue();
+      return;
+    case kFontStyleRangeClass:
+      ToCSSFontStyleRangeValue(this)->~CSSFontStyleRangeValue();
       return;
     case kFontVariationClass:
       ToCSSFontVariationValue(this)->~CSSFontVariationValue();
@@ -526,6 +534,9 @@ DEFINE_TRACE(CSSValue) {
       return;
     case kFontFeatureClass:
       ToCSSFontFeatureValue(this)->TraceAfterDispatch(visitor);
+      return;
+    case kFontStyleRangeClass:
+      ToCSSFontStyleRangeValue(this)->TraceAfterDispatch(visitor);
       return;
     case kFontVariationClass:
       ToCSSFontVariationValue(this)->TraceAfterDispatch(visitor);
