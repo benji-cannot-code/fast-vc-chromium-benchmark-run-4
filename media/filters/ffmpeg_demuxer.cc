17b003c0ae268242c0d2f5393cb8b6c275dcf5ff
Matt Wolenetz <wolenetz@chromium.org>
2017-07-27 01:01:38 +0000

    Clarify FFmpegDemuxer: failed creating ... stream log
    Otherwise valid audio or video resources may contain streams that Chrome
    does not support. When they are reported by FFmpeg as either an audio
    for video stream, we try to create a valid decoder configuration for
    them as part of constructing FFmpegDemuxerStreams for them.
    
    For some types of such streams (e.g., poster image in a flac or mp3 file
    is reported by FFmpeg as video), we previously logged an error message
    into chrome://media-internals, followed by an INFO message describing
    that we skipped the "invalid or unsupported {audio,video} track" (due to
    the failure creating that decoder config/demuxerstream). This could be
    confusing, since it's not necessarily an error.
    
    This change clarifies the error log messages for audio/video, and
    changes them to be DEBUG type. This also has the effect that
    MediaError.message, which might occur if *all* streams are skipped due
    to failures to create their FFmpegDemuxerStreams, won't be populated
    with the "FFmpegDemuxer: failed creating {audio,video} stream" message
    (or even the new DEBUG message). It should instead include a more
    informative message "FFmpegDemuxer: no supported streams".
    
    This change also adds references to bug 749178 to expand strict MediaLog
    verification in FFmpegDemuxerTests beyond the basics added here.
    
    Example:
    old:
    info  | FFmpegDemuxer: created audio stream, config codec: mp3 [codec details...]
    error | FFmpegDemuxer: failed creating video stream
    info  | FFmpegDemuxer: skipping invalid or unsupported video track
    
    new:
    info  | FFmpegDemuxer: created audio stream, config codec: mp3 [codec details...]
    debug | Warning, FFmpegDemuxer failed to create a valid video decoder configuration from muxed stream
    info  | FFmpegDemuxer: skipping invalid or unsupported video track
    
    BUG=748965,749178
    TEST=Updated FFmpegDemuxerTests, and manual
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I64b59512ff73013f4307b5dda639e84cb015d69a
    Reviewed-on: https://chromium-review.googlesource.com/587425
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489823}

--- media/filters/ffmpeg_demuxer.cc
+++ media/filters/ffmpeg_demuxer.cc
@@ -243,8 +243,9 @@ std::unique_ptr<FFmpegDemuxerStream> FFmpegDemuxerStream::Create(
     // IsValidConfig internally and return a null scoped_ptr if not valid.
     if (!AVStreamToAudioDecoderConfig(stream, audio_config.get()) ||
         !audio_config->IsValidConfig()) {
-      MEDIA_LOG(ERROR, media_log)
-          << "FFmpegDemuxer: failed creating audio stream";
+      MEDIA_LOG(DEBUG, media_log) << "Warning, FFmpegDemuxer failed to create "
+                                     "a valid audio decoder configuration from "
+                                     "muxed stream";
       return nullptr;
     }
 
@@ -260,8 +261,9 @@ std::unique_ptr<FFmpegDemuxerStream> FFmpegDemuxerStream::Create(
     // IsValidConfig internally and return a null scoped_ptr if not valid.
     if (!AVStreamToVideoDecoderConfig(stream, video_config.get()) ||
         !video_config->IsValidConfig()) {
-      MEDIA_LOG(ERROR, media_log)
-          << "FFmpegDemuxer: failed creating video stream";
+      MEDIA_LOG(DEBUG, media_log) << "Warning, FFmpegDemuxer failed to create "
+                                     "a valid video decoder configuration from "
+                                     "muxed stream";
       return nullptr;
     }
 
