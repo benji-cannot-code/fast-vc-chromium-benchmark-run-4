b21781ded74f11db6c3b3c1eea4a9a52880e313f
Gabriel Charette <gab@chromium.org>
2017-07-27 23:46:15 +0000

    Migrate FileUtilitiesMessageFilter off of FILE thread.
    Bug: 689520
    Change-Id: I369165e8cae99a17ca29f0d17430ae7f8d024c8c
    Reviewed-on: https://chromium-review.googlesource.com/585572
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490127}

--- content/browser/renderer_host/file_utilities_message_filter.cc
+++ content/browser/renderer_host/file_utilities_message_filter.cc
@@ -6,8 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/file_utilities_message_filter.h"
 
 #include "base/files/file_util.h"
+#include "base/task_scheduler/lazy_task_runner.h"
 #include "content/browser/child_process_security_policy_impl.h"
 #include "content/common/file_utilities_messages.h"
+
+namespace {
+
+base::LazySequencedTaskRunner g_file_utility_task_runner =
+    LAZY_SEQUENCED_TASK_RUNNER_INITIALIZER(
+        base::TaskTraits(base::MayBlock(), base::TaskPriority::USER_VISIBLE));
+
+}  // namespace
 
 namespace content {
 
@@ -19,11 +28,11 @@ FileUtilitiesMessageFilter::FileUtilitiesMessageFilter(int process_id)
 FileUtilitiesMessageFilter::~FileUtilitiesMessageFilter() {
 }
 
-void FileUtilitiesMessageFilter::OverrideThreadForMessage(
-    const IPC::Message& message,
-    BrowserThread::ID* thread) {
+base::TaskRunner* FileUtilitiesMessageFilter::OverrideTaskRunnerForMessage(
+    const IPC::Message& message) {
   if (IPC_MESSAGE_CLASS(message) == FileUtilitiesMsgStart)
-    *thread = BrowserThread::FILE;
+    return g_file_utility_task_runner.Get().get();
+  return nullptr;
 }
 
 bool FileUtilitiesMessageFilter::OnMessageReceived(
