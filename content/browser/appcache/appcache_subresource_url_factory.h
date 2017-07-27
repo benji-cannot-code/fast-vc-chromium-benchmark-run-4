fc99966d2419c873ff7a71330aa7e4ebc7a98bce
mmenke <mmenke@chromium.org>
2017-07-26 21:46:42 -0700

    Revert of The Appcache subresource URL factory needs to inform the URLLoaderClient if there is a failure. (patchset #7 id:120001 of https://codereview.chromium.org/2991443002/ )
    Reason for revert:
    AppCacheNetworkServiceBrowserTest.VerifySubresourceFactoryClearedOnNewNavigation is very flaky, with a lot of red try runs on the CQ and the builders.
    
    Windows CQ has a 2 hour backlog (Or did earlier), and while this isn't the only reason, it looks like a major contributor.
    
    Original issue's description:
    > The Appcache subresource URL factory needs to inform the URLLoaderClient if there is a failure.
    >
    > Failure cases include, an invalid AppCacheHost instance, failure to create a request handler, etc.
    > We already notify the client about the latter case above. Need to do this for an invalid host as well.
    > This caused the w3schools AppCache test page to spin indefinitely if we pass in an invalid URL.
    >
    > Additionally on the renderer side we need to reset the cached URLLoaderFactory pointer if a null factory
    > is passed in the CommitNavigation IPC. This ensures that the request hits the default network loader.
    >
    > BUG=715632
    > TEST=Covered by browser test. AppCacheNetworkServiceBrowserTest.VerifySubresourceFactoryClearedOnNewNavigation
    >
    > Review-Url: https://codereview.chromium.org/2991443002
    > Cr-Commit-Position: refs/heads/master@{#489710}
    > Committed: https://chromium.googlesource.com/chromium/src/+/4a9dd98efab25e78f28b800520c1868aaad2cb1f
    
    TBR=jam@chromium.org,michaeln@chromium.org,ananta@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=715632
    
    Review-Url: https://codereview.chromium.org/2988923002
    Cr-Commit-Position: refs/heads/master@{#489860}

--- content/browser/appcache/appcache_subresource_url_factory.h
+++ content/browser/appcache/appcache_subresource_url_factory.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
-#include "content/common/content_export.h"
 #include "content/public/common/url_loader_factory.mojom.h"
 #include "mojo/public/cpp/bindings/binding.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
@@ -22,8 +21,7 @@ class AppCacheServiceImpl;
 class URLLoaderFactoryGetter;
 
 // Implements the URLLoaderFactory mojom for AppCache subresource requests.
-class CONTENT_EXPORT AppCacheSubresourceURLFactory
-    : public mojom::URLLoaderFactory {
+class AppCacheSubresourceURLFactory : public mojom::URLLoaderFactory {
  public:
   ~AppCacheSubresourceURLFactory() override;
 
@@ -55,17 +53,11 @@ class CONTENT_EXPORT AppCacheSubresourceURLFactory
                 SyncLoadCallback callback) override;
 
  private:
-  friend class AppCacheNetworkServiceBrowserTest;
-
   AppCacheSubresourceURLFactory(mojom::URLLoaderFactoryRequest request,
                                 URLLoaderFactoryGetter* factory_getter,
                                 base::WeakPtr<AppCacheHost> host);
 
   void OnConnectionError();
-
-  // Notifies the |client| if there is a failure. The |error_code| contains the
-  // actual error.
-  void NotifyError(mojom::URLLoaderClientPtr client, int error_code);
 
   // Mojo binding.
   mojo::Binding<mojom::URLLoaderFactory> binding_;
