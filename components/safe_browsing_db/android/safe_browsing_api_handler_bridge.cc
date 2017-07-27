6145f9a829ce45e457195c13aba85a3041c0c15a
Nate Fischer <ntfschr@chromium.org>
2017-07-27 23:45:07 +0000

    Android: remove JNI registration from safe_browsing_db
    This cleans up unnecessary JNI registration from
    components/safe_browsing_db/. This code is no longer needed due to a
    change in how JNI registration works.
    
    Bug: 749244
    Test: manual - run chrome_apk & chrome_modern_apk & verify no crash when browsing and creating interstitials
    Change-Id: If2dcb57aec2357ca6a0ebb1f8b426021cfcd8a11
    Reviewed-on: https://chromium-review.googlesource.com/588367
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490125}

--- components/safe_browsing_db/android/safe_browsing_api_handler_bridge.cc
+++ components/safe_browsing_db/android/safe_browsing_api_handler_bridge.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
-#include "base/android/jni_android.h"
 #include "base/android/jni_array.h"
 #include "base/android/jni_string.h"
 #include "base/containers/flat_set.h"
@@ -73,10 +72,6 @@ ScopedJavaLocalRef<jintArray> SBThreatTypeSetToJavaArray(
 }
 
 }  // namespace
-
-bool RegisterSafeBrowsingApiBridge(JNIEnv* env) {
-  return RegisterNativesImpl(env);
-}
 
 // Java->Native call, invoked when a check is done.
 //   |callback_id| is an int form of pointer to a URLCheckCallbackMeta
