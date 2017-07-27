30c4b6414e1803be2d134bf5cd603b684bf3748f
Mohamad Ahmadi <mahmadi@chromium.org>
2017-07-27 16:46:09 +0000

    [Payment Request] PaymentRequestEGTestBase and EG tests for PR.
    - Creates a base class for various Payment Request related EarlGrey tests.
    - Adds a utility method to check for a text in WebView.
    - Converts an existing class to use the base test class.
    - Adds a couple of test classes.
    
    Bug: 602666
    Change-Id: Id33178ff63766d9f439795cf932d38c3ee3c3006
    Reviewed-on: https://chromium-review.googlesource.com/580570
    Reviewed-by: Mike Baxley <baxley@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: mahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489976}

--- ios/chrome/browser/ui/autofill/card_unmask_prompt_view_bridge.h
+++ ios/chrome/browser/ui/autofill/card_unmask_prompt_view_bridge.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/ui/card_unmask_prompt_view.h"
 #import "ios/chrome/browser/ui/collection_view/collection_view_controller.h"
 
+extern NSString* const kCardUnmaskPromptCollectionViewAccessibilityID;
+
 @class CardUnmaskPromptViewController;
 
 namespace autofill {
--- ios/chrome/browser/ui/autofill/card_unmask_prompt_view_bridge.mm
+++ ios/chrome/browser/ui/autofill/card_unmask_prompt_view_bridge.mm
@@ -32,6 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
+NSString* const kCardUnmaskPromptCollectionViewAccessibilityID =
+    @"kCardUnmaskPromptCollectionViewAccessibilityID";
+
 namespace {
 
 const CGFloat kTitleVerticalSpacing = 2.0f;
@@ -158,6 +161,9 @@ void CardUnmaskPromptViewBridge::DeleteSelf() {
 - (void)viewDidLoad {
   [super viewDidLoad];
 
+  self.collectionView.accessibilityIdentifier =
+      kCardUnmaskPromptCollectionViewAccessibilityID;
+
   self.styler.cellStyle = MDCCollectionViewCellStyleCard;
 
   UILabel* titleLabel = [[UILabel alloc] initWithFrame:CGRectZero];
@@ -193,6 +199,7 @@ void CardUnmaskPromptViewBridge::DeleteSelf() {
                                        style:UIBarButtonItemStylePlain
                                       target:self
                                       action:@selector(onVerify:)];
+  [_verifyButton setAccessibilityLabel:l10n_util::GetNSString(IDS_ACCNAME_OK)];
   [_verifyButton setTitleTextAttributes:@{
     NSForegroundColorAttributeName : [[MDCPalette cr_bluePalette] tint600]
   }
