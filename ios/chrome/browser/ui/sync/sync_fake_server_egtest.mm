e72506147ef569fbe07adacd53b5fca563ece7e2
Menglu Huang <huangml@chromium.org>
2017-07-27 23:49:40 +0000

    Disable SyncFakeServerTests on iOS11 due to a sign-in bug.
    Bug: 747445
    Change-Id: I92ab9830cde0d0546bedd6839f166ef4f1397a0d
    Reviewed-on: https://chromium-review.googlesource.com/587340
    Reviewed-by: Yuke Liao <liaoyuke@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Menglu Huang <huangml@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490133}

--- ios/chrome/browser/ui/sync/sync_fake_server_egtest.mm
+++ ios/chrome/browser/ui/sync/sync_fake_server_egtest.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <EarlGrey/EarlGrey.h>
 #import <XCTest/XCTest.h>
 
+#include "base/ios/ios_util.h"
 #include "base/strings/sys_string_conversions.h"
 #include "components/bookmarks/browser/bookmark_model.h"
 #include "components/bookmarks/browser/titled_url_match.h"
@@ -160,6 +161,11 @@ void AssertNumberOfEntitiesWithName(int entity_count,
 // Tests that a bookmark added on the client (before Sync is enabled) is
 // uploaded to the Sync server once Sync is turned on.
 - (void)testSyncUploadBookmarkOnFirstSync {
+  // TODO(crbug.com/747445): Re-enable this test on iOS 11.
+  if (base::ios::IsRunningOnIOS11OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Disabled on iOS 11.");
+  }
+
   [self addBookmark:GURL("https://www.foo.com") withTitle:@"foo"];
 
   // Sign in to sync, after a bookmark has been added.
@@ -175,6 +181,11 @@ void AssertNumberOfEntitiesWithName(int entity_count,
 
 // Tests that a bookmark added on the client is uploaded to the Sync server.
 - (void)testSyncUploadBookmark {
+  // TODO(crbug.com/747445): Re-enable this test on iOS 11.
+  if (base::ios::IsRunningOnIOS11OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Disabled on iOS 11.");
+  }
+
   ChromeIdentity* identity = GetFakeIdentity1();
   ios::FakeChromeIdentityService::GetInstanceFromChromeProvider()->AddIdentity(
       identity);
@@ -189,6 +200,11 @@ void AssertNumberOfEntitiesWithName(int entity_count,
 // Tests that a bookmark injected in the FakeServer is synced down to the
 // client.
 - (void)testSyncDownloadBookmark {
+  // TODO(crbug.com/747445): Re-enable this test on iOS 11.
+  if (base::ios::IsRunningOnIOS11OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Disabled on iOS 11.");
+  }
+
   [[self class] assertBookmarksWithTitle:@"hoo" expectedCount:0];
   chrome_test_util::InjectBookmarkOnFakeSyncServer("http://www.hoo.com", "hoo");
 
@@ -204,6 +220,11 @@ void AssertNumberOfEntitiesWithName(int entity_count,
 
 // Tests that the local cache guid does not change when sync is restarted.
 - (void)testSyncCheckSameCacheGuid_SyncRestarted {
+  // TODO(crbug.com/747445): Re-enable this test on iOS 11.
+  if (base::ios::IsRunningOnIOS11OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Disabled on iOS 11.");
+  }
+
   // Sign in the fake identity.
   ChromeIdentity* identity = GetFakeIdentity1();
   ios::FakeChromeIdentityService::GetInstanceFromChromeProvider()->AddIdentity(
@@ -226,6 +247,11 @@ void AssertNumberOfEntitiesWithName(int entity_count,
 // Tests that the local cache guid changes when the user signs out and then
 // signs back in with the same account.
 - (void)testSyncCheckDifferentCacheGuid_SignOutAndSignIn {
+  // TODO(crbug.com/747445): Re-enable this test on iOS 11.
+  if (base::ios::IsRunningOnIOS11OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Disabled on iOS 11.");
+  }
+
   // Sign in a fake identity, and store the initial sync guid.
   ChromeIdentity* identity = GetFakeIdentity1();
   ios::FakeChromeIdentityService::GetInstanceFromChromeProvider()->AddIdentity(
@@ -256,6 +282,11 @@ void AssertNumberOfEntitiesWithName(int entity_count,
 // a user previously signed out and back in.
 // Test for http://crbug.com/413611 .
 - (void)testSyncCheckSameCacheGuid_SyncRestartedAfterSignOutAndSignIn {
+  // TODO(crbug.com/747445): Re-enable this test on iOS 11.
+  if (base::ios::IsRunningOnIOS11OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Disabled on iOS 11.");
+  }
+
   // Sign in a fake idenitty.
   ChromeIdentity* identity = GetFakeIdentity1();
   ios::FakeChromeIdentityService::GetInstanceFromChromeProvider()->AddIdentity(
@@ -291,6 +322,11 @@ void AssertNumberOfEntitiesWithName(int entity_count,
 
 // Tests that autofill profile injected in FakeServer gets synced to client.
 - (void)testSyncDownloadAutofillProfile {
+  // TODO(crbug.com/747445): Re-enable this test on iOS 11.
+  if (base::ios::IsRunningOnIOS11OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Disabled on iOS 11.");
+  }
+
   const std::string kGuid = "2340E83B-5BEE-4560-8F95-5914EF7F539E";
   const std::string kFullName = "Peter Pan";
   GREYAssertFalse(chrome_test_util::IsAutofillProfilePresent(kGuid, kFullName),
@@ -316,6 +352,11 @@ void AssertNumberOfEntitiesWithName(int entity_count,
 // Test that update to autofill profile injected in FakeServer gets synced to
 // client.
 - (void)testSyncUpdateAutofillProfile {
+  // TODO(crbug.com/747445): Re-enable this test on iOS 11.
+  if (base::ios::IsRunningOnIOS11OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Disabled on iOS 11.");
+  }
+
   const std::string kGuid = "2340E83B-5BEE-4560-8F95-5914EF7F539E";
   const std::string kFullName = "Peter Pan";
   const std::string kUpdatedFullName = "Roger Rabbit";
@@ -360,6 +401,11 @@ void AssertNumberOfEntitiesWithName(int entity_count,
 // Test that autofill profile deleted from FakeServer gets deleted from client
 // as well.
 - (void)testSyncDeleteAutofillProfile {
+  // TODO(crbug.com/747445): Re-enable this test on iOS 11.
+  if (base::ios::IsRunningOnIOS11OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Disabled on iOS 11.");
+  }
+
   const std::string kGuid = "2340E83B-5BEE-4560-8F95-5914EF7F539E";
   const std::string kFullName = "Peter Pan";
   GREYAssertFalse(chrome_test_util::IsAutofillProfilePresent(kGuid, kFullName),
@@ -394,6 +440,11 @@ void AssertNumberOfEntitiesWithName(int entity_count,
 // Tests that tabs opened on this client are committed to the Sync server and
 // that the created sessions entities are correct.
 - (void)testSyncUploadOpenTabs {
+  // TODO(crbug.com/747445): Re-enable this test on iOS 11.
+  if (base::ios::IsRunningOnIOS11OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Disabled on iOS 11.");
+  }
+
   // Create map of canned responses and set up the test HTML server.
   const GURL URL1 = web::test::HttpServer::MakeUrl("http://page1");
   const GURL URL2 = web::test::HttpServer::MakeUrl("http://page2");
@@ -428,6 +479,11 @@ void AssertNumberOfEntitiesWithName(int entity_count,
 // Tests that a typed URL (after Sync is enabled) is uploaded to the Sync
 // server.
 - (void)testSyncTypedURLUpload {
+  // TODO(crbug.com/747445): Re-enable this test on iOS 11.
+  if (base::ios::IsRunningOnIOS11OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Disabled on iOS 11.");
+  }
+
   const GURL mockURL("http://not-a-real-site/");
 
   chrome_test_util::ClearBrowsingHistory();
@@ -465,6 +521,11 @@ void AssertNumberOfEntitiesWithName(int entity_count,
 
 // Tests that typed url is downloaded from sync server.
 - (void)testSyncTypedUrlDownload {
+  // TODO(crbug.com/747445): Re-enable this test on iOS 11.
+  if (base::ios::IsRunningOnIOS11OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Disabled on iOS 11.");
+  }
+
   const GURL mockURL("http://not-a-real-site/");
 
   chrome_test_util::ClearBrowsingHistory();
@@ -500,6 +561,11 @@ void AssertNumberOfEntitiesWithName(int entity_count,
 // Tests that when typed url is deleted on the client, sync the change gets
 // propagated to server.
 - (void)testSyncTypedURLDeleteFromClient {
+  // TODO(crbug.com/747445): Re-enable this test on iOS 11.
+  if (base::ios::IsRunningOnIOS11OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Disabled on iOS 11.");
+  }
+
   const GURL mockURL("http://not-a-real-site/");
 
   chrome_test_util::ClearBrowsingHistory();
@@ -546,6 +612,11 @@ void AssertNumberOfEntitiesWithName(int entity_count,
 // Test that typed url is deleted from client after server sends tombstone for
 // that typed url.
 - (void)testSyncTypedURLDeleteFromServer {
+  // TODO(crbug.com/747445): Re-enable this test on iOS 11.
+  if (base::ios::IsRunningOnIOS11OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Disabled on iOS 11.");
+  }
+
   const GURL mockURL("http://not-a-real-site/");
 
   chrome_test_util::ClearBrowsingHistory();
