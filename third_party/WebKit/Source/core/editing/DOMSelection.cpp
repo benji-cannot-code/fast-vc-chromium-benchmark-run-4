2af522eb90f5cf8b0382f24bbf24e1c4f8e40a4e
Yoshifumi Inoue <yosin@chromium.org>
2017-07-28 10:19:07 +0000

    Get rid of FrameSelection::SetSelectionOptions
    This patch gets rid of |FrameSelection::SetSelectionOptions| by utilizing
    |SetSelectionData| in |SetSelection()| and |DidSetSelection()| to simplify
    code base for improving code health.
    
    Bug: 742093
    Change-Id: I4f16011153340960e6b5e713c3b130c8d2d61fe2
    Reviewed-on: https://chromium-review.googlesource.com/569662
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490349}

--- third_party/WebKit/Source/core/editing/DOMSelection.cpp
+++ third_party/WebKit/Source/core/editing/DOMSelection.cpp
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/EditingUtilities.h"
 #include "core/editing/FrameSelection.h"
 #include "core/editing/SelectionModifier.h"
+#include "core/editing/SetSelectionData.h"
 #include "core/editing/iterators/TextIterator.h"
 #include "core/frame/Deprecation.h"
 #include "core/frame/LocalFrame.h"
@@ -82,12 +83,19 @@ void DOMSelection::UpdateFrameSelection(const SelectionInDOMTree& selection,
   DCHECK(GetFrame());
   FrameSelection& frame_selection = GetFrame()->Selection();
   // TODO(tkent): Specify FrameSelection::DoNotSetFocus. crbug.com/690272
-  bool did_set = frame_selection.SetSelectionDeprecated(selection);
+  const bool did_set = frame_selection.SetSelectionDeprecated(
+      selection, SetSelectionData::Builder()
+                     .SetShouldCloseTyping(true)
+                     .SetShouldClearTypingStyle(true)
+                     .Build());
   CacheRangeIfSelectionOfDocument(new_cached_range);
   if (!did_set)
     return;
   Element* focused_element = GetFrame()->GetDocument()->FocusedElement();
-  frame_selection.DidSetSelectionDeprecated();
+  frame_selection.DidSetSelectionDeprecated(SetSelectionData::Builder()
+                                                .SetShouldCloseTyping(true)
+                                                .SetShouldClearTypingStyle(true)
+                                                .Build());
   if (GetFrame() && GetFrame()->GetDocument() &&
       focused_element != GetFrame()->GetDocument()->FocusedElement())
     UseCounter::Count(GetFrame(), WebFeature::kSelectionFuncionsChangeFocus);
