083c8cc8404b92652c897d7fc7ea0b6295f7b670
Timothy Loh <timloh@chromium.org>
2017-07-28 05:17:36 +0000

    Adjust padding and text size of modal permission dialogs.
    This patch adjusts the modal permission dialog padding and text size as
    per the material spec.
    https://material.io/guidelines/components/dialogs.html#dialogs-specs
    
    Here's what it will look like: https://i.imgur.com/mmDS8Un.png
    
    Change-Id: Ief2c7c25893c0b96f6c0fc81a51a1e3989c97578
    Reviewed-on: https://chromium-review.googlesource.com/585948
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490280}

--- chrome/android/java/src/org/chromium/chrome/browser/permissions/PermissionDialogController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/permissions/PermissionDialogController.java
@@ -165,8 +165,7 @@ public class PermissionDialogController implements AndroidPermissionRequester.Re
             toggleTextView.announceForAccessibility(toggleMessage);
 
         } else {
-            mSwitchView.setVisibility(View.GONE);
-            toggleTextView.setVisibility(View.GONE);
+            view.findViewById(R.id.permission_dialog_persist_layout).setVisibility(View.GONE);
         }
 
         mDialog.setView(view);
