b66e2eabd714345165c04bcae19fb79d7b17c89c
Dale Curtis <dalecurtis@chromium.org>
2017-07-28 01:39:56 +0000

    Refactor watch time reporting into a mojo service.
    It's not sustainable to continue to stuff metrics into the MediaInternals
    reporter. Since these values aren't even displayed in the internals page
    they should go into their own mojo service. Which has the benefit of
    being a much simpler implementation to boot (~250 lines less).
    
    We now have a mojo WatchTimeRecorder interface that the WatchTimeReporter
    can send watch time updates too. It can also finalize individual keys with
    this new interface. As with the old interface if the process dies the mojo
    channel will close and we'll finalize all held values at destruction.
    
    BUG=716643, 735238
    TEST=new/existing unittests all pass
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I0565422fbf70eba676dc22e08ea22cec88f09778
    Reviewed-on: https://chromium-review.googlesource.com/546903
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490187}

--- content/renderer/media/media_factory.h
+++ content/renderer/media/media_factory.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/media_features.h"
 #include "media/mojo/features.h"
 #include "media/mojo/interfaces/remoting.mojom.h"
+#include "media/mojo/interfaces/watch_time_recorder.mojom.h"
 #include "third_party/WebKit/public/platform/WebMediaPlayerSource.h"
 #include "third_party/WebKit/public/platform/WebSecurityOrigin.h"
 #include "third_party/WebKit/public/platform/WebSetSinkIdCallbacks.h"
@@ -181,6 +182,8 @@ class MediaFactory {
   // EncryptedMediaClient attached to this frame; lazily initialized.
   std::unique_ptr<media::WebEncryptedMediaClientImpl>
       web_encrypted_media_client_;
+
+  media::mojom::WatchTimeRecorderProviderPtr watch_time_recorder_provider_;
 
 #if BUILDFLAG(ENABLE_MEDIA_REMOTING)
   // Lazy-bound pointer to the RemoterFactory service in the browser
--- media/base/ipc/media_param_traits_macros.h
+++ media/base/ipc/media_param_traits_macros.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/video_codecs.h"
 #include "media/base/video_color_space.h"
 #include "media/base/video_types.h"
+#include "media/base/watch_time_keys.h"
 #include "ui/gfx/ipc/color/gfx_param_traits_macros.h"
 
 // Enum traits.
@@ -89,6 +90,9 @@ IPC_ENUM_TRAITS_MAX_VALUE(media::OutputDeviceStatus,
 IPC_ENUM_TRAITS_MAX_VALUE(media::SampleFormat, media::kSampleFormatMax)
 
 IPC_ENUM_TRAITS_MAX_VALUE(media::VideoCodec, media::kVideoCodecMax)
+
+IPC_ENUM_TRAITS_MAX_VALUE(media::WatchTimeKey,
+                          media::WatchTimeKey::kWatchTimeKeyMax);
 
 IPC_ENUM_TRAITS_MIN_MAX_VALUE(media::VideoCodecProfile,
                               media::VIDEO_CODEC_PROFILE_MIN,
