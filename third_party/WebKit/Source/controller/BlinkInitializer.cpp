0cd1d20528d26fd4ef9bbbaf9de0133a8cbe9a09
peria <peria@chromium.org>
2017-07-28 01:54:56 -0700

    Create and use V8 context snapshots.
    This CL does two things.
    
    1. In compile time, creates a snapshot file, which consists of V8 contexts.
    2. Creates v8::Context from the snapshot in LocalWindowProxy::CreateContext().
    
    We expect this speeds up context creation for 3 times faster on Android.
    Detailed information is described in the design doc [1].
    
    [1] Design doc: https://docs.google.com/document/d/1jpQQX0piaxcHJPWakp_Kr_03g5Gnma5h5-Kdlqu7jVQ/edit#heading=h.k6iklq6rvd30
    
    Test expectations are changed due to http://crbug.com/705364
    
    BUG=588893, 617892, 705364
    
    Review-Url: https://codereview.chromium.org/2841443005
    Cr-Commit-Position: refs/heads/master@{#490329}

--- third_party/WebKit/Source/controller/BlinkInitializer.cpp
+++ third_party/WebKit/Source/controller/BlinkInitializer.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "bindings/core/v8/V8Initializer.h"
+#include "bindings/modules/v8/V8ContextSnapshotExternalReferences.h"
 #include "core/animation/AnimationClock.h"
 #include "modules/ModulesInitializer.h"
 #include "platform/bindings/Microtask.h"
@@ -70,8 +71,8 @@ static ModulesInitializer& GetModulesInitializer() {
 void Initialize(Platform* platform) {
   Platform::Initialize(platform);
 
-  V8Initializer::InitializeMainThread();
-
+  V8Initializer::InitializeMainThread(
+      V8ContextSnapshotExternalReferences::GetTable());
   GetModulesInitializer().Initialize();
 
   WebFactoryImpl::Initialize();
