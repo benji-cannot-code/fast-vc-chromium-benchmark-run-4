c294622adb6efddb249e331a63633bfa05611ad3
meade_UTC10 <meade@chromium.org>
2017-07-28 01:11:06 +0000

    Revert "[wasm] Response APIs: use promise-based module building API"
    This reverts commit 2f3d31477fc9b4e0f41242e7a63f0be4ee55d310.
    
    Reason for revert: Suspected cause of failures on WebKit Linux Trusty Leak bot :
    Unexpected Failures:
    * http/tests/wasm_streaming/wasm_response_apis.html
    * virtual/enable_wasm_streaming/http/tests/wasm_streaming/wasm_response_apis.html
    * virtual/mojo-loading/http/tests/wasm_streaming/wasm_response_apis.html
    
    First failure:
    https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux%20Trusty%20Leak/builds/7364
    
    Original change's description:
    > [wasm] Response APIs: use promise-based module building API
    > 
    > The first implementation of the response-based APIs used
    > sync compile as an implementation detail. As we're getting close
    > to having a true streaming implementation, we're switching to
    > a promise-based API, closer in shape and implementation to the
    > final design.
    > 
    > What differs from true streaming is that the implementation, just
    > like before this change, still pre buffers data before starting
    > compilation. The difference in this CL is that compilation is
    > asynchronous, and is responsible for resolving/rejecting the promise.
    > 
    > The case of network error is where we expect differences in the final
    > implementation. Currently, no compilation is started, and the
    > blink side rejects the promise. When we move to true streaming, 
    > we'll need to explicitly signal to the v8 side when the download is 
    > dropped, and probably let it (v8 side) reject the promise.
    > 
    > Also added a regression test for v8:6619, which is implicitly fixed by
    > the new design.
    >  
    > 
    > Bug: chromium:747396
    > Bug: v8:6619
    > Change-Id: Ic47c948a7cd001e07926ee4d0ae70669bab680bb
    > Reviewed-on: https://chromium-review.googlesource.com/580788
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Commit-Queue: Mircea Trofin <mtrofin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#490111}
    
    TBR=bradnelson@chromium.org,haraken@chromium.org,mtrofin@chromium.org,ahaas@chromium.org
    
    Change-Id: I6a7fdb9b3d49d2fb26bc070f99ba6d4247f0f04f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:747396, v8:6619
    Reviewed-on: https://chromium-review.googlesource.com/590389
    Commit-Queue: meade_UTC10 <meade@chromium.org>
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490175}

--- third_party/WebKit/LayoutTests/http/tests/wasm/resources/load-wasm.php
+++ third_party/WebKit/LayoutTests/http/tests/wasm/resources/load-wasm.php
@@ -2,9 +2,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
 
     $fileName = "incrementer.wasm";
-    if (isset($_GET['name'])) {
-       $fileName = $_GET['name'];
-    }
     $fileSize = filesize($fileName);
 
     header("Content-Type: " . "application/wasm");
--- third_party/WebKit/LayoutTests/http/tests/wasm_streaming/wasm_response_apis.js
+++ third_party/WebKit/LayoutTests/http/tests/wasm_streaming/wasm_response_apis.js
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 const incrementer_url = '../wasm/resources/load-wasm.php';
-const invalid_wasm_url = '../wasm/resources/load-wasm.php?name=invalid-wasm.wasm'
 
 function AssertType(obj, type) {
   assert_equals(obj.constructor, type);
@@ -12,10 +11,6 @@ function AssertType(obj, type) {
 
 function AssertTypeError(obj) {
   return AssertType(obj, TypeError);
-}
-
-function AssertCompileError(obj) {
-  return AssertType(obj, WebAssembly.CompileError);
 }
 
 function TestStreamedCompile() {
@@ -178,16 +173,4 @@ function TestInstantiateComplexModule() {
   return Promise.resolve(buildImportingModuleBytes())
     .then(b => WebAssembly.instantiateStreaming(b, ffi))
     .then(pair => AssertType(pair.instance, WebAssembly.Instance));
-}
-
-function CompileFromInvalidDownload() {
-  return WebAssembly.compileStreaming(fetch(invalid_wasm_url))
-    .then(assert_unreached,
-          AssertCompileError);
-}
-
-function InstantiateFromInvalidDownload() {
-  return WebAssembly.instantiateStreaming(fetch(invalid_wasm_url))
-    .then(assert_unreached,
-          AssertCompileError);
 }
