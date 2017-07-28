742711975dcd6ea253dd78ce3a14fb09a5cac53b
Reilly Grant <reillyg@chromium.org>
2017-07-28 17:33:32 +0000

    Restrict cases where we return USB disconnect error
    This patch restricts the cases in which UsbTransferStatus::DISCONNECTED
    is returned to only when we are absolutely sure that the device has been
    removed. Linux kernel error codes that may mean the device is
    disconnected are downgraded to a transfer error and are now always
    logged. This should help diagnose transfer/connection problems more
    easily.
    
    Bug: None
    Change-Id: I14278a8484ce18cc5729f82b17068d1e3ec8df0f
    Reviewed-on: https://chromium-review.googlesource.com/590764
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Jun Cai <juncai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490444}

--- third_party/WebKit/Source/modules/webusb/USBDevice.cpp
+++ third_party/WebKit/Source/modules/webusb/USBDevice.cpp
@@ -38,7 +38,7 @@ namespace {
 
 const char kDeviceStateChangeInProgress[] =
     "An operation that changes the device state is in progress.";
-const char kDeviceUnavailable[] = "Device unavailable.";
+const char kDeviceDisconnected[] = "The device was disconnected.";
 const char kInterfaceNotFound[] =
     "The interface number provided is not supported by the device in its "
     "current configuration.";
@@ -59,7 +59,7 @@ DOMException* ConvertFatalTransferStatus(const UsbTransferStatus& status) {
     case UsbTransferStatus::CANCELLED:
       return DOMException::Create(kAbortError, "The transfer was cancelled.");
     case UsbTransferStatus::DISCONNECT:
-      return DOMException::Create(kNotFoundError, kDeviceUnavailable);
+      return DOMException::Create(kNotFoundError, kDeviceDisconnected);
     case UsbTransferStatus::COMPLETED:
     case UsbTransferStatus::STALLED:
     case UsbTransferStatus::BABBLE:
@@ -514,7 +514,7 @@ int USBDevice::FindAlternateIndex(size_t interface_index,
 bool USBDevice::EnsureNoDeviceOrInterfaceChangeInProgress(
     ScriptPromiseResolver* resolver) const {
   if (!device_)
-    resolver->Reject(DOMException::Create(kNotFoundError, kDeviceUnavailable));
+    resolver->Reject(DOMException::Create(kNotFoundError, kDeviceDisconnected));
   else if (device_state_change_in_progress_)
     resolver->Reject(
         DOMException::Create(kInvalidStateError, kDeviceStateChangeInProgress));
@@ -528,7 +528,7 @@ bool USBDevice::EnsureNoDeviceOrInterfaceChangeInProgress(
 
 bool USBDevice::EnsureDeviceConfigured(ScriptPromiseResolver* resolver) const {
   if (!device_)
-    resolver->Reject(DOMException::Create(kNotFoundError, kDeviceUnavailable));
+    resolver->Reject(DOMException::Create(kNotFoundError, kDeviceDisconnected));
   else if (device_state_change_in_progress_)
     resolver->Reject(
         DOMException::Create(kInvalidStateError, kDeviceStateChangeInProgress));
@@ -920,7 +920,7 @@ void USBDevice::OnConnectionError() {
   device_.reset();
   opened_ = false;
   for (ScriptPromiseResolver* resolver : device_requests_)
-    resolver->Reject(DOMException::Create(kNotFoundError, kDeviceUnavailable));
+    resolver->Reject(DOMException::Create(kNotFoundError, kDeviceDisconnected));
   device_requests_.clear();
 }
 
