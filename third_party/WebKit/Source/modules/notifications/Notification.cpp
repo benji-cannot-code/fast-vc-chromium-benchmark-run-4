f68783b00a07c72ce9891e333e6e850943f191d9
Peter Beverloo <peter@chromium.org>
2017-07-28 15:55:29 +0000

    Revert "Restrict notification permission requests to top-level secure contexts"
    This reverts commit 393740484abb65c13ac7769ddc5a96a47befa79a.
    
    Reason for revert: this change hasn't been completed for M61,
    so we'll punt to M62 and finish integration with the rest of the
    permission systems.
    
    Original change's description:
    > Restrict notification permission requests to top-level secure contexts
    > 
    > Requesting notification permission could previously happen from any
    > context, which included iframes and insecure origins. Starting with
    > Chrome 61 we're restricting this to top-level secure contexts.
    > 
    > Usage will continue to be allowed in iframes once permission has been
    > granted from a top-level frame. Origins could easily work around such a
    > restriction by posting a message to their Service Worker, so it doesn't
    > make sense to impose it.
    > 
    > BUG=695693
    > TBR=stevenjb (http -> https in site settings unit test)
    > 
    > Change-Id: I33fe4278ed3ecd388872c74e6f299589855ea835
    > Reviewed-on: https://chromium-review.googlesource.com/558095
    > Reviewed-by: Peter Beverloo <peter@chromium.org>
    > Reviewed-by: Emil A Eklund <eae@chromium.org>
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Reviewed-by: Raymes Khoury <raymes@chromium.org>
    > Commit-Queue: Peter Beverloo <peter@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#487935}
    
    TBR=raymes@chromium.org,johnme@chromium.org,peter@chromium.org,eae@chromium.org,dominickn@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 695693
    Change-Id: If22e77639e8ee925855c47f127d466a5677aebaf
    Reviewed-on: https://chromium-review.googlesource.com/590492
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490414}

--- third_party/WebKit/Source/modules/notifications/Notification.cpp
+++ third_party/WebKit/Source/modules/notifications/Notification.cpp
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/notifications/NotificationData.h"
 #include "modules/notifications/NotificationManager.h"
 #include "modules/notifications/NotificationOptions.h"
-#include "modules/notifications/NotificationPermissionCallback.h"
 #include "modules/notifications/NotificationResourcesLoader.h"
 #include "platform/RuntimeEnabledFeatures.h"
 #include "platform/bindings/ScriptState.h"
@@ -157,11 +156,6 @@ void Notification::SchedulePrepareShow() {
 
 void Notification::PrepareShow() {
   DCHECK_EQ(state_, State::kLoading);
-  if (!GetExecutionContext()->IsSecureContext()) {
-    DispatchErrorEvent();
-    return;
-  }
-
   if (NotificationManager::From(GetExecutionContext())
           ->GetPermissionStatus(GetExecutionContext()) !=
       mojom::blink::PermissionStatus::GRANTED) {
@@ -364,33 +358,17 @@ String Notification::PermissionString(
 
 String Notification::permission(ScriptState* script_state) {
   ExecutionContext* context = ExecutionContext::From(script_state);
-  mojom::blink::PermissionStatus status =
-      mojom::blink::PermissionStatus::DENIED;
-
-  if (context->IsSecureContext())
-    status = NotificationManager::From(context)->GetPermissionStatus(context);
-
-  return PermissionString(status);
+  return PermissionString(
+      NotificationManager::From(context)->GetPermissionStatus(context));
 }
 
 ScriptPromise Notification::requestPermission(
     ScriptState* script_state,
     NotificationPermissionCallback* deprecated_callback) {
   ExecutionContext* context = ExecutionContext::From(script_state);
-
-  // Sites no longer have the ability to request notification permission from
-  // insecure contexts. Inform the developer.
   if (!context->IsSecureContext()) {
     Deprecation::CountDeprecation(
         context, WebFeature::kNotificationPermissionRequestedInsecureOrigin);
-
-    String status_denied =
-        Notification::PermissionString(mojom::blink::PermissionStatus::DENIED);
-
-    if (deprecated_callback)
-      deprecated_callback->handleEvent(status_denied);
-
-    return ScriptPromise::Cast(script_state, ToV8(status_denied, script_state));
   }
 
   if (context->IsDocument()) {
