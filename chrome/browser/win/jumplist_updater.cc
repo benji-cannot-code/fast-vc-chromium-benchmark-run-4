607d536a0f57a079aea80b34209fec1d7d0d0ad4
Xi Cheng <chengx@chromium.org>
2017-07-28 20:47:03 +0000

    Retire OS notification related UMA metrics in JumpList
    These metrics are no longer needed as crbug/736530 is fixed.
    
    Bug: 738126
    Change-Id: I497d9192063d16ba57e03924a10e27450ba7dc3a
    Reviewed-on: https://chromium-review.googlesource.com/586408
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Xi Cheng <chengx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490504}

--- chrome/browser/win/jumplist_updater.cc
+++ chrome/browser/win/jumplist_updater.cc
@@ -115,9 +115,6 @@ bool JumpListUpdater::IsEnabled() {
 }
 
 bool JumpListUpdater::BeginUpdate() {
-  // TODO(chengx): Remove the UMA histogram after fixing http://crbug.com/40407.
-  SCOPED_UMA_HISTOGRAM_TIMER("WinJumplistUpdater.BeginUpdateDuration");
-
   // This instance is expected to be one-time-use only.
   DCHECK(!destination_list_.Get());
 
@@ -154,9 +151,6 @@ bool JumpListUpdater::BeginUpdate() {
 }
 
 bool JumpListUpdater::CommitUpdate() {
-  // TODO(chengx): Remove the UMA histogram after fixing http://crbug.com/40407.
-  SCOPED_UMA_HISTOGRAM_TIMER("WinJumplistUpdater.CommitUpdateDuration");
-
   if (!destination_list_.Get())
     return false;
 
@@ -204,9 +198,6 @@ bool JumpListUpdater::AddTasks(const ShellLinkItemList& link_items) {
 bool JumpListUpdater::AddCustomCategory(const base::string16& category_name,
                                         const ShellLinkItemList& link_items,
                                         size_t max_items) {
-  // TODO(chengx): Remove the UMA histogram after fixing http://crbug.com/40407.
-  SCOPED_UMA_HISTOGRAM_TIMER("WinJumplistUpdater.AddCustomCategoryDuration");
-
   if (!destination_list_.Get())
     return false;
 
