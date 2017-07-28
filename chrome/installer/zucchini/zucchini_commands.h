8a6d473f364c4bccf76de844338a45f7af73ad7a
huangs <huangs@google.com>
2017-07-28 01:16:21 +0000

    [Zucchini] Refactor command line processing in main.
    This CL implements grt@'s post-commit suggestions from
    https://chromium-review.googlesource.com/c/567492/, notably:
    - Add OOM and crash handling.
    - Add error status and return them in main; remove exit() usage.
    - Remove static initializers for command registration.
    
    Additional refactoring of main program:
    - Add zucchini_commands.h for command specifications (including
      struct Command).
    - Add zucchini_commands.cc for main command flow, separate from
      command handling, dispatching, and help message code.
    - Add zucchini.h, containing enum zucchini::status::Code.
    - (Scoped)ResourceUsageTracker: Making it scoped, and applied only to
      main zucchini command invocation. Now the class is file-local.
    - Replace base::Callback usage with function pointers.
    - Replace explicit command registration with simple constexpr
      array, and CommandRegistry::Run() becomes RunZucchiniCommand().
    - Print memory usage stats *change* in addition to values.
    
    Bug: 729154
    Change-Id: Ia4c5d1eb0cef3249d858ec939168f3a97a3091d7
    Reviewed-on: https://chromium-review.googlesource.com/583807
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490176}

---
+++ chrome/installer/zucchini/zucchini_commands.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_INSTALLER_ZUCCHINI_ZUCCHINI_COMMANDS_H_
+#define CHROME_INSTALLER_ZUCCHINI_ZUCCHINI_COMMANDS_H_
+
+#include <vector>
+
+#include "base/files/file_path.h"
+#include "chrome/installer/zucchini/zucchini.h"
+
+// Zucchini commands and tools that can be invoked from command-line.
+
+namespace base {
+
+class CommandLine;
+
+}  // namespace base
+
+// Signature of a Zucchini Command Function.
+using CommandFunction =
+    zucchini::status::Code (*)(const base::CommandLine&,
+                               const std::vector<base::FilePath>&);
+
+// Command Function: Patch generation.
+zucchini::status::Code MainGen(const base::CommandLine& command_line,
+                               const std::vector<base::FilePath>& fnames);
+
+// Command Function: Patch application.
+zucchini::status::Code MainApply(const base::CommandLine& command_line,
+                                 const std::vector<base::FilePath>& fnames);
+
+#endif  // CHROME_INSTALLER_ZUCCHINI_ZUCCHINI_COMMANDS_H_
--- chrome/installer/zucchini/zucchini_main.cc
+++ chrome/installer/zucchini/zucchini_main.cc
@@ -3,9 +3,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <iostream>
+
 #include "base/command_line.h"
 #include "base/logging.h"
+#include "base/process/memory.h"
+#include "build/build_config.h"
 #include "chrome/installer/zucchini/main_utils.h"
+
+#if defined(OS_WIN)
+#include "base/win/process_startup_helper.h"
+#endif  // defined(OS_WIN)
 
 namespace {
 
@@ -19,21 +27,23 @@ void InitLogging() {
   CHECK(logging_res);
 }
 
+void InitErrorHandling(const base::CommandLine& command_line) {
+  base::EnableTerminationOnHeapCorruption();
+  base::EnableTerminationOnOutOfMemory();
+#if defined(OS_WIN)
+  base::win::RegisterInvalidParamHandler();
+  base::win::SetupCRT(command_line);
+#endif  // defined(OS_WIN)
+}
+
 }  // namespace
 
 int main(int argc, const char* argv[]) {
-  ResourceUsageTracker tracker;
+  // Initialize infrastructure from base.
   base::CommandLine::Init(argc, argv);
-  InitLogging();
-
   const base::CommandLine& command_line =
       *base::CommandLine::ForCurrentProcess();
-
-  // Instantiate Command registry and register Zucchini features.
-  CommandRegistry registry;
-  registry.Register(&kCommandGen);
-  registry.Register(&kCommandApply);
-
-  registry.RunOrExit(command_line);
-  return 0;
+  InitLogging();
+  InitErrorHandling(command_line);
+  return static_cast<int>(RunZucchiniCommand(command_line, std::cerr));
 }
