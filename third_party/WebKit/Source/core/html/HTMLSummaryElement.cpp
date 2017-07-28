9208da84640fd38c86d1b27b78a4674e3d2fe7da
Kent Tamura <tkent@chromium.org>
2017-07-28 05:21:35 +0000

    SUMMARY element: Enable focus with tabindex attribute.
    <summary tabindex=N> should make it focusable. HTMLSummaryElement::
    SupportsFocus() had a bug that it missed HTMLElement::SupportsFocus()
    check.
    
    Bug: 745305
    Change-Id: I0eaf17614950d8946152e153db67bc47b8a364f4
    Reviewed-on: https://chromium-review.googlesource.com/588207
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Takayoshi Kochi <kochi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490282}

--- third_party/WebKit/Source/core/html/HTMLSummaryElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLSummaryElement.cpp
@@ -95,7 +95,7 @@ static bool IsClickableControl(Node* node) {
 }
 
 bool HTMLSummaryElement::SupportsFocus() const {
-  return IsMainSummary();
+  return IsMainSummary() || HTMLElement::SupportsFocus();
 }
 
 void HTMLSummaryElement::DefaultEventHandler(Event* event) {
