21af7cc5b7d3fd9dd66f61ce7d4a492c776a3cf4
Jia <jiameng@chromium.org>
2017-07-28 02:36:39 +0000

    Implement parseShorthand for 6 legacy break properties.
    This implements parseShorthand for page-break-[after|before|inside] 
    and -webkit-column-break-[after|before|inside].
    
    This cl also refactors related util functions and moves them from
    CSSPropertyParsers to a new util file CSSPropertyLegacyBreakUtils.
    
    Bug: 668012
    Change-Id: I96673b991fd6cca72cc24e3d27dc3afdaddb76c4
    Reviewed-on: https://chromium-review.googlesource.com/585028
    Commit-Queue: Jia Meng <jiameng@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Bugs Nash <bugsnash@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490219}

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.h
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.h
@@ -98,8 +98,6 @@ class CSSPropertyParser {
   bool ConsumePlaceItemsShorthand(bool important);
   bool ConsumePlaceSelfShorthand(bool important);
 
-  bool ConsumeLegacyBreakProperty(CSSPropertyID, bool important);
-
  private:
   // Inputs:
   CSSParserTokenRange range_;
