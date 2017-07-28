3febfa83267e1a8e1296fca1a7ff2234153905b5
Luna Lu <loonybear@chromium.org>
2017-07-28 18:30:37 +0000

    Reland "Add a PageLoadMetricsObserver that tracks all observed features and reports a new histogram"
    This is a reland of 1423af088e7fd7b6671a6328329918936e7bd234
    Original change's description:
    > Add a PageLoadMetricsObserver that tracks all observed features and reports a new histogram
    >
    > For more information, please refer to this doc:
    >
    > https: //docs.google.com/document/d/1jaUv28xBPrOrkZV9gck9btEKvgM8u_awO83SXraPbQ4/edit?usp=sharing
    > Bug: 716565
    > Change-Id: I040ecc5e44df15768649684b4f5ee8d5e828c209
    > Reviewed-on: https://chromium-review.googlesource.com/534133
    > Commit-Queue: Luna Lu <loonybear@chromium.org>
    > Reviewed-by: Nico Weber <thakis@chromium.org>
    > Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Mark Pearson <mpearson@chromium.org>
    > Reviewed-by: Rick Byers <rbyers@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#487612}
    
    TBR=mpearson
    
    Bug: 716565
    Change-Id: I137792c35322876f3bbe936ea5f7a0c715ee05bb
    Reviewed-on: https://chromium-review.googlesource.com/577828
    Reviewed-by: Luna Lu <loonybear@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Luna Lu <loonybear@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490472}

--- chrome/browser/page_load_metrics/metrics_web_contents_observer.cc
+++ chrome/browser/page_load_metrics/metrics_web_contents_observer.cc
@@ -617,7 +617,7 @@ void MetricsWebContentsObserver::OnTimingUpdated(
 
   if (committed_load_) {
     committed_load_->metrics_update_dispatcher()->UpdateMetrics(
-        render_frame_host, timing, metadata);
+        render_frame_host, timing, metadata, new_features);
   }
 }
 
