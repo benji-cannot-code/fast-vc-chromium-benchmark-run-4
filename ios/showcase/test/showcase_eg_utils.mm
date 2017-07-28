45a02476f8111934c0bb4aef8697cb915fb9d616
Yuke Liao <liaoyuke@chromium.org>
2017-07-28 17:11:50 +0000

    Fix back button for show case on iOS 11.
    The original matcher fails on IOS 11 because it depends on a private
    class and that class is not used anymore. Find a more robust solution
    that is consistent across different iOS versions.
    
    Bug: 750185
    Change-Id: I54d54452a6dbf721401d3eaa14a010cfcd129d81
    Reviewed-on: https://chromium-review.googlesource.com/591911
    Commit-Queue: Yuke Liao <liaoyuke@chromium.org>
    Reviewed-by: Ed Chin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490436}

--- ios/showcase/test/showcase_eg_utils.mm
+++ ios/showcase/test/showcase_eg_utils.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/showcase/test/showcase_eg_utils.h"
 
+#include "base/ios/ios_util.h"
 #import "base/mac/foundation_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -16,8 +17,9 @@ namespace {
 // Matcher for the back button on screens presented from the Showcase home
 // screen.
 id<GREYMatcher> BackButton() {
-  return grey_kindOfClass(
-      NSClassFromString(@"_UINavigationBarBackIndicatorView"));
+  return grey_allOf(grey_accessibilityLabel(@"SC"),
+                    grey_accessibilityTrait(UIAccessibilityTraitButton),
+                    grey_userInteractionEnabled(), nil);
 }
 
 // Matcher for the Showcase home screen view.
