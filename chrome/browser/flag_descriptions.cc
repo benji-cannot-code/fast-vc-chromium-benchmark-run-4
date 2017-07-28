ae99867db10591d796065cf973d2f4a525a242b9
Tatiana Gornak <melandory@chromium.org>
2017-07-28 11:04:18 +0000

    Add flag for passwords viewing and copying for Android.
    This CL adds flag to chrome://flags for controlling copying and viewing
    functionality for the password manager settings page.
    
    BUG=619868
    
    Change-Id: I7353c17d6e40d6e584780d033555fbe0073f1a3c
    Reviewed-on: https://chromium-review.googlesource.com/586606
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Commit-Queue: Tatiana Gornak <melandory@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490357}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -1402,6 +1402,10 @@ const char kAndroidSigninPromosDescription[] =
     "New signin promos for Android which will be displayed in content "
     "suggestions, Settings, Bookmarks and Recent Tabs.";
 
+const char kAndroidViewPasswordsName[] = "Copy and view passwords for Android";
+const char kAndroidViewPasswordsDescription[] =
+    "Enables copying and viewing passwords in settings.";
+
 const char kAutofillAccessoryViewName[] =
     "Autofill suggestions as keyboard accessory view";
 const char kAutofillAccessoryViewDescription[] =
