2aa8fd191c475973172c50401178b876c0360beb
Sergey Ulanov <sergeyu@google.com>
2017-07-28 05:35:50 +0000

    Add DLOG(FATAL) for EIO on Fuchsia.
    crrev.com/489503 added EIO in MapSystemError(). Fuchsia's MXIO may
    return EIO for some errors currently, but it's better to use
    more-specific error codes. This CL adds an DLOG(FATAL) to make the
    cases when EIO is returned visible.
    
    Also updated warning that's logged for unrecognized error codes to
    include strerror().
    
    Bug: 731302
    Change-Id: Iea285424ee0fa0a1fa06ba91fe7204e50b48dd98
    Reviewed-on: https://chromium-review.googlesource.com/588030
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490286}

--- net/base/net_errors_posix.cc
+++ net/base/net_errors_posix.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unistd.h>
 
 #include "base/logging.h"
+#include "base/posix/safe_strerror.h"
+#include "build/build_config.h"
 
 namespace net {
 
@@ -109,14 +111,19 @@ Error MapSystemError(logging::SystemErrorCode os_error) {
       return ERR_INSUFFICIENT_RESOURCES;
     case EMFILE:  // Too many open files.
       return ERR_INSUFFICIENT_RESOURCES;
-    case EIO:  // Generic IO error.
+#if defined(OS_FUCHSIA)
+    case EIO:
+      // MXIO maps all unrecognized errors to EIO. If you see this message then
+      // consider adding custom error in MXIO for the corresponding error.
+      DLOG(FATAL) << "EIO was returned by MXIO.";
       return ERR_FAILED;
+#endif  // OS_FUCHSIA
 
     case 0:
       return OK;
     default:
-      LOG(WARNING) << "Unknown error " << os_error
-                   << " mapped to net::ERR_FAILED";
+      LOG(WARNING) << "Unknown error " << base::safe_strerror(os_error) << " ("
+                   << os_error << ") mapped to net::ERR_FAILED";
       return ERR_FAILED;
   }
 }
