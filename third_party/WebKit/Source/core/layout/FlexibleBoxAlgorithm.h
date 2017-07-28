96ac23b90d5174baf6c1a969e1e8f97b59965c9b
Christian Biesinger <cbiesinger@chromium.org>
2017-07-28 15:41:06 +0000

    [css-flexbox] Split up PlaceLineItems into Compute + Apply functions
    Change-Id: I36bcd29eb1000ac939dfae764ed98993838a5480
    Reviewed-on: https://chromium-review.googlesource.com/590729
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@opera.com>
    Cr-Commit-Position: refs/heads/master@{#490405}

--- third_party/WebKit/Source/core/layout/FlexibleBoxAlgorithm.h
+++ third_party/WebKit/Source/core/layout/FlexibleBoxAlgorithm.h
@@ -76,6 +76,10 @@ class FlexItem {
            main_axis_margin;
   }
 
+  LayoutUnit FlexedBorderBoxSize() const {
+    return flexed_content_size + main_axis_border_and_padding;
+  }
+
   LayoutUnit FlexedMarginBoxSize() const {
     return flexed_content_size + main_axis_border_and_padding +
            main_axis_margin;
@@ -91,7 +95,6 @@ class FlexItem {
   LayoutUnit FlowAwareMarginEnd() const;
   LayoutUnit FlowAwareMarginBefore() const;
   LayoutUnit CrossAxisMarginExtent() const;
-  LayoutUnit CrossAxisExtent() const;
 
   LayoutUnit MarginBoxAscent() const;
 
@@ -105,6 +108,10 @@ class FlexItem {
   const LayoutUnit main_axis_border_and_padding;
   const LayoutUnit main_axis_margin;
   LayoutUnit flexed_content_size;
+
+  LayoutUnit cross_axis_size;
+  LayoutPoint desired_location;
+
   bool frozen;
 };
 
@@ -163,8 +170,9 @@ class FlexLine {
   LayoutUnit initial_free_space;
   LayoutUnit remaining_free_space;
 
-  // These get filled in by PlaceLineItems (for now)
+  // These get filled in by ComputeLineItemsPosition (for now)
   // TODO(cbiesinger): Move that to FlexibleBoxAlgorithm.
+  LayoutUnit main_axis_extent;
   LayoutUnit cross_axis_offset;
   LayoutUnit cross_axis_extent;
   LayoutUnit max_ascent;
