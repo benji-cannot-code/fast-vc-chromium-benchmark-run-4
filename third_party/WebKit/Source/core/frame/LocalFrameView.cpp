2132ebe41a07ea2f28ef85485984e35d1e435204
Yoshifumi Inoue <yosin@chromium.org>
2017-07-28 10:07:47 +0000

    Use VisibleSelection::IsRange() instead of GetSelectionType() in LocalFrameView::ComputeCompositedSelection()
    This patch changes |LocalFrameView::ComputeCompositedSelection()| to use
    |IsRange()| instead of |GetSelectionType()| of |VisibleSelection| as a
    preparation of getting rid of |GetSelectionType()|.
    
    Bug: 657237
    Change-Id: I28d3ac203222c32625cb2a8e6254288236d469dc
    Reviewed-on: https://chromium-review.googlesource.com/590344
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490345}

--- third_party/WebKit/Source/core/frame/LocalFrameView.cpp
+++ third_party/WebKit/Source/core/frame/LocalFrameView.cpp
@@ -2079,7 +2079,9 @@ bool LocalFrameView::ComputeCompositedSelection(
   if (!selection.end.layer)
     return false;
 
-  selection.type = visible_selection.GetSelectionType();
+  DCHECK(!visible_selection.IsNone());
+  selection.type =
+      visible_selection.IsRange() ? kRangeSelection : kCaretSelection;
   selection.start.is_text_direction_rtl |=
       PrimaryDirectionOf(*visible_selection.Start().AnchorNode()) ==
       TextDirection::kRtl;
