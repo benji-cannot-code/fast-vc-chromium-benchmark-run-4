bc5f2606cc9f3eec138758c0e16cfe228c6a290c
Justin Novosad <junov@chromium.org>
2017-07-29 02:23:56 +0000

    Fix crashes in SharedGpuContext due to context creation failure
    Changing SharedGpuContext to not assume that context creation
    always succeeds.  Adding a bunch of unit tests to verify
    robustness with respect to GPU context creation failures.
    
    BUG=749990
    
    Change-Id: Ia7431a21e6934c998f8d646b377fd0c9ce2c63cf
    Reviewed-on: https://chromium-review.googlesource.com/592337
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490586}

--- third_party/WebKit/Source/platform/graphics/gpu/SharedGpuContextTest.cpp
+++ third_party/WebKit/Source/platform/graphics/gpu/SharedGpuContextTest.cpp
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/graphics/gpu/SharedGpuContext.h"
 
 #include "gpu/command_buffer/client/gles2_interface.h"
+#include "platform/graphics/Canvas2DLayerBridge.h"
+#include "platform/graphics/gpu/AcceleratedImageBufferSurface.h"
 #include "platform/graphics/test/FakeGLES2Interface.h"
 #include "platform/graphics/test/FakeWebGraphicsContext3DProvider.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -34,6 +36,19 @@ class SharedGpuContextTest : public Test {
   FakeGLES2Interface gl_;
 };
 
+// Test fixure that simulate a graphics context creation failure
+class BadSharedGpuContextTest : public Test {
+ public:
+  void SetUp() override {
+    SharedGpuContext::SetContextProviderFactoryForTesting(
+        [] { return std::unique_ptr<WebGraphicsContext3DProvider>(nullptr); });
+  }
+
+  void TearDown() override {
+    SharedGpuContext::SetContextProviderFactoryForTesting(nullptr);
+  }
+};
+
 TEST_F(SharedGpuContextTest, contextLossAutoRecovery) {
   EXPECT_NE(SharedGpuContext::ContextProviderWrapper(), nullptr);
   WeakPtr<WebGraphicsContext3DProviderWrapper> context =
@@ -45,6 +60,55 @@ TEST_F(SharedGpuContextTest, contextLossAutoRecovery) {
   // Context recreation results in old provider being discarded.
   EXPECT_TRUE(!!SharedGpuContext::ContextProviderWrapper());
   EXPECT_FALSE(!!context);
+}
+
+TEST_F(SharedGpuContextTest, AccelerateImageBufferSurfaceAutoRecovery) {
+  // Verifies that after a context loss, attempting to allocate an
+  // AcceleratedImageBufferSurface will restore the context and succeed
+  gl_.SetIsContextLost(true);
+  EXPECT_FALSE(SharedGpuContext::IsValidWithoutRestoring());
+  IntSize size(10, 10);
+  std::unique_ptr<ImageBufferSurface> surface =
+      WTF::WrapUnique(new AcceleratedImageBufferSurface(size, kNonOpaque));
+  EXPECT_TRUE(surface->IsValid());
+  EXPECT_TRUE(SharedGpuContext::IsValidWithoutRestoring());
+}
+
+TEST_F(SharedGpuContextTest, Canvas2DLayerBridgeAutoRecovery) {
+  // Verifies that after a context loss, attempting to allocate a
+  // Canvas2DLayerBridge will restore the context and succeed.
+  gl_.SetIsContextLost(true);
+  EXPECT_FALSE(SharedGpuContext::IsValidWithoutRestoring());
+  IntSize size(10, 10);
+  CanvasColorParams color_params;
+  RefPtr<Canvas2DLayerBridge> bridge = AdoptRef(new Canvas2DLayerBridge(
+      size, 0, /*msaa sample count*/
+      kNonOpaque, Canvas2DLayerBridge::kEnableAcceleration, color_params));
+  EXPECT_TRUE(bridge->IsAccelerated());
+  EXPECT_TRUE(SharedGpuContext::IsValidWithoutRestoring());
+  bridge->BeginDestruction();
+}
+
+TEST_F(SharedGpuContextTest, IsValidWithoutRestoring) {
+  EXPECT_NE(SharedGpuContext::ContextProviderWrapper(), nullptr);
+  EXPECT_TRUE(SharedGpuContext::IsValidWithoutRestoring());
+}
+
+TEST_F(BadSharedGpuContextTest, IsValidWithoutRestoring) {
+  EXPECT_FALSE(SharedGpuContext::IsValidWithoutRestoring());
+}
+
+TEST_F(BadSharedGpuContextTest, AllowSoftwareToAcceleratedCanvasUpgrade) {
+  EXPECT_FALSE(SharedGpuContext::AllowSoftwareToAcceleratedCanvasUpgrade());
+}
+
+TEST_F(BadSharedGpuContextTest, AccelerateImageBufferSurfaceCreationFails) {
+  // With a bad shared context, AccelerateImageBufferSurface creation should
+  // fail gracefully
+  IntSize size(10, 10);
+  std::unique_ptr<ImageBufferSurface> surface =
+      WTF::WrapUnique(new AcceleratedImageBufferSurface(size, kNonOpaque));
+  EXPECT_FALSE(surface->IsValid());
 }
 
 }  // unnamed namespace
