eab09aafbcb48c88b63cf2a4222529628515b6c5
Mohsen Izadi <mohsen@chromium.org>
2017-07-29 04:45:15 +0000

    Add --pull-to-refresh flag for pull-to-refresh
    Pull-to-refresh should land behind a flag. There is an unused
    --scroll-end-effect flag that was supposed to be used for a related
    scroll-end effect feature which was never implemented. This change
    renames the flag to use with pull-to-refresh.
    
    BUG=702404
    TEST=none
    
    Change-Id: If0d9b4e84ef6515137e1280154bf5dfc31eb4dd6
    Reviewed-on: https://chromium-review.googlesource.com/582169
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Reviewed-by: Nick Carter <nick@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490626}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -610,6 +610,9 @@ extern const char kProtectSyncCredentialDescription[];
 extern const char kProtectSyncCredentialOnReauthName[];
 extern const char kProtectSyncCredentialOnReauthDescription[];
 
+extern const char kPullToRefreshName[];
+extern const char kPullToRefreshDescription[];
+
 extern const char kPushApiBackgroundModeName[];
 extern const char kPushApiBackgroundModeDescription[];
 
@@ -636,9 +639,6 @@ extern const char kSaveasMenuLabelExperimentDescription[];
 
 extern const char kSavePageAsMhtmlName[];
 extern const char kSavePageAsMhtmlDescription[];
-
-extern const char kScrollEndEffectName[];
-extern const char kScrollEndEffectDescription[];
 
 extern const char kScrollPredictionName[];
 extern const char kScrollPredictionDescription[];
