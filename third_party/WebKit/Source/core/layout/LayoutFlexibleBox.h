f3f82f28d3bd44e3a3649cbd12971f73c7b73f82
Christian Biesinger <cbiesinger@chromium.org>
2017-07-29 07:08:23 +0000

    [css-flexbox] Move more functions to FlexLayoutAlgorithm
    Especially all the ones needed by ComputeLineItemsPosition, in preparation
    for moving that function to FlexLine.
    
    This replaces MainAxisExtent() with LogicalWidth(), because it's simpler to
    compute and it's only used when !IsColumnFlow(), so they are equivalent.
    
    It also moves the available space recomputation into ComputeLinePosition
    because it doesn't rely on any additional data, and moves out the
    main_axis_offset computation because it does.
    
    Change-Id: Icd18329b113127b464497c0ed3e354ac3de3654a
    Reviewed-on: https://chromium-review.googlesource.com/592405
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490641}

--- third_party/WebKit/Source/core/layout/LayoutFlexibleBox.h
+++ third_party/WebKit/Source/core/layout/LayoutFlexibleBox.h
@@ -60,7 +60,6 @@ class CORE_EXPORT LayoutFlexibleBox : public LayoutBlock {
       LineDirectionMode,
       LinePositionMode = kPositionOnContainingLine) const override;
 
-  static const StyleContentAlignmentData& ContentAlignmentNormalBehavior();
   static LayoutUnit SynthesizedBaselineFromContentBox(const LayoutBox&,
                                                       LineDirectionMode);
 
@@ -118,14 +117,11 @@ class CORE_EXPORT LayoutFlexibleBox : public LayoutBlock {
   LayoutUnit MainAxisContentExtentForChildIncludingScrollbar(
       const LayoutBox& child) const;
   LayoutUnit CrossAxisExtent() const;
-  LayoutUnit MainAxisExtent() const;
   LayoutUnit CrossAxisContentExtent() const;
   LayoutUnit MainAxisContentExtent(LayoutUnit content_logical_height);
   LayoutUnit ComputeMainAxisExtentForChild(const LayoutBox& child,
                                            SizeType,
                                            const Length& size) const;
-  StyleContentAlignmentData ResolvedJustifyContent() const;
-  StyleContentAlignmentData ResolvedAlignContent() const;
   LayoutUnit FlowAwareBorderStart() const;
   LayoutUnit FlowAwareBorderEnd() const;
   LayoutUnit FlowAwareBorderBefore() const;
@@ -148,7 +144,6 @@ class CORE_EXPORT LayoutFlexibleBox : public LayoutBlock {
       LayoutUnit main_axis_border_and_padding,
       ChildLayoutType = kLayoutIfNeeded);
   void AdjustAlignmentForChild(LayoutBox& child, LayoutUnit);
-  ItemPosition AlignmentForChild(const LayoutBox& child) const;
   bool MainAxisLengthIsDefinite(const LayoutBox& child,
                                 const Length& flex_basis) const;
   bool CrossAxisLengthIsDefinite(const LayoutBox& child,
@@ -189,7 +184,7 @@ class CORE_EXPORT LayoutFlexibleBox : public LayoutBlock {
   void LayoutLineItems(FlexLine*, bool relayout_children, SubtreeLayoutScope&);
   void ComputeLineItemsPosition(LayoutUnit& cross_axis_offset,
                                 FlexLine*,
-                                LayoutUnit available_free_space);
+                                LayoutUnit main_axis_offset);
   void ApplyLineItemsPosition(FlexLine*);
   void LayoutColumnReverse(Vector<FlexItem>&,
                            LayoutUnit cross_axis_offset,
