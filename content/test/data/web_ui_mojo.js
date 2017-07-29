a5aee5f24951defa01850ed453bdc5c62aa593c3
Yuzhu Shen <yzshen@chromium.org>
2017-07-29 06:56:07 +0000

    Mojo JS bindings: switch web_ui_test_mojo_bindings to use new mode.
    BUG=699569
    
    Change-Id: I98a9f7053742f29473f25ad97b70d519ce337b30
    Reviewed-on: https://chromium-review.googlesource.com/592276
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490639}

--- content/test/data/web_ui_mojo.js
+++ content/test/data/web_ui_mojo.js
@@ -3,19 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-define('main', [
-    'mojo/public/js/bindings',
-    'content/test/data/web_ui_test_mojo_bindings.mojom',
-    'content/public/renderer/frame_interfaces',
-], function (bindings, webUITest, interfaceProvider) {
-  var browserTarget;
+var browserTarget = new content.mojom.BrowserTargetPtr;
+Mojo.bindInterface(content.mojom.BrowserTarget.name,
+                   mojo.makeRequest(browserTarget).handle);
 
-  return function() {
-    browserTarget = new webUITest.BrowserTargetPtr(
-        interfaceProvider.getInterface(webUITest.BrowserTarget.name));
-
-    browserTarget.start().then(function() {
-      browserTarget.stop();
-    });
-  };
+browserTarget.start().then(function() {
+  browserTarget.stop();
 });
