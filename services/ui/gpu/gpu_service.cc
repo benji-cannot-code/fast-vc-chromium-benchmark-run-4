ccd046a3c55a68af60dadf287b52f7b01e4ac0ca
Miguel Casas-Sanchez <mcasas@chromium.org>
2017-07-29 14:19:21 +0000

    VEA mojification: allow for Mojo VEAs to be instantiated several times
    On ToT, RenderThreadImpl creates one "remote" mojom::VideoEncodeAcceleratorPtr
    on main render thread and gives it to GpuVideoAcceleratorFactoriesImpl [1];
    this factory gives it to the first constructed MojoVideoEncodeAccelerator;
    this is good for the first time, but after that it'd be gone, the construction
    of subsequent MojoVideoEncodeAccelerators will fail and we'll be falling back 
    to software and/or the old IPC mechanism.
    
    This CL changes that by adding a mojom VideoEncodeAcceleratorProvider interface,
    which is bound along the chain:
    
     RenderThreadImpl --> gpu.mojom --> GpuClient --> gpu_service.mojom --> GpuService
    
    GpuService finally bind the request to a new MojoVideoEncodeAcceleratorProvider 
    (added in this CL as well).
    
    GpuVideoAcceleratorFactoriesImpl uses this bound Provider to create 
    MojoVideoEncodeAccelerators every time.
    
    [1] https://cs.chromium.org/chromium/src/content/renderer/render_thread_impl.cc?dr=CSs&sq=package:chromium&l=1463
    
    Bug: 736517
    Change-Id: I3c753f951f120e1d2ffefb50a526fac15de6e881
    Reviewed-on: https://chromium-review.googlesource.com/590513
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490658}

--- services/ui/gpu/gpu_service.cc
+++ services/ui/gpu/gpu_service.cc
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/ipc/service/gpu_video_decode_accelerator.h"
 #include "media/gpu/ipc/service/gpu_video_encode_accelerator.h"
 #include "media/gpu/ipc/service/media_gpu_channel_manager.h"
-#include "media/mojo/services/mojo_video_encode_accelerator_service.h"
+#include "media/mojo/services/mojo_video_encode_accelerator_provider.h"
 #include "mojo/public/cpp/bindings/strong_binding.h"
 #include "ui/gl/gl_implementation.h"
 #include "ui/gl/gl_switches.h"
@@ -235,11 +235,11 @@ void GpuService::CreateJpegDecodeAccelerator(
   NOTIMPLEMENTED();
 }
 
-void GpuService::CreateVideoEncodeAccelerator(
-    media::mojom::VideoEncodeAcceleratorRequest vea_request) {
+void GpuService::CreateVideoEncodeAcceleratorProvider(
+    media::mojom::VideoEncodeAcceleratorProviderRequest vea_provider_request) {
   DCHECK(io_runner_->BelongsToCurrentThread());
-  media::MojoVideoEncodeAcceleratorService::Create(
-      std::move(vea_request),
+  media::MojoVideoEncodeAcceleratorProvider::Create(
+      std::move(vea_provider_request),
       base::Bind(&media::GpuVideoEncodeAcceleratorFactory::CreateVEA),
       gpu_preferences_);
 }
--- services/ui/public/cpp/tests/gpu_unittest.cc
+++ services/ui/public/cpp/tests/gpu_unittest.cc
@@ -57,8 +57,8 @@ class TestGpuImpl : public mojom::Gpu {
   void CreateJpegDecodeAccelerator(
       media::mojom::GpuJpegDecodeAcceleratorRequest jda_request) override {}
 
-  void CreateVideoEncodeAccelerator(
-      media::mojom::VideoEncodeAcceleratorRequest vea_request) override {}
+  void CreateVideoEncodeAcceleratorProvider(
+      media::mojom::VideoEncodeAcceleratorProviderRequest request) override {}
 
   void CreateGpuMemoryBuffer(
       gfx::GpuMemoryBufferId id,
