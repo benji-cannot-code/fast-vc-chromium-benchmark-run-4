ef944e52fb5305492b636ce4c99b84804071121d
Uzair Jaleel <uzair.jaleel@samsung.com>
2017-07-29 17:46:49 +0000

    Remove WeakPtrFactory from PepperFileSystemHost
    PepperFileSystemHost already inherits SupportsWeakPtr.
    Hence, instead of creating a WeakPtrFactory instance to
    access its weak pointer, we can directly use AsWeakPtr().
    
    BUG=None
    
    Change-Id: I97e716d9ceb479e02ae7b80ea77fe1eaf7bf28b0
    Reviewed-on: https://chromium-review.googlesource.com/590342
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Commit-Queue: SUCHIT AGRAWAL <a.suchit@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490664}

--- content/renderer/pepper/pepper_file_system_host.cc
+++ content/renderer/pepper/pepper_file_system_host.cc
@@ -34,8 +34,7 @@ PepperFileSystemHost::PepperFileSystemHost(RendererPpapiHost* host,
       renderer_ppapi_host_(host),
       type_(type),
       opened_(false),
-      called_open_(false),
-      weak_factory_(this) {}
+      called_open_(false) {}
 
 PepperFileSystemHost::PepperFileSystemHost(RendererPpapiHost* host,
                                            PP_Instance instance,
@@ -47,8 +46,7 @@ PepperFileSystemHost::PepperFileSystemHost(RendererPpapiHost* host,
       type_(type),
       opened_(true),
       root_url_(root_url),
-      called_open_(true),
-      weak_factory_(this) {}
+      called_open_(true) {}
 
 PepperFileSystemHost::~PepperFileSystemHost() {}
 
@@ -106,12 +104,9 @@ int32_t PepperFileSystemHost::OnHostMsgOpen(
       ChildThreadImpl::current()->file_system_dispatcher();
   reply_context_ = context->MakeReplyMessageContext();
   file_system_dispatcher->OpenFileSystem(
-      document_url.GetOrigin(),
-      file_system_type,
-      base::Bind(&PepperFileSystemHost::DidOpenFileSystem,
-                 weak_factory_.GetWeakPtr()),
-      base::Bind(&PepperFileSystemHost::DidFailOpenFileSystem,
-                 weak_factory_.GetWeakPtr()));
+      document_url.GetOrigin(), file_system_type,
+      base::Bind(&PepperFileSystemHost::DidOpenFileSystem, AsWeakPtr()),
+      base::Bind(&PepperFileSystemHost::DidFailOpenFileSystem, AsWeakPtr()));
   return PP_OK_COMPLETIONPENDING;
 }
 
