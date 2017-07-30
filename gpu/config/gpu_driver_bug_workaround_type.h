661e6a9b42a038730cdd5a5d1c06216c47d35cf2
Kenneth Russell <kbr@chromium.org>
2017-07-30 20:45:09 +0000

    Add workaround for intermittent compositor context losses.
    On current Android versions there is a race condition while sending
    the application to the background. The browser attempts to drain its
    graphics pipeline and cease rendering, but in the meantime one call to
    eglSwapBuffers fails. This causes the compositor's context to be lost,
    and since this is treated as a severe failure, it causes all active
    WebGL content to be blocked.
    
    To work around this problem but maintain strictness on other
    platforms, add a time-expiring driver bug workaround that applies to
    the current and next Android release.
    
    BUG=744678
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I4eb1344f17b6b12cabf8964856e0645473797a8e
    Reviewed-on: https://chromium-review.googlesource.com/584037
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490688}

--- gpu/config/gpu_driver_bug_workaround_type.h
+++ gpu/config/gpu_driver_bug_workaround_type.h
@@ -102,6 +102,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
          disable_webgl_rgb_multisampling_usage)              \
   GPU_OP(DISALLOW_LARGE_INSTANCED_DRAW,                      \
          disallow_large_instanced_draw)                      \
+  GPU_OP(DONT_DISABLE_WEBGL_WHEN_COMPOSITOR_CONTEXT_LOST,    \
+         dont_disable_webgl_when_compositor_context_lost)    \
   GPU_OP(DONT_REMOVE_INVARIANT_FOR_FRAGMENT_INPUT,           \
          dont_remove_invariant_for_fragment_input)           \
   GPU_OP(ETC1_POWER_OF_TWO_ONLY,                             \
