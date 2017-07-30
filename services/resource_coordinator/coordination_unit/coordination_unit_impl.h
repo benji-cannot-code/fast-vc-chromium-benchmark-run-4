66c913e5673771820948a00cf0d98ec9508e58c0
meade_UTC10 <meade@chromium.org>
2017-07-30 05:07:15 +0000

    Revert "Plumb URLs through memory_instrumentation UKMs."
    This reverts commit 1d8d254c7eb403770bfc310cb9abe3406fac9503.
    
    Reason for revert: Broke
    ProcessMemoryMetricsEmitterTest.FetchDuringTrace
    ProcessMemoryMetricsEmitterTest.FetchAndEmitMetrics
    ProcessMemoryMetricsEmitterTest.FetchThreeTimes
    
    on chromium.win/Win7 Tests (dbg)(1)
    
    First broken run: https://uberchromegw.corp.google.com/i/chromium.win/builders/Win7%20Tests%20%28dbg%29%281%29/builds/61933
    
    
    Original change's description:
    > Plumb URLs through memory_instrumentation UKMs.
    > 
    > The URLs are obtained by creating a new interface CoordinationUnitIntrospector
    > on the resource_coordinator service. The implementation of this interface crawls
    > all ProcessCoordinationUnits, and creates a map from pid to hosted URLs by
    > looking at associated FrameCoordinationUnits.
    > 
    > The interface for memory_instrumentation is modified slightly to include pid in
    > ProcessMemoryDump to allow correlation between the memory dump and the URLs
    > obtained from resource_coordinator. In a hypothetical future world where
    > memory_instrumentation and resource_coordinator live in the same service, this
    > can be removed.
    > 
    > Bug:730783
    > 
    > Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    > Change-Id: Ia1de1af692df4d7d08239db91c9fd14b71a15b70
    > Reviewed-on: https://chromium-review.googlesource.com/578482
    > Commit-Queue: Erik Chen <erikchen@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    > Reviewed-by: Oystein Eftevaag <oysteine@chromium.org>
    > Reviewed-by: Primiano Tucci <primiano@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#490668}
    
    TBR=avi@chromium.org,dcheng@chromium.org,thestig@chromium.org,primiano@chromium.org,rkaplow@chromium.org,erikchen@chromium.org,oysteine@chromium.org
    
    Change-Id: I07a942b89aa22dcddd924fdcaeda025b6484b4eb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 730783,750476
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Reviewed-on: https://chromium-review.googlesource.com/593267
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Commit-Queue: meade_UTC10 <meade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490682}

--- services/resource_coordinator/coordination_unit/coordination_unit_impl.h
+++ services/resource_coordinator/coordination_unit/coordination_unit_impl.h
@@ -36,8 +36,6 @@ class CoordinationUnitImpl : public mojom::CoordinationUnit {
 
   static const FrameCoordinationUnitImpl* ToFrameCoordinationUnit(
       const CoordinationUnitImpl* coordination_unit);
-  static std::vector<CoordinationUnitImpl*> GetCoordinationUnitsOfType(
-      CoordinationUnitType type);
 
   static CoordinationUnitImpl* CreateCoordinationUnit(
       const CoordinationUnitID& id,
--- services/resource_coordinator/resource_coordinator_service.h
+++ services/resource_coordinator/resource_coordinator_service.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "services/metrics/public/cpp/mojo_ukm_recorder.h"
-#include "services/resource_coordinator/coordination_unit/coordination_unit_introspector_impl.h"
 #include "services/resource_coordinator/coordination_unit/coordination_unit_manager.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
 #include "services/service_manager/public/cpp/service.h"
@@ -51,7 +50,6 @@ class ResourceCoordinatorService : public service_manager::Service {
   service_manager::BinderRegistry registry_;
   std::unique_ptr<service_manager::ServiceContextRefFactory> ref_factory_;
   CoordinationUnitManager coordination_unit_manager_;
-  CoordinationUnitIntrospectorImpl introspector_;
   std::unique_ptr<ukm::MojoUkmRecorder> ukm_recorder_;
 
   // WeakPtrFactory members should always come last so WeakPtrs are destructed
