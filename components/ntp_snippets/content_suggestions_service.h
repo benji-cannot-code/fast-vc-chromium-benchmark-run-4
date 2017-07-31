9d804bc25e0d9eaa9ff36d2a113568923e82ac0e
gaschler <gaschler@chromium.org>
2017-07-31 08:55:50 +0000

    [NTP] Add ContextualSuggestionsSource
    The new class ContextualSuggestionsSource can retrieve contextual
    suggestions for a given URL.
    For a given contextual suggestion, it can also fetch an image.
    Calls are asynchronous.
    A ContextualSuggestionsSource will only be created if flag "contextual-suggestions-carousel"
    is on.
    It is owned by the ContentSuggestionsService.
    
    Bug: 749988
    Change-Id: Idce653d611d558f50c1c18bf419fc3c1cf98df49
    Reviewed-on: https://chromium-review.googlesource.com/583091
    Commit-Queue: Andre Gaschler <gaschler@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490731}

--- components/ntp_snippets/content_suggestions_service.h
+++ components/ntp_snippets/content_suggestions_service.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/ntp_snippets/category_rankers/category_ranker.h"
 #include "components/ntp_snippets/category_status.h"
 #include "components/ntp_snippets/content_suggestions_provider.h"
+#include "components/ntp_snippets/contextual_suggestions_source.h"
 #include "components/ntp_snippets/remote/remote_suggestions_scheduler.h"
 #include "components/ntp_snippets/user_classifier.h"
 #include "components/signin/core/browser/signin_manager.h"
@@ -245,6 +246,12 @@ class ContentSuggestionsService : public KeyedService,
   // child account.
   bool AreRemoteSuggestionsManagedByCustodian() const;
 
+  void set_contextual_suggestions_source(
+      std::unique_ptr<ContextualSuggestionsSource>
+          contextual_suggestions_source) {
+    contextual_suggestions_source_ = std::move(contextual_suggestions_source);
+  }
+
   // The reference to the RemoteSuggestionsProvider provider should
   // only be set by the factory and only used for debugging.
   // TODO(jkrcal) The way we deal with the circular dependency feels wrong.
@@ -418,6 +425,9 @@ class ContentSuggestionsService : public KeyedService,
 
   // Provides order for categories.
   std::unique_ptr<CategoryRanker> category_ranker_;
+
+  // Fetches and caches contextual suggestions for a given URL.
+  std::unique_ptr<ContextualSuggestionsSource> contextual_suggestions_source_;
 
   DISALLOW_COPY_AND_ASSIGN(ContentSuggestionsService);
 };
