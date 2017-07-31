d82b621dcb6f63fc4445ecf78150e731430e87bb
Francois Doray <fdoray@chromium.org>
2017-07-31 22:10:15 +0000

    Remove usage of ScopedAsyncTaskScheduler from automatic_reboot_manager_unittest.cc.
    ScopedAsyncTaskScheduler is deprecated.
    
    Bug: 708584
    Change-Id: Id7fe750e4e3201ed8ccda267cf296ed83dffd96b
    Reviewed-on: https://chromium-review.googlesource.com/592244
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Francois Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490805}

--- chrome/browser/chromeos/system/automatic_reboot_manager_unittest.cc
+++ chrome/browser/chromeos/system/automatic_reboot_manager_unittest.cc
@@ -15,10 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/task_scheduler/task_scheduler.h"
-#include "base/test/scoped_async_task_scheduler.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/test/simple_test_tick_clock.h"
 #include "base/test/test_mock_time_task_runner.h"
-#include "base/threading/sequenced_worker_pool.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/tick_clock.h"
 #include "base/values.h"
@@ -201,8 +200,8 @@ class AutomaticRebootManagerBasicTest : public testing::Test {
 
   bool reboot_after_update_ = false;
 
-  base::ThreadTaskRunnerHandle ui_thread_task_runner_handle_;
-  base::test::ScopedAsyncTaskScheduler scoped_async_task_scheduler_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
+  base::ScopedClosureRunner reset_main_thread_task_runner_;
 
   TestingPrefServiceSimple local_state_;
   MockUserManager* mock_user_manager_;  // Not owned.
@@ -304,7 +303,8 @@ void MockAutomaticRebootManagerObserver::StopObserving() {
 
 AutomaticRebootManagerBasicTest::AutomaticRebootManagerBasicTest()
     : task_runner_(new TestAutomaticRebootManagerTaskRunner),
-      ui_thread_task_runner_handle_(task_runner_),
+      reset_main_thread_task_runner_(
+          base::ThreadTaskRunnerHandle::OverrideForTesting(task_runner_)),
       mock_user_manager_(new MockUserManager),
       user_manager_enabler_(mock_user_manager_) {}
 
