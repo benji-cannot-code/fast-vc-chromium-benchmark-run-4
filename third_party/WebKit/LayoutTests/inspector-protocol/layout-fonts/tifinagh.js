bd3622a11306d69ab797f7cdaa6f00605b8ead31
Dmitry Gozman <dgozman@chromium.org>
2017-07-31 20:50:05 +0000

    [DevTools] Migrate inspector-protocol/layout-fonts tests to new test harness
    These are the last ones, and we can cleanup now!
    
    BUG=734762
    
    Change-Id: I77457c6b110d081bc9d8247efffe9b65cdeccd3d
    Reviewed-on: https://chromium-review.googlesource.com/592406
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490795}

---
+++ third_party/WebKit/LayoutTests/inspector-protocol/layout-fonts/tifinagh.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  var page = await testRunner.createPage();
+  await page.loadHTML(`
+    <html>
+    <meta charset="UTF-8">
+    <div class="test">
+        <!-- Universal declaration of Human Rights written in Central
+             Atlas Tamazight using Tifinagh script. -->
+        <div id="tifinagh_text">ⵉⵎⴷⴰⵏⴻⵏ, ⴰⴽⴽⴻⵏ ⵎⴰ ⵍⵍⴰⵏ ⵜⵜⵍⴰⵍⴻⵏ ⴷ ⵉⵍⴻⵍⵍⵉⵢⴻⵏ ⵎⵙⴰⵡⴰⵏ ⴷⵉ ⵍⵃⵡⴻⵕⵎⴰ ⴷ ⵢⵉⵣⴻⵔⴼⴰⵏ-ⵖⵓⵔ ⵙⴻⵏ ⵜⴰⵎⵙⴰⴽⵡⵉⵜ ⴷ ⵍⴰⵇⵓⴻⵍ ⵓ ⵢⴻⵙⵙⴻⴼⴽ ⴰⴷ-ⵜⵉⵍⵉ ⵜⴻⴳⵎⴰⵜⵜ ⴳⴰⵔ ⴰⵙⴻⵏ</div>
+    </html>
+  `);
+  var session = await page.createSession();
+
+  var helper = await testRunner.loadScript('./resources/layout-font-test.js');
+  await helper(testRunner, session);
+  testRunner.completeTest();
+})
