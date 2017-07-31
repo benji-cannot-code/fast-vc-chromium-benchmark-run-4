b91c2f02304e9a865764b34241feb990bf34b7fc
Scott Graham <scottmg@chromium.org>
2017-07-31 23:08:42 +0000

    fuchsia: Stub a few functions to get Debug linking
    AmountOfAvailablePhysicalMemoryImpl isn't currently implementable as
    there's no system call for it.
    
    The GPU collection ones, there's no GPU access set up yet at all.
    
    Bug: 706592, 707031, 743296
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ib161bf45120ff13127ad198d57017ffc6d7c9a79
    Reviewed-on: https://chromium-review.googlesource.com/592642
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490815}

---
+++ gpu/config/gpu_info_collector_fuchsia.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "gpu/config/gpu_info_collector.h"
+
+#include "base/logging.h"
+
+namespace gpu {
+
+CollectInfoResult CollectBasicGraphicsInfo(GPUInfo* gpu_info) {
+  NOTIMPLEMENTED();  // TODO(fuchsia): https://crbug.com/707031 Some day.
+  return kCollectInfoNonFatalFailure;
+}
+
+CollectInfoResult CollectDriverInfoGL(GPUInfo* gpu_info) {
+  NOTIMPLEMENTED();  // TODO(fuchsia): https://crbug.com/707031 Some day.
+  return kCollectInfoNonFatalFailure;
+}
+
+}  // namespace gpu
