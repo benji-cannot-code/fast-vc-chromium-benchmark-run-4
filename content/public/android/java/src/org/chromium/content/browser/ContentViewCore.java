8eaa1208e0cfb79a5c04a70f1ef0644eedda3147
Jinsuk Kim <jinsukkim@chromium.org>
2017-07-31 02:35:59 +0000

    Instantiate WebContentsAX only when WebContents is ready
    The reported crash indicates that WebContents object is null
    when WebContentsAccessibility object gets instantiated.
    This may happen if AccessibilityNodeProvider is requested
    after WebContents/CVC get destroyed. In which case we should
    not proceed to creating one but return null instead.
    
    BUG=749761
    
    Change-Id: If74945442fd6d05f2f2090c6e0a6aca375fb04f2
    Reviewed-on: https://chromium-review.googlesource.com/590887
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490698}

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -1931,6 +1931,7 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Displa
             }
             mWebContentsAccessibility.enable();
         } else if (mNativeAccessibilityAllowed) {
+            if (mWebContents == null) return null;
             mWebContentsAccessibility = WebContentsAccessibility.create(mContext, mContainerView,
                     mWebContents, mRenderCoordinates, mShouldSetAccessibilityFocusOnPageLoad);
             mWebContentsAccessibility.enable();
