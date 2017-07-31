7900707e9ceaea395c03e0f3bd7003f60c7bece0
Samuel Huang <huangs@chromium.org>
2017-07-31 18:09:15 +0000

    [Clank] Move nativeGetBrowserUserAgent() out of ChromeApplication.
    This is part of the effort to remove ChromeApplication. Now
    nativeGetBrowserUserAgent() is moved to a new class
    org.chromium.chrome.browser.content.ContentUtils.
    
    Bug: 698015
    Change-Id: I22d848f484d81bb13bc8ba065ff9bda2ed02d16d
    Reviewed-on: https://chromium-review.googlesource.com/585630
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490784}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/content/ContentUtils.java
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.content;
+
+/**
+ * A utility class to expose content functionality.
+ */
+public class ContentUtils {
+    /**
+     * @return The user agent string of Chrome.
+     */
+    public static String getBrowserUserAgent() {
+        return nativeGetBrowserUserAgent();
+    }
+
+    private static native String nativeGetBrowserUserAgent();
+}
