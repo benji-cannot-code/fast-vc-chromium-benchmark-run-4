94544235c9dd076279e40e17d6100f4e16ee068d
Christopher Lam <calamity@chromium.org>
2017-08-01 18:11:04 +0000

    [MD Bookmarks] Left and right arrows focus inside item.
    This CL makes the right arrow on a bookmark item focus the menu and the
    left arrow go back to the item.
    
    Bug: 692302
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ida54f5e2c0995997b22489bda02bc37f6a7e1d2e
    Reviewed-on: https://chromium-review.googlesource.com/584207
    Commit-Queue: calamity <calamity@chromium.org>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491055}

--- chrome/browser/resources/md_bookmarks/item.js
+++ chrome/browser/resources/md_bookmarks/item.js
@@ -46,6 +46,7 @@ Polymer({
     'click': 'onClick_',
     'dblclick': 'onDblClick_',
     'contextmenu': 'onContextMenu_',
+    'keydown': 'onKeydown_',
     'auxclick': 'onMiddleClick_',
     'mousedown': 'cancelMiddleMouseBehavior_',
     'mouseup': 'cancelMiddleMouseBehavior_',
@@ -144,6 +145,17 @@ Polymer({
     }
     e.stopPropagation();
     e.preventDefault();
+  },
+
+  /**
+   * @private
+   * @param {KeyboardEvent} e
+   */
+  onKeydown_: function(e) {
+    if (e.key == 'ArrowLeft')
+      this.focus();
+    else if (e.key == 'ArrowRight')
+      this.$.menuButton.focus();
   },
 
   /**
