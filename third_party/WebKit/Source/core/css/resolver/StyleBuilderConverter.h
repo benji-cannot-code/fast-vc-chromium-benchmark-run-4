93a87342a91ebac68cb169e610fb68e9c042675a
Dominik Röttsches <drott@chromium.org>
2017-08-01 12:22:45 +0000

    Allow parsing of font-stretch from percentage into CSSPrimitiveValue
    At the same time change computed style to a number type according to CSS
    Fonts Level 4 [1].
    
    [1] https://drafts.csswg.org/css-fonts-4/#font-stretch-prop
    
    Bug: 739139
    Change-Id: I30db184559e7f047efcca999ae1b63aaa18b5eab
    Reviewed-on: https://chromium-review.googlesource.com/574590
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Bugs Nash <bugsnash@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490949}

--- third_party/WebKit/Source/core/css/resolver/StyleBuilderConverter.h
+++ third_party/WebKit/Source/core/css/resolver/StyleBuilderConverter.h
@@ -59,6 +59,7 @@ class StyleBuilderConverterBase {
   STATIC_ONLY(StyleBuilderConverterBase);
 
  public:
+  static FontSelectionValue ConvertFontStretch(const CSSValue&);
   static FontSelectionValue ConvertFontWeight(const CSSValue&,
                                               FontSelectionValue);
   static FontDescription::FontVariantCaps ConvertFontVariantCaps(
@@ -107,6 +108,9 @@ class StyleBuilderConverter {
   static FontDescription::Size ConvertFontSize(StyleResolverState&,
                                                const CSSValue&);
   static float ConvertFontSizeAdjust(StyleResolverState&, const CSSValue&);
+
+  static FontSelectionValue ConvertFontStretch(StyleResolverState&,
+                                               const CSSValue&);
   static FontSelectionValue ConvertFontWeight(StyleResolverState&,
                                               const CSSValue&);
 
