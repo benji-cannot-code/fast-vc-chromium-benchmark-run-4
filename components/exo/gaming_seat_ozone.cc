8ad5f36df039f6474c21b4f02128def3a75d05d0
yoshiki iguchi <yoshiki@chromium.org>
2017-08-01 11:27:30 +0000

    Prevent double-add (or -remove) of observer in gamepad_privider_ozone
    Double adding and removing of an observer hits DCHECK. This patch prevents that.
    Bug: none
    Change-Id: I289cadc8356783abdfac3fee6d2e3a57d620cc21
    
    Change-Id: I384ea7ee20a98ac03f1661f48cc599d6f8acb8c4
    Reviewed-on: https://chromium-review.googlesource.com/583958
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490942}

--- components/exo/gaming_seat_ozone.cc
+++ components/exo/gaming_seat_ozone.cc
@@ -24,7 +24,8 @@ GamingSeat::GamingSeat(GamingSeatDelegate* delegate,
 }
 
 GamingSeat::~GamingSeat() {
-  ui::GamepadProviderOzone::GetInstance()->RemoveGamepadObserver(this);
+  if (focused_)
+    ui::GamepadProviderOzone::GetInstance()->RemoveGamepadObserver(this);
   delegate_->OnGamingSeatDestroying(this);
   // Disconnect all the gamepads.
   for (auto& entry : gamepads_)
@@ -49,11 +50,14 @@ void GamingSeat::OnWindowFocused(aura::Window* gained_focus,
   }
 
   bool focused = target && delegate_->CanAcceptGamepadEventsForSurface(target);
-  if (focused) {
-    ui::GamepadProviderOzone::GetInstance()->AddGamepadObserver(this);
-    OnGamepadDevicesUpdated();
-  } else {
-    ui::GamepadProviderOzone::GetInstance()->RemoveGamepadObserver(this);
+  if (focused_ != focused) {
+    focused_ = focused;
+    if (focused) {
+      ui::GamepadProviderOzone::GetInstance()->AddGamepadObserver(this);
+      OnGamepadDevicesUpdated();
+    } else {
+      ui::GamepadProviderOzone::GetInstance()->RemoveGamepadObserver(this);
+    }
   }
 }
 
