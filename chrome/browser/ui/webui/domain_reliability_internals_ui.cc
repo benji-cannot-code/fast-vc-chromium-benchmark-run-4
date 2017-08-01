8eda68bc4d422e0841ddecc28a2ded448eaa3762
Dave Schuyler <dschuyler@chromium.org>
2017-08-01 23:00:30 +0000

    [i18n] remove i18n-content from domain-reliability-internals
    This CL removes the i18n-content on the <title> (afaik it was not being
    set so I removed it rather than converting to $i18n{title}). Also removes
    strings.js and i18n_template.js which should be unneeded at this point.
    
    Bug: 677338
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Iab29f07c23d200ed824f4207199853d982eefff2
    Reviewed-on: https://chromium-review.googlesource.com/585769
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Dave Schuyler <dschuyler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491143}

--- chrome/browser/ui/webui/domain_reliability_internals_ui.cc
+++ chrome/browser/ui/webui/domain_reliability_internals_ui.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/domain_reliability_internals_ui.h"
 
+#include <string>
+#include <unordered_set>
+
 #include "chrome/browser/domain_reliability/service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/url_constants.h"
@@ -21,7 +24,6 @@ DomainReliabilityInternalsUI::DomainReliabilityInternalsUI(
     : content::WebUIController(web_ui) {
   content::WebUIDataSource* html_source = content::WebUIDataSource::Create(
       chrome::kChromeUIDomainReliabilityInternalsHost);
-  html_source->SetJsonPath("strings.js");
   html_source->AddResourcePath("domain_reliability_internals.css",
       IDR_DOMAIN_RELIABILITY_INTERNALS_CSS);
   html_source->AddResourcePath("domain_reliability_internals.js",
