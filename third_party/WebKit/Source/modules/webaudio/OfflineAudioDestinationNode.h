3874e6c55a69f18de460c855dadc58d2b7801435
Raymond Toy <rtoy@chromium.org>
2017-08-01 16:22:11 +0000

    Lazily allocate AudioBuffer at startRendering()
    When constructing an OfflineAudioContext, defer construction of the
    AudioBuffer for the result until startRendering is called.  The
    construction of the AudioBuffer can block the main thread for
    significant amounts of time because the AudioBuffer needs to be zeroed
    out.
    
    startRendering() will now block the main thread to allocate the
    AudioBuffer, but this is a better place for that and a first step in
    speeding up AudioBuffer allocation.
    
    Bug: 736561
    Test: none
    Change-Id: I72beedac6b8a0445f51f0827a4f46e36c557115c
    Reviewed-on: https://chromium-review.googlesource.com/572043
    Commit-Queue: Raymond Toy <rtoy@chromium.org>
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491006}

--- third_party/WebKit/Source/modules/webaudio/OfflineAudioDestinationNode.h
+++ third_party/WebKit/Source/modules/webaudio/OfflineAudioDestinationNode.h
@@ -45,7 +45,9 @@ class OfflineAudioDestinationHandler final : public AudioDestinationHandler {
  public:
   static PassRefPtr<OfflineAudioDestinationHandler> Create(
       AudioNode&,
-      AudioBuffer* render_target);
+      unsigned number_of_channels,
+      size_t frames_to_process,
+      float sample_rate);
   ~OfflineAudioDestinationHandler() override;
 
   // AudioHandler
@@ -68,7 +70,7 @@ class OfflineAudioDestinationHandler final : public AudioDestinationHandler {
   // called.
   size_t CallbackBufferSize() const override;
 
-  double SampleRate() const override { return render_target_->sampleRate(); }
+  double SampleRate() const override { return sample_rate_; }
   int FramesPerBuffer() const override {
     NOTREACHED();
     return 0;
@@ -78,10 +80,20 @@ class OfflineAudioDestinationHandler final : public AudioDestinationHandler {
     return AudioUtilities::kRenderQuantumFrames;
   }
 
-  void InitializeOfflineRenderThread();
+  // This is called when rendering of the offline context is started
+  // which will save the rendered audio data in |render_target|.  This
+  // allows creation of the AudioBuffer when startRendering is called
+  // instead of when the OfflineAudioContext is created.
+  void InitializeOfflineRenderThread(AudioBuffer* render_target);
+  AudioBuffer* RenderTarget() const { return render_target_.Get(); }
+
+  unsigned NumberOfChannels() const { return number_of_channels_; }
 
  private:
-  OfflineAudioDestinationHandler(AudioNode&, AudioBuffer* render_target);
+  OfflineAudioDestinationHandler(AudioNode&,
+                                 unsigned number_of_channels,
+                                 size_t frames_to_process,
+                                 float sample_rate);
 
   // Set up the rendering and start. After setting the context up, it will
   // eventually call |doOfflineRendering|.
@@ -138,15 +150,23 @@ class OfflineAudioDestinationHandler final : public AudioDestinationHandler {
   // 'created' and 'suspended'. If this flag is false and the current state
   // is 'suspended', it means the context is created and have not started yet.
   bool is_rendering_started_;
+
+  unsigned number_of_channels_;
+  float sample_rate_;
 };
 
 class OfflineAudioDestinationNode final : public AudioDestinationNode {
  public:
   static OfflineAudioDestinationNode* Create(BaseAudioContext*,
-                                             AudioBuffer* render_target);
+                                             unsigned number_of_channels,
+                                             size_t frames_to_process,
+                                             float sample_rate);
 
  private:
-  OfflineAudioDestinationNode(BaseAudioContext&, AudioBuffer* render_target);
+  OfflineAudioDestinationNode(BaseAudioContext&,
+                              unsigned number_of_channels,
+                              size_t frames_to_process,
+                              float sample_rate);
 };
 
 }  // namespace blink
