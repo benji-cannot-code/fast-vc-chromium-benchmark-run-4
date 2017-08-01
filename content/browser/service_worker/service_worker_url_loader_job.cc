a0507563ef941abe182273aaacd9df2bbce3b80c
Matt Falkenhagen <falken@chromium.org>
2017-08-01 10:36:25 +0000

    service worker: Add a test for ServiceWorkerURLLoaderJob.
    Also teach it to handle the case where there is no response body.
    
    This test class will evolve as more cases are added. Currently
    it just tests the simple case where a service worker handles
    the request, with no response body.
    
    Bug: 748416
    Change-Id: Ifda79696eff766e725e6ee28fc27ca3d9ef9cf84
    Reviewed-on: https://chromium-review.googlesource.com/590836
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490935}

--- content/browser/service_worker/service_worker_url_loader_job.cc
+++ content/browser/service_worker/service_worker_url_loader_job.cc
@@ -32,7 +32,6 @@ ServiceWorkerURLLoaderJob::ServiceWorkerURLLoaderJob(
       blob_client_binding_(this),
       binding_(this),
       weak_factory_(this) {
-  DCHECK(ServiceWorkerUtils::IsServicificationEnabled());
 }
 
 ServiceWorkerURLLoaderJob::~ServiceWorkerURLLoaderJob() {}
@@ -278,9 +277,7 @@ void ServiceWorkerURLLoaderJob::StartResponse(
   SaveResponseHeaders(response.status_code, response.status_text,
                       response.headers);
 
-  // Ideally, we would always get a data pipe fom SWFetchDispatcher and use
-  // this case. See:
-  // https://docs.google.com/a/google.com/document/d/1_ROmusFvd8ATwIZa29-P6Ls5yyLjfld0KvKchVfA84Y/edit?usp=drive_web
+  // Handle a stream response body.
   if (!body_as_stream.is_null() && body_as_stream->stream.is_valid()) {
     CommitResponseHeaders();
     url_loader_client_->OnStartLoadingResponseBody(
@@ -289,6 +286,10 @@ void ServiceWorkerURLLoaderJob::StartResponse(
     return;
   }
 
+  // Handle a blob response body. Ideally we'd just get a data pipe from
+  // SWFetchDispatcher, and this could be treated the same as a stream response.
+  // See:
+  // https://docs.google.com/a/google.com/document/d/1_ROmusFvd8ATwIZa29-P6Ls5yyLjfld0KvKchVfA84Y/edit?usp=drive_web
   if (!response.blob_uuid.empty() && blob_storage_context_) {
     std::unique_ptr<storage::BlobDataHandle> blob_data_handle =
         blob_storage_context_->GetBlobDataFromUUID(response.blob_uuid);
@@ -298,7 +299,12 @@ void ServiceWorkerURLLoaderJob::StartResponse(
     BlobURLLoaderFactory::CreateLoaderAndStart(
         std::move(request), resource_request_, std::move(client),
         std::move(blob_data_handle), nullptr /* file_system_context */);
+    return;
   }
+
+  // The response has no body.
+  CommitResponseHeaders();
+  CommitCompleted(net::OK);
 }
 
 // URLLoader implementation----------------------------------------
