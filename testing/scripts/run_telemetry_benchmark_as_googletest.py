8e0787ead840d51b6069a870fb18ef4819867e2a
Ashley Enstad <ashleymarie@chromium.org>
2017-08-01 14:18:45 +0000

    Adding --output-format=json-test-results for all Telemetry benchmarks
    Now that all the benchmarks are set not to upload results until their
    individual flags are flipped, we can change all the benchmarks to output
    json of the json-test-results format instead of the old simplified json
    format.
    
    BUG=chromium:748638
    
    Change-Id: I10ad5029f29052aec44a0476fe17db74215f4786
    Reviewed-on: https://chromium-review.googlesource.com/589820
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Ashley Enstad <ashleymarie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490988}

--- testing/scripts/run_telemetry_benchmark_as_googletest.py
+++ testing/scripts/run_telemetry_benchmark_as_googletest.py
@@ -67,14 +67,13 @@ def main():
     failures = []
     chartjson_results_present = '--output-format=chartjson' in rest_args
     chartresults = None
-    json_test_results_present = '--output-format=json-test-results' in rest_args
     json_test_results = None
 
     results = None
     try:
       rc = common.run_command([sys.executable] + rest_args + [
         '--output-dir', tempfile_dir,
-        '--output-format=json'
+        '--output-format=json-test-results',
       ], env=env)
             # If we have also output chartjson read it in and return it.
       # results-chart.json is the file name output by telemetry when the
@@ -85,6 +84,8 @@ def main():
           chartresults = json.load(f)
       # We need to get chartjson results first as this may be a disabled
       # benchmark that was run
+      # TODO(ashleymarie): potentially remove the following if it's dead code
+      # http://crbug.com/748638
       if (not chartjson_results_present or
          (chartjson_results_present and chartresults.get('enabled', True))):
         tempfile_name = os.path.join(tempfile_dir, 'results.json')
@@ -100,10 +101,9 @@ def main():
             failures.append(name)
         valid = bool(rc == 0 or failures)
 
-      if json_test_results_present:
-        tempfile_name = os.path.join(tempfile_dir, 'test-results.json')
-        with open(tempfile_name) as f:
-          json_test_results = json.load(f)
+      tempfile_name = os.path.join(tempfile_dir, 'test-results.json')
+      with open(tempfile_name) as f:
+        json_test_results = json.load(f)
 
     except Exception:
       traceback.print_exc()
@@ -123,12 +123,6 @@ def main():
       chartjson_output_file = \
         open(args.isolated_script_test_chartjson_output, 'w')
       json.dump(chartresults, chartjson_output_file)
-
-    if not json_test_results_present:
-      json_test_results = {
-          'valid': valid,
-          'failures': failures
-      }
 
     json.dump(json_test_results, args.isolated_script_test_output)
     return rc
