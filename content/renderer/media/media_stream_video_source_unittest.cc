05c6da1e69c00bf728d2f9e1a223fd46fc9bca9a
Guido Urdaneta <guidou@chromium.org>
2017-08-01 07:38:52 +0000

    Enable rotation heuristic for MediaStreams for all platforms.
    It was originally enabled only on Android, but the bug the heuristic
    fixes has been reported on Microsoft Surface devices running Windows.
    
    BUG=746000
    
    Change-Id: Ibb4214cc6c461012679c84e33a977ccf2fe1ff01
    Reviewed-on: https://chromium-review.googlesource.com/594407
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490900}

--- content/renderer/media/media_stream_video_source_unittest.cc
+++ content/renderer/media/media_stream_video_source_unittest.cc
@@ -343,7 +343,6 @@ TEST_F(MediaStreamVideoSourceTest, SourceChangeFrameSize) {
   sink.DisconnectFromTrack();
 }
 
-#if defined(OS_ANDROID)
 TEST_F(MediaStreamVideoSourceTest, RotatedSource) {
   // Expect the source to start capture with the supported resolution.
   // Disable frame-rate adjustment in spec-compliant mode to ensure no frames
@@ -370,7 +369,6 @@ TEST_F(MediaStreamVideoSourceTest, RotatedSource) {
 
   sink.DisconnectFromTrack();
 }
-#endif
 
 // Test that a source producing no frames change the source ReadyState to muted.
 // that in a reasonable time frame the muted state turns to false.
