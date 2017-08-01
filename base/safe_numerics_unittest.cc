a4fd889eb02a048edf8b5a300d7e51ab97a3fcbe
Justin Schuh <jschuh@chromium.org>
2017-08-01 19:46:43 +0000

    Fix MSVC release build after r490852
    Disable integer overflow warnings in integer overflow tests.
    
    TBR=thakis@chromium.org
    
    Bug: 751198
    Change-Id: Ia5680199a51f0ecb951744838e624f9d6f1d7573
    Reviewed-on: https://chromium-review.googlesource.com/596511
    Reviewed-by: Justin Schuh <jschuh@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Justin Schuh <jschuh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491085}

--- base/safe_numerics_unittest.cc
+++ base/safe_numerics_unittest.cc
@@ -10,6 +10,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <type_traits>
 
 #include "base/compiler_specific.h"
+
+// WARNING: This block must come before the base/numerics headers are included.
+// These tests deliberately cause arithmetic boundary errors. If the compiler is
+// aggressive enough, it can const detect these errors, so we disable warnings.
+#if defined(OS_WIN)
+#pragma warning(disable : 4756)  // Arithmetic overflow.
+#pragma warning(disable : 4293)  // Invalid shift.
+#endif
+
 #include "base/logging.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/numerics/safe_math.h"
@@ -25,13 +34,6 @@ namespace base {
 namespace internal {
 
 using std::numeric_limits;
-
-// These tests deliberately cause arithmetic boundary errors. If the compiler is
-// aggressive enough, it can const detect these errors, so we disable warnings.
-#if defined(OS_WIN)
-#pragma warning(disable : 4756)  // Arithmetic overflow.
-#pragma warning(disable : 4293)  // Invalid shift.
-#endif
 
 // This is a helper function for finding the maximum value in Src that can be
 // wholy represented as the destination floating-point type.
