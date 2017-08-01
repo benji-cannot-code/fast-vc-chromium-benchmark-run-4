4e1f76e1fc6dde8e5764138ab25355973ed46bd4
Jorge Lucangeli Obes <jorgelo@google.com>
2017-08-01 07:05:47 +0000

    Add TPM firmware update option to reset screen.
    Allow the user to request a TPM firmware update via the powerwash
    screen if applicable.
    
    BUG=chromium:742985
    TEST=None
    TBR=alemate
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ibb6316a388af8b3d7b4714cd87ef2d9e02369e9a
    Reviewed-on: https://chromium-review.googlesource.com/595162
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Commit-Queue: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490895}

--- chrome/browser/resources/chromeos/login/oobe_screen_reset.js
+++ chrome/browser/resources/chromeos/login/oobe_screen_reset.js
@@ -16,6 +16,9 @@ login.createScreen('ResetScreen', 'reset', function() {
   var USER_ACTION_RESET_CONFIRM_DISMISSED = 'reset-confirm-dismissed';
   var CONTEXT_KEY_ROLLBACK_AVAILABLE = 'rollback-available';
   var CONTEXT_KEY_ROLLBACK_CHECKED = 'rollback-checked';
+  var CONTEXT_KEY_TPM_FIRMWARE_UPDATE_AVAILABLE =
+      'tpm-firmware-update-available';
+  var CONTEXT_KEY_TPM_FIRMWARE_UPDATE_CHECKED = 'tpm-firmware-update-checked';
   var CONTEXT_KEY_IS_OFFICIAL_BUILD = 'is-official-build';
   var CONTEXT_KEY_IS_CONFIRMATIONAL_VIEW = 'is-confirmational-view';
   var CONTEXT_KEY_SCREEN_STATE = 'screen-state';
@@ -71,6 +74,7 @@ login.createScreen('ResetScreen', 'reset', function() {
           announceAccessibleMessage(
               loadTimeData.getString('resetRevertSpinnerMessage'));
         }
+        self.setTPMFirmwareUpdateView_();
       });
 
       this.context.addObserver(
@@ -87,6 +91,14 @@ login.createScreen('ResetScreen', 'reset', function() {
             self.setRollbackOptionView();
           });
       this.context.addObserver(
+          CONTEXT_KEY_TPM_FIRMWARE_UPDATE_CHECKED, function() {
+            self.setTPMFirmwareUpdateView_();
+          });
+      this.context.addObserver(
+          CONTEXT_KEY_TPM_FIRMWARE_UPDATE_AVAILABLE, function() {
+            self.setTPMFirmwareUpdateView_();
+          });
+      this.context.addObserver(
           CONTEXT_KEY_IS_CONFIRMATIONAL_VIEW, function(is_confirmational) {
             if (is_confirmational) {
               console.log(self.context.get(CONTEXT_KEY_SCREEN_STATE, 0));
@@ -99,6 +111,8 @@ login.createScreen('ResetScreen', 'reset', function() {
               $('overlay-reset').setAttribute('hidden', true);
             }
           });
+
+      $('oobe-reset-md').screen = this;
     },
 
     /**
@@ -271,6 +285,20 @@ login.createScreen('ResetScreen', 'reset', function() {
         this.ui_state = this.RESET_SCREEN_UI_STATE.POWERWASH_PROPOSAL;
       }
       this.setDialogView_();
+      this.setTPMFirmwareUpdateView_();
+    },
+
+    setTPMFirmwareUpdateView_: function() {
+      $('oobe-reset-md').tpmFirmwareUpdateAvailable_ =
+          this.ui_state == this.RESET_SCREEN_UI_STATE.POWERWASH_PROPOSAL &&
+          this.context.get(CONTEXT_KEY_TPM_FIRMWARE_UPDATE_AVAILABLE);
+      $('oobe-reset-md').tpmFirmwareUpdateChecked_ =
+          this.context.get(CONTEXT_KEY_TPM_FIRMWARE_UPDATE_CHECKED);
+    },
+
+    onTPMFirmwareUpdateChanged_: function(value) {
+      this.context.set(CONTEXT_KEY_TPM_FIRMWARE_UPDATE_CHECKED, value);
+      this.commitContextChanges();
     }
   };
 });
