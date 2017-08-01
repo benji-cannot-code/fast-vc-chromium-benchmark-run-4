d7ee34a1b465658d945c8ccbe293b3dfdada7264
Reilly Grant <reillyg@chromium.org>
2017-08-01 19:06:59 +0000

    Switch Budget API LayoutTests to the new Mojo JS bindings
    This change rewrites the Budget API layout tests to use the new Mojo
    JS bindings.
    
    Bug: 699569
    Change-Id: I4af6e3a78c16cf4ecc6aeedc5317eaf1021ca9eb
    Reviewed-on: https://chromium-review.googlesource.com/595234
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491073}

--- third_party/WebKit/LayoutTests/http/tests/budget/resources/instrumentation-service-worker.js
+++ third_party/WebKit/LayoutTests/http/tests/budget/resources/instrumentation-service-worker.js
@@ -1,4 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+importScripts('/gen/layout_test_data/mojo/public/js/mojo_bindings.js');
+importScripts('/gen/url/mojo/origin.mojom.js');
+importScripts('/gen/third_party/WebKit/public/platform/modules/budget_service/budget_service.mojom.js');
+importScripts('../budget-service-mock.js');
+
 // Allows a document to exercise the Budget API within a service worker by sending commands.
 
 self.addEventListener('message', function(workerEvent) {
@@ -10,6 +15,30 @@ self.addEventListener('message', function(workerEvent) {
       return;
     var options = event.data.options || {};
     switch (event.data.command) {
+
+      case 'mockAddBudget':
+        budgetServiceMock.addBudget(event.data.time, event.data.budgetAt);
+        port.postMessage({
+          command: event.data.command,
+          success: true
+        });
+        break;
+
+      case 'mockSetCost':
+        budgetServiceMock.setCost(event.data.operation, event.data.cost);
+        port.postMessage({
+          command: event.data.command,
+          success: true
+        });
+        break;
+
+      case 'mockSetReserveSuccess':
+        budgetServiceMock.setReserveSuccess(event.data.success);
+        port.postMessage({
+          command: event.data.command,
+          success: true
+        });
+        break;
 
       case 'getCost':
         navigator.budget.getCost('silent-push').then(function(cost) {
