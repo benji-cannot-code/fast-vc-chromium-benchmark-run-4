2c294877c7a15b3d0e7a79e035b627a7452e5f19
Francois Doray <fdoray@chromium.org>
2017-08-01 17:31:39 +0000

    Add PRESUBMIT against usage of deprecated WebThreads.
    Bug: 689520
    Change-Id: Ie8418bc9afe6f9a106f5fdd85647006722dab50f
    Reviewed-on: https://chromium-review.googlesource.com/595872
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Francois Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491041}

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -333,7 +333,7 @@ _BANNED_CPP_FUNCTIONS = (
       (),
     ),
     (
-      'BrowserThread::GetBlockingPool',
+      r'/(WebThread|BrowserThread)::GetBlockingPool',
       (
         'Use base/task_scheduler/post_task.h instead of the blocking pool. See',
         'mapping between both APIs in content/public/browser/browser_thread.h.',
@@ -343,7 +343,8 @@ _BANNED_CPP_FUNCTIONS = (
       (),
     ),
     (
-      r'/BrowserThread::(FILE|FILE_USER_BLOCKING|DB|PROCESS_LAUNCHER|CACHE)',
+      r'/(WebThread|BrowserThread)::'
+      r'(FILE|FILE_USER_BLOCKING|DB|PROCESS_LAUNCHER|CACHE)',
       (
         'The non-UI/IO BrowserThreads are deprecated, please migrate this',
         'code to TaskScheduler. See https://goo.gl/mDSxKl for details.',
