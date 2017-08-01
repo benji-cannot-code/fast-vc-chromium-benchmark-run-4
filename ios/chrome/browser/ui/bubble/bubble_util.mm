fb4442c7b724274c5d0d5d4f393c08209c29edf4
Helen Yang <helenlyang@google.com>
2017-08-01 22:33:07 +0000

    [ios] Remove locale overrides from bubble_util unit tests
    Previously, unit tests for bubble_util overrode the ICU default
    locale in order to test methods in LTR/RTL. This caused
    CardStackLayoutManagerTest to fail on bots. This CL fixes the issue
    by removing default locale overrides from bubble_util unit tests,
    and instead adds an |isRTL| parameter to the relevant util method
    to make it more testable.
    
    Bug: 750865
    Change-Id: Ia53f92c8a08636c8c56519306b9347e8b425a37f
    Reviewed-on: https://chromium-review.googlesource.com/596673
    Commit-Queue: Helen Yang <helenlyang@google.com>
    Reviewed-by: Rohit Rao (ping after 24h) <rohitrao@chromium.org>
    Reviewed-by: Gregory Chatzinoff <gchatz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491134}

--- ios/chrome/browser/ui/bubble/bubble_util.mm
+++ ios/chrome/browser/ui/bubble/bubble_util.mm
@@ -66,12 +66,13 @@ CGFloat OriginY(CGRect targetFrame,
 
 CGFloat MaxWidth(CGRect targetFrame,
                  BubbleAlignment alignment,
-                 CGFloat boundingWidth) {
+                 CGFloat boundingWidth,
+                 bool isRTL) {
   CGFloat anchorX = CGRectGetMidX(targetFrame);
   CGFloat maxWidth;
   switch (alignment) {
     case BubbleAlignmentLeading:
-      if (base::i18n::IsRTL()) {
+      if (isRTL) {
         // The bubble is aligned right, and can use space to the left of the
         // anchor point and within |kBubbleAlignmentOffset| from the right.
         maxWidth = anchorX + kBubbleAlignmentOffset;
@@ -87,7 +88,7 @@ CGFloat MaxWidth(CGRect targetFrame,
       maxWidth = MIN(anchorX, boundingWidth - anchorX) * 2.0f;
       break;
     case BubbleAlignmentTrailing:
-      if (base::i18n::IsRTL()) {
+      if (isRTL) {
         // The bubble is aligned left, and can use space to the right of the
         // anchor point and within |kBubbleAlignmentOffset| from the left.
         maxWidth = boundingWidth - anchorX + kBubbleAlignmentOffset;
@@ -102,6 +103,13 @@ CGFloat MaxWidth(CGRect targetFrame,
       break;
   }
   return maxWidth;
+}
+
+CGFloat MaxWidth(CGRect targetFrame,
+                 BubbleAlignment alignment,
+                 CGFloat boundingWidth) {
+  bool isRTL = base::i18n::IsRTL();
+  return MaxWidth(targetFrame, alignment, boundingWidth, isRTL);
 }
 
 }  // namespace bubble_util
