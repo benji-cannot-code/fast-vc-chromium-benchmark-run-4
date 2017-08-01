a43c0e90b48a3eff5a4d50ad0a75e46a657800b7
Cooper Knaak <cooperknaak@google.com>
2017-08-01 23:44:56 +0000

    [iOS] Add ARC Guard to bubble_util.mm
    Add ARC guard to bubble_util.mm so it can only be compiled with
    Automatic Reference Counting enabled. Although it currently only
    contains C++ symbols, it is convention to include the ARC guard on all
    new files. If in the future Objective-C symbols were added to the file,
    not having the ARC guard could cause memory leaks.
    
    R=edchin@chromium.org, gchatz@chromium.org
    
    Bug: none
    Change-Id: I0b72bd93a904f04034ca85f9f21b969f9a358e07
    Reviewed-on: https://chromium-review.googlesource.com/596587
    Commit-Queue: Cooper Knaak <cooperknaak@google.com>
    Reviewed-by: Ed Chin <edchin@chromium.org>
    Reviewed-by: Gregory Chatzinoff <gchatz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491155}

--- ios/chrome/browser/ui/bubble/bubble_util.mm
+++ ios/chrome/browser/ui/bubble/bubble_util.mm
@@ -9,6 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #import "ios/chrome/browser/ui/bubble/bubble_view.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace bubble_util {
 
 // Calculate the coordinates of the point of the speech bubble's triangle based
