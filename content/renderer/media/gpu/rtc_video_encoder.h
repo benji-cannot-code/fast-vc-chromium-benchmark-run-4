b80bcb2dde75c7333ea0cbd0473210d13c7605ec
magjed <magjed@chromium.org>
2017-08-02 04:56:04 -0700

    Reland of RTCVideoEncoder: Report H264 profile information to WebRTC (patchset #1 id:1 of https://codereview.chromium.org/2973253002/ )
    Reason for revert:
    Update test to still use HW version of H264.
    
    Original issue's description:
    > Revert of TCVideoEncoder: Report H264 profile information to WebRTC (patchset #1 id:190001 of https://codereview.chromium.org/2548443002/ )
    >
    > Reason for revert:
    > Reverting this since it's causing multiple perf regression on mac, looks like HW encode/decode might get disabled.
    >
    > Original issue's description:
    > > Reland of RTCVideoEncoder: Report H264 profile information to WebRTC (patchset #1 id:1 of https://codereview.chromium.org/2521923002/ )
    > >
    > > Reason for revert:
    > > Try again.
    > >
    > > Original issue's description:
    > > > Revert of RTCVideoEncoder: Report H264 profile information to WebRTC (patchset #3 id:60001 of https://codereview.chromium.org/2499973002/ )
    > > >
    > > > Reason for revert:
    > > > Causes these tests to fail on chromium.webrtc bots for Win and Mac:
    > > > WebRtcPerfBrowserTest.MANUAL_RunsAudioVideoCall60SecsAndLogsInternalMetricsH264
    > > > WebRtcVideoQualityBrowserTests/WebRtcVideoQualityBrowserTest.MANUAL_TestVideoQualityH264
    > > > https://build.chromium.org/p/chromium.webrtc/builders/Win8%20Tester/builds/30367
    > > > https://build.chromium.org/p/chromium.webrtc/builders/Mac%20Tester/builds/62661
    > > >
    > > > Original issue's description:
    > > > > RTCVideoEncoder: Report H264 profile information to WebRTC
    > > > >
    > > > > This CL updates RTCVideoEncoderFactory to report cricket::VideoCodecs
    > > > > instead of WebRtcVideoEncoderFactory::VideoCodecs. The H264 profile
    > > > > information is added to the cricket::VideoCodec so that WebRTC receives
    > > > > this information. Also, the mapping between media::VideoCodecProfiles
    > > > > and cricket::VideoCodecs is cached so that we can send the
    > > > > media::VideoCodecProfile to RTCVideoEncoder instead of having to deal
    > > > > with webrtc::VideoCodecType.
    > > > >
    > > > > BUG=webrtc:6337
    > > > >
    > > > > Committed: https://crrev.com/510eddede44cb4b67c8f17fdd68cefb780a668c5
    > > > > Cr-Commit-Position: refs/heads/master@{#433508}
    > > >
    > > > TBR=emircan@chromium.org,posciak@chromium.org
    > > > # Skipping CQ checks because original CL landed less than 1 days ago.
    > > > NOPRESUBMIT=true
    > > > NOTREECHECKS=true
    > > > NOTRY=true
    > > > BUG=webrtc:6337
    > > >
    > > > Committed: https://crrev.com/c2564bc627cb950b124ac8e41bc5fd3187f7ad9c
    > > > Cr-Commit-Position: refs/heads/master@{#433828}
    > >
    > > TBR=emircan@chromium.org,posciak@chromium.org
    > > # Not skipping CQ checks because original CL landed more than 1 days ago.
    > > BUG=688541,735959
    > >
    > > Review-Url: https://codereview.chromium.org/2548443002
    > > Cr-Commit-Position: refs/heads/master@{#484874}
    > > Committed: https://chromium.googlesource.com/chromium/src/+/829b1d57525c3c6549d18a2c85a96527d59ea5e9
    >
    > TBR=emircan@chromium.org,magjed@chromium.org
    > # Skipping CQ checks because original CL landed less than 1 days ago.
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=688541,735959
    >
    > Review-Url: https://codereview.chromium.org/2973253002
    > Cr-Commit-Position: refs/heads/master@{#484960}
    > Committed: https://chromium.googlesource.com/chromium/src/+/df6e5a5c7e7c665603f9619930a1d7106b55160d
    
    TBR=emircan@chromium.org,niklase@chromium.org,phoglund@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=688541,735959
    
    Review-Url: https://codereview.chromium.org/2985263002
    Cr-Commit-Position: refs/heads/master@{#491338}

--- content/renderer/media/gpu/rtc_video_encoder.h
+++ content/renderer/media/gpu/rtc_video_encoder.h
@@ -42,7 +42,7 @@ namespace content {
 class CONTENT_EXPORT RTCVideoEncoder
     : NON_EXPORTED_BASE(public webrtc::VideoEncoder) {
  public:
-  RTCVideoEncoder(webrtc::VideoCodecType type,
+  RTCVideoEncoder(media::VideoCodecProfile profile,
                   media::GpuVideoAcceleratorFactories* gpu_factories);
   ~RTCVideoEncoder() override;
 
@@ -66,8 +66,7 @@ class CONTENT_EXPORT RTCVideoEncoder
   class Impl;
   friend class RTCVideoEncoder::Impl;
 
-  // The video codec type, as reported to WebRTC.
-  const webrtc::VideoCodecType video_codec_type_;
+  const media::VideoCodecProfile profile_;
 
   // Factory for creating VEAs, shared memory buffers, etc.
   media::GpuVideoAcceleratorFactories* gpu_factories_;
