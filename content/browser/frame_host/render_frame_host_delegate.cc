c1703720d039ce48f787696e6ae3ceaa1bf55b13
Conley Owens <cco3@chromium.org>
2017-08-02 01:56:52 +0000

    Perform browser-side geolocation permission checks
    Currently, there are no browser-side geolocation permission checks.
    This change adds a GeolocationService, which performs this check in
    the browser.
    
    BUG=426384
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I40925adc1e18ff8549a6d9dd94f998e013ebf3a1
    Reviewed-on: https://chromium-review.googlesource.com/558604
    Commit-Queue: Conley Owens <cco3@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491207}

--- content/browser/frame_host/render_frame_host_delegate.cc
+++ content/browser/frame_host/render_frame_host_delegate.cc
@@ -70,7 +70,7 @@ ui::AXMode RenderFrameHostDelegate::GetAccessibilityMode() const {
 RenderFrameHost* RenderFrameHostDelegate::GetGuestByInstanceID(
     RenderFrameHost* render_frame_host,
     int browser_plugin_instance_id) {
-  return NULL;
+  return nullptr;
 }
 
 device::GeolocationContext* RenderFrameHostDelegate::GetGeolocationContext() {
--- third_party/WebKit/Source/modules/geolocation/Geolocation.h
+++ third_party/WebKit/Source/modules/geolocation/Geolocation.h
@@ -43,9 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/bindings/ScriptWrappable.h"
 #include "platform/heap/Handle.h"
 
-#include "public/platform/modules/permissions/permission.mojom-blink.h"
-#include "public/platform/modules/permissions/permission_status.mojom-blink.h"
-
 namespace blink {
 
 class Document;
@@ -86,10 +83,6 @@ class MODULES_EXPORT Geolocation final
   // Removes all references to the watcher, it will not be updated again.
   void clearWatch(int watch_id);
 
-  bool IsAllowed() const {
-    return geolocation_permission_ == kPermissionAllowed;
-  }
-
   // Notifies this that a new position is available. Must never be called
   // before permission is granted by the user.
   void PositionChanged();
@@ -109,8 +102,6 @@ class MODULES_EXPORT Geolocation final
   void PageVisibilityChanged() override;
 
  private:
-  bool IsDenied() const { return geolocation_permission_ == kPermissionDenied; }
-
   explicit Geolocation(ExecutionContext*);
 
   typedef HeapVector<Member<GeoNotifier>> GeoNotifierVector;
@@ -151,9 +142,6 @@ class MODULES_EXPORT Geolocation final
   // and also  clears the watchers if the error is fatal.
   void HandleError(PositionError*);
 
-  // Requests permission to share positions with the page.
-  void RequestPermission();
-
   // Connects to the Geolocation mojo service and starts polling for updates.
   void StartUpdating(GeoNotifier*);
 
@@ -177,33 +165,15 @@ class MODULES_EXPORT Geolocation final
 
   void OnPositionUpdated(device::mojom::blink::GeopositionPtr);
 
-  // Processes the notifiers that were waiting for a permission decision. If
-  // granted then the notifier's timers are started. Otherwise, a fatal error
-  // is set on them.
-  void OnGeolocationPermissionUpdated(mojom::blink::PermissionStatus);
-
   void OnGeolocationConnectionError();
-  void OnPermissionConnectionError();
 
   GeoNotifierSet one_shots_;
   GeolocationWatchers watchers_;
-  GeoNotifierSet pending_for_permission_notifiers_;
   Member<Geoposition> last_position_;
 
-  // States of Geolocation permission as granted by the embedder. Unknown
-  // means that the embedder still has to be asked for the current permission
-  // level; Requested means that the user has yet to make a decision.
-  enum Permission {
-    kPermissionUnknown,
-    kPermissionRequested,
-    kPermissionAllowed,
-    kPermissionDenied
-  };
-
-  Permission geolocation_permission_;
   device::mojom::blink::GeolocationPtr geolocation_;
+  device::mojom::blink::GeolocationServicePtr geolocation_service_;
   bool enable_high_accuracy_ = false;
-  mojom::blink::PermissionServicePtr permission_service_;
 
   // Whether a GeoNotifier is waiting for a position update.
   bool updating_ = false;
