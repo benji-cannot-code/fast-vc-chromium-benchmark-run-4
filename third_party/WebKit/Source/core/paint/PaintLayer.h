b18e165aa2f17848166b4413b481884d35e30a09
Chris Harrelson <chrishtr@chromium.org>
2017-08-02 00:12:45 +0000

    Clipping and fragmentation code cleanup.
    1. Remove an unused method from PaintLayerClipper.
    2. Don't conditionally set cache slots which are ignored anyway for PaintLayerClipper
    use-cases that use GeometryMapper.
    3. Remove unnecessary pagination_clip parameter in PaintLayerFragment.
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I6cd7b8646ac68873a245c76813ec3723d48858c9
    Reviewed-on: https://chromium-review.googlesource.com/595109
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Walter Korman <wkorman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491162}

--- third_party/WebKit/Source/core/paint/PaintLayer.h
+++ third_party/WebKit/Source/core/paint/PaintLayer.h
@@ -1079,7 +1079,7 @@ class CORE_EXPORT PaintLayer : public DisplayItemClient {
       const LayoutPoint& translation_offset = LayoutPoint()) const;
 
   bool HitTestContents(HitTestResult&,
-                       const LayoutRect& layer_bounds,
+                       const LayoutPoint& fragment_offset,
                        const HitTestLocation&,
                        HitTestFilter) const;
   bool HitTestContentsForFragments(const PaintLayerFragments&,
--- third_party/WebKit/Source/core/paint/PaintLayerFragment.h
+++ third_party/WebKit/Source/core/paint/PaintLayerFragment.h
@@ -60,7 +60,6 @@ struct PaintLayerFragment {
     layer_bounds.MoveBy(offset);
     background_rect.MoveBy(offset);
     foreground_rect.MoveBy(offset);
-    pagination_clip.MoveBy(offset);
   }
 
   void Intersect(const LayoutRect& rect) {
@@ -109,15 +108,6 @@ struct PaintLayerFragment {
   // The physical translation to apply to shift the layer when
   // painting/hit-testing.
   LayoutPoint pagination_offset;
-
-  // Only set on paginated fragments.
-  //
-  // This is the additional clip from the fragmentainer (i.e. column or
-  // page) that applies to the layer. |backgroundRect| and |foregroundRect|
-  // are intersected with it (see collectFragments).
-  //
-  // It is in layer-local (physical) coordinates.
-  LayoutRect pagination_clip;
 };
 
 typedef Vector<PaintLayerFragment, 1> PaintLayerFragments;
