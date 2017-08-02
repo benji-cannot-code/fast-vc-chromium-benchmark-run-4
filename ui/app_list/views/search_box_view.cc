b459f17b132c09697fb6aa2cb20b192f94be8975
Weidong Guo <weidongg@chromium.org>
2017-08-02 01:54:34 +0000

    cros: X button should only clear query
    Changes:
    1. Clicking/touching the x button should clear the text and keep the
    searchbox active.
    2. Backspace clearing the last character should act the same as X
    button.
    3. Add unit tests for this behavior change.
    
    BUG=749361
    TEST=
    SearchBoxViewFullscreenTest.CloseButtonInvisibleByDefault
    SearchBoxViewFullscreenTest.CloseButtonVisibleAfterTyping
    SearchBoxViewFullscreenTest.SearchBoxEmptyAfterCloseButtonClicked
    SearchBoxViewFullscreenTest.SearchBoxActiveAfterCloseButtonClicked
    
    Change-Id: I6ad303bd222996c1aad343c17c08fb17f5667318
    Reviewed-on: https://chromium-review.googlesource.com/595256
    Commit-Queue: Weidong Guo <weidongg@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491203}

--- ui/app_list/views/search_box_view.cc
+++ ui/app_list/views/search_box_view.cc
@@ -262,10 +262,8 @@ void SearchBoxView::ClearSearch() {
   // does not generate ContentsChanged() notification.
   UpdateModel();
   NotifyQueryChanged();
-  if (is_fullscreen_app_list_enabled_) {
-    SetSearchBoxActive(false);
+  if (is_fullscreen_app_list_enabled_)
     app_list_view_->SetStateFromSearchBoxView(search_box_->text().empty());
-  }
 }
 
 void SearchBoxView::SetShadow(const gfx::ShadowValue& shadow) {
@@ -642,7 +640,7 @@ void SearchBoxView::ContentsChanged(views::Textfield* sender,
   view_delegate_->AutoLaunchCanceled();
   NotifyQueryChanged();
   if (is_fullscreen_app_list_enabled_) {
-    SetSearchBoxActive(!search_box_->text().empty());
+    SetSearchBoxActive(true);
     // If the query is only whitespace, don't transition the AppListView state.
     base::string16 trimmed_query = search_box_->text();
     base::TrimWhitespace(search_box_->text(), base::TrimPositions::TRIM_ALL,
