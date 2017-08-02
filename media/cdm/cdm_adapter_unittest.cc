69b6c0906cd9d74eb84573d12fd54b6df21802a2
Xiaohan Wang <xhwang@chromium.org>
2017-08-02 06:50:14 +0000

    media: Add CdmModule
    This is equivalent to the current PpapiCdmAdapterModule class, which
    represents a per-process singleton that handles CDM library/module
    initialization and deinitialization. This fixes the issue where
    INITIALIZE_CDM_MODULE and DeinitializeCdmModule are not called when
    using mojo CDM. In the future, we may choose to do CDM related
    presandbox initialization in this class as well (concrete plan TBD).
    
    This CL also updated ClearKeyCdm so that if INITIALIZE_CDM_MODULE is not
    called CreateCdmInstance() will fail, hence related ECK browser tests
    will fail.
    
    BUG=403462
    TEST=Updated browser tests to make sure the module is initialized.
    
    Change-Id: I3edd6cf5add3526258758af4f0eeae15c25c9816
    Reviewed-on: https://chromium-review.googlesource.com/578608
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491287}

--- media/cdm/cdm_adapter_unittest.cc
+++ media/cdm/cdm_adapter_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/media_switches.h"
 #include "media/base/mock_filters.h"
 #include "media/cdm/cdm_file_io.h"
+#include "media/cdm/cdm_module.h"
 #include "media/cdm/external_clear_key_test_helper.h"
 #include "media/cdm/simple_cdm_allocator.h"
 #include "media/media_features.h"
@@ -87,26 +88,26 @@ class CdmAdapterTest : public testing::Test {
  public:
   enum ExpectedResult { SUCCESS, FAILURE };
 
-  CdmAdapterTest() {}
-  ~CdmAdapterTest() override {}
-
- protected:
-  void SetUp() override {
+  CdmAdapterTest() {
     // Enable use of External Clear Key CDM.
     scoped_feature_list_.InitWithFeatures(
         {media::kExternalClearKeyForTesting,
          media::kSupportExperimentalCdmInterface},
         {});
+
+    CdmModule::GetInstance()->SetCdmPathForTesting(helper_.LibraryPath());
   }
 
+  ~CdmAdapterTest() override { CdmModule::ResetInstanceForTesting(); }
+
+ protected:
   // Initializes the adapter. |expected_result| tests that the call succeeds
   // or generates an error.
-  void InitializeAndExpect(base::FilePath library_path,
-                           ExpectedResult expected_result) {
+  void InitializeAndExpect(ExpectedResult expected_result) {
     CdmConfig cdm_config;  // default settings of false are sufficient.
     std::unique_ptr<CdmAllocator> allocator(new SimpleCdmAllocator());
     CdmAdapter::Create(
-        helper_.KeySystemName(), library_path, cdm_config, std::move(allocator),
+        helper_.KeySystemName(), cdm_config, std::move(allocator),
         base::Bind(&CdmAdapterTest::CreateCdmFileIO, base::Unretained(this)),
         base::Bind(&MockCdmClient::OnSessionMessage,
                    base::Unretained(&cdm_client_)),
@@ -176,8 +177,6 @@ class CdmAdapterTest : public testing::Test {
     RunUntilIdle();
   }
 
-  base::FilePath ExternalClearKeyLibrary() { return helper_.LibraryPath(); }
-
   std::string SessionId() { return session_id_; }
 
  private:
@@ -185,10 +184,11 @@ class CdmAdapterTest : public testing::Test {
                     const scoped_refptr<ContentDecryptionModule>& cdm,
                     const std::string& error_message) {
     if (cdm) {
-      EXPECT_EQ(expected_result, SUCCESS) << "CDM should not have loaded.";
+      ASSERT_EQ(expected_result, SUCCESS)
+          << "CDM creation succeeded unexpectedly.";
       adapter_ = cdm;
     } else {
-      EXPECT_EQ(expected_result, FAILURE) << error_message;
+      ASSERT_EQ(expected_result, FAILURE) << error_message;
     }
   }
 
@@ -260,23 +260,24 @@ class CdmAdapterTest : public testing::Test {
 };
 
 TEST_F(CdmAdapterTest, Initialize) {
-  InitializeAndExpect(ExternalClearKeyLibrary(), SUCCESS);
+  InitializeAndExpect(SUCCESS);
 }
 
 TEST_F(CdmAdapterTest, BadLibraryPath) {
-  InitializeAndExpect(base::FilePath(FILE_PATH_LITERAL("no_library_here")),
-                      FAILURE);
+  CdmModule::GetInstance()->SetCdmPathForTesting(
+      base::FilePath(FILE_PATH_LITERAL("no_library_here")));
+  InitializeAndExpect(FAILURE);
 }
 
 TEST_F(CdmAdapterTest, CreateWebmSession) {
-  InitializeAndExpect(ExternalClearKeyLibrary(), SUCCESS);
+  InitializeAndExpect(SUCCESS);
 
   std::vector<uint8_t> key_id(kKeyId, kKeyId + arraysize(kKeyId));
   CreateSessionAndExpect(EmeInitDataType::WEBM, key_id, SUCCESS);
 }
 
 TEST_F(CdmAdapterTest, CreateKeyIdsSession) {
-  InitializeAndExpect(ExternalClearKeyLibrary(), SUCCESS);
+  InitializeAndExpect(SUCCESS);
 
   // Don't include the trailing /0 from the string in the data passed in.
   std::vector<uint8_t> key_id(kKeyIdAsJWK,
@@ -285,7 +286,7 @@ TEST_F(CdmAdapterTest, CreateKeyIdsSession) {
 }
 
 TEST_F(CdmAdapterTest, CreateCencSession) {
-  InitializeAndExpect(ExternalClearKeyLibrary(), SUCCESS);
+  InitializeAndExpect(SUCCESS);
 
   std::vector<uint8_t> key_id(kKeyIdAsPssh,
                               kKeyIdAsPssh + arraysize(kKeyIdAsPssh));
@@ -297,7 +298,7 @@ TEST_F(CdmAdapterTest, CreateCencSession) {
 }
 
 TEST_F(CdmAdapterTest, CreateSessionWithBadData) {
-  InitializeAndExpect(ExternalClearKeyLibrary(), SUCCESS);
+  InitializeAndExpect(SUCCESS);
 
   // Use |kKeyId| but specify KEYIDS format.
   std::vector<uint8_t> key_id(kKeyId, kKeyId + arraysize(kKeyId));
@@ -305,7 +306,7 @@ TEST_F(CdmAdapterTest, CreateSessionWithBadData) {
 }
 
 TEST_F(CdmAdapterTest, LoadSession) {
-  InitializeAndExpect(ExternalClearKeyLibrary(), SUCCESS);
+  InitializeAndExpect(SUCCESS);
 
   // LoadSession() is not supported by AesDecryptor.
   std::vector<uint8_t> key_id(kKeyId, kKeyId + arraysize(kKeyId));
@@ -313,7 +314,7 @@ TEST_F(CdmAdapterTest, LoadSession) {
 }
 
 TEST_F(CdmAdapterTest, UpdateSession) {
-  InitializeAndExpect(ExternalClearKeyLibrary(), SUCCESS);
+  InitializeAndExpect(SUCCESS);
 
   std::vector<uint8_t> key_id(kKeyId, kKeyId + arraysize(kKeyId));
   CreateSessionAndExpect(EmeInitDataType::WEBM, key_id, SUCCESS);
@@ -322,7 +323,7 @@ TEST_F(CdmAdapterTest, UpdateSession) {
 }
 
 TEST_F(CdmAdapterTest, UpdateSessionWithBadData) {
-  InitializeAndExpect(ExternalClearKeyLibrary(), SUCCESS);
+  InitializeAndExpect(SUCCESS);
 
   std::vector<uint8_t> key_id(kKeyId, kKeyId + arraysize(kKeyId));
   CreateSessionAndExpect(EmeInitDataType::WEBM, key_id, SUCCESS);
