8713d27ea86d44624306bcf69f3a8178a558a995
sczs <sczs@chromium.org>
2017-08-02 02:42:41 +0000

    [ios] Moves history_popup into its own subdirectory.
    For boxing purposes this CL moves the history_popup classes into a 
    subdirectory of its own, this way boxing this component will be 
    easier. 
    
    Bug: 
    Change-Id: If652c9ab145fd268231fee538bfbde048a2a40e4
    Reviewed-on: https://chromium-review.googlesource.com/595224
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491234}

--- ios/chrome/browser/ui/history_popup/tab_history_cell.mm
+++ ios/chrome/browser/ui/history_popup/tab_history_cell.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/history/tab_history_cell.h"
+#import "ios/chrome/browser/ui/history_popup/tab_history_cell.h"
 
 #include "base/strings/sys_string_conversions.h"
 #include "ios/chrome/browser/ui/ui_util.h"
--- ios/chrome/browser/ui/history_popup/tab_history_popup_controller_unittest.mm
+++ ios/chrome/browser/ui/history_popup/tab_history_popup_controller_unittest.mm
@@ -3,13 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/history/tab_history_popup_controller.h"
+#import "ios/chrome/browser/ui/history_popup/tab_history_popup_controller.h"
 
 #include <memory>
 #include <utility>
 
 #include "components/sessions/core/session_types.h"
-#import "ios/chrome/browser/ui/history/tab_history_view_controller.h"
+#import "ios/chrome/browser/ui/history_popup/tab_history_view_controller.h"
 #include "ios/chrome/browser/ui/ui_util.h"
 #include "ios/web/public/navigation_item.h"
 #include "ios/web/public/navigation_item_list.h"
