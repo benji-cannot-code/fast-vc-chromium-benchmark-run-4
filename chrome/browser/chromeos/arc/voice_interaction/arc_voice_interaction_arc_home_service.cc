c210ba553499d78bdb920471d47ddb9b7c3f2b79
Muyuan Li <muyuanli@chromium.org>
2017-08-02 22:44:33 +0000

    fix scale issue in voice interaction context.
    BUG=b:64029031
    TEST=None
    
    Change-Id: I3b14f8f6f2fc3867548578937334a288b33e06ed
    Reviewed-on: https://chromium-review.googlesource.com/597169
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Commit-Queue: Muyuan Li <muyuanli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491536}

--- chrome/browser/chromeos/arc/voice_interaction/arc_voice_interaction_arc_home_service.cc
+++ chrome/browser/chromeos/arc/voice_interaction/arc_voice_interaction_arc_home_service.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "ui/accessibility/platform/ax_snapshot_node_android_platform.h"
 #include "ui/aura/window.h"
+#include "ui/compositor/dip_util.h"
 #include "ui/snapshot/snapshot.h"
 #include "ui/wm/public/activation_client.h"
 #include "url/gurl.h"
@@ -174,10 +175,11 @@ void ArcVoiceInteractionArcHomeService::GetVoiceInteractionStructure(
     return;
   }
 
-  web_contents->RequestAXTreeSnapshot(
-      base::Bind(&RequestVoiceInteractionStructureCallback, callback,
-                 browser->window()->GetBounds(),
-                 web_contents->GetLastCommittedURL().spec()));
+  web_contents->RequestAXTreeSnapshot(base::Bind(
+      &RequestVoiceInteractionStructureCallback, callback,
+      ui::ConvertRectToPixel(browser->window()->GetNativeWindow()->layer(),
+                             browser->window()->GetBounds()),
+      web_contents->GetLastCommittedURL().spec()));
 }
 
 void ArcVoiceInteractionArcHomeService::OnVoiceInteractionOobeSetupComplete() {
