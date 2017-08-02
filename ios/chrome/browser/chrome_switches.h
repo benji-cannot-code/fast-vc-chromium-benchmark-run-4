0168ea132c97580ccb9bca685daa173242d37553
Eric Noyau <noyau@google.com>
2017-08-02 16:26:39 +0000

    Remove flag for Request Mobile Site.
    First patch just removes the flag and the command line, the second removes all
    the associated code.
    
    Change-Id: Iad8fd182655dca68be28f00c8a55e200dd7557f8
    Reviewed-on: https://chromium-review.googlesource.com/598234
    Reviewed-by: Yuke Liao <liaoyuke@chromium.org>
    Commit-Queue: Eric Noyau <noyau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491397}

--- ios/chrome/browser/chrome_switches.h
+++ ios/chrome/browser/chrome_switches.h
@@ -20,7 +20,6 @@ extern const char kDisableNTPFavicons[];
 extern const char kDisableOfflineAutoReload[];
 extern const char kDisableTabStripAutoScrollNewTabs[];
 extern const char kDisableIOSPhysicalWeb[];
-extern const char kDisableRequestMobileSite[];
 extern const char kDisableSuggestionsUI[];
 extern const char kDisableBookmarkReordering[];
 extern const char kDisableSlimNavigationManager[];
