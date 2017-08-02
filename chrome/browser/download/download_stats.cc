7026c5c74f18a43f49d09ac904dac6f5806eb358
F . <zpeng@chromium.org>
2017-08-02 17:08:13 +0000

    Revert "Make downloads work when history database fails to initialize."
    This reverts commit d1bc5d0b5f6336fe076466ee42644b2e8fef4bc6.
    
    Reason for revert: Introduces new C++ static initializers. See crbug.com/751735
    
    Original change's description:
    > Make downloads work when history database fails to initialize.
    > 
    > Previously all download calls will be stuck when history database fails
    > to initialize.
    > 
    > This CL assigns 1 as the first id when history database failed to load,
    > all downloads in this browser session will not be persisted to history
    > database.
    > 
    > Bug: 736511
    > Change-Id: If4db04ca518633a7dd999f45629e60e97fc78b1a
    > Reviewed-on: https://chromium-review.googlesource.com/587327
    > Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > Commit-Queue: Xing Liu <xingliu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#490540}
    
    TBR=rkaplow@chromium.org,dtrainor@chromium.org,xingliu@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 736511, 751735
    Change-Id: Ifa73c694e919bda82eb37ea88020cf6b0ba89751
    Reviewed-on: https://chromium-review.googlesource.com/598347
    Reviewed-by: F . <zpeng@chromium.org>
    Commit-Queue: F . <zpeng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491410}

--- chrome/browser/download/download_stats.cc
+++ chrome/browser/download/download_stats.cc
@@ -51,7 +51,3 @@ void RecordDownloadOpenMethod(ChromeDownloadOpenMethod open_method) {
                             open_method,
                             DOWNLOAD_OPEN_METHOD_LAST_ENTRY);
 }
-
-void RecordDatabaseAvailability(bool is_available) {
-  UMA_HISTOGRAM_BOOLEAN("Download.Database.IsAvailable", is_available);
-}
