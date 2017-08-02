e9d3738d4e8aae0d36822357fa1f7a177d305dfa
Ken Rockot <rockot@chromium.org>
2017-08-02 20:48:57 +0000

    Remove unnecessary SetUpContext usage
    It's no longer necessary to use SetUpContext to compensate for
    individual field accessors being called twice, as they are no
    longer called twice.
    
    This removes all such uses of SetUpContext/TearDownContext.
    
    BUG=None
    
    Change-Id: I1162f095bc40fd73b2f06323c4f78ed5025e0ee6
    Reviewed-on: https://chromium-review.googlesource.com/596497
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491488}

--- third_party/WebKit/Source/platform/mojo/FetchAPIRequestStructTraits.cpp
+++ third_party/WebKit/Source/platform/mojo/FetchAPIRequestStructTraits.cpp
@@ -15,19 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace mojo {
 
-namespace {
-
-// Struct traits context for the FetchAPIRequest type. Since getters are invoked
-// twice when serializing the type, this reduces the load for heavy members.
-struct FetchAPIRequestStructTraitsContext {
-  FetchAPIRequestStructTraitsContext() = default;
-  ~FetchAPIRequestStructTraitsContext() = default;
-
-  WTF::HashMap<WTF::String, WTF::String> headers;
-};
-
-}  // namespace
-
 using blink::mojom::FetchCredentialsMode;
 using blink::mojom::FetchRedirectMode;
 using blink::mojom::FetchRequestMode;
@@ -381,26 +368,6 @@ bool EnumTraits<RequestContextType, blink::WebURLRequest::RequestContext>::
 }
 
 // static
-void* StructTraits<blink::mojom::FetchAPIRequestDataView,
-                   blink::WebServiceWorkerRequest>::
-    SetUpContext(const blink::WebServiceWorkerRequest& request) {
-  FetchAPIRequestStructTraitsContext* context =
-      new FetchAPIRequestStructTraitsContext();
-  for (const auto& pair : request.Headers())
-    context->headers.insert(pair.key, pair.value);
-
-  return context;
-}
-
-// static
-void StructTraits<blink::mojom::FetchAPIRequestDataView,
-                  blink::WebServiceWorkerRequest>::
-    TearDownContext(const blink::WebServiceWorkerRequest& request,
-                    void* context) {
-  delete static_cast<FetchAPIRequestStructTraitsContext*>(context);
-}
-
-// static
 blink::KURL StructTraits<blink::mojom::FetchAPIRequestDataView,
                          blink::WebServiceWorkerRequest>::
     url(const blink::WebServiceWorkerRequest& request) {
@@ -415,12 +382,14 @@ WTF::String StructTraits<blink::mojom::FetchAPIRequestDataView,
 }
 
 // static
-const WTF::HashMap<WTF::String, WTF::String>&
+WTF::HashMap<WTF::String, WTF::String>
 StructTraits<blink::mojom::FetchAPIRequestDataView,
              blink::WebServiceWorkerRequest>::
-    headers(const blink::WebServiceWorkerRequest& request, void* context) {
-  DCHECK(context);
-  return static_cast<FetchAPIRequestStructTraitsContext*>(context)->headers;
+    headers(const blink::WebServiceWorkerRequest& request) {
+  WTF::HashMap<WTF::String, WTF::String> header_map;
+  for (const auto& pair : request.Headers())
+    header_map.insert(pair.key, pair.value);
+  return header_map;
 }
 
 // static
