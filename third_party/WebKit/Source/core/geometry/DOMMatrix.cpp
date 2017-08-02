fc0aba27d95e79146c300f5d98482e44a10743a8
Yuki Shiino <yukishiino@chromium.org>
2017-08-02 07:30:06 +0000

    v8binding: Refactors MutationObserver and MutationCallback.
    Major changes in this CL are:
    - Drops [CustomConstructor] from MutationObserver.idl.
    - Replaces hand-written V8MutationCallback with code-generated
      MutationCallback. Note that the new (code-generated)
      MutationCallback is different from the old (hand-written)
      MutationCallback.
    - Rewrote the old (hand-written) MutationCallback as
      MutationObserver::Delegate.  Note that the name
      "MutationCallback" is taken by the new (code-generated)
      MutationCallback.
    
    This CL mostly follows the same way as https://crrev.com/c/581128 .
    
    Bug: 
    Change-Id: I3f8b36044499c50baade331e2bfafbbed4271e0d
    Reviewed-on: https://chromium-review.googlesource.com/590832
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491297}

--- third_party/WebKit/Source/core/geometry/DOMMatrix.cpp
+++ third_party/WebKit/Source/core/geometry/DOMMatrix.cpp
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/geometry/DOMMatrix.h"
 
+#include "core/dom/ExecutionContext.h"
+
 namespace blink {
 
 DOMMatrix* DOMMatrix::Create() {
