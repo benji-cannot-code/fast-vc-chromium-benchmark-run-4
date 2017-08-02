f919a59d37d1b96c06da1909b1adc6ad607dd587
Xiaocheng Hu <xiaochengh@chromium.org>
2017-08-02 02:16:31 +0000

    Use unsigned integers in TextIterator
    This patch changes TextIterator and its hellper classes to use unsigned
    integers to get rid of unnecessary sign checks and type casts.
    
    Public functions of TextIterator remain untouched.
    
    Bug: 751353
    Change-Id: I7b888d77fde272573c70f0a7fdbcc0410eec7e82
    Reviewed-on: https://chromium-review.googlesource.com/595120
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491221}

--- third_party/WebKit/Source/core/editing/iterators/TextIteratorTextState.cpp
+++ third_party/WebKit/Source/core/editing/iterators/TextIteratorTextState.cpp
@@ -34,13 +34,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 UChar TextIteratorTextState::CharacterAt(unsigned index) const {
-  SECURITY_DCHECK(index < static_cast<unsigned>(length()));
-  if (!(index < static_cast<unsigned>(length())))
+  SECURITY_DCHECK(index < length());
+  if (!(index < length()))
     return 0;
 
   if (single_character_buffer_) {
     DCHECK_EQ(index, 0u);
-    DCHECK_EQ(length(), 1);
+    DCHECK_EQ(length(), 1u);
     return single_character_buffer_;
   }
 
@@ -49,8 +49,8 @@ UChar TextIteratorTextState::CharacterAt(unsigned index) const {
 
 String TextIteratorTextState::Substring(unsigned position,
                                         unsigned length) const {
-  SECURITY_DCHECK(position <= static_cast<unsigned>(this->length()));
-  SECURITY_DCHECK(position + length <= static_cast<unsigned>(this->length()));
+  SECURITY_DCHECK(position <= this->length());
+  SECURITY_DCHECK(position + length <= this->length());
   if (!length)
     return g_empty_string;
   if (single_character_buffer_) {
@@ -65,8 +65,8 @@ void TextIteratorTextState::AppendTextToStringBuilder(
     StringBuilder& builder,
     unsigned position,
     unsigned max_length) const {
-  unsigned length_to_append =
-      std::min(static_cast<unsigned>(length()) - position, max_length);
+  SECURITY_DCHECK(position <= this->length());
+  unsigned length_to_append = std::min(length() - position, max_length);
   if (!length_to_append)
     return;
   if (single_character_buffer_) {
@@ -100,7 +100,7 @@ void TextIteratorTextState::EmitAltText(Node* node) {
 void TextIteratorTextState::FlushPositionOffsets() const {
   if (!position_offset_base_node_)
     return;
-  int index = position_offset_base_node_->NodeIndex();
+  unsigned index = position_offset_base_node_->NodeIndex();
   position_start_offset_ += index;
   position_end_offset_ += index;
   position_offset_base_node_ = nullptr;
@@ -109,8 +109,8 @@ void TextIteratorTextState::FlushPositionOffsets() const {
 void TextIteratorTextState::SpliceBuffer(UChar c,
                                          Node* text_node,
                                          Node* offset_base_node,
-                                         int text_start_offset,
-                                         int text_end_offset) {
+                                         unsigned text_start_offset,
+                                         unsigned text_end_offset) {
   DCHECK(text_node);
   has_emitted_ = true;
 
@@ -134,19 +134,17 @@ void TextIteratorTextState::SpliceBuffer(UChar c,
 }
 
 void TextIteratorTextState::EmitText(Node* text_node,
-                                     int position_start_offset,
-                                     int position_end_offset,
+                                     unsigned position_start_offset,
+                                     unsigned position_end_offset,
                                      const String& string,
-                                     int text_start_offset,
-                                     int text_end_offset) {
+                                     unsigned text_start_offset,
+                                     unsigned text_end_offset) {
   DCHECK(text_node);
   text_ = string;
 
   DCHECK(!text_.IsEmpty());
-  DCHECK_LE(0, text_start_offset);
-  DCHECK_LT(text_start_offset, static_cast<int>(text_.length()));
-  DCHECK_LE(0, text_end_offset);
-  DCHECK_LE(text_end_offset, static_cast<int>(text_.length()));
+  DCHECK_LT(text_start_offset, text_.length());
+  DCHECK_LE(text_end_offset, text_.length());
   DCHECK_LE(text_start_offset, text_end_offset);
 
   position_node_ = text_node;
@@ -164,8 +162,7 @@ void TextIteratorTextState::EmitText(Node* text_node,
 void TextIteratorTextState::AppendTextTo(ForwardsTextBuffer* output,
                                          unsigned position,
                                          unsigned length_to_append) const {
-  SECURITY_DCHECK(position + length_to_append <=
-                  static_cast<unsigned>(length()));
+  SECURITY_DCHECK(position + length_to_append <= length());
   // Make sure there's no integer overflow.
   SECURITY_DCHECK(position + length_to_append >= position);
   if (!length_to_append)
@@ -173,7 +170,7 @@ void TextIteratorTextState::AppendTextTo(ForwardsTextBuffer* output,
   DCHECK(output);
   if (single_character_buffer_) {
     DCHECK_EQ(position, 0u);
-    DCHECK_EQ(length(), 1);
+    DCHECK_EQ(length(), 1u);
     output->PushCharacters(single_character_buffer_, 1);
     return;
   }
