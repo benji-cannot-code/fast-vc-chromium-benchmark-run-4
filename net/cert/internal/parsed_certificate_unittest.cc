d210cba264489f871b1e7206af393467571d8776
Eric Roman <eroman@chromium.org>
2017-08-02 20:03:28 +0000

    Add some extra certificate parsing errors for policy qualifiers.
    Bug: 634443
    Change-Id: I415c4d968b29325307401bf1c8b23c24ba977d37
    Reviewed-on: https://chromium-review.googlesource.com/597409
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Eric Roman <eroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491478}

--- net/cert/internal/parsed_certificate_unittest.cc
+++ net/cert/internal/parsed_certificate_unittest.cc
@@ -142,7 +142,8 @@ TEST(ParsedCertificateTest, BadKeyUsage) {
   ASSERT_FALSE(ParseCertificateFromFile("bad_key_usage.pem", {}));
 }
 
-// TODO(eroman): What is wrong with policy qualifiers?
+// Parses a certificate that has a PolicyQualifierInfo that is missing the
+// qualifier field.
 TEST(ParsedCertificateTest, BadPolicyQualifiers) {
   ASSERT_FALSE(ParseCertificateFromFile("bad_policy_qualifiers.pem", {}));
 }
@@ -152,7 +153,8 @@ TEST(ParsedCertificateTest, BadSignatureAlgorithmOid) {
   ASSERT_FALSE(ParseCertificateFromFile("bad_signature_algorithm_oid.pem", {}));
 }
 
-// TODO(eroman): What is wrong with the validity?
+//  The validity encodes time as UTCTime but following the BER rules rather than
+//  DER rules (i.e. YYMMDDHHMMZ instead of YYMMDDHHMMSSZ).
 TEST(ParsedCertificateTest, BadValidity) {
   ASSERT_FALSE(ParseCertificateFromFile("bad_validity.pem", {}));
 }
