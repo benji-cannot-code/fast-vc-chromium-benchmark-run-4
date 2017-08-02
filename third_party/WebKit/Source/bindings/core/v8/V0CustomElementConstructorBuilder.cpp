f25cd28722da9350a1059a0d23aa88b7f9e1e441
Jeremy Roman <jbroman@chromium.org>
2017-08-02 23:59:20 +0000

    Use V8AtomicString for strings used as property keys.
    V8 always internalizes strings when doing property lookup, so this can only
    save the work of heap-allocating a new string that will just be looked up
    later, and likely already exists in the internalized string table (e.g.
    because it is the key of a property, or because it occurs as a string literal).
    
    Found mainly by searching for string literals passed to V8String. Other
    similar cases may yet remain.
    
    No perf measurement was done, but this seems like it's easily the nicer
    thing to do in these cases.
    
    Change-Id: Idd645a83495f5ec1323f3ce87548d0f2a16aca19
    Reviewed-on: https://chromium-review.googlesource.com/598540
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491561}

--- third_party/WebKit/Source/bindings/core/v8/V0CustomElementConstructorBuilder.cpp
+++ third_party/WebKit/Source/bindings/core/v8/V0CustomElementConstructorBuilder.cpp
@@ -165,7 +165,7 @@ V0CustomElementConstructorBuilder::RetrieveCallback(const char* name) {
   v8::Local<v8::Value> value;
   if (!prototype_
            ->Get(script_state_->GetContext(),
-                 V8String(script_state_->GetIsolate(), name))
+                 V8AtomicString(script_state_->GetIsolate(), name))
            .ToLocal(&value) ||
       !value->IsFunction())
     return v8::MaybeLocal<v8::Function>();
@@ -216,7 +216,7 @@ bool V0CustomElementConstructorBuilder::CreateConstructor(
   constructor_->SetName(v8_type->IsNull() ? v8_tag_name
                                           : v8_type.As<v8::String>());
 
-  v8::Local<v8::String> prototype_key = V8String(isolate, "prototype");
+  v8::Local<v8::String> prototype_key = V8AtomicString(isolate, "prototype");
   if (!V8CallBoolean(constructor_->HasOwnProperty(context, prototype_key)))
     return false;
   // This sets the property *value*; calling Set is safe because
@@ -232,7 +232,8 @@ bool V0CustomElementConstructorBuilder::CreateConstructor(
           v8::PropertyAttribute(v8::ReadOnly | v8::DontEnum | v8::DontDelete))))
     return false;
 
-  v8::Local<v8::String> constructor_key = V8String(isolate, "constructor");
+  v8::Local<v8::String> constructor_key =
+      V8AtomicString(isolate, "constructor");
   v8::Local<v8::Value> constructor_prototype;
   if (!prototype_->Get(context, constructor_key)
            .ToLocal(&constructor_prototype))
@@ -245,7 +246,7 @@ bool V0CustomElementConstructorBuilder::CreateConstructor(
   V8PrivateProperty::GetCustomElementIsInterfacePrototypeObject(isolate).Set(
       prototype_, v8::True(isolate));
   if (!V8CallBoolean(prototype_->DefineOwnProperty(
-          context, V8String(isolate, "constructor"), constructor_,
+          context, V8AtomicString(isolate, "constructor"), constructor_,
           v8::DontEnum)))
     return false;
 
@@ -268,7 +269,8 @@ bool V0CustomElementConstructorBuilder::PrototypeIsValid(
 
   v8::PropertyAttribute property_attribute;
   if (!prototype_
-           ->GetPropertyAttributes(context, V8String(isolate, "constructor"))
+           ->GetPropertyAttributes(context,
+                                   V8AtomicString(isolate, "constructor"))
            .To(&property_attribute) ||
       (property_attribute & v8::DontDelete)) {
     V0CustomElementException::ThrowException(
