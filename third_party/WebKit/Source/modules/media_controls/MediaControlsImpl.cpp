abac879fb637d7a012765915990339fff64a0d09
Guido Urdaneta <guidou@chromium.org>
2017-08-02 12:10:34 +0000

    Revert "Revert "v8binding: Refactors MutationObserver and MutationCallback.""
    This reverts commit a58af821d592f6a024cca4e93ea7dd8ba7e275dc.
    
    Reason for revert: Relanding the original CL since the revert did not fix the error.
    
    Original change's description:
    > Revert "v8binding: Refactors MutationObserver and MutationCallback."
    > 
    > This reverts commit e4d81dae1158bddcd44345329296df6ba95e34dd.
    > 
    > Reason for revert: Speculative revert.
    > 
    > Layout test tables/mozilla_expected_failures/marvin/backgr_fixed-bg.html started failing reliably on WebKit Mac10.9 bot following this CL.
    > It does not look very related, so I will reland it if the failure continues.
    > 
    > Original change's description:
    > > v8binding: Refactors MutationObserver and MutationCallback.
    > > 
    > > Major changes in this CL are:
    > > - Drops [CustomConstructor] from MutationObserver.idl.
    > > - Replaces hand-written V8MutationCallback with code-generated
    > >   MutationCallback. Note that the new (code-generated)
    > >   MutationCallback is different from the old (hand-written)
    > >   MutationCallback.
    > > - Rewrote the old (hand-written) MutationCallback as
    > >   MutationObserver::Delegate.  Note that the name
    > >   "MutationCallback" is taken by the new (code-generated)
    > >   MutationCallback.
    > > 
    > > This CL mostly follows the same way as https://crrev.com/c/581128 .
    > > 
    > > Bug: 
    > > Change-Id: I3f8b36044499c50baade331e2bfafbbed4271e0d
    > > Reviewed-on: https://chromium-review.googlesource.com/590832
    > > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > > Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    > > Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#491297}
    > 
    > TBR=yukishiino@chromium.org,bashi@chromium.org,haraken@chromium.org
    > 
    > Change-Id: Ie6aeebac4741cd3a7cac1dacd15c6f709e15ecab
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/597708
    > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#491333}
    
    TBR=yukishiino@chromium.org,bashi@chromium.org,haraken@chromium.org,guidou@chromium.org
    
    Change-Id: I4e2fba2f191aeb2caea234af1307b1a9dde144dc
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/597887
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491343}

--- third_party/WebKit/Source/modules/media_controls/MediaControlsImpl.cpp
+++ third_party/WebKit/Source/modules/media_controls/MediaControlsImpl.cpp
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/media_controls/MediaControlsImpl.h"
 
 #include "bindings/core/v8/ExceptionState.h"
-#include "core/dom/MutationCallback.h"
 #include "core/dom/MutationObserver.h"
 #include "core/dom/MutationObserverInit.h"
 #include "core/dom/MutationRecord.h"
@@ -153,7 +152,7 @@ bool PreferHiddenVolumeControls(const Document& document) {
          document.GetSettings()->GetPreferHiddenVolumeControls();
 }
 
-}  // anonymous namespace
+}  // namespace
 
 class MediaControlsImpl::BatchedControlUpdate {
   WTF_MAKE_NONCOPYABLE(BatchedControlUpdate);
@@ -209,31 +208,23 @@ class MediaControlsImpl::MediaControlsResizeObserverDelegate final
 // Observes changes to the HTMLMediaElement attributes that affect controls.
 // Currently only observes the disableRemotePlayback attribute.
 class MediaControlsImpl::MediaElementMutationCallback
-    : public MutationCallback {
+    : public MutationObserver::Delegate {
  public:
   explicit MediaElementMutationCallback(MediaControlsImpl* controls)
-      : controls_(controls) {
-    observer_ = MutationObserver::Create(this);
-    Vector<String> filter;
-    filter.push_back(HTMLNames::disableremoteplaybackAttr.ToString());
+      : controls_(controls), observer_(MutationObserver::Create(this)) {
     MutationObserverInit init;
     init.setAttributeOldValue(true);
     init.setAttributes(true);
-    init.setAttributeFilter(filter);
+    init.setAttributeFilter({HTMLNames::disableremoteplaybackAttr.ToString()});
     observer_->observe(&controls_->MediaElement(), init, ASSERT_NO_EXCEPTION);
   }
 
-  DEFINE_INLINE_VIRTUAL_TRACE() {
-    visitor->Trace(controls_);
-    visitor->Trace(observer_);
-    MutationCallback::Trace(visitor);
+  ExecutionContext* GetExecutionContext() const override {
+    return &controls_->GetDocument();
   }
 
-  void Disconnect() { observer_->disconnect(); }
-
- private:
-  void Call(const HeapVector<Member<MutationRecord>>& records,
-            MutationObserver*) override {
+  void Deliver(const MutationRecordVector& records,
+               MutationObserver&) override {
     for (const auto& record : records) {
       if (record->type() != "attributes")
         continue;
@@ -249,10 +240,15 @@ class MediaControlsImpl::MediaElementMutationCallback
     }
   }
 
-  ExecutionContext* GetExecutionContext() const override {
-    return &controls_->GetDocument();
+  void Disconnect() { observer_->disconnect(); }
+
+  DEFINE_INLINE_VIRTUAL_TRACE() {
+    visitor->Trace(controls_);
+    visitor->Trace(observer_);
+    MutationObserver::Delegate::Trace(visitor);
   }
 
+ private:
   Member<MediaControlsImpl> controls_;
   Member<MutationObserver> observer_;
 };
