088101679669c7d6e5c88e5e0eb4f2ddd0704c71
Yuke Liao <liaoyuke@chromium.org>
2017-08-02 21:22:12 +0000

    Disable 3 tests in NewTabPage test case on iOS 11.
    testRotation, testOmnibox and testScrollToolbar fail on iPhone 6 Plus,
    iOS 10, and this CL disables them on iOS 11 to unblock running the test
    case on iPhone 6 Plus, iOS 11.
    
    Bug: 751803
    Change-Id: Ibcda582e2f86617b1fcea89a5296dde182b9b81c
    Reviewed-on: https://chromium-review.googlesource.com/598646
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Yuke Liao <liaoyuke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491503}

--- ios/chrome/browser/ui/ntp/new_tab_page_egtest.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_egtest.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <EarlGrey/EarlGrey.h>
 #import <XCTest/XCTest.h>
 
+#include "base/ios/ios_util.h"
 #include "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/ui/commands/generic_chrome_command.h"
 #include "ios/chrome/browser/ui/commands/ios_command_ids.h"
@@ -144,6 +145,11 @@ void AssertNTPScrolledToTop(bool scrolledToTop) {
     EARL_GREY_TEST_DISABLED(@"Disabled for iPad due to device rotation bug.");
   }
 
+  // TODO(crbug.com/751803): Re-enable this test once the bug is fixed.
+  if (base::ios::IsRunningOnIOS11OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Disabled on iOS 11.")
+  }
+
   NSString* ntpOmniboxLabel = l10n_util::GetNSString(IDS_OMNIBOX_EMPTY_HINT);
   NSString* focusedOmniboxLabel = l10n_util::GetNSString(IDS_ACCNAME_LOCATION);
   SelectNewTabPagePanel(NewTabPage::kHomePanel);
@@ -215,6 +221,11 @@ void AssertNTPScrolledToTop(bool scrolledToTop) {
 
 // Tests focusing and defocusing the NTP's omnibox.
 - (void)testOmnibox {
+  // TODO(crbug.com/751803): Re-enable this test once the bug is fixed.
+  if (base::ios::IsRunningOnIOS11OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Disabled on iOS 11.")
+  }
+
   // Empty the pasteboard: if it contains a link the Google Landing will not be
   // interactable.
   [UIPasteboard generalPasteboard].string = @"";
@@ -281,6 +292,11 @@ void AssertNTPScrolledToTop(bool scrolledToTop) {
 - (void)testScrollToolbar {
   if (IsIPadIdiom()) {
     EARL_GREY_TEST_SKIPPED(@"Skipped for iPad (no hidden toolbar in tablet)");
+  }
+
+  // TODO(crbug.com/751803): Re-enable this test once the bug is fixed.
+  if (base::ios::IsRunningOnIOS11OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Disabled on iOS 11.")
   }
 
   NSString* toolsMenuLabel = l10n_util::GetNSString(IDS_IOS_TOOLBAR_SETTINGS);
