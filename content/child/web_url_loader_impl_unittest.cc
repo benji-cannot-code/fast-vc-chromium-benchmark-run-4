5b713bfc8a21d92b4d2d91df211180de5802eeea
Yutaka Hirano <yhirano@chromium.org>
2017-08-02 11:03:25 +0000

    Remove was_ignored_by_handler from ResourceRequestCompletionStatus
    Bug: 748491
    Change-Id: I302fe1c5dbd95926ab5cd96d3fb7d53463f15d53
    Reviewed-on: https://chromium-review.googlesource.com/588039
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491334}

--- content/child/web_url_loader_impl_unittest.cc
+++ content/child/web_url_loader_impl_unittest.cc
@@ -324,7 +324,7 @@ class WebURLLoaderImplTest : public testing::Test {
 
   void DoCompleteRequest() {
     EXPECT_FALSE(client()->did_finish());
-    peer()->OnCompletedRequest(net::OK, false, false, base::TimeTicks(),
+    peer()->OnCompletedRequest(net::OK, false, base::TimeTicks(),
                                strlen(kTestData), strlen(kTestData),
                                strlen(kTestData));
     EXPECT_TRUE(client()->did_finish());
@@ -335,7 +335,7 @@ class WebURLLoaderImplTest : public testing::Test {
 
   void DoFailRequest() {
     EXPECT_FALSE(client()->did_finish());
-    peer()->OnCompletedRequest(net::ERR_FAILED, false, false, base::TimeTicks(),
+    peer()->OnCompletedRequest(net::ERR_FAILED, false, base::TimeTicks(),
                                strlen(kTestData), strlen(kTestData),
                                strlen(kTestData));
     EXPECT_FALSE(client()->did_finish());
@@ -563,7 +563,7 @@ TEST_F(WebURLLoaderImplTest, FtpDeleteOnReceiveMoreData) {
   // Directory listings are only parsed once the request completes, so this will
   // cancel in DoReceiveDataFtp, before the request finishes.
   client()->set_delete_on_receive_data();
-  peer()->OnCompletedRequest(net::OK, false, false, base::TimeTicks(),
+  peer()->OnCompletedRequest(net::OK, false, base::TimeTicks(),
                              strlen(kTestData), strlen(kTestData),
                              strlen(kTestData));
   EXPECT_FALSE(client()->did_finish());
