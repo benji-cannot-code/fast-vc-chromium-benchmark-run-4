5d2a7d3b8708ebe8c44259b9246b272590efdb86
Nicholas Verne <nverne@chromium.org>
2017-08-02 13:44:16 +0000

    Move VirtualTimeTest to core/exported.
    Bug: 712963
    Change-Id: I9d88fb2153c9de3ff01c5542923a7d4e832ac382
    Reviewed-on: https://chromium-review.googlesource.com/597610
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491360}

--- third_party/WebKit/Source/core/scheduler/VirtualTimeTest.cpp
+++ third_party/WebKit/Source/core/scheduler/VirtualTimeTest.cpp
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/run_loop.h"
 #include "build/build_config.h"
 #include "core/dom/TaskRunnerHelper.h"
 #include "core/testing/sim/SimRequest.h"
@@ -55,19 +54,16 @@ class VirtualTimeTest : public SimTest {
 };
 
 namespace {
-void QuitRunLoop() {
-  base::RunLoop::QuitCurrentDeprecated();
-}
 
 // Some task queues may have repeating v8 tasks that run forever so we impose a
 // hard time limit.
 void RunTasksForPeriod(double delay_ms) {
   Platform::Current()->CurrentThread()->GetWebTaskRunner()->PostDelayedTask(
-      BLINK_FROM_HERE, WTF::Bind(&QuitRunLoop),
+      BLINK_FROM_HERE, WTF::Bind(&testing::ExitRunLoop),
       TimeDelta::FromMillisecondsD(delay_ms));
   testing::EnterRunLoop();
 }
-}
+}  // namespace
 
 // http://crbug.com/633321
 #if defined(OS_ANDROID)
