5264192d62e02061eb87d6f3deafd73f3824801a
Will Chen <chenwilliam@chromium.org>
2017-08-02 20:58:57 +0000

    DevTools: move console tests (part 2)
    Bug: 667560
    Change-Id: I1e41ec9991ef0c1aff5acb3ccc177318fd2c79bd
    Reviewed-on: https://chromium-review.googlesource.com/595238
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491494}

---
+++ third_party/WebKit/Source/devtools/scripts/migrate_test/migrate_utils.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+const path = require('path');
+
+function getOutPath(inputPath) {
+  const nonHttpLayoutTestPrefix = 'LayoutTests/inspector';
+  const httpLayoutTestPrefix = 'LayoutTests/http/tests/inspector';
+  const postfix = inputPath.indexOf(nonHttpLayoutTestPrefix) === -1 ?
+      inputPath.slice(inputPath.indexOf(httpLayoutTestPrefix) + httpLayoutTestPrefix.length + 1)
+          .replace('.html', '.js') :
+      inputPath.slice(inputPath.indexOf(nonHttpLayoutTestPrefix) + nonHttpLayoutTestPrefix.length + 1)
+          .replace('.html', '.js');
+  const out = path.resolve(__dirname, '..', '..', '..', '..', 'LayoutTests', 'http', 'tests', 'devtools', postfix);
+  return out;
+}
+
+module.exports = {
+  getOutPath,
+};
