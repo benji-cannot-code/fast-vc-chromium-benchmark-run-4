d25feaadf7f4dd328f2a846c275dcd5291d2406b
yucliu <yucliu@chromium.org>
2017-08-02 19:55:33 +0000

    Randomly generate origin id in MediaDrmStorage
    Use base::UnguessableToken for random origin id. MediaDrmStorage will
    persist the origin id in the origin map.
    
    BUG=493521
    TEST=shaka player demo
    
    Change-Id: I01690c9f1985c5346306b1b19e60fb5d434a867e
    Reviewed-on: https://chromium-review.googlesource.com/582212
    Commit-Queue: Yuchen Liu <yucliu@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491474}

--- components/cdm/browser/media_drm_storage_impl.h
+++ components/cdm/browser/media_drm_storage_impl.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_CDM_BROWSER_MEDIA_DRM_STORAGE_IMPL_H_
 
 #include "base/threading/thread_checker.h"
+#include "base/unguessable_token.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "media/mojo/interfaces/media_drm_storage.mojom.h"
@@ -37,7 +38,7 @@ class MediaDrmStorageImpl final : public media::mojom::MediaDrmStorage,
   ~MediaDrmStorageImpl() final;
 
   // media::mojom::MediaDrmStorage implementation.
-  void Initialize(const url::Origin& origin) final;
+  void Initialize(InitializeCallback callback) final;
   void OnProvisioned(OnProvisionedCallback callback) final;
   void SavePersistentSession(const std::string& session_id,
                              media::mojom::SessionDataPtr session_data,
@@ -51,6 +52,8 @@ class MediaDrmStorageImpl final : public media::mojom::MediaDrmStorage,
   void RenderFrameDeleted(content::RenderFrameHost* render_frame_host) final;
   void DidFinishNavigation(content::NavigationHandle* navigation_handle) final;
 
+  bool IsInitialized() const { return !!origin_id_; }
+
  private:
   base::ThreadChecker thread_checker_;
 
@@ -59,9 +62,14 @@ class MediaDrmStorageImpl final : public media::mojom::MediaDrmStorage,
 
   content::RenderFrameHost* const render_frame_host_ = nullptr;
   PrefService* const pref_service_ = nullptr;
-  const url::Origin origin_;
+
+  // String for the current origin. It will be used as a key in storage
+  // dictionary.
   const std::string origin_string_;
-  bool initialized_ = false;
+
+  // ID for the current origin. Per EME spec on individualization,
+  // implementation should not expose application-specific information.
+  base::UnguessableToken origin_id_;
 
   mojo::Binding<media::mojom::MediaDrmStorage> binding_;
 };
--- media/base/android/media_drm_storage.h
+++ media/base/android/media_drm_storage.h
@@ -18,6 +18,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/media_export.h"
 #include "url/origin.h"
 
+namespace base {
+class UnguessableToken;
+}  // namespace base
+
 namespace media {
 
 // Allows MediaDrmBridge to store and retrieve persistent data. This is needed
@@ -40,17 +44,21 @@ class MEDIA_EXPORT MediaDrmStorage
   // Callback to return whether the operation succeeded.
   using ResultCB = base::OnceCallback<void(bool)>;
 
+  // Callback for storage initialization.
+  using InitCB =
+      base::OnceCallback<void(const base::UnguessableToken& origin_id)>;
+
   // Callback to return the result of LoadPersistentSession. |key_set_id| and
   // |mime_type| must be non-empty if |success| is true, and vice versa.
   using LoadPersistentSessionCB =
       base::OnceCallback<void(std::unique_ptr<SessionData> session_data)>;
 
-  // Binds |this| to |origin|.
-  // TODO(xhwang): The host of the service should know about the last committed
-  // origin. We should solely use that origin, or check the |origin| against it.
-  // TODO(xhwang): We should NOT use the real origin for provisioning. Use a
-  // random origin ID instead.
-  virtual void Initialize(const url::Origin& origin) = 0;
+  // Initialize the storage for current origin. The implementation already know
+  // the origin for the storage.
+  // Implementation should return a random origin id in |init_cb|. The ID should
+  // be unique and persisted. Origin ID must be valid. If any corruption is
+  // detected, the old map should be removed in OnProvisioned.
+  virtual void Initialize(InitCB init_cb) = 0;
 
   // Called when MediaDrm is provisioned for the origin bound to |this|.
   // The implementation should keep track of the storing time so that the
