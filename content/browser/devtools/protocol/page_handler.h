473b467d3fb0aa58c7752a9118a1a1b90e04531f
Pavel Feldman <pfeldman@chromium.org>
2017-08-02 05:00:16 +0000

    DevTools: dispatch jsdialog-related events regardless of the embedder dialog delegate.
    Bug: 718235
    Change-Id: I39d763bec2a12ff2e77e67e201c1137983a8dc00
    Reviewed-on: https://chromium-review.googlesource.com/592608
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491272}

--- content/browser/devtools/protocol/page_handler.h
+++ content/browser/devtools/protocol/page_handler.h
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/readback_types.h"
+#include "content/public/common/javascript_dialog_type.h"
+#include "url/gurl.h"
 
 class SkBitmap;
 
@@ -50,6 +52,8 @@ class PageHandler : public DevToolsDomainHandler,
   explicit PageHandler(EmulationHandler* handler);
   ~PageHandler() override;
 
+  static std::vector<PageHandler*> EnabledForWebContents(
+      WebContentsImpl* contents);
   static std::vector<PageHandler*> ForAgentHost(DevToolsAgentHostImpl* host);
 
   void Wire(UberDispatcher* dispatcher) override;
@@ -60,6 +64,16 @@ class PageHandler : public DevToolsDomainHandler,
   void DidAttachInterstitialPage();
   void DidDetachInterstitialPage();
   bool screencast_enabled() const { return enabled_ && screencast_enabled_; }
+  using JavaScriptDialogCallback =
+      base::Callback<void(bool, const base::string16&)>;
+  void DidRunJavaScriptDialog(const GURL& url,
+                              const base::string16& message,
+                              const base::string16& default_prompt,
+                              JavaScriptDialogType dialog_type,
+                              const JavaScriptDialogCallback& callback);
+  void DidRunBeforeUnloadConfirm(const GURL& url,
+                                 const JavaScriptDialogCallback& callback);
+  void DidCloseJavaScriptDialog(bool success, const base::string16& user_input);
 
   Response Enable() override;
   Response Disable() override;
@@ -158,6 +172,7 @@ class PageHandler : public DevToolsDomainHandler,
   EmulationHandler* emulation_handler_;
   std::unique_ptr<Page::Frontend> frontend_;
   NotificationRegistrar registrar_;
+  JavaScriptDialogCallback pending_dialog_;
   base::WeakPtrFactory<PageHandler> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(PageHandler);
