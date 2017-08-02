96231bdb3ba2afdafb86ff21777ca2929a5a556c
Eric Stevenson <estevenson@chromium.org>
2017-08-02 21:40:06 +0000

    Update JNI docs/examples.
    Bug: 683256
    Change-Id: I3371e15bb4b0396c56ab4a4963c0c820d33674cf
    Reviewed-on: https://chromium-review.googlesource.com/590495
    Commit-Queue: Eric Stevenson <estevenson@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491511}

--- base/android/jni_generator/sample_for_tests.cc
+++ base/android/jni_generator/sample_for_tests.cc
@@ -36,10 +36,6 @@ CPPClass::~CPPClass() {
 }
 
 // static
-bool CPPClass::RegisterJNI(JNIEnv* env) {
-  return RegisterNativesImpl(env);  // Generated in SampleForTests_jni.h
-}
-
 void CPPClass::Destroy(JNIEnv* env, const JavaParamRef<jobject>& caller) {
   delete this;
 }
