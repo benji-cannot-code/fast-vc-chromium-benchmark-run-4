5c9e04eae1eb857dbd3c2838ecc808268cb70d68
Sammie Quon <sammiequon@google.com>
2017-08-03 23:18:17 +0000

    stylus: Hide palette tray unless stylus event seen before.
    This CL does not show the palette tray unless a stylus event has been previous seen (unless the device contains an internal stylus). Stores a pref to track whether a stylus has been seen before and if not, a pointer watcher is fired up to look for stylus events.
    
    Bug: 740298
    Test: ash_unittests "PaletteTray*Test.*"
    Change-Id: I75d03dd382321a4f4e2324a5c9bec43b7b3c02c3
    Reviewed-on: https://chromium-review.googlesource.com/592228
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491873}

--- ash/public/cpp/ash_pref_names.h
+++ ash/public/cpp/ash_pref_names.h
@@ -12,6 +12,7 @@ namespace ash {
 
 namespace prefs {
 
+ASH_PUBLIC_EXPORT extern const char kHasSeenStylus[];
 ASH_PUBLIC_EXPORT extern const char kNightLightEnabled[];
 ASH_PUBLIC_EXPORT extern const char kNightLightTemperature[];
 ASH_PUBLIC_EXPORT extern const char kNightLightScheduleType[];
