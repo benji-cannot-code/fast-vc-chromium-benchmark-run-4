0ce920d3a8d95019118a1b49c994b70c529d2ecd
Steven Holte <holte@google.com>
2017-08-03 00:49:23 +0000

    Make MetricsLog not need a PrefService.
    Move system profile persistence to MetricsService.
    
    Bug: 
    Change-Id: I5c31dc53de267dd9699a83af926f8a575bab18a0
    Reviewed-on: https://chromium-review.googlesource.com/592689
    Commit-Queue: Steven Holte <holte@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491573}

--- components/metrics/metrics_state_manager.h
+++ components/metrics/metrics_state_manager.h
@@ -24,6 +24,7 @@ namespace metrics {
 
 class ClonedInstallDetector;
 class EnabledStateProvider;
+class MetricsProvider;
 
 // Responsible for managing MetricsService state prefs, specifically the UMA
 // client id and low entropy source. Code outside the metrics directory should
@@ -41,6 +42,8 @@ class MetricsStateManager {
       LoadClientInfoCallback;
 
   virtual ~MetricsStateManager();
+
+  std::unique_ptr<MetricsProvider> GetProvider();
 
   // Returns true if the user has consented to sending metric reports, and there
   // is no other reason to disable reporting. One such reason is client
