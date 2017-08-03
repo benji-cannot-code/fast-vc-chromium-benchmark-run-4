6d275b4848f9ff16b0e4361ad27699841c246c00
Patti <patricialor@chromium.org>
2017-08-03 05:35:57 +0000

    MD Settings: Update Site Details dynamically when permissions change.
    Make the Site Details web UI update permission values when they change without
    needing to reload the Site Details page.
    
    Manual test - With #enable-site-details on, open https://permission.site. Open
    the Page Info bubble, click "Site settings", and drag the Site Details tab out
    into a new window so that the https://permission.site and the Site Details
    windows are viewable at the same time. On https://permission.site, open the Page
    Info bubble and change a permission there. It should automatically update to
    show the same permission in the Site Details page.
    
    Bug: 656758
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: If1b28afbed3317a7ac43ada1d8ca02424137b50d
    Reviewed-on: https://chromium-review.googlesource.com/595299
    Commit-Queue: Patti <patricialor@chromium.org>
    Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491653}

--- chrome/browser/resources/settings/site_settings/site_details.js
+++ chrome/browser/resources/settings/site_settings/site_details.js
@@ -11,7 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'site-details',
 
-  behaviors: [SiteSettingsBehavior, settings.RouteObserverBehavior],
+  behaviors: [
+    SiteSettingsBehavior, settings.RouteObserverBehavior, WebUIListenerBehavior
+  ],
 
   properties: {
     /**
@@ -61,6 +63,13 @@ Polymer({
   },
 
   /** @override */
+  attached: function() {
+    this.addWebUIListener(
+        'contentSettingSitePermissionChanged',
+        this.onPermissionChanged_.bind(this));
+  },
+
+  /** @override */
   ready: function() {
     this.ContentSettingsTypes = settings.ContentSettingsTypes;
   },
@@ -85,16 +94,55 @@ Polymer({
     if (this.enableSiteSettings_)
       this.$.usageApi.fetchUsageTotal(this.toUrl(this.origin).hostname);
 
-    var siteDetailsPermissions =
-        /** @type {!NodeList<!SiteDetailsPermissionElement>} */
-        (this.root.querySelectorAll('site-details-permission'));
+    this.updatePermissions_(this.getCategoryList_());
+  },
 
-    this.browserProxy.getOriginPermissions(this.origin, this.getCategoryList_())
+  /**
+   * Called when a site within a category has been changed.
+   * @param {!settings.ContentSettingsTypes} category The category that changed.
+   * @param {string} origin The origin of the site that changed.
+   * @param {string} embeddingOrigin The embedding origin of the site that
+   *     changed.
+   * @private
+   */
+  onPermissionChanged_: function(category, origin, embeddingOrigin) {
+    if (this.origin === undefined || this.origin == '' ||
+        origin === undefined || origin == '')
+      return;
+    if (!this.getCategoryList_().includes(category))
+      return;
+
+    // Site details currently doesn't support embedded origins, so ignore it and
+    // just check whether the origins are the same.
+    if (this.toUrl(origin).origin == this.toUrl(this.origin).origin)
+      this.updatePermissions_([category]);
+  },
+
+  /**
+   * Retrieves the permissions listed in |categoryList| from the backend for
+   * |this.origin|.
+   * @param {!Array<!settings.ContentSettingsTypes>} categoryList The list of
+   *     categories to update permissions for.
+   * @private
+   */
+  updatePermissions_: function(categoryList) {
+    var permissionsMap =
+        /** @type {!Object<!settings.ContentSettingsTypes,
+         *         !SiteDetailsPermissionElement>} */
+        (Array.prototype.reduce.call(
+            this.root.querySelectorAll('site-details-permission'),
+            (map, element) => {
+              if (categoryList.includes(element.category))
+                map[element.category] = element;
+              return map;
+            },
+            {}));
+
+    this.browserProxy.getOriginPermissions(this.origin, categoryList)
         .then((exceptionList) => {
           exceptionList.forEach((exception, i) => {
-            // |exceptionList| should be in the same order as the category list,
-            // which is in the same order as |siteDetailsPermissions|.
-            siteDetailsPermissions[i].site = exception;
+            // |exceptionList| should be in the same order as |categoryList|.
+            permissionsMap[categoryList[i]].site = exception;
           });
 
           // The displayName won't change, so just use the first exception.
@@ -156,7 +204,7 @@ Polymer({
 
   /**
    * Returns list of categories for each permission displayed in <site-details>.
-   * @return {!Array<string>}
+   * @return {!Array<!settings.ContentSettingsTypes>}
    * @private
    */
   getCategoryList_: function() {
