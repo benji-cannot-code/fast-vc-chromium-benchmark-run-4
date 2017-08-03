a2e3c5c30346c746abd75983700ac229ef50b92f
spqchan <spqchan@chromium.org>
2017-08-03 15:38:56 +0000

    [Mac] Feature Flag for the Fullscreen Toolbar Reveal Animation
    Create a feature flag for this feature.
    
    Bug: 751784
    Change-Id: I49ba9af40f68496b1e59f3e5f9fc5e0fa068ae6f
    Reviewed-on: https://chromium-review.googlesource.com/598628
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Sarah Chan <spqchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491746}

--- chrome/browser/ui/cocoa/fullscreen/fullscreen_toolbar_controller.mm
+++ chrome/browser/ui/cocoa/fullscreen/fullscreen_toolbar_controller.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/fullscreen/fullscreen_toolbar_controller.h"
 
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "chrome/browser/profiles/profile.h"
 #import "chrome/browser/ui/cocoa/browser_window_controller.h"
 #import "chrome/browser/ui/cocoa/fullscreen/fullscreen_menubar_tracker.h"
@@ -13,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/fullscreen/fullscreen_toolbar_mouse_tracker.h"
 #import "chrome/browser/ui/cocoa/fullscreen/fullscreen_toolbar_visibility_lock_controller.h"
 #import "chrome/browser/ui/cocoa/fullscreen/immersive_fullscreen_controller.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 
@@ -85,10 +87,8 @@ const CGFloat kShowFraction = 1.0;
 
 - (void)revealToolbarForWebContents:(content::WebContents*)contents
                        inForeground:(BOOL)inForeground {
-  if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kEnableFullscreenToolbarReveal)) {
+  if (!base::FeatureList::IsEnabled(features::kFullscreenToolbarReveal))
     return;
-  }
 
   animationController_->AnimateToolbarForTabstripChanges(contents,
                                                          inForeground);
