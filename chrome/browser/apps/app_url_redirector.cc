c4313de99512fb413405b84c8c20f204883873a2
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2017-08-03 04:26:40 +0000

    desktop-pwas: Open Bookmark app when link is within scope
    The DesktopPWAWindowing feature must be on in order for this to work.
    
    Makes it so that, once a user has installed a Bookmark App for a website, all
    navigations to that website will open the installed Bookmark App. This includes
    typing the address in the omnibox, clicking any links to an in-scope url, and
    opening any links to an in-scope url from the context menu. Future patches
    will change some of these so that the result is more intuitive.
    
    For example, if the user has installed https://www.foo.com through
    "Add to shelf" and the site's manifest defines scope as "/scope" and then the
    user visits https://www.foo.com/scope/test.html, a new app window to
    https://www.foo.com/scope/test.html will open. If the user
    navigated to https://www.foo.com/index.html, we would not open a new app
    window since the address is not in-scope as defined by the site's manifest.
    
    Three changes:
    
    1. Use 'scope' in the site's manifest to create a url handler for
       the bookmark app.
    2. Allow Hosted Apps that are Bookmark apps to use url handlers.
       This means that any non-bookmark hosted app that has url handlers
       will fail to load.
    3. When handling a url that a Bookmark app could handle open the
       the bookmark app with the specified url.
    
    Bug: 740783
    Change-Id: Iac69b13dc33be9ecd7336f24ae9021d5dd0161d0
    Reviewed-on: https://chromium-review.googlesource.com/566199
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Matt Giuca <mgiuca@chromium.org>
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491644}

--- chrome/browser/apps/app_url_redirector.cc
+++ chrome/browser/apps/app_url_redirector.cc
@@ -7,9 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "apps/launcher.h"
 #include "base/bind.h"
+#include "base/feature_list.h"
 #include "base/logging.h"
 #include "chrome/browser/prerender/prerender_contents.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser_finder.h"
+#include "chrome/browser/ui/extensions/app_launch_params.h"
+#include "chrome/browser/ui/extensions/application_launch.h"
+#include "chrome/browser/web_applications/web_app.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/common/extensions/api/url_handlers/url_handlers_parser.h"
 #include "components/navigation_interception/intercept_navigation_throttle.h"
 #include "components/navigation_interception/navigation_params.h"
@@ -36,6 +42,12 @@ bool LaunchAppWithUrl(
     const navigation_interception::NavigationParams& params) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
+  // Redirecting for Bookmark Apps is hidden behind a feature flag.
+  if (app->from_bookmark() &&
+      !base::FeatureList::IsEnabled(features::kDesktopPWAWindowing)) {
+    return false;
+  }
+
   // Redirect top-level navigations only. This excludes iframes and webviews
   // in particular.
   if (source->IsSubframe()) {
@@ -57,6 +69,27 @@ bool LaunchAppWithUrl(
 
   Profile* profile =
       Profile::FromBrowserContext(source->GetBrowserContext());
+
+  if (app->from_bookmark()) {
+    Browser* browser = chrome::FindBrowserWithWebContents(source);
+    if (browser == nullptr) {
+      DVLOG(1) << "Don't override: No browser, can't know if already in app.";
+      return false;
+    }
+
+    if (browser->app_name() ==
+        web_app::GenerateApplicationNameFromExtensionId(app->id())) {
+      DVLOG(1) << "Don't override: Already in app.";
+      return false;
+    }
+
+    AppLaunchParams launch_params(
+        profile, app.get(), extensions::LAUNCH_CONTAINER_WINDOW,
+        WindowOpenDisposition::CURRENT_TAB, extensions::SOURCE_URL_HANDLER);
+    launch_params.override_url = params.url();
+    OpenApplication(launch_params);
+    return true;
+  }
 
   DVLOG(1) << "Launching app handler with URL: "
            << params.url().spec() << " -> "
