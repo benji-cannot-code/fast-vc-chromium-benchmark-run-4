921559667138d1d107d4aa2150d074037b73d2c8
Bin Zhao <zhaobin@chromium.org>
2017-08-03 17:59:35 +0000

    [Media Router] Add Cast device count metrics
    - Moved device count metrics into its own class
    - Added Cast device count metrics
    
    Bug: 687377
    Change-Id: Id7ca7b7a529a79a8827889ad63f51b1da74db1db
    Reviewed-on: https://chromium-review.googlesource.com/596615
    Commit-Queue: Bin Zhao <zhaobin@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Derek Cheng <imcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491789}

--- chrome/browser/media/router/discovery/dial/dial_media_sink_service_impl.h
+++ chrome/browser/media/router/discovery/dial/dial_media_sink_service_impl.h
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/media/router/discovery/dial/device_description_service.h"
 #include "chrome/browser/media/router/discovery/dial/dial_registry.h"
+#include "chrome/browser/media/router/discovery/media_sink_discovery_metrics.h"
 #include "chrome/browser/media/router/discovery/media_sink_service_base.h"
-#include "chrome/browser/media/router/media_router_metrics.h"
 
 namespace media_router {
 
@@ -81,7 +81,7 @@ class DialMediaSinkServiceImpl : public MediaSinkServiceBase,
 
   scoped_refptr<net::URLRequestContextGetter> request_context_;
 
-  MediaRouterMetrics metrics_;
+  DialDeviceCountMetrics metrics_;
 };
 
 }  // namespace media_router
--- chrome/browser/media/router/media_router_metrics.h
+++ chrome/browser/media/router/media_router_metrics.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/gtest_prod_util.h"
-#include "base/time/clock.h"
 #include "base/time/time.h"
 #include "chrome/browser/ui/webui/media_router/media_cast_mode.h"
 #include "chrome/common/media_router/mojo/dial_device_description_parser.mojom.h"
@@ -65,8 +64,6 @@ class MediaRouterMetrics {
   ~MediaRouterMetrics();
 
   // UMA histogram names.
-  static const char kHistogramDialAvailableDeviceCount[];
-  static const char kHistogramDialKnownDeviceCount[];
   static const char kHistogramDialParsingError[];
   static const char kHistogramIconClickLocation[];
   static const char kHistogramMediaRouterCastingSource[];
@@ -113,20 +110,6 @@ class MediaRouterMetrics {
   // Records why DIAL device description resolution failed.
   static void RecordDialParsingError(
       chrome::mojom::DialParsingError parsing_error);
-
-  // Records device counts.
-  // TODO(zhaobin): Move device count specific metrics and state into its own
-  // class eventually.
-  void RecordDialDeviceCounts(size_t available_device_count,
-                              size_t known_device_count);
-
-  // Allows tests to swap in a fake clock.
-  void SetClockForTest(std::unique_ptr<base::Clock> clock);
-
- private:
-  base::Time device_count_metrics_record_time_;
-
-  std::unique_ptr<base::Clock> clock_;
 };
 
 }  // namespace media_router
