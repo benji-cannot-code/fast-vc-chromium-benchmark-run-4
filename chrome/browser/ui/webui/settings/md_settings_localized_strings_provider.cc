e264b9b4173a25ef19384424abe0bbd3c1aead22
Tommy C. Li <tommycli@chromium.org>
2017-08-03 01:09:32 +0000

    [HBD] Reduce tri-state Flash content setting to just ASK/BLOCK.
    This only affects the UI, not the actual behavior, which will be in a
    separate CL.
    
    Users on ALLOW will remain on ALLOW unless they access the Settings UI.
    
    Bug: 696790
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I27a0143f74d69d7985a0dd48645eab0bbc43193a
    Reviewed-on: https://chromium-review.googlesource.com/599115
    Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491590}

--- chrome/browser/ui/webui/settings/md_settings_localized_strings_provider.cc
+++ chrome/browser/ui/webui/settings/md_settings_localized_strings_provider.cc
@@ -1893,7 +1893,6 @@ void AddSiteSettingsStrings(content::WebUIDataSource* html_source,
      IDS_SETTINGS_SITE_SETTINGS_ASK_BEFORE_SENDING},
     {"siteSettingsAskBeforeSendingRecommended",
      IDS_SETTINGS_SITE_SETTINGS_ASK_BEFORE_SENDING_RECOMMENDED},
-    {"siteSettingsFlashAllow", IDS_SETTINGS_SITE_SETTINGS_FLASH_ALLOW},
     {"siteSettingsFlashBlock", IDS_SETTINGS_SITE_SETTINGS_FLASH_BLOCK},
     {"siteSettingsAllowRecentlyClosedSites",
      IDS_SETTINGS_SITE_SETTINGS_ALLOW_RECENTLY_CLOSED_SITES},
@@ -2025,19 +2024,18 @@ void AddSiteSettingsStrings(content::WebUIDataSource* html_source,
   if (PluginUtils::ShouldPreferHtmlOverPlugins(
           HostContentSettingsMapFactory::GetForProfile(profile))) {
     LocalizedString flash_strings[] = {
-        {"siteSettingsFlashAskBefore",
-         IDS_SETTINGS_SITE_SETTINGS_FLASH_ASK_BEFORE_RUNNING},
-        {"siteSettingsFlashAskBeforeSubtitle",
-         IDS_SETTINGS_SITE_SETTINGS_FLASH_ASK_BEFORE_RUNNING_SUBTITLE},
+        {"siteSettingsFlashAskFirst", IDS_SETTINGS_SITE_SETTINGS_ASK_FIRST},
+        {"siteSettingsFlashAskFirstRecommended",
+         IDS_SETTINGS_SITE_SETTINGS_ASK_FIRST_RECOMMENDED},
     };
     AddLocalizedStringsBulk(html_source, flash_strings,
                             arraysize(flash_strings));
   } else {
     LocalizedString flash_strings[] = {
-        {"siteSettingsFlashAskBefore",
+        {"siteSettingsFlashAskFirst",
          IDS_SETTINGS_SITE_SETTINGS_FLASH_DETECT_IMPORTANT},
-        {"siteSettingsFlashAskBeforeSubtitle",
-         IDS_SETTINGS_SITE_SETTINGS_FLASH_DETECT_IMPORTANT_SUBTITLE},
+        {"siteSettingsFlashAskFirstRecommended",
+         IDS_SETTINGS_SITE_SETTINGS_FLASH_DETECT_IMPORTANT_RECOMMENDED},
     };
     AddLocalizedStringsBulk(html_source, flash_strings,
                             arraysize(flash_strings));
