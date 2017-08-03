38f6a3acd0882ab8ee1089620c30dbc547d6b861
Khushal <khushalsagar@chromium.org>
2017-08-03 07:59:56 +0000

    cc: Also unref images with decode failures.
    The GPU cache expects an unref for an empty decode as well (returned in
    the case of a decode failure).
    
    Bug: 751444
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ie3875f20a5d5126ae0277520bb032f22eb4e0779
    Reviewed-on: https://chromium-review.googlesource.com/598488
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491672}

--- cc/raster/playback_image_provider.cc
+++ cc/raster/playback_image_provider.cc
@@ -19,8 +19,7 @@ SkIRect RoundOutRect(const SkRect& rect) {
 void UnrefImageFromCache(DrawImage draw_image,
                          ImageDecodeCache* cache,
                          DecodedDrawImage decoded_draw_image) {
-  if (decoded_draw_image.image())
-    cache->DrawWithImageFinished(draw_image, decoded_draw_image);
+  cache->DrawWithImageFinished(draw_image, decoded_draw_image);
 }
 
 }  // namespace
