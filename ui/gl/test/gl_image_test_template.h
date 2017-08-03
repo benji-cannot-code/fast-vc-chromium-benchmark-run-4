40c6323815478970492dfcd09962d6dba8ebe1f0
Francois Doray <fdoray@chromium.org>
2017-08-03 17:28:12 +0000

    Remove usage of ScopedAsyncTaskScheduler from gl_image_test_template.h.
    ScopedAsyncTaskScheduler is deprecated.
    
    Bug: 708584
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I89b92c36e714f4eb02b84f5130a75edf377cd8bf
    Reviewed-on: https://chromium-review.googlesource.com/591880
    Reviewed-by: David Reveman <reveman@chromium.org>
    Reviewed-by: John Bauman <jbauman@chromium.org>
    Commit-Queue: Francois Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491778}

--- ui/gl/test/gl_image_test_template.h
+++ ui/gl/test/gl_image_test_template.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/stringize_macros.h"
 #include "base/strings/stringprintf.h"
-#include "base/test/scoped_async_task_scheduler.h"
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/buffer_format_util.h"
@@ -178,7 +177,6 @@ class GLImageTest : public testing::Test {
   }
 
  protected:
-  base::test::ScopedAsyncTaskScheduler scoped_async_task_scheduler_;
   scoped_refptr<GLSurface> surface_;
   scoped_refptr<GLContext> context_;
   GLImageTestDelegate delegate_;
