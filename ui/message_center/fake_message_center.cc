250d30134f45685bfae9afe630f974178b10564f
Tetsui Ohkubo <tetsui@chromium.org>
2017-08-03 01:16:26 +0000

    Add first-party notification styling in MD notification.
    In new-style notification, first-party notification should have
    different styling compared to general web notifications.
    To do this, all system notifications should be created by
    new CreateSystemNotification. (Previously, it was not a requirement.)
    
    By using the function,
    * the display source defaults to string "ChromeOS system".
    * the notification has single accent color based on
      SystemNotificationWarningLevel.
    * gfx::VectorIcon will be passed as small_image instead of gfx::Image
      to perform icon coloring on CreateSystemNotification side.
    
    TEST=out/Debug/message_center_unittests
    BUG=738779,726241,740807,740813
    
    Change-Id: I9e85b3f1de33b6499297f060b5305d0b02157eef
    Reviewed-on: https://chromium-review.googlesource.com/566765
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491592}

--- ui/message_center/fake_message_center.cc
+++ ui/message_center/fake_message_center.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "ui/message_center/fake_message_center.h"
+#include "base/strings/string_util.h"
 #include "ui/message_center/notification_list.h"
 
 namespace message_center {
@@ -142,6 +143,13 @@ bool FakeMessageCenter::IsMessageCenterVisible() const {
 void FakeMessageCenter::RestartPopupTimers() {}
 
 void FakeMessageCenter::PausePopupTimers() {}
+
+const base::string16& FakeMessageCenter::GetProductOSName() const {
+  return base::EmptyString16();
+}
+
+void FakeMessageCenter::SetProductOSName(
+    const base::string16& product_os_name) {}
 
 void FakeMessageCenter::DisableTimersForTest() {}
 
