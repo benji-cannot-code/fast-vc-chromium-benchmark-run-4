3b39e3ec33a731d3c33b91553ce3235f087effb2
Scott Violet <sky@chromium.org>
2017-08-03 03:10:13 +0000

    chromeos: make Service call RequestQuit() vs QuitCurrentWhenIdleDeprecated()
    QuitCurrentWhenIdleDeprecated can end up leaving the thread in an
    unclear shutdown state and isn't really the right way to
    shutdown. Converts to use RequestQuit().
    
    BUG=none
    TEST=covered by tests
    
    Change-Id: I561a7e9a9615d759ceae44a7ad0e724a2d13d093
    Reviewed-on: https://chromium-review.googlesource.com/592530
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491627}

--- services/ui/service.cc
+++ services/ui/service.cc
@@ -150,6 +150,8 @@ Service::Service(const InProcessConfig* config)
                                                 : nullptr) {}
 
 Service::~Service() {
+  in_destructor_ = true;
+
   // Destroy |window_server_| first, since it depends on |event_source_|.
   // WindowServer (or more correctly its Displays) may have state that needs to
   // be destroyed before GpuState as well.
@@ -360,9 +362,13 @@ void Service::OnFirstDisplayReady() {
 }
 
 void Service::OnNoMoreDisplays() {
-  // We may get here from the destructor, in which case there is no messageloop.
-  if (base::RunLoop::IsRunningOnCurrentThread())
-    base::RunLoop::QuitCurrentWhenIdleDeprecated();
+  // We may get here from the destructor. Don't try to use RequestQuit() when
+  // that happens as ServiceContext DCHECKs in this case.
+  if (in_destructor_)
+    return;
+
+  DCHECK(context());
+  context()->RequestQuit();
 }
 
 bool Service::IsTestConfig() const {
