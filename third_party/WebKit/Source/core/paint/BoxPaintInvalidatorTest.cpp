910342e13798905e843a707eb074a613845fd2eb
Chris Harrelson <chrishtr@chromium.org>
2017-08-03 01:50:55 +0000

    Fix typo leading to double-calls to mapToVisualRectInAncestorSpace.
    This was accidentally introduced by https://codereview.chromium.org/2727093002.
    It affects only LayoutViews for iframes that end up in the slow path for
    paint invalidation, which is only for content under filter or multicolumn.
    
    Bug: 750925
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I9a183e0151f584e299134ba3091c440a7d24f0d1
    Reviewed-on: https://chromium-review.googlesource.com/599113
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491605}

--- third_party/WebKit/Source/core/paint/BoxPaintInvalidatorTest.cpp
+++ third_party/WebKit/Source/core/paint/BoxPaintInvalidatorTest.cpp
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/paint/BoxPaintInvalidator.h"
 
 #include "core/HTMLNames.h"
+#include "core/frame/FrameTestHelpers.h"
 #include "core/frame/LocalFrameView.h"
+#include "core/html/HTMLFrameOwnerElement.h"
 #include "core/layout/LayoutTestHelper.h"
 #include "core/layout/LayoutView.h"
 #include "core/paint/PaintInvalidator.h"
@@ -115,6 +117,33 @@ class BoxPaintInvalidatorTest : public ::testing::WithParamInterface<bool>,
 };
 
 INSTANTIATE_TEST_CASE_P(All, BoxPaintInvalidatorTest, ::testing::Bool());
+
+TEST_P(BoxPaintInvalidatorTest, SlowMapToVisualRectInAncestorSpaceLayoutView) {
+  SetBodyInnerHTML(
+      "<!doctype html>"
+      "<style>"
+      "#parent {"
+      "    display: inline-block;"
+      "    width: 300px;"
+      "    height: 300px;"
+      "    margin-top: 200px;"
+      "    filter: blur(3px);"  // Forces the slow path in
+                                // SlowMapToVisualRectInAncestorSpace.
+      "    border: 1px solid rebeccapurple;"
+      "}"
+      "</style>"
+      "<div id=parent>"
+      "  <iframe id='target' src='data:text/html,<body style='background: "
+      "blue;'></body>'></iframe>"
+      "</div>");
+
+  auto& target = *GetDocument().getElementById("target");
+  EXPECT_RECT_EQ(IntRect(2, 202, 318, 168),
+                 EnclosingIntRect(ToHTMLFrameOwnerElement(target)
+                                      .contentDocument()
+                                      ->GetLayoutView()
+                                      ->VisualRect()));
+}
 
 TEST_P(BoxPaintInvalidatorTest, ComputePaintInvalidationReasonPaintingNothing) {
   ScopedSlimmingPaintV2ForTest spv2(true);
