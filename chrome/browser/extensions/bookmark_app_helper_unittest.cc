5c019dc2305b4f3c7d509c40a80078aa3dccce67
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2017-08-03 10:16:15 +0000

    Reland "desktop-pwas: Open Bookmark app when link is within scope"
    This is a reland of aecc33fcc357fd232e5dbda7a4d03d3b0c3a8f5d
    
    Added test files for the bookmark appp. Without these tests fail in release
    builds.
    
    
    Original change's description:
    > desktop-pwas: Open Bookmark app when link is within scope
    > 
    > The DesktopPWAWindowing feature must be on in order for this to work.
    > 
    > Makes it so that, once a user has installed a Bookmark App for a website, all
    > navigations to that website will open the installed Bookmark App. This includes
    > typing the address in the omnibox, clicking any links to an in-scope url, and
    > opening any links to an in-scope url from the context menu. Future patches
    > will change some of these so that the result is more intuitive.
    > 
    > For example, if the user has installed https://www.foo.com through
    > "Add to shelf" and the site's manifest defines scope as "/scope" and then the
    > user visits https://www.foo.com/scope/test.html, a new app window to
    > https://www.foo.com/scope/test.html will open. If the user
    > navigated to https://www.foo.com/index.html, we would not open a new app
    > window since the address is not in-scope as defined by the site's manifest.
    > 
    > Three changes:
    > 
    > 1. Use 'scope' in the site's manifest to create a url handler for
    >    the bookmark app.
    > 2. Allow Hosted Apps that are Bookmark apps to use url handlers.
    >    This means that any non-bookmark hosted app that has url handlers
    >    will fail to load.
    > 3. When handling a url that a Bookmark app could handle open the
    >    the bookmark app with the specified url.
    > 
    > Bug: 740783
    > Change-Id: Iac69b13dc33be9ecd7336f24ae9021d5dd0161d0
    > Reviewed-on: https://chromium-review.googlesource.com/566199
    > Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    > Reviewed-by: Ben Wells <benwells@chromium.org>
    > Reviewed-by: Sky Malice <skym@chromium.org>
    > Reviewed-by: Matt Giuca <mgiuca@chromium.org>
    > Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#491644}
    
    TBR=benwells@chromium.org,mgiuca@chromium.org,skym@chromium.org,jochen@chromium.org
    
    Bug: 740783
    Change-Id: I6fe97df85507d1e5fbb209ea0877c4345a8e03ab
    Reviewed-on: https://chromium-review.googlesource.com/599647
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491692}

--- chrome/browser/extensions/bookmark_app_helper_unittest.cc
+++ chrome/browser/extensions/bookmark_app_helper_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/banners/app_banner_settings_helper.h"
+#include "chrome/browser/extensions/convert_web_app.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_service_test_base.h"
 #include "chrome/common/extensions/manifest_handlers/app_launch_info.h"
@@ -30,6 +31,8 @@ namespace {
 
 const char kAppUrl[] = "http://www.chromium.org";
 const char kAlternativeAppUrl[] = "http://www.notchromium.org";
+const char kAppScope[] = "http://www.chromium.org";
+const char kAppAlternativeScope[] = "http://www.chromium.org/new/";
 const char kAppTitle[] = "Test title";
 const char kAppShortName[] = "Test short name";
 const char kAlternativeAppTitle[] = "Different test title";
@@ -195,6 +198,7 @@ void ValidateWebApplicationInfo(base::Closure callback,
   EXPECT_EQ(original.title, newly_made.title);
   EXPECT_EQ(original.description, newly_made.description);
   EXPECT_EQ(original.app_url, newly_made.app_url);
+  EXPECT_EQ(original.scope, newly_made.scope);
   // There should be 6 icons, as there are three sizes which need to be
   // generated, and each will generate a 1x and 2x icon.
   EXPECT_EQ(6u, newly_made.icons.size());
@@ -361,6 +365,7 @@ TEST_F(BookmarkAppHelperExtensionServiceTest, CreateBookmarkAppWithManifest) {
   content::Manifest manifest;
   manifest.start_url = GURL(kAppUrl);
   manifest.name = base::NullableString16(base::UTF8ToUTF16(kAppTitle), false);
+  manifest.scope = GURL(kAppScope);
   helper.CompleteGetManifest(manifest);
 
   std::map<GURL, std::vector<SkBitmap> > icon_map;
@@ -375,6 +380,7 @@ TEST_F(BookmarkAppHelperExtensionServiceTest, CreateBookmarkAppWithManifest) {
   EXPECT_TRUE(extension->from_bookmark());
   EXPECT_EQ(kAppTitle, extension->name());
   EXPECT_EQ(GURL(kAppUrl), AppLaunchInfo::GetLaunchWebURL(extension));
+  EXPECT_EQ(GURL(kAppScope), GetScopeURLFromBookmarkApp(extension));
   EXPECT_FALSE(
       AppBannerSettingsHelper::GetSingleBannerEvent(
           contents.get(), manifest.start_url, manifest.start_url.spec(),
@@ -387,6 +393,7 @@ TEST_F(BookmarkAppHelperExtensionServiceTest, CreateBookmarkAppNoContents) {
   web_app_info.app_url = GURL(kAppUrl);
   web_app_info.title = base::UTF8ToUTF16(kAppTitle);
   web_app_info.description = base::UTF8ToUTF16(kAppDescription);
+  web_app_info.scope = GURL(kAppScope);
   web_app_info.icons.push_back(
       CreateIconInfoWithBitmap(kIconSizeTiny, SK_ColorRED));
 
@@ -404,6 +411,7 @@ TEST_F(BookmarkAppHelperExtensionServiceTest, CreateBookmarkAppNoContents) {
   EXPECT_EQ(kAppTitle, extension->name());
   EXPECT_EQ(kAppDescription, extension->description());
   EXPECT_EQ(GURL(kAppUrl), AppLaunchInfo::GetLaunchWebURL(extension));
+  EXPECT_EQ(GURL(kAppScope), GetScopeURLFromBookmarkApp(extension));
   EXPECT_FALSE(
       IconsInfo::GetIconResource(extension, kIconSizeTiny,
                                  ExtensionIconSet::MATCH_EXACTLY).empty());
@@ -429,6 +437,7 @@ TEST_F(BookmarkAppHelperExtensionServiceTest, CreateAndUpdateBookmarkApp) {
   web_app_info.app_url = GURL(kAppUrl);
   web_app_info.title = base::UTF8ToUTF16(kAppTitle);
   web_app_info.description = base::UTF8ToUTF16(kAppDescription);
+  web_app_info.scope = GURL(kAppScope);
   web_app_info.icons.push_back(
       CreateIconInfoWithBitmap(kIconSizeSmall, SK_ColorRED));
 
@@ -443,6 +452,7 @@ TEST_F(BookmarkAppHelperExtensionServiceTest, CreateAndUpdateBookmarkApp) {
     EXPECT_EQ(kAppTitle, extension->name());
     EXPECT_EQ(kAppDescription, extension->description());
     EXPECT_EQ(GURL(kAppUrl), AppLaunchInfo::GetLaunchWebURL(extension));
+    EXPECT_EQ(GURL(kAppScope), GetScopeURLFromBookmarkApp(extension));
     EXPECT_FALSE(extensions::IconsInfo::GetIconResource(
                      extension, kIconSizeSmall, ExtensionIconSet::MATCH_EXACTLY)
                      .empty());
@@ -450,6 +460,7 @@ TEST_F(BookmarkAppHelperExtensionServiceTest, CreateAndUpdateBookmarkApp) {
 
   web_app_info.title = base::UTF8ToUTF16(kAlternativeAppTitle);
   web_app_info.icons[0] = CreateIconInfoWithBitmap(kIconSizeLarge, SK_ColorRED);
+  web_app_info.scope = GURL(kAppAlternativeScope);
 
   extensions::CreateOrUpdateBookmarkApp(service_, &web_app_info);
   content::RunAllBlockingPoolTasksUntilIdle();
@@ -462,6 +473,8 @@ TEST_F(BookmarkAppHelperExtensionServiceTest, CreateAndUpdateBookmarkApp) {
     EXPECT_EQ(kAlternativeAppTitle, extension->name());
     EXPECT_EQ(kAppDescription, extension->description());
     EXPECT_EQ(GURL(kAppUrl), AppLaunchInfo::GetLaunchWebURL(extension));
+    EXPECT_EQ(GURL(kAppAlternativeScope),
+              GetScopeURLFromBookmarkApp(extension));
     EXPECT_FALSE(extensions::IconsInfo::GetIconResource(
                      extension, kIconSizeSmall, ExtensionIconSet::MATCH_EXACTLY)
                      .empty());
@@ -476,6 +489,7 @@ TEST_F(BookmarkAppHelperExtensionServiceTest, GetWebApplicationInfo) {
   web_app_info.app_url = GURL(kAppUrl);
   web_app_info.title = base::UTF8ToUTF16(kAppTitle);
   web_app_info.description = base::UTF8ToUTF16(kAppDescription);
+  web_app_info.scope = GURL(kAppScope);
 
   extensions::CreateOrUpdateBookmarkApp(service_, &web_app_info);
   content::RunAllBlockingPoolTasksUntilIdle();
