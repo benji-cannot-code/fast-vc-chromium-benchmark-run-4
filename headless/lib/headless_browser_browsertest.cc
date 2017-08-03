f022fe038c678643dd331cf306ddf7aef00598c1
Luca Versari <veluca@google.com>
2017-08-03 10:31:55 +0000

    headless: Add --allow-insecure-localhost flag.
    This flag was present in chrome, but ignored in headless.
    This commit makes headless behave as expected.
    
    R=eseckler@chromium.org
    
    Bug: 714287
    Change-Id: I2b06c345c1f29eb4390e7853537af9cf5d809bb8
    Reviewed-on: https://chromium-review.googlesource.com/599809
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Luca Versari <veluca@google.com>
    Cr-Commit-Position: refs/heads/master@{#491694}

--- headless/lib/headless_browser_browsertest.cc
+++ headless/lib/headless_browser_browsertest.cc
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_restrictions.h"
 #include "content/public/browser/permission_manager.h"
 #include "content/public/browser/permission_type.h"
+#include "content/public/browser/web_contents.h"
+#include "content/public/common/content_switches.h"
 #include "content/public/common/url_constants.h"
 #include "content/public/test/browser_test.h"
 #include "headless/lib/browser/headless_browser_context_impl.h"
@@ -912,6 +914,28 @@ IN_PROC_BROWSER_TEST_F(HeadlessBrowserTest, WindowPrint) {
   EXPECT_TRUE(WaitForLoad(web_contents));
   EXPECT_FALSE(
       EvaluateScript(web_contents, "window.print()")->HasExceptionDetails());
+}
+
+IN_PROC_BROWSER_TEST_F(HeadlessBrowserTest, AllowInsecureLocalhostFlag) {
+  net::EmbeddedTestServer https_server(net::EmbeddedTestServer::TYPE_HTTPS);
+  https_server.SetSSLConfig(net::EmbeddedTestServer::CERT_EXPIRED);
+  https_server.ServeFilesFromSourceDirectory("headless/test/data");
+  ASSERT_TRUE(https_server.Start());
+  GURL test_url = https_server.GetURL("/hello.html");
+
+  base::CommandLine::ForCurrentProcess()->AppendSwitch(
+      switches::kAllowInsecureLocalhost);
+
+  HeadlessBrowserContext* browser_context =
+      browser()->CreateBrowserContextBuilder().Build();
+
+  HeadlessWebContentsImpl* web_contents =
+      HeadlessWebContentsImpl::From(browser_context->CreateWebContentsBuilder()
+                                        .SetInitialURL(test_url)
+                                        .Build());
+
+  // If the certificate fails to validate, this should fail.
+  EXPECT_TRUE(WaitForLoad(web_contents));
 }
 
 }  // namespace headless
