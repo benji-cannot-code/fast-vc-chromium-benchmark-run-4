5641e7304e004b71fba5bcafa498aecab56fb01d
Dmitry Gozman <dgozman@chromium.org>
2017-08-03 19:46:26 +0000

    [DevTools] Unflake inspector-protocol/network/disable-interception-midway.js layout test
    BUG=734762,745345
    
    Change-Id: Ia9fa22f8259eb50281e1fd9087162e3c087df052
    Reviewed-on: https://chromium-review.googlesource.com/597007
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491819}

--- third_party/WebKit/LayoutTests/http/tests/inspector-protocol/network/disable-interception-midway.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector-protocol/network/disable-interception-midway.js
@@ -6,28 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   var InterceptionHelper = await testRunner.loadScript('../resources/interception-test.js');
   var helper = new InterceptionHelper(testRunner, session);
 
-  var numRequests = 0;
-  var maybeDisableRequestInterception = function(event) {
-    numRequests++;
-    // To make this test non-flaky wait until the first three requests have
-    // been made before disabling.  We can't wait for all for because the
-    // scripts are blocking.
-    if (numRequests === 3)
-      helper.disableRequestInterception(event);
-  };
-
   var requestInterceptedDict = {
-      'resource-iframe.html': event => helper.allowRequest(event),
-      'i-dont-exist.css': maybeDisableRequestInterception,
-      'script.js': maybeDisableRequestInterception,
-      'script2.js': maybeDisableRequestInterception,
-      'post-echo.pl': maybeDisableRequestInterception,
+      'disable-iframe.html': event => helper.allowRequest(event),
+      'i-dont-exist.css': event => helper.disableRequestInterception(event),
+      'post-echo.pl': event => helper.allowRequest(event),
   };
 
   await helper.startInterceptionTest(requestInterceptedDict, 1);
   session.evaluate(`
     var iframe = document.createElement('iframe');
-    iframe.src = '${testRunner.url('./resources/resource-iframe.html')}';
+    iframe.src = '${testRunner.url('./resources/disable-iframe.html')}';
     document.body.appendChild(iframe);
   `);
 })
