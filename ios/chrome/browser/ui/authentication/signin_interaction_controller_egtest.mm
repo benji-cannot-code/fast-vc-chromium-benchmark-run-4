31eb0187189e291bc5cdc53158508e3be9ffdd6e
Jérôme Lebel <jlebel@chromium.org>
2017-08-03 16:27:11 +0000

    Enabling sign-in promo view earlgrey tests
    Bug: 661794
    Change-Id: I9e9844e73583288581e9700228708edc39dfd580
    Reviewed-on: https://chromium-review.googlesource.com/561147
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Eric Noyau <noyau@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491761}

--- ios/chrome/browser/ui/authentication/signin_interaction_controller_egtest.mm
+++ ios/chrome/browser/ui/authentication/signin_interaction_controller_egtest.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/browser/signin_manager.h"
 #include "ios/chrome/browser/experimental_flags.h"
 #include "ios/chrome/browser/signin/signin_manager_factory.h"
+#import "ios/chrome/browser/ui/authentication/signin_promo_view.h"
 #import "ios/chrome/browser/ui/commands/open_url_command.h"
 #import "ios/chrome/browser/ui/ntp/new_tab_page_controller.h"
 #import "ios/chrome/browser/ui/settings/accounts_collection_view_controller.h"
@@ -92,7 +93,7 @@ void TapButtonWithLabelId(int message_id) {
 // User must not be signed in.
 void OpenSignInFromSettings() {
   [ChromeEarlGreyUI openSettingsMenu];
-  TapViewWithAccessibilityId(kSettingsSignInCellId);
+  TapViewWithAccessibilityId(kSigninPromoSecondaryButtonId);
 }
 
 // Wait until |matcher| is accessible (not nil)
@@ -333,7 +334,8 @@ void AssertAuthenticatedIdentityInActiveProfile(ChromeIdentity* identity) {
   TapButtonWithLabelId(IDS_IOS_DISCONNECT_DIALOG_CONTINUE_BUTTON_MOBILE);
 
   // Check that the settings home screen is shown.
-  WaitForMatcher(grey_accessibilityID(kSettingsSignInCellId));
+  WaitForMatcher(grey_allOf(grey_accessibilityID(kSigninPromoSecondaryButtonId),
+                            grey_sufficientlyVisible(), nil));
 
   [[EarlGrey selectElementWithMatcher:NavigationBarDoneButton()]
       performAction:grey_tap()];
@@ -383,7 +385,8 @@ void AssertAuthenticatedIdentityInActiveProfile(ChromeIdentity* identity) {
   TapButtonWithLabelId(IDS_IOS_MANAGED_DISCONNECT_DIALOG_ACCEPT);
 
   // Check that the settings home screen is shown.
-  WaitForMatcher(grey_accessibilityID(kSettingsSignInCellId));
+  WaitForMatcher(grey_allOf(grey_accessibilityID(kSigninPromoSecondaryButtonId),
+                            grey_sufficientlyVisible(), nil));
 
   [[EarlGrey selectElementWithMatcher:NavigationBarDoneButton()]
       performAction:grey_tap()];
@@ -534,8 +537,9 @@ void AssertAuthenticatedIdentityInActiveProfile(ChromeIdentity* identity) {
   AssertAuthenticatedIdentityInActiveProfile(nil);
 
   // Start sign-in with |identity1|.
-  WaitForMatcher(grey_accessibilityID(kSettingsSignInCellId));
-  TapViewWithAccessibilityId(kSettingsSignInCellId);
+  WaitForMatcher(grey_allOf(grey_accessibilityID(kSigninPromoSecondaryButtonId),
+                            grey_sufficientlyVisible(), nil));
+  TapViewWithAccessibilityId(kSigninPromoSecondaryButtonId);
   TapButtonWithAccessibilityLabel(identity1.userEmail);
   TapButtonWithLabelId(IDS_IOS_ACCOUNT_CONSISTENCY_SETUP_SIGNIN_BUTTON);
 
@@ -592,7 +596,7 @@ void AssertAuthenticatedIdentityInActiveProfile(ChromeIdentity* identity) {
   [[EarlGrey selectElementWithMatcher:all_bookmarks_matcher]
       performAction:grey_tap()];
 
-  TapButtonWithLabelId(IDS_IOS_BOOKMARK_PROMO_SIGN_IN_BUTTON);
+  TapViewWithAccessibilityId(kSigninPromoSecondaryButtonId);
 
   // Assert sign-in screen was shown.
   id<GREYMatcher> signin_matcher =
@@ -621,7 +625,7 @@ void AssertAuthenticatedIdentityInActiveProfile(ChromeIdentity* identity) {
   }
   [[EarlGrey selectElementWithMatcher:all_bookmarks_matcher]
       performAction:grey_tap()];
-  TapButtonWithLabelId(IDS_IOS_BOOKMARK_PROMO_SIGN_IN_BUTTON);
+  TapViewWithAccessibilityId(kSigninPromoSecondaryButtonId);
   [[EarlGrey selectElementWithMatcher:signin_matcher]
       assertWithMatcher:grey_sufficientlyVisible()];
 
