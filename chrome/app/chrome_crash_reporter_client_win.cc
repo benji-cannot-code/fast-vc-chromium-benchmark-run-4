4a690355cacc3a7bc793989abb058ed7d8dc7721
Sigurdur Asgeirsson <siggi@chromium.org>
2017-08-03 17:54:55 +0000

    Re-land Stability instrumentation Crashpad integration
    This is copy of https://codereview.chromium.org/2867063002 with all
    the install_static refactoring removed to avoid bug:748949.
    
    Bug: 718437
    TBR: alexclarke@chromium.org
    Cq-Include-Trybots: master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    Change-Id: I3341f9af6a73dd83fc631bbde5f5b0473ff9de55
    Reviewed-on: https://chromium-review.googlesource.com/591827
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Pierre-Antoine Manzagol (departed) <manzagop@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491787}

--- chrome/app/chrome_crash_reporter_client_win.cc
+++ chrome/app/chrome_crash_reporter_client_win.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <iterator>
 #include <memory>
 #include <string>
+#include <vector>
 
 #include "base/command_line.h"
 #include "base/debug/crash_logging.h"
@@ -239,15 +240,22 @@ void ChromeCrashReporterClient::InitializeCrashReportingForProcess() {
   instance = new ChromeCrashReporterClient();
   ANNOTATE_LEAKING_OBJECT_PTR(instance);
 
-  std::wstring process_type = install_static::GetSwitchValueFromCommandLine(
+  base::string16 process_type = install_static::GetSwitchValueFromCommandLine(
       ::GetCommandLine(), install_static::kProcessType);
   // Don't set up Crashpad crash reporting in the Crashpad handler itself, nor
   // in the fallback crash handler for the Crashpad handler process.
   if (process_type != install_static::kCrashpadHandler &&
       process_type != install_static::kFallbackHandler) {
     crash_reporter::SetCrashReporterClient(instance);
+
+    // Pass along the user data directory from the browser process.
+    base::string16 user_data_dir;
+    if (process_type.empty())
+      install_static::GetUserDataDirectory(&user_data_dir, nullptr);
+
     crash_reporter::InitializeCrashpadWithEmbeddedHandler(
-        process_type.empty(), install_static::UTF16ToUTF8(process_type));
+        process_type.empty(), install_static::UTF16ToUTF8(process_type),
+        install_static::UTF16ToUTF8(user_data_dir));
   }
 }
 #endif  // NACL_WIN64
@@ -355,12 +363,13 @@ bool ChromeCrashReporterClient::GetCrashDumpLocation(
     return true;
 
   *crash_dir = install_static::GetCrashDumpLocation();
-  return true;
+  return !crash_dir->empty();
 }
 
 bool ChromeCrashReporterClient::GetCrashMetricsLocation(
     base::string16* metrics_dir) {
-  return install_static::GetUserDataDirectory(metrics_dir, nullptr);
+  install_static::GetUserDataDirectory(metrics_dir, nullptr);
+  return !metrics_dir->empty();
 }
 
 // TODO(ananta)
