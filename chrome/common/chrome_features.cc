afc53e7056a2fbedaddb941e570eba536d603cf2
Luis Rabasa <erickzul@google.com>
2017-08-03 01:25:16 +0000

    Add a feature flag for Improved Fullscreen Content.
    Bug: 588114
    Change-Id: I1a4c0da04a5d3b8353b7e3f80f128799bc159d4e
    Reviewed-on: https://chromium-review.googlesource.com/549135
    Commit-Queue: Luis Rabasa <erickzul@google.com>
    Reviewed-by: Sidney San Martin <sdy@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491595}

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_features.h"
 
 #include "base/command_line.h"
+#include "build/build_config.h"
 #include "chrome/common/chrome_switches.h"
 #include "extensions/features/features.h"
 #include "ppapi/features/features.h"
@@ -136,6 +137,11 @@ const base::Feature kClickToOpenPDFPlaceholder{
 // search engine's search page consistent.
 const base::Feature kConsistentOmniboxGeolocation{
     "ConsistentOmniboxGeolocation", base::FEATURE_ENABLED_BY_DEFAULT};
+#endif
+
+#if defined(OS_MACOSX)
+const base::Feature kContentFullscreen{"ContentFullscreen",
+                                       base::FEATURE_DISABLED_BY_DEFAULT};
 #endif
 
 #if defined(OS_ANDROID)
