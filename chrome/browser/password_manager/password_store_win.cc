61b1c227962f5fb5e14b327f28116e3260679a97
Vasilii Sukhanov <vasilii@chromium.org>
2017-08-03 16:01:14 +0000

    Reland "Move the task runner creation to password store from the factory."
    This is a reland of d95936bdef4d430e20500a14d778fa0c8ff0ac7a
    Original change's description:
    > Move the task runner creation to password store from the factory.
    > 
    > In addition the CL changes some tests to use ScopedTaskEnvironment where appropriate instead of MessageLoop.
    > 
    > Bug: 741660
    > Change-Id: I9993eaba6bd92e30dfe4a39a5fb89518d5ba4a3d
    > Reviewed-on: https://chromium-review.googlesource.com/584908
    > Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    > Reviewed-by: Christos Froussios <cfroussios@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#491693}
    
    Bug: 741660
    Change-Id: I9ef1b4304f69dbf6fb3dc98dcec5843e92fc6371
    Reviewed-on: https://chromium-review.googlesource.com/600207
    Reviewed-by: Christos Froussios <cfroussios@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491755}

--- chrome/browser/password_manager/password_store_win.cc
+++ chrome/browser/password_manager/password_store_win.cc
@@ -86,7 +86,7 @@ class PasswordStoreWin::DBHandler : public WebDataServiceConsumer {
 
 PasswordStoreWin::DBHandler::~DBHandler() {
   DCHECK(
-      password_store_->GetBackgroundTaskRunner()->RunsTasksInCurrentSequence());
+      password_store_->background_task_runner()->RunsTasksInCurrentSequence());
   for (PendingRequestMap::const_iterator i = pending_requests_.begin();
        i != pending_requests_.end();
        ++i) {
@@ -98,7 +98,7 @@ void PasswordStoreWin::DBHandler::GetIE7Login(
     const PasswordStore::FormDigest& form,
     const ResultCallback& result_callback) {
   DCHECK(
-      password_store_->GetBackgroundTaskRunner()->RunsTasksInCurrentSequence());
+      password_store_->background_task_runner()->RunsTasksInCurrentSequence());
   IE7PasswordInfo info;
   info.url_hash =
       ie7_password::GetUrlHash(base::UTF8ToWide(form.origin.spec()));
@@ -113,7 +113,7 @@ PasswordStoreWin::DBHandler::GetIE7Results(
     const WDTypedResult* result,
     const PasswordStore::FormDigest& form) {
   DCHECK(
-      password_store_->GetBackgroundTaskRunner()->RunsTasksInCurrentSequence());
+      password_store_->background_task_runner()->RunsTasksInCurrentSequence());
   std::vector<std::unique_ptr<PasswordForm>> matched_forms;
   const WDResult<IE7PasswordInfo>* r =
       static_cast<const WDResult<IE7PasswordInfo>*>(result);
@@ -158,7 +158,7 @@ void PasswordStoreWin::DBHandler::OnWebDataServiceRequestDone(
           "422460 PasswordStoreWin::DBHandler::OnWebDataServiceRequestDone"));
 
   DCHECK(
-      password_store_->GetBackgroundTaskRunner()->RunsTasksInCurrentSequence());
+      password_store_->background_task_runner()->RunsTasksInCurrentSequence());
 
   PendingRequestMap::iterator i = pending_requests_.find(handle);
   DCHECK(i != pending_requests_.end());
@@ -186,13 +186,9 @@ void PasswordStoreWin::DBHandler::OnWebDataServiceRequestDone(
 }
 
 PasswordStoreWin::PasswordStoreWin(
-    scoped_refptr<base::SequencedTaskRunner> main_thread_runner,
-    scoped_refptr<base::SequencedTaskRunner> background_thread_runner,
     std::unique_ptr<password_manager::LoginDatabase> login_db,
     const scoped_refptr<PasswordWebDataService>& web_data_service)
-    : PasswordStoreDefault(main_thread_runner,
-                           background_thread_runner,
-                           std::move(login_db)) {
+    : PasswordStoreDefault(std::move(login_db)) {
   db_handler_.reset(new DBHandler(web_data_service, this));
 }
 
@@ -200,12 +196,12 @@ PasswordStoreWin::~PasswordStoreWin() {
 }
 
 void PasswordStoreWin::ShutdownOnBackgroundThread() {
-  DCHECK(GetBackgroundTaskRunner()->RunsTasksInCurrentSequence());
+  DCHECK(background_task_runner()->RunsTasksInCurrentSequence());
   db_handler_.reset();
 }
 
 void PasswordStoreWin::ShutdownOnUIThread() {
-  GetBackgroundTaskRunner()->PostTask(
+  background_task_runner()->PostTask(
       FROM_HERE,
       base::Bind(&PasswordStoreWin::ShutdownOnBackgroundThread, this));
   PasswordStoreDefault::ShutdownOnUIThread();
--- components/password_manager/core/browser/password_store_default_unittest.cc
+++ components/password_manager/core/browser/password_store_default_unittest.cc
@@ -8,16 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
-#include "base/bind_helpers.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
-#include "base/run_loop.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/scoped_task_environment.h"
-#include "base/threading/sequenced_task_runner_handle.h"
 #include "base/time/time.h"
 #include "components/password_manager/core/browser/login_database.h"
 #include "components/password_manager/core/browser/password_manager_test_utils.h"
@@ -87,7 +84,7 @@ class PasswordStoreDefaultTestDelegate {
 
   PasswordStoreDefault* store() { return store_.get(); }
 
-  static void FinishAsyncProcessing();
+  void FinishAsyncProcessing();
 
  private:
   void SetupTempDir();
@@ -127,7 +124,7 @@ PasswordStoreDefaultTestDelegate::~PasswordStoreDefaultTestDelegate() {
 }
 
 void PasswordStoreDefaultTestDelegate::FinishAsyncProcessing() {
-  base::RunLoop().RunUntilIdle();
+  scoped_task_environment_.RunUntilIdle();
 }
 
 void PasswordStoreDefaultTestDelegate::SetupTempDir() {
@@ -136,16 +133,15 @@ void PasswordStoreDefaultTestDelegate::SetupTempDir() {
 
 void PasswordStoreDefaultTestDelegate::ClosePasswordStore() {
   store_->ShutdownOnUIThread();
-  base::RunLoop().RunUntilIdle();
+  FinishAsyncProcessing();
   ASSERT_TRUE(temp_dir_.Delete());
 }
 
 scoped_refptr<PasswordStoreDefault>
 PasswordStoreDefaultTestDelegate::CreateInitializedStore(
     std::unique_ptr<LoginDatabase> database) {
-  scoped_refptr<PasswordStoreDefault> store(new PasswordStoreDefault(
-      base::SequencedTaskRunnerHandle::Get(),
-      base::SequencedTaskRunnerHandle::Get(), std::move(database)));
+  scoped_refptr<PasswordStoreDefault> store(
+      new PasswordStoreDefault(std::move(database)));
   store->Init(syncer::SyncableService::StartSyncFlare(), nullptr);
 
   return store;
@@ -180,8 +176,6 @@ TEST(PasswordStoreDefaultTest, NonASCIIData) {
     store->AddLogin(*expected_forms.back());
   }
 
-  base::RunLoop().RunUntilIdle();
-
   MockPasswordStoreConsumer consumer;
 
   // We expect to get the same data back, even though it's not all ASCII.
@@ -191,7 +185,7 @@ TEST(PasswordStoreDefaultTest, NonASCIIData) {
           password_manager::UnorderedPasswordFormElementsAre(&expected_forms)));
   store->GetAutofillableLogins(&consumer);
 
-  base::RunLoop().RunUntilIdle();
+  delegate.FinishAsyncProcessing();
 }
 
 TEST(PasswordStoreDefaultTest, Notifications) {
@@ -213,7 +207,7 @@ TEST(PasswordStoreDefaultTest, Notifications) {
 
   // Adding a login should trigger a notification.
   store->AddLogin(*form);
-  base::RunLoop().RunUntilIdle();
+  delegate.FinishAsyncProcessing();
 
   // Change the password.
   form->password_value = base::ASCIIToUTF16("a different password");
@@ -227,7 +221,7 @@ TEST(PasswordStoreDefaultTest, Notifications) {
 
   // Updating the login with the new password should trigger a notification.
   store->UpdateLogin(*form);
-  base::RunLoop().RunUntilIdle();
+  delegate.FinishAsyncProcessing();
 
   const PasswordStoreChange expected_delete_changes[] = {
       PasswordStoreChange(PasswordStoreChange::REMOVE, *form),
@@ -238,7 +232,7 @@ TEST(PasswordStoreDefaultTest, Notifications) {
 
   // Deleting the login should trigger a notification.
   store->RemoveLogin(*form);
-  base::RunLoop().RunUntilIdle();
+  delegate.FinishAsyncProcessing();
 
   store->RemoveObserver(&observer);
 }
@@ -248,9 +242,9 @@ TEST(PasswordStoreDefaultTest, Notifications) {
 // notifications.
 TEST(PasswordStoreDefaultTest, OperationsOnABadDatabaseSilentlyFail) {
   PasswordStoreDefaultTestDelegate delegate(
-      base::WrapUnique(new BadLoginDatabase));
+      base::MakeUnique<BadLoginDatabase>());
   PasswordStoreDefault* bad_store = delegate.store();
-  base::RunLoop().RunUntilIdle();
+  delegate.FinishAsyncProcessing();
   ASSERT_EQ(nullptr, bad_store->login_db());
 
   testing::StrictMock<MockPasswordStoreObserver> mock_observer;
@@ -266,41 +260,42 @@ TEST(PasswordStoreDefaultTest, OperationsOnABadDatabaseSilentlyFail) {
   blacklisted_form->blacklisted_by_user = true;
   bad_store->AddLogin(*form);
   bad_store->AddLogin(*blacklisted_form);
-  base::RunLoop().RunUntilIdle();
+  delegate.FinishAsyncProcessing();
 
   // Get all logins; autofillable logins; blacklisted logins.
   testing::StrictMock<MockPasswordStoreConsumer> mock_consumer;
   EXPECT_CALL(mock_consumer, OnGetPasswordStoreResultsConstRef(IsEmpty()));
   bad_store->GetLogins(PasswordStore::FormDigest(*form), &mock_consumer);
-  base::RunLoop().RunUntilIdle();
+  delegate.FinishAsyncProcessing();
   testing::Mock::VerifyAndClearExpectations(&mock_consumer);
   EXPECT_CALL(mock_consumer, OnGetPasswordStoreResultsConstRef(IsEmpty()));
   bad_store->GetAutofillableLogins(&mock_consumer);
-  base::RunLoop().RunUntilIdle();
+  delegate.FinishAsyncProcessing();
   testing::Mock::VerifyAndClearExpectations(&mock_consumer);
   EXPECT_CALL(mock_consumer, OnGetPasswordStoreResultsConstRef(IsEmpty()));
   bad_store->GetBlacklistLogins(&mock_consumer);
-  base::RunLoop().RunUntilIdle();
+  delegate.FinishAsyncProcessing();
+  testing::Mock::VerifyAndClearExpectations(&mock_consumer);
 
   // Report metrics.
   bad_store->ReportMetrics("Test Username", true);
-  base::RunLoop().RunUntilIdle();
+  delegate.FinishAsyncProcessing();
 
   // Change the login.
   form->password_value = base::ASCIIToUTF16("a different password");
   bad_store->UpdateLogin(*form);
-  base::RunLoop().RunUntilIdle();
+  delegate.FinishAsyncProcessing();
 
   // Delete one login; a range of logins.
   bad_store->RemoveLogin(*form);
-  base::RunLoop().RunUntilIdle();
+  delegate.FinishAsyncProcessing();
   base::RunLoop run_loop;
   bad_store->RemoveLoginsCreatedBetween(base::Time(), base::Time::Max(),
                                         run_loop.QuitClosure());
   run_loop.Run();
 
   bad_store->RemoveLoginsSyncedBetween(base::Time(), base::Time::Max());
-  base::RunLoop().RunUntilIdle();
+  delegate.FinishAsyncProcessing();
 
   // Ensure no notifications and no explosions during shutdown either.
   bad_store->RemoveObserver(&mock_observer);
