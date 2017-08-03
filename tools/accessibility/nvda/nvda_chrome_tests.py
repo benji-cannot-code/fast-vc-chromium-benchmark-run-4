158c8d6e94f2a2206bad2d4c097a9f584fd826e3
Doug Turner <dougt@chromium.org>
2017-08-03 08:50:49 +0000

    Fix up nvda_chrome_tests.py to run under Python 3.
    See "Print Is A Function".
    
    Bug: 742592
    Change-Id: Ic2744a01a4149a466dab9a7c1208d36238206008
    Reviewed-on: https://chromium-review.googlesource.com/599567
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Commit-Queue: Doug Turner <dougt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491681}

--- tools/accessibility/nvda/nvda_chrome_tests.py
+++ tools/accessibility/nvda/nvda_chrome_tests.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/env python
-# Copyright 2014 The Chromium Authors. All rights reserved.
+# Copyright 2017 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -53,13 +53,13 @@ WAIT_FOR_SPEECH_TIMEOUT_SECS = 3.0
 class NvdaChromeTest(unittest.TestCase):
   @classmethod
   def setUpClass(cls):
-    print 'user data: %s' % CHROME_PROFILES_PATH
-    print 'chrome: %s' % CHROME_PATH
-    print 'nvda: %s' % NVDA_PATH
-    print 'nvda_proctest: %s' % NVDA_PROCTEST_PATH
+    print('user data: %s' % CHROME_PROFILES_PATH)
+    print('chrome: %s' % CHROME_PATH)
+    print('nvda: %s' % NVDA_PATH)
+    print('nvda_proctest: %s' % NVDA_PROCTEST_PATH)
 
-    print
-    print 'Clearing user data directory and log file from previous runs'
+    print()
+    print('Clearing user data directory and log file from previous runs')
     if os.access(NVDA_LOGPATH, os.F_OK):
       os.remove(NVDA_LOGPATH)
     if os.access(CHROME_PROFILES_PATH, os.F_OK):
@@ -67,7 +67,7 @@ class NvdaChromeTest(unittest.TestCase):
     os.mkdir(CHROME_PROFILES_PATH, 0777)
 
     def handler(signum, frame):
-      print 'Test interrupted, attempting to kill subprocesses.'
+      print('Test interrupted, attempting to kill subprocesses.')
       self.tearDown()
       sys.exit()
     signal.signal(signal.SIGINT, handler)
@@ -78,16 +78,16 @@ class NvdaChromeTest(unittest.TestCase):
             '--user-data-dir=%s' % user_data_dir,
             '--no-first-run',
             'about:blank']
-    print
-    print ' '.join(args)
+    print()
+    print(' '.join(args))
     self._chrome_proc = subprocess.Popen(args)
     self._chrome_proc.poll()
     if self._chrome_proc.returncode is None:
-      print 'Chrome is running'
+      print('Chrome is running')
     else:
-      print 'Chrome exited with code', self._chrome_proc.returncode
+      print('Chrome exited with code', self._chrome_proc.returncode)
       sys.exit()
-    print 'Chrome pid: %d' % self._chrome_proc.pid
+    print('Chrome pid: %d' % self._chrome_proc.pid)
 
     os.environ['NVDA_SPECIFIC_PROCESS'] = str(self._chrome_proc.pid)
 
@@ -100,11 +100,11 @@ class NvdaChromeTest(unittest.TestCase):
     self._nvda_proc = subprocess.Popen(args)
     self._nvda_proc.poll()
     if self._nvda_proc.returncode is None:
-      print 'NVDA is running'
+      print('NVDA is running')
     else:
-      print 'NVDA exited with code', self._nvda_proc.returncode
+      print('NVDA exited with code', self._nvda_proc.returncode)
       sys.exit()
-    print 'NVDA pid: %d' % self._nvda_proc.pid
+    print('NVDA pid: %d' % self._nvda_proc.pid)
 
     app = pywinauto.application.Application()
     app.connect_(process = self._chrome_proc.pid)
@@ -116,22 +116,22 @@ class NvdaChromeTest(unittest.TestCase):
       self.tearDown()
 
   def tearDown(self):
-    print
-    print 'Shutting down'
+    print()
+    print('Shutting down')
 
     self._chrome_proc.poll()
     if self._chrome_proc.returncode is None:
-      print 'Killing Chrome subprocess'
+      print('Killing Chrome subprocess')
       self._chrome_proc.kill()
     else:
-      print 'Chrome already died.'
+      print('Chrome already died.')
 
     self._nvda_proc.poll()
     if self._nvda_proc.returncode is None:
-      print 'Killing NVDA subprocess'
+      print('Killing NVDA subprocess')
       self._nvda_proc.kill()
     else:
-      print 'NVDA already died.'
+      print('NVDA already died.')
 
   def _GetSpeechFromNvdaLogFile(self):
     """Return everything NVDA would have spoken as a list of strings.
@@ -177,12 +177,12 @@ class NvdaChromeTest(unittest.TestCase):
         break
 
       if time.time() - start_time >= WAIT_FOR_SPEECH_TIMEOUT_SECS:
-        print '** Speech from NVDA so far:'
+        print('** Speech from NVDA so far:')
         for line in lines:
-          print '"%s"' % line
-        print '** Was waiting for:'
+          print('"%s"' % line)
+        print('** Was waiting for:')
         for line in expected:
-          print '"%s"' % line
+          print('"%s"' % line)
         raise Exception('Timed out')
       time.sleep(0.1)
 
