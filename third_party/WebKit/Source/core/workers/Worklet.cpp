ce17f3d2d2312ae127e604e2339a34acc0766b11
Xida Chen <xidachen@chromium.org>
2017-08-03 15:39:26 +0000

    Implement switching global scope in paint worklet
    In this previous CL: https://chromium-review.googlesource.com/c/562197/,
    we introduced the concept of document paint definition, such that we
    can have two paint worklet global scopes. However, we didn't Implement
    the logic to switch between these two global scopes.
    
    This CL implements the logic. In particular, after every 120 frames that
    has called the paint function, we switch to use another global scope.
    
    Bug: 728595
    Change-Id: Ie48a7e597deb10342ede64c42e595c0b11d779f1
    Reviewed-on: https://chromium-review.googlesource.com/593633
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491747}

--- third_party/WebKit/Source/core/workers/Worklet.cpp
+++ third_party/WebKit/Source/core/workers/Worklet.cpp
@@ -96,8 +96,7 @@ void Worklet::ContextDestroyed(ExecutionContext* execution_context) {
 
 WorkletGlobalScopeProxy* Worklet::FindAvailableGlobalScope() const {
   DCHECK(IsMainThread());
-  // TODO(nhiroki): Support the case where there are multiple global scopes.
-  return proxies_.begin()->Get();
+  return proxies_.at(SelectGlobalScope());
 }
 
 // Implementation of the second half of the "addModule(moduleURL, options)"
@@ -157,6 +156,11 @@ void Worklet::FetchAndInvokeScript(const KURL& module_url_record,
                                 credentials_mode, outside_settings_task_runner,
                                 pending_tasks);
   }
+}
+
+size_t Worklet::SelectGlobalScope() const {
+  DCHECK_EQ(GetNumberOfGlobalScopes(), 1u);
+  return 0u;
 }
 
 DEFINE_TRACE(Worklet) {
