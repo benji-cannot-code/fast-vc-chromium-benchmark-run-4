fcbbd7f5cc62b80f1571982cf3ef2b7cd5a0e301
David Benjamin <davidben@chromium.org>
2017-08-03 20:40:30 +0000

    Remove some stray includes.
    net/cert including net/ssl would be a circular dependency, but these
    includes aren't actually used.
    
    Bug: none
    Change-Id: I47ee0f9e3ccdca07b6127ba653798aeae0084ffa
    Reviewed-on: https://chromium-review.googlesource.com/578498
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491833}

--- net/cert/cert_verify_proc_ios.cc
+++ net/cert/cert_verify_proc_ios.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cert/x509_certificate.h"
 #include "net/cert/x509_util_ios.h"
 #include "net/cert/x509_util_ios_and_mac.h"
-#include "net/ssl/openssl_ssl_util.h"
 
 using base::ScopedCFTypeRef;
 
--- net/cert/x509_certificate_ios.cc
+++ net/cert/x509_certificate_ios.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/ip_address.h"
 #include "net/cert/x509_util_ios.h"
 #include "net/cert/x509_util_openssl.h"
-#include "net/ssl/openssl_ssl_util.h"
 #include "third_party/boringssl/src/include/openssl/x509.h"
 #include "third_party/boringssl/src/include/openssl/x509v3.h"
 
