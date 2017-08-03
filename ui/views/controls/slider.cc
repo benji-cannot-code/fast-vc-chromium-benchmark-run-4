67b60077c07a32753e32c05744ff896a6f0778fc
Evan Stade <estade@chromium.org>
2017-08-03 02:29:28 +0000

    Make views::ToggleButton use the ink drop ripple (not higlight) for focus.
    This matches Polymer behavior and is preferential to the current focus
    effect (blue rectangle).
    
    Also get rid of the the focus rect for views::Slider, which already has
    the glow effect when focused and hence needs no further changes.
    
    Bug: 658783
    Change-Id: Id9d5e3f1434b0d444154be933f0067f97aaaed33
    Reviewed-on: https://chromium-review.googlesource.com/592494
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Valery Arkhangorodsky <varkha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491620}

--- ui/views/controls/slider.cc
+++ ui/views/controls/slider.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/rect.h"
-#include "ui/native_theme/native_theme.h"
 #include "ui/views/widget/widget.h"
 
 namespace {
@@ -185,21 +184,6 @@ void Slider::MoveButtonTo(const gfx::Point& point) {
       VALUE_CHANGED_BY_USER);
 }
 
-void Slider::OnPaintFocus(gfx::Canvas* canvas) {
-  if (!HasFocus())
-    return;
-
-  // TODO(estade): make this a glow effect instead: crbug.com/658783
-  gfx::Rect focus_bounds = GetLocalBounds();
-  focus_bounds.Inset(gfx::Insets(1));
-  canvas->DrawSolidFocusRect(
-      gfx::RectF(focus_bounds),
-      SkColorSetA(GetNativeTheme()->GetSystemColor(
-                      ui::NativeTheme::kColorId_FocusedBorderColor),
-                  0x99),
-      2);
-}
-
 void Slider::OnSliderDragStarted() {
   SetHighlighted(true);
   if (listener_)
@@ -322,8 +306,6 @@ void Slider::OnPaint(gfx::Canvas* canvas) {
   canvas->DrawCircle(
       thumb_center,
       is_active_ ? kThumbRadius : (kThumbRadius - kThumbStroke / 2), flags);
-
-  OnPaintFocus(canvas);
 }
 
 void Slider::OnFocus() {
