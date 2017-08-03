4bd67f5aea6910981934179afe08c986a45607ad
Tommy Martino <tmartino@chromium.org>
2017-08-03 15:53:58 +0000

    [WebPayments] Clicking selected row goes back
    Changing behavior when clicking on the currently selected item in an
    ItemList. Was a no-op; now goes back to the main sheet.
    
    Bug: 748552
    Change-Id: I43f716145c059b6d95d5f01526b1224528fb0ad3
    Reviewed-on: https://chromium-review.googlesource.com/585370
    Reviewed-by: Anthony Vallee-Dubois <anthonyvd@chromium.org>
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491751}

--- chrome/browser/ui/views/payments/payment_request_item_list.h
+++ chrome/browser/ui/views/payments/payment_request_item_list.h
@@ -20,6 +20,7 @@ class View;
 
 namespace payments {
 
+class PaymentRequestDialogView;
 class PaymentRequestSpec;
 class PaymentRequestState;
 
@@ -124,7 +125,7 @@ class PaymentRequestItemList {
     DISALLOW_COPY_AND_ASSIGN(Item);
   };
 
-  PaymentRequestItemList();
+  explicit PaymentRequestItemList(PaymentRequestDialogView* dialog);
   virtual ~PaymentRequestItemList();
 
   // Adds an item to this list. |item->list()| should return this object.
@@ -141,6 +142,8 @@ class PaymentRequestItemList {
   // Deselects the currently selected item and selects |item| instead.
   void SelectItem(Item* item);
 
+  PaymentRequestDialogView* dialog() { return dialog_; }
+
  private:
   // Unselects the currently selected item. This is private so that the list can
   // use it when selecting a new item while avoiding consumers of this class
@@ -149,6 +152,7 @@ class PaymentRequestItemList {
 
   std::vector<std::unique_ptr<Item>> items_;
   Item* selected_item_;
+  PaymentRequestDialogView* dialog_;
 
   DISALLOW_COPY_AND_ASSIGN(PaymentRequestItemList);
 };
