2b8dc14b79e38c66677f0b7154dad537e55dceb0
Danyao Wang <danyao@google.com>
2017-08-03 15:43:33 +0000

    [Nav Experiment] Enable NavigationManagerTest for WK-based nav manager.
    This uncovered a bug where GetLastCommittedItemIndex() returned the
    incorrect value because it assumes WKBackForwardList's |currentItem|
    is always the last committed item. This is not the case if the pending
    navigation is a back-forward navigation. It turned out that this can't
    be fixed 100%, but implemented a heurstic and documented the edge case.
    
    Bug: 734150
    Change-Id: Ie2c026a2c5231cbd4151c106cd0db664dcf6d587
    Reviewed-on: https://chromium-review.googlesource.com/596828
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Danyao Wang <danyao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491749}

--- ios/web/test/fakes/crw_test_back_forward_list.h
+++ ios/web/test/fakes/crw_test_back_forward_list.h
@@ -8,10 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @class WKBackForwardListItem;
 
 // A CRWTestBackForwardList can be used to stub out WKBackForwardList in tests.
 @interface CRWTestBackForwardList : NSObject
+
+// Returns an OCMock of WKBackForwardListItem with the given URL.
++ (WKBackForwardListItem*)itemWithURLString:(NSString*)URL;
 
 // WKBackForwardList interface
 @property(nullable, nonatomic, copy) NSArray<WKBackForwardListItem*>* backList;
@@ -28,9 +33,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Resets this instance to simulate a session with the current entry at
 // |currentItemURL|, and back and forward history entries as specified in
 // |backListURLs| and |forwardListURLs|.
-- (void)setCurrentURL:(nonnull NSString*)currentItemURL
+- (void)setCurrentURL:(NSString*)currentItemURL
          backListURLs:(nullable NSArray<NSString*>*)backListURLs
       forwardListURLs:(nullable NSArray<NSString*>*)forwardListURLs;
 @end
+
+NS_ASSUME_NONNULL_END
 
 #endif  // IOS_WEB_NAVIGATION_CRW_TEST_BACK_FORWARD_LIST_H_
--- ios/web/test/fakes/crw_test_back_forward_list.mm
+++ ios/web/test/fakes/crw_test_back_forward_list.mm
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 @interface CRWTestBackForwardList (PrivateMethods)
-- (WKBackForwardListItem*)mockItemWithURLString:(NSString*)URL;
 - (NSArray*)mockSublistWithURLArray:(NSArray<NSString*>*)URLs;
 @end
 
@@ -23,6 +22,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @synthesize backList;
 @synthesize forwardList;
 @synthesize currentItem;
+
++ (WKBackForwardListItem*)itemWithURLString:(NSString*)URL {
+  id mock = OCMClassMock([WKBackForwardListItem class]);
+  OCMStub([mock URL]).andReturn([NSURL URLWithString:URL]);
+  return mock;
+}
 
 - (WKBackForwardListItem*)itemAtIndex:(NSInteger)index {
   if (index == 0) {
@@ -42,21 +47,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)setCurrentURL:(NSString*)currentItemURL
          backListURLs:(nullable NSArray<NSString*>*)backListURLs
       forwardListURLs:(nullable NSArray<NSString*>*)forwardListURLs {
-  self.currentItem = [self mockItemWithURLString:currentItemURL];
+  self.currentItem = [CRWTestBackForwardList itemWithURLString:currentItemURL];
   self.backList = [self mockSublistWithURLArray:backListURLs];
   self.forwardList = [self mockSublistWithURLArray:forwardListURLs];
-}
-
-- (WKBackForwardListItem*)mockItemWithURLString:(NSString*)URL {
-  id mock = OCMClassMock([WKBackForwardListItem class]);
-  OCMStub([mock URL]).andReturn([NSURL URLWithString:URL]);
-  return mock;
 }
 
 - (NSArray*)mockSublistWithURLArray:(NSArray<NSString*>*)URLs {
   NSMutableArray* array = [NSMutableArray arrayWithCapacity:URLs.count];
   for (NSString* URL : URLs) {
-    [array addObject:[self mockItemWithURLString:URL]];
+    [array addObject:[CRWTestBackForwardList itemWithURLString:URL]];
   }
   return [NSArray arrayWithArray:array];
 }
