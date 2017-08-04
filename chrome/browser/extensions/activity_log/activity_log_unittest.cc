4396fd8280ae2fdff6f3b2380be501a7977fcaa0
John Mellor <johnme@chromium.org>
2017-08-04 16:51:00 +0000

    Revert "Disable prerender by default"
    This reverts commit c8c189a52acf6acc7a5dd9ee190beb0b35de2a8b.
    
    Reason for revert: broke many prerender tests
    
    All(?) prerender tests broke on official builds:
    https://build.chromium.org/p/chromium.fyi/builders/ClangToTWin%20tester/builds/8357
    
    and 3 Android tests broke:
    - chrome_public_test_apk ExternalPrerenderHandlerTest#testAddSeveralPrerenders
    - unit_tests PrerenderAdapterTest.StartPrerenderSucceeds
    - unit_tests PrerenderAdapterTest.StartPrerenderFailsForUnsupportedScheme
    on bots like https://build.chromium.org/p/chromium.swarm/builders/Android%20N5X%20Swarm/builds/1957
    
    Original change's description:
    > Disable prerender by default
    > 
    > While we are still compiling the report from the field trial, disable prerender
    > to be on the safer side in M61.
    > 
    > Bug: 678332, 735153, 520275
    > Change-Id: I30a2169533d30cbb85f53291223897caceddeb89
    > Reviewed-on: https://chromium-review.googlesource.com/584871
    > Commit-Queue: Egor Pasko <pasko@chromium.org>
    > Reviewed-by: David Roger <droger@chromium.org>
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Reviewed-by: Marc Treib <treib@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#491691}
    
    TBR=pasko@chromium.org,droger@chromium.org,reillyg@chromium.org,treib@chromium.org
    NOTRY=true
    
    Bug: 678332, 735153, 520275
    Change-Id: Ie4b4bd7c93175176c625696762e57356ff9c2413
    Reviewed-on: https://chromium-review.googlesource.com/602347
    Commit-Queue: John Mellor <johnme@chromium.org>
    Reviewed-by: John Mellor <johnme@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492051}

--- chrome/browser/extensions/activity_log/activity_log_unittest.cc
+++ chrome/browser/extensions/activity_log/activity_log_unittest.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/prerender/prerender_handle.h"
 #include "chrome/browser/prerender/prerender_manager.h"
 #include "chrome/browser/prerender/prerender_manager_factory.h"
-#include "chrome/browser/prerender/prerender_test_utils.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
@@ -244,9 +243,6 @@ TEST_F(ActivityLogTest, LogPrerender) {
   ASSERT_TRUE(GetDatabaseEnabled());
   GURL url("http://www.google.com");
 
-  prerender::test_utils::RestorePrerenderMode restore_prerender_mode;
-  prerender::PrerenderManager::SetMode(
-      prerender::PrerenderManager::PRERENDER_MODE_ENABLED);
   prerender::PrerenderManager* prerender_manager =
       prerender::PrerenderManagerFactory::GetForBrowserContext(profile());
 
