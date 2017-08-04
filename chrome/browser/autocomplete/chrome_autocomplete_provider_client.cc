97c6384e117d924514ede5524f80f6a7bd67517d
Justin Donnelly <jdonnelly@chromium.org>
2017-08-04 23:31:18 +0000

    Test disabling service worker startup when suggest is disabled.
    Actually disabling startup is done in http://crrev.com/c/598933 which is
    kept simple for merging to M61. This CL moves the test to a better
    location and tests the behavior.
    
    Bug: 751813
    Change-Id: Ic8b57af1586599fe73fe642eba759c3c1d678287
    Reviewed-on: https://chromium-review.googlesource.com/599011
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492173}

--- chrome/browser/autocomplete/chrome_autocomplete_provider_client.cc
+++ chrome/browser/autocomplete/chrome_autocomplete_provider_client.cc
@@ -300,6 +300,9 @@ void ChromeAutocompleteProviderClient::PrefetchImage(const GURL& url) {
 
 void ChromeAutocompleteProviderClient::StartServiceWorker(
     const GURL& destination_url) {
+  if (!SearchSuggestEnabled())
+    return;
+
   if (profile_->IsOffTheRecord())
     return;
 
@@ -311,9 +314,6 @@ void ChromeAutocompleteProviderClient::StartServiceWorker(
 
   content::ServiceWorkerContext* context = partition->GetServiceWorkerContext();
   if (!context)
-    return;
-
-  if (!SearchSuggestEnabled())
     return;
 
   context->StartServiceWorkerForNavigationHint(destination_url,
