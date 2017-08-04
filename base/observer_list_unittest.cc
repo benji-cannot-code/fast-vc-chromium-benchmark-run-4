72c4095ee0f1f8ba8f79a8d813697de09b2f3e14
Francois Doray <fdoray@chromium.org>
2017-08-04 14:56:22 +0000

    Remove ScopedTaskScheduler.
    ScopedTaskScheduler is deprecated. ScopedTaskEnvironment should be
    used instead.
    
    TBR=finnur@chromium.org
    
    Bug: 591697
    Cq-Include-Trybots: master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    Change-Id: Ifdb559d86985bd45cfbe1e1b72dd795a35d34313
    Reviewed-on: https://chromium-review.googlesource.com/595870
    Commit-Queue: Francois Doray <fdoray@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492031}

--- base/observer_list_unittest.cc
+++ base/observer_list_unittest.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task_scheduler/post_task.h"
 #include "base/task_scheduler/task_scheduler.h"
 #include "base/test/scoped_task_environment.h"
-#include "base/test/scoped_task_scheduler.h"
 #include "base/threading/platform_thread.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
--- chrome/browser/win/enumerate_modules_model_unittest.cc
+++ chrome/browser/win/enumerate_modules_model_unittest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/test/scoped_task_scheduler.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 typedef testing::Test EnumerateModulesTest;
