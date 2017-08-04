f18afc133fe6d5e84be6d15a01df6e4b83dea20c
Helen Li <xunjieli@chromium.org>
2017-08-04 23:57:30 +0000

    [cronet] Do not throw NPE in getResponse() when HttpURLConnection#disconnect() has been called
    When disconnect() is called on a different thread, CronetHttpURLConnection
    can throw NPE in getResponse() when it exits the message loop.
    This CL sets an IOException for the cancellation case and adds a regression
    test.
    
    Bug: 751786
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester
    Change-Id: I64bd89ecb2f5b31a2f65c3cadeb7bcc29a96f643
    Reviewed-on: https://chromium-review.googlesource.com/598772
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Commit-Queue: Paul Jensen <pauljensen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492182}

--- components/cronet/android/java/src/org/chromium/net/urlconnection/CronetHttpURLConnection.java
+++ components/cronet/android/java/src/org/chromium/net/urlconnection/CronetHttpURLConnection.java
@@ -45,7 +45,7 @@ public class CronetHttpURLConnection extends HttpURLConnection {
     private CronetInputStream mInputStream;
     private CronetOutputStream mOutputStream;
     private UrlResponseInfo mResponseInfo;
-    private CronetException mException;
+    private IOException mException;
     private boolean mOnRedirectCalled;
     // Whether response headers are received, the request is failed, or the request is canceled.
     private boolean mHasResponseHeadersOrCompleted;
@@ -488,14 +488,13 @@ public class CronetHttpURLConnection extends HttpURLConnection {
                         "Exception cannot be null in onFailed.");
             }
             mResponseInfo = info;
-            mException = exception;
-            setResponseDataCompleted(mException);
+            setResponseDataCompleted(exception);
         }
 
         @Override
         public void onCanceled(UrlRequest request, UrlResponseInfo info) {
             mResponseInfo = info;
-            setResponseDataCompleted(new IOException("stream closed"));
+            setResponseDataCompleted(new IOException("disconnect() called"));
         }
 
         /**
@@ -505,6 +504,7 @@ public class CronetHttpURLConnection extends HttpURLConnection {
          *            caller tries to read more data.
          */
         private void setResponseDataCompleted(IOException exception) {
+            mException = exception;
             if (mInputStream != null) {
                 mInputStream.setResponseDataCompleted(exception);
             }
