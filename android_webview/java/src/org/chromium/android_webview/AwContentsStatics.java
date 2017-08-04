c51bfc73f76b6be40529205943aa29e5eb827499
Gabriel Charette <gab@chromium.org>
2017-08-04 16:05:56 +0000

    Remove cleanup code from 2014 and get rid of one more call to BrowserThread::FILE
    R=torne@chromium.org
    
    Bug: 689520
    Change-Id: I376c712f7f9514ed2c1b712a0e5df2f897aad138
    Reviewed-on: https://chromium-review.googlesource.com/600288
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492044}

--- android_webview/java/src/org/chromium/android_webview/AwContentsStatics.java
+++ android_webview/java/src/org/chromium/android_webview/AwContentsStatics.java
@@ -77,10 +77,6 @@ public class AwContentsStatics {
         return sRecordFullDocument;
     }
 
-    public static void setLegacyCacheRemovalDelayForTest(long timeoutMs) {
-        nativeSetLegacyCacheRemovalDelayForTest(timeoutMs);
-    }
-
     public static String getProductVersion() {
         return nativeGetProductVersion();
     }
@@ -184,7 +180,6 @@ public class AwContentsStatics {
     //--------------------------------------------------------------------------------------------
     private static native void nativeClearClientCertPreferences(Runnable callback);
     private static native String nativeGetUnreachableWebDataUrl();
-    private static native void nativeSetLegacyCacheRemovalDelayForTest(long timeoutMs);
     private static native String nativeGetProductVersion();
     private static native void nativeSetServiceWorkerIoThreadClient(
             AwContentsIoThreadClient ioThreadClient, AwBrowserContext browserContext);
