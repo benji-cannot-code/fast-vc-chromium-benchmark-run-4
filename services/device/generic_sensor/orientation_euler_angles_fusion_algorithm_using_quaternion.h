b1b2dc6b20ea0862f7f5b61d3dd0a48072077727
Jun Cai <juncai@chromium.org>
2017-08-04 22:18:29 +0000

    Implement device orientation fusion sensors on Android
    This CL implements device orientation fusion sensors on Android. The
    following sensor fallback is used:
    
    1. For ABSOLUTE_ORIENTATION_EULER_ANGLES we use the following fallback:
       A: ABSOLUTE_ORIENTATION_QUATERNION (if it uses TYPE_ROTATION_VECTOR
    directly)
       B: Combination of ACCELEROMETER and MAGNETOMETER
    
    2. For ABSOLUTE_ORIENTATION_QUATERNION we use the following fallback:
       A: Use TYPE_ROTATION_VECTOR directly
       B: ABSOLUTE_ORIENTATION_EULER_ANGLES
    
    3. For RELATIVE_ORIENTATION_EULER_ANGLES we use the following fallback:
       A: RELATIVE_ORIENTATION_QUATERNION (if it uses
          TYPE_GAME_ROTATION_VECTOR directly)
       B: ACCELEROMETER
    
    4. For RELATIVE_ORIENTATION_QUATERNION we use the following fallback:
       A: Use TYPE_GAME_ROTATION_VECTOR directly
       B: RELATIVE_ORIENTATION_EULER_ANGLES
    
    Bug: 733707
    Change-Id: Ic201f450487b586132952cbffae064682e93a10c
    Reviewed-on: https://chromium-review.googlesource.com/585226
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492143}

---
+++ services/device/generic_sensor/orientation_euler_angles_fusion_algorithm_using_quaternion.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_DEVICE_GENERIC_SENSOR_ORIENTATION_EULER_ANGLES_FUSION_ALGORITHM_USING_QUATERNION_H_
+#define SERVICES_DEVICE_GENERIC_SENSOR_ORIENTATION_EULER_ANGLES_FUSION_ALGORITHM_USING_QUATERNION_H_
+
+#include "base/macros.h"
+#include "services/device/generic_sensor/platform_sensor_fusion_algorithm.h"
+
+namespace device {
+
+// Sensor fusion algorithm for implementing *_ORIENTATION_EULER_ANGLES
+// using *_ORIENTATION_QUATERNION.
+class OrientationEulerAnglesFusionAlgorithmUsingQuaternion
+    : public PlatformSensorFusionAlgorithm {
+ public:
+  OrientationEulerAnglesFusionAlgorithmUsingQuaternion();
+  ~OrientationEulerAnglesFusionAlgorithmUsingQuaternion() override;
+
+  bool GetFusedData(mojom::SensorType which_sensor_changed,
+                    SensorReading* fused_reading) override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(
+      OrientationEulerAnglesFusionAlgorithmUsingQuaternion);
+};
+
+}  // namespace device
+
+#endif  // SERVICES_DEVICE_GENERIC_SENSOR_ORIENTATION_EULER_ANGLES_FUSION_ALGORITHM_USING_QUATERNION_H_
