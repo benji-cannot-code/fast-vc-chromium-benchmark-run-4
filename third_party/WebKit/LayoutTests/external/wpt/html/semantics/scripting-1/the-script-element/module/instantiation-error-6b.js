1476a7c0c9f9a88308746492024a25f6c48454c3
Georg Neis <neis@chromium.org>
2017-08-04 10:02:05 +0000

    Add tests for module instantiation errors due to ambiguous exports.
    Bug: v8:6649
    Change-Id: I79555121ce72bef536ca99365cc5b12c3a6496df
    Reviewed-on: https://chromium-review.googlesource.com/590369
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491981}

---
+++ third_party/WebKit/LayoutTests/external/wpt/html/semantics/scripting-1/the-script-element/module/instantiation-error-6b.js
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+export * from "./instantiation-error-6c.js";
+export * from "./instantiation-error-6d.js";
+log.push("instantiation-error-6b");
---
+++ third_party/WebKit/LayoutTests/external/wpt/html/semantics/scripting-1/the-script-element/module/instantiation-error-7c.js
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+export * from "./instantiation-error-7d.js";
+export * from "./instantiation-error-7e.js";
+log.push("instantiation-error-7c");
