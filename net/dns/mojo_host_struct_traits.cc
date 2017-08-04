b826c869da5c695597d4e8e926797f3938e5b496
Bin Zhao <zhaobin@chromium.org>
2017-08-04 21:40:34 +0000

    [net_interfaces] Extract ip_endpoint.mojom from host_resolver_service.mojom
    Media Router needs ip_endpoint mojo struct to send data to MRP. ip_endpoint mojo struct is
    currently defined in host_resolver_service.mojom. Seperate it out so that media_router.mojom
    can include it.
    
    Bug: 749917
    Change-Id: I2a4ab93e1b2bd40a3579aa26930e393c12d5d942
    Reviewed-on: https://chromium-review.googlesource.com/600747
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Derek Cheng <imcheng@chromium.org>
    Commit-Queue: Bin Zhao <zhaobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492130}

--- net/dns/mojo_host_struct_traits.cc
+++ net/dns/mojo_host_struct_traits.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ptr_util.h"
 #include "net/base/address_list.h"
+#include "net/interfaces/ip_endpoint_struct_traits.h"
 
 namespace mojo {
 
@@ -66,19 +67,6 @@ bool StructTraits<net::interfaces::HostResolverRequestInfoDataView,
   net::HostResolver::RequestInfo& request = **out;
   request.set_address_family(address_family);
   request.set_is_my_ip_address(data.is_my_ip_address());
-  return true;
-}
-
-// static
-bool StructTraits<net::interfaces::IPEndPointDataView, net::IPEndPoint>::Read(
-    net::interfaces::IPEndPointDataView data,
-    net::IPEndPoint* out) {
-  std::vector<uint8_t> bytes;
-  if (!data.ReadAddress(&bytes))
-    return false;
-
-  *out =
-      net::IPEndPoint(net::IPAddress(bytes.data(), bytes.size()), data.port());
   return true;
 }
 
---
+++ net/interfaces/ip_endpoint_struct_traits.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_INTERFACES_IP_ENDPOINT_STRUCT_TRAITS_H_
+#define NET_INTERFACES_IP_ENDPOINT_STRUCT_TRAITS_H_
+
+#include "mojo/public/cpp/bindings/struct_traits.h"
+#include "net/base/ip_endpoint.h"
+#include "net/interfaces/ip_endpoint.mojom.h"
+
+namespace mojo {
+template <>
+struct StructTraits<net::interfaces::IPEndPointDataView, net::IPEndPoint> {
+  static const net::IPAddress& address(const net::IPEndPoint& obj) {
+    return obj.address();
+  }
+  static uint16_t port(const net::IPEndPoint& obj) { return obj.port(); }
+
+  static bool Read(net::interfaces::IPEndPointDataView obj,
+                   net::IPEndPoint* out);
+};
+
+}  // namespace mojo
+
+#endif  // NET_INTERFACES_IP_ENDPOINT_STRUCT_TRAITS_H_
