cd5faf0efba0cd6c722dcdef9bcc61e0021cdc24
Juan Jose Lopez Jaimez <jjlopezjaimez@google.com>
2017-08-04 23:57:47 +0000

    Add the dialog controller as an observer so we can prompt the user if no browser is available Bug:  http://crbug.com/734677
    Change-Id: Iec5a1e85bd4887dac5a821d2bb96d6db70f33cb6
    Reviewed-on: https://chromium-review.googlesource.com/593967
    Reviewed-by: Robert Shield <robertshield@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Ali Tofigh <alito@chromium.org>
    Commit-Queue: Juan José Lopez Jaimez <jjlopezjaimez@google.com>
    Cr-Commit-Position: refs/heads/master@{#492183}

--- chrome/browser/safe_browsing/chrome_cleaner/srt_field_trial_win.cc
+++ chrome/browser/safe_browsing/chrome_cleaner/srt_field_trial_win.cc
@@ -38,9 +38,6 @@ const char kCanarySRTDownloadURL[] =
     "https://dl.google.com/dl"
     "/softwareremovaltool/win/c/chrome_cleanup_tool.exe?chrome-prompt=1";
 
-constexpr char kSoftwareReporterPromptShownMetricName[] =
-    "SoftwareReporter.PromptShown";
-
 }  // namespace
 
 namespace safe_browsing {
@@ -115,13 +112,13 @@ void RecordSRTPromptHistogram(SRTPromptHistogramValue value) {
                             SRT_PROMPT_MAX);
 }
 
-void RecordPromptShownHistogram() {
-  UMA_HISTOGRAM_BOOLEAN(kSoftwareReporterPromptShownMetricName, true);
+void RecordPromptShownWithTypeHistogram(PromptTypeHistogramValue value) {
+  UMA_HISTOGRAM_ENUMERATION("SoftwareReporter.PromptShownWithType", value,
+                            PROMPT_TYPE_MAX);
 }
 
 void RecordPromptNotShownWithReasonHistogram(
     NoPromptReasonHistogramValue value) {
-  UMA_HISTOGRAM_BOOLEAN(kSoftwareReporterPromptShownMetricName, false);
   UMA_HISTOGRAM_ENUMERATION("SoftwareReporter.NoPromptReason", value,
                             NO_PROMPT_REASON_MAX);
 }
