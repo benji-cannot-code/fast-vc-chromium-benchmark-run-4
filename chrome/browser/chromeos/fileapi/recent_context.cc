563fec6d9a1b5178c3a4107a973a261ba3a42576
Shuhei Takahashi <nya@chromium.org>
2017-08-04 08:54:58 +0000

    recent: Introduce RecentModel.
    This change introduces core concepts of Recent file system:
    
    RecentModel:
    
      The most important class owning RecentSources.
    
      The only API it provides is GetRecentFiles() which queries
      RecentSources for recently modified files and build a list of
      recently modified files across file systems, and also caches the
      result for better file system operation performance.
    
      Files are represented as FileSystemURL.
    
    RecentSource:
    
      Interface class for a source of recent files.
    
      This interface will be implemented for several sources such as local
      storages and cloud storages so that they appear in Recent file
      system.
    
    RecentContext:
    
      Holds several "context" objects necessary for operations in
      RecentModel and RecentSource.
    
    Bug: 742722
    Test: unit_tests
    Change-Id: I2a2ff1fa28e8cf5c705ad1ee6bc75a39b49b2f0b
    Reviewed-on: https://chromium-review.googlesource.com/588950
    Reviewed-by: Tomasz Mikolajewski <mtomasz@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491968}

---
+++ chrome/browser/chromeos/fileapi/recent_context.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/fileapi/recent_context.h"
+
+#include <utility>
+
+namespace chromeos {
+
+RecentContext::RecentContext() : is_valid_(false) {}
+
+RecentContext::RecentContext(storage::FileSystemContext* file_system_context,
+                             const GURL& origin)
+    : is_valid_(true),
+      file_system_context_(file_system_context),
+      origin_(origin) {}
+
+RecentContext::RecentContext(const RecentContext& other) = default;
+
+RecentContext::~RecentContext() = default;
+
+RecentContext& RecentContext::operator=(const RecentContext& other) = default;
+
+}  // namespace chromeos
---
+++ chrome/browser/chromeos/fileapi/test/fake_recent_source.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/fileapi/test/fake_recent_source.h"
+
+#include <utility>
+
+#include "chrome/browser/chromeos/fileapi/recent_context.h"
+
+namespace chromeos {
+
+FakeRecentSource::FakeRecentSource() = default;
+
+FakeRecentSource::~FakeRecentSource() = default;
+
+void FakeRecentSource::AddFile(const storage::FileSystemURL& file) {
+  canned_files_.emplace_back(file);
+}
+
+void FakeRecentSource::GetRecentFiles(RecentContext context,
+                                      GetRecentFilesCallback callback) {
+  std::move(callback).Run(canned_files_);
+}
+
+}  // namespace chromeos
