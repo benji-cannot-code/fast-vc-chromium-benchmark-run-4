36c52ba3c20a6a18a55a8d0d5d75e5f1428e1bda
spqchan <spqchan@chromium.org>
2017-08-04 19:09:43 +0000

    Fix an Availability Annotation
    Change an availability check to an availability
    annotation.
    
    Bug: 747693
    Change-Id: I77b5e0c4d71b9771dc076b9802f0fb81a84c0c26
    Reviewed-on: https://chromium-review.googlesource.com/597347
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492093}

--- chrome/browser/ui/cocoa/tab_contents/tab_contents_controller.mm
+++ chrome/browser/ui/cocoa/tab_contents/tab_contents_controller.mm
@@ -235,11 +235,8 @@ class FullscreenObserver : public WebContentsObserver {
   [self setView:view];
 }
 
-- (NSTouchBar*)makeTouchBar {
-  if (@available(macOS 10.12.2, *))
-    return [touchBarController_ makeTouchBar];
-
-  return nil;
+- (NSTouchBar*)makeTouchBar API_AVAILABLE(macos(10.12.2)) {
+  return [touchBarController_ makeTouchBar];
 }
 
 - (void)ensureContentsVisibleInSuperview:(NSView*)superview {
