8f12bd7ca1576ab8e33cf150730b2ea488865596
Vasilii Sukhanov <vasilii@chromium.org>
2017-08-04 09:36:04 +0000

    Revert "Enable WebNavigationApiTest.CrossProcess(History|Fragment)."
    This reverts commit e03b42cc230039adc8aaf5ef5b804e12384aabbb.
    
    Reason for revert: both tests timeout quite reliably on Win10 Tests x64 (https://build.chromium.org/p/chromium.win/builders/Win10%20Tests%20x64/builds/14362)
    
    Original change's description:
    > Enable WebNavigationApiTest.CrossProcess(History|Fragment).
    > 
    > Bug: 708139
    > Change-Id: Idb9eb18755219be4164b6fc2585aa43ed9798dc1
    > Reviewed-on: https://chromium-review.googlesource.com/601027
    > Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    > Commit-Queue: Nasko Oskov <nasko@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#491836}
    
    TBR=nasko@chromium.org,alexmos@chromium.org
    
    Change-Id: Iee6a4124181b0e7761a5c77e1f65ea3cda23d4d8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 708139
    Reviewed-on: https://chromium-review.googlesource.com/601948
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491976}

--- chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
+++ chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
@@ -715,7 +715,8 @@ IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, CrossProcessAbort) {
   ASSERT_TRUE(catcher.GetNextResult()) << catcher.message();
 }
 
-IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, CrossProcessFragment) {
+// crbug.com/708139.
+IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, DISABLED_CrossProcessFragment) {
   ASSERT_TRUE(StartEmbeddedTestServer());
 
   // See crossProcessFragment/f.html.
@@ -736,7 +737,8 @@ IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, CrossProcessFragment) {
       << message_;
 }
 
-IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, CrossProcessHistory) {
+// crbug.com/708139.
+IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, DISABLED_CrossProcessHistory) {
   ASSERT_TRUE(StartEmbeddedTestServer());
 
   // See crossProcessHistory/e.html.
