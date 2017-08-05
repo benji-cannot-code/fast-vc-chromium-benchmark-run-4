c77053b3f7398a6360bddf7bfc1fc3d94d113ddc
Adrienne Walker <enne@chromium.org>
2017-08-05 02:40:32 +0000

    Validate deserialized rects in PaintOpBuffer
    Skia isn't happy about infinite or nan rects.
    
    Bug: 752487
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I318a7b04ca7089ceea02a971dae2d5a9c258f877
    Reviewed-on: https://chromium-review.googlesource.com/602603
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492220}

--- cc/paint/paint_op_buffer.h
+++ cc/paint/paint_op_buffer.h
@@ -173,6 +173,13 @@ class CC_PAINT_EXPORT PaintOp {
            static_cast<uint32_t>(SkClipOp::kMax_EnumValue);
   }
 
+  static bool IsUnsetRect(const SkRect& rect) {
+    return rect.fLeft == SK_ScalarInfinity;
+  }
+  static bool IsValidOrUnsetRect(const SkRect& rect) {
+    return IsUnsetRect(rect) || rect.isFinite();
+  }
+
   static constexpr bool kIsDrawOp = false;
   static constexpr bool kHasPaintFlags = false;
   // Since skip and type fit in a uint32_t, this is the max size of skip.
@@ -334,7 +341,7 @@ class CC_PAINT_EXPORT AnnotateOp final : public PaintOp {
   static void Raster(const AnnotateOp* op,
                      SkCanvas* canvas,
                      const PlaybackParams& params);
-  bool IsValid() const { return true; }
+  bool IsValid() const { return rect.isFinite(); }
   HAS_SERIALIZATION_FUNCTIONS();
 
   PaintCanvas::AnnotationType annotation_type;
@@ -392,7 +399,7 @@ class CC_PAINT_EXPORT ClipRectOp final : public PaintOp {
   static void Raster(const ClipRectOp* op,
                      SkCanvas* canvas,
                      const PlaybackParams& params);
-  bool IsValid() const { return IsValidSkClipOp(op); }
+  bool IsValid() const { return IsValidSkClipOp(op) && rect.isFinite(); }
   HAS_SERIALIZATION_FUNCTIONS();
 
   SkRect rect;
@@ -411,7 +418,7 @@ class CC_PAINT_EXPORT ClipRRectOp final : public PaintOp {
   static void Raster(const ClipRRectOp* op,
                      SkCanvas* canvas,
                      const PlaybackParams& params);
-  bool IsValid() const { return IsValidSkClipOp(op); }
+  bool IsValid() const { return IsValidSkClipOp(op) && rrect.isValid(); }
   bool HasNonAAPaint() const { return !antialias; }
   HAS_SERIALIZATION_FUNCTIONS();
 
@@ -457,7 +464,7 @@ class CC_PAINT_EXPORT DrawArcOp final : public PaintOpWithFlags {
                               const PaintFlags* flags,
                               SkCanvas* canvas,
                               const PlaybackParams& params);
-  bool IsValid() const { return flags.IsValid(); }
+  bool IsValid() const { return flags.IsValid() && oval.isFinite(); }
   HAS_SERIALIZATION_FUNCTIONS();
 
   SkRect oval;
@@ -575,7 +582,9 @@ class CC_PAINT_EXPORT DrawImageRectOp final : public PaintOpWithFlags {
                               const PaintFlags* flags,
                               SkCanvas* canvas,
                               const PlaybackParams& params);
-  bool IsValid() const { return flags.IsValid(); }
+  bool IsValid() const {
+    return flags.IsValid() && src.isFinite() && dst.isFinite();
+  }
   bool HasDiscardableImages() const;
   HAS_SERIALIZATION_FUNCTIONS();
 
@@ -646,7 +655,7 @@ class CC_PAINT_EXPORT DrawOvalOp final : public PaintOpWithFlags {
                               const PaintFlags* flags,
                               SkCanvas* canvas,
                               const PlaybackParams& params);
-  bool IsValid() const { return flags.IsValid(); }
+  bool IsValid() const { return flags.IsValid() && oval.isFinite(); }
   HAS_SERIALIZATION_FUNCTIONS();
 
   SkRect oval;
@@ -729,7 +738,7 @@ class CC_PAINT_EXPORT DrawRectOp final : public PaintOpWithFlags {
                               const PaintFlags* flags,
                               SkCanvas* canvas,
                               const PlaybackParams& params);
-  bool IsValid() const { return flags.IsValid(); }
+  bool IsValid() const { return flags.IsValid() && rect.isFinite(); }
   HAS_SERIALIZATION_FUNCTIONS();
 
   SkRect rect;
@@ -860,7 +869,7 @@ class CC_PAINT_EXPORT SaveLayerOp final : public PaintOpWithFlags {
                               const PaintFlags* flags,
                               SkCanvas* canvas,
                               const PlaybackParams& params);
-  bool IsValid() const { return flags.IsValid(); }
+  bool IsValid() const { return flags.IsValid() && IsValidOrUnsetRect(bounds); }
   bool HasNonAAPaint() const { return false; }
   HAS_SERIALIZATION_FUNCTIONS();
 
@@ -882,7 +891,7 @@ class CC_PAINT_EXPORT SaveLayerAlphaOp final : public PaintOp {
   static void Raster(const SaveLayerAlphaOp* op,
                      SkCanvas* canvas,
                      const PlaybackParams& params);
-  bool IsValid() const { return true; }
+  bool IsValid() const { return IsValidOrUnsetRect(bounds); }
   HAS_SERIALIZATION_FUNCTIONS();
 
   SkRect bounds;
