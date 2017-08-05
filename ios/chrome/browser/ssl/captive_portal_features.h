cd3fb682a57e4d5c3bca1bc052d8224a119eab84
Mike Dougherty <michaeldo@chromium.org>
2017-08-05 00:11:58 +0000

    Move Captive Portal flag to ios chrome.
    The flag for enabling the captive portal feature will only be used
    inside ios/chrome, so it doesn't need to be inside ios/components.
    
    Bug: None
    Change-Id: I5126827dda001816a73089453fc4d985f42bc667
    Reviewed-on: https://chromium-review.googlesource.com/600997
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Eric Noyau <noyau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492193}

--- ios/chrome/browser/ssl/captive_portal_features.h
+++ ios/chrome/browser/ssl/captive_portal_features.h
@@ -3,19 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_COMPONENTS_CAPTIVE_PORTAL_FEATURES_H_
-#define IOS_COMPONENTS_CAPTIVE_PORTAL_FEATURES_H_
+#ifndef IOS_CHROME_BROWSER_SSL_CAPTIVE_PORTAL_FEATURES_H_
+#define IOS_CHROME_BROWSER_SSL_CAPTIVE_PORTAL_FEATURES_H_
 
 #include "base/feature_list.h"
-#include "build/build_config.h"
 
-namespace captive_portal {
-
-#if defined(OS_IOS)
 // Used to control the state of the Captive Portal Login feature.
-extern const base::Feature kIosCaptivePortal;
-#endif
+extern const base::Feature kCaptivePortalFeature;
 
-}  // namespace captive_portal
-
-#endif  // IOS_COMPONENTS_CAPTIVE_PORTAL_FEATURES_H_
+#endif  // IOS_CHROME_BROWSER_SSL_CAPTIVE_PORTAL_FEATURES_H_
