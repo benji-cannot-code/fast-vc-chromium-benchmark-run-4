afe36029055c056871b36294801e518fc77e5264
Etienne Pierre-Doray <etiennep@chromium.org>
2017-08-07 18:45:03 +0000

    [Zucchini] Introduce encoded_view.
    This CL adds EncodedView, which offers an inexpensive 
    access to an Encoded Image, adapting on instances of
    ImageIndex.
    
    Bug: 729154
    Change-Id: I6441a3bed20528a40f20f3d7d269a093157731dc
    Reviewed-on: https://chromium-review.googlesource.com/599389
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492370}

--- chrome/installer/zucchini/image_index_unittest.cc
+++ chrome/installer/zucchini/image_index_unittest.cc
@@ -91,7 +91,7 @@ TEST_F(ImageIndexTest, GetType) {
       0,  0,         // ref 0
       2,  2,  2,     // ref 2
       -1, -1,        // raw
-      2,  2,  2,     // ref 0
+      2,  2,  2,     // ref 2
   };
 
   for (offset_t i = 0; i < image_index_.size(); ++i)
@@ -110,7 +110,7 @@ TEST_F(ImageIndexTest, IsToken) {
       1, 0,        // ref 0
       1, 0, 0,     // ref 2
       1, 1,        // raw
-      1, 0, 0,     // ref 0
+      1, 0, 0,     // ref 2
   };
 
   for (offset_t i = 0; i < image_index_.size(); ++i)
@@ -129,7 +129,7 @@ TEST_F(ImageIndexTest, IsReference) {
       1, 1,        // ref 0
       1, 1, 1,     // ref 2
       0, 0,        // raw
-      1, 1, 1,     // ref 0
+      1, 1, 1,     // ref 2
   };
 
   for (offset_t i = 0; i < image_index_.size(); ++i) {
