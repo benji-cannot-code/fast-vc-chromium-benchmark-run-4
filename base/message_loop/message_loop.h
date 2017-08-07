5d4bc8227db4943e8c1db90bb5d8e5a37eae9f08
Gabriel Charette <gab@chromium.org>
2017-08-07 04:22:48 +0000

    Introduce RunLoop::Type::NESTABLE_TASKS_ALLOWED to replace MessageLoop::ScopedNestableTaskAllower.
    (as well as MessageLoop::SetNestableTasksAllowed())
    
    Surveying usage: the scoped object is always instantiated right before
    RunLoop().Run(). The intent is really to allow nestable tasks in that
    RunLoop so it's better to explicitly label that RunLoop as such and it
    allows us to break the last dependency that forced some RunLoop users
    to use MessageLoop APIs.
    
    There's also the odd case of allowing nestable tasks for loops that are
    reentrant from a native task (without going through RunLoop), these
    are the minority but will have to be handled (after cleaning up the
    majority of cases that are RunLoop induced).
    As highlighted by robliao@ in https://chromium-review.googlesource.com/c/600517
    (which was merged in this CL).
    
    R=danakj@chromium.org
    
    Bug: 750779
    Change-Id: I43d122c93ec903cff3a6fe7b77ec461ea0656448
    Reviewed-on: https://chromium-review.googlesource.com/594713
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492263}

--- base/message_loop/message_loop.h
+++ base/message_loop/message_loop.h
@@ -226,10 +226,20 @@ class BASE_EXPORT MessageLoop : public MessagePump::Delegate,
   // - With NestableTasksAllowed set to true, the task #2 will run right away.
   //   Otherwise, it will get executed right after task #1 completes at "thread
   //   message loop level".
+  //
+  // DEPRECATED: Use RunLoop::Type on the relevant RunLoop instead of these
+  // methods.
+  // TODO(gab): Migrate usage and delete these methods.
   void SetNestableTasksAllowed(bool allowed);
   bool NestableTasksAllowed() const;
 
   // Enables nestable tasks on |loop| while in scope.
+  // DEPRECATED: This should not be used when the nested loop is driven by
+  // RunLoop (use RunLoop::Type::KNestableTasksAllowed instead). It can however
+  // still be useful in a few scenarios where re-entrancy is caused by a native
+  // message loop.
+  // TODO(gab): Remove usage of this class alongside RunLoop and rename it to
+  // ScopedApplicationTasksAllowedInNativeNestedLoop(?).
   class ScopedNestableTaskAllower {
    public:
     explicit ScopedNestableTaskAllower(MessageLoop* loop)
@@ -335,6 +345,7 @@ class BASE_EXPORT MessageLoop : public MessagePump::Delegate,
   // RunLoop::Delegate:
   void Run() override;
   void Quit() override;
+  void EnsureWorkScheduled() override;
 
   // Called to process any delayed non-nestable tasks.
   bool ProcessNextDelayedNonNestableTask();
@@ -402,7 +413,9 @@ class BASE_EXPORT MessageLoop : public MessagePump::Delegate,
   ObserverList<DestructionObserver> destruction_observers_;
 
   // A recursion block that prevents accidentally running additional tasks when
-  // insider a (accidentally induced?) nested message pump.
+  // insider a (accidentally induced?) nested message pump. Deprecated in favor
+  // of run_loop_client_->ProcessingTasksAllowed(), equivalent until then (both
+  // need to be checked in conditionals).
   bool nestable_tasks_allowed_;
 
   // pump_factory_.Run() is called to create a message pump for this loop
