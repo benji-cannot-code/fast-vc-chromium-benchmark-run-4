bd29e6d258122188a22b4b116f35051dc0b424d2
Elly Fong-Jones <ellyjones@chromium.org>
2017-08-07 18:11:36 +0000

    cocoa: include tab alert state & crash state in accessible title
    This change:
    1) Factors out most of BrowserView::GetAccessibleTabLabel into a new helper,
       chrome::AssembleTabAccessibilityLabel
    2) Has BrowserView::GetAccessibleTabLabel call the new helper
    3) Adds [TabController accessibleTitle] to the Cocoa TabController
    4) Has the Cocoa TabView use [TabController accessibleTitle] for the
       accessible title
    5) Has [TabController accessibleTitle] call the new helper
    
    Bug: 614922
    Change-Id: I94790fea08ba173b893a872fedc278ae5c8b8434
    Reviewed-on: https://chromium-review.googlesource.com/602807
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492362}

--- chrome/browser/ui/cocoa/tabs/tab_controller.h
+++ chrome/browser/ui/cocoa/tabs/tab_controller.h
@@ -127,6 +127,9 @@ class MenuDelegate;
 
 // Update the title color to match the tabs current state.
 - (void)updateTitleColor;
+
+// Returns the accessibility title that should be used for this tab.
+- (NSString*)accessibilityTitle;
 @end
 
 @interface TabController(TestingAPI)
--- chrome/browser/ui/cocoa/tabs/tab_controller.mm
+++ chrome/browser/ui/cocoa/tabs/tab_controller.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/rtl.h"
 #include "base/mac/bundle_locations.h"
 #include "base/metrics/user_metrics.h"
+#include "base/strings/sys_string_conversions.h"
 #import "chrome/browser/themes/theme_properties.h"
 #import "chrome/browser/themes/theme_service.h"
 #include "chrome/browser/ui/cocoa/l10n_util.h"
@@ -476,6 +477,16 @@ static const CGFloat kTabElementYOrigin = 6;
   if (theme && !titleColor)
     titleColor = theme->GetNSColor(ThemeProperties::COLOR_TAB_TEXT);
   [[self tabView] setTitleColor:titleColor ? titleColor : [NSColor textColor]];
+}
+
+- (NSString*)accessibilityTitle {
+  // TODO(ellyjones): the Cocoa tab strip code doesn't keep track of network
+  // error state, so it can't get surfaced here. It should, and then this could
+  // pass in the network error state.
+  return base::SysUTF16ToNSString(chrome::AssembleTabAccessibilityLabel(
+      base::SysNSStringToUTF16([self title]),
+      [self loadingState] == kTabCrashed, false,
+      [[self alertIndicatorButton] showingAlertState]));
 }
 
 - (void)themeChangedNotification:(NSNotification*)notification {
