8bdeb793ec50a092130830fe909bb240c0653d50
Zentaro Kavanagh <zentaro@google.com>
2017-08-07 22:35:30 +0000

    Fix bug in MD4 implementation that creates incorrect hash.
    - Inputs with length % 64 >= 56 potentially generate the wrong hash.
    - In MD4Sum function the last 4 bytes of the final array were never
      initialized.
    - This would result in NTLM failing due to incorrect hash when the
      password length % 32 >= 28 (unicode password).
    - Fixes fuzzer bug that was caught by new NTLM fuzzer.
    - Added test suite from RFC 1320 for MD4.
    - Md4Test.RfcTest6_Mod56CornerCase fails without this fix.
    
    BUG=chromium:752491
    
    Change-Id: If7b718b8c80f00819200332a027bb2f430b8f68e
    Reviewed-on: https://chromium-review.googlesource.com/602693
    Commit-Queue: Zentaro Kavanagh <zentaro@google.com>
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492432}

--- net/ntlm/md4.h
+++ net/ntlm/md4.h
@@ -47,6 +47,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include "net/base/net_export.h"
+
 namespace net {
 namespace weak_crypto {
 
@@ -67,7 +69,9 @@ namespace weak_crypto {
  * interface would make more sense if that were a requirement.  Currently, this
  * is good enough for the applications we care about.
  */
-void MD4Sum(const uint8_t* input, uint32_t inputLen, uint8_t* result);
+NET_EXPORT_PRIVATE void MD4Sum(const uint8_t* input,
+                               uint32_t inputLen,
+                               uint8_t* result);
 
 }  // namespace weak_crypto
 }  // namespace net
