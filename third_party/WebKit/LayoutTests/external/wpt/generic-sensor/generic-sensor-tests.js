ca867c080ab1500a41ea3fef89c66de6aded7d58
Philip Jägenstedt <foolip@chromium.org>
2017-08-07 16:19:58 +0000

    Import wpt@b752fe789b9382fefecaaf5b45ab01c599323769 (manual)
    Using wpt-import in Chromium edef955d95757053da45a4b8947d098273e2ac59.
    
    Manual import due to recent failure: https://crbug.com/752892
    
    No-Export: true
    Change-Id: Ieb5b488afe0bfb590db72328b4d353fec5ab1fbc
    Reviewed-on: https://chromium-review.googlesource.com/603409
    Reviewed-by: Raphael Kubo da Costa (rakuco) <raphael.kubo.da.costa@intel.com>
    Commit-Queue: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492335}

--- third_party/WebKit/LayoutTests/external/wpt/generic-sensor/generic-sensor-tests.js
+++ third_party/WebKit/LayoutTests/external/wpt/generic-sensor/generic-sensor-tests.js
@@ -1,13 +1,16 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 let unreached = event => {
-    assert_unreached(event.error.name + ":" + event.error.message);
+  assert_unreached(event.error.name + ": " + event.error.message);
 };
 
 let properties = {
-    'AmbientLightSensor' : ['timestamp', 'illuminance'],
-    'Accelerometer' : ['timestamp', 'x', 'y', 'z'],
-    'Gyroscope' : ['timestamp', 'x', 'y', 'z'],
-    'Magnetometer' : ['timestamp', 'x', 'y', 'z']
+  'AmbientLightSensor' : ['timestamp', 'illuminance'],
+  'Accelerometer' : ['timestamp', 'x', 'y', 'z'],
+  'LinearAccelerationSensor' : ['timestamp', 'x', 'y', 'z'],
+  'Gyroscope' : ['timestamp', 'x', 'y', 'z'],
+  'Magnetometer' : ['timestamp', 'x', 'y', 'z'],
+  'AbsoluteOrientationSensor' : ['timestamp', 'quaternion'],
+  'RelativeOrientationSensor' : ['timestamp', 'quaternion']
 };
 
 function assert_reading_not_null(sensor) {
