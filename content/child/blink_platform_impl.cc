0098bcae77b4ccea2eb4d693a76b250a2665c87c
Becca Hughes <beccahughes@chromium.org>
2017-08-07 11:15:57 +0000

    Media Controls: Move media controls resources to media_controls module
    As part of converting media_controls to using Grit, this adds
    blink_scaled_resources_100/200_percent rules to Blink that will
    repack all the scaled Blink resources and combine them. This then
    updates the dependencies of blink image_resources to use the
    new scaled_resources. Moves the media controls resources to the
    media_controls module.
    
    BUG=747975
    
    Change-Id: Ia64d3e2f3a79eb4f893a3ba5df7cf0069ca3e733
    Reviewed-on: https://chromium-review.googlesource.com/582812
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492298}

--- content/child/blink_platform_impl.cc
+++ content/child/blink_platform_impl.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "blink/public/resources/grit/blink_image_resources.h"
 #include "blink/public/resources/grit/blink_resources.h"
+#include "blink/public/resources/grit/media_controls_resources.h"
 #include "build/build_config.h"
 #include "content/app/resources/grit/content_resources.h"
 #include "content/app/strings/grit/content_strings.h"
