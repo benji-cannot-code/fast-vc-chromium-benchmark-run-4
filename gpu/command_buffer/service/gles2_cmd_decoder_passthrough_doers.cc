fa2a2cae1fe2a6042c19b9589080813bab064429
liberato@chromium.org <liberato@chromium.org>
2017-08-08 17:27:42 +0000

    Added display_{width, height} to overlay hint.
    Previously, we sent the display position but not the size to the
    GLStreamTextureImage during overlay processing.  It turns out that
    the size is also useful when using the hint.
    
    In particular, knowing the complete bounds for it lets us set the
    initial position properly, and avoid some ugly transitions into
    fullscreen on clank.  This CL doesn't include any code to use the
    size information.
    
    Bug: 752504
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ie5391eddf8129f788d20fd7bbefc1416ac80a884
    Reviewed-on: https://chromium-review.googlesource.com/602448
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492685}

--- gpu/command_buffer/service/gles2_cmd_decoder_passthrough_doers.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder_passthrough_doers.cc
@@ -4217,7 +4217,9 @@ error::Error GLES2DecoderPassthroughImpl::DoOverlayPromotionHintCHROMIUM(
     GLuint texture,
     GLboolean promotion_hint,
     GLint display_x,
-    GLint display_y) {
+    GLint display_y,
+    GLint display_width,
+    GLint display_height) {
   NOTIMPLEMENTED();
   return error::kNoError;
 }
