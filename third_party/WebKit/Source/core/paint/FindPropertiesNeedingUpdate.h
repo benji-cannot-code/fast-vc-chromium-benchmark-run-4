5ebca5e1d19611a36ee3975568c76d83d6fa2c54
Philip Rogers <pdr@chromium.org>
2017-08-08 08:13:49 +0000

    Separate scroll offset translation from scroll nodes
    This patch separates scroll offset translation from scroll nodes
    by moving ScrollPaintPropertyNode to be owned by ObjectPaintProperties
    (and LocalFrameView) instead of the scroll offset translation
    TransformPaintPropertyNode. This largely reverts [1]. With this
    change, overflow: hidden has a scroll offset translation without
    having a scroll node that could be composited.
    
    There are 3 main changes in this patch:
    1) Scroll nodes are no longer owned by the transform that handles
    scroll offset translation.
    2) Scroll nodes are no longer created for LayoutObjects that have
    overflow hidden but do not actually scroll (see:
    PaintPropertyTreeBuilder::UpdateScrollAndScrollTranslation).
    3) Scroll nodes are no longer created for FrameViews that have
    overflow hidden but do not actually scroll (see:
    PaintPropertyTreeBuilder::UpdateProperties(LocalFrameView&, ...)
    
    This patch is part of a series of patches to add scrollable hit
    test layers in SPV2. A proof-of-concept patch is at:
    https://chromium-review.googlesource.com/c/554060.
    
    [1] https://chromium.googlesource.com/chromium/src/+/3eee970eb6757c6ea3997e0722d7bab727b9c11c
    
    Bug: 738613
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I69cea34b513fce392adeebfd038cadc917eedcc8
    Reviewed-on: https://chromium-review.googlesource.com/602081
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Tien-Ren Chen <trchen@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492570}

--- third_party/WebKit/Source/core/paint/FindPropertiesNeedingUpdate.h
+++ third_party/WebKit/Source/core/paint/FindPropertiesNeedingUpdate.h
@@ -67,6 +67,8 @@ class FindFrameViewPropertiesNeedingUpdateScope {
       original_pre_translation_ = pre_translation->Clone();
     if (auto* content_clip = frame_view_->ContentClip())
       original_content_clip_ = content_clip->Clone();
+    if (auto* scroll_node = frame_view_->ScrollNode())
+      original_scroll_node_ = scroll_node->Clone();
     if (auto* scroll_translation = frame_view_->ScrollTranslation())
       original_scroll_translation_ = scroll_translation->Clone();
   }
@@ -86,6 +88,8 @@ class FindFrameViewPropertiesNeedingUpdateScope {
                                  frame_view_->PreTranslation());
     DCHECK_FRAMEVIEW_PROPERTY_EQ(original_content_clip_,
                                  frame_view_->ContentClip());
+    DCHECK_FRAMEVIEW_PROPERTY_EQ(original_scroll_node_,
+                                 frame_view_->ScrollNode());
     DCHECK_FRAMEVIEW_PROPERTY_EQ(original_scroll_translation_,
                                  frame_view_->ScrollTranslation());
 
@@ -99,6 +103,7 @@ class FindFrameViewPropertiesNeedingUpdateScope {
   bool needed_forced_subtree_update_;
   RefPtr<const TransformPaintPropertyNode> original_pre_translation_;
   RefPtr<const ClipPaintPropertyNode> original_content_clip_;
+  RefPtr<const ScrollPaintPropertyNode> original_scroll_node_;
   RefPtr<const TransformPaintPropertyNode> original_scroll_translation_;
 };
 
@@ -185,6 +190,8 @@ class FindObjectPropertiesNeedingUpdateScope {
       DCHECK_OBJECT_PROPERTY_EQ(
           object_, original_properties_->SvgLocalToBorderBoxTransform(),
           object_properties->SvgLocalToBorderBoxTransform());
+      DCHECK_OBJECT_PROPERTY_EQ(object_, original_properties_->Scroll(),
+                                object_properties->Scroll());
       DCHECK_OBJECT_PROPERTY_EQ(object_,
                                 original_properties_->ScrollTranslation(),
                                 object_properties->ScrollTranslation());
