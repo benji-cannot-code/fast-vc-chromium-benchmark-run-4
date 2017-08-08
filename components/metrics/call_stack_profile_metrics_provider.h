769aa4afaabbd0601cc3c10ab559047dc55e1d2c
Alexei Svitkine <asvitkine@chromium.org>
2017-08-08 17:19:46 +0000

    Fix shutdown hang with UMA sampling profiler periodic sampling.
    BUG=749122
    
    Change-Id: I7fa5580a7f40958a112547339ee7a5173f4fffab
    Reviewed-on: https://chromium-review.googlesource.com/604336
    Commit-Queue: Alexei Svitkine (slow) <asvitkine@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492678}

--- components/metrics/call_stack_profile_metrics_provider.h
+++ components/metrics/call_stack_profile_metrics_provider.h
@@ -72,6 +72,7 @@ class CallStackProfileMetricsProvider : public MetricsProvider {
   static bool IsPeriodicSamplingEnabled();
 
   // MetricsProvider:
+  void Init() override;
   void OnRecordingEnabled() override;
   void OnRecordingDisabled() override;
   void ProvideCurrentSessionData(
