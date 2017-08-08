062f3dbf79ec318627ccd53040db38adccfc6fbf
Theresa Wellington <twellington@google.com>
2017-08-08 00:37:10 +0000

    Speculative fix for flaky HistoryActivityTest#*
    Re-orders some setup calls and waits for observer callbacks as
    a speculative fix for flaky HistoryActivityTest#*. Leaves the
    @RetryOnFailure annotation in place, for now, while tests are
    monitored, and updates the error message of the assert that's
    intermittently failing.
    
    BUG=752520
    
    Change-Id: I201145c4db8297b225a97467f5936861a94dc490
    Reviewed-on: https://chromium-review.googlesource.com/604647
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492471}

--- chrome/android/java/src/org/chromium/chrome/browser/history/HistoryAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/history/HistoryAdapter.java
@@ -474,10 +474,10 @@ public class HistoryAdapter extends DateDividedAdapter implements BrowsingHistor
 
     @VisibleForTesting
     void setClearBrowsingDataButtonVisibilityForTest(boolean isVisible) {
-        if (mClearBrowsingDataButton == null) return;
         if (mClearBrowsingDataButtonVisible == isVisible) return;
         mClearBrowsingDataButtonVisible = isVisible;
-        if (mIsInitialized) setHeaders();
+
+        setHeaders();
     }
 
     @VisibleForTesting
@@ -490,5 +490,15 @@ public class HistoryAdapter extends DateDividedAdapter implements BrowsingHistor
         mPrivacyDisclaimerHeaderItem = new HeaderItem(0, null);
         mClearBrowsingDataButtonHeaderItem = new HeaderItem(1, null);
         mClearBrowsingDataButtonVisible = true;
+    }
+
+    @VisibleForTesting
+    boolean arePrivacyDisclaimersVisible() {
+        return mPrivacyDisclaimersVisible;
+    }
+
+    @VisibleForTesting
+    boolean isClearBrowsingDataButtonVisible() {
+        return mClearBrowsingDataButtonVisible;
     }
 }
