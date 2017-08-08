d8c4fb87a4500c81af2c1e6918070f290210a20c
yoshiki iguchi <yoshiki@chromium.org>
2017-08-08 19:13:46 +0000

    Activate Widget and Focus SurfaceWindow when DirectInput is used
    With the recent change (crrev.com/2897553002), the message center is not activatable by default, so that the direct reply was not working: key-events were not sent to Android correctly. This patch makes the message center activatable and active before user uses the direct reply.
    
    Bug: 753287
    Change-Id: Ib34db94e03e78f136a52ae2850551bf968b9c9e4
    Reviewed-on: https://chromium-review.googlesource.com/604644
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492721}

--- ui/arc/notification/arc_notification_content_view.h
+++ ui/arc/notification/arc_notification_content_view.h
@@ -69,7 +69,7 @@ class ArcNotificationContentView
   void UpdatePinnedState();
   void UpdateSnapshot();
   void AttachSurface();
-  void ActivateToast();
+  void Activate();
   void UpdateAccessibleName();
 
   // views::NativeViewHost
--- ui/arc/notification/arc_notification_surface_impl.h
+++ ui/arc/notification/arc_notification_surface_impl.h
@@ -31,6 +31,7 @@ class ArcNotificationSurfaceImpl : public ArcNotificationSurface {
   void Detach() override;
   bool IsAttached() const override;
   views::NativeViewHost* GetAttachedHost() const override;
+  void FocusSurfaceWindow() override;
 
   exo::NotificationSurface* surface() const { return surface_; }
 
