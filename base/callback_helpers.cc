2f4fb7bc690c22eec10d27b62576257a4bb00b08
tzik <tzik@chromium.org>
2017-08-08 05:19:40 +0000

    Convert base::ScopedClosureRunner to use OnceCallback
    After this CL, ScopedClosureRunner uses base::OnceClosure as its internal
    callback object. That implies it gets able to take base::OnceClosure in
    addition to base::Closure, and ScopedClosureRunner::Release returns
    base::OnceClosure instead of base::Closure.
    
    Bug: 714018
    Change-Id: I031ef7b70bc9673ea7781e292719e26ddc74b1a7
    Reviewed-on: https://chromium-review.googlesource.com/597090
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Kazuhiro Inaba <kinaba@chromium.org>
    Reviewed-by: Zijie He <zijiehe@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492538}

--- base/callback_helpers.cc
+++ base/callback_helpers.cc
@@ -5,18 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_helpers.h"
 
-#include "base/callback.h"
-
 namespace base {
 
 ScopedClosureRunner::ScopedClosureRunner() {}
 
-ScopedClosureRunner::ScopedClosureRunner(const Closure& closure)
-    : closure_(closure) {}
+ScopedClosureRunner::ScopedClosureRunner(OnceClosure closure)
+    : closure_(std::move(closure)) {}
 
 ScopedClosureRunner::~ScopedClosureRunner() {
   if (!closure_.is_null())
-    closure_.Run();
+    std::move(closure_).Run();
 }
 
 ScopedClosureRunner::ScopedClosureRunner(ScopedClosureRunner&& other)
@@ -29,19 +27,15 @@ ScopedClosureRunner& ScopedClosureRunner::operator=(
 }
 
 void ScopedClosureRunner::RunAndReset() {
-  Closure old_closure = Release();
-  if (!old_closure.is_null())
-    old_closure.Run();
+  std::move(closure_).Run();
 }
 
-void ScopedClosureRunner::ReplaceClosure(const Closure& closure) {
-  closure_ = closure;
+void ScopedClosureRunner::ReplaceClosure(OnceClosure closure) {
+  closure_ = std::move(closure);
 }
 
-Closure ScopedClosureRunner::Release() {
-  Closure result = closure_;
-  closure_.Reset();
-  return result;
+OnceClosure ScopedClosureRunner::Release() {
+  return std::move(closure_);
 }
 
 }  // namespace base
