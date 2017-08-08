a2da69eb29ab3400e3bf177f3494e6de17066c57
Mathieu Perreault <mathp@chromium.org>
2017-08-08 15:55:31 +0000

    [Autofill] Only hide the autofill popup on main frame navigations.
    A site with many subframe navigations (e.g., ads) would make the popup
    be suppressed.
    
    Bug: 739855
    Test: content_autofill_driver_browsertest
    Change-Id: Ifb7138d29326a9602c397fff185e6ce207546db3
    Reviewed-on: https://chromium-review.googlesource.com/604375
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Commit-Queue: Mathieu Perreault <mathp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492640}

--- components/autofill/content/browser/content_autofill_driver_unittest.cc
+++ components/autofill/content/browser/content_autofill_driver_unittest.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/frame_navigate_params.h"
 #include "content/public/test/test_renderer_host.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
+#include "net/base/net_errors.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -265,7 +266,7 @@ class TestContentAutofillDriver : public ContentAutofillDriver {
     return static_cast<MockAutofillManager*>(autofill_manager());
   }
 
-  using ContentAutofillDriver::DidNavigateFrame;
+  using ContentAutofillDriver::DidNavigateMainFrame;
 };
 
 class ContentAutofillDriverTest : public content::RenderViewHostTestHarness {
@@ -295,16 +296,11 @@ class ContentAutofillDriverTest : public content::RenderViewHostTestHarness {
     content::RenderViewHostTestHarness::TearDown();
   }
 
-  void Navigate(bool main_frame) {
-    content::RenderFrameHost* rfh = main_rfh();
-    content::RenderFrameHostTester* rfh_tester =
-        content::RenderFrameHostTester::For(rfh);
-    if (!main_frame)
-      rfh = rfh_tester->AppendChild("subframe");
+  void Navigate(bool same_document) {
     std::unique_ptr<content::NavigationHandle> navigation_handle =
         content::NavigationHandle::CreateNavigationHandleForTesting(
-            GURL(), rfh, true);
-   driver_->DidNavigateFrame(navigation_handle.get());
+            GURL(), main_rfh(), /*committed=*/true, net::OK, same_document);
+    driver_->DidNavigateMainFrame(navigation_handle.get());
   }
 
  protected:
@@ -323,14 +319,14 @@ TEST_F(ContentAutofillDriverTest, GetURLRequestContext) {
   EXPECT_EQ(request_context, expected_request_context);
 }
 
-TEST_F(ContentAutofillDriverTest, NavigatedToDifferentPage) {
+TEST_F(ContentAutofillDriverTest, NavigatedMainFrameDifferentDocument) {
   EXPECT_CALL(*driver_->mock_autofill_manager(), Reset());
-  Navigate(true);
+  Navigate(/*same_document=*/false);
 }
 
-TEST_F(ContentAutofillDriverTest, NavigatedWithinSamePage) {
+TEST_F(ContentAutofillDriverTest, NavigatedMainFrameSameDocument) {
   EXPECT_CALL(*driver_->mock_autofill_manager(), Reset()).Times(0);
-  Navigate(false);
+  Navigate(/*same_document=*/true);
 }
 
 TEST_F(ContentAutofillDriverTest, FormDataSentToRenderer_FillForm) {
