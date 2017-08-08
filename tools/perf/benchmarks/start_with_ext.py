8387171b82c1674cf981f758864d5d5ef547c595
Juan A. Navarro Perez <perezju@chromium.org>
2017-08-08 16:42:11 +0000

    [tools/perf] Re-enable start_wit_ext.* benchmarks
    The two benchmarks being reenabled everywhere are:
    
    - startup_with_ext.cold.blank_page
    - startup_with_ext.warm.blank_page
    
    This is part of the benchmark reenabling fix-it, where we're aiming to
    bring the stories that are disabled in line with the stories that are
    actually still failing. We're also migrating from wholesale benchmark
    disabling to per-story disabling when a story fails.
    
    NOTRY=true
    
    Bug: 753408,713222
    Change-Id: Ie5e188a6a7cea2112583df55b40e1424b241cff2
    Reviewed-on: https://chromium-review.googlesource.com/606427
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: rnephew <rnephew@chromium.org>
    Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492657}

--- tools/perf/benchmarks/start_with_ext.py
+++ tools/perf/benchmarks/start_with_ext.py
@@ -32,8 +32,6 @@ class _StartWithExt(perf_benchmark.PerfBenchmark):
 
 
 @benchmark.Enabled('has tabs')
-@benchmark.Disabled('mac')  # crbug.com/563424
-@benchmark.Disabled('win', 'linux', 'reference', 'android')
 class StartWithExtCold(_StartWithExt):
   """Measure time to start Chrome cold with extensions."""
   options = {'pageset_repeat': 5}
@@ -51,8 +49,6 @@ class StartWithExtCold(_StartWithExt):
 
 
 @benchmark.Enabled('has tabs')
-@benchmark.Disabled('mac')  # crbug.com/563424
-@benchmark.Disabled('win', 'linux', 'reference', 'android')
 class StartWithExtWarm(_StartWithExt):
   """Measure time to start Chrome warm with extensions."""
   options = {'pageset_repeat': 20}
