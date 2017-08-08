4a5e1c617c03310e6fd43fae695c4cce07ddc93f
Thanh Pham <thanhph@chromium.org>
2017-08-08 19:41:22 +0000

    Show corresponding window,widget, and view in the UIElement tree when hovered.
    Currently, in UI Devtools, when looking at the UI, users don't know 
    which UIElement is associated with which DOM node so it's hard to see
    or change its properties on the fly. This cl will identify the element
    under mouse cursor when in inspection mode. Users can exit inspection
    mode by left click or click on the blue box-in-arrow.
    
    Bug: 736855
    Change-Id: Ie36b41701eb9744aad7c7914b2e197515c40ce3b
    Reviewed-on: https://chromium-review.googlesource.com/582927
    Commit-Queue: Thanh Pham <thanhph@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Terry Anderson <tdanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492729}

--- components/ui_devtools/views/ui_devtools_css_agent.cc
+++ components/ui_devtools/views/ui_devtools_css_agent.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "components/ui_devtools/views/ui_element.h"
-#include "ui/aura/window.h"
 
 namespace ui_devtools {
 namespace {
--- components/ui_devtools/views/ui_element.h
+++ components/ui_devtools/views/ui_element.h
@@ -41,6 +41,9 @@ class UIElement {
   // Move |child| to position new_index in |children_|.
   void ReorderChild(UIElement* child, int new_index);
 
+  template <class T>
+  int FindUIElementIdForBackendElement(T* element) const;
+
   virtual void GetBounds(gfx::Rect* bounds) const = 0;
   virtual void SetBounds(const gfx::Rect& bounds) = 0;
   virtual void GetVisible(bool* visible) const = 0;
@@ -52,7 +55,7 @@ class UIElement {
       const = 0;
 
   template <typename BackingT, typename T>
-  static BackingT* GetBackingElement(UIElement* element) {
+  static BackingT* GetBackingElement(const UIElement* element) {
     return T::From(element);
   };
 
