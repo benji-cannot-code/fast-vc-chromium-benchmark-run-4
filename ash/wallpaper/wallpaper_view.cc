65f8783d21cfbb348f771a1139bca5693694ca67
Wenzhao Zang <wzang@chromium.org>
2017-08-08 01:37:01 +0000

    Let |WallpaperController| handle wallpaper dimming and blur
    Wallpaper dimming and blur should be applied at the same time when the
    wallpaper is being set to avoid two types of delays: the delay during
    login screen startup, and the delay (inconsistent timing) when
    tabbing through focused user pods at login screen (because the
    wallpaper of the focused pod is shown at login screen).
    
    There's a follow-up CL that cleans the existing HTML-based wallpaper
    dimming.
    
    Bug: 733409, 751248
    Change-Id: Ifc19751e28d6e7bf5d500423e83b57874025a20f
    Reviewed-on: https://chromium-review.googlesource.com/598938
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Reviewed-by: Biao She <bshe@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492496}

--- ash/wallpaper/wallpaper_view.cc
+++ ash/wallpaper/wallpaper_view.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/wallpaper/wallpaper_view.h"
 
+#include "ash/login/ui/login_constants.h"
 #include "ash/root_window_controller.h"
 #include "ash/session/session_controller.h"
 #include "ash/shell.h"
@@ -19,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/manager/managed_display_info.h"
 #include "ui/display/screen.h"
 #include "ui/gfx/canvas.h"
+#include "ui/gfx/color_analysis.h"
+#include "ui/gfx/color_utils.h"
 #include "ui/gfx/geometry/safe_integer_conversions.h"
 #include "ui/gfx/geometry/size_conversions.h"
 #include "ui/gfx/transform.h"
@@ -71,6 +74,23 @@ class LayerControlView : public views::View {
  private:
   DISALLOW_COPY_AND_ASSIGN(LayerControlView);
 };
+
+// Returns the color used to darken the wallpaper, needed by login, lock, OOBE
+// and add user screens.
+SkColor GetWallpaperDarkenColor() {
+  SkColor darken_color =
+      Shell::Get()->wallpaper_controller()->GetProminentColor(
+          color_utils::ColorProfile(color_utils::LumaRange::DARK,
+                                    color_utils::SaturationRange::MUTED));
+  if (darken_color == WallpaperController::kInvalidColor)
+    darken_color = login_constants::kDefaultBaseColor;
+
+  darken_color = color_utils::GetResultingPaintColor(
+      SkColorSetA(login_constants::kDefaultBaseColor,
+                  login_constants::kTranslucentColorDarkenAlpha),
+      SkColorSetA(darken_color, 0xFF));
+  return SkColorSetA(darken_color, login_constants::kTranslucentAlpha);
+}
 
 }  // namespace
 
@@ -140,6 +160,12 @@ void WallpaperView::OnPaint(gfx::Canvas* canvas) {
   if (wallpaper.isNull())
     return;
 
+  cc::PaintFlags flags;
+  if (Shell::Get()->session_controller()->IsUserSessionBlocked()) {
+    flags.setColorFilter(SkColorFilter::MakeModeFilter(
+        GetWallpaperDarkenColor(), SkBlendMode::kDarken));
+  }
+
   if (layout == wallpaper::WALLPAPER_LAYOUT_CENTER_CROPPED) {
     // The dimension with the smallest ratio must be cropped, the other one
     // is preserved. Both are set in gfx::Size cropped_size.
@@ -165,13 +191,14 @@ void WallpaperView::OnPaint(gfx::Canvas* canvas) {
     canvas->DrawImageInt(
         wallpaper, wallpaper_cropped_rect.x(), wallpaper_cropped_rect.y(),
         wallpaper_cropped_rect.width(), wallpaper_cropped_rect.height(), 0, 0,
-        width(), height(), true);
+        width(), height(), true, flags);
   } else if (layout == wallpaper::WALLPAPER_LAYOUT_TILE) {
-    canvas->TileImageInt(wallpaper, 0, 0, width(), height());
+    canvas->TileImageInt(wallpaper, 0, 0, 0, 0, width(), height(), 1.0f,
+                         &flags);
   } else if (layout == wallpaper::WALLPAPER_LAYOUT_STRETCH) {
     // This is generally not recommended as it may show artifacts.
     canvas->DrawImageInt(wallpaper, 0, 0, wallpaper.width(), wallpaper.height(),
-                         0, 0, width(), height(), true);
+                         0, 0, width(), height(), true, flags);
   } else {
     float image_scale = canvas->image_scale();
     gfx::Rect wallpaper_rect(0, 0, wallpaper.width() / image_scale,
@@ -180,7 +207,8 @@ void WallpaperView::OnPaint(gfx::Canvas* canvas) {
     canvas->DrawImageInt(wallpaper, 0, 0, wallpaper.width(), wallpaper.height(),
                          (width() - wallpaper_rect.width()) / 2,
                          (height() - wallpaper_rect.height()) / 2,
-                         wallpaper_rect.width(), wallpaper_rect.height(), true);
+                         wallpaper_rect.width(), wallpaper_rect.height(), true,
+                         flags);
   }
 }
 
@@ -236,6 +264,9 @@ views::Widget* CreateWallpaper(aura::Window* root_window, int container_id) {
 
   aura::Window* container = root_window->GetChildById(container_id);
   wallpaper_widget->SetBounds(container->bounds());
+  if (Shell::Get()->session_controller()->IsUserSessionBlocked())
+    wallpaper_widget->GetLayer()->SetLayerBlur(login_constants::kBlurSigma);
+
   return wallpaper_widget;
 }
 
