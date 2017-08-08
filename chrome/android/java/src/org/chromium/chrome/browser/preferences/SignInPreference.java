498aef78e2f04867710660ecf55029ae9ea64e64
Iulia Harasim <iuliah@google.com>
2017-08-08 15:44:15 +0000

    [Android] Update SigninPromoController
    This CL makes a couple of changes to SigninPromoController. The 'Sign
    in' button now redirects the user directly to the 'Create a new account'
    screen. SigninPromoController is updated to support the recording
    of impressions depending on the place the promos is shown and now the
    promo programatically sets the text depending on the access point.
    
    Bug: 737743
    Change-Id: I19da34606c3d901c2802399c0af7f97f292d3cfc
    Reviewed-on: https://chromium-review.googlesource.com/600219
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Iulia Harasim <iuliah@google.com>
    Cr-Commit-Position: refs/heads/master@{#492636}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/SignInPreference.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/SignInPreference.java
@@ -98,7 +98,7 @@ public class SignInPreference
         if (SigninManager.get(getContext()).isSigninDisabledByPolicy()) {
             setupSigninDisabled();
         } else if (accountName == null) {
-            if (SigninPromoController.shouldShowPromo()) {
+            if (SigninPromoController.shouldShowPromo(SigninAccessPoint.SETTINGS)) {
                 setupNewPromo();
             } else {
                 setupOldPromo();
