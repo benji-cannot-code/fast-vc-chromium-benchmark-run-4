ba53ea5d26675c11dbca39018c2ca58085a4704d
Wenzhao Zang <wzang@chromium.org>
2017-08-08 07:11:34 +0000

    Cleans up old account picker use in OOBE
    Fixes the edge case in OOBE that may show the old UI for public
    session pods (see bug). It's not necessary to have a forked md_oobe.js
    because it works with the switch --show-no-md-login (which is used to
    display the old UI).
    
    'oobe-shield' is replaced with 'login-shield' in oobe.js.
    
    Bug: 751922
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I3ee32fed770efdac74e13e253aafecdafed6126f
    Reviewed-on: https://chromium-review.googlesource.com/601439
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492561}

--- chrome/browser/resources/chromeos/login/oobe.js
+++ chrome/browser/resources/chromeos/login/oobe.js
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * This is the main code for the OOBE WebUI implementation.
  */
 
-// <include src="login_shared.js">
+// <include src="md_login_shared.js">
 // <include src="login_non_lock_shared.js">
 // <include src="oobe_screen_auto_enrollment_check.js">
 // <include src="oobe_screen_controller_pairing.js">
