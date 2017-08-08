17784baad58374402afb78160d6242070f3c7ba0
Luca Versari <veluca@google.com>
2017-08-08 14:14:15 +0000

    headless: Fill more information in LoadTimingInfo
    LoadTimingInfo. Because of this, some events (requestStart) were missing
    from the traces when using deterministic fetch. Moreover, this could
    cause events with timestamp 0 to appear when using a custom URLFetcher.
    
    GenericURLRequestJob: :GetLoadTimingInfo did not set all the fields in
    Change-Id: Idee743e8ae4004c9db486c2aa6c4c67bb36b7558
    Reviewed-on: https://chromium-review.googlesource.com/605551
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Commit-Queue: Luca Versari <veluca@google.com>
    Cr-Commit-Position: refs/heads/master@{#492615}

--- headless/public/util/url_fetcher.h
+++ headless/public/util/url_fetcher.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "headless/public/headless_export.h"
+#include "net/base/load_timing_info.h"
 #include "net/base/net_errors.h"
 #include "url/gurl.h"
 
@@ -44,13 +45,16 @@ class HEADLESS_EXPORT URLFetcher {
         const GURL& final_url,
         scoped_refptr<net::HttpResponseHeaders> response_headers,
         const char* body,
-        size_t body_size) = 0;
+        size_t body_size,
+        const net::LoadTimingInfo& load_timing_info) = 0;
 
     // Helper function which extracts the headers from |response_data| and calls
     // OnFetchComplete.
-    void OnFetchCompleteExtractHeaders(const GURL& final_url,
-                                       const char* response_data,
-                                       size_t response_data_size);
+    void OnFetchCompleteExtractHeaders(
+        const GURL& final_url,
+        const char* response_data,
+        size_t response_data_size,
+        const net::LoadTimingInfo& load_timing_info);
 
    protected:
     virtual ~ResultListener() {}
