8a0b57351c623c0822ad338c4d9b4db2d643c4e0
Olivier Robin <olivierrobin@chromium.org>
2017-08-09 10:09:09 +0000

    Remove DCHECK in OpenURL
    On iOS11 beta5, it is possible for the application to be inactive when
    calling openURL.
    Remove the DCHECK.
    
    Bug: 753734
    Change-Id: I627038932c5d2359c3c5199234defddf035101a3
    Reviewed-on: https://chromium-review.googlesource.com/607871
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492933}

--- ios/chrome/app/main_application_delegate.mm
+++ ios/chrome/app/main_application_delegate.mm
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/app/main_application_delegate.h"
 
-#include "base/ios/ios_util.h"
 #include "base/mac/foundation_util.h"
 #import "ios/chrome/app/application_delegate/app_navigation.h"
 #import "ios/chrome/app/application_delegate/app_state.h"
@@ -246,7 +245,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   BOOL applicationActive =
       [application applicationState] == UIApplicationStateActive;
-  DCHECK(applicationActive || !base::ios::IsRunningOnIOS11OrLater());
 
   return [URLOpener openURL:url
           applicationActive:applicationActive
