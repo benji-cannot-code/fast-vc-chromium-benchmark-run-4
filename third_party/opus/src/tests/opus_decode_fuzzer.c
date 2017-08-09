8ebe1b583b9fe17b26fd2a0396e2582e862ee1dc
flim <flim@chromium.org>
2017-08-09 01:05:31 +0000

    [opus] Update to latest
    The corresponding upstream commit hash is 3fe744ea04fdcc418fb85c2c133d13372ebb019b.
    This pulls in several fixes including a fix for ARM builds with neon disabled.
    
    Bug: 737323
    Change-Id: I1e0e3f5022d17aa7c16b985ba2bc9253f163576b
    Reviewed-on: https://chromium-review.googlesource.com/605970
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Felicia Lim <flim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492797}

--- third_party/opus/src/tests/opus_decode_fuzzer.c
+++ third_party/opus/src/tests/opus_decode_fuzzer.c
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #endif
 
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include "opus.h"
