ddc64b6762de0acb225576238b8835fe3e100d7f
Patti <patricialor@chromium.org>
2017-08-09 00:55:44 +0000

    MD Settings: Show "Ask" in Site Details dropdown for non-default sources.
    Currently, the Site Details UI will show a blank dropdown menu when a site
    setting is set to 'ask', but its source is not 'default'. Add a new non-default
    'ask' value which is shown when the source is non-default.
    
    Bug: 656758
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Iea5cf53e09f4d429938cabe002a84f7651cd4aac
    Reviewed-on: https://chromium-review.googlesource.com/597469
    Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Commit-Queue: Patti <patricialor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492794}

--- chrome/test/data/webui/settings/site_details_tests.js
+++ chrome/test/data/webui/settings/site_details_tests.js
@@ -127,7 +127,7 @@ suite('SiteDetails', function() {
           {
             embeddingOrigin: 'https://foo.com:443',
             origin: 'https://foo.com:443',
-            setting: settings.ContentSetting.BLOCK,
+            setting: settings.ContentSetting.ASK,
             source: settings.SiteSettingSource.POLICY,
           },
         ],
@@ -273,20 +273,19 @@ suite('SiteDetails', function() {
         .then(() => {
           // Make sure initial state is as expected.
           assertEquals(
-              settings.ContentSetting.BLOCK,
-              siteDetailsPermission.site.setting);
+              settings.ContentSetting.ASK, siteDetailsPermission.site.setting);
           assertEquals(
               settings.SiteSettingSource.POLICY,
               siteDetailsPermission.site.source);
           assertEquals(
-              settings.ContentSetting.BLOCK,
+              settings.ContentSetting.ASK,
               siteDetailsPermission.$.permission.value);
 
           // Set new prefs and make sure only that permission is updated.
           var newException = {
             embeddingOrigin: testElement.origin,
             origin: testElement.origin,
-            setting: settings.ContentSetting.ASK,
+            setting: settings.ContentSetting.BLOCK,
             source: settings.SiteSettingSource.DEFAULT,
           };
           browserProxy.resetResolver('getOriginPermissions');
@@ -302,7 +301,8 @@ suite('SiteDetails', function() {
 
           // Check |siteDetailsPermission| now shows the new permission value.
           assertEquals(
-              settings.ContentSetting.ASK, siteDetailsPermission.site.setting);
+              settings.ContentSetting.BLOCK,
+              siteDetailsPermission.site.setting);
           assertEquals(
               settings.SiteSettingSource.DEFAULT,
               siteDetailsPermission.site.source);
