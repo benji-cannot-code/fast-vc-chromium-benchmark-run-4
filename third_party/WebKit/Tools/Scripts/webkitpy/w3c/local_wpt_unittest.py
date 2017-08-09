4386784d3fb4cf80bd87a6301fe85b986340aaf1
Robert Ma <robertma@chromium.org>
2017-08-09 17:01:21 +0000

    Suppress logging when patches fail to apply in importer
    Refactor log handling when a patch cannot be cleanly applied. All error
    messages are now being passed back to high-level callers from utility
    functions, leaving the callers to decide how to handle them: the
    importer will ignore errors to avoid confusion in the log, and the
    exporter will log them more explicitly for examination.
    
    In addition, increase the test coverage regarding patch failures and
    improve documentation.
    
    Bug: 748192
    Change-Id: If6eae2f79f3426b3af97e28a4183c1987fac23ad
    Reviewed-on: https://chromium-review.googlesource.com/596769
    Commit-Queue: Robert Ma <robertma@chromium.org>
    Reviewed-by: Jeff Carpenter <jeffcarp@chromium.org>
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493028}

--- third_party/WebKit/Tools/Scripts/webkitpy/w3c/local_wpt_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/w3c/local_wpt_unittest.py
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import unittest
 
 from webkitpy.common.host_mock import MockHost
+from webkitpy.common.system.executive import ScriptError
+from webkitpy.common.system.executive_mock import MockExecutive, mock_git_commands
 from webkitpy.common.system.filesystem_mock import MockFileSystem
 from webkitpy.w3c.local_wpt import LocalWPT
 
@@ -28,8 +30,6 @@ class LocalWPTTest(unittest.TestCase):
 
     def test_fetch_when_wpt_dir_does_not_exist(self):
         host = MockHost()
-        host.filesystem = MockFileSystem()
-
         local_wpt = LocalWPT(host, 'token')
         local_wpt.fetch()
 
@@ -44,15 +44,12 @@ class LocalWPTTest(unittest.TestCase):
 
     def test_run(self):
         host = MockHost()
-        host.filesystem = MockFileSystem()
         local_wpt = LocalWPT(host, 'token')
         local_wpt.run(['echo', 'rutabaga'])
         self.assertEqual(host.executive.calls, [['echo', 'rutabaga']])
 
     def test_create_branch_with_patch(self):
         host = MockHost()
-        host.filesystem = MockFileSystem()
-
         local_wpt = LocalWPT(host, 'token')
         local_wpt.fetch()
 
@@ -68,3 +65,43 @@ class LocalWPTTest(unittest.TestCase):
             ['git', 'add', '.'],
             ['git', 'commit', '--author', 'author <author@author.com>', '-am', 'message'],
             ['git', 'push', 'origin', 'chromium-export-decafbad']])
+
+    def test_test_patch_success(self):
+        host = MockHost()
+        host.executive = mock_git_commands({
+            'apply': '',
+            'add': '',
+            'diff': 'non-trivial patch',
+            'reset': '',
+            'clean': '',
+            'checkout': '',
+        }, strict=True)
+        local_wpt = LocalWPT(host, 'token')
+
+        self.assertEqual(local_wpt.test_patch('dummy patch'), (True, ''))
+
+    def test_test_patch_empty_diff(self):
+        host = MockHost()
+        host.executive = mock_git_commands({
+            'apply': '',
+            'add': '',
+            'diff': '',
+            'reset': '',
+            'clean': '',
+            'checkout': '',
+        }, strict=True)
+        local_wpt = LocalWPT(host, 'token')
+
+        self.assertEqual(local_wpt.test_patch('dummy patch'), (False, ''))
+
+    def test_test_patch_error(self):
+        def _run_fn(args):
+            if args[0] == 'git' and args[1] == 'apply':
+                raise ScriptError('MOCK failed applying patch')
+            return ''
+
+        host = MockHost()
+        host.executive = MockExecutive(run_command_fn=_run_fn)
+        local_wpt = LocalWPT(host, 'token')
+
+        self.assertEqual(local_wpt.test_patch('dummy patch'), (False, 'MOCK failed applying patch'))
