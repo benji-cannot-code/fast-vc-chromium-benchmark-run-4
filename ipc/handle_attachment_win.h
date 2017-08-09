959dd5761bff60ec799e11cbac835e576ef59e6b
Wez <wez@chromium.org>
2017-08-09 05:51:38 +0000

    Remove unnecessary fields from ipc::HandleAttachmentWin.
    When we migrated to IPC-over-Mojo, we introduced a sender-process handle
    duplication, to adapt between IPC's handle copying semantics and Mojo's
    handle-passing semantics. This duplication removes the need to track
    whether the HandleAttachmentWin owns the HANDLE.
    
    Bug: 
    Change-Id: I3619c4034793365096416b65f9d8ac76fd2d5b99
    Reviewed-on: https://chromium-review.googlesource.com/602829
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492840}

--- ipc/handle_attachment_win.h
+++ ipc/handle_attachment_win.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
-#include "base/process/process_handle.h"
+#include "base/win/scoped_handle.h"
 #include "ipc/handle_win.h"
 #include "ipc/ipc_export.h"
 #include "ipc/ipc_message_attachment.h"
@@ -21,7 +21,7 @@ class IPC_EXPORT HandleAttachmentWin : public MessageAttachment {
  public:
   // This constructor makes a copy of |handle| and takes ownership of the
   // result. Should only be called by the sender of a Chrome IPC message.
-  HandleAttachmentWin(const HANDLE& handle, HandleWin::Permissions permissions);
+  explicit HandleAttachmentWin(const HANDLE& handle);
 
   enum FromWire {
     FROM_WIRE,
@@ -30,27 +30,15 @@ class IPC_EXPORT HandleAttachmentWin : public MessageAttachment {
   // receiver of a Chrome IPC message.
   HandleAttachmentWin(const HANDLE& handle, FromWire from_wire);
 
+  // MessageAttachment interface.
   Type GetType() const override;
 
-  HANDLE get_handle() const { return handle_; }
-
-  // The caller of this method has taken ownership of |handle_|.
-  void reset_handle_ownership() {
-    owns_handle_ = false;
-    handle_ = INVALID_HANDLE_VALUE;
-  }
+  HANDLE Take() { return handle_.Take(); }
 
  private:
   ~HandleAttachmentWin() override;
-  HANDLE handle_;
-  HandleWin::Permissions permissions_;
 
-  // In the sender process, the attachment owns the HANDLE of a newly created
-  // message. The attachment broker will eventually take ownership, and set
-  // this member to |false|.
-  // In the destination process, the attachment owns the Mach port until a call
-  // to ParamTraits<HandleWin>::Read() takes ownership.
-  bool owns_handle_;
+  base::win::ScopedHandle handle_;
 };
 
 }  // namespace internal
