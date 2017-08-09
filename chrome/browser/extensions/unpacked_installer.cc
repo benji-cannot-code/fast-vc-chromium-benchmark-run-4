cc37276bb273059637ce5af5d4446e977f7291f7
Istiaque Ahmed <lazyboy@chromium.org>
2017-08-09 16:00:59 +0000

    [Reland] [TaskScheduler] Migrate some extension installer code to TaskScheduler.
    The original race was due to an orthogal issue that was fixed
    in https://chromium-review.googlesource.com/c/599240
    
    Use ExtensionFileTaskRunner to make sure there are no possibilities
    around accessing installing extensions' resources. Unfortunately,
    this also means we no longer run in USER_BLOCKING priority for these
    tasks. ExtensionFileTaskRunner runs in USER_VISIBLE priority.
    
    
    Bug: 750382
    Bug: 751882
    Change-Id: Ia85904d81949e6194827477199243ab5b56ccb6f
    Reviewed-on: https://chromium-review.googlesource.com/607534
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493004}

--- chrome/browser/extensions/unpacked_installer.cc
+++ chrome/browser/extensions/unpacked_installer.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/thread_restrictions.h"
 #include "chrome/browser/extensions/extension_error_reporter.h"
 #include "chrome/browser/extensions/extension_install_prompt.h"
@@ -23,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/crx_file/id_util.h"
 #include "components/sync/model/string_ordinal.h"
 #include "content/public/browser/browser_thread.h"
+#include "extensions/browser/extension_file_task_runner.h"
 #include "extensions/browser/extension_prefs.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/install/extension_install_ui.h"
@@ -130,9 +132,8 @@ UnpackedInstaller::~UnpackedInstaller() {
 void UnpackedInstaller::Load(const base::FilePath& path_in) {
   DCHECK(extension_path_.empty());
   extension_path_ = path_in;
-  BrowserThread::PostTask(
-      BrowserThread::FILE, FROM_HERE,
-      base::BindOnce(&UnpackedInstaller::GetAbsolutePath, this));
+  GetExtensionFileTaskRunner()->PostTask(
+      FROM_HERE, base::BindOnce(&UnpackedInstaller::GetAbsolutePath, this));
 }
 
 bool UnpackedInstaller::LoadFromCommandLine(const base::FilePath& path_in,
@@ -302,7 +303,7 @@ bool UnpackedInstaller::IsLoadingUnpackedAllowed() const {
 }
 
 void UnpackedInstaller::GetAbsolutePath() {
-  DCHECK_CURRENTLY_ON(BrowserThread::FILE);
+  base::ThreadRestrictions::AssertIOAllowed();
 
   extension_path_ = base::MakeAbsoluteFilePath(extension_path_);
 
@@ -329,13 +330,13 @@ void UnpackedInstaller::CheckExtensionFileAccess() {
     return;
   }
 
-  BrowserThread::PostTask(
-      BrowserThread::FILE, FROM_HERE,
+  GetExtensionFileTaskRunner()->PostTask(
+      FROM_HERE,
       base::BindOnce(&UnpackedInstaller::LoadWithFileAccess, this, GetFlags()));
 }
 
 void UnpackedInstaller::LoadWithFileAccess(int flags) {
-  DCHECK_CURRENTLY_ON(BrowserThread::FILE);
+  base::ThreadRestrictions::AssertIOAllowed();
 
   std::string error;
   extension_ = file_util::LoadExtension(extension_path_, Manifest::UNPACKED,
