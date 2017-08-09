40574f751df8d806ac41665f29d328865cfbeba3
Chris Harrelson <chrishtr@chromium.org>
2017-08-09 06:40:34 +0000

    Re-land: Mark composited selection handles as hidden if they are within an <input> element and outside
    This reverts commit 4630c35d57a5a852828dc9719457398c51d82567, with a fix to WebFrameTest
    to set the correct hit test location for a gesture to lookup the selected region, and
    skipping geometry tests for possibly-hidden selection markers.
    
    Bug: 751068
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ic1e9e3b8598c6bae06854d050ef179f029c2ad6a
    Reviewed-on: https://chromium-review.googlesource.com/603880
    Reviewed-by: Alexandre Elias <aelias@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492861}

--- third_party/WebKit/Source/core/layout/compositing/CompositedSelectionBound.h
+++ third_party/WebKit/Source/core/layout/compositing/CompositedSelectionBound.h
@@ -40,7 +40,8 @@ namespace blink {
 
 struct CompositedSelectionBound {
   STACK_ALLOCATED();
-  CompositedSelectionBound() : layer(nullptr), is_text_direction_rtl(false) {}
+  CompositedSelectionBound()
+      : layer(nullptr), is_text_direction_rtl(false), hidden(false) {}
 
   // The structure describes the position of a caret in space of the
   // GraphicsLayer the caret resides in.  Where edgeTopInLayer is the top point
@@ -51,6 +52,9 @@ struct CompositedSelectionBound {
   FloatPoint edge_bottom_in_layer;
 
   bool is_text_direction_rtl;
+
+  // Whether this bound is hidden (clipped out/occluded).
+  bool hidden;
 };
 
 }  // namespace blink
