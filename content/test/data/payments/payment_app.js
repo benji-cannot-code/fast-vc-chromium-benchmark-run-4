4a4a24cab1a05e74a0a0a12f44f86d910352803c
Jinho Bang <jinho.bang@samsung.com>
2017-08-09 19:11:51 +0000

    PaymentHandler: Implement AbortPaymentEvent in content side.
    The AbortPaymentEvent is used to attempt to abort a payment request in progress.
    
    Related spec change:
      https://github.com/w3c/payment-handler/pull/170
    
    Bug: 736745
    Test: payment_app_provider_impl_unittest.cc, payment_app_browsertest.cc
    Change-Id: I7bbec0cb362c98c7c25518edf405a1a5a96241b9
    Reviewed-on: https://chromium-review.googlesource.com/600748
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Jinho Bang <jinho.bang@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#493087}

--- content/test/data/payments/payment_app.js
+++ content/test/data/payments/payment_app.js
@@ -3,6 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+self.addEventListener('abortpayment', e => {
+  e.respondWith(true);
+});
+
 self.addEventListener('canmakepayment', e => {
   // Note that the following postMessage operations are not normal usage
   // in CanMakePaymentEvent. They are only used for testing purpose.
