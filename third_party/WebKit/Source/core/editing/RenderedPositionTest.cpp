e640cae4ffb87704e341bc682efc8c5a94bdf950
John Mellor <johnme@chromium.org>
2017-08-09 12:28:35 +0000

    Mark RenderedPositionTest.IsVisible as failing on android
    It was recently added in commit 18235e2e87fdbddd735342cdfbcba7df1940b144
    from https://chromium-review.googlesource.com/c/603880
    
    But has failed every build since landing on
    https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Android%20%28Nexus4%29/builds/68199
    
    NOTREECHECKS=true
    NOTRY=true
    BUG=752827,751068
    TBR=chrishtr@chromium.org
    
    Change-Id: I50d550b50d358ba6a8ca1a154427079e0969fff6
    Reviewed-on: https://chromium-review.googlesource.com/608188
    Reviewed-by: John Mellor <johnme@chromium.org>
    Commit-Queue: John Mellor <johnme@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492950}

--- third_party/WebKit/Source/core/editing/RenderedPositionTest.cpp
+++ third_party/WebKit/Source/core/editing/RenderedPositionTest.cpp
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/editing/RenderedPosition.h"
 
+#include "build/build_config.h"
 #include "core/css/CSSStyleDeclaration.h"
 #include "core/editing/EditingTestBase.h"
 #include "core/editing/VisibleUnits.h"
@@ -13,7 +14,13 @@ namespace blink {
 
 class RenderedPositionTest : public EditingTestBase {};
 
-TEST_F(RenderedPositionTest, IsVisible) {
+#if defined(OS_ANDROID)
+// Failing on WebKit Android (Nexus4): https://crbug.com/752827
+#define MAYBE_IsVisible DISABLED_IsVisible
+#else
+#define MAYBE_IsVisible IsVisible
+#endif
+TEST_F(RenderedPositionTest, MAYBE_IsVisible) {
   SetBodyContent(
       "<input id=target width=100 value='test test test test test tes tes test'"
       " style='will-change: transform; font-size:40pt;'/>");
