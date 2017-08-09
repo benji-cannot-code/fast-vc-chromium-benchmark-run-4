c152dfd8d90be91232ae04fc2d17d80dac65dc69
Yutaka Hirano <yhirano@chromium.org>
2017-08-09 02:10:37 +0000

    RDHI::CancelRequestsForRoute must detach only matched detachable loaders
    ResourceDispatcherHostImpl::CancelRequestsForRoute detached all
    detachable resource loaders whose child id matches with the child id of
    the given GlobalFrameRoutingId, but it's too broad. Only detachable
    loaders whose GlobalFrameRoutingId matches with the given one should be
    detached.
    
    Bug: 695939
    Change-Id: I7d0364ddfae0dfea0a1bca70b8fcd9af1a5fd9e9
    Reviewed-on: https://chromium-review.googlesource.com/600148
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492806}

--- content/browser/loader/resource_dispatcher_host_impl.h
+++ content/browser/loader/resource_dispatcher_host_impl.h
@@ -358,6 +358,7 @@ class CONTENT_EXPORT ResourceDispatcherHostImpl
                            DetachableResourceTimesOut);
   FRIEND_TEST_ALL_PREFIXES(ResourceDispatcherHostTest,
                            TestProcessCancelDetachableTimesOut);
+  FRIEND_TEST_ALL_PREFIXES(ResourceDispatcherHostTest, CancelRequestsForRoute);
   FRIEND_TEST_ALL_PREFIXES(SitePerProcessIgnoreCertErrorsBrowserTest,
                            CrossSiteRedirectCertificateStore);
   FRIEND_TEST_ALL_PREFIXES(ResourceDispatcherHostTest, LoadInfo);
