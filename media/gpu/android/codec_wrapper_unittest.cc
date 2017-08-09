739e20acace3291161fc228af5057ee1d8eeaf47
Chris Watkins <watk@chromium.org>
2017-08-09 07:04:14 +0000

    media: Remove timeouts from CodecWrapper's interface
    This is a minor cleanup. The timeouts passed to CodecWrapper are always
    zero right now, and its interface has diverged enough from
    MediaCodecBridge that there's no great reason to keep timeout params.
    
    Bug: 660942
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I07d7e84157c96e08d98bc4b488d2296405fcce99
    Reviewed-on: https://chromium-review.googlesource.com/607733
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Chris Watkins <watk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492876}

--- media/gpu/android/codec_wrapper_unittest.cc
+++ media/gpu/android/codec_wrapper_unittest.cc
@@ -47,8 +47,7 @@ class CodecWrapperTest : public testing::Test {
   std::unique_ptr<CodecOutputBuffer> DequeueCodecOutputBuffer() {
     std::unique_ptr<CodecOutputBuffer> codec_buffer;
     bool eos = false;
-    wrapper_->DequeueOutputBuffer(base::TimeDelta(), nullptr, &eos,
-                                  &codec_buffer);
+    wrapper_->DequeueOutputBuffer(nullptr, &eos, &codec_buffer);
     return codec_buffer;
   }
 
@@ -167,8 +166,7 @@ TEST_F(CodecWrapperTest, FormatChangedStatusIsSwallowed) {
       .WillOnce(Return(MEDIA_CODEC_OUTPUT_FORMAT_CHANGED))
       .WillOnce(Return(MEDIA_CODEC_TRY_AGAIN_LATER));
   std::unique_ptr<CodecOutputBuffer> codec_buffer;
-  auto status = wrapper_->DequeueOutputBuffer(base::TimeDelta(), nullptr,
-                                              nullptr, &codec_buffer);
+  auto status = wrapper_->DequeueOutputBuffer(nullptr, nullptr, &codec_buffer);
   ASSERT_EQ(status, MEDIA_CODEC_TRY_AGAIN_LATER);
 }
 
@@ -177,8 +175,7 @@ TEST_F(CodecWrapperTest, BuffersChangedStatusIsSwallowed) {
       .WillOnce(Return(MEDIA_CODEC_OUTPUT_BUFFERS_CHANGED))
       .WillOnce(Return(MEDIA_CODEC_TRY_AGAIN_LATER));
   std::unique_ptr<CodecOutputBuffer> codec_buffer;
-  auto status = wrapper_->DequeueOutputBuffer(base::TimeDelta(), nullptr,
-                                              nullptr, &codec_buffer);
+  auto status = wrapper_->DequeueOutputBuffer(nullptr, nullptr, &codec_buffer);
   ASSERT_EQ(status, MEDIA_CODEC_TRY_AGAIN_LATER);
 }
 
@@ -186,8 +183,7 @@ TEST_F(CodecWrapperTest, MultipleFormatChangedStatusesIsAnError) {
   EXPECT_CALL(*codec_, DequeueOutputBuffer(_, _, _, _, _, _, _))
       .WillRepeatedly(Return(MEDIA_CODEC_OUTPUT_FORMAT_CHANGED));
   std::unique_ptr<CodecOutputBuffer> codec_buffer;
-  auto status = wrapper_->DequeueOutputBuffer(base::TimeDelta(), nullptr,
-                                              nullptr, &codec_buffer);
+  auto status = wrapper_->DequeueOutputBuffer(nullptr, nullptr, &codec_buffer);
   ASSERT_EQ(status, MEDIA_CODEC_ERROR);
 }
 
