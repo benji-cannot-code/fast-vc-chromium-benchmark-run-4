10b2e17ad575382467111247a24cb66f45622018
Michael Wasserman <msw@chromium.org>
2017-08-09 06:47:32 +0000

    chromeos: Make ShelfWindowWatcher a ShellObserver vs DisplayObserver.
    Follows https://chromium-review.googlesource.com/c/570777
    Eliminates the need to check for already-observed root windows.
    
    Bug: None
    Test: No cros task-manager, settings, or panel shelf item changes.
    Change-Id: I8cf9d79efc44b3a223f99162123ccb4e8a027cd9
    Reviewed-on: https://chromium-review.googlesource.com/607351
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492865}

--- ash/shelf/shelf_window_watcher.h
+++ ash/shelf/shelf_window_watcher.h
@@ -8,10 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <set>
 
+#include "ash/shell_observer.h"
 #include "base/macros.h"
 #include "base/scoped_observer.h"
 #include "ui/aura/window_observer.h"
-#include "ui/display/display_observer.h"
 #include "ui/wm/public/activation_change_observer.h"
 
 namespace ash {
@@ -25,7 +25,7 @@ class ShelfModel;
 // it until the window is closed, even if the window is transiently reparented
 // (e.g. during a drag).
 class ShelfWindowWatcher : public ::wm::ActivationChangeObserver,
-                           public display::DisplayObserver {
+                           public ShellObserver {
  public:
   explicit ShelfWindowWatcher(ShelfModel* model);
   ~ShelfWindowWatcher() override;
@@ -92,11 +92,8 @@ class ShelfWindowWatcher : public ::wm::ActivationChangeObserver,
                          aura::Window* gained_active,
                          aura::Window* lost_active) override;
 
-  // display::DisplayObserver overrides:
-  void OnDisplayAdded(const display::Display& display) override;
-  void OnDisplayRemoved(const display::Display& old_display) override;
-  void OnDisplayMetricsChanged(const display::Display& display,
-                               uint32_t metrics) override;
+  // ShellObserver:
+  void OnRootWindowAdded(aura::Window* root_window) override;
 
   ShelfModel* model_;
 
