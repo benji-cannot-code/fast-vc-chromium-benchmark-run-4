e926d489d48d07dba1d5b7fa2e58704cea2e2ff7
Elly Fong-Jones <ellyjones@chromium.org>
2017-08-09 15:18:15 +0000

    views: show hover shadows for buttons on Mac
    Button hover shadows are implemented as part of the ink drop code, which has a
    separate layer that it uses to draw these kinds of effects. The Mac code thus
    far has stubbed out ink drops, but that prevents the effects layer from being
    created, which prevents drop shadows from being drawn. This change causes
    the ink drop code to still create the effects layer, but to draw the actual
    ink drop itself with zero opacity on button press, so other effects that
    depend on the ink drop code still work.
    
    Note that controls which explicitly set their ink drop opacity (ie, don't just
    use the default) will still get ink drops *even if* PlatformStyle disables
    them. At the moment these controls are almost entirely ChromeOS-specific, but
    a more principled approach to disabling ink drops for Mac might be in order
    later.
    
    Bug: 654783
    Change-Id: I4caeabc6511a22fe3cdd457d0c78c77798a14f56
    Reviewed-on: https://chromium-review.googlesource.com/602253
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Ben Ruthig <bruthig@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492987}

--- ui/views/controls/button/toggle_button_unittest.cc
+++ ui/views/controls/button/toggle_button_unittest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/events/event_utils.h"
 #include "ui/events/test/event_generator.h"
-#include "ui/views/style/platform_style.h"
 #include "ui/views/test/views_test_base.h"
 
 namespace views {
@@ -96,11 +95,7 @@ TEST_F(ToggleButtonTest, ToggleButtonDestroyed) {
   button()->OnMousePressed(ui::MouseEvent(
       ui::ET_MOUSE_PRESSED, center, center, ui::EventTimeForNow(),
       ui::EF_LEFT_MOUSE_BUTTON, ui::EF_LEFT_MOUSE_BUTTON));
-  // On platforms with no ripples, there should never be an ink drop layer.
-  if (PlatformStyle::kUseRipples)
-    EXPECT_EQ(1, counter());
-  else
-    EXPECT_EQ(0, counter());
+  EXPECT_EQ(1, counter());
   delete button();
   EXPECT_EQ(0, counter());
 }
@@ -109,10 +104,7 @@ TEST_F(ToggleButtonTest, ToggleButtonDestroyed) {
 // ToggleButton has focus (and is showing a ripple).
 TEST_F(ToggleButtonTest, ShutdownWithFocus) {
   button()->RequestFocus();
-  if (PlatformStyle::kUseRipples)
-    EXPECT_EQ(1, counter());
-  else
-    EXPECT_EQ(0, counter());
+  EXPECT_EQ(1, counter());
 }
 
 // Verify that ToggleButton::accepts_events_ works as expected.
