e94f03af24a23fe4ddd6fe61d11c6a296e4736b4
Kurt Horimoto <kkhorimoto@chromium.org>
2017-08-09 18:06:02 +0000

    [iOS Clean] Created OverlayService.
    This CL introduces OverlayService, a BrowerStateKeyedService that manages
    enqueueing overlay coordinators and scheduling the overlays to be displayed.
    
    Classes of note:
    
    - OverlayService: Interface to the overlay system.  Concrete subclasses
      are vended by OverlayServiceFactory.
    - OverlayQueue: FIFO queue of overlays that have been added to the service.
    - OverlayCoordinator: A BrowserCoordinator subclass that can be presented
      via OverlayService.  This class manages notifying OverlayQueues of
      coordinator lifecycle events.
    - BrowserOverlayQueue: OverlayQueue for overlays that need to be displayed
      over a particular Browser.
    - WebStateOverlayQueue: OverlayQueue for overlays that need to be displayed
      over a particular WebState's content area.
    - OverlayQueueManager: A BrowserUserData subclass that manages creating
      OverlayQueues for a Browser.  It is a WebStateObserver and creates new
      WebStateOverlayQueues for each WebState that is added to the list.
    - OverlayScheduler: An OverlayQueueObserver that listens for events from
      the OverlayQueues managed by its OverlayQueueManager.  This class is
      responsible for queueing OverlayQueues and switching the foreground tab
      to display the appropriate WebState for OverlayCordinators added to a
      WebStateOverlayQueue.
    
    BUG: none
    Change-Id: I4cc6fca24888b499ce8bece8ca08f1ab0d251a1e
    Reviewed-on: https://chromium-review.googlesource.com/583945
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Colin Blundell (OOO until September 4) <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493052}

---
+++ ios/clean/chrome/browser/ui/overlays/test_helpers/test_overlay_queue.mm
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/clean/chrome/browser/ui/overlays/test_helpers/test_overlay_queue.h"
+
+#import "ios/clean/chrome/browser/ui/overlays/overlay_coordinator+internal.h"
+#import "ios/clean/chrome/browser/ui/overlays/overlay_coordinator.h"
+#import "ios/clean/chrome/browser/ui/overlays/test_helpers/test_overlay_parent_coordinator.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+TestOverlayQueue::TestOverlayQueue()
+    : OverlayQueue(), parent_([[TestOverlayParentCoordinator alloc] init]) {}
+
+void TestOverlayQueue::StartNextOverlay() {
+  [GetFirstOverlay() startOverlayingCoordinator:parent_];
+  OverlayWasStarted();
+}
+
+void TestOverlayQueue::AddOverlay(OverlayCoordinator* overlay) {
+  OverlayQueue::AddOverlay(overlay);
+}
