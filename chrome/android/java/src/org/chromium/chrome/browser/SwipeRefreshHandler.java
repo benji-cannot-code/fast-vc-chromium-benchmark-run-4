61d720b539ce77934b688edc0c775d84b3e1ff48
Matthew Jones <mdjones@chromium.org>
2017-08-09 19:48:42 +0000

    Overscroll from top shows bottom controls
    If the bottom browser controls are hidden and a user is scrolled to
    the top of a page, overscroll first shows the browser controls before
    swipe-to-refresh triggers.
    
    BUG=726145
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I930e4c4a4f94ba784f460918eb38cc053b18b579
    Reviewed-on: https://chromium-review.googlesource.com/608615
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493107}

--- chrome/android/java/src/org/chromium/chrome/browser/SwipeRefreshHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/SwipeRefreshHandler.java
@@ -12,6 +12,7 @@ import android.view.ViewGroup.LayoutParams;
 import org.chromium.base.TraceEvent;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.fullscreen.FullscreenManager;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.third_party.android.swiperefresh.SwipeRefreshLayout;
 import org.chromium.ui.OverscrollRefreshHandler;
@@ -125,6 +126,14 @@ public class SwipeRefreshHandler implements OverscrollRefreshHandler {
 
     @Override
     public boolean start() {
+        FullscreenManager manager = mTab.getFullscreenManager();
+
+        // If the controls are at the bottom and hidden, allow cc to handle the scroll event to show
+        // them.
+        if (manager.areBrowserControlsAtBottom() && manager.getBrowserControlHiddenRatio() > 0) {
+            return false;
+        }
+
         attachSwipeRefreshLayoutIfNecessary();
         return mSwipeRefreshLayout.start();
     }
