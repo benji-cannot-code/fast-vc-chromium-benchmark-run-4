8550d7ddb7fc042ec4a02e9fe27d91f45da746ed
Daniel Cheng <dcheng@chromium.org>
2017-08-09 20:04:43 +0000

    Evolve mojo::CArray/mojo::ConstCArray towards a future base::Span API
    At some point, //base may implement a span abstraction to represent a
    lightweight view over an array of n elements of type T. This simplifies
    CArray/ConstCArray to make it easier for potential migration in the
    future:
    - Add a size deducing helper for constructing from a statically-sized
      array.
    - Reverse size_t and T* arguments to be more consistent with the usual
      argument order in STL.
    - Remove public access to fields and add getters.
    - Remove |max_size|: CArray can't really be resized upward, so just
      implement |subspan()| to support deserialization.
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I0116cd9a1f2e75e26ff515455267725324ee137e
    Reviewed-on: https://chromium-review.googlesource.com/605029
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493114}

--- gpu/ipc/common/mailbox_struct_traits.h
+++ gpu/ipc/common/mailbox_struct_traits.h
@@ -12,13 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace mojo {
 
-// A buffer used to read bytes directly from MailboxDataView to gpu::Mailbox
-// name.
-using MailboxName = CArray<int8_t>;
-
 template <>
 struct StructTraits<gpu::mojom::MailboxDataView, gpu::Mailbox> {
-  static MailboxName name(const gpu::Mailbox& mailbox);
+  static ConstCArray<int8_t> name(const gpu::Mailbox& mailbox) {
+    return mailbox.name;
+  }
   static bool Read(gpu::mojom::MailboxDataView data, gpu::Mailbox* out);
 };
 
--- third_party/WebKit/Source/platform/graphics/CompositorFilterOperations.cpp
+++ third_party/WebKit/Source/platform/graphics/CompositorFilterOperations.cpp
@@ -65,7 +65,8 @@ void CompositorFilterOperations::AppendDropShadowFilter(IntPoint offset,
       gfx_offset, std_deviation, color.Rgb()));
 }
 
-void CompositorFilterOperations::AppendColorMatrixFilter(SkScalar matrix[20]) {
+void CompositorFilterOperations::AppendColorMatrixFilter(
+    const cc::FilterOperation::Matrix& matrix) {
   filter_operations_.Append(
       cc::FilterOperation::CreateColorMatrixFilter(matrix));
 }
