abe9994a0df23497da0f3814afca3212ef57c463
John Bauman <jbauman@chromium.org>
2017-08-09 19:58:13 +0000

    [dxvavda] Remove ScopedExceptionCatcher
    A new VP9 MFT has rolled out that fixes the bug that this was intended
    to catch. This exception catcher didn't seem to get much useful
    information anyway, because the actual hang happened long after any
    interesting exceptions happened.
    
    Bug: 673221,636158
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I33c3807639ebc5f813234f3583c846299267918c
    Reviewed-on: https://chromium-review.googlesource.com/602582
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: John Bauman <jbauman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493111}

--- media/gpu/dxva_video_decode_accelerator_win.cc
+++ media/gpu/dxva_video_decode_accelerator_win.cc
@@ -189,10 +189,6 @@ constexpr const wchar_t* const kMediaFoundationVideoDecoderDLLs[] = {
     L"mf.dll", L"mfplat.dll", L"msmpeg2vdec.dll",
 };
 
-// Vectored exception handlers are global to the entire process, so use TLS to
-// ensure only the thread with the ScopedExceptionCatcher dumps anything.
-base::ThreadLocalStorage::StaticSlot g_catcher_tls_slot = TLS_INITIALIZER;
-
 uint64_t GetCurrentQPC() {
   LARGE_INTEGER perf_counter_now = {};
   // Use raw QueryPerformanceCounter to avoid grabbing locks or allocating
@@ -201,47 +197,9 @@ uint64_t GetCurrentQPC() {
   return perf_counter_now.QuadPart;
 }
 
-// This is information about the last exception. Writing into it may be racy,
-// but that should be ok because the information is only used for debugging.
-DWORD g_last_exception_code;
-uint64_t g_last_exception_time;
 uint64_t g_last_process_output_time;
 HRESULT g_last_unhandled_error;
 HRESULT g_last_device_removed_reason;
-
-LONG CALLBACK VectoredCrashHandler(EXCEPTION_POINTERS* exception_pointers) {
-  if (g_catcher_tls_slot.Get()) {
-    g_last_exception_code = exception_pointers->ExceptionRecord->ExceptionCode;
-    g_last_exception_time = GetCurrentQPC();
-  }
-  return EXCEPTION_CONTINUE_SEARCH;
-}
-
-// The MS VP9 MFT swallows driver exceptions and later hangs because it gets
-// into a weird state. Add a vectored exception handler so information about
-// the exception can be retrieved. See http://crbug.com/636158
-class ScopedExceptionCatcher {
- public:
-  explicit ScopedExceptionCatcher(bool handle_exception) {
-    if (handle_exception) {
-      DCHECK(g_catcher_tls_slot.initialized());
-      g_catcher_tls_slot.Set(static_cast<void*>(this));
-      handler_ = AddVectoredExceptionHandler(1, &VectoredCrashHandler);
-    }
-  }
-
-  ~ScopedExceptionCatcher() {
-    if (handler_) {
-      g_catcher_tls_slot.Set(nullptr);
-      RemoveVectoredExceptionHandler(handler_);
-    }
-  }
-
- private:
-  void* handler_ = nullptr;
-
-  DISALLOW_COPY_AND_ASSIGN(ScopedExceptionCatcher);
-};
 
 }  // namespace
 
@@ -1251,7 +1209,6 @@ DXVAVideoDecodeAccelerator::GetSupportedProfiles(
 
 // static
 void DXVAVideoDecodeAccelerator::PreSandboxInitialization() {
-  g_catcher_tls_slot.Initialize(nullptr);
   for (const wchar_t* mfdll : kMediaFoundationVideoDecoderDLLs)
     ::LoadLibrary(mfdll);
   ::LoadLibrary(L"dxva2.dll");
@@ -1810,13 +1767,10 @@ void DXVAVideoDecodeAccelerator::DoDecode(const gfx::ColorSpace& color_space) {
     MFT_OUTPUT_DATA_BUFFER output_data_buffer = {0};
     DWORD status = 0;
     HRESULT hr;
-    {
-      ScopedExceptionCatcher catcher(using_ms_vp9_mft_);
-      g_last_process_output_time = GetCurrentQPC();
-      hr = decoder_->ProcessOutput(0,  // No flags
-                                   1,  // # of out streams to pull from
-                                   &output_data_buffer, &status);
-    }
+    g_last_process_output_time = GetCurrentQPC();
+    hr = decoder_->ProcessOutput(0,  // No flags
+                                 1,  // # of out streams to pull from
+                                 &output_data_buffer, &status);
     IMFCollection* events = output_data_buffer.pEvents;
     if (events != NULL) {
       DVLOG(1) << "Got events from ProcessOuput, but discarding";
@@ -2070,9 +2024,6 @@ void DXVAVideoDecodeAccelerator::Invalidate() {
 void DXVAVideoDecodeAccelerator::StopDecoderThread() {
   // Try to determine what, if any exception last happened before a hang. See
   // http://crbug.com/613701
-  DWORD last_exception_code = g_last_exception_code;
-  HRESULT last_unhandled_error = g_last_unhandled_error;
-  uint64_t last_exception_time = g_last_exception_time;
   uint64_t last_process_output_time = g_last_process_output_time;
   HRESULT last_device_removed_reason = g_last_device_removed_reason;
   LARGE_INTEGER perf_frequency;
@@ -2087,9 +2038,6 @@ void DXVAVideoDecodeAccelerator::StopDecoderThread() {
       stale_output_picture_buffers_.size();
   PictureBufferMechanism mechanism = GetPictureBufferMechanism();
 
-  base::debug::Alias(&last_exception_code);
-  base::debug::Alias(&last_unhandled_error);
-  base::debug::Alias(&last_exception_time);
   base::debug::Alias(&last_process_output_time);
   base::debug::Alias(&last_device_removed_reason);
   base::debug::Alias(&perf_frequency.QuadPart);
@@ -2293,10 +2241,7 @@ void DXVAVideoDecodeAccelerator::DecodeInternal(
                              this);
   }
   inputs_before_decode_++;
-  {
-    ScopedExceptionCatcher catcher(using_ms_vp9_mft_);
-    hr = decoder_->ProcessInput(0, sample.Get(), 0);
-  }
+  hr = decoder_->ProcessInput(0, sample.Get(), 0);
   // As per msdn if the decoder returns MF_E_NOTACCEPTING then it means that it
   // has enough data to produce one or more output samples. In this case the
   // recommended options are to
