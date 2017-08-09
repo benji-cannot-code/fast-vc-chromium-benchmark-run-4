b5aea66ef24fc282834fdc0ee1e62404d685f052
dpapad <dpapad@chromium.org>
2017-08-09 06:28:00 +0000

    ES6 JS styleguide: Allow usage of for..of syntax.
    Bug: 671426
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I20582c4e3ea9c68b39a5dce0386f813cc15bc93b
    Reviewed-on: https://chromium-review.googlesource.com/606690
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492855}

--- chrome/browser/resources/settings/appearance_page/fonts_browser_proxy.js
+++ chrome/browser/resources/settings/appearance_page/fonts_browser_proxy.js
@@ -5,7 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * @typedef {{
- *   fontList: Array<{0: string, 1: (string|undefined), 2: (string|undefined)}>,
+ *   fontList: !Array<{
+ *       0: string,
+ *       1: (string|undefined),
+ *       2: (string|undefined)}>,
  *   extensionUrl: string
  * }}
  */
