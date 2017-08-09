a30fbb76f36868567b2d4f4a00b5672a976f50c7
Tibor Goldschwendt <tiborg@chromium.org>
2017-08-09 18:33:40 +0000

    [vr] Get incognito state from tab instead of the web content.
    Also fixes the wrong color scheme on the incognito NTP.
    
    Bug: 743253
    Change-Id: I86428d79353ca9d7668632dac3e93ea9e1d3e316
    Reviewed-on: https://chromium-review.googlesource.com/580542
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493067}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
@@ -1021,6 +1021,7 @@ public class Tab
     /**
      * @return Whether or not the {@link Tab} represents a {@link NativePage}.
      */
+    @CalledByNative
     public boolean isNativePage() {
         return mNativePage != null;
     }
