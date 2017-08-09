590f291f420e90fd7c238f0ceb09d36a80d86185
Matt Mueller <mattm@chromium.org>
2017-08-09 20:27:49 +0000

    Remove X509Certificate::CreateOSCertHandleFromBytesWithNickname. Nothing seems to need it.
    Bug: 671420
    Change-Id: Ib471fec931482f92ec9931b3ab46c68588f9e356
    Reviewed-on: https://chromium-review.googlesource.com/606772
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493124}

--- net/cert/x509_certificate_nss.cc
+++ net/cert/x509_certificate_nss.cc
@@ -306,14 +306,6 @@ bool X509Certificate::IsSameOSCert(X509Certificate::OSCertHandle a,
 X509Certificate::OSCertHandle X509Certificate::CreateOSCertHandleFromBytes(
     const char* data,
     size_t length) {
-  return CreateOSCertHandleFromBytesWithNickname(data, length, NULL);
-}
-
-// static
-X509Certificate::OSCertHandle
-X509Certificate::CreateOSCertHandleFromBytesWithNickname(const char* data,
-                                                         size_t length,
-                                                         const char* nickname) {
   crypto::EnsureNSSInit();
 
   if (!NSS_IsInitialized())
@@ -326,8 +318,8 @@ X509Certificate::CreateOSCertHandleFromBytesWithNickname(const char* data,
 
   // Parse into a certificate structure.
   return CERT_NewTempCertificate(CERT_GetDefaultCertDB(), &der_cert,
-                                 const_cast<char*>(nickname),
-                                 PR_FALSE, PR_TRUE);
+                                 nullptr /* nickname */, PR_FALSE /* is_perm */,
+                                 PR_TRUE /* copyDER */);
 }
 
 // static
