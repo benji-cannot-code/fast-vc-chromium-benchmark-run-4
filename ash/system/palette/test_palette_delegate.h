5a33218b03640dd830b638bd3cfd40137389f692
Makoto Shimazu <shimazu@chromium.org>
2017-08-09 08:43:37 +0000

    Revert "Reland: Add a test for highlighter/palette interaction"
    This reverts commit a2429dc8522667d64e8ff8b943ce02315a2169d0.
    
    Reason for revert: This patch seems causing failure on MSan/LSan.
    See also https://crbug.com/753720 for detail.
    
    Original change's description:
    > Reland: Add a test for highlighter/palette interaction
    > 
    > (Original CL reverted due to an uninitialized pointer)
    > 
    > Provide additional test coverage for the interaction
    > between HighlighterController and the stylus palette.
    > 
    > TBR=jamescook@chromium.org
    > 
    > Bug: 750235
    > Change-Id: I69eee67062b515cc68557b19faf12ad93fd65600
    > Reviewed-on: https://chromium-review.googlesource.com/607467
    > Reviewed-by: Vladislav Kaznacheev <kaznacheev@chromium.org>
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Commit-Queue: Vladislav Kaznacheev <kaznacheev@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#492833}
    
    TBR=jamescook@chromium.org,kaznacheev@chromium.org
    
    Change-Id: Id70078cf9a48f73c5b8eb34a46f066cb2651de45
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 750235
    Reviewed-on: https://chromium-review.googlesource.com/606749
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492910}

--- ash/system/palette/test_palette_delegate.h
+++ ash/system/palette/test_palette_delegate.h
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-class HighlighterControllerTestApi;
-
 // A simple test double for a PaletteDelegate.
 class TestPaletteDelegate : public PaletteDelegate {
  public:
@@ -43,7 +41,9 @@ class TestPaletteDelegate : public PaletteDelegate {
     should_show_palette_ = should_show_palette;
   }
 
-  void SetMetalayerSupported(bool is_metalayer_supported);
+  void set_is_metalayer_supported(bool is_metalayer_supported) {
+    is_metalayer_supported_ = is_metalayer_supported;
+  }
 
   int show_metalayer_count() const { return show_metalayer_count_; }
 
@@ -51,11 +51,7 @@ class TestPaletteDelegate : public PaletteDelegate {
 
   base::Closure metalayer_closed() const { return metalayer_closed_; }
 
-  void set_highlighter_test_api(HighlighterControllerTestApi* api) {
-    highlighter_test_api_ = api;
-  }
-
- protected:
+ private:
   // PaletteDelegate:
   std::unique_ptr<EnableListenerSubscription> AddPaletteEnableListener(
       const EnableListener& on_state_changed) override;
@@ -82,8 +78,6 @@ class TestPaletteDelegate : public PaletteDelegate {
   int show_metalayer_count_ = 0;
   int hide_metalayer_count_ = 0;
   base::Closure metalayer_closed_;
-
-  HighlighterControllerTestApi* highlighter_test_api_ = nullptr;
 
   DISALLOW_COPY_AND_ASSIGN(TestPaletteDelegate);
 };
