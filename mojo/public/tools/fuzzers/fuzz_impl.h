9134a718c2376d0f7eee8442b8f83ad09d0cbd5f
Tim Becker <tjbecker@google.com>
2017-08-09 21:17:43 +0000

    Message Dumper / Corpus Generator for Mojo Fuzzer
    Adds a standalone executable which will dump raw mojo messages to the
    provided directory, which is used to generate a corpus for the
    mojo_parse_message_fuzzer.
    
    R=ochang@chromium.org
    
    Bug: 
    Change-Id: Id94348b6fbbdf1e1222ed6f46d1ec062eb6c7409
    Reviewed-on: https://chromium-review.googlesource.com/602513
    Commit-Queue: Timothy Becker <tjbecker@google.com>
    Reviewed-by: Oliver Chang <ochang@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493140}

---
+++ mojo/public/tools/fuzzers/fuzz_impl.h
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MOJO_PUBLIC_TOOLS_FUZZERS_FUZZ_IMPL_H_
+#define MOJO_PUBLIC_TOOLS_FUZZERS_FUZZ_IMPL_H_
+
+#include "mojo/public/cpp/bindings/binding.h"
+#include "mojo/public/tools/fuzzers/fuzz.mojom.h"
+
+class FuzzImpl : public fuzz::mojom::FuzzInterface {
+ public:
+  explicit FuzzImpl(fuzz::mojom::FuzzInterfaceRequest request);
+  ~FuzzImpl() override;
+
+  void FuzzBasic() override;
+  void FuzzBasicResp(FuzzBasicRespCallback callback) override;
+  void FuzzBasicSyncResp(FuzzBasicSyncRespCallback callback) override;
+  void FuzzArgs(fuzz::mojom::FuzzStructPtr a,
+                fuzz::mojom::FuzzStructPtr b) override;
+
+  void FuzzArgsResp(fuzz::mojom::FuzzStructPtr a,
+                    fuzz::mojom::FuzzStructPtr b,
+                    FuzzArgsRespCallback callback) override;
+  void FuzzArgsSyncResp(fuzz::mojom::FuzzStructPtr a,
+                        fuzz::mojom::FuzzStructPtr b,
+                        FuzzArgsSyncRespCallback callback) override;
+
+  /* Expose the binding to the fuzz harness. */
+  mojo::Binding<FuzzInterface> binding_;
+};
+
+#endif  // MOJO_PUBLIC_TOOLS_FUZZERS_FUZZ_IMPL_H_
