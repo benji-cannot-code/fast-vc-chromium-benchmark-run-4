9910345a32d4228114de3d39937f307c9aef9ef1
Roman Sorokin <rsorokin@chromium.org>
2017-08-09 08:32:37 +0000

    Chromad: Restart authpolicyd on auth fail.
    Makes sure state is cleared and nothing would leak from one user to
    another.
    Also shows error message in case unexpected error occurred during user auth.
    
    Bug: 752905
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Iacc03156055a0b29d66552343c1fecb16f83198e
    Reviewed-on: https://chromium-review.googlesource.com/603691
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Roman Sorokin <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492905}

--- chrome/browser/resources/chromeos/login/screen_gaia_signin.js
+++ chrome/browser/resources/chromeos/login/screen_gaia_signin.js
@@ -1204,6 +1204,7 @@ login.createScreen('GaiaSigninScreen', 'gaia-signin', function() {
       var adAuthUI = this.getSigninFrame_();
       adAuthUI.setUser(username);
       adAuthUI.setInvalid(errorState);
+      this.authCompleted_ = false;
       this.loading = false;
       Oobe.getInstance().headerHidden = false;
     }
