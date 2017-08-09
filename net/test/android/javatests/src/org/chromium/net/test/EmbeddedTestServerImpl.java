5890b296d26aa2ec90bcef101b64a1976a4e8b7a
Emily Stark <estark@google.com>
2017-08-09 22:58:22 +0000

    Expose EmbeddedTestServer's HTTPS abilities to Java
    This change allows Java users of EmbeddedTestServer to configure the server to
    run HTTPS, using the same ServerCertificate options that are available to C++
    users. This will allow us to write integration tests for security UI and will
    allow the replacement of some TestWebServer uses.
    
    One complication is that, for Android tests, EmbeddedTestServer runs in a
    separate process, so its method of installing test root certs doesn't
    work out of the box. To make this work, I've made the test process
    server ask the out-of-process server for its root certificate, which it
    then installs in a test root store.
    
    As a first user of this functionality, I've re-enabled an old flaky omnibox
    test. The test originally was hitting a real external url (google.com) to test
    HTTPS, which I assume was the source of the flakiness. I've rewritten it to hit
    an EmbeddedTestServer HTTPS URL, and updated it for recent UI changes.
    
    Bug: 750343, 736867
    Change-Id: I2f98924d6b9998bc75ec2f74bd8ad5582942cb28
    Reviewed-on: https://chromium-review.googlesource.com/598510
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493168}

--- net/test/android/javatests/src/org/chromium/net/test/EmbeddedTestServerImpl.java
+++ net/test/android/javatests/src/org/chromium/net/test/EmbeddedTestServerImpl.java
@@ -62,10 +62,11 @@ public class EmbeddedTestServerImpl extends IEmbeddedTestServerImpl.Stub {
 
     /** Initialize the native EmbeddedTestServer object.
      *
+     *  @param https True if the server should use HTTPS, and false otherwise.
      *  @return Whether the native object was successfully initialized.
      */
     @Override
-    public boolean initializeNative() {
+    public boolean initializeNative(final boolean https) {
         // This is necessary as EmbeddedTestServerImpl is in a different process than the tests
         // using it, so it needs to initialize its own application context.
         ContextUtils.initApplicationContext(mContext.getApplicationContext());
@@ -83,7 +84,9 @@ public class EmbeddedTestServerImpl extends IEmbeddedTestServerImpl.Stub {
         runOnHandlerThread(new Callable<Void>() {
             @Override
             public Void call() {
-                if (mNativeEmbeddedTestServer == 0) nativeInit(UrlUtils.getIsolatedTestRoot());
+                if (mNativeEmbeddedTestServer == 0) {
+                    nativeInit(UrlUtils.getIsolatedTestRoot(), https);
+                }
                 assert mNativeEmbeddedTestServer != 0;
                 return null;
             }
@@ -108,6 +111,20 @@ public class EmbeddedTestServerImpl extends IEmbeddedTestServerImpl.Stub {
         });
     }
 
+    /** Returns the path to a PEM file containing the server's root certificate.
+     *
+     *  @return The path to a PEM file containing the server's root certificate.
+     */
+    @Override
+    public String getRootCertPemPath() {
+        return runOnHandlerThread(new Callable<String>() {
+            @Override
+            public String call() {
+                return nativeGetRootCertPemPath(mNativeEmbeddedTestServer);
+            }
+        });
+    }
+
     /** Add the default handlers and serve files from the provided directory relative to the
      *  external storage directory.
      *
@@ -120,6 +137,21 @@ public class EmbeddedTestServerImpl extends IEmbeddedTestServerImpl.Stub {
             @Override
             public Void call() {
                 nativeAddDefaultHandlers(mNativeEmbeddedTestServer, directoryPath);
+                return null;
+            }
+        });
+    }
+
+    /** Configure the server to use a particular type of SSL certificate.
+     *
+     * @param serverCertificate The type of certificate the server should use.
+     */
+    @Override
+    public void setSSLConfig(final int serverCertificate) {
+        runOnHandlerThread(new Callable<Void>() {
+            @Override
+            public Void call() {
+                nativeSetSSLConfig(mNativeEmbeddedTestServer, serverCertificate);
                 return null;
             }
         });
@@ -264,12 +296,15 @@ public class EmbeddedTestServerImpl extends IEmbeddedTestServerImpl.Stub {
         mNativeEmbeddedTestServer = 0;
     }
 
-    private native void nativeInit(String testDataDir);
+    private native void nativeInit(String testDataDir, boolean https);
     private native void nativeDestroy(long nativeEmbeddedTestServerAndroid);
     private native boolean nativeStart(long nativeEmbeddedTestServerAndroid);
+    private native String nativeGetRootCertPemPath(long nativeEmbeddedTestServerAndroid);
     private native boolean nativeShutdownAndWaitUntilComplete(long nativeEmbeddedTestServerAndroid);
     private native void nativeAddDefaultHandlers(
             long nativeEmbeddedTestServerAndroid, String directoryPath);
+    private native void nativeSetSSLConfig(
+            long nativeEmbeddedTestServerAndroid, int serverCertificate);
     private native void nativeRegisterRequestHandler(
             long nativeEmbeddedTestServerAndroid, long handler);
     private native String nativeGetURL(long nativeEmbeddedTestServerAndroid, String relativeUrl);
--- net/test/embedded_test_server/embedded_test_server.cc
+++ net/test/embedded_test_server/embedded_test_server.cc
@@ -54,12 +54,13 @@ EmbeddedTestServer::EmbeddedTestServer(Type type)
       weak_factory_(this) {
   DCHECK(thread_checker_.CalledOnValidThread());
 
-  if (is_using_ssl_) {
-    base::ThreadRestrictions::ScopedAllowIO allow_io_for_importing_test_cert;
-    TestRootCerts* root_certs = TestRootCerts::GetInstance();
-    base::FilePath certs_dir(GetTestCertsDirectory());
-    root_certs->AddFromFile(certs_dir.AppendASCII("root_ca_cert.pem"));
-  }
+  if (!is_using_ssl_)
+    return;
+  base::ThreadRestrictions::ScopedAllowIO allow_io_for_importing_test_cert;
+  TestRootCerts* root_certs = TestRootCerts::GetInstance();
+  bool added_root_certs = root_certs->AddFromFile(GetRootCertPemPath());
+  DCHECK(added_root_certs)
+      << "Failed to install root cert from EmbeddedTestServer";
 }
 
 EmbeddedTestServer::~EmbeddedTestServer() {
@@ -183,6 +184,11 @@ bool EmbeddedTestServer::ShutdownAndWaitUntilComplete() {
 
   return PostTaskToIOThreadAndWait(base::Bind(
       &EmbeddedTestServer::ShutdownOnIOThread, base::Unretained(this)));
+}
+
+// static
+base::FilePath EmbeddedTestServer::GetRootCertPemPath() {
+  return GetTestCertsDirectory().AppendASCII("root_ca_cert.pem");
 }
 
 void EmbeddedTestServer::ShutdownOnIOThread() {
