2b939c8b4c43851addb9a8a37bb866c0d1056d47
Egor Pasko <pasko@chromium.org>
2017-08-10 19:05:05 +0000

    Re-land: Disable prerender by default
    Disable prerender to be on the safer side when the study expires.
    
    On top of the original patch (http://crrev.com/491691) these changes are made:
    * remove overrides in fieldtrial_testing_config.json to have the same
      prerender mode in tests as on the official bots
    * SetUpOnMainThread() in prerender_browsertest.cc
      * Avoid crashing when PRERENDER_DISABLED
      * replace ASSERT_* with CHECK* in SetUpOnMainThread to get better
        diagnostics from problems with prerender configuration
    * Enable prerendering for all of tests in prerender_unittest rather than
      selectively
    * For offline_pages/ tests enable prerendering to the correct mode without
      requiring an instance of PrerenderManager - in the current state the
      PrerenderManager is always found, but it was not the case for
      PRERENDER_MODE_DISABLED,
    * The CustomTabs tests force prerendering, but they still require an instance of
      PrerenderManager, which is not available with PRERENDER_MODE_DISABLED, added a
      comment about it.
    
    Bug: 678332, 735153, 520275
    Change-Id: I4761e0b99ebca615402ac1e71332c79bd3e4899f
    Reviewed-on: https://chromium-review.googlesource.com/603614
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493484}

--- chrome/browser/extensions/activity_log/activity_log_unittest.cc
+++ chrome/browser/extensions/activity_log/activity_log_unittest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/prerender/prerender_handle.h"
 #include "chrome/browser/prerender/prerender_manager.h"
 #include "chrome/browser/prerender/prerender_manager_factory.h"
+#include "chrome/browser/prerender/prerender_test_utils.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
@@ -243,6 +244,9 @@ TEST_F(ActivityLogTest, LogPrerender) {
   ASSERT_TRUE(GetDatabaseEnabled());
   GURL url("http://www.google.com");
 
+  prerender::test_utils::RestorePrerenderMode restore_prerender_mode;
+  prerender::PrerenderManager::SetOmniboxMode(
+      prerender::PrerenderManager::PRERENDER_MODE_ENABLED);
   prerender::PrerenderManager* prerender_manager =
       prerender::PrerenderManagerFactory::GetForBrowserContext(profile());
 
--- chrome/browser/predictors/autocomplete_action_predictor_unittest.cc
+++ chrome/browser/predictors/autocomplete_action_predictor_unittest.cc
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/prerender/prerender_field_trial.h"
+#include "chrome/browser/prerender/prerender_manager.h"
+#include "chrome/browser/prerender/prerender_test_utils.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/history/core/browser/history_service.h"
@@ -349,6 +351,9 @@ TEST_F(AutocompleteActionPredictorTest, RecommendActionURL) {
 
   AutocompleteMatch match;
   match.type = AutocompleteMatchType::HISTORY_URL;
+  prerender::test_utils::RestorePrerenderMode restore_prerender_mode;
+  prerender::PrerenderManager::SetMode(
+      prerender::PrerenderManager::PRERENDER_MODE_NOSTATE_PREFETCH);
 
   for (size_t i = 0; i < arraysize(test_url_db); ++i) {
     match.destination_url = GURL(test_url_db[i].url);
@@ -363,6 +368,9 @@ TEST_F(AutocompleteActionPredictorTest, RecommendActionSearch) {
 
   AutocompleteMatch match;
   match.type = AutocompleteMatchType::SEARCH_WHAT_YOU_TYPED;
+  prerender::test_utils::RestorePrerenderMode restore_prerender_mode;
+  prerender::PrerenderManager::SetMode(
+      prerender::PrerenderManager::PRERENDER_MODE_NOSTATE_PREFETCH);
 
   for (size_t i = 0; i < arraysize(test_url_db); ++i) {
     match.destination_url = GURL(test_url_db[i].url);
