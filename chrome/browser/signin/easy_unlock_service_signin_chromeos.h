d91b8f00367f08d5ca173bc4b414cf68a070b2a7
Tim Song <tengs@chromium.org>
2017-08-10 23:12:13 +0000

    [EasyUnlock] Control login flow based on is_chromeos_login_enabled pref.
    The login flow was previously controlled by a flag, which this CL removes. Now,
    the EasyUnlock login flow can be enabled or disabled through chrome://settings.
    
    This CL also does some much needed refactoring in EasyUnlockServiceSignIn in
    order to clean up some UI glitches when EasyUnlock is not enabled or allowed.
    
    BUG=750893
    
    Change-Id: I4398e185a67ea1554756970a13dc1d2194567e8d
    Reviewed-on: https://chromium-review.googlesource.com/601216
    Commit-Queue: Tim Song <tengs@chromium.org>
    Reviewed-by: Gustavo Sacomoto <sacomoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493589}

--- chrome/browser/signin/easy_unlock_service_signin_chromeos.h
+++ chrome/browser/signin/easy_unlock_service_signin_chromeos.h
@@ -110,6 +110,7 @@ class EasyUnlockServiceSignin
   bool IsEnabled() const override;
   void OnWillFinalizeUnlock(bool success) override;
   void OnSuspendDoneInternal() override;
+  void OnBluetoothAdapterPresentChanged() override;
 
   // proximity_auth::ScreenlockBridge::Observer implementation:
   void OnScreenDidLock(proximity_auth::ScreenlockBridge::LockHandler::ScreenType
@@ -134,6 +135,10 @@ class EasyUnlockServiceSignin
   // If the device data has been loaded for the current user, returns it.
   // Otherwise, returns NULL.
   const UserData* FindLoadedDataForCurrentUser() const;
+
+  // Shows the hardlock or connecting state as initial UI before cryptohome
+  // keys checking and state update from the app.
+  void ShowInitialUserPodState();
 
   // User id of the user currently associated with the service.
   AccountId account_id_;
--- components/proximity_auth/switches.h
+++ components/proximity_auth/switches.h
@@ -10,7 +10,6 @@ namespace proximity_auth {
 namespace switches {
 
 extern const char kDisableBluetoothLowEnergyDiscovery[];
-extern const char kEnableChromeOSLogin[];
 extern const char kEnableForcePasswordReauth[];
 extern const char kForceLoadEasyUnlockAppInTests[];
 
