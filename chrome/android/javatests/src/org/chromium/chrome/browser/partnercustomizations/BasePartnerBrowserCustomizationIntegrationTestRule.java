4945136928c9a6376d58550f5775b4ae44d63fa0
Yoland Yan <yolandyan@chromium.org>
2017-08-10 18:30:24 +0000

    Convert InstrumentationTestCase in chrome/ to JUnit4
    For more on JUnit4 migration, please check src/testing/android/docs/junit4.md
    
    Bug: 640116
    Change-Id: Iacde4cbf63158caa77fc4c8f7d839b220d347f46
    Reviewed-on: https://chromium-review.googlesource.com/567582
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Yoland Yan <yolandyan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493474}

---
+++ chrome/android/javatests/src/org/chromium/chrome/browser/partnercustomizations/BasePartnerBrowserCustomizationIntegrationTestRule.java
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.partnercustomizations;
+
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+import org.chromium.chrome.browser.ChromeActivity;
+import org.chromium.chrome.test.ChromeActivityTestRule;
+import org.chromium.chrome.test.partnercustomizations.TestPartnerBrowserCustomizationsProvider;
+
+/**
+ * Basic shared functionality for partner customization integration tests.
+ */
+public class BasePartnerBrowserCustomizationIntegrationTestRule
+        extends ChromeActivityTestRule<ChromeActivity> {
+    public BasePartnerBrowserCustomizationIntegrationTestRule() {
+        super(ChromeActivity.class);
+    }
+
+    @Override
+    public Statement apply(final Statement base, Description desc) {
+        return super.apply(new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                PartnerBrowserCustomizations.ignoreBrowserProviderSystemPackageCheckForTests(true);
+                PartnerBrowserCustomizations.setProviderAuthorityForTests(
+                        TestPartnerBrowserCustomizationsProvider.class.getName());
+                base.evaluate();
+            }
+        }, desc);
+    }
+}
