5b8160eef036da877d09421a3c8de605027b323d
Eric Holk <eholk@chromium.org>
2017-08-10 21:06:36 +0000

    Dynamically adjust address space limit for array buffer reservations
    WebAssembly memories with guard regions can quickly exhaust our address
    space. This change raises the maximum address space limit on Linux x64
    when WebAssembly's trap-based bounds checking feature is enabled to
    4TiB. The soft limit stays at 16GiB, but is dynamically adjusted when
    large WebAssembly memory regions are allocated and freed.
    
    Bug: 750378
    Change-Id: I7c58c39ca93eb38f9c23c0954d6fb3fa1f26f118
    Reviewed-on: https://chromium-review.googlesource.com/590758
    Commit-Queue: Eric Holk <eholk@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493542}

---
+++ third_party/WebKit/LayoutTests/external/wpt/wasm/create_multiple_memory.worker.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// The WebAssembly spec doesn't specify a limit on how many Memory objects can
+// be allocated. This test makes sure we can have at least two at once.
+
+importScripts("/resources/testharness.js");
+test(function () {
+  const mem1 = new WebAssembly.Memory({initial: 1});
+  const mem2 = new WebAssembly.Memory({initial: 1});
+}, "WebAssembly#CreateMultipleMemories.");
+done();
