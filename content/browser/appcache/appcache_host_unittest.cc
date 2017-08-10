8842ef08d31b8f9e1c04b213aaee34fbce2b76f2
Mark Pilgrim <pilgrim@chromium.org>
2017-08-10 05:10:23 +0000

    Appcache: BindOnce refactoring
    Make AppCacheDispatcherHost::StartUpdateCallback a OnceCallback and convert
    references from Bind to BindOnce.
    
    Bug: 714018
    Change-Id: Ida297bec73d5777e45c3c04f15592fd2a4e353bb
    Reviewed-on: https://chromium-review.googlesource.com/608269
    Commit-Queue: Mark Pilgrim <pilgrim@chromium.org>
    Reviewed-by: Michael Nordman <michaeln@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493240}

--- content/browser/appcache/appcache_host_unittest.cc
+++ content/browser/appcache/appcache_host_unittest.cc
@@ -28,9 +28,6 @@ class AppCacheHostTest : public testing::Test {
   AppCacheHostTest() {
     get_status_callback_ = base::BindRepeating(
         &AppCacheHostTest::GetStatusCallback, base::Unretained(this));
-    start_update_callback_ =
-        base::Bind(&AppCacheHostTest::StartUpdateCallback,
-                   base::Unretained(this));
     swap_cache_callback_ =
         base::Bind(&AppCacheHostTest::SwapCacheCallback,
                    base::Unretained(this));
@@ -156,7 +153,6 @@ class AppCacheHostTest : public testing::Test {
 
   // Mock callbacks we expect to receive from the 'host'
   content::GetStatusCallback get_status_callback_;
-  content::StartUpdateCallback start_update_callback_;
   content::SwapCacheCallback swap_cache_callback_;
 
   AppCacheStatus last_status_result_;
@@ -183,8 +179,10 @@ TEST_F(AppCacheHostTest, Basic) {
   EXPECT_EQ(reinterpret_cast<void*>(1), last_callback_param_);
 
   last_start_result_ = true;
-  host.StartUpdateWithCallback(start_update_callback_,
-                               reinterpret_cast<void*>(2));
+  host.StartUpdateWithCallback(
+      base::BindOnce(&AppCacheHostTest::StartUpdateCallback,
+                     base::Unretained(this)),
+      reinterpret_cast<void*>(2));
   EXPECT_FALSE(last_start_result_);
   EXPECT_EQ(reinterpret_cast<void*>(2), last_callback_param_);
 
