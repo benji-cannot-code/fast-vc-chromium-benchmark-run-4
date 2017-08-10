555ea4ede6d272d978b6376ed7fc35c6dfd054db
edchin <edchin@chromium.org>
2017-08-10 16:18:33 +0000

    [ios] Remove IDC_CLOSE_ALL_TABS and IDC_CLOSE_ALL_INCOGNITO_TABS
    Bug: 738881
    Change-Id: Ic39a8c455bbe3d0347f1ad4ad1982f18e3dc230c
    Reviewed-on: https://chromium-review.googlesource.com/609554
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Ed Chin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493413}

--- ios/chrome/browser/ui/commands/browser_commands.h
+++ ios/chrome/browser/ui/commands/browser_commands.h
@@ -63,6 +63,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Preloads voice search on the current BVC.
 - (void)preloadVoiceSearch;
 
+// Closes all tabs.
+- (void)closeAllTabs;
+
+// Closes all incognito tabs.
+- (void)closeAllIncognitoTabs;
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_UI_COMMANDS_BROWSER_COMMANDS_H_
