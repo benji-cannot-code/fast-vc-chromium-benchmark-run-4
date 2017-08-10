a87b088bd601973e7ced4803286598cdb62c0766
Duc Bui <ducbui@google.com>
2017-08-10 21:43:21 +0000

    Revert "[GRC] Track Expected Task Queueing Duration of tabs in GRC."
    This reverts commit 525244b2766b65ccb317bdf9f387eb3fb7053faf.
    
    Reason for revert: Compilation failure on Linux64/Win64/Mac64 trunk official. https://bugs.chromium.org/p/chromium/issues/detail?id=754400
    
    Original change's description:
    > [GRC] Track Expected Task Queueing Duration of tabs in GRC.
    > 
    > We want to track the smoothness of foreground tabs during session restore,
    > which can be estimated by their ExpectedTaskQueueingDuration.
    > 
    > This patch sends ExpectedTaskQueueingDuration metric measured in Blink
    > RendererScheduler to GRC.
    > 
    > BUG=737817
    > 
    > Change-Id: I54be5ef1171fb885598e259ffe0580631b8e6f9c
    > Reviewed-on: https://chromium-review.googlesource.com/567539
    > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > Reviewed-by: Nate Chapin <japhet@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Ken Rockot <rockot@chromium.org>
    > Reviewed-by: Zhen Wang <zhenw@chromium.org>
    > Commit-Queue: Duc Bui <ducbui@google.com>
    > Cr-Commit-Position: refs/heads/master@{#493501}
    
    TBR=dcheng@chromium.org,nasko@chromium.org,rockot@chromium.org,zhenw@chromium.org,fmeawad@chromium.org,haraken@chromium.org,japhet@chromium.org,oysteine@chromium.org,lpy@chromium.org,ducbui@google.com
    
    Change-Id: Idd2ae215bf2aad017021df73f722a1e2b6e03a47
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 737817
    Reviewed-on: https://chromium-review.googlesource.com/610451
    Reviewed-by: Fadi Meawad <fmeawad@chromium.org>
    Commit-Queue: Fadi Meawad <fmeawad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493555}

--- third_party/WebKit/public/platform/Platform.h
+++ third_party/WebKit/public/platform/Platform.h
@@ -621,8 +621,6 @@ class BLINK_PLATFORM_EXPORT Platform {
 
   virtual InterfaceProvider* GetInterfaceProvider();
 
-  virtual const char* GetBrowserServiceName() const { return ""; }
-
   // Platform events -----------------------------------------------------
   // Device Orientation, Device Motion, Battery, Gamepad.
 
