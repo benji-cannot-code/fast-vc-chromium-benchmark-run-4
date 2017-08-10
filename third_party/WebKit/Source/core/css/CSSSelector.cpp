1aae343c64d1167b91d17c5998d38a85ac223f59
Hayato Ito <hayato@chromium.org>
2017-08-10 08:10:18 +0000

    Revert "Reland: Make /deep/ as no-op and remove ::shadow in dynamic profile"
    This reverts commit b160073dd51148f56b6586a9902b8a24674886c4.
    
    Reason for revert: We've decided to delay the removal of /deep/ from M61 to M63.
    See https://bugs.chromium.org/p/chromium/issues/detail?id=489954#c99 for details.
    
    Original change's description:
    > Reland: Make /deep/ as no-op and remove ::shadow in dynamic profile
    > 
    > Reland the CL [1], which was reverted at [2], since CrOS's OOBE issue was fixed.
    > 
    > - [1]: https://codereview.chromium.org/2778983006
    > - [2]: https://codereview.chromium.org/2885153003
    > 
    > Bug: 489954,723259
    > Change-Id: Ie162502385443a4e9a9cc80444c596458ad1e100
    > Reviewed-on: https://chromium-review.googlesource.com/575322
    > Reviewed-by: Takayoshi Kochi <kochi@chromium.org>
    > Commit-Queue: Hayato Ito <hayato@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#487745}
    
    TBR=kochi@chromium.org,hayato@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 489954, 723259
    Change-Id: I1b58db5bc2f3268f16b832214b87f1d77ada38ee
    Reviewed-on: https://chromium-review.googlesource.com/609602
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Takayoshi Kochi <kochi@chromium.org>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493331}

--- third_party/WebKit/Source/core/css/CSSSelector.cpp
+++ third_party/WebKit/Source/core/css/CSSSelector.cpp
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/HTMLNames.h"
 #include "core/css/CSSMarkup.h"
 #include "core/css/CSSSelectorList.h"
-#include "core/css/parser/CSSParserContext.h"
 #include "platform/wtf/Assertions.h"
 #include "platform/wtf/HashMap.h"
 #include "platform/wtf/StdLibExtras.h"
@@ -497,7 +496,6 @@ void CSSSelector::UpdatePseudoPage(const AtomicString& value) {
 }
 
 void CSSSelector::UpdatePseudoType(const AtomicString& value,
-                                   const CSSParserContext& context,
                                    bool has_arguments,
                                    CSSParserMode mode) {
   DCHECK(match_ == kPseudoClass || match_ == kPseudoElement);
@@ -528,12 +526,9 @@ void CSSSelector::UpdatePseudoType(const AtomicString& value,
     case kPseudoSelection:
     case kPseudoWebKitCustomElement:
     case kPseudoContent:
+    case kPseudoShadow:
     case kPseudoSlotted:
       if (match_ != kPseudoElement)
-        pseudo_type_ = kPseudoUnknown;
-      break;
-    case kPseudoShadow:
-      if (match_ != kPseudoElement || context.IsDynamicProfile())
         pseudo_type_ = kPseudoUnknown;
       break;
     case kPseudoBlinkInternalElement:
@@ -802,7 +797,6 @@ String CSSSelector::SelectorText() const {
         result = " > " + builder.ToString() + result;
         break;
       case kShadowDeep:
-      case kShadowDeepAsDescendant:
         result = " /deep/ " + builder.ToString() + result;
         break;
       case kShadowPiercingDescendant:
