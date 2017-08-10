dbc6d0403d4da069b91f7601da42a1237b54e086
rbpotter <rbpotter@chromium.org>
2017-08-10 17:12:45 +0000

    Print Preview: Fix radio button selection issue
    Print preview relied on relatedTarget to filter for mouse clicks that
    blurred the pages input by selecting the corresponding radio button.
    To populate relatedTarget, the page settings wrapper div needs to
    have a tabindex.
    
    Bug: 751440
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I3475c473451a2ae15aa61f4845a38305e14445cf
    Reviewed-on: https://chromium-review.googlesource.com/607452
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493437}

--- chrome/browser/resources/print_preview/settings/page_settings.js
+++ chrome/browser/resources/print_preview/settings/page_settings.js
@@ -194,6 +194,9 @@ cr.define('print_preview', function() {
      */
     onCustomInputBlur_: function(event) {
       if (this.customInput_.value == '' &&
+          event.relatedTarget !=
+              this.getElement().querySelector(
+                  '.page-settings-print-pages-div') &&
           event.relatedTarget != this.customRadio_) {
         this.allRadio_.checked = true;
       }
