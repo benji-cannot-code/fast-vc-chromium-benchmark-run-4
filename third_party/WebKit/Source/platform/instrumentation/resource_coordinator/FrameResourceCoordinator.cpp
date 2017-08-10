6bd64516f0545f91e44a72b34cde26969c8c50e3
Duc Bui <ducbui@google.com>
2017-08-10 19:38:35 +0000

    [GRC] Track Expected Task Queueing Duration of tabs in GRC.
    We want to track the smoothness of foreground tabs during session restore,
    which can be estimated by their ExpectedTaskQueueingDuration.
    
    This patch sends ExpectedTaskQueueingDuration metric measured in Blink
    RendererScheduler to GRC.
    
    BUG=737817
    
    Change-Id: I54be5ef1171fb885598e259ffe0580631b8e6f9c
    Reviewed-on: https://chromium-review.googlesource.com/567539
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Zhen Wang <zhenw@chromium.org>
    Commit-Queue: Duc Bui <ducbui@google.com>
    Cr-Commit-Position: refs/heads/master@{#493501}

--- third_party/WebKit/Source/platform/instrumentation/resource_coordinator/FrameResourceCoordinator.cpp
+++ third_party/WebKit/Source/platform/instrumentation/resource_coordinator/FrameResourceCoordinator.cpp
@@ -5,22 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "platform/instrumentation/resource_coordinator/FrameResourceCoordinator.h"
 
-#include "services/resource_coordinator/public/cpp/resource_coordinator_features.h"
-#include "services/resource_coordinator/public/interfaces/coordination_unit.mojom-blink.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
 
 namespace blink {
-
-namespace {
-
-void onConnectionError() {}
-
-}  // namespace
-
-// static
-bool FrameResourceCoordinator::IsEnabled() {
-  return resource_coordinator::IsResourceCoordinatorEnabled();
-}
 
 // static
 FrameResourceCoordinator* FrameResourceCoordinator::Create(
@@ -29,21 +16,9 @@ FrameResourceCoordinator* FrameResourceCoordinator::Create(
 }
 
 FrameResourceCoordinator::FrameResourceCoordinator(
-    service_manager::InterfaceProvider* interface_provider) {
-  interface_provider->GetInterface(mojo::MakeRequest(&service_));
-
-  service_.set_connection_error_handler(
-      ConvertToBaseCallback(WTF::Bind(&onConnectionError)));
-}
+    service_manager::InterfaceProvider* interface_provider)
+    : BlinkResourceCoordinatorBase(interface_provider) {}
 
 FrameResourceCoordinator::~FrameResourceCoordinator() = default;
-
-void FrameResourceCoordinator::SetProperty(
-    const resource_coordinator::mojom::blink::PropertyType property_type,
-    const int64_t value) {
-  service_->SetProperty(property_type, value);
-}
-
-DEFINE_TRACE(FrameResourceCoordinator) {}
 
 }  // namespace blink
