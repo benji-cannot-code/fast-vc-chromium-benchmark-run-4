c7ae056c3a3e77bec8e2686d04b1250ec58b3033
lgcheng <lgcheng@google.com>
2017-08-10 22:56:30 +0000

    Arc optin reskin.
    Reskin surface of optin app to match oobe UX. No change of logic/behavior.
    
    Bug: 746569
    Test: Manual.
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ic8bc1cd48ac93fe852ea94ed960ae1d5d9ca4834
    Reviewed-on: https://chromium-review.googlesource.com/609583
    Commit-Queue: Long Cheng <lgcheng@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493581}

--- chrome/browser/resources/chromeos/arc_support/background.js
+++ chrome/browser/resources/chromeos/arc_support/background.js
@@ -44,13 +44,13 @@ var lastFocusedElement = null;
  * Host window inner default width.
  * @const {number}
  */
-var INNER_WIDTH = 960;
+var INNER_WIDTH = 768;
 
 /**
  * Host window inner default height.
  * @const {number}
  */
-var INNER_HEIGHT = 687;
+var INNER_HEIGHT = 640;
 
 
 /**
@@ -86,6 +86,8 @@ class PreferenceCheckbox {
 
     this.checkbox_ = container.querySelector('.checkbox-option');
     this.label_ = container.querySelector('.checkbox-text');
+    this.label_.addEventListener(
+        'click', () => this.onLabelClicked(this.label_));
 
     var learnMoreLink = this.label_.querySelector(learnMoreLinkId);
     if (learnMoreLink) {
@@ -134,8 +136,19 @@ class PreferenceCheckbox {
   /**
    * Called when the "Learn More" link is clicked.
    */
-  onLearnMoreLinkClicked() {
+  onLearnMoreLinkClicked(event) {
     showTextOverlay(this.learnMoreContent_);
+    event.stopPropagation();
+  }
+
+  /**
+   * Called when preference label is clicked. Toggle the sibling checkbox.
+   */
+  onLabelClicked(label) {
+    var checkbox = label.previousElementSibling;
+    if (checkbox && !checkbox.hidden) {
+      checkbox.checked = !checkbox.checked;
+    }
   }
 }
 
@@ -192,7 +205,7 @@ class MetricsPreferenceCheckbox extends PreferenceCheckbox {
   /** Called when "settings" link is clicked. */
   onSettingsLinkClicked(event) {
     chrome.browser.openTab({'url': 'chrome://settings'}, function() {});
-    event.preventDefault();
+    event.stopPropagation();
   }
 }
 
@@ -248,6 +261,7 @@ class TermsOfServicePage {
 
     var scriptSetCountryCode =
         'document.countryCode = \'' + countryCode.toLowerCase() + '\';';
+    scriptSetCountryCode += 'document.viewMode = \'large-view\';';
     this.termsView_.addContentScripts([
       {
         name: 'preProcess',
@@ -275,7 +289,6 @@ class TermsOfServicePage {
     // On managed case, do not show TermsOfService section. Note that the
     // checkbox for the prefereces are still visible.
     var visibility = isManaged ? 'hidden' : 'visible';
-    container.querySelector('#terms-title').style.visibility = visibility;
     container.querySelector('#terms-container').style.visibility = visibility;
 
     // Set event handler for buttons.
