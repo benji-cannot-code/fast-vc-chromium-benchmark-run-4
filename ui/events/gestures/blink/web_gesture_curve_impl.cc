04faf475914815021eb3d5b08bf668347a95f784
Alexandre Elias <aelias@chromium.org>
2017-08-10 01:34:08 +0000

    Remove zero-fling-velocity DCHECKs.
    As of https://codereview.chromium.org/2918053002, zero-velocity flings
    are used and expected to work (because using FlingCancel instead would
    lose the scroller latching information from the first fling which must
    be preserved for middle-click autoscroll).  But I had forgotten to
    verify against a DCHECK-enabled build.  This deletes all three DCHECKs.
    
    BUG=750774,727964
    
    Change-Id: If60f5f7699cf88d87adc642f8bffb95da2db8a3a
    Reviewed-on: https://chromium-review.googlesource.com/607213
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Commit-Queue: Alexandre Elias <aelias@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493215}

--- ui/events/gestures/blink/web_gesture_curve_impl.cc
+++ ui/events/gestures/blink/web_gesture_curve_impl.cc
@@ -33,7 +33,6 @@ namespace {
 std::unique_ptr<GestureCurve> CreateDefaultPlatformCurve(
     blink::WebGestureDevice device_source,
     const gfx::Vector2dF& initial_velocity) {
-  DCHECK(!initial_velocity.IsZero());
   if (device_source == blink::kWebGestureDeviceSyntheticAutoscroll) {
     return base::MakeUnique<FixedVelocityCurve>(initial_velocity,
                                                 base::TimeTicks());
