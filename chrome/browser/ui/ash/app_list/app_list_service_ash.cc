3e393a4d2b73af97064c44683b522f0dc9e5471b
Qiang Xu <warx@chromium.org>
2017-08-10 00:49:47 +0000

    app_list: do not show launcher after app is installed
    Test: test with fullscreen app list flag
    Bug: 754037
    Change-Id: I9ce62d3d608e0526aa62027665ecae374205abbe
    Reviewed-on: https://chromium-review.googlesource.com/609122
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Qiang(Joe) Xu <warx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493196}

--- chrome/browser/ui/ash/app_list/app_list_service_ash.cc
+++ chrome/browser/ui/ash/app_list/app_list_service_ash.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/ash_util.h"
 #include "chrome/browser/ui/ash/launcher/chrome_launcher_controller.h"
 #include "chrome/browser/ui/ash/session_util.h"
+#include "ui/app_list/app_list_features.h"
 #include "ui/app_list/app_list_switches.h"
 #include "ui/app_list/presenter/app_list_presenter_delegate_factory.h"
 #include "ui/app_list/presenter/app_list_presenter_impl.h"
@@ -162,6 +163,8 @@ void AppListServiceAsh::ShowForProfile(Profile* /*default_profile*/) {
 void AppListServiceAsh::ShowForAppInstall(Profile* profile,
                                           const std::string& extension_id,
                                           bool start_discovery_tracking) {
+  if (app_list::features::IsFullscreenAppListEnabled())
+    return;
   ShowAndSwitchToState(app_list::AppListModel::STATE_APPS);
   AppListServiceImpl::ShowForAppInstall(profile, extension_id,
                                         start_discovery_tracking);
