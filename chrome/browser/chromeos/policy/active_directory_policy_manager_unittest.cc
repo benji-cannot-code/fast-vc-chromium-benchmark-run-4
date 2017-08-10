9fd8a66f87443f0a178d13c919ab2050eda8b660
Roman Sorokin <rsorokin@chromium.org>
2017-08-10 09:32:54 +0000

    Fix MSAN and ASAN bots failing for
    Chromad: Get Kerberos files from authpolicyd
    
    The goal is to let Chrome use the user's Kerberos ticket (through GSSAPI) to enable
    single sign-on to other services that use Active Directory authentication.
    
    Fetches credential cache file and krb5.conf file for the user.
    Puts them on the user partition.
    Set environment variables for GSSAPI library.
    
    TBR=xiyuan@chromium.org,ljusten@chromium.org
    
    Bug: 737960
    Change-Id: I5e2129995a7d50b0411545d730545009b880d147
    Reviewed-on: https://chromium-review.googlesource.com/608701
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Roman Sorokin <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493342}

--- chrome/browser/chromeos/policy/active_directory_policy_manager_unittest.cc
+++ chrome/browser/chromeos/policy/active_directory_policy_manager_unittest.cc
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+// TODO(rsorokin): Replace with MockAuthPolicyClient (see
+// https://crbug.com/753355).
 class TestAuthPolicyClient : public chromeos::AuthPolicyClient {
  public:
   void Init(dbus::Bus* bus) override { NOTIMPLEMENTED(); }
@@ -48,6 +50,11 @@ class TestAuthPolicyClient : public chromeos::AuthPolicyClient {
     NOTIMPLEMENTED();
   }
 
+  void GetUserKerberosFiles(const std::string& object_guid,
+                            GetUserKerberosFilesCallback callback) override {
+    NOTIMPLEMENTED();
+  }
+
   void RefreshDevicePolicy(RefreshPolicyCallback callback) override {
     NOTIMPLEMENTED();
   }
@@ -57,6 +64,13 @@ class TestAuthPolicyClient : public chromeos::AuthPolicyClient {
     base::ThreadTaskRunnerHandle::Get()->PostTask(
         FROM_HERE, base::BindOnce(std::move(callback),
                                   refresh_user_policy_callback_success_));
+  }
+
+  void ConnectToSignal(
+      const std::string& signal_name,
+      dbus::ObjectProxy::SignalCallback signal_callback,
+      dbus::ObjectProxy::OnConnectedCallback on_connected_callback) override {
+    NOTIMPLEMENTED();
   }
 
   void SetRefreshUserPolicyCallbackSuccess(bool success) {
