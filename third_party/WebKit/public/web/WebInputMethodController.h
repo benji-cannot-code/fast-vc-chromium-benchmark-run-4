dc7e9a7596cd60a108c9fb08ba0e14e3275da701
Ryan Landay <rlanday@chromium.org>
2017-08-10 23:17:50 +0000

    Remove duplicate WebTextInputType enum
    We currently have two copies of WebTextInputType.h, one in public/web/ and one
    in public/platform/, that define the same enum, with the same header include
    guard, but with different enum values (one has a k prefix before all the
    values and one doesn't). This has periodically caused problems with refactoring
    efforts that involve re-naming (and therefore potentially re-ordering) header
    includes.
    
    I investigated and it appears that what happened was the file was moved from
    public/web/ into public/platform/ in
    inadvertently re-added by reverting another CL in
    to remove the version in public/web/. The compilation problems when refactoring
    are because WebInputMethodController.h is (unnecessarily) still including the
    old version of the file (an earlier include is bringing in the correct version).
    So we also need to remove this bad include.
    
    https: //crrev.com/7f4ddc2a0e365f9730782148c6f58444dcf470e8, but the file was
    https: //crrev.com/97b6c2bd1278f651b9b30e22685ede87f761a9ca. So the solution is
    Change-Id: I1fa87d32c76bd9d973364d8d4aeca3ec9ac0a126
    Reviewed-on: https://chromium-review.googlesource.com/611126
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Ryan Landay <rlanday@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493594}

--- third_party/WebKit/public/web/WebInputMethodController.h
+++ third_party/WebKit/public/web/WebInputMethodController.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "public/platform/WebTextInputInfo.h"
 #include "WebCompositionUnderline.h"
-#include "WebTextInputType.h"
 #include "WebWidget.h"
 
 namespace blink {
