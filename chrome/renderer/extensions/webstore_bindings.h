d9b0ae0507cc05548408127a0d8db62b92b39a61
Catherine Mullings <catmullings@chromium.org>
2017-08-10 22:54:06 +0000

    Fix null access of v8::Context in WebstoreBindings
    Fix a crash where WebstoreBindings::InlineInstallResponse() tries to
    access the WebstoreBindings object's v8::context after it has been
    destroyed.
    
    Bug: 729464
    Change-Id: I7b52e5e28471171fa592967b996f4ce043b11ed0
    Reviewed-on: https://chromium-review.googlesource.com/596592
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: catmullings <catmullings@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493580}

--- chrome/renderer/extensions/webstore_bindings.h
+++ chrome/renderer/extensions/webstore_bindings.h
@@ -52,6 +52,9 @@ class WebstoreBindings : public ObjectBackedNativeHandler,
       std::string* webstore_item_id,
       std::string* error);
 
+  // ObjectBackedNativeHandler:
+  void Invalidate() override;
+
   mojom::InlineInstallerAssociatedPtr inline_installer_;
 
   mojo::BindingSet<mojom::InlineInstallProgressListener>
