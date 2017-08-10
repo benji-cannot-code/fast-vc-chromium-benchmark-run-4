5ae3a2b546fcca9929fb68995f66519868e1836d
Timothy Loh <timloh@chromium.org>
2017-08-10 05:28:06 +0000

    Ensure the DSE is in the list of all sites / sites with location permissions
    Bug: 734469
    Change-Id: I4a29e439697ceac725b23354fd4dee38a03f5192
    Reviewed-on: https://chromium-review.googlesource.com/604774
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493270}

--- chrome/browser/android/search_geolocation/search_geolocation_service.cc
+++ chrome/browser/android/search_geolocation/search_geolocation_service.cc
@@ -212,6 +212,13 @@ void SearchGeolocationService::SetDSEGeolocationSetting(bool setting) {
   ResetContentSetting();
 }
 
+url::Origin SearchGeolocationService::GetDSEOriginIfEnabled() {
+  url::Origin dse_origin = delegate_->GetDSEOrigin();
+  if (UseDSEGeolocationSetting(dse_origin))
+    return dse_origin;
+  return url::Origin();
+}
+
 void SearchGeolocationService::Shutdown() {
   delegate_.reset();
 }
