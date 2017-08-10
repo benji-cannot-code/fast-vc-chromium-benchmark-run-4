91cc0353adb30c0897784818479db39ec59943aa
Lucas Furukawa Gadani <lfg@chromium.org>
2017-08-10 00:01:12 +0000

    Fix crash on array accessor in the DOMPlugin object.
    Bug: 753415
    Change-Id: Ie9fb3bb00590f56650ded4232e98e7a3f495d7b5
    Reviewed-on: https://chromium-review.googlesource.com/608847
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Lucas Gadani <lfg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493182}

--- third_party/WebKit/Source/platform/plugins/PluginData.cpp
+++ third_party/WebKit/Source/platform/plugins/PluginData.cpp
@@ -54,7 +54,7 @@ void PluginInfo::AddMimeType(MimeClassInfo* info) {
 }
 
 const MimeClassInfo* PluginInfo::GetMimeClassInfo(size_t index) const {
-  if (index > mimes_.size())
+  if (index >= mimes_.size())
     return nullptr;
   return mimes_[index];
 }
