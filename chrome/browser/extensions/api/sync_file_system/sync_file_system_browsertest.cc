f31d83a033368b3857cc364d219176912a9adf85
Chris Mumford <cmumford@chromium.org>
2017-08-10 22:22:29 +0000

    Use memory Env for SyncFileSystemTest.AuthorizationTest.
    There was a bug where SyncFileSystemTest::in_memory_env_ was always
    NULL which resulted in the default Env being used, thus writing
    test data to disk.
    
    Change-Id: Iaa109953b716523f15f4bc743e9c951222ba1758
    Reviewed-on: https://chromium-review.googlesource.com/601177
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Chris Mumford <cmumford@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493561}

--- chrome/browser/extensions/api/sync_file_system/sync_file_system_browsertest.cc
+++ chrome/browser/extensions/api/sync_file_system/sync_file_system_browsertest.cc
@@ -72,6 +72,7 @@ class SyncFileSystemTest : public extensions::PlatformAppBrowserTest,
   }
 
   void SetUpOnMainThread() override {
+    in_memory_env_.reset(leveldb::NewMemEnv(leveldb::Env::Default()));
     extensions::PlatformAppBrowserTest::SetUpOnMainThread();
     ASSERT_TRUE(base_dir_.CreateUniqueTempDir());
 
