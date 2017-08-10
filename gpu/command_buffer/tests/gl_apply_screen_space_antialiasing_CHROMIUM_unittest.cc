bd127551331788204434c16b9fd97e8cf05cb5c2
Geoff Lang <geofflang@chromium.org>
2017-08-10 17:47:19 +0000

    Always pass the real command line options when initializing gl_tests.
    This caused the validating command buffer to be created in some tests
    despite the --use-passthrough-cmd-decoder flag.
    
    BUG=754008
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I9c01a0d657a7324182b7a82b2805174693ee0491
    Reviewed-on: https://chromium-review.googlesource.com/609267
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Geoff Lang <geofflang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493453}

--- gpu/command_buffer/tests/gl_apply_screen_space_antialiasing_CHROMIUM_unittest.cc
+++ gpu/command_buffer/tests/gl_apply_screen_space_antialiasing_CHROMIUM_unittest.cc
@@ -76,7 +76,7 @@ class GLApplyScreenSpaceAntialiasingCHROMIUMTest : public testing::Test {
   }
 
   void SetUp() override {
-    base::CommandLine command_line(base::CommandLine::NO_PROGRAM);
+    base::CommandLine command_line(*base::CommandLine::ForCurrentProcess());
     GLManager::Options options;
     gl_.InitializeWithCommandLine(options, command_line);
     CheckStatus();
@@ -103,7 +103,7 @@ class GLApplyScreenSpaceAntialiasingCHROMIUMES3Test
     : public GLApplyScreenSpaceAntialiasingCHROMIUMTest {
  protected:
   void SetUp() override {
-    base::CommandLine command_line(base::CommandLine::NO_PROGRAM);
+    base::CommandLine command_line(*base::CommandLine::ForCurrentProcess());
     GLManager::Options options;
     options.context_type = gles2::CONTEXT_TYPE_OPENGLES3;
     gl_.InitializeWithCommandLine(options, command_line);
