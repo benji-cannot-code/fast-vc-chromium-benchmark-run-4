ec1d7b0fb992a618afb6d1a5742d0a5ee5279863
Min Qin <qinmin@chromium.org>
2017-08-10 23:15:46 +0000

    Download: Introduce a new method to create ResourceRequest for network service
    To launch a ThrottlingUrlLoader for download, we need to pass a ResourceRequest,
    instead of a URLRequest.
    This CL adds a method to create the ResourceRequest.
    
    BUG=715630
    
    Change-Id: Ife84d10c391c9d129c16218648e1917c6302d174
    Reviewed-on: https://chromium-review.googlesource.com/602601
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493593}

--- content/browser/download/download_utils.h
+++ content/browser/download/download_utils.h
@@ -10,13 +10,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_errors.h"
 #include "net/cert/cert_status_flags.h"
 
+namespace net {
+class URLRequest;
+}
+
 namespace content {
+
+class DownloadUrlParameters;
+struct ResourceRequest;
 
 // Handle the url request completion status and return the interrupt reasons.
 // |cert_status| is ignored if error_code is not net::ERR_ABORTED.
 DownloadInterruptReason CONTENT_EXPORT HandleRequestCompletionStatus(
     net::Error error_code, bool has_strong_validators,
     net::CertStatus cert_status, DownloadInterruptReason abort_reason);
+
+// Create a ResourceRequest from |params|.
+std::unique_ptr<ResourceRequest> CONTENT_EXPORT CreateResourceRequest(
+    DownloadUrlParameters* params);
+
+// Create a URLRequest from |params|.
+std::unique_ptr<net::URLRequest> CreateURLRequestOnIOThread(
+    DownloadUrlParameters* params);
 
 }  // namespace content
 
