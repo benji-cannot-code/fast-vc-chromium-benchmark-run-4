834a5bec0e73fb45382ad6ea511db86c6723a76c
Ryan Landay <rlanday@chromium.org>
2017-08-10 22:51:28 +0000

    Add SuggestionMarkerReplacementScope and related behavior
    This CL adds a class SuggestionMarkerReplacementScope for controlling how
    suggestion DocumentMarkers are updated in response to edits. When a scope object
    is active, the markers are updated to properly respond to a suggestion being
    applied. When a scope object is not active, the markers are updated to properly
    respond to any other sort of edit.
    
    Bug: 672259
    Change-Id: I60aca1cafbcc93658c04c4f867196514a30fe56f
    Reviewed-on: https://chromium-review.googlesource.com/609097
    Commit-Queue: Ryan Landay <rlanday@chromium.org>
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493578}

---
+++ third_party/WebKit/Source/core/editing/markers/SuggestionMarkerReplacementScope.cpp
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/editing/markers/SuggestionMarkerReplacementScope.h"
+
+namespace blink {
+
+bool SuggestionMarkerReplacementScope::currently_in_scope_ = false;
+
+SuggestionMarkerReplacementScope::SuggestionMarkerReplacementScope() {
+  DCHECK(!currently_in_scope_);
+  currently_in_scope_ = true;
+}
+
+SuggestionMarkerReplacementScope::~SuggestionMarkerReplacementScope() {
+  currently_in_scope_ = false;
+}
+
+// static
+bool SuggestionMarkerReplacementScope::CurrentlyInScope() {
+  return currently_in_scope_;
+}
+
+}  // namespace blink
---
+++ third_party/WebKit/Source/core/editing/markers/SuggestionMarkerReplacementScope.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SuggestionMarkerReplacementScope_h
+#define SuggestionMarkerReplacementScope_h
+
+#include "core/CoreExport.h"
+#include "platform/wtf/Allocator.h"
+#include "platform/wtf/Noncopyable.h"
+
+namespace blink {
+
+class CORE_EXPORT SuggestionMarkerReplacementScope {
+  WTF_MAKE_NONCOPYABLE(SuggestionMarkerReplacementScope);
+  STACK_ALLOCATED();
+
+ public:
+  SuggestionMarkerReplacementScope();
+  ~SuggestionMarkerReplacementScope();
+
+  static bool CurrentlyInScope();
+
+ private:
+  static bool currently_in_scope_;
+};
+
+}  // namespace blink
+
+#endif
