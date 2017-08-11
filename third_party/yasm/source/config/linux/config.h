d7ea1c39c9e0a950a0e3daa78309f5fe8d9e476e
David Benjamin <davidben@chromium.org>
2017-08-11 03:26:44 +0000

    Update to yasm 1.3.0.
    The patches we cherry-picked from upstream are all included in 1.3.0.
    I've also added patches which were previously missing and rewritten the
    instructions to match GN. In config/win, stdint.h is removed because
    MSVC provides it these days. I've also removed the Python build step
    because upstream ships the generated files these days.
    
    To reduce diff noise in the future, I've also trimmed all the generated
    Makefiles we'd check in so it only includes the one line we care about and
    is platform-independent. As this list does not appear to change even in
    upstream, we should perhaps send them a patch to be less clever.
    
    Bug: 753886
    Change-Id: Iaec9ea3bed8f81b31e19b997812467f81b95d6ce
    Reviewed-on: https://chromium-review.googlesource.com/608869
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493656}

--- third_party/yasm/source/config/linux/config.h
+++ third_party/yasm/source/config/linux/config.h
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* Command name to run C preprocessor */
-#define CPP_PROG "gcc -E"
+#define CPP_PROG "cc -E"
 
 /* */
 #define ENABLE_NLS 1
@@ -14,11 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* */
 /* #undef HAVE_CATGETS */
 
-/* Define to 1 if you have the MacOS X function CFLocaleCopyCurrent in the
+/* Define to 1 if you have the Mac OS X function CFLocaleCopyCurrent in the
    CoreFoundation framework. */
 /* #undef HAVE_CFLOCALECOPYCURRENT */
 
-/* Define to 1 if you have the MacOS X function CFPreferencesCopyAppValue in
+/* Define to 1 if you have the Mac OS X function CFPreferencesCopyAppValue in
    the CoreFoundation framework. */
 /* #undef HAVE_CFPREFERENCESCOPYAPPVALUE */
 
@@ -120,7 +120,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define PACKAGE_NAME "yasm"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "yasm 1.2.0"
+#define PACKAGE_STRING "yasm 1.3.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "yasm"
@@ -129,7 +129,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.2.0"
+#define PACKAGE_VERSION "1.3.0"
 
 /* Define to 1 if the C compiler supports function prototypes. */
 #define PROTOTYPES 1
@@ -153,7 +153,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.2.0"
+#define VERSION "1.3.0"
 
 /* Define if using the dmalloc debugging malloc package */
 /* #undef WITH_DMALLOC */
