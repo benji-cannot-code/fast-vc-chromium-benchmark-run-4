246d9d1f51d2d23ead2e013ac231f2e717ef1f63
Patrick Monette <pmonette@chromium.org>
2017-08-11 23:55:52 +0000

    [ModuleDatabase] Better identify microsoft programs
    The publisher check now matches all strings that starts with "Microsoft"
    instead of matching exactly "Microsoft Corporation".
    
    Bug: 754488
    Change-Id: I3351b2c53ddba23cf34b63986690e94a23e1e550
    Reviewed-on: https://chromium-review.googlesource.com/612453
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493887}

--- chrome/browser/conflicts/installed_programs_win.cc
+++ chrome/browser/conflicts/installed_programs_win.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/memory/ptr_util.h"
+#include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/task_scheduler/post_task.h"
 #include "base/win/registry.h"
@@ -114,7 +115,7 @@ void CheckRegistryKeyForInstalledProgram(
   // Ignore Microsoft programs.
   base::string16 publisher;
   if (GetValue(candidate, L"Publisher", &publisher) &&
-      publisher == L"Microsoft Corporation") {
+      base::StartsWith(publisher, L"Microsoft", base::CompareCase::SENSITIVE)) {
     return;
   }
 
