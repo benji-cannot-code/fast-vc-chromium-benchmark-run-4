9fc113bc09a0b11ced88c049edd10ed08b2fd305
Emircan Uysaler <emircan@chromium.org>
2017-08-11 18:52:06 +0000

    Avoid process backgrounding for renderers with an active video capture stream
    This CL follows how audio avoids process backgrounding.
    - We add counter in RenderProcessHost to keep track of active video sessions
    in tab.
    - We increment/decrement this counter through VideoCaptureHost which is the
    browser side communication endpoint for a renderer process using a video
    capture device.
    
    Bug: 753943
    Test: Added browsertests.
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I335d8e7d2efa0806d16f7b30d0d1a3ce8a3abef3
    Reviewed-on: https://chromium-review.googlesource.com/609032
    Commit-Queue: Emircan Uysaler <emircan@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Christian Fremerey <chfremer@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493827}

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -1049,7 +1049,7 @@ void RenderFrameHostImpl::RenderProcessGone(SiteInstanceImpl* site_instance) {
   set_nav_entry_id(0);
 
   if (is_audible_)
-    GetProcess()->OnAudioStreamRemoved();
+    GetProcess()->OnMediaStreamRemoved();
 }
 
 void RenderFrameHostImpl::ReportContentSecurityPolicyViolation(
@@ -1241,9 +1241,9 @@ void RenderFrameHostImpl::OnAudibleStateChanged(bool is_audible) {
   if (is_audible_ == is_audible)
     return;
   if (is_audible)
-    GetProcess()->OnAudioStreamAdded();
+    GetProcess()->OnMediaStreamAdded();
   else
-    GetProcess()->OnAudioStreamRemoved();
+    GetProcess()->OnMediaStreamRemoved();
   is_audible_ = is_audible;
 
   GetFrameResourceCoordinator()->SetProperty(
