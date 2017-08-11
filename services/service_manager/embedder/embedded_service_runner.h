8ed9cf8ded4a53c85e3dacac496632c6b28cbc18
Scott Violet <sky@chromium.org>
2017-08-11 14:25:58 +0000

    Moves EmbeddedServiceRunner::InstanceManager into own .h/.cc.
    And renames to EmbeddedInstanceManager. This patch should have no
    change to functionality (I updated thread checking macros because of
    presubmit warning). I'm moving out to make testing easier. I've
    changed the build file around so in theory no one but the target and
    tests can use it.
    
    BUG=none
    TEST=none
    
    Change-Id: Ife6bdb7d4db1a1edb910c2d9c7b9d0150802b21b
    Reviewed-on: https://chromium-review.googlesource.com/611327
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493726}

--- services/service_manager/embedder/embedded_service_runner.h
+++ services/service_manager/embedder/embedded_service_runner.h
@@ -21,6 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace service_manager {
 
+class EmbeddedInstanceManager;
+
 // Hosts in-process service instances for a given service.
 class SERVICE_MANAGER_EMBEDDER_EXPORT EmbeddedServiceRunner {
  public:
@@ -44,7 +46,7 @@ class SERVICE_MANAGER_EMBEDDER_EXPORT EmbeddedServiceRunner {
   void OnQuit();
 
   // A reference to the instance manager, which may operate on another thread.
-  scoped_refptr<InstanceManager> instance_manager_;
+  scoped_refptr<EmbeddedInstanceManager> instance_manager_;
 
   base::Closure quit_closure_;
 
