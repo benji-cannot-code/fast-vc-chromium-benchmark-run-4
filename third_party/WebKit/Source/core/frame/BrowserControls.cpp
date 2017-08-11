c6eb304da7dc0a42469d77022be06e0299bceda3
Matthew Jones <mdjones@chromium.org>
2017-08-11 17:07:26 +0000

    Fix scrolling issue caused by blink BrowserControls
    LayoutHeight in Blink's BrowserControls is subtracted from the current
    controls offset to get a viewport adjustment. Since the bottom controls
    do not offset the content, taking the bottom controls into consideration
    creates an invalid adjustment and breaks scrolling on certain pages.
    
    BUG=752355
    
    Change-Id: Ic170bdec66cd050b6f515c0755b2db50e025c9dc
    Reviewed-on: https://chromium-review.googlesource.com/611202
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493778}

--- third_party/WebKit/Source/core/frame/BrowserControls.cpp
+++ third_party/WebKit/Source/core/frame/BrowserControls.cpp
@@ -81,8 +81,8 @@ void BrowserControls::ResetBaseline() {
       top_height_ ? ContentOffset() : BottomContentOffset();
 }
 
-float BrowserControls::LayoutHeight() {
-  return shrink_viewport_ ? top_height_ + bottom_height_ : 0;
+float BrowserControls::UnreportedSizeAdjustment() {
+  return (shrink_viewport_ ? top_height_ : 0) - ContentOffset();
 }
 
 float BrowserControls::ContentOffset() {
