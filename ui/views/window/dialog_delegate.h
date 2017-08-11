69035d04c79135f5f8a0761cc2cd7858f230595f
Bret Sepulveda <bsep@chromium.org>
2017-08-11 01:17:08 +0000

    DialogClientView handles dialog content margins.
    Previously DialogDelegateView subclasses had to specify their own
    content margins, and BubbleDialogDelegateView subclasses would set
    their margins on BubbleFrameView. With this patch, they now work the
    same way: DialogClientView insets the content bounds.
    
    However, DialogDelegateView specifies a margin of 0 for now. Giving
    its subclasses a default margin has been deferred to a future patch.
    
    This patch also removes the bubble-specific layout metrics (changing
    all metrics pre-Harmony to 13 dips) and collapses the dialog layout
    metrics to these three:
    * INSETS_DIALOG: The margins around the edge of the dialog.
    * DISTANCE_DIALOG_TITLE_CONTENT_MARGIN: The margins between the title
      and the content.
    * DISTANCE_DIALOG_BUTTON_CONTENT_MARGIN: The margins between the button
      row and the content.
    
    Metrics that are redundant with those have been removed, and
    INSETS_DIALOG_* metrics are now defined in terms of those three.
    
    Bug: 733040
    Change-Id: I054654e6cf6e4e7602972216f2b1f76513135aa2
    Reviewed-on: https://chromium-review.googlesource.com/607398
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493630}

--- ui/views/window/dialog_delegate.h
+++ ui/views/window/dialog_delegate.h
@@ -112,6 +112,9 @@ class VIEWS_EXPORT DialogDelegate : public ui::DialogModel,
   // frame.
   virtual bool ShouldUseCustomFrame() const;
 
+  const gfx::Insets& margins() const { return margins_; }
+  void set_margins(const gfx::Insets& margins) { margins_ = margins; }
+
   // A helper for accessing the DialogClientView object contained by this
   // delegate's Window.
   const DialogClientView* GetDialogClientView() const;
@@ -125,6 +128,9 @@ class VIEWS_EXPORT DialogDelegate : public ui::DialogModel,
   // A flag indicating whether this dialog is able to use the custom frame
   // style for dialogs.
   bool supports_custom_frame_;
+
+  // The margins between the content and the inside of the border.
+  gfx::Insets margins_;
 };
 
 // A DialogDelegate implementation that is-a View. Used to override GetWidget()
