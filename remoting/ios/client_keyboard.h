2e6f66d59354fce25bc9234b2f4aa293a3ee4795
Yuwei Huang <yuweih@chromium.org>
2017-08-11 00:56:48 +0000

    [CRD iOS] Prevent crashing when tapping arrow buttons on the soft keyboard
    The soft keyboard has arrow buttons for moving the cursor. Theoretically
    speaking the buttons should be disabled or have no effect on
    ClientKeyboard since hasText returns NO and it doesn't implement
    UITextInput, but it seems to still try to call into
    [UITextInput selectedTextRange], which crashes the app.
    
    This CL fixes that by implementing the selectedTextRange property to
    prevent the app from crashing. The implementation itself does nothing.
    
    Change-Id: I7562d2e6e9ab0b6044ff443bd2b1ededec4223bb
    Reviewed-on: https://chromium-review.googlesource.com/608751
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493626}

--- remoting/ios/client_keyboard.h
+++ remoting/ios/client_keyboard.h
@@ -26,6 +26,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic) UITextAutocorrectionType autocorrectionType;
 @property(nonatomic) UITextSpellCheckingType spellCheckingType;
 
+// This property comes from the UITextInput protocol. It is called when the user
+// taps the arrow button on the soft keyboard. This property doesn't do anything
+// but is just to prevent the app from crashing. It's probably Apple's bug to
+// show the arrow buttons in the first place.
+// TODO(yuweih): Implement this as arrow key injection once we get the non-text
+// key injection working.
+// TODO(yuweih): Implement the UITextInput protocol to support multi-stage input
+// methods.
+@property(readwrite, copy) UITextRange* selectedTextRange;
+
 // Set to true to show the soft keyboard. Default value is NO.
 @property(nonatomic) BOOL showsSoftKeyboard;
 
--- remoting/ios/client_keyboard.mm
+++ remoting/ios/client_keyboard.mm
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @synthesize keyboardType = _keyboardType;
 @synthesize spellCheckingType = _spellCheckingType;
 
+@synthesize selectedTextRange = _selectedTextRange;
 @synthesize delegate = _delegate;
 
 // TODO(nicholss): For physical keyboard, look at UIKeyCommand
