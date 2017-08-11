116eb28d76059d00ec95e3dfd6fb11ae9651e5e5
yolandyan <yolandyan@chromium.org>
2017-08-10 17:03:24 -0700

    Create Next Gen Parameter Test Framework for JUnit4
    This test framework enables developers to write parameterized tests with simple
    static field. The framework allows both test class parameterization (through
    constructor arguments injection) and test method parameterization (through
    test method arguments injection).
    
    For more detail on how to write parameterized tests using this framework, please
    check the crbug or ExampleParameterizedTest.java file.
    
    BUG=673092
    
    Review-Url: https://codereview.chromium.org/2568633002
    Cr-Commit-Position: refs/heads/master@{#493605}

---
+++ base/test/android/javatests/src/org/chromium/base/test/params/ParameterizedRunnerDelegate.java
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.base.test.params;
+
+import org.junit.runners.model.FrameworkMethod;
+
+import java.util.List;
+
+/**
+ * This interface defines the methods that needs to be overriden for a Runner to
+ * be used by ParameterizedRunner to generate individual runners for parameters.
+ *
+ * To create a ParameterizedRunnerDelegate, extends from any BlockJUnit4Runner
+ * children class. You can copy all the implementation from
+ * org.chromium.base.test.params.BaseJUnit4RunnerDelegate.
+ */
+public interface ParameterizedRunnerDelegate {
+    /**
+     * Override to use DelegateCommon's implementation
+     */
+    void collectInitializationErrors(List<Throwable> errors);
+
+    /**
+     * Override to use DelegateCommon's implementation
+     */
+    List<FrameworkMethod> computeTestMethods();
+
+    /**
+     * Override to use DelegateCommon's implementation
+     */
+    Object createTest();
+}
