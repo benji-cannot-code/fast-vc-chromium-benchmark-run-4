e839f524037839531407f0640dab905e34cd6301
Piotr Swigon <piotrs@chromium.org>
2017-08-11 01:36:02 +0000

    [WebApps] Flag enabling a Minimal-UI display mode for installed PWAs.
    As Minimal-UI is a UX change it is considered useful to put it behind
    a flag to gather feedback from the team before enabling it for a wider
    audience.
    
    Bug: 604390
    Change-Id: I24f4588b357249b06f400e05e53c0805518cecb1
    Reviewed-on: https://chromium-review.googlesource.com/609570
    Commit-Queue: Piotr Swigon <piotrs@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493637}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1125,6 +1125,8 @@ extern const char
 extern const char kPullToRefreshEffectName[];
 extern const char kPullToRefreshEffectDescription[];
 
+extern const char kPwaMinimalUiName[];
+extern const char kPwaMinimalUiDescription[];
 extern const char kPwaPersistentNotificationName[];
 extern const char kPwaPersistentNotificationDescription[];
 
