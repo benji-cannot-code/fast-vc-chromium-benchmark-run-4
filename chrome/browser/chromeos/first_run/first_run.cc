4d15d66368782b44f12b07e8741e9ce82575ebf6
Denis Kuznetsov <antrim@google.com>
2017-08-11 23:43:49 +0000

    Disable first run for mananged profiles
    Bug: 753566
    Change-Id: I653c6476d219363da8f79a3479180cef0f776556
    Reviewed-on: https://chromium-review.googlesource.com/612346
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493883}

--- chrome/browser/chromeos/first_run/first_run.cc
+++ chrome/browser/chromeos/first_run/first_run.cc
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/wizard_controller.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/extensions/extension_service.h"
+#include "chrome/browser/policy/profile_policy_connector.h"
+#include "chrome/browser/policy/profile_policy_connector_factory.h"
 #include "chrome/browser/prefs/pref_service_syncable_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/signin_manager_factory.h"
@@ -72,6 +74,10 @@ void TryLaunchFirstRunDialog(Profile* profile) {
     LaunchDialogForProfile(profile);
     return;
   }
+
+  if (policy::ProfilePolicyConnectorFactory::GetForBrowserContext(profile)
+          ->IsManaged())
+    return;
 
   if (command_line->HasSwitch(::switches::kTestType))
     return;
