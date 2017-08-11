bafe9386f2208d7bb66d15588f0304900f23ab28
Ken MacKay <kmackay@chromium.org>
2017-08-11 17:03:38 +0000

    [Chromecast] Fix use-after-free in CastAudioOutputStreamTest
    BUG= internal b/64165838
    
    Change-Id: Ib55803e462bee58b5d8c91cbd7b3a61ea34fb6f7
    Reviewed-on: https://chromium-review.googlesource.com/611943
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Kenneth MacKay <kmackay@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493776}

--- chromecast/media/audio/cast_audio_output_stream_unittest.cc
+++ chromecast/media/audio/cast_audio_output_stream_unittest.cc
@@ -519,6 +519,7 @@ TEST_F(CastAudioOutputStreamTest, StartStopStart) {
   EXPECT_TRUE(audio_device);
   EXPECT_EQ(FakeMediaPipelineBackend::kStateRunning, GetBackend()->state());
 
+  stream->Stop();
   stream->Close();
 }
 
