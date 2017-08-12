b6fba6a39a0077f6dee837a7301aa6f502ed339c
David Reveman <reveman@chromium.org>
2017-08-12 05:55:26 +0000

    ui: 288x288px camera preview.
    Adjust the picture pane code for a 288px camera preview and
    384px capture. The image preview that is 192px needs to be
    more properly centered as a result.
    
    BUG=730031
    TEST=chrome://settings/people -> "Change picture" -> Camera
    TBR=stenvejb@chromium.org
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ib02dd50553bcbf0c7025333c1738ded86e87d174
    Reviewed-on: https://chromium-review.googlesource.com/605088
    Commit-Queue: David Reveman <reveman@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493963}

--- ui/webui/resources/cr_elements/chromeos/cr_picture/cr_camera.js
+++ ui/webui/resources/cr_elements/chromeos/cr_picture/cr_camera.js
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Dimensions for camera capture.
  * @const
  */
-var CAPTURE_SIZE = {height: 480, width: 480};
+var CAPTURE_SIZE = {height: 384, width: 384};
 
 Polymer({
   is: 'cr-camera',
