ccbae4a13cd165fc74482234658295beb4a0cf79
Tien-Ren Chen <trchen@chromium.org>
2017-08-12 01:49:22 +0000

    [Blink] Fix SVG background under-invalidation
    LayoutView bypasses standard style-diff invalidation logic. Instead
    we detect document background mutation at LayoutBox::StyleWillChange()
    of a <html> or <body> element. This failed to include SVGs, which are
    subject to document background special rule too. This CL simply expands
    the special invalidation in LayoutBox::StyleWillChange to also include
    every kind of document elements so LayoutView is guaranteed to be
    invalidated by background mutation.
    
    BUG=586383
    
    Change-Id: Ia44d4bc3d145b9f4d0b04338fc2877763b9efe92
    Reviewed-on: https://chromium-review.googlesource.com/607647
    Commit-Queue: Tien-Ren Chen <trchen@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493931}

--- third_party/WebKit/Source/core/layout/LayoutBox.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBox.cpp
@@ -191,8 +191,7 @@ void LayoutBox::StyleWillChange(StyleDifference diff,
     // to the canvas. Just dirty the entire canvas when our style changes
     // substantially.
     if ((diff.NeedsFullPaintInvalidation() || diff.NeedsLayout()) &&
-        GetNode() &&
-        (isHTMLHtmlElement(*GetNode()) || isHTMLBodyElement(*GetNode()))) {
+        GetNode() && (IsDocumentElement() || isHTMLBodyElement(*GetNode()))) {
       View()->SetShouldDoFullPaintInvalidation();
 
       if (old_style->HasEntirelyFixedBackground() !=
