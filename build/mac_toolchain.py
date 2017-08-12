12bf3564e18a8240cf03377c8fa6f46e762fedc8
Justin Cohen <justincohen@google.com>
2017-08-12 03:50:36 +0000

    Support pkg install step for Xcode 9 hermetic.
    Xcode 9's actool depends on the Simulator being properly installed, which means
    the proper Xcode run first launch step needs to be called.
    
    This depends on a puppet change to allow calling xcodebuild -runFirstLaunch
    
    Bug: 736060
    Change-Id: Idc00a0d6079999ddb95362cf4204ba577f7fb022
    Reviewed-on: https://chromium-review.googlesource.com/612471
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Sergey Berezin <sergeyberezin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493953}

--- build/mac_toolchain.py
+++ build/mac_toolchain.py
@@ -14,6 +14,7 @@ date:
       user interaction.
 """
 
+from distutils.version import LooseVersion
 import os
 import platform
 import plistlib
@@ -133,10 +134,11 @@ def LoadPlist(path):
     os.unlink(name)
 
 
-def AcceptLicense(target_os):
-  """Use xcodebuild to accept new toolchain license if necessary.  Don't accept
-  the license if a newer license has already been accepted. This only works if
-  xcodebuild and xcode-select are passwordless in sudoers."""
+def FinalizeUnpack(target_os):
+  """Use xcodebuild to accept new toolchain license and run first launch
+  installers if necessary.  Don't accept the license if a newer license has
+  already been accepted. This only works if xcodebuild and xcode-select are
+  passwordless in sudoers."""
 
   # Check old license
   try:
@@ -165,6 +167,11 @@ def AcceptLicense(target_os):
     pass
 
   print "Accepting license."
+  target_version_plist_path = \
+      os.path.join(TOOLCHAIN_BUILD_DIR % target_os,
+                   *['Contents','version.plist'])
+  target_version_plist = LoadPlist(target_version_plist_path)
+  short_version_string = target_version_plist['CFBundleShortVersionString']
   old_path = subprocess.Popen(['/usr/bin/xcode-select', '-p'],
                                stdout=subprocess.PIPE).communicate()[0].strip()
   try:
@@ -172,6 +179,11 @@ def AcceptLicense(target_os):
         TOOLCHAIN_BUILD_DIR % target_os, 'Contents/Developer')
     subprocess.check_call(['sudo', '/usr/bin/xcode-select', '-s', build_dir])
     subprocess.check_call(['sudo', '/usr/bin/xcodebuild', '-license', 'accept'])
+
+    if target_os == 'ios' and \
+        LooseVersion(short_version_string) >= LooseVersion("9.0"):
+      print "Installing packages."
+      subprocess.check_call(['sudo', '/usr/bin/xcodebuild', '-runFirstLaunch'])
   finally:
     subprocess.check_call(['sudo', '/usr/bin/xcode-select', '-s', old_path])
 
@@ -217,7 +229,7 @@ def DownloadHermeticBuild(target_os, default_version, toolchain_filename):
 
   if ReadStampFile(target_os) == toolchain_version:
     print 'Toolchain (%s) is already up to date.' % toolchain_version
-    AcceptLicense(target_os)
+    FinalizeUnpack(target_os)
     return 0
 
   if not CanAccessToolchainBucket():
@@ -235,7 +247,7 @@ def DownloadHermeticBuild(target_os, default_version, toolchain_filename):
     toolchain_file = toolchain_filename % toolchain_version
     toolchain_full_url = TOOLCHAIN_URL + toolchain_file
     DownloadAndUnpack(toolchain_full_url, TOOLCHAIN_BUILD_DIR % target_os)
-    AcceptLicense(target_os)
+    FinalizeUnpack(target_os)
 
     print 'Toolchain %s unpacked.' % toolchain_version
     WriteStampFile(target_os, toolchain_version)
