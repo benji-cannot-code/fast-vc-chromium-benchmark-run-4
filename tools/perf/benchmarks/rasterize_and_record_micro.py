582e950eb29c9de158478b268bc24b2334bb5be2
Annie Sullivan <sullivan@chromium.org>
2017-08-12 00:24:05 +0000

    Add owners to rasterize_and_record benchmarks.
    Change-Id: I72f6033db51fa0d49cae2ac7f00b905389cad1c7
    Reviewed-on: https://chromium-review.googlesource.com/612129
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#493895}

--- tools/perf/benchmarks/rasterize_and_record_micro.py
+++ tools/perf/benchmarks/rasterize_and_record_micro.py
@@ -7,6 +7,7 @@ from core import perf_benchmark
 
 from measurements import rasterize_and_record_micro
 import page_sets
+from telemetry import benchmark
 from telemetry import story
 
 
@@ -44,6 +45,9 @@ class _RasterizeAndRecordMicro(perf_benchmark.PerfBenchmark):
         options.record_repeat, options.timeout, options.report_detailed_results)
 
 
+@benchmark.Owner(
+    emails=['vmpstr@chromium.org', 'wkorman@chromium.org'],
+    component='Internals>Compositing>Rasterization')
 class RasterizeAndRecordMicroTop25(_RasterizeAndRecordMicro):
   """Measures rasterize and record performance on the top 25 web pages.
 
@@ -65,6 +69,9 @@ class RasterizeAndRecordMicroTop25(_RasterizeAndRecordMicro):
     return StoryExpectations()
 
 
+@benchmark.Owner(
+    emails=['vmpstr@chromium.org', 'wkorman@chromium.org'],
+    component='Internals>Compositing>Rasterization')
 class RasterizeAndRecordMicroPartialInvalidation(_RasterizeAndRecordMicro):
   """Measures rasterize and record performance for partial inval. on big pages.
 
