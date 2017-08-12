b55baf865bec8f7bb908e4dab7fd601c2a309fe7
Jeremy Roman <jbroman@chromium.org>
2017-08-12 03:14:21 +0000

    Make ScopedPersistent non-virtual.
    It has no base class and no derived class. Being polymorphic just makes objects
    bigger (to hold a vtable pointer) and possibly force virtual dispatch in cases
    where the concrete type may not be available.
    
    Change-Id: I27956bbb700a584f95bc595008b44abd6cc774a8
    Reviewed-on: https://chromium-review.googlesource.com/611674
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493949}

--- third_party/WebKit/Source/platform/bindings/ScopedPersistent.h
+++ third_party/WebKit/Source/platform/bindings/ScopedPersistent.h
@@ -62,7 +62,7 @@ class ScopedPersistent {
       handle_.Reset(isolate, local);
   }
 
-  virtual ~ScopedPersistent() { Clear(); }
+  ~ScopedPersistent() { Clear(); }
 
   ALWAYS_INLINE v8::Local<T> NewLocal(v8::Isolate* isolate) const {
     return v8::Local<T>::New(isolate, handle_);
@@ -86,7 +86,7 @@ class ScopedPersistent {
   bool IsEmpty() const { return handle_.IsEmpty(); }
   bool IsWeak() const { return handle_.IsWeak(); }
 
-  virtual void Set(v8::Isolate* isolate, v8::Local<T> handle) {
+  void Set(v8::Isolate* isolate, v8::Local<T> handle) {
     handle_.Reset(isolate, handle);
   }
 
