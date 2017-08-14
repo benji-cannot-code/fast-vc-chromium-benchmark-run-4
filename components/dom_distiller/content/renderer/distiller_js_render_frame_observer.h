25827e2f631731b9c92bb3cfbb5c5233b3ebe42e
Joel Hockey <joelhockey@chromium.org>
2017-08-14 04:52:49 +0000

    Remove unused blink public/web imports
    Tbr: mdjones@chromium.org
    Bug: 755046
    Change-Id: Ife6b48d85e14e67a40a07848d815ae682fa8ae53
    Reviewed-on: https://chromium-review.googlesource.com/612030
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494012}

--- components/dom_distiller/content/renderer/distiller_js_render_frame_observer.h
+++ components/dom_distiller/content/renderer/distiller_js_render_frame_observer.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/renderer/render_frame.h"
 #include "content/public/renderer/render_frame_observer.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
-#include "third_party/WebKit/public/web/WebLocalFrame.h"
 #include "v8/include/v8.h"
 
 namespace dom_distiller {
--- components/dom_distiller/content/renderer/distiller_native_javascript.cc
+++ components/dom_distiller/content/renderer/distiller_native_javascript.cc
@@ -15,10 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gin/function_template.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
 #include "third_party/WebKit/public/web/WebKit.h"
-#include "third_party/WebKit/public/web/WebLocalFrame.h"
 #include "v8/include/v8.h"
-
-using blink::WebLocalFrame;
 
 namespace dom_distiller {
 
