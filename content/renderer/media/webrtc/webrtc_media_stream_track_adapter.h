b96fc4adf0a95d810f1e90209aed31a8e5f131aa
Guido Urdaneta <guidou@chromium.org>
2017-08-14 18:51:05 +0000

    Fix race during WebRtcMediaStreamTrackAdapter initialization.
    This does not completely fix bug 751785, but it is a prerequisite.
    
    Bug: 751785
    Change-Id: I3ece316c04f80cbafdb5959d9a19851086a1baf1
    Reviewed-on: https://chromium-review.googlesource.com/610233
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494115}

--- content/renderer/media/webrtc/webrtc_media_stream_track_adapter.h
+++ content/renderer/media/webrtc/webrtc_media_stream_track_adapter.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/memory/ref_counted.h"
+#include "base/synchronization/waitable_event.h"
 #include "content/common/content_export.h"
 #include "content/renderer/media/remote_media_stream_track_adapter.h"
 #include "content/renderer/media/webrtc/media_stream_video_webrtc_sink.h"
@@ -54,9 +55,9 @@ class CONTENT_EXPORT WebRtcMediaStreamTrackAdapter
   // The adapter must be initialized in order to use |web_track| and
   // |webrtc_track|.
   bool is_initialized() const;
-  const blink::WebMediaStreamTrack& web_track() const;
-  webrtc::MediaStreamTrackInterface* webrtc_track() const;
-  bool IsEqual(const blink::WebMediaStreamTrack& web_track) const;
+  const blink::WebMediaStreamTrack& web_track();
+  webrtc::MediaStreamTrackInterface* webrtc_track();
+  bool IsEqual(const blink::WebMediaStreamTrack& web_track);
 
   // For testing.
   WebRtcAudioSink* GetLocalTrackAudioSinkForTesting() {
@@ -91,6 +92,7 @@ class CONTENT_EXPORT WebRtcMediaStreamTrackAdapter
   void InitializeRemoteVideoTrack(
       webrtc::VideoTrackInterface* webrtc_video_track);
   void FinalizeRemoteTrackInitializationOnMainThread();
+  void EnsureTrackIsInitialized();
 
   // Disposing starts and finishes on the main thread. Local tracks and remote
   // video tracks are disposed synchronously. Remote audio tracks are disposed
@@ -107,10 +109,11 @@ class CONTENT_EXPORT WebRtcMediaStreamTrackAdapter
   PeerConnectionDependencyFactory* const factory_;
   scoped_refptr<base::SingleThreadTaskRunner> main_thread_;
 
-  // This class is immutable between being initialized (triggered by
-  // |Create...|) and being disposed (by calling |Dispose|). As such, no locks
-  // are required in the implementation for only inherently racey actions could
-  // cause problems (such as disposing while still in use).
+  // Part of the initialization of remote tracks occurs on the signaling thread.
+  // |remote_track_can_complete_initialization_| allows waiting until that part
+  // of the process is finished, so that full initialization of the track can be
+  // completed on the main thread.
+  base::WaitableEvent remote_track_can_complete_initialization_;
   bool is_initialized_;
   blink::WebMediaStreamTrack web_track_;
   scoped_refptr<webrtc::MediaStreamTrackInterface> webrtc_track_;
