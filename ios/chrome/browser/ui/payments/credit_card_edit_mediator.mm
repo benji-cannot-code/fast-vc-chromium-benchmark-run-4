3f1680a38373bb247eb93a481006bf52345cb8bd
Marc-Antoine Courteau <macourteau@chromium.org>
2017-08-14 17:08:25 +0000

    [Payment Request] Editor title for the credit card edit dialog.
    The logic for the GetCardEditDialogTitleString function is the same as this:
    https://cs.chromium.org/chromium/src/chrome/android/java/src/org/chromium/chrome/browser/payments/AutofillPaymentInstrument.java?type=cs&l=317
    
    Bug: 602666
    Change-Id: I5b8268538f648a0b488519f3b0d31a1c77d79f0b
    Reviewed-on: https://chromium-review.googlesource.com/612365
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Reviewed-by: mahmadi (Moe) <mahmadi@chromium.org>
    Commit-Queue: Marc-Antoine Courteau <macourteau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494081}

--- ios/chrome/browser/ui/payments/credit_card_edit_mediator.mm
+++ ios/chrome/browser/ui/payments/credit_card_edit_mediator.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/autofill_profile.h"
 #include "components/autofill/core/browser/credit_card.h"
 #include "components/autofill/core/browser/personal_data_manager.h"
+#include "components/autofill/core/browser/validation.h"
 #import "components/autofill/ios/browser/credit_card_util.h"
 #include "components/payments/core/payment_request_data_util.h"
 #include "components/payments/core/strings_util.h"
@@ -105,9 +106,14 @@ using ::payment_request_util::GetBillingAddressLabelFromAutofillProfile;
 #pragma mark - PaymentRequestEditViewControllerDataSource
 
 - (NSString*)title {
-  // TODO(crbug.com/602666): Title varies depending on the missing fields.
-  return _creditCard ? l10n_util::GetNSString(IDS_PAYMENTS_EDIT_CARD)
-                     : l10n_util::GetNSString(IDS_PAYMENTS_ADD_CARD_LABEL);
+  if (!self.creditCard)
+    return l10n_util::GetNSString(IDS_PAYMENTS_ADD_CARD_LABEL);
+
+  const autofill::CreditCardCompletionStatus status =
+      autofill::GetCompletionStatusForCard(
+          *self.creditCard, self.paymentRequest->GetApplicationLocale(),
+          self.paymentRequest->billing_profiles());
+  return base::SysUTF16ToNSString(autofill::GetEditDialogTitleForCard(status));
 }
 
 - (CollectionViewItem*)headerItem {
