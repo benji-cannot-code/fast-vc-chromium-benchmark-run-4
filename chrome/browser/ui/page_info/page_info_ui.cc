ad19b9f6c9c27468ac316fbe22787982a10e4b34
Jialiu Lin <jialiul@chromium.org>
2017-08-14 18:16:26 +0000

    Password reuse PageInfo site identity section
    Build PageInfo for password reuse case.
    The new site identity section includes a title, a description,
    a "Change Password" button, and a "Site is legitimate" button.
    
    Its title varies based on finch parameter (softer vs. scarier).
    
    This new page info section is gated by finch feature and is not
    wired up yet.
    
    Bug: 741138
    Change-Id: Ib4b1db4331708ceea77178c63a9c3ed1d47d8bcd
    Reviewed-on: https://chromium-review.googlesource.com/602867
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Lucas Garron <lgarron@chromium.org>
    Commit-Queue: Jialiu Lin <jialiul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494105}

--- chrome/browser/ui/page_info/page_info_ui.cc
+++ chrome/browser/ui/page_info/page_info_ui.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/macros.h"
+#include "build/build_config.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/permissions/permission_manager.h"
 #include "chrome/browser/permissions/permission_result.h"
@@ -33,6 +34,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/paint_vector_icon.h"
+#endif
+
+#if defined(SAFE_BROWSING_DB_LOCAL) && !defined(OS_MACOSX)
+#include "components/safe_browsing/password_protection/password_protection_service.h"
 #endif
 
 namespace {
@@ -192,7 +197,8 @@ PageInfoUI::ChosenObjectInfo::~ChosenObjectInfo() {}
 PageInfoUI::IdentityInfo::IdentityInfo()
     : identity_status(PageInfo::SITE_IDENTITY_STATUS_UNKNOWN),
       connection_status(PageInfo::SITE_CONNECTION_STATUS_UNKNOWN),
-      show_ssl_decision_revoke_button(false) {}
+      show_ssl_decision_revoke_button(false),
+      show_change_password_buttons(false) {}
 
 PageInfoUI::IdentityInfo::~IdentityInfo() {}
 
@@ -240,6 +246,16 @@ PageInfoUI::IdentityInfo::GetSecurityDescription() const {
     case PageInfo::SITE_IDENTITY_STATUS_UNWANTED_SOFTWARE:
       return CreateSecurityDescription(IDS_PAGE_INFO_UNWANTED_SOFTWARE_SUMMARY,
                                        IDS_PAGE_INFO_UNWANTED_SOFTWARE_DETAILS);
+    case PageInfo::SITE_IDENTITY_STATUS_PASSWORD_REUSE:
+#if defined(SAFE_BROWSING_DB_LOCAL) && !defined(OS_MACOSX)
+      return safe_browsing::PasswordProtectionService::ShouldShowSofterWarning()
+                 ? CreateSecurityDescription(
+                       IDS_PAGE_INFO_CHANGE_PASSWORD_SUMMARY_SOFTER,
+                       IDS_PAGE_INFO_CHANGE_PASSWORD_DETAILS)
+                 : CreateSecurityDescription(
+                       IDS_PAGE_INFO_CHANGE_PASSWORD_SUMMARY,
+                       IDS_PAGE_INFO_CHANGE_PASSWORD_DETAILS);
+#endif
     case PageInfo::SITE_IDENTITY_STATUS_DEPRECATED_SIGNATURE_ALGORITHM:
     case PageInfo::SITE_IDENTITY_STATUS_UNKNOWN:
     case PageInfo::SITE_IDENTITY_STATUS_NO_CERT:
