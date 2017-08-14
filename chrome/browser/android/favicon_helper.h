565003212d6b3351ed1bef83e886dc975a1d2aee
Jan Krcal <jkrcal@chromium.org>
2017-08-14 16:03:09 +0000

    [Remote suggestions] Mark favicons stored by FaviconHelper as on-demand
    This CL updates the old code path for fetching and storing favicons for
    content suggestions so that it uses a new mechanism for automatic
    removal of old (on-demand) favicons.
    
    In detail, the CL:
     - updates the code that caches such downloaded icons to mark them as
       on-demand icons (see https://codereview.chromium.org/2903573002/);
     - removes the isTemporary parameter from the function because with the
       new removal mechanism, all icons downloaded by this method are by
       definition on-demand and temporary;
     - adds a new method in FaviconHelper that touches the downloaded icon
       and thus postpones its automatic eviction.
    
    Before this CL, these icons were never cleared (unless you visit the
    home page of the suggested article causing the icons to be re-downloaded
    and later removed by the standard history expiration).
    
    Bug: 709471
    Change-Id: I307e2c39c3b29af98270f1e9e2a6686a843489df
    Reviewed-on: https://chromium-review.googlesource.com/597857
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494059}

--- chrome/browser/android/favicon_helper.h
+++ chrome/browser/android/favicon_helper.h
@@ -40,6 +40,7 @@ class FaviconHelper {
       const base::android::JavaParamRef<jobject>& obj,
       const base::android::JavaParamRef<jobject>& jprofile,
       const base::android::JavaParamRef<jstring>& j_page_url);
+
   void EnsureIconIsAvailable(
       JNIEnv* env,
       const base::android::JavaParamRef<jobject>& obj,
@@ -48,8 +49,12 @@ class FaviconHelper {
       const base::android::JavaParamRef<jstring>& j_page_url,
       const base::android::JavaParamRef<jstring>& j_icon_url,
       jboolean j_is_large_icon,
-      jboolean j_is_temporary,
       const base::android::JavaParamRef<jobject>& j_availability_callback);
+  void TouchOnDemandFavicon(
+      JNIEnv* env,
+      const base::android::JavaParamRef<jobject>& obj,
+      const base::android::JavaParamRef<jobject>& j_profile,
+      const base::android::JavaParamRef<jstring>& j_icon_url);
 
  private:
   FRIEND_TEST_ALL_PREFIXES(FaviconHelperTest, GetLargestSizeIndex);
@@ -62,7 +67,6 @@ class FaviconHelper {
       const GURL& page_url,
       const GURL& icon_url,
       favicon_base::IconType icon_type,
-      bool is_temporary,
       const favicon_base::FaviconImageResult& result);
 
   static void OnFaviconDownloaded(
@@ -71,7 +75,6 @@ class FaviconHelper {
       Profile* profile,
       const GURL& page_url,
       favicon_base::IconType icon_type,
-      bool is_temporary,
       int download_request_id,
       int http_status_code,
       const GURL& image_url,
