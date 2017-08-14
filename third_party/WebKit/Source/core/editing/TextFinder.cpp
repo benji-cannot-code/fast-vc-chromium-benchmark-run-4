ba354dc2fa312aebf35b7d2aea2e43e976ce1b74
Hajime Hoshi <hajimehoshi@chromium.org>
2017-08-14 09:36:50 +0000

    Reset members at TextFinder::StopFindingAndClearSelection to avoid document leaks
    A document is leaked via a TextFinder since TextFinder's |active_match_|
    and |resume_scoping_from_range_| are not reset even after the find-in-
    page UI is closed e.g. when reloading, and TextFinder keeps the document
    until the members are updated e.g. when a new find-in-page is started.
    
    This CL resets |active_match_| and |resume_scoping_from_range_| so that
    the document object is no longer kept after the find-in-page UI is
    closed.
    
    Bug: 752902
    Change-Id: I56b4f86c406f595d53503f8042bf1bca927f1f38
    Reviewed-on: https://chromium-review.googlesource.com/609762
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494025}

--- third_party/WebKit/Source/core/editing/TextFinder.cpp
+++ third_party/WebKit/Source/core/editing/TextFinder.cpp
@@ -374,7 +374,6 @@ void TextFinder::StartScopingStringMatches(int identifier,
   // Clear the counters from last operation.
   last_match_count_ = 0;
   next_invalidate_after_ = 0;
-  resume_scoping_from_range_ = nullptr;
 
   // The view might be null on detached frames.
   LocalFrame* frame = OwnerFrame().GetFrame();
@@ -559,6 +558,8 @@ void TextFinder::CancelPendingScopingEffort() {
     last_find_request_completed_with_no_matches_ = false;
 
   scoping_in_progress_ = false;
+
+  resume_scoping_from_range_ = nullptr;
 }
 
 void TextFinder::IncreaseMatchCount(int identifier, int count) {
