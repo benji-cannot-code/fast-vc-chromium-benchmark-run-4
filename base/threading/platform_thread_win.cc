9677adb135fda8aa1f572f95732eecc5e1d97098
Brian White <bcwhite@chromium.org>
2017-08-14 19:07:32 +0000

    Move join activity tracking to after correctness checks.
    This avoids any chance of mistakenly reporting hands from other
    calls as a hang of the actual "join".
    
    Bug: 
    Change-Id: Ibaddb4705a0f56c3cf3c8665946089e61824aeff
    Reviewed-on: https://chromium-review.googlesource.com/612105
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494121}

--- base/threading/platform_thread_win.cc
+++ base/threading/platform_thread_win.cc
@@ -226,9 +226,6 @@ bool PlatformThread::CreateNonJoinableWithPriority(size_t stack_size,
 
 // static
 void PlatformThread::Join(PlatformThreadHandle thread_handle) {
-  // Record the event that this thread is blocking upon (for hang diagnosis).
-  base::debug::ScopedThreadJoinActivity thread_activity(&thread_handle);
-
   DCHECK(thread_handle.platform_handle());
   // TODO(willchan): Enable this check once I can get it to work for Windows
   // shutdown.
@@ -248,6 +245,9 @@ void PlatformThread::Join(PlatformThreadHandle thread_handle) {
   // Record information about the exiting thread in case joining hangs.
   base::debug::Alias(&thread_id);
   base::debug::Alias(&last_error);
+
+  // Record the event that this thread is blocking upon (for hang diagnosis).
+  base::debug::ScopedThreadJoinActivity thread_activity(&thread_handle);
 
   // Wait for the thread to exit.  It should already have terminated but make
   // sure this assumption is valid.
