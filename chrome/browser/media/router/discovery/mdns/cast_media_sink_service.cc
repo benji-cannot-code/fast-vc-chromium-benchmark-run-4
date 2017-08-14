67ecab72b647d38fa893c48a77d327e5784a994f
btolsch <btolsch@chromium.org>
2017-08-14 11:01:12 +0000

    Add mDNS service caching to CastMediaSinkService
    This change uses the DiscoveryNetworkMonitor to remember the list of
    services that were last seen on a set of connected
    networks.  When a previously-seen set of networks is seen again,
    CastMediaSinkService will load all of the cached services and attempt
    to resolve them if they are not already present.
    
    Bug: 698943, 687377
    Change-Id: Ib3a47ef412ad56e4348f4dbd754cbcc1825d1e41
    Reviewed-on: https://chromium-review.googlesource.com/595018
    Reviewed-by: Derek Cheng <imcheng@chromium.org>
    Commit-Queue: Brandon Tolsch <btolsch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494030}

--- chrome/browser/media/router/discovery/mdns/cast_media_sink_service.cc
+++ chrome/browser/media/router/discovery/mdns/cast_media_sink_service.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/string_number_conversions.h"
+#include "chrome/browser/media/router/discovery/discovery_network_monitor.h"
 #include "chrome/browser/media/router/discovery/mdns/cast_media_sink_service_impl.h"
 #include "chrome/browser/media/router/discovery/mdns/dns_sd_delegate.h"
 #include "chrome/browser/media/router/discovery/mdns/dns_sd_registry.h"
@@ -115,7 +116,8 @@ void CastMediaSinkService::Start() {
     cast_media_sink_service_impl_.reset(new CastMediaSinkServiceImpl(
         base::BindRepeating(&CastMediaSinkService::OnSinksDiscoveredOnIOThread,
                             this),
-        cast_channel::CastSocketService::GetInstance()));
+        cast_channel::CastSocketService::GetInstance(),
+        DiscoveryNetworkMonitor::GetInstance()));
   }
 
   content::BrowserThread::PostTask(
