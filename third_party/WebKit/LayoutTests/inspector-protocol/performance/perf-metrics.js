6dc668f21d475a07390367cfa8b6cd3eb009e468
Alexei Filippov <alph@chromium.org>
2017-08-14 22:10:31 +0000

    [DevTools] Add instance counters to Performance.getMetrics API
    BUG=751892
    
    Change-Id: I019c060a78ecaa7dfd91836c28c9a18d3ddc9470
    Reviewed-on: https://chromium-review.googlesource.com/612560
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494202}

--- third_party/WebKit/LayoutTests/inspector-protocol/performance/perf-metrics.js
+++ third_party/WebKit/LayoutTests/inspector-protocol/performance/perf-metrics.js
@@ -13,7 +13,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   async function dumpMetrics() {
     const {result:{metrics}} = await dp.Performance.getMetrics();
     testRunner.log(JSON.stringify(metrics.map(metric => metric.name)));
+    checkMetric('DocumentCount');
+    checkMetric('NodeCount');
+    checkMetric('PageDocumentCount');
+
+    function checkMetric(name) {
+      const metric = metrics.find(metric => metric.name === name);
+      if (metrics.length && !metric.value)
+        testRunner.log(`Error: Metric ${name} has a bad value ${metric.value}`);
+    }
   }
 
   testRunner.completeTest();
+
 })
--- third_party/WebKit/Source/core/frame/PerformanceMonitor.h
+++ third_party/WebKit/Source/core/frame/PerformanceMonitor.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define PerformanceMonitor_h
 
 #include "core/CoreExport.h"
+#include "core/dom/Document.h"
 #include "core/frame/LocalFrame.h"
 #include "platform/heap/Handle.h"
 #include "platform/scheduler/base/task_time_observer.h"
@@ -31,10 +32,7 @@ class ExecutionContext;
 class Performance;
 class SourceLocation;
 
-#define PERF_METRICS_LIST(V) \
-  V(DocumentCount)           \
-  V(JSEventListenerCount)    \
-  V(NodeCount)
+#define PERF_METRICS_LIST(V) V(PageDocument)
 
 // Performance monitor for Web Performance APIs and logging.
 // The monitor is maintained per local root.
@@ -106,12 +104,14 @@ class CORE_EXPORT PerformanceMonitor final
 
   void DocumentWriteFetchScript(Document*);
 
-  static inline void IncrementCounter(LocalFrame* frame, MetricsType type) {
+  static inline void IncrementCounter(Document* document, MetricsType type) {
+    LocalFrame* frame = document->GetFrame();
     if (frame)
       ++frame->GetPerformanceMonitor()->metric_values_[type];
   }
 
-  static inline void DecrementCounter(LocalFrame* frame, MetricsType type) {
+  static inline void DecrementCounter(Document* document, MetricsType type) {
+    LocalFrame* frame = document->GetFrame();
     if (frame)
       --frame->GetPerformanceMonitor()->metric_values_[type];
   }
