5d3f7fb5e3d516a2feb3b5fd9211b3246cdaa7ac
Maks Orlovich <morlovich@chromium.org>
2017-08-14 19:59:56 +0000

    Migrate blob storage away from deprecated CreateCacheBackend variant
    It is no longer necessary to pass in a thread (and it was not used for
    anything when using net::MEMORY_CACHE anyway), and I plan to remove
    the variant that permits one to.
    
    Bug: 747020
    Change-Id: I6f6cc3e5907a2da625fa3b1d7fce565dadec71c7
    Reviewed-on: https://chromium-review.googlesource.com/613960
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494149}

--- storage/browser/blob/blob_reader_unittest.cc
+++ storage/browser/blob/blob_reader_unittest.cc
@@ -161,13 +161,12 @@ class DelayedReadEntry : public disk_cache::Entry {
   std::vector<base::Callback<void(void)>> pending_read_callbacks_;
 };
 
-std::unique_ptr<disk_cache::Backend> CreateInMemoryDiskCache(
-    const scoped_refptr<base::SingleThreadTaskRunner>& thread) {
+std::unique_ptr<disk_cache::Backend> CreateInMemoryDiskCache() {
   std::unique_ptr<disk_cache::Backend> cache;
   net::TestCompletionCallback callback;
   int rv = disk_cache::CreateCacheBackend(
       net::MEMORY_CACHE, net::CACHE_BACKEND_DEFAULT, FilePath(), 0, false,
-      thread, nullptr, &cache, callback.callback());
+      nullptr, &cache, callback.callback());
   EXPECT_EQ(net::OK, callback.GetResult(rv));
 
   return cache;
@@ -517,8 +516,7 @@ TEST_F(BlobReaderTest, BasicFileSystem) {
 }
 
 TEST_F(BlobReaderTest, BasicDiskCache) {
-  std::unique_ptr<disk_cache::Backend> cache =
-      CreateInMemoryDiskCache(base::ThreadTaskRunnerHandle::Get());
+  std::unique_ptr<disk_cache::Backend> cache = CreateInMemoryDiskCache();
   ASSERT_TRUE(cache);
 
   BlobDataBuilder b("uuid");
@@ -552,8 +550,7 @@ TEST_F(BlobReaderTest, BasicDiskCache) {
 }
 
 TEST_F(BlobReaderTest, DiskCacheWithSideData) {
-  std::unique_ptr<disk_cache::Backend> cache =
-      CreateInMemoryDiskCache(base::ThreadTaskRunnerHandle::Get());
+  std::unique_ptr<disk_cache::Backend> cache = CreateInMemoryDiskCache();
   ASSERT_TRUE(cache);
 
   BlobDataBuilder b("uuid");
@@ -791,8 +788,7 @@ TEST_F(BlobReaderTest, FileSystemAsync) {
 }
 
 TEST_F(BlobReaderTest, DiskCacheAsync) {
-  std::unique_ptr<disk_cache::Backend> cache =
-      CreateInMemoryDiskCache(base::ThreadTaskRunnerHandle::Get());
+  std::unique_ptr<disk_cache::Backend> cache = CreateInMemoryDiskCache();
   ASSERT_TRUE(cache);
 
   BlobDataBuilder b("uuid");
@@ -869,8 +865,7 @@ TEST_F(BlobReaderTest, FileRange) {
 }
 
 TEST_F(BlobReaderTest, DiskCacheRange) {
-  std::unique_ptr<disk_cache::Backend> cache =
-      CreateInMemoryDiskCache(base::ThreadTaskRunnerHandle::Get());
+  std::unique_ptr<disk_cache::Backend> cache = CreateInMemoryDiskCache();
   ASSERT_TRUE(cache);
 
   BlobDataBuilder b("uuid");
@@ -978,8 +973,7 @@ TEST_F(BlobReaderTest, FileSomeAsyncSegmentedOffsetsUnknownSizes) {
 
 TEST_F(BlobReaderTest, MixedContent) {
   // Includes data, a file, and a disk cache entry.
-  std::unique_ptr<disk_cache::Backend> cache =
-      CreateInMemoryDiskCache(base::ThreadTaskRunnerHandle::Get());
+  std::unique_ptr<disk_cache::Backend> cache = CreateInMemoryDiskCache();
   ASSERT_TRUE(cache);
 
   BlobDataBuilder b("uuid");
