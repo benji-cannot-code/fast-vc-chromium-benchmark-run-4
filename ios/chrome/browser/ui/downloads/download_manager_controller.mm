068f5762955cf1d8e973231733aebf67632053f6
mrefaat <mrefaat@chromium.org>
2017-08-14 22:06:19 +0000

    Log net errors in case content fetch download faliure.
    use the existing NetErrorCodes to log the positive value of the
    fetcher->status->error code.
    
    Bug: 754398
    Change-Id: I4451410a76f8c8f34efc983df73f1f70142ae69f
    Reviewed-on: https://chromium-review.googlesource.com/611050
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Alexei Svitkine (very slow) <asvitkine@chromium.org>
    Commit-Queue: Mohammad Refaat <mrefaat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494195}

--- ios/chrome/browser/ui/downloads/download_manager_controller.mm
+++ ios/chrome/browser/ui/downloads/download_manager_controller.mm
@@ -236,6 +236,9 @@ enum DownloadedFileAction {
 const char kUMADownloadedFileStatusCode[] =
     "Download.IOSDownloadedFileStatusCode";
 
+// Key of the UMA.Download.IOSDownloadedFileNetError histogram.
+const char kUMADownloadFileNetError[] = "Download.IOSDownloadedFileNetError";
+
 int g_download_manager_id = 0;
 
 const int kNoFileSizeGiven = -1;
@@ -1436,6 +1439,9 @@ class DownloadContentDelegate : public URLFetcherDelegate {
   if (!_fetcher->GetStatus().is_success() ||
       _fetcher->GetResponseCode() != 200) {
     [self displayError];
+    // Log the Net Error code.
+    UMA_HISTOGRAM_SPARSE_SLOWLY(kUMADownloadFileNetError,
+                                -_fetcher->GetStatus().error());
     return;
   }
 
