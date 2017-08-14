85ad7c1230845fec68464935cfb7e83b4db5a601
Kyle Milka <kmilka@google.com>
2017-08-14 20:46:19 +0000

    Set up field trials within WebView
    This is part 6 of 6 for FieldTrial refactoring for WebView
    
    Part 1: https://chromium-review.googlesource.com/c/562098/
    Part 2: https://chromium-review.googlesource.com/c/561920
    Part 3: https://chromium-review.googlesource.com/c/561922/
    Part 4: https://chromium-review.googlesource.com/c/561980/
    Part 5: https://chromium-review.googlesource.com/c/562417/
    Part 6: https://chromium-review.googlesource.com/c/562021/
    
    This CL makes use of previous refactoring work to actually
    set up field trials within WebView. The AwFieldTrialCreator class is
    now used by AwBrowserMainParts to create the set of active experiments
    within WebView.
    
    BUG=678288
    
    Change-Id: Id76b98e5adb432ca3fa7ea4cffd7c0296f160e82
    Reviewed-on: https://chromium-review.googlesource.com/562021
    Commit-Queue: Kyle Milka <kmilka@google.com>
    Reviewed-by: Paul Miller <paulmiller@chromium.org>
    Reviewed-by: Tima Vaisburd <timav@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Alexei Svitkine (very slow) <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494175}

--- android_webview/browser/aw_browser_main_parts.h
+++ android_webview/browser/aw_browser_main_parts.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "android_webview/browser/aw_field_trial_creator.h"
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "content/public/browser/browser_main_parts.h"
@@ -36,6 +37,11 @@ class AwBrowserMainParts : public content::BrowserMainParts {
   std::unique_ptr<base::MessageLoop> main_message_loop_;
 
   AwContentBrowserClient* browser_client_;
+
+  // Responsible for creating a feature list from the seed. This object must
+  // exist for the lifetime of the process as it contains the FieldTrialList
+  // that can be queried for the state of experiments.
+  AwFieldTrialCreator aw_field_trial_creator_;
 
   DISALLOW_COPY_AND_ASSIGN(AwBrowserMainParts);
 };
