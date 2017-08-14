6f6bc36af88af5f81663d3d80abbe6a82661ffa4
Helen Li <xunjieli@chromium.org>
2017-08-14 13:58:27 +0000

    Make NetworkChangeNotifier::HistogramWatcher a private class
    This is to follow up on pauljensen@'s CL at
    https://codereview.chromium.org/11620007/ which proposed the change.
    
    Bug: 754695
    Change-Id: Id289623121d53e53b988dd23b11938600249ed99
    Reviewed-on: https://chromium-review.googlesource.com/612472
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Commit-Queue: Helen Li <xunjieli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494043}

--- net/base/network_change_notifier.h
+++ net/base/network_change_notifier.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace net {
 
 struct DnsConfig;
-class HistogramWatcher;
 class NetworkChangeNotifierFactory;
 struct NetworkInterface;
 typedef std::vector<NetworkInterface> NetworkInterfaceList;
@@ -561,6 +560,7 @@ class NET_EXPORT NetworkChangeNotifier {
   friend class NetworkChangeNotifierLinuxTest;
   friend class NetworkChangeNotifierWinTest;
 
+  class HistogramWatcher;
   class NetworkState;
   class NetworkChangeCalculator;
 
