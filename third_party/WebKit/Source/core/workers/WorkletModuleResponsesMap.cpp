c1ecd36449f0fb95606f902d02dd9256992acede
Hiroki Nakagawa <nhiroki@chromium.org>
2017-08-15 09:38:55 +0000

    Worklet: Abort WorkletModuleScriptMap access when an execution context is destroyed
    Before this CL, there is no mechanism to stop ongoing access to
    WorkletModuleScriptMap when an associated document context or a worklet
    execution context is destroyed. This results in crashes.
    
    After this CL, when the document context is destroyed,
    WorkletModuleScriptMap::Abort() fails all waiting clients. Also,
    WorkletModuleScriptFetcher checks the validity of the worklet context on
    completion of cross-thread operations.
    
    Bug: 751316
    Change-Id: Ia900e0e83278ef01c788e515d41d54c4bcf1d591
    Reviewed-on: https://chromium-review.googlesource.com/612966
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494350}

--- third_party/WebKit/Source/core/workers/WorkletModuleResponsesMap.cpp
+++ third_party/WebKit/Source/core/workers/WorkletModuleResponsesMap.cpp
@@ -77,7 +77,7 @@ class WorkletModuleResponsesMap::Entry
 void WorkletModuleResponsesMap::ReadOrCreateEntry(const KURL& url,
                                                   Client* client) {
   DCHECK(IsMainThread());
-  if (!IsValidURL(url)) {
+  if (!is_available_ || !IsValidURL(url)) {
     client->OnFailed();
     return;
   }
@@ -123,6 +123,9 @@ void WorkletModuleResponsesMap::UpdateEntry(
     const ModuleScriptCreationParams& params) {
   DCHECK(IsMainThread());
   DCHECK(IsValidURL(url));
+  if (!is_available_)
+    return;
+
   DCHECK(entries_.Contains(url));
   Entry* entry = entries_.find(url)->value;
 
@@ -136,9 +139,27 @@ void WorkletModuleResponsesMap::UpdateEntry(
 void WorkletModuleResponsesMap::InvalidateEntry(const KURL& url) {
   DCHECK(IsMainThread());
   DCHECK(IsValidURL(url));
+  if (!is_available_)
+    return;
+
   DCHECK(entries_.Contains(url));
   Entry* entry = entries_.find(url)->value;
   entry->NotifyFailure();
+}
+
+void WorkletModuleResponsesMap::Dispose() {
+  is_available_ = false;
+  for (auto it : entries_) {
+    switch (it.value->GetState()) {
+      case Entry::State::kFetching:
+        it.value->NotifyFailure();
+        break;
+      case Entry::State::kFetched:
+      case Entry::State::kFailed:
+        break;
+    }
+  }
+  entries_.clear();
 }
 
 DEFINE_TRACE(WorkletModuleResponsesMap) {
