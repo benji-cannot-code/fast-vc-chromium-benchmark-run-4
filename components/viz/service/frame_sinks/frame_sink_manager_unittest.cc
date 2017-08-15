ea828e0e7ed958862e4680fa9a685be7b828ed45
kylechar <kylechar@chromium.org>
2017-08-15 15:06:39 +0000

    Switch viz_unittests to use surface references.
    This CL switches viz_unittests to use surface references instead of
    surface sequences. The only major change is for
    CompositorFrameSinkSupportTests. Delete tests that specifically checked
    SurfaceSequence related code.
    
    Also make the default CompositorFrameSinkSupport object not be a root
    surface. Tests call EvictCurrentSurface() which won't delete a root
    surface with surface references. EvictCurrentSurface() is never actually
    called on a root surface in non-test code, so don't rely on the
    behaviour in tests.
    
    Bug: 676384
    Change-Id: I75a3fd1f14a38ecc21252695e0776f6df78b6339
    Reviewed-on: https://chromium-review.googlesource.com/614096
    Reviewed-by: Saman Sami <samans@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494380}

--- components/viz/service/frame_sinks/frame_sink_manager_unittest.cc
+++ components/viz/service/frame_sinks/frame_sink_manager_unittest.cc
@@ -65,7 +65,8 @@ class FakeFrameSinkManagerClient : public FrameSinkManagerClient {
 
 class FrameSinkManagerTest : public testing::Test {
  public:
-  FrameSinkManagerTest() = default;
+  FrameSinkManagerTest()
+      : manager_(nullptr, SurfaceManager::LifetimeType::REFERENCES) {}
 
   ~FrameSinkManagerTest() override = default;
 
