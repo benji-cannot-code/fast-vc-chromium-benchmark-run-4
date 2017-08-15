c986878fd76a24ebb9439a689e26d0dee9ee6e4b
Catherine Mullings <catmullings@chromium.org>
2017-08-15 16:18:49 +0000

    Handle extension uninstallation during extension message bubble creation
    Fixed crash due to dereferencing an uninstalled extension in the midst
    of extension message bubble creation in
    ExtensionMessageBubbleBridge::GetActionButtionText().
    
    Bug: 748952
    Change-Id: I282242179a2c66eeb14b6cb6bd62d8c904854247
    Reviewed-on: https://chromium-review.googlesource.com/596528
    Commit-Queue: catmullings <catmullings@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494399}

--- chrome/browser/extensions/extension_message_bubble_controller.h
+++ chrome/browser/extensions/extension_message_bubble_controller.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observer.h"
 #include "chrome/browser/ui/browser_list_observer.h"
 #include "extensions/browser/browser_context_keyed_api_factory.h"
+#include "extensions/browser/extension_registry_observer.h"
 #include "extensions/common/extension.h"
 
 class Browser;
@@ -26,7 +27,8 @@ namespace extensions {
 
 class ExtensionRegistry;
 
-class ExtensionMessageBubbleController : public chrome::BrowserListObserver {
+class ExtensionMessageBubbleController : public chrome::BrowserListObserver,
+                                         public ExtensionRegistryObserver {
  public:
   // UMA histogram constants.
   enum BubbleAction {
@@ -168,7 +170,8 @@ class ExtensionMessageBubbleController : public chrome::BrowserListObserver {
 
   // Called when the bubble is actually shown. Because some bubbles are delayed
   // (in order to weather the "focus storm"), they are not shown immediately.
-  void OnShown();
+  // Accepts a callback from platform-specifc ui code to close the bubble.
+  void OnShown(const base::Closure& close_bubble_callback);
 
   // Callbacks from bubble. Declared virtual for testing purposes.
   virtual void OnBubbleAction();
@@ -184,6 +187,10 @@ class ExtensionMessageBubbleController : public chrome::BrowserListObserver {
       bool should_ignore_learn_more);
 
  private:
+  // ExtensionRegistryObserver:
+  void OnExtensionUnloaded(content::BrowserContext* browser_context,
+                           const Extension* extension,
+                           UnloadedExtensionReason reason) override;
   // BrowserListObserver:
   void OnBrowserRemoved(Browser* browser) override;
 
@@ -222,6 +229,11 @@ class ExtensionMessageBubbleController : public chrome::BrowserListObserver {
   // Whether or not this bubble is the active bubble being shown.
   bool is_active_bubble_;
 
+  // Platform-specific implementation of closing the bubble.
+  base::Closure close_bubble_callback_;
+
+  ScopedObserver<ExtensionRegistry, ExtensionRegistryObserver>
+      extension_registry_observer_;
   ScopedObserver<BrowserList, BrowserListObserver> browser_list_observer_;
 
   DISALLOW_COPY_AND_ASSIGN(ExtensionMessageBubbleController);
--- chrome/browser/ui/views/extensions/extension_message_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/extensions/extension_message_bubble_view_browsertest.cc
@@ -282,3 +282,8 @@ IN_PROC_BROWSER_TEST_F(NtpExtensionBubbleViewBrowserTest,
                        TestControlledNewTabPageMessageBubbleLearnMore) {
   TestControlledNewTabPageBubbleShown(true);
 }
+
+IN_PROC_BROWSER_TEST_F(NtpExtensionBubbleViewBrowserTest,
+                       TestBubbleClosedAfterExtensionUninstall) {
+  TestBubbleClosedAfterExtensionUninstall();
+}
