8bd7ef91d501205ff72c9156ed510b1065f20950
Etienne Pierre-Doray <etiennep@chromium.org>
2017-08-15 17:58:18 +0000

    [Zucchini] Add utility functions for Generate.
    This CL adds utility function for manipulating labels, used when
    generating a patch:
    - MakeNewTargetsFromEquivalenceMap()
    - FindExtraTargets()
    
    Bug: 729154
    Change-Id: If87e750eb16d538d6131da07d9a35ffb09b6cc03
    Reviewed-on: https://chromium-review.googlesource.com/610517
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494444}

--- chrome/installer/zucchini/image_index.h
+++ chrome/installer/zucchini/image_index.h
@@ -54,6 +54,11 @@ class ImageIndex {
 
   PoolTag GetPoolTag(TypeTag type) const { return GetTraits(type).pool_tag; }
 
+  const std::vector<TypeTag>& GetTypeTags(PoolTag pool) const {
+    DCHECK_LT(pool.value(), pools_.size());
+    return pools_[pool.value()].types;
+  }
+
   // Returns true if |raw_image_[location]| is either:
   // - A raw value.
   // - The first byte of a reference.
@@ -88,6 +93,9 @@ class ImageIndex {
     DCHECK_LT(type.value(), types_.size());
     return types_[type.value()].references;
   }
+
+  // Creates and returns a vector of all targets in |pool|.
+  std::vector<offset_t> GetTargets(PoolTag pool) const;
 
   // Returns the size of the image.
   size_t size() const { return raw_image_.size(); }
