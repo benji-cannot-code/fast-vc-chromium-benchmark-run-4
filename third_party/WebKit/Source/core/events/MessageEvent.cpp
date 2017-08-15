d7c28f044e85df6d8fa153eab4183589b1c12709
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2017-08-15 23:55:39 +0000

    Make MessageEvent#ports a non-nullable FrozenArray.
    WebIDL arrays were removed from the spec back in 2015, and this CL adapts to
    https://github.com/whatwg/html/pull/863 ("Use FrozenArray for
    Navigator#languages and MessageEvent#ports") as well as
    https://github.com/whatwg/html/pull/1883 ("Make MessageEvent's ports
    attribute non-nullable").
    
    While here, also adapt to the part of the latter commit above that made
    MessageEventInit#ports non-nullable.
    
    The patch looks a bit longer than it would have been because we have to
    cache MessageEvent#ports and return the same value until it's changed via
    initMessageEvent().
    
    Also update a few WPT tests which were written before |ports| was turned
    into a FrozenArray: we now verify that attempting to change |ports| throws a
    TypeError.
    
    Bug: 654707
    Change-Id: I03c7aafc7e4148059cdbf5d75aa6c628c50ed05e
    Reviewed-on: https://chromium-review.googlesource.com/612955
    Commit-Queue: Raphael Kubo da Costa (rakuco) <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494614}

--- third_party/WebKit/Source/core/events/MessageEvent.cpp
+++ third_party/WebKit/Source/core/events/MessageEvent.cpp
@@ -172,6 +172,7 @@ void MessageEvent::initMessageEvent(const AtomicString& type,
   last_event_id_ = last_event_id;
   source_ = source;
   ports_ = ports;
+  is_ports_dirty_ = true;
   suborigin_ = "";
 }
 
@@ -195,6 +196,7 @@ void MessageEvent::initMessageEvent(const AtomicString& type,
   last_event_id_ = last_event_id;
   source_ = source;
   ports_ = ports;
+  is_ports_dirty_ = true;
   suborigin_ = "";
 
   if (data_as_serialized_script_value_)
@@ -221,6 +223,7 @@ void MessageEvent::initMessageEvent(const AtomicString& type,
   last_event_id_ = last_event_id;
   source_ = source;
   ports_ = ports;
+  is_ports_dirty_ = true;
   suborigin_ = "";
 }
 
@@ -228,26 +231,18 @@ const AtomicString& MessageEvent::InterfaceName() const {
   return EventNames::MessageEvent;
 }
 
-MessagePortArray MessageEvent::ports(bool& is_null) const {
+MessagePortArray MessageEvent::ports() {
   // TODO(bashi): Currently we return a copied array because the binding
   // layer could modify the content of the array while executing JS callbacks.
   // Avoid copying once we can make sure that the binding layer won't
   // modify the content.
-  if (ports_) {
-    is_null = false;
-    return *ports_;
-  }
-  is_null = true;
-  return MessagePortArray();
-}
-
-MessagePortArray MessageEvent::ports() const {
-  bool unused;
-  return ports(unused);
+  is_ports_dirty_ = false;
+  return ports_ ? *ports_ : MessagePortArray();
 }
 
 void MessageEvent::EntangleMessagePorts(ExecutionContext* context) {
   ports_ = MessagePort::EntanglePorts(*context, std::move(channels_));
+  is_ports_dirty_ = true;
 }
 
 DEFINE_TRACE(MessageEvent) {
