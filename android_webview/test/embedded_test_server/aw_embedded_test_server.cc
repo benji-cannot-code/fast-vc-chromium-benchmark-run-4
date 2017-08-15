1fd1b3f33406be06b38f0240f84dc5f0ed908431
Shimi Zhang <ctzsm@chromium.org>
2017-08-15 20:49:18 +0000

    aw: Cleanup after removal of RegisterNativesImpl()
    Since we removed RegisterNativesImpl() calls in aw, code
    under /test/embedded_test_server/ changed a lot:
    
    aw_test_jni_onload.{cc,h} contains only one function calling
    base::android::OnJNIOnLoadInit(), so delete these files and
    call OnJNIOnLoadInit() from aw_test_entry_point.cc.
    
    aw_embedded_test_server.h is unnecessary now, so remove it.
    
    Dependencies of net/test/android/net_test_jni_onload.{cc,h}
    are obsoleted, remove them from BUILD.gn.
    
    Bug: 749244
    Change-Id: I6bca95557a8874865404af29aac8b633afca9a85
    Reviewed-on: https://chromium-review.googlesource.com/614862
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494533}

--- android_webview/test/embedded_test_server/aw_embedded_test_server.cc
+++ android_webview/test/embedded_test_server/aw_embedded_test_server.cc
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "android_webview/test/embedded_test_server/aw_embedded_test_server.h"
-
 #include <jni.h>
 
 #include "android_webview/test/jni/AwEmbeddedTestServerImpl_jni.h"
