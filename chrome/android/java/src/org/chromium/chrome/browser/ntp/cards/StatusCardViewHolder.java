a2bcdf3cc4e797404685a70d870fe9e3bc81f77e
Nicolas Dossou-gbete <dgn@chromium.org>
2017-08-15 12:39:28 +0000

    [Suggestions] Add modern styles for sign in promo
    Adds styles to be used for modern cards and uses them in the
    existing one and for the new style of sign in promo. Additionally,
    sign in promos are now at the top in Modern.
    
    Preview:
    https://goo.gl/photos/QjeWSHMtphWKpBebA
    
    Bug: 754186
    Change-Id: I58600912a0c39385a502c7daaaf37cc614f882ab
    Reviewed-on: https://chromium-review.googlesource.com/611905
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494365}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/StatusCardViewHolder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/StatusCardViewHolder.java
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.ntp.cards;
 
 import android.content.Context;
-import android.support.annotation.IntegerRes;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.StringRes;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +14,7 @@ import android.widget.TextView;
 
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ntp.ContextMenuManager;
+import org.chromium.chrome.browser.suggestions.SuggestionsConfig;
 import org.chromium.chrome.browser.suggestions.SuggestionsMetrics;
 import org.chromium.chrome.browser.suggestions.SuggestionsRecyclerView;
 import org.chromium.chrome.browser.widget.displaystyle.UiConfig;
@@ -28,10 +29,10 @@ public class StatusCardViewHolder extends CardViewHolder {
 
     public StatusCardViewHolder(SuggestionsRecyclerView parent,
             ContextMenuManager contextMenuManager, UiConfig config) {
-        super(R.layout.new_tab_page_status_card, parent, config, contextMenuManager);
-        mTitleView = (TextView) itemView.findViewById(R.id.status_title);
-        mBodyView = (TextView) itemView.findViewById(R.id.status_body);
-        mActionView = (Button) itemView.findViewById(R.id.status_action_button);
+        super(getLayout(), parent, config, contextMenuManager);
+        mTitleView = itemView.findViewById(R.id.status_title);
+        mBodyView = itemView.findViewById(R.id.status_body);
+        mActionView = itemView.findViewById(R.id.status_action_button);
     }
 
     /**
@@ -69,21 +70,23 @@ public class StatusCardViewHolder extends CardViewHolder {
         mTitleView.setText(item.getHeader());
         mBodyView.setText(item.getDescription());
 
-        @IntegerRes
+        @StringRes
         int actionLabel = item.getActionLabel();
         if (actionLabel != 0) {
             mActionView.setText(actionLabel);
-            mActionView.setOnClickListener(new View.OnClickListener() {
-
-                @Override
-                public void onClick(View v) {
-                    SuggestionsMetrics.recordCardActionTapped();
-                    item.performAction(v.getContext());
-                }
+            mActionView.setOnClickListener(view -> {
+                SuggestionsMetrics.recordCardActionTapped();
+                item.performAction(view.getContext());
             });
             mActionView.setVisibility(View.VISIBLE);
         } else {
             mActionView.setVisibility(View.GONE);
         }
+    }
+
+    @LayoutRes
+    private static int getLayout() {
+        return SuggestionsConfig.useModern() ? R.layout.content_suggestions_status_card_modern
+                                             : R.layout.new_tab_page_status_card;
     }
 }
