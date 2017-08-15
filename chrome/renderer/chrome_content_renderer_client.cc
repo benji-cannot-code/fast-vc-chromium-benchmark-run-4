ac6d3637068c191800882f74cdf07ffe9d237266
Adam Rice <ricea@chromium.org>
2017-08-15 00:59:39 +0000

    Fix WebSocket SafeBrowsing for S*Workers
    The SafeBrowsing check for WebSockets in ServiceWorkers and SharedWorkers was
    crashing due to them not having a real frame associated. Use MSG_ROUTING_ID for
    the render_frame_id in this case.
    
    Re-enable the check which was disabled temporarily in
    https://chromium-review.googlesource.com/c/594750.
    
    Add tests for the ServiceWorker and SharedWorker, which should be blocked
    without displaying an interstitial. In order to distinguish between connected
    and blocked WebSockets, implement a simple EmbeddedTestServer handler sufficient
    for Chrome to consider a WebSocket connected. Also refactor the tests to reduce
    duplication and increase coverage.
    
    Bug: 750278
    Change-Id: I08368088fc384bd35549f6a388b0b23a9f18c561
    Reviewed-on: https://chromium-review.googlesource.com/602088
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Takeshi Yoshino <tyoshino@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494279}

--- chrome/renderer/chrome_content_renderer_client.cc
+++ chrome/renderer/chrome_content_renderer_client.cc
@@ -1372,7 +1372,9 @@ bool ChromeContentRendererClient::IsExtensionOrSharedModuleWhitelisted(
 
 std::unique_ptr<blink::WebSocketHandshakeThrottle>
 ChromeContentRendererClient::CreateWebSocketHandshakeThrottle() {
-  return nullptr;
+  InitSafeBrowsingIfNecessary();
+  return base::MakeUnique<safe_browsing::WebSocketSBHandshakeThrottle>(
+      safe_browsing_.get());
 }
 
 std::unique_ptr<blink::WebSpeechSynthesizer>
