672ad1a5876011ac20fc875efa43d233bc749dba
Michael Giuffrida <michaelpg@chromium.org>
2017-08-15 23:51:07 +0000

    Delegate CrOS system log creation for chrome.feedbackPrivate
    Currently, Chrome OS system logs for chrome.feedbackPrivate are created
    by a static function, SingleLogSourceFactory::CreateSingleLogSource.
    Tests can override the callback used by this "factory" to provide test
    log sources.
    
    Instead, use a virtual function on FeedbackPrivateDelegate so we can:
    
    1. Override its functionality naturally in tests without test code in
       the class itself
    2. Move the feedbackPrivate API to //extensions while still creating
       //chrome/browser/chromeos system logs only in Chrome
    
    Also consolidate tests into a new FeedbackPrivateApiUnittestBase which
    provides the common fakes for testing the API.
    
    Bug: 446329
    Change-Id: Ib855d5652d2e75e31bc2dcff3d0abe484d88be57
    Reviewed-on: https://chromium-review.googlesource.com/614680
    Reviewed-by: Simon Que <sque@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494612}

--- chrome/browser/extensions/api/feedback_private/chrome_feedback_private_delegate.h
+++ chrome/browser/extensions/api/feedback_private/chrome_feedback_private_delegate.h
@@ -11,6 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
+#include "chrome/common/extensions/api/feedback_private.h"
+
+namespace system_logs {
+class SystemLogsSource;
+}  // namespace system_logs
 
 namespace extensions {
 
@@ -25,6 +30,13 @@ class ChromeFeedbackPrivateDelegate : public FeedbackPrivateDelegate {
       bool from_crash) const override;
   system_logs::SystemLogsFetcher* CreateSystemLogsFetcher(
       content::BrowserContext* context) const override;
+
+#if defined(OS_CHROMEOS)
+  // TODO(michaelpg): Move declaration to FeedbackPrivateDelegate once the
+  // feedbackPrivate API header lives in //extensions.
+  virtual std::unique_ptr<system_logs::SystemLogsSource> CreateSingleLogSource(
+      api::feedback_private::LogSource source_type) const;
+#endif  // defined(OS_CHROMEOS)
 
  private:
   DISALLOW_COPY_AND_ASSIGN(ChromeFeedbackPrivateDelegate);
