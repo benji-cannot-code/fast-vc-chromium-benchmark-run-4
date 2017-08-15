b14b7af906a56ff1739e721b12f43e01018bc46d
Devlin Cronin <rdevlin.cronin@chromium.org>
2017-08-15 17:39:48 +0000

    [Extensions Bindings] Remove unnecessary requires
    Remove a few unnecessary requires in extension bindings files
    
    Bug: None
    Change-Id: I9079cefbbf9eb8f0b07d30b76cd7a687b4016229
    Reviewed-on: https://chromium-review.googlesource.com/612667
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494434}

--- chrome/renderer/resources/extensions/automation/automation_node.js
+++ chrome/renderer/resources/extensions/automation/automation_node.js
@@ -285,7 +285,6 @@ var GetLineThrough = natives.GetLineThrough;
  */
 var GetCustomActions = natives.GetCustomActions;
 
-var lastError = require('lastError');
 var logging = requireNative('logging');
 var utils = require('utils');
 
