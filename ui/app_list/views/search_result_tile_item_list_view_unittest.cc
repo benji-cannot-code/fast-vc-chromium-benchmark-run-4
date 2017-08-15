d72a35784b2647452ae9216e106d8f80bd9609c3
Qiang Xu <warx@chromium.org>
2017-08-15 18:56:31 +0000

    app_list: cap num of recommendation app results to 6
    changes:
    (1) cap num of recommendation app results to 6 for fullscreen app list.
    (2) recommendation app results layout should only be controlled by
    IsFullscreenAppListEnabled flag, except separators, play store badges, etc, whose
    visibility are controlled by IsPlayStoreAppSearchEnabled. This will fix the bug
    in crbug.com/754888 comment 2.
    (3) update SearchResultTileItemListViewTest, enable fullscreen app list flag
    for parameterized IsPlayStoreAppSearchEnabled tests.
    
    IsPlayStoreAppSearchEnabled flag.
    
    Test: test with/without IsFullscreenAppListEnabled flag and
    Bug: 754888
    Change-Id: If232b6bd90ce6fc427e373ae08d43e90f9d20b49
    Reviewed-on: https://chromium-review.googlesource.com/614750
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Qiang(Joe) Xu <warx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494472}

--- ui/app_list/views/search_result_tile_item_list_view_unittest.cc
+++ ui/app_list/views/search_result_tile_item_list_view_unittest.cc
@@ -26,8 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace app_list {
 
 namespace {
-constexpr int kNumSearchResultTiles = 8;
-// Constants when the Play Store app search feature is enabled.
 constexpr int kMaxNumSearchResultTiles = 6;
 constexpr int kInstalledApps = 4;
 constexpr int kPlayStoreApps = 2;
@@ -37,20 +35,24 @@ class SearchResultTileItemListViewTest
     : public views::ViewsTestBase,
       public ::testing::WithParamInterface<bool> {
  public:
-  SearchResultTileItemListViewTest() {}
-  ~SearchResultTileItemListViewTest() override {}
+  SearchResultTileItemListViewTest() = default;
+  ~SearchResultTileItemListViewTest() override = default;
 
  protected:
   void CreateSearchResultTileItemListView() {
-    // Switches on/off the Play Store app search feature.
+    // Enable fullscreen app list for parameterized Play Store app search
+    // feature.
     if (IsPlayStoreAppSearchEnabled()) {
-      scoped_feature_list_.InitAndEnableFeature(
-          app_list::features::kEnablePlayStoreAppSearch);
+      scoped_feature_list_.InitWithFeatures(
+          {features::kEnableFullscreenAppList,
+           features::kEnablePlayStoreAppSearch},
+          {});
     } else {
-      scoped_feature_list_.InitAndDisableFeature(
-          app_list::features::kEnablePlayStoreAppSearch);
+      scoped_feature_list_.InitWithFeatures(
+          {features::kEnableFullscreenAppList},
+          {features::kEnablePlayStoreAppSearch});
     }
-    EXPECT_EQ(IsPlayStoreAppSearchEnabled(),
+    ASSERT_EQ(IsPlayStoreAppSearchEnabled(),
               features::IsPlayStoreAppSearchEnabled());
 
     // Sets up the views.
@@ -143,7 +145,7 @@ TEST_P(SearchResultTileItemListViewTest, Basic) {
   // we added a separator for result type grouping.
   const int expected_child_count = IsPlayStoreAppSearchEnabled()
                                        ? kMaxNumSearchResultTiles * 2
-                                       : kNumSearchResultTiles;
+                                       : kMaxNumSearchResultTiles;
   EXPECT_EQ(expected_child_count, view()->child_count());
 
   // Tests item indexing by pressing TAB.
