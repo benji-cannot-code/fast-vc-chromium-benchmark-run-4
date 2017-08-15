8663601ae98c18fc37a69ebc3f57d22fab4fed76
Xi Cheng <chengx@chromium.org>
2017-08-15 20:37:34 +0000

    [Reland] Fix the decorating behavior of Chrome icon in the taskbar with Gaia image
    This is a reland of issue 604849. The only change is migrating the unit test added
    in profile_info_cache_unittest.cc from FILE thread to base/task_scheduler.
    
    Original change's description:
    
    Each profile has an icon, either one of built-in icons from Chrome, or from a G+
    profile image (Gaia image). In the multi-profile scenario, the Chrome icon in the
    taskbar is decorated with the profile icon.
    
    There have been reports that when a profile's icon is from a G+ profile image, it
    doesn't show up to decorate the Chrome icon sometimes. Instead, the built-in icon
    shows up.
    
    Here is the reason why this happens. The G+ profile image is read from disk
    asynchronously during launching Chrome. Meanwhile, the program thinks there's no
    such image so it uses the built-in icon to decorate the Chrome icon instead.
    
    The correct behavior for the Gaia image decoration should be avoid displaying
    anything until we've tried to load the image. If loading fails (because there is
    no image) then we should fall back to a built-in icon. This CL implements this.
    
    More code details and how this bug was introduced are discussed in
    https://docs.google.com/document/d/1f40dVO3ScbjQzqdh9n6c5CZEmhNZXR66HBSV1Ix_8-M/edit#
    
    Bug: 654499, 536553
    Change-Id: Ib23898578833a37f6d66de491017c77a25ef1abf
    Reviewed-on: https://chromium-review.googlesource.com/604849
    Commit-Queue: Xi Cheng <chengx@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Anthony Vallee-Dubois <anthonyvd@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#494272}
    Reviewed-on: https://chromium-review.googlesource.com/615217
    Cr-Commit-Position: refs/heads/master@{#494528}

--- chrome/browser/chromeos/profiles/avatar_menu_chromeos.cc
+++ chrome/browser/chromeos/profiles/avatar_menu_chromeos.cc
@@ -15,10 +15,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image.h"
 
 // static
-void AvatarMenu::GetImageForMenuButton(const base::FilePath& profile_path,
-                                       gfx::Image* image) {
+AvatarMenu::ImageLoadStatus AvatarMenu::GetImageForMenuButton(
+    const base::FilePath& profile_path,
+    gfx::Image* image) {
   // ChromeOS avatar icon is circular.
   Profile* profile =
       g_browser_process->profile_manager()->GetProfileByPath(profile_path);
   *image = gfx::Image(GetAvatarImageForContext(profile));
+  return AvatarMenu::ImageLoadStatus::LOADED;
 }
