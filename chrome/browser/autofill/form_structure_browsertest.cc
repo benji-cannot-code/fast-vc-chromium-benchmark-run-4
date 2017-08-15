b8587cd6dcad6397d6c4dc6c7dbedcc955e30c83
Roger McFarlane <rogerm@chromium.org>
2017-08-15 15:15:19 +0000

    [autofill] Rationalize CC field type predictions.
    This CL adds heuristic logic to avoid predicting lone credit card
    fields in forms.
    
    BUG=750293, 715828
    
    Change-Id: Ic6c85f3b6a7aa16cc3fce513b7e26ff022ec852e
    Reviewed-on: https://chromium-review.googlesource.com/605968
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Commit-Queue: Roger McFarlane <rogerm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494382}

--- chrome/browser/autofill/form_structure_browsertest.cc
+++ chrome/browser/autofill/form_structure_browsertest.cc
@@ -7,12 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/files/file_enumerator.h"
 #include "base/files/file_path.h"
 #include "base/macros.h"
 #include "base/path_service.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -21,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/autofill/content/browser/content_autofill_driver.h"
 #include "components/autofill/content/browser/content_autofill_driver_factory.h"
+#include "components/autofill/core/browser/autofill_experiments.h"
 #include "components/autofill/core/browser/autofill_manager.h"
 #include "components/autofill/core/browser/data_driven_test.h"
 #include "components/autofill/core/browser/form_structure.h"
@@ -99,11 +102,14 @@ class FormStructureBrowserTest
       const std::vector<std::unique_ptr<FormStructure>>& forms);
 
  private:
+  base::test::ScopedFeatureList feature_list_;
   DISALLOW_COPY_AND_ASSIGN(FormStructureBrowserTest);
 };
 
 FormStructureBrowserTest::FormStructureBrowserTest()
     : DataDrivenTest(GetTestDataDir()) {
+  feature_list_.InitAndEnableFeature(
+      autofill::kAutofillRationalizeFieldTypePredictions);
 }
 
 FormStructureBrowserTest::~FormStructureBrowserTest() {
