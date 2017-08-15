105ebb96c4f9cdb9a5f12455245a133a49050587
tzik <tzik@chromium.org>
2017-08-15 02:43:33 +0000

    Apply base_bind_rewriters to //content/browser/loader
    This CL applies //tools/clang/base_bind_rewriters to //content/browser/loader.
    It rewrites base::Bind to base::BindOnce where the resulting base::Callback
    is immediately converted to base::OnceCallback, which is considered safe
    to use base::BindOnce.
    
    E.g.:
      base::PostTask(FROM_HERE, base::Bind([]{}));
      base::OnceClosure cb = base::Bind([]{});
    are converted to:
      base::PostTask(FROM_HERE, base::BindOnce([]{}));
      base::OnceClosure cb = base::BindOnce([]{});
    
    Bug: 
    Change-Id: Ib734df51b161bd3ad84a5e7f0b02631526068f29
    Reviewed-on: https://chromium-review.googlesource.com/612936
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494309}

--- content/browser/loader/cross_site_resource_handler_browsertest.cc
+++ content/browser/loader/cross_site_resource_handler_browsertest.cc
@@ -77,9 +77,10 @@ class TestResourceDispatcherHostDelegate
 
     BrowserThread::PostTask(
         BrowserThread::IO, FROM_HERE,
-        base::Bind(&TestResourceDispatcherHostDelegate::SetTrackedURLOnIOThread,
-                   base::Unretained(this), tracked_url, run_on_start,
-                   run_loop_->QuitClosure()));
+        base::BindOnce(
+            &TestResourceDispatcherHostDelegate::SetTrackedURLOnIOThread,
+            base::Unretained(this), tracked_url, run_on_start,
+            run_loop_->QuitClosure()));
   }
 
   // Waits until the tracked URL has been requested, and the request for it has
@@ -114,7 +115,7 @@ class TestResourceDispatcherHostDelegate
                                 weak_factory_.GetWeakPtr()));
       BrowserThread::PostTask(
           BrowserThread::UI, FROM_HERE,
-          base::Bind(run_on_start_, resume_request_on_io_thread));
+          base::BindOnce(run_on_start_, resume_request_on_io_thread));
     }
 
     ~CallbackRunningResourceThrottle() override {
@@ -192,7 +193,7 @@ class CrossSiteResourceHandlerTest : public ContentBrowserTest {
   void SetUpOnMainThread() override {
     BrowserThread::PostTask(
         BrowserThread::IO, FROM_HERE,
-        base::Bind(
+        base::BindOnce(
             &CrossSiteResourceHandlerTest::InjectResourceDispatcherHostDelegate,
             base::Unretained(this)));
     host_resolver()->AddRule("*", "127.0.0.1");
@@ -203,9 +204,9 @@ class CrossSiteResourceHandlerTest : public ContentBrowserTest {
   void TearDownOnMainThread() override {
     BrowserThread::PostTask(
         BrowserThread::IO, FROM_HERE,
-        base::Bind(&CrossSiteResourceHandlerTest::
-                       RestoreResourceDispatcherHostDelegate,
-                   base::Unretained(this)));
+        base::BindOnce(&CrossSiteResourceHandlerTest::
+                           RestoreResourceDispatcherHostDelegate,
+                       base::Unretained(this)));
   }
 
  protected:
