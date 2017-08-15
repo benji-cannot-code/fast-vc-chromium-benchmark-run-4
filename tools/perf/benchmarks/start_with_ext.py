3d8f9527b5c26be7703800bfbe76f10ddd0387fc
Juan A. Navarro Perez <perezju@chromium.org>
2017-08-15 11:03:29 +0000

    Reland "[tools/perf] Re-enable start_wit_ext.* benchmarks"
    Changes: Keep the story disabled on Windows where it is
    still failing.
    
    This reverts commit afc7dd011216d97d717336c698d0bac4b6802a15.
    
    Original change's description:
    > Revert "[tools/perf] Re-enable start_wit_ext.* benchmarks"
    > 
    > This reverts commit 5a8f5e589ba81ef6e155457e3066d31e32ff99be.
    > 
    > Reason for revert: <INSERT REASONING HERE>
    > 
    > Original change's description:
    > > [tools/perf] Re-enable start_wit_ext.* benchmarks
    > > 
    > > The two benchmarks being reenabled everywhere are:
    > > 
    > > - startup_with_ext.cold.blank_page
    > > - startup_with_ext.warm.blank_page
    > > 
    > > This is part of the benchmark reenabling fix-it, where we're aiming to
    > > bring the stories that are disabled in line with the stories that are
    > > actually still failing. We're also migrating from wholesale benchmark
    > > disabling to per-story disabling when a story fails.
    > > 
    > > NOTRY=true
    > > 
    > > Bug: 753408,713222
    > > Change-Id: Ie5e188a6a7cea2112583df55b40e1424b241cff2
    > > Reviewed-on: https://chromium-review.googlesource.com/606427
    > > Reviewed-by: Ned Nguyen <nednguyen@google.com>
    > > Reviewed-by: rnephew <rnephew@chromium.org>
    > > Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#492657}
    > 
    > TBR=perezju@chromium.org,nednguyen@google.com,rnephew@chromium.org
    > 
    > Change-Id: Ie4866fbaf72f4ddf0e0c16d410a69415dc5e51af
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 753408, 713222
    > Reviewed-on: https://chromium-review.googlesource.com/606738
    > Reviewed-by: rnephew <rnephew@chromium.org>
    > Commit-Queue: rnephew <rnephew@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#492756}
    
    TBR=nednguyen@google.com,rnephew@chromium.org
    
    Bug: 753408, 713222
    Change-Id: I6744feef2ec391e787d9ca3c7582c5738e16eb6b
    Reviewed-on: https://chromium-review.googlesource.com/613160
    Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Reviewed-by: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494358}

--- tools/perf/benchmarks/start_with_ext.py
+++ tools/perf/benchmarks/start_with_ext.py
@@ -32,8 +32,6 @@ class _StartWithExt(perf_benchmark.PerfBenchmark):
 
 
 @benchmark.Enabled('has tabs')
-@benchmark.Disabled('mac')  # crbug.com/563424
-@benchmark.Disabled('win', 'linux', 'reference', 'android')
 class StartWithExtCold(_StartWithExt):
   """Measure time to start Chrome cold with extensions."""
   options = {'pageset_repeat': 5}
@@ -46,13 +44,12 @@ class StartWithExtCold(_StartWithExt):
   def GetExpectations(self):
     class StoryExpectations(story.expectations.StoryExpectations):
       def SetExpectations(self):
-        pass # blank_page.html not disabled.
+        self.DisableStory('blank_page.html', [story.expectations.ALL_WIN],
+                          'crbug.com/753408')
     return StoryExpectations()
 
 
 @benchmark.Enabled('has tabs')
-@benchmark.Disabled('mac')  # crbug.com/563424
-@benchmark.Disabled('win', 'linux', 'reference', 'android')
 class StartWithExtWarm(_StartWithExt):
   """Measure time to start Chrome warm with extensions."""
   options = {'pageset_repeat': 20}
@@ -65,5 +62,6 @@ class StartWithExtWarm(_StartWithExt):
   def GetExpectations(self):
     class StoryExpectations(story.expectations.StoryExpectations):
       def SetExpectations(self):
-        pass # blank_page.html not disabled.
+        self.DisableStory('blank_page.html', [story.expectations.ALL_WIN],
+                          'crbug.com/753408')
     return StoryExpectations()
