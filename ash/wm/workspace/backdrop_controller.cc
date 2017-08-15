2ef84be1edaa5ae742f5f6c642c5315532286700
MinChen <minch@chromium.org>
2017-08-15 21:24:19 +0000

    Remove tablet mode restriction for AppListVisibilityChanged notification in backdrop.
    Bug: 752383
    Change-Id: I990cbed13618e5857dc5927b787d9468d4a91626
    Reviewed-on: https://chromium-review.googlesource.com/615812
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: min c <minch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494552}

--- ash/wm/workspace/backdrop_controller.cc
+++ ash/wm/workspace/backdrop_controller.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/shell.h"
 #include "ash/system/tray/system_tray_notifier.h"
-#include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "ash/wm/window_util.h"
 #include "ash/wm/workspace/backdrop_delegate.h"
 #include "base/auto_reset.h"
@@ -160,13 +159,9 @@ void BackdropController::OnAppListVisibilityChanged(bool shown,
   if (container_->GetRootWindow() != root_window)
     return;
 
-  // Hide or update backdrop only for fullscreen app list in tablet mode.
-  if (!app_list::features::IsFullscreenAppListEnabled() ||
-      !Shell::Get()
-           ->tablet_mode_controller()
-           ->IsTabletModeWindowManagerEnabled()) {
+  // Hide or update backdrop only for fullscreen app list.
+  if (!app_list::features::IsFullscreenAppListEnabled())
     return;
-  }
 
   if (shown)
     AddForceHidden();
