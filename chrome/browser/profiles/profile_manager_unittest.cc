028ca9ed4f7cb0bdc300ec848d12cf76cdfd67a5
Gabriel Charette <gab@chromium.org>
2017-08-15 20:10:06 +0000

    Make profile directory creation always synchronous.
    This requires allowing IO on the main thread for two lightweight I/O
    operations (PathExists + CreateDirectory). The tradeoff is worth it as
    it ensures the directory is up before any of the asynchronous I/O from
    every service starts touching their portion of the profile directory.
    Sequencing all of these operations on the directory creation would be a
    herculian task and isn't worth it.
    
    R=rch@chromium.org
    
    NOPRESUBMIT=True (for intentional ScopedAllowIO usage)
    
    Bug: 755157, 689520
    Change-Id: I81498d20e91cae79ea3a1948b3d84d255adb6bd6
    Reviewed-on: https://chromium-review.googlesource.com/613683
    Reviewed-by: Anthony Vallee-Dubois <anthonyvd@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494517}

--- chrome/browser/profiles/profile_manager_unittest.cc
+++ chrome/browser/profiles/profile_manager_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "base/values.h"
 #include "build/build_config.h"
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
@@ -62,7 +63,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // defined(OS_CHROMEOS)
 
 using base::ASCIIToUTF16;
-using content::BrowserThread;
 
 namespace {
 
@@ -86,9 +86,10 @@ class UnittestProfileManager : public ::ProfileManagerWithoutInit {
 
   Profile* CreateProfileAsyncHelper(const base::FilePath& path,
                                     Delegate* delegate) override {
-    // This is safe while all file operations are done on the FILE thread.
-    BrowserThread::PostTask(
-        BrowserThread::FILE, FROM_HERE,
+    // ThreadTaskRunnerHandle::Get() is TestingProfile's "async" IOTaskRunner
+    // (ref. TestingProfile::GetIOTaskRunner()).
+    base::ThreadTaskRunnerHandle::Get()->PostTask(
+        FROM_HERE,
         base::BindOnce(base::IgnoreResult(&base::CreateDirectory), path));
 
     return new TestingProfile(path, this);
