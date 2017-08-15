550897538d24885c97a348da13dd78262bcb5d46
Shuhei Takahashi <nya@chromium.org>
2017-08-15 03:46:10 +0000

    recent: Add a private API to query recently modified files.
    Bug: 742722
    Test: browser_tests in CL:601571
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I85e03723f31841cbf01589286f9c0d71912afcbe
    Reviewed-on: https://chromium-review.googlesource.com/601727
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Tomasz Mikolajewski <mtomasz@chromium.org>
    Reviewed-by: Naoki Fukino <fukino@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494319}

--- extensions/browser/extension_function_histogram_value.h
+++ extensions/browser/extension_function_histogram_value.h
@@ -1250,6 +1250,7 @@ enum HistogramValue {
   WEBVIEWINTERNAL_SETAUDIOMUTED,
   WEBVIEWINTERNAL_ISAUDIOMUTED,
   WEBVIEWINTERNAL_GETAUDIOSTATE,
+  FILEMANAGERPRIVATE_GETRECENTFILES,
   // Last entry: Add new entries above, then run:
   // python tools/metrics/histograms/update_extension_histograms.py
   ENUM_BOUNDARY
