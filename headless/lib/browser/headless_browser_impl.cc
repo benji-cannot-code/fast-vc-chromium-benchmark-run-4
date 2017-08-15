421ebd2098b584ae38c1d2a16a85ce4999882d0e
David Vallet <dvallet@chromium.org>
2017-08-15 23:49:55 +0000

    Initialize IOThread for AIA fetching in headless
    Bug: 754319
    Change-Id: Iec5cebdfa53916bf8759909e444bafdc7acf5881
    Reviewed-on: https://chromium-review.googlesource.com/611695
    Reviewed-by: David Vallet <dvallet@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: David Vallet <dvallet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494610}

--- headless/lib/browser/headless_browser_impl.cc
+++ headless/lib/browser/headless_browser_impl.cc
@@ -31,6 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/devices/device_data_manager.h"
 #include "ui/gfx/geometry/size.h"
 
+#if defined(USE_NSS_CERTS)
+#include "net/cert_net/nss_ocsp.h"
+#endif
+
 namespace content {
 class DevToolsAgentHost;
 }
@@ -132,6 +136,11 @@ void HeadlessBrowserImpl::set_browser_main_parts(
 }
 
 void HeadlessBrowserImpl::RunOnStartCallback() {
+#if defined(USE_NSS_CERTS)
+  content::BrowserThread::PostTask(
+      content::BrowserThread::IO, FROM_HERE,
+      base::Bind(&net::SetMessageLoopForNSSHttpIO));
+#endif
   // We don't support the tethering domain on this agent host.
   agent_host_ = content::DevToolsAgentHost::CreateForBrowser(
       nullptr, content::DevToolsAgentHost::CreateServerSocketCallback());
