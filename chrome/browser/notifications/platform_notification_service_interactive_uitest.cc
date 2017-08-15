1740376c359bb286ad33dfd6c016e81e423d84ef
Reilly Grant <reillyg@chromium.org>
2017-08-15 19:34:29 +0000

    Revert "Unify testing infrastructure of the NotificationDisplayService"
    This reverts commit 2c5d724193977fcd9a67f60fea90c3f80d0d692e.
    
    Reason for revert: This change is triggering CFI (control-flow integrity) test failures: https://uberchromegw.corp.google.com/i/chromium.memory/builders/Linux%20CFI/builds/1054
    
    Original change's description:
    > Unify testing infrastructure of the NotificationDisplayService
    > 
    > This avoids individual tests from depending directly on a specific
    > testing mechanism, making future refactorings (much) easier and
    > enabling a path towards platform-specific testing routines.
    > 
    > BUG=
    > 
    > Change-Id: I781c1c4855b9b06030222c4f8e864cf815cc900b
    > Reviewed-on: https://chromium-review.googlesource.com/613166
    > Commit-Queue: Peter Beverloo <peter@chromium.org>
    > Reviewed-by: John Mellor <johnme@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#494393}
    
    TBR=johnme@chromium.org,peter@chromium.org,delphick@chromium.org
    
    Change-Id: I363e78a37003c1864cb27fb5995edd742ed014d3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/615882
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494498}

--- chrome/browser/notifications/platform_notification_service_interactive_uitest.cc
+++ chrome/browser/notifications/platform_notification_service_interactive_uitest.cc
@@ -22,9 +22,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/notifications/desktop_notification_profile_util.h"
 #include "chrome/browser/notifications/notification.h"
 #include "chrome/browser/notifications/notification_common.h"
-#include "chrome/browser/notifications/notification_display_service_tester.h"
+#include "chrome/browser/notifications/notification_display_service.h"
+#include "chrome/browser/notifications/notification_display_service_factory.h"
 #include "chrome/browser/notifications/notification_test_util.h"
 #include "chrome/browser/notifications/platform_notification_service_impl.h"
+#include "chrome/browser/notifications/stub_notification_display_service.h"
 #include "chrome/browser/notifications/web_notification_delegate.h"
 #include "chrome/browser/permissions/permission_manager.h"
 #include "chrome/browser/permissions/permission_request_manager.h"
@@ -88,23 +90,23 @@ class PlatformNotificationServiceBrowserTest : public InProcessBrowserTest {
   }
 
   void SetUpOnMainThread() override {
-    display_service_tester_ =
-        base::MakeUnique<NotificationDisplayServiceTester>(
-            browser()->profile());
+    NotificationDisplayServiceFactory::GetInstance()->SetTestingFactory(
+        browser()->profile(), &StubNotificationDisplayService::FactoryForTests);
 
     SiteEngagementScore::SetParamValuesForTesting();
     NavigateToTestPage(std::string("/") + kTestFileName);
-  }
-
-  void TearDown() override {
-    display_service_tester_.reset();
-    InProcessBrowserTest::TearDown();
   }
 
  protected:
   // Returns the Platform Notification Service these unit tests are for.
   PlatformNotificationServiceImpl* service() const {
     return PlatformNotificationServiceImpl::GetInstance();
+  }
+
+  // Returns the stub notification display service.
+  StubNotificationDisplayService* GetDisplayService() const {
+    return static_cast<StubNotificationDisplayService*>(
+        NotificationDisplayServiceFactory::GetForProfile(browser()->profile()));
   }
 
   // Returns a vector with the Notification objects that are being displayed
@@ -115,7 +117,7 @@ class PlatformNotificationServiceBrowserTest : public InProcessBrowserTest {
                                         ? NotificationCommon::PERSISTENT
                                         : NotificationCommon::NON_PERSISTENT;
 
-    return display_service_tester_->GetDisplayedNotificationsForType(type);
+    return GetDisplayService()->GetDisplayedNotificationsForType(type);
   }
 
   // Grants permission to display Web Notifications for origin of the test
@@ -195,7 +197,6 @@ class PlatformNotificationServiceBrowserTest : public InProcessBrowserTest {
   }
 
   const base::FilePath server_root_;
-  std::unique_ptr<NotificationDisplayServiceTester> display_service_tester_;
 
  private:
   std::unique_ptr<net::EmbeddedTestServer> https_server_;
@@ -672,7 +673,7 @@ IN_PROC_BROWSER_TEST_F(PlatformNotificationServiceBrowserTest,
   ASSERT_TRUE(
       base::StartsWith(notification.id(), "p:", base::CompareCase::SENSITIVE));
 
-  display_service_tester_->RemoveNotification(
+  GetDisplayService()->RemoveNotification(
       NotificationCommon::PERSISTENT, notification.delegate_id(),
       false /* by_user */, true /* silent */);
 
