df4f6c44b4676f1db067dc674b3156fabb7e98c1
Istiaque Ahmed <lazyboy@chromium.org>
2017-08-15 21:06:22 +0000

    [TaskScheduler] Migrate ValidateCrxHelper off FILE thread.
    FILE thread is deprecated, use SequencedTaskRunner instead.
    --validate-crx is fairly isolated from regular browsing,
    however since there is a very very small change of file
    read colliding with other extension system file access,
    use ExtensionFileTaskRunner to be safe.
    
    Bug: 689520
    Test: ./out/Release/chrome --validate-crx=crx_file_path With valid, invalid and non-existent file.
    
    Change-Id: I06e7ad5a4bba5cab3c86d470e891c4b6fe81cee4
    Reviewed-on: https://chromium-review.googlesource.com/610491
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494543}

--- chrome/browser/extensions/startup_helper.cc
+++ chrome/browser/extensions/startup_helper.cc
@@ -14,9 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/task_scheduler/post_task.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/chrome_extensions_client.h"
 #include "content/public/browser/browser_thread.h"
+#include "extensions/browser/extension_file_task_runner.h"
 #include "extensions/browser/sandboxed_unpacker.h"
 #include "extensions/common/extension.h"
 
@@ -79,21 +81,19 @@ class ValidateCrxHelper : public SandboxedUnpackerClient {
  public:
   ValidateCrxHelper(const CRXFileInfo& file,
                     const base::FilePath& temp_dir,
-                    base::RunLoop* run_loop)
+                    base::OnceClosure quit_closure)
       : crx_file_(file),
         temp_dir_(temp_dir),
-        run_loop_(run_loop),
-        finished_(false),
+        quit_closure_(std::move(quit_closure)),
         success_(false) {}
 
-  bool finished() { return finished_; }
   bool success() { return success_; }
   const base::string16& error() { return error_; }
 
   void Start() {
-    BrowserThread::PostTask(
-        BrowserThread::FILE, FROM_HERE,
-        base::BindOnce(&ValidateCrxHelper::StartOnFileThread, this));
+    GetExtensionFileTaskRunner()->PostTask(
+        FROM_HERE,
+        base::BindOnce(&ValidateCrxHelper::StartOnBlockingThread, this));
   }
 
  protected:
@@ -104,7 +104,7 @@ class ValidateCrxHelper : public SandboxedUnpackerClient {
                        std::unique_ptr<base::DictionaryValue> original_manifest,
                        const Extension* extension,
                        const SkBitmap& install_icon) override {
-    finished_ = true;
+    DCHECK(GetExtensionFileTaskRunner()->RunsTasksInCurrentSequence());
     success_ = true;
     BrowserThread::PostTask(
         BrowserThread::UI, FROM_HERE,
@@ -112,7 +112,7 @@ class ValidateCrxHelper : public SandboxedUnpackerClient {
   }
 
   void OnUnpackFailure(const CrxInstallError& error) override {
-    finished_ = true;
+    DCHECK(GetExtensionFileTaskRunner()->RunsTasksInCurrentSequence());
     success_ = false;
     error_ = error.message();
     BrowserThread::PostTask(
@@ -121,19 +121,15 @@ class ValidateCrxHelper : public SandboxedUnpackerClient {
   }
 
   void FinishOnUIThread() {
-    CHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-    if (run_loop_->running())
-      run_loop_->Quit();
+    DCHECK_CURRENTLY_ON(BrowserThread::UI);
+    std::move(quit_closure_).Run();
   }
 
-  void StartOnFileThread() {
-    CHECK(BrowserThread::CurrentlyOn(BrowserThread::FILE));
-    scoped_refptr<base::SingleThreadTaskRunner> file_task_runner =
-        BrowserThread::GetTaskRunnerForThread(BrowserThread::FILE);
-
+  void StartOnBlockingThread() {
+    DCHECK(GetExtensionFileTaskRunner()->RunsTasksInCurrentSequence());
     scoped_refptr<SandboxedUnpacker> unpacker(new SandboxedUnpacker(
         Manifest::INTERNAL, 0, /* no special creation flags */
-        temp_dir_, file_task_runner.get(), this));
+        temp_dir_, GetExtensionFileTaskRunner().get(), this));
     unpacker->StartWithCrx(crx_file_);
   }
 
@@ -143,17 +139,17 @@ class ValidateCrxHelper : public SandboxedUnpackerClient {
   // The temporary directory where the sandboxed unpacker will do work.
   const base::FilePath& temp_dir_;
 
-  // Unowned pointer to a runloop, so our consumer can wait for us to finish.
-  base::RunLoop* run_loop_;
-
-  // Whether we're finished unpacking;
-  bool finished_;
+  // Closure called upon completion.
+  base::OnceClosure quit_closure_;
 
   // Whether the unpacking was successful.
   bool success_;
 
   // If the unpacking wasn't successful, this contains an error message.
   base::string16 error_;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(ValidateCrxHelper);
 };
 
 }  // namespace
@@ -177,7 +173,7 @@ bool StartupHelper::ValidateCrx(const base::CommandLine& cmd_line,
   base::RunLoop run_loop;
   CRXFileInfo file(path);
   scoped_refptr<ValidateCrxHelper> helper(
-      new ValidateCrxHelper(file, temp_dir.GetPath(), &run_loop));
+      new ValidateCrxHelper(file, temp_dir.GetPath(), run_loop.QuitClosure()));
   helper->Start();
   run_loop.Run();
 
