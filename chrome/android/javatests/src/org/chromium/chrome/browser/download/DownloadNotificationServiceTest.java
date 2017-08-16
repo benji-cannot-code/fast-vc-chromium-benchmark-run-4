6067fe18811011742f7f55441ccd94f2f24232f2
Max Morin <maxmorin@chromium.org>
2017-08-16 10:13:17 +0000

    Disable a DownloadNotificationServiceTest.
    Disable DownloadNotificationServiceTest#testResumeAllPendingDownloads,
    since it's flaky.
    
    BUG=755907
    TBR=qinmin
    
    Change-Id: I5f766673db63fd81f18b925232b51988583eb1aa
    Reviewed-on: https://chromium-review.googlesource.com/616666
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Commit-Queue: Max Morin <maxmorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494742}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadNotificationServiceTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadNotificationServiceTest.java
@@ -17,6 +17,7 @@ import android.test.ServiceTestCase;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.AdvancedMockContext;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.components.offline_items_collection.ContentId;
@@ -305,9 +306,10 @@ public class DownloadNotificationServiceTest extends
     /**
      * Tests resume all pending downloads. Only auto resumable downloads can resume.
      */
-    @SmallTest
-    @Feature({"Download"})
-    @RetryOnFailure
+    //@SmallTest
+    //@Feature({"Download"})
+    //@RetryOnFailure
+    @DisabledTest
     public void testResumeAllPendingDownloads() throws Exception {
         setupService();
         Context mockContext = new AdvancedMockContext(getSystemContext());
