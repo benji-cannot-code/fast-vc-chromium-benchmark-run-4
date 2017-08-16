4c62896bf8cd4197a12909742a6ade7b2878b7ef
Tetsui Ohkubo <tetsui@chromium.org>
2017-08-16 09:18:04 +0000

    Add accessibility string to expand button in MD notification.
    This CL adds "Maximize" or "Minimize" accessibility string to expand /
    collapse button in the notification header of MD notification.
    
    BUG=755071
    TEST=manual
    
    Change-Id: Iee2d67a2ee01af34221ecc03b4f6f3b0a9ab97c0
    Reviewed-on: https://chromium-review.googlesource.com/612962
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Naoki Fukino <fukino@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494733}

--- ui/message_center/views/notification_header_view.cc
+++ ui/message_center/views/notification_header_view.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
+#include "ui/accessibility/ax_node_data.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/font_list.h"
@@ -60,9 +61,11 @@ class ExpandButton : public views::ImageView {
   ExpandButton();
   ~ExpandButton() override;
 
+  // Overridden from views::ImageView:
   void OnPaint(gfx::Canvas* canvas) override;
   void OnFocus() override;
   void OnBlur() override;
+  void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
 
  private:
   std::unique_ptr<views::Painter> focus_painter_;
@@ -89,6 +92,11 @@ void ExpandButton::OnFocus() {
 void ExpandButton::OnBlur() {
   views::ImageView::OnBlur();
   SchedulePaint();
+}
+
+void ExpandButton::GetAccessibleNodeData(ui::AXNodeData* node_data) {
+  node_data->role = ui::AX_ROLE_BUTTON;
+  node_data->SetName(views::ImageView::GetTooltipText());
 }
 
 // Do relative time string formatting that is similar to
@@ -279,6 +287,10 @@ void NotificationHeaderView::SetExpanded(bool expanded) {
   expand_button_->SetImage(gfx::CreateVectorIcon(
       expanded ? kNotificationExpandLessIcon : kNotificationExpandMoreIcon,
       kExpandIconSize, accent_color_));
+  // TODO(tetsui): Replace by more helpful accessibility strings.
+  // https://crbug.com/755855
+  expand_button_->SetTooltipText(l10n_util::GetStringUTF16(
+      expanded ? IDS_APP_ACCNAME_MINIMIZE : IDS_APP_ACCNAME_MAXIMIZE));
 }
 
 void NotificationHeaderView::SetAccentColor(SkColor color) {
