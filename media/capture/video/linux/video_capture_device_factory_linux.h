b7fc8bc7750807bedd4fd11916789b14451635a2
Aleksandar Stojiljkovic <aleksandar.stojiljkovic@intel.com>
2017-08-16 19:48:02 +0000

    Fix ambiguity with names of composited video capture devices
    Webcams with multiple cameras on single device would display all
    of them with the same name on Linux. This makes it look like on
    Windows; using different sub cameras names.
    
    BUG=686756
    
    Change-Id: Ib717e7dd8f375ad71cfa1ea7ac19cec02337e94d
    Reviewed-on: https://chromium-review.googlesource.com/572901
    Commit-Queue: Aleksandar Stojiljkovic <aleksandar.stojiljkovic@intel.com>
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Reviewed-by: Christian Fremerey <chfremer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494905}

--- media/capture/video/linux/video_capture_device_factory_linux.h
+++ media/capture/video/linux/video_capture_device_factory_linux.h
@@ -34,8 +34,6 @@ class CAPTURE_EXPORT VideoCaptureDeviceFactoryLinux
       VideoCaptureFormats* supported_formats) override;
 
  private:
-  std::string GetDeviceModelId(const std::string& device_id);
-
   scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner_;
   DISALLOW_COPY_AND_ASSIGN(VideoCaptureDeviceFactoryLinux);
 };
