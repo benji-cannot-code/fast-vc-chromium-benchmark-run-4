4aa44903c03eb605c98c53c81d7181398d40e94d
W. James MacLean <wjmaclean@chromium.org>
2017-08-16 15:00:43 +0000

    Keep track of which guests destroy on detach.
    To maintain consistency with non-oopif webview and PDF, we
    need to destroy PDF GuestViews on detach, but not other
    guestViews. https://chromium-review.googlesource.com/c/587448
    regressed the behaviour fixed in the related bug, this CL fixes
    that.
    
    Bug: 569903
    Change-Id: I796954c282631e9ba50270e79d6a1b84fe12c2f8
    Reviewed-on: https://chromium-review.googlesource.com/616006
    Reviewed-by: Lucas Gadani <lfg@chromium.org>
    Commit-Queue: James MacLean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494780}

--- components/guest_view/browser/guest_view_base.cc
+++ components/guest_view/browser/guest_view_base.cc
@@ -432,12 +432,16 @@ void GuestViewBase::DidAttach(int guest_proxy_routing_id) {
   SendQueuedEvents();
 }
 
+// TODO(wjmaclean): Delete this when browser plugin goes away;
+// https://crbug.com/533069 .
 void GuestViewBase::DidDetach() {
   GuestViewManager::FromBrowserContext(browser_context_)->DetachGuest(this);
   StopTrackingEmbedderZoomLevel();
   owner_web_contents()->Send(new GuestViewMsg_GuestDetached(
       element_instance_id_));
   element_instance_id_ = kInstanceIDNone;
+  if (ShouldDestroyOnDetach())
+    Destroy(true);
 }
 
 WebContents* GuestViewBase::GetOwnerWebContents() const {
@@ -450,6 +454,10 @@ void GuestViewBase::GuestSizeChanged(const gfx::Size& new_size) {
 
 const GURL& GuestViewBase::GetOwnerSiteURL() const {
   return owner_web_contents()->GetLastCommittedURL();
+}
+
+bool GuestViewBase::ShouldDestroyOnDetach() const {
+  return false;
 }
 
 void GuestViewBase::Destroy(bool also_delete) {
