7bfbd6e8045f61c22d80fafd18eda3ca0c7fdac8
Jun Cai <juncai@chromium.org>
2017-08-16 15:07:03 +0000

    Revert "Reland of Refactor DeviceMotionEventPump to use //services/device/generic_sensor instead of //device/sensors"
    This reverts commit 339480b39644cb3c98c57a69d183f04acdd5d2c5.
    
    Reason for revert:
    https://bugs.chromium.org/p/chromium/issues/detail?id=753263
    
    Original change's description:
    > Reland of Refactor DeviceMotionEventPump to use //services/device/generic_sensor instead of //device/sensors
    > 
    > The initial upload patch of this CL is the same as:
    > https://chromium-review.googlesource.com/c/550921
    > 
    > The latest patch of this CL contains the fix for issue 753263.
    > 
    > TBR=reillyg@chromium.org, timvolodine@chromium.org
    > 
    > Bug: 721427, 735420, 753263
    > Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    > Change-Id: If5f1d9ba33eabfc9c80858276daad8d5d08df435
    > Reviewed-on: https://chromium-review.googlesource.com/606611
    > Commit-Queue: Jun Cai <juncai@chromium.org>
    > Reviewed-by: Jun Cai <juncai@chromium.org>
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#494130}
    
    TBR=jam@chromium.org,reillyg@chromium.org,timvolodine@chromium.org,juncai@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 721427, 735420, 753263
    Change-Id: I7d88ee2b6121c76cfb2520d816c52ce07387c6f5
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Reviewed-on: https://chromium-review.googlesource.com/616820
    Reviewed-by: Jun Cai <juncai@chromium.org>
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494785}

--- services/device/device_service.cc
+++ services/device/device_service.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
+#include "base/feature_list.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/weak_ptr.h"
 #include "base/single_thread_task_runner.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/device/fingerprint/fingerprint.h"
 #include "services/device/generic_sensor/sensor_provider_impl.h"
 #include "services/device/power_monitor/power_monitor_message_broadcaster.h"
+#include "services/device/public/cpp/device_features.h"
 #include "services/device/public/interfaces/battery_monitor.mojom.h"
 #include "services/device/serial/serial_device_enumerator_impl.h"
 #include "services/device/time_zone_monitor/time_zone_monitor.h"
@@ -90,6 +92,8 @@ DeviceService::~DeviceService() {
 void DeviceService::OnStart() {
   registry_.AddInterface<mojom::Fingerprint>(base::Bind(
       &DeviceService::BindFingerprintRequest, base::Unretained(this)));
+  registry_.AddInterface<mojom::MotionSensor>(base::Bind(
+      &DeviceService::BindMotionSensorRequest, base::Unretained(this)));
   registry_.AddInterface<mojom::OrientationSensor>(base::Bind(
       &DeviceService::BindOrientationSensorRequest, base::Unretained(this)));
   registry_.AddInterface<mojom::OrientationAbsoluteSensor>(
@@ -100,8 +104,10 @@ void DeviceService::OnStart() {
   registry_.AddInterface<mojom::ScreenOrientationListener>(
       base::Bind(&DeviceService::BindScreenOrientationListenerRequest,
                  base::Unretained(this)));
-  registry_.AddInterface<mojom::SensorProvider>(base::Bind(
-      &DeviceService::BindSensorProviderRequest, base::Unretained(this)));
+  if (base::FeatureList::IsEnabled(features::kGenericSensor)) {
+    registry_.AddInterface<mojom::SensorProvider>(base::Bind(
+        &DeviceService::BindSensorProviderRequest, base::Unretained(this)));
+  }
   registry_.AddInterface<mojom::TimeZoneMonitor>(base::Bind(
       &DeviceService::BindTimeZoneMonitorRequest, base::Unretained(this)));
   registry_.AddInterface<mojom::WakeLockProvider>(base::Bind(
@@ -154,6 +160,22 @@ void DeviceService::BindVibrationManagerRequest(
 
 void DeviceService::BindFingerprintRequest(mojom::FingerprintRequest request) {
   Fingerprint::Create(std::move(request));
+}
+
+void DeviceService::BindMotionSensorRequest(
+    mojom::MotionSensorRequest request) {
+#if defined(OS_ANDROID)
+  // On Android the device sensors implementations need to run on the UI thread
+  // to communicate to Java.
+  DeviceMotionHost::Create(std::move(request));
+#else
+  // On platforms other than Android the device sensors implementations run on
+  // the IO thread.
+  if (io_task_runner_) {
+    io_task_runner_->PostTask(FROM_HERE, base::Bind(&DeviceMotionHost::Create,
+                                                    base::Passed(&request)));
+  }
+#endif  // defined(OS_ANDROID)
 }
 
 void DeviceService::BindOrientationSensorRequest(
