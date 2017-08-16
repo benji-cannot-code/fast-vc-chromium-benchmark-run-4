0c96eb2f3f7df3c6cea92e8a4d97a93b2873b5fa
Vladislav Kaznacheev <kaznacheev@google.com>
2017-08-16 20:26:57 +0000

    Notify the container about metalayer mode changes
    Entering the metalayer mode should cause an explanatory
    message shown in the voice interaction framework UI.
    
    This can be achieved by using existing mojo method
    VoiceInteractionFrameworkInstance.SetMetalayerVisibility, which
    has been introduced for the previous metalayer implementation and
    has not been deprecated yet.
    
    Bug: b:64218411
    Change-Id: Ib5d26dc60818b0d1594dc4a03e341a15af7ac2d1
    Reviewed-on: https://chromium-review.googlesource.com/617325
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Commit-Queue: Vladislav Kaznacheev <kaznacheev@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494924}

--- chrome/browser/chromeos/arc/voice_interaction/arc_voice_interaction_framework_service.cc
+++ chrome/browser/chromeos/arc/voice_interaction/arc_voice_interaction_framework_service.cc
@@ -321,6 +321,7 @@ void ArcVoiceInteractionFrameworkService::ShowMetalayer(
     return;
   }
   metalayer_closed_callback_ = closed;
+  NotifyMetalayerStatusChanged(true);
 }
 
 void ArcVoiceInteractionFrameworkService::HideMetalayer() {
@@ -330,6 +331,7 @@ void ArcVoiceInteractionFrameworkService::HideMetalayer() {
     return;
   }
   metalayer_closed_callback_ = base::Closure();
+  NotifyMetalayerStatusChanged(false);
 }
 
 void ArcVoiceInteractionFrameworkService::OnArcPlayStoreEnabledChanged(
@@ -353,6 +355,18 @@ void ArcVoiceInteractionFrameworkService::StartVoiceInteractionSetupWizard() {
   if (!framework_instance)
     return;
   framework_instance->StartVoiceInteractionSetupWizard();
+}
+
+void ArcVoiceInteractionFrameworkService::NotifyMetalayerStatusChanged(
+    bool visible) {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  mojom::VoiceInteractionFrameworkInstance* framework_instance =
+      ARC_GET_INSTANCE_FOR_METHOD(
+          arc_bridge_service_->voice_interaction_framework(),
+          SetMetalayerVisibility);
+  if (!framework_instance)
+    return;
+  framework_instance->SetMetalayerVisibility(visible);
 }
 
 void ArcVoiceInteractionFrameworkService::CallAndResetMetalayerCallback() {
