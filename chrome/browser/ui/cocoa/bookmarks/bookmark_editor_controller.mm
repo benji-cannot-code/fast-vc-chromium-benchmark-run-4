9fe956756712d0293abc246ed6711670665a9ecb
spqchan <spqchan@chromium.org>
2017-08-16 16:54:57 +0000

    Feature Flag for Dialog Touch Bar Support
    Dialog touch bar support is currently behind
    the Browser Touch Bar flag. This needs to be
    separated into a separate flag that's disabled
    by default.
    
    Bug: 755814
    Change-Id: I8f9ca544d48548f37f48aa529123e97910e58b79
    Reviewed-on: https://chromium-review.googlesource.com/616341
    Commit-Queue: Sarah Chan <spqchan@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494827}

--- chrome/browser/ui/cocoa/bookmarks/bookmark_editor_controller.mm
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_editor_controller.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/bookmarks/bookmark_utils.h"
 #import "chrome/browser/ui/cocoa/dialog_text_field_editor.h"
+#include "chrome/common/chrome_features.h"
 #include "components/bookmarks/browser/bookmark_expanded_state_tracker.h"
 #include "components/bookmarks/browser/bookmark_model.h"
 #include "components/url_formatter/url_fixer.h"
@@ -91,6 +92,9 @@ using bookmarks::BookmarkNode;
 }
 
 - (id)windowWillReturnFieldEditor:(NSWindow*)sender toObject:(id)obj {
+  if (!base::FeatureList::IsEnabled(features::kDialogTouchBar))
+    return nil;
+
   if (obj == urlField_ || obj == nameTextField_)
     return touchBarFieldEditor_.get();
 
