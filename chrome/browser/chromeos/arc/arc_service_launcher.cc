e807cb03ab4c98495fbca22e4490057896d018e9
Jeffrey Kardatzke <jkardatzke@google.com>
2017-08-16 20:04:33 +0000

    Widevine OEMCrypto ArcBridge proxy Mojo interface
    This is used by the Widevine L1 DRM implementation in ARC++.
    It is a proxy for connecting to the daemon service running in
    ChromeOS. This takes a Mojo connection over ArcBridge, then uses
    D-Bus to connect to the ChromeOS daemon to initiate a Mojo connection
    to that daemon. Then it returns the Mojo interface from the daemon back
    to the Android side. It retains a Mojo connection to the daemon
    for use in future sessions, and tears that down if the daemon dies.
    
    BUG=b:62576736
    
    Change-Id: Ib6632f189138f43c476ff3c9101c45eca15e0e52
    Reviewed-on: https://chromium-review.googlesource.com/612686
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494913}

--- chrome/browser/chromeos/arc/arc_service_launcher.cc
+++ chrome/browser/chromeos/arc/arc_service_launcher.cc
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/metrics/arc_metrics_service.h"
 #include "components/arc/net/arc_net_host_impl.h"
 #include "components/arc/obb_mounter/arc_obb_mounter_bridge.h"
+#include "components/arc/oemcrypto/arc_oemcrypto_bridge.h"
 #include "components/arc/power/arc_power_bridge.h"
 #include "components/arc/storage_manager/arc_storage_manager.h"
 #include "components/arc/volume_mounter/arc_volume_mounter_bridge.h"
@@ -147,6 +148,7 @@ void ArcServiceLauncher::OnPrimaryUserProfilePrepared(Profile* profile) {
   ArcNetHostImpl::GetForBrowserContext(profile);
   ArcNotificationManager::GetForBrowserContext(profile);
   ArcObbMounterBridge::GetForBrowserContext(profile);
+  ArcOemCryptoBridge::GetForBrowserContext(profile);
   ArcPolicyBridge::GetForBrowserContext(profile);
   ArcPowerBridge::GetForBrowserContext(profile);
   ArcPrintService::GetForBrowserContext(profile);
