91ddc0d247d9ee7e3bda73acfb49c42cad5b1499
Xida Chen <xidachen@chromium.org>
2017-08-16 21:21:01 +0000

    Add UMA to measure memory usage when prepare tiles
    This CL adds a histograms to record how much GPU memory do we use when
    tile manager assigns memory to tiles. The histogram will tell us
    what is the optimal memory limit for low-end android devices with physical
    memory of 512MB.
    
    Bug: 754343
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I49e03b04c41a6a505b7937a617e75664336d7887
    Reviewed-on: https://chromium-review.googlesource.com/612183
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494953}

--- cc/tiles/tile_manager.cc
+++ cc/tiles/tile_manager.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/optional.h"
+#include "base/sys_info.h"
 #include "base/threading/thread_checker.h"
 #include "base/trace_event/trace_event_argument.h"
 #include "cc/base/devtools_instrumentation.h"
@@ -826,6 +827,15 @@ TileManager::PrioritizedWorkToSchedule TileManager::AssignGpuMemoryToTiles() {
             &work_to_schedule.checker_image_decode_queue);
       }
     }
+  }
+
+  // The hard_limit for low-end devices is 8MB, so we set the max_value for the
+  // histogram to be 8200KB.
+  if (had_enough_memory_to_schedule_tiles_needed_now &&
+      base::SysInfo::AmountOfPhysicalMemoryMB() <= 512) {
+    int64_t tiles_gpu_memory_kb = memory_usage.memory_bytes() / 1024;
+    UMA_HISTOGRAM_CUSTOM_COUNTS("TileManager.TilesGPUMemoryUsage",
+                                tiles_gpu_memory_kb, 1, 8200, 100);
   }
 
   UMA_HISTOGRAM_BOOLEAN("TileManager.ExceededMemoryBudget",
