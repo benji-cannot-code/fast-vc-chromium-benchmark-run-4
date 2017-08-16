dfcabf6aabbc3b229f72f47bc855ec76101d864e
yoshiki iguchi <yoshiki@chromium.org>
2017-08-16 05:55:02 +0000

    Remove unnecessary operation on opening message center.
    This CL removes unnecessary image operation on opening message center. With this CL, opening the message center gets 50-500 times faster.
    
    Bug: b/64544553
    Bug: 750587
    Change-Id: I045f1a285fcacb6dafb997ad2fefbfe4ad54791b
    Reviewed-on: https://chromium-review.googlesource.com/616520
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494703}

--- chrome/browser/notifications/arc_application_notifier_source_chromeos.cc
+++ chrome/browser/notifications/arc_application_notifier_source_chromeos.cc
@@ -62,10 +62,6 @@ ArcApplicationNotifierSourceChromeOS::GetNotifierList(Profile* profile) {
                        kArcAppIconSizeInDp,
                        // The life time of icon must shorter than |this|.
                        this));
-    icon->image_skia().GetRepresentation(
-        ui::GetSupportedScaleFactor(display::Screen::GetScreen()
-                                        ->GetPrimaryDisplay()
-                                        .device_scale_factor()));
     // Apply icon now to set the default image.
     OnIconUpdated(icon.get());
 
