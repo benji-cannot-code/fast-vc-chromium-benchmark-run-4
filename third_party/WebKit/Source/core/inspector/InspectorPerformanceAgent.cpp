5fb1a937051827513b81397ff54edf1025006aa5
Alexei Filippov <alph@chromium.org>
2017-08-16 23:07:52 +0000

    DevTools: Add RecalcStyle, Layout & JS metrics to Performance.metrics
    These include timings and counts.
    
    BUG=751892
    
    Change-Id: I43cb6550fb5df550aa2b135de3572311b3924186
    Reviewed-on: https://chromium-review.googlesource.com/617373
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494988}

--- third_party/WebKit/Source/core/inspector/InspectorPerformanceAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorPerformanceAgent.cpp
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/LocalFrame.h"
 #include "core/inspector/InspectedFrames.h"
 #include "core/paint/PaintTiming.h"
+#include "core/probe/CoreProbes.h"
 #include "platform/InstanceCounters.h"
 #include "platform/wtf/dtoa/utils.h"
 
@@ -69,6 +70,12 @@ Response InspectorPerformanceAgent::getMetrics(
   AppendMetric(
       result.get(), "ResourceCount",
       InstanceCounters::CounterValue(InstanceCounters::kResourceCounter));
+  AppendMetric(result.get(), "LayoutCount", static_cast<double>(layout_count_));
+  AppendMetric(result.get(), "RecalcStyleCount",
+               static_cast<double>(recalc_style_count_));
+  AppendMetric(result.get(), "LayoutDuration", layout_duration_);
+  AppendMetric(result.get(), "RecalcStyleDuration", recalc_style_duration_);
+  AppendMetric(result.get(), "ScriptDuration", script_duration_);
 
   // Performance timings.
   Document* document = inspected_frames_->Root()->GetDocument();
@@ -91,6 +98,47 @@ void InspectorPerformanceAgent::ConsoleTimeStamp(const String& title) {
   std::unique_ptr<protocol::Array<protocol::Performance::Metric>> metrics;
   getMetrics(&metrics);
   GetFrontend()->metrics(std::move(metrics), title);
+}
+
+void InspectorPerformanceAgent::Will(const probe::CallFunction& probe) {
+  if (!script_call_depth_++)
+    probe.CaptureStartTime();
+}
+
+void InspectorPerformanceAgent::Did(const probe::CallFunction& probe) {
+  if (!--script_call_depth_)
+    script_duration_ += probe.Duration();
+}
+
+void InspectorPerformanceAgent::Will(const probe::ExecuteScript& probe) {
+  if (!script_call_depth_++)
+    probe.CaptureStartTime();
+}
+
+void InspectorPerformanceAgent::Did(const probe::ExecuteScript& probe) {
+  if (!--script_call_depth_)
+    script_duration_ += probe.Duration();
+}
+
+void InspectorPerformanceAgent::Will(const probe::RecalculateStyle& probe) {
+  probe.CaptureStartTime();
+}
+
+void InspectorPerformanceAgent::Did(const probe::RecalculateStyle& probe) {
+  recalc_style_duration_ += probe.Duration();
+  recalc_style_count_++;
+}
+
+void InspectorPerformanceAgent::Will(const probe::UpdateLayout& probe) {
+  if (!layout_depth_++)
+    probe.CaptureStartTime();
+}
+
+void InspectorPerformanceAgent::Did(const probe::UpdateLayout& probe) {
+  if (--layout_depth_)
+    return;
+  layout_duration_ += probe.Duration();
+  layout_count_++;
 }
 
 DEFINE_TRACE(InspectorPerformanceAgent) {
