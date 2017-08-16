6aa53d4bd1ba9e6d1f91d19ffbf09921f1f5cdf2
wutao <wutao@chromium.org>
2017-08-16 17:35:35 +0000

    Add a new flag to indicate layer_property_changed_from_property_trees.
    We would like to use render surface cache during layer animation. One
    changeling is that layer_property_changed_ in layer_impl.cc can be
    flagged through NoteLayerPropertyChanged while there is an animation.
    Adding a new flag layer_property_changed_from_property_trees_ can allow
    us to distinguish the sources of the layer property changes.
    
    Changes:
    1. Add the new flag layer_property_changed_from_property_trees_.
    2. Only set the new flag in LayerTreeImpl::MoveChangeTrackingToLayers.
    3. Add test expectations for new flag.
    
    Bug: 755351
    Test: with caching render surface and test new flag in cc_unittests.
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ie081acae9d7ca88aba56826cc3089ea78d1003cf
    Reviewed-on: https://chromium-review.googlesource.com/615023
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494842}

--- cc/layers/layer_impl.cc
+++ cc/layers/layer_impl.cc
@@ -57,7 +57,8 @@ LayerImpl::LayerImpl(LayerTreeImpl* tree_impl, int id)
           MainThreadScrollingReason::kNotScrollingOnMain),
       scrollable_(false),
       should_flatten_transform_from_property_tree_(false),
-      layer_property_changed_(false),
+      layer_property_changed_not_from_property_trees_(false),
+      layer_property_changed_from_property_trees_(false),
       may_contain_video_(false),
       masks_to_bounds_(false),
       contents_opaque_(false),
@@ -332,10 +333,13 @@ void LayerImpl::PushPropertiesTo(LayerImpl* layer) {
   if (needs_show_scrollbars_)
     layer->needs_show_scrollbars_ = needs_show_scrollbars_;
 
-  if (layer_property_changed_) {
+  if (layer_property_changed_not_from_property_trees_ ||
+      layer_property_changed_from_property_trees_)
     layer->layer_tree_impl()->set_needs_update_draw_properties();
-    layer->layer_property_changed_ = true;
-  }
+  if (layer_property_changed_not_from_property_trees_)
+    layer->layer_property_changed_not_from_property_trees_ = true;
+  if (layer_property_changed_from_property_trees_)
+    layer->layer_property_changed_from_property_trees_ = true;
 
   layer->SetBounds(bounds_);
   if (scrollable_)
@@ -354,7 +358,8 @@ void LayerImpl::PushPropertiesTo(LayerImpl* layer) {
 
   // Reset any state that should be cleared for the next update.
   needs_show_scrollbars_ = false;
-  layer_property_changed_ = false;
+  layer_property_changed_not_from_property_trees_ = false;
+  layer_property_changed_from_property_trees_ = false;
   needs_push_properties_ = false;
   update_rect_ = gfx::Rect();
   layer_tree_impl()->RemoveLayerShouldPushProperties(this);
@@ -420,7 +425,13 @@ std::unique_ptr<base::DictionaryValue> LayerImpl::LayerTreeAsJson() {
 }
 
 bool LayerImpl::LayerPropertyChanged() const {
-  if (layer_property_changed_ || GetPropertyTrees()->full_tree_damaged)
+  return layer_property_changed_not_from_property_trees_ ||
+         LayerPropertyChangedFromPropertyTrees();
+}
+
+bool LayerImpl::LayerPropertyChangedFromPropertyTrees() const {
+  if (layer_property_changed_from_property_trees_ ||
+      GetPropertyTrees()->full_tree_damaged)
     return true;
   if (transform_tree_index() == TransformTree::kInvalidNodeId)
     return false;
@@ -437,11 +448,17 @@ bool LayerImpl::LayerPropertyChanged() const {
 }
 
 bool LayerImpl::LayerPropertyChangedNotFromPropertyTrees() const {
-  return layer_property_changed_;
+  return layer_property_changed_not_from_property_trees_;
 }
 
 void LayerImpl::NoteLayerPropertyChanged() {
-  layer_property_changed_ = true;
+  layer_property_changed_not_from_property_trees_ = true;
+  layer_tree_impl()->set_needs_update_draw_properties();
+  SetNeedsPushProperties();
+}
+
+void LayerImpl::NoteLayerPropertyChangedFromPropertyTrees() {
+  layer_property_changed_from_property_trees_ = true;
   layer_tree_impl()->set_needs_update_draw_properties();
   SetNeedsPushProperties();
 }
@@ -460,7 +477,8 @@ const char* LayerImpl::LayerTypeAsString() const {
 }
 
 void LayerImpl::ResetChangeTracking() {
-  layer_property_changed_ = false;
+  layer_property_changed_not_from_property_trees_ = false;
+  layer_property_changed_from_property_trees_ = false;
   needs_push_properties_ = false;
 
   update_rect_.SetRect(0, 0, 0, 0);
--- cc/layers/layer_unittest.cc
+++ cc/layers/layer_unittest.cc
@@ -995,6 +995,8 @@ TEST_F(LayerTest, PushPropertiesCausesLayerPropertyChangedForTransform) {
   test_layer->PushPropertiesTo(impl_layer.get());
 
   EXPECT_TRUE(impl_layer->LayerPropertyChanged());
+  EXPECT_FALSE(impl_layer->LayerPropertyChangedFromPropertyTrees());
+  EXPECT_TRUE(impl_layer->LayerPropertyChangedNotFromPropertyTrees());
 }
 
 TEST_F(LayerTest, PushPropertiesCausesLayerPropertyChangedForOpacity) {
@@ -1012,6 +1014,8 @@ TEST_F(LayerTest, PushPropertiesCausesLayerPropertyChangedForOpacity) {
   test_layer->PushPropertiesTo(impl_layer.get());
 
   EXPECT_TRUE(impl_layer->LayerPropertyChanged());
+  EXPECT_FALSE(impl_layer->LayerPropertyChangedFromPropertyTrees());
+  EXPECT_TRUE(impl_layer->LayerPropertyChangedNotFromPropertyTrees());
 }
 
 TEST_F(LayerTest, MaskHasParent) {
