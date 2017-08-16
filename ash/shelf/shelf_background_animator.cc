69150f1af53d64390d7b7b1f08df66596fb7e517
wutao <wutao@chromium.org>
2017-08-16 19:23:08 +0000

    Add a new shelf background type when fullscreen app list is visible.
    Currently the status area is displayed with semi-transparent background
    in a transparent shelf container. When the launcher background color is
    themed as the shelf containers, the status area background will
    disappear and we will not be able to see the touch targets. We are
    adding one more shelf background type when the fullscreen app list is
    visible: to display the status area with the separators and no
    background.
    
    Changes:
    1. New shelf background type: SHELF_BACKGROUND_APP_LIST.
    2. When there is a launcher, the shelf will use the new background type.
    
    Bug: 748555
    Test: with flags enable-fullscreen-app-list && enable-background-blur
    Change-Id: Idda3757398662ed8300ac89a6c6559c5da76a861
    Reviewed-on: https://chromium-review.googlesource.com/614398
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494896}

--- ash/shelf/shelf_background_animator.cc
+++ ash/shelf/shelf_background_animator.cc
@@ -202,9 +202,8 @@ void ShelfBackgroundAnimator::CreateAnimator(
 
   switch (background_type) {
     case SHELF_BACKGROUND_DEFAULT:
-      duration_ms = 500;
-      break;
     case SHELF_BACKGROUND_OVERLAP:
+    case SHELF_BACKGROUND_APP_LIST:
       duration_ms = 500;
       break;
     case SHELF_BACKGROUND_MAXIMIZED:
@@ -247,6 +246,10 @@ void ShelfBackgroundAnimator::GetTargetValues(
       target_shelf_color_alpha = kMaxAlpha;
       target_item_color_alpha = 0;
       break;
+    case SHELF_BACKGROUND_APP_LIST:
+      target_shelf_color_alpha = 0;
+      target_item_color_alpha = 0;
+      break;
   }
 
   SkColor target_color =
@@ -261,6 +264,7 @@ void ShelfBackgroundAnimator::GetTargetValues(
     switch (background_type) {
       case SHELF_BACKGROUND_DEFAULT:
       case SHELF_BACKGROUND_OVERLAP:
+      case SHELF_BACKGROUND_APP_LIST:
         darkening_alpha = kShelfTranslucentColorDarkenAlpha;
         break;
       case SHELF_BACKGROUND_MAXIMIZED:
