8137530d64e4142340201c48b4608be16a355518
Paul Dyson <pdyson@chromium.org>
2017-08-16 01:02:07 +0000

    Record the time that dialog boxes are open.
    This histogram covers dialog boxes generated using the parent class
    DialogDelegate.
    
    Bug: 705331
    Change-Id: I325096d1ea76d8080c544121a2506da68c6af697
    Reviewed-on: https://chromium-review.googlesource.com/604637
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: Paul Dyson <pdyson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494647}

--- ui/views/window/dialog_delegate.h
+++ ui/views/window/dialog_delegate.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/strings/string16.h"
+#include "base/time/time.h"
 #include "ui/accessibility/ax_enums.h"
 #include "ui/base/models/dialog_model.h"
 #include "ui/base/ui_base_types.h"
@@ -131,6 +132,11 @@ class VIEWS_EXPORT DialogDelegate : public ui::DialogModel,
 
   // The margins between the content and the inside of the border.
   gfx::Insets margins_;
+
+  // The time the dialog is created.
+  base::TimeTicks creation_time_;
+
+  DISALLOW_COPY_AND_ASSIGN(DialogDelegate);
 };
 
 // A DialogDelegate implementation that is-a View. Used to override GetWidget()
