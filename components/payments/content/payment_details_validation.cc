778a0d69912c5a05e19572ae2e23f5eea5873c8b
Rouslan Solomakhin <rouslan@chromium.org>
2017-08-16 17:49:09 +0000

    [Payments] Split BCP-47 tag into language and script code.
    Before this patch, using an autofill profile with "ja-Latn" language tag
    on desktop would result in PaymentRequest.show() promise rejecting with
    "SyntaxError: 'ja-Latn' is not a valid BCP-47 language code, should be
    2-3 lower case letters [a-z]."
    
    This was happening because the BCP-47 language tag (e.g., "ja-Latn")
    from AutofillProfile was being written into the language code (e.g.,
    "ja") of PaymentAddress. The IPC validation logic in Blink recognized
    that "ja-Latn" is not a language code and aborted payment.
    
    This patch splits the AutofillProfile language tag (e.g., "ja-Latn")
    into its parts: language code (e.g., "ja") and script code (e.g.,
    "Latn"). These parts are then saved in PaymentAddress.language_code and
    PaymentAddress.script_code.
    
    After this patch, using an autofill profile with "ja-Latn" language tag
    on desktop will resolve the PaymentRequest.show() promise with the
    language tag correctly set in the shipping address returned to the
    webpage.
    
    Bug: 754587
    Change-Id: If34bd0962f58de95d05fe780391683c0ce1ee878
    Reviewed-on: https://chromium-review.googlesource.com/616200
    Reviewed-by: Anthony Vallee-Dubois <anthonyvd@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494848}

--- components/payments/content/payment_details_validation.cc
+++ components/payments/content/payment_details_validation.cc
@@ -41,13 +41,13 @@ bool validateShippingOptionOrPaymentItem(
     return false;
   }
 
-  if (!payments::PaymentsValidators::isValidCurrencyCodeFormat(
+  if (!payments::PaymentsValidators::IsValidCurrencyCodeFormat(
           item->amount->currency, item->amount->currency_system,
           error_message)) {
     return false;
   }
 
-  if (!payments::PaymentsValidators::isValidAmountFormat(item->amount->value,
+  if (!payments::PaymentsValidators::IsValidAmountFormat(item->amount->value,
                                                          error_message)) {
     return false;
   }
@@ -161,7 +161,7 @@ bool validatePaymentDetails(const mojom::PaymentDetailsPtr& details,
                                          error_message))
       return false;
   }
-  if (!PaymentsValidators::isValidErrorMsgFormat(details->error, error_message))
+  if (!PaymentsValidators::IsValidErrorMsgFormat(details->error, error_message))
     return false;
   return true;
 }
