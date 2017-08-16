6f7efea3cadc0f40d7fba3065b960a77d1acc505
Xiaocheng Hu <xiaochengh@chromium.org>
2017-08-16 20:18:44 +0000

    Stop ::first-letter from breaking surrogate pairs
    This patch makes FirstLetterPseudoElement::FirstLetterLength() consider
    surrogate pairs, to stop surrogate pairs broken between first letter and
    remaining text.
    
    Bug: 723848
    Change-Id: I62753b82d324c163ede45d96a9a4e7c1a3601e45
    Reviewed-on: https://chromium-review.googlesource.com/616462
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494921}

--- third_party/WebKit/Source/core/dom/FirstLetterPseudoElement.h
+++ third_party/WebKit/Source/core/dom/FirstLetterPseudoElement.h
@@ -36,7 +36,7 @@ class Element;
 class LayoutObject;
 class LayoutTextFragment;
 
-class FirstLetterPseudoElement final : public PseudoElement {
+class CORE_EXPORT FirstLetterPseudoElement final : public PseudoElement {
   WTF_MAKE_NONCOPYABLE(FirstLetterPseudoElement);
 
  public:
