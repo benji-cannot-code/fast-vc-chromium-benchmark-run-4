1140768db6f2e2c2ec5d72b287b1eb8643f59695
Kai Ninomiya <kainino@chromium.org>
2017-08-16 00:16:03 +0000

    DCHECKs that I should have added in 821895efec24
    Bug: 696126
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I49d33ad551d77b8eeaccfef5483f2ef4ca05dbfd
    Reviewed-on: https://chromium-review.googlesource.com/614692
    Commit-Queue: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494624}

--- gpu/command_buffer/service/framebuffer_manager.cc
+++ gpu/command_buffer/service/framebuffer_manager.cc
@@ -987,7 +987,7 @@ void Framebuffer::DoUnbindGLAttachmentsForWorkaround(GLenum target) {
 
 void Framebuffer::AttachRenderbuffer(
     GLenum attachment, Renderbuffer* renderbuffer) {
-  DCHECK(attachment != GL_DEPTH_STENCIL_ATTACHMENT);
+  DCHECK_NE(static_cast<GLenum>(GL_DEPTH_STENCIL_ATTACHMENT), attachment);
   const Attachment* a = GetAttachment(attachment);
   if (a)
     a->DetachFromFramebuffer(this, attachment);
@@ -1004,7 +1004,7 @@ void Framebuffer::AttachRenderbuffer(
 void Framebuffer::AttachTexture(
     GLenum attachment, TextureRef* texture_ref, GLenum target,
     GLint level, GLsizei samples) {
-  DCHECK(attachment != GL_DEPTH_STENCIL_ATTACHMENT);
+  DCHECK_NE(static_cast<GLenum>(GL_DEPTH_STENCIL_ATTACHMENT), attachment);
   const Attachment* a = GetAttachment(attachment);
   if (a)
     a->DetachFromFramebuffer(this, attachment);
@@ -1021,7 +1021,7 @@ void Framebuffer::AttachTexture(
 void Framebuffer::AttachTextureLayer(
     GLenum attachment, TextureRef* texture_ref, GLenum target,
     GLint level, GLint layer) {
-  DCHECK(attachment != GL_DEPTH_STENCIL_ATTACHMENT);
+  DCHECK_NE(static_cast<GLenum>(GL_DEPTH_STENCIL_ATTACHMENT), attachment);
   const Attachment* a = GetAttachment(attachment);
   if (a)
     a->DetachFromFramebuffer(this, attachment);
