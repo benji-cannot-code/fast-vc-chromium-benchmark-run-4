c3216691a4f221fb04d557a63d7191a1564d9fc9
Darren Shen <shend@chromium.org>
2017-08-16 23:07:59 +0000

    [CSSParser] Allow parsers with observers to use streams.
    Previously, we have CSSParserObservers which receive information about
    the parsing process (e.g. where a particular rule starts or ends). The
    way it worked for stylesheet parsing was that we first tokenized the
    entirety of the input, storing token information in a vector. Then,
    as we parsed, we queried the tokens for their location in the input
    string (their offset) using the vector.
    
    To get the same behaviour with streams, it's more difficult. Before,
    offset information were available prior to parsing. Now, offset
    information is only available as we parse. This requires some
    refactoring to work.
    
    In the meantime, we would like parsers with observers to still use
    streams. Hence, this patch implements a workaround: when we have
    observers, we tokenize everything before parsing as we did before.
    We then create a stream over that tokenizer (technically no longer
    a stream since everything's already tokenized) and use the same
    parsing code path. Any time we call into the observer with a range,
    we just convert the stream back into a range.
    
    This means that until we implement proper streaming with observers,
    parsing with observers will tokenize up front, whereas parsing
    without observers will tokenize on demand (mostly).
    
    For this to work, we also have to relax the requirement that
    streams must use 'fresh' tokenizers with no consumed tokens,
    since streams with observers requires using tokenizers that have
    already consumed the entirety of the input. Turns out this doesn't
    require any changes other than removing a DCHECK.
    
    For reviewers:
    - We add back the observer code that was in ConsumeDeclarationList
      but was deleted because observers didn't accept streams before.
    
    Bug: 661854
    Change-Id: Ifa53b2e7f2622edb371c74260fd24287d0fd4f89
    Reviewed-on: https://chromium-review.googlesource.com/599467
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Reviewed-by: Bugs Nash <bugsnash@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494989}

--- third_party/WebKit/Source/core/css/parser/CSSParserObserverWrapper.h
+++ third_party/WebKit/Source/core/css/parser/CSSParserObserverWrapper.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CSSParserObserverWrapper_h
 
 #include "core/css/parser/CSSParserObserver.h"
+#include "core/css/parser/CSSParserTokenStream.h"
 #include "platform/wtf/Allocator.h"
 
 namespace blink {
@@ -14,6 +15,8 @@ namespace blink {
 class CSSParserToken;
 class CSSParserTokenRange;
 
+// TODO(shend): Remove this class once we can use stream parsing directly
+// with CSSParserObserver.
 class CSSParserObserverWrapper {
   STACK_ALLOCATED();
 
@@ -28,6 +31,25 @@ class CSSParserObserverWrapper {
   void SkipCommentsBefore(const CSSParserTokenRange&,
                           bool leave_directly_before);
   void YieldCommentsBefore(const CSSParserTokenRange&);
+
+  // Overloads that work with streams
+  unsigned StartOffset(CSSParserTokenStream& stream) {
+    return StartOffset(stream.MakeRangeToEOF());
+  }
+  unsigned PreviousTokenStartOffset(CSSParserTokenStream& stream) {
+    return PreviousTokenStartOffset(stream.MakeRangeToEOF());
+  }
+  unsigned EndOffset(CSSParserTokenStream& stream) {
+    return EndOffset(stream.MakeRangeToEOF());
+  }
+
+  void SkipCommentsBefore(CSSParserTokenStream& stream,
+                          bool leave_directly_before) {
+    return SkipCommentsBefore(stream.MakeRangeToEOF(), leave_directly_before);
+  }
+  void YieldCommentsBefore(CSSParserTokenStream& stream) {
+    return YieldCommentsBefore(stream.MakeRangeToEOF());
+  }
 
   CSSParserObserver& Observer() { return observer_; }
   void AddComment(unsigned start_offset,
