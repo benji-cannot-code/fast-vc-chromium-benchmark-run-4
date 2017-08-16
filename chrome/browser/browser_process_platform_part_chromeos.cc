b3e6221a3e86153ef358a02becb807da56df6e0f
Antonio Gomes <tonikitoo@igalia.com>
2017-08-16 22:29:34 +0000

    Factor Services related logic out of BrowserProcessPlatforPart{chromeos}
    CL introduces a EmbeddedUIServiceInfoFactory factory, and factors the UI
    service logic in BrowserProcessPlatforPart{chromeos}::RegisterInProcessServices
    into.
    
    For ChromeOS there is no behavior change (both --mash and --mus continue
    with the same process/service model), but this refactor allows platforms
    other than ChromeOS (e.g. Ozone+Mus+Linux) more easily benefit from the
    same in-process UI as chromeos/mus.
    
    TEST=None
    BUG=722527
    
    Change-Id: Ic7a24778b3c57cc0c8620b83bcf555a5a98f5bf8
    Reviewed-on: https://chromium-review.googlesource.com/614522
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494980}

--- chrome/browser/browser_process_platform_part_chromeos.cc
+++ chrome/browser/browser_process_platform_part_chromeos.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/system/system_clock.h"
 #include "chrome/browser/chromeos/system/timezone_resolver_manager.h"
 #include "chrome/browser/chromeos/system/timezone_util.h"
+#include "chrome/browser/embedded_ui_service_info_factory.h"
 #include "chrome/browser/lifetime/keep_alive_types.h"
 #include "chrome/browser/lifetime/scoped_keep_alive.h"
 #include "chrome/browser/ui/ash/ash_util.h"
@@ -35,14 +36,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/timezone/timezone_resolver.h"
 #include "components/session_manager/core/session_manager.h"
 #include "components/user_manager/user_manager.h"
-#include "content/public/browser/discardable_shared_memory_manager.h"
 #include "services/preferences/public/interfaces/preferences.mojom.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
 #include "services/service_manager/public/cpp/service.h"
 #include "services/ui/common/image_cursors_set.h"
-#include "services/ui/public/interfaces/constants.mojom.h"
-#include "services/ui/service.h"
 
 #if defined(USE_OZONE)
 #include "content/public/common/service_manager_connection.h"
@@ -51,21 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/ui/public/cpp/input_devices/input_device_controller_client.h"
 #include "services/ui/public/interfaces/constants.mojom.h"
 #endif
-
-namespace {
-
-std::unique_ptr<service_manager::Service> CreateEmbeddedUIService(
-    const scoped_refptr<base::SingleThreadTaskRunner>& task_runner,
-    base::WeakPtr<ui::ImageCursorsSet> image_cursors_set_weak_ptr,
-    discardable_memory::DiscardableSharedMemoryManager* memory_manager) {
-  ui::Service::InProcessConfig config;
-  config.resource_runner = task_runner;
-  config.image_cursors_set_weak_ptr = image_cursors_set_weak_ptr;
-  config.memory_manager = memory_manager;
-  return base::MakeUnique<ui::Service>(&config);
-}
-
-}  // namespace
 
 BrowserProcessPlatformPart::BrowserProcessPlatformPart()
     : created_profile_helper_(false) {}
@@ -206,15 +189,9 @@ void BrowserProcessPlatformPart::RegisterInProcessServices(
   }
 
   if (chromeos::GetAshConfig() == ash::Config::MUS) {
-    service_manager::EmbeddedServiceInfo info;
     image_cursors_set_ = base::MakeUnique<ui::ImageCursorsSet>();
-    info.factory = base::Bind(&CreateEmbeddedUIService,
-                              base::ThreadTaskRunnerHandle::Get(),
-                              image_cursors_set_->GetWeakPtr(),
-                              content::GetDiscardableSharedMemoryManager());
-    info.use_own_thread = true;
-    info.message_loop_type = base::MessageLoop::TYPE_UI;
-    info.thread_priority = base::ThreadPriority::DISPLAY;
+    service_manager::EmbeddedServiceInfo info =
+        CreateEmbeddedUIServiceInfo(image_cursors_set_->GetWeakPtr());
     services->insert(std::make_pair(ui::mojom::kServiceName, info));
   }
 }
