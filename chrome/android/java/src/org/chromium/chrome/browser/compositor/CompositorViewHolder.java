5757fcde8493979fbb2564c702bee3dad81a503d
Piotr Swigon <piotrs@chromium.org>
2017-08-16 00:58:42 +0000

    [Installed PWA] Waits for a frame swap before hiding a splash screen.
    This patch fixes the flash of white between the splash screen and the
    web content in installed web apps. This is achieved with waiting for
    a compositor frame swap before starting splash screen hiding animation.
    
    A generic mechanism is introduced as we might need it in different
    places as well. E.g. In CustomTabActivity the same issue is solved with
    a time delay, which I plan to change to this mechanism in a follow up
    patch.
    
    Existing tests for a webapp splash screen cover showing/hiding it in
    various circumstances. Writing a specific test to assert flash of white
    is fixed would be more work than it's worth it.
    
    Bug: 734500
    Change-Id: Icc6c8c816179310e17bebf464de56d03721dd44e
    Reviewed-on: https://chromium-review.googlesource.com/580014
    Commit-Queue: Piotr Swigon <piotrs@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494645}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/CompositorViewHolder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/CompositorViewHolder.java
@@ -421,7 +421,7 @@ public class CompositorViewHolder extends FrameLayout
     /**
      * @return The SurfaceView proxy used by the Compositor.
      */
-    public View getCompositorView() {
+    public CompositorView getCompositorView() {
         return mCompositorView;
     }
 
