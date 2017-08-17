c995d3ccb980ac8890e8b946e89018c78fd18005
Erik Chen <erikchen@chromium.org>
2017-08-17 00:58:31 +0000

    Wrap MemlogSenderPipe in a lock.
    ::WriteFile() is not thread safe when called on different threads simultaneously with the same handle. This fixes non-deterministic crashes on Windows.
    
    Bug: 751321
    Change-Id: Ie0ca25b29ec7700ac9137908a675c84df9832255
    Reviewed-on: https://chromium-review.googlesource.com/617819
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495021}

--- chrome/common/profiling/memlog_sender_pipe_win.h
+++ chrome/common/profiling/memlog_sender_pipe_win.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/platform_file.h"
 #include "base/macros.h"
 #include "base/strings/string16.h"
+#include "base/synchronization/lock.h"
 
 namespace profiling {
 
@@ -25,6 +26,7 @@ class MemlogSenderPipe {
 
  private:
   base::ScopedPlatformFile file_;
+  base::Lock lock_;
 
   DISALLOW_COPY_AND_ASSIGN(MemlogSenderPipe);
 };
