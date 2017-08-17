726dd9113408cf44ee6e30c7a31cc74bf6c72804
Ben Chan <benchan@chromium.org>
2017-08-17 18:47:49 +0000

    Treat Cellular.SIMLockStatus.RetriesLeft property as int32.
    shill has changed the Cellular.SIMLockStatus.RetriesLeft property from
    an uint32 value to an int32 value (CL:615729). This CL updates Chrome
    code to treat the property as int32.
    
    BUG=753470
    TEST=Manually perform SIM unlock from the Chrome settings UI and observe
    the Cellular.SIMLockStatus property reported in chrome://network.
    
    Change-Id: Id490886f56df146805aa23d2965a7a1293601b36
    Reviewed-on: https://chromium-review.googlesource.com/615758
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Ben Chan <benchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495254}

--- chromeos/network/device_state.h
+++ chromeos/network/device_state.h
@@ -46,7 +46,7 @@ class CHROMEOS_EXPORT DeviceState : public ManagedState {
   const std::string& technology_family() const { return technology_family_; }
   const std::string& carrier() const { return carrier_; }
   const std::string& sim_lock_type() const { return sim_lock_type_; }
-  uint32_t sim_retries_left() const { return sim_retries_left_; }
+  int sim_retries_left() const { return sim_retries_left_; }
   const std::string& meid() const { return meid_; }
   const std::string& imei() const { return imei_; }
   const std::string& iccid() const { return iccid_; }
@@ -84,7 +84,7 @@ class CHROMEOS_EXPORT DeviceState : public ManagedState {
   std::string technology_family_;
   std::string carrier_;
   std::string sim_lock_type_;
-  uint32_t sim_retries_left_;
+  int sim_retries_left_;
   bool sim_present_;
   std::string meid_;
   std::string imei_;
