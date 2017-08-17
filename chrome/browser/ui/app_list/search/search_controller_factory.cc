5c5cd30af2ff5b4d4c2b2989e6e9daf73d44e377
Alex Newcomer <newcomer@chromium.org>
2017-08-17 18:38:10 +0000

    cros:Peeking Launcher:webstore search
    Include the header file instead of the cc file in search_controller_factory.cc.
    
    Bug: 756484
    Change-Id: I11fee5ca367165b622f186971e3040161d0faef3
    Reviewed-on: https://chromium-review.googlesource.com/619269
    Reviewed-by: Elijah Taylor <elijahtaylor@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495244}

--- chrome/browser/ui/app_list/search/search_controller_factory.cc
+++ chrome/browser/ui/app_list/search/search_controller_factory.cc
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/app_list/search/suggestions/suggestions_search_provider.h"
 #include "chrome/browser/ui/app_list/search/webstore/webstore_provider.h"
 #include "chrome/common/chrome_switches.h"
-#include "components/arc/arc_util.cc"
+#include "components/arc/arc_util.h"
 #include "ui/app_list/app_list_features.h"
 #include "ui/app_list/app_list_model.h"
 #include "ui/app_list/app_list_switches.h"
