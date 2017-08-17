9849599e13c60b339978ebc03f455957045740cc
John Rummell <jrummell@chromium.org>
2017-08-17 21:52:51 +0000

    Add UMA result for failed EME session creation
    MediaKeySession.generateRequest() and MediaKeySession.load() internally use
    promises that return the result as a string (session_id). Validation of the
    returned |session_id| may result in generateRequest() failing, or in false
    being returned by load(), indicating that the session to be loaded can't be
    found. Current UMA logging of the result always reports SUCCESS. Add
    additional values to the UMA logged to indicate SESSION_NOT_FOUND or
    SESSION_ALREADY_EXISTS.
    
    BUG=756109
    TEST=tested locally
    
    Change-Id: Ic8134efcbc95f2fd468dfe73439a6f1d5d6ababa
    Reviewed-on: https://chromium-review.googlesource.com/619408
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495334}

--- media/blink/cdm_result_promise_helper.h
+++ media/blink/cdm_result_promise_helper.h
@@ -29,6 +29,8 @@ enum CdmResultForUMA {
   // UNKNOWN_ERROR = 5,  // Deprecated.
   // CLIENT_ERROR = 6,   // Deprecated.
   // OUTPUT_ERROR = 7,   // Deprecated.
+  SESSION_NOT_FOUND = 8,
+  SESSION_ALREADY_EXISTS = 9,
   NUM_RESULT_CODES  // Must be last.
 };
 
