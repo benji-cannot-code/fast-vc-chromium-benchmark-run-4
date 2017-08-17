49592dd62bd3cdb8149a315db441e6939ca9007f
bsheedy <bsheedy@chromium.org>
2017-08-17 16:48:54 +0000

    Reland "Move some VR Telemetry setup into shared state"
    This is a reland of 550f26061a53fd69ada8fafc13170dff06e5e544
    Original change's description:
    > Move some VR Telemetry setup into shared state
    > 
    > Moves the VrCore installation and setup out of run_benchmark
    > and into the shared state class used by VR benchmarks. Has some
    > ugly temporary hacks to make presubmit happy that will be removed
    > in a follow-up CL that moves the remaining of setup code out of
    > run_benchmark, allowing us to remove the file completely.
    > 
    > This CL relies on the following Catapult CL being submitted
    > beforehand: https://codereview.chromium.org/2995713002/
    > 
    > Bug: 726906
    > Change-Id: Ib5969b1657aa11b9b640ea4f7420ff746991f0c1
    > Reviewed-on: https://chromium-review.googlesource.com/608984
    > Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    > Reviewed-by: Ned Nguyen <nednguyen@google.com>
    > Reviewed-by: John Budorick <jbudorick@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#494603}
    
    Bug: 726906
    Change-Id: I2ec5475b0e04023fb50ff8cc3d17bd77a8a07f37
    Reviewed-on: https://chromium-review.googlesource.com/617820
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495196}

--- tools/perf/contrib/vr_benchmarks/vr_page_sets/webvr_sample_pages.py
+++ tools/perf/contrib/vr_benchmarks/vr_page_sets/webvr_sample_pages.py
@@ -7,6 +7,13 @@ from telemetry import story
 
 import os
 
+# TODO(bsheedy): Remove the try/except once the VR-specific run_benchmark
+# is replaced with the regular run_benchmark
+try:
+  from vr_page_sets import shared_android_vr_page_state as vr_state
+except ImportError:
+  from contrib.vr_benchmarks.vr_page_sets import (
+      shared_android_vr_page_state as vr_state)
 
 SAMPLE_DIR = os.path.join(os.path.dirname(__file__),
                           '..', '..', '..', '..', '..',
@@ -21,8 +28,9 @@ class WebVrSamplePage(page_module.Page):
       url += '?' + '&'.join(get_parameters)
     name = url.replace('.html', '')
     url = 'file://' + os.path.join(SAMPLE_DIR, url)
-    super(WebVrSamplePage, self).__init__(url=url, page_set=page_set,
-                                          name=name)
+    super(WebVrSamplePage, self).__init__(
+        url=url, page_set=page_set, name=name,
+        shared_page_state_class=vr_state.SharedAndroidVrPageState)
 
   def RunPageInteractions(self, action_runner):
       action_runner.TapElement(selector='canvas[id="webgl-canvas"]')
@@ -34,6 +42,7 @@ class WebVrSamplePageSet(story.StorySet):
 
   def __init__(self):
     super(WebVrSamplePageSet, self).__init__()
+
     # Standard sample app with no changes
     self.AddStory(WebVrSamplePage(['canvasClickPresents=1',
                                    'renderScale=1'], self))
