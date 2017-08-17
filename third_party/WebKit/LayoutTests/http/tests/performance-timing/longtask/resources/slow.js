210eca126fdb9ad4d1a6678d90cc0faf1b0c664a
Nicolas Pena <npm@chromium.org>
2017-08-17 01:02:40 +0000

    Fix most external/wpt/longtask-timing tests
    This CL fixes all external/wpt/longtask-timing tests except for those
    with cross-origin iframes, which may pass or timeout. It also fixes
    some nits like using 'const' instead of 'var' and single quotes in js.
    Finally, it also removes a folder containing duplicate tests:
    http/tests/performance-timing/longtask
    
    Bug: chromium:754819
    Change-Id: Ib1931ba47522d9d3c64f519d781d080ba2b62b91
    Reviewed-on: https://chromium-review.googlesource.com/614095
    Reviewed-by: Shubhie Panicker <panicker@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495025}

--- third_party/WebKit/LayoutTests/http/tests/performance-timing/longtask/resources/slow.js
+++
@@ -1,5 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-
-/* Generate a slow task */
-var begin = window.performance.now();
-while (window.performance.now() < begin + 51);
