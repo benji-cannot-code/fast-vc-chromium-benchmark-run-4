2d25b9a94a194b18874027db0e205136566d2fe2
spqchan <spqchan@chromium.org>
2017-08-17 17:11:31 +0000

    Feature Flag for Dialog Touch Bar Support
    Dialog touch bar support is currently behind
    the Browser Touch Bar flag. This needs to be
    separated into a separate flag that's disabled
    by default.
    
    This is an attempt to reland
    https://chromium-review.googlesource.com/c/616341
    which had caused several tests to failed. The test
    failures are addressed in this CL
    
    Bug: 755814
    Change-Id: I458527a859527b8ebcccca5b12ca9f55b4d39b2c
    Reviewed-on: https://chromium-review.googlesource.com/617386
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Sarah Chan <spqchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495203}

--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -60,6 +60,7 @@ extern const base::Feature kBrowserHangFixesExperiment;
 
 #if defined(OS_MACOSX)
 extern const base::Feature kBrowserTouchBar;
+extern const base::Feature kDialogTouchBar;
 extern const base::Feature kTabStripKeyboardFocus;
 #endif  // defined(OS_MACOSX)
 
