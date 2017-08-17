26d5bfcf11b7c6fb145113b50d3249fc998c6c03
Muyuan Li <muyuanli@chromium.org>
2017-08-17 20:05:57 +0000

    Reland: fix window bounds transformation from dip to pixel.
    Previous transformation only considers device scale
    factor but does not handle user-defined scale factor
    with ctrl + +/=.
    
    Reland https://chromium-review.googlesource.com/c/608875
    reverted by https://chromium-review.googlesource.com/c/618426.
    
    
    BUG=b:64029031
    TEST=None
    
    Change-Id: Id550d81c3d97794a1a49dfc3f477ede57c5f7303
    Reviewed-on: https://chromium-review.googlesource.com/618287
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Muyuan Li <muyuanli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495294}

--- ash/highlighter/highlighter_controller.cc
+++ ash/highlighter/highlighter_controller.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/highlighter/highlighter_result_view.h"
 #include "ash/highlighter/highlighter_selection_observer.h"
 #include "ash/highlighter/highlighter_view.h"
+#include "ash/public/cpp/scale_utility.h"
 #include "base/metrics/histogram_macros.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_tree_host.h"
@@ -40,12 +41,7 @@ gfx::RectF AdjustHorizontalStroke(const gfx::RectF& box,
 // of points separated by the distance of 1, and measuring the distance between
 // the transformed points.
 float GetScreenshotScale(aura::Window* window) {
-  const gfx::Transform transform = window->GetHost()->GetRootTransform();
-  gfx::Point3F p1(0, 0, 0);
-  gfx::Point3F p2(1, 0, 0);
-  transform.TransformPoint(&p1);
-  transform.TransformPoint(&p2);
-  return (p2 - p1).Length();
+  return GetScaleFactorForTransform(window->GetHost()->GetRootTransform());
 }
 
 }  // namespace
