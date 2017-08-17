24aa877ad3d34f8ff240b853ab287c779f421386
Michael McGreevy <mcgreevy@chromium.org>
2017-08-17 04:54:26 +0000

    Have TaskQueue::Reset update is_active.
    Bug: 754570
    Change-Id: Ia962bc418942b49e8bdc41c54478f65002a4317b
    Reviewed-on: https://chromium-review.googlesource.com/618205
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Michael McGreevy <mcgreevy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495078}

--- chrome/browser/installable/installable_manager.cc
+++ chrome/browser/installable/installable_manager.cc
@@ -330,8 +330,6 @@ void InstallableManager::Reset() {
   manifest_ = base::MakeUnique<ManifestProperty>();
   valid_manifest_ = base::MakeUnique<ValidManifestProperty>();
   worker_ = base::MakeUnique<ServiceWorkerProperty>();
-
-  task_queue_.is_active_ = false;
 }
 
 void InstallableManager::SetManifestDependentTasksComplete() {
@@ -639,6 +637,7 @@ void InstallableManager::TaskQueue::Insert(Task task) {
 void InstallableManager::TaskQueue::Reset() {
   tasks_.clear();
   paused_tasks_.clear();
+  is_active_ = false;
 }
 
 bool InstallableManager::TaskQueue::HasPaused() const {
