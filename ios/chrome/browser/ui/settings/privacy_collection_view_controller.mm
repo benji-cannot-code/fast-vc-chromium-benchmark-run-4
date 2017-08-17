d65b8230a8147445ee3f7550119206f572deaf98
gambard <gambard@chromium.org>
2017-08-17 11:33:11 +0000

    Merge omnibox and content suggestions settings
    The privacy settings controlling the suggestions displayed in the NTP
    and omnibox need to be merged in one.
    
    Bug: 755961
    Change-Id: I015d859a1ffa07baee7d0391a96a0ae8035587ae
    Reviewed-on: https://chromium-review.googlesource.com/616774
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495140}

--- ios/chrome/browser/ui/settings/privacy_collection_view_controller.mm
+++ ios/chrome/browser/ui/settings/privacy_collection_view_controller.mm
@@ -68,7 +68,6 @@ typedef NS_ENUM(NSInteger, ItemType) {
   ItemTypeWebServicesHeader,
   ItemTypeWebServicesFooter,
   ItemTypeWebServicesShowSuggestions,
-  ItemTypeWebServicesShowContentSuggestions,
   ItemTypeWebServicesSendUsageData,
   ItemTypeWebServicesDoNotTrack,
   ItemTypeWebServicesPhysicalWeb,
@@ -81,11 +80,8 @@ typedef NS_ENUM(NSInteger, ItemType) {
                                               PrefObserverDelegate> {
   ios::ChromeBrowserState* _browserState;  // weak
   PrefBackedBoolean* _suggestionsEnabled;
-  PrefBackedBoolean* _contentSuggestionsEnabled;
   // The item related to the switch for the show suggestions setting.
   CollectionViewSwitchItem* _showSuggestionsItem;
-  // The item related to the switch for the show content suggestions setting.
-  CollectionViewSwitchItem* _showContentSuggestionsItem;
 
   // Pref observer to track changes to prefs.
   std::unique_ptr<PrefObserverBridge> _prefObserverBridge;
@@ -101,7 +97,6 @@ typedef NS_ENUM(NSInteger, ItemType) {
 // Initialization methods for various model items.
 - (CollectionViewItem*)handoffDetailItem;
 - (CollectionViewSwitchItem*)showSuggestionsSwitchItem;
-- (CollectionViewSwitchItem*)showContentSuggestionsSwitchItem;
 - (CollectionViewItem*)showSuggestionsFooterItem;
 - (CollectionViewItem*)clearBrowsingDetailItem;
 - (CollectionViewItem*)sendUsageDetailItem;
@@ -128,10 +123,6 @@ typedef NS_ENUM(NSInteger, ItemType) {
         initWithPrefService:_browserState->GetPrefs()
                    prefName:prefs::kSearchSuggestEnabled];
     [_suggestionsEnabled setObserver:self];
-    _contentSuggestionsEnabled = [[PrefBackedBoolean alloc]
-        initWithPrefService:_browserState->GetPrefs()
-                   prefName:prefs::kContentSuggestionsRemoteEnabled];
-    [_contentSuggestionsEnabled setObserver:self];
 
     PrefService* prefService = _browserState->GetPrefs();
 
@@ -157,7 +148,6 @@ typedef NS_ENUM(NSInteger, ItemType) {
 
 - (void)dealloc {
   [_suggestionsEnabled setObserver:nil];
-  [_contentSuggestionsEnabled setObserver:nil];
 }
 
 #pragma mark - SettingsRootCollectionViewController
@@ -191,12 +181,6 @@ typedef NS_ENUM(NSInteger, ItemType) {
   _showSuggestionsItem = [self showSuggestionsSwitchItem];
   [model addItem:_showSuggestionsItem
       toSectionWithIdentifier:SectionIdentifierWebServices];
-
-  if (experimental_flags::IsSuggestionsUIEnabled()) {
-    _showContentSuggestionsItem = [self showContentSuggestionsSwitchItem];
-    [model addItem:_showContentSuggestionsItem
-        toSectionWithIdentifier:SectionIdentifierWebServices];
-  }
 
   [model addItem:[self sendUsageDetailItem]
       toSectionWithIdentifier:SectionIdentifierWebServices];
@@ -246,17 +230,6 @@ typedef NS_ENUM(NSInteger, ItemType) {
   showSuggestionsSwitchItem.on = [_suggestionsEnabled value];
 
   return showSuggestionsSwitchItem;
-}
-
-- (CollectionViewSwitchItem*)showContentSuggestionsSwitchItem {
-  CollectionViewSwitchItem* showContentSuggestionsSwitchItem =
-      [[CollectionViewSwitchItem alloc]
-          initWithType:ItemTypeWebServicesShowContentSuggestions];
-  showContentSuggestionsSwitchItem.text =
-      l10n_util::GetNSString(IDS_IOS_OPTIONS_CONTENT_SUGGESTIONS);
-  showContentSuggestionsSwitchItem.on = [_contentSuggestionsEnabled value];
-
-  return showContentSuggestionsSwitchItem;
 }
 
 - (CollectionViewItem*)showSuggestionsFooterItem {
@@ -341,12 +314,6 @@ typedef NS_ENUM(NSInteger, ItemType) {
         base::mac::ObjCCastStrict<CollectionViewSwitchCell>(cell);
     [switchCell.switchView addTarget:self
                               action:@selector(showSuggestionsToggled:)
-                    forControlEvents:UIControlEventValueChanged];
-  } else if (itemType == ItemTypeWebServicesShowContentSuggestions) {
-    CollectionViewSwitchCell* switchCell =
-        base::mac::ObjCCastStrict<CollectionViewSwitchCell>(cell);
-    [switchCell.switchView addTarget:self
-                              action:@selector(showContentSuggestionsToggled:)
                     forControlEvents:UIControlEventValueChanged];
   }
 
@@ -450,21 +417,13 @@ typedef NS_ENUM(NSInteger, ItemType) {
 #pragma mark - BooleanObserver
 
 - (void)booleanDidChange:(id<ObservableBoolean>)observableBoolean {
-  if (observableBoolean == _suggestionsEnabled) {
-    // Update the item.
-    _showSuggestionsItem.on = [_suggestionsEnabled value];
+  DCHECK_EQ(observableBoolean, _suggestionsEnabled);
 
-    // Update the cell.
-    [self reconfigureCellsForItems:@[ _showSuggestionsItem ]];
-  } else if (observableBoolean == _contentSuggestionsEnabled) {
-    // Update the item.
-    _showContentSuggestionsItem.on = [_contentSuggestionsEnabled value];
+  // Update the item.
+  _showSuggestionsItem.on = [_suggestionsEnabled value];
 
-    // Update the cell.
-    [self reconfigureCellsForItems:@[ _showContentSuggestionsItem ]];
-  } else {
-    NOTREACHED();
-  }
+  // Update the cell.
+  [self reconfigureCellsForItems:@[ _showSuggestionsItem ]];
 }
 
 #pragma mark - Actions
@@ -485,24 +444,6 @@ typedef NS_ENUM(NSInteger, ItemType) {
   BOOL isOn = switchCell.switchView.isOn;
   switchItem.on = isOn;
   [_suggestionsEnabled setValue:isOn];
-}
-
-- (void)showContentSuggestionsToggled:(UISwitch*)sender {
-  NSIndexPath* switchPath = [self.collectionViewModel
-      indexPathForItemType:ItemTypeWebServicesShowContentSuggestions
-         sectionIdentifier:SectionIdentifierWebServices];
-
-  CollectionViewSwitchItem* switchItem =
-      base::mac::ObjCCastStrict<CollectionViewSwitchItem>(
-          [self.collectionViewModel itemAtIndexPath:switchPath]);
-  CollectionViewSwitchCell* switchCell =
-      base::mac::ObjCCastStrict<CollectionViewSwitchCell>(
-          [self.collectionView cellForItemAtIndexPath:switchPath]);
-
-  DCHECK_EQ(switchCell.switchView, sender);
-  BOOL isOn = switchCell.switchView.isOn;
-  switchItem.on = isOn;
-  [_contentSuggestionsEnabled setValue:isOn];
 }
 
 #pragma mark - PrefObserverDelegate
