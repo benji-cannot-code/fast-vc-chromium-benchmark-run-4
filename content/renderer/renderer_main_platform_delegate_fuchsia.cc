a759f52226c23424663697325b4c90aeb2e1059c
Kevin Marshall <kmarshall@chromium.org>
2017-08-17 19:28:29 +0000

    Fuchsia: Disable portions of sandbox setup logic of content/renderer
    - Disable portions of the sandbox setup logic.
    - Introduce a RendererMainPlatformDelegate stub for Fuchsia,
      to which the sandbox implementation will be added.
    
    Bug: 750938
    Change-Id: Ib090b010a20e5cf1512c7f8a9f20548cd0d1e88d
    Reviewed-on: https://chromium-review.googlesource.com/611866
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495277}

---
+++ content/renderer/renderer_main_platform_delegate_fuchsia.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/renderer/renderer_main_platform_delegate.h"
+
+namespace content {
+
+RendererMainPlatformDelegate::RendererMainPlatformDelegate(
+    const MainFunctionParams& parameters) {}
+
+RendererMainPlatformDelegate::~RendererMainPlatformDelegate() {}
+
+void RendererMainPlatformDelegate::PlatformInitialize() {}
+
+void RendererMainPlatformDelegate::PlatformUninitialize() {}
+
+bool RendererMainPlatformDelegate::EnableSandbox() {
+  // TODO(fuchsia): Implement this for Fuchsia.
+  NOTIMPLEMENTED();
+  return false;
+}
+
+}  // namespace content
