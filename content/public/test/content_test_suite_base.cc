3dbc915620fa64c01a809c6d0a945554a9eae370
Elliot Glaysher <erg@chromium.org>
2017-08-17 18:18:44 +0000

    Constrict DEPS so //content/ can't add further dependencies on ozone.
    Under --mus and --mash, the code in //content/ can't access the raw
    ozone devices. It appears the most of the uses of ozone aren't used in
    mus builds anyway. Further constrain DEPS so new instances can't be
    added.
    
    Bug: 734668
    Change-Id: I5b8661aa4c000a58602ffce0e6f3e9cf35cff87e
    Reviewed-on: https://chromium-review.googlesource.com/617591
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Elliot Glaysher <erg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495228}

--- content/public/test/content_test_suite_base.cc
+++ content/public/test/content_test_suite_base.cc
@@ -32,11 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/android/compositor.h"
 #endif
 
-#if defined(USE_OZONE)
-#include "ui/ozone/public/ozone_platform.h"
-#endif
-
-
 namespace content {
 
 ContentTestSuiteBase::ContentTestSuiteBase(int argc, char** argv)
