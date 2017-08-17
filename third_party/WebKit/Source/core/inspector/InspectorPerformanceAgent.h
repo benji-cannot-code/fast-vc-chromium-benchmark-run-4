65dfc35189c11834fb02d8705e5b81ab841a043b
Alexei Filippov <alph@chromium.org>
2017-08-17 00:01:38 +0000

    DevTools: Support restoring InspectorPerformanceAgent state.
    BUG=751892
    
    Change-Id: Idc342d4811b067bb96e56233b032ad611e78ee33
    Reviewed-on: https://chromium-review.googlesource.com/617306
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495003}

--- third_party/WebKit/Source/core/inspector/InspectorPerformanceAgent.h
+++ third_party/WebKit/Source/core/inspector/InspectorPerformanceAgent.h
@@ -33,6 +33,8 @@ class CORE_EXPORT InspectorPerformanceAgent final
   }
   ~InspectorPerformanceAgent() override;
 
+  void Restore() override;
+
   // Performance protocol domain implementation.
   protocol::Response enable() override;
   protocol::Response disable() override;
