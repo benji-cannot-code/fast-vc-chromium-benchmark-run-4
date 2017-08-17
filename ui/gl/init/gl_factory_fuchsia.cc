bae11972fc093e18e4095c2fe5031a3a27086657
Kevin Marshall <kmarshall@chromium.org>
2017-08-17 19:21:16 +0000

    Fix miswording in a gl_factory_fuchsia.cc function name.
    This fixes a Fuchsia-specific build error in commit
    e6f7865e39088ac3c51b72d2ff0fc6dee60ca95f.
    
    NOTRY=true
    
    R: zmo@chromium.org
    Bug: 744658
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ie7215419e0266f91fd19759d199865d3c7449bed
    Reviewed-on: https://chromium-review.googlesource.com/619111
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495272}

--- ui/gl/init/gl_factory_fuchsia.cc
+++ ui/gl/init/gl_factory_fuchsia.cc
@@ -46,7 +46,7 @@ void SetDisabledExtensionsPlatform(const std::string& disabled_extensions) {
   NOTIMPLEMENTED();
 }
 
-bool InitializeExtensionSettingsPlatformOneOff() {
+bool InitializeExtensionSettingsOneOffPlatform() {
   NOTIMPLEMENTED();
   return false;
 }
