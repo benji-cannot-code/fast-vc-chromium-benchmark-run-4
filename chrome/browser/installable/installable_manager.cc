6de01f4aa8f4b6ba7b06d5db7f6c6df49625f2e6
Michael McGreevy <mcgreevy@chromium.org>
2017-08-17 02:24:29 +0000

    Have TaskQueue::{Next,PauseCurrent} update is_active_.
    This is a transitional refactoring step towards having all management of
    is_active_ done from within TaskQueue, and then removing this explicit state
    althogether.
    
    Bug: 754570
    Change-Id: Ia0dc8ee2179eb97757a22038b0a62b0b3c768e1f
    Reviewed-on: https://chromium-review.googlesource.com/616487
    Commit-Queue: Michael McGreevy <mcgreevy@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495053}

--- chrome/browser/installable/installable_manager.cc
+++ chrome/browser/installable/installable_manager.cc
@@ -389,11 +389,10 @@ void InstallableManager::WorkOnTask() {
     // again.
     if (worker_error() == NO_MATCHING_SERVICE_WORKER)
       worker_ = base::MakeUnique<ServiceWorkerProperty>();
+
     task_queue_.Next();
 
-    if (task_queue_.IsEmpty())
-      task_queue_.is_active_ = false;
-    else
+    if (!task_queue_.IsEmpty())
       StartNextTask();
 
     return;
@@ -523,9 +522,7 @@ void InstallableManager::OnDidCheckHasServiceWorker(
         params.wait_for_worker = false;
         OnWaitingForServiceWorker();
         task_queue_.PauseCurrent();
-        if (task_queue_.IsEmpty())
-          task_queue_.is_active_ = false;
-        else
+        if (!task_queue_.IsEmpty())
           StartNextTask();
 
         return;
@@ -668,6 +665,8 @@ void InstallableManager::TaskQueue::PauseCurrent() {
 void InstallableManager::TaskQueue::Next() {
   DCHECK(!tasks_.empty());
   tasks_.erase(tasks_.begin());
+  if (IsEmpty())
+    is_active_ = false;
 }
 
 bool InstallableManager::TaskQueue::IsEmpty() const {
