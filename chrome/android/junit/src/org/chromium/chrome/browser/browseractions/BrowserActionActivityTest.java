4b89b32e363d413b5f2cf6e2df18a0b1c98da900
bengr <bengr@chromium.org>
2017-08-17 11:36:37 -0700

    Revert of [Android] Open a tab in the background from Browser Actions if ChromeTabbedActivity is available (patchset #13 id:240001 of https://codereview.chromium.org/2931433004/ )
    Reason for revert:
    Breaks testOpenMultipleTabInBackgroundWhenChromeAvailable on tablets.
    
    Original issue's description:
    > [Android] Open a tab in the background from Browser Actions if ChromeTabbedActivity is available
    >
    > Implements the function that opens a tab for the given url in the
    > background of Chrome from Browser Actions menu. Also add a new
    > TabLaunchType (Browser Actions) which does not show transition and
    > always add to the top of the tab list. Besides, postpones operation
    > by showing a ProgressDialog to block UI until native libraries are
    > loaded.
    >
    > BUG=730305
    >
    > Review-Url: https://codereview.chromium.org/2931433004
    > Cr-Commit-Position: refs/heads/master@{#494316}
    > Committed: https://chromium.googlesource.com/chromium/src/+/fc32a59653e65cee968472d8c91fa060fdf981d9
    
    TBR=tedchoc@chromium.org,yusufo@chromium.org,ltian@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=730305,756509
    
    Review-Url: https://codereview.chromium.org/2997163002
    Cr-Commit-Position: refs/heads/master@{#495242}

--- chrome/android/junit/src/org/chromium/chrome/browser/browseractions/BrowserActionActivityTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/browseractions/BrowserActionActivityTest.java
@@ -7,8 +7,7 @@ package org.chromium.chrome.browser.browseractions;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doAnswer;
 
 import android.app.PendingIntent;
 import android.content.Context;
@@ -20,8 +19,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
@@ -29,21 +29,19 @@ import org.chromium.base.test.util.Feature;
 import org.chromium.testing.local.LocalRobolectricTestRunner;
 
 /**
- * Unit tests for BrowserActionsIntent.
+ * Unit tests for BrowserActionActivity.
  */
 @RunWith(LocalRobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
-public class BrowserActionsIntentTest {
+public class BrowserActionActivityTest {
     private static final String HTTP_SCHEME_TEST_URL = "http://www.example.com";
     private static final String HTTPS_SCHEME_TEST_URL = "https://www.example.com";
     private static final String CHROME_SCHEME_TEST_URL = "chrome://example";
     private static final String CONTENT_SCHEME_TEST_URL = "content://example";
-    private static final String SENDER_PACKAGE_NAME = "some.other.app.package.sender_name";
-    private static final String RECEIVER_PACKAGE_NAME = "some.other.app.package.receiver_name";
 
+    private BrowserActionActivity mActivity = new BrowserActionActivity();
     private Context mContext;
-    @Mock
-    private BrowserActionActivity mActivity;
+
     @Mock
     private PendingIntent mPendingIntent;
 
@@ -51,10 +49,13 @@ public class BrowserActionsIntentTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         mContext = RuntimeEnvironment.application;
-        mActivity = Mockito.mock(BrowserActionActivity.class);
-        when(mActivity.getPackageName()).thenReturn(RECEIVER_PACKAGE_NAME);
-        when(mActivity.isStartedUpCorrectly(any(Intent.class))).thenCallRealMethod();
-        when(mPendingIntent.getCreatorPackage()).thenReturn(SENDER_PACKAGE_NAME);
+        Answer<String> answer = new Answer<String>() {
+            @Override
+            public String answer(InvocationOnMock invocation) {
+                return "some.other.app.package.name";
+            }
+        };
+        doAnswer(answer).when(mPendingIntent).getCreatorPackage();
     }
 
     @Test
