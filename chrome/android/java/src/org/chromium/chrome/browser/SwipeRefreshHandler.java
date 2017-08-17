261c9cf46d2604ab556ddacd3452a144bd801de9
Matthew Jones <mdjones@chromium.org>
2017-08-17 23:51:02 +0000

    Check fullscreen manager before use in SwipeRefreshHandler
    Swipes-to-refresh can happen before the browser is initialized. This
    change adds a check to prevent crashes when the fullscreen manager is
    not available.
    
    BUG=756525
    
    Change-Id: I743df7fa19c46c47080169409122de50f14a1edd
    Reviewed-on: https://chromium-review.googlesource.com/619849
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495385}

--- chrome/android/java/src/org/chromium/chrome/browser/SwipeRefreshHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/SwipeRefreshHandler.java
@@ -121,7 +121,8 @@ public class SwipeRefreshHandler implements OverscrollRefreshHandler {
 
         // If the controls are at the bottom and hidden, allow cc to handle the scroll event to show
         // them.
-        if (manager.areBrowserControlsAtBottom() && manager.getBrowserControlHiddenRatio() > 0) {
+        if (manager != null && manager.areBrowserControlsAtBottom()
+                && manager.getBrowserControlHiddenRatio() > 0) {
             return false;
         }
 
