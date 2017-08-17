aee6250e13b6427c45417d7eac6f220ca642e33b
Alexandr Ilin <alexilin@chromium.org>
2017-08-17 17:13:28 +0000

    predictors: Fix flaky tests
    Some predictors tests create a ResourcePrefetchPredictor object without
    waiting for the initialization of the PredictorDatabase that is
    performed asynchronously. It causes errors in sqlite when the
    initialization is done after the test is finished.
    
    This CL adds content::RunAllBlockingPoolTasksUntilIdle() to ensure that
    the initialization is done before the test is finished.
    
    Bug: 
    Change-Id: I114aa30a54c9c46cc506feac2e6d80e19be8b18c
    Reviewed-on: https://chromium-review.googlesource.com/619087
    Commit-Queue: Alexandr Ilin <alexilin@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495204}

--- chrome/browser/page_load_metrics/observers/loading_predictor_page_load_metrics_observer_unittest.cc
+++ chrome/browser/page_load_metrics/observers/loading_predictor_page_load_metrics_observer_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/page_load_metrics/test/page_load_metrics_test_util.h"
 #include "chrome/test/base/testing_profile.h"
 #include "content/public/test/test_browser_thread_bundle.h"
+#include "content/public/test/test_utils.h"
 #include "testing/gmock/include/gmock/gmock.h"
 
 using predictors::LoadingDataCollector;
@@ -43,6 +44,10 @@ class LoadingPredictorPageLoadMetricsObserverTest
     predictor_ =
         base::MakeUnique<testing::StrictMock<MockResourcePrefetchPredictor>>(
             config, profile());
+    // The base class of MockResourcePrefetchPredictor constructs the
+    // PredictorDatabase for the profile. The PredictorDatabase is initialized
+    // asynchronously and we have to wait for the initialization completion.
+    content::RunAllBlockingPoolTasksUntilIdle();
     page_load_metrics::InitPageLoadTimingForTest(&timing_);
     collector_ = base::MakeUnique<LoadingDataCollector>(predictor_.get(),
                                                         nullptr, config);
