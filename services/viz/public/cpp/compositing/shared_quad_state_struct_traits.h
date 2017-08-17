9409268d9238adabe1bcaf436c86f10dbcbab7c4
yiyix <yiyix@chromium.org>
2017-08-17 20:46:50 +0000

    viz: Move SharedQuadState's mojom from cc/ipc to services/viz
    For more details:
    https://docs.google.com/spreadsheets/d/1gHvkSVehWvWvCyCa-V0tZezULNTpiW0T8tLE3DjVgkg
    
    Bug: 
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I6c9b824e0f7236d2b2b393dbb930aa31bb00d027
    Reviewed-on: https://chromium-review.googlesource.com/618467
    Commit-Queue: Yi Xu <yiyix@chromium.org>
    Reviewed-by: weiliangc <weiliangc@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495309}

--- services/viz/public/cpp/compositing/shared_quad_state_struct_traits.h
+++ services/viz/public/cpp/compositing/shared_quad_state_struct_traits.h
@@ -3,11 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CC_IPC_SHARED_QUAD_STATE_STRUCT_TRAITS_H_
-#define CC_IPC_SHARED_QUAD_STATE_STRUCT_TRAITS_H_
+#ifndef SERVICES_VIZ_PUBLIC_CPP_COMPOSITING_SHARED_QUAD_STATE_STRUCT_TRAITS_H_
+#define SERVICES_VIZ_PUBLIC_CPP_COMPOSITING_SHARED_QUAD_STATE_STRUCT_TRAITS_H_
 
-#include "cc/ipc/shared_quad_state.mojom-shared.h"
 #include "cc/quads/shared_quad_state.h"
+#include "services/viz/public/interfaces/compositing/shared_quad_state.mojom-shared.h"
 
 namespace mojo {
 
@@ -16,7 +16,7 @@ struct OptSharedQuadState {
 };
 
 template <>
-struct StructTraits<cc::mojom::SharedQuadStateDataView, OptSharedQuadState> {
+struct StructTraits<viz::mojom::SharedQuadStateDataView, OptSharedQuadState> {
   static bool IsNull(const OptSharedQuadState& input) { return !input.sqs; }
 
   static void SetToNull(OptSharedQuadState* output) { output->sqs = nullptr; }
@@ -57,7 +57,7 @@ struct StructTraits<cc::mojom::SharedQuadStateDataView, OptSharedQuadState> {
 };
 
 template <>
-struct StructTraits<cc::mojom::SharedQuadStateDataView, cc::SharedQuadState> {
+struct StructTraits<viz::mojom::SharedQuadStateDataView, cc::SharedQuadState> {
   static const gfx::Transform& quad_to_target_transform(
       const cc::SharedQuadState& sqs) {
     return sqs.quad_to_target_transform;
@@ -90,7 +90,7 @@ struct StructTraits<cc::mojom::SharedQuadStateDataView, cc::SharedQuadState> {
     return sqs.sorting_context_id;
   }
 
-  static bool Read(cc::mojom::SharedQuadStateDataView data,
+  static bool Read(viz::mojom::SharedQuadStateDataView data,
                    cc::SharedQuadState* out) {
     if (!data.ReadQuadToTargetTransform(&out->quad_to_target_transform) ||
         !data.ReadQuadLayerRect(&out->quad_layer_rect) ||
@@ -111,4 +111,4 @@ struct StructTraits<cc::mojom::SharedQuadStateDataView, cc::SharedQuadState> {
 
 }  // namespace mojo
 
-#endif  // CC_IPC_SHARED_QUAD_STATE_STRUCT_TRAITS_H_
+#endif  // SERVICES_VIZ_PUBLIC_CPP_COMPOSITING_SHARED_QUAD_STATE_STRUCT_TRAITS_H_
