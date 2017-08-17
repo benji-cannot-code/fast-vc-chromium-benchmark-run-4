8dced95798fe65a4f1ffb55edde4ff95f6c92e19
clamy <clamy@chromium.org>
2017-08-17 18:59:05 +0000

    Reland "Have NavigationSimulator support browser-initiated navigations"
    Initial commit message:
    This CL adds support for simulating browser-initiated navigations using
    NavigationSimulator. It removes the one usage outside of content/ of
    RenderFrameHostTester::SimulateNavigationStart in simulation of
    browser-initiated navigations. It also makes
    TestWebContents::NavigateAndCommit use NavigationSimulator.
    
    The original CL was reverted because it casted WebContents into their
    TestWebContents version. This didn't work in tests that uses
    NavigationSimulator and don't inherit from RenderViewHostHarness (but
    use the RenderViewHostTestEnabler instead). In that case, WebContents
    are not created as TestWebContents. The reland removes the cast.
    
    BUG=728571
    TBR=mmenke@chromium.org, timloh@chromium.org, nparker@chromium.org
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: If7203b1be972e623bdc4b06d7fdefc3b8f755405
    Reviewed-on: https://chromium-review.googlesource.com/616938
    Commit-Queue: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495263}

--- chrome/browser/net/net_error_tab_helper_unittest.cc
+++ chrome/browser/net/net_error_tab_helper_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/error_page/common/net_error_info.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_handle.h"
+#include "content/public/test/navigation_simulator.h"
 #include "content/public/test/test_renderer_host.h"
 #include "net/base/net_errors.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -156,18 +157,12 @@ class NetErrorTabHelperTest : public ChromeRenderViewHostTestHarness {
   void FinishProbe(DnsProbeStatus status) { tab_helper_->FinishProbe(status); }
 
   void LoadURL(const GURL& url, bool succeeded) {
-    controller().LoadURL(
-        url, content::Referrer(), ui::PAGE_TRANSITION_TYPED, std::string());
-    content::RenderFrameHostTester::For(main_rfh())->
-        SimulateNavigationStart(url);
     if (succeeded) {
-      content::RenderFrameHostTester::For(main_rfh())->
-          SimulateNavigationCommit(url);
+      content::NavigationSimulator::NavigateAndCommitFromBrowser(web_contents(),
+                                                                 url);
     } else {
-      content::RenderFrameHostTester::For(main_rfh())->
-          SimulateNavigationError(url, net::ERR_TIMED_OUT);
-      content::RenderFrameHostTester::For(main_rfh())->
-          SimulateNavigationErrorPageCommit();
+      content::NavigationSimulator::NavigateAndFailFromBrowser(
+          web_contents(), url, net::ERR_TIMED_OUT);
     }
   }
 
