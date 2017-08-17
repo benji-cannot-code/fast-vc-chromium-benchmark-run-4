67fd03905048073d80f2aa68592694a94246e2ac
Duc Bui <ducbui@google.com>
2017-08-17 03:03:05 +0000

    Reland "Reland "[GRC] Track Expected Task Queueing Duration of tabs in GRC.""
    This CL fixes error "MemorySanitizer: use-of-uninitialized-value" on ChromeOS.
    
    The cause is that CoordinationUnitImpl::CreateCoordinationUnit() returns a
    value after it was uninitialized by std::move().
    https://luci-logdog.appspot.com/v/?s=chromium%2Fbb%2Fchromium.memory%2FLinux_ChromiumOS_MSan_Tests%2F2355%2F%2B%2Frecipes%2Fsteps%2Fbrowser_tests%2F0%2Flogs%2FExtensionURLRewriteBrowserTest.BookmarksURL%2F0
    This is a reland of 49124acbdd202db7fbf4db80b9f86eacfd0e48ab
    Original change's description:
    > Reland "[GRC] Track Expected Task Queueing Duration of tabs in GRC."
    > 
    > This CL fixes the compilation failure for the previously reverted CL
    > (https://chromium-review.googlesource.com/c/610451). The compilation
    > failure is due to the missing dependency from Blink platform/scheduler
    > to generated coordination_unit.mojom-blink.h. This CL separates
    > platform/instrumentation into an independent module and makes
    > scheduler/ depend on it.
    > 
    > The only differences with the original CL are BUILD.gn files in
    > Blink platform/, platform/scheduler/ and platform/instrumentation/.
    > 
    > This is a reland of 525244b2766b65ccb317bdf9f387eb3fb7053faf
    > Original change's description:
    > > [GRC] Track Expected Task Queueing Duration of tabs in GRC.
    > > 
    > > We want to track the smoothness of foreground tabs during session restore,
    > > which can be estimated by their ExpectedTaskQueueingDuration.
    > > 
    > > This patch sends ExpectedTaskQueueingDuration metric measured in Blink
    > > RendererScheduler to GRC.
    > > 
    > > BUG=737817
    > > 
    > > Change-Id: I54be5ef1171fb885598e259ffe0580631b8e6f9c
    > > Reviewed-on: https://chromium-review.googlesource.com/567539
    > > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > > Reviewed-by: Nate Chapin <japhet@chromium.org>
    > > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > > Reviewed-by: Ken Rockot <rockot@chromium.org>
    > > Reviewed-by: Zhen Wang <zhenw@chromium.org>
    > > Commit-Queue: Duc Bui <ducbui@google.com>
    > > Cr-Commit-Position: refs/heads/master@{#493501}
    > 
    > TBR=nasko@chromium.org
    > 
    > Bug: 737817
    > Change-Id: I353b9e98468e5dfbcf1bfe2e66deb75322aa3be2
    > Reviewed-on: https://chromium-review.googlesource.com/613820
    > Commit-Queue: Duc Bui <ducbui@google.com>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Ken Rockot <rockot@chromium.org>
    > Reviewed-by: Nate Chapin <japhet@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: lpy <lpy@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#494666}
    
    Bug: 737817
    Change-Id: Ifefe45613b09b8062261aa08f3921c6fe3368a5f
    
    TBR=nasko@chromium.org
    
    Change-Id: Ifefe45613b09b8062261aa08f3921c6fe3368a5f
    Reviewed-on: https://chromium-review.googlesource.com/617982
    Commit-Queue: Duc Bui <ducbui@google.com>
    Reviewed-by: lpy <lpy@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495062}

--- third_party/WebKit/Source/core/frame/LocalFrame.cpp
+++ third_party/WebKit/Source/core/frame/LocalFrame.cpp
@@ -86,6 +86,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/graphics/paint/PaintCanvas.h"
 #include "platform/graphics/paint/PaintController.h"
 #include "platform/graphics/paint/TransformDisplayItem.h"
+#include "platform/instrumentation/resource_coordinator/BlinkResourceCoordinatorBase.h"
 #include "platform/instrumentation/resource_coordinator/FrameResourceCoordinator.h"
 #include "platform/json/JSONValues.h"
 #include "platform/loader/fetch/FetchParameters.h"
@@ -226,7 +227,6 @@ DEFINE_TRACE(LocalFrame) {
   visitor->Trace(event_handler_);
   visitor->Trace(console_);
   visitor->Trace(input_method_controller_);
-  visitor->Trace(frame_resource_coordinator_);
   visitor->Trace(text_suggestion_controller_);
   Frame::Trace(visitor);
   Supplementable<LocalFrame>::Trace(visitor);
@@ -1002,16 +1002,16 @@ ContentSettingsClient* LocalFrame::GetContentSettingsClient() {
 }
 
 FrameResourceCoordinator* LocalFrame::GetFrameResourceCoordinator() {
-  if (!FrameResourceCoordinator::IsEnabled())
+  if (!BlinkResourceCoordinatorBase::IsEnabled())
     return nullptr;
   if (!frame_resource_coordinator_) {
     auto local_frame_client = Client();
     if (!local_frame_client)
       return nullptr;
-    frame_resource_coordinator_ = FrameResourceCoordinator::Create(
-        local_frame_client->GetInterfaceProvider());
+    frame_resource_coordinator_.reset(FrameResourceCoordinator::Create(
+        local_frame_client->GetInterfaceProvider()));
   }
-  return frame_resource_coordinator_;
+  return frame_resource_coordinator_.get();
 }
 
 PluginData* LocalFrame::GetPluginData() const {
