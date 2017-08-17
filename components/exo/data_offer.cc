06a28fb09c0aeb4cc0472596d78f52fbb15997e3
Daichi Hirono <hirono@chromium.org>
2017-08-17 00:28:10 +0000

    Implement DataOffer::Receive
    DataOffer::Receive is invoked by a client to get drop data bytes. The CL adds
    the implementation which feeds data to passed FD on task runner.
    
    BUG=750997,b:31988797
    TEST=To be added
    
    Change-Id: If4dabfbd7f73fb56e2e398d376f8f0cf6c88c6fd
    Reviewed-on: https://chromium-review.googlesource.com/609397
    Reviewed-by: David Reveman <reveman@chromium.org>
    Commit-Queue: Daichi Hirono <hirono@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495011}

--- components/exo/data_offer.cc
+++ components/exo/data_offer.cc
@@ -5,12 +5,48 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/exo/data_offer.h"
 
+#include "base/files/file_util.h"
+#include "base/memory/ref_counted_memory.h"
+#include "base/task_scheduler/post_task.h"
 #include "components/exo/data_offer_delegate.h"
 #include "components/exo/data_offer_observer.h"
 #include "ui/base/clipboard/clipboard.h"
 #include "ui/base/dragdrop/os_exchange_data.h"
 
 namespace exo {
+namespace {
+
+class RefCountedString16 : public base::RefCountedMemory {
+ public:
+  static scoped_refptr<RefCountedString16> TakeString(
+      base::string16&& to_destroy) {
+    scoped_refptr<RefCountedString16> self(new RefCountedString16);
+    to_destroy.swap(self->data_);
+    return self;
+  }
+
+  // Overridden from base::RefCountedMemory:
+  const unsigned char* front() const override {
+    return reinterpret_cast<const unsigned char*>(data_.data());
+  }
+  size_t size() const override { return data_.size() * sizeof(base::char16); }
+
+ protected:
+  ~RefCountedString16() override {}
+
+ private:
+  base::string16 data_;
+};
+
+void WriteFileDescriptor(base::ScopedFD fd,
+                         scoped_refptr<base::RefCountedMemory> memory) {
+  if (!base::WriteFileDescriptor(fd.get(),
+                                 reinterpret_cast<const char*>(memory->front()),
+                                 memory->size()))
+    DLOG(ERROR) << "Failed to write drop data";
+}
+
+}  // namespace
 
 DataOffer::DataOffer(DataOfferDelegate* delegate) : delegate_(delegate) {}
 
@@ -34,7 +70,15 @@ void DataOffer::Accept(const std::string& mime_type) {
 }
 
 void DataOffer::Receive(const std::string& mime_type, base::ScopedFD fd) {
-  NOTIMPLEMENTED();
+  const auto it = drop_data_.find(mime_type);
+  if (it == drop_data_.end()) {
+    DLOG(ERROR) << "Unexpected mime type is requested";
+    return;
+  }
+
+  base::PostTaskWithTraits(
+      FROM_HERE, {base::MayBlock(), base::TaskPriority::USER_BLOCKING},
+      base::BindOnce(&WriteFileDescriptor, std::move(fd), it->second));
 }
 
 void DataOffer::Finish() {
@@ -54,11 +98,16 @@ void DataOffer::SetSourceActions(
 }
 
 void DataOffer::SetDropData(const ui::OSExchangeData& data) {
-  DCHECK_EQ(0u, mime_types_.size());
-  if (data.HasString())
-    mime_types_.insert(ui::Clipboard::kMimeTypeText);
-  for (const std::string& mime_type : mime_types_) {
-    delegate_->OnOffer(mime_type);
+  DCHECK_EQ(0u, drop_data_.size());
+  if (data.HasString()) {
+    base::string16 string_content;
+    data.GetString(&string_content);
+    drop_data_.emplace(
+        std::string(ui::Clipboard::kMimeTypeText),
+        RefCountedString16::TakeString(std::move(string_content)));
+  }
+  for (const auto& pair : drop_data_) {
+    delegate_->OnOffer(pair.first);
   }
 }
 
