8b61462cb925b96b5ac661bf1681028812fcc324
Justin Carlson <justincarlson@chromium.org>
2017-08-17 23:38:26 +0000

    Zeroconf printer detector implementation.
    This adds printer detection support for ipp/ipps printers advertised
    via zeroconf (bonjour).
    
    BUG=744997,588234
    
    Change-Id: I9e0a633f8b026331090818d780e74c946492a71a
    Reviewed-on: https://chromium-review.googlesource.com/612223
    Commit-Queue: Justin Carlson <justincarlson@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495381}

--- chrome/browser/chromeos/printing/cups_printers_manager_unittest.cc
+++ chrome/browser/chromeos/printing/cups_printers_manager_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
+#include "base/memory/ptr_util.h"
 #include "base/sequenced_task_runner.h"
 #include "base/test/scoped_task_environment.h"
 #include "base/threading/sequenced_task_runner_handle.h"
@@ -250,12 +251,14 @@ class CupsPrintersManagerTest : public testing::Test,
  public:
   CupsPrintersManagerTest()
       : observed_printers_(CupsPrintersManager::kNumPrinterClasses),
-        ppd_provider_(new FakePpdProvider),
-        manager_(CupsPrintersManager::Create(&synced_printers_manager_,
-                                             &usb_detector_,
-                                             &zeroconf_detector_,
-                                             ppd_provider_,
-                                             &event_tracker_)) {
+        ppd_provider_(new FakePpdProvider) {
+    // Zeroconf detector ownership is taken by the manager, so we have
+    // to keep a raw pointer to it.
+    auto zeroconf_detector = base::MakeUnique<FakePrinterDetector>();
+    zeroconf_detector_ = zeroconf_detector.get();
+    manager_ = CupsPrintersManager::Create(
+        &synced_printers_manager_, &usb_detector_, std::move(zeroconf_detector),
+        ppd_provider_, &event_tracker_);
     manager_->AddObserver(this);
   }
 
@@ -285,7 +288,7 @@ class CupsPrintersManagerTest : public testing::Test,
   // Backend fakes driving the CupsPrintersManager.
   FakeSyncedPrintersManager synced_printers_manager_;
   FakePrinterDetector usb_detector_;
-  FakePrinterDetector zeroconf_detector_;
+  FakePrinterDetector* zeroconf_detector_;
   scoped_refptr<FakePpdProvider> ppd_provider_;
 
   // This is unused, it's just here for memory ownership.
@@ -349,8 +352,8 @@ TEST_F(CupsPrintersManagerTest, GetUsbPrinters) {
 
 // Same as GetUsbPrinters, only for Zeroconf printers.
 TEST_F(CupsPrintersManagerTest, GetZeroconfPrinters) {
-  zeroconf_detector_.AddDetections({MakeDiscoveredPrinter("DiscoveredPrinter"),
-                                    MakeAutomaticPrinter("AutomaticPrinter")});
+  zeroconf_detector_->AddDetections({MakeDiscoveredPrinter("DiscoveredPrinter"),
+                                     MakeAutomaticPrinter("AutomaticPrinter")});
   synced_printers_manager_.AddConfiguredPrinters(
       {Printer("Foo"), Printer("Bar")});
 
@@ -364,10 +367,11 @@ TEST_F(CupsPrintersManagerTest, GetZeroconfPrinters) {
 // Test that printers that appear in either a Configured or Enterprise set do
 // *not* appear in Discovered or Automatic, even if they are detected as such.
 TEST_F(CupsPrintersManagerTest, SyncedPrintersTrumpDetections) {
-  zeroconf_detector_.AddDetections({MakeDiscoveredPrinter("DiscoveredPrinter0"),
-                                    MakeDiscoveredPrinter("DiscoveredPrinter1"),
-                                    MakeAutomaticPrinter("AutomaticPrinter0"),
-                                    MakeAutomaticPrinter("AutomaticPrinter1")});
+  zeroconf_detector_->AddDetections(
+      {MakeDiscoveredPrinter("DiscoveredPrinter0"),
+       MakeDiscoveredPrinter("DiscoveredPrinter1"),
+       MakeAutomaticPrinter("AutomaticPrinter0"),
+       MakeAutomaticPrinter("AutomaticPrinter1")});
   scoped_task_environment_.RunUntilIdle();
   // Before we muck with anything else, check that automatic and discovered
   // classes are what we intended to set up.
@@ -400,7 +404,7 @@ TEST_F(CupsPrintersManagerTest, UpdateConfiguredPrinter) {
   Printer existing_configured("Configured");
   synced_printers_manager_.AddConfiguredPrinters({existing_configured});
   usb_detector_.AddDetections({MakeDiscoveredPrinter("Discovered")});
-  zeroconf_detector_.AddDetections({MakeAutomaticPrinter("Automatic")});
+  zeroconf_detector_->AddDetections({MakeAutomaticPrinter("Automatic")});
   scoped_task_environment_.RunUntilIdle();
 
   // Sanity check that we do, indeed, have one printer in each class.
@@ -447,7 +451,7 @@ TEST_F(CupsPrintersManagerTest, GetPrinter) {
   synced_printers_manager_.AddConfiguredPrinters({Printer("Configured")});
   synced_printers_manager_.AddEnterprisePrinters({Printer("Enterprise")});
   usb_detector_.AddDetections({MakeDiscoveredPrinter("Discovered")});
-  zeroconf_detector_.AddDetections({MakeAutomaticPrinter("Automatic")});
+  zeroconf_detector_->AddDetections({MakeAutomaticPrinter("Automatic")});
   scoped_task_environment_.RunUntilIdle();
 
   for (const std::string& id :
---
+++ chrome/browser/chromeos/printing/zeroconf_printer_detector.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#ifndef CHROME_BROWSER_CHROMEOS_PRINTING_ZEROCONF_PRINTER_DETECTOR_H_
+#define CHROME_BROWSER_CHROMEOS_PRINTING_ZEROCONF_PRINTER_DETECTOR_H_
+
+#include <memory>
+
+#include "chrome/browser/chromeos/printing/printer_detector.h"
+
+class Profile;
+
+namespace chromeos {
+
+// Use mDNS and DNS-SD to detect nearby networked printers.  This
+// is sometimes called zeroconf, or Bonjour.
+class ZeroconfPrinterDetector : public PrinterDetector {
+ public:
+  ~ZeroconfPrinterDetector() override = default;
+
+  static std::unique_ptr<ZeroconfPrinterDetector> Create(Profile* profile);
+};
+
+}  // namespace chromeos
+
+#endif  // CHROME_BROWSER_CHROMEOS_PRINTING_ZEROCONF_PRINTER_DETECTOR_H_
