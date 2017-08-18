8142ec4892caf39c78173efb7709bd03959f7af1
Thomas Guilbert <tguilbert@chromium.org>
2017-08-18 03:17:27 +0000

    Add demuxer based HLS detection
    Currently, we detect HLS streams based off of the presence of the string
    ".m3u8" in the media's URL.
    
    This CL removes URL based HLS detection, and replaces it with demuxer
    based detection. This means that we inspect the first few bytes of the
    file using container_names::DetermineContainer(). If we detect an HLS
    manifest, we interrupt the flow of StartPipeline(), update the
    RendererFactorySelector to use MediaPlayerRenderer and restart the
    pipeline's initialization.
    
    This change will also implicitly fix UMA metrics for HLS playback, since
    we log the results of container_names::DetermineContainer().
    
    Bug: 663503
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ia01ae77c0c2aac8882e4884f1bc2675c45924290
    Reviewed-on: https://chromium-review.googlesource.com/616205
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495448}

--- media/filters/ffmpeg_demuxer.cc
+++ media/filters/ffmpeg_demuxer.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/sequenced_worker_pool.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
+#include "build/build_config.h"
 #include "media/audio/sample_rates.h"
 #include "media/base/bind_to_current_loop.h"
 #include "media/base/decrypt_config.h"
@@ -1202,6 +1203,15 @@ void FFmpegDemuxer::OnOpenContextDone(const PipelineStatusCB& status_cb,
     status_cb.Run(PIPELINE_ERROR_ABORT);
     return;
   }
+
+#if defined(OS_ANDROID)
+  if (glue_->detected_hls()) {
+    MEDIA_LOG(INFO, media_log_)
+        << GetDisplayName() << ": detected HLS manifest";
+    status_cb.Run(DEMUXER_ERROR_DETECTED_HLS);
+    return;
+  }
+#endif
 
   if (!result) {
     MEDIA_LOG(ERROR, media_log_) << GetDisplayName() << ": open context failed";
