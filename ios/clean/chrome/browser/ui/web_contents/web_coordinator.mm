c793071d40082265348574b261de301d0ae5c46c
Kurt Horimoto <kkhorimoto@chromium.org>
2017-08-18 03:52:25 +0000

    Reland "[iOS Clean] Created OverlayService."
    This CL introduces OverlayService, a BrowerStateKeyedService that manages
    enqueueing overlay coordinators and scheduling the overlays to be displayed.
    
    Classes of note:
    
    - OverlayService: Interface to the overlay system.  Concrete subclasses
      are vended by OverlayServiceFactory.
    - OverlayQueue: FIFO queue of overlays that have been added to the service.
    - OverlayCoordinator: A BrowserCoordinator subclass that can be presented
      via OverlayService.  This class manages notifying OverlayQueues of
      coordinator lifecycle events.
    - BrowserOverlayQueue: OverlayQueue for overlays that need to be displayed
      over a particular Browser.
    - WebStateOverlayQueue: OverlayQueue for overlays that need to be displayed
      over a particular WebState's content area.
    - OverlayQueueManager: A BrowserUserData subclass that manages creating
      OverlayQueues for a Browser.  It is a WebStateObserver and creates new
      WebStateOverlayQueues for each WebState that is added to the list.
    - OverlayScheduler: An OverlayQueueObserver that listens for events from
      the OverlayQueues managed by its OverlayQueueManager.  This class is
      responsible for queueing OverlayQueues and switching the foreground tab
      to display the appropriate WebState for OverlayCordinators added to a
      WebStateOverlayQueue.
    
    Original CL: https://chromium-review.googlesource.com/c/583945
    
    Changes:
    - Updated unittests to new WebStateList API.
    
    Bug: none
    Change-Id: I6fe279962d36b39acd5d2992616f4fbe538bc87d
    Reviewed-on: https://chromium-review.googlesource.com/609474
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495454}

--- ios/clean/chrome/browser/ui/web_contents/web_coordinator.mm
+++ ios/clean/chrome/browser/ui/web_contents/web_coordinator.mm
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/clean/chrome/browser/ui/commands/context_menu_commands.h"
 #import "ios/clean/chrome/browser/ui/context_menu/context_menu_context_impl.h"
 #import "ios/clean/chrome/browser/ui/context_menu/web_context_menu_coordinator.h"
+#import "ios/clean/chrome/browser/ui/overlays/overlay_service.h"
+#import "ios/clean/chrome/browser/ui/overlays/overlay_service_factory.h"
 #import "ios/clean/chrome/browser/ui/web_contents/web_contents_mediator.h"
 #import "ios/clean/chrome/browser/ui/web_contents/web_contents_view_controller.h"
 #include "ios/web/public/navigation_manager.h"
@@ -28,6 +30,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 @property(nonatomic, strong) WebContentsViewController* viewController;
 @property(nonatomic, strong) WebContentsMediator* mediator;
+
+// Tells the OverlayService that this coordinator is presenting |webState|.
+- (void)setWebStateOverlayParent;
+// Tells the OverlayService that this coordinator is no longer presenting
+// |webState|.
+- (void)resetWebStateOverlayParent;
+
 @end
 
 @implementation WebCoordinator
@@ -44,21 +53,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)setWebState:(web::WebState*)webState {
-  // PLACEHOLDER: The web state delegate will be set by another object, and
-  // this coordinator will not need to know the active web state.
+  [self resetWebStateOverlayParent];
   _webState = webState;
   self.webState->SetDelegate(_webStateDelegate.get());
   self.mediator.webState = self.webState;
+  [self setWebStateOverlayParent];
 }
 
 - (void)start {
+  // Create the view controller and start it.
   self.viewController = [[WebContentsViewController alloc] init];
   self.mediator.consumer = self.viewController;
   self.mediator.webState = self.webState;
   [super start];
+  [self setWebStateOverlayParent];
 }
 
 - (void)stop {
+  [self resetWebStateOverlayParent];
   [super stop];
 }
 
@@ -113,6 +125,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self addChildCoordinator:contextMenu];
   [contextMenu start];
   return YES;
+}
+
+#pragma mark -
+
+- (void)setWebStateOverlayParent {
+  if (self.webState && self.started && self.browser) {
+    OverlayServiceFactory::GetInstance()
+        ->GetForBrowserState(self.browser->browser_state())
+        ->SetWebStateParentCoordinator(self, self.webState);
+  }
+}
+
+- (void)resetWebStateOverlayParent {
+  if (self.webState && self.browser) {
+    OverlayServiceFactory::GetInstance()
+        ->GetForBrowserState(self.browser->browser_state())
+        ->SetWebStateParentCoordinator(nil, self.webState);
+  }
 }
 
 @end
