ce9f8d6e7ac30f8b2253cb2941e8147fbdd96865
Trent Apted <tapted@chromium.org>
2017-08-18 04:37:11 +0000

    Mac: Fix case of "Webkit" in include paths from r495173
    Otherwise compile fails on case-sensitive file systems.
    
    TBR=erikchen@chromium.org,pdr@chromium.org,sahel@chromium.org
    
    Bug: None
    Change-Id: I746a60c138c7d2db7173d935d537b9a354b57948
    Reviewed-on: https://chromium-review.googlesource.com/620296
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495461}

--- chrome/browser/renderer_host/chrome_render_widget_host_view_mac_history_swiper_browsertest.mm
+++ chrome/browser/renderer_host/chrome_render_widget_host_view_mac_history_swiper_browsertest.mm
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test_utils.h"
-#include "third_party/Webkit/public/platform/WebMouseWheelEvent.h"
+#include "third_party/WebKit/public/platform/WebMouseWheelEvent.h"
 #import "third_party/ocmock/OCMock/OCMock.h"
 #import "third_party/ocmock/ocmock_extensions.h"
 #include "ui/events/base_event_utils.h"
