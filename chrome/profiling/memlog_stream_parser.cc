5e631bb3c5c8fd2f256f2a926475a4a92086aa32
erikchen <erikchen@chromium.org>
2017-08-18 19:53:11 +0000

    Add missing lock to MemlogStreamParser::DisconnectReceivers.
    The receivers are disconnected on a different thread than the parsing. If
    parsing is already in progress, this can cause a use after free.
    
    Bug: 751321
    Change-Id: I063711b37fd09ed447273607b79eaaf0e5178c94
    Reviewed-on: https://chromium-review.googlesource.com/619692
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495652}

--- chrome/profiling/memlog_stream_parser.cc
+++ chrome/profiling/memlog_stream_parser.cc
@@ -32,6 +32,7 @@ MemlogStreamParser::MemlogStreamParser(MemlogReceiver* receiver)
 MemlogStreamParser::~MemlogStreamParser() {}
 
 void MemlogStreamParser::DisconnectReceivers() {
+  base::AutoLock lock(lock_);
   receiver_ = nullptr;
 }
 
