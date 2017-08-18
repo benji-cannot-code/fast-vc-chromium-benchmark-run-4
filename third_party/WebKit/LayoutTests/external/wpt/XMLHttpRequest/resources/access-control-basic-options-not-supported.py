83fec29a8b2609bf3a59e8c50fa88d41d5c078ca
Austin James Ahlstrom <aahlstrom@google.com>
2017-08-18 09:29:43 +0000

    Porting access-control-basic-post-fail-non-simple-content-type to WPT
    Bug: 745385
    Change-Id: Iff8975a41dcace408427192c03087571b14bae53
    Reviewed-on: https://chromium-review.googlesource.com/616482
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Austin James Ahlstrom <aahlstrom@google.com>
    Cr-Commit-Position: refs/heads/master@{#495509}

---
+++ third_party/WebKit/LayoutTests/external/wpt/XMLHttpRequest/resources/access-control-basic-options-not-supported.py
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+def main(request, response):
+    response.headers.set("Cache-Control", "no-store")
+
+    # Allow simple requests, but deny preflight
+    if request.method != "OPTIONS":
+        response.headers.set("Access-Control-Allow-Credentials", "true")
+        response.headers.set("Access-Control-Allow-Origin", request.headers.get("origin"))
+    else:
+        response.status = 400
