7dabc1aa7e3aae65216332bb58b7cfb689de4522
kylechar <kylechar@chromium.org>
2017-08-18 03:42:17 +0000

    Check FrameSinkId registration.
    Add DCHECK in HostFrameSinkManager that FrameSinkId is registered before
    CompositorFrameSink or CompositorFrameSinkSupport is created for it.
    Also cleanup HostFrameSinkManagerTests and make sure they all pass with
    the new DCHECK.
    
    Bug: 657959
    Change-Id: I0f08c2abfee9b1c9bb25bf473ebeeb6c066f81fe
    Reviewed-on: https://chromium-review.googlesource.com/619547
    Reviewed-by: Ria Jiang <riajiang@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495452}

--- components/viz/host/host_frame_sink_manager.cc
+++ components/viz/host/host_frame_sink_manager.cc
@@ -79,6 +79,7 @@ void HostFrameSinkManager::CreateRootCompositorFrameSink(
     mojom::CompositorFrameSinkClientPtr client,
     mojom::DisplayPrivateAssociatedRequest display_private_request) {
   FrameSinkData& data = frame_sink_data_map_[frame_sink_id];
+  DCHECK(data.IsFrameSinkRegistered());
   DCHECK(!data.HasCompositorFrameSinkData());
 
   data.is_root = true;
@@ -94,6 +95,7 @@ void HostFrameSinkManager::CreateCompositorFrameSink(
     mojom::CompositorFrameSinkRequest request,
     mojom::CompositorFrameSinkClientPtr client) {
   FrameSinkData& data = frame_sink_data_map_[frame_sink_id];
+  DCHECK(data.IsFrameSinkRegistered());
   DCHECK(!data.HasCompositorFrameSinkData());
 
   data.is_root = false;
@@ -149,6 +151,7 @@ HostFrameSinkManager::CreateCompositorFrameSinkSupport(
   DCHECK(frame_sink_manager_impl_);
 
   FrameSinkData& data = frame_sink_data_map_[frame_sink_id];
+  DCHECK(data.IsFrameSinkRegistered());
   DCHECK(!data.HasCompositorFrameSinkData());
 
   auto support = CompositorFrameSinkSupport::Create(
