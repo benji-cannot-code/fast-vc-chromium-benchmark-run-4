5b145f28d3dd6ef8d56038d80244440ef9474a8e
edchin <edchin@chromium.org>
2017-08-18 13:57:35 +0000

    [ios] Refactor IDC_RATE_THIS_APP to use dispatcher
    This CL refactors IDC_RATE_THIS_APP to use 
    dispatcher rather than ChromeCommand. 
    This is a first step to removing the IDC.
    
    Bug: 738881
    Change-Id: Ib27e4d865707faa1387a26c5b503bb1c890cf54c
    Reviewed-on: https://chromium-review.googlesource.com/615884
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495542}

--- ios/chrome/browser/ui/ntp/google_landing_mediator.mm
+++ ios/chrome/browser/ui/ntp/google_landing_mediator.mm
@@ -30,8 +30,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/search_engines/template_url_service_factory.h"
 #import "ios/chrome/browser/ui/browser_view_controller.h"
 #import "ios/chrome/browser/ui/commands/UIKit+ChromeExecuteCommand.h"
+#include "ios/chrome/browser/ui/commands/browser_commands.h"
 #import "ios/chrome/browser/ui/commands/command_dispatcher.h"
 #import "ios/chrome/browser/ui/commands/generic_chrome_command.h"
+#include "ios/chrome/browser/ui/commands/ios_command_ids.h"
 #import "ios/chrome/browser/ui/ntp/google_landing_consumer.h"
 #import "ios/chrome/browser/ui/ntp/notification_promo_whats_new.h"
 #include "ios/chrome/browser/ui/ntp/ntp_tile_saver.h"
@@ -450,9 +452,14 @@ void SearchEngineObserver::OnTemplateURLServiceChanged() {
   }
 
   if (_notificationPromo->IsChromeCommand()) {
-    GenericChromeCommand* command = [[GenericChromeCommand alloc]
-        initWithTag:_notificationPromo->command_id()];
-    [self.dispatcher chromeExecuteCommand:command];
+    int command_id = _notificationPromo->command_id();
+    if (command_id == IDC_RATE_THIS_APP) {
+      [self.dispatcher performSelector:@selector(showRateThisAppDialog)];
+    } else {
+      GenericChromeCommand* command =
+          [[GenericChromeCommand alloc] initWithTag:command_id];
+      [self.dispatcher chromeExecuteCommand:command];
+    }
     return;
   }
   NOTREACHED();
