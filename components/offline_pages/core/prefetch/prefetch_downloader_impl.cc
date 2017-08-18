b58cee1422affcc8b99e78b034cc4fa4cfcd0d27
Justin DeWitt <dewittj@chromium.org>
2017-08-18 17:01:01 +0000

    [Offline Pages] Make PrefetchDownloader more resilient to destructon order.
    Checks for null when retrieving the PrefetchDispatcher.
    
    Bug: 701939
    Change-Id: I6e87a355422b0b83bf4cdc968b1b0109b7352a20
    Reviewed-on: https://chromium-review.googlesource.com/619808
    Reviewed-by: Dmitry Titov <dimich@chromium.org>
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495584}

--- components/offline_pages/core/prefetch/prefetch_downloader_impl.cc
+++ components/offline_pages/core/prefetch/prefetch_downloader_impl.cc
@@ -16,6 +16,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 namespace offline_pages {
+namespace {
+
+void NotifyDispatcher(PrefetchService* service, PrefetchDownloadResult result) {
+  if (service) {
+    PrefetchDispatcher* dispatcher = service->GetPrefetchDispatcher();
+    if (dispatcher)
+      dispatcher->DownloadCompleted(result);
+  }
+}
+
+}  // namespace
 
 PrefetchDownloaderImpl::PrefetchDownloaderImpl(
     download::DownloadService* download_service,
@@ -109,19 +120,15 @@ void PrefetchDownloaderImpl::OnDownloadSucceeded(
     return;
   }
 
-  if (prefetch_service_) {
-    prefetch_service_->GetPrefetchDispatcher()->DownloadCompleted(
-        PrefetchDownloadResult(download_id, file_path,
-                               static_cast<int64_t>(file_size)));
-  }
+  NotifyDispatcher(prefetch_service_,
+                   PrefetchDownloadResult(download_id, file_path,
+                                          static_cast<int64_t>(file_size)));
 }
 
 void PrefetchDownloaderImpl::OnDownloadFailed(const std::string& download_id) {
-  if (prefetch_service_) {
-    PrefetchDownloadResult result;
-    result.download_id = download_id;
-    prefetch_service_->GetPrefetchDispatcher()->DownloadCompleted(result);
-  }
+  PrefetchDownloadResult result;
+  result.download_id = download_id;
+  NotifyDispatcher(prefetch_service_, result);
 }
 
 void PrefetchDownloaderImpl::OnStartDownload(
