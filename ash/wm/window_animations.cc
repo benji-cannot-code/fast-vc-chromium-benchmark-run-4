107452b23b6c8f2b8247348dbb7fb55788fbe605
wutao <wutao@chromium.org>
2017-08-18 17:10:40 +0000

    CrOS: Cache render surface in cross fade animation.
    Cache render surface in animation can improve the UI performance
    significantly. We have built up fundamental blocks already. In this
    cl, we are caching the render surface in cross fade animation.
    
    Performance improvement:
    Linux build: ~50%
    LINK: ~20%
    KEVIN: ~15%
    
    Bug: 756695, 753224
    Test: Tested improvement with/out cache.
    Change-Id: Iad42f124b4e77c3c171bd0d59ea7b6f2831ae7f3
    Reviewed-on: https://chromium-review.googlesource.com/619931
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495591}

--- ash/wm/window_animations.cc
+++ ash/wm/window_animations.cc
@@ -335,6 +335,7 @@ base::TimeDelta CrossFadeAnimation(
     // Animation observer owns the old layer and deletes itself.
     settings.AddObserver(
         new CrossFadeObserver(window, std::move(old_layer_owner)));
+    settings.CacheRenderSurface();
     settings.SetTransitionDuration(duration);
     settings.SetTweenType(tween_type);
     // Only add reporter to |old_layer|.
@@ -377,6 +378,7 @@ base::TimeDelta CrossFadeAnimation(
     // Animate the new layer to the identity transform, so the window goes to
     // its newly set bounds.
     ui::ScopedLayerAnimationSettings settings(window->layer()->GetAnimator());
+    settings.CacheRenderSurface();
     settings.SetTransitionDuration(duration);
     settings.SetTweenType(tween_type);
     window->layer()->SetTransform(gfx::Transform());
