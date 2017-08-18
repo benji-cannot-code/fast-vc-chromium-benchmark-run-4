291bfa0b726cf8c3e8c1bca3210bf030aa819bd3
Xianzhu Wang <wangxianzhu@chromium.org>
2017-08-18 23:18:12 +0000

    Reland Devirtualize LayoutBoxModelObject logical orientation border/margin/padding accessors
    This relands commit 7d51399398db180fe1a7a83a95820e56a9fac755 which was
    reverted because of performance regression (crbug.com/739111).
    
    Now we have switched to clang on Windows. Tried on perf try bots and
    the CL seemed to no longer cause regressions (see perf try results of
    patchsets 1 and 2 in https://chromium-review.googlesource.com/c/619603).
    
    ----------------------------------------------------------------------------
    
    Previously, almost all border/margin/padding accessors in LayoutBoxModelObject
    were virtual. A subclass needed to override all accessors of a set, or
    otherwise these accessors would be inconsistent. Even if a subclass
    overrode all accessors, it was still hard to keep them consistent.
    For example, LayoutTable overrode PaddingLeft() etc. to return zero
    when there are collapsed borders, while it also overrode PaddingStart()
    and PaddingEnd() with different logic, causing sometimes PaddingLeft()
    != PaddingStart() in horizontal writing-mode.
    
    Devirtualize the logical orientation accessors and base them on the
    virtual physical accessors to ensure consistence.
    
    Also reduce duplicated code about physical/logical orientation
    conversion by using WritingModeUtils.h.
    
    BUG=737324
    TBR=pdr@chromium.org
    
    Change-Id: I7264901fb26426689d05427893bbf709ea5046ac
    Reviewed-on: https://chromium-review.googlesource.com/619603
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495736}

--- third_party/WebKit/Source/core/html/HTMLElement.h
+++ third_party/WebKit/Source/core/html/HTMLElement.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/CoreExport.h"
 #include "core/dom/Element.h"
+#include "platform/text/TextDirection.h"
 
 namespace blink {
 
