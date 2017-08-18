c06bfa19499eb72b1c0fe502c8c55ec043514d60
Matt Wolenetz <wolenetz@chromium.org>
2017-08-18 01:38:18 +0000

    Fix test-only UAF/uninitialized value/bad-cast from invalid vptr in fuzzer timeout mitigation
    Fixes PipelineIntegrationTestBase's UAF and bad-cast from invalid vptr
    of |audio_renderer_| and similar use of uninitialized
    audio_renderer_->first_packet_timestamp_ value in the
    media_pipeline_integration_fuzzer timeout mitigation code from 219c7500.
    
    Instead of waiting for (thread-trampolined) buffering state change
    notifications to inspect the audio renderer's |first_packet_timestamp_|
    (risking race of its destruction), this change adds a test-only callback
    for directly telling the test about any positive |play_delay| in
    AudioRendererImpl::Render(). (Using play_delay instead of
    |first_packet_timestamp_| will also help a future MSE pipeline fuzzer
    abort early after seeking hits a large audio play delay.) This new
    test-only callback also must trampoline from audio thread to the
    fuzzer's main thread, but it precludes the need to then ask the
    (potentially already destructed) AudioRendererImpl for info.
    
    BUG=755501,755499,755619,754500,756412
    
    Change-Id: I9ab2987aa120c21a30463951c75e51838614d62f
    Reviewed-on: https://chromium-review.googlesource.com/616064
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495414}

--- media/renderers/audio_renderer_impl.cc
+++ media/renderers/audio_renderer_impl.cc
@@ -592,6 +592,11 @@ void AudioRendererImpl::OnResume() {
   is_suspending_ = false;
 }
 
+void AudioRendererImpl::SetPlayDelayCBForTesting(PlayDelayCBForTesting cb) {
+  DCHECK_EQ(state_, kUninitialized);
+  play_delay_cb_for_testing_ = std::move(cb);
+}
+
 void AudioRendererImpl::DecodedAudioReady(
     AudioBufferStream::Status status,
     const scoped_refptr<AudioBuffer>& buffer) {
@@ -870,6 +875,9 @@ int AudioRendererImpl::Render(base::TimeDelta delay,
           first_packet_timestamp_ - audio_clock_->back_timestamp();
       if (play_delay > base::TimeDelta()) {
         DCHECK_EQ(frames_written, 0);
+
+        if (!play_delay_cb_for_testing_.is_null())
+          play_delay_cb_for_testing_.Run(play_delay);
 
         // Don't multiply |play_delay| out since it can be a huge value on
         // poorly encoded media and multiplying by the sample rate could cause
