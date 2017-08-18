428ec5673f69f2cac9246dc6344e9a9beb9c3c1d
Sunny Sachanandani <sunnyps@chromium.org>
2017-08-18 23:21:59 +0000

    cc: Fix for crash when GMB allocation fails.
    crrev.com/c/605087 moved GMB allocation code to resource lock but
    dropped the workaround to deal with GMB allocation failure in release
    builds.
    
    R=piman
    BUG=756802,756614,756431,554541
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ie147c25642c80654ca029b62afc9f753d1288a43
    Reviewed-on: https://chromium-review.googlesource.com/621869
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495739}

--- cc/resources/resource_provider.cc
+++ cc/resources/resource_provider.cc
@@ -1158,6 +1158,11 @@ void ResourceProvider::ScopedWriteLockGL::AllocateGpuMemoryBuffer(
   gpu_memory_buffer_ =
       resource_provider_->gpu_memory_buffer_manager()->CreateGpuMemoryBuffer(
           size_, BufferFormat(format_), usage_, gpu::kNullSurfaceHandle);
+  // Avoid crashing in release builds if GpuMemoryBuffer allocation fails.
+  // http://crbug.com/554541
+  if (!gpu_memory_buffer_)
+    return;
+
   if (color_space_.IsValid())
     gpu_memory_buffer_->SetColorSpaceForScanout(color_space_);
 
@@ -1344,7 +1349,9 @@ ResourceProvider::ScopedWriteLockGpuMemoryBuffer::GetGpuMemoryBuffer() {
     gpu_memory_buffer_ =
         resource_provider_->gpu_memory_buffer_manager()->CreateGpuMemoryBuffer(
             size_, BufferFormat(format_), usage_, gpu::kNullSurfaceHandle);
-    if (color_space_.IsValid())
+    // Avoid crashing in release builds if GpuMemoryBuffer allocation fails.
+    // http://crbug.com/554541
+    if (gpu_memory_buffer_ && color_space_.IsValid())
       gpu_memory_buffer_->SetColorSpaceForScanout(color_space_);
   }
   return gpu_memory_buffer_.get();
