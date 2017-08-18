9fd05a02347f0d04364fa97e23e731e245cdbe08
Bruce Dawson <brucedawson@chromium.org>
2017-08-18 03:32:26 +0000

    Document effective use of base::debug::Alias
    Many uses of Alias(&var) end up being ineffective because var is a
    pointer and the crash dump mostly only records the contents of the
    thread stacks - therefore we end up with a pointer but not the memory
    that it points to. This change documents this issue and gives examples
    of effective use of base::debug::Alias.
    
    BUG=756544,756589
    
    Change-Id: I6095d5018e72c159bbe907c6294d7a48cf28fd82
    Reviewed-on: https://chromium-review.googlesource.com/619112
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495449}

--- third_party/WebKit/Source/platform/wtf/debug/Alias.h
+++ third_party/WebKit/Source/platform/wtf/debug/Alias.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WTF {
 namespace debug {
 
+// See base::debug::Alias for details of how to use this effectively.
 inline void Alias(const void* var) {
   base::debug::Alias(var);
 }
