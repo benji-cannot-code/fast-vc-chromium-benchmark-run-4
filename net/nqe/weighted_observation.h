7c60e2cbb66cefb2422e9348e8c7d4d301a41f38
Devdeep Ray <devdeepray@chromium.org>
2017-08-18 01:06:04 +0000

    Remove unused imports from weighted_observation.h
    Bug: 
    Change-Id: If67310ef183d9f82fe534762a4fa108ac0bb33c3
    Reviewed-on: https://chromium-review.googlesource.com/612564
    Commit-Queue: Devdeep Ray <devdeepray@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495409}

--- net/nqe/weighted_observation.h
+++ net/nqe/weighted_observation.h
@@ -6,13 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_NQE_WEIGHTED_OBSERVATION_H_
 #define NET_NQE_WEIGHTED_OBSERVATION_H_
 
-#include <vector>
-
-#include "base/gtest_prod_util.h"
-#include "base/macros.h"
-#include "base/time/time.h"
 #include "net/base/net_export.h"
-#include "net/nqe/network_quality_observation_source.h"
 
 namespace net {
 
