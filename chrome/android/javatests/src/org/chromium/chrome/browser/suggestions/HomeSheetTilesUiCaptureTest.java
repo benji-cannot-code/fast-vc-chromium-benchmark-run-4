144e7cb7f4bf4451143a93fdc81d73d8af03a6d2
Michael van Ouwerkerk <mvanouwerkerk@google.com>
2017-08-18 10:27:58 +0000

    Only enable Modern when Chrome Home is enabled.
    Bug: 756377
    Change-Id: Ic5db5edcbd9c901752fd68ef9e2361a9ec5a5802
    Reviewed-on: https://chromium-review.googlesource.com/618988
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495521}

--- chrome/android/javatests/src/org/chromium/chrome/browser/suggestions/HomeSheetTilesUiCaptureTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/suggestions/HomeSheetTilesUiCaptureTest.java
@@ -25,6 +25,7 @@ import org.chromium.base.test.util.ScreenShooter;
 import org.chromium.base.test.util.parameter.CommandLineParameter;
 import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.ntp.NtpUiCaptureTestData;
+import org.chromium.chrome.browser.util.FeatureUtilities;
 import org.chromium.chrome.browser.widget.bottomsheet.BottomSheet;
 import org.chromium.chrome.test.BottomSheetTestRule;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
@@ -70,7 +71,8 @@ public class HomeSheetTilesUiCaptureTest {
     @ScreenShooter.Directory("Tiles")
     public void testTiles() {
         setSheetState(BottomSheet.SHEET_STATE_FULL);
-        mScreenShooter.shoot("Tiles" + (SuggestionsConfig.useModern() ? "_modern" : ""));
+        mScreenShooter.shoot(
+                "Tiles" + (FeatureUtilities.isChromeHomeModernEnabled() ? "_modern" : ""));
     }
 
     private void setSheetState(final int position) {
