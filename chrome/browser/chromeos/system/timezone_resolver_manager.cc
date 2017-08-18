e53a2a3c73c9c53fbfbfd9e25f703fd1077046f1
Alexander Alekseev <alemate@chromium.org>
2017-08-18 09:36:58 +0000

    Chrome OS: Fix checks if time zone resolver should be running.
    During OOBE automatic time zone resolve always happened once, even if timezone
    was statically configured, or time zone resolving was completely disabled.
    This CL adds additional checks to prevent this if configured.
    
    Bug: 612515
    Change-Id: Ia37b0de9252e9e74cc678a35dc50a376258d491b
    Reviewed-on: https://chromium-review.googlesource.com/620202
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495510}

--- chrome/browser/chromeos/system/timezone_resolver_manager.cc
+++ chrome/browser/chromeos/system/timezone_resolver_manager.cc
@@ -191,10 +191,6 @@ bool TimeZoneResolverManager::ShouldApplyResolvedTimezone() {
   return TimeZoneResolverShouldBeRunning();
 }
 
-bool TimeZoneResolverManager::TimeZoneResolverShouldBeRunningForTests() {
-  return TimeZoneResolverShouldBeRunning();
-}
-
 bool TimeZoneResolverManager::TimeZoneResolverShouldBeRunning() {
   ServiceConfiguration result = GetServiceConfigurationFromPolicy();
 
