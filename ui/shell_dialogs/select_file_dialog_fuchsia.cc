8a1b14584c7eb7d7494192f80c75fcacaae1b2f2
Scott Graham <scottmg@chromium.org>
2017-08-18 19:45:58 +0000

    fuchsia: Stub SelectFileDialog
    Needed for headless_shell in is_component_mode=true.
    
    Bug: 743296, 746674
    Change-Id: Idcec3a837dab82557876bc49d33e47a3b9405e8b
    Reviewed-on: https://chromium-review.googlesource.com/621396
    Commit-Queue: Scott Graham <scottmg@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495651}

---
+++ ui/shell_dialogs/select_file_dialog_fuchsia.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/shell_dialogs/select_file_dialog.h"
+
+#include "base/logging.h"
+
+namespace ui {
+
+SelectFileDialog* CreateSelectFileDialog(SelectFileDialog::Listener* listener,
+                                         SelectFilePolicy* policy) {
+  // TODO(fuchsia): Port once we have UI, see https://crbug.com/746674.
+  NOTREACHED();
+  return nullptr;
+}
+
+}  // namespace ui
