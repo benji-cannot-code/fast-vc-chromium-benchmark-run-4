db36f929eb5dcda76adc3ad07d83915677dcd122
Tommy C. Li <tommycli@chromium.org>
2017-08-18 17:21:37 +0000

    [HBD] Add console message when plugin throttled or blocked.
    Also includes some simplifying refactors to LoadablePluginPlaceholder.
    
    Bug: 748257
    Change-Id: I1830c47054e4e1c9255ec3939afdec9e36707b66
    Reviewed-on: https://chromium-review.googlesource.com/609470
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495595}

--- chrome/renderer/plugins/chrome_plugin_placeholder.cc
+++ chrome/renderer/plugins/chrome_plugin_placeholder.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/feature_list.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
 #include "chrome/common/chrome_features.h"
@@ -61,13 +62,10 @@ ChromePluginPlaceholder::ChromePluginPlaceholder(
     const blink::WebPluginParams& params,
     const std::string& html_data,
     const base::string16& title)
-    : plugins::LoadablePluginPlaceholder(render_frame,
-                                         params,
-                                         html_data),
+    : plugins::LoadablePluginPlaceholder(render_frame, params, html_data),
       status_(ChromeViewHostMsg_GetPluginInfo_Status::kAllowed),
       title_(title),
-      context_menu_request_id_(0),
-      did_send_blocked_content_notification_(false) {
+      context_menu_request_id_(0) {
   RenderThread::Get()->AddObserver(this);
 }
 
@@ -360,14 +358,27 @@ blink::WebPlugin* ChromePluginPlaceholder::CreatePlugin() {
                                       std::move(throttler));
 }
 
-void ChromePluginPlaceholder::OnBlockedTinyContent() {
+void ChromePluginPlaceholder::OnBlockedContent(
+    content::RenderFrame::PeripheralContentStatus status,
+    bool is_same_origin) {
   DCHECK(render_frame());
-  if (did_send_blocked_content_notification_)
-    return;
 
-  did_send_blocked_content_notification_ = true;
-  ContentSettingsObserver::Get(render_frame())
-      ->DidBlockContentType(CONTENT_SETTINGS_TYPE_PLUGINS, title_);
+  if (status ==
+      content::RenderFrame::PeripheralContentStatus::CONTENT_STATUS_TINY) {
+    ContentSettingsObserver::Get(render_frame())
+        ->DidBlockContentType(CONTENT_SETTINGS_TYPE_PLUGINS, title_);
+  }
+
+  std::string message = base::StringPrintf(
+      is_same_origin ? "Same-origin plugin content from %s must have a visible "
+                       "size larger than 6 x 6 pixels, or it will be blocked. "
+                       "Invisible content is always blocked."
+                     : "Cross-origin plugin content from %s must have a "
+                       "visible size larger than 400 x 300 pixels, or it will "
+                       "be blocked. Invisible content is always blocked.",
+      GetPluginParams().url.GetString().Utf8().c_str());
+  render_frame()->AddMessageToConsole(content::CONSOLE_MESSAGE_LEVEL_INFO,
+                                      message);
 }
 
 gin::ObjectTemplateBuilder ChromePluginPlaceholder::GetObjectTemplateBuilder(
--- chrome/renderer/plugins/chrome_plugin_placeholder.h
+++ chrome/renderer/plugins/chrome_plugin_placeholder.h
@@ -55,7 +55,8 @@ class ChromePluginPlaceholder final
 
   // content::LoadablePluginPlaceholder overrides.
   blink::WebPlugin* CreatePlugin() override;
-  void OnBlockedTinyContent() override;
+  void OnBlockedContent(content::RenderFrame::PeripheralContentStatus status,
+                        bool is_same_origin) override;
 
   // gin::Wrappable (via PluginPlaceholder) method
   gin::ObjectTemplateBuilder GetObjectTemplateBuilder(
@@ -95,8 +96,6 @@ class ChromePluginPlaceholder final
 
   int context_menu_request_id_;  // Nonzero when request pending.
   base::string16 plugin_name_;
-
-  bool did_send_blocked_content_notification_;
 
   DISALLOW_COPY_AND_ASSIGN(ChromePluginPlaceholder);
 };
