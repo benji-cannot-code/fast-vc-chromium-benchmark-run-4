8b60e000ae4fbd214cba743c42266d9d4ef8005b
Rune Lillesveen <rune@opera.com>
2017-08-18 16:31:46 +0000

    Let LayoutView::StyleWillChange handle viewport invalidation.
    Make sure we only do a single SetStyle for viewport ComputedStyle
    to which we propagate certain documentElement/body styles. That way we
    can do style invalidation diffing on SetStyle. The invalidation will
    now be done on LayoutView::StyleWillChange instead of invalidating
    LayoutView from LayoutBox::StyleWillChange for documentElement and body
    changes.
    
    The LayoutView invalidation does still use a limited repaint based on
    propagated background changes.
    
    Bug: 755539
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I629098e0073827513801311b730c62fc98079db4
    Reviewed-on: https://chromium-review.googlesource.com/609984
    Commit-Queue: Rune Lillesveen <rune@opera.com>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495573}

--- third_party/WebKit/Source/core/dom/Document.h
+++ third_party/WebKit/Source/core/dom/Document.h
@@ -1394,7 +1394,7 @@ class CORE_EXPORT Document : public ContainerNode,
 
   bool NeedsFullLayoutTreeUpdate() const;
 
-  void PropagateStyleToViewport();
+  void PropagateStyleToViewport(StyleRecalcChange);
 
   void UpdateUseShadowTreesIfNeeded();
   void EvaluateMediaQueryListIfNeeded();
