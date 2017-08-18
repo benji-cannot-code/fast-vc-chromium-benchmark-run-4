8ad9dca3e6132bb94465caadca8b0f1bad1e2ec0
Scott Graham <scottmg@chromium.org>
2017-08-18 20:16:30 +0000

    fuchsia: Add implementation of GetFontList_SlowBlocking()
    Needed for headless_shell in is_component_mode=true.
    
    Bug: 743296, 746674
    Change-Id: I8a7bf90a8b0456b0efddf5f1e4f37ee60db5d97b
    Reviewed-on: https://chromium-review.googlesource.com/621399
    Commit-Queue: Scott Graham <scottmg@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495663}

---
+++ content/common/font_list_fuchsia.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/common/font_list.h"
+
+#include "base/values.h"
+
+namespace content {
+
+std::unique_ptr<base::ListValue> GetFontList_SlowBlocking() {
+  return std::unique_ptr<base::ListValue>(new base::ListValue);
+}
+
+}  // namespace content
