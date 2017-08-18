abedabaa6a602fb4e14aff114cb82c783d0a05dc
Wez <wez@chromium.org>
2017-08-18 04:26:34 +0000

    Fix SysInfo::AmountOfVirtualMemory and PlatformSharedBuffer::Map().
    The meaning of base::SysInfo::AmountOfVirtualMemory() isn't well-
    specified (e.g. under Windows it returns the calling process' maximum
    address-space size, while under POSIX it returns the maximum memory
    available via brk()-based malloc()). Under Fuchsia the Virtual Memory
    Address Range is practically unlimited, so rather than return the size
    of e.g. the root VMAR object of the calling process, we just return
    zero (meaning "unlimited").
    
    PlatformSharedBuffer::Map() was previously DCHECK()ing that mapping the
    underlying SharedMemory object succeeded, and then always returning
    "success" to the caller. This is fixed so that callers, including the
    Mojo PlatformSharedBufferTest.TooBig test, can handle the Map()
    failure.
    
    The PlatformSharedBuffer.TooBig test is fixed to actually attempt to
    Create() a too-big shared buffer, rather than a zero-byte one, and the
    test is removed from the Fuchsia test filters.
    
    Bug: 740791, 706592
    Change-Id: I76d3f1e4201c8cc6c143e469e5d1f081b359aa38
    Reviewed-on: https://chromium-review.googlesource.com/620293
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495459}

--- base/sys_info_fuchsia.cc
+++ base/sys_info_fuchsia.cc
@@ -28,4 +28,9 @@ int SysInfo::NumberOfProcessors() {
   return mx_system_get_num_cpus();
 }
 
+// static
+int64_t SysInfo::AmountOfVirtualMemory() {
+  return 0;
+}
+
 }  // namespace base
--- mojo/edk/embedder/platform_shared_buffer_unittest.cc
+++ mojo/edk/embedder/platform_shared_buffer_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ref_counted.h"
 #include "base/memory/shared_memory.h"
+#include "base/process/process_metrics.h"
 #include "base/sys_info.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -136,7 +137,9 @@ TEST(PlatformSharedBufferTest, InvalidMappings) {
 TEST(PlatformSharedBufferTest, TooBig) {
   // If |size_t| is 32-bit, it's quite possible/likely that |Create()| succeeds
   // (since it only involves creating a 4 GB file).
-  size_t max_size = std::numeric_limits<size_t>::max();
+  // Ask for one page less than the maximum |size_t| can express, otherwise the
+  // |Create()| call will round-up the size, and it'll wrap to zero bytes.
+  size_t max_size = std::numeric_limits<size_t>::max() - base::GetPageSize();
   if (base::SysInfo::AmountOfVirtualMemory() &&
       max_size > static_cast<size_t>(base::SysInfo::AmountOfVirtualMemory()))
     max_size = static_cast<size_t>(base::SysInfo::AmountOfVirtualMemory());
