dd98cf932837acc23bc870a25b6bf2300742c0b1
Patrick Hulce <phulce@chromium.org>
2017-08-19 21:21:27 +0000

    DevTools: Add new Audits domain and getEncodedResponse
    * Adds a new Audits domain with InspectorAuditsAgent.
    * Adds an Audits.getEncodedResponse method that returns the
      request body and size with the specified encoding.
    * Refactors NetworkAgent to support FetchResourceContent
      using a requestId.
    * Adds a basic explicit test for Network.getResponseBody
    
    BUG=756256
    
    Change-Id: I263f38e5422ed2af695a5a3ee502d6e5044ee1e0
    Reviewed-on: https://chromium-review.googlesource.com/617991
    Commit-Queue: Patrick Hulce <phulce@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495826}

--- third_party/WebKit/Source/core/exported/WebDevToolsAgentImpl.cpp
+++ third_party/WebKit/Source/core/exported/WebDevToolsAgentImpl.cpp
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/inspector/InspectedFrames.h"
 #include "core/inspector/InspectorAnimationAgent.h"
 #include "core/inspector/InspectorApplicationCacheAgent.h"
+#include "core/inspector/InspectorAuditsAgent.h"
 #include "core/inspector/InspectorCSSAgent.h"
 #include "core/inspector/InspectorDOMAgent.h"
 #include "core/inspector/InspectorDOMDebuggerAgent.h"
@@ -370,6 +371,8 @@ InspectorSession* WebDevToolsAgentImpl::InitializeSession(int session_id,
   session->Append(overlay_agent);
 
   session->Append(new InspectorIOAgent(isolate, session->V8Session()));
+
+  session->Append(new InspectorAuditsAgent(network_agent));
 
   tracing_agent->SetLayerTreeId(layer_tree_id_);
   network_agent->SetHostId(host_id);
