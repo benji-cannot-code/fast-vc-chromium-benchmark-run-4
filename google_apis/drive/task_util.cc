a19e03ef69d130e63fc943b41f068955ab670a25
tzik <tzik@chromium.org>
2017-08-19 00:08:21 +0000

    Refactor google_apis/task_util
    This CL eliminates specializations of CreateComposedCallback impl, and
    updates them to support OnceCallback.
    
    Change-Id: Ib6bb05c6610b7cce6c1a762c102a1954c2ccb62f
    Reviewed-on: https://chromium-review.googlesource.com/616332
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495758}

--- google_apis/drive/task_util.cc
+++ google_apis/drive/task_util.cc
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace google_apis {
 
 void RunTaskWithTaskRunner(scoped_refptr<base::TaskRunner> task_runner,
-                           const base::Closure& task) {
-  const bool posted = task_runner->PostTask(FROM_HERE, task);
+                           base::OnceClosure task) {
+  const bool posted = task_runner->PostTask(FROM_HERE, std::move(task));
   DCHECK(posted);
 }
 
