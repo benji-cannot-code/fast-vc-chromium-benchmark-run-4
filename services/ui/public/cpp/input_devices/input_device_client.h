d121e041841431b4fbe2f0768e2f74197a3a6d4e
Scott Violet <sky@chromium.org>
2017-08-19 00:54:30 +0000

    [reland] chromeos: Converts OobeDisplayChooser to use InputDeviceManager
    Original patch was here: https://chromium-review.googlesource.com/c/617589
    
    This differs from the original in that the original triggered a never ending
    cycle. Specifically when the client modified ConfigureTouchDevices() it
    notify the touch-devices changed, which triggered another call to
    ConfigureTouchDevices ... To fix this I made it so that ConfigureTouchDevices
    only sends OnTouchDeviceAssociationChanged. I also changed the meaning
    of the mojom slightly to deal with this. Patchset 1 is the original patchset,
    2 the fix.
    
    As part of this I'm adding AreTouchscreenTargetDisplaysValid(), which
    lets client now whether TouchscreenDevice::target_display_id is valid.
    
    BUG=720917
    TEST=covered by test
    
    Change-Id: Iedc7e1ff7816d6c85e10d60f77b79bc392676861
    Reviewed-on: https://chromium-review.googlesource.com/620203
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495770}

--- services/ui/public/cpp/input_devices/input_device_client.h
+++ services/ui/public/cpp/input_devices/input_device_client.h
@@ -43,6 +43,7 @@ class InputDeviceClient : public mojom::InputDeviceObserverMojo,
 
   bool AreDeviceListsComplete() const override;
   bool AreTouchscreensEnabled() const override;
+  bool AreTouchscreenTargetDisplaysValid() const override;
 
   void AddObserver(ui::InputDeviceEventObserver* observer) override;
   void RemoveObserver(ui::InputDeviceEventObserver* observer) override;
@@ -57,7 +58,8 @@ class InputDeviceClient : public mojom::InputDeviceObserverMojo,
   void OnKeyboardDeviceConfigurationChanged(
       const std::vector<ui::InputDevice>& devices) override;
   void OnTouchscreenDeviceConfigurationChanged(
-      const std::vector<ui::TouchscreenDevice>& devices) override;
+      const std::vector<ui::TouchscreenDevice>& devices,
+      bool touchscreen_target_display_ids_changed) override;
   void OnMouseDeviceConfigurationChanged(
       const std::vector<ui::InputDevice>& devices) override;
   void OnTouchpadDeviceConfigurationChanged(
@@ -66,7 +68,8 @@ class InputDeviceClient : public mojom::InputDeviceObserverMojo,
       const std::vector<ui::InputDevice>& keyboard_devices,
       const std::vector<ui::TouchscreenDevice>& touchscreen_devices,
       const std::vector<ui::InputDevice>& mouse_devices,
-      const std::vector<ui::InputDevice>& touchpad_devices) override;
+      const std::vector<ui::InputDevice>& touchpad_devices,
+      bool are_touchscreen_target_displays_valid) override;
   void OnStylusStateChanged(StylusState state) override;
 
  private:
@@ -81,6 +84,7 @@ class InputDeviceClient : public mojom::InputDeviceObserverMojo,
   std::vector<ui::InputDevice> mouse_devices_;
   std::vector<ui::InputDevice> touchpad_devices_;
   bool device_lists_complete_ = false;
+  bool are_touchscreen_target_displays_valid_ = false;
 
   // List of in-process observers.
   base::ObserverList<ui::InputDeviceEventObserver> observers_;
