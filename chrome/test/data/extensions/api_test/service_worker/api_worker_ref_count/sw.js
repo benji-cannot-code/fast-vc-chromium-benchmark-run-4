ccec2f59e9abd1c1943de991bc06effeeeb36f21
Istiaque Ahmed <lazyboy@chromium.org>
2017-08-20 19:08:57 +0000

    Deflake and enable ServiceWorkerTest.WorkerRefCount.
    There are two fixes:
      - JS should have sent SUCCESS message after two messages from worker, it
        incorrectly sent SUCCESS on each of those messages. Fix it.
      - Decrement ref count message initiated from renderer/ asynchronously
        reaches IO thread. Previously GetWorkerRefCount could have inspected
        the ref count even before the IPC reaches IO thread on the browser/.
        Make sure we wait for those IPCs to arrive in the browser/ process.
    
    Bug: 702126
    Change-Id: I67a10f4371b4f9831ec2bb74b126f89ccc190f48
    Reviewed-on: https://chromium-review.googlesource.com/620307
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495845}

--- chrome/test/data/extensions/api_test/service_worker/api_worker_ref_count/sw.js
+++ chrome/test/data/extensions/api_test/service_worker/api_worker_ref_count/sw.js
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+var numMessagesReceived = 0;
 self.onmessage = function(e) {
   var fail = function() {
     e.ports[0].postMessage('FAILURE');
@@ -10,11 +11,19 @@ self.onmessage = function(e) {
   if (e.data == 'sendMessageTest') {
     try {
       chrome.test.sendMessage('CHECK_REF_COUNT', function(reply) {
+        ++numMessagesReceived;
+        // We expect two 'sendMessageTest' messages in the worker, reply to the
+        // browser when we have received both.
+        if (numMessagesReceived == 2) {
+          chrome.test.sendMessage('SUCCESS_FROM_WORKER');
+        }
         e.ports[0].postMessage('Worker reply: ' + reply);
       });
     } catch (e) {
       fail();
     }
+  } else if (e.data == 'roundtrip-request') {
+    e.ports[0].postMessage('roundtrip-response');
   } else {
     fail();
   }
