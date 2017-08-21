b80d9da6956408f98f1ae08420b34d77f2fd53e9
Wei Li <weili@chromium.org>
2017-08-21 23:04:37 +0000

    Uniformize and simplify printing initialization procedure
    Currently there are multiple paths which need to initialize printing.
    These paths all share similar code. Use a function to make the procedure
    more clear and uniform.
    
    BUG=455764
    
    Change-Id: I193142e0b325071020c4e58def7156b9fbacb29f
    Reviewed-on: https://chromium-review.googlesource.com/620249
    Commit-Queue: Wei Li <weili@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496106}

--- chrome/browser/extensions/api/chrome_extensions_api_client.cc
+++ chrome/browser/extensions/api/chrome_extensions_api_client.cc
@@ -46,13 +46,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(ENABLE_PRINTING)
-#if BUILDFLAG(ENABLE_PRINT_PREVIEW)
-#include "chrome/browser/printing/print_preview_message_handler.h"
-#include "chrome/browser/printing/print_view_manager.h"
-#else
-#include "chrome/browser/printing/print_view_manager_basic.h"
-#endif  // BUILDFLAG(ENABLE_PRINT_PREVIEW)
-#endif  // BUILDFLAG(ENABLE_PRINTING)
+#include "chrome/browser/printing/printing_init.h"
+#endif
 
 namespace extensions {
 
@@ -79,13 +74,8 @@ void ChromeExtensionsAPIClient::AttachWebContentsHelpers(
     content::WebContents* web_contents) const {
   favicon::CreateContentFaviconDriverForWebContents(web_contents);
 #if BUILDFLAG(ENABLE_PRINTING)
-#if BUILDFLAG(ENABLE_PRINT_PREVIEW)
-  printing::PrintViewManager::CreateForWebContents(web_contents);
-  printing::PrintPreviewMessageHandler::CreateForWebContents(web_contents);
-#else
-  printing::PrintViewManagerBasic::CreateForWebContents(web_contents);
-#endif  // BUILDFLAG(ENABLE_PRINT_PREVIEW)
-#endif  // BUILDFLAG(ENABLE_PRINTING)
+  printing::InitializePrinting(web_contents);
+#endif
   pdf::PDFWebContentsHelper::CreateForWebContentsWithClient(
       web_contents, std::unique_ptr<pdf::PDFWebContentsHelperClient>(
                         new ChromePDFWebContentsHelperClient()));
