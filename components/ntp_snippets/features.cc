9492802a57ff78ad13a97b47fdbd0ee91b92fef4
Vitalii Iarko <vitaliii@chromium.org>
2017-08-21 11:38:52 +0000

    [NTP::Article] Remove category if it is not in the last fetch.
    This CL removes a category (except articles) if it is not present in the
    last fetch. This functionality can be enabled via a feature param. By
    default it is disabled.
    
    Bug: 757372
    Change-Id: Id495faac67cbab9792ec5dd1bdf110fe0b31a57e
    Reviewed-on: https://chromium-review.googlesource.com/618890
    Commit-Queue: vitaliii <vitaliii@chromium.org>
    Reviewed-by: Tim Schumann <tschumann@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495914}

--- components/ntp_snippets/features.cc
+++ components/ntp_snippets/features.cc
@@ -18,9 +18,10 @@ namespace ntp_snippets {
 const base::Feature* const kAllFeatures[] = {
     &kArticleSuggestionsFeature,
     &kBookmarkSuggestionsFeature,
+    &kBreakingNewsPushFeature,
     &kCategoryOrder,
     &kCategoryRanker,
-    &kBreakingNewsPushFeature,
+    &kDeleteRemoteCategoriesNotPresentInLastFetch,
     &kForeignSessionsSuggestionsFeature,
     &kIncreasedVisibility,
     &kKeepPrefetchedContentSuggestions,
@@ -153,5 +154,9 @@ const char kNotificationsIgnoredLimitParam[] = "ignored_limit";
 
 const base::Feature kKeepPrefetchedContentSuggestions{
     "KeepPrefetchedContentSuggestions", base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kDeleteRemoteCategoriesNotPresentInLastFetch{
+    "DeleteRemoteCategoriesNotPresentInLastFetch",
+    base::FEATURE_DISABLED_BY_DEFAULT};
 
 }  // namespace ntp_snippets
