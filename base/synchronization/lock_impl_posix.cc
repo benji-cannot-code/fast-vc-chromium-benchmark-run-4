60d04dbd96e58c934d90cc988adb2c0b65c72b74
Ken Rockot <rockot@chromium.org>
2017-08-21 22:03:40 +0000

    Revert "Temporary CHECK in base::Lock destruction"
    This reverts commit 05b564e44b5289fa86ee281eb308f7437ec3299e.
    
    Reason for revert: Temporary check is temporary.
    
    Original change's description:
    > Temporary CHECK in base::Lock destruction
    > 
    > In search of a use-after-free lock release which could cause data races
    > in otherwise properly excluded sections.
    > 
    > This is intended to catch instances of a Lock being freed from one
    > thread while still held by another thread, which would result in
    > pthread_mutex_destroy returning EBUSY.
    > 
    > BUG=740044
    > 
    > Change-Id: Iff7863d5a0540c452ddd86b58846af7d384d79e1
    > Reviewed-on: https://chromium-review.googlesource.com/619610
    > Commit-Queue: Ken Rockot <rockot@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#495368}
    
    TBR=dcheng@chromium.org,rockot@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 740044
    Change-Id: Ia3d6ccaa21de16bef1a67f36da1bf8f4b57e3e25
    Reviewed-on: https://chromium-review.googlesource.com/623589
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496076}

--- base/synchronization/lock_impl_posix.cc
+++ base/synchronization/lock_impl_posix.cc
@@ -49,7 +49,7 @@ LockImpl::LockImpl() {
 
 LockImpl::~LockImpl() {
   int rv = pthread_mutex_destroy(&native_handle_);
-  CHECK_EQ(rv, 0) << ". " << strerror(rv);
+  DCHECK_EQ(rv, 0) << ". " << strerror(rv);
 }
 
 bool LockImpl::Try() {
