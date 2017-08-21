6a4fd37001b665c07d75e609199bda7e179bdf3e
Tima Vaisburd <timav@chromium.org>
2017-08-21 18:01:02 +0000

    [Smart Text] Disable surrounding text for input elements.
    This CL effectively reverts
    https://codereview.chromium.org/2907963002, which appears to
    causing problems, including leaking of input fields to
    Google servers.
    
    Disabling until a better fix is found.
    
    BUG=751408, 753579, 721840
    
    Change-Id: Iecddba6ebed8faf3c266daa232613c57fa5a7464
    Reviewed-on: https://chromium-review.googlesource.com/622747
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Tima Vaisburd <timav@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495985}

--- third_party/WebKit/Source/core/editing/SurroundingTextTest.cpp
+++ third_party/WebKit/Source/core/editing/SurroundingTextTest.cpp
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/Position.h"
 #include "core/editing/VisibleSelection.h"
 #include "core/html/HTMLElement.h"
-#include "core/html/TextControlElement.h"
 #include "core/testing/DummyPageHolder.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -285,26 +284,6 @@ TEST_F(SurroundingTextTest, TreeRangeSelection) {
     EXPECT_EQ(20u, surrounding_text.StartOffsetInContent());
     EXPECT_EQ(27u, surrounding_text.EndOffsetInContent());
   }
-}
-
-TEST_F(SurroundingTextTest, TextAreaSelection) {
-  SetHTML(
-      String("<p>First paragraph</p>"
-             "<textarea id='selection'>abc def ghi</textarea>"
-             "<p>Second paragraph</p>"));
-
-  TextControlElement* text_ctrl =
-      (TextControlElement*)GetDocument().getElementById("selection");
-
-  text_ctrl->SetSelectionRange(4, 7);
-  VisibleSelection selection = CreateVisibleSelection(text_ctrl->Selection());
-
-  SurroundingText surrounding_text(
-      *CreateRange(FirstEphemeralRangeOf(selection)), 20);
-
-  EXPECT_EQ("abc def ghi", surrounding_text.Content().SimplifyWhiteSpace());
-  EXPECT_EQ(4u, surrounding_text.StartOffsetInContent());
-  EXPECT_EQ(7u, surrounding_text.EndOffsetInContent());
 }
 
 }  // namespace blink
