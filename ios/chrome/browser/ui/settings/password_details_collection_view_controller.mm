5c8bb3426186fc902a695594777d28898a7f349d
Vasilii Sukhanov <vasilii@google.com>
2017-08-21 15:44:51 +0000

    Implement an alert on the password view page when no passcode is set up.
    If a user tries to copy/view a password without both PIN and TouchID
    configured then it's an error. A dialog appears. The screenshot is
    attached to the bug.
    
    Bug: 754266
    Change-Id: Ib529b401aff68ed717c306707ebb551b20dbfafc
    Reviewed-on: https://chromium-review.googlesource.com/622105
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495944}

--- ios/chrome/browser/ui/settings/password_details_collection_view_controller.mm
+++ ios/chrome/browser/ui/settings/password_details_collection_view_controller.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/password_manager_metrics_util.h"
 #include "components/password_manager/core/browser/password_store.h"
 #include "components/password_manager/core/browser/password_ui_utils.h"
+#include "components/strings/grit/components_strings.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/ui/collection_view/cells/MDCCollectionViewCell+Chrome.h"
 #import "ios/chrome/browser/ui/collection_view/cells/collection_view_item.h"
@@ -23,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/cells/password_details_item.h"
 #import "ios/chrome/browser/ui/settings/reauthentication_module.h"
 #import "ios/chrome/browser/ui/settings/save_passwords_collection_view_controller.h"
+#import "ios/chrome/browser/ui/settings/settings_utils.h"
 #include "ios/chrome/browser/ui/uikit_ui_util.h"
 #include "ios/chrome/grit/ios_strings.h"
 #import "ios/third_party/material_components_ios/src/components/CollectionCells/src/MaterialCollectionCells.h"
@@ -35,6 +37,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace {
+
+// A help article on how to set up a passcode.
+constexpr char kPasscodeArticleURL[] = "https://support.apple.com/HT204060";
 
 typedef NS_ENUM(NSInteger, SectionIdentifier) {
   SectionIdentifierSite = kSectionIdentifierEnumZero,
@@ -383,9 +388,7 @@ reauthenticationModule:(id<ReauthenticationProtocol>)reauthenticationModule {
             l10n_util::GetNSString(IDS_IOS_SETTINGS_PASSWORD_REAUTH_REASON_SHOW)
                                  handler:showPasswordHandler];
   } else {
-    [self showToast:l10n_util::GetNSString(
-                        IDS_IOS_SETTINGS_SET_UP_SCREENLOCK_MESSAGE)
-         forSuccess:NO];
+    [self showPasscodeDialog];
   }
 }
 
@@ -446,10 +449,37 @@ reauthenticationModule:(id<ReauthenticationProtocol>)reauthenticationModule {
             l10n_util::GetNSString(IDS_IOS_SETTINGS_PASSWORD_REAUTH_REASON_COPY)
                                  handler:copyPasswordHandler];
   } else {
-    [self showToast:l10n_util::GetNSString(
-                        IDS_IOS_SETTINGS_SET_UP_SCREENLOCK_MESSAGE)
-         forSuccess:NO];
+    [self showPasscodeDialog];
   }
+}
+
+// Show a dialog offering the user to set a passcode in order to see the
+// passwords.
+- (void)showPasscodeDialog {
+  UIAlertController* alertController = [UIAlertController
+      alertControllerWithTitle:l10n_util::GetNSString(
+                                   IDS_IOS_SETTINGS_SET_UP_SCREENLOCK_TITLE)
+                       message:l10n_util::GetNSString(
+                                   IDS_IOS_SETTINGS_SET_UP_SCREENLOCK_CONTENT)
+                preferredStyle:UIAlertControllerStyleAlert];
+
+  ProceduralBlockWithURL blockOpenURL =
+      ios_internal_settings::BlockToOpenURL(self);
+  UIAlertAction* learnAction = [UIAlertAction
+      actionWithTitle:l10n_util::GetNSString(
+                          IDS_IOS_SETTINGS_SET_UP_SCREENLOCK_LEARN_HOW)
+                style:UIAlertActionStyleDefault
+              handler:^(UIAlertAction*) {
+                blockOpenURL(GURL(kPasscodeArticleURL));
+              }];
+  [alertController addAction:learnAction];
+  UIAlertAction* okAction =
+      [UIAlertAction actionWithTitle:l10n_util::GetNSString(IDS_OK)
+                               style:UIAlertActionStyleDefault
+                             handler:nil];
+  [alertController addAction:okAction];
+  alertController.preferredAction = okAction;
+  [self presentViewController:alertController animated:YES completion:nil];
 }
 
 // Show a MD snack bar with |message| and provide haptic feedback. The haptic
