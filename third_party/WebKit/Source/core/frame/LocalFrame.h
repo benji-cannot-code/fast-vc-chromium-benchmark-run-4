ed54a650f7eabfe68b1f6c2b3ad1aefa6d832cd1
Charles Harrison <csharrison@chromium.org>
2017-08-21 16:24:28 +0000

    New mojo service for exposing FirstPaint per RenderFrameHost
    This service hooks directly in Blink's PaintTiming class which computes
    the first paint, and uses the same timestamp as exposed to the
    web platform.
    
    A followup will plumb the data to WebContentsObservers, and formally
    deprecate WebContentsObserver::DidFirstVisuallyNonEmptyPaint.
    
    Bug: 738464
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2;master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I67618fcdbca9670ff37d5f03f113a0f0dd65b62c
    Reviewed-on: https://chromium-review.googlesource.com/553178
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495955}

--- third_party/WebKit/Source/core/frame/LocalFrame.h
+++ third_party/WebKit/Source/core/frame/LocalFrame.h
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/Supplementable.h"
 #include "platform/heap/Handle.h"
 #include "platform/scroll/ScrollTypes.h"
+#include "public/platform/frame_broker.mojom-blink.h"
 
 namespace service_manager {
 class InterfaceProvider;
@@ -235,6 +236,8 @@ class CORE_EXPORT LocalFrame final : public Frame,
   service_manager::InterfaceProvider& GetInterfaceProvider();
   InterfaceRegistry* GetInterfaceRegistry() { return interface_registry_; }
 
+  mojom::blink::FrameBroker* GetFrameBroker();
+
   LocalFrameClient* Client() const;
 
   ContentSettingsClient* GetContentSettingsClient();
@@ -330,6 +333,7 @@ class CORE_EXPORT LocalFrame final : public Frame,
   Member<CoreProbeSink> probe_sink_;
   Member<PerformanceMonitor> performance_monitor_;
 
+  mojom::blink::FrameBrokerPtr frame_broker_;
   InterfaceRegistry* const interface_registry_;
 
   IntRect remote_viewport_intersection_;
