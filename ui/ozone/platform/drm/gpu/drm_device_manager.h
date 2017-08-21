a20d946e238ee8506cc61c9937917e4778754f76
Robert Kroeger <rjkroege@chromium.org>
2017-08-21 23:40:26 +0000

    Mojo ipc in ozone/drm for host/gpu communication.
    Ozone/DRM is split between the VIZ process and its host (typically the
    browser or services/ui process.) Provide complete mojo IPC connection
    between the host and viz processes.
    
    As part of this CL, the one-process mode for ozone/drm was converted
    to use mojo in-process communication and hence always requires a valid
    mojo connection ptr so also update the the GL unit test harness to provide
    a test-style in-process mojo connection to ozone/drm.
    
    BUG=620927
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I32d691d0c02868561d1dbe838c8f6bb23493dae6
    Reviewed-on: https://chromium-review.googlesource.com/614426
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496121}

--- ui/ozone/platform/drm/gpu/drm_device_manager.h
+++ ui/ozone/platform/drm/gpu/drm_device_manager.h
@@ -10,13 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <vector>
 
+#include "base/files/file.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "ui/gfx/native_widget_types.h"
 
 namespace base {
 class FilePath;
-struct FileDescriptor;
 }
 
 namespace ui {
@@ -34,7 +34,7 @@ class DrmDeviceManager {
   ~DrmDeviceManager();
 
   // The first device registered is assumed to be the primary device.
-  bool AddDrmDevice(const base::FilePath& path, const base::FileDescriptor& fd);
+  bool AddDrmDevice(const base::FilePath& path, base::File file);
   void RemoveDrmDevice(const base::FilePath& path);
 
   // Updates the device associated with |widget|.
--- ui/ozone/platform/drm/gpu/drm_thread_proxy.h
+++ ui/ozone/platform/drm/gpu/drm_thread_proxy.h
@@ -47,6 +47,7 @@ class DrmThreadProxy {
                          std::vector<gfx::BufferFormat>* scanout_formats);
 
   void AddBinding(ozone::mojom::DeviceCursorRequest request);
+  void AddBindingGpu(ozone::mojom::GpuAdapterRequest request);
 
  private:
   DrmThread drm_thread_;
