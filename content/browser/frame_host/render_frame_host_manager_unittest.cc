ff9edbe174db76065463069b661ed2a14855b4de
Arthur Hemery <ahemery@chromium.org>
2017-08-21 16:06:26 +0000

    PlzNavigate: Refactoring unit test to use NavigationSimulator
    Replaced all usage of TestWebContents::StartNavigation within
    content_unittests with a browser side NavigationSimulator.
    
    Bug: 728571
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I2e91f9a85e6b264a8ba4951c3b63d8ccafc92daa
    Reviewed-on: https://chromium-review.googlesource.com/621006
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Arthur Hemery <ahemery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495949}

--- content/browser/frame_host/render_frame_host_manager_unittest.cc
+++ content/browser/frame_host/render_frame_host_manager_unittest.cc
@@ -3117,7 +3117,9 @@ TEST_F(RenderFrameHostManagerTestWithBrowserSideNavigation,
   // should be pending delete.
   RenderFrameHostManager* manager =
       main_test_rfh()->frame_tree_node()->render_manager();
-  contents()->StartNavigation(kUrl2);
+  auto navigation_to_kUrl2 =
+      NavigationSimulator::CreateBrowserInitiated(kUrl2, contents());
+  navigation_to_kUrl2->ReadyToCommit();
   static_cast<TestRenderFrameHost*>(manager->speculative_frame_host())
       ->SimulateNavigationCommit(kUrl2);
   EXPECT_NE(initial_rfh, main_test_rfh());
@@ -3126,9 +3128,9 @@ TEST_F(RenderFrameHostManagerTestWithBrowserSideNavigation,
 
   // The initial RFH receives a BeginNavigation IPC. The navigation should not
   // start.
-  auto navigation =
+  auto navigation_to_kUrl3 =
       NavigationSimulator::CreateRendererInitiated(kUrl3, initial_rfh);
-  navigation->Start();
+  navigation_to_kUrl3->Start();
   EXPECT_FALSE(main_test_rfh()->frame_tree_node()->navigation_request());
 }
 
@@ -3157,7 +3159,9 @@ TEST_F(RenderFrameHostManagerTest,
   // should be pending delete.
   RenderFrameHostManager* manager =
       main_test_rfh()->frame_tree_node()->render_manager();
-  contents()->StartNavigation(kUrl2);
+  auto navigation =
+      NavigationSimulator::CreateBrowserInitiated(kUrl2, contents());
+  navigation->ReadyToCommit();
   static_cast<TestRenderFrameHost*>(manager->pending_frame_host())
       ->SimulateNavigationCommit(kUrl2);
   EXPECT_NE(initial_rfh, main_test_rfh());
