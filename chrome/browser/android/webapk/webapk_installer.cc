8dfb56dad77cec8e77a88ad8deb0efd6119afe1a
Xi Han <hanxi@google.com>
2017-08-21 17:46:56 +0000

    New WebAPK Install UI: display notification after install successfully. [1/4]
    See mock:
    https://drive.google.com/drive/folders/0B7zEF5GgyYmpMC1hQjhEeWZXcEU?usp=sharing
    
    Bug: 752338
    Change-Id: I9c28a7730806a2c205889b5d511cf97ade72cb63
    Reviewed-on: https://chromium-review.googlesource.com/602750
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495981}

--- chrome/browser/android/webapk/webapk_installer.cc
+++ chrome/browser/android/webapk/webapk_installer.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_status_code.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "net/url_request/url_fetcher.h"
+#include "ui/gfx/android/java_bitmap.h"
 #include "ui/gfx/codec/png_codec.h"
 #include "ui/gfx/color_utils.h"
 #include "url/gurl.h"
@@ -289,12 +290,14 @@ void WebApkInstaller::InstallOrUpdateWebApk(const std::string& package_name,
       base::android::ConvertUTF8ToJavaString(env, token);
   base::android::ScopedJavaLocalRef<jstring> java_url =
       base::android::ConvertUTF8ToJavaString(env, start_url_.spec());
+  base::android::ScopedJavaLocalRef<jobject> java_primary_icon =
+      gfx::ConvertToJavaBitmap(&install_primary_icon_);
 
   if (task_type_ == WebApkInstaller::INSTALL) {
     webapk::TrackRequestTokenDuration(install_duration_timer_->Elapsed());
     Java_WebApkInstaller_installWebApkAsync(
         env, java_ref_, java_webapk_package, version, java_title, java_token,
-        java_url, install_shortcut_info_->source);
+        java_url, install_shortcut_info_->source, java_primary_icon);
   } else {
     Java_WebApkInstaller_updateAsync(env, java_ref_, java_webapk_package,
                                      version, java_title, java_token, java_url);
