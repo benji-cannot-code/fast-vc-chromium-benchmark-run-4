d6c9e5bd1c314a1b3eb3c93bff8f1c74bb43e613
James Cook <jamescook@chromium.org>
2017-08-21 20:37:08 +0000

    mash: Add simple browser test for mash login
    This exercises similar code paths to the desktopui_MashLogin autotest
    on the Chrome OS waterfall. It should help us catch regressions earlier.
    
    Bug: 757511
    Test: browser_tests --run-in-mash --gtest_filter="LoginUtilsTest.MashLogin"
    Change-Id: I26424cb7359a8e69d9eadabec11bc2f4f068f8e5
    Reviewed-on: https://chromium-review.googlesource.com/624191
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496051}

--- chrome/browser/chromeos/login/login_utils_browsertest.cc
+++ chrome/browser/chromeos/login/login_utils_browsertest.cc
@@ -5,12 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "ash/public/cpp/config.h"
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/macros.h"
 #include "base/run_loop.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_notification_types.h"
+#include "chrome/browser/chromeos/ash_config.h"
 #include "chrome/browser/chromeos/login/existing_user_controller.h"
 #include "chrome/browser/chromeos/login/test/oobe_base_test.h"
 #include "chrome/browser/chromeos/login/ui/webui_login_display.h"
@@ -83,6 +85,16 @@ class LoginUtilsTest : public OobeBaseTest {
  private:
   DISALLOW_COPY_AND_ASSIGN(LoginUtilsTest);
 };
+
+// Exercises login, like the desktopui_MashLogin Chrome OS autotest.
+IN_PROC_BROWSER_TEST_F(LoginUtilsTest, MashLogin) {
+  if (GetAshConfig() != ash::Config::MASH)
+    return;
+
+  WaitForSigninScreen();
+  Login("username");
+  // Login did not time out and did not crash.
+}
 
 #if BUILDFLAG(ENABLE_RLZ)
 IN_PROC_BROWSER_TEST_F(LoginUtilsTest, RlzInitialized) {
