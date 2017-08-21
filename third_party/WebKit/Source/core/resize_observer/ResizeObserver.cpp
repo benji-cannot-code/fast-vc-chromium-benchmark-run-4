a014d1bec6dee99cc2ddbe178eb2265186ea4cf0
Aleks Totic <atotic@chromium.org>
2017-08-21 21:55:01 +0000

    Scale contentRect by zoom
    Reported size should be in CSS pixels, not physical, zoomed pixels.
    Added a test case.
    
    Also fixed existing iframe test. The existing test was incorrect.
    
    Bug: 724971
    Change-Id: I2f36429f08c6ab68b99d8754e9ddba9599c08348
    Reviewed-on: https://chromium-review.googlesource.com/614108
    Reviewed-by: Anton Vayvod <avayvod@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Aleks Totic <atotic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496072}

--- third_party/WebKit/Source/core/resize_observer/ResizeObserver.cpp
+++ third_party/WebKit/Source/core/resize_observer/ResizeObserver.cpp
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/ResizeObserverCallback.h"
 #include "core/dom/Element.h"
 #include "core/frame/LocalFrameView.h"
+#include "core/layout/LayoutObject.h"
 #include "core/resize_observer/ResizeObservation.h"
 #include "core/resize_observer/ResizeObserverController.h"
 #include "core/resize_observer/ResizeObserverEntry.h"
@@ -111,8 +112,22 @@ void ResizeObserver::DeliverObservations() {
     LayoutPoint location = observation->ComputeTargetLocation();
     LayoutSize size = observation->ComputeTargetSize();
     observation->SetObservationSize(size);
-    auto entry = new ResizeObserverEntry(observation->Target(),
-                                         LayoutRect(location, size));
+
+    LayoutRect content_rect(location, size);
+    if (observation->Target()->GetLayoutObject()) {
+      // Must adjust for zoom in order to report non-zoomed size.
+      const ComputedStyle& style =
+          observation->Target()->GetLayoutObject()->StyleRef();
+      content_rect.SetX(
+          AdjustLayoutUnitForAbsoluteZoom(content_rect.X(), style));
+      content_rect.SetY(
+          AdjustLayoutUnitForAbsoluteZoom(content_rect.Y(), style));
+      content_rect.SetWidth(
+          AdjustLayoutUnitForAbsoluteZoom(content_rect.Width(), style));
+      content_rect.SetHeight(
+          AdjustLayoutUnitForAbsoluteZoom(content_rect.Height(), style));
+    }
+    auto entry = new ResizeObserverEntry(observation->Target(), content_rect);
     entries.push_back(entry);
   }
   DCHECK(callback_ || delegate_);
