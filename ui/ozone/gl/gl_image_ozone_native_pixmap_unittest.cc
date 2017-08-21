c7ea56017f2a1d7bd672efd0dc5f66f091d172cd
Ricky Liang <jcliang@chromium.org>
2017-08-21 22:07:19 +0000

    ui: add gfx::BufferUsage::SCANOUT_CAMERA_READ_WRITE
    The camera HAL v3 on Chrome OS uses platform-specific camera buffers
    which are accessible by camera hardware, GPU, and CPU.
    
    BUG=b:63697757
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I60981d931c14005d9a56c6c47e4bf1b807a43d77
    Reviewed-on: https://chromium-review.googlesource.com/594609
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Commit-Queue: Ricky Liang <jcliang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496077}

--- ui/ozone/gl/gl_image_ozone_native_pixmap_unittest.cc
+++ ui/ozone/gl/gl_image_ozone_native_pixmap_unittest.cc
@@ -41,7 +41,8 @@ class GLImageNativePixmapTestDelegate {
         surface_factory->CreateNativePixmap(gfx::kNullAcceleratedWidget, size,
                                             format, usage);
     DCHECK(pixmap);
-    if (usage == gfx::BufferUsage::GPU_READ_CPU_READ_WRITE) {
+    if (usage == gfx::BufferUsage::GPU_READ_CPU_READ_WRITE ||
+        usage == gfx::BufferUsage::SCANOUT_CAMERA_READ_WRITE) {
       auto client_pixmap = client_pixmap_factory_->ImportFromHandle(
           pixmap->ExportHandle(), size, usage);
       bool mapped = client_pixmap->Map();
@@ -68,7 +69,8 @@ class GLImageNativePixmapTestDelegate {
   const uint8_t* GetImageColor() {
     if (format == gfx::BufferFormat::R_8) {
       return kRed;
-    } else if (format == gfx::BufferFormat::YVU_420) {
+    } else if (format == gfx::BufferFormat::YVU_420 ||
+               format == gfx::BufferFormat::YUV_420_BIPLANAR) {
       return kYvuColor;
     }
     return kGreen;
@@ -88,7 +90,9 @@ INSTANTIATE_TYPED_TEST_CASE_P(GLImageNativePixmapScanout,
 
 using GLImageReadWriteType = testing::Types<
     GLImageNativePixmapTestDelegate<gfx::BufferUsage::GPU_READ_CPU_READ_WRITE,
-                                    gfx::BufferFormat::R_8>>;
+                                    gfx::BufferFormat::R_8>,
+    GLImageNativePixmapTestDelegate<gfx::BufferUsage::SCANOUT_CAMERA_READ_WRITE,
+                                    gfx::BufferFormat::YUV_420_BIPLANAR>>;
 
 using GLImageBindTestTypes = testing::Types<
     GLImageNativePixmapTestDelegate<gfx::BufferUsage::GPU_READ_CPU_READ_WRITE,
@@ -98,6 +102,8 @@ using GLImageBindTestTypes = testing::Types<
     GLImageNativePixmapTestDelegate<gfx::BufferUsage::GPU_READ_CPU_READ_WRITE,
                                     gfx::BufferFormat::YVU_420>,
     GLImageNativePixmapTestDelegate<gfx::BufferUsage::GPU_READ_CPU_READ_WRITE,
+                                    gfx::BufferFormat::YUV_420_BIPLANAR>,
+    GLImageNativePixmapTestDelegate<gfx::BufferUsage::SCANOUT_CAMERA_READ_WRITE,
                                     gfx::BufferFormat::YUV_420_BIPLANAR>>;
 
 // These tests are disabled since the trybots are running with Ozone X11
