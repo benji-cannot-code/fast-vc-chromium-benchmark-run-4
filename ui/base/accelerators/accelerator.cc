92e7234c84744173ab9b4ce08727a322800732b7
Zijie He <zijiehe@chromium.org>
2017-08-21 21:12:52 +0000

    Remove BrowserCommandController::SetBlockCommandExecution() related functions
    After discussion offline, Scott suggested to directly retrieve chrome command id
    from BrowserView instead of using AcceleratorTarget::GetAcceleratorId(). The
    later one requires ash to register Accelerators within the same process as
    browser.
    
    So this change ensures both ash and browser accelerators are using the same
    keyboard shortcuts, and uses BrowserView::GetCommandIdForAccelerator() to
    replace BrowserCommandController::SetBlockCommandExecution() related mechanism.
    
    Meanwhile, since AcceleratorTarget::GetAcceleratorId() is not used anywhere, it
    has been removed.
    
    Bug: chromium:737307
    Change-Id: I54025007de36d12650163d51240375195e1ba7a3
    Reviewed-on: https://chromium-review.googlesource.com/620289
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Zijie He <zijiehe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496061}

--- ui/base/accelerators/accelerator.cc
+++ ui/base/accelerators/accelerator.cc
@@ -302,11 +302,4 @@ base::string16 Accelerator::GetShortcutText() const {
   return shortcut;
 }
 
-// static
-constexpr int AcceleratorTarget::kUnknownAcceleratorId;
-
-int AcceleratorTarget::GetAcceleratorId(const Accelerator& accelerator) const {
-  return kUnknownAcceleratorId;
-}
-
 }  // namespace ui
