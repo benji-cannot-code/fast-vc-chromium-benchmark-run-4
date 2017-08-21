7f1be97219d91f8d47f05249b40e6c72a058f491
Ian Kilpatrick <ikilpatrick@chromium.org>
2017-08-21 17:34:12 +0000

    [LayoutNG] Remove NGConstraintSpaceBuilder "copy" constructor.
    This removes the NGConstraintSpaceBuilder copy constructor which
    took a mutable constraint space. The constructor now just picks off
    the writing_mode, and icb_size, which are the two things that are
    strictly required.
    
    This is in preparation for making NGExclusionSpace immutable when
    passed between layouts, via a copy. (Needed to const-ify
    NGConstraintSpace in the ng_float_utils.cc file. 
    
    Bug: 635619
    Change-Id: Id5cdd2dd33b8d4ee655c26a3ba5f87f046506f2c
    Reviewed-on: https://chromium-review.googlesource.com/617062
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495975}

--- third_party/WebKit/Source/core/layout/ng/ng_floats_utils.h
+++ third_party/WebKit/Source/core/layout/ng/ng_floats_utils.h
@@ -18,7 +18,7 @@ namespace blink {
 // unpositioned float. If the float is in a different writing mode, this will
 // perform a layout.
 CORE_EXPORT LayoutUnit
-ComputeInlineSizeForUnpositionedFloat(NGConstraintSpace* parent_space,
+ComputeInlineSizeForUnpositionedFloat(const NGConstraintSpace& parent_space,
                                       NGUnpositionedFloat* unpositioned_float);
 
 // Positions {@code unpositioned_float} into {@code new_parent_space}.
@@ -27,7 +27,8 @@ CORE_EXPORT NGPositionedFloat
 PositionFloat(LayoutUnit origin_block_offset,
               LayoutUnit parent_bfc_block_offset,
               NGUnpositionedFloat*,
-              NGConstraintSpace* new_parent_space);
+              const NGConstraintSpace& parent_space,
+              NGExclusionSpace* exclusion_space);
 
 // Positions the list of {@code unpositioned_floats}. Adds them as exclusions to
 // {@code space}.
@@ -35,7 +36,8 @@ CORE_EXPORT const Vector<NGPositionedFloat> PositionFloats(
     LayoutUnit origin_block_offset,
     LayoutUnit container_block_offset,
     const Vector<RefPtr<NGUnpositionedFloat>>& unpositioned_floats,
-    NGConstraintSpace* space);
+    const NGConstraintSpace& space,
+    NGExclusionSpace* exclusion_space);
 
 }  // namespace blink
 
