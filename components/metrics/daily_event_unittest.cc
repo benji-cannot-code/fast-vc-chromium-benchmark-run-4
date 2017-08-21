919832a937d1392a20113d6f4683a2af9db9837c
Sebastien Marchand <sebmarchand@chromium.org>
2017-08-21 20:27:12 +0000

    Stop using Time::[To|From]InternalValue in DailyEventTest
    These functions have been deprecated and there's some ongoing effort to
    remove them (see crbug.com/634507)
    
    I'm using the DailyEvent class in the TabStatsTracker and in the
    unittests I need to modify the pref registry value used by my DailyEvent
    instance (as we do in the DailyEventTest). I could simply not update
    this and use "foo.since_origin().InMicroseconds()" in my new code
    (rather than "foo.ToInternalValue()") but I thought that it'd be better
    to update this class while I was here, this way my unittests will be
    similar to the DailyEvent ones. 
    
    
    In this CL I replace:
    - "base::Time().ToInternalValue()" by 0, as it's what it is in practice.
    - "base::Time::FromInternalValue(foo)" by "base::Time() + base::TimeDelta::FromMicroseconds(foo)"
    - "foo.ToInternalValue()" by "foo.since_origin().InMicroseconds()"
    
    Bug: 634507
    Change-Id: Ia4496fba6fbc052bfd7fb6d3334941142726038e
    Reviewed-on: https://chromium-review.googlesource.com/619695
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496048}

--- components/metrics/daily_event_unittest.cc
+++ components/metrics/daily_event_unittest.cc
@@ -64,7 +64,7 @@ TEST_F(DailyEventTest, TestNewFires) {
 // The event should fire if the preference is more than a day old.
 TEST_F(DailyEventTest, TestOldFires) {
   base::Time last_time = base::Time::Now() - base::TimeDelta::FromHours(25);
-  prefs_.SetInt64(kTestPrefName, last_time.ToInternalValue());
+  prefs_.SetInt64(kTestPrefName, last_time.since_origin().InMicroseconds());
   event_.CheckInterval();
   EXPECT_TRUE(observer_->fired());
 }
@@ -72,7 +72,7 @@ TEST_F(DailyEventTest, TestOldFires) {
 // The event should fire if the preference is more than a day in the future.
 TEST_F(DailyEventTest, TestFutureFires) {
   base::Time last_time = base::Time::Now() + base::TimeDelta::FromHours(25);
-  prefs_.SetInt64(kTestPrefName, last_time.ToInternalValue());
+  prefs_.SetInt64(kTestPrefName, last_time.since_origin().InMicroseconds());
   event_.CheckInterval();
   EXPECT_TRUE(observer_->fired());
 }
@@ -80,7 +80,7 @@ TEST_F(DailyEventTest, TestFutureFires) {
 // The event should not fire if the preference is more recent than a day.
 TEST_F(DailyEventTest, TestRecentNotFired) {
   base::Time last_time = base::Time::Now() - base::TimeDelta::FromMinutes(2);
-  prefs_.SetInt64(kTestPrefName, last_time.ToInternalValue());
+  prefs_.SetInt64(kTestPrefName, last_time.since_origin().InMicroseconds());
   event_.CheckInterval();
   EXPECT_FALSE(observer_->fired());
 }
@@ -88,7 +88,7 @@ TEST_F(DailyEventTest, TestRecentNotFired) {
 // The event should not fire if the preference is less than a day in the future.
 TEST_F(DailyEventTest, TestSoonNotFired) {
   base::Time last_time = base::Time::Now() + base::TimeDelta::FromMinutes(2);
-  prefs_.SetInt64(kTestPrefName, last_time.ToInternalValue());
+  prefs_.SetInt64(kTestPrefName, last_time.since_origin().InMicroseconds());
   event_.CheckInterval();
   EXPECT_FALSE(observer_->fired());
 }
