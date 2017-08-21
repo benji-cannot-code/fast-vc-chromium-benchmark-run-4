5ce5eb0e0320ad2c5bc1e84488112a4f0c3ab3c1
Yue Li <updowndota@chromium.org>
2017-08-21 23:30:32 +0000

    Update app list button when voice interaction settings changed
    - Sync system settings at primary user login.
    - Notify app list button with status change when the settings is
    changed.
    - Add AppListButton unittest for settings disabled.
    
    BUG=b/64310083
    
    Change-Id: I0a00f5f40460b5b9b3d65f8fd6b25621f477a16a
    Reviewed-on: https://chromium-review.googlesource.com/617332
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496115}

--- ash/shelf/app_list_button.cc
+++ ash/shelf/app_list_button.cc
@@ -504,6 +504,11 @@ void AppListButton::OnVoiceInteractionStatusChanged(
   }
 }
 
+void AppListButton::OnVoiceInteractionEnabled(bool enabled) {
+  voice_interaction_settings_enabled_ = enabled;
+  SchedulePaint();
+}
+
 void AppListButton::OnActiveUserSessionChanged(const AccountId& account_id) {
   SchedulePaint();
 
@@ -582,7 +587,7 @@ void AppListButton::GenerateAndSendBackEvent(
 
 bool AppListButton::IsVoiceInteractionActive() {
   if (chromeos::switches::IsVoiceInteractionEnabled() &&
-      is_primary_user_active_) {
+      is_primary_user_active_ && voice_interaction_settings_enabled_) {
     DCHECK(voice_interaction_overlay_);
     return true;
   }
