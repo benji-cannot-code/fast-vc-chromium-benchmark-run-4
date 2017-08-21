0bdc624843d402a4fd3ec299842ceeea9227ff3a
Greg Kerr <kerrnel@chromium.org>
2017-08-21 22:45:05 +0000

    Mac V2 Sandbox: Disable re-execution of helper executable.
    To address a performance regression in startup times, the Helper
    executable will no longer re-execute itself after the sandbox is
    engaged.
    
    Bug: 756623
    Change-Id: I36dd44b222eae10e2ac17574ebc70588fdebf592
    Reviewed-on: https://chromium-review.googlesource.com/624378
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Greg Kerr <kerrnel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496100}

--- chrome/app/chrome_exe_main_mac.cc
+++ chrome/app/chrome_exe_main_mac.cc
@@ -43,10 +43,10 @@ constexpr char v2_sandbox_enabled_arg[] = "--v2-sandbox-enabled";
 // sandbox policy.
 constexpr char fd_mapping_arg[] = "--fd_mapping=";
 
-__attribute__((noreturn)) void SandboxExec(const char* exec_path,
-                                           int argc,
-                                           char* argv[],
-                                           int fd_mapping) {
+void SandboxInit(const char* exec_path,
+                 int argc,
+                 char* argv[],
+                 int fd_mapping) {
   char rp[MAXPATHLEN];
   if (realpath(exec_path, rp) == NULL) {
     perror("realpath");
@@ -83,11 +83,6 @@ __attribute__((noreturn)) void SandboxExec(const char* exec_path,
   // const_cast is safe.
   new_argv.push_back(const_cast<char*>(v2_sandbox_enabled_arg));
   new_argv.push_back(nullptr);
-
-  // The helper executable re-executes itself under the sandbox.
-  execv(exec_path, new_argv.data());
-  perror("execve");
-  abort();
 }
 #endif  // defined(HELPER_EXECUTABLE)
 
@@ -126,9 +121,9 @@ __attribute__((visibility("default"))) int main(int argc, char* argv[]) {
     abort();
   }
 
-  // SandboxExec either aborts or execs, but there is no return.
+  // SandboxInit enables the sandbox and then returns.
   if (enable_v2_sandbox)
-    SandboxExec(exec_path, argc, argv, fd_mapping);
+    SandboxInit(exec_path, argc, argv, fd_mapping);
 
   const char* const rel_path =
       "../../../" PRODUCT_FULLNAME_STRING
