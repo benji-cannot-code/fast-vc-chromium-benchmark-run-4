ca8cce555c32db029ab7ed2ef5cff07f05e32170
Marc Treib <treib@chromium.org>
2017-08-21 09:57:08 +0000

    Cleanup: Remove InstantExtended/EmbeddedSearch field trial
    and all the associated param parsing logic.
    These haven't been used in years, and most of instant has been unlaunched.
    
    Bug: 627483
    Change-Id: Ib7d892d1e0f3df8fbc75c9aea5d8bf742c6416c8
    Reviewed-on: https://chromium-review.googlesource.com/620568
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495897}

--- components/search/search_android_unittest.cc
+++ components/search/search_android_unittest.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/search/search.h"
 
-#include "base/command_line.h"
-#include "components/search/search.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace search {
@@ -14,7 +12,6 @@ namespace search {
 namespace {
 
 TEST(SearchTest, EmbeddedSearchAPIEnabled) {
-  EXPECT_EQ(1ul, EmbeddedSearchPageVersion());
   EXPECT_FALSE(IsInstantExtendedAPIEnabled());
 }
 
