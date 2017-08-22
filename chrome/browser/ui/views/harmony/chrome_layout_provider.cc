8e266f1cfc23ac84a01ccb8902ef6c39cda5004f
Bret Sepulveda <bsep@chromium.org>
2017-08-22 03:07:20 +0000

    Harmony - Convert hung renderer dialog for the new spec.
    This patch removes the "frozen tab" image, changes the language of the
    explanatory label and buttons, and removes the bullets from the table of
    hung tabs. This patch also changes the layout of TableView to use
    metrics instead of constants and fixes the line height of Windows body
    fonts to be 20pt (instead of 21).
    
    TBR=grt@chromium.org
    
    Bug: 652507
    Change-Id: I3a3e08d915ecc8b3ac9089a5867468dfe8cc42eb
    Reviewed-on: https://chromium-review.googlesource.com/571199
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496193}

--- chrome/browser/ui/views/harmony/chrome_layout_provider.cc
+++ chrome/browser/ui/views/harmony/chrome_layout_provider.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/harmony/chrome_typography.h"
 #include "chrome/browser/ui/views/harmony/harmony_layout_provider.h"
 #include "ui/base/material_design/material_design_controller.h"
-#include "ui/gfx/font_list.h"
 
 // static
 ChromeLayoutProvider* ChromeLayoutProvider::Get() {
@@ -25,14 +24,6 @@ ChromeLayoutProvider::CreateLayoutProvider() {
   return ui::MaterialDesignController::IsSecondaryUiMaterial()
              ? base::MakeUnique<HarmonyLayoutProvider>()
              : base::MakeUnique<ChromeLayoutProvider>();
-}
-
-// static
-int ChromeLayoutProvider::GetControlHeightForFont(const gfx::FontList& font) {
-  return std::max(views::style::GetLineHeight(views::style::CONTEXT_LABEL,
-                                              views::style::STYLE_PRIMARY),
-                  font.GetHeight()) +
-         Get()->GetDistanceMetric(DISTANCE_CONTROL_TOTAL_VERTICAL_TEXT_PADDING);
 }
 
 gfx::Insets ChromeLayoutProvider::GetInsetsMetric(int metric) const {
@@ -50,14 +41,10 @@ int ChromeLayoutProvider::GetDistanceMetric(int metric) const {
       return 48;
     case DISTANCE_CONTROL_LIST_VERTICAL:
       return GetDistanceMetric(views::DISTANCE_UNRELATED_CONTROL_VERTICAL);
-    case DISTANCE_CONTROL_TOTAL_VERTICAL_TEXT_PADDING:
-      return 6;
     case DISTANCE_RELATED_CONTROL_HORIZONTAL_SMALL:
       return 8;
     case DISTANCE_RELATED_CONTROL_VERTICAL_SMALL:
       return 4;
-    case DISTANCE_RELATED_LABEL_HORIZONTAL:
-      return 10;
     case DISTANCE_RELATED_LABEL_HORIZONTAL_LIST:
       return 8;
     case DISTANCE_SUBSECTION_HORIZONTAL_INDENT:
