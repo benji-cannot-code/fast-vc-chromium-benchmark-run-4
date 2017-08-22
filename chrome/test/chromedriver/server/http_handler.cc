8cff16250e6ef66e6a48e1cc5b179554543dcefa
Jonathon Kereliuk <kereliuk@chromium.org>
2017-08-22 17:23:40 +0000

    [Chromedriver] POST request for session/:sessionId/element/active should be GET according to spec
    Bug: chromedriver:1932
    Change-Id: I62f220e407844f060732bf57ebb7d8ebb1c725c0
    Reviewed-on: https://chromium-review.googlesource.com/614262
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: Jonathon Kereliuk <kereliuk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496346}

--- chrome/test/chromedriver/server/http_handler.cc
+++ chrome/test/chromedriver/server/http_handler.cc
@@ -207,6 +207,9 @@ HttpHandler::HttpHandler(
       CommandMapping(
           kPost, "session/:sessionId/elements",
           WrapToCommand("FindElements", base::Bind(&ExecuteFindElements, 50))),
+      CommandMapping(kGet, "session/:sessionId/element/active",
+                     WrapToCommand("GetActiveElement",
+                                   base::Bind(&ExecuteGetActiveElement))),
       CommandMapping(kPost, "session/:sessionId/element/active",
                      WrapToCommand("GetActiveElement",
                                    base::Bind(&ExecuteGetActiveElement))),
