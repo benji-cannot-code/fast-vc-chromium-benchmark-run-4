4835ba9fc7dd07557c74d49a3109c9127cd2ee4f
Mike Dougherty <michaeldo@chromium.org>
2017-08-22 19:20:58 +0000

    Remove WebkitTouchCallout equals none context menu tests.
    These tests fail on iPads running iOS 11 and on the the newly refactored
    context menu presentation code in crrev.com/c/579585.
    
    Unittests have already been added to test the behaviors of context menu
    elements which set WebkitTouchCallout to none.
    
    Bug: 753119
    Change-Id: I229d7453a14f1d287653848f6d5c85df85b33f6f
    Reviewed-on: https://chromium-review.googlesource.com/625061
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496397}

--- ios/web/shell/test/earl_grey/shell_actions.mm
+++ ios/web/shell/test/earl_grey/shell_actions.mm
@@ -14,10 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace web {
 
-id<GREYAction> LongPressElementForContextMenu(const std::string& element_id,
-                                              bool triggers_context_menu) {
+id<GREYAction> LongPressElementForContextMenu(const std::string& element_id) {
   return WebViewLongPressElementForContextMenu(
-      shell_test_util::GetCurrentWebState(), element_id, triggers_context_menu);
+      shell_test_util::GetCurrentWebState(), element_id, true);
 }
 
 }  // namespace web
