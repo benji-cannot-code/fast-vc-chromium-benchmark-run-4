55360673f10c7a00ae332ebbd1b9b446df82ada6
Fredrik Söderquist <fs@opera.com>
2017-08-22 05:49:33 +0000

    Setting DOMMatrix.m33 or m44 to 1 should preserve is2D
    Per [1], setting a value other than one (1) should clear the is2D flag.
    We had the logic reversed.
    
    [1] https://drafts.fxtf.org/geometry/#dommatrix-attributes
    
    Bug: 756789
    Change-Id: I37fcd4e20fedee6ba29bb164e81cdf324971f9a1
    Reviewed-on: https://chromium-review.googlesource.com/623410
    Commit-Queue: Dominic Cooney <dominicc@chromium.org>
    Reviewed-by: Dominic Cooney <dominicc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496224}

--- third_party/WebKit/Source/core/geometry/DOMMatrix.h
+++ third_party/WebKit/Source/core/geometry/DOMMatrix.h
@@ -71,7 +71,7 @@ class CORE_EXPORT DOMMatrix : public DOMMatrixReadOnly {
   }
   void setM33(double value) {
     matrix_->SetM33(value);
-    SetIs2D(value != 1);
+    SetIs2D(value == 1);
   }
   void setM34(double value) {
     matrix_->SetM34(value);
@@ -85,7 +85,7 @@ class CORE_EXPORT DOMMatrix : public DOMMatrixReadOnly {
   }
   void setM44(double value) {
     matrix_->SetM44(value);
-    SetIs2D(value != 1);
+    SetIs2D(value == 1);
   }
 
   DOMMatrix* multiplySelf(DOMMatrixInit&, ExceptionState&);
