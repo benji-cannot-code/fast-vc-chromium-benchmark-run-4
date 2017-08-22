29c3ea52f27097311c76a8f29b482ef9c06ce9aa
Charles Harrison <csharrison@chromium.org>
2017-08-22 19:27:37 +0000

    Remove LoadURLExternally from blink public API
    This patch essentially just moves the single caller of LoadURLExternally
    into an OpenURL call inside DecidePolicyForNavigation.
    
    Bug: None
    Change-Id: I71f8f6b412ca17e8c7d465c3e500b96704baab49
    Reviewed-on: https://chromium-review.googlesource.com/571139
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496402}

--- content/renderer/render_frame_impl.h
+++ content/renderer/render_frame_impl.h
@@ -153,7 +153,6 @@ class RenderFrameObserver;
 class RenderViewImpl;
 class RenderWidget;
 class RenderWidgetFullscreenPepper;
-class ResourceRequestBody;
 class ScreenOrientationDispatcher;
 class SharedWorkerRepository;
 class UserMediaClientImpl;
@@ -429,10 +428,6 @@ class CONTENT_EXPORT RenderFrameImpl
       const WebPluginInfo& info,
       const blink::WebPluginParams& params,
       std::unique_ptr<PluginInstanceThrottler> throttler) override;
-  void LoadURLExternally(
-      const blink::WebURLRequest& request,
-      blink::WebNavigationPolicy policy,
-      blink::WebTriggeringEventInfo triggering_event_info) override;
   void ExecuteJavaScript(const base::string16& javascript) override;
   bool IsMainFrame() override;
   bool IsHidden() override;
@@ -558,10 +553,6 @@ class CONTENT_EXPORT RenderFrameImpl
                               const blink::WebString& stack_trace) override;
   void DownloadURL(const blink::WebURLRequest& request,
                    const blink::WebString& suggested_name) override;
-  void LoadURLExternally(const blink::WebURLRequest& request,
-                         blink::WebNavigationPolicy policy,
-                         blink::WebTriggeringEventInfo triggering_event_info,
-                         bool should_replace_current_entry) override;
   void LoadErrorPage(int reason) override;
   blink::WebNavigationPolicy DecidePolicyForNavigation(
       const NavigationPolicyInfo& info) override;
@@ -1026,15 +1017,9 @@ class CONTENT_EXPORT RenderFrameImpl
   // |is_history_navigation_in_new_child| is true, the browser process should
   // look for a matching FrameNavigationEntry in the last committed entry to use
   // instead of |url|.
-  void OpenURL(const GURL& url,
-               bool uses_post,
-               const scoped_refptr<ResourceRequestBody>& resource_request_body,
-               const std::string& extra_headers,
-               const Referrer& referrer,
-               blink::WebNavigationPolicy policy,
-               bool should_replace_current_entry,
-               bool is_history_navigation_in_new_child,
-               blink::WebTriggeringEventInfo triggering_event_info);
+  void OpenURL(const NavigationPolicyInfo& info,
+               bool send_referrer,
+               bool is_history_navigation_in_new_child);
 
   // Performs a navigation in the frame. This provides a unified function for
   // the current code path and the browser-side navigation path (in
