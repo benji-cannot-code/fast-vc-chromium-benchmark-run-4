e0dbdfa3876db9f5e7f0e047c849c6692892af43
Patrick Monette <pmonette@chromium.org>
2017-08-22 20:34:21 +0000

    [ModuleDatabase] No longer use BrowserThread::GetTaskRunnerForThread()
    Profiling shows that ModuleDatabase::OnModuleLoad() spends a lot of
    time in BrowserThread::CurrentlyOn() waiting on the global lock.
    
    It is possible to avoid this check by using SequencedTaskRunnerHandle::
    Get() to get the task runner of the UI thread, which doesn't require a
    lock to check if the task is running on the task runner.
    
    Change-Id: I14579cad52deb702f2656df28ba8c19dbf03ec79
    Reviewed-on: https://chromium-review.googlesource.com/611004
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496428}

--- chrome/browser/chrome_browser_main_win.cc
+++ chrome/browser/chrome_browser_main_win.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task_scheduler/post_task.h"
+#include "base/threading/sequenced_task_runner_handle.h"
 #include "base/threading/sequenced_worker_pool.h"
 #include "base/version.h"
 #include "base/win/pe_image.h"
@@ -223,12 +224,13 @@ void OnModuleEvent(uint32_t process_id,
 // the provided |module_watcher|, and starts the enumeration of registered
 // modules in the Windows Registry.
 void SetupModuleDatabase(std::unique_ptr<ModuleWatcher>* module_watcher) {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+
   uint64_t creation_time = 0;
   ModuleEventSinkImpl::GetProcessCreationTime(::GetCurrentProcess(),
                                               &creation_time);
-  ModuleDatabase::SetInstance(base::MakeUnique<ModuleDatabase>(
-      content::BrowserThread::GetTaskRunnerForThread(
-          content::BrowserThread::UI)));
+  ModuleDatabase::SetInstance(
+      base::MakeUnique<ModuleDatabase>(base::SequencedTaskRunnerHandle::Get()));
   auto* module_database = ModuleDatabase::GetInstance();
   uint32_t process_id = ::GetCurrentProcessId();
 
