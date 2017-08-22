0fd4702aad19a4b447668d2dd93ba8ea17414925
rnephew <rnephew@chromium.org>
2017-08-22 19:50:23 +0000

    [Telemetry] Reenable Blob Storage benchmark.
    This got passed over in the fixit by accident.
    
    Bug: 713222
    Change-Id: Icd1c2960e0e317a9d0b0fda56a683a67392730c8
    Reviewed-on: https://chromium-review.googlesource.com/626678
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: rnephew <rnephew@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496412}

--- tools/perf/benchmarks/blob_storage.py
+++ tools/perf/benchmarks/blob_storage.py
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 from core import perf_benchmark
 
-from telemetry import benchmark
 from telemetry import story
 from telemetry.timeline import chrome_trace_category_filter
 from telemetry.web_perf import timeline_based_measurement
@@ -17,7 +16,6 @@ BLOB_CATEGORY = 'Blob'
 TIMELINE_REQUIRED_CATEGORY = 'blink.console'
 
 
-@benchmark.Disabled('android')  # crbug.com/739214
 class BlobStorage(perf_benchmark.PerfBenchmark):
   """Timeline based measurement benchmark for Blob Storage."""
 
