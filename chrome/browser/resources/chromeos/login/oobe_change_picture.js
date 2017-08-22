0c8c3dbdb720f91b6c06b6aa9cd93f75a7c1be4a
David Reveman <reveman@chromium.org>
2017-08-22 01:16:34 +0000

    Change Picture: Revert to profile picture when discarding image
    Switch to default profile picture to ensure that photo is discarded
    properly. Prior to this change the photo would not actually be
    discarded unless another user icon was selected after pressing the
    discard button.
    
    Bug: 753191
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ibbf8220232189720352798a07a8c45ab5d4db49c
    Reviewed-on: https://chromium-review.googlesource.com/622968
    Commit-Queue: David Reveman <reveman@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496162}

--- chrome/browser/resources/chromeos/login/oobe_change_picture.js
+++ chrome/browser/resources/chromeos/login/oobe_change_picture.js
@@ -180,6 +180,8 @@ Polymer({
   /** @private */
   onDiscardImage_: function() {
     this.pictureList_.setOldImageUrl('');
+    // Revert to profile image as we don't know what last used default image is.
+    this.sendSelectImage_(CrPicture.SelectionTypes.PROFILE, '');
     chrome.send('discardPhoto');  // Plays 'SOUND_OBJECT_DELETE'.
     announceAccessibleMessage(this.i18n('photoDiscardAccessibleText'));
   },
