18ffe57b66f26f72b7c08f0610ff4d6c68bdf5dc
Mikhail Pozdnyakov <mikhail.pozdnyakov@intel.com>
2017-08-22 10:20:22 +0000

    [sensors] Improve accuracy of reporting interval
    This patch decreases minimum waiting interval treshold to 0.01 seconds
    making reporting intervals more accurate.
    
    The corresponding layout test is simplified.
    
    Bug: 731018
    Change-Id: I6efa89af02d4496dd959948b2a57c02108df36d2
    Reviewed-on: https://chromium-review.googlesource.com/625620
    Reviewed-by: Alexander Shalamov <alexander.shalamov@intel.com>
    Commit-Queue: Mikhail Pozdnyakov <mikhail.pozdnyakov@intel.com>
    Cr-Commit-Position: refs/heads/master@{#496269}

--- third_party/WebKit/LayoutTests/sensor/resources/sensor-helpers.js
+++ third_party/WebKit/LayoutTests/sensor/resources/sensor-helpers.js
@@ -32,7 +32,6 @@ function sensorMocks() {
       this.reportingMode_ = reportingMode;
       this.sensorReadingTimerId_ = null;
       this.updateReadingFunction_ = null;
-      this.readingUpdatesCount_ = 0;
       this.suspendCalled_ = null;
       this.resumeCalled_ = null;
       this.addConfigurationCalled_ = null;
@@ -55,9 +54,6 @@ function sensorMocks() {
       return Promise.resolve({frequency: 5});
     }
 
-    readingUpdatesCount() {
-      return this.readingUpdatesCount_;
-    }
     // Adds configuration for the sensor and starts reporting fake data
     // through updateReadingFunction_ callback.
     addConfiguration(configuration) {
@@ -117,7 +113,6 @@ function sensorMocks() {
     reset() {
       this.stopReading();
 
-      this.readingUpdatesCount_ = 0;
       this.startShouldFail_ = false;
       this.updateReadingFunction_ = null;
       this.requestedFrequencies_ = [];
@@ -187,7 +182,6 @@ function sensorMocks() {
             // For all tests sensor reading should have monotonically
             // increasing timestamp in seconds.
             this.buffer_[1] = window.performance.now() * 0.001;
-            this.readingUpdatesCount_++;
           }
           if (this.reportingMode_ === device.mojom.ReportingMode.ON_CHANGE) {
             this.client_.sensorReadingChanged();
