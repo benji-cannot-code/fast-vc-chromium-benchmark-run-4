ed0735f299702e02d4a37f5d242dd3afeb1a7729
Sigurdur Asgeirsson <siggi@chromium.org>
2017-08-22 16:18:41 +0000

    Use import binding instead of manual thunking for chrome_elf.
    This replaces two of the manual GetModuleHandle/GetProcAddress 
    dispatches to chrome_elf with import binding or static linking
    where appropriate.
    
    Bug: 753363
    Cq-Include-Trybots: master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    Change-Id: I74c2951daf53ef1d1e84154c0484c580f32c6df7
    Reviewed-on: https://chromium-review.googlesource.com/606702
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: Robert Shield <robertshield@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496322}

--- components/crash/content/app/crashpad.cc
+++ components/crash/content/app/crashpad.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_piece.h"
+#include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
@@ -36,6 +37,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_POSIX)
 #include <unistd.h>
 #endif  // OS_POSIX
+
+#if defined(OS_WIN)
+#include "components/crash/content/app/crash_export_thunks.h"
+#endif
 
 namespace crash_reporter {
 
@@ -241,7 +246,51 @@ bool GetUploadsEnabled() {
   return false;
 }
 
+void DumpWithoutCrashing() {
+  CRASHPAD_SIMULATE_CRASH();
+}
+
 void GetReports(std::vector<Report>* reports) {
+#if defined(OS_WIN)
+  // On Windows, the crash client may be linked into another module, which
+  // does the client registration. That means the global that holds the crash
+  // report database lives across a module boundary, where the other module
+  // implements the GetCrashReportsImpl function. Since the other module has
+  // a separate allocation domain, this awkward copying is necessary.
+
+  // Start with an arbitrary copy size.
+  reports->resize(25);
+  while (true) {
+    size_t available_reports =
+        GetCrashReportsImpl(&reports->at(0), reports->size());
+    if (available_reports <= reports->size()) {
+      // The input size was large enough to capture all available crashes.
+      // Trim the vector to the actual number of reports returned and return.
+      reports->resize(available_reports);
+      return;
+    }
+
+    // Resize to the number of available reports, plus some slop to all but
+    // eliminate the possibility of running around the loop again due to a
+    // newly arrived crash report.
+    reports->resize(available_reports + 5);
+  }
+#else
+  GetReportsImpl(reports);
+#endif
+}
+
+void RequestSingleCrashUpload(const std::string& local_id) {
+#if defined(OS_WIN)
+  // On Windows, crash reporting may be implemented in another module, which is
+  // why this can't call crash_reporter::RequestSingleCrashUpload directly.
+  RequestSingleCrashUploadImpl(local_id);
+#else
+  crash_reporter::RequestSingleCrashUploadImpl(local_id);
+#endif
+}
+
+void GetReportsImpl(std::vector<Report>* reports) {
   reports->clear();
 
   if (!g_database) {
@@ -263,10 +312,15 @@ void GetReports(std::vector<Report>* reports) {
 
   for (const crashpad::CrashReportDatabase::Report& completed_report :
        completed_reports) {
-    Report report;
-    report.local_id = completed_report.uuid.ToString();
+    Report report = {};
+
+    // TODO(siggi): CHECK that this fits?
+    base::strlcpy(report.local_id, completed_report.uuid.ToString().c_str(),
+                  sizeof(report.local_id));
+
     report.capture_time = completed_report.creation_time;
-    report.remote_id = completed_report.id;
+    base::strlcpy(report.remote_id, completed_report.id.c_str(),
+                  sizeof(report.remote_id));
     if (completed_report.uploaded) {
       report.upload_time = completed_report.last_upload_attempt_time;
       report.state = ReportUploadState::Uploaded;
@@ -279,8 +333,9 @@ void GetReports(std::vector<Report>* reports) {
 
   for (const crashpad::CrashReportDatabase::Report& pending_report :
        pending_reports) {
-    Report report;
-    report.local_id = pending_report.uuid.ToString();
+    Report report = {};
+    base::strlcpy(report.local_id, pending_report.uuid.ToString().c_str(),
+                  sizeof(report.local_id));
     report.capture_time = pending_report.creation_time;
     report.upload_time = 0;
     report.state = pending_report.upload_explicitly_requested
@@ -295,16 +350,12 @@ void GetReports(std::vector<Report>* reports) {
             });
 }
 
-void RequestSingleCrashUpload(const std::string& local_id) {
+void RequestSingleCrashUploadImpl(const std::string& local_id) {
   if (!g_database)
     return;
   crashpad::UUID uuid;
   uuid.InitializeFromString(local_id);
   g_database->RequestUpload(uuid);
-}
-
-void DumpWithoutCrashing() {
-  CRASHPAD_SIMULATE_CRASH();
 }
 
 }  // namespace crash_reporter
@@ -343,12 +394,6 @@ void SetCrashKeyValueImplEx(const char* key, const char* value) {
 
 void ClearCrashKeyValueImplEx(const char* key) {
   crash_reporter::ClearCrashKey(key);
-}
-
-// This helper is invoked by code in chrome.dll to request a single crash report
-// upload. See CrashUploadListCrashpad.
-void RequestSingleCrashUploadImpl(const std::string& local_id) {
-  crash_reporter::RequestSingleCrashUpload(local_id);
 }
 
 }  // extern "C"
