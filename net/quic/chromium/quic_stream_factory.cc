8ac51c35e00f6f7fdc3b7839d14cd34b06ac5d8a
Helen Li <xunjieli@chromium.org>
2017-08-22 22:57:40 +0000

    Make sure pending requests set and |active_jobs_| do not go out of sync.
    This CL removes the pending requests map and associate a
    QuicStreamRequest* with an active Job. This to avoid having
    to maintain two containers that can go out of sync. The linked
    bug suggested that they can be out of sync.
    
    Bug: 750271
    Change-Id: Idaa41ece587f1a63130b0b93fad4ed74f69f3468
    Reviewed-on: https://chromium-review.googlesource.com/627116
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Helen Li <xunjieli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496484}

--- net/quic/chromium/quic_stream_factory.cc
+++ net/quic/chromium/quic_stream_factory.cc
@@ -361,6 +361,20 @@ class QuicStreamFactory::Job {
   // Returns the estimate of dynamically allocated memory in bytes.
   size_t EstimateMemoryUsage() const;
 
+  void AddRequest(QuicStreamRequest* request) {
+    stream_requests_.insert(request);
+  }
+
+  void RemoveRequest(QuicStreamRequest* request) {
+    auto request_iter = stream_requests_.find(request);
+    DCHECK(request_iter != stream_requests_.end());
+    stream_requests_.erase(request_iter);
+  }
+
+  const std::set<QuicStreamRequest*>& stream_requests() {
+    return stream_requests_;
+  }
+
  private:
   enum IoState {
     STATE_NONE,
@@ -385,7 +399,9 @@ class QuicStreamFactory::Job {
   AddressList address_list_;
   base::TimeTicks dns_resolution_start_time_;
   base::TimeTicks dns_resolution_end_time_;
+  std::set<QuicStreamRequest*> stream_requests_;
   base::WeakPtrFactory<Job> weak_factory_;
+
   DISALLOW_COPY_AND_ASSIGN(Job);
 };
 
@@ -831,7 +847,6 @@ void QuicStreamFactory::DumpMemoryStats(
       base::trace_event::EstimateMemoryUsage(session_peer_ip_) +
       base::trace_event::EstimateMemoryUsage(gone_away_aliases_) +
       base::trace_event::EstimateMemoryUsage(active_jobs_) +
-      base::trace_event::EstimateMemoryUsage(job_requests_map_) +
       base::trace_event::EstimateMemoryUsage(active_cert_verifier_jobs_);
   factory_dump->AddScalar(base::trace_event::MemoryAllocatorDump::kNameSize,
                           base::trace_event::MemoryAllocatorDump::kUnitsBytes,
@@ -924,7 +939,7 @@ int QuicStreamFactory::Create(const QuicServerId& server_id,
     net_log.AddEvent(
         NetLogEventType::HTTP_STREAM_JOB_BOUND_TO_QUIC_STREAM_FACTORY_JOB,
         job_net_log.source().ToEventParametersCallback());
-    job_requests_map_[server_id].insert(request);
+    it->second->AddRequest(request);
     return ERR_IO_PENDING;
   }
 
@@ -954,7 +969,7 @@ int QuicStreamFactory::Create(const QuicServerId& server_id,
   int rv = job->Run(base::Bind(&QuicStreamFactory::OnJobComplete,
                                base::Unretained(this), job.get()));
   if (rv == ERR_IO_PENDING) {
-    job_requests_map_[server_id].insert(request);
+    job->AddRequest(request);
     active_jobs_[server_id] = std::move(job);
     return rv;
   }
@@ -1020,29 +1035,24 @@ void QuicStreamFactory::OnJobComplete(Job* job, int rv) {
   // returns.
   const QuicServerId server_id(job->key().server_id());
 
-  ServerIDRequestsMap::iterator requests_iter =
-      job_requests_map_.find(server_id);
+  auto iter = active_jobs_.find(server_id);
   // TODO(xunjieli): Change following CHECKs back to DCHECKs after
   // crbug.com/750271 is fixed.
-  CHECK(requests_iter != job_requests_map_.end());
+  CHECK(iter != active_jobs_.end());
   if (rv == OK) {
     set_require_confirmation(false);
 
-    if (!requests_iter->second.empty()) {
-      SessionMap::iterator session_it = active_sessions_.find(server_id);
-      CHECK(session_it != active_sessions_.end());
-      QuicChromiumClientSession* session = session_it->second;
-      for (QuicStreamRequest* request : requests_iter->second) {
-        CHECK(request->server_id() == server_id);
-        // Do not notify |request| yet.
-        request->SetSession(session->CreateHandle());
-      }
+    SessionMap::iterator session_it = active_sessions_.find(server_id);
+    CHECK(session_it != active_sessions_.end());
+    QuicChromiumClientSession* session = session_it->second;
+    for (auto* request : iter->second->stream_requests()) {
+      CHECK(request->server_id() == server_id);
+      // Do not notify |request| yet.
+      request->SetSession(session->CreateHandle());
     }
   }
 
-  // It's okay not to erase |request| from |requests_iter->second| because the
-  // entire RequestSet will be erased from |job_requests_map_|.
-  for (auto* request : requests_iter->second) {
+  for (auto* request : iter->second->stream_requests()) {
     // Even though we're invoking callbacks here, we don't need to worry
     // about |this| being deleted, because the factory is owned by the
     // profile which can not be deleted via callbacks.
@@ -1051,9 +1061,7 @@ void QuicStreamFactory::OnJobComplete(Job* job, int rv) {
     }
     request->OnRequestComplete(rv);
   }
-
-  active_jobs_.erase(server_id);
-  job_requests_map_.erase(requests_iter);
+  active_jobs_.erase(iter);
 }
 
 void QuicStreamFactory::OnCertVerifyJobComplete(CertVerifierJob* job, int rv) {
@@ -1122,10 +1130,8 @@ void QuicStreamFactory::OnBlackholeAfterHandshakeConfirmed(
 }
 
 void QuicStreamFactory::CancelRequest(QuicStreamRequest* request) {
-  ServerIDRequestsMap::iterator requests_it =
-      job_requests_map_.find(request->server_id());
-  DCHECK(requests_it != job_requests_map_.end());
-  requests_it->second.erase(request);
+  auto job_iter = active_jobs_.find(request->server_id());
+  job_iter->second->RemoveRequest(request);
 }
 
 void QuicStreamFactory::CloseAllSessions(int error, QuicErrorCode quic_error) {
