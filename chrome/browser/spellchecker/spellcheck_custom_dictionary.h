3139f3dbf983a44b7cdebbbf1aa6b1a980756c59
Patrick Monette <pmonette@chromium.org>
2017-08-22 19:10:16 +0000

    Remove the usage of BrowserThread::FILE in the spellchecker files
    The usage of BrowserThread::FILE is deprecated in favor of the
    post_task.h API.
    
    This is a new attempt at
    https://chromium-review.googlesource.com/c/510018,
    that got reverted because of flaky tests.
    
    I changed the code to make sure the dictionary is deleted on the
    same task runner is it used in.
    
    Bug: 689520
    Change-Id: Ie8bfe46de608a4b1944b259007314405c9b56d50
    Reviewed-on: https://chromium-review.googlesource.com/619607
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496394}

--- chrome/browser/spellchecker/spellcheck_custom_dictionary.h
+++ chrome/browser/spellchecker/spellcheck_custom_dictionary.h
@@ -13,8 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/cancelable_callback.h"
 #include "base/files/file_path.h"
 #include "base/macros.h"
+#include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
+#include "base/sequenced_task_runner.h"
 #include "components/spellcheck/browser/spellcheck_dictionary.h"
 #include "components/sync/model/sync_data.h"
 #include "components/sync/model/sync_error.h"
@@ -196,6 +198,9 @@ class SpellcheckCustomDictionary : public SpellcheckDictionary,
   // Notifies observers of the dictionary change if the dictionary has been
   // changed.
   void Notify(const Change& dictionary_change);
+
+  // Task runner where the file operations takes place.
+  scoped_refptr<base::SequencedTaskRunner> task_runner_;
 
   // In-memory cache of the custom words file.
   std::set<std::string> words_;
