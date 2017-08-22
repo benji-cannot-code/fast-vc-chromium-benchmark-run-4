651c9bdff44d4509c64fc8229dc0164e9fd2ce7d
Alice Boxhall <aboxhall@chromium.org>
2017-08-22 22:11:32 +0000

    [Devtools] Correctly get background color from elements in Shadow DOM
    Bug: 514674
    Change-Id: I5b8fbcb19ee512830f90be11defdbb003de3c9f7
    Reviewed-on: https://chromium-review.googlesource.com/622465
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496467}

--- third_party/WebKit/LayoutTests/inspector-protocol/css/css-get-background-colors.js
+++ third_party/WebKit/LayoutTests/inspector-protocol/css/css-get-background-colors.js
@@ -122,5 +122,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       testRunner.log('Video behind text: should be empty array');
       await testGetBgColors('.video > p');
     },
+    async function testShadowDOM() {
+      testRunner.log('Background color in Shadow DOM: should be blue');
+      await testGetBgColors('.shadowDOM > .shadowHost > p');
+    },
   ]);
 });
