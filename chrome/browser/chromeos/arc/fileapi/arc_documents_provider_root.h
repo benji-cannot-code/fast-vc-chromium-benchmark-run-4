3d872f291573655aa59b6b4098d9991dfaec8911
Shuhei Takahashi <nya@chromium.org>
2017-08-22 08:08:22 +0000

    recent: Process ReadDirectoryInternal() calls in batch.
    This performance improvement is good to have, especially for the case
    where the cache expires while a bunch of GetFileInfo() calls are made
    in parallel.
    
    Bug: 742722
    Test: unit_tests
    Change-Id: I8fc1fe13008296a6d5d4d1667efee64296684806
    Reviewed-on: https://chromium-review.googlesource.com/624768
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496247}

--- chrome/browser/chromeos/arc/fileapi/arc_documents_provider_root.h
+++ chrome/browser/chromeos/arc/fileapi/arc_documents_provider_root.h
@@ -42,12 +42,12 @@ class ArcDocumentsProviderRoot : public ArcFileSystemOperationRunner::Observer {
     base::Time last_modified;
   };
 
+  // TODO(crbug.com/755451): Use OnceCallback/RepeatingCallback.
   using GetFileInfoCallback = storage::AsyncFileUtil::GetFileInfoCallback;
   using ReadDirectoryCallback =
       base::OnceCallback<void(base::File::Error error,
                               std::vector<ThinFileInfo> files)>;
   using ChangeType = storage::WatcherManager::ChangeType;
-  // TODO(nya): Use OnceCallback/RepeatingCallback.
   using WatcherCallback = base::Callback<void(ChangeType type)>;
   using StatusCallback = base::Callback<void(base::File::Error error)>;
   using ResolveToContentUrlCallback =
@@ -144,6 +144,7 @@ class ArcDocumentsProviderRoot : public ArcFileSystemOperationRunner::Observer {
   using NameToThinDocumentMap =
       std::map<base::FilePath::StringType, ThinDocument>;
 
+  // TODO(nya): Use OnceCallback.
   using ResolveToDocumentIdCallback =
       base::Callback<void(const std::string& document_id)>;
   using ReadDirectoryInternalCallback =
@@ -207,7 +208,6 @@ class ArcDocumentsProviderRoot : public ArcFileSystemOperationRunner::Observer {
                              const ReadDirectoryInternalCallback& callback);
   void ReadDirectoryInternalWithChildDocuments(
       const std::string& document_id,
-      const ReadDirectoryInternalCallback& callback,
       base::Optional<std::vector<mojom::DocumentPtr>> maybe_children);
 
   // Clears a directory cache.
@@ -225,6 +225,11 @@ class ArcDocumentsProviderRoot : public ArcFileSystemOperationRunner::Observer {
 
   // Cache of directory contents. Keys are document IDs of directories.
   std::map<std::string, DirectoryCache> directory_cache_;
+
+  // Map from a document ID to callbacks pending for ReadDirectoryInternal()
+  // calls.
+  std::map<std::string, std::vector<ReadDirectoryInternalCallback>>
+      pending_callbacks_map_;
 
   // Map from a file path to a watcher data.
   //
