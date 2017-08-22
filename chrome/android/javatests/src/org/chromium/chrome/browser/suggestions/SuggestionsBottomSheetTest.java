aa1d25300340e7224d7408d33cb26f046ba08613
Michael van Ouwerkerk <mvanouwerkerk@google.com>
2017-08-22 10:37:30 +0000

    Enable Chrome Home Modern layout by default.
    The Modern feature only has an effect when Chrome Home is enabled.
    
    Bug: 756377
    Change-Id: I940490b3bf1e3b98ab62649464f005c53ab45a27
    Reviewed-on: https://chromium-review.googlesource.com/618989
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Ted Choc (OOO 8.21-25) <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496273}

--- chrome/android/javatests/src/org/chromium/chrome/browser/suggestions/SuggestionsBottomSheetTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/suggestions/SuggestionsBottomSheetTest.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.suggestions;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -42,6 +41,7 @@ import org.chromium.content.browser.test.util.TestTouchUtils;
 public class SuggestionsBottomSheetTest {
     @Rule
     public BottomSheetTestRule mActivityRule = new BottomSheetTestRule();
+
     @Rule
     public SuggestionsDependenciesRule createSuggestions() {
         return new SuggestionsDependenciesRule(NtpUiCaptureTestData.createFactory());
@@ -57,26 +57,21 @@ public class SuggestionsBottomSheetTest {
     @MediumTest
     public void testContextMenu() throws InterruptedException {
         SuggestionsRecyclerView recyclerView =
-                (SuggestionsRecyclerView) mActivityRule.getBottomSheetContent()
-                        .getContentView()
-                        .findViewById(R.id.recycler_view);
+                mActivityRule.getBottomSheetContent().getContentView().findViewById(
+                        R.id.recycler_view);
 
-        ViewHolder firstCardViewHolder = RecyclerViewTestUtils.waitForView(recyclerView, 2);
-        assertEquals(firstCardViewHolder.getItemViewType(), ItemViewType.SNIPPET);
+        int suggestionPosition =
+                recyclerView.getNewTabPageAdapter().getFirstPositionForType(ItemViewType.SNIPPET);
+        ViewHolder suggestionViewHolder =
+                RecyclerViewTestUtils.scrollToView(recyclerView, suggestionPosition);
 
         assertFalse(mActivityRule.getBottomSheet().onInterceptTouchEvent(createTapEvent()));
 
         TestTouchUtils.longClickView(
-                InstrumentationRegistry.getInstrumentation(), firstCardViewHolder.itemView);
+                InstrumentationRegistry.getInstrumentation(), suggestionViewHolder.itemView);
         assertTrue(mActivityRule.getBottomSheet().onInterceptTouchEvent(createTapEvent()));
 
-        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
-            @Override
-            public void run() {
-                mActivityRule.getActivity().closeContextMenu();
-            }
-        });
-
+        ThreadUtils.runOnUiThreadBlocking(mActivityRule.getActivity()::closeContextMenu);
         assertFalse(mActivityRule.getBottomSheet().onInterceptTouchEvent(createTapEvent()));
     }
 
