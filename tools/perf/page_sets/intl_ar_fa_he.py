b7533a719bcdd1f3e68a11dbec2162afcf3e8238
David Bokan <bokan@chromium.org>
2017-08-22 18:54:00 +0000

    Removed key_mobile_sites and dead code
    key_mobile_sites is redundant with key_mobile_sites_smooth. The
    Page.archive_data_file member is unused so I've removed it, for
    both key_mobile_sites and all other page sets.
    
    Bug: 756119, 730036
    Change-Id: I7d25d50d50bc8e6eaec9069d644052ec9f58adc5
    Reviewed-on: https://chromium-review.googlesource.com/626336
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496390}

--- tools/perf/page_sets/intl_ar_fa_he.py
+++ tools/perf/page_sets/intl_ar_fa_he.py
@@ -17,7 +17,6 @@ class IntlArFaHePage(page_cycler_story.PageCyclerStory):
         shared_page_state_class=shared_page_state.SharedDesktopPageState,
         cache_temperature=cache_temperature,
         name=url)
-    self.archive_data_file = 'data/intl_ar_fa_he.json'
 
 
 class IntlArFaHePageSet(story.StorySet):
