7ffdc0d838cab704b61707ce5c63068e25b71a3b
Charles Harrison <csharrison@chromium.org>
2017-08-22 01:04:17 +0000

    Extensions: properly check the extension URL for background permission
    Minimal CL for easy merging.
    
    Bug: 756465
    Change-Id: I63ecf2c0fed4af8ee5071a617a4e073b03a51395
    Reviewed-on: https://chromium-review.googlesource.com/624439
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Nick Carter <nick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496149}

--- chrome/browser/extensions/app_background_page_apitest.cc
+++ chrome/browser/extensions/app_background_page_apitest.cc
@@ -478,6 +478,8 @@ IN_PROC_BROWSER_TEST_F(AppBackgroundPageApiTest, DISABLED_OpenPopupFromBGPage) {
   ASSERT_TRUE(RunExtensionTest("app_background_page/bg_open")) << message_;
 }
 
+// Partly a regression test for crbug.com/756465. Namely, that window.open
+// correctly matches an app URL with a path component.
 IN_PROC_BROWSER_TEST_F(AppBackgroundPageApiTest, OpenThenClose) {
   std::string app_manifest = base::StringPrintf(
       "{"
@@ -486,10 +488,10 @@ IN_PROC_BROWSER_TEST_F(AppBackgroundPageApiTest, OpenThenClose) {
       "  \"manifest_version\": 2,"
       "  \"app\": {"
       "    \"urls\": ["
-      "      \"http://a.com/\""
+      "      \"http://a.com/extensions/api_test\""
       "    ],"
       "    \"launch\": {"
-      "      \"web_url\": \"http://a.com:%u/\""
+      "      \"web_url\": \"http://a.com:%u/extensions/api_test\""
       "    }"
       "  },"
       "  \"permissions\": [\"background\"]"
