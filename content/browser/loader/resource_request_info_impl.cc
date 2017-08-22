1907df4035ff07d27591deadb8ff59eb5f0a751e
Randy Smith <rdsmith@chromium.org>
2017-08-22 20:37:51 +0000

    Use non-null key for ResourceRequestInfoImpl* attachment to URLRequest.
    ResourceRequestInfoImpl is attached to the URL through the UserData
    mechanism, but the key used for that attachment is nullptr.  That renders
    that attachment vulnerable to collision with other users of UserData.  This
    Cl uses a unique key that is less vulnerable to collision.
    
    This CL is a speculative fix to http://crbug.com/756668 and
    http://crbug.com/754704 which are field crashers that may be caused
    by null ResourceRequestInfoImpl pointers.
    
    Bug: 756668
    Bug: 754704
    Change-Id: I5674b94c7624a584e75a72b547f97c758a42a74e
    Reviewed-on: https://chromium-review.googlesource.com/627119
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Randy Smith <rdsmith@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496431}

--- content/browser/loader/resource_request_info_impl.cc
+++ content/browser/loader/resource_request_info_impl.cc
@@ -28,6 +28,9 @@ int FrameTreeNodeIdFromHostIds(int render_process_host_id,
   return render_frame_host ? render_frame_host->GetFrameTreeNodeId() : -1;
 }
 
+// static
+const void* const kResourceRequestInfoImplKey = &kResourceRequestInfoImplKey;
+
 }  // namespace
 
 // ----------------------------------------------------------------------------
@@ -117,7 +120,8 @@ bool ResourceRequestInfo::OriginatedFromServiceWorker(
 // static
 ResourceRequestInfoImpl* ResourceRequestInfoImpl::ForRequest(
     net::URLRequest* request) {
-  return static_cast<ResourceRequestInfoImpl*>(request->GetUserData(NULL));
+  return static_cast<ResourceRequestInfoImpl*>(
+      request->GetUserData(kResourceRequestInfoImplKey));
 }
 
 // static
@@ -318,7 +322,7 @@ NavigationUIData* ResourceRequestInfoImpl::GetNavigationUIData() const {
 }
 
 void ResourceRequestInfoImpl::AssociateWithRequest(net::URLRequest* request) {
-  request->SetUserData(nullptr, base::WrapUnique(this));
+  request->SetUserData(kResourceRequestInfoImplKey, base::WrapUnique(this));
   int render_process_id;
   int render_frame_id;
   if (GetAssociatedRenderFrame(&render_process_id, &render_frame_id)) {
