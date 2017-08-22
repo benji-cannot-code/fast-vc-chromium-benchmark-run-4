a7cbf1ed0ae764f2f22b2b1495ac8cd2a7fa61f0
Matthew Jones <mdjones@chromium.org>
2017-08-22 15:37:06 +0000

    Avoid setting progress bar layout params if not attached to window
    This is a speculative fix to prevent a class cast exception on the
    progress bar's layout params. The progress bar is always attached to
    a view with MarginLayoutParams, the case where doesn't means it isn't
    attached to the window. This patch checks to make sure the progress
    bar is attached to the window before updating layout params.
    
    BUG=734641
    
    Change-Id: Idafbcdfe5a1971fa7c5dd30150d83ee2b89a88db
    Reviewed-on: https://chromium-review.googlesource.com/624459
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496313}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/ToolbarProgressBar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/ToolbarProgressBar.java
@@ -100,6 +100,9 @@ public class ToolbarProgressBar extends ClipDrawableProgressBar {
      */
     private ToolbarProgressBarAnimatingView mAnimatingView;
 
+    /** Whether or not the progress bar is attached to the window. */
+    private boolean mIsAttachedToWindow;
+
     private final Runnable mHideRunnable = new Runnable() {
         @Override
         public void run() {
@@ -181,15 +184,24 @@ public class ToolbarProgressBar extends ClipDrawableProgressBar {
     public void setTopMargin(int topMargin) {
         mMarginTop = topMargin;
 
-        assert getLayoutParams() != null;
-        ((ViewGroup.MarginLayoutParams) getLayoutParams()).topMargin = mMarginTop;
+        if (mIsAttachedToWindow) {
+            assert getLayoutParams() != null;
+            ((ViewGroup.MarginLayoutParams) getLayoutParams()).topMargin = mMarginTop;
+        }
     }
 
     @Override
     public void onAttachedToWindow() {
         super.onAttachedToWindow();
+        mIsAttachedToWindow = true;
 
         ((ViewGroup.MarginLayoutParams) getLayoutParams()).topMargin = mMarginTop;
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        mIsAttachedToWindow = false;
     }
 
     /**
