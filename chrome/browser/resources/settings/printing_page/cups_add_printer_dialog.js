41bb210fcff0549e3936931434dce90d259fe79d
xdai <xdai@chromium.org>
2017-08-22 19:03:06 +0000

    [CUPS] Fix the regression issue that "Add" button is disabled.
    The "selectedPrinter" property in "add-printer-discovery-dialog' and
    "newPrinter" property in "add-printer-manually-dialog' are both binded
    to the "newPrinter" property in "settings-cups-add-printer-dialog", thus
    they might interfere with each other unexpectedly. We need to make sure
    the value of the properties are properly reset when switching from one
    add-printer-flow to the other add-printer-flow.
    
    Bug: 757348
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I099a73d60bd02abd5326f3cc26d2e64d5e856f88
    Reviewed-on: https://chromium-review.googlesource.com/624530
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496391}

--- chrome/browser/resources/settings/printing_page/cups_add_printer_dialog.js
+++ chrome/browser/resources/settings/printing_page/cups_add_printer_dialog.js
@@ -109,8 +109,10 @@ Polymer({
     this.$$('add-printer-list').style.maxHeight = kPrinterListFullHeight + 'px';
     this.$.noPrinterMessage.hidden = !!this.discoveredPrinters.length;
 
-    if (!this.discoveredPrinters.length)
+    if (!this.discoveredPrinters.length) {
+      this.selectedPrinter = getEmptyPrinter_();
       this.fire('no-detected-printer');
+    }
   },
 
   /** @private */
@@ -154,6 +156,7 @@ Polymer({
 
   /** @private */
   switchToDiscoveryDialog_: function() {
+    this.newPrinter = getEmptyPrinter_();
     this.$$('add-printer-dialog').close();
     this.fire('open-discovery-printers-dialog');
   },
