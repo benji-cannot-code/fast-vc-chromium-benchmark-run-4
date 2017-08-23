55b11405b095cfd2e96b6d090cdfe185fb5d05e0
wutao <wutao@chromium.org>
2017-08-23 04:49:01 +0000

    CrOS: Add UMA for hiding window animation.
    Add one UMA for hiding window animation smoothness, used in closing
    windows, so that we can evaluate how much improvement we can achieve
    by caching render surface.
    
    Bug: 756739
    TEST: has histogram at local device.
    Change-Id: I11e3ab879e246b034f021caa3340061aec188220
    Reviewed-on: https://chromium-review.googlesource.com/620253
    Reviewed-by: David Reveman <reveman@chromium.org>
    Reviewed-by: Valery Arkhangorodsky <varkha@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496587}

--- ui/wm/core/window_animations.cc
+++ ui/wm/core/window_animations.cc
@@ -12,10 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
+#include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/message_loop/message_loop.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/stl_util.h"
 #include "base/time/time.h"
 #include "ui/aura/client/aura_constants.h"
@@ -143,6 +145,22 @@ class HidingWindowAnimationObserverBase : public aura::WindowObserver {
 
   DISALLOW_COPY_AND_ASSIGN(HidingWindowAnimationObserverBase);
 };
+
+class HidingWindowMetricsReporter : public ui::AnimationMetricsReporter {
+ public:
+  HidingWindowMetricsReporter() = default;
+  ~HidingWindowMetricsReporter() override = default;
+
+  void Report(int value) override {
+    UMA_HISTOGRAM_PERCENTAGE("Ash.Window.AnimationSmoothness.Hide", value);
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(HidingWindowMetricsReporter);
+};
+
+base::LazyInstance<HidingWindowMetricsReporter>::Leaky g_reporter_hide =
+    LAZY_INSTANCE_INITIALIZER;
 
 }  // namespace
 
@@ -302,6 +320,8 @@ void AnimateHideWindowCommon(aura::Window* window,
 
   // Property sets within this scope will be implicitly animated.
   ScopedHidingAnimationSettings hiding_settings(window);
+  hiding_settings.layer_animation_settings()->SetAnimationMetricsReporter(
+      g_reporter_hide.Pointer());
   base::TimeDelta duration = GetWindowVisibilityAnimationDuration(*window);
   if (duration > base::TimeDelta())
     hiding_settings.layer_animation_settings()->SetTransitionDuration(duration);
