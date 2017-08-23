6368b6f7e9f9d0f98d1ba7960e66d57c69e5e40c
Walter Korman <wkorman@chromium.org>
2017-08-23 02:35:21 +0000

    Remove unused LayerTreeHostImpl::AnimationsPreserveAxisAlignment.
    Removes a usage of LayerImpl::element_id(). Originally planned to
    revise this to pass the element id rather than the layer, only to
    find this to be, in fact, dead code.
    
    Bug: 709137
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ic50097ab522f560a1a2bdf916407ac7815ca109c
    Reviewed-on: https://chromium-review.googlesource.com/627023
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496566}

--- cc/trees/layer_tree_host_impl.h
+++ cc/trees/layer_tree_host_impl.h
@@ -274,7 +274,6 @@ class CC_EXPORT LayerTreeHostImpl
   void SetTreeLayerScrollOffsetMutated(ElementId element_id,
                                        LayerTreeImpl* tree,
                                        const gfx::ScrollOffset& scroll_offset);
-  bool AnimationsPreserveAxisAlignment(const LayerImpl* layer) const;
   void SetNeedUpdateGpuRasterizationStatus();
 
   // MutatorHostClient implementation.
