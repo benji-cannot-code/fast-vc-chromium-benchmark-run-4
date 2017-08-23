988a590ab384e29e405e425bbefbe4d22294829d
Xiaohui Chen <xiaohuic@google.com>
2017-08-23 01:09:42 +0000

    Change keyboard shortcuts to toggle voice interaction
    BUG=b/38124462
    TEST=build and flash locally, see keyboard shortcut toggles
    
    Change-Id: Iad6c244d38d9c8140a233d28bf20065bbdda36a1
    Reviewed-on: https://chromium-review.googlesource.com/624901
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496536}

--- ash/shell/example_app_list_presenter.cc
+++ ash/shell/example_app_list_presenter.cc
@@ -69,6 +69,8 @@ void ExampleAppListPresenter::ToggleAppList(int64_t display_id) {
 
 void ExampleAppListPresenter::StartVoiceInteractionSession() {}
 
+void ExampleAppListPresenter::ToggleVoiceInteractionSession() {}
+
 void ExampleAppListPresenter::UpdateYPositionAndOpacity(
     int new_y_position,
     float background_opacity,
