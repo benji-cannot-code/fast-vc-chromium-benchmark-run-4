8e267d5bed9f80f66b5b4d16f759955af63449bb
Fredrik Hubinette <hubbe@google.com>
2017-08-23 00:03:07 +0000

    Reland "Improve suspending logic."
    This is a reland of d2506f13c3fba1e6f3280c5ccb0a6802360aad8b
    Original change's description:
    > Improve suspending logic.
    > 
    > When loading takes too long, we can suspend the codec before
    > we have a chance to load the first frame. This is all normal,
    > but should only happen when we are waiting on the network.
    > This CL makes sure that we have additional progress callbacks
    > expected before suspending.
    > 
    > Bug: 728468
    > Change-Id: I815520e0ba832617b04948f7fbefb88498cf79b7
    > Reviewed-on: https://chromium-review.googlesource.com/614360
    > Reviewed-by: Dan Sanders <sandersd@chromium.org>
    > Commit-Queue: Fredrik Hubinette <hubbe@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#494212}
    
    Bug: 728468
    Change-Id: Ide175e6e5c8d2ecaeeb81b0055352f66c917d099
    Reviewed-on: https://chromium-review.googlesource.com/627616
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Fredrik Hubinette <hubbe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496513}

--- media/blink/webmediaplayer_impl_unittest.cc
+++ media/blink/webmediaplayer_impl_unittest.cc
@@ -271,6 +271,9 @@ class WebMediaPlayerImplTest : public testing::Test {
   }
 
  protected:
+  void SetNetworkState(blink::WebMediaPlayer::NetworkState state) {
+    wmpi_->SetNetworkState(state);
+  }
   void SetReadyState(blink::WebMediaPlayer::ReadyState state) {
     wmpi_->SetReadyState(state);
   }
@@ -428,11 +431,9 @@ TEST_F(WebMediaPlayerImplTest, ConstructAndDestroy) {
   EXPECT_FALSE(IsSuspended());
 }
 
-TEST_F(WebMediaPlayerImplTest, IdleSuspendIsEnabledBeforeLoadingBegins) {
+TEST_F(WebMediaPlayerImplTest, IdleSuspendBeforeLoadingBegins) {
   InitializeWebMediaPlayerImpl();
-  EXPECT_TRUE(delegate_.ExpireForTesting());
-  base::RunLoop().RunUntilIdle();
-  EXPECT_TRUE(IsSuspended());
+  EXPECT_FALSE(delegate_.ExpireForTesting());
 }
 
 TEST_F(WebMediaPlayerImplTest,
@@ -452,6 +453,7 @@ TEST_F(WebMediaPlayerImplTest,
 
 TEST_F(WebMediaPlayerImplTest, IdleSuspendIsEnabledIfLoadingHasStalled) {
   InitializeWebMediaPlayerImpl();
+  SetNetworkState(blink::WebMediaPlayer::kNetworkStateLoading);
   base::SimpleTestTickClock* clock = new base::SimpleTestTickClock();
   clock->Advance(base::TimeDelta::FromSeconds(1));
   SetTickClock(clock);
@@ -467,6 +469,7 @@ TEST_F(WebMediaPlayerImplTest, IdleSuspendIsEnabledIfLoadingHasStalled) {
 TEST_F(WebMediaPlayerImplTest, DidLoadingProgressTriggersResume) {
   // Same setup as IdleSuspendIsEnabledBeforeLoadingBegins.
   InitializeWebMediaPlayerImpl();
+  SetNetworkState(blink::WebMediaPlayer::kNetworkStateLoading);
   EXPECT_TRUE(delegate_.ExpireForTesting());
   base::RunLoop().RunUntilIdle();
   EXPECT_TRUE(IsSuspended());
