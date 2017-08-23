9651a0a6fbf4f26afa7925f1e00c06e01048aed9
Ian Kilpatrick <ikilpatrick@chromium.org>
2017-08-23 10:09:07 +0000

    [LayoutNG] Fix CreatesNewFormattingContext.
    This adds a simple IsBlock() check for NGLayoutInputNode's
    CreatesNewFormattingContext check.
    
    This fixes the case where an NGInlineNode was considered a new formatting
    context for positioning purposes. E.g.
    
    <div id=float>
      text text
    </div>
    
    Bug: 635619
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I6c8d35547979349661b1b05bdcfb5451d928d576
    Reviewed-on: https://chromium-review.googlesource.com/572428
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496646}

--- third_party/WebKit/Source/core/layout/ng/ng_layout_input_node.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_layout_input_node.cc
@@ -84,7 +84,7 @@ bool NGLayoutInputNode::IsQuirkyContainer() const {
 }
 
 bool NGLayoutInputNode::CreatesNewFormattingContext() const {
-  return box_->AvoidsFloats();
+  return IsBlock() && box_->AvoidsFloats();
 }
 
 RefPtr<NGLayoutResult> NGLayoutInputNode::Layout(NGConstraintSpace* space,
