c6b1ac94f66dee8955196c4ed87d14c8ec19607c
Brett Wilson <brettw@chromium.org>
2017-08-23 17:01:24 +0000

    Replace some uses of std::deque with base::circular_deque in components.
    This is roughly half of the uses in //components. Some of the uses require deque and TODOs have been added with the reason why.
    
    #include <algorithm> is added to circular_deque.h for the calls to
    std::max. Previous uses apparently included this some other way.
    
    Some nontrivial updates were made to dependency_graph.cc to simplify the code. In trying to study what this code's requirements were to see if the usage could be replaced, I realized it could be simplified by using the (relatively new) base::Erase function. The constructors that take a range are also used, instead of the existing std::copy with a back_inserter iterator.
    
    Bug: 757232
    TBR: erg@chromium.org
    Reland of https://chromium-review.googlesource.com/622887 which should have been fixed by https://chromium-review.googlesource.com/627017
    
    Change-Id: Iab83d5ec264f65786f39985e8bdfcdaf1e4a2762
    Reviewed-on: https://chromium-review.googlesource.com/627187
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Reviewed-by: Elliot Glaysher <erg@chromium.org>
    Commit-Queue: Brett Wilson <brettw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496716}

--- components/autofill/core/browser/autofill_manager.h
+++ components/autofill/core/browser/autofill_manager.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_AUTOFILL_MANAGER_H_
 #define COMPONENTS_AUTOFILL_CORE_BROWSER_AUTOFILL_MANAGER_H_
 
-#include <deque>
 #include <map>
 #include <memory>
 #include <string>
@@ -14,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_forward.h"
 #include "base/compiler_specific.h"
+#include "base/containers/circular_deque.h"
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
@@ -511,7 +511,7 @@ class AutofillManager : public AutofillHandler,
   // May be NULL.  NULL indicates OTR.
   PersonalDataManager* personal_data_;
 
-  std::deque<std::string> autofilled_form_signatures_;
+  base::circular_deque<std::string> autofilled_form_signatures_;
 
   // Handles queries and uploads to Autofill servers. Will be NULL if
   // the download manager functionality is disabled.
--- components/download/internal/download_service_impl.h
+++ components/download/internal/download_service_impl.h
@@ -6,11 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_DOWNLOAD_INTERNAL_DOWNLOAD_SERVICE_IMPL_H_
 #define COMPONENTS_DOWNLOAD_INTERNAL_DOWNLOAD_SERVICE_IMPL_H_
 
-#include <deque>
 #include <map>
 #include <memory>
 #include <string>
 
+#include "base/containers/circular_deque.h"
 #include "base/macros.h"
 #include "components/download/internal/config.h"
 #include "components/download/internal/service_config_impl.h"
@@ -52,7 +52,7 @@ class DownloadServiceImpl : public DownloadService {
   std::unique_ptr<Controller> controller_;
   ServiceConfigImpl service_config_;
 
-  std::deque<base::Closure> pending_actions_;
+  base::circular_deque<base::Closure> pending_actions_;
   std::map<DownloadTaskType, base::Closure> pending_tasks_;
   bool startup_completed_;
 
