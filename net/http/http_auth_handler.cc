83957e921ede65cee6b15851d5548381f0ee2e77
Scott Graham <scottmg@chromium.org>
2017-08-23 22:36:54 +0000

    TOOD -> TODO
    TBR=jochen@chromium.org
    
    Bug: https://cs.chromium.org/search/?q=%5E(%7C.*%5B%5ETtRrSsEe%5D)(TOOO%7CTOD0%7CTODI%7CTODP%7CTOO%7CTOSO%7CTODDO%7CTOOD%7CTOCO%7CTOTO%7CODO%7CTDOO%7CTODOD%7CTOFO%7CTDDO%7CTODD%7CTOD%7CTODOO%7CTDO%7CTODL)%5CW?%5C(%5Cs*(%5Ba-z%5D%7B3,14%7D)%5Cs*%5C)+case:yes&sq=package:chromium&type=cs
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:closure_compilation;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ie238e22c240bc2842543d6b224163f4c9a7e4e9f
    Reviewed-on: https://chromium-review.googlesource.com/629516
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Scott Graham <scottmg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496837}

--- net/http/http_auth_handler.cc
+++ net/http/http_auth_handler.cc
@@ -103,7 +103,7 @@ void HttpAuthHandler::OnGenerateAuthTokenComplete(int rv) {
 }
 
 void HttpAuthHandler::FinishGenerateAuthToken() {
-  // TOOD(cbentzel): Should this be done in OK case only?
+  // TODO(cbentzel): Should this be done in OK case only?
   net_log_.EndEvent(EventTypeFromAuthTarget(target_));
   callback_.Reset();
 }
--- ui/file_manager/file_manager/foreground/js/volume_manager_wrapper.js
+++ ui/file_manager/file_manager/foreground/js/volume_manager_wrapper.js
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @param {boolean} writableOnly If true, only writable volumes are returned.
  * @param {Window=} opt_backgroundPage Window object of the background
  *     page. If this is specified, the class skips to get background page.
- *     TOOD(hirono): Let all clients of the class pass the background page and
+ *     TODO(hirono): Let all clients of the class pass the background page and
  *     make the argument not optional.
  */
 function VolumeManagerWrapper(allowedPaths, writableOnly, opt_backgroundPage) {
