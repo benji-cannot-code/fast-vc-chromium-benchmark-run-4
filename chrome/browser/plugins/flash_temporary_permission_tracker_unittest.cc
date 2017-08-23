ac51dd4d24af7cf08b3c591dbb18bb0eac17c9ef
clamy <clamy@chromium.org>
2017-08-23 16:11:37 +0000

    Remove SimulateNavigationCommit from content/public API
    This CL removes RenderFrameHostTester::SimulateNavigationCommit from the
    content/ public API. Use NavigationSimulator to simulate navigations
    instead.
    
    BUG=728571
    
    Change-Id: I691df4385c202f261d0f543b3cc11b9e59038946
    Reviewed-on: https://chromium-review.googlesource.com/623809
    Commit-Queue: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496699}

--- chrome/browser/plugins/flash_temporary_permission_tracker_unittest.cc
+++ chrome/browser/plugins/flash_temporary_permission_tracker_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/testing_profile.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/test/navigation_simulator.h"
 #include "content/public/test/test_renderer_host.h"
 #include "content/public/test/web_contents_tester.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -33,23 +34,16 @@ class FlashTemporaryPermissionTrackerTest
   FlashTemporaryPermissionTracker* tracker() { return tracker_.get(); }
 
   content::RenderFrameHost* GetMainRFH(const char* origin) {
-    content::RenderFrameHost* result = web_contents()->GetMainFrame();
-    content::RenderFrameHostTester::For(result)
-        ->InitializeRenderFrameIfNeeded();
-    content::RenderFrameHostTester::For(result)->SimulateNavigationCommit(
-        GURL(origin));
-    return result;
+    return content::NavigationSimulator::NavigateAndCommitFromDocument(
+        GURL(origin), web_contents()->GetMainFrame());
   }
 
   content::RenderFrameHost* AddChildRFH(content::RenderFrameHost* parent,
                                         const char* origin) {
-    content::RenderFrameHost* result =
+    content::RenderFrameHost* subframe =
         content::RenderFrameHostTester::For(parent)->AppendChild("");
-    content::RenderFrameHostTester::For(result)
-        ->InitializeRenderFrameIfNeeded();
-    content::RenderFrameHostTester::For(result)->SimulateNavigationCommit(
-        GURL(origin));
-    return result;
+    return content::NavigationSimulator::NavigateAndCommitFromDocument(
+        GURL(origin), subframe);
   }
 
  private:
@@ -85,8 +79,8 @@ TEST_F(FlashTemporaryPermissionTrackerTest, GrantSurvivesNavigations) {
   EXPECT_TRUE(tracker()->IsFlashEnabled(GURL(kOrigin1)));
 
   // Navigate to another origin. Flash should still be enabled.
-  content::RenderFrameHostTester::For(rfh)->SimulateNavigationCommit(
-      GURL(kOrigin2));
+  content::NavigationSimulator::NavigateAndCommitFromDocument(GURL(kOrigin2),
+                                                              rfh);
   EXPECT_TRUE(tracker()->IsFlashEnabled(GURL(kOrigin1)));
 }
 
@@ -100,8 +94,8 @@ TEST_F(FlashTemporaryPermissionTrackerTest,
   EXPECT_TRUE(tracker()->IsFlashEnabled(GURL(kOrigin1)));
 
   // Navigate the child frame. Flash should still be enabled after this.
-  content::RenderFrameHostTester::For(child)->SimulateNavigationCommit(
-      GURL(kOrigin3));
+  content::NavigationSimulator::NavigateAndCommitFromDocument(GURL(kOrigin3),
+                                                              child);
   EXPECT_TRUE(tracker()->IsFlashEnabled(GURL(kOrigin1)));
 }
 
