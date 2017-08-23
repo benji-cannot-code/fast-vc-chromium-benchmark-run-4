d55482e68fccd85ceb6b9ea9e79a5689e14902cb
Mohamad Ahmadi <mahmadi@chromium.org>
2017-08-23 20:41:43 +0000

    [Payment Request] Disables Cancel button only once the response is returned
    Prior to this patch, the Cancel button was being disabled as soon as the
    payment method details were received but before the response was ready.
    Preparing the response may require normalizing addresses which is an async
    operation. User should be able to still cancel the payment during this
    period. After this patch, the Cancel button is disabled only when the
    response is ready to be sent off to the merchant.
    
    Bug: 758153
    Change-Id: I1c454d48604039f3273b34da7597b246dd2f4c44
    Reviewed-on: https://chromium-review.googlesource.com/629836
    Reviewed-by: Marc-Antoine Courteau <macourteau@chromium.org>
    Commit-Queue: Marc-Antoine Courteau <macourteau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496796}

--- ios/chrome/browser/ui/payments/payment_request_manager.mm
+++ ios/chrome/browser/ui/payments/payment_request_manager.mm
@@ -918,7 +918,6 @@ requestFullCreditCard:(const autofill::CreditCard&)creditCard
 
 - (void)paymentResponseHelperDidReceivePaymentMethodDetails {
   [_paymentRequestCoordinator setPending:YES];
-  [_paymentRequestCoordinator setCancellable:NO];
 }
 
 - (void)paymentResponseHelperDidFailToReceivePaymentMethodDetails {
@@ -928,6 +927,8 @@ requestFullCreditCard:(const autofill::CreditCard&)creditCard
 
 - (void)paymentResponseHelperDidCompleteWithPaymentResponse:
     (const web::PaymentResponse&)paymentResponse {
+  [_paymentRequestCoordinator setCancellable:NO];
+
   [_paymentRequestJsManager
       resolveRequestPromiseWithPaymentResponse:paymentResponse
                              completionHandler:nil];
