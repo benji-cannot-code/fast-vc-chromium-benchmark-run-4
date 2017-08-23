e86bf5f0782c6dedb90a4d70a4d9f02478f53a54
Greg Thompson <grt@chromium.org>
2017-08-23 06:26:42 +0000

    Fix browser registration for per-user dev and beta installs on Win7.
    This change also makes the handling of the make_chrome_default and
    make_chrome_default_for_user master preferences options conditional on a
    brand's support for in-product default browser handling.
    
    BUG=752007
    R=pmonette@chromium.org
    
    Change-Id: I1eb6d4344aa845943e726b69cf2319e6cf056cb9
    Reviewed-on: https://chromium-review.googlesource.com/625896
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496600}

--- chrome/installer/setup/setup_main.cc
+++ chrome/installer/setup/setup_main.cc
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/install_static/install_details.h"
+#include "chrome/install_static/install_util.h"
 #include "chrome/installer/setup/archive_patch_helper.h"
 #include "chrome/installer/setup/install.h"
 #include "chrome/installer/setup/install_worker.h"
@@ -1397,21 +1398,26 @@ int WINAPI wWinMain(HINSTANCE instance, HINSTANCE prev_instance,
     return installer::OS_ERROR;
   }
 
-  const install_static::InstallDetails& install_details =
-      install_static::InstallDetails::Get();
   // Make sure system_level is supported if requested. For historical reasons,
   // system-level installs have never been supported for Chrome canary (SxS).
   // This is a brand-specific policy for this particular mode. In general,
   // system-level installation of secondary install modes is fully supported.
-  if (system_install && !install_details.supports_system_level())
+  if (!install_static::InstallDetails::Get().supports_system_level() &&
+      (system_install ||
+       cmd_line.HasSwitch(installer::switches::kSelfDestruct) ||
+       cmd_line.HasSwitch(installer::switches::kRemoveChromeRegistration))) {
     return installer::SXS_OPTION_NOT_SUPPORTED;
-  // Some command line options don't work with secondary installs.
-  if (!install_details.is_primary_mode() &&
-      (cmd_line.HasSwitch(installer::switches::kSelfDestruct) ||
-       cmd_line.HasSwitch(installer::switches::kMakeChromeDefault) ||
-       cmd_line.HasSwitch(installer::switches::kRegisterChromeBrowser) ||
-       cmd_line.HasSwitch(installer::switches::kRemoveChromeRegistration) ||
-       cmd_line.HasSwitch(installer::switches::kInactiveUserToast) ||
+  }
+  // Some switches only apply for modes that can be made the user's default
+  // browser.
+  if (!install_static::SupportsSetAsDefaultBrowser() &&
+      (cmd_line.HasSwitch(installer::switches::kMakeChromeDefault) ||
+       cmd_line.HasSwitch(installer::switches::kRegisterChromeBrowser))) {
+    return installer::SXS_OPTION_NOT_SUPPORTED;
+  }
+  // Some switches only apply for modes that support retention experiments.
+  if (!install_static::SupportsRetentionExperiments() &&
+      (cmd_line.HasSwitch(installer::switches::kInactiveUserToast) ||
        cmd_line.HasSwitch(installer::switches::kSystemLevelToast))) {
     return installer::SXS_OPTION_NOT_SUPPORTED;
   }
