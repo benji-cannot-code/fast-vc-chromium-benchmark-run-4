376d326ad97a97b1d91e86cc262f37f35a7c1e05
Evan Stade <estade@chromium.org>
2017-08-23 17:06:18 +0000

    Re-land ad2949ac9cb3919d1f3866f1 - Count a WebContents as "no longer waiting for response" after receiving a DidGetResourceResponseStart callback.
    Skia memory regression on Windows is incidental (due to loading more of
    Segoe UI into memory).
    
    TBR=creis@chromium.org
    
    Bug: 752326
    Change-Id: I0e01331b8299ead239e9b1dc829fbe8810d92ced
    Reviewed-on: https://chromium-review.googlesource.com/627043
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496718}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -3431,6 +3431,7 @@ void WebContentsImpl::LoadStateChanged(
 
 void WebContentsImpl::DidGetResourceResponseStart(
   const ResourceRequestDetails& details) {
+  SetNotWaitingForResponse();
   controller_.ssl_manager()->DidStartResourceResponse(
       details.url, details.has_certificate, details.ssl_cert_status);
 
