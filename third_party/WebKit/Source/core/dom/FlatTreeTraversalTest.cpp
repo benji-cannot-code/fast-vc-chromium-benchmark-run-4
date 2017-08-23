d32ead10b600c607e164c481dda4989aa24adeeb
Eriko Kurimoto <elkurin@google.com>
2017-08-23 06:48:57 +0000

    Rename openShadowRoot to OpenShadowRoot
    Bug: 
    Change-Id: I87ca0541e15a803d7939a138dc0d4a844182a2e4
    Reviewed-on: https://chromium-review.googlesource.com/618430
    Commit-Queue: Eriko Kurimoto <elkurin@google.com>
    Reviewed-by: Takayoshi Kochi <kochi@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496602}

--- third_party/WebKit/Source/core/dom/FlatTreeTraversalTest.cpp
+++ third_party/WebKit/Source/core/dom/FlatTreeTraversalTest.cpp
@@ -130,7 +130,7 @@ TEST_F(FlatTreeTraversalTest, childAt) {
   Element* m01 = m0->QuerySelector("#m01");
 
   Element* shadow_host = m0;
-  ShadowRoot* shadow_root = shadow_host->openShadowRoot();
+  ShadowRoot* shadow_root = shadow_host->OpenShadowRoot();
   Element* s00 = shadow_root->QuerySelector("#s00");
   Element* s02 = shadow_root->QuerySelector("#s02");
   Element* s03 = shadow_root->QuerySelector("#s03");
@@ -215,7 +215,7 @@ TEST_F(FlatTreeTraversalTest, commonAncestor) {
   Element* m20 = body->QuerySelector("#m20");
   Element* m21 = body->QuerySelector("#m21");
 
-  ShadowRoot* shadow_root = m1->openShadowRoot();
+  ShadowRoot* shadow_root = m1->OpenShadowRoot();
   Element* s10 = shadow_root->QuerySelector("#s10");
   Element* s11 = shadow_root->QuerySelector("#s11");
   Element* s12 = shadow_root->QuerySelector("#s12");
@@ -280,7 +280,7 @@ TEST_F(FlatTreeTraversalTest, nextSkippingChildren) {
   Element* m10 = body->QuerySelector("#m10");
   Element* m11 = body->QuerySelector("#m11");
 
-  ShadowRoot* shadow_root = m1->openShadowRoot();
+  ShadowRoot* shadow_root = m1->OpenShadowRoot();
   Element* s11 = shadow_root->QuerySelector("#s11");
   Element* s12 = shadow_root->QuerySelector("#s12");
   Element* s120 = shadow_root->QuerySelector("#s120");
@@ -336,7 +336,7 @@ TEST_F(FlatTreeTraversalTest, lastWithin) {
 
   Element* m10 = body->QuerySelector("#m10");
 
-  ShadowRoot* shadow_root = m1->openShadowRoot();
+  ShadowRoot* shadow_root = m1->OpenShadowRoot();
   Element* s11 = shadow_root->QuerySelector("#s11");
   Element* s12 = shadow_root->QuerySelector("#s12");
 
@@ -381,7 +381,7 @@ TEST_F(FlatTreeTraversalTest, previousPostOrder) {
   Element* m10 = body->QuerySelector("#m10");
   Element* m11 = body->QuerySelector("#m11");
 
-  ShadowRoot* shadow_root = m1->openShadowRoot();
+  ShadowRoot* shadow_root = m1->OpenShadowRoot();
   Element* s11 = shadow_root->QuerySelector("#s11");
   Element* s12 = shadow_root->QuerySelector("#s12");
   Element* s120 = shadow_root->QuerySelector("#s120");
@@ -449,12 +449,12 @@ TEST_F(FlatTreeTraversalTest, redistribution) {
   Element* m1 = body->QuerySelector("#m1");
   Element* m10 = body->QuerySelector("#m10");
 
-  ShadowRoot* shadow_root1 = m1->openShadowRoot();
+  ShadowRoot* shadow_root1 = m1->OpenShadowRoot();
   Element* s1 = shadow_root1->QuerySelector("#s1");
 
   AttachV0ShadowRoot(*s1, shadow_html2);
 
-  ShadowRoot* shadow_root2 = s1->openShadowRoot();
+  ShadowRoot* shadow_root2 = s1->OpenShadowRoot();
   Element* s21 = shadow_root2->QuerySelector("#s21");
 
   EXPECT_EQ(s21, FlatTreeTraversal::NextSibling(*m10));
@@ -487,7 +487,7 @@ TEST_F(FlatTreeTraversalTest, v1Simple) {
   Element* child2 = body->QuerySelector("#child2");
 
   AttachOpenShadowRoot(*host, shadow_html);
-  ShadowRoot* shadow_root = host->openShadowRoot();
+  ShadowRoot* shadow_root = host->OpenShadowRoot();
   Element* slot1 = shadow_root->QuerySelector("[name=slot1]");
   Element* slot2 = shadow_root->QuerySelector("[name=slot2]");
   Element* shadow_child1 = shadow_root->QuerySelector("#shadow-child1");
@@ -536,7 +536,7 @@ TEST_F(FlatTreeTraversalTest, v1Redistribution) {
   Element* d6 = body->QuerySelector("#d6");
 
   AttachOpenShadowRoot(*d1, shadow_html1);
-  ShadowRoot* shadow_root1 = d1->openShadowRoot();
+  ShadowRoot* shadow_root1 = d1->OpenShadowRoot();
   Element* d11 = shadow_root1->QuerySelector("#d1-1");
   Element* d12 = shadow_root1->QuerySelector("#d1-2");
   Element* d13 = shadow_root1->QuerySelector("#d1-3");
@@ -546,7 +546,7 @@ TEST_F(FlatTreeTraversalTest, v1Redistribution) {
   Element* d1s2 = shadow_root1->QuerySelector("[name=d1-s2]");
 
   AttachOpenShadowRoot(*d11, shadow_html2);
-  ShadowRoot* shadow_root2 = d11->openShadowRoot();
+  ShadowRoot* shadow_root2 = d11->OpenShadowRoot();
   Element* d111 = shadow_root2->QuerySelector("#d1-1-1");
   Element* d112 = shadow_root2->QuerySelector("#d1-1-2");
   Element* d11s1 = shadow_root2->QuerySelector("[name=d1-1-s1]");
@@ -615,7 +615,7 @@ TEST_F(FlatTreeTraversalTest, v1FallbackContent) {
   Element* d1 = body->QuerySelector("#d1");
 
   AttachOpenShadowRoot(*d1, shadow_html);
-  ShadowRoot* shadow_root = d1->openShadowRoot();
+  ShadowRoot* shadow_root = d1->OpenShadowRoot();
   Element* before = shadow_root->QuerySelector("#before");
   Element* after = shadow_root->QuerySelector("#after");
   Element* fallback_content = shadow_root->QuerySelector("p");
@@ -647,7 +647,7 @@ TEST_F(FlatTreeTraversalTest, v1FallbackContentSkippedInTraversal) {
   Element* span = body->QuerySelector("span");
 
   AttachOpenShadowRoot(*d1, shadow_html);
-  ShadowRoot* shadow_root = d1->openShadowRoot();
+  ShadowRoot* shadow_root = d1->OpenShadowRoot();
   Element* before = shadow_root->QuerySelector("#before");
   Element* after = shadow_root->QuerySelector("#after");
   Element* fallback_content = shadow_root->QuerySelector("p");
@@ -682,7 +682,7 @@ TEST_F(FlatTreeTraversalTest, v1AllFallbackContent) {
   Element* d1 = body->QuerySelector("#d1");
 
   AttachOpenShadowRoot(*d1, shadow_html);
-  ShadowRoot* shadow_root = d1->openShadowRoot();
+  ShadowRoot* shadow_root = d1->OpenShadowRoot();
   Element* fallback_x = shadow_root->QuerySelector("#x");
   Element* fallback_y = shadow_root->QuerySelector("#y");
   Element* fallback_z = shadow_root->QuerySelector("#z");
--- third_party/WebKit/Source/core/html/TextControlElement.cpp
+++ third_party/WebKit/Source/core/html/TextControlElement.cpp
@@ -271,7 +271,7 @@ void TextControlElement::setRangeText(const String& replacement,
                              String::Number(end) + ").");
     return;
   }
-  if (openShadowRoot())
+  if (OpenShadowRoot())
     return;
 
   String text = InnerEditorValue();
@@ -399,7 +399,7 @@ bool TextControlElement::SetSelectionRange(
     unsigned start,
     unsigned end,
     TextFieldSelectionDirection direction) {
-  if (openShadowRoot() || !IsTextControl())
+  if (OpenShadowRoot() || !IsTextControl())
     return false;
   const unsigned editor_value_length = InnerEditorValue().length();
   end = std::min(end, editor_value_length);
@@ -798,8 +798,8 @@ void TextControlElement::AddPlaceholderBreakElementIfNecessary() {
 }
 
 void TextControlElement::SetInnerEditorValue(const String& value) {
-  DCHECK(!openShadowRoot());
-  if (!IsTextControl() || openShadowRoot())
+  DCHECK(!OpenShadowRoot());
+  if (!IsTextControl() || OpenShadowRoot())
     return;
 
   bool text_is_changed = value != InnerEditorValue();
@@ -829,7 +829,7 @@ void TextControlElement::SetInnerEditorValue(const String& value) {
 }
 
 String TextControlElement::InnerEditorValue() const {
-  DCHECK(!openShadowRoot());
+  DCHECK(!OpenShadowRoot());
   HTMLElement* inner_editor = InnerEditorElement();
   if (!inner_editor || !IsTextControl())
     return g_empty_string;
