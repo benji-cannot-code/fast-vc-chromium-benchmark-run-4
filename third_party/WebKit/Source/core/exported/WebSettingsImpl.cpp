a2b2ae8858461094e615acbb1778071ea67a240c
Dave Tapuska <dtapuska@chromium.org>
2017-08-23 16:28:27 +0000

    Add setting for barrel button drag.
    To enable the exact behaviour before changing the direct manipulation
    stylus allow the user to turn off barrel button drag for selection.
    
    BUG=746581
    
    Change-Id: I24312e9af4158a026cd8a255d045a8814713a764
    Reviewed-on: https://chromium-review.googlesource.com/624715
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496705}

--- third_party/WebKit/Source/core/exported/WebSettingsImpl.cpp
+++ third_party/WebKit/Source/core/exported/WebSettingsImpl.cpp
@@ -431,6 +431,10 @@ void WebSettingsImpl::SetTouchDragDropEnabled(bool enabled) {
   settings_->SetTouchDragDropEnabled(enabled);
 }
 
+void WebSettingsImpl::SetBarrelButtonForDragEnabled(bool enabled) {
+  settings_->SetBarrelButtonForDragEnabled(enabled);
+}
+
 void WebSettingsImpl::SetOfflineWebApplicationCacheEnabled(bool enabled) {
   settings_->SetOfflineWebApplicationCacheEnabled(enabled);
 }
