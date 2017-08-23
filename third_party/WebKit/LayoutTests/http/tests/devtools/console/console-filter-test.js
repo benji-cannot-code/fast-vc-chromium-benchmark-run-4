2f51b04a84b3dccb138632d3f90dcce3114a982b
Erik Luo <luoe@chromium.org>
2017-08-23 00:54:18 +0000

    DevTools: have console filter use query parser
    Enables the use of 'url:foo.js' style queries in the console filter bar,
    once exclusive to Network panel's filter. Also brings over the ability
    to make negative queries:
    -foo
    foo -/bar*/
    
    Screenshot: http://imgur.com/a/RAPUP
    
    Bug: 685283
    Change-Id: Ib79f68649df92714740aea1f1c36eeb1331f19cc
    Reviewed-on: https://chromium-review.googlesource.com/625236
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496532}

--- third_party/WebKit/LayoutTests/http/tests/devtools/console/console-filter-test.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/console/console-filter-test.js
@@ -110,6 +110,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       dumpVisibleMessages();
       next();
     },
+    function checkMultiTextFilter(next)
+    {
+      Console.ConsoleView.instance()._filter._textFilterUI.setValue("Group /[2-3]top/");
+      Console.ConsoleView.instance()._filter._textFilterChanged();
+      dumpVisibleMessages();
+      next();
+    },
+    function checkTextUrlFilter(next)
+    {
+      Console.ConsoleView.instance()._filter._textFilterUI.setValue("url:log-source");
+      Console.ConsoleView.instance()._filter._textFilterChanged();
+      dumpVisibleMessages();
+      next();
+    },
+    function checkNegativeTextUrlFilter(next)
+    {
+      Console.ConsoleView.instance()._filter._textFilterUI.setValue("-url:log-source");
+      Console.ConsoleView.instance()._filter._textFilterChanged();
+      dumpVisibleMessages();
+      next();
+    },
     function checkResetFilter(next) {
       Console.ConsoleView.instance()._filter.reset();
       dumpVisibleMessages();
--- third_party/WebKit/Source/devtools/front_end/text_utils/TextUtils.js
+++ third_party/WebKit/Source/devtools/front_end/text_utils/TextUtils.js
@@ -200,7 +200,7 @@ TextUtils.FilterParser = class {
 
   /**
    * @param {string} query
-   * @return {!Array<{key:(string|undefined), text:(string|undefined), regex:(!RegExp|undefined), negative:boolean}>}
+   * @return {!Array<!TextUtils.FilterParser.ParsedFilter>}
    */
   parse(query) {
     var splitResult = TextUtils.TextUtils.splitStringByRegexes(query, [
@@ -231,6 +231,9 @@ TextUtils.FilterParser = class {
     return filters;
   }
 };
+
+/** @typedef {{key:(string|undefined), text:(string|undefined), regex:(!RegExp|undefined), negative:boolean}} */
+TextUtils.FilterParser.ParsedFilter;
 
 TextUtils.TextUtils._keyValueFilterRegex = /(?:^|\s)(\-)?([\w\-]+):([^\s]+)/;
 TextUtils.TextUtils._regexFilterRegex = /(?:^|\s)(\-)?\/([^\s]+)\//;
