2211de91e515e5c971d3b382eff260c99ac382f7
Sky Malice <skym@chromium.org>
2017-08-23 19:43:43 +0000

    Reland "Componentize BrowsingHistoryService."
    This change moves BrowsingHistoryService from chrome/browser/history/
    to components/history/core/browser/. The hope is that this will allow
    iOS to eventually call into BrowsingHistoryService. To do this we
    needed abstract away a few dependencies, which was done by increasing
    the responsibility of the BrowsingHistoryServiceHandler.
    
    A base class of ProfileBasedHistoryHandler was created to hold logic
    that used to live in BrowsingHistoryService, and both desktop and
    Android still want, but iOS cannot use.
    
    Restored a null check that caused crashes in the original version of
    this CL and added unit tests to catch similar regressions.
    
    TBR=tommycli@chromium.org,twellington@chromium.org,dominickn@chromium.org
    
    Bug: 756097
    Change-Id: I23ebb740f1969b57e4e18be7fcf7745a11ef31eb
    Reviewed-on: https://chromium-review.googlesource.com/626776
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496772}

--- chrome/browser/ui/webui/browsing_history_handler.h
+++ chrome/browser/ui/webui/browsing_history_handler.h
@@ -16,13 +16,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/time/clock.h"
 #include "base/values.h"
-#include "chrome/browser/history/browsing_history_service_handler.h"
+#include "chrome/browser/history/profile_based_browsing_history_driver.h"
 #include "content/public/browser/web_ui_message_handler.h"
 
 // The handler for Javascript messages related to the "history" view.
-class BrowsingHistoryHandler :
-    public content::WebUIMessageHandler,
-    public BrowsingHistoryServiceHandler {
+class BrowsingHistoryHandler : public content::WebUIMessageHandler,
+                               public ProfileBasedBrowsingHistoryDriver {
  public:
   BrowsingHistoryHandler();
   ~BrowsingHistoryHandler() override;
@@ -42,15 +41,19 @@ class BrowsingHistoryHandler :
   // Handler for "removeBookmark" message.
   void HandleRemoveBookmark(const base::ListValue* args);
 
-  // BrowsingHistoryServiceHandler implementation.
+  // BrowsingHistoryDriver implementation.
   void OnQueryComplete(
-      std::vector<BrowsingHistoryService::HistoryEntry>* results,
-      BrowsingHistoryService::QueryResultsInfo* query_results_info) override;
+      const std::vector<history::BrowsingHistoryService::HistoryEntry>& results,
+      const history::BrowsingHistoryService::QueryResultsInfo&
+          query_results_info) override;
   void OnRemoveVisitsComplete() override;
   void OnRemoveVisitsFailed() override;
   void HistoryDeleted() override;
   void HasOtherFormsOfBrowsingHistory(
       bool has_other_forms, bool has_synced_results) override;
+
+  // ProfileBasedBrowsingHistoryDriver implementation.
+  Profile* GetProfile() override;
 
   // For tests.
   void set_clock(std::unique_ptr<base::Clock> clock) {
@@ -65,7 +68,7 @@ class BrowsingHistoryHandler :
   // The clock used to vend times.
   std::unique_ptr<base::Clock> clock_;
 
-  std::unique_ptr<BrowsingHistoryService> browsing_history_service_;
+  std::unique_ptr<history::BrowsingHistoryService> browsing_history_service_;
 
   DISALLOW_COPY_AND_ASSIGN(BrowsingHistoryHandler);
 };
--- chrome/browser/ui/webui/browsing_history_handler_unittest.cc
+++ chrome/browser/ui/webui/browsing_history_handler_unittest.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/simple_test_clock.h"
 #include "base/values.h"
-#include "chrome/browser/history/browsing_history_service.h"
 #include "chrome/browser/history/web_history_service_factory.h"
 #include "chrome/browser/signin/fake_profile_oauth2_token_service_builder.h"
 #include "chrome/browser/signin/fake_signin_manager_builder.h"
@@ -25,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/profile_sync_test_util.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/browser_sync/test_profile_sync_service.h"
+#include "components/history/core/browser/browsing_history_service.h"
 #include "components/history/core/test/fake_web_history_service.h"
 #include "components/signin/core/browser/fake_profile_oauth2_token_service.h"
 #include "components/signin/core/browser/fake_signin_manager.h"
@@ -215,7 +215,7 @@ TEST_F(BrowsingHistoryHandlerTest, ObservingWebHistoryDeletions) {
     web_history_service()->ExpireHistoryBetween(
         std::set<GURL>(), base::Time(), base::Time::Max(),
         base::Bind(
-            &BrowsingHistoryService::RemoveWebHistoryComplete,
+            &history::BrowsingHistoryService::RemoveWebHistoryComplete,
             handler.browsing_history_service_->weak_factory_.GetWeakPtr()),
         PARTIAL_TRAFFIC_ANNOTATION_FOR_TESTS);
 
