32ca031cffd300bb60ca6f6a13dc7d8af66f2199
Matt Wolenetz <wolenetz@chromium.org>
2017-08-23 23:06:08 +0000

    MSE: Fix a couple PipelineIntegrationTest infra issues found by fuzzing
    With a WIP MSE fuzzer, I discovered a couple issues in our
    PipelineIntegrationTestBase and MockMediaSource that need fixing to
    improve fuzzer stability:
    
    1) If the initialization segment is only partially appended without any
       error, then StartPipelineWithMediaSource will hang while waiting for
       DemuxerOpened.  This change lets the MockMediaSource know if the test
       wants EndOfStream to occur immediately after the initial append.
       Includes a new test case.
    
    2) If the initialization segment was successfully parsed, but the very
       first media segment is corrupt and contained within the first append,
       then the error signalling normally handled by WMPI (triggering
       PipelineImpl::Stop()) was not occurring, causing pipeline to continue
       to setup and seek the renderers. Yet the pipeline's seek_cb was reset
       due to the earlier error, leading to at least DCHECK failure. This
       change updates the pipeline status callback to tell the pipeline to
       Stop() if the status is not PIPELINE_OK and the pipeline is still
       running.  Includes a new test case.
    
    BUG=750818
    
    Change-Id: I01e5cbf4479da0ff1fa93f0951c6a1cb07d637c6
    Reviewed-on: https://chromium-review.googlesource.com/629537
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496849}

--- media/test/mock_media_source.h
+++ media/test/mock_media_source.h
@@ -39,6 +39,10 @@ class MockMediaSource {
     demuxer_failure_cb_ = demuxer_failure_cb;
   }
 
+  void set_do_eos_after_next_append(bool flag) {
+    do_eos_after_next_append_ = flag;
+  }
+
   void Seek(base::TimeDelta seek_time,
             size_t new_position,
             size_t seek_append_size);
@@ -67,6 +71,10 @@ class MockMediaSource {
     return last_timestamp_offset_;
   }
 
+  void set_expect_append_success(bool expectation) {
+    expect_append_success_ = expectation;
+  }
+
   void InitSegmentReceived(std::unique_ptr<MediaTracks> tracks);
   MOCK_METHOD1(InitSegmentReceivedMock, void(std::unique_ptr<MediaTracks>&));
 
@@ -83,6 +91,8 @@ class MockMediaSource {
   PipelineStatusCB demuxer_failure_cb_;
   Demuxer::EncryptedMediaInitDataCB encrypted_media_init_data_cb_;
   base::TimeDelta last_timestamp_offset_;
+  bool do_eos_after_next_append_ = false;
+  bool expect_append_success_ = true;
 
   DISALLOW_COPY_AND_ASSIGN(MockMediaSource);
 };
