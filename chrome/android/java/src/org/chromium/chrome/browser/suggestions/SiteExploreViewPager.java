ce619f648c47dac555abd8e43cf1568611be9a40
galinap <galinap@google.com>
2017-08-24 16:21:01 +0000

    [Site Explore] Add tab indicator at the bottom
    This adds a page indicator view for a ViewPager. It is added as a decor
    at the bottom of SiteExploreViewPager.
    
    Please find a video of the current Site Exploration UI here:
    https://drive.google.com/open?id=0B7eRCSITD4qmZ0lMQUVkVmRTZEE
    
    Bug: 740905
    Change-Id: I0a88c2935ede89d61ce83a3816328fc866ca024a
    Reviewed-on: https://chromium-review.googlesource.com/626124
    Commit-Queue: Galia Peycheva <galinap@google.com>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497075}

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/SiteExploreViewPager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/SiteExploreViewPager.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.suggestions;
 
 import android.content.Context;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.View;
@@ -21,22 +22,26 @@ public class SiteExploreViewPager extends ViewPager {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        View tabLayout = getChildAt(0);
-        tabLayout.measure(
-                widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
-        int tabLayoutHeight = tabLayout.getMeasuredHeight();
-
+        int tabLayoutHeight = 0;
+        int tabIndicatorHeight = 0;
         int maxChildHeight = 0;
-        for (int i = 1; i < getChildCount(); i++) {
+        for (int i = 0; i < getChildCount(); i++) {
             View child = getChildAt(i);
+
             child.measure(
                     widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
-            int childHeight = child.getMeasuredHeight();
-            if (childHeight > maxChildHeight) maxChildHeight = childHeight;
+            if (child instanceof TabLayout) {
+                tabLayoutHeight = child.getMeasuredHeight();
+            } else if (child instanceof SiteExplorePageIndicatorView) {
+                tabIndicatorHeight = child.getMeasuredHeight();
+            } else {
+                int tileGridHeight = child.getMeasuredHeight();
+                if (tileGridHeight > maxChildHeight) maxChildHeight = tileGridHeight;
+            }
         }
 
-        heightMeasureSpec =
-                MeasureSpec.makeMeasureSpec(maxChildHeight + tabLayoutHeight, MeasureSpec.EXACTLY);
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(
+                maxChildHeight + tabLayoutHeight + tabIndicatorHeight, MeasureSpec.EXACTLY);
 
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
