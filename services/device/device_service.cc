5c4c42c703327ba673c7fb0a2a226c848b6256b5
Andrew Grieve <agrieve@chromium.org>
2017-08-24 19:21:22 +0000

    Delete RegisterNatives() calls from //services
    These are all no-ops now.
    
    Bug: 749244
    Change-Id: I618d3ef6c1c98cacff13506dc9b80d2c140e793e
    Reviewed-on: https://chromium-review.googlesource.com/632996
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497152}

--- services/device/device_service.cc
+++ services/device/device_service.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_ANDROID)
 #include "base/android/jni_android.h"
 #include "jni/InterfaceRegistrar_jni.h"
-#include "services/device/android/register_jni.h"
 #include "services/device/screen_orientation/screen_orientation_listener_android.h"
 #else
 #include "services/device/battery/battery_monitor_impl.h"
@@ -46,11 +45,6 @@ std::unique_ptr<service_manager::Service> CreateDeviceService(
     scoped_refptr<base::SingleThreadTaskRunner> io_task_runner,
     const WakeLockContextCallback& wake_lock_context_callback,
     const base::android::JavaRef<jobject>& java_nfc_delegate) {
-  if (!EnsureJniRegistered()) {
-    DLOG(ERROR) << "Failed to register JNI for Device Service";
-    return nullptr;
-  }
-
   return base::MakeUnique<DeviceService>(
       std::move(file_task_runner), std::move(io_task_runner),
       wake_lock_context_callback, java_nfc_delegate);
