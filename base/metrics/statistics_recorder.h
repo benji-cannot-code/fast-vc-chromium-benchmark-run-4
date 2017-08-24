db81e31ea7bc4166b19d29caa86cc0cbd2ec59e0
Brian White <bcwhite@chromium.org>
2017-08-24 00:03:13 +0000

    Validate histograms before and after each task on Android.
    This is a temporary CL for Android to find the source of a core
    trampler that is overwriting various heap objects.  It is expected
    that this will have a significant performance impact to tasks
    (guess 1-to-10ms per task) so the intention is to produce one Canary
    build and then revert it while the resulting data is examined.
    
    Bug: 744734
    Change-Id: Icf556e61c358982fb3f09ef7318d6c4443554e5a
    Reviewed-on: https://chromium-review.googlesource.com/619272
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Alok Priyadarshi <alokp@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Alexei Svitkine (very slow) <asvitkine@chromium.org>
    Commit-Queue: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496878}

--- base/metrics/statistics_recorder.h
+++ base/metrics/statistics_recorder.h
@@ -29,6 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_piece.h"
 #include "base/synchronization/lock.h"
 
+namespace tracked_objects {
+class Location;
+}
+
 namespace base {
 
 class BucketRanges;
@@ -138,7 +142,8 @@ class BASE_EXPORT StatisticsRecorder {
                             HistogramSnapshotManager* snapshot_manager);
 
   // TODO(asvitkine): Remove this after crbug/736675.
-  static void ValidateAllHistograms();
+  static void ValidateAllHistograms(
+      tracked_objects::Location* location = nullptr);
 
   // GetSnapshot copies some of the pointers to registered histograms into the
   // caller supplied vector (Histograms). Only histograms which have |query| as
