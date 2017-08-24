fa5f577514501fe96c615d4c4a094cab9b1190eb
Vladislav Kaznacheev <kaznacheev@google.com>
2017-08-24 19:02:59 +0000

    Re-land: Hide Assistant palette tool for boards with no Assistant support
    Added PaletteToolManager::ToolExists to make tests more readable
    and straightforward.
    
    Bug: 758392
    Change-Id: I75ae3531e92a7e910d5f156a2c0bf5194c8e0d1f
    Reviewed-on: https://chromium-review.googlesource.com/633297
    Commit-Queue: Vladislav Kaznacheev <kaznacheev@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497147}

--- ash/system/palette/palette_tray_unittest.cc
+++ ash/system/palette/palette_tray_unittest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/test_shell_delegate.h"
 #include "base/command_line.h"
 #include "base/memory/ptr_util.h"
+#include "chromeos/chromeos_switches.h"
 #include "components/prefs/testing_pref_service.h"
 #include "components/session_manager/session_manager_types.h"
 #include "ui/events/devices/device_data_manager.h"
@@ -192,9 +193,32 @@ TEST_F(PaletteTrayTest, ModeToolDeactivatedAutomatically) {
   EXPECT_FALSE(palette_tray_->is_active());
 }
 
-TEST_F(PaletteTrayTest, MetalayerToolActivatesHighlighter) {
-  ash::Shell::Get()->NotifyVoiceInteractionStatusChanged(
-      ash::VoiceInteractionState::RUNNING);
+TEST_F(PaletteTrayTest, NoMetalayerToolViewCreated) {
+  EXPECT_FALSE(
+      test_api_->GetPaletteToolManager()->HasTool(PaletteToolId::METALAYER));
+}
+
+// Base class for tests that rely on voice interaction enabled.
+class PaletteTrayTestWithVoiceInteraction : public PaletteTrayTest {
+ public:
+  PaletteTrayTestWithVoiceInteraction() {
+    base::CommandLine::ForCurrentProcess()->AppendSwitch(
+        chromeos::switches::kEnableVoiceInteraction);
+  }
+  ~PaletteTrayTestWithVoiceInteraction() override = default;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(PaletteTrayTestWithVoiceInteraction);
+};
+
+TEST_F(PaletteTrayTestWithVoiceInteraction, MetalayerToolViewCreated) {
+  EXPECT_TRUE(
+      test_api_->GetPaletteToolManager()->HasTool(PaletteToolId::METALAYER));
+}
+
+TEST_F(PaletteTrayTestWithVoiceInteraction, MetalayerToolActivatesHighlighter) {
+  Shell::Get()->NotifyVoiceInteractionStatusChanged(
+      VoiceInteractionState::RUNNING);
 
   HighlighterController highlighter_controller;
   HighlighterControllerTestApi highlighter_test_api(&highlighter_controller);
@@ -266,9 +290,10 @@ TEST_F(PaletteTrayTest, MetalayerToolActivatesHighlighter) {
   test_palette_delegate()->set_highlighter_test_api(nullptr);
 }
 
-TEST_F(PaletteTrayTest, StylusBarrelButtonActivatesHighlighter) {
-  ash::Shell::Get()->NotifyVoiceInteractionStatusChanged(
-      ash::VoiceInteractionState::RUNNING);
+TEST_F(PaletteTrayTestWithVoiceInteraction,
+       StylusBarrelButtonActivatesHighlighter) {
+  Shell::Get()->NotifyVoiceInteractionStatusChanged(
+      VoiceInteractionState::RUNNING);
 
   HighlighterController highlighter_controller;
   HighlighterControllerTestApi highlighter_test_api(&highlighter_controller);
