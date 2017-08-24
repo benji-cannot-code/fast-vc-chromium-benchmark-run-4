2f165ff7dc1bf09014fce88a15999b366c5c3511
Jun Cai <juncai@chromium.org>
2017-08-24 19:44:17 +0000

    Update SensorReading comments for corresponding DeviceMotion values, etc
    This CL adds documentation for ACCELEROMETER, LINEAR_ACCELERATION, and
    GYROSCOPE for their corresponding DeviceMotion values in the W3C
    DeviceMotion Event Specification, so that it is easy to reference these
    values.
    
    This CL also removes an unnecessary include file from
    platform_sensor_fusion.h and fixes a typo at sensor_reading.h.
    
    Bug: 733707
    Change-Id: I35445ce7978c28bc5bb074405a0a2dee18f6464a
    Reviewed-on: https://chromium-review.googlesource.com/626340
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497160}

--- services/device/generic_sensor/platform_sensor_fusion.h
+++ services/device/generic_sensor/platform_sensor_fusion.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/flat_map.h"
 #include "base/macros.h"
-#include "base/timer/timer.h"
 #include "services/device/generic_sensor/platform_sensor.h"
 #include "services/device/generic_sensor/platform_sensor_provider_base.h"
 
