1177145a8680af58fe93642483aa9bd2de33412a
Matt Falkenhagen <falken@chromium.org>
2017-08-24 10:43:21 +0000

    service worker: Add CHECKs and defense mechanism to debug crash with destroyed bindings
    The theory is something is getting deleted on different threads. Also there
    are some crashes existing where apparently ServiceWorkerDispatcherHost tried to remove
    itself from ServiceWorkerContextCore, but it was already removed, so add a Phase
    mechanism to avoid doing that.
    
    Bug: 750267
    Change-Id: Ib9e595a23b499325b7d7b721520ab51a3ddb79c3
    Reviewed-on: https://chromium-review.googlesource.com/630902
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496998}

--- content/browser/service_worker/service_worker_context_core.cc
+++ content/browser/service_worker/service_worker_context_core.cc
@@ -313,7 +313,9 @@ ServiceWorkerDispatcherHost* ServiceWorkerContextCore::GetDispatcherHost(
 }
 
 void ServiceWorkerContextCore::RemoveDispatcherHost(int process_id) {
-  DCHECK(dispatcher_hosts_.find(process_id) != dispatcher_hosts_.end());
+  // Temporary CHECKs for debugging https://crbug.com/750267.
+  CHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  CHECK(dispatcher_hosts_.find(process_id) != dispatcher_hosts_.end());
   RemoveAllProviderHostsForProcess(process_id);
   embedded_worker_registry_->RemoveProcess(process_id);
   dispatcher_hosts_.erase(process_id);
@@ -321,6 +323,8 @@ void ServiceWorkerContextCore::RemoveDispatcherHost(int process_id) {
 
 void ServiceWorkerContextCore::AddProviderHost(
     std::unique_ptr<ServiceWorkerProviderHost> host) {
+  // Temporary CHECK for debugging https://crbug.com/750267.
+  CHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
   int process_id = host->process_id();
   int provider_id = host->provider_id();
   ProviderMap* map = GetProviderMapForProcess(process_id);
@@ -342,6 +346,8 @@ ServiceWorkerProviderHost* ServiceWorkerContextCore::GetProviderHost(
 
 void ServiceWorkerContextCore::RemoveProviderHost(
     int process_id, int provider_id) {
+  // Temporary CHECK for debugging https://crbug.com/750267.
+  CHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
   ProviderMap* map = GetProviderMapForProcess(process_id);
   DCHECK(map);
   map->Remove(provider_id);
@@ -349,18 +355,24 @@ void ServiceWorkerContextCore::RemoveProviderHost(
 
 void ServiceWorkerContextCore::RemoveAllProviderHostsForProcess(
     int process_id) {
+  // Temporary CHECK for debugging https://crbug.com/750267.
+  CHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
   if (providers_->Lookup(process_id))
     providers_->Remove(process_id);
 }
 
 std::unique_ptr<ServiceWorkerContextCore::ProviderHostIterator>
 ServiceWorkerContextCore::GetProviderHostIterator() {
+  // Temporary CHECK for debugging https://crbug.com/750267.
+  CHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
   return base::WrapUnique(new ProviderHostIterator(
       providers_.get(), ProviderHostIterator::ProviderHostPredicate()));
 }
 
 std::unique_ptr<ServiceWorkerContextCore::ProviderHostIterator>
 ServiceWorkerContextCore::GetClientProviderHostIterator(const GURL& origin) {
+  // Temporary CHECK for debugging https://crbug.com/750267.
+  CHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
   return base::WrapUnique(new ProviderHostIterator(
       providers_.get(), base::Bind(IsSameOriginClientProviderHost, origin)));
 }
@@ -495,6 +507,13 @@ void ServiceWorkerContextCore::DidGetAllRegistrationsForUnregisterForOrigin(
     UnregisterServiceWorker(
         scope, base::Bind(&SuccessCollectorCallback, barrier, overall_success));
   }
+}
+
+ServiceWorkerContextCore::ProviderMap*
+ServiceWorkerContextCore::GetProviderMapForProcess(int process_id) {
+  // Temporary CHECK for debugging https://crbug.com/750267.
+  CHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  return providers_->Lookup(process_id);
 }
 
 void ServiceWorkerContextCore::RegistrationComplete(
--- content/browser/service_worker/service_worker_dispatcher_host.h
+++ content/browser/service_worker/service_worker_dispatcher_host.h
@@ -133,6 +133,8 @@ class CONTENT_EXPORT ServiceWorkerDispatcherHost
 
   using StatusCallback = base::Callback<void(ServiceWorkerStatusCode status)>;
   enum class ProviderStatus { OK, NO_CONTEXT, DEAD_HOST, NO_HOST, NO_URL };
+  // Debugging for https://crbug.com/750267
+  enum class Phase { kInitial, kAddedToContext, kRemovedFromContext };
 
   // mojom::ServiceWorkerDispatcherHost implementation
   void OnProviderCreated(ServiceWorkerProviderHostInfo info) override;
@@ -280,6 +282,8 @@ class CONTENT_EXPORT ServiceWorkerDispatcherHost
 
   const int render_process_id_;
   ResourceContext* resource_context_;
+  // Only accessed on the IO thread.
+  Phase phase_ = Phase::kInitial;
   // Only accessed on the IO thread.
   scoped_refptr<ServiceWorkerContextWrapper> context_wrapper_;
 
