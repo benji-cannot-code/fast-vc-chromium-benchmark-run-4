0827707fd131919b0b9a38f7310b68a83a36b967
Gregory Chatzinoff <gchatz@chromium.org>
2017-08-24 20:33:26 +0000

    Plumb dispatcher through PasswordController as property.
    This CL plumbs the dispatcher through PasswordController as a property
    instead of through an initializer. This change better documents that
    the dispatcher is held weakly. Since the dispatcher is no longer
    required in the initializer of PasswordController, the
    PasswordController and all of its dependent tab helpers can be moved
    back to attachTabHelpers in Tab.
    
    Bug: none
    Change-Id: Icf3313aa01215a400af7e04f3effe808aded9cce
    Reviewed-on: https://chromium-review.googlesource.com/630504
    Commit-Queue: Gregory Chatzinoff <gchatz@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497176}

--- ios/chrome/browser/passwords/password_generation_agent.h
+++ ios/chrome/browser/passwords/password_generation_agent.h
@@ -43,8 +43,7 @@ class PasswordManagerDriver;
      initWithWebState:(web::WebState*)webState
       passwordManager:(password_manager::PasswordManager*)passwordManager
 passwordManagerDriver:(password_manager::PasswordManagerDriver*)driver
-  passwordsUiDelegate:(id<PasswordsUiDelegate>)delegate
-           dispatcher:(id<ApplicationCommands>)dispatcher;
+  passwordsUiDelegate:(id<PasswordsUiDelegate>)delegate;
 
 - (instancetype)init NS_UNAVAILABLE;
 
@@ -60,6 +59,9 @@ passwordManagerDriver:(password_manager::PasswordManagerDriver*)driver
 // Provides an input accessory view from this PasswordGenerationAgent.
 @property(nonatomic, readonly)
     id<FormInputAccessoryViewProvider> accessoryViewProvider;
+
+// The dispatcher used for the PasswordGenerationAgent.
+@property(nonatomic, weak) id<ApplicationCommands> dispatcher;
 
 @end
 
@@ -89,8 +91,7 @@ passwordManagerDriver:(password_manager::PasswordManagerDriver*)driver
 passwordManagerDriver:(password_manager::PasswordManagerDriver*)driver
     JSPasswordManager:(JsPasswordManager*)javaScriptPasswordManager
   JSSuggestionManager:(JsSuggestionManager*)suggestionManager
-  passwordsUiDelegate:(id<PasswordsUiDelegate>)delegate
-           dispatcher:(id<ApplicationCommands>)dispatcher;
+  passwordsUiDelegate:(id<PasswordsUiDelegate>)delegate;
 
 // Clears all per-page state.
 - (void)clearState;
