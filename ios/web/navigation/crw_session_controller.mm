c642cdc11ff5467997d922b676264cfc01b4dfc1
Danyao Wang <danyao@google.com>
2017-08-24 21:15:50 +0000

    [Nav Experiment] Add a GetCurrentItemImpl() API to NavigationManagerImpl
    CRWWebController relies extensively on the notion of |currentNavItem|
    in its navigation functions. Moving this API from CRWSessionController
    to NavigationManagerImpl allows CRWWebController to operate on top of
    WKBasedNavigationManagerImpl.
    
    Bug: 734150
    Change-Id: I7acb54507e5cbee980ecaf9ac3216c62b28797e8
    Reviewed-on: https://chromium-review.googlesource.com/629757
    Commit-Queue: Danyao Wang <danyao@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497189}

--- ios/web/navigation/crw_session_controller.mm
+++ ios/web/navigation/crw_session_controller.mm
@@ -157,11 +157,7 @@ initiationType:(web::NavigationInitiationType)initiationType;
 }
 
 - (web::NavigationItemImpl*)currentItem {
-  if (self.transientItem)
-    return self.transientItem;
-  if (self.pendingItem)
-    return self.pendingItem;
-  return self.lastCommittedItem;
+  return _navigationManager->GetCurrentItemImpl();
 }
 
 - (web::NavigationItemImpl*)visibleItem {
