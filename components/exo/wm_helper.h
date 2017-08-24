380491cc558187614cd6d35c2627f83097358cde
Daichi Hirono <hirono@chromium.org>
2017-08-24 01:29:02 +0000

    Remove unused <memory> header
    Bug: None
    Test: None
    Change-Id: I729a255a07d9455530819edbc9ed6508cf0bfee1
    Reviewed-on: https://chromium-review.googlesource.com/630797
    Reviewed-by: David Reveman <reveman@chromium.org>
    Commit-Queue: Daichi Hirono <hirono@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496910}

--- components/exo/wm_helper.h
+++ components/exo/wm_helper.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_EXO_WM_HELPER_H_
 #define COMPONENTS_EXO_WM_HELPER_H_
 
-#include <memory>
-
 #include "base/macros.h"
 #include "base/observer_list.h"
 #include "ui/aura/client/drag_drop_delegate.h"
