d8912c735c3634a96ea8d4bbb699e47d297d0784
Anton Urusov <urusant@google.com>
2017-08-24 20:01:02 +0000

    Split policy UI resource files
    Split policy UI resources (javascript and CSS) into separate files in order to make them more reusable.
    
    Bug: 
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I9a3a0935e2dab22d6e93dfea30e4a7072d2cb0be
    Reviewed-on: https://chromium-review.googlesource.com/632796
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Anton Urusov <urusant@google.com>
    Cr-Commit-Position: refs/heads/master@{#497165}

--- chrome/browser/ui/webui/policy_ui.cc
+++ chrome/browser/ui/webui/policy_ui.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/policy_ui.h"
 
 #include "base/memory/ptr_util.h"
+#include "build/build_config.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/policy_ui_handler.h"
 #include "chrome/common/url_constants.h"
@@ -49,7 +50,11 @@ content::WebUIDataSource* CreatePolicyUIHtmlSource() {
   source->AddLocalizedString("hideExpandedValue",
                              IDS_POLICY_HIDE_EXPANDED_VALUE);
   // Add required resources.
+#if !defined(OS_ANDROID)
+  source->AddResourcePath("policy_common.css", IDR_POLICY_COMMON_CSS);
+#endif
   source->AddResourcePath("policy.css", IDR_POLICY_CSS);
+  source->AddResourcePath("policy_base.js", IDR_POLICY_BASE_JS);
   source->AddResourcePath("policy.js", IDR_POLICY_JS);
   source->SetDefaultResource(IDR_POLICY_HTML);
   return source;
