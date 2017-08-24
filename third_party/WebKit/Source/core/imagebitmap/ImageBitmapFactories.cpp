64e2161c521bb13ba8ffd75c3bc2b4951ad33798
Reza.Zakerinasab <zakerinasab@chromium.org>
2017-08-24 16:43:49 +0000

    Add proper usage counters for createImageBitmap
    This CL adds a number of use counters for createImageBitmap from different
    sources.
    
    Bug: 751195
    Change-Id: I17cada39c90ef4e4ac6d799b9eb5905d103d389c
    Reviewed-on: https://chromium-review.googlesource.com/617641
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Reviewed-by: Alexei Svitkine (very slow) <asvitkine@chromium.org>
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Mohammad Reza Zakerinasab <zakerinasab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497083}

--- third_party/WebKit/Source/core/imagebitmap/ImageBitmapFactories.cpp
+++ third_party/WebKit/Source/core/imagebitmap/ImageBitmapFactories.cpp
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/svg/graphics/SVGImage.h"
 #include "core/workers/WorkerGlobalScope.h"
 #include "platform/CrossThreadFunctional.h"
+#include "platform/Histogram.h"
 #include "platform/SharedBuffer.h"
 #include "platform/image-decoders/ImageDecoder.h"
 #include "platform/threading/BackgroundTaskRunner.h"
@@ -60,16 +61,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+// This enum is used in a UMA histogram.
+enum CreateImageBitmapSource {
+  kCreateImageBitmapSourceBlob = 0,
+  kCreateImageBitmapSourceImageBitmap = 1,
+  kCreateImageBitmapSourceImageData = 2,
+  kCreateImageBitmapSourceHTMLCanvasElement = 3,
+  kCreateImageBitmapSourceHTMLImageElement = 4,
+  kCreateImageBitmapSourceHTMLVideoElement = 5,
+  kCreateImageBitmapSourceOffscreenCanvas = 6,
+  kCreateImageBitmapSourceSVGImageElement = 7,
+  kCreateImageBitmapSourceCount,
+};
+
 static inline ImageBitmapSource* ToImageBitmapSourceInternal(
     const ImageBitmapSourceUnion& value,
     ExceptionState& exception_state,
     const ImageBitmapOptions& options,
     bool has_crop_rect) {
   ImageElementBase* image_element = nullptr;
+  DEFINE_THREAD_SAFE_STATIC_LOCAL(
+      EnumerationHistogram, image_bitmap_source_histogram,
+      ("Canvas.CreateImageBitmapSource", kCreateImageBitmapSourceCount));
   if (value.isHTMLImageElement()) {
+    image_bitmap_source_histogram.Count(
+        kCreateImageBitmapSourceHTMLImageElement);
     if (!(image_element = value.getAsHTMLImageElement()))
       return nullptr;
   } else if (value.isSVGImageElement()) {
+    image_bitmap_source_histogram.Count(
+        kCreateImageBitmapSourceSVGImageElement);
     if (!(image_element = value.getAsSVGImageElement()))
       return nullptr;
   }
@@ -94,18 +115,33 @@ static inline ImageBitmapSource* ToImageBitmapSourceInternal(
     }
     return image_element;
   }
-  if (value.isHTMLVideoElement())
+  if (value.isHTMLVideoElement()) {
+    image_bitmap_source_histogram.Count(
+        kCreateImageBitmapSourceHTMLVideoElement);
     return value.getAsHTMLVideoElement();
-  if (value.isHTMLCanvasElement())
+  }
+  if (value.isHTMLCanvasElement()) {
+    image_bitmap_source_histogram.Count(
+        kCreateImageBitmapSourceHTMLCanvasElement);
     return value.getAsHTMLCanvasElement();
-  if (value.isBlob())
+  }
+  if (value.isBlob()) {
+    image_bitmap_source_histogram.Count(kCreateImageBitmapSourceBlob);
     return value.getAsBlob();
-  if (value.isImageData())
+  }
+  if (value.isImageData()) {
+    image_bitmap_source_histogram.Count(kCreateImageBitmapSourceImageData);
     return value.getAsImageData();
-  if (value.isImageBitmap())
+  }
+  if (value.isImageBitmap()) {
+    image_bitmap_source_histogram.Count(kCreateImageBitmapSourceImageBitmap);
     return value.getAsImageBitmap();
-  if (value.isOffscreenCanvas())
+  }
+  if (value.isOffscreenCanvas()) {
+    image_bitmap_source_histogram.Count(
+        kCreateImageBitmapSourceOffscreenCanvas);
     return value.getAsOffscreenCanvas();
+  }
   NOTREACHED();
   return nullptr;
 }
