2d21a8b7ac04e21643845c071232a3786d83a90a
Peter Kotwicz <pkotwicz@chromium.org>
2017-08-24 03:02:19 +0000

    [Android FRE] Remove redundant ChromeTabbedActivity#launchFirstRunExperience()
    As a result of https://codereview.chromium.org/2833673003 having landed,
    ChromeTabbedActivity checks for whether the First Run Experience needs
    to be shown in AsyncInitializationActivity#onCreateInternal() which
    occurs prior to ChromeTabbedActivity#preInflationStartup()
    
    BUG=755646
    
    Change-Id: I442b33a0ef6eb5b5a72e72bdf25cd97014b8e641
    Reviewed-on: https://chromium-review.googlesource.com/616440
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Ted Choc (OOO 8.21-25) <tedchoc@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496932}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunActivity.java
@@ -86,7 +86,6 @@ public class FirstRunActivity extends AsyncInitializationActivity implements Fir
     static final String POST_NATIVE_SETUP_NEEDED = "PostNativeSetupNeeded";
 
     // Outgoing results:
-    public static final String RESULT_CLOSE_APP = "Close App";
     public static final String RESULT_SIGNIN_ACCOUNT_NAME = "ResultSignInTo";
     public static final String RESULT_SHOW_SIGNIN_SETTINGS = "ResultShowSignInSettings";
     public static final String EXTRA_FIRST_RUN_ACTIVITY_RESULT = "Extra.FreActivityResult";
@@ -421,7 +420,6 @@ public class FirstRunActivity extends AsyncInitializationActivity implements Fir
     public void abortFirstRunExperience() {
         Intent intent = new Intent();
         if (mFreProperties != null) intent.putExtras(mFreProperties);
-        intent.putExtra(RESULT_CLOSE_APP, true);
         finishAllTheActivities(getLocalClassName(), Activity.RESULT_CANCELED, intent);
 
         sendPendingIntentIfNecessary(false);
