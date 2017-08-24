b31b287f999d23daa5cd6b02760d61e974bf55ff
Vitalii Iarko <vitaliii@chromium.org>
2017-08-24 08:39:01 +0000

    [NTP::Prefetch] Add NTP related metrics.
    This CL adds metrics to record:
    
    1) number of prefetched suggestions in Article section (if it is
    visible) when the user is offline.
    
    2) position of a clicked prefetched Article suggestion in the section
    when offline.
    
    3) position of a shown prefetched Article suggestions when offline.
    
    Bug: 740091
    Change-Id: I66ab7a4965e5b6ae945217af179cc54c1a075bca
    Reviewed-on: https://chromium-review.googlesource.com/629056
    Commit-Queue: vitaliii <vitaliii@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Alexei Svitkine (very slow) <asvitkine@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496986}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/SuggestionsSection.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/SuggestionsSection.java
@@ -369,6 +369,14 @@ public class SuggestionsSection extends InnerNode {
         return mSuggestionsList.getItemCount();
     }
 
+    public int getPrefetchedSuggestionsCount() {
+        int count = 0;
+        for (SnippetArticle suggestion : mSuggestionsList) {
+            if (suggestion.isPrefetched()) ++count;
+        }
+        return count;
+    }
+
     public boolean isDataStale() {
         return mIsDataStale;
     }
--- components/ntp_snippets/content_suggestions_metrics.h
+++ components/ntp_snippets/content_suggestions_metrics.h
@@ -21,7 +21,9 @@ namespace metrics {
 // whether the user actually saw the category.
 void OnPageShown(const std::vector<Category>& categories,
                  const std::vector<int>& suggestions_per_category,
-                 const std::vector<bool>& is_category_visible);
+                 const std::vector<int>& prefetched_suggestions_per_category,
+                 const std::vector<bool>& is_category_visible,
+                 bool is_offline);
 
 // Should only be called once per NTP for each suggestion.
 void OnSuggestionShown(int global_position,
@@ -29,7 +31,9 @@ void OnSuggestionShown(int global_position,
                        int position_in_category,
                        base::Time publish_date,
                        float score,
-                       base::Time fetch_date);
+                       base::Time fetch_date,
+                       bool is_prefetched,
+                       bool is_offline);
 
 // TODO(crbug.com/682160): Take struct, so that one could not mix up the
 // order of arguments.
@@ -39,7 +43,9 @@ void OnSuggestionOpened(int global_position,
                         int position_in_category,
                         base::Time publish_date,
                         float score,
-                        WindowOpenDisposition disposition);
+                        WindowOpenDisposition disposition,
+                        bool is_prefetched,
+                        bool is_offline);
 
 void OnSuggestionMenuOpened(int global_position,
                             Category category,
