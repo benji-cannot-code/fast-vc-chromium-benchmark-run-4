906e4ef6f708afa58aa41c958c366039bb326030
David Reveman <reveman@chromium.org>
2017-08-24 18:18:11 +0000

    exo: Implement Surface::SetBufferTransform.
    This adds an implementation of Surface::SetBufferTransform that
    supports rotation. 180 degree flip around a vertical axis can be
    implemented in the future.
    
    It also fixes the wl_output interface that used to return the
    transform that a display's output is currently adjusted for but
    should really return the transform that a compositor will apply
    to a surface to compensate for the rotation of an output device.
    
    Bug: 751232
    Test: exo_unittests --gtest_filter=SurfaceTest.SetBufferTransform
    Change-Id: I06eb68873df956c0b3ea752664965300efd694d0
    Reviewed-on: https://chromium-review.googlesource.com/627719
    Commit-Queue: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497126}

--- components/exo/wayland/clients/client_base.cc
+++ components/exo/wayland/clients/client_base.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/gpu/GrContext.h"
 #include "third_party/skia/include/gpu/gl/GrGLAssembleInterface.h"
 #include "third_party/skia/include/gpu/gl/GrGLInterface.h"
+#include "ui/gfx/geometry/size_conversions.h"
 #include "ui/gl/gl_bindings.h"
 #include "ui/gl/gl_enums.h"
 #include "ui/gl/gl_surface_egl.h"
@@ -50,6 +51,9 @@ const char kSize[] = "size";
 
 // Specifies the client scale factor (ie. number of physical pixels per DIP).
 const char kScale[] = "scale";
+
+// Specifies the client transform (ie. rotation).
+const char kTransform[] = "transform";
 
 // Specifies if the background should be transparent.
 const char kTransparentBackground[] = "transparent-background";
@@ -160,6 +164,23 @@ bool ClientBase::InitParams::FromCommandLine(
     return false;
   }
 
+  if (command_line.HasSwitch(switches::kTransform)) {
+    std::string transform_str =
+        command_line.GetSwitchValueASCII(switches::kTransform);
+    if (transform_str == "0") {
+      transform = WL_OUTPUT_TRANSFORM_NORMAL;
+    } else if (transform_str == "90") {
+      transform = WL_OUTPUT_TRANSFORM_90;
+    } else if (transform_str == "180") {
+      transform = WL_OUTPUT_TRANSFORM_180;
+    } else if (transform_str == "270") {
+      transform = WL_OUTPUT_TRANSFORM_270;
+    } else {
+      LOG(ERROR) << "Invalid value for " << switches::kTransform;
+      return false;
+    }
+  }
+
   use_drm = command_line.HasSwitch(switches::kUseDrm);
   if (use_drm)
     use_drm_value = command_line.GetSwitchValueASCII(switches::kUseDrm);
@@ -190,6 +211,22 @@ ClientBase::Buffer::~Buffer() {}
 bool ClientBase::Init(const InitParams& params) {
   size_.SetSize(params.width, params.height);
   scale_ = params.scale;
+  transform_ = params.transform;
+  switch (params.transform) {
+    case WL_OUTPUT_TRANSFORM_NORMAL:
+    case WL_OUTPUT_TRANSFORM_180:
+      surface_size_.SetSize(params.width, params.height);
+      break;
+    case WL_OUTPUT_TRANSFORM_90:
+    case WL_OUTPUT_TRANSFORM_270:
+      surface_size_.SetSize(params.height, params.width);
+      break;
+    default:
+      NOTREACHED();
+      break;
+  }
+  surface_size_ = gfx::ToCeiledSize(
+      gfx::ScaleSize(gfx::SizeF(surface_size_), 1.0f / params.scale));
   fullscreen_ = params.fullscreen;
   transparent_background_ = params.transparent_background;
 
