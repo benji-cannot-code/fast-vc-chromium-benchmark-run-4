eff7892493c4c6ee8ce754c5f59d01ae6eed886a
Shakti Sahu <shaktisahu@chromium.org>
2017-08-24 06:57:48 +0000

    IPH: Various fixes to make existing IPH work on chrome home
    1 - Rectangular highlight for Downloads in bottom sheet control container
    2 - Circular highlight for Expand button
    3 - For non-expand button mode, text bubble should point to the toolbar handle at the center of the screen. (crbug/750114)
    4 - There is a race condition between dismissal of the bubble and highlight of download arrow / downloads navigation item. (crbug/750123) Added a 200ms delay.
    5 - Download Home IPH and Screenshot IPH use the same UI. Combined both functions into one.
    6 - Chrome home toolbar expansion is a different IPH addressed in a different CL
    
    Bug: 749938
    Change-Id: I865e3d2f5e97ca28105024d5a3a12daef2daf834
    Reviewed-on: https://chromium-review.googlesource.com/592078
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496969}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheetContentController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheetContentController.java
@@ -10,6 +10,7 @@ import android.content.Context;
 import android.content.res.ColorStateList;
 import android.content.res.Resources;
 import android.support.annotation.IntDef;
+import android.support.annotation.Nullable;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
@@ -37,6 +38,7 @@ import org.chromium.chrome.browser.tabmodel.TabModel;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.tabmodel.TabModelSelectorObserver;
 import org.chromium.chrome.browser.util.MathUtils;
+import org.chromium.chrome.browser.widget.ViewHighlighter;
 import org.chromium.chrome.browser.widget.bottomsheet.BottomSheet.BottomSheetContent;
 import org.chromium.ui.UiUtils;
 
@@ -99,6 +101,10 @@ public class BottomSheetContentController extends BottomNavigationView
         @Override
         public void onSheetOpened() {
             if (!mDefaultContentInitialized) initializeDefaultContent();
+            if (mHighlightItemId != null) {
+                mHighlightedView = mActivity.findViewById(mHighlightItemId);
+                ViewHighlighter.turnOnHighlight(mHighlightedView, false);
+            }
         }
 
         @Override
@@ -113,6 +119,9 @@ public class BottomSheetContentController extends BottomNavigationView
             UiUtils.hideKeyboard((View) BottomSheetContentController.this);
             // TODO(twellington): determine a policy for destroying the
             //                    SuggestionsBottomSheetContent.
+            ViewHighlighter.turnOffHighlight(mHighlightedView);
+            mHighlightedView = null;
+            mHighlightItemId = null;
         }
 
         @Override
@@ -149,11 +158,17 @@ public class BottomSheetContentController extends BottomNavigationView
     private PlaceholderSheetContent mPlaceholderContent;
     private boolean mOmniboxHasFocus;
     private TabModelSelectorObserver mTabModelSelectorObserver;
+    private Integer mHighlightItemId;
+    private View mHighlightedView;
 
     public BottomSheetContentController(Context context, AttributeSet atts) {
         super(context, atts);
 
         mPlaceholderContent = new PlaceholderSheetContent(context);
+    }
+
+    public void setHighlightItemId(@Nullable Integer highlightItemId) {
+        mHighlightItemId = highlightItemId;
     }
 
     /** Called when the activity containing the bottom sheet is destroyed. */
@@ -274,6 +289,10 @@ public class BottomSheetContentController extends BottomNavigationView
                 && !mShouldOpenSheetOnNextContentChange) {
             return false;
         }
+
+        ViewHighlighter.turnOffHighlight(mHighlightedView);
+        mHighlightedView = null;
+        mHighlightItemId = null;
 
         if (mSelectedItemId == item.getItemId()) return false;
 
