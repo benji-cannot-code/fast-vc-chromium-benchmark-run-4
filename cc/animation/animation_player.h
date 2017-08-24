27f37b580b1b191c9fb2ab30127de7ad3751650d
Walter Korman <wkorman@chromium.org>
2017-08-24 22:52:39 +0000

    Clean up LayerImpl element id animation references.
    Inline the only non-test usage of
    HasPotentiallyRunningTransformAnimation (and test one as well).
    
    Remove unused HasAnimationThatInflatesBounds,
    HasFilterAnimationThatInflatesBounds and
    TransformAnimationBoundsForBox methods.
    
    Remove unimplemented FilterAnimationBoundsForBox method
    as it was added at least a year ago and TODO owner is no
    longer working on animation logic.
    
    Bug: 709137, 252472
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: If4074a5cf0bb180f02d0b10c7017eca54beed719
    Reviewed-on: https://chromium-review.googlesource.com/627622
    Commit-Queue: Walter Korman <wkorman@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497233}

--- cc/animation/animation_player.h
+++ cc/animation/animation_player.h
@@ -123,7 +123,6 @@ class CC_ANIMATION_EXPORT AnimationPlayer
   // are deleted.
   void ActivateAnimations();
 
-  bool HasFilterAnimationThatInflatesBounds() const;
   bool HasTransformAnimationThatInflatesBounds() const;
 
   bool TransformAnimationBoundsForBox(const gfx::BoxF& box,
