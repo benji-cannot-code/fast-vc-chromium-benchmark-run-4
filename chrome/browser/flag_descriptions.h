aa9dd69d5c4b60fbe55ac24953765809d0ebd1bf
Chris Pickel <sfiera@chromium.org>
2017-08-24 10:47:26 +0000

    Add chrome-search://local-ntp/doodle.js
    Add a feature to enable it, and expose the ddljson feature to all
    platforms so the test doodles can be used with it.
    
    Bug: 583290
    Change-Id: I82cb2b7fe849ebfcac09a50bc062220c2ee332d4
    Reviewed-on: https://chromium-review.googlesource.com/612178
    Commit-Queue: Chris Pickel <sfiera@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497001}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -800,6 +800,9 @@ extern const char kTrySupportedChannelLayoutsDescription[];
 extern const char kUiPartialSwapName[];
 extern const char kUiPartialSwapDescription[];
 
+extern const char kUseDdljsonApiName[];
+extern const char kUseDdljsonApiDescription[];
+
 extern const char kUserConsentForExtensionScriptsName[];
 extern const char kUserConsentForExtensionScriptsDescription[];
 
@@ -1177,9 +1180,6 @@ extern const char kUpdateMenuItemDescription[];
 extern const char kUseAndroidMidiApiName[];
 extern const char kUseAndroidMidiApiDescription[];
 
-extern const char kUseDdljsonApiName[];
-extern const char kUseDdljsonApiDescription[];
-
 extern const char kThirdPartyDoodlesName[];
 extern const char kThirdPartyDoodlesDescription[];
 
@@ -1194,6 +1194,9 @@ extern const char kAccountConsistencyName[];
 extern const char kAccountConsistencyDescription[];
 extern const char kAccountConsistencyChoiceMirror[];
 extern const char kAccountConsistencyChoiceDice[];
+
+extern const char kDoodlesOnLocalNtpName[];
+extern const char kDoodlesOnLocalNtpDescription[];
 
 extern const char kEnableAudioFocusName[];
 extern const char kEnableAudioFocusDescription[];
