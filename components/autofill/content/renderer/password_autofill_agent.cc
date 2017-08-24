3f660793c11e6c03a88d670a851edfc67da53714
Tatiana Gornak <melandory@chromium.org>
2017-08-24 15:25:55 +0000

    Do not show "Show all save passwords" fallback for chrome:// and about:// pages
    BUG=758158
    
    Change-Id: I6d6b2684d75a28461eeff7dcd3e15da6820c4aba
    Reviewed-on: https://chromium-review.googlesource.com/628679
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Commit-Queue: Tatiana Gornak <melandory@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497059}

--- components/autofill/content/renderer/password_autofill_agent.cc
+++ components/autofill/content/renderer/password_autofill_agent.cc
@@ -656,13 +656,16 @@ blink::WebInputElement FindUsernameElementPrecedingPasswordElement(
   return blink::WebInputElement();
 }
 
-bool ShouldShowStandaloneManuallFallback(
-    const blink::WebInputElement& element) {
-  return (element.IsPasswordField() &&
-          !IsCreditCardVerificationPasswordField(element) &&
-          !HasCreditCardAutocompleteAttributes(element) &&
-          base::FeatureList::IsEnabled(
-              password_manager::features::kEnableManualFallbacksFilling));
+bool ShouldShowStandaloneManuallFallback(const blink::WebInputElement& element,
+                                         const GURL& url) {
+  return (
+      element.IsPasswordField() &&
+      !IsCreditCardVerificationPasswordField(element) &&
+      !HasCreditCardAutocompleteAttributes(element) &&
+      !base::StartsWith(url.scheme(), "chrome", base::CompareCase::SENSITIVE) &&
+      !url.SchemeIs(url::kAboutScheme) &&
+      base::FeatureList::IsEnabled(
+          password_manager::features::kEnableManualFallbacksFilling));
 }
 
 }  // namespace
@@ -1039,6 +1042,8 @@ bool PasswordAutofillAgent::ShowSuggestions(
   if (!FindPasswordInfoForElement(element, &username_element, &password_element,
                                   &password_info)) {
     if (IsUsernameOrPasswordField(element)) {
+      blink::WebLocalFrame* frame = render_frame()->GetWebFrame();
+      GURL frame_url = GURL(frame->GetDocument().Url());
 #if defined(SAFE_BROWSING_DB_LOCAL)
       if (!checked_safe_browsing_reputation_) {
         checked_safe_browsing_reputation_ = true;
@@ -1046,13 +1051,11 @@ bool PasswordAutofillAgent::ShowSuggestions(
             element.Form().IsNull()
                 ? GURL()
                 : form_util::GetCanonicalActionForForm(element.Form());
-        blink::WebLocalFrame* frame = render_frame()->GetWebFrame();
-        GURL frame_url = GURL(frame->GetDocument().Url());
         GetPasswordManagerDriver()->CheckSafeBrowsingReputation(action_url,
                                                                 frame_url);
       }
 #endif
-      if (ShouldShowStandaloneManuallFallback(element) &&
+      if (ShouldShowStandaloneManuallFallback(element, frame_url) &&
           ShowManualFallbackSuggestion(element)) {
         return true;
       }
