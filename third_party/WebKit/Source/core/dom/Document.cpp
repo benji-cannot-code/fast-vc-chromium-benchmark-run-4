b06a4205338d0588cbb7877bfb0ac73cf70d17df
Zheng, Hong <hong.zheng@intel.com>
2017-08-24 01:42:23 +0000

    Make Document::IsLoadCompleted const
    This is a part of CL
    
    https: //chromium-review.googlesource.com/c/chromium/src/+/623847
    Change-Id: I5ccfaf187e1baa1b3d8c335bfacea48fa1b7d847
    Reviewed-on: https://chromium-review.googlesource.com/628137
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496913}

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -1437,7 +1437,7 @@ void Document::SetReadyState(DocumentReadyState ready_state) {
   DispatchEvent(Event::Create(EventTypeNames::readystatechange));
 }
 
-bool Document::IsLoadCompleted() {
+bool Document::IsLoadCompleted() const {
   return ready_state_ == kComplete;
 }
 
