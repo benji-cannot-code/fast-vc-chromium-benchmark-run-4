3280dcea3926a4bcd0512da297e71cff92b1ecf7
Joy Ming <jming@chromium.org>
2017-08-24 17:12:39 +0000

    Update download notification immediately after interaction.
    Because of the refactor that makes downloads a foreground service,
    interactions with download notifications were being propagated straight
    to native by DownloadBroadcastManager before manifesting in the
    notifications through SystemDownloadNotifier. However, if native takes
    too long to load, there can be a delay between the user action and the
    notification update. This CL bridges that gap by propagating
    notification updates immediately in certain cases (pause, resume,
    cancel) in order to give feedback to the user.
    
    Bug: 755588,747563
    Change-Id: Ie078f33189cc779825e3fc7216694db412be8eb7
    Reviewed-on: https://chromium-review.googlesource.com/621786
    Commit-Queue: Joy Ming <jming@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497096}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/MockDownloadNotificationService.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/MockDownloadNotificationService.java
@@ -80,11 +80,13 @@ public class MockDownloadNotificationService extends DownloadNotificationService
 
     @Override
     void notifyDownloadPaused(ContentId id, String fileName, boolean isResumable,
-            boolean isAutoResumable, boolean isOffTheRecord, boolean isTransient, Bitmap icon) {
+            boolean isAutoResumable, boolean isOffTheRecord, boolean isTransient, Bitmap icon,
+            boolean hasUserGesture) {
         ThreadUtils.runOnUiThreadBlocking(
                 ()
                         -> MockDownloadNotificationService.super.notifyDownloadPaused(id, fileName,
-                                isResumable, isAutoResumable, isOffTheRecord, isTransient, icon));
+                                isResumable, isAutoResumable, isOffTheRecord, isTransient, icon,
+                                hasUserGesture));
     }
 
     @Override
@@ -95,9 +97,11 @@ public class MockDownloadNotificationService extends DownloadNotificationService
     }
 
     @Override
-    public void notifyDownloadCanceled(final ContentId id) {
+    public void notifyDownloadCanceled(final ContentId id, boolean hasUserGesture) {
         ThreadUtils.runOnUiThreadBlocking(
-                () -> MockDownloadNotificationService.super.notifyDownloadCanceled(id));
+                ()
+                        -> MockDownloadNotificationService.super.notifyDownloadCanceled(
+                                id, hasUserGesture));
     }
 
     @Override
