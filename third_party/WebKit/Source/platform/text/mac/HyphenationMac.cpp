7ce3668ea4c1ded72dec1bf6ae7679bf2377b51d
Koji Ishii <kojii@chromium.org>
2017-08-24 03:51:16 +0000

    Add Hyphenation::FirstHyphenLocation() for ShapingLineBreaker
    This patch adds Hyphenation::FirstHyphenLocation(), in addition to the
    LastHyphenLocation(), since ShapingLineBreaker needs to seek for break
    opportunities in both directions.
    
    Also simplified tests by using array comparisons in gmock.
    
    Bug: 636993
    Change-Id: I159b3f26fb8842f88ba514e3e8d631c50dda76ec
    Reviewed-on: https://chromium-review.googlesource.com/629116
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496942}

--- third_party/WebKit/Source/platform/text/mac/HyphenationMac.cpp
+++ third_party/WebKit/Source/platform/text/mac/HyphenationMac.cpp
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class HyphenationCF : public Hyphenation {
+class HyphenationCF final : public Hyphenation {
  public:
   HyphenationCF(RetainPtr<CFLocaleRef>& locale_cf) : locale_cf_(locale_cf) {
     DCHECK(locale_cf_);
@@ -23,6 +23,27 @@ class HyphenationCF : public Hyphenation {
         text.ToString().Impl()->CreateCFString().Get(), before_index,
         CFRangeMake(0, text.length()), 0, locale_cf_.Get(), 0);
     return result == kCFNotFound ? 0 : result;
+  }
+
+  // While Hyphenation::FirstHyphenLocation() works good, it computes all
+  // locations and discards ones after |after_index|.
+  // This version minimizes the computation for platforms that supports
+  // LastHyphenLocation() but does not support HyphenLocations().
+  size_t FirstHyphenLocation(const StringView& text, size_t after_index) const {
+    after_index =
+        std::max(after_index, static_cast<size_t>(kMinimumPrefixLength - 1));
+    size_t hyphen_location = text.length();
+    if (hyphen_location <= kMinimumSuffixLength)
+      return 0;
+    size_t max_hyphen_location = hyphen_location - kMinimumSuffixLength;
+    hyphen_location = max_hyphen_location;
+    for (;;) {
+      size_t previous = LastHyphenLocation(text, hyphen_location);
+      if (previous <= after_index)
+        break;
+      hyphen_location = previous;
+    }
+    return hyphen_location >= max_hyphen_location ? 0 : hyphen_location;
   }
 
  private:
