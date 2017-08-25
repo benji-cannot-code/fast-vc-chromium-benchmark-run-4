63c5adda45369ae83e26e3121e15f318964e60ce
Scott Graham <scottmg@chromium.org>
2017-08-25 19:05:32 +0000

    Make VideoCaptureImpl tests create their own TaskScheduler instance
    This showed up as flake on Fuchsia when the test after the
    VideoCaptureImplTest fixture tried to set a TaskScheduler instance, but
    could occur on all platforms.
    
    (Previously they were relying on ChildProcess to do this. We probably
    shouldn't do that at all, but that will be addressed in subsequent
    changes as that will likely touch a lot more tests.)
    
    Bug: 759108
    Change-Id: Icae3ee04735a6ee04bcd32db190b18edeab1857c
    Reviewed-on: https://chromium-review.googlesource.com/636192
    Commit-Queue: Scott Graham <scottmg@chromium.org>
    Commit-Queue: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497481}

--- content/renderer/media/video_capture_impl_manager_unittest.cc
+++ content/renderer/media/video_capture_impl_manager_unittest.cc
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/ref_counted.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "content/child/child_process.h"
 #include "content/common/media/media_stream_options.h"
 #include "content/common/video_capture.mojom.h"
@@ -205,7 +205,7 @@ class VideoCaptureImplManagerTest : public ::testing::Test,
                    base::Unretained(this)));
   }
 
-  const base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
   ChildProcess child_process_;
   base::RunLoop cleanup_run_loop_;
   std::unique_ptr<MockVideoCaptureImplManager> manager_;
--- content/renderer/media/video_capture_impl_unittest.cc
+++ content/renderer/media/video_capture_impl_unittest.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/shared_memory.h"
-#include "base/message_loop/message_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "content/child/child_process.h"
 #include "content/common/video_capture.mojom.h"
 #include "content/renderer/media/video_capture_impl.h"
@@ -184,7 +184,7 @@ class VideoCaptureImplTest : public ::testing::Test {
     video_capture_impl_->OnStateChanged(state);
   }
 
-  const base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
   const ChildProcess child_process_;
   const std::unique_ptr<VideoCaptureImpl> video_capture_impl_;
   MockMojoVideoCaptureHost mock_video_capture_host_;
