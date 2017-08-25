b71093562b5bdddcabaf8efc791fe732776526c5
Marc Treib <treib@chromium.org>
2017-08-25 09:00:43 +0000

    Remove InstantExtended.ShowNTP user action
    It's the only InstantExtended.* user action still left, and it's not
    that useful - we have better metrics for NTP impressions.
    Also mark InstantExtended.ShowSRP as obsolete; it hasn't existed in the
    code for a long time.
    
    Bug: 627747
    Change-Id: Ib088de03db682ae996f47b023ef4a0079768b9bf
    Reviewed-on: https://chromium-review.googlesource.com/632536
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497360}

--- chrome/browser/ui/browser_instant_controller.cc
+++ chrome/browser/ui/browser_instant_controller.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_instant_controller.h"
 
 #include "base/bind.h"
-#include "base/metrics/user_metrics.h"
 #include "chrome/browser/infobars/infobar_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search/instant_service.h"
@@ -151,12 +150,6 @@ void BrowserInstantController::TabDeactivated(content::WebContents* contents) {
 
 void BrowserInstantController::ModelChanged(const SearchMode& old_mode,
                                             const SearchMode& new_mode) {
-  // Record some actions corresponding to the mode change. Note that to get
-  // the full story, it's necessary to look at other UMA actions as well,
-  // such as tab switches.
-  if (new_mode.is_ntp())
-    base::RecordAction(base::UserMetricsAction("InstantExtended.ShowNTP"));
-
   instant_.SearchModeChanged(old_mode, new_mode);
 }
 
