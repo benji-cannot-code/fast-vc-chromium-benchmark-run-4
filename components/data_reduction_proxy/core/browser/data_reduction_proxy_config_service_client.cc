356d1718dee5327db97484f2cd2e354482d2d98e
Tarun Bansal <tbansal@chromium.org>
2017-08-25 19:42:49 +0000

    Data Reduction Proxy: Append variations headers
    Append x-client-data to client config fetch requests made by Data
    Reduction Proxy (DRP).
    
    Bug: 757095
    Change-Id: I78fc4670623b1ff5684f181aad23c93340fda8d4
    Reviewed-on: https://chromium-review.googlesource.com/627862
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497501}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_config_service_client.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_config_service_client.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/data_reduction_proxy/core/common/data_reduction_proxy_util.h"
 #include "components/data_reduction_proxy/proto/client_config.pb.h"
 #include "components/data_use_measurement/core/data_use_user_data.h"
+#include "components/variations/net/variations_http_headers.h"
 #include "net/base/host_port_pair.h"
 #include "net/base/load_flags.h"
 #include "net/base/load_timing_info.h"
@@ -391,6 +392,15 @@ void DataReductionProxyConfigServiceClient::RetrieveRemoteConfig() {
 
   fetcher_ = std::move(fetcher);
   fetch_in_progress_ = true;
+
+  // Attach variations headers.
+  net::HttpRequestHeaders headers;
+  variations::AppendVariationHeaders(config_service_url_, false /* incognito */,
+                                     false /* uma_enabled */,
+                                     false /* is_signed_in */, &headers);
+  if (!headers.IsEmpty())
+    fetcher_->SetExtraRequestHeaders(headers.ToString());
+
   fetcher_->Start();
 }
 
