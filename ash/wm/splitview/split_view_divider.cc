e1bb0c13d77f52e92e4ef7d14c45a3cf18f50a73
xdai <xdai@chromium.org>
2017-08-25 01:51:03 +0000

    Cros Splitview - Adjust the bounds of divider if screen display configuration changes.
    Note the snapped windows' bounds will be updated by
    TabletModeWindowState if screen display configuration changes.
    
    Also make SplitViewController an observer of Shell and ActivationClient only after the 
    split view mode starts, and remove it as an observer after the split view mode ends.
    
    Bug: 725683
    Change-Id: Ic5907e911aa176e4483bdd2a20c7bd70800b16c3
    Reviewed-on: https://chromium-review.googlesource.com/630020
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497290}

--- ash/wm/splitview/split_view_divider.cc
+++ ash/wm/splitview/split_view_divider.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window_targeter.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
+#include "ui/display/display.h"
 #include "ui/gfx/canvas.h"
 #include "ui/views/view.h"
 #include "ui/views/view_targeter_delegate.h"
@@ -160,6 +161,7 @@ SplitViewDivider::SplitViewDivider(SplitViewController* controller,
                                    aura::Window* root_window)
     : controller_(controller) {
   Shell::Get()->activation_client()->AddObserver(this);
+  display::Screen::GetScreen()->AddObserver(this);
   CreateDividerWidget(root_window);
 
   aura::Window* always_on_top_container =
@@ -171,6 +173,7 @@ SplitViewDivider::SplitViewDivider(SplitViewController* controller,
 
 SplitViewDivider::~SplitViewDivider() {
   Shell::Get()->activation_client()->RemoveObserver(this);
+  display::Screen::GetScreen()->RemoveObserver(this);
   divider_widget_.reset();
   split_view_window_targeter_.reset();
   for (auto* iter : observed_windows_)
@@ -235,7 +238,20 @@ void SplitViewDivider::OnWindowActivated(ActivationReason reason,
                                          aura::Window* lost_active) {
   auto iter = std::find(observed_windows_.begin(), observed_windows_.end(),
                         gained_active);
-  divider_widget_->SetAlwaysOnTop(iter != observed_windows_.end());
+  if (iter != observed_windows_.end()) {
+    divider_widget_->SetAlwaysOnTop(true);
+  } else {
+    divider_widget_->SetAlwaysOnTop(false);
+    divider_widget_->Deactivate();
+  }
+}
+
+void SplitViewDivider::OnDisplayMetricsChanged(const display::Display& display,
+                                               uint32_t metrics) {
+  // Update the bounds of the divider.
+  bool is_dragging = divider_widget_->GetWindowBoundsInScreen().width() ==
+                     kDividerEnlargedShortSideLength;
+  UpdateDividerBounds(is_dragging);
 }
 
 void SplitViewDivider::CreateDividerWidget(aura::Window* root_window) {
--- ash/wm/splitview/split_view_divider.h
+++ ash/wm/splitview/split_view_divider.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_observer.h"
+#include "ui/display/display_observer.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
@@ -33,7 +34,8 @@ class SplitViewController;
 // to resize the left and right windows accordingly. The divider widget should
 // always placed above its observed windows to be able to receive events.
 class ASH_EXPORT SplitViewDivider : public aura::WindowObserver,
-                                    public ::wm::ActivationChangeObserver {
+                                    public ::wm::ActivationChangeObserver,
+                                    public display::DisplayObserver {
  public:
   SplitViewDivider(SplitViewController* controller, aura::Window* root_window);
   ~SplitViewDivider() override;
@@ -59,6 +61,10 @@ class ASH_EXPORT SplitViewDivider : public aura::WindowObserver,
   void OnWindowActivated(ActivationReason reason,
                          aura::Window* gained_active,
                          aura::Window* lost_active) override;
+
+  // display::DisplayObserver:
+  void OnDisplayMetricsChanged(const display::Display& display,
+                               uint32_t metrics) override;
 
   views::Widget* divider_widget() { return divider_widget_.get(); }
 
