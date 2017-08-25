6ac91a434c4a37110588dba4b25d8541b8c94cc8
tzik <tzik@chromium.org>
2017-08-25 05:22:47 +0000

    Deflake ObfuscatedFileUtilTest..TestRevokeUsageCache
    AsyncFileTestHelper::GetUsageAndQuota waits for QuotaManager to complete
    its GetUsageAndQuota until the current message loop is idle. However,
    it may post a job to other thread, and the idle loop doesn't imply the
    task completion.
    
    After this change, AsyncFileTestHelper waits for the completion until
    the completion callback runs.
    
    Bug: 758232
    Change-Id: I9a6bd65b14affa1a322ae447dd315b6f8e95a439
    Reviewed-on: https://chromium-review.googlesource.com/634087
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497328}

--- storage/browser/test/async_file_test_helper.cc
+++ storage/browser/test/async_file_test_helper.cc
@@ -77,6 +77,7 @@ void ReadDirectoryCallback(base::RunLoop* run_loop,
 void DidGetUsageAndQuota(storage::QuotaStatusCode* status_out,
                          int64_t* usage_out,
                          int64_t* quota_out,
+                         base::OnceClosure done_callback,
                          storage::QuotaStatusCode status,
                          int64_t usage,
                          int64_t quota) {
@@ -86,6 +87,8 @@ void DidGetUsageAndQuota(storage::QuotaStatusCode* status_out,
     *usage_out = usage;
   if (quota_out)
     *quota_out = quota;
+  if (done_callback)
+    std::move(done_callback).Run();
 }
 
 }  // namespace
@@ -260,10 +263,12 @@ storage::QuotaStatusCode AsyncFileTestHelper::GetUsageAndQuota(
     int64_t* usage,
     int64_t* quota) {
   storage::QuotaStatusCode status = storage::kQuotaStatusUnknown;
+  base::RunLoop run_loop;
   quota_manager->GetUsageAndQuota(
       origin, FileSystemTypeToQuotaStorageType(type),
-      base::Bind(&DidGetUsageAndQuota, &status, usage, quota));
-  base::RunLoop().RunUntilIdle();
+      base::Bind(&DidGetUsageAndQuota, &status, usage, quota,
+                 run_loop.QuitWhenIdleClosure()));
+  run_loop.Run();
   return status;
 }
 
