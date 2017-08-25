9780c75bed4bb011ced0c88f0e6c3d9d9b8f11a7
Paul Miller <paulmiller@chromium.org>
2017-08-25 01:17:59 +0000

    Revert "Add a way to check for handle_all_urls verification to OriginVerifier"
    This reverts commit 33aac55ba944d05d2bf6854d4ad4a79837cf9924.
    
    Reason for revert: depends on
    c822c477e0a3fb4934d92bfd3fbb517e6d7ad8e4, which was reverted.
    
    Original change's description:
    > Add a way to check for handle_all_urls verification to OriginVerifier
    > 
    > - Adds the relation type that will be moved to support lib later.
    > - Makes a new OriginVerifier for every verify call with the given
    > relationship.
    > - Handles the caching to be per package name/relation pair so that both
    > relations get verified independently.
    > - Adds a test for the above case.
    > 
    > BUG=748274
    > 
    > Change-Id: I61833f1b5e70f3532b67cf3910a6b004053bf9a0
    > Reviewed-on: https://chromium-review.googlesource.com/584012
    > Commit-Queue: Yusuf Ozuysal <yusufo@chromium.org>
    > Reviewed-by: Benoit L <lizeb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#497255}
    
    TBR=yusufo@chromium.org,lizeb@chromium.org
    
    Change-Id: I746ca7a52c25a0db2c25dae9015fdc91a2002357
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 748274
    Reviewed-on: https://chromium-review.googlesource.com/634569
    Reviewed-by: Paul Miller <paulmiller@chromium.org>
    Commit-Queue: Paul Miller <paulmiller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497279}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/ClientManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/ClientManager.java
@@ -16,7 +16,6 @@ import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.customtabs.CustomTabsCallback;
 import android.support.customtabs.CustomTabsService;
-import android.support.customtabs.CustomTabsService.Relation;
 import android.support.customtabs.CustomTabsSessionToken;
 import android.text.TextUtils;
 import android.util.SparseBooleanArray;
@@ -346,13 +345,13 @@ class ClientManager {
     }
 
     /**
-     * See {@link PostMessageHandler#verifyAndInitializeWithOrigin(Uri, int)}.
+     * See {@link PostMessageHandler#verifyAndInitializeWithOrigin(Uri)}.
      */
     public synchronized void verifyAndInitializeWithPostMessageOriginForSession(
-            CustomTabsSessionToken session, Uri origin, @Relation int relation) {
+            CustomTabsSessionToken session, Uri origin) {
         SessionParams params = mSessionParams.get(session);
         if (params == null) return;
-        params.postMessageHandler.verifyAndInitializeWithOrigin(origin, relation);
+        params.postMessageHandler.verifyAndInitializeWithOrigin(origin);
     }
 
     /**
@@ -525,9 +524,7 @@ class ClientManager {
     public synchronized boolean isFirstPartyOriginForSession(
             CustomTabsSessionToken session, Uri origin) {
         SessionParams params = mSessionParams.get(session);
-        return params == null ? false
-                              : OriginVerifier.isValidOrigin(params.packageName, origin,
-                                        CustomTabsService.RELATION_USE_AS_ORIGIN);
+        return params == null ? false : OriginVerifier.isValidOrigin(params.packageName, origin);
     }
 
     /** Tries to bind to a client to keep it alive, and returns true for success. */
