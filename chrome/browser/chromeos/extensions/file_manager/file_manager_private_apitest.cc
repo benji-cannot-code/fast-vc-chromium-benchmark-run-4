e03f2c6849cff8ebf06e1542371ddac711f53233
Klemen Kozjek <klemenko@google.com>
2017-08-25 20:20:09 +0000

    Files App: Add Rename to cros disks client.
    This CL adds Rename method to the disk client implementation which
    through dbus invokes cros disks disk renaming functionality. Before
    renaming happens on the mounted disk, client will save its mount point
    name for later when re-mounting will occur and try to re-mount the disk
    to previous mount point name to preserve file paths.
    
    BUG=274041
    CQ-DEPEND=CL:605108
    
    Change-Id: I40d0522cb5195940b0b949418a99e2eaca1e1041
    Reviewed-on: https://chromium-review.googlesource.com/616510
    Commit-Queue: Klemen Kozjek <klemenko@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Tomasz Mikolajewski <mtomasz@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Naoki Fukino <fukino@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497518}

--- chrome/browser/chromeos/extensions/file_manager/file_manager_private_apitest.cc
+++ chrome/browser/chromeos/extensions/file_manager/file_manager_private_apitest.cc
@@ -52,6 +52,7 @@ struct TestDiskInfo {
   bool on_removable_device;
   bool is_hidden;
   const char* file_system_type;
+  const char* base_mount_path;
 };
 
 struct TestMountPoint {
@@ -83,7 +84,8 @@ TestDiskInfo kTestDisks[] = {{"system_path1",
                               false,
                               false,
                               false,
-                              "exfat"},
+                              "exfat",
+                              ""},
                              {"system_path2",
                               "file_path2",
                               false,
@@ -103,7 +105,8 @@ TestDiskInfo kTestDisks[] = {{"system_path1",
                               true,
                               false,
                               false,
-                              "exfat"},
+                              "exfat",
+                              ""},
                              {"system_path3",
                               "file_path3",
                               true,  // write_disabled_by_policy
@@ -123,7 +126,8 @@ TestDiskInfo kTestDisks[] = {{"system_path1",
                               true,
                               false,
                               false,
-                              "exfat"}};
+                              "exfat",
+                              ""}};
 
 void DispatchDirectoryChangeEventImpl(
     int* counter,
@@ -290,7 +294,8 @@ class FileManagerPrivateApiTest : public ExtensionApiTest {
                 kTestDisks[disk_info_index].on_boot_device,
                 kTestDisks[disk_info_index].on_removable_device,
                 kTestDisks[disk_info_index].is_hidden,
-                kTestDisks[disk_info_index].file_system_type)));
+                kTestDisks[disk_info_index].file_system_type,
+                kTestDisks[disk_info_index].base_mount_path)));
       }
     }
   }
--- chrome/browser/chromeos/file_manager/volume_manager_unittest.cc
+++ chrome/browser/chromeos/file_manager/volume_manager_unittest.cc
@@ -44,6 +44,8 @@ class LoggingObserver : public VolumeManagerObserver {
       VOLUME_UNMOUNTED,
       FORMAT_STARTED,
       FORMAT_COMPLETED,
+      RENAME_STARTED,
+      RENAME_COMPLETED
     } type;
 
     // Available on DEVICE_ADDED, DEVICE_REMOVED, VOLUME_MOUNTED,
@@ -127,6 +129,23 @@ class LoggingObserver : public VolumeManagerObserver {
                          bool success) override {
     Event event;
     event.type = Event::FORMAT_COMPLETED;
+    event.device_path = device_path;
+    event.success = success;
+    events_.push_back(event);
+  }
+
+  void OnRenameStarted(const std::string& device_path, bool success) override {
+    Event event;
+    event.type = Event::RENAME_STARTED;
+    event.device_path = device_path;
+    event.success = success;
+    events_.push_back(event);
+  }
+
+  void OnRenameCompleted(const std::string& device_path,
+                         bool success) override {
+    Event event;
+    event.type = Event::RENAME_COMPLETED;
     event.device_path = device_path;
     event.success = success;
     events_.push_back(event);
@@ -244,7 +263,7 @@ TEST_F(VolumeManagerTest, OnDiskEvent_Hidden) {
   const chromeos::disks::DiskMountManager::Disk kDisk(
       "device1", "", false, "", "", "", "", "", "", "", "", "", "",
       chromeos::DEVICE_TYPE_UNKNOWN, 0, false, false, false, false, false,
-      kIsHidden, "");
+      kIsHidden, "", "");
 
   volume_manager()->OnDiskEvent(
       chromeos::disks::DiskMountManager::DISK_ADDED, &kDisk);
@@ -272,7 +291,7 @@ TEST_F(VolumeManagerTest, OnDiskEvent_Added) {
       "",  // empty device path.
       "", false, "", "", "", "", "", "", "", "", "", "",
       chromeos::DEVICE_TYPE_UNKNOWN, 0, false, false, false, false, false,
-      false, "");
+      false, "", "");
   volume_manager()->OnDiskEvent(
       chromeos::disks::DiskMountManager::DISK_ADDED, &kEmptyDevicePathDisk);
   EXPECT_EQ(0U, observer.events().size());
@@ -281,7 +300,7 @@ TEST_F(VolumeManagerTest, OnDiskEvent_Added) {
   const chromeos::disks::DiskMountManager::Disk kMediaDisk(
       "device1", "", false, "", "", "", "", "", "", "", "", "", "",
       chromeos::DEVICE_TYPE_UNKNOWN, 0, false, false, kHasMedia, false, false,
-      false, "");
+      false, "", "");
   volume_manager()->OnDiskEvent(
       chromeos::disks::DiskMountManager::DISK_ADDED, &kMediaDisk);
   ASSERT_EQ(1U, observer.events().size());
@@ -314,7 +333,7 @@ TEST_F(VolumeManagerTest, OnDiskEvent_AddedNonMounting) {
     const chromeos::disks::DiskMountManager::Disk kMountedMediaDisk(
         "device1", "mounted", false, "", "", "", "", "", "", "", "", "", "",
         chromeos::DEVICE_TYPE_UNKNOWN, 0, false, false, kHasMedia, false, false,
-        false, "");
+        false, "", "");
     volume_manager()->OnDiskEvent(
         chromeos::disks::DiskMountManager::DISK_ADDED, &kMountedMediaDisk);
     ASSERT_EQ(1U, observer.events().size());
@@ -337,7 +356,7 @@ TEST_F(VolumeManagerTest, OnDiskEvent_AddedNonMounting) {
     const chromeos::disks::DiskMountManager::Disk kNoMediaDisk(
         "device1", "", false, "", "", "", "", "", "", "", "", "", "",
         chromeos::DEVICE_TYPE_UNKNOWN, 0, false, false, kWithoutMedia, false,
-        false, false, "");
+        false, false, "", "");
     volume_manager()->OnDiskEvent(
         chromeos::disks::DiskMountManager::DISK_ADDED, &kNoMediaDisk);
     ASSERT_EQ(1U, observer.events().size());
@@ -362,7 +381,7 @@ TEST_F(VolumeManagerTest, OnDiskEvent_AddedNonMounting) {
     const chromeos::disks::DiskMountManager::Disk kMediaDisk(
         "device1", "", false, "", "", "", "", "", "", "", "", "", "",
         chromeos::DEVICE_TYPE_UNKNOWN, 0, false, false, kHasMedia, false, false,
-        false, "");
+        false, "", "");
     volume_manager()->OnDiskEvent(
         chromeos::disks::DiskMountManager::DISK_ADDED, &kMediaDisk);
     ASSERT_EQ(1U, observer.events().size());
@@ -384,7 +403,7 @@ TEST_F(VolumeManagerTest, OnDiskEvent_Removed) {
   const chromeos::disks::DiskMountManager::Disk kMountedDisk(
       "device1", "mount_path", false, "", "", "", "", "", "", "", "", "", "",
       chromeos::DEVICE_TYPE_UNKNOWN, 0, false, false, false, false, false,
-      false, "");
+      false, "", "");
   volume_manager()->OnDiskEvent(
       chromeos::disks::DiskMountManager::DISK_REMOVED, &kMountedDisk);
 
@@ -409,7 +428,7 @@ TEST_F(VolumeManagerTest, OnDiskEvent_RemovedNotMounted) {
   const chromeos::disks::DiskMountManager::Disk kNotMountedDisk(
       "device1", "", false, "", "", "", "", "", "", "", "", "", "",
       chromeos::DEVICE_TYPE_UNKNOWN, 0, false, false, false, false, false,
-      false, "");
+      false, "", "");
   volume_manager()->OnDiskEvent(
       chromeos::disks::DiskMountManager::DISK_REMOVED, &kNotMountedDisk);
 
@@ -431,7 +450,7 @@ TEST_F(VolumeManagerTest, OnDiskEvent_Changed) {
   const chromeos::disks::DiskMountManager::Disk kDisk(
       "device1", "", false, "", "", "", "", "", "", "", "", "", "",
       chromeos::DEVICE_TYPE_UNKNOWN, 0, false, false, true, false, false, false,
-      "");
+      "", "");
   volume_manager()->OnDiskEvent(
       chromeos::disks::DiskMountManager::DISK_CHANGED, &kDisk);
 
@@ -455,7 +474,7 @@ TEST_F(VolumeManagerTest, OnDiskEvent_ChangedInReadonly) {
   const chromeos::disks::DiskMountManager::Disk kDisk(
       "device1", "", false, "", "", "", "", "", "", "", "", "", "",
       chromeos::DEVICE_TYPE_UNKNOWN, 0, false, false, true, false, false, false,
-      "");
+      "", "");
   volume_manager()->OnDiskEvent(chromeos::disks::DiskMountManager::DISK_CHANGED,
                                 &kDisk);
 
@@ -550,7 +569,7 @@ TEST_F(VolumeManagerTest, OnMountEvent_Remounting) {
       new chromeos::disks::DiskMountManager::Disk(
           "device1", "", false, "", "", "", "", "", "", "", "", "uuid1", "",
           chromeos::DEVICE_TYPE_UNKNOWN, 0, false, false, false, false, false,
-          false, ""));
+          false, "", ""));
   disk_mount_manager_->AddDiskForTest(std::move(disk));
   disk_mount_manager_->MountPath("device1", "", "", chromeos::MOUNT_TYPE_DEVICE,
                                  chromeos::MOUNT_ACCESS_MODE_READ_WRITE);
@@ -760,7 +779,7 @@ TEST_F(VolumeManagerTest, ExternalStorageDisabledPolicyMultiProfile) {
   const chromeos::disks::DiskMountManager::Disk kMediaDisk(
       "device1", "", false, "", "", "", "", "", "", "", "", "", "",
       chromeos::DEVICE_TYPE_UNKNOWN, 0, false, false, true, false, false, false,
-      "");
+      "", "");
   volume_manager()->OnDiskEvent(
       chromeos::disks::DiskMountManager::DISK_ADDED, &kMediaDisk);
   secondary.volume_manager()->OnDiskEvent(
