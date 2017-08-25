f5c320b357812427640439158982737154a0a9bf
Tsuyoshi Horo <horo@chromium.org>
2017-08-25 16:01:13 +0000

    Reland "Stop using WorkerURLLoaderFactoryProvider"
    Reason for reland: http://crbug.com/758981#c3
    
    This is a reland of b73429ef4968c087931bc30f787357e8e0e1d4d0
    Original change's description:
    > Stop using WorkerURLLoaderFactoryProvider
    > 
    > I introduced WorkerURLLoaderFactoryProvider in e612058 to:
    >  1) Get the URLLoaderFactory in the worker thread.
    >  2) Register the WorkerFetchContextImpl in a (dedicated or shared) worker thread
    >     as a ServiceWorkerWorkerClient to the parent document's ServiceWorkerProviderHost
    >     in the browser process to receive SetControllerServiceWorker() method call.
    > 
    > But now we can get the URLLoaderFactory in the main thread and make a clone of
    > it and provide that to WorkerFetchContextImpl.
    > So this cl deletes WorkerURLLoaderFactoryProvider and use the Clone method of
    > URLLoaderFactory.
    > 
    > And also this cl changes the caller of SetControllerServiceWorker() from the
    > ServiceWorkerProviderHost in the browser process to ServiceWorkerProviderContext
    > in the main thread of the renderer process.
    > 
    > OffMainThreadFetch was disabled when NetworkService is enabled because
    > WorkerURLLoaderFactoryProvider can't a URLLoaderFactory. But we can enable it
    > now.
    > 
    > 
    > Bug: 756571
    > Change-Id: Ib2a549ce23dc0a612a2df88c1f45af1383192ad3
    > Reviewed-on: https://chromium-review.googlesource.com/620510
    > Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#497372}
    
    TBR=falken@chromium.org,pasko@chromium.org,dcheng@chromium.org,kinuko@chromium.org
    
    Bug: 756571,758981
    Change-Id: I3735b2841bdc91efb31da2b9dbea8636136aa5f9
    Reviewed-on: https://chromium-review.googlesource.com/635823
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497422}

--- content/renderer/renderer_blink_platform_impl.cc
+++ content/renderer/renderer_blink_platform_impl.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/files/file_path.h"
 #include "base/guid.h"
 #include "base/lazy_instance.h"
@@ -27,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "components/url_formatter/url_formatter.h"
 #include "content/child/blob_storage/webblobregistry_impl.h"
+#include "content/child/child_url_loader_factory_getter.h"
 #include "content/child/database_util.h"
 #include "content/child/file_info_util.h"
 #include "content/child/fileapi/webfilesystem_impl.h"
@@ -198,6 +200,13 @@ media::AudioParameters GetAudioHardwareParams() {
       .output_params();
 }
 
+mojom::URLLoaderFactoryPtr GetBlobURLLoaderFactoryGetter() {
+  mojom::URLLoaderFactoryPtr blob_loader_factory;
+  RenderThreadImpl::current()->GetRendererHost()->GetBlobURLLoaderFactory(
+      mojo::MakeRequest(&blob_loader_factory));
+  return blob_loader_factory;
+}
+
 }  // namespace
 
 //------------------------------------------------------------------------------
@@ -325,6 +334,15 @@ std::unique_ptr<blink::WebURLLoader> RendererBlinkPlatformImpl::CreateURLLoader(
   return base::MakeUnique<WebURLLoaderImpl>(
       child_thread ? child_thread->resource_dispatcher() : nullptr, task_runner,
       url_loader_factory_.get());
+}
+
+scoped_refptr<ChildURLLoaderFactoryGetter>
+RendererBlinkPlatformImpl::CreateDefaultURLLoaderFactoryGetter() {
+  return base::MakeRefCounted<ChildURLLoaderFactoryGetter>(
+      CreateNetworkURLLoaderFactory(),
+      base::FeatureList::IsEnabled(features::kNetworkService)
+          ? base::BindOnce(&GetBlobURLLoaderFactoryGetter)
+          : ChildURLLoaderFactoryGetter::URLLoaderFactoryGetterCallback());
 }
 
 PossiblyAssociatedInterfacePtr<mojom::URLLoaderFactory>
