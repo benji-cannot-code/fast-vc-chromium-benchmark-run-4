01045fed67c8fde088d43a8d74940ad1a4d7c1ac
Joy Ming <jming@chromium.org>
2017-08-25 23:15:01 +0000

    Add DownloadsForegroundService in AndroidManifest.
    This is part of a larger refactor to make downloads a foreground
    service on all versions of Android. Before this CL, the Downloads
    Foreground Service was not actually activated because its inclusion in
    the Android Manifest was accidentally omitted. This CL activates the
    Downloads Foreground Service in addition to solving a few of the bugs
    that arose while this omission was corrected, namely the double
    notification that was displayed because of the use of a notification
    tag (filed a bug with the Android notifications team b/65052774) as
    well as the logic behind the killing of a cancelled notification.
    
    Bug: 747563,758688
    Change-Id: Iae27a9e58384d515ed400403c93633facee2e3c0
    Reviewed-on: https://chromium-review.googlesource.com/633836
    Reviewed-by: Maria Khomenko <mariakhomenko@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Joy Ming <jming@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497587}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadNotificationService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadNotificationService.java
@@ -57,8 +57,6 @@ public class DownloadNotificationService {
     public static final String ACTION_DOWNLOAD_OPEN =
             "org.chromium.chrome.browser.download.DOWNLOAD_OPEN";
 
-    public static final String NOTIFICATION_NAMESPACE = "DownloadNotificationService";
-
     public static final String EXTRA_NOTIFICATION_BUNDLE_ICON_ID =
             "Chrome.NotificationBundleIconIdExtra";
     /** Notification Id starting value, to avoid conflicts from IDs used in prior versions. */
@@ -201,9 +199,8 @@ public class DownloadNotificationService {
             long timeRemainingInMillis, long startTime, boolean isOffTheRecord,
             boolean canDownloadWhileMetered, boolean isDownloadPending, boolean isTransient,
             Bitmap icon, boolean hasUserGesture) {
-        int notificationId = getNotificationId(id);
         Context context = ContextUtils.getApplicationContext();
-
+        int notificationId = getNotificationId(id);
         DownloadUpdate downloadUpdate = new DownloadUpdate.Builder()
                                                 .setContentId(id)
                                                 .setFileName(fileName)
@@ -237,7 +234,8 @@ public class DownloadNotificationService {
     }
 
     public void cancelNotification(int notificationId) {
-        mNotificationManager.cancel(NOTIFICATION_NAMESPACE, notificationId);
+        // TODO(b/65052774): Add back NOTIFICATION_NAMESPACE when able to.
+        mNotificationManager.cancel(notificationId);
     }
 
     /**
@@ -307,9 +305,9 @@ public class DownloadNotificationService {
             stopTrackingInProgressDownload(id);
             return;
         }
-        int notificationId = entry == null ? getNotificationId(id) : entry.notificationId;
-        Context context = ContextUtils.getApplicationContext();
 
+        Context context = ContextUtils.getApplicationContext();
+        int notificationId = entry == null ? getNotificationId(id) : entry.notificationId;
         DownloadUpdate downloadUpdate = new DownloadUpdate.Builder()
                                                 .setContentId(id)
                                                 .setFileName(fileName)
@@ -318,10 +316,8 @@ public class DownloadNotificationService {
                                                 .setIcon(icon)
                                                 .setNotificationId(notificationId)
                                                 .build();
-
         Notification notification = DownloadNotificationFactory.buildNotification(
                 context, DownloadNotificationFactory.DownloadStatus.PAUSED, downloadUpdate);
-
         updateNotification(notificationId, notification, id,
                 new DownloadSharedPreferenceEntry(id, notificationId, isOffTheRecord,
                         canDownloadWhileMetered, fileName, isAutoResumable, isTransient));
@@ -407,9 +403,8 @@ public class DownloadNotificationService {
             fileName = entry.fileName;
         }
 
-        int notificationId = getNotificationId(id);
         Context context = ContextUtils.getApplicationContext();
-
+        int notificationId = getNotificationId(id);
         DownloadUpdate downloadUpdate = new DownloadUpdate.Builder()
                                                 .setContentId(id)
                                                 .setFileName(fileName)
@@ -453,7 +448,8 @@ public class DownloadNotificationService {
 
     @VisibleForTesting
     void updateNotification(int id, Notification notification) {
-        mNotificationManager.notify(NOTIFICATION_NAMESPACE, id, notification);
+        // TODO(b/65052774): Add back NOTIFICATION_NAMESPACE when able to.
+        mNotificationManager.notify(id, notification);
     }
 
     private void updateNotification(int notificationId, Notification notification, ContentId id,
@@ -492,11 +488,10 @@ public class DownloadNotificationService {
      * already in progress, do nothing.
      */
     void resumeAllPendingDownloads() {
-        Context context = ContextUtils.getApplicationContext();
         List<DownloadSharedPreferenceEntry> entries = mDownloadSharedPreferenceHelper.getEntries();
         for (int i = 0; i < entries.size(); ++i) {
             DownloadSharedPreferenceEntry entry = entries.get(i);
-            if (!canResumeDownload(context, entry)) continue;
+            if (!canResumeDownload(ContextUtils.getApplicationContext(), entry)) continue;
             if (mDownloadsInProgress.contains(entry.id)) continue;
 
             Intent intent = new Intent();
