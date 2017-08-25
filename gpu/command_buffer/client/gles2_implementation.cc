5b083729bb7cee414fcb79e7f9a575df201db538
Antoine Labour <piman@chromium.org>
2017-08-25 21:23:12 +0000

    Remove WaitForCmd in GLES2Implementation::FreeEverything
    It's no longer needed for correctness, nor to make sure we release as
    much as possible.
    
    Bug: 741215
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ica02c588e5af7c5e52a86cf036df24775f5f3709
    Reviewed-on: https://chromium-review.googlesource.com/634469
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497539}

--- gpu/command_buffer/client/gles2_implementation.cc
+++ gpu/command_buffer/client/gles2_implementation.cc
@@ -343,7 +343,6 @@ void GLES2Implementation::FreeUnusedSharedMemory() {
 }
 
 void GLES2Implementation::FreeEverything() {
-  WaitForCmd();
   query_tracker_->Shrink(helper_);
   FreeUnusedSharedMemory();
   transfer_buffer_->Free();
