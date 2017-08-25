78e9ffb879d97c69a1367504594fdb9d58146fd3
Lei Zhang <thestig@chromium.org>
2017-08-25 22:46:53 +0000

    Remove ANNOTATE_SCOPED_MEMORY_LEAK in HostResolverImpl::LoopbackProbeJob.
    BUG=248513
    
    Change-Id: I04db97b51014c87f95f3c4c6f112b80258790a20
    Reviewed-on: https://chromium-review.googlesource.com/630760
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497574}

--- net/dns/host_resolver_impl.cc
+++ net/dns/host_resolver_impl.cc
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "base/compiler_specific.h"
 #include "base/debug/debugger.h"
-#include "base/debug/leak_annotations.h"
 #include "base/debug/stack_trace.h"
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
@@ -1044,13 +1043,6 @@ class HostResolverImpl::LoopbackProbeJob {
                    base::TaskRunner* worker_task_runner)
       : resolver_(resolver), result_(false) {
     DCHECK(resolver.get());
-
-    // |worker_task_runner| may posts tasks to the WorkerPool, so need this to
-    // avoid reporting worker pool leaks in tests. The WorkerPool doesn't have a
-    // flushing API, so can't do anything about them, other than using another
-    // task runner.
-    // http://crbug.com/248513
-    ANNOTATE_SCOPED_MEMORY_LEAK;
 
     worker_task_runner->PostTaskAndReply(
         FROM_HERE,
