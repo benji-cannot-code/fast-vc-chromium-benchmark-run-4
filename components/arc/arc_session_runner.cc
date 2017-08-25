c8bbe5e7dd3b8e9cb8001e3ed577530bc537e8c6
Luis Hector Chavez <lhchavez@google.com>
2017-08-25 21:56:21 +0000

    arc: Add '--arc-start-mode=only-start-after-login'
    This change adds a flag to delay starting the ARC instance until after
    the user has logged in.
    
    BUG=b/64759916
    TEST=ARC still starts, with and without this flag.
    TEST=With this flag, no ARC instance is present in login screen
    
    Change-Id: I065d89fe5aa9833b79c28cd41427942fb619f873
    Reviewed-on: https://chromium-review.googlesource.com/636110
    Commit-Queue: Luis Hector Chavez <lhchavez@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497555}

--- components/arc/arc_session_runner.cc
+++ components/arc/arc_session_runner.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/task_runner.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
+#include "components/arc/arc_util.h"
 
 namespace arc {
 
@@ -237,6 +238,11 @@ void ArcSessionRunner::OnSessionStopped(ArcStopReason stop_reason) {
 }
 
 void ArcSessionRunner::EmitLoginPromptVisibleCalled() {
+  if (ShouldArcOnlyStartAfterLogin()) {
+    // Skip starting ARC for now. We'll have another chance to start the full
+    // instance after the user logs in.
+    return;
+  }
   // Since 'login-prompt-visible' Upstart signal starts all Upstart jobs the
   // container may depend on such as cras, EmitLoginPromptVisibleCalled() is the
   // safe place to start the container for login screen.
