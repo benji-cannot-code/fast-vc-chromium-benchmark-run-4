ee7ca78d4593aa0ed6213ecd3e0fb04d092524b2
Piotr Swigon <piotrs@chromium.org>
2017-08-25 01:48:33 +0000

    [WebApps] Exposes Share and Open in Chrome through Android Notification.
    With this patch standalone web apps on Android will by accompanied by an
    ongoing notification which exposes the website URL, discloses that the
    app runs in Chrome and gives access to 'Share' and 'Open in Chrome'
    actions.
    
    Note that this feature is in its early days and is behind a flag. The
    plan is to iterate on a number of details while it's available on Canary.
    
    Screenshot of how the notification looks like as of this patch:
    Android N: https://goo.gl/photos/V6CnxEWZbSXyMiUF8
    Android M: https://goo.gl/photos/bxRKtzGzU5PFzTua8
    
    Bug: 471623
    Change-Id: I84860ffeab1423701abacaa13037a12836bbe3a5
    Reviewed-on: https://chromium-review.googlesource.com/627522
    Commit-Queue: Piotr Swigon <piotrs@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497285}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappActivity.java
@@ -80,6 +80,7 @@ public class WebappActivity extends SingleTabActivity {
             | View.SYSTEM_UI_FLAG_LOW_PROFILE
             | View.SYSTEM_UI_FLAG_IMMERSIVE;
 
+    private final WebappActionsNotificationManager mNotificationManager;
     private final WebappDirectoryManager mDirectoryManager;
 
     private WebContents mWebContents;
@@ -113,6 +114,7 @@ public class WebappActivity extends SingleTabActivity {
         mWebappInfo = createWebappInfo(null);
         mDirectoryManager = new WebappDirectoryManager();
         mSplashController = createWebappSplashScreenController();
+        mNotificationManager = new WebappActionsNotificationManager(this);
     }
 
     protected WebappSplashScreenController createWebappSplashScreenController() {
@@ -122,6 +124,9 @@ public class WebappActivity extends SingleTabActivity {
     @Override
     protected void onNewIntent(Intent intent) {
         if (intent == null) return;
+
+        if (mNotificationManager.handleNotificationAction(intent)) return;
+
         super.onNewIntent(intent);
 
         WebappInfo newWebappInfo = popWebappInfo(WebappInfo.idFromIntent(intent));
@@ -341,6 +346,18 @@ public class WebappActivity extends SingleTabActivity {
     }
 
     @Override
+    public void onResumeWithNative() {
+        super.onResumeWithNative();
+        mNotificationManager.maybeShowNotification();
+    }
+
+    @Override
+    public void onPauseWithNative() {
+        mNotificationManager.cancelNotification();
+        super.onPauseWithNative();
+    }
+
+    @Override
     protected void initDeferredStartupForActivity() {
         super.initDeferredStartupForActivity();
 
@@ -494,6 +511,17 @@ public class WebappActivity extends SingleTabActivity {
         return new EmptyTabObserver() {
 
             @Override
+            public void onDidFinishNavigation(Tab tab, String url, boolean isInMainFrame,
+                    boolean isErrorPage, boolean hasCommitted, boolean isSameDocument,
+                    boolean isFragmentNavigation, Integer pageTransition, int errorCode,
+                    int httpStatusCode) {
+                if (hasCommitted && isInMainFrame) {
+                    // Updates the URL.
+                    mNotificationManager.maybeShowNotification();
+                }
+            }
+
+            @Override
             public void onDidChangeThemeColor(Tab tab, int color) {
                 if (!isWebappDomain()) return;
                 mBrandColor = color;
@@ -637,7 +665,11 @@ public class WebappActivity extends SingleTabActivity {
     public boolean onMenuOrKeyboardAction(int id, boolean fromMenu) {
         if (id == R.id.open_in_browser_id) {
             openCurrentUrlInChrome();
-            RecordUserAction.record("WebappMenuOpenInChrome");
+            if (fromMenu) {
+                RecordUserAction.record("WebappMenuOpenInChrome");
+            } else {
+                RecordUserAction.record("Webapp.NotificationOpenInChrome");
+            }
             return true;
         }
         return super.onMenuOrKeyboardAction(id, fromMenu);
