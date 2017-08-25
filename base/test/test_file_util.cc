11f647986c533148d7d895f4989521521dcda7eb
Sergey Ulanov <sergeyu@google.com>
2017-08-25 23:43:36 +0000

    Export operator<< for FilePath. Remove PrintTo().
    file_path.h defines operator<< for FilePath, but it wasn't marked 
    as BASE_EXPORT, so it was impossible to use it outside of base.
    
    Also removed PrintTo(FilePath) - it's not longer needed since we have
    operator<<.
    
    Change-Id: I2632b59ae4b9c7daced480058cd07b187986c7c1
    Reviewed-on: https://chromium-review.googlesource.com/621927
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497598}

--- base/test/test_file_util.cc
+++ base/test/test_file_util.cc
@@ -21,9 +21,4 @@ bool EvictFileFromSystemCacheWithRetry(const FilePath& path) {
   return false;
 }
 
-// Declared in base/files/file_path.h.
-void PrintTo(const FilePath& path, std::ostream* out) {
-  *out << path.value();
-}
-
 }  // namespace base
