43da25222629293bc3e6da3f57b7e8feecdf2057
Vladislav Kaznacheev <kaznacheev@google.com>
2017-08-25 16:18:00 +0000

    Use chrome prefs to control metalayer availability.
    Bug: 758389
    Change-Id: Ie78c9ebf15707ffc9b58f384e106b250e30e2d73
    Reviewed-on: https://chromium-review.googlesource.com/630277
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Vladislav Kaznacheev <kaznacheev@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497431}

--- ash/shell.cc
+++ ash/shell.cc
@@ -624,6 +624,11 @@ void Shell::NotifyVoiceInteractionEnabled(bool enabled) {
     observer.OnVoiceInteractionEnabled(enabled);
 }
 
+void Shell::NotifyVoiceInteractionContextEnabled(bool enabled) {
+  for (auto& observer : shell_observers_)
+    observer.OnVoiceInteractionContextEnabled(enabled);
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // Shell, private:
 
