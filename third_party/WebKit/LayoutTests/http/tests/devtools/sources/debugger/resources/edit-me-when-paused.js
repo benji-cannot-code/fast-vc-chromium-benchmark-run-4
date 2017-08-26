df29d3b3519250d465455aaa36b79ec6158a69bb
Will Chen <chenwilliam@chromium.org>
2017-08-26 02:51:14 +0000

    DevTools: move some inspector/sources/debugger* tests (part 2)
    Bug: 667560
    Change-Id: I8d370bf22976b68563466ff425727ba67a4c3ae6
    Reviewed-on: https://chromium-review.googlesource.com/636287
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497642}

---
+++ third_party/WebKit/LayoutTests/http/tests/devtools/sources/debugger/resources/edit-me-when-paused.js
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function f2()
+{
+    return 1;
+}
+
+function f1()
+{
+    var x = 1;
+    debugger;
+    return x + f2();
+}
