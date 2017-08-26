3047ad499f55dd5e6131eec68027c6a50baebfdc
srirama <srirama.m@samsung.com>
2017-08-26 05:05:58 +0000

    Use ToIntSize in LocalFrameView::MinimumScrollOffsetInt()
    Change-Id: Iea8707d3db19b234008502d9ed6f8b275ef81571
    Reviewed-on: https://chromium-review.googlesource.com/635184
    Commit-Queue: srirama chandra sekhar <srirama.m@samsung.com>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497652}

--- third_party/WebKit/Source/core/frame/LocalFrameView.cpp
+++ third_party/WebKit/Source/core/frame/LocalFrameView.cpp
@@ -4072,7 +4072,7 @@ void LocalFrameView::ClipPaintRect(FloatRect* paint_rect) const {
 }
 
 IntSize LocalFrameView::MinimumScrollOffsetInt() const {
-  return IntSize(-ScrollOrigin().X(), -ScrollOrigin().Y());
+  return ToIntSize(-ScrollOrigin());
 }
 
 void LocalFrameView::AdjustScrollbarOpacity() {
