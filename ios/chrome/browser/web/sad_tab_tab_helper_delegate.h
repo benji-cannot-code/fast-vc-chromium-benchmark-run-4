60c9ac6ea3d03819ae0e69b1aea8a7492da25795
sczs <sczs@chromium.org>
2017-08-26 01:16:40 +0000

    [ios] Refactor SadTab so its presented via a delegate.
    This CL changes the SadTab presentation so its done via a delegate.
    Tab is used as a delegate and it will create the actual SadTabView and
    present it.
    
    Bug: 758453
    Change-Id: Ic3ee0f310cc942ff8182fdefebd85fec77e16e9f
    Reviewed-on: https://chromium-review.googlesource.com/631043
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497625}

---
+++ ios/chrome/browser/web/sad_tab_tab_helper_delegate.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_WEB_SAD_TAB_TAB_HELPER_DELEGATE_H_
+#define IOS_CHROME_BROWSER_WEB_SAD_TAB_TAB_HELPER_DELEGATE_H_
+
+#import <Foundation/Foundation.h>
+
+class SadTabTabHelper;
+
+// Delegate for SadTabTabHelper.
+@protocol SadTabTabHelperDelegate<NSObject>
+
+// Asks the delegate to present a SadTabView.
+- (void)sadTabHelper:(SadTabTabHelper*)tabHelper
+    presentSadTabForRepeatedFailure:(BOOL)repeatedFailure;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_WEB_SAD_TAB_TAB_HELPER_DELEGATE_H_
