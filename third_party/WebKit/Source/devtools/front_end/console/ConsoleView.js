d1000cddd2bbfc51bc5d6559493a95a31d53518e
Erik Luo <luoe@chromium.org>
2017-08-26 02:50:34 +0000

    DevTools: show counters for each sidebar group
    This CL adds upon the Log Management experiment by adding counters to
    each group, showing little icons with the number of info/warn/error
    messages from each group.
    
    Screenshot: http://imgur.com/a/wRmrI
    
    Bug: 685283
    Change-Id: I8180dc849ee6e8f9ba4c84fff63389a5f3848903
    Reviewed-on: https://chromium-review.googlesource.com/631056
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497641}

--- third_party/WebKit/Source/devtools/front_end/console/ConsoleView.js
+++ third_party/WebKit/Source/devtools/front_end/console/ConsoleView.js
@@ -448,6 +448,7 @@ Console.ConsoleView = class extends UI.VBox {
       this._needsFullUpdate = true;
     }
 
+    this._sidebar.onMessageAdded(message);
     this._scheduleViewportRefresh();
     this._consoleMessageAddedForTest(viewMessage);
 
@@ -483,10 +484,6 @@ Console.ConsoleView = class extends UI.VBox {
    * @param {!Console.ConsoleViewMessage} viewMessage
    */
   _appendMessageToEnd(viewMessage) {
-    var context = viewMessage.consoleMessage().context;
-    if (context)
-      this._sidebar.addGroup({name: context, context: context});
-
     if (!this._filter.shouldBeVisible(viewMessage)) {
       this._hiddenByFilterCount++;
       return;
