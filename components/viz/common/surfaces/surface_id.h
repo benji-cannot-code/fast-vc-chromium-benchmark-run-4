6be7897792965cb30cef396e53851f584a49fecf
Saman Sami <samans@chromium.org>
2017-08-26 03:22:15 +0000

    Move SurfaceId's mojom from cc to to viz
    Also merge build targets for compositing and hit_test in
    services/viz/public/interfaces, otherwise they would form a circular
    dependency.
    
    TBR=weiliangc@chromium.org,piman@chromium.org,tsepez@chromium.org,sadrul@chromium.org,thakis@chromium.org
    
    Bug: 722935
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I8bdafceb593367987a85cdf0c5dcedbd4becb221
    Reviewed-on: https://chromium-review.googlesource.com/633752
    Commit-Queue: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: Saman Sami <samans@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: weiliangc <weiliangc@chromium.org>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497647}

--- components/viz/common/surfaces/surface_id.h
+++ components/viz/common/surfaces/surface_id.h
@@ -18,13 +18,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/viz_common_export.h"
 #include "mojo/public/cpp/bindings/struct_traits.h"
 
-namespace cc {
+namespace viz {
+
 namespace mojom {
 class SurfaceIdDataView;
 }
-}  // namespace cc
-
-namespace viz {
 
 class VIZ_COMMON_EXPORT SurfaceId {
  public:
@@ -71,7 +69,7 @@ class VIZ_COMMON_EXPORT SurfaceId {
   }
 
  private:
-  friend struct mojo::StructTraits<cc::mojom::SurfaceIdDataView, SurfaceId>;
+  friend struct mojo::StructTraits<mojom::SurfaceIdDataView, SurfaceId>;
 
   FrameSinkId frame_sink_id_;
   LocalSurfaceId local_surface_id_;
