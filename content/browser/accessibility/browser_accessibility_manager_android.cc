6f55be5d68f1a226b71ca6c10e964dcc54c3c62c
Dominic Mazzoni <dmazzoni@chromium.org>
2017-08-26 01:04:52 +0000

    Fix Android optimization in SendLocationChangeEvents
    BrowserAccessibilityManagerAndroid::SendLocationChangeEvents contains an
    optimization (387488, crbug.com/588889) that avoids sending too many events
    when a lot of objects on the screen change their bounding box at the same
    time - for example, when rotating the screen.
    
    This optimization was accidentally broken by r478954, which accidentally
    dropped the "return" at the end of the conditional, causing it to run the
    efficient version and then the slow version.
    
    Bug: 734353
    Change-Id: I3f8abe9b6d4188257c5cc316f71f5ef07f345ecd
    Reviewed-on: https://chromium-review.googlesource.com/636169
    Reviewed-by: Paul Miller <paulmiller@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497622}

--- content/browser/accessibility/browser_accessibility_manager_android.cc
+++ content/browser/accessibility/browser_accessibility_manager_android.cc
@@ -182,6 +182,7 @@ void BrowserAccessibilityManagerAndroid::SendLocationChangeEvents(
     if (!wcax)
       return;
     wcax->SendDelayedWindowContentChangedEvent();
+    return;
   }
   BrowserAccessibilityManager::SendLocationChangeEvents(params);
 }
