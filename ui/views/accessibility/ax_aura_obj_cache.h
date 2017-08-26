cb0875ca17cf2cc377836ee7ac6d1589358cce63
David Tseng <dtseng@chromium.org>
2017-08-26 22:16:43 +0000

    Cleanup: unify window observer handling
    - both AXAuraObjWrapper and AXWindowObjWrapper serve as WindowObservers; make it so only the latter does (thereby hitting the same observer methods)
    - removes fragile code that forgets to unobserve focus changes
    - makes things less stateful
    
    Bug: 
    Change-Id: I54f505fb8c4772fd65a1a1ae5499c51a3562e304
    Reviewed-on: https://chromium-review.googlesource.com/636024
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497662}

--- ui/views/accessibility/ax_aura_obj_cache.h
+++ ui/views/accessibility/ax_aura_obj_cache.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "ui/accessibility/ax_enums.h"
 #include "ui/aura/client/focus_change_observer.h"
-#include "ui/aura/window_observer.h"
 #include "ui/views/views_export.h"
 
 namespace base {
@@ -23,9 +22,6 @@ template <typename T> struct DefaultSingletonTraits;
 }
 
 namespace aura {
-namespace client {
-class FocusClient;
-}
 class Window;
 }  // namespace aura
 
@@ -35,9 +31,7 @@ class View;
 class Widget;
 
 // A cache responsible for assigning id's to a set of interesting Aura views.
-class VIEWS_EXPORT AXAuraObjCache
-    : public aura::client::FocusChangeObserver,
-      public aura::WindowObserver {
+class VIEWS_EXPORT AXAuraObjCache : public aura::client::FocusChangeObserver {
  public:
   // Get the single instance of this class.
   static AXAuraObjCache* GetInstance();
@@ -95,6 +89,12 @@ class VIEWS_EXPORT AXAuraObjCache
   // Indicates if this object's currently being destroyed.
   bool is_destroying() { return is_destroying_; }
 
+  // Notifies this cache of a change in root window.
+  void OnRootWindowObjCreated(aura::Window* window);
+
+  // Notifies this cache of a change in root window.
+  void OnRootWindowObjDestroyed(aura::Window* window);
+
   void SetDelegate(Delegate* delegate) { delegate_ = delegate; }
 
  private:
@@ -108,10 +108,6 @@ class VIEWS_EXPORT AXAuraObjCache
   // aura::client::FocusChangeObserver override.
   void OnWindowFocused(aura::Window* gained_focus,
                        aura::Window* lost_focus) override;
-
-  // aura::WindowObserver override.
-  void OnWindowDestroying(aura::Window* window) override;
-  void OnWindowHierarchyChanged(const HierarchyChangeParams& params) override;
 
   template <typename AuraViewWrapper, typename AuraView>
   AXAuraObjWrapper* CreateInternal(
@@ -133,8 +129,6 @@ class VIEWS_EXPORT AXAuraObjCache
 
   std::map<int32_t, std::unique_ptr<AXAuraObjWrapper>> cache_;
   int32_t current_id_;
-
-  aura::client::FocusClient* focus_client_;
 
   // True immediately when entering this object's destructor.
   bool is_destroying_;
