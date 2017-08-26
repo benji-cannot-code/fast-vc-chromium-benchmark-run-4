55a5cd48a2be46a06874fd8fb7f19c9506076d13
Kurt Horimoto <kkhorimoto@chromium.org>
2017-08-26 01:28:33 +0000

    [iOS Clean] Added JavaScript dialog support.
    This CL adds a coordinator managing a DialogViewController that displays
    JavaScript dialogs.
    
    Bug: none
    Change-Id: Iab3a5d2456e43320d0d81de9c84b78a9e606ac8f
    Reviewed-on: https://chromium-review.googlesource.com/588373
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497628}

---
+++ ios/clean/chrome/browser/ui/commands/java_script_dialog_commands.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CLEAN_CHROME_BROWSER_UI_COMMANDS_JAVA_SCRIPT_DIALOG_COMMANDS_H_
+#define IOS_CLEAN_CHROME_BROWSER_UI_COMMANDS_JAVA_SCRIPT_DIALOG_COMMANDS_H_
+
+// Command protocol for dismissing JavaScript dialogs.
+@protocol JavaScriptDialogDismissalCommands<NSObject>
+
+// Called after the user interaction with the JavaScript dialog has been handled
+// and the dialog can be dismissed.
+- (void)dismissJavaScriptDialog;
+
+@end
+
+#endif  // IOS_CLEAN_CHROME_BROWSER_UI_COMMANDS_JAVA_SCRIPT_DIALOG_COMMANDS_H_
---
+++ ios/clean/chrome/browser/ui/dialogs/java_script_dialogs/java_script_dialog_coordinator.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CLEAN_CHROME_BROWSER_UI_DIALOGS_JAVA_SCRIPT_JAVA_SCRIPT_DIALOG_COORDINATOR_H_
+#define IOS_CLEAN_CHROME_BROWSER_UI_DIALOGS_JAVA_SCRIPT_JAVA_SCRIPT_DIALOG_COORDINATOR_H_
+
+#import <Foundation/Foundation.h>
+
+#import "ios/clean/chrome/browser/ui/dialogs/dialog_coordinator.h"
+
+@class JavaScriptDialogRequest;
+
+// An overlay coordinator for the UI elements used to present JavaScript
+// dialogs.
+@interface JavaScriptDialogCoordinator : DialogCoordinator
+
+// Designated initializer to display a JavaScript dialog for |request|.
+- (instancetype)initWithRequest:(JavaScriptDialogRequest*)request
+    NS_DESIGNATED_INITIALIZER;
+- (instancetype)init NS_UNAVAILABLE;
+
+@end
+
+#endif  // IOS_CLEAN_CHROME_BROWSER_UI_DIALOGS_JAVA_SCRIPT_JAVA_SCRIPT_DIALOG_COORDINATOR_H_
