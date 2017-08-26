1a1610334b10c40e6cb9f1f05bb8f7b7690f64b4
Yusuf Ozuysal <yusufo@google.com>
2017-08-26 01:36:09 +0000

    Changes to policy around referrers in external intents
    - Add a java class for WebReferrerPolicy so that values can be reflected
    truthfully wrt Blink equivalents
    - Add a new extra to define referral policy the client wants to add.
    - Add a way to include https:// referrers if the client app can be
    verified to have a 1st party relationship with the referrer domain.
    
    BUG=725921
    
    Change-Id: Ib3a0dfdabefe5669646fffac5924a675db7cc5eb
    Reviewed-on: https://chromium-review.googlesource.com/620148
    Commit-Queue: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Maria Khomenko <mariakhomenko@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497630}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/ClientManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/ClientManager.java
@@ -123,11 +123,11 @@ class ClientManager {
     private static class SessionParams {
         public final int uid;
         public final DisconnectCallback disconnectCallback;
-        public final String packageName;
         public final PostMessageHandler postMessageHandler;
         public boolean mIgnoreFragments;
         public boolean lowConfidencePrediction;
         public boolean highConfidencePrediction;
+        private String mPackageName;
         private boolean mShouldHideDomain;
         private boolean mShouldPrerenderOnCellular;
         private boolean mShouldSendNavigationInfo;
@@ -140,11 +140,25 @@ class ClientManager {
         public SessionParams(Context context, int uid, DisconnectCallback callback,
                 PostMessageHandler postMessageHandler) {
             this.uid = uid;
-            packageName = getPackageName(context, uid);
+            mPackageName = getPackageName(context, uid);
             disconnectCallback = callback;
             this.postMessageHandler = postMessageHandler;
-            if (postMessageHandler != null) this.postMessageHandler.setPackageName(packageName);
+            if (postMessageHandler != null) this.postMessageHandler.setPackageName(mPackageName);
             this.mSpeculationMode = CustomTabsConnection.SpeculationParams.PRERENDER;
+        }
+
+        /**
+         * Overrides package name with given String. TO be used for testing only.
+         */
+        void overridePackageNameForTesting(String newPackageName) {
+            mPackageName = newPackageName;
+        }
+
+        /**
+         * @return The package name for this session.
+         */
+        public String getPackageName() {
+            return mPackageName;
         }
 
         private static String getPackageName(Context context, int uid) {
@@ -331,8 +345,7 @@ class ClientManager {
     public synchronized boolean bindToPostMessageServiceForSession(CustomTabsSessionToken session) {
         SessionParams params = mSessionParams.get(session);
         if (params == null) return false;
-        return params.postMessageHandler.bindSessionToPostMessageService(
-                mContext, params.packageName);
+        return params.postMessageHandler.bindSessionToPostMessageService();
     }
 
     /**
@@ -379,10 +392,8 @@ class ClientManager {
      * @return The referrer that is associated with the client owning given session.
      */
     public synchronized Referrer getReferrerForSession(CustomTabsSessionToken session) {
-        SessionParams params = mSessionParams.get(session);
-        if (params == null) return null;
-        final String packageName = params.packageName;
-        return IntentHandler.constructValidReferrerForAuthority(packageName);
+        return IntentHandler.constructValidReferrerForAuthority(
+                getClientPackageNameForSession(session));
     }
 
     /**
@@ -390,7 +401,17 @@ class ClientManager {
      */
     public synchronized String getClientPackageNameForSession(CustomTabsSessionToken session) {
         SessionParams params = mSessionParams.get(session);
-        return params == null ? null : params.packageName;
+        return params == null ? null : params.getPackageName();
+    }
+
+    /**
+     * Overrides the package name for the given session to be the given package name. To be used
+     * for testing only.
+     */
+    public synchronized void overridePackageNameForSession(
+            CustomTabsSessionToken session, String packageName) {
+        SessionParams params = mSessionParams.get(session);
+        if (params != null) params.overridePackageNameForTesting(packageName);
     }
 
     /**
@@ -524,10 +545,8 @@ class ClientManager {
      */
     public synchronized boolean isFirstPartyOriginForSession(
             CustomTabsSessionToken session, Uri origin) {
-        SessionParams params = mSessionParams.get(session);
-        return params == null ? false
-                              : OriginVerifier.isValidOrigin(params.packageName, origin,
-                                        CustomTabsService.RELATION_USE_AS_ORIGIN);
+        return OriginVerifier.isValidOrigin(getClientPackageNameForSession(session), origin,
+                CustomTabsService.RELATION_USE_AS_ORIGIN);
     }
 
     /** Tries to bind to a client to keep it alive, and returns true for success. */
