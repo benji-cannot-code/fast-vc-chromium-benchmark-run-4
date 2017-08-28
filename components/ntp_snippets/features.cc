3f02edc98bc093872d1fecbca7c48ec97d03e03b
Chris Pickel <sfiera@chromium.org>
2017-08-28 11:36:03 +0000

    Add NTPRemoteSuggestionsBackend feature
    Originally, this was added to our Finch configuration for the purpose of
    holding an experiment ID, and nothing else. Since there were no other
    intended behavior changes client-side, there was no client-side feature
    created.
    
    However, being listed in kAllFeatures is important: experiment IDs
    associated with kAllFeatures are shown in chrome://snippets-internals.
    If the feature isn't listed there, then we lose our ability to tell what
    backend Chrome is using based on the internals page.
    
    Change-Id: I55b83c92d58f3f4d792578ef8890e43901e79a90
    Reviewed-on: https://chromium-review.googlesource.com/637693
    Reviewed-by: vitaliii <vitaliii@chromium.org>
    Commit-Queue: Chris Pickel <sfiera@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497735}

--- components/ntp_snippets/features.cc
+++ components/ntp_snippets/features.cc
@@ -14,6 +14,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ntp_snippets {
 
+// Holds an experiment ID. So long as the feature is set through a server-side
+// variations config, this feature should exist on the client. This ensures that
+// the experiment ID is visible in chrome://snippets-internals.
+const base::Feature kRemoteSuggestionsBackendFeature{
+    "NTPRemoteSuggestionsBackend", base::FEATURE_DISABLED_BY_DEFAULT};
+
 // Keep sorted, and keep nullptr at the end.
 const base::Feature* const kAllFeatures[] = {
     &kArticleSuggestionsFeature,
@@ -29,6 +35,7 @@ const base::Feature* const kAllFeatures[] = {
     &kPhysicalWebPageSuggestionsFeature,
     &kPublisherFaviconsFromNewServerFeature,
     &kRecentOfflineTabSuggestionsFeature,
+    &kRemoteSuggestionsBackendFeature,
     nullptr};
 
 const base::Feature kArticleSuggestionsFeature{
