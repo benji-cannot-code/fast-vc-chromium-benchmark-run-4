36afeac6ccad228a5ba217be51a2edbf14927b81
Jeffrey Kardatzke <jkardatzke@google.com>
2017-08-28 21:33:16 +0000

    Block OEMCrypto connection from ARC when Verified Access is disabled
    Since we use Remote Attestation as the root of trust for the OEMCrypto
    Widevine L1 DRM implementation in ARC, we need to block usage of it
    if the user turns off Verified Access in settings.
    
    The code also moved locations to avoid circular dependency issues that
    would have arose due to the usage of
    chrome/browser/chromeos/settings/cros_settings.h if we left it in
    components/arc.
    
    BUG=b:62576736
    TEST=Verified correct behavior in Android based on Chrome setting
    
    Change-Id: I8b5b810c8479b605a9d2830612e3296f530a65d1
    Reviewed-on: https://chromium-review.googlesource.com/638750
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497885}

--- chrome/browser/chromeos/arc/oemcrypto/arc_oemcrypto_bridge.cc
+++ chrome/browser/chromeos/arc/oemcrypto/arc_oemcrypto_bridge.cc
@@ -3,10 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/arc/oemcrypto/arc_oemcrypto_bridge.h"
+#include "chrome/browser/chromeos/arc/oemcrypto/arc_oemcrypto_bridge.h"
 
 #include "base/bind.h"
 #include "base/memory/singleton.h"
+#include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chromeos/dbus/arc_oemcrypto_client.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "components/arc/arc_bridge_service.h"
@@ -85,6 +86,24 @@ void ArcOemCryptoBridge::OnBootstrapMojoConnection(
 
 void ArcOemCryptoBridge::Connect(mojom::OemCryptoServiceRequest request) {
   DVLOG(1) << "ArcOemCryptoBridge::Connect called";
+
+  // Check that the user has Attestation for Content Protection enabled in
+  // their Chrome settings and if they do not then block this connection since
+  // OEMCrypto utilizes Attestation as the root of trust for its DRM
+  // implementation.
+  bool attestation_enabled = false;
+  if (!chromeos::CrosSettings::Get()->GetBoolean(
+          chromeos::kAttestationForContentProtectionEnabled,
+          &attestation_enabled)) {
+    LOG(ERROR) << "Failed to get attestation device setting";
+    return;
+  }
+  if (!attestation_enabled) {
+    DVLOG(1) << "OEMCrypto L1 DRM denied because Verified Access is disabled "
+                "for this device.";
+    return;
+  }
+
   if (oemcrypto_host_ptr_.is_bound()) {
     DVLOG(1) << "Re-using bootstrap connection for OemCryptoService Connect";
     oemcrypto_host_ptr_->Connect(std::move(request));
--- chrome/browser/chromeos/arc/oemcrypto/arc_oemcrypto_bridge.h
+++ chrome/browser/chromeos/arc/oemcrypto/arc_oemcrypto_bridge.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_ARC_OEMCRYPTO_ARC_OEMCRYPTO_BRIDGE_H_
-#define COMPONENTS_ARC_OEMCRYPTO_ARC_OEMCRYPTO_BRIDGE_H_
+#ifndef CHROME_BROWSER_CHROMEOS_ARC_OEMCRYPTO_ARC_OEMCRYPTO_BRIDGE_H_
+#define CHROME_BROWSER_CHROMEOS_ARC_OEMCRYPTO_ARC_OEMCRYPTO_BRIDGE_H_
 
 #include <stdint.h>
 
@@ -60,4 +60,4 @@ class ArcOemCryptoBridge
 
 }  // namespace arc
 
-#endif  // COMPONENTS_ARC_OEMCRYPTO_ARC_OEMCRYPTO_BRIDGE_H_
+#endif  // CHROME_BROWSER_CHROMEOS_ARC_OEMCRYPTO_ARC_OEMCRYPTO_BRIDGE_H_
