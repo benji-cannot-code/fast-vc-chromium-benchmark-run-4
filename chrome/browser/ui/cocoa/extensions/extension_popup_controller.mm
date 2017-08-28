02870cb51fb778b05fa64237495ca24a3187e72c
Leonard Grey <lgrey@chromium.org>
2017-08-28 15:25:23 +0000

    [Mac] RTL-support for resizable extension bubbles
    Bug: 748579, 748578
    Change-Id: Iec3f43459d7a62e1be19cb44b828594eedb8866a
    Reviewed-on: https://chromium-review.googlesource.com/638133
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497763}

--- chrome/browser/ui/cocoa/extensions/extension_popup_controller.mm
+++ chrome/browser/ui/cocoa/extensions/extension_popup_controller.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/browser_window_cocoa.h"
 #import "chrome/browser/ui/cocoa/extensions/extension_view_mac.h"
 #import "chrome/browser/ui/cocoa/info_bubble_window.h"
+#import "chrome/browser/ui/cocoa/l10n_util.h"
 #include "chrome/common/url_constants.h"
 #include "components/web_modal/web_contents_modal_dialog_manager.h"
 #include "content/public/browser/browser_context.h"
@@ -351,7 +352,11 @@ class ExtensionPopupNotificationBridge :
                                   info_bubble::kBubbleArrowWidth / 2.0,
                               info_bubble::kBubbleArrowHeight / 2.0);
   offsets = [extensionView_ convertSize:offsets toView:nil];
-  windowOrigin.x -= NSWidth(frame) - offsets.width;
+  if (cocoa_l10n_util::ShouldDoExperimentalRTLLayout()) {
+    windowOrigin.x -= offsets.width;
+  } else {
+    windowOrigin.x -= NSWidth(frame) - offsets.width;
+  }
   windowOrigin.y -= NSHeight(frame) - offsets.height;
   frame.origin = windowOrigin;
 
