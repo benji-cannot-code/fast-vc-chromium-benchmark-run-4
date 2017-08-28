583b2403e8a041098dd217057918c0b3c4cd6815
Keishi Hattori <keishi@chromium.org>
2017-08-28 13:21:36 +0000

    Move GC_PLUGIN_IGNORE to EventTargetWithInlineData::event_target_data_
    Move GC_PLUGIN_IGNORE to EventTargetWithInlineData::event_target_data_ so the plugin can detect missing EventTargetWithInlineData base class trace.
    
    Bug: None
    Change-Id: I4522827b6222f1d0942b4553b0d4e1bd94487eac
    Reviewed-on: https://chromium-review.googlesource.com/632196
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497749}

--- third_party/WebKit/Source/core/events/EventTarget.h
+++ third_party/WebKit/Source/core/events/EventTarget.h
@@ -223,12 +223,7 @@ class CORE_EXPORT EventTarget : public GarbageCollectedFinalized<EventTarget>,
   friend class EventListenerIterator;
 };
 
-// EventTargetData is a GCed object, so it should not be used as a part of
-// object. However, we intentionally use it as a part of object for performance,
-// assuming that no one extracts a pointer of
-// EventTargetWithInlineData::m_eventTargetData and store it to a Member etc.
-class GC_PLUGIN_IGNORE("513199") CORE_EXPORT EventTargetWithInlineData
-    : public EventTarget {
+class CORE_EXPORT EventTargetWithInlineData : public EventTarget {
  public:
   ~EventTargetWithInlineData() override {}
 
@@ -247,7 +242,11 @@ class GC_PLUGIN_IGNORE("513199") CORE_EXPORT EventTargetWithInlineData
   EventTargetData& EnsureEventTargetData() final { return event_target_data_; }
 
  private:
-  EventTargetData event_target_data_;
+  // EventTargetData is a GCed object, so it should not be used as a part of
+  // object. However, we intentionally use it as a part of object for
+  // performance, assuming that no one extracts a pointer of
+  // EventTargetWithInlineData::m_eventTargetData and store it to a Member etc.
+  GC_PLUGIN_IGNORE("513199") EventTargetData event_target_data_;
 };
 
 // FIXME: These macros should be split into separate DEFINE and DECLARE
