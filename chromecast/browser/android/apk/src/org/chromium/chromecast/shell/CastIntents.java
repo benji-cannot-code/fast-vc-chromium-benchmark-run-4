df070ddf3ddaf0f6c2b4f089df1c83639e7720e9
Simeon Anfinrud <sanfin@chromium.org>
2017-08-28 21:20:43 +0000

    [Chromecast] Extract CastAudioManager from cast_shell_java.
    This allows internal targets to depend on just
    cast_audio_manager_java without bringing in the huge set of
    transitive dependencies of cast_shell_java.
    
    This also factors out the constants for Intents into their own
    library, so that libraries sending broadcasts of these Intents
    don't have to depend on cast_shell_java directly either.
    
    BUG=Internal b/65059265
    
    Change-Id: Icf5ae83f8b97a9b7ce8e973441a0ed91cbf34870
    Reviewed-on: https://chromium-review.googlesource.com/638556
    Commit-Queue: Simeon Anfinrud <sanfin@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497877}

---
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastIntents.java
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chromecast.shell;
+
+/**
+ * A namespace for constants to uniquely describe certain public Intents that can be used to control
+ * the life cycle of CastWebContentsActivity.
+ */
+public class CastIntents {
+    public static final String ACTION_STOP_ACTIVITY =
+            "com.google.android.apps.castshell.intent.action.STOP_ACTIVITY";
+    public static final String ACTION_SCREEN_OFF =
+            "com.google.android.apps.castshell.intent.action.ACTION_SCREEN_OFF";
+}
