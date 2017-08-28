3d37f1bbafa37c2972c824200a6b28e014cf7f71
Ria Jiang <riajiang@chromium.org>
2017-08-28 22:38:09 +0000

    mus: Ignore the client id sent by clients.
    Instead of using the client id sent by clients as the ClientWindowId
    ::client_id part in WS, WindowTree would ignore invalid client ids
    sent by clients and use its WindowTree::id_ as the client_id. Clients
    can still use their server_id to reference their windows as we are
    using WindowTree::id_ as the high 16 bits before looking up windows
    as well. After this change, ClientWindowIds in WS should be globally
    unique.
    
    From https://chromium-review.googlesource.com/c/608270 to avoid gerrit
    issues.
    
    Bug: 712302
    Test: covered by tests
    Change-Id: I8cf2466ac532566fe6733e044e131dc8ffd401c2
    Reviewed-on: https://chromium-review.googlesource.com/617378
    Commit-Queue: Ria Jiang <riajiang@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497908}

--- services/ui/ws/test_change_tracker.cc
+++ services/ui/ws/test_change_tracker.cc
@@ -224,7 +224,6 @@ void WindowDatasToTestWindows(const std::vector<mojom::WindowDataPtr>& data,
 
 Change::Change()
     : type(CHANGE_TYPE_EMBED),
-      client_id(0),
       window_id(0),
       window_id2(0),
       window_id3(0),
@@ -244,12 +243,9 @@ TestChangeTracker::TestChangeTracker() : delegate_(NULL) {}
 
 TestChangeTracker::~TestChangeTracker() {}
 
-void TestChangeTracker::OnEmbed(ClientSpecificId client_id,
-                                mojom::WindowDataPtr root,
-                                bool drawn) {
+void TestChangeTracker::OnEmbed(mojom::WindowDataPtr root, bool drawn) {
   Change change;
   change.type = CHANGE_TYPE_EMBED;
-  change.client_id = client_id;
   change.bool_value = drawn;
   change.windows.push_back(WindowDataToTestWindow(root));
   AddChange(change);
