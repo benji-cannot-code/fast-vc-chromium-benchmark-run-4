8417b5915a13cd390079d0121ffdfde997118860
Tom Sepez <tsepez@chromium.org>
2017-08-28 18:55:47 +0000

    Add sandbox_type field to service manager catalog entries.
    First step in being able to specify sandboxing requirements
    using the same manifest in which services are specified.
    
    Bug: 715679
    Change-Id: I696081e42a712ec3ee154384b14578bd21fe5ca5
    Reviewed-on: https://chromium-review.googlesource.com/624732
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497818}

--- services/catalog/entry.cc
+++ services/catalog/entry.cc
@@ -159,6 +159,11 @@ std::unique_ptr<Entry> Entry::Deserialize(const base::Value& manifest_root) {
   }
   entry->set_display_name(std::move(display_name));
 
+  // Sandbox type, optional.
+  std::string sandbox_type;
+  if (value.GetString(Store::kSandboxTypeKey, &sandbox_type))
+    entry->set_sandbox_type(std::move(sandbox_type));
+
   // InterfaceProvider specs.
   const base::DictionaryValue* interface_provider_specs = nullptr;
   if (!value.GetDictionary(Store::kInterfaceProviderSpecsKey,
@@ -222,8 +227,8 @@ bool Entry::ProvidesCapability(const std::string& capability) const {
 }
 
 bool Entry::operator==(const Entry& other) const {
-  return other.name_ == name_ &&
-         other.display_name_ == display_name_ &&
+  return other.name_ == name_ && other.display_name_ == display_name_ &&
+         other.sandbox_type_ == sandbox_type_ &&
          other.interface_provider_specs_ == interface_provider_specs_;
 }
 
