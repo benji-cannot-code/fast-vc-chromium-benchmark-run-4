269c97cdf09afaaa8cf02fd7653c9cd17493db25
Jerome Jiang <jianj@google.com>
2017-08-28 19:37:58 +0000

    Roll src/third_party/libvpx/source/libvpx/ 6b9c691da..30c261b1e (27 commits)
    https://chromium.googlesource.com/webm/libvpx.git/+log/6b9c691dafc8..30c261b1ebe8
    
    $ git log 6b9c691da..30c261b1e --date=short --no-merges --format='%ad %ae %s'
    2017-08-23 marpan vp9: SVC: Skip NEWMV for small blocks for (0, 0) base_mv.
    2017-08-22 johannkoenig quantize avx: copy implementation to intrinsics
    2017-08-21 johannkoenig quantize neon: round dqcoeff towards zero
    2017-08-10 johannkoenig quantize fp: neon implementation
    2017-08-22 marpan vp9: Condition lighting change detection on CBR mode.
    2017-08-22 johannkoenig quantize ssse3: copy style from sse2
    2017-08-22 johannkoenig quantize sse2: copy opts from ssse3
    2017-08-22 johannkoenig quantize: capture skip block early
    2017-08-21 johannkoenig quantize: ignore skip_block in arm
    2017-08-21 johannkoenig quantize: ignore skip_block in x86
    2017-08-21 johannkoenig quantize fp: ignore skip_block in arm
    2017-08-21 johannkoenig quantize fp: ignore skip_block in x86
    2017-08-02 johannkoenig quantize test: test _fp_ version of quantize
    2017-08-16 johannkoenig Remove skip_block from quantize
    2017-08-18 slavarnway vpx_dsp: get32x32var_avx2() cleanup
    2017-08-07 slavarnway vpx_dsp: vpx_get16x16var_avx2() cleanup
    2017-08-02 yinshiyou-hf vpx_dsp:loongson optimize vpx_subtract_block_c (case 4x4,8x8,16x16) with mmi.
    2017-08-17 jzern highbd_idct32x32*,idct32_34_4x32_quarter_1_2: fix typo
    2017-08-16 johannkoenig quantize: normalize intermediate types
    2017-08-16 jzern inv_txfm_sse2.h: correct idct*/iadst* prototypes
    (...)
    
    Created with:
      roll-dep src/third_party/libvpx/source/libvpx
    R=johannkoenig@google.com
    
    Bug: 
    Change-Id: I950d78242ad44264c1a4f5e1a985956fea24affa
    Reviewed-on: https://chromium-review.googlesource.com/636045
    Reviewed-by: Tom Finegan <tomfinegan@chromium.org>
    Commit-Queue: Tom Finegan <tomfinegan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497842}

--- third_party/libvpx/source/config/linux/x64/vpx_dsp_rtcd.h
+++ third_party/libvpx/source/config/linux/x64/vpx_dsp_rtcd.h
@@ -3696,13 +3696,35 @@ void vpx_highbd_idct32x32_1024_add_c(const tran_low_t* input,
                                      uint16_t* dest,
                                      int stride,
                                      int bd);
-#define vpx_highbd_idct32x32_1024_add vpx_highbd_idct32x32_1024_add_c
+void vpx_highbd_idct32x32_1024_add_sse2(const tran_low_t* input,
+                                        uint16_t* dest,
+                                        int stride,
+                                        int bd);
+void vpx_highbd_idct32x32_1024_add_sse4_1(const tran_low_t* input,
+                                          uint16_t* dest,
+                                          int stride,
+                                          int bd);
+RTCD_EXTERN void (*vpx_highbd_idct32x32_1024_add)(const tran_low_t* input,
+                                                  uint16_t* dest,
+                                                  int stride,
+                                                  int bd);
 
 void vpx_highbd_idct32x32_135_add_c(const tran_low_t* input,
                                     uint16_t* dest,
                                     int stride,
                                     int bd);
-#define vpx_highbd_idct32x32_135_add vpx_highbd_idct32x32_135_add_c
+void vpx_highbd_idct32x32_135_add_sse2(const tran_low_t* input,
+                                       uint16_t* dest,
+                                       int stride,
+                                       int bd);
+void vpx_highbd_idct32x32_135_add_sse4_1(const tran_low_t* input,
+                                         uint16_t* dest,
+                                         int stride,
+                                         int bd);
+RTCD_EXTERN void (*vpx_highbd_idct32x32_135_add)(const tran_low_t* input,
+                                                 uint16_t* dest,
+                                                 int stride,
+                                                 int bd);
 
 void vpx_highbd_idct32x32_1_add_c(const tran_low_t* input,
                                   uint16_t* dest,
@@ -3718,7 +3740,18 @@ void vpx_highbd_idct32x32_34_add_c(const tran_low_t* input,
                                    uint16_t* dest,
                                    int stride,
                                    int bd);
-#define vpx_highbd_idct32x32_34_add vpx_highbd_idct32x32_34_add_c
+void vpx_highbd_idct32x32_34_add_sse2(const tran_low_t* input,
+                                      uint16_t* dest,
+                                      int stride,
+                                      int bd);
+void vpx_highbd_idct32x32_34_add_sse4_1(const tran_low_t* input,
+                                        uint16_t* dest,
+                                        int stride,
+                                        int bd);
+RTCD_EXTERN void (*vpx_highbd_idct32x32_34_add)(const tran_low_t* input,
+                                                uint16_t* dest,
+                                                int stride,
+                                                int bd);
 
 void vpx_highbd_idct4x4_16_add_c(const tran_low_t* input,
                                  uint16_t* dest,
@@ -7219,6 +7252,15 @@ static void setup_rtcd_internal(void) {
   vpx_highbd_idct16x16_38_add = vpx_highbd_idct16x16_38_add_sse2;
   if (flags & HAS_SSE4_1)
     vpx_highbd_idct16x16_38_add = vpx_highbd_idct16x16_38_add_sse4_1;
+  vpx_highbd_idct32x32_1024_add = vpx_highbd_idct32x32_1024_add_sse2;
+  if (flags & HAS_SSE4_1)
+    vpx_highbd_idct32x32_1024_add = vpx_highbd_idct32x32_1024_add_sse4_1;
+  vpx_highbd_idct32x32_135_add = vpx_highbd_idct32x32_135_add_sse2;
+  if (flags & HAS_SSE4_1)
+    vpx_highbd_idct32x32_135_add = vpx_highbd_idct32x32_135_add_sse4_1;
+  vpx_highbd_idct32x32_34_add = vpx_highbd_idct32x32_34_add_sse2;
+  if (flags & HAS_SSE4_1)
+    vpx_highbd_idct32x32_34_add = vpx_highbd_idct32x32_34_add_sse4_1;
   vpx_highbd_idct4x4_16_add = vpx_highbd_idct4x4_16_add_sse2;
   if (flags & HAS_SSE4_1)
     vpx_highbd_idct4x4_16_add = vpx_highbd_idct4x4_16_add_sse4_1;
