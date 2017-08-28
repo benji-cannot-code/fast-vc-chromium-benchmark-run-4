e476bb10dca6cfcc97752da03be5e5c5afb580ee
Friedrich Horschig <fhorschig@chromium.org>
2017-08-28 17:06:11 +0000

    Improve deduplication for ntp tiles
    This CL introduces a simple blacklist so that popular sites don't
    produce duplicates with similar mobile sites.
    If ML or TS deliver such duplicate, it will not be prevented.
    
    Bug: 752928
    Change-Id: Ie7222ecb6468d4d727343f7e5416a16be02b380c
    Reviewed-on: https://chromium-review.googlesource.com/628879
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Reviewed-by: Chris Pickel <sfiera@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497782}

--- components/ntp_tiles/most_visited_sites.cc
+++ components/ntp_tiles/most_visited_sites.cc
@@ -6,12 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/ntp_tiles/most_visited_sites.h"
 
 #include <algorithm>
+#include <iterator>
 #include <utility>
 
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/feature_list.h"
 #include "base/metrics/user_metrics.h"
+#include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "components/history/core/browser/top_sites.h"
 #include "components/ntp_tiles/constants.h"
@@ -34,6 +36,21 @@ namespace {
 const base::Feature kDisplaySuggestionsServiceTiles{
     "DisplaySuggestionsServiceTiles", base::FEATURE_ENABLED_BY_DEFAULT};
 
+// URL host prefixes. Hosts with these prefixes often redirect to each other, or
+// have the same content.
+// Popular sites are excluded if the user has visited a page whose host only
+// differs by one of these prefixes. Even if the URL does not point to the exact
+// same page, the user will have a personalized suggestion that is more likely
+// to be of use for them.
+// A cleaner way could be checking the history for redirects but this requires
+// the page to be visited on the device.
+const char* kKnownGenericPagePrefixes[] = {
+    "m.", "mobile.",  // Common prefixes among popular sites.
+    "edition.",       // Used among news papers (CNN, Independent, ...)
+    "www.",           // Usually no-www domains redirect to www or vice-versa.
+    // The following entry MUST REMAIN LAST as it is prefix of every string!
+    ""};  // The no-www domain matches domains on same level .
+
 // Determine whether we need any tiles from PopularSites to fill up a grid of
 // |num_tiles| tiles. If exploration sections are used, we need popular sites
 // regardless of how many tiles we already have.
@@ -54,6 +71,16 @@ bool HasHomeTile(const NTPTilesVector& tiles) {
     }
   }
   return false;
+}
+
+std::string StripFirstGenericPrefix(const std::string& host) {
+  for (const char* prefix : kKnownGenericPagePrefixes) {
+    if (base::StartsWith(host, prefix, base::CompareCase::INSENSITIVE_ASCII)) {
+      return std::string(
+          base::TrimString(host, prefix, base::TrimPositions::TRIM_LEADING));
+    }
+  }
+  return host;
 }
 
 }  // namespace
@@ -87,6 +114,20 @@ MostVisitedSites::MostVisitedSites(
 MostVisitedSites::~MostVisitedSites() {
   if (supervisor_)
     supervisor_->SetObserver(nullptr);
+}
+
+// static
+bool MostVisitedSites::IsHostOrMobilePageKnown(
+    const std::set<std::string>& hosts_to_skip,
+    const std::string& host) {
+  std::string no_prefix_host = StripFirstGenericPrefix(host);
+  for (const char* prefix : kKnownGenericPagePrefixes) {
+    if (hosts_to_skip.count(prefix + no_prefix_host) ||
+        hosts_to_skip.count(prefix + host)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 bool MostVisitedSites::DoesSourceExist(TileSource source) const {
@@ -401,7 +442,7 @@ NTPTilesVector MostVisitedSites::CreatePopularSitesTiles(
       continue;
 
     const std::string& host = popular_site.url.host();
-    if (hosts_to_skip.count(host)) {
+    if (IsHostOrMobilePageKnown(hosts_to_skip, host)) {
       continue;
     }
 
