fc506f8eac5da1475de6af0eac343d14b33445e1
Rune Lillesveen <rune@opera.com>
2017-08-28 12:58:47 +0000

    Support ::before and ::after pseudo elements after ::slotted.
    Allow pseudo elements after ::slotted() pseudo elements to allow for
    scopes to which an element has been slotted to generate ::before /
    ::after pseudo elements.
    
    The scoping spec[1] allows tree-abiding[2] pseudo elements after
    ::slotted. This CL adds support for ::before and ::after.
    
    [1] https://drafts.csswg.org/css-scoping/#slotted-pseudo
    [2] https://drafts.csswg.org/css-pseudo-4/#treelike
    
    Bug: 754081
    Change-Id: I95d91ea06d8fa75537cf8c845da477b2fb034a7d
    Reviewed-on: https://chromium-review.googlesource.com/616042
    Commit-Queue: Rune Lillesveen <rune@opera.com>
    Reviewed-by: Takayoshi Kochi <kochi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497742}

--- third_party/WebKit/Source/core/css/parser/CSSSelectorParserTest.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSSelectorParserTest.cpp
@@ -149,10 +149,13 @@ TEST(CSSSelectorParserTest, PseudoElementsInCompoundLists) {
 }
 
 TEST(CSSSelectorParserTest, ValidSimpleAfterPseudoElementInCompound) {
-  const char* test_cases[] = {
-      "::-webkit-volume-slider:hover", "::selection:window-inactive",
-      "::-webkit-scrollbar:disabled", "::-webkit-volume-slider:not(:hover)",
-      "::-webkit-scrollbar:not(:horizontal)"};
+  const char* test_cases[] = {"::-webkit-volume-slider:hover",
+                              "::selection:window-inactive",
+                              "::-webkit-scrollbar:disabled",
+                              "::-webkit-volume-slider:not(:hover)",
+                              "::-webkit-scrollbar:not(:horizontal)",
+                              "::slotted(span)::before",
+                              "::slotted(div)::after"};
 
   for (auto test_case : test_cases) {
     CSSTokenizer tokenizer(test_case);
@@ -175,7 +178,14 @@ TEST(CSSSelectorParserTest, InvalidSimpleAfterPseudoElementInCompound) {
       "::shadow:not(::after)",
       "::-webkit-scrollbar:vertical:not(:first-child)",
       "video::-webkit-media-text-track-region-container.scrolling",
-      "div ::before.a"};
+      "div ::before.a",
+      "::slotted(div):hover",
+      "::slotted(div)::slotted(span)",
+      "::slotted(div)::before:hover",
+      "::slotted(div)::before::slotted(span)",
+      "::slotted(*)::first-letter",
+      "::slotted(.class)::first-line",
+      "::slotted([attr])::-webkit-scrollbar"};
 
   for (auto test_case : test_cases) {
     CSSTokenizer tokenizer(test_case);
