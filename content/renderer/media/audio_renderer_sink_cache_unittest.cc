f38f806681e487780281ae29eaf32ad84a12f58c
Olga Sharonova <olka@chromium.org>
2017-08-28 13:03:53 +0000

    Revert "Disabled flaky AudioRendererSinkCacheTest.MultithreadedAccess on Linux"
    This reverts commit 26252c125ee74354bb9ac16af07bb7a0ee067091.
    
    Reason for revert:
    BUG=753228
    No logs in the bug/on bots; failed to reproduce it locally with a tsan build and on CQ tsan (https://chromium-review.googlesource.com/c/chromium/src/+/625776).
    Re-enabling to collect the logs.
    
    Original change's description:
    > Disabled flaky AudioRendererSinkCacheTest.MultithreadedAccess on Linux
    > 
    > AudioRendererSinkCacheTest.MultithreadedAccess is flaky on Linux TSan bots.
    > This patch disables the test on OS_LINUX.
    > 
    > TBR=olka@chromium.org,miu@chromium.org
    > 
    > Bug: 753228
    > Change-Id: I619ee7ab1ff3a285ca158dabdeb8c91d06c3e137
    > Reviewed-on: https://chromium-review.googlesource.com/604816
    > Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    > Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#492532}
    
    TBR=miu@chromium.org,shimazu@chromium.org,olka@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 753228
    Change-Id: I163a3f82f035ba3ae70c4148256f9e7716781568
    Reviewed-on: https://chromium-review.googlesource.com/627357
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497743}

--- content/renderer/media/audio_renderer_sink_cache_unittest.cc
+++ content/renderer/media/audio_renderer_sink_cache_unittest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_simple_task_runner.h"
 #include "base/test/test_timeouts.h"
 #include "base/threading/thread.h"
-#include "build/build_config.h"
 #include "content/renderer/media/audio_renderer_sink_cache_impl.h"
 #include "media/audio/audio_device_description.h"
 #include "media/base/audio_parameters.h"
@@ -307,14 +306,7 @@ TEST_F(AudioRendererSinkCacheTest, ReleaseSinkBeforeScheduledDeletion) {
 
 // Check that a sink created on one thread in response to GetSinkInfo can be
 // used on another thread.
-// Flaky on Linux TSan Tests. https://crbug.com/753228
-#if defined(OS_LINUX)
-#define MAYBE_MultithreadedAccess DISABLED_MultithreadedAccess
-#else
-#define MAYBE_MultithreadedAccess MultithreadedAccess
-#endif
-
-TEST_F(AudioRendererSinkCacheTest, MAYBE_MultithreadedAccess) {
+TEST_F(AudioRendererSinkCacheTest, MultithreadedAccess) {
   EXPECT_EQ(0, sink_count());
 
   base::Thread thread1("thread1");
