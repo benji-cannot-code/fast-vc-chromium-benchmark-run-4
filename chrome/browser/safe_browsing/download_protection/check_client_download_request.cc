d04d0b7a2d18f77c68483af92b77255cabc5e601
Jialiu Lin <jialiul@chromium.org>
2017-08-28 20:39:18 +0000

    Fill locale field in ClientDownloadRequest
    As requested by SB team, we fill the locale field in ClientDownloadRequest
    proto.
    
    Bug: 759089
    Change-Id: Ib8a2cc5e6b04333b1a077a9f318c1958f7f560eb
    Reviewed-on: https://chromium-review.googlesource.com/636164
    Reviewed-by: Luke Z <lpz@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: Jialiu Lin <jialiul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497862}

--- chrome/browser/safe_browsing/download_protection/check_client_download_request.cc
+++ chrome/browser/safe_browsing/download_protection/check_client_download_request.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/rand_util.h"
 #include "base/task_scheduler/post_task.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/safe_browsing/download_protection/download_feedback_service.h"
@@ -799,6 +800,7 @@ void CheckClientDownloadRequest::SendRequest() {
   request.set_length(item_->GetReceivedBytes());
   request.set_skipped_url_whitelist(skipped_url_whitelist_);
   request.set_skipped_certificate_whitelist(skipped_certificate_whitelist_);
+  request.set_locale(g_browser_process->GetApplicationLocale());
   for (size_t i = 0; i < item_->GetUrlChain().size(); ++i) {
     ClientDownloadRequest::Resource* resource = request.add_resources();
     resource->set_url(SanitizeUrl(item_->GetUrlChain()[i]));
