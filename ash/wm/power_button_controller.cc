8d82827de5ba0d2ad0141e6a087f78bd8f70a27e
Qiang Xu <warx@chromium.org>
2017-08-28 23:30:58 +0000

    cros: make touchscreen local state enabled for --force-clamshell-power-button
    changes:
    We may have the local state of touchscreen disabled if we switch from tablet
    power button to force clamshell power button by update. This CL is to
    eliminate this possible case.
    
    TabletPowerButtonControllerTest.TouchscreenStatusClamshell
    
    Test: ash_unittests
    Bug: b/64972736
    Change-Id: I534723b60f109a1749eeae3d23f21d857f3aac86
    Reviewed-on: https://chromium-review.googlesource.com/639350
    Commit-Queue: Qiang(Joe) Xu <warx@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497933}

--- ash/wm/power_button_controller.cc
+++ ash/wm/power_button_controller.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/session/session_controller.h"
 #include "ash/shell.h"
+#include "ash/shell_delegate.h"
 #include "ash/shutdown_reason.h"
 #include "ash/system/audio/tray_audio.h"
 #include "ash/system/power/tablet_power_button_controller.h"
@@ -206,6 +207,14 @@ void PowerButtonController::ProcessCommandLine() {
   has_legacy_power_button_ = cl->HasSwitch(switches::kAuraLegacyPowerButton);
   force_clamshell_power_button_ =
       cl->HasSwitch(switches::kForceClamshellPowerButton);
+  if (force_clamshell_power_button_) {
+    // We may update power button behavior from tablet behavior to clamshell
+    // behavior with touchscreen local state disabled. Enabling touchscreen
+    // local state to ensure touchscreen is initially enabled for clamshell.
+    ShellDelegate* delegate = Shell::Get()->shell_delegate();
+    delegate->SetTouchscreenEnabledInPrefs(true, true /* use_local_state */);
+    delegate->UpdateTouchscreenStatusFromPrefs();
+  }
 }
 
 }  // namespace ash
