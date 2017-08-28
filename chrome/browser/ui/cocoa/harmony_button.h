4223421282597d28ae2c80a3ccd26a1cc8d4eb91
Sidney San Martín <sdy@chromium.org>
2017-08-28 22:41:14 +0000

    Create a Harmony-style button for Mac.
    This change is a prerequisite for MD downloads.
    
    Bug: 589943
    Change-Id: I03facbf3e531163fb699032e40f7bfbbe1fa20bb
    Reviewed-on: https://chromium-review.googlesource.com/636605
    Commit-Queue: Sidney San Martín <sdy@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497911}

---
+++ chrome/browser/ui/cocoa/harmony_button.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_COCOA_HARMONY_BUTTON_H_
+#define CHROME_BROWSER_UI_COCOA_HARMONY_BUTTON_H_
+
+#import <AppKit/AppKit.h>
+
+#import "ui/base/cocoa/hover_button.h"
+
+// HarmonyButton follows the Harmony design spec. It has slightly rounded
+// corners, a border, and a shadow that appears on hover. Its color scheme
+// tracks the active theme. Not every part of the spec for Harmony buttons has
+// been implemented: it doesn't look different if it's the default button, or
+// if it's disabled, for instance. Anyone who needs these things is encouraged
+// to find joy in adding support for them.
+
+@interface HarmonyButton : HoverButton
+@end
+
+#endif  // CHROME_BROWSER_UI_COCOA_HARMONY_BUTTON_H_
