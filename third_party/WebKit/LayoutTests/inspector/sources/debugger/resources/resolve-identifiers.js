79302fb19613ce2804d129b10650044a3117b505
Will Chen <chenwilliam@chromium.org>
2017-08-28 22:27:26 +0000

    DevTools: finish moving inspector/sources, inspector/editor (part 3)
    Follow-up from: https://chromium-review.googlesource.com/c/chromium/src/+/636287
    
    Bug: 667560
    Change-Id: Iaf29f8fb426d329af8f0dabcf42e36039b2ffdc0
    Reviewed-on: https://chromium-review.googlesource.com/635950
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497903}

--- third_party/WebKit/LayoutTests/inspector/sources/debugger/resources/resolve-identifiers.js
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-function testFunction() {
-    var pos = 10;
-    for (var pos_1 = 0; pos_1 < 1; pos_1++) {
-        console.log(pos_1);
-        debugger;
-    }
-}
-//# sourceMappingURL=resolve-identifiers.js.map
--- third_party/WebKit/LayoutTests/inspector/sources/debugger/resources/resolve-identifiers.ts
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-function testFunction() {
-    let pos = 10;
-    for (let pos = 0; pos < 1; pos++) {
-        console.log(pos);
-        debugger;
-    }
-}
