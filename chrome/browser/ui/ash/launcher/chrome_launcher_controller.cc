9a9266be7fea0d7f0150453dac5fbed9aa7aa783
Mike Wasserman <msw@chromium.org>
2017-08-28 23:42:46 +0000

    mash: Fix shelf button minimize crash.
    Use Chrome's AppList[Service|Presenter] to avoid crashes.
    (in the mash config, Chrome can't access Ash's ash::Shell)
    
    Bug: 730890
    Test: "chrome --mash" shelf item minimize doesn't crash.
    Change-Id: Icc827345bf3b059b327ef38db49660b0c1b0fbb2
    Reviewed-on: https://chromium-review.googlesource.com/638552
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497939}

--- chrome/browser/ui/ash/launcher/chrome_launcher_controller.cc
+++ chrome/browser/ui/ash/launcher/chrome_launcher_controller.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/app_list/app_list_syncable_service_factory.h"
 #include "chrome/browser/ui/app_list/arc/arc_app_icon_loader.h"
 #include "chrome/browser/ui/app_list/arc/arc_app_utils.h"
+#include "chrome/browser/ui/ash/app_list/app_list_service_ash.h"
 #include "chrome/browser/ui/ash/app_sync_ui_state.h"
 #include "chrome/browser/ui/ash/ash_util.h"
 #include "chrome/browser/ui/ash/chrome_launcher_prefs.h"
@@ -67,6 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/service_manager_connection.h"
 #include "extensions/common/extension.h"
 #include "services/service_manager/public/cpp/connector.h"
+#include "ui/app_list/presenter/app_list_presenter_impl.h"
 #include "ui/aura/window.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
@@ -521,8 +523,10 @@ ash::ShelfAction ChromeLauncherController::ActivateWindowOrMinimizeIfActive(
     }
   }
 
+  const app_list::AppListPresenterImpl* app_list_presenter =
+      AppListServiceAsh::GetInstance()->GetAppListPresenter();
   if (window->IsActive() && allow_minimize &&
-      !ash::Shell::Get()->IsAppListVisible()) {
+      (!app_list_presenter || !app_list_presenter->IsVisible())) {
     window->Minimize();
     return ash::SHELF_ACTION_WINDOW_MINIMIZED;
   }
