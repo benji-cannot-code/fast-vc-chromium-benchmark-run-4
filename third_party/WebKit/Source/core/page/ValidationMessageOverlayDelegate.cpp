60528f75e0dbcf6a6ab818638a74a9bad845d753
Kent Tamura <tkent@chromium.org>
2017-08-29 03:19:57 +0000

    ValidationBubbleInRenderer: Scale animation origin should match to arrow anchor point.
    Apply a feedback from UX team.
    
    Bug: 739091
    Change-Id: I061f477b786fb4764540a6280d5b954f47cac027
    Reviewed-on: https://chromium-review.googlesource.com/640052
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498008}

--- third_party/WebKit/Source/core/page/ValidationMessageOverlayDelegate.cpp
+++ third_party/WebKit/Source/core/page/ValidationMessageOverlayDelegate.cpp
@@ -249,14 +249,6 @@ void ValidationMessageOverlayDelegate::AdjustBubblePosition(
                                    CSSPrimitiveValue::UnitType::kPixels);
   container.SetInlineStyleProperty(CSSPropertyTop, bubble_y / zoom_factor,
                                    CSSPrimitiveValue::UnitType::kPixels);
-  if (show_bottom_arrow) {
-    container.setAttribute(HTMLNames::classAttr, "shown-fully bottom-arrow");
-    container.SetInlineStyleProperty(CSSPropertyTransformOrigin,
-                                     "center bottom");
-  } else {
-    container.setAttribute(HTMLNames::classAttr, "shown-fully");
-    container.SetInlineStyleProperty(CSSPropertyTransformOrigin, "center top");
-  }
 
   // Should match to --arrow-size in validation_bubble.css.
   const int kArrowSize = 8;
@@ -293,6 +285,7 @@ void ValidationMessageOverlayDelegate::AdjustBubblePosition(
     }
   }
   double arrow_x = arrow_anchor_x / zoom_factor - kArrowSize;
+  double arrow_anchor_percent = arrow_anchor_x * 100 / bubble_size_.Width();
   if (show_bottom_arrow) {
     GetElementById("outer-arrow-bottom")
         .SetInlineStyleProperty(CSSPropertyLeft, arrow_x,
@@ -300,6 +293,10 @@ void ValidationMessageOverlayDelegate::AdjustBubblePosition(
     GetElementById("inner-arrow-bottom")
         .SetInlineStyleProperty(CSSPropertyLeft, arrow_x,
                                 CSSPrimitiveValue::UnitType::kPixels);
+    container.setAttribute(HTMLNames::classAttr, "shown-fully bottom-arrow");
+    container.SetInlineStyleProperty(
+        CSSPropertyTransformOrigin,
+        String::Format("%.2f%% bottom", arrow_anchor_percent));
   } else {
     GetElementById("outer-arrow-top")
         .SetInlineStyleProperty(CSSPropertyLeft, arrow_x,
@@ -307,6 +304,10 @@ void ValidationMessageOverlayDelegate::AdjustBubblePosition(
     GetElementById("inner-arrow-top")
         .SetInlineStyleProperty(CSSPropertyLeft, arrow_x,
                                 CSSPrimitiveValue::UnitType::kPixels);
+    container.setAttribute(HTMLNames::classAttr, "shown-fully");
+    container.SetInlineStyleProperty(
+        CSSPropertyTransformOrigin,
+        String::Format("%.2f%% top", arrow_anchor_percent));
   }
 }
 
