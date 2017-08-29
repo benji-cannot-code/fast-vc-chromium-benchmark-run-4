b57dccdf1881afcc6a7cd18992d1db1fcac5b2d8
Emilio Cobos Álvarez <emilio@chromium.org>
2017-08-29 02:25:38 +0000

    [cssom] Don't use the element layout object when querying a non-existent pseudo.
    BUG=758596
    
    Change-Id: I6defc3ce03d9f3cd80cec770d9412e55dfbd9580
    Reviewed-on: https://chromium-review.googlesource.com/636965
    Commit-Queue: Emilio Cobos Álvarez <emilio@chromium.org>
    Reviewed-by: Rune Lillesveen <rune@opera.com>
    Cr-Commit-Position: refs/heads/master@{#497990}

--- third_party/WebKit/Source/core/css/CSSComputedStyleDeclaration.h
+++ third_party/WebKit/Source/core/css/CSSComputedStyleDeclaration.h
@@ -35,6 +35,7 @@ namespace blink {
 
 class CSSVariableData;
 class ExceptionState;
+class LayoutObject;
 class MutableStylePropertySet;
 class Node;
 class ComputedStyle;
@@ -84,6 +85,10 @@ class CORE_EXPORT CSSComputedStyleDeclaration final
   // on Elements, but right now editing creates these for text nodes. We should
   // fix that.
   Node* StyledNode() const;
+
+  // The styled layout object is the layout object corresponding to the node
+  // being queried, if any.
+  LayoutObject* StyledLayoutObject() const;
 
   // CSSOM functions.
   CSSRule* parentRule() const override;
