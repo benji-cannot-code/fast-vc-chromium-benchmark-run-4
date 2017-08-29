cc68e7b83e8643829fcd35782d88ef8f651e9071
Gauthier Ambard <gambard@chromium.org>
2017-08-29 16:44:30 +0000

    Add hooks for franky
    This CL adds a hook for franky on the ContentSuggestions header cell.
    It also changes the NTP header from reusable view to UIView, as it is
    no longer used as a CollectionViewCell.
    
    Bug: 755553
    Change-Id: I6d33f6b8eb6e751b601e78b4598a74bf27c7a1db
    Reviewed-on: https://chromium-review.googlesource.com/640377
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498140}

--- ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_header_item.mm
+++ ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_header_item.mm
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)configureCell:(ContentSuggestionsHeaderCell*)cell {
   [super configureCell:cell];
   [cell setHeaderView:self.view];
+  cell.accessibilityIdentifier = @"CSHeaderIdentifier";
 }
 
 @end
