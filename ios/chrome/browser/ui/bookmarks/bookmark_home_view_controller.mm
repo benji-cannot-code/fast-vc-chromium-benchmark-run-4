a25a0f26d03ba703e9d3e1273c1316f126a4d4b4
Gregory Chatzinoff <gchatz@chromium.org>
2017-08-29 20:42:03 +0000

    Revert "Adding UI tests for sign-in promo in bookmarks"
    This reverts commit 39592fcbff3c865a9d4246419e9f76ca3b1defbe.
    
    Reason for revert: BookmarksNewGenTestCase/testAutomaticSigninPromoDismiss failed
    on iphone 9 simulator and iphone 9 simulator x64 bots downstream.
    
    Error:
    ../../ios/chrome/browser/ui/bookmarks/bookmarks_new_generation_egtest.mm:377: error: -[BookmarksNewGenTestCase testAutomaticSigninPromoDismiss] : Exception: NoMatchingElementException
    
    Exception Name: NoMatchingElementException
    Exception Reason: Cannot find UI element.
    Exception with Action: {
      "Action Name" : "Tap",
      "Element Matcher" : "((respondsToSelector(accessibilityIdentifier) && accessibilityID('DONE')) && matcherForSufficientlyVisible(>=0.750000))",
      "Recovery Suggestion" : "Check if the element exists in the UI hierarchy printed below. If it exists, adjust the matcher so that it accurately matches element."
    }
    
    Original change's description:
    > Adding UI tests for sign-in promo in bookmarks
    > 
    > Updating the current test for the sign-in promo to match the cold state
    > sign-in promo, and to check that only the primary button is visible.
    > 
    > Adding a warm state sign-in promo test, to check if both buttons
    > (primary and secondary) are visible, and check the behavior of the
    > secondary button.
    > 
    > Adding a test to check that sign-in promo is not shown after being
    > displayed 19 times.
    > 
    > Updating BookmarksTestCase and BookmarksNewGenTestCase.
    > 
    > Bug: 661794
    > Change-Id: Idd2870ca0b1ba618398dead05555b4613ec6986e
    > Reviewed-on: https://chromium-review.googlesource.com/633924
    > Reviewed-by: Eric Noyau <noyau@chromium.org>
    > Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#498151}
    
    TBR=noyau@chromium.org,jlebel@chromium.org
    
    Change-Id: Ibc1e02fb8307556d2312a638c6164e1884504552
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 661794
    Reviewed-on: https://chromium-review.googlesource.com/642132
    Reviewed-by: Gregory Chatzinoff <gchatz@chromium.org>
    Commit-Queue: Gregory Chatzinoff <gchatz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498218}

--- ios/chrome/browser/ui/bookmarks/bookmark_home_view_controller.mm
+++ ios/chrome/browser/ui/bookmarks/bookmark_home_view_controller.mm
@@ -194,7 +194,6 @@ const CGFloat kMenuWidth = 264;
                                          style:UIBarButtonItemStyleDone
                                         target:self
                                         action:@selector(navigationBarCancel:)];
-    doneButton.accessibilityIdentifier = @"DONE";
     self.navigationItem.rightBarButtonItem = doneButton;
     self.navigationItem.backBarButtonItem =
         [[UIBarButtonItem alloc] initWithTitle:@""
