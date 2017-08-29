4bed0f14ee2d427f610481350c6b2440dfa64b75
Mikhail Atuchin <atuchin@yandex-team.ru>
2017-08-29 05:07:59 +0000

    [subresource_filter] Fix several histogram problems
    This CL:
    1. Fix tests when !base::ThreadTicks::IsSupported();
    2. Fix the wrong values in SubresourceFilter.PageLoad.Activation*
       histograms.
    
    Bug: 
    Change-Id: Ia244d33eb8feed8faa53626052c22d5130da933a
    Reviewed-on: https://chromium-review.googlesource.com/635567
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Mikhail Atuchin <atuchin@yandex-team.ru>
    Cr-Commit-Position: refs/heads/master@{#498027}

--- components/subresource_filter/content/browser/content_subresource_filter_throttle_manager_unittest.cc
+++ components/subresource_filter/content/browser/content_subresource_filter_throttle_manager_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/test/histogram_tester.h"
 #include "base/test/test_simple_task_runner.h"
+#include "base/time/time.h"
 #include "components/subresource_filter/content/browser/async_document_subresource_filter.h"
 #include "components/subresource_filter/content/browser/subresource_filter_observer_manager.h"
 #include "components/subresource_filter/content/common/subresource_filter_messages.h"
@@ -717,8 +718,10 @@ TEST_F(ContentSubresourceFilterThrottleManagerTest, LogActivation) {
   // Only those with page level activation do ruleset lookups.
   tester.ExpectTotalCount("SubresourceFilter.PageLoad.Activation.WallDuration",
                           2);
+  // The *.CPUDuration histograms are recorded only if base::ThreadTicks is
+  // supported.
   tester.ExpectTotalCount("SubresourceFilter.PageLoad.Activation.CPUDuration",
-                          2);
+                          base::ThreadTicks::IsSupported() ? 2 : 0);
 }
 
 // If ruleset rules are disabled, should never map the ruleset into memory.
