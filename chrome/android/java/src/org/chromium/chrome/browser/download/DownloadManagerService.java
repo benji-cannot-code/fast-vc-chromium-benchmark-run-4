0ea86da3d9858f9b6a78b98ab6a75c3cfcad00e3
Shakti Sahu <shaktisahu@chromium.org>
2017-08-29 19:30:53 +0000

    Download Home : Using startService to perform user-initiated action
    Bug: 
    Change-Id: I5ff3d1d5cc8e4d03b0f631f635ab4235c7fa65c3
    Reviewed-on: https://chromium-review.googlesource.com/639060
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498198}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadManagerService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadManagerService.java
@@ -1372,7 +1372,7 @@ public class DownloadManagerService
         Intent intent = buildActionIntent(mContext, action,
                 LegacyHelpers.buildLegacyContentId(false, downloadItem.getId()),
                 downloadItem.getDownloadInfo().isOffTheRecord());
-        mContext.sendBroadcast(intent);
+        mContext.startService(intent);
     }
 
     /**
