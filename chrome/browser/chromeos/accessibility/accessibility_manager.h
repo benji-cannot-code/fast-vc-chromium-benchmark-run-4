296233ec9c485fedb6e2010a7abb6a9cbfbe7ee0
David Tseng <dtseng@chromium.org>
2017-08-29 02:23:41 +0000

    Fix startup sound when ChromeVox is enabled
    The flag enable-system-sounds never gets added anywhere in Chrome, so the logic in AccessibilityManager::PlayEarcon never evaluated to true when the play option is spoken feedback. The end result is that a ChromeVox user never actually hears the startup earcon.
    
    This cl also cleans up what appears to be somewhat convoluted logic in LoginDisplayManagerImpl, which tries to do the right thing for accessibility, but doesn't because of the above.
    
    Bug: 
    Test: have ChromeVox enabled. Reboot the machine. Verify that the Chrome OS startup sound is heard as well as the ChromeVox enabled sound.
    Change-Id: I396a0962b5a500fa7d65787f03e51d8e2d1e6269
    Reviewed-on: https://chromium-review.googlesource.com/634403
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497988}

--- chrome/browser/chromeos/accessibility/accessibility_manager.h
+++ chrome/browser/chromeos/accessibility/accessibility_manager.h
@@ -243,9 +243,6 @@ class AccessibilityManager
   static void SetBrailleControllerForTest(
       extensions::api::braille_display_private::BrailleController* controller);
 
-  // Enables/disables system sounds.
-  void EnableSystemSounds(bool system_sounds_enabled);
-
   // Initiates play of shutdown sound and returns it's duration.
   base::TimeDelta PlayShutdownSound();
 
@@ -420,8 +417,6 @@ class AccessibilityManager
   bool switch_access_enabled_;
 
   ash::AccessibilityNotificationVisibility spoken_feedback_notification_;
-
-  bool system_sounds_enabled_;
 
   AccessibilityStatusCallbackList callback_list_;
 
