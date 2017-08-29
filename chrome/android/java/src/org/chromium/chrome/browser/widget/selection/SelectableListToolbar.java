ed0b94230c6926ddfd62f327a130a3de15f0d12e
danielpark@chromium.org <danielpark@chromium.org>
2017-08-29 00:06:44 +0000

    [Home] Properly set search icon visibility when switching content
    > Adds new boolean mSelectableListHasItems in SelectableListToolbar.
    > Sets visibility for search icon using mSelectableListHasItems 
     in showNormalView.
    
    Bug: 729244
    Change-Id: I7e1f5511d14a346b8dbbfca433d42c62112d911e
    Reviewed-on: https://chromium-review.googlesource.com/627033
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Daniel Park <danielpark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497949}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/selection/SelectableListToolbar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/selection/SelectableListToolbar.java
@@ -115,6 +115,7 @@ public class SelectableListToolbar<E> extends Toolbar implements SelectionObserv
     private TintedImageButton mClearTextButton;
     private SearchDelegate mSearchDelegate;
     private boolean mIsLightTheme = true;
+    private boolean mSelectableListHasItems;
 
     protected NumberRollView mNumberRollView;
     private DrawerLayout mDrawerLayout;
@@ -431,8 +432,10 @@ public class SelectableListToolbar<E> extends Toolbar implements SelectionObserv
      */
     protected void onDataChanged(int numItems) {
         if (mHasSearchView) {
-            getMenu().findItem(mSearchMenuItemId).setVisible(
-                    !mIsSelectionEnabled && !mIsSearching && numItems != 0);
+            mSelectableListHasItems = numItems != 0;
+            getMenu()
+                    .findItem(mSearchMenuItemId)
+                    .setVisible(!mIsSelectionEnabled && !mIsSearching && mSelectableListHasItems);
         }
     }
 
@@ -552,7 +555,10 @@ public class SelectableListToolbar<E> extends Toolbar implements SelectionObserv
     protected void showNormalView() {
         getMenu().setGroupVisible(mNormalGroupResId, true);
         getMenu().setGroupVisible(mSelectedGroupResId, false);
-        if (mHasSearchView) mSearchView.setVisibility(View.GONE);
+        if (mHasSearchView) {
+            mSearchView.setVisibility(View.GONE);
+            getMenu().findItem(mSearchMenuItemId).setVisible(mSelectableListHasItems);
+        }
 
         setNavigationButton(NAVIGATION_BUTTON_MENU);
         setBackgroundColor(mNormalBackgroundColor);
