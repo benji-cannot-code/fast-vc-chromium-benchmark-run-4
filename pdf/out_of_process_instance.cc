bc853df69b53a73d0ba97b0028058ed0e4da70b1
Lei Zhang <thestig@chromium.org>
2017-08-29 13:14:02 +0000

    PDF: Simplify some PPAPI code.
    Change-Id: I6a38060b4f3cba71f706431da450adf694048d80
    Reviewed-on: https://chromium-review.googlesource.com/640557
    Commit-Queue: dsinclair <dsinclair@chromium.org>
    Reviewed-by: dsinclair <dsinclair@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498087}

--- pdf/out_of_process_instance.cc
+++ pdf/out_of_process_instance.cc
@@ -187,8 +187,7 @@ PP_Var GetLinkAtPosition(PP_Instance instance, PP_Point point) {
 void Transform(PP_Instance instance, PP_PrivatePageTransformType type) {
   void* object = pp::Instance::GetPerInstanceObject(instance, kPPPPdfInterface);
   if (object) {
-    OutOfProcessInstance* obj_instance =
-        static_cast<OutOfProcessInstance*>(object);
+    auto* obj_instance = static_cast<OutOfProcessInstance*>(object);
     switch (type) {
       case PP_PRIVATEPAGETRANSFORMTYPE_ROTATE_90_CW:
         obj_instance->RotateClockwise();
@@ -205,8 +204,7 @@ PP_Bool GetPrintPresetOptionsFromDocument(
     PP_PdfPrintPresetOptions_Dev* options) {
   void* object = pp::Instance::GetPerInstanceObject(instance, kPPPPdfInterface);
   if (object) {
-    OutOfProcessInstance* obj_instance =
-        static_cast<OutOfProcessInstance*>(object);
+    auto* obj_instance = static_cast<OutOfProcessInstance*>(object);
     obj_instance->GetPrintPresetOptionsFromDocument(options);
   }
   return PP_TRUE;
@@ -215,18 +213,16 @@ PP_Bool GetPrintPresetOptionsFromDocument(
 void EnableAccessibility(PP_Instance instance) {
   void* object = pp::Instance::GetPerInstanceObject(instance, kPPPPdfInterface);
   if (object) {
-    OutOfProcessInstance* obj_instance =
-        static_cast<OutOfProcessInstance*>(object);
-    return obj_instance->EnableAccessibility();
+    auto* obj_instance = static_cast<OutOfProcessInstance*>(object);
+    obj_instance->EnableAccessibility();
   }
 }
 
 void SetCaretPosition(PP_Instance instance, const PP_FloatPoint* position) {
   void* object = pp::Instance::GetPerInstanceObject(instance, kPPPPdfInterface);
   if (object) {
-    OutOfProcessInstance* obj_instance =
-        static_cast<OutOfProcessInstance*>(object);
-    return obj_instance->SetCaretPosition(pp::FloatPoint(*position));
+    auto* obj_instance = static_cast<OutOfProcessInstance*>(object);
+    obj_instance->SetCaretPosition(*position);
   }
 }
 
@@ -234,9 +230,8 @@ void MoveRangeSelectionExtent(PP_Instance instance,
                               const PP_FloatPoint* extent) {
   void* object = pp::Instance::GetPerInstanceObject(instance, kPPPPdfInterface);
   if (object) {
-    OutOfProcessInstance* obj_instance =
-        static_cast<OutOfProcessInstance*>(object);
-    return obj_instance->MoveRangeSelectionExtent(pp::FloatPoint(*extent));
+    auto* obj_instance = static_cast<OutOfProcessInstance*>(object);
+    obj_instance->MoveRangeSelectionExtent(*extent);
   }
 }
 
@@ -245,10 +240,8 @@ void SetSelectionBounds(PP_Instance instance,
                         const PP_FloatPoint* extent) {
   void* object = pp::Instance::GetPerInstanceObject(instance, kPPPPdfInterface);
   if (object) {
-    OutOfProcessInstance* obj_instance =
-        static_cast<OutOfProcessInstance*>(object);
-    return obj_instance->SetSelectionBounds(pp::FloatPoint(*base),
-                                            pp::FloatPoint(*extent));
+    auto* obj_instance = static_cast<OutOfProcessInstance*>(object);
+    obj_instance->SetSelectionBounds(*base, *extent);
   }
 }
 
@@ -257,19 +250,16 @@ PP_Bool CanEditText(PP_Instance instance) {
   if (!object)
     return PP_FALSE;
 
-  OutOfProcessInstance* obj_instance =
-      static_cast<OutOfProcessInstance*>(object);
+  auto* obj_instance = static_cast<OutOfProcessInstance*>(object);
   return PP_FromBool(obj_instance->CanEditText());
 }
 
 void ReplaceSelection(PP_Instance instance, const char* text) {
   void* object = pp::Instance::GetPerInstanceObject(instance, kPPPPdfInterface);
-  if (!object)
-    return;
-
-  OutOfProcessInstance* obj_instance =
-      static_cast<OutOfProcessInstance*>(object);
-  obj_instance->ReplaceSelection(text);
+  if (object) {
+    auto* obj_instance = static_cast<OutOfProcessInstance*>(object);
+    obj_instance->ReplaceSelection(text);
+  }
 }
 
 const PPP_Pdf ppp_private = {
