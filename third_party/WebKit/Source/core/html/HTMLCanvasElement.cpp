80590f0dda37a3b937d08ff8fe6eb11c6f10cc64
Justin Novosad <junov@chromium.org>
2017-08-29 13:30:40 +0000

    Speculative fix for crash in ImageBuffer::IsAccelerated
    BUG=759525
    
    Change-Id: I7d66aa20b96baabcc2913a7574b5ab1f02eb5719
    Reviewed-on: https://chromium-review.googlesource.com/639055
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498092}

--- third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
@@ -1237,7 +1237,8 @@ void HTMLCanvasElement::DidMoveToNewDocument(Document& old_document) {
 void HTMLCanvasElement::WillDrawImageTo2DContext(CanvasImageSource* source) {
   if (CanvasHeuristicParameters::kEnableAccelerationToAvoidReadbacks &&
       SharedGpuContext::AllowSoftwareToAcceleratedCanvasUpgrade() &&
-      source->IsAccelerated() && !GetOrCreateImageBuffer()->IsAccelerated() &&
+      source->IsAccelerated() && GetOrCreateImageBuffer() &&
+      !GetImageBuffer()->IsAccelerated() &&
       ShouldAccelerate(kIgnoreResourceLimitCriteria)) {
     OpacityMode opacity_mode =
         context_->CreationAttributes().alpha() ? kNonOpaque : kOpaque;
