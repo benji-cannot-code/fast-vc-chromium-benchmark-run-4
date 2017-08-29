c6d467f59dec52e69becd3b9a314906f74f3c80d
Tarun Bansal <tbansal@chromium.org>
2017-08-29 00:03:34 +0000

    Revert "Use swap time for first meaningful paint candidates"
    This reverts commit 20649c43b994d7735b43df473c7380d2091969d0.
    
    Reason for revert:
    Breaks build
    https://uberchromegw.corp.google.com/i/chromium.mac/builders/Mac10.9%20Tests%20%28dbg%29/builds/44380
    [78431:771:0828/163525.473940:FATAL:FirstMeaningfulPaintDetector.cpp(307)] Check failed: swap_stamp >= stamp (39864.3 vs. 39864.6)
    
    Original change's description:
    > Use swap time for first meaningful paint candidates
    > 
    > FMP was recently updated to use swap time
    > (https://chromium-review.googlesource.com/c/chromium/src/+/576371).
    > The diffs between the previous paint timestamp and swap timestamps look
    > as expected, and this CL updates the FMP candidate used by the renderer
    > scheduler and v8.
    > 
    > Bug: 738235
    > Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    > Change-Id: I7ae381ee272b2386aa65e9fa8cf6b602c517d463
    > Reviewed-on: https://chromium-review.googlesource.com/633903
    > Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    > Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    > Reviewed-by: Shubhie Panicker <panicker@chromium.org>
    > Reviewed-by: Steve Kobes <skobes@chromium.org>
    > Commit-Queue: Scott Haseley <shaseley@google.com>
    > Cr-Commit-Position: refs/heads/master@{#497909}
    
    TBR=ksakamoto@chromium.org,skyostil@chromium.org,skobes@chromium.org,panicker@chromium.org,shaseley@google.com
    
    Change-Id: Id23de975d7f939e3d51caab503d244aa9ad94f72
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 738235
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Reviewed-on: https://chromium-review.googlesource.com/639167
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497947}

--- third_party/WebKit/Source/core/paint/FirstMeaningfulPaintDetector.h
+++ third_party/WebKit/Source/core/paint/FirstMeaningfulPaintDetector.h
@@ -79,7 +79,6 @@ class CORE_EXPORT FirstMeaningfulPaintDetector
   void Network2QuietTimerFired(TimerBase*);
   void ReportHistograms();
   void RegisterNotifySwapTime(PaintEvent);
-  void SetFirstMeaningfulPaint(double stamp, double swap_stamp);
 
   bool next_paint_is_meaningful_ = false;
   HadUserInput had_user_input_ = kNoUserInput;
@@ -97,6 +96,7 @@ class CORE_EXPORT FirstMeaningfulPaintDetector
   bool network2_quiet_reached_ = false;
   double first_meaningful_paint0_quiet_ = 0.0;
   double first_meaningful_paint2_quiet_ = 0.0;
+  double first_meaningful_paint2_quiet_swap_ = 0.0;
   unsigned outstanding_swap_promise_count_ = 0;
   DeferFirstMeaningfulPaint defer_first_meaningful_paint_ = kDoNotDefer;
   TaskRunnerTimer<FirstMeaningfulPaintDetector> network0_quiet_timer_;
