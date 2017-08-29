d22d90df54f9927d0bdf7f82a928a7454bd6991f
Jacob Dufault <jdufault@google.com>
2017-08-29 21:29:24 +0000

    cros: Limit maximum number of signed in users to 5.
    The UI doesn't support 7+ users well. Context for 5 maximum is in bug.
    
    Bug: b/64593342.
    Change-Id: I108525356e1d34bdd1a0187e1dfd6bd9056a1ce5
    Reviewed-on: https://chromium-review.googlesource.com/641970
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Jacob Dufault <jdufault@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498236}

--- chrome/browser/ui/ash/session_controller_client.cc
+++ chrome/browser/ui/ash/session_controller_client.cc
@@ -244,7 +244,7 @@ void SessionControllerClient::ShowMultiProfileLogin() {
   }
 
   if (UserManager::Get()->GetLoggedInUsers().size() >=
-      session_manager::kMaxmiumNumberOfUserSessions) {
+      session_manager::kMaximumNumberOfUserSessions) {
     return;
   }
 
@@ -360,7 +360,7 @@ ash::AddUserSessionPolicy SessionControllerClient::GetAddUserSessionPolicy() {
   }
 
   if (UserManager::Get()->GetLoggedInUsers().size() >=
-      session_manager::kMaxmiumNumberOfUserSessions)
+      session_manager::kMaximumNumberOfUserSessions)
     return ash::AddUserSessionPolicy::ERROR_MAXIMUM_USERS_REACHED;
 
   return ash::AddUserSessionPolicy::ALLOWED;
