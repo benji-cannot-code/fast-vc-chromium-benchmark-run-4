d7d0cee334135647e127c9e5dccbcb1c487dfb31
gogerald <gogerald@google.com>
2017-08-29 15:25:10 +0000

    [Payments] Delegate PaymentRequest.abort to web payment handler
    Spec change:
    https://github.com/w3c/payment-handler/pull/207
    
    Bug: 736745
    Change-Id: Ifb78aa332080044180e2e836ac0709ba3f727567
    Reviewed-on: https://chromium-review.googlesource.com/636504
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Ganggui Tang <gogerald@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498114}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/ServiceWorkerPaymentApp.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ServiceWorkerPaymentApp.java
@@ -127,6 +127,11 @@ public class ServiceWorkerPaymentApp extends PaymentInstrument implements Paymen
     }
 
     @Override
+    public void abortPaymentApp(AbortCallback callback) {
+        ServiceWorkerPaymentAppBridge.abortPaymentApp(mWebContents, mRegistrationId, callback);
+    }
+
+    @Override
     public void dismissInstrument() {}
 
     @Override
