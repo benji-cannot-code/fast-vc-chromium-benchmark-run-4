9b4e25d3e4a5873a87ec4115995ce8c8d2ae34c0
nainar <nainar@chromium.org>
2017-08-29 03:50:43 +0000

    Revert "CSS Parser: Lazy Parsing for Pseudo attributes (before/after)"
    This reverts commit 5d173f3f4c19b3564d41853b77da410fdd5d7464.
    
    Reason for revert: See https://chromium-review.googlesource.com/c/chromium/src/+/613841#message-e939301c56917f4aead8e58f1fa9744b1807f5b0
    
    Original change's description:
    > CSS Parser: Lazy Parsing for Pseudo attributes (before/after)
    > 
    > This patch enables lazy parsing for pseudo attributes ::before and 
    > this isn't efficient but that is acceptable since lazy parsing is hidden
    > behind the --experimental-web-platform-features flag. 
    >  
    > 
    > : :after. As per discussion here: 
    > https: //groups.google.com/a/chromium.org/forum/#!topic/loading-dev/pVuMhUZg-qo
    > Bug: 642722
    > Change-Id: I708d5b9e7f50fdd77b9443af008904d312de34a9
    > Reviewed-on: https://chromium-review.googlesource.com/613841
    > Reviewed-by: Darren Shen <shend@chromium.org>
    > Commit-Queue: nainar <nainar@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#495887}
    
    TBR=nainar@chromium.org,shend@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 642722
    Change-Id: I57b9a4ca9e047f2c69e280272184446f7223cc29
    Reviewed-on: https://chromium-review.googlesource.com/639770
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: nainar <nainar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498013}

--- third_party/WebKit/Source/core/css/parser/CSSLazyParsingState.h
+++ third_party/WebKit/Source/core/css/parser/CSSLazyParsingState.h
@@ -39,7 +39,8 @@ class CSSLazyParsingState
 
   void CountRuleParsed();
 
-  bool IsEmptyBlock(const CSSParserTokenRange& block) const;
+  bool ShouldLazilyParseProperties(const CSSSelectorList&,
+                                   const CSSParserTokenRange& block) const;
 
   DECLARE_TRACE();
 
