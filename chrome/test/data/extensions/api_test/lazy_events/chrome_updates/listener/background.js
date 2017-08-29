f7a4a1d6c7d828d930d54a83151a58aa94d951c6
Devlin Cronin <rdevlin.cronin@chromium.org>
2017-08-29 14:24:46 +0000

    [Extensions] De-flake ChromeUpdatesEventsApiTest.ChromeUpdates
    ChromeUpdatesEventsApiTest.ChromeUpdates waits for a message to be
    sent from an extension background page when it's woken up for a
    chrome_updated event. However, there's a chance that the extension
    could already have woken up and received the event before we can
    register an extension listener.
    
    Remove the message listening aspect, since there's not a good way of
    ensuring determinism while testing it, and instead rely on the views
    created for the given extensions (which should be sufficient).
    
    Bug: 759546
    Change-Id: I4d0afa89139b905710cba434758b8f8121fcb154
    Reviewed-on: https://chromium-review.googlesource.com/639355
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498104}

--- chrome/test/data/extensions/api_test/lazy_events/chrome_updates/listener/background.js
+++ chrome/test/data/extensions/api_test/lazy_events/chrome_updates/listener/background.js
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-chrome.runtime.onInstalled.addListener(function(info) {
-  if (info.reason == 'chrome_update')
-    chrome.test.sendMessage('update event');
-});
+chrome.runtime.onInstalled.addListener(function(info) {});
+
 chrome.test.notifyPass();
