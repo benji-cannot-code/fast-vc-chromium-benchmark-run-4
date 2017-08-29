f5e3357311f77760ecaee47b214a81dff8734b85
Sonny Sasaka <sonnysasaka@chromium.org>
2017-08-29 02:45:17 +0000

    Save user pref for bluetooth power change in ARC++.
    ArcBluetoothBridge needs to be moved from components/ to chrome/ to
    allow dependency to set user pref.
    
    Note: this CL is split from https://crrev.com/6698c324 since the
    original CL was reverted due to breakage caused by ARC++ changes. This
    CL re-lands the ARC++ changes part and fixes the breakage.
    
    BUG=741962
    
    Change-Id: I7354f0e6a4d62b5cc5b41384975012243abe3fbf
    Reviewed-on: https://chromium-review.googlesource.com/639021
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Commit-Queue: Sonny Sasaka <sonnysasaka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497996}

--- chrome/browser/chromeos/arc/bluetooth/arc_bluetooth_bridge_unittest.cc
+++ chrome/browser/chromeos/arc/bluetooth/arc_bluetooth_bridge_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/arc/bluetooth/arc_bluetooth_bridge.h"
+#include "chrome/browser/chromeos/arc/bluetooth/arc_bluetooth_bridge.h"
 
 #include <string>
 #include <utility>
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 constexpr int16_t kTestRssi = -50;
 constexpr int16_t kTestRssi2 = -70;
-}
+}  // namespace
 
 namespace arc {
 
