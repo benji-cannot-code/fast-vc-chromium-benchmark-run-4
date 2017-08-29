f5a1b1648747aa255fbc975617dfe7f3413fa90d
Will Chen <chenwilliam@chromium.org>
2017-08-29 21:22:36 +0000

    DevTools: move inspector/tracing, inspector/profiler tests (part 6)
    Bug: 667560
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I014d6e16d755cd0bf017014e476c7802a22aae7f
    Reviewed-on: https://chromium-review.googlesource.com/639397
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498232}

--- third_party/WebKit/LayoutTests/http/tests/inspector/timeline-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/timeline-test.js
@@ -43,6 +43,7 @@ InspectorTest.timelinePropertyFormatters = {
     allottedMilliseconds: "formatAsTypeName",
     timedOut: "formatAsTypeName",
     networkTime: "formatAsTypeName",
+    timing: "formatAsTypeName",
 };
 
 InspectorTest.InvalidationFormatters = {
