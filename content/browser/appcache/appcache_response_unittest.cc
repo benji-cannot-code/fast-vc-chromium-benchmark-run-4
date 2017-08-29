e2fbd047f59d1406ce17dac8a32a8e30cd7b5c2f
Mark Pilgrim <pilgrim@chromium.org>
2017-08-29 13:33:03 +0000

    Appcache: BindOnce refactoring
    Make AppCacheResponseTest::task_stack_ be a stack of OnceClosure/bool
    pairs instead of Closure/bool pairs
    
    Bug: 714018
    Change-Id: I0aa5119aa4863bf6a7aae8b24d2436bb2809279a
    Reviewed-on: https://chromium-review.googlesource.com/635807
    Reviewed-by: Michael Nordman <michaeln@chromium.org>
    Commit-Queue: Mark Pilgrim <pilgrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498093}

--- content/browser/appcache/appcache_response_unittest.cc
+++ content/browser/appcache/appcache_response_unittest.cc
@@ -140,12 +140,13 @@ class AppCacheResponseTest : public testing::Test {
     test_finished_event_->Signal();
   }
 
-  void PushNextTask(const base::Closure& task) {
-    task_stack_.push(std::pair<base::Closure, bool>(task, false));
+  void PushNextTask(base::Closure task) {
+    task_stack_.push(
+        std::pair<base::OnceClosure, bool>(std::move(task), false));
   }
 
   void PushNextTaskAsImmediate(const base::Closure& task) {
-    task_stack_.push(std::pair<base::Closure, bool>(task, true));
+    task_stack_.push(std::pair<base::OnceClosure, bool>(std::move(task), true));
   }
 
   void ScheduleNextTask() {
@@ -154,13 +155,13 @@ class AppCacheResponseTest : public testing::Test {
       TestFinished();
       return;
     }
-    base::Closure task = task_stack_.top().first;
+    base::OnceClosure task = std::move(task_stack_.top().first);
     bool immediate = task_stack_.top().second;
     task_stack_.pop();
     if (immediate)
-      task.Run();
+      std::move(task).Run();
     else
-      base::ThreadTaskRunnerHandle::Get()->PostTask(FROM_HERE, task);
+      base::ThreadTaskRunnerHandle::Get()->PostTask(FROM_HERE, std::move(task));
   }
 
   // Wrappers to call AppCacheResponseReader/Writer Read and Write methods
@@ -768,7 +769,7 @@ class AppCacheResponseTest : public testing::Test {
   std::unique_ptr<base::WaitableEvent> test_finished_event_;
   std::unique_ptr<MockStorageDelegate> storage_delegate_;
   std::unique_ptr<MockAppCacheService> service_;
-  std::stack<std::pair<base::Closure, bool> > task_stack_;
+  std::stack<std::pair<base::OnceClosure, bool>> task_stack_;
 
   std::unique_ptr<AppCacheResponseReader> reader_;
   scoped_refptr<HttpResponseInfoIOBuffer> read_info_buffer_;
