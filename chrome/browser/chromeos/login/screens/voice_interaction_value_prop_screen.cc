ca749554a607bb7278e79870b3381fb83f00eb08
Yue Li <updowndota@chromium.org>
2017-08-29 19:18:55 +0000

    Change app list button animation before value prop accepted
    - Before user accept voice interaction value prop, only show bubble
      without icon and fake window for long press animation.
    - Synchronize value prop accepted prefs with app list button.
    - Update app list button unittest
    
    BUG=b/64846157
    
    Change-Id: Idc8403c45afd69f3ee21c16844c1d15c5e7b5b76
    Reviewed-on: https://chromium-review.googlesource.com/631057
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498191}

--- chrome/browser/chromeos/login/screens/voice_interaction_value_prop_screen.cc
+++ chrome/browser/chromeos/login/screens/voice_interaction_value_prop_screen.cc
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/screens/voice_interaction_value_prop_screen_view.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
-#include "chrome/common/pref_names.h"
-#include "components/prefs/pref_service.h"
 
 namespace chromeos {
 namespace {
@@ -73,8 +71,6 @@ void VoiceInteractionValuePropScreen::OnSkipPressed() {
 
 void VoiceInteractionValuePropScreen::OnNextPressed() {
   GetVoiceInteractionHomeService()->OnAssistantAppRequested();
-  ProfileManager::GetActiveUserProfile()->GetPrefs()->SetBoolean(
-      prefs::kArcVoiceInteractionValuePropAccepted, true);
   Finish(ScreenExitCode::VOICE_INTERACTION_VALUE_PROP_ACCEPTED);
 }
 
