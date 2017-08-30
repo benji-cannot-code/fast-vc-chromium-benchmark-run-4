bae4bf8862ca8a7a6e65be3d3c86b3f9fc73c47e
Gregory <gchatz@chromium.org>
2017-08-30 00:52:09 +0000

    Remove MobileTabClobbered user action in Android.
    This CL removes the MobileTabClobbered user action in Android, since it
    is unused. The user action will be marked obsolete in a follow on CL,
    after the metric is removed on all platforms.
    
    Bug: 546401
    Change-Id: I108720bdc5fab60f24ccee13f5607c83fa3c065a
    Reviewed-on: https://chromium-review.googlesource.com/630301
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Gregory Chatzinoff <gchatz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498317}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -1155,7 +1155,6 @@ public class ChromeTabbedActivity
                     if (tabToBeClobbered != null) {
                         TabModelUtils.setIndex(tabModel, tabToBeClobberedIndex);
                         tabToBeClobbered.reload();
-                        RecordUserAction.record("MobileTabClobbered");
                     } else {
                         launchIntent(url, referer, headers, externalAppId, true, intent);
                     }
@@ -1207,7 +1206,6 @@ public class ChromeTabbedActivity
                                     referer, IntentHandler.getReferrerPolicyFromIntent(intent)));
                         }
                         currentTab.loadUrl(loadUrlParams);
-                        RecordUserAction.record("MobileTabClobbered");
                     } else {
                         launchIntent(url, referer, headers, externalAppId, true, intent);
                     }
@@ -1707,7 +1705,6 @@ public class ChromeTabbedActivity
 
         if (getToolbarManager().back()) {
             recordBackPressedUma("Navigating backward", BACK_PRESSED_NAVIGATED_BACK);
-            RecordUserAction.record("MobileTabClobbered");
             return true;
         }
 
