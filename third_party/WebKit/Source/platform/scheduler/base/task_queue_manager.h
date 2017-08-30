1f3992b98cb3932efae0e3c50ad14acbdc322118
Alex Clarke <alexclarke@chromium.org>
2017-08-30 22:23:50 +0000

    Remove unused renderer scheduler UMAs.
    Bug: 759927, 703449
    Change-Id: I4aa9bbaca031ff4024a0fdfc8d9086d1707e70d3
    Reviewed-on: https://chromium-review.googlesource.com/640387
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498641}

--- third_party/WebKit/Source/platform/scheduler/base/task_queue_manager.h
+++ third_party/WebKit/Source/platform/scheduler/base/task_queue_manager.h
@@ -152,10 +152,6 @@ class PLATFORM_EXPORT TaskQueueManager
   // Removes all canceled delayed tasks.
   void SweepCanceledDelayedTasks();
 
-  // There is a small overhead to recording task delay histograms. If you don't
-  // need them, you can turn them off.
-  void SetRecordTaskDelayHistograms(bool record_task_delay_histograms);
-
  protected:
   friend class LazyNow;
   friend class internal::TaskQueueImpl;
@@ -288,10 +284,6 @@ class PLATFORM_EXPORT TaskQueueManager
   base::Optional<NextTaskDelay> ComputeDelayTillNextTaskLocked(
       LazyNow* lazy_now);
 
-  void MaybeRecordTaskDelayHistograms(
-      const internal::TaskQueueImpl::Task& pending_task,
-      const internal::TaskQueueImpl* queue);
-
   std::unique_ptr<base::trace_event::ConvertableToTraceFormat>
   AsValueWithSelectorResult(bool should_run,
                             internal::WorkQueue* selected_work_queue) const;
@@ -367,8 +359,6 @@ class PLATFORM_EXPORT TaskQueueManager
   }
 
   NextDelayedDoWork next_delayed_do_work_;
-
-  bool record_task_delay_histograms_;
 
   int work_batch_size_;
   size_t task_count_;
