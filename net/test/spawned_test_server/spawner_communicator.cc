4099de9a08d9b76b43e6c40d8642f3ad6eff973d
Sergey Ulanov <sergeyu@google.com>
2017-08-30 20:39:23 +0000

    Reland "Update test server spawner to run multiple test servers in parallel"
    This is a reland of d15b34fd6a7c4775c6488fef1a4b7abd7f1c46e5
    Original change's description:
    > Update test server spawner to run multiple test servers in parallel
    > 
    > Previously test server spawner didn't allow running more than one test
    > server. With this change it's now possible to run multiple tests that
    > depend on test_server in parallel.
    >  1. Updated chrome_test_launch_spawner.py to support more than one
    >     test server. /kill now requires a get parameter to specify which
    >     server should be killed. Number of concurrent test servers is
    >     specified by test runner which instantiates the spawner.
    >  2. Updated test runners for Android and Fuchsia to pass the new
    >     max_servers parameter for the spawner to match test parallelism
    >     on these platforms (1 on Android, 4 on Fuchsia).
    > 
    > Bug: 731302
    > Change-Id: I2e0e0c79dddb0b9f3745a444aae28e9235c3f126
    > Reviewed-on: https://chromium-review.googlesource.com/627339
    > Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    > Reviewed-by: Matt Menke <mmenke@chromium.org>
    > Reviewed-by: John Budorick <jbudorick@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#497967}
    
    TBR=mmenke@chromium.org
    
    Bug: 731302
    Change-Id: I153632cf1fc946f0543a55108555644993a942a5
    Reviewed-on: https://chromium-review.googlesource.com/642151
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498596}

--- net/test/spawned_test_server/spawner_communicator.cc
+++ net/test/spawned_test_server/spawner_communicator.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/test/spawned_test_server/spawner_communicator.h"
 
+#include <inttypes.h>
+
 #include <limits>
 #include <utility>
 
@@ -323,7 +325,7 @@ bool SpawnerCommunicator::StartServer(const std::string& arguments,
   return result == OK;
 }
 
-bool SpawnerCommunicator::StopServer() {
+bool SpawnerCommunicator::StopServer(uint16_t port) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   // It's OK to stop the SpawnerCommunicator without starting it. Some tests
   // have test server on their test fixture but do not actually use it.
@@ -333,7 +335,9 @@ bool SpawnerCommunicator::StopServer() {
   // When the test is done, ask the test server spawner to kill the test server
   // on the remote machine.
   std::string server_return_data;
-  int result = SendCommandAndWaitForResult("kill", "", &server_return_data);
+  std::string command = base::StringPrintf("kill?port=%" PRIu16, port);
+  int result =
+      SendCommandAndWaitForResult(command, std::string(), &server_return_data);
   Shutdown();
   if (result != OK || server_return_data != "killed")
     return false;
