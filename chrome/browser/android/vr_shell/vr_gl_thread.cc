4b4b2a0e76b017102cdada48d5277bc24d0cf7ff
Michael Thiessen <mthiesse@chromium.org>
2017-08-30 22:08:19 +0000

    VR: Introduce a timeout for WebVR to produce a frame before dropping back to browsing.
    Bug: 713730
    Change-Id: Ic65bb52b2b02fd8e06685a286e9aa2b7744a1bea
    Reviewed-on: https://chromium-review.googlesource.com/636386
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498633}

--- chrome/browser/android/vr_shell/vr_gl_thread.cc
+++ chrome/browser/android/vr_shell/vr_gl_thread.cc
@@ -256,6 +256,11 @@ void VrGLThread::OnWebVrFrameAvailable() {
   scene_manager_->OnWebVrFrameAvailable();
 }
 
+void VrGLThread::OnWebVrTimedOut() {
+  DCHECK(task_runner()->BelongsToCurrentThread());
+  scene_manager_->OnWebVrTimedOut();
+}
+
 void VrGLThread::OnProjMatrixChanged(const gfx::Transform& proj_matrix) {
   DCHECK(task_runner()->BelongsToCurrentThread());
   scene_manager_->OnProjMatrixChanged(proj_matrix);
