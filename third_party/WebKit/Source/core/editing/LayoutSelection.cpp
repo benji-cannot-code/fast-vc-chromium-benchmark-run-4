ddb1d94f2056d5c5ddc4af908e6903c256ecff00
Yoichi Osato <yoichio@chromium.org>
2017-08-30 10:09:22 +0000

    Reset LayoutTextFragment SelectionState for ::first-letter.
    We missed setting SelectionState::kNone for LayoutTextFragment.
    The root cause is that SelectionPaintRange::Iterator
     skips not |CanBeSelectionLeaf()| LayoutObject.
    |CanBeSelectionLeaf()| is function and it returns |true| if it should be
     selected currently.
    However, of course, we should reset SelectionState of LayoutObject which
     is not selection leaf when we clear selection.
    
    Bug: 739062
    
    Change-Id: I412910ecf0c1cdafadf3032828f72e384557220a
    Reviewed-on: https://chromium-review.googlesource.com/642095
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498411}

--- third_party/WebKit/Source/core/editing/LayoutSelection.cpp
+++ third_party/WebKit/Source/core/editing/LayoutSelection.cpp
@@ -87,11 +87,9 @@ LayoutObject* SelectionPaintRange::Iterator::operator*() const {
 
 SelectionPaintRange::Iterator& SelectionPaintRange::Iterator::operator++() {
   DCHECK(current_);
-  for (current_ = current_->NextInPreOrder(); current_ && current_ != stop_;
-       current_ = current_->NextInPreOrder()) {
-    if (current_->CanBeSelectionLeaf())
-      return *this;
-  }
+  current_ = current_->NextInPreOrder();
+  if (current_ && current_ != stop_)
+    return *this;
 
   current_ = nullptr;
   return *this;
@@ -303,7 +301,10 @@ static void SetShouldInvalidateSelection(const SelectionMarkingRange& new_range,
   // Invalidate previous selected LayoutObjects except already invalidated
   // above.
   for (LayoutObject* layout_object : old_invalidation_set.layout_objects) {
+    const SelectionState old_state = layout_object->GetSelectionState();
     layout_object->SetSelectionStateIfNeeded(SelectionState::kNone);
+    if (layout_object->GetSelectionState() == old_state)
+      continue;
     layout_object->SetShouldInvalidateSelection();
   }
   for (LayoutBlock* layout_block : old_invalidation_set.layout_blocks)
