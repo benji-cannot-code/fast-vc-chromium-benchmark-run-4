e57881e65d65a25afcc58d141ea5f40677ae7a75
Walter Korman <wkorman@chromium.org>
2017-08-30 22:57:20 +0000

    Rename ShouldActivatePendingTree to match its generated action.
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I72e12154fe7cd5efd5e3fba9b3981df82d0c5b02
    Reviewed-on: https://chromium-review.googlesource.com/642713
    Commit-Queue: Walter Korman <wkorman@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498655}

--- cc/scheduler/scheduler_state_machine.cc
+++ cc/scheduler/scheduler_state_machine.cc
@@ -356,7 +356,7 @@ bool SchedulerStateMachine::ShouldDraw() const {
   return needs_redraw_;
 }
 
-bool SchedulerStateMachine::ShouldActivatePendingTree() const {
+bool SchedulerStateMachine::ShouldActivateSyncTree() const {
   // There is nothing to activate.
   if (!has_pending_tree_)
     return false;
@@ -560,7 +560,7 @@ bool SchedulerStateMachine::ShouldInvalidateLayerTreeFrameSink() const {
 }
 
 SchedulerStateMachine::Action SchedulerStateMachine::NextAction() const {
-  if (ShouldActivatePendingTree())
+  if (ShouldActivateSyncTree())
     return ACTION_ACTIVATE_SYNC_TREE;
   if (ShouldCommit())
     return ACTION_COMMIT;
