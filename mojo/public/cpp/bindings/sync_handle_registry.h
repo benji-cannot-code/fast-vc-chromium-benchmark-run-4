96555deea68ca943e74659e60b3b5d3582a994f6
Ken Rockot <rockot@chromium.org>
2017-08-30 23:31:45 +0000

    Reland "Mojo: Allow duplicate events in SyncHandleRegistry"
    This is a reland of 327ed962ca0353e72e18a5eeeab5112d7d7058b6
    Original change's description:
    > Mojo: Allow duplicate events in SyncHandleRegistry
    > 
    > Allows mulitple registrations for the same WaitableEvent in
    > SyncHandleRegistry to allow for instances of independent nested waiters
    > unwittingly waiting on the same event.
    > 
    > BUG=754945
    > R=yzshen@chromium.org
    > 
    > Change-Id: Ia166d860bf2a07a2db9dc39ae1350b8758950ff4
    > Reviewed-on: https://chromium-review.googlesource.com/641402
    > Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    > Commit-Queue: Ken Rockot <rockot@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#498246}
    
    Bug: 754945
    Change-Id: Id0296e7aac938a6a13202766da2b8cb14ea30502
    Reviewed-on: https://chromium-review.googlesource.com/643487
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498665}

--- mojo/public/cpp/bindings/sync_handle_registry.h
+++ mojo/public/cpp/bindings/sync_handle_registry.h
@@ -7,9 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MOJO_PUBLIC_CPP_BINDINGS_SYNC_HANDLE_REGISTRY_H_
 
 #include <map>
-#include <unordered_map>
 
 #include "base/callback.h"
+#include "base/containers/stack_container.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/sequence_checker.h"
@@ -31,6 +31,10 @@ class MOJO_CPP_BINDINGS_EXPORT SyncHandleRegistry
   static scoped_refptr<SyncHandleRegistry> current();
 
   using HandleCallback = base::Callback<void(MojoResult)>;
+
+  // Registers a |Handle| to be watched for |handle_signals|. If any such
+  // signals are satisfied during a Wait(), the Wait() is woken up and
+  // |callback| is run.
   bool RegisterHandle(const Handle& handle,
                       MojoHandleSignals handle_signals,
                       const HandleCallback& callback);
@@ -39,11 +43,13 @@ class MOJO_CPP_BINDINGS_EXPORT SyncHandleRegistry
 
   // Registers a |base::WaitableEvent| which can be used to wake up
   // Wait() before any handle signals. |event| is not owned, and if it signals
-  // during Wait(), |callback| is invoked. Returns |true| if registered
-  // successfully or |false| if |event| was already registered.
-  bool RegisterEvent(base::WaitableEvent* event, const base::Closure& callback);
+  // during Wait(), |callback| is invoked.  Note that |event| may be registered
+  // multiple times with different callbacks.
+  void RegisterEvent(base::WaitableEvent* event, const base::Closure& callback);
 
-  void UnregisterEvent(base::WaitableEvent* event);
+  // Unregisters a specific |event|+|callback| pair.
+  void UnregisterEvent(base::WaitableEvent* event,
+                       const base::Closure& callback);
 
   // Waits on all the registered handles and events and runs callbacks
   // synchronously for any that become ready.
@@ -55,12 +61,26 @@ class MOJO_CPP_BINDINGS_EXPORT SyncHandleRegistry
  private:
   friend class base::RefCounted<SyncHandleRegistry>;
 
+  using EventCallbackList = base::StackVector<base::Closure, 1>;
+  using EventMap = std::map<base::WaitableEvent*, EventCallbackList>;
+
   SyncHandleRegistry();
   ~SyncHandleRegistry();
 
+  void RemoveInvalidEventCallbacks();
+
   WaitSet wait_set_;
   std::map<Handle, HandleCallback> handles_;
-  std::map<base::WaitableEvent*, base::Closure> events_;
+  EventMap events_;
+
+  // |true| iff this registry is currently dispatching event callbacks in
+  // Wait(). Used to allow for safe event registration/unregistration from event
+  // callbacks.
+  bool is_dispatching_event_callbacks_ = false;
+
+  // Indicates if one or more event callbacks was unregistered during the most
+  // recent event callback dispatch.
+  bool remove_invalid_event_callbacks_after_dispatch_ = false;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
