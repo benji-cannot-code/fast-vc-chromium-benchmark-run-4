d15fc5027b414b3eba3f7b8e4994a65aa9a02345
Kyle Horimoto <khorimoto@google.com>
2017-08-30 02:20:33 +0000

    [CrOS MultiDevice] Add new fields to Remote Device.
    This CL adds booleans for "unlock_key" and "supports_mobile_hotspot" to
    the RemoteDevice struct. This is needed because the new Mojo API will
    return RemoteDevice objects instead of unprocessed ExternalDeviceInfo
    objects, so it will now be necessary to pass this data along so that
    application-level code (e.g., EasyUnlock or Instant Tethering) will be
    able to process RemoteDevices according to these properties.
    
    Bug: 752273
    Change-Id: I7566cacea4d6063b2e4793a6238ad23792fd3664
    Reviewed-on: https://chromium-review.googlesource.com/638855
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Tim Song <tengs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498343}

--- components/cryptauth/remote_device.h
+++ components/cryptauth/remote_device.h
@@ -20,7 +20,8 @@ struct RemoteDevice {
   std::string public_key;
   std::string bluetooth_address;
   std::string persistent_symmetric_key;
-  std::string sign_in_challenge;
+  bool unlock_key;
+  bool supports_mobile_hotspot;
 
   // Note: To save space, the BeaconSeeds may not necessarily be included in
   // this object.
@@ -33,7 +34,8 @@ struct RemoteDevice {
                const std::string& public_key,
                const std::string& bluetooth_address,
                const std::string& persistent_symmetric_key,
-               std::string sign_in_challenge);
+               bool unlock_key,
+               bool supports_mobile_hotspot);
   RemoteDevice(const RemoteDevice& other);
   ~RemoteDevice();
 
