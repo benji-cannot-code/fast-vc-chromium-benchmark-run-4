8e7734fcd68f52813b316ffae13b1084beb8c74b
Randy Smith <rdsmith@chromium.org>
2017-08-30 19:35:34 +0000

    Remove CookieMonsterDelegate usage for tracking cookie changes, switching to registration interface.  This will allow cleaner conversion of cookie stores to Mojo.
    R=mmenke@chromium.org
    
    Bug: 721395
    Change-Id: Iaa4e13316a06781d9f37dad56e482ebec4921266
    Reviewed-on: https://chromium-review.googlesource.com/604547
    Commit-Queue: Randy Smith <rdsmith@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Rohit Rao (ping after 24h) <rohitrao@chromium.org>
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Alok Priyadarshi <alokp@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498570}

--- chrome/test/data/extensions/api_test/cookies/events_spanning/test.js
+++ chrome/test/data/extensions/api_test/cookies/events_spanning/test.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// This test verifies that the cookieshave the correct store Id in spanning
+// This test verifies that the cookies have the correct store Id in spanning
 // split mode.
 var SET_REMOVE_COOKIE = {
   name: 'testSetRemove',
