a97359f30a179122012ca5012b179aec097502a1
Alexei Filippov <alph@chromium.org>
2017-08-30 18:06:22 +0000

    DevTools: Put the wall time to the performance monitor scale.
    Change-Id: I72be9b5f8dd768850ce384f963dbd4706670a145
    Reviewed-on: https://chromium-review.googlesource.com/642686
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498534}

--- third_party/WebKit/Source/core/inspector/InspectorPerformanceAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorPerformanceAgent.cpp
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/frame/LocalFrame.h"
 #include "core/inspector/InspectedFrames.h"
+#include "core/loader/DocumentLoader.h"
 #include "core/paint/PaintTiming.h"
 #include "core/probe/CoreProbes.h"
 #include "platform/InstanceCounters.h"
@@ -83,6 +84,9 @@ Response InspectorPerformanceAgent::getMetrics(
   std::unique_ptr<protocol::Array<protocol::Performance::Metric>> result =
       protocol::Array<protocol::Performance::Metric>::create();
 
+  AppendMetric(result.get(), "Timestamp",
+               (TimeTicks::Now() - TimeTicks()).InSecondsF());
+
   // Renderer instance counters.
   for (size_t i = 0; i < ARRAY_SIZE(kInstanceCounterNames); ++i) {
     AppendMetric(result.get(), kInstanceCounterNames[i],
@@ -102,12 +106,12 @@ Response InspectorPerformanceAgent::getMetrics(
   // Performance timings.
   Document* document = inspected_frames_->Root()->GetDocument();
   if (document) {
-    const PaintTiming& paint_timing = PaintTiming::From(*document);
     AppendMetric(result.get(), "FirstMeaningfulPaint",
-                 paint_timing.FirstMeaningfulPaint());
-    const DocumentTiming& document_timing = document->GetTiming();
+                 PaintTiming::From(*document).FirstMeaningfulPaint());
     AppendMetric(result.get(), "DomContentLoaded",
-                 document_timing.DomContentLoadedEventStart());
+                 document->GetTiming().DomContentLoadedEventStart());
+    AppendMetric(result.get(), "NavigationStart",
+                 document->Loader()->GetTiming().NavigationStart());
   }
 
   *out_result = std::move(result);
