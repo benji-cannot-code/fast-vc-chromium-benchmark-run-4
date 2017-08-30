df2eefe4131d6f4f6a8651b70a06822c54a7494a
Morten Stenshorne <mstensho@opera.com>
2017-08-30 13:04:51 +0000

    Fix Jumbo build.
    InspectorCacheStorageAgent.cpp has recently got a "using" declaration of
    blink::protocol::CacheStorage::Header, and that collided with the
    forward-declaration of blink::Header FetchHeaderList.h. Just remove the
    forward-declaration. It looks like there's no class named Header in this
    district anymore.
    
    TBR=hiroshige@chromium.org
    
    Change-Id: I54e5f7ff3b53a388930b1101417e0dc1fa46af2b
    Reviewed-on: https://chromium-review.googlesource.com/643127
    Reviewed-by: Morten Stenshorne <mstensho@opera.com>
    Commit-Queue: Morten Stenshorne <mstensho@opera.com>
    Cr-Commit-Position: refs/heads/master@{#498439}

--- third_party/WebKit/Source/modules/fetch/FetchHeaderList.h
+++ third_party/WebKit/Source/modules/fetch/FetchHeaderList.h
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class Header;
-
 // http://fetch.spec.whatwg.org/#terminology-headers
 class MODULES_EXPORT FetchHeaderList final
     : public GarbageCollectedFinalized<FetchHeaderList> {
