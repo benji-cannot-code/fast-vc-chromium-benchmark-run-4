6ef1632756240ff2d90611bbeaf8a479db176cba
Tommy C. Li <tommycli@chromium.org>
2017-08-30 18:11:39 +0000

    Omnibox UI Mac: Remove leftover incognitoImage property.
    No longer needed since https://codereview.chromium.org/2082723002, so
    removing this property.
    
    Lays groundwork for implementing favicons on Cocoa.
    
    Bug: 757967
    Change-Id: Icea1a6deb5c1a61360a187b30e7e3b0293df9c4c
    Reviewed-on: https://chromium-review.googlesource.com/642487
    Reviewed-by: Rachel Blum <groby@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498536}

--- chrome/browser/ui/cocoa/omnibox/omnibox_popup_matrix.mm
+++ chrome/browser/ui/cocoa/omnibox/omnibox_popup_matrix.mm
@@ -43,8 +43,6 @@ const NSInteger kMiddleButtonNumber = 2;
                      image:popupView.ImageForMatch(match)
                answerImage:(match.answer ? answerImage : nil)
               forDarkTheme:isDarkTheme]);
-    if (isDarkTheme)
-      [cellData setIncognitoImage:popupView.ImageForMatch(match)];
     [array addObject:cellData];
   }
 
