7f4d0f1820f1d75246e4179e727951c09843aa3a
Charles Harrison <csharrison@chromium.org>
2017-08-30 15:10:55 +0000

    [subresource_filter] Add experimental finch param
    This CL adds a finch param to subresource_filter. This bool aligns with
    the new safe browsing metadata experimental bit.
    
    If the safe browsing check shows that the list entry has the experimental
    bit set to true, then we abort activation matching unless this finch
    param is set.
    
    Bug: 737201,756089
    Change-Id: I6df7c0d3f48ca261445d40e77b3ec7387ef24b84
    Reviewed-on: https://chromium-review.googlesource.com/629256
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498464}

--- components/subresource_filter/core/browser/subresource_filter_features.h
+++ components/subresource_filter/core/browser/subresource_filter_features.h
@@ -63,6 +63,9 @@ struct Configuration {
     // otherwise satisfied. A greater value indicates higher priority.
     int priority = 0;
 
+    // Whether to activate on SafeBrowsing lists with experimental metadata.
+    bool experimental = false;
+
     std::unique_ptr<base::trace_event::TracedValue> ToTracedValue() const;
   };
 
@@ -207,6 +210,7 @@ extern const char kActivationListAbusiveAds[];
 extern const char kActivationListAllAds[];
 
 extern const char kActivationPriorityParameterName[];
+extern const char kActivationExperimentalParameterName[];
 
 extern const char kPerformanceMeasurementRateParameterName[];
 
