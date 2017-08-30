f999d0a3a142d1e4b525b000406192c6a1730255
Vladimir Levin <vmpstr@chromium.org>
2017-08-30 02:21:48 +0000

    cc: Limit the number of colors supported during shader deserialization.
    This patch limits the number of colors we can deserialize to 10k.
    
    R=enne@chromium.org
    
    Bug: 759199
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ic83510decf1bd5f4c65dc4f3a2f9e8107cd9453f
    Reviewed-on: https://chromium-review.googlesource.com/641976
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498345}

--- cc/paint/paint_op_reader.cc
+++ cc/paint/paint_op_reader.cc
@@ -17,6 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace cc {
 namespace {
 
+// If we have more than this many colors, abort deserialization.
+const size_t kMaxShaderColorsSupported = 10000;
+
 bool IsValidPaintShaderType(PaintShader::Type type) {
   return static_cast<uint8_t>(type) <
          static_cast<uint8_t>(PaintShader::Type::kShaderCount);
@@ -248,8 +251,8 @@ void PaintOpReader::Read(sk_sp<PaintShader>* shader) {
   decltype(ref.colors_)::size_type colors_size = 0;
   ReadSimple(&colors_size);
 
-  // If colors_bytes would overflow, then definitely too many.
-  if (colors_size > std::numeric_limits<size_t>::max() / sizeof(SkColor)) {
+  // If there are too many colors, abort.
+  if (colors_size > kMaxShaderColorsSupported) {
     valid_ = false;
     return;
   }
@@ -258,8 +261,6 @@ void PaintOpReader::Read(sk_sp<PaintShader>* shader) {
     valid_ = false;
     return;
   }
-  // TODO(enne): maybe we should cap the number of colors or positions to
-  // something much more reasonable, e.g. 1k?
   ref.colors_.resize(colors_size);
   ReadData(colors_bytes, ref.colors_.data());
 
