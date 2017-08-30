1c5514e3ec92e7dc75bfcaf5dac06c750ac54c8e
Sunny Sachanandani <sunnyps@chromium.org>
2017-08-30 22:21:33 +0000

    cc: Flush command buffers when all tiles are done.
    Command buffer multi flush requires explicit flushing of ordering
    barriers. Flushing in PrepareSendToParent is not sufficient because
    that will not be called when there's no damage.
    
    Flush when all tiles are done and we have no other work. This fixes the
    thread times regressions at least in thread_times.key_silk_cases
    list_animation_simple.html on Nexus 6. Also remove the flush in
    PrepareSendToParent as that's not required.
    
    The regressions were caused by not marking tiles as ready to draw, and
    then evicting those tiles because they weren't used. This would cause
    tiles to be rastered over and over again.
    
    R=piman
    BUG=759554
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I66827a6686b1ebb396ff69ae0f4e40d6db29c63f
    Reviewed-on: https://chromium-review.googlesource.com/641381
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498640}

--- cc/tiles/tile_manager.cc
+++ cc/tiles/tile_manager.cc
@@ -1402,6 +1402,8 @@ void TileManager::CheckIfMoreTilesNeedToBePrepared() {
   resource_pool_->ReduceResourceUsage();
   image_controller_.ReduceMemoryUsage();
 
+  raster_buffer_provider_->Flush();
+
   // TODO(vmpstr): Temporary check to debug crbug.com/642927.
   CHECK(tile_task_manager_);
 
