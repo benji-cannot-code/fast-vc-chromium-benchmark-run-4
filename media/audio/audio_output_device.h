83dbb092758dfd1948d5447f44c631450d840210
Max Morin <maxmorin@chromium.org>
2017-08-30 07:34:32 +0000

    Measure device authorization time from renderer.
    The current stat (Media.Audio.OutputDeviceAuthorizationTime) doesn't
    include time for IPC calls, and may be subject to subtle issues
    regarding e.g. lifetime of IPC objects. The new stat
    (Media.Audio.OutputDeviceAuthorizationTimeForRenderer) more closely
    measures what we actually want: the time that is subject to the timeout.
    
    The new stat also has a larger range, to better capture very long times.
    
    Bug: 752953
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ib57ef6d38a2ca35a1f5c6c7e77ae25156163243a
    Reviewed-on: https://chromium-review.googlesource.com/637840
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Max Morin <maxmorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498387}

--- media/audio/audio_output_device.h
+++ media/audio/audio_output_device.h
@@ -70,6 +70,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/shared_memory.h"
 #include "base/synchronization/waitable_event.h"
+#include "base/time/time.h"
 #include "media/audio/audio_device_thread.h"
 #include "media/audio/audio_output_ipc.h"
 #include "media/audio/scoped_task_runner_observer.h"
@@ -217,6 +218,9 @@ class MEDIA_EXPORT AudioOutputDevice : public AudioRendererSink,
 
   const base::TimeDelta auth_timeout_;
   std::unique_ptr<base::OneShotTimer> auth_timeout_action_;
+
+  // Set when authorization starts, for UMA stats.
+  base::TimeTicks auth_start_time_;
 
   DISALLOW_COPY_AND_ASSIGN(AudioOutputDevice);
 };
