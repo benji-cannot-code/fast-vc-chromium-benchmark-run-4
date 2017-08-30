f4e8acf77fae4cf6a6157d7460216262af9924f6
Yoshifumi Inoue <yosin@chromium.org>
2017-08-30 01:34:45 +0000

    Make BackwardsCharacterIterator constructor to take EphemeralRange
    This patch makes |BackwardsCharacterIterator| constructor to take
    |EphemeralRange| instead of start and end position to make sure start <= end
    in DOM tree order instead of start and end of iteration positions for improving
    code health.
    
    This patch is a preparation of the patch[1].
    
    [1] http://crrev.com/c/640237 Make SimplifiedBackwardsTextIterator constructor
    to take EphemeralRange
    
    Change-Id: I9ca1d9014b56a6dd67ec5e49f3ddcf9d55a624df
    Reviewed-on: https://chromium-review.googlesource.com/640433
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498333}

--- third_party/WebKit/Source/core/editing/iterators/BackwardsCharacterIterator.h
+++ third_party/WebKit/Source/core/editing/iterators/BackwardsCharacterIterator.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BackwardsCharacterIterator_h
 #define BackwardsCharacterIterator_h
 
+#include "core/editing/EphemeralRange.h"
 #include "core/editing/iterators/SimplifiedBackwardsTextIterator.h"
 #include "platform/heap/Heap.h"
 
@@ -38,8 +39,7 @@ class BackwardsCharacterIteratorAlgorithm {
 
  public:
   BackwardsCharacterIteratorAlgorithm(
-      const PositionTemplate<Strategy>&,
-      const PositionTemplate<Strategy>&,
+      const EphemeralRangeTemplate<Strategy>&,
       const TextIteratorBehavior& = TextIteratorBehavior());
 
   void Advance(int);
