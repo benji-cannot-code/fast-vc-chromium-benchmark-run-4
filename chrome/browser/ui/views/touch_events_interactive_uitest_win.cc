3e2a71ef20869310ea39e2d24b06db3838d74f16
Mariia Leliuk <leliukmar@yandex-team.ru>
2017-08-30 16:44:38 +0000

    Ignore WM_POINTERENTER/WM_POINTERLEAVE touch pointer events.
    With current handling of touch events as PointerEvents we get double
    handling of ui::ET_TOUCH_PRESSED and ui::ET_TOUCH_RELEASED event.
    It happens so because at first WM_POINTERENTER is received, transformed
    and handled as ui::ET_TOUCH_PRESSED. And then the same thing happens to
    subsequent WM_POINTERDOWN (though actually touch press is not handled
    twice due to the code in MotionEventAura::OnTouch).
    The same situation occurs with touch release event - it produces
    WM_POINTERUP and subsequent WM_POINTERLEAVE which are both transformed
    to ui::ET_TOUCH_RELEASED.
    So I guess we should just ignore enter/leave events as they can not be
    converted to any ui::TouchEvent.
    
    As another option I was thinking about converting them to
    events.
    
    ui: :MouseEvent(ET_MOUSE_ENTERED/ET_MOUSE_EXITED) like for Pen pointer
    Change-Id: I379a55847c50d47e80defa4485eeb7c97f25311f
    Reviewed-on: https://chromium-review.googlesource.com/629019
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498498}

--- chrome/browser/ui/views/touch_events_interactive_uitest_win.cc
+++ chrome/browser/ui/views/touch_events_interactive_uitest_win.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window_tree_host.h"
 #include "ui/base/models/simple_menu_model.h"
 #include "ui/base/test/ui_controls.h"
+#include "ui/events/gestures/gesture_recognizer_impl.h"
 #include "ui/views/controls/menu/menu_runner.h"
 
 namespace {
@@ -95,6 +96,39 @@ class TouchEventHandler : public ui::EventHandler {
   DISALLOW_COPY_AND_ASSIGN(TouchEventHandler);
 };
 
+class TestingGestureRecognizer : public ui::GestureRecognizerImpl {
+ public:
+  TestingGestureRecognizer() = default;
+  ~TestingGestureRecognizer() override = default;
+
+  int num_touch_press_events() const { return num_touch_press_events_; }
+  int num_touch_release_events() const { return num_touch_release_events_; }
+
+ protected:
+  // Overriden from GestureRecognizerImpl:
+  bool ProcessTouchEventPreDispatch(ui::TouchEvent* event,
+                                    ui::GestureConsumer* consumer) override {
+    switch (event->type()) {
+      case ui::ET_TOUCH_PRESSED:
+        num_touch_press_events_++;
+        break;
+      case ui::ET_TOUCH_RELEASED:
+        num_touch_release_events_++;
+        break;
+      default:
+        break;
+    }
+
+    return ui::GestureRecognizerImpl::ProcessTouchEventPreDispatch(event,
+                                                                   consumer);
+  }
+
+ private:
+  int num_touch_press_events_ = 0;
+  int num_touch_release_events_ = 0;
+  DISALLOW_COPY_AND_ASSIGN(TestingGestureRecognizer);
+};
+
 }  // namespace
 
 class TouchEventsViewTest : public ViewEventTestBase {
@@ -104,11 +138,15 @@ class TouchEventsViewTest : public ViewEventTestBase {
   // ViewEventTestBase:
   void SetUp() override {
     touch_view_ = new views::View();
+    initial_gr_ = ui::GestureRecognizer::Get();
+    gesture_recognizer_ = std::make_unique<TestingGestureRecognizer>();
+    ui::SetGestureRecognizerForTesting(gesture_recognizer_.get());
     ViewEventTestBase::SetUp();
   }
 
   void TearDown() override {
     touch_view_ = nullptr;
+    ui::SetGestureRecognizerForTesting(initial_gr_);
     ViewEventTestBase::TearDown();
   }
 
@@ -141,6 +179,11 @@ class TouchEventsViewTest : public ViewEventTestBase {
     EXPECT_EQ(touch_pointer_count, touch_event_handler.num_touch_presses());
     EXPECT_EQ(0, touch_event_handler.num_pointers_down());
 
+    EXPECT_EQ(touch_pointer_count,
+              gesture_recognizer_->num_touch_press_events());
+    EXPECT_EQ(touch_pointer_count,
+              gesture_recognizer_->num_touch_release_events());
+
     GetWidget()->GetNativeWindow()->GetHost()->window()->RemovePreTargetHandler(
         &touch_event_handler);
     Done();
@@ -148,6 +191,8 @@ class TouchEventsViewTest : public ViewEventTestBase {
 
  protected:
   views::View* touch_view_ = nullptr;
+  std::unique_ptr<TestingGestureRecognizer> gesture_recognizer_;
+  ui::GestureRecognizer* initial_gr_ = nullptr;
 
   DISALLOW_COPY_AND_ASSIGN(TouchEventsViewTest);
 };
