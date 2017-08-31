7aa95f0033bc7e9e66c47ab8c5d2ecbeccbdaa9d
Joshua Bell <jsbell@chromium.org>
2017-08-31 21:45:32 +0000

    Blob interface: remove close() / isClosed
    Chrome had an experimental implementation of a close() method (and
    corresponding isClosed attribute) to allow web apps to explicitly
    drop the resources associated with a Blob when no longer needed.
    This was in the spec for a while but due to flaws [1] and lack of
    implementation by other browsers, it was removed from the spec [2].
    
    Remove the method/attribute and associated tests. It required the
    "experimental" flag to use, so there is no risk of web breakage.
    
    [1] https://github.com/w3c/FileAPI/issues/10
    [2] https://github.com/w3c/FileAPI/pull/68
    
    Bug: 344820
    Change-Id: I6c8ff1f73590f40b68f2b862e15a9db3936bc0f1
    Reviewed-on: https://chromium-review.googlesource.com/638915
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499019}

--- third_party/WebKit/Source/core/fileapi/File.h
+++ third_party/WebKit/Source/core/fileapi/File.h
@@ -41,7 +41,6 @@ class ExecutionContext;
 class FilePropertyBag;
 class FileMetadata;
 class KURL;
-class ScriptState;
 
 class CORE_EXPORT File final : public Blob {
   DEFINE_WRAPPERTYPEINFO();
@@ -156,7 +155,6 @@ class CORE_EXPORT File final : public Blob {
               long long end,
               const String& content_type,
               ExceptionState&) const override;
-  void close(ScriptState*, ExceptionState&) override;
 
   bool IsFile() const override { return true; }
   bool HasBackingFile() const override { return has_backing_file_; }
