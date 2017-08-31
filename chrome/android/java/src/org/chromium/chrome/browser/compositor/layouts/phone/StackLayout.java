4ba7d9d0a9baca8e3e85900caffdf0bec986df32
Matthew Jones <mdjones@chromium.org>
2017-08-31 00:05:14 +0000

    [Home] Fix tab switcher top margin
    This change removes modifications to the layout's viewport and instead
    changes the stack's viewport. This fixes a touch event offset problem
    experienced by using the former method.
    
    BUG=760246
    
    Change-Id: Idf0815e24492fc921ebb0128769e6827af89f9dc
    Reviewed-on: https://chromium-review.googlesource.com/644181
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498682}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/phone/StackLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/phone/StackLayout.java
@@ -90,8 +90,6 @@ public class StackLayout extends Layout implements Animatable<StackLayout.Proper
     /** Rectangles that defines the area where each stack need to be laid out. */
     private final RectF[] mStackRects;
 
-    private final RectF mCachedRect = new RectF();
-
     private int mStackAnimationCount;
 
     private float mFlingSpeed; // pixel/ms
@@ -877,6 +875,7 @@ public class StackLayout extends Layout implements Animatable<StackLayout.Proper
         }
 
         float getTopHeightOffset() {
+            if (FeatureUtilities.isChromeHomeModernEnabled()) return MODERN_TOP_MARGIN_DP;
             if (FeatureUtilities.isChromeHomeEnabled()) return 0;
             return (StackLayout.this.getHeight() - getHeightMinusBrowserControls())
                     * mStackOffsetYPercent;
@@ -1335,16 +1334,12 @@ public class StackLayout extends Layout implements Animatable<StackLayout.Proper
                 resourceManager, fullscreenManager);
         // If the browser controls are at the bottom make sure to use theme colors for this layout
         // specifically.
-        mCachedRect.set(viewport);
         if (fullscreenManager.areBrowserControlsAtBottom() && mLayoutTabs != null) {
             for (LayoutTab t : mLayoutTabs) t.setForceDefaultThemeColor(false);
-            if (FeatureUtilities.isChromeHomeModernEnabled()) {
-                mCachedRect.offset(0, MODERN_TOP_MARGIN_DP * mDpToPx);
-            }
         }
         assert mSceneLayer != null;
 
-        mSceneLayer.pushLayers(getContext(), mCachedRect, contentViewport, this, layerTitleCache,
+        mSceneLayer.pushLayers(getContext(), viewport, contentViewport, this, layerTitleCache,
                 tabContentManager, resourceManager, fullscreenManager);
     }
 }
