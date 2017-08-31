707b3cd685f8dbe46a5d3760b9b4db975b1f81b1
Alexander Alekseev <alemate@chromium.org>
2017-08-31 21:27:51 +0000

    Chrome OS OOBE: Adjust Update screen after UI review.
    Bug: 709256
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ia5c4fbdd5d02780b0f846dbe23f6cb0943059d6f
    Reviewed-on: https://chromium-review.googlesource.com/645166
    Reviewed-by: Wenzhao (Colin) Zang <wzang@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499006}

--- chrome/browser/resources/chromeos/login/oobe_update.js
+++ chrome/browser/resources/chromeos/login/oobe_update.js
@@ -56,10 +56,12 @@ Polymer({
     },
 
     /**
-     * Message above the progress bar.
+     * True if update is fully completed and, probably manual action is
+     * required.
      */
-    updateUpperLabel: {
-      type: String,
+    updateCompleted: {
+      type: Boolean,
+      value: false,
     },
 
     /**
@@ -77,5 +79,15 @@ Polymer({
   setCancelHint: function(message) {
     this.$.checkingForUpdateCancelHint.textContent = message;
     this.$.updatingCancelHint.textContent = message;
+  },
+
+  /**
+   * Calculates visibility of UI element. Returns true if element is hidden.
+   * @param {Boolean} isAllowed Element flag that marks it visible.
+   * @param {Boolean} updateCompleted If update is completed and all
+   * intermediate status elements are hidden.
+   */
+  isNotAllowedOrUpdateCompleted_: function(isAllowed, updateCompleted) {
+    return !isAllowed || updateCompleted;
   },
 });
