6a4ac1a391517829e4127664ae07cb7d4b40a0c7
Iulia Harasim <iuliah@google.com>
2017-08-31 09:01:38 +0000

    [Android] Add signin activity metrics for personalized signin promos
    This CL adds metrics recording for personalized signin promos. New
    histograms related to account information are recorded: whether
    the user has started or completed the signin flow using its default
    account, some other account or has created a new account.
    
    Bug: 737743
    Change-Id: I6bd3d73baaa5ca9b8e1944b823eb92d6bc4ddd46
    Reviewed-on: https://chromium-review.googlesource.com/640384
    Commit-Queue: Iulia Harasim <iuliah@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498794}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninPromoController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninPromoController.java
@@ -207,7 +207,7 @@ public class SigninPromoController {
         view.getSigninButton().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AccountSigninActivity.startFromAddAccountPage(context, mAccessPoint);
+                AccountSigninActivity.startFromAddAccountPage(context, mAccessPoint, true);
             }
         });
 
@@ -227,7 +227,7 @@ public class SigninPromoController {
             @Override
             public void onClick(View view) {
                 AccountSigninActivity.startFromConfirmationPage(
-                        context, mAccessPoint, mAccountName, true);
+                        context, mAccessPoint, mAccountName, true, true);
             }
         });
 
@@ -237,7 +237,7 @@ public class SigninPromoController {
         view.getChooseAccountButton().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AccountSigninActivity.startAccountSigninActivity(context, mAccessPoint);
+                AccountSigninActivity.startAccountSigninActivity(context, mAccessPoint, true);
             }
         });
         view.getChooseAccountButton().setVisibility(View.VISIBLE);
