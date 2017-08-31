30e43b6a52bb2abb9780f956c333e1dd2e400acf
Stefan Zager <szager@chromium.org>
2017-08-31 23:39:08 +0000

    Use LayoutUnits when computing scrollbar existence.
    Before this patch, a non-pixel-aligned box with overflow:auto could get spurious
    scrollbars.
    
    BUG=758920
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ifeba759baa2a2503b12673e53aba0dfe55cb5510
    Reviewed-on: https://chromium-review.googlesource.com/639454
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499071}

--- third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.h
+++ third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.h
@@ -529,6 +529,7 @@ class CORE_EXPORT PaintLayerScrollableArea final
 
   void UpdateScrollCornerStyle();
   LayoutSize MinimumSizeForResizing(float zoom_factor);
+  LayoutRect LayoutContentRect(IncludeScrollbarsInRect) const;
 
   // See comments on isPointInResizeControl.
   void UpdateResizerAreaSet();
