832775477d7e3acb734660184e5be36c243e4960
Kent Tamura <tkent@chromium.org>
2017-08-31 23:01:13 +0000

    Add a wrapper of devtools/scripts/compile_frontend.py
    This is a preparation to remove third_party/WebKit dependency from
    DevTools bot.
    
    NOTRY=true
    
    Bug: 622551
    Change-Id: Id5a8906db02ef47aa9afb222fc3c4391bb4e4160
    Reviewed-on: https://chromium-review.googlesource.com/644833
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499061}

---
+++ third_party/blink/tools/compile_devtools_frontend.py
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env vpython
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Compile DevTools frontend code with Closure compiler.
+
+This script wraps devtools/scripts/compile_frontend.py.
+DevTools bot kicks this script.
+"""
+
+import os
+import sys
+
+sys.path.append(os.path.join(
+    os.path.dirname(__file__), '..', '..', 'WebKit', 'Source', 'devtools', 'scripts'))
+import compile_frontend
+
+if __name__ == '__main__':
+    sys.exit(compile_frontend.main())
