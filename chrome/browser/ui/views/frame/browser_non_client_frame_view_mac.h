9c5df4d9d2d61e4c9eb84407cfa731f54f85f09d
Elly Fong-Jones <ellyjones@google.com>
2017-08-31 18:55:01 +0000

    views: remove BrowserNonClientFrameViewMac::PaintToolbarBackground
    This method is painting the toolbar background incorrectly, and the parent
    class's implementation does it correctly.
    
    Bug: 712271
    Change-Id: Ib9fd3ef67799545ec6c826b0e2e9c3a4b3cc3486
    Reviewed-on: https://chromium-review.googlesource.com/646451
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498938}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_mac.h
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_mac.h
@@ -44,7 +44,6 @@ class BrowserNonClientFrameViewMac : public BrowserNonClientFrameView {
 
  private:
   void PaintThemedFrame(gfx::Canvas* canvas);
-  void PaintToolbarBackground(gfx::Canvas* canvas);
 
   DISALLOW_COPY_AND_ASSIGN(BrowserNonClientFrameViewMac);
 };
