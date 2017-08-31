b35a13faa42c591bf9e91af23124ced29d175523
Bret Sepulveda <bsep@chromium.org>
2017-08-31 21:43:30 +0000

    Refactor ExternalProtocolDialog to remove empty space.
    There's a blank patch of space in the layout due to the dialog using
    MessageBoxView without a message. This patch simplifies the layout. This
    should also take care of all dialog-specific changes needed for Harmony.
    
    Bug: 652508
    Change-Id: I71936e36b860c04f3eca6f95c693d07ebff61ed1
    Reviewed-on: https://chromium-review.googlesource.com/639342
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499018}

--- chrome/browser/ui/views/external_protocol_dialog_browsertest.cc
+++ chrome/browser/ui/views/external_protocol_dialog_browsertest.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <string>
+
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/test/histogram_tester.h"
@@ -10,12 +12,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/external_protocol_dialog_delegate.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/browser/ui/test/test_browser_dialog.h"
 #include "chrome/browser/ui/views/external_protocol_dialog.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
-#include "ui/views/controls/message_box_view.h"
+#include "ui/views/controls/button/checkbox.h"
 #include "url/gurl.h"
 
 namespace test {
@@ -26,7 +29,7 @@ class ExternalProtocolDialogTestApi {
       : dialog_(dialog) {}
 
   void SetCheckBoxSelected(bool checked) {
-    dialog_->message_box_view_->SetCheckBoxSelected(checked);
+    dialog_->remember_decision_checkbox_->SetChecked(checked);
   }
 
  private:
@@ -81,11 +84,12 @@ class TestExternalProtocolDialogDelegate
   DISALLOW_COPY_AND_ASSIGN(TestExternalProtocolDialogDelegate);
 };
 
-class ExternalProtocolDialogBrowserTest : public InProcessBrowserTest {
+class ExternalProtocolDialogBrowserTest : public DialogBrowserTest {
  public:
   ExternalProtocolDialogBrowserTest() {}
 
-  void ShowDialog() {
+  // DialogBrowserTest:
+  void ShowDialog(const std::string& name = "") override {
     content::WebContents* web_contents =
         browser()->tab_strip_model()->GetActiveWebContents();
     int render_process_host_id = web_contents->GetRenderProcessHost()->GetID();
@@ -204,4 +208,11 @@ IN_PROC_BROWSER_TEST_F(ExternalProtocolDialogBrowserTest,
       ExternalProtocolHandler::kRememberCheckboxMetric, 0);
   histogram_tester_.ExpectTotalCount(
       ExternalProtocolHandler::kHandleStateMetric, 0);
+}
+
+// Invokes a dialog that asks the user if an external application is allowed to
+// run. See test_browser_dialog.h.
+IN_PROC_BROWSER_TEST_F(ExternalProtocolDialogBrowserTest,
+                       InvokeDialog_default) {
+  RunDialog();
 }
