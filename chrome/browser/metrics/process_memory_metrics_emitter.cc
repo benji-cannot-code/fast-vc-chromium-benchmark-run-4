5925574c6f719bc01d3e79faaaaad1a4520cf471
Erik Chen <erikchen@chromium.org>
2017-08-31 23:00:00 +0000

    Fix bug in ResourceCoordinatorWebContentsObserver.
    ProcessMemoryMetricsEmitterBrowserTest was failing to catch the null case -
    where no UKM sources were being created. This allowed a bug in
    ResourceCoordinatorWebContentsObserver sneak through which caused no URL to be
    emitted for sources.
    
    Bug: 760733
    Change-Id: Ib59139d88763156d2b8dbe18b24416bd711d5cb0
    Reviewed-on: https://chromium-review.googlesource.com/644388
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Zhen Wang <zhenw@chromium.org>
    Reviewed-by: lpy <lpy@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499059}

--- chrome/browser/metrics/process_memory_metrics_emitter.cc
+++ chrome/browser/metrics/process_memory_metrics_emitter.cc
@@ -249,8 +249,10 @@ void ProcessMemoryMetricsEmitter::CollateResults() {
     private_footprint_total_kb += pmd->os_dump->private_footprint_kb;
     switch (pmd->process_type) {
       case memory_instrumentation::mojom::ProcessType::BROWSER: {
-        EmitBrowserMemoryMetrics(pmd, ukm::UkmRecorder::GetNewSourceID(),
-                                 GetUkmRecorder());
+        // Create a separate source for each non-renderer process type.
+        ukm::SourceId source_id = ukm::UkmRecorder::GetNewSourceID();
+        ukm::UkmRecorder::Get()->UpdateSourceURL(source_id, GURL());
+        EmitBrowserMemoryMetrics(pmd, source_id, GetUkmRecorder());
         break;
       }
       case memory_instrumentation::mojom::ProcessType::RENDERER: {
@@ -271,8 +273,10 @@ void ProcessMemoryMetricsEmitter::CollateResults() {
         break;
       }
       case memory_instrumentation::mojom::ProcessType::GPU: {
-        EmitGpuMemoryMetrics(pmd, ukm::UkmRecorder::GetNewSourceID(),
-                             GetUkmRecorder());
+        // Create a separate source for each non-renderer process type.
+        ukm::SourceId source_id = ukm::UkmRecorder::GetNewSourceID();
+        ukm::UkmRecorder::Get()->UpdateSourceURL(source_id, GURL());
+        EmitGpuMemoryMetrics(pmd, source_id, GetUkmRecorder());
         break;
       }
       case memory_instrumentation::mojom::ProcessType::UTILITY:
@@ -287,7 +291,9 @@ void ProcessMemoryMetricsEmitter::CollateResults() {
   UMA_HISTOGRAM_MEMORY_LARGE_MB("Memory.Total.PrivateMemoryFootprint",
                                 private_footprint_total_kb / 1024);
 
-  ukm::builders::Memory_Experimental(ukm::UkmRecorder::GetNewSourceID())
+  ukm::SourceId source_id = ukm::UkmRecorder::GetNewSourceID();
+  ukm::UkmRecorder::Get()->UpdateSourceURL(source_id, GURL());
+  ukm::builders::Memory_Experimental(source_id)
       .SetTotal2_PrivateMemoryFootprint(private_footprint_total_kb / 1024)
       .Record(GetUkmRecorder());
 }
