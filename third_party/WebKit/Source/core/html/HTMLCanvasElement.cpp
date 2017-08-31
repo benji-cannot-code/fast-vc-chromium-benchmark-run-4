bdf89737d89eb177e9cb74128b455ec6eeb2e5f9
Pavel Feldman <pfeldman@chromium.org>
2017-08-31 03:19:03 +0000

    Canvas: introduce forceDisplayList2dCanvasEnabled setting and set it to true for inspector overlay.
    Bug: 760738
    Change-Id: I24313ae93f92b2bed8ddbdf5589af2dd23629d23
    Reviewed-on: https://chromium-review.googlesource.com/644616
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498744}

--- third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
@@ -897,6 +897,11 @@ bool HTMLCanvasElement::ShouldUseDisplayList() {
   if (RuntimeEnabledFeatures::ForceDisplayList2dCanvasEnabled())
     return true;
 
+  if (GetDocument().GetSettings() &&
+      GetDocument().GetSettings()->GetForceDisplayList2dCanvasEnabled()) {
+    return true;
+  }
+
   if (MemoryCoordinator::IsLowEndDevice())
     return false;
 
