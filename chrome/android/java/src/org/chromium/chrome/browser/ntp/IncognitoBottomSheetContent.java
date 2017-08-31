d90ffa570646437913c61e25d26e86a5704ed3fc
Troy Hildebrandt <thildebr@chromium.org>
2017-08-31 16:35:56 +0000

    [Home] Match non-MD incognito NTP page styling with the MD version.
    When using Chrome Home, this brings the MD incognito NTP styling to the
    non-MD version, leaving the original intact when not using Chrome Home.
    
    Bug: 735294
    Change-Id: I048103f3194159ea5c9858b4dd5a36d68d33b4bb
    Reviewed-on: https://chromium-review.googlesource.com/643949
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Troy Hildebrandt <thildebr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498888}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/IncognitoBottomSheetContent.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/IncognitoBottomSheetContent.java
@@ -6,9 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.ntp;
 
 import android.app.Activity;
+import android.content.res.Resources;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewTreeObserver.OnScrollChangedListener;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
@@ -38,7 +42,7 @@ public class IncognitoBottomSheetContent extends IncognitoNewTabPage implements 
                             mIncognitoNewTabPageView.getResources(), R.color.toolbar_shadow_color),
                 FadingShadow.POSITION_TOP);
 
-        initTextViewColors();
+        initTextViews();
 
         // Hide the incognito image from the Chrome Home NTP pages.
         ImageView incognitoSplash =
@@ -64,7 +68,7 @@ public class IncognitoBottomSheetContent extends IncognitoNewTabPage implements 
         });
     }
 
-    private void initTextViewColors() {
+    private void initTextViews() {
         final int locationBarLightHintTextColor = ApiCompatibilityUtils.getColor(
                 mIncognitoNewTabPageView.getResources(), R.color.locationbar_light_hint_text);
         final int googleBlueColor = ApiCompatibilityUtils.getColor(
@@ -98,9 +102,29 @@ public class IncognitoBottomSheetContent extends IncognitoNewTabPage implements 
             final TextView newTabIncognitoMessageView =
                     (TextView) mIncognitoNewTabPageView.findViewById(
                             R.id.new_tab_incognito_message);
+            final LinearLayout newTabIncognitoContainer =
+                    (LinearLayout) mIncognitoNewTabPageView.findViewById(
+                            R.id.new_tab_incognito_container);
 
             incognitoNtpHeaderView.setTextColor(locationBarLightHintTextColor);
+
+            Resources resources = getContentView().getResources();
             newTabIncognitoMessageView.setTextColor(locationBarLightHintTextColor);
+            newTabIncognitoMessageView.setLineSpacing(
+                    resources.getDimensionPixelSize(R.dimen.md_incognito_ntp_line_spacing), 1.0f);
+            LayoutParams layoutParams = (LayoutParams) newTabIncognitoMessageView.getLayoutParams();
+            layoutParams.bottomMargin = resources.getDimensionPixelSize(
+                    R.dimen.chrome_home_incognito_ntp_bottom_margin);
+
+            newTabIncognitoContainer.setPadding(
+                    resources.getDimensionPixelSize(R.dimen.md_incognito_ntp_padding_left),
+                    newTabIncognitoContainer.getPaddingTop(),
+                    newTabIncognitoContainer.getPaddingRight(),
+                    newTabIncognitoContainer.getPaddingBottom());
+            newTabIncognitoContainer.setGravity(Gravity.START);
+
+            learnMoreView.setPadding(0, 0, 0, 0);
+            learnMoreView.setAllCaps(false);
         }
     }
 
