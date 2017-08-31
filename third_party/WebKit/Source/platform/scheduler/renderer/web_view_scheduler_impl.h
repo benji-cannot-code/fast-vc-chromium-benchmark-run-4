f46dc9058dfeb66f31bdd4696eea59e447f784d9
Alex Clarke <alexclarke@chromium.org>
2017-08-31 12:46:17 +0000

    Adds Emulation.virtualTimePaused event sent when virtual time pauses
    This enables the observer to perform whatever housekeeping they might need
    to do when virtual time has paused.
    
    Bug: 
    Change-Id: I51f8d330c4daa1cdedb8967bbec9aa908693d461
    Reviewed-on: https://chromium-review.googlesource.com/641455
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498831}

--- third_party/WebKit/Source/platform/scheduler/renderer/web_view_scheduler_impl.h
+++ third_party/WebKit/Source/platform/scheduler/renderer/web_view_scheduler_impl.h
@@ -11,7 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/macros.h"
+#include "base/observer_list.h"
 #include "platform/PlatformExport.h"
+#include "platform/scheduler/base/cancelable_closure_holder.h"
 #include "platform/scheduler/base/task_queue.h"
 #include "platform/scheduler/child/web_scheduler.h"
 #include "platform/scheduler/child/web_task_runner_impl.h"
@@ -55,6 +57,8 @@ class PLATFORM_EXPORT WebViewSchedulerImpl : public WebViewScheduler {
   void AudioStateChanged(bool is_audio_playing) override;
   bool HasActiveConnectionForTest() const override;
   void RequestBeginMainFrameNotExpected(bool new_state) override;
+  void AddVirtualTimeObserver(VirtualTimeObserver*) override;
+  void RemoveVirtualTimeObserver(VirtualTimeObserver*) override;
 
   // Virtual for testing.
   virtual void ReportIntervention(const std::string& message);
@@ -102,6 +106,8 @@ class PLATFORM_EXPORT WebViewSchedulerImpl : public WebViewScheduler {
   // number of active connections.
   void UpdateBackgroundBudgetPoolThrottlingState();
 
+  void NotifyVirtualTimePaused();
+
   std::set<WebFrameSchedulerImpl*> frame_schedulers_;
   std::set<unsigned long> pending_loads_;
   std::set<WebFrameSchedulerImpl*> provisional_loads_;
@@ -123,6 +129,9 @@ class PLATFORM_EXPORT WebViewSchedulerImpl : public WebViewScheduler {
   bool has_active_connection_;
   CPUTimeBudgetPool* background_time_budget_pool_;  // Not owned.
   WebViewScheduler::WebViewSchedulerDelegate* delegate_;  // Not owned.
+  base::ObserverList<VirtualTimeObserver> virtual_time_observers_;
+  CancelableClosureHolder virtual_time_paused_notification_;
+  base::TimeTicks initial_virtual_time_;
 
   DISALLOW_COPY_AND_ASSIGN(WebViewSchedulerImpl);
 };
