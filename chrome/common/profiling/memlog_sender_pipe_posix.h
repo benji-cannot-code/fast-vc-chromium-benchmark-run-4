94f534351cd820aa113c2602103e754ec008b492
Erik Chen <erikchen@chromium.org>
2017-08-31 23:03:57 +0000

    Increase the maximum buffer size of messages sent to MemlogSenderPipe.
    Writes on Posix greater than PIPE_BUF are not guaranteed to be atomic, but
    PIPE_BUF is potentially as low as 512, which isn't large enough to accomodate a
    message with 256 stack frames. Instead, use a large buffer size [artifically
    chosen to match that of Windows], and make the Send() method of the
    MemlogSenderPipe a critical section protected by a Lock.
    
    Bug: 761064
    Change-Id: I7b4ef67d0f19bdf93d8239f5bd7bc9fddba6a4b4
    Reviewed-on: https://chromium-review.googlesource.com/646661
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499062}

--- chrome/common/profiling/memlog_sender_pipe_posix.h
+++ chrome/common/profiling/memlog_sender_pipe_posix.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/platform_file.h"
 #include "base/macros.h"
+#include "base/synchronization/lock.h"
 #include "mojo/edk/embedder/scoped_platform_handle.h"
 
 namespace profiling {
@@ -26,6 +27,10 @@ class MemlogSenderPipe {
 
  private:
   mojo::edk::ScopedPlatformHandle handle_;
+
+  // All calls to Send() are wrapped in a Lock, since the size of the data might
+  // be larger than the maximum atomic write size of a pipe on Posix [PIPE_BUF].
+  base::Lock lock_;
 
   DISALLOW_COPY_AND_ASSIGN(MemlogSenderPipe);
 };
