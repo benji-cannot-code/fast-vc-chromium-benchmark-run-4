f8b72ae4a538f380c9c99a007bd417b8622e4e33
Gauthier Ambard <gambard@chromium.org>
2017-08-31 09:49:42 +0000

    Adapt scroll offset to content on back navigation
    When the user navigates back to Home using the back arrow, the offset
    stored might be bigger than the content of the collection.
    If the collection is set to an offset bigger than the content, some
    constraints are not possible to satisfy and the omnibox is not
    displayed.
    This CL fixes it by limiting the offset.
    
    Bug: 760439
    Change-Id: I5f30bcc4e36d3ff03d9caf809bfd1e0e85cd3d5b
    Reviewed-on: https://chromium-review.googlesource.com/645348
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498800}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.h
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.h
@@ -65,6 +65,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Returns the number of sections containing suggestions displayed above this
 // |section|.
 - (NSInteger)numberOfSectionsAbove:(NSInteger)section;
+// Updates the constraints of the collection.
+- (void)updateConstraints;
 
 // Returns the accessibility identifier of the collection.
 + (NSString*)collectionAccessibilityIdentifier;
