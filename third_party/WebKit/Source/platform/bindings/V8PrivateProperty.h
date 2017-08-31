181691254d504ec9f1fa03e6e2847178fa144219
Sayaka Takayama <stakayama@google.com>
2017-08-31 02:10:24 +0000

    v8binding: deleted V8IDBObserverCustom
    V8IDBObserverCustom is now unnecessary because I added TraceWrapper at
    https://crrev.com/c/636790 and https://crrev.com/c/637290 .
    So I deleted V8IDBObserverCustom.cpp.
    
    Bug: 759425
    Change-Id: I2f4a56bb93b35333e9cb5e3b6a03aba13c723841
    Reviewed-on: https://chromium-review.googlesource.com/642635
    Commit-Queue: Sayaka Takayama <stakayama@google.com>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498719}

--- third_party/WebKit/Source/platform/bindings/V8PrivateProperty.h
+++ third_party/WebKit/Source/platform/bindings/V8PrivateProperty.h
@@ -39,7 +39,6 @@ class ScriptWrappable;
   X(FetchEvent, Request)                              \
   X(Global, Event)                                    \
   X(IDBCursor, Request)                               \
-  X(IDBObserver, Callback)                            \
   X(InternalBody, Buffer)                             \
   X(InternalBody, Stream)                             \
   X(IntersectionObserver, Callback)                   \
