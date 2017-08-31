8bdb58d927a2486a7ad8c77e3aa4a111f462762a
Kenichi Ishibashi <bashi@chromium.org>
2017-08-31 06:29:27 +0000

    idl_parser: Fix a typo in IDLNode
    POSSITION -> POSITION.
    
    No one uses this property so we may want to remove it, but I feel
    that this is worth keeping for debugging etc.
    
    Bug: None
    Change-Id: I9625a4ee33917703a926f83febf38734a9579814
    Reviewed-on: https://chromium-review.googlesource.com/644629
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498770}

--- tools/idl_parser/idl_node.py
+++ tools/idl_parser/idl_node.py
@@ -84,7 +84,7 @@ class IDLNode(object):
       'WARNINGS': [],
       'FILENAME': filename,
       'LINENO' : lineno,
-      'POSSITION' : pos,
+      'POSITION' : pos,
     }
 
     self._children = []
