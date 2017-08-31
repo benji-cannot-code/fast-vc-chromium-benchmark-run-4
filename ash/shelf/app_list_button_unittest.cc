f5e81a5d60021db9ae68a170a4bae79504640733
Yue Li <updowndota@chromium.org>
2017-08-31 22:20:13 +0000

    Synchronize flags for app list button when secondary display added
    - A new instance of app list button will be created if a secondary
      display is added.
    - Synchronize the state and flags and add getters in the controller (currently shell, will be move into new voice interaction related controller)
    - Update unittests so that each test case will also test on secondary
      display.
    
    BUG=b/65162777
    
    Change-Id: Ib761362e3e87ab2f08eee6e40ff23378e1ec9b85
    Reviewed-on: https://chromium-review.googlesource.com/644228
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499041}

--- ash/shelf/app_list_button_unittest.cc
+++ ash/shelf/app_list_button_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/shelf/app_list_button.h"
 
+#include "ash/root_window_controller.h"
 #include "ash/session/session_controller.h"
 #include "ash/shelf/shelf.h"
 #include "ash/shelf/shelf_view.h"
@@ -48,6 +49,17 @@ class AppListButtonTest : public AshTestBase {
     app_list_button_->OnGestureEvent(event);
   }
 
+  void SendGestureEventToSecondaryDisplay(ui::GestureEvent* event) {
+    // Add secondary display.
+    UpdateDisplay("1+1-1000x600,1002+0-600x400");
+    // Send the gesture event to the secondary display.
+    Shell::GetRootWindowControllerWithDisplayId(GetSecondaryDisplay().id())
+        ->shelf()
+        ->GetShelfViewForTesting()
+        ->GetAppListButton()
+        ->OnGestureEvent(event);
+  }
+
   const AppListButton* app_list_button() const { return app_list_button_; }
 
  protected:
@@ -71,6 +83,11 @@ TEST_F(AppListButtonTest, LongPressGestureWithoutVoiceInteractionFlag) {
   ui::GestureEvent long_press =
       CreateGestureEvent(ui::GestureEventDetails(ui::ET_GESTURE_LONG_PRESS));
   SendGestureEvent(&long_press);
+  RunAllPendingInMessageLoop();
+  EXPECT_EQ(0u, test_app_list_presenter.voice_session_count());
+
+  // Test long press gesture on secondary display.
+  SendGestureEventToSecondaryDisplay(&long_press);
   RunAllPendingInMessageLoop();
   EXPECT_EQ(0u, test_app_list_presenter.voice_session_count());
 }
@@ -103,6 +120,11 @@ TEST_F(VoiceInteractionAppListButtonTest,
   SendGestureEvent(&long_press);
   RunAllPendingInMessageLoop();
   EXPECT_EQ(1u, test_app_list_presenter.voice_session_count());
+
+  // Test long press gesture on secondary display.
+  SendGestureEventToSecondaryDisplay(&long_press);
+  RunAllPendingInMessageLoop();
+  EXPECT_EQ(2u, test_app_list_presenter.voice_session_count());
 }
 
 TEST_F(VoiceInteractionAppListButtonTest, LongPressGestureWithSecondaryUser) {
@@ -122,6 +144,11 @@ TEST_F(VoiceInteractionAppListButtonTest, LongPressGestureWithSecondaryUser) {
   RunAllPendingInMessageLoop();
   // Voice interaction is disabled for secondary user, so the count here should
   // be 0.
+  EXPECT_EQ(0u, test_app_list_presenter.voice_session_count());
+
+  // Test long press gesture on secondary display.
+  SendGestureEventToSecondaryDisplay(&long_press);
+  RunAllPendingInMessageLoop();
   EXPECT_EQ(0u, test_app_list_presenter.voice_session_count());
 }
 
@@ -149,6 +176,11 @@ TEST_F(VoiceInteractionAppListButtonTest,
   // After value prop has been accepted, if voice interaction is disalbed in
   // settings we should not handle long press action in app list button.
   EXPECT_EQ(0u, test_app_list_presenter.voice_session_count());
+
+  // Test long press gesture on secondary display.
+  SendGestureEventToSecondaryDisplay(&long_press);
+  RunAllPendingInMessageLoop();
+  EXPECT_EQ(0u, test_app_list_presenter.voice_session_count());
 }
 
 TEST_F(VoiceInteractionAppListButtonTest,
@@ -173,6 +205,11 @@ TEST_F(VoiceInteractionAppListButtonTest,
   // Before setup flow completed we should show the animation even if the
   // settings are disabled.
   EXPECT_EQ(1u, test_app_list_presenter.voice_session_count());
+
+  // Test long press gesture on secondary display.
+  SendGestureEventToSecondaryDisplay(&long_press);
+  RunAllPendingInMessageLoop();
+  EXPECT_EQ(2u, test_app_list_presenter.voice_session_count());
 }
 
 namespace {
