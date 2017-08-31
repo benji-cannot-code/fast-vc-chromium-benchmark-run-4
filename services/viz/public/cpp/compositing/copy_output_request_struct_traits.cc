892169f120861d39dfcd0f0aa503c852b73f91a7
yiyix <yiyix@chromium.org>
2017-08-31 02:50:58 +0000

    viz: Move CopyOutputRequest's mojom from cc/ipc to services/viz
    For more details:
    https://docs.google.com/spreadsheets/d/1gHvkSVehWvWvCyCa-V0tZezULNTpiW0T8tLE3DjVgkg
    
    TBR=weiliangc@chromium.org
    
    Bug: 722935
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I4c24436fcdf674c475c39e4dbb07f7512f8288ff
    Reviewed-on: https://chromium-review.googlesource.com/641960
    Commit-Queue: Yi Xu <yiyix@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498739}

--- services/viz/public/cpp/compositing/copy_output_request_struct_traits.cc
+++ services/viz/public/cpp/compositing/copy_output_request_struct_traits.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "cc/ipc/copy_output_request_struct_traits.h"
+#include "services/viz/public/cpp/compositing/copy_output_request_struct_traits.h"
 
 #include <utility>
 
@@ -15,7 +15,7 @@ namespace {
 // When we're sending a CopyOutputRequest, we keep the result_callback_ in a
 // CopyOutputResultSenderImpl and send a CopyOutputResultSenderPtr to the other
 // process. When SendResult is called, we run the stored result_callback_.
-class CopyOutputResultSenderImpl : public cc::mojom::CopyOutputResultSender {
+class CopyOutputResultSenderImpl : public viz::mojom::CopyOutputResultSender {
  public:
   CopyOutputResultSenderImpl(
       viz::CopyOutputRequest::CopyOutputRequestCallback result_callback)
@@ -41,7 +41,7 @@ class CopyOutputResultSenderImpl : public cc::mojom::CopyOutputResultSender {
   viz::CopyOutputRequest::CopyOutputRequestCallback result_callback_;
 };
 
-void SendResult(cc::mojom::CopyOutputResultSenderPtr ptr,
+void SendResult(viz::mojom::CopyOutputResultSenderPtr ptr,
                 std::unique_ptr<viz::CopyOutputResult> result) {
   ptr->SendResult(std::move(result));
 }
@@ -51,11 +51,11 @@ void SendResult(cc::mojom::CopyOutputResultSenderPtr ptr,
 namespace mojo {
 
 // static
-cc::mojom::CopyOutputResultSenderPtr
-StructTraits<cc::mojom::CopyOutputRequestDataView,
+viz::mojom::CopyOutputResultSenderPtr
+StructTraits<viz::mojom::CopyOutputRequestDataView,
              std::unique_ptr<viz::CopyOutputRequest>>::
     result_sender(const std::unique_ptr<viz::CopyOutputRequest>& request) {
-  cc::mojom::CopyOutputResultSenderPtr result_sender;
+  viz::mojom::CopyOutputResultSenderPtr result_sender;
   auto impl = std::make_unique<CopyOutputResultSenderImpl>(
       std::move(request->result_callback_));
   MakeStrongBinding(std::move(impl), MakeRequest(&result_sender));
@@ -63,9 +63,9 @@ StructTraits<cc::mojom::CopyOutputRequestDataView,
 }
 
 // static
-bool StructTraits<cc::mojom::CopyOutputRequestDataView,
+bool StructTraits<viz::mojom::CopyOutputRequestDataView,
                   std::unique_ptr<viz::CopyOutputRequest>>::
-    Read(cc::mojom::CopyOutputRequestDataView data,
+    Read(viz::mojom::CopyOutputRequestDataView data,
          std::unique_ptr<viz::CopyOutputRequest>* out_p) {
   auto request = viz::CopyOutputRequest::CreateEmptyRequest();
 
@@ -81,7 +81,7 @@ bool StructTraits<cc::mojom::CopyOutputRequestDataView,
     return false;
 
   auto result_sender =
-      data.TakeResultSender<cc::mojom::CopyOutputResultSenderPtr>();
+      data.TakeResultSender<viz::mojom::CopyOutputResultSenderPtr>();
   request->result_callback_ =
       base::BindOnce(SendResult, base::Passed(&result_sender));
 
