4fb57db966aecd8f41227655d62a05b664551734
Greg Thompson <grt@chromium.org>
2017-08-31 12:35:09 +0000

    Revert "Generate VisualElementsManifests based on the OS's support for dark text."
    This reverts commit 992d40958b86c74cb5b73a543946614607ad4232.
    
    Reason for revert: x64 compile break
    
    Original change's description:
    > Generate VisualElementsManifests based on the OS's support for dark text.
    > 
    > When brands provide assets with light backgrounds and the OS supports
    > drawing dark text, generated manifests will point to those light
    > assets. Otherwise, the dark background assets will be used. The
    > OnOsUpdate handler also now supports updating the manifest and
    > refreshing the Start Menu as needed.
    > 
    > BUG=722573
    > 
    > Change-Id: Ic959828c84755431cb1d9112f3d8df13ede63bb1
    > Reviewed-on: https://chromium-review.googlesource.com/632157
    > Commit-Queue: Greg Thompson <grt@chromium.org>
    > Reviewed-by: Scott Graham <scottmg@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#498826}
    
    TBR=scottmg@chromium.org,grt@chromium.org
    
    Change-Id: I1a08e0d640dbeb12e54f1e9eb18651701dc50547
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 722573
    Reviewed-on: https://chromium-review.googlesource.com/645846
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498828}

--- chrome/installer/setup/setup_util.cc
+++ chrome/installer/setup/setup_util.cc
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/version.h"
 #include "base/win/registry.h"
-#include "base/win/windows_version.h"
 #include "chrome/install_static/install_details.h"
 #include "chrome/install_static/install_modes.h"
 #include "chrome/install_static/install_util.h"
@@ -847,12 +846,6 @@ base::Time GetConsoleSessionStartTime() {
   FILETIME filetime = {wts_info->LogonTime.u.LowPart,
                        wts_info->LogonTime.u.HighPart};
   return base::Time::FromFileTime(filetime);
-}
-
-bool OsSupportsDarkTextTiles() {
-  auto windows_version = base::win::GetVersion();
-  return windows_version == base::win::VERSION_WIN8_1 ||
-         windows_version >= base::win::VERSION_WIN10_RS1;
 }
 
 }  // namespace installer
