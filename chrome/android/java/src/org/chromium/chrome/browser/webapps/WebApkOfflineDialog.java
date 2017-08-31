11b44eb894e3a1d9ec2a95e4691b4f5728ef4afb
Xi Han <hanxi@google.com>
2017-08-31 12:02:12 +0000

    Polish WebAPK's offline dialog.
    This dialog is implemented in Chrome, so we use android.support.v7.app library
    and reuse Chrome's code.
    
    See screenshots:https://drive.google.com/open?id=0B7zEF5GgyYmpZ2lIRjdDS2pYUTg
    
    Bug: 759582
    Change-Id: I009b41d49a4c765a7fd996d4d6acf363d8d99822
    Reviewed-on: https://chromium-review.googlesource.com/644221
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498824}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkOfflineDialog.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkOfflineDialog.java
@@ -5,11 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.webapps;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.view.ContextThemeWrapper;
+import android.support.v7.app.AlertDialog;
 
 import org.chromium.chrome.R;
 
@@ -30,9 +29,7 @@ public class WebApkOfflineDialog {
      * @param appName The name of the WebAPK for which the dialog is shown.
      */
     public void show(Context context, final DialogListener listener, String appName) {
-        // The context theme wrapper is needed for pre-L.
-        AlertDialog.Builder builder = new AlertDialog.Builder(
-                new ContextThemeWrapper(context, android.R.style.Theme_DeviceDefault_Light_Dialog));
+        AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.AlertDialogTheme);
         builder.setMessage(context.getString(R.string.webapk_offline_dialog, appName))
                 .setNegativeButton(R.string.webapk_offline_dialog_quit_button,
                         new DialogInterface.OnClickListener() {
