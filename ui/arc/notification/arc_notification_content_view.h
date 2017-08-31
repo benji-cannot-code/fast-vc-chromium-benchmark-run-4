3781eaf2b634fea662412ed3c5942f6535d83256
yoshiki iguchi <yoshiki@chromium.org>
2017-08-31 19:15:31 +0000

    Auto-expand popup notification
    This cl expands popup notification. This is consistent behavior with Android.
    
    This Cl affects only expandable notification: new-styled and ARC notifications.
    
    Bug: b/64776170
    Change-Id: Ie595a2acdb810a5397ee0c6c26c758369eba04d9
    Reviewed-on: https://chromium-review.googlesource.com/644973
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498946}

--- ui/arc/notification/arc_notification_content_view.h
+++ ui/arc/notification/arc_notification_content_view.h
@@ -71,6 +71,8 @@ class ArcNotificationContentView
   void AttachSurface();
   void Activate();
   void UpdateAccessibleName();
+  void SetExpanded(bool expanded);
+  bool IsExpanded() const;
 
   // views::NativeViewHost
   void ViewHierarchyChanged(
