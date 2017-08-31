a392c0654c5bbaeb2fc44377e3b74b464e40447c
Robert Ma <robertma@chromium.org>
2017-08-31 20:27:09 +0000

    Extract all filename testing/filtering routines into common.py for reuse
    There were similar routines for checking whether a file is exportable based on
    its name (or path) all over the codebase. Some of them were slightly different,
    which caused intricate issues like 759181. This change extracts all of them
    into one central place (w3c/common.py) so that they can be reused.
    
    
    Bug: 734799, 759181
    Change-Id: Ie54e2f455c046383f1aaab2670596ffdf24128b7
    Reviewed-on: https://chromium-review.googlesource.com/643760
    Commit-Queue: Robert Ma <robertma@chromium.org>
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498977}

--- third_party/WebKit/Tools/Scripts/webkitpy/w3c/gerrit.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/w3c/gerrit.py
@@ -6,10 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import base64
 import json
 import logging
-import os
 import re
 
-from webkitpy.w3c.common import CHROMIUM_WPT_DIR
+from webkitpy.w3c.common import CHROMIUM_WPT_DIR, is_file_exportable
 
 _log = logging.getLogger(__name__)
 URL_BASE = 'https://chromium-review.googlesource.com'
@@ -134,25 +133,16 @@ class GerritCL(object):
         if 'NOEXPORT=true' in self.current_revision['commit_with_footers']:
             return False
 
-        files_in_wpt = [f for f in files if f.startswith('third_party/WebKit/LayoutTests/external/wpt')]
+        files_in_wpt = [f for f in files if f.startswith(CHROMIUM_WPT_DIR)]
         if not files_in_wpt:
             return False
 
-        exportable_files = [f for f in files_in_wpt if self.exportable_filename(f)]
+        exportable_files = [f for f in files_in_wpt if is_file_exportable(f)]
 
         if not exportable_files:
             return False
 
         return True
-
-    def exportable_filename(self, filename):
-        """Returns True if the file could be exportable, or False otherwise."""
-        filename = os.path.basename(filename.lower())
-        return (
-            not filename.endswith('-expected.txt')
-            and not filename.startswith('.')
-            and not filename.endswith('.json')
-        )
 
     def get_patch(self):
         """Gets patch for latest revision of CL.
@@ -171,6 +161,7 @@ class GerritCL(object):
         This method expects a `git diff`-formatted patch.
         """
         filtered_patch = []
+        diff_re = re.compile(r'^diff --git a/(.*) b/(.*)$')
 
         # Patch begins with message, always applicable.
         in_exportable_diff = True
@@ -183,7 +174,10 @@ class GerritCL(object):
                 continue
 
             # File is being changed, detect if it's exportable.
-            if CHROMIUM_WPT_DIR in line and not line.endswith('-expected.txt'):
+            match = diff_re.match(line)
+            assert match, "%s is not an expected git diff header" % line
+            _, new_file = match.groups()
+            if CHROMIUM_WPT_DIR in new_file and is_file_exportable(new_file):
                 in_exportable_diff = True
                 filtered_patch.append(line)
             else:
