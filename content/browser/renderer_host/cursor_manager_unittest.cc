abb256e9bf9828cb64a3f47e7e78fbac9cc00960
Dave Tapuska <dtapuska@chromium.org>
2017-08-31 21:37:34 +0000

    Move mock widget impl around.
    There was an implementation inside the security_exploit_browsertest
    that was unncessary provided the code was organized correctly.
    
    BUG=722928
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: Ic7a57fef2d7dfb0b51aa19cd8d479c812c292b8e
    Reviewed-on: https://chromium-review.googlesource.com/646272
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499012}

--- content/browser/renderer_host/cursor_manager_unittest.cc
+++ content/browser/renderer_host/cursor_manager_unittest.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/scoped_task_environment.h"
 #include "build/build_config.h"
-#include "content/browser/renderer_host/mock_widget_impl.h"
 #include "content/browser/renderer_host/render_widget_host_delegate.h"
 #include "content/browser/renderer_host/render_widget_host_impl.h"
 #include "content/browser/renderer_host/render_widget_host_view_base.h"
@@ -16,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/mock_render_process_host.h"
 #include "content/public/test/test_browser_context.h"
 #include "content/test/dummy_render_widget_host_delegate.h"
+#include "content/test/mock_widget_impl.h"
 #include "content/test/test_render_view_host.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
--- content/test/mock_widget_impl.h
+++ content/test/mock_widget_impl.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_BROWSER_RENDERER_HOST_MOCK_WIDGET_IMPL_H_
-#define CONTENT_BROWSER_RENDERER_HOST_MOCK_WIDGET_IMPL_H_
+#ifndef CONTENT_TEST_MOCK_WIDGET_IMPL_H_
+#define CONTENT_TEST_MOCK_WIDGET_IMPL_H_
 
 #include "content/common/widget.mojom.h"
 #include "mojo/public/cpp/bindings/binding.h"
@@ -25,4 +25,4 @@ class MockWidgetImpl : public mojom::Widget {
 
 }  // namespace content
 
-#endif  // CONTENT_BROWSER_RENDERER_HOST_MOCK_WIDGET_IMPL_H_
+#endif  // CONTENT_TEST_MOCK_WIDGET_IMPL_H_
