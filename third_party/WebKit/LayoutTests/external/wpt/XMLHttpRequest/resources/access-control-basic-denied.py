eb800fd906b9e20701d160aedfc8c3396a4e7c1c
Austin James Ahlstrom <aahlstrom@google.com>
2017-08-31 05:00:22 +0000

    Porting access-control-basic-denied-preflight-cache to WPT
    Note that in the server-side file,
    the "Cache-Control" header is set to "no-store".
    This does not relate specifically to this bug,
    but is added as a general practice to avoid
    unexpected behavior caused by caching.
    
    Bug: 745385
    Change-Id: I0b25edd79cf2bf34c6c3602f74c99c895b2ca490
    Reviewed-on: https://chromium-review.googlesource.com/630338
    Commit-Queue: Austin James Ahlstrom <aahlstrom@google.com>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Takeshi Yoshino <tyoshino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498755}

--- third_party/WebKit/LayoutTests/external/wpt/XMLHttpRequest/resources/access-control-basic-denied.py
+++ third_party/WebKit/LayoutTests/external/wpt/XMLHttpRequest/resources/access-control-basic-denied.py
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 def main(request, response):
-    response.headers.set("Content-Type", "text/plain");
+    response.headers.set("Cache-Control", "no-store")
+    response.headers.set("Content-Type", "text/plain")
 
     response.text = "FAIL: Cross-domain access allowed."
