3056e268f40d486f56cb20586a1a7317cd788d6e
Nico Weber <thakis@chromium.org>
2017-09-01 17:43:12 +0000

    Fix some more -Wdelete-non-virtual-dtor warnings on iOS.
    Bug: 673171
    Change-Id: I25e05829fdb54412e681bc6a58171117644be819
    Reviewed-on: https://chromium-review.googlesource.com/647989
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499228}

--- ios/chrome/browser/ui/dialogs/java_script_dialog_presenter_impl.h
+++ ios/chrome/browser/ui/dialogs/java_script_dialog_presenter_impl.h
@@ -13,7 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @class AlertCoordinator;
 @class DialogPresenter;
 
-class JavaScriptDialogPresenterImpl : public web::JavaScriptDialogPresenter {
+class JavaScriptDialogPresenterImpl final
+    : public web::JavaScriptDialogPresenter {
  public:
   explicit JavaScriptDialogPresenterImpl(DialogPresenter* dialogPresenter);
   ~JavaScriptDialogPresenterImpl();
