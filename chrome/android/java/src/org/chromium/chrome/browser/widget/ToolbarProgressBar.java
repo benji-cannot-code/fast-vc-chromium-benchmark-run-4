ddbc1bca1f86b34abdbb72605aadc8b62a93ce61
Matthew Jones <mdjones@chromium.org>
2017-09-01 17:40:54 +0000

    Fix progress bar crash
    The animation logic in the progress bar has no dependencies since it
    has launched (previously depended on native flags). This change moves
    the animation init to the progress bar constructor so it is always
    available.
    
    BUG=761051
    
    Change-Id: If59215a669db64be07c55f4dd19ca893d8af823f
    Reviewed-on: https://chromium-review.googlesource.com/646696
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499224}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/ToolbarProgressBar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/ToolbarProgressBar.java
@@ -171,6 +171,7 @@ public class ToolbarProgressBar extends ClipDrawableProgressBar {
         setAlpha(0.0f);
         mMarginTop = topMargin;
         mUseStatusBarColorAsBackground = useStatusBarColorAsBackground;
+        mAnimationLogic = new ProgressAnimationSmooth();
 
         // This tells accessibility services that progress bar changes are important enough to
         // announce to the user even when not focused.
@@ -232,7 +233,6 @@ public class ToolbarProgressBar extends ClipDrawableProgressBar {
         if (mAnimationInitialized) return;
 
         mAnimationInitialized = true;
-        mAnimationLogic = new ProgressAnimationSmooth();
 
         // Only use the indeterminate animation if the Android version is > J.
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN_MR2) {
