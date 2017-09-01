2d929eea9ced2ecd0198d51fafaf58d4a8cd4391
Tetsui Ohkubo <tetsui@chromium.org>
2017-09-01 07:59:50 +0000

    Apply system style to resolution change notification.
    In new-style notification system notification has different style from
    other notifications such as web notifications.
    * The context header shous the title "Chrome OS system".
    * The context header has accent color.
    * New MD icon is used.
    This CL applies the system style to "resolution was changed to..."
    notification.
    
    If chrome://flags#enable-message-center-new-style-notification is
    disabled, the notification will be shown with the left icon missing.
    
    TEST=manually tested on chell device (Ctrl-Shift-+)
    BUG=760883
    
    Change-Id: I882d8980b5ecf493a63ba7a56d379cb22c928f6c
    Reviewed-on: https://chromium-review.googlesource.com/605342
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499151}

--- ash/system/screen_layout_observer_unittest.cc
+++ ash/system/screen_layout_observer_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/system/tray/system_tray.h"
+#include "ash/system/web_notification/web_notification_tray.h"
 #include "ash/test/ash_test_base.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "base/strings/string16.h"
@@ -32,6 +33,16 @@ class ScreenLayoutObserverTest : public AshTestBase {
   ~ScreenLayoutObserverTest() override;
 
  protected:
+  void SetUp() override {
+    AshTestBase::SetUp();
+    WebNotificationTray::DisableAnimationsForTest(true);
+  }
+
+  void TearDown() override {
+    WebNotificationTray::DisableAnimationsForTest(false);
+    AshTestBase::TearDown();
+  }
+
   ScreenLayoutObserver* GetScreenLayoutObserver();
   void CheckUpdate();
 
