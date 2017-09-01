56919b7c14cc9d7114e8fd25f55a910f67831e49
Jiaquan He <hejq@google.com>
2017-09-01 21:19:43 +0000

    Add Play Store query state histogram.
    Bug: 761155
    Change-Id: I2d6f1e7aa8875e102b823818a6d275a3302c454b
    Reviewed-on: https://chromium-review.googlesource.com/646831
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Jiaquan He <hejq@google.com>
    Cr-Commit-Position: refs/heads/master@{#499301}

--- chrome/browser/ui/app_list/search/arc/arc_playstore_search_provider.cc
+++ chrome/browser/ui/app_list/search/arc/arc_playstore_search_provider.cc
@@ -19,6 +19,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 constexpr int kHistogramBuckets = 13;
+constexpr char kAppListPlayStoreQueryStateHistogram[] =
+    "Apps.AppListPlayStoreQueryState";
+constexpr int kAppListPlayStoreQueryStateNum = 3;
 
 // Skips Play Store apps that have equivalent extensions installed.
 // Do not skip recent instant apps since they should be treated like
@@ -77,6 +80,8 @@ void ArcPlayStoreSearchProvider::OnResults(
     base::TimeTicks query_start_time,
     arc::mojom::AppDiscoveryRequestState state,
     std::vector<arc::mojom::AppDiscoveryResultPtr> results) {
+  UMA_HISTOGRAM_ENUMERATION(kAppListPlayStoreQueryStateHistogram, state,
+                            kAppListPlayStoreQueryStateNum);
   if (state != arc::mojom::AppDiscoveryRequestState::SUCCESS) {
     DCHECK(results.empty());
     ClearResults();
