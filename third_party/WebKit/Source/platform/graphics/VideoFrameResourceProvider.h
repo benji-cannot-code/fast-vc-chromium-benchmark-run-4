c231d808932396a0d29de22f0f72d666e9c8a103
Daniel Bratell <bratell@opera.com>
2017-09-01 16:08:33 +0000

    Adding missing include guard on blink header
    VideoFrameResourceProvider.h was missing an include guard which happened
    to work in normal builds, but not in jumbo builds.
    
    R=haraken@chromium.org, lethalantidote@chromium.org
    
    Change-Id: I72b9c9343ce6862f26d2ae89beaf3af3b084e0ed
    Reviewed-on: https://chromium-review.googlesource.com/647538
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#499206}

--- third_party/WebKit/Source/platform/graphics/VideoFrameResourceProvider.h
+++ third_party/WebKit/Source/platform/graphics/VideoFrameResourceProvider.h
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifndef VideoFrameResourceProvider_h
+#define VideoFrameResourceProvider_h
+
 namespace cc {
 class RenderPass;
 }
@@ -20,3 +23,5 @@ class VideoFrameResourceProvider {
 };
 
 }  // namespace blink
+
+#endif  // VideoFrameResourceProvider_h
