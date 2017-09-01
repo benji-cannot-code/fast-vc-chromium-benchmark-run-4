26ac851262dae55556033125fc9b11df4bf193b5
Matthew Jones <mdjones@chromium.org>
2017-09-01 15:05:08 +0000

    [Home] Update bottom toolbar top shadow to work with infobars
    This change has the toolbar's shadow transition to a line when an
    infobar appears. The result is less jank when scrolling the infobar
    off-screen (no shadow instantly disappearing) and making the infobar
    appear as if it is part of the same layer as the toolbar.
    
    BUG=755030
    
    Change-Id: I9197e75ea2e54529e4aee18545b71bd3aaeb3385
    Reviewed-on: https://chromium-review.googlesource.com/646428
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499198}

--- chrome/android/java/src/org/chromium/chrome/browser/infobar/IPHInfoBarSupport.java
+++ chrome/android/java/src/org/chromium/chrome/browser/infobar/IPHInfoBarSupport.java
@@ -138,6 +138,9 @@ class IPHInfoBarSupport implements OnDismissListener, InfoBarContainer.InfoBarAn
     @Override
     public void onInfoBarContainerAttachedToWindow(boolean hasInfobars) {}
 
+    @Override
+    public void onInfoBarContainerShownRatioChanged(InfoBarContainer container, float shownRatio) {}
+
     // PopupWindow.OnDismissListener implementation.
     @Override
     public void onDismiss() {
--- chrome/android/javatests/src/org/chromium/chrome/browser/dom_distiller/DistillabilityServiceTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/dom_distiller/DistillabilityServiceTest.java
@@ -79,6 +79,10 @@ public class DistillabilityServiceTest {
 
             @Override
             public void onInfoBarContainerAttachedToWindow(boolean hasInfobars) {}
+
+            @Override
+            public void onInfoBarContainerShownRatioChanged(
+                    InfoBarContainer container, float shownRatio) {}
         });
 
         TestWebContentsObserver observer = new TestWebContentsObserver(
