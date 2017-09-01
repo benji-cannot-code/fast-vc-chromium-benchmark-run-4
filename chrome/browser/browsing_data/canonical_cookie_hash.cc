b31bf5cd010f8570994d6b35c9e90991cbb1b0e5
Brett Wilson <brettw@chromium.org>
2017-09-01 20:23:53 +0000

    Make base::Hash take a void*.
    Previously it took a const char* which caused many annoying reinterpret_cast
    calls. The reinterpret_casts in calls to this function have been removed.
    
    Renamed base::SuperFastHash to base::PersistentHash to make its use more clear
    and added comments about the split between it and base::Hash.
    
    Converts some calls to use the std::string variant rather than manually get the
    data and length out. De-inlines the string variants since it doesn't seem
    like this needs to be inline. Added a 16-bit variant.
    
    Minor tweak to Backtrace object for C++11.
    
    Change-Id: Ia883e9324ca129fbee2b22ec8ab43d05040bb4fe
    Reviewed-on: https://chromium-review.googlesource.com/646758
    Reviewed-by: Matt Giuca <mgiuca@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499276}

--- chrome/browser/browsing_data/canonical_cookie_hash.cc
+++ chrome/browser/browsing_data/canonical_cookie_hash.cc
@@ -10,10 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace canonical_cookie {
 
 size_t FastHash(const net::CanonicalCookie& cookie) {
-  return base::SuperFastHash(cookie.Name().c_str(), cookie.Name().size()) +
-         3 * base::SuperFastHash(cookie.Domain().c_str(),
-                                 cookie.Domain().size()) +
-         7 * base::SuperFastHash(cookie.Path().c_str(), cookie.Path().size());
+  return base::PersistentHash(cookie.Name()) +
+         3 * base::PersistentHash(cookie.Domain()) +
+         7 * base::PersistentHash(cookie.Path());
 }
 
-};  // namespace canonical_cookie
+}  // namespace canonical_cookie
--- testing/libfuzzer/fuzzers/hash_fuzzer.cc
+++ testing/libfuzzer/fuzzers/hash_fuzzer.cc
@@ -10,6 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Entry point for LibFuzzer.
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  base::Hash(reinterpret_cast<const char*>(data), size);
+  base::Hash(data, size);
   return 0;
 }
