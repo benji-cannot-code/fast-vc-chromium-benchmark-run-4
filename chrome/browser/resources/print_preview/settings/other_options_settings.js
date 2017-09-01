a4b7269a5e896540bd688325c421ee6a0b1d4470
rbpotter <rbpotter@chromium.org>
2017-09-01 01:05:55 +0000

    Print Preview: Move fit to page checkbox into scaling section
    Per UX advice, move fit to page checkbox into scaling section since
    the two controls are related. Change behavior so that fit to page is
    immediately unchecked when scaling field is modified. Also disable
    print button if an invalid scaling value is currently displayed.
    
    Bug: None
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I2b31cb1872a59effe433a19a8b526f6824328141
    Reviewed-on: https://chromium-review.googlesource.com/639910
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499092}

--- chrome/browser/resources/print_preview/settings/other_options_settings.js
+++ chrome/browser/resources/print_preview/settings/other_options_settings.js
@@ -121,8 +121,6 @@ cr.define('print_preview', function() {
   /**
    * UI component that renders checkboxes for various print options.
    * @param {!print_preview.ticket_items.Duplex} duplex Duplex ticket item.
-   * @param {!print_preview.ticket_items.FitToPage} fitToPage Fit-to-page ticket
-   *     item.
    * @param {!print_preview.ticket_items.CssBackground} cssBackground CSS
    *     background ticket item.
    * @param {!print_preview.ticket_items.SelectionOnly} selectionOnly Selection
@@ -135,8 +133,7 @@ cr.define('print_preview', function() {
    * @extends {print_preview.SettingsSection}
    */
   function OtherOptionsSettings(
-      duplex, fitToPage, cssBackground, selectionOnly, headerFooter,
-      rasterize) {
+      duplex, cssBackground, selectionOnly, headerFooter, rasterize) {
     print_preview.SettingsSection.call(this);
     /**
      * @private {boolean} rasterizeEnabled Whether the print as image feature is
@@ -153,7 +150,6 @@ cr.define('print_preview', function() {
     this.elements_ = [
       new CheckboxTicketItemElement(
           headerFooter, true, 'header-footer-container'),
-      new CheckboxTicketItemElement(fitToPage, false, 'fit-to-page-container'),
       new CheckboxTicketItemElement(duplex, false, 'duplex-container'),
       new CheckboxTicketItemElement(
           cssBackground, true, 'css-background-container'),
@@ -162,7 +158,7 @@ cr.define('print_preview', function() {
     ];
     if (this.rasterizeEnabled_) {
       this.elements_.splice(
-          4, 0,
+          this.elements_.length - 1, 0,
           new CheckboxTicketItemElement(
               rasterize, true, 'rasterize-container'));
     }
