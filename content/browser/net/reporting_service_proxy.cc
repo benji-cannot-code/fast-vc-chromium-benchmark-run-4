0db20dd733ff92b9cca0fa7c1632eecbc2c3a129
Jeremy Roman <jbroman@chromium.org>
2017-09-01 19:12:58 +0000

    Remove unnecessary unwrap/wrap of unique_ptr in ReportingServiceProxyImpl.
    Applying WrapUnique to a just-released pointer is a no-op. The conversion to
    unique_ptr<const T> is legal (and done here); it might as well be done at the
    point where it is used.
    
    Change-Id: I016d8298330796f76647df4cc1956f19cef8f705
    Reviewed-on: https://chromium-review.googlesource.com/646757
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499250}

--- content/browser/net/reporting_service_proxy.cc
+++ content/browser/net/reporting_service_proxy.cc
@@ -37,9 +37,6 @@ class ReportingServiceProxyImpl : public mojom::ReportingServiceProxy {
                    const std::string& group,
                    const std::string& type,
                    std::unique_ptr<base::Value> body) override {
-    std::unique_ptr<const base::Value> const_body =
-        base::WrapUnique(body.release());
-
     net::URLRequestContext* request_context =
         request_context_getter_->GetURLRequestContext();
     if (!request_context) {
@@ -54,7 +51,7 @@ class ReportingServiceProxyImpl : public mojom::ReportingServiceProxy {
       return;
     }
 
-    reporting_service->QueueReport(url, group, type, std::move(const_body));
+    reporting_service->QueueReport(url, group, type, std::move(body));
   }
 
  private:
