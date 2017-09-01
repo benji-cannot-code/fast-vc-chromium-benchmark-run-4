7273c2b63b06e298b937fd9f5d7fc463f5220a67
Sonny Sasaka <sonnysasaka@chromium.org>
2017-09-01 01:37:34 +0000

    Revert bluetooth settings page changes of crrev.com/55f69fc
    For now we don't want to tie the toggle button with pref value, but tie
    it with the actual bluetooth power state. The next CL will add the
    preference saving when bluetooth power toggle is completed.
    
    BUG=741962
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I694da134690241b7953f66ccebc9e6a34451a03a
    Reviewed-on: https://chromium-review.googlesource.com/646756
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Sonny Sasaka <sonnysasaka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499105}

--- chrome/browser/resources/settings/bluetooth_page/compiled_resources2.gyp
+++ chrome/browser/resources/settings/bluetooth_page/compiled_resources2.gyp
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'target_name': 'bluetooth_page',
       'dependencies': [
         '../compiled_resources2.gyp:route',
-        '../prefs/compiled_resources2.gyp:prefs_behavior',
         '../settings_page/compiled_resources2.gyp:settings_animated_pages',
         '<(DEPTH)/ui/webui/resources/js/compiled_resources2.gyp:assert',
         '<(DEPTH)/ui/webui/resources/js/compiled_resources2.gyp:i18n_behavior',
@@ -23,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'target_name': 'bluetooth_subpage',
       'dependencies': [
         '../compiled_resources2.gyp:route',
-        '../prefs/compiled_resources2.gyp:prefs_behavior',
         '<(DEPTH)/ui/webui/resources/cr_elements/compiled_resources2.gyp:cr_scrollable_behavior',
         '<(DEPTH)/ui/webui/resources/js/compiled_resources2.gyp:assert',
         '<(DEPTH)/ui/webui/resources/js/compiled_resources2.gyp:i18n_behavior',
--- chrome/test/data/webui/settings/bluetooth_page_tests.js
+++ chrome/test/data/webui/settings/bluetooth_page_tests.js
@@ -3,22 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-function getFakePrefs() {
-  return {
-    ash: {
-      user: {
-        bluetooth: {
-          adapter_enabled: {
-            key: 'ash.user.bluetooth.adapter_enabled',
-            type: chrome.settingsPrivate.PrefType.BOOLEAN,
-            value: false,
-          }
-        }
-      }
-    }
-  };
-}
-
 suite('Bluetooth', function() {
   var bluetoothPage = null;
 
@@ -77,7 +61,6 @@ suite('Bluetooth', function() {
   setup(function() {
     PolymerTest.clearBody();
     bluetoothPage = document.createElement('settings-bluetooth-page');
-    bluetoothPage.prefs = getFakePrefs();
     assertTrue(!!bluetoothPage);
 
     bluetoothApi_.setDevicesForTest([]);
@@ -91,54 +74,43 @@ suite('Bluetooth', function() {
 
   test('MainPage', function() {
     assertFalse(bluetoothApi_.getAdapterStateForTest().powered);
-    assertFalse(bluetoothPage.prefs.ash.user.bluetooth.adapter_enabled.value);
+    assertFalse(bluetoothPage.bluetoothToggleState_);
     // Test that tapping the single settings-box div enables bluetooth.
     var div = bluetoothPage.$$('div.settings-box');
     assertTrue(!!div);
     MockInteractions.tap(div);
-    assertTrue(bluetoothPage.prefs.ash.user.bluetooth.adapter_enabled.value);
+    assertTrue(bluetoothPage.bluetoothToggleState_);
+    assertTrue(bluetoothApi_.getAdapterStateForTest().powered);
   });
 
   suite('SubPage', function() {
     var subpage;
 
     setup(function() {
-      assertFalse(bluetoothApi_.getAdapterStateForTest().powered);
-      assertFalse(bluetoothPage.prefs.ash.user.bluetooth.adapter_enabled.value);
-      var div = bluetoothPage.$$('div.settings-box');
-
-      // First tap will turn on bluetooth.
-      MockInteractions.tap(div);
-      assertTrue(bluetoothPage.prefs.ash.user.bluetooth.adapter_enabled.value);
-      bluetoothPage.adapterState_.powered = true;
-      // Second tap will open bluetooth subpage.
-      MockInteractions.tap(div);
-
+      bluetoothApi_.setEnabled(true);
       Polymer.dom.flush();
+      var div = bluetoothPage.$$('div.settings-box');
+      MockInteractions.tap(div);
       subpage = bluetoothPage.$$('settings-bluetooth-subpage');
       assertTrue(!!subpage);
+      assertTrue(subpage.bluetoothToggleState);
+      assertFalse(subpage.bluetoothToggleDisabled);
     });
 
-    // Skipping all SubPage tests as this is currently flaky.
-    // TODO(sonnysasaka): re-enable these tests once the flakiness is fixed
-    // (http://crbug.com/756283).
-    test.skip('toggle', function() {
-      assertTrue(bluetoothPage.prefs.ash.user.bluetooth.adapter_enabled.value);
+    test('toggle', function() {
+      assertTrue(subpage.bluetoothToggleState);
 
       var enableButton = subpage.$.enableBluetooth;
       assertTrue(!!enableButton);
       assertTrue(enableButton.checked);
 
-      bluetoothPage.setPrefValue('ash.user.bluetooth.adapter_enabled', false);
-
+      subpage.bluetoothToggleState = false;
       assertFalse(enableButton.checked);
       assertFalse(bluetoothApi_.getAdapterStateForTest().powered);
-      assertFalse(bluetoothPage.prefs.ash.user.bluetooth.adapter_enabled.value);
+      assertFalse(bluetoothPage.bluetoothToggleState_);
     });
 
-    test.skip('paired device list', function() {
-      assertTrue(subpage.adapterState.powered);
-
+    test('paired device list', function() {
       var pairedContainer = subpage.$.pairedContainer;
       assertTrue(!!pairedContainer);
       assertTrue(pairedContainer.hidden);
@@ -160,9 +132,7 @@ suite('Bluetooth', function() {
       assertFalse(devices[1].device.connected);
     });
 
-    test.skip('unpaired device list', function() {
-      assertTrue(subpage.adapterState.powered);
-
+    test('unpaired device list', function() {
       var unpairedContainer = subpage.$.unpairedContainer;
       assertTrue(!!unpairedContainer);
       assertTrue(unpairedContainer.hidden);
@@ -184,9 +154,7 @@ suite('Bluetooth', function() {
       assertFalse(devices[1].device.paired);
     });
 
-    test.skip('pair device', function(done) {
-      assertTrue(subpage.adapterState.powered);
-
+    test('pair device', function(done) {
       bluetoothApi_.setDevicesForTest(fakeDevices_);
       Polymer.dom.flush();
       assertEquals(4, subpage.deviceList_.length);
@@ -202,9 +170,7 @@ suite('Bluetooth', function() {
       });
     });
 
-    test.skip('pair dialog', function() {
-      assertTrue(subpage.adapterState.powered);
-
+    test('pair dialog', function() {
       bluetoothApi_.setDevicesForTest(fakeDevices_);
       Polymer.dom.flush();
       var dialog = subpage.$.deviceDialog;
