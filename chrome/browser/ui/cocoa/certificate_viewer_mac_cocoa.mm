63f3a1dfd67a2117c255b32bcccccd934bb9d86e
Nico Weber <thakis@chromium.org>
2017-09-01 04:56:28 +0000

    mac: Fix some -Wdelete-non-virtual-dtor warnings.
    TBR=hans
    
    Bug: 673171
    Change-Id: I45c85782f3559456acaddfeeac4d6e3cc6fee818
    Reviewed-on: https://chromium-review.googlesource.com/647166
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499128}

--- chrome/browser/ui/cocoa/certificate_viewer_mac_cocoa.mm
+++ chrome/browser/ui/cocoa/certificate_viewer_mac_cocoa.mm
@@ -19,7 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-class SSLCertificateViewerCocoaBridge : public ConstrainedWindowMacDelegate {
+class SSLCertificateViewerCocoaBridge final
+    : public ConstrainedWindowMacDelegate {
  public:
   explicit SSLCertificateViewerCocoaBridge(
       SSLCertificateViewerCocoa* controller)
