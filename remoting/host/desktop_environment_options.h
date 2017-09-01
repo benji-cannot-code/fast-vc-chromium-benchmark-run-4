b5f4da469cf8d9bce334827ceb0e1a5cc5f1734d
Joseph Arhar <jarhar@google.com>
2017-09-01 04:57:30 +0000

    Move file transfer capability control to DesktopEnvironmentOptions
    Change-Id: I779bd33a4751b434072da7b17901694643f67d3b
    Reviewed-on: https://chromium-review.googlesource.com/646387
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Joseph Arhar <jarhar@google.com>
    Cr-Commit-Position: refs/heads/master@{#499129}

--- remoting/host/desktop_environment_options.h
+++ remoting/host/desktop_environment_options.h
@@ -37,6 +37,9 @@ class DesktopEnvironmentOptions final {
   bool enable_user_interface() const;
   void set_enable_user_interface(bool enabled);
 
+  bool enable_file_transfer() const;
+  void set_enable_file_transfer(bool enabled);
+
   const webrtc::DesktopCaptureOptions* desktop_capture_options() const;
   webrtc::DesktopCaptureOptions* desktop_capture_options();
 
@@ -53,6 +56,9 @@ class DesktopEnvironmentOptions final {
 
   // True if a user-interactive window is showing up in it2me scenario.
   bool enable_user_interface_ = true;
+
+  // True if this host has file transfer enabled.
+  bool enable_file_transfer_ = false;
 
   // The DesktopCaptureOptions to initialize DesktopCapturer.
   webrtc::DesktopCaptureOptions desktop_capture_options_;
