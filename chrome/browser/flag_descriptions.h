4673f5b4638e2548fa9f30d54df94affdf279a29
Chung-Sheng Wu <chungsheng@google.com>
2017-09-01 14:41:57 +0000

    [CrOS] Show system information via Chrome Internal Page
    Shows information like CPU usage, Memory status, Zram status.
    
    This patch adds the implementation for the Internal Page backend.
    Get system information via Chrome base api, and pass the data to
    frontend by calling Javascript function.
    
    Bug: 752002
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I76c863176270ee61268d22c880b74dafb0958b10
    Reviewed-on: https://chromium-review.googlesource.com/625499
    Commit-Queue: Zhong-sheng Wu <chungsheng@google.com>
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499191}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -766,6 +766,9 @@ extern const char kSyncAppListDescription[];
 extern const char kSyncSandboxName[];
 extern const char kSyncSandboxDescription[];
 
+extern const char kSysInternalsName[];
+extern const char kSysInternalsDescription[];
+
 extern const char kTabAudioMutingName[];
 extern const char kTabAudioMutingDescription[];
 
