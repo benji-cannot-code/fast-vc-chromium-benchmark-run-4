2d1304d84e9babf8033350cc81d384cdad8012c5
Gauthier Ambard <gambard@chromium.org>
2017-09-01 09:00:35 +0000

    Test tapping promo
    Add a test to the NTP to tap on the promo displayed and ensure that
    the promo is correctly removed.
    
    Bug: 761004, 740894
    Change-Id: I45ac067d5b6e1ed7e74336f4b04c35e88b12951d
    Reviewed-on: https://chromium-review.googlesource.com/645955
    Reviewed-by: Elodie Banel <lod@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499155}

--- ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_whats_new_item.h
+++ ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_whats_new_item.h
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Text describing what is new.
 @property(nonatomic, copy, nullable) NSString* text;
 
++ (nonnull NSString*)accessibilityIdentifier;
+
 @end
 
 // Associated cell, displaying what is new.
