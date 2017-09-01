473e24feea81895454a5530b1c70f93fe713dc1a
Mihai Sardarescu <msarda@chromium.org>
2017-09-01 08:58:41 +0000

    Remove signin-dice-internals
    This CL is a follow-up for http://crrev/c/586929 that folded the DICE
    internals in signin-internals.
    
    Bug: 733226
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I4c8bf37a60b972b04ed3b2c76381ea2ee0afdbc3
    Reviewed-on: https://chromium-review.googlesource.com/641460
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499154}

--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -194,10 +194,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/app_list/start_page_ui.h"
 #endif
 
-#if BUILDFLAG(ENABLE_DICE_SUPPORT)
-#include "chrome/browser/ui/webui/signin/signin_dice_internals_ui.h"
-#endif
-
 #if BUILDFLAG(ENABLE_EXTENSIONS)
 #include "chrome/browser/extensions/extension_web_ui.h"
 #include "chrome/browser/ui/webui/extensions/extensions_ui.h"
@@ -552,12 +548,6 @@ WebUIFactoryFunction GetWebUIFactoryFunction(WebUI* web_ui,
 #if BUILDFLAG(ENABLE_APP_LIST)
   if (url.host_piece() == chrome::kChromeUIAppListStartPageHost)
     return &NewWebUI<app_list::StartPageUI>;
-#endif
-#if BUILDFLAG(ENABLE_DICE_SUPPORT)
-  if (url.host() == chrome::kChromeUISigninDiceInternalsHost &&
-      !profile->IsOffTheRecord() && signin::IsAccountConsistencyDiceEnabled()) {
-    return &NewWebUI<SigninDiceInternalsUI>;
-  }
 #endif
 #if BUILDFLAG(ENABLE_EXTENSIONS)
   if (url.host_piece() == chrome::kChromeUIExtensionsFrameHost)
