7d307e44b92d8efba0ea8df84d45795ca77a23ca
Biao She <bshe@chromium.org>
2017-09-01 13:07:14 +0000

    Use computed_opacity to render ui elements
    computed_opacity has opacity inheriated from parent. We should use it instead
    of the element's opacity.
    
    Bug: 761013
    Change-Id: I24ed910f82d53a41d9b184a453a0d60d6d5ccb08
    Reviewed-on: https://chromium-review.googlesource.com/646506
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499176}

---
+++ chrome/browser/vr/test/fake_ui_element_renderer.cc
@@ -0,0 +1,46 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/vr/test/fake_ui_element_renderer.h"
+
+#include "ui/gfx/geometry/size_f.h"
+
+namespace vr {
+
+FakeUiElementRenderer::FakeUiElementRenderer() {}
+FakeUiElementRenderer::~FakeUiElementRenderer() {}
+
+void FakeUiElementRenderer::DrawTexturedQuad(
+    int texture_data_handle,
+    TextureLocation texture_location,
+    const gfx::Transform& view_proj_matrix,
+    const gfx::RectF& copy_rect,
+    float opacity,
+    gfx::SizeF element_size,
+    float corner_radius) {
+  texture_opacity_ = opacity;
+}
+
+void FakeUiElementRenderer::DrawGradientQuad(
+    const gfx::Transform& view_proj_matrix,
+    const SkColor edge_color,
+    const SkColor center_color,
+    float opacity,
+    gfx::SizeF element_size,
+    float corner_radius) {
+  gradient_opacity_ = opacity;
+}
+
+void FakeUiElementRenderer::DrawGradientGridQuad(
+    const gfx::Transform& view_proj_matrix,
+    const SkColor edge_color,
+    const SkColor center_color,
+    const SkColor grid_color,
+    int gridline_count,
+    float opacity) {
+  grid_opacity_ = opacity;
+}
+
+}  // namespace vr
