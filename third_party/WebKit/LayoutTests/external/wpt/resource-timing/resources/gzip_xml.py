0eda0208df3b9e446a5551a4a3eb831791b767eb
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2017-09-01 11:25:50 +0000

    Import wpt@44aca332f60f2ccbb60f10e77f05fb0a193ef637
    Using wpt-import in Chromium 07c14f0f3ff915af330aef3092420ece3430d266.
    
    Build: https://build.chromium.org/p/chromium.infra.cron/builders/wpt-importer/builds/1025
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    blink-network-dev@chromium.org:
      external/wpt/XMLHttpRequest
    dom-dev@chromium.org:
      external/wpt/html
    drott@chromium.org, kojii@chromium.org:
      external/wpt/css/css-fonts-3
    editing-dev@chromium.org:
      external/wpt/selection
    hta@chromium.org:
      external/wpt/webrtc
    ksakamoto@chromium.org:
      external/wpt/resource-timing
    meade@chromium.org:
      external/wpt/web-animations
    michaeln@chromium.org, jsbell@chromium.org:
      external/wpt/webstorage
    paint-dev@chromium.org:
      external/wpt/2dcontext
    rego@igalia.com:
      external/wpt/css/css-grid-1
      external/wpt/css/css-ui-3
    style-dev@chromium.org:
      external/wpt/cssom
      external/wpt/cssom-view
    tyoshino@chromium.org, yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    TBR=qyearsley@chromium.org
    
    No-Export: true
    Change-Id: I2b0aefec5881a64a05cbf323dc294b1417c81d50
    Reviewed-on: https://chromium-review.googlesource.com/646454
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499169}

--- third_party/WebKit/LayoutTests/external/wpt/resource-timing/resources/gzip_xml.py
+++ third_party/WebKit/LayoutTests/external/wpt/resource-timing/resources/gzip_xml.py
@@ -1,9 +1,12 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import gzip as gzip_module
 from cStringIO import StringIO
+import os
 
 def main(request, response):
-    f = open('resource-timing/resources/resource_timing_test0.xml', 'r')
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    file_path = os.path.join(dir_path, 'resource_timing_test0.xml')
+    f = open(file_path, 'r')
     output = f.read()
 
     out = StringIO()
