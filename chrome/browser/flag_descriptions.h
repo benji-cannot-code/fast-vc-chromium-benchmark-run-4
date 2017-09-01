c24a41c678cbf66c1ce7f8dc9c483cd58aabd31c
Klemen Kozjek <klemenko@google.com>
2017-09-01 05:04:51 +0000

    Files App: Add a flag for rename feature to flag descriptions
    This CL adds a flag that enables external drive rename action in the
    context menu. The flag is added to flag descriptions and as a result, it
    is visible under chrome://flags.
    
    BUG=274041
    
    Change-Id: Ia83b12daab124b0f664699b432d48c6138410678
    Reviewed-on: https://chromium-review.googlesource.com/644667
    Reviewed-by: Naoki Fukino <fukino@chromium.org>
    Commit-Queue: Klemen Kozjek <klemenko@google.com>
    Cr-Commit-Position: refs/heads/master@{#499134}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1410,6 +1410,9 @@ extern const char kEnableEhvInputDescription[];
 extern const char kEnableEncryptionMigrationName[];
 extern const char kEnableEncryptionMigrationDescription[];
 
+extern const char kEnableExternalDriveRename[];
+extern const char kEnableExternalDriveRenameDescription[];
+
 extern const char kEnableImeMenuName[];
 extern const char kEnableImeMenuDescription[];
 
