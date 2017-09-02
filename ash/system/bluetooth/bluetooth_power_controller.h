ce1df4c02f606a916001ad2bdac82568d1bcb28d
Sonny Sasaka <sonnysasaka@chromium.org>
2017-09-02 00:06:53 +0000

    BluetoothPowerController use std::queue instead of base::queue
    This patch needs to be merged to M61 but base::queue is not available in M61.
    
    BUG=741962
    TBR=jamescook
    
    Change-Id: I58f381f6ced93f83a2b91fe086729c87686e6cff
    Reviewed-on: https://chromium-review.googlesource.com/648511
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Sonny Sasaka <sonnysasaka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499352}

--- ash/system/bluetooth/bluetooth_power_controller.h
+++ ash/system/bluetooth/bluetooth_power_controller.h
@@ -9,12 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "ash/session/session_observer.h"
 #include "ash/shell_observer.h"
-#include "base/containers/queue.h"
 #include "base/logging.h"
 #include "base/macros.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "components/user_manager/user_manager.h"
 #include "device/bluetooth/bluetooth_adapter.h"
+
+#include <queue>
 
 class PrefRegistrySimple;
 class PrefService;
@@ -129,7 +130,7 @@ class ASH_EXPORT BluetoothPowerController
 
   // Contains pending tasks which depend on the availability of bluetooth
   // adapter.
-  base::queue<BluetoothTask> pending_bluetooth_tasks_;
+  std::queue<BluetoothTask> pending_bluetooth_tasks_;
 
   // The registrar used to watch prefs changes in the above
   // |active_user_pref_service_| from outside ash.
