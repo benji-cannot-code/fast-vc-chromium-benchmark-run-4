1affa36eefcc9b2aa824a1f4a16664d82eeb82fb
Shakti Sahu <shaktisahu@chromium.org>
2017-09-02 01:00:58 +0000

    Revert "Download Home : Using startService to perform user-initiated action"
    This reverts commit 2054f23825cab99f1b17ed0f3ca889ea3ac03b1d.
    
    Bug: 761562
    Change-Id: Idf037b5fbc1b9f61935c3f91a2472a68b78d7028
    Reviewed-on: https://chromium-review.googlesource.com/648229
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499364}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadManagerService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadManagerService.java
@@ -1373,7 +1373,7 @@ public class DownloadManagerService
         Intent intent = DownloadNotificationService.buildActionIntent(mContext, action,
                 LegacyHelpers.buildLegacyContentId(false, downloadItem.getId()),
                 downloadItem.getDownloadInfo().isOffTheRecord());
-        mContext.startService(intent);
+        mContext.sendBroadcast(intent);
     }
 
     /**
