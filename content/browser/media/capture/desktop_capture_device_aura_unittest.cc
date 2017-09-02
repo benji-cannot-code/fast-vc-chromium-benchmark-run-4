b0fa3315292995a075314ad240b1d4566852ba0b
Yuri Wiitala <miu@chromium.org>
2017-09-02 22:13:07 +0000

    Repair unit test broken by recent AuraWindowCaptureMachine crash fix.
    The recent crash fix broke a unit test that runs only on CrOS. The
    breakage was simply because the unit test never created an
    ImageTranportFactory instance. This change rectifies that.
    
    TBR=amp@chromium.org,wez@chromium.org
    
    Bug: 760251
    Change-Id: Ib1e4e72918bdda6b5e0d202507a28e6563239be7
    Reviewed-on: https://chromium-review.googlesource.com/647493
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Commit-Queue: Yuri Wiitala <miu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499404}

--- content/browser/media/capture/desktop_capture_device_aura_unittest.cc
+++ content/browser/media/capture/desktop_capture_device_aura_unittest.cc
@@ -11,7 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/location.h"
 #include "base/macros.h"
+#include "base/run_loop.h"
 #include "base/synchronization/waitable_event.h"
+#include "content/browser/compositor/test/no_transport_image_transport_factory.h"
 #include "content/public/browser/desktop_media_id.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "media/capture/video_capture_types.h"
@@ -21,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/test/aura_test_helper.h"
 #include "ui/aura/test/test_window_delegate.h"
 #include "ui/aura/window.h"
-#include "ui/compositor/test/context_factories_for_test.h"
 #include "ui/wm/core/default_activation_client.h"
 
 using ::testing::_;
@@ -106,14 +107,12 @@ class DesktopCaptureDeviceAuraTest : public testing::Test {
  protected:
   void SetUp() override {
     // The ContextFactory must exist before any Compositors are created.
-
-    bool enable_pixel_output = false;
-    ui::ContextFactory* context_factory = nullptr;
-    ui::ContextFactoryPrivate* context_factory_private = nullptr;
-    ui::InitializeContextFactoryForTests(enable_pixel_output, &context_factory,
-                                         &context_factory_private);
+    ImageTransportFactory::InitializeForUnitTests(
+        std::make_unique<NoTransportImageTransportFactory>());
     helper_.reset(new aura::test::AuraTestHelper());
-    helper_->SetUp(context_factory, context_factory_private);
+    helper_->SetUp(
+        ImageTransportFactory::GetInstance()->GetContextFactory(),
+        ImageTransportFactory::GetInstance()->GetContextFactoryPrivate());
     new wm::DefaultActivationClient(helper_->root_window());
     // We need a window to cover desktop area so that DesktopCaptureDeviceAura
     // can use gfx::NativeWindow::GetWindowAtScreenPoint() to locate the
@@ -134,7 +133,8 @@ class DesktopCaptureDeviceAuraTest : public testing::Test {
     desktop_window_.reset();
     window_delegate_.reset();
     helper_->TearDown();
-    ui::TerminateContextFactoryForTests();
+    base::RunLoop().RunUntilIdle();
+    ImageTransportFactory::Terminate();
   }
 
   aura::Window* root_window() { return helper_->root_window(); }
