8de13241ab79bdfb04f9d63eb8bbf87248104274
Yuri Wiitala <miu@chromium.org>
2017-09-02 00:11:34 +0000

    Crash fix for AuraWindowCaptureMachine/ReadbackYUV
    Adds missing ContextFactoryObserver::OnLostResources() implementation
    that is required for when GLHelper is destroyed/re-created because the
    current ReadbackYUVPipeline instance is no longer valid.
    
    TBR=wez@chromium.org
    
    Bug: 760251
    Change-Id: I85f54b60b32501aa3e08e1dcca7891ce739a913a
    Reviewed-on: https://chromium-review.googlesource.com/647409
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499354}

--- content/browser/media/capture/aura_window_capture_machine.h
+++ content/browser/media/capture/aura_window_capture_machine.h
@@ -26,10 +26,10 @@ class ReadbackYUVInterface;
 
 namespace content {
 
-class AuraWindowCaptureMachine
-    : public media::VideoCaptureMachine,
-      public aura::WindowObserver,
-      public ui::CompositorAnimationObserver {
+class AuraWindowCaptureMachine : public media::VideoCaptureMachine,
+                                 public aura::WindowObserver,
+                                 public ui::ContextFactoryObserver,
+                                 public ui::CompositorAnimationObserver {
  public:
   AuraWindowCaptureMachine();
   ~AuraWindowCaptureMachine() override;
@@ -92,6 +92,9 @@ class AuraWindowCaptureMachine
                                  base::TimeTicks event_time,
                                  const CaptureFrameCallback& capture_frame_cb,
                                  std::unique_ptr<viz::CopyOutputResult> result);
+
+  // ui::ContextFactoryObserver implementation.
+  void OnLostResources() override;
 
   // Renders the cursor if needed and then delivers the captured frame.
   static void CopyOutputFinishedForVideo(
