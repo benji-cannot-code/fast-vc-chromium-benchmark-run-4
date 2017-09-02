2cfb3ce08372248c116db10c90334055663b6de7
hkamila <hkamila@google.com>
2017-09-02 00:07:00 +0000

    Added ThreatDetails to chrome://safe-browsing (AutoRefresh)
    Added ThreatDetails fields in the chrome://safe-browsing page.
    When a new ping is sent in ThreatDetails class, the data will
    automatically be shown on the WebUI tabs that are opened. If no WebUI 
    tabs are opened, no data will be recorded.
    
    Bug: 734667
    Change-Id: I9bd3ec7bbd50e0b515abd9aaa2866b6f2abf29e2
    Reviewed-on: https://chromium-review.googlesource.com/621273
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Commit-Queue: Kamila Hasanbega <hkamila@google.com>
    Cr-Commit-Position: refs/heads/master@{#499353}

--- components/safe_browsing/browser/threat_details.cc
+++ components/safe_browsing/browser/threat_details.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/browser/threat_details_cache.h"
 #include "components/safe_browsing/browser/threat_details_history.h"
 #include "components/safe_browsing/common/safebrowsing_messages.h"
+#include "components/safe_browsing/web_ui/safe_browsing_ui.h"
 #include "components/safe_browsing_db/hit_report.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_controller.h"
@@ -743,6 +744,12 @@ void ThreatDetails::OnCacheCollectionReady() {
     DLOG(ERROR) << "Unable to serialize the threat report.";
     return;
   }
+
+  BrowserThread::PostTask(
+      content::BrowserThread::UI, FROM_HERE,
+      base::Bind(&WebUIInfoSingleton::AddToReportsSent,
+                 base::Unretained(WebUIInfoSingleton::GetInstance()),
+                 base::Passed(&report_)));
   ui_manager_->SendSerializedThreatDetails(serialized);
 }
 
