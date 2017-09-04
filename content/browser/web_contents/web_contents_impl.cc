ffb23ca0f927d83e5625c64332ad573872b7bba4
Greg Thompson <grt@chromium.org>
2017-09-04 10:25:52 +0000

    Revert "<webview> BrowserPlugin: Ensure embedder is focused for guest"
    This reverts commit 587ccb63cc08aca1636bb98aa26a90ce67fc9241.
    
    Reason for revert: WebViewBrowserPluginInteractiveTest.EnsureFocusSynced timing out on Linux ASan LSan Tests (1); e.g., https://build.chromium.org/p/chromium.memory/builders/Linux%20ASan%20LSan%20Tests%20%281%29/builds/38473.
    
    
    Original change's description:
    > <webview> BrowserPlugin: Ensure embedder is focused for guest
    > 
    > There is a problem when the WebContents for a BrowserPlugin guest is
    > focused. The embedder expects to be focused and drives the focus state 
    > of its guests. This notifies the embedder when one of its guests becomes
    > focused in order to correctly set state.
    > 
    > BUG=754890
    > 
    > Change-Id: I259aed621094c6c9fabf79411771d8229de141f6
    > Reviewed-on: https://chromium-review.googlesource.com/635816
    > Commit-Queue: Alex Vallee <avallee@chromium.org>
    > Reviewed-by: James MacLean <wjmaclean@chromium.org>
    > Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#499361}
    
    TBR=avallee@chromium.org,alexmos@chromium.org,lfg@chromium.org,wjmaclean@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 754890
    Change-Id: I8f1cca766620a51a40ba4172126b8191b240d6a3
    Reviewed-on: https://chromium-review.googlesource.com/647448
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499472}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -5241,13 +5241,8 @@ void WebContentsImpl::SetAsFocusedWebContentsIfNecessary() {
 
   GetOutermostWebContents()->node_.SetFocusedWebContents(this);
 
-  if (!GuestMode::IsCrossProcessFrameGuest(this) && browser_plugin_guest_) {
-    // A BrowserPlugin based-guest needs to focus its embedder to be told about
-    // focus state.
-    GetOuterWebContents()->GetMainFrame()->GetRenderWidgetHost()->SetPageFocus(
-        true);
+  if (!GuestMode::IsCrossProcessFrameGuest(this) && browser_plugin_guest_)
     return;
-  }
 
   // Send a page level blur to the old contents so that it displays inactive UI
   // and focus this contents to activate it.
