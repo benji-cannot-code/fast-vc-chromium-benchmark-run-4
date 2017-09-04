2a52b56b1062d6f3ade87b7513be8320d1c310c7
Olga Sharonova <olka@chromium.org>
2017-09-04 12:24:29 +0000

    Moving reusable AudioSystemImpl logic into AudioSystemHelper
    AudioSystemHelper will be reused by mojo implementation.
    AudioSystemImpl becomes a trampoline to run AudioSystemHelper on AudopManager thread.
    
    Threading and Trampoline:
    
    At the moment the main thread for AudioSystemImpl is AudioManager thread.
    When we have mojo version of AudioSystem, we can choose between:
    (a) IO thread as the main thread for AudioSystem implementation on top of Mojo interface.
    (b) Each client holds an instance of AudioSystem and accesses the underlying Mojo interface
    on whatever thread it likes.
    
    In both cases we don't want the current clients to be aware of AudioManager thread,
    since it would complicate transition to mojo. Automatically trampolining calls to
    AudioManager thread allows to hide it from the clients.
    
    If we go for option (b) with mojo, we can do a preliminary refactoring to convert
    the clients to owning individual AudioSystem(Impl) instances.
    Carefully though: AudioSystemImpl is basically stateless,
    wheres Mojo-based implementation is bound to the thread it's called on the first time. 
    
    TBR=dalecurtis@chromium.org
    
    Bug: 740943
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I6d1c80f1e9cf697198bd6f43a2bc47b232b69a7f
    Reviewed-on: https://chromium-review.googlesource.com/645810
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499486}

--- media/audio/audio_system.h
+++ media/audio/audio_system.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/media_export.h"
 
 namespace media {
-class AudioManager;
 
 // Provides asynchronous interface to access audio device information
 class MEDIA_EXPORT AudioSystem {
