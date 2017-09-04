57c0ef3dcd59c63413490184fde8ba7ea872f137
Finnur Thorarinsson <finnur@chromium.org>
2017-09-04 18:59:18 +0000

    First step towards being able to show Action Center notifications.
    This CL introduces a NotificationPlatformBridge for Windows that can
    display notifications in the Windows Action Center. Only the title, body
    text and attribution text of the notification are included for now.
    
    In addition, events for the notifications aren't being listened to yet
    either. Because of this I've made it possible to toggle the base::Feature
    for Windows, but have not amended the chrome://flags entry yet.
    
    Note: This is an evolution of https://codereview.chromium.org/2906133005/
    but with a notification_template_builder class replacing the
    notification_toast_builder.
    
    Note: The code in notification_platform_bridge_win.* that relates to the
    ComBase functions, is adapted from the WinRT Midi Manager and I plan to
    move them to base/win and share with that code.
    
    BUG=734095
    
    Change-Id: I331b84e6d72cdecf304364f47ee62efad320b5a1
    Reviewed-on: https://chromium-review.googlesource.com/625966
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499525}

--- chrome/browser/notifications/notification_template_builder_unittest.cc
+++ chrome/browser/notifications/notification_template_builder_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
+#include "base/strings/string16.h"
 #include "base/strings/utf_string_conversions.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/message_center/notification.h"
@@ -52,7 +53,7 @@ class NotificationTemplateBuilderTest : public ::testing::Test {
   // the given |notification_data|, and writes that to |*xml_template|. Calls
   // must be wrapped in ASSERT_NO_FATAL_FAILURE().
   void BuildTemplate(const NotificationData& notification_data,
-                     std::string* xml_template) {
+                     base::string16* xml_template) {
     GURL origin_url(notification_data.origin);
 
     Notification notification(
@@ -78,11 +79,12 @@ class NotificationTemplateBuilderTest : public ::testing::Test {
 
 TEST_F(NotificationTemplateBuilderTest, SimpleToast) {
   NotificationData notification_data;
-  std::string xml_template;
+  base::string16 xml_template;
 
   ASSERT_NO_FATAL_FAILURE(BuildTemplate(notification_data, &xml_template));
 
-  const char kExpectedXml[] = R"(<toast launch="notification_id">
+  const wchar_t kExpectedXml[] =
+      LR"(<toast launch="notification_id">
  <visual>
   <binding template="ToastText04">
    <text id="1">My Title</text>
