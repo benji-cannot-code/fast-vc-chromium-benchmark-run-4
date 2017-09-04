60455fc58f226e780818090fabca946c45a28204
Darren Shen <shend@chromium.org>
2017-09-04 04:17:55 +0000

    [CSSParser] Add offset information to CSSParserTokenStream.
    One of the benefits of streaming parser is that we can get token offset
    information (where a token starts in the input string) on the fly.
    This patch adds CSSParserTokenStream::Offset() which returns the index
    in the input string of the next token to be consumed. Naively, we would
    implement this by querying CSSTokenizerParserInputStream::Offset().
    However, CSSTokenizerInputStream::Offset() might be ahead of
    CSSParserTokenStream::Offset() because peeking a token would advance
    CSSTokenizerInputStream but not CSSParserTokenStream, so some extra
    logic is required to handle this.
    
    Note that this method is not used (except for tests) until later,
    but we put it in this patch to avoid large patches.
    
    Bug: 661854
    Change-Id: I67ceaeb4cb16345454b1ccf92c103401996fd298
    Reviewed-on: https://chromium-review.googlesource.com/615857
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Bugs Nash <bugsnash@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499434}

--- third_party/WebKit/Source/core/css/parser/CSSParserTokenStream.h
+++ third_party/WebKit/Source/core/css/parser/CSSParserTokenStream.h
@@ -134,6 +134,9 @@ class CORE_EXPORT CSSParserTokenStream {
     return Iterator(next_index_);
   }
 
+  // Get the index of the character in the original string to be consumed next.
+  size_t Offset() const { return offset_; }
+
   void ConsumeWhitespace();
   CSSParserToken ConsumeIncludingWhitespace();
   void UncheckedConsumeComponentValue(unsigned nesting_level = 0);
@@ -158,6 +161,7 @@ class CORE_EXPORT CSSParserTokenStream {
 
   const CSSParserToken& UncheckedConsumeInternal() {
     DCHECK(HasLookAhead());
+    offset_ = tokenizer_.input_.Offset();
     return tokenizer_.tokens_[next_index_++];
   }
 
@@ -165,6 +169,7 @@ class CORE_EXPORT CSSParserTokenStream {
 
   CSSTokenizer& tokenizer_;
   size_t next_index_;  // Index of next token to be consumed.
+  size_t offset_ = 0;
 };
 
 }  // namespace blink
