e0a0fee833869a6e49edae644266e02bcc447780
Filip Gorski <fgorski@chromium.org>
2017-09-04 07:20:00 +0000

    [Offline pages] Adding Execute method to OPMStore
    This patch:
    * Introduces OPMStoreSQL::Execute methods
    * Introduces OPMStoreSQL::InitializeInternal and
      OPMStoreSQL::OnIninializeInternal done.
    * Reimplements existing Add/Remove/Update/Delete/Initialize methods
      using Execute methods.
    * Makes a small update to test, and reuses existing tests
      to cover new functionality otherwise.
    
    Except for a small change in behavior of calling methods
    on uninitialized store and still having them work and adding
    Execute method that makes it happen, this patch can be considered
    a refactoring.
    
    Bug: 758690
    Change-Id: I4c6c25edb13c78d895f47e2f8948b4d9c314f99f
    Reviewed-on: https://chromium-review.googlesource.com/634044
    Commit-Queue: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Dmitry Titov <dimich@chromium.org>
    Reviewed-by: Yafei Duan <romax@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499449}

--- components/offline_pages/core/offline_page_metadata_store_unittest.cc
+++ components/offline_pages/core/offline_page_metadata_store_unittest.cc
@@ -444,8 +444,7 @@ class OfflinePageMetadataStoreTest : public testing::Test {
   void PumpLoop();
 
   void InitializeCallback(bool success);
-  void GetOfflinePagesCallback(
-      const std::vector<OfflinePageItem>& offline_pages);
+  void GetOfflinePagesCallback(std::vector<OfflinePageItem> offline_pages);
   void AddCallback(ItemActionStatus status);
   void UpdateCallback(CalledCallback called_callback,
                       std::unique_ptr<OfflinePagesUpdateResult> result);
@@ -492,9 +491,9 @@ void OfflinePageMetadataStoreTest::InitializeCallback(bool success) {
 }
 
 void OfflinePageMetadataStoreTest::GetOfflinePagesCallback(
-    const std::vector<OfflinePageItem>& offline_pages) {
+    std::vector<OfflinePageItem> offline_pages) {
   last_called_callback_ = LOAD;
-  offline_pages_.swap(const_cast<std::vector<OfflinePageItem>&>(offline_pages));
+  offline_pages_.swap(offline_pages);
 }
 
 void OfflinePageMetadataStoreTest::AddCallback(ItemActionStatus status) {
@@ -706,6 +705,8 @@ TEST_F(OfflinePageMetadataStoreTest, GetOfflinePagesFromInvalidStore) {
   OfflinePageMetadataStoreSQL* sql_store =
       static_cast<OfflinePageMetadataStoreSQL*>(store.get());
 
+  // Because execute method is self-healing this part of the test expects a
+  // positive results now.
   sql_store->SetStateForTesting(StoreState::NOT_LOADED, false);
   store->GetOfflinePages(
       base::Bind(&OfflinePageMetadataStoreTest::GetOfflinePagesCallback,
@@ -713,7 +714,7 @@ TEST_F(OfflinePageMetadataStoreTest, GetOfflinePagesFromInvalidStore) {
   PumpLoop();
   EXPECT_EQ(LOAD, last_called_callback_);
   EXPECT_EQ(0UL, offline_pages_.size());
-  EXPECT_EQ(StoreState::NOT_LOADED, store->state());
+  EXPECT_EQ(StoreState::LOADED, store->state());
 
   ClearResults();
   sql_store->SetStateForTesting(StoreState::FAILED_LOADING, false);
