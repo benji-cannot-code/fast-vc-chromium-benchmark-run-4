8fc17c2b630d35699d5ae4fa3c456e4aadf84751
Hiroki Nakagawa <nhiroki@chromium.org>
2017-09-04 10:53:24 +0000

    ServiceWorker: Remove dependency on ServiceWorkerProviderHost from ServiceWorkerScriptURLLoader
    Dependency on ServiceWorkerProviderHost makes it difficult to write unittests
    because it requires complicated setup routines. This CL removes the dependency.
    
    Bug: 748415
    Change-Id: I799c2368ac6d1b830e7e6780f189580d287158ea
    Reviewed-on: https://chromium-review.googlesource.com/647415
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499476}

--- content/browser/service_worker/service_worker_script_url_loader.cc
+++ content/browser/service_worker/service_worker_script_url_loader.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include "content/browser/service_worker/service_worker_context_core.h"
-#include "content/browser/service_worker/service_worker_provider_host.h"
-#include "content/browser/service_worker/service_worker_version.h"
 #include "content/browser/url_loader_factory_getter.h"
 #include "content/public/common/resource_response.h"
 
@@ -20,13 +18,12 @@ ServiceWorkerScriptURLLoader::ServiceWorkerScriptURLLoader(
     uint32_t options,
     const ResourceRequest& resource_request,
     mojom::URLLoaderClientPtr client,
-    base::WeakPtr<ServiceWorkerContextCore> context,
-    base::WeakPtr<ServiceWorkerProviderHost> provider_host,
+    scoped_refptr<ServiceWorkerVersion> version,
     scoped_refptr<URLLoaderFactoryGetter> loader_factory_getter,
     const net::MutableNetworkTrafficAnnotationTag& traffic_annotation)
     : network_client_binding_(this),
       forwarding_client_(std::move(client)),
-      provider_host_(provider_host) {
+      version_(version) {
   mojom::URLLoaderClientPtr network_client;
   network_client_binding_.Bind(mojo::MakeRequest(&network_client));
   loader_factory_getter->GetNetworkFactory()->get()->CreateLoaderAndStart(
@@ -49,7 +46,7 @@ void ServiceWorkerScriptURLLoader::OnReceiveResponse(
     const ResourceResponseHead& response_head,
     const base::Optional<net::SSLInfo>& ssl_info,
     mojom::DownloadedTempFilePtr downloaded_file) {
-  if (provider_host_) {
+  if (version_->context() && !version_->is_redundant()) {
     // We don't have complete info here, but fill in what we have now.
     // At least we need headers and SSL info.
     net::HttpResponseInfo response_info;
@@ -63,9 +60,7 @@ void ServiceWorkerScriptURLLoader::OnReceiveResponse(
     response_info.connection_info = response_head.connection_info;
     response_info.socket_address = response_head.socket_address;
 
-    DCHECK(provider_host_->IsHostToRunningServiceWorker());
-    provider_host_->running_hosted_version()->SetMainScriptHttpResponseInfo(
-        response_info);
+    version_->SetMainScriptHttpResponseInfo(response_info);
   }
   forwarding_client_->OnReceiveResponse(response_head, ssl_info,
                                         std::move(downloaded_file));
