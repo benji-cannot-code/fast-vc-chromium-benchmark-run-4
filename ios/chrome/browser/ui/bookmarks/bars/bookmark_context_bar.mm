783fbfd97349fbb1b20320bb1a592a6a3cf44f81
Ramya Sharma <ramyasharma@chromium.org>
2017-09-04 06:20:55 +0000

    Implements multiple selection state on the bookmarks context bar.
    - Implements select functionality by making the table editable.
    - Implements multiple state the context bar can be in according to
    the selection.
    - Adds eg test for the same.
    
    Demo: https://drive.google.com/a/google.com/file/d/0B-aCbwkKD2XZMTdzTElTNUlkc00/view?usp=sharing
    
    TBR: noyau@chromium.org
    Bug: 695749
    Change-Id: I084ab3f47fe3db34c91ea6441c1a805a58488076
    Reviewed-on: https://chromium-review.googlesource.com/646398
    Commit-Queue: Ramya Sharma <ramyasharma@chromium.org>
    Reviewed-by: Ramya Sharma <ramyasharma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499441}

--- ios/chrome/browser/ui/bookmarks/bars/bookmark_context_bar.mm
+++ ios/chrome/browser/ui/bookmarks/bars/bookmark_context_bar.mm
@@ -67,7 +67,11 @@ CGFloat kHorizontalMargin = 8.0f;
   UIColor* textColor = style == ContextBarButtonStyleDelete
                            ? [[MDCPalette redPalette] tint500]
                            : [[MDCPalette bluePalette] tint500];
+  UIColor* disabledColor = style == ContextBarButtonStyleDelete
+                               ? [[MDCPalette redPalette] tint200]
+                               : [[MDCPalette bluePalette] tint200];
   [button setTitleColor:textColor forState:UIControlStateNormal];
+  [button setTitleColor:disabledColor forState:UIControlStateDisabled];
 }
 
 - (void)initStyleForButton:(UIButton*)button {
@@ -86,7 +90,7 @@ CGFloat kHorizontalMargin = 8.0f;
 - (instancetype)initWithFrame:(CGRect)frame {
   self = [super initWithFrame:frame];
   if (self) {
-    self.accessibilityIdentifier = @"Context Bar";
+    self.accessibilityIdentifier = @"context_bar";
     _leadingButton = [UIButton buttonWithType:UIButtonTypeCustom];
     [self initStyleForButton:_leadingButton];
     _leadingButton.contentHorizontalAlignment =
@@ -95,7 +99,7 @@ CGFloat kHorizontalMargin = 8.0f;
     [_leadingButton addTarget:self
                        action:@selector(leadingButtonClicked:)
              forControlEvents:UIControlEventTouchUpInside];
-    _leadingButton.accessibilityIdentifier = @"Context Bar Leading Button";
+    _leadingButton.accessibilityIdentifier = @"context_bar_leading_button";
 
     _centerButton = [UIButton buttonWithType:UIButtonTypeCustom];
     [self initStyleForButton:_centerButton];
@@ -104,7 +108,7 @@ CGFloat kHorizontalMargin = 8.0f;
     [_centerButton addTarget:self
                       action:@selector(centerButtonClicked:)
             forControlEvents:UIControlEventTouchUpInside];
-    _centerButton.accessibilityIdentifier = @"Context Bar Center Button";
+    _centerButton.accessibilityIdentifier = @"context_bar_center_button";
 
     _trailingButton = [UIButton buttonWithType:UIButtonTypeCustom];
     [self initStyleForButton:_trailingButton];
@@ -114,7 +118,7 @@ CGFloat kHorizontalMargin = 8.0f;
     [_trailingButton addTarget:self
                         action:@selector(trailingButtonClicked:)
               forControlEvents:UIControlEventTouchUpInside];
-    _trailingButton.accessibilityIdentifier = @"Context Bar Trailing Button";
+    _trailingButton.accessibilityIdentifier = @"context_bar_trailing_button";
 
     _stackView = [[UIStackView alloc] initWithArrangedSubviews:@[
       _leadingButton, _centerButton, _trailingButton
@@ -145,7 +149,6 @@ CGFloat kHorizontalMargin = 8.0f;
 
 - (void)setButtonTitle:(NSString*)title forButton:(ContextBarButton)button {
   [[self getButton:button] setTitle:title forState:UIControlStateNormal];
-  [[self getButton:button] setTitle:title forState:UIControlStateNormal];
 }
 
 - (void)setButtonStyle:(ContextBarButtonStyle)style
@@ -157,6 +160,9 @@ CGFloat kHorizontalMargin = 8.0f;
   [self getButton:button].hidden = !visible;
 }
 
+- (void)setButtonEnabled:(BOOL)enabled forButton:(ContextBarButton)button {
+  [[self getButton:button] setEnabled:enabled];
+}
 - (void)leadingButtonClicked:(UIButton*)button {
   if (!self.delegate) {
     NOTREACHED();
