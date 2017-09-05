65789c101e4267cab22d06f0a79308fd9ab5e0fe
Alex Clarke <alexclarke@chromium.org>
2017-09-05 01:21:48 +0000

    SetDeviceMetricsOverride to override screen size & position on desktop
    Bug: 761798
    Change-Id: Ic596f09085464550a9f484ac84bb8bd2743e7ece
    Reviewed-on: https://chromium-review.googlesource.com/597747
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499532}

---
+++ third_party/WebKit/LayoutTests/inspector-protocol/emulation/device-emulation-desktop.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  let {page, session, dp} = await testRunner.startBlank('Tests that desktop device emulation affects media rules, viewport meta tag, body dimensions and window.screen.');
+
+  await session.protocol.Emulation.setDeviceMetricsOverride({
+    width: 800,
+    height: 600,
+    deviceScaleFactor: 1,
+    mobile: false,
+    fitWindow: false,
+    scale: 1,
+    screenWidth: 1200,
+    screenHeight: 1000,
+    positionX: 110,
+    positionY: 120
+  });
+
+  var viewport = 'w=dw';
+  testRunner.log(`Loading page with viewport=${viewport}`);
+  await session.navigate('../resources/device-emulation.html?' + viewport);
+
+  testRunner.log(await session.evaluate(`dumpMetrics(true)`));
+  testRunner.completeTest();
+})
