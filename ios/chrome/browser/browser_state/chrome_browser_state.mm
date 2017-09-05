05a6d261000a499fd4f13a53091465bba01ccfa4
Sylvain Defresne <sdefresne@chromium.org>
2017-09-05 12:09:07 +0000

    Remove uses of web::TestBrowserState in ios/chrome & ios/clean/chrome.
    All code in ios/chrome and ios/clean/chrome assume that it is safe to
    cast a web::BrowserState* to an ios::ChromeBrowserState because it is
    the only concrete sub-class of ios::ChromeBrowserState.
    
    This is true in production but for test, there are two sub-classes of
    web::BrowserState, web::TestBrowserState and TestChromeBrowserState
    and only the latter inherits from ios::ChromeBrowserState. The former
    should only be used by code below ios/chrome (like ios/web) but some
    uses have slipped into the code.
    
    Fix all the use of web::TestBrowserState to instead use the correct
    class TestChromeBrowserState and add a DCHECK in the function used
    to convert a web::BrowserState to an ios::ChromeBrowserState.
    
    Fix small errors (incorrect destruction order, ...) while fixing the
    code, and convert some tests to use BrowserCoordinatorTest to share
    the code constructing a test Browser.
    
    Bug: None
    Change-Id: I59f6b0e59877225c3b12769a19d3ef057e7890c5
    Reviewed-on: https://chromium-review.googlesource.com/649327
    Reviewed-by: Eric Noyau <noyau@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499605}

--- ios/chrome/browser/browser_state/chrome_browser_state.mm
+++ ios/chrome/browser/browser_state/chrome_browser_state.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 
+#include <memory>
 #include <utility>
 
 #include "base/files/file_path.h"
@@ -24,11 +25,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace ios {
+namespace {
+#if DCHECK_IS_ON()
+// All ChromeBrowserState will store a dummy base::SupportsUserData::Data
+// object with this key. It can be used to check that a web::BrowserState
+// is effectively a ChromeBrowserState when converting.
+const char kBrowserStateIsChromeBrowserState[] = "IsChromeBrowserState";
+#endif
+}
 
 ChromeBrowserState::ChromeBrowserState(
     scoped_refptr<base::SequencedTaskRunner> io_task_runner)
     : io_task_runner_(std::move(io_task_runner)) {
   DCHECK(io_task_runner_);
+#if DCHECK_IS_ON()
+  SetUserData(kBrowserStateIsChromeBrowserState,
+              std::make_unique<base::SupportsUserData::Data>());
+#endif
 }
 
 ChromeBrowserState::~ChromeBrowserState() {}
@@ -36,7 +49,13 @@ ChromeBrowserState::~ChromeBrowserState() {}
 // static
 ChromeBrowserState* ChromeBrowserState::FromBrowserState(
     web::BrowserState* browser_state) {
-  // This is safe; this is the only implementation of BrowserState.
+  if (!browser_state)
+    return nullptr;
+
+  // Check that the BrowserState is a ChromeBrowserState. It should always
+  // be true in production and during tests as the only BrowserState that
+  // should be used in ios/chrome inherits from ChromeBrowserState.
+  DCHECK(browser_state->GetUserData(kBrowserStateIsChromeBrowserState));
   return static_cast<ChromeBrowserState*>(browser_state);
 }
 
