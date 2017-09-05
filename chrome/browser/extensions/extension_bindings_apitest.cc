ef0832c59fd57a61c7055902ff5ceb09ead643cf
Dave Tapuska <dtapuska@chromium.org>
2017-09-05 21:42:08 +0000

    Revert "Disable flaking test FramesBeforeNavigation"
    This reverts commit d5576cda8f2c3c01d2eaed7284f6d04a74d4cc75.
    
    Reason for revert: Actual offending change was reverted here: https://chromium-review.googlesource.com/c/chromium/src/+/647907
    
    Original change's description:
    > Disable flaking test FramesBeforeNavigation
    > 
    > JavaScript/FramesExtensionBindingsApiTest.FramesBeforeNavigation has
    > been flaky on Linux and Mac builders; marking as flaky until the issue
    > is resolved.
    > 
    > Bug: 761368
    > Change-Id: I26f08b57a654a2578f24b3f3836787da3f13075b
    > TBR: rdevlin.cronin@chromium.org
    > TBR: alexmos@chromium.org
    > Reviewed-on: https://chromium-review.googlesource.com/647467
    > Reviewed-by: Ian Clelland <iclelland@chromium.org>
    > Commit-Queue: Ian Clelland <iclelland@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#499197}
    
    TBR=iclelland@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 761368
    Change-Id: Iceea547386768ae3fc2f0f5fa759e083ecd9c4e8
    Reviewed-on: https://chromium-review.googlesource.com/651467
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499756}

--- chrome/browser/extensions/extension_bindings_apitest.cc
+++ chrome/browser/extensions/extension_bindings_apitest.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Contains holistic tests of the bindings infrastructure
 
 #include "base/run_loop.h"
-#include "build/build_config.h"
 #include "chrome/browser/extensions/api/permissions/permissions_api.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/net/url_request_mock_util.h"
@@ -242,16 +241,7 @@ class FramesExtensionBindingsApiTest : public ExtensionBindingsApiTest {
 // messages ("receiver") and one which we'll try first faking messages from in
 // the web page's iframe, as well as actually send a message from later
 // ("sender").
-
-// This test is currently flaking on Linux and Mac builders
-// https://crbug.com/761368
-#if defined(OS_LINUX) || defined(OS_MACOSX)
-#define MAYBE_FramesBeforeNavigation DISABLED_FramesBeforeNavigation
-#else
-#define MAYBE_FramesBeforeNavigation FramesBeforeNavigation
-#endif
-IN_PROC_BROWSER_TEST_P(FramesExtensionBindingsApiTest,
-                       MAYBE_FramesBeforeNavigation) {
+IN_PROC_BROWSER_TEST_P(FramesExtensionBindingsApiTest, FramesBeforeNavigation) {
   // Load the sender and receiver extensions, and make sure they are ready.
   ExtensionTestMessageListener sender_ready("sender_ready", true);
   const Extension* sender = LoadExtension(
