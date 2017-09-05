ed3979109a71f47f88d9fdaed8b2639d8d557d73
Christopher Lam <calamity@chromium.org>
2017-09-05 02:59:29 +0000

    [MD Bookmarks] Announce when the bookmark list changes.
    This CL makes screenreaders announce the number of bookmarks in the
    bookmark list whenever the number of items in it changes.
    
    Bug: 735808
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Icb58090c576eead3d9634126c95bd8ce291e66dc
    Reviewed-on: https://chromium-review.googlesource.com/646811
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499543}

--- chrome/browser/resources/md_bookmarks/list.js
+++ chrome/browser/resources/md_bookmarks/list.js
@@ -71,6 +71,10 @@ Polymer({
     /** @private {function(!Event)} */
     this.boundOnHighlightItems_ = this.onHighlightItems_.bind(this);
     document.addEventListener('highlight-items', this.boundOnHighlightItems_);
+
+    Polymer.RenderStatus.afterNextRender(this, function() {
+      Polymer.IronA11yAnnouncer.requestAvailability();
+    });
   },
 
   detached: function() {
@@ -110,6 +114,10 @@ Polymer({
           'displayedList_', splice.index, splice.removed.length
         ].concat(additions));
       });
+
+      cr.sendWithPromise(
+            'getPluralString', 'listChanged', this.displayedList_.length)
+          .then((label) => this.fire('iron-announce', {text: label}));
     }
   },
 
