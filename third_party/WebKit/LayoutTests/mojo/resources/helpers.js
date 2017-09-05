89d2d39df0a15a0d7ca76996acbf759f3f4a09fd
Reilly Grant <reillyg@chromium.org>
2017-09-05 23:10:30 +0000

    [MojoJS] Support intercepting interfaces at process scope
    This patch expands the concept of "process" scope to the
    MojoInterfaceInterceptor class. Interceptors created with this option
    will add binder overrides to the process's Connector.
    
    Bug: 758675
    Change-Id: Iae8fbc6617faedf46ac43d5171234518e35e1eab
    Reviewed-on: https://chromium-review.googlesource.com/646764
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499786}

---
+++ third_party/WebKit/LayoutTests/mojo/resources/helpers.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+const kTestReply = "hehe got ya";
+
+// An impl of the test interface which replies to reverse() with a fixed
+// message rather than the normally expected value.
+class TestHelperImpl {
+  constructor() {
+    this.binding_ =
+        new mojo.Binding(content.mojom.MojoLayoutTestHelper, this);
+  }
+  bindRequest(request) { this.binding_.bind(request); }
+  getLastString() { return this.lastString_; }
+  reverse(message) {
+    this.lastString_ = message;
+    return Promise.resolve({ reversed: kTestReply });
+  }
+}
