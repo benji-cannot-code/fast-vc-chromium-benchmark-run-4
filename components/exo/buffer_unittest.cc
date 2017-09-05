91a6925e0f8e615ebc03b675f27c68e9102feace
Peng Huang <penghuang@google.com>
2017-09-05 16:00:39 +0000

    exo: Recreate the transferable resource from exo::Buffer if necessary.
    The exo always releases the TransferableResource, when it is returned from the
    compositor. But when a subsurface is removed from the parent, the exo will not
    generate TextureDrawQuad for the subsurface, so the compositor will return the
    TransferableResource for this subsurface, and then the exo will release it. When
    the subsurace is added to the parent again, the TransferableResource is not
    accessable. This CL fixes the issue by creating the ThrasferableResource if
    necessary.
    
    Bug: 757717
    Change-Id: I1c43d4aed9301080d302c7f618d10ed62f25459a
    Reviewed-on: https://chromium-review.googlesource.com/641951
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499646}

--- components/exo/buffer_unittest.cc
+++ components/exo/buffer_unittest.cc
@@ -46,8 +46,8 @@ TEST_F(BufferTest, ReleaseCallback) {
   buffer->OnAttach();
   viz::TransferableResource resource;
   // Produce a transferable resource for the contents of the buffer.
-  bool rv = buffer->ProduceTransferableResource(frame_sink_holder, false, true,
-                                                &resource);
+  bool rv =
+      buffer->ProduceTransferableResource(frame_sink_holder, false, &resource);
   ASSERT_TRUE(rv);
 
   // Release buffer.
@@ -79,8 +79,8 @@ TEST_F(BufferTest, IsLost) {
   buffer->OnAttach();
   // Acquire a texture transferable resource for the contents of the buffer.
   viz::TransferableResource resource;
-  bool rv = buffer->ProduceTransferableResource(frame_sink_holder, false, true,
-                                                &resource);
+  bool rv =
+      buffer->ProduceTransferableResource(frame_sink_holder, false, &resource);
   ASSERT_TRUE(rv);
 
   scoped_refptr<viz::ContextProvider> context_provider =
@@ -106,7 +106,7 @@ TEST_F(BufferTest, IsLost) {
   // Producing a new texture transferable resource for the contents of the
   // buffer.
   viz::TransferableResource new_resource;
-  rv = buffer->ProduceTransferableResource(frame_sink_holder, false, false,
+  rv = buffer->ProduceTransferableResource(frame_sink_holder, false,
                                            &new_resource);
   ASSERT_TRUE(rv);
   buffer->OnDetach();
@@ -135,8 +135,8 @@ TEST_F(BufferTest, OnLostResources) {
   buffer->OnAttach();
   // Acquire a texture transferable resource for the contents of the buffer.
   viz::TransferableResource resource;
-  bool rv = buffer->ProduceTransferableResource(frame_sink_holder, false, true,
-                                                &resource);
+  bool rv =
+      buffer->ProduceTransferableResource(frame_sink_holder, false, &resource);
   ASSERT_TRUE(rv);
 
   static_cast<ui::InProcessContextFactory*>(
--- components/exo/pointer_unittest.cc
+++ components/exo/pointer_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/buffer.h"
 #include "components/exo/pointer_delegate.h"
 #include "components/exo/shell_surface.h"
+#include "components/exo/sub_surface.h"
 #include "components/exo/surface.h"
 #include "components/exo/test/exo_test_base.h"
 #include "components/exo/test/exo_test_helper.h"
@@ -153,7 +154,8 @@ TEST_F(PointerTest, OnPointerMotion) {
                         gfx::Vector2d(1, 1));
 
   std::unique_ptr<Surface> sub_surface(new Surface);
-  surface->AddSubSurface(sub_surface.get());
+  std::unique_ptr<SubSurface> sub(
+      new SubSurface(sub_surface.get(), surface.get()));
   surface->SetSubSurfacePosition(sub_surface.get(), gfx::Point(5, 5));
   gfx::Size sub_buffer_size(5, 5);
   std::unique_ptr<Buffer> sub_buffer(
