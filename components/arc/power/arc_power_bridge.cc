ffd898e89c119f2a9da25edebbdd9dcd31a94870
Mike Wasserman <msw@chromium.org>
2017-09-05 18:14:19 +0000

    mash: Workaround arc and exo startup crashes.
    Move exo VSync observation to WMHelper with Ash impl, mus no-op.
    Stub out other crashing code, see the details in the bug.
    
    Bug: 761148
    Change-Id: I95f8c4d452a8857d9763f1f468981e731a3ce96e
    Reviewed-on: https://chromium-review.googlesource.com/646846
    Commit-Queue: Michael Wasserman <msw@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499678}

--- components/arc/power/arc_power_bridge.cc
+++ components/arc/power/arc_power_bridge.cc
@@ -72,7 +72,9 @@ void ArcPowerBridge::OnInstanceReady() {
   mojom::PowerHostPtr host_proxy;
   binding_.Bind(mojo::MakeRequest(&host_proxy));
   power_instance->Init(std::move(host_proxy));
-  ash::Shell::Get()->display_configurator()->AddObserver(this);
+  // TODO(mash): Support this functionality without ash::Shell access in Chrome.
+  if (ash::Shell::HasInstance())
+    ash::Shell::Get()->display_configurator()->AddObserver(this);
   chromeos::DBusThreadManager::Get()->GetPowerManagerClient()->
       AddObserver(this);
   chromeos::DBusThreadManager::Get()
@@ -83,7 +85,9 @@ void ArcPowerBridge::OnInstanceReady() {
 }
 
 void ArcPowerBridge::OnInstanceClosed() {
-  ash::Shell::Get()->display_configurator()->RemoveObserver(this);
+  // TODO(mash): Support this functionality without ash::Shell access in Chrome.
+  if (ash::Shell::HasInstance())
+    ash::Shell::Get()->display_configurator()->RemoveObserver(this);
   chromeos::DBusThreadManager::Get()->GetPowerManagerClient()->
       RemoveObserver(this);
   ReleaseAllDisplayWakeLocks();
@@ -184,7 +188,11 @@ void ArcPowerBridge::OnReleaseDisplayWakeLock(mojom::DisplayWakeLockType type) {
 }
 
 void ArcPowerBridge::IsDisplayOn(const IsDisplayOnCallback& callback) {
-  callback.Run(ash::Shell::Get()->display_configurator()->IsDisplayOn());
+  bool is_display_on = false;
+  // TODO(mash): Support this functionality without ash::Shell access in Chrome.
+  if (ash::Shell::HasInstance())
+    is_display_on = ash::Shell::Get()->display_configurator()->IsDisplayOn();
+  callback.Run(is_display_on);
 }
 
 void ArcPowerBridge::OnScreenBrightnessUpdateRequest(double percent) {
--- components/exo/wm_helper.h
+++ components/exo/wm_helper.h
@@ -95,6 +95,15 @@ class WMHelper : public aura::client::DragDropDelegate {
     virtual ~DragDropObserver() {}
   };
 
+  class VSyncObserver {
+   public:
+    virtual void OnUpdateVSyncParameters(base::TimeTicks timebase,
+                                         base::TimeDelta interval) = 0;
+
+   protected:
+    virtual ~VSyncObserver() {}
+  };
+
   ~WMHelper() override;
 
   static void SetInstance(WMHelper* helper);
@@ -118,6 +127,8 @@ class WMHelper : public aura::client::DragDropDelegate {
   void RemoveDragDropObserver(DragDropObserver* observer);
   void SetDragDropDelegate(aura::Window*);
   void ResetDragDropDelegate(aura::Window*);
+  void AddVSyncObserver(VSyncObserver* observer);
+  void RemoveVSyncObserver(VSyncObserver* observer);
 
   virtual const display::ManagedDisplayInfo& GetDisplayInfo(
       int64_t display_id) const = 0;
@@ -155,6 +166,8 @@ class WMHelper : public aura::client::DragDropDelegate {
   void NotifyTabletModeEnded();
   void NotifyKeyboardDeviceConfigurationChanged();
   void NotifyDisplayConfigurationChanged();
+  void NotifyUpdateVSyncParameters(base::TimeTicks timebase,
+                                   base::TimeDelta interval);
 
  private:
   base::ObserverList<ActivationObserver> activation_observers_;
@@ -164,6 +177,11 @@ class WMHelper : public aura::client::DragDropDelegate {
   base::ObserverList<InputDeviceEventObserver> input_device_event_observers_;
   base::ObserverList<DisplayConfigurationObserver> display_config_observers_;
   base::ObserverList<DragDropObserver> drag_drop_observers_;
+  base::ObserverList<VSyncObserver> vsync_observers_;
+
+  // The most recently cached VSync parameters, sent to observers on addition.
+  base::TimeTicks vsync_timebase_;
+  base::TimeDelta vsync_interval_;
 
   DISALLOW_COPY_AND_ASSIGN(WMHelper);
 };
