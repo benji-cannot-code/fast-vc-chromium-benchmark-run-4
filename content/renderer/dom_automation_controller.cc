b893acde311b77f63b54dc05e6dac17fbeca1536
Lukasz Anforowicz <lukasza@chromium.org>
2017-09-05 20:20:54 +0000

    Remove domAutomationController.setAutomationId/sendWithId/sendJSON.
    The 3 methods can be removed, because:
    
    *) domAutomationController.setAutomationId is a no-op after r485461
    *) after a series of CLs associated with bug 662543, there are no
       more calls to domAutomationController.setAutomationId
    
    *) this CL replaces the only 2 calls of
       domAutomationController.sendWithId with calls to
       domAutomationController.send (they are equivalent, because the
       |automation_id| parameter has been ignored r350364)
    
    *) there were/are no callers of
       domAutomationController.sendJSON
    
    Bug: 662543
    Change-Id: Iab8b2034861b2096ff5b7b24329d600e6fb0b89c
    Reviewed-on: https://chromium-review.googlesource.com/626697
    Reviewed-by: Nick Carter <nick@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499728}

--- content/renderer/dom_automation_controller.cc
+++ content/renderer/dom_automation_controller.cc
@@ -51,10 +51,7 @@ gin::ObjectTemplateBuilder DomAutomationController::GetObjectTemplateBuilder(
     v8::Isolate* isolate) {
   return gin::Wrappable<DomAutomationController>::GetObjectTemplateBuilder(
              isolate)
-      .SetMethod("send", &DomAutomationController::SendMsg)
-      .SetMethod("setAutomationId", &DomAutomationController::SetAutomationId)
-      .SetMethod("sendJSON", &DomAutomationController::SendJSON)
-      .SetMethod("sendWithId", &DomAutomationController::SendWithId);
+      .SetMethod("send", &DomAutomationController::SendMsg);
 }
 
 void DomAutomationController::OnDestruct() {}
@@ -107,32 +104,7 @@ bool DomAutomationController::SendMsg(const gin::Arguments& args) {
   if (!value || !serializer.Serialize(*value))
     return false;
 
-  bool succeeded = Send(new FrameHostMsg_DomOperationResponse(
-      routing_id(), json));
-
-  return succeeded;
-}
-
-bool DomAutomationController::SendJSON(const std::string& json) {
-  if (!render_frame())
-    return false;
-
-  bool result = Send(new FrameHostMsg_DomOperationResponse(
-      routing_id(), json));
-
-  return result;
-}
-
-bool DomAutomationController::SendWithId(int automation_id,
-                                         const std::string& str) {
-  if (!render_frame())
-    return false;
-  return Send(
-      new FrameHostMsg_DomOperationResponse(routing_id(), str));
-}
-
-bool DomAutomationController::SetAutomationId(int automation_id) {
-  return true;
+  return Send(new FrameHostMsg_DomOperationResponse(routing_id(), json));
 }
 
 }  // namespace content
