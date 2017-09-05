e0cef3d1aec5190aa0ae46f0fda9b807e047da5c
Pavol Marko <pmarko@chromium.org>
2017-09-05 18:58:42 +0000

    Commit minimal migration status to local state prefs
    Commit minimal migration status to local state PrefService.
    Introduces a CommitPendingWrite function with a callback to be sure that
    prefs landed on disk.
    
    BUG=747907
    
    Change-Id: I565ffbae7b8a1e934875101012c39c8a9841a139
    Reviewed-on: https://chromium-review.googlesource.com/647454
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499693}

--- components/prefs/persistent_pref_store.cc
+++ components/prefs/persistent_pref_store.cc
@@ -9,10 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/threading/sequenced_task_runner_handle.h"
 
-void PersistentPrefStore::CommitPendingWrite() {
-  CommitPendingWrite(base::OnceClosure());
-}
-
 void PersistentPrefStore::CommitPendingWrite(base::OnceClosure done_callback) {
   // Default behavior for PersistentPrefStore implementation that don't issue
   // disk operations: schedule the callback immediately.
