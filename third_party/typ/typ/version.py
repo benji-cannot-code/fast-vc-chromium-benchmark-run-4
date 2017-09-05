a011975abfcf10669bde601523261619d9d75775
Dirk Pranke <dpranke@chromium.org>
2017-09-05 19:30:01 +0000

    Roll typ 0.9.12 .. 0.10.0
    This rolls typ from 2b5a4fcf..1e254b3e and picks up the
    following change:
    
        Make multiple top-level dirs work, bump version to 0.10.0.
    
        Previously all of the tests in a given typ invocation had to
        exist in a single importable python tree. In other words,
        there had to be a single directory at the top and you could
        import any module or file from under that directory. This
        meant that you couldn't run tests from the
        //third_party/WebKit/Tools/Scripts/webkitpy and
        //third_party/blink/tools directories of a Chromium checkout
        in a single invocation, and that meant you had to jump through
        hoops (potentially incorrectly) to try and get coverage in a
        single "test suite".
    
        This CL adds support for multiple disjoint top-level directories,
        adding a new --top-level-dirs flag that can be set multiple times
        (i.e, as a list). The existing --top-level-dir flag still works,
        and is equivalent to setting top-level-dirs once. However, setting
        both is an error.
    
    which is a fix needed to clean up test-webkitpy so that it reports
    the full set of test results from the two directories.
    
    R=qyearsley@chromium.org, nednguyen@chromium.org
    
    Bug: 
    Change-Id: Ia5da837b7f7c655abb6592f8664a0f9b1311c421
    Reviewed-on: https://chromium-review.googlesource.com/648514
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Commit-Queue: Dirk Pranke <dpranke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499704}

--- third_party/typ/typ/version.py
+++ third_party/typ/typ/version.py
@@ -13,4 +13,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-VERSION = '0.9.12'
+VERSION = '0.10.0'
