1d6aa01bfc794ac5553bdde9315bf4750383743c
Sergey Ulanov <sergeyu@chromium.org>
2017-09-05 21:36:02 +0000

    Update RemoteTestServer to support asynchronous startup.
    LocalTestServer allows to start testserver asynchronously, but
    RemoteTestServer didn't support it. This feature is used by some
    unittests that need to run multiple instances of the server. Currently
    these tests are disabled on Android. This change will allow to enable
    them on Android and on Fuchsia.
    
    Replaced SpawnedCommunicator with RemoteTestServerSpawnerRequest.
    The new class is asynchronous and sends only one request. This makes
    it easier to track request state in RemoteTestServer for asynchronous
    operations.
    
    Bug: 731302
    Change-Id: I6e8f43cecf753a923a418ded972917adb4c56c12
    Reviewed-on: https://chromium-review.googlesource.com/640130
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499755}

--- net/test/spawned_test_server/base_test_server.cc
+++ net/test/spawned_test_server/base_test_server.cc
@@ -261,6 +261,10 @@ BaseTestServer::BaseTestServer(Type type, const SSLOptions& ssl_options)
 
 BaseTestServer::~BaseTestServer() {}
 
+bool BaseTestServer::Start() {
+  return StartInBackground() && BlockUntilStarted();
+}
+
 const HostPortPair& BaseTestServer::host_port_pair() const {
   DCHECK(started_);
   return host_port_pair_;
@@ -464,6 +468,7 @@ bool BaseTestServer::SetAndParseServerData(const std::string& server_data,
 
 bool BaseTestServer::SetupWhenServerStarted() {
   DCHECK(host_port_pair_.port());
+  DCHECK(!started_);
 
   if (UsingSSL(type_) && !LoadTestRootCert())
       return false;
