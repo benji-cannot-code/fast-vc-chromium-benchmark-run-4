288816fc8091aabb2a60479536b0349daf9f8f6e
Yutaka Hirano <yhirano@chromium.org>
2017-09-05 05:41:09 +0000

    Add query parameters to investigate suborigin test flakiness
    Bug: 705125
    Change-Id: Idf9fd1261982ba74f86372a4d63ae7a6b09cc840
    Reviewed-on: https://chromium-review.googlesource.com/650126
    Reviewed-by: Takeshi Yoshino <tyoshino@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499568}

--- third_party/WebKit/LayoutTests/http/tests/security/suborigins/crossorigin/suborigin-cors-xhr-failure-output.php
+++ third_party/WebKit/LayoutTests/http/tests/security/suborigins/crossorigin/suborigin-cors-xhr-failure-output.php
@@ -16,8 +16,9 @@ async_test(t => {
     var xhr = new XMLHttpRequest();
     xhr.onerror = t.step_func_done();
     xhr.onload = t.unreached_func('XHR succeeded');
+    // with-preflight is attached to distinguish console error outputs.
     xhr.open('GET', 'http://127.0.0.1:8000/security/resources/' +
-                    'cors-script.php?cors=false');
+                    'cors-script.php?cors=false&with-preflight');
     xhr.setRequestHeader('x-custom-header', 'foobar');
     xhr.send();
   }, 'Custom headers causes preflight failure');
@@ -26,8 +27,9 @@ async_test(t => {
     var xhr = new XMLHttpRequest();
     xhr.onerror = t.step_func_done();
     xhr.onload = t.unreached_func();
+    // without-preflight is attached to distinguish console error outputs.
     xhr.open('GET', 'http://127.0.0.1:8000/security/resources/' +
-                    'cors-script.php?cors=false');
+                    'cors-script.php?cors=false&without-preflight');
     xhr.send();
   }, 'Lack of Access-Control-Allow-Suborigin on response causes failure');
 </script>
