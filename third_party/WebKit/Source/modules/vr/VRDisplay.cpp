e26e1e6eafebb14e02611ad1093f7fe521b775fb
Brandon Jones <bajones@chromium.org>
2017-09-05 18:33:44 +0000

    Renamed VRLayer to VRLayerInit to match WebVR spec
    This won't have any visible effect to developers, since they
    never call the dictionary by name. It does however clear a
    conflict with the upcoming 2.0 spec, which uses the VRLayer
    name.
    
    Bug: 700682
    Change-Id: I39d0b0dab72235b7c088b3ab1eca8959582eb069
    Reviewed-on: https://chromium-review.googlesource.com/648078
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499686}

--- third_party/WebKit/Source/modules/vr/VRDisplay.cpp
+++ third_party/WebKit/Source/modules/vr/VRDisplay.cpp
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/vr/VRDisplayCapabilities.h"
 #include "modules/vr/VREyeParameters.h"
 #include "modules/vr/VRFrameData.h"
-#include "modules/vr/VRLayer.h"
+#include "modules/vr/VRLayerInit.h"
 #include "modules/vr/VRPose.h"
 #include "modules/vr/VRStageParameters.h"
 #include "modules/webgl/WebGLRenderingContextBase.h"
@@ -290,7 +290,7 @@ void ReportPresentationResult(PresentationResult result) {
 }
 
 ScriptPromise VRDisplay::requestPresent(ScriptState* script_state,
-                                        const HeapVector<VRLayer>& layers) {
+                                        const HeapVector<VRLayerInit>& layers) {
   DVLOG(1) << __FUNCTION__;
   ExecutionContext* execution_context = ExecutionContext::From(script_state);
   UseCounter::Count(execution_context, WebFeature::kVRRequestPresent);
@@ -602,8 +602,8 @@ void VRDisplay::UpdateLayerBounds() {
       WebSize(source_width_, source_height_));
 }
 
-HeapVector<VRLayer> VRDisplay::getLayers() {
-  HeapVector<VRLayer> layers;
+HeapVector<VRLayerInit> VRDisplay::getLayers() {
+  HeapVector<VRLayerInit> layers;
 
   if (is_presenting_) {
     layers.push_back(layer_);
