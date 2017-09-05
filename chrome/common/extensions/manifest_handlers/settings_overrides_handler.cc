13a620bc3935f9a44948827eba690b5062d19c3c
Catherine Mullings <catmullings@chromium.org>
2017-09-05 17:03:57 +0000

    Replace custom code to strip 'www' in setting override permission warning
    SettingsOverrideHandler::FormatUrlForDisplay() implements a custom way to
    strip a 'www' prefix from an origin. Instead the method should use the
    existing function, url_formatter:StripWWW(). This CL makes this change.
    
    Bug: 717283
    Change-Id: I273bec6b5677f39d45298b334b1c66bde9cc1849
    Reviewed-on: https://chromium-review.googlesource.com/646955
    Commit-Queue: catmullings <catmullings@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499658}

--- chrome/common/extensions/manifest_handlers/settings_overrides_handler.cc
+++ chrome/common/extensions/manifest_handlers/settings_overrides_handler.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
+#include "components/url_formatter/url_formatter.h"
 #include "extensions/common/error_utils.h"
 #include "extensions/common/extension_set.h"
 #include "extensions/common/feature_switch.h"
@@ -28,8 +29,6 @@ using extensions::api::manifest_types::ChromeSettingsOverrides;
 
 namespace extensions {
 namespace {
-
-const char kWwwPrefix[] = "www.";
 
 std::unique_ptr<GURL> CreateManifestURL(const std::string& url) {
   std::unique_ptr<GURL> manifest_url(new GURL(url));
@@ -106,11 +105,9 @@ std::string FormatUrlForDisplay(const GURL& url) {
   base::StringPiece host = url.host_piece();
   // A www. prefix is not informative and thus not worth the limited real estate
   // in the permissions UI.
-  base::StringPiece formatted_host =
-      base::StartsWith(host, kWwwPrefix, base::CompareCase::INSENSITIVE_ASCII)
-          ? host.substr(strlen(kWwwPrefix))
-          : host;
-  return formatted_host.as_string();
+  // TODO(catmullings): Ideally, we wouldn't be using custom code to format URLs
+  // here, since we have a number of methods that do that more universally.
+  return base::UTF16ToUTF8(url_formatter::StripWWW(base::UTF8ToUTF16(host)));
 }
 
 }  // namespace
