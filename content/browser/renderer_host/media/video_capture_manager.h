22d623e0f050f007748dcf682cfa0321750f2e7a
Christian Fremerey <chfremer@chromium.org>
2017-09-05 17:44:12 +0000

    [Video Capture] Add more verbose WebRtc logs for video capture
    This is to facilitate the investigation of camera issues for which WebRtc logs
    are available but no detailed bug reports or reproduction steps can be obtained.
    
    Bug: 643068
    Change-Id: I0b8db85d749788f1897926bfef7d8b5f9312e41d
    Reviewed-on: https://chromium-review.googlesource.com/642567
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499670}

--- content/browser/renderer_host/media/video_capture_manager.h
+++ content/browser/renderer_host/media/video_capture_manager.h
@@ -55,7 +55,8 @@ class CONTENT_EXPORT VideoCaptureManager
       base::Callback<void(const base::WeakPtr<VideoCaptureController>&)>;
 
   explicit VideoCaptureManager(
-      std::unique_ptr<VideoCaptureProvider> video_capture_provider);
+      std::unique_ptr<VideoCaptureProvider> video_capture_provider,
+      base::RepeatingCallback<void(const std::string&)> emit_log_message_cb);
 
   // AddVideoCaptureObserver() can be called only before any devices are opened.
   // RemoveAllVideoCaptureObservers() can be called only after all devices
@@ -263,6 +264,8 @@ class CONTENT_EXPORT VideoCaptureManager
   bool application_state_has_running_activities_;
 #endif
 
+  void EmitLogMessage(const std::string& message, int verbose_log_level);
+
   // Only accessed on Browser::IO thread.
   base::ObserverList<MediaStreamProviderListener> listeners_;
   media::VideoCaptureSessionId new_capture_session_id_;
@@ -286,6 +289,7 @@ class CONTENT_EXPORT VideoCaptureManager
   std::list<std::pair<int, base::Closure>> photo_request_queue_;
 
   const std::unique_ptr<VideoCaptureProvider> video_capture_provider_;
+  base::RepeatingCallback<void(const std::string&)> emit_log_message_cb_;
 
   base::ObserverList<media::VideoCaptureObserver> capture_observers_;
 
