f474c2755d92a64ba6d46631276fa5347d778fd8
Peter K. Lee <pkl@chromium.org>
2017-09-05 01:25:26 +0000

    Add Inbox support for mailto:// URLs
    This adds a new class based on the MailtoHandler class for Inbox by
    Gmail app.
    
    Bug: 761502
    Change-Id: Ia5e1d6fe866d662919bc5df81b8c1c8159acc899
    Reviewed-on: https://chromium-review.googlesource.com/648464
    Reviewed-by: Jean-François Geyelin <jif@chromium.org>
    Commit-Queue: Peter Lee <pkl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499538}

--- ios/chrome/browser/web/nullable_mailto_url_rewriter.mm
+++ ios/chrome/browser/web/nullable_mailto_url_rewriter.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/logging.h"
 #import "ios/chrome/browser/web/mailto_handler.h"
 #import "ios/chrome/browser/web/mailto_handler_gmail.h"
+#import "ios/chrome/browser/web/mailto_handler_inbox.h"
 #import "ios/chrome/browser/web/mailto_handler_system_mail.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -36,7 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 + (instancetype)mailtoURLRewriterWithStandardHandlers {
   id result = [[NullableMailtoURLRewriter alloc] init];
   [result setDefaultHandlers:@[
-    [[MailtoHandlerSystemMail alloc] init], [[MailtoHandlerGmail alloc] init]
+    [[MailtoHandlerSystemMail alloc] init], [[MailtoHandlerGmail alloc] init],
+    [[MailtoHandlerInbox alloc] init]
   ]];
   return result;
 }
