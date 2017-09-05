55b6d6e0ecd42c01e68623b9d2bcb9d9198df9f2
Hidehiko Abe <hidehiko@chromium.org>
2017-09-05 17:34:29 +0000

    Replace CustomFakeCryptohomeClient by FakeCrytohomeClient.
    The customized part is unused. So, just replace it.
    
    BUG=739622
    TEST=Ran trybots.
    
    Change-Id: I6886dffe650310b8452c7126cd8deabdbce183fb
    Reviewed-on: https://chromium-review.googlesource.com/578706
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Bartosz Fabianowski <bartfab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499665}

--- chrome/browser/chromeos/attestation/attestation_policy_browsertest.cc
+++ chrome/browser/chromeos/attestation/attestation_policy_browsertest.cc
@@ -4,13 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/bind.h"
-#include "base/location.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/run_loop.h"
-#include "base/single_thread_task_runner.h"
 #include "base/threading/platform_thread.h"
-#include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "chrome/browser/chromeos/attestation/platform_verification_flow.h"
 #include "chrome/browser/chromeos/policy/device_policy_builder.h"
@@ -25,16 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using chromeos::attestation::PlatformVerificationFlow;
 
 namespace policy {
-
-class CustomFakeCryptohomeClient : public chromeos::FakeCryptohomeClient {
- public:
-  void TpmAttestationIsEnrolled(
-      const chromeos::BoolDBusMethodCallback& callback) override {
-    base::ThreadTaskRunnerHandle::Get()->PostTask(
-        FROM_HERE,
-        base::BindOnce(callback, chromeos::DBUS_METHOD_CALL_FAILURE, false));
-  }
-};
 
 class AttestationDevicePolicyTest
     : public DevicePolicyCrosBrowserTest,
@@ -90,7 +77,7 @@ class AttestationDevicePolicyTest
  private:
   bool operation_complete_;
   PlatformVerificationFlow::Result result_;
-  CustomFakeCryptohomeClient fake_cryptohome_client_;
+  chromeos::FakeCryptohomeClient fake_cryptohome_client_;
 
   void WaitForAsyncOperation() {
     while (!operation_complete_) {
@@ -122,4 +109,4 @@ IN_PROC_BROWSER_TEST_F(AttestationDevicePolicyTest, ContentProtectionTest) {
             SyncContentProtectionAttestation());
 }
 
-} // namespace policy
+}  // namespace policy
