1d60dac340abc49e84413fe4f96f6f0cb54d8914
Mike Wasserman <msw@chromium.org>
2017-09-05 23:32:43 +0000

    mash: Avoid ChromeLauncherController and ShelfWindowWatcher race.
    Initialize the shelf item type for chrome app [panel] windows early.
    The window type should be marked before adding it to a container.
    (that prevents ash::ShelfWindowWatcher from ever creating a shelf item)
    (otherwise, Ash and Chrome race to create a shelf items for windows)
    
    TODO: Apply this window property similarly early in classic ash.
    (we don't currently use ShelfWindowWatcher for unknown windows there)
    
    Bug: 730884, 750334
    Change-Id: I67fbf0fa48ddfdb4c89500e1f4dd3606ac69d675
    Reviewed-on: https://chromium-review.googlesource.com/644849
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499796}

--- chrome/browser/ui/views/apps/chrome_native_app_window_views_aura_ash.cc
+++ chrome/browser/ui/views/apps/chrome_native_app_window_views_aura_ash.cc
@@ -181,6 +181,10 @@ void ChromeNativeAppWindowViewsAuraAsh::OnBeforeWidgetInit(
   init_params->mus_properties
       [ui::mojom::WindowManager::kRemoveStandardFrame_InitProperty] =
       mojo::ConvertTo<std::vector<uint8_t>>(init_params->remove_standard_frame);
+  init_params
+      ->mus_properties[ui::mojom::WindowManager::kShelfItemType_Property] =
+      mojo::ConvertTo<std::vector<uint8_t>>(
+          static_cast<int64_t>(ash::TYPE_APP));
 }
 
 void ChromeNativeAppWindowViewsAuraAsh::OnBeforePanelWidgetInit(
@@ -195,6 +199,10 @@ void ChromeNativeAppWindowViewsAuraAsh::OnBeforePanelWidgetInit(
     wm::ConvertRectToScreen(ash::Shell::GetRootWindowForNewWindows(),
                             &init_params->bounds);
   }
+  init_params
+      ->mus_properties[ui::mojom::WindowManager::kShelfItemType_Property] =
+      mojo::ConvertTo<std::vector<uint8_t>>(
+          static_cast<int64_t>(ash::TYPE_APP_PANEL));
 }
 
 views::NonClientFrameView*
