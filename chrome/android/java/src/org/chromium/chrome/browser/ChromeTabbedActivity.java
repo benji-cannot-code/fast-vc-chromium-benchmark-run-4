d4e49b60a879a063c32a6c2e57a01631c7d5c20f
Matthew Jones <mdjones@chromium.org>
2017-09-06 23:36:23 +0000

    [Home] Do not intercept back button if in fullscreen mode
    While in fullscreen mode, the bottom sheet should not intercept the
    back button; exiting fullscreen should take priority. This change
    moves the fullscreen check in ChromeTabbedActivity so that it is handled
    before the bottom sheet has a chance to intercept it.
    
    BUG=762481
    
    Change-Id: I1def229ae8329b3028a7e9fe37b803ef3d1a3bf4
    Reviewed-on: https://chromium-review.googlesource.com/653178
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500111}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -1703,6 +1703,11 @@ public class ChromeTabbedActivity
         if (!mUIInitialized) return false;
         final Tab currentTab = getActivityTab();
 
+        if (exitFullscreenIfShowing()) {
+            recordBackPressedUma("Exited fullscreen", BACK_PRESSED_EXITED_FULLSCREEN);
+            return true;
+        }
+
         if (getBottomSheet() != null && getBottomSheet().handleBackPress()) return true;
 
         if (currentTab == null) {
@@ -1715,11 +1720,6 @@ public class ChromeTabbedActivity
         if (mLayoutManager.overviewVisible() && !isTablet()) {
             recordBackPressedUma("Hid overview", BACK_PRESSED_EXITED_TAB_SWITCHER);
             mLayoutManager.hideOverview(true);
-            return true;
-        }
-
-        if (exitFullscreenIfShowing()) {
-            recordBackPressedUma("Exited fullscreen", BACK_PRESSED_EXITED_FULLSCREEN);
             return true;
         }
 
