b62aab61a2f4dba0528dc8850f390537eff5cea2
Matt Falkenhagen <falken@chromium.org>
2017-09-06 14:04:00 +0000

    service worker: cleanup: Start migrating to OnceCallback in SWContext
    It's coming up more and more in reviews that SW code should be using
    OnceCallback instead of Callback.
    
    Change-Id: If9a371051cb2fb80fced3148fb0626e67cbe0eed
    Reviewed-on: https://chromium-review.googlesource.com/652009
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499955}

--- content/public/browser/service_worker_context.h
+++ content/public/browser/service_worker_context.h
@@ -52,7 +52,7 @@ class ServiceWorkerContext {
   // roughly, must be of the form "<origin>/<path>/*".
   using Scope = GURL;
 
-  using ResultCallback = base::Callback<void(bool success)>;
+  using ResultCallback = base::OnceCallback<void(bool success)>;
 
   using GetUsageInfoCallback = base::Callback<void(
       const std::vector<ServiceWorkerUsageInfo>& usage_info)>;
@@ -100,7 +100,7 @@ class ServiceWorkerContext {
   // be called on the UI thread.
   virtual void RegisterServiceWorker(const Scope& pattern,
                                      const GURL& script_url,
-                                     const ResultCallback& callback) = 0;
+                                     ResultCallback callback) = 0;
 
   // Equivalent to calling navigator.serviceWorker.unregister(pattern) from a
   // renderer, except that |pattern| is an absolute URL instead of relative to
@@ -114,7 +114,7 @@ class ServiceWorkerContext {
   // This function can be called from any thread, but the callback will always
   // be called on the UI thread.
   virtual void UnregisterServiceWorker(const Scope& pattern,
-                                       const ResultCallback& callback) = 0;
+                                       ResultCallback callback) = 0;
 
   // Mechanism for embedder to increment/decrement ref count of a service
   // worker.
@@ -142,7 +142,7 @@ class ServiceWorkerContext {
   // This function can be called from any thread, but the callback will always
   // be called on the IO thread.
   virtual void DeleteForOrigin(const GURL& origin_url,
-                               const ResultCallback& callback) = 0;
+                               ResultCallback callback) = 0;
 
   // Returns ServiceWorkerCapability describing existence and properties of a
   // Service Worker registration matching |url|. Found service worker
