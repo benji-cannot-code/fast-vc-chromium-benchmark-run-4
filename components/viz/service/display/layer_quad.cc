9eec363c6280f33a83e72c2923474444c5872cbb
Alex Zhang <staraz@chromium.org>
2017-09-06 23:41:40 +0000

    Move layer_quad* to components/viz/service/display
    Move LayerQuad and its unit tests to components/viz/service/display.
    All includes and namespaces are updated accordingly.
    
    Bug: 722935
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I2730f0b9843aa1cdfb2eb5bc8b2cd84e65644ad8
    Reviewed-on: https://chromium-review.googlesource.com/647830
    Reviewed-by: weiliangc <weiliangc@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Commit-Queue: Xingyu Zhang <staraz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500120}

--- components/viz/service/display/layer_quad.cc
+++ components/viz/service/display/layer_quad.cc
@@ -3,14 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "cc/output/layer_quad.h"
+#include "components/viz/service/display/layer_quad.h"
 
 #include <stddef.h>
 
 #include "base/logging.h"
 #include "ui/gfx/geometry/quad_f.h"
 
-namespace cc {
+namespace viz {
 
 LayerQuad::Edge::Edge(const gfx::PointF& p, const gfx::PointF& q) {
   if (p == q) {
@@ -51,10 +51,7 @@ LayerQuad::LayerQuad(const Edge& left,
                      const Edge& top,
                      const Edge& right,
                      const Edge& bottom)
-    : left_(left),
-      top_(top),
-      right_(right),
-      bottom_(bottom) {}
+    : left_(left), top_(top), right_(right), bottom_(bottom) {}
 
 gfx::QuadF LayerQuad::ToQuadF() const {
   size_t num_degenerate_edges = left_.degenerate() + right_.degenerate() +
@@ -79,10 +76,8 @@ gfx::QuadF LayerQuad::ToQuadF() const {
     return gfx::QuadF(left_.Intersect(top_), top_.Intersect(right_),
                       right_.Intersect(left_), left_.Intersect(right_));
   }
-  return gfx::QuadF(left_.Intersect(top_),
-                    top_.Intersect(right_),
-                    right_.Intersect(bottom_),
-                    bottom_.Intersect(left_));
+  return gfx::QuadF(left_.Intersect(top_), top_.Intersect(right_),
+                    right_.Intersect(bottom_), bottom_.Intersect(left_));
 }
 
 void LayerQuad::ToFloatArray(float flattened[12]) const {
@@ -124,4 +119,4 @@ void LayerQuad::ToFloatArray(float flattened[12]) const {
   }
 }
 
-}  // namespace cc
+}  // namespace viz
--- components/viz/service/display/layer_quad_unittest.cc
+++ components/viz/service/display/layer_quad_unittest.cc
@@ -3,12 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "cc/output/layer_quad.h"
+#include "components/viz/service/display/layer_quad.h"
 
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/geometry/quad_f.h"
 
-namespace cc {
+namespace viz {
 namespace {
 
 TEST(LayerQuadTest, QuadFConversion) {
@@ -67,4 +67,4 @@ TEST(LayerQuadTest, Degenerate) {
 }
 
 }  // namespace
-}  // namespace cc
+}  // namespace viz
