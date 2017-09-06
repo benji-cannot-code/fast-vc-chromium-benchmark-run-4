c188f1cf5e50c0875ce2d0624b5f6948b96f52d9
Chandan Padhi <c.padhi@samsung.com>
2017-09-06 15:57:08 +0000

    Remove StreamDeviceInfo from MediaStreamDispatcher
    This is one of the many CLs aimed at removal of StreamDeviceInfo.
    
    Bug: 760493
    Change-Id: If2a7b780469849f5c08b19e683af303fd258f7f9
    Reviewed-on: https://chromium-review.googlesource.com/649591
    Commit-Queue: Chandan Padhi <c.padhi@samsung.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499976}

--- content/renderer/media/media_stream_dispatcher.h
+++ content/renderer/media/media_stream_dispatcher.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_checker.h"
 #include "content/common/content_export.h"
 #include "content/common/media/media_stream.mojom.h"
-#include "content/common/media/media_stream_options.h"
+#include "content/public/common/media_stream_request.h"
 #include "content/public/renderer/render_frame_observer.h"
 #include "mojo/public/cpp/bindings/binding.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
@@ -59,7 +59,7 @@ class CONTENT_EXPORT MediaStreamDispatcher
       const base::WeakPtr<MediaStreamDispatcherEventHandler>& event_handler);
 
   // Stop a started device that has been requested by calling GenerateStream.
-  virtual void StopStreamDevice(const StreamDeviceInfo& device_info);
+  virtual void StopStreamDevice(const MediaStreamDevice& device);
 
   // Request to open a device.
   void OpenDevice(
@@ -118,16 +118,16 @@ class CONTENT_EXPORT MediaStreamDispatcher
   // mojom::MediaStreamDispatcher implementation.
   void OnStreamGenerated(int32_t request_id,
                          const std::string& label,
-                         const StreamDeviceInfoArray& audio_array,
-                         const StreamDeviceInfoArray& video_array) override;
+                         const MediaStreamDevices& audio_devices,
+                         const MediaStreamDevices& video_devices) override;
   void OnStreamGenerationFailed(int32_t request_id,
                                 MediaStreamRequestResult result) override;
   void OnDeviceOpened(int32_t request_id,
                       const std::string& label,
-                      const StreamDeviceInfo& device_info) override;
+                      const MediaStreamDevice& device) override;
   void OnDeviceOpenFailed(int32_t request_id) override;
   void OnDeviceStopped(const std::string& label,
-                       const StreamDeviceInfo& device_info) override;
+                       const MediaStreamDevice& device) override;
 
   void BindMediaStreamDispatcherRequest(
       mojom::MediaStreamDispatcherRequest request);
--- content/renderer/media/user_media_client_impl.cc
+++ content/renderer/media/user_media_client_impl.cc
@@ -826,14 +826,24 @@ void UserMediaClientImpl::OnStreamGeneratedForCancelledRequest(
   // Only stop the device if the device is not used in another MediaStream.
   for (StreamDeviceInfoArray::const_iterator device_it = audio_array.begin();
        device_it != audio_array.end(); ++device_it) {
-    if (!FindLocalSource(*device_it))
-      media_stream_dispatcher_->StopStreamDevice(*device_it);
+    if (!FindLocalSource(*device_it)) {
+      // TODO(c.padhi): Remove this when |audio_array|'s type is
+      // changed to MediaStreamDevices, see https://crbug.com/760493.
+      MediaStreamDevice audio_device = device_it->device;
+      audio_device.session_id = device_it->session_id;
+      media_stream_dispatcher_->StopStreamDevice(audio_device);
+    }
   }
 
   for (StreamDeviceInfoArray::const_iterator device_it = video_array.begin();
        device_it != video_array.end(); ++device_it) {
-    if (!FindLocalSource(*device_it))
-      media_stream_dispatcher_->StopStreamDevice(*device_it);
+    if (!FindLocalSource(*device_it)) {
+      // TODO(c.padhi): Remove this when |video_array|'s type is
+      // changed to MediaStreamDevices, see https://crbug.com/760493.
+      MediaStreamDevice video_device = device_it->device;
+      video_device.session_id = device_it->session_id;
+      media_stream_dispatcher_->StopStreamDevice(video_device);
+    }
   }
 }
 
@@ -1436,8 +1446,7 @@ void UserMediaClientImpl::OnLocalSourceStopped(
 
   MediaStreamSource* source_impl =
       static_cast<MediaStreamSource*>(source.GetExtraData());
-  media_stream_dispatcher_->StopStreamDevice(
-      StreamDeviceInfo(source_impl->device()));
+  media_stream_dispatcher_->StopStreamDevice(source_impl->device());
 }
 
 void UserMediaClientImpl::StopLocalSource(
@@ -1449,8 +1458,7 @@ void UserMediaClientImpl::StopLocalSource(
            << "{device_id = " << source_impl->device().id << "})";
 
   if (notify_dispatcher) {
-    media_stream_dispatcher_->StopStreamDevice(
-        StreamDeviceInfo(source_impl->device()));
+    media_stream_dispatcher_->StopStreamDevice(source_impl->device());
   }
 
   source_impl->ResetSourceStoppedCallback();
