54afe97b371a1570b793b6ccc6067091d30f4754
Siddhartha <ssid@chromium.org>
2017-09-06 21:35:37 +0000

    LevelDatabaseImpl does not create allocator dumps under "leveldb"
    LevelDatabaseImpl just uses the dump created by LevelDB tracker to create
    global dump edges.
    
    BUG=735269
    
    Change-Id: If4d87635bd69704460a20e2a8be17f3dc773e2c6
    Reviewed-on: https://chromium-review.googlesource.com/644498
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Elliot Glaysher <erg@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Chris Mumford <cmumford@chromium.org>
    Reviewed-by: Michael Nordman <michaeln@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500097}

--- third_party/leveldatabase/env_chromium.h
+++ third_party/leveldatabase/env_chromium.h
@@ -23,6 +23,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "port/port_chromium.h"
 #include "util/mutexlock.h"
 
+namespace base {
+namespace trace_event {
+class MemoryAllocatorDump;
+class ProcessMemoryDump;
+}  // namespace trace_event
+}  // namespace base
+
 namespace leveldb_env {
 
 // These entries map to values in tools/metrics/histograms/histograms.xml. New
@@ -249,12 +256,14 @@ class DBTracker {
   // DBTracker singleton instance.
   static DBTracker* GetInstance();
 
-  // Returns name of memory-infra dump for |tracked_db|. Can be used to attach
+  // Returns the memory-infra dump for |tracked_db|. Can be used to attach
   // additional info to the database dump, or to properly attribute memory
   // usage in memory dump providers that also dump |tracked_db|.
   // Note that |tracked_db| should be a live database instance produced by
   // OpenDatabase() method or leveldb_env::OpenDB() function.
-  static std::string GetMemoryDumpName(leveldb::DB* tracked_db);
+  static base::trace_event::MemoryAllocatorDump* GetOrCreateAllocatorDump(
+      base::trace_event::ProcessMemoryDump* pmd,
+      leveldb::DB* tracked_db);
 
   // Provides extra information about a tracked database.
   class TrackedDB : public leveldb::DB {
