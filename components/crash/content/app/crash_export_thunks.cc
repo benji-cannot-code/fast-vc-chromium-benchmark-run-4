9cf870fbcfa7656d5fae96ba158d7e0562c44ce3
Sigurdur Asgeirsson <siggi@chromium.org>
2017-09-06 20:13:07 +0000

    Pass crash key and value length through export thunks.
    This uses the StringPiece interface to crash keys which appeared
    in Crashpad 20ed4146d37705a12a5b603a89c97f206f58d14c. With this change,
    it is safe to use non-NUL terminated StringPiece crash keys and values.
    
    Bug: 758724
    Change-Id: I9c1630cdef9d5393dbb9eb50041eeb166f99a680
    Reviewed-on: https://chromium-review.googlesource.com/652942
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500059}

--- components/crash/content/app/crash_export_thunks.cc
+++ components/crash/content/app/crash_export_thunks.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <type_traits>
 
+#include "base/strings/string_piece.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "components/crash/content/app/crashpad.h"
@@ -66,12 +67,16 @@ void ClearCrashKeyValue_ExportThunk(const wchar_t* key) {
   crash_reporter::ClearCrashKey(base::UTF16ToUTF8(key));
 }
 
-void SetCrashKeyValueEx_ExportThunk(const char* key, const char* value) {
-  crash_reporter::SetCrashKeyValue(key, value);
+void SetCrashKeyValueEx_ExportThunk(const char* key,
+                                    size_t key_len,
+                                    const char* value,
+                                    size_t value_len) {
+  crash_reporter::SetCrashKeyValue(base::StringPiece(key, key_len),
+                                   base::StringPiece(value, value_len));
 }
 
-void ClearCrashKeyValueEx_ExportThunk(const char* key) {
-  crash_reporter::ClearCrashKey(key);
+void ClearCrashKeyValueEx_ExportThunk(const char* key, size_t key_len) {
+  crash_reporter::ClearCrashKey(base::StringPiece(key, key_len));
 }
 
 HANDLE InjectDumpForHungInput_ExportThunk(HANDLE process,
