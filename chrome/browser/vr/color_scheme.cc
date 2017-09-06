6f60b0cc95705b2df21dbfc3e82322b5471a4a36
Yash Malik <ymalik@google.com>
2017-09-06 23:44:01 +0000

    [vr] Make splash screen text viewport aware and other minor UX tweaks
    This CL also makes the splash screen 65% white instead of pure white.
    
    Bug: 
    Change-Id: I1281bce6068b05888b6aebbb8c38191e774a22d6
    Reviewed-on: https://chromium-review.googlesource.com/653811
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Yash Malik <ymalik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500124}

--- chrome/browser/vr/color_scheme.cc
+++ chrome/browser/vr/color_scheme.cc
@@ -66,7 +66,7 @@ void InitializeColorSchemes() {
   normal_scheme.dimmer_inner = 0xCC0D0D0D;
   normal_scheme.dimmer_outer = 0xE6000000;
   normal_scheme.splash_screen_background = SK_ColorBLACK;
-  normal_scheme.splash_screen_text_color = SK_ColorWHITE;
+  normal_scheme.splash_screen_text_color = 0xA6FFFFFF;
 
   gColorSchemes[ColorScheme::kModeFullscreen] =
       gColorSchemes[ColorScheme::kModeNormal];
