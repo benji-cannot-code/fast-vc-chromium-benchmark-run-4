cc0c6afb91f9f9fc009e8ab7ea749d4146f49a6d
Sunny Sachanandani <sunnyps@chromium.org>
2017-09-06 02:01:21 +0000

    cc: Flush after every tile task set completes.
    Flushing in PrepareSendToParent isn't sufficient because that's skipped
    when there's nothing to draw, and there can be nothing to draw if we
    haven't flushed.
    
    Flushing after all tiles are done is too late. The all tile set can be
    very large for even simple pages.
    
    Flushing after every task set and in PrepareSendToParent is reasonable
    and seems to work well.
    
    BUG=760886
    R=piman
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I4915e8d10137d0c882c20464bce7563582691342
    Reviewed-on: https://chromium-review.googlesource.com/651342
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499845}

--- cc/tiles/tile_manager.cc
+++ cc/tiles/tile_manager.cc
@@ -547,7 +547,7 @@ void TileManager::CheckForCompletedTasks() {
   tile_task_manager_->CheckForCompletedTasks();
   did_check_for_completed_tasks_since_last_schedule_tasks_ = true;
 
-  CheckPendingGpuWorkTiles(true /* issue_signals */);
+  CheckPendingGpuWorkTiles(true /* issue_signals */, false /* flush */);
 
   TRACE_EVENT_INSTANT1(
       "cc", "TileManager::CheckForCompletedTasksFinished",
@@ -1319,7 +1319,7 @@ void TileManager::CheckAndIssueSignals() {
   tile_task_manager_->CheckForCompletedTasks();
   did_check_for_completed_tasks_since_last_schedule_tasks_ = true;
 
-  CheckPendingGpuWorkTiles(false /* issue_signals */);
+  CheckPendingGpuWorkTiles(false /* issue_signals */, true /* flush */);
 
   // Ready to activate.
   if (signals_.ready_to_activate && !signals_.did_notify_ready_to_activate) {
@@ -1401,8 +1401,6 @@ void TileManager::CheckIfMoreTilesNeedToBePrepared() {
 
   resource_pool_->ReduceResourceUsage();
   image_controller_.ReduceMemoryUsage();
-
-  raster_buffer_provider_->Flush();
 
   // TODO(vmpstr): Temporary check to debug crbug.com/642927.
   CHECK(tile_task_manager_);
@@ -1507,11 +1505,14 @@ bool TileManager::UsePartialRaster() const {
          raster_buffer_provider_->CanPartialRasterIntoProvidedResource();
 }
 
-void TileManager::CheckPendingGpuWorkTiles(bool issue_signals) {
+void TileManager::CheckPendingGpuWorkTiles(bool issue_signals, bool flush) {
   TRACE_EVENT2("cc", "TileManager::CheckPendingGpuWorkTiles",
                "pending_gpu_work_tiles", pending_gpu_work_tiles_.size(),
                "tree_priority",
                TreePriorityToString(global_state_.tree_priority));
+
+  if (flush)
+    raster_buffer_provider_->Flush();
 
   ResourceProvider::ResourceIdArray required_for_activation_ids;
   ResourceProvider::ResourceIdArray required_for_draw_ids;
@@ -1551,7 +1552,7 @@ void TileManager::CheckPendingGpuWorkTiles(bool issue_signals) {
             required_for_activation_ids,
             base::Bind(&TileManager::CheckPendingGpuWorkTiles,
                        ready_to_draw_callback_weak_ptr_factory_.GetWeakPtr(),
-                       true /* issue_signals */),
+                       true /* issue_signals */, false /* flush */),
             pending_required_for_activation_callback_id_);
   }
 
@@ -1562,7 +1563,7 @@ void TileManager::CheckPendingGpuWorkTiles(bool issue_signals) {
             required_for_draw_ids,
             base::Bind(&TileManager::CheckPendingGpuWorkTiles,
                        ready_to_draw_callback_weak_ptr_factory_.GetWeakPtr(),
-                       true /* issue_signals */),
+                       true /* issue_signals */, false /* flush */),
             pending_required_for_draw_callback_id_);
   }
 
--- cc/tiles/tile_manager.h
+++ cc/tiles/tile_manager.h
@@ -373,7 +373,7 @@ class CC_EXPORT TileManager : CheckerImageTrackerClient {
 
   bool UsePartialRaster() const;
 
-  void CheckPendingGpuWorkTiles(bool issue_signals);
+  void CheckPendingGpuWorkTiles(bool issue_signals, bool flush);
 
   TileManagerClient* client_;
   base::SequencedTaskRunner* task_runner_;
