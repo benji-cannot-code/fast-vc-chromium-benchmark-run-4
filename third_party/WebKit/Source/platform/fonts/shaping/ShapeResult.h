2bc09c42ce0d895e680bd0648bc446743c4b8257
Koji Ishii <kojii@chromium.org>
2017-09-06 07:49:06 +0000

    [LayoutNG] Implement 'text-align: justify'
    This patch implements 'text-align: justify'.
    
    ShapeResult::ApplySpacing() is changed to use index instead of sub-run
    to make the interface simpler and possible future optimizations.
    
    Not supported yet:
    - Atomic inlines.
    - Some tests do not match due to rounding errors. Needs investigation.
    - Justification for 'white-space: pre-wrap' has a spec issue.
    - Leading expansion is supported for NG Paint, but not for
      InlineTextBox (justify-ideograph-leading-expansion.html).
    
    Bug: 636993
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I2818546fd996ced6b4e751670e9af0eeba964559
    Reviewed-on: https://chromium-review.googlesource.com/644596
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499899}

--- third_party/WebKit/Source/platform/fonts/shaping/ShapeResult.h
+++ third_party/WebKit/Source/platform/fonts/shaping/ShapeResult.h
@@ -66,6 +66,10 @@ class PLATFORM_EXPORT ShapeResult : public RefCounted<ShapeResult> {
       unsigned count);
   ~ShapeResult();
 
+  // Returns a mutalbe unique instance. If |this| has more than 1 ref count,
+  // a clone is created.
+  RefPtr<ShapeResult> MutableUnique() const;
+
   // The logical width of this result.
   float Width() const { return width_; }
   LayoutUnit SnappedWidth() const { return LayoutUnit::FromFloatCeil(width_); }
@@ -100,7 +104,12 @@ class PLATFORM_EXPORT ShapeResult : public RefCounted<ShapeResult> {
     return LayoutUnit::FromFloatCeil(PositionForOffset(offset));
   }
 
-  void ApplySpacing(ShapeResultSpacing<String>&);
+  // Apply spacings (letter-spacing, word-spacing, and justification) as
+  // configured to |ShapeResultSpacing|.
+  // |text_start_offset| adjusts the character index in the ShapeResult before
+  // giving it to |ShapeResultSpacing|. It can be negative if
+  // |StartIndexForResult()| is larger than the text in |ShapeResultSpacing|.
+  void ApplySpacing(ShapeResultSpacing<String>&, int text_start_offset = 0);
   PassRefPtr<ShapeResult> ApplySpacingToCopy(ShapeResultSpacing<TextRun>&,
                                              const TextRun&) const;
 
@@ -117,8 +126,8 @@ class PLATFORM_EXPORT ShapeResult : public RefCounted<ShapeResult> {
   }
 
   template <typename TextContainerType>
-  void ApplySpacing(ShapeResultSpacing<TextContainerType>&,
-                    const TextContainerType&);
+  void ApplySpacingImpl(ShapeResultSpacing<TextContainerType>&,
+                        int text_start_offset = 0);
   template <bool is_horizontal_run>
   void ComputeGlyphPositions(ShapeResult::RunInfo*,
                              unsigned start_glyph,
--- third_party/WebKit/Source/platform/text/TextRun.h
+++ third_party/WebKit/Source/platform/text/TextRun.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/text/TextDirection.h"
 #include "platform/text/TextJustify.h"
 #include "platform/wtf/Allocator.h"
+#include "platform/wtf/Optional.h"
 #include "platform/wtf/text/StringView.h"
 #include "platform/wtf/text/WTFString.h"
 
@@ -150,6 +151,10 @@ class PLATFORM_EXPORT TextRun final {
     result.SetText(Data16(start_offset), length);
     return result;
   }
+
+  // Returns the start index of a sub run if it was created by |SubRun|.
+  // std::numeric_limits<unsigned>::max() if not a sub run.
+  unsigned IndexOfSubRun(const TextRun&) const;
 
   UChar operator[](unsigned i) const {
     SECURITY_DCHECK(i < len_);
