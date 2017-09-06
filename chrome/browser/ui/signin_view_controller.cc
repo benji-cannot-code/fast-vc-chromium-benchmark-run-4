cd34516bf2ec5431e25a4efe171fe1bc7639dd64
Mihai Sardarescu <msarda@chromium.org>
2017-09-06 11:17:40 +0000

    Avoid calling PerformNavigation when the delegate is null.
    This CL avoid calling PerformNavigation on the delegate when it is null.
    
    It also makes the SigninViewController's delegate private to avoid
    it being accessed by other classes and exposing it to similar crashes.
    
    Bug: 758830
    Change-Id: Ic778de6957115855273e6137721b0c0ace1315a6
    Reviewed-on: https://chromium-review.googlesource.com/647889
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499929}

--- chrome/browser/ui/signin_view_controller.cc
+++ chrome/browser/ui/signin_view_controller.cc
@@ -8,8 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_dialogs.h"
 #include "chrome/browser/ui/signin_view_controller_delegate.h"
 
-SigninViewController::SigninViewController()
-    : signin_view_controller_delegate_(nullptr) {}
+SigninViewController::SigninViewController() : delegate_(nullptr) {}
 
 SigninViewController::~SigninViewController() {
   CloseModalSignin();
@@ -22,14 +21,13 @@ void SigninViewController::ShowModalSignin(
   CloseModalSignin();
   // The delegate will delete itself on request of the UI code when the widget
   // is closed.
-  signin_view_controller_delegate_ =
-      SigninViewControllerDelegate::CreateModalSigninDelegate(
-          this, mode, browser, access_point);
+  delegate_ = SigninViewControllerDelegate::CreateModalSigninDelegate(
+      this, mode, browser, access_point);
 
   // When the user has a proxy that requires HTTP auth, loading the sign-in
   // dialog can trigger the HTTP auth dialog.  This means the signin view
   // controller needs a dialog manager to handle any such dialog.
-  signin_view_controller_delegate_->AttachDialogManager();
+  delegate_->AttachDialogManager();
   chrome::RecordDialogCreation(chrome::DialogIdentifier::SIGN_IN);
 }
 
@@ -37,9 +35,8 @@ void SigninViewController::ShowModalSyncConfirmationDialog(Browser* browser) {
   CloseModalSignin();
   // The delegate will delete itself on request of the UI code when the widget
   // is closed.
-  signin_view_controller_delegate_ =
-      SigninViewControllerDelegate::CreateSyncConfirmationDelegate(this,
-                                                                   browser);
+  delegate_ = SigninViewControllerDelegate::CreateSyncConfirmationDelegate(
+      this, browser);
   chrome::RecordDialogCreation(
       chrome::DialogIdentifier::SIGN_IN_SYNC_CONFIRMATION);
 }
@@ -48,25 +45,34 @@ void SigninViewController::ShowModalSigninErrorDialog(Browser* browser) {
   CloseModalSignin();
   // The delegate will delete itself on request of the UI code when the widget
   // is closed.
-  signin_view_controller_delegate_ =
+  delegate_ =
       SigninViewControllerDelegate::CreateSigninErrorDelegate(this, browser);
   chrome::RecordDialogCreation(chrome::DialogIdentifier::SIGN_IN_ERROR);
 }
 
-void SigninViewController::CloseModalSignin() {
-  if (signin_view_controller_delegate_)
-    signin_view_controller_delegate_->CloseModalSignin();
+bool SigninViewController::ShowsModalDialog() {
+  return delegate_ != nullptr;
+}
 
-  DCHECK(!signin_view_controller_delegate_);
+void SigninViewController::CloseModalSignin() {
+  if (delegate_)
+    delegate_->CloseModalSignin();
+
+  DCHECK(!delegate_);
 }
 
 void SigninViewController::SetModalSigninHeight(int height) {
-  if (signin_view_controller_delegate_)
-    signin_view_controller_delegate_->ResizeNativeView(height);
+  if (delegate_)
+    delegate_->ResizeNativeView(height);
+}
+
+void SigninViewController::PerformNavigation() {
+  if (delegate_)
+    delegate_->PerformNavigation();
 }
 
 void SigninViewController::ResetModalSigninDelegate() {
-  signin_view_controller_delegate_ = nullptr;
+  delegate_ = nullptr;
 }
 
 // static
@@ -75,4 +81,10 @@ bool SigninViewController::ShouldShowModalSigninForMode(
   return mode == profiles::BUBBLE_VIEW_MODE_GAIA_SIGNIN ||
          mode == profiles::BUBBLE_VIEW_MODE_GAIA_ADD_ACCOUNT ||
          mode == profiles::BUBBLE_VIEW_MODE_GAIA_REAUTH;
+}
+
+content::WebContents*
+SigninViewController::GetModalDialogWebContentsForTesting() {
+  DCHECK(delegate_);
+  return delegate_->web_contents();
 }
--- chrome/browser/ui/signin_view_controller.h
+++ chrome/browser/ui/signin_view_controller.h
@@ -12,6 +12,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class Browser;
 class SigninViewControllerDelegate;
 
+namespace content {
+class WebContents;
+}
+
+namespace login_ui_test_utils {
+class SigninViewControllerTestUtil;
+}
+
 namespace signin_metrics {
 enum class AccessPoint;
 }
@@ -36,6 +44,9 @@ class SigninViewController {
   void ShowModalSyncConfirmationDialog(Browser* browser);
   void ShowModalSigninErrorDialog(Browser* browser);
 
+  // Returns true if the modal dialog is shown.
+  bool ShowsModalDialog();
+
   // Closes the tab-modal signin flow previously shown using this
   // SigninViewController, if one exists. Does nothing otherwise.
   void CloseModalSignin();
@@ -43,16 +54,21 @@ class SigninViewController {
   // Sets the height of the modal signin dialog.
   void SetModalSigninHeight(int height);
 
-  // Notifies this object that it's |signin_view_controller_delegate_|
-  // member has become invalid.
+  // Either navigates back in the signin flow if the history state allows it or
+  // closes the flow otherwise.
+  // Does nothing if the signin flow does not exist.
+  void PerformNavigation();
+
+  // Notifies this object that it's |delegate_| member has become invalid.
   void ResetModalSigninDelegate();
 
-  SigninViewControllerDelegate* delegate() {
-    return signin_view_controller_delegate_;
-  }
-
  private:
-  SigninViewControllerDelegate* signin_view_controller_delegate_;
+  friend class login_ui_test_utils::SigninViewControllerTestUtil;
+
+  // Returns the web contents of the modal dialog.
+  content::WebContents* GetModalDialogWebContentsForTesting();
+
+  SigninViewControllerDelegate* delegate_;
 
   DISALLOW_COPY_AND_ASSIGN(SigninViewController);
 };
