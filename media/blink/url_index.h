06198e92b171cad8668b348acc9487b20eea4563
Fredrik Hubinette <hubbe@google.com>
2017-09-06 23:50:19 +0000

    media: preload more, free less, respond to memory pressure
    This introdces the idea that media that has been watched for a while deserves a larger buffer.
    After 100 seconds, this reaches the maximum, where the preload will be twice as much.
    We also wait longer before we free data, which allows short seeks into the past to respond faster.
    Most of this data can be freed on demand by a memory pressure callback.
    
    Bug: 758246
    Change-Id: Ia7be3ddbe70fc50bfed8970fb3b822a998294a22
    Reviewed-on: https://chromium-review.googlesource.com/646800
    Commit-Queue: Fredrik Hubinette <hubbe@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500135}

--- media/blink/url_index.h
+++ media/blink/url_index.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/macros.h"
+#include "base/memory/memory_pressure_listener.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
 #include "base/threading/thread_checker.h"
@@ -256,6 +257,9 @@ class MEDIA_BLINK_EXPORT UrlIndex {
   virtual scoped_refptr<UrlData> NewUrlData(const GURL& url,
                                             UrlData::CORSMode cors_mode);
 
+  void OnMemoryPressure(
+      base::MemoryPressureListener::MemoryPressureLevel memory_pressure_level);
+
   ResourceFetchContext* fetch_context_;
   using UrlDataMap = std::map<UrlData::KeyType, scoped_refptr<UrlData>>;
   UrlDataMap unindexed_data_;
@@ -265,6 +269,8 @@ class MEDIA_BLINK_EXPORT UrlIndex {
   // log2 of block size in multibuffer cache. Defaults to kBlockSizeShift.
   // Currently only changed for testing purposes.
   const int block_shift_;
+
+  base::MemoryPressureListener memory_pressure_listener_;
 };
 
 }  // namespace media
