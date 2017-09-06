f75e6025345f8bdb8dd8bc1d8b475f5dcddbc6df
Fady Samuel <fsamuel@chromium.org>
2017-09-06 23:51:44 +0000

    Surface synchronization: Simplify flags
    Add kEnableSurfaceSynchronization to the CommandLine if running in Mus
    mode. That way, we don't need to check IsRunningInMash all over the place.
    
    Bug: 651108
    Change-Id: I386585347223f5e40f3bfdd0bfbd28a23f52ccc2
    Reviewed-on: https://chromium-review.googlesource.com/653805
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Fady Samuel <fsamuel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500137}

--- content/renderer/browser_plugin/browser_plugin.cc
+++ content/renderer/browser_plugin/browser_plugin.cc
@@ -60,11 +60,6 @@ using PluginContainerMap =
 static base::LazyInstance<PluginContainerMap>::DestructorAtExit
     g_plugin_container_map = LAZY_INSTANCE_INITIALIZER;
 
-bool IsRunningInMash() {
-  const base::CommandLine* cmdline = base::CommandLine::ForCurrentProcess();
-  return cmdline->HasSwitch(switches::kIsRunningInMash);
-}
-
 }  // namespace
 
 namespace content {
@@ -103,7 +98,6 @@ BrowserPlugin::BrowserPlugin(
   const base::CommandLine& command_line =
       *base::CommandLine::ForCurrentProcess();
   enable_surface_synchronization_ =
-      IsRunningInMash() ||
       command_line.HasSwitch(switches::kEnableSurfaceSynchronization);
 }
 
