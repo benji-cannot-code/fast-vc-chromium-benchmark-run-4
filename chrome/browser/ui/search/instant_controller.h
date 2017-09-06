17c40f49a8bd414f5d0f6a1ba6db0c607c3b7eb8
Marc Treib <treib@chromium.org>
2017-09-06 07:48:25 +0000

    Cleanup: Replace InstantTab by a private helper in instant_controller.cc
    This also lets us replace InstantTab::Delegate by a simple callback.
    
    Bug: 627747
    Change-Id: I44fa199c1dcb323619de1cb7faa198b2ad308de7
    Reviewed-on: https://chromium-review.googlesource.com/643507
    Reviewed-by: Chris Pickel <sfiera@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499898}

--- chrome/browser/ui/search/instant_controller.h
+++ chrome/browser/ui/search/instant_controller.h
@@ -15,14 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
-#include "chrome/browser/ui/search/instant_tab.h"
 #include "chrome/browser/ui/search/search_model.h"
 
 class BrowserInstantController;
-
-namespace content {
-class WebContents;
-}
 
 // InstantController is responsible for updating the theme and most visited info
 // of the current tab when
@@ -30,18 +25,18 @@ class WebContents;
 // * an open tab navigates to an NTP.
 //
 // InstantController is owned by Browser via BrowserInstantController.
-class InstantController : public InstantTab::Delegate {
+class InstantController {
  public:
   explicit InstantController(BrowserInstantController* browser);
-  ~InstantController() override;
+  ~InstantController();
 
-  // The search mode in the active tab has changed. Bind |instant_tab_| if the
-  // |new_mode| reflects an Instant NTP.
+  // The search mode in the active tab has changed. Bind |instant_tab_observer_|
+  // if the |new_origin| reflects an Instant NTP.
   void SearchModeChanged(SearchModel::Origin old_origin,
                          SearchModel::Origin new_origin);
 
-  // The user switched tabs. Bind |instant_tab_| if the newly active tab is an
-  // Instant NTP.
+  // The user switched tabs. Bind |instant_tab_observer_| if the newly active
+  // tab is an Instant NTP.
   void ActiveTabChanged();
 
   // Resets list of debug events.
@@ -53,6 +48,8 @@ class InstantController : public InstantTab::Delegate {
   }
 
  private:
+  class TabObserver;
+
   FRIEND_TEST_ALL_PREFIXES(InstantExtendedTest,
                            SearchDoesntReuseInstantTabWithoutSupport);
   FRIEND_TEST_ALL_PREFIXES(InstantExtendedTest,
@@ -60,11 +57,7 @@ class InstantController : public InstantTab::Delegate {
   FRIEND_TEST_ALL_PREFIXES(InstantExtendedTest,
                            DispatchMVChangeEventWhileNavigatingBackToNTP);
 
-  // Overridden from InstantTab::Delegate:
-  // TODO(shishir): We assume that the WebContent's current RenderViewHost is
-  // the RenderViewHost being created which is not always true. Fix this.
-  void InstantTabAboutToNavigateMainFrame(const content::WebContents* contents,
-                                          const GURL& url) override;
+  void InstantTabAboutToNavigateMainFrame();
 
   // Adds a new event to |debug_events_| and also DVLOG's it. Ensures that
   // |debug_events_| doesn't get too large.
@@ -79,8 +72,8 @@ class InstantController : public InstantTab::Delegate {
 
   BrowserInstantController* const browser_;
 
-  // The instance of InstantTab maintained by InstantController.
-  std::unique_ptr<InstantTab> instant_tab_;
+  // Only non-null if the current tab is an Instant tab, i.e. an NTP.
+  std::unique_ptr<TabObserver> instant_tab_observer_;
 
   // The search model mode for the active tab.
   SearchModel::Origin search_origin_;
