d99155550ab29ee18d3f5d9fdedef1ee56701102
Tarun Bansal <tbansal@chromium.org>
2017-09-06 18:35:08 +0000

    Add Kbps estimate to UKM
    Add downlink throughput estimate (in Kbps) obtained
    from the Network Quality Estimator (NQE) to the
    UKM reports.
    
    Bug: 725146
    Change-Id: Id8acd185db5f3e746d408ce9b008377d7c338a8e
    Reviewed-on: https://chromium-review.googlesource.com/648947
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500018}

--- chrome/browser/page_load_metrics/observers/ukm_page_load_metrics_observer_unittest.cc
+++ chrome/browser/page_load_metrics/observers/ukm_page_load_metrics_observer_unittest.cc
@@ -34,6 +34,7 @@ class MockNetworkQualityProvider : public net::NetworkQualityProvider {
                      net::EffectiveConnectionType());
   MOCK_CONST_METHOD0(GetHttpRTT, base::Optional<base::TimeDelta>());
   MOCK_CONST_METHOD0(GetTransportRTT, base::Optional<base::TimeDelta>());
+  MOCK_CONST_METHOD0(GetDownstreamThroughputKbps, base::Optional<int32_t>());
 };
 
 }  // namespace
@@ -60,6 +61,10 @@ class UkmPageLoadMetricsObserverTest
     EXPECT_CALL(mock_network_quality_provider_, GetTransportRTT())
         .Times(AnyNumber())
         .WillRepeatedly(Return(base::Optional<base::TimeDelta>()));
+
+    EXPECT_CALL(mock_network_quality_provider_, GetDownstreamThroughputKbps())
+        .Times(AnyNumber())
+        .WillRepeatedly(Return(base::Optional<int32_t>()));
   }
 
   MockNetworkQualityProvider& mock_network_quality_provider() {
@@ -258,6 +263,8 @@ TEST_F(UkmPageLoadMetricsObserverTest, NetworkQualityEstimates) {
       .WillRepeatedly(Return(base::TimeDelta::FromMilliseconds(100)));
   EXPECT_CALL(mock_network_quality_provider(), GetTransportRTT())
       .WillRepeatedly(Return(base::TimeDelta::FromMilliseconds(200)));
+  EXPECT_CALL(mock_network_quality_provider(), GetDownstreamThroughputKbps())
+      .WillRepeatedly(Return(300));
 
   NavigateAndCommit(GURL(kTestUrl1));
 
@@ -277,6 +284,8 @@ TEST_F(UkmPageLoadMetricsObserverTest, NetworkQualityEstimates) {
                                    internal::kUkmHttpRttEstimate, 100);
   test_ukm_recorder().ExpectMetric(*source, internal::kUkmPageLoadEventName,
                                    internal::kUkmTransportRttEstimate, 200);
+  test_ukm_recorder().ExpectMetric(*source, internal::kUkmPageLoadEventName,
+                                   internal::kUkmDownstreamKbpsEstimate, 300);
 }
 
 TEST_F(UkmPageLoadMetricsObserverTest, PageTransitionReload) {
