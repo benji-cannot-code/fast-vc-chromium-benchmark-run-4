a8c557ff86bb43497f4b11ee4350e7e5d2aee5c9
Fredrik Hubinette <hubbe@google.com>
2017-09-06 17:50:25 +0000

    Create more threads as needed
    Also add tests which loads many videos.
    
    Bug: 
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I7a745e57b2ed43011f414f4806827cc23915fb11
    Reviewed-on: https://chromium-review.googlesource.com/646966
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Fredrik Hubinette <hubbe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500007}

--- media/filters/blocking_url_protocol.cc
+++ media/filters/blocking_url_protocol.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/macros.h"
+#include "base/threading/scoped_blocking_call.h"
 #include "media/base/data_source.h"
 #include "media/ffmpeg/ffmpeg_common.h"
 
@@ -61,7 +62,12 @@ int BlockingUrlProtocol::Read(int size, uint8_t* data) {
   }
 
   base::WaitableEvent* events[] = { &aborted_, &read_complete_ };
-  size_t index = base::WaitableEvent::WaitMany(events, arraysize(events));
+  size_t index;
+  {
+    base::ScopedBlockingCall scoped_blocking_call(
+        base::BlockingType::MAY_BLOCK);
+    index = base::WaitableEvent::WaitMany(events, arraysize(events));
+  }
 
   if (events[index] == &aborted_)
     return AVERROR(EIO);
