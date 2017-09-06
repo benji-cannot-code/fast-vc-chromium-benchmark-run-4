e3bdb8a06af82b8f9358815bac438b383c21ef36
Karan Bhatia <karandeepb@chromium.org>
2017-09-06 02:37:29 +0000

    DNR: Implement indexing and persisting of manifest declarative rules.
    This CL implements support for indexing and persisting of manifest declarative
    rules. It uses IndexedRule and FlatRulesetIndexer classes to persist the
    declarative rules in flatbuffer format.
    
    In a subsequent CL, support will be added to index and persist the rules during 
    each load of unpacked extensions and during install time for packed extensions.
    
    Doc=http://go/declarative-net-request (Internal only)
    BUG=696822
    
    Change-Id: I7b9034137017cf9e7fdc007e9647c5c8ae434f24
    Reviewed-on: https://chromium-review.googlesource.com/625481
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499857}

--- extensions/browser/api/declarative_net_request/constants.h
+++ extensions/browser/api/declarative_net_request/constants.h
@@ -9,7 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace extensions {
 namespace declarative_net_request {
 
-// The result of parsing a JSON rule provided by an extension.
+// The result of parsing JSON rules provided by an extension. Can correspond to
+// a single or multiple rules.
 enum class ParseResult {
   SUCCESS,
   ERROR_RESOURCE_TYPE_DUPLICATED,
@@ -22,7 +23,25 @@ enum class ParseResult {
   ERROR_EMPTY_RESOURCE_TYPES_LIST,
   ERROR_EMPTY_URL_FILTER,
   ERROR_INVALID_REDIRECT_URL,
+  ERROR_LIST_NOT_PASSED,
+  ERROR_DUPLICATE_IDS,
+  ERROR_PERSISTING_RULESET,
 };
+
+// Rule parsing errors.
+extern const char kErrorResourceTypeDuplicated[];
+extern const char kErrorEmptyRedirectRuleKey[];
+extern const char kErrorInvalidRuleKey[];
+extern const char kErrorNoApplicableResourceTypes[];
+extern const char kErrorEmptyList[];
+extern const char kErrorEmptyUrlFilter[];
+extern const char kErrorInvalidRedirectUrl[];
+extern const char kErrorListNotPassed[];
+extern const char kErrorDuplicateIDs[];
+extern const char kErrorPersisting[];
+
+// Rule parsing install warnings.
+extern const char kRulesNotParsedWarning[];
 
 // Minimum valid value of a declarative rule ID.
 constexpr int kMinValidID = 1;
@@ -33,6 +52,21 @@ constexpr int kMinValidPriority = 1;
 // Default priority used for rules when the priority is not explicity provided
 // by an extension.
 constexpr int kDefaultPriority = 1;
+
+// Keys used in rules.
+extern const char kIDKey[];
+extern const char kPriorityKey[];
+extern const char kUrlFilterKey[];
+extern const char kIsUrlFilterCaseSensitiveKey[];
+extern const char kDomainsKey[];
+extern const char kResourceTypesKey[];
+extern const char kRedirectUrlKey[];
+
+// Histogram names.
+extern const char kIndexAndPersistRulesTimeHistogram[];
+extern const char kIndexRulesTimeHistogram[];
+extern const char kManifestRulesCountHistogram[];
+
 }  // namespace declarative_net_request
 }  // namespace extensions
 
