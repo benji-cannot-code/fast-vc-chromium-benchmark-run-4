ded0a2808229b2c9601e55113188dcc272a69929
Menglu Huang <huangml@chromium.org>
2017-09-06 19:21:29 +0000

    Remove view controllers dependency of settings tests
    Bug: 750372
    Change-Id: I50e8fa524ef65adc3bedf88c1eaa0ad59d784d16
    Reviewed-on: https://chromium-review.googlesource.com/642113
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mike Baxley <baxley@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Menglu Huang <huangml@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500037}

--- ios/chrome/browser/ui/settings/translate_ui_egtest.mm
+++ ios/chrome/browser/ui/settings/translate_ui_egtest.mm
@@ -6,9 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <EarlGrey/EarlGrey.h>
 #import <XCTest/XCTest.h>
 
-#import "ios/chrome/browser/ui/settings/settings_collection_view_controller.h"
 #include "ios/chrome/browser/ui/tools_menu/tools_menu_constants.h"
-#import "ios/chrome/browser/ui/tools_menu/tools_popup_controller.h"
 #include "ios/chrome/grit/ios_strings.h"
 #include "ios/chrome/test/earl_grey/accessibility_util.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
