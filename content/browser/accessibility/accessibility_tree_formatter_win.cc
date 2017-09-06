6f5f338ec8f559a8b433b3157a528231b0a458a9
Nektarios Paisios <nektar@chromium.org>
2017-09-06 02:20:10 +0000

    Reduces the number of items that are announced as clickable by Jaws or NVDA.
    An element gets the click action when there is an anchor or an element with a click listener in its ancestry chain. For the latter case this change exposes a different action name, "click_ancestor", on Windows to stop Windows screen readers unnecessarily announcing many items as clickable.
    Bug: 689201
    R=dmazzoni@chromium.org, aleventhal@chromium.org
    TESTED=Manually with NVDA, browser test, layout test
    
    Change-Id: I19ed8c53861b47e7f5fbe884c474a1c07afee916
    Reviewed-on: https://chromium-review.googlesource.com/602261
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499854}

--- content/browser/accessibility/accessibility_tree_formatter_win.cc
+++ content/browser/accessibility/accessibility_tree_formatter_win.cc
@@ -71,6 +71,7 @@ const char* const ALL_ATTRIBUTES[] = {
     "maximumValue",
     "description",
     "default_action",
+    "action_name",
     "keyboard_shortcut",
     "location",
     "size",
@@ -272,10 +273,19 @@ void AccessibilityTreeFormatterWin::AddProperties(
   }
   temp_bstr.Reset();
 
+  // |get_accDefaultAction| returns a localized string.
   if (SUCCEEDED(ax_object->GetCOM()->get_accDefaultAction(
           variant_self, temp_bstr.Receive()))) {
     dict->SetString("default_action", base::string16(temp_bstr,
         temp_bstr.Length()));
+  }
+  temp_bstr.Reset();
+
+  // |IAccessibleAction::get_name| returns a localized string.
+  if (SUCCEEDED(ax_object->GetCOM()->get_name(0 /* action_index */,
+                                              temp_bstr.Receive()))) {
+    dict->SetString("action_name",
+                    base::string16(temp_bstr, temp_bstr.Length()));
   }
   temp_bstr.Reset();
 
--- third_party/WebKit/Source/modules/accessibility/AXObject.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXObject.cpp
@@ -1369,7 +1369,8 @@ AccessibilityOrientation AXObject::Orientation() const {
 }
 
 AXDefaultActionVerb AXObject::Action() const {
-  if (!ActionElement())
+  Element* action_element = ActionElement();
+  if (!action_element)
     return AXDefaultActionVerb::kNone;
 
   // TODO(dmazzoni): Ensure that combo box text field is handled here.
@@ -1399,7 +1400,9 @@ AXDefaultActionVerb AXObject::Action() const {
     case kPopUpButtonRole:
       return AXDefaultActionVerb::kOpen;
     default:
-      return AXDefaultActionVerb::kClick;
+      if (action_element == GetNode())
+        return AXDefaultActionVerb::kClick;
+      return AXDefaultActionVerb::kClickAncestor;
   }
 }
 
