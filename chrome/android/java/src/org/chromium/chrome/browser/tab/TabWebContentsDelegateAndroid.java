1f4a64dbb756fee5ebaf75d5f98cc122c7813a02
Peter E Conn <peconn@chromium.org>
2017-09-06 11:51:39 +0000

    📺 Only use FullscreenActivity if the current Activity is RESUMED.
    When Chrome goes into the background, the web-page dismisses fullscreen.
    If FullscreenActivity is enabled, this dismissal launches the Tab back
    to ChromeTabbedActivity with an Intent, in effect launching Chrome
    again.
    
    Therefore we use the normal, non-FullscreenActivity transition when
    fullscreen is requested and the Activity is not RESUMED.
    
    Bug: 760211
    Change-Id: I0cfe3b6af085f1cd635b7b66c0e481184bfbbba2
    Reviewed-on: https://chromium-review.googlesource.com/650351
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499933}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabWebContentsDelegateAndroid.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabWebContentsDelegateAndroid.java
@@ -28,7 +28,6 @@ import org.chromium.blink_public.platform.WebDisplayMode;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.AppHooks;
 import org.chromium.chrome.browser.ChromeActivity;
-import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.FullscreenActivity;
 import org.chromium.chrome.browser.RepostFormWarningDialog;
 import org.chromium.chrome.browser.document.DocumentUtils;
@@ -220,8 +219,7 @@ public class TabWebContentsDelegateAndroid extends WebContentsDelegateAndroid {
 
     @Override
     public void toggleFullscreenModeForTab(boolean enableFullscreen) {
-        if (ChromeFeatureList.isEnabled(ChromeFeatureList.FULLSCREEN_ACTIVITY)
-                && mTab.getActivity().supportsFullscreenActivity()) {
+        if (FullscreenActivity.shouldUseFullscreenActivity(mTab)) {
             FullscreenActivity.toggleFullscreenMode(enableFullscreen, mTab);
         } else {
             mTab.toggleFullscreenMode(enableFullscreen);
