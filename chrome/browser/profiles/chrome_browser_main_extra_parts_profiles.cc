3e11a195e8f1bbb09818ab05bee2cddd90df35f8
Bettina Dea <bdea@chromium.org>
2017-09-06 03:23:14 +0000

    Added Bookmark feature in IPH.
    Changes in this Patch:
    - Added event and feature constants for Bookmarks.
    - Added the BookmarkInProductHelp trial to the fieldtrial json.
    - Created a BookmarkTracker class, which contains methods to notify the IPH server
    when events happen and if the help UI should be triggered.
    - Created a BookmarkTrackerFactory class and registered in the chrome browser main parts.
    - Added unit tests for BookmarkTracker event logging and expected functionality.
    
    Design Doc: https://docs.google.com/a/chromium.org/document/d/1j_npx4R1V8IhWY1bHCwvAVpzWUbAl26tlY5jpJ_Rdgc/edit?usp=sharing
    
    This is following the intern's, Chris, CL that wasn't submitted:
    https://chromium-review.googlesource.com/c/581872
    
    Bug: 734132
    Change-Id: Ifc4b4e07ce5f2427f9210dac739df67637cfd66e
    Reviewed-on: https://chromium-review.googlesource.com/614367
    Commit-Queue: Bettina Dea <bdea@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499872}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -143,7 +143,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/desktop_ios_promotion/sms_service_factory.h"
 #endif
 
-#if !defined(OS_ANDROID) && !defined(OS_CHROMEOS) && !defined(OS_MACOSX)
+#if defined(OS_WIN) || (defined(OS_LINUX) && !defined(OS_CHROMEOS))
+#include "chrome/browser/feature_engagement/bookmark/bookmark_tracker_factory.h"
 #include "chrome/browser/feature_engagement/new_tab/new_tab_tracker_factory.h"
 #endif
 
@@ -291,7 +292,8 @@ EnsureBrowserContextKeyedServiceFactoriesBuilt() {
   MediaGalleriesPreferencesFactory::GetInstance();
   NTPResourceCacheFactory::GetInstance();
 #endif
-#if !defined(OS_ANDROID) && !defined(OS_CHROMEOS) && !defined(OS_MACOSX)
+#if defined(OS_WIN) || (defined(OS_LINUX) && !defined(OS_CHROMEOS))
+  feature_engagement::BookmarkTrackerFactory::GetInstance();
   feature_engagement::NewTabTrackerFactory::GetInstance();
 #endif
   ContentSuggestionsServiceFactory::GetInstance();
