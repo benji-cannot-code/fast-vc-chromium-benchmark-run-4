e28d1f9dc63ca6fd88fe6032a16a89dc0cb1498e
Marti Wong <martiw@chromium.org>
2017-09-06 09:23:12 +0000

    Implements new folder functionality on the context bar of bookmarks iOS.
    Demo: https://drive.google.com/open?id=0B1O0Z7eoZMuGVHA4M3FPNkVINTA
    
    Bug: 695749
    Change-Id: I3acb6c663da1c0cb2ae63d31f006704f0a52710f
    Reviewed-on: https://chromium-review.googlesource.com/651389
    Reviewed-by: Ramya Sharma <ramyasharma@chromium.org>
    Commit-Queue: Marti Wong <martiw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499917}

--- ios/chrome/browser/ui/bookmarks/bookmark_home_view_controller.mm
+++ ios/chrome/browser/ui/bookmarks/bookmark_home_view_controller.mm
@@ -921,6 +921,7 @@ const CGFloat kSpacer = 50;
       setButtonTitle:l10n_util::GetNSString(IDS_IOS_BOOKMARK_CONTEXT_BAR_SELECT)
            forButton:ContextBarTrailingButton];
 
+  [self setContextBarState:BookmarksContextBarDefault];
   [self.view addSubview:self.contextBar];
 }
 
@@ -1080,7 +1081,7 @@ const CGFloat kSpacer = 50;
   switch (self.contextBarState) {
     case BookmarksContextBarDefault:
       // New Folder clicked.
-      // TODO(crbug.com/695749): Implement the button action here.
+      [self.bookmarksTableView addNewFolder];
       break;
     case BookmarksContextBarBeginSelection:
       // This must never happen, as the leading button is disabled at this
@@ -1152,7 +1153,9 @@ const CGFloat kSpacer = 50;
 - (void)trailingButtonClicked {
   // Toggle edit mode.
   [self.bookmarksTableView setEditing:!self.bookmarksTableView.editing];
-  [self setContextBarState:BookmarksContextBarBeginSelection];
+  [self setContextBarState:self.bookmarksTableView.editing
+                               ? BookmarksContextBarBeginSelection
+                               : BookmarksContextBarDefault];
 }
 
 #pragma mark - ContextBarStates
@@ -1165,11 +1168,7 @@ const CGFloat kSpacer = 50;
       [self setBookmarksContextBarButtonsDefaultState];
       break;
     case BookmarksContextBarBeginSelection:
-      if (self.bookmarksTableView.editing) {
-        [self setBookmarksContextBarSelectionStartState];
-      } else {
-        [self setBookmarksContextBarButtonsDefaultState];
-      }
+      [self setBookmarksContextBarSelectionStartState];
       break;
     case BookmarksContextBarSingleURLSelection:
     case BookmarksContextBarMultipleURLSelection:
