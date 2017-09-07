a8804b0e4546c80d5d122964107def5f3f445e45
Daniel Bratell <bratell@opera.com>
2017-09-07 20:29:02 +0000

    Remove unused "using base::Time"
    When experimenting with jumbo builds in content this Time conflicts
    with other things, and since it's unused it's easy to just remove it.
    
    Change-Id: I853e2da58963c875158b750839a706e49edad93e
    Reviewed-on: https://chromium-review.googlesource.com/653161
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#500370}

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -119,7 +119,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accelerated_widget_mac/window_resize_helper_mac.h"
 #endif
 
-using base::Time;
 using base::TimeDelta;
 using base::TimeTicks;
 using blink::WebDragOperation;
