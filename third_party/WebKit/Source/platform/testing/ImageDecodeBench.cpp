042116558583c7a453da92c4edd66794b8b338e3
Sam McNally <sammc@chromium.org>
2017-09-07 09:07:21 +0000

    ImageDecodeBench: Initialize mojo and create a MessageLoop.
    blink::Platform now requires mojo to be initialized so add
    mojo::edk::Init() and base::MessageLoop in main().
    
    Bug: 760864
    Change-Id: Iaa61bb3b06765047cc34b607d827a0f45210e779
    Reviewed-on: https://chromium-review.googlesource.com/654467
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500250}

--- third_party/WebKit/Source/platform/testing/ImageDecodeBench.cpp
+++ third_party/WebKit/Source/platform/testing/ImageDecodeBench.cpp
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include "base/command_line.h"
+#include "base/message_loop/message_loop.h"
+#include "mojo/edk/embedder/embedder.h"
 #include "platform/SharedBuffer.h"
 #include "platform/image-decoders/ImageDecoder.h"
 #include "platform/wtf/PassRefPtr.h"
@@ -356,5 +358,7 @@ int Main(int argc, char* argv[]) {
 }  // namespace blink
 
 int main(int argc, char* argv[]) {
+  base::MessageLoop message_loop;
+  mojo::edk::Init();
   return blink::Main(argc, argv);
 }
