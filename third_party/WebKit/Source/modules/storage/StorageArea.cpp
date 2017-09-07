32ed192540f8e00bf516915aeb54a469dde4ca3c
Sam McNally <sammc@chromium.org>
2017-09-07 00:56:06 +0000

    Fix {local,session}Storage event dispatch with document.domain set.
    Previously, StorageArea used SecurityOrigin::CanAccess() to filter
    storage events to the frames of the right origin. However, this checks
    whether the two SecurityOrigins are same origin-domain, while storage
    events are always same origin. Replace it with IsSameSchemeHostPort() to
    avoid document.domain influencing filtering of these events.
    
    Bug: 136356
    Change-Id: I72eee7ea50252c1e2cd2d54b520efd0d8e4b596a
    Reviewed-on: https://chromium-review.googlesource.com/651832
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500164}

--- third_party/WebKit/Source/modules/storage/StorageArea.cpp
+++ third_party/WebKit/Source/modules/storage/StorageArea.cpp
@@ -177,7 +177,7 @@ void StorageArea::DispatchLocalStorageEvent(
       Storage* storage =
           DOMWindowStorage::From(*local_window).OptionalLocalStorage();
       if (storage &&
-          local_frame->GetDocument()->GetSecurityOrigin()->CanAccess(
+          local_frame->GetDocument()->GetSecurityOrigin()->IsSameSchemeHostPort(
               security_origin) &&
           !IsEventSource(storage, source_area_instance))
         local_frame->DomWindow()->EnqueueWindowEvent(
@@ -229,7 +229,7 @@ void StorageArea::DispatchSessionStorageEvent(
     Storage* storage =
         DOMWindowStorage::From(*local_window).OptionalSessionStorage();
     if (storage &&
-        local_frame->GetDocument()->GetSecurityOrigin()->CanAccess(
+        local_frame->GetDocument()->GetSecurityOrigin()->IsSameSchemeHostPort(
             security_origin) &&
         !IsEventSource(storage, source_area_instance))
       local_frame->DomWindow()->EnqueueWindowEvent(
