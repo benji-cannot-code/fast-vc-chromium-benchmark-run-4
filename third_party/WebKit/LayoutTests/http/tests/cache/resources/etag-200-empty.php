f3bcc9a4984012de5c93df172026e2b8c640a13c
Hiroshige Hayashizaki <hiroshige@chromium.org>
2017-09-07 21:27:11 +0000

    Add tests for SRI with empty body after revalidation
    Crash tests for Issue 752830:
      [1] introduced the root cause of the crash, and
      [2] and [3] expose the crash and make the tests in this CL to crash.
      [4a] and [4b] fix the crashes in these tests.
    
    [1] https://chromium-review.googlesource.com/c/568481
    [2] https://chromium-review.googlesource.com/c/576950
    [3] https://chromium-review.googlesource.com/c/562859
    [4a] https://chromium-review.googlesource.com/c/621239 
    [4b] https://chromium-review.googlesource.com/c/622278
    
    Bug: 752830
    Change-Id: If978dc79debf0edfe6de6a1e646ea0f905cc37d3
    Reviewed-on: https://chromium-review.googlesource.com/621240
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500387}

---
+++ third_party/WebKit/LayoutTests/http/tests/cache/resources/etag-200-empty.php
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?
+// Returns non-empty body and response headers that cause revalidation, and
+// returns 200 with empty body for revalidating requests.
+header('ETag: foo');
+header('Cache-control: max-age=0');
+
+if ($_SERVER['HTTP_IF_NONE_MATCH'] == 'foo') {
+    // The body is intentionally empty.
+    exit;
+}
+echo "/* comment */";
+?>
