071dca74121310860dbe682615ae49c204ae132a
Ryan Landay <rlanday@chromium.org>
2017-09-07 18:53:28 +0000

    Remove some dead code for fetching tab thumbnails on Android
    I was reading through this code to try to understand how it works and I
    noticed these methods are unused, so I am removing them to improve code
    health.
    
    Bug: 
    Change-Id: I00c4277825f06d94add563c9b748d5c1a31920d2
    Reviewed-on: https://chromium-review.googlesource.com/648636
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Ryan Landay <rlanday@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500349}

--- chrome/browser/android/compositor/tab_content_manager.h
+++ chrome/browser/android/compositor/tab_content_manager.h
@@ -98,9 +98,6 @@ class TabContentManager : public ThumbnailCacheObserver {
   void RemoveTabThumbnail(JNIEnv* env,
                           const base::android::JavaParamRef<jobject>& obj,
                           jint tab_id);
-  void GetDecompressedThumbnail(JNIEnv* env,
-                                const base::android::JavaParamRef<jobject>& obj,
-                                jint tab_id);
   void OnUIResourcesWereEvicted();
 
   // ThumbnailCacheObserver implementation;
