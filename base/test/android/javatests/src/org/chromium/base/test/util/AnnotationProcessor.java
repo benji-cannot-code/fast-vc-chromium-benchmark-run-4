ea2f29df32b73c8113f15ec912ecea3e3f4c7a12
Bernhard Bauer <bauerb@chromium.org>
2017-09-07 09:41:55 +0000

    Collect test annotations in Annotation from both test class and method.
    Bug: 754778
    Change-Id: I3e0adfb06b877ab99edcbb291d76b435ff26c3b9
    Reviewed-on: https://chromium-review.googlesource.com/620712
    Commit-Queue: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500258}

--- base/test/android/javatests/src/org/chromium/base/test/util/AnnotationProcessor.java
+++ base/test/android/javatests/src/org/chromium/base/test/util/AnnotationProcessor.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.base.test.util;
 
+import android.support.annotation.Nullable;
+
 import org.junit.rules.ExternalResource;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
@@ -12,8 +14,9 @@ import org.junit.runners.model.Statement;
 import java.lang.annotation.Annotation;
 
 /**
- * Test rule that is activated when a test has a specific annotation. It allows to run some code
- * before the test (and the {@link org.junit.Before}) runs, and guarantees to also run code after.
+ * Test rule that is activated when a test or its class has a specific annotation.
+ * It allows to run some code before the test (and the {@link org.junit.Before}) runs,
+ * and guarantees to also run code after.
  *
  * Usage:
  *
@@ -39,7 +42,12 @@ import java.lang.annotation.Annotation;
 public abstract class AnnotationProcessor<T extends Annotation> extends ExternalResource {
     private final Class<T> mAnnotationClass;
     private Description mTestDescription;
-    private T mAnnotation;
+
+    @Nullable
+    private T mClassAnnotation;
+
+    @Nullable
+    private T mTestAnnotation;
 
     public AnnotationProcessor(Class<T> annotationClass) {
         mAnnotationClass = annotationClass;
@@ -48,8 +56,10 @@ public abstract class AnnotationProcessor<T extends Annotation> extends External
     @Override
     public Statement apply(Statement base, Description description) {
         mTestDescription = description;
-        mAnnotation = getAnnotation(description);
-        if (mAnnotation == null) return base;
+        mClassAnnotation = description.getTestClass().getAnnotation(mAnnotationClass);
+        mTestAnnotation = description.getAnnotation(mAnnotationClass);
+
+        if (mClassAnnotation == null && mTestAnnotation == null) return base;
 
         // Return the wrapped statement to execute before() and after().
         return super.apply(base, description);
@@ -60,16 +70,15 @@ public abstract class AnnotationProcessor<T extends Annotation> extends External
         return mTestDescription;
     }
 
-    /** @return the annotation that caused the test to be processed. */
-    protected T getAnnotation() {
-        return mAnnotation;
+    /** @return the annotation on the test class. */
+    @Nullable
+    protected T getClassAnnotation() {
+        return mClassAnnotation;
     }
 
-    private T getAnnotation(Description description) {
-        T annotation = description.getAnnotation(mAnnotationClass);
-        if (annotation != null) return annotation;
-
-        annotation = description.getTestClass().getAnnotation(mAnnotationClass);
-        return annotation;
+    /** @return the annotation on the test method. */
+    @Nullable
+    protected T getTestAnnotation() {
+        return mTestAnnotation;
     }
 }
