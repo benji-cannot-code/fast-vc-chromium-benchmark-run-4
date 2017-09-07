067fba727c092ef0d2be23a233e1d22ab6817572
Chris Watkins <watk@chromium.org>
2017-09-07 08:28:33 +0000

    media: Simplify CodecWrapper's interface; accept DecoderBuffers as input
    CodecWrapper and its caller are both simpler if it accepts
    DecoderBuffers as inputs. QueueEOS, and QueueSecureInputBuffer are now
    gone in favor of a single QueueInputBuffer.
    
    CodecWrapper also now keeps track of whether it still owns a dequeued
    input buffer after trying to submit a buffer fails with
    MEDIA_CODEC_NO_KEY. It automatically reuses the input buffer for
    subsequent inputs.
    
    Bug: 660942
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ie8785721fe93c8ccf46399b587e8de9c2e1b4bdd
    Reviewed-on: https://chromium-review.googlesource.com/647266
    Commit-Queue: Chris Watkins <watk@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500244}

--- media/gpu/android/codec_wrapper.h
+++ media/gpu/android/codec_wrapper.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/synchronization/lock.h"
 #include "media/base/android/media_codec_bridge.h"
+#include "media/base/decoder_buffer.h"
 #include "media/gpu/android/device_info.h"
 #include "media/gpu/media_gpu_export.h"
 #include "media/gpu/surface_texture_gl_owner.h"
@@ -96,23 +97,12 @@ class MEDIA_GPU_EXPORT CodecWrapper {
   // Whether the codec supports Flush().
   bool SupportsFlush(DeviceInfo* device_info) const;
 
-  // See MediaCodecBridge documentation for the following.
+  // Flushes the codec and discards all output buffers.
   bool Flush();
-  MediaCodecStatus QueueInputBuffer(int index,
-                                    const uint8_t* data,
-                                    size_t data_size,
-                                    base::TimeDelta presentation_time);
-  MediaCodecStatus QueueSecureInputBuffer(
-      int index,
-      const uint8_t* data,
-      size_t data_size,
-      const std::string& key_id,
-      const std::string& iv,
-      const std::vector<SubsampleEntry>& subsamples,
-      const EncryptionScheme& encryption_scheme,
-      base::TimeDelta presentation_time);
-  void QueueEOS(int input_buffer_index);
-  MediaCodecStatus DequeueInputBuffer(int* index);
+
+  // Queues |buffer| if the codec has an available input buffer.
+  MediaCodecStatus QueueInputBuffer(const DecoderBuffer& buffer,
+                                    const EncryptionScheme& encryption_scheme);
 
   // Like MediaCodecBridge::DequeueOutputBuffer() but it outputs a
   // CodecOutputBuffer instead of an index. |*codec_buffer| must be null.
