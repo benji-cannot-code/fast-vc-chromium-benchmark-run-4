30bc735aacabe19428e4669a7eab7bb6579567bd
Brian Salomon <bsalomon@google.com>
2017-09-07 15:12:41 +0000

    Add missing include of SkImage.h to GraphicsContext3DUtils.h
    Change-Id: I77ccf6023b3a47a28d24e2b173f661f9b8b1f76a
    Reviewed-on: https://chromium-review.googlesource.com/655217
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Brian Salomon <bsalomon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500302}

--- third_party/WebKit/Source/platform/graphics/gpu/GraphicsContext3DUtils.h
+++ third_party/WebKit/Source/platform/graphics/gpu/GraphicsContext3DUtils.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/PlatformExport.h"
 #include "platform/wtf/HashMap.h"
 #include "platform/wtf/WeakPtr.h"
+#include "third_party/skia/include/core/SkImage.h"
 #include "third_party/skia/include/gpu/GrTexture.h"
 
 namespace blink {
