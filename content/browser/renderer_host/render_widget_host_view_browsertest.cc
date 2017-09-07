c76d1aa9149589c4e529870aecdaf7bd0f9cc31e
Roger McFarlane <rogerm@chromium.org>
2017-09-07 15:41:54 +0000

    Revert "Mojofy DidCommitProvisionalLoad."
    This reverts commit fa8adf8fea6dd127158b9e98984338cf73970b4e.
    
    Reason for revert:
    
    Seeing new Win10 failures in navigation_controller_impl_browsertest.cc post this commit. 
    
    Original change's description:
    > Mojofy DidCommitProvisionalLoad.
    > 
    > Refactor FrameHostMsg_DidCommitProvisionalLoad from being a legacy
    > IPC message into a method on mojom::FrameHost, a frame-scoped,
    > Channel-associated interface. There is no change to functionality.
    > 
    > Bug: 729021
    > Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    > Change-Id: I794a8f1cb433a82e2c1d9351630116b584f8fcf9
    > Reviewed-on: https://chromium-review.googlesource.com/607607
    > Commit-Queue: Balazs Engedy <engedy@chromium.org>
    > Reviewed-by: Alexander Timin <altimin@chromium.org>
    > Reviewed-by: Ken Rockot <rockot@chromium.org>
    > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#500294}
    
    TBR=dcheng@chromium.org,nasko@chromium.org,rockot@chromium.org,engedy@chromium.org,altimin@chromium.org
    
    Change-Id: I2d8798575907a7d195e65ee2bc9fd49e190f84f9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 729021
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Reviewed-on: https://chromium-review.googlesource.com/655142
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Commit-Queue: Roger McFarlane <rogerm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500308}

--- content/browser/renderer_host/render_widget_host_view_browsertest.cc
+++ content/browser/renderer_host/render_widget_host_view_browsertest.cc
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/content_browser_test_utils.h"
 #include "content/public/test/test_utils.h"
 #include "content/shell/browser/shell.h"
-#include "content/test/did_commit_provisional_load_interceptor.h"
 #include "media/base/video_frame.h"
 #include "media/renderers/skcanvas_video_renderer.h"
 #include "net/base/filename_util.h"
@@ -210,20 +209,25 @@ class RenderWidgetHostViewBrowserTest : public ContentBrowserTest {
 
 // Helps to ensure that a navigation is committed after a compositor frame was
 // submitted by the renderer, but before corresponding ACK is sent back.
-class CommitBeforeSwapAckSentHelper
-    : public DidCommitProvisionalLoadInterceptor {
+class CommitBeforeSwapAckSentHelper : public WebContentsObserver {
  public:
   explicit CommitBeforeSwapAckSentHelper(WebContents* web_contents)
-      : DidCommitProvisionalLoadInterceptor(web_contents) {}
+      : WebContentsObserver(web_contents) {}
 
  private:
-  // DidCommitProvisionalLoadInterceptor:
-  void WillDispatchDidCommitProvisionalLoad(
-      RenderFrameHost* render_frame_host,
-      ::FrameHostMsg_DidCommitProvisionalLoad_Params*) override {
+  void WaitForSwapCompositorFrame() {
     base::MessageLoop::ScopedNestableTaskAllower allow(
         base::MessageLoop::current());
     FrameWatcher(web_contents()).WaitFrames(1);
+  }
+
+  bool OnMessageReceived(const IPC::Message& message,
+                         RenderFrameHost* rfh) override {
+    IPC_BEGIN_MESSAGE_MAP(CommitBeforeSwapAckSentHelper, message)
+      IPC_MESSAGE_HANDLER_GENERIC(FrameHostMsg_DidCommitProvisionalLoad,
+                                  WaitForSwapCompositorFrame())
+    IPC_END_MESSAGE_MAP()
+    return false;
   }
 
   DISALLOW_COPY_AND_ASSIGN(CommitBeforeSwapAckSentHelper);
