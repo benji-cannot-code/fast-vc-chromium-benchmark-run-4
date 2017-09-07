a2cda7238b02590bfdbe139393df644dd832188f
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2017-09-07 16:16:55 +0000

    Import wpt@c3bc351c86f2ef203b030ddec8176b38da65ec99
    Using wpt-import in Chromium 67cf82e154d777a951c124e5ce8e4a1b5dda24c7.
    
    Build: https://build.chromium.org/p/chromium.infra.cron/builders/wpt-importer/builds/1386
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    dom-dev@chromium.org:
      external/wpt/html
    tyoshino@chromium.org, yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    TBR=qyearsley@chromium.org
    
    No-Export: true
    Change-Id: Ib593c604655282fcb8c98c6e255470c6df347cd0
    Reviewed-on: https://chromium-review.googlesource.com/655218
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500317}

--- third_party/WebKit/LayoutTests/external/wpt/fetch/api/cors/cors-expose-star.js
+++ third_party/WebKit/LayoutTests/external/wpt/fetch/api/cors/cors-expose-star.js
@@ -5,7 +5,7 @@ if (this.document === undefined) {
 }
 
 const url = "http://{{host}}:{{ports[http][1]}}" + dirname(location.pathname) + RESOURCES_DIR + "top.txt",
-      sharedHeaders = "?pipe=header(Access-Control-Expose-Headers,*)|header(Test,X)|header(Set-Cookie,X)|"
+      sharedHeaders = "?pipe=header(Access-Control-Expose-Headers,*)|header(Test,X)|header(Set-Cookie,X)|header(*,whoa)|"
 
 promise_test(() => {
   const headers = "header(Access-Control-Allow-Origin,*)"
@@ -14,6 +14,7 @@ promise_test(() => {
     assert_equals(resp.type , "cors")
     assert_equals(resp.headers.get("test"), "X")
     assert_equals(resp.headers.get("set-cookie"), null)
+    assert_equals(resp.headers.get("*"), "whoa")
   })
 }, "Basic Access-Control-Expose-Headers: * support")
 
@@ -26,7 +27,19 @@ promise_test(() => {
     assert_equals(resp.headers.get("content-type"), "text/plain") // safelisted
     assert_equals(resp.headers.get("test"), null)
     assert_equals(resp.headers.get("set-cookie"), null)
+    assert_equals(resp.headers.get("*"), "whoa")
   })
-}, "Cannot use * for credentialed fetches")
+}, "* for credentialed fetches only matches literally")
+
+promise_test(() => {
+  const headers =  "header(Access-Control-Allow-Origin,*)|header(Access-Control-Expose-Headers,set-cookie)"
+  return fetch(url + sharedHeaders + headers).then(resp => {
+    assert_equals(resp.status, 200)
+    assert_equals(resp.type , "cors")
+    assert_equals(resp.headers.get("test"), "X")
+    assert_equals(resp.headers.get("set-cookie"), null)
+    assert_equals(resp.headers.get("*"), "whoa")
+  })
+}, "* can be one of several values")
 
 done();
