0372a645bf39940e741100296688670842615aa8
Bruce Dawson <brucedawson@chromium.org>
2017-09-07 18:47:24 +0000

    Improve battor_agent interactive mode
    The battor_agent StopTracing command, if no file name is specified,
    printed the raw sampling data which can easily be 100,000 lines of
    numbers. This is not helpful. This change improves this case,
    especially when in interactive mode.
    
    Now a BattorResults object is returned which contains a detailed
    string representation of the results and enough information to allow
    creating a summary. When in battor_agent's interactive mode the
    summary is printed and the details are saved to a file (using a
    generated file name if none is supplied). The summary result format
    is for human consumption and is subject to change but currently it
    looks like this:
    
    StopTracing
    Second  0 average power:  9.20 W
    Second  1 average power:  8.65 W
    Second  2 average power: 11.38 W
    Average power over 3.14 s :  9.74 W
    Summary of power-by-seconds:
    Minimum:  8.65
    Median:   9.20
    Maximum: 11.38
    Saving detailed results to c:\users\brucedawson\desktop\trace_data.txt
    Done.
    
    This lets the developer easily see the average power and the variability
    of power, which is helpful for getting a sense of how consistent the
    results are and therefore (in steady-state scenarios) how meaningful
    they are. This format has been tested for a few weeks on a Battor
    equipped SurfaceBook.
    
    Bug: 751863
    Change-Id: I57af162f183a4d65388c88c901573cd6720ae2b8
    Reviewed-on: https://chromium-review.googlesource.com/609385
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Reviewed-by: Charlie Andrews <charliea@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500344}

--- tools/battor_agent/battor_agent.h
+++ tools/battor_agent/battor_agent.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define TOOLS_BATTOR_AGENT_BATTOR_AGENT_H_
 
 #include <map>
+#include <vector>
 
 #include "base/cancelable_callback.h"
 #include "base/macros.h"
@@ -18,6 +19,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "tools/battor_agent/battor_error.h"
 
 namespace battor {
+
+// A BattOrResults object contains the results of BattOr tracing, including a
+// summary and sample data in watts.
+class BattOrResults {
+ public:
+  BattOrResults();
+  BattOrResults(std::string details,
+                std::vector<float> power_samples_W,
+                uint32_t sample_rate);
+  BattOrResults(const BattOrResults&);
+  ~BattOrResults();
+
+  // Get a detailed textual representation of the data recorded.
+  const std::string& ToString() const { return details_; }
+  // Returns a vector of power samples (in watts).
+  const std::vector<float>& GetPowerSamples() const { return power_samples_W_; }
+  uint32_t GetSampleRate() const { return sample_rate_; }
+
+ private:
+  std::string details_;
+  std::vector<float> power_samples_W_;
+  uint32_t sample_rate_ = 0;
+};
 
 // A BattOrAgent is a class used to asynchronously communicate with a BattOr for
 // the purpose of collecting power samples. A BattOr is an external USB device
@@ -42,7 +66,7 @@ class BattOrAgent : public BattOrConnection::Listener,
   class Listener {
    public:
     virtual void OnStartTracingComplete(BattOrError error) = 0;
-    virtual void OnStopTracingComplete(const std::string& trace,
+    virtual void OnStopTracingComplete(const BattOrResults& trace,
                                        BattOrError error) = 0;
     virtual void OnRecordClockSyncMarkerComplete(BattOrError error) = 0;
     virtual void OnGetFirmwareGitHashComplete(const std::string& version,
@@ -142,7 +166,7 @@ class BattOrAgent : public BattOrConnection::Listener,
   void CompleteCommand(BattOrError error);
 
   // Returns a formatted version of samples_ with timestamps and real units.
-  std::string SamplesToString();
+  BattOrResults SamplesToResults();
 
   // Sets and restarts the action timeout timer.
   void SetActionTimeout(uint16_t timeout_seconds);
