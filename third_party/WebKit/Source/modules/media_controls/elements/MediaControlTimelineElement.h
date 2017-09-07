2bc51f453ab0ae7dcb8a36f5726095069bf3e741
Becca Hughes <beccahughes@chromium.org>
2017-09-07 16:05:06 +0000

    Media Controls: Replace painter with CSS.
    Replace MediaControlPainter with CSS. Add some classes to some
    elements to allow styling by state.
    
    BUG=746872
    
    Change-Id: I1c226a85ff133b9bf440925b238abf7810c9482a
    Reviewed-on: https://chromium-review.googlesource.com/584771
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500312}

--- third_party/WebKit/Source/modules/media_controls/elements/MediaControlTimelineElement.h
+++ third_party/WebKit/Source/modules/media_controls/elements/MediaControlTimelineElement.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MediaControlTimelineElement_h
 #define MediaControlTimelineElement_h
 
-#include "modules/media_controls/elements/MediaControlInputElement.h"
+#include "modules/media_controls/elements/MediaControlSliderElement.h"
 #include "modules/media_controls/elements/MediaControlTimelineMetrics.h"
 
 namespace blink {
@@ -14,7 +14,7 @@ namespace blink {
 class Event;
 class MediaControlsImpl;
 
-class MediaControlTimelineElement final : public MediaControlInputElement {
+class MediaControlTimelineElement final : public MediaControlSliderElement {
  public:
   explicit MediaControlTimelineElement(MediaControlsImpl&);
 
@@ -41,7 +41,12 @@ class MediaControlTimelineElement final : public MediaControlInputElement {
   // simplicity; deliberately ignores pinch zoom's pageScaleFactor).
   int TimelineWidth();
 
+  void RenderBarSegments();
+
   MediaControlTimelineMetrics metrics_;
+
+  double current_time_;
+  double duration_;
 };
 
 }  // namespace blink
