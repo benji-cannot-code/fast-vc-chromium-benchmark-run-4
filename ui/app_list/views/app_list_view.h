ab4b745a2c2234c8190343b3d09f10255eee8514
Alex Newcomer <newcomer@chromium.org>
2017-09-08 20:48:11 +0000

    cros: Announce launcher state changes in ChromeVox
    Announce peeking and fullscreen all-apps transitions in chromeVox.
    
    Bug: 761161
    Change-Id: Ia3d44bcae45f9d8816507822ddbc82e145ae87fc
    Reviewed-on: https://chromium-review.googlesource.com/653486
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500652}

--- ui/app_list/views/app_list_view.h
+++ ui/app_list/views/app_list_view.h
@@ -138,6 +138,7 @@ class APP_LIST_EXPORT AppListView : public views::BubbleDialogDelegateView,
   bool AcceleratorPressed(const ui::Accelerator& accelerator) override;
   void Layout() override;
   void SchedulePaintInRect(const gfx::Rect& rect) override;
+  void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
 
   // Overridden from ui::EventHandler:
   void OnScrollEvent(ui::ScrollEvent* event) override;
@@ -252,6 +253,9 @@ class APP_LIST_EXPORT AppListView : public views::BubbleDialogDelegateView,
   // Records the state transition for UMA.
   void RecordStateTransitionForUma(AppListState new_state);
 
+  // Creates an Accessibility Event if the state transition warrants one.
+  void CreateAccessibilityEvent(AppListState new_state);
+
   // Gets the display nearest to the parent window.
   display::Display GetDisplayNearestView() const;
 
@@ -361,6 +365,9 @@ class APP_LIST_EXPORT AppListView : public views::BubbleDialogDelegateView,
 
   // True if the dragging started from PEEKING state.
   bool drag_started_from_peeking_ = false;
+
+  // Accessibility announcement dialogue.
+  base::string16 state_announcement_;
 
   DISALLOW_COPY_AND_ASSIGN(AppListView);
 };
