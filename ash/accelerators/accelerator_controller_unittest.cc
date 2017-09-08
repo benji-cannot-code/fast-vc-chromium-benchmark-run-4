fecba1ebe48205871431e4630b884ecfc59dbe70
Qiang Xu <warx@chromium.org>
2017-09-08 18:12:15 +0000

    cros: add tests for media accelerators
    changes:
    Add test coverage for media accelerators. This includes
    ui::VKEY_MEDIA_NEXT_TRACK, ui::VKEY_MEDIA_PLAY_PAUSE,
    ui::VKEY_MEDIA_PREV_TRACK.
    
    Bug=none
    Test=covered by tests
    
    Change-Id: Id21c153b96e47753e9cd5f7767960a073d6bf9ca
    Reviewed-on: https://chromium-review.googlesource.com/656608
    Commit-Queue: Qiang(Joe) Xu <warx@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500621}

--- ash/accelerators/accelerator_controller_unittest.cc
+++ ash/accelerators/accelerator_controller_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/accessibility_types.h"
 #include "ash/ash_switches.h"
 #include "ash/ime/ime_controller.h"
+#include "ash/media_controller.h"
 #include "ash/public/cpp/config.h"
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/public/interfaces/ime_info.mojom.h"
@@ -19,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/brightness_control_delegate.h"
 #include "ash/system/keyboard_brightness_control_delegate.h"
 #include "ash/test/ash_test_base.h"
+#include "ash/test_media_client.h"
 #include "ash/test_screenshot_delegate.h"
 #include "ash/wm/lock_state_controller.h"
 #include "ash/wm/lock_state_controller_test_api.h"
@@ -869,6 +871,25 @@ TEST_F(AcceleratorControllerTest, GlobalAcceleratorsToggleAppList) {
       CreateReleaseAccelerator(ui::VKEY_LWIN, ui::EF_NONE)));
   RunAllPendingInMessageLoop();
   EXPECT_EQ(3u, test_app_list_presenter.toggle_count());
+}
+
+TEST_F(AcceleratorControllerTest, MediaGlobalAccelerators) {
+  TestMediaClient client;
+  Shell::Get()->media_controller()->SetClient(client.CreateAssociatedPtrInfo());
+  EXPECT_EQ(0, client.handle_media_next_track_count());
+  ProcessInController(ui::Accelerator(ui::VKEY_MEDIA_NEXT_TRACK, ui::EF_NONE));
+  base::RunLoop().RunUntilIdle();
+  EXPECT_EQ(1, client.handle_media_next_track_count());
+
+  EXPECT_EQ(0, client.handle_media_play_pause_count());
+  ProcessInController(ui::Accelerator(ui::VKEY_MEDIA_PLAY_PAUSE, ui::EF_NONE));
+  base::RunLoop().RunUntilIdle();
+  EXPECT_EQ(1, client.handle_media_play_pause_count());
+
+  EXPECT_EQ(0, client.handle_media_prev_track_count());
+  ProcessInController(ui::Accelerator(ui::VKEY_MEDIA_PREV_TRACK, ui::EF_NONE));
+  base::RunLoop().RunUntilIdle();
+  EXPECT_EQ(1, client.handle_media_prev_track_count());
 }
 
 TEST_F(AcceleratorControllerTest, ImeGlobalAccelerators) {
