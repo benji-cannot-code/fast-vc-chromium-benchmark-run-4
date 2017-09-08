64506223319a9d048fdc5e0f5884621eef4d857c
Marti Wong <martiw@chromium.org>
2017-09-08 06:18:39 +0000

    Resubmit with tests fixed (implement New Folder for iOS new bookmark ui)
    This is resubmitting of crrev.com/c/651389 with egtests fixed.
    
    Bug: 695749
    Change-Id: I83350c841dfd347198838d9c22f4a8f3afdb6a49
    Reviewed-on: https://chromium-review.googlesource.com/656640
    Commit-Queue: Marti Wong <martiw@chromium.org>
    Reviewed-by: Ramya Sharma <ramyasharma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500524}

--- ios/chrome/browser/ui/bookmarks/cells/bookmark_table_cell.h
+++ ios/chrome/browser/ui/bookmarks/cells/bookmark_table_cell.h
@@ -13,6 +13,9 @@ namespace bookmarks {
 class BookmarkNode;
 }  // namespace bookmarks
 
+@class BookmarkTableCell;
+@class BookmarkTableCellTextFieldDelegate;
+
 // Cell to display bookmark folders and URLs.
 // |---------------------------------------------|
 // |                                             |
@@ -20,6 +23,15 @@ class BookmarkNode;
 // |                                             |
 // |---------------------------------------------|
 //
+
+// Delegates the cell's text field's event.
+@protocol BookmarkTableCellTitleEditDelegate
+
+// Called when the |titleText| of the cell was changed.
+- (void)textDidChangeTo:(NSString*)newName;
+
+@end
+
 @interface BookmarkTableCell : UITableViewCell
 
 - (instancetype)initWithReuseIdentifier:(NSString*)bookmarkCellIdentifier
@@ -46,6 +58,13 @@ class BookmarkNode;
 
 // Set the bookmark node this cell shows.
 - (void)setNode:(const bookmarks::BookmarkNode*)node;
+
+// Start editing the |titleText| of this cell.
+- (void)startEdit;
+
+// Receives the text field events.
+@property(nonatomic, weak) id<BookmarkTableCellTitleEditDelegate> textDelegate;
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_UI_BOOKMARKS_CELLS_BOOKMARK_TABLE_CELL_H_
