1abe619a0d97a3c51834bcb92208a5c23a6aa236
Shanmuga Pandi M <shanmuga.m@samsung.com>
2017-09-08 07:00:42 +0000

    Measure the usage of image button form data with value attribute.
    Image submit button makes extra form data if "value" attribute
    is present. As interop Edge and Firefox is not makes extra form
    data.
    
    Bug: 753746
    Change-Id: Ibbcf49b62fb704008c7f0bdc17915770e328bfff
    Reviewed-on: https://chromium-review.googlesource.com/652727
    Commit-Queue: Shanmuga Pandi <shanmuga.m@samsung.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500531}

--- third_party/WebKit/Source/core/html/forms/ImageInputType.cpp
+++ third_party/WebKit/Source/core/html/forms/ImageInputType.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/ShadowRoot.h"
 #include "core/dom/SyncReattachContext.h"
 #include "core/events/MouseEvent.h"
+#include "core/frame/UseCounter.h"
 #include "core/html/FormData.h"
 #include "core/html/HTMLFormElement.h"
 #include "core/html/HTMLImageFallbackHelper.h"
@@ -73,8 +74,11 @@ void ImageInputType::AppendToFormData(FormData& form_data) const {
   form_data.append(name + dot_x_string, click_location_.X());
   form_data.append(name + dot_y_string, click_location_.Y());
 
-  if (!GetElement().value().IsEmpty())
+  if (!GetElement().value().IsEmpty()) {
+    UseCounter::Count(GetElement().GetDocument(),
+                      WebFeature::kImageInputTypeFormDataWithNonEmptyValue);
     form_data.append(name, GetElement().value());
+  }
 }
 
 String ImageInputType::ResultForDialogSubmit() const {
