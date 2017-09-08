36571832fa83ac69940e790c7905e7cdc5dfd47b
Peter Kotwicz <pkotwicz@chromium.org>
2017-09-08 18:44:04 +0000

    Revert "Split ThumbnailProviderImpl cache into a LRU and a deduplication cache."
    This reverts commit a4f8bff1bb207d38f2b083166fb7b28f248049bd.
    
    Reason for revert: The CL was causing multiple TabStripTest.* to crash
    
    Crash stack:
    E/chromium(18334): [ERROR:java_exception_reporter.cc(38)] java.lang.NullPointerException: Attempt to invoke virtual method 'void org.chromium.base.DiscardableReferencePool.drain()' on a null object reference
    E/chromium(18334): 	at org.chromium.chrome.browser.ChromeApplication.onTrimMemory(ChromeApplication.java:72)
    E/chromium(18334): 	at android.app.ActivityThread.handleTrimMemory(ActivityThread.java:4251)
    E/chromium(18334): 	at android.app.ActivityThread$H.handleMessage(ActivityThread.java:1491)
    E/chromium(18334): 	at android.os.Handler.dispatchMessage(Handler.java:102)
    E/chromium(18334): 	at android.os.Looper.loop(Looper.java:135)
    E/chromium(18334): 	at android.app.ActivityThread.main(ActivityThread.java:5254)
    E/chromium(18334): 	at java.lang.reflect.Method.invoke(Native Method)
    E/chromium(18334): 	at java.lang.reflect.Method.invoke(Method.java:372)
    E/chromium(18334): 	at com.android.internal.os.ZygoteInit$MethodAndArgsCaller.run(ZygoteInit.java:903)
    E/chromium(18334): 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:698)
    E/chromium(18334): 
    
    Original change's description:
    > Split ThumbnailProviderImpl cache into a LRU and a deduplication cache.
    > 
    > Bug: 718925
    > Change-Id: I8bfe2d4b743c8fdb014b2112683aba1f74835753
    > Reviewed-on: https://chromium-review.googlesource.com/582958
    > Reviewed-by: Theresa <twellington@chromium.org>
    > Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    > Commit-Queue: Bernhard Bauer <bauerb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#500574}
    
    TBR=bauerb@chromium.org,mvanouwerkerk@chromium.org,twellington@chromium.org
    
    Change-Id: Ic79a768f6e2c696f01c92e3b569cff04b2e21050
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 718925
    Reviewed-on: https://chromium-review.googlesource.com/658178
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500630}

--- chrome/android/junit/src/org/chromium/chrome/browser/suggestions/ImageFetcherTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/suggestions/ImageFetcherTest.java
@@ -22,7 +22,6 @@ import org.mockito.MockitoAnnotations;
 import org.robolectric.annotation.Config;
 
 import org.chromium.base.Callback;
-import org.chromium.base.DiscardableReferencePool;
 import org.chromium.chrome.browser.DisableHistogramsRule;
 import org.chromium.chrome.browser.NativePageHost;
 import org.chromium.chrome.browser.download.ui.ThumbnailProvider;
@@ -55,10 +54,8 @@ public class ImageFetcherTest {
     @Rule
     public SuggestionsDependenciesRule mSuggestionsDeps = new SuggestionsDependenciesRule();
 
-    private DiscardableReferencePool mReferencePool = new DiscardableReferencePool();
-
     @Mock
-    private FaviconHelper mFaviconHelper;
+    FaviconHelper mFaviconHelper;
     @Mock
     private ThumbnailProvider mThumbnailProvider;
     @Mock
@@ -79,8 +76,8 @@ public class ImageFetcherTest {
 
     @Test
     public void testFaviconFetch() {
-        ImageFetcher imageFetcher = new ImageFetcher(mSuggestionsSource, mock(Profile.class),
-                mReferencePool, mock(NativePageHost.class));
+        ImageFetcher imageFetcher = new ImageFetcher(
+                mSuggestionsSource, mock(Profile.class), mock(NativePageHost.class));
 
         SnippetArticle suggestion = createDummySuggestion(KnownCategories.BOOKMARKS);
         imageFetcher.makeFaviconRequest(suggestion, IMAGE_SIZE_PX, mock(Callback.class));
@@ -94,8 +91,8 @@ public class ImageFetcherTest {
 
     @Test
     public void testDownloadThumbnailFetch() {
-        ImageFetcher imageFetcher = new ImageFetcher(mSuggestionsSource, mock(Profile.class),
-                mReferencePool, mock(NativePageHost.class));
+        ImageFetcher imageFetcher = new ImageFetcher(
+                mSuggestionsSource, mock(Profile.class), mock(NativePageHost.class));
 
         SnippetArticle suggestion = createDummySuggestion(KnownCategories.DOWNLOADS);
 
@@ -109,8 +106,8 @@ public class ImageFetcherTest {
 
     @Test
     public void testArticleThumbnailFetch() {
-        ImageFetcher imageFetcher = new ImageFetcher(mSuggestionsSource, mock(Profile.class),
-                mReferencePool, mock(NativePageHost.class));
+        ImageFetcher imageFetcher = new ImageFetcher(
+                mSuggestionsSource, mock(Profile.class), mock(NativePageHost.class));
 
         SnippetArticle suggestion = createDummySuggestion(KnownCategories.ARTICLES);
         imageFetcher.makeArticleThumbnailRequest(suggestion, mock(Callback.class));
@@ -120,8 +117,8 @@ public class ImageFetcherTest {
 
     @Test
     public void testLargeIconFetch() {
-        ImageFetcher imageFetcher = new ImageFetcher(mSuggestionsSource, mock(Profile.class),
-                mReferencePool, mock(NativePageHost.class));
+        ImageFetcher imageFetcher = new ImageFetcher(
+                mSuggestionsSource, mock(Profile.class), mock(NativePageHost.class));
 
         imageFetcher.makeLargeIconRequest(URL_STRING, IMAGE_SIZE_PX, mock(LargeIconCallback.class));
 
--- chrome/test/android/javatests/src/org/chromium/chrome/test/util/browser/suggestions/SuggestionsDependenciesRule.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/util/browser/suggestions/SuggestionsDependenciesRule.java
@@ -8,7 +8,6 @@ package org.chromium.chrome.test.util.browser.suggestions;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
 
-import org.chromium.base.DiscardableReferencePool;
 import org.chromium.base.annotations.SuppressFBWarnings;
 import org.chromium.chrome.browser.download.ui.ThumbnailProvider;
 import org.chromium.chrome.browser.favicon.FaviconHelper;
@@ -91,9 +90,9 @@ public class SuggestionsDependenciesRule extends TestWatcher {
         }
 
         @Override
-        public ThumbnailProvider createThumbnailProvider(DiscardableReferencePool referencePool) {
+        public ThumbnailProvider createThumbnailProvider() {
             if (thumbnailProvider != null) return thumbnailProvider;
-            return super.createThumbnailProvider(referencePool);
+            return super.createThumbnailProvider();
         }
 
         @Override
