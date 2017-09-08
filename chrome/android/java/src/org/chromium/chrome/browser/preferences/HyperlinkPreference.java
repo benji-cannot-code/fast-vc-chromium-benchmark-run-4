3283a949ed7b1b30eb5fadba326b322dff0a708b
Becky Zhou <huayinz@chromium.org>
2017-09-08 21:27:24 +0000

    [Snowflake] Unify small blue link text and grey text
    + Change blue link text no larger than Reg 16sp or Med 14sp 
      to google blue 700
    + Change "powered by chrome" in CCT to black 54
    + Change #333, #000 to black 87 (except omnibox and toolbar)
    
    Bug: 723040, 723051
    Change-Id: I2c6b05bb6b7539c90c75b8aa790186f6ad23ad50
    Reviewed-on: https://chromium-review.googlesource.com/646582
    Commit-Queue: Becky Zhou <huayinz@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500684}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/HyperlinkPreference.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/HyperlinkPreference.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
 
+import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.customtabs.CustomTabActivity;
 import org.chromium.ui.base.LocalizationUtils;
@@ -24,6 +25,7 @@ public class HyperlinkPreference extends Preference {
 
     private final int mTitleResId;
     private final int mUrlResId;
+    private final int mColor;
     private final boolean mImitateWebLink;
 
     public HyperlinkPreference(Context context, AttributeSet attrs) {
@@ -34,6 +36,7 @@ public class HyperlinkPreference extends Preference {
         mImitateWebLink = a.getBoolean(R.styleable.HyperlinkPreference_imitateWebLink, false);
         a.recycle();
         mTitleResId = getTitleRes();
+        mColor = ApiCompatibilityUtils.getColor(context.getResources(), R.color.google_blue_700);
     }
 
     @Override
@@ -52,7 +55,7 @@ public class HyperlinkPreference extends Preference {
             setSelectable(false);
 
             titleView.setClickable(true);
-            titleView.setTextColor(titleView.getPaint().linkColor);
+            titleView.setTextColor(mColor);
             titleView.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
