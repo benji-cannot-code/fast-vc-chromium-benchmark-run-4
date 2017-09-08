cbbb426643d66dc10088eeb9f83d9be91a028a6e
Satyajit Sahu <satyajit.sahu@amd.com>
2017-09-08 17:33:26 +0000

    Enable GPU sandboxing for AMD platform
    Default sandboxing fails for AMD platform as the GPU process spawns
    multiple threads. So GPU sandboxing needs to be started early. And
    all dependent libraries need to be preloaded.
    
    BUG=b:63252288
    TEST=after adding gpu-sandbox-start-early flag GPU sandbox is
    successful and UI also comes up
    
    Change-Id: I97032f2404bfbd2d2c36cc3a4d2b203ca7e6d75d
    Signed-off-by: Satyajit Sahu <satyajit.sahu@amd.com>
    Reviewed-on: https://chromium-review.googlesource.com/618427
    Commit-Queue: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500617}

--- content/common/sandbox_linux/sandbox_seccomp_bpf_linux.h
+++ content/common/sandbox_linux/sandbox_seccomp_bpf_linux.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/scoped_file.h"
 #include "base/macros.h"
+#include "gpu/config/gpu_info.h"
 #include "sandbox/linux/bpf_dsl/policy.h"
 
 namespace content {
@@ -35,7 +36,8 @@ class SandboxSeccompBPF {
   // Start the sandbox and apply the policy for process_type, depending on
   // command line switches.
   static bool StartSandbox(const std::string& process_type,
-                           base::ScopedFD proc_fd);
+                           base::ScopedFD proc_fd,
+                           const gpu::GPUInfo* gpu_info);
 
   // This is the API to enable a seccomp-bpf sandbox by using an
   // external policy.
