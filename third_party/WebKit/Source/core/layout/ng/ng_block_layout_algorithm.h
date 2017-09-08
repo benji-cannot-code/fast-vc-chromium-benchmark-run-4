b02b0f4021c2c7e764831c955e743ea94f5edb5b
Morten Stenshorne <mstensho@opera.com>
2017-09-08 17:22:11 +0000

    [LayoutNG] Ignore block-start margin in subsequent fragments.
    The block-start margin only matters in the first fragmentainer where the node
    occurs. This used to trigger DCHECK failures.
    
    The new unit test used to DCHECK-fail (but would pass with DCHECKs disabled).
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I8ee47f0d08a989f121c5bd8f18806398a4d6d837
    Reviewed-on: https://chromium-review.googlesource.com/657182
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@opera.com>
    Cr-Commit-Position: refs/heads/master@{#500614}

--- third_party/WebKit/Source/core/layout/ng/ng_block_layout_algorithm.h
+++ third_party/WebKit/Source/core/layout/ng/ng_block_layout_algorithm.h
@@ -68,7 +68,8 @@ class CORE_EXPORT NGBlockLayoutAlgorithm
   virtual RefPtr<NGLayoutResult> Layout() override;
 
  private:
-  NGBoxStrut CalculateMargins(NGLayoutInputNode child);
+  NGBoxStrut CalculateMargins(NGLayoutInputNode child,
+                              const NGBreakToken* child_break_token);
 
   // Creates a new constraint space for the current child.
   RefPtr<NGConstraintSpace> CreateConstraintSpaceForChild(
@@ -78,7 +79,8 @@ class CORE_EXPORT NGBlockLayoutAlgorithm
 
   // @return Estimated BFC offset for the "to be layout" child.
   NGInflowChildData ComputeChildData(const NGPreviousInflowPosition&,
-                                     NGLayoutInputNode);
+                                     NGLayoutInputNode,
+                                     const NGBreakToken* child_break_token);
 
   NGPreviousInflowPosition ComputeInflowPosition(
       const NGPreviousInflowPosition& previous_inflow_position,
