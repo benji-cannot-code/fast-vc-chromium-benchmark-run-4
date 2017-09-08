ae131cf840956b3763eebd8b902840f2f3d7becd
Justin DeWitt <dewittj@chromium.org>
2017-09-08 23:39:36 +0000

    [Offline Pages] Fix crash in PrefetchBackgroundTask#onStopTaskWithNative
    This allows onStopWithNative to be called even after doneProcessing has
    finished, which appears to happen in the wild when we race with
    JobScheduler to report that we are done.
    
    Bug: 762045
    Change-Id: I075a63dd1c315f9fdb8d60352a5fc8e18acf65a2
    Reviewed-on: https://chromium-review.googlesource.com/658417
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500740}

--- chrome/android/java/src/org/chromium/chrome/browser/offlinepages/prefetch/PrefetchBackgroundTask.java
+++ chrome/android/java/src/org/chromium/chrome/browser/offlinepages/prefetch/PrefetchBackgroundTask.java
@@ -44,6 +44,11 @@ public class PrefetchBackgroundTask extends NativeBackgroundTask {
     private long mNativeTask = 0;
     private TaskFinishedCallback mTaskFinishedCallback = null;
     private Profile mProfile = null;
+    // We update this when we call TaskFinishedCallback, so that subsequent calls to
+    // onStopTask* can respond the same way.  This is possible due to races with the JobScheduler.
+    // Defaults to true so that we are rescheduled automatically if somehow we were unable to start
+    // up native.
+    private boolean mCachedRescheduleResult = true;
 
     public PrefetchBackgroundTask() {}
 
@@ -137,14 +142,15 @@ public class PrefetchBackgroundTask extends NativeBackgroundTask {
 
     @Override
     protected boolean onStopTaskBeforeNativeLoaded(Context context, TaskParameters taskParameters) {
-        // TODO(dewittj): Implement this properly.
-        return true;
+        return mCachedRescheduleResult;
     }
 
     @Override
     protected boolean onStopTaskWithNative(Context context, TaskParameters taskParameters) {
         assert taskParameters.getTaskId() == TaskIds.OFFLINE_PAGES_PREFETCH_JOB_ID;
-        assert mNativeTask != 0;
+        // Sometimes we can race with JobScheduler and receive a stop call after we have called the
+        // TaskFinishedCallback, so we need to save the reschedule result.
+        if (mNativeTask == 0) return mCachedRescheduleResult;
 
         return nativeOnStopTask(mNativeTask);
     }
@@ -173,6 +179,7 @@ public class PrefetchBackgroundTask extends NativeBackgroundTask {
     @CalledByNative
     void doneProcessing(boolean needsReschedule) {
         assert mTaskFinishedCallback != null;
+        mCachedRescheduleResult = needsReschedule;
         mTaskFinishedCallback.taskFinished(needsReschedule);
         setNativeTask(0);
     }
