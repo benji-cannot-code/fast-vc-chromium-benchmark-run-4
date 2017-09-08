db4fc16bfe5d1fc55841ec38cab545b7cb76ee2e
Mitsuru Oshima <oshima@chromium.org>
2017-09-08 00:41:35 +0000

    Don't move lock screen wallpaper window in unified desktop mode.
    It's not clear why I did this, but wallpaper window gets
    created on the new root window.
    
    Bug=761413
    Test=Updated the unit test. Tested on device too.
    
    Change-Id: Id165cc72897ae644e087d980c227b109a804e96d
    Reviewed-on: https://chromium-review.googlesource.com/653362
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500454}

--- ash/root_window_controller_unittest.cc
+++ ash/root_window_controller_unittest.cc
@@ -305,28 +305,19 @@ TEST_F(RootWindowControllerTest, MoveWindows_LockWindowsInUnified) {
 
   UpdateDisplay("500x500");
   const int kLockScreenWindowId = 1000;
-  const int kLockWallpaperWindowId = 1001;
 
   RootWindowController* controller = Shell::GetPrimaryRootWindowController();
 
   aura::Window* lock_container =
       controller->GetContainer(kShellWindowId_LockScreenContainer);
-  aura::Window* lock_wallpaper_container =
-      controller->GetContainer(kShellWindowId_LockScreenWallpaperContainer);
 
   views::Widget* lock_screen =
       CreateModalWidgetWithParent(gfx::Rect(10, 10, 100, 100), lock_container);
   lock_screen->GetNativeWindow()->set_id(kLockScreenWindowId);
   lock_screen->SetFullscreen(true);
 
-  views::Widget* lock_wallpaper = CreateModalWidgetWithParent(
-      gfx::Rect(10, 10, 100, 100), lock_wallpaper_container);
-  lock_wallpaper->GetNativeWindow()->set_id(kLockWallpaperWindowId);
-
   ASSERT_EQ(lock_screen->GetNativeWindow(),
             controller->GetRootWindow()->GetChildById(kLockScreenWindowId));
-  ASSERT_EQ(lock_wallpaper->GetNativeWindow(),
-            controller->GetRootWindow()->GetChildById(kLockWallpaperWindowId));
   EXPECT_EQ("0,0 500x500", lock_screen->GetNativeWindow()->bounds().ToString());
 
   // Switch to unified.
@@ -337,8 +328,6 @@ TEST_F(RootWindowControllerTest, MoveWindows_LockWindowsInUnified) {
 
   ASSERT_EQ(lock_screen->GetNativeWindow(),
             controller->GetRootWindow()->GetChildById(kLockScreenWindowId));
-  ASSERT_EQ(lock_wallpaper->GetNativeWindow(),
-            controller->GetRootWindow()->GetChildById(kLockWallpaperWindowId));
   EXPECT_EQ("0,0 500x500", lock_screen->GetNativeWindow()->bounds().ToString());
 
   // Switch to mirror.
@@ -348,8 +337,6 @@ TEST_F(RootWindowControllerTest, MoveWindows_LockWindowsInUnified) {
   controller = Shell::GetPrimaryRootWindowController();
   ASSERT_EQ(lock_screen->GetNativeWindow(),
             controller->GetRootWindow()->GetChildById(kLockScreenWindowId));
-  ASSERT_EQ(lock_wallpaper->GetNativeWindow(),
-            controller->GetRootWindow()->GetChildById(kLockWallpaperWindowId));
   EXPECT_EQ("0,0 500x500", lock_screen->GetNativeWindow()->bounds().ToString());
 
   // Switch to unified.
@@ -360,8 +347,6 @@ TEST_F(RootWindowControllerTest, MoveWindows_LockWindowsInUnified) {
 
   ASSERT_EQ(lock_screen->GetNativeWindow(),
             controller->GetRootWindow()->GetChildById(kLockScreenWindowId));
-  ASSERT_EQ(lock_wallpaper->GetNativeWindow(),
-            controller->GetRootWindow()->GetChildById(kLockWallpaperWindowId));
   EXPECT_EQ("0,0 500x500", lock_screen->GetNativeWindow()->bounds().ToString());
 
   // Switch to single display.
@@ -373,8 +358,6 @@ TEST_F(RootWindowControllerTest, MoveWindows_LockWindowsInUnified) {
 
   ASSERT_EQ(lock_screen->GetNativeWindow(),
             controller->GetRootWindow()->GetChildById(kLockScreenWindowId));
-  ASSERT_EQ(lock_wallpaper->GetNativeWindow(),
-            controller->GetRootWindow()->GetChildById(kLockWallpaperWindowId));
   EXPECT_EQ("0,0 600x500", lock_screen->GetNativeWindow()->bounds().ToString());
 }
 
