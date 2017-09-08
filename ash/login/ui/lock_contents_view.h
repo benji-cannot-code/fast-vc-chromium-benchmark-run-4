3eb62f359b245f37e560bf01d94caeea2dd85431
Sarah Hu <xiaoyinh@chromium.org>
2017-09-08 23:10:38 +0000

    Cros: Add bubbles in the views-based lockscreen
    This CL does the following:
    1. Create bubble view for error bubble and user drop down menu bubble
    2. Add pre-target handler for key events, mouse events and touch events
    to dismiss the bubble when necessary.
    
    Bug: 719015
    Change-Id: I45a0339fd1dcffb7ce143a72a72acdf4f378bcee
    Reviewed-on: https://chromium-review.googlesource.com/647877
    Commit-Queue: Xiaoyin Hu <xiaoyinh@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500727}

--- ash/login/ui/lock_contents_view.h
+++ ash/login/ui/lock_contents_view.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_LOGIN_UI_CONTENTS_VIEW_H_
 #define ASH_LOGIN_UI_CONTENTS_VIEW_H_
 
+#include <memory>
+
 #include "ash/ash_export.h"
 #include "ash/login/ui/login_data_dispatcher.h"
 #include "ash/system/status_area_focus_observer.h"
@@ -24,6 +26,7 @@ class ScrollView;
 namespace ash {
 
 class LoginAuthUserView;
+class LoginBubble;
 class LoginUserView;
 
 // LockContentsView hosts the root view for the lock screen. All other lock
@@ -128,6 +131,9 @@ class ASH_EXPORT LockContentsView : public views::View,
   // Get the LoginAuthUserView of the current auth user.
   LoginAuthUserView* CurrentAuthUserView();
 
+  // Opens an error bubble to indicate authentication failure.
+  void ShowErrorMessage();
+
   std::vector<UserState> users_;
 
   LoginDataDispatcher* const data_dispatcher_;  // Unowned.
@@ -146,6 +152,9 @@ class ASH_EXPORT LockContentsView : public views::View,
   std::vector<OnRotate> rotation_actions_;
 
   ScopedObserver<display::Screen, display::DisplayObserver> display_observer_;
+
+  std::unique_ptr<LoginBubble> error_bubble_;
+  int unlock_attempt_ = 0;
 
   DISALLOW_COPY_AND_ASSIGN(LockContentsView);
 };
--- ash/login/ui/login_user_view.h
+++ ash/login/ui/login_user_view.h
@@ -14,10 +14,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/view.h"
 
 namespace views {
-class ImageView;
+class ImageButton;
 }
 
 namespace ash {
+
+class LoginBubble;
 
 // Display the user's profile icon, name, and a menu icon in various layout
 // styles.
@@ -96,7 +98,8 @@ class ASH_EXPORT LoginUserView : public views::Button,
   LoginDisplayStyle display_style_;
   UserImage* user_image_ = nullptr;
   UserLabel* user_label_ = nullptr;
-  views::ImageView* user_dropdown_ = nullptr;
+  views::ImageButton* user_dropdown_ = nullptr;
+  std::unique_ptr<LoginBubble> user_menu_;
 
   // True iff the view is currently opaque (ie, opacity = 1).
   bool is_opaque_ = false;
