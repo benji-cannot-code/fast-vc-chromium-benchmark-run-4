e97f89c37c97cd3330888c74f7b6a46f6f49b786
Charles Harrison <csharrison@chromium.org>
2017-09-08 06:17:31 +0000

    [subresource_filter] Remove CHROME_BRAND ifdefs for browsertests
    Currently, tests have no way of accessing the SubresourceFilter list
    without being a chrome branded build. This is non-ideal since there
    could be tests which fail on the waterfall but not on the CQ.
    
    This patch adds some safe browsing instrumentation to automatically
    insert the store on database initialization if it isn't present.
    
    Bug: None
    Change-Id: I1ff6e48f3dfed705f61fcc23f9b864d8021e8671
    Reviewed-on: https://chromium-review.googlesource.com/651107
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500522}

--- components/safe_browsing_db/v4_database.h
+++ components/safe_browsing_db/v4_database.h
@@ -21,9 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing_db/v4_protocol_manager_util.h"
 #include "components/safe_browsing_db/v4_store.h"
 
-namespace subresource_filter {
-class SubresourceFilterBrowserTest;
-}
+class TestSafeBrowsingDatabaseHelper;
 
 namespace safe_browsing {
 
@@ -180,7 +178,7 @@ class V4Database {
   const std::unique_ptr<StoreMap> store_map_;
 
  private:
-  friend class subresource_filter::SubresourceFilterBrowserTest;
+  friend class ::TestSafeBrowsingDatabaseHelper;
   friend class V4DatabaseFactory;
   friend class V4DatabaseTest;
   friend class V4SafeBrowsingServiceTest;
