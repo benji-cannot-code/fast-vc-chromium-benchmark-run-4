76bf6a60fa142ef7c599f9dfbfb57c30cd7bc5f1
Conley Owens <cco3@chromium.org>
2017-09-08 17:23:10 +0000

    bluetooth: Add SetNextSubscribeToNotificationsResp
    This change adds a SetNextSubscribeToNotificationsResponse method to the
    testing API.  This may be called to ensure that the next time a
    subscription is attempted, it succeeds or fails with the appropriate
    error code.
    
    BUG=569709
    
    Change-Id: I6983468aac262e2e9bea3716d3c527a33aa77417
    Reviewed-on: https://chromium-review.googlesource.com/653372
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Vincent Scheib <scheib@chromium.org>
    Commit-Queue: Conley Owens <cco3@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500615}

--- third_party/WebKit/LayoutTests/resources/bluetooth/web-bluetooth-test.js
+++ third_party/WebKit/LayoutTests/resources/bluetooth/web-bluetooth-test.js
@@ -351,6 +351,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       if (!success) throw 'setNextWriteResponse failed';
     }
 
+    // Sets the next subscribe to notifications response for characteristic with
+    // |characteristic_id| in |service_id| and in |peripheral_address| to
+    // |code|. |code| could be a GATT Error Response from BT 4.2 Vol 3 Part F
+    // 3.4.1.1 Error Response or a number outside that range returned by
+    // specific platforms e.g. Android returns 0x101 to signal a GATT failure.
+    async setNextSubscribeToNotificationsResponse(gatt_code) {
+      let {success} =
+        await this.fake_central_ptr_.setNextSubscribeToNotificationsResponse(
+          gatt_code, ...this.ids_);
+
+      if (!success) throw 'setNextSubscribeToNotificationsResponse failed';
+    }
+
     // Gets the last successfully written value to the characteristic.
     // Returns null if no value has yet been written to the characteristic.
     async getLastWrittenValue() {
@@ -390,7 +403,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     // https://developer.android.com/reference/android/bluetooth/BluetoothGatt.html#GATT_FAILURE
     async setNextReadResponse(gatt_code, value=null) {
       if (gatt_code === 0 && value === null) {
-        throw '|value| can\'t be null if read should success.';
+        throw '|value| cannot be null if read should succeed.';
       }
       if (gatt_code !== 0 && value !== null) {
         throw '|value| must be null if read should fail.';
