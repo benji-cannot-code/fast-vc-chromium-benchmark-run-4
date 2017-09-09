a6c6d3a0b538ab11832f8560defc4b779ecc3fc0
Gauri Manikpure <gmanikpure@chromium.org>
2017-09-09 01:25:07 +0000

    [Chromedriver] Drop support for M59 and add M62.
    Bug: 
    Change-Id: I618fc514ede43568f4259e6f660fa80e4746f4f5
    Reviewed-on: https://chromium-review.googlesource.com/658118
    Reviewed-by: Shuotao Gao <stgao@chromium.org>
    Commit-Queue: Gauri Manikpure <gmanikpure@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500771}

--- chrome/test/chromedriver/chrome/version.cc
+++ chrome/test/chromedriver/chrome/version.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // This variable must be able to be found and parsed by the upload script.
-const int kMinimumSupportedChromeVersion[] = {59, 0, 3071, 0};
+const int kMinimumSupportedChromeVersion[] = {60, 0, 3112, 0};
 
 }  // namespace
 
