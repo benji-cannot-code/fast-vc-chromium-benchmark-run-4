4a949d0a8b00569a4be2cfc6260663a53f3b5ee0
Chung-Sheng Wu <chungsheng@google.com>
2017-09-11 08:07:09 +0000

    [CrOS] SysInternals: Line chart library
    Shows information like CPU usage, Memory status, Zram status.
    
    This patch adds the implementation for the Internal Page line chart
    library. The line chart library maintain a HTML canvas and show the
    data with a scrollable line chart.
    
    
    Bug: 752002
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ifd7a1aadb95693dce2c88202486b951e94d86513
    Reviewed-on: https://chromium-review.googlesource.com/649390
    Reviewed-by: Naoki Fukino <fukino@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Commit-Queue: Zhong-sheng Wu <chungsheng@google.com>
    Cr-Commit-Position: refs/heads/master@{#500858}

---
+++ chrome/browser/resources/sys_internals/line_chart/index.js
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// <include src="constants.js">
+// <include src="data_series.js">
+// <include src="unit_label.js">
+// <include src="line_chart.js">
+// <include src="menu.js">
+// <include src="scrollbar.js">
+// <include src="sub_chart.js">
---
+++ chrome/test/data/webui/sys_internals/line_chart/scrollbar_test.js
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var LineChartTest = LineChartTest || {};
+
+LineChartTest.Scrollbar = function() {
+  test('Scrollbar integration test', function() {
+    const scrollbar = new LineChart.Scrollbar(function() {});
+    scrollbar.resize(100);
+    scrollbar.setRange(1000);
+
+    /* See |LineChart.Scrollbar.isScrolledToRightEdge()|. */
+    const scrollError = 2;
+    assertFalse(scrollbar.isScrolledToRightEdge());
+    TestUtil.assertCloseTo(scrollbar.getPosition(), 0, scrollError);
+    scrollbar.scrollToRightEdge();
+    assertTrue(scrollbar.isScrolledToRightEdge());
+    TestUtil.assertCloseTo(scrollbar.getPosition(), 1000, scrollError);
+    scrollbar.setPosition(500);
+    assertFalse(scrollbar.isScrolledToRightEdge());
+    TestUtil.assertCloseTo(scrollbar.getPosition(), 500, scrollError);
+    scrollbar.setRange(100);
+    assertTrue(scrollbar.isScrolledToRightEdge());
+    TestUtil.assertCloseTo(scrollbar.getPosition(), 100, scrollError);
+  });
+
+  mocha.run();
+};
