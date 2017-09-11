3cd75bd2b56557874025c2ff68de677f4099445b
Xiaocheng Hu <xiaochengh@chromium.org>
2017-09-11 22:30:15 +0000

    Fix InputMethodController::IsAvailable and GetDocument
    The current implementation of InputMethodController::IsAvailable only
    checks the existence of the frame's document, which can be wrong when
    the document is detached and inactive. This patch changes it to check
    LifecycleContext() instead to correct the issue.
    
    GetDocument() is also changed accordingly to make the implementations
    more consistent.
    
    This patch actually catches a bug in ComputeWebTextInputNextPreviousFlags
    that if can be called with inactive document, which is also fixed.
    
    Bug: 657256
    Change-Id: I56c644ba1e5509fe9e3e89c3c72b28fdceb134b1
    Reviewed-on: https://chromium-review.googlesource.com/661162
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Ryan Landay <rlanday@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501065}

--- third_party/WebKit/Source/core/editing/InputMethodController.cpp
+++ third_party/WebKit/Source/core/editing/InputMethodController.cpp
@@ -302,12 +302,12 @@ InputMethodController::InputMethodController(LocalFrame& frame)
 InputMethodController::~InputMethodController() = default;
 
 bool InputMethodController::IsAvailable() const {
-  return GetFrame().GetDocument();
+  return LifecycleContext();
 }
 
 Document& InputMethodController::GetDocument() const {
   DCHECK(IsAvailable());
-  return *GetFrame().GetDocument();
+  return *LifecycleContext();
 }
 
 bool InputMethodController::HasComposition() const {
@@ -1185,6 +1185,9 @@ int InputMethodController::TextInputFlags() const {
 }
 
 int InputMethodController::ComputeWebTextInputNextPreviousFlags() const {
+  if (!IsAvailable())
+    return kWebTextInputFlagNone;
+
   Element* const element = GetDocument().FocusedElement();
   if (!element)
     return kWebTextInputFlagNone;
