e9a29b64616ae0e357af7a34c6dc2d8b68042118
Regan Hsu <hsuregan@google.com>
2017-09-11 18:16:01 +0000

    [CrOS MultiDevice] Create a FakeDeviceSync class to test DeviceSync service.
    The FakeDeviceSync class will be used to test the service itself
    rather than the implementation.
    
    Bug: 752273
    Change-Id: I7a789e2a3dce0dc2510e4a321611bdda3eb57518
    Reviewed-on: https://chromium-review.googlesource.com/658120
    Commit-Queue: Regan Hsu <hsuregan@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500977}

--- components/multidevice/service/device_sync_impl.cc
+++ components/multidevice/service/device_sync_impl.cc
@@ -7,6 +7,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace multidevice {
 
+// static
+DeviceSyncImpl::Factory* DeviceSyncImpl::Factory::factory_instance_ = nullptr;
+
+// static
+std::unique_ptr<device_sync::mojom::DeviceSync>
+DeviceSyncImpl::Factory::NewInstance(
+    std::unique_ptr<service_manager::ServiceContextRef> service_ref) {
+  if (!factory_instance_) {
+    factory_instance_ = new Factory();
+  }
+  return factory_instance_->BuildInstance(std::move(service_ref));
+}
+
+DeviceSyncImpl::Factory::~Factory() {}
+
+// static
+void DeviceSyncImpl::Factory::SetInstanceForTesting(Factory* factory) {
+  factory_instance_ = factory;
+}
+
+std::unique_ptr<device_sync::mojom::DeviceSync>
+DeviceSyncImpl::Factory::BuildInstance(
+    std::unique_ptr<service_manager::ServiceContextRef> service_ref) {
+  return base::WrapUnique(new DeviceSyncImpl(std::move(service_ref)));
+}
+
 DeviceSyncImpl::DeviceSyncImpl(
     std::unique_ptr<service_manager::ServiceContextRef> service_ref)
     : service_ref_(std::move(service_ref)) {}
--- components/multidevice/service/multidevice_service.h
+++ components/multidevice/service/multidevice_service.h
@@ -20,6 +20,7 @@ namespace multidevice {
 class MultiDeviceService : public service_manager::Service {
  public:
   MultiDeviceService();
+
   ~MultiDeviceService() override;
 
   // service_manager::Service:
