07a5891d14332a4a7defbd4f182f402878f0fce7
Erik Chen <erikchen@chromium.org>
2017-09-11 23:27:26 +0000

    memlog: Fill out End-to-end browser tests.
    Create a test that:
    1) Starts the memlog process
    2) Makes a bunch of allocations
    3) Flush the allocations [with more allocations]
    4) Performs a heap dump.
    5) Checks that the allocations from (2) made it into (4).
    
    Bug: 751321
    Change-Id: Ic9e2511da06ee436075eb91714616efdae988c4f
    Reviewed-on: https://chromium-review.googlesource.com/653430
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501093}

--- chrome/browser/profiling_host/profiling_process_host.h
+++ chrome/browser/profiling_host/profiling_process_host.h
@@ -81,7 +81,9 @@ class ProfilingProcessHost : public content::BrowserChildProcessObserver,
 
   // Sends a message to the profiling process that it dump the given process'
   // memory data to the given file.
-  void RequestProcessDump(base::ProcessId pid, const base::FilePath& dest);
+  void RequestProcessDump(base::ProcessId pid,
+                          const base::FilePath& dest,
+                          base::OnceClosure done);
 
   // Sends a message to the profiling process that it report the given process'
   // memory data to the crash server (slow-report).
@@ -135,15 +137,18 @@ class ProfilingProcessHost : public content::BrowserChildProcessObserver,
   void GetOutputFileOnBlockingThread(base::ProcessId pid,
                                      const base::FilePath& dest,
                                      std::string trigger_name,
-                                     bool upload);
+                                     bool upload,
+                                     base::OnceClosure done);
   void HandleDumpProcessOnIOThread(base::ProcessId pid,
                                    base::FilePath file_path,
                                    base::File file,
                                    std::string trigger_name,
-                                   bool upload);
+                                   bool upload,
+                                   base::OnceClosure done);
   void OnProcessDumpComplete(base::FilePath file_path,
                              std::string trigger_name,
                              bool upload,
+                             base::OnceClosure done,
                              bool success);
 
   // Returns the metadata for the trace. This is the minimum amount of metadata
