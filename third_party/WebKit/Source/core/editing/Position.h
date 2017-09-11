bfb6f941b878b8ecde9897daf10ee25e618de5b5
Ryan Landay <rlanday@chromium.org>
2017-09-11 05:26:47 +0000

    Reland "Add VisiblePositionTemplate::IsValidFor()"
    This is a reland of f496799edcce9c160708df0af17718dbed0f1ec7
    Original change's description:
    > Add VisiblePositionTemplate::IsValidFor()
    > 
    > This method allows us to check if a position is attached to a specified
    > Document. It is analogous to SelectionTemplate::IsValidFor() and
    > SelectionForUndoStep::IsValidFor(). I'm also adding
    > PositionTemplate::IsValidFor() and
    > PositionWithAffinityTemplate::IsValidFor() to implement
    > VisiblePositionTemplate::IsValidFor() on top of.
    > 
    > For Position, we can just check IsOrphan() to determine if the position
    > is connected to the specified document, but for PositionInFlatTree, we
    > need to walk all the way to to the root, since we may be connected to
    > the root of a shadow tree but not actually connected to the document.
    > 
    > Bug: 759971
    > Change-Id: I04fa7b065bc894f6c29928e58083efa3f2d5315c
    > Reviewed-on: https://chromium-review.googlesource.com/644575
    > Commit-Queue: Ryan Landay <rlanday@chromium.org>
    > Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    > Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#500541}
    
    Bug: 759971
    Change-Id: I39686db17e09be6734279066c531aefe6dfb2966
    Reviewed-on: https://chromium-review.googlesource.com/659418
    Reviewed-by: Ryan Landay <rlanday@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500839}

--- third_party/WebKit/Source/core/editing/Position.h
+++ third_party/WebKit/Source/core/editing/Position.h
@@ -159,6 +159,8 @@ class CORE_TEMPLATE_CLASS_EXPORT PositionTemplate {
     return anchor_node_ && anchor_node_->isConnected();
   }
 
+  bool IsValidFor(const Document&) const;
+
   bool IsNull() const { return !anchor_node_; }
   bool IsNotNull() const { return anchor_node_; }
   bool IsOrphan() const { return anchor_node_ && !anchor_node_->isConnected(); }
