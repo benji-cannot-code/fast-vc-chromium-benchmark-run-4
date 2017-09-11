a14841a9f9c6d6f16564991e1b5178fd095071bd
Dan Elphick <delphick@chromium.org>
2017-09-11 12:06:28 +0000

    Add missing virtual destructor to Controller
    Change-Id: Ie7e004acdaf1c991f5a5ec653cb61a622331a22f
    Reviewed-on: https://chromium-review.googlesource.com/659657
    Reviewed-by: Dan Elphick <delphick@chromium.org>
    Reviewed-by: John Mellor <johnme@chromium.org>
    Commit-Queue: John Mellor <johnme@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500890}

--- content/browser/background_fetch/background_fetch_delegate_proxy.h
+++ content/browser/background_fetch/background_fetch_delegate_proxy.h
@@ -39,6 +39,8 @@ class CONTENT_EXPORT BackgroundFetchDelegateProxy {
     // Called when the given |request| has been completed.
     virtual void DidCompleteRequest(
         const scoped_refptr<BackgroundFetchRequestInfo>& request) = 0;
+
+    virtual ~Controller() {}
   };
 
   explicit BackgroundFetchDelegateProxy(
--- content/browser/background_fetch/background_fetch_job_controller.h
+++ content/browser/background_fetch/background_fetch_job_controller.h
@@ -41,7 +41,7 @@ class CONTENT_EXPORT BackgroundFetchJobController final
       const BackgroundFetchOptions& options,
       BackgroundFetchDataManager* data_manager,
       CompletedCallback completed_callback);
-  ~BackgroundFetchJobController();
+  ~BackgroundFetchJobController() override;
 
   // Starts fetching the first few requests. The controller will continue to
   // fetch new content until all requests have been handled.
