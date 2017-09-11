29193f889efc1bf07e89dde1df61fddd49811a23
Alex Chu <alexchu@google.com>
2017-09-11 19:46:34 +0000

    Remove references to android.webkit.WebSettings.ZoomDensity from AwContents.
    Change-Id: Iceea304e4d28017e294d840ca5cd31ef152f3c0d
    Reviewed-on: https://chromium-review.googlesource.com/651747
    Commit-Queue: Alex Chu <alexchu@google.com>
    Reviewed-by: Tobias Sargeant <tobiasjs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500999}

--- android_webview/java/src/org/chromium/android_webview/AwSettings.java
+++ android_webview/java/src/org/chromium/android_webview/AwSettings.java
@@ -16,7 +16,6 @@ import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebSettings.LayoutAlgorithm;
 import android.webkit.WebSettings.PluginState;
-import android.webkit.WebSettings.ZoomDensity;
 
 import org.chromium.base.BuildInfo;
 import org.chromium.base.ThreadUtils;
@@ -670,17 +669,6 @@ public class AwSettings {
         synchronized (mAwSettingsLock) {
             return getTextSizePercentLocked();
         }
-    }
-
-    public void setDefaultZoom(ZoomDensity zoom) {
-        if (zoom != ZoomDensity.MEDIUM) {
-            Log.w(LOGTAG, "setDefaultZoom not supported, zoom=" + zoom);
-        }
-    }
-
-    public ZoomDensity getDefaultZoom() {
-        // Intentional no-op.
-        return ZoomDensity.MEDIUM;
     }
 
     @CalledByNative
