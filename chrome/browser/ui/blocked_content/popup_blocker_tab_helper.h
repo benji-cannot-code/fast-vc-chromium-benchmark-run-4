e1253184015082210345b1cf0496b954645b045a
Charles Harrison <csharrison@chromium.org>
2017-09-11 17:57:50 +0000

    Popup metrics: Log popup position at click through time
    This allows us to see if a user is e.g. getting a bunch of spammy
    popups at first and then one last "legitimate" one.
    
    Bug: 762617
    Change-Id: I5ea40208930fe69b4f75c57db36170ec55029f25
    Reviewed-on: https://chromium-review.googlesource.com/658287
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500970}

--- chrome/browser/ui/blocked_content/popup_blocker_tab_helper.h
+++ chrome/browser/ui/blocked_content/popup_blocker_tab_helper.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <memory>
 
-#include "base/containers/id_map.h"
 #include "base/macros.h"
 #include "base/observer_list.h"
 #include "base/optional.h"
@@ -39,6 +38,17 @@ class PopupBlockerTabHelper
   // Mapping from popup IDs to blocked popup requests.
   typedef std::map<int32_t, GURL> PopupIdMap;
 
+  // This class backs a histogram. Make sure you update enums.xml if you make
+  // any changes.
+  enum class PopupPosition : int {
+    kOnlyPopup,
+    kFirstPopup,
+    kMiddlePopup,
+    kLastPopup,
+
+    // Any new values should go before this one.
+    kLast,
+  };
   class Observer {
    public:
     virtual void BlockedPopupAdded(int32_t id, const GURL& url) {}
@@ -97,9 +107,15 @@ class PopupBlockerTabHelper
   // Called when the blocked popup notification is shown or hidden.
   void PopupNotificationVisibilityChanged(bool visible);
 
-  base::IDMap<std::unique_ptr<BlockedRequest>> blocked_popups_;
+  PopupPosition GetPopupPosition(int32_t id) const;
+
+  // Note, this container should be sorted based on the position in the popup
+  // list, so it is keyed by an id which is continually increased.
+  std::map<int32_t, std::unique_ptr<BlockedRequest>> blocked_popups_;
 
   base::ObserverList<Observer> observers_;
+
+  int32_t next_id_ = 0;
 
   DISALLOW_COPY_AND_ASSIGN(PopupBlockerTabHelper);
 };
