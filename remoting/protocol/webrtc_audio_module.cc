ad9cf884120428708a697a8421062138ec77fe84
henrika <henrika@chromium.org>
2017-09-11 08:33:10 +0000

    Removes unused APIs in WebRTC's audio device module (ADM).
    Goal is to match recent changes in third_party/webrtc/modules/audio_device/include/audio_device.h
    
    Bug: webrtc:7306
    Change-Id: I5c97c12886f073e3c9d6dafe355e94cee8fa0d49
    Reviewed-on: https://chromium-review.googlesource.com/657399
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Commit-Queue: Henrik Andreasson <henrika@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500859}

--- remoting/protocol/webrtc_audio_module.cc
+++ remoting/protocol/webrtc_audio_module.cc
@@ -205,18 +205,6 @@ bool WebrtcAudioModule::AGC() const {
   return false;
 }
 
-int32_t WebrtcAudioModule::SetWaveOutVolume(uint16_t volume_left,
-                                            uint16_t volume_right) {
-  NOTREACHED();
-  return -1;
-}
-
-int32_t WebrtcAudioModule::WaveOutVolume(uint16_t* volume_left,
-                                         uint16_t* volume_right) const {
-  NOTREACHED();
-  return -1;
-}
-
 int32_t WebrtcAudioModule::InitSpeaker() {
   return 0;
 }
@@ -258,11 +246,6 @@ int32_t WebrtcAudioModule::MinSpeakerVolume(uint32_t* min_volume) const {
   return -1;
 }
 
-int32_t WebrtcAudioModule::SpeakerVolumeStepSize(uint16_t* step_size) const {
-  NOTREACHED();
-  return -1;
-}
-
 int32_t WebrtcAudioModule::MicrophoneVolumeIsAvailable(bool* available) {
   NOTREACHED();
   return -1;
@@ -284,11 +267,6 @@ int32_t WebrtcAudioModule::MaxMicrophoneVolume(uint32_t* max_volume) const {
 }
 
 int32_t WebrtcAudioModule::MinMicrophoneVolume(uint32_t* min_volume) const {
-  NOTREACHED();
-  return -1;
-}
-
-int32_t WebrtcAudioModule::MicrophoneVolumeStepSize(uint16_t* step_size) const {
   NOTREACHED();
   return -1;
 }
@@ -319,21 +297,6 @@ int32_t WebrtcAudioModule::SetMicrophoneMute(bool enable) {
 }
 
 int32_t WebrtcAudioModule::MicrophoneMute(bool* enabled) const {
-  NOTREACHED();
-  return -1;
-}
-
-int32_t WebrtcAudioModule::MicrophoneBoostIsAvailable(bool* available) {
-  NOTREACHED();
-  return -1;
-}
-
-int32_t WebrtcAudioModule::SetMicrophoneBoost(bool enable) {
-  NOTREACHED();
-  return -1;
-}
-
-int32_t WebrtcAudioModule::MicrophoneBoost(bool* enabled) const {
   NOTREACHED();
   return -1;
 }
@@ -376,51 +339,12 @@ int32_t WebrtcAudioModule::RecordingChannel(ChannelType* channel) const {
   return -1;
 }
 
-int32_t WebrtcAudioModule::SetPlayoutBuffer(const BufferType type,
-                                            uint16_t size_ms) {
-  NOTREACHED();
-  return -1;
-}
-
-int32_t WebrtcAudioModule::PlayoutBuffer(BufferType* type,
-                                         uint16_t* size_ms) const {
-  NOTREACHED();
-  return -1;
-}
-
 int32_t WebrtcAudioModule::PlayoutDelay(uint16_t* delay_ms) const {
   *delay_ms = 0;
   return 0;
 }
 
 int32_t WebrtcAudioModule::RecordingDelay(uint16_t* delay_ms) const {
-  NOTREACHED();
-  return -1;
-}
-
-int32_t WebrtcAudioModule::CPULoad(uint16_t* load) const {
-  NOTREACHED();
-  return -1;
-}
-
-int32_t WebrtcAudioModule::StartRawOutputFileRecording(
-    const char pcm_file_name_utf8[webrtc::kAdmMaxFileNameSize]) {
-  NOTREACHED();
-  return -1;
-}
-
-int32_t WebrtcAudioModule::StopRawOutputFileRecording() {
-  NOTREACHED();
-  return -1;
-}
-
-int32_t WebrtcAudioModule::StartRawInputFileRecording(
-    const char pcm_file_name_utf8[webrtc::kAdmMaxFileNameSize]) {
-  NOTREACHED();
-  return -1;
-}
-
-int32_t WebrtcAudioModule::StopRawInputFileRecording() {
   NOTREACHED();
   return -1;
 }
@@ -444,11 +368,6 @@ int32_t WebrtcAudioModule::SetPlayoutSampleRate(
 }
 
 int32_t WebrtcAudioModule::PlayoutSampleRate(uint32_t* samples_per_sec) const {
-  NOTREACHED();
-  return -1;
-}
-
-int32_t WebrtcAudioModule::ResetAudioDevice() {
   NOTREACHED();
   return -1;
 }
