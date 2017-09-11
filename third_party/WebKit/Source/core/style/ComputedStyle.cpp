59268ca39f1e6133876eb2fa1671b8241907db84
Minh-Duc Nguyen <nmduc@google.com>
2017-09-11 00:17:58 +0000

    Remove storage_only template
    Currently storage_only is used to specify properties that need
    custom getter, setter. We introduce manual_function for other
    field_template so that they can specify which function they
    want to hand code. We also remove storage_only because it is now
    redundant.
    Diff: https://gist.github.com/nguyen-minh-duc/0ecd29d3d0be44e2ec523983c4d809f2/revisions
    
    Bug: 
    Change-Id: I5f2b8d8416cd4acf03f1c6e7b890d4c9acdd517f
    Reviewed-on: https://chromium-review.googlesource.com/644668
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Reviewed-by: nainar <nainar@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Minh-Duc Nguyen <nmduc@google.com>
    Cr-Commit-Position: refs/heads/master@{#500818}

--- third_party/WebKit/Source/core/style/ComputedStyle.cpp
+++ third_party/WebKit/Source/core/style/ComputedStyle.cpp
@@ -1570,10 +1570,6 @@ Length ComputedStyle::LineHeight() const {
   return lh;
 }
 
-void ComputedStyle::SetLineHeight(const Length& specified_line_height) {
-  SetLineHeightInternal(specified_line_height);
-}
-
 int ComputedStyle::ComputedLineHeight() const {
   const Length& lh = LineHeight();
 
