0ee3c03dd3ba2854b251cb297e9fef236f56abf5
Ondrej Skopek <oskopek@google.com>
2017-09-11 14:23:19 +0000

    [Local NTP Voice] Test and test utility refactoring.
    Moves functions to prototypes of helper classes in test_utils.
    Prefers assertFalse/assertTrue to assert(!...)/assert(...).
    
    Bug: 583291
    Change-Id: I5648eacef049284ed52ad45655ef21f39c81b56c
    Reviewed-on: https://chromium-review.googlesource.com/654663
    Commit-Queue: Ondrej Škopek <oskopek@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500909}

--- chrome/test/data/local_ntp/voice_microphone_browsertest.js
+++ chrome/test/data/local_ntp/voice_microphone_browsertest.js
@@ -40,7 +40,7 @@ test.microphone.setUp = function() {
  * Makes sure the microphone module sets up with the correct settings.
  */
 test.microphone.testInitialization = function() {
-  assert(!microphone.isLevelAnimating_);
+  assertFalse(microphone.isLevelAnimating_);
 };
 
 
@@ -49,7 +49,7 @@ test.microphone.testInitialization = function() {
  */
 test.microphone.testStartLevelAnimationFromInactive = function() {
   microphone.startInputAnimation();
-  assert(microphone.isLevelAnimating_);
+  assertTrue(microphone.isLevelAnimating_);
 };
 
 
@@ -59,7 +59,7 @@ test.microphone.testStartLevelAnimationFromInactive = function() {
 test.microphone.testStopLevelAnimationFromActive = function() {
   microphone.startInputAnimation();
   microphone.stopInputAnimation();
-  assert(!microphone.isLevelAnimating_);
+  assertFalse(microphone.isLevelAnimating_);
 };
 
 
@@ -70,13 +70,13 @@ test.microphone.testStartLevelAnimationFromActive = function() {
   // Start the animation.
   test.microphone.clock.setTime(1);
   microphone.startInputAnimation();
-  assert(microphone.isLevelAnimating_);
+  assertTrue(microphone.isLevelAnimating_);
   assertEquals(1, test.microphone.clock.pendingTimeouts.length);
   const stepOneTimeoutId = test.microphone.clock.pendingTimeouts[0].id;
 
   // Try to start the animation again and observe that it hasn't restarted.
   microphone.startInputAnimation();
-  assert(microphone.isLevelAnimating_);
+  assertTrue(microphone.isLevelAnimating_);
   assertEquals(1, test.microphone.clock.pendingTimeouts.length);
   assertEquals(stepOneTimeoutId, test.microphone.clock.pendingTimeouts[0].id);
 };
