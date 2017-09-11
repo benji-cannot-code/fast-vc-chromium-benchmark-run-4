515e47cbf6ffc9d778f4a6adef6db9fc7fb58cc0
Francois Doray <fdoray@chromium.org>
2017-09-11 20:46:42 +0000

    Remove WebThread::FILE.
    This thread is deprecated and no longer used.
    
    Bug: 689520
    Change-Id: I5258d8e0892bb55326ebf6c3f41966f0757cd527
    Reviewed-on: https://chromium-review.googlesource.com/657885
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Francois Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501029}

--- ios/web/app/web_main_loop.h
+++ ios/web/app/web_main_loop.h
@@ -87,7 +87,6 @@ class WebMainLoop {
 
   // Members initialized in |RunMainMessageLoopParts()| ------------------------
   std::unique_ptr<WebThreadImpl> db_thread_;
-  std::unique_ptr<WebThreadImpl> file_thread_;
   std::unique_ptr<WebThreadImpl> io_thread_;
 
   // Members initialized in |WebThreadsStarted()| --------------------------
