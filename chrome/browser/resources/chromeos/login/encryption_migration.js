a440fdb34164138dabb495c71af2941a09707614
Naoki Fukino <fukino@chromium.org>
2017-09-11 06:30:08 +0000

    Ext4 migration UI: Disable UPDATE button when SKIP button is clicked.
    The screen can stay 1+ second when SKIP button is clicked.
    If a User click UPDATE button just after SKIP button, the migration will
    result in a failure.
    UPDATE button should be disabled once SKIP button is clicked.
    
    Bug: 763211
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ieabafeac355fc733675d20197b9c2cbf07a5503b
    Reviewed-on: https://chromium-review.googlesource.com/657539
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Naoki Fukino <fukino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500849}

--- chrome/browser/resources/chromeos/login/encryption_migration.js
+++ chrome/browser/resources/chromeos/login/encryption_migration.js
@@ -61,6 +61,11 @@ Polymer({
     isCharging: {type: Boolean, value: false},
 
     /**
+     * True if the migration was skipped.
+     */
+    isSkipped: {type: Boolean, value: false},
+
+    /**
      * Formatted string of the current available space size.
      */
     availableSpaceInString: {type: String, value: ''},
@@ -135,6 +140,16 @@ Polymer({
   },
 
   /**
+   * Returns true if the 'Update' button should be disabled.
+   * @param {boolean} isEnoughBattery
+   * @param {boolean} isSkipped
+   * @private
+   */
+  isUpdateDisabled_: function(isEnoughBattery, isSkipped) {
+    return !isEnoughBattery || isSkipped;
+  },
+
+  /**
    * Computes the label shown under progress bar.
    * @param {number} progress
    * @return {string}
@@ -196,6 +211,7 @@ Polymer({
    * @private
    */
   onSkip_: function() {
+    this.isSkipped = true;
     this.fire('skip');
   },
 
