61827d3650347e76230d4f0021a54844726cd041
Ondrej Skopek <oskopek@google.com>
2017-09-11 15:41:24 +0000

    [Local NTP Voice] a11y quick-fixes.
    Add NumpadEnter as an alternative to Enter for query sumbission.
    Fixes tab order to: Omnibox, Voice, Tiles, OGB.
    
    Bug: 583291
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Iae96c0b45126eb592a5ae20f5894142b83d04ca0
    Reviewed-on: https://chromium-review.googlesource.com/657762
    Commit-Queue: Ondrej Škopek <oskopek@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500922}

--- chrome/browser/resources/local_ntp/voice.js
+++ chrome/browser/resources/local_ntp/voice.js
@@ -44,6 +44,7 @@ function getChromeUILanguage() {
 const KEYCODE = {
   ENTER: 'Enter',
   ESC: 'Escape',
+  NUMPAD_ENTER: 'NumpadEnter',
   PERIOD: 'Period'
 };
 
@@ -280,6 +281,12 @@ speech.init = function(
     // If propagated, closes the overlay (click on the background).
     event.stopPropagation();
     speech.toggleStartStop();
+  };
+  fakeboxMicrophoneElem.onkeydown = function(event) {
+    if (!event.repeat &&
+        (event.code == KEYCODE.ENTER || event.code == KEYCODE.NUMPAD_ENTER)) {
+      speech.toggleStartStop();
+    }
   };
   window.addEventListener('keydown', speech.onKeyDown);
   if (searchboxApiHandle.onfocuschange) {
@@ -587,7 +594,9 @@ speech.onKeyDown = function(event) {
     event.stopPropagation();
     if (event.code == KEYCODE.ESC) {
       speech.stop_();
-    } else if (event.code == KEYCODE.ENTER && speech.finalResult_) {
+    } else if (
+        (event.code == KEYCODE.ENTER || event.code == KEYCODE.NUMPAD_ENTER) &&
+        speech.finalResult_) {
       speech.submitFinalResult_();
     }
   }
