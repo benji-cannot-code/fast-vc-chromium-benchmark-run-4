c34d104bc0181ed898bf8a3b69923f79aaf220a1
Karan Bhatia <karandeepb@chromium.org>
2017-09-11 21:50:45 +0000

    Extensions: Delete generated _metadata folder before unpacked extension load.
    This CL changes how _metadata folder is handled during load for unpacked
    extensions. This is done to prevent spurious warnings on extension reload due to
    the presence of a generated _metadata folder and to ensure that we are somehow
    not tricked into using a flatbuffer file provided by an extension for the
    Declarative Net Request API. Currently, presence of the _metadata folder causes
    a warning while loading extensions (r492079). This change will remove _metadata
    folder on unpacked extension load when it only contains known Extension system
    files that are safe to remove.
    
    Loading unpacked extensions with non-reserved filenames in the _metadata folder
    should now yield an error. Loading unpacked extensions with only reserved
    filenames (eg computed_hashes.json) should work, with the _metadata folder
    getting deleted on extension load. Loading a packed extension with a _metadata
    folder should still yield a warning.
    
    BUG=402278, 696822, 760850
    
    Change-Id: Ie7daf6450757702353d1d4fc8b96181eba2f669d
    Reviewed-on: https://chromium-review.googlesource.com/656567
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501052}

--- extensions/common/file_util_unittest.cc
+++ extensions/common/file_util_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/optional.h"
 #include "base/path_service.h"
+#include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
@@ -68,15 +69,15 @@ scoped_refptr<Extension> LoadExtensionManifest(
                                manifest_dir, location, extra_flags, error);
 }
 
-void RunDirectoryTest(std::vector<std::string> extra_directories,
-                      base::Optional<std::string> expected_warning) {
+void RunUnderscoreDirectoriesTest(
+    const std::vector<std::string>& underscore_directories) {
   base::ScopedTempDir temp;
   ASSERT_TRUE(temp.CreateUniqueTempDir());
 
   base::FilePath ext_path = temp.GetPath();
   ASSERT_TRUE(base::CreateDirectory(ext_path));
 
-  for (const auto& dir : extra_directories)
+  for (const auto& dir : underscore_directories)
     ASSERT_TRUE(base::CreateDirectory(ext_path.AppendASCII(dir)));
 
   ASSERT_EQ(static_cast<int>(strlen(manifest_content)),
@@ -87,15 +88,25 @@ void RunDirectoryTest(std::vector<std::string> extra_directories,
   scoped_refptr<Extension> extension = file_util::LoadExtension(
       ext_path, Manifest::UNPACKED, Extension::NO_FLAGS, &error);
   ASSERT_TRUE(extension) << error;
+  EXPECT_TRUE(error.empty());
 
   const std::vector<InstallWarning>& warnings = extension->install_warnings();
-  if (expected_warning) {
-    EXPECT_EQ(1u, warnings.size());
-    EXPECT_EQ(*expected_warning, warnings[0].message);
-  } else {
-    EXPECT_TRUE(warnings.empty());
+  ASSERT_EQ(1u, warnings.size());
+
+  // The warning should report any one of the illegal underscore directories.
+  bool warning_matched = false;
+  for (const auto& dir : underscore_directories) {
+    std::string expected_warning = base::StringPrintf(
+        "Cannot load extension with file or directory name %s. Filenames "
+        "starting with \"_\" are reserved for use by the system.",
+        dir.c_str());
+    if (expected_warning == warnings[0].message)
+      warning_matched = true;
   }
-  EXPECT_TRUE(error.empty());
+
+  EXPECT_TRUE(warning_matched)
+      << "Correct warning not generated for an unpacked extension with "
+      << base::JoinString(underscore_directories, ",") << " directories.";
 }
 
 }  // namespace
@@ -162,28 +173,15 @@ TEST_F(FileUtilTest, InstallUninstallGarbageCollect) {
 }
 
 TEST_F(FileUtilTest, LoadExtensionWithMetadataFolder) {
-  RunDirectoryTest(
-      {"_metadata"},
-      std::string("_metadata is a reserved directory that will "
-                  "not be allowed at the time of Chrome Web Store upload."));
+  RunUnderscoreDirectoriesTest({"_metadata"});
 }
 
 TEST_F(FileUtilTest, LoadExtensionWithUnderscoreFolder) {
-  RunDirectoryTest(
-      {"_badfolder"},
-      std::string(
-          "Cannot load extension with file or directory"
-          " name _badfolder. Filenames starting with \"_\" are reserved for "
-          "use by the system."));
+  RunUnderscoreDirectoriesTest({"_badfolder"});
 }
 
-TEST_F(FileUtilTest, LoadExtensionWithMetadataAndUnderscoreFolders) {
-  RunDirectoryTest(
-      {"_metadata", "_badfolder"},
-      std::string(
-          "Cannot load extension with file or directory"
-          " name _badfolder. Filenames starting with \"_\" are reserved for "
-          "use by the system."));
+TEST_F(FileUtilTest, LoadExtensionWithUnderscoreAndMetadataFolder) {
+  RunUnderscoreDirectoriesTest({"_metadata", "_badfolder"});
 }
 
 TEST_F(FileUtilTest, LoadExtensionWithValidLocales) {
