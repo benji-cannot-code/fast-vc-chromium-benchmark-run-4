7034795f0c1ebf820065bb246d85303b5625896e
Yafei Duan <romax@chromium.org>
2017-09-11 23:04:39 +0000

    [Offline Pages] Implementing add page task.
    Implementing AddPageTask which will insert a page into metedata store, then 
    invoke the callback with an AddPageResult. This is part of the OfflinePageModel
    taskification.
    Also OfflineStoreUtils is introduced, containing the utility methods used in
    tasks.
    The unit test will be in another patch, in order to adopt the testing framework
    which will be landed with GetPageTask.
    
    Bug: 753595
    Change-Id: I4ef848c24d40217e0e071a72aae49c3fdd9a3f70
    Reviewed-on: https://chromium-review.googlesource.com/636725
    Commit-Queue: Yafei Duan <romax@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501078}

---
+++ components/offline_pages/core/model/offline_store_utils.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_OFFLINE_PAGES_CORE_MODEL_OFFLINE_STORE_UTILS_H_
+#define COMPONENTS_OFFLINE_PAGES_CORE_MODEL_OFFLINE_STORE_UTILS_H_
+
+#include <string>
+
+#include "components/offline_pages/core/offline_page_types.h"
+#include "components/offline_pages/core/offline_store_types.h"
+
+namespace base {
+class FilePath;
+}  // namespace base
+
+namespace offline_pages {
+
+class OfflineStoreUtils {
+ public:
+  // Converts a file path to a UTF8 string.
+  static std::string GetUTF8StringFromPath(const base::FilePath& path);
+
+  // Converts an ItemActionStatus to AddPageResult.
+  static AddPageResult ItemActionStatusToAddPageResult(ItemActionStatus status);
+};
+
+}  // namespace offline_pages
+
+#endif  // COMPONENTS_OFFLINE_PAGES_CORE_MODEL_OFFLINE_STORE_UTILS_H_
