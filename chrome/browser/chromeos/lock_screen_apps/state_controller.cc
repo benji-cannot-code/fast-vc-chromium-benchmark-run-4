63a1f18e556427906ec56d83f850f25fdb38e1ec
Toni Barzic <tbarzic@google.com>
2017-09-11 19:56:26 +0000

    Skip lock screen apps initialization for non gaia users
    Lock screen apps are available only for gaia users, so there is no
    need for lock screen apps initialization for other users (and no need
    to create lock screen apps profile).
    
    BUG=763572
    
    Change-Id: I01ad311e9c5bac4be87921d4871758fdd4718abb
    Reviewed-on: https://chromium-review.googlesource.com/660880
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501003}

--- chrome/browser/chromeos/lock_screen_apps/state_controller.cc
+++ chrome/browser/chromeos/lock_screen_apps/state_controller.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/safe_browsing/common/safe_browsing_prefs.h"
 #include "components/session_manager/core/session_manager.h"
+#include "components/user_manager/user.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/service_manager_connection.h"
 #include "crypto/symmetric_key.h"
@@ -139,6 +140,16 @@ void StateController::Initialize() {
 }
 
 void StateController::SetPrimaryProfile(Profile* profile) {
+  const user_manager::User* user =
+      chromeos::ProfileHelper::Get()->GetUserByProfile(profile);
+  if (!user || !user->HasGaiaAccount()) {
+    if (!ready_callback_.is_null()) {
+      ready_callback_.Run();
+      ready_callback_.Reset();
+    }
+    return;
+  }
+
   g_browser_process->profile_manager()->CreateProfileAsync(
       chromeos::ProfileHelper::GetLockScreenAppProfilePath(),
       base::Bind(&StateController::OnProfilesReady,
