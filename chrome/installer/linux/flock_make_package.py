ae817927859cde94120ac8bfabe690ac76b477c2
Thomas Anderson <thomasanderson@chromium.org>
2017-09-11 22:25:18 +0000

    Revert "Enable parallel Linux package builds"
    This reverts commit 0e2fc3a0b945320fb73188181858661f545af9f3.
    
    Reason for revert: https://build.chromium.org/p/chromium.chrome/builders/Google%20Chrome%20Linux%20x64/builds/21223 "Google Chrome Linux x64"
    
    Original change's description:
    > Enable parallel Linux package builds
    > 
    > r499086 switched the Linux packaging staging dirs from /tmp to the out
    > directory.  This means it's no longer necessary to serialize package
    > builds.
    > 
    > BUG=758654
    > R=​thestig@chromium.org
    > TBR=rdevlin.cronin@chromium.org
    > 
    > Change-Id: I2ef5d9903aa2a590aa9571dfb7ab23af22c1e1bf
    > Reviewed-on: https://chromium-review.googlesource.com/645831
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    > Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#501051}
    
    TBR=thestig@chromium.org,rdevlin.cronin@chromium.org,thomasanderson@chromium.org
    
    Change-Id: I3616d9c1ee2fd449ea843556b40078cf37c65b19
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 758654
    Reviewed-on: https://chromium-review.googlesource.com/661658
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501061}

---
+++ chrome/installer/linux/flock_make_package.py
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Linux package generation is serialized because it creates large numbers of
+# temporary files that can overload the /tmp partition on the builders.
+# See https://codereview.chromium.org/243019
+#
+# This script does a flock to serialize, and then runs the given shell
+# script with the given parameters.
+#
+# Usage:
+#   flock_make_package.py <lockfile> <shell_script> [<args_to_script>*]
+
+import subprocess
+import sys
+
+if len(sys.argv) < 3:
+  print "Incorrect args."
+  sys.exit(1)
+sys.exit(subprocess.call(["flock", "--", sys.argv[1], "bash"] + sys.argv[2:]))
