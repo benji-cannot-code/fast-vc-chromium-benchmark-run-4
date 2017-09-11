e6551f8991b21dce0b02cc255de6541ffd41b442
Eric Seckler <eseckler@chromium.org>
2017-09-11 14:31:52 +0000

    headless: Make HeadlessDevToolsClientImpl public for use in unit tests.
    Bug: none
    Change-Id: If27142ceaba9ebdf0de3aa401319e4cfd359ddf1
    Reviewed-on: https://chromium-review.googlesource.com/657837
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500910}

--- headless/lib/browser/headless_devtools_client_impl.cc
+++ headless/lib/browser/headless_devtools_client_impl.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "headless/lib/browser/headless_devtools_client_impl.h"
+#include "headless/public/internal/headless_devtools_client_impl.h"
 
 #include <memory>
 
--- headless/lib/browser/headless_web_contents_impl.cc
+++ headless/lib/browser/headless_web_contents_impl.cc
@@ -32,8 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "headless/lib/browser/headless_browser_context_impl.h"
 #include "headless/lib/browser/headless_browser_impl.h"
 #include "headless/lib/browser/headless_browser_main_parts.h"
-#include "headless/lib/browser/headless_devtools_client_impl.h"
 #include "headless/lib/browser/headless_tab_socket_impl.h"
+#include "headless/public/internal/headless_devtools_client_impl.h"
 #include "printing/features/features.h"
 
 #if BUILDFLAG(ENABLE_BASIC_PRINTING)
