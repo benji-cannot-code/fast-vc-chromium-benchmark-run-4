91c2ad2cd28673788a52f0f3657a949500df4048
Francois Doray <fdoray@chromium.org>
2017-09-11 20:11:27 +0000

    Remove the blocking pool from WebThread.
    This API is deprecated and no longer used.
    
    Bug: 667892
    Change-Id: Ia885f0082e78c12cb7f2a2668c3e46b0e7f4ba6a
    Reviewed-on: https://chromium-review.googlesource.com/657805
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Francois Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501013}

--- ios/web/web_thread_impl.h
+++ ios/web/web_thread_impl.h
@@ -26,8 +26,6 @@ class WebThreadImpl : public WebThread, public base::Thread {
   WebThreadImpl(WebThread::ID identifier, base::MessageLoop* message_loop);
   ~WebThreadImpl() override;
 
-  static void ShutdownThreadPool();
-
  protected:
   void Init() override;
   void Run(base::RunLoop* run_loop) override;
@@ -65,7 +63,6 @@ class WebThreadImpl : public WebThread, public base::Thread {
   friend class TestWebThreadBundle;
   friend class TestWebThreadBundleImpl;
   friend class WebTestSuiteListener;
-  static void FlushThreadPoolHelperForTesting();
 
   // The identifier of this thread.  Only one thread can exist with a given
   // identifier at a given time.
