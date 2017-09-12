87afdd457742be6d22e8905ebe5b7b8db5fe558e
Tomasz Garbus <tgarbus@chromium.org>
2017-09-12 11:53:45 +0000

    [JS] Implemented navigator.credentials.create()
    This CL contains navigator.credentials.create(options), as described
    here: https://w3c.github.io/webappsec-credential-management/#dom-credentialscontainer-create
    
    7 out of 15 tests are passing on iOS here:
    https://w3c-test.org/credential-management/credentialscontainer-create-basics.https.html
    
     * 3 tests are failing because they test PublicKeyCredential which is not meant
    to be implemented in this file.
    
     * 1 test is failing because of bug crbug.com/762011. Instead of TypeError, Promise is rejected
    with NotSupportedError
    
     * 4 tests are failing because on iOS DOMException.name is not a string, so
    testharness.js fails this line: https://cs.chromium.org/chromium/src/third_party/WebKit/LayoutTests/resources/testharness.js?type=cs&q=testharness+promise_test&l=1279
    Promise is still rejected with DOMException of correct type (DOMException.NOT_SUPPORTED_ERR)
    
    Bug: 435046
    Change-Id: Ica5a55425f1071398fe2e2b5d936234bcffa5750
    Reviewed-on: https://chromium-review.googlesource.com/650355
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Tomasz Garbus <tgarbus@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501239}

--- ios/chrome/browser/web/resources/credential_manager.js
+++ ios/chrome/browser/web/resources/credential_manager.js
@@ -11,10 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * the window.navigator.object.
  */
 
-// TODO(crbug.com/435046) This only contains method signatures.
-// Implement the JavaScript API and types according to the spec:
-// https://w3c.github.io/webappsec-credential-management
-
 // TODO(crbug.com/435046) After get, store, preventSilentAccess are
 // implemented app-side, make sure that all tests at
 // https://w3c-test.org/credential-management/idl.https.html
@@ -411,7 +407,10 @@ var FederatedCredentialRequestOptions;
  * The CredentialCreationOptions dictionary, for more information see
  * https://w3c.github.io/webappsec-credential-management/#credentialcreationoptions-dictionary
  * @dict
- * @typedef {Object}
+ * @typedef {{
+ *     password: ?PasswordCredentialInit,
+ *     federated: ?FederatedCredentialInit
+ * }}
  */
 var CredentialCreationOptions;
 
@@ -475,7 +474,30 @@ CredentialsContainer.prototype.preventSilentAccess = function() {
  *     of the request.
  */
 CredentialsContainer.prototype.create = function(options) {
-  // TODO(crbug.com/435046) Implement |create| as JS function
+  // According to
+  // https://w3c.github.io/webappsec-credential-management/#abstract-opdef-create-a-credential,
+  // we should also check for secure context. Instead it is done only in
+  // browser-side methods. Since public JS interface is exposed, user can create
+  // a Credential using a constructor anyway.
+  return new Promise(function(resolve, reject) {
+    try {
+      if (options && options.password && !options.federated) {
+        resolve(new PasswordCredential(options.password));
+        return;
+      }
+      if (options && options.federated && !options.password) {
+        resolve(new FederatedCredential(options.federated));
+        return;
+      }
+    } catch (err) {
+      reject(err);
+      return;
+    }
+    reject(Object.create(DOMException.prototype, {
+      name: {value: DOMException.NOT_SUPPORTED_ERR},
+      message: {value: 'Invalid CredentialRequestOptions'}
+    }));
+  });
 };
 
 /**
