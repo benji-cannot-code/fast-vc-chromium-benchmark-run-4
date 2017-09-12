2cdeb5a68232778e6c38f2c09c59f6d019ed624f
Will Chen <chenwilliam@chromium.org>
2017-09-12 23:36:38 +0000

    DevTools: move tests (part 11)
    Bug: 667560
    Change-Id: I417e24d6f2657107aaec74022353f4f329232804
    Reviewed-on: https://chromium-review.googlesource.com/660781
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501453}

---
+++ third_party/WebKit/LayoutTests/http/tests/devtools/service-workers/resources/bypass-for-network-redirect.php
@@ -0,0 +1,6 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+  $url = "http://127.0.0.1:8000/devtools/service-workers/resources/" .
+         "bypass-for-network-redirected.html";
+  header("Location: $url");
+?>
--- third_party/WebKit/LayoutTests/http/tests/inspector/service-workers/resources/bypass-for-network-redirect.php
+++
@@ -1,6 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<?php
-  $url = "http://127.0.0.1:8000/inspector/service-workers/resources/" .
-         "bypass-for-network-redirected.html";
-  header("Location: $url");
-?>
