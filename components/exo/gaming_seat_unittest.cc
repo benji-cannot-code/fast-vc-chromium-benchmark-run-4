7a8318cf89cdbcdd664dffa0bb9a971c2a615798
Jingkui Wang <jkwang@google.com>
2017-09-12 21:27:48 +0000

    exo: Remove old gamepad implementation
    This patch removes the gaming seat implementation for evdev. All the
    gamepad support will use ozone implementation.
    
    This patch also removes the dummy interface for gaming_input_v1 in Wayland
    server. The dummy interface was there to make sure the system is robust
    when we moved the protocol to v2.
    
    Bug: 717246
    Change-Id: I70b29d3c6a81761d1e15c502f78fd0615bf768fe
    Reviewed-on: https://chromium-review.googlesource.com/661100
    Commit-Queue: Jingkui Wang <jkwang@google.com>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501402}

--- components/exo/gaming_seat_unittest.cc
+++ components/exo/gaming_seat_unittest.cc
@@ -47,7 +47,7 @@ class GamingSeatTest : public test::ExoTestBase {
  public:
   GamingSeatTest() {}
   void InitializeGamingSeat(MockGamingSeatDelegate* delegate) {
-    gaming_seat_.reset(new GamingSeat(delegate, nullptr));
+    gaming_seat_.reset(new GamingSeat(delegate));
   }
 
   void DestroyGamingSeat(MockGamingSeatDelegate* delegate) {
