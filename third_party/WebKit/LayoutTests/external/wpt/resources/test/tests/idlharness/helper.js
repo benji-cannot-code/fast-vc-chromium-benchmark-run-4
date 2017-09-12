402af5a7fdba29b0841cf2385119dbca594d805c
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2017-09-12 11:07:39 +0000

    Import wpt@8f30dbe39b329027d64625e7f94e0314e8512a60
    Using wpt-import in Chromium 7e104a64545c3506040150d0903a79efb3353e41.
    
    Build: https://build.chromium.org/p/chromium.infra.cron/builders/wpt-importer/builds/1657
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    jsbell@chromium.org:
      external/wpt/resources
    rego@igalia.com:
      external/wpt/css/css-ui-3
    rouslan@chromium.org, mathp@chromium.org:
      external/wpt/payment-request
    
    TBR=qyearsley@chromium.org
    
    No-Export: true
    Change-Id: I3bc94d7ae081d2adc9f5ad0ec0304de8033c0982
    Reviewed-on: https://chromium-review.googlesource.com/662518
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501236}

--- third_party/WebKit/LayoutTests/external/wpt/resources/test/tests/idlharness/helper.js
+++ third_party/WebKit/LayoutTests/external/wpt/resources/test/tests/idlharness/helper.js
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 "use strict";
 
+var typedefFrom = interfaceFrom;
 var dictionaryFrom = interfaceFrom;
 function interfaceFrom(i) {
     var idl = new IdlArray();
