5a323739252f5344fcdf47ee3083260f4b4d085a
Gauthier Ambard <gambard@chromium.org>
2017-09-12 15:19:24 +0000

    Unfocus omnibox when showing context menu on NTP
    When the user long press an item on the NTP, a context menu is shown.
    The omnibox should be unfocused when this context menu is presented.
    It is only necessary to specify it on iPad as showing the alert menu
    on iPhone automatically unfocus it.
    
    Bug: 763907
    Change-Id: I9d1b8bcf94b64867464ba9ff1ab0a49c423f3a47
    Reviewed-on: https://chromium-review.googlesource.com/663342
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501281}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.mm
@@ -638,6 +638,8 @@ BOOL ShouldCellsBeFullWidth(UITraitCollection* collection) {
       break;
   }
 
+  if (IsIPadIdiom())
+    [self.headerSynchronizer unfocusOmnibox];
 }
 
 // Checks if the |section| is empty and add an empty element if it is the case.
