f98bff8556a8c3c66ac1f400400ea2cfdcf5c65e
Alice Boxhall <aboxhall@chromium.org>
2017-09-12 01:16:25 +0000

    [Devtools] Maintain selection when selecting accessibility tree nodes which aren't represented in Elements tree
    Bug: 560525
    Change-Id: I77f911996bbb4cad6ed7967b7e63d2286e39592f
    Reviewed-on: https://chromium-review.googlesource.com/644646
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501135}

--- third_party/WebKit/Source/devtools/front_end/accessibility/AccessibilitySidebarView.js
+++ third_party/WebKit/Source/devtools/front_end/accessibility/AccessibilitySidebarView.js
@@ -10,6 +10,7 @@ Accessibility.AccessibilitySidebarView = class extends UI.ThrottledWidget {
     super();
     this._node = null;
     this._axNode = null;
+    this._skipNextPullNode = false;
     this._sidebarPaneStack = UI.viewManager.createStackLocation();
     this._breadcrumbsSubPane = new Accessibility.AXBreadcrumbsPane(this);
     this._sidebarPaneStack.showView(this._breadcrumbsSubPane);
@@ -38,8 +39,10 @@ Accessibility.AccessibilitySidebarView = class extends UI.ThrottledWidget {
 
   /**
    * @param {?SDK.DOMNode} node
+   * @param {boolean=} fromAXTree
    */
-  setNode(node) {
+  setNode(node, fromAXTree) {
+    this._skipNextPullNode = !!fromAXTree;
     this._node = node;
     this.update();
   }
@@ -115,6 +118,10 @@ Accessibility.AccessibilitySidebarView = class extends UI.ThrottledWidget {
   }
 
   _pullNode() {
+    if (this._skipNextPullNode) {
+      this._skipNextPullNode = false;
+      return;
+    }
     this.setNode(UI.context.flavor(SDK.DOMNode));
   }
 
