8d09963bc767f90c299ce7734c5f467c7344deaa
Sadrul Habib Chowdhury <sadrul@chromium.org>
2017-09-12 17:09:29 +0000

    chromeos: Remove some left over x11 code.
    chromeos always builds with ozone, and no longer has direct dependency on
    x11. So USE_X11 is no longer applicable for chromeos code.
    
    BUG=750642
    
    Change-Id: Iade4e609dbbb5d20d7a683153d23f84fc7848b46
    Reviewed-on: https://chromium-review.googlesource.com/663403
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501320}

--- ui/chromeos/events/event_rewriter_chromeos.cc
+++ ui/chromeos/events/event_rewriter_chromeos.cc
@@ -30,18 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/keycodes/dom/keycode_converter.h"
 #include "ui/events/keycodes/keyboard_code_conversion.h"
 
-#if defined(USE_X11)
-#include <X11/Xlib.h>
-#include <X11/extensions/XInput2.h>
-
-// Get rid of macros from Xlib.h that conflicts with other parts of the code.
-#undef RootWindow
-#undef Status
-
-#include "ui/base/x/x11_util.h"
-#include "ui/events/keycodes/keyboard_code_conversion_x.h"
-#endif
-
 namespace ui {
 
 namespace {
@@ -541,15 +529,6 @@ ui::EventRewriteStatus EventRewriterChromeOS::RewriteKeyEvent(
   }
   if ((key_event.flags() == state.flags) &&
       (key_event.key_code() == state.key_code) &&
-#if defined(USE_X11)
-      // TODO(kpschoedel): This test is present because several consumers of
-      // key events depend on having a native core X11 event, so we rewrite
-      // all XI2 key events (GenericEvent) into corresponding core X11 key
-      // events. Remove this when event consumers no longer care about
-      // native X11 event details (crbug.com/380349).
-      (!key_event.HasNativeEvent() ||
-       (key_event.native_event()->type != GenericEvent)) &&
-#endif
       (status == ui::EVENT_REWRITE_CONTINUE)) {
     return ui::EVENT_REWRITE_CONTINUE;
   }
@@ -592,15 +571,8 @@ ui::EventRewriteStatus EventRewriterChromeOS::RewriteMouseButtonEvent(
   ui::MouseEvent* rewritten_mouse_event = new ui::MouseEvent(mouse_event);
   rewritten_event->reset(rewritten_mouse_event);
   rewritten_mouse_event->set_flags(flags);
-#if defined(USE_X11)
-  ui::UpdateX11EventForFlags(rewritten_mouse_event);
-#endif
-  if (changed_button != ui::EF_NONE) {
+  if (changed_button != ui::EF_NONE)
     rewritten_mouse_event->set_changed_button_flags(changed_button);
-#if defined(USE_X11)
-    ui::UpdateX11EventForChangedButtonFlags(rewritten_mouse_event);
-#endif
-  }
   return status;
 }
 
@@ -620,18 +592,12 @@ ui::EventRewriteStatus EventRewriterChromeOS::RewriteMouseWheelEvent(
     case ui::EVENT_REWRITE_REWRITTEN:
     case ui::EVENT_REWRITE_DISPATCH_ANOTHER:
       // whell event has been rewritten and stored in |rewritten_event|.
-#if defined(USE_X11)
-      ui::UpdateX11EventForFlags(rewritten_event->get());
-#endif
       break;
     case ui::EVENT_REWRITE_CONTINUE:
       if (flags != wheel_event.flags()) {
         *rewritten_event = base::MakeUnique<ui::MouseWheelEvent>(wheel_event);
         (*rewritten_event)->set_flags(flags);
         status = ui::EVENT_REWRITE_REWRITTEN;
-#if defined(USE_X11)
-        ui::UpdateX11EventForFlags(rewritten_event->get());
-#endif
       }
       break;
     case ui::EVENT_REWRITE_DISCARD:
@@ -652,9 +618,6 @@ ui::EventRewriteStatus EventRewriterChromeOS::RewriteTouchEvent(
   ui::TouchEvent* rewritten_touch_event = new ui::TouchEvent(touch_event);
   rewritten_event->reset(rewritten_touch_event);
   rewritten_touch_event->set_flags(flags);
-#if defined(USE_X11)
-  ui::UpdateX11EventForFlags(rewritten_touch_event);
-#endif
   return ui::EVENT_REWRITE_REWRITTEN;
 }
 
@@ -667,10 +630,6 @@ ui::EventRewriteStatus EventRewriterChromeOS::RewriteScrollEvent(
       sticky_keys_controller_->RewriteEvent(scroll_event, rewritten_event);
   // Scroll event shouldn't be discarded.
   DCHECK_NE(status, ui::EVENT_REWRITE_DISCARD);
-#if defined(USE_X11)
-  if (status != ui::EVENT_REWRITE_CONTINUE)
-    ui::UpdateX11EventForFlags(rewritten_event->get());
-#endif
   return status;
 }
 
@@ -729,7 +688,6 @@ bool EventRewriterChromeOS::RewriteModifierKeys(const ui::KeyEvent& key_event,
       if (remapped_key && remapped_key->result.key_code == ui::VKEY_CAPITAL)
         remapped_key = kModifierRemappingNeoMod3;
       break;
-#if !defined(USE_X11)
     case ui::DomKey::ALT_GRAPH_LATCH:
       if (key_event.type() == ui::ET_KEY_PRESSED) {
         pressed_modifier_latches_ |= ui::EF_ALTGR_DOWN;
@@ -749,7 +707,6 @@ bool EventRewriterChromeOS::RewriteModifierKeys(const ui::KeyEvent& key_event,
       state->key_code = ui::VKEY_ALTGR;
       exact_event = true;
       break;
-#endif
     default:
       break;
   }
