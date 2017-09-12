7cddab187d19538549074d70fb6dce5f0b48bcf0
Wenzhao Zang <wzang@chromium.org>
2017-09-12 20:19:45 +0000

    Add tray action interface to close lock screen notes
    The interface is used to close the note taking app from the views-based
    login shelf.
    
    Also added TestTrayActionClient which are shared by various unit tests.
    
    Bug: 701157
    Change-Id: I88c871f1597369f11b5b2c9d1c3548c790c0b088
    Reviewed-on: https://chromium-review.googlesource.com/656059
    Reviewed-by: Oliver Chang <ochang@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501378}

---
+++ ash/tray_action/test_tray_action_client.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/tray_action/test_tray_action_client.h"
+
+#include "mojo/public/cpp/bindings/interface_request.h"
+
+namespace ash {
+
+TestTrayActionClient::TestTrayActionClient() : binding_(this) {}
+
+TestTrayActionClient::~TestTrayActionClient() = default;
+
+void TestTrayActionClient::ClearCounts() {
+  action_requests_count_ = 0;
+  action_close_count_ = 0;
+}
+
+void TestTrayActionClient::RequestNewLockScreenNote() {
+  action_requests_count_++;
+}
+
+void TestTrayActionClient::CloseLockScreenNote() {
+  action_close_count_++;
+}
+
+mojom::TrayActionClientPtr TestTrayActionClient::CreateInterfacePtrAndBind() {
+  mojom::TrayActionClientPtr ptr;
+  binding_.Bind(mojo::MakeRequest(&ptr));
+  return ptr;
+}
+
+}  // namespace ash
---
+++ ash/tray_action/test_tray_action_client.h
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_TRAY_ACTION_TEST_TRAY_ACTION_CLIENT_H_
+#define ASH_TRAY_ACTION_TEST_TRAY_ACTION_CLIENT_H_
+
+#include "ash/public/interfaces/tray_action.mojom.h"
+#include "base/macros.h"
+#include "mojo/public/cpp/bindings/binding.h"
+
+namespace ash {
+
+class TestTrayActionClient : public mojom::TrayActionClient {
+ public:
+  TestTrayActionClient();
+
+  ~TestTrayActionClient() override;
+
+  mojom::TrayActionClientPtr CreateInterfacePtrAndBind();
+
+  void ClearCounts();
+  int action_requests_count() const { return action_requests_count_; }
+  int action_close_count() const { return action_close_count_; }
+
+  // mojom::TrayActionClient:
+  void RequestNewLockScreenNote() override;
+  void CloseLockScreenNote() override;
+
+ private:
+  mojo::Binding<mojom::TrayActionClient> binding_;
+
+  int action_requests_count_ = 0;
+  int action_close_count_ = 0;
+
+  DISALLOW_COPY_AND_ASSIGN(TestTrayActionClient);
+};
+
+}  // namespace ash
+
+#endif  // ASH_TRAY_ACTION_TEST_TRAY_ACTION_CLIENT_H_
