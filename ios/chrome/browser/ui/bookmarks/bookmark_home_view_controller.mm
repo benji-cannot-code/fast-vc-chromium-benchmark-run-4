21ff19e79614989bee22254f6dd864e23868064c
Ramya Sharma <ramyasharma@chromium.org>
2017-09-12 07:26:29 +0000

    Implements move for multiple selection on context menu.
    - Move functionality for multiple Folder selection
    - Move functionality for multiple URL selection
    - Move functionality for multiple Folder / URL selection
    - Eg tests for the same.
    
    Bug: 695749
    Change-Id: I5c64c6ddc10fb5e47e1fcd521afea9498f653ae4
    Reviewed-on: https://chromium-review.googlesource.com/662538
    Commit-Queue: Ramya Sharma <ramyasharma@chromium.org>
    Reviewed-by: Marti Wong <martiw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501207}

--- ios/chrome/browser/ui/bookmarks/bookmark_home_view_controller.mm
+++ ios/chrome/browser/ui/bookmarks/bookmark_home_view_controller.mm
@@ -1146,9 +1146,10 @@ const CGFloat kSpacer = 50;
       break;
     case BookmarksContextBarMultipleURLSelection:
       // More clicked, show action sheet with context menu.
-      [self presentViewController:[self contextMenuForMultipleBookmarkURLs]
-                         animated:YES
-                       completion:nil];
+      [self
+          presentViewController:[self contextMenuForMultipleBookmarkURLs:nodes]
+                       animated:YES
+                     completion:nil];
       break;
     case BookmarksContextBarSingleFolderSelection:
       // Edit clicked, open the editor.
@@ -1260,7 +1261,9 @@ const CGFloat kSpacer = 50;
 
 #pragma mark - Context Menu
 
-- (UIAlertController*)contextMenuForMultipleBookmarkURLs {
+- (UIAlertController*)contextMenuForMultipleBookmarkURLs:
+    (const std::set<const bookmarks::BookmarkNode*>)nodes {
+  __weak BookmarkHomeViewController* weakSelf = self;
   UIAlertController* alert = [UIAlertController
       alertControllerWithTitle:nil
                        message:nil
@@ -1286,7 +1289,9 @@ const CGFloat kSpacer = 50;
   UIAlertAction* moveAction = [UIAlertAction
       actionWithTitle:l10n_util::GetNSString(IDS_IOS_BOOKMARK_CONTEXT_MENU_MOVE)
                 style:UIAlertActionStyleDefault
-              handler:nil];
+              handler:^(UIAlertAction* _Nonnull action) {
+                [weakSelf moveNodes:nodes];
+              }];
   [alert addAction:openAllAction];
   [alert addAction:openInIncognitoAction];
   [alert addAction:moveAction];
@@ -1374,7 +1379,8 @@ const CGFloat kSpacer = 50;
 }
 
 - (UIAlertController*)contextMenuForMixedAndMultiFolderSelection:
-    (const std::set<const bookmarks::BookmarkNode*>&)nodes {
+    (const std::set<const bookmarks::BookmarkNode*>)nodes {
+  __weak BookmarkHomeViewController* weakSelf = self;
   UIAlertController* alert = [UIAlertController
       alertControllerWithTitle:nil
                        message:nil
@@ -1389,7 +1395,9 @@ const CGFloat kSpacer = 50;
   UIAlertAction* moveAction = [UIAlertAction
       actionWithTitle:l10n_util::GetNSString(IDS_IOS_BOOKMARK_CONTEXT_MENU_MOVE)
                 style:UIAlertActionStyleDefault
-              handler:nil];
+              handler:^(UIAlertAction* _Nonnull action) {
+                [weakSelf moveNodes:nodes];
+              }];
   [alert addAction:moveAction];
   [alert addAction:cancelAction];
   return alert;
