585406fa6459f07ceb8f75f6059d8cdd0f986f04
Emi Morikawa <emim@google.com>
2017-09-12 02:22:02 +0000

    service worker: Set performance timing info in ServiceWorkerURLLoaderJob.
    Set ResourceResponseHead::service_worker_start_time and service_worker_ready_time in ServiceWorkerURLLoaderJob.
    
    Bug: 763232
    Change-Id: I52b8f3495bbae076ef10608b11065e77fa7c3c43
    Reviewed-on: https://chromium-review.googlesource.com/659480
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Emi Morikawa <emim@google.com>
    Cr-Commit-Position: refs/heads/master@{#501156}

--- content/browser/service_worker/service_worker_url_loader_job.cc
+++ content/browser/service_worker/service_worker_url_loader_job.cc
@@ -112,6 +112,7 @@ void ServiceWorkerURLLoaderJob::StartRequest() {
       fetch_dispatcher_->MaybeStartNavigationPreloadWithURLLoader(
           resource_request_, url_loader_factory_getter_.get(),
           base::BindOnce(&base::DoNothing /* TODO(crbug/762357): metrics? */));
+  response_head_.service_worker_start_time = base::TimeTicks::Now();
   fetch_dispatcher_->Run();
 }
 
@@ -144,7 +145,9 @@ void ServiceWorkerURLLoaderJob::DeliverErrorResponse() {
 }
 
 void ServiceWorkerURLLoaderJob::DidPrepareFetchEvent(
-    scoped_refptr<ServiceWorkerVersion> version) {}
+    scoped_refptr<ServiceWorkerVersion> version) {
+  response_head_.service_worker_ready_time = base::TimeTicks::Now();
+}
 
 void ServiceWorkerURLLoaderJob::DidDispatchFetchEvent(
     ServiceWorkerStatusCode status,
