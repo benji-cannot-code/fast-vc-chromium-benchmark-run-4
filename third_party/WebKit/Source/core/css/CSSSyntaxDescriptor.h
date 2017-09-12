89498b8eba8f3a7f59dca5c82e6212125f78accf
Jeremy Roman <jbroman@chromium.org>
2017-09-12 01:02:48 +0000

    Make CSSSyntaxDescriptor constructor explicit and not bump string ref count.
    It's not clear that much is gained by passing by value here (the inner functions
    use const references), and in any event, single-argument constructors should
    be explicit unless it's intended that they be used for implicit conversion.
    
    Change-Id: I0e4d0824dadfd8ef4de1ea513bf48059612ac53a
    Reviewed-on: https://chromium-review.googlesource.com/655858
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501132}

--- third_party/WebKit/Source/core/css/CSSSyntaxDescriptor.h
+++ third_party/WebKit/Source/core/css/CSSSyntaxDescriptor.h
@@ -47,7 +47,7 @@ struct CSSSyntaxComponent {
 
 class CORE_EXPORT CSSSyntaxDescriptor {
  public:
-  CSSSyntaxDescriptor(String syntax);
+  explicit CSSSyntaxDescriptor(const String& syntax);
 
   const CSSValue* Parse(CSSParserTokenRange,
                         const CSSParserContext*,
