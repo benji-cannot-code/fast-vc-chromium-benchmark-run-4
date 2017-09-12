2108742748edf54a24fb59b901868bce914d5d04
Francois Doray <fdoray@chromium.org>
2017-09-12 14:15:51 +0000

    Remove the blocking pool from the service process.
    The blocking pool is deprecated.
    
    The only remaining user was the ServiceProcessPrefs' JsonPrefStore,
    which now uses TaskScheduler instead.
    
    Bug: 667892
    Change-Id: Ic4739018ff111b7886bbc186e50f40408a8d9367
    Reviewed-on: https://chromium-review.googlesource.com/660677
    Reviewed-by: Gene Gutnik <gene@chromium.org>
    Commit-Queue: Francois Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501264}

--- chrome/service/service_process.cc
+++ chrome/service/service_process.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/synchronization/waitable_event.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/task_scheduler/scheduler_worker_pool_params.h"
 #include "base/task_scheduler/task_scheduler.h"
 #include "base/threading/sequenced_worker_pool.h"
@@ -186,13 +187,6 @@ bool ServiceProcess::Initialize(base::MessageLoopForUI* message_loop,
     return false;
   }
 
-  // Since SequencedWorkerPool is redirected to TaskScheduler, the value of
-  // |kMaxBlockingPoolThreads| is ignored.
-  constexpr int kMaxBlockingPoolThreads = 3;
-  blocking_pool_ =
-      new base::SequencedWorkerPool(kMaxBlockingPoolThreads, "ServiceBlocking",
-                                    base::TaskPriority::USER_VISIBLE);
-
   // Initialize Mojo early so things can use it.
   mojo::edk::Init();
   mojo_ipc_support_.reset(new mojo::edk::ScopedIPCSupport(
@@ -205,10 +199,11 @@ bool ServiceProcess::Initialize(base::MessageLoopForUI* message_loop,
   PathService::Get(chrome::DIR_USER_DATA, &user_data_dir);
   base::FilePath pref_path =
       user_data_dir.Append(chrome::kServiceStateFileName);
-  service_prefs_.reset(new ServiceProcessPrefs(
+  service_prefs_ = std::make_unique<ServiceProcessPrefs>(
       pref_path,
-      JsonPrefStore::GetTaskRunnerForFile(pref_path, blocking_pool_.get())
-          .get()));
+      base::CreateSequencedTaskRunnerWithTraits(
+          {base::MayBlock(), base::TaskShutdownBehavior::BLOCK_SHUTDOWN})
+          .get());
   service_prefs_->ReadPrefs();
 
   // This switch it required to run connector with test gaia.
@@ -280,16 +275,6 @@ bool ServiceProcess::Teardown() {
   shutdown_event_.Signal();
   io_thread_.reset();
   file_thread_.reset();
-
-  if (blocking_pool_.get()) {
-    // The goal is to make it impossible for chrome to 'infinite loop' during
-    // shutdown, but to reasonably expect that all BLOCKING_SHUTDOWN tasks
-    // queued during shutdown get run. There's nothing particularly scientific
-    // about the number chosen.
-    const int kMaxNewShutdownBlockingTasks = 1000;
-    blocking_pool_->Shutdown(kMaxNewShutdownBlockingTasks);
-    blocking_pool_ = NULL;
-  }
 
   if (base::TaskScheduler::GetInstance())
     base::TaskScheduler::GetInstance()->Shutdown();
