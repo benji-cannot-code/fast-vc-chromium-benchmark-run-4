9f7746d04e0b17e025317e98653dc695138f4a34
Peter Beverloo <peter@chromium.org>
2017-09-12 14:19:32 +0000

    Initialize PathUtils for the decoder service
    This should fix the crashes that we've been seeing on the bots, so those
    have been re-enabled as well.
    
    BUG=761060
    
    Change-Id: I75dfcc04e972e5dd9a0478cc297701603fa71551
    Reviewed-on: https://chromium-review.googlesource.com/663219
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501265}

--- chrome/android/javatests/src/org/chromium/chrome/browser/photo_picker/PhotoPickerDialogTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/photo_picker/PhotoPickerDialogTest.java
@@ -7,13 +7,13 @@ package org.chromium.chrome.browser.photo_picker;
 
 import static org.chromium.base.test.util.ScalableTimeout.scaleTimeout;
 
+import android.support.test.filters.LargeTest;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CallbackHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.widget.selection.SelectionDelegate;
@@ -195,14 +195,7 @@ public class PhotoPickerDialogTest extends ChromeActivityTestCaseBase<ChromeActi
         });
     }
 
-    /**
-     * Continues to be flaky on bots which doesn't reproduce on local devices,
-     * continuing to investigate offline.
-     *
-     * https://crbug.com/761060
-     * @LargeTest
-     */
-    @DisabledTest
+    @LargeTest
     public void testNoSelection() throws Throwable {
         createDialog(false, Arrays.asList("image/*")); // Multi-select = false.
         assertTrue(mDialog.isShowing());
@@ -218,14 +211,7 @@ public class PhotoPickerDialogTest extends ChromeActivityTestCaseBase<ChromeActi
         dismissDialog();
     }
 
-    /**
-     * Continues to be flaky on bots which doesn't reproduce on local devices,
-     * continuing to investigate offline.
-     *
-     * https://crbug.com/761060
-     * @LargeTest
-     */
-    @DisabledTest
+    @LargeTest
     public void testSingleSelectionPhoto() throws Throwable {
         createDialog(false, Arrays.asList("image/*")); // Multi-select = false.
         assertTrue(mDialog.isShowing());
@@ -244,14 +230,7 @@ public class PhotoPickerDialogTest extends ChromeActivityTestCaseBase<ChromeActi
         dismissDialog();
     }
 
-    /**
-     * Continues to be flaky on bots which doesn't reproduce on local devices,
-     * continuing to investigate offline.
-     *
-     * https://crbug.com/761060
-     * @LargeTest
-     */
-    @DisabledTest
+    @LargeTest
     public void testMultiSelectionPhoto() throws Throwable {
         createDialog(true, Arrays.asList("image/*")); // Multi-select = true.
         assertTrue(mDialog.isShowing());
