e28d5f0f8de23906040d6b57dc0257517d4023b7
Tom Anderson <thomasanderson@chromium.org>
2017-09-12 00:18:34 +0000

    Reland of: Enable parallel Linux package builds
    r499086 switched the Linux packaging staging dirs from /tmp to the out
    directory.  This means it's no longer necessary to serialize package
    builds.
    
    The original CL passed argv[2:] to bash instead of argv[1:].
    
    BUG=758654
    R=thestig@chromium.org
    TBR=rdevlin.cronin@chromium.org
    
    Change-Id: If6dc9c35edeb60337b944a2a4350875274241747
    Reviewed-on: https://chromium-review.googlesource.com/661780
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501111}

---
+++ chrome/installer/linux/make_package.py
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Usage:
+#   make_package.py <shell_script> [<args_to_script>*]
+
+import subprocess
+import sys
+
+if len(sys.argv) < 2:
+  print "Incorrect args."
+  sys.exit(1)
+sys.exit(subprocess.call(["bash"] + sys.argv[1:]))
