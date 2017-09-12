779333458ea1350b4d2f70647371fb9f0069d9ff
Pavel Feldman <pfeldman@chromium.org>
2017-09-12 20:37:04 +0000

    DevTools: sort service workers in the applications panel.
    Bug: 753159
    Change-Id: I2415b1def48ab5ba7fbbd29e586304ce5655ad1e
    Reviewed-on: https://chromium-review.googlesource.com/646732
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501383}

--- third_party/WebKit/Source/devtools/front_end/ui/ReportView.js
+++ third_party/WebKit/Source/devtools/front_end/ui/ReportView.js
@@ -25,7 +25,7 @@ UI.ReportView = class extends UI.VBox {
    * @param {string} title
    */
   setTitle(title) {
-    if (this._titleElement && this._titleElement.textContent === title)
+    if (this._titleElement.textContent === title)
       return;
     this._titleElement.textContent = title;
   }
@@ -72,8 +72,19 @@ UI.ReportView = class extends UI.VBox {
     return section;
   }
 
-  removeAllSection() {
-    this._sectionList.removeChildren();
+  /**
+   * @param {function(!UI.ReportView.Section, !UI.ReportView.Section): number} comparator
+   */
+  sortSections(comparator) {
+    var sections = /** @type {!Array<!UI.ReportView.Section>} */ (this.children().slice());
+    var sorted = sections.every((e, i, a) => !i || comparator(a[i - 1], a[i]) <= 0);
+    if (sorted)
+      return;
+
+    this.detachChildWidgets();
+    sections.sort(comparator);
+    for (var section of sections)
+      section.show(this._sectionList);
   }
 };
 
@@ -96,6 +107,13 @@ UI.ReportView.Section = class extends UI.VBox {
     this._fieldList = this.element.createChild('div', 'vbox');
     /** @type {!Map.<string, !Element>} */
     this._fieldMap = new Map();
+  }
+
+  /**
+   * @return {string}
+   */
+  title() {
+    return this._titleElement.textContent;
   }
 
   /**
@@ -131,10 +149,6 @@ UI.ReportView.Section = class extends UI.VBox {
     if (textValue)
       row.lastElementChild.textContent = textValue;
     return /** @type {!Element} */ (row.lastElementChild);
-  }
-
-  remove() {
-    this.element.remove();
   }
 
   /**
