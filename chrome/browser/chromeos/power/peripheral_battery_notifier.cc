7074d30e54a91ce89bfd336188921b2a1049c391
Tetsui Ohkubo <tetsui@chromium.org>
2017-09-12 08:24:03 +0000

    Apply system style to peripheral battery low notification.
    In new-style notification, system notification has different style from
    other notifications such as web notifications.
    * The context header shows the title "Chrome OS system".
    * The context header has accent color.
    * New MD icon is used.
    
    TEST=manual
    BUG=761885
    
    Change-Id: I8de0863bd95080f66123ae34704ccb6fa09e19b6
    Reviewed-on: https://chromium-review.googlesource.com/649449
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501214}

--- chrome/browser/chromeos/power/peripheral_battery_notifier.cc
+++ chrome/browser/chromeos/power/peripheral_battery_notifier.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/system/system_notifier.h"
@@ -129,11 +130,13 @@ struct NotificationParams {
   int image_id;
   std::string notifier_name;
   GURL url;
+  const gfx::VectorIcon* icon;
 };
 
 NotificationParams GetNonStylusNotificationParams(const std::string& address,
                                                   const std::string& name,
-                                                  int battery_level) {
+                                                  int battery_level,
+                                                  bool is_bluetooth) {
   return NotificationParams{
       address,
       base::ASCIIToUTF16(name),
@@ -141,7 +144,9 @@ NotificationParams GetNonStylusNotificationParams(const std::string& address,
           IDS_ASH_LOW_PERIPHERAL_BATTERY_NOTIFICATION_TEXT, battery_level),
       IDR_NOTIFICATION_PERIPHERAL_BATTERY_LOW,
       kNotifierId,
-      GURL(kNotificationOriginUrl)};
+      GURL(kNotificationOriginUrl),
+      is_bluetooth ? &ash::kNotificationBluetoothBatteryWarningIcon
+                   : &ash::kNotificationBatteryCriticalIcon};
 }
 
 NotificationParams GetStylusNotificationParams() {
@@ -151,7 +156,8 @@ NotificationParams GetStylusNotificationParams() {
       l10n_util::GetStringUTF16(IDS_ASH_LOW_STYLUS_BATTERY_NOTIFICATION_BODY),
       IDR_NOTIFICATION_STYLUS_BATTERY_LOW,
       ash::system_notifier::kNotifierStylusBattery,
-      GURL()};
+      GURL(),
+      &ash::kNotificationBatteryCriticalIcon};
 }
 
 }  // namespace
@@ -275,16 +281,18 @@ bool PeripheralBatteryNotifier::PostNotification(const std::string& path,
   NotificationParams params =
       battery.is_stylus
           ? GetStylusNotificationParams()
-          : GetNonStylusNotificationParams(path, battery.name, battery.level);
+          : GetNonStylusNotificationParams(path, battery.name, battery.level,
+                                           !battery.bluetooth_address.empty());
 
-  auto notification = base::MakeUnique<message_center::Notification>(
+  auto notification = ash::system_notifier::CreateSystemNotification(
       message_center::NOTIFICATION_TYPE_SIMPLE, params.id, params.title,
       params.message,
       ui::ResourceBundle::GetSharedInstance().GetImageNamed(params.image_id),
       base::string16(), params.url,
       message_center::NotifierId(message_center::NotifierId::SYSTEM_COMPONENT,
                                  params.notifier_name),
-      message_center::RichNotificationData(), nullptr);
+      message_center::RichNotificationData(), nullptr, *params.icon,
+      message_center::SystemNotificationWarningLevel::CRITICAL_WARNING);
   notification->SetSystemPriority();
 
   message_center::MessageCenter::Get()->AddNotification(
