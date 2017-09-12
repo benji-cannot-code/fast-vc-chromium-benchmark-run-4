afde11754478bd8d5e1e0985311cc4ef5e638aa0
Matt Mueller <mattm@chromium.org>
2017-09-12 23:44:43 +0000

    Try #2: Convert Linux and ChromeOS to use X509CertificateBytes.
    Now including a hack to handle certificate dates outside the range that
    base::Time::FromUTCExploded can handle on 32-bit linux. (Previously those
    dates would be imploded with NSPR, which always uses 64bit time values.)
    
    Bug: 671420
    Change-Id: I3c633b6f2f434bbb356b83bbc99cf8c8b8a08ff8
    Reviewed-on: https://chromium-review.googlesource.com/658326
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501456}

--- net/cert/x509_certificate_bytes.cc
+++ net/cert/x509_certificate_bytes.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/numerics/safe_conversions.h"
 #include "base/pickle.h"
 #include "base/stl_util.h"
+#include "build/build_config.h"
 #include "crypto/openssl_util.h"
 #include "net/base/ip_address.h"
 #include "net/cert/asn1_util.h"
@@ -40,7 +41,31 @@ bool GeneralizedTimeToBaseTime(const der::GeneralizedTime& generalized,
   exploded.hour = generalized.hours;
   exploded.minute = generalized.minutes;
   exploded.second = generalized.seconds;
+#if defined(OS_POSIX) && !defined(OS_MACOSX) && !defined(OS_ANDROID)
+  if (base::Time::FromUTCExploded(exploded, result))
+    return true;
+
+  if (sizeof(time_t) == 4) {
+    // Fail on obviously bad dates before trying the 32-bit hacks.
+    if (!exploded.HasValidValues())
+      return false;
+
+    // Hack to handle dates that can't be converted on 32-bit systems.
+    // TODO(mattm): consider consolidating this with
+    // SaturatedTimeFromUTCExploded from cookie_util.cc
+    if (generalized.year >= 2038) {
+      *result = base::Time::Max();
+      return true;
+    }
+    if (generalized.year < 1970) {
+      *result = base::Time::Min();
+      return true;
+    }
+  }
+  return false;
+#else
   return base::Time::FromUTCExploded(exploded, result);
+#endif
 }
 
 // Sets |value| to the Value from a DER Sequence Tag-Length-Value and return
