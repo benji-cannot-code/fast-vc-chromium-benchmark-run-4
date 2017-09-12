0d2d5e85f3b14f7390574b1be94ca8fd40a036b2
Peter Kotwicz <pkotwicz@chromium.org>
2017-09-12 04:03:31 +0000

    [Android FRE] Remove LightweightFirstRunActivity dependence on FirstRunActivity
    This CL introduces FirstRunActivityBase which contains logic to be shared
    between FirstRunActivity and LightweightFirstRunActivity.
    This CL changes LightweightFirstRunActivity to extend
    FirstRunActivityBase instead of FirstRunActivity.
    
    BUG=755646
    
    Change-Id: Id839e78361030aef6d7a5d2e94544760f3c117c8
    Reviewed-on: https://chromium-review.googlesource.com/619252
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501173}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunFlowSequencer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunFlowSequencer.java
@@ -298,7 +298,6 @@ public abstract class FirstRunFlowSequencer  {
         Intent intent = new Intent();
         intent.setClassName(context, LightweightFirstRunActivity.class.getName());
         intent.putExtra(FirstRunActivity.EXTRA_COMING_FROM_CHROME_ICON, fromChromeIcon);
-        intent.putExtra(FirstRunActivity.EXTRA_START_LIGHTWEIGHT_FRE, true);
         return intent;
     }
 
@@ -370,8 +369,7 @@ public abstract class FirstRunFlowSequencer  {
             List<WeakReference<Activity>> activities = ApplicationStatus.getRunningActivities();
             for (WeakReference<Activity> weakActivity : activities) {
                 Activity activity = weakActivity.get();
-                if (activity instanceof FirstRunActivity
-                        && !(activity instanceof LightweightFirstRunActivity)) {
+                if (activity instanceof FirstRunActivity) {
                     isGenericFreActive = true;
                     break;
                 }
