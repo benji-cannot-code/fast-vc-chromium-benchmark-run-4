efee1abd7bb225549feb02438d4d283d5445f107
James Forshaw <forshaw@chromium.org>
2017-09-12 17:03:52 +0000

    Revert "Implemented changes to Sid class for AC support."
    This reverts commit 251a3311605b4f065b84d26a150ff1a3feb4abd0.
    
    Reason for revert: People building with actual VS2015 according to the official instructions don't have the definitions for some of the AC functions. I'd argue they shouldn't be building with that but it seems that it's part of the official instructions. Reverting for now.
    
    Original change's description:
    > Implemented changes to Sid class for AC support.
    > 
    > This CL implements changes to the sandbox Sid class to support the creation
    > of Capability Sid objects. It also contains a few refactoring efforts to
    > cleanup up older code.
    > 
    > Bug: 760977
    > Cq-Include-Trybots: master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    > Change-Id: Id9c05b8017abb81896c1bc30f23599441812d1fd
    > Reviewed-on: https://chromium-review.googlesource.com/649026
    > Commit-Queue: James Forshaw <forshaw@chromium.org>
    > Reviewed-by: Will Harris <wfh@chromium.org>
    > Reviewed-by: Penny MacNeil <pennymac@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#501229}
    
    TBR=pennymac@chromium.org,forshaw@chromium.org,wfh@chromium.org
    
    Change-Id: Icafd0c2281d8442562fc24c869ca88595277f065
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 760977
    Cq-Include-Trybots: master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    Reviewed-on: https://chromium-review.googlesource.com/663698
    Reviewed-by: James Forshaw <forshaw@chromium.org>
    Commit-Queue: James Forshaw <forshaw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501316}

--- sandbox/win/src/sid.h
+++ sandbox/win/src/sid.h
@@ -10,41 +10,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace sandbox {
 
-enum WellKnownCapabilities {
-  kInternetClient,
-  kInternetClientServer,
-  kRegistryRead,
-  kLpacCryptoServices,
-  kEnterpriseAuthentication,
-  kPrivateNetworkClientServer,
-  kMaxWellKnownCapability
-};
-
 // This class is used to hold and generate SIDS.
 class Sid {
  public:
   // Constructors initializing the object with the SID passed.
   // This is a converting constructor. It is not explicit.
-  Sid(PSID sid);
-  Sid(const SID* sid);
+  Sid(const SID *sid);
   Sid(WELL_KNOWN_SID_TYPE type);
 
-  // Create a Sid from an AppContainer capability name. The name can be
-  // completely arbitrary.
-  static Sid FromNamedCapability(const wchar_t* capability_name);
-  // Create a Sid from a known capability enumeration value.
-  static Sid FromKnownCapability(WellKnownCapabilities capability);
-  // Create a Sid from a SDDL format string, such as S-1-1-0.
-  static Sid FromSddlString(const wchar_t* sddl_sid);
-
   // Returns sid_.
-  PSID GetPSID() const;
-
-  // Gets whether the sid is valid.
-  bool IsValid() const;
+  const SID *GetPSID() const;
 
  private:
-  Sid();
   BYTE sid_[SECURITY_MAX_SID_SIZE];
 };
 
