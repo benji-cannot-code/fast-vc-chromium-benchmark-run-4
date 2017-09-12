7a414a5961605adce62883f57cb52c5881f510f3
Mohamad Ahmadi <mahmadi@chromium.org>
2017-09-12 19:38:46 +0000

    [Payment Request] Enables Payment Request on iOS by default
    - Sets PaymentRequestManager's active WebState to nullptr in BVC's shutdown
      method to make sure the active WebState is clean when BVC shuts down.
    - Adds a call to update PaymentRequestManager's active WebState in
      -tabModel:didReplaceTab:withTab:atIndex: in order to clean the the
      previously active WebState and enable PaymentRequest on the new one. This
      call happens when a recent tab is opened.
    - This CL adds a unit test to verify that the BVC shuts down cleanly with
      Payment Request enabled.
    - This CL removes the calls to enable/disable the active WebState in the
      [PaymentRequestManager enablePaymentRequest:] and instead changes the
      behavior so that the script commands are ignored when the Payment Request
      is disabled. The reason for this change is that this method is called
      when the BVC is disabled before it shuts down. This could result in a
      second attempt to disable the active WebState when the BVC actually shuts
      down which is wrong.
    
    TBR=rouslan@
    
    Bug: 761358, 755799, 602666
    Change-Id: Id984aec77194993b23085448cb802ba9fe120379
    Reviewed-on: https://chromium-review.googlesource.com/660480
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501363}

--- ios/chrome/browser/ui/payments/payment_request_manager.mm
+++ ios/chrome/browser/ui/payments/payment_request_manager.mm
@@ -329,12 +329,8 @@ struct PendingPaymentResponse {
     return;
 
   _enabled = enabled;
-  if (!enabled) {
+  if (!enabled)
     [self cancelRequest];
-    [self disableActiveWebState];
-  } else if (!_activeWebStateEnabled) {
-    [self enableActiveWebState];
-  }
 }
 
 - (void)cancelRequest {
@@ -427,6 +423,10 @@ struct PendingPaymentResponse {
 }
 
 - (BOOL)handleScriptCommand:(const base::DictionaryValue&)JSONCommand {
+  // Early return if the Payment Request is not enabled.
+  if (!_enabled)
+    return NO;
+
   std::string command;
   if (!JSONCommand.GetString("command", &command)) {
     LOG(ERROR) << "Received bad JSON: No 'command' field";
