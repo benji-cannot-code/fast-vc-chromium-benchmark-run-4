055c05af4b8e30ea2185a2e701a775a7257361df
Kunihiko Sakamoto <ksakamoto@chromium.org>
2017-09-13 06:47:37 +0000

    Revert "splitview: Cleanup window selector unittests."
    This reverts commit cc477ff2aa3bb185b2b959a562a96a128dcaf403.
    
    Reason for revert: ASan detected heap-use-after-free in SplitViewWindowSelectorTest.WindowGridSizeWhileDraggingWithSplitView.
    
    https://uberchromegw.corp.google.com/i/chromium.memory/builders/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29/builds/23602
    
    Original change's description:
    > splitview: Cleanup window selector unittests.
    > 
    > Move splitview window selector unittests to their own test class.
    > 
    > Bug: None
    > Test: ash_unittests
    > Change-Id: I8153c916a05ef9ab6011105ca670dfc0e73ee257
    > Reviewed-on: https://chromium-review.googlesource.com/661327
    > Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    > Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    > Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#501487}
    
    TBR=oshima@chromium.org,xdai@chromium.org,sammiequon@chromium.org
    
    Change-Id: Ie609324d8a10bc3f35951d583df5c7782a03aae9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: None
    Reviewed-on: https://chromium-review.googlesource.com/663107
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Commit-Queue: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501558}

--- ash/wm/splitview/split_view_controller.h
+++ ash/wm/splitview/split_view_controller.h
@@ -23,7 +23,7 @@ namespace ash {
 
 class SplitViewControllerTest;
 class SplitViewDivider;
-class SplitViewWindowSelectorTest;
+class WindowSelectorTest;
 
 // The controller for the split view. It snaps a window to left/right side of
 // the screen. It also observes the two snapped windows and decides when to exit
@@ -108,7 +108,7 @@ class ASH_EXPORT SplitViewController : public aura::WindowObserver,
 
  private:
   friend class SplitViewControllerTest;
-  friend class SplitViewWindowSelectorTest;
+  friend class WindowSelectorTest;
 
   // Ends the split view mode.
   void EndSplitView();
