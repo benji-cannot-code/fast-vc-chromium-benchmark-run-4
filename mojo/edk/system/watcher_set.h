f8b1231a72defb7dc56af09411346d6d107ea927
Reilly Grant <reillyg@chromium.org>
2017-09-13 04:58:20 +0000

    Switch mojo::edk::WatcherSet to flat_map
    Like mojo::edk::WatcherDispatcher the map within a WatcherSet typically
    only contains 2 entries and thus a base::flat_map is more appropriate
    than a full std::map.
    
    Change-Id: I7e93ea95c4037468512aa5991672779f8636f8ea
    Reviewed-on: https://chromium-review.googlesource.com/664179
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501545}

--- mojo/edk/system/watcher_set.h
+++ mojo/edk/system/watcher_set.h
@@ -6,8 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MOJO_EDK_SYSTEM_WATCHER_SET_H_
 #define MOJO_EDK_SYSTEM_WATCHER_SET_H_
 
-#include <map>
-
+#include "base/containers/flat_map.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/optional.h"
@@ -60,7 +59,7 @@ class WatcherSet {
   };
 
   Dispatcher* const owner_;
-  std::map<WatcherDispatcher*, Entry> watchers_;
+  base::flat_map<WatcherDispatcher*, Entry> watchers_;
   base::Optional<HandleSignalsState> last_known_state_;
 
   DISALLOW_COPY_AND_ASSIGN(WatcherSet);
