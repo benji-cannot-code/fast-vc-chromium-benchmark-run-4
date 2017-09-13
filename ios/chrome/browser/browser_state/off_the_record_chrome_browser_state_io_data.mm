08f7b1d08cc7d7be5530a228a7eb26a7b6f5df1a
Gabriel Charette <gab@chromium.org>
2017-09-13 13:10:02 +0000

    Migrate OffTheRecordChromeBrowserStateIOData off of WebThread::DB
    ios/ equivalent to https://codereview.chromium.org/2841593003
    
    R=sdefresne@chromium.org
    
    Bug: 689520
    Change-Id: I6aa6a0042f26177f9db38053f9c05bb1402aaf75
    Reviewed-on: https://chromium-review.googlesource.com/664064
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501606}

--- ios/chrome/browser/browser_state/off_the_record_chrome_browser_state_io_data.mm
+++ ios/chrome/browser/browser_state/off_the_record_chrome_browser_state_io_data.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/mac/bind_objc_block.h"
 #include "base/stl_util.h"
+#include "base/task_scheduler/post_task.h"
 #include "components/net_log/chrome_net_log.h"
 #include "components/prefs/pref_service.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
@@ -188,7 +189,8 @@ void OffTheRecordChromeBrowserStateIOData::InitializeInternal(
   DCHECK(!channel_id_path_.empty());
   channel_id_store = new net::SQLiteChannelIDStore(
       channel_id_path_,
-      web::WebThread::GetTaskRunnerForThread(web::WebThread::DB));
+      base::CreateSequencedTaskRunnerWithTraits(
+          {base::MayBlock(), base::TaskPriority::BACKGROUND}));
 
   net::ChannelIDService* channel_id_service = new net::ChannelIDService(
       new net::DefaultChannelIDStore(channel_id_store.get()));
