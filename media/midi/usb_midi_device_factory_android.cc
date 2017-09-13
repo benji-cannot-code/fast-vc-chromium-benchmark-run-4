34c9dd4581c9d53d5af521abd146a9da57a1212e
Takashi Toyoshima <toyoshim@chromium.org>
2017-09-13 10:02:54 +0000

    media/midi: use BindOnce and OnceCallback explicitly
    Mechanical changes to use BindOnce and OnceCallback
    explicitly at everywhere in media/midi.
    
    Bug: 672793
    Change-Id: Ibef3543d3b91f6617f5116a845b03e0ccb823629
    Reviewed-on: https://chromium-review.googlesource.com/657357
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501585}

--- media/midi/usb_midi_device_factory_android.cc
+++ media/midi/usb_midi_device_factory_android.cc
@@ -42,7 +42,7 @@ void UsbMidiDeviceFactoryAndroid::EnumerateDevices(
   raw_factory_.Reset(Java_UsbMidiDeviceFactoryAndroid_create(env, pointer));
 
   delegate_ = delegate;
-  callback_ = callback;
+  callback_ = std::move(callback);
 
   if (Java_UsbMidiDeviceFactoryAndroid_enumerateDevices(env, raw_factory_)) {
     // Asynchronous operation.
@@ -50,7 +50,7 @@ void UsbMidiDeviceFactoryAndroid::EnumerateDevices(
   }
   // No devices are found.
   UsbMidiDevice::Devices devices;
-  callback.Run(true, &devices);
+  std::move(callback_).Run(true, &devices);
 }
 
 // Called from the Java world.
@@ -67,7 +67,7 @@ void UsbMidiDeviceFactoryAndroid::OnUsbMidiDeviceRequestDone(
         base::MakeUnique<UsbMidiDeviceAndroid>(raw_device, delegate_));
   }
 
-  callback_.Run(true, &devices_to_pass);
+  std::move(callback_).Run(true, &devices_to_pass);
 }
 
 // Called from the Java world.
