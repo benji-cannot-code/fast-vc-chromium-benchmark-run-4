fc81889df7e2a9cf7ab3e8e3e160f8b3234f8cd0
Mark Cogan <marq@google.com>
2017-09-13 13:36:14 +0000

    Restore MainViewControllerTest.StatusBar for iOS11
    This test, disabled because it fails in iOS 11b3 and b4, works correctly
    under beta 6. This CL can be landed when beta 6 is on the trybots.
    
    Bug: 748738
    Change-Id: I514eaa8dc104bff6f38b13bff2f541e18edbddbf
    Reviewed-on: https://chromium-review.googlesource.com/647746
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501611}

--- ios/chrome/browser/ui/main/main_view_controller_unittest.mm
+++ ios/chrome/browser/ui/main/main_view_controller_unittest.mm
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
-#include "base/ios/ios_util.h"
 #include "testing/gtest_mac.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -115,9 +114,6 @@ TEST(MainViewControllerTest, SetActiveVC) {
 }
 
 TEST(MainViewControllerTest, StatusBar) {
-  // TODO(crbug.com/748738) This is broken with iOS11/Xcode 9 beta 4.
-  if (base::ios::IsRunningOnIOS11OrLater())
-    return;
   MainViewController* main_view_controller = [[MainViewController alloc] init];
   // MVC needs to be the root view controller for this to work, so save the
   // current one and restore it at the end of the test.
