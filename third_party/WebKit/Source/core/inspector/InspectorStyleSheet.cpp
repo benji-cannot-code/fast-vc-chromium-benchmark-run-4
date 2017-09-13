f67bfec0f9673f48b66bedb97c73e685e6dff8e3
Daniel Cheng <dcheng@chromium.org>
2017-09-13 23:12:48 +0000

    Make StyleSheetHandler stack allocated.
    gcc6 complains that Trace() is unused--and so it is. Just make it stack
    allocated.
    
    Bug: 
    Change-Id: I1ca8d614edf198c59d7bcb61a63de0092e673633
    Reviewed-on: https://chromium-review.googlesource.com/665174
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501788}

--- third_party/WebKit/Source/core/inspector/InspectorStyleSheet.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorStyleSheet.cpp
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/inspector/InspectorNetworkAgent.h"
 #include "core/inspector/InspectorResourceContainer.h"
 #include "core/svg/SVGStyleElement.h"
+#include "platform/wtf/Allocator.h"
 #include "platform/wtf/PtrUtil.h"
 #include "platform/wtf/text/StringBuilder.h"
 #include "platform/wtf/text/TextPosition.h"
@@ -146,8 +147,9 @@ void GetClassNamesFromRule(CSSStyleRule* rule, HashSet<String>& unique_names) {
   }
 }
 
-class StyleSheetHandler final : public GarbageCollected<StyleSheetHandler>,
-                                public CSSParserObserver {
+class StyleSheetHandler final : public CSSParserObserver {
+  STACK_ALLOCATED();
+
  public:
   StyleSheetHandler(const String& parsed_text,
                     Document* document,
@@ -155,8 +157,6 @@ class StyleSheetHandler final : public GarbageCollected<StyleSheetHandler>,
       : parsed_text_(parsed_text), document_(document), result_(result) {
     DCHECK(result_);
   }
-
-  DECLARE_TRACE();
 
  private:
   void StartRuleHeader(StyleRule::RuleType, unsigned) override;
@@ -334,13 +334,6 @@ void StyleSheetHandler::ObserveComment(unsigned start_offset,
   current_rule_data_stack_.back()->property_data.push_back(
       CSSPropertySourceData(property_data.name, property_data.value, false,
                             true, true, SourceRange(start_offset, end_offset)));
-}
-
-DEFINE_TRACE(StyleSheetHandler) {
-  visitor->Trace(document_);
-  visitor->Trace(result_);
-  visitor->Trace(current_rule_data_stack_);
-  visitor->Trace(current_rule_data_);
 }
 
 bool VerifyRuleText(Document* document, const String& rule_text) {
