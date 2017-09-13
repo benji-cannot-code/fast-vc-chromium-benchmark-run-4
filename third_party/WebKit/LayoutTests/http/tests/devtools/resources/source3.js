6e43201306f07d599406a92757f64af65f4e9b8e
Will Chen <chenwilliam@chromium.org>
2017-09-13 23:10:19 +0000

    DevTools: move the rest of http/tests/inspector tests (part 12)
    Bug: 667560
    Change-Id: I99cf82ce362842f7d28f8b652ad803e0fd12768d
    Reviewed-on: https://chromium-review.googlesource.com/664338
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501784}

--- third_party/WebKit/LayoutTests/http/tests/devtools/resources/source3.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/resources/source3.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 function foo()
 {
-    console.error(new Error("http://127.0.0.1:8000/inspector/resources/source3.js"));
+    console.error(new Error("http://127.0.0.1:8000/devtools/resources/source3.js"));
 }
--- third_party/WebKit/LayoutTests/http/tests/devtools/service-workers/resources/user-agent-override-worker.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/service-workers/resources/user-agent-override-worker.js
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 function dumpUserAgent()
 {
-    fetch('http://127.0.0.1:8000/inspector/resources/echo-headers.php')
+    fetch('http://127.0.0.1:8000/devtools/resources/echo-headers.php')
         .then(function(response) {
             return response.text();
         }).then(function(body) {
