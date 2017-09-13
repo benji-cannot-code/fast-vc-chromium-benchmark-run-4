b7808181fbb6b5b4f7be7d50e03b6274ee0b3185
Jinho Bang <jinho.bang@samsung.com>
2017-09-13 10:45:30 +0000

    PaymentHandler: Refactor PaymentInstrumentIconFetcher class.
    The current PaymentInstrumentIconFetcher class is too complicated and
    can not ensure thread-safety due to misuse some APIs. So, this patch
    refactors the class to use ManifestIconDownloader internally. After
    this patch, we can remove much duplicated codes and resolve some
    crashes in debug mode as well.
    
    Bug: 752836
    Change-Id: I04c01d433d093ae138148e6aa2849bc1713c1c85
    Reviewed-on: https://chromium-review.googlesource.com/658201
    Commit-Queue: Jinho Bang <jinho.bang@samsung.com>
    Reviewed-by: Ganggui Tang <gogerald@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501589}

--- content/browser/payments/payment_app_database.cc
+++ content/browser/payments/payment_app_database.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/service_worker/service_worker_context_wrapper.h"
 #include "content/browser/service_worker/service_worker_registration.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/common/manifest.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/gfx/image/image.h"
 #include "url/gurl.h"
@@ -195,15 +196,26 @@ void PaymentAppDatabase::WritePaymentInstrument(
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
 
   if (instrument->icons.size() > 0) {
-    std::unique_ptr<PaymentInstrumentIconFetcher> icon_fetcher =
-        base::MakeUnique<PaymentInstrumentIconFetcher>();
-    icon_fetcher->Start(
-        instrument->icons, service_worker_context_,
+    std::vector<Manifest::Icon> icons;
+    for (const auto& image_object : instrument->icons) {
+      Manifest::Icon icon;
+      icon.src = image_object->src;
+      // TODO(zino): We should pass an actual image size comes from the
+      // renderer, but we just use gfx::Size(0, 0) for now until the feature
+      // is implemented. It means "any" size.
+      // Please see https://crbug.com/763886
+      icon.sizes.emplace_back(gfx::Size());
+      icon.purpose.emplace_back(Manifest::Icon::IconPurpose::ANY);
+      icons.emplace_back(icon);
+    }
+
+    PaymentInstrumentIconFetcher::Start(
+        scope, service_worker_context_->GetProviderHostIds(scope.GetOrigin()),
+        std::move(icons),
         base::BindOnce(&PaymentAppDatabase::DidFetchedPaymentInstrumentIcon,
                        weak_ptr_factory_.GetWeakPtr(), scope, instrument_key,
                        base::Passed(std::move(instrument)),
-                       base::Passed(std::move(callback)),
-                       base::Passed(std::move(icon_fetcher))));
+                       base::Passed(std::move(callback))));
   } else {
     service_worker_context_->FindReadyRegistrationForPattern(
         scope,
@@ -220,11 +232,9 @@ void PaymentAppDatabase::DidFetchedPaymentInstrumentIcon(
     const std::string& instrument_key,
     payments::mojom::PaymentInstrumentPtr instrument,
     WritePaymentInstrumentCallback callback,
-    std::unique_ptr<PaymentInstrumentIconFetcher> icon_fetcher,
     const std::string& icon) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
 
-  icon_fetcher.reset();
   if (icon.empty()) {
     std::move(callback).Run(PaymentHandlerStatus::FETCH_INSTRUMENT_ICON_FAILED);
     return;
