92d979a423e3902ab40577db7b5bd3dbd9b5aa01
David Tseng <dtseng@chromium.org>
2017-09-13 17:15:36 +0000

    Fix event target when performing programmatic click
    Given
    <div id="outer">
      <span id="inner"></span>
      </div>
    
    and an event listener gets added on outer, all programmatic clicks,
    from the perspective of the page, have their event targets set to
    outer even if the client triggering the click action clicks the inner
    node.
    
    This is because we check AXNodeObject::IsClickable and
    Node::IsElementNode, both restrictions seem to break various
    assumptions by page authors.
    
    When those checks fail, we simply get the ancestor that is clickable
    (or an anchor), so click targets are always the ones with click event
    listeners. Unfortunately, this is not useful for page authors that
    want to add an event listener for a collection of nodes in a subtree.
    
    Bug: 
    Change-Id: I43a28b265d5af6ea664c4d5fd5528763b78b499e
    Reviewed-on: https://chromium-review.googlesource.com/663214
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501670}

--- third_party/WebKit/Source/modules/accessibility/AXObject.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXObject.cpp
@@ -1920,9 +1920,13 @@ bool AXObject::OnNativeClickAction() {
   std::unique_ptr<UserGestureIndicator> gesture_indicator =
       LocalFrame::CreateUserGesture(document->GetFrame(),
                                     UserGestureToken::kNewGesture);
-  Element* action_elem = ActionElement();
-  if (action_elem) {
-    action_elem->AccessKeyAction(true);
+
+  Element* element = GetElement();
+  if (!element && GetNode())
+    element = GetNode()->parentElement();
+
+  if (element) {
+    element->AccessKeyAction(true);
     return true;
   }
 
