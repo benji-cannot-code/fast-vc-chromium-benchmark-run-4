ed8d3133c97060a1fecc07a6814d9db2e7a248a5
Alexey Kozyatinskiy <kozyatinskiy@chromium.org>
2017-09-13 18:03:23 +0000

    [DevTools] added test for expending document.all in iframe
    R=dgozman@chromium.org
    
    Bug: chromium:759913
    Change-Id: I10c21e3689c567fe79ccd97320ffc53c3d8ecef3
    Reviewed-on: https://chromium-review.googlesource.com/661791
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501688}

---
+++ third_party/WebKit/LayoutTests/inspector-protocol/runtime/runtime-get-properties-document-all-in-iframe.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  var {page, session, dp} = await testRunner.startBlank('Tests Runtime.getProperties doesnt crash on document.all in iframe');
+  await dp.Runtime.enable();
+  await dp.Debugger.enable();
+  session.evaluate(`
+    window.frame = document.createElement('iframe');
+    frame.src = '${testRunner.url('../resources/blank.html')}';
+    document.body.appendChild(frame);
+  `);
+  let {params:{context:{id}}} = await dp.Runtime.onceExecutionContextCreated();
+  let {result:{result:{objectId}}} = await dp.Runtime.evaluate({
+    contextId: id,
+    expression: 'document.all',
+  });
+  await dp.Runtime.getProperties({objectId});
+  testRunner.completeTest();
+})
