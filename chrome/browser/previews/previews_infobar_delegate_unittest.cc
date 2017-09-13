5b83604742366c24856f3c23649d22f49d5bf398
Thanh Le <thanhdle@chromium.org>
2017-09-13 17:38:04 +0000

    Change the enum name of DISABLE_LOFI_MODE to DISABLE_PREVIEWS since it disables all previews outside of general reload topic.
    Bug: 707272
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: Ied478af0c8322a7d48494d5614695b4ef7c368d2
    Reviewed-on: https://chromium-review.googlesource.com/663822
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Thanh Le <thanhdle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501680}

--- chrome/browser/previews/previews_infobar_delegate_unittest.cc
+++ chrome/browser/previews/previews_infobar_delegate_unittest.cc
@@ -396,7 +396,7 @@ TEST_F(PreviewsInfoBarDelegateUnitTest, InfobarTestClickLinkLitePage) {
 
   content::WebContentsTester::For(web_contents())->CommitPendingNavigation();
 
-  EXPECT_EQ(content::ReloadType::DISABLE_LOFI_MODE,
+  EXPECT_EQ(content::ReloadType::DISABLE_PREVIEWS,
             TestPreviewsWebContentsObserver::FromWebContents(web_contents())
                 ->last_navigation_reload_type());
 
@@ -539,7 +539,7 @@ TEST_F(PreviewsInfoBarDelegateUnitTest, OfflineInfobarDisablesLoFi) {
 
   content::WebContentsTester::For(web_contents())->CommitPendingNavigation();
 
-  EXPECT_EQ(content::ReloadType::DISABLE_LOFI_MODE,
+  EXPECT_EQ(content::ReloadType::DISABLE_PREVIEWS,
             TestPreviewsWebContentsObserver::FromWebContents(web_contents())
                 ->last_navigation_reload_type());
 
