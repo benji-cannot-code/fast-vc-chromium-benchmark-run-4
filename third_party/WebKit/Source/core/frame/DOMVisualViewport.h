7a073e183a7afcd2d893f831c453b417261a9491
David Bokan <bokan@chromium.org>
2017-09-14 03:11:12 +0000

    Add attribute event listeners to visualViewport
    These were previously available but removed in r407955. In the mean time
    they made their way into the visualViewport draft spec:
    
    https://wicg.github.io/ViewportAPI/spec.html#dom-visualviewport-onresize
    
    But we forgot to actually re-add them. This patch adds them back in.
    
    Bug: 760916
    Change-Id: Ib2dc3dbd21d648d9118bbb88e61bba7b65369688
    Reviewed-on: https://chromium-review.googlesource.com/658318
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501855}

--- third_party/WebKit/Source/core/frame/DOMVisualViewport.h
+++ third_party/WebKit/Source/core/frame/DOMVisualViewport.h
@@ -67,6 +67,9 @@ class DOMVisualViewport final : public EventTargetWithInlineData {
   double height() const;
   double scale() const;
 
+  DEFINE_ATTRIBUTE_EVENT_LISTENER(resize);
+  DEFINE_ATTRIBUTE_EVENT_LISTENER(scroll);
+
  private:
   explicit DOMVisualViewport(LocalDOMWindow*);
 
