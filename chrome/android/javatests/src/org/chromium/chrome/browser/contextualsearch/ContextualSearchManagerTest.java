c35a5d484f4dde3d2fc6978d79600e3d0c46b985
Donn Denman <donnd@google.com>
2017-09-14 23:28:29 +0000

    [TTS] Disable two tests that are flaky on 64 bit.
    For some reason there are two tests that are flaky on a 64 bit
    Marshmalow bot.  This Change disables those tests for 64 bit
    devices.
    
    BUG=765403
    TBR=twellington@chromium.org
    
    Change-Id: I97c4b8aa93e284e53fc786422962186323742e35
    Reviewed-on: https://chromium-review.googlesource.com/668019
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502097}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
@@ -1284,6 +1284,7 @@ public class ContextualSearchManagerTest {
     @SmallTest
     @Feature({"ContextualSearch"})
     @Restriction(UiRestriction.RESTRICTION_TYPE_PHONE)
+    @DisableIf.Build(supported_abis_includes = "arm64-v8a", message = "crbug.com/765403")
     public void testSwipeExpand() throws InterruptedException, TimeoutException {
         assertNoSearchesLoaded();
         clickWordNode("intelligence");
@@ -1860,6 +1861,7 @@ public class ContextualSearchManagerTest {
     @Test
     @SmallTest
     @Feature({"ContextualSearch"})
+    @DisableIf.Build(supported_abis_includes = "arm64-v8a", message = "crbug.com/765403")
     public void testSearchTermResolutionError() throws InterruptedException, TimeoutException {
         clickWordNode("states");
         assertSearchTermRequested();
