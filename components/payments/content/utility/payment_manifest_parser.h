c9354a91a68651f4530d44e62c1439752b725114
Rouslan Solomakhin <rouslan@chromium.org>
2017-09-14 18:39:44 +0000

    [Payments] Fuzz payment method manifest parser.
    Bug: None
    Change-Id: I26d6b05046801dacb4c59ff31af0bf5bf32cf0aa
    Reviewed-on: https://chromium-review.googlesource.com/667000
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501997}

--- components/payments/content/utility/payment_manifest_parser.h
+++ components/payments/content/utility/payment_manifest_parser.h
@@ -11,8 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "components/payments/mojom/payment_manifest_parser.mojom.h"
-#include "url/gurl.h"
-#include "url/origin.h"
+
+class GURL;
+
+namespace url {
+class Origin;
+}
 
 namespace payments {
 
