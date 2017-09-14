aaca49b4f73112a964d16cf9a75d8e27322716fa
Varun Khaneja <vakh@chromium.org>
2017-09-14 01:11:59 +0000

    Use GetThreatSource instead of hardcoding ThreatSource.
    Removes a long time TODO.
    
    Bug: 764965
    Change-Id: I0e1eeb2f4fb824effc73af75ebbd6db0b754a2a1
    Reviewed-on: https://chromium-review.googlesource.com/666120
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501822}

--- chrome/browser/ui/webui/interstitials/interstitial_ui.cc
+++ chrome/browser/ui/webui/interstitials/interstitial_ui.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/features.h"
 #include "chrome/common/url_constants.h"
 #include "components/grit/components_resources.h"
+#include "components/safe_browsing_db/database_manager.h"
 #include "components/security_interstitials/core/ssl_error_ui.h"
 #include "components/supervised_user_error_page/supervised_user_error_page.h"
 #include "content/public/browser/interstitial_page_delegate.h"
@@ -286,7 +287,9 @@ safe_browsing::SafeBrowsingBlockingPage* CreateSafeBrowsingBlockingPage(
       security_interstitials::UnsafeResource::
           GetWebContentsGetter(web_contents->GetRenderProcessHost()->GetID(),
                                web_contents->GetMainFrame()->GetRoutingID());
-  resource.threat_source = safe_browsing::ThreatSource::LOCAL_PVER3;
+  resource.threat_source = g_browser_process->safe_browsing_service()
+                               ->database_manager()
+                               ->GetThreatSource();
 
   // Normally safebrowsing interstitial types which block the main page load
   // (SB_THREAT_TYPE_URL_MALWARE, SB_THREAT_TYPE_URL_PHISHING, and
@@ -332,7 +335,9 @@ TestSafeBrowsingBlockingPageQuiet* CreateSafeBrowsingQuietBlockingPage(
       security_interstitials::UnsafeResource::GetWebContentsGetter(
           web_contents->GetRenderProcessHost()->GetID(),
           web_contents->GetMainFrame()->GetRoutingID());
-  resource.threat_source = safe_browsing::ThreatSource::LOCAL_PVER3;
+  resource.threat_source = g_browser_process->safe_browsing_service()
+                               ->database_manager()
+                               ->GetThreatSource();
 
   // Normally safebrowsing interstitial types which block the main page load
   // (SB_THREAT_TYPE_URL_MALWARE, SB_THREAT_TYPE_URL_PHISHING, and
@@ -451,12 +456,12 @@ void InterstitialHTMLSource::StartDataRequest(
     interstitial_delegate.reset(CreateSafeBrowsingBlockingPage(web_contents));
   } else if (path_without_query == "/clock") {
     interstitial_delegate.reset(CreateBadClockBlockingPage(web_contents));
-  }
 #if BUILDFLAG(ENABLE_CAPTIVE_PORTAL_DETECTION)
-  else if (path_without_query == "/captiveportal") {
+  } else if (path_without_query == "/captiveportal") {
     interstitial_delegate.reset(CreateCaptivePortalBlockingPage(web_contents));
-  }
 #endif
+  }
+
   if (path_without_query == "/supervised_user") {
     html = GetSupervisedUserInterstitialHTML(path);
   } else if (path_without_query == "/quietsafebrowsing") {
