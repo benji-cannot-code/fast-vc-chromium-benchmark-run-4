2535b27ec02d03a66079c9c19f3d4ff52cf613d3
Steven Bennetts <stevenjb@chromium.org>
2017-09-14 20:16:05 +0000

    NetworkStateHandler: Add EnsureCellularNetwork (Take 2)
    This CL provide support for Cellular devices with no service:
    * Cellular networks all use the same specifier so that the guid
      remains consistent (we only support one Cellular Service)
    * Assume a SIM may be present unless Cellular.NetworkTechnology
      is CDMA so that the SIM UI shows up even if NetworkTechnology
      is unset (e.g. because there is no Service).
    * Create a dummy Cellular NetworkState when none exists so that
      Cellular Device properties are translated and passed to the UI.
      This also allows Cellular network UI to be shown so that
      Device properties can be viewed and SIM and FoundNetworks may
      be selected.
    
    + Fix AutoConnectHandler::DisconnectFromUnmanagedSharedWiFiNetworks
      (bug found in ASAN tests)
    
    Previously reviewed at: crrev.com/c/653723
    
    Bug: 717063
    Change-Id: Ie54e90b57a9b0453fb4b7ba2853b498d93d7b27b
    TBR: khorimoto@chromium.org,benchan@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/666978
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502030}

--- chromeos/network/network_state.cc
+++ chromeos/network/network_state.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "base/memory/ptr_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
 #include "base/values.h"
@@ -25,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 const char kErrorUnknown[] = "Unknown";
+
+const char kDefaultCellularNetworkPath[] = "/cellular";
 
 bool ConvertListValueToStringVector(const base::ListValue& string_list,
                                     std::vector<std::string>* result) {
@@ -244,9 +247,10 @@ void NetworkState::GetStateProperties(base::DictionaryValue* dictionary) const {
   dictionary->SetKey(shill::kProfileProperty, base::Value(profile_path()));
   dictionary->SetKey(shill::kPriorityProperty, base::Value(priority_));
 
-  if (visible()) {
+  if (visible())
     dictionary->SetKey(shill::kStateProperty, base::Value(connection_state()));
-  }
+  if (!device_path().empty())
+    dictionary->SetKey(shill::kDeviceProperty, base::Value(device_path()));
 
   // VPN properties.
   if (NetworkTypePattern::VPN().MatchesType(type())) {
@@ -405,6 +409,11 @@ bool NetworkState::IsPrivate() const {
          profile_path_ != NetworkProfileHandler::GetSharedProfilePath();
 }
 
+bool NetworkState::IsDefaultCellular() const {
+  return type() == shill::kTypeCellular &&
+         path() == kDefaultCellularNetworkPath;
+}
+
 std::string NetworkState::GetHexSsid() const {
   return base::HexEncode(raw_ssid().data(), raw_ssid().size());
 }
@@ -430,9 +439,9 @@ std::string NetworkState::GetSpecifier() const {
   }
   if (type() == shill::kTypeWifi)
     return name() + "_" + security_class_;
-  if (!name().empty())
+  if (type() != shill::kTypeCellular && !name().empty())
     return name();
-  return type();  // For unnamed networks such as ethernet.
+  return type();  // For unnamed networks, i.e. Ethernet and Cellular.
 }
 
 void NetworkState::SetGuid(const std::string& guid) {
@@ -479,6 +488,17 @@ bool NetworkState::NetworkStateIsCaptivePortal(
 bool NetworkState::ErrorIsValid(const std::string& error) {
   // Shill uses "Unknown" to indicate an unset or cleared error state.
   return !error.empty() && error != kErrorUnknown;
+}
+
+// static
+std::unique_ptr<NetworkState> NetworkState::CreateDefaultCellular(
+    const std::string& device_path) {
+  auto new_state = base::MakeUnique<NetworkState>(kDefaultCellularNetworkPath);
+  new_state->set_type(shill::kTypeCellular);
+  new_state->set_update_received();
+  new_state->set_visible(true);
+  new_state->device_path_ = device_path;
+  return new_state;
 }
 
 }  // namespace chromeos
