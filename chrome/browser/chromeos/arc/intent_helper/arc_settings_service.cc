1122e8b43a9aa7093c13923411ca491f374ab1d9
Oren Blasberg <orenb@google.com>
2017-09-14 23:49:04 +0000

    Sync "SMS Connect enabled" pref to ARC.
    Bug: 718617
    Change-Id: Ibf8e1a2e26cc6194a7cc0642854ee2be52392b06
    Reviewed-on: https://chromium-review.googlesource.com/665668
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Commit-Queue: Oren Blasberg <orenb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502104}

--- chrome/browser/chromeos/arc/intent_helper/arc_settings_service.cc
+++ chrome/browser/chromeos/arc/intent_helper/arc_settings_service.cc
@@ -160,6 +160,7 @@ class ArcSettingsServiceImpl
   void SyncLocationServiceEnabled() const;
   void SyncProxySettings() const;
   void SyncReportingConsent() const;
+  void SyncSmsConnectEnabled() const;
   void SyncSpokenFeedbackEnabled() const;
   void SyncTimeZone() const;
   void SyncTimeZoneByGeolocation() const;
@@ -266,6 +267,8 @@ void ArcSettingsServiceImpl::OnPrefChanged(const std::string& pref_name) const {
              pref_name == ::prefs::kWebKitDefaultFontSize ||
              pref_name == ::prefs::kWebKitMinimumFontSize) {
     SyncFontSize();
+  } else if (pref_name == prefs::kSmsConnectEnabled) {
+    SyncSmsConnectEnabled();
   } else if (pref_name == proxy_config::prefs::kProxy) {
     SyncProxySettings();
   } else {
@@ -306,6 +309,7 @@ void ArcSettingsServiceImpl::StartObservingSettingsChanges() {
   AddPrefToObserve(ash::prefs::kAccessibilityVirtualKeyboardEnabled);
   AddPrefToObserve(prefs::kArcBackupRestoreEnabled);
   AddPrefToObserve(prefs::kArcLocationServiceEnabled);
+  AddPrefToObserve(prefs::kSmsConnectEnabled);
   AddPrefToObserve(::prefs::kResolveTimezoneByGeolocation);
   AddPrefToObserve(::prefs::kUse24HourClock);
   AddPrefToObserve(::prefs::kWebKitDefaultFixedFontSize);
@@ -349,6 +353,7 @@ void ArcSettingsServiceImpl::SyncBootTimeSettings() const {
   SyncFontSize();
   SyncProxySettings();
   SyncReportingConsent();
+  SyncSmsConnectEnabled();
   SyncSpokenFeedbackEnabled();
   SyncTimeZone();
   SyncTimeZoneByGeolocation();
@@ -518,6 +523,12 @@ void ArcSettingsServiceImpl::SyncReportingConsent() const {
   extras.SetBoolean("reportingConsent", consent);
   SendSettingsBroadcast("org.chromium.arc.intent_helper.SET_REPORTING_CONSENT",
                         extras);
+}
+
+void ArcSettingsServiceImpl::SyncSmsConnectEnabled() const {
+  SendBoolPrefSettingsBroadcast(
+      prefs::kSmsConnectEnabled,
+      "org.chromium.arc.intent_helper.SET_SMS_CONNECT_ENABLED");
 }
 
 void ArcSettingsServiceImpl::SyncSpokenFeedbackEnabled() const {
