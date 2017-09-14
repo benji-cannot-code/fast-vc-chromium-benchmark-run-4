c3e7fa26d524a96c30634266c3ca8848805f37ce
Yuzhu Shen <yzshen@chromium.org>
2017-09-14 00:49:36 +0000

    Mojo C++ MultiplexRouter: use a base::small_map with 1 inlined slot for endpoints.
    The common case is that the master interface is the only interface running on
    a message pipe (i.e., no associated interfaces). Using a base::small_map
    is expected to improve memory usage and performance.
    
    Another purpose of this CL is to see whether the change of memory allocation
    behavior has any impact on the (possible) memory corruption bug affecting this
    map. (crbug.com/754946)
    
    BUG=754946
    
    Change-Id: I0a0c569550421cb8be6efa505866fba7be16286b
    Reviewed-on: https://chromium-review.googlesource.com/665175
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501817}

--- mojo/public/cpp/bindings/lib/multiplex_router.h
+++ mojo/public/cpp/bindings/lib/multiplex_router.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/containers/queue.h"
+#include "base/containers/small_map.h"
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
@@ -259,7 +260,8 @@ class MOJO_CPP_BINDINGS_EXPORT MultiplexRouter
   // NOTE: It is unsafe to call into this object while holding |lock_|.
   PipeControlMessageProxy control_message_proxy_;
 
-  std::map<InterfaceId, scoped_refptr<InterfaceEndpoint>> endpoints_;
+  base::small_map<std::map<InterfaceId, scoped_refptr<InterfaceEndpoint>>, 1>
+      endpoints_;
   uint32_t next_interface_id_value_ = 1;
 
   base::circular_deque<std::unique_ptr<Task>> tasks_;
