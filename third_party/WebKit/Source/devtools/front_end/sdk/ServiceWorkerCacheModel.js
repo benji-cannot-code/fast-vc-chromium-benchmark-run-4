a3fa349d91e915213083e458ab846494d4667836
kristipark <kristipark@chromium.org>
2017-09-14 22:44:19 +0000

    [DevTools] Added layout test for track valid security origin
    For Cache Storage live update.
    
    Bug: 764292
    Change-Id: I314c9acea9cb271562986efcfee6a870051d9cea
    Reviewed-on: https://chromium-review.googlesource.com/664244
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502057}

--- third_party/WebKit/Source/devtools/front_end/sdk/ServiceWorkerCacheModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/ServiceWorkerCacheModel.js
@@ -125,10 +125,8 @@ SDK.ServiceWorkerCacheModel = class extends SDK.SDKModel {
 
   _addOrigin(securityOrigin) {
     this._loadCacheNames(securityOrigin);
-    var parsedURL = securityOrigin.asParsedURL();
-    if (!parsedURL || !parsedURL.scheme.startsWith('http'))
-      return;
-    this._storageAgent.trackCacheStorageForOrigin(securityOrigin);
+    if (this._isValidSecurityOrigin(securityOrigin))
+      this._storageAgent.trackCacheStorageForOrigin(securityOrigin);
   }
 
   /**
@@ -142,10 +140,17 @@ SDK.ServiceWorkerCacheModel = class extends SDK.SDKModel {
         this._cacheRemoved(cache);
       }
     }
+    if (this._isValidSecurityOrigin(securityOrigin))
+      this._storageAgent.untrackCacheStorageForOrigin(securityOrigin);
+  }
+
+  /**
+   * @param {string} securityOrigin
+   * @return {boolean}
+   */
+  _isValidSecurityOrigin(securityOrigin) {
     var parsedURL = securityOrigin.asParsedURL();
-    if (!parsedURL || !parsedURL.scheme.startsWith('http'))
-      return;
-    this._storageAgent.untrackCacheStorageForOrigin(securityOrigin);
+    return !!parsedURL && parsedURL.scheme.startsWith('http');
   }
 
   /**
