933dc7cd45fb5bac91729a199849b1f1c354c249
Austin James Ahlstrom <aahlstrom@google.com>
2017-09-14 09:05:27 +0000

    Porting access-control-basic-whitelist-response-headers to WPT
    Bug: 745385
    Change-Id: I21ebfbb2fb0c22ddb2a3872119c87a1dcd36d90a
    Reviewed-on: https://chromium-review.googlesource.com/634025
    Commit-Queue: Austin James Ahlstrom <aahlstrom@google.com>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Takeshi Yoshino <tyoshino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501906}

---
+++ third_party/WebKit/LayoutTests/external/wpt/XMLHttpRequest/resources/access-control-basic-whitelist-response-headers.py
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+def main(request, response):
+    headers = {
+            # CORS-safelisted
+            "content-type": "text/plain",
+            "cache-control": "no cache",
+            "content-language": "en",
+            "expires": "Fri, 30 Oct 1998 14:19:41 GMT",
+            "last-modified": "Tue, 15 Nov 1994 12:45:26 GMT",
+            "pragma": "no-cache",
+
+            # Non-CORS-safelisted
+            "x-test": "foobar",
+
+            "Access-Control-Allow-Origin": "*"
+    }
+    for header in headers:
+        response.headers.set(header, headers[header])
+
+    response.content = "PASS: Cross-domain access allowed."
