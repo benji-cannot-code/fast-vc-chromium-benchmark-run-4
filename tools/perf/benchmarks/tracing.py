2d1eb1c6b27c0d5effa3d1207a118d7730fc236b
Primiano Tucci <primiano@chromium.org>
2017-09-14 12:16:36 +0000

    telemetry: disable TracingWithBackgroundMemoryInfra
    It is flaky and causes catapult roller issues, see attached bug.
    
    TBR=nednguyen@google.com,sullivan@chromium.org,ssid@chromium.org
    
    Bug: 765140
    Change-Id: I1298046d2cd0697c192d1953484f3d1c4b5d1c4f
    Reviewed-on: https://chromium-review.googlesource.com/666917
    Commit-Queue: Primiano Tucci <primiano@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501929}

--- tools/perf/benchmarks/tracing.py
+++ tools/perf/benchmarks/tracing.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from core import perf_benchmark
 
 from telemetry import benchmark
+from telemetry import decorators
 from telemetry import story
 from telemetry.timeline import chrome_trace_category_filter
 from telemetry.timeline import chrome_trace_config
@@ -15,6 +16,7 @@ import page_sets
 
 
 @benchmark.Owner(emails=['ssid@chromium.org'])
+@decorators.Disabled('all')  # crbug.com/765140
 class TracingWithBackgroundMemoryInfra(perf_benchmark.PerfBenchmark):
   """Measures the overhead of background memory-infra dumps"""
   page_set = page_sets.Top10PageSet
