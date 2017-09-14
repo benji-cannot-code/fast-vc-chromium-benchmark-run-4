acbdd8b7d63f7894f89a30d5b2320fe55cdae298
Nektarios Paisios <nektar@chromium.org>
2017-09-14 03:22:26 +0000

    Correctly reports the required and disabled states after they changed dynamically.
    Makes completing HTML forms a much better experience as previously the unavailable and required states would seemingly be announced randomly.
    R=aleventhal@chromium.org
    TESTED=Manually with Jaws NVDA and VoiceOver, browser tests
    
    Bug: 760364
    Change-Id: Ia3678314142cf32a916e64b81b4a92d7e118caa1
    Reviewed-on: https://chromium-review.googlesource.com/648550
    Reviewed-by: Dimitri Glazkov <dglazkov@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501857}

--- content/browser/accessibility/dump_accessibility_events_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_events_browsertest.cc
@@ -223,6 +223,16 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityEventsTest,
 }
 
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityEventsTest,
+                       AccessibilityEventsAriaDisabledChanged) {
+  RunEventTest(FILE_PATH_LITERAL("aria-disabled-changed.html"));
+}
+
+IN_PROC_BROWSER_TEST_F(DumpAccessibilityEventsTest,
+                       AccessibilityEventsAriaRequiredChanged) {
+  RunEventTest(FILE_PATH_LITERAL("aria-required-changed.html"));
+}
+
+IN_PROC_BROWSER_TEST_F(DumpAccessibilityEventsTest,
                        AccessibilityEventsCheckedStateChanged) {
   RunEventTest(FILE_PATH_LITERAL("checked-state-changed.html"));
 }
@@ -245,6 +255,16 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityEventsTest,
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityEventsTest,
                        AccessibilityEventsExpandedChange) {
   RunEventTest(FILE_PATH_LITERAL("expanded-change.html"));
+}
+
+IN_PROC_BROWSER_TEST_F(DumpAccessibilityEventsTest,
+                       AccessibilityEventsFormDisabledChanged) {
+  RunEventTest(FILE_PATH_LITERAL("form-disabled-changed.html"));
+}
+
+IN_PROC_BROWSER_TEST_F(DumpAccessibilityEventsTest,
+                       AccessibilityEventsFormRequiredChanged) {
+  RunEventTest(FILE_PATH_LITERAL("form-required-changed.html"));
 }
 
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityEventsTest,
--- third_party/WebKit/Source/core/html/HTMLFormControlElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLFormControlElement.cpp
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/html/HTMLFormControlElement.h"
 
+#include "core/dom/AXObjectCache.h"
 #include "core/dom/ElementTraversal.h"
 #include "core/dom/TaskRunnerHelper.h"
 #include "core/dom/events/Event.h"
@@ -189,12 +190,20 @@ void HTMLFormControlElement::DisabledAttributeChanged() {
   if (GetLayoutObject())
     LayoutTheme::GetTheme().ControlStateChanged(*GetLayoutObject(),
                                                 kEnabledControlState);
+  // TODO(dmazzoni): http://crbug.com/699438.
+  // Replace |CheckedStateChanged| with a generic tree changed event.
+  if (AXObjectCache* cache = GetDocument().ExistingAXObjectCache())
+    cache->CheckedStateChanged(this);
 }
 
 void HTMLFormControlElement::RequiredAttributeChanged() {
   SetNeedsValidityCheck();
   PseudoStateChanged(CSSSelector::kPseudoRequired);
   PseudoStateChanged(CSSSelector::kPseudoOptional);
+  // TODO(dmazzoni): http://crbug.com/699438.
+  // Replace |CheckedStateChanged| with a generic tree changed event.
+  if (AXObjectCache* cache = GetDocument().ExistingAXObjectCache())
+    cache->CheckedStateChanged(this);
 }
 
 bool HTMLFormControlElement::IsReadOnly() const {
