95bb2cfda51a6269f106924c10312e771fa51935
Kent Tamura <tkent@chromium.org>
2017-09-15 05:19:16 +0000

    Add directory names to #includes in CharacterPropertyDataGenerator.cpp
    This CL will help move_blink_source.py to qualify #includes.
    
    Bug: 578345
    Change-Id: Ib19a07ea65cb1e39471760beac9e3d9a8f8289fd
    Reviewed-on: https://chromium-review.googlesource.com/666942
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502162}

--- third_party/WebKit/Source/platform/text/CharacterPropertyDataGenerator.cpp
+++ third_party/WebKit/Source/platform/text/CharacterPropertyDataGenerator.cpp
@@ -3,16 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO(kojii): This file is compiled with $host_toolchain, which cannot find
-// include files in platform/fonts. The build scripts should include this
-// directory, and fix these #include's to "platform/fonts/...".
-#include "CharacterPropertyDataGenerator.h"
+#include "platform/text/CharacterPropertyDataGenerator.h"
 
-#include "CharacterProperty.h"
+#include <stdio.h>
 #include <cassert>
 #include <cstring>
 #include <memory>
-#include <stdio.h>
+#include "platform/text/CharacterProperty.h"
 #if !defined(USING_SYSTEM_ICU)
 #define MUTEX_H  // Prevent compile failure of utrie2.h on Windows
 #include <utrie2.h>
