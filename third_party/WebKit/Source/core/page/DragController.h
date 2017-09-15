64cccba010ac0d1cdc17253cd7d2d48cab4b47c7
Philip Rogers <pdr@chromium.org>
2017-09-15 20:17:13 +0000

    Include page scale when clipping selection bounds
    This patch updates FrameSelection's clipped bounds to be clipped
    by the visual viewport which includes pinch-zoom. For example, a
    800x600 window which has been pinch-zoomed 2x:
      LocalFrameView::VisibleContentRect(): 800x600
      VisualViewport::VisibleRectInDocument(): 400x300
    If the entire window is selected, a clipped selection rect of only
    400x300 css pixels will be visible. Previously, 800x600 was returned.
    
    This patch moves the frame selection clipping to DragController which
    is the only code that needs to clip selection. Callsites that did not
    need clipping have been switched to FrameSelection::UnclippedBounds.
    Tests have been added of both unclipped and clipped selection bounds.
    
    Bug: 758902
    Change-Id: Ib5c036c2d9422eec73d87e3816f4686a0e0da7ee
    Reviewed-on: https://chromium-review.googlesource.com/668124
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502344}

--- third_party/WebKit/Source/core/page/DragController.h
+++ third_party/WebKit/Source/core/page/DragController.h
@@ -44,6 +44,7 @@ class DragImage;
 struct DragSession;
 class DragState;
 class LocalFrame;
+class FloatRect;
 class FrameSelection;
 class HTMLInputElement;
 class Node;
@@ -84,6 +85,9 @@ class CORE_EXPORT DragController final
 
   static std::unique_ptr<DragImage> DragImageForSelection(const LocalFrame&,
                                                           float);
+
+  // Return the selection in document coords, clipped to the visual viewport.
+  static FloatRect ClippedSelection(const LocalFrame&);
 
   DECLARE_TRACE();
 
