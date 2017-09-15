cf0dce35d7b6fa4ee8428adf9e12fef34ee2ca1d
Rune Lillesveen <rune@opera.com>
2017-09-15 08:19:08 +0000

    Rebuild non-distributed children with separate WhitespaceAttacher.
    We used to pass in the same WhitespaceAttacher to the pass over non-
    distributed child nodes which caused DidVisitText() to mess up the
    whitespace re-attachment of whitespace being the left-most node of the
    distributed nodes. In particular if there was a ::before element
    changing display type preceding that left-most space.
    
    Instead, make a RebuildNonDistributedChildren() method which passes in
    a separate WhitespaceAttacher.
    
    Bug: 765090
    Change-Id: I4fd3bd417d7a5d7accdcb8b6155991093ab05f50
    Reviewed-on: https://chromium-review.googlesource.com/666681
    Reviewed-by: nainar <nainar@chromium.org>
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Commit-Queue: Rune Lillesveen <rune@opera.com>
    Cr-Commit-Position: refs/heads/master@{#502202}

--- third_party/WebKit/Source/core/dom/Element.cpp
+++ third_party/WebKit/Source/core/dom/Element.cpp
@@ -2187,8 +2187,10 @@ void Element::RebuildLayoutTree(WhitespaceAttacher& whitespace_attacher) {
       child_attacher = &whitespace_attacher;
     }
     RebuildPseudoElementLayoutTree(kPseudoIdAfter, *child_attacher);
-    RebuildShadowRootLayoutTree(*child_attacher);
-    RebuildChildrenLayoutTrees(*child_attacher);
+    if (Shadow())
+      RebuildShadowRootLayoutTree(*child_attacher);
+    else
+      RebuildChildrenLayoutTrees(*child_attacher);
     RebuildPseudoElementLayoutTree(kPseudoIdBefore, *child_attacher);
     RebuildPseudoElementLayoutTree(kPseudoIdBackdrop, *child_attacher);
     RebuildPseudoElementLayoutTree(kPseudoIdFirstLetter, *child_attacher);
@@ -2201,10 +2203,12 @@ void Element::RebuildLayoutTree(WhitespaceAttacher& whitespace_attacher) {
 
 void Element::RebuildShadowRootLayoutTree(
     WhitespaceAttacher& whitespace_attacher) {
+  DCHECK(Shadow());
   for (ShadowRoot* root = YoungestShadowRoot(); root;
        root = root->OlderShadowRoot()) {
     root->RebuildLayoutTree(whitespace_attacher);
   }
+  RebuildNonDistributedChildren();
 }
 
 void Element::RebuildPseudoElementLayoutTree(
