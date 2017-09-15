50202e0744ff65e0bcf2d3a1fa15a7e26c72206f
Evan Stade <estade@chromium.org>
2017-09-15 16:28:10 +0000

    Move user switching checks to ash/
    Bug: none
    Change-Id: Ideb4fcf31e5d6430b4c4b08addc150616a6d4a52
    Reviewed-on: https://chromium-review.googlesource.com/665938
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502276}

--- ash/session/session_controller.cc
+++ ash/session/session_controller.cc
@@ -13,7 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/session/session_observer.h"
 #include "ash/shell.h"
 #include "ash/system/power/power_event_observer.h"
+#include "ash/system/tray/system_tray.h"
 #include "ash/wm/lock_state_controller.h"
+#include "ash/wm/overview/window_selector_controller.h"
 #include "ash/wm/window_state.h"
 #include "ash/wm/window_util.h"
 #include "ash/wm/wm_event.h"
@@ -390,6 +392,18 @@ void SessionController::SetSessionLengthLimit(base::TimeDelta length_limit,
   session_start_time_ = start_time;
   for (auto& observer : observers_)
     observer.OnSessionLengthLimitChanged();
+}
+
+void SessionController::CanSwitchActiveUser(
+    CanSwitchActiveUserCallback callback) {
+  // Cancel overview mode when switching user profiles.
+  WindowSelectorController* controller =
+      Shell::Get()->window_selector_controller();
+  if (controller->IsSelecting())
+    controller->ToggleOverview();
+
+  ash::Shell::Get()->GetPrimarySystemTray()->CanSwitchAwayFromActiveUser(
+      std::move(callback));
 }
 
 void SessionController::ClearUserSessionsForTest() {
--- ash/system/tray/system_tray.h
+++ ash/system/tray/system_tray.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "ash/system/tray/system_tray_bubble.h"
 #include "ash/system/tray/tray_background_view.h"
+#include "base/callback.h"
 #include "base/macros.h"
 #include "ui/views/bubble/tray_bubble_view.h"
 #include "ui/views/view.h"
@@ -117,6 +118,11 @@ class ASH_EXPORT SystemTray : public TrayBackgroundView {
 
   // Returns TrayAudio object if present or null otherwise.
   TrayAudio* GetTrayAudio() const;
+
+  // Determines if it's ok to switch away from the currently active user. Screen
+  // casting may block this (or at least throw up a confirmation dialog). Calls
+  // |callback| with the result.
+  void CanSwitchAwayFromActiveUser(base::OnceCallback<void(bool)> callback);
 
   // TrayBackgroundView:
   void UpdateAfterShelfAlignmentChange() override;
