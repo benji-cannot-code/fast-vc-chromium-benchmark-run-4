cbaf8bc4d544ce2aa1405a899b7783420068208b
Selim Gurun <sgurun@google.com>
2017-09-15 20:05:51 +0000

    Use a WebContentsGetter to receive Webcontents
    After plznavigate, render_frame_id can be -1. Use a Webcontentsgetter
    instead
    
    BUG=765587
    
    Change-Id: If1bac1e90cffe524fd25f18099191bbfa21b06f6
    Reviewed-on: https://chromium-review.googlesource.com/669144
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Selim Gurun <sgurun@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502341}

--- android_webview/browser/aw_login_delegate.h
+++ android_webview/browser/aw_login_delegate.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/strings/string16.h"
 #include "content/public/browser/resource_dispatcher_host_login_delegate.h"
+#include "content/public/browser/resource_request_info.h"
 
 namespace net {
 class AuthChallengeInfo;
@@ -35,7 +36,10 @@ class AwLoginDelegate :
 
  private:
   ~AwLoginDelegate() override;
-  void HandleHttpAuthRequestOnUIThread(bool first_auth_attempt);
+  void HandleHttpAuthRequestOnUIThread(
+      bool first_auth_attempt,
+      const content::ResourceRequestInfo::WebContentsGetter&
+          web_contents_getter);
   void CancelOnIOThread();
   void ProceedOnIOThread(const base::string16& user,
                          const base::string16& password);
@@ -44,8 +48,6 @@ class AwLoginDelegate :
   std::unique_ptr<AwHttpAuthHandler> aw_http_auth_handler_;
   scoped_refptr<net::AuthChallengeInfo> auth_info_;
   net::URLRequest* request_;
-  int render_process_id_;
-  int render_frame_id_;
 };
 
 }  // namespace android_webview
