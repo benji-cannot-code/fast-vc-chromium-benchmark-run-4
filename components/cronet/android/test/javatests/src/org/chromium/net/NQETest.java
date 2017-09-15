1019216a249a3b6c6d9945987608a2cbd8c84d9c
Yoland Yan <yolandyan@chromium.org>
2017-09-15 17:20:11 +0000

    Convert NQETest to JUnit4
    For more on JUnit4 migration, please check src/testing/android/docs/junit4.md
    
    Bug: 640116
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester
    Change-Id: I658f6f8fcc01a5299a2f4b431ba793d4ade25ed8
    Reviewed-on: https://chromium-review.googlesource.com/668084
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Commit-Queue: Yoland Yan <yolandyan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502292}

--- components/cronet/android/test/javatests/src/org/chromium/net/NQETest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/NQETest.java
@@ -5,14 +5,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.net;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import static org.chromium.net.CronetTestRule.getContext;
+import static org.chromium.net.CronetTestRule.getTestStorage;
+
 import android.os.StrictMode;
 import android.support.test.filters.SmallTest;
 
 import org.json.JSONObject;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import org.chromium.base.Log;
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.annotations.SuppressFBWarnings;
+import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.MetricsUtils.HistogramDelta;
 import org.chromium.net.CronetTestRule.OnlyRunNativeCronet;
@@ -30,8 +43,12 @@ import java.util.concurrent.ThreadFactory;
 /**
  * Test Network Quality Estimator.
  */
+@RunWith(BaseJUnit4ClassRunner.class)
 @JNINamespace("cronet")
-public class NQETest extends CronetTestBase {
+public class NQETest {
+    @Rule
+    public final CronetTestRule mTestRule = new CronetTestRule();
+
     private static final String TAG = NQETest.class.getSimpleName();
 
     private EmbeddedTestServer mTestServer;
@@ -40,14 +57,19 @@ public class NQETest extends CronetTestBase {
     // Thread on which network quality listeners should be notified.
     private Thread mNetworkQualityThread;
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
+    @Before
+    public void setUp() throws Exception {
         mTestServer = EmbeddedTestServer.createAndStartServer(getContext());
         mUrl = mTestServer.getURL("/echo?status=200");
     }
 
+    @After
+    public void tearDown() throws Exception {
+        mTestServer.stopAndDestroyServer();
+    }
+
     private class ExecutorThreadFactory implements ThreadFactory {
+        @Override
         public Thread newThread(final Runnable r) {
             mNetworkQualityThread = new Thread(new Runnable() {
                 @Override
@@ -69,6 +91,7 @@ public class NQETest extends CronetTestBase {
         }
     }
 
+    @Test
     @SmallTest
     @Feature({"Cronet"})
     public void testNotEnabled() throws Exception {
@@ -102,6 +125,7 @@ public class NQETest extends CronetTestBase {
         cronetEngine.shutdown();
     }
 
+    @Test
     @SmallTest
     @Feature({"Cronet"})
     public void testListenerRemoved() throws Exception {
@@ -138,6 +162,7 @@ public class NQETest extends CronetTestBase {
         return new String(data, "UTF-8").contains(content);
     }
 
+    @Test
     @SmallTest
     @Feature({"Cronet"})
     public void testQuicDisabled() throws Exception {
@@ -230,6 +255,7 @@ public class NQETest extends CronetTestBase {
         assertTrue(writeCountHistogram.getDelta() > 0);
     }
 
+    @Test
     @SmallTest
     @OnlyRunNativeCronet
     @Feature({"Cronet"})
@@ -306,6 +332,7 @@ public class NQETest extends CronetTestBase {
         }
     }
 
+    @Test
     @SmallTest
     @Feature({"Cronet"})
     public void testQuicDisabledWithParams() throws Exception {
