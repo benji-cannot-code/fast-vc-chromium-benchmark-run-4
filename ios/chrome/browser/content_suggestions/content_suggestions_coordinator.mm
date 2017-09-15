418b97b6e99ca56060f20a9bf7a8c8c1994869eb
Gauthier Ambard <gambard@chromium.org>
2017-09-15 13:15:23 +0000

    Save NTP position before the omnibox is moved
    When navigating away from the NTP, the position saved should take into
    account the fact that the omnibox is automatically lifted up when
    focused. When navigating back to the NTP, the position restored should
    be the one used before the lift of the fake omnibox.
    
    Bug: 765151
    Change-Id: I52a1d09252f2eac2ffb0fc997681d936de54c886
    Reviewed-on: https://chromium-review.googlesource.com/668364
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502237}

--- ios/chrome/browser/content_suggestions/content_suggestions_coordinator.mm
+++ ios/chrome/browser/content_suggestions/content_suggestions_coordinator.mm
@@ -300,4 +300,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)setScrollsToTop:(BOOL)enable {
 }
 
+- (CGPoint)scrollOffset {
+  CGPoint collectionOffset =
+      self.suggestionsViewController.collectionView.contentOffset;
+  collectionOffset.y -=
+      self.headerCollectionInteractionHandler.collectionShiftingOffset;
+  return collectionOffset;
+}
+
 @end
