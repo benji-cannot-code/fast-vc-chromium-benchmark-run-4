e80d52d133d98b07330b5f5e64a734a4bcada921
erikchen <erikchen@chromium.org>
2017-09-15 20:02:43 +0000

    Allow local instances of PartitionAllocatorGeneric to exist.
    It's very useful for tests. However, initializing a local
    PartitionAllocatorGeneric will non-deterministically deadlock unless the
    instance is memset to 0. Given that there's already an initialization phase of
    PartitionAllocatorGeneric, add plumbing for this into base::Subtle::SpinLock as
    well.
    
    Bug: 765735
    Change-Id: Id9ab560d5e5e6111d529c7e021c39b8ae2ceac09
    Reviewed-on: https://chromium-review.googlesource.com/668571
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502339}

--- base/allocator/partition_allocator/partition_alloc.cc
+++ base/allocator/partition_allocator/partition_alloc.cc
@@ -160,6 +160,7 @@ void PartitionAllocInit(PartitionRoot* root,
 }
 
 void PartitionAllocGenericInit(PartitionRootGeneric* root) {
+  root->lock.init();
   subtle::SpinLock::Guard guard(root->lock);
 
   PartitionAllocBaseInit(root);
