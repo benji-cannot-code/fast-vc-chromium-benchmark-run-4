10838fd1ced3863deece9aff4f679eb85106827b
Justin Cohen <justincohen@google.com>
2017-09-15 12:40:09 +0000

    [ios] Fix layout issues when starting phone in voice call.
    UIKit seems to be insetting our root view controller by 20 points when
    in a phone call, but setting our main_view_controller to
    self.window.bounds is stomping on that. It's seems that pre-iOS11
    this inset would still happen later.  This issue appears when compiling
    against the iOS11 SDK, even when running on iOS10 or iOS9 devices, it
    seems.
    
    Bug: 755974
    Change-Id: I25b6e3848ae506b5531b5eb28348ac5328a6c779
    Reviewed-on: https://chromium-review.googlesource.com/668046
    Reviewed-by: Rohit Rao (ping after 24h) <rohitrao@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502230}

--- ios/chrome/browser/ui/main/main_coordinator.mm
+++ ios/chrome/browser/ui/main/main_coordinator.mm
@@ -33,9 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   MainViewController* mainViewController = [[MainViewController alloc] init];
   _mainViewController = mainViewController;
   self.window.rootViewController = self.mainViewController;
-
-  // Size the main view controller to fit the whole screen.
-  [self.mainViewController.view setFrame:self.window.bounds];
 }
 
 @end
