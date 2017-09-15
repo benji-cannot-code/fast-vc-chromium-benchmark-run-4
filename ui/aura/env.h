42ac1e186aba5dae4a3058945ae4195010721f06
Sadrul Habib Chowdhury <sadrul@chromium.org>
2017-09-15 17:56:07 +0000

    aura: We do not actively support running aura on multiple threads.
    BUG=none
    
    Change-Id: I699fd0bfd1c2c063a48082e4882126127f8e3302
    Reviewed-on: https://chromium-review.googlesource.com/663628
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502301}

--- ui/aura/env.h
+++ ui/aura/env.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/observer_list.h"
 #include "base/supports_user_data.h"
+#include "base/threading/thread_checker.h"
 #include "ui/aura/aura_export.h"
 #include "ui/base/dragdrop/os_exchange_data_provider_factory.h"
 #include "ui/events/event_handler.h"
@@ -188,6 +189,8 @@ class AURA_EXPORT Env : public ui::EventTarget,
   // This is set to true when the WindowTreeClient is destroyed. It triggers
   // creating a different WindowPort implementation.
   bool in_mus_shutdown_ = false;
+
+  THREAD_CHECKER(thread_checker_);
 
   DISALLOW_COPY_AND_ASSIGN(Env);
 };
