43dab0ab853e8c897d91eb435eb70b9578fb981e
Matt Falkenhagen <falken@chromium.org>
2017-09-15 07:43:10 +0000

    service worker: Revert registration timeout timer.
    Revert the following due to crashes.
    
    Revert "Remove the unused wait-forever-in-install-worker.js"
    This reverts commit 10503e57dda6f97af129ccb4429150163b8dddc5.
    
    Revert "Pass the WPT: register-wait-forever-in-install-worker.https.html"
    This reverts commit ddf5acab903eaca3a63d65b0558c6bf7680864d8.
    
    Bug: 761251, 723037
    Change-Id: I72235453c9465df48f637f293f3e3a122a79b567
    Reviewed-on: https://chromium-review.googlesource.com/668260
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502189}

--- content/browser/service_worker/service_worker_unregister_job.cc
+++ content/browser/service_worker/service_worker_unregister_job.cc
@@ -35,7 +35,6 @@ void ServiceWorkerUnregisterJob::AddCallback(
 }
 
 void ServiceWorkerUnregisterJob::Start() {
-  start_time_ = base::TimeTicks::Now();
   context_->storage()->FindRegistrationForPattern(
       pattern_,
       base::Bind(&ServiceWorkerUnregisterJob::OnRegistrationFound,
@@ -52,10 +51,6 @@ bool ServiceWorkerUnregisterJob::Equals(
   if (job->GetType() != GetType())
     return false;
   return static_cast<ServiceWorkerUnregisterJob*>(job)->pattern_ == pattern_;
-}
-
-base::TimeTicks ServiceWorkerUnregisterJob::StartTime() const {
-  return start_time_;
 }
 
 RegistrationJobType ServiceWorkerUnregisterJob::GetType() const {
