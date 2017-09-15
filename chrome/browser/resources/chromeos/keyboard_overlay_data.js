008fc1bba0c7faf3c47884473bf06d664bcdea6f
wutao <wutao@chromium.org>
2017-09-15 16:27:48 +0000

    Fix keyboardOverlayUnpin typo.
    Fix a typo of keyboardOverlayUnpin.
    
    Bug: 765509
    Test: On device.
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I2c429acd8e469219bbb506ae64e9a2fb6bd867f6
    Reviewed-on: https://chromium-review.googlesource.com/667984
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502275}

--- chrome/browser/resources/chromeos/keyboard_overlay_data.js
+++ chrome/browser/resources/chromeos/keyboard_overlay_data.js
@@ -4189,7 +4189,7 @@ var keyboardOverlayData = {
     'enter<>CTRL': 'keyboardOverlayAddWwwAndComAndOpenAddress',
     'enter<>SHIFT': 'keyboardOverlayFindPreviousText',
     'esc<>SEARCH': 'keyboardOverlayTaskManager',
-    'esc<>SEARCH<>SHIFT': 'keyboardOverlayUnPin',
+    'esc<>SEARCH<>SHIFT': 'keyboardOverlayUnpin',
     'f<>ALT': 'keyboardOverlayShowWrenchMenu',
     'f<>CTRL': 'keyboardOverlayFindText',
     'f1<>CTRL<>SEARCH': 'keyboardOverlayFocusPreviousPane',
