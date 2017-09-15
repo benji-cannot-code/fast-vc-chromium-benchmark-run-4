864afec4a977cccb165c1177af7e3c3d2f7589fc
Yuzhu Shen <yzshen@chromium.org>
2017-09-15 00:18:10 +0000

    SafeBrowsing URL checks: supporting notification of starting slow checks.
    This notification will be used by the URL check initiator (e.g.,
    ThrottlingURLLoader) to ask the network service to pause reading response body.
    It is useful to reduce the time window during which the network service could
    write potentially unsafe resources into disk cache.
    
    Bug: 715673
    Change-Id: Ibc606998a58709df8901a85915942af0d8986f83
    Reviewed-on: https://chromium-review.googlesource.com/661888
    Commit-Queue: Yuzhu Shen <yzshen@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502107}

--- components/safe_browsing/renderer/renderer_url_loader_throttle.h
+++ components/safe_browsing/renderer/renderer_url_loader_throttle.h
@@ -6,9 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_SAFE_BROWSING_RENDERER_RENDERER_URL_LOADER_THROTTLE_H_
 #define COMPONENTS_SAFE_BROWSING_RENDERER_RENDERER_URL_LOADER_THROTTLE_H_
 
+#include <memory>
+
 #include "base/memory/weak_ptr.h"
 #include "components/safe_browsing/common/safe_browsing.mojom.h"
 #include "content/public/common/url_loader_throttle.h"
+#include "mojo/public/cpp/bindings/binding_set.h"
 
 namespace safe_browsing {
 
@@ -16,7 +19,8 @@ namespace safe_browsing {
 // SafeBrowsing and determine whether a URL and its redirect URLs are safe to
 // load. It defers response processing until all URL checks are completed;
 // cancels the load if any URLs turn out to be bad.
-class RendererURLLoaderThrottle : public content::URLLoaderThrottle {
+class RendererURLLoaderThrottle : public content::URLLoaderThrottle,
+                                  public mojom::UrlCheckNotifier {
  public:
   // |safe_browsing| must stay alive until WillStartRequest() (if it is called)
   // or the end of this object.
@@ -25,6 +29,7 @@ class RendererURLLoaderThrottle : public content::URLLoaderThrottle {
                             int render_frame_id);
   ~RendererURLLoaderThrottle() override;
 
+ private:
   // content::URLLoaderThrottle implementation.
   void DetachFromCurrentSequence() override;
   void WillStartRequest(const content::ResourceRequest& request,
@@ -33,8 +38,12 @@ class RendererURLLoaderThrottle : public content::URLLoaderThrottle {
                            bool* defer) override;
   void WillProcessResponse(bool* defer) override;
 
- private:
-  void OnCheckUrlResult(bool proceed, bool showed_interstitial);
+  // mojom::UrlCheckNotifier implementation.
+  void OnCompleteCheck(bool proceed, bool showed_interstitial) override;
+
+  void OnCheckUrlResult(mojom::UrlCheckNotifierRequest slow_check_notifier,
+                        bool proceed,
+                        bool showed_interstitial);
 
   void OnConnectionError();
 
@@ -54,6 +63,8 @@ class RendererURLLoaderThrottle : public content::URLLoaderThrottle {
   // The time when we started deferring the request.
   base::TimeTicks defer_start_time_;
   bool deferred_ = false;
+
+  std::unique_ptr<mojo::BindingSet<mojom::UrlCheckNotifier>> notifier_bindings_;
 
   base::WeakPtrFactory<RendererURLLoaderThrottle> weak_factory_;
 };
