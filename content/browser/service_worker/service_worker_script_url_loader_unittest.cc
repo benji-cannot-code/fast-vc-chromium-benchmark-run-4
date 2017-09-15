d5a48dc2f49708555e378ccc250d15b3f339f36a
Hiroki Nakagawa <nhiroki@chromium.org>
2017-09-15 11:53:52 +0000

    S13nServiceWorker: Store fetched scripts into the service worker script storage
    This CL implements ServiceWorkerScriptURLLoader used for loading scripts for a
    new (installing) service worker. It fetches the script (the main script or
    imported script) from network, and forwards the response to a service worker in
    a renderer process, while also writing the response into the service worker
    script storage. See the class-level comments on ServiceWorkerScriptURLLoader for
    more detailed sequence.
    
    Some security checks and error handling will be implemented in following CLs
    (e.g., MIME check).
    
    This disables ServiceWorkerVersionBrowserV8CacheTest.Restart in
    content_browsertests and some layout tests. Regarding the browser test, it needs
    to read installed scripts, but the mechanism for that hasn't been implemented
    yet (see https://crbug.com/756312).
    
    Bug: 748415, 756312
    Change-Id: I388ea13083347eb276f168a9eef4efadcb069e59
    Reviewed-on: https://chromium-review.googlesource.com/618434
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502227}

--- content/browser/service_worker/service_worker_script_url_loader_unittest.cc
+++ content/browser/service_worker/service_worker_script_url_loader_unittest.cc
@@ -8,12 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "content/browser/service_worker/embedded_worker_test_helper.h"
 #include "content/browser/service_worker/service_worker_context_core.h"
+#include "content/browser/service_worker/service_worker_disk_cache.h"
 #include "content/browser/url_loader_factory_getter.h"
 #include "content/public/common/resource_request_completion_status.h"
 #include "content/public/common/url_loader_factory.mojom.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "content/public/test/test_url_loader_client.h"
+#include "mojo/common/data_pipe_utils.h"
 #include "mojo/public/cpp/bindings/strong_binding.h"
+#include "net/base/test_completion_callback.h"
 #include "net/http/http_util.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
@@ -23,7 +26,12 @@ namespace content {
 
 namespace {
 
-// A URLLoaderFactory that returns 200 OK with a simple body to any request.
+// TODO(nhiroki): Introduce a generic mechanism to inject dummy responses like
+// MockHTTPServer.
+const char kDummyHeaders[] = "HTTP/1.1 200 OK\n\n";
+const char kDummyBody[] = "this body came from the network";
+
+// A URLLoaderFactory that returns kDummyHeaders with kDummyBody to any request.
 //
 // TODO(nhiroki): We copied this from service_worker_url_loader_job_unittest.cc
 // instead of making it a common test helper because we might want to customize
@@ -42,16 +50,19 @@ class MockNetworkURLLoaderFactory final : public mojom::URLLoaderFactory {
                             mojom::URLLoaderClientPtr client,
                             const net::MutableNetworkTrafficAnnotationTag&
                                 traffic_annotation) override {
-    std::string headers = "HTTP/1.1 200 OK\n\n";
+    std::string headers(kDummyHeaders, arraysize(kDummyHeaders));
     net::HttpResponseInfo info;
-    info.headers = new net::HttpResponseHeaders(
-        net::HttpUtil::AssembleRawHeaders(headers.c_str(), headers.length()));
+    info.headers =
+        new net::HttpResponseHeaders(net::HttpUtil::AssembleRawHeaders(
+            kDummyHeaders, arraysize(kDummyHeaders)));
     ResourceResponseHead response;
     response.headers = info.headers;
     response.headers->GetMimeType(&response.mime_type);
     client->OnReceiveResponse(response, base::nullopt, nullptr);
 
-    std::string body = "this body came from the network";
+    // TODO(nhiroki): Add test cases where the body size is bigger than the
+    // buffer size used in ServiceWorkerScriptURLLoader.
+    std::string body(kDummyBody, arraysize(kDummyBody));
     uint32_t bytes_written = body.size();
     mojo::DataPipe data_pipe;
     data_pipe.producer_handle->WriteData(body.data(), &bytes_written,
@@ -80,12 +91,14 @@ class ServiceWorkerScriptURLLoaderTest : public testing::Test {
   ~ServiceWorkerScriptURLLoaderTest() override = default;
 
   void SetUp() override {
-    helper_ = base::MakeUnique<EmbeddedWorkerTestHelper>(
+    helper_ = std::make_unique<EmbeddedWorkerTestHelper>(
         base::FilePath(), base::MakeRefCounted<URLLoaderFactoryGetter>());
+
+    InitializeStorage();
 
     // Initialize URLLoaderFactory.
     mojom::URLLoaderFactoryPtr test_loader_factory;
-    mojo::MakeStrongBinding(base::MakeUnique<MockNetworkURLLoaderFactory>(),
+    mojo::MakeStrongBinding(std::make_unique<MockNetworkURLLoaderFactory>(),
                             MakeRequest(&test_loader_factory));
     helper_->url_loader_factory_getter()->SetNetworkFactoryForTesting(
         std::move(test_loader_factory));
@@ -102,6 +115,13 @@ class ServiceWorkerScriptURLLoaderTest : public testing::Test {
     version_->SetStatus(ServiceWorkerVersion::NEW);
   }
 
+  void InitializeStorage() {
+    base::RunLoop run_loop;
+    helper_->context()->storage()->LazyInitializeForTest(
+        run_loop.QuitClosure());
+    run_loop.Run();
+  }
+
   void DoRequest() {
     // Dummy values.
     int routing_id = 0;
@@ -114,10 +134,52 @@ class ServiceWorkerScriptURLLoaderTest : public testing::Test {
     request.resource_type = RESOURCE_TYPE_SERVICE_WORKER;
 
     DCHECK(!loader_);
-    loader_ = base::MakeUnique<ServiceWorkerScriptURLLoader>(
+    loader_ = std::make_unique<ServiceWorkerScriptURLLoader>(
         routing_id, request_id, options, request, client_.CreateInterfacePtr(),
         version_, helper_->url_loader_factory_getter(),
         net::MutableNetworkTrafficAnnotationTag(TRAFFIC_ANNOTATION_FOR_TESTS));
+  }
+
+  // Returns false if the entry doesn't exist for |cache_resource_id| in the
+  // storage.
+  bool VerifyStoredResponse(int64_t cache_resource_id) {
+    // Verify the response status.
+    {
+      std::unique_ptr<ServiceWorkerResponseReader> reader =
+          helper_->context()->storage()->CreateResponseReader(
+              cache_resource_id);
+      auto info_buffer = base::MakeRefCounted<HttpResponseInfoIOBuffer>();
+      net::TestCompletionCallback cb;
+      reader->ReadInfo(info_buffer.get(), cb.callback());
+      int rv = cb.WaitForResult();
+      if (rv < 0)
+        return false;
+      EXPECT_LT(0, rv);
+      EXPECT_EQ("OK", info_buffer->http_info->headers->GetStatusText());
+    }
+
+    // Verify the response body.
+    {
+      const std::string kExpectedBody(kDummyBody, arraysize(kDummyBody));
+      std::unique_ptr<ServiceWorkerResponseReader> reader =
+          helper_->context()->storage()->CreateResponseReader(
+              cache_resource_id);
+      auto buffer = base::MakeRefCounted<net::IOBufferWithSize>(512);
+      net::TestCompletionCallback cb;
+      reader->ReadData(buffer.get(), buffer->size(), cb.callback());
+      int rv = cb.WaitForResult();
+      if (rv < 0)
+        return false;
+      EXPECT_EQ(static_cast<int>(kExpectedBody.size()), rv);
+
+      std::string received_body(buffer->data(), rv);
+      EXPECT_EQ(kExpectedBody, received_body);
+    }
+    return true;
+  }
+
+  int64_t LookupResourceId(const GURL& url) {
+    return version_->script_cache_map()->LookupResourceId(url);
   }
 
  protected:
@@ -134,10 +196,18 @@ class ServiceWorkerScriptURLLoaderTest : public testing::Test {
 TEST_F(ServiceWorkerScriptURLLoaderTest, Success) {
   DoRequest();
   client_.RunUntilComplete();
-
-  EXPECT_TRUE(client_.has_received_response());
-  // TODO(nhiroki): Check the received response.
   EXPECT_EQ(net::OK, client_.completion_status().error_code);
+
+  // The response should be responded with to the client.
+  EXPECT_TRUE(client_.has_received_response());
+  EXPECT_TRUE(client_.response_body().is_valid());
+  std::string response;
+  EXPECT_TRUE(mojo::common::BlockingCopyToString(
+      client_.response_body_release(), &response));
+  EXPECT_EQ(std::string(kDummyBody, arraysize(kDummyBody)), response);
+
+  // The response should also be stored in the storage.
+  EXPECT_TRUE(VerifyStoredResponse(LookupResourceId(version_->script_url())));
 }
 
 TEST_F(ServiceWorkerScriptURLLoaderTest, RedundantWorker) {
@@ -150,8 +220,13 @@ TEST_F(ServiceWorkerScriptURLLoaderTest, RedundantWorker) {
   client_.RunUntilComplete();
 
   // The request should be aborted.
-  EXPECT_FALSE(client_.has_received_response());
   EXPECT_EQ(net::ERR_FAILED, client_.completion_status().error_code);
+  EXPECT_FALSE(client_.has_received_response());
+
+  // The response shouldn't be stored in the storage.
+  EXPECT_FALSE(VerifyStoredResponse(LookupResourceId(version_->script_url())));
 }
+
+// TODO(nhiroki): Add a test to verify an empty response body.
 
 }  // namespace content
