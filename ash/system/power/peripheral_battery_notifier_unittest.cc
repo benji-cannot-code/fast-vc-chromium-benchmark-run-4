f14f456af6b0e4641a283a2a70d59ddc3c610850
kelvinp@chromium.org <kelvinp@chromium.org>
2017-09-15 21:09:37 +0000

    Disable failing PeripheralBatteryNotifierTest
    TBR=jennyz@chromium.org
    
    Bug: 765794
    Change-Id: I708dd1e3e9cbd0d6d1f0399710869fa1dc4e026d
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/669312
    Commit-Queue: Kelvin Poon <kelvinp@chromium.org>
    Reviewed-by: Kelvin Poon <kelvinp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502371}

--- ash/system/power/peripheral_battery_notifier_unittest.cc
+++ ash/system/power/peripheral_battery_notifier_unittest.cc
@@ -167,7 +167,8 @@ TEST_F(PeripheralBatteryNotifierTest, ExtractBluetoothAddress) {
   EXPECT_TRUE(non_bluetooth_device_info.bluetooth_address.empty());
 }
 
-TEST_F(PeripheralBatteryNotifierTest, DeviceRemove) {
+// TODO(crbug.com/765794): Flaky on ash_unittests.
+TEST_F(PeripheralBatteryNotifierTest, DISABLED_DeviceRemove) {
   message_center::MessageCenter* message_center =
       message_center::MessageCenter::Get();
 
@@ -182,7 +183,8 @@ TEST_F(PeripheralBatteryNotifierTest, DeviceRemove) {
               nullptr);
 }
 
-TEST_F(PeripheralBatteryNotifierTest, StylusNotification) {
+// TODO(crbug.com/765794): Flaky on ash_unittests.
+TEST_F(PeripheralBatteryNotifierTest, DISABLED_StylusNotification) {
   // DeviceDataManager is nullptr when the config is not classic.
   // TODO(sammiequon): Make this work for mash.
   if (Shell::GetAshConfig() != Config::CLASSIC)
