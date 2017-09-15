7566a682bfdb005440fd72e86c63051e426ec633
Stephen Chenney <schenney@chromium.org>
2017-09-15 20:41:32 +0000

    Account for table sections with transforms in geometry mapping
    We have an assert in LayoutObject::OffsetFromAncestorContainer that the
    current container not have a transform property. But table sections are
    typically not containing blocks, yet may still have a transform. Make
    the code match our existing rendering (which works as expected) by
    catching the case in LayoutBoxModelObject::PushMappingToContainer
    and using a transform for the ancestor to container offset.
    
    All this is needed because we hit subsequent asserts if the computed
    bounds are not internally consistent.
    
    R=chrishtr@chromium.org
    BUG=753614
    
    Change-Id: Icd70e12def629addc625781dc4b140a18ce6942a
    Reviewed-on: https://chromium-review.googlesource.com/648027
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502355}

--- third_party/WebKit/Source/core/layout/LayoutTableSection.cpp
+++ third_party/WebKit/Source/core/layout/LayoutTableSection.cpp
@@ -125,9 +125,10 @@ void LayoutTableSection::StyleDidChange(StyleDifference diff,
       *this, *table, diff, *old_style);
 
   if (LayoutTableBoxComponent::DoCellsHaveDirtyWidth(*this, *table, diff,
-                                                     *old_style))
+                                                     *old_style)) {
     MarkAllCellsWidthsDirtyAndOrNeedsLayout(
         LayoutTable::kMarkDirtyAndNeedsLayout);
+  }
 }
 
 void LayoutTableSection::WillBeRemovedFromTree() {
