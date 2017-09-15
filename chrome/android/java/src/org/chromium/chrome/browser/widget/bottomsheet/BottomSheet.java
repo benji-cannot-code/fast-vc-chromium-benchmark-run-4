68a53206054dbf28dc2f18fd2c35f1e4b3637dbb
Theresa Wellington <twellington@google.com>
2017-09-15 17:25:43 +0000

    [Home] Announce panel opened at half/full height
    BUG=724694
    
    Change-Id: I8761d26c8281a3550101b55053a1bc311fb2f6eb
    Reviewed-on: https://chromium-review.googlesource.com/668199
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502294}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheet.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheet.java
@@ -1134,7 +1134,6 @@ public class BottomSheet
 
         dismissSelectedText();
         for (BottomSheetObserver o : mObservers) o.onSheetOpened(reason);
-        announceForAccessibility(getResources().getString(R.string.bottom_sheet_opened));
         mActivity.addViewObscuringAllTabs(this);
 
         setFocusable(true);
@@ -1444,6 +1443,12 @@ public class BottomSheet
         }
 
         mCurrentState = state;
+
+        if (mCurrentState == SHEET_STATE_HALF) {
+            announceForAccessibility(getResources().getString(R.string.bottom_sheet_opened_half));
+        } else if (mCurrentState == SHEET_STATE_FULL) {
+            announceForAccessibility(getResources().getString(R.string.bottom_sheet_opened_full));
+        }
 
         for (BottomSheetObserver o : mObservers) {
             o.onSheetStateChanged(mCurrentState);
