165f4fad2d6c93f3eea2d4b6bfec5fa8678718f8
Daniel Park <danielpark@chromium.org>
2017-09-16 02:37:33 +0000

    [Custom Context Menu] Restoring Copy Link Text Removal
    Reverts the changes in https://codereview.chromium.org/2942963002/.
    One test currently fails so it's left out so we can merge this asap.
    
    Bug: 764966
    Change-Id: I30cb67c0633fba87178720347eac6c64e440d37c
    Reviewed-on: https://chromium-review.googlesource.com/669761
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502477}

--- chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ChromeContextMenuPopulator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ChromeContextMenuPopulator.java
@@ -83,9 +83,10 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
             Collections.unmodifiableSet(CollectionUtil.newHashSet(
                     ChromeContextMenuItem.COPY_LINK_ADDRESS, ChromeContextMenuItem.CALL,
                     ChromeContextMenuItem.SEND_MESSAGE, ChromeContextMenuItem.ADD_TO_CONTACTS,
-                    ChromeContextMenuItem.COPY, ChromeContextMenuItem.LOAD_ORIGINAL_IMAGE,
-                    ChromeContextMenuItem.SAVE_LINK_AS, ChromeContextMenuItem.SAVE_IMAGE,
-                    SHARE_IMAGE, ChromeContextMenuItem.SAVE_VIDEO, SHARE_LINK));
+                    ChromeContextMenuItem.COPY, ChromeContextMenuItem.COPY_LINK_TEXT,
+                    ChromeContextMenuItem.LOAD_ORIGINAL_IMAGE, ChromeContextMenuItem.SAVE_LINK_AS,
+                    ChromeContextMenuItem.SAVE_IMAGE, SHARE_IMAGE, ChromeContextMenuItem.SAVE_VIDEO,
+                    SHARE_LINK));
 
     // Items that are included for normal Chrome browser mode.
     private static final Set<? extends ContextMenuItem> NORMAL_MODE_WHITELIST =
@@ -119,8 +120,9 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
     private static final List<? extends ContextMenuItem> LINK_GROUP = Collections.unmodifiableList(
             CollectionUtil.newArrayList(ChromeContextMenuItem.OPEN_IN_OTHER_WINDOW,
                     ChromeContextMenuItem.OPEN_IN_NEW_TAB,
-                    ChromeContextMenuItem.OPEN_IN_INCOGNITO_TAB, ChromeContextMenuItem.SAVE_LINK_AS,
-                    ChromeContextMenuItem.COPY_LINK_ADDRESS, SHARE_LINK));
+                    ChromeContextMenuItem.OPEN_IN_INCOGNITO_TAB,
+                    ChromeContextMenuItem.COPY_LINK_ADDRESS, ChromeContextMenuItem.COPY_LINK_TEXT,
+                    ChromeContextMenuItem.SAVE_LINK_AS, SHARE_LINK));
 
     private static final List<? extends ContextMenuItem> IMAGE_GROUP =
             Collections.unmodifiableList(CollectionUtil.newArrayList(
@@ -148,6 +150,7 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
         static final int ACTION_OPEN_IN_INCOGNITO_TAB = 1;
         static final int ACTION_COPY_LINK_ADDRESS = 2;
         static final int ACTION_COPY_EMAIL_ADDRESS = 3;
+        static final int ACTION_COPY_LINK_TEXT = 4;
         static final int ACTION_SAVE_LINK = 5;
         static final int ACTION_SAVE_IMAGE = 6;
         static final int ACTION_OPEN_IMAGE = 7;
@@ -296,6 +299,7 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
             }
         } else {
             supportedOptions.add(ChromeContextMenuItem.COPY_LINK_ADDRESS);
+            supportedOptions.add(ChromeContextMenuItem.COPY_LINK_TEXT);
             supportedOptions.add(ChromeContextMenuItem.COPY);
         }
 
@@ -376,6 +380,7 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
                 }
             }
         }
+
         return groupedItems;
     }
 
@@ -461,6 +466,10 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
             disabledOptions.add(ChromeContextMenuItem.OPEN_IN_INCOGNITO_TAB);
         }
 
+        if (params.getLinkText().trim().isEmpty() || params.isImage()) {
+            disabledOptions.add(ChromeContextMenuItem.COPY_LINK_TEXT);
+        }
+
         if (params.isAnchor() && !isAcceptedScheme(params.getLinkUrl())) {
             disabledOptions.add(ChromeContextMenuItem.OPEN_IN_OTHER_WINDOW);
             disabledOptions.add(ChromeContextMenuItem.OPEN_IN_NEW_TAB);
@@ -474,6 +483,7 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
         }
 
         if (MailTo.isMailTo(params.getLinkUrl())) {
+            disabledOptions.add(ChromeContextMenuItem.COPY_LINK_TEXT);
             disabledOptions.add(ChromeContextMenuItem.COPY_LINK_ADDRESS);
             if (!mDelegate.supportsSendEmailMessage()) {
                 disabledOptions.add(ChromeContextMenuItem.SEND_MESSAGE);
@@ -484,6 +494,7 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
             }
             disabledOptions.add(ChromeContextMenuItem.CALL);
         } else if (UrlUtilities.isTelScheme(params.getLinkUrl())) {
+            disabledOptions.add(ChromeContextMenuItem.COPY_LINK_TEXT);
             disabledOptions.add(ChromeContextMenuItem.COPY_LINK_ADDRESS);
             if (!mDelegate.supportsCall()) {
                 disabledOptions.add(ChromeContextMenuItem.CALL);
@@ -560,6 +571,10 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
             }
         }
 
+        if (ChromeFeatureList.isEnabled(ChromeFeatureList.CUSTOM_CONTEXT_MENU)) {
+            disabledOptions.add(ChromeContextMenuItem.COPY_LINK_TEXT);
+        }
+
         return disabledOptions;
     }
 
@@ -617,6 +632,10 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
                 mDelegate.onSaveToClipboard(UrlUtilities.getTelNumber(params.getLinkUrl()),
                         ContextMenuItemDelegate.CLIPBOARD_TYPE_LINK_URL);
             }
+        } else if (itemId == R.id.contextmenu_copy_link_text) {
+            ContextMenuUma.record(params, ContextMenuUma.ACTION_COPY_LINK_TEXT);
+            mDelegate.onSaveToClipboard(
+                    params.getLinkText(), ContextMenuItemDelegate.CLIPBOARD_TYPE_LINK_TEXT);
         } else if (itemId == R.id.contextmenu_save_image) {
             ContextMenuUma.record(params, ContextMenuUma.ACTION_SAVE_IMAGE);
             if (mDelegate.startDownload(params.getSrcUrl(), false)) {
@@ -664,6 +683,7 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
         } else {
             assert false;
         }
+
         return true;
     }
 
