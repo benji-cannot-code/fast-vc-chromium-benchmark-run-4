0e33c3315028222e4e1e5b11e5103c1be00ad404
Vladimir Levin <vmpstr@chromium.org>
2017-09-16 00:51:18 +0000

    oop raster: Plumb linux font service to the gpu process.
    This patch allows fonts to be requested on the gpu process. This uses
    the same sandbox socket that the renderers use to get to the browser
    which handles the IPC requests.
    
    R=enne@chromium.org, piman@chromium.org
    
    Change-Id: I8d674d7efd6095b404917815b6ecbf8a08e1a3f1
    Reviewed-on: https://chromium-review.googlesource.com/656536
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502456}

--- content/browser/child_process_launcher_helper_linux.cc
+++ content/browser/child_process_launcher_helper_linux.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_switches.h"
 #include "content/public/common/result_codes.h"
 #include "content/public/common/sandboxed_process_launcher_delegate.h"
+#include "gpu/config/gpu_switches.h"
 
 namespace content {
 namespace internal {
@@ -47,7 +48,10 @@ void ChildProcessLauncherHelper::BeforeLaunchOnLauncherThread(
   options->fds_to_remap = files_to_register.GetMappingWithIDAdjustment(
       base::GlobalDescriptors::kBaseDescriptor);
 
-  if (GetProcessType() == switches::kRendererProcess) {
+  if (GetProcessType() == switches::kRendererProcess ||
+      (GetProcessType() == switches::kGpuProcess &&
+       base::CommandLine::ForCurrentProcess()->HasSwitch(
+           switches::kEnableOOPRasterization))) {
     const int sandbox_fd = SandboxHostLinux::GetInstance()->GetChildSocket();
     options->fds_to_remap.push_back(std::make_pair(sandbox_fd, GetSandboxFD()));
   }
