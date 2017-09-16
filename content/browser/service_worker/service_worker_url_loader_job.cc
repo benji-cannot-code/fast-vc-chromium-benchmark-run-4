7ae46f0730e2784d07992292b90f917419e00c83
Yuzhu Shen <yzshen@chromium.org>
2017-09-16 01:31:59 +0000

    Extend URLLoader mojo interface to support pause/resume caching response body.
    Two methods are added:
    - PauseCachingResponseBody();
    - ResumeCachingResponseBody();
    
    These methods will be used later by ThrottlingURLLoader. When it gets
    notified by SafeBrowsing that a resource is potentially unsafe, it needs
    to notify the network service to pause caching. That way we minimize the
    time window in which the network service could write unsafe resources
    into disk cache.
    
    The reason *not* to introduce Pause/Resume() instead: although they are more
    general-purposed, Pause/Resume() apply to all kinds of URLLoaders which means
    we will need to implement these methods for all of them. That would be a
    waste of effort because the current use case only cares about pause/resume
    caching when fetching from network.
    
    This CL only changes the interface. The actual implementation in network
    service will be in a follow-up CL.
    
    BUG=715673
    
    Change-Id: If718e5aeabe0019c91dd95e9b988f8dda3d861c6
    Reviewed-on: https://chromium-review.googlesource.com/667783
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502464}

--- content/browser/service_worker/service_worker_url_loader_job.cc
+++ content/browser/service_worker/service_worker_url_loader_job.cc
@@ -308,6 +308,10 @@ void ServiceWorkerURLLoaderJob::SetPriority(net::RequestPriority priority,
   NOTIMPLEMENTED();
 }
 
+void ServiceWorkerURLLoaderJob::PauseCachingResponseBody() {}
+
+void ServiceWorkerURLLoaderJob::ResumeCachingResponseBody() {}
+
 // Blob URLLoaderClient implementation for blob reading ------------
 
 void ServiceWorkerURLLoaderJob::OnReceiveResponse(
