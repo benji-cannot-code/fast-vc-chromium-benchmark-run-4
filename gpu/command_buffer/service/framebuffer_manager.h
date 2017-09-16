87aed5ca7d4b3d1af1e9f21a337e17c547b825ec
Kai Ninomiya <kainino@chromium.org>
2017-09-16 04:06:43 +0000

    Fix adjust_src_dst_region_for_blitframebuffer for depth-only framebuffers
    Bug: 731877, 644740
    Test: conformance2/renderbuffers/multisampled-depth-renderbuffer-initialization.html
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I37f09a2bdbe4466cadd640abf76cacedaf364f31
    Reviewed-on: https://chromium-review.googlesource.com/668568
    Commit-Queue: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502486}

--- gpu/command_buffer/service/framebuffer_manager.h
+++ gpu/command_buffer/service/framebuffer_manager.h
@@ -134,6 +134,10 @@ class GPU_EXPORT Framebuffer : public base::RefCounted<Framebuffer> {
 
   const Attachment* GetReadBufferAttachment() const;
 
+  // Returns the max dimensions which fit inside all of the attachments.
+  // Can only be called after the framebuffer has been checked to be complete.
+  gfx::Size GetFramebufferValidSize() const;
+
   GLsizei GetSamples() const;
 
   bool IsDeleted() const {
@@ -346,7 +350,7 @@ class GPU_EXPORT FramebufferManager {
 
   void MarkAsComplete(Framebuffer* framebuffer);
 
-  bool IsComplete(Framebuffer* framebuffer);
+  bool IsComplete(const Framebuffer* framebuffer);
 
   void IncFramebufferStateChangeCount() {
     // make sure this is never 0.
