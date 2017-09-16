52d3d7df4efa5c42c9cf614b0de833fa4be9169e
Marti Wong <martiw@chromium.org>
2017-09-16 09:29:07 +0000

    Implement 'Open (All) in incognito' of context bar (new iOS bookmark ui)
    Demo:
    https://drive.google.com/open?id=0B1O0Z7eoZMuGRVJIU0R0RDJmTkU
    https://drive.google.com/open?id=0B1O0Z7eoZMuGQjItQ2Jfd191ZEU
    
    Bug: 695749
    Change-Id: Ia52366d721b6c59747579aab616e39c951ef3c91
    Reviewed-on: https://chromium-review.googlesource.com/666353
    Commit-Queue: Marti Wong <martiw@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Ramya Sharma <ramyasharma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502499}

--- ios/clean/chrome/browser/ui/bookmarks/bookmarks_coordinator.mm
+++ ios/clean/chrome/browser/ui/bookmarks/bookmarks_coordinator.mm
@@ -95,9 +95,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)
 bookmarkHomeViewControllerWantsDismissal:(BookmarkHomeViewController*)controller
                         navigationToUrls:(const std::vector<GURL>&)urls {
-  // TODO(crbug.com/695749): Test if the following works when adding bookmark
-  // becomes available in chrome_clean_skeleton.
-  // TODO(crbug.com/695749): See if we can share the code below from
+  // TODO(crbug.com/695749):  Rewrite this function when adding bookmark becomes
+  // available in chrome_clean_skeleton.  See if we can share the code from
+  // bookmark_home_view_controller.  Also see if we can add _currentBrowserState
+  // to ths class which can store user's tab mode before Bookmarks is open.
+  [self bookmarkHomeViewControllerWantsDismissal:controller
+                                navigationToUrls:urls
+                                     inIncognito:NO];
+}
+
+- (void)bookmarkHomeViewControllerWantsDismissal:
+            (BookmarkHomeViewController*)controller
+                                navigationToUrls:(const std::vector<GURL>&)urls
+                                     inIncognito:(BOOL)inIncognito {
+  // TODO(crbug.com/695749): Rewrite this function when adding bookmark becomes
+  // available in chrome_clean_skeleton.  See if we can share the code from
   // bookmark_home_view_controller.
 
   [self dismissBookmarkBrowser];
