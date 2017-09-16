cc3c9f8181d4aef93ded4570e699e8f3ba6450ab
Guido Urdaneta <guidou@chromium.org>
2017-09-16 17:45:56 +0000

    Add ability to reconfigure a MediaStreamVideoTrack
    This CL adds the ability to change the VideoTrackAdapterSettings of a
    track already present in a MediaStreamVideoSource.
    This is a preparatory step towards MediaStreamTrack.applyConstraints()
    since updating constraints may result in new settings for a track.
    
    Bug: 763319
    Change-Id: I68debcd4df6eca903709e9dcee6ddb852ae8d190
    Reviewed-on: https://chromium-review.googlesource.com/663857
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502507}

--- content/renderer/media/media_stream_video_source.h
+++ content/renderer/media/media_stream_video_source.h
@@ -67,6 +67,14 @@ class CONTENT_EXPORT MediaStreamVideoSource : public MediaStreamSource {
                 const ConstraintsCallback& callback);
   void RemoveTrack(MediaStreamVideoTrack* track);
 
+  // Reconfigures this MediaStreamVideoSource to use |adapter_settings| on
+  // |track|, as long as |track| is connected to this source.
+  // Do not invoke if |track| is connected to a different source, as the
+  // internal state of |track| might become inconsistent with that of its
+  // source.
+  void ReconfigureTrack(MediaStreamVideoTrack* track,
+                        const VideoTrackAdapterSettings& adapter_settings);
+
   // Called by |track| to notify the source whether it has any paths to a
   // consuming endpoint.
   void UpdateHasConsumers(MediaStreamVideoTrack* track, bool has_consumers);
@@ -145,6 +153,8 @@ class CONTENT_EXPORT MediaStreamVideoSource : public MediaStreamSource {
   // to this object being deleted.
   void FinalizeAddTrack();
   void StartFrameMonitoring();
+  void UpdateTrackSettings(MediaStreamVideoTrack* track,
+                           const VideoTrackAdapterSettings& adapter_settings);
 
   State state_;
 
