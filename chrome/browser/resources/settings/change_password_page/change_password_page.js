8b79b8e30d478b2529070f0bedce46b820557552
Jialiu Lin <jialiul@chromium.org>
2017-09-18 18:47:33 +0000

    Reland "Dismiss change password card conditionally"
    This is a reland of 1d601d98917aacdd65072497f1e1f69801c220e7
    Original change's description:
    > Dismiss change password card conditionally
    > 
    > Change password card should be dismissed if there is no more unhandled
    > password reuses. This could happen if:
    > (1) user changed their Gaia password
    > (2) user whitelisted all password reuses.
    > 
    > Bug: 761116
    > Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    > Change-Id: Id36a463f87d468d5056c93c3778141e350816a06
    > Reviewed-on: https://chromium-review.googlesource.com/653721
    > Commit-Queue: Jialiu Lin <jialiul@chromium.org>
    > Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    > Reviewed-by: Ilya Sherman <isherman@chromium.org>
    > Reviewed-by: Varun Khaneja <vakh@chromium.org>
    > Reviewed-by: Nathan Parker <nparker@chromium.org>
    > Reviewed-by: Luke Z <lpz@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#502397}
    
    TBR=dschuyler@chromium.org,isherman@chromium.org, 
    
    Bug: 761116
    Change-Id: Id9299a20a4ecebe5881d6efe7857473fe4510769
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Reviewed-on: https://chromium-review.googlesource.com/671184
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Commit-Queue: Jialiu Lin <jialiul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502621}

--- chrome/browser/resources/settings/change_password_page/change_password_page.js
+++ chrome/browser/resources/settings/change_password_page/change_password_page.js
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'settings-change-password-page',
 
+  behaviors: [WebUIListenerBehavior],
+
   properties: {
     /**
      * Preferences state.
@@ -28,6 +30,8 @@ Polymer({
   attached: function() {
     this.browserProxy_ = settings.ChangePasswordBrowserProxyImpl.getInstance();
     this.browserProxy_.onChangePasswordPageShown();
+    this.addWebUIListener(
+        'change-password-on-dismiss', this.onDismiss_.bind(this));
   },
 
   /** @private */
@@ -35,6 +39,14 @@ Polymer({
     listenOnce(this, 'transitionend', () => {
       this.browserProxy_.changePassword();
     });
-    this.fire('change-password-clicked');
   },
+
+  /**
+   * Listener of event "change-password-on-dismiss".
+   * This function hides the change password card.
+   * @private
+   */
+  onDismiss_: function() {
+    this.fire('change-password-dismissed');
+  }
 });
