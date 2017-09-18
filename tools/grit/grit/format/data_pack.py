66bbdd46592b9e24e15583d19d5a41c7742ab392
Peter Wen <wnwen@chromium.org>
2017-09-18 14:24:48 +0000

    Grit: Add pak.info mapping id to source grd file
    Knowing which ids map to which names and grd or grdp file is important
    for attributing pak file sizes. This information will then be processed
    to be visible through the supersize tool.
    
    BUG=723672
    
    Change-Id: I218faf7f786174f93d5518e90917ed0656e6e4ce
    Reviewed-on: https://chromium-review.googlesource.com/650652
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502573}

--- tools/grit/grit/format/data_pack.py
+++ tools/grit/grit/format/data_pack.py
@@ -41,6 +41,7 @@ DataPackContents = collections.namedtuple(
 def Format(root, lang='en', output_dir='.'):
   """Writes out the data pack file format (platform agnostic resource file)."""
   data = {}
+  root.info = []
   for node in root.ActiveDescendants():
     with node:
       if isinstance(node, (include.IncludeNode, message.MessageNode,
@@ -48,6 +49,8 @@ def Format(root, lang='en', output_dir='.'):
         id, value = node.GetDataPackPair(lang, UTF8)
         if value is not None:
           data[id] = value
+          root.info.append(
+              '{},{},{}'.format(node.attrs.get('name'), id, node.source))
   return WriteDataPackToString(data, UTF8)
 
 
@@ -174,6 +177,7 @@ def RePack(output_file, input_files, whitelist_file=None,
       inconsistent.
   """
   input_data_packs = [ReadDataPack(filename) for filename in input_files]
+  input_info_files = [filename + '.info' for filename in input_files]
   whitelist = None
   if whitelist_file:
     whitelist = util.ReadFile(whitelist_file, util.RAW_TEXT).strip().split('\n')
@@ -181,6 +185,10 @@ def RePack(output_file, input_files, whitelist_file=None,
   resources, encoding = RePackFromDataPackStrings(
       input_data_packs, whitelist, suppress_removed_key_output)
   WriteDataPack(resources, output_file, encoding)
+  with open(output_file + '.info', 'w') as output_info_file:
+    for filename in input_info_files:
+      with open(filename, 'r') as info_file:
+        output_info_file.writelines(info_file.readlines())
 
 
 def RePackFromDataPackStrings(inputs, whitelist,
