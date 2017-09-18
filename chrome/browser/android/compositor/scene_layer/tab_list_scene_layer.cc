24f53b0dd04d881018a66cdb3d40383d975c98d6
Yaron Friedman <yfriedman@chromium.org>
2017-09-18 14:27:11 +0000

    Remove unused Java Global Ref in TabListScenePlayer.
    Noticed this while digging through crash reports for this bug.
    BUG=726426
    
    Change-Id: I46af334a2366df7cf0b5597a06941b15116fd394
    Reviewed-on: https://chromium-review.googlesource.com/665585
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502574}

--- chrome/browser/android/compositor/scene_layer/tab_list_scene_layer.cc
+++ chrome/browser/android/compositor/scene_layer/tab_list_scene_layer.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/android/compositor/scene_layer/tab_list_scene_layer.h"
 
-#include "base/android/jni_android.h"
 #include "chrome/browser/android/compositor/layer/content_layer.h"
 #include "chrome/browser/android/compositor/layer/tab_layer.h"
 #include "chrome/browser/android/compositor/layer_title_cache.h"
@@ -185,11 +184,6 @@ void TabListSceneLayer::PutTabLayer(
   gfx::RectF content(x, y, width, height);
 
   content_obscures_self_ |= content.Contains(self);
-}
-
-base::android::ScopedJavaLocalRef<jobject> TabListSceneLayer::GetJavaObject(
-    JNIEnv* env) {
-  return base::android::ScopedJavaLocalRef<jobject>(java_obj_);
 }
 
 void TabListSceneLayer::OnDetach() {
--- chrome/browser/android/compositor/scene_layer/tab_list_scene_layer.h
+++ chrome/browser/android/compositor/scene_layer/tab_list_scene_layer.h
@@ -11,9 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <unordered_set>
 
-#include "base/android/jni_android.h"
-#include "base/android/jni_weak_ref.h"
-#include "base/android/scoped_java_ref.h"
 #include "base/macros.h"
 #include "cc/layers/layer.h"
 #include "chrome/browser/android/compositor/layer/layer.h"
@@ -108,8 +105,6 @@ class TabListSceneLayer : public SceneLayer {
       jfloat side_border_scale,
       jboolean inset_border);
 
-  base::android::ScopedJavaLocalRef<jobject> GetJavaObject(JNIEnv* env);
-
   void OnDetach() override;
   bool ShouldShowBackground() override;
   SkColor GetBackgroundColor() override;
@@ -122,7 +117,6 @@ class TabListSceneLayer : public SceneLayer {
   TabMap tab_map_;
   std::set<int> visible_tabs_this_frame_;
 
-  base::android::ScopedJavaGlobalRef<jobject> java_obj_;
   bool content_obscures_self_;
   ui::ResourceManager* resource_manager_;
   LayerTitleCache* layer_title_cache_;
