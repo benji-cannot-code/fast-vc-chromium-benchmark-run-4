775ccd47c66c0aaf9c973b0b8bfec8351bd09a48
Aaron Leventhal <aleventhal@chromium.org>
2017-09-18 19:59:11 +0000

    Refactor relations
    This moves AXObjectCacheImpl's code related to relations (specifically aria-owns at this point), into it's own class and makes the code simpler to understand.
    This is pre-work for addressing issues with aria-labelledby. 
     
    
    Bug: 764025, 641325, 641324
    Change-Id: I182f6577393927cf1e93bf9849e7b1ed2c87719c
    Reviewed-on: https://chromium-review.googlesource.com/669004
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502654}

--- third_party/WebKit/Source/modules/accessibility/AXNodeObject.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXNodeObject.cpp
@@ -2671,6 +2671,7 @@ void AXNodeObject::UpdateAccessibilityRole() {
 void AXNodeObject::ComputeAriaOwnsChildren(
     HeapVector<Member<AXObject>>& owned_children) const {
   Vector<String> id_vector;
+  // Case 1: owned children not allowed
   if (!CanHaveChildren() || IsNativeTextControl() ||
       HasContentEditableAttributeSet()) {
     if (GetNode())
@@ -2678,6 +2679,7 @@ void AXNodeObject::ComputeAriaOwnsChildren(
     return;
   }
 
+  // Case 2: AOM owns property
   HeapVector<Member<Element>> elements;
   if (HasAOMProperty(AOMRelationListProperty::kOwns, elements)) {
     AxObjectCache().UpdateAriaOwns(this, id_vector, owned_children);
@@ -2694,6 +2696,7 @@ void AXNodeObject::ComputeAriaOwnsChildren(
   if (!HasAttribute(aria_ownsAttr))
     return;
 
+  // Case 3: aria-owns attribute
   TokenVectorFromAttribute(id_vector, aria_ownsAttr);
   AxObjectCache().UpdateAriaOwns(this, id_vector, owned_children);
 }
