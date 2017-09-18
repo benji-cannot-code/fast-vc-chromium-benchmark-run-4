936d3c13ebf294545afa7163ef5ed6f613f21558
Weidong Guo <weidongg@chromium.org>
2017-09-18 22:34:39 +0000

    cros: Increase timeout for app list
    Changes:
    Increase timeout of retrieving search results in app list to 60 seconds.
    
    BUG=765339
    
    Change-Id: I0038b401d2fd2698bb98cbdf07d6c36312c652f8
    Reviewed-on: https://chromium-review.googlesource.com/671813
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Vadim Tryshev <vadimt@chromium.org>
    Commit-Queue: Weidong Guo <weidongg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502699}

--- ui/app_list/search_controller.cc
+++ ui/app_list/search_controller.cc
@@ -24,7 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Maximum time (in milliseconds) to wait to the search providers to finish.
-constexpr int kStopTimeMS = 1500;
+// The value is increased from 1500 ms. See crbug.com/765339.
+constexpr int kStopTimeMS = 60000;
+
 }
 
 namespace app_list {
