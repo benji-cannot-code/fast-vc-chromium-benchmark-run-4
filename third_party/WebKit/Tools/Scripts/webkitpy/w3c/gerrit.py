99605f526e87d7cad4bbc98296c6b8a6b81d7724
Robert Ma <robertma@chromium.org>
2017-09-18 19:38:23 +0000

    Revert "WPT export: use CL issue number in branch name"
    This reverts commit 857f2d66dc9d75282c77a2fcd9b61c373c25d25a.
    
    Reason for revert: exception thrown from exporter:
      branch_name = 'chromium-export-cl-' + cl.number
    TypeError: cannot concatenate 'str' and 'int' objects
    
    Original change's description:
    > WPT export: use CL issue number in branch name
    > 
    > Branch names with Change-Id in them were a bit too long. The issue
    > number on the other hand is much more reasonable.
    > 
    > This CL will not break previsouly created PRs, because we always
    > query the branch names of existing PRs and keep using them.
    > 
    > Change-Id: Icec3cb225aac75f5d8a2382465b2faee679d936f
    > Reviewed-on: https://chromium-review.googlesource.com/669479
    > Commit-Queue: Robert Ma <robertma@chromium.org>
    > Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#502586}
    
    TBR=qyearsley@chromium.org,robertma@chromium.org
    
    Change-Id: If978d065daae6d47c2f40f78b8c28d1976c4a770
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/671526
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: Robert Ma <robertma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502643}

--- third_party/WebKit/Tools/Scripts/webkitpy/w3c/gerrit.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/w3c/gerrit.py
@@ -65,12 +65,8 @@ class GerritCL(object):
         self.api = api
 
     @property
-    def number(self):
-        return self._data['_number']
-
-    @property
     def url(self):
-        return '{}/{}'.format(URL_BASE, self.number)
+        return '{}/{}'.format(URL_BASE, self._data['_number'])
 
     @property
     def subject(self):
