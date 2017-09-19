fdb3d07f3c01e6bab5f67be9ea0b0e3dbcb5ee21
Evan Stade <estade@chromium.org>
2017-09-19 23:01:17 +0000

    Move session abort dialog from chrome/ to ash/.
    Bug: 756085
    Change-Id: I749251c80206467a37b40bb99c2a55f0825fc6be
    Reviewed-on: https://chromium-review.googlesource.com/671924
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502978}

--- ash/session/session_controller.cc
+++ ash/session/session_controller.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/interfaces/pref_connector.mojom.h"
 #include "ash/public/interfaces/user_info.mojom.h"
+#include "ash/session/session_aborted_dialog.h"
 #include "ash/session/session_observer.h"
 #include "ash/session/teleport_warning_dialog.h"
 #include "ash/shell.h"
@@ -410,6 +411,11 @@ void SessionController::CanSwitchActiveUser(
 void SessionController::ShowTeleportWarningDialog(
     ShowTeleportWarningDialogCallback callback) {
   TeleportWarningDialog::Show(std::move(callback));
+}
+
+void SessionController::ShowMultiprofilesSessionAbortedDialog(
+    const std::string& user_email) {
+  SessionAbortedDialog::Show(user_email);
 }
 
 void SessionController::ClearUserSessionsForTest() {
