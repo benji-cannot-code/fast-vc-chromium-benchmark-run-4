77feb20f80437deb0ffa0d45e04aeb7cda72212e
Ian Vollick <vollick@chromium.org>
2017-09-19 19:54:46 +0000

    [vr] Add UiElement iterators
    With this CL you can now iterate across elements both forward and
    backward (for rendering and hit testing, respectively).
    
    Bug: None
    Change-Id: Iadc1f43353ac5ed8cef550103bac223129802035
    Reviewed-on: https://chromium-review.googlesource.com/672605
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502926}

--- chrome/browser/vr/ui_scene.h
+++ chrome/browser/vr/ui_scene.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "chrome/browser/vr/color_scheme.h"
+#include "chrome/browser/vr/elements/ui_element.h"
+#include "chrome/browser/vr/elements/ui_element_iterator.h"
 #include "chrome/browser/vr/elements/ui_element_name.h"
 #include "third_party/skia/include/core/SkColor.h"
 
@@ -33,15 +35,6 @@ class UiElement;
 
 class UiScene {
  public:
-  enum Command {
-    ADD_ELEMENT,
-    UPDATE_ELEMENT,
-    REMOVE_ELEMENT,
-    ADD_ANIMATION,
-    REMOVE_ANIMATION,
-    CONFIGURE_SCENE,
-  };
-
   UiScene();
   virtual ~UiScene();
 
@@ -98,6 +91,23 @@ class UiScene {
   }
 
   void OnGlInitialized();
+
+  UiElement::iterator begin() { return root_element_->begin(); }
+  UiElement::iterator end() { return root_element_->end(); }
+
+  UiElement::const_iterator begin() const {
+    return const_cast<const UiElement&>(*root_element_).begin();
+  }
+  UiElement::const_iterator end() const {
+    return const_cast<const UiElement&>(*root_element_).end();
+  }
+
+  UiElement::Reversed<UiElement> reversed() {
+    return UiElement::Reversed<UiElement>(root_element_.get());
+  }
+  const UiElement::Reversed<const UiElement> reversed() const {
+    return UiElement::Reversed<const UiElement>(root_element_.get());
+  }
 
  private:
   void Animate(const base::TimeTicks& current_time);
