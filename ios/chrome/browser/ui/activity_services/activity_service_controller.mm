7a99d6158d3e6781e2e31cc50f3a4b1e30cf14fe
Olivier Robin <olivierrobin@chromium.org>
2017-09-19 09:21:08 +0000

    [iOS] Remove UIActivityTextSource.
    Bug: 755620
    
    Change-Id: Id22d2b9b986494328f7dcf93d06771118e44289c
    Reviewed-on: https://chromium-review.googlesource.com/671007
    Reviewed-by: Jean-François Geyelin <jif@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502813}

--- ios/chrome/browser/ui/activity_services/activity_service_controller.mm
+++ ios/chrome/browser/ui/activity_services/activity_service_controller.mm
@@ -221,10 +221,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                             thumbnailGenerator:data.thumbnailGenerator];
   [activityItems addObject:loginActionProvider];
 
-  UIActivityTextSource* textProvider =
-      [[UIActivityTextSource alloc] initWithText:data.title];
-  [activityItems addObject:textProvider];
-
   if (data.image) {
     UIActivityImageSource* imageProvider =
         [[UIActivityImageSource alloc] initWithImage:data.image];
--- ios/chrome/browser/ui/activity_services/chrome_activity_item_source.h
+++ ios/chrome/browser/ui/activity_services/chrome_activity_item_source.h
@@ -11,14 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/activity_services/chrome_activity_item_thumbnail_generator.h"
 
-// Returns a text to the UIActivities that can take advantage of it.
-@interface UIActivityTextSource : NSObject<UIActivityItemSource>
-
-// Default initializer. |text| must not be nil.
-- (instancetype)initWithText:(NSString*)text;
-
-@end
-
 // Returns an image to the UIActivities that can take advantage of it.
 @interface UIActivityImageSource : NSObject<UIActivityItemSource>
 
