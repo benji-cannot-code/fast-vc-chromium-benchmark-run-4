e854285b87fd5731eac4b699f843e423d4e44c8d
Gauthier Ambard <gambard@chromium.org>
2017-09-19 12:49:22 +0000

    Display the empty section if needed
    The ContentSuggestions empty sections can be displayed if their section
    info allow them to.
    For now they are only displayed if they are started at the opening of
    the NTP. This CL fixes it by adding them if needed.
    
    Bug: 765075
    Change-Id: Ieb8dde40d14044bfb674bcd9108b84775d1b4531
    Reviewed-on: https://chromium-review.googlesource.com/671021
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502833}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_updater.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_updater.mm
@@ -363,6 +363,9 @@ NSString* const kContentSuggestionsCollectionUpdaterSnackbarCategory =
       self.collectionViewController.collectionViewModel;
   NSInteger sectionIdentifier = SectionIdentifierForInfo(sectionInfo);
 
+  if (![model hasSectionForSectionIdentifier:sectionIdentifier])
+    return nil;
+
   NSArray<CSCollectionViewItem*>* existingItems =
       [model itemsInSectionWithIdentifier:sectionIdentifier];
   if (existingItems.count == 1 && existingItems[0].type == ItemTypeEmpty) {
--- ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.mm
@@ -138,10 +138,6 @@ BOOL ShouldCellsBeFullWidth(UITraitCollection* collection) {
 
 - (void)addSuggestions:(NSArray<CSCollectionViewItem*>*)suggestions
          toSectionInfo:(ContentSuggestionsSectionInformation*)sectionInfo {
-  if (suggestions.count == 0) {
-    return;
-  }
-
   [self.collectionView performBatchUpdates:^{
     NSIndexSet* addedSections = [self.collectionUpdater
         addSectionsForSectionInfoToModel:@[ sectionInfo ]];
