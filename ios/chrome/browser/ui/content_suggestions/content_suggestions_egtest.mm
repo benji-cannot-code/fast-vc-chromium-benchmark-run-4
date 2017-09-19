c706d9c0effe6bdaf115aeb5379c40d4930be4c0
stkhapugin@chromium.org <stkhapugin@chromium.org>
2017-09-19 12:30:51 +0000

    Disables testMostVisitedNewIncognitoTab on iOS 11 phones.
    Disables testMostVisitedNewIncognitoTab because it's failing multiple
    times on bots downstream.
    
    Bug: 766495
    Change-Id: Ie640c58e32eb87ed7b6ae070e35e111fbe23cd81
    Reviewed-on: https://chromium-review.googlesource.com/671011
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502830}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_egtest.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_egtest.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/ios/ios_util.h"
 #include "base/mac/foundation_util.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/sys_string_conversions.h"
@@ -678,6 +679,11 @@ GREYElementInteraction* CellWithMatcher(id<GREYMatcher> matcher) {
 // Tests the "Open in New Incognito Tab" action of the Most Visited context
 // menu.
 - (void)testMostVisitedNewIncognitoTab {
+  // TODO(crbug.com/766495): Reenable this test.
+  if (base::ios::IsRunningOnIOS11OrLater() && !IsIPadIdiom()) {
+    EARL_GREY_TEST_DISABLED(@"Disabled on iOS 11 phones.");
+  }
+
   [self setupMostVisitedTileLongPress];
   const GURL pageURL = self.testServer->GetURL(kPageURL);
 
