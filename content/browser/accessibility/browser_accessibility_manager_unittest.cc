3b6d4c3eb6c167dd3b50af97d14c6ca5301e83ec
Katie Dektar <katydek@google.com>
2017-09-19 21:28:30 +0000

    Clips accessible bounding box to nearest window.
    For example, if a bounding box extends partially below of a window,
    it will be shrunk in Y to match the border of the window. If a
    bounding box is fully below a window, it will be decreased to a
    height of 1 and placed at the very bottom of that window.
    
    R=dmazzoni@chromium.org
    
    Bug: 764065
    Change-Id: Ibb105874710ebb2a0f3a7623d81b10a3f51f09ca
    Reviewed-on: https://chromium-review.googlesource.com/669949
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Katie Dektar <katydek@google.com>
    Cr-Commit-Position: refs/heads/master@{#502954}

--- content/browser/accessibility/browser_accessibility_manager_unittest.cc
+++ content/browser/accessibility/browser_accessibility_manager_unittest.cc
@@ -583,6 +583,7 @@ TEST(BrowserAccessibilityManagerTest, BoundsForRange) {
   ui::AXNodeData root;
   root.id = 1;
   root.role = ui::AX_ROLE_ROOT_WEB_AREA;
+  root.location = gfx::RectF(0, 0, 800, 600);
 
   ui::AXNodeData static_text;
   static_text.id = 2;
@@ -668,6 +669,7 @@ TEST(BrowserAccessibilityManagerTest, BoundsForRangeMultiElement) {
   ui::AXNodeData root;
   root.id = 1;
   root.role = ui::AX_ROLE_ROOT_WEB_AREA;
+  root.location = gfx::RectF(0, 0, 800, 600);
 
   ui::AXNodeData static_text;
   static_text.id = 2;
@@ -776,6 +778,7 @@ TEST(BrowserAccessibilityManagerTest, BoundsForRangeBiDi) {
   ui::AXNodeData root;
   root.id = 1;
   root.role = ui::AX_ROLE_ROOT_WEB_AREA;
+  root.location = gfx::RectF(0, 0, 800, 600);
 
   ui::AXNodeData static_text;
   static_text.id = 2;
@@ -852,6 +855,7 @@ TEST(BrowserAccessibilityManagerTest, BoundsForRangeScrolledWindow) {
   root.role = ui::AX_ROLE_ROOT_WEB_AREA;
   root.AddIntAttribute(ui::AX_ATTR_SCROLL_X, 25);
   root.AddIntAttribute(ui::AX_ATTR_SCROLL_Y, 50);
+  root.location = gfx::RectF(0, 0, 800, 600);
 
   ui::AXNodeData static_text;
   static_text.id = 2;
@@ -900,6 +904,7 @@ TEST(BrowserAccessibilityManagerTest, BoundsForRangeOnParentElement) {
   root.id = 1;
   root.role = ui::AX_ROLE_ROOT_WEB_AREA;
   root.child_ids.push_back(2);
+  root.location = gfx::RectF(0, 0, 800, 600);
 
   ui::AXNodeData div;
   div.id = 2;
