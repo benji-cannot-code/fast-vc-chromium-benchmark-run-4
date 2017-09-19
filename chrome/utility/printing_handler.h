532d7e735d77ee63551ecfce95f662c2fc0fe0fe
Jay Civelli <jcivelli@chromium.org>
2017-09-19 17:46:31 +0000

    Mojofying printing::PWGRasterConverter.
    Changing printing::PWGRasterConverter to use Mojo.
    Moving the generation code from printing_handler.cc to its own file.
    
    Bug: 758355
    Change-Id: Iaeca1665cd49c08794a6146e7ee75c694bf946d8
    Reviewed-on: https://chromium-review.googlesource.com/636743
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Jay Civelli <jcivelli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502884}

--- chrome/utility/printing_handler.h
+++ chrome/utility/printing_handler.h
@@ -21,8 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace printing {
 
-struct PwgRasterSettings;
-
 // Dispatches IPCs for printing.
 class PrintingHandler : public UtilityMessageHandler {
  public:
@@ -42,12 +40,6 @@ class PrintingHandler : public UtilityMessageHandler {
       IPC::PlatformFileForTransit output_file);
   void OnRenderPDFPagesToMetafileStop();
 #endif  // OS_WIN
-#if BUILDFLAG(ENABLE_PRINT_PREVIEW)
-  void OnRenderPDFPagesToPWGRaster(IPC::PlatformFileForTransit pdf_transit,
-                                   const PdfRenderSettings& settings,
-                                   const PwgRasterSettings& bitmap_settings,
-                                   IPC::PlatformFileForTransit bitmap_transit);
-#endif  // ENABLE_PRINT_PREVIEW
 
 #if defined(OS_WIN)
   int LoadPDF(base::File pdf_file);
@@ -57,11 +49,6 @@ class PrintingHandler : public UtilityMessageHandler {
                                bool postscript);
 #endif  // OS_WIN
 #if BUILDFLAG(ENABLE_PRINT_PREVIEW)
-  bool RenderPDFPagesToPWGRaster(base::File pdf_file,
-                                 const PdfRenderSettings& settings,
-                                 const PwgRasterSettings& bitmap_settings,
-                                 base::File bitmap_file);
-
   void OnGetPrinterCapsAndDefaults(const std::string& printer_name);
   void OnGetPrinterSemanticCapsAndDefaults(const std::string& printer_name);
 #endif  // ENABLE_PRINT_PREVIEW
