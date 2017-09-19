d12fa544600ff0d32c7a37893dec69871b77ab3d
John Budorick <jbudorick@chromium.org>
2017-09-19 01:56:36 +0000

    [android] Stop capturing full-suite logcats.
    This also switches from running tests through the butler to
    running tests alongside the butler s.t. we can independently
    control process lifetimes.
    
    Bug: 721889
    Change-Id: I176b7340d9b3f097ad660b66ea64ffb4816287a8
    Reviewed-on: https://chromium-review.googlesource.com/667808
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Benjamin Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502757}

--- tools/mb/mb.py
+++ tools/mb/mb.py
@@ -1119,7 +1119,6 @@ class MetaBuildWrapper(object):
           '--target', target,
           '--target-devices-file', '${SWARMING_BOT_FILE}',
           '--logdog-bin-cmd', '../../bin/logdog_butler',
-          '--logcat-output-file', '${ISOLATED_OUTDIR}/logcats',
           '--store-tombstones']
     elif is_fuchsia and test_type != 'script':
       cmdline = [os.path.join('bin', 'run_%s' % target)]
