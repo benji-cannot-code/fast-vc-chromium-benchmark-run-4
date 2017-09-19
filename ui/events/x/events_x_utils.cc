e0982270886d09d5d7abbb58f8cfa8c01745b688
David Reveman <reveman@chromium.org>
2017-09-19 19:37:33 +0000

    ui: Add --pen-devices flag.
    This adds a --pen-devices flag that works the same way as
    --touch-devices and makes it possible to simulate pen
    devices on Linux in addition to the current support for
    touch devices.
    
    Bug: 
    Test: manual
    Change-Id: Icc103b029b91b1e9e5d12e89c594069da38577ed
    Reviewed-on: https://chromium-review.googlesource.com/667719
    Commit-Queue: David Reveman <reveman@chromium.org>
    Reviewed-by: Vladislav Kaznacheev <kaznacheev@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502918}

--- ui/events/x/events_x_utils.cc
+++ ui/events/x/events_x_utils.cc
@@ -744,6 +744,13 @@ float GetTouchForceFromXEvent(const XEvent& xev) {
   return force;
 }
 
+EventPointerType GetTouchPointerTypeFromXEvent(const XEvent& xev) {
+  XIDeviceEvent* event = static_cast<XIDeviceEvent*>(xev.xcookie.data);
+  DCHECK(ui::TouchFactory::GetInstance()->IsTouchDevice(event->sourceid));
+  return ui::TouchFactory::GetInstance()->GetTouchDevicePointerType(
+      event->sourceid);
+}
+
 bool GetScrollOffsetsFromXEvent(const XEvent& xev,
                                 float* x_offset,
                                 float* y_offset,
