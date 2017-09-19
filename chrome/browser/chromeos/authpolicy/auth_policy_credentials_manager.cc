1b3798f9403f9358a0bea9eb44e3feb22f92d477
Tetsui Ohkubo <tetsui@chromium.org>
2017-09-19 05:33:45 +0000

    Add material design icon to AD sign in error notification.
    This change is equivalent change of https://crrev.com/c/662481 for
    Active Directory login.
    
    This CL adds new material design icon to AD sign in error notification.
    This CL also changes the notification theme color based on UX review (
    https://crbug.com/726241#c20).
    
    The icon and the accent color are effective only when
    chrome://flags/#enable-message-center-new-style-notification
    is Enabled (which is ToT default).
    
    TEST=manual
    BUG=726241
    
    Change-Id: Ifc32134967e1ecd1b4adc5fafd602c055dbcb089
    Reviewed-on: https://chromium-review.googlesource.com/666955
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502785}

--- chrome/browser/chromeos/authpolicy/auth_policy_credentials_manager.cc
+++ chrome/browser/chromeos/authpolicy/auth_policy_credentials_manager.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task_scheduler/post_task.h"
+#include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
@@ -32,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/message_center/notification_delegate.h"
 #include "ui/message_center/public/cpp/message_center_constants.h"
+#include "ui/message_center/public/cpp/message_center_switches.h"
 
 namespace {
 
@@ -318,13 +320,21 @@ void AuthPolicyCredentialsManager::ShowNotification(int message_id) {
       message_center::NOTIFICATION_TYPE_SIMPLE, notification_id,
       l10n_util::GetStringUTF16(IDS_SIGNIN_ERROR_BUBBLE_VIEW_TITLE),
       l10n_util::GetStringUTF16(message_id),
-      ui::ResourceBundle::GetSharedInstance().GetImageNamed(
-          IDR_NOTIFICATION_ALERT),
+      message_center::IsNewStyleNotificationEnabled()
+          ? gfx::Image()
+          : ui::ResourceBundle::GetSharedInstance().GetImageNamed(
+                IDR_NOTIFICATION_ALERT),
       notifier_id, l10n_util::GetStringUTF16(IDS_SIGNIN_ERROR_DISPLAY_SOURCE),
       GURL(notification_id), notification_id, data,
       new SigninNotificationDelegate());
-  notification.set_accent_color(
-      message_center::kSystemNotificationColorCriticalWarning);
+  if (message_center::IsNewStyleNotificationEnabled()) {
+    notification.set_accent_color(
+        message_center::kSystemNotificationColorCriticalWarning);
+    notification.set_small_image(gfx::Image(gfx::CreateVectorIcon(
+        kNotificationWarningIcon, message_center::kSmallImageSizeMD,
+        message_center::kSystemNotificationColorWarning)));
+    notification.set_vector_small_image(kNotificationWarningIcon);
+  }
   notification.SetSystemPriority();
 
   NotificationUIManager* notification_ui_manager =
