10c0208b23fbf8a562e680613f71460330c0d32d
Benjamin Pastene <bpastene@chromium.org>
2017-09-19 23:56:48 +0000

    mb: Stop adding --target-devices-file arg for android tests.
    Now that every swarming bot only has a single device, the file is
    unnecessary. It was initially added to prevent swarming tests from
    running on any of the bot's devices that were unhealthy. Now that
    each bot only has a single device, the entire bot will be quarantined
    if its device is unhealthy.
    
    R=jbudorick@chromium.org
    
    Bug: 
    Change-Id: I316102dfa12c860e13f3e735dd8a057767d37288
    Reviewed-on: https://chromium-review.googlesource.com/673825
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: Benjamin Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502993}

--- tools/mb/mb.py
+++ tools/mb/mb.py
@@ -1117,7 +1117,6 @@ class MetaBuildWrapper(object):
       cmdline = [
           '../../build/android/test_wrapper/logdog_wrapper.py',
           '--target', target,
-          '--target-devices-file', '${SWARMING_BOT_FILE}',
           '--logdog-bin-cmd', '../../bin/logdog_butler',
           '--store-tombstones']
     elif is_fuchsia and test_type != 'script':
