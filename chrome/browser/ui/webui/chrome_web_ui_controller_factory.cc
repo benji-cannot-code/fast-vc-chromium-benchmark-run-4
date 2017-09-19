31e1751e23ce8e7a5b7121767fef873cb7bfe14f
Anton Urusov <urusant@google.com>
2017-09-19 14:41:41 +0000

    Added an initial version of policy-tool page.
    This is the initial version of the policy-tool page that is created to make
    managing policies easier for admins.
    
    Bug: 
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ia58300dc786a6dabcd14721d134fb52027934739
    Reviewed-on: https://chromium-review.googlesource.com/613507
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Georges Khalil <georgesak@chromium.org>
    Commit-Queue: Anton Urusov <urusant@google.com>
    Cr-Commit-Position: refs/heads/master@{#502851}

--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/omnibox/omnibox_ui.h"
 #include "chrome/browser/ui/webui/password_manager_internals/password_manager_internals_ui.h"
 #include "chrome/browser/ui/webui/physical_web/physical_web_ui.h"
+#include "chrome/browser/ui/webui/policy_tool_ui.h"
 #include "chrome/browser/ui/webui/policy_ui.h"
 #include "chrome/browser/ui/webui/predictors/predictors_ui.h"
 #include "chrome/browser/ui/webui/quota_internals/quota_internals_ui.h"
@@ -549,6 +550,12 @@ WebUIFactoryFunction GetWebUIFactoryFunction(WebUI* web_ui,
 
   if (url.host_piece() == chrome::kChromeUIPolicyHost)
     return &NewWebUI<PolicyUI>;
+#if !defined(OS_ANDROID)
+  if (url.host_piece() == chrome::kChromeUIPolicyToolHost &&
+      PolicyToolUI::IsEnabled()) {
+    return &NewWebUI<PolicyToolUI>;
+  }
+#endif
 
 #if BUILDFLAG(ENABLE_APP_LIST)
   if (url.host_piece() == chrome::kChromeUIAppListStartPageHost)
