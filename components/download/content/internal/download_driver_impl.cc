aae84e90a0024d0cb639ee9587607a06578531bf
Xing Liu <xingliu@chromium.org>
2017-09-19 17:02:40 +0000

    Download Service: Fixed an issue in interrupt reason metric.
    In Download.Service.Driver.InterruptReason, it tracks all downloads,
    this CL changes to only track downloads with guid in download service's
    database.
    
    
    
    Bug: 765430
    Change-Id: Id2ddf1a5bb39f5fa47c4d301171aa5704c1633a0
    Reviewed-on: https://chromium-review.googlesource.com/667246
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502869}

--- components/download/content/internal/download_driver_impl.cc
+++ components/download/content/internal/download_driver_impl.cc
@@ -236,7 +236,8 @@ void DownloadDriverImpl::OnDownloadUpdated(content::DownloadManager* manager,
     client_->OnDownloadUpdated(entry);
   } else if (reason !=
              content::DownloadInterruptReason::DOWNLOAD_INTERRUPT_REASON_NONE) {
-    LogDownloadInterruptReason(reason);
+    if (client_->IsTrackingDownload(item->GetGuid()))
+      LogDownloadInterruptReason(reason);
     client_->OnDownloadFailed(entry, FailureTypeFromInterruptReason(reason));
   }
 }
