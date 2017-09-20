c570e103dcebdc111d6075077ca34d470cd4c8c6
Takumi Fujimoto <takumif@chromium.org>
2017-09-20 02:29:54 +0000

    [Media Router] Notify RequestManager when extension MRP disconnects
    This should prevent cases in which EventPageRequestManager::RunOrDefer() executes requests even though the MRP Mojo pointer held by ExtensionMRPProxy is invalid.
    Also make MRDesktop call ExtensionMRPProxy::OnMojoConnectionError() instead of calling EventPageRequestManager::OnMojoConnectionError() directly.
    
    Bug: 766033
    Change-Id: I2028038077e712e702fd4e81b6cbb4df703251b4
    Reviewed-on: https://chromium-review.googlesource.com/671466
    Reviewed-by: Derek Cheng <imcheng@chromium.org>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503030}

--- chrome/browser/media/router/mojo/media_router_desktop.cc
+++ chrome/browser/media/router/mojo/media_router_desktop.cc
@@ -44,7 +44,7 @@ void MediaRouterDesktop::OnUserGesture() {
 }
 
 void MediaRouterDesktop::OnConnectionError() {
-  request_manager_->OnMojoConnectionError();
+  extension_provider_.OnMojoConnectionError();
   binding_.Close();
   MediaRouterMojoImpl::OnConnectionError();
 }
