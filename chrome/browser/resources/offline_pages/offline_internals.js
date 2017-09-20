5161ed8d66d2cb41e3776b983ab06b0494bcb769
Justin DeWitt <dewittj@chromium.org>
2017-09-20 18:11:00 +0000

    [Offline Pages] Adds a notification for prefetching.
    Nothing calls into the notification handler yet, but the notification,
    when clicked, sends the user to the appropriate UI (either Downloads Home
    or the Notification Settings panel).
    
    Bug: 701939
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I448e11dccd93b0e3953a1fed3bbd09cea6315134
    Reviewed-on: https://chromium-review.googlesource.com/668025
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503200}

--- chrome/browser/resources/offline_pages/offline_internals.js
+++ chrome/browser/resources/offline_pages/offline_internals.js
@@ -315,6 +315,9 @@ cr.define('offlineInternals', function() {
           .then(setPrefetchResult)
           .catch(prefetchResultError);
     };
+    $('show-notification').onclick = function() {
+      browserProxy.showPrefetchNotification().then(setPrefetchResult);
+    };
     $('generate-page-bundle').onclick = function() {
       browserProxy.generatePageBundle($('generate-urls').value)
           .then(setPrefetchResult)
