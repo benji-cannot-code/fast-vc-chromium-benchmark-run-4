3cc994f986b41d24f0c58ef5e32eb96577d6fab5
Jaebaek Seo <jaebaek@chromium.org>
2017-09-20 05:18:41 +0000

    Scale layout and text for non-viewport-meta pages by DSF
    Enabling use-zoom-for-dsf flag scales the visual viewport and image elements by
    DSF in Blink layer, but the layout viewport and text elements are not. This
    results in showing small text and incorrect scaled layout viewports for web
    pages without the viewport meta. This CL scales the minimum width of layout
    viewport specified in viewportAndroid.css and text elements by DSF.
    
    Bug: 737777
    Change-Id: Iaff5daa47cbd6ed855dbc50e3e3a26660cc53d6e
    Reviewed-on: https://chromium-review.googlesource.com/666578
    Commit-Queue: Jaebaek Seo <jaebaek@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Alexandre Elias <aelias@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503058}

--- third_party/WebKit/Source/core/css/resolver/ViewportStyleResolver.cpp
+++ third_party/WebKit/Source/core/css/resolver/ViewportStyleResolver.cpp
@@ -48,6 +48,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/LocalFrameView.h"
 #include "core/frame/Settings.h"
 #include "core/layout/api/LayoutViewItem.h"
+#include "core/page/ChromeClient.h"
+#include "core/page/Page.h"
 
 namespace blink {
 
@@ -299,6 +301,12 @@ Length ViewportStyleResolver::ViewportLengthValue(CSSPropertyID id) {
     has_viewport_units_ = true;
   document_style->SetHasViewportUnits(document_style_has_viewport_units);
 
+  if (result.IsFixed() && document_->GetPage()) {
+    float scaled_value =
+        document_->GetPage()->GetChromeClient().WindowToViewportScalar(
+            result.GetFloatValue());
+    result.SetValue(scaled_value);
+  }
   return result;
 }
 
--- third_party/WebKit/Source/core/layout/TextAutosizer.cpp
+++ third_party/WebKit/Source/core/layout/TextAutosizer.cpp
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/LayoutView.h"
 #include "core/layout/api/LayoutAPIShim.h"
 #include "core/layout/api/LayoutViewItem.h"
+#include "core/page/ChromeClient.h"
 #include "core/page/Page.h"
 #include "platform/wtf/PtrUtil.h"
 
@@ -593,7 +594,8 @@ void TextAutosizer::UpdatePageInfo() {
              ->GetViewportDescription()
              .IsSpecifiedByAuthor()) {
       page_info_.device_scale_adjustment_ =
-          document_->GetSettings()->GetDeviceScaleAdjustment();
+          document_->GetPage()->GetChromeClient().WindowToViewportScalar(
+              document_->GetSettings()->GetDeviceScaleAdjustment());
     } else {
       page_info_.device_scale_adjustment_ = 1.0f;
     }
