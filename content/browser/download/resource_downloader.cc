85e1370c1620e4bbdcc496deb7d495f0acb0095d
Min Qin <qinmin@chromium.org>
2017-09-20 21:19:36 +0000

    Fix Cross-Origin download suggested name
    On redirection, the suggested name for download should get cleared if it is cross origin.
    
    BUG=715630
    
    Change-Id: Ib9f3c7cc2b021b5b1c1eebe47ab2a56a42dc1f18
    Reviewed-on: https://chromium-review.googlesource.com/675572
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503250}

--- content/browser/download/resource_downloader.cc
+++ content/browser/download/resource_downloader.cc
@@ -81,7 +81,6 @@ void ResourceDownloader::Start(std::unique_ptr<ResourceRequest> request) {
       download_url_parameters_->url().SchemeIs(url::kBlobScheme)
           ? url_loader_factory_getter_->GetBlobFactory()
           : url_loader_factory_getter_->GetNetworkFactory();
-
   url_loader_ = ThrottlingURLLoader::CreateLoaderAndStart(
       factory->get(), std::vector<std::unique_ptr<URLLoaderThrottle>>(),
       0,  // routing_id
@@ -109,6 +108,10 @@ void ResourceDownloader::OnResponseStarted(
                      base::MakeUnique<DownloadManager::InputStream>(
                          std::move(stream_handle)),
                      download_url_parameters_->callback()));
+}
+
+void ResourceDownloader::OnReceiveRedirect() {
+  url_loader_->FollowRedirect();
 }
 
 }  // namespace content
