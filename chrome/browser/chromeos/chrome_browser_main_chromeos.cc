c3eb347edac2fdf6029b1bae737dbdc2003d9799
Ryo Hashimoto <hashimoto@chromium.org>
2017-09-20 06:55:03 +0000

    chromeos: Add VirtualFileRequestServiceProvider
    This service will be used by the VirtualFileProvider service to read data.
    ChromeVirtualFileRequestServiceProviderDelegate will have the actual
    implementation to handle incoming method calls.
    
    BUG=740063
    
    Change-Id: I607c726a5280687643a217a94ef90c188787c87f
    Reviewed-on: https://chromium-review.googlesource.com/662088
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503075}

--- chrome/browser/chromeos/chrome_browser_main_chromeos.cc
+++ chrome/browser/chromeos/chrome_browser_main_chromeos.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/dbus/chrome_console_service_provider_delegate.h"
 #include "chrome/browser/chromeos/dbus/chrome_display_power_service_provider_delegate.h"
 #include "chrome/browser/chromeos/dbus/chrome_proxy_resolution_service_provider_delegate.h"
+#include "chrome/browser/chromeos/dbus/chrome_virtual_file_request_service_provider_delegate.h"
 #include "chrome/browser/chromeos/dbus/kiosk_info_service_provider.h"
 #include "chrome/browser/chromeos/dbus/screen_lock_service_provider.h"
 #include "chrome/browser/chromeos/display/quirks_manager_delegate_impl.h"
@@ -129,6 +130,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/services/display_power_service_provider.h"
 #include "chromeos/dbus/services/liveness_service_provider.h"
 #include "chromeos/dbus/services/proxy_resolution_service_provider.h"
+#include "chromeos/dbus/services/virtual_file_request_service_provider.h"
 #include "chromeos/dbus/session_manager_client.h"
 #include "chromeos/disks/disk_mount_manager.h"
 #include "chromeos/login/login_state.h"
@@ -379,6 +381,14 @@ class DBusServices {
             base::MakeUnique<LivenessServiceProvider>(
                 kLivenessServiceInterface)));
 
+    virtual_file_request_service_ = CrosDBusService::Create(
+        kVirtualFileRequestServiceName,
+        dbus::ObjectPath(kVirtualFileRequestServicePath),
+        CrosDBusService::CreateServiceProviderList(
+            base::MakeUnique<VirtualFileRequestServiceProvider>(
+                base::MakeUnique<
+                    ChromeVirtualFileRequestServiceProviderDelegate>())));
+
     // Initialize PowerDataCollector after DBusThreadManager is initialized.
     PowerDataCollector::Initialize();
 
@@ -432,6 +442,7 @@ class DBusServices {
     proxy_resolution_service_.reset();
     kiosk_info_service_.reset();
     liveness_service_.reset();
+    virtual_file_request_service_.reset();
     PowerDataCollector::Shutdown();
     PowerPolicyController::Shutdown();
     device::BluetoothAdapterFactory::Shutdown();
@@ -458,6 +469,7 @@ class DBusServices {
   std::unique_ptr<CrosDBusService> proxy_resolution_service_;
   std::unique_ptr<CrosDBusService> kiosk_info_service_;
   std::unique_ptr<CrosDBusService> liveness_service_;
+  std::unique_ptr<CrosDBusService> virtual_file_request_service_;
 
   std::unique_ptr<NetworkConnectDelegateChromeOS> network_connect_delegate_;
 
