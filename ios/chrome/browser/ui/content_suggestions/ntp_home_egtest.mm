4f4d72ba00ea27839fafb380c47f516f31a1e5f5
Yuke Liao <liaoyuke@chromium.org>
2017-09-20 18:07:01 +0000

    Disable a few tests in NTPHomeTestCase
    These tests are flaky on all iOS versions and are failing constantly
    on iOS 11.
    
    TBR=gambard@chromium.org
    
    Bug: 767095
    Change-Id: Iba5d82a2dce53382c1b54dc7c3f6b9f738c4356b
    Reviewed-on: https://chromium-review.googlesource.com/675766
    Reviewed-by: Yuke Liao <liaoyuke@chromium.org>
    Commit-Queue: Yuke Liao <liaoyuke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503197}

--- ios/chrome/browser/ui/content_suggestions/ntp_home_egtest.mm
+++ ios/chrome/browser/ui/content_suggestions/ntp_home_egtest.mm
@@ -269,6 +269,9 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
 
 // Tests that the promo is correctly displayed and removed once tapped.
 - (void)testPromoTap {
+  // TODO(crbug.com/767095): Enable this test.
+  EARL_GREY_TEST_DISABLED(@"Test disabled.");
+
   // Setup the promo.
   NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
   [defaults setInteger:experimental_flags::WHATS_NEW_APP_RATING
@@ -300,6 +303,9 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
 // Tests that the position of the collection view is restored when navigating
 // back to the NTP.
 - (void)testPositionRestored {
+  // TODO(crbug.com/767095): Enable this test.
+  EARL_GREY_TEST_DISABLED(@"Test disabled.");
+
   [self addMostVisitedTile];
 
   // Add suggestions to be able to scroll on iPad.
@@ -337,6 +343,9 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
 // and moved up, the scroll position restored is the position before the omnibox
 // is selected.
 - (void)testPositionRestoredWithOmniboxFocused {
+  // TODO(crbug.com/767095): Enable this test.
+  EARL_GREY_TEST_DISABLED(@"Test disabled.");
+
   [self addMostVisitedTile];
 
   // Add suggestions to be able to scroll on iPad.
@@ -377,6 +386,9 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
 
 // Tests that tapping the fake omnibox focuses the real omnibox.
 - (void)testTapFakeOmnibox {
+  // TODO(crbug.com/767095): Enable this test.
+  EARL_GREY_TEST_DISABLED(@"Test disabled.");
+
   // Setup the server.
   self.testServer->RegisterRequestHandler(base::Bind(&StandardResponse));
   GREYAssertTrue(self.testServer->Start(), @"Test server failed to start.");
@@ -394,6 +406,9 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
 
 // Tests that tapping the fake omnibox moves the collection.
 - (void)testTapFakeOmniboxScroll {
+  // TODO(crbug.com/767095): Enable this test.
+  EARL_GREY_TEST_DISABLED(@"Test disabled.");
+
   // Get the collection and its layout.
   UIView* collection = ntp_home::CollectionView();
   GREYAssertTrue([collection isKindOfClass:[UICollectionView class]],
@@ -449,6 +464,9 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
 // Tests that tapping the fake omnibox then unfocusing it moves the collection
 // back to where it was.
 - (void)testTapFakeOmniboxScrollScrolled {
+  // TODO(crbug.com/767095): Enable this test.
+  EARL_GREY_TEST_DISABLED(@"Test disabled.");
+
   // Get the collection and its layout.
   UIView* collection = ntp_home::CollectionView();
   GREYAssertTrue([collection isKindOfClass:[UICollectionView class]],
