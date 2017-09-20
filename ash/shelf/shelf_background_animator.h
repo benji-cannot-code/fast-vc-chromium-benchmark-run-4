ffaad206afc449f3750f2fba61e63bda916421b4
Wenzhao Zang <wzang@chromium.org>
2017-09-20 00:42:32 +0000

    Add fade-in animation to make shelf visible on lock screen
    The views-based shelf is hidden behind flag. The fade-in animation
    is a no-op if the flag is not added because the shelf is in
    non_lock_screen_containers without the flag. (Follow-up to CL 583942.)
    
    Bug: 701157, 760775
    Change-Id: I14421b22c21361a7ff97e9c0e50a23ff0dd6fa45
    Reviewed-on: https://chromium-review.googlesource.com/662417
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503008}

--- ash/shelf/shelf_background_animator.h
+++ ash/shelf/shelf_background_animator.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/public/cpp/shelf_types.h"
+#include "ash/session/session_observer.h"
 #include "ash/shelf/shelf_observer.h"
 #include "ash/wallpaper/wallpaper_controller_observer.h"
 #include "base/macros.h"
@@ -43,7 +44,8 @@ class WallpaperController;
 //       states.
 class ASH_EXPORT ShelfBackgroundAnimator : public ShelfObserver,
                                            public gfx::AnimationDelegate,
-                                           public WallpaperControllerObserver {
+                                           public WallpaperControllerObserver,
+                                           public SessionObserver {
  public:
   // The maximum alpha value that can be used.
   static const int kMaxAlpha = SK_AlphaOPAQUE;
@@ -92,6 +94,9 @@ class ASH_EXPORT ShelfBackgroundAnimator : public ShelfObserver,
   // WallpaperControllerObserver:
   void OnWallpaperDataChanged() override;
   void OnWallpaperColorsChanged() override;
+
+  // SessionObserver:
+  void OnSessionStateChanged(session_manager::SessionState state) override;
 
  private:
   friend class ShelfBackgroundAnimatorTestApi;
