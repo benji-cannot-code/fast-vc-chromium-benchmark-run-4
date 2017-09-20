5cd91cf5118f3e6fb35b0a42a0e89e7d5d28a841
Jacob Dufault <jdufault@google.com>
2017-09-20 16:33:38 +0000

    cros: Fix crash in the note taking app observer in stylus settings page.
    I suspect it is caused from a race between closing the settings window
    and Android apps being refreshed/delivered to the stylus handler, but
    was unable to reproduce.
    
    Bug: 766781
    Change-Id: Ibfa96757abf94b98c8c8367a000537155a7ced05
    Reviewed-on: https://chromium-review.googlesource.com/673748
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Jacob Dufault <jdufault@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503176}

--- chrome/browser/ui/webui/settings/chromeos/device_stylus_handler.h
+++ chrome/browser/ui/webui/settings/chromeos/device_stylus_handler.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/macros.h"
+#include "base/scoped_observer.h"
 #include "chrome/browser/chromeos/note_taking_helper.h"
 #include "chrome/browser/ui/webui/settings/settings_page_ui_handler.h"
 #include "ui/events/devices/input_device_event_observer.h"
@@ -18,12 +19,16 @@ namespace base {
 class ListValue;
 }
 
+namespace ui {
+class InputDeviceManager;
+}
+
 namespace chromeos {
 namespace settings {
 
 // Chrome OS stylus settings handler.
 class StylusHandler : public ::settings::SettingsPageUIHandler,
-                      public chromeos::NoteTakingHelper::Observer,
+                      public NoteTakingHelper::Observer,
                       public ui::InputDeviceEventObserver {
  public:
   StylusHandler();
@@ -31,8 +36,8 @@ class StylusHandler : public ::settings::SettingsPageUIHandler,
 
   // SettingsPageUIHandler implementation.
   void RegisterMessages() override;
-  void OnJavascriptAllowed() override {}
-  void OnJavascriptDisallowed() override {}
+  void OnJavascriptAllowed() override;
+  void OnJavascriptDisallowed() override;
 
   // chromeos::NoteTakingHelper::Observer implementation.
   void OnAvailableNoteTakingAppsUpdated() override;
@@ -58,6 +63,11 @@ class StylusHandler : public ::settings::SettingsPageUIHandler,
 
   // IDs of available note-taking apps.
   std::set<std::string> note_taking_app_ids_;
+
+  // Observer registration.
+  ScopedObserver<NoteTakingHelper, NoteTakingHelper::Observer> note_observer_;
+  ScopedObserver<ui::InputDeviceManager, ui::InputDeviceEventObserver>
+      input_observer_;
 
   DISALLOW_COPY_AND_ASSIGN(StylusHandler);
 };
