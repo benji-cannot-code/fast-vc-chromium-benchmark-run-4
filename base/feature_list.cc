4be871e6f9c542303ae02aadaaacc10ab9d79673
Sigurdur Asgeirsson <siggi@chromium.org>
2017-09-20 19:10:29 +0000

    Give kSyzyAsanDCheckIsFatalFeature unique linkage.
    This is exploding in CheckFeatureIdentity on second use of the constexpr,
    as presumably the Feature object is instantiated per call.
    
    Bug: 763010
    Change-Id: I330690fcf052a66ed3287f3a3134866025803a01
    Reviewed-on: https://chromium-review.googlesource.com/674096
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503224}

--- base/feature_list.cc
+++ base/feature_list.cc
@@ -77,6 +77,11 @@ bool IsValidFeatureOrFieldTrialName(const std::string& name) {
 
 }  // namespace
 
+#if DCHECK_IS_ON() && defined(SYZYASAN)
+const Feature kSyzyAsanDCheckIsFatalFeature{"DcheckIsFatal",
+                                            base::FEATURE_DISABLED_BY_DEFAULT};
+#endif  // defined(SYZYASAN)
+
 FeatureList::FeatureList() {}
 
 FeatureList::~FeatureList() {}
