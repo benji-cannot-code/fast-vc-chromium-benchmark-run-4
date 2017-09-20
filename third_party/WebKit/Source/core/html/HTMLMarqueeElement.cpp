b8468bc3ce8d6bc7d25766adb86c8e99b30f6eb0
Rob Buis <rob.buis@samsung.com>
2017-09-20 12:28:37 +0000

    Changed width and height to unsigned long for HTMLMarqueeElement
    HTMLMarqueeElement should have scrollAmount and scrollDelay as unsigned long as
    per the specification [1]. It has been updated to match this behavior.
    
    Behavior matches Safari.
    
    [1] https://html.spec.whatwg.org/multipage/obsolete.html#the-marquee-element
    
    Bug: 496400
    Change-Id: I274e26ea2a88dd2cd0fd8a7c6a0e1813cfd43792
    Reviewed-on: https://chromium-review.googlesource.com/667928
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Rob Buis <rob.buis@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#503118}

--- third_party/WebKit/Source/core/html/HTMLMarqueeElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLMarqueeElement.cpp
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/HTMLContentElement.h"
 #include "core/html/HTMLDivElement.h"
 #include "core/html/HTMLStyleElement.h"
+#include "core/html/parser/HTMLParserIdioms.h"
 #include "platform/wtf/Noncopyable.h"
 
 namespace blink {
@@ -145,42 +146,32 @@ bool HTMLMarqueeElement::IsHorizontal() const {
   return direction != kUp && direction != kDown;
 }
 
-int HTMLMarqueeElement::scrollAmount() const {
-  bool ok;
-  int scroll_amount = FastGetAttribute(HTMLNames::scrollamountAttr).ToInt(&ok);
-  if (!ok || scroll_amount < 0)
+unsigned HTMLMarqueeElement::scrollAmount() const {
+  unsigned scroll_amount = 0;
+  AtomicString value = FastGetAttribute(HTMLNames::scrollamountAttr);
+  if (value.IsEmpty() || !ParseHTMLNonNegativeInteger(value, scroll_amount) ||
+      scroll_amount > 0x7fffffffu)
     return kDefaultScrollAmount;
   return scroll_amount;
 }
 
-void HTMLMarqueeElement::setScrollAmount(int value,
-                                         ExceptionState& exception_state) {
-  if (value < 0) {
-    exception_state.ThrowDOMException(
-        kIndexSizeError,
-        "The provided value (" + String::Number(value) + ") is negative.");
-    return;
-  }
-  SetIntegralAttribute(HTMLNames::scrollamountAttr, value);
+void HTMLMarqueeElement::setScrollAmount(unsigned value) {
+  SetUnsignedIntegralAttribute(HTMLNames::scrollamountAttr, value,
+                               kDefaultScrollAmount);
 }
 
-int HTMLMarqueeElement::scrollDelay() const {
-  bool ok;
-  int scroll_delay = FastGetAttribute(HTMLNames::scrolldelayAttr).ToInt(&ok);
-  if (!ok || scroll_delay < 0)
+unsigned HTMLMarqueeElement::scrollDelay() const {
+  unsigned scroll_delay = 0;
+  AtomicString value = FastGetAttribute(HTMLNames::scrolldelayAttr);
+  if (value.IsEmpty() || !ParseHTMLNonNegativeInteger(value, scroll_delay) ||
+      scroll_delay > 0x7fffffffu)
     return kDefaultScrollDelayMS;
   return scroll_delay;
 }
 
-void HTMLMarqueeElement::setScrollDelay(int value,
-                                        ExceptionState& exception_state) {
-  if (value < 0) {
-    exception_state.ThrowDOMException(
-        kIndexSizeError,
-        "The provided value (" + String::Number(value) + ") is negative.");
-    return;
-  }
-  SetIntegralAttribute(HTMLNames::scrolldelayAttr, value);
+void HTMLMarqueeElement::setScrollDelay(unsigned value) {
+  SetUnsignedIntegralAttribute(HTMLNames::scrolldelayAttr, value,
+                               kDefaultScrollDelayMS);
 }
 
 int HTMLMarqueeElement::loop() const {
