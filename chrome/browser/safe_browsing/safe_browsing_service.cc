c7385579fc908ce3b99a336ab03b91da872438a8
proberge <proberge@chromium.org>
2017-09-20 15:27:13 +0000

    Clean up suspicious module incident code from /incident_reporting
    Bug: 760592
    Change-Id: I957d9e23adc34e1f8b8a9974d838cb6f3c897875
    Reviewed-on: https://chromium-review.googlesource.com/656086
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: proberge <proberge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503148}

--- chrome/browser/safe_browsing/safe_browsing_service.cc
+++ chrome/browser/safe_browsing/safe_browsing_service.cc
@@ -67,7 +67,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/safe_browsing/incident_reporting/binary_integrity_analyzer.h"
 #include "chrome/browser/safe_browsing/incident_reporting/blacklist_load_analyzer.h"
 #include "chrome/browser/safe_browsing/incident_reporting/incident_reporting_service.h"
-#include "chrome/browser/safe_browsing/incident_reporting/module_load_analyzer.h"
 #include "chrome/browser/safe_browsing/incident_reporting/resource_request_detector.h"
 #include "chrome/browser/safe_browsing/incident_reporting/variations_seed_signature_analyzer.h"
 #include "chrome/browser/safe_browsing/protocol_manager.h"
