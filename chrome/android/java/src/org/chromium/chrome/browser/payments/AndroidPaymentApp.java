b390d4572f4025dfdb0bb8fdc521e9af37b662cd
Rouslan Solomakhin <rouslan@chromium.org>
2017-09-20 15:51:43 +0000

    [Payments] Improve privacy in 3rd party payment app integration.
    Although the spec has been improved for privacy, the old fields have
    been deprecated, but not removed. In particular, Chrome sends the
    deprecated "displayItems" field to the payment app. In order to improve
    user privacy without breaking old integrators, the "displayItems" field
    should be empty.
    
    In addition, https://w3c.github.io/payment-handler/ does not send the
    label of the total parameter to the payment app, so Chrome should remove
    it as well.
    
    Spec:
    https://docs.google.com/document/d/1izV4uC-tiRJG3JLooqY3YRLU22tYOsLTNq0P_InPJeE/edit
    
    Bug: 767033
    Change-Id: I135e4292828e5fca236b9e7b610edac5d260258e
    Reviewed-on: https://chromium-review.googlesource.com/675046
    Reviewed-by: Ganggui Tang <gogerald@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503161}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/AndroidPaymentApp.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/AndroidPaymentApp.java
@@ -406,7 +406,7 @@ public class AndroidPaymentApp
 
         extras.putParcelable(EXTRA_DEPRECATED_DATA_MAP, methodDataBundle);
 
-        String details = serializeDetails(total, displayItems);
+        String details = deprecatedSerializeDetails(total, displayItems);
         extras.putString(EXTRA_DEPRECATED_DETAILS, details == null ? EMPTY_JSON_DATA : details);
 
         return extras;
@@ -426,7 +426,7 @@ public class AndroidPaymentApp
         mHandler.post(() -> mInstrumentDetailsCallback.onInstrumentDetailsError());
     }
 
-    private static String serializeDetails(
+    private static String deprecatedSerializeDetails(
             @Nullable PaymentItem total, @Nullable List<PaymentItem> displayItems) {
         StringWriter stringWriter = new StringWriter();
         JsonWriter json = new JsonWriter(stringWriter);
@@ -437,16 +437,14 @@ public class AndroidPaymentApp
             if (total != null) {
                 // total {{{
                 json.name("total");
-                serializePaymentItem(total, json);
+                serializeTotal(total, json);
                 // }}} total
             }
 
             // displayitems {{{
             if (displayItems != null) {
                 json.name("displayItems").beginArray();
-                for (int i = 0; i < displayItems.size(); i++) {
-                    serializePaymentItem(displayItems.get(i), json);
-                }
+                // Do not pass any display items to the payment app.
                 json.endArray();
             }
             // }}} displayItems
@@ -476,10 +474,14 @@ public class AndroidPaymentApp
         return stringWriter.toString();
     }
 
-    private static void serializePaymentItem(PaymentItem item, JsonWriter json) throws IOException {
+    private static void serializeTotal(PaymentItem item, JsonWriter json)
+            throws IOException {
         // item {{{
         json.beginObject();
-        json.name("label").value(item.label);
+        // Sanitize the total name, because the payment app does not need it to complete the
+        // transaction. Matches the behavior of:
+        // https://w3c.github.io/payment-handler/#total-attribute
+        json.name("label").value("");
 
         // amount {{{
         json.name("amount").beginObject();
@@ -515,7 +517,7 @@ public class AndroidPaymentApp
         // total {{{
         if (modifier.total != null) {
             json.name("total");
-            serializePaymentItem(modifier.total, json);
+            serializeTotal(modifier.total, json);
         } else {
             json.name("total").nullValue();
         }
