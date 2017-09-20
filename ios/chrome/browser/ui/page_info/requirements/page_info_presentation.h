bc2c63ceb77f210defec57e118e8e25102c0f133
Gregory Chatzinoff <gchatz@chromium.org>
2017-09-20 20:06:39 +0000

    Remove use of parent view from PageInfoVC.
    The PageInfoViewController class used to add a subview directly to its
    parent view. This CL moves that logic to the presentation provider, so
    that the parent view does not need to be passed to the Page Info
    component.
    
    Bug: none
    Change-Id: I150cae3928adebcb378c7d7ed968a59885ca9401
    Reviewed-on: https://chromium-review.googlesource.com/673873
    Commit-Queue: Gregory Chatzinoff <gchatz@chromium.org>
    Reviewed-by: Rohit Rao (ping after 24h) <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503230}

--- ios/chrome/browser/ui/page_info/requirements/page_info_presentation.h
+++ ios/chrome/browser/ui/page_info/requirements/page_info_presentation.h
@@ -6,17 +6,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_UI_PAGE_INFO_REQUIREMENTS_PAGE_INFO_PRESENTATION_H_
 #define IOS_CHROME_BROWSER_UI_PAGE_INFO_REQUIREMENTS_PAGE_INFO_PRESENTATION_H_
 
+#import <CoreGraphics/CoreGraphics.h>
+
 @class UIView;
 
 // PageInfoPresentation contains methods related to the presentation of the Page
 // Info UI.
 @protocol PageInfoPresentation
 
-// The view in which the Page Info UI should be presented.
-- (UIView*)viewForPageInfoPresentation;
+// Presents the |pageInfoView| for the Page Info UI. Implementors must ensure
+// that |pageInfoView| is the appropriate size for presentation.
+- (void)presentPageInfoView:(UIView*)pageInfoView;
 
 // Called before the Page Info UI is presented.
 - (void)prepareForPageInfoPresentation;
+
+// Converts |origin| to the coordinate system used for presenting the Page Info
+// UI. |origin| should be in window coordinates.
+- (CGPoint)convertToPresentationCoordinatesForOrigin:(CGPoint)origin;
 
 @end
 
