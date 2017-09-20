d46775b8481a8e80b99263057c2bfb78a1f5bda8
Kenneth Russell <kbr@chromium.org>
2017-09-20 04:09:51 +0000

    Generate new reference images for antialiased WebGL pixel tests.
    It looks like there are stale reference images in cloud storage for
    the following three tests:
    
    Pixel_2DCanvasWebGL
    Pixel_WebGLGreenTriangle_AA_Alpha
    Pixel_WebGLGreenTriangle_AA_NoAlpha
    
    BUG=730303
    TBR=zmo@chromium.org
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ifb6f397799278083c19388e04d8bfce7da557b15
    Reviewed-on: https://chromium-review.googlesource.com/674061
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503043}

--- content/test/gpu/gpu_tests/pixel_test_pages.py
+++ content/test/gpu/gpu_tests/pixel_test_pages.py
@@ -71,7 +71,7 @@ def DefaultPages(base_name):
       'pixel_webgl_aa_alpha.html',
       base_name + '_WebGLGreenTriangle_AA_Alpha',
       test_rect=[0, 0, 300, 300],
-      revision=2),
+      revision=3),
 
     PixelTestPage(
       'pixel_webgl_noaa_alpha.html',
@@ -83,7 +83,7 @@ def DefaultPages(base_name):
       'pixel_webgl_aa_noalpha.html',
       base_name + '_WebGLGreenTriangle_AA_NoAlpha',
       test_rect=[0, 0, 300, 300],
-      revision=2),
+      revision=3),
 
     PixelTestPage(
       'pixel_webgl_noaa_noalpha.html',
@@ -130,7 +130,7 @@ def DefaultPages(base_name):
       'pixel_canvas2d_webgl.html',
       base_name + '_2DCanvasWebGL',
       test_rect=[0, 0, 300, 300],
-      revision=4),
+      revision=5),
 
     PixelTestPage(
       'pixel_background.html',
