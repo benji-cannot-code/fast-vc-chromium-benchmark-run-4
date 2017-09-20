0839559bf431697eb630bb6f67037cff911e8456
Alexei Filippov <alph@chromium.org>
2017-09-20 02:26:20 +0000

    DevTools: make sure there's a single cpu throttler thread.
    BUG=765074
    
    Change-Id: Ifde666f07b568d3e87852b10a6cab8fc7372bce8
    Reviewed-on: https://chromium-review.googlesource.com/669902
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503029}

--- content/renderer/devtools/devtools_cpu_throttler.cc
+++ content/renderer/devtools/devtools_cpu_throttler.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/atomicops.h"
 #include "base/macros.h"
+#include "base/memory/singleton.h"
 #include "base/synchronization/cancellation_flag.h"
 #include "base/threading/platform_thread.h"
 #include "build/build_config.h"
@@ -187,9 +188,8 @@ void CPUThrottlingThread::Stop() {
 #endif
 }
 
-DevToolsCPUThrottler::DevToolsCPUThrottler() {}
-
-DevToolsCPUThrottler::~DevToolsCPUThrottler() {}
+DevToolsCPUThrottler::DevToolsCPUThrottler() = default;
+DevToolsCPUThrottler::~DevToolsCPUThrottler() = default;
 
 void DevToolsCPUThrottler::SetThrottlingRate(double rate) {
   if (rate <= 1) {
@@ -203,6 +203,11 @@ void DevToolsCPUThrottler::SetThrottlingRate(double rate) {
   } else {
     throttling_thread_.reset(new CPUThrottlingThread(rate));
   }
+}
+
+// static
+DevToolsCPUThrottler* DevToolsCPUThrottler::GetInstance() {
+  return base::Singleton<DevToolsCPUThrottler>::get();
 }
 
 }  // namespace content
