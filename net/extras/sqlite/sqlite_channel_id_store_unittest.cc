2359093bafd8591f0badb3618128b1eb07c05c3d
David Benjamin <davidben@chromium.org>
2017-09-21 16:55:36 +0000

    Remove unnecessary SPKI parameter to CreateFromEncryptedPrivateKeyInfo.
    This hasn't been used for a while. We should remove this function
    altogether, but components/gcm_driver hasn't switched to the more
    efficient format yet. Meanwhile, we can still simplify some stuff.
    
    This removes all uses of public_key_x509 in components/gcm_crypto, but
    it does not stop computing it. That will be done in a follow-up CL.
    
    Bug: 667060
    Change-Id: I6d5f952c4fd822152f0aa142bafe59be933e393f
    Reviewed-on: https://chromium-review.googlesource.com/667060
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Nick Harper <nharper@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503473}

--- net/extras/sqlite/sqlite_channel_id_store_unittest.cc
+++ net/extras/sqlite/sqlite_channel_id_store_unittest.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "crypto/ec_private_key.h"
-#include "net/cert/asn1_util.h"
 #include "net/ssl/channel_id_service.h"
 #include "net/ssl/ssl_client_cert_type.h"
 #include "net/test/cert_test_util.h"
