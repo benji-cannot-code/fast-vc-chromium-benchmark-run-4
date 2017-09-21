4199639d16e5193f6dafa0aac30621e6faed6398
Joshua Bell <jsbell@chromium.org>
2017-09-21 20:46:40 +0000

    Rebaseline bindings tests
    Via: run-bindings-tests --reset-results
    
    Following e92361313635c5d5f29ed29b053442c9be8be6ec the test interface
    that inherits from Document pulls in a new header file.
    
    TBR=mkwst@chromium.org
    NOTRY=true
    
    Change-Id: I3d05d69c88590e0561e269e9a018b5322f197be0
    Reviewed-on: https://chromium-review.googlesource.com/677129
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Commit-Queue: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503553}

--- third_party/WebKit/Source/bindings/tests/results/core/V8TestInterfaceDocument.h
+++ third_party/WebKit/Source/bindings/tests/results/core/V8TestInterfaceDocument.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/ToV8ForCore.h"
 #include "bindings/core/v8/V8BindingForCore.h"
 #include "bindings/core/v8/V8Document.h"
+#include "bindings/core/v8/string_or_trusted_url.h"
 #include "bindings/tests/idls/core/TestInterfaceDocument.h"
 #include "core/CoreExport.h"
 #include "platform/bindings/ScriptWrappable.h"
