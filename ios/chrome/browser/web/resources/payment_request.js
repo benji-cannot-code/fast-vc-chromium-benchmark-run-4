12f30e93a44215383eb79f57d3d6ea6f2451ad90
Ravi Nanjundappa <nravi.n@samsung.com>
2017-09-21 04:31:37 +0000

    Correct the order of PaymentComplete enum members
    Order of  PaymentComplete enum members are not matching with that of the specs
    mentioned in "https://w3c.github.io/payment-request/#paymentcomplete-enum"
    This CL fixes this issue.
    
    
    Change-Id: Idb203998ae3537f2e86fcba5d902a6cf9f5ae8e1
    Reviewed-on: https://chromium-review.googlesource.com/655197
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Jinho Bang <jinho.bang@samsung.com>
    Commit-Queue: SUCHIT AGRAWAL <a.suchit@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503351}

--- ios/chrome/browser/web/resources/payment_request.js
+++ ios/chrome/browser/web/resources/payment_request.js
@@ -1418,8 +1418,8 @@ PaymentResponse.prototype.payerPhone = null;
  * @enum {string}
  */
 var PaymentComplete = {
-  SUCCESS: 'success',
   FAIL: 'fail',
+  SUCCESS: 'success',
   UNKNOWN: 'unknown'
 };
 
