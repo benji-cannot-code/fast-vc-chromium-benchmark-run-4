65efddf767930d49825f0f019c23b11f424070e2
Peter K. Lee <pkl@chromium.org>
2017-09-21 16:10:28 +0000

    Annotate calls to -loadModel from initializer with TODO
    Initializer should not call instance methods.
    
    Bug: 764578
    Change-Id: Ideba5fac5df83bc1d22c1b3f3d77b0508e179cab
    Reviewed-on: https://chromium-review.googlesource.com/676685
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503459}

--- ios/chrome/browser/ui/settings/block_popups_collection_view_controller.mm
+++ ios/chrome/browser/ui/settings/block_popups_collection_view_controller.mm
@@ -84,6 +84,8 @@ typedef NS_ENUM(NSInteger, ItemType) {
     self.collectionViewAccessibilityIdentifier =
         @"block_popups_settings_view_controller";
 
+    // TODO(crbug.com/764578): Instance methods should not be called from
+    // initializer.
     [self populateExceptionsList];
     [self updateEditButton];
     [self loadModel];
