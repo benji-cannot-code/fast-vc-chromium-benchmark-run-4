e285b85aad1ba6c05d15c21e2128ae6968ab992a
Kouhei Ueno <kouhei@chromium.org>
2017-09-21 05:01:58 +0000

    Dedupe: use "WebScriptSource::operator ScriptSourceCode()" where possible
    This CL dedupes custom WebScriptSource -> ScriptSourceCode conversion and
    make them all use "WebScriptSource::operator ScriptSourceCode()".
    
    Bug: 711706
    Change-Id: Ifef70136505d5aba2a10ae66814a2ab90085922d
    Reviewed-on: https://chromium-review.googlesource.com/676703
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503355}

--- third_party/WebKit/Source/core/frame/WebLocalFrameImpl.cpp
+++ third_party/WebKit/Source/core/frame/WebLocalFrameImpl.cpp
@@ -648,11 +648,8 @@ void WebLocalFrameImpl::DispatchUnloadEvent() {
 
 void WebLocalFrameImpl::ExecuteScript(const WebScriptSource& source) {
   DCHECK(GetFrame());
-  TextPosition position(OrdinalNumber::FromOneBasedInt(source.start_line),
-                        OrdinalNumber::First());
   v8::HandleScope handle_scope(ToIsolate(GetFrame()));
-  GetFrame()->GetScriptController().ExecuteScriptInMainWorld(
-      ScriptSourceCode(source.code, source.url, position));
+  GetFrame()->GetScriptController().ExecuteScriptInMainWorld(source);
 }
 
 void WebLocalFrameImpl::ExecuteScriptInIsolatedWorld(
@@ -730,12 +727,9 @@ v8::Local<v8::Value> WebLocalFrameImpl::ExecuteScriptAndReturnValue(
     const WebScriptSource& source) {
   DCHECK(GetFrame());
 
-  TextPosition position(OrdinalNumber::FromOneBasedInt(source.start_line),
-                        OrdinalNumber::First());
   return GetFrame()
       ->GetScriptController()
-      .ExecuteScriptInMainWorldAndReturnValue(
-          ScriptSourceCode(source.code, source.url, position));
+      .ExecuteScriptInMainWorldAndReturnValue(source);
 }
 
 void WebLocalFrameImpl::RequestExecuteScriptAndReturnValue(
