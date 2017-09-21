09512a91ba496c5dc03e17864d0865373e19a1e8
Nate Chapin <japhet@chromium.org>
2017-09-21 23:43:56 +0000

    Move NavigationScheduler and WebSocketHandle timers to be per-frame
    Bug: 
    Change-Id: I92558b0137e7fc1159f5f7ed3648312645c283f5
    Reviewed-on: https://chromium-review.googlesource.com/677493
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503611}

--- third_party/WebKit/Source/modules/websockets/DocumentWebSocketChannelTest.cpp
+++ third_party/WebKit/Source/modules/websockets/DocumentWebSocketChannelTest.cpp
@@ -85,13 +85,14 @@ class MockWebSocketHandle : public WebSocketHandle {
     DoInitialize(&websocket);
   }
 
-  MOCK_METHOD6(Connect,
+  MOCK_METHOD7(Connect,
                void(const KURL&,
                     const Vector<String>&,
                     SecurityOrigin*,
                     const KURL&,
                     const String&,
-                    WebSocketHandleClient*));
+                    WebSocketHandleClient*,
+                    WebTaskRunner*));
   MOCK_METHOD4(Send,
                void(bool, WebSocketHandle::MessageType, const char*, size_t));
   MOCK_METHOD1(FlowControl, void(int64_t));
@@ -163,7 +164,7 @@ class DocumentWebSocketChannelTest : public ::testing::Test {
       InSequence s;
       EXPECT_CALL(*Handle(), DoInitialize(_));
       EXPECT_CALL(*Handle(), Connect(KURL(NullURL(), "ws://localhost/"), _, _,
-                                     _, _, HandleClient()));
+                                     _, _, HandleClient(), _));
       EXPECT_CALL(*Handle(), FlowControl(65536));
       EXPECT_CALL(*ChannelClient(), DidConnect(String("a"), String("b")));
     }
@@ -214,7 +215,7 @@ TEST_F(DocumentWebSocketChannelTest, connectSuccess) {
     EXPECT_CALL(*Handle(), DoInitialize(_));
     EXPECT_CALL(*Handle(),
                 Connect(KURLEq("ws://localhost/"), _, _,
-                        KURLEq("http://example.com/"), _, HandleClient()))
+                        KURLEq("http://example.com/"), _, HandleClient(), _))
         .WillOnce(DoAll(SaveArg<1>(&protocols), SaveArg<2>(&origin)));
     EXPECT_CALL(*Handle(), FlowControl(65536));
     EXPECT_CALL(checkpoint, Call(1));
@@ -823,7 +824,7 @@ class DocumentWebSocketChannelHandshakeThrottleTest
   // non-null throttle.
   void NormalHandshakeExpectations() {
     EXPECT_CALL(*Handle(), DoInitialize(_));
-    EXPECT_CALL(*Handle(), Connect(_, _, _, _, _, _));
+    EXPECT_CALL(*Handle(), Connect(_, _, _, _, _, _, _));
     EXPECT_CALL(*Handle(), FlowControl(_));
     EXPECT_CALL(*handshake_throttle_, ThrottleHandshake(_, _, _));
   }
@@ -833,7 +834,7 @@ class DocumentWebSocketChannelHandshakeThrottleTest
 
 TEST_F(DocumentWebSocketChannelHandshakeThrottleTest, ThrottleArguments) {
   EXPECT_CALL(*Handle(), DoInitialize(_));
-  EXPECT_CALL(*Handle(), Connect(_, _, _, _, _, _));
+  EXPECT_CALL(*Handle(), Connect(_, _, _, _, _, _, _));
   EXPECT_CALL(*Handle(), FlowControl(_));
   EXPECT_CALL(*handshake_throttle_,
               ThrottleHandshake(WebURL(url()), _, WebCallbacks()));
