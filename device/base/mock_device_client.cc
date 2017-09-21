e9d5bc1be6567352d1de96c9ceb2537a8b4726e3
Reilly Grant <reillyg@chromium.org>
2017-09-21 00:56:50 +0000

    Remove HID from the device service on Android
    HID is not supported on Android so there's no reason to include all this
    code. To prevent this from regressing an assert() is added to
    device/hid/BUILD.gn which requires some updates to MockDeviceClient to
    completely remove the dependency when building tests.
    
    Bug: 765671
    Change-Id: Ieaab1f28594a7e0cdd177389d1bdb91159e868c9
    Reviewed-on: https://chromium-review.googlesource.com/674050
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503303}

--- device/base/mock_device_client.cc
+++ device/base/mock_device_client.cc
@@ -5,8 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "device/base/mock_device_client.h"
 
-#include "device/hid/mock_hid_service.h"
+#include "base/logging.h"
 #include "device/usb/mock_usb_service.h"
+
+#if !defined(OS_ANDROID)
+#include "device/hid/mock_hid_service.h"
+#endif
 
 namespace device {
 
@@ -15,7 +19,12 @@ MockDeviceClient::MockDeviceClient() {}
 MockDeviceClient::~MockDeviceClient() {}
 
 HidService* MockDeviceClient::GetHidService() {
+#if defined(OS_ANDROID)
+  NOTREACHED();
+  return nullptr;
+#else
   return hid_service();
+#endif
 }
 
 UsbService* MockDeviceClient::GetUsbService() {
@@ -23,9 +32,14 @@ UsbService* MockDeviceClient::GetUsbService() {
 }
 
 MockHidService* MockDeviceClient::hid_service() {
+#if defined(OS_ANDROID)
+  NOTREACHED();
+  return nullptr;
+#else
   if (!hid_service_)
     hid_service_.reset(new MockHidService());
   return hid_service_.get();
+#endif
 }
 
 MockUsbService* MockDeviceClient::usb_service() {
