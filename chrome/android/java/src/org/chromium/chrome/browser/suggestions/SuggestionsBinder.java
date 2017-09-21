8b6b749df7bdc15ffeaaf2760d810d9ee2cf7b3a
Jesse Doherty <jwd@chromium.org>
2017-09-21 17:56:35 +0000

    Revert "Fix suggestions thumbnail and title text sizes."
    This reverts commit 3761901e7e3ed79142dc1a90f2b21a8e54100a1a.
    
    Reason for revert: Causing compile failure: https://build.chromium.org/p/chromium.linux/builders/Android%20Builder/builds/87759
    with error:
    ../../chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsBinder.java:404: error: cannot find symbol         if (FeatureUtilities.isChromeHomeModernEnabled()) {
    
    Original change's description:
    > Fix suggestions thumbnail and title text sizes.
    > 
    > Bug: i761987
    > Change-Id: I34c7e03e549589c6de959de07486371b89d3dbfb
    > Reviewed-on: https://chromium-review.googlesource.com/677287
    > Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    > Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#503488}
    
    TBR=bauerb@chromium.org,mvanouwerkerk@chromium.org
    
    Change-Id: Ib7ec5816963b0cc0a2ab479b4738b469ce4b726b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: i761987
    Reviewed-on: https://chromium-review.googlesource.com/677073
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503499}

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsBinder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsBinder.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.suggestions;
 
 import android.content.res.ColorStateList;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -14,7 +13,6 @@ import android.graphics.drawable.TransitionDrawable;
 import android.media.ThumbnailUtils;
 import android.os.StrictMode;
 import android.os.SystemClock;
-import android.support.annotation.DimenRes;
 import android.support.annotation.Nullable;
 import android.support.v4.text.BidiFormatter;
 import android.text.TextUtils;
@@ -36,7 +34,6 @@ import org.chromium.chrome.browser.download.DownloadUtils;
 import org.chromium.chrome.browser.download.ui.DownloadFilter;
 import org.chromium.chrome.browser.ntp.cards.NewTabPageViewHolder;
 import org.chromium.chrome.browser.ntp.snippets.SnippetArticle;
-import org.chromium.chrome.browser.util.FeatureUtilities;
 import org.chromium.chrome.browser.widget.TintedImageView;
 
 /**
@@ -88,7 +85,11 @@ public class SuggestionsBinder {
         mPublisherBar = mCardContainerView.findViewById(R.id.publisher_bar);
         mOfflineBadge = mCardContainerView.findViewById(R.id.offline_icon);
 
-        mThumbnailSize = getThumbnailSize(mCardContainerView.getResources());
+        boolean useLargeThumbnailLayout =
+                ChromeFeatureList.isEnabled(ChromeFeatureList.CONTENT_SUGGESTIONS_LARGE_THUMBNAIL);
+        mThumbnailSize = mCardContainerView.getResources().getDimensionPixelSize(
+                useLargeThumbnailLayout ? R.dimen.snippets_thumbnail_size_large
+                                        : R.dimen.snippets_thumbnail_size);
         mThumbnailFootprintPx = mThumbnailSize
                 + mCardContainerView.getResources().getDimensionPixelSize(
                           R.dimen.snippets_thumbnail_margin);
@@ -397,19 +398,5 @@ public class SuggestionsBinder {
     private void verifyBitmap(Bitmap bitmap) {
         assert !bitmap.isRecycled();
         assert bitmap.getWidth() <= mThumbnailSize || bitmap.getHeight() <= mThumbnailSize;
-    }
-
-    private static int getThumbnailSize(Resources resources) {
-        @DimenRes
-        final int dimension;
-        if (FeatureUtilities.isChromeHomeModernEnabled()) {
-            dimension = R.dimen.snippets_thumbnail_size_modern;
-        } else if (ChromeFeatureList.isEnabled(
-                           ChromeFeatureList.CONTENT_SUGGESTIONS_LARGE_THUMBNAIL)) {
-            dimension = R.dimen.snippets_thumbnail_size_large;
-        } else {
-            dimension = R.dimen.snippets_thumbnail_size;
-        }
-        return resources.getDimensionPixelSize(dimension);
     }
 }
