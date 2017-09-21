57473fa8360d636d8b010c2ee3d68f983842e369
Yuki Yamada <yukiy@google.com>
2017-09-21 10:29:52 +0000

    Wait service worker registration at RunPageInteractions
    Wait service worker registration to be completed at RunPageInteractions.
    tab.IsServiceWorkerActivatedOrNotRegistered returns true if service worker
    registration is finished or service worker is not registered, so
    RunPageInteractions waits this to be true.
    
    Design doc of perf benchmark for PWA:
    https://docs.google.com/document/d/1Nf97CVp1X7aSqvAspyJ7yOCDyr1osUNrnfrGwZ_Yuuo/edit?usp=sharing
    
    Bug: 736697
    Change-Id: I507b0bdf76bdf460841348136dc86db0f32aeab7
    Reviewed-on: https://chromium-review.googlesource.com/647128
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Yuki Yamada <yukiy@google.com>
    Cr-Commit-Position: refs/heads/master@{#503410}

--- tools/perf/page_sets/page_cycler_story.py
+++ tools/perf/page_sets/page_cycler_story.py
@@ -3,7 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from telemetry.core import util
+import py_utils
+
 from telemetry.page import page
 from telemetry.page import cache_temperature as cache_temperature_module
 from telemetry.page import shared_page_state
@@ -30,5 +31,7 @@ class PageCyclerStory(page.Page):
                            timeout_in_seconds=_NAVIGATION_TIMEOUT)
 
   def RunPageInteractions(self, action_runner):
-    util.WaitFor(action_runner.tab.HasReachedQuiescence,
-        _WEB_CONTENTS_TIMEOUT)
+    py_utils.WaitFor(action_runner.tab.HasReachedQuiescence,
+                     _WEB_CONTENTS_TIMEOUT)
+    py_utils.WaitFor(action_runner.tab.IsServiceWorkerActivatedOrNotRegistered,
+                     _WEB_CONTENTS_TIMEOUT)
