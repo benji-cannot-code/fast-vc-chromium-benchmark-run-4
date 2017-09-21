db04dd56e2d069a741bbe056115b245b78112c8d
Lei Zhang <thestig@chromium.org>
2017-09-21 19:04:53 +0000

    Cleanup nits in vr_usage_monitor.cc.
    Change-Id: I04d4783fa93540d964721ac4973d186f1399a9bc
    Reviewed-on: https://chromium-review.googlesource.com/645148
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503526}

--- chrome/browser/android/vr_shell/vr_shell_delegate.cc
+++ chrome/browser/android/vr_shell/vr_shell_delegate.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_widget_host.h"
 #include "content/public/browser/render_widget_host_view.h"
+#include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/common/origin_util.h"
 #include "device/vr/android/gvr/gvr_delegate.h"
