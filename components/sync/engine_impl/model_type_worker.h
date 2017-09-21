74b54dd065a0e411746899830fc6a32ab50cd986
Sky Malice <skym@chromium.org>
2017-09-21 22:13:10 +0000

    [Sync] Don't DCHECK on version for commit only types.
    To save bandwidth for commit only types we strip off various peices of
    metadata on commits. This includes versions. The previous contract was
    that version should always increase by at least 1, which the server
    respects as best it can, but to mechanisms that are tracking the
    version before it is stripped, things seem weird.
    
    The solution taken here was to remove the checking logic when we know
    the verison is going to be meaningless.
    
    Bug: 764103
    Change-Id: Ia41a42ce0c17985a3dedad43e059e51313a1f316
    Reviewed-on: https://chromium-review.googlesource.com/677624
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503580}

--- components/sync/engine_impl/model_type_worker.h
+++ components/sync/engine_impl/model_type_worker.h
@@ -144,7 +144,7 @@ class ModelTypeWorker : public UpdateHandler,
 
   // Updates the encryption key name stored in |model_type_state_| if it differs
   // from the default encryption key name in |cryptographer_|. Returns whether
-  // an update occured.
+  // an update occurred.
   bool UpdateEncryptionKeyName();
 
   // Iterates through all elements in |entities_| and tries to decrypt anything
@@ -196,7 +196,7 @@ class ModelTypeWorker : public UpdateHandler,
   //
   // When commits are pending, their information is stored here. This
   // information is dropped from memory when the commit succeeds or gets
-  // cancelled.
+  // canceled.
   //
   // This also stores some information related to received server state in
   // order to implement reflection blocking and conflict detection. This
@@ -210,7 +210,8 @@ class ModelTypeWorker : public UpdateHandler,
   // Whether there are outstanding encrypted updates in |entities_|.
   bool has_encrypted_updates_ = false;
 
-  // Cancelation signal is used to cancel blocking operation on engine shutdown.
+  // Cancellation signal is used to cancel blocking operation on engine
+  // shutdown.
   CancelationSignal* cancelation_signal_;
 
   base::ThreadChecker thread_checker_;
@@ -221,7 +222,7 @@ class ModelTypeWorker : public UpdateHandler,
 
 // GetLocalChangesRequest is a container for GetLocalChanges call response. It
 // allows sync thread to block waiting for model thread to call SetResponse.
-// This class supports cancelling blocking call through CancelationSignal during
+// This class supports canceling blocking call through CancelationSignal during
 // sync engine shutdown.
 //
 // It should be used in the following manner:
@@ -244,7 +245,7 @@ class GetLocalChangesRequest
   void OnSignalReceived() override;
 
   // Blocks current thread until either SetResponse is called or
-  // cancelation_signal_ is signalled.
+  // cancelation_signal_ is signaled.
   void WaitForResponse();
 
   // SetResponse takes ownership of |local_changes| and unblocks WaitForResponse
@@ -252,7 +253,7 @@ class GetLocalChangesRequest
   // GetLocalChanges.
   void SetResponse(CommitRequestDataList&& local_changes);
 
-  // Checks if WaitForResponse was cancelled through CancelationSignal. When
+  // Checks if WaitForResponse was canceled through CancelationSignal. When
   // returns true calling ExtractResponse is unsafe.
   bool WasCancelled();
 
