bacd8880ab9be166b3340350d36492c52b08750c
Hidehiko Abe <hidehiko@chromium.org>
2017-09-21 18:15:00 +0000

    Reland "Migrate Bool(ean)?DBusMethodCallback to DBusMethodCallback<bool>."
    This is a reland of bbebe805f0e24f6cb7dd909559a8a6ed3e94e380
    Original change's description:
    > Migrate Bool(ean)?DBusMethodCallback to DBusMethodCallback<bool>.
    > 
    > Part of OnceCallback migration in chromeos/dbus.
    > 
    > BUG=739622
    > TEST=Ran trybot.
    > 
    > Change-Id: Ic932aa7c018297a064889c4f05c8578bca712474
    > Reviewed-on: https://chromium-review.googlesource.com/666944
    > Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    > Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    > Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    > Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    > Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    > Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    > Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#502831}
    
    Bug: 739622
    Change-Id: Idc0b0659e24ebd96eadaf57f872dea926e2e52ac
    Reviewed-on: https://chromium-review.googlesource.com/672563
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503512}

--- chromeos/tpm/tpm_password_fetcher.h
+++ chromeos/tpm/tpm_password_fetcher.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/optional.h"
 #include "chromeos/chromeos_export.h"
-#include "chromeos/dbus/dbus_method_call_status.h"
 
 namespace chromeos {
 
@@ -38,7 +37,7 @@ class CHROMEOS_EXPORT TpmPasswordFetcher {
 
  private:
   // Used to implement Fetch().
-  void OnTpmIsReady(DBusMethodCallStatus call_status, bool tpm_is_ready);
+  void OnTpmIsReady(base::Optional<bool> tpm_is_ready);
 
   // Used to implement Fetch().
   void OnTpmGetPassword(base::Optional<std::string> password);
