c1fcc469fbbce2f9475b8541e58fb2c160082f25
edchin <edchin@chromium.org>
2017-09-21 22:30:22 +0000

    Revert "[Payment Request] Adds a navigation bar to the country picker."
    This reverts commit 70e88907ec18a76cca76f39e96612c8b13d9720d.
    
    Reason for revert:
    This fails showcase egtests for 
    SCPaymentsPickerTestCase on iPhone 5/5s iOS 9, 10. 
    Most likely due to smaller form factor needing 
    scrolling to find a view.
    
    
    Original change's description:
    > [Payment Request] Adds a navigation bar to the country picker.
    > 
    > Video of the fix in action:
    > https://drive.google.com/file/d/0B4g_WskzVRJdQUVjLUE2RWRWREE/view?usp=sharing
    > 
    > Bug: 767154
    > Change-Id: I1881976b27ba6aade47d5d06879fb65dcf3d24de
    > Reviewed-on: https://chromium-review.googlesource.com/674667
    > Reviewed-by: edchin <edchin@chromium.org>
    > Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    > Commit-Queue: Marc-Antoine Courteau <macourteau@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#503521}
    
    TBR=macourteau@chromium.org,edchin@chromium.org,mahmadi@chromium.org
    
    Change-Id: I4c64e34558c3d5fe57d522f452a2286e40b1dd6a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 767154
    Reviewed-on: https://chromium-review.googlesource.com/677414
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503584}

--- ios/showcase/payments/sc_payments_picker_egtest.mm
+++ ios/showcase/payments/sc_payments_picker_egtest.mm
@@ -128,11 +128,6 @@ id<GREYMatcher> UIAlertViewMessageForDelegateCallWithArgument(
     EARL_GREY_TEST_DISABLED(@"Test disabled on iOS 11.");
   }
 
-  [[EarlGrey selectElementWithMatcher:
-                 grey_accessibilityID(
-                     kPaymentRequestPickerViewControllerAccessibilityID)]
-      performAction:grey_scrollToContentEdge(kGREYContentEdgeTop)];
-
   // Type 'c' in the search bar.
   [[EarlGrey
       selectElementWithMatcher:
