4541b801cd7111d01f68f7370336b459e52b9d62
sahel <sahel@chromium.org>
2017-09-21 05:43:37 +0000

    Wheel end event must have its own timestamp.
    The wheel end event generated by Mouse_wheel_phase_handler used to have
    the timestamp of the last wheel event. This caused a 100ms+ latency to
    handling this event since it is released 100ms after the last wheel
    event. This cl adds proper timestamp to the synthetic wheel end event.
    
    Bug: 759458
    Change-Id: Iebfe9638cb401618d178a5b4caa4d4108b1048ca
    Reviewed-on: https://chromium-review.googlesource.com/672166
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Commit-Queue: Sahel Sharifymoghaddam <sahel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503364}

--- content/browser/renderer_host/input/mouse_wheel_phase_handler.cc
+++ content/browser/renderer_host/input/mouse_wheel_phase_handler.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/render_widget_host_impl.h"
 #include "content/browser/renderer_host/render_widget_host_input_event_router.h"
 #include "content/browser/renderer_host/render_widget_host_view_base.h"
+#include "ui/events/base_event_utils.h"
 
 namespace content {
 MouseWheelPhaseHandler::MouseWheelPhaseHandler(
@@ -71,6 +72,8 @@ void MouseWheelPhaseHandler::SendSyntheticWheelEventWithPhaseEnded(
     bool should_route_event) {
   DCHECK(host_view_->wheel_scroll_latching_enabled());
   blink::WebMouseWheelEvent mouse_wheel_event = last_mouse_wheel_event;
+  mouse_wheel_event.SetTimeStampSeconds(
+      ui::EventTimeStampToSeconds(ui::EventTimeForNow()));
   mouse_wheel_event.delta_x = 0;
   mouse_wheel_event.delta_y = 0;
   mouse_wheel_event.phase = blink::WebMouseWheelEvent::kPhaseEnded;
