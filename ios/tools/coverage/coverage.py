a089fe0c35d0885a982e2dbf4d538c959f868a7b
Yuke Liao <liaoyuke@chromium.org>
2017-09-21 23:18:52 +0000

    [Coverage] Move TODO out of help message.
    There is a TODO in the help message to make the test file identifiers
    configurable, however, this doesn't help users in any way, so move it
    closer to where the variable is defined.
    
    Bug: 
    Change-Id: Id6f3fed4b121e115cfa71f6dc89d576529e0acd0
    Reviewed-on: https://chromium-review.googlesource.com/677054
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Yuke Liao <liaoyuke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503606}

--- ios/tools/coverage/coverage.py
+++ ios/tools/coverage/coverage.py
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   The generated code coverage report excludes test files, and test files are
   identified by postfixes: ['unittest.cc', 'unittest.mm', 'egtest.mm'].
-  TODO(crbug.com/763957): Make test file identifiers configurable.
 
   NOTE: This script must be called from the root of checkout. It may take up to
         a few minutes to generate a report for targets that depend on Chrome,
@@ -60,6 +59,7 @@ EARL_GREY_TEST_TARGET_POSTFIX = 'egtests'
 
 # Used to determine if a file is a test file. The coverage of test files should
 # be excluded from code coverage report.
+# TODO(crbug.com/763957): Make test file identifiers configurable.
 TEST_FILES_POSTFIXES = ['unittest.mm', 'unittest.cc', 'egtest.mm']
 
 
