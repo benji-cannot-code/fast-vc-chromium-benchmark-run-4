99d41f7815c6ebe974aefcb4841dbfee15cf2203
sczs <sczs@chromium.org>
2017-09-21 09:44:33 +0000

    [ios] Disables webUsage when clearing browsing data.
    This is a revert of https://chromium-review.googlesource.com/c/chromium/src/+/653484
    because visited links are not being cleared from browsing history.
    
    Bug: 767213
    Change-Id: I21d9942edc17960e08ca3f2643531a37fc5d2ad8
    Reviewed-on: https://chromium-review.googlesource.com/677191
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503401}

--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -351,6 +351,10 @@ enum class StackViewDismissalMode { NONE, NORMAL, INCOGNITO };
 @property(nonatomic, readwrite)
     NTPTabOpeningPostOpeningAction NTPActionAfterTabSwitcherDismissal;
 
+// Activates browsing and enables web views if |enabled| is YES.
+// Disables browsing and purges web views if |enabled| is NO.
+// Must be called only on the main thread.
+- (void)setWebUsageEnabled:(BOOL)enabled;
 // Activates |mainBVC| and |otrBVC| and sets |currentBVC| as primary iff
 // |currentBVC| can be made active.
 - (void)activateBVCAndMakeCurrentBVCPrimary;
@@ -819,6 +823,16 @@ enum class StackViewDismissalMode { NONE, NORMAL, INCOGNITO };
         [[BrowsingDataRemovalController alloc] initWithDelegate:self];
   }
   return _browsingDataRemovalController;
+}
+
+- (void)setWebUsageEnabled:(BOOL)enabled {
+  DCHECK([NSThread isMainThread]);
+  if (enabled) {
+    [self activateBVCAndMakeCurrentBVCPrimary];
+  } else {
+    [self.mainBVC setActive:NO];
+    [self.otrBVC setActive:NO];
+  }
 }
 
 - (void)activateBVCAndMakeCurrentBVCPrimary {
@@ -1933,8 +1947,13 @@ enum class StackViewDismissalMode { NONE, NORMAL, INCOGNITO };
                                       mask:(int)mask
                                 timePeriod:(browsing_data::TimePeriod)timePeriod
                          completionHandler:(ProceduralBlock)completionHandler {
-
+  // TODO(crbug.com/632772): Remove web usage disabling once
+  // https://bugs.webkit.org/show_bug.cgi?id=149079 has been fixed.
+  if (mask & IOSChromeBrowsingDataRemover::REMOVE_SITE_DATA) {
+    [self setWebUsageEnabled:NO];
+  }
   ProceduralBlock browsingDataRemoved = ^{
+    [self setWebUsageEnabled:YES];
     if (completionHandler) {
       completionHandler();
     }
