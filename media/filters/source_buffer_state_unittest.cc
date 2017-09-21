4a4c523aa1c65562f6c65f68a26dff59a0b1b61f
Matt Wolenetz <wolenetz@chromium.org>
2017-09-21 22:52:27 +0000

    MSE: Don't vary kMseBufferByPts in unit tests unnecessarily
    Since the kMseBufferByPts feature enabled/disabled state is cached by
    ChunkDemuxer's ctor (to coherently configure the MSE portion of all of
    the buffering for that ChunkDemuxer and protect against feature status
    possibly changing during the lifetime of the ChunkDemuxer), various unit
    tests that test layers below (not including ChunkDemuxer) no longer need
    to vary the feature state. They already vary how they configure the
    system under test directly (like how ChunkDemuxer would).
    
    This change removes unnecessary kMseBufferByPts toggling in these lower
    layer tests:
      SourceBufferStateTests
      FrameProcessorTests
      SourceBufferStreamTests
    
    Note: PipelineIntegrationTests and ChunkDemuxerTests must still do
    feature toggling to retain coverage.
    
    BUG=718641
    
    Change-Id: I69e8e59d2b198e38e4f4a7f51ce37b70c862745e
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Reviewed-on: https://chromium-review.googlesource.com/676536
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503593}

--- media/filters/source_buffer_state_unittest.cc
+++ media/filters/source_buffer_state_unittest.cc
@@ -10,9 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/string_number_conversions.h"
-#include "base/test/scoped_feature_list.h"
 #include "media/base/gmock_callback_support.h"
-#include "media/base/media_switches.h"
 #include "media/base/media_util.h"
 #include "media/base/mock_filters.h"
 #include "media/base/mock_media_log.h"
@@ -63,14 +61,6 @@ class SourceBufferStateTest
  public:
   SourceBufferStateTest() : mock_stream_parser_(nullptr) {
     range_api_ = GetParam();
-    switch (range_api_) {
-      case ChunkDemuxerStream::RangeApi::kLegacyByDts:
-        scoped_feature_list_.InitAndDisableFeature(media::kMseBufferByPts);
-        break;
-      case ChunkDemuxerStream::RangeApi::kNewByPts:
-        scoped_feature_list_.InitAndEnableFeature(media::kMseBufferByPts);
-        break;
-    }
   }
 
   std::unique_ptr<SourceBufferState> CreateSourceBufferState() {
@@ -159,7 +149,6 @@ class SourceBufferStateTest
   MockStreamParser* mock_stream_parser_;
   StreamParser::NewConfigCB new_config_cb_;
   ChunkDemuxerStream::RangeApi range_api_;
-  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 TEST_P(SourceBufferStateTest, InitSingleAudioTrack) {
