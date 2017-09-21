67f28aff6f8fa86b150f76ffb7fba52e509d3df8
Jinsuk Kim <jinsukkim@chromium.org>
2017-09-21 22:34:01 +0000

    Convert MediaSessionTest to JUnit4
    - The test needs to run with ContentJUnit4ClassRunner to initialize
      command line switch that allows for programmatic media play as expected.
    - There are SkipChecks that expects CommandLine instance be ready.
      Called CommandLine.init() with an empty value in ContentJUnit4ClassRunner
      before skip check process gets started.
    
    Bug: 640116
    Change-Id: Ic2142a98646ae45d30e37f30633a97267b557b92
    Reviewed-on: https://chromium-review.googlesource.com/671167
    Reviewed-by: Alexandre Elias <aelias@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503588}

--- content/public/test/android/javatests/src/org/chromium/content/browser/test/ContentJUnit4ClassRunner.java
+++ content/public/test/android/javatests/src/org/chromium/content/browser/test/ContentJUnit4ClassRunner.java
@@ -10,6 +10,7 @@ import android.support.test.InstrumentationRegistry;
 import org.junit.runners.model.InitializationError;
 
 import org.chromium.base.CollectionUtil;
+import org.chromium.base.CommandLine;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.BaseTestResult.PreTestHook;
 import org.chromium.base.test.util.CommandLineFlags;
@@ -34,6 +35,7 @@ public class ContentJUnit4ClassRunner extends BaseJUnit4ClassRunner {
     }
 
     private static List<SkipCheck> defaultSkipChecks() {
+        CommandLine.init(null);
         return CollectionUtil.newArrayList(
                 new UiRestrictionSkipCheck(InstrumentationRegistry.getTargetContext()),
                 new UiDisableIfSkipCheck(InstrumentationRegistry.getTargetContext()));
