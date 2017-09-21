7de16b1677acc0b69dd0e536397b882e16e7352c
Kouhei Ueno <kouhei@chromium.org>
2017-09-21 05:13:01 +0000

    Make ScriptReferrerInfo non-optional for 2 overloads of V8ScriptRunner::CompileScript
    ScriptReferrerInfo should be passed explicitly for all invocation of
    V8ScriptRunner::CompileScript.  This CL partially addresses the issue by making
    the ScriptReferrerInfo argument non-optional for its 2 overloads.
    
    A follow-up CL is planned to fix the last overload of the V8ScriptRunner::CompileScript.
    
    Bug: 711706
    Change-Id: Idf883bac5174ad856176b895ca47ae7b9704e964
    Reviewed-on: https://chromium-review.googlesource.com/676603
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503358}

--- third_party/WebKit/Source/bindings/core/v8/V8ScriptRunnerTest.cpp
+++ third_party/WebKit/Source/bindings/core/v8/V8ScriptRunnerTest.cpp
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "bindings/core/v8/V8ScriptRunner.h"
 
+#include "bindings/core/v8/ReferrerScriptInfo.h"
 #include "bindings/core/v8/V8BindingForCore.h"
 #include "bindings/core/v8/V8BindingForTesting.h"
 #include "core/loader/resource/ScriptResource.h"
@@ -64,7 +65,7 @@ class V8ScriptRunnerTest : public ::testing::Test {
                 script_state, V8String(script_state->GetIsolate(), Code()),
                 Filename(), String(), WTF::TextPosition(), resource_.Get(),
                 nullptr, resource_.Get() ? resource_->CacheHandler() : nullptr,
-                kNotSharableCrossOrigin, cache_options)
+                kNotSharableCrossOrigin, cache_options, ReferrerScriptInfo())
                 .IsEmpty();
   }
 
