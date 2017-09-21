3a0316b889cd418056cfbeef3f63541050a67a4f
Nicolas Dossou-gbete <dgn@chromium.org>
2017-09-21 12:43:25 +0000

    🏡 Use all the space for tiles in modern
    In modern, uses MarginResizer to apply similar baseline lateral
    whitespace as content suggestions and spreads equally the rest of the
    space in the intervals between and around the tiles.
    
    Bug: 760806
    Change-Id: I1cd2bce01732a7293657403d403c88597859889d
    Reviewed-on: https://chromium-review.googlesource.com/657100
    Commit-Queue: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503425}

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/TileGridViewHolder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/TileGridViewHolder.java
@@ -5,10 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.suggestions;
 
+import android.content.res.Resources;
 import android.view.ViewGroup;
 
-import java.util.List;
+import org.chromium.chrome.R;
+import org.chromium.chrome.browser.util.FeatureUtilities;
+import org.chromium.chrome.browser.widget.displaystyle.MarginResizer;
+import org.chromium.chrome.browser.widget.displaystyle.UiConfig;
 
+import java.util.List;
 /**
  * A {@link SiteSectionViewHolder} specialised in displaying sites as a simple grid of tiles,
  * through
@@ -16,13 +21,26 @@ import java.util.List;
  */
 public class TileGridViewHolder extends SiteSectionViewHolder {
     private final TileGridLayout mSectionView;
+    private final MarginResizer mMarginResizer;
 
-    public TileGridViewHolder(ViewGroup view, int maxRows, int maxColumns) {
+    public TileGridViewHolder(ViewGroup view, int maxRows, int maxColumns, UiConfig uiConfig) {
         super(view);
 
         mSectionView = (TileGridLayout) itemView;
         mSectionView.setMaxRows(maxRows);
         mSectionView.setMaxColumns(maxColumns);
+
+        if (FeatureUtilities.isChromeHomeModernEnabled()) {
+            Resources res = itemView.getResources();
+            int defaultLateralMargin =
+                    res.getDimensionPixelSize(R.dimen.tile_grid_layout_padding_start);
+            int wideLateralMargin =
+                    res.getDimensionPixelSize(R.dimen.ntp_wide_card_lateral_margins);
+            mMarginResizer =
+                    new MarginResizer(itemView, uiConfig, defaultLateralMargin, wideLateralMargin);
+        } else {
+            mMarginResizer = null;
+        }
     }
 
     @Override
@@ -40,4 +58,15 @@ public class TileGridViewHolder extends SiteSectionViewHolder {
         return mSectionView.getTileView(data);
     }
 
+    @Override
+    public void bindDataSource(TileGroup tileGroup, TileRenderer tileRenderer) {
+        super.bindDataSource(tileGroup, tileRenderer);
+        if (mMarginResizer != null) mMarginResizer.attach();
+    }
+
+    @Override
+    public void recycle() {
+        super.recycle();
+        if (mMarginResizer != null) mMarginResizer.detach();
+    }
 }
