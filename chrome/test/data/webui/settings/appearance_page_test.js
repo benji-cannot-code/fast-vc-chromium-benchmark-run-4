5488b72c0d68f2d3bb6269c52d708c32d520a645
MinChen <minch@chromium.org>
2017-09-21 17:22:44 +0000

    Make it can't open wallpaper manager if wallpaper is policy controlled.
    Changes,
    1. Show the wallpaper setting row only for login, whitelist types and active users
    (should satisfy the whole conditions at the same time).
    2. Only can open the wallpaper manager if the wallpaper is not policy controlled.
    3. Added wallpaper policy indicator. Show it if the wallpaper is policy
    controlled.
    
    Bug: 749755
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ie6b9875db7f1958d3cdd8fb9fc817e4df79782b3
    Reviewed-on: https://chromium-review.googlesource.com/661070
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: min c <minch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503487}

--- chrome/test/data/webui/settings/appearance_page_test.js
+++ chrome/test/data/webui/settings/appearance_page_test.js
@@ -10,6 +10,8 @@ class TestAppearanceBrowserProxy extends TestBrowserProxy {
       'getDefaultZoom',
       'getThemeInfo',
       'isSupervised',
+      'isWallpaperSettingVisible',
+      'isWallpaperPolicyControlled',
       'openWallpaperManager',
       'useDefaultTheme',
       'useSystemTheme',
@@ -24,6 +26,12 @@ class TestAppearanceBrowserProxy extends TestBrowserProxy {
 
     /** @private */
     this.isHomeUrlValid_ = true;
+
+    /** @private */
+    this.isWallpaperSettingVisible_ = true;
+
+    /** @private */
+    this.isWallpaperPolicyControlled_ = false;
   }
 
   /** @override */
@@ -42,6 +50,18 @@ class TestAppearanceBrowserProxy extends TestBrowserProxy {
   isSupervised() {
     this.methodCalled('isSupervised');
     return this.isSupervised_;
+  }
+
+  /** @override */
+  isWallpaperSettingVisible() {
+    this.methodCalled('isWallpaperSettingVisible');
+    return Promise.resolve(this.isWallpaperSettingVisible_);
+  }
+
+  /** @override */
+  isWallpaperPolicyControlled() {
+    this.methodCalled('isWallpaperPolicyControlled');
+    return Promise.resolve(this.isWallpaperPolicyControlled_);
   }
 
   /** @override */
@@ -81,6 +101,11 @@ class TestAppearanceBrowserProxy extends TestBrowserProxy {
   setValidStartupPageResponse(isValid) {
     this.isHomeUrlValid_ = isValid;
   }
+
+  /** @param {boolean} Whether the wallpaper is policy controlled. */
+  setIsWallpaperPolicyControlled(isPolicyControlled) {
+    this.isWallpaperPolicyControlled_ = isPolicyControlled;
+  }
 }
 
 var appearancePage = null;
@@ -106,6 +131,10 @@ function createAppearancePage() {
     },
   });
 
+  appearancePage.set('pageVisibility', {
+    setWallpaper: true,
+  });
+
   document.body.appendChild(appearancePage);
   Polymer.dom.flush();
 }
@@ -125,6 +154,28 @@ suite('AppearanceHandler', function() {
       assertTrue(!!button);
       MockInteractions.tap(button);
       return appearanceBrowserProxy.whenCalled('openWallpaperManager');
+    });
+
+    test('wallpaperSettingVisible', function() {
+      appearancePage.set("pageVisibility.setWallpaper", false);
+      return appearanceBrowserProxy.whenCalled('isWallpaperSettingVisible')
+          .then(function() {
+            Polymer.dom.flush();
+            assertTrue(appearancePage.$$('#wallpaperButton').hidden);
+          });
+    });
+
+    test('wallpaperPolicyControlled', function() {
+      // Should show the wallpaper policy indicator and disable the toggle
+      // button if the wallpaper is policy controlled.
+      appearanceBrowserProxy.setIsWallpaperPolicyControlled(true);
+      createAppearancePage();
+      return appearanceBrowserProxy.whenCalled('isWallpaperPolicyControlled')
+          .then(function() {
+            Polymer.dom.flush();
+            assertFalse(appearancePage.$$('#wallpaperPolicyIndicator').hidden);
+            assertTrue(appearancePage.$$('#showWallpaperManager').disabled);
+          });
     });
   } else {
     test('noWallpaperManager', function() {
--- chrome/test/data/webui/settings/settings_main_test.js
+++ chrome/test/data/webui/settings/settings_main_test.js
@@ -68,6 +68,7 @@ cr.define('settings_main_page', function() {
       settingsMain = document.createElement('settings-main');
       settingsMain.prefs = settingsPrefs.prefs;
       settingsMain.toolbarSpinnerActive = false;
+      settingsMain.pageVisibility = settings.pageVisibility;
       document.body.appendChild(settingsMain);
     });
 
