a788fb7adef7efe2929be6f0cfd94a2c4b4a0efd
Ivan Sandrk <isandrk@google.com>
2017-09-21 15:52:36 +0000

    Migrate DeviceLocalAccountPolicyService to use TaskScheduler
    Removing use of FILE thread, replacing with SequencedTaskRunner.
    
    Bug: chromium:689520
    Change-Id: Ieaff2340a1238bcad753d32490b5957847366aa1
    Reviewed-on: https://chromium-review.googlesource.com/673463
    Commit-Queue: Ivan Šandrk <isandrk@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503457}

--- chrome/browser/chromeos/policy/device_local_account_policy_service.h
+++ chrome/browser/chromeos/policy/device_local_account_policy_service.h
@@ -59,6 +59,8 @@ class DeviceLocalAccountPolicyBroker
   // |policy_update_callback| will be invoked to notify observers that the
   // policy for |account| has been updated.
   // |task_runner| is the runner for policy refresh tasks.
+  // |resource_cache_task_runner| is the task runner used for file operations,
+  // it must be sequenced together with other tasks running on the same files.
   DeviceLocalAccountPolicyBroker(
       const DeviceLocalAccount& account,
       const base::FilePath& component_policy_cache_path,
@@ -67,6 +69,8 @@ class DeviceLocalAccountPolicyBroker
           external_data_manager,
       const base::Closure& policy_updated_callback,
       const scoped_refptr<base::SequencedTaskRunner>& task_runner,
+      const scoped_refptr<base::SequencedTaskRunner>&
+          resource_cache_task_runner,
       AffiliatedInvalidationServiceProvider* invalidation_service_provider);
   ~DeviceLocalAccountPolicyBroker() override;
 
@@ -139,6 +143,7 @@ class DeviceLocalAccountPolicyBroker
   std::unique_ptr<ComponentCloudPolicyService> component_policy_service_;
   base::Closure policy_update_callback_;
   std::unique_ptr<AffiliatedCloudPolicyInvalidator> invalidator_;
+  const scoped_refptr<base::SequencedTaskRunner> resource_cache_task_runner_;
 
   DISALLOW_COPY_AND_ASSIGN(DeviceLocalAccountPolicyBroker);
 };
@@ -267,6 +272,7 @@ class DeviceLocalAccountPolicyService {
 
   const scoped_refptr<base::SequencedTaskRunner> store_background_task_runner_;
   const scoped_refptr<base::SequencedTaskRunner> extension_cache_task_runner_;
+  const scoped_refptr<base::SequencedTaskRunner> resource_cache_task_runner_;
 
   std::unique_ptr<DeviceLocalAccountExternalDataService> external_data_service_;
 
