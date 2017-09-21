56861126158aa83ddcb2d8b184f527fff8019039
Dave Schuyler <dschuyler@chromium.org>
2017-09-21 21:10:07 +0000

    [cr webui] keyboard selection on cr-link-row
    This CL fixes keyboard navigation to cr-link-row(s). A cr-icon class was
    added for the purpose of creating cr-icons that are not within a button.
    
    Bug: 766067
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I392a432c86313a81341deca4eda9e7505b5441e5
    Reviewed-on: https://chromium-review.googlesource.com/671198
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Commit-Queue: Dave Schuyler <dschuyler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503563}

--- ui/webui/resources/cr_elements/cr_link_row/cr_link_row.js
+++ ui/webui/resources/cr_elements/cr_link_row/cr_link_row.js
@@ -9,10 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * button (taking up the whole 'row'). The name link comes from the intended use
  * of this element to take the user to another page in the app or to an external
  * page (somewhat like an HTML link).
+ * Note: the ripple handling was taken from Polymer v1 paper-icon-button-light.
  */
 Polymer({
   is: 'cr-link-row',
   extends: 'button',
+
+  behaviors: [Polymer.PaperRippleBehavior],
 
   properties: {
     iconClass: String,
@@ -25,4 +28,36 @@ Polymer({
       value: '',
     },
   },
+
+  listeners: {
+    'down': '_rippleDown',
+    'up': '_rippleUp',
+    'focus': '_rippleDown',
+    'blur': '_rippleUp',
+  },
+
+  _rippleDown: function() {
+    this.getRipple().uiDownAction();
+  },
+
+  _rippleUp: function() {
+    this.getRipple().uiUpAction();
+  },
+
+  _createRipple: function() {
+    this._rippleContainer = this.$.icon;
+    return Polymer.PaperInkyFocusBehaviorImpl._createRipple.call(this);
+  },
+
+  /**
+   * @param {...*} var_args
+   */
+  ensureRipple: function(var_args) {
+    var lastRipple = this._ripple;
+    Polymer.PaperRippleBehavior.ensureRipple.apply(this, arguments);
+    if (this._ripple && this._ripple !== lastRipple) {
+      this._ripple.center = true;
+      this._ripple.classList.add('circle');
+    }
+  }
 });
