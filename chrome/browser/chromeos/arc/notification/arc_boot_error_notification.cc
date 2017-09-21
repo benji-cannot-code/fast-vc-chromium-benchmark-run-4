6c73de2a99a60023782edb94cfb8ecb47a4c3ea3
Tetsui Ohkubo <tetsui@chromium.org>
2017-09-21 03:41:22 +0000

    Add material design icon to ARC storage low notification.
    This CL adds new material design icon to ARC storage low notification.
    
    The icon and the accent color are effective only when
    chrome://flags/#enable-message-center-new-style-notification
    is Enabled (which is ToT default).
    
    TEST=manual
    BUG=726241
    
    Change-Id: I967dc65dd549d86ffb10be5371653b8c2b9196fc
    Reviewed-on: https://chromium-review.googlesource.com/674645
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503345}

--- chrome/browser/chromeos/arc/notification/arc_boot_error_notification.cc
+++ chrome/browser/chromeos/arc/notification/arc_boot_error_notification.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
 #include "chrome/browser/profiles/profile.h"
@@ -23,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/message_center/notification.h"
 #include "ui/message_center/notification_types.h"
 #include "ui/message_center/notifier_settings.h"
+#include "ui/message_center/public/cpp/message_center_switches.h"
 
 namespace arc {
 
@@ -80,18 +82,36 @@ void ShowLowDiskSpaceErrorNotification(content::BrowserContext* context) {
       user_manager::UserManager::Get()->GetPrimaryUser()->GetAccountId();
   notifier_id.profile_id = account_id.GetUserEmail();
 
+  std::unique_ptr<message_center::Notification> notification;
+  if (message_center::IsNewStyleNotificationEnabled()) {
+    notification = message_center::Notification::CreateSystemNotification(
+        message_center::NOTIFICATION_TYPE_SIMPLE, kLowDiskSpaceId,
+        l10n_util::GetStringUTF16(
+            IDS_ARC_CRITICALLY_LOW_DISK_NOTIFICATION_TITLE),
+        l10n_util::GetStringUTF16(
+            IDS_ARC_CRITICALLY_LOW_DISK_NOTIFICATION_MESSAGE),
+        gfx::Image(ui::ResourceBundle::GetSharedInstance().GetImageNamed(
+            IDR_DISK_SPACE_NOTIFICATION_CRITICAL)),
+        l10n_util::GetStringUTF16(IDS_ARC_NOTIFICATION_DISPLAY_SOURCE), GURL(),
+        notifier_id, optional_fields,
+        new LowDiskSpaceErrorNotificationDelegate(context),
+        kNotificationStorageFullIcon,
+        message_center::SystemNotificationWarningLevel::CRITICAL_WARNING);
+  } else {
+    notification = std::make_unique<message_center::Notification>(
+        message_center::NOTIFICATION_TYPE_SIMPLE, kLowDiskSpaceId,
+        l10n_util::GetStringUTF16(
+            IDS_ARC_CRITICALLY_LOW_DISK_NOTIFICATION_TITLE),
+        l10n_util::GetStringUTF16(
+            IDS_ARC_CRITICALLY_LOW_DISK_NOTIFICATION_MESSAGE),
+        gfx::Image(ui::ResourceBundle::GetSharedInstance().GetImageNamed(
+            IDR_DISK_SPACE_NOTIFICATION_CRITICAL)),
+        l10n_util::GetStringUTF16(IDS_ARC_NOTIFICATION_DISPLAY_SOURCE), GURL(),
+        notifier_id, optional_fields,
+        new LowDiskSpaceErrorNotificationDelegate(context));
+  }
   message_center::MessageCenter::Get()->AddNotification(
-      base::MakeUnique<message_center::Notification>(
-          message_center::NOTIFICATION_TYPE_SIMPLE, kLowDiskSpaceId,
-          l10n_util::GetStringUTF16(
-              IDS_ARC_CRITICALLY_LOW_DISK_NOTIFICATION_TITLE),
-          l10n_util::GetStringUTF16(
-              IDS_ARC_CRITICALLY_LOW_DISK_NOTIFICATION_MESSAGE),
-          gfx::Image(ui::ResourceBundle::GetSharedInstance().GetImageNamed(
-              IDR_DISK_SPACE_NOTIFICATION_CRITICAL)),
-          l10n_util::GetStringUTF16(IDS_ARC_NOTIFICATION_DISPLAY_SOURCE),
-          GURL(), notifier_id, optional_fields,
-          new LowDiskSpaceErrorNotificationDelegate(context)));
+      std::move(notification));
 }
 
 // Singleton factory for ArcBootErrorNotificationFactory.
