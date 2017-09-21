741d2bdf68f0451926f75970632b254d251d60b1
Gauthier Ambard <gambard@chromium.org>
2017-09-21 08:47:51 +0000

    Disable ContextMenuJsTest tests
    TextAreaStopsProximity and GetImageUrlAtPoint are failing on iOS 11
    iPhone devices. This is disabling them.
    
    Bug: 767339
    Tbr: eugenebut@chromium.org
    Change-Id: I6f081d5ded1f22c80d2360aef262ea95ad0abb99
    Reviewed-on: https://chromium-review.googlesource.com/676846
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503393}

--- ios/web/web_state/js/context_menu_js_unittest.mm
+++ ios/web/web_state/js/context_menu_js_unittest.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+#include "base/ios/ios_util.h"
 #include "base/macros.h"
 #include "base/strings/stringprintf.h"
 #import "ios/web/public/test/web_test_with_web_state.h"
@@ -91,6 +92,14 @@ class ContextMenuJsTest : public web::WebTestWithWebState {
 
 // Tests that __gCrWeb.getElementFromPoint function returns correct src.
 TEST_F(ContextMenuJsTest, GetImageUrlAtPoint) {
+// TODO(crbug.com/767339): This is extremely flaky on iPhone device with iOS 11.
+// Fix it and reenable it.
+#if !TARGET_IPHONE_SIMULATOR
+  if (base::ios::IsRunningOnIOS11OrLater()) {
+    return;
+  }
+#endif
+
   NSString* html =
       @"<img id='foo' style='width:200;height:200;' src='file:///bogus'/>";
   NSDictionary* expected_value = @{
@@ -127,6 +136,13 @@ TEST_F(ContextMenuJsTest, GetLinkImageUrlAtPoint) {
 }
 
 TEST_F(ContextMenuJsTest, TextAreaStopsProximity) {
+// TODO(crbug.com/767339): This is extremely flaky on iPhone device with iOS 11.
+// Fix it and reenable it.
+#if !TARGET_IPHONE_SIMULATOR
+  if (base::ios::IsRunningOnIOS11OrLater()) {
+    return;
+  }
+#endif
   NSString* html =
       @"<html><body style='margin-left:10px;margin-top:10px;'>"
        "<div style='width:100px;height:100px;'>"
