39e385442a9e93a4524fb6431bf178a96055d54a
Scott Graham <scottmg@chromium.org>
2017-09-22 17:21:06 +0000

    fuchsia: Disable VideoRendererAlgorithmTest.CadenceBasedTest
    While it only takes ~5s locally in a debug build, it's taking ~50s on
    the fyi waterfall bot, causing failures due to timeout.
    
    Bug: 767166
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I95fde0b34a743a6281f269efd625015baaa0d853
    Reviewed-on: https://chromium-review.googlesource.com/678920
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Scott Graham <scottmg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503777}

--- media/filters/video_renderer_algorithm_unittest.cc
+++ media/filters/video_renderer_algorithm_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/simple_test_tick_clock.h"
+#include "build/build_config.h"
 #include "media/base/media_log.h"
 #include "media/base/timestamp_constants.h"
 #include "media/base/video_frame_pool.h"
@@ -1335,7 +1336,9 @@ TEST_F(VideoRendererAlgorithmTest, RemoveExpiredFramesCadence) {
 }
 
 // Test runs too slowly on debug builds.
-#if defined(NDEBUG)
+// TODO(fuchsia): Also runs too slowly on Fuchsia, this should be investigated,
+// see https://crbug.com/767166.
+#if defined(NDEBUG) && !defined(OS_FUCHSIA)
 TEST_F(VideoRendererAlgorithmTest, CadenceBasedTest) {
   // Common display rates.
   const double kDisplayRates[] = {
@@ -1363,7 +1366,7 @@ TEST_F(VideoRendererAlgorithmTest, CadenceBasedTest) {
     }
   }
 }
-#endif
+#endif  // defined(NDEBUG) && !defined(OS_FUCHSIA)
 
 // Rotate through various playback rates and ensure algorithm adapts correctly.
 TEST_F(VideoRendererAlgorithmTest, VariablePlaybackRateCadence) {
