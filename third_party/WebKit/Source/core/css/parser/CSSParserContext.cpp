4ed6bc1220eeab0af3adfdac94bda449ea5ac759
Fernando Serboncini <fserb@chromium.org>
2017-09-22 03:51:15 +0000

    Creates a new CSSParserContext constructor for workers
    Bug: 722511
    Change-Id: I0fb1f4a027cc1c90d026583e6f3ab3ca03bb801b
    Reviewed-on: https://chromium-review.googlesource.com/677405
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503647}

--- third_party/WebKit/Source/core/css/parser/CSSParserContext.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSParserContext.cpp
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/css/CSSStyleSheet.h"
 #include "core/css/StyleSheetContents.h"
+#include "core/dom/ExecutionContext.h"
 #include "core/frame/Deprecation.h"
 #include "core/frame/Settings.h"
 #include "core/frame/csp/ContentSecurityPolicy.h"
@@ -14,6 +15,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/page/Page.h"
 
 namespace blink {
+
+// static
+CSSParserContext* CSSParserContext::Create(const ExecutionContext& context) {
+  const Referrer referrer(context.Url().StrippedForUseAsReferrer(),
+                          context.GetReferrerPolicy());
+
+  ContentSecurityPolicyDisposition policy_disposition;
+  if (ContentSecurityPolicy::ShouldBypassMainWorld(&context))
+    policy_disposition = kDoNotCheckContentSecurityPolicy;
+  else
+    policy_disposition = kCheckContentSecurityPolicy;
+
+  return new CSSParserContext(
+      context.Url(), WTF::TextEncoding(), kHTMLStandardMode, kHTMLStandardMode,
+      kDynamicProfile, referrer, true, false, policy_disposition,
+      context.IsDocument() ? &ToDocument(context) : nullptr);
+}
 
 // static
 CSSParserContext* CSSParserContext::CreateWithStyleSheet(
