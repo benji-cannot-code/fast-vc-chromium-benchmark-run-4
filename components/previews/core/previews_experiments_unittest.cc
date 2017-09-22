a22273027b500cf1c44531c061d20e5381198dcc
Thanh Le <thanhdle@chromium.org>
2017-09-22 21:48:52 +0000

    Fixed TestClientLoFiExplicitlyDisabled implementation.
    The test now disables kClientLoFiFieldTrial, and test that params::IsClientLoFiEnabled() is false.
    
    Bug: 767932
    Change-Id: Ib10f804c0f5410fdd8fc768a96b232f35727ece5
    Reviewed-on: https://chromium-review.googlesource.com/679201
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Thanh Le <thanhdle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503867}

--- components/previews/core/previews_experiments_unittest.cc
+++ components/previews/core/previews_experiments_unittest.cc
@@ -24,6 +24,7 @@ namespace {
 const char kClientSidePreviewsFieldTrial[] = "ClientSidePreviews";
 const char kClientLoFiFieldTrial[] = "PreviewsClientLoFi";
 const char kEnabled[] = "Enabled";
+const char kDisabled[] = "Disabled";
 
 // Verifies that we can enable offline previews via comand line.
 TEST(PreviewsExperimentsTest, TestCommandLineOfflinePage) {
@@ -108,8 +109,8 @@ TEST(PreviewsExperimentsTest, TestClientLoFiDisabledByDefault) {
 TEST(PreviewsExperimentsTest, TestClientLoFiExplicitlyDisabled) {
   base::FieldTrialList field_trial_list(nullptr);
   EXPECT_TRUE(
-      base::FieldTrialList::CreateFieldTrial(kClientLoFiFieldTrial, kEnabled));
-  EXPECT_TRUE(params::IsClientLoFiEnabled());
+      base::FieldTrialList::CreateFieldTrial(kClientLoFiFieldTrial, kDisabled));
+  EXPECT_FALSE(params::IsClientLoFiEnabled());
 }
 
 TEST(PreviewsExperimentsTest, TestClientLoFiEnabled) {
