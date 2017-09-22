a9ee2cac26896f4762f00761100a2a4b10ab5dc7
Gabriel Charette <gab@chromium.org>
2017-09-22 14:33:59 +0000

    Remove all remaining deprecated Web/BrowserThreads!!
    (except BrowserThread::FILE which has a handful use cases left still and
    will follow suit shortly)
    
    (BrowserThread::PROCESS_LAUNCHER migration was put on hold because of
    Android specific requirements around process launching)
    
    Remaining usage of TestBrowserThreadBundle::REAL_DB_THREAD could simply
    be removed as all of the product code was already migrated to TaskScheduler
    which always uses real threads in unit tests.
    
    Bug: 689520, 365909, 752144
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester
    Change-Id: Id70b722aa35e8568795d5aac16947392ac1597be
    
    NOPRESUBMIT=True (for content/browser/browser_thread_impl.cc:211)
    
    Change-Id: Id70b722aa35e8568795d5aac16947392ac1597be
    Reviewed-on: https://chromium-review.googlesource.com/610880
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503732}

--- ios/chrome/browser/ui/settings/save_passwords_collection_view_controller_unittest.mm
+++ ios/chrome/browser/ui/settings/save_passwords_collection_view_controller_unittest.mm
@@ -35,8 +35,7 @@ namespace {
 class SavePasswordsCollectionViewControllerTest
     : public CollectionViewControllerTest {
  protected:
-  SavePasswordsCollectionViewControllerTest()
-      : thread_bundle_(web::TestWebThreadBundle::REAL_DB_THREAD) {}
+  SavePasswordsCollectionViewControllerTest() = default;
 
   void SetUp() override {
     TestChromeBrowserState::Builder test_cbs_builder;
