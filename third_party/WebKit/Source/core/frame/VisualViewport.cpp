3daf62ec8fb937dd2da72969cc5c615ac081d46b
Sriram <srirama.m@samsung.com>
2017-09-22 13:10:56 +0000

    [RLS] Get MinimumScrollOffsetInt() from LVSA whereever applicable
    Use LayoutViewportScrollableArea()->MinimumScrollOffsetInt()
    whereever applicable
    
    Change-Id: Ifdf98c5da64837f630759618da9b971333ce93d7
    Reviewed-on: https://chromium-review.googlesource.com/677911
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: srirama chandra sekhar <srirama.m@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#503725}

--- third_party/WebKit/Source/core/frame/VisualViewport.cpp
+++ third_party/WebKit/Source/core/frame/VisualViewport.cpp
@@ -599,7 +599,8 @@ IntPoint VisualViewport::ClampDocumentOffsetAtScale(const IntPoint& offset,
   IntSize max = view->LayoutViewportScrollableArea()->MaximumScrollOffsetInt() +
                 visual_viewport_max;
   IntSize min =
-      view->MinimumScrollOffsetInt();  // VisualViewportMin should be (0, 0)
+      view->LayoutViewportScrollableArea()
+          ->MinimumScrollOffsetInt();  // VisualViewportMin should be (0, 0)
 
   IntSize clamped = ToIntSize(offset);
   clamped = clamped.ShrunkTo(max);
