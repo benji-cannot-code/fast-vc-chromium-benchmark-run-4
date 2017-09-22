0d00ed2893a2d5c850314177644c294d868e447c
spqchan <spqchan@chromium.org>
2017-09-22 03:40:58 +0000

    [Mac] Invalidate the Touch Bar for Tab Changes
    When the active tab changes, the page might not be get reloaded. As
    such, we'll have to invalidate the touch bar when that happens.
    
    Bug: 766969
    Change-Id: I6cd9288966d7cd618a38709eb5e2dadcebb1a3c2
    Reviewed-on: https://chromium-review.googlesource.com/677481
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Sarah Chan <spqchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503631}

--- chrome/browser/ui/cocoa/browser_window_controller.mm
+++ chrome/browser/ui/cocoa/browser_window_controller.mm
@@ -1081,6 +1081,7 @@ bool IsTabDetachingInFullscreenEnabled() {
     [[self fullscreenToolbarController] revealToolbarForWebContents:newContents
                                                        inForeground:YES];
   }
+  [self invalidateTouchBar];
 }
 
 - (void)zoomChangedForActiveTab:(BOOL)canShowBubble {
