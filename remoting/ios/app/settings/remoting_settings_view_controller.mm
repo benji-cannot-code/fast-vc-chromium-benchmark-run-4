1944766955c254dd22085e88b5dab97adaa01ac4
Yuwei Huang <yuweih@chromium.org>
2017-09-22 19:37:46 +0000

    [CRD iOS] Close settings menu when keys are injected
    This CL closes the settings menu immediately after a key combination is
    sent to the host so that the user can immediately see the effect.
    
    Bug: 766869
    Change-Id: I93c6e8aa416baabeba714bddc13bc20ba80defbe
    Reviewed-on: https://chromium-review.googlesource.com/674127
    Reviewed-by: Scott Nichols <nicholss@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503823}

--- remoting/ios/app/settings/remoting_settings_view_controller.mm
+++ remoting/ios/app/settings/remoting_settings_view_controller.mm
@@ -301,6 +301,7 @@ static NSString* const kFeedbackContext = @"InSessionFeedbackContext";
   ctrlAltDelOption.action = ^{
     if ([weakSelf.delegate respondsToSelector:@selector(sendCtrAltDel)]) {
       [weakSelf.delegate sendCtrAltDel];
+      [weakSelf dismissViewControllerAnimated:YES completion:nil];
     }
   };
 
@@ -310,6 +311,7 @@ static NSString* const kFeedbackContext = @"InSessionFeedbackContext";
   printScreenOption.action = ^{
     if ([weakSelf.delegate respondsToSelector:@selector(sendPrintScreen)]) {
       [weakSelf.delegate sendPrintScreen];
+      [weakSelf dismissViewControllerAnimated:YES completion:nil];
     }
   };
 
