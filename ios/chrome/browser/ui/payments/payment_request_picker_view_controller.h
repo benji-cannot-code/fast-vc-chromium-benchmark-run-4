fbc616afcda44079940af7466b0ab586309e487f
edchin <edchin@chromium.org>
2017-09-22 22:57:00 +0000

    Revert "[Payment Request] Adds a navigation bar to the country picker."
    This reverts commit 266e6ebd419359717c8d6aadf9311717765883f7.
    
    Reason for revert: 
    
    Sorry for another revert! The test is failing on iPhone 5s and iPad retina on iOS 9.
    
    SCPaymentsPickerTestCase/testVerifySelection: 
    ../../ios/showcase/payments/sc_payments_picker_egtest.mm:249: error: -[SCPaymentsPickerTestCase testVerifySelection] : Exception: NoMatchingElementException
    
    Exception Name: NoMatchingElementException
    Exception Reason: Cannot find UI element.
    Exception with Action: {
      "Action Name" : "Tap",
      "Element Matcher" : "((((kindOfClass('UIView') || respondsToSelector(accessibilityContainer)) && ancestorThatMatches((respondsToSelector(accessibilityIdentifier) && accessibilityID('kPaymentRequestPickerRowAccessibilityID')))) && ((kindOfClass('UILabel') || kindOfClass('UITextField') || kindOfClass('UITextView')) && hasText('Canada')) && kindOfClass('UILabel') && matcherForSufficientlyVisible(>=0.750000)) && ((kindOfClass('UIView') || respondsToSelector(accessibilityContainer)) && ancestorThatMatches(((respondsToSelector(isAccessibilityElement) && isAccessibilityElement) && accessibilityTraits: UIAccessibilityTraitSelected))))",
      "Recovery Suggestion" : "Check if the element exists in the UI hierarchy printed below. If it exists, adjust the matcher so that it accurately matches element."
    }
    
    Exception Details: Error Trace: [
      {
        "Description" : "Interaction cannot continue because the desired element was not found.",
        "Error Domain" : "com.google.earlgrey.ElementInteractionErrorDomain",
        "Error Code" : "0",
        "File Name" : "GREYElementInteraction.m",
        "Function Name" : "-[GREYElementInteraction matchedElementsWithTimeout:error:]",
        "Line" : "124",
        "TestCase Class" : "SCPaymentsPickerTestCase",
        "TestCase Method" : "testVerifySelection"
      }
    ]
    
    Original change's description:
    > [Payment Request] Adds a navigation bar to the country picker.
    > 
    > Video of the fix in action:
    > https://drive.google.com/file/d/0B4g_WskzVRJdQUVjLUE2RWRWREE/view?usp=sharing
    > 
    > This is a second attempt at fixing this issue. The first one was reverted due to failures in ios_showcase_egtests on smaller devices. Those failures were caused by the fact that some elements were off-screen, and EarlGrey was unable to select them.
    > 
    > Bug: 767154
    > Change-Id: I60df99bf12a0a0858c900faa6544bf562667eda8
    > Reviewed-on: https://chromium-review.googlesource.com/678020
    > Reviewed-by: edchin <edchin@chromium.org>
    > Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    > Commit-Queue: Marc-Antoine Courteau <macourteau@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#503771}
    
    TBR=macourteau@chromium.org,edchin@chromium.org,mahmadi@chromium.org
    
    Change-Id: Iff3bb784ec3b3a572830aefc35ffaabd040e313d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 767154
    Reviewed-on: https://chromium-review.googlesource.com/679576
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503898}

--- ios/chrome/browser/ui/payments/payment_request_picker_view_controller.h
+++ ios/chrome/browser/ui/payments/payment_request_picker_view_controller.h
@@ -8,10 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
-#import "ios/chrome/browser/ui/material_components/app_bar_presenting.h"
-
 extern NSString* const kPaymentRequestPickerRowAccessibilityID;
-extern NSString* const kPaymentRequestPickerViewControllerAccessibilityID;
 extern NSString* const kPaymentRequestPickerSearchBarAccessibilityID;
 
 @class PaymentRequestPickerViewController;
@@ -29,8 +26,7 @@ extern NSString* const kPaymentRequestPickerSearchBarAccessibilityID;
 
 // TableViewController that displays a searchable list of rows featuring a
 // selected row as well as an index list.
-@interface PaymentRequestPickerViewController
-    : UITableViewController<AppBarPresenting>
+@interface PaymentRequestPickerViewController : UITableViewController
 
 // The delegate to be notified when the user selects a row.
 @property(nonatomic, weak) id<PaymentRequestPickerViewControllerDelegate>
