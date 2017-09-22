758556b21d22e8221a97499576e8a68d72dcfcd4
Tomasz Garbus <tgarbus@chromium.org>
2017-09-22 18:28:06 +0000

    Moved IDS_MANAGE_PASSWORDS_AUTO_SIGNIN_TITLE to //components.
    In this CL I have moved IDS_MANAGE_PASSWORDS_AUTO_SIGNIN_TITLE string
    from chrome/app/generated_resources.grd to components/password_manager_strings.grdp.
    This is in order to reuse the string in Auto Sign-in notification on iOS,
    which is currently Work In Progress in the following CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/665119
    
    Bug: 435048
    Change-Id: Ie72edd435f3f9db08a40bb92a729119773a5db48
    Reviewed-on: https://chromium-review.googlesource.com/674689
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Tomasz Garbus <tgarbus@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503802}

--- chrome/browser/ui/android/snackbars/auto_signin_prompt_controller.cc
+++ chrome/browser/ui/android/snackbars/auto_signin_prompt_controller.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/password_manager/core/browser/password_bubble_experiment.h"
+#include "components/strings/grit/components_strings.h"
 #include "content/public/browser/web_contents.h"
 #include "jni/AutoSigninSnackbarController_jni.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -32,4 +33,3 @@ void ShowAutoSigninPrompt(content::WebContents* web_contents,
   Java_AutoSigninSnackbarController_showSnackbar(env, tab->GetJavaObject(),
                                                  java_message);
 }
-
--- chrome/browser/ui/cocoa/passwords/auto_signin_view_controller.mm
+++ chrome/browser/ui/cocoa/passwords/auto_signin_view_controller.mm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/passwords/manage_passwords_bubble_model.h"
 #include "chrome/browser/ui/passwords/manage_passwords_view_utils.h"
 #include "chrome/grit/generated_resources.h"
+#include "components/strings/grit/components_strings.h"
 #include "skia/ext/skia_utils_mac.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
