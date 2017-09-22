637252b7a9643c09c236b9005157f6b89714c739
Pavel Feldman <pfeldman@chromium.org>
2017-09-22 18:10:38 +0000

    Reland "DevTools: escape protocol strings that are originally utf8."
    This is a reland of 74e192b49bfceb69da7ccf1efed1a2d7c07a9e83
    Original change's description:
    > DevTools: escape protocol strings that are originally utf8.
    > 
    > Bug: 765506
    > Change-Id: I16fbe56180f96eb1daf504510a6d07965e226622
    TBR: (api is changing)
    > Reviewed-on: https://chromium-review.googlesource.com/667804
    > Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    > Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    > Reviewed-by: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#502929}
    
    Bug: 765506
    Change-Id: Ib2fbf5b700bdfae31b55f2d8ccd3327bbd86b884
    Reviewed-on: https://chromium-review.googlesource.com/675774
    Reviewed-by: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503794}

--- content/browser/devtools/protocol_string.cc
+++ content/browser/devtools/protocol_string.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/json/json_reader.h"
 #include "base/memory/ptr_util.h"
+#include "base/strings/string16.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
 #include "content/browser/devtools/protocol/protocol.h"
 
@@ -143,6 +145,16 @@ void StringBuilder::append(char c) {
 
 void StringBuilder::append(const char* characters, size_t length) {
   string_.append(characters, length);
+}
+
+// static
+void StringUtil::builderAppendQuotedString(StringBuilder& builder,
+                                           const String& str) {
+  builder.append('"');
+  base::string16 str16 = base::UTF8ToUTF16(str);
+  escapeWideStringForJSON(reinterpret_cast<const uint16_t*>(&str16[0]),
+                          str16.length(), &builder);
+  builder.append('"');
 }
 
 std::string StringBuilder::toString() {
