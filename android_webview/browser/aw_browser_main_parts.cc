99662048d876c07838119b7d707170cc7271df73
Andrew Moylan <amoylan@chromium.org>
2017-09-22 00:10:31 +0000

    Remove UseNetworkLocationProviders
    This CL removes GeolocationDelegate::UseNetworkLocationProviders. Its
    responsibility (of optionally disabling network geolocation) is
    explicitly moved to GeolocationDelegate::CreateAccessTokenStore.
    
    Today GeolocationDelegate has UseNetworkLocationProviders (default true)
    and CreateAccessTokenStore (default nullptr), and these are effectively
    &&'d together by LocationArbitrator to decide whether to try to use
    network geolocation. Since AccessTokenStore has other uses,
    UseNetworkLocationProviders is essentially redundant.
    
    I surveyed embedders internal and external to Chromium repo and found no
    cases of contradictory use of these two signals (i.e. no cases of
    setting UseNetworkLocationProviders to false but still overriding
    CreateAccessTokenStore to return a non-nullptr).
    
    I also spruced up the LocationArbitrator unit tests with some comments
    and made them a bit more "injecty" to simplify understanding them.
    
    Bug: 748921
    Change-Id: I528f6f0a81263df868795be81201141c260aa836
    Reviewed-on: https://chromium-review.googlesource.com/670964
    Reviewed-by: Selim Gurun <sgurun@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Commit-Queue: Andrew Moylan <amoylan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503619}

--- android_webview/browser/aw_browser_main_parts.cc
+++ android_webview/browser/aw_browser_main_parts.cc
@@ -47,27 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_surface.h"
 
 namespace android_webview {
-namespace {
-
-// A provider of Geolocation services to override AccessTokenStore.
-class AwGeolocationDelegate : public device::GeolocationDelegate {
- public:
-  AwGeolocationDelegate() = default;
-
-  // Android doesn't use NetworkLocationProvider (the capability is folded into
-  // the system location provider).
-  bool UseNetworkLocationProviders() override { return false; }
-
-  scoped_refptr<device::AccessTokenStore> CreateAccessTokenStore() final {
-    NOTREACHED() << "No network geolocation for Android webview";
-    return nullptr;
-  }
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(AwGeolocationDelegate);
-};
-
-}  // anonymous namespace
 
 AwBrowserMainParts::AwBrowserMainParts(AwContentBrowserClient* browser_client)
     : browser_client_(browser_client) {
@@ -144,9 +123,6 @@ int AwBrowserMainParts::PreCreateThreads() {
 
 void AwBrowserMainParts::PreMainMessageLoopRun() {
   browser_client_->InitBrowserContext()->PreMainMessageLoopRun();
-
-  device::GeolocationProvider::SetGeolocationDelegate(
-      new AwGeolocationDelegate());
 
   content::RenderFrameHost::AllowInjectingJavaScriptForAndroidWebView();
 
