42611dfdcdd19b090e990c0b269d7a154ba2d920
Wenzhao (Colin) Zang <wzang@chromium.org>
2017-09-22 20:47:55 +0000

    Revert "Ignore calls of showing header bar if views-based shelf is enabled"
    This reverts commit 36889f6674a09e84332bf6516373fc23bd7eb0f7.
    
    Reason for revert: Test failure:
    https://luci-logdog.appspot.com/v/?s=chromium%2Fbb%2Fchromium.memory%2FLinux_CFI%2F2233%2F%2B%2Frecipes%2Fsteps%2Fbrowser_tests%2F0%2Flogs%2FUserManagerBrowserTest.UserManagerTest%2F0
    
    Original change's description:
    > Ignore calls of showing header bar if views-based shelf is enabled
    > 
    > There is a case that the web UI and views-based shelf are used
    > together. We should not allow the requests of showing the web UI
    > header bar in such cases, otherwise a duplicate shelf will be shown.
    > 
    > Also modified some comments and remove deprecated functions.
    > 
    > Bug: NONE
    > Change-Id: I16d620c9c0a2502d64ed3a650a8959cdc310aa94
    > Reviewed-on: https://chromium-review.googlesource.com/678238
    > Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    > Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#503832}
    
    TBR=alemate@chromium.org,wzang@chromium.org
    
    Change-Id: Ib05dcce29ec4f47571c6968c214055f7a41ab34e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: NONE
    Reviewed-on: https://chromium-review.googlesource.com/679474
    Reviewed-by: Wenzhao (Colin) Zang <wzang@chromium.org>
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503844}

--- ui/login/display_manager.js
+++ ui/login/display_manager.js
@@ -279,11 +279,6 @@ cr.define('cr.ui.login', function() {
     },
 
     set headerHidden(hidden) {
-      if (this.showingViewsBasedShelf && !hidden) {
-        // Callers should not use this function to show header bar if
-        // views-based shelf is enabled.
-        return;
-      }
       $('login-header-bar').hidden = hidden;
     },
 
