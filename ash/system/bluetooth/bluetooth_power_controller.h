28facb4eb60c3a1f0e4c58b7983fbdb2caa496cd
Sonny Sasaka <sonnysasaka@chromium.org>
2017-09-22 09:46:20 +0000

    Revert "BluetoothPowerController use std::queue instead of base::queue"
    This reverts commit 16d6682d4d606024768a5c49ba462b27ec994326.
    
    The following change to revert base::queue usage was needed to merge to
    M61, which does not have base::queue yet. This CL reverts it on master
    to use base::queue again.
    
    
    Original change's description:
    > BluetoothPowerController use std::queue instead of base::queue
    > 
    > This patch needs to be merged to M61 but base::queue is not available in M61.
    > 
    > BUG=741962
    > TBR=jamescook
    > 
    > Change-Id: I58f381f6ced93f83a2b91fe086729c87686e6cff
    > Reviewed-on: https://chromium-review.googlesource.com/648511
    > Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    > Commit-Queue: Sonny Sasaka <sonnysasaka@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#499352}
    
    TBR=jamescook@chromium.org,oshima@chromium.org,sonnysasaka@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 741962
    Change-Id: I2ea12cf55587911f4316aa81479ef661e5945ea3
    Reviewed-on: https://chromium-review.googlesource.com/677477
    Commit-Queue: Sonny Sasaka <sonnysasaka@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503699}

--- ash/system/bluetooth/bluetooth_power_controller.h
+++ ash/system/bluetooth/bluetooth_power_controller.h
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_manager/user_manager.h"
 #include "device/bluetooth/bluetooth_adapter.h"
 
-#include <queue>
-
 class PrefRegistrySimple;
 class PrefService;
 
