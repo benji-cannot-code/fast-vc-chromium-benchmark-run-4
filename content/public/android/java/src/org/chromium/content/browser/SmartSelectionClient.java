f6d005d5f54a6905f7551e58040f02152b900a22
Shimi Zhang <ctzsm@chromium.org>
2017-09-22 05:07:05 +0000

    [Smart Selection] Plumping for robolectric tests.
    This CL removes android.view.textclassifier.TextClassifier
    dependency in SelectionPopupController. Since our robolectric library
    doesn't support O yet.
    
    1) Moved SmartSelectionProvider.Result/ResultCallback to
    SelectionClient.Result/ResultCallback. This makes more sense since
    SelectionPopupController was unnecessarily depending on
    SmartSelectionProvider to get those class/interface.
    
    2) Added default implementation for TextClassifier getter/setter in 
    SelectionClient, makes it can be implemented in robolectric test.
    
    3) Let ContentViewCore call SelectionClient TextClassifier
    getter/setter directly, so we could remove those getter/setter in
    SelectionPopupController.
    
    4) Injects SelectionClient to SelectionPopupController instead of
    creating one inside.
    
    5) A dummy robolectric test to verify it works.
    
    6) No Object used here..
    
    Bug: 735699
    Change-Id: I7d526b62fb30694dd00295274a5f9b6d24872be7
    Reviewed-on: https://chromium-review.googlesource.com/676563
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503664}

--- content/public/android/java/src/org/chromium/content/browser/SmartSelectionClient.java
+++ content/public/android/java/src/org/chromium/content/browser/SmartSelectionClient.java
@@ -46,7 +46,7 @@ public class SmartSelectionClient implements SelectionClient {
 
     private long mNativeSmartSelectionClient;
     private SmartSelectionProvider mProvider;
-    private SmartSelectionProvider.ResultCallback mCallback;
+    private ResultCallback mCallback;
 
     public static void setEnabled(boolean enabled) {
         sEnabled = enabled;
@@ -56,15 +56,15 @@ public class SmartSelectionClient implements SelectionClient {
      * Creates the SmartSelectionClient. Returns null in case SmartSelectionProvider does not exist
      * in the system.
      */
-    public static SmartSelectionClient create(SmartSelectionProvider.ResultCallback callback,
-            WindowAndroid windowAndroid, WebContents webContents) {
+    public static SmartSelectionClient create(
+            ResultCallback callback, WindowAndroid windowAndroid, WebContents webContents) {
         if (!sEnabled) return null;
         SmartSelectionProvider provider = new SmartSelectionProvider(callback, windowAndroid);
         return new SmartSelectionClient(provider, callback, webContents);
     }
 
-    private SmartSelectionClient(SmartSelectionProvider provider,
-            SmartSelectionProvider.ResultCallback callback, WebContents webContents) {
+    private SmartSelectionClient(
+            SmartSelectionProvider provider, ResultCallback callback, WebContents webContents) {
         mProvider = provider;
         mCallback = callback;
         mNativeSmartSelectionClient = nativeInit(webContents);
@@ -133,7 +133,7 @@ public class SmartSelectionClient implements SelectionClient {
     private void onSurroundingTextReceived(
             @RequestType int callbackData, String text, int start, int end) {
         if (!textHasValidSelection(text, start, end)) {
-            mCallback.onClassified(new SmartSelectionProvider.Result());
+            mCallback.onClassified(new Result());
             return;
         }
 
--- content/public/android/java/src/org/chromium/content/browser/SmartSelectionProvider.java
+++ content/public/android/java/src/org/chromium/content/browser/SmartSelectionProvider.java
@@ -8,14 +8,11 @@ package org.chromium.content.browser;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Handler;
 import android.os.LocaleList;
 import android.support.annotation.IntDef;
-import android.view.View.OnClickListener;
 import android.view.textclassifier.TextClassification;
 import android.view.textclassifier.TextClassificationManager;
 import android.view.textclassifier.TextClassifier;
@@ -31,61 +28,6 @@ import java.util.Locale;
  * Controls Smart Text selection. Talks to the Android TextClassificationManager API.
  */
 public class SmartSelectionProvider {
-    /**
-     * The result of the text analysis.
-     */
-    public static class Result {
-        /**
-         * The number of characters that the left boundary of the original
-         * selection should be moved. Negative number means moving left.
-         */
-        public int startAdjust;
-
-        /**
-         * The number of characters that the right boundary of the original
-         * selection should be moved. Negative number means moving left.
-         */
-        public int endAdjust;
-
-        /**
-         * Label for the suggested menu item.
-         */
-        public CharSequence label;
-
-        /**
-         * Icon for the suggested menu item.
-         */
-        public Drawable icon;
-
-        /**
-         * Intent for the suggested menu item.
-         */
-        public Intent intent;
-
-        /**
-         * OnClickListener for the suggested menu item.
-         */
-        public OnClickListener onClickListener;
-
-        /**
-         * A helper method that returns true if the result has both visual info
-         * and an action so that, for instance, one can make a new menu item.
-         */
-        public boolean hasNamedAction() {
-            return (label != null || icon != null) && (intent != null || onClickListener != null);
-        }
-    }
-
-    /**
-     * The interface that returns the result of the selected text analysis.
-     */
-    public interface ResultCallback {
-        /**
-         * The result is delivered with this method.
-         */
-        void onClassified(Result result);
-    }
-
     private static final String TAG = "SmartSelProvider";
 
     @IntDef({CLASSIFY, SUGGEST_AND_CLASSIFY})
@@ -95,7 +37,7 @@ public class SmartSelectionProvider {
     private static final int CLASSIFY = 0;
     private static final int SUGGEST_AND_CLASSIFY = 1;
 
-    private ResultCallback mResultCallback;
+    private SelectionClient.ResultCallback mResultCallback;
     private WindowAndroid mWindowAndroid;
     private ClassificationTask mClassificationTask;
     private TextClassifier mTextClassifier;
@@ -103,14 +45,15 @@ public class SmartSelectionProvider {
     private Handler mHandler;
     private Runnable mFailureResponseRunnable;
 
-    public SmartSelectionProvider(ResultCallback callback, WindowAndroid windowAndroid) {
+    public SmartSelectionProvider(
+            SelectionClient.ResultCallback callback, WindowAndroid windowAndroid) {
         mResultCallback = callback;
         mWindowAndroid = windowAndroid;
         mHandler = new Handler();
         mFailureResponseRunnable = new Runnable() {
             @Override
             public void run() {
-                mResultCallback.onClassified(new Result());
+                mResultCallback.onClassified(new SelectionClient.Result());
             }
         };
     }
@@ -156,7 +99,7 @@ public class SmartSelectionProvider {
     @TargetApi(Build.VERSION_CODES.O)
     private void sendSmartSelectionRequest(
             @RequestType int requestType, CharSequence text, int start, int end, Locale[] locales) {
-        TextClassifier classifier = (TextClassifier) getTextClassifier();
+        TextClassifier classifier = getTextClassifier();
         if (classifier == null || classifier == TextClassifier.NO_OP) {
             mHandler.post(mFailureResponseRunnable);
             return;
@@ -173,7 +116,7 @@ public class SmartSelectionProvider {
     }
 
     @TargetApi(Build.VERSION_CODES.O)
-    private class ClassificationTask extends AsyncTask<Void, Void, Result> {
+    private class ClassificationTask extends AsyncTask<Void, Void, SelectionClient.Result> {
         private final TextClassifier mTextClassifier;
         private final int mRequestType;
         private final CharSequence mText;
@@ -192,7 +135,7 @@ public class SmartSelectionProvider {
         }
 
         @Override
-        protected Result doInBackground(Void... params) {
+        protected SelectionClient.Result doInBackground(Void... params) {
             int start = mOriginalStart;
             int end = mOriginalEnd;
 
@@ -201,7 +144,7 @@ public class SmartSelectionProvider {
                         mText, start, end, makeLocaleList(mLocales));
                 start = Math.max(0, suggested.getSelectionStartIndex());
                 end = Math.min(mText.length(), suggested.getSelectionEndIndex());
-                if (isCancelled()) return new Result();
+                if (isCancelled()) return new SelectionClient.Result();
             }
 
             TextClassification tc =
@@ -215,8 +158,8 @@ public class SmartSelectionProvider {
             return new LocaleList(locales);
         }
 
-        private Result makeResult(int start, int end, TextClassification tc) {
-            Result result = new Result();
+        private SelectionClient.Result makeResult(int start, int end, TextClassification tc) {
+            SelectionClient.Result result = new SelectionClient.Result();
 
             result.startAdjust = start - mOriginalStart;
             result.endAdjust = end - mOriginalEnd;
@@ -229,7 +172,7 @@ public class SmartSelectionProvider {
         }
 
         @Override
-        protected void onPostExecute(Result result) {
+        protected void onPostExecute(SelectionClient.Result result) {
             mResultCallback.onClassified(result);
         }
     }
