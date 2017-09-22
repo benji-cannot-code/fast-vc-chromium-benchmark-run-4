0e43ab7edc701b37aad1be20fe3b03a331ada5b5
Thanh Le <thanhdle@chromium.org>
2017-09-22 21:38:36 +0000

    Use mojo to communicate between Javascript and C++.
    Displays basic previews status on chrome://interventions-internals. Right now it
    is only displaying Offline Previews' status. Functionalities and tests for C++ code will be
    in future CLs.
    
    Added tests for the basic JavaScript method for displaying previews'
    enabled/disabled status.
    
    Bug: 764413
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Id943304b28cd6fb64f0e1ed72a12e392d7cfda8e
    Reviewed-on: https://chromium-review.googlesource.com/671499
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Thanh Le <thanhdle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503862}

---
+++ chrome/browser/ui/webui/interventions_internals/interventions_internals_page_handler.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/webui/interventions_internals/interventions_internals_page_handler.h"
+
+#include "components/previews/core/previews_experiments.h"
+
+InterventionsInternalsPageHandler::InterventionsInternalsPageHandler(
+    mojom::InterventionsInternalsPageHandlerRequest request)
+    : binding_(this, std::move(request)) {}
+
+InterventionsInternalsPageHandler::~InterventionsInternalsPageHandler() {}
+
+void InterventionsInternalsPageHandler::GetPreviewsEnabled(
+    GetPreviewsEnabledCallback callback) {
+  // TODO(thanhdle): change enable to a dictionary with all previews mode
+  // status.
+  bool enabled = previews::params::IsOfflinePreviewsEnabled();
+
+  std::move(callback).Run(enabled);
+}
