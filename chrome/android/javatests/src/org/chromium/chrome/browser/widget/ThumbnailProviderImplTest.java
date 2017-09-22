3c74cff71537d80a990f9bf2646b9a1cee7be3f1
Bernhard Bauer <bauerb@chromium.org>
2017-09-22 11:47:11 +0000

    Move ThumbnailProvider etc. to org.chromium.chrome.browser.widget.
    Change-Id: I6d9583249cd5058f556ff5dd22b888814415e263
    Reviewed-on: https://chromium-review.googlesource.com/657426
    Commit-Queue: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503713}

--- chrome/android/javatests/src/org/chromium/chrome/browser/widget/ThumbnailProviderImplTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/widget/ThumbnailProviderImplTest.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.download.ui;
+package org.chromium.chrome.browser.widget;
 
 import android.graphics.Bitmap;
 import android.support.test.filters.MediumTest;
@@ -22,7 +22,7 @@ import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.chrome.browser.ChromeSwitches;
-import org.chromium.chrome.browser.download.ui.ThumbnailProvider.ThumbnailRequest;
+import org.chromium.chrome.browser.widget.ThumbnailProvider.ThumbnailRequest;
 import org.chromium.chrome.test.ChromeActivityTestRule;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
