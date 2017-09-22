a357ad373ee078c295e8304890bae311f6571751
Eduard Satdarov <sath@yandex-team.ru>
2017-09-22 22:33:43 +0000

    Enabling test which seems to be disabled by mistake.
    Bug: 
    Change-Id: I92c496a98d4b9ff097951264f54f0b300224e98f
    Reviewed-on: https://chromium-review.googlesource.com/674871
    Reviewed-by: Nicolas Zea <zea@chromium.org>
    Commit-Queue: Nicolas Zea <zea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503874}

--- chrome/browser/sync/chrome_sync_client_unittest.cc
+++ chrome/browser/sync/chrome_sync_client_unittest.cc
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/sync/chrome_sync_client.h"
 
+#include <memory>
 #include <string>
 
 #include "chrome/common/url_constants.h"
+#include "chrome/test/base/testing_profile.h"
 #include "components/sync/driver/sync_api_component_factory.h"
 #include "components/sync_sessions/sync_sessions_client.h"
 #include "content/public/test/test_browser_thread_bundle.h"
@@ -24,13 +26,15 @@ const char kInvalidUrl[] = "invalid.url";
 class ChromeSyncClientTest : public testing::Test {
  public:
   ChromeSyncClientTest()
-      : sync_client_(new ChromeSyncClient(nullptr, nullptr)) {}
+      : profile_(new TestingProfile()),
+        sync_client_(new ChromeSyncClient(profile_.get())) {}
   ~ChromeSyncClientTest() override {}
 
   ChromeSyncClient* sync_client() { return sync_client_.get(); }
 
  private:
   content::TestBrowserThreadBundle thread_bundle_;
+  std::unique_ptr<TestingProfile> profile_;
   std::unique_ptr<ChromeSyncClient> sync_client_;
 };
 
