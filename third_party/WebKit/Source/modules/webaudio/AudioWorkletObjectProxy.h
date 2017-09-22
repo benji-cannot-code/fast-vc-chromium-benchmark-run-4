b0a6bb122bd00669384a15efcd6a8cd486f5adc4
Hongchan Choi <hongchan@chromium.org>
2017-09-22 16:30:24 +0000

    Remove MODULES_EXPORT from AudioWorkletObjectProxy
    AudioWorkletObjectProxy should not be exposed out of "modules" scope
    because it is not used outside of WebAudio.
    
    Bug: None
    Change-Id: I2e3e11e6c9f43c94a811b795381b24a857d4d213
    Reviewed-on: https://chromium-review.googlesource.com/676252
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503763}

--- third_party/WebKit/Source/modules/webaudio/AudioWorkletObjectProxy.h
+++ third_party/WebKit/Source/modules/webaudio/AudioWorkletObjectProxy.h
@@ -7,14 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define AudioWorkletObjectProxy_h
 
 #include "core/workers/ThreadedWorkletObjectProxy.h"
-#include "modules/ModulesExport.h"
 
 namespace blink {
 
 class AudioWorkletGlobalScope;
 class AudioWorkletMessagingProxy;
 
-class MODULES_EXPORT AudioWorkletObjectProxy final
+class AudioWorkletObjectProxy final
     : public ThreadedWorkletObjectProxy {
  public:
   AudioWorkletObjectProxy(AudioWorkletMessagingProxy*,
