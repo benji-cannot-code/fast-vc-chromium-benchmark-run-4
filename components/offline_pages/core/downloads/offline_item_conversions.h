d7b8336ea9967f2edbbad6b1bd5f0a64f38a52d7
Cathy Li <chili@chromium.org>
2017-09-22 15:45:11 +0000

    [Offline pages] Pass correct is_suggested bit to OfflineItem.
    Bug: 701939
    Change-Id: Id4b95b57b4ff0ab6995dd40796f1b2c21ef72d57
    Reviewed-on: https://chromium-review.googlesource.com/678237
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503744}

--- components/offline_pages/core/downloads/offline_item_conversions.h
+++ components/offline_pages/core/downloads/offline_item_conversions.h
@@ -22,7 +22,8 @@ class SavePageRequest;
 // collection representation (for displaying in UI).
 class OfflineItemConversions {
  public:
-  static OfflineItem CreateOfflineItem(const OfflinePageItem& page);
+  static OfflineItem CreateOfflineItem(const OfflinePageItem& page,
+                                       bool is_suggested);
   static OfflineItem CreateOfflineItem(const SavePageRequest& request);
 
  private:
