46bc8924621f2a9c6a3ae7206d7118ec43528605
Bailey Forrest <bcf@chromium.org>
2017-09-23 00:39:29 +0000

    [Chromecast] Add MediaPipelineBackend::BufferDelegate
    This allows CMA media streams to be redirected.
    
    BUG=b/64231556
    TEST=Build and run on device.
    
    Change-Id: I06b52e4a88e9544e84eaba66ce8db83598beebaa
    Reviewed-on: https://chromium-review.googlesource.com/677566
    Reviewed-by: Stephen Lanham <slan@chromium.org>
    Commit-Queue: Bailey Forrest <bcf@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503917}

--- chromecast/media/cma/backend/audio_decoder_wrapper.h
+++ chromecast/media/cma/backend/audio_decoder_wrapper.h
@@ -7,19 +7,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROMECAST_MEDIA_CMA_BACKEND_AUDIO_DECODER_WRAPPER_H_
 
 #include "base/macros.h"
+#include "chromecast/media/cma/backend/media_pipeline_backend_manager.h"
 #include "chromecast/public/media/media_pipeline_backend.h"
 
 namespace chromecast {
 namespace media {
 
 enum class AudioContentType;
-class MediaPipelineBackendManager;
 
 class AudioDecoderWrapper : public MediaPipelineBackend::AudioDecoder {
  public:
-  AudioDecoderWrapper(MediaPipelineBackendManager* backend_manager,
-                      MediaPipelineBackend::AudioDecoder* decoder,
-                      AudioContentType type);
+  AudioDecoderWrapper(
+      MediaPipelineBackendManager* backend_manager,
+      MediaPipelineBackend::AudioDecoder* decoder,
+      AudioContentType type,
+      MediaPipelineBackendManager::BufferDelegate* buffer_delegate);
   ~AudioDecoderWrapper() override;
 
   void SetGlobalVolumeMultiplier(float multiplier);
@@ -38,6 +40,9 @@ class AudioDecoderWrapper : public MediaPipelineBackend::AudioDecoder {
   MediaPipelineBackendManager* const backend_manager_;
   MediaPipelineBackend::AudioDecoder* const decoder_;
   const AudioContentType content_type_;
+
+  MediaPipelineBackendManager::BufferDelegate* const buffer_delegate_;
+  bool delegate_active_;
 
   float global_volume_multiplier_;
   float stream_volume_multiplier_;
