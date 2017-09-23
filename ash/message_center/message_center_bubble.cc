6a368a3e739bfd02f0eae387700b1ec867b90576
Evan Stade <estade@chromium.org>
2017-09-23 00:02:26 +0000

    Reland 69b1a3dd0c9f3fbff: Move some message center views... Move some message center views from ui/message_center to ash/.
    These are some classes that are only used in the message center (not in
    standalone notifications). Thus they're only used in ash/, even though
    we've been compiling them on all platforms.
    
    There are probably more classes than can move and certainly some
    constants as well; this is just a first step.
    
    TBR=jamescook@chromium.org,stevenjb@chromium.org,oshima@chromium.org
    
    Bug: 585175, 755413
    Change-Id: I3a47b69c31061d92d98b4d880708c50688c415f9
    Reviewed-on: https://chromium-review.googlesource.com/679486
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503912}

--- ash/message_center/message_center_bubble.cc
+++ ash/message_center/message_center_bubble.cc
@@ -3,19 +3,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ash/system/web_notification/message_center_bubble.h"
+#include "ash/message_center/message_center_bubble.h"
 
+#include "ash/message_center/message_center_view.h"
 #include "base/macros.h"
 #include "ui/message_center/message_center.h"
 #include "ui/message_center/public/cpp/message_center_constants.h"
-#include "ui/message_center/views/message_center_view.h"
 #include "ui/views/bubble/tray_bubble_view.h"
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/widget/widget.h"
 
 using message_center::MessageCenter;
 using message_center::MessageCenterTray;
-using message_center::MessageCenterView;
 
 namespace ash {
 
