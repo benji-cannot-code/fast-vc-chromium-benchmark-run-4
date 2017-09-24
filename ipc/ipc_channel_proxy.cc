0e842c487caf6c091990cb162ca9164ae14569fd
Brian White <bcwhite@chromium.org>
2017-09-24 04:33:45 +0000

    Validate histograms around IPC calls on Android.
    This is an extension of cl/655922 that (temporarily) adds
    instrumentation to Android builds to try to determine the source
    of a core trampler.
    
    TBR=rockot
    
    Bug: 744734
    Change-Id: I1a4e33cd07fd6fb2285fa868b118420833d49922
    Reviewed-on: https://chromium-review.googlesource.com/671286
    Commit-Queue: Brian White <bcwhite@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Carlos Pizano <cpu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503951}

--- ipc/ipc_channel_proxy.cc
+++ ipc/ipc_channel_proxy.cc
@@ -25,6 +25,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/message_filter.h"
 #include "ipc/message_filter_router.h"
 
+// This is temporary to try to locate a core trampler.
+// TODO(bcwhite):  Remove when crbug/736675 is resolved.
+#if defined(OS_ANDROID)
+#include "base/metrics/statistics_recorder.h"
+#define VALIDATE_ALL_HISTOGRAMS(x) \
+  base::StatisticsRecorder::ValidateAllHistograms(static_cast<int>(x))
+#else
+#define VALIDATE_ALL_HISTOGRAMS(x)
+#endif
+
 namespace IPC {
 
 //------------------------------------------------------------------------------
@@ -305,6 +315,8 @@ void ChannelProxy::Context::OnDispatchMessage(const Message& message) {
   if (!listener_)
     return;
 
+  VALIDATE_ALL_HISTOGRAMS(message.type());
+
   OnDispatchConnected();
 
 #if BUILDFLAG(IPC_MESSAGE_LOG_ENABLED)
@@ -318,9 +330,11 @@ void ChannelProxy::Context::OnDispatchMessage(const Message& message) {
     logger->OnPreDispatchMessage(message);
 #endif
 
+  VALIDATE_ALL_HISTOGRAMS(message.type());
   listener_->OnMessageReceived(message);
   if (message.dispatch_error())
     listener_->OnBadMessageReceived(message);
+  VALIDATE_ALL_HISTOGRAMS(message.type());
 
 #if BUILDFLAG(IPC_MESSAGE_LOG_ENABLED)
   if (logger->Enabled())
