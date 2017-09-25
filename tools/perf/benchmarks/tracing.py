30933b6ddc840c3d8575eba7d0efa5a575ae38a6
rnephew <rnephew@chromium.org>
2017-09-25 18:54:02 +0000

    [Telemetry] Add presubmit check to ensure no one disables benchmarks with decorators.
    Bug: 713222
    Change-Id: I9d13330ef71bd6bfb27232e6472b35b404b19b0a
    Reviewed-on: https://chromium-review.googlesource.com/682109
    Commit-Queue: rnephew <rnephew@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#504114}

--- tools/perf/benchmarks/tracing.py
+++ tools/perf/benchmarks/tracing.py
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from core import perf_benchmark
 
 from telemetry import benchmark
-from telemetry import decorators
 from telemetry import story
 from telemetry.timeline import chrome_trace_category_filter
 from telemetry.timeline import chrome_trace_config
@@ -16,7 +15,6 @@ import page_sets
 
 
 @benchmark.Owner(emails=['ssid@chromium.org'])
-@decorators.Disabled('all')  # crbug.com/765140
 class TracingWithBackgroundMemoryInfra(perf_benchmark.PerfBenchmark):
   """Measures the overhead of background memory-infra dumps"""
   page_set = page_sets.Top10PageSet
@@ -40,5 +38,5 @@ class TracingWithBackgroundMemoryInfra(perf_benchmark.PerfBenchmark):
   def GetExpectations(self):
     class StoryExpectations(story.expectations.StoryExpectations):
       def SetExpectations(self):
-        pass # No tests disabled.
+        self.DisableBenchmark([story.expectations.ALL], 'crbug.com/765140')
     return StoryExpectations()
