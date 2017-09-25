733fdb775757ca68511010647620a6f05ba1593c
tzik <tzik@chromium.org>
2017-09-25 06:34:13 +0000

    Use qualified name for WTF::AdoptRef
    This CL replaces bare AdoptRef with WTF::AdoptRef, so that it doesn't
    conflict with base::AdoptRef.
    
    Bug: 763844
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I016799673aeec4a81ffd973fc4708e135568c04f
    Reviewed-on: https://chromium-review.googlesource.com/672305
    Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503989}

--- third_party/WebKit/Source/platform/wtf/HashMapTest.cpp
+++ third_party/WebKit/Source/platform/wtf/HashMapTest.cpp
@@ -168,7 +168,7 @@ int DummyRefCounted::ref_invokes_count_ = 0;
 TEST(HashMapTest, RefPtrAsKey) {
   bool is_deleted = false;
   DummyRefCounted::ref_invokes_count_ = 0;
-  RefPtr<DummyRefCounted> ptr = AdoptRef(new DummyRefCounted(is_deleted));
+  RefPtr<DummyRefCounted> ptr = WTF::AdoptRef(new DummyRefCounted(is_deleted));
   EXPECT_EQ(0, DummyRefCounted::ref_invokes_count_);
   HashMap<RefPtr<DummyRefCounted>, int> map;
   map.insert(ptr, 1);
@@ -200,7 +200,7 @@ TEST(HashMaptest, RemoveAdd) {
   typedef HashMap<int, RefPtr<DummyRefCounted>> Map;
   Map map;
 
-  RefPtr<DummyRefCounted> ptr = AdoptRef(new DummyRefCounted(is_deleted));
+  RefPtr<DummyRefCounted> ptr = WTF::AdoptRef(new DummyRefCounted(is_deleted));
   EXPECT_EQ(0, DummyRefCounted::ref_invokes_count_);
 
   map.insert(1, ptr);
@@ -219,7 +219,8 @@ TEST(HashMaptest, RemoveAdd) {
   // Add and remove until the deleted slot is reused.
   for (int i = 1; i < 100; i++) {
     bool is_deleted2 = false;
-    RefPtr<DummyRefCounted> ptr2 = AdoptRef(new DummyRefCounted(is_deleted2));
+    RefPtr<DummyRefCounted> ptr2 =
+        WTF::AdoptRef(new DummyRefCounted(is_deleted2));
     map.insert(i, ptr2);
     EXPECT_FALSE(is_deleted2);
     ptr2.Clear();
