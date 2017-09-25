2704ec6ce7685008d77d70a8800e0f172b38aac7
Matt Menke <mmenke@chromium.org>
2017-09-25 23:52:26 +0000

    Remove use of g_browser_process->net_log() in CastTransportHostFilter.
    I'm removing all use of NetLog except through URLRequestContext, as part
    of servicification. Eventually URLRequestContexts, will be out of
    process, too, but that's quite a ways off.
    
    Bug: 767450
    Change-Id: If905f1cf7f5737ecb55f5b1ac9433ed4f14a85c8
    Reviewed-on: https://chromium-review.googlesource.com/677232
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504219}

--- chrome/browser/media/cast_transport_host_filter_unittest.cc
+++ chrome/browser/media/cast_transport_host_filter_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/default_tick_clock.h"
 #include "chrome/browser/media/cast_transport_host_filter.h"
 #include "chrome/common/cast_messages.h"
+#include "chrome/test/base/testing_profile.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "media/cast/logging/logging_defines.h"
 #include "net/base/ip_address.h"
@@ -22,7 +23,7 @@ class CastTransportHostFilterTest : public testing::Test {
   CastTransportHostFilterTest()
       : browser_thread_bundle_(
             content::TestBrowserThreadBundle::IO_MAINLOOP) {
-    filter_ = new cast::CastTransportHostFilter();
+    filter_ = new cast::CastTransportHostFilter(&profile_);
     static_cast<cast::CastTransportHostFilter*>(filter_.get())
         ->InitializeNoOpWakeLockForTesting();
     // 127.0.0.1:7 is the local echo service port, which
@@ -39,6 +40,7 @@ class CastTransportHostFilterTest : public testing::Test {
 
   base::DictionaryValue options_;
   content::TestBrowserThreadBundle browser_thread_bundle_;
+  TestingProfile profile_;
   scoped_refptr<content::BrowserMessageFilter> filter_;
   net::IPEndPoint receive_endpoint_;
 };
