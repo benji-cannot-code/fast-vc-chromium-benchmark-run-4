80c099c4b34cbeb409734477084dddf86ef4652c
Hector Carmona <hcarmona@chromium.org>
2017-09-25 17:15:36 +0000

    Revert "Make new NTLM implementation live."
    This reverts commit 25a20e235e2ace5ac6dea7e138c1a174ec018c9b.
    
    Reason for revert: Speculative revert to make tree green again.
    See: https://build.chromium.org/p/chromium.win/builders/Win%20x64%20Builder%20%28dbg%29/builds/58540
    
    Original change's description:
    > Make new NTLM implementation live.
    > 
    > - Passes all unit tests of previous implementation with minor exception
    > - New implementation points empty security buffers at end of header
    >   rather than 0 to follow the recommendation of the spec.
    > 
    > BUG=chromium:22532
    > 
    > Change-Id: I3e7617f512af70600b5c7e03be2dd8a7936a5bbd
    > Reviewed-on: https://chromium-review.googlesource.com/598309
    > Commit-Queue: Zentaro Kavanagh <zentaro@google.com>
    > Reviewed-by: Asanka Herath <asanka@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#504077}
    
    TBR=rsleevi@chromium.org,zentaro@google.com,asanka@chromium.org
    
    Change-Id: I2eb8da3634d096298b6d18f8dd5ae865817ce3b0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:22532
    Reviewed-on: https://chromium-review.googlesource.com/682274
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Commit-Queue: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504083}

--- net/http/http_auth_handler_ntlm.h
+++ net/http/http_auth_handler_ntlm.h
@@ -24,8 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 #include <security.h>
 #include "net/http/http_auth_sspi_win.h"
-#elif defined(NTLM_PORTABLE)
-#include "net/ntlm/ntlm_client.h"
 #endif
 
 #include <string>
@@ -137,23 +135,24 @@ class NET_EXPORT_PRIVATE HttpAuthHandlerNTLM : public HttpAuthHandler {
   // Returns the old function.
   static GenerateRandomProc SetGenerateRandomProc(GenerateRandomProc proc);
   static HostNameProc SetHostNameProc(HostNameProc proc);
-
-  // Given an input token received from the server, generate the next output
-  // token to be sent to the server.
-  ntlm::Buffer GetNextToken(const ntlm::Buffer& in_token);
 #endif
 
   // Parse the challenge, saving the results into this instance.
   HttpAuth::AuthorizationResult ParseChallenge(
       HttpAuthChallengeTokenizer* tok, bool initial_challenge);
 
+  // Given an input token received from the server, generate the next output
+  // token to be sent to the server.
+  int GetNextToken(const void* in_token,
+                   uint32_t in_token_len,
+                   void** out_token,
+                   uint32_t* out_token_len);
+
   // Create an NTLM SPN to identify the |origin| server.
   static std::string CreateSPN(const GURL& origin);
 
 #if defined(NTLM_SSPI)
   HttpAuthSSPI auth_sspi_;
-#elif defined(NTLM_PORTABLE)
-  ntlm::NtlmClient ntlm_client_;
 #endif
 
 #if defined(NTLM_PORTABLE)
