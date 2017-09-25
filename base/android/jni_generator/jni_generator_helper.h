e6d37fbfe14b880c86ff7278c0fd08a028785fb7
Magnus Jedvert <magjed@chromium.org>
2017-09-25 20:00:46 +0000

    Android: Generalize JNI generation script so it can be used from WebRTC
    In order to use the JNI generation script from WebRTC, we need to remove
    any hardcoded includes of Chromium files.
    
    Bug: webrtc:8278
    Change-Id: I4dd27c82a7957986e92c6db380479e44b49bb69e
    Reviewed-on: https://chromium-review.googlesource.com/681938
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Magnus Jedvert <magjed@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504143}

--- base/android/jni_generator/jni_generator_helper.h
+++ base/android/jni_generator/jni_generator_helper.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <jni.h>
 
 #include "base/android/jni_android.h"
+#include "base/android/jni_int_wrapper.h"
 #include "base/android/scoped_java_ref.h"
 #include "base/logging.h"
 #include "build/build_config.h"
