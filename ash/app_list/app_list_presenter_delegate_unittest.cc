146657583a7107ed3af015f650fe6629fe91ce67
Alex Newcomer <newcomer@chromium.org>
2017-09-25 21:29:24 +0000

    cros: Don't allow launcher to be dragged below shelf height
    This change ensures the launcher cant be dragged below the shelf when
    the drag originates on the AppListView.
    
    Bug: 765522
    Change-Id: I8101f8f5568ff72cf91013199fe7c44438a2c679
    Reviewed-on: https://chromium-review.googlesource.com/679855
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504169}

--- ash/app_list/app_list_presenter_delegate_unittest.cc
+++ ash/app_list/app_list_presenter_delegate_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/shelf/shelf.h"
 #include "ash/shelf/shelf_layout_manager.h"
+#include "ash/shelf/shelf_view.h"
 #include "ash/shell.h"
 #include "ash/shell_port.h"
 #include "ash/test/ash_test_base.h"
@@ -1051,6 +1052,26 @@ TEST_P(FullscreenAppListPresenterDelegateTest,
   else
     EXPECT_EQ(app_list::AppListView::FULLSCREEN_ALL_APPS,
               view->app_list_state());
+}
+
+// Tests that a drag can not make the app list smaller than the shelf height.
+TEST_F(FullscreenAppListPresenterDelegateTest,
+       LauncherCannotGetSmallerThanShelf) {
+  app_list_presenter_impl()->Show(GetPrimaryDisplayId());
+  app_list::AppListView* view = app_list_presenter_impl()->GetView();
+
+  // Try to place the app list 1 px below the shelf, it should stay at shelf
+  // height.
+  int target_y = GetPrimaryShelf()
+                     ->GetShelfViewForTesting()
+                     ->GetBoundsInScreen()
+                     .top_right()
+                     .y();
+  const int expected_app_list_y = target_y;
+  target_y += 1;
+  view->UpdateYPositionAndOpacity(target_y, 1);
+
+  EXPECT_EQ(expected_app_list_y, view->GetBoundsInScreen().top_right().y());
 }
 
 }  // namespace ash
