1dff6f729370f532867b79feb6c8c62dca757f40
Pedro Amaral <amaralp@chromium.org>
2017-09-25 23:47:17 +0000

    Start/End handles can have the same direction in mixed LTR/RTL selection
    If we have Arabic text in one line and English text in the second line
    and select both lines then both touch handles will be right handles
    (for RTL reasons). A previous CL (crrev.com/c/658599) assumed that
    each handle had to have a different direction. This CL removes that
    assumption.
    
    Bug: 766466
    Change-Id: Icdb725cc2cf21f593e90d5d90acbd2a489dbca1d
    Reviewed-on: https://chromium-review.googlesource.com/678036
    Reviewed-by: Mohsen Izadi <mohsen@chromium.org>
    Reviewed-by: Pedro Amaral <amaralp@chromium.org>
    Commit-Queue: Pedro Amaral <amaralp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504215}

--- ui/touch_selection/touch_selection_controller.cc
+++ ui/touch_selection/touch_selection_controller.cc
@@ -110,9 +110,9 @@ void TouchSelectionController::OnSelectionBoundsChanged(
   base::AutoReset<InputEventType> auto_reset_response_pending_input_event(
       &response_pending_input_event_, causal_input_event);
 
-  if ((start_orientation_ == TouchHandleOrientation::LEFT &&
-       end_orientation_ == TouchHandleOrientation::RIGHT) ||
-      (start_orientation_ == TouchHandleOrientation::RIGHT &&
+  if ((start_orientation_ == TouchHandleOrientation::LEFT ||
+       start_orientation_ == TouchHandleOrientation::RIGHT) &&
+      (end_orientation_ == TouchHandleOrientation::RIGHT ||
        end_orientation_ == TouchHandleOrientation::LEFT)) {
     OnSelectionChanged();
     return;
