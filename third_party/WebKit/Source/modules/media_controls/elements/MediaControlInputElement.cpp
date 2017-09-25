2c0c1554bcc52c955450938992e4bb3c0056e82f
Becca Hughes <beccahughes@chromium.org>
2017-09-25 17:45:18 +0000

    Media Controls: Replace painter with CSS.
    Replace MediaControlPainter with CSS. Add some classes to some
    elements to allow styling by state. This is a resubmit of the
    button code only from crrev.com/c/584771.
    
    BUG=746872
    
    TBR=pfeldman@chromium.org
    
    Change-Id: If79ce339a10f6671ddd02a223468bb3d7bbcd1df
    Reviewed-on: https://chromium-review.googlesource.com/657024
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504095}

--- third_party/WebKit/Source/modules/media_controls/elements/MediaControlInputElement.cpp
+++ third_party/WebKit/Source/modules/media_controls/elements/MediaControlInputElement.cpp
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "modules/media_controls/elements/MediaControlInputElement.h"
 
+#include "core/dom/DOMTokenList.h"
 #include "core/dom/events/Event.h"
 #include "core/html/HTMLLabelElement.h"
 #include "core/html/HTMLMediaElement.h"
@@ -177,6 +178,14 @@ void MediaControlInputElement::RecordCTREvent(CTREvent event) {
   EnumerationHistogram ctr_histogram(histogram_name.Ascii().data(),
                                      static_cast<int>(CTREvent::kCount));
   ctr_histogram.Count(static_cast<int>(event));
+}
+
+void MediaControlInputElement::SetClass(const AtomicString& class_name,
+                                        bool should_have_class) {
+  if (should_have_class)
+    classList().Add(class_name);
+  else
+    classList().Remove(class_name);
 }
 
 DEFINE_TRACE(MediaControlInputElement) {
--- third_party/WebKit/Source/modules/media_controls/elements/MediaControlPlayButtonElement.cpp
+++ third_party/WebKit/Source/modules/media_controls/elements/MediaControlPlayButtonElement.cpp
@@ -29,6 +29,7 @@ bool MediaControlPlayButtonElement::WillRespondToMouseClickEvents() {
 void MediaControlPlayButtonElement::UpdateDisplayType() {
   SetDisplayType(MediaElement().paused() ? kMediaPlayButton
                                          : kMediaPauseButton);
+  SetClass("pause", MediaElement().paused());
   UpdateOverflowString();
 }
 
