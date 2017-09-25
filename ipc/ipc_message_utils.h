1584a3d33b7cf0e1cd221a3e687232c42078b217
Chris Palmer <palmer@chromium.org>
2017-09-25 22:53:22 +0000

    Use checked_cast instead of static_cast when casting sizes in Pickle.
    These functions should arguably have been defined to take uint32_t all along.
    Use checked_cast to see if signedness or width bugs have been lurking in the
    shadows.
    
    Bug: None
    Change-Id: Ieef9244f1ecf8e72b7dc4d5e85a42007b145d932
    Reviewed-on: https://chromium-review.googlesource.com/676694
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Chris Palmer <palmer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504195}

--- ipc/ipc_message_utils.h
+++ ipc/ipc_message_utils.h
@@ -366,7 +366,7 @@ template <class P>
 struct ParamTraits<std::vector<P>> {
   typedef std::vector<P> param_type;
   static void Write(base::Pickle* m, const param_type& p) {
-    WriteParam(m, static_cast<int>(p.size()));
+    WriteParam(m, base::checked_cast<int>(p.size()));
     for (size_t i = 0; i < p.size(); i++)
       WriteParam(m, p[i]);
   }
@@ -400,7 +400,7 @@ template <class P>
 struct ParamTraits<std::set<P> > {
   typedef std::set<P> param_type;
   static void Write(base::Pickle* m, const param_type& p) {
-    WriteParam(m, static_cast<int>(p.size()));
+    WriteParam(m, base::checked_cast<int>(p.size()));
     typename param_type::const_iterator iter;
     for (iter = p.begin(); iter != p.end(); ++iter)
       WriteParam(m, *iter);
@@ -428,7 +428,7 @@ template <class K, class V, class C, class A>
 struct ParamTraits<std::map<K, V, C, A> > {
   typedef std::map<K, V, C, A> param_type;
   static void Write(base::Pickle* m, const param_type& p) {
-    WriteParam(m, static_cast<int>(p.size()));
+    WriteParam(m, base::checked_cast<int>(p.size()));
     typename param_type::const_iterator iter;
     for (iter = p.begin(); iter != p.end(); ++iter) {
       WriteParam(m, iter->first);
@@ -696,7 +696,7 @@ template <class P, size_t stack_capacity>
 struct ParamTraits<base::StackVector<P, stack_capacity> > {
   typedef base::StackVector<P, stack_capacity> param_type;
   static void Write(base::Pickle* m, const param_type& p) {
-    WriteParam(m, static_cast<int>(p->size()));
+    WriteParam(m, base::checked_cast<int>(p->size()));
     for (size_t i = 0; i < p->size(); i++)
       WriteParam(m, p[i]);
   }
@@ -736,7 +736,7 @@ struct ParamTraits<base::small_map<NormalMap, kArraySize, EqualKey, MapInit>> {
   using K = typename param_type::key_type;
   using V = typename param_type::data_type;
   static void Write(base::Pickle* m, const param_type& p) {
-    WriteParam(m, static_cast<int>(p.size()));
+    WriteParam(m, base::checked_cast<int>(p.size()));
     typename param_type::const_iterator iter;
     for (iter = p.begin(); iter != p.end(); ++iter) {
       WriteParam(m, iter->first);
@@ -769,7 +769,7 @@ struct ParamTraits<base::flat_map<Key, Mapped, Compare>> {
   using param_type = base::flat_map<Key, Mapped, Compare>;
   static void Write(base::Pickle* m, const param_type& p) {
     DCHECK(base::IsValueInRangeForNumericType<int>(p.size()));
-    WriteParam(m, static_cast<int>(p.size()));
+    WriteParam(m, base::checked_cast<int>(p.size()));
     for (const auto& iter : p) {
       WriteParam(m, iter.first);
       WriteParam(m, iter.second);
