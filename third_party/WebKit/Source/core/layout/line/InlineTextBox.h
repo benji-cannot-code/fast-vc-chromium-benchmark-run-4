1e15c2b986feae5b612778b3d782e1b29bf964ad
Emil A Eklund <eae@chromium.org>
2017-09-25 05:44:14 +0000

    Move truncation constants into Truncation.h
    Move the truncation constants used for ellipsis truncation to new file,
    Truncation.h, to allow sharing it across LayoutNG and legacy layout.
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Iea648c46e45b02279a237dc068986b5879d14fed
    Reviewed-on: https://chromium-review.googlesource.com/680714
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503985}

--- third_party/WebKit/Source/core/layout/line/InlineTextBox.h
+++ third_party/WebKit/Source/core/layout/line/InlineTextBox.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/api/SelectionState.h"
 #include "core/layout/line/InlineBox.h"
 #include "platform/text/TextRun.h"
+#include "platform/text/Truncation.h"
 #include "platform/wtf/Forward.h"
 
 namespace blink {
@@ -37,18 +38,6 @@ namespace blink {
 class DocumentMarker;
 class GraphicsContext;
 class TextMatchMarker;
-
-// The two truncation values below are used as tokens representing truncation
-// state for the text box, are intended to be relative to |m_start|, and are set
-// directly into |m_truncation|. In the case where there is some truncation of
-// the text but it is not full, |m_truncation| is set to the character offset
-// from |m_start| representing the characters that are not truncated.
-//
-// Thus the maximum possible length of the text displayed before an ellipsis in
-// a single InlineTextBox is |USHRT_MAX - 2| to allow for the no-truncation and
-// full-truncation states.
-const unsigned short kCNoTruncation = USHRT_MAX;
-const unsigned short kCFullTruncation = USHRT_MAX - 1;
 
 class CORE_EXPORT InlineTextBox : public InlineBox {
  public:
--- third_party/WebKit/Source/core/paint/ng/ng_text_fragment_painter.cc
+++ third_party/WebKit/Source/core/paint/ng/ng_text_fragment_painter.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/style/AppliedTextDecoration.h"
 #include "core/style/ComputedStyle.h"
 #include "platform/graphics/GraphicsContextStateSaver.h"
+#include "platform/text/Truncation.h"
 
 namespace blink {
 
@@ -45,12 +46,6 @@ static void PaintDecorationsExceptLineThrough(
 void NGTextFragmentPainter::Paint(const Document& document,
                                   const PaintInfo& paint_info,
                                   const LayoutPoint& paint_offset) {
-  // TODO(eae): These constants are currently defined in core/layout/line/
-  // InlineTextBox.h, move them to a separate header and have InlineTextBox.h
-  // and this file include it.
-  static unsigned short kCNoTruncation = USHRT_MAX;
-  static unsigned short kCFullTruncation = USHRT_MAX - 1;
-
   const ComputedStyle& style_to_use = fragment_.Style();
 
   NGPhysicalSize size_;
