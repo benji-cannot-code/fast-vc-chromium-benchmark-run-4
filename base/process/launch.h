947dd37fcc03fd887fa67d3beee898b42c2f8029
Kevin Marshall <kmarshall@chromium.org>
2017-09-25 18:21:42 +0000

    Fuchsia: implement sandbox support via Fuchsia launch clone policy.
    This CL adds a function "GetClonePolicyForSandbox()" which returns
    the set of clone flags needed to achieve the level of isolation
    appropriate for a SandboxType.
    
    Calling LaunchProcess() without setting the clone policy will result
    in the child process being spawned with the same capabilities as its
    parent, so as to not break the existing call sites of LaunchProcess().
    
    
    Bug: 750938
    Change-Id: I09033892246a93322970b24ab79d107b103c1671
    Reviewed-on: https://chromium-review.googlesource.com/628861
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504107}

--- base/process/launch.h
+++ base/process/launch.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(OS_FUCHSIA)
+#include <launchpad/launchpad.h>
 #include <zircon/types.h>
 #endif
 
@@ -199,6 +200,15 @@ struct BASE_EXPORT LaunchOptions {
   // Each entry is an <id,handle> pair, with an |id| created using the PA_HND()
   // macro. The child retrieves the handle |zx_get_startup_handle(id)|.
   HandlesToTransferVector handles_to_transfer;
+
+  // If set, specifies which capabilities should be granted (cloned) to the
+  // child process.
+  // A zero value indicates that the child process will receive
+  // no capabilities.
+  // By default the child will inherit the same capabilities, job, and CWD
+  // from the parent process.
+  uint32_t clone_flags = LP_CLONE_FDIO_NAMESPACE | LP_CLONE_DEFAULT_JOB |
+                         LP_CLONE_FDIO_CWD | LP_CLONE_FDIO_STDIO;
 #endif  // defined(OS_FUCHSIA)
 
 #if defined(OS_POSIX)
--- content/browser/child_process_launcher_helper_fuchsia.cc
+++ content/browser/child_process_launcher_helper_fuchsia.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/process/launch.h"
 #include "content/browser/child_process_launcher.h"
+#include "content/common/sandbox_policy_fuchsia.h"
+#include "content/public/common/sandboxed_process_launcher_delegate.h"
 #include "mojo/edk/embedder/platform_channel_pair.h"
 
 namespace content {
@@ -75,6 +77,8 @@ void ChildProcessLauncherHelper::BeforeLaunchOnLauncherThread(
 
   mojo::edk::PlatformChannelPair::PrepareToPassHandleToChildProcess(
       mojo_client_handle(), command_line(), &options->handles_to_transfer);
+
+  UpdateLaunchOptionsForSandbox(delegate_->GetSandboxType(), options);
 }
 
 ChildProcessLauncherHelper::Process
