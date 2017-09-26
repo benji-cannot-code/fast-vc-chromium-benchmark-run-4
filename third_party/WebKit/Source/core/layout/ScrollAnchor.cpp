2e6d2d3867982400d05e299739cc0932185ad88c
David Grogan <dgrogan@chromium.org>
2017-09-26 03:58:21 +0000

    [css] Fix scroll anchor X dirty table layout crash
    When an element is selected as scroll anchor, we need to get its
    bounding box. But when a table has unlaid out table parts, we DCHECK
    that its borders aren't being queried. When a table with unlaid out
    table parts is the scroll anchor, the DCHECK was triggered.
    
    The "fix" in this CL is to use the old border widths when scroll
    anchoring code is looking for an anchor.
    
    Bug: 746570
    Change-Id: I424e3807c2ed7a7a20f02617e2e0586dba83e708
    Reviewed-on: https://chromium-review.googlesource.com/679994
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: David Grogan <dgrogan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504279}

--- third_party/WebKit/Source/core/layout/ScrollAnchor.cpp
+++ third_party/WebKit/Source/core/layout/ScrollAnchor.cpp
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/LocalFrameView.h"
 #include "core/frame/UseCounter.h"
 #include "core/layout/LayoutBlockFlow.h"
+#include "core/layout/LayoutTable.h"
 #include "core/layout/api/LayoutBoxItem.h"
 #include "core/layout/line/InlineTextBox.h"
 #include "core/paint/PaintLayer.h"
@@ -247,6 +248,11 @@ void ScrollAnchor::NotifyBeforeLayout() {
   }
 
   if (!anchor_object_) {
+    // FindAnchor() and ComputeRelativeOffset() query a box's borders as part of
+    // its geometry. But when collapsed, table borders can depend on internal
+    // parts, which get sorted during a layout pass. When a table with dirty
+    // internal structure is checked as an anchor candidate, a DCHECK was hit.
+    LayoutTable::DisableUpdatingCollapsedBorders scoped_disabler;
     FindAnchor();
     if (!anchor_object_)
       return;
