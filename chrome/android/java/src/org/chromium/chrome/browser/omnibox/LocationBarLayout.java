448a1aca797420ee190609a0ba0d3aa5faad379a
Lei Tian <ltian@google.com>
2017-09-26 21:30:06 +0000

    Change logic for deciding whether emphasizing the https scheme.
    Both LocationBarLayout and CustomTabToolbar will emphasize the https
    scheme if the toolbar uses the defualt color theme.
    
    BUG=726565
    
    Change-Id: I9d9ed97a1a245cf8262fa43008cee5184e1657ca
    Reviewed-on: https://chromium-review.googlesource.com/676199
    Commit-Queue: Lei Tian <ltian@google.com>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504489}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
@@ -1404,7 +1404,9 @@ public class LocationBarLayout extends FrameLayout
 
     @Override
     public boolean shouldEmphasizeHttpsScheme() {
-        if (mToolbarDataProvider.isUsingBrandColor() || mToolbarDataProvider.isIncognito()) {
+        if (!ColorUtils.isUsingDefaultToolbarColor(
+                    getResources(), mToolbarDataProvider.getPrimaryColor())
+                || mToolbarDataProvider.isIncognito()) {
             return false;
         }
         return true;
