84b895f33f3e6c57be95821547c9d2d5d4903c0d
Khushal <khushalsagar@chromium.org>
2017-09-26 05:28:55 +0000

    cc/blink: Plumb resetting of image animations from blink to cc.
    The animation can be reset to start from the beginning in blink. Plumb
    this to cc to replicate this behaviour if image animations are being
    ticked by the compositor. This uses a counter sequence incremented on
    the main thread each time the animation is reset to act as a
    synchronization point, which is necessary since recordings can be
    cached and resued by blink.
    
    Bug: 735743
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I7ff6fe22b1c5179a1ea8ec893f5d239895a4d2a4
    Reviewed-on: https://chromium-review.googlesource.com/664245
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504290}

--- cc/paint/discardable_image_map.h
+++ cc/paint/discardable_image_map.h
@@ -32,10 +32,12 @@ class PaintOpBuffer;
 class CC_PAINT_EXPORT DiscardableImageMap {
  public:
   struct CC_PAINT_EXPORT AnimatedImageMetadata {
-    AnimatedImageMetadata(PaintImage::Id paint_image_id,
-                          PaintImage::CompletionState completion_state,
-                          std::vector<FrameMetadata> frames,
-                          int repetition_count);
+    AnimatedImageMetadata(
+        PaintImage::Id paint_image_id,
+        PaintImage::CompletionState completion_state,
+        std::vector<FrameMetadata> frames,
+        int repetition_count,
+        PaintImage::AnimationSequenceId reset_animation_sequence_id);
     AnimatedImageMetadata(const AnimatedImageMetadata& other);
     ~AnimatedImageMetadata();
 
@@ -43,6 +45,7 @@ class CC_PAINT_EXPORT DiscardableImageMap {
     PaintImage::CompletionState completion_state;
     std::vector<FrameMetadata> frames;
     int repetition_count;
+    PaintImage::AnimationSequenceId reset_animation_sequence_id;
   };
 
   DiscardableImageMap();
