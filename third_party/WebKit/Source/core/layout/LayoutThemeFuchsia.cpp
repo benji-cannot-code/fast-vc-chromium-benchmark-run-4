6d9fcc706556a1830a829971f81acf3fe44ad26f
tzik <tzik@chromium.org>
2017-09-26 09:17:30 +0000

    Remove ambiguities affected by {WTF,base}::RefCounted unification
    The upcoming unification of WTF::RefCounted and base::RefCounted causes
    compile errors around AdoptRef, Bind, Passed due ADL.
    This CL converts them to fully qualified names before it happens.
    
    Bug: 763844
    Change-Id: Ib2361e02e70de193f3b6d0401ff626426d0a162c
    Reviewed-on: https://chromium-review.googlesource.com/684095
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504320}

--- third_party/WebKit/Source/core/layout/LayoutThemeFuchsia.cpp
+++ third_party/WebKit/Source/core/layout/LayoutThemeFuchsia.cpp
@@ -13,7 +13,7 @@ namespace {
 class LayoutThemeFuchsia : public LayoutThemeDefault {
  public:
   static RefPtr<LayoutTheme> Create() {
-    return AdoptRef(new LayoutThemeFuchsia());
+    return WTF::AdoptRef(new LayoutThemeFuchsia());
   }
 };
 
