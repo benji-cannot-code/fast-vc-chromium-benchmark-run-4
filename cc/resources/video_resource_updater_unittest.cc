cc3d7bbbf10be5701bc1ffd4b0ac9b21a1041ce7
danakj <danakj@chromium.org>
2017-09-26 21:08:03 +0000

    Use immutable textures in VideoResourceUpdater.
    The upload uses CopySubTextureCHROMIUM which is compatible with
    immutable textures, as it does not change the texture size. It used
    to use CopyTextureCHROMIUM, which is not compatible, and had a unit
    test describing this. But in reality this has changed, so always make
    the resource immutible, and stop checking for that when recycling.
    
    R=piman@chromium.org
    
    Bug: 768976
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ia9a31769fcc6c513291b6e67be4dea07d1c02168
    Reviewed-on: https://chromium-review.googlesource.com/685742
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504481}

--- cc/resources/video_resource_updater_unittest.cc
+++ cc/resources/video_resource_updater_unittest.cc
@@ -42,7 +42,6 @@ class WebGraphicsContext3DUploadCounter : public TestWebGraphicsContext3D {
                        GLuint internalformat,
                        GLint width,
                        GLint height) override {
-    immutable_texture_created_ = true;
   }
 
   GLuint createTexture() override {
@@ -66,13 +65,9 @@ class WebGraphicsContext3DUploadCounter : public TestWebGraphicsContext3D {
   int TextureCreationCount() { return created_texture_count_; }
   void ResetTextureCreationCount() { created_texture_count_ = 0; }
 
-  bool WasImmutableTextureCreated() { return immutable_texture_created_; }
-  void ResetImmutableTextureCreated() { immutable_texture_created_ = false; }
-
  private:
   int upload_count_;
   int created_texture_count_;
-  bool immutable_texture_created_;
 };
 
 class SharedBitmapManagerAllocationCounter : public TestSharedBitmapManager {
@@ -576,7 +571,6 @@ TEST_F(VideoResourceUpdaterTest, CreateForHardwarePlanes_StreamTexture) {
   // GL_TEXTURE_2D texture.
   context3d_->ResetTextureCreationCount();
   video_frame = CreateTestStreamTextureHardwareVideoFrame(true);
-  context3d_->ResetImmutableTextureCreated();
   resources = updater.CreateExternalResourcesFromVideoFrame(video_frame);
   EXPECT_EQ(VideoFrameExternalResources::RGBA_PREMULTIPLIED_RESOURCE,
             resources.type);
@@ -585,13 +579,6 @@ TEST_F(VideoResourceUpdaterTest, CreateForHardwarePlanes_StreamTexture) {
   EXPECT_EQ(1u, resources.release_callbacks.size());
   EXPECT_EQ(0u, resources.software_resources.size());
   EXPECT_EQ(1, context3d_->TextureCreationCount());
-
-  // The texture copy path requires the use of CopyTextureCHROMIUM, which
-  // enforces that the target texture not be immutable, as it may need
-  // to alter the storage of the texture. Therefore, this test asserts
-  // that an immutable texture wasn't created by glTexStorage2DEXT, when
-  // that extension is supported.
-  EXPECT_FALSE(context3d_->WasImmutableTextureCreated());
 }
 
 TEST_F(VideoResourceUpdaterTest, CreateForHardwarePlanes_TextureQuad) {
