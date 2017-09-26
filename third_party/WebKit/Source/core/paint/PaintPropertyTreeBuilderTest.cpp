6c6d4f5bf7710ea81c7662c82394a4929c262693
Xianzhu Wang <wangxianzhu@chromium.org>
2017-09-26 05:24:48 +0000

    Use GeometryMapper to map visual rect with filters for paint invalidation
    Bug: 637313
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Icad7f9354b7cb92ddfe8caa5224e3ba2540f7440
    Reviewed-on: https://chromium-review.googlesource.com/667756
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504287}

--- third_party/WebKit/Source/core/paint/PaintPropertyTreeBuilderTest.cpp
+++ third_party/WebKit/Source/core/paint/PaintPropertyTreeBuilderTest.cpp
@@ -3283,6 +3283,7 @@ TEST_P(PaintPropertyTreeBuilderTest, Reflection) {
   EXPECT_EQ(FrameScrollTranslation(),
             filter_properties->Filter()->LocalTransformSpace());
   EXPECT_EQ(FrameContentClip(), filter_properties->Filter()->OutputClip());
+  EXPECT_EQ(FloatPoint(8, 8), filter_properties->Filter()->PaintOffset());
 }
 
 TEST_P(PaintPropertyTreeBuilderTest, SimpleFilter) {
@@ -3295,6 +3296,7 @@ TEST_P(PaintPropertyTreeBuilderTest, SimpleFilter) {
   EXPECT_EQ(FrameScrollTranslation(),
             filter_properties->Filter()->LocalTransformSpace());
   EXPECT_EQ(FrameContentClip(), filter_properties->Filter()->OutputClip());
+  EXPECT_EQ(FloatPoint(8, 8), filter_properties->Filter()->PaintOffset());
 }
 
 TEST_P(PaintPropertyTreeBuilderTest, FilterReparentClips) {
@@ -3313,6 +3315,7 @@ TEST_P(PaintPropertyTreeBuilderTest, FilterReparentClips) {
             filter_properties->Filter()->LocalTransformSpace());
   EXPECT_EQ(clip_properties->OverflowClip(),
             filter_properties->Filter()->OutputClip());
+  EXPECT_EQ(FloatPoint(8, 8), filter_properties->Filter()->PaintOffset());
 
   const PropertyTreeState& child_paint_state =
       *GetLayoutObjectByElementId("child")
