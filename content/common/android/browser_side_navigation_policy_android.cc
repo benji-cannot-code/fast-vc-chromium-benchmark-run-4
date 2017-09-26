525ab0080ebdf46de9bc647dec9e3d3698768e1c
Nate Fischer <ntfschr@chromium.org>
2017-09-26 18:55:09 +0000

    AW: only call onPageStarted once for loadDataWithBaseUrl
    Previously, WebView would explicitly post onPageStarted for
    loadDataWithBaseUrl, since this navigation was handled by blink and it
    did not generate the event.
    
    With browser-side navigation (PlzNavigate), this is no longer the case
    and the navigation code will post this event under the normal pipeline.
    For this reason, we need to disable the extra postOnPageStarted when
    browser-side is turned on.
    
    As part of the implementation, this adds a Java wrapper around
    IsBrowserSideNavigationEnabled.
    
    This also modifies an integration test to check that we only get
    onPageStarted called once.
    
    Bug: 702785
    Test: run_webview_instrumentation_test_apk -f LoadDataWithBaseUrlTest#testloadDataWithBaseUrlCallsOnPageStarted
    Change-Id: I3abc7b3503feee26e5b2438f9b039be30750e4ef
    Reviewed-on: https://chromium-review.googlesource.com/683634
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504441}

---
+++ content/common/android/browser_side_navigation_policy_android.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <jni.h>
+
+#include "base/android/scoped_java_ref.h"
+#include "content/public/common/browser_side_navigation_policy.h"
+#include "jni/BrowserSideNavigationPolicy_jni.h"
+
+using base::android::JavaParamRef;
+
+namespace content {
+
+jboolean IsBrowserSideNavigationEnabled(JNIEnv* env,
+                                        const JavaParamRef<jclass>& clazz) {
+  return IsBrowserSideNavigationEnabled();
+}
+
+}  // namespace content
---
+++ content/public/android/java/src/org/chromium/content_public/common/BrowserSideNavigationPolicy.java
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.content_public.common;
+
+import org.chromium.base.annotations.JNINamespace;
+
+/**
+ * This is a utility class to wrap browser_side_navigation_policy.cc.
+ */
+@JNINamespace("content")
+public final class BrowserSideNavigationPolicy {
+    public static boolean isBrowserSideNavigationEnabled() {
+        return nativeIsBrowserSideNavigationEnabled();
+    }
+
+    private static native boolean nativeIsBrowserSideNavigationEnabled();
+
+    // Do not instantiate this class.
+    private BrowserSideNavigationPolicy() {}
+}
