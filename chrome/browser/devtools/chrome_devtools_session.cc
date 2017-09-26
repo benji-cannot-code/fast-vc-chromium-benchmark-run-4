996abe02670c8589e87c83e4c23e69a03ee6d4f7
Sadrul Habib Chowdhury <sadrul@chromium.org>
2017-09-26 20:36:19 +0000

    devtools: Add an UberDispatcher in chrome.
    Dispatch the commands in Page domain through an UberDispatcher. Follow up
    CLs make this change for the other domains (Browser, Target).
    
    This is in preparation for adding WindowManager domain.
    
    BUG=758061
    
    Change-Id: I35e9dcae13ee9f80e4d81054bdfaf2c5292785ad
    Reviewed-on: https://chromium-review.googlesource.com/674263
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504474}

---
+++ chrome/browser/devtools/chrome_devtools_session.cc
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/devtools/chrome_devtools_session.h"
+
+#include "chrome/browser/devtools/protocol/page_handler.h"
+#include "content/public/browser/devtools_agent_host.h"
+
+ChromeDevToolsSession::ChromeDevToolsSession(
+    content::DevToolsAgentHost* agent_host,
+    int session_id)
+    : agent_host_(agent_host),
+      session_id_(session_id),
+      dispatcher_(std::make_unique<protocol::UberDispatcher>(this)) {
+  dispatcher_->setFallThroughForNotFound(true);
+  if (agent_host->GetWebContents()) {
+    page_handler_ = std::make_unique<PageHandler>(agent_host->GetWebContents(),
+                                                  dispatcher_.get());
+  }
+}
+
+ChromeDevToolsSession::~ChromeDevToolsSession() = default;
+
+void ChromeDevToolsSession::sendProtocolResponse(
+    int call_id,
+    std::unique_ptr<protocol::Serializable> message) {
+  agent_host_->SendProtocolMessageToClient(session_id_, message->serialize());
+}
+
+void ChromeDevToolsSession::sendProtocolNotification(
+    std::unique_ptr<protocol::Serializable> message) {
+  agent_host_->SendProtocolMessageToClient(session_id_, message->serialize());
+}
+
+void ChromeDevToolsSession::flushProtocolNotifications() {}
