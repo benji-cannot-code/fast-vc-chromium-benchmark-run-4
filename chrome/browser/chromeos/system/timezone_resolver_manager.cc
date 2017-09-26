0aea27d117b1927615cc829a681e29a80293a637
Alexander Alekseev <alemate@chromium.org>
2017-09-26 19:59:07 +0000

    Chrome OS: Fix Local State initialization check in Time zone resolver.
    This Cl fixes a race in Time Zone Resolver Manager when checking if Local State
    has been initialized.
    
    Bug: b/64065854
    Change-Id: If681dc675e0219fda7c9b37307c7f205394c62fd
    Reviewed-on: https://chromium-review.googlesource.com/682734
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504465}

--- chrome/browser/chromeos/system/timezone_resolver_manager.cc
+++ chrome/browser/chromeos/system/timezone_resolver_manager.cc
@@ -123,6 +123,9 @@ ServiceConfiguration GetServiceConfigurationForSigninScreen() {
 }  // anonymous namespace.
 
 TimeZoneResolverManager::TimeZoneResolverManager() : weak_factory_(this) {
+  local_state_initialized_ =
+      g_browser_process->local_state()->GetInitializationStatus() ==
+      PrefService::INITIALIZATION_STATUS_SUCCESS;
   g_browser_process->local_state()->AddPrefInitObserver(
       base::Bind(&TimeZoneResolverManager::OnLocalStateInitialized,
                  weak_factory_.GetWeakPtr()));
