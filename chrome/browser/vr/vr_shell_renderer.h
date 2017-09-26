8ea69303e0973cbaa1e36b47a388e69a8b0b5de8
Biao She <bshe@chromium.org>
2017-09-26 18:37:04 +0000

    [Code cleanup]Rename view_proj_matrix to model_view_proj_matrix when appropriate
    Bug: None
    Change-Id: I17162ce72e6047e0d26613fad6a041703e8f2698
    Reviewed-on: https://chromium-review.googlesource.com/684699
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Biao She <bshe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504433}

--- chrome/browser/vr/vr_shell_renderer.h
+++ chrome/browser/vr/vr_shell_renderer.h
@@ -40,18 +40,18 @@ class VrShellRenderer : public vr::UiElementRenderer {
   // vr::UiElementRenderer interface (exposed to UI elements).
   void DrawTexturedQuad(int texture_data_handle,
                         TextureLocation texture_location,
-                        const gfx::Transform& view_proj_matrix,
+                        const gfx::Transform& model_view_proj_matrix,
                         const gfx::RectF& copy_rect,
                         float opacity,
                         gfx::SizeF element_size,
                         float corner_radius) override;
-  void DrawGradientQuad(const gfx::Transform& view_proj_matrix,
+  void DrawGradientQuad(const gfx::Transform& model_view_proj_matrix,
                         const SkColor edge_color,
                         const SkColor center_color,
                         float opacity,
                         gfx::SizeF element_size,
                         float corner_radius) override;
-  void DrawGradientGridQuad(const gfx::Transform& view_proj_matrix,
+  void DrawGradientGridQuad(const gfx::Transform& model_view_proj_matrix,
                             const SkColor edge_color,
                             const SkColor center_color,
                             const SkColor grid_color,
@@ -137,7 +137,7 @@ class TexturedQuadRenderer : public BaseRenderer {
   // Enqueues a textured quad for rendering. The GL will ultimately be issued
   // in |Flush|.
   void AddQuad(int texture_data_handle,
-               const gfx::Transform& view_proj_matrix,
+               const gfx::Transform& model_view_proj_matrix,
                const gfx::RectF& copy_rect,
                float opacity,
                const gfx::Size& surface_texture_size,
@@ -154,7 +154,7 @@ class TexturedQuadRenderer : public BaseRenderer {
  private:
   struct QuadData {
     int texture_data_handle;
-    gfx::Transform view_proj_matrix;
+    gfx::Transform model_view_proj_matrix;
     gfx::RectF copy_rect;
     float opacity;
     gfx::Size surface_texture_size;
@@ -287,7 +287,7 @@ class GradientQuadRenderer : public BaseRenderer {
   GradientQuadRenderer();
   ~GradientQuadRenderer() override;
 
-  void Draw(const gfx::Transform& view_proj_matrix,
+  void Draw(const gfx::Transform& model_view_proj_matrix,
             SkColor edge_color,
             SkColor center_color,
             float opacity,
@@ -318,7 +318,7 @@ class GradientGridRenderer : public BaseQuadRenderer {
   GradientGridRenderer();
   ~GradientGridRenderer() override;
 
-  void Draw(const gfx::Transform& view_proj_matrix,
+  void Draw(const gfx::Transform& model_view_proj_matrix,
             SkColor edge_color,
             SkColor center_color,
             SkColor grid_color,
