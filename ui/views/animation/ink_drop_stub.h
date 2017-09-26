ff8a57070d28022383cfc3025bcd4da7f7e158b3
Daniel Bratell <bratell@opera.com>
2017-09-26 16:26:53 +0000

    Adding missing include guard to ui/views/animation/ink_drop_stub.h
    Change-Id: Id3e0139beeca89444f4100de67c21da579c49047
    Reviewed-on: https://chromium-review.googlesource.com/684739
    Reviewed-by: Mohsen Izadi <mohsen@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#504388}

--- ui/views/animation/ink_drop_stub.h
+++ ui/views/animation/ink_drop_stub.h
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifndef UI_VIEWS_ANIMATION_INK_DROP_STUB_H_
+#define UI_VIEWS_ANIMATION_INK_DROP_STUB_H_
+
 #include "base/macros.h"
 #include "ui/views/animation/ink_drop.h"
 #include "ui/views/views_export.h"
@@ -32,3 +35,5 @@ class VIEWS_EXPORT InkDropStub : public InkDrop {
 };
 
 }  // namespace views
+
+#endif  // UI_VIEWS_ANIMATION_INK_DROP_STUB_H_
