88da1ef130e4c25e968c07faf0fd5a71bb7da524
Christian Dullweber <dullweber@chromium.org>
2017-09-26 16:29:45 +0000

    Choose clear site settings label based on switch
    The title of the site settings / content settings section depends on the
    value of switches::kEnableSiteSettings, so the option in Clear Browsing
    Data should use the same logic to decide which title to show.
    
    Bug: 725401
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I0cdd9642759f9cc321ec9a591445a4c17606c2ef
    Reviewed-on: https://chromium-review.googlesource.com/681215
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504390}

--- chrome/browser/resources/settings/clear_browsing_data_dialog/clear_browsing_data_dialog_tabs.js
+++ chrome/browser/resources/settings/clear_browsing_data_dialog/clear_browsing_data_dialog_tabs.js
@@ -157,6 +157,16 @@ Polymer({
     this.$.clearBrowsingDataDialog.classList.add('fully-rendered');
   },
 
+  /**
+   * Choose a summary checkbox label.
+   * @param {boolean} isSignedIn
+   * @param {boolean} isSyncingHistory
+   * @param {string} historySummary
+   * @param {string} historySummarySigned
+   * @param {string} historySummarySynced
+   * @return {string}
+   * @private
+   */
   browsingCheckboxLabel_: function(
       isSignedIn, isSyncingHistory, historySummary, historySummarySigned,
       historySummarySynced) {
@@ -166,6 +176,18 @@ Polymer({
       return historySummarySigned;
     }
     return historySummary;
+  },
+
+  /**
+   * Choose a content/site settings label.
+   * @param {string} siteSettings
+   * @param {string} contentSettings
+   * @return {string}
+   * @private
+   */
+  siteSettingsLabel_: function(siteSettings, contentSettings) {
+    return loadTimeData.getBoolean('enableSiteSettings') ? siteSettings :
+                                                           contentSettings;
   },
 
   /**
