b66452126495e159d0a2d988462ebddf91325f4a
Sadrul Habib Chowdhury <sadrul@chromium.org>
2017-09-26 23:07:05 +0000

    devtools: Use UberDispatcher for Browser commands.
    BUG=758061
    
    Change-Id: If50bfbc29088f0cde640cb540a40602650ee3bef
    Reviewed-on: https://chromium-review.googlesource.com/676144
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504508}

--- chrome/browser/devtools/chrome_devtools_session.cc
+++ chrome/browser/devtools/chrome_devtools_session.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/devtools/chrome_devtools_session.h"
 
+#include "chrome/browser/devtools/protocol/browser_handler.h"
 #include "chrome/browser/devtools/protocol/page_handler.h"
 #include "content/public/browser/devtools_agent_host.h"
 
@@ -19,6 +20,7 @@ ChromeDevToolsSession::ChromeDevToolsSession(
     page_handler_ = std::make_unique<PageHandler>(agent_host->GetWebContents(),
                                                   dispatcher_.get());
   }
+  browser_handler_ = std::make_unique<BrowserHandler>(dispatcher_.get());
 }
 
 ChromeDevToolsSession::~ChromeDevToolsSession() = default;
