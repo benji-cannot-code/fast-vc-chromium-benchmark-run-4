3c6f0997e1ae31b731b88bd1a6be8a67e882a6b4
Vladislav Kaznacheev <kaznacheev@google.com>
2017-09-26 17:45:43 +0000

    Disable stylus barrel button for a brief period after pen up.
    This should prevent metalayer triggering when a user is
    writing/sketching while accidentally holding down the stylus button.
    
    Bug: 767440
    Test: ash_unittests --gtest_filter=PaletteTrayTest*
    Change-Id: I98a1dc4be2bb888c6cc06e3e93a60b73b56c6a8d
    Reviewed-on: https://chromium-review.googlesource.com/683254
    Commit-Queue: Vladislav Kaznacheev <kaznacheev@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504419}

--- ash/system/palette/tools/metalayer_mode.h
+++ ash/system/palette/tools/metalayer_mode.h
@@ -84,6 +84,8 @@ class ASH_EXPORT MetalayerMode : public CommonPaletteTool,
 
   bool voice_interaction_context_enabled_ = false;
 
+  base::TimeTicks previous_stroke_end_;
+
   base::WeakPtrFactory<MetalayerMode> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(MetalayerMode);
