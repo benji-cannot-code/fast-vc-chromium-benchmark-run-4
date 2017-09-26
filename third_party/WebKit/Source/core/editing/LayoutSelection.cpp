bd6b695b0a254551889f2a440abf6913a0bed6ed
Yoichi Osato <yoichio@chromium.org>
2017-09-26 07:34:09 +0000

    Nit pick: remove tailing semicolon.
    Bug: 
    Change-Id: I918ea9781189c3aeba223e4ef0af7085e4e78c19
    Reviewed-on: https://chromium-review.googlesource.com/683975
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504307}

--- third_party/WebKit/Source/core/editing/LayoutSelection.cpp
+++ third_party/WebKit/Source/core/editing/LayoutSelection.cpp
@@ -244,7 +244,7 @@ class SelectionMarkingRange {
     invalidation_set_ = std::move(other.invalidation_set_);
   }
 
-  SelectionPaintRange PaintRange() const { return paint_range_; };
+  SelectionPaintRange PaintRange() const { return paint_range_; }
 
   const PaintInvalidationSet& InvalidationSet() const {
     return invalidation_set_;
