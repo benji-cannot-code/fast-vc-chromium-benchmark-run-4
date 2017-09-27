581fe9103428e639bf11dc93d893489a20aa0ab4
Mike Wasserman <msw@chromium.org>
2017-09-27 23:30:39 +0000

    ChromeOS: Open chrome on the shelf context menu's display in cash.
    Make Chrome shelf item context menus open items on the right display.
    Use a ScopedRootWindowForNewWindows when executing commands in cash.
    (we'll need another fix for mash, but my first attempt didn't work)
    
    Fixes the defect when using --ash-enable-shelf-model-synchronization.
    Nix the ShelfView codepath that only matches the local menu lifetime.
    (ShelfModel Sync handles the menu command execution async/remotely)
    
    Bug: 768908
    Test: Chrome opens on the same display as the shelf item [context menu].
    Change-Id: If8c812c892c4c628e9c012ea9e9cd75461d3b71a
    Reviewed-on: https://chromium-review.googlesource.com/688108
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504800}

--- chrome/browser/ui/ash/launcher/extension_launcher_context_menu.cc
+++ chrome/browser/ui/ash/launcher/extension_launcher_context_menu.cc
@@ -5,7 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/ash/launcher/extension_launcher_context_menu.h"
 
+#include "ash/scoped_root_window_for_new_windows.h"  // mash-ok
+#include "ash/shell.h"                               // mash-ok
 #include "base/bind.h"
+#include "chrome/browser/chromeos/ash_config.h"
 #include "chrome/browser/extensions/context_menu_matcher.h"
 #include "chrome/browser/extensions/extension_util.h"
 #include "chrome/browser/extensions/launch_util.h"
@@ -150,6 +153,15 @@ void ExtensionLauncherContextMenu::ExecuteCommand(int command_id,
                                                   int event_flags) {
   if (ExecuteCommonCommand(command_id, event_flags))
     return;
+
+  // Place new windows on the same display as the context menu.
+  // TODO(crbug.com/768908): Fix this in mash (where Chrome can't use Shell).
+  std::unique_ptr<ash::ScopedRootWindowForNewWindows> scoped_root;
+  if (chromeos::GetAshConfig() != ash::Config::MASH) {
+    aura::Window* root = ash::Shell::GetRootWindowForDisplayId(display_id());
+    scoped_root = std::make_unique<ash::ScopedRootWindowForNewWindows>(root);
+  }
+
   switch (static_cast<MenuItem>(command_id)) {
     case LAUNCH_TYPE_PINNED_TAB:
       SetLaunchType(extensions::LAUNCH_TYPE_PINNED);
