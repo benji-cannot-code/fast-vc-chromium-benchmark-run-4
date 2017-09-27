d7d1c318006813e4600fb95d059c4587f156f607
Marc Treib <treib@chromium.org>
2017-09-27 08:40:34 +0000

    Instant cleanup: Implement TabStripModelObserver
    Before this CL, BrowserInstantController/InstantController relied on
    custom callbacks from Browser to track the active tab. Now,
    InstantController implements TabStripModelObserver to directly get
    notified of the relevant events.
    
    Bug: 627747
    Change-Id: I28b9b07b9609e6020f68e41e126665dd7ded557b
    Reviewed-on: https://chromium-review.googlesource.com/684439
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504601}

--- chrome/browser/ui/browser_instant_controller.h
+++ chrome/browser/ui/browser_instant_controller.h
@@ -16,33 +16,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class Browser;
 class Profile;
 
-namespace content {
-class WebContents;
-}
-
 // BrowserInstantController is responsible for reloading any Instant tabs (which
 // today just means NTPs) when the default search provider changes. This can
 // happen when the user chooses a different default search engine, or when the
-// Google base URL changes while Google is the default search engine. Also owns
-// a SearchModel instance that corresponds to the currently-active tab.
+// Google base URL changes while Google is the default search engine.
 class BrowserInstantController {
  public:
   explicit BrowserInstantController(Browser* browser);
   ~BrowserInstantController();
 
-  // Returns the Profile associated with the Browser that owns this object.
-  Profile* profile() const;
-
-  // Invoked by |browser_| when the active tab changes.
-  // TODO(treib): Implement TabStripModelObserver instead of relying on custom
-  // callbacks from Browser.
-  void OnTabActivated(content::WebContents* web_contents);
-  void OnTabDeactivated(content::WebContents* web_contents);
-  void OnTabDetached(content::WebContents* web_contents);
-
  private:
   void OnSearchEngineBaseURLChanged(
       SearchEngineBaseURLTracker::ChangeReason change_reason);
+
+  Profile* profile() const;
 
   Browser* const browser_;
 
