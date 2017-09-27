11f724a78fa0ca863d76b669ea1ba64da040f05f
Peng Huang <penghuang@chromium.org>
2017-09-27 20:01:23 +0000

    exo: send surface's input region to mus
    The surface input region doesn't work in mus and mash. This CL
    implements WindowTargeter::GetExtraHitTestShapeRects() for
    exo::Surface::window_. In mus and mash, this function will be called and
    returned hittest rects will be sent to mus.
    
    Bug: 766174
    Change-Id: I67dc04cc331f0174ff3390920c3de98c00100df5
    Reviewed-on: https://chromium-review.googlesource.com/685277
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504758}

--- components/exo/surface.h
+++ components/exo/surface.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkBlendMode.h"
 #include "third_party/skia/include/core/SkRegion.h"
 #include "ui/aura/window.h"
+#include "ui/aura/window_targeter.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/native_widget_types.h"
 #include "ui/gfx/transform.h"
@@ -172,6 +173,11 @@ class Surface final : public ui::PropertyHandler {
 
   // Returns the current input region of surface in the form of a hit-test mask.
   void GetHitTestMask(gfx::Path* mask) const;
+
+  // Returns the current input region of surface in the form of a set of
+  // hit-test rects.
+  std::unique_ptr<aura::WindowTargeter::HitTestRects> GetHitTestShapeRects()
+      const;
 
   // Surface does not own cursor providers. It is the responsibility of the
   // caller to remove the cursor provider before it is destroyed.
