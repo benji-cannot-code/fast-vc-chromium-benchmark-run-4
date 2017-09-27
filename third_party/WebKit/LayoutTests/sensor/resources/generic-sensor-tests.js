91b757ce7d43415aacbb219ff5c222a7184ac024
Mikhail Pozdnyakov <mikhail.pozdnyakov@intel.com>
2017-09-27 10:27:36 +0000

    Add Sensor.hasReading attribute
    ED: https://w3c.github.io/sensors/#dom-sensor-hasreading
    
    Bug: 750018
    Change-Id: If9ffbd23c3e1eea66fe815792dd5e3b2ff3214ca
    Reviewed-on: https://chromium-review.googlesource.com/684437
    Reviewed-by: Alexander Shalamov <alexander.shalamov@intel.com>
    Commit-Queue: Mikhail Pozdnyakov <mikhail.pozdnyakov@intel.com>
    Cr-Commit-Position: refs/heads/master@{#504620}

--- third_party/WebKit/LayoutTests/sensor/resources/generic-sensor-tests.js
+++ third_party/WebKit/LayoutTests/sensor/resources/generic-sensor-tests.js
@@ -215,10 +215,11 @@ function runGenericSensorTests(sensorType, updateReading, verifyReading) {
     return testPromise;
   }, prefix + 'Test that addConfiguration and removeConfiguration is called.');
 
-  function checkOnChangeIsCalledAndReadingIsValid(sensor) {
+  function checkOnReadingIsCalledAndReadingIsValid(sensor) {
     let sensorObject = new sensorType({frequency: 60});
     sensorObject.start();
-    let testPromise = sensor.mockSensorProvider.getCreatedSensor()
+    assert_false(sensorObject.hasReading);
+    return sensor.mockSensorProvider.getCreatedSensor()
         .then(mockSensor => {
           return mockSensor.setUpdateSensorReadingFunction(updateReading);
         })
@@ -226,8 +227,10 @@ function runGenericSensorTests(sensorType, updateReading, verifyReading) {
           return new Promise((resolve, reject) => {
             let wrapper = new CallbackWrapper(() => {
               assert_true(verifyReading(sensorObject));
+              assert_true(sensorObject.hasReading);
               sensorObject.stop();
               assert_true(verifyReading(sensorObject, true /*should be null*/));
+              assert_false(sensorObject.hasReading);
               resolve(mockSensor);
             }, reject);
 
@@ -236,18 +239,16 @@ function runGenericSensorTests(sensorType, updateReading, verifyReading) {
           });
         })
         .then(mockSensor => { return mockSensor.removeConfigurationCalled(); });
-
-      return testPromise;
   }
 
   sensor_test(sensor => {
-    return checkOnChangeIsCalledAndReadingIsValid(sensor);
-  }, prefix + 'Test that onChange is called and sensor reading is valid (onreading reporting).');
+    return checkOnReadingIsCalledAndReadingIsValid(sensor);
+  }, prefix + 'Test that onreading is called and sensor reading is valid (onchange reporting).');
 
   sensor_test(sensor => {
     sensor.mockSensorProvider.setContinuousReportingMode();
-    return checkOnChangeIsCalledAndReadingIsValid(sensor);
-  }, prefix + 'Test that onChange is called and sensor reading is valid (continuous reporting).');
+    return checkOnReadingIsCalledAndReadingIsValid(sensor);
+  }, prefix + 'Test that onreading is called and sensor reading is valid (continuous reporting).');
 
   sensor_test(sensor => {
     let sensorObject = new sensorType;
@@ -408,7 +409,7 @@ function runGenericSensorTests(sensorType, updateReading, verifyReading) {
 
   sensor_test(sensor => {
     return checkFrequencyHintWorks(sensor);
-  }, prefix + 'Test that frequency hint works (onreading reporting).');
+  }, prefix + 'Test that frequency hint works (onchange reporting).');
 
   sensor_test(sensor => {
     sensor.mockSensorProvider.setContinuousReportingMode();
--- third_party/WebKit/Source/modules/sensor/Sensor.h
+++ third_party/WebKit/Source/modules/sensor/Sensor.h
@@ -50,6 +50,7 @@ class Sensor : public EventTargetWithInlineData,
 
   // Getters
   bool activated() const;
+  bool hasReading() const;
   DOMHighResTimeStamp timestamp(ScriptState*, bool& is_null) const;
 
   DEFINE_ATTRIBUTE_EVENT_LISTENER(error);
@@ -75,7 +76,6 @@ class Sensor : public EventTargetWithInlineData,
   // parameters if needed.
   virtual SensorConfigurationPtr CreateSensorConfig();
 
-  bool CanReturnReadings() const;
   bool IsActivated() const { return state_ == SensorState::kActivated; }
   bool IsIdleOrErrored() const;
   const SensorProxy* proxy() const { return sensor_proxy_; }
@@ -125,7 +125,7 @@ class Sensor : public EventTargetWithInlineData,
 // To be used in getters in concrete sensors
 // bindings code.
 #define INIT_IS_NULL_AND_RETURN(is_null, x) \
-  is_null = !CanReturnReadings();           \
+  is_null = !hasReading();                  \
   if (is_null)                              \
   return (x)
 
