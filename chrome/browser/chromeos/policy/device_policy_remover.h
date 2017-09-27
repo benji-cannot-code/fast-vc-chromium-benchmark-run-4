de25c89fa2b41e32bdbb014fbabcd942890850d5
Daria Yakovleva <yakovleva@google.com>
2017-09-27 09:56:46 +0000

    [DeviceOffHours] Change type of |ignored_policies| values in proto
    Change type of |ignored_policies| values from names in
    ChromeDeviceSettingsProto to proto tags in the same proto.
    Current list of ignored policies contains policy proto tags
    from ChromeDeviceSettingsProto (i.e. proto tag = 1 means
    device_policy_refresh_rate policy). Proto tags are used because
    they are consistent identifiers.
    
    Bug: 739713
    Change-Id: I88b55c37f59445e7daf22e73b594cb7eae0bc011
    Reviewed-on: https://chromium-review.googlesource.com/663265
    Commit-Queue: Daria Iakovleva <yakovleva@google.com>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504617}

--- chrome/browser/chromeos/policy/device_policy_remover.h
+++ chrome/browser/chromeos/policy/device_policy_remover.h
@@ -12,13 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace policy {
 
-// Remove |policies_to_remove| fields from |policies|.
-// Vector |policies_to_remove| contains device policy names from
-// ChromeDeviceSettingsProto.
-// Implementation of this method is generated
+// Remove |policy_proto_tags_to_remove| fields from |policies|.
+// Vector |policy_proto_tags_to_remove| contains device policy proto tags
+// from ChromeDeviceSettingsProto. Implementation of this method is generated
 // automatically by generate_device_policy_remover.py.
 void RemovePolicies(enterprise_management::ChromeDeviceSettingsProto* policies,
-                    const std::vector<std::string>& policies_to_remove);
+                    const std::vector<int>& policy_proto_tags_to_remove);
 
 }  // namespace policy
 
