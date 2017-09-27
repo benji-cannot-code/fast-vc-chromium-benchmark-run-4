f56fd6a71972c7971ed7129de8349853b586e8e0
mariannet <mariannet@google.com>
2017-09-27 04:08:04 +0000

    Make sure that folder quick view can be opened reliably.
    A race condition sometimes closed the quick view the moment it was opened.
    This behavior could be witnessed when rendering the quick view was fast, like
    it is for opening folders.
    Make sure the <space> key down event only gets handled once (for either opening
    or closing) instead of twice, thus eliminating the race condition.
    Opening quick view for folders didn't work reliably, fix applied.
    
    Bug: 762699
    Test: manually tested
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I899437e312cfa7ad76d45706b670a2db4c83fc48
    Reviewed-on: https://chromium-review.googlesource.com/683498
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Reviewed-by: Naoki Fukino <fukino@chromium.org>
    Commit-Queue: Marianne Thieffry <mariannet@google.com>
    Cr-Commit-Position: refs/heads/master@{#504577}

--- ui/file_manager/file_manager/foreground/js/quick_view_controller.js
+++ ui/file_manager/file_manager/foreground/js/quick_view_controller.js
@@ -194,7 +194,7 @@ QuickViewController.prototype.onOpenInNewButtonTap_ = function(event) {
 };
 
 /**
- * Handles key event on listContainer if it's relevent to quick view.
+ * Handles key event on listContainer if it's relevant to quick view.
  *
  * @param {!Event} event A keyboard event.
  * @private
@@ -204,6 +204,7 @@ QuickViewController.prototype.onKeyDownToOpen_ = function(event) {
     return;
   if (event.key === ' ') {
     event.preventDefault();
+    event.stopImmediatePropagation();
     this.display_(QuickViewUma.WayToOpen.SPACE_KEY);
   }
 };
