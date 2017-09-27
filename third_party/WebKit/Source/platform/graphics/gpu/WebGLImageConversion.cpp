1095f42b1dccd888515d108ff9d6811fc2af1e56
Zhenyao Mo <zmo@chromium.org>
2017-09-27 03:17:24 +0000

    Handle invalid format/type in texture uploading when FlipY/PremultiplyAlpha is set.
    Currently if uploading from ArrayBuffer, NOTREACHED() will be hit in such situations
    and in Release build, after hitting NOTREACHED() statement, unintended code path is taken.
    
    BUG=765469
    TEST=webgl_conformance
    R=kbr@chromium.org
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Icf1ab03723b6c13595bd6c4dbc4e5fa40fdf9d96
    Reviewed-on: https://chromium-review.googlesource.com/685487
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504568}

--- third_party/WebKit/Source/platform/graphics/gpu/WebGLImageConversion.cpp
+++ third_party/WebKit/Source/platform/graphics/gpu/WebGLImageConversion.cpp
@@ -43,6 +43,7 @@ int32_t ClampMin(int32_t value) {
   return value < kMinInt32Value ? kMinInt32Value : value;
 }
 
+// Return kDataFormatNumFormats if format/type combination is invalid.
 WebGLImageConversion::DataFormat GetDataFormat(GLenum destination_format,
                                                GLenum destination_type) {
   WebGLImageConversion::DataFormat dst_format =
@@ -67,7 +68,7 @@ WebGLImageConversion::DataFormat GetDataFormat(GLenum destination_format,
           dst_format = WebGLImageConversion::kDataFormatRGBA8_S;
           break;
         default:
-          NOTREACHED();
+          return WebGLImageConversion::kDataFormatNumFormats;
       }
       break;
     case GL_UNSIGNED_BYTE:
@@ -98,7 +99,7 @@ WebGLImageConversion::DataFormat GetDataFormat(GLenum destination_format,
           dst_format = WebGLImageConversion::kDataFormatRA8;
           break;
         default:
-          NOTREACHED();
+          return WebGLImageConversion::kDataFormatNumFormats;
       }
       break;
     case GL_SHORT:
@@ -114,7 +115,7 @@ WebGLImageConversion::DataFormat GetDataFormat(GLenum destination_format,
         case GL_RGBA_INTEGER:
           dst_format = WebGLImageConversion::kDataFormatRGBA16_S;
         default:
-          NOTREACHED();
+          return WebGLImageConversion::kDataFormatNumFormats;
       }
       break;
     case GL_UNSIGNED_SHORT:
@@ -135,7 +136,7 @@ WebGLImageConversion::DataFormat GetDataFormat(GLenum destination_format,
           dst_format = WebGLImageConversion::kDataFormatRGBA16;
           break;
         default:
-          NOTREACHED();
+          return WebGLImageConversion::kDataFormatNumFormats;
       }
       break;
     case GL_INT:
@@ -153,7 +154,7 @@ WebGLImageConversion::DataFormat GetDataFormat(GLenum destination_format,
           dst_format = WebGLImageConversion::kDataFormatRGBA32_S;
           break;
         default:
-          NOTREACHED();
+          return WebGLImageConversion::kDataFormatNumFormats;
       }
       break;
     case GL_UNSIGNED_INT:
@@ -174,7 +175,7 @@ WebGLImageConversion::DataFormat GetDataFormat(GLenum destination_format,
           dst_format = WebGLImageConversion::kDataFormatRGBA32;
           break;
         default:
-          NOTREACHED();
+          return WebGLImageConversion::kDataFormatNumFormats;
       }
       break;
     case GL_HALF_FLOAT_OES:  // OES_texture_half_float
@@ -200,7 +201,7 @@ WebGLImageConversion::DataFormat GetDataFormat(GLenum destination_format,
           dst_format = WebGLImageConversion::kDataFormatRA16F;
           break;
         default:
-          NOTREACHED();
+          return WebGLImageConversion::kDataFormatNumFormats;
       }
       break;
     case GL_FLOAT:  // OES_texture_float
@@ -228,7 +229,7 @@ WebGLImageConversion::DataFormat GetDataFormat(GLenum destination_format,
           dst_format = WebGLImageConversion::kDataFormatRA32F;
           break;
         default:
-          NOTREACHED();
+          return WebGLImageConversion::kDataFormatNumFormats;
       }
       break;
     case GL_UNSIGNED_SHORT_4_4_4_4:
@@ -253,7 +254,7 @@ WebGLImageConversion::DataFormat GetDataFormat(GLenum destination_format,
       dst_format = WebGLImageConversion::kDataFormatRGBA2_10_10_10;
       break;
     default:
-      NOTREACHED();
+      return WebGLImageConversion::kDataFormatNumFormats;
   }
   return dst_format;
 }
@@ -3036,6 +3037,8 @@ bool WebGLImageConversion::ExtractTextureData(unsigned width,
                                               Vector<uint8_t>& data) {
   // Assumes format, type, etc. have already been validated.
   DataFormat source_data_format = GetDataFormat(format, type);
+  if (source_data_format == kDataFormatNumFormats)
+    return false;
 
   // Resize the output buffer.
   unsigned int components_per_pixel, bytes_per_component;
@@ -3082,6 +3085,8 @@ bool WebGLImageConversion::PackPixels(const uint8_t* source_data,
 
   DataFormat dst_data_format =
       GetDataFormat(destination_format, destination_type);
+  if (dst_data_format == kDataFormatNumFormats)
+    return false;
   int dst_stride =
       source_data_sub_rectangle.Width() * TexelBytesForFormat(dst_data_format);
   if (flip_y) {
