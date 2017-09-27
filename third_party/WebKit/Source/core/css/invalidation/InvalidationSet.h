74bd14424e1dec3c17dfedadc67cbedd0a92f8fc
tzik <tzik@chromium.org>
2017-09-27 09:10:37 +0000

    Convert InvalidationSet to RefCounted
    InvalidationSet used to have a manual implementation for RefPtr for the
    custom destruction logic. However, after WTF::RefCounted gets a trait
    to inject the custom deleter, InvalidationSet can be a RefCounted type.
    
    Bug: 763844
    Change-Id: If24576d28c0fe64433596fa4ddfdf91d422b4a68
    Reviewed-on: https://chromium-review.googlesource.com/685194
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504604}

--- third_party/WebKit/Source/core/css/invalidation/InvalidationSet.h
+++ third_party/WebKit/Source/core/css/invalidation/InvalidationSet.h
@@ -49,6 +49,12 @@ class TracedValue;
 
 enum InvalidationType { kInvalidateDescendants, kInvalidateSiblings };
 
+class InvalidationSet;
+
+struct CORE_EXPORT InvalidationSetDeleter {
+  static void Destruct(const InvalidationSet*);
+};
+
 // Tracks data to determine which descendants in a DOM subtree, or
 // siblings and their descendants, need to have style recalculated.
 //
@@ -82,7 +88,8 @@ enum InvalidationType { kInvalidateDescendants, kInvalidateSiblings };
 //   the SiblingInvalidationSet also holding descendants containing class v.
 //
 // We avoid virtual functions to minimize space consumption.
-class CORE_EXPORT InvalidationSet {
+class CORE_EXPORT InvalidationSet
+    : public WTF::RefCounted<InvalidationSet, InvalidationSetDeleter> {
   WTF_MAKE_NONCOPYABLE(InvalidationSet);
   USING_FAST_MALLOC_WITH_TYPE_NAME(blink::InvalidationSet);
 
@@ -156,14 +163,6 @@ class CORE_EXPORT InvalidationSet {
     return *attributes_;
   }
 
-  void Ref() { ++ref_count_; }
-  void Deref() {
-    DCHECK_GT(ref_count_, 0);
-    --ref_count_;
-    if (!ref_count_)
-      Destroy();
-  }
-
   void Combine(const InvalidationSet& other);
 
  protected:
@@ -175,19 +174,14 @@ class CORE_EXPORT InvalidationSet {
   }
 
  private:
-  void Destroy();
+  friend class WTF::RefCounted<InvalidationSet, InvalidationSetDeleter>;
+  friend struct InvalidationSetDeleter;
+  void Destroy() const;
 
   HashSet<AtomicString>& EnsureClassSet();
   HashSet<AtomicString>& EnsureIdSet();
   HashSet<AtomicString>& EnsureTagNameSet();
   HashSet<AtomicString>& EnsureAttributeSet();
-
-  // Implement reference counting manually so we can call a derived
-  // class destructor when the reference count decreases to 0.
-  // If we use RefCounted instead, at least one of our compilers
-  // requires the ability for RefCounted<InvalidationSet>::Deref()
-  // to call ~InvalidationSet(), but this is not a virtual call.
-  int ref_count_;
 
   // FIXME: optimize this if it becomes a memory issue.
   std::unique_ptr<HashSet<AtomicString>> classes_;
