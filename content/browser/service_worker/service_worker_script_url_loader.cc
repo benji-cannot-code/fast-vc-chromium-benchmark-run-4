40c6e2df066765dde57c6f87ae2cf0ec4d977bd8
Hiroki Nakagawa <nhiroki@chromium.org>
2017-09-27 14:21:03 +0000

    S13nServiceWorker: Check certificate errors in ServiceWorkerScriptURLLoader
    This CL makes ServiceWorkerScriptURLLoader check certificate errors. The errors
    should be ignored when the --ignore-certificate-errors flag etc are specified,
    but it's not implemented in this CL yet.
    
    Bug: 748415
    Change-Id: Ie62043fd307929b978f37ecec0bb359aa2201f77
    Reviewed-on: https://chromium-review.googlesource.com/686016
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504660}

--- content/browser/service_worker/service_worker_script_url_loader.cc
+++ content/browser/service_worker/service_worker_script_url_loader.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/url_loader_factory_getter.h"
 #include "content/common/service_worker/service_worker_utils.h"
 #include "content/public/common/resource_response.h"
+#include "net/cert/cert_status_flags.h"
 #include "third_party/WebKit/common/mime_util/mime_util.h"
 
 namespace content {
@@ -159,7 +160,17 @@ void ServiceWorkerScriptURLLoader::OnReceiveResponse(
     return;
   }
 
-  // TODO(nhiroki): Check the SSL certificate.
+  // Check the certificate error.
+  // TODO(nhiroki): Ignore the certificate error when the
+  // --ignore-certificate-errors flag etc are specified.
+  // See ShouldIgnoreSSLError() in service_worker_write_to_cache_job.cc.
+  if (net::IsCertStatusError(response_head.cert_status)) {
+    // TODO(nhiroki): Show an error message equivalent to kSSLError in
+    // service_worker_write_to_cache_job.cc.
+    CommitCompleted(
+        ResourceRequestCompletionStatus(net::ERR_INSECURE_RESPONSE));
+    return;
+  }
 
   if (resource_type_ == RESOURCE_TYPE_SERVICE_WORKER) {
     if (!blink::IsSupportedJavascriptMimeType(response_head.mime_type)) {
