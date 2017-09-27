91de8a360486ed9e72081b5bb0f72174eb7ee2e3
Andrey Kraynov <iceman@yandex-team.ru>
2017-09-27 05:30:04 +0000

    Change function {Previous,Next}GraphemeBoundaryOf() to use |const Node&|
    The original version can't handle a nullptr, so it can be changed to
    take a reference.
    
    Bug: 766448
    Change-Id: Ifd9c3dc3e8cb3504cc0590120f540c8afbd4b319
    Reviewed-on: https://chromium-review.googlesource.com/673383
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504586}

--- third_party/WebKit/Source/core/editing/SelectionModifierCharacter.cpp
+++ third_party/WebKit/Source/core/editing/SelectionModifierCharacter.cpp
@@ -89,7 +89,7 @@ struct TraversalLeft {
   }
 
   static int ForwardGraphemeBoundaryOf(TextDirection direction,
-                                       Node* node,
+                                       const Node& node,
                                        int offset) {
     if (direction == TextDirection::kLtr)
       return PreviousGraphemeBoundaryOf(node, offset);
@@ -181,7 +181,7 @@ struct TraversalRight {
   }
 
   static int ForwardGraphemeBoundaryOf(TextDirection direction,
-                                       Node* node,
+                                       const Node& node,
                                        int offset) {
     if (direction == TextDirection::kLtr)
       return NextGraphemeBoundaryOf(node, offset);
@@ -276,7 +276,7 @@ static PositionTemplate<Strategy> TraverseInternalAlgorithm(
       }
 
       offset = Traversal::ForwardGraphemeBoundaryOf(
-          box->Direction(), line_layout_item.GetNode(), offset);
+          box->Direction(), *line_layout_item.GetNode(), offset);
 
       const int caret_min_offset = box->CaretMinOffset();
       const int caret_max_offset = box->CaretMaxOffset();
