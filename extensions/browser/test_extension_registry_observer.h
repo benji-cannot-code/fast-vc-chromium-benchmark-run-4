dc9ac3163e25dd9cd798110d0fd808df46d4f976
Michael Giuffrida <michaelpg@chromium.org>
2017-09-28 23:21:21 +0000

    ExtensionRegistrar for enabling and disabling extensions
    Classes will use ExtensionRegistrar to enable and disable extensions, as
    well as add, remove, reload, and terminate them (later CLs).
    
    Currently Chrome's ExtensionService does this, among many other things.
    A standalone class for this is preferred because:
    * Code outside //chrome uses similar steps to run extensions, so we
      should share this code in //extensions
    * ExtensionService already does too much and is very complex
    
    This CL adds EnableExtension() and DisableExtension() to
    ExtensionRegistrar. ExtensionService still does some Chrome-specific
    work, but later we'll factor that out into a delegate.
    
    Later CLs will add the other extension lifecycle methods, so eventually
    ExtensionRegistrar is the only class allowed to change the
    ExtensionRegistry (thus the name). We'll also update calls to
    ExtensionService methods to use ExtensionRegistrar directly when it
    makes sense.
    
    
    Design doc: https://goo.gl/trZKep (Google-internal).
    
    Bug: 762642
    Test: extensions_unittests: ExtensionRegistrarTest
    Change-Id: I2910a76f873122fbc405b7cc76c766b00a8b216f
    Reviewed-on: https://chromium-review.googlesource.com/671206
    Commit-Queue: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505206}

--- extensions/browser/test_extension_registry_observer.h
+++ extensions/browser/test_extension_registry_observer.h
@@ -31,6 +31,7 @@ class TestExtensionRegistryObserver : public ExtensionRegistryObserver {
   const Extension* WaitForExtensionWillBeInstalled();
   const Extension* WaitForExtensionUninstalled();
   const Extension* WaitForExtensionLoaded();
+  const Extension* WaitForExtensionReady();
   const Extension* WaitForExtensionUnloaded();
 
  private:
@@ -46,6 +47,8 @@ class TestExtensionRegistryObserver : public ExtensionRegistryObserver {
                               extensions::UninstallReason reason) override;
   void OnExtensionLoaded(content::BrowserContext* browser_context,
                          const Extension* extension) override;
+  void OnExtensionReady(content::BrowserContext* browser_context,
+                        const Extension* extension) override;
   void OnExtensionUnloaded(content::BrowserContext* browser_context,
                            const Extension* extension,
                            UnloadedExtensionReason reason) override;
@@ -55,6 +58,7 @@ class TestExtensionRegistryObserver : public ExtensionRegistryObserver {
   std::unique_ptr<Waiter> will_be_installed_waiter_;
   std::unique_ptr<Waiter> uninstalled_waiter_;
   std::unique_ptr<Waiter> loaded_waiter_;
+  std::unique_ptr<Waiter> ready_waiter_;
   std::unique_ptr<Waiter> unloaded_waiter_;
 
   ScopedObserver<ExtensionRegistry, ExtensionRegistryObserver>
