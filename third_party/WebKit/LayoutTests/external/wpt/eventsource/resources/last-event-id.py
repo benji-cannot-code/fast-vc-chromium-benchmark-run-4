aa6a073bcf8976b16c10938634df25a091161fb7
Takeshi Yoshino <tyoshino@chromium.org>
2017-09-28 05:25:17 +0000

    Clean up last-event-id.py
    - replace the U+2026 in UTF-8 with escape sequences, and remove the
      "coding" magic comment so that it's easier to edit
    - align indentation
    - define a variable where it's used
    
    Bug: 
    Change-Id: I73245bcadfd29bd666315d3f2931dfb67f6c82e7
    Reviewed-on: https://chromium-review.googlesource.com/683854
    Commit-Queue: Takeshi Yoshino <tyoshino@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504914}

--- third_party/WebKit/LayoutTests/external/wpt/eventsource/resources/last-event-id.py
+++ third_party/WebKit/LayoutTests/external/wpt/eventsource/resources/last-event-id.py
@@ -1,12 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
- # -*- coding: utf-8 -*-
-
 def main(request, response):
   response.headers.set("Content-Type", "text/event-stream")
-  last_event_id = request.headers.get('Last-Event-ID', None)
-  value = request.GET.first("idvalue", "…")
 
-  if(last_event_id):
-      return "data: " + last_event_id + "\n\n"
+  last_event_id = request.headers.get("Last-Event-ID", None)
+  if last_event_id:
+    return "data: " + last_event_id + "\n\n"
   else:
-    return "id: " + value + "\nretry: 200\ndata: hello\n\n"
+    idvalue = request.GET.first("idvalue", u"\u2026")
+    return "id: " + idvalue + "\nretry: 200\ndata: hello\n\n"
