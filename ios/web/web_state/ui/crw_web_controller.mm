3eecfc8daef3b146c0670fb61c45a2a35ef34023
Eugene But <eugenebut@google.com>
2017-09-28 21:46:35 +0000

    Removed nsurlrequest_util.h include from crw_web_controller.mm.
    TBR=kkhorimoto@chromium.org
    
    Bug: None
    Change-Id: I33ea8d591bdbe1ef6add67d2a8a9ef43a5da0ddf
    Reviewed-on: https://chromium-review.googlesource.com/691078
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505171}

--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/values.h"
 #import "ios/net/http_response_headers_util.h"
-#import "ios/net/nsurlrequest_util.h"
 #include "ios/web/history_state_util.h"
 #import "ios/web/interstitials/web_interstitial_impl.h"
 #import "ios/web/navigation/crw_placeholder_navigation_info.h"
