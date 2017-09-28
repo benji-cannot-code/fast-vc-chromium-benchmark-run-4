b9326e9f18af6f2da58794db0609da75c09ec1f4
Becca Hughes <beccahughes@chromium.org>
2017-09-28 07:56:59 +0000

    Reland "Media Controls: Replace painter with CSS."
    This is a reland of d94db169aaa0575c69748741718b80b2526b4097
    Original change's description:
    > Media Controls: Replace painter with CSS.
    > 
    > Replace MediaControlPainter with CSS. Add some classes to some
    > elements to allow styling by state. This is a resubmit of the
    > button code only from crrev.com/c/584771.
    > 
    > BUG=746872
    > 
    > TBR=pfeldman@chromium.org
    > 
    > Change-Id: If79ce339a10f6671ddd02a223468bb3d7bbcd1df
    > Reviewed-on: https://chromium-review.googlesource.com/657024
    > Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    > Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    > Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#504095}
    
    TBR=mlamouri@chromium.org,pfeldman@chromium.org
    
    Bug: 746872
    Change-Id: Iad1df7ec72b0e581acdc39774a2460bac51557d5
    Reviewed-on: https://chromium-review.googlesource.com/684074
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504936}

--- third_party/WebKit/Source/modules/media_controls/elements/MediaControlCastButtonElement.cpp
+++ third_party/WebKit/Source/modules/media_controls/elements/MediaControlCastButtonElement.cpp
@@ -69,6 +69,7 @@ void MediaControlCastButtonElement::UpdateDisplayType() {
     }
   }
   UpdateOverflowString();
+  SetClass("on", IsPlayingRemotely());
 }
 
 bool MediaControlCastButtonElement::WillRespondToMouseClickEvents() {
