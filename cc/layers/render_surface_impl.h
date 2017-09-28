3dab9db484f36c92de6799ea6b521461d4180588
Tien-Ren Chen <trchen@chromium.org>
2017-09-28 04:33:38 +0000

    [cc] Render surface with kDstIn child surface should not occlude parent
    Just like render surface that has a mask implemented as mask layer, a
    surface that has a mask implemented as kDstIn child surface may no
    longer be opaque, thus should not occlude its parent.
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Idb2e3b0a5873747868040422490451f032c8d076
    Reviewed-on: https://chromium-review.googlesource.com/670039
    Commit-Queue: Tien-Ren Chen <trchen@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504899}

--- cc/layers/render_surface_impl.h
+++ cc/layers/render_surface_impl.h
@@ -147,6 +147,7 @@ class CC_EXPORT RenderSurfaceImpl {
 
   LayerImpl* MaskLayer();
   bool HasMask() const;
+  bool HasMaskingContributingSurface() const;
 
   const FilterOperations& Filters() const;
   const FilterOperations& BackgroundFilters() const;
