3358f8b0c55ad08036264208ad0a196cd1cfe243
Max Moroz <mmoroz@chromium.org>
2017-09-28 04:39:00 +0000

    Use "is_component_build=true" for libFuzzer UBSan builder.
    R=inferno@chromium.org, ochang@chromium.org
    TBR=dpranke@chromium.org
    
    Bug: 768918, 701825
    Change-Id: Ib36c63801d45c2fcb64cea52e17eb3b8d6db6e8c
    Reviewed-on: https://chromium-review.googlesource.com/687235
    Commit-Queue: Max Moroz <mmoroz@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Abhishek Arya <inferno@chromium.org>
    Reviewed-by: Oliver Chang <ochang@chromium.org>
    Reviewed-by: Max Moroz <mmoroz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504901}

--- content/test/fuzzer/clear_site_data_fuzzer.cc
+++ content/test/fuzzer/clear_site_data_fuzzer.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 #include <string>
 
-#include "content/browser/browsing_data/clear_site_data_throttle.h"
+#include "content/browser/browsing_data/clear_site_data_throttle.h"  // nogncheck
 
 namespace content {
 
