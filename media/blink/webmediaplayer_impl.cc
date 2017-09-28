b661207e99c3db755dddfc181bf0b61346a10683
John Chen <johnchen@chromium.org>
2017-09-28 16:16:51 +0000

    Add media trace events for perf benchmark
    Add media trace events needed to calculate buffering time, dropped
    frame count, and seek time in media perf benchmarks.
    
    Bug: 700160
    Change-Id: I340fe3763a73fe3a77e6a04d0d8761811ae6838e
    Reviewed-on: https://chromium-review.googlesource.com/674166
    Commit-Queue: John Chen <johnchen@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505045}

--- media/blink/webmediaplayer_impl.cc
+++ media/blink/webmediaplayer_impl.cc
@@ -653,6 +653,8 @@ void WebMediaPlayerImpl::Seek(double seconds) {
 
 void WebMediaPlayerImpl::DoSeek(base::TimeDelta time, bool time_updated) {
   DCHECK(main_task_runner_->BelongsToCurrentThread());
+  TRACE_EVENT1("media", "WebMediaPlayerImpl::DoSeek", "target",
+               time.InSecondsF());
 
 #if defined(OS_ANDROID)  // WMPI_CAST
   if (IsRemote()) {
@@ -1302,6 +1304,8 @@ void WebMediaPlayerImpl::OnCdmAttached(bool success) {
 }
 
 void WebMediaPlayerImpl::OnPipelineSeeked(bool time_updated) {
+  TRACE_EVENT1("media", "WebMediaPlayerImpl::OnPipelineSeeked", "target",
+               seek_time_.InSecondsF());
   seeking_ = false;
   seek_time_ = base::TimeDelta();
 
@@ -1444,6 +1448,7 @@ void WebMediaPlayerImpl::OnError(PipelineStatus status) {
 }
 
 void WebMediaPlayerImpl::OnEnded() {
+  TRACE_EVENT1("media", "WebMediaPlayerImpl::OnEnded", "duration", Duration());
   DVLOG(1) << __func__;
   DCHECK(main_task_runner_->BelongsToCurrentThread());
 
