12ef54ff9a9186527a00e764459b38b2b45176ec
Bruce Dawson <brucedawson@chromium.org>
2017-09-28 19:56:06 +0000

    Require VS 2017 Update 3.2 to build on Windows
    Last week VS 2017 was made the default compiler on Windows. It didn't
    take long for a dependency to creep in and now VS 2017 is *required* to
    build Chrome on Windows. This CL makes this explicit, to avoid confusing
    error messages, and it requires a particular minimum *version* of VS
    2017 in order to avoid compiler bugs that were seen in previous
    versions.
    
    crrev.com/c/678859 was the change that made VS 2017 the default.
    
    Bug: 683729
    Change-Id: I3db12c0c9d98400077e6bd41afea16d4d071352a
    Reviewed-on: https://chromium-review.googlesource.com/688746
    Reviewed-by: Daniel Bratell <bratell@opera.com>
    Reviewed-by: Justin Schuh <jschuh@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505124}

--- base/win/windows_version.cc
+++ base/win/windows_version.cc
@@ -15,8 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/win/registry.h"
 
-#if !defined(__clang__) && _MSC_FULL_VER < 190024213
-#error VS 2015 Update 3 with Cumulative Servicing Release or higher is required
+#if !defined(__clang__) && _MSC_FULL_VER < 191125507
+#error VS 2017 Update 3.2 or higher is required
 #endif
 
 #if !defined(NTDDI_WIN10_RS2)
