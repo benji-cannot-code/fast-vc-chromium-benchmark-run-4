325af386d27e3deed615a1d1ac885789b3ab6348
Alex Newcomer <newcomer@chromium.org>
2017-09-28 19:09:30 +0000

    cros: Scroll events anywhere on AppListView
    can change app grid pages
    
    New behavior: Scroll events (ET_MOUSEWHEEL and ET_SCROLL) used to modify
    the app grid if the mouse was over the grid, and modify the AppListState
    otherwise.
    
    Now, we want scroll events to first try to move the grid. If this
    succeds, set a timer to keep the stream of scrolling events from also
    closing the AppListView.
    
    If the scroll could not move the grid, allow it to modify the
    AppListState.
    
    I used a timer because there is no reliable way to both determine the
    end of an ET_SCROLL and the end of a mousewheel scroll. We need to avoid
    acting twice with one user intent to scroll one surface. The behavior of
    this change was reviewed by UX/PM and approved for M61.
    
    Misc refactoring:
    - No longer need OnMouseWheel/OnScroll in AppsGridView because AppListView
      will handle all scroll events.
    - Broke AppListView::HandleScroll into a few different functions because
      it was starting to do multiple things.
    - In HandleScroll, also combined mousewheel and touchpad logic to avoid
      code duplication.
    - Added tests to AppsGridView while debugging. Also moved the scroll test
      from FullscreenAppListPresenterDelegateTest to
      AppListViewFullscreenTest.
    
    TEST=manual and automated
    
    BUG: 769356
    Bug: 
    Change-Id: I15396b9845404d0c13de916e01ef62ea9af22ae8
    Reviewed-on: https://chromium-review.googlesource.com/688637
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505112}

--- ui/app_list/views/app_list_view.h
+++ ui/app_list/views/app_list_view.h
@@ -24,6 +24,10 @@ namespace aura {
 class Window;
 }
 
+namespace base {
+class ElapsedTimer;
+}
+
 namespace display {
 class Screen;
 }
@@ -65,6 +69,10 @@ class APP_LIST_EXPORT AppListView : public views::BubbleDialogDelegateView,
 
   // The defualt color of the app list background.
   static constexpr SkColor kDefaultBackgroundColor = SK_ColorBLACK;
+
+  // The duration the AppListView ignores scroll events which could transition
+  // its state.
+  static constexpr int kScrollIgnoreTimeMs = 500;
 
   enum AppListState {
     // Closes |app_list_main_view_| and dismisses the delegate.
@@ -247,6 +255,12 @@ class APP_LIST_EXPORT AppListView : public views::BubbleDialogDelegateView,
   // AppListView.
   void HandleClickOrTap(ui::LocatedEvent* event);
 
+  // Sets or restarts the scroll ignore timer.
+  void SetOrRestartScrollIgnoreTimer();
+
+  // Whether scroll events should be ignored.
+  bool ShouldIgnoreScrollEvents();
+
   // Initializes |initial_drag_point_|.
   void StartDrag(const gfx::Point& location);
 
@@ -375,6 +389,9 @@ class APP_LIST_EXPORT AppListView : public views::BubbleDialogDelegateView,
 
   // True if the dragging started from PEEKING state.
   bool drag_started_from_peeking_ = false;
+
+  // Timer to ignore scroll events which would close the view by accident.
+  std::unique_ptr<base::ElapsedTimer> scroll_ignore_timer_;
 
   // Accessibility announcement dialogue.
   base::string16 state_announcement_;
