789f82ecad5176294c3162495d3e7a24801227ce
Sriram <srirama.m@samsung.com>
2017-09-28 04:38:24 +0000

    [RLS] Add test to track layout pass count
    Add test coverage to make sure there isn't an extra layout for a page
    which requires scrollbar. Scrollbar is created by default to avoid
    extra layout incase of RLS.
    
    Bug: 763341
    Change-Id: I3e935dee974429fedf1c7ef41e9d3ec19cfb1698
    Reviewed-on: https://chromium-review.googlesource.com/680194
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: srirama chandra sekhar <srirama.m@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#504900}

--- third_party/WebKit/Source/core/layout/LayoutBlockFlow.h
+++ third_party/WebKit/Source/core/layout/LayoutBlockFlow.h
@@ -743,6 +743,8 @@ class CORE_EXPORT LayoutBlockFlow : public LayoutBlock {
   static void UpdateAncestorShouldPaintFloatingObject(
       const LayoutBox& float_box);
 
+  int GetLayoutPassCountForTesting();
+
  protected:
   LayoutUnit MaxPositiveMarginBefore() const {
     return rare_data_
@@ -978,6 +980,7 @@ class CORE_EXPORT LayoutBlockFlow : public LayoutBlock {
   // Positions new floats and also adjust all floats encountered on the line if
   // any of them have to move to the next page/column.
   void PositionDialog();
+  void IncrementLayoutPassCount();
 
   // END METHODS DEFINED IN LayoutBlockFlowLine
 };
---
+++ third_party/WebKit/Source/core/layout/LayoutCountTest.cpp
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/layout/LayoutBlockFlow.h"
+#include "core/layout/LayoutTestHelper.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace blink {
+
+class LayoutCountTest : public RenderingTest {};
+
+TEST_F(LayoutCountTest, SimpleBlockLayoutIsOnePass) {
+  RuntimeEnabledFeatures::SetTrackLayoutPassesPerBlockEnabled(true);
+  RuntimeEnabledFeatures::SetRootLayerScrollingEnabled(true);
+  SetBodyInnerHTML(
+      "<!DOCTYPE html>"
+      " <div id='block' style='height:1000px'>Item</div>");
+
+  LayoutBlockFlow* block = ToLayoutBlockFlow(
+      GetDocument().getElementById("block")->GetLayoutObject());
+  ASSERT_EQ(block->GetLayoutPassCountForTesting(), 1);
+}
+
+}  // namespace blink
