c21f017eae71aa984dfc515d700e7279a59b8945
James Cook <jamescook@chromium.org>
2017-09-28 22:34:22 +0000

    cros: Eliminate MultiUserWindowManager::MultiUserMode
    There's only one mode (split desktops). The uninitialized state isn't
    needed anymore.  It was added a long time ago as an optimization to
    avoid looking up flag values and those flags no longer exist.
    
    Bug: none
    Test: unit_tests, browser_tests, manually test teleporting windows between user sessions and verify avatar icons appear in the frames
    Change-Id: Icc660ca2af047b39066ae501a5c6dc6a28f7aa8f
    Reviewed-on: https://chromium-review.googlesource.com/687986
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505195}

--- chrome/browser/ui/ash/launcher/chrome_launcher_controller.cc
+++ chrome/browser/ui/ash/launcher/chrome_launcher_controller.cc
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/launcher/multi_profile_browser_status_monitor.h"
 #include "chrome/browser/ui/ash/multi_user/multi_user_util.h"
 #include "chrome/browser/ui/ash/multi_user/multi_user_window_manager.h"
+#include "chrome/browser/ui/ash/session_controller_client.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_list.h"
@@ -232,8 +233,7 @@ ChromeLauncherController::ChromeLauncherController(Profile* profile,
 
   // On Chrome OS using multi profile we want to switch the content of the shelf
   // with a user change. Note that for unit tests the instance can be NULL.
-  if (chrome::MultiUserWindowManager::GetMultiProfileMode() !=
-      chrome::MultiUserWindowManager::MULTI_PROFILE_MODE_OFF) {
+  if (SessionControllerClient::IsMultiProfileAvailable()) {
     user_switch_observer_.reset(
         new ChromeLauncherControllerUserSwitchObserver(this));
   }
@@ -241,8 +241,7 @@ ChromeLauncherController::ChromeLauncherController(Profile* profile,
   std::unique_ptr<AppWindowLauncherController> extension_app_window_controller;
   // Create our v1/v2 application / browser monitors which will inform the
   // launcher of status changes.
-  if (chrome::MultiUserWindowManager::GetMultiProfileMode() ==
-      chrome::MultiUserWindowManager::MULTI_PROFILE_MODE_ON) {
+  if (SessionControllerClient::IsMultiProfileAvailable()) {
     // If running in separated destkop mode, we create the multi profile version
     // of status monitor.
     browser_status_monitor_.reset(new MultiProfileBrowserStatusMonitor(this));
