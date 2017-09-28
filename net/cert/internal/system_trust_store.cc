eb09f5d5df0da6184d20eec550b4a4a38ad0accc
David Benjamin <davidben@chromium.org>
2017-09-28 18:50:08 +0000

    Remove unused include.
    The Fuchsia code is not using the legacy X.509 stack.
    
    Bug: 706445
    Change-Id: I6c2cc42bc302c01d38067657317aa1b958442270
    Reviewed-on: https://chromium-review.googlesource.com/690376
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505102}

--- net/cert/internal/system_trust_store.cc
+++ net/cert/internal/system_trust_store.cc
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cert/x509_util_mac.h"
 #elif defined(OS_FUCHSIA)
 #include "third_party/boringssl/src/include/openssl/pool.h"
-#include "third_party/boringssl/src/include/openssl/x509.h"
 #endif
 
 namespace net {
