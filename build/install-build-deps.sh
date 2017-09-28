9c0ccd82bbd8f36f16c50185c3ec4e2971416171
Tim Brown <timbrown@chromium.org>
2017-09-28 22:08:00 +0000

    Add libdconf-dev to to install-build-deps.sh
    This is the first step in moving from using gconf to dconf.
    
    Bug: 768027
    Change-Id: Ifb05a6679c38e115b322e0a7caa6c3aad309389e
    Reviewed-on: https://chromium-review.googlesource.com/691097
    Commit-Queue: Tim Brown <timbrown@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505183}

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -166,6 +166,7 @@ dev_list="\
   libcap-dev
   libcups2-dev
   libcurl4-gnutls-dev
+  libdconf-dev
   libdrm-dev
   libelf-dev
   libffi-dev
@@ -233,6 +234,7 @@ lib_list="\
   libcairo2
   libcap2
   libcups2
+  libdconf1
   libexpat1
   libffi6
   libfontconfig1
@@ -378,6 +380,7 @@ nacl_list="\
   libasound2:i386
   libcap2:i386
   libelf-dev:i386
+  libdconf-dev:i386
   libfontconfig1:i386
   libgconf-2-4:i386
   libglib2.0-0:i386
