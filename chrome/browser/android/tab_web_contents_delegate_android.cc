86fac3a1519d893603e77afaa95807a80111680d
Lukasz Anforowicz <lukasza@chromium.org>
2017-09-28 12:36:51 +0000

    Removing callers of the WebContents::GetRenderProcessHost() method.
    WebContents::GetRenderProcessHost is an API that doesn't behave as
    expected with out-of-process iframes, as multiple processes can be
    associated with a single WebContents.  This CL replaces callers of this
    API, so that they go through an equivalent API that explicitly selects
    which frame's process is needed.
    
    This CL was started by using an ad-hoc clang-plugin that replaced
    existing callsites of WebContents::GetRenderProcessHost() with a call to
    something like wc->GetMainFrame()->GetProcess().  This was followed-up
    by manually adding an include of render_frame_host.h and git cl format
    and git cl lint and then a self-review with small tweaks (e.g. sometimes
    using a wc->GetRenderViewHost()->GetProcess() is more appropriate).
    
    This CL was uploaded by git cl split.
    
    R=bauerb@chromium.org
    
    Bug: 666525
    Change-Id: Ib10ad6ad0f52490a16200addd4f63cd60cb431ee
    Reviewed-on: https://chromium-review.googlesource.com/689116
    Commit-Queue: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504979}

--- chrome/browser/android/tab_web_contents_delegate_android.cc
+++ chrome/browser/android/tab_web_contents_delegate_android.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_details.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_source.h"
+#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
@@ -441,8 +442,8 @@ void OnRendererUnresponsive(JNIEnv* env,
   InfoBarService* infobar_service =
       InfoBarService::FromWebContents(web_contents);
   DCHECK(!FindHungRendererInfoBar(infobar_service));
-  HungRendererInfoBarDelegate::Create(infobar_service,
-                                      web_contents->GetRenderProcessHost());
+  HungRendererInfoBarDelegate::Create(
+      infobar_service, web_contents->GetMainFrame()->GetProcess());
 }
 
 void OnRendererResponsive(JNIEnv* env,
