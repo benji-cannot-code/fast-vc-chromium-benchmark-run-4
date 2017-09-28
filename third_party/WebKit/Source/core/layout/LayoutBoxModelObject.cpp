69881fb9f7772e6b6084e2b26ba1d4563364d638
Fredrik Söderquist <fs@opera.com>
2017-09-28 17:19:28 +0000

    LayoutBoxModelObject::HasNonEmptyLayoutSize ignores SVG model objects
    Extend the DCHECK to also cover SVG layout objects. The IsBox() check
    should cover the SVG root, and if it is zero-sized any descendant should
    not be considered to have a "non-empty" layout size. Overflow is
    disregarded (as it seems to be in general in this method.)
    
    Bug: 769459
    Change-Id: Iea15b86032094a15608b614f5baa1057c5d6a686
    Reviewed-on: https://chromium-review.googlesource.com/689997
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#505072}

--- third_party/WebKit/Source/core/layout/LayoutBoxModelObject.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBoxModelObject.cpp
@@ -604,7 +604,7 @@ bool LayoutBoxModelObject::HasNonEmptyLayoutSize() const {
         if (!layout_inline.LinesBoundingBox().IsEmpty())
           return true;
       } else {
-        DCHECK(object->IsText());
+        DCHECK(object->IsText() || object->IsSVG());
       }
     }
   }
