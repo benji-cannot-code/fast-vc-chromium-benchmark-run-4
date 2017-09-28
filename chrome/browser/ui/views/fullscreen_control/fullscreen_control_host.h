c6461ccee3ff4d9a16b0b4d284d604d179bb0fe2
Yuwei Huang <yuweih@chromium.org>
2017-09-28 00:36:49 +0000

    [Fullscreen Control] Finish up the fullscreen control UI and add the animation class
    This CL finishes up the fullscreen control UI. It is a transparent-black
    circular button with a close (X) icon on the middle. It is used both as an
    indicator for exiting fullscreen when the keyboard lock reserves the ESC
    key and an actual button to exit fullscreen for touch devices.
    
    This CL also implements a FullscreenControlPopup class to show the
    indicator with a dropdown animation in replacement of the DropdownBarHost
    implementation.
    
    This is the UX spec (Google internal):
    https://docs.google.com/presentation/d/11D86S87NZK5sAMLhc8WSV9pk_VWkjMAjvnN08uUD5po/edit#slide=id.g23756e43b7_3_0
    
    Bug: 758456
    Change-Id: I44dc2d0944386f5f6f5e607831c5ea4e38364357
    Reviewed-on: https://chromium-review.googlesource.com/647950
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504816}

--- chrome/browser/ui/views/fullscreen_control/fullscreen_control_host.h
+++ chrome/browser/ui/views/fullscreen_control/fullscreen_control_host.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_FULLSCREEN_CONTROL_FULLSCREEN_CONTROL_HOST_H_
 
 #include "base/macros.h"
-#include "chrome/browser/ui/views/dropdown_bar_host.h"
+#include "chrome/browser/ui/views/fullscreen_control/fullscreen_control_popup.h"
 #include "ui/events/event_handler.h"
 
 class BrowserView;
@@ -24,25 +24,24 @@ namespace views {
 class View;
 }  // namespace views
 
-class FullscreenControlHost : public DropdownBarHost, public ui::EventHandler {
+class FullscreenControlHost : public ui::EventHandler {
  public:
   // |host_view| allows the host to control the z-order of the underlying view.
   explicit FullscreenControlHost(BrowserView* browser_view,
                                  views::View* host_view);
   ~FullscreenControlHost() override;
 
-  // DropdownBarHost:
-  bool AcceleratorPressed(const ui::Accelerator& accelerator) override;
-  bool CanHandleAccelerators() const override;
-  gfx::Rect GetDialogPosition(gfx::Rect avoid_overlapping_rect) override;
-
   // ui::EventHandler:
   void OnMouseEvent(ui::MouseEvent* event) override;
   void OnTouchEvent(ui::TouchEvent* event) override;
   void OnGestureEvent(ui::GestureEvent* event) override;
 
+  void Hide(bool animate);
+
+  bool IsVisible() const;
+
   FullscreenControlView* fullscreen_control_view() {
-    return fullscreen_control_view_;
+    return fullscreen_control_popup_.control_view();
   }
 
  private:
@@ -54,9 +53,6 @@ class FullscreenControlHost : public DropdownBarHost, public ui::EventHandler {
     TOUCH,       // A touch event caused the view to show.
   };
 
-  // DropdownBarHost:
-  void OnVisibilityChanged() override;
-
   void HandleFullScreenControlVisibility(const ui::LocatedEvent* event,
                                          InputEntryMethod input_entry_method);
   void ShowForInputEntryMethod(InputEntryMethod input_entry_method);
@@ -65,8 +61,7 @@ class FullscreenControlHost : public DropdownBarHost, public ui::EventHandler {
 
   BrowserView* const browser_view_;
 
-  // Owned by DropdownBarHost.
-  FullscreenControlView* const fullscreen_control_view_;
+  FullscreenControlPopup fullscreen_control_popup_;
 
   DISALLOW_COPY_AND_ASSIGN(FullscreenControlHost);
 };
