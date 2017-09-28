cd898fba7eb6e029ff588edd5ef16adb2e9a2403
Emil A Eklund <eae@chromium.org>
2017-09-28 01:16:09 +0000

    [LayoutNG] Implement text decoration painting
    Add support for text decoration (underline, overline, and strikethrough)
    painting for LayoutNG. Shares most of the line position resolution logic
    and paint implementation with legacy layout.
    
    Correctly supports roman underlines while the under (element edge) style
    isn't fully supported due to missing baseline and decorating box data.
    
    Edit
    
    Bug: 714962
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ie493469d7846605626c395645017e7214ae5224e
    Reviewed-on: https://chromium-review.googlesource.com/686091
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504853}

--- third_party/WebKit/Source/core/layout/TextDecorationOffset.cpp
+++ third_party/WebKit/Source/core/layout/TextDecorationOffset.cpp
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "core/layout/TextDecorationOffset.h"
+
 #include "core/layout/line/InlineTextBox.h"
 #include "core/layout/line/RootInlineBox.h"
 #include "platform/fonts/FontMetrics.h"
