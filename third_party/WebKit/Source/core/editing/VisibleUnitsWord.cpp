aa06bea98c8a829d6a86de0977443687e65ca981
Yoshifumi Inoue <yosin@chromium.org>
2017-09-28 09:32:13 +0000

    Get rid of redundant include directives from VisubleUnitsWord.cpp
    This patch gets rid of redundant include directives from "VisubleUnitsWord.cpp"
    for improving code health.
    
    Change-Id: I381465fda4a76724df044d59a3a806cdc4d21475
    Reviewed-on: https://chromium-review.googlesource.com/686023
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504954}

--- third_party/WebKit/Source/core/editing/VisibleUnitsWord.cpp
+++ third_party/WebKit/Source/core/editing/VisibleUnitsWord.cpp
@@ -33,14 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/editing/EditingUtilities.h"
 #include "core/editing/VisiblePosition.h"
-#include "core/editing/iterators/BackwardsCharacterIterator.h"
-#include "core/editing/iterators/BackwardsTextBuffer.h"
-#include "core/editing/iterators/CharacterIterator.h"
-#include "core/editing/iterators/ForwardsTextBuffer.h"
-#include "core/editing/iterators/SimplifiedBackwardsTextIterator.h"
 #include "platform/instrumentation/tracing/TraceEvent.h"
 #include "platform/text/TextBoundaries.h"
-#include "platform/text/TextBreakIterator.h"
 
 namespace blink {
 
