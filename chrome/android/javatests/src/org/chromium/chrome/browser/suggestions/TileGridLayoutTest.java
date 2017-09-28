596cb451ef2ed257c43c64dfd3571ee28dd06a38
Xida Chen <xidachen@chromium.org>
2017-09-28 14:33:56 +0000

    Disable TileGridLayoutTest#testModernTileGridAppearance_Two
    TBR=dgn@chromium.org
    NOTRY=true
    
    Bug: 769738
    Change-Id: Ied6cdecac72229353f3c41ca769b6d872ea7a2ea
    Reviewed-on: https://chromium-review.googlesource.com/690238
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505016}

--- chrome/android/javatests/src/org/chromium/chrome/browser/suggestions/TileGridLayoutTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/suggestions/TileGridLayoutTest.java
@@ -187,8 +187,9 @@ public class TileGridLayoutTest {
     }
 
     @Test
-    @MediumTest
-    @Feature({"NewTabPage", "RenderTest"})
+    // @MediumTest
+    // @Feature({"NewTabPage", "RenderTest"})
+    @DisabledTest
     @ChromeHome
     @Restriction(UiRestriction.RESTRICTION_TYPE_PHONE)
     public void testModernTileGridAppearance_Two() throws IOException, InterruptedException {
