93e76b6d5b7978bcf975331f2248ae8e179c37c1
Jun Cai <juncai@chromium.org>
2017-09-28 17:02:09 +0000

    Change device chooser paired text to be white when selected (Mac)
    On Mac, currently when an entry is selected in the device chooser, the
    paired text is shown as light green. This CL changes its color to be
    white to increase the contrast.
    
    I uploaded a screenshot on the issue page.
    
    Bug: 711563
    Change-Id: I4842bfe5e1ea04fcc0a868b8d6f16b8552bd2d20
    Reviewed-on: https://chromium-review.googlesource.com/688753
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505066}

--- chrome/browser/ui/cocoa/extensions/chooser_dialog_cocoa_controller_unittest.mm
+++ chrome/browser/ui/cocoa/extensions/chooser_dialog_cocoa_controller_unittest.mm
@@ -484,8 +484,7 @@ TEST_F(ChooserDialogCocoaControllerTest,
   ExpectRowTextColorIs(0, [NSColor whiteColor]);
   ExpectRowTextColorIs(1, [NSColor blackColor]);
   ExpectRowTextColorIs(2, [NSColor blackColor]);
-  ExpectPairedTextColorIs(
-      0, skia::SkColorToCalibratedNSColor(gfx::kGoogleGreen300));
+  ExpectPairedTextColorIs(0, [NSColor whiteColor]);
 
   // Deselect option 0.
   [table_view_ deselectRow:0];
@@ -591,8 +590,7 @@ TEST_F(ChooserDialogCocoaControllerTest,
   ExpectRowTextColorIs(0, [NSColor blackColor]);
   ExpectRowTextColorIs(1, [NSColor blackColor]);
   ExpectRowTextColorIs(2, [NSColor whiteColor]);
-  ExpectPairedTextColorIs(
-      2, skia::SkColorToCalibratedNSColor(gfx::kGoogleGreen300));
+  ExpectPairedTextColorIs(2, [NSColor whiteColor]);
 }
 
 TEST_F(ChooserDialogCocoaControllerTest, SelectAndDeselectAnOption) {
@@ -1152,8 +1150,7 @@ TEST_F(ChooserDialogCocoaControllerTest, MultipleSelection) {
   ExpectRowTextColorIs(3, [NSColor whiteColor]);
   ExpectRowTextColorIs(4, [NSColor blackColor]);
   ExpectRowTextColorIs(5, [NSColor blackColor]);
-  ExpectPairedTextColorIs(
-      0, skia::SkColorToCalibratedNSColor(gfx::kGoogleGreen300));
+  ExpectPairedTextColorIs(0, [NSColor whiteColor]);
   EXPECT_TRUE(connect_button_.enabled);
 
   // Remove option "b".
