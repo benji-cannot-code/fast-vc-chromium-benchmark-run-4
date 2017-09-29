471a5964b7e0def6264069e9486aa019c11b0fb7
Brett Wilson <brettw@chromium.org>
2017-09-29 19:50:14 +0000

    Revert "Add UKM Document.OutliveTimeAfterShutdown"
    This reverts commit 55364a64915fe7aca37fc3e02d09a9fa667df006.
    
    Reason for revert: Broke component build
    https://build.chromium.org/p/chromium.gpu.fyi/builders/GPU%20Win%20Clang%20Builder%20%28dbg%29/builds/16604
    While that's just an FYI bot, this also broke my local x86 Windows debug builds.
    
    Original change's description:
    > Add UKM Document.OutliveTimeAfterShutdown
    > 
    > This CL adds a new UKM Document.OutliveTimeAfterShutdown, that is
    > recorded when a Document object survives 5, 10, 20 or 50 garbage
    > collections after detached. If a document outlives such long time, the
    > document might be leaked. The UKM would be very useful to know where such
    > leaky documents exist and to fix them.
    > 
    > Design doc: https://docs.google.com/document/d/1fbs5smdd-pBLLMpq7u8EkyddZILtI7CZPJlo_AA1kak/edit?usp=sharing
    > 
    > Bug: 757374
    > Change-Id: Idc05b03f625db11ab54c5203d18344b1ca72b4eb
    > Reviewed-on: https://chromium-review.googlesource.com/647050
    > Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Reviewed-by: Chris Palmer <palmer@chromium.org>
    > Reviewed-by: Steven Holte <holte@chromium.org>
    > Reviewed-by: Brett Wilson <brettw@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Nico Weber <thakis@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#505275}
    
    TBR=palmer@chromium.org,thakis@chromium.org,brettw@chromium.org,erikchen@chromium.org,hajimehoshi@chromium.org,haraken@chromium.org,tkent@chromium.org,holte@chromium.org
    
    Change-Id: Ie3e3e3ec3ce401d934cd5af4453e0ca92a1d8a63
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 757374
    Reviewed-on: https://chromium-review.googlesource.com/692840
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Commit-Queue: Brett Wilson <brettw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505448}

--- services/metrics/public/cpp/ukm_recorder.h
+++ services/metrics/public/cpp/ukm_recorder.h
@@ -33,10 +33,6 @@ namespace autofill {
 class AutofillMetrics;
 }
 
-namespace blink {
-class Document;
-}
-
 namespace content {
 class RenderFrameImpl;
 class RenderWidgetHostLatencyTracker;
@@ -106,7 +102,6 @@ class METRICS_EXPORT UkmRecorder {
 
  private:
   friend autofill::AutofillMetrics;
-  friend blink::Document;
   friend payments::JourneyLogger;
   friend ContextualSearchRankerLoggerImpl;
   friend ProcessMemoryMetricsEmitter;
