98222af343587662aeee5e2132b0e0d16ee12b11
Tomasz Garbus <tgarbus@chromium.org>
2017-09-29 12:55:43 +0000

    Fetch and display user's avatar on auto sign-in snackbar.
    In this CL I added an image view on auto sign-in snackbar, displaying
    user's avatar. Until avatar is fetched, a placeholder image is shown.
    For network traffic annotation tag, I used this sample:
    https://cs.chromium.org/chromium/src/tools/traffic_annotation/sample_traffic_annotation.cc?l=120
    
    Image view size is 24x24pt, fetched image is scaled accordingly.
    
    Screenshot with fetched icon: https://screenshot.googleplex.com/ruQRUXGOLN9.png
    Screenshot with placeholder: https://screenshot.googleplex.com/bYUnFR0pFru.png
    Screenshot with big (1024x1024) icon: https://screenshot.googleplex.com/4OwJuwCdj3J.png
    
    Bug: 435048
    Change-Id: Ib74ef74716d81ab661ee2aa4b7246e27ebab3276
    Reviewed-on: https://chromium-review.googlesource.com/681254
    Commit-Queue: Tomasz Garbus <tgarbus@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Tatiana Gornak <melandory@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505356}

--- ios/chrome/browser/passwords/notify_auto_signin_view_controller.h
+++ ios/chrome/browser/passwords/notify_auto_signin_view_controller.h
@@ -8,20 +8,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
-// TODO(crbug.com/435048): Add UIImageView with user's avatar.
+namespace net {
+class URLRequestContextGetter;
+}  // namespace net
+
+class GURL;
 
 // UIViewController for the notification about auto sign-in.
 @interface NotifyUserAutoSigninViewController : UIViewController
 
-- (instancetype)initWithUsername:(NSString*)username NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithUsername:(NSString*)username
+                         iconURL:(GURL)iconURL
+                   contextGetter:(net::URLRequestContextGetter*)contextGetter
+    NS_DESIGNATED_INITIALIZER;
 
 - (instancetype)initWithNibName:(NSString*)nibNameOrNil
                          bundle:(NSBundle*)nibBundleOrNil NS_UNAVAILABLE;
 - (instancetype)initWithCoder:(NSCoder*)aDecoder NS_UNAVAILABLE;
 - (instancetype)init NS_UNAVAILABLE;
-
-// Username, corresponding to Credential.id field in JS.
-@property(copy, nonatomic) NSString* username;
 
 @end
 
