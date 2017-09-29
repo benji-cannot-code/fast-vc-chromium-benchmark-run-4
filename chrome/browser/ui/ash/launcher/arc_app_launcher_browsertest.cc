886f7cb0d1a3aafb721a36b42fc6d35d8e467fde
Mike Wasserman <msw@chromium.org>
2017-09-29 01:30:54 +0000

    ChromeOS: Add test support for ShelfModel synchronization.
    Fix tests that break with --ash-enable-shelf-model-synchronization.
    (I'm hoping to imminently enable that functionality by default)
    
    Perform some extra cleanup in chrome_launcher_controller_browsertest.cc
    (Fix ShelfModel use, avoid Shelf, nix redundant fixture, add another)
    
    Bug: 557406
    Test: Automated
    Change-Id: If93c8d0c3ec70f4aa427b5eab2b2c43ffc571158
    Reviewed-on: https://chromium-review.googlesource.com/691104
    Commit-Queue: Michael Wasserman <msw@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505244}

--- chrome/browser/ui/ash/launcher/arc_app_launcher_browsertest.cc
+++ chrome/browser/ui/ash/launcher/arc_app_launcher_browsertest.cc
@@ -257,7 +257,7 @@ class ArcAppLauncherBrowserTest : public ExtensionBrowserTest {
   }
 
   ash::ShelfItemDelegate* GetShelfItemDelegate(const std::string& id) {
-    ash::ShelfModel* model = ash::Shell::Get()->shelf_model();
+    auto* model = ChromeLauncherController::instance()->shelf_model();
     return model->GetShelfItemDelegate(ash::ShelfID(id));
   }
 
