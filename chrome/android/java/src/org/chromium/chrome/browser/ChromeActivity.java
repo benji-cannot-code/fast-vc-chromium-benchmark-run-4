222aa978db6f6858bc7464adaf97bdd4822ef29e
Shakti Sahu <shaktisahu@chromium.org>
2017-09-29 02:13:49 +0000

    IPH : Various Fixes
    1 - Fixed download page screenshot to point to download arrow.
    2 - Dismiss bubbles on hardware menu press
    3 - Fixed accessibility text for download home IPH
    
    Bug: 768304, 768585, 765596
    Change-Id: I11d1fe64cee9ba99a9fbe3603a58b72097ac8c49
    Reviewed-on: https://chromium-review.googlesource.com/688059
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505255}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -1374,6 +1374,7 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
     protected void showAppMenuForKeyboardEvent() {
         if (getAppMenuHandler() == null) return;
 
+        TextBubble.dismissBubbles();
         boolean hasPermanentMenuKey = ViewConfiguration.get(this).hasPermanentMenuKey();
         getAppMenuHandler().showAppMenu(
                 hasPermanentMenuKey ? null : getToolbarManager().getMenuButton(), false);
@@ -1797,7 +1798,7 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
     public final void onBackPressed() {
         if (mNativeInitialized) RecordUserAction.record("SystemBack");
 
-        TextBubble.onBackPressed();
+        TextBubble.dismissBubbles();
         if (VrShellDelegate.onBackPressed()) return;
         if (mCompositorViewHolder != null) {
             LayoutManager layoutManager = mCompositorViewHolder.getLayoutManager();
