1d0a23fb123a307fae44fbe1daca5cc3934e417c
Justin DeWitt <dewittj@chromium.org>
2017-09-29 19:40:23 +0000

    [Offline Pages] Filter out suggested articles in NTP Download Section
    We enabled prefetching offline pages by calling "suggested articles"
    supported by downloads.  This has the nice effect that downloads home
    can display them but the unwanted side effect that they appear in the
    download section of the NTP.  This fixes the problem with a temporary
    hack on the NTP side.
    
    Bug: 701939
    Change-Id: Ie09d48f8a1d92f8bbc16d4e6a822c95311e6d8c2
    Reviewed-on: https://chromium-review.googlesource.com/690601
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: vitaliii <vitaliii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505445}

--- chrome/browser/ntp_snippets/download_suggestions_provider.cc
+++ chrome/browser/ntp_snippets/download_suggestions_provider.cc
@@ -139,7 +139,8 @@ bool CompareDownloadsMostRecentlyPublishedFirst(const DownloadItem* left,
 bool IsClientIdForOfflinePageDownload(
     offline_pages::ClientPolicyController* policy_controller,
     const offline_pages::ClientId& client_id) {
-  return policy_controller->IsSupportedByDownload(client_id.name_space);
+  return policy_controller->IsSupportedByDownload(client_id.name_space) &&
+         !policy_controller->IsSuggested(client_id.name_space);
 }
 
 }  // namespace
@@ -736,7 +737,9 @@ void DownloadSuggestionsProvider::UpdateOfflinePagesCache(
       retained_dismissed_ids.insert(id_within_category);
     } else {
       if (!IsDownloadOutdated(GetOfflinePagePublishedTime(item),
-                              item.last_access_time)) {
+                              item.last_access_time) &&
+          IsClientIdForOfflinePageDownload(
+              offline_page_model_->GetPolicyController(), item.client_id)) {
         items.push_back(&item);
       }
     }
