f0f18f0cf338c618d5fda53014968b4b96c4d43f
Matt Menke <mmenke@chromium.org>
2017-09-29 15:15:23 +0000

    ios:  Make PrefServiceAdapter use the network pref store.
    This means that it only lives on on thread: The network thread, instead
    of jumping between the UI thread and the IO thread, since the network
    pref store lives on the IO thread, just like the network stack.
    
    A similar change was made to chrome/, as part of network
    servicification (Though in Chrome it's created by the NetworkService
    now). In a followup CL, I'll simplify HttpServerPropertiesManager to
    only support the case where the prefs thread is the network thread.
    
    Bug: 768884
    Change-Id: I92c97e8e5fb3f5cab46f1c998b92eaec48a3db5f
    Reviewed-on: https://chromium-review.googlesource.com/687803
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505375}

--- ios/chrome/browser/net/http_server_properties_manager_factory.h
+++ ios/chrome/browser/net/http_server_properties_manager_factory.h
@@ -7,16 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_NET_HTTP_SERVER_PROPERTIES_MANAGER_FACTORY_H_
 
 #include "base/macros.h"
-
-class PrefService;
+#include "base/memory/ref_counted.h"
+#include "components/prefs/writeable_pref_store.h"
 
 namespace net {
 class HttpServerPropertiesManager;
 class NetLog;
-}
-
-namespace user_prefs {
-class PrefRegistrySyncable;
 }
 
 // Class for registration and creation of HttpServerPropertiesManager
@@ -24,11 +20,8 @@ class HttpServerPropertiesManagerFactory {
  public:
   // Create an instance of HttpServerPropertiesManager.
   static net::HttpServerPropertiesManager* CreateManager(
-      PrefService* pref_service,
+      scoped_refptr<WriteablePrefStore> pref_store,
       net::NetLog* net_log);
-
-  // Register prefs for properties managed by HttpServerPropertiesManager.
-  static void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry);
 
  private:
   DISALLOW_COPY_AND_ASSIGN(HttpServerPropertiesManagerFactory);
--- ios/chrome/browser/prefs/browser_prefs.mm
+++ ios/chrome/browser/prefs/browser_prefs.mm
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/geolocation/omnibox_geolocation_local_state.h"
 #import "ios/chrome/browser/memory/memory_debugger_manager.h"
 #import "ios/chrome/browser/metrics/ios_chrome_metrics_service_client.h"
-#include "ios/chrome/browser/net/http_server_properties_manager_factory.h"
 #include "ios/chrome/browser/notification_promo.h"
 #include "ios/chrome/browser/physical_web/physical_web_prefs_registration.h"
 #include "ios/chrome/browser/pref_names.h"
@@ -105,7 +104,6 @@ void RegisterBrowserStatePrefs(user_prefs::PrefRegistrySyncable* registry) {
   FirstRun::RegisterProfilePrefs(registry);
   gcm::GCMChannelStatusSyncer::RegisterProfilePrefs(registry);
   HostContentSettingsMap::RegisterProfilePrefs(registry);
-  HttpServerPropertiesManagerFactory::RegisterProfilePrefs(registry);
   language::UrlLanguageHistogram::RegisterProfilePrefs(registry);
   ntp_snippets::ClickBasedCategoryRanker::RegisterProfilePrefs(registry);
   ntp_snippets::ContentSuggestionsService::RegisterProfilePrefs(registry);
