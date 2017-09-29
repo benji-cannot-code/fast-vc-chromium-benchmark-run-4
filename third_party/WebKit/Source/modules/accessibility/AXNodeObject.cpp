4f7b28fc55742fa29f8258ac46d3a866a3fe7a4b
deejay <djmix.kim@samsung.com>
2017-09-29 18:29:46 +0000

    Change default value of aria-level to 2
    According from ARIA 1.1 [1],
    default value of aria-level was changed to 2.
    
    We will return 2 as default in aria-level
    if not otherwise specified.
    
    [1] https://www.w3.org/WAI/ARIA/wiki/ARIA_1.1_Changes
    
    Bug=688581
    
    Change-Id: Ib03c23f96b74814fe9f52f196c6e0e417059112f
    Reviewed-on: https://chromium-review.googlesource.com/670539
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505425}

--- third_party/WebKit/Source/modules/accessibility/AXNodeObject.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXNodeObject.cpp
@@ -76,6 +76,9 @@ namespace blink {
 
 using namespace HTMLNames;
 
+// In ARIA 1.1, default value of aria-level was changed to 2.
+const int kDefaultHeadingLevel = 2;
+
 AXNodeObject::AXNodeObject(Node* node, AXObjectCacheImpl& ax_object_cache)
     : AXObject(ax_object_cache),
       children_dirty_(false),
@@ -1074,7 +1077,6 @@ int AXNodeObject::HeadingLevel() const {
     if (HasAOMPropertyOrARIAAttribute(AOMUIntProperty::kLevel, level)) {
       if (level >= 1 && level <= 9)
         return level;
-      return 1;
     }
   }
 
@@ -1100,6 +1102,9 @@ int AXNodeObject::HeadingLevel() const {
   if (element.HasTagName(h6Tag))
     return 6;
 
+  if (RoleValue() == kHeadingRole)
+    return kDefaultHeadingLevel;
+
   return 0;
 }
 
@@ -1112,7 +1117,6 @@ unsigned AXNodeObject::HierarchicalLevel() const {
   if (HasAOMPropertyOrARIAAttribute(AOMUIntProperty::kLevel, level)) {
     if (level >= 1 && level <= 9)
       return level;
-    return 1;
   }
 
   // Only tree item will calculate its level through the DOM currently.
