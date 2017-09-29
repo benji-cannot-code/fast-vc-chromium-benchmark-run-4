34c0b78fe560372bc314868eb441623012b60965
Alice Boxhall <aboxhall@chromium.org>
2017-09-29 05:27:46 +0000

    [Devtools] Ensure 'omit_focus' is respected when revealing a DOM node
    Bug: 560525
    Change-Id: I8ab96299a63d33830d7c779ed15c0bee0bcbc28f
    Reviewed-on: https://chromium-review.googlesource.com/644626
    Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505303}

--- third_party/WebKit/Source/devtools/front_end/elements/ElementsPanel.js
+++ third_party/WebKit/Source/devtools/front_end/elements/ElementsPanel.js
@@ -698,9 +698,10 @@ Elements.ElementsPanel = class extends UI.Panel {
 
   /**
    * @param {!SDK.DOMNode} node
+   * @param {boolean} focus
    * @return {!Promise}
    */
-  revealAndSelectNode(node) {
+  revealAndSelectNode(node, focus) {
     if (Elements.inspectElementModeController && Elements.inspectElementModeController.isInInspectElementMode())
       Elements.inspectElementModeController.stopInspection();
 
@@ -709,8 +710,8 @@ Elements.ElementsPanel = class extends UI.Panel {
     node = Common.moduleSetting('showUAShadowDOM').get() ? node : this._leaveUserAgentShadowDOM(node);
     node.highlightForTwoSeconds();
 
-    return UI.viewManager.showView('elements').then(() => {
-      this.selectDOMNode(node, true);
+    return UI.viewManager.showView('elements', false, !focus).then(() => {
+      this.selectDOMNode(node, focus);
       delete this._omitDefaultSelection;
 
       if (!this._notFirstInspectElement)
@@ -888,9 +889,10 @@ Elements.ElementsPanel.DOMNodeRevealer = class {
   /**
    * @override
    * @param {!Object} node
+   * @param {boolean=} omitFocus
    * @return {!Promise}
    */
-  reveal(node) {
+  reveal(node, omitFocus) {
     var panel = Elements.ElementsPanel.instance();
     panel._pendingNodeReveal = true;
 
@@ -923,7 +925,7 @@ Elements.ElementsPanel.DOMNodeRevealer = class {
         panel._pendingNodeReveal = false;
 
         if (resolvedNode) {
-          panel.revealAndSelectNode(resolvedNode).then(resolve);
+          panel.revealAndSelectNode(resolvedNode, !omitFocus).then(resolve);
           return;
         }
         reject(new Error('Could not resolve node to reveal.'));
