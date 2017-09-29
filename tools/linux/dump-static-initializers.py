3b4be191a4d4febff2a49e1611999d8eff1d69ae
Tim Brown <timbrown@chromium.org>
2017-09-29 19:32:44 +0000

    Revert "Simplify static initializer test"
    This reverts commit 6b3f0a074a1101236c80de644d2795e3da99eec8.
    
    Reason for revert: sizes test fails in waterfall
    
    Original change's description:
    > Simplify static initializer test
    > 
    > The old sizes.py perf test used to check for a whole set of performance
    > regressions. This is now handled elsewhere with the exception of the SI
    > check. The SI check is fairly simple, however, and doesn't require all
    > of the the perf_expectations infrastructure which is now obsolete.
    > 
    > This CL removes the obsolete perf_expectations, hard codes the expected
    > number of SIs in the script, and removes the windows and android code
    > as it doesn't actually do anything.
    > 
    > Bug: 572393
    > Change-Id: I960cb8fec63e25c489e8c4e90f670c7e35dd4fd6
    > Reviewed-on: https://chromium-review.googlesource.com/654178
    > Reviewed-by: Scott Graham <scottmg@chromium.org>
    > Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    > Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    > Commit-Queue: Tim Brown <timbrown@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#505434}
    
    TBR=primiano@chromium.org,dpranke@chromium.org,scottmg@chromium.org,thomasanderson@chromium.org,timbrown@chromium.org
    
    Change-Id: Ia610eb13a485d4c6044248b5c0c5dc4f71ffeec3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 572393
    Reviewed-on: https://chromium-review.googlesource.com/692567
    Commit-Queue: Tim Brown <timbrown@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505441}

--- tools/linux/dump-static-initializers.py
+++ tools/linux/dump-static-initializers.py
@@ -39,10 +39,8 @@ NOTES = {
 IS_GIT_WORKSPACE = (subprocess.Popen(
     ['git', 'rev-parse'], stderr=subprocess.PIPE).wait() == 0)
 
-
 class Demangler(object):
   """A wrapper around c++filt to provide a function to demangle symbols."""
-
   def __init__(self, toolchain):
     self.cppfilt = subprocess.Popen([toolchain + 'c++filt'],
                                     stdin=subprocess.PIPE,
@@ -52,7 +50,6 @@ class Demangler(object):
     """Given mangled symbol |sym|, return its demangled form."""
     self.cppfilt.stdin.write(sym + '\n')
     return self.cppfilt.stdout.readline().strip()
-
 
 # Matches for example: "cert_logger.pb.cc", capturing "cert_logger".
 protobuf_filename_re = re.compile(r'(.*)\.pb\.cc$')
@@ -75,7 +72,6 @@ def QualifyFilenameAsProto(filename):
       return filename # Multiple hits, can't help.
     candidate = line.strip()
   return candidate
-
 
 # Regex matching the substring of a symbol's demangled text representation most
 # likely to appear in a source file.
@@ -104,7 +100,6 @@ def QualifyFilename(filename, symbol):
     candidate = line.strip()
   return candidate
 
-
 # Regex matching nm output for the symbols we're interested in.
 # See test_ParseNmLine for examples.
 nm_re = re.compile(r'(\S+) (\S+) t (?:_ZN12)?_GLOBAL__(?:sub_)?I_(.*)')
@@ -129,7 +124,6 @@ def test_ParseNmLine():
     '_GLOBAL__sub_I_extension_specifics.pb.cc')
   assert parse == ('extension_specifics.pb.cc', 40607408, 36), parse
 
-
 # Just always run the test; it is fast enough.
 test_ParseNmLine()
 
@@ -142,7 +136,6 @@ def ParseNm(toolchain, binary):
     parse = ParseNmLine(line)
     if parse:
       yield parse
-
 
 # Regex matching objdump output for the symbols we're interested in.
 # Example line:
@@ -166,13 +159,12 @@ def ExtractSymbolReferences(toolchain, binary, start, end):
       if ref.startswith('.LC') or ref.startswith('_DYNAMIC'):
         # Ignore these, they are uninformative.
         continue
-      if re.match('_GLOBAL__(?:sub_)?I_', ref):
+      if ref.startswith('_GLOBAL__I_'):
         # Probably a relative jump within this function.
         continue
       refs.add(ref)
 
   return sorted(refs)
-
 
 def main():
   parser = optparse.OptionParser(usage='%prog [option] filename')
@@ -244,7 +236,6 @@ def main():
                                                        file_count)
 
   return 0
-
 
 if '__main__' == __name__:
   sys.exit(main())
---
+++ tools/perf_expectations/README.txt
@@ -0,0 +1,3 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+For instructions see
+http://www.chromium.org/developers/tree-sheriffs/perf-sheriffs
