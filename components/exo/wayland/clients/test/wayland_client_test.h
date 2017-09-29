7497e0095da4795433f46951d5bb8e73ac5a16b5
Daniele Castagna <dcastagna@chromium.org>
2017-09-29 00:14:40 +0000

    exo: Explicitly include memory for unique_ptr.
    wayland_client_perftests doens't build for cros without explicitly
    including 'memory' header for unique_ptr.
    
    Bug: 
    Change-Id: I74b8bbe07e2fd8b0b52bee2bacb49e445d9a46a9
    Reviewed-on: https://chromium-review.googlesource.com/691334
    Commit-Queue: Daniele Castagna <dcastagna@chromium.org>
    Commit-Queue: David Reveman <reveman@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505224}

--- components/exo/wayland/clients/test/wayland_client_test.h
+++ components/exo/wayland/clients/test/wayland_client_test.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENT_EXO_WAYLAND_CLIENTS_TEST_WAYLAND_CLIENT_TEST_H_
 #define COMPONENT_EXO_WAYLAND_CLIENTS_TEST_WAYLAND_CLIENT_TEST_H_
 
+#include <memory>
+
 #include "base/macros.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
