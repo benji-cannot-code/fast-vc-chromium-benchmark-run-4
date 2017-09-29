3d280da1a83d6966d4508f56f913597bc4d6ca1c
Eugene But <eugenebut@google.com>
2017-09-29 13:11:44 +0000

    Use WebState::SetShouldSuppressDialogs instead of CRWWebController API.
    CRWWebController is private class from //ios/web and should not be used
    in //ios/chrome.
    
    Bug: 620465
    Change-Id: I18a9ea2b225e48186b556bc5076f91442c6add8d
    Reviewed-on: https://chromium-review.googlesource.com/691038
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505357}

--- ios/chrome/browser/tabs/tab.mm
+++ ios/chrome/browser/tabs/tab.mm
@@ -471,8 +471,8 @@ void TabInfoBarObserver::OnInfoBarReplaced(infobars::InfoBar* old_infobar,
 
   _isPrerenderTab = isPrerender;
 
-  self.webController.shouldSuppressDialogs =
-      (isPrerender && !isLinkLoadingPrerenderTab_);
+  self.webState->SetShouldSuppressDialogs(isPrerender &&
+                                          !isLinkLoadingPrerenderTab_);
 
   if (_isPrerenderTab)
     return;
