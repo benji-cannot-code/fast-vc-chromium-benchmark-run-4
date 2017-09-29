05e54df96022b0d270b1109c65d2e413d8a5bbe6
Xianzhu Wang <wangxianzhu@chromium.org>
2017-09-29 23:09:08 +0000

    [SPv2] Partial raster invalidation
    Save partial invalidation in LayoutObject::PartialInvalidationRect()
    and issue raster invalidation in PaintController.
    
    Bug: 732612
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I9a8f01c0f00b18106371750c7c9c9a8667a4af5b
    Reviewed-on: https://chromium-review.googlesource.com/689964
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505464}

--- third_party/WebKit/Source/core/layout/LayoutObject.h
+++ third_party/WebKit/Source/core/layout/LayoutObject.h
@@ -224,6 +224,14 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
 
   // DisplayItemClient methods.
   LayoutRect VisualRect() const final { return visual_rect_; }
+  LayoutRect PartialInvalidationRect() const final {
+    return rare_paint_data_ ? rare_paint_data_->PartialInvalidationRect()
+                            : LayoutRect();
+  }
+  void ClearPartialInvalidationRect() const final {
+    DCHECK(RuntimeEnabledFeatures::SlimmingPaintV2Enabled());
+    return GetMutableForPainting().SetPartialInvalidationRect(LayoutRect());
+  }
   String DebugName() const final;
 
   LayoutObject* Parent() const { return parent_; }
@@ -1781,6 +1789,10 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
       layout_object_.SetNeedsPaintPropertyUpdate();
     }
 
+    void SetPartialInvalidationRect(const LayoutRect& r) {
+      layout_object_.SetPartialInvalidationRect(r);
+    }
+
 #if DCHECK_IS_ON()
     // Same as setNeedsPaintPropertyUpdate() but does not mark ancestors as
     // having a descendant needing a paint property update.
@@ -1896,10 +1908,6 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
   }
   LayoutRect SelectionVisualRect() const {
     return rare_paint_data_ ? rare_paint_data_->SelectionVisualRect()
-                            : LayoutRect();
-  }
-  LayoutRect PartialInvalidationRect() const {
-    return rare_paint_data_ ? rare_paint_data_->PartialInvalidationRect()
                             : LayoutRect();
   }
 
@@ -2036,12 +2044,19 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
                                        const LayoutPoint& layer_offset) const {}
 
   void SetVisualRect(const LayoutRect& rect) { visual_rect_ = rect; }
+  void SetPartialInvalidationRect(const LayoutRect& rect) {
+    if (GetRarePaintData() || !rect.IsEmpty())
+      EnsureRarePaintData().SetPartialInvalidationRect(rect);
+  }
 
 #if DCHECK_IS_ON()
   virtual bool PaintInvalidationStateIsDirty() const {
     return BackgroundChangedSinceLastPaintInvalidation() ||
            ShouldCheckForPaintInvalidation() || ShouldInvalidateSelection() ||
-           NeedsPaintOffsetAndVisualRectUpdate();
+           NeedsPaintOffsetAndVisualRectUpdate() ||
+           (!RuntimeEnabledFeatures::SlimmingPaintV2Enabled() &&
+            rare_paint_data_ &&
+            !rare_paint_data_->PartialInvalidationRect().IsEmpty());
   }
 #endif
 
