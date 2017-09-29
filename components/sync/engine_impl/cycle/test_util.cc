ee3b9ee974422c4db534f3cca65b708cd76c60cd
Sky Malice <skym@chromium.org>
2017-09-29 16:13:29 +0000

    Remove dead SimulateSessionsCommitDelayUpdateImpl.
    Bug: 
    Change-Id: Id0b90f07d3be8355a29063519a95c1f6ca5e710a
    Reviewed-on: https://chromium-review.googlesource.com/691152
    Reviewed-by: Patrick Noland <pnoland@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505385}

--- components/sync/engine_impl/cycle/test_util.cc
+++ components/sync/engine_impl/cycle/test_util.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/sync/engine_impl/cycle/test_util.h"
 
-#include <map>
-
 namespace syncer {
 namespace test_util {
 
@@ -112,16 +110,6 @@ void SimulatePollIntervalUpdateImpl(ModelTypeSet requested_types,
                                     const base::TimeDelta& new_poll) {
   SimulatePollSuccess(requested_types, cycle);
   cycle->delegate()->OnReceivedLongPollIntervalUpdate(new_poll);
-}
-
-void SimulateSessionsCommitDelayUpdateImpl(ModelTypeSet requested_types,
-                                           NudgeTracker* nudge_tracker,
-                                           SyncCycle* cycle,
-                                           const base::TimeDelta& new_delay) {
-  SimulateNormalSuccess(requested_types, nudge_tracker, cycle);
-  std::map<ModelType, base::TimeDelta> delay_map;
-  delay_map[SESSIONS] = new_delay;
-  cycle->delegate()->OnReceivedCustomNudgeDelays(delay_map);
 }
 
 void SimulateGuRetryDelayCommandImpl(SyncCycle* cycle, base::TimeDelta delay) {
