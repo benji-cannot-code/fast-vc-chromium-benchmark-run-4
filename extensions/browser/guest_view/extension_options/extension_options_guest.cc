c51f56d49f48f7c4f1ab85180ec33b585e4252e9
Lukasz Anforowicz <lukasza@chromium.org>
2017-09-29 17:08:58 +0000

    Removing callers of the WebContents::GetRenderProcessHost() method.
    WebContents::GetRenderProcessHost is an API that doesn't behave as
    expected with out-of-process iframes, as multiple processes can be
    associated with a single WebContents.  This CL replaces callers of this
    API, so that they go through an equivalent API that explicitly selects
    which frame's process is needed.
    
    This CL was started by using an ad-hoc clang-plugin that replaced
    existing callsites of WebContents::GetRenderProcessHost() with a call to
    something like wc->GetMainFrame()->GetProcess().  This was followed-up
    by manually adding an include of render_frame_host.h and git cl format
    and git cl lint and then a self-review with small tweaks (e.g. sometimes
    using a wc->GetRenderViewHost()->GetProcess() is more appropriate).
    
    This CL was uploaded by git cl split.
    
    R=ekaramad@chromium.org
    
    Bug: 666525
    Change-Id: I8c9c9436301eabd31b5bfe1054956c63d5457dfc
    Reviewed-on: https://chromium-review.googlesource.com/689107
    Reviewed-by: Ehsan Karamad <ekaramad@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505399}

--- extensions/browser/guest_view/extension_options/extension_options_guest.cc
+++ extensions/browser/guest_view/extension_options/extension_options_guest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "extensions/browser/guest_view/extension_options/extension_options_guest.h"
 
+#include <memory>
+#include <string>
 #include <utility>
 
 #include "base/memory/ptr_util.h"
@@ -13,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/guest_view/browser/guest_view_event.h"
 #include "components/guest_view/browser/guest_view_manager.h"
 #include "content/public/browser/navigation_handle.h"
+#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/site_instance.h"
 #include "content/public/browser/web_contents.h"
@@ -227,8 +230,9 @@ void ExtensionOptionsGuest::DidFinishNavigation(
   SetGuestZoomLevelToMatchEmbedder();
 
   if (!url::IsSameOriginWith(navigation_handle->GetURL(), options_page_)) {
-    bad_message::ReceivedBadMessage(web_contents()->GetRenderProcessHost(),
-                                    bad_message::EOG_BAD_ORIGIN);
+    bad_message::ReceivedBadMessage(
+        web_contents()->GetMainFrame()->GetProcess(),
+        bad_message::EOG_BAD_ORIGIN);
   }
 }
 
