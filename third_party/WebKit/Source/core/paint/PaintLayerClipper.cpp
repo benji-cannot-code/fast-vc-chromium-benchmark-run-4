e2f7db161254db1df951e5ce0d5ce92d831268db
Xianzhu Wang <wangxianzhu@chromium.org>
2017-09-29 20:47:12 +0000

    Allow showLayerTree() to show PaintLayers with dirty position
    showLayerTree() is useful for debugging, but previously it would
    crash if a PaintLayer had needs_position_update_.
    
    Now dump the current position of the layer with a "needs position
    update" message.
    
    BTW renamed PaintLayer::size() to PaintLayer::Size().
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I412a120b08184d1a5958879f199c2eff20717b89
    Reviewed-on: https://chromium-review.googlesource.com/692631
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505455}

--- third_party/WebKit/Source/core/paint/PaintLayerClipper.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayerClipper.cpp
@@ -295,7 +295,7 @@ void PaintLayerClipper::CalculateRectsWithGeometryMapper(
       layer_.ConvertToLayerCoords(context.root_layer, offset);
     }
   }
-  layer_bounds = LayoutRect(offset, LayoutSize(layer_.size()));
+  layer_bounds = LayoutRect(offset, LayoutSize(layer_.Size()));
 
   CalculateBackgroundClipRectWithGeometryMapper(context, fragment_data,
                                                 background_rect);
@@ -348,7 +348,7 @@ void PaintLayerClipper::CalculateRects(
     offset = *offset_from_root;
   else
     layer_.ConvertToLayerCoords(context.root_layer, offset);
-  layer_bounds = LayoutRect(offset, LayoutSize(layer_.size()));
+  layer_bounds = LayoutRect(offset, LayoutSize(layer_.Size()));
 
   // Update the clip rects that will be passed to child layers.
   if (ShouldClipOverflow(context)) {
