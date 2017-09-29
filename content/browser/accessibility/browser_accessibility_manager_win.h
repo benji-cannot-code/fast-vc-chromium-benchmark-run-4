61bcf6849d256a8f68ab78ac07f2b7bd08f32fcd
Aaron Leventhal <aleventhal@chromium.org>
2017-09-29 05:17:51 +0000

    Unused method
    Bug: None
    Change-Id: I44961962ccc51fe4fdcb5e1df07b8fb9e876c868
    Reviewed-on: https://chromium-review.googlesource.com/688036
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505301}

--- content/browser/accessibility/browser_accessibility_manager_win.h
+++ content/browser/accessibility/browser_accessibility_manager_win.h
@@ -36,9 +36,6 @@ class CONTENT_EXPORT BrowserAccessibilityManagerWin
   // Get the closest containing HWND.
   HWND GetParentHWND();
 
-  // The IAccessible for the parent window.
-  IAccessible* GetParentIAccessible();
-
   // IAccessible2UsageObserver
   void OnIAccessible2Used() override;
 
