f4c3cb1c7b80fee4dac15c8daad926c086d8a616
Brandon Jones <bajones@chromium.org>
2017-09-29 05:31:02 +0000

    Added VRSession creation.
    This CL only enables the creation of exclusive VRSession objects. It
    does not provide the ability to present to a headset, or many of the
    functions of a VRSession. Those will be added in follow up CLs to allow
    the more complicated logic to be reviewed independently.
    
    Bug: 670510
    Change-Id: Ic2b3b1801bf8f3ee4263d009daab8215b0b179d5
    Reviewed-on: https://chromium-review.googlesource.com/685396
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505305}

--- third_party/WebKit/Source/modules/vr/latest/VRDevice.cpp
+++ third_party/WebKit/Source/modules/vr/latest/VRDevice.cpp
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/UserGestureIndicator.h"
 #include "modules/EventTargetModules.h"
 #include "modules/vr/latest/VR.h"
+#include "modules/vr/latest/VRSession.h"
 
 namespace blink {
 
@@ -23,9 +24,6 @@ const char kNonExclusiveNotSupported[] =
 
 const char kRequestNotInUserGesture[] =
     "Exclusive sessions can only be requested during a user gesture.";
-
-const char kSessionCreationNotImplemented[] =
-    "Session creation not implemented.";
 
 }  // namespace
 
@@ -106,10 +104,16 @@ ScriptPromise VRDevice::requestSession(
     }
   }
 
-  // TODO: Session creation will be implemented in a follow up CL.
-  return ScriptPromise::RejectWithDOMException(
-      script_state,
-      DOMException::Create(kNotSupportedError, kSessionCreationNotImplemented));
+  ScriptPromiseResolver* resolver = ScriptPromiseResolver::Create(script_state);
+  ScriptPromise promise = resolver->Promise();
+
+  VRSession* session = new VRSession(this, options.exclusive());
+
+  // TODO: A follow up CL will establish a VRPresentationProvider connection
+  // before resolving the promise.
+  resolver->Resolve(session);
+
+  return promise;
 }
 
 // TODO: Forward these calls on to the sessions once they've been implemented.
