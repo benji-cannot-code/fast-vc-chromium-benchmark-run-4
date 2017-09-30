36b689d5b3d62d253ef1e5d2c8dbcaa5b360e6bd
Finnur Thorarinsson <finnur@chromium.org>
2017-09-30 23:36:49 +0000

    Move ComBase functions and HSTRING helpers to base/win.
    This is a reland of:
    https://chromium-review.googlesource.com/c/chromium/src/+/649528/
    ... which failed because the CL exposed a caller using IO on the main thread.
    
    I'm relanding without the asserts, because the underlying problem exists with
    and without my CL (which is simply a cleanup job), so that problem should be
    handled separately.
    
    Bug: 734095
    Change-Id: I79b809fede6097756e365f24f128db82977278a4
    TBR: toyoshim, grt, peter, dcheng, robliao
    Reviewed-on: https://chromium-review.googlesource.com/693094
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505478}

---
+++ base/win/core_winrt_util_unittest.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/win/core_winrt_util.h"
+
+#include "base/win/scoped_com_initializer.h"
+#include "base/win/windows_version.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace base {
+namespace win {
+
+TEST(CoreWinrtUtilTest, PreloadFunctions) {
+  ScopedCOMInitializer com_initializer(ScopedCOMInitializer::kMTA);
+
+  if (GetVersion() < VERSION_WIN8)
+    EXPECT_FALSE(ResolveCoreWinRTDelayload());
+  else
+    EXPECT_TRUE(ResolveCoreWinRTDelayload());
+}
+
+}  // namespace win
+}  // namespace base
