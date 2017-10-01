727ddbb62f6c0ca10cf5583df78e8e5cb075ffde
Xianzhu Wang <wangxianzhu@chromium.org>
2017-10-01 01:37:14 +0000

    [SPv175/SPv2] Repaint layer when backface-visibility changes
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ifb151605f3c0c5341faf020bb338fdcb8eba236e
    Reviewed-on: https://chromium-review.googlesource.com/693297
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Tien-Ren Chen <trchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505488}

--- third_party/WebKit/Source/core/layout/LayoutBoxModelObjectTest.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBoxModelObjectTest.cpp
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/page/scrolling/StickyPositionScrollingConstraints.h"
 #include "core/paint/PaintLayer.h"
 #include "core/paint/PaintLayerScrollableArea.h"
+#include "platform/testing/RuntimeEnabledFeaturesTestHelpers.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace blink {
@@ -1036,6 +1037,34 @@ TEST_F(LayoutBoxModelObjectTest, StickyRemovedFromRootScrollableArea) {
 
   // Now try to scroll to the sticky element, this used to crash.
   GetDocument().GetFrame()->DomWindow()->scrollTo(0, 500);
+}
+
+TEST_F(LayoutBoxModelObjectTest, BackfaceVisibilityChange) {
+  // TODO(wangxianzhu): Change this to V175.
+  ScopedSlimmingPaintV2ForTest spv2(true);
+
+  AtomicString base_style =
+      "width: 100px; height: 100px; background: blue; position: absolute";
+  SetBodyInnerHTML("<div id='target' style='" + base_style + "'></div>");
+
+  auto* target = GetDocument().getElementById("target");
+  auto* target_layer =
+      ToLayoutBoxModelObject(target->GetLayoutObject())->Layer();
+  ASSERT_NE(nullptr, target_layer);
+  EXPECT_FALSE(target_layer->NeedsRepaint());
+
+  target->setAttribute(HTMLNames::styleAttr,
+                       base_style + "; backface-visibility: hidden");
+  GetDocument().View()->UpdateAllLifecyclePhasesExceptPaint();
+  EXPECT_TRUE(target_layer->NeedsRepaint());
+  GetDocument().View()->UpdateAllLifecyclePhases();
+  EXPECT_FALSE(target_layer->NeedsRepaint());
+
+  target->setAttribute(HTMLNames::styleAttr, base_style);
+  GetDocument().View()->UpdateAllLifecyclePhasesExceptPaint();
+  EXPECT_TRUE(target_layer->NeedsRepaint());
+  GetDocument().View()->UpdateAllLifecyclePhases();
+  EXPECT_FALSE(target_layer->NeedsRepaint());
 }
 
 }  // namespace blink
