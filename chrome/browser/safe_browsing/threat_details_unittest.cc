7907eae23c84433f1d5bec732ec8782ceaf777a3
Lukasz Anforowicz <lukasza@chromium.org>
2017-10-02 18:38:34 +0000

    Removing callers of the WebContents::GetRenderProcessHost() method.
    WebContents::GetRenderProcessHost is an API that doesn't behave as
    expected with out-of-process iframes, as multiple processes can be
    associated with a single WebContents.  This CL replaces callers of this
    API, so that they go through an equivalent API that explicitly selects
    which frame's process is needed.
    
    This CL was started by using an ad-hoc clang-plugin that replaced
    existing callsites of WebContents::GetRenderProcessHost() with a call to
    something like wc->GetMainFrame()->GetProcess().  This was followed-up
    by manually adding an include of render_frame_host.h and git cl format
    and git cl lint and then a self-review with small tweaks (e.g. sometimes
    using a wc->GetRenderViewHost()->GetProcess() is more appropriate).
    
    This CL was uploaded by git cl split.
    
    R=felt@chromium.org
    
    Bug: 666525
    Change-Id: Icde83bc5446d80758b3033025907ad472cea43e4
    Reviewed-on: https://chromium-review.googlesource.com/689021
    Reviewed-by: Adrienne Porter Felt <felt@chromium.org>
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505692}

--- chrome/browser/safe_browsing/threat_details_unittest.cc
+++ chrome/browser/safe_browsing/threat_details_unittest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/browser/threat_details_history.h"
 #include "components/safe_browsing/common/safebrowsing_messages.h"
 #include "components/safe_browsing/proto/csd.pb.h"
+#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/test_renderer_host.h"
@@ -267,7 +268,7 @@ class ThreatDetailsTest : public ChromeRenderViewHostTestHarness {
     resource->threat_source = threat_source;
     resource->web_contents_getter =
         SafeBrowsingUIManager::UnsafeResource::GetWebContentsGetter(
-            web_contents()->GetRenderProcessHost()->GetID(),
+            web_contents()->GetMainFrame()->GetProcess()->GetID(),
             web_contents()->GetMainFrame()->GetRoutingID());
   }
 
@@ -298,7 +299,7 @@ class ThreatDetailsTest : public ChromeRenderViewHostTestHarness {
               static_cast<int>(actual_resource_map.size()));
     for (const ClientSafeBrowsingReportRequest::Resource& expected_resource :
          expected_pb.resources()) {
-      ASSERT_TRUE(actual_resource_map.count(expected_resource.id()) > 0);
+      ASSERT_GT(actual_resource_map.count(expected_resource.id()), 0u);
       VerifyResource(*actual_resource_map[expected_resource.id()],
                      expected_resource);
     }
@@ -314,7 +315,7 @@ class ThreatDetailsTest : public ChromeRenderViewHostTestHarness {
     // be unique).
     ASSERT_EQ(expected_pb.dom_size(), static_cast<int>(actual_dom_map.size()));
     for (const HTMLElement& expected_element : expected_pb.dom()) {
-      ASSERT_TRUE(actual_dom_map.count(expected_element.id()) > 0);
+      ASSERT_GT(actual_dom_map.count(expected_element.id()), 0u);
       VerifyElement(*actual_dom_map[expected_element.id()], expected_element);
     }
 
@@ -376,7 +377,7 @@ class ThreatDetailsTest : public ChromeRenderViewHostTestHarness {
               static_cast<int>(actual_attributes_map.size()));
     for (const HTMLElement::Attribute& expected_attribute :
          expected.attribute()) {
-      ASSERT_TRUE(actual_attributes_map.count(expected_attribute.name()) > 0);
+      ASSERT_GT(actual_attributes_map.count(expected_attribute.name()), 0u);
       EXPECT_EQ(expected_attribute.value(),
                 actual_attributes_map[expected_attribute.name()]);
     }
--- chrome/browser/safe_browsing/ui_manager_unittest.cc
+++ chrome/browser/safe_browsing/ui_manager_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/db/util.h"
 #include "components/security_interstitials/core/base_safe_browsing_error_ui.h"
 #include "content/public/browser/navigation_entry.h"
+#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
@@ -120,7 +121,7 @@ class SafeBrowsingUIManagerTest : public ChromeRenderViewHostTestHarness {
     resource.is_subresource = is_subresource;
     resource.web_contents_getter =
         security_interstitials::UnsafeResource::GetWebContentsGetter(
-            web_contents()->GetRenderProcessHost()->GetID(),
+            web_contents()->GetMainFrame()->GetProcess()->GetID(),
             web_contents()->GetMainFrame()->GetRoutingID());
     resource.threat_type = SB_THREAT_TYPE_URL_MALWARE;
     return resource;
