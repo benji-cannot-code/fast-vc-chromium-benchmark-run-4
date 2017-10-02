787f33d2d247a065fe6b2f248172cd9ceb19d7b2
Nathan Bruer <allada@chromium.org>
2017-10-02 22:42:41 +0000

    [Devtools] Added file path return to save response
    Adds the ability for devtools to know where a file was saved on disk
    when a save dialog was saved. This will allow us to serve files from
    disk for interception.
    
    R=pfeldman,dgozman
    BUG=760316
    
    Change-Id: Ic4477032219c66d00439bc7adecdb4783512a418
    Reviewed-on: https://chromium-review.googlesource.com/690625
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Commit-Queue: Blaise Bruer <allada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505825}

--- third_party/WebKit/Source/devtools/front_end/devtools_compatibility.js
+++ third_party/WebKit/Source/devtools/front_end/devtools_compatibility.js
@@ -251,9 +251,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     /**
      * @param {string} url
+     * @param {string=} fileSystemPath
      */
-    savedURL(url) {
-      this._dispatchOnInspectorFrontendAPI('savedURL', [url]);
+    savedURL(url, fileSystemPath) {
+      this._dispatchOnInspectorFrontendAPI('savedURL', [url, fileSystemPath]);
     }
 
     /**
