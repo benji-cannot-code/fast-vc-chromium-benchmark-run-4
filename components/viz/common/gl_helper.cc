a0ac896ca5304776aaaeb071f95c010a59cd8ce0
Yuri Wiitala <miu@chromium.org>
2017-10-02 20:29:11 +0000

    Expose GLHelperReadbackSupport to query for ideal readback configs.
    This exposes the GLHelperReadbackSupport instance owned by a GLHelper so
    that client code can query for platform-supported and performance-
    optimal readback configurations.
    
    This will be used for a number of things in soon-upcoming changes: 1) to
    avoid unnecessary byte-order swizzling; 2) to avoid extra memcpy()'s in
    GPU drivers. See crbug for further details.
    
    Bug: 770422
    Change-Id: I544313f731b313c48f4d8198530e840559285157
    Reviewed-on: https://chromium-review.googlesource.com/692853
    Reviewed-by: Xiangjun Zhang <xjz@chromium.org>
    Commit-Queue: Yuri Wiitala <miu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505758}

--- components/viz/common/gl_helper.cc
+++ components/viz/common/gl_helper.cc
@@ -1068,6 +1068,11 @@ bool GLHelper::IsReadbackConfigSupported(SkColorType color_type) {
   return (support == GLHelperReadbackSupport::SUPPORTED);
 }
 
+GLHelperReadbackSupport* GLHelper::GetReadbackSupport() {
+  LazyInitReadbackSupportImpl();
+  return readback_support_.get();
+}
+
 std::unique_ptr<ReadbackYUVInterface>
 GLHelper::CopyTextureToImpl::CreateReadbackPipelineYUV(bool flip_vertically,
                                                        bool use_mrt) {
