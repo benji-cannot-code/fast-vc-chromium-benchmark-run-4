d0989051249d9e94b01e1ba824348b4dbbdf825b
Mihai Sardarescu <msarda@chromium.org>
2017-10-02 14:04:46 +0000

    [DICE] Start sync after web sign-in.
    This CL implements the flow to start sync after a web sign-in and uses
    it from the DICE section of the chrome://signin-internals page.
    
    The solution consists in creating a new tab for the web sign-in and attaching
    a DiceTabHelper which monitors all tab navigations. If the user finishes
    the web sign-in flow without navigating outside of account.google.com,
    then Chrome starts sync automatically and displays the sync confirmation
    dialog.
    
    Bug: 716053
    Change-Id: I4905af24ea1d4fbdaa4ec087b625ce1bbd5d7c88
    Reviewed-on: https://chromium-review.googlesource.com/674865
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505599}

--- chrome/browser/signin/chrome_signin_helper.cc
+++ chrome/browser/signin/chrome_signin_helper.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/chrome_signin_client.h"
 #include "chrome/browser/signin/chrome_signin_client_factory.h"
 #include "chrome/browser/signin/dice_response_handler.h"
+#include "chrome/browser/signin/process_dice_header_observer_impl.h"
 #include "chrome/browser/tab_contents/tab_util.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/common/url_constants.h"
@@ -244,7 +245,9 @@ void ProcessDiceHeaderUIThread(
 
   DiceResponseHandler* dice_response_handler =
       DiceResponseHandler::GetForProfile(profile);
-  dice_response_handler->ProcessDiceHeader(dice_params);
+  dice_response_handler->ProcessDiceHeader(
+      dice_params,
+      base::MakeUnique<ProcessDiceHeaderObserverImpl>(web_contents));
 }
 #endif  // BUILDFLAG(ENABLE_DICE_SUPPORT)
 
--- chrome/browser/signin/dice_response_handler.h
+++ chrome/browser/signin/dice_response_handler.h
@@ -31,6 +31,20 @@ class Profile;
 // Exposed for testing.
 extern const int kDiceTokenFetchTimeoutSeconds;
 
+// Delegate interface for processing a dice request.
+class ProcessDiceHeaderObserver {
+ public:
+  virtual ~ProcessDiceHeaderObserver() = default;
+
+  // Called before starting to fetch a refresh token.
+  virtual void WillStartRefreshTokenFetch(const std::string& gaia_id,
+                                          const std::string& email) = 0;
+
+  // Called after the refresh token was fetched and added in the token service.
+  virtual void DidFinishRefreshTokenFetch(const std::string& gaia_id,
+                                          const std::string& email) = 0;
+};
+
 // Processes the Dice responses from Gaia.
 class DiceResponseHandler : public KeyedService {
  public:
@@ -46,7 +60,8 @@ class DiceResponseHandler : public KeyedService {
   ~DiceResponseHandler() override;
 
   // Must be called when receiving a Dice response header.
-  void ProcessDiceHeader(const signin::DiceResponseParams& dice_params);
+  void ProcessDiceHeader(const signin::DiceResponseParams& dice_params,
+                         std::unique_ptr<ProcessDiceHeaderObserver> observer);
 
   // Returns the number of pending DiceTokenFetchers. Exposed for testing.
   size_t GetPendingDiceTokenFetchersCountForTesting() const;
@@ -60,6 +75,7 @@ class DiceResponseHandler : public KeyedService {
                      const std::string& authorization_code,
                      SigninClient* signin_client,
                      AccountReconcilor* account_reconcilor,
+                     std::unique_ptr<ProcessDiceHeaderObserver> observer,
                      DiceResponseHandler* dice_response_handler);
     ~DiceTokenFetcher() override;
 
@@ -84,6 +100,7 @@ class DiceResponseHandler : public KeyedService {
     std::string gaia_id_;
     std::string email_;
     std::string authorization_code_;
+    std::unique_ptr<ProcessDiceHeaderObserver> observer_;
     DiceResponseHandler* dice_response_handler_;
     base::CancelableClosure timeout_closure_;
     std::unique_ptr<GaiaAuthFetcher> gaia_auth_fetcher_;
@@ -100,9 +117,11 @@ class DiceResponseHandler : public KeyedService {
                              const std::string& email);
 
   // Process the Dice signin action.
-  void ProcessDiceSigninHeader(const std::string& gaia_id,
-                               const std::string& email,
-                               const std::string& authorization_code);
+  void ProcessDiceSigninHeader(
+      const std::string& gaia_id,
+      const std::string& email,
+      const std::string& authorization_code,
+      std::unique_ptr<ProcessDiceHeaderObserver> observer);
 
   // Process the Dice signout action.
   void ProcessDiceSignoutHeader(const std::vector<std::string>& gaia_ids,
@@ -112,9 +131,10 @@ class DiceResponseHandler : public KeyedService {
   // after DiceAction::SIGNIN.
   void OnTokenExchangeSuccess(
       DiceTokenFetcher* token_fetcher,
-      const std::string& gaia_id,
-      const std::string& email,
-      const GaiaAuthConsumer::ClientOAuthResult& result);
+      std::string gaia_id,
+      std::string email,
+      std::string refresh_token,
+      std::unique_ptr<ProcessDiceHeaderObserver> observer);
   void OnTokenExchangeFailure(DiceTokenFetcher* token_fetcher,
                               const GoogleServiceAuthError& error);
 
