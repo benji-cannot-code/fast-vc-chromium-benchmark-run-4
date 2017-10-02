e4add132b284892e54a7c0a5eba92a6b15c15876
Peter E Conn <peconn@chromium.org>
2017-10-02 18:55:09 +0000

    🏡 Polish the Scroll to Load triggering logic.
    This slightly refines the logic to load more when the user approaches
    the end of their suggestions through dismissing items and when the user
    scrolls down after having received only a few new suggestions.
    
    Bug: 754184
    Change-Id: If3f6ec0e71e4d859fc1046cec68c17a134c17a82
    Reviewed-on: https://chromium-review.googlesource.com/686766
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505704}

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsRecyclerView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsRecyclerView.java
@@ -13,6 +13,7 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Canvas;
+import android.support.annotation.Nullable;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.view.animation.FastOutLinearInInterpolator;
 import android.support.v7.view.ContextThemeWrapper;
@@ -33,6 +34,7 @@ import org.chromium.chrome.browser.ntp.ContextMenuManager;
 import org.chromium.chrome.browser.ntp.cards.CardViewHolder;
 import org.chromium.chrome.browser.ntp.cards.NewTabPageAdapter;
 import org.chromium.chrome.browser.ntp.cards.NewTabPageViewHolder;
+import org.chromium.chrome.browser.ntp.cards.ScrollToLoadListener;
 import org.chromium.chrome.browser.widget.displaystyle.UiConfig;
 
 import java.util.ArrayList;
@@ -54,6 +56,9 @@ public class SuggestionsRecyclerView extends RecyclerView {
     private final GestureDetector mGestureDetector;
     private final LinearLayoutManager mLayoutManager;
     private final SuggestionsMetrics.ScrollEventReporter mScrollEventReporter;
+
+    // The ScrollToLoadListener triggers loading more content when the user is near the end.
+    @Nullable private ScrollToLoadListener mScrollToLoadListener;
 
     /**
      * Total height of the items being dismissed.  Tracked to allow the bottom space to compensate
@@ -294,6 +299,7 @@ public class SuggestionsRecyclerView extends RecyclerView {
         getNewTabPageAdapter().dismissItem(position, removedItemTitle -> {
             announceForAccessibility(getResources().getString(
                     R.string.ntp_accessibility_item_removed, removedItemTitle));
+            if (mScrollToLoadListener != null) mScrollToLoadListener.onItemDismissed();
         });
     }
 
@@ -353,6 +359,24 @@ public class SuggestionsRecyclerView extends RecyclerView {
      */
     public static void resetForDismissCallback(NewTabPageViewHolder holder) {
         ((CardViewHolder) holder).getRecyclerView().updateViewStateForDismiss(0, holder);
+    }
+
+    /**
+     * Sets the ScrollToLoadListener for the RecyclerView.
+     */
+    public void setScrollToLoadListener(ScrollToLoadListener scrollToLoadListener) {
+        mScrollToLoadListener = scrollToLoadListener;
+        addOnScrollListener(mScrollToLoadListener);
+    }
+
+    /**
+     * Clears the currently registered ScrollToLoadListener.
+     */
+    public void clearScrollToLoadListener() {
+        if (mScrollToLoadListener == null) return;
+
+        removeOnScrollListener(mScrollToLoadListener);
+        mScrollToLoadListener = null;
     }
 
     private class ItemTouchCallbacks extends ItemTouchHelper.Callback {
