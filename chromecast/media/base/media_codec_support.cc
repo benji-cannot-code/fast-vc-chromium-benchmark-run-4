4befceefd8e4957a2ae5f2d6ddcbea927e59222f
Luke Halliwell <halliwell@chromium.org>
2017-10-02 15:03:12 +0000

    [Chromecast] Extract libcast_media target from CMA backend
    libcast_media has multiple variants (dummy, default, shared lib, static),
    previously encapsulated in a target inside CMA backend. Any components
    calling CastMediaShlib:: static functions should depend on libcast_media,
    but shouldn't necessarily depend on CMA backend. This change lifts the
    target to media/ for wider use.
    
    With this done, gn check found a circular dependency (Android audio
    backend -> libcast_media -> media/base -> libcast_media), which I've fixed
    by extracting media_codec_support from media/base.
    
    TBR=slan@chromium.org
    
    Bug: 66898015
    Test: Build, gn check multiple platforms
    Change-Id: Id297b042245d422892eebbef0e4fcbdccf8b773a
    Reviewed-on: https://chromium-review.googlesource.com/687274
    Commit-Queue: Luke Halliwell <halliwell@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505613}

--- chromecast/media/base/media_codec_support.cc
+++ chromecast/media/base/media_codec_support.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/strings/string_util.h"
-#include "chromecast/media/base/media_caps.h"
 
 namespace chromecast {
 namespace media {
