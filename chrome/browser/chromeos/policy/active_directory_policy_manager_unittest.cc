8cb1687bd1282c53282a8d1ebbaf5e40b64892fc
Roman Sorokin <rsorokin@chromium.org>
2017-10-02 11:06:51 +0000

    Move login tests for Active Directory to a separate file.
    Bug: 682421
    Change-Id: I20be3a94693913b93475aec73756d8f6522c4859
    Reviewed-on: https://chromium-review.googlesource.com/683338
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Roman Sorokin <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505573}

--- chrome/browser/chromeos/policy/active_directory_policy_manager_unittest.cc
+++ chrome/browser/chromeos/policy/active_directory_policy_manager_unittest.cc
@@ -25,8 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// TODO(rsorokin): Replace with MockAuthPolicyClient (see
-// https://crbug.com/753355).
 class TestAuthPolicyClient : public chromeos::AuthPolicyClient {
  public:
   void Init(dbus::Bus* bus) override { NOTIMPLEMENTED(); }
