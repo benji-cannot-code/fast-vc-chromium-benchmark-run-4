fee3d3139462659d5ad478ccd628cd5d2c722c52
spqchan <spqchan@chromium.org>
2017-10-02 19:48:13 +0000

    [Mac] Touch Bar Browser Tests
    Add browser tests to check if the touch bar gets
    updated.
    
    Bug: 764295
    Change-Id: I9c579319050360e6bbc440137fd3e452f5142c6c
    Reviewed-on: https://chromium-review.googlesource.com/688803
    Commit-Queue: Sarah Chan <spqchan@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505733}

--- chrome/browser/ui/cocoa/browser_window_controller.h
+++ chrome/browser/ui/cocoa/browser_window_controller.h
@@ -630,6 +630,9 @@ constexpr const gfx::Size kMinCocoaPopupWindowSize(100, 122);
 // Sets the fullscreen toolbar controller.
 - (void)setFullscreenToolbarController:(FullscreenToolbarController*)controller;
 
+// Sets |browserWindowTouchbar_|.
+- (void)setBrowserWindowTouchBar:(BrowserWindowTouchBar*)touchBar;
+
 @end  // @interface BrowserWindowController (TestingAPI)
 
 
