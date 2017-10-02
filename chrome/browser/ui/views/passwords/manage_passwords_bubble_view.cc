637fe09950cc53f3d2b1e2b00652a546334df841
Peter Boström <pbos@chromium.org>
2017-10-02 20:48:30 +0000

    Add trash icon to Harmony password dialog.
    BUG=chromium:654115
    R=bsep@chromium.org
    
    Change-Id: Iead94366cb68df77f396d92d1f54bc83677551be
    Reviewed-on: https://chromium-review.googlesource.com/691178
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505776}

--- chrome/browser/ui/views/passwords/manage_passwords_bubble_view.cc
+++ chrome/browser/ui/views/passwords/manage_passwords_bubble_view.cc
@@ -210,7 +210,7 @@ views::StyledLabel::RangeStyleInfo GetLinkStyle() {
   return result;
 }
 
-std::unique_ptr<views::ToggleImageButton> GeneratePasswordViewButton(
+std::unique_ptr<views::ToggleImageButton> CreatePasswordViewButton(
     views::ButtonListener* listener) {
   std::unique_ptr<views::ToggleImageButton> button(
       new views::ToggleImageButton(listener));
@@ -239,7 +239,7 @@ std::unique_ptr<views::ToggleImageButton> GeneratePasswordViewButton(
 
 // Creates a dropdown from the other possible passwords.
 // The items are made of '*'s if not visible.
-std::unique_ptr<views::Combobox> GeneratePasswordDropdownView(
+std::unique_ptr<views::Combobox> CreatePasswordDropdownView(
     const autofill::PasswordForm& form,
     bool visible) {
   DCHECK(!form.other_possible_passwords.empty());
@@ -457,16 +457,16 @@ ManagePasswordsBubbleView::PendingView::PendingView(
       parent_->model()->pending_password();
   if (base::FeatureList::IsEnabled(
           password_manager::features::kEnableUsernameCorrection)) {
-    username_field_ = GenerateUsernameEditable(password_form).release();
+    username_field_ = CreateUsernameEditable(password_form).release();
   } else {
-    username_field_ = GenerateUsernameLabel(password_form).release();
+    username_field_ = CreateUsernameLabel(password_form).release();
   }
 
   CreatePasswordField();
 
   if (base::FeatureList::IsEnabled(
           password_manager::features::kEnablePasswordSelection)) {
-    password_view_button_ = GeneratePasswordViewButton(this).release();
+    password_view_button_ = CreatePasswordViewButton(this).release();
   }
 
   // Create buttons.
@@ -536,9 +536,9 @@ void ManagePasswordsBubbleView::PendingView::CreatePasswordField() {
   if (enable_password_selection &&
       password_form.other_possible_passwords.size() > 1) {
     password_field_ =
-        GeneratePasswordDropdownView(password_form, password_visible_);
+        CreatePasswordDropdownView(password_form, password_visible_);
   } else {
-    password_field_ = GeneratePasswordLabel(password_form, password_visible_);
+    password_field_ = CreatePasswordLabel(password_form, password_visible_);
   }
   password_field_->set_owned_by_client();
 }
@@ -849,8 +849,8 @@ ManagePasswordsBubbleView::UpdatePendingView::UpdatePendingView(
   } else {
     const autofill::PasswordForm& password_form =
         parent_->model()->pending_password();
-    BuildCredentialRows(layout, GenerateUsernameLabel(password_form).release(),
-                        GeneratePasswordLabel(password_form, false).release(),
+    BuildCredentialRows(layout, CreateUsernameLabel(password_form).release(),
+                        CreatePasswordLabel(password_form, false).release(),
                         nullptr);
   }
   layout->AddPaddingRow(
