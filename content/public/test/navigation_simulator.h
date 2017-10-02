a0692c14576770eed96b77b1f209a661c7666b2b
Lucas Garron <lgarron@chromium.org>
2017-10-02 20:38:58 +0000

    Remove self-import of navigation_simulator.h
    Bug: 
    Change-Id: If768b70d187f91c091d853bc903fd15ea3fb96b7
    Reviewed-on: https://chromium-review.googlesource.com/692583
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Lucas Garron <lgarron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505767}

--- content/public/test/navigation_simulator.h
+++ content/public/test/navigation_simulator.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_throttle.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/common/referrer.h"
-#include "content/public/test/navigation_simulator.h"
 #include "net/base/host_port_pair.h"
 #include "ui/base/page_transition_types.h"
 
