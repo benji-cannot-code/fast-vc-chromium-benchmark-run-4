1c5bd521bdd642e9718844cf7ba9f337c45cf968
Devlin Cronin <rdevlin.cronin@chromium.org>
2017-10-02 17:07:10 +0000

    [Sheriff] Disable failing tests
    ActiveDirectoryLoginTest.LoginErrors and
    ActiveDirectoryLoginTest.LoginSuccess fail on Linux MSAN since they were
    moved in revision 037de995fa87710f53f675dc382bdc35e578e28d. Disable them.
    
    TBR: rsorokin@chromium.org
    Bug: 770738
    NOTRY: true
    Change-Id: Ic43a1a35accce4960515c6164c57892654a55f08
    Reviewed-on: https://chromium-review.googlesource.com/695384
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505654}

--- chrome/browser/chromeos/login/active_directory_login_browsertest.cc
+++ chrome/browser/chromeos/login/active_directory_login_browsertest.cc
@@ -258,7 +258,8 @@ IN_PROC_BROWSER_TEST_F(ActiveDirectoryLoginTest, PRE_LoginSuccess) {
 }
 
 // Test successful Active Directory login.
-IN_PROC_BROWSER_TEST_F(ActiveDirectoryLoginTest, LoginSuccess) {
+// Fails on MSAN: https://crbug.com/770738.
+IN_PROC_BROWSER_TEST_F(ActiveDirectoryLoginTest, DISABLED_LoginSuccess) {
   TestNoError();
   TestDomainVisible();
 
@@ -275,7 +276,8 @@ IN_PROC_BROWSER_TEST_F(ActiveDirectoryLoginTest, PRE_LoginErrors) {
 }
 
 // Test different UI errors for Active Directory login.
-IN_PROC_BROWSER_TEST_F(ActiveDirectoryLoginTest, LoginErrors) {
+// Fails on MSAN: https://crbug.com/770738.
+IN_PROC_BROWSER_TEST_F(ActiveDirectoryLoginTest, DISABLED_LoginErrors) {
   SetupActiveDirectoryJSNotifications();
   TestNoError();
   TestDomainVisible();
