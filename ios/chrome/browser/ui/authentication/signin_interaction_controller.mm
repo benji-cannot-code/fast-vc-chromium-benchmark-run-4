e0117c89b28c9397fe3128531cc1f6eaef3a79cc
Gregory Chatzinoff <gchatz@chromium.org>
2017-10-02 20:54:34 +0000

    Clean Up SignInInteractionController's API.
    Removes unnecessary view controller parameters from APIs in preparation
    for boxing. These view Controllers are unnecessary since the
    |presentingViewController_| variable can be used instead.
    
    Bug: none
    Change-Id: I93805365a20438bfd40a5f6b9dd7b229f700df73
    Reviewed-on: https://chromium-review.googlesource.com/691517
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Rohit Rao (ping after 24h) <rohitrao@chromium.org>
    Commit-Queue: Gregory Chatzinoff <gchatz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505778}

--- ios/chrome/browser/ui/authentication/signin_interaction_controller.mm
+++ ios/chrome/browser/ui/authentication/signin_interaction_controller.mm
@@ -107,9 +107,8 @@ using signin_ui::CompletionCallback;
   isDismissing_ = NO;
 }
 
-- (void)signInWithViewController:(UIViewController*)viewController
-                        identity:(ChromeIdentity*)identity
-                      completion:(signin_ui::CompletionCallback)completion {
+- (void)signInWithIdentity:(ChromeIdentity*)identity
+                completion:(signin_ui::CompletionCallback)completion {
   signin_metrics::LogSigninAccessPointStarted(accessPoint_, promoAction_);
   completionCallback_ = [completion copy];
   ios::ChromeIdentityService* identityService =
@@ -135,16 +134,12 @@ using signin_ui::CompletionCallback;
     __weak SigninInteractionController* weakSelf = self;
     [identityInteractionManager_
         addAccountWithCompletion:^(ChromeIdentity* identity, NSError* error) {
-          [weakSelf handleIdentityAdded:identity
-                                  error:error
-                           shouldSignIn:YES
-                         viewController:viewController];
+          [weakSelf handleIdentityAdded:identity error:error shouldSignIn:YES];
         }];
   }
 }
 
-- (void)reAuthenticateWithCompletion:(CompletionCallback)completion
-                      viewController:(UIViewController*)viewController {
+- (void)reAuthenticateWithCompletion:(CompletionCallback)completion {
   signin_metrics::LogSigninAccessPointStarted(accessPoint_, promoAction_);
   completionCallback_ = [completion copy];
   AccountInfo accountInfo =
@@ -178,13 +173,11 @@ using signin_ui::CompletionCallback;
                     completion:^(ChromeIdentity* identity, NSError* error) {
                       [weakSelf handleIdentityAdded:identity
                                               error:error
-                                       shouldSignIn:YES
-                                     viewController:viewController];
+                                       shouldSignIn:YES];
                     }];
 }
 
-- (void)addAccountWithCompletion:(CompletionCallback)completion
-                  viewController:(UIViewController*)viewController {
+- (void)addAccountWithCompletion:(CompletionCallback)completion {
   completionCallback_ = [completion copy];
   identityInteractionManager_ =
       ios::GetChromeBrowserProvider()
@@ -193,10 +186,7 @@ using signin_ui::CompletionCallback;
   __weak SigninInteractionController* weakSelf = self;
   [identityInteractionManager_
       addAccountWithCompletion:^(ChromeIdentity* identity, NSError* error) {
-        [weakSelf handleIdentityAdded:identity
-                                error:error
-                         shouldSignIn:NO
-                       viewController:viewController];
+        [weakSelf handleIdentityAdded:identity error:error shouldSignIn:NO];
       }];
 }
 
@@ -204,8 +194,7 @@ using signin_ui::CompletionCallback;
 
 - (void)handleIdentityAdded:(ChromeIdentity*)identity
                       error:(NSError*)error
-               shouldSignIn:(BOOL)shouldSignIn
-             viewController:(UIViewController*)viewController {
+               shouldSignIn:(BOOL)shouldSignIn {
   if (!identityInteractionManager_)
     return;
 
@@ -222,9 +211,10 @@ using signin_ui::CompletionCallback;
     };
 
     // TODO(crbug.com/754642): Stop using TopPresentedViewControllerFrom().
-    alertCoordinator_ = ErrorCoordinator(
-        error, dismissAction,
-        top_view_controller::TopPresentedViewControllerFrom(viewController));
+    alertCoordinator_ =
+        ErrorCoordinator(error, dismissAction,
+                         top_view_controller::TopPresentedViewControllerFrom(
+                             presentingViewController_));
     [alertCoordinator_ start];
     return;
   }
