c4364fef03bfd54f80004183a501214b1d8e0620
Erik Jensen <rkjnsn@chromium.org>
2017-10-02 19:38:09 +0000

    Add discrete caps/num lock fields to event protocol
    Previously, there was a single field for all lock states. This was
    problematic, because some platforms only support caps lock. These
    platforms were thus forced to include bogus state for num lock. Making
    them separate fields allows these platforms to specify only the
    caps-lock state.
    
    Bug: 760731
    Change-Id: Id72bbb09e1a8ed218cf77496cfd76cf1df8c97c0
    Reviewed-on: https://chromium-review.googlesource.com/692752
    Commit-Queue: Erik Jensen <rkjnsn@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505725}

--- remoting/host/input_injector_win.cc
+++ remoting/host/input_injector_win.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/ref_counted.h"
+#include "base/optional.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/string16.h"
 #include "base/strings/utf_string_conversions.h"
@@ -150,6 +151,44 @@ void ParseMouseWheelEvent(const MouseEvent& event, std::vector<INPUT>* output) {
   }
 }
 
+// Check if the given scan code is caps lock or num lock.
+bool IsLockKey(int scancode) {
+  UINT virtual_key = MapVirtualKey(scancode, MAPVK_VSC_TO_VK);
+  return virtual_key == VK_CAPITAL || virtual_key == VK_NUMLOCK;
+}
+
+// Sets the keyboard lock states to those provided.
+void SetLockStates(base::Optional<bool> caps_lock,
+                   base::Optional<bool> num_lock) {
+  // Can't use SendKeyboardInput because we need to send virtual key codes, not
+  // scan codes.
+  INPUT input[2] = {};
+  input[0].type = INPUT_KEYBOARD;
+  input[1].type = INPUT_KEYBOARD;
+  input[1].ki.dwFlags = KEYEVENTF_KEYUP;
+
+  if (caps_lock) {
+    bool client_capslock_state = *caps_lock;
+    bool host_capslock_state = (GetKeyState(VK_CAPITAL) & 1) != 0;
+    if (client_capslock_state != host_capslock_state) {
+      input[0].ki.wVk = VK_CAPITAL;
+      input[1].ki.wVk = VK_CAPITAL;
+      SendInput(arraysize(input), input, sizeof(INPUT));
+    }
+  }
+
+  // Sets the keyboard lock states to those provided.
+  if (num_lock) {
+    bool client_numlock_state = *num_lock;
+    bool host_numlock_state = (GetKeyState(VK_NUMLOCK) & 1) != 0;
+    if (client_numlock_state != host_numlock_state) {
+      input[0].ki.wVk = VK_NUMLOCK;
+      input[1].ki.wVk = VK_NUMLOCK;
+      SendInput(arraysize(input), input, sizeof(INPUT));
+    }
+  }
+}
+
 // A class to generate events on Windows.
 class InputInjectorWin : public InputInjector {
  public:
@@ -199,12 +238,6 @@ class InputInjectorWin : public InputInjector {
     void HandleText(const TextEvent& event);
     void HandleMouse(const MouseEvent& event);
     void HandleTouch(const TouchEvent& event);
-
-    // Check if the given scan code is caps lock or num lock.
-    bool IsLockKey(int scancode);
-
-    // Sets the keyboard lock states to those provided.
-    void SetLockStates(uint32_t states);
 
     scoped_refptr<base::SingleThreadTaskRunner> main_task_runner_;
     scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner_;
@@ -355,8 +388,27 @@ void InputInjectorWin::Core::HandleKey(const KeyEvent& event) {
   if (scancode == ui::KeycodeConverter::InvalidNativeKeycode())
     return;
 
-  if (event.has_lock_states() && event.pressed() && !IsLockKey(scancode)) {
-    SetLockStates(event.lock_states());
+  if (event.pressed() && !IsLockKey(scancode)) {
+    base::Optional<bool> caps_lock;
+    base::Optional<bool> num_lock;
+
+    // For caps lock, check both the new caps_lock field and the old lock_states
+    // field.
+    if (event.has_caps_lock_state()) {
+      caps_lock = event.caps_lock_state();
+    } else if (event.has_lock_states()) {
+      caps_lock =
+          (event.lock_states() & protocol::KeyEvent::LOCK_STATES_CAPSLOCK) != 0;
+    }
+
+    // Not all clients have a concept of num lock. Since there's no way to
+    // distinguish these clients using the legacy lock_states field, only update
+    // if the new num_lock field is specified.
+    if (event.has_num_lock_state()) {
+      num_lock = event.num_lock_state();
+    }
+
+    SetLockStates(caps_lock, num_lock);
   }
 
   uint32_t flags = KEYEVENTF_SCANCODE | (event.pressed() ? 0 : KEYEVENTF_KEYUP);
@@ -393,31 +445,6 @@ void InputInjectorWin::Core::HandleMouse(const MouseEvent& event) {
 void InputInjectorWin::Core::HandleTouch(const TouchEvent& event) {
   DCHECK(touch_injector_);
   touch_injector_->InjectTouchEvent(event);
-}
-
-bool InputInjectorWin::Core::IsLockKey(int scancode) {
-  UINT virtual_key = MapVirtualKey(scancode, MAPVK_VSC_TO_VK);
-  return virtual_key == VK_CAPITAL || virtual_key == VK_NUMLOCK;
-}
-
-void InputInjectorWin::Core::SetLockStates(uint32_t states) {
-  // Can't use SendKeyboardInput because we need to send virtual key codes, not
-  // scan codes.
-  INPUT input[2] = {};
-  input[0].type = INPUT_KEYBOARD;
-  input[1].type = INPUT_KEYBOARD;
-  input[1].ki.dwFlags = KEYEVENTF_KEYUP;
-
-  bool client_capslock_state =
-      (states & protocol::KeyEvent::LOCK_STATES_CAPSLOCK) != 0;
-  bool host_capslock_state = (GetKeyState(VK_CAPITAL) & 1) != 0;
-  if (client_capslock_state != host_capslock_state) {
-    input[0].ki.wVk = VK_CAPITAL;
-    input[1].ki.wVk = VK_CAPITAL;
-    SendInput(arraysize(input), input, sizeof(INPUT));
-  }
-  // TODO(jamiewalch): Reinstate NumLock synchronization when the protocol
-  //     supports the client reporting it as "unknown".
 }
 
 }  // namespace
