27b5a653eb228d5f547fe8d1060bb6d3a7ee0295
Will Chen <chenwilliam@chromium.org>
2017-10-02 22:13:37 +0000

    DevTools: refactor with new method TestRunner.url(...)
    This cleans up several call-sites in the new test framework that's just
    copy-and-paste code.
    
    Bug: 667560
    Change-Id: I7da98a1d25f4103f7e1789067f6f0ed3651830d1
    Reviewed-on: https://chromium-review.googlesource.com/693144
    Reviewed-by: Will Chen <chenwilliam@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505812}

--- third_party/WebKit/Source/devtools/front_end/integration_test_runner/IntegrationTestRunner.js
+++ third_party/WebKit/Source/devtools/front_end/integration_test_runner/IntegrationTestRunner.js
@@ -164,13 +164,10 @@ TestRunner.loadHTML = function(html) {
  * @return {!Promise<!SDK.RemoteObject>}
  */
 TestRunner.addScriptTag = function(path) {
-  var testScriptURL = /** @type {string} */ (Runtime.queryParam('test'));
-  var resolvedPath = testScriptURL + '/../' + path;
-
   return TestRunner.evaluateInPagePromise(`
     (function(){
       var script = document.createElement('script');
-      script.src = '${resolvedPath}';
+      script.src = '${TestRunner.url(path)}';
       document.head.append(script);
     })();
   `);
@@ -181,15 +178,12 @@ TestRunner.addScriptTag = function(path) {
  * @return {!Promise<!SDK.RemoteObject|undefined>}
  */
 TestRunner.addStylesheetTag = function(path) {
-  var testScriptURL = /** @type {string} */ (Runtime.queryParam('test'));
-  var resolvedPath = testScriptURL + '/../' + path;
-
   return TestRunner.evaluateInPageAsync(`
     (function(){
       var link = document.createElement('link');
       link.rel = 'stylesheet';
       link.type = 'text/css';
-      link.href = '${resolvedPath}';
+      link.href = '${TestRunner.url(path)}';
       link.onload = onload;
       document.head.append(link);
       var resolve;
@@ -209,13 +203,10 @@ TestRunner.addStylesheetTag = function(path) {
  * @return {!Promise<!SDK.RemoteObject|undefined>}
  */
 TestRunner.addIframe = function(path) {
-  var testScriptURL = /** @type {string} */ (Runtime.queryParam('test'));
-  var resolvedPath = testScriptURL + '/../' + path;
-
   return TestRunner.evaluateInPageAsync(`
     (function(){
       var iframe = document.createElement('iframe');
-      iframe.src = '${resolvedPath}';
+      iframe.src = '${TestRunner.url(path)}';
       iframe.onload = onload;
       document.body.appendChild(iframe);
 
@@ -830,6 +821,15 @@ TestRunner.waitForUISourceCodeRemoved = function(callback) {
   Workspace.workspace.once(Workspace.Workspace.Events.UISourceCodeRemoved).then(callback);
 };
 
+/**
+ * @param {string} relativeURL
+ * @return {string}
+ */
+TestRunner.url = function(relativeURL) {
+  var testScriptURL = /** @type {string} */ (Runtime.queryParam('test'));
+  return testScriptURL + '/../' + relativeURL;
+};
+
 /** @type {boolean} */
 IntegrationTestRunner._startedTest = false;
 
@@ -858,8 +858,7 @@ IntegrationTestRunner.TestObserver = class {
 };
 
 IntegrationTestRunner.runTest = async function() {
-  var testScriptURL = /** @type {string} */ (Runtime.queryParam('test'));
-  var basePath = testScriptURL + '/../';
+  var basePath = TestRunner.url('');
   await TestRunner.evaluateInPagePromise(`
     function relativeToTest(relativePath) {
       return '${basePath}' + relativePath;
