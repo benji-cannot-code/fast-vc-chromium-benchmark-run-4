28d682fff8be211808bd3b71e558fd2c8cef560e
Tom Anderson <thomasanderson@chromium.org>
2017-10-03 17:08:15 +0000

    Remove native window nav button feature flag
    BUG=753067
    R=sky@chromium.org
    CC=erg@chromium.org
    
    Change-Id: I360e5260e155e55513c2f7b3e1572d665a17423a
    Reviewed-on: https://chromium-review.googlesource.com/693375
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506080}

--- chrome/browser/ui/views/frame/opaque_browser_frame_view.cc
+++ chrome/browser/ui/views/frame/opaque_browser_frame_view.cc
@@ -19,8 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/tab_icon_view.h"
 #include "chrome/browser/ui/views/tabs/tab_strip.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
-#include "chrome/common/chrome_features.h"
-#include "chrome/common/features.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/theme_resources.h"
 #include "components/strings/grit/components_strings.h"
@@ -436,8 +434,6 @@ gfx::Size OpaqueBrowserFrameView::GetTabstripPreferredSize() const {
 bool OpaqueBrowserFrameView::ShouldRenderNativeNavButtons() const {
 #if BUILDFLAG(ENABLE_NATIVE_WINDOW_NAV_BUTTONS)
   if (!nav_button_provider_)
-    return false;
-  if (!base::FeatureList::IsEnabled(features::kNativeWindowNavButtons))
     return false;
   return ThemeServiceFactory::GetForProfile(
              browser_view()->browser()->profile())
