e424c5039d9aea4c6e57f354fea11e233e4b2565
Eugene But <eugenebut@chromium.org>
2017-10-03 01:57:56 +0000

    Removed extra ios/web includes from tab_model and tab_model_unittest
    Bug: 620480
    Change-Id: Ie13eb8615381509900da7df7d78a9f93e13fdc58
    Reviewed-on: https://chromium-review.googlesource.com/693050
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505919}

--- ios/chrome/browser/tabs/tab_model.mm
+++ ios/chrome/browser/tabs/tab_model.mm
@@ -61,7 +61,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/serializable_user_data_manager.h"
 #include "ios/web/public/web_state/session_certificate_policy_cache.h"
 #include "ios/web/public/web_thread.h"
-#import "ios/web/web_state/ui/crw_web_controller.h"
 #include "url/gurl.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
