b4db8c505a9cd24024b083193acf992ac1b2e4c3
Tom Anderson <thomasanderson@chromium.org>
2017-10-03 17:06:41 +0000

    Linux native notifications: Limit origin url to eTLD+1
    BUG=676220
    R=peter@chromium.org
    CC=thestig@chromium.org
    
    Change-Id: I0725d4ca3eed639f0284f3f03191d58abcca9295
    Reviewed-on: https://chromium-review.googlesource.com/692682
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506079}

--- chrome/browser/notifications/notification_platform_bridge_linux.cc
+++ chrome/browser/notifications/notification_platform_bridge_linux.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "dbus/message.h"
 #include "dbus/object_proxy.h"
 #include "net/base/escape.h"
+#include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 #include "skia/ext/image_operations.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
@@ -84,6 +85,9 @@ const int kMaxImageHeight = 100;
 
 // Notification on-screen time, in milliseconds.
 const int32_t kExpireTimeout = 25000;
+
+// The maximum amount of characters for displaying the full origin path.
+const size_t kMaxAllowedOriginLength = 28;
 
 // The values in this enumeration correspond to those of the
 // Linux.NotificationPlatformBridge.InitializationStatus histogram, so
@@ -545,6 +549,16 @@ class NotificationPlatformBridgeLinuxImpl
             base::UTF16ToUTF8(url_formatter::FormatUrlForSecurityDisplay(
                 notification->origin_url(),
                 url_formatter::SchemeDisplay::OMIT_HTTP_AND_HTTPS));
+        if (url_display_text.size() > kMaxAllowedOriginLength) {
+          std::string domain_and_registry =
+              net::registry_controlled_domains::GetDomainAndRegistry(
+                  notification->origin_url(),
+                  net::registry_controlled_domains::INCLUDE_PRIVATE_REGISTRIES);
+          // localhost, raw IPs etc. are not handled by GetDomainAndRegistry.
+          if (!domain_and_registry.empty()) {
+            url_display_text = domain_and_registry;
+          }
+        }
         EscapeUnsafeCharacters(&url_display_text);
         if (body_markup &&
             base::ContainsKey(capabilities_, kCapabilityBodyHyperlinks)) {
@@ -565,9 +579,11 @@ class NotificationPlatformBridgeLinuxImpl
       std::string message = base::UTF16ToUTF8(notification->message());
       if (body_markup)
         EscapeUnsafeCharacters(&message);
-      if (body.tellp())
-        body << "\n";
-      body << message;
+      if (!message.empty()) {
+        if (body.tellp())
+          body << "\n";
+        body << message;
+      }
 
       if (notification->type() == message_center::NOTIFICATION_TYPE_MULTIPLE) {
         for (const auto& item : notification->items()) {
