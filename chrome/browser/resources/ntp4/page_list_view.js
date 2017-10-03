f3d8c72d45062f615e955b96e3612f2b35a8795b
Evan Stade <estade@chromium.org>
2017-10-03 00:35:28 +0000

    Remove obsolete special casing in ntp_resource_cache.cc
    The showApps property is only used by ntp4.js, which despite its name
    is the apps page, which is non-Chrome OS desktop only.
    
    Bug: 770866
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I16e313cb01f28dbe76cc42f79f42d6bb4d126f7c
    Reviewed-on: https://chromium-review.googlesource.com/695710
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505884}

--- chrome/browser/resources/ntp4/page_list_view.js
+++ chrome/browser/resources/ntp4/page_list_view.js
@@ -163,9 +163,6 @@ cr.define('ntp', function() {
 
       this.shownPageIndex = loadTimeData.getInteger('shown_page_index');
 
-      // TODO(dbeam): remove showApps and everything that says if (apps).
-      assert(loadTimeData.getBoolean('showApps'));
-
       // Request data on the apps so we can fill them in.
       // Note that this is kicked off asynchronously.  'getAppsCallback' will
       // be invoked at some point after this function returns.
@@ -262,8 +259,6 @@ cr.define('ntp', function() {
      *     position indices.
      */
     appMoved: function(appData) {
-      assert(loadTimeData.getBoolean('showApps'));
-
       var app = /** @type {ntp.App} */ ($(appData.id));
       assert(app, 'trying to move an app that doesn\'t exist');
       app.remove(false);
@@ -281,8 +276,6 @@ cr.define('ntp', function() {
      * @param {boolean} fromPage True if the removal was from the current page.
      */
     appRemoved: function(appData, isUninstall, fromPage) {
-      assert(loadTimeData.getBoolean('showApps'));
-
       var app = /** @type {ntp.App} */ ($(appData.id));
       assert(app, 'trying to remove an app that doesn\'t exist');
 
@@ -317,8 +310,6 @@ cr.define('ntp', function() {
      *     An object with all the data on available applications.
      */
     getAppsCallback: function(data) {
-      assert(loadTimeData.getBoolean('showApps'));
-
       var startTime = Date.now();
 
       // Remember this to select the correct card when done rebuilding.
@@ -424,8 +415,6 @@ cr.define('ntp', function() {
      *     be highlighted.
      */
     appAdded: function(appData, opt_highlight) {
-      assert(loadTimeData.getBoolean('showApps'));
-
       if (appData.id == this.highlightAppId) {
         opt_highlight = true;
         this.highlightAppId = null;
@@ -460,8 +449,6 @@ cr.define('ntp', function() {
      *     applications.
      */
     appsPrefChangedCallback: function(data) {
-      assert(loadTimeData.getBoolean('showApps'));
-
       for (var i = 0; i < data.apps.length; ++i) {
         $(data.apps[i].id).appData = data.apps[i];
       }
@@ -501,10 +488,8 @@ cr.define('ntp', function() {
           0, Math.min(this.cardSlider.currentCard, this.tilePages.length - 1));
       this.cardSlider.setCards(
           Array.prototype.slice.call(this.tilePages), pageNo);
-      if (loadTimeData.getBoolean('showApps')) {
-        this.cardSlider.selectCardByValue(this.appsPages[Math.min(
-            this.shownPageIndex, this.appsPages.length - 1)]);
-      }
+      this.cardSlider.selectCardByValue(this.appsPages[Math.min(
+          this.shownPageIndex, this.appsPages.length - 1)]);
     },
 
     /**
@@ -512,12 +497,10 @@ cr.define('ntp', function() {
      * of a moving or insert tile.
      */
     enterRearrangeMode: function() {
-      if (loadTimeData.getBoolean('showApps')) {
-        var tempPage = new ntp.AppsPage();
-        tempPage.classList.add('temporary');
-        var pageName = loadTimeData.getString('appDefaultPageName');
-        this.appendTilePage(tempPage, pageName, true);
-      }
+      var tempPage = new ntp.AppsPage();
+      tempPage.classList.add('temporary');
+      var pageName = loadTimeData.getString('appDefaultPageName');
+      this.appendTilePage(tempPage, pageName, true);
 
       if (ntp.getCurrentlyDraggingTile().firstChild.canBeRemoved()) {
         $('footer').classList.add('showing-trash-mode');
