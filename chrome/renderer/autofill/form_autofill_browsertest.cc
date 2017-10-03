1cfc9a58b7ecfaba0a87c5b277ff2d10430d7634
Sebastien Seguin-Gagnon <sebsg@chromium.org>
2017-10-03 12:40:55 +0000

    Revert "[Autofill] Use ShadowDom placeholder to preview suggestions."
    This reverts commit 0727466e09c7285f3c4fe6a7974bfc68fd9bbccb.
    
    Reason for revert: Makes some passwords be visible when suggested to the user.
    
    Bug: 771097
    
    
    Original change's description:
    > [Autofill] Use ShadowDom placeholder to preview suggestions.
    > 
    > Bug: 753645
    > Change-Id: Idabb4d01b45aa08a71f9fc8ad1dd89b192cfb0c5
    > Reviewed-on: https://chromium-review.googlesource.com/646754
    > Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#505743}
    
    TBR=yosin@chromium.org,tkent@chromium.org,sebsg@chromium.org
    
    Change-Id: I3a874ea052f598724831bdfcf0e16576a09353b4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 753645
    Reviewed-on: https://chromium-review.googlesource.com/697644
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506011}

--- chrome/renderer/autofill/form_autofill_browsertest.cc
+++ chrome/renderer/autofill/form_autofill_browsertest.cc
@@ -599,10 +599,8 @@ class FormAutofillTest : public ChromeRenderViewTest {
 
     // Verify preview selection.
     WebInputElement firstname = GetInputElementById("firstname");
-    // Since the suggestion is previewed as a placeholder, there should be no
-    // selected text.
     EXPECT_EQ(0, firstname.SelectionStart());
-    EXPECT_EQ(0, firstname.SelectionEnd());
+    EXPECT_EQ(19, firstname.SelectionEnd());
   }
 
   void TestUnmatchedUnownedForm(const char* html, const char* url_override) {
@@ -1151,10 +1149,8 @@ class FormAutofillTest : public ChromeRenderViewTest {
     form.fields[1].is_autofilled = true;
     form.fields[2].is_autofilled = true;
     PreviewForm(form, input_element);
-    // Since the suggestion is previewed as a placeholder, there should be no
-    // selected text.
-    EXPECT_EQ(0, input_element.SelectionStart());
-    EXPECT_EQ(0, input_element.SelectionEnd());
+    EXPECT_EQ(2, input_element.SelectionStart());
+    EXPECT_EQ(5, input_element.SelectionEnd());
 
     // Fill the form.
     FillForm(form, input_element);
