3d42c4824bbb11a0565b5601f830f0ca3b842711
Hidehiko Abe <hidehiko@chromium.org>
2017-10-03 06:13:42 +0000

    Add presubmit check about std::unique_ptr().
    BUG=None
    TEST=Ran locally/manually. Ran bots.
    
    Change-Id: I53a719481f0614d65e6446ea07fbc92adfd7f9fb
    Reviewed-on: https://chromium-review.googlesource.com/691754
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505962}

--- chrome/browser/chromeos/arc/PRESUBMIT.py
+++ chrome/browser/chromeos/arc/PRESUBMIT.py
@@ -4,4 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 def CheckChangeOnUpload(input_api, output_api):
-  return input_api.canned_checks.CheckChangeLintsClean(input_api, output_api)
+  # Apply the same PRESUBMIT for components/arc.
+  presubmit_path = (
+      input_api.change.RepositoryRoot() + '/components/arc/PRESUBMIT.py')
+  presubmit_content = input_api.ReadFile(presubmit_path)
+  global_vars = {}
+  exec(presubmit_content, global_vars)
+  return global_vars['CheckChangeOnUpload'](input_api, output_api)
