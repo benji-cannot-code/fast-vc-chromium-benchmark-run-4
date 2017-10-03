47a4e47f8a39970dcfd13d28d2265e5111e1fa67
Lukasz Anforowicz <lukasza@chromium.org>
2017-10-03 00:20:43 +0000

    Removing callers of the WebContents::GetRenderProcessHost() method.
    WebContents::GetRenderProcessHost is an API that doesn't behave as
    expected with out-of-process iframes, as multiple processes can be
    associated with a single WebContents.  This CL replaces callers of this
    API, so that they go through an equivalent API that explicitly selects
    which frame's process is needed.
    
    This CL was started by using an ad-hoc clang-plugin that replaced
    existing callsites of WebContents::GetRenderProcessHost() with a call to
    something like wc->GetMainFrame()->GetProcess().  This was followed-up
    by manually adding an include of render_frame_host.h and git cl format
    and git cl lint and then a self-review with small tweaks (e.g. sometimes
    using a wc->GetRenderViewHost()->GetProcess() is more appropriate).
    
    This CL was uploaded by git cl split.
    
    R=raymes@chromium.org
    
    Bug: 666525
    Change-Id: Ic44b6d3465e3affd405f188c7743fbe543f846c4
    Reviewed-on: https://chromium-review.googlesource.com/688898
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505874}

--- chrome/browser/content_settings/tab_specific_content_settings.cc
+++ chrome/browser/content_settings/tab_specific_content_settings.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/content_settings/tab_specific_content_settings.h"
 
 #include <list>
+#include <vector>
 
 #include "base/command_line.h"
 #include "base/lazy_instance.h"
@@ -742,7 +743,7 @@ void TabSpecificContentSettings::OnContentSettingChanged(
     GetRendererContentSettingRules(map, &rules);
 
     IPC::ChannelProxy* channel =
-        web_contents()->GetRenderProcessHost()->GetChannel();
+        web_contents()->GetMainFrame()->GetProcess()->GetChannel();
     // channel might be NULL in tests.
     if (channel) {
       chrome::mojom::RendererConfigurationAssociatedPtr rc_interface;
