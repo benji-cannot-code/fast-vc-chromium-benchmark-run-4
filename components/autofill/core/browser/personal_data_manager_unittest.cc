2dc42b310297882d3b3681b86e1e07192d63503e
Roger McFarlane <rogerm@chromium.org>
2017-10-03 16:21:45 +0000

    [autofill] Clean up PDM GetProfiles+GetCreditCards
    Due to legacy/technical-debt, the autofill::PersonalDataManager
    was holding a cache of profile and credit card raw pointers in
    addition to the owning vectors of std::unique_ptr for each. The
    getter functions for these raw pointer vectors was wastefully
    clearing the vectors and repopulating them on each get.
    
    This CL cleans up the useless cache variables, fixes the function
    signatures, and adds explicit memory reservations to the getters. It
    also merges several identical getter functions for retrieving the
    profile pointers: GetProfiles(), GetProfiles(bool), web_profiles().
    
    Bug: 687352
    Change-Id: Id614d14832ce926a75e61d2663a0be7805988e33
    Reviewed-on: https://chromium-review.googlesource.com/692717
    Commit-Queue: Roger McFarlane <rogerm@chromium.org>
    Reviewed-by: Nicolas Zea <zea@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506062}

--- components/autofill/core/browser/personal_data_manager_unittest.cc
+++ components/autofill/core/browser/personal_data_manager_unittest.cc
@@ -3343,7 +3343,7 @@ TEST_F(PersonalDataManagerTest, UpdateLanguageCodeInProfile) {
 
   // Make sure everything is set up correctly.
   WaitForOnPersonalDataChanged();
-  EXPECT_EQ(1U, personal_data_->web_profiles().size());
+  EXPECT_EQ(1U, personal_data_->GetProfiles().size());
   EXPECT_EQ(1U, personal_data_->GetProfiles().size());
 
   profile.set_language_code("en");
@@ -6714,7 +6714,7 @@ TEST_F(PersonalDataManagerTest,
   // Make sure everything is set up correctly.
   personal_data_->Refresh();
   WaitForOnPersonalDataChanged();
-  EXPECT_EQ(1U, personal_data_->web_profiles().size());
+  EXPECT_EQ(1U, personal_data_->GetProfiles().size());
   EXPECT_EQ(1U, personal_data_->GetServerProfiles().size());
   EXPECT_EQ(2U, personal_data_->GetCreditCards().size());
 
@@ -6729,7 +6729,7 @@ TEST_F(PersonalDataManagerTest,
   WaitForOnPersonalDataChanged();
 
   // The Wallet address should have been added as a new local profile.
-  EXPECT_EQ(2U, personal_data_->web_profiles().size());
+  EXPECT_EQ(2U, personal_data_->GetProfiles().size());
   EXPECT_EQ(1U, personal_data_->GetServerProfiles().size());
   histogram_tester.ExpectUniqueSample("Autofill.WalletAddressConversionType",
                                       AutofillMetrics::CONVERTED_ADDRESS_ADDED,
@@ -6819,7 +6819,7 @@ TEST_F(PersonalDataManagerTest,
   // Make sure everything is set up correctly.
   personal_data_->Refresh();
   WaitForOnPersonalDataChanged();
-  EXPECT_EQ(1U, personal_data_->web_profiles().size());
+  EXPECT_EQ(1U, personal_data_->GetProfiles().size());
   EXPECT_EQ(1U, personal_data_->GetServerProfiles().size());
   EXPECT_EQ(2U, personal_data_->GetCreditCards().size());
 
@@ -6834,7 +6834,7 @@ TEST_F(PersonalDataManagerTest,
   WaitForOnPersonalDataChanged();
 
   // The Wallet address should have been merged with the existing local profile.
-  EXPECT_EQ(1U, personal_data_->web_profiles().size());
+  EXPECT_EQ(1U, personal_data_->GetProfiles().size());
   EXPECT_EQ(1U, personal_data_->GetServerProfiles().size());
   histogram_tester.ExpectUniqueSample("Autofill.WalletAddressConversionType",
                                       AutofillMetrics::CONVERTED_ADDRESS_MERGED,
@@ -6891,7 +6891,7 @@ TEST_F(PersonalDataManagerTest,
   // Make sure everything is set up correctly.
   personal_data_->Refresh();
   WaitForOnPersonalDataChanged();
-  EXPECT_EQ(0U, personal_data_->web_profiles().size());
+  EXPECT_EQ(0U, personal_data_->GetProfiles().size());
   EXPECT_EQ(1U, personal_data_->GetServerProfiles().size());
 
   ///////////////////////////////////////////////////////////////////////
@@ -6910,7 +6910,7 @@ TEST_F(PersonalDataManagerTest,
   WaitForOnPersonalDataChanged();
 
   // There should be no local profiles added.
-  EXPECT_EQ(0U, personal_data_->web_profiles().size());
+  EXPECT_EQ(0U, personal_data_->GetProfiles().size());
   EXPECT_EQ(1U, personal_data_->GetServerProfiles().size());
 }
 
@@ -6987,7 +6987,7 @@ TEST_F(
   // Make sure everything is set up correctly.
   personal_data_->Refresh();
   WaitForOnPersonalDataChanged();
-  EXPECT_EQ(1U, personal_data_->web_profiles().size());
+  EXPECT_EQ(1U, personal_data_->GetProfiles().size());
   EXPECT_EQ(2U, personal_data_->GetServerProfiles().size());
   EXPECT_EQ(2U, personal_data_->GetCreditCards().size());
 
@@ -7003,7 +7003,7 @@ TEST_F(
 
   // The first Wallet address should have been added as a new local profile and
   // the second one should have merged with the first.
-  EXPECT_EQ(2U, personal_data_->web_profiles().size());
+  EXPECT_EQ(2U, personal_data_->GetProfiles().size());
   EXPECT_EQ(2U, personal_data_->GetServerProfiles().size());
   histogram_tester.ExpectBucketCount("Autofill.WalletAddressConversionType",
                                      AutofillMetrics::CONVERTED_ADDRESS_ADDED,
@@ -7089,7 +7089,7 @@ TEST_F(
   WaitForOnPersonalDataChanged();
 
   // The Wallet address should have been converted to a new local profile.
-  EXPECT_EQ(1U, personal_data_->web_profiles().size());
+  EXPECT_EQ(1U, personal_data_->GetProfiles().size());
 
   // The conversion should be recorded in the Wallet address.
   EXPECT_TRUE(personal_data_->GetServerProfiles().back()->has_converted());
@@ -7114,7 +7114,7 @@ TEST_F(
   // Make sure everything is set up correctly.
   personal_data_->Refresh();
   WaitForOnPersonalDataChanged();
-  EXPECT_EQ(1U, personal_data_->web_profiles().size());
+  EXPECT_EQ(1U, personal_data_->GetProfiles().size());
   EXPECT_EQ(2U, personal_data_->GetCreditCards().size());
 
   ///////////////////////////////////////////////////////////////////////
