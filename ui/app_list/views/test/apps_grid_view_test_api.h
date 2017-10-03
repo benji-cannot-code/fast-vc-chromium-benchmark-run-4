ee204fa80640567c761ae3e7f2058c209a4b6186
Qiang Xu <warx@chromium.org>
2017-10-03 18:42:04 +0000

    app_list: Fix launcher apps page scrolling jarring
    changes:
    (1) apps grid view fixed 576px width and 623px height.
    (2) app list folder view fixed 576px width and 504px height.
    (3) add GetExpectedTileBounds(const Index& index) API, since now first
    page and other pages are different. It is better to have parameter that
    is page dependent. This removes the offset adjustment logic in
    CalculateIdealBounds.
    (4) While calculating page transition offset, for page break space that
    is 80px (56px+24px), reduces it to 48px. But do not over adjust it, it
    is ok with 80px there since they are already because we clip icons which
    are outside the grid view's bounds.
    
    Bug: 767663
    Test: device test
    Change-Id: Ibd08cf3c3c93ee48a20f2ba47c5a2680a9389c04
    Reviewed-on: https://chromium-review.googlesource.com/696964
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Qiang(Joe) Xu <warx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506119}

--- ui/app_list/views/test/apps_grid_view_test_api.h
+++ ui/app_list/views/test/apps_grid_view_test_api.h
@@ -31,7 +31,7 @@ class AppsGridViewTestApi {
 
   void LayoutToIdealBounds();
 
-  gfx::Rect GetItemTileRectAt(int row, int col) const;
+  gfx::Rect GetItemTileRectOnCurrentPageAt(int row, int col) const;
 
   void PressItemAt(int index);
 
