3d6dd045eb2e57c45a87a7dac76e326be50c8b44
Tarun Bansal <tbansal@chromium.org>
2017-10-03 22:23:36 +0000

    Notify external estimate provider on connection change
    If there is a connection change, Network Quality
    Estimator (NQE) notifies the external estimate provider
    
    Bug: 768264
    Change-Id: I3f037645af8eb6b105652c6cae78b31308dfb42d
    Reviewed-on: https://chromium-review.googlesource.com/698876
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506208}

--- net/nqe/network_quality_estimator_unittest.cc
+++ net/nqe/network_quality_estimator_unittest.cc
@@ -1340,6 +1340,8 @@ class InvalidExternalEstimateProvider : public ExternalEstimateProvider {
   size_t update_count() const { return update_count_; }
 
  private:
+  void ClearCachedEstimate() override {}
+
   mutable size_t update_count_;
 
   DISALLOW_COPY_AND_ASSIGN(InvalidExternalEstimateProvider);
@@ -1386,7 +1388,8 @@ class TestExternalEstimateProvider : public ExternalEstimateProvider {
         should_notify_delegate_(true),
         rtt_(rtt),
         downstream_throughput_kbps_(downstream_throughput_kbps),
-        update_count_(0) {}
+        update_count_(0),
+        cached_estimate_cleared_(false) {}
   ~TestExternalEstimateProvider() override {}
 
   void SetUpdatedEstimateDelegate(UpdatedEstimateDelegate* delegate) override {
@@ -1406,7 +1409,11 @@ class TestExternalEstimateProvider : public ExternalEstimateProvider {
 
   size_t update_count() const { return update_count_; }
 
+  bool cached_estimate_cleared() { return cached_estimate_cleared_; }
+
  private:
+  void ClearCachedEstimate() override { cached_estimate_cleared_ = true; }
+
   UpdatedEstimateDelegate* delegate_;
 
   bool should_notify_delegate_;
@@ -1416,6 +1423,9 @@ class TestExternalEstimateProvider : public ExternalEstimateProvider {
   const int32_t downstream_throughput_kbps_;
 
   mutable size_t update_count_;
+
+  // True if the cached estimate has been cleared.
+  bool cached_estimate_cleared_;
 
   DISALLOW_COPY_AND_ASSIGN(TestExternalEstimateProvider);
 };
@@ -1436,8 +1446,11 @@ TEST(NetworkQualityEstimatorTest, TestExternalEstimateProvider) {
       test_external_estimate_provider);
   TestNetworkQualityEstimator estimator(std::map<std::string, std::string>(),
                                         std::move(external_estimate_provider));
+  EXPECT_FALSE(test_external_estimate_provider->cached_estimate_cleared());
   estimator.SimulateNetworkChange(net::NetworkChangeNotifier::CONNECTION_WIFI,
                                   "test");
+  EXPECT_TRUE(test_external_estimate_provider->cached_estimate_cleared());
+
   base::TimeDelta rtt;
   int32_t kbps;
   EXPECT_TRUE(estimator.GetRecentHttpRTT(base::TimeTicks(), &rtt));
