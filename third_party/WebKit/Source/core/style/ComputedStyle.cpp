daa112627481d14a0eea079c90a88beda820c25f
Darren Shen <shend@chromium.org>
2017-10-03 06:18:43 +0000

    [Clockwork] Replace trivial handwritten functions with generated ones.
    This patch replaces some trivial handwritten getters/setters with
    generated ones.
    
    Diff of generated files:
    https://gist.github.com/ab838d33a5e5357c9985c559fc60bcfc/revisions
    
    Change-Id: I7b15266450da8669aa7b17aa4ef34b7de4e35955
    Reviewed-on: https://chromium-review.googlesource.com/688518
    Reviewed-by: nainar <nainar@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505966}

--- third_party/WebKit/Source/core/style/ComputedStyle.cpp
+++ third_party/WebKit/Source/core/style/ComputedStyle.cpp
@@ -709,9 +709,8 @@ void ComputedStyle::UpdatePropertySpecificDifferences(
     diff.SetTextDecorationOrColorChanged();
   }
 
-  bool has_clip = HasOutOfFlowPosition() && !HasAutoClipInternal();
-  bool other_has_clip =
-      other.HasOutOfFlowPosition() && !other.HasAutoClipInternal();
+  bool has_clip = HasOutOfFlowPosition() && !HasAutoClip();
+  bool other_has_clip = other.HasOutOfFlowPosition() && !other.HasAutoClip();
   if (has_clip != other_has_clip ||
       (has_clip && Clip() != other.Clip()))
     diff.SetCSSClipChanged();
@@ -1753,10 +1752,10 @@ void ComputedStyle::ClearMultiCol() {
   SetColumnRuleColorIsCurrentColor(InitialColumnRuleColorIsCurrentColor());
   SetVisitedLinkColumnRuleColorInternal(InitialVisitedLinkColumnRuleColor());
   SetColumnCountInternal(InitialColumnCount());
-  SetColumnAutoCountInternal(InitialColumnAutoCount());
-  SetColumnAutoWidthInternal(InitialColumnAutoWidth());
+  SetHasAutoColumnCountInternal(InitialHasAutoColumnCount());
+  SetHasAutoColumnWidthInternal(InitialHasAutoColumnWidth());
   ResetColumnFill();
-  SetColumnNormalGapInternal(InitialColumnNormalGap());
+  SetHasNormalColumnGapInternal(InitialHasNormalColumnGap());
   ResetColumnSpan();
 }
 
