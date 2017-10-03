2540089c53bbd700fe0f592b7c1d0b682b948b60
Quinten Yearsley <qyearsley@chromium.org>
2017-10-03 21:15:28 +0000

    Update apache32-win to Apache version 2.4.
    This CL should include the non-binary changes, including
    changes to the instructions in README.chromium and httpd.conf,
    as well as sha1 files that correspond to binary files uploaded
    to Google Storage. 
    
    Bug: 747666
    Change-Id: I724e9989045b08f63200618be1aa6061fcd57e42
    Reviewed-on: https://chromium-review.googlesource.com/595041
    Commit-Queue: Quinten Yearsley <qyearsley@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506175}

--- third_party/WebKit/LayoutTests/http/tests/misc/non-utf8-header-name.php
+++ third_party/WebKit/LayoutTests/http/tests/misc/non-utf8-header-name.php
@@ -1,5 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
+# This header with a special character should be meaningless to the browser,
+# and not cause a crash. Note that some HTTP servers may refuse to serve this.
+# See: a href="https://blog.tigertech.net/posts/apache-cve-2016-8743/
 header('HTTP/1.1 200 OK');
 header('Ã: text/html');
 echo '<script>';
