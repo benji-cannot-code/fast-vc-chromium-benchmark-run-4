02f47173929b81db425ab20262cc2cfdf8546250
Weidong Guo <weidongg@chromium.org>
2017-10-04 18:18:55 +0000

    Reland "applist-focus: Handle arrow key focus traversal"
    This is a reland of b123ea271ea0c2d820b813125455522ccb24a0d6
    Original change's description:
    > applist-focus: Handle arrow key focus traversal
    >
    > Changes:
    > 1. Enable arrow key focus handling in FocusManager.
    > 2. Handle arrow up/down in SearchBoxView to move focus to the first/last
    >    focusable element in ContentsView.
    > 3. Handle arrow up/down in AppsGridView. AppsGridView has three states:
    >    Peeking, Fullscreen all apps (outside folder) and Fullscreen all apps
    >    (inside folder). Do separate handling for them, since they have
    >    different layout.
    >
    > Design Doc: go/applist-focus
    >
    > BUG=766810
    > TEST=AppListViewFocusTest.*
    >
    > Change-Id: I2ace4b3b21a69f10011eba2ed0079e1f7aee48d0
    > Reviewed-on: https://chromium-review.googlesource.com/687818
    > Commit-Queue: Weidong Guo <weidongg@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#504818}
    
    Bug: 766810
    Change-Id: Ie7bb141fe052531cc249ed3df5bd70ed2fc2fdbd
    Reviewed-on: https://chromium-review.googlesource.com/688808
    Commit-Queue: Weidong Guo <weidongg@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506446}

--- ui/app_list/views/apps_container_view.cc
+++ ui/app_list/views/apps_container_view.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/events/event.h"
 #include "ui/strings/grit/ui_strings.h"
+#include "ui/views/controls/textfield/textfield.h"
 
 namespace app_list {
 
@@ -39,7 +40,8 @@ constexpr int kSearchBoxMinimumTopPadding = 24;
 
 AppsContainerView::AppsContainerView(AppListMainView* app_list_main_view,
                                      AppListModel* model)
-    : is_fullscreen_app_list_enabled_(features::IsFullscreenAppListEnabled()) {
+    : is_fullscreen_app_list_enabled_(features::IsFullscreenAppListEnabled()),
+      is_app_list_focus_enabled_(features::IsAppListFocusEnabled()) {
   apps_grid_view_ =
       new AppsGridView(app_list_main_view->contents_view(), nullptr);
   if (is_fullscreen_app_list_enabled_) {
@@ -77,7 +79,10 @@ void AppsContainerView::ShowActiveFolder(AppListFolderItem* folder_item) {
 
   CreateViewsForFolderTopItemsAnimation(folder_item, true);
 
-  apps_grid_view_->ClearAnySelectedView();
+  if (is_app_list_focus_enabled_)
+    contents_view()->GetSearchBoxView()->search_box()->RequestFocus();
+  else
+    apps_grid_view_->ClearAnySelectedView();
 }
 
 void AppsContainerView::ShowApps(AppListFolderItem* folder_item) {
