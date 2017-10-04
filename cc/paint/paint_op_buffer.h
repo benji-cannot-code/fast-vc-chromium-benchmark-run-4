9ae4c0214ae4439f0f00ad6d7d4831fcc00261f5
Khushal <khushalsagar@chromium.org>
2017-10-04 02:52:03 +0000

    Revert "Reland cc: Decode discardable images in PaintRecord backed shaders."
    This reverts commit 53b8ed642644f245e3cb8d5cf2541bf3778ae7de.
    
    Reason for revert: Causing flakiness on Linux Release (NVIDIA) for webgl_conformance_tests. See crbug.com/771156.
    
    Original change's description:
    > Reland cc: Decode discardable images in PaintRecord backed shaders.
    > 
    > This reverts commit fe24d7f3ac1b5bcfb840a5ada01627df90a099eb. Since we
    > rasterize the SkPicture with decoded images in the test, we need to
    > initialize its pixels. Fix that.
    > 
    > R=​enne@chromium.org
    > TBR=chrishtr@chromium.org
    > 
    > Bug: 735741,728359
    > Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    > Change-Id: I10989e2bc28594c9fa8e291559ae2753e65de29d
    > Reviewed-on: https://chromium-review.googlesource.com/695449
    > Reviewed-by: enne <enne@chromium.org>
    > Commit-Queue: Khushal <khushalsagar@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#505885}
    
    TBR=chrishtr@chromium.org,enne@chromium.org,khushalsagar@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 735741, 728359
    Change-Id: I9bc577e4f6488ef3bccb991609828db20467231f
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Reviewed-on: https://chromium-review.googlesource.com/699755
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506285}

--- cc/paint/paint_op_buffer.h
+++ cc/paint/paint_op_buffer.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/aligned_memory.h"
 #include "base/optional.h"
 #include "cc/base/math_util.h"
-#include "cc/paint/image_provider.h"
 #include "cc/paint/paint_canvas.h"
 #include "cc/paint/paint_export.h"
 #include "cc/paint/paint_flags.h"
@@ -31,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc {
 class ImageDecodeCache;
+class ImageProvider;
 
 class CC_PAINT_EXPORT ThreadsafeMatrix : public SkMatrix {
  public:
@@ -138,10 +138,6 @@ class CC_PAINT_EXPORT PaintOp {
   // for the op.
   static bool GetBounds(const PaintOp* op, SkRect* rect);
 
-  // Returns true if executing this op will require decoding of any lazy
-  // generated images.
-  static bool OpHasDiscardableImages(const PaintOp* op);
-
   int CountSlowPaths() const { return 0; }
   int CountSlowPathsFromFlags() const { return 0; }
 
@@ -200,7 +196,14 @@ class CC_PAINT_EXPORT PaintOpWithFlags : public PaintOp {
 
   int CountSlowPathsFromFlags() const { return flags.getPathEffect() ? 1 : 0; }
   bool HasNonAAPaint() const { return !flags.isAntiAlias(); }
-  bool HasDiscardableImagesFromFlags() const;
+  bool HasDiscardableImagesFromFlags() const {
+    if (!IsDrawOp())
+      return false;
+
+    SkShader* shader = flags.getSkShader();
+    SkImage* image = shader ? shader->isAImage(nullptr, nullptr) : nullptr;
+    return image && image->isLazyGenerated();
+  }
 
   void RasterWithFlags(SkCanvas* canvas,
                        const PaintFlags* flags,
@@ -979,7 +982,6 @@ class CC_PAINT_EXPORT PaintOpBuffer : public SkRefCnt {
   friend class DisplayItemList;
   friend class PaintOpBufferOffsetsTest;
   friend class SolidColorAnalyzer;
-  friend class ScopedImageFlags;
 
   // Replays the paint op buffer into the canvas. If |indices| is specified, it
   // contains indices in an increasing order and only the indices specified in
