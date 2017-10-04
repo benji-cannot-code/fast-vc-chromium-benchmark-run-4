4a827c62de0d062d4f9adc5d0570af475bf9c581
Hector Carmona <hcarmona@chromium.org>
2017-10-04 21:45:06 +0000

    WebUI: Move A11y Audit files to generic location to test Extensions.
    Bug: 764125
    Change-Id: Id90db21dd9333565bee94da935df90639be082c6
    Reviewed-on: https://chromium-review.googlesource.com/699567
    Commit-Queue: Hector Carmona <hcarmona@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506529}

--- chrome/test/data/webui/a11y/accessibility_test.js
+++ chrome/test/data/webui/a11y/accessibility_test.js
@@ -7,6 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview Accessibility Test API
  */
 
+GEN_INCLUDE([
+  'accessibility_audit_rules.js',
+]);
+
 /**
  * Accessibility Test
  * @namespace
