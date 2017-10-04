ae56032f145648e81c667821ad9e5f9c92eadcf5
Thomas Guilbert <tguilbert@chromium.org>
2017-10-04 03:38:29 +0000

    Fix int64 overflow in WebmClusterParser
    If the value of |cluster_timecode_| and |timecode_multiplier_|
    is big enough, we can end up with a value that does not fit in
    an int64_t. This in turn means that all buffers will have kNoTimestamp
    as a timestamp, which causes some CHECKs to fail later on.
    
    Bug: 764606, 765627
    Change-Id: Ia11b19606a05c412390a597ae5a4ae5ccf2dab3c
    Reviewed-on: https://chromium-review.googlesource.com/699522
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Matthew Wolenetz <wolenetz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506294}

--- media/formats/webm/webm_cluster_parser.cc
+++ media/formats/webm/webm_cluster_parser.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/macros.h"
+#include "base/numerics/checked_math.h"
 #include "base/sys_byteorder.h"
 #include "media/base/decrypt_config.h"
 #include "media/base/timestamp_constants.h"
@@ -494,8 +495,21 @@ bool WebMClusterParser::OnBlock(bool is_simple_block,
 
   last_block_timecode_ = timecode;
 
-  base::TimeDelta timestamp = base::TimeDelta::FromMicroseconds(
-      (cluster_timecode_ + timecode) * timecode_multiplier_);
+  int64_t microseconds;
+
+  if (!base::CheckMul(base::CheckAdd(cluster_timecode_, timecode),
+                      timecode_multiplier_)
+           .AssignIfValid(&microseconds)) {
+    MEDIA_LOG(ERROR, media_log_) << "Invalid cluster timecode.";
+    return false;
+  }
+
+  base::TimeDelta timestamp = base::TimeDelta::FromMicroseconds(microseconds);
+
+  if (timestamp == kNoTimestamp || timestamp == kInfiniteDuration) {
+    MEDIA_LOG(ERROR, media_log_) << "Invalid block timestamp.";
+    return false;
+  }
 
   scoped_refptr<StreamParserBuffer> buffer;
   if (buffer_type != DemuxerStream::TEXT) {
