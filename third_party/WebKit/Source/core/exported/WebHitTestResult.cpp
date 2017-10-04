1ab9609336432649f6241a0c3c6d17b6d267a508
Yoshifumi Inoue <yosin@chromium.org>
2017-10-04 09:56:33 +0000

    Get rid of redundant include of "VisiblePosition.h" from "WebHitTestResult.cpp"
    This patch gets rid of redundant include of "VisiblePosition.h" from
    "WebHitTestResult.cpp" for improving code health.
    
    Bug: 657237
    Change-Id: I80b9c1a5f534706fa3d86df4b6c91758d9a02b49
    Reviewed-on: https://chromium-review.googlesource.com/700355
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506353}

--- third_party/WebKit/Source/core/exported/WebHitTestResult.cpp
+++ third_party/WebKit/Source/core/exported/WebHitTestResult.cpp
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/dom/Element.h"
 #include "core/dom/Node.h"
-#include "core/editing/VisiblePosition.h"
 #include "core/layout/HitTestResult.h"
 #include "platform/weborigin/KURL.h"
 #include "public/platform/WebPoint.h"
