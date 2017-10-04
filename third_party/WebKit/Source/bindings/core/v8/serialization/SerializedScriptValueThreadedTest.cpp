376964644f5fa37edcdb29f1763c7065f507967e
Henrik Kjellander <kjellander@chromium.org>
2017-10-04 09:12:35 +0000

    Disable SerializedScriptValueThreadedTest.SafeDestructionIfSendingThreadKeepsAlive on Windows.
    This test which is a part of webkit_unit_tests is failing flakily on 'WebKit Win x64 Builder (dbg)'.
    
    BUG=771169
    TBR=jbroman@chromium.org
    
    Change-Id: If43cce306846ca6f3baf6de923afd81a4652e90e
    Reviewed-on: https://chromium-review.googlesource.com/700257
    Reviewed-by: Henrik Kjellander <kjellander@chromium.org>
    Commit-Queue: Henrik Kjellander <kjellander@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506343}

--- third_party/WebKit/Source/bindings/core/v8/serialization/SerializedScriptValueThreadedTest.cpp
+++ third_party/WebKit/Source/bindings/core/v8/serialization/SerializedScriptValueThreadedTest.cpp
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/V8BindingForTesting.h"
 #include "bindings/core/v8/WorkerOrWorkletScriptController.h"
 #include "bindings/core/v8/serialization/UnpackedSerializedScriptValue.h"
+#include "build/build_config.h"
 #include "core/dom/Document.h"
 #include "core/typed_arrays/DOMArrayBuffer.h"
 #include "core/workers/WorkerThreadTestHelper.h"
@@ -18,8 +19,16 @@ namespace blink {
 
 // On debug builds, Oilpan contains checks that will fail if a persistent handle
 // is destroyed on the wrong thread.
+// Flaky test on 'WebKit Win x64 Builder (dbg)'. See crbug.com/771169.
+#if defined(OS_WIN)
+#define MAYBE_SafeDestructionIfSendingThreadKeepsAlive \
+  DISABLED_SafeDestructionIfSendingThreadKeepsAlive
+#else
+#define MAYBE_SafeDestructionIfSendingThreadKeepsAlive \
+  SafeDestructionIfSendingThreadKeepsAlive
+#endif
 TEST(SerializedScriptValueThreadedTest,
-     SafeDestructionIfSendingThreadKeepsAlive) {
+     MAYBE_SafeDestructionIfSendingThreadKeepsAlive) {
   V8TestingScope scope;
 
   // Start a worker.
