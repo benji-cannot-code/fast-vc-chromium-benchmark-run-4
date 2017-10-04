6e613767b373fbaf5164085b864b18050cb37747
Jochen Eisinger <jochen@chromium.org>
2017-10-04 10:58:13 +0000

    Don't consume user gestures when we don't support multiple windows
    There's no point in protecting against multiple popups in that case..
    
    BUG=762482
    R=torne@chromium.org
    
    Change-Id: Ic8c953ff5235c5cb32b1db318c3828b86d722a95
    Reviewed-on: https://chromium-review.googlesource.com/695543
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506363}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwSettingsTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwSettingsTest.java
@@ -1141,10 +1141,10 @@ public class AwSettingsTest {
         private static final String POPUP_ENABLED = "Popup enabled";
         private static final String POPUP_BLOCKED = "Popup blocked";
 
-        AwSettingsJavaScriptPopupsTestHelper(
-                AwTestContainerView containerView,
-                TestAwContentsClient contentViewClient) throws Throwable {
+        AwSettingsJavaScriptPopupsTestHelper(AwTestContainerView containerView,
+                TestAwContentsClient contentViewClient, boolean openTwice) throws Throwable {
             super(containerView, contentViewClient, true);
+            mOpenTwice = openTwice;
         }
 
         @Override
@@ -1184,6 +1184,7 @@ public class AwSettingsTest {
                     + "<script>"
                     + "    function tryOpenWindow() {"
                     + "        var newWindow = window.open('about:blank');"
+                    + (mOpenTwice ? "newWindow = window.open('about:blank');" : "")
                     + "        if (newWindow) {"
                     + "            if (newWindow === window) {"
                     + "                newWindow.document.write("
@@ -1199,6 +1200,8 @@ public class AwSettingsTest {
                     + "</script></head>"
                     + "<body onload='tryOpenWindow()'></body></html>";
         }
+
+        private boolean mOpenTwice;
     }
 
     class AwSettingsCacheModeTestHelper extends AwSettingsTestHelper<Integer> {
@@ -2264,10 +2267,10 @@ public class AwSettingsTest {
     @Feature({"AndroidWebView", "Preferences"})
     public void testJavaScriptPopupsWithTwoViews() throws Throwable {
         ViewPair views = createViews();
-        runPerViewSettingsTest(
-                new AwSettingsJavaScriptPopupsTestHelper(views.getContainer0(), views.getClient0()),
+        runPerViewSettingsTest(new AwSettingsJavaScriptPopupsTestHelper(
+                                       views.getContainer0(), views.getClient0(), false),
                 new AwSettingsJavaScriptPopupsTestHelper(
-                        views.getContainer1(), views.getClient1()));
+                        views.getContainer1(), views.getClient1(), false));
     }
 
     @Test
@@ -2283,10 +2286,21 @@ public class AwSettingsTest {
         });
         views.getClient0().getOnCreateWindowHelper().setReturnValue(true);
         views.getClient1().getOnCreateWindowHelper().setReturnValue(true);
-        runPerViewSettingsTest(
-                new AwSettingsJavaScriptPopupsTestHelper(views.getContainer0(), views.getClient0()),
+        runPerViewSettingsTest(new AwSettingsJavaScriptPopupsTestHelper(
+                                       views.getContainer0(), views.getClient0(), false),
                 new AwSettingsJavaScriptPopupsTestHelper(
-                        views.getContainer1(), views.getClient1()));
+                        views.getContainer1(), views.getClient1(), false));
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"AndroidWebView", "Preferences"})
+    public void testJavaScriptPopupsOpenTwice() throws Throwable {
+        final ViewPair views = createViews();
+        runPerViewSettingsTest(new AwSettingsJavaScriptPopupsTestHelper(
+                                       views.getContainer0(), views.getClient0(), true),
+                new AwSettingsJavaScriptPopupsTestHelper(
+                        views.getContainer1(), views.getClient1(), true));
     }
 
     @Test
