c0a9eb5fa69206d65db66210dd3614b65768c064
Yoshifumi Inoue <yosin@chromium.org>
2017-10-04 09:40:59 +0000

    Get rid of redundant include of "VisiblePosition.h" from "WebSurroundingText.cpp"
    This patch gets rid of redundant include of "VisiblePosition.h" from
    "WebSurroundingText.cpp" for improving code health.
    
    Bug: 657237
    Change-Id: I5bb543f9f36e8df6b0d973ac55b756aff5243776
    Reviewed-on: https://chromium-review.googlesource.com/699977
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506349}

--- third_party/WebKit/Source/core/exported/WebSurroundingText.cpp
+++ third_party/WebKit/Source/core/exported/WebSurroundingText.cpp
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/EphemeralRange.h"
 #include "core/editing/FrameSelection.h"
 #include "core/editing/SurroundingText.h"
-#include "core/editing/VisiblePosition.h"
 #include "core/editing/VisibleSelection.h"
 #include "core/frame/LocalFrame.h"
 #include "core/frame/WebLocalFrameImpl.h"
