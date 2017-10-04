6a2c58e70e7d93189dffbcca5b7b70b9cd469273
Yuwei Huang <yuweih@chromium.org>
2017-10-04 22:13:02 +0000

    [CRD iOS] Add a view for host fetch failure for non-user-triggered refresh
    Previously if a host list fetch is not triggered by the user (refresh
    control) and fails, then the app will simply pop up a toast complaining
    about unavailable network and show nothing else. Pull-to-refresh
    doesn't work.
    
    This CL fixes this by providing a dialog-like view to allow user to
    retry in that case.
    
    Screenshot:
    https://drive.google.com/file/d/0BytzIZKeM8nBMmN3b3A0bUhoMFU/view?usp=sharing
    
    Bug: 771532
    Change-Id: I64cb4015acc066322c3c6127b79b87d13cb06652
    Reviewed-on: https://chromium-review.googlesource.com/700316
    Reviewed-by: Scott Nichols <nicholss@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506537}

---
+++ remoting/ios/app/host_fetching_error_view_controller.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_IOS_APP_HOST_FETCHING_ERROR_VIEW_CONTROLLER_H_
+#define REMOTING_IOS_APP_HOST_FETCHING_ERROR_VIEW_CONTROLLER_H_
+
+#import <UIKit/UIKit.h>
+
+// This VC shows a dialog-like view to allow the user to retry host list
+// fetching. This is used when the host list has never been successfully,
+// fetched, i.e. pull-to-refresh is not available.
+@interface HostFetchingErrorViewController : UIViewController
+
+// Called when the retry button is tapped.
+@property(nonatomic) void (^onRetryCallback)();
+
+// Returns the label that shows the error message.
+@property(nonatomic, readonly) UILabel* label;
+
+@end
+
+#endif  // REMOTING_IOS_APP_HOST_FETCHING_ERROR_VIEW_CONTROLLER_H_
--- remoting/ios/facade/remoting_service.h
+++ remoting/ios/facade/remoting_service.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @protocol RemotingAuthentication;
 
 typedef NS_ENUM(NSInteger, HostListState) {
-  // Nobody has requested a host list fetch.
+  // Nobody has requested a host list fetch since login or last failure.
   HostListStateNotFetched,
 
   // The host list is currently being fetched.
@@ -25,6 +25,7 @@ typedef NS_ENUM(NSInteger, HostListState) {
 };
 
 typedef NS_ENUM(NSInteger, HostListFetchFailureReason) {
+  HostListFetchFailureReasonNoFailure,
   HostListFetchFailureReasonNetworkError,
   HostListFetchFailureReasonAuthError,
   HostListFetchFailureReasonUnknown,
@@ -64,6 +65,12 @@ extern NSString* const kUserInfo;
 // The Chromoting Client Runtime, this holds the threads and other shared
 // resources used by the Chromoting clients
 @property(nonatomic, readonly) remoting::ChromotingClientRuntime* runtime;
+
+// Returns the last failure reason when fetching the host list. Returns
+// HostListFetchFailureReasonNoFailure when the host list has never been fetched
+// or the last fetch has succeeded.
+@property(nonatomic, readonly)
+    HostListFetchFailureReason lastFetchFailureReason;
 
 // This must be set immediately after the authentication object is created. It
 // can only be set once.
