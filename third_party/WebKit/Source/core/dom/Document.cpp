d3576350015ddf111bbc26d7271d1652b7769132
Bhagirathi Satpathy <bhagirathi.s@samsung.com>
2017-10-04 05:06:37 +0000

    Remove unused header HTMLIFrameElement.h
    HTMLIFrameElement.h is included in Document.cpp but not used. Removed this unnecessary header.
    
    Bug: 
    Change-Id: I21b887a9052d367079600ad162e0cae0f710f867
    Reviewed-on: https://chromium-review.googlesource.com/697351
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: srirama chandra sekhar <srirama.m@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#506306}

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -149,7 +149,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/HTMLFrameOwnerElement.h"
 #include "core/html/HTMLHeadElement.h"
 #include "core/html/HTMLHtmlElement.h"
-#include "core/html/HTMLIFrameElement.h"
 #include "core/html/HTMLInputElement.h"
 #include "core/html/HTMLLinkElement.h"
 #include "core/html/HTMLMetaElement.h"
