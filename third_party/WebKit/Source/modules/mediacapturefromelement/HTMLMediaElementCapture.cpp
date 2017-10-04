f0168737bde0ad0ddafcbe940f6fd60205d36840
Guido Urdaneta <guidou@chromium.org>
2017-10-04 18:48:45 +0000

    Element capture: Do not check cross origin for elements with srcObject.
    Elements with srcObject contain MediaStreams and they cannot be
    cross origin since capture from cross-origin elements (with src)
    is disabled.
    The cross-origin check fails for mediaStreams since it uses the src,
    which is empty when the stream has been assigned to the element as an
    srcObject.
    
    Bug: 771388
    Change-Id: I060ce54e37ba34814ac6186b0e098edfcc3d752e
    Reviewed-on: https://chromium-review.googlesource.com/699434
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506465}

--- third_party/WebKit/Source/modules/mediacapturefromelement/HTMLMediaElementCapture.cpp
+++ third_party/WebKit/Source/modules/mediacapturefromelement/HTMLMediaElementCapture.cpp
@@ -105,7 +105,8 @@ void MediaElementEventListener::UpdateSources(ExecutionContext* context) {
   for (auto track : media_stream_->getTracks())
     sources_.insert(track->Component()->Source());
 
-  if (!media_element_->IsMediaDataCORSSameOrigin(
+  if (!media_element_->currentSrc().IsEmpty() &&
+      !media_element_->IsMediaDataCORSSameOrigin(
           context->GetSecurityOrigin())) {
     for (auto source : sources_)
       MediaStreamCenter::Instance().DidStopMediaStreamSource(source);
@@ -134,7 +135,7 @@ MediaStream* HTMLMediaElementCapture::captureStream(
     return nullptr;
   }
 
-  if (!(element.currentSrc().IsNull() && !element.GetSrcObject()) &&
+  if (!element.currentSrc().IsEmpty() &&
       !element.IsMediaDataCORSSameOrigin(
           element.GetExecutionContext()->GetSecurityOrigin())) {
     exception_state.ThrowSecurityError(
@@ -158,7 +159,7 @@ MediaStream* HTMLMediaElementCapture::captureStream(
   // If |element| is actually playing a MediaStream, just clone it.
   if (element.GetLoadType() == WebMediaPlayer::kLoadTypeMediaStream) {
     MediaStreamDescriptor* const descriptor =
-        element.currentSrc().IsNull()
+        element.currentSrc().IsEmpty()
             ? element.GetSrcObject()
             : MediaStreamRegistry::Registry().LookupMediaStreamDescriptor(
                   element.currentSrc().GetString());
