f036d8a43a7b3b66a1ae6813f17b49e8c09c6225
Justin DeWitt <dewittj@chromium.org>
2017-10-04 19:15:22 +0000

    [Offline Pages] Enable the prefetching UI by default, when prefetching is enabled.
    This enables the Downloads Home integration as well as the notification background
    task.  The UI can still be disabled manually in chrome://flags.
    
    Bug: 701939
    Change-Id: Ifd38830b4442465287bb47f6440afeb499e3e234
    Reviewed-on: https://chromium-review.googlesource.com/699578
    Reviewed-by: Dmitry Titov <dimich@chromium.org>
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506474}

--- components/offline_pages/core/offline_page_feature.cc
+++ components/offline_pages/core/offline_page_feature.cc
@@ -59,7 +59,7 @@ const base::Feature kOfflinePagesCTV2Feature{"OfflinePagesCTV2",
                                              base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kOfflinePagesPrefetchingUIFeature{
-    "OfflinePagesPrefetchingUI", base::FEATURE_DISABLED_BY_DEFAULT};
+    "OfflinePagesPrefetchingUI", base::FEATURE_ENABLED_BY_DEFAULT};
 
 bool IsOfflineBookmarksEnabled() {
   return base::FeatureList::IsEnabled(kOfflineBookmarksFeature);
@@ -95,7 +95,8 @@ bool IsPrefetchingOfflinePagesEnabled() {
 }
 
 bool IsOfflinePagesPrefetchingUIEnabled() {
-  return base::FeatureList::IsEnabled(kOfflinePagesPrefetchingUIFeature);
+  return IsPrefetchingOfflinePagesEnabled() &&
+         base::FeatureList::IsEnabled(kOfflinePagesPrefetchingUIFeature);
 }
 
 bool IsOfflinePagesLoadSignalCollectingEnabled() {
