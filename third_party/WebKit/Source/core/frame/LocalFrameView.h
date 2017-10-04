8c33f7861e42717e54273dfe91fd7d60bed705b8
pdr <pdr@chromium.org>
2017-10-04 23:24:45 +0000

    Remove unused ContentsProperties from ObjectPaintProperties
    longer implemented. ObjectPaintProperties also had quite a few
    unnecessary includes that have been removed.
    
    ObjectPaintProperties: :ContentsProperties was declared but is no
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I8fa46a22866855d3e381d5b2493436f65abf620c
    Reviewed-on: https://chromium-review.googlesource.com/695810
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506560}

--- third_party/WebKit/Source/core/frame/LocalFrameView.h
+++ third_party/WebKit/Source/core/frame/LocalFrameView.h
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/graphics/Color.h"
 #include "platform/graphics/CompositorElementId.h"
 #include "platform/graphics/GraphicsLayerClient.h"
+#include "platform/graphics/paint/PropertyTreeState.h"
 #include "platform/runtime_enabled_features.h"
 #include "platform/scroll/ScrollTypes.h"
 #include "platform/scroll/Scrollbar.h"
