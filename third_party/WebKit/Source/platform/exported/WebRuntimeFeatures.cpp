7a3050456fcbe715107f83ea379fa390a6c2ee5b
dhausknecht <dhausknecht@google.com>
2017-10-04 13:04:16 +0000

    feature OriginManifest added.
    The intent to implement was announced on
    https://groups.google.com/a/chromium.org/d/topic/blink-dev/mdkHs4jybG4/discussion
    
    Bug: 751996
    Change-Id: I6938ba02fdefa795d1d8a9e35fd94428f61f54d0
    Reviewed-on: https://chromium-review.googlesource.com/680218
    Commit-Queue: Daniel Hausknecht <dhausknecht@google.com>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506372}

--- third_party/WebKit/Source/platform/exported/WebRuntimeFeatures.cpp
+++ third_party/WebKit/Source/platform/exported/WebRuntimeFeatures.cpp
@@ -69,6 +69,10 @@ void WebRuntimeFeatures::EnableOriginTrialControlledFeatures(bool enable) {
   RuntimeEnabledFeatures::SetOriginTrialControlledFeaturesEnabled(enable);
 }
 
+void WebRuntimeFeatures::EnableOriginManifest(bool enable) {
+  RuntimeEnabledFeatures::SetOriginManifestEnabled(enable);
+}
+
 void WebRuntimeFeatures::EnableOutOfBlinkCORS(bool enable) {
   RuntimeEnabledFeatures::SetOutOfBlinkCORSEnabled(enable);
 }
