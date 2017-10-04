c6a73058e3b44583ee1016b07c4a9e5245c1436b
Fredrik Solenberg <solenberg@chromium.org>
2017-10-04 10:12:03 +0000

    Remove implementations of deprecated webrtc::AudioDevice APIs.
    Bug: webrtc:7306
    Change-Id: I46b3d6246b2a35869f32150339744676a5c7e9c0
    Reviewed-on: https://chromium-review.googlesource.com/700256
    Reviewed-by: Henrik Andreasson <henrika@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Fredrik Solenberg <solenberg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506354}

--- content/renderer/media/webrtc_audio_device_not_impl.cc
+++ content/renderer/media/webrtc_audio_device_not_impl.cc
@@ -7,24 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-WebRtcAudioDeviceNotImpl::WebRtcAudioDeviceNotImpl()
-    : last_process_time_(base::TimeTicks::Now()) {
-}
-
-int64_t WebRtcAudioDeviceNotImpl::TimeUntilNextProcess() {
-  const int64_t kMillisecondsBetweenProcessCalls = 5000;
-  base::TimeDelta delta_time = (base::TimeTicks::Now() - last_process_time_);
-  return kMillisecondsBetweenProcessCalls - delta_time.InMilliseconds();
-}
-
-void WebRtcAudioDeviceNotImpl::Process() {
-  last_process_time_ = base::TimeTicks::Now();
-}
-
-int32_t WebRtcAudioDeviceNotImpl::RegisterEventObserver(
-    webrtc::AudioDeviceObserver* event_callback) {
-  return 0;
-}
+WebRtcAudioDeviceNotImpl::WebRtcAudioDeviceNotImpl() = default;
 
 int32_t WebRtcAudioDeviceNotImpl::ActiveAudioLayer(
     AudioLayer* audio_layer) const {
