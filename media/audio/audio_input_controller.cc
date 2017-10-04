b07749f1afb4a032c1649b627cf24a691a6f64f0
maxmorin <maxmorin@chromium.org>
2017-10-04 08:46:23 +0000

    Move microphone debug recording to AM.
    Introduces the AudioInputStreamDataInterceptor, which transparently sends audio
    data off for debug recording. AudioManagerBase wraps each created input stream
    in an interceptor to enable debug recording. A side effect of this refactoring
    is that WebContents input streams will not be recorded, but AFAIK noone ever
    used recordings anyways. I also somehow had to fix a bunch of IWYU, not sure
    how I got dragged into that :).
    
    Since AudioInputRendererHost doesn't have to be accessed by the
    RenderProcessHostImpl anymore, the member variable was removed; the lifetime of
    the AudioInputRendererHost is now managed by the IPC::Channel as it is for other
    message filters.
    
    Bug: 768610
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I24a89435b283cd5df68373eb37d776a320d6ffd5
    Reviewed-on: https://chromium-review.googlesource.com/680217
    Commit-Queue: Max Morin <maxmorin@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Tommi <tommi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Henrik Grunell <grunell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506338}

--- media/audio/audio_input_controller.cc
+++ media/audio/audio_input_controller.cc
@@ -21,6 +21,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "base/trace_event/trace_event.h"
+#include "media/audio/audio_io.h"
+#include "media/audio/audio_manager.h"
+#include "media/base/audio_bus.h"
 #include "media/base/user_input_monitor.h"
 
 namespace media {
@@ -124,10 +127,6 @@ class AudioInputController::AudioCallback
     received_callback_ = true;
 
     DeliverDataToSyncWriter(source, capture_time, volume);
-
-#if BUILDFLAG(ENABLE_WEBRTC)
-    controller_->debug_recording_helper_.OnData(source);
-#endif
   }
 
   void OnError() override {
@@ -187,9 +186,6 @@ AudioInputController::AudioInputController(
       sync_writer_(sync_writer),
       type_(type),
       user_input_monitor_(user_input_monitor),
-#if BUILDFLAG(ENABLE_WEBRTC)
-      debug_recording_helper_(params, task_runner_, base::OnceClosure()),
-#endif
       weak_ptr_factory_(this) {
   DCHECK(creator_task_runner_.get());
   DCHECK(handler_);
@@ -248,8 +244,7 @@ scoped_refptr<AudioInputController> AudioInputController::CreateForStream(
     EventHandler* event_handler,
     AudioInputStream* stream,
     SyncWriter* sync_writer,
-    UserInputMonitor* user_input_monitor,
-    const AudioParameters& params) {
+    UserInputMonitor* user_input_monitor) {
   DCHECK(sync_writer);
   DCHECK(stream);
   DCHECK(event_handler);
@@ -261,11 +256,12 @@ scoped_refptr<AudioInputController> AudioInputController::CreateForStream(
                             user_input_monitor, VIRTUAL);
   }
 
-  // Create the AudioInputController object and ensure that it runs on
-  // the audio-manager thread.
-  scoped_refptr<AudioInputController> controller(
-      new AudioInputController(task_runner, event_handler, sync_writer,
-                               user_input_monitor, params, VIRTUAL));
+  // Create the AudioInputController object and ensure that it runs on the
+  // audio-manager thread. Note that the AudioParameters are irrelevant for this
+  // use case.
+  scoped_refptr<AudioInputController> controller(new AudioInputController(
+      task_runner, event_handler, sync_writer, user_input_monitor,
+      AudioParameters::UnavailableDeviceParams(), VIRTUAL));
 
   if (!controller->task_runner_->PostTask(
           FROM_HERE,
@@ -455,10 +451,6 @@ void AudioInputController::DoClose() {
     LogSilenceState(silence_state_);
 #endif
 
-#if BUILDFLAG(ENABLE_WEBRTC)
-  debug_recording_helper_.DisableDebugRecording();
-#endif
-
   max_volume_ = 0.0;
   weak_ptr_factory_.InvalidateWeakPtrs();
 }
@@ -526,25 +518,6 @@ void AudioInputController::DoLogAudioLevels(float level_dbfs,
   if (microphone_volume_percent < kLowLevelMicrophoneLevelPercent)
     log_string += " <=> low microphone level!";
   handler_->OnLog(this, log_string);
-#endif
-}
-
-void AudioInputController::EnableDebugRecording(
-    const base::FilePath& file_name) {
-#if BUILDFLAG(ENABLE_WEBRTC)
-  DCHECK(creator_task_runner_->BelongsToCurrentThread());
-  task_runner_->PostTask(
-      FROM_HERE, base::BindOnce(&AudioInputController::DoEnableDebugRecording,
-                                this, file_name));
-#endif
-}
-
-void AudioInputController::DisableDebugRecording() {
-#if BUILDFLAG(ENABLE_WEBRTC)
-  DCHECK(creator_task_runner_->BelongsToCurrentThread());
-  task_runner_->PostTask(
-      FROM_HERE,
-      base::BindOnce(&AudioInputController::DoDisableDebugRecording, this));
 #endif
 }
 
@@ -617,19 +590,6 @@ void AudioInputController::LogCallbackError() {
       break;
   }
 }
-
-#if BUILDFLAG(ENABLE_WEBRTC)
-void AudioInputController::DoEnableDebugRecording(
-    const base::FilePath& file_name) {
-  DCHECK(task_runner_->BelongsToCurrentThread());
-  debug_recording_helper_.EnableDebugRecording(file_name);
-}
-
-void AudioInputController::DoDisableDebugRecording() {
-  DCHECK(task_runner_->BelongsToCurrentThread());
-  debug_recording_helper_.DisableDebugRecording();
-}
-#endif  // BUILDFLAG(ENABLE_WEBRTC)
 
 void AudioInputController::LogMessage(const std::string& message) {
   DCHECK(task_runner_->BelongsToCurrentThread());
--- media/audio/test_audio_input_controller_factory.cc
+++ media/audio/test_audio_input_controller_factory.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "media/audio/audio_io.h"
+#include "media/audio/audio_manager.h"
 
 namespace media {
 
