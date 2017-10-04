8663e4fb278dead0286e1a04f221cb205720ecd4
Lukasz Anforowicz <lukasza@chromium.org>
2017-10-04 16:20:25 +0000

    Removing callers of the WebContents::GetRenderProcessHost() method.
    WebContents::GetRenderProcessHost is an API that doesn't behave as
    expected with out-of-process iframes, as multiple processes can be
    associated with a single WebContents.  This CL replaces callers of this
    API, so that they go through an equivalent API that explicitly selects
    which frame's process is needed.
    
    This CL was started by using an ad-hoc clang-plugin that replaced
    existing callsites of WebContents::GetRenderProcessHost() with a call to
    something like wc->GetMainFrame()->GetProcess().  This was followed-up
    by manually adding an include of render_frame_host.h and git cl format
    and git cl lint and then a self-review with small tweaks (e.g. sometimes
    using a wc->GetRenderViewHost()->GetProcess() is more appropriate).
    
    This CL was uploaded by git cl split.
    
    R=sky@chromium.org
    
    Bug: 666525
    Change-Id: I8167f7c6577f457939f2d2379640523f583c9dff
    Reviewed-on: https://chromium-review.googlesource.com/689109
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506405}

--- chrome/browser/ui/views/hung_renderer_view.cc
+++ chrome/browser/ui/views/hung_renderer_view.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/hung_renderer_view.h"
 
+#include <utility>
+#include <vector>
+
 #include "base/i18n/rtl.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/string_number_conversions.h"
@@ -26,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/constrained_window/constrained_window_views.h"
 #include "components/favicon/content/content_favicon_driver.h"
 #include "components/web_modal/web_contents_modal_dialog_host.h"
+#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/render_widget_host.h"
@@ -70,8 +74,9 @@ HungPagesTableModel::~HungPagesTableModel() {
 }
 
 content::RenderProcessHost* HungPagesTableModel::GetRenderProcessHost() {
-  return tab_observers_.empty() ? NULL :
-      tab_observers_[0]->web_contents()->GetRenderProcessHost();
+  return tab_observers_.empty()
+             ? NULL
+             : tab_observers_[0]->web_contents()->GetMainFrame()->GetProcess();
 }
 
 content::RenderViewHost* HungPagesTableModel::GetRenderViewHost() {
@@ -89,7 +94,8 @@ void HungPagesTableModel::InitForWebContents(WebContents* hung_contents) {
     }
     for (TabContentsIterator it; !it.done(); it.Next()) {
       if (*it != hung_contents &&
-          it->GetRenderProcessHost() == hung_contents->GetRenderProcessHost())
+          it->GetMainFrame()->GetProcess() ==
+              hung_contents->GetMainFrame()->GetProcess())
         tab_observers_.push_back(
             base::MakeUnique<WebContentsObserverImpl>(this, *it));
     }
@@ -296,7 +302,7 @@ void HungRendererDialogView::EndForWebContents(WebContents* contents) {
   DCHECK(contents);
   if (hung_pages_table_model_->RowCount() == 0 ||
       hung_pages_table_model_->GetRenderProcessHost() ==
-      contents->GetRenderProcessHost()) {
+          contents->GetMainFrame()->GetProcess()) {
     GetWidget()->Close();
     // Close is async, make sure we drop our references to the tab immediately
     // (it may be going away).
