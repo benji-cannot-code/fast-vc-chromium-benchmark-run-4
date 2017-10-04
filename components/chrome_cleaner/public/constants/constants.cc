efb6094ead849e00d0af328f6f342be7e53e0095
Fabio Tirelo <ftirelo@chromium.org>
2017-10-04 21:38:57 +0000

    Chrome Cleaner: collect srt field trial group name
    Bug: 
    Change-Id: Ib77d051ef0813cd63eafd92b578919f46982d26b
    Reviewed-on: https://chromium-review.googlesource.com/700992
    Reviewed-by: Chris Sharp <csharp@chromium.org>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506527}

--- components/chrome_cleaner/public/constants/constants.cc
+++ components/chrome_cleaner/public/constants/constants.cc
@@ -22,6 +22,7 @@ const char kExtendedSafeBrowsingEnabledSwitch[] =
     "extended-safebrowsing-enabled";
 const char kRegistrySuffixSwitch[] = "registry-suffix";
 const char kSessionIdSwitch[] = "session-id";
+const char kSRTPromptFieldTrialGroupNameSwitch[] = "srt-field-trial-group-name";
 const char kUmaUserSwitch[] = "uma-user";
 
 // Registry paths and subkeys.
