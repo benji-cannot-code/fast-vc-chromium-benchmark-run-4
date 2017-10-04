062663515cb146ebfbb3e06ae4c84349e31b73fa
Xiaocheng Hu <xiaochengh@chromium.org>
2017-10-04 03:54:09 +0000

    Make LayoutText::IsRenderedCharacter absorbed by EditCommand::IsRenderedCharacter
    LayoutText::IsRenderedCharacter is only used by editing commands. Hence,
    this patch makes it absorbed by its only call site in EditCommand, as
    a preparation step to stop editing from depending on the legacy line
    layout structure.
    
    This patch also removes some TODOs related to this function. These TODOs
    originates from an ancient CL [1], but do not make a lot of sense.
    
    [1] crrev.com/289fa98c43250e4e9fc44aad39670122e4046bf6
    
    Bug: 771398, 731540
    Change-Id: I38b85f5a4c8a8adfba0568632337f1e42cfeded5
    Reviewed-on: https://chromium-review.googlesource.com/699011
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506297}

--- third_party/WebKit/Source/core/editing/commands/EditCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/EditCommand.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/commands/CompositeEditCommand.h"
 #include "core/frame/LocalFrame.h"
 #include "core/layout/LayoutText.h"
+#include "core/layout/line/InlineTextBox.h"
 
 namespace blink {
 
@@ -59,11 +60,28 @@ bool EditCommand::IsRenderedCharacter(const Position& position) {
     return false;
 
   LayoutObject* layout_object = position.AnchorNode()->GetLayoutObject();
-  if (!layout_object)
+  if (!layout_object || !layout_object->IsText())
     return false;
 
-  return ToLayoutText(layout_object)
-      ->IsRenderedCharacter(position.OffsetInContainerNode());
+  // TODO(xiaochengh): When LayoutNG is enabled, we should check offset mapping
+  // instead of legacy InlineTextBoxes.
+
+  // TODO(editing-dev): This doesn't handle first-letter correctly. Fix it.
+  const LayoutText* layout_text = ToLayoutText(layout_object);
+  const int offset_in_node = position.OffsetInContainerNode();
+  for (InlineTextBox* box : InlineTextBoxesOf(*layout_text)) {
+    if (offset_in_node < static_cast<int>(box->Start()) &&
+        !layout_text->ContainsReversedText()) {
+      // The offset we're looking for is before this node this means the offset
+      // must be in content that is not laid out. Return false.
+      return false;
+    }
+    if (offset_in_node >= static_cast<int>(box->Start()) &&
+        offset_in_node < static_cast<int>(box->Start() + box->Len()))
+      return true;
+  }
+
+  return false;
 }
 
 void EditCommand::SetParent(CompositeEditCommand* parent) {
