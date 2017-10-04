0b0aa10a84edef6bb98ac98270054002146b319b
Marc Treib <treib@chromium.org>
2017-10-04 18:19:14 +0000

    Instant IPC: Add return values to ChromeIdentityCheck and HistorySyncCheck
    instead of separate *Result functions. This is made possible by Mojo,
    and resolves a TODO.
    
    Bug: none
    Change-Id: I5c1cda056b18beb0d9a2354078c05dffe5823c02
    Reviewed-on: https://chromium-review.googlesource.com/695227
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506447}

--- chrome/browser/ui/search/search_ipc_router_unittest.cc
+++ chrome/browser/ui/search/search_ipc_router_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/field_trial.h"
 #include "base/strings/string16.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/mock_callback.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search/search.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
@@ -70,8 +71,8 @@ class MockSearchIPCRouterDelegate : public SearchIPCRouter::Delegate {
                     ntp_tiles::TileSource tile_source,
                     ntp_tiles::TileVisualType tile_type));
   MOCK_METHOD1(PasteIntoOmnibox, void(const base::string16&));
-  MOCK_METHOD1(OnChromeIdentityCheck, void(const base::string16& identity));
-  MOCK_METHOD0(OnHistorySyncCheck, void());
+  MOCK_METHOD1(ChromeIdentityCheck, bool(const base::string16& identity));
+  MOCK_METHOD0(HistorySyncCheck, bool());
 };
 
 class MockSearchIPCRouterPolicy : public SearchIPCRouter::Policy {
@@ -86,12 +87,10 @@ class MockSearchIPCRouterPolicy : public SearchIPCRouter::Policy {
   MOCK_METHOD1(ShouldProcessPasteIntoOmnibox, bool(bool));
   MOCK_METHOD0(ShouldProcessChromeIdentityCheck, bool());
   MOCK_METHOD0(ShouldProcessHistorySyncCheck, bool());
-  MOCK_METHOD0(ShouldSendSetSuggestionToPrefetch, bool());
   MOCK_METHOD1(ShouldSendSetInputInProgress, bool(bool));
   MOCK_METHOD0(ShouldSendOmniboxFocusChanged, bool());
   MOCK_METHOD0(ShouldSendMostVisitedItems, bool());
   MOCK_METHOD0(ShouldSendThemeBackgroundInfo, bool());
-  MOCK_METHOD0(ShouldSubmitQuery, bool());
 };
 
 class MockEmbeddedSearchClientFactory
@@ -312,13 +311,17 @@ TEST_F(SearchIPCRouterTest, ProcessChromeIdentityCheckMsg) {
   SetupMockDelegateAndPolicy();
   MockSearchIPCRouterPolicy* policy = GetSearchIPCRouterPolicy();
   const base::string16 test_identity = base::ASCIIToUTF16("foo@bar.com");
-  EXPECT_CALL(*mock_delegate(), OnChromeIdentityCheck(test_identity)).Times(1);
+  EXPECT_CALL(*mock_delegate(), ChromeIdentityCheck(test_identity))
+      .Times(1)
+      .WillOnce(Return(true));
   EXPECT_CALL(*policy, ShouldProcessChromeIdentityCheck())
       .Times(1)
       .WillOnce(Return(true));
 
+  base::MockCallback<SearchIPCRouter::ChromeIdentityCheckCallback> callback;
+  EXPECT_CALL(callback, Run(true));
   GetSearchIPCRouter().ChromeIdentityCheck(GetSearchIPCRouterSeqNo(),
-                                           test_identity);
+                                           test_identity, callback.Get());
 }
 
 TEST_F(SearchIPCRouterTest, IgnoreChromeIdentityCheckMsg) {
@@ -327,25 +330,31 @@ TEST_F(SearchIPCRouterTest, IgnoreChromeIdentityCheckMsg) {
   MockSearchIPCRouterPolicy* policy = GetSearchIPCRouterPolicy();
 
   const base::string16 test_identity = base::ASCIIToUTF16("foo@bar.com");
-  EXPECT_CALL(*mock_delegate(), OnChromeIdentityCheck(test_identity)).Times(0);
+  EXPECT_CALL(*mock_delegate(), ChromeIdentityCheck(test_identity)).Times(0);
   EXPECT_CALL(*policy, ShouldProcessChromeIdentityCheck())
       .Times(1)
       .WillOnce(Return(false));
 
+  base::MockCallback<SearchIPCRouter::ChromeIdentityCheckCallback> callback;
   GetSearchIPCRouter().ChromeIdentityCheck(GetSearchIPCRouterSeqNo(),
-                                           test_identity);
+                                           test_identity, callback.Get());
 }
 
 TEST_F(SearchIPCRouterTest, ProcessHistorySyncCheckMsg) {
   NavigateAndCommitActiveTab(GURL(chrome::kChromeSearchLocalNtpUrl));
   SetupMockDelegateAndPolicy();
   MockSearchIPCRouterPolicy* policy = GetSearchIPCRouterPolicy();
-  EXPECT_CALL(*mock_delegate(), OnHistorySyncCheck()).Times(1);
+  EXPECT_CALL(*mock_delegate(), HistorySyncCheck())
+      .Times(1)
+      .WillOnce(Return(true));
   EXPECT_CALL(*policy, ShouldProcessHistorySyncCheck())
       .Times(1)
       .WillOnce(Return(true));
 
-  GetSearchIPCRouter().HistorySyncCheck(GetSearchIPCRouterSeqNo());
+  base::MockCallback<SearchIPCRouter::ChromeIdentityCheckCallback> callback;
+  EXPECT_CALL(callback, Run(true));
+  GetSearchIPCRouter().HistorySyncCheck(GetSearchIPCRouterSeqNo(),
+                                        callback.Get());
 }
 
 TEST_F(SearchIPCRouterTest, IgnoreHistorySyncCheckMsg) {
@@ -353,12 +362,14 @@ TEST_F(SearchIPCRouterTest, IgnoreHistorySyncCheckMsg) {
   SetupMockDelegateAndPolicy();
   MockSearchIPCRouterPolicy* policy = GetSearchIPCRouterPolicy();
 
-  EXPECT_CALL(*mock_delegate(), OnHistorySyncCheck()).Times(0);
+  EXPECT_CALL(*mock_delegate(), HistorySyncCheck()).Times(0);
   EXPECT_CALL(*policy, ShouldProcessHistorySyncCheck())
       .Times(1)
       .WillOnce(Return(false));
 
-  GetSearchIPCRouter().HistorySyncCheck(GetSearchIPCRouterSeqNo());
+  base::MockCallback<SearchIPCRouter::ChromeIdentityCheckCallback> callback;
+  GetSearchIPCRouter().HistorySyncCheck(GetSearchIPCRouterSeqNo(),
+                                        callback.Get());
 }
 
 TEST_F(SearchIPCRouterTest, ProcessDeleteMostVisitedItemMsg) {
--- chrome/renderer/searchbox/searchbox.cc
+++ chrome/renderer/searchbox/searchbox.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/bind.h"
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
@@ -221,7 +222,8 @@ SearchBox::SearchBox(content::RenderFrame* render_frame)
       is_input_in_progress_(false),
       is_key_capture_enabled_(false),
       most_visited_items_cache_(kMaxInstantMostVisitedItemCacheSize),
-      binding_(this) {
+      binding_(this),
+      weak_ptr_factory_(this) {
   // Connect to the embedded search interface in the browser.
   chrome::mojom::EmbeddedSearchConnectorAssociatedPtr connector;
   render_frame->GetRemoteAssociatedInterfaces()->GetInterface(&connector);
@@ -263,11 +265,16 @@ void SearchBox::LogMostVisitedNavigation(
 }
 
 void SearchBox::CheckIsUserSignedInToChromeAs(const base::string16& identity) {
-  embedded_search_service_->ChromeIdentityCheck(page_seq_no_, identity);
+  embedded_search_service_->ChromeIdentityCheck(
+      page_seq_no_, identity,
+      base::BindOnce(&SearchBox::ChromeIdentityCheckResult,
+                     weak_ptr_factory_.GetWeakPtr(), identity));
 }
 
 void SearchBox::CheckIsUserSyncingHistory() {
-  embedded_search_service_->HistorySyncCheck(page_seq_no_);
+  embedded_search_service_->HistorySyncCheck(
+      page_seq_no_, base::BindOnce(&SearchBox::HistorySyncCheckResult,
+                                   weak_ptr_factory_.GetWeakPtr()));
 }
 
 void SearchBox::DeleteMostVisitedItem(
@@ -414,12 +421,6 @@ GURL SearchBox::GetURLForMostVisitedItem(InstantRestrictedID item_id) const {
 void SearchBox::Bind(
     chrome::mojom::EmbeddedSearchClientAssociatedRequest request) {
   binding_.Bind(std::move(request));
-}
-
-void SearchBox::Reset() {
-  is_focused_ = false;
-  is_key_capture_enabled_ = false;
-  theme_info_ = ThemeBackgroundInfo();
 }
 
 void SearchBox::OnDestruct() {
