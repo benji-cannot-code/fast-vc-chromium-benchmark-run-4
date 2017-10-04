35a4f9072375e0c9f5fa9729cc9a99aff3617ccc
Yoland Yan <yolandyan@chromium.org>
2017-10-04 21:16:16 +0000

    Fix 4 chrome instru test class runner
    Command line flags are initiated by instrumentation tests' class
    runner. One should avoid calling CommandLine.init(null) manually
    in tests after native command line starts.
    
    Bug: 771205
    Change-Id: I1e6d026a45c60eb81ce0860971fa2da44ab0285b
    Reviewed-on: https://chromium-review.googlesource.com/698351
    Commit-Queue: Yoland Yan <yolandyan@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506517}

--- chrome/android/javatests/src/org/chromium/chrome/browser/preferences/website/WebsiteAddressTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/preferences/website/WebsiteAddressTest.java
@@ -12,15 +12,14 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import org.chromium.base.CommandLine;
-import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.test.ChromeBrowserTestRule;
+import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 
 /**
  * Tests for WebsiteAddress.
  */
-@RunWith(BaseJUnit4ClassRunner.class)
+@RunWith(ChromeJUnit4ClassRunner.class)
 public class WebsiteAddressTest {
     @Rule
     public final ChromeBrowserTestRule mBrowserTestRule = new ChromeBrowserTestRule();
@@ -62,8 +61,6 @@ public class WebsiteAddressTest {
     @SmallTest
     @Feature({"Preferences"})
     public void testEqualsHashCodeCompareTo() {
-        CommandLine.init(null);
-
         Object[][] testData = {
             { 0, "http://google.com", "http://google.com" },
             { -1, "[*.]google.com", "http://google.com" },
