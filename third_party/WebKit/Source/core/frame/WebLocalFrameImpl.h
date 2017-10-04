f87f3123a9b684e2d3bac1c99e2e4862adc170f5
Yoshifumi Inoue <yosin@chromium.org>
2017-10-04 10:42:56 +0000

    Get rid of unused function WebLocalFrame::SelectWordAroundPosition()
    This patch gets rid of unused function |SelectWordAroundPosition()| from
    |WebLocalFrame| class for improving code health.
    
    Bug: 657237
    Change-Id: I0f49f98fc786ea85e286208dd4942ca6ab7b8882
    Reviewed-on: https://chromium-review.googlesource.com/700354
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506359}

--- third_party/WebKit/Source/core/frame/WebLocalFrameImpl.h
+++ third_party/WebKit/Source/core/frame/WebLocalFrameImpl.h
@@ -400,8 +400,6 @@ class CORE_EXPORT WebLocalFrameImpl final
 
   void SetInputEventsScaleForEmulation(float);
 
-  static void SelectWordAroundPosition(LocalFrame*, VisiblePosition);
-
   TextCheckerClient& GetTextCheckerClient() const;
   WebTextCheckClient* TextCheckClient() const { return text_check_client_; }
 
