b3e14e60f8cdfd6beabd90d56bb12e369e57771b
Raymes Khoury <raymes@chromium.org>
2017-10-05 05:56:57 +0000

    Rename WebFeaturePolicyFeature::kEme to WebFeaturePolicyFeature::kEncryptedMedia
    EncryptedMedia is more consistent with the rest of the code.
    
    Bug: 689802
    Change-Id: I990212b8e3972ee1c2581ed1237d15321628fab5
    Reviewed-on: https://chromium-review.googlesource.com/596617
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Commit-Queue: Raymes Khoury <raymes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506662}

--- chrome/browser/media/protected_media_identifier_permission_context.cc
+++ chrome/browser/media/protected_media_identifier_permission_context.cc
@@ -43,7 +43,7 @@ ProtectedMediaIdentifierPermissionContext::
     ProtectedMediaIdentifierPermissionContext(Profile* profile)
     : PermissionContextBase(profile,
                             CONTENT_SETTINGS_TYPE_PROTECTED_MEDIA_IDENTIFIER,
-                            blink::WebFeaturePolicyFeature::kEme)
+                            blink::WebFeaturePolicyFeature::kEncryptedMedia)
 #if defined(OS_CHROMEOS)
       ,
       weak_factory_(this)
