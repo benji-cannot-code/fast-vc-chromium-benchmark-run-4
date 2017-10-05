458a5613aa769e6ef795e0145efd937ea52694a5
Hiroki Nakagawa <nhiroki@chromium.org>
2017-10-05 05:22:30 +0000

    Revert "[vr] Add a flag for experimental VR features"
    This reverts commit c536e658005c4fbacdb9de6d7f83a293e117f994.
    
    Reason for revert:
    This is failing the Google Chrome Win bot:
    https://build.chromium.org/p/chromium.chrome/builders/Google%20Chrome%20Win/builds/22616
    
    Error log:
    ../..\chrome/common/chrome_features.h(14): fatal error C1083: Cannot open include file: 'device/vr/features/features.h': No such file or directory
    
    Original change's description:
    > [vr] Add a flag for experimental VR features
    > 
    > Having this flag will allow VR feature work without putting the shipping
    > code at risk. Having it available via about:flags makes for much easier
    > use on Android.
    > 
    > Bug: None
    > Change-Id: I12cf3edaad80f9129fe4b00fcbf630f71b5e0b53
    > Reviewed-on: https://chromium-review.googlesource.com/700477
    > Commit-Queue: Ian Vollick <vollick@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#506646}
    
    TBR=vollick@chromium.org,sky@chromium.org
    
    Change-Id: I82a0a817772a5b5e26ad9a94d3e5431165efdab4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: None
    Reviewed-on: https://chromium-review.googlesource.com/701956
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506658}

--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "build/build_config.h"
 #include "chrome/common/features.h"
-#include "device/vr/features/features.h"  // nogncheck
 #include "extensions/features/features.h"
 #include "ppapi/features/features.h"
 #include "printing/features/features.h"
@@ -103,10 +102,6 @@ extern const base::Feature kExpectCTReporting;
 
 extern const base::Feature kExperimentalAppBanners;
 extern const base::Feature kExperimentalKeyboardLockUI;
-
-#if BUILDFLAG(ENABLE_VR)
-extern const base::Feature kExperimentalVRFeatures;
-#endif
 
 #if defined(OS_MACOSX)
 extern const base::Feature kFullscreenToolbarReveal;
