cb00fe6a1c50739d8a896444083206fd1b384009
Xiaocheng Hu <xiaochengh@chromium.org>
2017-10-05 02:17:09 +0000

    Move InlineBoxPosition related code to InlineBoxPosition.h/cpp
    Bug: 771398
    Change-Id: Ic35d94a75a5bb9d4ef00fc9abdb839c0e26c2c78
    Reviewed-on: https://chromium-review.googlesource.com/701414
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506626}

--- third_party/WebKit/Source/core/editing/VisibleUnitsLine.cpp
+++ third_party/WebKit/Source/core/editing/VisibleUnitsLine.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/dom/AXObjectCache.h"
 #include "core/editing/EditingUtilities.h"
+#include "core/editing/InlineBoxPosition.h"
 #include "core/editing/RenderedPosition.h"
 #include "core/editing/VisiblePosition.h"
 #include "core/layout/api/LineLayoutBlockFlow.h"
--- third_party/WebKit/Source/core/editing/VisibleUnitsTest.cpp
+++ third_party/WebKit/Source/core/editing/VisibleUnitsTest.cpp
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/editing/VisibleUnits.h"
 
-#include <ostream>  // NOLINT
 #include "bindings/core/v8/V8BindingForTesting.h"
 #include "core/dom/Text.h"
 #include "core/editing/PositionWithAffinity.h"
@@ -13,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/testing/EditingTestBase.h"
 #include "core/html/TextControlElement.h"
 #include "core/layout/LayoutTextFragment.h"
-#include "core/layout/line/InlineTextBox.h"
 
 namespace blink {
 
@@ -50,15 +48,6 @@ VisiblePositionInFlatTree CreateVisiblePositionInFlatTree(
 }
 
 }  // namespace
-
-std::ostream& operator<<(std::ostream& ostream,
-                         const InlineBoxPosition& inline_box_position) {
-  if (!inline_box_position.inline_box)
-    return ostream << "null";
-  return ostream
-         << inline_box_position.inline_box->GetLineLayoutItem().GetNode() << "@"
-         << inline_box_position.offset_in_box;
-}
 
 class VisibleUnitsTest : public EditingTestBase {};
 
@@ -207,33 +196,6 @@ TEST_F(VisibleUnitsTest, characterBefore) {
 
   EXPECT_EQ('4', CharacterBefore(CreateVisiblePositionInDOMTree(*five, 0)));
   EXPECT_EQ('1', CharacterBefore(CreateVisiblePositionInFlatTree(*five, 0)));
-}
-
-TEST_F(VisibleUnitsTest, computeInlineBoxPositionBidiIsolate) {
-  // "|" is bidi-level 0, and "foo" and "bar" are bidi-level 2
-  SetBodyContent(
-      "|<span id=sample style='unicode-bidi: isolate;'>foo<br>bar</span>|");
-
-  Element* sample = GetDocument().getElementById("sample");
-  Node* text = sample->firstChild();
-
-  const InlineBoxPosition& actual =
-      ComputeInlineBoxPosition(Position(text, 0), TextAffinity::kDownstream);
-  EXPECT_EQ(ToLayoutText(text->GetLayoutObject())->FirstTextBox(),
-            actual.inline_box);
-}
-
-// http://crbug.com/716093
-TEST_F(VisibleUnitsTest, ComputeInlineBoxPositionMixedEditable) {
-  SetBodyContent(
-      "<div contenteditable id=sample>abc<input contenteditable=false></div>");
-  Element* const sample = GetDocument().getElementById("sample");
-
-  const InlineBoxPosition& actual = ComputeInlineBoxPosition(
-      Position::LastPositionInNode(*sample), TextAffinity::kDownstream);
-  // Should not be in infinite-loop
-  EXPECT_EQ(nullptr, actual.inline_box);
-  EXPECT_EQ(0, actual.offset_in_box);
 }
 
 TEST_F(VisibleUnitsTest, endOfDocument) {
