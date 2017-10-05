25f108c283369643a69759ed3477c92573a351dc
Istiaque Ahmed <lazyboy@chromium.org>
2017-10-05 01:23:26 +0000

    Extension SW events: correctly load unfiltered worker listeners.
    EventRouter didn't used to load pref-saved listeners for Service
    Workers. Load them. This CL is a precursor to dispatching a SW event
    after a browser restart.
    
    One caveat (TODO) in this CL is that it is currently assumed that
    SW events are only registered from '/' scope. That support will be
    incrementally added.
    
    Also, add a test to demonstrate that an event can be dispatched after
    a worker is shut down along with its extension (event page). This test
    can easily be expanded in future to test the aforementioned browser
    restart scenario.
    
    Bug: 721147
    Change-Id: I80f0ae097772500f41c7509435b878762d55ff98
    Reviewed-on: https://chromium-review.googlesource.com/696720
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506607}

---
+++ chrome/test/data/extensions/api_test/service_worker/events_to_stopped_extension/page.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var readyPromise = new Promise(function(resolve, reject) {
+  navigator.serviceWorker.register('sw.js').then(function() {
+    return navigator.serviceWorker.ready;
+  }).then(function(registration) {
+    resolve('ready');
+  }).catch(function(err) {
+    reject(err);
+  });
+});
+
+window.runServiceWorkerAsync = function() {
+  chrome.test.log('runServiceWorkerAsync');
+  readyPromise.then(function(message) {
+    chrome.test.sendMessage('listener-added');
+  }).catch(function(err) {
+    chrome.test.sendMessage('FAILURE');
+  });
+};
