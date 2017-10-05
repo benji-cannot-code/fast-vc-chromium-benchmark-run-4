a59beffb1735603d4022aa5ecb0306261e19f360
Shimi Zhang <ctzsm@chromium.org>
2017-10-05 05:15:02 +0000

    Reorder & add test for SetSelectionOptions
    Adding tests so we could test |should_shrink_next_tap_|, which is
    going to be added in http://crrev.com/c/653432.
    
    Also did a line reorder in this CL.
    
    Bug: 703884
    Change-Id: If0d90219de85fbcfa64004abf6b8e420ad17d968
    Reviewed-on: https://chromium-review.googlesource.com/701281
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506656}

--- third_party/WebKit/Source/core/editing/SetSelectionOptions.cpp
+++ third_party/WebKit/Source/core/editing/SetSelectionOptions.cpp
@@ -27,15 +27,15 @@ SetSelectionOptions::Builder::SetCursorAlignOnScroll(
   return *this;
 }
 
-SetSelectionOptions::Builder& SetSelectionOptions::Builder::SetDoNotSetFocus(
-    bool new_value) {
-  data_.do_not_set_focus_ = new_value;
-  return *this;
-}
-
 SetSelectionOptions::Builder&
 SetSelectionOptions::Builder::SetDoNotClearStrategy(bool new_value) {
   data_.do_not_clear_strategy_ = new_value;
+  return *this;
+}
+
+SetSelectionOptions::Builder& SetSelectionOptions::Builder::SetDoNotSetFocus(
+    bool new_value) {
+  data_.do_not_set_focus_ = new_value;
   return *this;
 }
 
@@ -45,9 +45,9 @@ SetSelectionOptions::Builder& SetSelectionOptions::Builder::SetGranularity(
   return *this;
 }
 
-SetSelectionOptions::Builder&
-SetSelectionOptions::Builder::SetShouldCloseTyping(bool new_value) {
-  data_.should_close_typing_ = new_value;
+SetSelectionOptions::Builder& SetSelectionOptions::Builder::SetSetSelectionBy(
+    SetSelectionBy new_value) {
+  data_.set_selection_by_ = new_value;
   return *this;
 }
 
@@ -57,15 +57,15 @@ SetSelectionOptions::Builder::SetShouldClearTypingStyle(bool new_value) {
   return *this;
 }
 
-SetSelectionOptions::Builder& SetSelectionOptions::Builder::SetShouldShowHandle(
-    bool new_value) {
-  data_.should_show_handle_ = new_value;
+SetSelectionOptions::Builder&
+SetSelectionOptions::Builder::SetShouldCloseTyping(bool new_value) {
+  data_.should_close_typing_ = new_value;
   return *this;
 }
 
-SetSelectionOptions::Builder& SetSelectionOptions::Builder::SetSetSelectionBy(
-    SetSelectionBy new_value) {
-  data_.set_selection_by_ = new_value;
+SetSelectionOptions::Builder& SetSelectionOptions::Builder::SetShouldShowHandle(
+    bool new_value) {
+  data_.should_show_handle_ = new_value;
   return *this;
 }
 
