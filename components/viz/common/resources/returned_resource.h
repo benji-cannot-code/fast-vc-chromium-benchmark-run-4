1f676cef8e342e05d7a7485b743e415fbee26bc9
Jinsong Fan <fanjinsong@sogou-inc.com>
2017-10-05 17:22:51 +0000

    viz: Should not include itself in returned_resource.h
    BUG=none
    TEST=none
    
    Change-Id: I721fd9c401a987611773025349bea391928ca810
    Reviewed-on: https://chromium-review.googlesource.com/693821
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506777}

--- components/viz/common/resources/returned_resource.h
+++ components/viz/common/resources/returned_resource.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "components/viz/common/resources/resource_id.h"
-#include "components/viz/common/resources/returned_resource.h"
 #include "components/viz/common/viz_common_export.h"
 #include "gpu/command_buffer/common/sync_token.h"
 
