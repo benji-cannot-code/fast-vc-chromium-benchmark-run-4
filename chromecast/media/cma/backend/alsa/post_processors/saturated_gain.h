b662b94a9e1edc17802d102e077884ba1604999f
Benjamin Shaya <bshaya@google.com>
2017-10-05 00:43:26 +0000

    [chromecast] Add volume in dbfs to AudioPostProcessor::ProcessFrames
    Some Post Processors prefer system volume, some need the actual dbfs applied.
    Solve this by providing both.
    
    Merge-With: eureka-internal/104865
    BUG=internal b/65652496
    TEST=cast_alsa_cma_backend_unittests
    
    Change-Id: I6aecdf3c70e65927972de25569b3a56d2fb5f7d2
    Reviewed-on: https://chromium-review.googlesource.com/666043
    Commit-Queue: Benjamin Shaya <bshaya@google.com>
    Reviewed-by: Stephen Lanham <slan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506588}

--- chromecast/media/cma/backend/alsa/post_processors/saturated_gain.h
+++ chromecast/media/cma/backend/alsa/post_processors/saturated_gain.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "chromecast/media/cma/backend/alsa/slew_volume.h"
-#include "chromecast/media/cma/backend/alsa/volume_map.h"
 #include "chromecast/public/media/audio_post_processor_shlib.h"
 
 namespace chromecast {
@@ -24,14 +23,16 @@ class SaturatedGain : public AudioPostProcessor {
 
   // AudioPostProcessor implementation:
   bool SetSampleRate(int sample_rate) override;
-  int ProcessFrames(float* data, int frames, float volume) override;
+  int ProcessFrames(float* data,
+                    int frames,
+                    float volume,
+                    float volume_dbfs) override;
   int GetRingingTimeInFrames() override;
 
  private:
   int channels_;
   int sample_rate_;
-  float last_volume_;
-  VolumeMap volume_map_;
+  float last_volume_dbfs_;
   SlewVolume slew_volume_;
   float gain_;
 
