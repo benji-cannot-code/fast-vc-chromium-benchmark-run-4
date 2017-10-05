7a1dfcf45483ed155d76d54341beb14bea257b46
wutao <wutao@chromium.org>
2017-10-05 01:02:43 +0000

    Reland: Add DeferredPaintObserver to ScopedLayerAnimationSettings.
    We want to defer layer painting during some UI animations so that we can
    avoid resterization at the same time and we can cache the render suface
    of the animating layer. Adding an observer to
    ScopedLayerAnimationSettings can AddDeferredPaintRequest and
    RemoveDeferredPaintRequest at the beginning and end of the animation.
    
    Bug=761109
    Test=LayerAnimatorTest.DeferredPaint*
    
    Change-Id: I319c52f8f00b402d84363c7f926b0a376e07458c
    Reviewed-on: https://chromium-review.googlesource.com/700357
    Reviewed-by: David Reveman <reveman@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506598}

--- ui/compositor/layer_animator.h
+++ ui/compositor/layer_animator.h
@@ -38,6 +38,7 @@ class Transform;
 
 namespace ui {
 class Compositor;
+class ImplicitAnimationObserver;
 class Layer;
 class LayerAnimationSequence;
 class LayerAnimationDelegate;
@@ -196,6 +197,11 @@ class COMPOSITOR_EXPORT LayerAnimator : public base::RefCounted<LayerAnimator>,
   // list. The observers are notified when animations end.
   void AddObserver(LayerAnimationObserver* observer);
   void RemoveObserver(LayerAnimationObserver* observer);
+
+  void AddOwnedObserver(
+      std::unique_ptr<ImplicitAnimationObserver> animation_observer);
+  void RemoveAndDestroyOwnedObserver(
+      ImplicitAnimationObserver* animation_observer);
 
   // Called when a threaded animation is actually started.
   void OnThreadedAnimationStarted(base::TimeTicks monotonic_time,
@@ -416,6 +422,8 @@ class COMPOSITOR_EXPORT LayerAnimator : public base::RefCounted<LayerAnimator>,
   // Observers are notified when layer animations end, are scheduled or are
   // aborted.
   base::ObserverList<LayerAnimationObserver> observers_;
+
+  std::vector<std::unique_ptr<ImplicitAnimationObserver>> owned_observer_list_;
 
   DISALLOW_COPY_AND_ASSIGN(LayerAnimator);
 };
