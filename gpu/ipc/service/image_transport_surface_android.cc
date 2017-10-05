a82c5e76557160e62f99515c90c2e812f9435e03
Brian Anderson <brianderson@chromium.org>
2017-10-05 22:16:19 +0000

    egl: Update khronos EGL headers.
    Copy latest khronos EGL headers to pick up
    eglGetFrameTimstampsANDROID.
    
    Also copy over existing chromium-specific edits.
    
    BUG=769415
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ibd9b185d568c9e714e664e7380e15138f30fc3d2
    Reviewed-on: https://chromium-review.googlesource.com/703076
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Brian Anderson <brianderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506880}

--- gpu/ipc/service/image_transport_surface_android.cc
+++ gpu/ipc/service/image_transport_surface_android.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_surface_egl.h"
 #include "ui/gl/gl_surface_stub.h"
 
+#include <android/native_window.h>
+
 namespace gpu {
 
 // static
