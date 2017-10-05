771313f5574537eb39510980dfc559b4a410e588
lgcheng <lgcheng@google.com>
2017-10-05 19:17:50 +0000

    Enable Arc Pacakge sync datatype at proper time.
    We enable Arc package sync datatype when user enables Arc, and call
    StartModels when Arc app instance is ready and Arc packages get
    refreshed. But There is a big time gap since we need perform provision
    for Arc during opt-in. And this may lead to the high config time-out
    rate for Arc packages.
    
    Enable the datatype when provision is successful and reduce the gap
    between enabling the sync data type and calling startModels.
    
    Bug: 771838
    Test: Manual test and Arc pacakge related integration test.
    Change-Id: I4c70c9d7e3c690fc953dac981398424d166ce51f
    Reviewed-on: https://chromium-review.googlesource.com/702014
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Long Cheng <lgcheng@google.com>
    Cr-Commit-Position: refs/heads/master@{#506817}

--- chrome/browser/ui/app_list/arc/arc_package_sync_data_type_controller.h
+++ chrome/browser/ui/app_list/arc/arc_package_sync_data_type_controller.h
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_APP_LIST_ARC_ARC_PACKAGE_SYNC_DATA_TYPE_CONTROLLER_H_
 
 #include "base/macros.h"
+#include "chrome/browser/chromeos/arc/arc_session_manager.h"
 #include "chrome/browser/ui/app_list/arc/arc_app_list_prefs.h"
-#include "components/prefs/pref_change_registrar.h"
 #include "components/sync/driver/async_directory_type_controller.h"
 #include "components/sync/driver/data_type_controller.h"
 
@@ -22,7 +22,8 @@ class SyncClient;
 // disables these types based on whether ArcAppInstance is ready.
 class ArcPackageSyncDataTypeController
     : public syncer::AsyncDirectoryTypeController,
-      public ArcAppListPrefs::Observer {
+      public ArcAppListPrefs::Observer,
+      public arc::ArcSessionManager::Observer {
  public:
   // |dump_stack| is called when an unrecoverable error occurs.
   ArcPackageSyncDataTypeController(syncer::ModelType type,
@@ -40,17 +41,18 @@ class ArcPackageSyncDataTypeController
   // ArcAppListPrefs::Observer:
   void OnPackageListInitialRefreshed() override;
 
-  void OnArcEnabledPrefChanged();
+  // ArcSessionManager::Observer:
+  void OnArcPlayStoreEnabledChanged(bool enabled) override;
+  void OnArcInitialStart() override;
 
   void EnableDataType();
 
+  // Returns true if user enables app sync.
   bool ShouldSyncArc() const;
 
   bool model_normal_start_ = true;
 
   Profile* const profile_;
-
-  PrefChangeRegistrar pref_registrar_;
 
   DISALLOW_COPY_AND_ASSIGN(ArcPackageSyncDataTypeController);
 };
