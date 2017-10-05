82398ce73d36fd497b229e6de7ace2de9ba3780b
Devlin Cronin <rdevlin.cronin@chromium.org>
2017-10-05 00:31:56 +0000

    [Extensions Cleanup] Use ExtensionId in RendererMessagingService
    Use the ExtensionId typedef instead of std::string in
    RendererMessagingService method.
    
    Bug: None
    Change-Id: Ibdf8e9ee5a0de65c220d70c255ffd58f52e0f451
    Reviewed-on: https://chromium-review.googlesource.com/699753
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506583}

--- extensions/renderer/renderer_messaging_service.h
+++ extensions/renderer/renderer_messaging_service.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/macros.h"
+#include "extensions/common/extension_id.h"
 
 struct ExtensionMsg_ExternalConnectionInfo;
 struct ExtensionMsg_TabConnectionInfo;
@@ -90,7 +91,7 @@ class RendererMessagingService {
   virtual void DispatchOnConnectToListeners(
       ScriptContext* script_context,
       const PortId& target_port_id,
-      const std::string& target_extension_id,
+      const ExtensionId& target_extension_id,
       const std::string& channel_name,
       const ExtensionMsg_TabConnectionInfo* source,
       const ExtensionMsg_ExternalConnectionInfo& info,
