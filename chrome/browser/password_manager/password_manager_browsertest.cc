cd642b577efdaae7f3722aabf2cea5b92c42d671
Irmak Kavasoglu <irmakkavasoglu@gmail.com>
2017-10-05 19:29:11 +0000

    Renamed |other_possible_passwords| to |all_possible_passwords|
    Other possible passwords variable of the password form is currently
    holding all possible passwords. See this cl:
    https://chromium-review.googlesource.com/c/chromium/src/+/660077
    
    This cl renames the variable accordingly.
    
    Bug: 753806
    Change-Id: Id6e19b876392039704d1f23a8b42d1ca18e79ea6
    Reviewed-on: https://chromium-review.googlesource.com/693014
    Commit-Queue: Maxim Kolosovskiy <kolos@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506823}

--- chrome/browser/password_manager/password_manager_browsertest.cc
+++ chrome/browser/password_manager/password_manager_browsertest.cc
@@ -66,7 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/point.h"
 
 using testing::_;
-using testing::UnorderedElementsAre;
+using testing::ElementsAre;
 
 namespace {
 
@@ -2465,7 +2465,7 @@ IN_PROC_BROWSER_TEST_F(PasswordManagerBrowserTestForPasswordSelection,
   observer.Wait();
   // 3 possible passwords are going to be shown in a dropdown when the password
   // selection feature is enabled. The first one will be selected as the main
-  // password by default. All three will be in the other_possible_passwords
+  // password by default. All three will be in the all_possible_passwords
   // list. The save password prompt is expected.
   BubbleObserver bubble_observer(WebContents());
   EXPECT_TRUE(bubble_observer.IsSavePromptShownAutomatically());
@@ -2473,12 +2473,12 @@ IN_PROC_BROWSER_TEST_F(PasswordManagerBrowserTestForPasswordSelection,
             ManagePasswordsUIController::FromWebContents(WebContents())
                 ->GetPendingPassword()
                 .password_value);
-  EXPECT_THAT(ManagePasswordsUIController::FromWebContents(WebContents())
-                  ->GetPendingPassword()
-                  .other_possible_passwords,
-              UnorderedElementsAre(base::ASCIIToUTF16("pass1"),
-                                   base::ASCIIToUTF16("pass2"),
-                                   base::ASCIIToUTF16("pass3")));
+  EXPECT_THAT(
+      ManagePasswordsUIController::FromWebContents(WebContents())
+          ->GetPendingPassword()
+          .all_possible_passwords,
+      ElementsAre(base::ASCIIToUTF16("pass1"), base::ASCIIToUTF16("pass2"),
+                  base::ASCIIToUTF16("pass3")));
   bubble_observer.AcceptSavePrompt();
   WaitForPasswordStore();
   CheckThatCredentialsStored(base::ASCIIToUTF16(""),
