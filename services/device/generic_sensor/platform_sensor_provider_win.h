eb4671947e67a5b57e59cbec0adf0dc4619d6dbd
Alexander Shalamov <alexander.shalamov@intel.com>
2017-10-05 08:29:51 +0000

    [sensors][win] Move ownership of ISensorManager to sensor thread
    This CL moves ownership of ISensorManager to sensor thread, so that
    ISensorManager is initialized when new sensor is created or in use,
    and destroyed when there are no sensors. Before this CL, ISensorManager
    could have been reset from IO thread.
    
    Bug: 765527
    Change-Id: I579f06dfd4cec7b678fe0080fe99cc6b182de27c
    Reviewed-on: https://chromium-review.googlesource.com/699759
    Reviewed-by: Mikhail Pozdnyakov <mikhail.pozdnyakov@intel.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Alexander Shalamov <alexander.shalamov@intel.com>
    Cr-Commit-Position: refs/heads/master@{#506676}

--- services/device/generic_sensor/platform_sensor_provider_win.h
+++ services/device/generic_sensor/platform_sensor_provider_win.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 template <typename T>
 struct DefaultSingletonTraits;
-class Thread;
 }  // namespace base
 
 namespace device {
@@ -47,7 +46,7 @@ class PlatformSensorProviderWin final : public PlatformSensorProvider {
  private:
   PlatformSensorProviderWin();
 
-  bool InitializeSensorManager();
+  void CreateSensorThread();
   bool StartSensorThread();
   void StopSensorThread();
   std::unique_ptr<PlatformSensorReaderWin> CreateSensorReader(
@@ -61,8 +60,8 @@ class PlatformSensorProviderWin final : public PlatformSensorProvider {
  private:
   friend struct base::DefaultSingletonTraits<PlatformSensorProviderWin>;
 
-  base::win::ScopedComPtr<ISensorManager> sensor_manager_;
-  std::unique_ptr<base::Thread> sensor_thread_;
+  class SensorThread;
+  std::unique_ptr<SensorThread> sensor_thread_;
 
   DISALLOW_COPY_AND_ASSIGN(PlatformSensorProviderWin);
 };
