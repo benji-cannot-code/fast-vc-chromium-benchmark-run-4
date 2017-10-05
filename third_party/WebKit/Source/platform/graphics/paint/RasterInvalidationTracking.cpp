616afa5a27933014731a9e8207ab4d3c314b2aa3
Xianzhu Wang <wangxianzhu@chromium.org>
2017-10-05 01:01:36 +0000

    class CompositedLayerRasterInvalidator
    This class is seperated from ContentLayerClientImpl so that it can be
    used from GraphicsLayer to generate raster invalidations based on
    paint chunks for SPv175.
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Icd83f4767788c49fa86f9676e85751e15158b5c6
    Reviewed-on: https://chromium-review.googlesource.com/698873
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Tien-Ren Chen <trchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506596}

--- third_party/WebKit/Source/platform/graphics/paint/RasterInvalidationTracking.cpp
+++ third_party/WebKit/Source/platform/graphics/paint/RasterInvalidationTracking.cpp
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/graphics/paint/RasterInvalidationTracking.h"
 
 #include "SkImageFilter.h"
+#include "platform/geometry/GeometryAsJSON.h"
 #include "platform/geometry/LayoutRect.h"
 #include "platform/graphics/Color.h"
 #include "platform/graphics/paint/PaintCanvas.h"
@@ -32,16 +33,6 @@ static bool CompareRasterInvalidationInfo(const RasterInvalidationInfo& a,
     return name_compare_result < 0;
 
   return a.reason < b.reason;
-}
-
-template <typename T>
-static std::unique_ptr<JSONArray> RectAsJSONArray(const T& rect) {
-  std::unique_ptr<JSONArray> array = JSONArray::Create();
-  array->PushDouble(rect.X());
-  array->PushDouble(rect.Y());
-  array->PushDouble(rect.Width());
-  array->PushDouble(rect.Height());
-  return array;
 }
 
 void RasterInvalidationTracking::AsJSON(JSONObject* json) {
