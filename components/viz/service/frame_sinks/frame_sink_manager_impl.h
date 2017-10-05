52b6c699872edf9540933aa6ff84560011733f3f
Gary Klassen <gklassen@chromium.org>
2017-10-05 17:32:38 +0000

    Route HitTestRegionList through FrameSinkManager
    This change routes HitTestRegionList objects received by
    CompositorFrameSinkSupport to the HitTestManager.
    
    The HitTestAggregator will query the HitTestManager for the
    active HitTestRegionLists when aggregating the data.
    
    HitTestManager also manages the lifetime of HitTestRegionList
    objects by deleting old objects when surfaces are discarded
    and deleting objects that correspond to older frames when
    a new surface is activated.
    
    This change also modifies HitTestAggregator memory management
    so that new memory is allocated as needed while aggregating
    HitTestRegionList data.
    
    Bug: 732399
    Change-Id: Iaedec31a34f04446a539acd10d77c6fe18034d44
    Reviewed-on: https://chromium-review.googlesource.com/650220
    Commit-Queue: Gary Klassen <gklassen@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: Ria Jiang <riajiang@chromium.org>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506780}

--- components/viz/service/frame_sinks/frame_sink_manager_impl.h
+++ components/viz/service/frame_sinks/frame_sink_manager_impl.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_checker.h"
 #include "components/viz/common/surfaces/frame_sink_id.h"
 #include "components/viz/service/frame_sinks/primary_begin_frame_source.h"
+#include "components/viz/service/hit_test/hit_test_manager.h"
 #include "components/viz/service/surfaces/surface_manager.h"
 #include "components/viz/service/surfaces/surface_observer.h"
 #include "components/viz/service/viz_service_export.h"
@@ -113,6 +114,8 @@ class VIZ_SERVICE_EXPORT FrameSinkManagerImpl : public SurfaceObserver,
 
   SurfaceManager* surface_manager() { return &surface_manager_; }
 
+  const HitTestManager* hit_test_manager() { return &hit_test_manager_; }
+
   // SurfaceObserver implementation.
   void OnFirstSurfaceActivation(const SurfaceInfo& surface_info) override;
   void OnSurfaceActivated(const SurfaceId& surface_id) override;
@@ -146,6 +149,9 @@ class VIZ_SERVICE_EXPORT FrameSinkManagerImpl : public SurfaceObserver,
       const SurfaceId& surface_id,
       uint64_t frame_index,
       mojom::HitTestRegionListPtr hit_test_region_list);
+
+  // This method is virtual so the implementation can be modified in unit tests.
+  virtual uint64_t GetActiveFrameIndex(const SurfaceId& surface_id);
 
  private:
   friend class cc::test::SurfaceSynchronizationTest;
@@ -191,6 +197,8 @@ class VIZ_SERVICE_EXPORT FrameSinkManagerImpl : public SurfaceObserver,
   // |surface_manager_| should be placed under |primary_source_| so that all
   // surfaces are destroyed before |primary_source_|.
   SurfaceManager surface_manager_;
+
+  HitTestManager hit_test_manager_;
 
   std::unordered_map<FrameSinkId,
                      std::unique_ptr<mojom::CompositorFrameSink>,
