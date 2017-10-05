921fd4811815cbbf1a99b23b45daa774cad35a22
Yoland Yan <yolandyan@chromium.org>
2017-10-05 16:36:31 +0000

    Convert Shape Detection tests to JUnit4
    For more on JUnit4 migration, please check
    src/testing/android/docs/junit4.md
    
    Bug: 640116
    Cq-Include-Trybots: master.tryserver.chromium.mac:mac_optional_gpu_tests_rel
    Change-Id: I06ab6c5d14bdc382b294482d6a7b3cec497117e0
    Reviewed-on: https://chromium-review.googlesource.com/701363
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Yoland Yan <yolandyan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506760}

--- services/shape_detection/android/javatests/src/org/chromium/shape_detection/BarcodeDetectionImplTest.java
+++ services/shape_detection/android/javatests/src/org/chromium/shape_detection/BarcodeDetectionImplTest.java
@@ -6,8 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.shape_detection;
 
 import android.support.test.filters.SmallTest;
-import android.test.InstrumentationTestCase;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.Feature;
 import org.chromium.shape_detection.mojom.BarcodeDetection;
 import org.chromium.shape_detection.mojom.BarcodeDetectionResult;
@@ -18,11 +22,10 @@ import java.util.concurrent.TimeUnit;
 /**
  * Test suite for BarcodeDetectionImpl.
  */
-public class BarcodeDetectionImplTest extends InstrumentationTestCase {
+@RunWith(BaseJUnit4ClassRunner.class)
+public class BarcodeDetectionImplTest {
     private static final org.chromium.skia.mojom.Bitmap QR_CODE_BITMAP =
             TestUtils.mojoBitmapFromFile("qr_code.png");
-
-    public BarcodeDetectionImplTest() {}
 
     private static BarcodeDetectionResult[] detect(org.chromium.skia.mojom.Bitmap mojoBitmap) {
         BarcodeDetection detector = new BarcodeDetectionImpl();
@@ -37,12 +40,13 @@ public class BarcodeDetectionImplTest extends InstrumentationTestCase {
         try {
             toReturn = queue.poll(5L, TimeUnit.SECONDS);
         } catch (InterruptedException e) {
-            fail("Could not get BarcodeDetectionResult: " + e.toString());
+            Assert.fail("Could not get BarcodeDetectionResult: " + e.toString());
         }
-        assertNotNull(toReturn);
+        Assert.assertNotNull(toReturn);
         return toReturn;
     }
 
+    @Test
     @SmallTest
     @Feature({"ShapeDetection"})
     public void testDetectBase64ValidImageString() {
@@ -50,11 +54,11 @@ public class BarcodeDetectionImplTest extends InstrumentationTestCase {
             return;
         }
         BarcodeDetectionResult[] results = detect(QR_CODE_BITMAP);
-        assertEquals(1, results.length);
-        assertEquals("https://chromium.org", results[0].rawValue);
-        assertEquals(40.0, results[0].boundingBox.x, 0.0);
-        assertEquals(40.0, results[0].boundingBox.y, 0.0);
-        assertEquals(250.0, results[0].boundingBox.width, 0.0);
-        assertEquals(250.0, results[0].boundingBox.height, 0.0);
+        Assert.assertEquals(1, results.length);
+        Assert.assertEquals("https://chromium.org", results[0].rawValue);
+        Assert.assertEquals(40.0, results[0].boundingBox.x, 0.0);
+        Assert.assertEquals(40.0, results[0].boundingBox.y, 0.0);
+        Assert.assertEquals(250.0, results[0].boundingBox.width, 0.0);
+        Assert.assertEquals(250.0, results[0].boundingBox.height, 0.0);
     }
 }
