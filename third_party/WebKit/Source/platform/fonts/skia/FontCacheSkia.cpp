8c174bedb084d407b3d36cf7ceec044048eff36a
Kent Tamura <tkent@chromium.org>
2017-10-05 04:41:14 +0000

    Build fix for FontCacheSkia.cpp
    TBR=peria@chromium.org
    
    Change-Id: I2df1b960d4fea915777c1aa9039d4b64844f5b9e
    Reviewed-on: https://chromium-review.googlesource.com/702178
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506650}

--- third_party/WebKit/Source/platform/fonts/skia/FontCacheSkia.cpp
+++ third_party/WebKit/Source/platform/fonts/skia/FontCacheSkia.cpp
@@ -39,8 +39,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SkTypeface.h"
 
 #include "build/build_config.h"
-#include "platform/FontFamilyNames.h"
 #include "platform/Language.h"
+#include "platform/font_family_names.h"
 #include "platform/fonts/AlternateFontFamily.h"
 #include "platform/fonts/FontCache.h"
 #include "platform/fonts/FontDescription.h"
