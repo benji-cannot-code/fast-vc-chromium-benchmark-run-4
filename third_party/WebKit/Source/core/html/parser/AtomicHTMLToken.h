d27a1ede302472a16005d2f57b465f09f0167fac
Kent Tamura <tkent@chromium.org>
2017-10-06 07:37:33 +0000

    Rename generated HTMLElementLookupTrie.{cpp,h} to html_element_lookup_trie.{cc,h}.
    Bug: 770603
    Change-Id: Id5a0d478b5f42eab384a95d0e63b98187292aa28
    Reviewed-on: https://chromium-review.googlesource.com/704294
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507002}

--- third_party/WebKit/Source/core/html/parser/AtomicHTMLToken.h
+++ third_party/WebKit/Source/core/html/parser/AtomicHTMLToken.h
@@ -28,10 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define AtomicHTMLToken_h
 
 #include <memory>
-#include "core/HTMLElementLookupTrie.h"
 #include "core/dom/Attribute.h"
 #include "core/html/parser/CompactHTMLToken.h"
 #include "core/html/parser/HTMLToken.h"
+#include "core/html_element_lookup_trie.h"
 #include "platform/wtf/Allocator.h"
 #include "platform/wtf/PtrUtil.h"
 
