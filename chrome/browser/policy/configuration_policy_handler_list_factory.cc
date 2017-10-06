14fb9b07b1c771765bdc278f4c008a2b487f495f
Cait Phillips <caitkp@chromium.org>
2017-10-06 00:40:22 +0000

    Add a pref to disable autofill credit cards so we can control it by policy.
    Bug: 700812
    Change-Id: I4ab2534057debd0d1c94b20e6c8b8844fd364d87
    Reviewed-on: https://chromium-review.googlesource.com/666059
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Roger McFarlane <rogerm@chromium.org>
    Commit-Queue: Cait Phillips <caitkp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506929}

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/network_time/network_time_pref_names.h"
 #include "components/ntp_snippets/pref_names.h"
 #include "components/password_manager/core/common/password_manager_pref_names.h"
+#include "components/policy/core/browser/autofill_credit_card_policy_handler.h"
 #include "components/policy/core/browser/autofill_policy_handler.h"
 #include "components/policy/core/browser/configuration_policy_handler.h"
 #include "components/policy/core/browser/configuration_policy_handler_list.h"
@@ -882,6 +883,7 @@ std::unique_ptr<ConfigurationPolicyHandlerList> BuildHandlerList(
         kSimplePolicyMap[i].value_type));
   }
 
+  handlers->AddHandler(base::MakeUnique<AutofillCreditCardPolicyHandler>());
   handlers->AddHandler(base::MakeUnique<AutofillPolicyHandler>());
   handlers->AddHandler(base::MakeUnique<DefaultSearchPolicyHandler>());
   handlers->AddHandler(base::MakeUnique<ForceSafeSearchPolicyHandler>());
