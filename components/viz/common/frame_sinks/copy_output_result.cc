019dba17a245df9c845128975b7dfe09af2938a2
Yuri Wiitala <miu@chromium.org>
2017-10-06 23:50:16 +0000

    Add scaling capability to CopyOutputRequests. (Part 2 of 2)
    This adds the GLRenderer implementation needed to support "scaling"
    CopyOutputRequests. Because this and additional upcoming implementation
    are rather "meaty," the decision was made to break out all the code
    related to copy request execution into a separate helper class,
    GLRendererCopier.
    
    GLRendererCopier not only executes CopyOutputRequests using GL, it also
    manages the caching of resources needed to ensure efficient video
    performance (i.e., for screen capture use cases). This change should
    perform as well, or better, than the previous implementation in the perf
    bot waterfall.
    
    This change also adds a plethora of testing to ensure all possible code
    paths are exercised, rect calculations and Y-flipping are being done
    correctly, caching works as expected, and enables the GLRenderer-variant
    of the recently-added scaling tests in copy_output_scaling_pixeltest.cc.
    
    This is a prerequisite change needed for moving tab capture into VIZ, as
    well as several clean-up efforts to simplify the snapshot-taking code in
    libcontent.
    
    Bug: 760348, 759310
    Change-Id: Idff091ec34c8f76bffce859615308084be310abe
    Reviewed-on: https://chromium-review.googlesource.com/696489
    Commit-Queue: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507232}

--- components/viz/common/frame_sinks/copy_output_result.cc
+++ components/viz/common/frame_sinks/copy_output_result.cc
@@ -77,7 +77,6 @@ const SkBitmap& CopyOutputSkBitmapResult::AsSkBitmap() const {
     replacement.writePixels(src_pixmap);
   }
   *bitmap = replacement;
-  bitmap->setImmutable();
 
   return *bitmap;
 }
