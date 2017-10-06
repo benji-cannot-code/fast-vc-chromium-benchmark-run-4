8222d4ea6f11ee445bff6be3fd0fcd6fb804a027
Justin Donnelly <jdonnelly@google.com>
2017-10-06 16:07:13 +0000

    Show the "= " text in calculator suggestions on phones.
    This prefix text was removed when we introduced the calculator
    icon (looks like '=') but then suggestion type icons were removed
    from the phone UIs. Reintroducing the "= " text makes it clearer
    that these are the result of the mathematical expression that the
    user has input.
    
    Bug: 772015
    Change-Id: If60f462a688b0f39288df0c7c713e21170d971e2
    Reviewed-on: https://chromium-review.googlesource.com/703435
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507086}

--- components/omnibox/browser/search_suggestion_parser.cc
+++ components/omnibox/browser/search_suggestion_parser.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/url_formatter/url_formatter.h"
 #include "net/http/http_response_headers.h"
 #include "net/url_request/url_fetcher.h"
+#include "ui/base/device_form_factor.h"
 #include "url/url_constants.h"
 
 namespace {
@@ -514,13 +515,19 @@ bool SearchSuggestionParser::ParseSuggestResults(
             input.text()));
       }
     } else {
-      // TODO(dschuyler) If the "= " is no longer sent from the back-end
-      // then this may be removed.
-      if ((match_type == AutocompleteMatchType::CALCULATOR) &&
-          !suggestion.compare(0, 2, base::UTF8ToUTF16("= ")))
-        suggestion.erase(0, 2);
-
       base::string16 match_contents = suggestion;
+      if ((match_type == AutocompleteMatchType::CALCULATOR) &&
+          !suggestion.compare(0, 2, base::UTF8ToUTF16("= "))) {
+        // Calculator results include a "= " prefix but we don't want to include
+        // this in the search terms.
+        suggestion.erase(0, 2);
+        // Additionally, on larger (non-phone) form factors, we don't want to
+        // display it in the suggestion contents either, because those devices
+        // display a suggestion type icon that looks like a '='.
+        if (ui::GetDeviceFormFactor() != ui::DEVICE_FORM_FACTOR_PHONE)
+          match_contents.erase(0, 2);
+      }
+
       base::string16 match_contents_prefix;
       base::string16 annotation;
       base::string16 answer_contents;
