88a416a03fdb8b0f9a465d96570e0aa51d706329
Alex Clarke <alexclarke@chromium.org>
2017-10-06 12:49:53 +0000

    Reland "Send Emulation.virtualTimeAdvanced when virtual time advances"
    This is a reland of 5638e72cdcf738ef90ea7bb75e2f0ffa79c0a5c5
    Original change's description:
    > Send Emulation.virtualTimeAdvanced when virtual time advances
    > 
    > This helps controls scripts get a better idea of the current virtual
    > time.
    > 
    > Bug: 
    > Change-Id: I39433f2790cdc51c14a3c02736c9320b0bce7c0a
    > Reviewed-on: https://chromium-review.googlesource.com/702374
    > Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    > Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    > Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#506789}
    
    TBR=dgozman@chromium.org
    
    Change-Id: I09aa9b37dd504f571cc55db0acfc7fadd3e3fda6
    Reviewed-on: https://chromium-review.googlesource.com/704674
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507039}

--- third_party/WebKit/Source/platform/scheduler/renderer/auto_advancing_virtual_time_domain.cc
+++ third_party/WebKit/Source/platform/scheduler/renderer/auto_advancing_virtual_time_domain.cc
@@ -10,7 +10,9 @@ namespace scheduler {
 
 AutoAdvancingVirtualTimeDomain::AutoAdvancingVirtualTimeDomain(
     base::TimeTicks initial_time)
-    : VirtualTimeDomain(initial_time), can_advance_virtual_time_(true) {}
+    : VirtualTimeDomain(initial_time),
+      can_advance_virtual_time_(true),
+      observer_(nullptr) {}
 
 AutoAdvancingVirtualTimeDomain::~AutoAdvancingVirtualTimeDomain() {}
 
@@ -21,6 +23,8 @@ AutoAdvancingVirtualTimeDomain::DelayTillNextTask(LazyNow* lazy_now) {
     return base::nullopt;
 
   AdvanceTo(run_time);
+  if (observer_)
+    observer_->OnVirtualTimeAdvanced();
   return base::TimeDelta();  // Makes DoWork post an immediate continuation.
 }
 
@@ -36,6 +40,10 @@ void AutoAdvancingVirtualTimeDomain::CancelWakeUpAt(base::TimeTicks run_time) {
   // We ignore this because RequestWakeUpAt doesn't post a delayed task.
 }
 
+void AutoAdvancingVirtualTimeDomain::SetObserver(Observer* observer) {
+  observer_ = observer;
+}
+
 void AutoAdvancingVirtualTimeDomain::SetCanAdvanceVirtualTime(
     bool can_advance_virtual_time) {
   can_advance_virtual_time_ = can_advance_virtual_time;
@@ -46,6 +54,10 @@ void AutoAdvancingVirtualTimeDomain::SetCanAdvanceVirtualTime(
 const char* AutoAdvancingVirtualTimeDomain::GetName() const {
   return "AutoAdvancingVirtualTimeDomain";
 }
+
+AutoAdvancingVirtualTimeDomain::Observer::Observer() {}
+
+AutoAdvancingVirtualTimeDomain::Observer::~Observer() {}
 
 }  // namespace scheduler
 }  // namespace blink
--- third_party/WebKit/Source/platform/scheduler/renderer/web_view_scheduler_impl.h
+++ third_party/WebKit/Source/platform/scheduler/renderer/web_view_scheduler_impl.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/scheduler/base/task_queue.h"
 #include "platform/scheduler/child/web_scheduler.h"
 #include "platform/scheduler/child/web_task_runner_impl.h"
+#include "platform/scheduler/renderer/auto_advancing_virtual_time_domain.h"
 #include "platform/scheduler/renderer/task_queue_throttler.h"
 #include "platform/scheduler/renderer/web_view_scheduler.h"
 
@@ -33,7 +34,9 @@ class RendererSchedulerImpl;
 class CPUTimeBudgetPool;
 class WebFrameSchedulerImpl;
 
-class PLATFORM_EXPORT WebViewSchedulerImpl : public WebViewScheduler {
+class PLATFORM_EXPORT WebViewSchedulerImpl
+    : public WebViewScheduler,
+      public AutoAdvancingVirtualTimeDomain::Observer {
  public:
   WebViewSchedulerImpl(
       WebScheduler::InterventionReporter* intervention_reporter,
@@ -59,6 +62,9 @@ class PLATFORM_EXPORT WebViewSchedulerImpl : public WebViewScheduler {
   void RequestBeginMainFrameNotExpected(bool new_state) override;
   void AddVirtualTimeObserver(VirtualTimeObserver*) override;
   void RemoveVirtualTimeObserver(VirtualTimeObserver*) override;
+
+  // AutoAdvancingVirtualTimeDomain::Observer implementation:
+  void OnVirtualTimeAdvanced() override;
 
   // Virtual for testing.
   virtual void ReportIntervention(const std::string& message);
