e8c43fa4f53f5adf0d3ab8b16d5136271959e9cc
Reilly Grant <reillyg@chromium.org>
2017-10-06 21:11:22 +0000

    Convert MediaTrackConstraints to a ScriptValue
    IDLDictionaries such as MediaTrackConstraints should not be stored on
    the heap which would happen when binding one as a parameter to a
    callback. This change converts the object to a ScriptValue ahead of
    time. This is fine because the value will be passed to a
    ScriptPromiseResolver that will converted it to a V8 value if it
    isn't already.
    
    Bug: 759457
    Change-Id: I3009a0f7711cc264aeaae07a36c18a6db8c915c8
    Reviewed-on: https://chromium-review.googlesource.com/701358
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507177}

--- third_party/WebKit/Source/modules/imagecapture/ImageCapture.h
+++ third_party/WebKit/Source/modules/imagecapture/ImageCapture.h
@@ -24,7 +24,6 @@ namespace blink {
 
 class ExceptionState;
 class MediaStreamTrack;
-class MediaTrackConstraints;
 class PhotoCapabilities;
 class ScriptPromiseResolver;
 class WebImageCaptureFrameGrabber;
@@ -98,7 +97,7 @@ class MODULES_EXPORT ImageCapture final
   void ResolveWithNothing(ScriptPromiseResolver*);
   void ResolveWithPhotoSettings(ScriptPromiseResolver*);
   void ResolveWithPhotoCapabilities(ScriptPromiseResolver*);
-  void ResolveWithMediaTrackConstraints(MediaTrackConstraints,
+  void ResolveWithMediaTrackConstraints(ScriptValue constraints,
                                         ScriptPromiseResolver*);
 
   Member<MediaStreamTrack> stream_track_;
