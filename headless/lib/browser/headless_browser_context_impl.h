4a13f9046aa7f82beaa2ab1733b431fe3d1b5556
Iris Uy <irisu@chromium.org>
2017-10-06 05:23:06 +0000

    Store emulated network conditions in HeadlessBrowserContextImpl.
    This is needed so that the network conditions can be visibile to
    GenericURLRequestJob in headless/public/util. We need to store
    it in the HeadlessBrowserContextImpl, since GenericURLRequestJob
    does not reuse the network stack (e.g. DevToolsNetworkTransaction,
    etc).
    
    Bug: 
    Change-Id: I531acda73bffad2e504efbfbaa4fd8df2ef4b23e
    Reviewed-on: https://chromium-review.googlesource.com/696887
    Commit-Queue: Iris Uy <irisu@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: David Vallet <dvallet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506980}

--- headless/lib/browser/headless_browser_context_impl.h
+++ headless/lib/browser/headless_browser_context_impl.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/browser/resource_context.h"
 #include "headless/lib/browser/headless_browser_context_options.h"
+#include "headless/lib/browser/headless_network_conditions.h"
 #include "headless/lib/browser/headless_url_request_context_getter.h"
 #include "headless/public/headless_browser.h"
 #include "headless/public/headless_browser_context.h"
@@ -110,6 +111,9 @@ class HeadlessBrowserContextImpl : public HeadlessBrowserContext,
                               int net_error,
                               bool canceled_by_devtools);
 
+  void SetNetworkConditions(HeadlessNetworkConditions conditions);
+  HeadlessNetworkConditions GetNetworkConditions();
+
  private:
   HeadlessBrowserContextImpl(
       HeadlessBrowserImpl* browser,
@@ -139,6 +143,8 @@ class HeadlessBrowserContextImpl : public HeadlessBrowserContext,
   std::unique_ptr<content::PermissionManager> permission_manager_;
 
   std::string id_;
+
+  HeadlessNetworkConditions network_conditions_;
 
   DISALLOW_COPY_AND_ASSIGN(HeadlessBrowserContextImpl);
 };
---
+++ headless/lib/browser/headless_network_conditions.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "headless/lib/browser/headless_network_conditions.h"
+
+namespace headless {
+
+HeadlessNetworkConditions::HeadlessNetworkConditions()
+    : HeadlessNetworkConditions(false) {}
+
+HeadlessNetworkConditions::HeadlessNetworkConditions(bool offline)
+    : HeadlessNetworkConditions(offline, 0, 0, 0) {}
+
+HeadlessNetworkConditions::HeadlessNetworkConditions(bool offline,
+                                                     double latency,
+                                                     double download_throughput,
+                                                     double upload_throughput)
+    : offline(offline),
+      latency(latency),
+      download_throughput(download_throughput),
+      upload_throughput(upload_throughput) {}
+
+HeadlessNetworkConditions::~HeadlessNetworkConditions() {}
+
+}  // namespace headless
