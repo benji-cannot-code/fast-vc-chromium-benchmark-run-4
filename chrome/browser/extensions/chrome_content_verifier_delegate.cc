95c92c623a18d710fbca96327d11735378cf955e
proberge <proberge@chromium.org>
2017-10-06 14:43:30 +0000

    Reland "Reland "Extension content verification: unify "is from store" logic""
    The first re-land failed on official bots (https://bugs.chromium.org/p/chromium/issues/detail?id=770001#c8)
    The failures should be fixed by https://chromium-review.googlesource.com/c/chromium/src/+/703508.
    
    TBR=rdevlin.cronin@chromium.org (previously approved in 
    https://chromium-review.googlesource.com/c/chromium/src/+/698985)
    
    This is a reland of 7cd5437782cc60de95c6ae8b27603ee396d9e187
    Original change's description:
    > Reland "Extension content verification: unify "is from store" logic"
    > 
    > (Not a direct reland: the change to extension_settings_browsertest.h
    > was not in the original CL)
    > 
    > This is a reland of f3bd7c65886dedfdce09ec6af5d0f95907e89b1f
    > Original change's description:
    > > Extension content verification: unify "is from store" logic
    > > 
    > > Bug: 766806
    > > Change-Id: I1a210ded77cc9ffdc0039fca097c6fdba7ff0131
    > > Reviewed-on: https://chromium-review.googlesource.com/677403
    > > Commit-Queue: proberge <proberge@chromium.org>
    > > Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#505092}
    > 
    > Bug: 766806
    > Change-Id: I4a0c69c50d7bc41a47e264ad9b9e3136a729ec96
    > Reviewed-on: https://chromium-review.googlesource.com/698985
    > Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    > Commit-Queue: proberge <proberge@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#506422}
    
    Bug: 766806
    Change-Id: I2144bf340c38d5e81f0b599b39c6f4d84479d9a3
    Reviewed-on: https://chromium-review.googlesource.com/704234
    Reviewed-by: proberge <proberge@chromium.org>
    Commit-Queue: proberge <proberge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507065}

--- chrome/browser/extensions/chrome_content_verifier_delegate.cc
+++ chrome/browser/extensions/chrome_content_verifier_delegate.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/version.h"
 #include "build/build_config.h"
 #include "chrome/browser/extensions/extension_service.h"
+#include "chrome/browser/extensions/install_verifier.h"
 #include "chrome/browser/extensions/policy_extension_reinstaller.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/extension_constants.h"
@@ -131,7 +132,10 @@ ContentVerifierDelegate::Mode ChromeContentVerifierDelegate::ShouldBeVerified(
   if (!Manifest::IsAutoUpdateableLocation(extension.location()))
     return ContentVerifierDelegate::NONE;
 
-  if (!ManifestURL::UpdatesFromGallery(&extension)) {
+  // Use the InstallVerifier's |IsFromStore| method to avoid discrepancies
+  // between which extensions are considered in-store.
+  // See https://crbug.com/766806 for details.
+  if (!InstallVerifier::IsFromStore(extension)) {
     // It's possible that the webstore update url was overridden for testing
     // so also consider extensions with the default (production) update url
     // to be from the store as well.
