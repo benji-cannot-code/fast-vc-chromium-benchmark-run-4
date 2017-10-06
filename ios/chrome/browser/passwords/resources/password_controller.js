2f67ea5ab5955d030727d053d230168ef0580136
Vadym Doroshenko <dvadym@chromium.org>
2017-10-06 14:34:21 +0000

    [Bling Password Manager] Stop extraction of form.method attribute.
    Injected JavaScript PasswordController extracts form.method attribute,
    but it's not used anymore. Let's remove it.
    
    Bug: 
    Change-Id: I1cf3fe7138974b22dbc386f2b2ebb0df7134ac5f
    Reviewed-on: https://chromium-review.googlesource.com/704815
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507060}

--- ios/chrome/browser/passwords/resources/password_controller.js
+++ ios/chrome/browser/passwords/resources/password_controller.js
@@ -506,7 +506,6 @@ if (__gCrWeb && !__gCrWeb['fillPasswordForm']) {
 
     return {
       'action': getCanonicalActionForForm_(formElement),
-      'method': formElement.getAttribute('method'),
       'name': __gCrWeb.common.getFormIdentifier(formElement),
       'origin': origin,
       'fields': fields,
