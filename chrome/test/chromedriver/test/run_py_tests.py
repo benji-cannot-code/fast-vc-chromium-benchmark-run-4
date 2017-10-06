9792f38dd7d610a37eb1ceb89008f5b75e83cd30
John Chen <johnchen@chromium.org>
2017-10-06 16:45:02 +0000

    [ChromeDriver] Fix test case testDeviceName
    Fix test case MobileEmulationCapabilityTest.testDeviceName in Python
    ChromeDriver tests, which was broken by https://crrev.com/c/689966.
    
    Bug: chromedriver:2062
    Change-Id: I974852fcd5cd9308bada6c1614cf33d40d343f00
    Reviewed-on: https://chromium-review.googlesource.com/703995
    Commit-Queue: John Chen <johnchen@chromium.org>
    Reviewed-by: Shuotao Gao <stgao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507098}

--- chrome/test/chromedriver/test/run_py_tests.py
+++ chrome/test/chromedriver/test/run_py_tests.py
@@ -11,6 +11,7 @@ import json
 import math
 import optparse
 import os
+import re
 import shutil
 import socket
 import subprocess
@@ -2103,11 +2104,13 @@ class MobileEmulationCapabilityTest(ChromeDriverBaseTest):
     self.assertEqual(360, driver.ExecuteScript('return window.screen.width'))
     self.assertEqual(640, driver.ExecuteScript('return window.screen.height'))
     body_tag = driver.FindElement('tag name', 'body')
-    self.assertEqual(
-        'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) '
-        'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/%s Mobile '
-        'Safari/537.36',
-        body_tag.GetText())
+    self.assertRegexpMatches(
+        body_tag.GetText(),
+        '^' +
+        re.escape('Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) '
+                  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/') +
+        r'\d+\.\d+\.\d+\.\d+' +
+        re.escape(' Mobile Safari/537.36') + '$')
 
   def testSendKeysToElement(self):
     driver = self.CreateDriver(
