2a6289b462bafb0a6b0f403072f5a6a3f34ec3a8
Francois Beaufort <beaufort.francois@gmail.com>
2017-10-06 11:57:56 +0000

    Add DevTools warning when protected content is not CORS-same-origin
    Change-Id: Ia9a370645871ac5c1bc1305ba91c3e06c3fedf32
    Reviewed-on: https://chromium-review.googlesource.com/700496
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507034}

--- third_party/WebKit/Source/modules/encryptedmedia/HTMLMediaElementEncryptedMedia.cpp
+++ third_party/WebKit/Source/modules/encryptedmedia/HTMLMediaElementEncryptedMedia.cpp
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/ExceptionCode.h"
 #include "core/dom/TaskRunnerHelper.h"
 #include "core/html/HTMLMediaElement.h"
+#include "core/inspector/ConsoleMessage.h"
 #include "core/typed_arrays/DOMTypedArray.h"
 #include "modules/encryptedmedia/ContentDecryptionModuleResultPromise.h"
 #include "modules/encryptedmedia/EncryptedMediaUtils.h"
@@ -407,6 +408,13 @@ void HTMLMediaElementEncryptedMedia::Encrypted(
     // so don't return the initData. However, they still get an event.
     event = CreateEncryptedEvent(WebEncryptedMediaInitDataType::kUnknown,
                                  nullptr, 0);
+    media_element_->GetExecutionContext()->AddConsoleMessage(
+        ConsoleMessage::Create(kJSMessageSource, kWarningMessageLevel,
+                               "Media element must be CORS-same-origin with "
+                               "the embedding page. If cross-origin, you "
+                               "should use the `crossorigin` attribute and "
+                               "make sure CORS headers on the media data "
+                               "response are CORS-same-origin."));
   }
 
   event->SetTarget(media_element_);
