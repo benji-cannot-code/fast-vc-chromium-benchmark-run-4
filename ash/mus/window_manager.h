e59e231da77951fc9118bf4376ea2050cd7c3719
Peng Huang <penghuang@chromium.org>
2017-10-06 15:21:47 +0000

    Run exo in ash process for mus+ash
    For short term, we would like to run exo and ash in the same process.
    It should have good performance. If not, we may consider other options.
    
    Known issues:
     * The notification doesn't work.
     * The ChromeFileHelper isn't implemented.
    
    Bug: 672961
    Change-Id: Ia80124593f55654303cf20d9d63971db10d18449
    Reviewed-on: https://chromium-review.googlesource.com/682652
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507077}

--- ash/mus/window_manager.h
+++ ash/mus/window_manager.h
@@ -47,6 +47,7 @@ class WMState;
 namespace ash {
 class AshTestHelper;
 enum class Config;
+class WaylandServerController;
 
 namespace mus {
 
@@ -204,6 +205,8 @@ class WindowManager : public aura::WindowManagerDelegate,
   std::unique_ptr<DragState> drag_state_;
 
   std::unique_ptr<ui::InputDeviceClient> input_device_client_;
+
+  std::unique_ptr<WaylandServerController> wayland_server_controller_;
 
   DISALLOW_COPY_AND_ASSIGN(WindowManager);
 };
