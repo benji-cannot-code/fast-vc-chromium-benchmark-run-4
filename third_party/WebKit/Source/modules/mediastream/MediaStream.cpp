bd90dad2343ed60ee3e3d8e44a07c9b3055b8f70
Thiemo Nagel <tnagel@chromium.org>
2017-10-06 12:14:57 +0000

    Revert "Include ended tracks when cloning/constructing MediaStreams"
    This reverts commit 5342a1c2607885ce35edff4c713cd51c197e87d1.
    
    Speculative reason for revert: MediaStream-idl.https.html is failing [1] and this is the only CL in the regression range that changes MediaStream.
    
    [1] https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Android%20%28Nexus4%29/builds/70104
    
    Original change's description:
    > Include ended tracks when cloning/constructing MediaStreams
    > 
    > Bug: 770908
    > Change-Id: If9537579fd14b5fe88b7a084e7e941646d5f81f2
    > Reviewed-on: https://chromium-review.googlesource.com/702265
    > Commit-Queue: Harald Alvestrand <hta@chromium.org>
    > Reviewed-by: Harald Alvestrand <hta@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#507011}
    
    TBR=hta@chromium.org,guidou@chromium.org
    
    Change-Id: Ic8b553744c9ac2bad62f425fb6ad558f3f003fb4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 770908
    Reviewed-on: https://chromium-review.googlesource.com/704894
    Reviewed-by: Thiemo Nagel <tnagel@chromium.org>
    Commit-Queue: Thiemo Nagel <tnagel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507036}

--- third_party/WebKit/Source/modules/mediastream/MediaStream.cpp
+++ third_party/WebKit/Source/modules/mediastream/MediaStream.cpp
@@ -50,6 +50,9 @@ static bool ContainsSource(MediaStreamTrackVector& track_vector,
 
 static void ProcessTrack(MediaStreamTrack* track,
                          MediaStreamTrackVector& track_vector) {
+  if (track->Ended())
+    return;
+
   MediaStreamSource* source = track->Component()->Source();
   if (!ContainsSource(track_vector, source))
     track_vector.push_back(track);
