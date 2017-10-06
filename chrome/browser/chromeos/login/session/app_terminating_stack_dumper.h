0829da9f24c717aea21e95bd8db6182a48dce6b9
Xiyuan Xia <xiyuan@chromium.org>
2017-10-06 17:23:13 +0000

    cros: Fix CHECK in UserSessionManager::OnProfileCreated
    - Move CHECK() into expected profile creation status branch;
    - Create AppTerimatingStackDumper between starting primary user session
      and primary user profile loaded to catch the code that attempts to
      exit in-between;
    
    Bug: 717585
    Change-Id: I05f6d9182d8a9e2fc69f2f1437a03d0c0de22376
    Reviewed-on: https://chromium-review.googlesource.com/703977
    Reviewed-by: Rahul Chaturvedi <rkc@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507111}

---
+++ chrome/browser/chromeos/login/session/app_terminating_stack_dumper.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_LOGIN_SESSION_APP_TERMINATING_STACK_DUMPER_H_
+#define CHROME_BROWSER_CHROMEOS_LOGIN_SESSION_APP_TERMINATING_STACK_DUMPER_H_
+
+#include "base/macros.h"
+#include "content/public/browser/notification_observer.h"
+#include "content/public/browser/notification_registrar.h"
+
+namespace chromeos {
+
+// Helper class that dumps stack trace on NOTIFICATION_APP_TERMINATING.
+// TODO(crbug.com/717585): Remove after the root cause of bug identified.
+class AppTerminatingStackDumper : public content::NotificationObserver {
+ public:
+  AppTerminatingStackDumper();
+  ~AppTerminatingStackDumper() override;
+
+  // content::NotificationObserver:
+  void Observe(int type,
+               const content::NotificationSource& source,
+               const content::NotificationDetails& details) override;
+
+ private:
+  content::NotificationRegistrar registrar_;
+
+  DISALLOW_COPY_AND_ASSIGN(AppTerminatingStackDumper);
+};
+
+}  // namespace chromeos
+
+#endif  // CHROME_BROWSER_CHROMEOS_LOGIN_SESSION_APP_TERMINATING_STACK_DUMPER_H_
