66747b8daee94a9502050352fdaa1ee198832d42
Guido Urdaneta <guidou@chromium.org>
2017-10-07 11:09:36 +0000

    Reland "Element capture: Do not check cross origin for elements with srcObject."
    This is a reland of a443faa044b0e1472b36d35aaf425ae0ee6b2ac5
    Original change's description:
    > Element capture: Do not check cross origin for elements with srcObject.
    > 
    > Elements with srcObject contain MediaStreams and they cannot be
    > cross origin since capture from cross-origin elements (with src)
    > is disabled.
    > The cross-origin check fails for mediaStreams since it uses the src,
    > which is empty when the stream has been assigned to the element as an
    > srcObject.
    > 
    > Bug: 771388
    > Change-Id: I060ce54e37ba34814ac6186b0e098edfcc3d752e
    > Reviewed-on: https://chromium-review.googlesource.com/699434
    > Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    > Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#506465}
    
    Now that https://crrev.com/c/699747 has landed, it is safe to reland this.
    
    TBR=emircan@chromium.org
    
    Bug: 771388
    Change-Id: I7ae8ec814985ce218b4d17c39d0c1d406c0a86b4
    Reviewed-on: https://chromium-review.googlesource.com/706254
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507292}

--- third_party/WebKit/Source/modules/mediacapturefromelement/HTMLMediaElementCapture.cpp
+++ third_party/WebKit/Source/modules/mediacapturefromelement/HTMLMediaElementCapture.cpp
@@ -126,7 +126,8 @@ void MediaElementEventListener::UpdateSources(ExecutionContext* context) {
   for (auto track : media_stream_->getTracks())
     sources_.insert(track->Component()->Source());
 
-  if (!media_element_->IsMediaDataCORSSameOrigin(
+  if (!media_element_->currentSrc().IsEmpty() &&
+      !media_element_->IsMediaDataCORSSameOrigin(
           context->GetSecurityOrigin())) {
     for (auto source : sources_)
       MediaStreamCenter::Instance().DidStopMediaStreamSource(source);
@@ -155,7 +156,7 @@ MediaStream* HTMLMediaElementCapture::captureStream(
     return nullptr;
   }
 
-  if (!(element.currentSrc().IsNull() && !element.GetSrcObject()) &&
+  if (!element.currentSrc().IsEmpty() &&
       !element.IsMediaDataCORSSameOrigin(
           element.GetExecutionContext()->GetSecurityOrigin())) {
     exception_state.ThrowSecurityError(
@@ -179,7 +180,7 @@ MediaStream* HTMLMediaElementCapture::captureStream(
   // If |element| is actually playing a MediaStream, just clone it.
   if (element.GetLoadType() == WebMediaPlayer::kLoadTypeMediaStream) {
     MediaStreamDescriptor* const descriptor =
-        element.currentSrc().IsNull()
+        element.currentSrc().IsEmpty()
             ? element.GetSrcObject()
             : MediaStreamRegistry::Registry().LookupMediaStreamDescriptor(
                   element.currentSrc().GetString());
