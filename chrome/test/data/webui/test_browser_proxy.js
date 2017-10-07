7222babf5ef20013a08813bc175ad9a2080c4e7a
Dave Schuyler <dschuyler@chromium.org>
2017-10-07 04:09:15 +0000

    [test webui] provide hints for test_browser_proxy mistakes
    This CL adds more explanation for errors on mistyped TestBrowserProxy
    calls.
    
    Bug: None
    Change-Id: I990c23a643ee0adcff27c13cb0b37e36fc36f965
    Reviewed-on: https://chromium-review.googlesource.com/706098
    Commit-Queue: Dave Schuyler <dschuyler@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507280}

--- chrome/test/data/webui/test_browser_proxy.js
+++ chrome/test/data/webui/test_browser_proxy.js
@@ -39,7 +39,9 @@ class TestBrowserProxy {
   constructor(methodNames) {
     /** @private {!Map<string, !PromiseResolver>} */
     this.resolverMap_ = new Map();
-    methodNames.forEach(this.resetResolver, this);
+    methodNames.forEach((methodName) => {
+      this.resolverMap_.set(methodName, new PromiseResolver());
+    });
   }
 
   /**
@@ -52,7 +54,7 @@ class TestBrowserProxy {
    * @protected
    */
   methodCalled(methodName, opt_arg) {
-    this.resolverMap_.get(methodName).resolve(opt_arg);
+    this.getResolver_(methodName).resolve(opt_arg);
   }
 
   /**
@@ -61,7 +63,7 @@ class TestBrowserProxy {
    *     is called.
    */
   whenCalled(methodName) {
-    return this.resolverMap_.get(methodName).promise;
+    return this.getResolver_(methodName).promise;
   }
 
   /**
@@ -69,6 +71,7 @@ class TestBrowserProxy {
    * @param {string} methodName
    */
   resetResolver(methodName) {
+    assert(!!this.resolverMap_.get(methodName), `'${methodName}' not found`);
     this.resolverMap_.set(methodName, new PromiseResolver());
   }
 
@@ -76,8 +79,20 @@ class TestBrowserProxy {
    * Resets all PromiseResolvers.
    */
   reset() {
-    this.resolverMap_.forEach(function(value, methodName) {
+    this.resolverMap_.forEach((value, methodName) => {
       this.resolverMap_.set(methodName, new PromiseResolver());
-    }.bind(this));
+    });
+  }
+
+  /**
+   * Try to give programmers help with mistyped methodNames.
+   * @param {string} methodName
+   * @private
+   */
+  getResolver_(methodName) {
+    let method = this.resolverMap_.get(methodName);
+    // Tip: check that the |methodName| is being passed to |this.constructor|.
+    assert(!!method, `Method '${methodName}' not found in TestBrowserProxy.`);
+    return method;
   }
 }
