464853ce2c172616651ba98f1fc25c7a94c2157d
Mehran Mahmoudi <mahmoudi@chromium.org>
2017-10-07 01:34:18 +0000

    [TTS] Add In-Product Help backend for CS
    Add four IPH features for:
    1. Users who externally search queries instead of using CS
    2. Users who keep longpressing instead of tapping
    3. Users who do not open the CS panel
    4. Users who have not opted-in for CS
    
    This CL only includes backend and logging changes, with no user-visible features.
    
    Bug: 771750
    Change-Id: I86a08e802f829271142dc204ef9dc41e73a9c153
    Reviewed-on: https://chromium-review.googlesource.com/700981
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Commit-Queue: Mehran Mahmoudi <mahmoudi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507259}

--- chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManager.java
@@ -31,6 +31,7 @@ import org.chromium.chrome.browser.contextualsearch.ContextualSearchSelectionCon
 import org.chromium.chrome.browser.externalnav.ExternalNavigationHandler;
 import org.chromium.chrome.browser.externalnav.ExternalNavigationHandler.OverrideUrlLoadingResult;
 import org.chromium.chrome.browser.externalnav.ExternalNavigationParams;
+import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
 import org.chromium.chrome.browser.gsa.GSAContextDisplaySelection;
 import org.chromium.chrome.browser.infobar.InfoBarContainer;
 import org.chromium.chrome.browser.tab.Tab;
@@ -44,6 +45,8 @@ import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.tabmodel.TabModelSelectorTabObserver;
 import org.chromium.chrome.browser.widget.findinpage.FindToolbarManager;
 import org.chromium.chrome.browser.widget.findinpage.FindToolbarObserver;
+import org.chromium.components.feature_engagement.EventConstants;
+import org.chromium.components.feature_engagement.Tracker;
 import org.chromium.components.navigation_interception.NavigationParams;
 import org.chromium.content.browser.ContentViewCore;
 import org.chromium.content.browser.SelectionClient;
@@ -501,6 +504,12 @@ public class ContextualSearchManager implements ContextualSearchManagementDelega
 
         assert mSelectionController.getSelectionType() != SelectionType.UNDETERMINED;
         mWasActivatedByTap = mSelectionController.getSelectionType() == SelectionType.TAP;
+
+        Tracker tracker =
+                TrackerFactory.getTrackerForProfile(mActivity.getActivityTab().getProfile());
+        tracker.notifyEvent(mWasActivatedByTap
+                        ? EventConstants.CONTEXTUAL_SEARCH_TRIGGERED_BY_TAP
+                        : EventConstants.CONTEXTUAL_SEARCH_TRIGGERED_BY_LONGPRESS);
     }
 
     @Override
@@ -701,6 +710,12 @@ public class ContextualSearchManager implements ContextualSearchManagementDelega
         boolean quickActionShown =
                 mSearchPanel.getSearchBarControl().getQuickActionControl().hasQuickAction();
         boolean receivedContextualCardsEntityData = !quickActionShown && receivedCaptionOrThumbnail;
+
+        if (receivedContextualCardsEntityData) {
+            Tracker tracker =
+                    TrackerFactory.getTrackerForProfile(mActivity.getActivityTab().getProfile());
+            tracker.notifyEvent(EventConstants.CONTEXTUAL_SEARCH_ENTITY_RESULT);
+        }
 
         ContextualSearchUma.logContextualCardsDataShown(receivedContextualCardsEntityData);
         mSearchPanel.getPanelMetrics().setWasContextualCardsDataShown(
