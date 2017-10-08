be257ccd9323df156372f13fc25385bbc0192738
tzik <tzik@chromium.org>
2017-10-08 23:26:14 +0000

    Use DCHECK instead of assert on scoped_refptr assertions
    assert() here is less informative than DCHECK on a failure.
    
    Change-Id: Ia0ca2b673657368d31f7d637e8ee93a03de05c47
    Reviewed-on: https://chromium-review.googlesource.com/706875
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507310}

--- base/memory/ref_counted.h
+++ base/memory/ref_counted.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
-#include <cassert>
 #include <iosfwd>
 #include <type_traits>
 
@@ -576,12 +575,12 @@ class scoped_refptr {
   T* get() const { return ptr_; }
 
   T& operator*() const {
-    assert(ptr_ != nullptr);
+    DCHECK(ptr_);
     return *ptr_;
   }
 
   T* operator->() const {
-    assert(ptr_ != nullptr);
+    DCHECK(ptr_);
     return ptr_;
   }
 
