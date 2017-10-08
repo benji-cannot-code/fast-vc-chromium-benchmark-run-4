837c0267ace2063d31eb25ff5a6050c4ab593083
Peiyong Lin <lpy@chromium.org>
2017-10-08 02:50:30 +0000

    Fix crashes when DOMContentLoaded is fired synchronously.
    When using JavaScript to attach an existing iframe again when
    DOMContentLoaded is fired, it triggers a series of DOMContentLoaded
    events fired while LocalFrame is being detached, which results in
    Use-After-Poison bug because we re-add IdlenessDetector to scheduler
    when DOMContentLoaded is fired, an IdlenessDetector from LocalFrame that
    is being detached may get added again.
    
    This patch sets local_frame_ to nullptr in IdlenessDetector explicitly
    when LocalFrame shuts down IdlenessDetector.
    
    BUG=772257
    
    Change-Id: I466bf898a6ba0ba7387af4e5ca8f35ea2f699f7b
    Reviewed-on: https://chromium-review.googlesource.com/706182
    Commit-Queue: lpy <lpy@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507303}

--- third_party/WebKit/Source/core/loader/IdlenessDetector.cpp
+++ third_party/WebKit/Source/core/loader/IdlenessDetector.cpp
@@ -16,17 +16,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 void IdlenessDetector::Shutdown() {
-  if (!started_)
-    return;
-  network_quiet_timer_.Stop();
-  Platform::Current()->CurrentThread()->RemoveTaskTimeObserver(this);
-  started_ = false;
+  Stop();
+  local_frame_ = nullptr;
 }
 
 void IdlenessDetector::DomContentLoadedEventFired() {
-  if (!started_) {
+  if (!local_frame_)
+    return;
+
+  if (!task_observer_added_) {
     Platform::Current()->CurrentThread()->AddTaskTimeObserver(this);
-    started_ = true;
+    task_observer_added_ = true;
   }
 
   network_2_quiet_ = 0;
@@ -41,6 +41,9 @@ void IdlenessDetector::DomContentLoadedEventFired() {
 }
 
 void IdlenessDetector::OnWillSendRequest() {
+  if (!local_frame_)
+    return;
+
   int request_count =
       local_frame_->GetDocument()->Fetcher()->ActiveRequestCount();
   // If we are above the allowed number of active requests, reset timers.
@@ -53,6 +56,9 @@ void IdlenessDetector::OnWillSendRequest() {
 // This function is called when the number of active connections is decreased.
 // Note that the number of active connections doesn't decrease monotonically.
 void IdlenessDetector::OnDidLoadResource() {
+  if (!local_frame_)
+    return;
+
   // Document finishes parsing after DomContentLoadedEventEnd is fired,
   // check the status in order to avoid false signals.
   if (!local_frame_->GetDocument()->HasFinishedParsing())
@@ -115,7 +121,7 @@ void IdlenessDetector::WillProcessTask(double start_time) {
   }
 
   if (network_0_quiet_ < 0 && network_2_quiet_ < 0)
-    ShutdownIfPossible();
+    Stop();
 }
 
 void IdlenessDetector::DidProcessTask(double start_time, double end_time) {
@@ -128,21 +134,18 @@ void IdlenessDetector::DidProcessTask(double start_time, double end_time) {
 
 IdlenessDetector::IdlenessDetector(LocalFrame* local_frame)
     : local_frame_(local_frame),
-      started_(false),
+      task_observer_added_(false),
       network_quiet_timer_(
           TaskRunnerHelper::Get(TaskType::kUnthrottled, local_frame),
           this,
           &IdlenessDetector::NetworkQuietTimerFired) {}
 
-IdlenessDetector::~IdlenessDetector() {
-  Shutdown();
-}
-
-void IdlenessDetector::ShutdownIfPossible() {
-  if (network_0_quiet_ >= 0 || network_2_quiet_ >= 0)
+void IdlenessDetector::Stop() {
+  network_quiet_timer_.Stop();
+  if (!task_observer_added_)
     return;
-
-  Shutdown();
+  Platform::Current()->CurrentThread()->RemoveTaskTimeObserver(this);
+  task_observer_added_ = false;
 }
 
 void IdlenessDetector::NetworkQuietTimerFired(TimerBase*) {
