f42fab5f8cba43540a6e88c459c6bcb916be17d3
Yafei Duan <romax@chromium.org>
2017-10-09 23:22:27 +0000

    [Offline Pages] Implementing TemporaryPagesConsistencyCheckTask.
    Implementing TemporaryPagesConsistencyCheckTask, which is responsible for
    metadata consistency check of temporary Offline Pages. The task will
    clean temporary pages which are saved in abandoned directory or missing
    archive files, as well as the files in temporary archive directory but
    has no associated DB entries.
    This is part of the taskifying OfflinePageModel effort.
    Also Added new unit tests.
    
    Bug: 753595
    Change-Id: I12d45e55813ef40a20209859047367b042525dea
    Reviewed-on: https://chromium-review.googlesource.com/691094
    Commit-Queue: Yafei Duan <romax@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507530}

--- components/offline_pages/core/model/mark_page_accessed_task.h
+++ components/offline_pages/core/model/mark_page_accessed_task.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_OFFLINE_PAGES_CORE_MODEL_MARK_PAGE_ACCESSED_TASK_H_
 
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "components/offline_pages/core/task.h"
 
 namespace base {
@@ -31,12 +32,15 @@ class MarkPageAccessedTask : public Task {
   void Run() override;
 
  private:
+  void OnMarkPageAccessedDone(bool result);
+
   // The metadata store used to update the page. Not owned.
   OfflinePageMetadataStoreSQL* store_;
 
   int64_t offline_id_;
   base::Time access_time_;
 
+  base::WeakPtrFactory<MarkPageAccessedTask> weak_ptr_factory_;
   DISALLOW_COPY_AND_ASSIGN(MarkPageAccessedTask);
 };
 
