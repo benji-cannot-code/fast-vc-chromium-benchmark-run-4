8260b46366aa576a386c7b010bc1a3243b82a9c3
Steven Bennetts <stevenjb@chromium.org>
2017-10-09 19:31:59 +0000

    Network config: Add password show/hide icon, fix layout
    This CL:
    * Adds an icon to the right of password input fields to show/hide
      the password.
    * Adds a margin to the right of the config element to make space
      for the password icons.
    * Fixes the height of rows and other minor layout issues.
    * Fixes focus on the config element.
    
    Bug: 380937
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I6f6dfd1cb8a1382bead5e43a43b5f33353c00096
    Reviewed-on: https://chromium-review.googlesource.com/701360
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507447}

--- ui/webui/resources/cr_components/chromeos/network/compiled_resources2.gyp
+++ ui/webui/resources/cr_components/chromeos/network/compiled_resources2.gyp
@@ -36,7 +36,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     },
     {
       'target_name': 'network_config_input',
-      'dependencies': [],
+      'dependencies': [
+        '<(DEPTH)/ui/webui/resources/js/compiled_resources2.gyp:i18n_behavior',
+      ],
       'includes': ['../../../../../../third_party/closure_compiler/compile_js2.gypi'],
     },
     {
--- ui/webui/resources/cr_components/chromeos/network/network_config_input.js
+++ ui/webui/resources/cr_components/chromeos/network/network_config_input.js
@@ -9,14 +9,60 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'network-config-input',
 
+  behaviors: [I18nBehavior],
+
   properties: {
     label: String,
 
-    disabled: Boolean,
+    disabled: {
+      type: Boolean,
+      reflectToAttribute: true,
+    },
 
     value: {
       type: String,
       notify: true,
-    }
+    },
+
+    password: Boolean,
+
+    showPassword: {
+      type: Boolean,
+      value: false,
+    },
+  },
+
+  focus: function() {
+    this.$$('input').focus();
+  },
+
+  /**
+   * @return {string}
+   * @private
+   */
+  getInputType_: function() {
+    return (this.password && !this.showPassword) ? 'password' : 'text';
+  },
+
+  /**
+   * @return {string}
+   * @private
+   */
+  getIconClass_: function() {
+    return this.showPassword ? 'icon-visibility-off' : 'icon-visibility';
+  },
+
+  /**
+   * @return {string}
+   * @private
+   */
+  getShowPasswordTitle_: function() {
+    return this.showPassword ? this.i18n('hidePassword') :
+                               this.i18n('showPassword');
+  },
+
+  /** @private */
+  onShowPasswordTap_: function() {
+    this.showPassword = !this.showPassword;
   },
 });
