834f8ba1fc7d72a1ef5f65bae56a2caa80aa78f2
Devlin Cronin <rdevlin.cronin@chromium.org>
2017-10-09 19:40:55 +0000

    [Extensions Bindings] Use stronger context invalidation
    It's apparently possible for script to between the time we call
    WillReleaseScriptContext() and when we collect the gin::PerContextData,
    which means that using gin::PerContextData as a flag for if a context
    is valid is insufficient.
    
    Introduce binding::IsContextValid() and binding::InvalidateContext()
    methods to check if a context is valid and invalidate a context, and
    use these for the IsContextValid() checks in APIBinding.
    
    Bug: 772071
    Change-Id: Ie835ec2bebc24f9767bb645015bb6f07c114d227
    Reviewed-on: https://chromium-review.googlesource.com/703294
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507450}

--- extensions/renderer/bindings/api_bindings_system.cc
+++ extensions/renderer/bindings/api_bindings_system.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/values.h"
 #include "extensions/renderer/bindings/api_binding_hooks.h"
+#include "extensions/renderer/bindings/api_binding_util.h"
 
 namespace extensions {
 
@@ -137,6 +138,7 @@ void APIBindingsSystem::RegisterCustomType(const std::string& type_name,
 }
 
 void APIBindingsSystem::WillReleaseContext(v8::Local<v8::Context> context) {
+  binding::InvalidateContext(context);
   request_handler_.InvalidateContext(context);
   event_handler_.InvalidateContext(context);
 }
