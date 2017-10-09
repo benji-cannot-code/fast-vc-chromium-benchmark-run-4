d5565e52d50fa6eead4561c501d2fc7428cd3384
Nektarios Paisios <nektar@chromium.org>
2017-10-09 17:54:56 +0000

    Updates the accessibility tree when a spelling marker is added/removed.
    The spell checker runs in the idle step of the document life cycle.
    Modifications that it makes should be reflected in the accessibility tree as soon as
    the related node has been paint invalidated.
    Currently, any mispelling markers that are added or removed are not updated in the accessibility tree,
    unless there is an unrelated update that would cause them to be updated,
    e.g. some more text was typed or the caret got moved.
    This breaks the ability of the NVDA screen reader to play a sound
    as soon as a mispelling is detected.
    R=aleventhal@chromium.org, kojii@chromium.org
    
    Bug: 730977
    Change-Id: Icfb2d724867d7895c3c6ad7964928c778c9ceca1
    Reviewed-on: https://chromium-review.googlesource.com/700175
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507405}

--- third_party/WebKit/Source/core/editing/markers/DocumentMarkerController.cpp
+++ third_party/WebKit/Source/core/editing/markers/DocumentMarkerController.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/markers/DocumentMarkerController.h"
 
 #include <algorithm>
+#include "core/dom/AXObjectCache.h"
 #include "core/dom/Node.h"
 #include "core/dom/NodeTraversal.h"
 #include "core/dom/Text.h"
@@ -107,6 +108,15 @@ void InvalidatePaintForNode(const Node& node) {
 
   node.GetLayoutObject()->SetShouldDoFullPaintInvalidation(
       PaintInvalidationReason::kDocumentMarker);
+
+  // Tell accessibility about the new marker.
+  AXObjectCache* ax_object_cache = node.GetDocument().ExistingAXObjectCache();
+  if (!ax_object_cache)
+    return;
+  // TODO(nektar): Do major refactoring of all AX classes to comply with const
+  // correctness.
+  Node* non_const_node = &const_cast<Node&>(node);
+  ax_object_cache->HandleTextMarkerDataAdded(non_const_node, non_const_node);
 }
 
 }  // namespace
