3f1113e762963ae0b679114b304d4d32d9e89c3e
Miriam Gershenson <mgersh@chromium.org>
2017-10-09 20:50:57 +0000

    Add heap profiling annotations in net/dns/
    Bug: 772538
    Change-Id: Ib4646f90cb1d820ed3568950fb18c29aece90a02
    Reviewed-on: https://chromium-review.googlesource.com/706717
    Reviewed-by: Helen Li <xunjieli@chromium.org>
    Commit-Queue: Miriam Gershenson <mgersh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507476}

--- net/dns/dns_config_service_posix.cc
+++ net/dns/dns_config_service_posix.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/scoped_blocking_call.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
+#include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "net/base/ip_address.h"
 #include "net/base/ip_endpoint.h"
@@ -267,6 +268,8 @@ class DnsConfigServicePosix::Watcher {
   }
 
   void OnConfigChangedDelayed(bool succeeded) {
+    TRACE_HEAP_PROFILER_API_SCOPED_TASK_EXECUTION scoped_heap_context(
+        "net/dns/configchanged");
     service_->OnConfigChanged(succeeded);
   }
 
@@ -298,6 +301,8 @@ class DnsConfigServicePosix::ConfigReader : public SerialWorker {
   }
 
   void DoWork() override {
+    TRACE_HEAP_PROFILER_API_SCOPED_TASK_EXECUTION scoped_heap_context(
+        "net/dns/configreader");
     base::TimeTicks start_time = base::TimeTicks::Now();
     ConfigParsePosixResult result = ReadDnsConfig(&dns_config_);
     if (dns_config_for_testing_) {
@@ -324,6 +329,8 @@ class DnsConfigServicePosix::ConfigReader : public SerialWorker {
 
   void OnWorkFinished() override {
     DCHECK(!IsCancelled());
+    TRACE_HEAP_PROFILER_API_SCOPED_TASK_EXECUTION scoped_heap_context(
+        "net/dns/configreaderfinished");
     if (success_) {
       service_->OnConfigRead(dns_config_);
     } else {
@@ -359,6 +366,8 @@ class DnsConfigServicePosix::HostsReader : public SerialWorker {
   ~HostsReader() override {}
 
   void DoWork() override {
+    TRACE_HEAP_PROFILER_API_SCOPED_TASK_EXECUTION scoped_heap_context(
+        "net/dns/hostsreader");
     base::TimeTicks start_time = base::TimeTicks::Now();
     base::ScopedBlockingCall scoped_blocking_call(
         base::BlockingType::MAY_BLOCK);
@@ -369,6 +378,8 @@ class DnsConfigServicePosix::HostsReader : public SerialWorker {
   }
 
   void OnWorkFinished() override {
+    TRACE_HEAP_PROFILER_API_SCOPED_TASK_EXECUTION scoped_heap_context(
+        "net/dns/hostsreaderfinished");
     if (success_) {
       service_->OnHostsRead(hosts_);
     } else {
