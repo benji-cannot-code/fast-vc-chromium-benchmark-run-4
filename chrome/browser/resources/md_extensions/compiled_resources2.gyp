a3d2200e3cf36047339513c3f6979de9b5340dbe
dpapad <dpapad@chromium.org>
2017-10-09 19:03:29 +0000

    MD Extensions: Convert remaining classes to ES6 syntax.
    Bug: None
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I404a7fc2d46a7fada644c132f3941e680ee0d5c5
    Reviewed-on: https://chromium-review.googlesource.com/706378
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507436}

--- chrome/browser/resources/md_extensions/compiled_resources2.gyp
+++ chrome/browser/resources/md_extensions/compiled_resources2.gyp
@@ -174,7 +174,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     {
       'target_name': 'pack_dialog',
       'dependencies': [
-        '<(DEPTH)/ui/webui/resources/js/compiled_resources2.gyp:assert',
         '<(DEPTH)/ui/webui/resources/js/compiled_resources2.gyp:cr',
         '<(EXTERNS_GYP):developer_private',
       ],
@@ -220,7 +219,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     {
       'target_name': 'toolbar',
       'dependencies': [
-        '<(DEPTH)/ui/webui/resources/js/compiled_resources2.gyp:assert',
         '<(DEPTH)/ui/webui/resources/js/compiled_resources2.gyp:cr',
         '<(DEPTH)/ui/webui/resources/js/compiled_resources2.gyp:i18n_behavior',
       ],
--- chrome/browser/resources/md_extensions/item.js
+++ chrome/browser/resources/md_extensions/item.js
@@ -5,57 +5,55 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 cr.define('extensions', function() {
   /** @interface */
-  const ItemDelegate = function() {};
-
-  ItemDelegate.prototype = {
+  class ItemDelegate {
     /** @param {string} id */
-    deleteItem: assertNotReached,
+    deleteItem(id) {}
 
     /**
      * @param {string} id
      * @param {boolean} isEnabled
      */
-    setItemEnabled: assertNotReached,
+    setItemEnabled(id, isEnabled) {}
 
     /**
      * @param {string} id
      * @param {boolean} isAllowedIncognito
      */
-    setItemAllowedIncognito: assertNotReached,
+    setItemAllowedIncognito(id, isAllowedIncognito) {}
 
     /**
      * @param {string} id
      * @param {boolean} isAllowedOnFileUrls
      */
-    setItemAllowedOnFileUrls: assertNotReached,
+    setItemAllowedOnFileUrls(id, isAllowedOnFileUrls) {}
 
     /**
      * @param {string} id
      * @param {boolean} isAllowedOnAllSites
      */
-    setItemAllowedOnAllSites: assertNotReached,
+    setItemAllowedOnAllSites(id, isAllowedOnAllSites) {}
 
     /**
      * @param {string} id
      * @param {boolean} collectsErrors
      */
-    setItemCollectsErrors: assertNotReached,
+    setItemCollectsErrors(id, collectsErrors) {}
 
     /**
      * @param {string} id
      * @param {chrome.developerPrivate.ExtensionView} view
      */
-    inspectItemView: assertNotReached,
+    inspectItemView(id, view) {}
 
     /** @param {string} id */
-    reloadItem: assertNotReached,
+    reloadItem(id) {}
 
     /** @param {string} id */
-    repairItem: assertNotReached,
+    repairItem(id) {}
 
     /** @param {string} id */
-    showItemOptionsPage: assertNotReached,
-  };
+    showItemOptionsPage(id) {}
+  }
 
   const Item = Polymer({
     is: 'extensions-item',
