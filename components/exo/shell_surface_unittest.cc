00581b61c474e8210f04424ee4be82f5ae804cd3
Mitsuru Oshima <oshima@chromium.org>
2017-10-09 05:59:52 +0000

    Add --force-remote-shell-scale to specify the remote-shell client's scale
    The VM was using ARC_UI_SCALE, ARC_LCD_DENSITY which has been
    deprecated, so it needs a way to specify the density used by on ARC++.
    
    Bug: b/66963637
    Test: --remote-shell-scale=2.0 on 1.0x device shoud use 1.5 dsf on Android
    and the size is about the same ratio of 2x device like samus.
    
    Change-Id: I4d73583d3fbb5957110ab6ed97c5cbbe7bfb92b7
    Reviewed-on: https://chromium-review.googlesource.com/686049
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507331}

--- components/exo/shell_surface_unittest.cc
+++ components/exo/shell_surface_unittest.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/surface.h"
 #include "components/exo/test/exo_test_base.h"
 #include "components/exo/test/exo_test_helper.h"
+#include "components/exo/wm_helper.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/window.h"
@@ -90,7 +91,7 @@ class ShellSurfaceBoundsModeTest
     if (IsClientBoundsMode()) {
       return Display().CreateRemoteShellSurface(
           surface, ash::kShellWindowId_DefaultContainer,
-          true /* scale_by_default_scale_factor */);
+          WMHelper::GetInstance()->GetDefaultDeviceScaleFactor());
     } else {
       return Display().CreateShellSurface(surface);
     }
