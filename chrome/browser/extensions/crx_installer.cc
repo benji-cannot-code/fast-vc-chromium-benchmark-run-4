f09fabe485ec5464f2237887f6f30fdb264f1557
Karan Bhatia <karandeepb@chromium.org>
2017-10-09 22:20:26 +0000

    DNR: Save indexed ruleset checksum as part of Extension preferences.
    This CL adds a new extension preference "dnr_ruleset_checksum" to save the
    checksum of the indexed ruleset for the Declarative Net Request API. This
    preference is saved during extension installation for packed and loading for
    unpacked extensions. The checksum is useful to determine whether an indexed
    ruleset exists for an extension and to verify the integrity of the ruleset on
    disk.
    
    Doc=http://go/declarative-net-request (Internal only)
    BUG=696822
    
    Change-Id: I092cc9c113a7ba61562314462f8fcd74bd8a8b11
    Reviewed-on: https://chromium-review.googlesource.com/673829
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507510}

--- chrome/browser/extensions/crx_installer.cc
+++ chrome/browser/extensions/crx_installer.cc
@@ -220,7 +220,7 @@ void CrxInstaller::ConvertUserScriptOnFileThread() {
   }
 
   OnUnpackSuccess(extension->path(), extension->path(), nullptr,
-                  extension.get(), SkBitmap());
+                  extension.get(), SkBitmap(), base::nullopt);
 }
 
 void CrxInstaller::InstallWebApp(const WebApplicationInfo& web_app) {
@@ -245,7 +245,7 @@ void CrxInstaller::ConvertWebAppOnFileThread(
   // TODO(aa): conversion data gets lost here :(
 
   OnUnpackSuccess(extension->path(), extension->path(), nullptr,
-                  extension.get(), SkBitmap());
+                  extension.get(), SkBitmap(), base::nullopt);
 }
 
 CrxInstallError CrxInstaller::AllowInstall(const Extension* extension) {
@@ -424,7 +424,8 @@ void CrxInstaller::OnUnpackSuccess(
     const base::FilePath& extension_dir,
     std::unique_ptr<base::DictionaryValue> original_manifest,
     const Extension* extension,
-    const SkBitmap& install_icon) {
+    const SkBitmap& install_icon,
+    const base::Optional<int>& dnr_ruleset_checksum) {
   DCHECK(installer_task_runner_->RunsTasksInCurrentSequence());
 
   UMA_HISTOGRAM_ENUMERATION("Extensions.UnpackSuccessInstallSource",
@@ -437,6 +438,8 @@ void CrxInstaller::OnUnpackSuccess(
 
   extension_ = extension;
   temp_dir_ = temp_dir;
+  dnr_ruleset_checksum_ = dnr_ruleset_checksum;
+
   if (!install_icon.empty())
     install_icon_.reset(new SkBitmap(install_icon));
 
@@ -833,8 +836,8 @@ void CrxInstaller::ReportSuccessFromUIThread() {
     }
   }
 
-  service_weak_->OnExtensionInstalled(
-      extension(), page_ordinal_, install_flags_);
+  service_weak_->OnExtensionInstalled(extension(), page_ordinal_,
+                                      install_flags_, dnr_ruleset_checksum_);
   NotifyCrxInstallComplete(true);
 }
 
--- extensions/browser/extension_prefs.h
+++ extensions/browser/extension_prefs.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/observer_list.h"
+#include "base/optional.h"
 #include "base/strings/string_piece_forward.h"
 #include "base/time/time.h"
 #include "base/values.h"
@@ -25,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/constants.h"
 #include "extensions/common/disable_reason.h"
 #include "extensions/common/extension.h"
+#include "extensions/common/extension_id.h"
 #include "extensions/common/url_pattern_set.h"
 #include "services/preferences/public/cpp/dictionary_value_update.h"
 #include "services/preferences/public/cpp/scoped_pref_update.h"
@@ -192,21 +194,21 @@ class ExtensionPrefs : public ExtensionScopedPrefs, public KeyedService {
   // Called when an extension is installed, so that prefs get created.
   // If |page_ordinal| is invalid then a page will be found for the App.
   // |install_flags| are a bitmask of extension::InstallFlags.
+  // |dnr_ruleset_checksum| is the checksum for the indexed ruleset
+  // corresponding to the Declarative Net Request API.
   void OnExtensionInstalled(const Extension* extension,
                             Extension::State initial_state,
                             const syncer::StringOrdinal& page_ordinal,
                             int install_flags,
-                            const std::string& install_parameter);
-  // OnExtensionInstalled with no install flags.
+                            const std::string& install_parameter,
+                            const base::Optional<int>& dnr_ruleset_checksum);
+  // OnExtensionInstalled with no install flags and |dnr_ruleset_checksum|.
   void OnExtensionInstalled(const Extension* extension,
                             Extension::State initial_state,
                             const syncer::StringOrdinal& page_ordinal,
                             const std::string& install_parameter) {
-    OnExtensionInstalled(extension,
-                         initial_state,
-                         page_ordinal,
-                         kInstallFlagNone,
-                         install_parameter);
+    OnExtensionInstalled(extension, initial_state, page_ordinal,
+                         kInstallFlagNone, install_parameter, base::nullopt);
   }
 
   // Called when an extension is uninstalled, so that prefs get cleaned up.
@@ -437,12 +439,14 @@ class ExtensionPrefs : public ExtensionScopedPrefs, public KeyedService {
   // to install it.
   //
   // |install_flags| are a bitmask of extension::InstallFlags.
-  void SetDelayedInstallInfo(const Extension* extension,
-                             Extension::State initial_state,
-                             int install_flags,
-                             DelayReason delay_reason,
-                             const syncer::StringOrdinal& page_ordinal,
-                             const std::string& install_parameter);
+  void SetDelayedInstallInfo(
+      const Extension* extension,
+      Extension::State initial_state,
+      int install_flags,
+      DelayReason delay_reason,
+      const syncer::StringOrdinal& page_ordinal,
+      const std::string& install_parameter,
+      const base::Optional<int>& dnr_ruleset_checksum = base::nullopt);
 
   // Removes any delayed install information we have for the given
   // |extension_id|. Returns true if there was info to remove; false otherwise.
@@ -553,6 +557,12 @@ class ExtensionPrefs : public ExtensionScopedPrefs, public KeyedService {
   // changed before Sync is ready.
   bool NeedsSync(const std::string& extension_id) const;
   void SetNeedsSync(const std::string& extension_id, bool needs_sync);
+
+  // Returns false if there is no ruleset checksum corresponding to
+  // |extension_id|. On success, returns true and populates
+  // |dnr_ruleset_checksum|.
+  bool GetDNRRulesetChecksum(const ExtensionId& extension_id,
+                             int* dnr_ruleset_checksum) const;
 
   // When called before the ExtensionService is created, alerts that are
   // normally suppressed in first run will still trigger.
@@ -676,6 +686,7 @@ class ExtensionPrefs : public ExtensionScopedPrefs, public KeyedService {
       Extension::State initial_state,
       int install_flags,
       const std::string& install_parameter,
+      const base::Optional<int>& dnr_ruleset_checksum,
       prefs::DictionaryValueUpdate* extension_dict) const;
 
   void InitExtensionControlledPrefs(ExtensionPrefValueMap* value_map);
