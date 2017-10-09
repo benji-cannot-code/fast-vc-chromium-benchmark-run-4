746ceafe332cc102a4ac3b83e8183a0a9e5c6efe
Mitsuru Oshima <oshima@chromium.org>
2017-10-09 14:18:36 +0000

    TabletMode should default to mirror mode
    * Enable mirror when switching to tablet mode
     This is done in DisplayConfigurationObserver so that we can safely disable
     saving to preference.
    * Use mirror mode when an external display is added in tablet mode
    
    Bug: 752181
    Change-Id: I46dea6dd7ad0ac72234a4ab153f52ead21d80cc8
    Reviewed-on: https://chromium-review.googlesource.com/690704
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507369}

--- chrome/browser/chromeos/display/display_configuration_observer.cc
+++ chrome/browser/chromeos/display/display_configuration_observer.cc
@@ -7,9 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/display/window_tree_host_manager.h"
 #include "ash/shell.h"
+#include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "base/command_line.h"
 #include "chrome/browser/chromeos/display/display_preferences.h"
 #include "chromeos/chromeos_switches.h"
+#include "ui/display/manager/display_layout_store.h"
+#include "ui/display/manager/display_manager.h"
 
 namespace chromeos {
 
@@ -18,18 +21,45 @@ DisplayConfigurationObserver::DisplayConfigurationObserver() {
 }
 
 DisplayConfigurationObserver::~DisplayConfigurationObserver() {
+  ash::Shell::Get()->tablet_mode_controller()->RemoveObserver(this);
   ash::Shell::Get()->window_tree_host_manager()->RemoveObserver(this);
 }
 
 void DisplayConfigurationObserver::OnDisplaysInitialized() {
+  ash::Shell::Get()->tablet_mode_controller()->AddObserver(this);
   // Update the display pref with the initial power state.
   base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
-  if (command_line->HasSwitch(chromeos::switches::kFirstExecAfterBoot))
+  if (command_line->HasSwitch(chromeos::switches::kFirstExecAfterBoot) &&
+      save_preference_) {
     StoreDisplayPrefs();
+  }
 }
 
 void DisplayConfigurationObserver::OnDisplayConfigurationChanged() {
-  StoreDisplayPrefs();
+  if (save_preference_)
+    StoreDisplayPrefs();
+}
+
+void DisplayConfigurationObserver::OnTabletModeStarted() {
+  // TODO(oshima): Tablet mode defaults to mirror mode until we figure out
+  // how to handle this scenario, and we shouldn't save this state.
+  // crbug.com/733092.
+  save_preference_ = false;
+  // TODO(oshima): Mirroring won't work with 3 displays. crbug.com/737667.
+  display::DisplayManager* display_manager =
+      ash::Shell::Get()->display_manager();
+  was_in_mirror_mode_ = display_manager->IsInMirrorMode();
+  display_manager->SetMirrorMode(true);
+  display_manager->layout_store()->set_forced_mirror_mode(true);
+}
+
+void DisplayConfigurationObserver::OnTabletModeEnded() {
+  if (!was_in_mirror_mode_)
+    ash::Shell::Get()->display_manager()->SetMirrorMode(false);
+  display::DisplayManager* display_manager =
+      ash::Shell::Get()->display_manager();
+  display_manager->layout_store()->set_forced_mirror_mode(false);
+  save_preference_ = true;
 }
 
 }  // namespace chromeos
