72f6cddb1dcfb0141b8bbbc5bb9c0d85dd2992cf
Vasilii Sukhanov <vasilii@chromium.org>
2017-10-10 11:33:44 +0000

    Implement password selection and viewing on Mac.
    The CL changes the layout of the password bubble on Mac according to the mocks.
    The password selection combobox and eye icon are now fully functional.
    
    Bug: 769666,753806,768781
    Change-Id: Iac6f6186f07bad3da2e5f66407ddc782b61f1842
    Reviewed-on: https://chromium-review.googlesource.com/708261
    Reviewed-by: Tatiana Gornak <melandory@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507639}

--- chrome/browser/ui/cocoa/passwords/save_pending_password_view_controller.h
+++ chrome/browser/ui/cocoa/passwords/save_pending_password_view_controller.h
@@ -11,15 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Manages the view that offers to save the user's password.
 @interface SavePendingPasswordViewController
-    : PendingPasswordViewController<NSTextViewDelegate, NSTouchBarDelegate> {
- @private
-  base::scoped_nsobject<NSButton> saveButton_;
-  base::scoped_nsobject<NSButton> neverButton_;
-  base::scoped_nsobject<NSButton> editButton_;
-  // Container for credentials row. Holds a PendingPasswordItemView instance.
-  base::scoped_nsobject<NSView> passwordItemContainer_;
-  BOOL editMode_;
-}
+    : PendingPasswordViewController<NSTextViewDelegate, NSTouchBarDelegate>
 
 - (NSView*)createPasswordView;
 - (NSArray*)createButtonsAndAddThemToView:(NSView*)view;
@@ -29,10 +21,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @end
 
 @interface SavePendingPasswordViewController (Testing)
+@property(readonly) NSTextField* usernameField;
+@property(readonly) NSTextField* passwordField;
 @property(readonly) NSButton* saveButton;
 @property(readonly) NSButton* neverButton;
-@property(readonly) NSButton* editButton;
-@property(readonly) NSView* passwordItemContainer;
+@property(readonly) NSButton* eyeButton;
 @end
 
 #endif  // CHROME_BROWSER_UI_COCOA_PASSWORDS_SAVE_PENDING_PASSWORD_VIEW_CONTROLLER_H_
