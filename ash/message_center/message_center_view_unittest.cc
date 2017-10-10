2858d22c2f1a64ec42d2b5c1d4bb76bbb6a3c408
Tetsui Ohkubo <tetsui@chromium.org>
2017-10-10 03:30:59 +0000

    Replace MessageCenterButtonBar icons by vector icons.
    As a part of message center restyling, this CL replaces button bar icons
    by new vector ones.
    
    Raster back arrow button (notifier settings -> notification list) is
    still kept, because it is better to remove the button along with new
    settings expand & collapse implementation (see mock).
    
    Font size, color, and paddings are also adjusted following the mock.
    
    This CL changes UI without using switch (i.e. cannot disable the UI
    change by switch).
    
    Screenshot (before): http://screen/xGUuGAjgyyT
    Screenshot (after): http://screen/t98Ajk0yoN1
    Mock: https://crbug.com/726246#c17
    
    TEST=ash_unittests
    BUG=726246
    
    Change-Id: I41767e08df4ac56f0dd8525be44c134945ebabea
    Reviewed-on: https://chromium-review.googlesource.com/697168
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507580}

--- ash/message_center/message_center_view_unittest.cc
+++ ash/message_center/message_center_view_unittest.cc
@@ -869,7 +869,8 @@ TEST_F(MessageCenterViewTest,
 }
 
 TEST_F(MessageCenterViewTest, LockScreen) {
-  const int kLockedMessageCenterViewHeight = 51;
+  // Plain button bar height (56) + border (2)
+  const int kLockedMessageCenterViewHeight = 58;
 
   EXPECT_TRUE(GetNotificationView(kNotificationId1)->IsDrawn());
   EXPECT_TRUE(GetNotificationView(kNotificationId2)->IsDrawn());
@@ -946,7 +947,8 @@ TEST_F(MessageCenterViewTest, LockScreen) {
 }
 
 TEST_F(MessageCenterViewTest, NoNotification) {
-  const int kEmptyMessageCenterViewHeight = 51;
+  // Plain button bar height (56) + border (2)
+  const int kEmptyMessageCenterViewHeight = 58;
 
   GetMessageCenterView()->SizeToPreferredSize();
   EXPECT_NE(kEmptyMessageCenterViewHeight, GetMessageCenterView()->height());
