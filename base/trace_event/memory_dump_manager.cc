563407dadf1a722153dedd0e642d45d06506fd49
Siddhartha <ssid@chromium.org>
2017-10-10 20:39:02 +0000

    Rename kHeapProfilingModeNoStack mode to kHeapProfilingModeBackground
    We would need more information in background traces than just context.
    To enable categories in future, change the mode name to background.
    Use CaptureMode::PSEUDO_STACK in Background mode.
    
    BUG=773188
    
    Change-Id: I2dcb71f554b7084b442a0977dca6413e2235d704
    Reviewed-on: https://chromium-review.googlesource.com/707789
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507760}

--- base/trace_event/memory_dump_manager.cc
+++ base/trace_event/memory_dump_manager.cc
@@ -107,7 +107,7 @@ void NotifyHeapProfilingEnabledOnMDPThread(
 inline bool ShouldEnableMDPAllocatorHooks(HeapProfilingMode mode) {
   return (mode == kHeapProfilingModePseudo) ||
          (mode == kHeapProfilingModeNative) ||
-         (mode == kHeapProfilingModeNoStack);
+         (mode == kHeapProfilingModeBackground);
 }
 
 #if BUILDFLAG(USE_ALLOCATOR_SHIM) && !defined(OS_NACL)
@@ -215,12 +215,12 @@ HeapProfilingMode MemoryDumpManager::GetHeapProfilingModeFromCommandLine() {
   std::string profiling_mode =
       CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
           switches::kEnableHeapProfiling);
+  if (profiling_mode == switches::kEnableHeapProfilingTaskProfiler)
+    return kHeapProfilingModeTaskProfiler;
   if (profiling_mode == switches::kEnableHeapProfilingModePseudo)
     return kHeapProfilingModePseudo;
   if (profiling_mode == switches::kEnableHeapProfilingModeNative)
     return kHeapProfilingModeNative;
-  if (profiling_mode == switches::kEnableHeapProfilingTaskProfiler)
-    return kHeapProfilingModeTaskProfiler;
 #endif  // BUILDFLAG(USE_ALLOCATOR_SHIM) && !defined(OS_NACL)
   return kHeapProfilingModeInvalid;
 }
@@ -258,6 +258,17 @@ bool MemoryDumpManager::EnableHeapProfiling(HeapProfilingMode profiling_mode) {
   }
 
   switch (profiling_mode) {
+    case kHeapProfilingModeTaskProfiler:
+      if (!base::debug::ThreadHeapUsageTracker::IsHeapTrackingEnabled())
+        base::debug::ThreadHeapUsageTracker::EnableHeapTracking();
+      notify_mdps = false;
+      break;
+
+    case kHeapProfilingModeBackground:
+      AllocationContextTracker::SetCaptureMode(
+          AllocationContextTracker::CaptureMode::PSEUDO_STACK);
+      break;
+
     case kHeapProfilingModePseudo:
       AllocationContextTracker::SetCaptureMode(
           AllocationContextTracker::CaptureMode::PSEUDO_STACK);
@@ -269,17 +280,6 @@ bool MemoryDumpManager::EnableHeapProfiling(HeapProfilingMode profiling_mode) {
       // using base::debug::StackTrace, which may be slow.
       AllocationContextTracker::SetCaptureMode(
           AllocationContextTracker::CaptureMode::NATIVE_STACK);
-      break;
-
-    case kHeapProfilingModeNoStack:
-      AllocationContextTracker::SetCaptureMode(
-          AllocationContextTracker::CaptureMode::NO_STACK);
-      break;
-
-    case kHeapProfilingModeTaskProfiler:
-      if (!base::debug::ThreadHeapUsageTracker::IsHeapTrackingEnabled())
-        base::debug::ThreadHeapUsageTracker::EnableHeapTracking();
-      notify_mdps = false;
       break;
 
     case kHeapProfilingModeDisabled:
