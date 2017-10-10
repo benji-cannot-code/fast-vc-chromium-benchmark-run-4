9100d2edaf5bf41533125869c23697e054d66386
Varun Khaneja <vakh@chromium.org>
2017-10-10 00:54:11 +0000

    Send database update notification from PVer4 code
    Bug: 771697
    
    TBR=dglazkov
    for trivial changes in
      chrome/browser/chrome_notification_types.h
      chrome/browser/extensions/blacklist.cc
    
    Change-Id: If920aeed593d958a48419b36d676a3be999bace1
    Reviewed-on: https://chromium-review.googlesource.com/706204
    Commit-Queue: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507556}

--- chrome/browser/safe_browsing/local_database_manager.cc
+++ chrome/browser/safe_browsing/local_database_manager.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task_scheduler/post_task.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/prerender/prerender_field_trial.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/safe_browsing/client_side_detection_service.h"
@@ -37,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/safe_browsing/common/safe_browsing_prefs.h"
 #include "components/safe_browsing/common/safebrowsing_switches.h"
+#include "components/safe_browsing/db/notification_types.h"
 #include "components/safe_browsing/db/util.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/notification_service.h"
@@ -713,7 +713,7 @@ void LocalSafeBrowsingDatabaseManager::NotifyDatabaseUpdateFinished(
     bool update_succeeded) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   content::NotificationService::current()->Notify(
-      chrome::NOTIFICATION_SAFE_BROWSING_UPDATE_COMPLETE,
+      NOTIFICATION_SAFE_BROWSING_UPDATE_COMPLETE,
       content::Source<SafeBrowsingDatabaseManager>(this),
       content::Details<bool>(&update_succeeded));
 }
--- chrome/browser/safe_browsing/safe_browsing_service_browsertest.cc
+++ chrome/browser/safe_browsing/safe_browsing_service_browsertest.cc
@@ -64,6 +64,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/common/safebrowsing_switches.h"
 #include "components/safe_browsing/db/database_manager.h"
 #include "components/safe_browsing/db/metadata.pb.h"
+#include "components/safe_browsing/db/notification_types.h"
 #include "components/safe_browsing/db/test_database_manager.h"
 #include "components/safe_browsing/db/util.h"
 #include "components/safe_browsing/db/v4_database.h"
@@ -1872,7 +1873,7 @@ class SafeBrowsingDatabaseManagerCookieTest : public InProcessBrowserTest {
 IN_PROC_BROWSER_TEST_F(SafeBrowsingDatabaseManagerCookieTest,
                        TestSBUpdateCookies) {
   content::WindowedNotificationObserver observer(
-      chrome::NOTIFICATION_SAFE_BROWSING_UPDATE_COMPLETE,
+      NOTIFICATION_SAFE_BROWSING_UPDATE_COMPLETE,
       content::Source<SafeBrowsingDatabaseManager>(
           sb_factory_->test_safe_browsing_service()->database_manager().get()));
   BrowserThread::PostTask(
