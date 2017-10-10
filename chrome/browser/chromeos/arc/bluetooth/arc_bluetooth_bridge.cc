28ccd55c068778d1e177e0e8ae2dada44a3db0ea
Sonny Sasaka <sonnysasaka@chromium.org>
2017-10-10 22:12:53 +0000

    Add debug logs to places where bluetooth power could be set.
    Recent bugs cause Chrome to unexpectedly set bluetooth power on/off and
    it is impractical to reproduce intentionally. This CL adds debug info
    to help investigate when this happens in the future. This uses warning
    logs so that it can be investigated in feedback report.
    
    BUG=772561
    TEST=check that the logs appear.
    
    Change-Id: Ib62de08fc6b2df812a18ca6e6395d154892bc653
    Reviewed-on: https://chromium-review.googlesource.com/706209
    Commit-Queue: Sonny Sasaka <sonnysasaka@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Rahul Chaturvedi <rkc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507789}

--- chrome/browser/chromeos/arc/bluetooth/arc_bluetooth_bridge.cc
+++ chrome/browser/chromeos/arc/bluetooth/arc_bluetooth_bridge.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/arc_bridge_service.h"
 #include "components/arc/arc_browser_context_keyed_service_factory_base.h"
 #include "components/arc/bluetooth/bluetooth_type_converters.h"
+#include "components/device_event_log/device_event_log.h"
 #include "components/prefs/pref_service.h"
 #include "components/user_manager/user_manager.h"
 #include "device/bluetooth/bluetooth_common.h"
@@ -2196,6 +2197,7 @@ void ArcBluetoothBridge::EnqueueRemotePowerChange(
   // the callee interface.
   auto repeating_callback =
       base::AdaptCallbackForRepeating(std::move(callback));
+  BLUETOOTH_LOG(EVENT) << "ARC bluetooth set power: " << turn_on;
   bluetooth_adapter_->SetPowered(
       turn_on,
       base::Bind(turn_on ? &ArcBluetoothBridge::OnPoweredOn
