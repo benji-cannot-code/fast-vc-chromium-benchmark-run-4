5491e9fb54c5a85522e6e64d0b9e8510a147f33e
Steven Bennetts <stevenjb@chromium.org>
2017-10-10 17:55:04 +0000

    CrOS Settings: Network: Update 'allow shared proxies' dialog title
    Bug: 720109
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I47d27ff98a89682dd979e69fc3e52fd381488585
    Reviewed-on: https://chromium-review.googlesource.com/703987
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507711}

--- chrome/browser/resources/settings/internet_page/network_proxy_section.js
+++ chrome/browser/resources/settings/internet_page/network_proxy_section.js
@@ -68,6 +68,17 @@ Polymer({
   },
 
   /**
+   * @param {boolean} allowShared
+   * @param {string} enableStr
+   * @param {string} disableStr
+   * @return {string}
+   * @private
+   */
+  getAllowSharedDialogTitle_: function(allowShared, enableStr, disableStr) {
+    return allowShared ? disableStr : enableStr;
+  },
+
+  /**
    * @return {boolean}
    * @private
    */
