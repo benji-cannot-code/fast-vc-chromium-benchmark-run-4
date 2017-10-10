a5311639200e5ebe8c0a0167857043fcf21047df
stkhapugin@chromium.org <stkhapugin@chromium.org>
2017-10-10 09:59:30 +0000

    Fix editing a new folder's title in new bookmarks on iPhone N Plus.
    This CL dismisses the keyboard when the done button is pressed while
    a folder is being edited in the bookmarks list by calling endEditing:.
    It also tells the bookmark cell to exit the editable state when the
    trait collection of bookmarks changes, i.e. when iPhone N Plus is
    rotated in the middle of the edit.
    
    Bug: 767049
    Change-Id: I57b80972e7a758ccccce4160470d2b4588c5d67a
    Reviewed-on: https://chromium-review.googlesource.com/707112
    Reviewed-by: Marti Wong <martiw@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507626}

--- ios/chrome/browser/ui/bookmarks/bookmark_table_view.mm
+++ ios/chrome/browser/ui/bookmarks/bookmark_table_view.mm
@@ -117,6 +117,10 @@ using IntegerPair = std::pair<NSInteger, NSInteger>;
 // If a new folder is being added currently.
 @property(nonatomic, assign) BOOL addingNewFolder;
 
+// The cell for the newly created folder while its name is being edited. Set
+// to nil once the editing completes. Corresponds to |_editingFolderNode|.
+@property(nonatomic, weak) BookmarkTableCell* editingFolderCell;
+
 @end
 
 @implementation BookmarkTableView
@@ -131,6 +135,7 @@ using IntegerPair = std::pair<NSInteger, NSInteger>;
 @synthesize editing = _editing;
 @synthesize dispatcher = _dispatcher;
 @synthesize addingNewFolder = _addingNewFolder;
+@synthesize editingFolderCell = _editingFolderCell;
 
 + (void)registerBrowserStatePrefs:(user_prefs::PrefRegistrySyncable*)registry {
   registry->RegisterIntegerPref(prefs::kIosBookmarkSigninPromoDisplayedCount,
@@ -310,6 +315,14 @@ using IntegerPair = std::pair<NSInteger, NSInteger>;
   [self.tableView scrollRectToVisible:visibleRect animated:NO];
 }
 
+#pragma mark - UIView
+
+- (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
+  [super traitCollectionDidChange:previousTraitCollection];
+  // Stop edit of current bookmark folder name, if any.
+  [self.editingFolderCell stopEdit];
+}
+
 #pragma mark - UITableViewDataSource
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView*)tableView {
@@ -365,6 +378,7 @@ using IntegerPair = std::pair<NSInteger, NSInteger>;
   if (node == _editingFolderNode) {
     // Delay starting edit, so that the cell is fully created.
     dispatch_async(dispatch_get_main_queue(), ^{
+      self.editingFolderCell = cell;
       [cell startEdit];
       cell.textDelegate = self;
     });
@@ -461,6 +475,7 @@ using IntegerPair = std::pair<NSInteger, NSInteger>;
       // if editing folder name, cancel it.
       if (_editingFolderNode) {
         _editingFolderNode = NULL;
+        self.editingFolderCell = nil;
         self.addingNewFolder = NO;
         [self refreshContents];
       }
@@ -1008,6 +1023,7 @@ using IntegerPair = std::pair<NSInteger, NSInteger>;
                                  base::SysNSStringToUTF16(newName));
   }
   _editingFolderNode = NULL;
+  self.editingFolderCell = nil;
   [self refreshContents];
 }
 
