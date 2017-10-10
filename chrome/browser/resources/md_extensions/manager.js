b476fddc528d50f20c6db2f474d5e68a329f3874
Demetrios Papadopoulos <dpapad@chromium.org>
2017-10-10 07:09:36 +0000

    MD Extensions: Remove navigation listeners when element gets detached.
    Currently even when <extensions-manager> was detached from the page, it kept
    receiving navigation updates, which causes test failures in optimized mode.
    
    Also changing navigation_helper_test to use a new NavigationHelper instance,
    instead of re-using the existing singleton instance, which is more error prone.
    
    This is in preparation for turning on optimized_webui for MD Extensions.
    
    Bug: 764126
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I0a2b553f2e1eda7e4588ff6b78b10e7a6127e3b8
    Reviewed-on: https://chromium-review.googlesource.com/707916
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507602}

--- chrome/browser/resources/md_extensions/manager.js
+++ chrome/browser/resources/md_extensions/manager.js
@@ -125,6 +125,13 @@ cr.define('extensions', function() {
      */
     currentPage_: null,
 
+    /**
+     * The ID of the listner on |extensions.navigation|. Stored so that the
+     * listener can be removed when this element is detached (happens in tests).
+     * @private {?number}
+     */
+    navigationListener_: null,
+
     /** @override */
     created: function() {
       this.readyPromiseResolver = new PromiseResolver();
@@ -135,9 +142,6 @@ cr.define('extensions', function() {
       this.toolbar =
           /** @type {extensions.Toolbar} */ (this.$$('extensions-toolbar'));
       this.readyPromiseResolver.resolve();
-      extensions.navigation.onRouteChanged(newPage => {
-        this.changePage_(newPage);
-      });
 
       // <if expr="chromeos">
       extensions.KioskBrowserProxyImpl.getInstance()
@@ -146,6 +150,19 @@ cr.define('extensions', function() {
             this.kioskEnabled_ = params.kioskEnabled;
           });
       // </if>
+    },
+
+    /** @override */
+    attached: function() {
+      this.navigationListener_ = extensions.navigation.addListener(newPage => {
+        this.changePage_(newPage);
+      });
+    },
+
+    /** @override */
+    detached: function() {
+      assert(extensions.navigation.removeListener(this.navigationListener_));
+      this.navigationListener_ = null;
     },
 
     get keyboardShortcuts() {
