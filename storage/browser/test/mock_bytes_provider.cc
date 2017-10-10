a6c557400a535000fce5febcaea1cdaec3736571
Francois Doray <fdoray@chromium.org>
2017-10-10 19:35:45 +0000

    Remove WithBaseSyncPrimitives() from storage/browser/blob/.
    The WithBaseSyncPrimitives() trait is deprecated.
    
    ScopedAllowBaseSyncPrimitives(ForTesting|OutsideBlockingScope)
    should be used instead.
    
    Bug: 766678
    Change-Id: I752e53d44d36eb5c6316a83e0f330472ed013b5f
    Reviewed-on: https://chromium-review.googlesource.com/703479
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507742}

--- storage/browser/test/mock_bytes_provider.cc
+++ storage/browser/test/mock_bytes_provider.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "storage/browser/test/mock_bytes_provider.h"
 
+#include "base/threading/thread_restrictions.h"
 #include "mojo/common/data_pipe_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -34,6 +35,7 @@ void MockBytesProvider::RequestAsStream(
     mojo::ScopedDataPipeProducerHandle pipe) {
   if (stream_request_count_)
     ++*stream_request_count_;
+  base::ScopedAllowBaseSyncPrimitivesForTesting allow_base_sync_primitives;
   mojo::common::BlockingCopyFromString(
       std::string(reinterpret_cast<const char*>(data_.data()), data_.size()),
       pipe);
