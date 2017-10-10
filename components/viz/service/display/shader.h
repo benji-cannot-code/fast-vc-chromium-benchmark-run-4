847596606d2bbd0df34cd83bfc9ee5e5aeeb6aa2
Jeremy Roman <jbroman@chromium.org>
2017-10-10 18:04:47 +0000

    viz: Refactor FragmentShader to construct fewer std::strings.
    Should be more efficient at runtime, and saves 8 kB of Android APK size.
    This should also eliminate some static destructors for these strings.
    
    Bug: 770266,341941
    Change-Id: Idb471aa6f22288865ed80b0089c48ad7f8acbe65
    Reviewed-on: https://chromium-review.googlesource.com/648289
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: weiliangc <weiliangc@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507714}

--- components/viz/service/display/shader.h
+++ components/viz/service/display/shader.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/macros.h"
+#include "base/strings/string_piece.h"
 #include "components/viz/service/viz_service_export.h"
 
 namespace gfx {
@@ -234,7 +235,7 @@ class VIZ_SERVICE_EXPORT FragmentShader {
   virtual std::string GetShaderSource() const;
   bool has_blend_mode() const { return blend_mode_ != BLEND_MODE_NONE; }
 
-  std::string SetBlendModeFunctions(const std::string& shader_string) const;
+  void SetBlendModeFunctions(std::string* shader_string) const;
 
   // Settings that are modified by sub-classes.
   AAMode aa_mode_ = NO_AA;
@@ -314,10 +315,10 @@ class VIZ_SERVICE_EXPORT FragmentShader {
  private:
   friend class Program;
 
-  std::string GetHelperFunctions() const;
-  std::string GetBlendFunction() const;
-  std::string GetBlendFunctionBodyForAlpha() const;
-  std::string GetBlendFunctionBodyForRGB() const;
+  void AppendHelperFunctions(std::string* buffer) const;
+  void AppendBlendFunction(std::string* buffer) const;
+  base::StringPiece GetBlendFunctionBodyForAlpha() const;
+  base::StringPiece GetBlendFunctionBodyForRGB() const;
 
   DISALLOW_COPY_AND_ASSIGN(FragmentShader);
 };
