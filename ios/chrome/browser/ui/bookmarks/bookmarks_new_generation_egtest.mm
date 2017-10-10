5d8c3ae6bd8977dae8bbb0c6dc511d40fe011660
Marti Wong <martiw@chromium.org>
2017-10-10 12:49:43 +0000

    Exit selection mode after deletion thru context bar (New iOS bookmark)
    Make Bookmarks leave the selection mode (edit mode) after deletion of
    selected rows.  To be more consistent with History and Reading List.
    
    Bug: 768341, 768279
    Change-Id: I43e66dfd015c62034c5211667900538feb6c0a43
    Reviewed-on: https://chromium-review.googlesource.com/708077
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Marti Wong <martiw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507649}

--- ios/chrome/browser/ui/bookmarks/bookmarks_new_generation_egtest.mm
+++ ios/chrome/browser/ui/bookmarks/bookmarks_new_generation_egtest.mm
@@ -1464,17 +1464,8 @@ id<GREYMatcher> TappableBookmarkNodeWithLabel(NSString* label) {
   [[EarlGrey selectElementWithMatcher:grey_accessibilityID(@"Second URL")]
       assertWithMatcher:grey_notNil()];
 
-  // Verify Delete is disabled.
-  [[EarlGrey selectElementWithMatcher:ContextBarLeadingButtonWithLabel(
-                                          [BookmarksNewGenTestCase
-                                              contextBarDeleteString])]
-      assertWithMatcher:grey_allOf(grey_notNil(),
-                                   grey_accessibilityTrait(
-                                       UIAccessibilityTraitNotEnabled),
-                                   nil)];
-
-  // Cancel edit mode.
-  [BookmarksNewGenTestCase closeContextBarEditMode];
+  // Verify edit mode is closed (context bar back to default state).
+  [self verifyContextBarInDefaultStateWithSelectEnabled:YES];
 }
 
 - (void)testDeleteSingleFolderNode {
@@ -1512,17 +1503,8 @@ id<GREYMatcher> TappableBookmarkNodeWithLabel(NSString* label) {
   [[EarlGrey selectElementWithMatcher:grey_accessibilityID(@"Folder 1")]
       assertWithMatcher:grey_notNil()];
 
-  // Verify Delete is disabled.
-  [[EarlGrey selectElementWithMatcher:ContextBarLeadingButtonWithLabel(
-                                          [BookmarksNewGenTestCase
-                                              contextBarDeleteString])]
-      assertWithMatcher:grey_allOf(grey_notNil(),
-                                   grey_accessibilityTrait(
-                                       UIAccessibilityTraitNotEnabled),
-                                   nil)];
-
-  // Cancel edit mode.
-  [BookmarksNewGenTestCase closeContextBarEditMode];
+  // Verify edit mode is closed (context bar back to default state).
+  [self verifyContextBarInDefaultStateWithSelectEnabled:YES];
 }
 
 - (void)testDeleteMultipleNodes {
@@ -1566,8 +1548,8 @@ id<GREYMatcher> TappableBookmarkNodeWithLabel(NSString* label) {
   [[EarlGrey selectElementWithMatcher:grey_accessibilityID(@"Folder 1")]
       assertWithMatcher:grey_notNil()];
 
-  // Cancel edit mode.
-  [BookmarksNewGenTestCase closeContextBarEditMode];
+  // Verify edit mode is closed (context bar back to default state).
+  [self verifyContextBarInDefaultStateWithSelectEnabled:YES];
 }
 
 // Tests that the promo view is only seen at root level and not in any of the
