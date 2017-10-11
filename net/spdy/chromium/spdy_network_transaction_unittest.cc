a3c61aef62108ffd5beb9dde70bc6d5273a0c953
Bence Béky <bnc@chromium.org>
2017-10-11 22:46:16 +0000

    Cleanup in Http2PushPromiseIndex.
    Cleanup like comments, early return, change argument from value to const
    ref etc. from https://crrev.com/c/706930 which was abandoned.
    
    Bug: 554220
    Change-Id: I8462bdf329c601cb01bd8b7c5ac15dd61ba02ae3
    Reviewed-on: https://chromium-review.googlesource.com/713857
    Reviewed-by: Helen Li <xunjieli@chromium.org>
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508149}

--- net/spdy/chromium/spdy_network_transaction_unittest.cc
+++ net/spdy/chromium/spdy_network_transaction_unittest.cc
@@ -4945,7 +4945,6 @@ TEST_F(SpdyNetworkTransactionTest, ServerPushValidCrossOrigin) {
       spdy_session_pool->FindAvailableSession(
           key, /* enable_ip_based_pooling = */ true, log_);
 
-  EXPECT_FALSE(spdy_session->unclaimed_pushed_streams_.empty());
   EXPECT_EQ(1u, spdy_session->unclaimed_pushed_streams_.size());
   EXPECT_EQ(1u,
             spdy_session->unclaimed_pushed_streams_.count(GURL(url_to_push)));
@@ -4960,7 +4959,6 @@ TEST_F(SpdyNetworkTransactionTest, ServerPushValidCrossOrigin) {
   EXPECT_THAT(rv, IsOk());
 
   EXPECT_TRUE(spdy_session->unclaimed_pushed_streams_.empty());
-  EXPECT_EQ(0u, spdy_session->unclaimed_pushed_streams_.size());
 
   HttpResponseInfo response = *trans0->GetResponseInfo();
   EXPECT_TRUE(response.headers);
@@ -5086,7 +5084,6 @@ TEST_F(SpdyNetworkTransactionTest, ServerPushValidCrossOriginWithOpenSession) {
           key0, /* enable_ip_based_pooling = */ true, log_);
 
   EXPECT_TRUE(spdy_session0->unclaimed_pushed_streams_.empty());
-  EXPECT_EQ(0u, spdy_session0->unclaimed_pushed_streams_.size());
 
   HostPortPair host_port_pair1("docs.example.org", 443);
   SpdySessionKey key1(host_port_pair1, ProxyServer::Direct(),
@@ -5095,7 +5092,6 @@ TEST_F(SpdyNetworkTransactionTest, ServerPushValidCrossOriginWithOpenSession) {
       spdy_session_pool->FindAvailableSession(
           key1, /* enable_ip_based_pooling = */ true, log_);
 
-  EXPECT_FALSE(spdy_session1->unclaimed_pushed_streams_.empty());
   EXPECT_EQ(1u, spdy_session1->unclaimed_pushed_streams_.size());
   EXPECT_EQ(1u,
             spdy_session1->unclaimed_pushed_streams_.count(GURL(url_to_push)));
@@ -5111,10 +5107,7 @@ TEST_F(SpdyNetworkTransactionTest, ServerPushValidCrossOriginWithOpenSession) {
   EXPECT_THAT(rv, IsOk());
 
   EXPECT_TRUE(spdy_session0->unclaimed_pushed_streams_.empty());
-  EXPECT_EQ(0u, spdy_session0->unclaimed_pushed_streams_.size());
-
   EXPECT_TRUE(spdy_session1->unclaimed_pushed_streams_.empty());
-  EXPECT_EQ(0u, spdy_session1->unclaimed_pushed_streams_.size());
 
   HttpResponseInfo response0 = *trans0->GetResponseInfo();
   EXPECT_TRUE(response0.headers);
