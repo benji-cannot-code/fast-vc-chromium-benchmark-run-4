99e51dce5c728fed7d253e66b9817a1447182909
Aleks Totic <atotic@chromium.org>
2017-10-11 20:49:19 +0000

    Fix border writing mode mixup
    Update expectations with newly passing tests.
    
    Bug: 740993
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: Ibba8866e0335c851f4e3d6ff2dabc0235f6bee2b
    Reviewed-on: https://chromium-review.googlesource.com/709946
    Commit-Queue: Aleks Totic <atotic@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508103}

--- third_party/WebKit/Source/core/layout/ng/ng_absolute_utils.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_absolute_utils.cc
@@ -469,7 +469,7 @@ void ComputeFullAbsoluteWithChildBlockSize(
   }
   if (style.IsHorizontalWritingMode()) {
     if (child_minmax) {
-      LayoutUnit border_padding = HorizontalBorderPadding(space, style);
+      LayoutUnit border_padding = VerticalBorderPadding(space, style);
       child_minmax.value().min_size -= border_padding;
       child_minmax.value().max_size -= border_padding;
     }
@@ -484,7 +484,7 @@ void ComputeFullAbsoluteWithChildBlockSize(
                             position);
   } else {
     if (child_minmax) {
-      LayoutUnit border_padding = VerticalBorderPadding(space, style);
+      LayoutUnit border_padding = HorizontalBorderPadding(space, style);
       child_minmax.value().min_size -= border_padding;
       child_minmax.value().max_size -= border_padding;
     }
--- third_party/WebKit/Source/core/layout/ng/ng_absolute_utils_test.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_absolute_utils_test.cc
@@ -340,7 +340,7 @@ TEST_F(NGAbsoluteUtilsTest, Vertical) {
   auto_height = LayoutUnit(60);
   ComputeFullAbsoluteWithChildBlockSize(*ltr_space_, *style_, static_position,
                                         auto_height, WTF::nullopt, &p);
-  EXPECT_EQ(*auto_height + border_padding, p.size.height);
+  EXPECT_EQ(*auto_height, p.size.height);
   EXPECT_EQ(LayoutUnit(0), p.inset.top);
 
   // All auto, static position bottom
@@ -392,7 +392,7 @@ TEST_F(NGAbsoluteUtilsTest, Vertical) {
   auto_height = LayoutUnit(60);
   ComputeFullAbsoluteWithChildBlockSize(*ltr_space_, *style_, static_position,
                                         auto_height, WTF::nullopt, &p);
-  EXPECT_EQ(*auto_height + border_padding, p.size.height);
+  EXPECT_EQ(*auto_height, p.size.height);
 
   // Rule 2: top and bottom are unknown.
   SetVerticalStyle(NGAuto, margin_top, height, margin_bottom, NGAuto);
@@ -409,7 +409,7 @@ TEST_F(NGAbsoluteUtilsTest, Vertical) {
   auto_height = LayoutUnit(20);
   ComputeFullAbsoluteWithChildBlockSize(*ltr_space_, *style_, static_position,
                                         auto_height, WTF::nullopt, &p);
-  EXPECT_EQ(border_padding + *auto_height, p.size.height);
+  EXPECT_EQ(border_padding, p.size.height);
 
   // Rule 3: height and bottom are unknown.
   SetVerticalStyle(top, margin_top, NGAuto, margin_bottom, NGAuto);
@@ -417,7 +417,7 @@ TEST_F(NGAbsoluteUtilsTest, Vertical) {
   auto_height = LayoutUnit(70);
   ComputeFullAbsoluteWithChildBlockSize(*ltr_space_, *style_, static_position,
                                         auto_height, WTF::nullopt, &p);
-  EXPECT_EQ(*auto_height + border_padding, p.size.height);
+  EXPECT_EQ(*auto_height, p.size.height);
 
   // Rule 4: top is unknown.
   SetVerticalStyle(NGAuto, margin_top, height, margin_bottom, bottom);
