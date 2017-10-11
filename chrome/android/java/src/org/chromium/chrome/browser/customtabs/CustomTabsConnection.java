7b236885b3c74ccc25cbfcd1ab0b9815f107724d
Yusuf Ozuysal <yusufo@google.com>
2017-10-11 18:31:08 +0000

    Add a way to check whether an app can launch a TrustedWebActivity
    - Adds implementation for validateRelationship async
    - Moves OriginVerifier ownership to SessionParams from
    PostMessageHandler to allow verification to be used by non-postMessage
    code paths.
    -Adds a method to handle initial (sync) checks for launching a
    TrustedWebActivity. The async checks will still need to be handled after
    launch of the Activity with fallback behavior on failure.
    
    Change-Id: I3d2f8a5a95d97edb36ef1abf06276d43e8cbf9fa
    Reviewed-on: https://chromium-review.googlesource.com/693377
    Commit-Queue: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508043}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabsConnection.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabsConnection.java
@@ -695,6 +695,14 @@ public class CustomTabsConnection {
         return null;
     }
 
+    /**
+     * See {@link ClientManager#canSessionLaunchInTrustedWebActivity(CustomTabsSessionToken, Uri)}
+     */
+    protected boolean canSessionLaunchInTrustedWebActivity(
+            CustomTabsSessionToken session, Uri origin) {
+        return mClientManager.canSessionLaunchInTrustedWebActivity(session, origin);
+    }
+
     public int postMessage(CustomTabsSessionToken session, String message, Bundle extras) {
         int result;
         if (!mWarmupHasBeenCalled.get()) result = CustomTabsService.RESULT_FAILURE_DISALLOWED;
@@ -706,6 +714,11 @@ public class CustomTabsConnection {
         result = mClientManager.postMessage(session, message);
         logCall("postMessage", result);
         return result;
+    }
+
+    public boolean validateRelationship(
+            CustomTabsSessionToken sessionToken, int relation, Uri origin, Bundle extras) {
+        return mClientManager.validateRelationship(sessionToken, relation, origin, extras);
     }
 
     /**
