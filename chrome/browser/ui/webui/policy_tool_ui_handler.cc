b862ac2bf214127b9006a7c50fbbfb9280f46f64
Anton Urusov <urusant@google.com>
2017-10-11 17:59:25 +0000

    Improved message about error while logging to disk
    Bug:
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I397dd74e333a81cba3a7b783e61deea93507acd2
    Reviewed-on: https://chromium-review.googlesource.com/663203
    Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Georges Khalil <georgesak@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Anton Urusov <urusant@google.com>
    Cr-Commit-Position: refs/heads/master@{#508029}

--- chrome/browser/ui/webui/policy_tool_ui_handler.cc
+++ chrome/browser/ui/webui/policy_tool_ui_handler.cc
@@ -102,11 +102,11 @@ void PolicyToolUIHandler::SetDefaultSessionName() {
 std::string PolicyToolUIHandler::ReadOrCreateFileCallback() {
   // Create sessions directory, if it doesn't exist yet.
   // If unable to create a directory, just silently return a dictionary
-  // indicating that logging was unsuccessful.
-  // TODO(urusant): add a possibility to disable logging to disk in similar
+  // indicating that saving was unsuccessful.
+  // TODO(urusant): add a possibility to disable saving to disk in similar
   // cases.
   if (!base::CreateDirectory(sessions_dir_))
-    return "{\"logged\": false}";
+    is_saving_enabled_ = false;
 
   // Initialize session name if it is not initialized yet.
   if (session_name_.empty())
@@ -127,11 +127,11 @@ std::string PolicyToolUIHandler::ReadOrCreateFileCallback() {
 
   // Check that the file exists by now. If it doesn't, it means that at least
   // one of the filesystem operations wasn't successful. In this case, return
-  // a dictionary indicating that logging was unsuccessful. Potentially this can
-  // also be the place to disable logging to disk.
-  if (!PathExists(session_path)) {
-    return "{\"logged\": false}";
-  }
+  // a dictionary indicating that saving was unsuccessful. Potentially this can
+  // also be the place to disable saving to disk.
+  if (!PathExists(session_path))
+    is_saving_enabled_ = false;
+
   // Read file contents.
   std::string contents;
   base::ReadFileToString(session_path, &contents);
@@ -145,6 +145,11 @@ std::string PolicyToolUIHandler::ReadOrCreateFileCallback() {
 }
 
 void PolicyToolUIHandler::OnFileRead(const std::string& contents) {
+  // If the saving is disabled, send a message about that to the UI.
+  if (!is_saving_enabled_) {
+    CallJavascriptFunction("policy.Page.disableSaving");
+    return;
+  }
   std::unique_ptr<base::DictionaryValue> value =
       base::DictionaryValue::From(base::JSONReader::Read(contents));
 
@@ -155,15 +160,8 @@ void PolicyToolUIHandler::OnFileRead(const std::string& contents) {
                            base::DictionaryValue());
     CallJavascriptFunction("policy.Page.disableEditing");
   } else {
-    bool logged = false;
-    if (value->GetBoolean("logged", &logged)) {
-      if (!logged) {
-        ShowErrorMessageToUser("errorLoggingDisabled");
-      }
-      value->Remove("logged", nullptr);
-    }
-    // TODO(urusant): convert the policy values so that the types are consistent
-    // with actual policy types.
+    // TODO(urusant): convert the policy values so that the types are
+    // consistent with actual policy types.
     CallJavascriptFunction("policy.Page.setPolicyValues", *value);
     base::PostTaskWithTraitsAndReplyWithResult(
         FROM_HERE, {base::MayBlock(), base::TaskPriority::USER_VISIBLE},
@@ -190,6 +188,7 @@ void PolicyToolUIHandler::ImportFile() {
 void PolicyToolUIHandler::HandleInitializedAdmin(const base::ListValue* args) {
   DCHECK_EQ(0U, args->GetSize());
   AllowJavascript();
+  is_saving_enabled_ = true;
   SendPolicyNames();
   ImportFile();
 }
@@ -214,14 +213,25 @@ void PolicyToolUIHandler::HandleLoadSession(const base::ListValue* args) {
   ImportFile();
 }
 
-void PolicyToolUIHandler::DoUpdateSession(const std::string& contents) {
-  if (base::WriteFile(GetSessionPath(session_name_), contents.c_str(),
-                      contents.size()) < static_cast<int>(contents.size())) {
-    ShowErrorMessageToUser("errorLoggingDisabled");
+bool PolicyToolUIHandler::DoUpdateSession(const std::string& contents) {
+  // Sanity check that contents is not too big. Otherwise, passing it to
+  // WriteFile will be int overflow.
+  if (contents.size() > static_cast<size_t>(std::numeric_limits<int>::max()))
+    return false;
+  int bytes_written = base::WriteFile(GetSessionPath(session_name_),
+                                      contents.c_str(), contents.size());
+  return bytes_written == static_cast<int>(contents.size());
+}
+
+void PolicyToolUIHandler::OnSessionUpdated(bool is_successful) {
+  if (!is_successful) {
+    is_saving_enabled_ = false;
+    CallJavascriptFunction("policy.Page.disableSaving");
   }
 }
 
 void PolicyToolUIHandler::HandleUpdateSession(const base::ListValue* args) {
+  DCHECK(is_saving_enabled_);
   DCHECK_EQ(1U, args->GetSize());
 
   const base::DictionaryValue* policy_values = nullptr;
@@ -229,19 +239,22 @@ void PolicyToolUIHandler::HandleUpdateSession(const base::ListValue* args) {
   DCHECK(policy_values);
   std::string converted_values;
   base::JSONWriter::Write(*policy_values, &converted_values);
-  base::PostTaskWithTraits(
+  base::PostTaskWithTraitsAndReplyWithResult(
       FROM_HERE, {base::MayBlock(), base::TaskPriority::BACKGROUND},
       base::BindOnce(&PolicyToolUIHandler::DoUpdateSession,
-                     callback_weak_ptr_factory_.GetWeakPtr(),
-                     converted_values));
+                     base::Unretained(this), converted_values),
+      base::BindOnce(&PolicyToolUIHandler::OnSessionUpdated,
+                     callback_weak_ptr_factory_.GetWeakPtr()));
 }
 
 void PolicyToolUIHandler::HandleResetSession(const base::ListValue* args) {
   DCHECK_EQ(0U, args->GetSize());
-  base::PostTaskWithTraits(
+  base::PostTaskWithTraitsAndReplyWithResult(
       FROM_HERE, {base::MayBlock(), base::TaskPriority::BACKGROUND},
       base::BindOnce(&PolicyToolUIHandler::DoUpdateSession,
-                     callback_weak_ptr_factory_.GetWeakPtr(), "{}"));
+                     base::Unretained(this), "{}"),
+      base::BindOnce(&PolicyToolUIHandler::OnSessionUpdated,
+                     callback_weak_ptr_factory_.GetWeakPtr()));
 }
 
 void PolicyToolUIHandler::ShowErrorMessageToUser(
