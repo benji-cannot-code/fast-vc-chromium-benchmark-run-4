cdcedd1d63f3b1c592a1ae9ceafa811c23d768f3
Daniel Cheng <dcheng@chromium.org>
2017-10-11 04:55:51 +0000

    Remove ManualConstructor usage from JSON parser.
    Bug: 773351
    Change-Id: Ife80a5466678ccc52c44e16f568193d53638be3a
    Reviewed-on: https://chromium-review.googlesource.com/710056
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507896}

--- base/json/json_parser.h
+++ base/json/json_parser.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/json/json_reader.h"
 #include "base/macros.h"
-#include "base/memory/manual_constructor.h"
+#include "base/optional.h"
 #include "base/strings/string_piece.h"
 
 namespace base {
@@ -103,7 +103,7 @@ class BASE_EXPORT JSONParser {
 
     ~StringBuilder();
 
-    void operator=(StringBuilder&& other);
+    StringBuilder& operator=(StringBuilder&& other);
 
     // Either increases the |length_| of the string or copies the character if
     // the StringBuilder has been converted. |c| must be in the basic ASCII
@@ -137,10 +137,9 @@ class BASE_EXPORT JSONParser {
     // Number of bytes in |pos_| that make up the string being built.
     size_t length_;
 
-    // The copied string representation. Will be uninitialized until Convert()
-    // is called, which will set has_string_ to true.
-    bool has_string_;
-    base::ManualConstructor<std::string> string_;
+    // The copied string representation. Will be unset until Convert() is
+    // called.
+    base::Optional<std::string> string_;
   };
 
   // Quick check that the stream has capacity to consume |length| more bytes.
