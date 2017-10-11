a177d636640669179d9af137570ec7128ded2ae1
Yuki Shiino <yukishiino@chromium.org>
2017-10-11 09:18:33 +0000

    Oilpan: Stop virtual inheritance of GarbageCollectedMixin.
    There is no need to use virtual inheritance, except for resolving
    ambiguity of look-up-by-name of GCMixin's member functions.
    We can resolve the ambiguity by overriding the member functions
    in the classes that inherit two or more GCMixins.
    
    Note: GCMixin is only the case of virtual inheritance in
    Blink codebase.  (Chromium has some amount of usage.)
    
    Bug: 
    Change-Id: Ie53c83dd8732af7f584e89dc90e8a24ccde40a44
    Reviewed-on: https://chromium-review.googlesource.com/697404
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507935}

--- third_party/WebKit/Source/platform/LifecycleNotifier.h
+++ third_party/WebKit/Source/platform/LifecycleNotifier.h
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 template <typename T, typename Observer>
-class LifecycleNotifier : public virtual GarbageCollectedMixin {
+class LifecycleNotifier : public GarbageCollectedMixin {
  public:
   virtual ~LifecycleNotifier();
 
