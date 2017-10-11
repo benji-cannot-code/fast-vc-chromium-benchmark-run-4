c216003261cf834b71a11ec8f2f6aa2e99cd5b56
xdai <xdai@chromium.org>
2017-10-11 19:37:33 +0000

    Cros splitscreen: Exit split view mode when exiting tablet mode.
    We should exit split view mode if it's active when exiting tablet mode.
    
    Bug: 725683
    Change-Id: I95696762f7942f2c1dcf7d2a7fd712abbcb6043a
    Reviewed-on: https://chromium-review.googlesource.com/703484
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508071}

--- ash/wm/splitview/split_view_controller.cc
+++ ash/wm/splitview/split_view_controller.cc
@@ -37,8 +37,9 @@ float FindClosestFixedPositionRatio(float distance, float length) {
   float closest_ratio = 0.f;
   for (float ratio : kFixedPositionRatios) {
     if (std::abs(current_ratio - ratio) <
-        std::abs(current_ratio - closest_ratio))
+        std::abs(current_ratio - closest_ratio)) {
       closest_ratio = ratio;
+    }
   }
   return closest_ratio;
 }
@@ -117,6 +118,7 @@ void SplitViewController::SnapWindow(aura::Window* window,
     Shell::Get()->AddShellObserver(this);
     Shell::Get()->activation_client()->AddObserver(this);
     display::Screen::GetScreen()->AddObserver(this);
+    Shell::Get()->tablet_mode_controller()->AddObserver(this);
     Shell::Get()->NotifySplitViewModeStarting();
 
     screen_orientation_ =
@@ -142,13 +144,12 @@ void SplitViewController::SnapWindow(aura::Window* window,
     left_window_ = (window == left_window_) ? nullptr : left_window_;
   }
 
-  if (left_window_ && right_window_) {
+  if (left_window_ && right_window_)
     state_ = BOTH_SNAPPED;
-  } else if (left_window_) {
+  else if (left_window_)
     state_ = LEFT_SNAPPED;
-  } else if (right_window_) {
+  else if (right_window_)
     state_ = RIGHT_SNAPPED;
-  }
 
   StartObserving(window);
   const wm::WMEvent event((snap_position == LEFT) ? wm::WM_EVENT_SNAP_LEFT
@@ -310,6 +311,7 @@ void SplitViewController::EndSplitView() {
   Shell::Get()->RemoveShellObserver(this);
   Shell::Get()->activation_client()->RemoveObserver(this);
   display::Screen::GetScreen()->RemoveObserver(this);
+  Shell::Get()->tablet_mode_controller()->RemoveObserver(this);
 
   StopObserving(left_window_);
   StopObserving(right_window_);
@@ -464,6 +466,10 @@ void SplitViewController::OnDisplayMetricsChanged(
 
   NotifyDividerPositionChanged();
   UpdateSnappedWindowsAndDividerBounds();
+}
+
+void SplitViewController::OnTabletModeEnding() {
+  EndSplitView();
 }
 
 void SplitViewController::StartObserving(aura::Window* window) {
--- ash/wm/splitview/split_view_controller.h
+++ ash/wm/splitview/split_view_controller.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/shell_observer.h"
+#include "ash/wm/tablet_mode/tablet_mode_observer.h"
 #include "ash/wm/window_state_observer.h"
 #include "base/macros.h"
 #include "base/observer_list.h"
@@ -35,7 +36,8 @@ class ASH_EXPORT SplitViewController : public aura::WindowObserver,
                                        public ash::wm::WindowStateObserver,
                                        public ::wm::ActivationChangeObserver,
                                        public ShellObserver,
-                                       public display::DisplayObserver {
+                                       public display::DisplayObserver,
+                                       public TabletModeObserver {
  public:
   enum State { NO_SNAP, LEFT_SNAPPED, RIGHT_SNAPPED, BOTH_SNAPPED };
 
@@ -126,6 +128,9 @@ class ASH_EXPORT SplitViewController : public aura::WindowObserver,
   // display::DisplayObserver:
   void OnDisplayMetricsChanged(const display::Display& display,
                                uint32_t metrics) override;
+
+  // TabletModeObserver:
+  void OnTabletModeEnding() override;
 
   aura::Window* left_window() { return left_window_; }
   aura::Window* right_window() { return right_window_; }
