de18352e2564aa0e35dc9513fc49ca695d6d5576
Mike Dougherty <michaeldo@chromium.org>
2017-10-11 18:35:34 +0000

    Re-enable testVersion WebUITest.
    Bug: 734079
    Change-Id: I944324018c4b2d1971a3d6c764b57f8350b67a18
    Reviewed-on: https://chromium-review.googlesource.com/710004
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508044}

--- ios/chrome/browser/ui/webui/web_ui_egtest.mm
+++ ios/chrome/browser/ui/webui/web_ui_egtest.mm
@@ -82,8 +82,7 @@ id<GREYMatcher> WaitForOmniboxText(std::string text) {
 
 // Tests that chrome://version renders and contains correct version number and
 // user agent string.
-// TODO(crbug.com/734079): Re-enable this test.
-- (void)DISABLED_testVersion {
+- (void)testVersion {
   LoadWebUIUrl(kChromeUIVersionHost);
 
   // Verify that app version is present on the page.
@@ -97,7 +96,7 @@ id<GREYMatcher> WaitForOmniboxText(std::string text) {
 }
 
 // Tests that chrome://physical-web renders and the page title is present.
-// TODO(crbug.com/734079): Re-enable this test.
+// TODO(crbug.com/760104): Re-enable this test once physical web is re-enabled.
 - (void)DISABLED_testPhysicalWeb {
   // Enable the Physical Web via Chrome variation.
   base::FieldTrialList::CreateFieldTrial("PhysicalWebEnabled", "Enabled");
