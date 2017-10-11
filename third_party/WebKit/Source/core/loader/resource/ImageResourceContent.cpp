09b4c7a82e498cf03fbab43e866a16864a209ac7
Nate Chapin <japhet@chromium.org>
2017-10-11 23:08:37 +0000

    LayoutImage shouldn't need to know about an image's revalidation state
    Bug: 761026
    Change-Id: Ic911b8fcce6f1e37759739f20e8cab154c13ff91
    Reviewed-on: https://chromium-review.googlesource.com/639243
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508158}

--- third_party/WebKit/Source/core/loader/resource/ImageResourceContent.cpp
+++ third_party/WebKit/Source/core/loader/resource/ImageResourceContent.cpp
@@ -623,8 +623,4 @@ const ResourceError& ImageResourceContent::GetResourceError() const {
   return info_->GetResourceError();
 }
 
-bool ImageResourceContent::IsCacheValidator() const {
-  return info_->IsCacheValidator();
-}
-
 }  // namespace blink
