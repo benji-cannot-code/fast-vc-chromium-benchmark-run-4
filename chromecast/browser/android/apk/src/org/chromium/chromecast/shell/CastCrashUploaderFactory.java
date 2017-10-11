a1e9829bda5540944be4425b6f9eff2f88f4a615
Sandeep Vijayasekar <sandv@chromium.org>
2017-10-11 21:37:24 +0000

    [Chromecast] Add ability to upload minidump on demand non-periodicly
    Used for filing feedback through the home app
    BUG=internal b/62371673
    TEST=sent feedback from home app
    
    Change-Id: I40f8694a6711b821e58580f0a8eaca7e69a5b6ef
    Reviewed-on: https://chromium-review.googlesource.com/653557
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Sandeep Vijayasekar <sandv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508124}

---
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastCrashUploaderFactory.java
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chromecast.shell;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+/**
+ * Factory class that creates CastCrashUploader using a ScheduledExecutorService singleton to
+ * schedule all tasks on one thread.
+ */
+public final class CastCrashUploaderFactory {
+    private static ScheduledExecutorService sExecutorService = null;
+
+    public static CastCrashUploader createCastCrashUploader(
+            String crashDumpPath, String uuid, boolean uploadCrashToStaging) {
+        if (sExecutorService == null) {
+            sExecutorService = Executors.newScheduledThreadPool(1);
+        }
+        return new CastCrashUploader(sExecutorService, crashDumpPath, uuid, uploadCrashToStaging);
+    }
+}
