67ff19eb2edde5ed0f69595c35d709e1f48a7a35
Parastoo Geranmayeh <parastoog@google.com>
2017-10-11 17:36:33 +0000

    Do not trigger Android Autofill in Chrome Settings' credit card editor.
    Solution:
    -- Mark the views in Chrome Settings' credit card editor as not
    important for autofill.
    
    Bug: 773504
    Change-Id: I92a3557605f939bb35ae7a58b184ee9b46280a4c
    Reviewed-on: https://chromium-review.googlesource.com/710744
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Parastoo Geranmayeh <parastoog@google.com>
    Cr-Commit-Position: refs/heads/master@{#508018}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/autofill/AutofillCreditCardEditor.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/autofill/AutofillCreditCardEditor.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.preferences.autofill;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -32,6 +33,12 @@ abstract class AutofillCreditCardEditor extends AutofillEditorBase {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View v = super.onCreateView(inflater, container, savedInstanceState);
+
+        // Do not use autofill for the fields.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            getActivity().getWindow().getDecorView().setImportantForAutofill(
+                    View.IMPORTANT_FOR_AUTOFILL_NO_EXCLUDE_DESCENDANTS);
+        }
 
         // Populate the billing address dropdown.
         ArrayAdapter<AutofillProfile> profilesAdapter = new ArrayAdapter<AutofillProfile>(
