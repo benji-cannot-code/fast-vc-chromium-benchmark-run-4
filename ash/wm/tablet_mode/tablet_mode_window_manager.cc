da592dfe41f19ef5059ef286ed2a00443bb3b4bc
xdai <xdai@chromium.org>
2017-10-11 17:38:34 +0000

    Cros splitscreen: Support screen rotation.
    In this CL, if the split view mode is active, the snapped windows and split
    divider's bounds will be adjusted accordingly when the display screen rotates.
    
    Not covered:
    "Block screen rotation while dragging the divider" case is not supported
    in this CL.
    
    Bug: 725683
    Change-Id: I5fdd0930444b9880a817dc1e5d70ec5a1f5bcc69
    Reviewed-on: https://chromium-review.googlesource.com/651571
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508021}

--- ash/wm/tablet_mode/tablet_mode_window_manager.cc
+++ ash/wm/tablet_mode/tablet_mode_window_manager.cc
@@ -50,6 +50,7 @@ TabletModeWindowManager::~TabletModeWindowManager() {
   added_windows_.clear();
   Shell::Get()->RemoveShellObserver(this);
   display::Screen::GetScreen()->RemoveObserver(this);
+  Shell::Get()->split_view_controller()->RemoveObserver(this);
   EnableBackdropBehindTopWindowOnEachDisplay(false);
   RemoveWindowCreationObservers();
   RestoreAllWindows();
@@ -82,11 +83,13 @@ void TabletModeWindowManager::WindowStateDestroyed(aura::Window* window) {
 }
 
 void TabletModeWindowManager::OnOverviewModeStarting() {
-  SetDeferBoundsUpdates(true);
+  for (auto& pair : window_state_map_)
+    SetDeferBoundsUpdates(pair.first, true);
 }
 
 void TabletModeWindowManager::OnOverviewModeEnded() {
-  SetDeferBoundsUpdates(false);
+  for (auto& pair : window_state_map_)
+    SetDeferBoundsUpdates(pair.first, false);
 }
 
 void TabletModeWindowManager::OnSplitViewModeEnded() {
@@ -196,6 +199,26 @@ void TabletModeWindowManager::OnDisplayMetricsChanged(const display::Display&,
   // Nothing to do here.
 }
 
+void TabletModeWindowManager::OnSplitViewStateChanged(
+    SplitViewController::State previous_state,
+    SplitViewController::State state) {
+  // It might be possible that split view mode and overview mode are active at
+  // the same time. We need make sure the snapped windows bounds can be updated
+  // immediately.
+  // TODO(xdai): In overview mode, if we snap two windows to the same position
+  // one by one, we need to restore the first window's |defer_bounds_updates_|
+  // state here. It's unnecessary now since we don't insert the first window
+  // back to the overview grid but we probably should do so in the future.
+  if (state != SplitViewController::NO_SNAP) {
+    SplitViewController* split_view_controller =
+        Shell::Get()->split_view_controller();
+    if (split_view_controller->left_window())
+      SetDeferBoundsUpdates(split_view_controller->left_window(), false);
+    if (split_view_controller->right_window())
+      SetDeferBoundsUpdates(split_view_controller->right_window(), false);
+  }
+}
+
 void TabletModeWindowManager::SetIgnoreWmEventsForExit() {
   for (auto& pair : window_state_map_) {
     pair.second->set_ignore_wm_events(true);
@@ -212,6 +235,7 @@ TabletModeWindowManager::TabletModeWindowManager() {
   EnableBackdropBehindTopWindowOnEachDisplay(true);
   display::Screen::GetScreen()->AddObserver(this);
   Shell::Get()->AddShellObserver(this);
+  Shell::Get()->split_view_controller()->AddObserver(this);
   event_handler_ = ShellPort::Get()->CreateTabletModeEventHandler();
 }
 
@@ -231,9 +255,11 @@ void TabletModeWindowManager::RestoreAllWindows() {
     ForgetWindow(window_state_map_.begin()->first);
 }
 
-void TabletModeWindowManager::SetDeferBoundsUpdates(bool defer_bounds_updates) {
-  for (auto& pair : window_state_map_)
-    pair.second->SetDeferBoundsUpdates(defer_bounds_updates);
+void TabletModeWindowManager::SetDeferBoundsUpdates(aura::Window* window,
+                                                    bool defer_bounds_updates) {
+  auto iter = window_state_map_.find(window);
+  if (iter != window_state_map_.end())
+    iter->second->SetDeferBoundsUpdates(defer_bounds_updates);
 }
 
 void TabletModeWindowManager::MaximizeAndTrackWindow(aura::Window* window) {
