b53fac6d8c8ae7e8e9cf0379685e0735bebc7104
Xiaocheng Hu <xiaochengh@chromium.org>
2017-10-12 01:51:59 +0000

    Add offset range check in PositionTemplate::IsValidFor()
    A stored Position can be invalidated when DOM mutations decreases the
    max offset in its anchor node, making the stored offset invalid.
    
    This patch adds the check to declare such positions as invalid, so that
    we won't be mistakenly using it in, for example, spellchecker.
    
    Bug: 772718
    Change-Id: I4b520c421adf1a3ff903785cbfb430f3e7751a33
    Reviewed-on: https://chromium-review.googlesource.com/707687
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508216}

--- third_party/WebKit/Source/core/editing/Position.cpp
+++ third_party/WebKit/Source/core/editing/Position.cpp
@@ -357,7 +357,10 @@ bool PositionTemplate<Strategy>::IsValidFor(const Document& document) const {
     return true;
   if (GetDocument() != document)
     return false;
-  return IsConnected();
+  if (!IsConnected())
+    return false;
+  return !IsOffsetInAnchor() ||
+         OffsetInContainerNode() <= LastOffsetInNode(*AnchorNode());
 }
 
 int ComparePositions(const PositionInFlatTree& position_a,
