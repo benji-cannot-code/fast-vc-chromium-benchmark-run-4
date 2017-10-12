f9e329fabfcde1e541788faab16966f0315c877e
Xiaocheng Hu <xiaochengh@chromium.org>
2017-10-12 21:00:12 +0000

    [LayoutNG] Add NG alternative of EditCommand::IsRenderedCharacter
    Bug: 771398
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I987ad066c763a53f3de8a86a7ab4d2b87d8e7bb6
    Reviewed-on: https://chromium-review.googlesource.com/715157
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508460}

--- third_party/WebKit/Source/core/editing/commands/EditCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/EditCommand.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/LocalFrame.h"
 #include "core/layout/LayoutText.h"
 #include "core/layout/line/InlineTextBox.h"
+#include "core/layout/ng/inline/ng_offset_mapping_result.h"
 
 namespace blink {
 
@@ -56,19 +57,25 @@ bool EditCommand::IsRenderedCharacter(const Position& position) {
   if (position.IsNull())
     return false;
   DCHECK(position.IsOffsetInAnchor()) << position;
-  if (!position.AnchorNode()->IsTextNode())
+
+  const Node& node = *position.AnchorNode();
+  if (!node.IsTextNode())
     return false;
 
-  LayoutObject* layout_object = position.AnchorNode()->GetLayoutObject();
+  LayoutObject* layout_object = node.GetLayoutObject();
   if (!layout_object || !layout_object->IsText())
     return false;
 
-  // TODO(xiaochengh): When LayoutNG is enabled, we should check offset mapping
-  // instead of legacy InlineTextBoxes.
+  const int offset_in_node = position.OffsetInContainerNode();
+
+  // Use NG offset mapping when LayoutNG is enabled.
+  if (const NGOffsetMappingResult* mapping =
+          GetNGOffsetMappingFor(node, offset_in_node)) {
+    return mapping->IsNonCollapsedCharacter(node, offset_in_node);
+  }
 
   // TODO(editing-dev): This doesn't handle first-letter correctly. Fix it.
   const LayoutText* layout_text = ToLayoutText(layout_object);
-  const int offset_in_node = position.OffsetInContainerNode();
   for (InlineTextBox* box : InlineTextBoxesOf(*layout_text)) {
     if (offset_in_node < static_cast<int>(box->Start()) &&
         !layout_text->ContainsReversedText()) {
