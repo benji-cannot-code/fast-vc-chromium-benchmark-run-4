6b1aa080cc097a23b37929d851a8f913212c717e
Jay Civelli <jcivelli@google.com>
2017-10-12 02:38:53 +0000

    Abstrating file access operation in zip creation.
    Abstracting file access operations when creating ZIP files. This is
    going to be useful when ZIP operations are done by a Mojo service in a
    sandboxed environment and can only access files through IPC.
    
    In order to make the API cleaner, introduced a parameter class that
    lets caller specify which options to use.
    
    Bug: 772815
    Change-Id: I229291a0c37bfee8fd29fe894a8118c91afab074
    Reviewed-on: https://chromium-review.googlesource.com/706282
    Commit-Queue: Jay Civelli <jcivelli@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Chris Blume <cblume@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508233}

--- third_party/zlib/google/zip_internal.h
+++ third_party/zlib/google/zip_internal.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/time/time.h"
 #include "build/build_config.h"
 
 #if defined(OS_WIN)
@@ -60,13 +61,10 @@ zipFile OpenForZipping(const std::string& file_name_utf8, int append_flag);
 zipFile OpenFdForZipping(int zip_fd, int append_flag);
 #endif
 
-// Returns a zip_fileinfo with the last modification date of |path| set.
-zip_fileinfo GetFileInfoForZipping(const base::FilePath& path);
-
 // Wrapper around zipOpenNewFileInZip4 which passes most common options.
 bool ZipOpenNewFileInZip(zipFile zip_file,
                          const std::string& str_path,
-                         const zip_fileinfo* file_info);
+                         base::Time last_modified_time);
 
 const int kZipMaxPath = 256;
 const int kZipBufSize = 8192;
