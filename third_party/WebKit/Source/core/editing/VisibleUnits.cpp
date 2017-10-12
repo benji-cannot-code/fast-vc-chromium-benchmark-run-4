aa1da1e545182b3eb26c84e33a1b942e3397c41b
Xiaocheng Hu <xiaochengh@chromium.org>
2017-10-12 19:57:01 +0000

    Change some callers of LayoutText::HasTextBoxes() to use LayoutNG alternative
    Some callers of LayoutText::HasTextBoxes() do not actually care about text
    boxes, but instead, whether the text node is fully collapsed or not.
    
    Hence, this patch introduces LayoutText::HasNonCollapsedText() that
    - Checks text boxes when LayoutNG is not enabled
    - Checks LayoutNG offset mapping when LayoutNG is enabled
    and change those call sites to use HasNonCollapsedText() instead.
    
    Bug: 771398
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: Iada3978209bffe828a70e0e81d3b189589c8a94d
    Reviewed-on: https://chromium-review.googlesource.com/710860
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508403}

--- third_party/WebKit/Source/core/editing/VisibleUnits.cpp
+++ third_party/WebKit/Source/core/editing/VisibleUnits.cpp
@@ -871,7 +871,7 @@ static bool IsVisuallyEmpty(const LayoutObject* layout) {
       if (ToLayoutInline(child)->FirstLineBoxIncludingCulling())
         return false;
     } else if (child->IsText()) {
-      if (ToLayoutText(child)->HasTextBoxes())
+      if (ToLayoutText(child)->HasNonCollapsedText())
         return false;
     } else {
       return false;
@@ -1015,7 +1015,7 @@ static PositionTemplate<Strategy> MostBackwardCaretPosition(
     if (!layout_object->IsText())
       continue;
     const LayoutText* const text_layout_object = ToLayoutText(layout_object);
-    if (!text_layout_object->FirstTextBox())
+    if (!text_layout_object->HasNonCollapsedText())
       continue;
     const unsigned text_start_offset = text_layout_object->TextStartOffset();
     if (current_node != start_node) {
@@ -1222,7 +1222,7 @@ PositionTemplate<Strategy> MostForwardCaretPosition(
     if (!layout_object->IsText())
       continue;
     const LayoutText* const text_layout_object = ToLayoutText(layout_object);
-    if (!text_layout_object->FirstTextBox())
+    if (!text_layout_object->HasNonCollapsedText())
       continue;
     const unsigned text_start_offset = text_layout_object->TextStartOffset();
     if (current_node != start_node) {
