a5c1b491eaae94c9c81bc742f9da73e126d200ff
John Z Wu <jzw@chromium.org>
2017-10-12 21:19:52 +0000

    Move AutofillClientIOS into //components/autofill/ios/browser.
    //ios/web_view is a library that allows other iOS apps to use Bling's
    web view implementation. As part of that effort, we'd like to enable
    autofill as a feature.
    
    This CL refactors autofill_client_ios from //ios/chrome into 
    //components/autofill/ios so it may be shared with //ios/web_view.
    
    //ios/chrome's autofill client is renamed to chrome_autofill_client_ios
    which is a subclass of the new shared autofill_client_ios.
    
    Bug: 
    Change-Id: I94de1b3a835601f550f166c3301c2e88de37ae10
    Reviewed-on: https://chromium-review.googlesource.com/705636
    Commit-Queue: John Wu <jzw@chromium.org>
    Reviewed-by: Mihai Sardarescu (Slow to respond) <msarda@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508471}

--- ios/chrome/browser/autofill/autofill_controller_unittest.mm
+++ ios/chrome/browser/autofill/autofill_controller_unittest.mm
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/autofill/form_suggestion_controller.h"
 #include "ios/chrome/browser/browser_state/test_chrome_browser_state.h"
 #include "ios/chrome/browser/infobars/infobar_manager_impl.h"
-#import "ios/chrome/browser/ui/autofill/autofill_client_ios.h"
+#import "ios/chrome/browser/ui/autofill/chrome_autofill_client_ios.h"
 #include "ios/chrome/browser/ui/settings/personal_data_manager_data_changed_observer.h"
 #import "ios/chrome/browser/web/chrome_web_test.h"
 #include "ios/chrome/browser/web_data_service_factory.h"
