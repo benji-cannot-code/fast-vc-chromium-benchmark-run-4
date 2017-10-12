c1e9474ad42e28b8204be3034387267b76752e74
Charles Harrison <csharrison@chromium.org>
2017-10-12 23:04:12 +0000

    Add console unit tests to SafeBrowsingTriggeredPopupBlocker
    Bug: 761385
    Change-Id: Ib364e578c3f4b3fc99cc2987e55c9331776ad67a
    Reviewed-on: https://chromium-review.googlesource.com/713615
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508522}

--- chrome/browser/ui/blocked_content/safe_browsing_triggered_popup_blocker.cc
+++ chrome/browser/ui/blocked_content/safe_browsing_triggered_popup_blocker.cc
@@ -5,14 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/blocked_content/safe_browsing_triggered_popup_blocker.h"
 
+#include <utility>
+
 #include "base/memory/ptr_util.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/metrics/histogram_macros.h"
+#include "chrome/browser/ui/blocked_content/console_logger.h"
 #include "components/safe_browsing/db/util.h"
 #include "components/safe_browsing/db/v4_protocol_manager_util.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/page_navigator.h"
-#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/console_message_level.h"
 #include "third_party/WebKit/public/web/WebTriggeringEventInfo.h"
@@ -20,14 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 const char kIgnoreSublistsParam[] = "ignore_sublists";
-
-constexpr char kDisallowNewWindowMessage[] =
-    "Chrome prevented this site from opening a new tab or window. Learn more "
-    "at https://www.chromestatus.com/feature/5243055179300864";
-constexpr char kWarnNewWindowMessage[] =
-    "Chrome might start preventing this site from opening new tabs or windows "
-    "in the future. Learn more at "
-    "https://www.chromestatus.com/feature/5243055179300864";
 
 void LogAction(SafeBrowsingTriggeredPopupBlocker::Action action) {
   UMA_HISTOGRAM_ENUMERATION("ContentSettings.Popups.StrongBlockerActions",
@@ -46,9 +40,11 @@ SafeBrowsingTriggeredPopupBlocker::~SafeBrowsingTriggeredPopupBlocker() =
     default;
 
 SafeBrowsingTriggeredPopupBlocker::SafeBrowsingTriggeredPopupBlocker(
-    content::WebContents* web_contents)
+    content::WebContents* web_contents,
+    std::unique_ptr<ConsoleLogger> logger)
     : content::WebContentsObserver(web_contents),
       scoped_observer_(this),
+      logger_(std::move(logger)),
       ignore_sublists_(
           base::GetFieldTrialParamByFeatureAsBool(kAbusiveExperienceEnforce,
                                                   kIgnoreSublistsParam,
@@ -81,8 +77,9 @@ bool SafeBrowsingTriggeredPopupBlocker::ShouldApplyStrongPopupBlocker(
   // TODO(csharrison): Migrate SubresourceFilter* popup metrics.
   if (should_block) {
     LogAction(Action::kBlocked);
-    web_contents()->GetMainFrame()->AddMessageToConsole(
-        content::CONSOLE_MESSAGE_LEVEL_ERROR, kDisallowNewWindowMessage);
+    logger_->LogInFrame(web_contents()->GetMainFrame(),
+                        content::CONSOLE_MESSAGE_LEVEL_ERROR,
+                        kAbusiveEnforceMessage);
   }
   return should_block;
 }
@@ -110,8 +107,9 @@ void SafeBrowsingTriggeredPopupBlocker::DidFinishNavigation(
     is_triggered_for_current_committed_load_ = true;
     LogAction(Action::kEnforcedSite);
   } else if (level == SubresourceFilterLevel::WARN) {
-    web_contents()->GetMainFrame()->AddMessageToConsole(
-        content::CONSOLE_MESSAGE_LEVEL_WARNING, kWarnNewWindowMessage);
+    logger_->LogInFrame(web_contents()->GetMainFrame(),
+                        content::CONSOLE_MESSAGE_LEVEL_WARNING,
+                        kAbusiveWarnMessage);
     LogAction(Action::kWarningSite);
   }
   LogAction(Action::kNavigation);
