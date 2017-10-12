a0cb433152ebc7fd1aadea356bae9ccdceae81ea
Kinuko Yasuda <kinuko@chromium.org>
2017-10-12 05:57:48 +0000

    Add ChildURLLoaderFactoryGetter::GetFactoryForURL()
    We have multiple similar code that checks request.Url().ProtocolIs(kBlobScheme)
    just to return BlobURLLoaderFactory, which should be probably removed.
    
    This patch adds ChildURLLoaderFactoryGetter::GetFactoryForURL()
    which centralizes the code to return appropriate URLLoaderFactory for
    a given request URL.
    
    Bug: 730338
    Change-Id: If3ebcfb4b6c1367e7ee3c97e8a6e32256343a780
    Reviewed-on: https://chromium-review.googlesource.com/710274
    Commit-Queue: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508264}

--- content/child/child_url_loader_factory_getter_impl.cc
+++ content/child/child_url_loader_factory_getter_impl.cc
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/child/child_url_loader_factory_getter_impl.h"
 
+#include "base/feature_list.h"
+#include "content/public/common/content_features.h"
+#include "third_party/WebKit/public/platform/WebURLRequest.h"
+
 namespace content {
 
 ChildURLLoaderFactoryGetter::Info::Info(
@@ -56,6 +60,18 @@ ChildURLLoaderFactoryGetterImpl::GetClonedInfo() {
 
   return Info(std::move(network_loader_factory_info),
               std::move(blob_loader_factory_info));
+}
+
+mojom::URLLoaderFactory* ChildURLLoaderFactoryGetterImpl::GetFactoryForURL(
+    const GURL& url,
+    mojom::URLLoaderFactory* default_factory) {
+  if (base::FeatureList::IsEnabled(features::kNetworkService) &&
+      url.SchemeIsBlob()) {
+    return GetBlobLoaderFactory();
+  }
+  if (default_factory)
+    return default_factory;
+  return GetNetworkLoaderFactory();
 }
 
 mojom::URLLoaderFactory*
