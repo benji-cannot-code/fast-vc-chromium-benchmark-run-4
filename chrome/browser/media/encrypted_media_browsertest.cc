7140e3c78ea41fcd38248f4ff0fe252ce3cfce82
Xiaohan Wang <xhwang@chromium.org>
2017-10-12 21:15:38 +0000

    media: Enable crash-related encrypted media browser tests on Windows
    Previously when using mojo CDM, the CDM runs in an unsandboxed CDM
    (utility) process. When crash happens, a popup dialog containing the
    stacktrace is shown. If I close the dialog by clicking the button,
    the test passes. Otherwise, the test will timeout, which is what
    happens on test bots.
    
    Now the CDM process is sandboxed. When the CDM crashes, no dialog
    is shown, and the test always passes.
    
    BUG=770748
    TEST=Enables disabled browsers tests.
    
    Change-Id: Ib084b14ee4fce745405a215ca827ade1e2050cf4
    Reviewed-on: https://chromium-review.googlesource.com/717141
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508470}

--- chrome/browser/media/encrypted_media_browsertest.cc
+++ chrome/browser/media/encrypted_media_browsertest.cc
@@ -754,15 +754,6 @@ IN_PROC_BROWSER_TEST_P(ECKEncryptedMediaTest, InitializeCDMFail) {
 // When CDM crashes, we should still get a decode error and all sessions should
 // be closed.
 IN_PROC_BROWSER_TEST_P(ECKEncryptedMediaTest, CDMCrashDuringDecode) {
-// TODO(xhwang): This test times out when using mojo CDM, possibly due to the
-// crash pop-up dialog. See http://crbug.com/770748
-#if defined(OS_WIN)
-  if (IsUsingMojoCdm()) {
-    DVLOG(0) << "Skipping test; Not working with mojo CDM yet.";
-    return;
-  }
-#endif  // defined(OS_WIN)
-
   IgnorePluginCrash();
   TestNonPlaybackCases(kExternalClearKeyCrashKeySystem,
                        kEmeSessionClosedAndError);
@@ -876,9 +867,6 @@ IN_PROC_BROWSER_TEST_P(ECKEncryptedMediaTest, StorageIdTest) {
                        kUnitTestSuccess);
 }
 
-// TODO(xhwang): This test times out on Windows, possibly due to the
-// crash pop-up dialog. See http://crbug.com/770748
-#if !defined(OS_WIN)
 IN_PROC_BROWSER_TEST_P(ECKEncryptedMediaTest, MultipleCdmTypes) {
   if (!IsUsingMojoCdm()) {
     DVLOG(0) << "Skipping test; Mojo CDM specific.";
@@ -888,7 +876,6 @@ IN_PROC_BROWSER_TEST_P(ECKEncryptedMediaTest, MultipleCdmTypes) {
   base::StringPairs empty_query_params;
   RunMediaTestPage("multiple_cdm_types.html", empty_query_params, kEnded, true);
 }
-#endif  // defined(OS_WIN)
 
 #endif  // BUILDFLAG(ENABLE_LIBRARY_CDMS)
 
