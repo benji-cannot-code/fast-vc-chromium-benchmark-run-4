208d9aa96985df3f4024645f0cf1f0d366864ef6
David Reveman <reveman@chromium.org>
2017-10-12 22:52:34 +0000

    ash: Delete frame sinks when shell is destroyed.
    Surface manager doesn't expect frame sinks to outlive it.
    
    Bug: 774115
    Test: exo_unittests --gtest_filter=BufferTest.SurfaceTreeHostDestruction
    Change-Id: I48e2783ab840f5bfad35a0c80fb7cdd1fb216fcc
    Reviewed-on: https://chromium-review.googlesource.com/716461
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Commit-Queue: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508515}

--- ash/fast_ink/fast_ink_view.cc
+++ ash/fast_ink/fast_ink_view.cc
@@ -67,7 +67,12 @@ class FastInkView::LayerTreeFrameSinkHolder
       : view_(view), frame_sink_(std::move(frame_sink)) {
     frame_sink_->BindToClient(this);
   }
-  ~LayerTreeFrameSinkHolder() override { frame_sink_->DetachFromClient(); }
+  ~LayerTreeFrameSinkHolder() override {
+    if (frame_sink_)
+      frame_sink_->DetachFromClient();
+    if (shell_)
+      shell_->RemoveShellObserver(this);
+  }
 
   // Delete frame sink after having reclaimed all exported resources.
   // TODO(reveman): Find a better way to handle deletion of in-flight resources.
@@ -129,7 +134,7 @@ class FastInkView::LayerTreeFrameSinkHolder
     }
 
     if (shell_ && exported_resources_.empty())
-      DeleteSoon();
+      ScheduleDelete();
   }
   void SetTreeActivationCallback(const base::Closure& callback) override {}
   void DidReceiveCompositorFrameAck() override {
@@ -139,7 +144,7 @@ class FastInkView::LayerTreeFrameSinkHolder
   void DidLoseLayerTreeFrameSink() override {
     exported_resources_.clear();
     if (shell_)
-      DeleteSoon();
+      ScheduleDelete();
   }
   void OnDraw(const gfx::Transform& transform,
               const gfx::Rect& viewport,
@@ -152,15 +157,18 @@ class FastInkView::LayerTreeFrameSinkHolder
   // Overridden from ash::ShellObserver:
   void OnShellDestroyed() override {
     shell_->RemoveShellObserver(this);
-    delete this;
+    shell_ = nullptr;
+    // Make sure frame sink never outlives the shell.
+    frame_sink_->DetachFromClient();
+    frame_sink_.reset();
+    ScheduleDelete();
   }
 
  private:
-  void DeleteSoon() {
-    // Move strong reference from shell observer list to DeleteSoon.
-    DCHECK(shell_);
-    shell_->RemoveShellObserver(this);
-    shell_ = nullptr;
+  void ScheduleDelete() {
+    if (delete_pending_)
+      return;
+    delete_pending_ = true;
     base::ThreadTaskRunnerHandle::Get()->DeleteSoon(FROM_HERE, this);
   }
 
@@ -171,6 +179,7 @@ class FastInkView::LayerTreeFrameSinkHolder
   gfx::Size last_frame_size_in_pixels_;
   float last_frame_device_scale_factor_ = 1.0f;
   ash::Shell* shell_ = nullptr;
+  bool delete_pending_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(LayerTreeFrameSinkHolder);
 };
--- components/exo/layer_tree_frame_sink_holder.h
+++ components/exo/layer_tree_frame_sink_holder.h
@@ -69,7 +69,7 @@ class LayerTreeFrameSinkHolder : public cc::LayerTreeFrameSinkClient,
   void OnShellDestroyed() override;
 
  private:
-  void DeleteSoon();
+  void ScheduleDelete();
 
   // A collection of callbacks used to release resources.
   using ResourceReleaseCallbackMap =
@@ -85,6 +85,8 @@ class LayerTreeFrameSinkHolder : public cc::LayerTreeFrameSinkClient,
 
   gfx::Size last_frame_size_in_pixels_;
   float last_frame_device_scale_factor_ = 1.0f;
+
+  bool delete_pending_ = false;
 
   base::WeakPtrFactory<LayerTreeFrameSinkHolder> weak_ptr_factory_;
 
