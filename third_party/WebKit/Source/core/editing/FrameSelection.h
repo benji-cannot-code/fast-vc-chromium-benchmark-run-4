d9bf51b8436f238330239fe9a6be0e226c233951
Yoshifumi Inoue <yosin@chromium.org>
2017-10-12 10:30:26 +0000

    Integrate SelectWordAroundPosition() into SelectWordAroundCaret() in FrameSelection
    This patch gets rid of |SelectWordAroundPosition()| by integrate it into
    |SelectWordAroundCaret()| in |FrameSeleciton| class to simplify source code for
    improving code health.
    
    In FrameSelectionTest, this patch introduces |SelectWordAroundPosition()|
    helper function for ease of calling |SelectWordAroundCaret()| with specified
    position.
    
    Change-Id: I8497d78bf077a2d454c078ecfa9e2dff0e38f48d
    Reviewed-on: https://chromium-review.googlesource.com/711575
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508304}

--- third_party/WebKit/Source/core/editing/FrameSelection.h
+++ third_party/WebKit/Source/core/editing/FrameSelection.h
@@ -185,11 +185,6 @@ class CORE_EXPORT FrameSelection final
   // Returns true if a word is selected.
   bool SelectWordAroundCaret();
 
-  // TODO(editing-dev): We should replace |SelectWordAroundPosition()|
-  // with |SelectWordAroundCaret()|.
-  // Returns true if a word is selected.
-  bool SelectWordAroundPosition(const VisiblePosition&);
-
 #ifndef NDEBUG
   void ShowTreeForThis() const;
 #endif
