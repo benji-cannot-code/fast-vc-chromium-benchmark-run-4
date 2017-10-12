6e1b8b2a1f7b8bcf45d9281eb5990906cecf4195
Dianna Hu <diannahu@chromium.org>
2017-10-12 15:59:01 +0000

    Add SpdyMemSlice. Not used and not protected.
    This CL lands server change 170485623 by yasong,
    171215460 by diannahu, and 171886305 by bnc.
    
    BUG=488484
    
    Change-Id: I74f8b358de54466724ccd341b1e5c1d92436cba4
    Reviewed-on: https://chromium-review.googlesource.com/703758
    Commit-Queue: Dianna Hu <diannahu@chromium.org>
    Reviewed-by: Bence Béky <bnc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508359}

--- net/spdy/platform/api/spdy_string_utils.h
+++ net/spdy/platform/api/spdy_string_utils.h
@@ -8,6 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+// The following header file has to be included from at least
+// non-test file in order to avoid strange linking errors.
+// TODO(bnc): Remove this include as soon as it is included elsewhere in
+// non-test code.
+#include "net/spdy/platform/api/spdy_mem_slice.h"
+
 #include "net/spdy/platform/api/spdy_string.h"
 #include "net/spdy/platform/api/spdy_string_piece.h"
 #include "net/spdy/platform/impl/spdy_string_utils_impl.h"
