7fa6fec35b1e9ec29c4daa59fd1aba9a4139c708
Sadrul Habib Chowdhury <sadrul@chromium.org>
2017-10-12 20:53:19 +0000

    viz: Move mojom.GpuHost into viz.
    Move mojom.GpuHost out of services/ui/gpu into services/viz/privileged.
    Change namespace from ui:: to viz:: accordingly.
    
    BUG=719931
    TBR=piman@ for content/browser, yzshen@ for mojo/public for trivial changes
    
    Change-Id: Ib2acf2b47ed8c3cbbe0829ad76ee078bc2a6b138
    Reviewed-on: https://chromium-review.googlesource.com/716697
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508454}

--- services/ui/gpu/gpu_main.h
+++ services/ui/gpu/gpu_main.h
@@ -80,7 +80,7 @@ class GpuMain : public gpu::GpuSandboxHelper, public mojom::GpuMain {
 
   // mojom::GpuMain implementation:
   void CreateGpuService(viz::mojom::GpuServiceRequest request,
-                        mojom::GpuHostPtr gpu_host,
+                        viz::mojom::GpuHostPtr gpu_host,
                         const gpu::GpuPreferences& preferences,
                         mojo::ScopedSharedBufferHandle activity_flags) override;
   void CreateFrameSinkManager(
--- services/ui/ws/gpu_host.h
+++ services/ui/ws/gpu_host.h
@@ -16,8 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/interface_request.h"
 #include "mojo/public/cpp/bindings/strong_binding_set.h"
 #include "services/ui/gpu/gpu_main.h"
-#include "services/ui/gpu/interfaces/gpu_host.mojom.h"
 #include "services/ui/public/interfaces/gpu.mojom.h"
+#include "services/viz/privileged/interfaces/gl/gpu_host.mojom.h"
 #include "services/viz/privileged/interfaces/gl/gpu_service.mojom.h"
 
 namespace viz {
@@ -53,7 +53,7 @@ class GpuHost {
       viz::mojom::FrameSinkManagerClientPtr client) = 0;
 };
 
-class DefaultGpuHost : public GpuHost, public mojom::GpuHost {
+class DefaultGpuHost : public GpuHost, public viz::mojom::GpuHost {
  public:
   explicit DefaultGpuHost(GpuHostDelegate* delegate);
   ~DefaultGpuHost() override;
@@ -75,7 +75,7 @@ class DefaultGpuHost : public GpuHost, public mojom::GpuHost {
       viz::mojom::FrameSinkManagerRequest request,
       viz::mojom::FrameSinkManagerClientPtr client) override;
 
-  // mojom::GpuHost:
+  // viz::mojom::GpuHost:
   void DidInitialize(const gpu::GPUInfo& gpu_info,
                      const gpu::GpuFeatureInfo& gpu_feature_info) override;
   void DidFailInitialize() override;
@@ -98,7 +98,7 @@ class DefaultGpuHost : public GpuHost, public mojom::GpuHost {
   int32_t next_client_id_;
   scoped_refptr<base::SingleThreadTaskRunner> main_thread_task_runner_;
   viz::mojom::GpuServicePtr gpu_service_;
-  mojo::Binding<mojom::GpuHost> gpu_host_binding_;
+  mojo::Binding<viz::mojom::GpuHost> gpu_host_binding_;
   gpu::GPUInfo gpu_info_;
   gpu::GpuFeatureInfo gpu_feature_info_;
   std::unique_ptr<viz::ServerGpuMemoryBufferManager> gpu_memory_buffer_manager_;
