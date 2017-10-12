127b999c92131a58bb4b363ca46b7893763ccb0a
Daniel Bratell <bratell@opera.com>
2017-10-12 21:12:35 +0000

    Remove unused using statement in legacy_router_impl.cc
    Noticed because Time is also a macro in X11.
    
    Change-Id: I827617492e82b62eeaf668e24470b5c199718171
    Reviewed-on: https://chromium-review.googlesource.com/716482
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#508466}

--- content/browser/renderer_host/input/legacy_input_router_impl.cc
+++ content/browser/renderer_host/input/legacy_input_router_impl.cc
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event.h"
 #include "ui/events/keycodes/keyboard_codes.h"
 
-using base::Time;
 using base::TimeDelta;
 using base::TimeTicks;
 using blink::WebGestureEvent;
