b3bc75b1fc3bf2028e92d39fc81e07b9257d69ea
Samuel Huang <huangs@chromium.org>
2017-10-12 01:21:51 +0000

    [Zucchini] Add and use test_utils.*.
    Currently test_utils.* only contains ParseHexString().
    
    Bug: 729154
    Change-Id: Ica92437d4f0d6dbc183fb68989ec63d0fe7059a1
    Reviewed-on: https://chromium-review.googlesource.com/706243
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508206}

--- chrome/installer/zucchini/reloc_utils_unittest.cc
+++ chrome/installer/zucchini/reloc_utils_unittest.cc
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <algorithm>
-#include <ios>
-#include <sstream>
 #include <string>
 #include <utility>
 #include <vector>
@@ -21,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/installer/zucchini/address_translator.h"
 #include "chrome/installer/zucchini/algorithm.h"
 #include "chrome/installer/zucchini/image_utils.h"
+#include "chrome/installer/zucchini/test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace zucchini {
@@ -40,19 +39,6 @@ std::vector<T> Cat(const std::vector<T>& a, const std::vector<T>& b) {
 template <class T>
 std::vector<T> Sub(const std::vector<T>& a, size_t lo, size_t hi) {
   return std::vector<T>(a.begin() + lo, a.begin() + hi);
-}
-
-// Parses space-separated list of byte hex values into list.
-std::vector<uint8_t> ParseHexString(const std::string& s) {
-  std::vector<uint8_t> ret;
-  std::istringstream iss(s);
-  iss >> std::hex;
-  uint32_t temp = 0;  // Cannot be uint8_t: istringstream treats this as char!
-  while (iss >> temp) {
-    CHECK_LE(temp, 0xFFU);
-    ret.push_back(temp);
-  }
-  return ret;
 }
 
 }  // namespace
