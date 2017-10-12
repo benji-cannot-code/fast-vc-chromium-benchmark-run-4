cc7106fdf1e2ebd221eb7240e1dba0f7d0cf0f20
Dmitry Titov <dimich@chromium.org>
2017-10-12 03:37:17 +0000

    Add UMA collection for the prefetch daily stats.
    This starts to collect statistics for number of user-days when:
    - Prefetch and Zine were enabled and Chrome has received new URLs for prefetch
    - A page was successfully prefetched.
    - User opened a prefetched page
    The data is collected during a day and accumulated in daily persistent counters that are
    periodically reported to UMA.
    
    Bug: 701939
    Change-Id: I1a67f8bf0daeca3022485293daca7cdfffefeba4
    Reviewed-on: https://chromium-review.googlesource.com/710509
    Commit-Queue: Dmitry Titov <dimich@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508249}

--- components/offline_pages/core/prefetch/test_offline_metrics_collector.h
+++ components/offline_pages/core/prefetch/test_offline_metrics_collector.h
@@ -21,6 +21,10 @@ class TestOfflineMetricsCollector : public OfflineMetricsCollector {
   void OnAppStartupOrResume() override {}
   void OnSuccessfulNavigationOnline() override {}
   void OnSuccessfulNavigationOffline() override {}
+  void OnPrefetchEnabled() override {}
+  void OnHasPrefetchedPagesDetected() override {}
+  void OnSuccessfulPagePrefetch() override {}
+  void OnPrefetchedPageOpened() override {}
   void ReportAccumulatedStats() override {}
 };
 
