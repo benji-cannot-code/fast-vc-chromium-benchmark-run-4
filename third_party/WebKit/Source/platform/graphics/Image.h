0a3bc95d81d858039c1f1dae5fa77cf37eb33d13
Khushal <khushalsagar@chromium.org>
2017-10-12 23:42:07 +0000

    blink: Add an UMA to record checkerable DOM image types.
    This change adds an UMA to record the source of an image in the DOM
    that could have been checkered/async decoded to gain performance. The
    source here refers to the element type with which the image was used,
    i.e., img, svg or as a css attribute (background-img, border-img) on a
    div.
    
    R=chrishtr@chromium.org
    
    Change-Id: I1a274069e90350b474bcb1512a3ef2e14e9fb937
    Reviewed-on: https://chromium-review.googlesource.com/710677
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508542}

--- third_party/WebKit/Source/platform/graphics/Image.h
+++ third_party/WebKit/Source/platform/graphics/Image.h
@@ -233,6 +233,9 @@ class PLATFORM_EXPORT Image : public ThreadSafeRefCounted<Image> {
                                         const FloatRect& dest,
                                         const FloatSize& image_size);
 
+  enum class ImageType { kImg, kSvg, kCss };
+  static void RecordCheckerableImageUMA(Image&, ImageType);
+
   virtual sk_sp<PaintRecord> PaintRecordForContainer(
       const KURL& url,
       const IntSize& container_size,
@@ -281,6 +284,9 @@ class PLATFORM_EXPORT Image : public ThreadSafeRefCounted<Image> {
   // Creates and initializes a PaintImageBuilder with the metadata flags for the
   // PaintImage.
   PaintImageBuilder CreatePaintImageBuilder();
+
+  // Whether or not size is available yet.
+  virtual bool IsSizeAvailable() { return true; }
 
  private:
   bool image_observer_disabled_;
