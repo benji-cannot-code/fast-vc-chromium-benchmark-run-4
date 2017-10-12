9263fd8c5f161b3f3dad12df4da758492cddb00b
Tom Sepez <tsepez@chromium.org>
2017-10-12 01:23:55 +0000

    Reland "Move sandbox_mac.{h,mm} files to service_manager""
    This reverts commit 727d1287bfbcc724d15de182335243cbcd9aa51e,
    which re-lands commit sa2faba88d6ad5a188f9f2ae36dd9247267ef99da.
    
    Fix is to restore generating files under mac/ sub-directory.
    
    TBR=rsesek@chromium.org, jochen@chromium.org
    
    Change-Id: I81a65b0c6b83f35732974f457ce57a2fc6e59160
    Reviewed-on: https://chromium-review.googlesource.com/713436
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508209}

--- content/renderer/renderer_main_platform_delegate_mac.mm
+++ content/renderer/renderer_main_platform_delegate_mac.mm
@@ -18,9 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/sys_string_conversions.h"
 #include "content/common/sandbox_init_mac.h"
-#include "content/common/sandbox_mac.h"
 #include "content/public/common/content_switches.h"
 #include "sandbox/mac/seatbelt.h"
+#include "services/service_manager/sandbox/mac/sandbox_mac.h"
 
 extern "C" {
 void CGSSetDenyWindowServerConnections(bool);
