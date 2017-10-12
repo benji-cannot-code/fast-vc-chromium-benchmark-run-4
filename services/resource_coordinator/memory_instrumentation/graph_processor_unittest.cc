fe9b88b36073f22d1373341c3f4075d2b132da5d
Lalit Maganti <lalitm@chromium.org>
2017-10-12 15:19:04 +0000

    memory-infra: mark nodes as weak if corresponding dumps are weak
    Bug: 768373
    Change-Id: I6926d906ee8bf4dcdb8e57c9bbcb46615102dd9f
    Reviewed-on: https://chromium-review.googlesource.com/701754
    Commit-Queue: Lalit Maganti <lalitm@chromium.org>
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508353}

--- services/resource_coordinator/memory_instrumentation/graph_processor_unittest.cc
+++ services/resource_coordinator/memory_instrumentation/graph_processor_unittest.cc
@@ -12,6 +12,7 @@ namespace memory_instrumentation {
 using base::ProcessId;
 using base::trace_event::HeapProfilerSerializationState;
 using base::trace_event::MemoryAllocatorDump;
+using base::trace_event::MemoryAllocatorDumpGuid;
 using base::trace_event::MemoryDumpArgs;
 using base::trace_event::MemoryDumpLevelOfDetail;
 using base::trace_event::ProcessMemoryDump;
@@ -28,6 +29,9 @@ TEST(GraphProcessorTest, ComputeMemoryGraph) {
 
   auto* target = pmd.CreateAllocatorDump("target");
   pmd.AddOwnershipEdge(source->guid(), target->guid(), 10);
+
+  auto* weak =
+      pmd.CreateWeakSharedGlobalAllocatorDump(MemoryAllocatorDumpGuid(1));
 
   process_dumps.emplace(1, std::move(pmd));
 
@@ -56,6 +60,8 @@ TEST(GraphProcessorTest, ComputeMemoryGraph) {
 
   auto size = third_child->entries().find(MemoryAllocatorDump::kNameSize);
   ASSERT_EQ(10ul, size->second.value_uint64);
+
+  ASSERT_TRUE(weak->flags() & MemoryAllocatorDump::Flags::WEAK);
 
   auto& edges = global_dump->edges();
   auto edge_it = edges.begin();
