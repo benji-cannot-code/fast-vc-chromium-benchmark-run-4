7b9faeb4071df709e2b91ac8509297114ac50c99
Bhagirathi Satpathy <bhagirathi.s@samsung.com>
2017-10-12 03:23:19 +0000

    Remove unused header HTMLAnchorElement.h
    HTMLAnchorElement.h is included in HTMLImageElement.cpp but not used.
    Removed this unnecessary header.
    
    Bug: 
    Change-Id: I8cd5d7320e9ad4269957db2c857904672d108780
    Reviewed-on: https://chromium-review.googlesource.com/712294
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: srirama chandra sekhar <srirama.m@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#508243}

--- third_party/WebKit/Source/core/html/HTMLImageElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLImageElement.cpp
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/SyncReattachContext.h"
 #include "core/frame/Deprecation.h"
 #include "core/frame/LocalDOMWindow.h"
-#include "core/html/HTMLAnchorElement.h"
 #include "core/html/HTMLCanvasElement.h"
 #include "core/html/HTMLImageFallbackHelper.h"
 #include "core/html/HTMLPictureElement.h"
