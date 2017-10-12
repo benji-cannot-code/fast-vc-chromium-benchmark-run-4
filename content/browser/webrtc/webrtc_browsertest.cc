aefc1ee50c3a64627fe32337ea6bcaaf37582dc6
Guido Urdaneta <guidou@chromium.org>
2017-10-12 11:24:53 +0000

    Do not set settings for video tracks without format.
    This affects tracks connected to a MediaStreamRemoteVideoSource,
    which does not provide a format.
    Currently, these tracks report 32678 as placeholder width/height
    because the real dimensions are unknown.
    It is better to leave these values undefined instead of providing
    incorrect values.
    
    Bug: 772358
    Change-Id: Iaa243d8453e7de1cd8206d99c9249dae6bc1aee2
    Reviewed-on: https://chromium-review.googlesource.com/704836
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508312}

--- content/browser/webrtc/webrtc_browsertest.cc
+++ content/browser/webrtc/webrtc_browsertest.cc
@@ -232,4 +232,10 @@ IN_PROC_BROWSER_TEST_F(MAYBE_WebRtcBrowserTest, ApplyConstraints) {
   MakeTypicalPeerConnectionCall("testApplyConstraints();");
 }
 
+IN_PROC_BROWSER_TEST_F(MAYBE_WebRtcBrowserTest,
+                       GetSettingsWhenRemoteDimensionsUnknown) {
+  MakeTypicalPeerConnectionCall(
+      "testGetSettingsWhenRemoteDimensionsUnknown();");
+}
+
 }  // namespace content
