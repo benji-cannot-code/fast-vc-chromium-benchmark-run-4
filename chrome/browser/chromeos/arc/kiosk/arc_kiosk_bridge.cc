9464a42823446bdc763dcb8a40bd28999ad22d21
yusukes <yusukes@chromium.org>
2017-10-13 21:04:41 +0000

    arc: Stop #including ptr_util.h in c/b/c/arc/ and components/arc/
    The header is unused now.
    
    BUG=None
    TEST=try
    
    Change-Id: I9e8b8d2ca04f25125ba63eb6c6a0b4f2f67d51eb
    Reviewed-on: https://chromium-review.googlesource.com/719498
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508815}

--- chrome/browser/chromeos/arc/kiosk/arc_kiosk_bridge.cc
+++ chrome/browser/chromeos/arc/kiosk/arc_kiosk_bridge.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "base/memory/ptr_util.h"
 #include "base/memory/singleton.h"
 #include "chrome/browser/chromeos/app_mode/arc/arc_kiosk_app_service.h"
 #include "chrome/browser/chromeos/app_mode/arc/arc_kiosk_app_service_factory.h"
--- components/arc/arc_util_unittest.cc
+++ components/arc/arc_util_unittest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/app_types.h"
 #include "base/command_line.h"
 #include "base/macros.h"
-#include "base/memory/ptr_util.h"
 #include "base/test/scoped_feature_list.h"
 #include "components/signin/core/account_id/account_id.h"
 #include "components/user_manager/fake_user_manager.h"
