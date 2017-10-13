94d5564884709c8ecea54252742403b9db7450ef
Matthew Jones <mdjones@chromium.org>
2017-10-13 16:18:13 +0000

    [Home] Add metrics for Chrome Home promo
    This change adds metrics around the Chrome Home promo. Histograms are
    recorded for open reason and the result of showing the promo; whether
    the feature was on and switched off or otherwise.
    
    BUG=767738
    
    Change-Id: I10a5bbdf8b962652b6a3882e55b167fdc3f6e48f
    Reviewed-on: https://chromium-review.googlesource.com/701288
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508716}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -516,7 +516,9 @@ public class ChromeTabbedActivity
                     if (!DataReductionPromoScreen.launchDataReductionPromo(
                                 this, currentModel.isIncognito())) {
                         if (FeatureUtilities.shouldShowChromeHomePromoForStartup()) {
-                            new ChromeHomePromoDialog(this).show();
+                            new ChromeHomePromoDialog(
+                                    this, ChromeHomePromoDialog.ShowReason.STARTUP)
+                                    .show();
                         } else if (getBottomSheet() != null) {
                             getBottomSheet().showHelpBubbleIfNecessary();
                         }
@@ -1615,7 +1617,9 @@ public class ChromeTabbedActivity
                         if (getBottomSheet() != null
                                 && ChromeFeatureList.isEnabled(
                                            ChromeFeatureList.CHROME_HOME_PROMO)) {
-                            new ChromeHomePromoDialog(ChromeTabbedActivity.this).show();
+                            new ChromeHomePromoDialog(ChromeTabbedActivity.this,
+                                    ChromeHomePromoDialog.ShowReason.MENU)
+                                    .show();
                             return;
                         }
 
