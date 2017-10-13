d5a9cedcd42d5817b082b76e92789c5d9bcb7465
Marc Treib <treib@chromium.org>
2017-10-13 16:40:54 +0000

    Cleanup: Remove launched CaptureThumbnail feature checks
    CaptureThumbnailDependingOnTransitionType and CaptureThumbnailOnNavigatingAway
    have been launched. This CL removes the feature checks.
    
    Bug: 765588
    Change-Id: I68906ef0533ede82dc3c47724603b9af78494969
    Reviewed-on: https://chromium-review.googlesource.com/684839
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508722}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -82,9 +82,6 @@ extern const char kBrowserTaskSchedulerDescription[];
 extern const char kBypassAppBannerEngagementChecksName[];
 extern const char kBypassAppBannerEngagementChecksDescription[];
 
-extern const char kCaptureThumbnailOnNavigatingAwayName[];
-extern const char kCaptureThumbnailOnNavigatingAwayDescription[];
-
 extern const char kCastStreamingHwEncodingName[];
 extern const char kCastStreamingHwEncodingDescription[];
 
