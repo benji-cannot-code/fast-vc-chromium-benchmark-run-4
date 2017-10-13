991a1f4a71301feaea371e5cd88c042f666242b3
Matt Menke <mmenke@chromium.org>
2017-10-13 23:42:19 +0000

    Revert "Add some CHECKs to verify that issue 756993 is memory corruption."
    This reverts commit 95ed6339ee6fc416cb91cc06bb07c44b433fc437.
    
    Reason for revert: The crash has gone away (Likely due to a memory
    stomper on Android being fixed). So CHECKs are no longer needed.
    
    Original change's description:
    > Add some CHECKs to verify that issue 756993 is memory corruption.
    > 
    > Bug: 756993
    > Change-Id: If708b72d07b645905e849d1706e89a49d4ac6323
    > Reviewed-on: https://chromium-review.googlesource.com/624295
    > Reviewed-by: Helen Li <xunjieli@chromium.org>
    > Commit-Queue: Matt Menke <mmenke@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#496364}
    
    TBR=mmenke@chromium.org,xunjieli@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 756993
    Change-Id: I22e1703c7e2415f6203779e6badb6bc26a940010
    Reviewed-on: https://chromium-review.googlesource.com/719916
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Helen Li <xunjieli@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508869}

--- net/socket/client_socket_pool_base.cc
+++ net/socket/client_socket_pool_base.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/compiler_specific.h"
-#include "base/debug/alias.h"
 #include "base/format_macros.h"
 #include "base/location.h"
 #include "base/logging.h"
@@ -57,7 +56,6 @@ ConnectJob::ConnectJob(const std::string& group_name,
                        Delegate* delegate,
                        const NetLogWithSource& net_log)
     : group_name_(group_name),
-      group_name_copy_(group_name),
       timeout_duration_(timeout_duration),
       priority_(priority),
       respect_limits_(respect_limits),
@@ -72,7 +70,6 @@ ConnectJob::ConnectJob(const std::string& group_name,
 }
 
 ConnectJob::~ConnectJob() {
-  CheckGroupName();
   net_log().EndEvent(NetLogEventType::SOCKET_POOL_CONNECT_JOB);
 }
 
@@ -81,7 +78,6 @@ std::unique_ptr<StreamSocket> ConnectJob::PassSocket() {
 }
 
 int ConnectJob::Connect() {
-  CheckGroupName();
   if (!timeout_duration_.is_zero())
     timer_.Start(FROM_HERE, timeout_duration_, this, &ConnectJob::OnTimeout);
 
@@ -99,23 +95,6 @@ int ConnectJob::Connect() {
   return rv;
 }
 
-void ConnectJob::CheckGroupName() const {
-  if (group_name_ != group_name_copy_)
-    LogGroupNameAndCrash();
-}
-
-void ConnectJob::LogGroupNameAndCrash() const {
-  char group_name[128];
-  char group_name_copy[128];
-  base::strlcpy(group_name, group_name_.c_str(), arraysize(group_name));
-  base::strlcpy(group_name_copy, group_name_copy_.c_str(),
-                arraysize(group_name_copy));
-  base::debug::Alias(group_name);
-  base::debug::Alias(group_name_copy);
-  CHECK(false) << "Memory corruption: " << group_name_
-               << " != " << group_name_copy_;
-}
-
 void ConnectJob::SetSocket(std::unique_ptr<StreamSocket> socket) {
   if (socket) {
     net_log().AddEvent(NetLogEventType::CONNECT_JOB_SET_SOCKET,
@@ -125,8 +104,6 @@ void ConnectJob::SetSocket(std::unique_ptr<StreamSocket> socket) {
 }
 
 void ConnectJob::NotifyDelegateOfCompletion(int rv) {
-  CheckGroupName();
-
   TRACE_EVENT0(kNetTracingCategory, "ConnectJob::NotifyDelegateOfCompletion");
   // The delegate will own |this|.
   Delegate* delegate = delegate_;
