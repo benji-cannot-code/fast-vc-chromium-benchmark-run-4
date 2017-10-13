0b14640a0e8721f129e2a23db2cd94c049a692e9
Erik Luo <luoe@chromium.org>
2017-10-13 21:28:09 +0000

    DevTools: filter settings should use ConsoleFilter
    Filter settings appear in console UI, such as the
    "Hide network messages" checkbox. Now,
    - these settings are built using ConsoleFilter
    - ConsoleViewFilter (CVF) no longer depends on ConsoleSidebar.
      Instead, ConsoleView calls sidebar.applyFilters(), and CVF acts only
      as a controller owning settings and text input filter.
    
    Bug: 685283
    Change-Id: I266a1656748d6c9db24c5420e1cd7930ee7279da
    Reviewed-on: https://chromium-review.googlesource.com/696439
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508825}

--- third_party/WebKit/LayoutTests/http/tests/devtools/console/console-filter-level-test.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/console/console-filter-level-test.js
@@ -80,14 +80,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     function abcMessagePlain(next) {
       Console.ConsoleViewFilter.levelFilterSetting().set({ verbose: true });
       Console.ConsoleView.instance()._filter._textFilterUI.setValue('abc');
-      Console.ConsoleView.instance()._filter._textFilterChanged();
+      Console.ConsoleView.instance()._filter._onFilterChanged();
       dumpVisibleMessages();
       next();
     },
 
     function abcMessageRegex(next) {
       Console.ConsoleView.instance()._filter._textFilterUI.setValue('/ab[a-z]/');
-      Console.ConsoleView.instance()._filter._textFilterChanged();
+      Console.ConsoleView.instance()._filter._onFilterChanged();
       dumpVisibleMessages();
       next();
     },
