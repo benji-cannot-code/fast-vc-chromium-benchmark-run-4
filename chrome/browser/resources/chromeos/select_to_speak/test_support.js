0468930142ae38841220623d51ea9ca00624a152
Katie Dektar <katydek@google.com>
2017-10-13 22:33:56 +0000

    Smoothly speak paragraphs of text with Select-to-Speak.
    Also adds a verbosity setting for logging, since it was very
    noisy.
    
    Bug: 753022
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ida2a63ce84738cfe83f6095cdaebf9576c3ac6ff
    Reviewed-on: https://chromium-review.googlesource.com/717417
    Commit-Queue: Katie Dektar <katydek@google.com>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508852}

--- chrome/browser/resources/chromeos/select_to_speak/test_support.js
+++ chrome/browser/resources/chromeos/select_to_speak/test_support.js
@@ -20,7 +20,10 @@ chrome.automation.getDesktop = function() {};
  */
 chrome.automation.RoleType = {
   WINDOW: 'window',
-  ROOT_WEB_AREA: 'rootWebArea'
+  ROOT_WEB_AREA: 'rootWebArea',
+  STATIC_TEXT: 'staticText',
+  INLINE_TEXT_BOX: 'inlineTextBox',
+  PARAGRAPH: 'paragraph'
 };
 chrome.automation.StateType = {
   INVISIBLE: 'invisible'
