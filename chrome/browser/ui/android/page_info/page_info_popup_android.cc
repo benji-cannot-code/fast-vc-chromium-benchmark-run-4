97bac1bb3708945951e911d36c2b187294a6f9fb
Tommy Steimel <steimel@chromium.org>
2017-10-13 15:54:31 +0000

    Show sound content setting in page info on Android
    Currently, even if the sound content setting is in a non-default state,
    we don't show it in page info on Android. This CL fixes that oversight.
    
    Bug: 743000
    Change-Id: Icf7b0ca7db1ca63faa5b93787036bf8cb53370b4
    Reviewed-on: https://chromium-review.googlesource.com/717419
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508713}

--- chrome/browser/ui/android/page_info/page_info_popup_android.cc
+++ chrome/browser/ui/android/page_info/page_info_popup_android.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ssl/security_state_tab_helper.h"
 #include "chrome/browser/ui/page_info/page_info.h"
 #include "chrome/browser/ui/page_info/page_info_ui.h"
+#include "chrome/common/chrome_features.h"
 #include "components/content_settings/core/common/content_settings.h"
 #include "components/content_settings/core/common/content_settings_types.h"
 #include "components/security_state/core/security_state.h"
@@ -120,6 +121,8 @@ void PageInfoPopupAndroid::SetPermissionInfo(
   permissions_to_display.push_back(CONTENT_SETTINGS_TYPE_POPUPS);
   permissions_to_display.push_back(CONTENT_SETTINGS_TYPE_ADS);
   permissions_to_display.push_back(CONTENT_SETTINGS_TYPE_AUTOPLAY);
+  if (base::FeatureList::IsEnabled(features::kSoundContentSetting))
+    permissions_to_display.push_back(CONTENT_SETTINGS_TYPE_SOUND);
 
   std::map<ContentSettingsType, ContentSetting>
       user_specified_settings_to_display;
