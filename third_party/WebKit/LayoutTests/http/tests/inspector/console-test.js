2416da1ea72e0ad58225eb8c2a4230e34f19fd51
Will Chen <chenwilliam@chromium.org>
2017-10-13 18:45:23 +0000

    DevTools: clean up inspector-test.js (part 1)
    * Move dumpEventListener to elements-test.js which will soon be a small
    wrapper to load elements_test_runner
    * Move wrapListener to console-test.js which will soon be a wrapper around
    console_test_runner
    * Clean up usages of navigator methods and addScriptUISourceCode by loading
    sources_test_runner via debugger-test.js (these will become proper module
    loading in the new integration test framework)
      - e.g. TestRunner.loadModule('sources_test_runner')
    
    Bug: 667560
    Change-Id: Ia2566b64669b8d24d8e36d795cad646632eeaf05
    Reviewed-on: https://chromium-review.googlesource.com/717723
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508766}

--- third_party/WebKit/LayoutTests/http/tests/inspector/console-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/console-test.js
@@ -452,4 +452,21 @@ InspectorTest.selectConsoleMessages = function(fromMessage, fromTextOffset, toMe
     }
 }
 
+InspectorTest.wrapListener = function(func)
+{
+    function wrapper()
+    {
+        var wrapArgs = arguments;
+        var wrapThis = this;
+        // Give a chance to other listeners.
+        setTimeout(apply, 0);
+
+        function apply()
+        {
+            func.apply(wrapThis, wrapArgs);
+        }
+    }
+    return wrapper;
+}
+
 }
