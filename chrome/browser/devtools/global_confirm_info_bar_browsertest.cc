d4a66dd7a877f85a0dd831b6f9018b2c1c4ac628
Patrick Monette <pmonette@chromium.org>
2017-10-13 17:03:32 +0000

    Fix timeout in the GlobalConfirmInfoBar browser tests
    Bug: 764079
    Change-Id: I3f48e77d3ba735bdef71d69a1b0380cc5110e033
    Reviewed-on: https://chromium-review.googlesource.com/698865
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508732}

--- chrome/browser/devtools/global_confirm_info_bar_browsertest.cc
+++ chrome/browser/devtools/global_confirm_info_bar_browsertest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
-#include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "chrome/browser/ui/browser.h"
@@ -18,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/in_process_browser_test.h"
 #include "components/infobars/core/infobar.h"
 #include "components/infobars/core/infobars_switches.h"
+#include "content/public/test/test_utils.h"
 
 namespace {
 
@@ -148,14 +148,8 @@ IN_PROC_BROWSER_TEST_F(GlobalConfirmInfoBarTest, UserInteraction) {
     EXPECT_EQ(0u, GetInfoBarServiceFromTabIndex(i)->infobar_count());
 }
 
-// https://crbug.com/764079
-#if defined(OS_LINUX) || defined(OS_MACOSX)
-#define MAYBE_InfoBarsDisabled DISABLED_InfoBarsDisabled
-#else
-#define MAYBE_InfoBarsDisabled InfoBarsDisabled
-#endif
 IN_PROC_BROWSER_TEST_F(GlobalConfirmInfoBarWithInfoBarDisabledTest,
-                       MAYBE_InfoBarsDisabled) {
+                       InfoBarsDisabled) {
   ASSERT_EQ(1, browser()->tab_strip_model()->count());
 
   auto delegate = base::MakeUnique<TestConfirmInfoBarDelegate>();
@@ -163,7 +157,7 @@ IN_PROC_BROWSER_TEST_F(GlobalConfirmInfoBarWithInfoBarDisabledTest,
       GlobalConfirmInfoBar::Show(std::move(delegate));
 
   // In this case, the deletion is done asynchronously.
-  base::RunLoop().RunUntilIdle();
+  content::RunAllPendingInMessageLoop();
 
   ASSERT_FALSE(global_confirm_info_bar);
 }
