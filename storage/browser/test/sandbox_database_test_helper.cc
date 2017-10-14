840a2573a15ed8930d00d4d8a16eb3bf63f9d47c
Chris Mumford <cmumford@chromium.org>
2017-10-14 00:49:20 +0000

    leveldb: Moved from shared library to a component.
    The Chromium leveldb related code now has static variables, but
    was in a static library. This change moves this code into a component
    to avoid having multiple copies which confuses the memory-infra
    reporting statistics.
    
    TBR=michaeln
    
    Bug: 764810
    Change-Id: I895b553923ce06cf6788688975ec430da7f2655f
    Reviewed-on: https://chromium-review.googlesource.com/671814
    Commit-Queue: Chris Mumford <cmumford@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508893}

--- storage/browser/test/sandbox_database_test_helper.cc
+++ storage/browser/test/sandbox_database_test_helper.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "storage/common/fileapi/file_system_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/leveldatabase/leveldb_chrome.h"
 
 using storage::FilePathToString;
 
@@ -35,8 +36,8 @@ void CorruptDatabase(const base::FilePath& db_path,
   while (!(file_path = file_enum.Next()).empty()) {
     uint64_t number = std::numeric_limits<uint64_t>::max();
     leveldb::FileType file_type;
-    EXPECT_TRUE(leveldb::ParseFileName(FilePathToString(file_path.BaseName()),
-                                       &number, &file_type));
+    EXPECT_TRUE(leveldb_chrome::ParseFileName(
+        FilePathToString(file_path.BaseName()), &number, &file_type));
     if (file_type == type &&
         (picked_file_number == std::numeric_limits<uint64_t>::max() ||
          picked_file_number < number)) {
@@ -85,8 +86,8 @@ void DeleteDatabaseFile(const base::FilePath& db_path,
   while (!(file_path = file_enum.Next()).empty()) {
     uint64_t number = std::numeric_limits<uint64_t>::max();
     leveldb::FileType file_type;
-    EXPECT_TRUE(leveldb::ParseFileName(FilePathToString(file_path.BaseName()),
-                                       &number, &file_type));
+    EXPECT_TRUE(leveldb_chrome::ParseFileName(
+        FilePathToString(file_path.BaseName()), &number, &file_type));
     if (file_type == type) {
       base::DeleteFile(file_path, false);
       // We may have multiple files for the same type, so don't break here.
