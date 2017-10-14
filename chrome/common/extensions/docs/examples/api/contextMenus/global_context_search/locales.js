03a4e6a634f64a110bb3968e7fe336dd3825f3e4
crystallambert@chromium.org <crystallambert@chromium.org>
2017-10-14 01:45:02 +0000

    Add a sample extension for event pages and the contextMenus API
    Add a sample extension that demonstrates the usage of event pages and
    utilizes the contextMenus API. The extension allows the user to search
    google with a different country locale. Additionally, demonstrates
    usage of an options page, browserAction and storage. 
    
    Add more sample extensions 
    
    Bug: 
    Change-Id: Iaa785dabe474dc94bb09f02fa87359700f63ddf4
    Reviewed-on: https://chromium-review.googlesource.com/685456
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Crystal Lambert <crystallambert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508908}

---
+++ chrome/common/extensions/docs/examples/api/contextMenus/global_context_search/locales.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const kLocales = {
+  'com.au': 'Australia',
+  'com.br': 'Brazil',
+  'ca': 'Canada',
+  'cn': 'China',
+  'fr': 'France',
+  'it': 'Italy',
+  'co.in': 'India',
+  'co.jp': 'Japan',
+  'com.ms': 'Mexico',
+  'ru': 'Russia',
+  'co.za': 'South Africa',
+  'co.uk': 'United Kingdom'
+};
