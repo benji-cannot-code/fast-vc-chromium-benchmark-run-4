0cb670ecdfb41cb31b8654195e27701161803a8b
Tom Sepez <tsepez@chromium.org>
2017-10-14 00:21:25 +0000

    Merge sandbox_init_mac.h into sandbox_init.h
    Make sandbox_init.h describe all the functions provided by
    sandbox_init_{linux,mac.win}.cc. This reduces platform-specific
    ifdefs in other files.
    
    Change-Id: If8c10a5157edb35e75df16fa755ba9c70160e417
    Reviewed-on: https://chromium-review.googlesource.com/717260
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508887}

--- content/app/content_main_runner.cc
+++ content/app/content_main_runner.cc
@@ -74,7 +74,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/scoped_nsautorelease_pool.h"
 #include "base/power_monitor/power_monitor_device_source.h"
 #include "content/browser/mach_broker_mac.h"
-#include "content/common/sandbox_init_mac.h"
 #endif  // OS_WIN
 
 #if defined(OS_POSIX)
--- content/ppapi_plugin/ppapi_thread.cc
+++ content/ppapi_plugin/ppapi_thread.cc
@@ -62,8 +62,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/win_util.h"
 #include "content/child/font_warmup_win.h"
 #include "sandbox/win/src/sandbox.h"
-#elif defined(OS_MACOSX)
-#include "content/common/sandbox_init_mac.h"
 #endif
 
 #if BUILDFLAG(ENABLE_CDM_HOST_VERIFICATION)
