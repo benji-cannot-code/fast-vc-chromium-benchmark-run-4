fe1364d160e8c0de073b6c01450c051ade9be606
Nico Weber <thakis@chromium.org>
2017-10-14 15:55:31 +0000

    remove static initializer and exit-time destructor from aw_contents.cc
    no intended behavior change.
    
    Bug: 94925
    Change-Id: Ic8851094ab7f851c3fffdf515502c5e89cefccf4
    Reviewed-on: https://chromium-review.googlesource.com/719159
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508938}

--- android_webview/browser/aw_contents.cc
+++ android_webview/browser/aw_contents.cc
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/json/json_writer.h"
 #include "base/location.h"
+#include "base/macros.h"
 #include "base/memory/memory_pressure_listener.h"
 #include "base/memory/ptr_util.h"
 #include "base/pickle.h"
@@ -109,9 +110,15 @@ namespace {
 
 bool g_should_download_favicons = false;
 
-std::string g_locale;
+std::string* g_locale() {
+  CR_DEFINE_STATIC_LOCAL(std::string, locale, ());
+  return &locale;
+}
 
-std::string g_locale_list;
+std::string* g_locale_list() {
+  CR_DEFINE_STATIC_LOCAL(std::string, locale_list, ());
+  return &locale_list;
+}
 
 const void* const kAwContentsUserDataKey = &kAwContentsUserDataKey;
 const void* const kComputedRendererPriorityUserDataKey =
@@ -171,18 +178,18 @@ void UpdateDefaultLocale(JNIEnv* env,
                          const JavaParamRef<jclass>&,
                          const JavaParamRef<jstring>& locale,
                          const JavaParamRef<jstring>& locale_list) {
-  g_locale = ConvertJavaStringToUTF8(env, locale);
-  g_locale_list = ConvertJavaStringToUTF8(env, locale_list);
+  *g_locale() = ConvertJavaStringToUTF8(env, locale);
+  *g_locale_list() = ConvertJavaStringToUTF8(env, locale_list);
 }
 
 // static
 std::string AwContents::GetLocale() {
-  return g_locale;
+  return *g_locale();
 }
 
 // static
 std::string AwContents::GetLocaleList() {
-  return g_locale_list;
+  return *g_locale_list();
 }
 
 // static
