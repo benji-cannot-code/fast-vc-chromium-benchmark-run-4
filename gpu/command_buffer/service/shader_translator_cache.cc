74fd5bfa754c7819bbded1ec0664e42c8ac111ac
Sunny Sachanandani <sunnyps@chromium.org>
2017-10-14 02:08:43 +0000

    gpu: Fix const ref to temporary.
    ShaderTranslatorCache keeps a const ref to the GpuPreferences passed to
    it, but that may be a temporary like in egl::Context[1] used for
    gles2_conform_test. This causes subtle bugs in the conformance tests
    on mac when GpuPreferences memory layout is changed.
    
    Found while trying to remove the old scheduling code in
    crrev.com/c/699418.
    
    [1] https://cs.chromium.org/chromium/src/gpu/gles2_conform_support/egl/context.cc?l=65&rcl=abd229a5e99863d497ad07bcd9284bfd3f86be89
    
    BUG=736117
    R=kbr
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I0ca6b35dc3aa6e9620c665b77778861d59a6c73f
    Reviewed-on: https://chromium-review.googlesource.com/720246
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508914}

--- gpu/command_buffer/service/shader_translator_cache.cc
+++ gpu/command_buffer/service/shader_translator_cache.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <GLES2/gl2.h>
 
-#include "gpu/command_buffer/service/gpu_preferences.h"
-
 namespace gpu {
 namespace gles2 {
 
