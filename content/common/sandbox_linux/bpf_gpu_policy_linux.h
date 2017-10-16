98af52a2c112b6cde6e9bdea71d80f11aa36c63b
Tom Sepez <tsepez@chromium.org>
2017-10-16 19:10:00 +0000

    Introducing content/common/sandbox_init_gpu_linux.cc
    First patch in a series to split the content/gpu warmup from the
    sandboxing infrastructure so that it can move out from under content.
    
    A subsequent patch will remove the calls from bpf_* back into
    /sandbox_init_gpu_linux.cc in favor of passing closures.
    
    Freed from these dependencies, the content/common/sandbox_linux
    code can move into services/service_manager/sandbox/linux in a
    third patch.
    
    Eventually, sandbox_init_gpu_linux.cc itself can move nearer to gpu/.
    
    Also move sandbox_init_linux.cc up a level so that it can be next to
    its win and mac counterparts.  This file will not move along with the
    other sandbox_linux files, so move it now.
    
    Other functional changes are:
    Adding a set_broker_process() to the GpuProcessPolicy.
    Removing bare new in favor of unique_ptrs.
    Removing some used-once statics in favor of lambdas.
    
    
    Change-Id: I66ce7d86c6c697d1704aac19c1fc5d6f8fd318be
    Reviewed-on: https://chromium-review.googlesource.com/716711
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509125}

--- content/common/sandbox_linux/bpf_gpu_policy_linux.h
+++ content/common/sandbox_linux/bpf_gpu_policy_linux.h
@@ -6,19 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_COMMON_SANDBOX_LINUX_BPF_GPU_POLICY_LINUX_H_
 #define CONTENT_COMMON_SANDBOX_LINUX_BPF_GPU_POLICY_LINUX_H_
 
-#include <string>
-#include <vector>
+#include <memory>
 
-#include "base/callback_forward.h"
+#include "base/logging.h"
 #include "base/macros.h"
 #include "content/common/sandbox_linux/sandbox_bpf_base_policy_linux.h"
-
-namespace sandbox {
-namespace syscall_broker {
-class BrokerFilePermission;
-class BrokerProcess;
-}
-}
+#include "sandbox/linux/syscall_broker/broker_process.h"
 
 namespace content {
 
@@ -32,20 +25,14 @@ class GpuProcessPolicy : public SandboxBPFBasePolicy {
 
   bool PreSandboxHook() override;
 
- protected:
-  // Start a broker process to handle open() inside the sandbox.
-  // |broker_sandboxer_allocator| is a function pointer which can allocate a
-  // suitable sandbox policy for the broker process itself.
-  // |permissions_extra| is a list of file permissions
-  // that should be whitelisted by the broker process, in addition to
-  // the basic ones.
-  void InitGpuBrokerProcess(
-      sandbox::bpf_dsl::Policy* (*broker_sandboxer_allocator)(void),
-      const std::vector<sandbox::syscall_broker::BrokerFilePermission>&
-          permissions_extra);
-
-  sandbox::syscall_broker::BrokerProcess* broker_process() {
+  sandbox::syscall_broker::BrokerProcess* broker_process() const {
     return broker_process_;
+  }
+
+  void set_broker_process(
+      std::unique_ptr<sandbox::syscall_broker::BrokerProcess> broker_process) {
+    DCHECK(!broker_process_);
+    broker_process_ = broker_process.release();
   }
 
  private:
@@ -54,11 +41,21 @@ class GpuProcessPolicy : public SandboxBPFBasePolicy {
   // this runs from a SIGSYS handler triggered by the seccomp-bpf sandbox.
   // This should never be destroyed, as after the sandbox is started it is
   // vital to the process.
-  // This is allocated by InitGpuBrokerProcess, called from PreSandboxHook(),
-  // which executes iff the sandbox is going to be enabled afterwards.
-  sandbox::syscall_broker::BrokerProcess* broker_process_;
+  sandbox::syscall_broker::BrokerProcess* broker_process_;  // Leaked as global.
 
   DISALLOW_COPY_AND_ASSIGN(GpuProcessPolicy);
+};
+
+class GpuBrokerProcessPolicy : public GpuProcessPolicy {
+ public:
+  GpuBrokerProcessPolicy();
+  ~GpuBrokerProcessPolicy() override;
+
+  sandbox::bpf_dsl::ResultExpr EvaluateSyscall(
+      int system_call_number) const override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(GpuBrokerProcessPolicy);
 };
 
 }  // namespace content
