b9175d8248b6fa588c05cffc27612979b3a60f57
Tetsui Ohkubo <tetsui@chromium.org>
2017-10-16 05:18:14 +0000

    Activate native notification widget when clicked
    Previously, Chrome OS native notification (NotificationViewMD) doesn't
    get activated on click, while ARC notification (ArcNotificationView)
    does.
    This CL adds NotificationViewMD::EventForwarder, based on the
    implementation of ArcNotificationContentView::EventForwarder.
    
    This change is required for two reasons:
    - Consistency with ARC notifications
    - Future implementation of inline reply notification
    
    TEST=manual
    BUG=773618
    
    Change-Id: I728cdfdd45896cf2a317b9d7e41d1a097e58aa4f
    Reviewed-on: https://chromium-review.googlesource.com/718056
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508987}

--- ui/message_center/views/notification_view_md.cc
+++ ui/message_center/views/notification_view_md.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/native_cursor.h"
 #include "ui/views/view_targeter.h"
 #include "ui/views/widget/widget.h"
+#include "ui/views/widget/widget_delegate.h"
 
 namespace message_center {
 
@@ -111,6 +112,25 @@ gfx::FontList GetTextFontList() {
   DCHECK_EQ(kTextFontSize, font.GetFontSize());
   return gfx::FontList(font);
 }
+
+class ClickActivator : public ui::EventHandler {
+ public:
+  explicit ClickActivator(NotificationViewMD* owner) : owner_(owner) {}
+  ~ClickActivator() override = default;
+
+ private:
+  // ui::EventHandler
+  void OnEvent(ui::Event* event) override {
+    if (event->type() == ui::ET_MOUSE_PRESSED ||
+        event->type() == ui::ET_GESTURE_TAP) {
+      owner_->Activate();
+    }
+  }
+
+  NotificationViewMD* const owner_;
+
+  DISALLOW_COPY_AND_ASSIGN(ClickActivator);
+};
 
 }  // anonymous namespace
 
@@ -418,9 +438,19 @@ NotificationViewMD::NotificationViewMD(MessageCenterController* controller,
   SetEventTargeter(
       std::unique_ptr<views::ViewTargeter>(new views::ViewTargeter(this)));
   set_notify_enter_exit_on_child(true);
+
+  click_activator_ = std::make_unique<ClickActivator>(this);
+  // Reasons to use pretarget handler instead of OnMousePressed:
+  // - To make it look similar to ArcNotificationContentView::EventForwarder.
+  // - If we're going to support inline reply feature in native notification,
+  //   then NotificationViewMD::OnMousePresssed would not fire anymore on the
+  //   Textfield click.
+  AddPreTargetHandler(click_activator_.get());
 }
 
-NotificationViewMD::~NotificationViewMD() {}
+NotificationViewMD::~NotificationViewMD() {
+  RemovePreTargetHandler(click_activator_.get());
+}
 
 void NotificationViewMD::Layout() {
   MessageView::Layout();
@@ -896,6 +926,11 @@ void NotificationViewMD::SetExpanded(bool expanded) {
   content_row_->InvalidateLayout();
   if (controller())
     controller()->UpdateNotificationSize(notification_id());
+}
+
+void NotificationViewMD::Activate() {
+  GetWidget()->widget_delegate()->set_can_activate(true);
+  GetWidget()->Activate();
 }
 
 }  // namespace message_center
