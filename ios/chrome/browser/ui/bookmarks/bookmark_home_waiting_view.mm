2ee50bd2702dd496bd36985c944c5236ef5e9733
Marti Wong <martiw@chromium.org>
2017-10-16 21:12:18 +0000

    Change the color of spinning loader icon to blue only (iOS bookmarks)
    Per UI review request, make the loader icon to Blue-only rather than
    4-color google spinner to avoid initial red (error looking) state.
    
    screenshot:
    https://drive.google.com/file/d/0B1dtd3IUt4I7ZlhZRkl1SEE4OVU
    
    demo video:
    https://drive.google.com/open?id=0B1dtd3IUt4I7X0hVcWdEUERmdWs
    
    Bug: 774500
    Change-Id: I9bc3834ec92865658029ed5d4a5f197c366f95c3
    Reviewed-on: https://chromium-review.googlesource.com/719056
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509171}

--- ios/chrome/browser/ui/bookmarks/bookmark_home_waiting_view.mm
+++ ios/chrome/browser/ui/bookmarks/bookmark_home_waiting_view.mm
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/bookmarks/bookmark_home_waiting_view.h"
 
 #import "ios/chrome/browser/ui/bookmarks/bookmark_utils_ios.h"
+#import "ios/chrome/browser/ui/colors/MDCPalette+CrAdditions.h"
 #import "ios/chrome/browser/ui/material_components/activity_indicator.h"
 #import "ios/chrome/browser/ui/rtl_geometry.h"
 #import "ios/third_party/material_components_ios/src/components/ActivityIndicator/src/MaterialActivityIndicator.h"
+#import "ios/third_party/material_components_ios/src/components/Palettes/src/MaterialPalettes.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -54,7 +56,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         UIViewAutoresizingFlexibleBottomMargin;
     self.activityIndicator.center = CGPointMake(
         CGRectGetWidth(self.bounds) / 2, CGRectGetHeight(self.bounds) / 2);
-    self.activityIndicator.cycleColors = ActivityIndicatorBrandedCycleColors();
+    self.activityIndicator.cycleColors =
+        @[ [[MDCPalette cr_bluePalette] tint500] ];
     [self addSubview:self.activityIndicator];
     [self.activityIndicator startAnimating];
   });
