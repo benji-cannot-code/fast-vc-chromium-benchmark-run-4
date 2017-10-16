5de5e68f52796022c5cd6b9e964920f7ef680c7b
Hiroki Nakagawa <nhiroki@chromium.org>
2017-10-16 09:50:31 +0000

    Worklet: Replace classic script eval with module script eval in unit tests
    Worklet should be executed as module scripts as opposed to classic scripts.
    
    Bug: 773772
    Change-Id: Ib32899fa776bcf56be5ec5f58eb84b04df7bd603
    Reviewed-on: https://chromium-review.googlesource.com/717957
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509006}

--- third_party/WebKit/Source/modules/compositorworker/AnimationWorkletGlobalScope.h
+++ third_party/WebKit/Source/modules/compositorworker/AnimationWorkletGlobalScope.h
@@ -72,6 +72,12 @@ class MODULES_EXPORT AnimationWorkletGlobalScope
   AnimatorList animators_;
 };
 
+DEFINE_TYPE_CASTS(AnimationWorkletGlobalScope,
+                  ExecutionContext,
+                  context,
+                  context->IsAnimationWorkletGlobalScope(),
+                  context.IsAnimationWorkletGlobalScope());
+
 }  // namespace blink
 
 #endif  // AnimationWorkletGlobalScope_h
