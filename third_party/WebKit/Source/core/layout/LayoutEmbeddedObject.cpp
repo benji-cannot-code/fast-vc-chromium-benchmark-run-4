15876cd38f090161676dc30b339efbac8722f2b8
Bhagirathi Satpathy <bhagirathi.s@samsung.com>
2017-10-16 04:36:59 +0000

    Remove unused HTMLIFrameElement.h inclusion from LayoutEmbeddedObject.cpp
    HTMLIFrameElement.h is included in LayoutEmbeddedObject.cpp but not used.
    Removed this unnecessary header.
    
    Bug: 
    Change-Id: Ib956754f2daf75c5cc3c4a15334621997b1011f8
    Reviewed-on: https://chromium-review.googlesource.com/718796
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508982}

--- third_party/WebKit/Source/core/layout/LayoutEmbeddedObject.cpp
+++ third_party/WebKit/Source/core/layout/LayoutEmbeddedObject.cpp
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/CSSValueKeywords.h"
 #include "core/frame/LocalFrame.h"
 #include "core/frame/LocalFrameView.h"
-#include "core/html/HTMLIFrameElement.h"
 #include "core/html/HTMLPlugInElement.h"
 #include "core/html_names.h"
 #include "core/layout/LayoutAnalyzer.h"
