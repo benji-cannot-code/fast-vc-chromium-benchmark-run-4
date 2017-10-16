6e18b3d065efea6e4d4f62ad9e97fc2100517898
Maks Orlovich <morlovich@chromium.org>
2017-10-16 14:26:34 +0000

    SimpleCache: deflake SimpleCacheEnumerationCorruption test on Fuchsia (and re-enable it on Android).
    The trick of serializing I/O ops by doing a read-after-write to ensure
    the write commit doesn't work on stream 0 since that's in memory now;
    but even doing it on stream 1 would still potentially race the writes
    done in close with the call to CreateCorruptFileForTests... so just use
    a thread + scheduler flush to synchronize instead.
    
    Bug: 638891
    Change-Id: Ie2d06fd97f9356b695e138cc7d63b8ff06892b47
    Reviewed-on: https://chromium-review.googlesource.com/710394
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509039}

--- net/disk_cache/backend_unittest.cc
+++ net/disk_cache/backend_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/mock_entropy_provider.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/third_party/dynamic_annotations/dynamic_annotations.h"
 #include "base/threading/platform_thread.h"
 #include "base/threading/thread_restrictions.h"
@@ -43,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/disk_cache/simple/simple_test_util.h"
 #include "net/disk_cache/simple/simple_util.h"
 #include "net/test/gtest_util.h"
+#include "net/test/net_test_suite.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -4014,15 +4016,11 @@ TEST_F(DiskCacheBackendTest, SimpleCacheEnumerationWhileDoomed) {
   EXPECT_TRUE(keys_to_match.empty());
 }
 
-// This test is flaky on Android Marshmallow crbug.com/638891.
-#if !defined(OS_ANDROID)
 // Tests that enumerations are not affected by corrupt files.
 TEST_F(DiskCacheBackendTest, SimpleCacheEnumerationCorruption) {
   SetSimpleCacheMode();
   InitCache();
-  // Create a corrupt entry. The write/read sequence ensures that the entry will
-  // have been created before corrupting the platform files, in the case of
-  // optimistic operations.
+  // Create a corrupt entry.
   const std::string key = "the key";
   disk_cache::Entry* corrupted_entry;
 
@@ -4035,6 +4033,8 @@ TEST_F(DiskCacheBackendTest, SimpleCacheEnumerationCorruption) {
             WriteData(corrupted_entry, 0, 0, buffer.get(), kSize, false));
   ASSERT_EQ(kSize, ReadData(corrupted_entry, 0, 0, buffer.get(), kSize));
   corrupted_entry->Close();
+  // Let all I/O finish so it doesn't race with corrupting the file below.
+  NetTestSuite::GetScopedTaskEnvironment()->RunUntilIdle();
 
   std::set<std::string> key_pool;
   ASSERT_TRUE(CreateSetOfRandomEntries(&key_pool));
@@ -4053,7 +4053,6 @@ TEST_F(DiskCacheBackendTest, SimpleCacheEnumerationCorruption) {
   EXPECT_EQ(key_pool.size(), count);
   EXPECT_TRUE(keys_to_match.empty());
 }
-#endif
 
 // Tests that enumerations don't leak memory when the backend is destructed
 // mid-enumeration.
