fcf9957ad0dc2a801d84b034a6b90f67490d4c68
Yoichi Osato <yoichio@chromium.org>
2017-10-16 09:29:37 +0000

    Use nullopt for non Text start/end Node in SelectionPaintRange.
    We used to return 0 for such Node but the value should
    not be used because offset is valid only for Text node.
    This patch changes offset type from int to Optional<int> and
    returns nullopt for non Text node.
    
    Bug: 739062
    Change-Id: Ia1035ee5f494cb52b1f6ae2fbed73c219a6e8ce7
    Reviewed-on: https://chromium-review.googlesource.com/711556
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509002}

--- third_party/WebKit/Source/core/editing/LayoutSelectionTest.cpp
+++ third_party/WebKit/Source/core/editing/LayoutSelectionTest.cpp
@@ -188,6 +188,8 @@ TEST_F(LayoutSelectionTest, TraverseLayoutObjectBRs) {
   TEST_NEXT(IsBR, kInside, ShouldInvalidate);
   TEST_NEXT(IsBR, kEnd, ShouldInvalidate);
   TEST_NO_NEXT_LAYOUT_OBJECT();
+  EXPECT_FALSE(Selection().LayoutSelectionStart().has_value());
+  EXPECT_FALSE(Selection().LayoutSelectionEnd().has_value());
 }
 
 TEST_F(LayoutSelectionTest, TraverseLayoutObjectListStyleImage) {
@@ -410,6 +412,8 @@ TEST_F(LayoutSelectionTest, SelectImage) {
   TEST_NEXT(IsLayoutBlock, kStartAndEnd, NotInvalidate);
   TEST_NEXT(IsLayoutImage, kStartAndEnd, ShouldInvalidate);
   TEST_NO_NEXT_LAYOUT_OBJECT();
+  EXPECT_FALSE(Selection().LayoutSelectionStart().has_value());
+  EXPECT_FALSE(Selection().LayoutSelectionEnd().has_value());
 }
 
 TEST_F(LayoutSelectionTest, MoveOnSameNode_Start) {
