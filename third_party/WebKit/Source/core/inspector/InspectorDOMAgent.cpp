2f2a7877b54cfeb65b34094991764bba230e08ec
Rakina Zata Amni <rakina@chromium.org>
2017-10-16 04:59:15 +0000

    Check if slot is in a shadow tree before building its distributed nodes
    If you open a page with a slot that's not under a shadow tree
    and try to inspect that slot, it will crash because of a DCHECK
    when trying to build distributed nodes for that slot.
    
    Solution: Added a condition to check whether a slot is in a shadow tree
    or not before building distributed nodes for that slot.
    
    Bug: 715033
    Change-Id: Ic778c1ebff95d6a035d2bb53932ac50761357beb
    Reviewed-on: https://chromium-review.googlesource.com/715976
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Takayoshi Kochi <kochi@chromium.org>
    Commit-Queue: Rakina Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508985}

--- third_party/WebKit/Source/core/inspector/InspectorDOMAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorDOMAgent.cpp
@@ -1517,8 +1517,10 @@ std::unique_ptr<protocol::DOM::Node> InspectorDOMAgent::BuildObjectForNode(
       force_push_children = true;
     }
     if (auto* slot = ToHTMLSlotElementOrNull(*element)) {
-      value->setDistributedNodes(BuildDistributedNodesForSlot(slot));
-      force_push_children = true;
+      if (node->IsInShadowTree()) {
+        value->setDistributedNodes(BuildDistributedNodesForSlot(slot));
+        force_push_children = true;
+      }
     }
   } else if (node->IsDocumentNode()) {
     Document* document = ToDocument(node);
