e10efea180d051eff9ad76c1bd191f976a3648da
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2017-10-16 20:33:39 +0000

    IWYU: include sys/stat.h in storage_monitor_linux.cc
    The code uses stat(2), but never included the appropriate header, which is
    currently implicitly added via the udev headers.
    
    Change-Id: I0640cef6e023217962fbccba507a51905f1b608e
    Reviewed-on: https://chromium-review.googlesource.com/715736
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Raphael Kubo da Costa (rakuco) <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/master@{#509154}

--- components/storage_monitor/storage_monitor_linux.cc
+++ components/storage_monitor/storage_monitor_linux.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <mntent.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <sys/stat.h>
+
 #include <limits>
 #include <list>
 #include <utility>
