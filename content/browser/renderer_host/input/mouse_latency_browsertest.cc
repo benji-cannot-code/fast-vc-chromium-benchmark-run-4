1b61a384b514242dd8100f10f57be57be9890350
Ehsan Chiniforooshan <chiniforooshan@chromium.org>
2017-10-16 19:11:14 +0000

    Fix a mouse latency browsertest bug
    The quit closure of a runloop is passed to the StartTracing call but
    the runloop is never actually run. So, the test is not properly
    waiting for the completion of StartTracing. This was not a problem
    before 507716 since the tracing controller would immidiately start
    tracing in the browser process. However the controller starts tracing
    in the browser by a mojo call now which will create a PostTask.
    
    BUG=775040
    
    Change-Id: Ib74267c66a45eeba5065170a6727dfaa7e81846f
    Reviewed-on: https://chromium-review.googlesource.com/721762
    Commit-Queue: Ehsan Chiniforooshan <chiniforooshan@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509127}

--- content/browser/renderer_host/input/mouse_latency_browsertest.cc
+++ content/browser/renderer_host/input/mouse_latency_browsertest.cc
@@ -122,6 +122,7 @@ class MouseLatencyBrowserTest : public ContentBrowserTest {
     base::RunLoop run_loop;
     ASSERT_TRUE(TracingController::GetInstance()->StartTracing(
         trace_config, run_loop.QuitClosure()));
+    run_loop.Run();
   }
 
   const base::Value& StopTracing() {
@@ -150,8 +151,7 @@ class MouseLatencyBrowserTest : public ContentBrowserTest {
 // MouseDown events in the case where no swap is generated.
 // Disabled on Android because we don't support synthetic mouse input on
 // Android (crbug.com/723618).
-// Flaky on Mac10.10 (crbug.com/774169).
-#if defined(OS_ANDROID) || defined(OS_MACOSX)
+#if defined(OS_ANDROID)
 #define MAYBE_MouseDownAndUpRecordedWithoutSwap \
   DISABLED_MouseDownAndUpRecordedWithoutSwap
 #else
