46574e9f56fff56021f7edd85821adee69d43c8d
Mikhail Atuchin <atuchin@yandex-team.ru>
2017-10-16 20:11:59 +0000

    Fix use-after-free in MHTMLGenerationManager
    This CL fix using data of died WebContents by:
    1. Replacing a raw pointer to weak for holding MHTMLExtraPartsImpl.
    2. Passing the collected data instead of passing raw pointer
       to FILE-thread.
    
    BUG=None
    TEST=content_browsertests.MHTMLGenerationTest.*
    
    Change-Id: I1eea6e086b241fff0fdb2c697d7a2aea9ecd09c1
    Reviewed-on: https://chromium-review.googlesource.com/711834
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Mikhail Atuchin <atuchin@yandex-team.ru>
    Cr-Commit-Position: refs/heads/master@{#509148}

--- content/browser/download/mhtml_generation_manager.cc
+++ content/browser/download/mhtml_generation_manager.cc
@@ -113,15 +113,16 @@ class MHTMLGenerationManager::Job : public RenderProcessHostObserver {
       MhtmlSaveStatus save_status,
       const std::string& boundary,
       base::File file,
-      const MHTMLExtraPartsImpl* extra_parts);
+      const std::vector<MHTMLExtraDataPart>& extra_data_parts);
   void AddFrame(RenderFrameHost* render_frame_host);
 
   // If we have any extra MHTML parts to write out, write them into the file
   // while on the file thread.  Returns true for success, or if there is no data
   // to write.
-  static bool WriteExtraDataParts(const std::string& boundary,
-                                  base::File& file,
-                                  const MHTMLExtraPartsImpl* extra_parts);
+  static bool WriteExtraDataParts(
+      const std::string& boundary,
+      base::File& file,
+      const std::vector<MHTMLExtraDataPart>& extra_data_parts);
 
   // Writes the footer into the MHTML file.  Returns false for faiulre.
   static bool WriteFooter(const std::string& boundary, base::File& file);
@@ -183,7 +184,7 @@ class MHTMLGenerationManager::Job : public RenderProcessHostObserver {
   bool is_finished_;
 
   // Any extra data parts that should be emitted into the output MHTML.
-  MHTMLExtraPartsImpl* extra_parts_;
+  std::vector<MHTMLExtraDataPart> extra_data_parts_;
 
   // RAII helper for registering this Job as a RenderProcessHost observer.
   ScopedObserver<RenderProcessHost, MHTMLGenerationManager::Job>
@@ -216,8 +217,10 @@ MHTMLGenerationManager::Job::Job(int job_id,
              ->parent() == nullptr);
 
   // Save off any extra data.
-  extra_parts_ = static_cast<MHTMLExtraPartsImpl*>(
+  auto* extra_parts = static_cast<MHTMLExtraPartsImpl*>(
       MHTMLExtraParts::FromWebContents(web_contents));
+  if (extra_parts)
+    extra_data_parts_ = extra_parts->parts();
 }
 
 MHTMLGenerationManager::Job::~Job() {
@@ -390,7 +393,7 @@ void MHTMLGenerationManager::Job::CloseFile(
           save_status,
           (save_status == MhtmlSaveStatus::SUCCESS ? mhtml_boundary_marker_
                                                    : std::string()),
-          base::Passed(&browser_file_), extra_parts_),
+          base::Passed(&browser_file_), base::Passed(&extra_data_parts_)),
       callback);
 }
 
@@ -440,7 +443,7 @@ MHTMLGenerationManager::Job::FinalizeAndCloseFileOnFileThread(
     MhtmlSaveStatus save_status,
     const std::string& boundary,
     base::File file,
-    const MHTMLExtraPartsImpl* extra_parts) {
+    const std::vector<MHTMLExtraDataPart>& extra_data_parts) {
   DCHECK(GetDownloadTaskRunner()->RunsTasksInCurrentSequence());
 
   // If no previous error occurred the boundary should have been provided.
@@ -450,7 +453,7 @@ MHTMLGenerationManager::Job::FinalizeAndCloseFileOnFileThread(
     DCHECK(!boundary.empty());
 
     // Write the extra data into a part of its own, if we have any.
-    if (!WriteExtraDataParts(boundary, file, extra_parts)) {
+    if (!WriteExtraDataParts(boundary, file, extra_data_parts)) {
       save_status = MhtmlSaveStatus::FILE_WRITTING_ERROR;
     }
 
@@ -476,13 +479,9 @@ MHTMLGenerationManager::Job::FinalizeAndCloseFileOnFileThread(
 bool MHTMLGenerationManager::Job::WriteExtraDataParts(
     const std::string& boundary,
     base::File& file,
-    const MHTMLExtraPartsImpl* extra_parts) {
+    const std::vector<MHTMLExtraDataPart>& extra_data_parts) {
   DCHECK(GetDownloadTaskRunner()->RunsTasksInCurrentSequence());
   // Don't write an extra data part if there is none.
-  if (extra_parts == nullptr)
-    return true;
-
-  const std::vector<MHTMLExtraDataPart>& extra_data_parts(extra_parts->parts());
   if (extra_data_parts.empty())
     return true;
 
@@ -490,7 +489,7 @@ bool MHTMLGenerationManager::Job::WriteExtraDataParts(
 
   // For each extra part, serialize that part and add to our accumulator
   // string.
-  for (auto part : extra_data_parts) {
+  for (const auto& part : extra_data_parts) {
     // Write a newline, then a boundary, a newline, then the content
     // location, a newline, the content type, a newline, extra_headers,
     // two newlines, the body, and end with a newline.
