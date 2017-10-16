66a243b88ac237a9159eb34f2de28825f8ad3aaa
Chris Pickel <sfiera@chromium.org>
2017-10-16 12:34:18 +0000

    Local NTP: don't set tabindex
    Allow the tab order to be set naturally by the flow of the document.
    
    Also, improve tab/space navigation on non-white NTPs by automatically
    focusing the doodle after the notifier is clicked.
    
    Bug: 774147
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I7a6154fce4ce983f55ea60a318a97dd44aab2954
    Reviewed-on: https://chromium-review.googlesource.com/720917
    Commit-Queue: Chris Pickel <sfiera@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509027}

--- chrome/browser/resources/local_ntp/local_ntp.js
+++ chrome/browser/resources/local_ntp/local_ntp.js
@@ -675,6 +675,9 @@ function init() {
       state.notheme = true;
       window.history.replaceState(state, document.title);
       onThemeChange();
+      if (e.detail === 0) {  // Activated by keyboard.
+        $(IDS.LOGO_DOODLE_BUTTON).focus();
+      }
     });
   } else {
     document.body.classList.add(CLASSES.NON_GOOGLE_PAGE);
@@ -702,7 +705,6 @@ function init() {
   // Create the most visited iframe.
   var iframe = document.createElement('iframe');
   iframe.id = IDS.TILES_IFRAME;
-  iframe.tabIndex = 1;
   iframe.src = 'chrome-search://most-visited/single.html?' + args.join('&');
   $(IDS.TILES).appendChild(iframe);
 
