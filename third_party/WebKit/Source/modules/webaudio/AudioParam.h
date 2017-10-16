f3258261ed3639535101f04283bdeb3b767e3fa3
Raymond Toy <rtoy@chromium.org>
2017-10-16 22:42:04 +0000

    Remove BiquadFilter.Q histograms
    The definition of the Q parameter for lowpass and highpass filters
    were changed a while ago in the WebAudio specification.  These
    were added to determine how much impact this change would have because
    the change is aubible.
    
    It's been over a year, and no bugs have been filed on this, and the
    histograms show that the default values are, by far, the most common
    values.  Hence, we can remove these histograms.
    
    Bug: 774526
    Test: none
    Change-Id: I17ad180a40964bd4d81988492d43e522a47f2f2f
    Reviewed-on: https://chromium-review.googlesource.com/718839
    Commit-Queue: Raymond Toy <rtoy@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509187}

--- third_party/WebKit/Source/modules/webaudio/AudioParam.h
+++ third_party/WebKit/Source/modules/webaudio/AudioParam.h
@@ -54,8 +54,6 @@ enum AudioParamType {
   kParamTypeAudioBufferSourceDetune,
   kParamTypeBiquadFilterFrequency,
   kParamTypeBiquadFilterQ,
-  kParamTypeBiquadFilterQLowpass,
-  kParamTypeBiquadFilterQHighpass,
   kParamTypeBiquadFilterGain,
   kParamTypeBiquadFilterDetune,
   kParamTypeDelayDelayTime,
@@ -162,9 +160,6 @@ class AudioParamHandler final : public ThreadSafeRefCounted<AudioParamHandler>,
   void Disconnect(AudioNodeOutput&);
 
   float IntrinsicValue() const { return NoBarrierLoad(&intrinsic_value_); }
-
-  // Update any histograms with the given value.
-  void UpdateHistograms(float new_value);
 
  private:
   AudioParamHandler(BaseAudioContext&,
