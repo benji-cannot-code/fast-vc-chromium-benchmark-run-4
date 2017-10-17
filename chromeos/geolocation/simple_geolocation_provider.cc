0b0c60452036ec22e92880c207241524ded1de77
Ben Chan <benchan@chromium.org>
2017-10-17 18:48:48 +0000

    chromeos: geolocation: replace base::MakeUnique with std::make_unique
    Bug: 755727
    Change-Id: I4c2a8db5950d867140cca53b6d28747d6eae9b31
    Reviewed-on: https://chromium-review.googlesource.com/722177
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Ben Chan <benchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509457}

--- chromeos/geolocation/simple_geolocation_provider.cc
+++ chromeos/geolocation/simple_geolocation_provider.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <iterator>
+#include <memory>
 
 #include "base/bind.h"
 #include "base/memory/ptr_util.h"
@@ -41,8 +42,8 @@ void SimpleGeolocationProvider::RequestGeolocation(
     SimpleGeolocationRequest::ResponseCallback callback) {
   DCHECK(thread_checker_.CalledOnValidThread());
 
-  auto cell_vector = base::MakeUnique<chromeos::CellTowerVector>();
-  auto wifi_vector = base::MakeUnique<chromeos::WifiAccessPointVector>();
+  auto cell_vector = std::make_unique<chromeos::CellTowerVector>();
+  auto wifi_vector = std::make_unique<chromeos::WifiAccessPointVector>();
 
   // Mostly necessary for testing and rare cases where NetworkHandler is not
   // initialized: in that case, calls to Get() will fail.
