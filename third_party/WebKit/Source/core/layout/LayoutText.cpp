789d19a069dba0b3329b614b25a7a3cabd15ef24
Xiaocheng Hu <xiaochengh@chromium.org>
2017-10-17 06:20:07 +0000

    [LayoutNG] Handle ::first-letter in three LayoutText methods
    This patch adds ::first-letter handling to the following functions by adding
    LayoutTextFragment overrides:
    - LayoutText::CaretMinOffset
    - LayoutText::CaretMaxOffset
    - LayoutText::ResolvedTextLength
    
    Bug: 771398
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I5309e706a5161498b9e909b9d2098a232e328020
    Reviewed-on: https://chromium-review.googlesource.com/719410
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509299}

--- third_party/WebKit/Source/core/layout/LayoutText.cpp
+++ third_party/WebKit/Source/core/layout/LayoutText.cpp
@@ -1917,7 +1917,8 @@ const NGOffsetMappingResult& LayoutText::GetNGOffsetMapping() const {
 
 int LayoutText::CaretMinOffset() const {
   if (ShouldUseNGAlternatives()) {
-    // TODO(xiaochengh): Support ::first-letter.
+    // ::first-letter handling should be done by LayoutTextFragment override.
+    DCHECK(!IsTextFragment());
     if (!GetNode())
       return 0;
     const unsigned candidate =
@@ -1938,7 +1939,8 @@ int LayoutText::CaretMinOffset() const {
 
 int LayoutText::CaretMaxOffset() const {
   if (ShouldUseNGAlternatives()) {
-    // TODO(xiaochengh): Support ::first-letter.
+    // ::first-letter handling should be done by LayoutTextFragment override.
+    DCHECK(!IsTextFragment());
     if (!GetNode())
       return TextLength();
     const unsigned candidate =
@@ -1961,7 +1963,8 @@ int LayoutText::CaretMaxOffset() const {
 
 unsigned LayoutText::ResolvedTextLength() const {
   if (ShouldUseNGAlternatives()) {
-    // TODO(xiaochengh): Support ::first-letter.
+    // ::first-letter handling should be done by LayoutTextFragment override.
+    DCHECK(!IsTextFragment());
     if (!GetNode())
       return 0;
     const NGOffsetMappingResult& mapping = GetNGOffsetMapping();
