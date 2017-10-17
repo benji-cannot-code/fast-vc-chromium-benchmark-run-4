b42c2534626bdb2b51a8ad7ec4c9f210ee994a3e
Alex Moshchuk <alexmos@chromium.org>
2017-10-17 23:20:32 +0000

    Add more crash keys for renderer kills.
    This adds some more information for CHECKs that fire in origin lock
    mismatches, which we're seeing in the --site-per-process trial.
    
    It also adds a crash key for the origin lock in
    CanAccessDataForOrigin, since the existing crash key was not picked up
    for either GetCookies kills or local storage kills.
    
    Bug: 773809
    Change-Id: I81e9a06e7c001d2520e3a49b18b6cbd63be32931
    Reviewed-on: https://chromium-review.googlesource.com/723851
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509585}

--- content/browser/site_instance_impl.cc
+++ content/browser/site_instance_impl.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/site_instance_impl.h"
 
 #include "base/command_line.h"
+#include "base/debug/crash_logging.h"
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "content/browser/browsing_instance.h"
@@ -540,6 +541,10 @@ void SiteInstanceImpl::LockToOriginIfNeeded() {
           HAS_WRONG_LOCK:
         // We should never attempt to reassign a different origin lock to a
         // process.
+        base::debug::SetCrashKeyValue("requested_site_url", site_.spec());
+        base::debug::SetCrashKeyValue(
+            "killed_process_origin_lock",
+            policy->GetOriginLock(process_->GetID()).spec());
         CHECK(false) << "Trying to lock a process to " << site_
                      << " but the process is already locked to "
                      << policy->GetOriginLock(process_->GetID());
@@ -556,6 +561,10 @@ void SiteInstanceImpl::LockToOriginIfNeeded() {
     // If the site that we've just committed doesn't require a dedicated
     // process, make sure we aren't putting it in a process for a site that
     // does.
+    base::debug::SetCrashKeyValue("requested_site_url", site_.spec());
+    base::debug::SetCrashKeyValue(
+        "killed_process_origin_lock",
+        policy->GetOriginLock(process_->GetID()).spec());
     CHECK_EQ(lock_state,
              ChildProcessSecurityPolicyImpl::CheckOriginLockResult::NO_LOCK)
         << "Trying to commit non-isolated site " << site_
