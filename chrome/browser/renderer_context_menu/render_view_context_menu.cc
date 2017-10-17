c47790c4de1c8217eb312b7b9b8e02c9d9f2f29a
Tatiana Gornak <melandory@chromium.org>
2017-10-17 18:50:01 +0000

    Restrict generation context menu item only to non passphrase sync users
    BUG=739343, 775509
    
    Change-Id: I7f9b42cadd63c55c5b59d9787f6a41ff0742a556
    Reviewed-on: https://chromium-review.googlesource.com/707436
    Commit-Queue: Tatiana Gornak <melandory@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509459}

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search/search.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/browser/spellchecker/spellcheck_service.h"
+#include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/translate/chrome_translate_client.h"
 #include "chrome/browser/translate/translate_service.h"
 #include "chrome/browser/ui/autofill/chrome_autofill_client.h"
@@ -82,6 +83,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/metrics/proto/omnibox_input_type.pb.h"
 #include "components/omnibox/browser/autocomplete_classifier.h"
 #include "components/omnibox/browser/autocomplete_match.h"
+#include "components/password_manager/core/browser/password_manager_util.h"
 #include "components/password_manager/core/common/experiments.h"
 #include "components/prefs/pref_member.h"
 #include "components/prefs/pref_service.h"
@@ -1533,7 +1535,9 @@ void RenderViewContextMenu::AppendPasswordItems() {
                                     IDS_CONTENT_CONTEXT_FORCESAVEPASSWORD);
     add_separator = true;
   }
-  if (password_manager::ManualPasswordGenerationEnabled()) {
+  if (password_manager_util::ManualPasswordGenerationEnabled(
+          ProfileSyncServiceFactory::GetSyncServiceForBrowserContext(
+              source_web_contents_->GetBrowserContext()))) {
     menu_model_.AddItemWithStringId(IDC_CONTENT_CONTEXT_GENERATEPASSWORD,
                                     IDS_CONTENT_CONTEXT_GENERATEPASSWORD);
     add_separator = true;
--- components/password_manager/core/browser/password_manager_util.cc
+++ components/password_manager/core/browser/password_manager_util.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "components/autofill/core/common/password_form.h"
 #include "components/password_manager/core/browser/log_manager.h"
+#include "components/password_manager/core/common/password_manager_features.h"
 #include "components/sync/driver/sync_service.h"
 #include "crypto/openssl_util.h"
 #include "third_party/boringssl/src/include/openssl/evp.h"
@@ -117,6 +118,13 @@ uint64_t CalculateSyncPasswordHash(const base::StringPiece16& text,
                     (((static_cast<uint64_t>(hash[4])) & 0x1F) << 32);
 
   return hash37;
+}
+
+bool ManualPasswordGenerationEnabled(syncer::SyncService* sync_service) {
+  return (base::FeatureList::IsEnabled(
+              password_manager::features::kEnableManualPasswordGeneration) &&
+          (password_manager_util::GetPasswordSyncState(sync_service) ==
+           password_manager::SYNCING_NORMAL_ENCRYPTION));
 }
 
 }  // namespace password_manager_util
