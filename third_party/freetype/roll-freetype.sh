f4df13b45f3fb80edc77c2f946c8e15e0f2ea144
Ben Wagner <bungeman@chromium.org>
2017-10-17 14:15:54 +0000

    Roll src/third_party/freetype/src/ ae7dc1f62..94f6d57a4 (32 commits)
    https://chromium.googlesource.com/chromium/src/third_party/freetype2.git/+log/ae7dc1f62d82..94f6d57a4c42
    
    $ git log ae7dc1f62..94f6d57a4 --date=short --no-merges --format='%ad %ae %s'
    2017-10-16 ewaldhew [cff] Fix segfault on missing `psaux' (#52218)
    2017-10-15 apodtele [base, cff] Fix MSVC warnings.
    2017-10-14 apodtele [base] Netpbm image tracing.
    2017-10-14 apodtele * builds/windows/ftdebug.c (FT_Message): Print to stderr.
    2017-10-14 behdad [afshaper] Delay creating `hb_set' objects until needed.
    2017-10-12 ewaldhew * devel/ftoption.h: Enable T1_CONFIG_OPTION_OLD_ENGINE by default.
    2017-10-12 ewaldhew [type1, cid] Add hinting engine switch.
    2017-10-12 ewaldhew Add T1_CONFIG_OPTION_OLD_ENGINE configuration option.
    2017-10-12 ewaldhew Extract width parsing from Type 1 parser.
    2017-10-12 wl [autofit] Better visualize table tracing in source code.
    2017-10-09 wl * src/base/ftoutln.c (FT_Outline_Translate): Fix integer overflow.
    2017-10-08 wl * src/base/ftobjs.c (ft_glyphslot_preset_bitmap): Integer overflows.
    2017-10-08 wl Document global size metrics needed for native bytecode hinting (#52165).
    2017-10-08 wl Prevent creation of an incorrect documentation entry.
    2017-10-07 wl [sfnt] Adjust behaviour of PS font names for variation fonts.
    2017-10-07 wl [cff, truetype] Adjust behaviour of named instances.
    2017-10-07 wl Make `FT_Set_Named_Instance' work.
    2017-10-07 wl Make `FT_FACE_FLAG_VARIATION' work.
    2017-10-07 wl New function `FT_Set_Named_Instance'.
    2017-10-07 wl Add macros for checking whether a font variation is active.
    2017-10-07 wl Add framework for setting named instance in MM service.
    2017-10-07 wl [type1] Minor code shuffling.
    2017-10-05 wl * src/base/ftobjs.c (ft_glyphslot_preset_bitmap): Fix integer overflow.
    2017-10-05 wl Fix compiler warnings.
    2017-10-04 John.Tytgat [cff] Add support for `FSType'.
    2017-10-02 wl CHANGES: Formatting.
    2017-10-01 wl [smooth,type1] Formatting, whitespace.
    2017-10-01 wl [psaux] Formatting, minor fixes, whitespace, copyright notices.
    2017-09-30 wl [base,cff,cid] Whitespace, formatting.
    2017-09-30 wl [include] Whitespace.
    2017-09-30 apodtele Signedness fixes in bitmap presetting.
    2017-09-29 apodtele Bitmap metrics presetting [2/2].
    
    Created with:
      roll-dep src/third_party/freetype/src
    R=bungeman@chromium.org,drott@chromium.org
    
    Change-Id: Ie327d67350aeaec40df6d1a6d4379c8c20c8b58a
    PDFium-bug: https://bugs.chromium.org/p/pdfium/issues/detail?id=918
    Reviewed-on: https://chromium-review.googlesource.com/721779
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Ben Wagner <bungeman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509378}

--- third_party/freetype/roll-freetype.sh
+++ third_party/freetype/roll-freetype.sh
@@ -1,27 +1,45 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/bin/bash
 
-REVIEWERS=`paste -s -d, third_party/freetype/OWNERS` &&
-roll-dep -r "${REVIEWERS}" "$@" src/third_party/freetype/src/ &&
+rolldeps() {
+  STEP="roll-deps" &&
+  REVIEWERS=`paste -s -d, third_party/freetype/OWNERS` &&
+  roll-dep -r "${REVIEWERS}" "$@" src/third_party/freetype/src/
+}
 
-TMPFILE=`mktemp` &&
-git -C third_party/freetype/src/ cat-file blob HEAD@{1}:include/freetype/config/ftoption.h >> ${TMPFILE} &&
-git merge-file third_party/freetype/include/freetype-custom-config/ftoption.h ${TMPFILE} third_party/freetype/src/include/freetype/config/ftoption.h &&
-rm ${TMPFILE} &&
-git add third_party/freetype/include/freetype-custom-config/ftoption.h &&
+checkmodules() {
+  STEP="check modules.cfg: check list of modules and dependencies" &&
+  ! git -C third_party/freetype/src/ diff --name-only HEAD@{1} | grep -q modules.cfg
+}
 
-TMPFILE=`mktemp` &&
-git -C third_party/freetype/src/ cat-file blob HEAD@{1}:include/freetype/config/ftconfig.h >> ${TMPFILE} &&
-git merge-file third_party/freetype/include/freetype-custom-config/ftconfig.h ${TMPFILE} third_party/freetype/src/include/freetype/config/ftconfig.h &&
-rm ${TMPFILE} &&
-git add third_party/freetype/include/freetype-custom-config/ftconfig.h &&
+mergeinclude() {
+  INCLUDE=$1 &&
+  STEP="merge ${INCLUDE}: check for merge conflicts" &&
+  TMPFILE=`mktemp` &&
+  git -C third_party/freetype/src/ cat-file blob HEAD@{1}:include/freetype/config/${INCLUDE} >> ${TMPFILE} &&
+  git merge-file third_party/freetype/include/freetype-custom-config/${INCLUDE} ${TMPFILE} third_party/freetype/src/include/freetype/config/${INCLUDE} &&
+  rm ${TMPFILE} &&
+  git add third_party/freetype/include/freetype-custom-config/${INCLUDE}
+}
 
-FTVERSION=`git -C third_party/freetype/src/ describe --long` &&
-FTCOMMIT=`git -C third_party/freetype/src/ rev-parse HEAD` &&
-sed -i "s/^Version: .*\$/Version: ${FTVERSION%-*}/" third_party/freetype/README.chromium &&
-sed -i "s/^Revision: .*\$/Revision: ${FTCOMMIT}/" third_party/freetype/README.chromium &&
-git add third_party/freetype/README.chromium &&
+updatereadme() {
+  STEP="update README.chromium" &&
+  FTVERSION=`git -C third_party/freetype/src/ describe --long` &&
+  FTCOMMIT=`git -C third_party/freetype/src/ rev-parse HEAD` &&
+  sed -i "s/^Version: .*\$/Version: ${FTVERSION%-*}/" third_party/freetype/README.chromium &&
+  sed -i "s/^Revision: .*\$/Revision: ${FTCOMMIT}/" third_party/freetype/README.chromium &&
+  git add third_party/freetype/README.chromium
+}
 
-git commit --quiet --amend --no-edit &&
+commit() {
+  STEP="commit" &&
+  git commit --quiet --amend --no-edit
+}
 
-true
+rolldeps "$@" &&
+checkmodules &&
+mergeinclude ftoption.h &&
+mergeinclude ftconfig.h &&
+updatereadme &&
+commit &&
+true || echo "Failed step ${STEP}" && false
