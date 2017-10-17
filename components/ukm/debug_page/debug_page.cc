5832b7e17e244b4f188fe19939a0b035a6efd9e9
Steven Holte <holte@google.com>
2017-10-17 23:54:52 +0000

    Refactor ukm codegen templates.
    Splits decoder logic into a seperate generated file, and breaks
    logic into several templates with the same parameters.
    
    Bug: 
    Change-Id: I8d6bf188ec73a475428cc49991bdace36458a468
    Reviewed-on: https://chromium-review.googlesource.com/721849
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509608}

--- components/ukm/debug_page/debug_page.cc
+++ components/ukm/debug_page/debug_page.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "components/ukm/ukm_service.h"
 #include "components/ukm/ukm_source.h"
-#include "services/metrics/public/cpp/ukm_builders.h"
+#include "services/metrics/public/cpp/ukm_decode.h"
 #include "url/gurl.h"
 
 namespace ukm {
