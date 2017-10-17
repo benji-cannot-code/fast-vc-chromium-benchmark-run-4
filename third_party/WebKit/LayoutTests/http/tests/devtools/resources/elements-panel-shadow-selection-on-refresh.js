547840cfeca7171b653a59d0bb85d2c89554372f
Will Chen <chenwilliam@chromium.org>
2017-10-17 08:03:38 +0000

    DevTools: re-use new test runner modules in legacy test helpers (part 2)
    This eliminates a lot of the code duplication between the new and legacy
    integration test frameworks by making the legacy test helper files small
    wrappers to load the new *test_runner modules.
    
    * Fixes data_grid_test_runner because the legacy test helpers had two
    data grid implementations, one of which was not actually used anymore.
    
    Bug: 667560
    Change-Id: I2ad21ec7b50be89a44f643858cec3b9348d8d857
    Reviewed-on: https://chromium-review.googlesource.com/722112
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509319}

--- third_party/WebKit/LayoutTests/http/tests/devtools/resources/elements-panel-shadow-selection-on-refresh.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/resources/elements-panel-shadow-selection-on-refresh.js
@@ -1,40 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var initialize_ElementPanelShadowSelection = function() {
-
-InspectorTest.selectReloadAndDump = function(next, node)
-{
-    InspectorTest.selectNode(node).then(onSelected);
-    var reloaded = false;
-    var selected = false;
-
-    function onSelected()
-    {
-        InspectorTest.reloadPage(onReloaded);
-        InspectorTest.addSniffer(Elements.ElementsPanel.prototype, "_lastSelectedNodeSelectedForTest", onReSelected);
-    }
-
-    function onReloaded()
-    {
-        reloaded = true;
-        maybeDumpSelectedNode();
-    }
-
-    function onReSelected()
-    {
-        selected = true;
-        maybeDumpSelectedNode();
-    }
-
-    function maybeDumpSelectedNode()
-    {
-        if (!reloaded || !selected)
-            return;
-        var selectedElement = InspectorTest.firstElementsTreeOutline().selectedTreeElement;
-        var nodeName = selectedElement ? selectedElement.node().nodeNameInCorrectCase() : "null";
-        InspectorTest.addResult("Selected node: '" + nodeName + "'");
-        next();
-    }
-}
-
-}
+    InspectorTest.preloadModule("elements_test_runner");
+};
 
