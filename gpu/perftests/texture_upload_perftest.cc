efca20d2b029e1ff26347b29ebbd07dba8b49d2a
Antoine Labour <piman@chromium.org>
2017-10-17 03:49:00 +0000

    Introduce DecoderPerfTest
    This is a microbenchmark that measures the raw throughput of the GLES2 decoders.
    
    Bug: 775212
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ic701af8c5e83a5eee347f481ee5319dfa6fc0ffc
    Reviewed-on: https://chromium-review.googlesource.com/719462
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509270}

--- gpu/perftests/texture_upload_perftest.cc
+++ gpu/perftests/texture_upload_perftest.cc
@@ -29,10 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/init/gl_factory.h"
 #include "ui/gl/scoped_make_current.h"
 
-#if defined(USE_OZONE)
-#include "base/message_loop/message_loop.h"
-#endif
-
 namespace gpu {
 namespace {
 
@@ -180,13 +176,6 @@ class TextureUploadPerfTest : public testing::Test {
 
   // Overridden from testing::Test
   void SetUp() override {
-#if defined(USE_OZONE)
-    // On Ozone, the backend initializes the event system using a UI
-    // thread.
-    base::MessageLoopForUI main_loop;
-#endif
-    static bool gl_initialized = gl::init::InitializeGLOneOff();
-    DCHECK(gl_initialized);
     // Initialize an offscreen surface and a gl context.
     surface_ = gl::init::CreateOffscreenGLSurface(gfx::Size());
     gl_context_ =
