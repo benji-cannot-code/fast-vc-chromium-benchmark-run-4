10a153b6ede955ef0d57750d89b07f112f9f1d57
Mike Wasserman <msw@chromium.org>
2017-10-17 21:32:25 +0000

    Remove deprecated OS_WIN checks in src/ash
    Ash is no longer built on Windows.
    Also cleanup DragDropControllerTest.
    
    Bug: NONE
    Change-Id: I73b3ce606aca98abc6248f7cf8356c6307a785b6
    Reviewed-on: https://chromium-review.googlesource.com/723866
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509543}

--- ash/display/cursor_window_controller_unittest.cc
+++ ash/display/cursor_window_controller_unittest.cc
@@ -115,8 +115,6 @@ TEST_F(CursorWindowControllerTest, MoveToDifferentDisplay) {
   EXPECT_EQ(50, cursor_bounds.y() + hot_point.y());
 }
 
-// Windows doesn't support compositor based cursor.
-#if !defined(OS_WIN)
 // Make sure that composition cursor inherits the visibility state.
 TEST_F(CursorWindowControllerTest, VisibilityTest) {
   ASSERT_TRUE(GetCursorWindow());
@@ -170,6 +168,5 @@ TEST_F(CursorWindowControllerTest, DSF) {
       display::Screen::GetScreen()->GetPrimaryDisplay().device_scale_factor());
   EXPECT_TRUE(GetCursorImage().HasRepresentation(2.0f));
 }
-#endif
 
 }  // namespace ash
