b4761de5423a35594956ba5341d352d2e587d968
Erik Luo <luoe@chromium.org>
2017-10-17 21:51:59 +0000

    DevTools: switch list to treeoutline for log management UI, part 1
    This CL pivots the log management UI to use a tree instead of a list.
    - Introduces SidebarLevelByUrlGroups, which contain SidebarItems
    - Adding a single message will let groups create SidebarItem children
    - Sidebar's applyFilter lets Groups choose which children to apply to
    
    Some messages have no 'url' such as evaluations, and will be addressed
    in a future CL.
    
    Counters and styles will be introduced in a followup.
    
    Bug: 685283
    Change-Id: I0335138c89ba3d4ff81dcbb3a88787e8ac0fa395
    Reviewed-on: https://chromium-review.googlesource.com/719389
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509554}

---
+++ third_party/WebKit/LayoutTests/http/tests/devtools/console-sidebar/resources/log-source.js
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function log2()
+{
+    console.log.apply(console, arguments);
+}
+
+function warn2()
+{
+    console.warn.apply(console, arguments);
+}
