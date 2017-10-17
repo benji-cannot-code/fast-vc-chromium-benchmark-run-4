acc26179d323b3f005a6066b869c5ec754a0b52f
Leo Zhang <googleo@chromium.org>
2017-10-17 06:23:05 +0000

    Add metrics and an experiment when checking if a language is accepted.
    Original check will load the whole accepted language code list, then
    check whether the given code is in the list or not. But in Android
    client, loading a list of codes will trigger 100+ JNI calls which
    costs 30~60 ms (based on the local testings on my Nexus 6).
    
    This change will call only one JNI to check whether a given language
    is accepted in Chrome. It matters in Clank, because the JNI is very
    expensive. It will save 30~60 ms for each check in my local testing.
    
    Bug:771835
    
    Change-Id: If2f3838492521373a139a4220c843fc7bc3b1f8b
    Reviewed-on: https://chromium-review.googlesource.com/708097
    Commit-Queue: Leo Zhang <googleo@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Jungshik Shin <jshin@chromium.org>
    Reviewed-by: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509303}

--- components/translate/core/browser/translate_accept_languages.cc
+++ components/translate/core/browser/translate_accept_languages.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include "base/bind.h"
+#include "base/feature_list.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "components/prefs/pref_service.h"
@@ -16,6 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 
 namespace translate {
+
+const base::Feature kTranslateAcceptLanguagesOptimization{
+    "TranslateAcceptLanguagesOptimization", base::FEATURE_DISABLED_BY_DEFAULT};
 
 TranslateAcceptLanguages::TranslateAcceptLanguages(
     PrefService* prefs,
@@ -38,11 +43,17 @@ TranslateAcceptLanguages::~TranslateAcceptLanguages() {
 // static
 bool TranslateAcceptLanguages::CanBeAcceptLanguage(
     const std::string& language) {
+  SCOPED_UMA_HISTOGRAM_TIMER("Translate.AcceptLanguages.CanBeAcceptDuration");
+
   std::string accept_language = language;
   translate::ToChromeLanguageSynonym(&accept_language);
 
   const std::string locale =
       TranslateDownloadManager::GetInstance()->application_locale();
+
+  if (base::FeatureList::IsEnabled(kTranslateAcceptLanguagesOptimization))
+    return l10n_util::IsLanguageAccepted(locale, accept_language);
+
   std::vector<std::string> accept_language_codes;
   l10n_util::GetAcceptLanguagesForLocale(locale, &accept_language_codes);
 
