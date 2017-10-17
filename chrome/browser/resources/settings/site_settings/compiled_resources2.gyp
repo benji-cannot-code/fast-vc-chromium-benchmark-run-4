17be502dc0c9f23cd8efd07c983c53fe430c3979
Dave Schuyler <dschuyler@chromium.org>
2017-10-17 19:19:58 +0000

    [MD settings] fix scrolling in siteData page.
    This CL applies GlobalScrollTargetBehavior to the siteData iron-list so
    that the list only shows one scroll bar (rather than two) and the list
    scrolls properly.
    
    Bug: 775215
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I7b21f099446dabdf3e356dd66898352d8bc73610
    Reviewed-on: https://chromium-review.googlesource.com/722340
    Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Commit-Queue: Dave Schuyler <dschuyler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509480}

--- chrome/browser/resources/settings/site_settings/compiled_resources2.gyp
+++ chrome/browser/resources/settings/site_settings/compiled_resources2.gyp
@@ -109,6 +109,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         '<(DEPTH)/ui/webui/resources/js/compiled_resources2.gyp:i18n_behavior',
         '<(DEPTH)/ui/webui/resources/js/compiled_resources2.gyp:web_ui_listener_behavior',
         '<(DEPTH)/ui/webui/resources/js/cr/ui/compiled_resources2.gyp:focus_without_ink',
+        '../compiled_resources2.gyp:global_scroll_target_behavior',
         '../settings_page/compiled_resources2.gyp:settings_subpage_search',
         'cookie_info',
         'local_data_browser_proxy',
--- chrome/browser/resources/settings/site_settings/site_data.js
+++ chrome/browser/resources/settings/site_settings/site_data.js
@@ -41,7 +41,7 @@ Polymer({
 
   behaviors: [
     I18nBehavior,
-    settings.RouteObserverBehavior,
+    settings.GlobalScrollTargetBehavior,
     WebUIListenerBehavior,
   ],
 
@@ -68,6 +68,15 @@ Polymer({
         return [];
       },
     },
+
+    /**
+     * settings.GlobalScrollTargetBehavior
+     * @override
+     */
+    subpageRoute: {
+      type: Object,
+      value: settings.routes.SITE_SETTINGS_SITE_DATA,
+    },
   },
 
   /** @private {settings.LocalDataBrowserProxy} */
@@ -88,6 +97,8 @@ Polymer({
    * @protected
    */
   currentRouteChanged: function(currentRoute) {
+    settings.GlobalScrollTargetBehaviorImpl.currentRouteChanged.call(
+        this, currentRoute);
     if (currentRoute == settings.routes.SITE_SETTINGS_SITE_DATA) {
       this.browserProxy_.reloadCookies().then(this.updateSiteList_.bind(this));
     }
