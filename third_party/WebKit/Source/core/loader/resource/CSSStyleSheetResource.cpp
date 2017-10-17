9ebac647bba048bd17ab819c9c46de4acdf34941
Darren Shen <shend@chromium.org>
2017-10-17 06:15:27 +0000

    Avoid reparsing stylesheets that contain media queries.
    When we fill in the RuleSet for a StyleSheetContents object, a rule
    in a media query would only be added if the media query evaluate to true.
    Hence, a StyleSheetContents may have different RuleSets depending on
    the current media. This is why we do not cache StyleSheetContents that
    contain media queries, since the RuleSet needs to be different.
    
    This patch enables (hidden behind a flag) caching of StyleSheetContents
    with media queries, but when they are retrieved from that cache, they are cloned.
    
    Design doc:
    https://docs.google.com/document/d/1IpyqChzXDJczfsxnbv0kh-gAx6HBGGfIpSu4P6nKI1s/edit?usp=sharing
    
    Bug: 472923
    Change-Id: I61778a9e50fc11881a3cb5f5ddfe358876c43fc6
    Reviewed-on: https://chromium-review.googlesource.com/704474
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Rune Lillesveen <rune@opera.com>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509291}

--- third_party/WebKit/Source/core/loader/resource/CSSStyleSheetResource.cpp
+++ third_party/WebKit/Source/core/loader/resource/CSSStyleSheetResource.cpp
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/loader/fetch/ResourceFetcher.h"
 #include "platform/loader/fetch/ResourceLoaderOptions.h"
 #include "platform/loader/fetch/TextResourceDecoderOptions.h"
+#include "platform/runtime_enabled_features.h"
 #include "platform/weborigin/SecurityPolicy.h"
 #include "platform/wtf/CurrentTime.h"
 #include "platform/wtf/text/TextEncoding.h"
@@ -214,7 +215,7 @@ bool CSSStyleSheetResource::CanUseSheet(MIMETypeCheck mime_type_check) const {
                                      "application/x-unknown-content-type");
 }
 
-StyleSheetContents* CSSStyleSheetResource::RestoreParsedStyleSheet(
+StyleSheetContents* CSSStyleSheetResource::CreateParsedStyleSheetFromCache(
     const CSSParserContext* context) {
   if (!parsed_style_sheet_cache_)
     return nullptr;
@@ -230,6 +231,15 @@ StyleSheetContents* CSSStyleSheetResource::RestoreParsedStyleSheet(
   // we parsed again.
   if (*parsed_style_sheet_cache_->ParserContext() != *context)
     return nullptr;
+
+  DCHECK(!parsed_style_sheet_cache_->IsLoading());
+
+  // If the stylesheet has a media query, we need to clone the cached sheet
+  // due to potential differences in the rule set.
+  if (RuntimeEnabledFeatures::CacheStyleSheetWithMediaQueriesEnabled() &&
+      parsed_style_sheet_cache_->HasMediaQueries()) {
+    return parsed_style_sheet_cache_->Copy();
+  }
 
   return parsed_style_sheet_cache_;
 }
