92930c008202b571c4c8374ca20ef80ee7e6af59
Lei Zhang <thestig@chromium.org>
2017-10-17 20:42:50 +0000

    Remove incorrect uses of extern in chrome/
    Change-Id: I764240469ce0bcb954009de601d9fdcb5a1766b8
    Reviewed-on: https://chromium-review.googlesource.com/703898
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509514}

--- chrome/browser/safe_browsing/incident_reporting/incident_reporting_service.cc
+++ chrome/browser/safe_browsing/incident_reporting/incident_reporting_service.cc
@@ -47,7 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace safe_browsing {
 
-extern const base::Feature kIncidentReportingEnableUpload {
+const base::Feature kIncidentReportingEnableUpload {
   "IncidentReportingEnableUpload",
 #if defined(GOOGLE_CHROME_BUILD)
       base::FEATURE_ENABLED_BY_DEFAULT
