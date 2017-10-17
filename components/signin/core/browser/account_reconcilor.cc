85c91f110437e91bec0182bcc771bcd13b4740be
Mihai Sardarescu <msarda@chromium.org>
2017-10-17 09:52:15 +0000

    [DICE] Fix reconcilor when turning off sync.
    When DICE is enabled, the user should continue to be signed in to Google
    on the web when sync is turned off. This CL ensures that the account
    reconcilor does not force sign the user out when they turn off sync.
    
    
    Bug: 774990
    Change-Id: Iafdaa6a31e4f5bad7e6cfda5ac56529e2b30b76c
    Reviewed-on: https://chromium-review.googlesource.com/719137
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509338}

--- components/signin/core/browser/account_reconcilor.cc
+++ components/signin/core/browser/account_reconcilor.cc
@@ -114,16 +114,27 @@ void AccountReconcilor::Initialize(bool start_reconcile_if_tokens_available) {
 void AccountReconcilor::Shutdown() {
   VLOG(1) << "AccountReconcilor::Shutdown";
   UnregisterWithCookieManagerService();
-  UnregisterWithSigninManager();
   UnregisterWithTokenService();
   UnregisterWithContentSettings();
+  UnregisterWithSigninManager();
 }
 
 void AccountReconcilor::RegisterWithSigninManager() {
+  if (signin::IsAccountConsistencyDiceEnabled()) {
+    // Reconcilor is always turned on when DICE is enabled. It does not need to
+    // observe the SigninManager events.
+    return;
+  }
+
+  VLOG(1) << "AccountReconcilor::RegisterWithSigninManager";
   signin_manager_->AddObserver(this);
 }
 
 void AccountReconcilor::UnregisterWithSigninManager() {
+  if (signin::IsAccountConsistencyDiceEnabled())
+    return;
+
+  VLOG(1) << "AccountReconcilor::UnregisterWithSigninManager";
   signin_manager_->RemoveObserver(this);
 }
 
@@ -180,6 +191,7 @@ void AccountReconcilor::RegisterWithCookieManagerService() {
   cookie_manager_service_->AddObserver(this);
   registered_with_cookie_manager_service_ = true;
 }
+
 void AccountReconcilor::UnregisterWithCookieManagerService() {
   VLOG(1) << "AccountReconcilor::UnregisterWithCookieManagerService";
   if (!registered_with_cookie_manager_service_)
@@ -247,6 +259,7 @@ void AccountReconcilor::OnRefreshTokensLoaded() {
 
 void AccountReconcilor::GoogleSigninSucceeded(const std::string& account_id,
                                               const std::string& username) {
+  DCHECK(!signin::IsAccountConsistencyDiceEnabled());
   VLOG(1) << "AccountReconcilor::GoogleSigninSucceeded: signed in";
   RegisterWithCookieManagerService();
   RegisterWithContentSettings();
@@ -255,6 +268,7 @@ void AccountReconcilor::GoogleSigninSucceeded(const std::string& account_id,
 
 void AccountReconcilor::GoogleSignedOut(const std::string& account_id,
                                         const std::string& username) {
+  DCHECK(!signin::IsAccountConsistencyDiceEnabled());
   VLOG(1) << "AccountReconcilor::GoogleSignedOut: signed out";
   AbortReconcile();
   UnregisterWithCookieManagerService();
