9a11b52aab4e541d5992e3fd667d4b2de66056df
Bill Budge <bbudge@chromium.org>
2017-10-17 22:46:35 +0000

    [page_allocator] Support predictable ASLR calculation for fuzzers.
    - Adds a 'initial_seed' parameter to GetRandomPageBase, which defaults
      to 0. If non-zero, generates a predictable sequence, using the value
      as the initial seed.
    - Adds a unit test for the predictable sequence.
    
    Bug: chromium:756050
    Change-Id: Iaf718baa2b7d49dc8eda7b0f97aa12b576ddca28
    Reviewed-on: https://chromium-review.googlesource.com/703509
    Commit-Queue: Bill Budge <bbudge@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509575}

--- base/allocator/partition_allocator/address_space_randomization.h
+++ base/allocator/partition_allocator/address_space_randomization.h
@@ -12,6 +12,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
+// Sets the seed for the random number generator used by GetRandomPageBase in
+// order to generate a predictable sequence of addresses. May be called multiple
+// times. On official Chrome builds this function is disabled and has no effect.
+BASE_EXPORT void SetRandomPageBaseSeed(int64_t seed);
+
+// Calculates a random preferred mapping address. In calculating an address, we
+// balance good ASLR against not fragmenting the address space too badly.
+BASE_EXPORT void* GetRandomPageBase();
+
 namespace internal {
 
 constexpr uintptr_t AslrAddress(uintptr_t mask) {
@@ -129,10 +138,6 @@ constexpr uintptr_t kASLROffset = AslrAddress(0x20000000ULL);
 #endif  // defined(ARCH_CPU_32_BITS)
 
 }  // namespace internal
-
-// Calculates a random preferred mapping address. In calculating an address, we
-// balance good ASLR against not fragmenting the address space too badly.
-BASE_EXPORT void* GetRandomPageBase();
 
 }  // namespace base
 
