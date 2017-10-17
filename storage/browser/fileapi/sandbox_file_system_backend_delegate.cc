377640dd4face0c7318ba4ddf26e1a8e5ebaf36b
Joshua Bell <jsbell@chromium.org>
2017-10-17 22:28:24 +0000

    Don't create quota-last-modified entries for nonexistent filesystems
    When asked by chrome://settings/siteData to delete an origin, the
    sandboxed file system code enumerates the types (temporary,
    persistent, syncable) and tries to delete each (origin, type) pair.
    This would create "last modified" entries for pair even if there was
    no such file system in use, and those entries would confuse later
    cleanup attempts by chrome://settings/clearBrowserData
    
    The fix is simple: don't notify the quota system if deleting the
    directory resulted in a zero-byte change. This prevents the creation
    of bogus entries.
    
    Cleanup of bogus entries was hindered by two additional subtle bugs:
    
    * When asked to delete an origin which didn't exist, the File System
       quota client would try to 'rm -rf' an empty path, which
       (fortunately) fails. Detect that case and early-exit with success
       (since there's nothing to delete).
    
    * When asked to delete an unsupported type the IndexedDB quota client
       would report failure; it should report success instead like the
       other quota clients.
    
    With those changes in place, chrome://settings/clearBrowserData cleans
    up any bogus entries left behind by the first part of the bug.
    
    Bug: 774666
    Change-Id: Ie49810ef0408b1ae3ad3e2e7546929f1177c5963
    Reviewed-on: https://chromium-review.googlesource.com/719390
    Commit-Queue: Joshua Bell <jsbell@chromium.org>
    Reviewed-by: Michael Nordman <michaeln@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509572}

--- storage/browser/fileapi/sandbox_file_system_backend_delegate.cc
+++ storage/browser/fileapi/sandbox_file_system_backend_delegate.cc
@@ -335,7 +335,7 @@ SandboxFileSystemBackendDelegate::DeleteOriginDataOnFileTaskRunner(
   usage_cache()->CloseCacheFiles();
   bool result = obfuscated_file_util()->DeleteDirectoryForOriginAndType(
       origin_url, GetTypeString(type));
-  if (result && proxy) {
+  if (result && proxy && usage) {
     proxy->NotifyStorageModified(storage::QuotaClient::kFileSystem,
                                  origin_url,
                                  FileSystemTypeToQuotaStorageType(type),
