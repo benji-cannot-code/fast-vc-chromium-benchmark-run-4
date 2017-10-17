f9a8967c1e5dcbf11611e296939eeb99eac15283
Nathan Bruer <allada@chromium.org>
2017-10-17 21:24:28 +0000

    [Devtools] createFile in projects promisified
    This patch is for code health and to make it eassier for followup
    patches. Simply makes createFile() in Workspace.Project return a promise
    instead of a callback.
    
    R=lushnikov
    BUG=None
    
    Change-Id: If9e0a8d1e24cb01e3a1cba8188d31d4e2b9f61c5
    Reviewed-on: https://chromium-review.googlesource.com/720078
    Commit-Queue: Blaise Bruer <allada@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509539}

--- third_party/WebKit/LayoutTests/http/tests/devtools/console/console-link-to-snippet.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/console/console-link-to-snippet.js
@@ -51,10 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   ]);
 
   function createSnippetPromise(content) {
-    var callback;
-    var promise = new Promise(fullfill => (callback = fullfill));
-    Snippets.scriptSnippetModel._project.createFile('', null, content, callback);
-    return promise;
+    return Snippets.scriptSnippetModel._project.createFile('', null, content);
   }
 
   function renameSourceCodePromise(newName, uiSourceCode) {
--- third_party/WebKit/Source/devtools/front_end/workspace/Workspace.js
+++ third_party/WebKit/Source/devtools/front_end/workspace/Workspace.js
@@ -149,9 +149,9 @@ Workspace.Project.prototype = {
    * @param {string} path
    * @param {?string} name
    * @param {string} content
-   * @param {function(?Workspace.UISourceCode)} callback
+   * @return {!Promise<?Workspace.UISourceCode>}
    */
-  createFile(path, name, content, callback) {},
+  createFile(path, name, content) {},
 
   /**
    * @param {!Workspace.UISourceCode} uiSourceCode
