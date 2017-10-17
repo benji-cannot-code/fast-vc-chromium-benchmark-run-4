19b06fa32d4e6ef21196812543a76f95f3b4e003
Klaus Weidner <klausw@chromium.org>
2017-10-17 02:43:00 +0000

    egl: update Khronos EGL headers
    This picks up the EGL_ANDROID_get_native_client_buffer extension for
    eglGetNativeClientBufferANDROID, and declares struct AHardwareBuffer.
    
    Preserve Chromium-specific support for EGL_EXT_image_flush_external
    extension, and remove "Chromium-specific" comment for Ozone types such
    as EGLNativeDisplayType since that section is now upstreamed.
    
    Also ran "git cl format ." but reverted the changes to the
    autogenerated date header.
    
    BUG=761432
    
    Change-Id: If911142151ecfc14b1eba6cf9bc8fc72e8ccf672
    Reviewed-on: https://chromium-review.googlesource.com/721600
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Klaus Weidner <klausw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509239}

--- third_party/khronos/EGL/eglplatform.h
+++ third_party/khronos/EGL/eglplatform.h
@@ -96,7 +96,6 @@ typedef void*                           EGLNativeDisplayType;
 
 #elif defined(USE_OZONE)
 
-/* Chromium-specific */
 typedef intptr_t EGLNativeDisplayType;
 typedef intptr_t EGLNativeWindowType;
 typedef intptr_t EGLNativePixmapType;
