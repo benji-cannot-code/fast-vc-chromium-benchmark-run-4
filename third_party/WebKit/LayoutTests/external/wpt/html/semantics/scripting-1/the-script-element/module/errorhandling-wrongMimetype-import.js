7eb0e43f30e04b927d84c423698b0ef3070df4fc
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2017-10-18 02:36:28 +0000

    Import wpt@75c6dd3dfd25a6993aa417eb2f2fc890be9a2a9f
    Using wpt-import in Chromium 4479b97cb558f478b3250fe6c7e1a5eb0b111858.
    
    Build: https://build.chromium.org/p/chromium.infra.cron/builders/wpt-importer/builds/3386
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    TBR=qyearsley@chromium.org
    
    No-Export: true
    Change-Id: I3bbe2f334d976f483d006f3d980b7ce4c9cd936e
    Reviewed-on: https://chromium-review.googlesource.com/724459
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509663}

--- third_party/WebKit/LayoutTests/external/wpt/html/semantics/scripting-1/the-script-element/module/errorhandling-wrongMimetype-import.js
+++ third_party/WebKit/LayoutTests/external/wpt/html/semantics/scripting-1/the-script-element/module/errorhandling-wrongMimetype-import.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-import foo from "./errorhandling-wrongMimetype.js?pipe=header(X-Content-Type-Options,nosniff),header(Content-Type,text/plain)";
+import foo from "./errorhandling-wrongMimetype.js?pipe=header(Content-Type,text/plain)";
 
 // We don't expect this code to run, the import above should fail!
 // If we do run though, don't trigger an error that the testharness
---
+++ third_party/WebKit/LayoutTests/external/wpt/html/semantics/scripting-1/the-script-element/module/resources/imports-b-cross-origin.sub.js
@@ -0,0 +1,2 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import "http://{{domains[www2]}}:{{ports[http][0]}}/html/semantics/scripting-1/the-script-element/module/imports-b.js";
