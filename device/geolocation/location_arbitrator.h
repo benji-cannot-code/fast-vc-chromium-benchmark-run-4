f8efc698827308d9f67a7664d209e38fed05fe0e
Andrew Moylan <amoylan@chromium.org>
2017-10-18 00:11:28 +0000

    Skip network geolocation if request context = null
    This CL changes the way embedders enable/disable network geolocation:
    
    Before: Disabled if GeolocationDelegate::CreateAccessTokenStore()
            returns null (the default).
    Now:    Disabled if
            ContentBrowserClient::GetGeolocationRequestContext
    	produces a nullptr URLRequestContextGetter (also the default).
    
    This change doesn't alter which embedders enable/disable network
    geolocation, because the only two embedders that currently return a
    non-null result from CreateAccessTokenStore() are also the only two
    embedders that produce a non-null result from
    GetGeolocationRequestContextGetter: Chrome and Chromecast.
    
    Embedders external to the Chromium repo will need to implement
    GetGeolocationRequestContext:
    https://groups.google.com/a/chromium.org/forum/#!topic/embedder-dev/0ln9lxrsBw8
    
    With this change, LocationArbitrator no longer knows about
    AccessTokenStore at all, and AccessTokenStore becomes fully unused. It
    can be removed in a subsequent CL.
    
    This is a step in the removal of AccessTokenStore as detailed here:
    https://docs.google.com/document/d/1A-ZYMadJkwdQad_pC3MbpuCVNOOnRP_UZjGnXl-FOqE
    
    Bug: 748921
    Change-Id: Id413a0cdb207fcec8fd25f381068c6c13ad4068c
    Reviewed-on: https://chromium-review.googlesource.com/676826
    Commit-Queue: Andrew Moylan <amoylan@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509616}

--- device/geolocation/location_arbitrator.h
+++ device/geolocation/location_arbitrator.h
@@ -15,19 +15,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/strings/string16.h"
 #include "base/time/time.h"
-#include "device/geolocation/access_token_store.h"
 #include "device/geolocation/geolocation_export.h"
 #include "device/geolocation/geolocation_provider.h"
 #include "device/geolocation/geoposition.h"
 #include "device/geolocation/location_provider.h"
 #include "net/url_request/url_request_context_getter.h"
+#include "url/gurl.h"
 
 namespace net {
 class URLRequestContextGetter;
 }
 
 namespace device {
-class AccessTokenStore;
 class GeolocationDelegate;
 class LocationProvider;
 
@@ -61,7 +60,6 @@ class DEVICE_GEOLOCATION_EXPORT LocationArbitrator : public LocationProvider {
  protected:
   // These functions are useful for injection of dependencies in derived
   // testing classes.
-  virtual scoped_refptr<AccessTokenStore> NewAccessTokenStore();
   virtual std::unique_ptr<LocationProvider> NewNetworkLocationProvider(
       scoped_refptr<net::URLRequestContextGetter> context,
       const std::string& api_key);
@@ -70,8 +68,6 @@ class DEVICE_GEOLOCATION_EXPORT LocationArbitrator : public LocationProvider {
 
  private:
   friend class TestingLocationArbitrator;
-
-  scoped_refptr<AccessTokenStore> GetAccessTokenStore();
 
   // Provider will either be added to |providers_| or
   // deleted on error (e.g. it fails to start).
@@ -103,7 +99,6 @@ class DEVICE_GEOLOCATION_EXPORT LocationArbitrator : public LocationProvider {
   const GeolocationProvider::RequestContextProducer request_context_producer_;
   const std::string api_key_;
 
-  scoped_refptr<AccessTokenStore> access_token_store_;
   LocationProvider::LocationProviderUpdateCallback arbitrator_update_callback_;
 
   // CancelableCallback to prevent OnRequestContextReponse from being called
