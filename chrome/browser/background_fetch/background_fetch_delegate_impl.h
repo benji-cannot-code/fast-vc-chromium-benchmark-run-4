e1f16d4625c6458b7a33f1a15c94c07c2bec6295
Dan Elphick <delphick@chromium.org>
2017-10-18 14:43:53 +0000

    Improve look up time of download guids in BGFetch
    Add a vector of download guids to the JobDetails structure to make it
    easier to find all the download guids associated with a download job and
    avoid iterating over all of the download/job id pairs to find them.
    
    Since there are generally two types of used in
    BackgroundFetchDelegateImpl, download guids and unique job ids, make the
    download guids more explicit by renaming guid->download_guid.
    
    Change-Id: Ia2434118f53165e56f75206fa6dcc8ce8d5adc69
    Reviewed-on: https://chromium-review.googlesource.com/725420
    Commit-Queue: Dan Elphick <delphick@chromium.org>
    Reviewed-by: John Mellor <johnme@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509766}

--- chrome/browser/background_fetch/background_fetch_delegate_impl.h
+++ chrome/browser/background_fetch/background_fetch_delegate_impl.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/containers/flat_set.h"
 #include "base/memory/weak_ptr.h"
 #include "components/download/public/download_params.h"
 #include "components/keyed_service/core/keyed_service.h"
@@ -108,6 +109,11 @@ class BackgroundFetchDelegateImpl
     const url::Origin origin;
     int completed_parts;
     const int total_parts;
+
+    // Set of DownloadService GUIDs that are currently downloading. They are
+    // added by DownloadUrl and are removed when the download completes, fails
+    // or is cancelled.
+    base::flat_set<std::string> current_download_guids;
 
     offline_items_collection::OfflineItem offline_item;
 
