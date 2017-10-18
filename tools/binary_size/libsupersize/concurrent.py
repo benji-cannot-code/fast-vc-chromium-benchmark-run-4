c34de1354bcfec80ba6e7d9b8a7e3472cd1fb99f
Andrew Grieve <agrieve@chromium.org>
2017-10-18 20:12:30 +0000

    Supersize: Break ** merge strings symbols into individual literals
    * nm.py now uses multiprocessing.Pipe() and os.fork() rather than
      subprocess.Popen().
    * nm.py now directly executable (for testing its logic on single .o/.a
      files).
    * Adds "ReadStringLiterals()" console function for dumping values of
      literals.
    * "supersize archive" now runs in 40s on my machine, up from 25s with
      --no-string-literals
    * libmonochrome.apk.size file now 9.67mb, up from 9.30mb
    
    Stats for libmonochrome.so:
    Section .rodata: has 100.0% of 6184870 bytes accounted for from 172885 symbols. 0 bytes are unaccounted for.
    * Padding accounts for 19684 bytes (0.3%)
    * Contains 156446 string literals. Total size=2888602, padding=13275
    * Contains 73269 aliases, mapped to 17774 unique addresses (308060 bytes)
    * 550 symbols have shared ownership (285821 bytes)
    
    Comparing .rodata symbols with & without string literals, these two
    paths now show in the top 20:
    15)   1879143 (30.4%) r@Group      45520 ui/gl/gl_bindings_autogen_gl.cc (count=1)
    16)   1923623 (31.1%) r@Group      44480 components/autofill/core/browser/address_rewriter_rules.cc (count=2)
    
    It works!
    
    Bug: 746761
    Change-Id: I8aee0cafffc8f2941ea0ff7dd4e5f825b8165bc0
    Reviewed-on: https://chromium-review.googlesource.com/721465
    Commit-Queue: agrieve <agrieve@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509849}

--- tools/binary_size/libsupersize/concurrent.py
+++ tools/binary_size/libsupersize/concurrent.py
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import __builtin__  # __builtins__ does not have exception types.
 import atexit
+import itertools
 import logging
 import multiprocessing
 import multiprocessing.dummy
@@ -216,7 +217,12 @@ def CallOnThread(func, *args, **kwargs):
 
 
 def EncodeDictOfLists(d, key_transform=None):
-  """Serializes a dict where values are lists of strings."""
+  """Serializes a dict where values are lists of strings.
+
+  Does not support '' as keys, nor [''] as values.
+  """
+  assert '' not in d
+  assert [''] not in d.itervalues()
   keys = iter(d)
   if key_transform:
     keys = (key_transform(k) for k in keys)
@@ -226,11 +232,13 @@ def EncodeDictOfLists(d, key_transform=None):
 
 
 def JoinEncodedDictOfLists(encoded_values):
+  assert isinstance(encoded_values, list), 'Does not work with generators'
   return ('\x01'.join(x[0] for x in encoded_values if x[0]),
           '\x01'.join(x[1] for x in encoded_values if x[1]))
 
 
-def DecodeDictOfLists(encoded_keys_and_values, key_transform=None):
+def DecodeDictOfLists(encoded_keys_and_values, key_transform=None,
+                      value_transform=None):
   """Deserializes a dict where values are lists of strings."""
   encoded_keys, encoded_values = encoded_keys_and_values
   if not encoded_keys:
@@ -238,8 +246,18 @@ def DecodeDictOfLists(encoded_keys_and_values, key_transform=None):
   keys = encoded_keys.split('\x01')
   if key_transform:
     keys = (key_transform(k) for k in keys)
-  values = encoded_values.split('\x01')
+  encoded_lists = encoded_values.split('\x01')
   ret = {}
-  for i, key in enumerate(keys):
-    ret[key] = values[i].split('\x02')
+  for key, encoded_list in itertools.izip(keys, encoded_lists):
+    if not encoded_list:
+      values = []
+    else:
+      values = encoded_list.split('\x02')
+      if value_transform:
+        for i in xrange(len(values)):
+          values[i] = value_transform(values[i])
+    ret[key] = values
   return ret
+
+
+EMPTY_ENCODED_DICT = EncodeDictOfLists({})
