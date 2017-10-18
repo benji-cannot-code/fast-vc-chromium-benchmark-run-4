703e6773b96a6cd71ee3d3e6c4ec0f60088d50c2
Manuel Rego Casasnovas <rego@igalia.com>
2017-10-18 12:45:44 +0000

    Remove unused argument outputContainer from checkLayout() method
    checkLayout() method defined in resources/check-layout-th.js
    had a outputContainer argument that was not used.
    This patch removes that argument.
    
    On top of that, some tests that were trying to use that argument
    have been modified as it was not needed for them.
    
    Last, the copies of check-layout-th.js in WPT CSS test suites
    have been updated with this change and another change introduced
    in r377261.
    
    Change-Id: Iefb649d811e0fd09106bf3d02225539e481fa713
    Reviewed-on: https://chromium-review.googlesource.com/721545
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: Manuel Rego Casasnovas <rego@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#509752}

--- third_party/WebKit/LayoutTests/external/wpt/css/css-flexbox-1/support/check-layout-th.js
+++ third_party/WebKit/LayoutTests/external/wpt/css/css-flexbox-1/support/check-layout-th.js
@@ -70,6 +70,11 @@ function checkExpectedValues(t, node, prefix)
         assert_tolerance(node.scrollHeight, expectedHeight, prefix + "scrollHeight");
     }
 
+    var expectedWidth = checkAttribute(output, node, "data-expected-bounding-client-rect-width");
+    if (expectedWidth) {
+        assert_tolerance(node.getBoundingClientRect().width, expectedWidth, prefix + "getBoundingClientRect().width");
+    }
+
     var expectedOffset = checkAttribute(output, node, "data-total-x");
     if (expectedOffset) {
         var totalLeft = node.clientLeft + node.offsetLeft;
@@ -157,7 +162,7 @@ function checkExpectedValues(t, node, prefix)
     return output.checked;
 }
 
-window.checkLayout = function(selectorList, outputContainer)
+window.checkLayout = function(selectorList)
 {
     if (!selectorList) {
         console.error("You must provide a CSS selector of nodes to check.");
--- third_party/WebKit/LayoutTests/resources/check-layout-th.js
+++ third_party/WebKit/LayoutTests/resources/check-layout-th.js
@@ -162,7 +162,7 @@ function checkExpectedValues(t, node, prefix)
     return output.checked;
 }
 
-window.checkLayout = function(selectorList, outputContainer)
+window.checkLayout = function(selectorList)
 {
     if (!selectorList) {
         console.error("You must provide a CSS selector of nodes to check.");
