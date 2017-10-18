08761061176da1dcb7a46d23a2bc7b18b1aae6ed
Will Chen <chenwilliam@chromium.org>
2017-10-18 21:40:43 +0000

    DevTools: security-test.js relies on security_test_runner
    This eliminates a lot of the code duplication between the new and legacy
    integration test frameworks by making security-test.js a small
    wrapper to load the new security_test_runner modules.
    
    I made the old test framework initialization processs sequential (i.e.
    promise chain) instead of concurrent (i.e. Promise.all(...)) because
    origin-view-then-interstitial.html test didn't work if the Security module
    was already loaded before showing the Security panel. I spent a while debugging
    but couldn't figure out the root cause.
    
    Bug: 667560
    Change-Id: I74a2a783a5a6e8d81c1e592774928c400131691a
    Reviewed-on: https://chromium-review.googlesource.com/726367
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509885}

--- third_party/WebKit/LayoutTests/http/tests/inspector/security-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/security-test.js
@@ -1,26 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var initialize_SecurityTest = function() {
-
-InspectorTest.preloadPanel("security");
-
-InspectorTest.dumpSecurityPanelSidebarOrigins = function() {
-    for (var key in Security.SecurityPanelSidebarTree.OriginGroupName) {
-        var originGroupName = Security.SecurityPanelSidebarTree.OriginGroupName[key];
-        var originGroup = Security.SecurityPanel._instance()._sidebarTree._originGroups.get(originGroupName);
-        if (originGroup.hidden)
-            continue;
-        InspectorTest.addResult("Group: " + originGroupName);
-        var originTitles = originGroup.childrenListElement.getElementsByClassName("title");
-        for (var originTitle of originTitles)
-            InspectorTest.dumpDeepInnerHTML(originTitle);
-    }
-}
-
-/**
- * @param {!SDK.NetworkRequest} request
- */
-InspectorTest.dispatchRequestFinished = function(request) {
-    InspectorTest.networkManager.dispatchEventToListeners(SDK.NetworkManager.Events.RequestFinished, request);
-}
-
+    InspectorTest.preloadModule("security_test_runner");
+    InspectorTest.preloadPanel("security");
 }
