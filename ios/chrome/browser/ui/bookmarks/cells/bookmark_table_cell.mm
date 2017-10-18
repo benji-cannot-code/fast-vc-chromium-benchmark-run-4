fd62f93324bffd96285669c0a7bf27940b879b79
Marti Wong <martiw@chromium.org>
2017-10-18 16:20:49 +0000

    Remove the unwanted stroke below promo card (new iOS bookmark ui)
    Introduce a custom separator for the new bookmark table cell, so that we
    don't show a separator under promo section.
    
    screenshot:
    https://drive.google.com/file/d/0B1dtd3IUt4I7WERlaWVlR0N4cUE
    
    Bug: 774388
    Change-Id: I5edcd199fd3daa3b055769873d8983918cb6310e
    Reviewed-on: https://chromium-review.googlesource.com/725128
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509783}

--- ios/chrome/browser/ui/bookmarks/cells/bookmark_table_cell.mm
+++ ios/chrome/browser/ui/bookmarks/cells/bookmark_table_cell.mm
@@ -31,12 +31,16 @@ const CGFloat kBookmarkTableCellImagePadding = 16.0;
 // The title text.
 @property(nonatomic, strong) UITextField* titleText;
 
+// Separator view. Displayed at 1 pixel height at the bottom.
+@property(nonatomic, strong) UIView* separatorView;
+
 @end
 
 @implementation BookmarkTableCell
 @synthesize placeholderLabel = _placeholderLabel;
 @synthesize titleText = _titleText;
 @synthesize textDelegate = _textDelegate;
+@synthesize separatorView = _separatorView;
 
 #pragma mark - Initializer
 
@@ -59,6 +63,22 @@ const CGFloat kBookmarkTableCellImagePadding = 16.0;
 
     [_placeholderLabel setHidden:YES];
     [self.contentView addSubview:_placeholderLabel];
+
+    // Add separator view.
+    _separatorView = [[UIView alloc] initWithFrame:CGRectZero];
+    [self.contentView addSubview:_separatorView];
+
+    CGFloat pixelSize = 1 / [[UIScreen mainScreen] scale];
+    [NSLayoutConstraint activateConstraints:@[
+      [_separatorView.leadingAnchor
+          constraintEqualToAnchor:_titleText.leadingAnchor],
+      [_separatorView.trailingAnchor
+          constraintEqualToAnchor:self.trailingAnchor],
+      [_separatorView.heightAnchor constraintEqualToConstant:pixelSize],
+      [_separatorView.bottomAnchor constraintEqualToAnchor:self.bottomAnchor],
+    ]];
+    _separatorView.translatesAutoresizingMaskIntoConstraints = NO;
+    _separatorView.backgroundColor = [UIColor colorWithWhite:0.0 alpha:.12];
   }
   return self;
 }
@@ -154,9 +174,6 @@ const CGFloat kBookmarkTableCellImagePadding = 16.0;
   [super layoutSubviews];
   CGFloat titleTextStart =
       kBookmarkTableCellDefaultImageSize + kBookmarkTableCellImagePadding * 2;
-  // TODO(crbug.com/695749): Investigate why this separator inset is not
-  // set correctly in landscape mode.
-  [self setSeparatorInset:UIEdgeInsetsMake(0, titleTextStart, 0, 0)];
 
   // TODO(crbug.com/695749): Investigate using constraints instead of manual
   // layout.
