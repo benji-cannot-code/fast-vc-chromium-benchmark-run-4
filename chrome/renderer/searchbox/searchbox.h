6b7b8fc26678e51edb7e42938a91893d431f3bd1
Marc Treib <treib@chromium.org>
2017-10-18 10:10:08 +0000

    SearchBox: Hook up Mojo interfaces in DidCommitProvisionalLoad
    SearchBox may execute JS in response to IPCs, but running JS in the
    given frame is illegal before the provisional navigation has committed.
    
    Bug: 765101
    Change-Id: I7fc01989177b768667b5d64c2198b2da3ec0adb9
    Reviewed-on: https://chromium-review.googlesource.com/723079
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509728}

--- chrome/renderer/searchbox/searchbox.h
+++ chrome/renderer/searchbox/searchbox.h
@@ -124,6 +124,8 @@ class SearchBox : public content::RenderFrameObserver,
 
  private:
   // Overridden from content::RenderFrameObserver:
+  void DidCommitProvisionalLoad(bool is_new_navigation,
+                                bool is_same_document_navigation) override;
   void OnDestruct() override;
 
   // Overridden from chrome::mojom::EmbeddedSearchClient:
@@ -141,8 +143,6 @@ class SearchBox : public content::RenderFrameObserver,
 
   // Returns the URL of the Most Visited item specified by the |item_id|.
   GURL GetURLForMostVisitedItem(InstantRestrictedID item_id) const;
-
-  void Bind(chrome::mojom::EmbeddedSearchClientAssociatedRequest request);
 
   int page_seq_no_;
   bool is_focused_;
