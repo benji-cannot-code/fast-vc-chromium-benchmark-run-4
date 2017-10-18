b35efca845a4e3e9688e2ad8fe5119ffb002d752
Cait Phillips <caitkp@chromium.org>
2017-10-18 15:58:49 +0000

    Test that credit cards stay disabled when PersonalData is reset.
    Bug: 700812
    Change-Id: Id1b5809768abfa25f83df36d80e498837e4a6c01
    Reviewed-on: https://chromium-review.googlesource.com/716844
    Commit-Queue: Cait Phillips <caitkp@chromium.org>
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509781}

--- components/autofill/core/browser/personal_data_manager.h
+++ components/autofill/core/browser/personal_data_manager.h
@@ -384,6 +384,8 @@ class PersonalDataManager : public KeyedService,
       DeleteDisusedCreditCards_OnlyDeleteExpiredDisusedLocalCards);
   FRIEND_TEST_ALL_PREFIXES(PersonalDataManagerTest,
                            GetCreditCardSuggestions_CreditCardAutofillDisabled);
+  FRIEND_TEST_ALL_PREFIXES(PersonalDataManagerTest,
+                           GetCreditCardSuggestions_NoCardsLoadedIfDisabled);
 
   friend class autofill::AutofillInteractiveTest;
   friend class autofill::AutofillTest;
