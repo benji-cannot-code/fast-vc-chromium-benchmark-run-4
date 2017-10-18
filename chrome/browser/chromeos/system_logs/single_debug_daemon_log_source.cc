5b7cece66163933d3ca79b7eeb82b9fcceda189d
Dariusz Marcinkiewicz <darekm@chromium.org>
2017-10-18 07:14:24 +0000

    Add uptime to SingleDebugDaemonLogSource.
    BUG=b:67364190
    
    Change-Id: I7cd3816955683342276b585132de5fc37ed936e5
    Reviewed-on: https://chromium-review.googlesource.com/718748
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Dariusz Marcinkiewicz <darekm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509706}

--- chrome/browser/chromeos/system_logs/single_debug_daemon_log_source.cc
+++ chrome/browser/chromeos/system_logs/single_debug_daemon_log_source.cc
@@ -28,6 +28,8 @@ std::string GetLogName(SupportedSource source_type) {
       return "lspci";
     case SupportedSource::kIfconfig:
       return "ifconfig";
+    case SupportedSource::kUptime:
+      return "uptime";
   }
   NOTREACHED();
   return "";
--- chrome/browser/chromeos/system_logs/single_debug_daemon_log_source.h
+++ chrome/browser/chromeos/system_logs/single_debug_daemon_log_source.h
@@ -29,6 +29,9 @@ class SingleDebugDaemonLogSource : public SystemLogsSource {
 
     // For "ifconfig" command.
     kIfconfig,
+
+    // For "/proc/uptime" entry.
+    kUptime,
   };
 
   explicit SingleDebugDaemonLogSource(SupportedSource source);
