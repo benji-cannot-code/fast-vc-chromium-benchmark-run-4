7f742a8ae23f554464810fae5c4ee0e7c3279c76
David Tseng <dtseng@chromium.org>
2017-10-18 00:43:40 +0000

    selectstart event dispatch requires a valid node
    Bug: 775320
    Test: manual.
    Change-Id: I763adcb2ff287cb4594984eec3d494ff93fb8f10
    Reviewed-on: https://chromium-review.googlesource.com/723787
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509628}

--- third_party/WebKit/Source/modules/accessibility/AXLayoutObject.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXLayoutObject.cpp
@@ -2018,9 +2018,10 @@ bool AXLayoutObject::OnNativeSetSelectionAction(const AXRange& selection) {
     return false;
   }
 
-  if (anchor_object->GetLayoutObject()->GetNode()->DispatchEvent(
+  if (anchor_object->GetLayoutObject()->GetNode() &&
+      anchor_object->GetLayoutObject()->GetNode()->DispatchEvent(
           Event::CreateCancelableBubble(EventTypeNames::selectstart)) !=
-      DispatchEventResult::kNotCanceled)
+          DispatchEventResult::kNotCanceled)
     return false;
 
   if (!IsValidSelectionBound(anchor_object) ||
