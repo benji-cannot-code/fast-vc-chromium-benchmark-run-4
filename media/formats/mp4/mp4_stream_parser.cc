9fc5f2d2496947f229ba809c12ba459a43617fd6
Dan Sanders <sandersd@chromium.org>
2017-10-18 02:37:12 +0000

    [media] Check for overflow in TrackRunIterator::cts().
    This CL moves the CTS computation to AdvanceSample(), which now returns
    a bool. Callers have been updated to fail when AdvanceSample() fails.
    
    Bug: 772874
    Change-Id: I1215f6a259eff310b964e1899be4e4138ff34087
    Reviewed-on: https://chromium-review.googlesource.com/722164
    Reviewed-by: Matthew Wolenetz <wolenetz@chromium.org>
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509664}

--- media/formats/mp4/mp4_stream_parser.cc
+++ media/formats/mp4/mp4_stream_parser.cc
@@ -619,7 +619,9 @@ bool MP4StreamParser::EnqueueSample(BufferQueueMap* buffers, bool* err) {
   }
 
   if (!runs_->IsSampleValid()) {
-    runs_->AdvanceRun();
+    *err = !runs_->AdvanceRun();
+    if (*err)
+      return false;
     return true;
   }
 
@@ -637,7 +639,9 @@ bool MP4StreamParser::EnqueueSample(BufferQueueMap* buffers, bool* err) {
 
   // Skip this entire track if it's not one we're interested in
   if (!audio && !video) {
-    runs_->AdvanceRun();
+    *err = !runs_->AdvanceRun();
+    if (*err)
+      return false;
     return true;
   }
 
@@ -675,7 +679,9 @@ bool MP4StreamParser::EnqueueSample(BufferQueueMap* buffers, bool* err) {
     LIMITED_MEDIA_LOG(DEBUG, media_log_, num_empty_samples_skipped_,
                       kMaxEmptySampleLogs)
         << "Skipping 'trun' sample with size of 0.";
-    runs_->AdvanceSample();
+    *err = !runs_->AdvanceSample();
+    if (*err)
+      return false;
     return true;
   }
 
@@ -758,7 +764,7 @@ bool MP4StreamParser::EnqueueSample(BufferQueueMap* buffers, bool* err) {
   if (runs_->cts() != kNoTimestamp) {
     stream_buf->set_timestamp(runs_->cts());
   } else {
-    MEDIA_LOG(ERROR, media_log_) << "Frame CTS exceeds representable limit";
+    MEDIA_LOG(ERROR, media_log_) << "Frame PTS exceeds representable limit";
     *err = true;
     return false;
   }
@@ -780,7 +786,9 @@ bool MP4StreamParser::EnqueueSample(BufferQueueMap* buffers, bool* err) {
            << ", size=" << sample_size;
 
   (*buffers)[runs_->track_id()].push_back(stream_buf);
-  runs_->AdvanceSample();
+  *err = !runs_->AdvanceSample();
+  if (*err)
+    return false;
   return true;
 }
 
@@ -849,10 +857,11 @@ bool MP4StreamParser::ComputeHighestEndOffset(const MovieFragment& moof) {
       int64_t sample_end_offset = runs.sample_offset() + runs.sample_size();
       if (sample_end_offset > highest_end_offset_)
         highest_end_offset_ = sample_end_offset;
-
-      runs.AdvanceSample();
+      if (!runs.AdvanceSample())
+        return false;
     }
-    runs.AdvanceRun();
+    if (!runs.AdvanceRun())
+      return false;
   }
 
   return true;
