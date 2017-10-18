a37f2dade8730e2ee3738e676c32a29294e03b97
Bhagirathi Satpathy <bhagirathi.s@samsung.com>
2017-10-18 07:17:52 +0000

    Reduce unused memory inclusion from Document
    This CL removes unused memory inclusion from Document.
    
    Bug: 
    Change-Id: I14b5f687b901270301d456b924373355692bf07f
    Reviewed-on: https://chromium-review.googlesource.com/723239
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: srirama chandra sekhar <srirama.m@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#509708}

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -30,8 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/dom/Document.h"
 
-#include <memory>
-
 #include "bindings/core/v8/ExceptionMessages.h"
 #include "bindings/core/v8/ExceptionState.h"
 #include "bindings/core/v8/ScriptController.h"
