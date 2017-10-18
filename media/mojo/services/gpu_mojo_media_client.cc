1060fb1ead71bf47f809650015d3c08e0fd88b3d
Frank Liberato <liberato@chromium.org>
2017-10-18 00:10:58 +0000

    Add WIP Mojo D3D11VideoDecoder.
    This is a WIP.  Basic playback works, though EOS is ignored.  There is
    quite a bit of thread-hopping, since the accelerator / decoder is run
    on the GPU main thread rather than the mojo thread.  This will be
    eliminated in a later CL.
    
    Resource cleanup is of the "and the process will exit anyway" variety.
    
    It's based heavily on D3D11VideoDecodeAccelerator.  Re-uses the H264
    Accelerated Video Decoder for D3D11.  The accelerator was written with
    the VDA in mind, so the VideoDecoder jumps through some minor hoops
    to use it also.  It is intended that this will replace the VDA as soon
    as it reaches parity with it, and the accelerator can be modified for
    easier use with the VideoDecoder.
    
    Set "enable_d3d11_video_decoder=true" in gn config.  This will enable
    the decoder, and:
     - enable MojoVideoDecoder
     - enable mojo CDM
     - set the media mojo host to be the GPU process.
    
    Also incorporates fixes to the d3d11_h264_accelerator from:
    https://chromium-review.googlesource.com/c/chromium/src/+/661237
    by rkuroiwa@ .
    
    Bug: 
    Change-Id: Ie5f2b651895bc812e5e6bba7c5eb302f2702a1b6
    Reviewed-on: https://chromium-review.googlesource.com/693284
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509615}

--- media/mojo/services/gpu_mojo_media_client.cc
+++ media/mojo/services/gpu_mojo_media_client.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
+#include "build/build_config.h"
 #include "gpu/ipc/service/gpu_channel.h"
 #include "media/base/audio_decoder.h"
 #include "media/base/cdm_factory.h"
@@ -29,6 +30,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/mojo/services/mojo_provision_fetcher.h"
 #include "services/service_manager/public/cpp/connect.h"
 #endif  // defined(OS_ANDROID)
+
+#if defined(OS_WIN)
+#include "media/gpu/windows/d3d11_video_decoder.h"
+#endif  // defined(OS_WIN)
 
 namespace media {
 
@@ -54,8 +59,10 @@ std::unique_ptr<MediaDrmStorage> CreateMediaDrmStorage(
   return base::MakeUnique<MojoMediaDrmStorage>(
       std::move(media_drm_storage_ptr));
 }
+#endif  // defined(OS_ANDROID)
 
-#if BUILDFLAG(ENABLE_MEDIA_CODEC_VIDEO_DECODER)
+#if BUILDFLAG(ENABLE_MEDIA_CODEC_VIDEO_DECODER) || \
+    BUILDFLAG(ENABLE_D3D11_VIDEO_DECODER)
 gpu::GpuCommandBufferStub* GetGpuCommandBufferStub(
     base::WeakPtr<MediaGpuChannelManager> media_gpu_channel_manager,
     base::UnguessableToken channel_token,
@@ -70,8 +77,7 @@ gpu::GpuCommandBufferStub* GetGpuCommandBufferStub(
 
   return channel->LookupCommandBuffer(route_id);
 }
-#endif  // BUILDFLAG(ENABLE_MEDIA_CODEC_VIDEO_DECODER)
-#endif  // defined(OS_ANDROID)
+#endif  // BUILDFLAG(ENABLE_MEDIA_CODEC_VIDEO_DECODER || D3D11)
 
 }  // namespace
 
@@ -122,9 +128,15 @@ std::unique_ptr<VideoDecoder> GpuMojoMediaClient::CreateVideoDecoder(
       base::MakeUnique<VideoFrameFactoryImpl>(gpu_task_runner_,
                                               std::move(get_stub_cb)),
       context_ref_factory_->CreateRef());
+#elif BUILDFLAG(ENABLE_D3D11_VIDEO_DECODER)
+  return base::MakeUnique<D3D11VideoDecoder>(
+      gpu_task_runner_,
+      base::Bind(&GetGpuCommandBufferStub, media_gpu_channel_manager_,
+                 command_buffer_id->channel_token, command_buffer_id->route_id),
+      std::move(output_cb));
 #else
   return nullptr;
-#endif  // BUILDFLAG(ENABLE_MEDIA_CODEC_VIDEO_DECODER)
+#endif  // BUILDFLAG(ENABLE_{MEDIA_CODEC | D3D11}_VIDEO_DECODER)
 }
 
 std::unique_ptr<CdmFactory> GpuMojoMediaClient::CreateCdmFactory(
