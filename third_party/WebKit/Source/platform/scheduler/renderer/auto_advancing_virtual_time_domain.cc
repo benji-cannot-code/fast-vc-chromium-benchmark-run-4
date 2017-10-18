d2d82fef5c6f2cb7878b7adff890b35e31702a21
Alex Clarke <alexclarke@chromium.org>
2017-10-18 15:57:17 +0000

    It's possible for pages to send infinite messages which can arbitrarily block virtual time.  We can prevent this by setting an upper limit on the number of tasks that can run before virtual time is advanced.
    Bug: 
    Change-Id: If2f8a82b3f9fc51effeb10b67bef0a684833d339
    Reviewed-on: https://chromium-review.googlesource.com/725329
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509780}

--- third_party/WebKit/Source/platform/scheduler/renderer/auto_advancing_virtual_time_domain.cc
+++ third_party/WebKit/Source/platform/scheduler/renderer/auto_advancing_virtual_time_domain.cc
@@ -5,16 +5,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "platform/scheduler/renderer/auto_advancing_virtual_time_domain.h"
 
+#include "platform/scheduler/child/scheduler_helper.h"
+
 namespace blink {
 namespace scheduler {
 
 AutoAdvancingVirtualTimeDomain::AutoAdvancingVirtualTimeDomain(
-    base::TimeTicks initial_time)
+    base::TimeTicks initial_time,
+    SchedulerHelper* helper)
     : VirtualTimeDomain(initial_time),
+      task_starvation_count_(0),
+      max_task_starvation_count_(0),
       can_advance_virtual_time_(true),
-      observer_(nullptr) {}
+      observer_(nullptr),
+      helper_(helper) {
+  helper_->AddTaskObserver(this);
+}
 
-AutoAdvancingVirtualTimeDomain::~AutoAdvancingVirtualTimeDomain() {}
+AutoAdvancingVirtualTimeDomain::~AutoAdvancingVirtualTimeDomain() {
+  helper_->RemoveTaskObserver(this);
+}
 
 base::Optional<base::TimeDelta>
 AutoAdvancingVirtualTimeDomain::DelayTillNextTask(LazyNow* lazy_now) {
@@ -22,6 +32,7 @@ AutoAdvancingVirtualTimeDomain::DelayTillNextTask(LazyNow* lazy_now) {
   if (!can_advance_virtual_time_ || !NextScheduledRunTime(&run_time))
     return base::nullopt;
 
+  task_starvation_count_ = 0;
   AdvanceTo(run_time);
   if (observer_)
     observer_->OnVirtualTimeAdvanced();
@@ -51,8 +62,34 @@ void AutoAdvancingVirtualTimeDomain::SetCanAdvanceVirtualTime(
     RequestDoWork();
 }
 
+void AutoAdvancingVirtualTimeDomain::SetMaxVirtualTimeTaskStarvationCount(
+    int max_task_starvation_count) {
+  max_task_starvation_count_ = max_task_starvation_count;
+  if (max_task_starvation_count_ == 0)
+    task_starvation_count_ = 0;
+}
+
 const char* AutoAdvancingVirtualTimeDomain::GetName() const {
   return "AutoAdvancingVirtualTimeDomain";
+}
+
+void AutoAdvancingVirtualTimeDomain::WillProcessTask(
+    const base::PendingTask& pending_task) {}
+
+void AutoAdvancingVirtualTimeDomain::DidProcessTask(
+    const base::PendingTask& pending_task) {
+  if (max_task_starvation_count_ == 0 ||
+      ++task_starvation_count_ < max_task_starvation_count_) {
+    return;
+  }
+
+  // Delayed tasks are being excessively starved, so allow virtual time to
+  // advance.
+  base::TimeTicks run_time;
+  if (NextScheduledRunTime(&run_time)) {
+    AdvanceTo(run_time);
+    task_starvation_count_ = 0;
+  }
 }
 
 AutoAdvancingVirtualTimeDomain::Observer::Observer() {}
