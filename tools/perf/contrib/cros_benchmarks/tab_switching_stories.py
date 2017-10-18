06210f6f68113806fd8098f4537d0a348ab78435
Vovo Yang <vovoy@google.com>
2017-10-18 15:07:28 +0000

    Add option --pause-after-creation
    Add option to specify the pause between tab creation and tab switch
    so that tester can manually do something during this pause.
    
    Bug: 724381
    Change-Id: I3440edab585c64aef4c74b64a86162d75e3f6173
    Reviewed-on: https://chromium-review.googlesource.com/721105
    Commit-Queue: Vovo Yang <vovoy@chromium.org>
    Reviewed-by: Cheng-Yu Lee <cylee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509771}

--- tools/perf/contrib/cros_benchmarks/tab_switching_stories.py
+++ tools/perf/contrib/cros_benchmarks/tab_switching_stories.py
@@ -3,9 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import py_utils
 import logging
+import time
 
+import py_utils
 from telemetry.core import exceptions
 from telemetry.page import page as page_module
 from telemetry.page import shared_page_state
@@ -17,12 +18,14 @@ from contrib.cros_benchmarks import cros_utils
 class CrosMultiTabStory(page_module.Page):
   """Base class for multi-tab stories."""
 
-  def __init__(self, story_set, cros_remote, tabset_repeat=1):
+  def __init__(self, story_set, cros_remote, tabset_repeat=1,
+               pause_after_creation=0):
     super(CrosMultiTabStory, self).__init__(
         shared_page_state_class=shared_page_state.SharedPageState,
         page_set=story_set, name=self.NAME, url=self.URL)
     self._cros_remote = cros_remote
     self._tabset_repeat = tabset_repeat
+    self._pause_after_creation = pause_after_creation
 
   def RunNavigateSteps(self, action_runner):
     """Opening tabs and waiting for them to load."""
@@ -58,6 +61,7 @@ class CrosMultiTabStory(page_module.Page):
 
   def RunPageInteractions(self, action_runner):
     """Tab switching to each tabs."""
+    time.sleep(self._pause_after_creation)
     url_list = self.URL_LIST * self._tabset_repeat
     browser = action_runner.tab.browser
 
