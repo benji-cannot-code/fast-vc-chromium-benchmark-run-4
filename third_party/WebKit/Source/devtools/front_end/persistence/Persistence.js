f518569d75c747debbd305017ffd5744efa391d0
Nathan Bruer <allada@chromium.org>
2017-10-18 00:18:28 +0000

    [Devtools] Added ability to manage binding outside of workspaces
    Adds new interfaces to manage bindings from outside persistence mappers.
    
    * Adds ignoreProject/removeIgnoredProject - This is used to ignore a
    project and automapping will not auto-map anything in these projects.
    * Added addBinding/removeBinding - These are used to manually bind two
    UISourceCodes together.
    
    R=lushnikov,pfeldman
    BUG=760316
    
    Change-Id: Id86a368a1b5ec50c34ed38deee8714b190fb6869
    Reviewed-on: https://chromium-review.googlesource.com/720244
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Commit-Queue: Blaise Bruer <allada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509620}

--- third_party/WebKit/Source/devtools/front_end/persistence/Persistence.js
+++ third_party/WebKit/Source/devtools/front_end/persistence/Persistence.js
@@ -34,6 +34,34 @@ Persistence.Persistence = class extends Common.Object {
   }
 
   /**
+   * @param {!Workspace.Project} project
+   */
+  ignoreProject(project) {
+    this._mapping.ignoreProject(project);
+  }
+
+  /**
+   * @param {!Workspace.Project} project
+   */
+  removeIgnoredProject(project) {
+    this._mapping.removeIgnoredProject(project);
+  }
+
+  /**
+   * @param {!Persistence.PersistenceBinding} binding
+   */
+  addBinding(binding) {
+    this._establishBinding(binding);
+  }
+
+  /**
+   * @param {!Persistence.PersistenceBinding} binding
+   */
+  removeBinding(binding) {
+    this._innerRemoveBinding(binding);
+  }
+
+  /**
    * @param {function(function(!Persistence.PersistenceBinding), function(!Persistence.PersistenceBinding)):!Persistence.MappingSystem} mappingFactory
    */
   _setMappingForTest(mappingFactory) {
@@ -116,7 +144,7 @@ Persistence.Persistence = class extends Common.Object {
   /**
    * @param {!Persistence.PersistenceBinding} binding
    */
-  _onBindingRemoved(binding) {
+  _innerRemoveBinding(binding) {
     binding._removed = true;
     if (binding.network[Persistence.Persistence._binding] !== binding)
       return;
@@ -142,6 +170,13 @@ Persistence.Persistence = class extends Common.Object {
     this._notifyBindingEvent(binding.network);
     this._notifyBindingEvent(binding.fileSystem);
     this.dispatchEventToListeners(Persistence.Persistence.Events.BindingRemoved, binding);
+  }
+
+  /**
+   * @param {!Persistence.PersistenceBinding} binding
+   */
+  _onBindingRemoved(binding) {
+    this._innerRemoveBinding(binding);
   }
 
   /**
@@ -398,7 +433,17 @@ Persistence.PersistenceBinding = class {
 Persistence.MappingSystem = function() {};
 
 Persistence.MappingSystem.prototype = {
-  dispose: function() {}
+  dispose: function() {},
+
+  /**
+   * @param {!Workspace.Project} project
+   */
+  ignoreProject(project) {},
+
+  /**
+   * @param {!Workspace.Project} project
+   */
+  removeIgnoredProject(project) {}
 };
 
 /** @type {!Persistence.Persistence} */
