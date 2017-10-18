1eac270ca49690d722dfb2de7ce56654f25437a5
Tetsui Ohkubo <tetsui@chromium.org>
2017-10-18 05:02:58 +0000

    Add empty notifier settings view to message center.
    As a part of message center restyling, this CL adds a new view for
    message center when there are no notifier sources.
    
    Previously, there was no such view and the layout was broken when
    there were no notifier sources.
    
    TEST=ash_unittests
    BUG=726246
    
    Change-Id: I51c6b6b074995003c622e4bd4e1de19d2db5f56b
    Reviewed-on: https://chromium-review.googlesource.com/708106
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509688}

--- ash/message_center/notifier_settings_view.h
+++ ash/message_center/notifier_settings_view.h
@@ -53,6 +53,7 @@ class ASH_EXPORT NotifierSettingsView
 
  private:
   FRIEND_TEST_ALL_PREFIXES(NotifierSettingsViewTest, TestLearnMoreButton);
+  FRIEND_TEST_ALL_PREFIXES(NotifierSettingsViewTest, TestEmptyNotifierView);
 
   class ASH_EXPORT NotifierButton : public views::Button,
                                     public views::ButtonListener {
@@ -110,7 +111,9 @@ class ASH_EXPORT NotifierSettingsView
   views::ImageButton* title_arrow_;
   views::ToggleButton* quiet_mode_toggle_;
   views::View* header_view_;
+  views::Label* top_label_;
   views::ScrollView* scroller_;
+  views::View* no_notifiers_view_;
   message_center::NotifierSettingsProvider* provider_;
   std::set<NotifierButton*> buttons_;
 
