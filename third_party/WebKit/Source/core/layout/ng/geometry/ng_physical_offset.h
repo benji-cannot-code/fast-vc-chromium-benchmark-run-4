9c82492527286a2b3022d9364bc9fb51e9f08900
Ian Kilpatrick <ikilpatrick@chromium.org>
2017-10-18 20:51:30 +0000

    [LayoutNG] Remove NGFragment::Offset in favour of ConvertToLogical
    This was something I found yesterday, while looking at the remaining
    ng-bot failures for the line-by-line patch.
    
    We weren't calculating the NGFragment::Offset correctly (impossible
    without a parent size), so I removed it in favour of:
    NGPhysicalOffset::ConvertToLogical.
    
    Bug: 635619
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I88196b26d6702acf4c3b65a386eba04e2d4d1d33
    Reviewed-on: https://chromium-review.googlesource.com/726299
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509866}

--- third_party/WebKit/Source/core/layout/ng/geometry/ng_physical_offset.h
+++ third_party/WebKit/Source/core/layout/ng/geometry/ng_physical_offset.h
@@ -7,12 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define NGPhysicalOffset_h
 
 #include "core/CoreExport.h"
-
+#include "core/layout/ng/ng_writing_mode.h"
 #include "platform/LayoutUnit.h"
+#include "platform/text/TextDirection.h"
 
 namespace blink {
 
 class LayoutPoint;
+struct NGLogicalOffset;
+struct NGPhysicalSize;
 
 // NGPhysicalOffset is the position of a rect (typically a fragment) relative to
 // its parent rect in the physical coordinate system.
@@ -22,6 +25,15 @@ struct CORE_EXPORT NGPhysicalOffset {
 
   LayoutUnit left;
   LayoutUnit top;
+
+  // Converts a physical offset to a logical offset. See:
+  // https://drafts.csswg.org/css-writing-modes-3/#logical-to-physical
+  // @param outer_size the size of the rect (typically a fragment).
+  // @param inner_size the size of the inner rect (typically a child fragment).
+  NGLogicalOffset ConvertToLogical(NGWritingMode,
+                                   TextDirection,
+                                   NGPhysicalSize outer_size,
+                                   NGPhysicalSize inner_size) const;
 
   NGPhysicalOffset operator+(const NGPhysicalOffset& other) const;
   NGPhysicalOffset& operator+=(const NGPhysicalOffset& other);
--- third_party/WebKit/Source/core/layout/ng/ng_fragment.h
+++ third_party/WebKit/Source/core/layout/ng/ng_fragment.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 struct NGBorderEdges;
-struct NGLogicalOffset;
 struct NGLogicalSize;
 
 class CORE_EXPORT NGFragment {
@@ -33,11 +32,6 @@ class CORE_EXPORT NGFragment {
   LayoutUnit InlineSize() const;
   LayoutUnit BlockSize() const;
   NGLogicalSize Size() const;
-
-  // Returns the offset relative to the parent fragment's content-box.
-  LayoutUnit InlineOffset() const;
-  LayoutUnit BlockOffset() const;
-  NGLogicalOffset Offset() const;
 
   NGBorderEdges BorderEdges() const;
 
