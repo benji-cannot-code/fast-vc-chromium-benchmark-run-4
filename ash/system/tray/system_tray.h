bac7d88e09496224730bd251f8a8d4314b22c942
Tetsui Ohkubo <tetsui@chromium.org>
2017-10-18 04:41:16 +0000

    Close message center on another window activation.
    Previously, message center was closed only when
    WebNotificationTray::ClickedOutsideBubble() was called.
    As a result, when a user opens the launcher by Search key,
    message center bubble remained open.
    
    This CL also closes the bubble when another window is activated.
    The implementation is based on SystemTray::ActivationObserver.
    
    TEST=WebNotificationTrayTest.CloseOnActivation
    BUG=757979
    
    Change-Id: Ifea4c2cc56cfe4ca7b0ecfc065426cc30eccc512
    Reviewed-on: https://chromium-review.googlesource.com/716441
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509685}

--- ash/system/tray/system_tray.h
+++ ash/system/tray/system_tray.h
@@ -153,7 +153,6 @@ class ASH_EXPORT SystemTray : public TrayBackgroundView {
 
  private:
   friend class SystemTrayTestApi;
-  class ActivationObserver;
 
   // Activates the bubble and starts key navigation with the |key_event|.
   void ActivateAndStartNavigation(const ui::KeyEvent& key_event);
@@ -231,8 +230,6 @@ class ASH_EXPORT SystemTray : public TrayBackgroundView {
   // A reference to the Screen share and capture item.
   ScreenTrayItem* screen_capture_tray_item_ = nullptr;  // not owned
   ScreenTrayItem* screen_share_tray_item_ = nullptr;    // not owned
-
-  std::unique_ptr<ActivationObserver> activation_observer_;
 
   DISALLOW_COPY_AND_ASSIGN(SystemTray);
 };
--- ash/system/web_notification/web_notification_tray.h
+++ ash/system/web_notification/web_notification_tray.h
@@ -121,6 +121,7 @@ class ASH_EXPORT WebNotificationTray
   FRIEND_TEST_ALL_PREFIXES(WebNotificationTrayTest, PopupAndSystemTray);
   FRIEND_TEST_ALL_PREFIXES(WebNotificationTrayTest, PopupAndAutoHideShelf);
   FRIEND_TEST_ALL_PREFIXES(WebNotificationTrayTest, VisibleSmallIcon);
+  FRIEND_TEST_ALL_PREFIXES(WebNotificationTrayTest, CloseOnActivation);
 
   void UpdateTrayContent();
 
