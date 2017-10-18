74f2759fff235fb386417bc7411bc7c2bfd706fe
Daniel Cheng <dcheng@chromium.org>
2017-10-18 18:39:53 +0000

    Suspend plugin updates when scripting is forbidden.
    Executing deferred plugin updates can run script, so ensure that
    plugin updates are deferred until script forbidden scope is no
    longer active.
    
    Bug: 765920
    Change-Id: I42f89287c62bcf6ee748e04d984539644e839c99
    Reviewed-on: https://chromium-review.googlesource.com/720173
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509818}

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -2101,12 +2101,8 @@ static void AssertLayoutTreeUpdated(Node& root) {
 void Document::UpdateStyleAndLayoutTree() {
   DCHECK(IsMainThread());
 
+  HTMLFrameOwnerElement::PluginDisposeSuspendScope suspend_plugin_dispose;
   ScriptForbiddenScope forbid_script;
-  // We should forbid script execution for plugins here because update while
-  // layout is changing, HTMLPlugin element can be reattached and plugin can be
-  // destroyed. Plugin can execute scripts on destroy. It produces crash without
-  // PluginScriptForbiddenScope: crbug.com/550427.
-  PluginScriptForbiddenScope plugin_forbid_script;
 
   if (!View() || !IsActive())
     return;
@@ -2205,7 +2201,6 @@ void Document::UpdateStyle() {
 
   unsigned initial_element_count = GetStyleEngine().StyleForElementCount();
 
-  HTMLFrameOwnerElement::PluginDisposeSuspendScope suspend_plugin_dispose;
   lifecycle_.AdvanceTo(DocumentLifecycle::kInStyleRecalc);
 
   StyleRecalcChange change = kNoChange;
@@ -2358,6 +2353,7 @@ void Document::UpdateStyleAndLayoutIgnorePendingStylesheetsForNode(Node* node) {
 void Document::UpdateStyleAndLayout() {
   DCHECK(IsMainThread());
 
+  HTMLFrameOwnerElement::PluginDisposeSuspendScope suspend_plugin_dispose;
   ScriptForbiddenScope forbid_script;
 
   LocalFrameView* frame_view = View();
