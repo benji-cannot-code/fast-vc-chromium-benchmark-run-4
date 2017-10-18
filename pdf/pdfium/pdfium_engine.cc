a21e72b3e0159628304fa753fae3d6d8f0ea435d
Henrique Nakashima <hnakashima@chromium.org>
2017-10-18 20:43:13 +0000

    Measure % of PDFs with attachments and % of PDFs that are linearized.
    Bug: chromium:768986
    Change-Id: I255fb808efd17a48d9e3760f2d3ed3317196a2bc
    Reviewed-on: https://chromium-review.googlesource.com/716306
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509861}

--- pdf/pdfium/pdfium_engine.cc
+++ pdf/pdfium/pdfium_engine.cc
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "printing/pdf_transform.h"
 #include "printing/units.h"
 #include "third_party/pdfium/public/fpdf_annot.h"
+#include "third_party/pdfium/public/fpdf_attachment.h"
 #include "third_party/pdfium/public/fpdf_edit.h"
 #include "third_party/pdfium/public/fpdf_ext.h"
 #include "third_party/pdfium/public/fpdf_flatten.h"
@@ -1282,6 +1283,9 @@ void PDFiumEngine::FinishLoadingDocument() {
   if (doc_) {
     DocumentFeatures document_features;
     document_features.page_count = pages_.size();
+    document_features.has_attachments = (FPDFDoc_GetAttachmentCount(doc_) > 0);
+    document_features.is_linearized =
+        (FPDFAvail_IsLinearized(fpdf_availability_) == PDF_LINEARIZED);
     client_->DocumentLoadComplete(document_features);
   }
 }
