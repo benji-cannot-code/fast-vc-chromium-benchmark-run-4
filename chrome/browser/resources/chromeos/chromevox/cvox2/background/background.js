190d8c6d46954eda2e6dcd3385b76960fd4e9e92
David Tseng <dtseng@chromium.org>
2017-10-19 19:58:58 +0000

    Translate braille routing on node spans to selection action in rich text
    Bug: None
    Test: braille routing over rich contextual output routes selection within rich text.
    For example, routing over'tbl' moves the selection to the beginning of the table (i.e. a selection on the table node).
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I02bfbcf8e48dbb502f1e031c67bc778a219e4a92
    Reviewed-on: https://chromium-review.googlesource.com/726383
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510169}

--- chrome/browser/resources/chromeos/chromevox/cvox2/background/background.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/background/background.js
@@ -632,7 +632,8 @@ Background.prototype = {
     actionNode.doDefault();
 
     if (actionNode.role != RoleType.STATIC_TEXT &&
-        actionNode.role != RoleType.TEXT_FIELD)
+        actionNode.role != RoleType.TEXT_FIELD &&
+        !actionNode.state[StateType.RICHLY_EDITABLE])
       return;
 
     if (!selectionSpan)
