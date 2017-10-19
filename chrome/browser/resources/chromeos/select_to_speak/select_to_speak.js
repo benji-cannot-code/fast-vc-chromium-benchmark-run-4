fd58207dcc8026dba2c310190c53e2a8c0adeade
Katie Dektar <katydek@google.com>
2017-10-19 20:08:31 +0000

    Nodes of size 0 inherit size from ancestor and now are labeled offscreen.
    This allows automation API users to determine if something should be
    ignored because it isn't visible to the user. It generally applies to
    elements with 0 height and overflow:hidden whose contents also have
    no size.
    
    Bug: 773851
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ia656a5907191aaad71dd2315e3f60a21fbecb6d8
    Reviewed-on: https://chromium-review.googlesource.com/726242
    Commit-Queue: Katie Dektar <katydek@google.com>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510175}

--- chrome/browser/resources/chromeos/select_to_speak/select_to_speak.js
+++ chrome/browser/resources/chromeos/select_to_speak/select_to_speak.js
@@ -417,7 +417,7 @@ SelectToSpeak.prototype = {
     if (found)
       return true;
 
-    if (!node.name || !node.location)
+    if (!node.name || !node.location || node.state.offscreen)
       return false;
 
     if (overlaps(node.location, rect)) {
