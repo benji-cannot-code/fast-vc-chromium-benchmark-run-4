9f5f9e01e075fdfca267da57f2188f864d3d269b
Theresa Wellington <twellington@google.com>
2017-10-19 20:44:12 +0000

    Remove btn_menu in favor of ic_more_vert_black_24dp
    BUG=746712
    
    Change-Id: I8fc4116433defb3ec7fe973d071c65f2885aeaee
    Reviewed-on: https://chromium-review.googlesource.com/728341
    Commit-Queue: Theresa <twellington@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510198}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarPhone.java
@@ -2416,8 +2416,8 @@ public class ToolbarPhone extends ToolbarLayout
     }
 
     private void setTabSwitcherAnimationMenuDrawable() {
-        mTabSwitcherAnimationMenuDrawable = ApiCompatibilityUtils.getDrawable(getResources(),
-                R.drawable.btn_menu);
+        mTabSwitcherAnimationMenuDrawable = ApiCompatibilityUtils.getDrawable(
+                getResources(), R.drawable.ic_more_vert_black_24dp);
         mTabSwitcherAnimationMenuDrawable.mutate();
         mTabSwitcherAnimationMenuDrawable.setColorFilter(
                 isIncognito() ? mLightModeDefaultColor : mDarkModeDefaultColor,
