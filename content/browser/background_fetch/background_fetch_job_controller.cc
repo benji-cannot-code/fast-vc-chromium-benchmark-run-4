666680c2b40fe2ac3e2435bdaefaa8156fc51dc9
Dan Elphick <delphick@chromium.org>
2017-10-19 15:28:03 +0000

    BackgroundFetch aborts now cancel active Downloads.
    Pass through the Abort message to the DownloadService for any
    outstanding downloads matching the given job_unique_id.
    
    Update abort unit tests to verify that the download successful messages
    do not get delivered.
    
    Change-Id: I600a3f102047939d7189dc6d30e9ca88eedf4c04
    Bug: 758562
    Reviewed-on: https://chromium-review.googlesource.com/723321
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: John Mellor <johnme@chromium.org>
    Commit-Queue: Dan Elphick <delphick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510090}

--- content/browser/background_fetch/background_fetch_job_controller.cc
+++ content/browser/background_fetch/background_fetch_job_controller.cc
@@ -98,12 +98,7 @@ void BackgroundFetchJobController::DidCompleteRequest(
     const scoped_refptr<BackgroundFetchRequestInfo>& request,
     const std::string& download_guid) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
-  DCHECK(state_ == State::FETCHING || state_ == State::ABORTED);
-
-  // TODO(delphick): When ABORT is implemented correctly we should hopefully
-  // never get here and the DCHECK above should only allow FETCHING.
-  if (state_ == State::ABORTED)
-    return;
+  DCHECK(state_ == State::FETCHING);
 
   // This request is no longer in-progress, so the DataManager will take over
   // responsibility for storing its downloaded bytes, though still need a cache.
@@ -163,7 +158,7 @@ void BackgroundFetchJobController::Abort() {
       return;  // Ignore attempt to abort after completion/abort.
   }
 
-  delegate_proxy_->Abort();
+  delegate_proxy_->Abort(registration_id_.unique_id());
 
   state_ = State::ABORTED;
   // Inform the owner of the controller about the job having aborted.
