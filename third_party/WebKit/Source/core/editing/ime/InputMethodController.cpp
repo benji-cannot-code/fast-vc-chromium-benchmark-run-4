b85da960d70596d50eaf62ca760e5d189732e483
Yoshifumi Inoue <yosin@chromium.org>
2017-10-19 02:32:55 +0000

    Get rid of redundant include directive of "ChromeClient.h" from "InputMethodController.cpp"
    This patch gets rid of redundant include directive of "ChromeClient.h" from
    "InputMethodController.cpp" for improving code health.
    
    Change-Id: I05e00762e14b1d665bcd7da81203335d900865e1
    Reviewed-on: https://chromium-review.googlesource.com/725206
    Reviewed-by: Takayoshi Kochi <kochi@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509978}

--- third_party/WebKit/Source/core/editing/ime/InputMethodController.cpp
+++ third_party/WebKit/Source/core/editing/ime/InputMethodController.cpp
@@ -52,7 +52,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/input_type_names.h"
 #include "core/layout/LayoutObject.h"
 #include "core/layout/LayoutTheme.h"
-#include "core/page/ChromeClient.h"
 #include "core/page/FocusController.h"
 #include "core/page/Page.h"
 
