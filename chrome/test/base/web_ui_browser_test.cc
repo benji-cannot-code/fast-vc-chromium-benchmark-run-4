a6457b544716834d7c1cc20a3e8e23360e57e2da
Reilly Grant <reillyg@chromium.org>
2017-10-19 04:05:34 +0000

    Enable MojoJSTest for WebUI only in WebUI browser tests
    This change removes the additional "context enabled" feature for the
    MojoJSTest bindings and instead simply modifies the base class for all
    WebUI browser tests to add a command line flag enabling these bindings.
    
    This enables the bindings in all renderers launched by such tests but
    that is okay.
    
    Bug: 775760
    Change-Id: I36ec28aed07ff9bcf46228f9828e157c96222723
    Reviewed-on: https://chromium-review.googlesource.com/727477
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509994}

--- chrome/test/base/web_ui_browser_test.cc
+++ chrome/test/base/web_ui_browser_test.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_ui_controller.h"
 #include "content/public/browser/web_ui_message_handler.h"
+#include "content/public/common/content_switches.h"
 #include "content/public/common/url_constants.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_navigation_observer.h"
@@ -371,6 +372,14 @@ base::LazyInstance<MockWebUIProvider>::DestructorAtExit mock_provider_ =
     LAZY_INSTANCE_INITIALIZER;
 
 }  // namespace
+
+void WebUIBrowserTest::SetUpCommandLine(base::CommandLine* command_line) {
+  JavaScriptBrowserTest::SetUpCommandLine(command_line);
+
+  // Enables the MojoJSTest bindings which are used for WebUI tests.
+  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+      switches::kEnableBlinkFeatures, "MojoJSTest");
+}
 
 void WebUIBrowserTest::SetUpOnMainThread() {
   JavaScriptBrowserTest::SetUpOnMainThread();
