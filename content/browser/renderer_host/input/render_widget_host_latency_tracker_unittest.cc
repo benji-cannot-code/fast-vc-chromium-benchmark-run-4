7a542d49daca496a54ffc93415c9dfc6bd79d545
Navid Zolghadr <nzolghadr@chromium.org>
2017-10-19 21:18:33 +0000

    Remove old input metrics that were replaced
    Remove old touch scrolling metrics that were
    replaced back in M56.
    
    Bug: 649754
    Change-Id: If47392ac6902bd9cf4e8a001752943e0f615ba70
    Reviewed-on: https://chromium-review.googlesource.com/728714
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Navid Zolghadr <nzolghadr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510218}

--- content/browser/renderer_host/input/render_widget_host_latency_tracker_unittest.cc
+++ content/browser/renderer_host/input/render_widget_host_latency_tracker_unittest.cc
@@ -478,10 +478,6 @@ TEST_F(RenderWidgetHostLatencyTrackerTest, TestTouchToFirstScrollHistograms) {
     // UMA histograms.
     EXPECT_TRUE(HistogramSizeEq("Event.Latency.Browser.TouchUI", 1));
     EXPECT_TRUE(HistogramSizeEq("Event.Latency.Browser.TouchAcked", 1));
-    EXPECT_TRUE(
-        HistogramSizeEq("Event.Latency.TouchToFirstScrollUpdateSwapBegin", 1));
-    EXPECT_TRUE(
-        HistogramSizeEq("Event.Latency.TouchToScrollUpdateSwapBegin", 1));
     EXPECT_TRUE(HistogramSizeEq(
         "Event.Latency.ScrollBegin.Touch.TimeToScrollUpdateSwapBegin2", 1));
 
@@ -731,9 +727,6 @@ TEST_F(RenderWidgetHostLatencyTrackerTest,
   EXPECT_TRUE(HistogramSizeEq("Event.Latency.Browser.TouchUI", 1));
   EXPECT_TRUE(HistogramSizeEq("Event.Latency.Browser.WheelAcked", 1));
   EXPECT_TRUE(HistogramSizeEq("Event.Latency.Browser.TouchAcked", 1));
-  EXPECT_TRUE(
-      HistogramSizeEq("Event.Latency.TouchToFirstScrollUpdateSwapBegin", 1));
-  EXPECT_TRUE(HistogramSizeEq("Event.Latency.TouchToScrollUpdateSwapBegin", 1));
   EXPECT_TRUE(
       HistogramSizeEq("Event.Latency.ScrollUpdate.TouchToHandled_Main", 0));
   EXPECT_TRUE(
