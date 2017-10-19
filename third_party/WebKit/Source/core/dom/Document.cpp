62798038fdd4df018add6872c85adc498f630c7b
Dirk Pranke <dpranke@chromium.org>
2017-10-19 23:18:55 +0000

    Revert "Reland: Add UKM Document.OutliveTimeAfterShutdown"
    This reverts commit 71c0f5b542ba0e51a699d84c22d5eed63799466b.
    
    Reason for revert: This CL logs to stderr on every test, causing the archive_webkit_layout_tests step to explode. See crbug.com/776334.
    
    Original change's description:
    > Reland: Add UKM Document.OutliveTimeAfterShutdown
    >
    > This is basically same as https://chromium-review.googlesource.com/c/chromium/src/+/647050.
    > This CL tries to reland the change after rebasing.
    >
    > This CL adds a new UKM Document.OutliveTimeAfterShutdown, that is
    > recorded when a Document object survives 5, 10, 20 or 50 garbage
    > collections after detached. If a document outlives such long time, the
    > document might be leaked. The UKM would be very useful to know where such
    > leaky documents exist and to fix them.
    >
    > Design doc: https://docs.google.com/document/d/1fbs5smdd-pBLLMpq7u8EkyddZILtI7CZPJlo_AA1kak/edit?usp=sharing
    >
    > Bug: 757374
    > Change-Id: I1f64d2a9260d898c386bb948dd25a1c5586f8eb7
    > Reviewed-on: https://chromium-review.googlesource.com/722301
    > Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Chris Palmer <palmer@chromium.org>
    > Reviewed-by: Steven Holte <holte@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#510029}
    
    TBR=palmer@chromium.org,hajimehoshi@chromium.org,haraken@chromium.org,holte@chromium.org
    
    Change-Id: I59155476ced5620a87e027225f9f017c296813e2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 757374
    Reviewed-on: https://chromium-review.googlesource.com/729462
    Commit-Queue: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510247}

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -262,7 +262,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/platform/modules/insecure_input/insecure_input_service.mojom-blink.h"
 #include "public/platform/site_engagement.mojom-blink.h"
 #include "services/metrics/public/cpp/mojo_ukm_recorder.h"
-#include "services/metrics/public/cpp/ukm_builders.h"
 #include "services/metrics/public/interfaces/ukm_interface.mojom-shared.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
 
@@ -313,18 +312,12 @@ class DocumentOutliveTimeReporter : public BlinkGCObserver {
     int outlive_time_count = GetOutliveTimeCount();
     if (outlive_time_count == 5 || outlive_time_count == 10) {
       const char* kUMAString = "Document.OutliveTimeAfterShutdown.GCCount";
-
       if (outlive_time_count == 5)
         UMA_HISTOGRAM_ENUMERATION(kUMAString, kGCCount5, kGCCountMax);
       else if (outlive_time_count == 10)
         UMA_HISTOGRAM_ENUMERATION(kUMAString, kGCCount10, kGCCountMax);
       else
         NOTREACHED();
-    }
-
-    if (outlive_time_count == 5 || outlive_time_count == 10 ||
-        outlive_time_count == 20 || outlive_time_count == 50) {
-      document_->RecordUkmOutliveTimeAfterShutdown(outlive_time_count);
     }
   }
 
@@ -3707,17 +3700,8 @@ void Document::SetURL(const KURL& url) {
   UpdateBaseURL();
   GetContextFeatures().UrlDidChange(this);
 
-  if (!ukm_recorder_ && IsInMainFrame()) {
-    ukm::mojom::UkmRecorderInterfacePtr interface;
-    frame_->GetInterfaceProvider().GetInterface(mojo::MakeRequest(&interface));
-    ukm_source_id_ = ukm::UkmRecorder::GetNewSourceID();
-    ukm_recorder_.reset(new ukm::MojoUkmRecorder(std::move(interface)));
-  }
-
-  if (ukm_recorder_) {
-    DCHECK(IsInMainFrame());
+  if (ukm_recorder_)
     ukm_recorder_->UpdateSourceURL(ukm_source_id_, url_);
-  }
 }
 
 KURL Document::ValidBaseElementURL() const {
