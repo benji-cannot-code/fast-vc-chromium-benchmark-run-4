96964ba2e17172ec44498637f54517e4a1bf4bce
Lei Zhang <thestig@chromium.org>
2017-10-19 21:56:24 +0000

    More PDFiumEngine cleanup.
    - Use base::TimeDelta for timers.
    - Initialize more members in headers.
    - Put some members near related members.
    - Add FormFillTimerData which is more readable than a std::pair.
    
    Change-Id: Ifc2ad1004abe3d83b66b68f678ef7ebb04098c3c
    Reviewed-on: https://chromium-review.googlesource.com/727409
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: dsinclair <dsinclair@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510227}

--- pdf/pdf_engine.h
+++ pdf/pdf_engine.h
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/strings/string16.h"
-
+#include "base/time/time.h"
 #include "ppapi/c/dev/pp_cursor_type_dev.h"
 #include "ppapi/c/dev/ppp_printing_dev.h"
 #include "ppapi/c/ppb_input_event.h"
@@ -154,12 +154,11 @@ class PDFEngine {
     // Creates and returns new URL loader for partial document requests.
     virtual pp::URLLoader CreateURLLoader() = 0;
 
-    // Calls the client's OnCallback() function in |delay_in_ms| with the given
-    // |id|.
-    virtual void ScheduleCallback(int id, int delay_in_ms) = 0;
-    // Calls the client's OnTouchTimerCallback() function in |delay_in_ms| with
-    // the given |id|.
-    virtual void ScheduleTouchTimerCallback(int id, int delay_in_ms) = 0;
+    // Calls the client's OnCallback() function in |delay| with the given |id|.
+    virtual void ScheduleCallback(int id, base::TimeDelta delay) = 0;
+    // Calls the client's OnTouchTimerCallback() function in |delay| with the
+    // given |id|.
+    virtual void ScheduleTouchTimerCallback(int id, base::TimeDelta delay) = 0;
 
     // Searches the given string for "term" and returns the results.  Unicode-
     // aware.
