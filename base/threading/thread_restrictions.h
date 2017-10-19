07d8067141b0fbfb652f3e26093e06e2d7ba396c
Francois Doray <fdoray@chromium.org>
2017-10-19 19:26:34 +0000

    Add ScopedAllowBaseSyncPrimitives in MultiThreadedCertVerifier.
    Bug: 766678
    Change-Id: Id38d07cc60a5a97f2e60903449050032ea0a6cb8
    Reviewed-on: https://chromium-review.googlesource.com/696124
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510157}

--- base/threading/thread_restrictions.h
+++ base/threading/thread_restrictions.h
@@ -78,6 +78,7 @@ class CommandBufferLocal;
 class GpuState;
 }
 namespace net {
+class MultiThreadedCertVerifierScopedAllowBaseSyncPrimitives;
 class NetworkChangeNotifierMac;
 class OCSPScopedAllowBaseSyncPrimitives;
 namespace internal {
@@ -218,6 +219,7 @@ class BASE_EXPORT ScopedAllowBaseSyncPrimitives {
                            ScopedAllowBaseSyncPrimitivesWithBlockingDisallowed);
   friend class leveldb::LevelDBMojoProxy;
   friend class media::BlockingUrlProtocol;
+  friend class net::MultiThreadedCertVerifierScopedAllowBaseSyncPrimitives;
   friend class net::OCSPScopedAllowBaseSyncPrimitives;
 
   ScopedAllowBaseSyncPrimitives() EMPTY_BODY_IF_DCHECK_IS_OFF;
