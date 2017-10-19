c0efbfea42e4d0a4c8a3bcf5ab11822011423302
Peter E Conn <peconn@chromium.org>
2017-10-19 13:28:54 +0000

    📺 Reparent Tab after FullscreenActivity has started.
    In the current ChromeTabbedActivity to FullscreenActivity transition we
    detach the Tab from the CTA then launch the FA which reattaches the Tab.
    Since Activity creation takes time, the user gets a glimpse of a second
    Tab before the FA opens with the fullscreen Tab.
    
    Here we change the flow so the Tab is only detached and reattached once
    the FA has started.
    
    Bug: 757878, 758116
    Change-Id: I8cd4afe80ff053ae2bf0bc89454a286429330440
    Reviewed-on: https://chromium-review.googlesource.com/632686
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510071}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabReparentingParams.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabReparentingParams.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.tabmodel;
 
 import android.content.ComponentName;
 import android.content.Intent;
+import android.support.annotation.Nullable;
 
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.content_public.browser.LoadUrlParams;
@@ -61,10 +62,10 @@ public class TabReparentingParams implements AsyncTabParams {
     }
 
     /**
-     * Carry out any remaining finalization to be done after the tab is reparented.
+     * Returns the callback to be used once Tab reparenting has finished, if any.
      */
-    public void finalizeTabReparenting() {
-        if (mFinalizeCallback != null) mFinalizeCallback.run();
+    public @Nullable Runnable getFinalizeCallback() {
+        return mFinalizeCallback;
     }
 
     @Override
