86ad41068e3b92b90b63b7b8847095376ac943ce
Marti Wong <martiw@chromium.org>
2017-10-19 02:21:01 +0000

    Skip bottom toolbar button actions when context menu is showing.
    In iOS Bookmarks, Reading List and History, skip bottom toolbar button
    actions when context menu is showing.  This is to prevent the bug that
    simultaneously tapping on toolbar button and table row triggers both
    actions.
    
    Bug: 773646
    Change-Id: Iab6d3fe30642366443f88679e547a6dfa172adce
    Reviewed-on: https://chromium-review.googlesource.com/727480
    Commit-Queue: Marti Wong <martiw@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509975}

--- ios/chrome/browser/ui/reading_list/reading_list_view_controller.mm
+++ ios/chrome/browser/ui/reading_list/reading_list_view_controller.mm
@@ -103,6 +103,10 @@ typedef NS_ENUM(NSInteger, LayoutPriority) {
 }
 
 - (void)enterEditingModePressed {
+  // Ignore the button tap if view controller presenting.
+  if ([self presentedViewController]) {
+    return;
+  }
   [self.readingListCollectionViewController enterEditingModePressed];
 }
 
