aca8207c703fda30188fb23b2171d17be431d876
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2017-10-19 07:52:21 +0000

    WebCORS: Use WebString() directly instead of converting from const char*.
    When we want to return an empty WebString, just explicitly return a
    WebString() instead of going through the
    
        const char* -> WTF::String::FromUTF8 -> WTF::String -> WebString
    
    path to achieve the same thing.
    
    Bug: 736308
    Change-Id: If3c9a45b3cac140a1a2d9d48c2b68558d09f106a
    Reviewed-on: https://chromium-review.googlesource.com/726085
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Raphael Kubo da Costa (rakuco) <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/master@{#510028}

--- third_party/WebKit/Source/platform/exported/WebCORS.cpp
+++ third_party/WebKit/Source/platform/exported/WebCORS.cpp
@@ -393,7 +393,7 @@ WebString AccessControlErrorString(
   switch (status) {
     case AccessStatus::kAccessAllowed:
       NOTREACHED();
-      return "";
+      return WebString();
     case AccessStatus::kInvalidResponse:
       return String::Format("Invalid response. %s",
                             origin_denied.Utf8().data());
@@ -470,7 +470,7 @@ WebString AccessControlErrorString(
                : ""));
   }
   NOTREACHED();
-  return "";
+  return WebString();
 }
 
 WebString PreflightErrorString(const PreflightStatus status,
@@ -479,7 +479,7 @@ WebString PreflightErrorString(const PreflightStatus status,
   switch (status) {
     case PreflightStatus::kPreflightSuccess:
       NOTREACHED();
-      return "";
+      return WebString();
     case PreflightStatus::kPreflightInvalidStatus:
       return String::Format(
           "Response for preflight has invalid HTTP status code %d",
@@ -501,7 +501,7 @@ WebString PreflightErrorString(const PreflightStatus status,
               .data());
   }
   NOTREACHED();
-  return "";
+  return WebString();
 }
 
 WebString RedirectErrorString(const RedirectStatus status,
@@ -509,7 +509,7 @@ WebString RedirectErrorString(const RedirectStatus status,
   switch (status) {
     case RedirectStatus::kRedirectSuccess:
       NOTREACHED();
-      return "";
+      return WebString();
     case RedirectStatus::kRedirectDisallowedScheme:
       return String::Format(
           "Redirect location '%s' has a disallowed scheme for cross-origin "
@@ -522,7 +522,7 @@ WebString RedirectErrorString(const RedirectStatus status,
           redirect_url.GetString().Utf8().data());
   }
   NOTREACHED();
-  return "";
+  return WebString();
 }
 
 void ExtractCorsExposedHeaderNamesList(const WebURLResponse& response,
