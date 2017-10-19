c0191066748a99f03a5d1abf84a0f656362f26bc
Daniel Vogelheim <vogelheim@chromium.org>
2017-10-19 14:35:58 +0000

    Add WorkStealingInScriptRunner feature flag.
    This mirrors the existing Blink feature flag, in order to support runtime
    experiments with this feature.
    
    Bug: 557466
    Change-Id: Ifa74a5ac15b35474bda14abca9aea40755f48c5c
    Reviewed-on: https://chromium-review.googlesource.com/642966
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510081}

--- third_party/WebKit/Source/platform/exported/WebRuntimeFeatures.cpp
+++ third_party/WebKit/Source/platform/exported/WebRuntimeFeatures.cpp
@@ -471,4 +471,8 @@ void WebRuntimeFeatures::EnableAutomationControlled(bool enable) {
   RuntimeEnabledFeatures::SetAutomationControlledEnabled(enable);
 }
 
+void WebRuntimeFeatures::EnableWorkStealingInScriptRunner(bool enable) {
+  RuntimeEnabledFeatures::SetWorkStealingInScriptRunnerEnabled(enable);
+}
+
 }  // namespace blink
