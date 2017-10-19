2ed7281e3e0c51b61c0cc5b4c7cfbeaf20840ccd
Fabio Tirelo <ftirelo@chromium.org>
2017-10-19 13:23:31 +0000

    Chrome Cleaner UI: Add new reboot dialog prompt
    This should be called when a cleanup ends in a reboot required state
    and the Settings page is not the current active tab.
    
    Bug: 770749
    Change-Id: I3e81cc52e4973b767ce375647331dc51ae925d69
    Reviewed-on: https://chromium-review.googlesource.com/695754
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Chris Sharp <csharp@chromium.org>
    Reviewed-by: Ali Tofigh <alito@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510070}

--- chrome/browser/safe_browsing/chrome_cleaner/chrome_cleaner_controller_impl_win_unittest.cc
+++ chrome/browser/safe_browsing/chrome_cleaner/chrome_cleaner_controller_impl_win_unittest.cc
@@ -140,6 +140,10 @@ class ChromeCleanerControllerSimpleTest
     FAIL();
   }
 
+  void StartRebootPromptFlow(ChromeCleanerController* controller) override {
+    FAIL();
+  }
+
   // ChromeCleanerRunnerTestDelegate overrides.
 
   base::Process LaunchTestProcess(
@@ -291,6 +295,10 @@ class ChromeCleanerControllerTest
     std::move(continuation).Run();
   }
 
+  void StartRebootPromptFlow(ChromeCleanerController* controller) override {
+    reboot_flow_started_ = true;
+  }
+
   // ChromeCleanerRunnerTestDelegate overrides.
 
   base::Process LaunchTestProcess(
@@ -326,8 +334,7 @@ class ChromeCleanerControllerTest
     if (process_status_ == CleanerProcessStatus::kFetchSuccessValidProcess &&
         crash_point_ == CrashPoint::kNone &&
         uws_found_status_ == UwsFoundStatus::kUwsFoundRebootRequired &&
-        (user_response_ == UserResponse::kAcceptedWithLogs ||
-         user_response_ == UserResponse::kAcceptedWithoutLogs)) {
+        ExpectedPromptAccepted()) {
       return State::kRebootRequired;
     }
     return State::kIdle;
@@ -345,8 +352,7 @@ class ChromeCleanerControllerTest
   bool ExpectedOnCleaningCalled() {
     return ExpectedOnInfectedCalled() &&
            crash_point_ != CrashPoint::kAfterRequestSent &&
-           (user_response_ == UserResponse::kAcceptedWithLogs ||
-            user_response_ == UserResponse::kAcceptedWithoutLogs);
+           ExpectedPromptAccepted();
   }
 
   bool ExpectedOnRebootRequiredCalled() {
@@ -355,22 +361,25 @@ class ChromeCleanerControllerTest
 
   bool ExpectedUwsFound() { return ExpectedOnInfectedCalled(); }
 
+  bool ExpectedPromptAccepted() {
+    return user_response_ == UserResponse::kAcceptedWithLogs ||
+           user_response_ == UserResponse::kAcceptedWithoutLogs;
+  }
+
   bool ExpectedToTagProfile() {
     return process_status_ == CleanerProcessStatus::kFetchSuccessValidProcess &&
            (crash_point_ == CrashPoint::kNone ||
             crash_point_ == CrashPoint::kAfterResponseReceived) &&
            (uws_found_status_ == UwsFoundStatus::kUwsFoundNoRebootRequired ||
             uws_found_status_ == UwsFoundStatus::kUwsFoundRebootRequired) &&
-           (user_response_ == UserResponse::kAcceptedWithLogs ||
-            user_response_ == UserResponse::kAcceptedWithoutLogs);
+           ExpectedPromptAccepted();
   }
 
   bool ExpectedToResetSettings() {
     return process_status_ == CleanerProcessStatus::kFetchSuccessValidProcess &&
            crash_point_ == CrashPoint::kNone &&
            uws_found_status_ == UwsFoundStatus::kUwsFoundNoRebootRequired &&
-           (user_response_ == UserResponse::kAcceptedWithLogs ||
-            user_response_ == UserResponse::kAcceptedWithoutLogs);
+           ExpectedPromptAccepted();
   }
 
   ChromeCleanerController::IdleReason ExpectedIdleReason() {
@@ -391,14 +400,19 @@ class ChromeCleanerControllerTest
       return IdleReason::kUserDeclinedCleanup;
     }
 
-    if (ExpectedOnInfectedCalled() &&
-        (user_response_ == UserResponse::kAcceptedWithLogs ||
-         user_response_ == UserResponse::kAcceptedWithoutLogs) &&
+    if (ExpectedOnInfectedCalled() && ExpectedPromptAccepted() &&
         crash_point_ == CrashPoint::kAfterResponseReceived) {
       return IdleReason::kCleaningFailed;
     }
 
     return IdleReason::kCleaningSucceeded;
+  }
+
+  bool ExpectedRebootFlowStarted() {
+    return process_status_ == CleanerProcessStatus::kFetchSuccessValidProcess &&
+           crash_point_ == CrashPoint::kNone &&
+           uws_found_status_ == UwsFoundStatus::kUwsFoundRebootRequired &&
+           ExpectedPromptAccepted();
   }
 
  protected:
@@ -421,6 +435,8 @@ class ChromeCleanerControllerTest
 
   std::vector<Profile*> profiles_tagged_;
   std::vector<Profile*> profiles_to_reset_if_tagged_;
+
+  bool reboot_flow_started_ = false;
 };
 
 MULTIPROCESS_TEST_MAIN(MockChromeCleanerProcessMain) {
@@ -522,6 +538,8 @@ TEST_P(ChromeCleanerControllerTest, WithMockCleanerProcess) {
       !files_to_delete_on_cleaning.empty()) {
     EXPECT_EQ(files_to_delete_on_infected, files_to_delete_on_cleaning);
   }
+
+  EXPECT_EQ(ExpectedRebootFlowStarted(), reboot_flow_started_);
 
   std::vector<Profile*> expected_tagged;
   if (ExpectedToTagProfile())
