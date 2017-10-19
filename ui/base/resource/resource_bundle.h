689ecaaa3c658bb1fe1774a31063ef54ff55b96c
Trent Apted <tapted@chromium.org>
2017-10-19 06:13:39 +0000

    Revert "Refer to ui::ResourceBundle with namespace."
    This reverts commit d7b19dd0be5d3c5538dfa71c9108139ea21ce00a.
    
    Reason for revert: widespread compile failures, e.g.
    
    ../../ui/base/resource/resource_bundle_ios.mm:93:10: error: use of undeclared identifier 'sequence_checker_'
      DCHECK(sequence_checker_.CalledOnValidSequence());
    
    Original change's description:
    > Refer to ui::ResourceBundle with namespace.
    > 
    > ResourceBundle became ui::ResourceBundle in r72038 but many callers
    > still refer to it without the namespace.
    > 
    > TBR=jamescook@chromium.org,treib@chromium.org
    > 
    > Change-Id: Ic0b7116beabc945ea4ac27cf4d65dc177274cc85
    > Reviewed-on: https://chromium-review.googlesource.com/696139
    > Commit-Queue: Lei Zhang <thestig@chromium.org>
    > Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#510005}
    
    TBR=jamescook@chromium.org,sadrul@chromium.org,ben@chromium.org,thestig@chromium.org,treib@chromium.org
    
    Change-Id: Ib4428ba90efe811331e8190c9d22608d94d51cd6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/727442
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510009}

--- ui/base/resource/resource_bundle.h
+++ ui/base/resource/resource_bundle.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequence_checker.h"
 #include "base/strings/string16.h"
 #include "base/strings/string_piece.h"
+#include "build/build_config.h"
 #include "ui/base/layout.h"
 #include "ui/base/ui_base_export.h"
 #include "ui/gfx/font_list.h"
@@ -309,7 +310,7 @@ class UI_BASE_EXPORT ResourceBundle {
 
   struct FontKey;
 
-  using IdToStringMap = base::hash_map<int, base::string16>;
+  typedef base::hash_map<int, base::string16> IdToStringMap;
 
   // Ctor/dtor are private, since we're a singleton.
   explicit ResourceBundle(Delegate* delegate);
@@ -412,7 +413,7 @@ class UI_BASE_EXPORT ResourceBundle {
 
   // Cached images. The ResourceBundle caches all retrieved images and keeps
   // ownership of the pointers.
-  using ImageMap = std::map<int, gfx::Image>;
+  typedef std::map<int, gfx::Image> ImageMap;
   ImageMap images_;
 
   gfx::Image empty_image_;
@@ -429,11 +430,14 @@ class UI_BASE_EXPORT ResourceBundle {
 
   bool is_test_resources_ = false;
 
-  SEQUENCE_CHECKER(sequence_checker_);
+  base::SequenceChecker sequence_checker_;
 
   DISALLOW_COPY_AND_ASSIGN(ResourceBundle);
 };
 
 }  // namespace ui
+
+// TODO(beng): Someday, maybe, get rid of this.
+using ui::ResourceBundle;
 
 #endif  // UI_BASE_RESOURCE_RESOURCE_BUNDLE_H_
