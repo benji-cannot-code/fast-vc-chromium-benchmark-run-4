e13f481a44415e4f4a3257a258007f91f0489b27
Tatiana Gornak <melandory@chromium.org>
2017-10-19 08:27:13 +0000

    Record metrics when "Show all saved passwords" is triggered from Context Menu.
    BUG=739343, 775825
    
    Change-Id: Ib0f997a0228fcf02b117d769644a31cd4b3352d0
    Reviewed-on: https://chromium-review.googlesource.com/725342
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Tatiana Gornak <melandory@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510036}

--- components/password_manager/core/browser/password_manager_util.h
+++ components/password_manager/core/browser/password_manager_util.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 struct PasswordForm;
+class AutofillClient;
 }
 
 namespace syncer {
@@ -58,6 +59,15 @@ uint64_t CalculateSyncPasswordHash(const base::StringPiece16& text,
 // True iff the manual password generation is enabled and the user is sync user
 // without custom passphrase.
 bool ManualPasswordGenerationEnabled(syncer::SyncService* sync_service);
+
+// Returns true iff the "Show all saved passwords" option should be shown in
+// Context Menu. Also records metric, that the Context Menu will have "Show all
+// saved passwords" option.
+bool ShowAllSavedPasswordsContextMenuEnabled();
+
+// Opens Password Manager setting page and records the metrics.
+void UserTriggeredShowAllSavedPasswordsFromContextMenu(
+    autofill::AutofillClient* autofill_client);
 
 }  // namespace password_manager_util
 
