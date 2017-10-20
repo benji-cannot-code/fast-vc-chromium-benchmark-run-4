057bf541bb1e77ff1c2f5db722bccd0605f5d2eb
Hajime Hoshi <hajimehoshi@chromium.org>
2017-10-20 07:33:53 +0000

    scheduler: Refactoring: Add WebFrameScheduler::GetTaskRunner
    This CL moves the implemenration of TaskRunnerHelper::Get to
    WebFrameScheduler::GetTaskRunner so that selecting a task runner with
    task type can be done inside schedulers.
    
    This is a part of adding-task-type work (https://docs.google.com/document/d/1Py2ZdjpaCMdpVtKfdHMITAn5gst_owjQiqlbPm3mCxc/edit?ts=59d49142#)
    
    Bug: 762453
    Change-Id: I7e4137eabd87ff152b488410d199e4e050208064
    Reviewed-on: https://chromium-review.googlesource.com/727862
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510367}

--- third_party/WebKit/Source/platform/loader/testing/MockFetchContext.h
+++ third_party/WebKit/Source/platform/loader/testing/MockFetchContext.h
@@ -93,7 +93,7 @@ class MockFetchContext : public FetchContext {
   }
 
   RefPtr<WebTaskRunner> GetLoadingTaskRunner() override {
-    return frame_scheduler_->LoadingTaskRunner();
+    return frame_scheduler_->GetTaskRunner(TaskType::kUnspecedLoading);
   }
 
  private:
@@ -101,14 +101,7 @@ class MockFetchContext : public FetchContext {
    public:
     MockFrameScheduler(RefPtr<WebTaskRunner> runner)
         : runner_(std::move(runner)) {}
-    RefPtr<WebTaskRunner> LoadingTaskRunner() override { return runner_; }
-    RefPtr<WebTaskRunner> LoadingControlTaskRunner() override {
-      return runner_;
-    }
-    RefPtr<WebTaskRunner> ThrottleableTaskRunner() override { return runner_; }
-    RefPtr<WebTaskRunner> DeferrableTaskRunner() override { return runner_; }
-    RefPtr<WebTaskRunner> PausableTaskRunner() override { return runner_; }
-    RefPtr<WebTaskRunner> UnpausableTaskRunner() override { return runner_; }
+    RefPtr<WebTaskRunner> GetTaskRunner(TaskType) override { return runner_; }
 
    private:
     RefPtr<WebTaskRunner> runner_;
