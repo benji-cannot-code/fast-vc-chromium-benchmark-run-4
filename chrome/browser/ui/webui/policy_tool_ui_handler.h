e4d5c50b5c7fd2030754fdb74066c664edfe839d
Georges Khalil <georgesak@chromium.org>
2017-10-20 14:21:32 +0000

    Added delete session button
    Note that this is a mirror of 700809, which cannot be landed as the owner is not longer active.
    
    TBR=palmer@chromium.org,pastarmovj@chromium.org
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Bug: 
    Change-Id: I44e5164fd0929def5fa8cb863245bcb21314ab39
    Reviewed-on: https://chromium-review.googlesource.com/723563
    Commit-Queue: Georges Khalil <georgesak@chromium.org>
    Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    Reviewed-by: Georges Khalil <georgesak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510427}

--- chrome/browser/ui/webui/policy_tool_ui_handler.h
+++ chrome/browser/ui/webui/policy_tool_ui_handler.h
@@ -32,6 +32,9 @@ class PolicyToolUIHandler : public PolicyUIHandler {
   void HandleLoadSession(const base::ListValue* args);
   void HandleUpdateSession(const base::ListValue* args);
   void HandleResetSession(const base::ListValue* args);
+  void HandleDeleteSession(const base::ListValue* args);
+
+  void OnSessionDeleted(bool is_successful);
 
   std::string ReadOrCreateFileCallback();
   void OnFileRead(const std::string& contents);
