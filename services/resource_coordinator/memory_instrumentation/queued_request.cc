83200b555945b56a7bea941181077a2f56a751a5
Lalit Maganti <lalitm@chromium.org>
2017-10-20 17:18:34 +0000

    memory-infra: split CoordinatorImpl into two classes
    CoordinatorImpl was becoming an unwieldy class with a lot of
    functionality tied up into a single file and risked becoming a god
    class. In particular, it performed the following functions:
    1. Queued requests and handled threading hops to do with maintaining the
    queue etc.
    2. Dispatched requests to process-local MDMs and then collated the
    responses once everything responded/failed.
    3. Listened to Mojo callbacks to track which clients have
    registered/unregistered.
    
    By examining the code, one sees that number 2 is in some ways orthogonal
    to the work done for 1 and 3. In particular, big chunks of code deal
    with only a single request and how it should be dispatched or how the
    responses should be collated and returned.
    
    This CL splits the functions described by 2 into a new class. It also
    extracts the concept of a queued request into a namespaced class to
    allow for sharing between CoordinatorImpl and the new class.
    
    This vastly reduces the apparent complexity of CoordinatorImpl and
    solidfies its purpose as a class handling the queueing of requests with
    the new class handling single requests.
    
    Bug: 775039
    Change-Id: I313695782ae73619a15b4acad40f657177467c3d
    Reviewed-on: https://chromium-review.googlesource.com/728060
    Reviewed-by: Hector Dearman <hjd@chromium.org>
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Commit-Queue: Lalit Maganti <lalitm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510474}

---
+++ services/resource_coordinator/memory_instrumentation/queued_request.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/resource_coordinator/memory_instrumentation/queued_request.h"
+
+namespace memory_instrumentation {
+
+QueuedRequest::Response::Response() {}
+QueuedRequest::Response::~Response() {}
+
+QueuedRequest::QueuedRequest(
+    const base::trace_event::MemoryDumpRequestArgs& args,
+    const RequestGlobalMemoryDumpInternalCallback& callback,
+    bool add_to_trace)
+    : args(args), callback(callback), add_to_trace(add_to_trace) {}
+
+QueuedRequest::~QueuedRequest() {}
+
+QueuedRequest::PendingResponse::PendingResponse(
+    const mojom::ClientProcess* client,
+    Type type)
+    : client(client), type(type) {}
+
+bool QueuedRequest::PendingResponse::operator<(
+    const PendingResponse& other) const {
+  return std::tie(client, type) < std::tie(other.client, other.type);
+}
+
+}  // namespace memory_instrumentation
