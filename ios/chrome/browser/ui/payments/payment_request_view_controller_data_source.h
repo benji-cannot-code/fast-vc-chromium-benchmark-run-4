501f9034ff968a02fae12943ab532e23bee69418
Mohamad Ahmadi <mahmadi@chromium.org>
2017-10-20 20:47:51 +0000

    [PR] No shipping section header if there's no selected shipping address
    Bug: 776439
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I2de6460213dfeda39a2040d2039c05025067cd21
    Reviewed-on: https://chromium-review.googlesource.com/728715
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Marc-Antoine Courteau <macourteau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510553}

--- ios/chrome/browser/ui/payments/payment_request_view_controller_data_source.h
+++ ios/chrome/browser/ui/payments/payment_request_view_controller_data_source.h
@@ -19,10 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // be enabled.
 - (BOOL)canPay;
 
-// Returns whether shipment can be done and therefore the shipping options
-// should be presented.
-- (BOOL)canShip;
-
 // Returns whether the total price is itemized.
 - (BOOL)hasPaymentItems;
 
