5b125d2196e654252b3b02a8f96e74815608f87f
Joel Einbinder <einbinder@chromium.org>
2017-10-20 02:45:28 +0000

    DevTools: Don't focus Accessibility Sidebar on wasShown
    The accessibility sidebar was stealing focus whenever it was shown, and
    not responding to widget.focus().
    
    Bug: none
    Change-Id: I244c1fc9094e53332c44c3f91b1764148d16f693
    Reviewed-on: https://chromium-review.googlesource.com/703476
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510306}

--- third_party/WebKit/Source/devtools/front_end/accessibility/AXBreadcrumbsPane.js
+++ third_party/WebKit/Source/devtools/front_end/accessibility/AXBreadcrumbsPane.js
@@ -12,6 +12,7 @@ Accessibility.AXBreadcrumbsPane = class extends Accessibility.AccessibilitySubPa
 
     this.element.classList.add('ax-subpane');
     UI.ARIAUtils.markAsTree(this.element);
+    this.element.tabIndex = -1;
 
     this._axSidebarView = axSidebarView;
 
@@ -19,8 +20,6 @@ Accessibility.AXBreadcrumbsPane = class extends Accessibility.AccessibilitySubPa
     this._preselectedBreadcrumb = null;
     /** @type {?Accessibility.AXBreadcrumb} */
     this._inspectedNodeBreadcrumb = null;
-
-    this._selectedByUser = true;
 
     this._hoveredBreadcrumb = null;
     this._rootElement = this.element.createChild('div', 'ax-breadcrumbs');
@@ -35,18 +34,27 @@ Accessibility.AXBreadcrumbsPane = class extends Accessibility.AccessibilitySubPa
   }
 
   /**
+   * @override
+   */
+  focus() {
+    if (this._inspectedNodeBreadcrumb)
+      this._inspectedNodeBreadcrumb.nodeElement().focus();
+    else
+      this.element.focus();
+  }
+
+  /**
    * @param {?Accessibility.AccessibilityNode} axNode
    * @override
    */
   setAXNode(axNode) {
+    var hadFocus = this.element.hasFocus();
     super.setAXNode(axNode);
 
     this._rootElement.removeChildren();
 
-    if (!axNode) {
-      this._selectedByUser = false;
+    if (!axNode)
       return;
-    }
 
     var ancestorChain = [];
     var ancestor = axNode;
@@ -70,7 +78,7 @@ Accessibility.AXBreadcrumbsPane = class extends Accessibility.AccessibilitySubPa
     }
 
     this._inspectedNodeBreadcrumb = breadcrumb;
-    this._inspectedNodeBreadcrumb.setPreselected(true, this._selectedByUser);
+    this._inspectedNodeBreadcrumb.setPreselected(true, hadFocus);
 
     this._setPreselectedBreadcrumb(this._inspectedNodeBreadcrumb);
 
@@ -90,15 +98,6 @@ Accessibility.AXBreadcrumbsPane = class extends Accessibility.AccessibilitySubPa
 
     for (var child of axNode.children())
       append(this._inspectedNodeBreadcrumb, child, depth);
-
-    this._selectedByUser = false;
-  }
-
-  /**
-   * @override
-   */
-  wasShown() {
-    this._selectedByUser = true;
   }
 
   /**
@@ -138,7 +137,6 @@ Accessibility.AXBreadcrumbsPane = class extends Accessibility.AccessibilitySubPa
     var previousBreadcrumb = this._preselectedBreadcrumb.previousBreadcrumb();
     if (!previousBreadcrumb)
       return false;
-    this._selectedByUser = true;
     this._setPreselectedBreadcrumb(previousBreadcrumb);
     return true;
   }
@@ -150,7 +148,6 @@ Accessibility.AXBreadcrumbsPane = class extends Accessibility.AccessibilitySubPa
     var nextBreadcrumb = this._preselectedBreadcrumb.nextBreadcrumb();
     if (!nextBreadcrumb)
       return false;
-    this._selectedByUser = true;
     this._setPreselectedBreadcrumb(nextBreadcrumb);
     return true;
   }
@@ -161,14 +158,16 @@ Accessibility.AXBreadcrumbsPane = class extends Accessibility.AccessibilitySubPa
   _setPreselectedBreadcrumb(breadcrumb) {
     if (breadcrumb === this._preselectedBreadcrumb)
       return;
+    var hadFocus = this.element.hasFocus();
     if (this._preselectedBreadcrumb)
-      this._preselectedBreadcrumb.setPreselected(false, this._selectedByUser);
+      this._preselectedBreadcrumb.setPreselected(false, hadFocus);
+
     if (breadcrumb)
       this._preselectedBreadcrumb = breadcrumb;
     else
       this._preselectedBreadcrumb = this._inspectedNodeBreadcrumb;
-    this._preselectedBreadcrumb.setPreselected(true, this._selectedByUser);
-    if (!breadcrumb && this._selectedByUser)
+    this._preselectedBreadcrumb.setPreselected(true, hadFocus);
+    if (!breadcrumb && hadFocus)
       SDK.OverlayModel.hideDOMNodeHighlight();
   }
 
@@ -251,8 +250,6 @@ Accessibility.AXBreadcrumbsPane = class extends Accessibility.AccessibilitySubPa
   _inspectDOMNode(axNode) {
     if (!axNode.isDOMNode())
       return false;
-
-    this._selectedByUser = true;
 
     axNode.deferredDOMNode().resolve(domNode => {
       this._axSidebarView.setNode(domNode, true /* fromAXTree */);
