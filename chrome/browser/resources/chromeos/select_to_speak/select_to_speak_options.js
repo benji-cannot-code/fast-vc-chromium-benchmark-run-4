7d54c7b4f84fa086f9c5ecfb8fed9e94b1dc0242
Katie Dektar <katydek@google.com>
2017-10-20 00:07:16 +0000

    Per-word highlighting option in Select-to-Speak
    Adds a setting to allow per-word highlighting instead
    of node-based highlighting. This works well in nearly all
    cases but there are two tts bugs identified and filed to the
    tts team, one regarding lots of quotes, and the other with
    phrases that start with a single letter word.
    
    Removes empty nodes from paragraphs so that these will
    not be highlighted at all.
    
    Bug: 676451
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I2d08e0cd8ed786e894f65ecf5c664a784cf12f01
    Reviewed-on: https://chromium-review.googlesource.com/724361
    Commit-Queue: Katie Dektar <katydek@google.com>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510259}

--- chrome/browser/resources/chromeos/select_to_speak/select_to_speak_options.js
+++ chrome/browser/resources/chromeos/select_to_speak/select_to_speak_options.js
@@ -23,6 +23,7 @@ SelectToSpeakOptionsPage.prototype = {
     }.bind(this));
     this.syncSelectControlToPref_('voice', 'voice');
     this.syncSelectControlToPref_('rate', 'rate');
+    this.syncCheckboxControlToPref_('wordHighlight', 'wordHighlight');
   },
 
   /**
@@ -81,6 +82,34 @@ SelectToSpeakOptionsPage.prototype = {
         select.updateFunction();
       }
     });
+  },
+
+  /**
+   * Populate a checkbox with its current setting.
+   * @param {string} checkboxId The id of the checkbox element.
+   * @private
+   */
+  syncCheckboxControlToPref_: function(checkboxId, pref) {
+    let checkbox = document.getElementById(checkboxId);
+
+    function updateFromPref() {
+      chrome.storage.sync.get(pref, function(items) {
+        let value = items[pref];
+        if (value != null) {
+          checkbox.checked = value;
+        }
+      });
+    }
+
+    checkbox.addEventListener('change', function() {
+      let setParams = {};
+      setParams[pref] = checkbox.checked;
+      chrome.storage.sync.set(setParams);
+    });
+
+    checkbox.updateFunction = updateFromPref;
+    updateFromPref();
+    chrome.storage.onChanged.addListener(updateFromPref);
   },
 
   /**
