fb1b3c2d79bc5800effd994acf888e224a53de9f
Theresa Wellington <twellington@google.com>
2017-10-20 21:26:33 +0000

    [Bookmarks] Return early from some methods if destroyed
    If the BookmarkManager or SelectableListToolbar have already been
    destroyed, return early from SelectableListToolbar#onClick() and
    HistroyManager#onBackPress. This is a speculative fix for a low-volume
    crash.
    
    BUG=747455
    
    Change-Id: I9e7c3bed1ded1d01f9877efaa8976a2ceb44b6a4
    Reviewed-on: https://chromium-review.googlesource.com/731232
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510566}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/selection/SelectableListToolbar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/selection/SelectableListToolbar.java
@@ -312,6 +312,8 @@ public class SelectableListToolbar<E> extends Toolbar implements SelectionObserv
 
     @Override
     public void onClick(View view) {
+        if (mIsDestroyed) return;
+
         switch (mNavigationButton) {
             case NAVIGATION_BUTTON_NONE:
                 break;
