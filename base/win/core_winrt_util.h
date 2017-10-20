9378a1bf2587e2bd254db945096920dd8d663466
Bruce Dawson <brucedawson@chromium.org>
2017-10-20 23:46:28 +0000

    Add missing #include
    core_winrt_util.h references ScopedHString without a definition of it
    necessarily being in scope. This slipped by because the reference is in
    a template. However when compiling with /permissive- this error is
    caught. The fix is to add the necessary #include.
    
    With this fix (and the Fall Creators Update SDK) I can build base
    cleanly with /permissive-.
    
    The error message was:
    
        c:\src\chromium3\src\base\win\core_winrt_util.h(48):
            error C2760: syntax error: expected ';' not 'identifier'
    
    Change-Id: Iac5729b617c8915e1a52eef3fa2c282c00bf90bc
    Reviewed-on: https://chromium-review.googlesource.com/731379
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510614}

--- base/win/core_winrt_util.h
+++ base/win/core_winrt_util.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base_export.h"
 #include "base/strings/string16.h"
+#include "base/win/scoped_hstring.h"
 
 namespace base {
 namespace win {
