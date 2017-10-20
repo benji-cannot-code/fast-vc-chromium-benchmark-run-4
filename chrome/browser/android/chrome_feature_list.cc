2f7c64e6f6ae9963e93affd2b2d85457502a1b96
Theresa Wellington <twellington@google.com>
2017-10-20 15:08:36 +0000

    [Home] Remove Doodle flag
    The Doodle is always enabled now, so remove the flag. Also remove
    support for showing the Google 'G' when Chrome Home is enabled and clear
    out variables in LocationBarPhone.java to save browser heap memory
    (~28kb on Nexus 6P).
    
    BUG=708006,735556
    
    Change-Id: Ibdab28c4495e01a65bb2f2e4617bbcc8c4309e56
    Reviewed-on: https://chromium-review.googlesource.com/728363
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510437}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -64,7 +64,6 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kChromeHomeFeature,
     &kChromeHomeDestroySuggestions,
     &kChromeHomeBottomNavLabels,
-    &kChromeHomeDoodle,
     &kChromeHomePromo,
     &kChromeHomeOptOutSnackbar,
     &kChromeHomeSwipeLogic,
@@ -176,9 +175,6 @@ const base::Feature kChromeHomeDestroySuggestions{
 
 const base::Feature kChromeHomeBottomNavLabels{
     "ChromeHomeBottomNavLabels", base::FEATURE_DISABLED_BY_DEFAULT};
-
-const base::Feature kChromeHomeDoodle{"ChromeHomeDoodle",
-                                      base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kChromeHomePromo{"ChromeHomePromo",
                                      base::FEATURE_DISABLED_BY_DEFAULT};
