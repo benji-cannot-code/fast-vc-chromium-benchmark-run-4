8c30121f1178aa0cff41fa526739702231a4f1a6
Ella Ge <eirage@chromium.org>
2017-10-20 15:36:14 +0000

    fractional: EventHandler.HitTestResultAtPoint
    Change HitTest at Point from LayoutPoint to FloatPoint
    
    Bug: 456625
    Change-Id: I0219d379d81854caa8fe1fcedd55bc3633254800
    Reviewed-on: https://chromium-review.googlesource.com/692455
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510446}

--- third_party/WebKit/Source/core/frame/WebLocalFrameImpl.cpp
+++ third_party/WebKit/Source/core/frame/WebLocalFrameImpl.cpp
@@ -1016,7 +1016,7 @@ size_t WebLocalFrameImpl::CharacterIndexForPoint(
   if (!GetFrame())
     return kNotFound;
 
-  IntPoint point = GetFrame()->View()->ViewportToContents(point_in_viewport);
+  LayoutPoint point = GetFrame()->View()->ViewportToContents(point_in_viewport);
   HitTestResult result = GetFrame()->GetEventHandler().HitTestResultAtPoint(
       point, HitTestRequest::kReadOnly | HitTestRequest::kActive);
   const EphemeralRange range =
