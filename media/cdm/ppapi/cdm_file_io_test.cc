423ead49fc197e8a5c9d5c40019ec5db28210827
Xiaohan Wang <xhwang@chromium.org>
2017-10-20 20:30:12 +0000

    media: Update ECKEncryptedMediaTest.FileIOTest
    This is a test case in browser_tests that has multiple sub-test-cases
    running in the ClearKeyCdm in the CDM (pepper or mojo) process.
    
    This CL:
    - Reenables steps previously disabled due to crbug.com/415401
    - Adds a new sub-test-case WriteExistingFile
    
    BUG=415401,479923
    TEST=This CL adds more tests
    
    Change-Id: Ia0262c367c8880393c2f02f738fa867cd4bcbabd
    Reviewed-on: https://chromium-review.googlesource.com/730772
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510547}

--- media/cdm/ppapi/cdm_file_io_test.cc
+++ media/cdm/ppapi/cdm_file_io_test.cc
@@ -294,6 +294,38 @@ void FileIOTestRunner::AddTests() {
     EXPECT_FILE_READ(kSuccess, kData, kDataSize)
   END_TEST_CASE
 
+  START_TEST_CASE("WriteExistingFile")
+    OPEN_FILE
+    EXPECT_FILE_OPENED(kSuccess)
+    // Write big data first
+    WRITE_FILE(kBigData, kBigDataSize)
+    EXPECT_FILE_WRITTEN(kSuccess)
+    CLOSE_FILE
+    CREATE_FILE_IO
+    OPEN_FILE
+    EXPECT_FILE_OPENED(kSuccess)
+    READ_FILE
+    EXPECT_FILE_READ(kSuccess, kBigData, kBigDataSize)
+    // Now overwrite the file with normal data
+    WRITE_FILE(kData, kDataSize)
+    EXPECT_FILE_WRITTEN(kSuccess)
+    CLOSE_FILE
+    CREATE_FILE_IO
+    OPEN_FILE
+    EXPECT_FILE_OPENED(kSuccess)
+    READ_FILE
+    EXPECT_FILE_READ(kSuccess, kData, kDataSize)
+    // Now overwrite the file with big data again
+    WRITE_FILE(kBigData, kBigDataSize)
+    EXPECT_FILE_WRITTEN(kSuccess)
+    CLOSE_FILE
+    CREATE_FILE_IO
+    OPEN_FILE
+    EXPECT_FILE_OPENED(kSuccess)
+    READ_FILE
+    EXPECT_FILE_READ(kSuccess, kBigData, kBigDataSize)
+  END_TEST_CASE
+
   START_TEST_CASE("MultipleReadsAndWrites")
     OPEN_FILE
     EXPECT_FILE_OPENED(kSuccess)
@@ -365,8 +397,7 @@ void FileIOTestRunner::AddTests() {
   START_TEST_CASE("CloseDuringPendingWrite")
     OPEN_FILE
     EXPECT_FILE_OPENED(kSuccess)
-    // TODO(xhwang): Reenable this after http:://crbug.com/415401 is fixed.
-    // WRITE_FILE(kData, kDataSize)
+    WRITE_FILE(kData, kDataSize)
     CLOSE_FILE
   END_TEST_CASE
 
@@ -375,8 +406,7 @@ void FileIOTestRunner::AddTests() {
     EXPECT_FILE_OPENED(kSuccess)
     WRITE_FILE(kData, kDataSize)
     EXPECT_FILE_WRITTEN(kSuccess)
-    // TODO(xhwang): Reenable this after http:://crbug.com/415401 is fixed.
-    // WRITE_FILE(kBigData, kBigDataSize)
+    WRITE_FILE(kBigData, kBigDataSize)
     CLOSE_FILE
     // Write() didn't finish and the content of the file is not modified.
     CREATE_FILE_IO
@@ -391,8 +421,7 @@ void FileIOTestRunner::AddTests() {
     EXPECT_FILE_OPENED(kSuccess)
     WRITE_FILE(kBigData, kBigDataSize)
     EXPECT_FILE_WRITTEN(kSuccess)
-    // TODO(xhwang): Reenable this after http:://crbug.com/415401 is fixed.
-    // WRITE_FILE(kData, kDataSize)
+    WRITE_FILE(kData, kDataSize)
     CLOSE_FILE
     // Write() didn't finish and the content of the file is not modified.
     CREATE_FILE_IO
@@ -407,8 +436,7 @@ void FileIOTestRunner::AddTests() {
     EXPECT_FILE_OPENED(kSuccess)
     WRITE_FILE(kData, kDataSize)
     EXPECT_FILE_WRITTEN(kSuccess)
-    // TODO(xhwang): Reenable this after http:://crbug.com/415401 is fixed.
-    // READ_FILE
+    READ_FILE
     CLOSE_FILE
     // Make sure the file is not modified.
     CREATE_FILE_IO
@@ -425,8 +453,7 @@ void FileIOTestRunner::AddTests() {
       EXPECT_FILE_OPENED(kSuccess)
       WRITE_FILE(kData, kDataSize)
       EXPECT_FILE_WRITTEN(kSuccess)
-      // TODO(xhwang): Reenable this after http:://crbug.com/415401 is fixed.
-      // WRITE_FILE(kBigData, kBigDataSize)
+      WRITE_FILE(kBigData, kBigDataSize)
       CLOSE_FILE
       // Make sure the file is not modified.
       CREATE_FILE_IO
