41f459a26f667e01b69ba5b794b17331ccc5afb4
kylechar <kylechar@chromium.org>
2017-10-20 01:12:30 +0000

    Revert "Add confine bounds code for Ozone X11."
    This reverts commit b2d58bc10e2729b564c407eae67d718b457e2e65.
    
    Reason for revert: This change makes CrOS development too difficult.
    
    Original change's description:
    > Add confine bounds code for Ozone X11.
    > 
    > This CL implements ConfineCursorToBounds() for Ozone X11. The mouse
    > cursor is confined inside the XWindow to emulate a real Chrome OS
    > experience. Since Ozone X11 is only used for developers and moving the
    > cursor outside the window is sometimes necessary, the cursor barriers
    > intentionally leave a small gap in each corner.
    > 
    > Bug: 771212
    > Change-Id: I763c3d96041cf95a06c63ad58012a21006dd9c6d
    > Reviewed-on: https://chromium-review.googlesource.com/705115
    > Commit-Queue: kylechar <kylechar@chromium.org>
    > Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    > Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#509832}
    
    TBR=sadrul@chromium.org,oshima@chromium.org,kylechar@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 771212
    Change-Id: I2ecf7fb98e23c0337d15ce55da840dcf24a6448a
    Reviewed-on: https://chromium-review.googlesource.com/729179
    Commit-Queue: kylechar <kylechar@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510284}

--- ui/platform_window/x11/x11_window_base.h
+++ ui/platform_window/x11/x11_window_base.h
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
-#include <array>
-
 #include "base/callback.h"
 #include "base/macros.h"
 #include "ui/gfx/geometry/rect.h"
@@ -51,7 +49,6 @@ class X11_WINDOW_EXPORT X11WindowBase : public PlatformWindow {
 
  protected:
   void Destroy();
-  void UnConfineCursor();
 
   PlatformWindowDelegate* delegate() { return delegate_; }
   XDisplay* xdisplay() { return xdisplay_; }
@@ -75,10 +72,6 @@ class X11_WINDOW_EXPORT X11WindowBase : public PlatformWindow {
 
   // The bounds of |xwindow_|.
   gfx::Rect bounds_;
-
-  // Keep track of barriers to confine cursor.
-  bool has_pointer_barriers_ = false;
-  std::array<XID, 4> pointer_barriers_;
 
   bool window_mapped_ = false;
 
