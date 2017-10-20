321dd08d499a3c8557a58c542a3a08e20c85bf1f
Will Chen <chenwilliam@chromium.org>
2017-10-20 19:12:20 +0000

    DevTools: finish automatically migrating as many tests in http/tests/devtools/sources
    This converts a batch of tests in http/tests/devtools/sources from the
    old test framework (html file) into the new test framework (js file).
    
    Most of it was done through an automated transformation script, with
    a small amount of manual clean-up.
    
    The rest of the *.html tests in http/tests/devtools/sources will need manual
    migration.
    
    Bug: 667560
    Change-Id: Ia2bb66c274e49c4803857e9a8021746e2425ac75
    Reviewed-on: https://chromium-review.googlesource.com/728711
    Reviewed-by: Will Chen <chenwilliam@chromium.org>
    Reviewed-by: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510514}

---
+++ third_party/WebKit/LayoutTests/http/tests/devtools/sources/debugger-ui/copy-stack-trace.js
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function() {
+  TestRunner.addResult(`Tests that debugger will copy valid stack trace upon context menu action.\n`);
+  await TestRunner.loadModule('sources_test_runner');
+  await TestRunner.showPanel('sources');
+  await TestRunner.evaluateInPagePromise(`
+      function testFunction()
+      {
+          functionBar();
+      }
+
+      function functionBar()
+      {
+          functionBaz();
+      }
+
+      function functionBaz()
+      {
+          debugger;
+      }
+  `);
+
+  SourcesTestRunner.startDebuggerTest(step1);
+
+  function step1() {
+    SourcesTestRunner.runTestFunctionAndWaitUntilPaused(step2);
+  }
+
+  function step2() {
+    InspectorFrontendHost.copyText = text => TestRunner.addResult(TestRunner.clearSpecificInfoFromStackFrames(text));
+    self.runtime.sharedInstance(Sources.CallStackSidebarPane)._copyStackTrace();
+    SourcesTestRunner.completeDebuggerTest();
+  }
+})();
