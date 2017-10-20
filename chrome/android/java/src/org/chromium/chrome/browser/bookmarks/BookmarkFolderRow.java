e4d35629f86f2f1ab3d4e33cb091e4cfe773aea3
Theresa Wellington <twellington@google.com>
2017-10-20 21:51:15 +0000

    [Bookmarks] Fix folder icon tinting after de-selecting
    Adds a SelectableItemView#getDefaultIconTint() method that sub-classes
    can override to provide a ColorStateList for use when the item is not
    selected.
    
    BUG=776886
    
    Change-Id: I0e10cc86c196634c54d224a33fbcdc8d79266f06
    Reviewed-on: https://chromium-review.googlesource.com/731113
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510574}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkFolderRow.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkFolderRow.java
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.bookmarks;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.util.AttributeSet;
 
+import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.bookmarks.BookmarkBridge.BookmarkItem;
 import org.chromium.components.bookmarks.BookmarkId;
@@ -47,5 +49,11 @@ public class BookmarkFolderRow extends BookmarkRow {
                                   R.plurals.bookmarks_count, childCount, childCount)
                         : getResources().getString(R.string.no_bookmarks));
         return item;
+    }
+
+    @Override
+    protected ColorStateList getDefaultIconTint() {
+        return ApiCompatibilityUtils.getColorStateList(
+                getResources(), BookmarkUtils.getFolderIconTint());
     }
 }
