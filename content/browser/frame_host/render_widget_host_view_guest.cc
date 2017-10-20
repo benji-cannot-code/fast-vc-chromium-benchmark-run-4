b103ea24f1925b3e8016ad3c90b5e4b671321712
Scott Violet <sky@chromium.org>
2017-10-20 20:02:46 +0000

    Converts RenderWidgetHostViewGuest::InitAsChild to NOTREACHED
    Unless I'm missing something this function is never called.
    
    BUG=none
    TEST=none
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I511f4b3d451a983694c7a5bbc9a8d4ce492f06f5
    Reviewed-on: https://chromium-review.googlesource.com/727181
    Reviewed-by: Nick Carter <nick@chromium.org>
    Commit-Queue: Nick Carter <nick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510533}

--- content/browser/frame_host/render_widget_host_view_guest.cc
+++ content/browser/frame_host/render_widget_host_view_guest.cc
@@ -356,7 +356,8 @@ bool RenderWidgetHostViewGuest::OnMessageReceived(const IPC::Message& msg) {
 
 void RenderWidgetHostViewGuest::InitAsChild(
     gfx::NativeView parent_view) {
-  platform_view_->InitAsChild(parent_view);
+  // This should never get called.
+  NOTREACHED();
 }
 
 void RenderWidgetHostViewGuest::InitAsPopup(
