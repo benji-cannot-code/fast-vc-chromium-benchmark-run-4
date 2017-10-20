cd22d9a8835e625c3a04573e08a973e0bffb51c9
Qiang Xu <warx@chromium.org>
2017-10-20 03:01:30 +0000

    cros: remove TabletPowerButtonController::ShouldHandlePowerButtonEvents
    changes:
    ShouldHandlePowerButtonEvents() is equivalent to
    TabletModeController::CanEnterTabletMode(), which is equivalent to having
    tablet mode switch and having seen accelerometer data. We could just not
    initialize TabletPowerButtonController in the first place.
    
    Bug: 773853
    Test: test coverage by NoTabletModePowerButtonControllerTest
    Change-Id: I3d386cf3aaa4f8957fdfb9e5badce33dcef75c97
    Reviewed-on: https://chromium-review.googlesource.com/723605
    Commit-Queue: Qiang(Joe) Xu <warx@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510310}

--- ash/system/power/power_button_controller.cc
+++ ash/system/power/power_button_controller.cc
@@ -196,8 +196,7 @@ void PowerButtonController::PowerButtonEventReceived(
   }
 
   // Handle tablet power button behavior.
-  if (button_type_ == ButtonType::NORMAL && tablet_controller_ &&
-      tablet_controller_->ShouldHandlePowerButtonEvents()) {
+  if (button_type_ == ButtonType::NORMAL && tablet_controller_) {
     tablet_controller_->OnPowerButtonEvent(down, timestamp);
     return;
   }
@@ -211,6 +210,8 @@ void PowerButtonController::OnAccelerometerUpdated(
   // Tablet power button behavior (excepts |force_clamshell_power_button_|) and
   // power button screenshot accelerator are enabled on devices that can enter
   // tablet mode, which must have seen accelerometer data before user actions.
+  if (!enable_tablet_mode_)
+    return;
   if (!force_clamshell_power_button_ && !tablet_controller_) {
     tablet_controller_ = std::make_unique<TabletPowerButtonController>(
         display_controller_.get(), tick_clock_.get());
@@ -247,6 +248,7 @@ void PowerButtonController::ProcessCommandLine() {
   button_type_ = cl->HasSwitch(switches::kAuraLegacyPowerButton)
                      ? ButtonType::LEGACY
                      : ButtonType::NORMAL;
+  enable_tablet_mode_ = cl->HasSwitch(switches::kAshEnableTabletMode);
   force_clamshell_power_button_ =
       cl->HasSwitch(switches::kForceClamshellPowerButton);
 }
