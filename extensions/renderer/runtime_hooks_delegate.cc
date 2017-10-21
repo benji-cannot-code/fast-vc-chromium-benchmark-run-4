5d71c36b2fd78ce0cf1758a11c9aaa99ba2a835e
Devlin Cronin <rdevlin.cronin@chromium.org>
2017-10-21 01:27:34 +0000

    [Extensions Bindings] Add nativeMessaging support to runtime bindings
    Add support for native messaging to native runtime bindings. This
    includes adding handlers for connect and connectNative, as well as
    updating the IPC message sender. Add unittests for the new handlers.
    
    Bug: 653596
    Change-Id: Icdf54ab625841220d226a6bf60d0999943ba0ebe
    Reviewed-on: https://chromium-review.googlesource.com/710223
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510633}

--- extensions/renderer/runtime_hooks_delegate.cc
+++ extensions/renderer/runtime_hooks_delegate.cc
@@ -197,7 +197,9 @@ void GetExtensionId(v8::Local<v8::Name> property_name,
 constexpr char kGetManifest[] = "runtime.getManifest";
 constexpr char kGetURL[] = "runtime.getURL";
 constexpr char kConnect[] = "runtime.connect";
+constexpr char kConnectNative[] = "runtime.connectNative";
 constexpr char kSendMessage[] = "runtime.sendMessage";
+constexpr char kSendNativeMessage[] = "runtime.sendNativeMessage";
 
 constexpr char kSendMessageChannel[] = "chrome.runtime.sendMessage";
 
@@ -224,6 +226,8 @@ RequestResult RuntimeHooksDelegate::HandleRequest(
       {&RuntimeHooksDelegate::HandleConnect, kConnect},
       {&RuntimeHooksDelegate::HandleGetURL, kGetURL},
       {&RuntimeHooksDelegate::HandleGetManifest, kGetManifest},
+      {&RuntimeHooksDelegate::HandleConnectNative, kConnectNative},
+      {&RuntimeHooksDelegate::HandleSendNativeMessage, kSendNativeMessage},
   };
 
   ScriptContext* script_context =
@@ -328,7 +332,6 @@ RequestResult RuntimeHooksDelegate::HandleSendMessage(
   }
 
   v8::Local<v8::Value> v8_message = arguments[1];
-  DCHECK(!v8_message.IsEmpty());
   std::unique_ptr<Message> message =
       messaging_util::MessageFromV8(v8_context, v8_message);
   if (!message) {
@@ -345,6 +348,35 @@ RequestResult RuntimeHooksDelegate::HandleSendMessage(
       script_context, MessageTarget::ForExtension(target_id),
       kSendMessageChannel, options.include_tls_channel_id, *message,
       response_callback);
+
+  return RequestResult(RequestResult::HANDLED);
+}
+
+RequestResult RuntimeHooksDelegate::HandleSendNativeMessage(
+    ScriptContext* script_context,
+    const std::vector<v8::Local<v8::Value>>& arguments) {
+  DCHECK_EQ(3u, arguments.size());
+
+  std::string application_name = gin::V8ToString(arguments[0]);
+
+  v8::Local<v8::Value> v8_message = arguments[1];
+  DCHECK(!v8_message.IsEmpty());
+  std::unique_ptr<Message> message =
+      messaging_util::MessageFromV8(script_context->v8_context(), v8_message);
+  if (!message) {
+    RequestResult result(RequestResult::INVALID_INVOCATION);
+    result.error =
+        "Illegal argument to runtime.sendNativeMessage for 'message'.";
+    return result;
+  }
+
+  v8::Local<v8::Function> response_callback;
+  if (!arguments[2]->IsNull())
+    response_callback = arguments[2].As<v8::Function>();
+
+  messaging_service_->SendOneTimeMessage(
+      script_context, MessageTarget::ForNativeApp(application_name),
+      std::string(), false, *message, response_callback);
 
   return RequestResult(RequestResult::HANDLED);
 }
@@ -385,6 +417,22 @@ RequestResult RuntimeHooksDelegate::HandleConnect(
       script_context, MessageTarget::ForExtension(target_id),
       options.channel_name, options.include_tls_channel_id);
   DCHECK(!port.IsEmpty());
+
+  RequestResult result(RequestResult::HANDLED);
+  result.return_value = port.ToV8();
+  return result;
+}
+
+RequestResult RuntimeHooksDelegate::HandleConnectNative(
+    ScriptContext* script_context,
+    const std::vector<v8::Local<v8::Value>>& arguments) {
+  DCHECK_EQ(1u, arguments.size());
+  DCHECK(arguments[0]->IsString());
+
+  std::string application_name = gin::V8ToString(arguments[0]);
+  gin::Handle<GinPort> port = messaging_service_->Connect(
+      script_context, MessageTarget::ForNativeApp(application_name),
+      std::string(), false);
 
   RequestResult result(RequestResult::HANDLED);
   result.return_value = port.ToV8();
