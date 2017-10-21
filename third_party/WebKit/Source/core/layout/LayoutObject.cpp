882bf675c4624e7912a00bb99b5dba5eba08e917
Gyuyoung Kim <gyuyoung.kim@lge.com>
2017-10-21 08:12:12 +0000

    Remove WTFLogAlways in blink
    As a step not to use utilities in Assertions.h, this CL removes both WTFLogAlways() and its all
    of uses in the blink. Additionally vprintf_stderr_with_trailing_newline() is also removed because
    it has been only using by WTFLogAlways().
    
    BUG=638849
    
    Change-Id: Iab934423393de48a09cee2aee6e3703f843c3931
    Reviewed-on: https://chromium-review.googlesource.com/729382
    Commit-Queue: Gyuyoung Kim <gyuyoung.kim@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510673}

--- third_party/WebKit/Source/core/layout/LayoutObject.cpp
+++ third_party/WebKit/Source/core/layout/LayoutObject.cpp
@@ -1305,11 +1305,9 @@ void LayoutObject::ShowLayoutObject(StringBuilder& string_builder) const {
     while (string_builder.length() < kShowTreeCharacterOffset)
       string_builder.Append(' ');
     string_builder.Append('\t');
-    WTFLogAlways("%s%s", string_builder.ToString().Utf8().data(),
-                 GetNode()->ToString().Utf8().data());
-  } else {
-    WTFLogAlways("%s", string_builder.ToString().Utf8().data());
+    string_builder.Append(GetNode()->ToString());
   }
+  DLOG(INFO) << string_builder.ToString().Utf8().data();
 }
 
 void LayoutObject::ShowLayoutTreeAndMark(const LayoutObject* marked_object1,
@@ -3617,14 +3615,14 @@ void showTree(const blink::LayoutObject* object) {
   if (object)
     object->ShowTreeForThis();
   else
-    WTFLogAlways("%s", "Cannot showTree. Root is (nil)");
+    DLOG(INFO) << "Cannot showTree. Root is (nil)";
 }
 
 void showLineTree(const blink::LayoutObject* object) {
   if (object)
     object->ShowLineTreeForThis();
   else
-    WTFLogAlways("%s", "Cannot showLineTree. Root is (nil)");
+    DLOG(INFO) << "Cannot showLineTree. Root is (nil)";
 }
 
 void showLayoutTree(const blink::LayoutObject* object1) {
@@ -3639,7 +3637,7 @@ void showLayoutTree(const blink::LayoutObject* object1,
       root = root->Parent();
     root->ShowLayoutTreeAndMark(object1, "*", object2, "-", 0);
   } else {
-    WTFLogAlways("%s", "Cannot showLayoutTree. Root is (nil)");
+    DLOG(INFO) << "Cannot showLayoutTree. Root is (nil)";
   }
 }
 
