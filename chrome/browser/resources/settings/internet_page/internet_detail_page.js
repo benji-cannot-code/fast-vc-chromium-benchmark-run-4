a8c47efcca980bcb295c9048fe37398b7b24da91
Steven Bennetts <stevenjb@chromium.org>
2017-10-21 03:27:58 +0000

    Settings: Network: Fix Cellular siminfo
    Bug: 776901
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Id07479771abd6bf1eacfdc0abc13bbcfb12b6d25
    Reviewed-on: https://chromium-review.googlesource.com/730849
    Reviewed-by: Ben Chan <benchan@chromium.org>
    Commit-Queue: Ben Chan <benchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510653}

--- chrome/browser/resources/settings/internet_page/internet_detail_page.js
+++ chrome/browser/resources/settings/internet_page/internet_detail_page.js
@@ -1014,8 +1014,9 @@ Polymer({
    * @private
    */
   showCellularSim_: function(networkProperties) {
-    return networkProperties.Type == CrOnc.Type.CELLULAR && !!this.Cellular &&
-        this.Cellular.Family != 'CDMA';
+    return networkProperties.Type == CrOnc.Type.CELLULAR &&
+        !!networkProperties.Cellular &&
+        networkProperties.Cellular.Family != 'CDMA';
   },
 
   /**
